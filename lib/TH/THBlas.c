54bb1cf358f3b46f43ea6536e45e1d809a5e2f62
Ronan Collobert <ronan@collobert.com>
2012-02-01 14:33:10 +0100

    more cmake cleanup

--- lib/TH/THBlas.c
+++ lib/TH/THBlas.c
@@ -1,5 +1,4 @@
 #include "THBlas.h"
 
-/* #include "THCBlas.h" */
 #include "generic/THBlas.c"
 #include "THGenerateAllTypes.h"
--- lib/TH/THLapack.c
+++ lib/TH/THLapack.c
@@ -1,5 +1,4 @@
 #include "THLapack.h"
 
-/* #include "THCBlas.h" */
 #include "generic/THLapack.c"
 #include "THGenerateFloatTypes.h"
