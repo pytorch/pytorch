d0bc38e0a5fe61eb637127d4f6b54269e2371fe5
koray kavukcuoglu <koray@kavukcuoglu.org>
2012-03-07 10:44:15 -0500

    histogram correctly calculated for tensors where all elements are same.

--- lib/TH/generic/THTensorMath.c
+++ lib/TH/generic/THTensorMath.c
@@ -1145,6 +1145,11 @@ void THTensor_(histc)(THTensor *hist, THTensor *tensor, long nbins, real minvalu
     minval = THTensor_(minall)(tensor);
     maxval = THTensor_(maxall)(tensor);
   }
+  if (minval == maxval)
+  {
+    minval = minval - 1;
+    maxval = maxval + 1;
+  }
   real bins = (real)(nbins)-1e-6;
 
   THTensor *clone = THTensor_(newWithSize1d)(THTensor_(nElement)(tensor));
