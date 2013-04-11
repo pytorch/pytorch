f246b873260043fcdf4ac5b3a5454dfac8988776
Leon Bottou <leon@bottou.org>
2013-04-11 12:28:16 -0400

    introduce THC_API and work around log1p issue.

--- utils.h
+++ utils.h
@@ -12,9 +12,25 @@
 #include <unistd.h>
 #endif
 
-THLongStorage* torch_checklongargs(lua_State *L, int index);
-int torch_islongargs(lua_State *L, int index);
+#ifdef __cplusplus
+# define TORCH_EXTERNC extern "C"
+#else
+# define TORCH_EXTERNC extern
+#endif
 
-const char* torch_getdefaulttensortype(lua_State *L);
+#ifdef WIN32
+# ifdef torch_EXPORTS
+#  define TORCH_API TORCH_EXTERNC __declspec(dllexport)
+# else
+#  define TORCH_API TORCH_EXTERNC __declspec(dllimport)
+# endif
+#else
+# define TORCH_API TORCH_EXTERNC
+#endif
+
+
+TORCH_API THLongStorage* torch_checklongargs(lua_State *L, int index);
+TORCH_API int torch_islongargs(lua_State *L, int index);
+TORCH_API const char* torch_getdefaulttensortype(lua_State *L);
 
 #endif
