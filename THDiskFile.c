aa84a626312a5cf3c04d78e62f6fb4c41c23bf14
Ronan Collobert <ronan@collobert.com>
2013-10-08 11:24:29 +0200

    windows popen/pclose

--- THDiskFile.c
+++ THDiskFile.c
@@ -528,7 +528,11 @@ static void THPipeFile_free(THFile *self)
 {
   THDiskFile *dfself = (THDiskFile*)(self);
   if(dfself->handle)
+#ifdef _WIN32
+    _pclose(dfself->handle);
+#else
     pclose(dfself->handle);
+#endif
   THFree(dfself->name);
   THFree(dfself);
 }
@@ -572,7 +576,7 @@ THFile *THPipeFile_new(const char *name, const char *mode, int isQuiet)
   THArgCheck(THPipeFile_mode(mode, &isReadable, &isWritable), 2, "file mode should be 'r','w'");
 
 #ifdef _WIN32
-  handle = popen(name, (isReadable ? "rb" : "wb"));
+  handle = _popen(name, (isReadable ? "rb" : "wb"));
 #else
   handle = popen(name, (isReadable ? "r" : "w"));
 #endif
