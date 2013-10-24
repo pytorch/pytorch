e23732735ab203d814795f9df8aa09e1dc66a481
Julien Cornebise <julien.cornebise@gmail.com>
2013-10-24 17:19:48 +0100

    Plot time for constant array

--- test/timeSort.lua
+++ test/timeSort.lua
@@ -7,6 +7,7 @@ function testSort(output, descending)
     local pow10 = torch.linspace(1,5,10)
     local bench_rnd = torch.zeros(pow10:numel())
     local bench_srt = torch.zeros(pow10:numel())
+    local bench_cst = torch.zeros(pow10:numel())
     local nrep = 3
 
     local function time_sort(x)
@@ -27,22 +28,30 @@ function testSort(output, descending)
             bench_rnd[i] = bench_rnd[i] + this_time/nrep
             collectgarbage()
 
-            -- on random
+            -- on sorted
             this_time = time_sort(torch.linspace(0,1,n))
             print('SRT j:', j, 'for 10^', pow10[i], ' time: ', this_time)
             bench_srt[i] = bench_srt[i] + this_time/nrep
+            collectgarbage()
+
+            -- on constant
+            this_time = time_sort(torch.zeros(n))
+            print('CST j:', j, 'for 10^', pow10[i], ' time: ', this_time)
+            bench_cst[i] = bench_cst[i] + this_time/nrep
             collectgarbage()
 
         end
         io.flush()
     end
     gnuplot.plot({'Random', pow10, bench_rnd},
-                 {'Sorted', pow10, bench_srt})
+                 {'Sorted', pow10, bench_srt},
+                 {'Constant', pow10, bench_cst})
     gnuplot.xlabel('Log10(N)')
     gnuplot.ylabel('Time (s)')
     gnuplot.figprint(output)
     print('RND:', bench_rnd)
     print('SRT:', bench_srt)
+    print('CST:', bench_cst)
 end
 
 testSort('timeSortAscending.png', false) -- Ascending
