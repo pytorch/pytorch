c68b50d4cf585ef63a8ff2ce30f9fed44ec0b87d
Hycis <hyciswu@gmail.com>
2014-06-11 22:01:41 -0700

    small bug

--- THCTensorMath.h
+++ THCTensorMath.h
@@ -76,7 +76,7 @@ THC_API float THCudaTensor_dist(THCudaTensor *self, THCudaTensor *src, float val
 THC_API void THCudaTensor_rand(THCudaTensor *r_, THLongStorage *size);
 THC_API void THCudaTensor_randn(THCudaTensor *r_, THLongStorage *size);
 
-THC_API void THCudaTensor_indexSelect(THCudaTensor *tensor, THCudaTensor *src, int dim, THLongTensor *index)
+THC_API void THCudaTensor_indexSelect(THCudaTensor *tensor, THCudaTensor *src, int dim, THLongTensor *index);
 
 
 #endif
