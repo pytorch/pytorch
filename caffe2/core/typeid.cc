33f99d137f7cc452f4b6458f1f14a7c63516de74
Yangqing Jia <jiayq84@gmail.com>
2015-07-05 22:23:56 -0700

    [bugfix] static initialization order fiasco.

--- caffe2/core/typeid.cc
+++ caffe2/core/typeid.cc
@@ -5,7 +5,10 @@
 namespace caffe2 {
 namespace internal {
 
-std::map<TypeId, string> g_caffe2_type_name_map;
+std::map<TypeId, string>& Caffe2TypeNameMap() {
+  static std::map<TypeId, string> g_caffe2_type_name_map;
+  return g_caffe2_type_name_map;
+}
 
 }  // namespace internal
 }  // namespace caffe2
