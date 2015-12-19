d7725e3484a5924442f1ff4f2d944080107c42db
Yangqing Jia <jiayq84@gmail.com>
2015-12-18 22:38:30 -0800

    speed benchmark

---
+++ caffe2/binaries/speed_benchmark.cc
@@ -0,0 +1,26 @@
+#include "caffe2/core/init.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/proto/caffe2.pb.h"
+#include "caffe2/utils/proto_utils.h"
+#include "caffe2/core/logging.h"
+
+CAFFE2_DEFINE_string(net, "", "The given net to benchmark.");
+CAFFE2_DEFINE_string(init_net, "",
+                     "The given net to initialize any parameters.");
+CAFFE2_DEFINE_int(warmup, 0, "The number of iterations to warm up.");
+CAFFE2_DEFINE_int(iter, 10, "The number of iterations to run.");
+
+int main(int argc, char** argv) {
+  caffe2::GlobalInit(&argc, argv);
+  std::unique_ptr<caffe2::Workspace> workspace(new caffe2::Workspace());
+  // Run initialization network.
+  caffe2::NetDef net_def;
+  CAFFE_CHECK(ReadProtoFromFile(caffe2::FLAGS_init_net, &net_def));
+  CAFFE_CHECK(workspace->RunNetOnce(net_def));
+  CAFFE_CHECK(ReadProtoFromFile(caffe2::FLAGS_net, &net_def));
+  caffe2::NetBase* net = workspace->CreateNet(net_def);
+  CAFFE_CHECK_NOTNULL(net);
+  CAFFE_CHECK(net->Run());
+  net->TEST_Benchmark(caffe2::FLAGS_warmup, caffe2::FLAGS_iter, true);
+  return 0;
+}
