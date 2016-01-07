e2b9172b4c947d29f6c9643b120f4e2db325cb2c
Yangqing Jia <jiayq84@gmail.com>
2016-01-07 18:49:41 +0000

    print cudnn version

---
+++ caffe2/core/common_cudnn.cc
@@ -0,0 +1,17 @@
+#include <sstream>
+
+#include "caffe2/core/common_cudnn.h"
+#include "caffe2/core/init.h"
+
+namespace caffe2 {
+namespace {
+bool PrintCuDNNInfo() {
+  CAFFE_LOG_INFO << "Caffe2 is built with CuDNN version " << CUDNN_VERSION;
+  return true;
+}
+
+REGISTER_CAFFE2_INIT_FUNCTION(PrintCuDNNInfo, &PrintCuDNNInfo,
+                              "Print CuDNN Info for possible inspection.");
+
+}  // namespace
+}  // namespace caffe2
