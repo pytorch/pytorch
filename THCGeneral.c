275dc502230bf021d54d2471f560951c5f575f6c
Hugh Perkins <hughperkins@gmail.com>
2016-02-15 12:18:53 +0000

    Add printf to __THCudaCheck fail clause, in case stack unwind itself crashes

--- THCGeneral.c
+++ THCGeneral.c
@@ -520,6 +520,11 @@ void __THCudaCheck(cudaError_t err, const char *file, const int line)
 {
   if(err != cudaSuccess)
   {
+    static int alreadyFailed = 0;
+    if(!alreadyFailed) {
+      fprintf(stderr, "THCudaCheck FAIL file=%s line=%i error=%i : %s\n", file, line, err, cudaGetErrorString(err));
+      alreadyFailed = 1;
+    }
     _THError(file, line, "cuda runtime error (%d) : %s", err,
              cudaGetErrorString(err));
   }
