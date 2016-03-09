747614b1aa6916e61bc6593d108076a1ef92c922
Aiden Nibali <dismaldenizen@gmail.com>
2016-03-09 15:54:00 +1100

    In-place ELU

--- generic/ELU.c
+++ generic/ELU.c
@@ -6,12 +6,22 @@ void THNN_(ELU_updateOutput)(
           THNNState *state,
           THTensor *input,
           THTensor *output,
-          real alpha)
+          real alpha,
+          bool inplace)
 {
-  THTensor_(resizeAs)(output, input);
-  TH_TENSOR_APPLY2(real, input, real, output,
-    *output_data = *input_data <= 0 ? (exp(*input_data)-1)*alpha : *input_data;
-  );
+  if(inplace) {
+    TH_TENSOR_APPLY(real, input,
+      if(*input_data <= 0) {
+        *input_data = (exp(*input_data) - 1) * alpha;
+      }
+    );
+    THTensor_(set)(output, input);
+  } else {
+    THTensor_(resizeAs)(output, input);
+    TH_TENSOR_APPLY2(real, input, real, output,
+      *output_data = *input_data <= 0 ? (exp(*input_data)-1)*alpha : *input_data;
+    );
+  }
 }
 
 void THNN_(ELU_updateGradInput)(
@@ -20,12 +30,22 @@ void THNN_(ELU_updateGradInput)(
           THTensor *gradOutput,
           THTensor *gradInput,
           THTensor *output,
-          real alpha)
+          real alpha,
+          bool inplace)
 {
-  THTensor_(resizeAs)(gradInput, output);
-  TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, output,
-    *gradInput_data = *output_data <= 0 ? *gradOutput_data * (*output_data + alpha) : *gradOutput_data;
-  );
+  if(inplace) {
+    TH_TENSOR_APPLY2(real, gradOutput, real, output,
+      if(*output_data <= 0) {
+        *gradOutput_data *= *output_data + alpha;
+      }
+    );
+    THTensor_(set)(gradInput, gradOutput);
+  } else {
+    THTensor_(resizeAs)(gradInput, output);
+    TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, output,
+      *gradInput_data = *output_data <= 0 ? *gradOutput_data * (*output_data + alpha) : *gradOutput_data;
+    );
+  }
 }
 
 #endif
