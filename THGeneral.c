6db57b8a4d0ae1a9efe542fc7952dda499862a93
tpltnt <tpltnt.github@dropcut.net>
2016-05-07 15:08:25 +0200

    added correct inclusion for malloc_usable_size()

--- THGeneral.c
+++ THGeneral.c
@@ -6,7 +6,11 @@
 #endif
 
 #if (defined(__unix) || defined(_WIN32))
-#include <malloc.h>
+  #if defined(__FreeBSD__)
+    #include <malloc_np.h>
+  #else
+    #include <malloc.h>
+  #endif
 #elif defined(__APPLE__)
 #include <malloc/malloc.h>
 #endif
