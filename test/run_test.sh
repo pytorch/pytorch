47700a4c20a22d0fa00ea6f05f91aa45a853de80
Soumith Chintala <soumith@fb.com>
2016-09-06 23:11:21 -0400

    fixing run_test.sh to switch to it's own directory before running commands

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+pushd "$(dirname "$0")"
+
 python test_torch.py
 python test_autograd.py
 python test_nn.py
@@ -5,4 +10,8 @@ python test_legacy_nn.py
 if which nvcc >/dev/null 2>&1
 then
     python test_cuda.py
+else
+    echo "nvcc not found in PATH, skipping CUDA tests"
 fi
+
+popd
