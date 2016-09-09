f5755e493ade4ddf7d09c137328628811d8d84c3
soumith <soumith@fb.com>
2016-09-09 12:31:15 -0700

    splitting sort compilation into individual types

---
+++ THCGenerateCharType.h
@@ -0,0 +1,20 @@
+#ifndef THC_GENERIC_FILE
+#error "You must define THC_GENERIC_FILE before including THGenerateCharType.h"
+#endif
+
+#define real char
+#define accreal long
+#define Real Char
+#define CReal CudaChar
+#define THC_REAL_IS_CHAR
+#line 1 THC_GENERIC_FILE
+#include THC_GENERIC_FILE
+#undef real
+#undef accreal
+#undef Real
+#undef CReal
+#undef THC_REAL_IS_CHAR
+
+#ifndef THCGenerateAllTypes
+#undef THC_GENERIC_FILE
+#endif
