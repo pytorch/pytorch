621d6a44756970b13212556fb8d691bbe0f6a0d7
soumith <soumith@fb.com>
2016-09-09 12:31:15 -0700

    splitting sort compilation into individual types

---
+++ THCGenerateDoubleType.h
@@ -0,0 +1,20 @@
+#ifndef THC_GENERIC_FILE
+#error "You must define THC_GENERIC_FILE before including THGenerateDoubleType.h"
+#endif
+
+#define real double
+#define accreal double
+#define Real Double
+#define CReal CudaDouble
+#define THC_REAL_IS_DOUBLE
+#line 1 THC_GENERIC_FILE
+#include THC_GENERIC_FILE
+#undef real
+#undef accreal
+#undef Real
+#undef CReal
+#undef THC_REAL_IS_DOUBLE
+
+#ifndef THCGenerateAllTypes
+#undef THC_GENERIC_FILE
+#endif
