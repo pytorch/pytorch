31c45e0a08db5ebd7ade93251e2c83ee341fe2bb
soumith <soumith@gmail.com>
2016-09-12 01:02:44 -0400

    adding verbose run_test.sh

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,14 +3,26 @@ set -e
 
 pushd "$(dirname "$0")"
 
+echo "Running torch tests"
 python test_torch.py
+
+echo "Running autograd tests"
 python test_autograd.py
+
+echo "Running nn tests"
 python test_nn.py
+
+echo "Running legacy nn tests"
 python test_legacy_nn.py
+
+echo "Running multiprocessing tests"
 python test_multiprocessing.py
+
+echo "Running util tests"
 python test_utils.py
 if which nvcc >/dev/null 2>&1
 then
+    echo "Running cuda tests"
     python test_cuda.py
 else
     echo "nvcc not found in PATH, skipping CUDA tests"
