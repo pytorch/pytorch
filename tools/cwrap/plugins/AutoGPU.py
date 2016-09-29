626da01c329b67712b45da0c8f8ae1062ce9a834
Sam Gross <sgross@fb.com>
2016-09-28 18:44:51 -0700

    Auto GPU for CUNN

--- tools/cwrap/plugins/AutoGPU.py
+++ tools/cwrap/plugins/AutoGPU.py
@@ -2,6 +2,10 @@ from . import CWrapPlugin
 
 class AutoGPU(CWrapPlugin):
 
+    def __init__(self, has_self=True, condition=None):
+        self.has_self = has_self
+        self.condition = condition
+
     DEFINES = """
 #ifdef THC_GENERIC_FILE
 #define THCP_AUTO_GPU 1
@@ -10,16 +14,14 @@ class AutoGPU(CWrapPlugin):
 #endif
 """
 
-    BEFORE_CALL = """
-#if IS_CUDA
-      THCPAutoGPU __autogpu_guard = THCPAutoGPU(args, (PyObject*)self);
-#endif
-"""
-
     def process_option_code_template(self, template, option):
-        return [self.BEFORE_CALL] + template
+        call = 'THCPAutoGPU __autogpu_guard = THCPAutoGPU(args{});'.format(
+            ', (PyObject*)self' if self.has_self else '')
+
+        if self.condition is not None:
+            call = "#if {0}\n      {1}\n#endif\n".format(self.condition, call)
+
+        return [call] + template
 
     def process_full_file(self, code):
         return self.DEFINES + code
-
-
--- torch/csrc/cuda/Tensor.cpp
+++ torch/csrc/cuda/Tensor.cpp
@@ -10,54 +10,49 @@
 
 #include "override_macros.h"
 
-class THCPAutoGPU {
-public:
-  THCPAutoGPU(PyObject *args, PyObject *self=NULL) {
-    if (self && setDevice(self))
-      return;
+THCPAutoGPU::THCPAutoGPU(PyObject *args, PyObject *self) {
+  if (self && setDevice(self))
+    return;
 
-    if (!args)
-      return;
-    for (int i = 0; i < PyTuple_Size(args); i++) {
-      PyObject *arg = PyTuple_GET_ITEM(args, i);
-      if (setDevice(arg)) return;
-    }
+  if (!args)
+    return;
+  for (int i = 0; i < PyTuple_Size(args); i++) {
+    PyObject *arg = PyTuple_GET_ITEM(args, i);
+    if (setDevice(arg)) return;
   }
+}
 
-  bool setDevice(PyObject *obj) {
-    int new_device = -1;
-    PyObject *obj_type = (PyObject*)Py_TYPE(obj);
-    if (obj_type == THCPDoubleTensorClass) {
-      new_device = THCudaDoubleTensor_getDevice(LIBRARY_STATE ((THCPDoubleTensor*)obj)->cdata);
-    } else if (obj_type == THCPFloatTensorClass) {
-      new_device = THCudaTensor_getDevice(LIBRARY_STATE ((THCPFloatTensor*)obj)->cdata);
-    } else if (obj_type == THCPLongTensorClass) {
-      new_device = THCudaLongTensor_getDevice(LIBRARY_STATE ((THCPLongTensor*)obj)->cdata);
-    } else if (obj_type == THCPIntTensorClass) {
-      new_device = THCudaIntTensor_getDevice(LIBRARY_STATE ((THCPIntTensor*)obj)->cdata);
-    } else if (obj_type == THCPShortTensorClass) {
-      new_device = THCudaShortTensor_getDevice(LIBRARY_STATE ((THCPShortTensor*)obj)->cdata);
-    } else if (obj_type == THCPCharTensorClass) {
-      new_device = THCudaCharTensor_getDevice(LIBRARY_STATE ((THCPCharTensor*)obj)->cdata);
-    } else if (obj_type == THCPByteTensorClass) {
-      new_device = THCudaByteTensor_getDevice(LIBRARY_STATE ((THCPByteTensor*)obj)->cdata);
-    }
-    if (new_device != -1) {
-      THCudaCheck(cudaGetDevice(&device));
-      THCPModule_setDevice(new_device);
-      return true;
-    }
-    return false;
+bool THCPAutoGPU::setDevice(PyObject *obj) {
+  int new_device = -1;
+  PyObject *obj_type = (PyObject*)Py_TYPE(obj);
+  if (obj_type == THCPDoubleTensorClass) {
+    new_device = THCudaDoubleTensor_getDevice(LIBRARY_STATE ((THCPDoubleTensor*)obj)->cdata);
+  } else if (obj_type == THCPFloatTensorClass) {
+    new_device = THCudaTensor_getDevice(LIBRARY_STATE ((THCPFloatTensor*)obj)->cdata);
+  } else if (obj_type == THCPLongTensorClass) {
+    new_device = THCudaLongTensor_getDevice(LIBRARY_STATE ((THCPLongTensor*)obj)->cdata);
+  } else if (obj_type == THCPIntTensorClass) {
+    new_device = THCudaIntTensor_getDevice(LIBRARY_STATE ((THCPIntTensor*)obj)->cdata);
+  } else if (obj_type == THCPShortTensorClass) {
+    new_device = THCudaShortTensor_getDevice(LIBRARY_STATE ((THCPShortTensor*)obj)->cdata);
+  } else if (obj_type == THCPCharTensorClass) {
+    new_device = THCudaCharTensor_getDevice(LIBRARY_STATE ((THCPCharTensor*)obj)->cdata);
+  } else if (obj_type == THCPByteTensorClass) {
+    new_device = THCudaByteTensor_getDevice(LIBRARY_STATE ((THCPByteTensor*)obj)->cdata);
   }
-
-  // This can throw... But if it does I have no idea how to recover.
-  ~THCPAutoGPU() {
-    if (device != -1)
-      THCPModule_setDevice(device);
+  if (new_device != -1) {
+    THCudaCheck(cudaGetDevice(&device));
+    THCPModule_setDevice(new_device);
+    return true;
   }
+  return false;
+}
 
-  int device = -1;
-};
+// This can throw... But if it does I have no idea how to recover.
+THCPAutoGPU::~THCPAutoGPU() {
+  if (device != -1)
+    THCPModule_setDevice(device);
+}
 
 #define THC_GENERIC_FILE "torch/csrc/generic/Tensor.cpp"
 #include <THC/THCGenerateAllTypes.h>
@@ -70,4 +65,3 @@ public:
 
 #include "generic/TensorCopyAsync.cpp"
 #include <THC/THCGenerateAllTypes.h>
-
