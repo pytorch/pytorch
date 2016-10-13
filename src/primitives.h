b2781d050179b158c053edc4614cb147de712554
Sylvain Jeaugey <sjeaugey@nvidia.com>
2016-10-13 10:32:42 -0700

    Fix primitives function prototype

--- src/primitives.h
+++ src/primitives.h
@@ -177,29 +177,29 @@ class Primitives {
   template <typename... SYNC_Ts>
   static __device__ __forceinline__ void
   Copy(const T* src, T* dst,
-      int len, int step, SYNC_Ts... flags) {
-    GenericOp(src, nullptr, dst, nullptr, len, step, flags...);
+      int len, int maxOffset, int step, SYNC_Ts... flags) {
+    GenericOp(src, nullptr, dst, nullptr, len, maxOffset, step, flags...);
   }
 
   template <typename... SYNC_Ts>
   static __device__ __forceinline__ void
   DoubleCopy(const T* src, T* dst1, T* dst2,
-      int len, int step, SYNC_Ts... flags) {
-    GenericOp(src, nullptr, dst1, dst2, len, step, flags...);
+      int len, int maxOffset, int step, SYNC_Ts... flags) {
+    GenericOp(src, nullptr, dst1, dst2, len, maxOffset, step, flags...);
   }
 
   template <typename... SYNC_Ts>
   static __device__ __forceinline__ void
   Reduce(const T* src1, const T* src2, T* dst,
-      int len, int step, SYNC_Ts... flags) {
-    GenericOp(src1, src2, dst, nullptr, len, step, flags...);
+      int len, int maxOffset, int step, SYNC_Ts... flags) {
+    GenericOp(src1, src2, dst, nullptr, len, maxOffset, step, flags...);
   }
 
   template <typename... SYNC_Ts>
   static __device__ __forceinline__ void
   ReduceCopy(const T* src1, const T* src2, T* dst1, T* dst2,
-      int len, int step, SYNC_Ts... flags) {
-    GenericOp(src1, src2, dst1, dst2, len, step, flags...);
+      int len, int maxOffset, int step, SYNC_Ts... flags) {
+    GenericOp(src1, src2, dst1, dst2, len, maxOffset, step, flags...);
   }
 };
 
