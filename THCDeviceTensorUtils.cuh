4d0d775d1620d5bfa5a8ee5effee2d6727d9003d
Gregory Chanan <gchanan@fb.com>
2016-10-13 15:43:48 -0700

    Add generic type support for toDeviceTensor.

--- THCDeviceTensorUtils.cuh
+++ THCDeviceTensorUtils.cuh
@@ -3,25 +3,7 @@
 
 #include "THCDeviceTensor.cuh"
 #include "THCTensor.h"
-
-/// Constructs a THCDeviceTensor initialized from a THCudaTensor. Will
-/// error if the dimensionality does not match exactly.
-template <typename T, int Dim,
-          typename IndexT, template <typename U> class PtrTraits>
-THCDeviceTensor<T, Dim, IndexT, PtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t);
-
-template <typename T, int Dim, typename IndexT>
-THCDeviceTensor<T, Dim, IndexT, DefaultPtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t) {
-  return toDeviceTensor<T, Dim, IndexT, DefaultPtrTraits>(state, t);
-}
-
-template <typename T, int Dim>
-THCDeviceTensor<T, Dim, int, DefaultPtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t) {
-  return toDeviceTensor<T, Dim, int, DefaultPtrTraits>(state, t);
-}
+#include <limits>
 
 /// Constructs a DeviceTensor initialized from a THCudaTensor by
 /// upcasting or downcasting the tensor to that of a different
@@ -42,6 +24,9 @@ THCDeviceTensor<T, Dim, int, DefaultPtrTraits>
 toDeviceTensorCast(THCState* state, THCudaTensor* t) {
   return toDeviceTensorCast<T, Dim, int, DefaultPtrTraits>(state, t);
 }
+
+#include "generic/THCDeviceTensorUtils.cu"
+#include "THCGenerateAllTypes.h"
 
 #include "THCDeviceTensorUtils-inl.cuh"
 
