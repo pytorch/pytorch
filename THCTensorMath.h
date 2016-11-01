16cac6442a1b9f6328e39ccd283694cb7163fc10
soumith <soumith@fb.com>
2016-10-31 20:03:13 -0700

    adding multiple types for cumsum, cumprod

--- THCTensorMath.h
+++ THCTensorMath.h
@@ -25,6 +25,9 @@
 #include "generic/THCTensorMathCompareT.h"
 #include "THCGenerateAllTypes.h"
 
+#include "generic/THCTensorMathScan.h"
+#include "THCGenerateAllTypes.h"
+
 #include "generic/THCTensorMasked.h"
 #include "THCGenerateAllTypes.h"
 
@@ -36,9 +39,6 @@
 
 #include "generic/THCTensorSort.h"
 #include "THCGenerateAllTypes.h"
-
-THC_API void THCudaTensor_cumsum(THCState *state, THCudaTensor *self, THCudaTensor *src, long dim);
-THC_API void THCudaTensor_cumprod(THCState *state, THCudaTensor *self, THCudaTensor *src, long dim);
 
 // MAGMA (i.e. CUDA implementation of LAPACK functions)
 THC_API void THCudaTensor_gesv(THCState *state, THCudaTensor *rb_, THCudaTensor *ra_, THCudaTensor *b_, THCudaTensor *a_);
