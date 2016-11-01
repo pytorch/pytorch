43fe64446b09d4c38c6a082ed89bbcf376f7dcb8
Adam Paszke <adam.paszke@gmail.com>
2016-11-01 13:21:14 +0100

    Improve Parameter's __repr__

--- torch/nn/parameter.py
+++ torch/nn/parameter.py
@@ -5,3 +5,6 @@ class Parameter(Variable):
     def __init__(self, data, requires_grad=True):
         super(Parameter, self).__init__(data, requires_grad=requires_grad)
 
+    def __repr__(self):
+        return 'Parameter containing:' + self.data.__repr__()
+
