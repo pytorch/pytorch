8fd9cc160c76c168f6b946514f018d0fdf951d40
Trevor Killeen <killeentm@gmail.com>
2016-11-07 13:26:59 -0800

    [cutorch rand2gen] move normal to generic

--- generic/THCTensorRandom.cu
+++ generic/THCTensorRandom.cu
@@ -18,6 +18,22 @@ THC_API void THCTensor_(uniform)(THCState* state, THCTensor *self_, double a, do
 
   THCTensor_(freeCopyTo)(state, self, self_);
 };
+
+GENERATE_KERNEL2(generate_normal, real, double mean, double stdv, float, curand_normal, (ScalarConvert<float, real>::to((x * stdv) + mean)))
+
+THC_API void THCTensor_(normal)(THCState* state, THCTensor *self_, double mean, double stdv)
+{
+  THAssert(THCTensor_(checkGPU)(state, 1, self_));
+  Generator* gen = THCRandom_getGenerator(state);
+  THCTensor *self = THCTensor_(newContiguous)(state, self_);
+  ptrdiff_t size = THCTensor_(nElement)(state, self);
+  real *data = THCTensor_(data)(state, self);
+
+  generate_normal<<<NUM_BLOCKS, BLOCK_SIZE, 0, THCState_getCurrentStream(state)>>>(
+      gen->gen_states, size, data, mean, stdv);
+
+  THCTensor_(freeCopyTo)(state, self, self_);
+};
 #undef NUM_BLOCKS
 
 THC_API void THCTensor_(rand)(THCState *state, THCTensor *r_, THLongStorage *size)
@@ -45,6 +61,7 @@ THC_API void THCTensor_(bernoulli)(THCState* state, THCTensor *self_, double p)
 
   THCTensor_(freeCopyTo)(state, self, self_);
 };
+
 #undef NUM_BLOCKS
 
 #endif
