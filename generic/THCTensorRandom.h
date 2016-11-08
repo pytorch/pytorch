16728d2f26f1acba264fc59a29e1b9cb4efa845c
Trevor Killeen <killeentm@gmail.com>
2016-11-08 09:34:19 -0800

    [cutorch rand2gen] move multinomial to generic

--- generic/THCTensorRandom.h
+++ generic/THCTensorRandom.h
@@ -9,6 +9,7 @@ THC_API void THCTensor_(rand)(THCState *state, THCTensor *r_, THLongStorage *siz
 THC_API void THCTensor_(normal)(struct THCState *state, THCTensor *self, double mean, double stdv);
 THC_API void THCTensor_(exponential)(struct THCState *state, THCTensor *self, double lambda);
 THC_API void THCTensor_(cauchy)(struct THCState *state, THCTensor *self, double median, double sigma);
+THC_API void THCTensor_(multinomial)(struct THCState *state, THCTensor *self, THCTensor *prob_dist, int n_sample, int with_replacement);
 
 #endif
 
