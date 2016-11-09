b1116329655c9b10740fe61baf86dade533a501e
Trevor Killeen <killeentm@gmail.com>
2016-11-09 11:09:30 -0800

    [cutorch rand2gen] move randn to generic

--- generic/THCTensorRandom.h
+++ generic/THCTensorRandom.h
@@ -6,6 +6,7 @@
 
 THC_API void THCTensor_(uniform)(struct THCState *state, THCTensor *self, double a, double b);
 THC_API void THCTensor_(rand)(THCState *state, THCTensor *r_, THLongStorage *size);
+THC_API void THCTensor_(randn)(THCState *state, THCTensor *r_, THLongStorage *size);
 THC_API void THCTensor_(normal)(struct THCState *state, THCTensor *self, double mean, double stdv);
 THC_API void THCTensor_(logNormal)(struct THCState *state, THCTensor *self, double mean, double stdv);
 THC_API void THCTensor_(exponential)(struct THCState *state, THCTensor *self, double lambda);
