3838dc2fd8c8c03abb96bd6580a2dbeef83e565c
Adam Paszke <adam.paszke@gmail.com>
2016-11-30 20:24:10 +0100

    Remove optim submodule attributes from torch.optim package

--- torch/optim/__init__.py
+++ torch/optim/__init__.py
@@ -6,3 +6,13 @@ from .asgd import ASGD
 from .sgd import SGD
 from .rprop import Rprop
 from .rmsprop import RMSprop
+
+del adadelta
+del adagrad
+del adam
+del adamax
+del asgd
+del sgd
+del rprop
+del rmsprop
+del optimizer
