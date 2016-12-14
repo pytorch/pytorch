9212b9ca0954a427ade830b94b75d3fef883641b
Eric Cosatto <cosatto@nec-labs.com>
2016-12-14 18:36:03 -0500

    fix wrong export directive for THCCachingHostAllocator (#633)
    fix wrong export directive for THCCachingHostAllocator

--- THCCachingHostAllocator.h
+++ THCCachingHostAllocator.h
@@ -18,7 +18,7 @@
 // Note that this allocator does not split larger allocations into smaller
 // blocks, unlike the caching device allocator.
 //
-extern THAllocator THCCachingHostAllocator;
+THC_API THAllocator THCCachingHostAllocator;
 
 // Records an event in the specified stream. The allocation 'ptr' will not be
 // re-used until the event has occured.
