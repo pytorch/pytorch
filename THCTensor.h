0c1c0e21b8c90545bfc451589c9ebec774361b6e
Gao Yingkai <popol.subscribe@gmail.com>
2016-12-16 15:37:06 -0500

    Bugfix of type in THCTensor macro.
    A fix for issue #632.

--- THCTensor.h
+++ THCTensor.h
@@ -5,8 +5,8 @@
 #include "THCStorage.h"
 #include "THCGeneral.h"
 
-#define THCTensor          TH_CONCAT_3(TH,CReal,Tensor)
-#define THCTensor_(NAME)   TH_CONCAT_4(TH,CReal,Tensor_,NAME)
+#define THCTensor          TH_CONCAT_3(THC,Real,Tensor)
+#define THCTensor_(NAME)   TH_CONCAT_4(THC,Real,Tensor_,NAME)
 
 #define THC_DESC_BUFF_LEN 64
 
