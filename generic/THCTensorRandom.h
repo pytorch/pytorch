f2606a7502473c53e40a5b3412e690b7e8143880
Adam Paszke <adam.paszke@gmail.com>
2016-12-27 21:17:45 +0100

    Make multinomial return a LongTensor (compatible with CPU version)

--- generic/THCTensorRandom.h
+++ generic/THCTensorRandom.h
@@ -11,7 +11,7 @@ THC_API void THCTensor_(normal)(struct THCState *state, THCTensor *self, double 
 THC_API void THCTensor_(logNormal)(struct THCState *state, THCTensor *self, double mean, double stdv);
 THC_API void THCTensor_(exponential)(struct THCState *state, THCTensor *self, double lambda);
 THC_API void THCTensor_(cauchy)(struct THCState *state, THCTensor *self, double median, double sigma);
-THC_API void THCTensor_(multinomial)(struct THCState *state, THCTensor *self, THCTensor *prob_dist, int n_sample, int with_replacement);
+THC_API void THCTensor_(multinomial)(struct THCState *state, THCudaLongTensor *self, THCTensor *prob_dist, int n_sample, int with_replacement);
 
 #endif
 
