89c812b62c8df81061dce60bf92e1f4410469338
Jason Jeong <joosjeong@fb.com>
2016-12-27 11:39:50 -0800

    specify path to write htrace logs
    Summary: This diff adds a gflag for specifying the path for htrace span log files. This flag is used by the net types `HTraceDAGNet` and `HTraceAsyncDAGNet`.
    
    Differential Revision: D4366849
    
    fbshipit-source-id: 56038d3d64a3fd5ab363feda86a19a6f2496971c

--- caffe2/python/convnet_benchmarks.py
+++ caffe2/python/convnet_benchmarks.py
@@ -630,7 +630,7 @@ def GetArgumentParser():
     parser.add_argument("--net_type", type=str, default="dag")
     parser.add_argument("--num_workers", type=int, default=2)
     parser.add_argument("--use-nvtx", default=False, action='store_true')
-    parser.add_argument("--htrace_conf", type=str)
+    parser.add_argument("--htrace_span_log_path", type=str)
     return parser
 
 
@@ -645,8 +645,9 @@ if __name__ == '__main__':
     workspace.GlobalInit(
         ['caffe2', '--caffe2_log_level=0'] +
         (['--caffe2_use_nvtx'] if args.use_nvtx else []) +
-        (['--caffe2_htrace_conf=' + args.htrace_conf]
-            if args.htrace_conf else []))
+        (['--caffe2_htrace_span_log_path=' + args.htrace_span_log_path]
+            if args.htrace_span_log_path else []))
+
     model_map = {
         'AlexNet': AlexNet,
         'OverFeat': OverFeat,
