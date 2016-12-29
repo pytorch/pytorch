989a2cc7b720c2cc0f5cf488597fd567b96f5d4a
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-12-29 15:23:30 +0100

    F.tanh and F.sigmoid in rnn

--- torch/nn/functions/rnn.py
+++ torch/nn/functions/rnn.py
@@ -13,7 +13,7 @@ def RNNReLUCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
 
 
 def RNNTanhCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
-        hy = torch.tanh(F.linear(input, w_ih, b_ih) + F.linear(hidden, w_hh, b_hh))
+        hy = F.tanh(F.linear(input, w_ih, b_ih) + F.linear(hidden, w_hh, b_hh))
         return hy
 
 
@@ -22,13 +22,13 @@ def LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
         gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
         ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
 
-        ingate = torch.sigmoid(ingate)
-        forgetgate = torch.sigmoid(forgetgate)
-        cellgate = torch.tanh(cellgate)
-        outgate = torch.sigmoid(outgate)
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
 
         cy = (forgetgate * cx) + (ingate * cellgate)
-        hy = outgate * torch.tanh(cy)
+        hy = outgate * F.tanh(cy)
 
         return hy, cy
 
@@ -39,9 +39,9 @@ def GRUCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
         i_r, i_i, i_n = gi.chunk(3, 1)
         h_r, h_i, h_n = gh.chunk(3, 1)
 
-        resetgate = torch.sigmoid(i_r + h_r)
-        inputgate = torch.sigmoid(i_i + h_i)
-        newgate = torch.tanh(i_n + resetgate * h_n)
+        resetgate = F.sigmoid(i_r + h_r)
+        inputgate = F.sigmoid(i_i + h_i)
+        newgate = F.tanh(i_n + resetgate * h_n)
         hy = newgate + inputgate * (hidden - newgate)
 
         return hy
