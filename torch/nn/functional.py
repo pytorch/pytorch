f0195e795b8ffc012bc643f20e9c9560573e6123
Adam Paszke <adam.paszke@gmail.com>
2016-12-30 19:15:21 +0100

    Move PixelShuffle implementation to functional

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -373,3 +373,34 @@ def cross_entropy(input, target, weight=None, size_average=True):
 
 def binary_cross_entropy(input, target, weight=None, size_average=True):
     return _functions.thnn.BCELoss(size_average, weight=weight)(input, target)
+
+def pixel_shuffle(input, upscale_factor):
+    """Rearranges elements in a tensor of shape [*, C*r^2, H, W] to a
+    tensor of shape [C, H*r, W*r]. This is useful for implementing
+    efficient sub-pixel convolution with a stride of 1/r.
+    "Real-Time Single Image and Video Super-Resolution Using an Efficient
+    Sub-Pixel Convolutional Neural Network" - Shi et. al (2016) for more details
+    Args:
+        input (Tensor): Input
+        upscale_factor (int): factor to increase spatial resolution by
+    Input Shape: [*, channels*upscale_factor^2, height, width]
+    Output Shape:[*, channels, height*upscale_factor, width*upscale_factor]
+    Examples:
+        >>> ps = nn.PixelShuffle(3)
+        >>> input = autograd.Variable(torch.Tensor(1, 9, 4, 4))
+        >>> output = ps(input)
+        >>> print(output.size())
+        torch.Size([1, 1, 12, 12])
+    """
+    batch_size, channels, in_height, in_width = input.size()
+    channels //= upscale_factor ** 2
+
+    out_height = in_height * upscale_factor
+    out_width = in_width * upscale_factor
+
+    input_view = input.contiguous().view(
+        batch_size, channels, upscale_factor, upscale_factor,
+        in_height, in_width)
+
+    shuffle_out = input_view.permute(0, 1, 4, 2, 5, 3).contiguous()
+    return shuffle_out.view(batch_size, channels, out_height, out_width)
