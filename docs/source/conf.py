d2ef49384ec83d4c336f63d985688a9dfc198a83
Adam Paszke <adam.paszke@gmail.com>
2016-12-31 16:32:00 +0100

    Add custom docs stylesheet (#387)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,12 +107,23 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     'collapse_navigation': False,
     'display_version': False,
+    'logo_only': True,
 }
+
+html_logo = '_static/img/pytorch-logo-dark.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# html_style_path = 'css/pytorch_theme.css'
+html_context = {
+    'css_files': [
+        'https://fonts.googleapis.com/css?family=Lato',
+        '_static/css/pytorch_theme.css'
+    ],
+}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
