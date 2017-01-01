a0c614ece3418a90875ee3d07b9764829a167b32
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-01-01 23:10:04 +0100

    unsqueeze instead of view in dataloader

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -61,7 +61,7 @@ def _pin_memory_loop(in_queue, out_queue, done_event):
 def default_collate(batch):
     "Puts each data field into a tensor with outer dimension batch size"
     if torch.is_tensor(batch[0]):
-        return torch.cat([t.view(1, *t.size()) for t in batch], 0)
+        return torch.cat([t.unsqueeze(0) for t in batch], 0)
     elif isinstance(batch[0], int):
         return torch.LongTensor(batch)
     elif isinstance(batch[0], float):
