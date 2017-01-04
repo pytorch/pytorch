b2ab6891c5486f75cf6b40a29d833cac0f47b3cc
Soumith Chintala <soumith@fb.com>
2017-01-04 12:51:55 -0500

    fix the rest of Pool module docs for rst

--- torch/nn/modules/pooling.py
+++ torch/nn/modules/pooling.py
@@ -155,18 +155,26 @@ class MaxPool2d(Module):
 
 
 class MaxUnpool2d(Module):
-    """Computes the inverse operation of MaxPool2d
-    MaxPool2d is not invertible, as the locations of the max locations are lost.
-    MaxUnpool2d takes in as input the output of MaxPool2d and the indices of the Max locations
+    """Computes the inverse operation of `MaxPool2d`
+    `MaxPool2d` is not invertible, as the locations of the max locations are lost.
+    :func:`MaxUnpool2d` takes in as input the output of `MaxPool2d` and the indices of the max locations
     and computes the inverse.
 
     Args:
-        kernel_size: the size of the max window. Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
+        kernel_size: the size of the max window. 
+                     Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
         stride: the stride of the window. Can be a single number s or a tuple (sh x sw). Default: kernel_size
         padding: implicit padding that was added to the input. Can be a single number or a tuple. Default: 0
-    Input Shape: [ * , * , *, * ] : Input is minibatch x channels x iH x iW
-    Output Shape:[ * , * , *, * ]  : Output shape is minibatch x channels x padH x (iH - 1) * sH + kH x padW x (iW - 1) * sW + kW, or as specified to the call.
-    Examples:
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})` where 
+          :math:`H_{out} = padding[0] * (H_{in} - 1) * stride[0] + kernel_size[0]`
+          :math:`W_{out} = padding[1] * (W_{in} - 1) * stride[1] + kernel_size[1]`
+          or as given by :attr:`output_size` in the call operator
+
+    Examples::
+
         >>> # pool of square window of size=3, stride=2
         >>> m = nn.MaxPool2d(2, stride=2, return_indices = True)
         >>> mu = nn.MaxUnpool2d(2, stride=2)
@@ -179,6 +187,7 @@ class MaxUnpool2d(Module):
         >>> upsample = nn.MaxUnpool2d(3, 3)
         >>> h, indices = downsample(input)
         >>> output = upsample(h, indices, output_size=input.size())
+
     """
     def __init__(self, kernel_size, stride=None, padding=0):
         super(MaxUnpool2d, self).__init__()
@@ -211,29 +220,50 @@ class MaxUnpool2d(Module):
                 out_height)(input, indices)
 
 class AvgPool2d(Module):
-    """Applies a 2D average pooling over an input signal composed of several input
+    r"""Applies a 2D average pooling over an input signal composed of several input
     planes.
 
-    ```
-    The output value of the layer with input (b x C x H x W) and output (b x C x oH x oW)
+    In the simplest case, the output value of the layer with input size :math:`(N, C, H, W)`, 
+    output :math:`(N, C, H_{out}, W_{out})` and :attr:`kernel_size` :math:`(kH, kW)` 
     can be precisely described as:
-    output[b_i][c_i][h_i][w_i] = (1 / K) * sum_{kh=1, KH} sum_{kw=1, kW}  input[b_i][c_i][stride_h * h_i + kh)][stride_w * w_i + kw)]
-    ```
+
+    .. math::
+    
+        \begin{array}{ll}
+        out(N_i, C_j, h, w)  = 1 / (kH * kW) * \sum_{{m}=0}^{kH-1} \sum_{{n}=0}^{kW-1} 
+                               input(N_i, C_j, stride[0] * h + m, stride[1] * w + n)
+        \end{array}
+
+    | If :attr:`padding` is non-zero, then the input is implicitly zero-padded on both sides 
+      for :attr:`padding` number of points
+
+    The parameters :attr:`kernel_size`, :attr:`stride`, :attr:`padding` can either be:
+
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of two ints -- in which case, the first `int` is used for the height dimension, 
+          and the second `int` for the width dimension
 
     Args:
-        kernel_size: the size of the window. Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
-        stride: the stride of the window. Can be a single number s or a tuple (sh x sw). Default: kernel_size
-        padding: implicit padding to be added. Can be a single number or a tuple. Default: 0
-        ceil_mode: when True, will use "ceil" instead of "floor" to compute the output shape
-    Input Shape: [ * , * , *, * ] : Input is minibatch x channels x iH x iW
-    Output Shape:[ * , * , *, * ]  : Output shape = minibatch x channels x floor((iH  + 2*padH - kH) / sH + 1) x floor((iW  + 2*padW - kW) / sW + 1)
-    Examples:
+        kernel_size: the size of the window
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+        padding: implicit zero padding to be added on both sides
+        ceil_mode: when True, will use `ceil` instead of `floor` to compute the output shape
+        count_include_pad: when True, will include the zero-padding in the averaging calculation
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})` where 
+          :math:`H_{out} = floor((H_{in}  + 2 * padding[0] - kernel\_size[0]) / stride[0] + 1)`
+          :math:`W_{out} = floor((W_{in}  + 2 * padding[1] - kernel\_size[1]) / stride[1] + 1)`
+    Examples::
+
         >>> # pool of square window of size=3, stride=2
         >>> m = nn.AvgPool2d(3, stride=2)
         >>> # pool of non-square window
         >>> m = nn.AvgPool2d((3, 2), stride=(2, 1))
         >>> input = autograd.Variable(torch.randn(20, 16, 50, 32))
         >>> output = m(input)
+
     """
     def __init__(self, kernel_size, stride=None, padding=0, ceil_mode=False,
             count_include_pad=True):
@@ -319,15 +349,39 @@ class MaxPool3d(Module):
                 self.return_indices)
 
 class AvgPool3d(Module):
-    """Applies a 3D average pooling over an input signal composed of several input
+    r"""Applies a 3D average pooling over an input signal composed of several input
     planes.
 
+    In the simplest case, the output value of the layer with input size :math:`(N, C, D, H, W)`, 
+    output :math:`(N, C, D_{out}, H_{out}, W_{out})` and :attr:`kernel_size` :math:`(kD, kH, kW)` 
+    can be precisely described as:
+
+    .. math::
+    
+        \begin{array}{ll}
+        out(N_i, C_j, d, h, w)  = 1 / (kD * kH * kW) * \sum_{{k}=0}^{kD-1} \sum_{{m}=0}^{kH-1} \sum_{{n}=0}^{kW-1} 
+                               input(N_i, C_j, stride[0] * d + k, stride[1] * h + m, stride[2] * w + n)
+        \end{array}
+
+    The parameters :attr:`kernel_size`, :attr:`stride` can either be:
+
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of three ints -- in which case, the first `int` is used for the depth dimension, 
+          the second `int` for the width dimension and the third `int` for the width dimension
+
     Args:
-        kernel_size: the size of the window to take a average over. Can be a single number k (for a square kernel of k x k x k) or a tuple (kt x kh x kw)
-        stride: the stride of the window. Can be a single number s or a tuple (st x sh x sw). Default: kernel_size
-    Input Shape: [ * , * , *, *, * ] : Input is minibatch x channels x iT x iH x iW
-    Output Shape:[ * , * , *, *, * ]  : Output shape = minibatch x channels x floor((iT  + 2*padT - kT) / sT + 1) x floor((iH  + 2*padH - kH) / sH + 1) x floor((iW  + 2*padW - kW) / sW + 1)
-    Examples:
+        kernel_size: the size of the window
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+
+    Shape:
+        - Input: :math:`(N, C, D_{in}, H_{in}, W_{in})`
+        - Output: :math:`(N, C, D_{out}, H_{out}, W_{out})` where 
+          :math:`D_{out} = floor((D_{in}  - kernel\_size[0]) / stride[0] + 1)`
+          :math:`H_{out} = floor((H_{in}  - kernel\_size[1]) / stride[1] + 1)`
+          :math:`W_{out} = floor((W_{in}  - kernel\_size[2]) / stride[2] + 1)`
+
+    Examples::
+
         >>> # pool of square window of size=3, stride=2
         >>> m = nn.AvgPool3d(3, stride=2)
         >>> # pool of non-square window
@@ -345,21 +399,24 @@ class AvgPool3d(Module):
 
 
 class FractionalMaxPool2d(Module):
-    """Applies a 2D fractional max pooling over an input signal composed of several input
-    planes.
+    """Applies a 2D fractional max pooling over an input signal composed of several input planes.
 
-    Fractiona MaxPooling is described in detail in the paper ["Fractional Max-Pooling" by Ben Graham](http://arxiv.org/abs/1412.6071)
+    Fractiona MaxPooling is described in detail in the paper `Fractional MaxPooling`_ by Ben Graham
+
     The max-pooling operation is applied in kHxkW regions by a stochastic
     step size determined by the target output size.
     The number of output features is equal to the number of input planes.
 
     Args:
-        kernel_size: the size of the window to take a max over. Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
-        output_size: the target output size of the image of the form oH x oW. Can be a tuple (oH, oW) or a single number oH for a square image oH x oH
-        output_ratio: If one wants to have an output size as a ratio of the input size, this option can be given. This has to be a number or tuple in the range (0, 1)
-        return_indices: if True, will return the indices along with the outputs. Useful to pass to nn.MaxUnpool2d . Default: False
-    Input Shape: [ * , * , *, * ] : Input is minibatch x channels x iH x iW
-    Output Shape:[ * , * , *, * ]  : Output shape = minibatch x channels x floor((iH  + 2*padH - kH) / sH + 1) x floor((iW  + 2*padW - kW) / sW + 1)
+        kernel_size: the size of the window to take a max over. 
+                     Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
+        output_size: the target output size of the image of the form oH x oW. 
+                     Can be a tuple (oH, oW) or a single number oH for a square image oH x oH
+        output_ratio: If one wants to have an output size as a ratio of the input size, this option can be given. 
+                      This has to be a number or tuple in the range (0, 1)
+        return_indices: if True, will return the indices along with the outputs. 
+                        Useful to pass to nn.MaxUnpool2d . Default: False
+
     Examples:
         >>> # pool of square window of size=3, and target output size 13x12
         >>> m = nn.FractionalMaxPool2d(3, output_size=(13, 12))
@@ -367,6 +424,9 @@ class FractionalMaxPool2d(Module):
         >>> m = nn.FractionalMaxPool2d(3, output_ratio=(0.5, 0.5))
         >>> input = autograd.Variable(torch.randn(20, 16, 50, 32))
         >>> output = m(input)
+
+    .. _Fractional MaxPooling:
+        http://arxiv.org/abs/1412.6071
     """
     def __init__(self, kernel_size, output_size=None, output_ratio=None,
             return_indices=False, _random_samples=None):
@@ -401,24 +461,34 @@ class FractionalMaxPool2d(Module):
 
 
 class MaxUnpool3d(Module):
-    """Computes the inverse operation of MaxPool3d
-    MaxPool3d is not invertible, as the locations of the max locations are lost.
-    MaxUnpool3d takes in as input the output of MaxPool3d and the indices of the Max locations
+    """Computes the inverse operation of `MaxPool3d`
+    `MaxPool3d` is not invertible, as the locations of the max locations are lost.
+    :func:`MaxUnpool3d` takes in as input the output of `MaxPool3d` and the indices of the max locations
     and computes the inverse.
 
     Args:
-        kernel_size: the size of the max window. Can be a single number k (for a square kernel of k x k) or a tuple (kt x kh x kw)
-        stride: the stride of the window. Can be a single number s or a tuple (st x sh x sw). Default: kernel_size
+        kernel_size: the size of the max window. 
+                     Can be a single number k (for a square kernel of k x k x k) or a tuple (kd x kh x kw)
+        stride: the stride of the window. Can be a single number s or a tuple (sd x sh x sw). Default: kernel_size
         padding: implicit padding that was added to the input. Can be a single number or a tuple. Default: 0
-    Input Shape: [ * , * , *, *, * ] : Input is minibatch x channels x iT x iH x iW
-    Output Shape:[ * , * , *, *, * ]  : Output shape = minibatch x channels x padT x (iT - 1) * sT + kT x padH x (iH - 1) * sH + kH x padW x (iW - 1) * sW + kW
-    Examples:
+
+    Shape:
+        - Input: :math:`(N, C, D_{in}, H_{in}, W_{in})`
+        - Output: :math:`(N, C, D_{out}, H_{out}, W_{out})` where 
+          :math:`D_{out} = padding[0] * (D_{in} - 1) * stride[0] + kernel_size[0]`
+          :math:`H_{out} = padding[1] * (H_{in} - 1) * stride[1] + kernel_size[1]`
+          :math:`W_{out} = padding[2] * (W_{in} - 1) * stride[2] + kernel_size[2]`
+          or as given by :attr:`output_size` in the call operator
+
+    Examples::
+
         >>> # pool of square window of size=3, stride=2
         >>> m = nn.MaxPool3d(3, stride=2, return_indices = True)
         >>> mu = nn.MaxUnpool3d(3, stride=2)
         >>> input, indices = autograd.Variable(torch.randn(20, 16, 50, 32, 15))
         >>> output = m(input)
         >>> unpooled_output = m2.forward(output, indices)
+
     """
     def __init__(self, kernel_size, stride=None, padding=0):
         super(MaxUnpool3d, self).__init__()
@@ -436,25 +506,41 @@ class MaxUnpool3d(Module):
 
 
 class LPPool2d(Module):
-    """Applies a 2D power-average pooling over an input signal composed of several input
+    r"""Applies a 2D power-average pooling over an input signal composed of several input
     planes.
-    On each window, the function computed is: f(X) = pow(sum(pow(X, p)), 1/p)
-    At p = infinity, one gets Max Pooling
-    At p = 1, one gets Average Pooling
+
+    On each window, the function computed is: :math:`f(X) = pow(sum(pow(X, p)), 1/p)`
+    
+        - At p = infinity, one gets Max Pooling
+        - At p = 1, one gets Average Pooling
+
+    The parameters :attr:`kernel_size`, :attr:`stride` can either be:
+
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of two ints -- in which case, the first `int` is used for the height dimension, 
+          and the second `int` for the width dimension
+
     Args:
-        kernel_size: the size of the window. Can be a single number k (for a square kernel of k x k) or a tuple (kh x kw)
-        stride: the stride of the window. Can be a single number s or a tuple (sh x sw). Default: kernel_size
-        ceil_mode: when True, will use "ceil" instead of "floor" to compute the output shape
-    Input Shape: [ * , * , *, * ] : Input is minibatch x channels x iH x iW
-    Output Shape:[ * , * , *, * ]  : Output shape = minibatch x channels x floor((iH  + 2*padH - kH) / sH + 1) x floor((iW  + 2*padW - kW) / sW + 1)
-    Examples:
+        kernel_size: the size of the window
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+        ceil_mode: when True, will use `ceil` instead of `floor` to compute the output shape
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})` where 
+          :math:`H_{out} = floor((H_{in}  + 2 * padding[0] - dilation[0] * (kernel\_size[0] - 1) - 1) / stride[0] + 1)`
+          :math:`W_{out} = floor((W_{in}  + 2 * padding[1] - dilation[1] * (kernel\_size[1] - 1) - 1) / stride[1] + 1)`
+    Examples::
+
         >>> # power-2 pool of square window of size=3, stride=2
         >>> m = nn.LPPool2d(2, 3, stride=2)
         >>> # pool of non-square window of power 1.2
         >>> m = nn.LPPool2d(1.2, (3, 2), stride=(2, 1))
         >>> input = autograd.Variable(torch.randn(20, 16, 50, 32))
         >>> output = m(input)
+
     """
+
     def __init__(self, norm_type, kernel_size, stride=None, ceil_mode=False):
         super(LPPool2d, self).__init__()
         self.norm_type = norm_type
