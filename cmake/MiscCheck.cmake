51f1e3fb4312287b8e936c6147b51dbc8d5aa182
Yangqing Jia <jiayq84@gmail.com>
2017-01-04 20:45:03 -0800

    Add misc check for the long type, and temporarily disabled core_overhead_benchmark to remove the benchmark dependency for all binaries

---
+++ cmake/MiscCheck.cmake
@@ -0,0 +1,19 @@
+INCLUDE(CheckCXXSourceCompiles)
+
+CHECK_CXX_SOURCE_COMPILES(
+    "#include <cstdint>
+
+    template <typename T> void Foo();
+    template<> void Foo<int32_t>() {}
+    template<> void Foo<int64_t>() {}
+    int main(int argc, char** argv) {
+      Foo<long>();
+      return 0;
+    }" CAFFE2_LONG_IS_INT32_OR_64)
+
+if (CAFFE2_LONG_IS_INT32_OR_64)
+  message(STATUS "Does not need to define long separately.")
+else()
+  message(STATUS "Need to define long as a separate typeid.")
+  add_definitions(-DCAFFE2_UNIQUE_LONG_TYPEMETA)
+endif()
