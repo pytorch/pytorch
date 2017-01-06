46c6e621cbbc161f4cd268a02787ff92633d6efe
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2017-01-06 00:35:38 -0800

    Fix warning in ScaleOp grad
    Summary: #accept2ship
    
    Reviewed By: Yangqing
    
    Differential Revision: D4386362
    
    fbshipit-source-id: 634410e73034ac31b7f2bec39f41c52ea9935e3a

--- caffe2/operators/scale_op.cc
+++ caffe2/operators/scale_op.cc
@@ -16,11 +16,9 @@ Scale takes one input data (Tensor<float>) and produces one output data
 class GetScaleGradient : public GradientMakerBase {
   using GradientMakerBase::GradientMakerBase;
   vector<OperatorDef> GetGradientDefs() override {
+    // CopyArguments is true by default so the "scale" arg is going to be copied
     return SingleGradientDef(
-        "Scale", "",
-        vector<string>{GO(0)},
-        vector<string>{GI(0)},
-        Def().arg());
+        "Scale", "", vector<string>{GO(0)}, vector<string>{GI(0)});
   }
 };
 REGISTER_GRADIENT(Scale, GetScaleGradient);
