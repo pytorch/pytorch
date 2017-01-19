a0a95c95d44cae7cfd1db23c71a81b2cbb59608a
Ronny <ronrest@users.noreply.github.com>
2017-01-20 03:10:01 +1100

    Add Random Number Generator Docstrings (#506)

--- torch/__init__.py
+++ torch/__init__.py
@@ -90,18 +90,33 @@ def set_default_tensor_type(t):
 
 
 def set_rng_state(new_state):
+    r"""Sets the random number generator state.
+    
+    Args:
+        new_state (torch.ByteTensor): The desired state
+    """
     default_generator.set_state(new_state)
 
 
 def get_rng_state():
+    r"""Returns the random number generator state as a ByteTensor."""
     return default_generator.get_state()
 
 
 def manual_seed(seed):
+    r"""Sets the seed for generating random numbers. And returns a 
+    `torch._C.Generator` object.
+    
+    Args:
+        seed (int or long): The desired seed.
+    """
     return default_generator.manual_seed(seed)
 
 
 def initial_seed():
+    r"""Returns the initial seed for generating random numbers as a 
+    python `long`.
+    """
     return default_generator.initial_seed()
 
 
