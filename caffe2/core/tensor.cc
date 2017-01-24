e7d274ce54c8418d2d5899752fcc4e1a7b0f8483
Alexander Sidorov <salex@fb.com>
2017-01-24 15:48:13 -0800

    TensorPrinter helper c++ class
    Summary:
    it is annoying to print tensors from c++ (while it is easy
    from python when you have a net). So I just took logic out of PrintOp
    into a separate class
    
    Reviewed By: urikz
    
    Differential Revision: D4452793
    
    fbshipit-source-id: d512559fe07bc468423c9ce38da0c44eaad4fdec

--- caffe2/core/tensor.cc
+++ caffe2/core/tensor.cc
@@ -1,4 +1,5 @@
 #include "caffe2/core/tensor.h"
+
 #include "caffe2/core/flags.h"
 
 CAFFE2_DEFINE_bool(
@@ -9,4 +10,43 @@ CAFFE2_DEFINE_bool(
 namespace caffe2 {
 // declaring it here instead of context.cc because tensor.h includes context.h
 CAFFE_KNOWN_TYPE(Tensor<CPUContext>);
+
+TensorPrinter::TensorPrinter(
+    const std::string& tensor_name,
+    const std::string& file_name,
+    int limit)
+    : to_file_(!file_name.empty()),
+      limit_(limit ? limit : k_limit_default_),
+      tensor_name_(tensor_name) {
+  if (to_file_) {
+    // We will output to file instead of printing on screen.
+    // We will write each individual tensor to its individual file.
+    log_file_.reset(new std::ofstream(
+        file_name, std::ofstream::out | std::ofstream::trunc));
+    CAFFE_ENFORCE(
+        log_file_->good(),
+        "Failed to open TensorPrinter file ",
+        file_name,
+        ". rdstate() = ",
+        log_file_->rdstate());
+  }
 }
+
+TensorPrinter::~TensorPrinter() {
+  if (log_file_.get()) {
+    log_file_->close();
+  }
+}
+
+std::string TensorPrinter::MetaStr(const Tensor<CPUContext>& tensor) {
+  std::stringstream meta_stream;
+  meta_stream << "Tensor " << tensor_name_ << " of type "
+              << tensor.meta().name() << ". Dims: (";
+  for (const auto dim : tensor.dims()) {
+    meta_stream << dim << ",";
+  }
+  meta_stream << "): ";
+  return meta_stream.str();
+}
+
+} // namespace caffe2
