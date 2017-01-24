2748b920ab9beafb00abe03318abf58368e8374c
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-01-25 00:35:28 +0300

    make adam have the same lr as lua torch (#576)

--- torch/optim/adam.py
+++ torch/optim/adam.py
@@ -20,7 +20,7 @@ class Adam(Optimizer):
         https://arxiv.org/abs/1412.6980
     """
 
-    def __init__(self, params, lr=1e-2, betas=(0.9, 0.999), eps=1e-8,
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
             weight_decay=0):
         defaults = dict(lr=lr, betas=betas, eps=eps,
                 weight_decay=weight_decay)
