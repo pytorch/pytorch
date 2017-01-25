2cad802b6856a4b35573a6d221ac51faa5f0ed75
Soumith Chintala <soumith@gmail.com>
2017-01-25 13:15:22 -0500

    Revert "cuda implementation of Gated Linear Unit"

--- generic/THCUNN.h
+++ generic/THCUNN.h
@@ -138,19 +138,6 @@ TH_API void THNN_(HardTanh_updateGradInput)(
                   accreal max_val,
                   bool inplace);
 
-TH_API void THNN_CudaGatedLinear_updateOutput(
-                  THCState *state,
-                  THCudaTensor *input,
-                  THCudaTensor *output,
-                  int dim);
-
-TH_API void THNN_CudaGatedLinear_updateGradInput(
-                  THCState *state,
-                  THCudaTensor *input,
-                  THCudaTensor *gradOutput,
-                  THCudaTensor *gradInput,
-                  int dim);
-
 TH_API void THNN_(LeakyReLU_updateOutput)(
                   THCState *state,
                   THCTensor *input,
