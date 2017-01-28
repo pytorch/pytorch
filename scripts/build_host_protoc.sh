d47395cca7fbd21e94e1648f7be492b52466524e
Yangqing Jia <jiayq84@gmail.com>
2017-01-28 13:18:40 -0800

    Use protobuf's own cmake scripts and add travis for ios
    Summary: Closes https://github.com/caffe2/caffe2/pull/110
    
    Differential Revision: D4475170
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 5964db04186619ac563f516cb202c5e2ba543403

---
+++ scripts/build_host_protoc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##############################################################################
+# Build script to build the protoc compiler for the host platform.
+##############################################################################
+# This script builds the protoc compiler for the host platform, which is needed
+# for any cross-compilation as we will need to convert the protobuf source
+# files to cc files.
+#
+# After the execution of the file, one should be able to find the host protoc
+# binary at build_host_protoc/bin/protoc.
+
+CAFFE2_ROOT="$( cd "$(dirname -- "$0")"/.. ; pwd -P)"
+BUILD_ROOT=$CAFFE2_ROOT/build_host_protoc
+mkdir -p $BUILD_ROOT/build
+
+cd $BUILD_ROOT/build
+cmake $CAFFE2_ROOT/third_party/protobuf/cmake \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_ROOT \
+    -Dprotobuf_BUILD_TESTS=OFF \
+    || exit 1
+make -j 4 || exit 1
+make install || exit 1
