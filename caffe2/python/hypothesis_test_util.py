Fix for gradient propagation for initial recurrent state for RecurrentNetwork
Summary: looks like we don't a good job with initial recurrent input gradients yet. Here is some fix, but gradient doesn't check yet. The shape is correct now though

Reviewed By: salexspb

Differential Revision: D4475447

fbshipit-source-id: 280f1f59f19e487fd0dce0d440609c50ddce294a

caffe2/python/hypothesis_test_util.py
@@ -378,8 +378,15 @@ class HypothesisTestCase(test_util.TestCase):
                     ref_vals, ref_indices = ref
                     val_name = grad_names.values
                 vals = workspace.FetchBlob(str(val_name))
-                np.testing.assert_allclose(vals, ref_vals,
-                                           atol=threshold, rtol=threshold)
+                np.testing.assert_allclose(
+                    vals,
+                    ref_vals,
+                    atol=threshold,
+                    rtol=threshold,
+                    err_msg='Gradient {0} is not matching the reference'.format(
+                        val_name,
+                    ),
+                )
                 if ref_indices is not None:
                     indices = workspace.FetchBlob(str(grad_names.indices))
                     np.testing.assert_allclose(indices, ref_indices,