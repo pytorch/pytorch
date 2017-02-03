eb9c6519ea5c7b69dd4994c825b81b5188bc3b77
Aapo Kyrola <akyrola@fb.com>
2017-02-03 07:35:54 -0800

    Improve dagnet chain computation by pruning redundant dependencies
    Summary:
    We have noticed that the number of chains computed is usually much larger than necessary, when there is a backward pass. For example having a network of 5 FCs with gradient operators (but no parameter updates) should yield only one chain, but instead over 20 were created.  After adding parameter updates, the forward pass still should remain one chain, while the backward pass will be splintered.
    
    Analysis showed that the problem was the dependices from forward ops to the gradient computation. But these are redundant since the gradient op is already dependent from the op via the full path over ops. Example:
    
      fc1     -> fc2   --->   fc3  --> loss
        |          |             |          |
      fc1grad <- fc2grad    <- fc3grad <-
    
    Here fc1 and fc1 grad have a direct dependency, but indirect dependency via fc2->fc3->[...]->fc1grad already covers that dependency.
    
    To fix this, I added a pruning step prior to the chain computation. The chain computation is done on the pruned tree, but I do not modify the runtime chains for safety.
    
    Pruning is based on  following logic:
      - if one of my direct parents is ancestor via an another traversar, I can remove the direct dependency
    
    Pruning is extremely fast, linear in the number of dependencies.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D4500293
    
    fbshipit-source-id: 0994ae6775c53378ea1e0074365cef041764a1b4

--- caffe2/core/net_test.cc
+++ caffe2/core/net_test.cc
@@ -23,8 +23,17 @@ class NetTestDummyOp final : public OperatorBase {
 
 REGISTER_CPU_OPERATOR(NetTestDummy, NetTestDummyOp);
 REGISTER_CUDA_OPERATOR(NetTestDummy, NetTestDummyOp);
+REGISTER_CPU_OPERATOR(NetTestDummy2, NetTestDummyOp);
+REGISTER_CUDA_OPERATOR(NetTestDummy2, NetTestDummyOp);
 
-OPERATOR_SCHEMA(NetTestDummy).NumInputs(0, INT_MAX).NumOutputs(0, INT_MAX);
+OPERATOR_SCHEMA(NetTestDummy)
+    .NumInputs(0, INT_MAX)
+    .NumOutputs(0, INT_MAX)
+    .AllowInplace({{0, 0}, {1, 1}});
+OPERATOR_SCHEMA(NetTestDummy2)
+    .NumInputs(0, INT_MAX)
+    .NumOutputs(0, INT_MAX)
+    .AllowInplace({{1, 0}});
 
 const char kExampleNetDefString[] =
 "  name: \"example\""
@@ -116,6 +125,30 @@ void checkChaining(
     CHECK_NOTNULL(dag);
     const auto& chains = dag->TEST_execution_chains();
     EXPECT_TRUE(chains == expected);
+  }
+}
+
+void checkNumChains(const char* spec, const int expected_num_chains) {
+  Workspace ws;
+
+  NetDef net_def;
+  CAFFE_ENFORCE(google::protobuf::TextFormat::ParseFromString(spec, &net_def));
+
+  // Create all external inputs
+  for (auto inp : net_def.external_input()) {
+    ws.CreateBlob(inp);
+  }
+
+  {
+    auto old = FLAGS_caffe2_disable_chaining;
+    auto g = MakeGuard([&]() { FLAGS_caffe2_disable_chaining = old; });
+    FLAGS_caffe2_disable_chaining = false;
+
+    std::unique_ptr<NetBase> net(CreateNet(net_def, &ws));
+    auto* dag = dynamic_cast_if_rtti<DAGNetBase*>(net.get());
+    CHECK_NOTNULL(dag);
+    const auto& chains = dag->TEST_execution_chains();
+    EXPECT_EQ(expected_num_chains, chains.size());
   }
 }
 
@@ -258,6 +291,256 @@ TEST(NetTest, ChainingForForkJoin) {
         }
 )DOC";
   checkChaining(spec, {{0, {0}}, {1, {1}}, {2, {2, 3}}});
+}
+
+TEST(NetTest, ChainingForwardBackward) {
+  const auto spec = R"DOC(
+  name: "gpu_0"
+  type: "dag"
+  op {
+    input: "in"
+    input: "fc_0_w"
+    input: "fc_0_b"
+    output: "fc_0"
+    name: "0"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_0"
+    output: "fc_0"
+    name: "1"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_0"
+    input: "fc_1_w"
+    input: "fc_1_b"
+    output: "fc_1"
+    name: "2"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_1"
+    output: "fc_1"
+    name: "3"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_1"
+    input: "fc_2_w"
+    input: "fc_2_b"
+    output: "fc_2"
+    name: "4"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_2"
+    output: "fc_2"
+    name: "5"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_2"
+    input: "fc_3_w"
+    input: "fc_3_b"
+    output: "fc_3"
+    name: "6"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_3"
+    output: "fc_3"
+    name: "7"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_3"
+    input: "fc_4_w"
+    input: "fc_4_b"
+    output: "fc_4"
+    name: "8"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_4"
+    output: "fc_4"
+    name: "9"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_4"
+    input: "in2"
+    output: "LabelCrossEntropy"
+    name: "10"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "LabelCrossEntropy"
+    output: "AveragedLoss"
+    name: "11"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "AveragedLoss"
+    output: "AveragedLoss_autogen_grad"
+    name: "12"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "LabelCrossEntropy"
+    input: "AveragedLoss_autogen_grad"
+    output: "LabelCrossEntropy_grad"
+    name: "13"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_4"
+    input: "label"
+    input: "LabelCrossEntropy_grad"
+    output: "fc_4_grad"
+    name: "14"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "fc_4"
+    input: "fc_4_grad"
+    output: "fc_4_grad"
+    name: "15"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "fc_3"
+    input: "fc_4_w"
+    input: "fc_4_grad"
+    output: "fc_4_w_grad"
+    output: "fc_4_b_grad"
+    output: "fc_3_grad"
+    name: "16"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_3"
+    input: "fc_3_grad"
+    output: "fc_3_grad"
+    name: "17"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "fc_2"
+    input: "fc_3_w"
+    input: "fc_3_grad"
+    output: "fc_3_w_grad"
+    output: "fc_3_b_grad"
+    output: "fc_2_grad"
+    name: "18"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_2"
+    input: "fc_2_grad"
+    output: "fc_2_grad"
+    name: "19"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "fc_1"
+    input: "fc_2_w"
+    input: "fc_2_grad"
+    output: "fc_2_w_grad"
+    output: "fc_2_b_grad"
+    output: "fc_1_grad"
+    name: "20"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_1"
+    input: "fc_1_grad"
+    output: "fc_1_grad"
+    name: "21"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "fc_0"
+    input: "fc_1_w"
+    input: "fc_1_grad"
+    output: "fc_1_w_grad"
+    output: "fc_1_b_grad"
+    output: "fc_0_grad"
+    name: "22"
+    type: "NetTestDummy"
+  }
+  op {
+    input: "fc_0"
+    input: "fc_0_grad"
+    output: "fc_0_grad"
+    name: "23"
+    type: "NetTestDummy2"
+  }
+  op {
+    input: "in"
+    input: "fc_0_w"
+    input: "fc_0_grad"
+    output: "fc_0_w_grad"
+    output: "fc_0_b_grad"
+    output: "data_grad"
+    name: "24"
+    type: "NetTestDummy"
+  }
+  external_input: "in"
+  external_input: "in2"
+  external_input: "LR"
+  external_input: "fc_0_w"
+  external_input: "fc_0_b"
+  external_input: "fc_1_w"
+  external_input: "fc_1_b"
+  external_input: "fc_2_w"
+  external_input: "fc_2_b"
+  external_input: "fc_3_w"
+  external_input: "fc_3_b"
+  external_input: "fc_4_w"
+  external_input: "fc_4_b"
+  external_input: "label"
+  )DOC";
+  checkNumChains(spec, 1);
+}
+
+TEST(NetTest, ChainingForHogwildModel) {
+  const auto spec = R"DOC(
+        name: "example"
+        type: "dag"
+        external_input: "in"
+        op {
+          input: "in"
+          output: "hidden1"
+          type: "NetTestDummy"
+        }
+        op {
+          input: "hidden1"
+          output: "mid1"
+          type: "NetTestDummy"
+        }
+        op {
+          input: "mid1"
+          output: "out1"
+          type: "NetTestDummy"
+        }
+        op {
+          input: "in"
+          output: "hidden2"
+          type: "NetTestDummy"
+        }
+        op {
+          input: "hidden2"
+          output: "mid2"
+          type: "NetTestDummy"
+        }
+        op {
+          input: "mid2"
+          output: "out2"
+          type: "NetTestDummy"
+        }
+)DOC";
+  checkNumChains(spec, 2);
 }
 
 } // namespace caffe2
