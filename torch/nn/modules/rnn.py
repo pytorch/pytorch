6c77fa91211f398d9cd5d9fe1a4407878b915ef5
Bryan Marcus McCann <bryan.mccann.is@gmail.com>
2017-02-09 09:06:26 -0800

    Changes in RNNBase and Embedding for compatibility with DataParallel (#660)

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -22,7 +22,7 @@ class RNNBase(Module):
         self.bidirectional = bidirectional
         num_directions = 2 if bidirectional else 1
 
-        self.all_weights = []
+        self._all_weights = []
         for layer in range(num_layers):
             for direction in range(num_directions):
                 layer_input_size = input_size if layer == 0 else hidden_size * num_directions
@@ -39,14 +39,16 @@ class RNNBase(Module):
                 b_hh = Parameter(torch.Tensor(gate_size))
 
                 suffix = '_reverse' if direction == 1 else ''
-                setattr(self, 'weight_ih_l{}{}'.format(layer, suffix), w_ih)
-                setattr(self, 'weight_hh_l{}{}'.format(layer, suffix), w_hh)
+                weights = ['weight_ih_l{}{}', 'weight_hh_l{}{}', 'bias_ih_l{}{}', 'bias_hh_l{}{}']
+                weights = [x.format(layer, suffix) for x in weights]
+                setattr(self, weights[0], w_ih)
+                setattr(self, weights[1], w_hh)
                 if bias:
-                    setattr(self, 'bias_ih_l{}{}'.format(layer, suffix), b_ih)
-                    setattr(self, 'bias_hh_l{}{}'.format(layer, suffix), b_hh)
-                    self.all_weights += [(w_ih, w_hh, b_ih, b_hh)]
+                    setattr(self, weights[2], b_ih)
+                    setattr(self, weights[3], b_hh)
+                    self._all_weights += [weights]
                 else:
-                    self.all_weights += [(w_ih, w_hh)]
+                    self._all_weights += [weights[:2]]
 
         self.reset_parameters()
 
@@ -92,6 +94,29 @@ class RNNBase(Module):
             s += ', bidirectional={bidirectional}'
         s += ')'
         return s.format(name=self.__class__.__name__, **self.__dict__)
+
+    def __setstate__(self, d):
+        self.__dict__.update(d)
+        if 'all_weights' in d:
+            self._all_weights = d['all_weights']
+        if isinstance(self._all_weights[0][0], str):
+            return
+        num_layers = self.num_layers
+        num_directions = 2 if self.bidirectional else 1
+        self._all_weights = []
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                suffix = '_reverse' if direction == 1 else ''
+                weights = ['weight_ih_l{}{}', 'weight_hh_l{}{}', 'bias_ih_l{}{}', 'bias_hh_l{}{}']
+                weights = [x.format(layer, suffix) for x in weights]
+                if self.bias:
+                    self._all_weights += [weights]
+                else:
+                    self._all_weights += [weights[:2]]
+
+    @property
+    def all_weights(self):
+        return [[getattr(self, weight) for weight in weights] for weights in self._all_weights]
 
 
 class RNN(RNNBase):
