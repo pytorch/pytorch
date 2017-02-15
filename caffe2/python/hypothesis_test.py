cb3c41b9a95c456270bd0eeeedae35db27d15dfc
Huazhong Ning <ninghz@fb.com>
2017-02-15 10:41:48 -0800

    PiecewiseLinearTransformOp transform binary predictions specially
    Summary:
    The existing op tranforms the input in a general way. It needs M transform mappings to transform a NxM input tensor.
    But for binary predictions X (Nx2 tensor), we know that X[:, 0] = 1 - X[:, 1].
    So we just need one mapping for X[:, 1]. After being transformed, we can compute X[:, 0].
    This diff is to handle this.
    
    Differential Revision: D4550441
    
    fbshipit-source-id: 42d8c6e88d830c97628ee930b543740a32acf904

--- caffe2/python/hypothesis_test.py
+++ caffe2/python/hypothesis_test.py
@@ -2076,38 +2076,6 @@ class TestOperators(hu.HypothesisTestCase):
         self.assertDeviceChecks(dc, op, [X], [0])
         self.assertGradientChecks(gc, op, [X], 0, [0])
 
-    @given(n=st.integers(10000, 10003), **hu.gcs_cpu_only)
-    def test_piecewise_linear_transform(self, n, gc, dc):
-        W = np.random.uniform(-1, 1, (2, n)).astype(np.float32)
-        b = np.random.uniform(-1, 1, (2, n)).astype(np.float32)
-        # make sure bucket range are increating!
-        bucket_range = np.random.uniform(0.1, 0.9,
-                                         (2, n + 1)).astype(np.float32)
-        bucket_base = np.array(list(range(n + 1)))
-        bucket_range[0, :] = bucket_range[0, :] + bucket_base
-        bucket_range[1, :] = bucket_range[1, :] + bucket_base
-        # make x[i] inside bucket i, for the ease of testing
-        X = np.random.uniform(0, 0.9, (n, 2)).astype(np.float32)
-        for i in range(len(X)):
-            X[i][0] = X[i][0] * bucket_range[0][i] + \
-                (1 - X[i][0]) * bucket_range[0][i + 1]
-            X[i][1] = X[i][1] * bucket_range[1][i] + \
-                (1 - X[i][1]) * bucket_range[1][i + 1]
-
-        op = core.CreateOperator(
-            "PiecewiseLinearTransform", ["X"], ["Y"],
-            bounds=bucket_range.flatten().tolist(),
-            slopes=W.flatten().tolist(),
-            intercepts=b.flatten().tolist(),
-            pieces=n
-        )
-
-        def piecewise(x, *args, **kw):
-            return [W.transpose() * x + b.transpose()]
-
-        self.assertReferenceChecks(gc, op, [X], piecewise)
-        self.assertDeviceChecks(dc, op, [X], [0])
-
     @given(X=hu.tensor(min_dim=1,
                        max_dim=4,
                        elements=st.floats(min_value=-100, max_value=100)),
