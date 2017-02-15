a8d70f3552110b907c2a8e221ac5d34708938b94
Andrey Malevich <amalevich@fb.com>
2017-02-15 01:22:09 -0800

    Try to improve serialization speed for SparseNN.
    Summary:
    Created some simple benchmark to test model saving speed, plus few possible
    optimization on top of it.
    
    Since we don't really want to have partial LogFileDB ever, it makes sense to
    commit the transactions only after we've finished serialization.
    
    As a result in my test serialization time in my dummy test drops from
    480 seconds, to:
    Serialization time: 52.5134651661
    Deserialization time: 60.5741639137
    
    One more really scary things that I've found:
    it looks like load_op with load_all might actually load corrupted DBs (if they'll be truncated), so we do need to fix it really badly (save all blobs we have in the DB or even better checksum).
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D4558216
    
    fbshipit-source-id: 4145c07f29b9dda527a2e57842f3abd8023d71a3

--- caffe2/operators/load_save_op.h
+++ caffe2/operators/load_save_op.h
@@ -254,6 +254,7 @@ class SaveOp final : public Operator<Context> {
     for (int i = 0; i < inputs.size(); ++i) {
       inputs[i]->Serialize(def().input(i), acceptor);
     }
+    out_db->Close();
     return true;
   }
 
