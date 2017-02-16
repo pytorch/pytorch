8757be173111c3e8ca482fea23a52403ab6d928b
Andy Brock <andrewjbrock92@gmail.com>
2017-02-16 22:28:49 +0000

    Add Nesterov Momentum
    Adds the Nesterov variant of momentum as an option for SGD.

--- torch/optim/sgd.py
+++ torch/optim/sgd.py
@@ -3,16 +3,16 @@ from .optimizer import Optimizer, required
 
 class SGD(Optimizer):
     """Implements stochastic gradient descent (optionally with momentum).
-
     Args:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         lr (float): learning rate
         momentum (float, optional): momentum factor (default: 0)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov(bool, optional): enables Nesterov momentum (default: False)
 
     Example:
-
         >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
         >>> optimizer.zero_grad()
         >>> loss_fn(model(input), target).backward()
@@ -20,14 +20,20 @@ class SGD(Optimizer):
     """
 
     def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0):
+                 weight_decay=0, nesterov=False):
         defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        weight_decay=weight_decay)
+                        weight_decay=weight_decay, nesterov=nesterov)
+        if nesterov and (momentum <= 0 and dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
         super(SGD, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(SGD, self).__setstate__(state)
+        for group in self.parameter_groups:
+            group.set_default('nesterov', False)
 
     def step(self, closure=None):
         """Performs a single optimization step.
-
         Arguments:
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
@@ -40,6 +46,7 @@ class SGD(Optimizer):
             weight_decay = group['weight_decay']
             momentum = group['momentum']
             dampening = group['dampening']
+            nesterov = group['nesterov']
 
             for p in group['params']:
                 d_p = p.grad.data
@@ -51,7 +58,11 @@ class SGD(Optimizer):
                         param_state['momentum_buffer'] = d_p.clone()
                     else:
                         buf = param_state['momentum_buffer']
-                        d_p = buf.mul_(momentum).add_(1 - dampening, d_p)
+                        buf.mul_(momentum).add_(1 - dampening, d_p)
+                        if nesterov:
+                            d_p.add_(momentum, buf)
+                        else:
+                            d_p = buf
 
                 p.data.add_(-group['lr'], d_p)
 
