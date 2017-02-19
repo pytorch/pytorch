a96e92d77a9a948aa52def8d9d12ea344bf705a3
Andy Brock <andrewjbrock92@gmail.com>
2017-02-19 00:40:45 +0000

    Update sgd.py

--- torch/optim/sgd.py
+++ torch/optim/sgd.py
@@ -23,7 +23,7 @@ class SGD(Optimizer):
                  weight_decay=0, nesterov=False):
         defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
                         weight_decay=weight_decay, nesterov=nesterov)
-        if nesterov and (momentum <= 0 and damp != 0):
+        if nesterov and (momentum <= 0 and dampening != 0):
             raise ValueError("Nesterov momentum requires a momentum and zero dampening")
         super(SGD, self).__init__(params, defaults)
 
