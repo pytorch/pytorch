a652700fa758f714c20ec6063e296195fa913dec
edgarriba <edgar.riba@gmail.com>
2017-02-23 15:42:42 +0100

    add Pairwise distance

--- torch/nn/modules/distance.py
+++ torch/nn/modules/distance.py
@@ -1,6 +1,25 @@
+import torch
+from .module import Module
+
+
+class PairwiseDistance(Module):
+    r"""Computes the distance between two vectors using the p-norm.
+    """
+
+    def __init__(self, p=2):
+        super(PairwiseDistance, self).__init__()
+        assert p % 1 == 0
+        self.norm = p
+
+    def forward(self, x1, x2):
+        assert x1.size() == x2.size()
+        eps = 1e-4 / x1.size(1)
+        diff = torch.abs(x1 - x2)
+        out = torch.pow(diff, self.norm).sum(dim=1)
+        return torch.pow(out + eps, 1. / self.norm)
+
 
 # TODO: Cosine
 # TODO: CosineDistance - make sure lua's CosineDistance isn't actually cosine similarity
 # TODO: Euclidean
 # TODO: WeightedEuclidean
-# TODO: PairwiseDistance
