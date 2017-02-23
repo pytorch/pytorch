8a85d6bd34849ab0af4fc347808b971517d18a2a
Chonglin Sun <clsun@fb.com>
2017-02-23 11:01:18 -0800

    support vectors with different dims in for DotProductOp.
    Summary:
    Add two argument to DotProductOp operator, `force_same_dim` (1 if we want
    DotProductOp to only accept two tensors with equal dimension, 0 otherwise) and
    pad_value (only useful when force_same_dim = 0, pad the tensor with smaller
    size to the same as the other one).
    
    Differential Revision: D4502619
    
    fbshipit-source-id: 46f7da710c6f6365f76a7af6234c34c7f656be62

--- caffe2/operators/distance_op.h
+++ caffe2/operators/distance_op.h
@@ -137,6 +137,24 @@ class DotProductGradientOp final : public Operator<Context> {
 };
 
 template <typename T, class Context>
+class DotProductWithPaddingOp : public Operator<Context> {
+ public:
+  DotProductWithPaddingOp(const OperatorDef& def, Workspace* ws)
+      : Operator<Context>(def, ws),
+        pad_value_(OperatorBase::GetSingleArgument<float>("pad_value", 0.0)),
+        replicate_(OperatorBase::GetSingleArgument<bool>("replicate", false)) {}
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+
+  bool RunOnDevice() override;
+
+ protected:
+  float pad_value_;
+  bool replicate_;
+  INPUT_TAGS(X_IN, Y_IN);
+  OUTPUT_TAGS(DOT_OUT);
+};
+
+template <typename T, class Context>
 class CosineSimilarityOp : public Operator<Context> {
  public:
   CosineSimilarityOp(const OperatorDef& def, Workspace* ws)
@@ -223,6 +241,109 @@ class CosineSimilarityGradientOp final : public Operator<Context> {
 
  protected:
   INPUT_TAGS(X_IN, Y_IN, DER_COS_IN);
+  OUTPUT_TAGS(DER_X_OUT, DER_Y_OUT);
+};
+
+template <typename T, class Context>
+class DotProductWithPaddingGradientOp final : public Operator<Context> {
+ public:
+  DotProductWithPaddingGradientOp(const OperatorDef& def, Workspace* ws)
+      : Operator<Context>(def, ws),
+        pad_value_(OperatorBase::GetSingleArgument<float>("pad_value", 0.0)),
+        replicate_(OperatorBase::GetSingleArgument<bool>("replicate", false)) {}
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+
+  bool RunOnDevice() override {
+    auto& X = Input(X_IN);
+    auto& Y = Input(Y_IN);
+    auto& dDot = Input(DER_DOT_IN);
+    auto* dX = Output(DER_X_OUT);
+    auto* dY = Output(DER_Y_OUT);
+    int N, D, DX, DY, restD;
+    if (X.size() > 0) {
+      N = X.ndim() > 0 ? X.dim32(0) : 1;
+      DX = X.size() / N;
+      DY = Y.size() / N;
+    } else {
+      N = 0;
+      DX = 0;
+      DY = 0;
+    }
+    CAFFE_ENFORCE(!replicate_ || DX % DY == 0 || DY % DX == 0);
+    D = std::min(DX, DY);
+    restD = std::max(DX, DY) - D;
+    CAFFE_ENFORCE_EQ(X.ndim(), Y.ndim());
+    CAFFE_ENFORCE_EQ(X.dim32(0), Y.dim32(0));
+    CAFFE_ENFORCE_EQ(dDot.ndim(), 1);
+    CAFFE_ENFORCE_EQ(dDot.dim32(0), N);
+    dX->ResizeLike(X);
+    dY->ResizeLike(Y);
+
+    const auto* X_data = X.template data<T>();
+    const auto* Y_data = Y.template data<T>();
+    const auto* dDot_data = dDot.template data<T>();
+    auto* dX_data = dX->template mutable_data<T>();
+    auto* dY_data = dY->template mutable_data<T>();
+    for (int i = 0; i < N; ++i) { // TODO: multithreading
+      auto offsetX = i * DX;
+      auto offsetY = i * DY;
+      if (replicate_) {
+        // L_ for longer vector and S_ for shorter vector
+        const T *L_data, *S_data;
+        T *dL_data, *dS_data;
+        int DL, DS;
+        if (DX > DY) {
+          L_data = X_data + offsetX;
+          S_data = Y_data + offsetY;
+          dL_data = dX_data + offsetX;
+          dS_data = dY_data + offsetY;
+          DL = DX;
+          DS = DY;
+        } else {
+          L_data = Y_data + offsetY;
+          S_data = X_data + offsetX;
+          dL_data = dY_data + offsetY;
+          dS_data = dX_data + offsetX;
+          DL = DY;
+          DS = DX;
+        }
+
+        // TODO: get rid of temp memory use
+        std::vector<T> tmp_data(DS);
+        math::Set<T, Context>(DS, 0.0, dS_data, &context_);
+        for (int j = 0; j < DL / DS; j++) {
+          math::Scale<T, Context>(
+              DS, dDot_data[i], S_data, dL_data + j * DS, &context_);
+          math::Scale<T, Context>(
+              DS, dDot_data[i], L_data + j * DS, tmp_data.data(), &context_);
+          math::Axpy<T, Context>(DS, 1.0, tmp_data.data(), dS_data, &context_);
+        }
+      } else {
+        math::Scale<T, Context>(
+            D, dDot_data[i], X_data + offsetX, dY_data + offsetY, &context_);
+        math::Scale<T, Context>(
+            D, dDot_data[i], Y_data + offsetY, dX_data + offsetX, &context_);
+      }
+
+      if (!replicate_ && DX != DY) {
+        T* rest_data;
+        if (DX > DY) {
+          rest_data = dX_data + offsetX + D;
+        } else {
+          rest_data = dY_data + offsetY + D;
+        }
+        auto pad_gradient = dDot_data[i] * pad_value_;
+        math::Set<T, Context>(restD, pad_gradient, rest_data, &context_);
+      }
+    }
+
+    return true;
+  }
+
+ protected:
+  float pad_value_;
+  bool replicate_;
+  INPUT_TAGS(X_IN, Y_IN, DER_DOT_IN);
   OUTPUT_TAGS(DER_X_OUT, DER_Y_OUT);
 };
 
