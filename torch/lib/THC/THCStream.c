d1705a327a1dc25336bb253c6ac3071a16b217a8
Sam Gross <sgross@fb.com>
2017-02-27 13:00:00 -0800

    Fix bug where pinned memory event could be recorded on incorrect device

--- torch/lib/THC/THCStream.c
+++ torch/lib/THC/THCStream.c
@@ -3,6 +3,9 @@
 #include <cuda_runtime_api.h>
 #include "THAtomic.h"
 
+#define MAX_DEVICES 256
+static THCStream default_streams[MAX_DEVICES];
+
 
 THCStream* THCStream_new(int flags)
 {
@@ -13,18 +16,38 @@ THCStream* THCStream_new(int flags)
   return self;
 }
 
+THC_API THCStream* THCStream_defaultStream(int device)
+{
+  // default streams aren't refcounted
+  THAssert(device >= 0 && device < MAX_DEVICES);
+  THCStream* stream = &default_streams[device];
+  stream->device = device;
+  return stream;
+}
+
+THCStream* THCStream_newWithPriority(int flags, int priority)
+{
+  THCStream* self = (THCStream*) malloc(sizeof(THCStream));
+  self->refcount = 1;
+  THCudaCheck(cudaGetDevice(&self->device));
+  THCudaCheck(cudaStreamCreateWithPriority(&self->stream, flags, priority));
+  return self;
+}
+
 void THCStream_free(THCStream* self)
 {
-  if (!self) {
+  if (!self || !self->stream) {
     return;
   }
   if (THAtomicDecrementRef(&self->refcount)) {
-    THCudaCheck(cudaStreamDestroy(self->stream));
+    THCudaCheckWarn(cudaStreamDestroy(self->stream));
     free(self);
   }
 }
 
 void THCStream_retain(THCStream* self)
 {
-  THAtomicIncrementRef(&self->refcount);
+  if (self->stream) {
+    THAtomicIncrementRef(&self->refcount);
+  }
 }
