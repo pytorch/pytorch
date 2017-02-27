83c0fa0cd0b31c9f34261808321f17242d4e0617
Luke Yeager <lukeyeager@users.noreply.github.com>
2017-02-27 12:31:16 -0800

    [Lint] Address F812

--- tools/cwrap/plugins/KwargsPlugin.py
+++ tools/cwrap/plugins/KwargsPlugin.py
@@ -53,9 +53,9 @@ class KwargsPlugin(CWrapPlugin):
                         name not in seen_args):
                     seen_args.add(name)
                     args.append(name)
-        declarations = '\n    '.join(['PyObject *__kw_{} = NULL;'.format(name) for name in args])
+        declarations = '\n    '.join(['PyObject *__kw_{} = NULL;'.format(a) for a in args])
         lookups = '\n      '.join(
-            ['__kw_{name} = PyDict_GetItemString(kwargs, "{name}");'.format(name=name) for name in args])
+            ['__kw_{name} = PyDict_GetItemString(kwargs, "{name}");'.format(name=a) for a in args])
         start_idx = code.find('{') + 1
         new_code = self.WRAPPER_TEMPLATE.substitute(declarations=declarations, lookups=lookups)
         return code[:start_idx] + new_code + code[start_idx:]
