65b66264d49d153060b820e1f33919038b7978b6
Sam Gross <sgross@fb.com>
2017-02-28 19:38:06 -0800

    Improve broadcast/reduce performance by coalescing tensors

--- torch/nn/parallel/_functions.py
+++ torch/nn/parallel/_functions.py
@@ -1,4 +1,3 @@
-import torch.cuda
 import torch.cuda.comm as comm
 from torch.autograd import Function
 
@@ -9,13 +8,19 @@ class Broadcast(Function):
         super(Broadcast, self).__init__()
         self.target_gpus = target_gpus
 
-    def forward(self, input):
-        assert input.is_cuda, "Broadcast function not implemented for CPU tensors"
-        self.input_device = input.get_device()
-        return comm.broadcast(input, self.target_gpus)
+    def forward(self, *inputs):
+        if not all(input.is_cuda for input in inputs):
+            raise TypeError('Broadcast function not implemented for CPU tensors')
+        if len(inputs) == 0:
+            return tuple()
+        self.input_device = inputs[0].get_device()
+        outputs = comm.broadcast_coalesced(inputs, self.target_gpus)
+        return tuple([t for tensors in outputs for t in tensors])
 
-    def backward(self, *grad_output):
-        return comm.reduce_add(grad_output, self.input_device)
+    def backward(self, *grad_outputs):
+        grad_outputs = [grad_outputs[i:i + self.num_inputs]
+                        for i in range(0, len(grad_outputs), self.num_inputs)]
+        return comm.reduce_add_coalesced(grad_outputs, self.input_device)
 
 
 class Gather(Function):
