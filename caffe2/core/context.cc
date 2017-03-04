4c9d6d6a3276eb00e1135a63026ef32b9e30dfd0
Yangqing Jia <jiayq@fb.com>
2017-03-04 15:42:14 -0800

    cpu memory allocation reporter
    Summary:
    This allows one to report the CPU memory allocation over a Caffe2 run.
    To enable, use --caffe2_report_cpu_memory_usage in the commandline arguments.
    This has to happen before any Caffe2 allocation has taken place.
    
    Reviewed By: salexspb
    
    Differential Revision: D4641353
    
    fbshipit-source-id: 13a4315f63154edad9e925bb5c276cad4fe78c70

--- caffe2/core/context.cc
+++ caffe2/core/context.cc
@@ -1,6 +1,12 @@
 #include "caffe2/core/context.h"
-#include "caffe2/core/typeid.h"
+#include "caffe2/core/logging.h"
 #include "caffe2/core/tensor.h"
+#include "caffe2/core/typeid.h"
+
+CAFFE2_DEFINE_bool(
+    caffe2_report_cpu_memory_usage,
+    false,
+    "If set, print out detailed memory usage");
 
 namespace caffe2 {
 
@@ -11,6 +17,26 @@ CPUAllocator* GetCPUAllocator() {
 
 void SetCPUAllocator(CPUAllocator* alloc) {
   g_cpu_allocator.reset(alloc);
+}
+
+MemoryAllocationReporter CPUContext::reporter_;
+
+void MemoryAllocationReporter::New(void* ptr, size_t nbytes) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  size_table_[ptr] = nbytes;
+  allocated_ += nbytes;
+  LOG(INFO) << "Caffe2 alloc " << nbytes << " bytes, total alloc " << allocated_
+            << " bytes.";
+}
+
+void MemoryAllocationReporter::Delete(void* ptr) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  auto it = size_table_.find(ptr);
+  CHECK(it != size_table_.end());
+  allocated_ -= it->second;
+  LOG(INFO) << "Caffe2 deleted " << it->second << " bytes, total alloc "
+            << allocated_ << " bytes.";
+  size_table_.erase(it);
 }
 
 }  // namespace caffe2
