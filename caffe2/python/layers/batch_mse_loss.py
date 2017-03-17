2c8bf2525b1c4bd3e1d09f926e4dbc8eae293c68
Kevin Waugh <kwaugh@fb.com>
2017-03-16 17:26:55 -0700

    added BatchL2Loss layer
    Summary: layer that takes a label, prediction pair and outputs the L2 loss
    
    Reviewed By: kittipatv
    
    Differential Revision: D4702111
    
    fbshipit-source-id: 09f2ede44d1b548e61096de741f1b2aa0b66bbcb

---
+++ caffe2/python/layers/batch_mse_loss.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from caffe2.python import schema
+from caffe2.python.layers.layers import (
+    ModelLayer,
+)
+from caffe2.python.layers.tags import (
+    Tags
+)
+import numpy as np
+
+
+class BatchMSELoss(ModelLayer):
+
+    def __init__(self, model, input_record, name='batch_mse_loss', **kwargs):
+        super(BatchMSELoss, self).__init__(model, name, input_record, **kwargs)
+
+        assert schema.is_schema_subset(
+            schema.Struct(
+                ('label', schema.Scalar()),
+                ('prediction', schema.Scalar())
+            ),
+            input_record
+        )
+        self.tags.update({Tags.TRAIN_ONLY})
+
+        self.output_schema = schema.Scalar(
+            np.float32,
+            model.net.NextScopedBlob(name + '_output'))
+
+    def add_ops(self, net):
+        prediction = net.Squeeze(
+            self.input_record.prediction(),
+            net.NextScopedBlob('squeezed_prediction'),
+            dims=[1]
+        )
+
+        label = net.StopGradient(
+            self.input_record.label(),
+            net.NextScopedBlob('stopped_label')
+        )
+
+        l2dist = net.SquaredL2Distance(
+            [label, prediction],
+            net.NextScopedBlob('l2')
+        )
+
+        net.AveragedLoss(l2dist, self.output_schema.field_blobs())
