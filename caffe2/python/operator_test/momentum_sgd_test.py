b41449b680a6e6477e737b28430c99fca95405f0
James Cross <jcross@fb.com>
2017-03-28 07:43:54 -0700

    SparseMomentumSGDUpdateOp
    Summary: Creates SparseMomentumSGDUpdate, a sparse version of MomentumSGDUpdate, to make that optimization method (via in-place updating operator) compatible with GradientSlices.
    
    Differential Revision: D4784973
    
    fbshipit-source-id: e6330f471a4d5f53589a6ac245e38f256ca7f354

--- caffe2/python/operator_test/momentum_sgd_test.py
+++ caffe2/python/operator_test/momentum_sgd_test.py
@@ -2,12 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
 from caffe2.python import core
-from hypothesis import given
 import caffe2.python.hypothesis_test_util as hu
+
+from hypothesis import given
 import hypothesis.strategies as st
 import numpy as np
-
 import unittest
 
 
@@ -58,6 +59,47 @@ class TestMomentumSGD(hu.HypothesisTestCase):
             inputs=[grad, param_momentum, lr],
             reference=momentum_sgd
         )
+
+    @given(inputs=hu.tensors(n=3),
+           momentum=st.floats(min_value=0.1, max_value=0.9),
+           nesterov=st.booleans(),
+           lr=st.floats(min_value=0.1, max_value=0.9),
+           **hu.gcs_cpu_only)
+    def test_sparse_momentum_sgd(
+            self, inputs, momentum, nesterov, lr, gc, dc):
+        w, grad, m = inputs
+        indices = np.arange(m.shape[0])
+        indices = indices[indices % 2 == 0]
+
+        grad = grad[indices]
+        m = np.abs(m)
+        lr = np.asarray([lr], dtype=np.float32)
+
+        op = core.CreateOperator(
+            "SparseMomentumSGDUpdate",
+            ["grad", "m", "lr", "param", "indices"],
+            ["adjusted_grad", "m", "param"],
+            momentum=momentum,
+            nesterov=int(nesterov),
+            device_option=gc)
+
+        # Reference
+        def momentum_sgd(grad, m, lr):
+            lr = lr[0]
+            if not nesterov:
+                adjusted_gradient = lr * grad + momentum * m
+                return (adjusted_gradient, adjusted_gradient)
+            else:
+                m_new = momentum * m + lr * grad
+                return ((1 + momentum) * m_new - momentum * m, m_new)
+
+        def sparse(grad, m, lr, param, i):
+            grad_new, m_new = momentum_sgd(grad, m[i], lr)
+            m[i] = m_new
+            param[i] -= grad_new
+            return (grad_new, m, param)
+
+        self.assertReferenceChecks(gc, op, [grad, m, lr, w, indices], sparse)
 
 
 if __name__ == "__main__":
