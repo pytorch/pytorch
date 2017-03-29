2a288803919dad481f52ea50c2e9c45ec3a0a06b
Andrey Malevich <amalevich@fb.com>
2017-03-29 03:51:24 -0700

    Add eval net for layer_model_helper
    Summary:
    This diff is adding eval nets to layer model helper. It should be useful for
    the cases when train/eval nets need some extra input (usually some supervision)
    for train/eval. For example various sampled layers, etc.
    
    Differential Revision: D4769453
    
    fbshipit-source-id: 7a8ec7024051eab73b8869ec21e20b5f10fd9acb

--- caffe2/python/layers/layers.py
+++ caffe2/python/layers/layers.py
@@ -18,6 +18,7 @@ class InstantiationContext(object):
     """
     List of contexts where layer could be instantitated
     """
+    EVAL = 'eval'
     TRAINING = 'training'
     PREDICTION = 'prediction'
 
@@ -60,11 +61,41 @@ def _is_request_only_scalar(scalar):
 
 class ModelLayer(object):
 
-    def __init__(self, model, prefix, input_record, tags=None, **kwargs):
+    def __init__(self, model, prefix, input_record,
+                 predict_input_record_fields=None, tags=None, **kwargs):
+        """
+        Base class for model layers. Layer is an abstraction that allows to
+        provide model description in terms of meta-operators, where each of the
+        meta-operators can have different implementations for training,
+        evaluation and prediction, that are instantiated later. As an example
+        SampledSoftmax can do something related to sampling depending on
+        supervision during the training and just apply softmax if it's used for
+        prediction/evaluation.
+
+        All inputs/outputs from layers are represented as a record (instance of
+        schema bounded to blobs) and are accessible through input_record and
+        output_schema. If Layer needs to have only a subset of inputs/provides
+        subset of outputs during the inference - it should provide
+        predict_input_record and predict_output_schema correspondingly (those
+        records are expected to be a subset of input_record/output_schema).
+
+        Each layer is also have list of Tags associated with it, that depends on
+        current context and arguments. It's possible to use those tags during
+        the instantiation time.
+
+        """
         self.name = model.next_layer_name(prefix)
         self.model = model
         self.kwargs = kwargs
-        self.input_record = input_record
+        self._input_record = input_record
+        if predict_input_record_fields:
+            if not isinstance(predict_input_record_fields, list):
+                predict_input_record_fields = [predict_input_record_fields]
+            self._predict_input_record = self._input_record[
+                predict_input_record_fields]
+        else:
+            self._predict_input_record = None
+
         self.request_only = True
         if len(input_record.all_scalars()) == 0:
             self.request_only = False
@@ -72,17 +103,51 @@ class ModelLayer(object):
             if not _is_request_only_scalar(scalar):
                 self.request_only = False
                 break
-        self.output_schema = None
-        self.tags = set(tags or set())
+
+        self._output_schema = None
+        self._predict_output_schema = None
+        self.eval_output_schema = None
+        self.tags = set(tags or [])
         self.tags.update(TagContext.current().tags)
         self.params = []
 
     def get_type(self):
         return self.__class__.__name__
 
-    def get_output_schema(self):
-        assert self.output_schema is not None, "Schema is not initialized"
-        return self.output_schema
+    def _check_output_schema(self):
+        assert self._output_schema is not None, "Schema is not initialized"
+        assert (self._predict_output_schema is None or
+                schema.is_schema_subset(self._predict_output_schema,
+                                        self._output_schema)), (
+            "predict_output_schema is not a subset of the output_schema")
+
+    @property
+    def predict_input_record(self):
+        return self._predict_input_record or self._input_record
+
+    @property
+    def input_record(self):
+        return self._input_record
+
+    @property
+    def predict_output_schema(self):
+        self._check_output_schema()
+        return self._predict_output_schema or self._output_schema
+
+    @predict_output_schema.setter
+    def predict_output_schema(self, output_schema):
+        assert self._predict_output_schema is None
+        self._predict_output_schema = output_schema
+
+    @property
+    def output_schema(self):
+        self._check_output_schema()
+        return self._output_schema
+
+    @output_schema.setter
+    def output_schema(self, output_schema):
+        assert self._output_schema is None
+        self._output_schema = output_schema
 
     def get_parameters(self):
         return self.params
@@ -99,9 +164,10 @@ class ModelLayer(object):
         # Namescope below should warranty that all intermediate blobs will be
         # assiciated with the layer that produces them
         with scope.NameScope(self.name):
-            if context != InstantiationContext.PREDICTION:
-                assert init_net,\
-                    "Only prediction context can be used without init_net"
+            if context not in {InstantiationContext.PREDICTION,
+                               InstantiationContext.EVAL}:
+                assert init_net, (
+                    "Only prediction and eval context don't need init_net")
             if init_net:
                 for param in self.params:
                     # TODO(amalevich): Either return back to lambdas, that add
@@ -111,13 +177,20 @@ class ModelLayer(object):
                     init_net._net.op.extend([param.initializer])
             if context == InstantiationContext.TRAINING:
                 self.add_train_ops(net)
+            elif context == InstantiationContext.EVAL:
+                self.add_eval_ops(net)
             else:
                 self.add_ops(net)
 
     def add_ops(self, net):
         raise NotImplementedError
 
-    def add_train_ops(self, net):
+    def add_eval_ops(self, net):
         # Default train layer implementation is completely matching predict
         # layer implementation.
         self.add_ops(net)
+
+    def add_train_ops(self, net):
+        # Default eval layer implementation is completely matching eval
+        # layer implementation.
+        self.add_eval_ops(net)
--- caffe2/python/layers_test.py
+++ caffe2/python/layers_test.py
@@ -3,79 +3,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from collections import namedtuple
-
 from caffe2.python import (
-    core,
     layer_model_instantiator,
-    layer_model_helper,
     schema,
-    test_util,
+)
+from caffe2.python.layer_test_util import (
+    LayersTestCase,
+    OpSpec,
 )
 import numpy as np
 
 
-OpSpec = namedtuple("OpSpec", "type input output")
-
-
-class TestLayers(test_util.TestCase):
-
-    def setUp(self):
-        super(TestLayers, self).setUp()
-        input_feature_schema = schema.Struct(
-            ('float_features', schema.Scalar((np.float32, (32,)))),
-        )
-        trainer_extra_schema = schema.Struct()
-
-        self.model = layer_model_helper.LayerModelHelper(
-            'test_model',
-            input_feature_schema=input_feature_schema,
-            trainer_extra_schema=trainer_extra_schema)
-
-    def new_record(self, schema_obj):
-        return schema.NewRecord(self.model.net, schema_obj)
-
-    def get_training_nets(self):
-        """
-        We don't use
-        layer_model_instantiator.generate_training_nets_forward_only()
-        here because it includes initialization of global constants, which make
-        testing tricky
-        """
-        train_net = core.Net('train_net')
-        train_init_net = core.Net('trai_init_net')
-        for layer in self.model.layers:
-            layer.add_operators(train_net, train_init_net)
-        return train_init_net, train_net
-
-    def get_predict_net(self):
-        return layer_model_instantiator.generate_predict_net(self.model)
-
-    def assertBlobsEqual(self, spec_blobs, op_blobs):
-        """
-        spec_blobs can either be None or a list of blob names. If it's None,
-        then no assertion is performed. The elements of the list can be None,
-        in that case, it means that position will not be checked.
-        """
-        if spec_blobs is None:
-            return
-        self.assertEqual(len(spec_blobs), len(op_blobs))
-        for spec_blob, op_blob in zip(spec_blobs, op_blobs):
-            if spec_blob is None:
-                continue
-            self.assertEqual(spec_blob, op_blob)
-
-    def assertNetContainOps(self, net, op_specs):
-        """
-        Given a net and a list of OpSpec's, check that the net match the spec
-        """
-        ops = net.Proto().op
-        self.assertEqual(len(op_specs), len(ops))
-        for op, op_spec in zip(ops, op_specs):
-            self.assertEqual(op_spec.type, op.type)
-            self.assertBlobsEqual(op_spec.input, op.input)
-            self.assertBlobsEqual(op_spec.output, op.output)
-        return ops
+class TestLayers(LayersTestCase):
 
     def testFCWithoutBias(self):
         output_dims = 2
