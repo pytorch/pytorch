3d8420611c9602edfc3d0503adb4558280f24986
Uridah Sami Ahmed <11beseuahmed@seecs.edu.pk>
2017-03-31 02:51:37 +0500

    Bilinear module

--- torch/nn/modules/bilinear.py
+++ torch/nn/modules/bilinear.py
@@ -5,38 +5,36 @@ from torch.nn.parameter import Parameter
 
 from .module import Module
 
+
 class Bilinear(Module):
 
-	def __init__(self, in_features1, in_features2, out_features, bias=True):
-		super(Bilinear, self).__init__()
-		self.in_features1 = in_features1
-		self.in_features2 = in_features2
-		self.out_features = out_features
+    def __init__(self, in_features1, in_features2, out_features, bias=True):
+        super(Bilinear, self).__init__()
+        self.in_features1 = in_features1
+        self.in_features2 = in_features2
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features1, in_features2))
 
-		self.weight = Parameter(torch.Tensor(out_features, in_features1, in_features2))
-		
-		if bias:
-		    self.bias = Parameter(torch.Tensor(out_features))
-		    
-		else:
-		    #self.bias = None
-		    self.register_parameter('bias', None)
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
 
-	    
-	def reset_parameters(self):
-		stdv = 1. / math.sqrt(self.weight.size(1))
-		self.weight.data.uniform_(-stdv, stdv)
-		if self.bias is not None:
-			self.bias.data.uniform_(-stdv, stdv)
-	        
-	def forward(self, input1, input2):
-		if self.bias is None:
-			return self._backend.Bilinear()(input1, input2, self.weight)
-		else:
-			return self._backend.Bilinear()(input1, input2, self.weight, self.bias)
-	        
-	def __repr__(self):
-		return self.__class__.__name__ + ' (' \
-			+ str(self.in_features1) + ' x ' \
-			+ str(self.in_features2) + ' -> ' \
-			+ str(self.out_features) + ')'
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input1, input2):
+        if self.bias is None:
+            return self._backend.Bilinear()(input1, input2, self.weight)
+        else:
+            return self._backend.Bilinear()(input1, input2, self.weight, self.bias)
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+            + str(self.in_features1) + ' x ' \
+            + str(self.in_features2) + ' -> ' \
+            + str(self.out_features) + ')'
