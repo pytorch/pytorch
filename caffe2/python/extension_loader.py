ffd188322941cacdf4e3d239528cd9e58aaba999
Yangqing Jia <jiayq84@gmail.com>
2017-03-30 14:27:35 -0700

    Make extension loader properly handle visibility.
    Summary:
    (Also, exposed the macros that we use during build time via the macros.h header file)
    Closes https://github.com/caffe2/caffe2/pull/233
    
    Differential Revision: D4803311
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 9f8ce57692f81f7a8994344846d3c90aa2c7070a

--- caffe2/python/extension_loader.py
+++ caffe2/python/extension_loader.py
@@ -5,11 +5,19 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import contextlib
+import ctypes
+import sys
+
+
+_set_global_flags = (
+    hasattr(sys, 'getdlopenflags') and hasattr(sys, 'setdlopenflags'))
+
 
 @contextlib.contextmanager
 def DlopenGuard():
-    # This is a stub for setting up special tricks around python extensions
-    # loading. For example, it might do
-    #   sys.setdlopenflags(DLFCN.RTLD_GLOBAL | DLFCN.RTLD_NOW)
-    # which might be required in some setups of python
+    if _set_global_flags:
+        old_flags = sys.getdlopenflags()
+        sys.setdlopenflags(old_flags | ctypes.RTLD_GLOBAL)
     yield
+    if _set_global_flags:
+        sys.setdlopenflags(old_flags)
