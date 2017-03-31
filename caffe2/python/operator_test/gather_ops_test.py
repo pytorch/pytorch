9b9f7e0c83c5505a33d443b6ff5ee304c6b1d26c
Pooya Davoodi <pdavoodi@nvidia.com>
2017-03-31 13:14:41 -0700

    Add GatherOp for GPU, and update its tests.
    Summary:
    This is an initial (read: unoptimized) implementation of GatherOp on GPU.
    Closes https://github.com/caffe2/caffe2/pull/209
    
    Differential Revision: D4809676
    
    Pulled By: Yangqing
    
    fbshipit-source-id: bc36fa02e9964370ca845e9cc13344e5f3dbf176

--- caffe2/python/operator_test/gather_ops_test.py
+++ caffe2/python/operator_test/gather_ops_test.py
@@ -6,18 +6,30 @@ import numpy as np
 
 from caffe2.python import core, workspace
 from caffe2.python.test_util import TestCase
+from hypothesis import given
+import caffe2.python.hypothesis_test_util as hu
+import hypothesis.strategies as st
 
-
-class TestGatherOps(TestCase):
-    def test_gather_ops(self):
-        data = np.array(["world", "hello", "!"], dtype='|S')
-        ind = np.array([1, 0, 2], dtype=np.int32)
-        workspace.FeedBlob('data', data)
-        workspace.FeedBlob('ind', ind)
-        workspace.RunOperatorOnce(core.CreateOperator(
-            'Gather', ['data', 'ind'], ['word']))
-        outdata = np.array(["hello", "world", "!"], dtype='|S')
-        assert((workspace.FetchBlob('word') == outdata).all())
+class TestGatherOps(hu.HypothesisTestCase):
+    @given(rows_num=st.integers(1, 10000), 
+           index_num=st.integers(1, 5000),
+           **hu.gcs)
+    def test_gather_ops(self, rows_num, index_num, gc, dc):
+        data = np.random.random((rows_num, 10, 20)).astype(np.float32)
+        ind = np.random.randint(rows_num, size=(index_num,1)).astype('int32')
+        op = core.CreateOperator(
+            'Gather', 
+            ['data', 'ind'], 
+            ['output'])
+        def ref_gather(data, ind):
+            output = [r for r in [data[i] for i in ind]]
+            return [output]
+        self.assertReferenceChecks(
+            gc,
+            op=op,
+            inputs=[data, ind],
+            reference=ref_gather,
+        )
 
 if __name__ == "__main__":
     import unittest
