4848e1d75baf2753697a8edd88dc68b334026aad
Yangqing Jia <jiayq84@gmail.com>
2017-03-31 13:15:44 -0700

    Fix protobuf build to properly include directories
    Summary:
    aaronmarkham - I think this should fix the oss build.
    slayton58 FYI
    kmatzen FYI
    Closes https://github.com/caffe2/caffe2/pull/238
    
    Differential Revision: D4812550
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 5703e403ef22c02e87f885bad8379fd5a8e06cdb

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -45,44 +45,14 @@ if (NOT (Protobuf_FOUND OR PROTOBUF_FOUND) )
   message(FATAL_ERROR "Could not find Protobuf or compile local version.")
 endif()
 
-# place where to generate protobuf sources
-set(proto_gen_folder "${PROJECT_BINARY_DIR}/include/caffe/proto")
-include_directories("${PROJECT_BINARY_DIR}/include")
-
-set(PROTOBUF_GENERATE_CPP_APPEND_PATH TRUE)
-
 ################################################################################################
 # Modification of standard 'protobuf_generate_cpp()' with output dir parameter and python support
 # Usage:
-#   caffe_protobuf_generate_cpp_py(<output_dir> <srcs_var> <hdrs_var> <python_var> <proto_files>)
-function(caffe_protobuf_generate_cpp_py output_dir srcs_var hdrs_var python_var)
+#   caffe2_protobuf_generate_cpp_py(<srcs_var> <hdrs_var> <python_var> <proto_files>)
+function(caffe2_protobuf_generate_cpp_py srcs_var hdrs_var python_var)
   if(NOT ARGN)
     message(SEND_ERROR "Error: caffe_protobuf_generate_cpp_py() called without any proto files")
     return()
-  endif()
-
-  if(PROTOBUF_GENERATE_CPP_APPEND_PATH)
-    # Create an include path for each file specified
-    foreach(fil ${ARGN})
-      get_filename_component(abs_fil ${fil} ABSOLUTE)
-      get_filename_component(abs_path ${abs_fil} PATH)
-      list(FIND _protoc_include ${abs_path} _contains_already)
-      if(${_contains_already} EQUAL -1)
-        list(APPEND _protoc_include -I ${abs_path})
-      endif()
-    endforeach()
-  else()
-    set(_protoc_include -I ${CMAKE_CURRENT_SOURCE_DIR})
-  endif()
-
-  if(DEFINED PROTOBUF_IMPORT_DIRS)
-    foreach(dir ${PROTOBUF_IMPORT_DIRS})
-      get_filename_component(abs_path ${dir} ABSOLUTE)
-      list(FIND _protoc_include ${abs_path} _contains_already)
-      if(${_contains_already} EQUAL -1)
-        list(APPEND _protoc_include -I ${abs_path})
-      endif()
-    endforeach()
   endif()
 
   set(${srcs_var})
@@ -92,17 +62,18 @@ function(caffe_protobuf_generate_cpp_py output_dir srcs_var hdrs_var python_var)
     get_filename_component(abs_fil ${fil} ABSOLUTE)
     get_filename_component(fil_we ${fil} NAME_WE)
 
-    list(APPEND ${srcs_var} "${output_dir}/${fil_we}.pb.cc")
-    list(APPEND ${hdrs_var} "${output_dir}/${fil_we}.pb.h")
-    list(APPEND ${python_var} "${output_dir}/${fil_we}_pb2.py")
+    list(APPEND ${srcs_var} "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.cc")
+    list(APPEND ${hdrs_var} "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.h")
+    list(APPEND ${python_var} "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}_pb2.py")
 
     add_custom_command(
-      OUTPUT "${output_dir}/${fil_we}.pb.cc"
-             "${output_dir}/${fil_we}.pb.h"
-             "${output_dir}/${fil_we}_pb2.py"
-      COMMAND ${CMAKE_COMMAND} -E make_directory "${output_dir}"
-      COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} --cpp_out    ${output_dir} ${_protoc_include} ${abs_fil}
-      COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} --python_out ${output_dir} ${_protoc_include} ${abs_fil}
+      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.cc"
+             "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.h"
+             "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}_pb2.py"
+      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+      COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_CURRENT_BINARY_DIR}"
+      COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} -I${PROJECT_SOURCE_DIR} --cpp_out    "${PROJECT_BINARY_DIR}" ${abs_fil}
+      COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} -I${PROJECT_SOURCE_DIR} --python_out "${PROJECT_BINARY_DIR}" ${abs_fil}
       DEPENDS ${abs_fil}
       COMMENT "Running C++/Python protocol buffer compiler on ${fil}" VERBATIM )
   endforeach()
