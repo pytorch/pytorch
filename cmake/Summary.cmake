46e62d28ec18e00f9b96f74fc2f0419ccc5f7e86
Yangqing Jia <jiayq84@gmail.com>
2017-03-31 00:21:22 -0700

    Add git submodule init command
    Summary:
    aaronmarkham
    Closes https://github.com/caffe2/caffe2/pull/237
    
    Differential Revision: D4808930
    
    Pulled By: Yangqing
    
    fbshipit-source-id: c598fac789e97280d12961b0be257607ebf82244

--- cmake/Summary.cmake
+++ cmake/Summary.cmake
@@ -1,23 +1,21 @@
 # Prints accumulated Caffe2 configuration summary
 function (Caffe2_print_configuration_summary)
 
-  find_package(Git)
   if(GIT_FOUND)
     execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always --dirty
                     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
                     WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-                    OUTPUT_VARIABLE Caffe_GIT_VERSION
+                    OUTPUT_VARIABLE Caffe2_GIT_VERSION
                     RESULT_VARIABLE __git_result)
     if(NOT ${__git_result} EQUAL 0)
-      set(Caffe_GIT_VERSION "unknown")
+      set(Caffe2_GIT_VERSION "unknown")
     endif()
   endif()
 
   message(STATUS "")
   message(STATUS "******** Summary ********")
   message(STATUS "General:")
-  message(STATUS "  Version               : ${Caffe2_VERSION}")
-  message(STATUS "  Git                   : ${Caffe2_GIT_VERSION}")
+  message(STATUS "  Git version           : ${Caffe2_GIT_VERSION}")
   message(STATUS "  System                : ${CMAKE_SYSTEM_NAME}")
   message(STATUS "  C++ compiler          : ${CMAKE_CXX_COMPILER}")
   message(STATUS "  Protobuf compiler     : ${PROTOBUF_PROTOC_EXECUTABLE}")
@@ -43,14 +41,8 @@ function (Caffe2_print_configuration_summary)
   endif()
 
   message(STATUS "  USE_GLOG              : ${USE_GLOG}")
-  if(${USE_GLOG})
-  message(STATUS "    glog version        : ${GLOG_VERSION}")
-  endif()
 
   message(STATUS "  USE_GFLAGS            : ${USE_GFLAGS}")
-  if(${USE_GFLAGS})
-  message(STATUS "    gflags version      : ${GFLAGS_VERSION}")
-  endif()
 
   message(STATUS "  USE_LMDB              : ${USE_LMDB}")
   if(${USE_LMDB})
