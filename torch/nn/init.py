3b6213958566a57f79c067269ff4f9ed76339920
Kaixhin <design@kaixhin.com>
2017-04-02 15:02:53 +0100

    Tidy up init docs

--- torch/nn/init.py
+++ torch/nn/init.py
@@ -6,10 +6,10 @@ from torch.autograd import Variable
 
 
 def uniform(tensor, a=0, b=1):
-    """Fills the input Tensor or Variable with values drawn from a uniform U(a,b)
+    """Fills the input Tensor or Variable with values drawn from the uniform distribution :math:`U(a, b)`
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         a: the lower bound of the uniform distribution
         b: the upper bound of the uniform distribution
 
@@ -24,10 +24,10 @@ def uniform(tensor, a=0, b=1):
 
 
 def normal(tensor, mean=0, std=1):
-    """Fills the input Tensor or Variable with values drawn from a normal distribution with the given mean and std
+    """Fills the input Tensor or Variable with values drawn from the normal distribution :math:`N(mean, std)`
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         mean: the mean of the normal distribution
         std: the standard deviation of the normal distribution
 
@@ -45,7 +45,7 @@ def constant(tensor, val):
     """Fills the input Tensor or Variable with the value `val`
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         val: the value to fill the tensor with
 
     Examples:
@@ -60,7 +60,8 @@ def constant(tensor, val):
 
 def _calculate_fan_in_and_fan_out(tensor):
     if tensor.ndimension() < 2:
-        raise ValueError("fan in and fan out can not be computed for tensor of size ", tensor.size())
+        raise ValueError("Fan in and fan out can not be computed for tensor with less than 2 dimensions ",
+                         tensor.ndimension())
 
     if tensor.ndimension() == 2:  # Linear
         fan_in = tensor.size(1)
@@ -80,12 +81,12 @@ def _calculate_fan_in_and_fan_out(tensor):
 def xavier_uniform(tensor, gain=1):
     """Fills the input Tensor or Variable with values according to the method described in "Understanding the
     difficulty of training deep feedforward neural networks" - Glorot, X. and Bengio, Y., using a uniform
-    distribution. The resulting tensor will have values sampled from U(-a, a) where a = gain * sqrt(2/(fan_in +
-    fan_out)) * sqrt(3)
+    distribution. The resulting tensor will have values sampled from :math:`U(-a, a)` where
+    `a = gain * sqrt(2/(fan_in + fan_out)) * sqrt(3)`
 
     Args:
-        tensor: a n-dimension torch.Tensor
-        gain: an optional scaling factor to be applied
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
+        gain: an optional scaling factor
 
     Examples:
         >>> w = torch.Tensor(3, 5)
@@ -104,12 +105,12 @@ def xavier_uniform(tensor, gain=1):
 def xavier_normal(tensor, gain=1):
     """Fills the input Tensor or Variable with values according to the method described in "Understanding the
     difficulty of training deep feedforward neural networks" - Glorot, X. and Bengio, Y., using a normal
-    distribution. The resulting tensor will have values sampled from normal distribution with mean=0 and std = gain *
-    sqrt(2/(fan_in + fan_out))
+    distribution. The resulting tensor will have values sampled from :math:`N(0, std)` where
+    `std = gain * sqrt(2/(fan_in + fan_out))`
 
     Args:
-        tensor: a n-dimension torch.Tensor
-        gain: an optional scaling factor to be applied
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
+        gain: an optional scaling factor
 
     Examples:
         >>> w = torch.Tensor(3, 5)
@@ -128,7 +129,7 @@ def _calculate_correct_fan(tensor, mode):
     mode = mode.lower()
     valid_modes = ['fan_in', 'fan_out']
     if mode not in valid_modes:
-        raise ValueError("mode {} not supported, please use one of {}".format(mode, valid_modes))
+        raise ValueError("Mode {} not supported, please use one of {}".format(mode, valid_modes))
 
     fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
     if mode == 'fan_in':
@@ -140,11 +141,11 @@ def _calculate_correct_fan(tensor, mode):
 def kaiming_uniform(tensor, a=0, mode='fan_in'):
     """Fills the input Tensor or Variable with values according to the method described in "Delving deep into
     rectifiers: Surpassing human-level performance on ImageNet classification" - He, K. et al using a uniform
-    distribution. The resulting tensor will have values sampled from U(-bound, bound) where bound = sqrt(2/((1 + a^2)
-    * fan_in)) * sqrt(3)
+    distribution. The resulting tensor will have values sampled from :math:`U(-bound, bound)` where
+    `bound = sqrt(2/((1 + a^2) * fan_in)) * sqrt(3)`
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         a: the coefficient of the slope of the rectifier used after this layer (0 for ReLU by default)
         mode: either 'fan_in' (default) or 'fan_out'. Choosing `fan_in` preserves the magnitude of the variance of the
               weights in the forward pass. Choosing `fan_out` preserves the magnitudes in the backwards pass.
@@ -166,11 +167,11 @@ def kaiming_uniform(tensor, a=0, mode='fan_in'):
 def kaiming_normal(tensor, a=0, mode='fan_in'):
     """Fills the input Tensor or Variable with values according to the method described in "Delving deep into
     rectifiers: Surpassing human-level performance on ImageNet classification" - He, K. et al using a normal
-    distribution. The resulting tensor will have values sampled from normal distribution with mean=0 and std = sqrt(
-    2/((1 + a^2) * fan_in))
+    distribution. The resulting tensor will have values sampled from :math:`N(0, std)` where
+    `std = sqrt(2/((1 + a^2) * fan_in))`
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         a: the coefficient of the slope of the rectifier used after this layer (0 for ReLU by default)
         mode: either 'fan_in' (default) or 'fan_out'. Choosing `fan_in` preserves the magnitude of the variance of the
               weights in the forward pass. Choosing `fan_out` preserves the magnitudes in the backwards pass.
@@ -191,13 +192,13 @@ def kaiming_normal(tensor, a=0, mode='fan_in'):
 def orthogonal(tensor, gain=1):
     """Fills the input Tensor or Variable with a (semi) orthogonal matrix. The input tensor must have at least 2
     dimensions, and for tensors with more than 2 dimensions the trailing dimensions are flattened. viewed as 2D
-    representation with rows equal to the first dimension and columns equal to the product of  as a sparse matrix,
-    where the non-zero elements will be drawn from a normal distribution with mean=0 and std=`std`. Reference: "Exact
+    representation with rows equal to the first dimension and columns equal to the product of as a sparse matrix???,
+    where the non-zero elements will be drawn from the normal distribution :math:`N(0, 1)`. Reference: "Exact
     solutions to the nonlinear dynamics of learning in deep linear neural networks"-Saxe, A. et al.
 
     Args:
-        tensor: a n-dimension torch.Tensor, where n >= 2
-        gain: optional gain to be applied
+        tensor: an n-dimensional torch.Tensor or autograd.Variable, where n >= 2
+        gain: optional scaling factor
 
     Examples:
         >>> w = torch.Tensor(3, 5)
@@ -208,7 +209,7 @@ def orthogonal(tensor, gain=1):
         return tensor
 
     if tensor.ndimension() < 2:
-        raise ValueError("Only tensors with 2 or more dimensions are supported.")
+        raise ValueError("Only tensors with 2 or more dimensions are supported")
     rows = tensor.size(0)
     cols = tensor[0].numel()
     flattened = torch.Tensor(rows, cols).normal_(0, 1)
@@ -224,11 +225,11 @@ def orthogonal(tensor, gain=1):
 
 
 def sparse(tensor, sparsity, std=0.01):
-    """Fills the 2D input Tensor or Variable as a sparse matrix, where the non-zero elements will be drawn from a
-    normal distribution with mean=0 and std=`std`.
+    """Fills the 2D input Tensor or Variable as a sparse matrix, where the non-zero elements will be drawn from
+    the normal distribution :math:`N(0, 0.01)`.
 
     Args:
-        tensor: a n-dimension torch.Tensor
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
         sparsity: The fraction of elements in each column to be set to zero
         std: the standard deviation of the normal distribution used to generate the non-zero values
 
