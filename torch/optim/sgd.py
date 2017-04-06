1a04b92226f084558db7c285818f6a4fa3db68a0
Nitish Shirish Keskar <keskarnitish@users.noreply.github.com>
2017-04-05 17:45:41 -0700

    add note regarding SGD momentum

--- torch/optim/sgd.py
+++ torch/optim/sgd.py
@@ -23,6 +23,19 @@ class SGD(Optimizer):
         >>> optimizer.step()
 
     __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+
+    .. note::
+        The implementation of SGD with Momentum/Nesterov subtly differs from
+        Sutskever et. al. and implementations in some other frameworks.
+        Considering the specific case of Momentum, the update can be written as
+        :math:`v = \rho * v + g`,
+        :math:`p = p - lr * v`,
+        where p, g, v and :math:`\rho` denote the parameters, gradient, velocity, and
+        momentum respectively. This is in constrast to Sutskever et. al. and
+        other frameworks which employ an update of the form:
+        :math:`v = \rho * v + lr * g`,
+        :math:`p = p - v`.
+        The Nesterov version is analogously modified.
     """
 
     def __init__(self, params, lr=required, momentum=0, dampening=0,
