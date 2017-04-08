d1af311224a6589be9cd20cd73a28d2af7069b07
Huazhong Ning <ninghz@fb.com>
2017-04-08 10:57:11 -0700

    PiecewiseLinearTransformOp supports passing params from input blobs.
    Summary:
    The PiecewiseLinearTransformOp passes the transform parameters (bounds, slopes, intercepts) via operator arg. This diff supports to pass these parameters through input blobs.
    
    The purpose is to allow us to create a model calibration net that can be exported when saving model.
    
    Reviewed By: dragonxlwang
    
    Differential Revision: D4777086
    
    fbshipit-source-id: 0d157154860f61ec6ecfab95aea80beed54aa5c6

--- caffe2/operators/piecewise_linear_transform_op.cc
+++ caffe2/operators/piecewise_linear_transform_op.cc
@@ -7,31 +7,38 @@ REGISTER_CPU_OPERATOR(
     PiecewiseLinearTransformOp<float, CPUContext>);
 
 OPERATOR_SCHEMA(PiecewiseLinearTransform)
-    .NumInputs(1)
+    .NumInputs(1, 4)
     .NumOutputs(1)
     .SetDoc(R"DOC(
-PiecewiseLinearTransform takes one inputs- predictions, a 2-D tensor
-(Tensor<float>) of size (batch_size x prediction_dimensions), and three args -
-upper bounds, slopes and intercepts of piecewise functions. The output tensor
-has the same shape of input tensor and contains the piecewise linear
-transformation. Each feature dimension has its own piecewise linear
-transformation function. Therefore the size of piecewise function parameters are
-all (pieces x prediction_dimensions). Note that in each piece, low bound is
-excluded while high bound is included. Also the piecewise linear function
-must be continuous. If the input is binary predictions (Nx2 tensor), set
-the binary arg to true (see details below).
+PiecewiseLinearTransform takes inputs -- predictions, a 2-D or 1-D tensor
+(Tensor<float>) of size (batch_size x prediction_dimensions). The piecewise
+linear functions are stored in bounds, slopes and intercepts. The output tensor
+has the same shape of input `predictions` and contains the predictions
+transformed by the piecewise linear functions. Each column of predictions has
+its own piecewise linear transformation functions. Therefore the size of
+piecewise function parameters are pieces x prediction_dimensions, except for
+binary predictions where only the positive prediction needs them. Note that in
+each piece, low bound is excluded while high bound is included. Also the
+piecewise linear function must be continuous.
+
+Notes
+- If the input is binary predictions (Nx2 or Nx1 tensor), set the binary arg
+to true so that one group of piecewise linear functions is needed (see
+details below).
+- The transform parameters (bounds, slopes, intercepts) can be passed either
+through args or through input blobs.
+- If we have multiple groups of piecewise linear functions, each group has the
+same number of pieces.
+- If a prediction is out of the bounds, it is capped to the smallest or largest
+bound.
 )DOC")
-    .Input(
-        0,
-        "predictions",
-        "2-D tensor (Tensor<float>) of size "
-        "(num_batches x num_classes) containing scores")
     .Arg(
         "bounds",
         "1-D vector of size (prediction_dimensions x (pieces+1)) contain the "
         "upper bounds of each piece of linear function. One special case is "
         "the first bound is the lower bound of whole piecewise function and we "
-        "treat it the same as the left most functions")
+        "treat it the same as the left most functions. (bounds, slopes, "
+        "intercepts) can passed through either arg or input blobs.")
     .Arg(
         "slopes",
         "1-D vector of size (prediction_dimensions x pieces) containing the "
@@ -41,14 +48,32 @@ the binary arg to true (see details below).
         "1-D vector of size (prediction_dimensions x pieces) containing the "
         "intercepts of linear function")
     .Arg(
-        "pieces",
-        "int value for the number of pieces for the piecewise linear function")
-    .Arg(
         "binary",
-        "If set true, we assume the input is a Nx2 tensor. Its first column is "
-        "negative predictions and second column is positive and "
-        "negative + positive = 1. We just need one set of transforms for the "
-        "positive column.")
+        "If set true, we assume the input is a Nx1 or Nx2 tensor. If it is Nx1 "
+        "tensor, it is positive predictions. If the input is Nx2 tensor, its "
+        "first column is negative predictions and second column is positive "
+        "and negative + positive = 1. We just need one group of piecewise "
+        "linear functions for the positive predictions.")
+    .Input(
+        0,
+        "predictions",
+        "2-D tensor (Tensor<float>) of size "
+        "(num_batches x num_classes) containing scores")
+    .Input(
+        1,
+        "bounds (optional)",
+        "See bounds in Arg. (bounds, slopes, intercepts) can passed through "
+        "either arg or input blobs.")
+    .Input(
+        2,
+        "slopes (optional)",
+        "See slopes in Arg. (bounds, slopes, intercepts) can passed through "
+        "either arg or input blobs.")
+    .Input(
+        3,
+        "intercepts (optional)",
+        "See intercepts in Arg. (bounds, slopes, intercepts) can passed "
+        "through either arg or input blobs.")
     .Output(
         0,
         "transforms",
