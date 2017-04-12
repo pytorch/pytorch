59d51325db14a423a1987c4e6a3ad11a84371854
Yiming Wu <wyiming@fb.com>
2017-04-12 01:15:23 -0700

    Add math::Gemv<double, CUDAContext> by cublas::cublasDgemv
    Summary: support double gemv in CUDAContext
    
    Differential Revision: D4872986
    
    fbshipit-source-id: c6397c5a3b2667ca446deca0f5edbcc7f29f7a1e

--- caffe2/utils/math_gpu_test.cc
+++ caffe2/utils/math_gpu_test.cc
@@ -67,4 +67,61 @@ TEST(MathUtilGPUTest, testAddStripedBatch) {
   }
 }
 
+#define TEST_GEMV_WITH_TYPE(field_name)                                      \
+  TEST(MathUtilGPUTest, testGemv_##field_name) {                             \
+    if (!HasCudaGPU())                                                       \
+      return;                                                                \
+    Workspace ws;                                                            \
+    DeviceOption option;                                                     \
+    option.set_device_type(CUDA);                                            \
+    CUDAContext context(option);                                             \
+    Blob* blobx = ws.CreateBlob("X");                                        \
+    Blob* bloby = ws.CreateBlob("Y");                                        \
+    Blob* blobz = ws.CreateBlob("Z");                                        \
+    Blob* bloby_host = ws.CreateBlob("Y_host");                              \
+                                                                             \
+    vector<int> shapex{64, 128};                                             \
+    vector<int> shapey{64};                                                  \
+    vector<int> shapez{128};                                                 \
+                                                                             \
+    auto* tensorx = blobx->GetMutable<Tensor<CUDAContext>>();                \
+    tensorx->Resize(shapex);                                                 \
+    math::Set<field_name, CUDAContext>(                                      \
+        64 * 128,                                                            \
+        (field_name)1.0,                                                     \
+        tensorx->mutable_data<field_name>(),                                 \
+        &context);                                                           \
+                                                                             \
+    auto* tensory = bloby->GetMutable<Tensor<CUDAContext>>();                \
+    tensory->Resize(shapey);                                                 \
+    math::Set<field_name, CUDAContext>(                                      \
+        64, (field_name)1.0, tensory->mutable_data<field_name>(), &context); \
+                                                                             \
+    auto* tensorz = blobz->GetMutable<Tensor<CUDAContext>>();                \
+    tensorz->Resize(shapez);                                                 \
+                                                                             \
+    math::Gemv<field_name, CUDAContext>(                                     \
+        CblasTrans,                                                          \
+        64,                                                                  \
+        128,                                                                 \
+        1.0,                                                                 \
+        tensorx->template data<field_name>(),                                \
+        tensory->mutable_data<field_name>(),                                 \
+        0.0,                                                                 \
+        tensorz->template mutable_data<field_name>(),                        \
+        &context);                                                           \
+    context.FinishDeviceComputation();                                       \
+                                                                             \
+    auto* tensory_host = bloby_host->GetMutable<Tensor<CPUContext>>();       \
+    tensory_host->CopyFrom<CUDAContext, CUDAContext>(*tensorz, &context);    \
+    context.FinishDeviceComputation();                                       \
+                                                                             \
+    for (int i = 0; i < 128; i++) {                                          \
+      EXPECT_EQ(tensory_host->data<field_name>()[i], 64.0);                  \
+    }                                                                        \
+  }
+
+TEST_GEMV_WITH_TYPE(float);
+TEST_GEMV_WITH_TYPE(double);
+
 } // namespace caffe2
