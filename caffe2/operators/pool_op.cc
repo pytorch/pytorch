c776dcf5f64eeb9f488366f4e567202ad27ba207
Ahmed Taei <ataei@fb.com>
2017-04-11 18:14:42 -0700

    Generalize PoolingOp(CPU) to compute 1D, 2D and 3D pooling.
    Summary: Extend the op compute 1D, 2D & 3D pooling.
    
    Differential Revision: D4828691
    
    fbshipit-source-id: 87540e82ed20d1361476cfbc43a708de9ca7a88e

--- caffe2/operators/pool_op.cc
+++ caffe2/operators/pool_op.cc
@@ -1,4 +1,4 @@
-// TODO: reduce the apparent redundancy of all the code below.
+// TODO(ataei): reduce the apparent redundancy of all the code below.
 #include "caffe2/operators/pool_op.h"
 #include "caffe2/utils/cpu_neon.h"
 
@@ -8,10 +8,72 @@ using std::max;
 using std::min;
 
 namespace {
-// These two classe are just used as template arguments passed to the PoolOp
-// template to instantiate the different algorithms.
-class AveragePool {};
-class MaxPool {};
+
+template <typename T>
+class AveragePool {
+ public:
+  static float initialize() {
+    return 0.0;
+  }
+
+  static void process(
+      const int x_col,
+      const int y_col,
+      ConstEigenMatrixMap<float>& x_mat,
+      EigenMatrixMap<float>& y_mat) {
+    y_mat.col(y_col) += x_mat.col(x_col);
+  }
+
+  static void process(const T& x_data, T& y_data) {
+    y_data += x_data;
+  }
+
+  static void finalize(const int size, T& y_data) {
+    y_data /= size;
+  }
+
+  static void
+  finalize(const int size, const int col, EigenMatrixMap<float>& y_mat) {
+    y_mat.col(col) /= size;
+  }
+
+  static bool runNeon() {
+    return true;
+  }
+};
+
+template <typename T>
+class MaxPool {
+ public:
+  static float initialize() {
+    return std::numeric_limits<float>::lowest();
+  }
+
+  static void process(
+      const int x_col,
+      const int y_col,
+      ConstEigenMatrixMap<float>& x_mat,
+      EigenMatrixMap<float>& y_mat) {
+    y_mat.col(y_col) = y_mat.col(y_col).cwiseMax(x_mat.col(x_col));
+  }
+
+  static void process(const T& x_data, T& y_data) {
+    if (x_data > y_data) {
+      y_data = x_data;
+    }
+  }
+
+  static void finalize(const int /*size*/, T& /*y_data*/) {}
+
+  static void finalize(
+      const int /*size*/,
+      const int col,
+      EigenMatrixMap<float>& /*y_mat*/) {}
+
+  static bool runNeon() {
+    return false;
+  }
+};
 
 #ifdef __ARM_NEON__
 
@@ -162,209 +224,245 @@ runNeonAveragePool4x4p0s0NCHW(int N, int C, int inputH, int inputW,
 
 }  // namespace
 
-template <>
-bool PoolOp<float, CPUContext, AveragePool>::RunOnDeviceWithOrderNCHW() {
+template <typename T, class Context, typename PoolType>
+bool PoolOp<T, Context, PoolType>::RunOnDeviceWithOrderNCHW() {
   auto& X = Input(0);
   auto* Y = Output(0);
-  ConvPoolOpBase::SetOutputSize(X, Y, X.dim32(1));
+  ConvPoolOpBase<Context>::SetOutputSize(X, Y, X.dim32(1));
 
-  const float* Xdata = X.data<float>();
-  float* Ydata = Y->mutable_data<float>();
+  const float* Xdata = X.template data<float>();
+  float* Ydata = Y->template mutable_data<float>();
   math::Set<float, CPUContext>(
-      Y->size(), 0, Ydata, &context_);
+      Y->size(), PoolType::initialize(), Ydata, &context_);
   // The main loop
   int channels = X.dim32(1);
   int height = X.dim32(2);
-  int width = X.dim32(3);
+  int width = kernel_.size() > 1 ? X.dim32(3) : 1;
+  int depth = kernel_.size() > 2 ? X.dim32(4) : 1;
   int pooled_height = Y->dim32(2);
-  int pooled_width = Y->dim32(3);
+  int pooled_width = kernel_.size() > 1 ? Y->dim32(3) : 1;
+  int pooled_depth = kernel_.size() > 2 ? Y->dim32(4) : 1;
 
 #ifdef __ARM_NEON__
   // We specialize certain variants on ARM for vectorization
-  if (isNeonEligible(
-          X.dim32(2),
-          X.dim32(3),
-          Y->dim32(2),
-          Y->dim32(3),
-          kernel_h(),
-          kernel_w(),
-          stride_h(),
-          stride_w(),
-          pad_t(),
-          pad_l(),
-          pad_b(),
-          pad_r(),
-          dilation_h(),
-          dilation_w(),
-          Xdata,
-          Ydata)) {
-    runNeonAveragePool4x4p0s0NCHW(X.dim32(0), X.dim32(1),
-                                  X.dim32(2), X.dim32(3),
-                                  Xdata, Ydata);
+  if (PoolType::runNeon() && isNeonEligible(
+                                 X.dim32(2),
+                                 X.dim32(3),
+                                 Y->dim32(2),
+                                 Y->dim32(3),
+                                 kernel_h(),
+                                 kernel_w(),
+                                 stride_h(),
+                                 stride_w(),
+                                 pad_t(),
+                                 pad_l(),
+                                 pad_b(),
+                                 pad_r(),
+                                 dilation_h(),
+                                 dilation_w(),
+                                 Xdata,
+                                 Ydata)) {
+    runNeonAveragePool4x4p0s0NCHW(
+        X.dim32(0), X.dim32(1), X.dim32(2), X.dim32(3), Xdata, Ydata);
     return true;
   }
 #endif // __ARM_NEON__
-
-  for (int n = 0; n < X.dim32(0); ++n) {
-    for (int c = 0; c < channels; ++c) {
-      for (int ph = 0; ph < pooled_height; ++ph) {
-        for (int pw = 0; pw < pooled_width; ++pw) {
-          int hstart = ph * stride_h() - pad_t();
-          int wstart = pw * stride_w() - pad_l();
-          int hend = min(hstart + kernel_h(), height);
-          int wend = min(wstart + kernel_w(), width);
-          hstart = max(hstart, 0);
-          wstart = max(wstart, 0);
-          const int pool_index = ph * pooled_width + pw;
-          for (int h = hstart; h < hend; ++h) {
-            for (int w = wstart; w < wend; ++w) {
-              const int input_index = h * width + w;
-              Ydata[pool_index] += Xdata[input_index];
-            }
-          }
-          Ydata[pool_index] /= (hend - hstart) * (wend - wstart);
-        }
-      }
-      // Do offset.
-      Xdata += height * width;
-      Ydata += pooled_height * pooled_width;
-    }
-  }
-  return true;
-}
-
-template <>
-bool PoolOp<float, CPUContext, AveragePool>::RunOnDeviceWithOrderNHWC() {
-  auto& X = Input(0);
-  auto* Y = Output(0);
-  int height = X.dim32(1);
-  int width = X.dim32(2);
-  int channels = X.dim32(3);
-  ConvPoolOpBase::SetOutputSize(X, Y, channels);
-  const float* Xdata = X.data<float>();
-  float* Ydata = Y->mutable_data<float>();
-  math::Set<float, CPUContext>(Y->size(), 0, Ydata, &context_);
-  // The main loop
-  int pooled_height = Y->dim32(1);
-  int pooled_width = Y->dim32(2);
-  for (int n = 0; n < X.dim32(0); ++n) {
-    for (int ph = 0; ph < pooled_height; ++ph) {
-      for (int pw = 0; pw < pooled_width; ++pw) {
-        int hstart = ph * stride_h() - pad_t();
-        int wstart = pw * stride_w() - pad_l();
-        int hend = min(hstart + kernel_h(), height);
-        int wend = min(wstart + kernel_w(), width);
-        hstart = max(hstart, 0);
-        wstart = max(wstart, 0);
-        const int pool_index = (ph * pooled_width + pw) * channels;
-        for (int h = hstart; h < hend; ++h) {
-          for (int w = wstart; w < wend; ++w) {
-            const int input_index = (h * width + w) * channels;
-            for (int c = 0; c < channels; ++c) {
-              Ydata[pool_index + c] += Xdata[input_index + c];
-            }
-          }
-        }
-        float scale = 1. / (hend - hstart) / (wend - wstart);
+  switch (kernel_.size()) {
+    case 1:
+      for (int n = 0; n < X.dim32(0); ++n) {
         for (int c = 0; c < channels; ++c) {
-          Ydata[pool_index + c] *= scale;
+          for (int ph = 0; ph < pooled_height; ++ph) {
+            int hstart = ph * stride_h() - pad_t();
+            int hend = min(hstart + kernel_h(), height);
+            hstart = max(hstart, 0);
+            for (int h = hstart; h < hend; ++h) {
+              PoolType::process(Xdata[h], Ydata[ph]);
+            }
+            PoolType::finalize(hend - hstart, Ydata[ph]);
+          }
+          // Do offset.
+          Xdata += height;
+          Ydata += pooled_height;
         }
       }
-    }
-    // Do offset.
-    Xdata += X.size() / X.dim32(0);
-    Ydata += Y->size() / Y->dim32(0);
-  }
-  return true;
-}
-
-template <>
-bool PoolOp<float, CPUContext, MaxPool>::RunOnDeviceWithOrderNCHW() {
-  auto& X = Input(0);
-  auto* Y = Output(0);
-  ConvPoolOpBase::SetOutputSize(X, Y, X.dim32(1));
-
-  const float* Xdata = X.data<float>();
-  float* Ydata = Y->mutable_data<float>();
-  math::Set<float, CPUContext>(
-      Y->size(), std::numeric_limits<float>::lowest(), Ydata, &context_);
-  // The main loop
-  int channels = X.dim32(1);
-  int height = X.dim32(2);
-  int width = X.dim32(3);
-  int pooled_height = Y->dim32(2);
-  int pooled_width = Y->dim32(3);
-  for (int n = 0; n < X.dim32(0); ++n) {
-    for (int c = 0; c < channels; ++c) {
-      for (int ph = 0; ph < pooled_height; ++ph) {
-        for (int pw = 0; pw < pooled_width; ++pw) {
-          int hstart = ph * stride_h() - pad_t();
-          int wstart = pw * stride_w() - pad_l();
-          int hend = min(hstart + kernel_h(), height);
-          int wend = min(wstart + kernel_w(), width);
-          hstart = max(hstart, 0);
-          wstart = max(wstart, 0);
-          const int pool_index = ph * pooled_width + pw;
-          for (int h = hstart; h < hend; ++h) {
-            for (int w = wstart; w < wend; ++w) {
-              const int input_index = h * width + w;
-              if (Xdata[input_index] > Ydata[pool_index]) {
-                Ydata[pool_index] = Xdata[input_index];
+      break;
+    case 2:
+      for (int n = 0; n < X.dim32(0); ++n) {
+        for (int c = 0; c < channels; ++c) {
+          for (int ph = 0; ph < pooled_height; ++ph) {
+            int hstart = ph * stride_h() - pad_t();
+            int hend = min(hstart + kernel_h(), height);
+            hstart = max(hstart, 0);
+            for (int pw = 0; pw < pooled_width; ++pw) {
+              int wstart = pw * stride_w() - pad_l();
+              int wend = min(wstart + kernel_w(), width);
+              wstart = max(wstart, 0);
+              const int pool_index = ph * pooled_width + pw;
+              for (int h = hstart; h < hend; ++h) {
+                for (int w = wstart; w < wend; ++w) {
+                  const int input_index = h * width + w;
+                  PoolType::process(Xdata[input_index], Ydata[pool_index]);
+                }
+              }
+              PoolType::finalize(
+                  (hend - hstart) * (wend - wstart), Ydata[pool_index]);
+            }
+          }
+          // Do offset.
+          Xdata += height * width;
+          Ydata += pooled_height * pooled_width;
+        }
+      }
+      break;
+    case 3:
+      for (int n = 0; n < X.dim32(0); ++n) {
+        for (int c = 0; c < channels; ++c) {
+          for (int ph = 0; ph < pooled_height; ++ph) {
+            int hstart = ph * stride_h() - pad_t();
+            int hend = min(hstart + kernel_h(), height);
+            hstart = max(hstart, 0);
+            for (int pw = 0; pw < pooled_width; ++pw) {
+              int wstart = pw * stride_w() - pad_l();
+              int wend = min(wstart + kernel_w(), width);
+              wstart = max(wstart, 0);
+              for (int pd = 0; pd < pooled_depth; ++pd) {
+                int dstart = pd * stride_[2] - pads_[2];
+                int dend = min(dstart + kernel_[2], depth);
+                dstart = max(dstart, 0);
+                const int pool_index =
+                    ph * pooled_width * pooled_depth + pw * pooled_depth + pd;
+                for (int h = hstart; h < hend; ++h) {
+                  for (int w = wstart; w < wend; ++w) {
+                    for (int d = dstart; d < dend; ++d) {
+                      const int input_index = h * width * depth + w * depth + d;
+                      PoolType::process(Xdata[input_index], Ydata[pool_index]);
+                    }
+                  }
+                }
+                PoolType::finalize(
+                    (hend - hstart) * (wend - wstart) * (dend - dstart),
+                    Ydata[pool_index]);
               }
             }
           }
+          // Do offset.
+          Xdata += height * width * depth;
+          Ydata += pooled_height * pooled_width * pooled_depth;
         }
       }
-      // Do offset.
-      Xdata += height * width;
-      Ydata += pooled_height * pooled_width;
-    }
+      break;
+    default:
+      CAFFE_THROW("Unsupported pooling size : ", kernel_.size());
+      return false;
   }
   return true;
 }
 
-template <>
-bool PoolOp<float, CPUContext, MaxPool>::RunOnDeviceWithOrderNHWC() {
+template <typename T, class Context, typename PoolType>
+bool PoolOp<T, Context, PoolType>::RunOnDeviceWithOrderNHWC() {
   auto& X = Input(0);
   auto* Y = Output(0);
   int height = X.dim32(1);
-  int width = X.dim32(2);
-  int channels = X.dim32(3);
-  ConvPoolOpBase::SetOutputSize(X, Y, channels);
+  int width = kernel_.size() > 1 ? X.dim32(2) : 1;
+  int depth = kernel_.size() > 2 ? X.dim32(3) : 1;
+  int channels = X.dim32(X.ndim() - 1);
+  ConvPoolOpBase<Context>::SetOutputSize(X, Y, channels);
 
   EigenMatrixMap<float> Ymat(
-      Y->mutable_data<float>(), channels, Y->size() / channels);
+      Y->template mutable_data<float>(), channels, Y->size() / channels);
   ConstEigenMatrixMap<float> Xmat(
-      X.data<float>(), channels, X.size() / channels);
+      X.template data<float>(), channels, X.size() / channels);
   int pooled_height = Y->dim32(1);
-  int pooled_width = Y->dim32(2);
-
+  int pooled_width = kernel_.size() > 1 ? Y->dim32(2) : 1;
+  int pooled_depth = kernel_.size() > 2 ? Y->dim32(3) : 1;
   // The main loop
-  for (int n = 0; n < X.dim32(0); ++n) {
-    for (int ph = 0; ph < pooled_height; ++ph) {
-      int hstart = ph * stride_h() - pad_t();
-      int hend = min(hstart + kernel_h(), height);
-      hstart = max(hstart, 0);
-      for (int pw = 0; pw < pooled_width; ++pw) {
-        int wstart = pw * stride_w() - pad_l();
-        int wend = min(wstart + kernel_w(), width);
-        wstart = max(wstart, 0);
-        // compute max in range X[n, hstart:hend, wstart:wend, :]
-        auto Y_col = Ymat.col((n * pooled_height + ph) * pooled_width + pw);
-        Y_col.setConstant(std::numeric_limits<float>::lowest());
-        for (int h = hstart; h < hend; ++h) {
-          for (int w = wstart; w < wend; ++w) {
-            Y_col = Y_col.cwiseMax(Xmat.col((n * height + h) * width + w));
+  switch (kernel_.size()) {
+    case 1:
+      for (int n = 0; n < X.dim32(0); ++n) {
+        for (int ph = 0; ph < pooled_height; ++ph) {
+          int hstart = ph * stride_h() - pad_t();
+          int hend = min(hstart + kernel_h(), height);
+          hstart = max(hstart, 0);
+          const int y_col = n * pooled_height + ph;
+          Ymat.col(y_col).setConstant(PoolType::initialize());
+          for (int h = hstart; h < hend; ++h) {
+            const int x_col = n * height + h;
+            PoolType::process(x_col, y_col, Xmat, Ymat);
+          }
+          PoolType::finalize((hend - hstart), y_col, Ymat);
+        }
+      }
+      break;
+    case 2:
+      for (int n = 0; n < X.dim32(0); ++n) {
+        for (int ph = 0; ph < pooled_height; ++ph) {
+          int hstart = ph * stride_h() - pad_t();
+          int hend = min(hstart + kernel_h(), height);
+          hstart = max(hstart, 0);
+          for (int pw = 0; pw < pooled_width; ++pw) {
+            int wstart = pw * stride_w() - pad_l();
+            int wend = min(wstart + kernel_w(), width);
+            wstart = max(wstart, 0);
+            const int y_col = (n * pooled_height + ph) * pooled_width + pw;
+            Ymat.col(y_col).setConstant(PoolType::initialize());
+            for (int h = hstart; h < hend; ++h) {
+              for (int w = wstart; w < wend; ++w) {
+                const int x_col = (n * height + h) * width + w;
+                PoolType::process(x_col, y_col, Xmat, Ymat);
+              }
+            }
+            PoolType::finalize((hend - hstart) * (wend - wstart), y_col, Ymat);
           }
         }
       }
+      break;
+    case 3:
+      for (int n = 0; n < X.dim32(0); ++n) {
+        for (int ph = 0; ph < pooled_height; ++ph) {
+          int hstart = ph * stride_h() - pad_t();
+          int hend = min(hstart + kernel_h(), height);
+          hstart = max(hstart, 0);
+          for (int pw = 0; pw < pooled_width; ++pw) {
+            int wstart = pw * stride_w() - pad_l();
+            int wend = min(wstart + kernel_w(), width);
+            wstart = max(wstart, 0);
+            for (int pd = 0; pd < pooled_depth; ++pd) {
+              int dstart = pd * stride_[2] - pads_[2];
+              int dend = min(dstart + kernel_[2], depth);
+              dstart = max(dstart, 0);
+              const int y_col = ((n * pooled_height + ph) * pooled_width + pw) *
+                      pooled_depth +
+                  pd;
+              Ymat.col(y_col).setConstant(PoolType::initialize());
+              for (int h = hstart; h < hend; ++h) {
+                for (int w = wstart; w < wend; ++w) {
+                  for (int d = dstart; d < dend; ++d) {
+                    const int x_col =
+                        ((n * height + h) * width + w) * depth + d;
+                    PoolType::process(x_col, y_col, Xmat, Ymat);
+                  }
+                }
+              }
+              PoolType::finalize(
+                  (hend - hstart) * (wend - wstart) * (dend - dstart),
+                  y_col,
+                  Ymat);
+            }
+          }
+        }
     }
+    break;
+    default:
+      CAFFE_THROW("Unsupported pooling size : ", kernel_.size());
+      return false;
   }
   return true;
 }
 
 namespace {
-REGISTER_CPU_OPERATOR(AveragePool, PoolOp<float, CPUContext, AveragePool>);
+REGISTER_CPU_OPERATOR(
+    AveragePool,
+    PoolOp<float, CPUContext, AveragePool<float>>);
 
 OPERATOR_SCHEMA(AveragePool)
   .NumInputs(1)
@@ -387,8 +485,7 @@ data into the output blob Y for further processing.
   "tensor. Dimensions will vary based on various kernel, stride, and pad "
   "sizes.");
 
-
-REGISTER_CPU_OPERATOR(MaxPool, PoolOp<float, CPUContext, MaxPool>);
+REGISTER_CPU_OPERATOR(MaxPool, PoolOp<float, CPUContext, MaxPool<float>>);
 
 OPERATOR_SCHEMA(MaxPool)
   .NumInputs(1)
