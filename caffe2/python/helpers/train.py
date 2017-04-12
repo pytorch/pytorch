8de1ce57d27ce2bd40a1917a36a0bc74de848cab
Yiming Wu <wyiming@fb.com>
2017-04-11 23:00:27 -0700

    Add Algebra and train helpers and proxy them to CNNMH
    Summary: Add Algebra and train helpers and proxy them to CNNMH
    
    Reviewed By: salexspb
    
    Differential Revision: D4855040
    
    fbshipit-source-id: d948ea913f674a6e47c4b72629a2d33253cb3130

---
+++ caffe2/python/helpers/train.py
@@ -0,0 +1,43 @@
+## @package train
+# Module caffe2.python.helpers.train
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from caffe2.python import core, scope
+from caffe2.proto import caffe2_pb2
+
+__all__ = [
+    'Iter',
+    'Accuracy',
+]
+
+
+def Iter(model, blob_out, **kwargs):
+    if 'device_option' in kwargs:
+        del kwargs['device_option']
+    model.param_init_net.ConstantFill(
+        [], blob_out, shape=[1], value=0, dtype=core.DataType.INT64,
+        device_option=core.DeviceOption(caffe2_pb2.CPU, 0),
+        **kwargs)
+    return model.net.Iter(blob_out, blob_out, **kwargs)
+
+
+def Accuracy(model, blob_in, blob_out, **kwargs):
+    dev = kwargs['device_option'] if 'device_option' in kwargs \
+        else scope.CurrentDeviceScope()
+    is_cpu = dev is None or dev.device_type == caffe2_pb2.CPU
+
+    # We support top_k > 1 only on CPU
+    if not is_cpu and 'top_k' in kwargs and kwargs['top_k'] > 1:
+        pred_host = model.net.CopyGPUToCPU(blob_in[0], blob_in[0] + "_host")
+        label_host = model.net.CopyGPUToCPU(blob_in[1], blob_in[1] + "_host")
+
+        # Now use the Host version of the accuracy op
+        model.net.Accuracy([pred_host, label_host],
+                           blob_out,
+                           device_option=core.DeviceOption(caffe2_pb2.CPU, 0),
+                           **kwargs)
+    else:
+        model.net.Accuracy(blob_in, blob_out)
