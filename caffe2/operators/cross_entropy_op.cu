4db7bec6867d1bb6dba62cb559738b4180d769ae
Aapo Kyrola <akyrola@fb.com>
2017-04-14 15:57:52 -0700

    CUDA version of SigmoidCrossEntropyWithLogits
    Summary: CUDA versions of SigmoidCrossEntropyWithLogits/Gradient.
    
    Reviewed By: jay-mahadeokar
    
    Differential Revision: D4891254
    
    fbshipit-source-id: cabad908026e30d9a0721cad092ba948659ab917

--- caffe2/operators/cross_entropy_op.cu
+++ caffe2/operators/cross_entropy_op.cu
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cub/block/block_reduce.cuh>
 
 #include "caffe2/core/context_gpu.h"
 #include "caffe2/operators/cross_entropy_op.h"
@@ -115,10 +116,122 @@ bool MakeTwoClassGradientOp<float, CUDAContext>::RunOnDevice() {
 }
 
 namespace {
+
+__device__ float sigmoid_xent_forward(float lgt, float tgt) {
+  return lgt * (tgt - (lgt >= 0)) - log(1 + exp(lgt - 2 * lgt * (lgt >= 0)));
+}
+
+__device__ float sigmoid_xent_backward(float lgt, float tgt) {
+  return tgt - 1. / (1. + exp(-lgt));
+}
+
+__global__ void SigmoidCrossEntropyWithLogitsKernel(
+    const int outer_size,
+    const int inner_size,
+    const float* logits_ptr,
+    const float* targets_ptr,
+    float* out_ptr) {
+  int i = blockIdx.x;
+  int last_idx = (i + 1) * inner_size;
+  float value = 0;
+  for (int in_idx = i * inner_size + threadIdx.x; in_idx < last_idx;
+       in_idx += blockDim.x) {
+    value += sigmoid_xent_forward(logits_ptr[in_idx], targets_ptr[in_idx]);
+  }
+
+  typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+  float sum = BlockReduce(temp_storage).Sum(value);
+  if (threadIdx.x == 0) {
+    out_ptr[i] = -sum / inner_size;
+  }
+}
+
+__global__ void SigmoidCrossEntropyGradientWithLogitsKernel(
+    const int outer_size,
+    const int inner_size,
+    const float* g_ptr,
+    const float* logits_ptr,
+    const float* targets_ptr,
+    float* out_ptr) {
+  CUDA_1D_KERNEL_LOOP(in_idx, outer_size * inner_size) {
+    int i = in_idx / inner_size;
+    auto g_factor = -g_ptr[i] / inner_size;
+    out_ptr[in_idx] = g_factor *
+        sigmoid_xent_backward(logits_ptr[in_idx], targets_ptr[in_idx]);
+  }
+}
+} // namespace
+
+template <>
+bool SigmoidCrossEntropyWithLogitsOp<float, CUDAContext>::RunOnDevice() {
+  auto& logits = Input(0);
+  auto& targets = Input(1);
+  CAFFE_ENFORCE(logits.dims() == targets.dims());
+  const auto inner_size = logits.ndim() > 0 ? logits.dims().back() : 1;
+  const auto outer_size = logits.size() / inner_size;
+
+  auto* out = Output(0);
+  if (logits.ndim() == 0) {
+    out->Resize(std::vector<TIndex>{});
+  } else {
+    std::vector<TIndex> dims(logits.dims().begin(), logits.dims().end() - 1);
+    out->Resize(dims);
+  }
+  auto* out_ptr = out->mutable_data<float>();
+
+  auto* logits_ptr = logits.data<float>();
+  auto* targets_ptr = targets.data<float>();
+
+  SigmoidCrossEntropyWithLogitsKernel<<<
+      outer_size,
+      CAFFE_CUDA_NUM_THREADS,
+      0,
+      context_.cuda_stream()>>>(
+      outer_size, inner_size, logits_ptr, targets_ptr, out_ptr);
+  return true;
+}
+
+template <>
+bool SigmoidCrossEntropyWithLogitsGradientOp<float, CUDAContext>::
+    RunOnDevice() {
+  auto& g = Input(0);
+  auto& logits = Input(1);
+  auto& targets = Input(2);
+  CAFFE_ENFORCE(logits.dims() == targets.dims());
+  const auto inner_size = logits.ndim() > 0 ? logits.dims().back() : 1;
+  const auto outer_size = logits.size() / inner_size;
+  CAFFE_ENFORCE(g.size() == outer_size);
+
+  auto* out = Output(0);
+  out->ResizeLike(logits);
+  auto* out_ptr = out->mutable_data<float>();
+
+  auto* logits_ptr = logits.data<float>();
+  auto* targets_ptr = targets.data<float>();
+  auto* g_ptr = g.data<float>();
+
+  SigmoidCrossEntropyGradientWithLogitsKernel<<<
+      CAFFE_GET_BLOCKS(outer_size * inner_size),
+      CAFFE_CUDA_NUM_THREADS,
+      0,
+      context_.cuda_stream()>>>(
+      outer_size, inner_size, g_ptr, logits_ptr, targets_ptr, out_ptr);
+  return true;
+}
+
+namespace {
 REGISTER_CUDA_OPERATOR(LabelCrossEntropy,
                        LabelCrossEntropyOp<float, CUDAContext>);
 REGISTER_CUDA_OPERATOR(LabelCrossEntropyGradient,
                        LabelCrossEntropyGradientOp<float, CUDAContext>);
+
+REGISTER_CUDA_OPERATOR(
+    SigmoidCrossEntropyWithLogits,
+    SigmoidCrossEntropyWithLogitsOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    SigmoidCrossEntropyWithLogitsGradient,
+    SigmoidCrossEntropyWithLogitsGradientOp<float, CUDAContext>);
 
 REGISTER_CUDA_OPERATOR(MakeTwoClass,
                        MakeTwoClassOp<float, CUDAContext>);
