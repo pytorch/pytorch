2043b3c1147bfc17b4427faf7c31662508d5cbd5
Yiming Wu <wyiming@fb.com>
2017-04-17 14:58:37 -0700

    train and algebra helpers
    Summary: Adding train and algebra helpers
    
    Reviewed By: salexspb
    
    Differential Revision: D4884951
    
    fbshipit-source-id: 7a18eb986a7356977a6c3d7a62a996ddce0c793e

---
+++ caffe2/python/helpers/algebra.py
@@ -0,0 +1,18 @@
+## @package algebra
+# Module caffe2.python.helpers.algebra
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def Transpose(model, blob_in, blob_out, use_cudnn=False, **kwargs):
+    """Transpose."""
+    if use_cudnn:
+        kwargs['engine'] = 'CUDNN'
+    return model.net.Transpose(blob_in, blob_out, **kwargs)
+
+
+def Sum(model, blob_in, blob_out, **kwargs):
+    """Sum"""
+    return model.net.Sum(blob_in, blob_out, **kwargs)
