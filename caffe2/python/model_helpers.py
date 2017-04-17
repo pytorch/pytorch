277b4eca9737af940a077e5033dffd6039783ee3
Yiming Wu <wyiming@fb.com>
2017-04-17 14:58:36 -0700

    array helpers (concat)
    Summary: Adding array helpers
    
    Reviewed By: salexspb
    
    Differential Revision: D4884933
    
    fbshipit-source-id: 2ec3dd37b243c8c717e299876eef7650a08d3f2b

--- caffe2/python/model_helpers.py
+++ caffe2/python/model_helpers.py
@@ -14,3 +14,4 @@ from caffe2.python.helpers.fc import *
 from caffe2.python.helpers.pooling import *
 from caffe2.python.helpers.normalization import *
 from caffe2.python.helpers.nonlinearity import *
+from caffe2.python.helpers.array_helpers import *
