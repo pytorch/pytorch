5311fd3d6ab2364c916e490b3093f8185304124d
Simon Layton <slayton58@gmail.com>
2017-04-17 11:37:21 -0700

    Conv no dx
    Summary:
    Based on a discussion with Yangqing, optionally disables the calculation of dX for a convolution op (i.e. conv1 in Alexnet), where the data gradient is not needed.
    Closes https://github.com/caffe2/caffe2/pull/242
    
    Differential Revision: D4844013
    
    Pulled By: bwasti
    
    fbshipit-source-id: 202d2410ed6c66671e83e8e49a1383883c6ab29e

--- caffe2/operators/conv_gradient_op.cc
+++ caffe2/operators/conv_gradient_op.cc
@@ -6,24 +6,46 @@ namespace caffe2 {
 namespace {
 
 REGISTER_CPU_OPERATOR(ConvGradient, ConvGradientOp<float, CPUContext>);
-OPERATOR_SCHEMA(ConvGradient).NumInputs(2, 3).NumOutputs(2, 3);
+OPERATOR_SCHEMA(ConvGradient).NumInputs(2, 3).NumOutputs(1, 3);
 class GetConvGradient : public GradientMakerBase {
   using GradientMakerBase::GradientMakerBase;
   vector<OperatorDef> GetGradientDefs() override {
     CAFFE_ENFORCE(def_.input_size() == 3 || def_.input_size() == 2);
+
+    ArgumentHelper argsHelper(def_);
+
+    auto compute_dX = !argsHelper.GetSingleArgument<bool>("no_gradient_to_input", 0);
+
     if (def_.input_size() == 3) {
-      return SingleGradientDef(
-          "ConvGradient",
-          "",
-          vector<string>{I(0), I(1), GO(0)},
-          vector<string>{GI(1), GI(2), GI(0)});
+      if (compute_dX) {
+        return SingleGradientDef(
+            "ConvGradient",
+            "",
+            vector<string>{I(0), I(1), GO(0)},
+            vector<string>{GI(1), GI(2), GI(0)});
+      } else {
+        return SingleGradientDef(
+            "ConvGradient",
+            "",
+            vector<string>{I(0), I(1), GO(0)},
+            vector<string>{GI(1), GI(2)});
+      }
     } else {
-      return SingleGradientDef(
-          "ConvGradient",
-          "",
-          vector<string>{I(0), I(1), GO(0)},
-          vector<string>{GI(1), GI(0)},
-          vector<Argument>{MakeArgument<int>("no_bias", 1)});
+      if (compute_dX) {
+        return SingleGradientDef(
+            "ConvGradient",
+            "",
+            vector<string>{I(0), I(1), GO(0)},
+            vector<string>{GI(1), GI(0)},
+            vector<Argument>{MakeArgument<int>("no_bias", 1)});
+      } else {
+        return SingleGradientDef(
+            "ConvGradient",
+            "",
+            vector<string>{I(0), I(1), GO(0)},
+            vector<string>{GI(1)},
+            vector<Argument>{MakeArgument<int>("no_bias", 1)});
+      }
     }
   }
 };
