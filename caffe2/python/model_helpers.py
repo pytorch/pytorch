9c76bfa592056f9aa7d146011ef0b41300c48de2
Yiming Wu <wyiming@fb.com>
2017-04-21 11:16:14 -0700

    arg_scope for model_helper
    Summary:
    arg_scope module for model_helpers.
    
    Some coding example with it:
    
      with model_helpers.arg_scope([model_helpers.FC], kwargs):
                  model_helpers.FC(model, "x", "out_1", n, n)
    
      with model_helpers.arg_scope([myhelper], n=-3):
                  with model_helpers.arg_scope([myhelper], n=-2):
                      with model_helpers.arg_scope([myhelper], n=n):
                          res = model_helpers.myhelper(None)
    
      with model_helpers.arg_scope([myhelper], n=-3), \
              model_helpers.arg_scope([myhelper], n=-2), \
              model_helpers.arg_scope([myhelper], n=n):
                      res = model_helpers.myhelper(None)
    
    Reviewed By: salexspb
    
    Differential Revision: D4837180
    
    fbshipit-source-id: 2cbd81681779d6cd1e61ee189edcc1cf3bb07d15

--- caffe2/python/model_helpers.py
+++ caffe2/python/model_helpers.py
@@ -5,9 +5,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-#  from caffe2.python.helpers.arg_scope import *
+import sys
+import copy
+
 # flake8: noqa
 from caffe2.python.helpers.dropout import *
+from caffe2.python.helpers.arg_scope import *
 from caffe2.python.helpers.fc import *
 from caffe2.python.helpers.pooling import *
 from caffe2.python.helpers.normalization import *
@@ -16,3 +19,73 @@ from caffe2.python.helpers.array_helpers import *
 from caffe2.python.helpers.algebra import *
 from caffe2.python.helpers.train import *
 from caffe2.python.helpers.conv import *
+
+
+class HelperWrapper(object):
+    _registry = {
+        'arg_scope': arg_scope,
+        'FC': FC,
+        'PackedFC': PackedFC,
+        'FC_Decomp': FC_Decomp,
+        'FC_Sparse': FC_Sparse,
+        'FC_Prune': FC_Prune,
+        'Dropout': Dropout,
+        'MaxPool': MaxPool,
+        'AveragePool': AveragePool,
+        'LRN': LRN,
+        'Softmax': Softmax,
+        'InstanceNorm': InstanceNorm,
+        'SpatialBN': SpatialBN,
+        'Relu': Relu,
+        'PRelu': PRelu,
+        'Concat': Concat,
+        'DepthConcat': DepthConcat,
+        'Sum': Sum,
+        'Transpose': Transpose,
+        'Iter': Iter,
+        'Accuracy': Accuracy,
+        'Conv': Conv,
+        'ConvNd': ConvNd,
+        'ConvTranspose': ConvTranspose,
+        'GroupConv': GroupConv,
+        'GroupConv_Deprecated': GroupConv_Deprecated,
+    }
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __getattr__(self, helper_name):
+        if helper_name not in self._registry:
+            raise AttributeError(
+                "Helper function {} not "
+                "registered.".format(helper_name)
+            )
+
+        def scope_wrapper(*args, **kwargs):
+            cur_scope = get_current_scope()
+            new_kwargs = copy.deepcopy(cur_scope.get(helper_name, {}))
+            new_kwargs.update(kwargs)
+            return self._registry[helper_name](*args, **new_kwargs)
+
+        scope_wrapper.__name__ = helper_name
+        return scope_wrapper
+
+    def Register(self, helper):
+        name = helper.__name__
+        if name in self._registry:
+            raise AttributeError(
+                "Helper {} already exists. Please change your "
+                "helper name.".format(name)
+            )
+        self._registry[name] = helper
+
+    def has_helper(self, helper_or_helper_name):
+        helper_name = (
+            helper_or_helper_name
+            if isinstance(helper_or_helper_name, basestring) else
+            helper_or_helper_name.__name__
+        )
+        return helper_name in self._registry
+
+
+sys.modules[__name__] = HelperWrapper(sys.modules[__name__])
