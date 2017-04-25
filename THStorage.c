928f6516c16ff91c0a789d0a653551041d1bafd0
Trevor Killeen <killeentm@gmail.com>
2017-04-25 06:12:31 -0700

    implement expand/expandAs in CPU/GPU code

--- THStorage.c
+++ THStorage.c
@@ -65,3 +65,40 @@ TH_API THLongStorage *THLongStorage_newInferSize(THLongStorage *size, ptrdiff_t 
   }
   return copy;
 }
+
+TH_API void THLongStorage_calculateExpandGeometry(long *tensorSizes, long *tensorStrides, long tensorDim, THLongStorage *sizes, long **esz, long **est) {
+  ptrdiff_t ndim = THLongStorage_size(sizes);
+  long numUnsqueezed = ndim - tensorDim;
+
+  long *expandedSizes = THAlloc(sizeof(long)*ndim);
+  long *expandedStrides = THAlloc(sizeof(long)*ndim);
+
+  for (long i = numUnsqueezed; i < ndim; ++i) {
+    expandedSizes[i] = tensorSizes[i - numUnsqueezed];
+    expandedStrides[i] = tensorStrides[i - numUnsqueezed];
+  }
+
+  for (long i = numUnsqueezed - 1; i > -1; --i) {
+    expandedSizes[i] = 1;
+    expandedStrides[i] = expandedSizes[i+1] * expandedStrides[i+1];
+  }
+
+  // create a new geometry for the tensor
+  for (long i = 0; i < ndim; ++i) {
+    long size = expandedSizes[i];
+    long targetSize = THLongStorage_data(sizes)[i];
+    if (size == 1) {
+      if (targetSize != 1) {
+        expandedSizes[i] = targetSize;
+        expandedStrides[i] = 0;
+      }
+    } else if (size != targetSize) {
+      THFree(expandedSizes);
+      THFree(expandedStrides);
+      THError("The expanded size of the tensor (%d) must match the existing size (%d) at \
+              non-singleton dimension %ld.", targetSize, size, i);
+    }
+  }
+  *esz = expandedSizes;
+  *est = expandedStrides;
+}
