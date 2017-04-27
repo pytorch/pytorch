2a371c8695dc587ff9ae679d947d344f5995aa5f
Xiaolong Wang <xlwang@fb.com>
2017-04-27 01:02:43 -0700

    Allow Load operator to load into overriden names
    Summary:
    A new argument `blob_name_overrides` is added, which is to specify the
    destination of loaded blob (in order to allow they have different names than
    what are in the saved file/db).
    
    This will be used for parameter initailization by pretrained model
    in Dper 2. When loading a blob, we need to avoid name collision by assigning the
    loaded blob with a new (temp) name.
    
    Reviewed By: xianjiec
    
    Differential Revision: D4952485
    
    fbshipit-source-id: 4ce79bf40223314bb94981c22cbe537ae3f3d27c

--- caffe2/operators/load_save_op.cc
+++ caffe2/operators/load_save_op.cc
@@ -67,7 +67,12 @@ DBReader to load from, and we ignore the db and db_type arguments.
     .Arg(
         "allow_incomplete",
         "(bool, default false) if true, will allow not loading all the output "
-        "blobs specified in the outputs");
+        "blobs specified in the outputs")
+    .Arg(
+        "source_blob_names",
+        "(list of strings) if set, used instead of output "
+        "blob names, to specify which blobs in the db shall be loaded. Must be "
+        "the same length as number of output blobs.");
 
 OPERATOR_SCHEMA(Save)
     .NumInputs(1, INT_MAX)
