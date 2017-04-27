9687103db8f87a779ffae3e0f05a4ff053f981d4
Edward Z. Yang <ezyang@fb.com>
2017-04-27 15:31:56 -0400

    More sparse tests (failing)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/test_optim.py
+++ test/test_optim.py
@@ -98,6 +98,7 @@ class TestOptim(TestCase):
             self.assertEqual(params.data, params_c.data)
 
         optimizer.flush()
+        optimizer.flush()
 
         self.assertLessEqual(params.data.dist(solution), initial_dist)
 
@@ -226,6 +227,9 @@ class TestOptim(TestCase):
         self._test_rosenbrock_sparse(
             lambda params: optim.SGD(params, lr=1e-3, weight_decay=1e-4)
         )
+        self._test_rosenbrock_sparse(
+            lambda params: optim.SGD(params, lr=1e-3, momentum=0.9)
+        )
 
     def test_adam(self):
         self._test_rosenbrock(
@@ -243,6 +247,11 @@ class TestOptim(TestCase):
             lambda weight, bias: optim.Adam(
                 self._build_params_dict(weight, bias, lr=1e-2),
                 lr=1e-3)
+        )
+
+    def test_adam_sparse(self):
+        self._test_rosenbrock_sparse(
+            lambda params: optim.Adam(params, lr=1e-2)
         )
 
     def test_adadelta(self):
@@ -264,6 +273,11 @@ class TestOptim(TestCase):
         self._test_basic_cases(
             lambda weight, bias: optim.Adadelta(
                 self._build_params_dict(weight, bias, rho=0.95))
+        )
+
+    def test_adadelta_sparse(self):
+        self._test_rosenbrock_sparse(
+            lambda params: optim.Adadelta(params)
         )
 
     def test_adagrad(self):
@@ -291,6 +305,9 @@ class TestOptim(TestCase):
     def test_adagrad_sparse(self):
         self._test_rosenbrock_sparse(
             lambda params: optim.Adagrad(params, lr=1e-1)
+        )
+        self._test_rosenbrock_sparse(
+            lambda params: optim.Adagrad(params, lr=1e-1, weight_decay=1e-2)
         )
 
     def test_adamax(self):
@@ -379,6 +396,11 @@ class TestOptim(TestCase):
             lambda weight, bias: optim.Rprop(
                 self._build_params_dict(weight, bias, lr=1e-2),
                 lr=1e-3)
+        )
+
+    def test_rprop_sparse(self):
+        self._test_rosenbrock_sparse(
+            lambda params: optim.Rprop(params, lr=1e-3)
         )
 
     def test_lbfgs(self):
