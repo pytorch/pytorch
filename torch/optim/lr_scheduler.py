f09a11a440744ff77d50db0bae6e98a56b263dde
Jiaming Liu <memono.liu@gmail.com>
2017-04-27 17:31:33 -0700

    Relative Threshold

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -98,8 +98,14 @@ class ReduceLROnPlateau(object):
             monitored has stopped decreasing; in `max`
             mode it will be reduced when the quantity
             monitored has stopped increasing.
-        epsilon: threshold for measuring the new optimum,
+        threshold: threshold for measuring the new optimum,
             to only focus on significant changes.
+        threshold_mode: one of {rel, abs}. In 'rel' mode,
+            dynamic_threshold = best * ( 1 + threshold )
+            in 'max' mode or best * ( 1 - threshold ) in
+            'min' mode. In 'abs' mode, dynamic_threshold =
+            best + threshold in 'max' mode or
+            best - threshold in 'min' mode.
         cooldown: number of epochs to wait before resuming
             normal operation after lr has been reduced.
         min_lr: lower bound on the learning rate.
@@ -116,7 +122,7 @@ class ReduceLROnPlateau(object):
     """
 
     def __init__(self, optimizer, mode='min', factor=0.1, patience=10,
-                 verbose=0, epsilon=1e-4, cooldown=0, min_lr=0):
+                 verbose=0, threshold=1e-4, threshold_mode='rel', cooldown=0, min_lr=0):
 
         if factor >= 1.0:
             raise ValueError('ReduceLROnPlateau '
@@ -127,7 +133,8 @@ class ReduceLROnPlateau(object):
         self.verbose = verbose
         self.cooldown = cooldown
         self.cooldown_counter = 0  # Cooldown counter.
-        self.monitor_op = _MonitorOp(mode=mode, epsilon=epsilon)
+        self.monitor_op = _MonitorOp(mode=mode, threshold=threshold,
+                                     threshold_mode=threshold_mode)
         self.wait = 0
         self.best = 0
         assert isinstance(optimizer, Optimizer)
@@ -174,15 +181,24 @@ class ReduceLROnPlateau(object):
 
 
 class _MonitorOp(object):
-
-    def __init__(self, mode, epsilon):
+    def __init__(self, mode, threshold, threshold_mode):
         if mode not in ['min', 'max']:
             raise RuntimeError('Learning Rate Plateau Reducing mode %s is unknown!')
-        if mode == 'min':
-            self.monitor_op = lambda a, b: np.less(a, b - epsilon)
+        if threshold_mode not in ['rel', 'abs']:
+            raise RuntimeError('Learning Rate Plateau threshold mode %s is unknown!')
+        if mode == 'min' and threshold_mode == 'rel':
+            rel_epsilon = 1. - threshold
+            self.monitor_op = lambda a, b: np.less(a, b * rel_epsilon)
             self.worse = np.Inf
-        else:
-            self.monitor_op = lambda a, b: np.greater(a, b + epsilon)
+        elif mode == 'min' and threshold_mode == 'abs':
+            self.monitor_op = lambda a, b: np.less(a, b - threshold)
+            self.worse = np.Inf
+        elif mode == 'max' and threshold_mode == 'rel':
+            rel_epsilon = threshold + 1.
+            self.monitor_op = lambda a, b: np.greater(a, b * rel_epsilon)
+            self.worse = -np.Inf
+        else:  # mode == 'max' and epsilon_mode == 'abs':
+            self.monitor_op = lambda a, b: np.greater(a, b + threshold)
             self.worse = -np.Inf
 
     def __call__(self, a, b):
