a809e1adfa42a8e4f0622e6a4a9507ba2d688cbe
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-05-05 17:39:19 +0300

    F.normalize default args

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -744,14 +744,17 @@ def triplet_margin_loss(anchor, positive, negative, margin=1.0, p=2, eps=1e-6, s
     return loss
 
 
-def normalize(input, p, dim, eps=1e-12):
-    r"""Performs l_p normalization of inputs over specified dimension.
+def normalize(input, p=2, dim=-1, eps=1e-12):
+    r"""Performs :math:`L_p` normalization of inputs over specified dimension.
 
     Does:
 
     .. math::
         v = \frac{v}{\max(\lVert v \rVert_p, \epsilon)}
 
-    for each subtensor v over dimension dim of input.
+    for each subtensor v over dimension dim of input. Each subtensor is flattened into a vector,
+    i.e. :math:`\lVert v \rVert_p` is not a matrix norm.
+
+    With default arguments normalizes over the last dimension with Euclidean norm.
     """
     return input / input.norm(p, dim).clamp(min=eps).expand_as(input)
