403a49eea097a3329ba2f00f3d70583961cad6d2
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-05-07 09:51:45 +0300

    change default dim to 1 and add input args

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -744,7 +744,7 @@ def triplet_margin_loss(anchor, positive, negative, margin=1.0, p=2, eps=1e-6, s
     return loss
 
 
-def normalize(input, p=2, dim=-1, eps=1e-12):
+def normalize(input, p=2, dim=1, eps=1e-12):
     r"""Performs :math:`L_p` normalization of inputs over specified dimension.
 
     Does:
@@ -755,6 +755,12 @@ def normalize(input, p=2, dim=-1, eps=1e-12):
     for each subtensor v over dimension dim of input. Each subtensor is flattened into a vector,
     i.e. :math:`\lVert v \rVert_p` is not a matrix norm.
 
-    With default arguments normalizes over the last dimension with Euclidean norm.
+    With default arguments normalizes over the second dimension with Euclidean norm.
+
+    Args:
+        input: input tensor of any shape
+        p (float): the exponent value in the norm formulation
+        dim (int): the dimension to reduce
+        eps (float): small value to avoid division by zero
     """
     return input / input.norm(p, dim).clamp(min=eps).expand_as(input)
