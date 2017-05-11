1f71db1a9faef43ad442a72ea595c4f3a350e282
Romain Cledat <romainc@fb.com>
2017-05-10 17:33:21 -0700

    Extended ImageInputOp
    Summary:
    Added several features to the ImageInputOp:
      - bounding box (per image as well as default for the operator). For per-image, it
        only works in Caffe2 format and is passed as the third tensor in the form
        (ymin, xmin, height, width). For the operator, pass bounding_xmin, bounding_ymin,
        bounding_width and bounding_height as parameters.
      - per-channel mean/std. You can use the usual mean/std to pass a single
        value to be used for all channels or also pass mean_per_channel and std_per_channel
        to specify different values per channel. Order of channels is BGR.
      - A minimum size parameter that can be specified instead of the scale parameter.
        The minsize parameter will only scale the image if it is smaller than required.
        This differs from scale which will scale up as well as down. You can only specify
        one of scale or minsize.
    
    Added a test case to test some of the features
    
    Differential Revision: D4874988
    
    fbshipit-source-id: 437191052a46e9916defe8b100d7cc7864373f61

--- caffe2/image/image_input_op.cc
+++ caffe2/image/image_input_op.cc
@@ -21,7 +21,63 @@ OPERATOR_SCHEMA(ImageInput)
           out[1] =
               CreateTensorShape(vector<int>{1, batch_size}, TensorProto::INT32);
           return out;
-        });
+        })
+    .SetDoc(R"DOC(
+Imports and processes images from a database. For each run of the operator,
+batch_size images will be processed. GPUs can optionally be used for
+part of the processing.
+
+The following transformations are applied to the image
+  - A bounding box is applied to the initial image (optional)
+  - The image is rescaled either up or down (with the scale argument) or
+    just up (with the minsize argument)
+  - The image is randomly cropped (crop size is passed as an argument but
+    the location of the crop is random except if is_test is passed in which case
+    the image in cropped at the center)
+  - The image is normalized. Each of its color channels can have separate
+    normalization values
+
+The dimension of the output image will always be cropxcrop
+)DOC")
+    .Arg("batch_size", "Number of images to output for each run of the operator"
+         ". Must be 1 or greater")
+    .Arg("color", "Number of color channels (1 or 3). Defaults to 1")
+    .Arg("scale", "Scale the size of the smallest dimension of the image to"
+         " this. Scale and minsize are mutually exclusive."
+         " Must be larger than crop")
+    .Arg("minsize", "Scale the size of the smallest dimension of the image to"
+         " this only if the size is initially smaller. Scale and minsize are"
+         " mutually exclusive. Must be larger than crop.")
+    .Arg("warp", "If 1, both dimensions of the image will be set to minsize or"
+         " scale; otherwise, the other dimension is proportionally scaled."
+         " Defaults to 0")
+    .Arg("crop", "Size to crop the image to. Must be provided")
+    .Arg("mirror", "Whether or not to mirror the image. Defaults to 0")
+    .Arg("mean", "Mean by which to normalize color channels."
+         " Defaults to 0.")
+    .Arg("mean_per_channel", "Vector of means per color channel "
+         " (1 or 3 elements). Defaults to mean argument. Channel order BGR")
+    .Arg("std", "Standard deviation by which to normalize color channels."
+         " Defaults to 1.")
+    .Arg("std_per_channel", "Vector of standard dev. per color channel "
+     " (1 or 3 elements). Defaults to std argument. Channel order is BGR")
+    .Arg("bounding_ymin", "Bounding box coordinate. Defaults to -1 (none)")
+    .Arg("bounding_xmin", "Bounding box coordinate. Defaults to -1 (none)")
+    .Arg("bounding_height", "Bounding box coordinate. Defaults to -1 (none)")
+    .Arg("bounding_width", "Bounding box coordinate. Defaults to -1 (none)")
+    .Arg("is_test", "Set to 1 to do deterministic cropping. Defaults to 0")
+    .Arg("use_caffe_datum", "1 if the input is in Caffe format. Defaults to 0")
+    .Arg("use_gpu_transform", "1 if GPU acceleration should be used."
+         " Defaults to 0. Can only be 1 in a CUDAContext")
+    .Arg("decode_threads", "Number of CPU decode/transform threads."
+         " Defaults to 4")
+    .Arg("output_type", "If gpu_transform, can set to FLOAT or FLOAT16.")
+    .Arg("db", "Name of the database (if not passed as input)")
+    .Arg("db_type", "Type of database (if not passed as input)."
+         " Defaults to leveldb")
+    .Input(0, "reader", "The input reader (a db::DBReader)")
+    .Output(0, "data", "Tensor containing the images")
+    .Output(1, "label", "Tensor containing the labels");
 
 NO_GRADIENT(ImageInput);
 
---
+++ caffe2/python/operator_test/image_input_op_test.py
@@ -0,0 +1,280 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from PIL import Image
+import cv2
+import numpy as np
+import lmdb
+import shutil
+import StringIO
+import tempfile
+
+# TODO: This test does not test scaling because
+# the algorithms used by OpenCV in the C and Python
+# version seem to differ slightly. It does test
+# most other features
+
+from hypothesis import given, settings, Verbosity
+import hypothesis.strategies as st
+
+from caffe2.proto import caffe2_pb2
+import caffe2.python.hypothesis_test_util as hu
+
+from caffe2.python import workspace, core
+
+
+# Verification routines (applies transformations to image to
+# verify if the operator produces same result)
+def verify_apply_bounding_box(img, box):
+    import skimage.util
+    if any(type(box[f]) is not int or np.isnan(box[f] or box[f] < 0)
+           for f in range(0, 4)):
+        return img
+    # Box is ymin, xmin, bound_height, bound_width
+    y_bounds = (box[0], img.shape[0] - box[0] - box[2])
+    x_bounds = (box[1], img.shape[1] - box[1] - box[3])
+    c_bounds = (0, 0)
+
+    if any(el < 0 for el in list(y_bounds) + list(x_bounds) + list(c_bounds)):
+        return img
+
+    bboxed = skimage.util.crop(img, (y_bounds, x_bounds, c_bounds))
+    return bboxed
+
+
+# This function is called but not used. It will trip on assert False if
+# the arguments are wrong (improper example)
+def verify_rescale(img, minsize):
+    # Here we use OpenCV transformation to match the C code
+    scale_amount = float(minsize) / min(img.shape[0], img.shape[1])
+    if scale_amount <= 1.0:
+        return img
+
+    print("Scale amount is %f -- should be < 1.0; got shape %s" %
+          (scale_amount, str(img.shape)))
+    assert False
+    img_cv = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    output_shape = (int(np.ceil(scale_amount * img_cv.shape[0])),
+                    int(np.ceil(scale_amount * img_cv.shape[1])))
+    resized = cv2.resize(img_cv,
+                         dsize=output_shape,
+                         interpolation=cv2.INTER_AREA)
+
+    resized = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+    assert resized.shape[0] >= minsize
+    assert resized.shape[1] >= minsize
+    return resized
+
+
+def verify_crop(img, crop):
+    import skimage.util
+    assert img.shape[0] >= crop
+    assert img.shape[1] >= crop
+    y_offset = 0
+    if img.shape[0] > crop:
+        y_offset = (img.shape[0] - crop) // 2
+
+    x_offset = 0
+    if img.shape[1] > crop:
+        x_offset = (img.shape[1] - crop) // 2
+
+    y_bounds = (y_offset, img.shape[0] - crop - y_offset)
+    x_bounds = (x_offset, img.shape[1] - crop - x_offset)
+    c_bounds = (0, 0)
+    cropped = skimage.util.crop(img, (y_bounds, x_bounds, c_bounds))
+    assert cropped.shape[0] == crop
+    assert cropped.shape[1] == crop
+    return cropped
+
+
+def verify_color_normalize(img, means, stds):
+    # Note the RGB/BGR inversion
+    # Operate on integers like the C version
+    img = img * 255.0
+    img[:, :, 0] = (img[:, :, 0] - means[2]) / stds[2]
+    img[:, :, 1] = (img[:, :, 1] - means[1]) / stds[1]
+    img[:, :, 2] = (img[:, :, 2] - means[0]) / stds[0]
+    return img * (1.0 / 255.0)
+
+
+# Printing function (for debugging)
+def caffe2_img(img):
+    # Convert RGB to BGR
+    img = img[:, :, (2, 1, 0)]
+    # Convert HWC to CHW
+    img = img.swapaxes(1, 2).swapaxes(0, 1)
+    img = img * 255.0
+    return img.astype(np.int32)
+
+
+# Bounding box is ymin, xmin, height, width
+def create_test(output_dir, width, height, default_bound,
+                minsize, crop, means, stds, count):
+    print("Creating a temporary lmdb database of %d pictures..." % (count))
+
+    if default_bound is None:
+        default_bound = [-1] * 4
+
+    LMDB_MAP_SIZE = 1 << 40
+    env = lmdb.open(output_dir, map_size=LMDB_MAP_SIZE, subdir=True)
+    index = 0
+    # Create images and the expected results
+    expected_results = []
+    with env.begin(write=True) as txn:
+        while index < count:
+            img_array = np.random.random_integers(
+                0, 255, [height, width, 3]).astype(np.uint8)
+            img_obj = Image.fromarray(img_array)
+            img_str = StringIO.StringIO()
+            img_obj.save(img_str, 'PNG')
+
+            # Create a random bounding box for every other image
+            # ymin, xmin, bound_height, bound_width
+            # TODO: To ensure that we never need to scale, we
+            # ensure that the bounding-box is larger than the
+            # minsize parameter
+            bounding_box = list(default_bound)
+            do_default_bound = True
+            if index % 2 == 0:
+                if height > minsize and width > minsize:
+                    do_default_bound = False
+                    bounding_box[0:2] = [np.random.randint(a) for a in
+                                         (height - minsize, width - minsize)]
+                    bounding_box[2:4] = [np.random.randint(a) + minsize for a in
+                                         (height - bounding_box[0] - minsize + 1,
+                                          width - bounding_box[1] - minsize + 1)]
+                    # print("Bounding box is %s" % (str(bounding_box)))
+            # Create expected result
+            img_expected = img_array.astype(np.float32) * (1.0 / 255.0)
+            # print("Orig image: %s" % (str(caffe2_img(img_expected))))
+            img_expected = verify_apply_bounding_box(
+                img_expected,
+                bounding_box)
+            # print("Bounded image: %s" % (str(caffe2_img(img_expected))))
+
+            img_expected = verify_rescale(img_expected, minsize)
+
+            img_expected = verify_crop(img_expected, crop)
+            # print("Crop image: %s" % (str(caffe2_img(img_expected))))
+
+            img_expected = verify_color_normalize(img_expected, means, stds)
+            # print("Color image: %s" % (str(caffe2_img(img_expected))))
+
+            expected_results.append(caffe2_img(img_expected))
+
+            tensor_protos = caffe2_pb2.TensorProtos()
+            image_tensor = tensor_protos.protos.add()
+            image_tensor.data_type = 4  # string data
+            image_tensor.string_data.append(img_str.getvalue())
+            img_str.close()
+
+            label_tensor = tensor_protos.protos.add()
+            label_tensor.data_type = 2  # int32 data
+            label_tensor.int32_data.append(index)
+
+            if not do_default_bound:
+                bounding_tensor = tensor_protos.protos.add()
+                bounding_tensor.data_type = 2  # int32 data
+                bounding_tensor.int32_data.extend(bounding_box)
+
+            txn.put(
+                '{}'.format(index).encode('ascii'),
+                tensor_protos.SerializeToString()
+            )
+            index = index + 1
+        # End while
+    # End with
+    return expected_results
+
+
+class TestImport(hu.HypothesisTestCase):
+    @given(size_tuple=st.tuples(
+        st.integers(min_value=8, max_value=4096),
+        st.integers(min_value=8, max_value=4096)).flatmap(lambda t: st.tuples(
+            st.just(t[0]), st.just(t[1]),
+            st.just(min(t[0] - 6, t[1] - 4)),
+            st.integers(min_value=1, max_value=min(t[0] - 6, t[1] - 4)))),
+        means=st.tuples(st.integers(min_value=0, max_value=255),
+                        st.integers(min_value=0, max_value=255),
+                        st.integers(min_value=0, max_value=255)),
+        stds=st.tuples(st.floats(min_value=1, max_value=10),
+                       st.floats(min_value=1, max_value=10),
+                       st.floats(min_value=1, max_value=10)),
+        **hu.gcs)
+    @settings(verbosity=Verbosity.verbose)
+    def test_imageinput(self, size_tuple, means, stds, gc, dc):
+        # TODO: Does not test on GPU and does not test use_gpu_transform
+        # WARNING: Using CNNModelHelper automatically does NHWC to NCHW
+        # transformation if needed.
+        width, height, minsize, crop = size_tuple
+        means = [float(m) for m in means]
+        stds = [float(s) for s in stds]
+        out_dir = tempfile.mkdtemp()
+        count_images = 2  # One with bounding box and one without
+        expected_images = create_test(
+            out_dir,
+            width=width,
+            height=height,
+            default_bound=(3, 5, height - 3, width - 5),
+            minsize=minsize,
+            crop=crop,
+            means=means,
+            stds=stds,
+            count=count_images
+        )
+        for device_option in dc:
+            with hu.temp_workspace():
+                reader_net = core.Net('reader')
+                reader_net.CreateDB(
+                    [],
+                    'DB',
+                    db=out_dir,
+                    db_type="lmdb"
+                )
+                workspace.RunNetOnce(reader_net)
+                imageop = core.CreateOperator(
+                    'ImageInput',
+                    ['DB'],
+                    ["data", "label"],
+                    batch_size=count_images,
+                    color=3,
+                    minsize=minsize,
+                    crop=crop,
+                    is_test=True,
+                    bounding_ymin=3,
+                    bounding_xmin=5,
+                    bounding_height=height - 3,
+                    bounding_width=width - 5,
+                    mean_per_channel=means,
+                    std_per_channel=stds,
+                    use_gpu_transform=(device_option.device_type == 1)
+                )
+
+                imageop.device_option.CopyFrom(device_option)
+                main_net = core.Net('main')
+                main_net.Proto().op.extend([imageop])
+                workspace.RunNetOnce(main_net)
+                l = workspace.FetchBlob('label')
+                result = workspace.FetchBlob('data').astype(np.int32)
+                # If we don't use_gpu_transform, the output is in NHWC
+                # Our reference output is CHW so we swap
+                if device_option.device_type != 1:
+                    expected = [img.swapaxes(0, 1).swapaxes(1, 2) for
+                                img in expected_images]
+                else:
+                    expected = expected_images
+                for i in range(count_images):
+                    self.assertEqual(l[i], i)
+                    self.assertEqual((expected[i] - result[i] > 1).sum(), 0)
+                # End for
+            # End with
+        # End for
+        shutil.rmtree(out_dir)
+    # End test_imageinput
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
