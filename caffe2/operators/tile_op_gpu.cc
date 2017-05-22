d99f55b751284e1f1ca5029d36b463acca56aee8
Aapo Kyrola <akyrola@fb.com>
2017-05-22 12:07:28 -0700

    better TileOp/Gradient CUDA implementation
    Summary: ves and jamesr66a had noticed that TileOp for CUDA was very slow, as it started kernels inside double loops. It was my fault not to notice this in the code review. This diff uses 1 kernel for forward and backward passes and is probably much faster. I did not test though, maybe ves or jamesr66a can help?
    
    Reviewed By: jamesr66a
    
    Differential Revision: D5101968
    
    fbshipit-source-id: 64b6ac933785e3710b3c1d8c692a4c48650bca96

--- caffe2/operators/tile_op_gpu.cc
+++
@@ -1,9 +0,0 @@
-#include "caffe2/core/context_gpu.h"
-#include "caffe2/operators/tile_op.h"
-
-namespace caffe2 {
-namespace {
-REGISTER_CUDA_OPERATOR(Tile, TileOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(TileGradient, TileGradientOp<float, CUDAContext>);
-} // namespace
-} // namespace caffe2
