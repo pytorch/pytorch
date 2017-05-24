6f0c266b05e98f16d2f4adef1a0518731ad4f1aa
Hans Gaiser <hansg91@gmail.com>
2017-05-24 14:31:14 -0700

    Check system dependencies first
    Summary:
    This PR changes the cmake of Caffe2 to look for system dependencies before resorting to the submodules in `third-party`. Only googletest should logically be in third-party, the other libraries should ideally be installed as system dependencies by the user. This PR adds system dependency checks for Gloo, CUB, pybind11, Eigen and benchmark, as these were missing from the cmake files.
    
    In addition it removes the execution of `git submodule update --init` in cmake. This seems like bad behavior to me, it should be up to the user to download submodules and manage the git repository.
    Closes https://github.com/caffe2/caffe2/pull/382
    
    Differential Revision: D5124123
    
    Pulled By: Yangqing
    
    fbshipit-source-id: cc34dda58ffec447874a89d01058721c02a52476

---
+++ cmake/Modules/FindCUB.cmake
@@ -0,0 +1,18 @@
+# Try to find the CUB library and headers.
+#  CUB_FOUND        - system has CUB
+#  CUB_INCLUDE_DIRS - the CUB include directory
+
+find_path(CUB_INCLUDE_DIR
+	NAMES cub/cub.cuh
+	DOC "The directory where CUB includes reside"
+)
+
+set(CUB_INCLUDE_DIRS ${CUB_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CUB
+	FOUND_VAR CUB_FOUND
+	REQUIRED_VARS CUB_INCLUDE_DIR
+)
+
+mark_as_advanced(CUB_FOUND)
