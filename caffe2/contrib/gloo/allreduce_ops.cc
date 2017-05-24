457720459d0f5a16caa70f7e056e083572d6b3aa
Lukasz Wesolowski <lwesolowski@fb.com>
2017-05-24 16:34:11 -0700

    Change AllreduceOp and BroadcastOp to allow initializing gloo algorithms to take float16 inputs
    Summary: Modify BroadcastOp and AllreduceOp to allow initializing algorithms on buffers of float16 values. Previously the Allreduce algorithm definitions were hardcoded to take float.
    
    Reviewed By: pietern
    
    Differential Revision: D5042015
    
    fbshipit-source-id: c5c3ea5566f9f23969847dcc0735f5f4b075f56f

--- caffe2/contrib/gloo/allreduce_ops.cc
+++ caffe2/contrib/gloo/allreduce_ops.cc
@@ -3,34 +3,59 @@
 #include <gloo/allreduce_halving_doubling.h>
 #include <gloo/allreduce_ring.h>
 #include <gloo/allreduce_ring_chunked.h>
+#include <gloo/types.h>
 
 namespace caffe2 {
 namespace gloo {
 
-template <typename T, class Context>
-void AllreduceOp<T, Context>::initializeHalvingDoubling() {
-  algorithm_.reset(new ::gloo::AllreduceHalvingDoubling<T>(
-      init_.context, init_.outputs, init_.size));
+template <class Context>
+void AllreduceOp<Context>::initializeHalvingDoubling() {
+  if (init_.template IsType<float>()) {
+    algorithm_.reset(new ::gloo::AllreduceHalvingDoubling<float>(
+        init_.context, init_.template getOutputs<float>(), init_.size));
+  } else if (init_.template IsType<::caffe2::float16>()) {
+    algorithm_.reset(new ::gloo::AllreduceHalvingDoubling<::gloo::float16>(
+        init_.context,
+        init_.template getOutputs<::gloo::float16>(),
+        init_.size));
+  } else {
+    CAFFE_ENFORCE(false, "Unhandled type: ", init_.meta.name());
+  }
 }
 
-template <typename T, class Context>
-void AllreduceOp<T, Context>::initializeRingFull() {
-  algorithm_.reset(
-      new ::gloo::AllreduceRing<T>(init_.context, init_.outputs, init_.size));
+template <class Context>
+void AllreduceOp<Context>::initializeRingFull() {
+  if (init_.template IsType<float>()) {
+    algorithm_.reset(new ::gloo::AllreduceRing<float>(
+        init_.context, init_.template getOutputs<float>(), init_.size));
+  } else if (init_.template IsType<::caffe2::float16>()) {
+    algorithm_.reset(new ::gloo::AllreduceRing<::gloo::float16>(
+        init_.context,
+        init_.template getOutputs<::gloo::float16>(),
+        init_.size));
+  } else {
+    CAFFE_ENFORCE(false, "Unhandled type: ", init_.meta.name());
+  }
 }
 
-template <typename T, class Context>
-void AllreduceOp<T, Context>::initializeRingChunked() {
-  algorithm_.reset(new ::gloo::AllreduceRingChunked<T>(
-      init_.context, init_.outputs, init_.size));
+template <class Context>
+void AllreduceOp<Context>::initializeRingChunked() {
+  if (init_.template IsType<float>()) {
+    algorithm_.reset(new ::gloo::AllreduceRingChunked<float>(
+        init_.context, init_.template getOutputs<float>(), init_.size));
+  } else if (init_.template IsType<::caffe2::float16>()) {
+    algorithm_.reset(new ::gloo::AllreduceRingChunked<::gloo::float16>(
+        init_.context,
+        init_.template getOutputs<::gloo::float16>(),
+        init_.size));
+  } else {
+    CAFFE_ENFORCE(false, "Unhandled type: ", init_.meta.name());
+  }
 }
 
 namespace {
 
-REGISTER_CPU_OPERATOR_WITH_ENGINE(
-    Allreduce,
-    GLOO,
-    AllreduceOp<float, CPUContext>);
+REGISTER_CPU_OPERATOR_WITH_ENGINE(Allreduce, GLOO, AllreduceOp<CPUContext>);
 
 } // namespace
 } // namespace gloo
