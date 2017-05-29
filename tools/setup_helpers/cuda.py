ee855ebbe4efdcee6379a392b35327ca8af20361
Joao Felipe Santos <joao.eel@gmail.com>
2017-05-29 11:51:16 -0400

    Fix CUDA_HOME detection

--- tools/setup_helpers/cuda.py
+++ tools/setup_helpers/cuda.py
@@ -1,7 +1,16 @@
-import ctypes.util
 import os
+from subprocess import Popen, PIPE
 
 from .env import check_env_flag
+
+def find_nvcc():
+    proc = Popen(['which', 'nvcc'], stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    out = out.decode().strip()
+    if len(out) > 0:
+        return os.path.dirname(out)
+    else:
+        return None
 
 if check_env_flag('NO_CUDA'):
     WITH_CUDA = False
@@ -9,9 +18,9 @@ if check_env_flag('NO_CUDA'):
 else:
     CUDA_HOME = os.getenv('CUDA_HOME', '/usr/local/cuda')
     if not os.path.exists(CUDA_HOME):
-        cudart_path = ctypes.util.find_library('cudart')
-        if cudart_path is not None:
-            CUDA_HOME = os.path.dirname(cudart_path)
+        nvcc_path = find_nvcc()
+        if nvcc_path is not None:
+            CUDA_HOME = os.path.dirname(nvcc_path)
         else:
             CUDA_HOME = None
     WITH_CUDA = CUDA_HOME is not None
