503002eda765851ee0a79443b13c766704a9c0a5
Gregory Chanan <gchanan@fb.com>
2017-05-30 11:10:16 -0700

    Add broadcasting support for copy_, simplify code generation by moving a lot of currently generated code to expand_utils.

--- THSize.c
+++ THSize.c
@@ -1,3 +1,5 @@
+#include "THSize.h"
+
 int THSize_isSameSizeAs(const long *sizeA, long dimsA, const long *sizeB, long dimsB) {
   int d;
   if (dimsA != dimsB)
@@ -8,4 +10,17 @@ int THSize_isSameSizeAs(const long *sizeA, long dimsA, const long *sizeB, long d
       return 0;
   }
   return 1;
+}
+
+ptrdiff_t THSize_nElement(long dims, long *size) {
+  if(dims == 0)
+    return 0;
+  else
+  {
+    ptrdiff_t nElement = 1;
+    int d;
+    for(d = 0; d < dims; d++)
+      nElement *= size[d];
+    return nElement;
+  }
 }
