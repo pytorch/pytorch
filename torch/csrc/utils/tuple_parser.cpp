9191f152aadeaba9191542bf2cbdbdac87de8a34
Alykhan Tejani <alykhan.tejani@gmail.com>
2017-06-02 14:36:31 +0100

    added param name to tuple_parser for better error messages

--- torch/csrc/utils/tuple_parser.cpp
+++ torch/csrc/utils/tuple_parser.cpp
@@ -18,60 +18,60 @@ TupleParser::TupleParser(PyObject* args, int num_args) : args(args), idx(0) {
    }
  }
 
-auto TupleParser::parse(bool& x) -> void {
+auto TupleParser::parse(bool& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   if (!PyBool_Check(obj)) {
-    throw invalid_type("bool");
+    throw invalid_type("bool", param_name);
   }
   x = (obj == Py_True);
 }
 
-auto TupleParser::parse(int& x) -> void {
+auto TupleParser::parse(int& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   if (!THPUtils_checkLong(obj)) {
-    throw invalid_type("int");
+    throw invalid_type("int", param_name);
   }
   x = THPUtils_unpackLong(obj);
 }
 
-auto TupleParser::parse(double& x) -> void {
+auto TupleParser::parse(double& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   if (!THPUtils_checkDouble(obj)) {
-    throw invalid_type("float");
+    throw invalid_type("float", param_name);
   }
   x = THPUtils_unpackDouble(obj);
 }
 
-auto TupleParser::parse(std::unique_ptr<thpp::Tensor>& x) -> void {
+auto TupleParser::parse(std::unique_ptr<thpp::Tensor>& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   x = torch::createTensor(obj);
 }
 
-auto TupleParser::parse(std::shared_ptr<thpp::Tensor>& x) -> void {
+auto TupleParser::parse(std::shared_ptr<thpp::Tensor>& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   x.reset(torch::createTensor(obj)->clone_shallow());
 }
 
-auto TupleParser::parse(std::vector<int>& x) -> void {
+auto TupleParser::parse(std::vector<int>& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   if (!PyTuple_Check(obj)) {
-    throw invalid_type("tuple of int");
+    throw invalid_type("tuple of int", param_name);
   }
   int size = PyTuple_GET_SIZE(obj);
   x.resize(size);
   for (int i = 0; i < size; ++i) {
     PyObject* item = PyTuple_GET_ITEM(obj, i);
     if (!THPUtils_checkLong(item)) {
-      throw invalid_type("tuple of int");
+      throw invalid_type("tuple of int", param_name);
     }
     x[i] = THPUtils_unpackLong(item);
   }
 }
 
-auto TupleParser::parse(std::string& x) -> void {
+auto TupleParser::parse(std::string& x, const std::string& param_name) -> void {
   PyObject* obj = next_arg();
   if (!THPUtils_checkString(obj)) {
-    throw invalid_type("bytes/str");
+    throw invalid_type("bytes/str", param_name);
   }
   x = THPUtils_unpackString(obj);
 }
@@ -83,13 +83,31 @@ auto TupleParser::next_arg() -> PyObject* {
   return PyTuple_GET_ITEM(args, idx++);
 }
 
-auto TupleParser::invalid_type(const char* expected) -> std::runtime_error {
+auto TupleParser::invalid_type(const std::string& expected, const std::string& param_name) -> std::runtime_error {
   std::string msg("argument ");
   msg += std::to_string(idx - 1);
-  msg += " must be ";
+  msg += " (";
+  msg += param_name;
+  msg += ") ";
+  msg += "must be ";
   msg += expected;
-  msg += ", not ";
-  msg += Py_TYPE(PyTuple_GET_ITEM(args, idx - 1))->tp_name;
+
+  PyObject* obj = PyTuple_GET_ITEM(args, idx -1);
+  if (PyTuple_Check(obj)){
+    msg += " but got tuple of (";
+    int size = PyTuple_GET_SIZE(obj);
+    for (int i = 0; i < size; ++i) {
+      msg += Py_TYPE(PyTuple_GET_ITEM(obj, i))->tp_name;
+      if (i != size - 1){
+        msg += ", ";
+      }
+    }
+    msg += ")";
+  }
+  else{
+    msg += ", not ";
+    msg += Py_TYPE(PyTuple_GET_ITEM(args, idx - 1))->tp_name;
+  }
   return std::runtime_error(msg);
 }
 
