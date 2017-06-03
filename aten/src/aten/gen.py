ccd9dc1bb174c6c1ce0635d82720da4e636fb4cf
Zachary DeVito <zdevito@gmail.com>
2017-06-03 13:05:22 +0000

    add tensor skeleton

--- aten/src/aten/gen.py
+++ aten/src/aten/gen.py
@@ -13,10 +13,16 @@ TEMPLATE_PATH =  options.source_path+"/templates"
 GENERATOR_DERIVED = CodeTemplate.from_file(TEMPLATE_PATH+"/GeneratorDerived.h")
 STORAGE_DERIVED_CPP = CodeTemplate.from_file(TEMPLATE_PATH+"/StorageDerived.cpp")
 STORAGE_DERIVED_H = CodeTemplate.from_file(TEMPLATE_PATH+"/StorageDerived.h")
+
 TYPE_DERIVED_CPP = CodeTemplate.from_file(TEMPLATE_PATH+"/TypeDerived.cpp")
 TYPE_DERIVED_H = CodeTemplate.from_file(TEMPLATE_PATH+"/TypeDerived.h")
 TYPE_H = CodeTemplate.from_file(TEMPLATE_PATH+"/Type.h")
 TYPE_CPP = CodeTemplate.from_file(TEMPLATE_PATH+"/Type.cpp")
+
+TENSOR_DERIVED_CPP = CodeTemplate.from_file(TEMPLATE_PATH+"/TensorDerived.cpp")
+TENSOR_DERIVED_H = CodeTemplate.from_file(TEMPLATE_PATH+"/TensorDerived.h")
+TENSOR_H = CodeTemplate.from_file(TEMPLATE_PATH+"/Tensor.h")
+TENSOR_CPP = CodeTemplate.from_file(TEMPLATE_PATH+"/Tensor.cpp")
 
 generators = {
     'CPUGenerator.h' : {
@@ -43,7 +49,8 @@ scalar_types = [
     ('Half','Half'),
 ]
 
-type_env = {
+# shared environment for non-derived base classes Type.h Tensor.h Storage.h
+top_env = {
     'type_registrations' : [],
     'type_headers' : []
 }
@@ -55,25 +62,28 @@ def write(filename,s):
     with open(filename,"w") as f:
         f.write(s)
 
-def generate_storage_and_type(processor, scalar_type):
+def generate_storage_type_and_tensor(processor, scalar_type):
     scalar_name, c_type = scalar_type
     env = {}
     env['ScalarName'] = scalar_name
     env['ScalarType'] = c_type
     env['Storage'] = "{}{}Storage".format(processor,scalar_name)
     env['Type'] = "{}{}Type".format(processor,scalar_name)
+    env['Tensor'] = "{}{}Tensor".format(processor,scalar_name)
     env['Processor'] = processor
 
     if processor == 'CUDA':
         env['th_header'] = "THC/THC.h"
         sname = '' if scalar_name == "Float" else scalar_name
         env['THStorage'] = 'THCuda{}Storage'.format(sname)
+        env['THTensor'] = 'THCuda{}Tensor'.format(sname)
         env['state'] = ['context->thc_state']
         env['isCUDA'] = 'true'
         env['storage_device'] = 'return storage->device;'
     else:
         env['th_header'] = "TH/TH.h"
         env['THStorage'] = "TH{}Storage".format(scalar_name)
+        env['THTensor'] = 'TH{}Tensor'.format(scalar_name)
         env['state'] = []
         env['isCUDA'] = 'false'
         env['storage_device'] = 'throw std::runtime_error("CPU storage has no device");'
@@ -92,19 +102,28 @@ def generate_storage_and_type(processor, scalar_type):
 
     write(env['Storage']+".cpp",STORAGE_DERIVED_CPP.substitute(env))
     write(env['Storage']+".h",STORAGE_DERIVED_H.substitute(env))
+
     write(env['Type']+".cpp",TYPE_DERIVED_CPP.substitute(env))
     write(env['Type']+".h",TYPE_DERIVED_H.substitute(env))
+
+    write(env['Tensor']+".cpp",TENSOR_DERIVED_CPP.substitute(env))
+    write(env['Tensor']+".h",TENSOR_DERIVED_H.substitute(env))
+
+
     type_register = ('context->type_registry[static_cast<int>(Processor::{})][static_cast<int>(ScalarType::{})].reset(new {}(context));'
         .format(processor,scalar_name,env['Type']))
-    type_env['type_registrations'].append(type_register)
-    type_env['type_headers'].append('#include "{}.h"'.format(env['Type']))
+    top_env['type_registrations'].append(type_register)
+    top_env['type_headers'].append('#include "{}.h"'.format(env['Type']))
 
 for fname,env in generators.items():
     write(fname,GENERATOR_DERIVED.substitute(env))
 
 for processor in processors:
     for scalar_type in scalar_types:
-        generate_storage_and_type(processor,scalar_type)
+        generate_storage_type_and_tensor(processor,scalar_type)
 
-write("Type.h",TYPE_H.substitute(type_env))
-write("Type.cpp",TYPE_CPP.substitute(type_env))
+write('Type.h',TYPE_H.substitute(top_env))
+write('Type.cpp',TYPE_CPP.substitute(top_env))
+
+write('Tensor.h',TENSOR_H.substitute(top_env))
+write('Tensor.cpp',TENSOR_CPP.substitute(top_env))
