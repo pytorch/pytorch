9307ebf2e842ac7286484e2de3ff7deda5f84512
Zachary DeVito <zdevito@fb.com>
2017-06-05 17:25:54 -0400

    initial sanitize

--- aten/src/aten/preprocess_declarations.py
+++ aten/src/aten/preprocess_declarations.py
@@ -1,8 +1,12 @@
+import common_with_cwrap
 
+def exclude(declaration):
+    return 'only_register' in declaration
 
-def exclude(d):
-    return 'only_register' in d
 
 def run(declarations):
+    for declaration in declarations:
+        common_with_cwrap.set_declaration_defaults(declaration)
+        
     declarations = [d for d in declarations if not exclude(d)]
     return declarations
