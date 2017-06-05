4fd1daacdabef593221efcf2bc1b7b688a842e28
Zachary DeVito <zdevito@fb.com>
2017-06-05 18:10:58 -0400

    add sort...

--- aten/src/aten/preprocess_declarations.py
+++ aten/src/aten/preprocess_declarations.py
@@ -8,6 +8,7 @@ def run(declarations):
     for declaration in declarations:
         common_with_cwrap.set_declaration_defaults(declaration)
         common_with_cwrap.enumerate_options_due_to_default(declaration)
-
+        common_with_cwrap.sort_by_number_of_options(declaration)
+        
     declarations = [d for d in declarations if not exclude(d)]
     return declarations
