d3c125bec994500a3e92060115ab7471c1df0e76
Wenyi Huang <harrywy@fb.com>
2017-06-06 14:07:37 -0700

    Add gradient for topK op
    Summary:
    Input of topK op: X (dense)
    Output of topK op: Value and Indices (sparse representation)
    Value will have gradient in some cases,
    
    We backprop (copy) the gradient from sparse (d Value) to dense (d X)
    
    Differential Revision: D5133461
    
    fbshipit-source-id: 7bad55b60e8a22dfe0e51357ce2099d7f752c133

--- caffe2/operators/top_k.h
+++ caffe2/operators/top_k.h
@@ -51,6 +51,8 @@ class TopKOp : public Operator<Context> {
     // Linearize input tensor except for last dimension
     // e.g. [3, 4, 5] -> [12, 5]
     // [5] -> [5]
+    CAFFE_ENFORCE(
+        in_dims.back() >= k_, "k argment should not be greater than last dim");
     vector<TIndex> linear_shape = {size_to_dim_(in_dims.size() - 1, in_dims),
                                    in_dims[in_dims.size() - 1]};
     auto input_map = ConstEigenMatrixMapRowMajor<T>(
@@ -110,6 +112,55 @@ class TopKOp : public Operator<Context> {
 
  private:
   int k_;
+};
+
+template <typename T, class Context>
+class TopKGradientOp : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+
+  TopKGradientOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws) {}
+
+  bool RunOnDevice() override {
+    auto& values = Input(0);
+    auto& indices = Input(1);
+    auto& original_input = Input(2);
+    auto* output = Output(0);
+
+    vector<TIndex> in_dims = values.dims();
+    // Linearize input tensor except for last dimension
+    // e.g. [3, 4, 5] -> [12, 5]
+    // [5] -> [5]
+    vector<TIndex> linear_shape = {size_to_dim_(in_dims.size() - 1, in_dims),
+                                   in_dims[in_dims.size() - 1]};
+    auto values_map = ConstEigenMatrixMapRowMajor<T>(
+        static_cast<const T*>(values.raw_data()),
+        linear_shape[0],
+        linear_shape[1]);
+    auto indices_map = ConstEigenMatrixMapRowMajor<TIndex>(
+        static_cast<const TIndex*>(indices.raw_data()),
+        linear_shape[0],
+        linear_shape[1]);
+
+    // Resize output tensors to be as orignial_input size and initialized with 0
+    vector<TIndex> original_dims = original_input.dims();
+    output->Resize(original_dims);
+    T* output_data = output->template mutable_data<T>();
+    memset(output_data, 0, output->nbytes());
+
+    // Use Eigen maps to allow indexing into the 2d tensors
+    auto output_map = EigenMatrixMapRowMajor<T>(
+        output_data, linear_shape[0], original_dims.back());
+
+    for (TIndex i = 0; i < linear_shape[0]; ++i) {
+      for (TIndex j = 0; j < linear_shape[1]; ++j) {
+        output_map(i, indices_map(i, j)) = values_map(i, j);
+      }
+    }
+
+    return true;
+  }
 };
 
 } // namespace caffe2
