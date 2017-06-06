174b4c9462bd3db0d6a69700d2d9a058d3803c54
Zachary DeVito <zdevito@fb.com>
2017-06-06 18:07:56 -0400

    integrate checked_cast

--- aten/src/aten/Utils.h
+++ aten/src/aten/Utils.h
@@ -3,9 +3,15 @@
 namespace tlib {
 
 #define TensorLib_assert(cond, ...) if (!cond) { tlib::runtime_error(__VA_ARGS__); }
+
+[[noreturn]]
 void runtime_error(const char *format, ...);
 
 template <typename T, typename Base>
-T checked_cast(Base expr);
+static inline T* checked_cast(Base* expr) {
+  if(auto result = dynamic_cast<T*>(expr))
+    return result;
+  runtime_error("Expected a '%s' but found '%s'",T::typeString(),expr->type().toString());
+}
 
 } // tlib
