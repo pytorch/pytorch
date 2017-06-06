fe1c286f481f80a13ba4e8392484b8d9e242b2d7
Trevor Killeen <killeentm@gmail.com>
2017-06-06 14:49:32 -0700

    add cast that may or may not work

--- aten/src/aten/Utils.cpp
+++ aten/src/aten/Utils.cpp
@@ -1,6 +1,7 @@
 #include "TensorLib/Utils.h"
 #include <stdarg.h>
 #include <stdexcept>
+#include <typeinfo>
 
 namespace tlib {
 
@@ -14,6 +15,16 @@ void runtime_error(const char *format, ...) {
   va_end(fmt_args);
 
   throw std::runtime_error(error_buf);
+}
+
+template <typename T, typename Base>
+T checked_cast(Base expr) {
+  try {
+    return dynamic_cast<T>(expr);
+  } catch (const std::bad_cast& exception) {
+    runtime_error("Dynamic cast failed. Tried to cast to type %s from expr of type %s\n",
+                  T::typeString(), expr.type().toString());
+  }
 }
 
 } // tlib
