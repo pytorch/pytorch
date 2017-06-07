48ae7276a60326e93f94f1a204ce3e4caebb0878
Gregory Chanan <gchanan@fb.com>
2017-06-07 09:31:26 -0700

    Fix masked_copy call to masked_scatter.

--- torch/tensor.py
+++ torch/tensor.py
@@ -265,7 +265,7 @@ class _TensorBase(object):
 
     def masked_copy_(self, *args, **kwargs):
         warnings.warn("masked_copy_ is deprecated and renamed to masked_scatter_, and will be removed in v0.3")
-        return self.masked_scatter_(self, *args, **kwargs)
+        return self.masked_scatter_(*args, **kwargs)
 
     # TODO: add tests for operators
     def __add__(self, other):
