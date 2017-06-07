fe06aa71d663aa9692c70082c9c4cb1a4a4e39f7
Gregory Chanan <gchanan@fb.com>
2017-06-07 13:06:01 -0700

    Clarify use of warn vs raise in expand_utils and don't catch exception in Broadcast plugin when fallback = false.

--- torch/csrc/expand_utils.h
+++ torch/csrc/expand_utils.h
@@ -38,11 +38,12 @@ void check_backincompat_expand_warn(ExpandType *to_expand, TensorType *tensor,
 template <typename ExpandType, typename TensorType>
 void expand_inplace(LIBRARY_STATE_TYPE ExpandType *r, ExpandType *to_expand, TensorType *tensor,
                     char *to_expand_name, char *tensor_name, bool fallback,
-                    THLongStorage *tensor_size,  ptrdiff_t to_expand_nElem, ptrdiff_t tensor_nElem, bool raise) {
+                    THLongStorage *tensor_size,  ptrdiff_t to_expand_nElem, ptrdiff_t tensor_nElem,
+                    bool warn_pointwise_fallback) {
   try {
     expand<ExpandType>(LIBRARY_STATE r, to_expand, tensor_size);
   } catch (std::exception &e) {
-    if (!raise) {
+    if (warn_pointwise_fallback) {
       std::ostringstream warn;
       warn << to_expand_name << " is not broadcastable to " << tensor_name
            << ", but they have the same number of elements.  Falling back to deprecated pointwise behavior.";
@@ -57,12 +58,12 @@ void expand_inplace1(LIBRARY_STATE_TYPE ExpandType *r, ExpandType *to_expand, Te
                      char *to_expand_name, char *tensor_name, bool fallback) {
   ptrdiff_t to_expand_nElem = THSize_nElement(to_expand->nDimension, to_expand->size);
   ptrdiff_t tensor_nElem = THSize_nElement(tensor->nDimension, tensor->size);
-  bool to_expand_raise = !fallback || (to_expand_nElem != tensor_nElem) || to_expand_nElem == 0;
+  bool to_expand_warn = fallback && (to_expand_nElem == tensor_nElem) && to_expand_nElem != 0;
   THLongStoragePtr tensor_size(THLongStorage_newWithSize(tensor->nDimension));
   THLongStorage_rawCopy(tensor_size.get(), tensor->size);
 
   expand_inplace(LIBRARY_STATE r, to_expand, tensor, to_expand_name, tensor_name, fallback,
-                 tensor_size, to_expand_nElem, tensor_nElem, to_expand_raise);
+                 tensor_size, to_expand_nElem, tensor_nElem, to_expand_warn);
   check_backincompat_expand_warn<ExpandType, TensorType>(to_expand, tensor, to_expand_name, tensor_name, fallback,
                                                          to_expand_nElem, tensor_nElem);
 }
@@ -74,15 +75,15 @@ void expand_inplace2(LIBRARY_STATE_TYPE TensorType *r1, TensorType *r2,
   ptrdiff_t tensor_nElem = THSize_nElement(tensor->nDimension, tensor->size);
   ptrdiff_t to_expand1_nElem = THSize_nElement(to_expand1->nDimension, to_expand1->size);
   ptrdiff_t to_expand2_nElem = THSize_nElement(to_expand2->nDimension, to_expand2->size);
-  bool to_expand1_raise = !fallback || (tensor_nElem != to_expand1_nElem) || tensor_nElem == 0;
-  bool to_expand2_raise = !fallback || (tensor_nElem != to_expand2_nElem) || tensor_nElem == 0;
+  bool to_expand1_warn = fallback && (tensor_nElem == to_expand1_nElem) && tensor_nElem != 0;
+  bool to_expand2_warn = fallback && (tensor_nElem == to_expand2_nElem) && tensor_nElem != 0;
   THLongStoragePtr tensor_size(THLongStorage_newWithSize(tensor->nDimension));
   THLongStorage_rawCopy(tensor_size.get(), tensor->size);
 
   expand_inplace(LIBRARY_STATE r1, to_expand1, tensor, to_expand1_name, tensor_name, fallback,
-                 tensor_size, to_expand1_nElem, tensor_nElem, to_expand1_raise || to_expand2_raise);
+                 tensor_size, to_expand1_nElem, tensor_nElem, to_expand1_warn && to_expand2_warn);
   expand_inplace(LIBRARY_STATE r2, to_expand2, tensor, to_expand2_name, tensor_name, fallback,
-                 tensor_size, to_expand2_nElem, tensor_nElem, to_expand1_raise || to_expand2_raise);
+                 tensor_size, to_expand2_nElem, tensor_nElem, to_expand1_warn && to_expand2_warn);
 
   check_backincompat_expand_warn<TensorType, TensorType>(to_expand1, tensor, to_expand1_name, tensor_name, fallback,
                                                          to_expand1_nElem, tensor_nElem);
@@ -96,11 +97,11 @@ void expand_outplace2(LIBRARY_STATE_TYPE TensorType *r1, TensorType *r2,
                       char *to_expand1_name, char *to_expand2_name, bool fallback) {
   ptrdiff_t to_expand1_nElem = THSize_nElement(to_expand1->nDimension, to_expand1->size);
   ptrdiff_t to_expand2_nElem = THSize_nElement(to_expand2->nDimension, to_expand2->size);
-  bool raise = !fallback || (to_expand1_nElem != to_expand2_nElem) || to_expand1_nElem == 0;
+  bool expand_warn = fallback && (to_expand1_nElem == to_expand2_nElem) && to_expand1_nElem != 0;
   try {
     expand2<TensorType>(LIBRARY_STATE r1, r2, to_expand1, to_expand2);
   } catch (std::exception &e) {
-    if (!raise) {
+    if (expand_warn) {
       std::ostringstream warn;
       warn << to_expand1_name << " and " << to_expand2_name << " not broadcastable, but have the same number of "
            << "elements.  Falling back to deprecated pointwise behavior.";
@@ -120,13 +121,13 @@ void expand_outplace3(LIBRARY_STATE_TYPE TensorType *r1, TensorType *r2, TensorT
   ptrdiff_t to_expand1_nElem = THSize_nElement(to_expand1->nDimension, to_expand1->size);
   ptrdiff_t to_expand2_nElem = THSize_nElement(to_expand2->nDimension, to_expand2->size);
   ptrdiff_t to_expand3_nElem = THSize_nElement(to_expand3->nDimension, to_expand3->size);
-  bool to_expand2_raise = !fallback || (to_expand1_nElem != to_expand2_nElem) || to_expand1_nElem == 0;
-  bool to_expand3_raise = !fallback || (to_expand1_nElem != to_expand2_nElem) || to_expand1_nElem == 0;
+  bool to_expand2_warn = fallback && (to_expand1_nElem == to_expand2_nElem) && to_expand1_nElem != 0;
+  bool to_expand3_warn = fallback && (to_expand1_nElem == to_expand3_nElem) && to_expand1_nElem != 0;
 
   try {
     expand3<TensorType>(LIBRARY_STATE r1, r2, r3, to_expand1, to_expand2, to_expand3);
   } catch (std::exception &e) {
-    if(!to_expand2_raise && !to_expand3_raise) {
+    if(to_expand2_warn && to_expand3_warn) {
       std::ostringstream warn;
       warn << to_expand1_name << ", " << to_expand2_name << ", and " << to_expand3_name << " not broadcastable,"
            << " but have the same number of elements.  Falling back to deprecated pointwise behavior.";
