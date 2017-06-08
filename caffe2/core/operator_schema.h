8e9b751315a43074d82cfd4f65141555e2467570
Luke Yeager <lukeyeager@users.noreply.github.com>
2017-06-08 13:25:32 -0700

    Avoid compiler warning about unreachable code
    Summary:
    Fix https://github.com/caffe2/caffe2/issues/764
    
    I don't think we care much about the behavior, exactly, as long as it's a loud clear crash - right?
    Closes https://github.com/caffe2/caffe2/pull/766
    
    Differential Revision: D5211826
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 9bb134b387d6620f1235a7b1ddf13092d73ae44c

--- caffe2/core/operator_schema.h
+++ caffe2/core/operator_schema.h
@@ -273,7 +273,7 @@ class OpSchema {
       };
   CostInferenceFunctionType cost_inference_function_ =
       [](const OperatorDef& def, const vector<TensorShape>&) {
-        CAFFE_THROW("No cost inference function registered.");
+        LOG(FATAL) << "No cost inference function registered.";
         return Cost();
       };
   DeviceInferenceFunctionType device_inference_function_ =
