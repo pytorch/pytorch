4b2d9f95fd7ad4721c3e8a9b653ddca651701816
Romain Cledat <romainc@fb.com>
2017-06-08 11:33:59 -0700

    Fixed thread safety issues in ImageInputOp
    Summary:
    The random number generators could be used in a thread-unsafe method.
    This patch fixes this by adding a way for tasks to get the thread ID they are
    running on.
    
    Reviewed By: panshen1
    
    Differential Revision: D5051334
    
    fbshipit-source-id: 9a9f9e2e7b7a86ff456f37b40422af4fa100b5d9

--- caffe2/utils/thread_pool.h
+++ caffe2/utils/thread_pool.h
@@ -6,10 +6,21 @@
 #include <mutex>
 #include <queue>
 #include <thread>
+#include <utility>
 
 class TaskThreadPool{
  private:
-    std::queue< std::function< void() > > tasks_;
+    struct task_element_t {
+        bool run_with_id;
+        const std::function< void() > no_id;
+        const std::function< void(std::size_t) > with_id;
+
+        explicit task_element_t(const std::function< void() >& f) :
+            run_with_id(false), no_id(f), with_id(nullptr) { }
+        explicit task_element_t(const std::function< void(std::size_t) >& f) :
+            run_with_id(true), no_id(nullptr), with_id(f) { }
+    };
+    std::queue<task_element_t> tasks_;
     std::vector<std::thread> threads_;
     std::mutex mutex_;
     std::condition_variable condition_;
@@ -26,7 +37,7 @@ class TaskThreadPool{
            available_(pool_size), total_(pool_size) {
         for ( std::size_t i = 0; i < pool_size; ++i ) {
             threads_[i] = std::thread(
-                std::bind(&TaskThreadPool::main_loop, this));
+                std::bind(&TaskThreadPool::main_loop, this, i));
         }
     }
 
@@ -55,9 +66,21 @@ class TaskThreadPool{
 
         // Set task and signal condition variable so that a worker thread will
         // wake up and use the task.
-        tasks_.push(std::function<void()>(task));
+        tasks_.push(task_element_t(static_cast<std::function< void() >>(task)));
         complete_ = false;
         condition_.notify_one();
+    }
+
+    template <typename Task>
+    void runTaskWithID(Task task) {
+      std::unique_lock<std::mutex> lock(mutex_);
+
+      // Set task and signal condition variable so that a worker thread will
+      // wake up and use the task.
+      tasks_.push(task_element_t(static_cast<std::function< void(std::size_t) >>(
+                                   task)));
+      complete_ = false;
+      condition_.notify_one();
     }
 
     /// @brief Wait for queue to be empty
@@ -69,7 +92,7 @@ class TaskThreadPool{
 
  private:
     /// @brief Entry point for pool threads.
-    void main_loop() {
+    void main_loop(std::size_t index) {
         while (running_) {
             // Wait on condition variable while the task is empty and
             // the pool is still running.
@@ -86,7 +109,7 @@ class TaskThreadPool{
             // useful in the event that the function contains
             // shared_ptr arguments bound via bind.
             {
-                std::function< void() > task = tasks_.front();
+                auto tasks = tasks_.front();
                 tasks_.pop();
                 // Decrement count, indicating thread is no longer available.
                 --available_;
@@ -95,7 +118,11 @@ class TaskThreadPool{
 
                 // Run the task.
                 try {
-                    task();
+                  if (tasks.run_with_id) {
+                      tasks.with_id(index);
+                  } else {
+                      tasks.no_id();
+                  }
                 }
                 // Suppress all exceptions.
                 catch ( const std::exception& ) {}
