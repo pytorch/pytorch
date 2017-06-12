1cc25b1399b0e5abf6860dc12e14fa6f8a1d7efe
Zachary DeVito <zdevito@fb.com>
2017-06-11 20:29:26 -0700

    remove TensorRef. Instead correctly mark const Tensor & and Tensor & in arguments depending on use.

--- aten/src/aten/templates/Tensor.h
+++ aten/src/aten/templates/Tensor.h
@@ -6,25 +6,6 @@
 
 namespace tlib {
 class Type;
-class Tensor;
-
-// This allows non-const l-value and r-value references to be passed to tensor arguments:
-// void foo(TensorRef t) {} <-- pass a tensor by reference.
-// because this is a pass by reference it avoids ref-counting costs for arguments
-// which we know will not be destroyed.
-// This is equivalent to const Tensor &, except that it allows non-const Tensors as well
-class TensorRef {
-public:
-  TensorRef(Tensor && t)
-  : tensor(t) {}
-  TensorRef(Tensor & t)
-  : tensor(t) {}
-  Tensor & operator*() { return tensor; }
-  Tensor * operator->() { return &tensor; }
-  operator Tensor& () { return tensor; }
-private:
-  Tensor & tensor;
-};
 
 struct Tensor {
 
