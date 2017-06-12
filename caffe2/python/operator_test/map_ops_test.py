0f787a01bce441999e80db8c1db764fc413916e0
Bokai Cao <caobokai@fb.com>
2017-06-12 14:42:01 -0700

    map operator (move maptrait def out of class)
    Summary: added an operator that converts key/value blobs into a blob containing a map pointer, unittest passed.
    
    Differential Revision: D5224449
    
    fbshipit-source-id: 2f60754ed3ba6ed16039c09019117ae3c3646ab2

--- caffe2/python/operator_test/map_ops_test.py
+++ caffe2/python/operator_test/map_ops_test.py
@@ -4,8 +4,9 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import numpy as np
-
+import tempfile
 import unittest
+import os
 
 from caffe2.python import core, workspace
 import caffe2.python.hypothesis_test_util as hu
@@ -16,6 +17,7 @@ class TestMap(hu.HypothesisTestCase):
     def test_map(self):
 
         def test_map_func(KEY_T, VALUE_T):
+            model_file = os.path.join(tempfile.mkdtemp(), 'db')
             key_data = np.asarray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=KEY_T)
             value_data = np.asarray([2, 3, 3, 3, 3, 2, 3, 3, 3, 3], dtype=VALUE_T)
             workspace.FeedBlob("key_data", key_data)
@@ -24,19 +26,19 @@ class TestMap(hu.HypothesisTestCase):
             save_net.KeyValueToMap(["key_data", "value_data"], "map_data")
             save_net.Save(
                 ["map_data"], [],
-                db="tmp_blob",
+                db=model_file,
                 db_type="minidb",
-                absolute_path=False
+                absolute_path=True
             )
             workspace.RunNetOnce(save_net)
             workspace.ResetWorkspace()
             load_net = core.Net("load_net")
             load_net.Load(
                 [], ["map_data"],
-                db="tmp_blob",
+                db=model_file,
                 db_type="minidb",
                 load_all=True,
-                absolute_path=False
+                absolute_path=True
             )
             load_net.MapToKeyValue("map_data", ["key_data", "value_data"])
             workspace.RunNetOnce(load_net)
