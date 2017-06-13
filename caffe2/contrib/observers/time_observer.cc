a2521148b4346f37d9876940c7e4bd77c54dc4ac
Ben Zhang <benz@fb.com>
2017-06-13 16:52:57 -0700

    TimeObserver for SimpleNet, an example usage of Observers.
    Summary: Implemented TimeObserver for SimpleNet.
    
    Reviewed By: pietern
    
    Differential Revision: D5188373
    
    fbshipit-source-id: 530d75d176aa29d38c131338c3a2be70bc221a47

---
+++ caffe2/contrib/observers/time_observer.cc
@@ -0,0 +1,41 @@
+#include "caffe2/contrib/observers/time_observer.h"
+#include "caffe2/core/logging.h"
+
+namespace caffe2 {
+
+template <>
+bool TimeObserver<SimpleNet>::Start() {
+  vector<OperatorBase*> operators = subject.getOperators();
+  for (auto& op : operators) {
+    children_.push_back(caffe2::make_unique<TimeObserver<OperatorBase>>(*op));
+  }
+  start_time_ = timer_.MilliSeconds();
+  ++iterations_;
+  return true;
+}
+
+template <>
+bool TimeObserver<SimpleNet>::Stop() {
+  double current_run = timer_.MilliSeconds() - start_time_;
+  total_time_ += current_run;
+  VLOG(1) << "This net iteration took " << current_run << " ms to complete.\n";
+  return true;
+}
+
+template <>
+bool TimeObserver<OperatorBase>::Start() {
+  start_time_ = timer_.MilliSeconds();
+  ++iterations_;
+  return true;
+}
+
+template <>
+bool TimeObserver<OperatorBase>::Stop() {
+  double current_run = timer_.MilliSeconds() - start_time_;
+  total_time_ += current_run;
+  VLOG(1) << "This operator iteration took " << current_run
+          << " ms to complete.\n";
+  return true;
+}
+
+} // namespace caffe2
---
+++ caffe2/contrib/observers/time_observer.h
@@ -0,0 +1,43 @@
+#ifndef CAFFE2_CONTRIB_OBSERVERS_TIME_OBSERVER_H_
+#define CAFFE2_CONTRIB_OBSERVERS_TIME_OBSERVER_H_
+
+#include <unordered_map>
+
+#include "caffe2/core/common.h"
+#include "caffe2/core/observer.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/core/timer.h"
+
+namespace caffe2 {
+
+template <class T>
+class TimeObserver final : public ObserverBase<T> {
+ public:
+  explicit TimeObserver<T>(T& subject) : ObserverBase<T>(subject) {}
+  inline float average_time() const {
+    return total_time_ / iterations_;
+  }
+  float average_time_children() const {
+    float sum = 0.0f;
+    for (auto& ob : children_) {
+      sum += ob.get()->average_time();
+    }
+    return sum / children_.size();
+  }
+  ~TimeObserver() {}
+
+ private:
+  Timer timer_;
+  float start_time_ = 0.0f;
+  float total_time_ = 0.0f;
+  int iterations_ = 0;
+
+  vector<unique_ptr<TimeObserver<OperatorBase>>> children_;
+
+  bool Start() override;
+  bool Stop() override;
+};
+
+} // namespace caffe2
+
+#endif // CAFFE2_CONTRIB_OBSERVERS_TIME_OBSERVER_H_
