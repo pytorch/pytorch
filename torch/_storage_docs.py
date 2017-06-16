6a11203126feb0686cbaf96febc07e04e72fff34
Leonid Vlasenkov <leo.vlasenkov@gmail.com>
2017-06-16 17:44:00 +0300

    storage docs

--- torch/_storage_docs.py
+++ torch/_storage_docs.py
@@ -7,6 +7,11 @@ from torch._C import _add_docstr as add_docstr
 storage_classes = [
     'DoubleStorageBase',
     'FloatStorageBase',
+    'LongStorageBase',
+    'IntStorageBase',
+    'ShortStorageBase',
+    'CharStorageBase',
+    'ByteStorageBase',
 ]
 
 
