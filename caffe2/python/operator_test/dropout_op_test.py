10b6bf80e32dc225117f06904a3a6a6bb59c7d0d
Simon Layton <slayton58@gmail.com>
2017-06-15 22:34:59 -0700

    Fixes for CuDNNDropoutOp
    Summary: Closes https://github.com/caffe2/caffe2/pull/809
    
    Differential Revision: D5263514
    
    Pulled By: akyrola
    
    fbshipit-source-id: 1f1e5bdb6fa551cb1f9beb3e5d3ad9c0c8813ed0

--- caffe2/python/operator_test/dropout_op_test.py
+++ caffe2/python/operator_test/dropout_op_test.py
@@ -14,10 +14,41 @@ import caffe2.python.hypothesis_test_util as hu
 
 class TestDropout(hu.HypothesisTestCase):
 
-    @given(X=hu.tensor(), in_place=st.booleans(), **hu.gcs)
-    def test_dropout_ratio0(self, X, in_place, gc, dc):
+    @given(X=hu.tensor(),
+           in_place=st.booleans(),
+           ratio=st.floats(0, 0.999),
+           engine=st.sampled_from(["", "CUDNN"]),
+           **hu.gcs)
+    def test_dropout_is_test(self, X, in_place, ratio, engine, gc, dc):
+        """Test with is_test=True for a deterministic reference impl."""
+        # TODO(lukeyeager): enable this path when the GPU path is fixed
+        if in_place:
+            # Skip if trying in-place on GPU
+            assume(not (gc.device_type == caffe2_pb2.CUDA and engine == ''))
+            # If in-place on CPU, don't compare with GPU
+            dc = dc[:1]
 
-        # TODO: enable this path when the op is fixed
+        op = core.CreateOperator("Dropout", ["X"],
+                                 ["X" if in_place else "Y", "mask"],
+                                 ratio=ratio, engine=engine, is_test=True)
+
+        self.assertDeviceChecks(dc, op, [X], [0])
+        # No sense in checking gradients for test phase
+
+        def reference_dropout_test(x):
+            return x, np.ones(x.shape, dtype=np.bool)
+        self.assertReferenceChecks(
+            gc, op, [X], reference_dropout_test,
+            # The 'mask' output may be uninitialized
+            outputs_to_check=[0])
+
+    @given(X=hu.tensor(),
+           in_place=st.booleans(),
+           engine=st.sampled_from(["", "CUDNN"]),
+           **hu.gcs)
+    def test_dropout_ratio0(self, X, in_place, engine, gc, dc):
+        """Test with ratio=0 for a deterministic reference impl."""
+        # TODO(lukeyeager): enable this path when the op is fixed
         if in_place:
             # Skip if trying in-place on GPU
             assume(gc.device_type != caffe2_pb2.CUDA)
@@ -26,9 +57,14 @@ class TestDropout(hu.HypothesisTestCase):
 
         op = core.CreateOperator("Dropout", ["X"],
                                  ["X" if in_place else "Y", "mask"],
-                                 ratio=0.0)
+                                 ratio=0.0, engine=engine)
+
         self.assertDeviceChecks(dc, op, [X], [0])
         self.assertGradientChecks(gc, op, [X], 0, [0])
+
         def reference_dropout_ratio0(x):
             return x, np.ones(x.shape, dtype=np.bool)
-        self.assertReferenceChecks(gc, op, [X], reference_dropout_ratio0)
+        self.assertReferenceChecks(
+            gc, op, [X], reference_dropout_ratio0,
+            # Don't check the mask with cuDNN because it's packed data
+            outputs_to_check=None if engine != 'CUDNN' else [0])
