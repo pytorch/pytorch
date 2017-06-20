62cfc94f445bfaeaccc3dcc1fc69ea5b75039823
Soumith Chintala <soumith@gmail.com>
2017-06-19 21:49:17 -0400

    improving TH error messages in Apply macros

--- THStorage.h
+++ THStorage.h
@@ -7,11 +7,6 @@
 #define THStorage        TH_CONCAT_3(TH,Real,Storage)
 #define THStorage_(NAME) TH_CONCAT_4(TH,Real,Storage_,NAME)
 
-#define TH_DESC_BUFF_LEN 64
-typedef struct {
-    char str[TH_DESC_BUFF_LEN];
-} THDescBuff;
-
 /* fast access methods */
 #define TH_STORAGE_GET(storage, idx) ((storage)->data[(idx)])
 #define TH_STORAGE_SET(storage, idx, value) ((storage)->data[(idx)] = (value))
