6f9859caef1a898f0e27db1ca4d191cb47dc617e
Soumith Chintala <soumith@gmail.com>
2017-06-19 21:49:17 -0400

    improving TH error messages in Apply macros

--- torch/lib/TH/THGeneral.c
+++ torch/lib/TH/THGeneral.c
@@ -358,3 +358,26 @@ TH_API void THInferNumThreads(void)
   omp_set_num_threads(mkl_get_max_threads());
 #endif
 }
+
+TH_API THDescBuff _THSizeDesc(const long *size, const long ndim) {
+  const int L = TH_DESC_BUFF_LEN;
+  THDescBuff buf;
+  char *str = buf.str;
+  int n = 0;
+  n += snprintf(str, L-n, "[");
+  int i;
+  for(i = 0; i < ndim; i++) {
+    if(n >= L) break;
+    n += snprintf(str+n, L-n, "%ld", size[i]);
+    if(i < ndim-1) {
+      n += snprintf(str+n, L-n, " x ");
+    }
+  }
+  if(n < L - 2) {
+    snprintf(str+n, L-n, "]");
+  } else {
+    snprintf(str+L-5, 5, "...]");
+  }
+  return buf;
+}
+
