f937e4bffb652e5a7f97b368b40b7adbb6e50c40
Ben Zhang <benz@fb.com>
2017-06-21 13:34:55 -0700

    Revert D5288993: Memonger Graph Equality into Memonger
    Summary: This reverts commit b9f105ce00148b2673eed2dd390ab74f82f990ad
    
    Differential Revision: D5288993
    
    fbshipit-source-id: 8f2e69c0ca21e142eb43b450d0b52ba76a5e429f

--- caffe2/python/models/resnet_test.py
+++ caffe2/python/models/resnet_test.py
@@ -77,6 +77,7 @@ class ResnetMemongerTest(hu.HypothesisTestCase):
             dont_share_blobs=set([str(param_to_grad["gpu_0/conv1_w"])]),
             blob_shapes=shapes if with_shapes else None,
         )
+        self.assertTrue(memonger.verify_graph_equality(model.net.Proto(), optim_proto))
         count_after = count_blobs(optim_proto)
         self.assertTrue(count_after < count_before)
 
@@ -138,6 +139,8 @@ class ResnetMemongerTest(hu.HypothesisTestCase):
         model.net.Proto().type = 'dag'
         model.net.Proto().num_workers = 4
         loss1 = workspace.FetchBlob("gpu_0/last_out_L1000")
+        self.assertTrue(memonger.verify_graph_equality(
+            model.net.Proto(), optim_proto))
 
         workspace.RunNetOnce(optim_proto)
         optimized_loss1 = workspace.FetchBlob("gpu_0/last_out_L1000")
