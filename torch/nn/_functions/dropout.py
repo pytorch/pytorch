e81babf7c46f9d98f9c77819b52afb3f7a1ace78
Edward Z. Yang <ezyang@fb.com>
2017-06-26 10:15:32 -0700

    Convert Dropout to new style Function.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/nn/_functions/dropout.py
+++ torch/nn/_functions/dropout.py
@@ -1,49 +1,50 @@
 import torch
+from torch.autograd import Variable
 from torch.autograd.function import InplaceFunction
 from itertools import repeat
 
 
 class Dropout(InplaceFunction):
+    @staticmethod
+    def _make_noise(input):
+        return input.new().resize_as_(input)
 
-    def __init__(self, p=0.5, train=False, inplace=False):
-        super(Dropout, self).__init__()
+    # classmethod so we can override _make_noise
+    @classmethod
+    def forward(cls, ctx, input, p=0.5, train=False, inplace=False):
         if p < 0 or p > 1:
             raise ValueError("dropout probability has to be between 0 and 1, "
                              "but got {}".format(p))
-        self.p = p
-        self.train = train
-        self.inplace = inplace
-
-    def _make_noise(self, input):
-        return input.new().resize_as_(input)
-
-    def forward(self, input):
-        if self.inplace:
-            self.mark_dirty(input)
+        ctx.p = p
+        ctx.train = train
+        if inplace:
+            ctx.mark_dirty(input)
             output = input
         else:
             output = input.clone()
 
-        if self.p > 0 and self.train:
-            self.noise = self._make_noise(input)
-            if self.p == 1:
-                self.noise.fill_(0)
+        if p > 0 and train:
+            ctx.noise = cls._make_noise(input)
+            if p == 1:
+                ctx.noise.fill_(0)
             else:
-                self.noise.bernoulli_(1 - self.p).div_(1 - self.p)
-            self.noise = self.noise.expand_as(input)
-            output.mul_(self.noise)
+                ctx.noise.bernoulli_(1 - p).div_(1 - p)
+            ctx.noise = ctx.noise.expand_as(input)
+            output.mul_(ctx.noise)
 
         return output
 
-    def backward(self, grad_output):
-        if self.p > 0 and self.train:
-            return grad_output.mul(self.noise)
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.p > 0 and ctx.train:
+            return grad_output.mul(Variable(ctx.noise)), None, None, None
         else:
-            return grad_output
+            return grad_output, None, None, None
 
 
 class FeatureDropout(Dropout):
 
-    def _make_noise(self, input):
+    @staticmethod
+    def _make_noise(input):
         return input.new().resize_(input.size(0), input.size(1),
                                    *repeat(1, input.dim() - 2))
