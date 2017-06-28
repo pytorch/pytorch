82600029416936b4bf54da38d1143cd370c3e85a
Jiyan Yang <chocjy@fb.com>
2017-06-28 00:22:21 -0700

    Partial eval layers
    Summary:
    In some cases we don't want to compute the full FC during eval.
    These layers allow us to compute dot product between
    X and W[idx,:] where idx is an input, e.g., label.
    
    Reviewed By: kittipatv
    
    Differential Revision: D5305364
    
    fbshipit-source-id: 0b6a1b61cc8fcb26c8def8bcd037a4a35d223078

--- caffe2/python/layers/__init__.py
+++ caffe2/python/layers/__init__.py
@@ -6,22 +6,28 @@ from __future__ import unicode_literals
 from importlib import import_module
 import pkgutil
 import sys
+import inspect
 from . import layers
 
 
-def import_recursive(package):
+def import_recursive(package, clsmembers):
     """
     Takes a package and imports all modules underneath it
     """
+
     pkg_dir = package.__path__
     module_location = package.__name__
-    for (module_loader, name, ispkg) in pkgutil.iter_modules(pkg_dir):
+    for (_module_loader, name, ispkg) in pkgutil.iter_modules(pkg_dir):
         module_name = "{}.{}".format(module_location, name)  # Module/package
         module = import_module(module_name)
+        clsmembers += [cls[1] for cls in inspect.getmembers(module, inspect.isclass)]
         if ispkg:
-            import_recursive(module)
+            import_recursive(module, clsmembers)
 
-import_recursive(sys.modules[__name__])
 
-for cls in layers.ModelLayer.__subclasses__():
-    layers.register_layer(cls.__name__, cls)
+clsmembers = []
+import_recursive(sys.modules[__name__], clsmembers)
+
+for cls in clsmembers:
+    if issubclass(cls, layers.ModelLayer) and cls is not layers.ModelLayer:
+        layers.register_layer(cls.__name__, cls)
