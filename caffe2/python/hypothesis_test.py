Fix `test_sparse_to_dense` precision failures
Summary: ..

Reviewed By: tomdz

Differential Revision: D5349561

fbshipit-source-id: 4c510905515eb03a64abc36f33d59a1d998c2ab1

caffe2/python/hypothesis_test.py
@@ -2164,12 +2164,12 @@ class TestOperators(hu.HypothesisTestCase):
         self.assertReferenceChecks(gc, op, Xs, unsafe_coalesce)
 
     @given(inp=_dtypes().flatmap(lambda dt: _tensor_and_indices(
-        elements=st.floats(min_value=0.5, max_value=10), dtype=dt)),
+        elements=st.floats(min_value=0, max_value=1), dtype=dt)),
         **hu.gcs_cpu_only)
     def test_sparse_to_dense(self, inp, gc, dc):
         first_dim, X, I = inp
         # values don't matter
-        D = np.random.uniform(0, 1, size=(first_dim,) + X.shape[1:])
+        D = np.zeros((first_dim,) + X.shape[1:])
 
         op = core.CreateOperator("SparseToDense", ["I", "X", "D"], ["Y"])
 