8a65ef1098bc56cbf0b5141b703bf29fb32bae08
Soumith Chintala <soumith@gmail.com>
2017-07-02 22:08:42 -0400

    cc 2.0 -> 3.0 in docs.

--- torch/__init__.py
+++ torch/__init__.py
@@ -5,7 +5,7 @@ Additionally, it provides many utilities for efficient serializing of
 Tensors and arbitrary types, and other useful utilities.
 
 It has a CUDA counterpart, that enables you to run your tensor computations
-on an NVIDIA GPU with compute capability >= 2.0.
+on an NVIDIA GPU with compute capability >= 3.0.
 """
 
 import sys
