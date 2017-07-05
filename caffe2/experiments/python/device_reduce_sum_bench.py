4fddc04054c932f4b36dea4d3ef9c5cbd5a80267
Junjie Bai <jbai@fb.com>
2017-07-05 10:40:01 -0700

    Use the same schema of switching to device reduce sum for SumSqrElements
    Summary: Based on benchmark script located at `caffe2/experiments/python/device_reduce_sum_bench.py`, device reduce sum is slower for N <= 10000, so we only switch to use device reduce for large N in SumElements. This diff applies the same schema for SumSqrElements.
    
    Reviewed By: jamesr66a
    
    Differential Revision: D5369868
    
    fbshipit-source-id: ae13a611aff9d3464d1c4950ee155c740a2da339

--- caffe2/experiments/python/device_reduce_sum_bench.py
+++ caffe2/experiments/python/device_reduce_sum_bench.py
@@ -5,10 +5,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import argparse
 import itertools
 import logging
 import os
 
+from six import add_metaclass
 import numpy as np
 
 from caffe2.python import workspace, core
@@ -18,8 +20,20 @@ logging.basicConfig()
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.INFO)
 
+ALL_BENCHMARKS = {}
 
+
+class BenchmarkMeta(type):
+    def __new__(metacls, name, bases, class_dict):
+        cls = type.__new__(metacls, name, bases, class_dict)
+        if name != 'Benchmark':
+            ALL_BENCHMARKS[name] = cls
+        return cls
+
+
+@add_metaclass(BenchmarkMeta)
 class Benchmark(object):
+
     def __init__(self):
         self.results = []
 
@@ -35,6 +49,21 @@ class SumElements(Benchmark):
     def run(self):
         op = core.CreateOperator(
             "SumElements",
+            ["X"],
+            ["y"]
+        )
+
+        for n in itertools.imap(pow, itertools.cycle([10]), range(10)):
+            X = np.random.rand(n).astype(np.float32)
+            logger.info('Running benchmark for n = {}'.format(n))
+            ret = runOpBenchmark(gpu_do, op, inputs=[X])
+            self.results.append((n, ret[1]))
+
+
+class SumSqrElements(Benchmark):
+    def run(self):
+        op = core.CreateOperator(
+            "SumSqrElements",
             ["X"],
             ["y"]
         )
@@ -63,12 +92,23 @@ class SoftMaxWithLoss(Benchmark):
                 self.results.append(((n, D), ret[1]))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(os.path.basename(__file__))
+    parser.add_argument('-b', '--benchmarks', nargs='+',
+                        default=ALL_BENCHMARKS.keys(),
+                        help='benchmarks to run (default: %(default)s))')
+    return parser.parse_args()
+
+
 def main():
-    benchmarks = [SumElements(), SoftMaxWithLoss()]
+    args = parse_args()
+
+    benchmarks = [ALL_BENCHMARKS[name]() for name in args.benchmarks]
     for bench in benchmarks:
         bench.run()
     for bench in benchmarks:
         bench.display()
+
 
 if __name__ == '__main__':
     workspace.GlobalInit(['caffe2', '--caffe2_log_level=2'])
