e001298723ee3fa99d0708c98880ba0fda8af457
Clément Godard <cgodard@fb.com>
2017-07-07 14:10:44 -0700

    added gradients for ResizeNearest (CPU + CUDA) and ref
    Summary:
    # Added the gradients of the operation for both CPU and CUDA kernels.
      # Unified variable names across all ops.
      # Added reference implementation in numpy.
      # The gradient check needs a larger stepsize to succeed, is that normal?
    
    Reviewed By: akyrola
    
    Differential Revision: D5313682
    
    fbshipit-source-id: aceb92649e01c5caeba8774e678f9095502d396c

--- caffe2/operators/resize_op.cc
+++ caffe2/operators/resize_op.cc
@@ -6,49 +6,49 @@
 namespace caffe2 {
 
 void resizeNearest2x(
-    int N,
-    int C,
-    int H,
-    int W,
+    int batch_size,
+    int num_channels,
+    int input_height,
+    int input_width,
     const float* input,
     float* output) {
-  const int outputH = H * 2;
-  const int outputW = W * 2;
-  for (int n = 0; n < N; ++n) {
-    for (int c = 0; c < C; ++c) {
-      for (int y = 0; y < outputH; ++y) {
-        const int y_in = y / 2;
+  const int output_height = input_height * 2;
+  const int output_width = input_width * 2;
+  for (int n = 0; n < batch_size; ++n) {
+    for (int c = 0; c < num_channels; ++c) {
+      for (int y = 0; y < output_height; ++y) {
+        const int in_y = y / 2;
 
 #ifdef __ARM_NEON__
-        int vecW = (W / 4) * 4; // round down
+        int vecW = (input_width / 4) * 4; // round down
         int x = 0;
         for (; x < vecW; x += 4) {
           // load 0 1 2 3
-          float32x4_t v = vld1q_f32(input + y_in * W + x);
-          const int oidx = outputW * y + x * 2;
+          float32x4_t v = vld1q_f32(input + in_y * input_width + x);
+          const int oidx = output_width * y + x * 2;
           float32x4x2_t v2 = {{v, v}};
           // store 00 11 22 33
           vst2q_f32(output + oidx + 0, v2);
         }
 
         // handle remainder
-        for (; x < W; ++x) {
-          const float v = input[y_in * W + x];
-          const int oidx = outputW * y + x * 2;
+        for (; x < input_width; ++x) {
+          const float v = input[in_y * input_width + x];
+          const int oidx = output_width * y + x * 2;
           output[oidx + 0] = v;
           output[oidx + 1] = v;
         }
 #else
-        for (int x = 0; x < W; ++x) {
-          const float v = input[y_in * W + x];
-          const int oidx = outputW * y + x * 2;
+        for (int x = 0; x < input_width; ++x) {
+          const float v = input[in_y * input_width + x];
+          const int oidx = output_width * y + x * 2;
           output[oidx + 0] = v;
           output[oidx + 1] = v;
         }
 #endif
       }
-      input += H * W;
-      output += outputH * outputW;
+      input += input_height * input_width;
+      output += output_height * output_width;
     }
   }
 }
@@ -58,31 +58,77 @@ bool ResizeNearestOp<float, CPUContext>::RunOnDevice() {
   const auto& X = Input(0);
   auto* Y = Output(0);
 
-  const int N = X.dim32(0), C = X.dim32(1), H = X.dim32(2), W = X.dim32(3);
-  int output_width = W * width_scale_;
-  int output_height = H * height_scale_;
-  Y->Resize(N, C, output_height, output_width);
+  const int batch_size = X.dim32(0),
+            num_channels = X.dim32(1),
+            input_height = X.dim32(2),
+            input_width = X.dim32(3);
+  int output_width = input_width * width_scale_;
+  int output_height = input_height * height_scale_;
+  Y->Resize(batch_size, num_channels, output_height, output_width);
 
   const float* Xdata = X.data<float>();
   float* Ydata = Y->mutable_data<float>();
 
   // Specialized implementation for fast 2x upsampling
   if (width_scale_ == 2.0 && height_scale_ == 2.0) {
-    resizeNearest2x(N, C, H, W, Xdata, Ydata);
+    resizeNearest2x(
+        batch_size, num_channels, input_height, input_width, Xdata, Ydata);
     return true;
   }
 
-  for (int n = 0; n < N; ++n) {
-    for (int c = 0; c < C; ++c) {
+  for (int n = 0; n < batch_size; ++n) {
+    for (int c = 0; c < num_channels; ++c) {
       for (int y = 0; y < output_height; ++y) {
-        const int in_y = std::min((int)(y / height_scale_), (H - 1));
+        const int in_y = std::min((int)(y / height_scale_), (input_height - 1));
         for (int x = 0; x < output_width; ++x) {
-          const int in_x = std::min((int)(x / width_scale_), (W - 1));
-          Ydata[output_width * y + x] = Xdata[in_y * W + in_x];
+          const int in_x = std::min((int)(x / width_scale_), (input_width - 1));
+          Ydata[output_width * y + x] = Xdata[input_width * in_y + in_x];
         }
       }
-      Xdata += H * W;
+      Xdata += input_height * input_width;
       Ydata += output_width * output_height;
+    }
+  }
+
+  return true;
+}
+
+template <>
+bool ResizeNearestGradientOp<float, CPUContext>::RunOnDevice() {
+  const auto& dY = Input(0);
+  const auto& X = Input(1);
+  auto* dX = Output(0);
+
+  const auto& inputDims = dY.dims();
+  CAFFE_ENFORCE_EQ(4, inputDims.size());
+  const int batch_size = dY.dim32(0),
+            num_channels = dY.dim32(1),
+            input_height = dY.dim32(2),
+            input_width = dY.dim32(3);
+  const int output_height = X.dim32(2);
+  const int output_width = X.dim32(3);
+  dX->Resize(batch_size, num_channels, output_height, output_width);
+  math::Set<float, CPUContext>(dX->size(),
+                               0.0f,
+                               dX->mutable_data<float>(),
+                               &context_);
+
+  const float* dYdata = dY.data<float>();
+  float* dXdata = dX->mutable_data<float>();
+
+  for (int n = 0; n < batch_size; ++n) {
+    for (int c = 0; c < num_channels; ++c) {
+      for (int y = 0; y < input_height; ++y) {
+        const int out_y = std::min((int)(y / height_scale_),
+                                   (output_height - 1));
+        for (int x = 0; x < input_width; ++x) {
+          const int out_x = std::min((int)(x / width_scale_),
+                                     (output_width - 1));
+          dXdata[output_width * out_y + out_x] += dYdata[input_width * y + x];
+        }
+      }
+      dYdata += input_height * input_width;
+      dXdata += output_height * output_width;
     }
   }
 
@@ -92,6 +138,8 @@ bool ResizeNearestOp<float, CPUContext>::RunOnDevice() {
 namespace {
 
 REGISTER_CPU_OPERATOR(ResizeNearest, ResizeNearestOp<float, CPUContext>);
+REGISTER_CPU_OPERATOR(ResizeNearestGradient,
+                      ResizeNearestGradientOp<float, CPUContext>);
 
 // Input: X, output: Y
 OPERATOR_SCHEMA(ResizeNearest)
@@ -108,6 +156,24 @@ OPERATOR_SCHEMA(ResizeNearest)
             )DOC")
     .Input(0, "X", "1D input tensor")
     .Output(0, "Y", "1D input tensor");
+
+// Input: dY, output: dX
+OPERATOR_SCHEMA(ResizeNearestGradient)
+    .NumInputs(2)
+    .NumOutputs(1)
+    .Arg("width_scale", "Scale along width dimension")
+    .Arg("height_scale", "Scale along height dimension");
+
+class GetResizeNearestGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef("ResizeNearestGradient",
+                             "",
+                             vector<string>{GO(0), I(0)},
+                             vector<string>{GI(0)});
+  }
+};
+REGISTER_GRADIENT(ResizeNearest, GetResizeNearestGradient);
 
 } // namespace
 } // namespace caffe2
