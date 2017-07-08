3bca2bf49805bd14f42e903dcfb2ee678cb9eee1
Carlos Garcia Jurado Suarez <carlosgjs@fb.com>
2017-07-08 10:56:43 -0700

    Add Sum operator to mobile
    Summary: Moving the Sum operator into its own file (elementwise_sum_op.cc)
    
    Reviewed By: oyvindkinsey
    
    Differential Revision: D5379274
    
    fbshipit-source-id: c504d91c9fb5e95b369f2aa7e7b5be31fd8e0d4b

---
+++ caffe2/operators/elementwise_sum_op.cc
@@ -0,0 +1,22 @@
+#include "caffe2/operators/utility_ops.h"
+
+namespace caffe2 {
+namespace {
+
+REGISTER_CPU_OPERATOR(Sum, SumOp<CPUContext>);
+
+OPERATOR_SCHEMA(Sum)
+    .NumInputs(1, INT_MAX)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .IdenticalTypeAndShapeOfInput(0)
+    .SetDoc(R"DOC(
+Element-wise sum of each of the input tensors. The first input tensor can be
+used in-place as the output tensor, in which case the sum will be done in
+place and results will be accumulated in input0. All inputs and outputs must
+have the same shape and data type.
+)DOC")
+    .Input(0, "data_0", "First of the input tensors. Can be inplace.")
+    .Output(0, "sum", "Output tensor. Same dimension as inputs.");
+}
+}
