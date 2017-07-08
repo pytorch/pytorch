5f63f5697aeb5df7106e7c490502d1255980f99d
Bangsheng Tang <bangsheng@fb.com>
2017-07-07 22:54:05 -0700

    IndexHash
    Summary:
    1. IndexHashOp
    2. Helper class SparseFeatureHash
    3. FeatureSpec changes to add desired_hash_size
    
    Reviewed By: kennyhorror
    
    Differential Revision: D5361370
    
    fbshipit-source-id: bf02e3ca12b3654f1d291f77c8af9248b6c4ac55

---
+++ caffe2/python/layers/sparse_feature_hash.py
@@ -0,0 +1,102 @@
+## @package sparse_feature_hash
+# Module caffe2.python.layers.sparse_feature_hash
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from caffe2.python import schema
+from caffe2.python.layers.layers import (
+    ModelLayer,
+    IdList,
+    IdScoreList,
+)
+
+import numpy as np
+
+
+class SparseFeatureHash(ModelLayer):
+
+    def __init__(self, model, input_record, seed,
+                 name='sparse_feature_hash', **kwargs):
+        super(SparseFeatureHash, self).__init__(model, name, input_record, **kwargs)
+
+        self.seed = seed
+        self.lengths_blob = schema.Scalar(
+            np.int32,
+            model.net.NextScopedBlob(name + "_lengths"),
+        )
+
+        if schema.equal_schemas(input_record, IdList):
+            self.modulo = self.extract_hash_size(input_record.items.metadata)
+            metadata = schema.Metadata(
+                categorical_limit=self.modulo,
+                feature_specs=input_record.items.metadata.feature_specs,
+            )
+            hashed_indices = schema.Scalar(
+                np.int64,
+                model.net.NextScopedBlob(name + "_hashed_idx")
+            )
+            hashed_indices.set_metadata(metadata)
+            self.output_schema = schema.List(
+                values=hashed_indices,
+                lengths_blob=self.lengths_blob,
+            )
+        elif schema.equal_schemas(input_record, IdScoreList):
+            self.values_blob = schema.Scalar(
+                np.float32,
+                model.net.NextScopedBlob(name + "_values"),
+            )
+            self.modulo = self.extract_hash_size(input_record.keys.metadata)
+            metadata = schema.Metadata(
+                categorical_limit=self.modulo,
+                feature_specs=input_record.keys.metadata.feature_specs,
+            )
+            hashed_indices = schema.Scalar(
+                np.int64,
+                model.net.NextScopedBlob(name + "_hashed_idx")
+            )
+            hashed_indices.set_metadata(metadata)
+            self.output_schema = schema.Map(
+                keys=hashed_indices,
+                values=self.values_blob,
+                lengths_blob=self.lengths_blob,
+            )
+        else:
+            assert False, "Input type must be one of (IdList, IdScoreList)"
+
+    def extract_hash_size(self, metadata):
+        if metadata.feature_specs and metadata.feature_specs.desired_hash_size:
+            return metadata.feature_specs.desired_hash_size
+        elif metadata.categorical_limit is not None:
+            return metadata.categorical_limit
+        else:
+            assert False, "desired_hash_size or categorical_limit must be set"
+
+    def add_ops(self, net):
+        if schema.equal_schemas(self.output_schema, IdList):
+            input_blobs = self.input_record.items.field_blobs()
+            output_blobs = self.output_schema.items.field_blobs()
+
+            net.Alias(
+                self.input_record.lengths.field_blobs(),
+                self.lengths_blob.field_blobs()
+            )
+        elif schema.equal_schemas(self.output_schema, IdScoreList):
+            input_blobs = self.input_record.keys.field_blobs()
+            output_blobs = self.output_schema.keys.field_blobs()
+
+            net.Alias(
+                self.input_record.values.field_blobs(),
+                self.values_blob.field_blobs()
+            )
+            net.Alias(
+                self.input_record.lengths.field_blobs(),
+                self.lengths_blob.field_blobs()
+            )
+        else:
+            raise NotImplementedError()
+        net.IndexHash(input_blobs,
+                      output_blobs,
+                      seed=self.seed,
+                      modulo=self.modulo)
---
+++ caffe2/python/operator_test/index_hash_ops_test.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from caffe2.python import core
+from hypothesis import given
+import caffe2.python.hypothesis_test_util as hu
+import hypothesis.strategies as st
+import numpy as np
+
+
+class TestIndexHashOps(hu.HypothesisTestCase):
+    @given(
+        indices=st.sampled_from([
+            np.int32, np.int64
+        ]).flatmap(lambda dtype: hu.tensor(min_dim=1, max_dim=1, dtype=dtype)),
+        seed=st.integers(min_value=0, max_value=10),
+        modulo=st.integers(min_value=100000, max_value=200000),
+        **hu.gcs_cpu_only
+    )
+    def test_index_hash_ops(self, indices, seed, modulo, gc, dc):
+        op = core.CreateOperator("IndexHash",
+                                 ["indices"], ["hashed_indices"],
+                                 seed=seed, modulo=modulo)
+
+        def index_hash(indices):
+            dtype = np.array(indices).dtype
+            assert dtype == np.int32 or dtype == np.int64
+            hashed_indices = []
+            for index in indices:
+                hashed = dtype.type(0xDEADBEEF * seed)
+                indices_bytes = np.array([index], dtype).view(np.int8)
+                for b in indices_bytes:
+                    hashed = dtype.type(hashed * 65537 + b)
+                hashed = (modulo + hashed % modulo) % modulo
+                hashed_indices.append(hashed)
+            return [hashed_indices]
+
+        self.assertDeviceChecks(dc, op, [indices], [0])
+        self.assertReferenceChecks(gc, op, [indices], index_hash)
