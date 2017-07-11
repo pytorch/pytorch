3bb19c33c6bcca2c288116f951eea631c5d08e52
Robert Verkuil <rverkuil@fb.com>
2017-07-10 17:47:34 -0700

    GRUUnit Op Backwards Pass
    Summary:
    Just an implementation of the forward pass of the GRU Unit Op, not the full RNNCell.
    Functions were created to mimic LSTM implementation as closely as possible.
    Backwards pass implementations are defined in GRU_unit_op.{h, cc}
    assertGradientChecks call added to gru_cell_test.py
    
    Reviewed By: salexspb
    
    Differential Revision: D5364856
    
    fbshipit-source-id: 09cff4478091827763b40cc331e4e0abf0ec258f

--- caffe2/operators/gru_unit_op.cc
+++ caffe2/operators/gru_unit_op.cc
@@ -47,6 +47,58 @@ void GRUUnit(
   }
 }
 
+template <typename T, typename Context>
+void GRUUnitGradient(
+    int N,
+    int D,
+    int t,
+    const T* H_prev,
+    const T* X,
+    const int32_t* seqLengths,
+    const T* H,
+    const T* H_diff,
+    bool drop_states,
+    T* H_prev_diff,
+    T* X_diff) {
+  for (int n = 0; n < N; ++n) {
+    const bool valid = t < seqLengths[n];
+
+    for (int d = 0; d < D; ++d) {
+      T* h_prev_diff = H_prev_diff + d;
+      T* reset_diff = X_diff + 0 * D + d;
+      T* update_diff = X_diff + 1 * D + d;
+      T* output_diff = X_diff + 2 * D + d;
+
+      if (!valid) {
+        if (drop_states) {
+          *h_prev_diff = 0;
+        } else {
+          *h_prev_diff = H_diff[d];
+        }
+        *reset_diff = 0;
+        *update_diff = 0;
+        *output_diff = 0;
+      } else {
+        // Calculate Gate Outputs
+        const T u = sigmoid(X[1 * D + d]);
+        const T o = host_tanh(X[2 * D + d]);
+
+        *h_prev_diff = H_diff[d] * u;
+        *reset_diff = 0; // 0 contribution to gradient from this operation
+        *update_diff = (H_diff[d] * H_prev[d] - H_diff[d] * o) * u * (1.0f - u);
+        *output_diff = H_diff[d] * (1.0f - u) * (1.0f - o * o);
+      }
+    }
+
+    H_prev += D;
+    X += 3 * D;
+    H += D;
+    H_diff += D;
+    X_diff += 3 * D;
+    H_prev_diff += D;
+  }
+}
+
 } // namespace detail
 
 namespace {
@@ -81,5 +133,19 @@ value at X[t][n] >= seqLengths[n].
         "len(seq_lengths) should equal batch size N.")
     .Input(3, "t", "The timestep for this operation.")
     .Output(0, "hidden", "The new GRU hidden state calculated by this op.");
+REGISTER_CPU_OPERATOR(GRUUnitGradient, GRUUnitGradientOp<float, CPUContext>);
+OPERATOR_SCHEMA(GRUUnitGradient).NumInputs(6).NumOutputs(2);
+
+class GetGRUUnitGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        "GRUUnitGradient",
+        "",
+        vector<string>{I(0), I(1), I(2), I(3), O(0), GO(0)},
+        vector<string>{GI(0), GI(1)});
+  }
+};
+REGISTER_GRADIENT(GRUUnit, GetGRUUnitGradient);
 }
-}
+} // namespace caffe2
--- caffe2/operators/gru_unit_op.h
+++ caffe2/operators/gru_unit_op.h
@@ -19,6 +19,20 @@ void GRUUnit(
     bool drop_states,
     T* H);
 
+template <typename T, typename Context>
+void GRUUnitGradient(
+    int N,
+    int D,
+    int t,
+    const T* H_prev,
+    const T* X,
+    const int32_t* seqLengths,
+    const T* H,
+    const T* H_diff,
+    bool drop_states,
+    T* H_prev_diff,
+    T* X_diff);
+
 }; // namespace detail
 
 template <typename T, typename Context>
@@ -57,6 +71,66 @@ class GRUUnitOp : public Operator<Context> {
  protected:
   INPUT_TAGS(HIDDEN_T_M_1, GATES, SEQ_LENGTHS, TIMESTEP);
   OUTPUT_TAGS(HIDDEN_T);
+
+ private:
+  bool drop_states_;
+};
+
+template <typename T, typename Context>
+class GRUUnitGradientOp : public Operator<Context> {
+ public:
+  GRUUnitGradientOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws),
+        drop_states_(OperatorBase::template GetSingleArgument<bool>(
+            "drop_states",
+            false)) {}
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+
+  bool RunOnDevice() override {
+    // Extract N
+    const auto N = Input(HIDDEN_T_M_1).dim(1);
+
+    // Gates: 1xNxG
+    const auto G = Input(GATES).dim(2);
+    const auto D = Input(HIDDEN_T_M_1).dim(2);
+
+    CAFFE_ENFORCE_EQ(3 * D, G);
+    const auto* H_prev = Input(HIDDEN_T_M_1).template data<T>();
+    const auto* X = Input(GATES).template data<T>();
+    const auto t = OperatorBase::Input<Tensor<CPUContext>>(TIMESTEP)
+                       .template data<int32_t>()[0];
+    const auto* H = Input(HIDDEN_T).template data<T>();
+    const auto* H_diff = Input(HIDDEN_T_GRAD).template data<T>();
+    const auto* seqLengths = Input(SEQ_LENGTHS).template data<int32_t>();
+    Output(HIDDEN_T_M_1_GRAD)->ResizeLike(Input(HIDDEN_T_M_1));
+    auto* H_prev_diff = Output(HIDDEN_T_M_1_GRAD)->template mutable_data<T>();
+    Output(GATES_GRAD)->ResizeLike(Input(GATES));
+    auto* X_diff = Output(GATES_GRAD)->template mutable_data<T>();
+
+    detail::GRUUnitGradient<T, Context>(
+        N,
+        D,
+        t,
+        H_prev,
+        X,
+        seqLengths,
+        H,
+        H_diff,
+        drop_states_,
+        H_prev_diff,
+        X_diff);
+    return true;
+  }
+
+ protected:
+  INPUT_TAGS(
+      HIDDEN_T_M_1,
+      GATES,
+      SEQ_LENGTHS,
+      TIMESTEP,
+      HIDDEN_T,
+      HIDDEN_T_GRAD, );
+  OUTPUT_TAGS(HIDDEN_T_M_1_GRAD, GATES_GRAD);
 
  private:
   bool drop_states_;
