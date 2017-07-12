77bfeb75656675ea6191d0135576c6e7b19c83b6
Zach DeVito <zdevito@fb.com>
2017-07-11 22:57:08 -0700

    simplify IR

--- torch/csrc/autograd/tracer.h
+++ torch/csrc/autograd/tracer.h
@@ -8,25 +8,19 @@
 namespace torch { namespace autograd {
 
 class TracingState {
-private:
-  int next_unique;
-  std::unique_ptr<LetBuilder> builder;
-  local_list params;
 public:
+  int next_unique;
+  std::unique_ptr<Graph> graph;
   TracingState()
     : next_unique(0)
-    , builder(std::unique_ptr<LetBuilder>(new LetBuilder()))
+    , graph(new Graph())
     {}
-  std::shared_ptr<Local> makeLocal();
-  void addParam(std::shared_ptr<Local> param) { params.emplace_back(param); }
-  void addBinding(local_list lvals, std::shared_ptr<Instruction> rval);
-  std::shared_ptr<Graph> graph(local_list locals);
+  std::shared_ptr<Value> makeValue();
 };
 
 // Ugh, global state
 extern std::unique_ptr<TracingState> GlobalTracingState;
 
 void Tracer_enter();
-std::shared_ptr<Graph> Tracer_exit(local_list locals);
-
+std::unique_ptr<Graph> Tracer_exit(value_list locals);
 }}
