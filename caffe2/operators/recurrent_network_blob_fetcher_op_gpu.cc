bfd0f7f305bfbdedd0c65b105e7e2945ac1246d3
Geet Sethi <gsethi@fb.com>
2017-07-17 12:37:33 -0700

    Fixed error when compiling with clang
    Summary:
    recurrent_network_blob_fetcher_op_gpu.cc was failing when compiled with clang
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: wesolwsk
    
    Differential Revision: D5436161
    
    fbshipit-source-id: f4ea31066fe5abc108c6d6c15ee92bf828a2ff96

--- caffe2/operators/recurrent_network_blob_fetcher_op_gpu.cc
+++ caffe2/operators/recurrent_network_blob_fetcher_op_gpu.cc
@@ -2,9 +2,7 @@
 #include "caffe2/operators/recurrent_network_blob_fetcher_op.h"
 
 namespace caffe2 {
-namespace {
 REGISTER_CUDA_OPERATOR(
     RecurrentNetworkBlobFetcher,
     RecurrentNetworkBlobFetcherOp<CUDAContext>);
-} // namespace
 } // namespace caffe2
