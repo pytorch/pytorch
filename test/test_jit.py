d4da716e64d1e840d0082f391d4608d9f2b07728
Edward Z. Yang <ezyang@fb.com>
2017-07-17 07:49:10 -0700

    Add a very simple visual (non-automated) test.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ test/test_jit.py
@@ -0,0 +1,27 @@
+import torch
+import torch.jit
+from torch.autograd import Variable
+from common import TestCase, run_tests
+
+
+class TestJit(TestCase):
+    def test_simple(self):
+        x = Variable(torch.Tensor([0.4]), requires_grad=True)
+        y = Variable(torch.Tensor([0.7]), requires_grad=True)
+
+        torch._C._tracer_enter((x,y))
+        z = torch.sigmoid(torch.tanh(x * (x + y)))
+        trace = torch._C._tracer_exit((z,))
+
+        # TODO: Do something more automated here
+        print(trace)
+        return
+
+        # Re-enable this when the interpreter is back
+        zs = z._execution_engine.run_forward(trace, (x, y))
+        self.assertEqual(z, zs)
+
+        # TODO: test that backwards works correctly
+
+if __name__ == '__main__':
+    run_tests()
