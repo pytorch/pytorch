583be5df5f9fbfb1b33b601ff58775e3ce10a6ab
Edward Z. Yang <ezyang@fb.com>
2017-07-18 11:05:22 -0700

    Break when an assert fails.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/jit/assert.cpp
+++ torch/csrc/jit/assert.cpp
@@ -15,6 +15,10 @@ barf(const char *fmt, ...)
   vsnprintf(msg, 2048, fmt, args);
   va_end(args);
 
+  // Convenience for gdb; helps you avoid fumbling around
+  // to figure out how to trap on throwing a C++ exception.
+  __builtin_trap();
+
   throw assert_error(msg);
 }
 
