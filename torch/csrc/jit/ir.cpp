c00e922caedb3a6c9713078cbb626f3a8bcf5ee0
Zach DeVito <zdevito@fb.com>
2017-07-19 16:23:17 -0700

    print uses in output

--- torch/csrc/jit/ir.cpp
+++ torch/csrc/jit/ir.cpp
@@ -23,16 +23,19 @@ std::string getPythonName(const PyObject* obj, bool is_legacy) {
     return THPUtils_unpackString(name.get());
   }
 }
-
+std::ostream& operator<<(std::ostream & out, Node & n) {
+  if(auto s = n.cast<Select>())
+    out << "%" << s->base()->unique() << "." << s->offset();
+  else
+    out << "%" << n.unique();
+  return out;
+}
 std::ostream& operator<<(std::ostream & out, const node_list & nodes) {
   size_t i = 0;
   for(auto n : nodes) {
     if(i++ > 0)
       out << ", ";
-    if(auto s = n->cast<Select>())
-      out << "%" << s->base()->unique() << "." << s->offset();
-    else
-      out << "%" << n->unique();
+    out << *n;
   }
   return out;
 }
@@ -49,21 +52,28 @@ std::string PythonOp::name() {
 std::ostream& operator<<(std::ostream & out, Graph & g) {
   out << "graph(" << g.inputs() << ") {\n";
   for(auto n : g.nodes()) {
-    if(!n->cast<Select>()) //improve readibility by printing selects inline
+    if(!n->cast<Select>()) { //improve readibility by printing selects inline
       out << "  %" << n->unique() << " = ";
-    IR_IF(n,PythonOp)
-      out << value->name();
-      for (auto& scalar : value->scalar_args) {
-        out << " " << scalar;
+      IR_IF(n,PythonOp)
+        out << value->name();
+        for (auto& scalar : value->scalar_args) {
+          out << " " << scalar;
+        }
+      IR_ELSEIF(SimpleMap)
+        out << value->op << "!";
+      IR_ELSE()
+        out << toString(n->kind()) << "??";
+      IR_END()
+      out << "(" << n->inputs() << "), uses = [";
+      size_t i = 0;
+      for(auto u : n->uses()) {
+        if(i++ > 0)
+          out << ", ";
+        out << *u.user << ".i" << u.offset;
       }
-      out << "(" << value->inputs() << ");\n";
-    IR_ELSEIF(Select) // print selects inline to improve readability
-    IR_ELSEIF(SimpleMap)
-      out << value->op << "!(" << value->inputs() << ");\n";
-    IR_ELSE()
-      out << toString(n->kind()) << "(" << n->inputs() << ");\n";
-    IR_END()
+      out << "];\n";
     }
+  }
   out << "  return (" << g.outputs() << ");\n}\n";
   return out;
 }
