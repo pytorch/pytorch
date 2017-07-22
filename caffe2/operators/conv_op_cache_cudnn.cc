6892b03499f780957d934986d166164b7d01c7e9
Andrew Tulloch <tulloch@fb.com>
2017-07-21 19:00:38 -0700

    bindings
    Reviewed By: Yangqing
    
    Differential Revision: D5458167
    
    fbshipit-source-id: 74b52df567e4b44977685c5b396795a0ff056682

--- caffe2/operators/conv_op_cache_cudnn.cc
+++ caffe2/operators/conv_op_cache_cudnn.cc
@@ -7,36 +7,6 @@
 
 namespace caffe2 {
 
-template <typename T>
-T AlgorithmsCache<T>::getAlgorithm(
-    const std::vector<TIndex>& vec1,
-    const std::vector<TIndex>& vec2,
-    std::function<T()> generatingFunc) {
-  int64_t seed = 0;
-  std::hash<TIndex> hashFn;
-  for (const auto num : vec1) {
-    // Copied from boost::hash_combine.
-    // Adding 1 to differentiate between first and second vector.
-    seed ^= hashFn(num) + 0x9e3779b9 + (seed << 6) + (seed >> 2) + 1;
-  }
-
-  for (const auto num : vec2) {
-    // Copied from boost::hash_combine.
-    seed ^= hashFn(num) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-  }
-
-  if (seed == 0) {
-    return generatingFunc();
-  }
-
-  if (hash_.find(seed) == hash_.end()) {
-    T value = generatingFunc();
-    hash_[seed] = value;
-  }
-
-  return hash_[seed];
-}
-
 template class AlgorithmsCache<cudnnConvolutionFwdAlgo_t>;
 template class AlgorithmsCache<cudnnConvolutionBwdFilterAlgo_t>;
 template class AlgorithmsCache<cudnnConvolutionBwdDataAlgo_t>;
