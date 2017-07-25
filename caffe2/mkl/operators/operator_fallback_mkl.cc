fec00f365f2c27b060a099101ce064d8465ec13b
Andrew Tulloch <tulloch@fb.com>
2017-07-25 14:04:29 -0700

    Fallback for MSRAFill
    Summary: TSIA
    
    Reviewed By: Yangqing
    
    Differential Revision: D5482575
    
    fbshipit-source-id: 57bcf4b980c42ca4200e8a2fab50fe5152f67501

--- caffe2/mkl/operators/operator_fallback_mkl.cc
+++ caffe2/mkl/operators/operator_fallback_mkl.cc
@@ -4,6 +4,7 @@
 #include "caffe2/operators/filler_op.h"
 #include "caffe2/operators/loss_op.h"
 #include "caffe2/operators/softmax_op.h"
+#include "caffe2/operators/utility_ops.h"
 #include "caffe2/utils/mkl/mkl_operator.h"
 
 // can add more non-MKL operators if needed
@@ -28,5 +29,8 @@ REGISTER_MKL_OPERATOR(
 REGISTER_MKL_OPERATOR(
     GaussianFill,
     mkl::MKLFallbackOp<GaussianFillOp<float, CPUContext>>);
+REGISTER_MKL_OPERATOR(
+    MSRAFill,
+    mkl::MKLFallbackOp<MSRAFillOp<float, CPUContext>>);
 
 } // namespace caffe2
