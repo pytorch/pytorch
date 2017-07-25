133dc2603ede9c602ce968e60fc8532ab65b2326
Andrew Tulloch <tulloch@fb.com>
2017-07-25 14:04:59 -0700

    Support grouped convolutions in MKL
    Reviewed By: Yangqing
    
    Differential Revision: D5487692
    
    fbshipit-source-id: 94fb66b3b104cf16dcad07743def4ea940515689

--- caffe2/python/mkl/mkl_conv_op_test.py
+++ caffe2/python/mkl/mkl_conv_op_test.py
@@ -18,16 +18,16 @@ class MKLConvTest(hu.HypothesisTestCase):
     @given(stride=st.integers(1, 3),
            pad=st.integers(0, 3),
            kernel=st.integers(3, 5),
-           size=st.integers(8, 8),
-           input_channels=st.integers(1, 3),
-           output_channels=st.integers(1, 3),
+           size=st.integers(8, 20),
+           input_channels=st.integers(1, 16),
+           output_channels=st.integers(1, 16),
            batch_size=st.integers(1, 3),
            use_bias=st.booleans(),
+           group=st.integers(1, 8),
            **mu.gcs)
-    @settings(max_examples=2, timeout=100)
     def test_mkl_convolution(self, stride, pad, kernel, size,
                              input_channels, output_channels,
-                             batch_size, use_bias, gc, dc):
+                             batch_size, use_bias, group, gc, dc):
         op = core.CreateOperator(
             "Conv",
             ["X", "w", "b"] if use_bias else ["X", "w"],
@@ -35,13 +35,14 @@ class MKLConvTest(hu.HypothesisTestCase):
             stride=stride,
             pad=pad,
             kernel=kernel,
+            group=group
         )
         X = np.random.rand(
-            batch_size, input_channels, size, size).astype(np.float32) - 0.5
+            batch_size, input_channels * group, size, size).astype(np.float32) - 0.5
         w = np.random.rand(
-                output_channels, input_channels, kernel, kernel) \
+                output_channels * group, input_channels, kernel, kernel) \
             .astype(np.float32) - 0.5
-        b = np.random.rand(output_channels).astype(np.float32) - 0.5
+        b = np.random.rand(output_channels * group).astype(np.float32) - 0.5
 
         inputs = [X, w, b] if use_bias else [X, w]
         self.assertDeviceChecks(dc, op, inputs, [0])
