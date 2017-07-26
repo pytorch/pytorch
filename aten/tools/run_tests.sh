6644aa134645dd35996b245ed6bdf890a7dae258
Zach DeVito <zdevito@fb.com>
2017-07-26 16:02:32 -0700

    add valgrind to CI

---
+++ aten/tools/run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+BUILD_ROOT=$1
+$BUILD_ROOT/src/ATen/test/basic
+$BUILD_ROOT/src/ATen/test/atest
+$BUILD_ROOT/src/ATen/test/scalar_test
+valgrind --suppressions=`dirname $0`/valgrind.sup --error-exitcode=1 $BUILD_ROOT/src/ATen/test/basic -n
