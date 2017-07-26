fix bug in bce_with_logits logsumexp calculation

test/test_nn.py
@@ -2333,7 +2333,7 @@ class TestNN(NNTestCase):
         with self.assertRaises(ValueError):
             nn.BCEWithLogitsLoss()(input, target)
 
-    def test_bce_with_logits_gives_same_result_as_sigmooid_and_bce_loss(self):
+    def test_bce_with_logits_gives_same_result_as_sigmoid_and_bce_loss(self):
         sigmoid = nn.Sigmoid()
 
         target = Variable(torch.rand(64, 4))
@@ -2343,6 +2343,15 @@ class TestNN(NNTestCase):
 
         weight = torch.rand(4)
         self.assertEqual(nn.BCEWithLogitsLoss(weight)(output, target), nn.BCELoss(weight)(sigmoid(output), target))
+
+        target = Variable(torch.FloatTensor(4, 1).fill_(0))
+        output = Variable(torch.FloatTensor(4, 1).fill_(-100))
+
+        self.assertEqual(nn.BCEWithLogitsLoss()(output, target), nn.BCELoss()(sigmoid(output), target))
+
+        weight = torch.FloatTensor(1).uniform_()
+        self.assertEqual(nn.BCEWithLogitsLoss(weight)(output, target), nn.BCELoss(weight)(sigmoid(output), target))
+
 
     def test_bce_with_logits_has_correct_grad_at_zero(self):
         output = Variable(torch.zeros(3, 1), requires_grad=True)