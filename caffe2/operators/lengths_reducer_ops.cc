96395b85b405da7d54cd8e0cd652b84196a71c08
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2017-07-25 18:17:22 -0700

    Fix SparseLengthSum undeclared schema
    Differential Revision: D5495760
    
    fbshipit-source-id: a2c9e6204021687f6df830ea2bfbe355bfc888be

--- caffe2/operators/lengths_reducer_ops.cc
+++ caffe2/operators/lengths_reducer_ops.cc
@@ -5,14 +5,18 @@
 
 namespace caffe2 {
 
-REGISTER_CPU_OPERATOR(
-    SparseLengthsSum,
+// Use _STR option because the schema is declared using _STR version too in
+// generic fashion. Otherwise it'd break schema declaration check.
+// TODO(dzhulgakov): remove _STR when all lengths ops are off generic version.
+
+REGISTER_CPU_OPERATOR_STR(
+    "SparseLengthsSum",
     CPUSparseLengthsReductionOp<float, TensorTypes<float, float16>, 0, 0>);
-REGISTER_CPU_OPERATOR(
-    SparseLengthsWeightedSum,
+REGISTER_CPU_OPERATOR_STR(
+    "SparseLengthsWeightedSum",
     CPUSparseLengthsReductionOp<float, TensorTypes<float, float16>, 1, 0>);
-REGISTER_CPU_OPERATOR(
-    SparseLengthsMean,
+REGISTER_CPU_OPERATOR_STR(
+    "SparseLengthsMean",
     CPUSparseLengthsReductionOp<float, TensorTypes<float, float16>, 0, 1>);
 
 } // namespace caffe2
