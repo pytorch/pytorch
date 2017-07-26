fb8f9de49878edd66931a0609df5b6ffb3ed937a
Soumith Chintala <soumith@gmail.com>
2017-07-26 18:48:03 -0400

    fix for ATen API Change

--- torch/csrc/DynamicTypes.cpp
+++ torch/csrc/DynamicTypes.cpp
@@ -213,14 +213,14 @@ at::Tensor createTensorAT(PyObject *data)
 {
   auto tensor_type = pytype_to_attype.at(Py_TYPE(data));
   auto tensor = ((THPVoidTensor *)data)->cdata;
-  return tensor_type->unsafeTensorFromTH(tensor);
+  return tensor_type->unsafeTensorFromTH(tensor, true);
 }
 PyObject* createPyObject(at::Tensor tensor)
 {
   auto type = getPyTypeObject(tensor);
   PyObject *obj = type->tp_alloc(type, 0);
   if (obj) {
-    ((THPVoidTensor*)obj)->cdata = (THVoidTensor *)tensor.detach()->unsafeGetTH();
+    ((THPVoidTensor*)obj)->cdata = (THVoidTensor *)tensor.detach()->unsafeGetTH(true);
   }
   return obj;
 }
