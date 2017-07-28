3016f459d259a97d641f3b852c3f8412136137f5
Edward Z. Yang <ezyang@fb.com>
2017-07-28 10:46:59 -0700

    Partial lint pass.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/jit/ir.cpp
+++ torch/csrc/jit/ir.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <stack>
 #include <sstream>
 
@@ -87,6 +88,89 @@ std::ostream& operator<<(std::ostream & out, Graph & g) {
     out << "with fusion_group_" <<i++ << " = " << fg->subgraph();
   }
   return out;
+}
+
+// These functions purposely operate on the internal members directly, to force
+// you to think about how the invariants change if you change the data
+// representation (even if the external API does not change.)
+
+void Use::lint() {
+  // Use invariants
+  // - Use is consistent with inputs
+  // - Every user node is live (in all_nodes)
+}
+
+void Node::lint() {
+  // Node invariants
+  // - nodes_iter is consistent with node
+  // - Inputs are all live (checked by topological order) and marked
+  //   as a use
+  // - Stage is consistent (stage is >= all input stages)
+  // - Owning graph is non-null and consistent
+  // - The "Select" invariant, when the node is MultiReturn
+
+  // Node subclass invariants
+  // - Return uses is zero
+  // - Param inputs is zero
+  // - Select inputs is one
+  // - Python operator cconv is correct
+
+}
+
+using node_set = std::set<Node*>;
+#define ALL_OF(container) container.begin(), container.end()
+
+void Graph::lint() {
+  // Graph invariants
+
+  // std::cout << *this << std::endl;
+
+  // nodes
+  // - nodes_ is a valid topological ordering for inputs
+  // - No repeated nodes
+  // - Params and return do NOT occur in nodes
+
+  std::unordered_set<Node*> in_scope;
+  for (auto input : inputs_) {
+    JIT_ASSERT(input->kind() == NodeKind::Param);
+    input->lint();
+    auto b = in_scope.insert(input);
+    JIT_ASSERT(b.second);
+  }
+  for (auto n : nodes_) {
+    n->lint();
+    for (auto input : n->inputs_) {
+      JIT_ASSERT(in_scope.count(input) == 1);
+    }
+    auto b = in_scope.insert(n);
+    JIT_ASSERT(b.second); // no repeated nodes
+  }
+  JIT_ASSERT(output_->kind() == NodeKind::Return);
+  output_->lint();
+  for (auto output : output_->inputs_) {
+    JIT_ASSERT(in_scope.count(output) == 1);
+  }
+
+  // all_nodes
+  // - inputs_, output_ and nodes_ are all included in all_nodes
+  // - all_nodes does not contain dead nodes??? (likely to be temporarily
+  // suspended).  Weaker: all_nodes contains all inputs and returns
+  // - only one return node???
+
+  node_set nodes_set(ALL_OF(nodes_));
+  node_set inputs_set(ALL_OF(inputs_));
+  node_set output_set{output_};
+  // This assert is currently failing
+  // JIT_ASSERT(std::includes(ALL_OF(all_nodes), ALL_OF(nodes_set)));
+  // JIT_ASSERT(std::includes(ALL_OF(all_nodes), ALL_OF(inputs_set)));
+  // JIT_ASSERT(std::includes(ALL_OF(all_nodes), ALL_OF(output_set)));
+
+  node_set all_set;
+
+  // Uniques
+  // - next_unique_ is greater than all uniques in graph
+  // - uniques in all_nodes are unique
+
 }
 
 }}
