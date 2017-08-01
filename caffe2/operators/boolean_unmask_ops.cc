3579f8de236604aeca0533e0da2ce3f11438f606
Junjie Bai <jbai@fb.com>
2017-08-01 16:46:18 -0700

    Add CUDA implementation of BooleanUnmask and fixed some bugs in the test
    Reviewed By: akyrola
    
    Differential Revision: D5405606
    
    fbshipit-source-id: fd755ee2ec3d742597f7f5500f54caa396db4da4

--- caffe2/operators/boolean_unmask_ops.cc
+++ caffe2/operators/boolean_unmask_ops.cc
@@ -1,65 +1,57 @@
+#include "caffe2/operators/boolean_unmask_ops.h"
 #include "caffe2/core/operator.h"
 #include "caffe2/core/tensor.h"
 
 namespace caffe2 {
-namespace {
 
-template <class Context>
-class BooleanUnmaskOp final : public Operator<Context> {
- public:
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  BooleanUnmaskOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<Context>(operator_def, ws) {}
+template <>
+bool BooleanUnmaskOp<CPUContext>::RunOnDevice() {
+  int maskSize = Input(0).size();
+  int numMasks = InputSize() / 2;
+  auto& valueMeta = Input(1).meta();
 
-  bool RunOnDevice() override {
-    int maskSize = Input(0).size();
-    int numMasks = InputSize() / 2;
-    auto* valuesOut = Output(0);
-    auto& valueMeta = Input(1).meta();
-    validateInput(numMasks, maskSize);
+  auto* valuesOut = Output(0);
+  valuesOut->Resize(maskSize);
+  auto* valuesOutPtr = (char*)valuesOut->raw_mutable_data(valueMeta);
 
-    valuesOut->Resize(maskSize);
-    auto* valuesOutPtr = (char*)valuesOut->raw_mutable_data(valueMeta);
-
-    std::vector<int> nextValueIndices(numMasks, 0);
-    for (int maskOffset = 0; maskOffset < maskSize; ++maskOffset) {
-      bool maskFound = false;
-      for (int maskIndex = 0; maskIndex < numMasks; ++maskIndex) {
-        auto& mask = Input(maskIndex * 2);
-        auto& values = Input(maskIndex * 2 + 1);
-        const auto* maskPtr = mask.template data<bool>();
-        const auto* valuesPtr = (char*)values.raw_data();
-        if (maskPtr[maskOffset]) {
-          auto& valueIndex = nextValueIndices[maskIndex];
-          CAFFE_ENFORCE_LT(valueIndex, values.size());
-          auto* src = valuesPtr + (valueIndex++) * valueMeta.itemsize();
-          auto* dst = valuesOutPtr + maskOffset * valueMeta.itemsize();
-          std::copy(src, src + valueMeta.itemsize(), dst);
-          maskFound = true;
-          break;
-        }
-      }
-      CAFFE_ENFORCE(maskFound);
-    }
-    // check all indices match value length
-    for (int i = 0; i < numMasks; ++i) {
-      auto& values = Input(i * 2 + 1);
-      CAFFE_ENFORCE_EQ(values.size(), nextValueIndices[i]);
-    }
-    return true;
-  }
-
- private:
-  void validateInput(int numMasks, int maskSize) {
-    for (int i = 0; i < numMasks; ++i) {
-      auto& mask = Input(2 * i);
-      auto& values = Input(2 * i + 1);
+  std::vector<int> nextValueIndices(numMasks, 0);
+  for (int maskOffset = 0; maskOffset < maskSize; ++maskOffset) {
+    bool maskFound = false;
+    for (int maskIndex = 0; maskIndex < numMasks; ++maskIndex) {
+      auto& mask = Input(maskIndex * 2);
       CAFFE_ENFORCE_EQ(mask.ndim(), 1);
       CAFFE_ENFORCE_EQ(mask.size(), maskSize);
+      const auto* maskPtr = mask.template data<bool>();
+
+      auto& values = Input(maskIndex * 2 + 1);
       CAFFE_ENFORCE_EQ(values.ndim(), 1);
+      const auto* valuesPtr = (char*)values.raw_data();
+
+      if (maskPtr[maskOffset]) {
+        auto& valueIndex = nextValueIndices[maskIndex];
+        CAFFE_ENFORCE_LT(valueIndex, values.size());
+        auto* src = valuesPtr + (valueIndex++) * valueMeta.itemsize();
+        auto* dst = valuesOutPtr + maskOffset * valueMeta.itemsize();
+        std::copy(src, src + valueMeta.itemsize(), dst);
+        maskFound = true;
+        break;
+      }
     }
+    CAFFE_ENFORCE(
+        maskFound, "All masks have False at position ", maskOffset, ".");
   }
-};
+  // check all indices match value length
+  for (int i = 0; i < numMasks; ++i) {
+    auto& values = Input(i * 2 + 1);
+    CAFFE_ENFORCE_EQ(
+        values.size(),
+        nextValueIndices[i],
+        "The number of true at mask ",
+        i,
+        " does not match the corresponding value size.");
+  }
+  return true;
+}
 
 REGISTER_CPU_OPERATOR(BooleanUnmask, BooleanUnmaskOp<CPUContext>);
 
@@ -110,5 +102,4 @@ output = 1.0, 2.0
     .Output(0, "unmasked_data", "The final reconstructed unmasked data");
 
 NO_GRADIENT(BooleanUnmask)
-}
 }
