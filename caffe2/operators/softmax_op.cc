979bc599cfff4680f48a37d0a14f29dbea98e9a5
Si Chen <sichen@fb.com>
2017-08-01 10:28:47 -0700

    Update SoftmaxOp documentation: input not necessarily 2-D
    Summary: Update SoftmaxOp documentation: input not necessarily 2-D
    
    Reviewed By: jamesr66a
    
    Differential Revision: D5535238
    
    fbshipit-source-id: 1beed3b35348737c51cd564d95e9f87e9ba0608a

--- caffe2/operators/softmax_op.cc
+++ caffe2/operators/softmax_op.cc
@@ -87,8 +87,25 @@ The operator computes the softmax normalized values for each layer in the batch
  of the given input. The input is a 2-D tensor (Tensor<float>) of size
 (batch_size x input_feature_dimensions). The output tensor has the same shape
 and contains the softmax normalized values of the corresponding input.
+
+X does not need to explicitly be a 2D vector; rather, it will be
+coerced into one. For an arbitrary n-dimensional tensor
+X \in [a_0, a_1, ..., a_{k-1}, a_k, ..., a_{n-1}] and k is
+the axis provided, then X will be coerced into a 2-dimensional tensor with
+dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}]. For the default
+case where axis=1, this means the X tensor will be coerced into a 2D tensor
+of dimensions [a_0, a_1 * ... * a_{n-1}], where a_0 is often the batch size.
+In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D.
+Each of these dimensions must be matched correctly, or else the operator
+will throw errors.
 )DOC")
-  .Input(0, "input", "The input data as 2-D Tensor<float>.")
+  .Arg("axis",
+       "(int) default to 1; describes the axis of the inputs when coerced "
+       "to 2D; defaults to one because the 0th axis most likely describes "
+       "the batch_size")
+  .Input(0, "input",
+         "The input tensor that's coerced into a 2D matrix of size (NxD) "
+         "as described above.")
   .Output(0, "output", "The softmax normalized output values with the same "
           "shape as input tensor.");
 
