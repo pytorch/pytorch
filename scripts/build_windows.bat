5d721c1c14107b68b385352bcfd45898010e864b
Xiang Wei <weixiang77@gmail.com>
2017-08-03 17:40:49 -0700

    Some adjustments for Windows build
    Summary:
    1. switch the protoc building system from msbuild to cmake
    2. set default CMAKE_GENERATE to VS2015
    3. set default CMAKE_BUILD_TYPE to Release
    4. improve error handling
    5. add the generated protobuf include path
    6. exclude many optional dependencies from build_windows.bat
    Closes https://github.com/caffe2/caffe2/pull/1014
    
    Differential Revision: D5559402
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 019e3a6c3c909154027fa932ce1d6549476b23bb

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -4,14 +4,14 @@
 
 :: This script shows how one can build a Caffe2 binary for windows.
 
-@echo on
+@echo off
 
 SET ORIGINAL_DIR=%cd%
 SET CAFFE2_ROOT=%~dp0%..
-if not exist %CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe call %CAFFE2_ROOT%\scripts\build_host_protoc.bat
 
-if not exist %CAFFE2_ROOT%\build mkdir %CAFFE2_ROOT%\build
-cd %CAFFE2_ROOT%\build
+if NOT DEFINED CMAKE_BUILD_TYPE (
+  set CMAKE_BUILD_TYPE=Release
+)
 
 if NOT DEFINED USE_CUDA (
   set USE_CUDA=OFF
@@ -34,9 +34,20 @@ if NOT DEFINED CMAKE_GENERATOR (
     )
   ) else (
     :: In default we use win64 VS 2017.
-    set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+    set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
   )
 )
+
+if not exist %CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe call %CAFFE2_ROOT%\scripts\build_host_protoc.bat || goto :label_error
+
+echo CAFFE2_ROOT=%CAFFE2_ROOT%
+echo CMAKE_GENERATOR=%CMAKE_GENERATOR%
+echo CMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
+
+if not exist %CAFFE2_ROOT%\build mkdir %CAFFE2_ROOT%\build
+cd %CAFFE2_ROOT%\build
+
+SET Protobuf_DIR=%CAFFE2_ROOT%\build_host_protoc\cmake
 
 :: Set up cmake. We will skip building the test files right now.
 :: TODO: enable cuda support.
@@ -47,10 +58,26 @@ cmake .. ^
   -DBUILD_SHARED_LIBS=OFF ^
   -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
   -DUSE_CUDA=%USE_CUDA% ^
+  -DUSE_NNPACK=OFF ^
+  -DUSE_GLOG=OFF ^
+  -DUSE_GFLAGS=OFF ^
+  -DUSE_LMDB=OFF ^
+  -DUSE_LEVELDB=OFF ^
+  -DUSE_ROCKSDB=OFF ^
+  -DUSE_OPENCV=OFF ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DBUILD_PYTHON=OFF^
   -DPROTOBUF_PROTOC_EXECUTABLE=%CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe ^
-  || exit /b
+  || goto :label_error
 
 :: Actually run the build
-cmake --build . --config %CMAKE_BUILD_TYPE% || exit /b
+cmake --build . --config %CMAKE_BUILD_TYPE% || goto :label_error
 
+echo "Caffe2 built successfully"
 cd %ORIGINAL_DIR%
+exit /b 0
+
+:label_error
+echo "Caffe2 building failed"
+cd %ORIGINAL_DIR%
+exit /b 1
