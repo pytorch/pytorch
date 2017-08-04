3787c7fcbe245d6f66e5160e41884b75f4ea56b8
Xiang Wei <weixiang77@gmail.com>
2017-08-03 17:40:49 -0700

    Some adjustments for Windows build
    Summary:
    1. switch the protoc building system from msbuild to cmake
    2. set default CMAKE_GENERATE to VS2015
    3. set default CMAKE_BUILD_TYPE to Release
    4. improve error handling
    5. add the generated protobuf include path
    6. exclude many optional dependencies from build_windows.bat
    Closes https://github.com/caffe2/caffe2/pull/1014
    
    Differential Revision: D5559402
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 019e3a6c3c909154027fa932ce1d6549476b23bb

--- scripts/build_host_protoc.bat
+++ scripts/build_host_protoc.bat
@@ -8,13 +8,17 @@
 :: After the execution of the file, one should be able to find the host protoc
 :: binary at build_host_protoc/bin/protoc.exe.
 
-@echo on
+@echo off
 
 SET ORIGINAL_DIR=%cd%
 SET CAFFE2_ROOT=%~dp0%..
+
+if NOT DEFINED CMAKE_BUILD_TYPE (
+  set CMAKE_BUILD_TYPE=Release
+)
+
 if not exist %CAFFE2_ROOT%\build_host_protoc mkdir %CAFFE2_ROOT%\build_host_protoc
 echo "Created %CAFFE2_ROOT%\build_host_protoc"
-
 cd %CAFFE2_ROOT%\build_host_protoc
 
 if NOT DEFINED CMAKE_GENERATOR (
@@ -30,18 +34,31 @@ if NOT DEFINED CMAKE_GENERATOR (
     )
   ) else (
   	:: In default we use win64 VS 2017.
-  	set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+  	set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
   )
 )
 
+echo CAFFE2_ROOT=%CAFFE2_ROOT%
+echo CMAKE_GENERATOR=%CMAKE_GENERATOR%
+echo CMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
+
+echo "Generating cmake"
 cmake ..\third_party\protobuf\cmake ^
   -G%CMAKE_GENERATOR% ^
   -DCMAKE_INSTALL_PREFIX=. ^
   -Dprotobuf_BUILD_TESTS=OFF ^
-  -DCMAKE_BUILD_TYPE=Debug ^
-  || exit /b
+  -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+  || goto :label_error
 
 :: Actually run the build
-msbuild INSTALL.vcxproj
+echo "Building protobuf"
+cmake --build . --config %CMAKE_BUILD_TYPE% --target INSTALL || goto :label_error
 
+echo "protobuf built successfully"
 cd %ORIGINAL_DIR%
+exit /b 0
+
+:label_error
+echo "protobuf building failed"
+cd %ORIGINAL_DIR%
+exit /b 1
