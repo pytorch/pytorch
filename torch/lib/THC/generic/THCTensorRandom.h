e9234e16ce36ecd6bbc47640eac4ff7b85f597a6
Trevor Killeen <killeentm@gmail.com>
2017-08-04 11:32:52 -0700

    move normal variants to TH/THC

--- torch/lib/THC/generic/THCTensorRandom.h
+++ torch/lib/THC/generic/THCTensorRandom.h
@@ -8,6 +8,9 @@ THC_API void THCTensor_(uniform)(struct THCState *state, THCTensor *self, double
 THC_API void THCTensor_(rand)(THCState *state, THCTensor *r_, THLongStorage *size);
 THC_API void THCTensor_(randn)(THCState *state, THCTensor *r_, THLongStorage *size);
 THC_API void THCTensor_(normal)(struct THCState *state, THCTensor *self, double mean, double stdv);
+THC_API void THCTensor_(normal_means)(struct THCState *state, THCTensor *self, THCTensor *means, double stddev);
+THC_API void THCTensor_(normal_stddevs)(struct THCState *state, THCTensor *self, double mean, THCTensor *stddevs);
+THC_API void THCTensor_(normal_means_stddevs)(struct THCState *state, THCTensor *self, THCTensor *means, THCTensor *stddevs);
 THC_API void THCTensor_(logNormal)(struct THCState *state, THCTensor *self, double mean, double stdv);
 THC_API void THCTensor_(exponential)(struct THCState *state, THCTensor *self, double lambda);
 THC_API void THCTensor_(cauchy)(struct THCState *state, THCTensor *self, double median, double sigma);
