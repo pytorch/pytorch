d72adbf0ed665a3f70153b9998d7650f5717893c
Yangqing Jia <jiayq@fb.com>
2017-08-08 10:10:18 -0700

    Fix metal build after sync
    Summary:
    While I was trying to make a quick oss cmakefile, I found that some of the
    ios source files are out of sync with the most code changes. This diff should
    fix the issues.
    
    I manually ran cmake on the oss side with scripts/build_ios.sh to make sure
    things pass.
    
    Reviewed By: ajtulloch
    
    Differential Revision: D5582265
    
    fbshipit-source-id: 2636d353d32fcd8fb7087385b9bbed8476e33e74

--- caffe2/contrib/ios/metal/metal_test.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-#include "caffe2/proto/caffe2.pb.h"
-
-namespace caffe2 {
-  void testMetal();
-  void compareModels(const NetDef& initNet, NetDef predictNet);
-}
