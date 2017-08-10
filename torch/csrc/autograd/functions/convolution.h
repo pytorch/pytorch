d998913859a02327d37801d4aba1e41f61274b17
Gregory Chanan <gchanan@fb.com>
2017-08-10 15:50:22 -0700

    Remove unnecessary moves in convolution autograd.

--- torch/csrc/autograd/functions/convolution.h
+++ torch/csrc/autograd/functions/convolution.h
@@ -59,9 +59,9 @@ struct ConvBackward : public Function, public ConvParams {
     , ConvParams(std::move(params))
     , convolution(std::move(convolution)) {
       if (is_executable) {
-        this->input_ = std::move(input->save(this));
-        this->weight_ = std::move(weight->save(this));
-        this->bias_ = std::move(Variable::save_opt(bias.get(), this));
+        this->input_ = input->save(this);
+        this->weight_ = weight->save(this);
+        this->bias_ = Variable::save_opt(bias.get(), this);
         this->columns = std::move(columns);
         this->ones = std::move(ones);
       }
@@ -90,10 +90,10 @@ struct ConvBackwardBackward : public Function, public ConvParams {
     : Function(std::move(flags))
     , ConvParams(std::move(params)) {
       if (is_executable) {
-        this->input_ = std::move(input->save(this));
-        this->weight_ = std::move(weight->save(this));
-        this->bias_ = std::move(Variable::save_opt(bias.get(), this));
-        this->grad_output_ = std::move(grad_output->save(this));
+        this->input_ = input->save(this);
+        this->weight_ = weight->save(this);
+        this->bias_ = Variable::save_opt(bias.get(), this);
+        this->grad_output_ = grad_output->save(this);
       }
     }
 
