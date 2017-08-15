c14fd2b570f58c6e177fd567b3f922243fc391a2
peterjc123 <peterjc123@users.noreply.github.com>
2017-08-15 22:09:25 +0300

    Win64 support for lib/THCS

--- torch/lib/THCS/generic/THCSTensor.h
+++ torch/lib/THCS/generic/THCSTensor.h
@@ -4,7 +4,7 @@
 
 typedef struct THCSTensor
 {  // Stored in COO format, indices + values
-    long *size;
+    int64_t *size;
     ptrdiff_t nnz;
     int nDimensionI; // dimension of indices
     int nDimensionV; // dimension of values
@@ -23,7 +23,7 @@ typedef struct THCSTensor
 TH_API int THCSTensor_(nDimension)(THCState *state, const THCSTensor *self);
 TH_API int THCSTensor_(nDimensionI)(THCState *state, const THCSTensor *self);
 TH_API int THCSTensor_(nDimensionV)(THCState *state, const THCSTensor *self);
-TH_API long THCSTensor_(size)(THCState *state, const THCSTensor *self, int dim);
+TH_API int64_t THCSTensor_(size)(THCState *state, const THCSTensor *self, int dim);
 TH_API ptrdiff_t THCSTensor_(nnz)(THCState *state, const THCSTensor *self);
 TH_API THLongStorage *THCSTensor_(newSizeOf)(THCState *state, THCSTensor *self);
 TH_API THCIndexTensor *THCSTensor_(newIndices)(THCState *state, const THCSTensor *self);
@@ -35,10 +35,10 @@ TH_API THCSTensor *THCSTensor_(newWithTensor)(THCState *state, THCIndexTensor *i
 TH_API THCSTensor *THCSTensor_(newWithTensorAndSize)(THCState *state, THCIndexTensor *indices, THCTensor *values, THLongStorage *sizes);
 
 TH_API THCSTensor *THCSTensor_(newWithSize)(THCState *state, THLongStorage *size_);
-TH_API THCSTensor *THCSTensor_(newWithSize1d)(THCState *state, long size0_);
-TH_API THCSTensor *THCSTensor_(newWithSize2d)(THCState *state, long size0_, long size1_);
-TH_API THCSTensor *THCSTensor_(newWithSize3d)(THCState *state, long size0_, long size1_, long size2_);
-TH_API THCSTensor *THCSTensor_(newWithSize4d)(THCState *state, long size0_, long size1_, long size2_, long size3_);
+TH_API THCSTensor *THCSTensor_(newWithSize1d)(THCState *state, int64_t size0_);
+TH_API THCSTensor *THCSTensor_(newWithSize2d)(THCState *state, int64_t size0_, int64_t size1_);
+TH_API THCSTensor *THCSTensor_(newWithSize3d)(THCState *state, int64_t size0_, int64_t size1_, int64_t size2_);
+TH_API THCSTensor *THCSTensor_(newWithSize4d)(THCState *state, int64_t size0_, int64_t size1_, int64_t size2_, int64_t size3_);
 
 TH_API THCSTensor *THCSTensor_(newClone)(THCState *state, THCSTensor *self);
 TH_API THCSTensor *THCSTensor_(newTranspose)(THCState *state, THCSTensor *self, int dimension1_, int dimension2_);
@@ -48,10 +48,10 @@ TH_API int THCSTensor_(isSameSizeAs)(THCState *state, const THCSTensor *self, co
 TH_API int THCSTensor_(isSameSizeAsDense)(THCState *state, const THCSTensor *self, const THCTensor* src);
 TH_API THCSTensor *THCSTensor_(resize)(THCState *state, THCSTensor *self, THLongStorage *size);
 TH_API THCSTensor *THCSTensor_(resizeAs)(THCState *state, THCSTensor *self, THCSTensor *src);
-TH_API THCSTensor *THCSTensor_(resize1d)(THCState *state, THCSTensor *self, long size0);
-TH_API THCSTensor *THCSTensor_(resize2d)(THCState *state, THCSTensor *self, long size0, long size1);
-TH_API THCSTensor *THCSTensor_(resize3d)(THCState *state, THCSTensor *self, long size0, long size1, long size2);
-TH_API THCSTensor *THCSTensor_(resize4d)(THCState *state, THCSTensor *self, long size0, long size1, long size2, long size3);
+TH_API THCSTensor *THCSTensor_(resize1d)(THCState *state, THCSTensor *self, int64_t size0);
+TH_API THCSTensor *THCSTensor_(resize2d)(THCState *state, THCSTensor *self, int64_t size0, int64_t size1);
+TH_API THCSTensor *THCSTensor_(resize3d)(THCState *state, THCSTensor *self, int64_t size0, int64_t size1, int64_t size2);
+TH_API THCSTensor *THCSTensor_(resize4d)(THCState *state, THCSTensor *self, int64_t size0, int64_t size1, int64_t size2, int64_t size3);
 
 TH_API THCTensor *THCSTensor_(toDense)(THCState *state, THCSTensor *self);
 TH_API void THCSTensor_(copy)(THCState *state, THCSTensor *self, THCSTensor *src);
@@ -72,8 +72,8 @@ TH_API int THCSTensor_(getDevice)(THCState *state, const THCSTensor *self);
 TH_API int THCSTensor_(checkGPU)(THCState *state, unsigned int nSparseTensors, unsigned int nTensors, ...);
 
 /* internal methods */
-TH_API void THCSTensor_(rawResize)(THCState *state, THCSTensor *self, int nDimI, int nDimV, long *size);
-TH_API THCTensor *THCSTensor_(newValuesWithSizeOf)(THCState *state, THCTensor *values, long nnz);
+TH_API void THCSTensor_(rawResize)(THCState *state, THCSTensor *self, int nDimI, int nDimV, int64_t *size);
+TH_API THCTensor *THCSTensor_(newValuesWithSizeOf)(THCState *state, THCTensor *values, int64_t nnz);
 TH_API THCSTensor* THCSTensor_(_move)(THCState *state, THCSTensor *self, THCIndexTensor *indices, THCTensor *values);
 TH_API THCSTensor* THCSTensor_(_set)(THCState *state, THCSTensor *self, THCIndexTensor *indices, THCTensor *values);
 TH_API THCIndexTensor* THCSTensor_(newFlattenedIndices)(THCState *state, THCSTensor *self);
