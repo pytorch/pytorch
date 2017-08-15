4a47eafa32fd5016a967c127e2f4556084d86180
Edward Z. Yang <ezyang@mit.edu>
2017-08-15 13:37:04 -0400

    Make autograd engine reentrant (#37)

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1464,6 +1464,27 @@ class TestAutograd(TestCase):
         keepdim_check(torch.std)
         torch.utils.backcompat.keepdim_warning.enabled = False
 
+    def test_reentrant(self):
+        y_data = torch.randn(2, 2)
+
+        class Reenter(Function):
+            @staticmethod
+            def forward(ctx, x_data):
+                ctx.x = Variable(x_data, requires_grad=True)
+                ctx.y = Variable(y_data, requires_grad=True)
+                ctx.output_var = ctx.x * ctx.y
+                return ctx.output_var.data
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                ctx.output_var.sum().backward()
+                return ctx.x.grad * grad_output
+
+        x = Variable(torch.randn(2, 2), requires_grad=True)
+        out = Reenter.apply(x)
+        out.sum().backward()
+        self.assertEqual(x.grad.data, y_data)
+
 
 def index_variable(shape, max_indices):
     if not isinstance(shape, tuple):
