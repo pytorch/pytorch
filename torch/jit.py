890c2071f053b3e1fe42c5b71f1737e128265112
Adam Paszke <adam.paszke@gmail.com>
2017-08-15 09:49:28 -0700

    PR comments

--- torch/jit.py
+++ torch/jit.py
@@ -1,4 +1,4 @@
-import torch.autograd.function as F
+import torch.autograd.function as function
 import torch._C
 from torch.autograd import Variable
 from torch.nn import Module
@@ -14,7 +14,7 @@ import os
 
 
 def flatten(x):
-    return tuple(F._iter_variables(x))
+    return tuple(function._iter_variables(x))
 
 
 @contextlib.contextmanager
@@ -63,30 +63,20 @@ def _time(name, enabled=True):
 
 
 def _clone_inputs(all_args):
-    # It doesn't matter that we throw away flags, because
-    # we're not going to try to do backwards on the trace output.
-    return tuple(Variable(a.data.clone()) for a in all_args)
+    return tuple(Variable(a.data.clone(), requires_grad=a.requires_grad, volatile=a.volatile) for a in all_args)
 
 
 def _verify(flat_trace_out, flat_real_out):
     # test for equality
     for x, y in zip(flat_trace_out, flat_real_out):
-        if isinstance(x, Variable) and isinstance(y, Variable):
-            max_diff = torch.max(torch.abs(x.data - y.data))
-            print("max diff: ", max_diff)
-            if max_diff > 1e-6:
-                print(x)
-                print(y)
-                raise "JIT and real computation mismatch"
-        else:
-            print(x)
-            print(y)
-            raise "Output is not variables"
+        if not (isinstance(x, Variable) and isinstance(y, Variable)):
+            raise RuntimeError("non-Variable output")
+        if x.data.sub(y.data).abs().max() > 1e-6:
+            raise RuntimeError("JIT and real computation mismatch")
+
 
 # holds run() to run the function and self.inputs which
 # are all the variable inputs
-
-
 class Traceable(object):
     _next_trace_id = 0
     _dump_traces = os.environ.get('PYTORCH_JIT_DUMP', False)
@@ -100,11 +90,10 @@ class Traceable(object):
         """
 
         if isinstance(function_or_module, Module):
-            real_forward = function_or_module.forward
-            self._run = lambda args: real_forward(*args)
+            self._run = function_or_module.forward
             self._additional_inputs = lambda: function_or_module.parameters()
         else:
-            self._run = lambda args: function_or_module(*args)
+            self._run = function_or_module
             self._additional_inputs = lambda: ()
 
         self.trace_name = trace_name
@@ -118,70 +107,71 @@ class Traceable(object):
             self.trace_name = "trace_{}".format(Traceable._next_trace_id)
             Traceable._next_trace_id += 1
 
-    def _run_pass(self, name, p, trace):
+    def _run_pass(self, p, suffix=''):
+        name = p.__doc__
         if Traceable._dump_traces:
-            with open("{}_{}_input.ir".format(self.trace_name, name), "w") as file:
-                file.write(str(trace))
-        p(trace)
-        torch._C._jit_pass_lint(trace)
+            with open("{}_{}_input.ir".format(self.trace_name, name), "w") as f:
+                f.write(str(self.saved_trace))
+        p(self.saved_trace)
+        torch._C._jit_pass_lint(self.saved_trace)
         if Traceable._dump_traces:
-            with open("{}_{}_output.ir".format(self.trace_name, name), "w") as file:
-                file.write(str(trace))
+            with open("{}_{}_output.ir".format(self.trace_name, name), "w") as f:
+                f.write(str(self.saved_trace))
 
-    def run_trace(self, all_inputs):
+    def run_trace(self, trace_inputs):
         if self.saved_closure is None:
             self.saved_closure = torch._C._jit_createAutogradClosure(
                 self.saved_trace)
         with _time("run_trace", self.time):
             assert(self.saved_closure is not None)
             return Variable._execution_engine.run_forward(
-                self.saved_closure, all_inputs)
+                self.saved_closure, trace_inputs)
 
-    def get_all_inputs(self, args, extra):
+    def get_trace_inputs(self, args, extra):
         return tuple(x for x in itertools.chain(self._additional_inputs(), flatten(args), extra))
 
     # create and return a trace, possibly verifying it before returning it
     def record_trace(self, args, extra):
-        all_inputs = self.get_all_inputs(args, extra)
+        trace_inputs = self.get_trace_inputs(args, extra)
         if self.verify:
-            cloned_inputs = _clone_inputs(all_inputs)
+            cloned_inputs = _clone_inputs(trace_inputs)
         with _time("record_trace", self.time), _fork_rng(self.verify):
-            self.saved_trace, inputs = torch._C._tracer_enter(all_inputs)
-            out = self._run(args)
+            self.saved_trace, inputs = torch._C._tracer_enter(trace_inputs)
+            out = self._run(*args)
             flat_out = torch._C._tracer_exit(flatten(out))
 
         torch._C._jit_pass_lint(self.saved_trace)
         if self.optimize:
-            self._run_pass("init", torch._C._jit_pass_init, self.saved_trace)
-            self._run_pass("fuse", torch._C._jit_pass_fuse, self.saved_trace)
+            self._run_pass("init", torch._C._jit_pass_init)
+            self._run_pass("fuse", torch._C._jit_pass_fuse)
 
         if self.verify:
             flat_trace_out = self.run_trace(cloned_inputs)
             _verify(flat_trace_out, flat_out)
 
-        return self.saved_trace, F._unflatten(flat_out, out)
+        return self.saved_trace, function._unflatten(flat_out, out)
 
     def run(self, args, extra):
         # tracing is disabled, run the real thing, possibly timing it
         if not self.enabled:
             with _time("run_real", self.time):
-                return self._run(args)
+                return self._run(*args)
         # tracing, but no trace exists, create one, possibly verifying it
         # by running it after creating it
         if self.saved_trace is None:
             _, out = self.record_trace(args, extra)
-            self.proto = F._to_proto(out)
+            self.proto = function._to_proto(out)
             return out
-        all_inputs = self.get_all_inputs(args, extra)
+        trace_inputs = self.get_trace_inputs(args, extra)
 
         # just run the already created trace
         if not self.verify:
-            return F._unflatten(self.run_trace(all_inputs), self.proto)
+            return function._unflatten(self.run_trace(trace_inputs), self.proto)
 
         # verify an already created trace...
-        cloned_inputs = _clone_inputs(all_inputs)
+        cloned_inputs = _clone_inputs(trace_inputs)
         with _time("run_real", self.time), _fork_rng():
-            out_real = self._run(args)
+            out_real = self._run(*args)
         flat_trace_out = self.run_trace(cloned_inputs)
         _verify(flat_trace_out, flatten(out_real))
         return out_real
@@ -193,16 +183,16 @@ def record_trace(traceable, *args, **kwargs):
         args, parameters)
 
 
-def traced(traceable, **kwargs):
-    t = Traceable(traceable, **kwargs)
+def traced(traceable, **traced_kwargs):
+    parameters = traced_kwargs.pop('parameters', ())
+    t = Traceable(traceable, **traced_kwargs)
     if isinstance(traceable, Module):
         def forward(self, *args):
             return t.run(args, ())
         traceable.forward = types.MethodType(forward, traceable)
         return traceable
     else:
-        return lambda *args, **kwargs: \
-            t.run(args, kwargs.get('parameters', ()))
+        return lambda *args: t.run(args, traced_kwargs.get('parameters', ()))
 
 
 if not torch._C._jit_init():
