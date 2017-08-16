6138cb888fefd41248781bb02bf02579c58aabde
Priya Goyal <prigoyal@devgpu205.prn2.facebook.com>
2017-08-15 18:12:30 -0700

    SqueezeNet traces

--- test/test_jit.py
+++ test/test_jit.py
@@ -737,6 +737,116 @@ class TestJit(TestCase):
         self.assertExpected(str(trace), "3")
         # self.assertExpected(torch._C._jit_pass_export(trace), "3-pbtxt")
 
+    def test_squeezenet(self):
+        inplace = False
+
+        class Fire(nn.Module):
+
+            def __init__(self, inplanes, squeeze_planes,
+                         expand1x1_planes, expand3x3_planes):
+                super(Fire, self).__init__()
+                self.inplanes = inplanes
+                self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+                self.squeeze_activation = nn.ReLU(inplace=inplace)
+                self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
+                                           kernel_size=1)
+                self.expand1x1_activation = nn.ReLU(inplace=inplace)
+                self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
+                                           kernel_size=3, padding=1)
+                self.expand3x3_activation = nn.ReLU(inplace=inplace)
+
+            def forward(self, x):
+                x = self.squeeze_activation(self.squeeze(x))
+                return torch.cat([
+                    self.expand1x1_activation(self.expand1x1(x)),
+                    self.expand3x3_activation(self.expand3x3(x))
+                ], 1)
+
+
+        class SqueezeNet(nn.Module):
+
+            def __init__(self, version=1.0, num_classes=1000):
+                super(SqueezeNet, self).__init__()
+                if version not in [1.0, 1.1]:
+                    raise ValueError("Unsupported SqueezeNet version {version}:"
+                                     "1.0 or 1.1 expected".format(version=version))
+                self.num_classes = num_classes
+                if version == 1.0:
+                    self.features = nn.Sequential(
+                        nn.Conv2d(3, 96, kernel_size=7, stride=2),
+                        nn.ReLU(inplace=inplace),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(96, 16, 64, 64),
+                        Fire(128, 16, 64, 64),
+                        Fire(128, 32, 128, 128),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(256, 32, 128, 128),
+                        Fire(256, 48, 192, 192),
+                        Fire(384, 48, 192, 192),
+                        Fire(384, 64, 256, 256),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(512, 64, 256, 256),
+                    )
+                else:
+                    self.features = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size=3, stride=2),
+                        nn.ReLU(inplace=inplace),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(64, 16, 64, 64),
+                        Fire(128, 16, 64, 64),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(128, 32, 128, 128),
+                        Fire(256, 32, 128, 128),
+                        nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                        Fire(256, 48, 192, 192),
+                        Fire(384, 48, 192, 192),
+                        Fire(384, 64, 256, 256),
+                        Fire(512, 64, 256, 256),
+                    )
+                # Final convolution is initialized differently form the rest
+                final_conv = nn.Conv2d(512, num_classes, kernel_size=1)
+                self.classifier = nn.Sequential(
+                    nn.Dropout(p=0.5),
+                    final_conv,
+                    nn.ReLU(inplace=inplace),
+                    nn.AvgPool2d(13)
+                )
+
+                for m in self.modules():
+                    if isinstance(m, nn.Conv2d):
+                        gain = 2.0
+                        if m is final_conv:
+                            m.weight.data.normal_(0, 0.01)
+                        else:
+                            fan_in = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+                            u = math.sqrt(3.0 * gain / fan_in)
+                            m.weight.data.uniform_(-u, u)
+                        if m.bias is not None:
+                            m.bias.data.zero_()
+
+            def forward(self, x):
+                x = self.features(x)
+                x = self.classifier(x)
+                return x.view(x.size(0), self.num_classes)
+
+        # SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and
+        # <0.5MB model size
+        x = Variable(torch.randn(10, 3, 224, 224).fill_(1.0), requires_grad=True)
+        sqnet_v1_0 = SqueezeNet(version=1.1)
+        trace, _ = torch.jit.record_trace(sqnet_v1_0, x)
+        print(str(trace))
+        self.assertExpected(str(trace), "1_0")
+        # self.assertExpected(torch._C._jit_pass_export(trace), "1_0-pbtxt")
+
+        # SqueezeNet 1.1 has 2.4x less computation and slightly fewer parameters
+        # than SqueezeNet 1.0, without sacrificing accuracy.
+        x = Variable(torch.randn(10, 3, 224, 224).fill_(1.0), requires_grad=True)
+        sqnet_v1_1 = SqueezeNet(version=1.1)
+        trace, _ = torch.jit.record_trace(sqnet_v1_1, x)
+        print(str(trace))
+        self.assertExpected(str(trace), "1_1")
+        # self.assertExpected(torch._C._jit_pass_export(trace), "1_1-pbtxt")
+
     def test_autograd_closure(self):
         a = x = Variable(torch.Tensor([0.4]), requires_grad=True)
         b = y = Variable(torch.Tensor([0.7]), requires_grad=True)
