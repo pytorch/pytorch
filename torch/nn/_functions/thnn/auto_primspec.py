f805c379ba30ac15330f2adfd419c1af00412897
Priya Goyal <priy2201@gmail.com>
2017-08-16 18:29:20 -0700

    premspec for leaky_relu (#70)

--- torch/nn/_functions/thnn/auto_primspec.py
+++ torch/nn/_functions/thnn/auto_primspec.py
@@ -7,6 +7,14 @@ def threshold_primspec(input, threshold=0, value=0, inplace=False):
     if threshold == 0 and value == 0:
         return torch.toffee.op("Relu", input)
 
+
+def leakyrelu_primspec(input, negative_slope, inplace=False):
+    if inplace:
+        return None
+    return torch.toffee.op("LeakyRelu", input, alpha=float(negative_slope))
+
+
 primspec_fns = {
     'Threshold': threshold_primspec,
+    'LeakyReLU': leakyrelu_primspec,
 }
