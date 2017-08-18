ff1b93d1d76409a65128b9b661f869f6c61f60db
WarBean <zhhuab123456@gmail.com>
2017-08-18 10:29:00 +0800

    add last_step_indices, last_step_tensor to PackedSequence

--- torch/nn/utils/rnn.py
+++ torch/nn/utils/rnn.py
@@ -20,7 +20,45 @@ class PackedSequence(PackedSequence_):
         batch_sizes (list[int]): list of integers holding information about
             the batch size at each sequence step
     """
-    pass
+
+    def last_step_indices(self):
+        """A helper function for :func:`last_step_tensor`.
+
+        The returned indices is used to select the last step of a :class:`PackedSequence` object.
+
+        Arguments:
+
+        Returns:
+            List of int containing the indices of the last step of each sequence.
+        """
+        indices = []
+        sum_batch_size = 0
+        for i, batch_size in enumerate(self.batch_sizes):
+            sum_batch_size += batch_size
+            if i == len(self.batch_sizes) - 1:
+                indices.append(sum_batch_size - 1)
+            elif batch_size > self.batch_sizes[i + 1]:
+                for j in range(batch_size - self.batch_sizes[i + 1]):
+                    indices.append(sum_batch_size - 1 - j)
+        return indices[::-1]
+
+    def last_step_tensor(self):
+        """Extract the last step of each sequence of a :class:`PackedSequence` object.
+
+        It is useful for rnn's output.
+
+        The returned Variable's data will be of size Bx*, where B is the batch size.
+
+        Arguments:
+
+        Returns:
+            Variable containing the last step of each sequence in the batch.
+        """
+        indices = Variable(torch.LongTensor(self.last_step_indices()))
+        if self.data.data.is_cuda:
+            indices = indices.cuda(self.data.data.get_device())
+        last_step = self.data.index_select(0, indices)
+        return last_step
 
 
 def pack_padded_sequence(input, lengths, batch_first=False):
@@ -124,40 +162,3 @@ def pad_packed_sequence(sequence, batch_first=False):
     if batch_first:
         output = output.transpose(0, 1)
     return output, lengths
-
-def get_last_step_indices(lengths):
-    """A helper function for :func:`get_last_step_tensor`.
-
-    The returned indices is used to select the last step of a :class:`PackedSequence` object.
-
-    Arguments:
-        lengths (list[int]): list of sequences lengths of each batch element.
-
-    Returns:
-        List of int containing the indices of the last step of each sequence.
-    """
-    indices = []
-    for i, length in enumerate(lengths):
-        index = sum([min(length - 1, l) for l in lengths]) + i
-        indices.append(index)
-    return indices
-
-def get_last_step_tensor(packed_sequence, lengths):
-    """Extract the last step of each sequence of a :class:`PackedSequence` object.
-
-    It is useful for rnn's output.
-
-    The returned Variable's data will be of size Bx*, where B is the batch size.
-
-    Arguments:
-        packed_sequence (PackedSequence): batch of sequences to extract last step
-        lengths (list[int]): list of sequences lengths of each batch element.
-
-    Returns:
-        Variable containing the last step of each sequence in the batch.
-    """
-    indices = Variable(torch.LongTensor(get_last_step_indices(lengths)))
-    if packed_sequence.data.data.is_cuda:
-        indices = indices.cuda(packed_sequence.data.data.get_device())
-    last_step = packed_sequence.data.index_select(0, indices)
-    return last_step
