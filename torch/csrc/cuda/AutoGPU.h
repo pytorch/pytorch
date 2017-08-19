166e27ca52c1818fe5f819f1ddcfcf99abfef4a5
peter <peter_jiachen@163.com>
2017-08-19 23:08:31 +0800

    Fix build errors with Windows GPU in csrc

--- torch/csrc/cuda/AutoGPU.h
+++ torch/csrc/cuda/AutoGPU.h
@@ -2,9 +2,14 @@
 #define THCP_AUTOGPU_INC
 
 #include <Python.h>
+#include "THP.h"
 #include "torch/csrc/utils/auto_gpu.h"
 
+#ifdef WITH_CUDA
 class THP_CLASS THCPAutoGPU : public AutoGPU {
+#else
+class THCPAutoGPU : public AutoGPU {
+#endif
 public:
   explicit THCPAutoGPU(int device_id=-1);
   THCPAutoGPU(PyObject *args, PyObject *self=NULL);
