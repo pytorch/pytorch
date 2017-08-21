8dab0237e249ba955e3e1af4ca598522bb5ef8d7
Adam Paszke <adam.paszke@gmail.com>
2017-08-21 07:32:33 -0700

    Maintain Select-node invariant in DCE

--- torch/csrc/jit/dead_code_elimination.cpp
+++ torch/csrc/jit/dead_code_elimination.cpp
@@ -2,11 +2,31 @@
 
 namespace torch { namespace jit {
 
+namespace {
+
+bool allSelectsUnused(Node* node) {
+  for (auto & use : node->uses()) {
+    if (use.user->uses().size() != 0)
+      return false;
+  }
+  return true;
+}
+
+} // anonymous namespace
+
 void EliminateDeadCode(std::unique_ptr<Graph>& graph) {
   auto nodes = graph->nodes().reverse();
-  for(auto it = nodes.begin(); it != nodes.end(); it++) {
+  for (auto it = nodes.begin(); it != nodes.end(); it++) {
     auto node = *it;
-    if(node->uses().size() == 0) {
+    if (node->kind() == kSelect)
+      continue;
+
+    if (node->hasMultipleOutputs() && allSelectsUnused(node)) {
+      auto uses = node->uses(); // A copy so we don't modify it within a range loop
+      for (auto & use : uses)
+        use.user->destroy();
+      it.destroyCurrent();
+    } else if (node->uses().size() == 0) {
       it.destroyCurrent();
     }
   }
