4a038a739d2a80b97120df56967f58ba01f0dd51
Christopher Hay <haychris@fb.com>
2017-08-24 10:10:58 -0700

    Added norm-based gradient clipping to optimizer library
    Summary: Moved code for global norm-based gradient clipping from fb specific workflows (seq2seq) to the open-source caffe2 optimizer library
    
    Reviewed By: jhcross
    
    Differential Revision: D5637453
    
    fbshipit-source-id: 7e73c9a1c97c28a152c188467b27a6449f79242e

--- caffe2/python/optimizer_test_util.py
+++ caffe2/python/optimizer_test_util.py
@@ -148,3 +148,65 @@ class OptimizerTestBase(object):
                 atol=1e-2
             )
         self.check_optimizer(optimizer)
+
+
+class GradientClippingTestBase(object):
+    """
+    This is an abstract base class.
+    Don't inherit from unittest.TestCase, and don't name it 'Test*'.
+    Do, however, do these things in classes which inherit from this.
+    """
+
+    def _gradient_ratio_reference(self, model, params, max_gradient_norm):
+        from caffe2.python import core
+        sum_squared_norms = 0.0
+        for param in params:
+            grad = (
+                model.param_to_grad[param]
+                if not isinstance(
+                    model.param_to_grad[param],
+                    core.GradientSlice,
+                ) else model.param_to_grad[param].values
+            )
+            val = workspace.FetchBlob(grad)
+            sum_squared_norms += np.power(np.linalg.norm(val), 2.0)
+        global_norm = np.sqrt(sum_squared_norms)
+        clip_norm = max_gradient_norm
+        norm_ratio = clip_norm / np.maximum(clip_norm, global_norm)
+        return norm_ratio
+
+    def test_global_norm_based_gradient_clipping(self):
+        max_gradient_norm = 1
+        model, perfect_model, data, label = self._createDense()
+        self.build_optimizer(model, max_gradient_norm=max_gradient_norm)
+
+        params = []
+        for param in model.GetParams(top_scope=True):
+            if param in model.param_to_grad:
+                if not isinstance(
+                    model.param_to_grad[param],
+                    core.GradientSlice,
+                ):
+                    params.append(param)
+
+        workspace.FeedBlob('data', data[0])
+        workspace.FeedBlob('label', label[0])
+        workspace.RunNetOnce(model.param_init_net)
+        workspace.CreateNet(model.net, True)
+        for _ in range(1):
+            idx = np.random.randint(data.shape[0])
+            workspace.FeedBlob('data', data[idx])
+            workspace.FeedBlob('label', label[idx])
+            workspace.RunNet(model.net.Proto().name)
+
+        reference = self._gradient_ratio_reference(
+            model,
+            params,
+            max_gradient_norm,
+        )
+        norm_ratio = workspace.FetchBlob(
+            'norm_clipped_grad_update/norm_ratio')
+        np.testing.assert_almost_equal(norm_ratio, reference)
+        self.assertTrue(
+            norm_ratio < 1.0, "Bad test, gradient not being scaled."
+        )
