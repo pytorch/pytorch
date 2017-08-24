55ac596ea9af986553475aa059d24af4631e85a0
Edward Z. Yang <ezyang@fb.com>
2017-08-24 14:18:36 -0700

    Faster tensor serialization.
    Instead of dynamically allocating a float for each element of the tensor
    (lol!) save the tensor itself, and directly read out the data.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/toffee.cpp
+++ torch/csrc/toffee.cpp
@@ -23,6 +23,24 @@ bool micropb_encode<double, nullptr>(pb_ostream_t *stream, double* arg) {
   return pb_encode_fixed64(stream, static_cast<void*>(arg));
 }
 
+// TODO: I'm not entirely sure why this can't be in the header...
+bool micropb_callback_tensor(pb_ostream_t *stream, const pb_field_t *field, void * const *arg) {
+  at::Tensor* t = static_cast<at::Tensor*>(*arg);
+  JIT_ASSERT(t->type().scalarType() == at::kFloat);
+  JIT_ASSERT(t->is_contiguous());
+  // TODO: Generalize beyond float
+  // Packed array format!
+  pb_encode_tag(stream, PB_WT_STRING, toffee_TensorProto_float_data_tag);
+  static_assert(sizeof(float) == 4, "float is not four bytes");
+  pb_encode_varint(stream, sizeof(float) * t->numel()); // number of bytes to write
+  // TODO: If you have a better way of doing this, I'm all ears.
+  float *addr = t->data<float>();
+  for (float *p = addr; p < addr + t->numel(); p++) {
+    if (!pb_encode_fixed32(stream, static_cast<void*>(p))) return false;
+  }
+  return true;
+}
+
 GraphProto* AttributeProto::add_graphs() {
   auto ptr = new GraphProto();
   graphs.emplace_back(ptr);
