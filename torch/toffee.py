ff77906e449d37a226852b58f5859fb5869fd3b6
Priya Goyal <prigoyal@devgpu205.prn2.facebook.com>
2017-08-28 14:34:04 -0700

    Refactor the user facing e2e test API - hide trace

--- torch/toffee.py
+++ torch/toffee.py
@@ -1,3 +1,6 @@
+import torch
+
+
 def op(op_name, *args, **kwargs):
     """A primitive operator
 
@@ -7,3 +10,15 @@ def op(op_name, *args, **kwargs):
     trace correctly).  DO NOT rely on this returning a dictionary!!!
     """
     return {"name": op_name, "inputs": args, "attrs": kwargs}
+
+
+def export(model, input, embed_params):
+
+    # Enable tracing on the model
+    trace, torch_out = torch.jit.record_trace(model, input)
+    if embed_params is False:
+        proto = trace.export()
+    else:
+        proto = trace.export(model.state_dict().values())
+    # TODO: a way to print the proto
+    return proto, torch_out
