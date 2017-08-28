898f3f398c8a3ceac5a88092f587bb82aefe0814
Andrew Tulloch <tulloch@fb.com>
2017-08-28 00:19:24 -0700

    Use gemmlowp-based worker pool (spinning + #threads of blocks of work) instead of custom work-stealing impl
    Reviewed By: Yangqing
    
    Differential Revision: D5696841
    
    fbshipit-source-id: 84b629d2c1ebd418c75d5da907799e580cc59d1e

--- caffe2/utils/threadpool/ThreadPool.cc
+++ caffe2/utils/threadpool/ThreadPool.cc
@@ -1,14 +1,13 @@
 #include "caffe2/utils/threadpool/ThreadPool.h"
+#include "WorkersPool.h"
 #include "caffe2/core/logging.h"
 
 #if CAFFE2_ANDROID
 #include <cpu-features.h>
 #endif
 
-CAFFE2_DEFINE_bool(
-    caffe2_threadpool_force_inline,
-    false,
-    "Force to always run jobs on the calling thread");
+CAFFE2_DEFINE_bool(caffe2_threadpool_force_inline, false,
+                   "Force to always run jobs on the calling thread");
 
 // Whether or not threadpool caps apply to Android
 CAFFE2_DEFINE_int(caffe2_threadpool_android_cap, true, "");
@@ -26,10 +25,6 @@ namespace caffe2 {
 constexpr size_t kDefaultMinWorkSize = 8;
 #else
 constexpr size_t kDefaultMinWorkSize = 80;
-#endif
-
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-constexpr float kDefaultImbalanceRatio = 1.0f;
 #endif
 
 std::unique_ptr<ThreadPool> ThreadPool::defaultThreadPool() {
@@ -65,8 +60,8 @@ std::unique_ptr<ThreadPool> ThreadPool::defaultThreadPool() {
     if (numThreads <= 3) {
       // no change
     } else if (numThreads <= 5) {
-      // limit to 2
-      numThreads = 2;
+      // limit to 4 - seems to work well for 2+2, 4+1, 4+0.
+      numThreads = 4;
     } else {
       // Use half the cores
       numThreads = numThreads / 2;
@@ -77,53 +72,14 @@ std::unique_ptr<ThreadPool> ThreadPool::defaultThreadPool() {
 }
 
 ThreadPool::ThreadPool(int numThreads)
-    : fn_(nullptr),
-      workItemsPending_(0),
-      currentWorkId_(0),
-      threadsReady_(0),
-      minWorkSize_(kDefaultMinWorkSize)
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-      ,
-      imbalanceRatio_(kDefaultImbalanceRatio)
-#endif
-{
-  std::lock_guard<std::mutex> guard(mutex_);
+    : minWorkSize_(kDefaultMinWorkSize), numThreads_(numThreads),
+      workersPool_(std::make_shared<WorkersPool>()) {}
 
-  // All worker threads (and the main thread) have a ThreadInfo
-  for (auto i = 0; i < numThreads; ++i) {
-    threadInfo_.emplace_back(
-        MakeAligned<ThreadInfo>::make(kCacheLineSize, i, numThreads));
-  }
-
-  // The first ThreadInfo is for the main thread
-  for (auto i = 1; i < numThreads; ++i) {
-    auto pInfo = &(threadInfo_[i]);
-    auto fn = [pInfo, this, i]() { (*pInfo)->threadMain(i, this); };
-
-    threads_.emplace_back(std::thread(std::move(fn)));
-  }
-}
-
-ThreadPool::~ThreadPool() {
-  {
-    std::lock_guard<std::mutex> guard(mutex_);
-    for (auto& info : threadInfo_) {
-      info->wantExit_ = true;
-    }
-  }
-
-  threadStartMonitor_.notify_all();
-
-  // Wait on all threads to exit
-  for (auto& thread : threads_) {
-    thread.join();
-  }
-}
+ThreadPool::~ThreadPool() {}
 
 int ThreadPool::getNumThreads() const {
   std::lock_guard<std::mutex> guard(executionMutex_);
-
-  return threadInfo_.size();
+  return numThreads_;
 }
 
 // Sets the minimum work size (range) for which to invoke the
@@ -131,275 +87,61 @@ int ThreadPool::getNumThreads() const {
 // main (calling) thread
 void ThreadPool::setMinWorkSize(size_t size) {
   std::lock_guard<std::mutex> guard(executionMutex_);
-
   minWorkSize_ = size;
 }
 
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-void ThreadPool::setImbalanceRatio(float ratio) {
-  std::lock_guard<std::mutex> guard(executionMutex_);
-
-  imbalanceRatio_ = ratio;
-}
-#endif
-
-#ifdef CAFFE2_THREADPOOL_STATS
-std::vector<ThreadStats> ThreadPool::getStats(bool reset) {
-  std::lock_guard<std::mutex> guard(executionMutex_);
-
-  // Set up thread state
-  {
-    std::unique_lock<std::mutex> lock(mutex_);
-
-    // We've guaranteed that all threads have finished work for the
-    // previous round, but we don't want threads to read new work
-    // information out of order. Wait for all of the old threads to
-    // check in first
-    while (threadsReady_ < threads_.size()) {
-      threadReadyMonitor_.wait(lock);
-    }
-
-    // The above serves as a barrier to ensure the stats are complete
-
-    std::vector<ThreadStats> stats;
-    for (auto& t : threadInfo_) {
-      stats.push_back(t->stats_);
-      if (reset) {
-        t->stats_.reset();
-      }
-    }
-
-    return stats;
-  }
-}
-#endif
-
 void ThreadPool::run(const std::function<void(int, size_t)>& fn, size_t range) {
   std::lock_guard<std::mutex> guard(executionMutex_);
-
   // If there are no worker threads, or if the range is too small (too
   // little work), just run locally
-  bool runLocally = threads_.empty() || range < minWorkSize_ ||
-      FLAGS_caffe2_threadpool_force_inline;
-
-  auto numThreads = threadInfo_.size();
-  size_t workUnitsPerThread = 0;
-  size_t firstThreadWork = 0;
-  size_t otherThreadWork = 0;
-
-  if (!runLocally) {
-    size_t workUnitsPerThread = (numThreads + range - 1) / numThreads;
-
-// On mobile devices (especially big.LITTLE cores), there is
-// significant lag in getting other threads to participate versus
-// the current thread, which is likely already running on a big
-// core.
-// Based on tests, the main thread will execute (through its own
-// work and stealing others) about 25% more work than other
-// threads.
-// To reduce the work stealing overhead, give the main thread 25%
-// more work to start with.
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-    firstThreadWork = (size_t)(imbalanceRatio_ * workUnitsPerThread);
-    if (firstThreadWork >= range) {
-      // give all to first thread
-      runLocally = true;
-    }
-
-    size_t remainderWork = range - firstThreadWork;
-    otherThreadWork = ((numThreads - 1) + remainderWork - 1) / (numThreads - 1);
-#else
-    firstThreadWork = workUnitsPerThread;
-    otherThreadWork = workUnitsPerThread;
-#endif
-  }
-
+  const bool runLocally = range < minWorkSize_ ||
+                          FLAGS_caffe2_threadpool_force_inline ||
+                          (numThreads_ == 0);
   if (runLocally) {
     // Work is small enough to just run locally; multithread overhead
     // is too high
     for (size_t i = 0; i < range; ++i) {
       fn(0, i);
     }
-
-#ifdef CAFFE2_THREADPOOL_STATS
-    // The main thread worked on this directly
-    auto& stats = threadInfo_[0]->stats_;
-    stats.numWorkedOn += range;
-    stats.numAssigned += range;
-#endif
-
     return;
   }
 
-  // Otherwise, all worker threads participate
-  // Set up thread state
-  {
-    std::unique_lock<std::mutex> lock(mutex_);
-
-    // We've guaranteed that all threads have finished work for the
-    // previous round, but we don't want threads to read new work
-    // information out of order. Wait for all of the old threads to
-    // check in first
-    while (threadsReady_ < threads_.size()) {
-      threadReadyMonitor_.wait(lock);
-    }
-
-    // Our threads are ready, and are waiting for us to start them.
-    threadsReady_ = 0;
-
-    fn_ = &fn;
-
-    // Work given to main thread
-    {
-      auto& info = threadInfo_[0];
-      info->rangeStart_ = 0;
-      // already guaranteed to be within bounds
-      info->rangeEnd_ = firstThreadWork;
-      info->rangeLength_ = firstThreadWork;
-#ifdef CAFFE2_THREADPOOL_STATS
-      info->stats_.numAssigned += firstThreadWork;
-#endif
-    }
-
-    // Work given to other threads
-    size_t workStart = firstThreadWork;
-    for (size_t i = 1; i < numThreads; ++i) {
-      auto& info = threadInfo_[i];
-
-      auto start = std::min(workStart, range);
-      auto end = std::min(workStart + otherThreadWork, range);
-      auto numAssigned = end - start;
-      info->rangeStart_ = start;
-      info->rangeEnd_ = end;
-      info->rangeLength_ = numAssigned;
-#ifdef CAFFE2_THREADPOOL_STATS
-      info->stats_.numAssigned += numAssigned;
-#endif
-      workStart += otherThreadWork;
-    }
-
-    workItemsPending_ = range;
-    ++currentWorkId_;
-  }
-
-  // Wake all worker threads
-  threadStartMonitor_.notify_all();
-
-  // We participate as well
-  bool done = threadInfo_[0]->runAndSteal(0, this);
-
-  // This thread may have been the one to finish all the work
-  if (!done) {
-    // Wait until we get signalled back
-    {
-      std::unique_lock<std::mutex> lock(mutex_);
-      while (workItemsPending_.load() > 0) {
-        threadDoneMonitor_.wait(lock);
+  struct FnTask : public Task {
+    FnTask(){};
+    virtual ~FnTask(){};
+    const std::function<void(int, size_t)> *fn_;
+    int idx_;
+    size_t start_;
+    size_t end_;
+    virtual void Run() override {
+      for (auto i = start_; i < end_; ++i) {
+        (*fn_)(idx_, i);
       }
     }
-  }
-}
+  };
 
-void ThreadInfo::threadMain(int threadId, ThreadPool* pool) {
-  long lastProcessedWorkId = 0;
-
-  while (true) {
-    {
-      // Kick off
-      std::unique_lock<std::mutex> lock(pool->mutex_);
-      int numAtBarrier = ++(pool->threadsReady_);
-      // numThreads includes main thread, we only care about the # of
-      // worker threads here
-      if (numAtBarrier == (numThreads_ - 1)) {
-        pool->threadReadyMonitor_.notify_one();
-      }
-
-      // Wait on main to give us new work
-      while (!wantExit_ && pool->currentWorkId_ <= lastProcessedWorkId) {
-        pool->threadStartMonitor_.wait(lock);
-      }
-
-      // Whether or not we actually do some work, this is the new work
-      // item we're handling
-      lastProcessedWorkId = pool->currentWorkId_;
+  CAFFE_ENFORCE_GE(numThreads_, 1);
+  const size_t unitsPerTask = (range + numThreads_ - 1) / numThreads_;
+  tasks_.resize(numThreads_);
+  for (size_t i = 0; i < numThreads_; ++i) {
+    if (!tasks_[i]) {
+      tasks_[i].reset(new FnTask());
     }
-
-    if (wantExit_) {
-      return;
-    }
-
-    bool shouldSignal = runAndSteal(threadId, pool);
-
-    if (shouldSignal) {
-      std::lock_guard<std::mutex> guard(pool->mutex_);
-      pool->threadDoneMonitor_.notify_one();
-    }
-  }
-}
-
-bool ThreadInfo::runAndSteal(int threadId, ThreadPool* pool) {
-  auto lambdaFunctionToRun = pool->fn_;
-  int localItemsCompleted = 0;
-  int localItemsStolen = 0;
-
-  /* Process thread's own range of items */
-  auto curItem = rangeStart_;
-  while (true) {
-    auto curRangeLength = --rangeLength_; // atomic
-
-    if (curRangeLength < 0) {
-      // someone stole all of our work
+    auto *task = (FnTask *)tasks_[i].get();
+    task->fn_ = &fn;
+    task->idx_ = i;
+    task->start_ = std::min<size_t>(range, i * unitsPerTask);
+    task->end_ = std::min<size_t>(range, (i + 1) * unitsPerTask);
+    if (task->start_ >= task->end_) {
+      tasks_.resize(i);
       break;
     }
-
-    (*lambdaFunctionToRun)(threadId, curItem);
-
-    ++curItem;
-    ++localItemsCompleted;
+    CAFFE_ENFORCE_LE(task->start_, range);
+    CAFFE_ENFORCE_LE(task->end_, range);
   }
-
-  // Done, now look for other threads' items to steal
-  for (auto i = (threadId_ + 1) % numThreads_; i != threadId_;
-       i = (i + 1) % numThreads_) {
-    auto& otherThread = pool->threadInfo_[i];
-
-    while (true) {
-      auto curRangeLength = --(otherThread->rangeLength_); // atomic
-
-      if (curRangeLength < 0) {
-        break;
-      }
-
-      // We're successfully stealing a work item from the other thread
-      auto itemId = --(otherThread->rangeEnd_); // atomic
-
-      (*lambdaFunctionToRun)(threadId, itemId);
-      ++localItemsCompleted;
-#ifdef CAFFE2_THREADPOOL_STATS
-      ++localItemsStolen;
-#endif
-    }
-  }
-
-  bool lastThread = false;
-
-  if (localItemsCompleted > 0) {
-    auto numRemaining =
-        (pool->workItemsPending_ -= localItemsCompleted); // atomic
-    DCHECK_GE(numRemaining, 0);
-
-    if (numRemaining == 0) {
-      // We were the last thread to finish all work
-      lastThread = true;
-    }
-  }
-
-#ifdef CAFFE2_THREADPOOL_STATS
-  stats_.numWorkedOn += localItemsCompleted;
-  stats_.numStolen += localItemsStolen;
-#endif
-
-  return lastThread;
+  CAFFE_ENFORCE_LE(tasks_.size(), numThreads_);
+  CAFFE_ENFORCE_GE(tasks_.size(), 1);
+  workersPool_->Execute(tasks_);
 }
 
 } // namespace caffe2
--- caffe2/utils/threadpool/ThreadPool.h
+++ caffe2/utils/threadpool/ThreadPool.h
@@ -10,19 +10,8 @@
 // ThreadPool only used in mobile builds at the moment
 #if CAFFE2_THREADPOOL_MOBILE
 
-// Compile-time flag to control inclusion of per-worker thread stats
-// #define CAFFE2_THREADPOOL_STATS
-
-// Compile-time flag to control usage of main thread work imbalance
-// #define CAFFE2_THREADPOOL_MAIN_IMBALANCE
-
-#include <stdlib.h> // posix_memalign
-#include <atomic>
-#include <condition_variable>
-#include <functional>
 #include <memory>
 #include <mutex>
-#include <thread>
 #include <vector>
 
 //
@@ -31,125 +20,10 @@
 
 namespace caffe2 {
 
+class Task;
+class WorkersPool;
+
 constexpr size_t kCacheLineSize = 64;
-
-template <typename T>
-struct AllocAligned {
-  // Allocate a T aligned at an `align` byte address
-  template <typename... Args>
-  static T* alloc(size_t align, Args&&... args) {
-    void* p = nullptr;
-// FIXME: we should just be able to use std::align
-#if !defined(__ANDROID__)
-    posix_memalign((void**)&p, align, sizeof(T));
-#else
-    p = memalign(align, sizeof(T));
-#endif
-
-    if (p) {
-      return new (p) T(std::forward<Args>(args)...);
-    }
-
-    return nullptr;
-  }
-
-  // Free a T previously allocated via AllocAligned<T>::alloc()
-  static void release(T* p) {
-    if (p) {
-      p->~T();
-      free((void*)p);
-    }
-  }
-};
-
-// Deleter object for unique_ptr for an aligned object
-template <typename T>
-struct AlignedDeleter {
-  void operator()(T* p) const {
-    AllocAligned<T>::release(p);
-  }
-};
-
-// make_unique that guarantees alignment
-template <typename T>
-struct MakeAligned {
-  template <typename... Args>
-  static std::unique_ptr<T, AlignedDeleter<T>> make(
-      size_t align,
-      Args&&... args) {
-    return std::unique_ptr<T, AlignedDeleter<T>>(
-        AllocAligned<T>::alloc(align, std::forward<Args>(args)...));
-  }
-};
-
-struct ThreadPool;
-
-#ifdef CAFFE2_THREADPOOL_STATS
-struct ThreadStats {
-  inline ThreadStats() : numAssigned(0), numWorkedOn(0), numStolen(0) {}
-
-  inline void reset() {
-    numAssigned = 0;
-    numWorkedOn = 0;
-    numStolen = 0;
-  }
-
-  int numAssigned;
-  int numWorkedOn;
-  int numStolen;
-};
-#endif
-
-struct alignas(kCacheLineSize) ThreadInfo {
-  inline ThreadInfo(int threadId, int numThreads)
-      : rangeStart_(0),
-        rangeEnd_(0),
-        rangeLength_(0),
-        wantExit_(false),
-        threadId_(threadId),
-        numThreads_(numThreads) {}
-
-  // Entry point for all worker threads
-  void threadMain(int threadId, ThreadPool* pool);
-
-  // Runs a task, and when we're done with our local queue, steal from
-  // neighbors.
-  // Returns true if all work is done (we were the last thread to do
-  // work)
-  bool runAndSteal(int threadId, ThreadPool* pool);
-
-  // Index of first element in the work range.
-  // Before processing a new element the owning worker thread
-  // increments this value.
-  long rangeStart_;
-
-  // Index of the element after the last element of the work range.
-  // Before processing a new element the stealing worker thread
-  // decrements this value.
-  std::atomic<long> rangeEnd_;
-
-  // The number of elements in the work range.
-  // Due to race conditions range_length <= range_end - range_start.
-  // The owning worker thread must decrement this value before
-  // incrementing @a range_start.
-  // The stealing worker thread must decrement this value before
-  // decrementing @a range_end.
-  std::atomic<long> rangeLength_;
-
-  // Should this thread exit?
-  bool wantExit_;
-
-  // Our thread index
-  int threadId_;
-
-  // How many threads are there in total?
-  int numThreads_;
-
-#ifdef CAFFE2_THREADPOOL_STATS
-  // Updated stats
-  ThreadStats stats_;
-#endif
-};
 
 class alignas(kCacheLineSize) ThreadPool {
  public:
@@ -157,10 +31,7 @@ class alignas(kCacheLineSize) ThreadPool {
   // threads
   static std::unique_ptr<ThreadPool> defaultThreadPool();
   ThreadPool(int numThreads);
-
-  // Shuts down all worker threads (if any) before destroying ourselves
   ~ThreadPool();
-
   // Returns the number of threads currently in use
   int getNumThreads() const;
 
@@ -168,76 +39,15 @@ class alignas(kCacheLineSize) ThreadPool {
   // threadpool; work sizes smaller than this will just be run on the
   // main (calling) thread
   void setMinWorkSize(size_t size);
-  size_t getMinWorkSize() const {
-    return minWorkSize_;
-  }
-
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-  // Set imbalance factor for the main thread versus other threads;
-  // default is 1.25
-  void setImbalanceRatio(float ratio);
-#endif
-
-  // Called to schedule work on the threadpool
+  size_t getMinWorkSize() const { return minWorkSize_; }
   void run(const std::function<void(int, size_t)>& fn, size_t range);
 
-#ifdef CAFFE2_THREADPOOL_STATS
-  // Returns current per-thread statistics. If reset is true, reset
-  // current values.
-  std::vector<ThreadStats> getStats(bool reset = false);
-#endif
-
- protected:
-  friend struct ThreadInfo;
-
-  // What we are currently working on
-  const std::function<void(int, size_t)>* fn_;
-
-  // How many work items are outstanding? When this reaches 0, our
-  // main thread is resumed
-  std::atomic<long> workItemsPending_;
-
-  // Current work ID that we're running; sequentially increments
-  long currentWorkId_;
-
-  // Mutex that guards all monitors and state updates
-  std::mutex mutex_;
-
-  // Main thread waits on this before running new work, to make sure
-  // that all worker threads have looped back around to await new work
-  std::condition_variable threadReadyMonitor_;
-
-  // All worker threads wait on this to make sure that they have work
-  // available for processing
-  std::condition_variable threadStartMonitor_;
-
-  // Main thread waits on this before returning to the thread pool
-  // caller; note that we don't actually wait on the worker threads
-  // saying that they're all done (woken up); we only check when the
-  // thread pool is called again
-  std::condition_variable threadDoneMonitor_;
-
-  // How many threads are ready to process new work?
-  size_t threadsReady_;
-
-  // The first entry is always for the main thread
-  std::vector<std::unique_ptr<ThreadInfo, AlignedDeleter<ThreadInfo>>>
-      threadInfo_;
-
-  // Set of threads that we are managing
-  std::vector<std::thread> threads_;
-
-  // What's the minimum work size for using the threadpool?
-  size_t minWorkSize_;
-
-#ifdef CAFFE2_THREADPOOL_MAIN_IMBALANCE
-  // Imbalance factor for main vs. other thread work
-  float imbalanceRatio_;
-#endif
-
-  // Mutex that ensures that only one user call to the ThreadPool is
-  // outstanding
+private:
   mutable std::mutex executionMutex_;
+  size_t minWorkSize_;
+  size_t numThreads_;
+  std::shared_ptr<WorkersPool> workersPool_;
+  std::vector<std::shared_ptr<Task>> tasks_;
 };
 
 } // namespace caffe2
