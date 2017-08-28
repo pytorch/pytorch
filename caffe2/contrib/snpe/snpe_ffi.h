7631e4ec43e811df241b0103cc41d4f65365fe1d
Bram Wasti <bwasti@fb.com>
2017-08-28 15:23:56 -0700

    @allow-large-files [caffe2] update snpe for oss
    Summary: oss build
    
    Reviewed By: Yangqing
    
    Differential Revision: D5605281
    
    fbshipit-source-id: 289283d5ce8267a2ba22e6c35f6c4af0d45c439b

--- caffe2/contrib/snpe/snpe_ffi.h
+++ caffe2/contrib/snpe/snpe_ffi.h
@@ -1,10 +1,22 @@
+#ifndef CAFFE2_SNPE_FFI_H_
+#define CAFFE2_SNPE_FFI_H_
+
 #include <stdint.h>
+#include <string>
+
+namespace caffe2 {
+
+std::string& gSNPELocation();
+
+const char* const snpe_ffi_so = "libsnpe_ffi.so";
+
+}
 
 extern "C" {
 
 bool snpe_has_gpu();
 
-void* snpe_create(const uint8_t* container, size_t size);
+void* snpe_create(const uint8_t* container, size_t size, const char* input_name);
 
 void snpe_destroy(void* ctx);
 
@@ -17,4 +29,7 @@ void snpe_run(void* ctx,
               size_t* outputSize);
 
 void snpe_copy_output_to(void* ctx, float* outputData);
+
 }
+
+#endif  // CAFFE2_SNPE_FFI_H_
