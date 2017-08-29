a0204331a8430ce2b2adbbc86a64f7eedbdb8b9a
Ilia Cherniavskii <iliacher@fb.com>
2017-08-28 19:56:36 -0700

    Control flow operators
    Summary:
    This diff adds control flow operators in Caffe2 (starting with If, While):
     - Added If operator that executes then/else subnet
     - Branch subnet is executed in a separate isolated workspace, with some of the blobs transparently forwarded from the outer workspace
     - Adding a new NetBuilder subclass to construct nets using new operator
     - NetBuilder also keeps track of outer blob names and automatically sets blob bindings between outer and inner workspace, implementing generic convention on handling local/global variables in blocks
    
    Reviewed By: volkhin
    
    Differential Revision: D5720644
    
    fbshipit-source-id: a674cde0c789f6a6ffdcd9d80159d1e42e49133f

---
+++ caffe2/operators/while_op.cc
@@ -0,0 +1,58 @@
+#include "caffe2/operators/while_op.h"
+
+namespace caffe2 {
+
+template <>
+bool WhileOp<CPUContext>::RunOnDevice() {
+  CAFFE_ENFORCE_GT(
+      InputSize(), 0, "Condition must be specified in While operator");
+  CAFFE_ENFORCE(
+      InputIsType<Tensor<CPUContext>>(0),
+      "Invalid condition in While operator: tensor expected");
+
+  const auto& condition = Input(0);
+  CAFFE_ENFORCE(
+      condition.IsType<bool>(),
+      "Invalid condition tensor in While operator: boolean expected");
+  CAFFE_ENFORCE_EQ(
+      condition.size(),
+      1,
+      "Invalid condition tensor in While operator: single value expected");
+  CAFFE_ENFORCE_EQ(
+      condition.ndim(),
+      0,
+      "Invalid condition tensor in While operator: scalar expected");
+
+  while (true) {
+    if (cond_net_ && !cond_net_->Run()) {
+      return false;
+    }
+    if (!*condition.data<bool>()) {
+      return true;
+    }
+    if (!loop_net_->Run()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+REGISTER_CPU_OPERATOR(While, WhileOp<CPUContext>);
+
+OPERATOR_SCHEMA(While)
+    .NumInputs(1, INT_MAX)
+    .NumOutputs(0, INT_MAX)
+    .SetDoc(R"DOC(
+'While' control operator, first input is a scalar boolean blob that stores loop's
+condition value. Accepts 'loop_net' (required) and 'cond_net' (optional) arguments for
+loop's body and condition subnets respectively. If condition subnet is specified,
+it is executed before the first and after each iteration. Subnets are executed in
+the same workspace as 'While'.
+    )DOC")
+    .Arg("loop_net", "Net executed on each iteration")
+    .Arg("cond_net", "Net to (re)compute condition value")
+    .Input(0, "condition", "Scalar boolean condition")
+    .AllowInplace([](int in, int out) -> bool { return true; });
+
+} // namespace caffe2
