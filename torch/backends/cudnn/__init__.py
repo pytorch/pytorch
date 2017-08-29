47bc218ebb638dbbe8fbd86759da4c5fd4749efc
peter <peter_jiachen@163.com>
2017-08-29 17:11:56 +0800

    Fixing wrong if conditions

--- torch/backends/cudnn/__init__.py
+++ torch/backends/cudnn/__init__.py
@@ -19,17 +19,17 @@ def _libcudnn():
             lib = ctypes.cdll.LoadLibrary(CUDNN_WIN_LIB)
         else:
             lib = ctypes.cdll.LoadLibrary(None)
-            if hasattr(lib, 'cudnnGetErrorString'):
-                lib.cudnnGetErrorString.restype = ctypes.c_char_p
-                __cudnn_version = lib.cudnnGetVersion()
-                compile_version = torch._C._cudnn_version()
-                # Check that cuDNN major and minor versions match
-                if (__cudnn_version // 100) != (compile_version // 100):
-                    raise RuntimeError(
-                        'cuDNN version mismatch: PyTorch was compiled against {} '
-                        'but linked against {}'.format(compile_version, __cudnn_version))
-            else:
-                lib = None
+        if hasattr(lib, 'cudnnGetErrorString'):
+            lib.cudnnGetErrorString.restype = ctypes.c_char_p
+            __cudnn_version = lib.cudnnGetVersion()
+            compile_version = torch._C._cudnn_version()
+            # Check that cuDNN major and minor versions match
+            if (__cudnn_version // 100) != (compile_version // 100):
+                raise RuntimeError(
+                    'cuDNN version mismatch: PyTorch was compiled against {} '
+                    'but linked against {}'.format(compile_version, __cudnn_version))
+        else:
+            lib = None
     return lib
 
 
