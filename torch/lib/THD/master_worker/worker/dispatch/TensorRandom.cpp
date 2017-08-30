83766e183a012312b8a2eadde5bd0767e12488ec
Trevor Killeen <killeentm@gmail.com>
2017-08-30 08:51:42 -0700

    port random

--- torch/lib/THD/master_worker/worker/dispatch/TensorRandom.cpp
+++ torch/lib/THD/master_worker/worker/dispatch/TensorRandom.cpp
@@ -1,46 +1,46 @@
 
 static void tensorRandom(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   finalize(raw_message);
-  tensor->random(*_generator);
+  tensor.random_(*_generator);
 }
 
 static void tensorGeometric(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double p = unpackFloat(raw_message);
   finalize(raw_message);
-  tensor->geometric(*_generator, p);
+  tensor.geometric_(*_generator, p);
 }
 
 static void tensorBernoulli(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double p = unpackFloat(raw_message);
   finalize(raw_message);
   tensor->bernoulli(*_generator, p);
 }
 
 static void tensorBernoulli_FloatTensor(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
-  thpp::Tensor *p = unpackRetrieveTensor(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor p = unpackRetrieveTensor(raw_message);
   finalize(raw_message);
   tensor->bernoulli_FloatTensor(*_generator, *p);
 }
 
 static void tensorBernoulli_DoubleTensor(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
-  thpp::Tensor *p = unpackRetrieveTensor(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor p = unpackRetrieveTensor(raw_message);
   finalize(raw_message);
   tensor->bernoulli_DoubleTensor(*_generator, *p);
 }
 
 static void tensorUniform(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double a = unpackFloat(raw_message);
   double b = unpackFloat(raw_message);
   finalize(raw_message);
@@ -48,8 +48,8 @@ static void tensorUniform(rpc::RPCMessage& raw_message) {
 }
 
 static void tensorNormal(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double mean = unpackFloat(raw_message);
   double stdv = unpackFloat(raw_message);
   finalize(raw_message);
@@ -57,16 +57,16 @@ static void tensorNormal(rpc::RPCMessage& raw_message) {
 }
 
 static void tensorExponential(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double lambda = unpackFloat(raw_message);
   finalize(raw_message);
   tensor->exponential(*_generator, lambda);
 }
 
 static void tensorCauchy(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double median = unpackFloat(raw_message);
   double sigma = unpackFloat(raw_message);
   finalize(raw_message);
@@ -74,8 +74,8 @@ static void tensorCauchy(rpc::RPCMessage& raw_message) {
 }
 
 static void tensorLogNormal(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
   double mean = unpackFloat(raw_message);
   double stdv = unpackFloat(raw_message);
   finalize(raw_message);
@@ -83,9 +83,9 @@ static void tensorLogNormal(rpc::RPCMessage& raw_message) {
 }
 
 static void tensorMultinomial(rpc::RPCMessage& raw_message) {
-  thpp::Tensor *tensor = unpackRetrieveTensor(raw_message);
-  thpp::Generator *_generator = unpackRetrieveGenerator(raw_message);
-  thpp::Tensor *prob_dist = unpackRetrieveTensor(raw_message);
+  at::Tensor tensor = unpackRetrieveTensor(raw_message);
+  at::Generator *_generator = unpackRetrieveGenerator(raw_message);
+  at::Tensor prob_dist = unpackRetrieveTensor(raw_message);
   int n_sample = unpackInteger(raw_message);
   int with_replacement = unpackInteger(raw_message);
   finalize(raw_message);
