7227e9240d0ac160c6921134832ce0d32186a14f
Sourav Singh <souravsingh@users.noreply.github.com>
2017-08-31 15:05:15 +0530

    Add support for colors in NVTX

--- torch/cuda/nvtx.py
+++ torch/cuda/nvtx.py
@@ -46,3 +46,17 @@ def mark(msg):
     if _libnvToolsExt() is None:
         raise RuntimeError('Unable to load nvToolsExt library')
     return lib.nvtxMarkA(ctypes.c_char_p(msg.encode("ascii")))
+
+class nvtxEventAttributes_t(ctypes.Structure):
+    """
+    A C struct containing essential attributes and optional
+    attributes about a CUDA event. The optional attributes currently
+    implemented are 'msg' and 'color'.
+
+    The data types of the fields are based on cupy's implementation.
+    """
+    _fields_ = = [('version', ctypes.c_ushort),
+                  ('size', ctypes.c_ushort),
+                  ('color', ctypes.c_char_p),
+                  ('msg', ctypes.c_char_p)
+                 ]
