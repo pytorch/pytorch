997c8d889832633352b12a736ce2c06204bc96f6
Edward Z. Yang <ezyang@fb.com>
2017-08-31 07:07:03 -0700

    Delete dead torch.toffee.op
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/toffee.py
+++ torch/toffee.py
@@ -1,17 +1,6 @@
 import torch
 
 
-def op(op_name, *args, **kwargs):
-    """A primitive operator
-
-    TODO: better docs here
-
-    TODO: This doesn't actually do an operation, eventually we want it to (and
-    trace correctly).  DO NOT rely on this returning a dictionary!!!
-    """
-    return {"name": op_name, "inputs": args, "attrs": kwargs}
-
-
 def export(model, input, embed_params):
 
     # Enable tracing on the model
