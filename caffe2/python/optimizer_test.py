f7cb31d8420a2f33d442abf337c86d36e0f890b2
Wojciech Glogowski <glogowski@fb.com>
2017-08-30 18:46:17 -0700

    Initial implementation of YellowFin algorithm
    Summary:
    Added YellowFin optimizer to Caffe2.
    This implemention is different from the original: It has separate alpha and mu for each parameter and it uses different version of Momentum SGD.
    Tests / benchmarks for the optimizer are to be done. Some refactor of the code is to be done before pushing. This is still a working version.
    
    Reviewed By: akyrola
    
    Differential Revision: D5652689
    
    fbshipit-source-id: c10dc0424f47c3051b454aede1d121902cb759a8

--- caffe2/python/optimizer_test.py
+++ caffe2/python/optimizer_test.py
@@ -1,17 +1,20 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from caffe2.proto import caffe2_pb2
+import caffe2.python.optimizer as optimizer
 from caffe2.python.optimizer import (
-    build_sgd, build_multi_precision_sgd, build_ftrl,
-    build_adagrad, build_adam, add_weight_decay, SgdOptimizer)
+    build_sgd, build_multi_precision_sgd, build_ftrl, build_adagrad,
+    build_adam, build_yellowfin, add_weight_decay, SgdOptimizer)
 from caffe2.python.optimizer_context import UseOptimizer
 from caffe2.python.optimizer_test_util import (
     OptimizerTestBase, LRModificationTestBase
 )
+from caffe2.python import core, workspace
 from caffe2.python.test_util import TestCase
-from caffe2.python import workspace
-from caffe2.python.core import DataType
 import numpy as np
+from numpy.testing import assert_allclose, assert_equal
+import math
 import unittest
 
 
@@ -46,7 +49,7 @@ class TestMultiPrecisionSgd(
 
     @unittest.skipIf(not workspace.has_gpu_support, "No GPU support")
     def testGPUDense(self):
-        super(TestMultiPrecisionSgd, self).testGPUDense(DataType.FLOAT16)
+        super(TestMultiPrecisionSgd, self).testGPUDense(core.DataType.FLOAT16)
 
 
 class TestFtrl(OptimizerTestBase, TestCase):
@@ -98,6 +101,273 @@ class TestAdam(OptimizerTestBase, LRModificationTestBase, TestCase):
             workspace.FetchBlob(param)
         for param in optimizer.get_auxiliary_parameters().local:
             workspace.FetchBlob(param)
+
+
+class TestYellowFin(OptimizerTestBase, TestCase):
+    # YellowFin: An automatic tuner for momentum SGD
+    # (https://arxiv.org/abs/1706.03471)
+    def build_optimizer(self, model):
+        self._skip_gpu = False
+        return build_yellowfin(model, base_learning_rate=0.1)
+
+    def check_optimizer(self, optimizer):
+        self.assertTrue(optimizer.get_auxiliary_parameters().shared)
+        self.assertTrue(optimizer.get_auxiliary_parameters().local)
+        self.assertTrue(workspace.HasBlob("optimizer_iteration"))
+        iteration_tensor = workspace.FetchBlob("optimizer_iteration")
+        np.testing.assert_allclose(np.array([2000]),
+                                   iteration_tensor,
+                                   atol=1e-5)
+        for param in optimizer.get_auxiliary_parameters().shared:
+            workspace.FetchBlob(param)
+        for param in optimizer.get_auxiliary_parameters().local:
+            workspace.FetchBlob(param)
+
+    def testSparse(self):
+        raise unittest.SkipTest("no sparse support")
+
+    def deb(self, val, beta, i, zero_debias):
+        if zero_debias:
+            return val / (1.0 - beta ** i)
+        else:
+            return val
+
+    def get_lr_mu(self, distance, grad_var, h_min, h_max):
+        # First tune based on dynamic range
+        if grad_var == 0:
+            dr = h_max / h_min
+            mu = ((np.sqrt(dr) - 1) / (np.sqrt(dr) + 1)) ** 2
+            lr_min = (1 + np.sqrt(mu)) ** 2 / h_max
+            return lr_min, mu
+
+        p = distance ** 2 * h_min ** 2 / 2 / grad_var
+        w3 = (-math.sqrt(p * p + 4.0 / 27.0 * p * p * p) - p) / 2.0
+        w = math.copysign(1.0, w3) * math.pow(math.fabs(w3), 1.0 / 3.0)
+        y = w - p / 3.0 / w
+        root = y + 1
+        root = min(root, 1.0 - 1e-6)
+        dr = h_max / h_min
+        mu = max(((np.sqrt(dr) - 1) / (np.sqrt(dr) + 1)) ** 2, root**2)
+        lr_min = (1 - np.sqrt(mu)) ** 2 / h_min
+        return lr_min, mu
+
+    def caffe2_yellowfin(self, zero_debias, grad_coef, n_dim, n_iter, gpu):
+        caffe2_res = {}
+
+        alpha = 1.0
+        mu = 0.0
+        beta = 0.999
+        curv_win_width = 20
+        epsilon = 1e-6
+
+        net = core.Net("net")
+        param_init_net = core.Net("param_init_net")
+        workspace.ResetWorkspace()
+
+        with core.DeviceScope(core.DeviceOption(caffe2_pb2.CPU)):
+            iteration = param_init_net.ConstantFill(
+                [],
+                "iteration",
+                shape=[1],
+                value=0,
+                dtype=core.DataType.INT64)
+            iter_mutex = param_init_net.CreateMutex([], ["iteration_mutex"])
+            net.AtomicIter([iter_mutex, iteration], [iteration])
+        pre_grad = param_init_net.ConstantFill(
+            [],
+            "pre_grad",
+            shape=[n_dim],
+            value=grad_coef
+        )
+        if gpu:
+            iteration = net.CopyCPUToGPU(
+                [iteration],
+                "iteration_cpu"
+            )
+        iteration_float = net.Cast([iteration], "iteration_float")
+        grad = net.Mul([pre_grad, iteration_float], "grad", broadcast=True)
+        w = param_init_net.ConstantFill([], "w", shape=[n_dim], value=0.0)
+
+        # a hack to create an object with __dict__
+        param_info = lambda: None
+        param_info.blob = w
+        param_info.grad = grad
+
+        optimizer.YellowFinOptimizer(
+            alpha=alpha,
+            mu=mu,
+            beta=beta,
+            curv_win_width=curv_win_width,
+            epsilon=epsilon,
+            zero_debias=zero_debias
+        )._run(
+            net,
+            param_init_net,
+            param_info
+        )
+
+        workspace.RunNetOnce(param_init_net)
+        workspace.CreateNet(net, overwrite=True)
+        for i in range(n_iter):
+            workspace.RunNet(net)
+            scalars_memory_blob = workspace.FetchBlob("w_scalars_memory")
+            g_norm2_avg = scalars_memory_blob[1]
+            g_norm2_min_avg = scalars_memory_blob[2]
+            g_norm2_max_avg = scalars_memory_blob[3]
+            distance_avg = scalars_memory_blob[4]
+            g_avg_blob = workspace.FetchBlob("w_g_avg")
+            res_lr = workspace.FetchBlob("w_lr_avg")[0]
+            res_mu = workspace.FetchBlob("w_mu_avg")[0]
+            g_deb = self.deb(g_avg_blob, beta, i + 1, zero_debias)
+            variance = max(
+                self.deb(g_norm2_avg, beta, i + 1, zero_debias) -
+                g_deb.dot(g_deb),
+                epsilon
+            )
+            if i > 0:
+                caffe2_res[i] = {
+                    'h_max': np.exp(self.deb(g_norm2_max_avg,
+                                             beta,
+                                             i + 1,
+                                             zero_debias)),
+                    'h_min': np.exp(self.deb(g_norm2_min_avg,
+                                             beta,
+                                             i + 1,
+                                             zero_debias)),
+                    'var': variance,
+                    'dist': self.deb(distance_avg, beta, i + 1, zero_debias),
+                    'lr': res_lr,
+                    'mu': res_mu
+                }
+        return caffe2_res
+
+    def numpy_yellowfin(self, zero_debias, grad_coef, n_dim, n_iter, gpu):
+        numpy_res = {}
+
+        target_h_max = 0.0
+        target_h_min = 0.0
+        target_g_norm_squared_avg = 0.0
+        target_g_norm_avg = 0.0
+        target_g_avg = 0.0
+        target_dist_avg = 0.0
+        target_lr = 1.0
+        target_mu = 0.0
+
+        for i in range(n_iter):
+            grad_val = (i + 1) * grad_coef
+            target_g_norm_squared_avg = 0.999 * target_g_norm_squared_avg + \
+                0.001 * np.sum((grad_val * np.ones([n_dim, ])) ** 2)
+            target_g_norm_avg = 0.999 * target_g_norm_avg + \
+                0.001 * np.linalg.norm(grad_val * np.ones([n_dim, ]))
+            target_g_avg = 0.999 * target_g_avg + 0.001 * grad_val
+
+            target_h_max = 0.999 * target_h_max + \
+                0.001 * np.log(grad_val ** 2 * n_dim)
+            target_h_min = 0.999 * target_h_min + \
+                0.001 * np.log((max(1, i + 2 - 20) * grad_coef) ** 2 * n_dim)
+            if zero_debias:
+                target_var = target_g_norm_squared_avg / \
+                    (1 - 0.999 ** (i + 1)) - \
+                    target_g_avg ** 2 * n_dim / (1 - 0.999 ** (i + 1)) ** 2
+            else:
+                target_var = target_g_norm_squared_avg - \
+                    target_g_avg ** 2 * n_dim
+            target_dist_avg = 0.999 * target_dist_avg + \
+                0.001 * target_g_norm_avg / target_g_norm_squared_avg
+
+            if i > 0:
+                if zero_debias:
+                    lr, mu = self.get_lr_mu(
+                        target_dist_avg / (1.0 - 0.999 ** (i + 1)),
+                        target_var,
+                        np.exp(target_h_min / (1.0 - 0.999 ** (i + 1))),
+                        np.exp(target_h_max / (1.0 - 0.999 ** (i + 1))))
+                    target_lr = 0.999 * target_lr + 0.001 * lr
+                    target_mu = 0.999 * target_mu + 0.001 * mu
+                    numpy_res[i] = {
+                        'h_max': np.exp(target_h_max / (1 - 0.999 ** (i + 1))),
+                        'h_min': np.exp(target_h_min / (1 - 0.999 ** (i + 1))),
+                        'var': target_var,
+                        'dist': target_dist_avg / (1 - 0.999 ** (i + 1)),
+                        'lr': target_lr,
+                        'mu': target_mu
+                    }
+                else:
+                    lr, mu = self.get_lr_mu(
+                        target_dist_avg,
+                        target_var,
+                        np.exp(target_h_min),
+                        np.exp(target_h_max))
+                    target_lr = 0.999 * target_lr + 0.001 * lr
+                    target_mu = 0.999 * target_mu + 0.001 * mu
+                    numpy_res[i] = {
+                        'h_max': np.exp(target_h_max),
+                        'h_min': np.exp(target_h_min),
+                        'var': target_var,
+                        'dist': target_dist_avg,
+                        'lr': target_lr,
+                        'mu': target_mu
+                    }
+        return numpy_res
+
+    def compare_yellowfin_models(self,
+                                 model0,
+                                 model1,
+                                 zero_debias,
+                                 grad_coef,
+                                 n_dim,
+                                 n_iter,
+                                 gpu):
+        model0_res = model0(zero_debias, grad_coef, n_dim, n_iter, gpu)
+        model1_res = model1(zero_debias, grad_coef, n_dim, n_iter, gpu)
+        assert_equal(len(model0_res), len(model1_res))
+        for i in range(1, len(model0_res)):
+            assert_equal(model0_res[i].keys(), model1_res[i].keys())
+            for feat in model0_res[i].keys():
+                err_msg = \
+                    'i=' + str(i) + ',\n' + \
+                    'feat=' + feat + ',\n' + \
+                    'grad_coef=' + str(grad_coef) + ',\n' + \
+                    'zero_debias=' + str(zero_debias)
+                assert_allclose(model0_res[i][feat],
+                                model1_res[i][feat],
+                                rtol=1e-2,
+                                err_msg=err_msg)
+
+    def test_caffe2_cpu_vs_numpy(self):
+        n_dim = 1000000
+        n_iter = 50
+        cpu_device_opt = core.DeviceOption(caffe2_pb2.CPU)
+        with core.DeviceScope(cpu_device_opt):
+            for zero_debias in [False, True]:
+                for grad_coef in [1.0, 0.1, 0.01]:
+                    self.compare_yellowfin_models(
+                        self.caffe2_yellowfin,
+                        self.numpy_yellowfin,
+                        zero_debias,
+                        grad_coef,
+                        n_dim,
+                        n_iter,
+                        gpu=False
+                    )
+
+    @unittest.skipIf(not workspace.has_gpu_support, "No gpu support")
+    def test_caffe2_gpu_vs_numpy(self):
+        n_dim = 1000000
+        n_iter = 50
+        gpu_device_opt = core.DeviceOption(caffe2_pb2.CUDA, 0)
+        with core.DeviceScope(gpu_device_opt):
+            for zero_debias in [False, True]:
+                for grad_coef in [1.0, 0.1, 0.01]:
+                    self.compare_yellowfin_models(
+                        self.caffe2_yellowfin,
+                        self.numpy_yellowfin,
+                        zero_debias,
+                        grad_coef,
+                        n_dim,
+                        n_iter,
+                        gpu=True
+                    )
 
 
 class TestMultiOptimizers(TestCase):
