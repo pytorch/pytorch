3dafab8826ce9090159772e5019584b453e5c7a7
Gregory Chanan <gchanan@fb.com>
2017-08-31 14:17:55 -0700

    Support broadcast specifications from cwrap.
    This respects all the broadcast cwrap specifications except for 'fallback';
    i.e. pointwise functions operating on tensors where the number of elements
    match but the sizes are different and not broadcastable.  This behavior is
    currently deprecated in PyTorch.  Note that this is a breaking change in ATen,
    because ATen just passes through to TH/THC, where the fallback behavior is
    actually implemented.
    
    This also changes expand semantics wrt Scalars (as tensors).  Previously,
    one could 'expand' a 1-dimensional tensor with size 1 to a 'scalar' (i.e.
    empty size initializer list).

---
+++ aten/src/ATen/test/broadcast_test.cpp
@@ -0,0 +1,171 @@
+#include "ATen/ATen.h"
+
+using namespace at;
+
+int main() {
+  Type & T = CPU(kFloat);
+
+  // 0) pre-req tests:
+  // can't expand empty tensor
+  try {
+    auto empty = T.randn({0});
+    empty.expand({3});
+    assert(false);
+  } catch(std::runtime_error &e) {}
+
+  // can't "expand" a Tensor to  Scalar
+  try {
+    auto tScalar = T.ones({1});
+    tScalar.get()->maybeScalar(true);
+    tScalar.expand({});
+    assert(false);
+  } catch (std::runtime_error &e) {}
+
+  // 1) out-place function with 2 args
+  {
+    // basic
+    auto a = T.randn({3, 1});
+    auto b = T.randn({5});
+    std::vector<int64_t> expanded_sizes = {3, 5};
+    assert((a + b).equal(a.expand(expanded_sizes) + b.expand(expanded_sizes)));
+
+    // with scalar
+    auto aScalar = T.ones({1});
+    aScalar.get()->maybeScalar(true);
+    b = T.randn({3, 5});
+    assert((aScalar + b).equal(aScalar.expand(b.sizes()) + b.expand(b.sizes())));
+
+    // old fallback behavior yields error
+    try {
+      auto a = T.randn({3, 5});
+      auto b = T.randn({5, 3});
+      a + b;
+      assert(false);
+    } catch (std::runtime_error &e) {}
+
+    // with mismatched sizes
+    try {
+      auto a = T.randn({3, 5});
+      auto b = T.randn({7, 5});
+      a + b;
+      assert(false);
+    } catch (std::runtime_error &e) {}
+  }
+
+  // 2) out-place function with 3 args
+  {
+    // basic
+    auto a = T.randn({3, 1, 1});
+    auto b = T.randn({1, 2, 1});
+    auto c = T.randn({1, 1, 5});
+    std::vector<int64_t> expanded_sizes = {3, 2, 5};
+    assert((a + b + c).equal(a.expand(expanded_sizes) + b.expand(expanded_sizes) + c.expand(expanded_sizes)));
+
+    // with scalar
+    auto aTensorScalar = T.ones({1});
+    aTensorScalar.get()->maybeScalar(true);
+    b = T.randn({3, 2, 1});
+    c = T.randn({1, 2, 5});
+    assert(aTensorScalar.addcmul(b, c).equal(
+           aTensorScalar.expand(expanded_sizes).addcmul(b.expand(expanded_sizes), c.expand(expanded_sizes))));
+
+    // old fallback behavior yields error
+    try {
+      auto a = T.randn({3, 2, 5});
+      auto b = T.randn({2, 3, 5});
+      auto c = T.randn({5, 3, 2});
+      a.addcmul(b, c);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+
+    // with mistmatched sizes
+    try {
+      auto c = T.randn({5, 5, 5});
+      a.addcmul(b, c);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+  }
+
+  // 3) in-place function with 2 args
+  {
+    // basic
+    auto a = T.randn({3, 5});
+    auto b = T.randn({3, 1});
+    assert((a + b).equal(a + b.expand({3, 5})));
+
+    // with scalar
+    auto bScalar = T.ones({1});
+    bScalar.get()->maybeScalar(true);
+    assert((a + bScalar).equal(a + bScalar.expand(a.sizes())));
+
+    // shouldn't be able to "expand" to a scalar_type
+    try {
+      auto a = T.ones({1});
+      bScalar.add_(a);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+    
+    // error: would have to expand inplace arg
+    try {
+      auto a = T.randn({1, 5});
+      auto b = T.randn({3, 1});
+      a.add_(b);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+  }
+
+  // 4) in-place function with 3 args
+  {
+    // basic
+    auto a = T.randn({3, 5, 2});
+    auto aClone = a.clone();
+    auto b = T.randn({3, 1, 2});
+    auto c = T.randn({1, 5, 1});
+
+    assert(a.addcmul_(b, c).equal(aClone.addcmul_(b.expand(a.sizes()), c.expand(a.sizes()))));
+
+    // with scalar
+    auto bScalar = T.ones({1});
+    bScalar.get()->maybeScalar(true);
+    assert(a.addcmul_(bScalar, c).equal(aClone.addcmul_(bScalar.expand(a.sizes()), c.expand(a.sizes()))));
+
+    // shouldn't be able to "expand" to a scalar_type
+    try {
+      auto a = T.ones({1});
+      bScalar.addcmul_(a, a);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+
+    // error: would have to expand inplace arg
+    try {
+      auto a = T.randn({1, 3, 5});
+      auto b = T.randn({4, 1, 1});
+      auto c = T.randn({1, 3, 1});
+      a.addcmul_(b, c);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+  }
+  
+  // explicit dim specification
+  {
+    // basic
+    auto a = T.randn({1});
+    auto b = T.randn({5, 3});
+    auto c = T.randn({3, 7});
+    assert(a.addmm(b, c).equal(a.expand({5,7}).addmm(b, c)));
+
+    // with scalar
+    Tensor aScalar = T.ones({1});
+    aScalar.get()->maybeScalar(true);
+    assert(aScalar.addmm(b, c).equal(aScalar.expand({5, 7}).addmm(b, c)));
+
+    // with mistmatched sizes
+    try {
+      auto a = T.randn({3, 3});
+      a.addmm(b, c);
+      assert(false);
+    } catch(std::runtime_error &e) {}
+  }
+
+  return 0;
+}
