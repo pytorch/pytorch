4d04ce2307bafbd2abc0c2704e4372127e75336b
Zach DeVito <zdevito@fb.com>
2017-08-31 18:42:17 -0700

    make API debuggable

--- torch/csrc/toffee/export.h
+++ torch/csrc/toffee/export.h
@@ -7,6 +7,7 @@ namespace torch { namespace jit {
 // Exports a graph to ToffeeIR
 std::string ExportGraph(std::shared_ptr<Graph>& graph,
                         const std::unordered_map<void*, Node*>& buffer_map,
-                        const std::vector<at::Tensor> & initializers);
+                        const std::vector<at::Tensor> & initializers,
+                        bool verbose=false);
 
 }}
