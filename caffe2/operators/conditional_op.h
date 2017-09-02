94ee7bbb08ba58cdf3b3f7732b75a7c3accd0f88
Curtis Huang <curtishuang@fb.com>
2017-09-01 19:11:04 -0700

    Use char-ngram embedding for out-of-vocabulary words
    Summary:
    **Description**
    
    Provide DeepText model with the functionality to load a secondary index (pre-trained char-ngram embedding, e.g. FastText) during training/test.  Embeddings of out-of-vocabulary words will be computed on-the-fly during training/test by averaging the char-ngram embeddings.
    
    **Approach**
    
    This diff provides two custom operators to accomplish this task – ConditionalOp and IndexCharNgramGetOp.  We first use IndexCharNgramGetOp to perform char-ngram index lookup and return a sparse tensor segmented by lengths for each token.  The sparse tensor is then used to compute the average embedding provided by the char-ngram index.  Finally, we use a ConditionalOp to replace those whose embeddings were not found in the original index during the feature apply stage.  Please refer to documentations of the code for more details.
    
    Reviewed By: jamesr66a
    
    Differential Revision: D5666924
    
    fbshipit-source-id: f76605d093154a014d5b9ebf9510de9d79874eee

---
+++ caffe2/operators/conditional_op.h
@@ -0,0 +1,24 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#ifndef CONDITIONAL_OP_H
+#define CONDITIONAL_OP_H
+
+#include "caffe2/core/context.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/core/tensor.h"
+
+namespace caffe2 {
+
+template <class Context>
+class ConditionalOp final : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  ConditionalOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws) {}
+
+  bool RunOnDevice() override;
+};
+
+} // caffe2
+
+#endif
