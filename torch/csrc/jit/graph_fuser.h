ezyang/pytorch-unattached

Remove GlobalTracingState

torch/csrc/jit/graph_fuser.h
@@ -4,6 +4,6 @@
 
 namespace torch { namespace jit {
 
-std::unique_ptr<Graph> FuseGraph(std::unique_ptr<Graph> graph);
+void FuseGraph(std::shared_ptr<Graph>& graph);
 
 }}

torch/csrc/jit/init_pass.cpp
@@ -22,7 +22,7 @@ std::unordered_map<std::string, constructor_type> constructors = {
 
 } // anonymous namespace
 
-std::unique_ptr<Graph> InitializePyGraph(std::unique_ptr<Graph> graph) {
+void InitializePyGraph(std::shared_ptr<Graph>& graph) {
   auto & nodes = graph->nodes();
   auto it = nodes.begin();
   while (it != nodes.end()) {
@@ -58,7 +58,6 @@ std::unique_ptr<Graph> InitializePyGraph(std::unique_ptr<Graph> graph) {
     ++it;
     p->destroy();
   }
-  return graph;
 }
 
 }}

torch/csrc/jit/ir.cpp
@@ -160,7 +160,7 @@ void Node::lint() {
   IR_ELSEIF(Select)
     JIT_ASSERT(inputs_.size() == 1);
   IR_ELSEIF(PythonOp)
-    int n_scalars = 0, n_tensors = 0;
+    std::size_t n_scalars = 0, n_tensors = 0;
     for (auto c : value->cconv) {
       if (c == 's') {
         n_scalars++;
@@ -262,9 +262,8 @@ void Graph::lint() {
 
 }
 
-std::unique_ptr<Graph> LintGraph(std::unique_ptr<Graph> graph) {
+void LintGraph(std::shared_ptr<Graph>& graph) {
   graph->lint();
-  return graph;
 }
 
 }}

pytorch/pytorch

torch/csrc/jit/graph_fuser.h
@@ -4,6 +4,6 @@
 
 namespace torch { namespace jit {
 
-std::unique_ptr<Graph> FuseGraph(std::unique_ptr<Graph> graph);
+void FuseGraph(std::unique_ptr<Graph>& graph);
 
 }}

torch/csrc/jit/init_pass.cpp
@@ -28,7 +28,7 @@ std::unordered_map<std::string, constructor_type> constructors = {
 
 } // anonymous namespace
 
-std::unique_ptr<Graph> MatchJITOps(std::unique_ptr<Graph> graph) {
+void MatchJITOps(std::unique_ptr<Graph>& graph) {
   auto & nodes = graph->nodes();
   auto it = nodes.begin();
   while (it != nodes.end()) {
@@ -67,7 +67,6 @@ std::unique_ptr<Graph> MatchJITOps(std::unique_ptr<Graph> graph) {
     ++it;
     p->destroy();
   }
-  return graph;
 }
 
 }}

torch/csrc/jit/ir.cpp
@@ -184,7 +184,7 @@ void Node::lint() {
   IR_ELSEIF(Select)
     JIT_ASSERT(inputs_.size() == 1);
   IR_ELSEIF(PythonOp)
-    int n_scalars = 0, n_tensors = 0;
+    std::size_t n_scalars = 0, n_tensors = 0;
     for (auto c : value->cconv) {
       if (c == 's') {
         n_scalars++;
@@ -288,9 +288,8 @@ void Graph::lint() {
 
 }
 
-std::unique_ptr<Graph> LintGraph(std::unique_ptr<Graph> graph) {
+void LintGraph(std::unique_ptr<Graph>& graph) {
   graph->lint();
-  return graph;
 }
 
 }}