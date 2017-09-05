af649c19a287a4f7a3b29feb30d46f02fc7556d1
Zach DeVito <zdevito@fb.com>
2017-09-05 13:15:43 -0700

    ONNXIR -> to ONNX

--- tools/gen_onnx.sh
+++ tools/gen_onnx.sh
@@ -4,11 +4,11 @@ set -ex
 (cd torch/lib/nanopb/generator/proto && make)
 # It always searches the same dir as the proto, so
 # we have got to copy the option file over
-cp torch/csrc/onnx.options torch/lib/ONNXIR/onnx/onnx.options
+cp torch/csrc/onnx.options torch/lib/onnx/onnx/onnx.options
 protoc --plugin=protoc-gen-nanopb=$PWD/torch/lib/nanopb/generator/protoc-gen-nanopb \
-       torch/lib/ONNXIR/onnx/onnx.proto \
+       torch/lib/onnx/onnx/onnx.proto \
        --nanopb_out=-T:.
 # NB: -T suppresses timestamp. See https://github.com/nanopb/nanopb/issues/274
 # nanopb generated C files are valid CPP! Yay!
-cp torch/lib/ONNXIR/onnx/onnx.pb.c torch/csrc/onnx.pb.cpp
-cp torch/lib/ONNXIR/onnx/onnx.pb.h torch/csrc/onnx.pb.h
+cp torch/lib/onnx/onnx/onnx.pb.c torch/csrc/onnx.pb.cpp
+cp torch/lib/onnx/onnx/onnx.pb.h torch/csrc/onnx.pb.h
--- torch/csrc/onnx/export.h
+++ torch/csrc/onnx/export.h
@@ -4,7 +4,7 @@
 
 namespace torch { namespace jit {
 
-// Exports a graph to ONNXIR
+// Exports a graph to ONNX
 std::string ExportGraph(std::shared_ptr<Graph>& graph,
                         const std::unordered_map<void*, Node*>& buffer_map,
                         const std::vector<at::Tensor> & initializers,
