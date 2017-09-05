1104bab7965c504a42f046715c07b126bb31cce9
Dong Li <dongli@fb.com>
2017-09-05 11:14:40 -0700

    add axis argument to NormalizeOp and NormalizeGradientOp
    Summary:
    As described in task T21337239, NormalizeOp currently normalizes over only the last dimension.
    In this commit, the following changes have been made:
    (1) Added an axis-parameter to NormalizeOp in both the CPU and CUDA context.
    (2) Added the same axis parameter to  NormalizeGradient in both the CPU and CUDA context
    (3) Removed the limit that the original NormalizeOp operator requires the input dimension to be 2
    
    Reviewed By: akyrola
    
    Differential Revision: D5745162
    
    fbshipit-source-id: 69e04f59ac4d954b0062c3b2a53c8ca465a1027b

--- caffe2/operators/normalize_op.h
+++ caffe2/operators/normalize_op.h
@@ -14,7 +14,25 @@ class NormalizeOp final : public Operator<Context> {
   NormalizeOp(const OperatorDef& def, Workspace* ws)
       : Operator<Context>(def, ws) {}
 
-  bool RunOnDevice() override;
+  bool RunOnDevice() override {
+    const auto& x = Input(0);
+    auto* y = Output(0);
+    const auto* xData = x.template data<T>();
+    y->ResizeLike(x);
+    auto* yData = y->template mutable_data<T>();
+
+    const auto canonical_axis = x.canonical_axis_index(
+        OperatorBase::GetSingleArgument<int>("axis", -1));
+    const int m = x.dim32(canonical_axis);
+    const int n = x.size() / m;
+    const int sf = x.size_from_dim(canonical_axis + 1);
+    DoNormalize(xData, yData, m, n, sf);
+    return true;
+  }
+
+ private:
+  void
+  DoNormalize(const T* xData, T* yData, const int m, const int n, const int sf);
 };
 
 template <typename T, class Context>
@@ -24,9 +42,34 @@ class NormalizeGradientOp final : public Operator<Context> {
   NormalizeGradientOp(const OperatorDef& def, Workspace* ws)
       : Operator<Context>(def, ws) {}
 
-  bool RunOnDevice() override;
+  bool RunOnDevice() override {
+    const auto& x = Input(0);
+    const auto& gOut = Input(GRAD_OUT);
+    auto* gIn = Output(GRAD_IN);
+    gIn->ResizeLike(gOut);
+
+    const auto* xData = x.template data<T>();
+    const auto* gOutData = gOut.template data<T>();
+    auto* gInData = gIn->template mutable_data<T>();
+
+    const auto canonical_axis = x.canonical_axis_index(
+        OperatorBase::GetSingleArgument<int>("axis", -1));
+    const int m = x.dim32(canonical_axis);
+    const int n = x.size() / m;
+    const int sf = x.size_from_dim(canonical_axis + 1);
+    DoNormalize(xData, gOutData, gInData, m, n, sf);
+    return true;
+  }
 
  private:
+  void DoNormalize(
+      const T* xData,
+      const T* gOutData,
+      T* gInData,
+      const int m,
+      const int n,
+      const int sf);
+
   INPUT_TAGS(INPUT, GRAD_OUT);
   OUTPUT_TAGS(GRAD_IN);
 };
