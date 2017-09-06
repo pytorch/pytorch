e33d154bcc7aa50c1e11ca192f7f0090fd6eeb9b
Gregory Chanan <gchanan@fb.com>
2017-09-06 10:40:51 -0700

    Support wrap_dim specifications from cwrap.

--- aten/tools/run_tests.sh
+++ aten/tools/run_tests.sh
@@ -6,4 +6,5 @@ $BUILD_ROOT/src/ATen/test/basic
 $BUILD_ROOT/src/ATen/test/atest
 $BUILD_ROOT/src/ATen/test/scalar_test
 $BUILD_ROOT/src/ATen/test/broadcast_test
+$BUILD_ROOT/src/ATen/test/wrapdim_test
 valgrind --suppressions=`dirname $0`/valgrind.sup --error-exitcode=1 $BUILD_ROOT/src/ATen/test/basic -n
