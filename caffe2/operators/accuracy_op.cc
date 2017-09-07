e10cc93e0e8ba8b0b7d663f36e30fa400fd1e223
Jongsoo Park <jongsoo@fb.com>
2017-09-07 15:01:30 -0700

    add documentation on top_k option in accuracy op
    Summary: top_k argument was missing in accuracy op documentation
    
    Reviewed By: zem7
    
    Differential Revision: D5758807
    
    fbshipit-source-id: ca8a0c172d0a5eafb825a0b134529294edc0b8b4

--- caffe2/operators/accuracy_op.cc
+++ caffe2/operators/accuracy_op.cc
@@ -57,6 +57,10 @@ containing a batch of scores for various classes, and labels are expected in the
 the score for the label index in the predictions is the highest among all
 classes, it is considered a correct prediction.
 )DOC")
+  .Arg(
+      "top_k",
+      "Count as correct by comparing the true label to the top k scoring "
+      "clsses (default 1: only compare to the top scoring class i.e. argmax)")
   .Input(0, "predictions", "2-D tensor (Tensor<float>) of size "
          "(num_batches x num_classes) containing scores")
   .Input(1, "labels", "1-D tensor (Tensor<int>) of size (num_batches) having "
