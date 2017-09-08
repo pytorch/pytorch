6763c14e84824b04e434f8d747afd0f99ad5fe85
Honghao Wei <weihh16@fb.com>
2017-09-08 11:32:06 -0700

    add base class ModifierContext, rewrite OptimizerContext, add RegularizerContext
    Summary:
    `ModifierContext` is the base class for `OptimizerContext` and `RegularizationContext`.
    `UseModifierBase` is the base class for `UseRegularizer `and `UseOptimizer`
    
    Most of codes in `OptimizerContext`, `RegularizationContext` and other potential Context class in future could be shared. We thus implemented a new base class, called `ModifierContext` to support it.
    
    It happens to be the same for `UseRegularizer` and `UseOptimizer`, and we implemented a new base  class called `UseModifierBase`.
    
    In this way, users only need to provide API for **get** and **has** operation. Also, they need to tell what's the **context class**.
    
    **Note**
    Mirrored code in fbandroid and fbobj would be added when finally check in.
    
    Reviewed By: kittipatv, xianjiec
    
    Differential Revision: D5724613
    
    fbshipit-source-id: de19bb822dcd41ec5c459d65065603a0abe2fd20

--- caffe2/python/optimizer_context.py
+++ caffe2/python/optimizer_context.py
@@ -6,45 +6,29 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from caffe2.python import context
+from caffe2.python.modifier_context import (
+    ModifierContext, UseModifierBase)
+
 
 DEFAULT_OPTIM = 'DEFAULT'
 
 
 @context.define_context(allow_default=True)
-class OptimizerContext(object):
+class OptimizerContext(ModifierContext):
     """
     provide context to allow param_info to have different optimizers
     """
 
-    def __init__(self):
-        self._optimizers = {}
-        self._optimizers_list = []
-
-    def _rebuild_optimizers(self):
-        self._optimizers = {}
-        for m in self._optimizers_list:
-            self._optimizers.update(m)
-
     def has_optimizer(self, name):
-        return name in self._optimizers
+        return self._has_modifier(name)
 
     def get_optimizer(self, name):
         assert self.has_optimizer(name), (
             "{} optimizer is not provided!".format(name))
-        return self._optimizers.get(name)
-
-    def push_optimizers(self, optimizers):
-        # optimizer override is allowed
-        self._optimizers_list.append(optimizers)
-        self._optimizers.update(optimizers)
-
-    def pop_optimizers(self):
-        assert len(self._optimizers_list) > 0
-        self._optimizers_list.pop()
-        self._rebuild_optimizers()
+        return self._get_modifier(name)
 
 
-class UseOptimizer(object):
+class UseOptimizer(UseModifierBase):
     '''
     context class to allow setting the current context.
     Example usage with brew:
@@ -66,16 +50,5 @@ class UseOptimizer(object):
             optim = OptimizerContext.current().get_optimizer('optim1')
             layer(optim=optim)
     '''
-
-    def __init__(self, optim_or_dict):
-        if isinstance(optim_or_dict, dict):
-            self._optimizers = optim_or_dict
-        else:
-            self._optimizers = {DEFAULT_OPTIM: optim_or_dict}
-
-    def __enter__(self):
-        OptimizerContext.current().push_optimizers(self._optimizers)
-        return self
-
-    def __exit__(self, type, value, traceback):
-        OptimizerContext.current().pop_optimizers()
+    def _context_class(self):
+        return OptimizerContext
