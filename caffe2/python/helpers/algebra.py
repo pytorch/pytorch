81026cc058ca36dcb06cd21afb7395f02c93e81b
Luke Yeager <lukeyeager@users.noreply.github.com>
2017-09-13 14:21:38 -0700

    fp16: BatchMatMul
    Summary:
    Was https://github.com/caffe2/caffe2/pull/1151
    Closes https://github.com/caffe2/caffe2/pull/1175
    
    Reviewed By: Yangqing
    
    Differential Revision: D5794634
    
    Pulled By: akyrola
    
    fbshipit-source-id: 911c462824edec3de529a5a4385a4c437e24bf59

--- caffe2/python/helpers/algebra.py
+++ caffe2/python/helpers/algebra.py
@@ -16,3 +16,10 @@ def transpose(model, blob_in, blob_out, use_cudnn=False, **kwargs):
 def sum(model, blob_in, blob_out, **kwargs):
     """Sum"""
     return model.net.Sum(blob_in, blob_out, **kwargs)
+
+def batch_mat_mul(model, blob_in, blob_out,
+                  enable_tensor_core=False, **kwargs):
+    if enable_tensor_core:
+        kwargs['engine'] = 'TENSORCORE'
+
+    return model.net.BatchMatMul(blob_in, blob_out, **kwargs)
