e878bc79e62843d2ea7d49a50b1d3d69ff644ee5
Richard Zou <zou3519@gmail.com>
2017-09-14 08:47:07 -0700

    Implement THCUNN code for GridSampler

--- torch/nn/_functions/vision.py
+++ torch/nn/_functions/vision.py
@@ -9,50 +9,24 @@ import torch.backends.cudnn as cudnn
 class GridSampler(Function):
 
     @staticmethod
-    def _enforce_cudnn(input):
-        if not cudnn.enabled:
-            raise RuntimeError("GridSampler needs CuDNN for processing CUDA inputs,"
-                               " but CuDNN is not enabled")
-        assert cudnn.is_acceptable(input)
-
-    @staticmethod
     def forward(ctx, input, grid):
         ctx.save_for_backward(input, grid)
         grid_sz = grid.size()
-        if input.is_cuda:
-            GridSampler._enforce_cudnn(input)
-            output = input.new(grid_sz[0], input.size(1), grid_sz[1], grid_sz[2])
-            grid = grid.contiguous()
-            if 0 in input.stride():
-                input = input.contiguous()
-            torch._C._cudnn_grid_sampler_forward(input, grid, output)
-        else:
-            backend = type2backend[type(input)]
-            output = input.new(grid_sz[0], input.size(1), grid_sz[1], grid_sz[2])
-            backend.SpatialGridSamplerBilinear_updateOutput(backend.library_state, input, grid, output)
+        backend = type2backend[type(input)]
+        output = input.new(grid_sz[0], input.size(1), grid_sz[1], grid_sz[2])
+        backend.SpatialGridSamplerBilinear_updateOutput(backend.library_state, input, grid, output)
         return output
 
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output):
         input, grid = ctx.saved_tensors
-        if input.is_cuda:
-            GridSampler._enforce_cudnn(input)
-            grad_input = input.new(input.size())
-            grad_grid = grid.new(grid.size())
-            grid = grid.contiguous()
-            if 0 in input.stride():
-                input = input.contiguous()
-            torch._C._cudnn_grid_sampler_backward(input, grad_input,
-                                                  grid, grad_grid,
-                                                  grad_output)
-        else:
-            backend = type2backend[type(input)]
-            grad_input = input.new(input.size())
-            grad_grid = grid.new(grid.size())
-            backend.SpatialGridSamplerBilinear_updateGradInput(
-                backend.library_state, input, grad_input,
-                grid, grad_grid, grad_output)
+        backend = type2backend[type(input)]
+        grad_input = input.new(input.size())
+        grad_grid = grid.new(grid.size())
+        backend.SpatialGridSamplerBilinear_updateGradInput(
+            backend.library_state, input, grad_input,
+            grid, grad_grid, grad_output)
         return grad_input, grad_grid
 
 
