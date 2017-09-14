670ec4bc5950dd70c5d69af0877989d213bfafcb
Edward Z. Yang <ezyang@fb.com>
2017-09-13 18:58:51 -0700

    Split Type into its own header file.
    No other substantive changes.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ torch/csrc/jit/type.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include "torch/csrc/jit/interned_strings.h"
+#include "torch/csrc/jit/generic_if.h"
+#include "torch/csrc/jit/assert.h"
+
+#include <ATen/ATen.h>
+
+#include <memory>
+#include <iostream>
+
+namespace torch { namespace jit {
+
+#define TH_FORALL_TYPES(_) \
+_(MultiType) \
+_(TensorType) \
+_(HandleType)
+
+enum class TypeKind {
+#define DEFINE_TYPE(T) T,
+  TH_FORALL_TYPES(DEFINE_TYPE)
+#undef DEFINE_TYPE
+};
+
+struct Type;
+using TypePtr = std::shared_ptr<Type>;
+
+struct Type {
+private:
+  TypeKind kind_;
+
+protected:
+  Type(TypeKind kind)
+    : kind_(kind) {}
+
+public:
+  TypeKind kind() const {
+    return kind_;
+  }
+
+  // Dynamically cast this object to the subclass indicated by the
+  // template variable, returning nullptr if the cast is invalid..
+  template<typename T>
+  T* cast() {
+    if (T::Kind == kind())
+      return static_cast<T*>(this);
+    return nullptr;
+  }
+  template<typename T>
+  T* expect() {
+    JIT_ASSERT(T::Kind == kind());
+    return static_cast<T*>(this);
+  }
+};
+
+// This node represents a single Tensor value
+struct TensorType : public Type {
+  friend struct Type;
+  TensorType(const at::Tensor& tensor)
+    : Type(TypeKind::TensorType)
+    , scalar_type_(tensor.type().scalarType())
+    , device_(tensor.type().isCuda() ? tensor.get_device() : -1)
+    , sizes_(tensor.sizes())
+    , strides_(tensor.strides()) {}
+
+  static const TypeKind Kind = TypeKind::TensorType;
+
+  at::ScalarType scalarType() const { return scalar_type_; }
+  int device() const { return device_; }
+  const std::vector<std::int64_t>& sizes() const { return sizes_; }
+  const std::vector<std::int64_t>& strides() const { return strides_; }
+
+  TypePtr contiguous() const {
+    auto t = std::make_shared<TensorType>(*this);
+    t->strides_.resize(sizes_.size());
+    t->strides_.back() = 1;
+    for(size_t i = t->strides_.size() - 1; i > 0; i--) {
+      t->strides_[i-1] = t->strides_[i] * t->sizes_[i];
+    }
+    return t;
+  }
+private:
+  at::ScalarType scalar_type_;
+  int device_;
+  std::vector<int64_t> sizes_;
+  std::vector<int64_t> strides_;
+};
+
+// Type of multireturn nodes. Note that it doesn't mean that they must always
+// have multiple outputs, but each output will be represented with a select node.
+struct MultiType : public Type {
+  friend struct Type;
+
+  MultiType()
+    : Type(TypeKind::MultiType) {}
+
+public:
+  static const TypeKind Kind = TypeKind::MultiType;
+};
+
+// This value represents an opaque handle to external state.
+// Operators that produce/consume values of this type agree on
+// the format.
+
+/* Example Usage: passing state to opaque autograd Functions:
+graph(%1, %8) {
+  %2.0, %2.1 = ^AddConstant(2, False)(%1) // first output is Type::Handle, containing ctx
+  %4.0, %4.1 = ^Add(False)(%2.1, %1) // first output is Type::Handle, containing ctx
+  %6.0, %6.1 = ^Abs()(%4.1) // first output is Type::Handle, containing ctx
+  ---------------- stage 1 ----------------
+  %13 = AutogradOp[AbsBackward](%6.0, %8) // first argument is Type::Handle, consuming ctx
+  %15 = AutogradOp[AddBackward](%4.0, %13.0) // first argument is Type::Handle, consuming ctx
+  %18 = AutogradOp[AddConstantBackward](%2.0, %15.1) // first argument is Type::Handle, consuming ctx
+  %20 = AutogradOp[N5torch8autograd3AddE](%18.0, %18.0)
+  return (%6.0, %20.0);
+}
+*/
+struct HandleType : public Type {
+  friend struct Type;
+
+  HandleType()
+    : Type(TypeKind::HandleType) {}
+
+public:
+  static const TypeKind Kind = TypeKind::HandleType;
+};
+
+inline TypePtr multiType() {
+  static TypePtr multiType = std::make_shared<MultiType>();
+  return multiType;
+}
+
+std::ostream& operator<<(std::ostream & out, const Type & t);
+
+// Immutable case
+#define TYPE_IF(x,Kind) GENERIC_IF(const,TypeKind::Kind,x,Kind)
+#define TYPE_ELSEIF(Kind) GENERIC_ELSEIF(const,TypeKind::Kind,Kind)
+#define TYPE_ELSE() GENERIC_ELSE()
+#define TYPE_END() GENERIC_END()
+
+}} // namespace torch::jit
