99141e62a682ad0d1e10974eccad12e0cbdc5e51
peter <peter_jiachen@163.com>
2017-09-18 10:07:14 +0800

    Fix build failure in MSVC

--- aten/src/ATen/Formatting.h
+++ aten/src/ATen/Formatting.h
@@ -6,11 +6,10 @@
 
 namespace at {
 
-
-std::ostream& operator<<(std::ostream & out, IntList list);
-std::ostream& operator<<(std::ostream & out, Backend b);
-std::ostream& operator<<(std::ostream & out, ScalarType t);
-std::ostream& print(std::ostream& stream, const Tensor & tensor, int64_t linesize);
+ATen_CLASS std::ostream& operator<<(std::ostream & out, IntList list);
+ATen_CLASS std::ostream& operator<<(std::ostream & out, Backend b);
+ATen_CLASS std::ostream& operator<<(std::ostream & out, ScalarType t);
+ATen_CLASS std::ostream& print(std::ostream& stream, const Tensor & tensor, int64_t linesize);
 static inline std::ostream& operator<<(std::ostream & out, const Tensor & t) {
   return print(out,t,80);
 }
