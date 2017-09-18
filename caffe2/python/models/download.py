66ee3de903e9678b9a79efbcb1d158c40cb8ccab
Junjie Bai <jbai@fb.com>
2017-09-17 20:13:37 -0700

    Add an argument for suppressing download progress
    Summary: In some cases (e.g. CI), showing progress bar will mess up the log.
    
    Reviewed By: jerryzh168
    
    Differential Revision: D5850918
    
    fbshipit-source-id: 2da9d020832264cef977391dc2fd8d1e2677d159

--- caffe2/python/models/download.py
+++ caffe2/python/models/download.py
@@ -51,23 +51,25 @@ def progressBar(percentage):
     sys.stdout.flush()
 
 
-def downloadFromURLToFile(url, filename):
+def downloadFromURLToFile(url, filename, show_progress=True):
     try:
         print("Downloading from {url}".format(url=url))
         response = urllib.urlopen(url)
         size = int(response.info().get('Content-Length').strip())
-        downloaded_size = 0
         chunk = min(size, 8192)
         print("Writing to {filename}".format(filename=filename))
-        progressBar(0)
+        if show_progress:
+            downloaded_size = 0
+            progressBar(0)
         with open(filename, "wb") as local_file:
             while True:
                 data_chunk = response.read(chunk)
                 if not data_chunk:
                     break
                 local_file.write(data_chunk)
-                downloaded_size += len(data_chunk)
-                progressBar(int(100 * downloaded_size / size))
+                if show_progress:
+                    downloaded_size += len(data_chunk)
+                    progressBar(int(100 * downloaded_size / size))
         print("")  # New line to fix for progress bar
     except HTTPError as e:
         raise Exception("Could not download model. [HTTP Error] {code}: {reason}."
