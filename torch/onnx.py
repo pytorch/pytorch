Ask for the correct number of derivatives when tracing.
- If you operate with TracingState, you MUST check if it is live.
  Otherwise you will segfault if it is expired; it is VALID for
  tracing states to become expired.

- Tracing states can expire if they request backward tracing
  (which the tracer does by default).  We don't want this to
  happen for exports, which only look at forwards.  So make
  sure we set the correct num_derivatives.

Signed-off-by: Edward Z. Yang <ezyang@fb.com>

torch/onnx.py
@@ -94,7 +94,7 @@ def _export(model, args, f, export_params=True, verbose=False, training=False):
     # It's important to run the model in inference mode when exporting;
     # otherwise internal buffers may get updated, dropout gets applied, etc.
     with set_training(model, training):
-        trace, torch_out = torch.jit.record_trace(model, *args)
+        trace, torch_out = torch.jit.record_trace(model, *args, num_derivatives=0)
     torch._C._jit_pass_onnx(trace)
     if verbose:
         print(trace)