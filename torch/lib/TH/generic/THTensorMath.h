c8993a3e2cda68ccb270366119655cd2c29c3770
Sam Gross <colesbury@gmail.com>
2017-09-20 11:39:40 -0400

    Add add_scaled and sub_scaled to TH and THC (#2789)
    These functions accept a scaling parameter like THTensor_(cadd)/(csub),
    which will make it easier to have the same signature for tensor and
    scalar addition in PyTorch and ATen. For example:
    
      tensor.add(other, alpha=2)
    
    Will work if other is a scalar or a tensor value.
    
    See #2739

--- torch/lib/TH/generic/THTensorMath.h
+++ torch/lib/TH/generic/THTensorMath.h
@@ -33,7 +33,9 @@ TH_API void THTensor_(neg)(THTensor *self, THTensor *src);
 TH_API void THTensor_(cinv)(THTensor *self, THTensor *src);
 
 TH_API void THTensor_(add)(THTensor *r_, THTensor *t, real value);
-TH_API void THTensor_(sub)(THTensor *self, THTensor *src, real value);
+TH_API void THTensor_(sub)(THTensor *r_, THTensor *t, real value);
+TH_API void THTensor_(add_scaled)(THTensor *r_, THTensor *t, real value, real alpha);
+TH_API void THTensor_(sub_scaled)(THTensor *r_, THTensor *t, real value, real alpha);
 TH_API void THTensor_(mul)(THTensor *r_, THTensor *t, real value);
 TH_API void THTensor_(div)(THTensor *r_, THTensor *t, real value);
 TH_API void THTensor_(lshift)(THTensor *r_, THTensor *t, real value);
