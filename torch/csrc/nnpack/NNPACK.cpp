d58c47249a1b72947642329abd03014699498ef5
Trevor Killeen <killeentm@gmail.com>
2017-09-20 13:58:41 -0700

    skeleton commit for building and linking nnpack library in PyTorch

---
+++ torch/csrc/nnpack/NNPACK.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+
+namespace torch {
+namespace nnpack {
+
+void someFunc() {
+  std::exit(123);
+}
+
+} // torch::nnpack
+} // torch
---
+++ torch/csrc/nnpack/NNPACK.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "nnpack.h"
+
+namespace torch {
+namespace nnpack {
+
+void someFunc();
+
+} // torch::nnpack
+} // torch
