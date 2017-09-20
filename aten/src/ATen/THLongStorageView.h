a10030eec79fa72abea103f9fbf4a0d37105569c
Gregory Chanan <gchanan@fb.com>
2017-09-20 11:19:23 -0700

    Represent empty tensors as size {0} tensors and fix scalar checks.
    This gets rid of kUndefinedDimensions and has nice properties like:
    - the dimensionality always matches the length of the sizes and strides.
    - the number of elements is always the product of the sizes (starting at the identity)
    - the shape you pass to factory functions (e.g. randn) matches the shape that is returned
    etc.
    
    In addition to the empty tensor change, this makes some related changes:
    1) expand is now a native function, because it needs to operate on the ATen view of the size/strides.
    2) adds tests for a number of functions operating on empty, scalar, non-scalar tensors.
    This uncovered a number of scalar_check bugs; some of these are fixed in the generated code,
    some that need to be manually specified can be specified by a 'scalar_check' argument in the cwrap.
    3) fixes the formatting of empty tensors
    4) changes the THLongStorageView API; the public API was getting overly complicated, so now you call
    'makeFromSize', 'makeFromStride', 'makeFromLength' and it just handles the correct mapping for that type.

--- aten/src/ATen/THLongStorageView.h
+++ aten/src/ATen/THLongStorageView.h
@@ -4,25 +4,37 @@
 
 namespace at {
 
+static inline bool is_noelem_tensor_size(ArrayRef<int64_t> size) {
+  return size.size() == 1 && size[0] == 0;
+}
+
 // make a fake storage out of a size, pointer pair...
 // used as an argument where THSize and THStride are passed into TH
 class THLongStorageView {
 public:
-  // zero_dim_to_one converts an empty ArrayRef into [1]
-  // empty_to_null converts an empty ArrayRef into a null THLongStorage
-  static THLongStorageView make(ArrayRef<int64_t> ref, bool zero_dim_to_one = false, bool empty_to_null = false) {
-    assert(!(zero_dim_to_one && empty_to_null));
-    return THLongStorageView(ref, zero_dim_to_one, empty_to_null);
+  static THLongStorageView makeFromSize(ArrayRef<int64_t> ref) {
+    return THLongStorageView(ref, true, false, false);
+  }
+  // noelem_to_empty is to differentiate strides of empty tensors vs scalars.  In ATen, both may have strides [1],
+  // but in TH an empty tensor should have stride [], while a scalar should have stride [1].
+  static THLongStorageView makeFromStride(ArrayRef<int64_t> ref, bool noelem_to_empty) {
+    return THLongStorageView(ref, false, true, noelem_to_empty);
+  }
+  static THLongStorageView makeFromLength(ArrayRef<int64_t> ref) {
+    return THLongStorageView(ref, false, false, false);
   }
   operator THLongStorage*() {
-    if (storage.size == 0 && empty_to_null) {
+    if (storage.size == 0 && zero_dim_to_null) {
       return nullptr;
     }
     return &storage;
   }
 private:
-  THLongStorageView(ArrayRef<int64_t> ref, bool zero_dim_to_one, bool empty_to_null)
-    : empty_to_null(empty_to_null)
+  // zero_dim_to_one converts an empty ArrayRef into [1]
+  // zero_dim_to_null converts an empty ArrayRef into a null THLongStorage
+  // noelem_to_empty makes an ArrayRef of [0] into an empty THLongStorage
+  THLongStorageView(ArrayRef<int64_t> ref, bool zero_dim_to_one, bool zero_dim_to_null, bool noelem_to_empty)
+    : zero_dim_to_null(zero_dim_to_null)
   {
     if(zero_dim_to_one && ref.size() == 0) {
       // make storage of size 0 actually a 1-length storage with 1 element
@@ -30,7 +42,11 @@ private:
       one = 1;
       storage.data = &one;
       storage.size = 1;
-    } else {
+    } else if (noelem_to_empty && is_noelem_tensor_size(ref)) {
+      storage.data = (int64_t*)(ref.data());
+      storage.size = 0;
+    }
+    else {
       storage.data = (int64_t*)(ref.data());
       storage.size = ref.size();
     }
@@ -41,7 +57,7 @@ private:
   }
   int64_t one;
   THLongStorage storage;
-  bool empty_to_null;
+  bool zero_dim_to_null;
 };
 
 }
--- aten/tools/run_tests.sh
+++ aten/tools/run_tests.sh
@@ -9,4 +9,5 @@ $BUILD_ROOT/src/ATen/test/broadcast_test
 $BUILD_ROOT/src/ATen/test/wrapdim_test
 $BUILD_ROOT/src/ATen/test/dlconvertor_test
 $BUILD_ROOT/src/ATen/test/native_test
+$BUILD_ROOT/src/ATen/test/scalar_tensor_test
 valgrind --suppressions=`dirname $0`/valgrind.sup --error-exitcode=1 $BUILD_ROOT/src/ATen/test/basic -n
