090af152c00d84b0b41ed6617aac2a49e56ea4c4
Edward Z. Yang <ezyang@fb.com>
2017-09-22 14:22:21 -0700

    Indicate if the last invocation of setup.py was debug or not.
    How to use:
    
        import torch.version
        print(torch.version.debug)
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- setup.py
+++ setup.py
@@ -162,6 +162,10 @@ class build_py(setuptools.command.build_py.build_py):
         version_path = os.path.join(cwd, 'torch', 'version.py')
         with open(version_path, 'w') as f:
             f.write("__version__ = '{}'\n".format(version))
+            # NB: This is not 100% accurate, because you could have built the
+            # library code with DEBUG, but csrc without DEBUG (in which case
+            # this would claim to be a release build when it's not.)
+            f.write("debug = {}\n".format(repr(DEBUG)))
 
 
 class develop(setuptools.command.develop.develop):
