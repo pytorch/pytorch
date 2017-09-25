670d3d973ead52f9b4cef13476fd4823d16e86c7
Adam Paszke <adam.paszke@gmail.com>
2017-09-25 11:58:04 -0700

    Always regenerate nn wrappers after rebuilding THNN and THCUNN

--- setup.py
+++ setup.py
@@ -91,11 +91,15 @@ def build_libs(libs):
     my_env["PYTORCH_PYTHON"] = sys.executable
     if WITH_SYSTEM_NCCL:
         my_env["NCCL_ROOT_DIR"] = NCCL_ROOT_DIR
-
     if WITH_CUDA:
         build_libs_cmd += ['--with-cuda']
+
     if subprocess.call(build_libs_cmd + libs, env=my_env) != 0:
         sys.exit(1)
+
+    if 'THNN' in libs or 'THCUNN' in libs:
+        from tools.nnwrap import generate_wrappers as generate_nn_wrappers
+        generate_nn_wrappers()
 
 
 class build_deps(Command):
@@ -119,9 +123,6 @@ class build_deps(Command):
                 libs += ['gloo']
             libs += ['THD']
         build_libs(libs)
-
-        from tools.nnwrap import generate_wrappers as generate_nn_wrappers
-        generate_nn_wrappers()
 
 
 build_dep_cmds = {}
