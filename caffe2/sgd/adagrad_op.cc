aff8cc0d7e4ac21e8d0df80029ebdc940817f236
Frank Jiang <ffjiang@fb.com>
2017-09-26 13:32:01 -0700

    Implemented RowWiseSparseAdagrad operator that only keeps one moment term per embedding
    Summary: Implemented version of SparseAdagrad that only keeps track of an average sum of squared gradients term for each row of the parameter tensor, rather than a sum of squared gradients term for each individual parameter.
    
    Differential Revision: D5881918
    
    fbshipit-source-id: bd96ccf25554b457baaaca9309fc8048adbb37f7

--- caffe2/sgd/adagrad_op.cc
+++ caffe2/sgd/adagrad_op.cc
@@ -10,13 +10,13 @@ OPERATOR_SCHEMA(Adagrad)
     .SetDoc(R"DOC(
 
 Computes the AdaGrad update for an input gradient and accumulated
-history. Concretely, given inputs (param, grad, history, learning_rate),
+history. Concretely, given inputs (param, grad, moment, learning_rate),
 computes
 
-    new_history = history + square(grad)
-    new_grad = learning_rate * grad / (sqrt(new_history) + epsilon)
+    new_moment = moment + square(grad)
+    new_grad = learning_rate * grad / (sqrt(new_moment) + epsilon)
     new_param = param + new_grad
-and returns (new_param, new_history).
+and returns (new_param, new_moment).
 
 )DOC")
     .Input(0, "param", "Parameters to be updated")
@@ -35,12 +35,39 @@ REGISTER_CPU_OPERATOR(SparseAdagrad, SparseAdagradOp<float, CPUContext>);
 OPERATOR_SCHEMA(SparseAdagrad)
     .NumInputs(5)
     .NumOutputs(2)
-    .EnforceInplace({{0, 0}, {1, 1}})
+    .EnforceOneToOneInplace()
     .SetDoc(R"DOC(
 
-Given inputs (param, history, indices, grad, lr), runs the dense AdaGrad
-update on (param, grad, history[indices], lr), and returns (new_param,
-new_history) as in the dense case.
+Given inputs (param, moment, indices, grad, lr), runs the dense AdaGrad
+update on (param, grad, moment[indices], lr), and returns (new_param,
+new_moment) as in the dense case.
+
+)DOC")
+    .Input(0, "param", "Parameters to be updated")
+    .Input(1, "moment", "Moment history")
+    .Input(2, "indices", "Sparse indices")
+    .Input(3, "grad", "Gradient computed")
+    .Input(4, "lr", "learning rate")
+    .Output(0, "output_param", "Updated parameters")
+    .Output(1, "output_moment_1", "Updated moment")
+    .Arg("epsilon", "Default 1e-5");
+
+REGISTER_CPU_OPERATOR(
+    RowWiseSparseAdagrad,
+    RowWiseSparseAdagradOp<float, CPUContext>);
+OPERATOR_SCHEMA(RowWiseSparseAdagrad)
+    .NumInputs(5)
+    .NumOutputs(2)
+    .EnforceOneToOneInplace()
+    .SetDoc(R"DOC(
+
+Given inputs (param, moment, indices, grad, lr), runs a modified sparse Adagrad
+update on (param, grad, moment[indices], lr), and returns (new_param,
+new_momwnr), where moment is a 1D tensor with length equal to the number of
+rows in param: shape(moment) == shape(param)[0]. Each element of moment is
+applied to an entire row of param, and the new moment is calculated by adding
+the average squared sum of gradients across each row. Note that indices must
+also be a 1D tensor indexing into the rows of param.
 
 )DOC")
     .Input(0, "param", "Parameters to be updated")
@@ -54,4 +81,5 @@ new_history) as in the dense case.
 
 SHOULD_NOT_DO_GRADIENT(Adagrad);
 SHOULD_NOT_DO_GRADIENT(SparseAdagrad);
+SHOULD_NOT_DO_GRADIENT(RowWiseSparseAdagrad);
 }
