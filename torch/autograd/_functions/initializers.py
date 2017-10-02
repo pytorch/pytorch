621603169c19308e13ac071be803c92099c739c6
Alykhan Tejani <atejani@twitter.com>
2017-10-02 14:46:01 +0100

    initialize new tensor

--- torch/autograd/_functions/initializers.py
+++ torch/autograd/_functions/initializers.py
@@ -17,4 +17,4 @@ class Zero(InplaceFunction):
     @staticmethod
     def backward(ctx, grad_output):
         result, = ctx.saved_variables
-        return Variable(result.data.new(1).expand_as(result))
+        return Variable(result.data.new(1).zero_().expand_as(result))
