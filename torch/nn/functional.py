ba766ef39a4fff2d8856e17747393d469e409775
SsnL <SsnL@users.noreply.github.com>
2017-10-04 16:03:20 -0400

    Fix BN size check in eval mode (#2977)

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -702,9 +702,10 @@ def embedding(input, embedding_matrix,
 
 def batch_norm(input, running_mean, running_var, weight=None, bias=None,
                training=False, momentum=0.1, eps=1e-5):
-    size = list(input.size())
-    if reduce(mul, size[2:], size[0]) == 1:
-        raise ValueError('Expected more than 1 value per channel, got input size {}'.format(size))
+    if training:
+        size = list(input.size())
+        if reduce(mul, size[2:], size[0]) == 1:
+            raise ValueError('Expected more than 1 value per channel when training, got input size {}'.format(size))
     f = torch._C._functions.BatchNorm(running_mean, running_var, training, momentum, eps, torch.backends.cudnn.enabled)
     return f(input, weight, bias)
 
