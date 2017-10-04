c30290ce50943c7f8d4cd5736e3c1aed8f067194
Soumith Chintala <soumith@gmail.com>
2017-10-04 16:03:00 -0400

    fix lint

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -704,7 +704,7 @@ def batch_norm(input, running_mean, running_var, weight=None, bias=None,
                training=False, momentum=0.1, eps=1e-5):
     if training:
         size = list(input.size())
-        if  reduce(mul, size[2:], size[0]) == 1:
+        if reduce(mul, size[2:], size[0]) == 1:
             raise ValueError('Expected more than 1 value per channel when training, got input size {}'.format(size))
     f = torch._C._functions.BatchNorm(running_mean, running_var, training, momentum, eps, torch.backends.cudnn.enabled)
     return f(input, weight, bias)
