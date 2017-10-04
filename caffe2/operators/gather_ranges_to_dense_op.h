9673cbdfa6ffa41b23696ea2e869b76019d1bf8d
Artem Volkhin <volkhin@fb.com>
2017-10-04 15:13:29 -0700

    add GatherRangesToDense operator
    Summary: adding an operator with behavior similar to fused GatherRanges and Split.
    
    Reviewed By: kennyhorror
    
    Differential Revision: D5961761
    
    fbshipit-source-id: 616d4668b8901256418004def90d91a0b2041620

---
+++ caffe2/operators/gather_ranges_to_dense_op.h
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CAFFE2_OPERATORS_GATHER_RANGES_TO_DENSE_OPS_H_
+#define CAFFE2_OPERATORS_GATHER_RANGES_TO_DENSE_OPS_H_
+
+#include <math.h>
+
+#include "caffe2/core/common_omp.h"
+#include "caffe2/core/context.h"
+#include "caffe2/core/logging.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/core/types.h"
+#include "caffe2/utils/math.h"
+
+#include <map>
+#include <utility>
+
+namespace caffe2 {
+template <class Context>
+class GatherRangesToDenseOp final : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  GatherRangesToDenseOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws),
+        lengths_(OperatorBase::GetRepeatedArgument<int>("lengths")) {
+    CAFFE_ENFORCE_GT(lengths_.size(), 0, "There has to be at least one length");
+    for (auto length : lengths_) {
+      CAFFE_ENFORCE_GT(length, 0, "Each length should be positive");
+    }
+  }
+
+  bool RunOnDevice() override {
+    return DispatchHelper<TensorTypes<int32_t, int64_t>>::call(
+        this, OperatorBase::Input<TensorCPU>(RANGES));
+  }
+
+  template <typename Index>
+  bool DoRunWithType() {
+    auto& data = Input(DATA);
+    auto& ranges = Input(RANGES);
+    CAFFE_ENFORCE_EQ(data.ndim(), 1, "Data has to be 1-D");
+    CAFFE_ENFORCE_EQ(ranges.ndim(), 3, "Data has to be 3-D");
+    CAFFE_ENFORCE_EQ(
+        ranges.dim(1),
+        lengths_.size(),
+        "Nummber of ranges should match number of lengths");
+    CAFFE_ENFORCE_EQ(
+        ranges.dim(1),
+        OutputSize(),
+        "Nummber of ranges should match number of outputs");
+    CAFFE_ENFORCE_EQ(
+        ranges.dim(2), 2, "Ranges last dimension should be of size 2");
+
+    auto* rawData = static_cast<const char*>(data.raw_data());
+    auto* rangesData = ranges.template data<Index>();
+    int rangesDataOffset = 0;
+    auto itemsize = data.meta().itemsize();
+
+    auto batchSize = ranges.dim(0);
+    vector<TIndex> outputDims{batchSize, 0};
+    vector<char*> outputRawData;
+    for (int i = 0; i < OutputSize(); ++i) {
+      auto* output = Output(i);
+      outputDims[1] = lengths_[i];
+      output->Resize(outputDims);
+      char* ptr = static_cast<char*>(output->raw_mutable_data(data.meta()));
+      memset(ptr, 0, output->nbytes());
+      outputRawData.push_back(ptr);
+    }
+
+    for (int i = 0; i < batchSize; ++i) {
+      for (int j = 0; j < OutputSize(); ++j) {
+        auto rangeStart = rangesData[rangesDataOffset++];
+        auto rangeLength = rangesData[rangesDataOffset++];
+        if (rangeLength == 0) {
+          // empty range, will be filled with zeros
+          continue;
+        }
+        CAFFE_ENFORCE_EQ(
+            rangeLength,
+            lengths_[j],
+            "Range lengths missmatch for output #",
+            j);
+        context_.template CopyItems<Context, Context>(
+            data.meta(),
+            rangeLength,
+            rawData + rangeStart * itemsize,
+            outputRawData[j] + i * itemsize * lengths_[j]);
+      }
+    }
+    CAFFE_ENFORCE_EQ(rangesDataOffset, ranges.size());
+
+    return true;
+  }
+
+  INPUT_TAGS(DATA, RANGES);
+
+ private:
+  vector<int> lengths_;
+};
+
+} // namespace caffe2
+
+#endif // CAFFE2_OPERATORS_GATHER_RANGES_TO_DENSE_OPS_H_
--- caffe2/python/operator_test/gather_ranges_op_test.py
+++ caffe2/python/operator_test/gather_ranges_op_test.py
@@ -49,6 +49,31 @@ def batched_boarders_and_data(
     ))
 
 
+@st.composite
+def _tensor_splits(draw):
+    lengths = draw(st.lists(st.integers(1, 5), min_size=1, max_size=10))
+    batch_size = draw(st.integers(1, 5))
+    element_pairs = [
+        (batch, r) for batch in range(batch_size) for r in range(len(lengths))
+    ]
+    perm = draw(st.permutations(element_pairs))
+    perm = perm[:-1]  # skip one range
+    ranges = [[(0, 0)] * len(lengths) for _ in range(batch_size)]
+    offset = 0
+    for pair in perm:
+        ranges[pair[0]][pair[1]] = (offset, lengths[pair[1]])
+        offset += lengths[pair[1]]
+
+    data = draw(st.lists(
+        st.floats(min_value=-1.0, max_value=1.0),
+        min_size=offset,
+        max_size=offset
+    ))
+    return (
+        np.array(data).astype(np.float32), np.array(ranges), np.array(lengths)
+    )
+
+
 def gather_ranges(data, ranges):
     lengths = []
     output = []
@@ -60,6 +85,26 @@ def gather_ranges(data, ranges):
             length += range[1]
         lengths.append(length)
     return output, lengths
+
+
+def gather_ranges_to_dense(data, ranges, lengths):
+    outputs = []
+    assert len(ranges)
+    batch_size = len(ranges)
+    assert len(ranges[0])
+    num_ranges = len(ranges[0])
+    assert ranges.shape[2] == 2
+    for i in range(num_ranges):
+        out = []
+        for j in range(batch_size):
+            start, length = ranges[j][i]
+            if not length:
+                out.append([0] * lengths[i])
+            else:
+                assert length == lengths[i]
+                out.append(data[start:start + length])
+        outputs.append(np.array(out))
+    return outputs
 
 
 class TestGatherRanges(hu.HypothesisTestCase):
@@ -81,6 +126,22 @@ class TestGatherRanges(hu.HypothesisTestCase):
                                    ["output", "lengths"]),
             inputs=[data, ranges],
             reference=gather_ranges,
+        )
+
+    @given(tensor_splits=_tensor_splits(), **hu.gcs)
+    def test_gather_ranges_split(self, tensor_splits, gc, dc):
+        data, ranges, lengths = tensor_splits
+
+        self.assertReferenceChecks(
+            device_option=gc,
+            op=core.CreateOperator(
+                "GatherRangesToDense",
+                ['data', 'ranges'],
+                ['X_{}'.format(i) for i in range(len(lengths))],
+                lengths=lengths
+            ),
+            inputs=[data, ranges, lengths],
+            reference=gather_ranges_to_dense
         )
 
 if __name__ == "__main__":
