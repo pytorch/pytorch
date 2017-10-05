9425a2bf19abc6ec67b1c5db961a8b1512f1ec69
Richard Zou <zou3519@users.noreply.github.com>
2017-10-05 17:57:35 -0400

    Fix cudnn grid_sample backward for implicit gradOutput (#2993)

--- torch/nn/_functions/vision.py
+++ torch/nn/_functions/vision.py
@@ -34,6 +34,10 @@ class GridSampler(Function):
             grid = grid.contiguous()
             if 0 in input.stride():
                 input = input.contiguous()
+            # Sometimes grad_output is a scalar (like 1) expanded as a tensor.
+            # cudnn requires a tensor that has non-zero strides.
+            if 0 in grad_output.stride():
+                grad_output = grad_output.contiguous()
             torch._C._cudnn_grid_sampler_backward(input, grad_input,
                                                   grid, grad_grid,
                                                   grad_output)
