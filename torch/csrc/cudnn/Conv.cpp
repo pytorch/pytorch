4af66c43045a317b477918c503d105f565b4a66b
Richard Zou <zou3519@users.noreply.github.com>
2017-10-05 23:04:59 -0400

    Cleanup: remove useCurrentStream function (#2990)

--- torch/csrc/cudnn/Conv.cpp
+++ torch/csrc/cudnn/Conv.cpp
@@ -3,7 +3,6 @@
 #include "THC/THC.h"
 #include "Exceptions.h"
 #include "Types.h"
-#include "Handles.h"
 
 #include <cudnn.h>
 #include <functional>
@@ -494,7 +493,7 @@ void cudnn_convolution_forward(
     THVoidTensor* input, THVoidTensor* weight, THVoidTensor* output,
     Convolution* info, bool benchmark)
 {
-  useCurrentStream(handle, state);
+  CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
   assertSameGPU(dataType, input, weight, output);
   int groups = info->groups;
 
@@ -530,7 +529,7 @@ void cudnn_convolution_add_bias(
     THVoidTensor* bias, THVoidTensor* output,
     Convolution* info)
 {
-  useCurrentStream(handle, state);
+  CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
   assertSameGPU(dataType, bias, output);
   CHECK_ARG(output->nDimension <= 5);
   TensorDescriptor& bdesc = info->bdesc;
@@ -552,7 +551,7 @@ void cudnn_convolution_backward_data(
     THVoidTensor* gradOutput, THVoidTensor* gradInput, THVoidTensor* weight,
     Convolution* info, bool benchmark)
 {
-  useCurrentStream(handle, state);
+  CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
   assertSameGPU(dataType, gradOutput, gradInput, weight);
   int groups = info->params.groups;
 
@@ -587,7 +586,7 @@ void cudnn_convolution_backward_filter(
     THVoidTensor* gradOutput, THVoidTensor* input, THVoidTensor* gradWeight,
     Convolution* info, bool benchmark)
 {
-  useCurrentStream(handle, state);
+  CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
   assertSameGPU(dataType, gradOutput, input, gradWeight);
   int groups = info->params.groups;
 
@@ -628,7 +627,7 @@ void cudnn_convolution_backward_bias(
     THCState* state, cudnnHandle_t handle, cudnnDataType_t dataType,
     THVoidTensor* gradOutput, THVoidTensor* gradBias, Convolution* info)
 {
-  useCurrentStream(handle, state);
+  CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
   assertSameGPU(dataType, gradOutput, gradBias);
   Constant one(dataType, 1);
   Constant zero(dataType, 0);
@@ -645,7 +644,7 @@ Convolution* cudnn_convolution_full_forward(
     THVoidTensor* input, THVoidTensor* weight, THVoidTensor* bias, THVoidTensor* output,
     std::vector<int> pad, std::vector<int> stride, std::vector<int> dilation, int groups, bool benchmark)
 {
-    useCurrentStream(handle, state);
+    CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
     std::unique_ptr<Convolution> info(new Convolution(
         dataType, input, weight, bias, output, pad, stride, dilation, groups, false));
     cudnn_convolution_forward(
@@ -662,7 +661,7 @@ Convolution* cudnn_convolution_transpose_full_forward(
     THVoidTensor* input, THVoidTensor* weight, THVoidTensor* bias, THVoidTensor* output,
     std::vector<int> pad, std::vector<int> stride, std::vector<int> dilation, int groups, bool benchmark)
 {
-    useCurrentStream(handle, state);
+    CHECK(cudnnSetStream(handle, THCState_getCurrentStream(state)));
     std::unique_ptr<Convolution> info(new Convolution(
         dataType, output, weight, bias, input, pad, stride, dilation, groups, true));
     cudnn_convolution_backward_data(
--- torch/csrc/cudnn/Handles.h
+++ torch/csrc/cudnn/Handles.h
@@ -2,13 +2,10 @@
 #define THP_CUDNN_HANDLE_INC
 
 #include <cudnn.h>
-#include "THC/THC.h"
-#include "Types.h"
 
 namespace torch { namespace cudnn {
 
 cudnnHandle_t getCudnnHandle();
-void useCurrentStream(cudnnHandle_t handle, THCState *state);
 
 }} // namespace
 
