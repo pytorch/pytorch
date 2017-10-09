b30bb7db125308866d32cc8dd9541cc0e0df1242
Francisco Massa <fvsmassa@gmail.com>
2017-10-09 18:20:48 +0100

    Add compiled CUDA version in torch.version.cuda

--- tools/setup_helpers/cuda.py
+++ tools/setup_helpers/cuda.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import platform
 import ctypes.util
 from subprocess import Popen, PIPE
@@ -16,9 +17,38 @@ def find_nvcc():
         return None
 
 
+def find_cuda_version(cuda_home=None):
+    if cuda_home is  None:
+        return None
+    # first try loading the version from the version.txt file
+    version_file = os.path.join(cuda_home, 'version.txt')
+    if os.path.exists(version_file):
+        with open(version_file, 'r') as f:
+            version = f.read().strip().split(' ')
+        # take only the version number
+        version = version[-1]
+    else:
+        cuda_lib_dirs = ['lib64', 'lib']
+        for lib_dir in cuda_lib_dirs:
+            cuda_lib_path = os.path.join(CUDA_HOME, lib_dir)
+            if os.path.exists(cuda_lib_path):
+                break
+        # get a list of candidates for the version number
+        # which are files containing libcudart
+        candidates = list(glob.glob(os.path.join(cuda_lib_path, 'libcudart*')))
+        candidates = [os.path.basename(c) for c in candidates]
+        # suppose version is MAJOR.MINOR.PATCH, all numbers
+        d = re.compile('[0-9]+\.[0-9]+\.[0-9]+')
+        candidates = [c.group() for c in map(d.search, candidates) if c]
+        assert len(candidates) == 1
+        version = candidates[0]
+    return version
+
+
 if check_env_flag('NO_CUDA'):
     WITH_CUDA = False
     CUDA_HOME = None
+    CUDA_VERSION = None
 else:
     CUDA_HOME = os.getenv('CUDA_HOME', '/usr/local/cuda')
     if not os.path.exists(CUDA_HOME):
@@ -36,4 +66,5 @@ else:
             CUDA_HOME = os.path.dirname(cuda_path)
         else:
             CUDA_HOME = None
+    CUDA_VERSION = find_cuda_version(CUDA_HOME)
     WITH_CUDA = CUDA_HOME is not None
