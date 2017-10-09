139aaf65d662d6f103fff24a9169162c6528b73a
Francisco Massa <fvsmassa@gmail.com>
2017-10-09 18:29:21 +0100

    Bugfix plus remove other option that depends on the version.txt file
    Restrict search for cudart instead of libcudart

--- tools/setup_helpers/cuda.py
+++ tools/setup_helpers/cuda.py
@@ -20,28 +20,21 @@ def find_nvcc():
 def find_cuda_version(cuda_home=None):
     if cuda_home is  None:
         return None
-    # first try loading the version from the version.txt file
-    version_file = os.path.join(cuda_home, 'version.txt')
-    if os.path.exists(version_file):
-        with open(version_file, 'r') as f:
-            version = f.read().strip().split(' ')
-        # take only the version number
-        version = version[-1]
-    else:
-        cuda_lib_dirs = ['lib64', 'lib']
-        for lib_dir in cuda_lib_dirs:
-            cuda_lib_path = os.path.join(CUDA_HOME, lib_dir)
-            if os.path.exists(cuda_lib_path):
-                break
-        # get a list of candidates for the version number
-        # which are files containing libcudart
-        candidates = list(glob.glob(os.path.join(cuda_lib_path, 'libcudart*')))
-        candidates = [os.path.basename(c) for c in candidates]
-        # suppose version is MAJOR.MINOR.PATCH, all numbers
-        d = re.compile('[0-9]+\.[0-9]+\.[0-9]+')
-        candidates = [c.group() for c in map(d.search, candidates) if c]
-        assert len(candidates) == 1
-        version = candidates[0]
+    # get CUDA lib folder
+    cuda_lib_dirs = ['lib64', 'lib']
+    for lib_dir in cuda_lib_dirs:
+        cuda_lib_path = os.path.join(cuda_home, lib_dir)
+        if os.path.exists(cuda_lib_path):
+            break
+    # get a list of candidates for the version number
+    # which are files containing cudart
+    candidates = list(glob.glob(os.path.join(cuda_lib_path, '*cudart*')))
+    candidates = [os.path.basename(c) for c in candidates]
+    # suppose version is MAJOR.MINOR.PATCH, all numbers
+    version_regex = re.compile('[0-9]+\.[0-9]+\.[0-9]+')
+    candidates = [c.group() for c in map(version_regex.search, candidates) if c]
+    assert len(candidates) == 1
+    version = candidates[0]
     return version
 
 
