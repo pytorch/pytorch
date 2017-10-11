d3b19b8ae10be6957ab65eae6d1fe493e6067c0e
frombeijingwithlove <frombeijingwithlove@users.noreply.github.com>
2017-10-11 12:54:02 +0800

    Added Class-Aware Sampler
    Implemented Class-Aware Sampling: https://arxiv.org/abs/1512.05830. Li Shen, Zhouchen Lin, Qingming Huang, Relay Backpropagation for Effective Learning of Deep Convolutional Neural Networks, ECCV 2016

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -1,4 +1,35 @@
+import random
 import torch
+
+
+class RandomCycleIter:
+    """Randomly iterate element in each cycle
+
+    Example:
+        >>> rand_cyc_iter = RandomCycleIter([1, 2, 3])
+        >>> [next(rand_cyc_iter) for _ in range(10)]
+        [2, 1, 3, 2, 3, 1, 1, 2, 3, 2]
+    """
+    def __init__(self, data):
+        self.data_list = list(data)
+        self.length = len(self.data_list)
+        self.i = self.length - 1
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.i += 1
+        if self.i == self.length:
+            self.i = 0
+            random.shuffle(self.data_list)
+        return self.data_list[self.i]
+
+def class_aware_sample_generator(cls_iter, data_iter_list, n):
+    i = 0
+    while i < n:
+        yield next(data_iter_list[next(cls_iter)])
+        i += 1
 
 
 class Sampler(object):
@@ -72,7 +103,6 @@ class SubsetRandomSampler(Sampler):
 
 class WeightedRandomSampler(Sampler):
     """Samples elements from [0,..,len(weights)-1] with given probabilities (weights).
-
     Arguments:
         weights (list)   : a list of weights, not necessary summing up to one
         num_samples (int): number of samples to draw
@@ -85,6 +115,39 @@ class WeightedRandomSampler(Sampler):
 
     def __iter__(self):
         return iter(torch.multinomial(self.weights, self.num_samples, self.replacement))
+
+    def __len__(self):
+        return self.num_samples
+
+
+class ClassAwareSampler(Sampler):
+    """Samples elements randomly, without replacement.
+
+    Arguments:
+        data_source (Dataset): dataset to sample from
+    
+    Implemented Class-Aware Sampling: https://arxiv.org/abs/1512.05830
+    Li Shen, Zhouchen Lin, Qingming Huang, Relay Backpropagation for Effective 
+    Learning of Deep Convolutional Neural Networks, ECCV 2016
+    By default num_samples equals to number of samples in the largest class 
+    multiplied by num of classes such that all samples can be sampled.
+    """
+
+    def __init__(self, data_source, num_samples=0):
+        self.data_source = data_source
+        n_cls = len(data_source.classes)
+        self.class_iter = RandomCycleIter(range(n_cls))
+        cls_data_list = [list() for _ in range(n_cls)]
+        for i, (_, label) in enumerate(data_source.imgs):
+            cls_data_list[label].append(i)
+        self.data_iter_list = [RandomCycleIter(x) for x in cls_data_list]
+        if num_samples:
+            self.num_samples = num_samples
+        else:
+            self.num_samples = max([len(x) for x in cls_data_list]) * len(cls_data_list)
+
+    def __iter__(self):
+        return class_aware_sample_generator(self.class_iter, self.data_iter_list, self.num_samples)
 
     def __len__(self):
         return self.num_samples
