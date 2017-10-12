9575364d302eb5e6187724b122dc902041f38330
Pieter Noordhuis <pietern@fb.com>
2017-10-12 11:48:11 -0700

    Update protobuf detection
    Summary:
    The scripts/build_local.sh script would always build protoc from the
    third_party protobuf tree and override the PROTOBUF_PROTOC_EXECUTABLE
    CMake variable. This variable is used by the protobuf CMake files, so
    it doesn't let us detect whether the protoc was specified by the user
    or by the protobuf CMake files (e.g. an existing installation). This
    in turn led to a problem where system installed headers would be
    picked up while using protoc built from third_party. This only works
    if the system installed version matches the version included in the
    Caffe2 tree. Therefore, this commit changes the variable to specify a
    custom protoc executable to CAFFE2_CUSTOM_PROTOC_EXECUTABLE, and
    forces the use of the bundled libprotobuf when it is specified.
    
    The result is that we now EITHER specify a custom protoc (as required
    for cross-compilation where protoc must be compiled for the host and
    libprotobuf for the target architecture) and use libprotobuf from the
    Caffe2 tree, OR use system protobuf.
    
    If system protobuf cannot be found, we fall back to building protoc
    and libprotobuf in tree and packaging it as part of the Caffe2 build
    artifacts.
    Closes https://github.com/caffe2/caffe2/pull/1328
    
    Differential Revision: D6032836
    
    Pulled By: pietern
    
    fbshipit-source-id: b75f8dd88412f02c947dc81ca43f7b2788da51e5

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -15,25 +15,30 @@ function(custom_protobuf_find)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" PARENT_SCOPE)
   endif()
   add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/protobuf/cmake)
+  # To build shared Caffe2 libraries that link to a static protobuf,
+  # we need those static libraries to be compiled as PIC.
+  set_property(TARGET libprotobuf PROPERTY POSITION_INDEPENDENT_CODE ON)
   set(PROTOBUF_LIBRARIES libprotobuf PARENT_SCOPE)
   set(PROTOBUF_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/third_party/protobuf/src PARENT_SCOPE)
   set(Caffe2_DEPENDENCY_LIBS ${Caffe2_DEPENDENCY_LIBS} PARENT_SCOPE)
-  if(NOT EXISTS ${PROTOBUF_PROTOC_EXECUTABLE})
-    message(FATAL_ERROR
-            "To build protobufs locally (required for Android/iOS/Windows), "
-            "you will need to manually specify a PROTOBUF_PROTOC_EXECUTABLE. "
-            "See scripts/build_host_protoc.{sh,bat} for more details.")
+  # Figure out which protoc to use.
+  # If CAFFE2_CUSTOM_PROTOC_EXECUTABLE is set, we assume the user knows
+  # what they're doing and we blindly use the specified protoc. This
+  # is typically the case when cross-compiling where protoc must be
+  # compiled for the host architecture and libprotobuf must be
+  # compiled for the target architecture.
+  # If CAFFE2_CUSTOM_PROTOC_EXECUTABLE is NOT set, we use the protoc
+  # target that is built as part of including the protobuf project.
+  if(EXISTS "${CAFFE2_CUSTOM_PROTOC_EXECUTABLE}")
+    set(PROTOBUF_PROTOC_EXECUTABLE ${CAFFE2_CUSTOM_PROTOC_EXECUTABLE} PARENT_SCOPE)
   else()
-    message(STATUS "Using protobuf compiler ${PROTOBUF_PROTOC_EXECUTABLE}.")
+    set(PROTOBUF_PROTOC_EXECUTABLE $<TARGET_FILE:protoc> PARENT_SCOPE)
   endif()
   set(Protobuf_FOUND TRUE PARENT_SCOPE)
 endfunction()
 
 if (WIN32)
-  find_package( Protobuf NO_MODULE)
-  if ( NOT (Protobuf_FOUND OR PROTOBUF_FOUND) )
-    custom_protobuf_find()
-  endif()
+  find_package(Protobuf NO_MODULE)
 elseif (ANDROID OR IOS)
   custom_protobuf_find()
   if (IOS_PLATFORM STREQUAL "WATCHOS")
@@ -47,21 +52,31 @@ elseif (ANDROID OR IOS)
         EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
   endif()
 else()
-  find_package( Protobuf )
+  # Always use libprotobuf from tree if a custom PROTOBUF
+  if(EXISTS "${CAFFE2_CUSTOM_PROTOC_EXECUTABLE}")
+    custom_protobuf_find()
+  else()
+    find_package(Protobuf)
+    if(Protobuf_FOUND OR PROTOBUF_FOUND)
+      # Add PROTOBUF_LIBRARY for backwards compatibility.
+      # Newer versions use *PROTOBUF_LIBRARIES*.
+      list(APPEND Caffe2_DEPENDENCY_LIBS ${PROTOBUF_LIBRARY})
+    endif()
+  endif()
 endif()
 
 # If Protobuf is not found, do custom protobuf find.
-if ( NOT (Protobuf_FOUND OR PROTOBUF_FOUND) )
+if(NOT(Protobuf_FOUND OR PROTOBUF_FOUND))
   custom_protobuf_find()
 endif()
 
-caffe2_include_directories(${PROTOBUF_INCLUDE_DIR})
-# Adding PROTOBUF_LIBRARY for legacy support.
-list(APPEND Caffe2_DEPENDENCY_LIBS ${PROTOBUF_LIBRARIES} ${PROTOBUF_LIBRARY})
-
-if (NOT (Protobuf_FOUND OR PROTOBUF_FOUND) )
-  message(FATAL_ERROR "Could not find Protobuf or compile local version.")
+if(NOT(Protobuf_FOUND OR PROTOBUF_FOUND))
+  message(FATAL_ERROR "Could not find protobuf or compile local version")
 endif()
+
+message(STATUS "Using protobuf compiler ${PROTOBUF_PROTOC_EXECUTABLE}")
+caffe2_include_directories(${PROTOBUF_INCLUDE_DIR})
+list(APPEND Caffe2_DEPENDENCY_LIBS ${PROTOBUF_LIBRARIES})
 
 ################################################################################################
 # Modification of standard 'protobuf_generate_cpp()' with output dir parameter and python support
@@ -84,6 +99,11 @@ function(caffe2_protobuf_generate_cpp_py srcs_var hdrs_var python_var)
     list(APPEND ${hdrs_var} "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.h")
     list(APPEND ${python_var} "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}_pb2.py")
 
+    # Note: the following depends on PROTOBUF_PROTOC_EXECUTABLE. This
+    # is done to make sure protoc is built before attempting to
+    # generate sources if we're using protoc from the third_party
+    # directory and are building it as part of the Caffe2 build. If
+    # points to an existing path, it is a no-op.
     add_custom_command(
       OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.cc"
              "${CMAKE_CURRENT_BINARY_DIR}/${fil_we}.pb.h"
@@ -92,7 +112,7 @@ function(caffe2_protobuf_generate_cpp_py srcs_var hdrs_var python_var)
       COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_CURRENT_BINARY_DIR}"
       COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} -I${PROJECT_SOURCE_DIR} --cpp_out    "${PROJECT_BINARY_DIR}" ${abs_fil}
       COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} -I${PROJECT_SOURCE_DIR} --python_out "${PROJECT_BINARY_DIR}" ${abs_fil}
-      DEPENDS ${abs_fil}
+      DEPENDS ${PROTOBUF_PROTOC_EXECUTABLE} ${abs_fil}
       COMMENT "Running C++/Python protocol buffer compiler on ${fil}" VERBATIM )
   endforeach()
 
--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-##############################################################################
-# Example command to build Caffe2 locally without installing many dependencies
-##############################################################################
 #
-# This script builds protoc locally and then sets the appropriate dependencies
-# to remove the need for many external dependencies.
+##############################################################################
+# Example command to build Caffe2
+##############################################################################
 #
 
 CAFFE2_ROOT="$( cd "$(dirname "$0")"/.. ; pwd -P)"
 echo "Caffe2 codebase root is: $CAFFE2_ROOT"
 
-# We are going to build the target into build.
-BUILD_ROOT=$CAFFE2_ROOT/build
-mkdir -p $BUILD_ROOT
-echo "Build Caffe2 into: $BUILD_ROOT"
+# Build protobuf compiler from third_party if configured to do so
+if [ -n "${USE_HOST_PROTOC:-}" ]; then
+    echo "Building protoc before compiling Caffe2..."
+    $CAFFE2_ROOT/scripts/build_host_protoc.sh || exit 1
+    CMAKE_ARGS="$CMAKE_ARGS \
+        -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=$CAFFE2_ROOT/build_host_protoc/bin/protoc"
+fi
 
-# Build protobuf from third_party so we have a host protoc binary.
-echo "Building protoc"
-$CAFFE2_ROOT/scripts/build_host_protoc.sh || exit 1
+# We are going to build the target into build.
+BUILD_ROOT="$CAFFE2_ROOT/build"
+mkdir -p "$BUILD_ROOT"
+echo "Building Caffe2 in: $BUILD_ROOT"
 
 # Now, actually build the target.
-echo "Building caffe2"
-cd $BUILD_ROOT
+cd "$BUILD_ROOT"
+set -x
+cmake .. ${CMAKE_ARGS} "$@" || exit 1
 
-cmake .. \
-    -DPROTOBUF_PROTOC_EXECUTABLE=$CAFFE2_ROOT/build_host_protoc/bin/protoc \
-    -DBUILD_SHARED_LIBS=OFF \
-    || exit 1
-    
 if [ "$(uname)" = 'Darwin' ]; then
     cmake --build . -- "-j$(sysctl -n hw.ncpu)"
 else
