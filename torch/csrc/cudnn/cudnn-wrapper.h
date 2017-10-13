6bdbade595f03d0fb5d0efd346513641330b31c5
Richard Zou <zou3519@gmail.com>
2017-10-13 10:00:10 -0700

    Fix messages

--- torch/csrc/cudnn/cudnn-wrapper.h
+++ torch/csrc/cudnn/cudnn-wrapper.h
@@ -6,8 +6,8 @@
 #define STRING(x) STRINGIFY(x)
 
 #if CUDNN_MAJOR < 6
-#pragma message ("CuDNN v" STRING(CUDNN_MAJOR) " found, but need at least CUDNN v6. You can get the latest version of CUDNN from https://developer.nvidia.com/cudnn")
-#error "Old version of CuDNN found but need at least CuDNN v6. You can get the latest version of CuDNN from https://developer.nvidia.com/cudnn"
+#pragma message ("CuDNN v" STRING(CUDNN_MAJOR) " found, but need at least CuDNN v6. You can get the latest version of CuDNN from https://developer.nvidia.com/cudnn")
+#error "CuDNN version not supported"
 #endif
 
 #undef STRINGIFY
