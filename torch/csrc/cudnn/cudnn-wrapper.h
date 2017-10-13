0250b990bf71c7f08e4dc6144da90d6a6e835efc
Richard Zou <zou3519@gmail.com>
2017-10-13 09:51:11 -0700

    Used #pragma once, more descriptive error message.

--- torch/csrc/cudnn/cudnn-wrapper.h
+++ torch/csrc/cudnn/cudnn-wrapper.h
@@ -1,9 +1,15 @@
-#ifndef THP_CUDNN_CUDNN_WRAPPER_INC
-#define THP_CUDNN_CUDNN_WRAPPER_INC
+#pragma once
 
 #include <cudnn.h>
+
+#define STRINGIFY(x) #x
+#define STRING(x) STRINGIFY(x)
+
 #if CUDNN_MAJOR < 6
-#error CuDNN v5 and lower not supported. Please update or build without it.
+#pragma message ("CuDNN v" STRING(CUDNN_MAJOR) " found, but need at least CUDNN v6. You can get the latest version of CUDNN from https://developer.nvidia.com/cudnn")
+#error "Old version of CuDNN found but need at least CuDNN v6. You can get the latest version of CuDNN from https://developer.nvidia.com/cudnn"
 #endif
 
-#endif
+#undef STRINGIFY
+#undef STRING
+
