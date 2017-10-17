848a68e41b01fed31ce7cc5ede03b839d257b1c6
Jerry Zhang <jerryzh@fb.com>
2017-10-16 18:18:04 -0700

    Some bug-fixs in mpscnn backend
    Summary: att
    
    Reviewed By: ajtulloch
    
    Differential Revision: D6037723
    
    fbshipit-source-id: d7405b27089210abfd48a33ecee47a87f67ae9a0

--- caffe2/mobile/contrib/ios/mpscnn/mpscnn.h
+++ caffe2/mobile/contrib/ios/mpscnn/mpscnn.h
@@ -21,7 +21,7 @@
 namespace caffe2 {
 static constexpr const char* kMPSCNNReadCountArg = "__mpscnn_read_count__";
 static constexpr const char* kMPSCNNOutputIsTempImageArg = "__mpscnn_output_is_temp_img__";
-
+static constexpr const int kMetalMaxTextureArrLength = 2048;
 // We currently only try to convert a fixed set of operators that handle a subset of a full
 // CNN. We also only run when MPSCNN is available, provides a speedup.
 // On failure, returns false. On success, returns true, and sets the MPSCNN net in the output
