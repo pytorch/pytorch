a7443ca01d6832db77a913e70ce75c3b856d8cac
Fei Sun <feisun@fb.com>
2017-10-18 12:21:04 -0700

    Disable logging by default
    Summary: By default, do not log anything to reduce the runtime overhead
    
    Reviewed By: Maratyszcza
    
    Differential Revision: D6082490
    
    fbshipit-source-id: 35fd09ea439925139d66b4623211e01af46e18f2

--- caffe2/share/contrib/observers/observer_config.cc
+++ caffe2/share/contrib/observers/observer_config.cc
@@ -2,9 +2,9 @@
 
 namespace caffe2 {
 
-int ObserverConfig::netSampleRate_ = 1;
+int ObserverConfig::netSampleRate_ = 0;
 int ObserverConfig::operatorNetSampleRatio_ = 0;
-int ObserverConfig::skipIters_ = 10;
+int ObserverConfig::skipIters_ = 0;
 unique_ptr<NetObserverReporter> ObserverConfig::reporter_ = nullptr;
 int ObserverConfig::marker_ = -1;
 }
