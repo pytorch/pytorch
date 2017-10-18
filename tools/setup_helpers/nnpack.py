d3b8105f584a179022247ebe1c732c6bec7d0fe5
Trevor Killeen <killeentm@gmail.com>
2017-10-18 10:11:54 -0700

    unify nnpack discovery between conda and user

--- tools/setup_helpers/nnpack.py
+++ tools/setup_helpers/nnpack.py
@@ -5,24 +5,25 @@ from .env import check_env_flag
 
 WITH_NNPACK = False
 
-NNPACK_IS_USER_DEFINED = False
-NNPACK_LIB_DIR = []
 NNPACK_INCLUDE_DIRS = []
-
-NNPACK_IS_CONDA_DEFINED = False
 NNPACK_LIB_PATHS = []
-NNPACK_HEADER_PATHS = []
 
 if not check_env_flag('NO_NNPACK') and 'Windows' not in platform.system():
     # assume we have a flag set that determines the path
     nnpack_dir = os.getenv('NNPACK_DIR')
     if nnpack_dir is not None:
-        NNPACK_LIB_DIR = os.path.join(nnpack_dir, 'lib')
+        lib = os.path.join(nnpack_dir, 'lib')
+        libs = [
+            os.path.join(lib, 'libnnpack.a'),
+            os.path.join(lib, 'libpthreadpool.a')
+        ]
+
         NNPACK_INCLUDE_DIRS = [os.path.join(nnpack_dir, 'include'),
                                os.path.join(nnpack_dir, 'deps', 'pthreadpool', 'include')]
 
-        if os.path.exists(NNPACK_LIB_DIR) and all(os.path.exists(d) for d in NNPACK_INCLUDE_DIRS):
-            WITH_NNPACK = NNPACK_IS_USER_DEFINED = True
+        if all(os.path.exists(d) for d in NNPACK_INCLUDE_DIRS + libs):
+            WITH_NNPACK = True
+            NNPACK_LIB_PATHS = libs
         else:
             NNPACK_LIB_DIR = NNPACK_INCLUDE_DIRS = None
     else:
@@ -42,6 +43,6 @@ if not check_env_flag('NO_NNPACK') and 'Windows' not in platform.system():
             ]
 
             if all([os.path.exists(f) for f in libs + headers]):
-                WITH_NNPACK = NNPACK_IS_CONDA_DEFINED = True
+                WITH_NNPACK = True
                 NNPACK_LIB_PATHS = libs
-                NNPACK_HEADER_PATHS = headers
+                NNPACK_INCLUDE_DIRS = [include]
