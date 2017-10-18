23a3f78988aeeaf3ca193ec0b03fd755ddbcdd49
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2017-10-18 12:30:05 +0200

    Reverse the order of checks in torch.gather (#3130)
    * Reverse the order of checks in torch.gather
    
    * Remove unnecessary comment
    
    * Add missing check for indexing dimension

--- torch/lib/TH/generic/THTensorMath.c
+++ torch/lib/TH/generic/THTensorMath.c
@@ -424,11 +424,12 @@ void THTensor_(gather)(THTensor *tensor, THTensor *src, int dim, THLongTensor *i
 {
   int64_t elems_per_row, i, idx;
 
-  THArgCheck(THTensor_(nDimension)(src) == THTensor_(nDimension)(tensor), 2,
-             "Input tensor must have same dimensions as output tensor");
-  THArgCheck(dim < THTensor_(nDimension)(tensor), 3, "Index dimension is out of bounds");
   THArgCheck(THLongTensor_nDimension(index) == THTensor_(nDimension)(src), 4,
              "Index tensor must have same dimensions as input tensor");
+  THArgCheck(dim >= 0 && dim < THTensor_(nDimension)(tensor), 3,
+             "Index dimension is out of bounds");
+  THArgCheck(THTensor_(nDimension)(src) == THTensor_(nDimension)(tensor), 2,
+             "Input tensor must have same dimensions as output tensor");
 
   elems_per_row = THLongTensor_size(index, dim);
 
