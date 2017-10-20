7a67407db65d0612952dcfe040332f96b779ce2b
Dong Li <dongli@fb.com>
2017-10-20 15:04:40 -0700

    Add GPU support to operator RowWiseSparseAdagrad
    Summary: Previously, CPU version of operator RowWiseSparseAdagrad has been implemented, Here the GPU version of of the operator has been implemented and tested
    
    Reviewed By: azzolini
    
    Differential Revision: D6082828
    
    fbshipit-source-id: 74befd495666c357d5ab425a698c5880cd8f927c

--- caffe2/sgd/adagrad_op_gpu.cu
+++ caffe2/sgd/adagrad_op_gpu.cu
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
+#include <cub/block/block_reduce.cuh>
 #include "adagrad_op.h"
 #include "caffe2/core/common_gpu.h"
 #include "caffe2/core/context_gpu.h"
-
 namespace caffe2 {
 
 __global__ void AdagradUpdate(
@@ -80,6 +80,52 @@ __global__ void SparseAdagradKernel(
   }
 }
 
+/**
+ * Calculate RowwiseSparseAdagrad
+ * M: gradients.dims[0]
+ * N: gradients.size_from_dim(1)
+ * grad: pointer to the gradients
+ * param: pointer to weights
+ * param_mom: pointer to the momentum
+ * indices: keys
+ */
+template <typename SIndex>
+__global__ void RowWiseSparseAdagradKernel(
+    const int M,
+    const int N,
+    const float epsilon,
+    float* param,
+    float* param_mom,
+    const SIndex* indices,
+    const float* grad,
+    const float* lr) {
+  typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
+  __shared__ BlockReduce::TempStorage temp_storage;
+  // in case gridDim is smaller than M
+  for (int i = blockIdx.x; i < M; i += gridDim.x) {
+    const SIndex index = indices[i];
+    float sum_squares = 0.0;
+    __shared__ float row_sum_squares_avg;
+
+    // in case N is bigger than block size which is 512 by default
+    for (int j = threadIdx.x; j < N; j += blockDim.x) {
+      const float x_ij = grad[i * N + j];
+      sum_squares += x_ij * x_ij;
+    }
+    float reduce_result = BlockReduce(temp_storage).Sum(sum_squares);
+    if (threadIdx.x == 0) {
+      row_sum_squares_avg = reduce_result / (float)N;
+      param_mom[index] += row_sum_squares_avg;
+    }
+    __syncthreads();
+    // update param
+    float step = lr[0] / (std::sqrt(param_mom[index]) + epsilon);
+    for (int j = threadIdx.x; j < N; j += blockDim.x) {
+      param[index * N + j] = param[index * N + j] + grad[i * N + j] * step;
+    }
+  }
+}
+
 template<>
 template<typename SIndex>
 bool SparseAdagradOp<float, CUDAContext>::DoRunWithType()
@@ -104,6 +150,38 @@ bool SparseAdagradOp<float, CUDAContext>::DoRunWithType()
   return true;
 }
 
+template <>
+template <typename SIndex>
+bool RowWiseSparseAdagradOp<float, CUDAContext>::DoRunWithType() {
+  auto N = Input(GRAD).size();
+  if (N == 0) {
+    // empty grad, nothing to do here, not even launching the kernel
+    return true;
+  }
+  // size of the 1st dimension of the input gradient
+  auto GRAD_M = Input(GRAD).dim32(0);
+  auto GRAD_N = N / GRAD_M;
+
+  // each thread block will handle multiple rows of the input and output
+  RowWiseSparseAdagradKernel<<<
+      min(GRAD_M, CAFFE_MAXIMUM_NUM_BLOCKS),
+      CAFFE_CUDA_NUM_THREADS,
+      0,
+      context_.cuda_stream()>>>(
+      GRAD_M,
+      GRAD_N,
+      epsilon_,
+      Output(OUTPUT_PARAM)->template mutable_data<float>(),
+      Output(OUTPUT_MOMENT_1)->template mutable_data<float>(),
+      Input(INDICES).template data<SIndex>(),
+      Input(GRAD).template data<float>(),
+      Input(LR).template data<float>());
+  return true;
+}
+
 REGISTER_CUDA_OPERATOR(Adagrad, AdagradOp<float, CUDAContext>);
 REGISTER_CUDA_OPERATOR(SparseAdagrad, SparseAdagradOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    RowWiseSparseAdagrad,
+    RowWiseSparseAdagradOp<float, CUDAContext>);
 }
