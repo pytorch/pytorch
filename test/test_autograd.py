dc74aec7ca603a5217702101ec2c35cdeb454590
Sam Gross <sgross@fb.com>
2017-10-21 04:35:43 -0700

    Fix lint

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -545,8 +545,9 @@ class TestAutograd(TestCase):
         # NB: we currently raise an exception if any arguments to backwards
         # have requires_grad=False and don't have a grad_fn. We may want to
         # relax that check to a warning.
-        self.assertRaises(RuntimeError, lambda:
-            torch.autograd.backward([z, q], [torch.ones(5, 5), torch.ones(5, 5)]))
+        def call_backwards():
+            torch.autograd.backward([z, q], [torch.ones(5, 5), torch.ones(5, 5)])
+        self.assertRaises(RuntimeError, call_backwards)
 
     def test_dependent_backward(self):
         x = Variable(torch.randn(10), requires_grad=True)
