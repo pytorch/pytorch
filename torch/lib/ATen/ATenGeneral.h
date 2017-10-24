8a95c477c22b09c67910ed62360eae5f62a46699
peter <peter_jiachen@163.com>
2017-10-24 11:04:56 +0800

    Remove C exports and rename AT_API

--- torch/lib/ATen/ATenGeneral.h
+++ torch/lib/ATen/ATenGeneral.h
@@ -2,13 +2,10 @@
 
 #ifdef _WIN32
 # ifdef ATen_EXPORTS
-#  define ATen_API extern "C" __declspec(dllexport)
-#  define ATen_CLASS __declspec(dllexport)
+#  define AT_API __declspec(dllexport)
 # else
-#  define ATen_API extern "C" __declspec(dllimport)
-#  define ATen_CLASS __declspec(dllimport)
+#  define AT_API __declspec(dllimport)
 # endif
 #else
-# define ATen_API extern
-# define ATen_CLASS
+# define AT_API
 #endif
