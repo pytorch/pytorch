6111365c6cf2890721019a5a5dc52b54217378a3
Tzu-Wei Huang <huang.dexter@gmail.com>
2017-10-25 13:53:37 +0800

    check for exact equivalent

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -68,9 +68,9 @@ class TestConcatDataset(TestCase):
         d3 = TensorDataset(torch.rand(7, 3, 28, 28), torch.rand(7))
         result = d1 + d2 + d3
         self.assertEqual(21, len(result))
-        self.assertAlmostEqual(0, (d1[0][0] - result[0][0]).abs().sum())
-        self.assertAlmostEqual(0, (d2[0][0] - result[7][0]).abs().sum())
-        self.assertAlmostEqual(0, (d3[0][0] - result[14][0]).abs().sum())
+        self.assertEqual(0, (d1[0][0] - result[0][0]).abs().sum())
+        self.assertEqual(0, (d2[0][0] - result[7][0]).abs().sum())
+        self.assertEqual(0, (d3[0][0] - result[14][0]).abs().sum())
 
 
 class ErrorDataset(Dataset):
