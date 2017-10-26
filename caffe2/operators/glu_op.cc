5bb8ed67e3e5a8f73fe8198c324d49974220223a
Ahmed Taei <ataei@fb.com>
2017-10-25 19:31:59 -0700

    Compute GLU for an arbitrary axis
    Summary: As in title
    
    Differential Revision: D6151804
    
    fbshipit-source-id: bd0fa08be1676ebd1abd9720711c221c61c11ad1

--- caffe2/operators/glu_op.cc
+++ caffe2/operators/glu_op.cc
@@ -18,14 +18,25 @@ float sigmoid(const float x) {
 template <>
 void GluOp<float, CPUContext>::ComputeGlu(
     const int M,
+    const int split_dim,
     const int N,
     const float* Xdata,
     float* Ydata) {
-  const int xOffset = 2 * N;
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
   for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
-      Ydata[i * N + j] =
-          Xdata[i * xOffset + j] * sigmoid(Xdata[i * xOffset + j + N]);
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
     }
   }
 }
