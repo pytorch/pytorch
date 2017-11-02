3d68648eac58710ecb7f45d14e2a8ddab5d84ce0
Aapo Kyrola <akyrola@fb.com>
2017-11-02 11:03:13 -0700

    Updated brew SpatialBN to use initializers
    Summary: Updated brew SpatialBN to use initializers similar to other brew ops such as conv and fc instead of initilaizing all of its parameters itself within the brew call.
    
    Reviewed By: asaadaldien
    
    Differential Revision: D5840359
    
    fbshipit-source-id: 9f3d688d4957605eaf7ecd2488bc26bfb1da3f78

--- caffe2/python/helpers/normalization.py
+++ caffe2/python/helpers/normalization.py
@@ -20,9 +20,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from caffe2.python import core, scope
+from caffe2.python import scope
 from caffe2.python.modeling.parameter_info import ParameterTags
 from caffe2.proto import caffe2_pb2
+from caffe2.python.modeling import initializers
 
 
 def lrn(model, blob_in, blob_out, order="NCHW", use_cudnn=False, **kwargs):
@@ -87,7 +88,10 @@ def instance_norm(model, blob_in, blob_out, dim_in, order="NCHW", **kwargs):
 
 
 def spatial_bn(model, blob_in, blob_out, dim_in,
-               init_scale=1., init_bias=0., order="NCHW", **kwargs):
+               init_scale=1., init_bias=0.,
+               ScaleInitializer=None, BiasInitializer=None,
+               RunningMeanInitializer=None, RunningVarianceInitializer=None,
+               order="NCHW", **kwargs):
     blob_out = blob_out or model.net.NextName()
     # Input: input, scale, bias, est_mean, est_inv_var
     # Output: output, running_mean, running_inv_var, saved_mean,
@@ -97,28 +101,57 @@ def spatial_bn(model, blob_in, blob_out, dim_in,
     # est mean: zero
     # est var: ones
 
-    def init_blob(value, suffix):
-        return model.param_init_net.ConstantFill(
-            [], blob_out + "_" + suffix, shape=[dim_in], value=value)
-
     if model.init_params:
-        scale, bias = init_blob(init_scale, "s"), init_blob(init_bias, "b")
-        running_mean = init_blob(0.0, "rm")
-        running_inv_var = init_blob(1.0, "riv")
-    else:
-        scale = core.ScopedBlobReference(
-            blob_out + '_s', model.param_init_net)
-        bias = core.ScopedBlobReference(
-            blob_out + '_b', model.param_init_net)
-        running_mean = core.ScopedBlobReference(
-            blob_out + '_rm', model.param_init_net)
-        running_inv_var = core.ScopedBlobReference(
-            blob_out + '_riv', model.param_init_net)
+        scale_init = ("ConstantFill", {'value': init_scale})
+        bias_init = ("ConstantFill", {'value': init_bias})
+        rm_init = ("ConstantFill", {'value': 0.0})
+        riv_init = ("ConstantFill", {'value': 1.0})
 
-    model.AddParameter(running_mean, ParameterTags.COMPUTED_PARAM)
-    model.AddParameter(running_inv_var, ParameterTags.COMPUTED_PARAM)
-    model.AddParameter(scale, ParameterTags.WEIGHT)
-    model.AddParameter(bias, ParameterTags.BIAS)
+        ScaleInitializer = initializers.update_initializer(
+            ScaleInitializer, scale_init, ("ConstantFill", {})
+        )
+        BiasInitializer = initializers.update_initializer(
+            BiasInitializer, bias_init, ("ConstantFill", {})
+        )
+        RunningMeanInitializer = initializers.update_initializer(
+            RunningMeanInitializer, rm_init, ("ConstantFill", {})
+        )
+        RunningVarianceInitializer = initializers.update_initializer(
+            RunningVarianceInitializer, riv_init, ("ConstantFill", {})
+        )
+    else:
+        ScaleInitializer = initializers.ExternalInitializer()
+        BiasInitializer = initializers.ExternalInitializer()
+        RunningMeanInitializer = initializers.ExternalInitializer()
+        RunningVarianceInitializer = initializers.ExternalInitializer()
+
+    scale = model.create_param(
+        param_name=blob_out + '_s',
+        shape=[dim_in],
+        initializer=ScaleInitializer,
+        tags=ParameterTags.WEIGHT
+    )
+
+    bias = model.create_param(
+        param_name=blob_out + '_b',
+        shape=[dim_in],
+        initializer=BiasInitializer,
+        tags=ParameterTags.BIAS
+    )
+
+    running_mean = model.create_param(
+        param_name=blob_out + '_rm',
+        shape=[dim_in],
+        initializer=RunningMeanInitializer,
+        tags=ParameterTags.COMPUTED_PARAM
+    )
+
+    running_inv_var = model.create_param(
+        param_name=blob_out + '_riv',
+        shape=[dim_in],
+        initializer=RunningVarianceInitializer,
+        tags=ParameterTags.COMPUTED_PARAM
+    )
 
     blob_outs = [blob_out, running_mean, running_inv_var,
                  blob_out + "_sm", blob_out + "_siv"]
