a1eb5a728460a8297478c508a7ad3ad1c2a1b715
Andrew Tulloch <tulloch@fb.com>
2017-11-02 16:56:34 -0700

    Extend rewrite functionality to handle multiple outputs.
    Summary: Still assumes a complete subgraph, but slightly more generic.
    
    Reviewed By: Yangqing
    
    Differential Revision: D6103228
    
    fbshipit-source-id: bfa0d46067e05baa0478a4c37a67ccf8f81f34ec

--- caffe2/python/mkl/rewrite_graph_test.py
+++ caffe2/python/mkl/rewrite_graph_test.py
@@ -35,20 +35,26 @@ def deterministic_io(model):
     model = copy.deepcopy(model)
     for i, op in enumerate(model.InitProto().op):
         op.device_option.random_seed = i + 1
-    model.Proto().external_output.extend(
-        [model.Proto().op[-1].output[0]])
+    if not model.Proto().external_output:
+        model.Proto().external_output.extend([model.Proto().op[-1].output[0]])
     return model
 
 def simple_fc():
     model = ModelHelper(name="r")
     brew.fc(model, "data", "fc", 10, 10)
-    return model, (1, 10)
+    return model, [(1, 10)]
 
+def double_matmul():
+    model = ModelHelper(name="r")
+    fc0 = brew.fc(model, "data", "fc0", 10, 10)
+    fc1 = brew.fc(model, fc0, "fc1", 10, 10)
+    model.Proto().external_output[:] = [str(fc0), str(fc1)]
+    return model, [(1, 10)]
 
 def simple_relu():
     model = ModelHelper(name="r")
     brew.relu(model, "data", "fc")
-    return model, (1, 10)
+    return model, [(1, 10)]
 
 
 def simple_mlp():
@@ -70,7 +76,7 @@ def simple_mlp():
             10,
             10),
         "rl2")
-    return model, (1, 10)
+    return model, [(1, 10)]
 
 
 def simple_cnn():
@@ -82,7 +88,7 @@ def simple_cnn():
         model, 'conv1', 'conv1_spatbn', 16, epsilon=1e-3
     )
     brew.relu(model, 'conv1_spatbn', 'relu1')
-    return model, (1, 3, 32, 32)
+    return model, [(1, 3, 32, 32)]
 
 
 def simple_resnet():
@@ -90,7 +96,7 @@ def simple_resnet():
     resnet.create_resnet_32x32(
         model, "data", num_input_channels=1, num_groups=1, num_labels=5,
         is_test=True)
-    return model, (1, 1, 32, 32)
+    return model, [(1, 1, 32, 32)]
 
 
 def complex_resnet():
@@ -98,7 +104,7 @@ def complex_resnet():
     resnet.create_resnet50(
         model, "data", num_input_channels=1, num_labels=5, is_test=True,
         no_loss=True)
-    return model, (1, 1, 224, 224)
+    return model, [(1, 1, 224, 224)]
 
 
 @unittest.skipIf(not workspace.C.has_mkldnn,
@@ -107,7 +113,7 @@ class MKLRewriteTest(hu.HypothesisTestCase):
     @given(gen=st.sampled_from([simple_relu, simple_fc,
                                 simple_mlp, simple_cnn]))
     def test_mkl_simple_rewrite(self, gen):
-        cpu_model, shape = gen()
+        cpu_model, (shape,) = gen()
         cpu_model = deterministic_io(cpu_model)
         mkl_model = rewrite_graph.rewrite_model_helper_simple(cpu_model)
         X = np.random.randn(*shape).astype(np.float32)
@@ -122,7 +128,7 @@ class MKLRewriteTest(hu.HypothesisTestCase):
                                    atol=1e-4, rtol=1e-4)
 
     def test_mkl_resnet_rewrite(self):
-        cpu_model, shape = complex_resnet()
+        cpu_model, (shape,) = complex_resnet()
         cpu_model = deterministic_io(cpu_model)
         mkl_model = rewrite_graph.rewrite_model_helper_simple(cpu_model)
         np.random.seed(1701)
@@ -133,6 +139,27 @@ class MKLRewriteTest(hu.HypothesisTestCase):
             self.ws.create_blob(model.Proto().external_input[0]).feed(X)
             self.ws.run(model.Proto())
             return self.ws.blobs[model.Proto().external_output[0]].fetch()
+        np.testing.assert_allclose(run(cpu_model), run(mkl_model),
+                                   atol=1e-4, rtol=1e-4)
+
+    def test_mkl_multi_output_rewrite(self):
+        cpu_model, shapes = double_matmul()
+        cpu_model = deterministic_io(cpu_model)
+        mkl_model = rewrite_graph.rewrite_model_helper_simple(cpu_model)
+        np.random.seed(1701)
+        Xs = [np.random.randn(*shape).astype(np.float32) for shape in shapes]
+
+        def run(model):
+            self.ws.run(model.InitProto())
+            for (name, X) in zip(model.Proto().external_input, Xs):
+                self.ws.create_blob(name).feed(X)
+            print(model.Proto())
+            self.ws.run(model.Proto())
+            return [self.ws.blobs[name].fetch()
+                    for name in model.Proto().external_output]
+
+        run(mkl_model)
+
         np.testing.assert_allclose(run(cpu_model), run(mkl_model),
                                    atol=1e-4, rtol=1e-4)
 
