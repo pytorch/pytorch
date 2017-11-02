11ba4e581df12b05e715468bcdd6c12cb4b863b9
Jon Crall <erotemic@gmail.com>
2017-11-02 14:59:00 -0400

    addressed reviewer comments

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -179,11 +179,10 @@ class NLLLoss2d(NLLLoss):
         >>> loss = nn.NLLLoss2d()
         >>> # input is of size N x C x height x width
         >>> data = Variable(torch.randn(N, 16, 10, 10))
-        >>> m = nn.Conv2d(16, C, (3, 3)).float()
-        >>> input = m(data)
+        >>> m = nn.Conv2d(16, C, (3, 3))
         >>> # each element in target has to have 0 <= value < C
         >>> target = Variable(torch.LongTensor(N, 8, 8).random_(0, C))
-        >>> output = loss(input, target)
+        >>> output = loss(m(data), target)
         >>> output.backward()
     """
     pass
