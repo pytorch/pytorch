7ac341c86201ba7b50bcdab1ac37f17db6d5021d
Ilia Cherniavskii <iliacher@fb.com>
2017-11-03 15:52:59 -0700

    Fix EventBasics test
    Summary: Add missing event reset.
    
    Reviewed By: Yangqing
    
    Differential Revision: D6236352
    
    fbshipit-source-id: 4bee6dd22fa69532a9f376f03bb2f69c9c24c01e

--- caffe2/core/event_test.cc
+++ caffe2/core/event_test.cc
@@ -20,21 +20,21 @@
 
 namespace caffe2 {
 
-// For the CPU event test, we only test if these functions are properly
-// registered. Nothing special needs to be checked since CPU events are no-ops.
 TEST(EventCPUTest, EventBasics) {
   DeviceOption device_option;
   device_option.set_device_type(CPU);
   Event event(device_option);
   CPUContext context;
 
-  // Calling from Context
-  context.WaitEvent(event);
   context.Record(&event);
+  event.SetFinished();
 
-  // Calling from Event
+  context.WaitEvent(event);
   event.Finish();
+
+  event.Reset();
   event.Record(CPU, &context);
+  event.SetFinished();
   event.Wait(CPU, &context);
 }
 
