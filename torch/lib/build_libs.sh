209b7d9ff7095eee45e878e0d7c723144333da3d
Trevor Killeen <killeentm@gmail.com>
2017-11-06 10:02:22 -0700

    add CMake flag for disabling contrib builds

--- torch/lib/build_libs.sh
+++ torch/lib/build_libs.sh
@@ -143,6 +143,7 @@ function build_aten() {
   ${CMAKE_VERSION} ../../../../aten \
   -DCMAKE_BUILD_TYPE=Release \
   -DNO_CUDA=$((1-$WITH_CUDA)) \
+  -DATEN_NO_CONTRIB=1 \
   -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
   # purpusefully not passing C_FLAGS for the same reason as above
   make -j$(getconf _NPROCESSORS_ONLN) install
