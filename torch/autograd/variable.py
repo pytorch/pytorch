4b3a5fc38c63f211f2d25d0962eb08646d13b93d
Sam Gross <sgross@fb.com>
2017-11-07 14:58:13 -0800

    Raise exception when Variable.reinforce is called
    Fixes #3554

--- torch/autograd/variable.py
+++ torch/autograd/variable.py
@@ -206,20 +206,9 @@ class Variable(_C._VariableBase):
         return handle
 
     def reinforce(self, reward):
-        """Registers a reward obtained as a result of a stochastic process.
-
-        Differentiating stochastic nodes requires providing them with reward
-        value. If your graph contains any stochastic operations, you should
-        call this function on their outputs. Otherwise an error will be raised.
-
-        Parameters:
-            reward(Tensor): Tensor with per-element rewards. It has to match
-                the device location and shape of Variable's data.
-        """
-        if not isinstance(self.grad_fn, StochasticFunction):
-            raise RuntimeError("reinforce() can be only called on outputs "
-                               "of stochastic functions")
-        self.grad_fn._reinforce(reward)
+        raise RuntimeError(
+            "reinforce() was removed. Use torch.distributions instead.\n"
+            "See http://pytorch.org/docs/master/distributions.html")
 
     detach = _add_docstr(_C._VariableBase.detach, r"""
     Returns a new Variable, detached from the current graph.
