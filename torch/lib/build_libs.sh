a19d695b600b6ea707701eeef0b446320df08682
avmgithub <root@mnode10-11.pub.pic2.ibm.com>
2017-11-07 09:53:35 -0500

    add -fexceptions to aten build function for C and CXX builds

--- torch/lib/build_libs.sh
+++ torch/lib/build_libs.sh
@@ -143,9 +143,7 @@ function build_aten() {
   ${CMAKE_VERSION} ../../../../aten \
   -DCMAKE_BUILD_TYPE=Release \
   -DNO_CUDA=$((1-$WITH_CUDA)) \
-  -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-  -DCMAKE_CXX_FLAGS="-fexceptions" \
-  -DCMAKE_C_FLAGS="-fexceptions"
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" 
   # purpusefully not passing C_FLAGS for the same reason as above
   make -j$(getconf _NPROCESSORS_ONLN) install
   cd ../..
