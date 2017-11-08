f39168b3072fdc6d59ea8697f0324ba269212db6
Alisson Gusatti Azzolini <azzolini@fb.com>
2017-11-08 15:44:08 -0800

    Unit test that compares net snippets after parallelization
    Summary:
    - This is meant as a set of examples on how parallelize_net works.
    - Currently, only one example is provided. More to be added.
    
    Reviewed By: mraway, xianjiec
    
    Differential Revision: D6240160
    
    fbshipit-source-id: 6f6f2d77445825883e050498cb6e06fb74508bbf

--- caffe2/python/net_printer.py
+++ caffe2/python/net_printer.py
@@ -193,10 +193,12 @@ class Text(object):
 
 
 class Printer(Visitor, Text):
-    def __init__(self, factor_prefixes=False):
+    def __init__(self, factor_prefixes=False, c2_syntax=True):
         super(Visitor, self).__init__()
         super(Text, self).__init__()
         self.factor_prefixes = factor_prefixes
+        self.c2_syntax = c2_syntax
+        self.c2_net_name = None
 
 
 def _sanitize_str(s):
@@ -207,9 +209,9 @@ def _sanitize_str(s):
     else:
         sanitized = str(s)
     if len(sanitized) < 64:
-        return sanitized
+        return "'%s'" % sanitized
     else:
-        return sanitized[:64] + '...<+len=%d>' % (len(sanitized) - 64)
+        return "'%s'" % sanitized[:64] + '...<+len=%d>' % (len(sanitized) - 64)
 
 
 def _arg_val(arg):
@@ -240,8 +242,15 @@ def commonprefix(m):
     return s1
 
 
+def format_value(val):
+    if isinstance(val, list):
+        return '[%s]' % ', '.join("'%s'" % str(v) for v in val)
+    else:
+        return str(val)
+
+
 def factor_prefix(vals, do_it):
-    vals = [str(v) for v in vals]
+    vals = [format_value(v) for v in vals]
     prefix = commonprefix(vals) if len(vals) > 1 and do_it else ''
     joined = ', '.join(v[len(prefix):] for v in vals)
     return '%s[%s]' % (prefix, joined) if prefix else joined
@@ -266,13 +275,32 @@ def call(op, inputs=None, outputs=None, factor_prefixes=False):
         factor_prefix(outputs, factor_prefixes), call)
 
 
+def format_device_option(dev_opt):
+    if not dev_opt or not (
+            dev_opt.device_type or dev_opt.cuda_gpu_id or dev_opt.node_name):
+        return None
+    return call(
+        'DeviceOption',
+        [dev_opt.device_type, dev_opt.cuda_gpu_id, "'%s'" % dev_opt.node_name])
+
+
 @Printer.register(OperatorDef)
 def print_op(text, op):
-    text.add(call(
-        op.type,
-        list(op.input) + [(a.name, _arg_val(a)) for a in op.arg],
-        op.output,
-        factor_prefixes=text.factor_prefixes))
+    args = [(a.name, _arg_val(a)) for a in op.arg]
+    dev_opt_txt = format_device_option(op.device_option)
+    if dev_opt_txt:
+        args.append(('device_option', dev_opt_txt))
+
+    if text.c2_net_name:
+        text.add(call(
+            text.c2_net_name + '.' + op.type,
+            [list(op.input), list(op.output)] + args))
+    else:
+        text.add(call(
+            op.type,
+            list(op.input) + args,
+            op.output,
+            factor_prefixes=text.factor_prefixes))
     for arg in op.arg:
         if arg.HasField('n'):
             with text.context('arg: %s' % arg.name):
@@ -280,9 +308,15 @@ def print_op(text, op):
 
 @Printer.register(NetDef)
 def print_net_def(text, net_def):
-    text.add('# net: %s' % net_def.name)
+    if text.c2_syntax:
+        text.add(call('core.Net', ["'%s'" % net_def.name], [net_def.name]))
+        text.c2_net_name = net_def.name
+    else:
+        text.add('# net: %s' % net_def.name)
     for op in net_def.op:
         text(op)
+    if text.c2_syntax:
+        text.c2_net_name = None
 
 
 @Printer.register(Net)
@@ -369,12 +403,12 @@ def print_job(text, job):
     text(job.exit_group, 'Job.current().exit_group')
 
 
-def to_string(obj):
+def to_string(obj, **kwargs):
     """
     Given a Net, ExecutionStep, Task, TaskGroup or Job, produces a string
     with detailed description of the execution steps.
     """
-    printer = Printer()
+    printer = Printer(**kwargs)
     printer(obj)
     return str(printer)
 
