2bc7fc86988ec1c512835396a8c2a176ff593d8c
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-11-08 14:24:19 -0800

    Add Jenkins build scripts
    Summary:
    Let's see if we can make this work...
    Closes https://github.com/caffe2/caffe2/pull/1417
    
    Differential Revision: D6276601
    
    Pulled By: pietern
    
    fbshipit-source-id: 4d51a66b693a1c5cff1e0c03373cd42bb273c885

---
+++ .jenkins/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -ex
+
+LOCAL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ROOT_DIR=$(dirname "$LOCAL_DIR")
+
+# Remove potential leftovers from previous build
+rm -rf build install
+
+# Run cmake from ./build directory
+mkdir build
+cd build
+
+CMAKE_ARGS=("-DCMAKE_INSTALL_PREFIX=/usr/local/caffe2")
+
+case "${BUILD_ENVIRONMENT}" in
+  *-mkl)
+    CMAKE_ARGS+=("-DBLAS=MKL")
+    ;;
+  *-cuda*)
+    CMAKE_ARGS+=("-DUSE_CUDA=ON")
+    CMAKE_ARGS+=("-DCUDA_ARCH_NAME=Maxwell")
+    CMAKE_ARGS+=("-DUSE_NNPACK=OFF")
+
+    # Explicitly set path to NVCC such that the symlink to ccache is used
+    CMAKE_ARGS+=("-DCUDA_NVCC_EXECUTABLE=/usr/local/bin/nvcc")
+
+    # The CMake code that finds the CUDA distribution looks for nvcc in $PATH.
+    # This doesn't work here, as that would yield /usr/local/bin, which doesn't
+    # contain a CUDA distribution. We need it to resolve to /usr/local/cuda/bin,
+    # so we add it to $PATH here. We can make CMake still use ccache by
+    # specifying CUDA_NVCC_EXECUTABLE above.
+    export PATH=/usr/local/cuda/bin:$PATH
+    ;;
+esac
+
+# Configure
+cmake .. ${CMAKE_ARGS[*]}
+
+# Build
+if [ "$(uname)" == "Linux" ]; then
+  make "-j$(nproc)" install
+else
+  echo "Don't know how to build on $(uname)"
+  exit 1
+fi
---
+++ .jenkins/test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+
+LOCAL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ROOT_DIR=$(dirname "$LOCAL_DIR")
+
+cd "$ROOT_DIR"
+
+export PYTHONPATH="${PYTHONPATH}:/usr/local/caffe2"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/caffe2/lib"
+
+rm -rf test
+mkdir -p test/{cpp,python}
+TEST_DIR="$ROOT_DIR/test"
+
+cd /usr/local/caffe2
+
+# Commands below may exit with non-zero status
+set +e
+exit_code=0
+
+# C++ tests
+echo "Running C++ tests.."
+for test in ./test/*; do
+  # Skip tests we know are hanging or bad
+  case "$(basename "$test")" in
+    event_test)
+      continue
+      ;;
+    event_gpu_test)
+      continue
+      ;;
+    net_test)
+      continue
+      ;;
+    mkl_utils_test)
+      continue
+      ;;
+  esac
+
+  "$test" --gtest_output=xml:"$TEST_DIR"/cpp/$(basename "$test").xml
+  tmp_exit_code="$?"
+  if [ "$exit_code" -eq 0 ]; then
+    exit_code="$tmp_exit_code"
+  fi
+done
+
+# Python tests
+echo "Running Python tests.."
+python \
+  -m pytest \
+  -v \
+  --junit-xml="$TEST_DIR"/python/result.xml \
+  --ignore caffe2/python/test/executor_test.py \
+  --ignore caffe2/python/operator_test/matmul_op_test.py \
+  --ignore caffe2/python/operator_test/rnn_cell_test.py \
+  --ignore caffe2/python/mkl/mkl_sbn_op_test.py \
+  --ignore caffe2/python/mkl/mkl_sbn_speed_test.py \
+  caffe2/python/
+tmp_exit_code="$?"
+if [ "$exit_code" -eq 0 ]; then
+  exit_code="$tmp_exit_code"
+fi
+
+# Exit with the first non-zero status we got
+exit "$exit_code"
