0509f401d14b2edf3fa6cde8a1644bd0289b9231
Francisco Massa <fvsmassa@gmail.com>
2017-11-08 22:25:49 +0000

    Update ATen to fix issues with old g++ (#3574)
    * Update ATen to fix issues with old g++
    
    * Add comments

--- aten/src/ATen/templates/TypeDerived.cpp
+++ aten/src/ATen/templates/TypeDerived.cpp
@@ -1,3 +1,8 @@
+// required for old g++ to compile PRId64 macros, see
+// https://github.com/pytorch/pytorch/issues/3571
+// for context
+#define __STDC_FORMAT_MACROS
+
 #include "ATen/${Type}.h"
 #include "ATen/${Storage}.h"
 #include "ATen/${Tensor}.h"
