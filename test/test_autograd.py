706f050e500b587bbfaede08cb56e97e98189460
Gregory Chanan <gchanan@fb.com>
2017-11-09 08:09:42 -0800

    Support gradcheck on empty inputs, check it for cat with an empty Variable.

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1503,11 +1503,14 @@ class TestAutograd(TestCase):
     def test_cat_empty(self):
         f_args_variable = (Variable(torch.randn(0), requires_grad=True),
                            Variable(torch.randn(S, S), requires_grad=True))
-        # gradcheck doesn't work on empty input Variables (hence False passed below)
+        # gradgradcheck doesn't work (because gradcheck doesn't work for empty outputs?)
+        # hence False passed below, but gradcheck checked explicitly.
         f_args_tensor = deepcopy(unpack_variables(f_args_variable))
         run_functional_checks(self, "test_cat_empty", "cat",
                               lambda a, b: torch.cat((a, b)),
                               False, f_args_variable, f_args_tensor)
+        self.assertTrue(gradcheck(lambda a, b: torch.cat((a, b)), f_args_variable, eps=1e-6, atol=PRECISION))
+
 
     def test_variable_traverse(self):
         def get_out_and_unrefed_cycle():
