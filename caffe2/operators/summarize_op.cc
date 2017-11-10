209883e31990605579b29ba74bce5555449f67a2
Xianjie Chen <cxj@fb.com>
2017-11-09 16:50:22 -0800

    make summarize op support larger blob and more robust
    Summary:
    - so that it can also summarize blob of size larger than int
    - the calculation of the mean and std may overflow/underflow, change to use double for intermediate calculation
    
    Differential Revision: D6278275
    
    fbshipit-source-id: f0bb72a5279212d429fa6d09b5487cad1baacdbe

--- caffe2/operators/summarize_op.cc
+++ caffe2/operators/summarize_op.cc
@@ -18,26 +18,26 @@
 
 namespace caffe2 {
 
-template<>
+template <>
 bool SummarizeOp<float, CPUContext>::RunOnDevice() {
   auto& X = Input(0);
-  const int N = X.size();
-  DCHECK_GT(N, 0);
+  const auto N = X.size();
+  CAFFE_ENFORCE_GT(N, 0);
+
   const float* Xdata = X.data<float>();
-  float mean = 0;
+  double mean = 0;
   float max = Xdata[0];
   float min = Xdata[0];
-  for (int i = 0; i < N; ++i) {
-    mean += Xdata[i];
+  for (auto i = 0; i < N; ++i) {
+    mean += static_cast<double>(Xdata[i]) / N;
     max = std::max(max, Xdata[i]);
     min = std::min(min, Xdata[i]);
   }
-  mean /= N;
   // We will simply do a two-pass. More efficient solutions can be written but
   // I'll keep code simple for now.
-  float standard_deviation = 0;
-  for (int i = 0; i < N; ++i) {
-    float diff = Xdata[i] - mean;
+  double standard_deviation = 0;
+  for (auto i = 0; i < N; ++i) {
+    double diff = Xdata[i] - mean;
     standard_deviation += diff * diff;
   }
   // Unbiased or biased? Let's do unbiased now.
@@ -52,8 +52,8 @@ bool SummarizeOp<float, CPUContext>::RunOnDevice() {
     float* Ydata = Y->mutable_data<float>();
     Ydata[MIN_IDX] = min;
     Ydata[MAX_IDX] = max;
-    Ydata[MEAN_IDX] = mean;
-    Ydata[STD_IDX] = standard_deviation;
+    Ydata[MEAN_IDX] = static_cast<float>(mean);
+    Ydata[STD_IDX] = static_cast<float>(standard_deviation);
   }
   return true;
 }
@@ -63,19 +63,24 @@ REGISTER_CPU_OPERATOR(Summarize, SummarizeOp<float, CPUContext>);
 // Input: X; output: if set, a summarized Tensor of shape 4, with the values
 // being min, max, mean and std respectively.
 OPERATOR_SCHEMA(Summarize)
-  .NumInputs(1)
-  .NumOutputs(0, 1)
-  .SetDoc(R"DOC(
+    .NumInputs(1)
+    .NumOutputs(0, 1)
+    .SetDoc(R"DOC(
 Summarize computes four statistics of the input tensor (Tensor<float>)- min,
 max, mean and standard deviation. The output will be written to a 1-D tensor of
 size 4 if an output tensor is provided. Else, if the argument 'to_file' is
 greater than 0, the values are written to a log file in the root folder.
 )DOC")
-  .Arg("to_file", "(int, default 0) flag to indicate if the summarized "
-       "statistics have to be written to a log file.")
-  .Input(0, "data", "The input data as Tensor<float>.")
-  .Output(0, "output", "1-D tensor (Tensor<float>) of size 4 containing min, "
-          "max, mean and standard deviation");
+    .Arg(
+        "to_file",
+        "(int, default 0) flag to indicate if the summarized "
+        "statistics have to be written to a log file.")
+    .Input(0, "data", "The input data as Tensor<float>.")
+    .Output(
+        0,
+        "output",
+        "1-D tensor (Tensor<float>) of size 4 containing min, "
+        "max, mean and standard deviation");
 
 SHOULD_NOT_DO_GRADIENT(Summarize);
-}  // namespace caffe2
+} // namespace caffe2
