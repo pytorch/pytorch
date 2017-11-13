97e4743aafe949c4128c1c175390e547ec73a484
Fei Sun <feisun@fb.com>
2017-11-13 10:39:03 -0800

    Caffe2_benchmark can benchmark multiple backend engines
    Summary:
    Support the default, nnpack, and opengl back end engines. There is no need to change the model. The file would convert the model to appropriate backend.
    Closes https://github.com/caffe2/caffe2/pull/1436
    
    Reviewed By: hlu1
    
    Differential Revision: D6275975
    
    Pulled By: sf-wind
    
    fbshipit-source-id: fbd864e18f00372b4c03de294c22383c405a9210

--- caffe2/share/contrib/binaries/caffe2_benchmark/caffe2_benchmark.cc
+++ caffe2/share/contrib/binaries/caffe2_benchmark/caffe2_benchmark.cc
@@ -11,6 +11,15 @@
 #include "caffe2/utils/proto_utils.h"
 #include "caffe2/utils/string_utils.h"
 
+#if CAFFE2_MOBILE && (CAFFE2_ANDROID || CAFFE2_IOS)
+#include "caffe2/mobile/contrib/opengl/core/rewrite_net.h"
+#endif
+
+CAFFE2_DEFINE_string(
+    backend,
+    "default",
+    "The backend to use when running the model. The allowed "
+    "backend choices are: default, nnpack, opengl");
 CAFFE2_DEFINE_string(
     init_net,
     "",
@@ -106,9 +115,9 @@ int main(int argc, char** argv) {
   unique_ptr<caffe2::Workspace> workspace(new caffe2::Workspace());
 
   // Run initialization network.
-  caffe2::NetDef net_def;
-  CAFFE_ENFORCE(ReadProtoFromFile(caffe2::FLAGS_init_net, &net_def));
-  CAFFE_ENFORCE(workspace->RunNetOnce(net_def));
+  caffe2::NetDef init_net_def;
+  CAFFE_ENFORCE(ReadProtoFromFile(caffe2::FLAGS_init_net, &init_net_def));
+  CAFFE_ENFORCE(workspace->RunNetOnce(init_net_def));
 
   // Load input.
   if (caffe2::FLAGS_input.size()) {
@@ -150,7 +159,36 @@ int main(int argc, char** argv) {
   }
 
   // Run main network.
+  caffe2::NetDef net_def;
   CAFFE_ENFORCE(ReadProtoFromFile(caffe2::FLAGS_net, &net_def));
+  if (caffe2::FLAGS_backend == "opengl") {
+#if CAFFE2_MOBILE && (CAFFE2_ANDROID || CAFFE2_IOS)
+    caffe2::NetDef opengl_net_def;
+    if (caffe2::tryConvertToOpenGL(init_net_def, net_def, &opengl_net_def)) {
+      net_def = opengl_net_def;
+      if (caffe2::FLAGS_run_individual) {
+        caffe2::FLAGS_run_individual = false;
+        LOG(INFO)
+            << "OpenGL implementation does not support individual operator delay. Run net delay only";
+      }
+    } else {
+      LOG(ERROR)
+          << "Net cannot be converted to OpenGL format, use original model instead";
+    }
+#else
+    LOG(ERROR) << "OpenGL build can only be used in mobile platform";
+#endif
+
+  } else if (caffe2::FLAGS_backend == "nnpack") {
+    for (int i = 0; i < net_def.op_size(); i++) {
+      caffe2::OperatorDef* op_def = net_def.mutable_op(i);
+      op_def->set_engine("NNPACK");
+    }
+  } else {
+    CAFFE_ENFORCE(
+        caffe2::FLAGS_backend == "default", "Backend is not supported");
+  }
+
   caffe2::NetBase* net = workspace->CreateNet(net_def);
   CHECK_NOTNULL(net);
 
