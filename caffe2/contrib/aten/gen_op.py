5d61b1f55972aac243cfebde15afa93d96b59eae
Zachary DeVito <zdevito@fb.com>
2017-11-13 18:15:11 -0800

    Update ATen operator in C2
    Summary: Update ATen operator to new version of aten library. This adds support for many neural network functions that previously were not exposed. This also supports operators that take a list of tensor inputs or produce a list of outputs by appending them to the end of the input/output lists.
    
    Reviewed By: jamesr66a
    
    Differential Revision: D6267327
    
    fbshipit-source-id: 0df6af18369241afa8600fd51923811749900c2e

--- caffe2/contrib/aten/gen_op.py
+++ caffe2/contrib/aten/gen_op.py
@@ -15,17 +15,18 @@
 
 import sys
 import yaml
+from copy import deepcopy
 project_root = sys.argv[1]
 sys.path.append(project_root + "/third_party/aten/src/ATen")
 from code_template import CodeTemplate as CT
+OP_TEMPLATE = CT.from_file(project_root +
+                           '/caffe2/contrib/aten/aten_op_template.h')
 
 try:
     # use faster C loader if available
     from yaml import CLoader as Loader
 except ImportError:
     from yaml import Loader
-
-OP_TEMPLATE = CT.from_file(project_root+'/caffe2/contrib/aten/aten_op_template.h')
 
 
 def write(filename, s):
@@ -38,20 +39,13 @@ def read(filename):
         return f.read()
 
 
-decls = yaml.load(read('aten/src/ATen/ATen/Declarations.yaml'), Loader=Loader)
-
-top_env = {
-    'mappings': [],
-    'implementations': [],
-}
-
-
-def is_tensor_type(t):
-    return "Tensor" in t
+def value_has_tensors(v):
+    return "Tensor" in v['dynamic_type']
 
 
 def value_is_tensor_type(v):
-    return is_tensor_type(v['dynamic_type'])
+    return value_has_tensors(v) and v['dynamic_type'] != 'TensorList'
+
 
 # for each aten type, how do we handle a return value of that type?
 RETURN_MAP = {
@@ -59,6 +53,7 @@ RETURN_MAP = {
     'Scalar': 'assignTo(Output(${offset}),*inferred_type, ${output});',
     'bool': 'assignToValue<int64_t>(Output(${offset}),${output});',
     'int64_t': 'assignToValue<int64_t>(Output(${offset}),${output});',
+    'std::vector<Tensor>': 'assignListStartingAt(${offset}, ${output});',
 }
 
 # for each non-Tensor aten argument, how to we read it from caffe2's
@@ -68,14 +63,28 @@ ARGUMENT_MAP = {
     'Scalar': 'at::Scalar ${arg} = readScalarAttribute("${arg}");',
     'bool': 'bool ${arg} = readAttribute<int64_t>("${arg}");',
     'int': 'int ${arg} = readAttribute<int64_t>("${arg}");',
+    'double': 'double ${arg} = readAttribute<float>("${arg}");',
     'int64_t': 'int64_t ${arg} = readAttribute<int64_t>("${arg}");',
     'IntList': 'auto ${arg} = readIntList("${arg}");',
+    'std::array<bool, 2>': 'auto ${arg} = readBoolMask<2>("${arg}");',
+    'std::array<bool, 3>': 'auto ${arg} = readBoolMask<3>("${arg}");',
 }
+
+
+def expand(o):
+    num_defaults = sum(1 if 'default' in arg else 0 for arg in o['arguments'])
+    results = [o]
+    for i in range(0, num_defaults):
+        # last num_default values should be default
+        assert('default' in o['arguments'][-(i + 1)])
+        v = deepcopy(o)
+        v['arguments'] = v['arguments'][:-(i + 1)]
+        results.append(v)
+    return results
 
 
 # filter the list of declarations removing things we cannot support
 def supports(o):
-
     # skip all in-place operators for now since aten cannot Resize
     # caffe2 memory inside an operator
     if o['inplace']:
@@ -88,19 +97,19 @@ def supports(o):
 
     # skip return types we cannot handle
     for ret in o['returns']:
-        if not value_is_tensor_type(ret) and ret['type'] not in RETURN_MAP:
-            print("Skipping {} Because of Ret: {} ({})".format(o['name'], ret['type'], ret['dynamic_type']))
+        if not value_has_tensors(ret) and ret['type'] not in RETURN_MAP:
+            print("Skipping {} Because of Ret: {} ({})".format(
+                  o['name'], ret['type'], ret['dynamic_type']))
             return False
 
     # skip arguments we cannot handle
     for arg in o['arguments']:
-        if not value_is_tensor_type(arg) and arg['type'] not in ARGUMENT_MAP:
-            print("Skipping {} Because of Arg: {} ({}) ".format(o['name'], arg['type'], arg['dynamic_type']))
+        if not value_has_tensors(arg) and arg['type'] not in ARGUMENT_MAP:
+            print("Skipping {} Because of Arg: {} ({}) ".format(
+                  o['name'], arg['type'], arg['dynamic_type']))
             return False
     return True
 
-
-filtered = [o for o in decls if supports(o)]
 
 # template for each potential operator.
 # each operator has an integer 'key' associated with it, and
@@ -129,88 +138,117 @@ def get_output(o, i):
 
 
 def attribute_names(o):
-    return sorted([a['name'] for a in o['arguments'] if not value_is_tensor_type(a)])
+    return sorted([a['name'] for a in o['arguments'] if not value_has_tensors(a)])
+
+
+def required_attribute_names(o):
+    return sorted([a['name'] for a in o['arguments'] if not value_has_tensors(a) and 'default' not in a])
 
 
 def self_as_first_argument(arguments):
     return ([a for a in arguments if a['name'] == 'self'] +
             [a for a in arguments if a['name'] != 'self'])
 
-seen = set()
-key = 0
-for o in filtered:
-    # [DESCRIPTORS]
-    # each option is associated with a descriptor string that is used
-    # to figure out which version of an op is being used:
-    # The format is:
-    #     opname-num_inputs-attribute_1-attribute2
-    # Example:
-    #  lerp-2-weight
-    #  the operator lerp takes 2 arguments and has the attribute weight
-    attr_names = attribute_names(o)
-    num_inputs = len(o['arguments']) - len(attr_names)
-    descriptor = '-'.join([o['name'], str(num_inputs)] + attr_names)
 
-    if descriptor in seen:
-        continue
-    seen.add(descriptor)
+def get_num_inputs(o):
+    args = 0
+    for a in o['arguments']:
+        if a['type'] == 'TensorList':
+            return '*'
+        elif value_has_tensors(a):
+            args += 1
+    return str(args)
 
-    # map from descriptor string to the integer key in the switch statements
-    # that initializes the operators
-    top_env['mappings'].append('{{ "{}", {} }},'.format(descriptor, key))
-    env = {
-        'name': o['name'],
-        'statements': [],
-        'arguments': [],
-        'assignments': [],
-        'initialization': [],
-        'key': str(key),
+
+if __name__ == '__main__':
+    decls = yaml.load(read('aten/src/ATen/ATen/Declarations.yaml'), Loader=Loader)
+    filtered = [expanded for o in decls for expanded in expand(o) if supports(expanded)]
+    top_env = {
+        'mappings': [],
+        'implementations': [],
     }
-    defined_inferred_type = False
+    seen = set()
+    key = 0
+    for o in filtered:
+        # [DESCRIPTORS]
+        # each option is associated with a descriptor string that is used
+        # to figure out which version of an op is being used:
+        # The format is:
+        #     opname-num_inputs-attribute_1-attribute2
+        # Example:
+        #  lerp-2-weight
+        #  the operator lerp takes 2 arguments and has the attribute weight
+        attr_names = attribute_names(o)
+        num_inputs = get_num_inputs(o)
+        descriptor = '-'.join([o['name']] + attr_names + [num_inputs])
+        if descriptor in seen:
+            continue
+        seen.add(descriptor)
 
-    if 'Tensor' in o['method_of']:
-        # make sure 'self' is the first argument. currently Declarations.yaml
-        # does not always do this. Instead it keeps the argument list the same order
-        # as the Type method.
-        o['arguments'] = self_as_first_argument(o['arguments'])
-    elif 'namespace' not in o['method_of']:
-        # methods on type like 'ones' or 'zeros' always take a
-        # string attribute that is translated into the at::Type object
-        # e.g. "Float" is at::kFloat
-        assert('Type' in o['method_of'])
-        defined_inferred_type = True
-        env['initialization'].append('auto inferred_type = readTypeAttribute("type");')
+        # map from descriptor string to the integer key in the switch statements
+        # that initializes the operators
+        top_env['mappings'].append('{{ "{}", {} }},'.format(descriptor, key))
+        env = {
+            'name': o['name'],
+            'statements': [],
+            'arguments': [],
+            'assignments': [],
+            'initialization': [],
+            'key': str(key),
+        }
+        defined_inferred_type = False
 
-    i = 0
-    for arg in o['arguments']:
-        env['arguments'].append(arg['name'])
-        if value_is_tensor_type(arg):
-            # load tensor inputs from Caffe2
-            env['statements'].append("auto {}_ = Input({});".format(arg['name'], i))
-            i += 1
-            env['statements'].append(CT(
-                "auto ${name} = tensorWrapping(${name}_);").substitute(arg))
-            if arg['dynamic_type'] == 'Tensor' and not defined_inferred_type:
-                # first tensor input is used to define the output type.
-                defined_inferred_type = True
-                env['statements'].append('auto inferred_type = &({}.type());'.format(arg['name']))
+        if 'Tensor' in o['method_of']:
+            # make sure 'self' is the first argument. currently Declarations.yaml
+            # does not always do this. Instead it keeps the argument list the same order
+            # as the Type method.
+            o['arguments'] = self_as_first_argument(o['arguments'])
+        elif 'namespace' not in o['method_of']:
+            # methods on type like 'ones' or 'zeros' always take a
+            # string attribute that is translated into the at::Type object
+            # e.g. "Float" is at::kFloat
+            assert('Type' in o['method_of'])
+            defined_inferred_type = True
+            env['initialization'].append(
+                'auto inferred_type = readTypeAttribute("type");')
+
+        i = 0
+        for arg in o['arguments']:
+            env['arguments'].append(arg['name'])
+            if arg['type'] == 'TensorList':
+                env['statements'].append(
+                    'auto {} = loadInputsAtOffset({});'.format(arg['name'], i))
+            elif value_is_tensor_type(arg):
+                assert(i != '*')  # tensor list is not last argument
+                # load tensor inputs from Caffe2
+                env['statements'].append(
+                    "auto {} = loadInput({});".format(arg['name'], i))
+                i += 1
+                if arg['dynamic_type'] == 'Tensor' and not defined_inferred_type:
+                    # first tensor input is used to define the output type.
+                    defined_inferred_type = True
+                    env['statements'].append(
+                        'auto inferred_type = &({}.type());'.format(
+                            arg['name']))
+            else:
+                init = CT(ARGUMENT_MAP[arg['type']]).substitute(env, arg=arg['name'])
+                env['initialization'].append(init)
+
+        for i, r in enumerate(o['returns']):
+            t = RETURN_MAP[r['type'] if not value_is_tensor_type(r) else 'Tensor']
+            assignment = CT(t).substitute(env, offset=i, output=get_output(o, i))
+            env['assignments'].append(assignment)
+
+        if 'Tensor' in o['method_of']:
+            env['invocation'] = "self.{}({})".format(
+                o['name'], ', '.join(env['arguments'][1:]))
+        elif 'namespace' in o['method_of']:
+            env['invocation'] = CT("at::${name}(${arguments})").substitute(env)
         else:
-            init = CT(ARGUMENT_MAP[arg['type']]).substitute(env, arg=arg['name'])
-            env['initialization'].append(init)
+            assert('Type' in o['method_of'])
+            env['invocation'] = CT(
+                'inferred_type->${name}(${arguments})').substitute(env)
 
-    for i, r in enumerate(o['returns']):
-        t = RETURN_MAP[r['type'] if not value_is_tensor_type(r) else 'Tensor']
-        assignment = CT(t).substitute(env, offset=i, output=get_output(o, i))
-        env['assignments'].append(assignment)
-
-    if 'Tensor' in o['method_of']:
-        env['invocation'] = "self.{}({})".format(o['name'], ', '.join(env['arguments'][1:]))
-    elif 'namespace' in o['method_of']:
-        env['invocation'] = CT("at::${name}(${arguments})").substitute(env)
-    else:
-        assert('Type' in o['method_of'])
-        env['invocation'] = CT('inferred_type->${name}(${arguments})').substitute(env)
-
-    top_env['implementations'].append(OPTION_TEMPLATE.substitute(env))
-    key += 1
-write("aten_op.h", OP_TEMPLATE.substitute(top_env))
+        top_env['implementations'].append(OPTION_TEMPLATE.substitute(env))
+        key += 1
+    write("aten_op.h", OP_TEMPLATE.substitute(top_env))
