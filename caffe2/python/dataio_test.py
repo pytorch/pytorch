067bc141c35a6c14d1951c54d6217efc5908fe9f
Artem Volkhin <volkhin@fb.com>
2017-11-15 12:12:49 -0800

    Cached reader
    Summary: a wrapper around reader with persistent file cache.
    
    Reviewed By: kennyhorror
    
    Differential Revision: D6257639
    
    fbshipit-source-id: 113296173ca18d25b86e188e0c09e3dbd830969d

--- caffe2/python/dataio_test.py
+++ caffe2/python/dataio_test.py
@@ -25,20 +25,38 @@ from caffe2.python.schema import Struct, NewRecord, FeedRecord
 from caffe2.python.session import LocalSession
 from caffe2.python.task import TaskGroup, final_output, WorkspaceType
 from caffe2.python.test_util import TestCase
+from caffe2.python.cached_reader import CachedReader
 from caffe2.python import core, workspace
 from caffe2.python.net_builder import ops
 import numpy as np
+import os
+import shutil
+import tempfile
 
 
-def init_dataset(ws):
+def init_dataset(ws, size=100):
     src_init = core.Net('src_init')
     with core.NameScope('src'):
-        src_values = Struct(('label', np.array(range(100))))
+        src_values = Struct(('label', np.array(range(size))))
         src_blobs = NewRecord(src_init, src_values)
         src_ds = Dataset(src_blobs)
         FeedRecord(src_blobs, src_values, ws)
     ws.run(src_init)
     return src_ds
+
+
+def read_all_data(ws, reader, session):
+    dst_init = core.Net('dst_init')
+    with core.NameScope('dst'):
+        dst_ds = Dataset(reader.schema().clone_schema())
+        dst_ds.init_empty(dst_init)
+    session.run(dst_init)
+
+    with TaskGroup(workspace_type=WorkspaceType.GLOBAL) as tg:
+        pipe(reader, dst_ds.writer(), num_threads=8)
+    session.run(tg)
+
+    return ws.blobs[str(dst_ds.content().label())].fetch()
 
 
 class TestReaderWithLimit(TestCase):
@@ -91,7 +109,6 @@ class TestReaderWithLimit(TestCase):
         self.assertEquals(totals[0].fetch(), 25)
         self.assertEquals(totals[1].fetch(), 25)
         self.assertEquals(totals[2].fetch(), 6)
-
 
     def test_reader_with_limit(self):
         ws = workspace.C.Workspace()
@@ -150,3 +167,47 @@ class TestReaderWithLimit(TestCase):
             sorted(ws.blobs[str(dst_ds.content().label())].fetch()),
             sorted(list(range(100)) * 2)
         )
+
+    def test_cached_reader(self):
+        ws = workspace.C.Workspace()
+        session = LocalSession(ws)
+
+        def build_source_reader(size):
+            src_ds = init_dataset(ws, size)
+            return src_ds.reader()
+
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            path = f.name
+            f.close()
+            os.remove(path)
+
+            """ 1. Read data for the first time. """
+            cached_reader1 = CachedReader(build_source_reader(100))
+            init_step = cached_reader1.build_cache(path)
+            session.run(init_step)
+
+            data = read_all_data(ws, cached_reader1, session)
+            self.assertEqual(sorted(data), list(range(100)))
+
+            """ 2. Read data from cache. """
+            workspace.ResetWorkspace()
+            cached_reader2 = CachedReader(build_source_reader(200))
+            init_step = cached_reader2.build_cache(path)
+            session.run(init_step)
+
+            data = read_all_data(ws, cached_reader2, session)
+            self.assertEqual(sorted(data), list(range(100)))
+
+            shutil.rmtree(path)
+
+            """ 3. We removed cache so we expect to receive data from original
+            reader. """
+            workspace.ResetWorkspace()
+            cached_reader3 = CachedReader(build_source_reader(300))
+            init_step = cached_reader3.build_cache(path)
+            session.run(init_step)
+
+            data = read_all_data(ws, cached_reader3, session)
+            self.assertEqual(sorted(data), list(range(300)))
+
+            shutil.rmtree(path)
