d6ee53e1cbedae5e364ae5f86dd9d190cb571584
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-11-16 14:06:25 -0800

    Make macOS build use ccache via CMAKE_C*_COMPILER
    Summary: Closes https://github.com/caffe2/caffe2/pull/1484
    
    Reviewed By: Yangqing
    
    Differential Revision: D6352137
    
    Pulled By: pietern
    
    fbshipit-source-id: f17c7c8cf38e7a4b8e2af60010bdde920f39e7c5

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -5,33 +5,42 @@
 ##############################################################################
 #
 
+set -e
+
 CAFFE2_ROOT="$( cd "$(dirname "$0")"/.. ; pwd -P)"
-echo "Caffe2 codebase root is: $CAFFE2_ROOT"
+
+CMAKE_ARGS=()
 
 # Build protobuf compiler from third_party if configured to do so
 if [ -n "${USE_HOST_PROTOC:-}" ]; then
-    echo "Building protoc before compiling Caffe2..."
-    $CAFFE2_ROOT/scripts/build_host_protoc.sh || exit 1
-    CMAKE_ARGS="$CMAKE_ARGS \
-        -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=$CAFFE2_ROOT/build_host_protoc/bin/protoc"
+  echo "USE_HOST_PROTOC is set; building protoc before building Caffe2..."
+  "$CAFFE2_ROOT/scripts/build_host_protoc.sh"
+  CUSTOM_PROTOC_EXECUTABLE="$CAFFE2_ROOT/build_host_protoc/bin/protoc"
+  echo "Built protoc $("$CUSTOM_PROTOC_EXECUTABLE" --version)"
+  CMAKE_ARGS+=("-DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=$CUSTOM_PROTOC_EXECUTABLE")
 fi
 
 # We are going to build the target into build.
 BUILD_ROOT="$CAFFE2_ROOT/build"
 mkdir -p "$BUILD_ROOT"
-echo "Building Caffe2 in: $BUILD_ROOT"
-
-# Now, actually build the target.
 cd "$BUILD_ROOT"
-set -x
-cmake .. ${CMAKE_ARGS} "$@" || exit 1
+echo "Building Caffe2 in: $BUILD_ROOT"
 
 if [ "$(uname)" == 'Darwin' ]; then
   # Use ccache if available (this path is where Homebrew installs ccache symlinks)
-  if [ -d /usr/local/opt/ccache/libexec ]; then
-    export PATH="/usr/local/opt/ccache/libexec:$PATH"
+  CCACHE_WRAPPER_PATH=/usr/local/opt/ccache/libexec
+  if [ -d "$CCACHE_WRAPPER_PATH" ]; then
+    CMAKE_ARGS+=("-DCMAKE_C_COMPILER=$CCACHE_WRAPPER_PATH/gcc")
+    CMAKE_ARGS+=("-DCMAKE_CXX_COMPILER=$CCACHE_WRAPPER_PATH/g++")
   fi
+fi
 
+# Now, actually build the target.
+cmake .. \
+      "${CMAKE_ARGS[@]}" \
+      "$@"
+
+if [ "$(uname)" == 'Darwin' ]; then
   cmake --build . -- "-j$(sysctl -n hw.ncpu)"
 else
   cmake --build . -- "-j$(nproc)"
