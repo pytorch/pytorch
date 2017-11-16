0f0e116652a849f217d8f25b7e2bf3a46a05a973
Yiming Wu <wyiming@fb.com>
2017-11-16 12:03:44 -0800

    cast op for empty batch
    Summary: Cast op cuda can deal with empty batch now.
    
    Reviewed By: azzolini
    
    Differential Revision: D6350138
    
    fbshipit-source-id: 2f3d19f4d42ff34806aa9597690e66f6b4de1a6b

--- caffe2/operators/cast_op.cu
+++ caffe2/operators/cast_op.cu
@@ -20,7 +20,6 @@
 
 namespace caffe2 {
 
-namespace {
 template <typename DstType, typename SrcType>
 __global__ void CastKernel(const int N, const SrcType* X, DstType* Y) {
   CUDA_1D_KERNEL_LOOP(i, N) {
@@ -28,7 +27,6 @@ __global__ void CastKernel(const int N, const SrcType* X, DstType* Y) {
     Y[i] = convert::To<SrcType, DstType>(X[i]);
   }
 }
-}  // namespace
 
 template <>
 template <typename DstType, typename SrcType>
@@ -40,9 +38,15 @@ bool CastOp<CUDAContext>::DoRunWithType() {
   auto* out = output->template mutable_data<DstType>();
   DCHECK(input.size() < INT_MAX);
   int N = input.size();
-  CastKernel<DstType, SrcType><<<
-      CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS,
-      0, context_.cuda_stream()>>>(N, data, out);
+  if (N == 0) {
+    // skip the rest of the computation if input is empty
+    return true;
+  }
+  CastKernel<DstType, SrcType>
+      <<<CAFFE_GET_BLOCKS(N),
+         CAFFE_CUDA_NUM_THREADS,
+         0,
+         context_.cuda_stream()>>>(N, data, out);
   return true;
 }
 
