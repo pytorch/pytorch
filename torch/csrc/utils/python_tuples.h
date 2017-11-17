c6ed85a0cd43311b2c0f7b442f91be3439359ff2
Gregory Chanan <gchanan@fb.com>
2017-11-17 08:23:25 -0800

    Throw python errors.

--- torch/csrc/utils/python_tuples.h
+++ torch/csrc/utils/python_tuples.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Python.h>
-#include <sstream>
+#include "torch/csrc/Exceptions.h"
 #include "torch/csrc/utils/object_ptr.h"
 #include "torch/csrc/utils/python_numbers.h"
 
@@ -9,9 +9,7 @@ inline void THPUtils_packInt64Array(PyObject *tuple, size_t size, const int64_t 
   for (size_t i = 0; i != size; ++i) {
     PyObject *i64 = THPUtils_packInt64(sizes[i]);
     if (!i64) {
-      std::ostringstream oss;
-      oss << "Could not pack int64 at position " << i;
-      throw std::runtime_error(oss.str());
+      throw python_error();
     }
     PyTuple_SET_ITEM(tuple, i, THPUtils_packInt64(sizes[i]));
   }
@@ -19,6 +17,7 @@ inline void THPUtils_packInt64Array(PyObject *tuple, size_t size, const int64_t 
 
 inline PyObject* THPUtils_packInt64Array(size_t size, const int64_t *sizes) {
   THPObjectPtr tuple(PyTuple_New(size));
+  if (!tuple) throw python_error();
   THPUtils_packInt64Array(tuple.get(), size, sizes);
   return tuple.release();
 }
