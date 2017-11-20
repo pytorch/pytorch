11fe22ec8098d37ddc98a92a30bedeab87015931
Scott Stevenson <scott@stevenson.io>
2017-11-20 20:33:30 +0000

    Reflect renaming of OS X to macOS

--- setup.py
+++ setup.py
@@ -671,7 +671,7 @@ if WITH_CUDA:
         thnvrtc_link_flags += ['-lcuda', '-lnvrtc']
     cuda_stub_path = [cuda_lib_path + '/stubs']
     if IS_DARWIN:
-        # on OSX this is where the CUDA stub is installed according to the manual
+        # on macOS this is where the CUDA stub is installed according to the manual
         cuda_stub_path = ["/usr/local/cuda/lib"]
     THNVRTC = Extension("torch._nvrtc",
                         sources=['torch/csrc/nvrtc.cpp'],
