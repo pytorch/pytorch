5215640a41d616d508a6e3b67a043bcb71d09988
Richard Zou <zou3519@users.noreply.github.com>
2017-11-21 18:33:41 -0500

    Fix cosine_similarity's output shape (#3811)

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -1650,7 +1650,7 @@ def cosine_similarity(x1, x2, dim=1, eps=1e-8):
     w12 = torch.sum(x1 * x2, dim)
     w1 = torch.norm(x1, 2, dim)
     w2 = torch.norm(x2, 2, dim)
-    return (w12 / (w1 * w2).clamp(min=eps)).squeeze()
+    return w12 / (w1 * w2).clamp(min=eps)
 
 
 def triplet_margin_loss(anchor, positive, negative, margin=1.0, p=2, eps=1e-6, swap=False):
