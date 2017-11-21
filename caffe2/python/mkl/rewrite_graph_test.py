cd2ecd669657ca3a109ea4c0a181d7ef8aa29094
Andrew Tulloch <tulloch@fb.com>
2017-11-20 16:53:44 -0800

    More extensions
    Reviewed By: kevinbchen
    
    Differential Revision: D6300944
    
    fbshipit-source-id: e915c3f3d6b475752d8b7df82ec467d86f88a7c7

--- caffe2/python/mkl/rewrite_graph_test.py
+++ caffe2/python/mkl/rewrite_graph_test.py
@@ -91,6 +91,94 @@ def simple_cnn():
     return model, [(1, 3, 32, 32)]
 
 
+def alexnet():
+    model = ModelHelper(name="r", arg_scope={"order": "NCHW", "is_test": True})
+    conv1 = brew.conv(
+        model,
+        "data",
+        "conv1",
+        3,
+        64,
+        11, ('XavierFill', {}), ('ConstantFill', {}),
+        stride=4,
+        pad=2
+    )
+    relu1 = brew.relu(model, conv1, "conv1")
+    pool1 = brew.max_pool(model, relu1, "pool1", kernel=3, stride=2, pad=0,
+                          legacy_pad=3)
+    lrn1 = brew.lrn(
+        model, pool1, "pool1_lrn", size=5, alpha=1.0e-4, beta=0.75, bias=1.0)
+    conv2 = brew.conv(
+        model,
+        lrn1,
+        "conv2",
+        64,
+        192,
+        5,
+        ('XavierFill', {}),
+        ('ConstantFill', {}),
+        pad=2
+    )
+    relu2 = brew.relu(model, conv2, "conv2")
+    pool2 = brew.max_pool(model, relu2, "pool2", kernel=3, stride=2)
+    lrn2 = brew.lrn(
+        model, pool2, "pool2_lrn", size=5, alpha=1.0e-4, beta=0.75, bias=1.0)
+    conv3 = brew.conv(
+        model,
+        lrn2,
+        "conv3",
+        192,
+        384,
+        3,
+        ('XavierFill', {}),
+        ('ConstantFill', {}),
+        pad=1
+    )
+    relu3 = brew.relu(model, conv3, "conv3")
+    conv4 = brew.conv(
+        model,
+        relu3,
+        "conv4",
+        384,
+        256,
+        3,
+        ('XavierFill', {}),
+        ('ConstantFill', {}),
+        pad=1
+    )
+    relu4 = brew.relu(model, conv4, "conv4")
+    conv5 = brew.conv(
+        model,
+        relu4,
+        "conv5",
+        256,
+        256,
+        3,
+        ('XavierFill', {}),
+        ('ConstantFill', {}),
+        pad=1
+    )
+    relu5 = brew.relu(model, conv5, "conv5")
+    pool5 = brew.max_pool(model, relu5, "pool5", kernel=3, stride=2)
+    fc6 = brew.fc(
+        model,
+        pool5, "fc6", 256 * 6 * 6, 4096, ('XavierFill', {}),
+        ('ConstantFill', {})
+    )
+    relu6 = brew.relu(model, fc6, "fc6")
+    fc7 = brew.fc(
+        model, relu6, "fc7", 4096, 4096, ('XavierFill', {}), ('ConstantFill', {})
+    )
+    relu7 = brew.relu(model, fc7, "fc7")
+    drop7 = brew.dropout(model, relu7, "fc7_dropout", is_test=1, ratio=0.5)
+    fc8 = brew.fc(
+        model, drop7, "fc8", 4096, 1000, ('XavierFill', {}), ('ConstantFill', {})
+    )
+    relu8 = brew.relu(model, fc8, "fc8")
+    _ = brew.dropout(model, relu8, "fc8_dropout", is_test=1, ratio=0.5)
+    return model, [(1, 3, 224, 224)]
+
+
 def simple_resnet():
     model = ModelHelper(name="r", arg_scope={"order": "NCHW", "is_test": True})
     resnet.create_resnet_32x32(
@@ -163,6 +251,20 @@ class MKLRewriteTest(hu.HypothesisTestCase):
         np.testing.assert_allclose(run(cpu_model), run(mkl_model),
                                    atol=1e-4, rtol=1e-4)
 
+    def test_mkl_alexnet_rewrite(self):
+        cpu_model, (shape,) = alexnet()
+        cpu_model = deterministic_io(cpu_model)
+        mkl_model = rewrite_graph.rewrite_model_helper_simple(cpu_model)
+        np.random.seed(1701)
+        X = np.random.randn(*shape).astype(np.float32)
+
+        def run(model):
+            self.ws.run(model.InitProto())
+            self.ws.create_blob(model.Proto().external_input[0]).feed(X)
+            self.ws.run(model.Proto())
+            return self.ws.blobs[model.Proto().external_output[0]].fetch()
+        np.testing.assert_allclose(run(cpu_model), run(mkl_model),
+                                   atol=1e-4, rtol=1e-4)
 
 if __name__ == "__main__":
     import unittest
