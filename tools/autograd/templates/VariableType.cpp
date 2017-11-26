78ea42e37c6b04c5b050db3412c09ec5da457fb2
Adam Paszke <adam.paszke@gmail.com>
2017-11-26 18:08:36 -0500

    Accept sparse tensors of corresponding type in VariableType casts

--- tools/autograd/templates/VariableType.cpp
+++ tools/autograd/templates/VariableType.cpp
@@ -98,7 +98,7 @@ Variable & VariableType::checked_cast(const Type & type, const Tensor & t, const
     runtime_error("Expected a Tensor of type %s but found an undefined Tensor for argument #%d '%s'",
         type.toString(), pos, name);
   }
-  if (&t.type() != &type) {
+  if (&t.type() != &type && &t.type() != &type.toBackend(toSparse(t.type().backend()))) {
     runtime_error("Expected object of type %s but found type %s for argument #%d '%s'",
         type.toString(), t.type().toString(), pos, name);
   }
