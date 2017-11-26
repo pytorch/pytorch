6c465f2013a5bb76c9977190efef55e4ecc6aee3
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2017-11-26 14:58:19 -0800

    Fix padding according to https://github.com/onnx/onnx/issues/261

--- torch/nn/_functions/thnn/auto_symbolic.py
+++ torch/nn/_functions/thnn/auto_symbolic.py
@@ -4,13 +4,13 @@ from torch.autograd._functions.utils import prepare_onnx_paddings
 def reflectionpad_symbolic(g, input, *params):
     mode = "reflect"
     paddings = prepare_onnx_paddings(len(input.type().sizes()), params)
-    return g.op("Pad", input, paddings_i=paddings, mode_s=mode)
+    return g.op("Pad", input, pads_i=paddings, mode_s=mode)
 
 
 def replicationpad_symbolic(g, input, *params):
     mode = "edge"
     paddings = prepare_onnx_paddings(len(input.type().sizes()), params)
-    return g.op("Pad", input, paddings_i=paddings, mode_s=mode)
+    return g.op("Pad", input, pads_i=paddings, mode_s=mode)
 
 
 symbolic_fns = {
