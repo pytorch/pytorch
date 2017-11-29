0e54c3a9899ff4f92a76395d4cfb2f67ebfb452c
Zachary DeVito <zdevito@fb.com>
2017-11-29 16:10:51 -0500

    Significantly speed up the incremental build.
    This commit adds code to setup.py to use ninja to manage
    C++ and code generator dependencies rather than use raw setuptools.
    This is based on similar code added to ONNX.
    
    Enabled optionally when ninja is installed.
    
    On my computer speed for a do-nothing build drops from 10s to 1.5 seconds.
    Speed of other compilation steps is significantly improved as well.
    Dependencies are tracked correctly so the need for ccache is reduced.

---
+++ tools/setup_helpers/ninja_builder.py
@@ -0,0 +1,81 @@
+import os
+import setuptools
+import distutils
+from contextlib import contextmanager
+
+
+# on the fly create a ninja file in build/ and then
+# run it when run() is called.
+class NinjaBuilder(object):
+    def __init__(self, name):
+        import ninja
+        build_dir = 'build'
+        if not os.path.exists(build_dir):
+            os.mkdir(build_dir)
+        self.name = name
+        self.filename = os.path.join(build_dir, 'build.{}.ninja'.format(name))
+        print('Ninja build file at {}'.format(self.filename))
+        self.writer = ninja.Writer(open(self.filename, 'w'))
+        self.writer.rule('do_cmd', '$cmd')
+
+    def run(self):
+        import ninja
+        self.writer.close()
+        assert(0 == ninja._program('ninja', ['-f', self.filename]))
+        # weird build logic in build develop causes some things to be run
+        # twice so make sure even after we run the command we still
+        # reset this to a valid state
+        # don't use the same name or you can't inspect the real ninja files
+        self.__init__(self.name + "_")
+
+
+class ninja_build_ext(setuptools.command.build_ext.build_ext):
+    def _build_default(self, ext):
+        return setuptools.command.build_ext.build_ext.build_extension(self, ext)
+
+    def build_extension(self, ext):
+        builder = NinjaBuilder(ext.name)
+
+        @contextmanager
+        def patch(obj, attr_name, val):
+            orig_val = getattr(obj, attr_name)
+            setattr(obj, attr_name, val)
+            yield
+            setattr(obj, attr_name, orig_val)
+
+        orig_compile = distutils.unixccompiler.UnixCCompiler._compile
+        orig_link = distutils.unixccompiler.UnixCCompiler.link
+
+        def _compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
+            depfile = os.path.splitext(obj)[0] + '.d'
+
+            def spawn(cmd):
+                builder.writer.build(
+                    [obj], 'do_cmd', [src],
+                    variables={
+                        'cmd': cmd,
+                        'depfile': depfile,
+                        'deps': 'gcc'
+                    })
+
+            extra_postargs = extra_postargs + ['-MMD', '-MF', depfile]
+            with patch(self, 'spawn', spawn):
+                orig_compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+        def link(self, target_desc, objects,
+                 output_filename, output_dir=None, libraries=None,
+                 library_dirs=None, runtime_library_dirs=None,
+                 export_symbols=None, debug=0, extra_preargs=None,
+                 extra_postargs=None, build_temp=None, target_lang=None):
+
+            builder.run()
+            orig_link(self, target_desc, objects,
+                      output_filename, output_dir, libraries,
+                      library_dirs, runtime_library_dirs,
+                      export_symbols, debug, extra_preargs,
+                      extra_postargs, build_temp, target_lang)
+
+        with patch(distutils.unixccompiler.UnixCCompiler, '_compile', _compile):
+            with patch(distutils.unixccompiler.UnixCCompiler, 'link', link):
+                with patch(self, 'force', True):
+                    self._build_default(ext)
