8711e549c0266e9932d3ab4ff4744ed99e04ef33
Lu Fang <lufang@fb.com>
2017-12-04 14:39:50 -0800

    Fix the symbolic for view

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -206,7 +206,7 @@ def permute(g, self, dims):
 
 
 def view(g, self, size):
-    if self.type().sizes()[0] == size[0]:
+    if self.type().sizes()[0] == size[0] and len(size) == 2:
         return g.op("Flatten", self, axis_i=1)
     return g.op("Reshape", self, shape_i=size)
 
