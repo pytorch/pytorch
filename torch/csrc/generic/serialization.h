2addfbf399fae7b2e7c1ae44a9e5dbf125bcc2a4
Sam Gross <sgross@fb.com>
2017-12-04 15:19:19 -0800

    Delete _write_metadata and move _new_with_metadata_file into Python
    This will make it easier to merge Variable and Tensor

--- torch/csrc/generic/serialization.h
+++ torch/csrc/generic/serialization.h
@@ -2,8 +2,6 @@
 #define TH_GENERIC_FILE "generic/serialization.h"
 #else
 
-void THPTensor_(writeMetadataRaw)(THTensor *self, int fd);
-THTensor * THPTensor_(newWithMetadataFileRaw)(int fd, THStorage *storage);
 void THPStorage_(writeFileRaw)(THStorage *self, int fd);
 THStorage * THPStorage_(readFileRaw)(int fd, THStorage *storage);
 
