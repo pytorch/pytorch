a11fac76cf20dd041e3dab46747bab4ca33b3741
Sam Gross <sgross@fb.com>
2017-12-04 13:50:01 -0800

    kByte maps to NPY_UINT8

--- torch/csrc/autograd/python_variable_numpy.cpp
+++ torch/csrc/autograd/python_variable_numpy.cpp
@@ -93,7 +93,7 @@ static int numpy_dtype(const at::Type& type) {
       case kLong: return NPY_INT64;
       case kInt: return NPY_INT32;
       case kShort: return NPY_INT16;
-      case kByte: return NPY_INT8;
+      case kByte: return NPY_UINT8;
       default: break;
     }
   }
