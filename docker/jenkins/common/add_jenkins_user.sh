9d1c24afc6245f5e42009108a2e105b35bef0e3a
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-08 11:48:36 -0800

    Parameterize jenkins user uid/gid in docker build
    Summary: Closes https://github.com/caffe2/caffe2/pull/1587
    
    Differential Revision: D6521818
    
    Pulled By: pietern
    
    fbshipit-source-id: 6336e6af917b9f71abb63dd4b78009dad26890ca

--- docker/jenkins/common/add_jenkins_user.sh
+++ docker/jenkins/common/add_jenkins_user.sh
@@ -2,13 +2,18 @@
 
 set -ex
 
+JENKINS_UID=$1
+JENKINS_GID=$2
+
 # Mirror jenkins user in container
-echo "jenkins:x:1001:1001::/var/lib/jenkins:" >> /etc/passwd
-echo "jenkins:x:1001:" >> /etc/group
+echo "jenkins:x:$JENKINS_UID:$JENKINS_GID::/var/lib/jenkins:" >> /etc/passwd
+echo "jenkins:x:$JENKINS_GID:" >> /etc/group
 
 # Create $HOME
 mkdir -p /var/lib/jenkins
 chown jenkins:jenkins /var/lib/jenkins
+mkdir -p /var/lib/jenkins/.ccache
+chown jenkins:jenkins /var/lib/jenkins/.ccache
 
 # Allow writing to /usr/local (for make install)
 chown jenkins:jenkins /usr/local
