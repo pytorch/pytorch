d4b8bc69c8817fe7f8f7651894516738eab299a6
Neeraj Pradhan <npradhan@uber.com>
2017-12-13 00:36:07 -0800

    indent docstring

--- torch/distributions/utils.py
+++ torch/distributions/utils.py
@@ -31,18 +31,21 @@ def broadcast_all(*values):
     """
     Given a list of values (possibly containing numbers), returns a list where each
     value is broadcasted based on the following rules:
-      - `torch.Tensor` and `torch.autograd.Variable` instances are broadcasted as per
-        the `broadcasting rules <http://pytorch.org/docs/master/notes/broadcasting.html>`_
-      - numbers.Number instances (scalars) are upcast to Tensor/Variable having the same
-        size and type as the first tensor passed to `values`. If all the values are scalars,
-        then they are upcasted to `torch.Tensor` having size `(1,)`.
+      - `torch.Tensor` and `torch.autograd.Variable` instances are broadcasted as
+        per the `broadcasting rules
+        <http://pytorch.org/docs/master/notes/broadcasting.html>`_
+      - numbers.Number instances (scalars) are upcast to Tensor/Variable having
+        the same size and type as the first tensor passed to `values`. If all the
+        values are scalars, then they are upcasted to `torch.Tensor` having size
+        `(1,)`.
 
     Args:
-        values (list of `numbers.Number`, `torch.autograd.Variable` or `torch.Tensor`)
+        values (list of `numbers.Number`, `torch.autograd.Variable` or
+        `torch.Tensor`)
 
     Raises:
-        ValueError: if any of the values is not a `numbers.Number`, `torch.Tensor` or
-            `torch.autograd.Variable` instance
+        ValueError: if any of the values is not a `numbers.Number`, `torch.Tensor`
+            or `torch.autograd.Variable` instance
     """
     scalars = [(idx, v) for idx, v in enumerate(values) if isinstance(v, Number)]
     tensors = [(idx, v) for idx, v in enumerate(values) if isinstance(v, (torch.Tensor, torch.autograd.Variable))]
