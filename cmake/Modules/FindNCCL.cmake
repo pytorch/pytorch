54342287fed769021543c0197a836eec94dd351d
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-12 21:41:18 -0800

    Look for NCCL in CUDA_TOOLKIT_ROOT_DIR
    Summary: Closes https://github.com/caffe2/caffe2/pull/1611
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D6550168
    
    Pulled By: pietern
    
    fbshipit-source-id: e034ce4057d37bfc8b53949c56cbcb701ea5d958

--- cmake/Modules/FindNCCL.cmake
+++ cmake/Modules/FindNCCL.cmake
@@ -9,6 +9,10 @@
 #  NCCL_FOUND
 #  NCCL_INCLUDE_DIRS
 #  NCCL_LIBRARIES
+#
+# The path hints include CUDA_TOOLKIT_ROOT_DIR seeing as some folks
+# install NCCL in the same location as the CUDA toolkit.
+# See https://github.com/caffe2/caffe2/issues/1601
 
 set(NCCL_ROOT_DIR "" CACHE PATH "Folder contains NVIDIA NCCL")
 
@@ -17,7 +21,8 @@ find_path(NCCL_INCLUDE_DIRS
   HINTS
   ${NCCL_INCLUDE_DIR}
   ${NCCL_ROOT_DIR}
-  ${NCCL_ROOT_DIR}/include)
+  ${NCCL_ROOT_DIR}/include
+  ${CUDA_TOOLKIT_ROOT_DIR}/include)
 
 find_library(NCCL_LIBRARIES
   NAMES nccl
@@ -26,7 +31,8 @@ find_library(NCCL_LIBRARIES
   ${NCCL_ROOT_DIR}
   ${NCCL_ROOT_DIR}/lib
   ${NCCL_ROOT_DIR}/lib/x86_64-linux-gnu
-  ${NCCL_ROOT_DIR}/lib64)
+  ${NCCL_ROOT_DIR}/lib64
+  ${CUDA_TOOLKIT_ROOT_DIR}/lib64)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NCCL DEFAULT_MSG NCCL_INCLUDE_DIRS NCCL_LIBRARIES)
