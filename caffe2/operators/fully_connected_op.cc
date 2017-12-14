948094dcad58fb89399a2e69b6c4ced49870f5b7
Yangqing Jia <jiayq84@gmail.com>
2017-12-14 12:25:49 -0800

    Simple fix for windows
    Summary:
    TSIA
    Closes https://github.com/caffe2/caffe2/pull/1620
    
    Reviewed By: pietern
    
    Differential Revision: D6566180
    
    Pulled By: Yangqing
    
    fbshipit-source-id: 904e8f43831fc2a4c1f7c475d1f839ab4b7d250c

--- caffe2/operators/fully_connected_op.cc
+++ caffe2/operators/fully_connected_op.cc
@@ -128,25 +128,22 @@ OPERATOR_SCHEMA(FCTransposedGradient).NumInputs(3).NumOutputs(2, 3);
 
 namespace {
 
-template <const char* GradientOp>
 class GetFCGradient : public GradientMakerBase {
   using GradientMakerBase::GradientMakerBase;
 
   std::vector<OperatorDef> GetGradientDefs() override {
     CAFFE_ENFORCE_EQ(def_.input_size(), 3);
+    CAFFE_ENFORCE(def_.type() == "FC" || def_.type() == "FCTransposed");
     return SingleGradientDef(
-        GradientOp,
+        def_.type() + "Gradient",
         "",
         vector<string>{I(0), I(1), GO(0)},
         vector<string>{GI(1), GI(2), GI(0)});
   }
 };
 
-constexpr char kFCGradient[] = "FCGradient";
-constexpr char kFCTransposedGradient[] = "FCTransposedGradient";
-
-REGISTER_GRADIENT(FC, GetFCGradient<kFCGradient>);
-REGISTER_GRADIENT(FCTransposed, GetFCGradient<kFCTransposedGradient>);
+REGISTER_GRADIENT(FC, GetFCGradient);
+REGISTER_GRADIENT(FCTransposed, GetFCGradient);
 
 } // namespace
 
