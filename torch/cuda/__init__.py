2fe686c198add84dca49c53ef141481a8f443421
Edward Z. Yang <ezyang@mit.edu>
2017-12-14 17:48:05 -0500

    Add function to explicitly initialize PyTorch CUDA state. (#4180)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -122,6 +122,19 @@ class DeferredCudaCallError(Exception):
     pass
 
 
+def init():
+    """Initialize PyTorch's CUDA state.  You may need to call
+    this explicitly if you are interacting with PyTorch via
+    its C API, as Python bindings for CUDA functionality will not
+    be until this initialization takes place.  Ordinary users
+    should not need this, as all of PyTorch's CUDA methods
+    automatically initialize CUDA state on-demand.
+
+    Does nothing if the CUDA state is already initialized.
+    """
+    _lazy_init()
+
+
 def _lazy_init():
     global _initialized, _cudart, _original_pid, _queued_calls
     if _initialized:
