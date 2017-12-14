f3d0e97ae579f4012c751379bef6e0ae025bde63
Alican Bozkurt <alican@ece.neu.edu>
2017-12-14 11:30:32 -0500

    DeprecationWarning->UserWarning

--- torch/distributions/distribution.py
+++ torch/distributions/distribution.py
@@ -32,7 +32,7 @@ class Distribution(object):
         Generates n samples or n batches of samples if the distribution
         parameters are batched.
         """
-        warnings.warn('sample_n will be deprecated. Use .sample((n,)) instead', DeprecationWarning)
+        warnings.warn('sample_n will be deprecated. Use .sample((n,)) instead', UserWarning)
         return self.sample((n,))
 
     def log_prob(self, value):
