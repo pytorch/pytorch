6552ea110f05c7271fc708571cbb87a5351ccdc8
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-15 09:41:07 -0800

    Make timing based test more likely to pass
    Summary:
    This assumed that the expect statement would run within 1us, whereas
    we only care it runs in less than the 100ms to check that it got reset.
    Closes https://github.com/caffe2/caffe2/pull/1606
    
    Reviewed By: Yangqing
    
    Differential Revision: D6572951
    
    Pulled By: pietern
    
    fbshipit-source-id: fd0c2854bc6459c8bf0e17fa75035eb0a4e522cd

--- caffe2/core/timer_test.cc
+++ caffe2/core/timer_test.cc
@@ -26,21 +26,26 @@ namespace {
 
 TEST(TimerTest, Test) {
   Timer timer;
+
   // A timer auto-starts when it is constructed.
+  std::this_thread::sleep_for(std::chrono::microseconds(1));
   EXPECT_GT(timer.NanoSeconds(), 0);
+
   // Sleep for a while, and get the time.
   timer.Start();
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
   float ns = timer.NanoSeconds();
   float us = timer.MicroSeconds();
   float ms = timer.MilliSeconds();
+
   // Time should be at least accurate +- 10%.
   EXPECT_NEAR(ns, 100000000, 10000000);
   EXPECT_NEAR(us, 100000, 10000);
   EXPECT_NEAR(ms, 100, 10);
+
   // Test restarting the clock.
   timer.Start();
-  EXPECT_LT(timer.NanoSeconds(), 1000);
+  EXPECT_LT(timer.MicroSeconds(), 1000);
 }
 
 TEST(TimerTest, TestLatency) {
