ba914cea360f1bd18c1da2086370c7d7df5e0bf9
Neeraj Pradhan <npradhan@uber.com>
2017-12-15 00:50:04 -0800

    check event shape too

--- torch/distributions/distribution.py
+++ torch/distributions/distribution.py
@@ -97,6 +97,6 @@ class Distribution(object):
         if not (torch.is_tensor(value) or isinstance(value, Variable)):
             raise ValueError('The value argument to log_prob must be a Tensor or Variable instance.')
         batch_dim_start = len(value.size()) - len(self._batch_shape) - len(self._event_shape)
-        if value.size()[batch_dim_start:] != self._batch_shape:
+        if value.size()[batch_dim_start:] != self._batch_shape + self._event_shape:
             raise ValueError('The right-most size of value must match: {}.'.
                              format(self._batch_shape + self._event_shape))
