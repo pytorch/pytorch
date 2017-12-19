293a56ae02ab9ec1ca4cd972f0bd564a255c3683
Edward Z. Yang <ezyang@fb.com>
2017-12-19 07:58:55 -0800

    Minimize the test some more.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/test_jit.py
+++ test/test_jit.py
@@ -960,10 +960,13 @@ class TestJit(TestCase):
         self.assertTrue("stage 1" in info_str)
 
     @unittest.expectedFailure
-    def test_index_trace(self):
+    def test_inplace_copy(self):
         x = Variable(torch.randn(4, 4), requires_grad=True)
-        trace, z = torch.jit.trace(lambda x: x[0], (x, ), nderivs=1)
-        z.sum().backward()
+        def f(x):
+            out = Variable(torch.zeros(x.size()))
+            out.copy_(x)
+            return out
+        trace, z = torch.jit.trace(f, (x, ), nderivs=0)
         torch._C._jit_pass_lint(trace)
         torch._C._jit_pass_dce(trace)
         self.assertExpectedTrace(trace)
