552544d70a515bd62a47e8e6d96ab19ca74af45b
Alican Bozkurt <alican@ece.neu.edu>
2017-12-20 17:52:29 -0500

    fix laplace inf sampling

--- torch/distributions/laplace.py
+++ torch/distributions/laplace.py
@@ -31,8 +31,11 @@ class Laplace(Distribution):
 
     def rsample(self, sample_shape=torch.Size()):
         shape = self._extended_shape(sample_shape)
-        u = self.loc.new(*shape).uniform_() - .5
-        return self.loc - self.scale * u.sign() * torch.log(1 - 2 * u.abs())
+        u = self.loc.new(*shape).uniform_(-.5, .5)
+        u[u == -0.5] = 0
+        # TODO: If we ever implement tensor.nextafter, below is what we want ideally.
+        # u = self.loc.new(*shape).uniform_(self.loc.nextafter(-.5, 0), .5)
+        return self.loc - self.scale * u.sign() * torch.log1p(-2 * u.abs())
 
     def log_prob(self, value):
         self._validate_log_prob_arg(value)
