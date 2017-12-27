Numerical fixes for single-precision standard_gamma_grad()

test/test_distributions.py
@@ -456,8 +456,8 @@ class TestDistributions(TestCase):
         self._set_rng_seed(1)
         num_samples = 100
         for alpha in [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4]:
-            alphas = Variable(torch.Tensor([alpha] * num_samples), requires_grad=True)
-            betas = Variable(torch.ones(num_samples))
+            alphas = Variable(torch.FloatTensor([alpha] * num_samples), requires_grad=True)
+            betas = Variable(torch.ones(num_samples).type_as(alphas))
             x = Gamma(alphas, betas).rsample()
             x.sum().backward()
             x, ind = x.data.sort()
@@ -470,14 +470,15 @@ class TestDistributions(TestCase):
             cdf_alpha = (cdf(x, alpha + eps) - cdf(x, alpha - eps)) / (2 * eps)
             cdf_x = pdf(x, alpha)
             expected_grad = -cdf_alpha / cdf_x
-            rel_error = np.abs(actual_grad - expected_grad) / (expected_grad + 1e-100)
+            rel_error = np.abs(actual_grad - expected_grad) / (expected_grad + 1e-30)
             self.assertLess(np.max(rel_error), 0.0005,
                             '\n'.join(['Bad gradients for Gamma({}, 1)'.format(alpha),
                                        'x {}'.format(x),
                                        'expected {}'.format(expected_grad),
                                        'actual {}'.format(actual_grad),
                                        'rel error {}'.format(rel_error),
-                                       'max error {}'.format(rel_error.max())]))
+                                       'max error {}'.format(rel_error.max()),
+                                       'at alpha={}, x={}'.format(alpha, x[rel_error.argmax()])]))
 
     def test_dirichlet_shape(self):
         alpha = Variable(torch.exp(torch.randn(2, 3)), requires_grad=True)