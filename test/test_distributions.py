f99cc78b77473b8bf700ff9791624f5373804958
Fritz Obermeyer <fritzo@uber.com>
2017-12-27 13:10:22 -0800

    Improve precision of standard_gamma_grad()

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -466,12 +466,12 @@ class TestDistributions(TestCase):
             # Compare with expected gradient dx/dalpha along constant cdf(x,alpha).
             cdf = scipy.stats.gamma.cdf
             pdf = scipy.stats.gamma.pdf
-            eps = 0.02 * alpha if alpha < 100 else 0.02 * alpha ** 0.5
+            eps = 0.01 * alpha / (1.0 + alpha ** 0.5)
             cdf_alpha = (cdf(x, alpha + eps) - cdf(x, alpha - eps)) / (2 * eps)
             cdf_x = pdf(x, alpha)
             expected_grad = -cdf_alpha / cdf_x
             rel_error = np.abs(actual_grad - expected_grad) / (expected_grad + 1e-100)
-            self.assertLess(np.max(rel_error), 0.005,
+            self.assertLess(np.max(rel_error), 0.0005,
                             '\n'.join(['Bad gradients for Gamma({}, 1)'.format(alpha),
                                        'x {}'.format(x),
                                        'expected {}'.format(expected_grad),
