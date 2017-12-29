6185b27cc6645d8055b76f9cc330b010d1c2a258
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-12-29 03:11:04 -0800

    Improve precision of standard_gamma_grad() (#4369)

--- aten/src/ATen/native/Distributions.cpp
+++ aten/src/ATen/native/Distributions.cpp
@@ -21,44 +21,65 @@ Tensor& bernoulli_(Tensor& self, double p, Generator* generator) {
 // TODO Replace this with more accurate digamma().
 template <typename scalar>
 static inline scalar digamma_one(scalar x) {
-  const scalar eps = x * 1e-2;
+  const double eps = x * 1e-3;
   return (std::lgamma(x + eps) - std::lgamma(x - eps)) / (eps + eps);
 }
 
-/** Computes the reparameterized gradient -(d/dalpha cdf(x;alpha)) / pdf(x;alpha)
-    for random number x drawn from a standard Gamma distribution Gamma(alpha).
-*/
+// Computes the reparameterized gradient -(d/dalpha cdf(x;alpha)) / pdf(x;alpha)
+// for random number x drawn from a standard Gamma distribution Gamma(alpha).
 template <typename scalar>
 static inline scalar standard_gamma_grad_one(scalar alpha, scalar x) {
-  // Use an asymptotic approximation for small x.
-  if (x < 0.2f) {
-    const auto a0 = 1 / alpha;
-    const auto a1 = 1 / (alpha + 1);
-    const auto a2 = 1 / (alpha + 2);
+  // Use a Taylor series expansion for small x.
+  if (x < 0.8f) {
+    scalar numer = 1;
+    scalar denom = alpha;
+    auto series1 = numer / denom;
+    auto series2 = numer / (denom * denom);
+    for (int i = 1; i <= 5; ++i) {
+      numer *= -x / i;
+      denom += 1;
+      series1 += numer / denom;
+      series2 += numer / (denom * denom);
+    }
     const auto pow_x_alpha = std::pow(x, alpha);
     const auto gamma_pdf = std::pow(x, alpha - 1) * std::exp(-x);
-    const auto gamma_cdf = pow_x_alpha * (a0 - x*a1 + 0.5f*x*x*a2);
+    const auto gamma_cdf = pow_x_alpha * series1;
     const auto gamma_cdf_alpha = (std::log(x) - digamma_one(alpha)) * gamma_cdf
-        - pow_x_alpha * (a0*a0 - x*a1*a1 + 0.5f*x*x*a2*a2);
+        - pow_x_alpha * series2;
     const auto result = -gamma_cdf_alpha / gamma_pdf;
     return std::isnan(result) ? 0 : result;
   }
 
-  // Use an asymptotic approximation for large alpha.
-  if (alpha > 50.0f) {
-    return std::sqrt(x / alpha);
+  // Use a Rice saddle point expansion for large alpha.
+  if (alpha > 8.0f) {
+    if (0.9f * alpha <= x && x <= 1.1f * alpha) {
+      const auto numer_1 = 1 + 24 * alpha * (1 + 12 * alpha);
+      const auto numer_2 = 1440 * (alpha * alpha) + 6 * x * (53 - 120 * x)
+          - 65 * x * x / alpha + alpha * (107 + 3600 * x);
+      const auto denom = 1244160 * (alpha * alpha) * (alpha * alpha);
+      return numer_1 * numer_2 / denom;
+    }
+    const auto denom = std::sqrt(8 * alpha);
+    const auto term2 = denom / (alpha - x);
+    const auto term3 = std::pow(x - alpha - alpha * std::log(x / alpha), -1.5f);
+    const auto term23 = (x < alpha) ? term2 - term3 : term2 + term3;
+    const auto term1 = std::log(x / alpha) * term23
+                     - std::sqrt(2 / alpha) * (alpha + x) / ((alpha - x) * (alpha - x));
+    const auto stirling = 1 + 1 / (12 * alpha) * (1 + 1 / (24 * alpha));
+    const auto numer = x * term1;
+    return -stirling * numer / denom;
   }
 
   // Use a bivariate rational approximation to the reparameterized gradient.
   const auto u = std::log(x / alpha);
   const auto v = std::log(alpha);
   static const scalar coef_uv[3][8] = {
-    {0.16028008, -0.088064309, 0.019630876, -0.0016920282,
-     1.0, 0.36659853, 0.10843863, 0.0066895454},
-    {0.521894, 0.16095838, 0.06237597, 0.0023884253,
-     0.083457714, 0.0073297628, -0.0059299053, -0.00093720389},
-    {-0.0031143957, -0.012143877, -0.0057656484, -0.00064847254,
-     0.0087262576, -0.00022820524, 1.8871047e-05, 9.6307964e-06},
+    {0.16009398, -0.094634809, 0.025146376, -0.0030648343,
+     1, 0.32668115, 0.10406089, 0.0014179084},
+    {0.53487893, 0.1298071, 0.065735949, -0.0015649758,
+     0.16639465, 0.020070113, -0.0035938915, -0.00058392623},
+    {0.040121004, -0.0065914022, -0.0026286047, -0.0013441777,
+     0.017050642, -0.0021309326, 0.00085092367, -1.5247877e-07},
   };
   scalar coef_v[8];
   for (int i = 0; i < 8; ++ i) {
