e07c4bf9e9cd40287188ba19125188bc97b884e4
Fritz Obermeyer <fritzo@uber.com>
2017-12-31 13:35:12 -0800

    Increase order of rational polynomial approximating dirichlet_grad

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -567,7 +567,7 @@ class TestDistributions(TestCase):
             cdf_x = pdf(x, alpha, beta)
             expected_grad = -cdf_alpha / cdf_x
             rel_error = np.abs(actual_grad - expected_grad) / (expected_grad + 1e-100)
-            self.assertLess(np.max(rel_error), 0.002,
+            self.assertLess(np.max(rel_error), 0.001,
                             '\n'.join(['Bad gradients for Beta({}, {})'.format(alpha, beta),
                                        'x {}'.format(x),
                                        'expected {}'.format(expected_grad),
