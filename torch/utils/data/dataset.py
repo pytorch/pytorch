f6df5c8293006425ef05c4e675ca9a4a63611459
Alykhan Tejani <alykhan.tejani@gmail.com>
2018-01-02 16:07:11 +0000

    flake8

--- torch/utils/data/dataset.py
+++ torch/utils/data/dataset.py
@@ -115,5 +115,4 @@ def random_split(dataset, lengths):
         raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
 
     indices = randperm(sum(lengths))
-    return [Subset(dataset, indices[offset - length:offset])
-        for offset, length in zip(_accumulate(lengths), lengths)]
+    return [Subset(dataset, indices[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
