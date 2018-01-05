2bfbd859594dc86f9c90e1a15cf61377d4be1813
Edward Z. Yang <ezyang@fb.com>
2018-01-05 14:28:21 -0800

    Fix #4492, make it impossible to forget to reset cudnn flags
    Three stage plan to no more stupidly weird "why isn't cuDNN enabled"
    bugs:
    
    - Add torch.backends.cudnn.disable_global_flags(), which as its name suggests,
      disables global flag setting in cuDNN, so that you are not allowed to
      make changes to this state.  However, the flags() context
      manager continues to work (since they are non-global changes).
    
    - Call disable_global_flags() in test/common.py
    
    - Switch all of the manual flag setting/unsetting in test/test_nn.py
      to use the context manager.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/backends/cudnn/__init__.py
+++ torch/backends/cudnn/__init__.py
@@ -18,6 +18,13 @@ __cudnn_version = None
 # TODO: dynamic version checks via cudnnGetVersion
 
 
+# The idea for this parameter is that we forbid bare assignment
+# to torch.backends.cudnn.enabled and friends when running our
+# test suite, where it's very easy to forget to undo the change
+# later.
+__allow_nonbracketed_mutation_flag = True
+
+
 def find_cudnn_windows_lib():
     proc = Popen(['where', 'cudnn64*.dll'], stdout=PIPE, stderr=PIPE)
     out, err = proc.communicate()
@@ -122,14 +129,36 @@ def set_flags(_enabled, _benchmark, _deterministic, _verbose):
     return orig_flags
 
 
+def disable_global_flags():
+    global __allow_nonbracketed_mutation_flag
+    __allow_nonbracketed_mutation_flag = False
+
+
+def flags_frozen():
+    return not __allow_nonbracketed_mutation_flag
+
+
+@contextmanager
+def __allow_nonbracketed_mutation():
+    global __allow_nonbracketed_mutation_flag
+    old = __allow_nonbracketed_mutation_flag
+    __allow_nonbracketed_mutation_flag = True
+    try:
+        yield
+    finally:
+        __allow_nonbracketed_mutation_flag = old
+
+
 @contextmanager
 def flags(enabled=False, benchmark=False, deterministic=False, verbose=False):
-    orig_flags = set_flags(enabled, benchmark, deterministic, verbose)
+    with __allow_nonbracketed_mutation():
+        orig_flags = set_flags(enabled, benchmark, deterministic, verbose)
     try:
         yield
     finally:
         # recover the previous values
-        set_flags(orig_flags[0], orig_flags[1], orig_flags[2], orig_flags[3])
+        with __allow_nonbracketed_mutation():
+            set_flags(orig_flags[0], orig_flags[1], orig_flags[2], orig_flags[3])
 
 
 class CuDNNHandle:
@@ -397,7 +426,11 @@ class ContextProp(object):
         return self.getter()
 
     def __set__(self, obj, val):
-        self.setter(val)
+        if not flags_frozen():
+            self.setter(val)
+        else:
+            raise RuntimeError("not allowed to set torch.backends.cudnn flags "
+                               "after disable_global_flags; please use flags() context manager instead")
 
 
 class CudnnModule(object):
