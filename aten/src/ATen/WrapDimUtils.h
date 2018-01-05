02407970df038beb2c554988b206ffd379cdfa4c
Gregory Chanan <gchanan@fb.com>
2018-01-05 15:11:55 -0800

    [ATen] Support wrapping dimensions over scalars.
    This follows the behavior of numpy in that you can wrap dimensions over a scalar (0-dimensional
    tensor) in the range [-1, 0].  I.e. scalarTensor.prod(0) and scalarTensor.prod(-1) works, but
    scalarTensor.prod(2) does not.
    
    The only current exception to this is with size(dim) and stride(dim);
    there are no numpy equivalents of these (they are attributes), so it seems cleaner to just have
    these as (dimensional wrapping) sugar for sizes()[dim] and strides()[dim]; otherwise there are
    subtle differences in semantics, e.g. you have to use size(dim) when you want it to directly
    apply to scalars, if the default value (1?) makes sense in that case.  Simpler to just not have
    that difference.
    
    Note that this change can cause problems if code assumed that maybe_wrap_dim would throw an
    exception in this case and then called sizes()[dim] or size(dim) without checking; I went
    through the code and only found this case in squeeze/squeeze_.

--- aten/src/ATen/WrapDimUtils.h
+++ aten/src/ATen/WrapDimUtils.h
@@ -5,16 +5,22 @@
 
 namespace at {
 
-static inline int64_t maybe_wrap_dim(int64_t dim, int64_t dim_post_expr) {
+static inline int64_t maybe_wrap_dim(int64_t dim, int64_t dim_post_expr, bool wrap_scalar=true) {
   if (dim_post_expr <= 0) {
-    std::ostringstream oss;
-    oss << "dimension specified as " << dim << " but tensor has no dimensions";
-    throw std::runtime_error(oss.str());
+    if (!wrap_scalar) {
+      std::ostringstream oss;
+      oss << "dimension specified as " << dim << " but tensor has no dimensions";
+      throw std::runtime_error(oss.str());
+    }
+    dim_post_expr = 1; // this will make range [-1, 0]
   }
-  if (dim < -(dim_post_expr) || dim >= (dim_post_expr)) {
+
+  int64_t min = -dim_post_expr;
+  int64_t max = dim_post_expr - 1;
+  if (dim < min || dim > max) {
     std::ostringstream oss;
-    oss << "dimension out of range (expected to be in range of [" << -(dim_post_expr)
-        << ", " << (dim_post_expr)-1 << "], but got " << dim << ")",
+    oss << "dimension out of range (expected to be in range of [" << min
+        << ", " << max << "], but got " << dim << ")",
     throw std::runtime_error(oss.str());
   }
   if (dim < 0) dim += dim_post_expr;
