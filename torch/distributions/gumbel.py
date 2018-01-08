2c8019531ca852b19dcf1e518cabde9b01f3528c
vishwakftw <cs15btech11043@iith.ac.in>
2018-01-08 22:35:35 +0530

    fix nits and remove unnecessary import

--- torch/distributions/gumbel.py
+++ torch/distributions/gumbel.py
@@ -1,8 +1,5 @@
 from numbers import Number
 
-import math
-from numpy import euler_gamma
-
 import torch
 from torch.distributions import constraints
 from torch.distributions.distribution import Distribution
@@ -49,4 +46,4 @@ class Gumbel(Distribution):
         return -(self.scale.log() + z + torch.exp(-z))
 
     def entropy(self):
-        return self.scale.log() + 1 + euler_gamma
+        return self.scale.log() + (1.57721566490153286060)  # 1 + Euler Mascheroni Constant
