f99c7d9429fc6155d4efff7a966c4995f9f78ace
Riddhiman Dasgupta <subhoman_sp07@yahoo.co.in>
2018-01-09 16:34:11 +0530

    Padding_idx in Embedding supports negative indexing (#4496)

--- torch/nn/modules/sparse.py
+++ torch/nn/modules/sparse.py
@@ -79,6 +79,12 @@ class Embedding(Module):
         super(Embedding, self).__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
+        if padding_idx is not None:
+            if padding_idx > 0:
+                assert padding_idx < self.num_embeddings, 'Padding_idx must be within num_embeddings'
+            elif padding_idx < 0:
+                assert padding_idx >= -self.num_embeddings, 'Padding_idx must be within num_embeddings'
+                padding_idx = self.num_embeddings + padding_idx
         self.padding_idx = padding_idx
         self.max_norm = max_norm
         self.norm_type = norm_type
