fd966815a5ad5a01070a97309b8e4f317136198d
Alican Bozkurt <alican@ece.neu.edu>
2018-01-09 11:10:46 -0500

    use scatter_add_ instead of stack in .sample

--- torch/distributions/multinomial.py
+++ torch/distributions/multinomial.py
@@ -1,5 +1,6 @@
 import torch
 from torch.distributions.distribution import Distribution
+from torch.autograd import Variable
 from torch.distributions import Categorical
 from numbers import Number
 from torch.distributions import constraints
@@ -9,31 +10,35 @@ from torch.distributions.utils import log_sum_exp
 class Multinomial(Distribution):
     r"""
     Creates a Multinomial distribution parameterized by `probs` and `total_count`.
-
-    If `probs` is 1D with length-`K`, each element is the relative probability
-    of sampling the class at that index.
-
-    If `probs` is 2D, it is treated as a batch of probability vectors.
+    The innermost dimension of `probs` indexes over categories. All other dimensions index over batches.
+    - `total_count` need not be specified if only .log_prob() is called (see example below)
+    - .sample() requires a single shared `total_count` for all parameters and samples
+    - .log_prob() allows different total_count for each parameter and sample.
 
     Example::
 
         >>> m = Multinomial(10, torch.Tensor([ 1, 1, 1, 1]))
-        >>> m.sample()  # equal probability of 0, 1, 2, 3
-         2
-         2
+        >>> x= m.sample()  # equal probability of 0, 1, 2, 3
+         1
+         3
          3
          3
         [torch.FloatTensor of size 4]
+
+        >>> Multinomial(probs=torch.Tensor([1, 1, 1, 1])).log_prob(x)
+        -4.1338
+        [torch.FloatTensor of size 1]
 
 
     Args:
         total_count (Tensor or Variable): number of trials
         probs (Tensor or Variable): event probabilities
+        logits (Tensor or Variable): event log probabilities
     """
 
     params = {'total_count': constraints.nonnegative_integer, 'probs': constraints.simplex}
 
-    def __init__(self, total_count, probs=None, logits=None):
+    def __init__(self, total_count=1, probs=None, logits=None):
         self.total_count = total_count if isinstance(total_count, Number) else total_count.data[0]
         if (probs is None) == (logits is None):
             raise ValueError("Either `probs` or `logits` must be specified, but not both.")
@@ -60,28 +65,19 @@ class Multinomial(Distribution):
 
     def sample(self, sample_shape=torch.Size()):
         sample_shape = torch.Size(sample_shape)
-        num_events = self._event_shape[0]
-        extended_shape = torch.Size((1,)) if not sample_shape else sample_shape
-        samples = self._categorical.sample((self.total_count,) + extended_shape).float()
-        countsList = [torch.histc(m, bins=num_events, min=0, max=num_events - 1)
-                      for m in torch.unbind(samples.view(self.total_count, -1), dim=-1)]
-        counts = torch.stack(countsList, dim=0)
-        if isinstance(counts, torch.autograd.Variable):
-            counts = counts.detach()
-        if (isinstance(self.logits, torch.DoubleTensor) or
-            (isinstance(self.logits, torch.autograd.Variable) and
-             isinstance(self.logits.data, (torch.DoubleTensor, torch.cuda.DoubleTensor)))):
-            counts = counts.double()
-        return counts.contiguous().view(self._extended_shape(sample_shape))
+        samples = self._categorical.sample(torch.Size((self.total_count,))+sample_shape)
+        # samples.shape is (total_count, sample_shape, batch_shape), need to change it to
+        # (sample_shape, batch_shape, total_count)
+        shifted_idx = list(range(samples.dim()))
+        shifted_idx.append(shifted_idx.pop(0))
+        samples = samples.permute(*shifted_idx)
+        counts = samples.new(self._extended_shape(sample_shape)).zero_()
+        counts.scatter_add_(-1, samples, torch.ones_like(samples))
+        return counts.type_as(self.probs)
 
     def log_prob(self, value):
         self._validate_log_prob_arg(value)
-        batch_log_pdf_shape = torch.Size((1,)) if not value.size()[:-1] else value.size()[:-1]
         log_factorial_n = torch.lgamma(value.sum(-1) + 1)
         log_factorial_xs = torch.lgamma(value + 1).sum(-1)
         log_powers = (self.logits[value != 0] * value[value != 0]).sum(-1)
-        batch_log_pdf = log_factorial_n - log_factorial_xs + log_powers
-        return batch_log_pdf.contiguous().view(batch_log_pdf_shape)
-
-    def entropy(self):
-        raise NotImplementedError
+        return log_factorial_n - log_factorial_xs + log_powers
