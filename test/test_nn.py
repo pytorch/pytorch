a20ac05c8b18ff0f07a46bc8f9002baa3d7a17c1
Justus Schwabedal <jusjusjus@users.noreply.github.com>
2018-01-10 21:42:37 +0100

    Added method cuda to PackedSequence. (#4430)

--- test/test_nn.py
+++ test/test_nn.py
@@ -36,6 +36,65 @@ if TEST_SCIPY:
     from scipy import stats
 
 
+class PackedSequenceTest(TestCase):
+
+    _type_by_name = {
+        'torch.DoubleTensor': (torch.DoubleTensor, 'double'),
+        'torch.FloatTensor': (torch.FloatTensor, 'float'),
+        # We leave out `'torch.HalfTensor': (torch.HalfTensor, 'half'),`
+        # because of an error in `pad_packed_sequence`
+        # > AttributeError: 'torch.HalfTensor' object has no attribute 'fill_'
+        'torch.LongTensor': (torch.LongTensor, 'long'),
+        'torch.IntTensor': (torch.IntTensor, 'int'),
+        'torch.ShortTensor': (torch.ShortTensor, 'short'),
+        'torch.CharTensor': (torch.CharTensor, 'char'),
+        'torch.ByteTensor': (torch.ByteTensor, 'byte'),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(PackedSequenceTest, self).__init__(*args, **kwargs)
+        self.batch_size = 5
+        self.max_length = 6
+
+    def _ordered_sequence(self, tensor_type):
+        """Create ordered list of random sequences"""
+        seqs = [tensor_type(random.randint(1, self.max_length))
+                for _ in range(self.batch_size)]
+        seqs = [Variable(s.random_()) for s in seqs]
+        ordered = sorted(seqs, key=len, reverse=True)
+        return ordered
+
+    def _padded_sequence(self, tensor_type):
+        """Create Variable of random padded sequences"""
+        ordered = self._ordered_sequence(tensor_type)
+        lengths = list(map(len, ordered))
+        padded_tensor = rnn_utils.pad_sequence(ordered)
+        return padded_tensor, lengths
+
+    def test_type_casts(self):
+        """Test type casting of `PackedSequence` against type casting of tensor"""
+        for _, (input_type, _) in self._type_by_name.items():
+            for expected_type_str, (_, cast_str) in self._type_by_name.items():
+                padded, lengths = self._padded_sequence(input_type)
+                packed = rnn_utils.pack_padded_sequence(padded, lengths)
+                # Apply cast to `PackedSequence` instance and unpack
+                masked = getattr(packed, cast_str)()
+                unpacked, lengths_out = rnn_utils.pad_packed_sequence(masked)
+                self.assertEqual(unpacked.type(), expected_type_str)
+
+    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
+    def test_cuda_mask(self):
+        tensor_type = torch.FloatTensor
+        cuda_type_str = 'torch.cuda.FloatTensor'
+        padded, lengths = self._padded_sequence(tensor_type)
+        packed = rnn_utils.pack_padded_sequence(padded, lengths)
+        self.assertFalse(packed.is_cuda)
+        packed = packed.cuda()
+        self.assertTrue(packed.is_cuda)
+        unpacked, _ = rnn_utils.pad_packed_sequence(packed)
+        self.assertEqual(unpacked.type(), cuda_type_str)
+
+
 def default_tensor_type(type):
     type_str = torch.typename(type)
 
--- torch/nn/utils/rnn.py
+++ torch/nn/utils/rnn.py
@@ -28,7 +28,56 @@ class PackedSequence(PackedSequence_):
         batch_sizes (list[int]): list of integers holding information about
             the batch size at each sequence step
     """
-    pass
+    def cuda(self, *args, **kwargs):
+        """Returns a GPU copy if `self.data` not already on the GPU"""
+        if self.is_cuda:
+            return self
+        else:
+            return type(self)(self.data.cuda(*args, **kwargs), self.batch_sizes)
+
+    def cpu(self):
+        """Returns a CPU copy if `self.data` not already on the CPU"""
+        if self.is_cuda:
+            return type(self)(self.data.cpu(), self.batch_sizes)
+        else:
+            return self
+
+    def double(self):
+        r"""Returns copy with `self.data` cast to double type"""
+        return type(self)(self.data.double(), self.batch_sizes)
+
+    def float(self):
+        r"""Returns copy with `self.data` cast to float type"""
+        return type(self)(self.data.float(), self.batch_sizes)
+
+    def half(self):
+        r"""Returns copy with `self.data` cast to half type"""
+        return type(self)(self.data.half(), self.batch_sizes)
+
+    def long(self):
+        r"""Returns copy with `self.data` cast to long type"""
+        return type(self)(self.data.long(), self.batch_sizes)
+
+    def int(self):
+        r"""Returns copy with `self.data` cast to int type"""
+        return type(self)(self.data.int(), self.batch_sizes)
+
+    def short(self):
+        r"""Returns copy with `self.data` cast to short type"""
+        return type(self)(self.data.short(), self.batch_sizes)
+
+    def char(self):
+        r"""Returns copy with `self.data` cast to char type"""
+        return type(self)(self.data.char(), self.batch_sizes)
+
+    def byte(self):
+        r"""Returns copy with `self.data` cast to byte type"""
+        return type(self)(self.data.byte(), self.batch_sizes)
+
+    @property
+    def is_cuda(self):
+        r"""Returns true if `self.data` stored on a gpu"""
+        return self.data.is_cuda
 
 
 def pack_padded_sequence(input, lengths, batch_first=False):
@@ -63,7 +112,7 @@ def pack_padded_sequence(input, lengths, batch_first=False):
     return PackedSequence(data, list(batch_sizes.data))
 
 
-def pad_packed_sequence(sequence, batch_first=False, padding_value=0.0):
+def pad_packed_sequence(sequence, batch_first=False, padding_value=0):
     r"""Pads a packed batch of variable length sequences.
 
     It is an inverse operation to :func:`pack_padded_sequence`.
@@ -78,7 +127,7 @@ def pad_packed_sequence(sequence, batch_first=False, padding_value=0.0):
         sequence (PackedSequence): batch to pad
         batch_first (bool, optional): if ``True``, the output will be in BxTx*
             format.
-        padding_value (float, optional): values for padded elements
+        padding_value (float, optional): values for padded elements.
 
     Returns:
         Tuple of Variable containing the padded sequence, and a list of lengths
