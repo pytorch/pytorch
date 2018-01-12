e615ac64e4d77091d63a664ae01a7f88779537e8
HE, Tao <sighingnow@gmail.com>
2018-01-12 20:44:56 +0800

    [ONNX] export sum, prod, sqrt improve log_softmax. (#4579)
    * ONNX: export sum, prod, sqrt improve log_softmax and fix a typo in doc.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Add new exported op to doc.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Double quotes.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Update trace log of log_softmax.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Improve export when dim is None and axes_i should be a list of ints.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Fix prod when no dim given.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>
    
    * Update line ends in test expected file.
    
    Signed-off-by: HE, Tao <sighingnow@gmail.com>

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -170,6 +170,10 @@ def neg(g, self):
     return g.op("Neg", self)
 
 
+def sqrt(g, self):
+    return g.op("Sqrt", self)
+
+
 def tanh(g, self):
     return g.op("Tanh", self)
 
@@ -179,11 +183,30 @@ def sigmoid(g, self):
 
 
 def mean(g, self, dim=None, keepdim=None):
-    kwargs = {}
+    if dim is None and keepdim is None:
+        return g.op("Mean", self)
     # NB: ONNX's default is different from PyTorch's
     if keepdim is None:
         keepdim = 0
-    return g.op("ReduceMean", self, axes_i=dim, keepdims_i=keepdim)
+    return g.op("ReduceMean", self, axes_i=[dim], keepdims_i=keepdim)
+
+
+def sum(g, self, dim=None, keepdim=None):
+    if dim is None and keepdim is None:
+        return g.op("Sum", self)
+    if keepdim is None:
+        keepdim = 0
+    return g.op("ReduceSum", self, axes_i=[dim], keepdims_i=keepdim)
+
+
+def prod(g, self, dim=None, keepdim=None):
+    if dim is None:
+        dims = None
+    else:
+        dims = [dim]
+    if keepdim is None:
+        keepdim = 0
+    return g.op("ReduceProd", self, axes_i=dims, keepdims_i=keepdim)
 
 
 def t(g, self):
@@ -389,7 +412,7 @@ def upsample_nearest2d(g, input, scale_factor):
 
 
 def log_softmax(g, input, dim=None):
-    return g.op("Log", g.op('Softmax', input, axis_i=dim).setTypeAs(input))
+    return g.op("LogSoftmax", input, axis_i=dim)
 
 
 def _convolution(g, input, weight, bias, stride, padding, dilation,
