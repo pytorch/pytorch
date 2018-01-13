d14ef97637c51aa738d40991519c318862549861
Priya Goyal <prigoyal@devgpu172.prn2.facebook.com>
2018-01-12 16:21:47 -0800

    adding some docs for the checkpoint api

--- torch/utils/checkpoint.py
+++ torch/utils/checkpoint.py
@@ -58,10 +58,56 @@ class CheckpointFunction(Function):
 
 
 def checkpoint(run_function, *args):
+    r"""Checkpoint a model or part of the model
+
+    Checkpoint works by trading compute for memory. It can be applied on any
+    part of the model. In the forward pass, the model is run in volatile
+    manner i.e. the activations are not stored. The forward pass save the
+    inputs tuple and the run_function parameter. In the backwards pass, the
+    saved inputs and run_function is retreived, and the forward pass is done
+    on the model again (non-volatile this time) since we need to get the
+    activations values for calculating the gradient and then the gradients are
+    calculated.
+
+    Args:
+        run_function : describes what to run in the forward pass of the model or
+                       part of the model. It should also know how to handle
+                       the inputs passed as the tuple. For example, in LSTM,
+                       user passes (activation, hidden), run_function should
+                       correctly use first input as activation and second input
+                       as hidden
+        args:         tuple containing inputs to the run_function
+
+    Returns:
+        Output of running the run_function on *args
+    """
     return CheckpointFunction.apply(run_function, *args)
 
 
 def checkpoint_sequential(modules, segments, run_function, *inputs):
+    r"""A helper function for checkpointing sequential based models.
+
+    For models that are constructed using sequential, they normally are built
+    using various modules. For such models, given a list of modules it executes
+    sequentially, we can divide the model in various segments and checkpoint
+    the segments. All segments except the last will be run in volatile manner.
+    The inputs of each checkpointed segment will be saved for re-running the
+    segment in the backward pass.
+
+    Args:
+        modules: The sequence of modules (comprising the model) to run in order.
+                 Usually modules = model._modules.items()
+
+        segments: Number of times chunks to create in the model
+
+        run_function: It takes start, end point of the segment and returns a
+                      function that describes what to run in the checkpoint api
+                      on the given inputs.
+
+        inputs: tuple containing the inputs to run_function
+
+    Returns:
+    """
     segment_size = len(modules) // segments
     # the last chunk has to be non-volatile
     for start in range(0, segment_size * (segments - 1), segment_size):
