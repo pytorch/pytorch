cb7350fc8d593fb8050c807d6db309469ffae27c
Hao Lu <hlu@fb.com>
2018-01-12 21:02:15 -0800

    Add vulkanSymbolWrapperReset function
    Reviewed By: Maratyszcza
    
    Differential Revision: D6707702
    
    fbshipit-source-id: 140c4be7884a307953684a13202c668cb2c1a927

--- caffe2/mobile/contrib/libvulkan-stub/src/libvulkan-stub.c
+++ caffe2/mobile/contrib/libvulkan-stub/src/libvulkan-stub.c
@@ -2,15 +2,15 @@
 * Copyright (c) 2016-2017, ARM Limited and Contributors
 *
 * SPDX-License-Identifier: MIT
-* 
+*
 * Permission is hereby granted, free of charge,
 * to any person obtaining a copy of this software and associated documentation files (the "Software"),
 * to deal in the Software without restriction, including without limitation the rights to
 * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -216,7 +216,7 @@ VkBool32 vulkanSymbolWrapperInitLoader(void)
 
   return dylib != NULL ? VK_TRUE : VK_FALSE;
 #else
-  return VK_FALSE;
+  #error "Platform-specific implementation required"
 #endif
 }
 
@@ -229,6 +229,19 @@ void vulkanSymbolWrapperInit(PFN_vkGetInstanceProcAddr getInstanceProcAddr)
 PFN_vkGetInstanceProcAddr vulkanSymbolWrapperInstanceProcAddr(void)
 {
     return GetInstanceProcAddr;
+}
+
+void vulkanSymbolWrapperReset(void)
+{
+#ifndef _WIN32
+  if (dylib) {
+    dlclose(dylib);
+  }
+  dylib = NULL;
+#else
+  #error "Platform-specific implementation required"
+#endif
+  GetInstanceProcAddr = NULL;
 }
 
 
