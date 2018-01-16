fix Dirichlet-Dirichlet KL divergence

torch/distributions/beta.py
@@ -30,10 +30,9 @@ class Beta(Distribution):
     def __init__(self, alpha, beta):
         if isinstance(alpha, Number) and isinstance(beta, Number):
             alpha_beta = torch.Tensor([alpha, beta])
-            self.alpha, self.beta = torch.Tensor([alpha]), torch.Tensor([beta])
         else:
-            self.alpha, self.beta = broadcast_all(alpha, beta)
-            alpha_beta = torch.stack([self.alpha, self.beta], -1)
+            alpha, beta = broadcast_all(alpha, beta)
+            alpha_beta = torch.stack([alpha, beta], -1)
         self._dirichlet = Dirichlet(alpha_beta)
         super(Beta, self).__init__(self._dirichlet._batch_shape)
 

torch/distributions/kl.py
@@ -172,14 +172,14 @@ def _kl_beta_beta(p, q):
 @register_kl(Dirichlet, Dirichlet)
 def _kl_dirichlet_dirichlet(p, q):
     # From http://bariskurt.com/kullback-leibler-divergence-between-two-dirichlet-and-beta-distributions/
-    sum_p_alpha = p.alpha.sum(-1)
-    sum_q_alpha = q.alpha.sum(-1)
+    sum_p_alpha = p.alpha.sum(0)
+    sum_q_alpha = q.alpha.sum(0)
     t1 = torch.lgamma(sum_p_alpha)
     t2 = torch.lgamma(sum_q_alpha)
-    t3 = p.alpha.lgamma().sum(-1)
-    t4 = q.alpha.lgamma().sum(-1)
+    t3 = p.alpha.lgamma().sum(0)
+    t4 = q.alpha.lgamma().sum(0)
     t5 = (p.alpha - q.alpha) * (p.alpha.digamma() - sum_p_alpha.digamma())
-    return t1 - t3 - t2 + t4 + t5.sum(-1)
+    return t1 - t3 - t2 + t4 + t5.sum(0)
 
 
 @register_kl(Exponential, Exponential)