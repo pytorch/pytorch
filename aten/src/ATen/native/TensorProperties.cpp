b7d0490823e27c58b6188b4ce171fb9705a2cfa9
Edward Z. Yang <ezyang@fb.com>
2018-01-19 12:41:17 -0800

    Add cudnn_is_acceptable function.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/native/TensorProperties.cpp
+++ aten/src/ATen/native/TensorProperties.cpp
@@ -21,5 +21,17 @@ int64_t stride(const Tensor& self, int64_t dim) {
   return self.strides()[dim];
 }
 
+bool cudnn_is_acceptable(const Tensor& self) {
+  if (!globalContext().userEnabledCuDNN()) return false;
+  if (!self.is_cuda()) return false;
+  auto st = self.type().scalarType();
+  if (!(st == kDouble || st == kFloat || st == kHalf)) return false;
+  if (!AT_CUDNN_ENABLED()) return false;
+  // NB: In the old Python code, there was also a test to see if the
+  // cuDNN library was actually dynamically linked or not.  I'm not
+  // sure if we can actually test this.
+  return true;
+}
+
 }
 }
