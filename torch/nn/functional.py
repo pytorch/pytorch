548a628ba708994f2b2599f3a956c0cda701a6f8
justheuristic <justheuristic@gmail.com>
2018-01-22 05:52:19 +0300

    pyflake compliance

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -1942,13 +1942,13 @@ def assert_int_or_pair(arg, arg_name, message):
 
 
 def to_one_hot(y, depth=None):
-    r""" 
+    r"""
     Takes integer with n dims and converts it to 1-hot representation with n + 1 dims.
     The n+1'st dimension will have zeros everywhere but at y'th index, where it will be equal to 1.
     Args:
         y: input integer (IntTensor, LongTensor or Variable) of any shape
         depth (int):  the size of the one hot dimension
-        
+
     Examples::
         >>> to_one_hot(torch.arange(0, 5) % 3)
          1  0  0
@@ -1965,15 +1965,15 @@ def to_one_hot(y, depth=None):
          0  1  0  0  0
         [torch.FloatTensor of size 5x5]
         >>> to_one_hot(torch.arange(0, 6).view(3,2) % 3)
-        (0 ,.,.) = 
+        (0 ,.,.) =
           1  0  0
           0  1  0
 
-        (1 ,.,.) = 
+        (1 ,.,.) =
           0  0  1
           1  0  0
 
-        (2 ,.,.) = 
+        (2 ,.,.) =
           0  1  0
           0  0  1
         [torch.FloatTensor of size 3x2x3]
@@ -1982,7 +1982,7 @@ def to_one_hot(y, depth=None):
     y_tensor = y_tensor.type(torch.LongTensor).view(-1, 1)
     depth = depth if depth is not None else int(torch.max(y_tensor)) + 1
     y_one_hot = torch.zeros(y_tensor.size()[0], depth).scatter_(1, y_tensor, 1)
-    y_one_hot = y_one_hot.view(*y.shape, -1)
+    y_one_hot = y_one_hot.view(*(tuple(y.shape) + (-1,)))
     return Variable(y_one_hot) if isinstance(y, Variable) else y_one_hot
 
 
