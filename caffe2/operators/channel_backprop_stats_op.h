349f1ab1cfa4c73ab4c482dcf542d096dea32e57
Lukasz Wesolowski <lwesolowski@fb.com>
2018-01-24 13:10:53 -0800

    Add operators to support batch normalization across multiple devices on the same node
    Summary: This is the first in a series of diffs to enable batch normalization across multiple devices on the same node with data parallel model. The diff contains the ops for computing the per-channel statistics required to obtain the mean and variance across multiple devices on the same node on the forward pass, and the gradient of the bias and scale during backpropagation. The actual modifications to SpatialBN and SpatialBNGradient to make use of these results will be in a separate diff.
    
    Reviewed By: rbgirshick
    
    Differential Revision: D6697336
    
    fbshipit-source-id: 0de2750fe7e851795f238d9f625aeb4d74023dc2

---
+++ caffe2/operators/channel_backprop_stats_op.h
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CHANNEL_BACKPROP_STATS_OP_H
+#define CHANNEL_BACKPROP_STATS_OP_H
+
+#include "caffe2/core/context.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/utils/math.h"
+
+namespace caffe2 {
+
+template <class Context>
+class ChannelBackpropStatsOp : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  ChannelBackpropStatsOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws) {}
+  ~ChannelBackpropStatsOp() {}
+
+  bool RunOnDevice() override {
+    return true;
+  }
+
+ protected:
+  INPUT_TAGS(INPUT, SAVED_MEAN, SAVED_INV_STDDEV, OUTPUT_GRAD);
+  OUTPUT_TAGS(SCALE_GRAD, BIAS_GRAD);
+};
+
+} // namespace caffe2
+
+#endif
