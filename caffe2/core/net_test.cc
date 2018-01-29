94b659034e094b24c3e56fd8d2db174f433d8150
Orion Reblitz-Richardson <orionr@gmail.com>
2018-01-28 23:17:36 -0800

    Potential fix to net_test failure with one GPU
    Summary:
    * Putting up to test on Jenkins since I can't test locally on my Mac.
    
    Might fix https://github.com/caffe2/caffe2/issues/1796 but I haven't touched these files before, so it's a guess. :)
    Closes https://github.com/caffe2/caffe2/pull/1826
    
    Reviewed By: Yangqing
    
    Differential Revision: D6832918
    
    Pulled By: orionr
    
    fbshipit-source-id: 22bdeafa031dbe6457d81cb105b41a451ca3a25d

--- caffe2/core/net_test.cc
+++ caffe2/core/net_test.cc
@@ -222,47 +222,6 @@ TEST(NetTest, ChainingForLinearModel) {
   checkChainingAndRun(spec, {{0, {0, 1}}});
 }
 
-TEST(NetTest, ChainingForDifferentDevices) {
-  const auto spec = R"DOC(
-        name: "example"
-        type: "dag"
-        external_input: "in"
-        op {
-          input: "in"
-          output: "hidden"
-          type: "NetTestDummy"
-        }
-        op {
-          input: "hidden"
-          output: "out"
-          type: "NetTestDummy"
-          device_option {
-            device_type: 1
-          }
-        }
-        op {
-          input: "out"
-          output: "out2"
-          type: "NetTestDummy"
-          device_option {
-            device_type: 1
-          }
-        }
-        op {
-          input: "out2"
-          output: "out3"
-          type: "NetTestDummy"
-          device_option {
-            device_type: 1
-            cuda_gpu_id: 1
-          }
-        }
-)DOC";
-  if (HasCudaRuntime()) {
-    checkChainingAndRun(spec, {{0, {0, 1, 2}}, {3, {3}}});
-  }
-}
-
 TEST(NetTest, ChainingForFork) {
   const auto spec = R"DOC(
         name: "example"
