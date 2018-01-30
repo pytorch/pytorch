3ad5c0019e84b14ca7c5aa457808778de8995ab6
Gregory Chanan <gchanan@fb.com>
2018-01-30 15:00:56 -0800

    Properly fill in make_non_contiguous data for sizes that can't be made contiguous.

--- torch/testing/__init__.py
+++ torch/testing/__init__.py
@@ -12,7 +12,7 @@ __all__ = [
 
 def make_non_contiguous(tensor):
     if tensor.numel() <= 1:  # can't make non-contiguous
-        return tensor.new(tensor.size())
+        return tensor.new(tensor.size()).copy_(tensor)
     osize = list(tensor.size())
 
     # randomly inflate a few dimensions in osize
