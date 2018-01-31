f6ff846ce6de95f95ccd2b5ed9cd39a651a29bb0
Lynn <menglin.wu@intel.com>
2018-01-31 22:43:09 +0800

    interface test of conv3d

--- aten/src/THNN/generic/VolumetricConvolutionMM.c
+++ aten/src/THNN/generic/VolumetricConvolutionMM.c
@@ -130,13 +130,13 @@ static void THNN_(unfolded_acc_vol)(
           int pT,
           int pW,
           int pH,
-          int64_t nInputPlane,
-          int64_t inputDepth,
-          int64_t inputWidth,
-          int64_t inputHeight,
-          int64_t outputDepth,
-          int64_t outputWidth,
-          int64_t outputHeight)
+          long nInputPlane,
+          long inputDepth,
+          long inputWidth,
+          long inputHeight,
+          long outputDepth,
+          long outputWidth,
+          long outputHeight)
 {
   real *input_data = THTensor_(data)(input);
   real *finput_data = THTensor_(data)(finput);
@@ -283,13 +283,13 @@ static void THNN_(unfolded_copy_vol)(
           int pT,
           int pW,
           int pH,
-          int64_t nInputPlane,
-          int64_t inputDepth,
-          int64_t inputWidth,
-          int64_t inputHeight,
-          int64_t outputDepth,
-          int64_t outputWidth,
-          int64_t outputHeight)
+          long nInputPlane,
+          long inputDepth,
+          long inputWidth,
+          long inputHeight,
+          long outputDepth,
+          long outputWidth,
+          long outputHeight)
 {
   real *input_data = THTensor_(data)(input);
   real *finput_data = THTensor_(data)(finput);
