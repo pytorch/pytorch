ae69341d558c6bc13a8550a6924db864e1389f11
Richard Zou <zou3519@gmail.com>
2018-02-02 13:28:23 -0700

    Search for aten_dense_sparse instead of sparse=True in preprocess_declarations

--- aten/src/ATen/preprocess_declarations.py
+++ aten/src/ATen/preprocess_declarations.py
@@ -192,11 +192,11 @@ def discover_sparse_tensor_operations(declaration):
                     if not exclude(arg)]
         return '#'.join(elements)
 
-    # Determine if any options have the 'sparse: True' flag
-    sparse_options = [option
-                      for option in declaration['options']
-                      if option.get('sparse', False)]
-    if len(sparse_options) > 0:
+    # Determine if any options have the 'aten_dense_sparse' flag
+    dense_sparse_options = [option
+                            for option in declaration['options']
+                            if option.get('aten_dense_sparse', False)]
+    if len(dense_sparse_options) > 0:
         signature_to_option = {signature(option): option
                                for option in declaration['options']}
 
