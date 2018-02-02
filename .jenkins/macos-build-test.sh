1efa38faa78920f8798a2fdf6a2ba952312da97f
Edward Z. Yang <ezyang@mit.edu>
2018-02-02 11:14:56 -0500

    Add some more builder scripts from ossci-job-dsl (#4945)
    * Add some more builder scripts from ossci-job-dsl
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * Relax precision requirement on test_Upsample_trilinear_scale_3d_cuda
    
    Partially addresses #5006.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ .jenkins/macos-build-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+# Set up conda environment
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o $PWD/miniconda3.sh
+rm -rf $PWD/miniconda3
+bash $PWD/miniconda3.sh -b -p $PWD/miniconda3
+export PATH="$PWD/miniconda3/bin:$PATH"
+source $PWD/miniconda3/bin/activate
+conda install -y numpy pyyaml setuptools cmake cffi ninja
+
+# Build and test PyTorch
+git submodule update --init --recursive
+export CMAKE_PREFIX_PATH=$PWD/miniconda3/
+
+echo "ENTERED_USER_LAND"
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+export CXX=clang++
+export CC=clang
+python setup.py install
+cd test/
+echo "Ninja version: $(ninja --version)"
+sh run_test.sh
+echo "EXITED_USER_LAND"
+echo "BUILD PASSED"
