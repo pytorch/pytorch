89547ec304e60380ba3660f35222cd412831cea6
Edward Z. Yang <ezyang@fb.com>
2018-02-02 16:15:48 -0800

    Typofix.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .jenkins/short-perf-test-gpu.sh
+++ .jenkins/short-perf-test-gpu.sh
@@ -50,7 +50,7 @@ sample_data_list = sys.argv[2:]
 sample_data_list = [float(v.strip()) for v in sample_data_list]
 
 sample_mean = numpy.mean(sample_data_list)
-sample_sigma = numpy.sigma(sample_data_list)
+sample_sigma = numpy.std(sample_data_list)
 
 print("test mean: ", sample_mean)
 print("test sigma: ", sample_sigma)
