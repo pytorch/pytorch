c3eb7294b4984ddf4930025c79a2073a562276ed
Richard Zou <zou3519@gmail.com>
2018-02-05 14:20:44 -0800

    Address comments

--- aten/src/THC/generic/THCTensor.c
+++ aten/src/THC/generic/THCTensor.c
@@ -280,10 +280,13 @@ THCTensor *THCTensor_(newView)(THCState *state, THCTensor *tensor, THLongStorage
   return self;
 }
 
-// Collapses the first two dimensions of a tensor
+// Collapses the first two dimensions of a tensor.
+// Assumes the input tensor is contiguous.
 THCTensor *THCTensor_(newFoldBatchDim)(THCState *state, THCTensor *input) {
   int in_dims = THCTensor_(nDimension)(state, input);
   THArgCheck(in_dims >= 2, 1, "Tensor needs to have at least two dimensions");
+  THArgCheck(THCTensor_(isContiguous)(state, input), 1,
+             "Tensor must be contiguous");
   THLongStorage *newSize = THLongStorage_newWithSize(in_dims - 1);
   newSize->data[0] = THCTensor_(size)(state, input, 0) * THCTensor_(size)(state, input, 1);
   for (int i = 2; i < in_dims; i++) {
