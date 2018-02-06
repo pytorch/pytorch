fc856f0036ee5cee6cb9276424b718a8e9144edb
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2018-02-05 19:20:09 -0800

    Fix type and disable warning about cpu arch
    Reviewed By: Yangqing
    
    Differential Revision: D6908986
    
    fbshipit-source-id: 42bba410ea772999717353b749c411bd8484af6b

--- caffe2/core/init_intrinsics_check.cc
+++ caffe2/core/init_intrinsics_check.cc
@@ -51,19 +51,26 @@ static void WarnIfFeatureUnused(
     const bool cpu_has_feature, const string& feature) {
   VLOG(1) << "Caffe2 not built with " << feature << ".";
   if (cpu_has_feature) {
+#ifdef CAFFE2_NO_CROSS_ARCH_WARNING
+    // When cross-compiling single binary for multiple archs - turns off the
+    // annoying warning
+    VLOG(1)
+#else
     LOG(ERROR)
-        << "CPU feature " << feature << " is present on your machine, "
-        "but the Caffe2 binary is not compiled with it. It means you "
-        "may not get the full speed of your CPU.";
+#endif
+        << "CPU feature " << feature
+        << " is present on your machine, "
+           "but the Caffe2 binary is not compiled with it. It means you "
+           "may not get the full speed of your CPU.";
   }
 }
 
 bool Caffe2CheckIntrinsicsFeatures(int*, char***) {
 
 #ifdef __AVX__
-  QuitIfFeatureUnsupported(GetCpuId().avx2(), "avx");
+  QuitIfFeatureUnsupported(GetCpuId().avx(), "avx");
 #else
-  WarnIfFeatureUnused(GetCpuId().avx2(), "avx");
+  WarnIfFeatureUnused(GetCpuId().avx(), "avx");
 #endif
 
 #ifdef __AVX2__
