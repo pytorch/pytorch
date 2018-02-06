0d38737ceb0c28eb977d453b621d8a1798ce92a6
vishwakftw <cs15btech11043@iith.ac.in>
2018-02-06 23:02:21 +0530

    Add .cdf and .icdf for Laplace distribution

--- torch/distributions/laplace.py
+++ torch/distributions/laplace.py
@@ -55,5 +55,14 @@ class Laplace(Distribution):
         self._validate_log_prob_arg(value)
         return -torch.log(2 * self.scale) - torch.abs(value - self.loc) / self.scale
 
+    def cdf(self, value):
+        self._validate_log_prob_arg(value)
+        return 0.5 - 0.5 * (value - self.loc).sign() * torch.expm1(-(value - self.loc).abs() / self.scale)
+
+    def icdf(self, value):
+        self._validate_log_prob_arg(value)
+        term = value - 0.5
+        return self.loc - self.scale * (term).sign() * torch.log1p(-2 * term.abs())
+
     def entropy(self):
         return 1 + torch.log(2 * self.scale)
