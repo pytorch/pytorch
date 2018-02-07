8f78dd724994a7a0fd1371d0504f252b2de175e1
Will Feng <yf225@cornell.edu>
2018-02-06 22:12:53 -0500

    Refactor CPU and GPU perf tests (#5078)

---
+++ .jenkins/perf_test/compare_with_baseline.py
@@ -0,0 +1,35 @@
+import sys
+import json
+import numpy
+from scipy import stats
+
+with open('../perf_test_numbers.json') as data_file:
+    data = json.load(data_file)
+
+test_name = sys.argv[1]
+
+mean = float(data[test_name]['mean'])
+sigma = float(data[test_name]['sigma'])
+
+print("population mean: ", mean)
+print("population sigma: ", sigma)
+
+sample_stats_data = json.loads(sys.argv[2])
+
+sample_mean = sample_stats_data['mean']
+sample_sigma = sample_stats_data['sigma']
+
+print("sample mean: ", sample_mean)
+print("sample sigma: ", sample_sigma)
+
+z_value = (sample_mean - mean) / sigma
+
+print("z-value: ", z_value)
+
+if z_value >= 3:
+    raise Exception('''\n
+z-value >= 3, there is >99.7% chance of perf regression.\n
+To reproduce this regression, run `cd .jenkins/perf_test/ && bash ''' + test_name + '''.sh` on your local machine and compare the runtime before/after your code change.
+''')
+else:
+    print("z-value < 3, no perf regression detected.")
--- .jenkins/short-perf-test-cpu.sh
+++ .jenkins/short-perf-test-cpu.sh
@@ -2,125 +2,20 @@
 
 set -ex
 
+cd .jenkins/perf_test
+
 export PATH=/opt/conda/bin:$PATH
 
 echo "Running CPU perf test for PyTorch..."
-export OMP_NUM_THREADS=4
-export MKL_NUM_THREADS=4
-
-cd test/
-
-# TODO: Move this into its own file
-cat >compare_with_baseline.py << EOL
-
-import sys
-import numpy
-from scipy import stats
-
-mean_values = {
-  "commit": "92aeca1279265d24493dc6ced7dde9a368faf048",
-
-  "test_cpu_speed_mini_sequence_labeler": "2.62557",
-  "test_cpu_speed_mnist": "18.79468",
-}
-
-sigma_values = {
-  "commit": "92aeca1279265d24493dc6ced7dde9a368faf048",
-
-  "test_cpu_speed_mini_sequence_labeler": "0.12167",
-  "test_cpu_speed_mnist": "2.37561",
-}
-
-mean = float(mean_values[sys.argv[1]])
-sigma = float(sigma_values[sys.argv[1]])
-
-print("population mean: ", mean)
-print("population sigma: ", sigma)
-
-sample_data_list = sys.argv[2:]
-sample_data_list = [float(v.strip()) for v in sample_data_list]
-
-sample_mean = numpy.mean(sample_data_list)
-
-print("sample mean: ", sample_mean)
-
-z_value = (sample_mean - mean) / sigma
-
-print("z-value: ", z_value)
-
-if z_value >= 3:
-  raise Exception("z-value >= 3, there is 99.7% chance of perf regression.")
-else:
-  print("z-value < 3, no perf regression detected.")
-
-EOL
-
-run_test () {
-  mkdir test_tmp/ && cd test_tmp/
-  $1
-  cd .. && rm -rf test_tmp/
-}
-
-get_runtime_of_command () {
-  TIMEFORMAT=%R
-
-  # runtime=$( { time ($1 &> /dev/null); } 2>&1 1>/dev/null)
-  runtime=$( { time $1; } 2>&1 1>/dev/null)
-  runtime=${runtime#+++ $1}
-  runtime=$(python -c "print($runtime)")
-
-  echo $runtime
-}
-
-# Define tests
-test_cpu_speed_mini_sequence_labeler () {
-  echo "Testing: mini sequence labeler, CPU"
-
-  curl https://gist.githubusercontent.com/yf225/40c0adb8bfb2a7b774fa266fb4bc409e/raw/20c67ebadbd75f41c6c9fd2e00b4b2562b60700a/mini_sequence_labeler.py -O
-  curl https://gist.githubusercontent.com/yf225/592b39ca6a3fc835a4d1532fb1474d26/raw/76f57c198cb7afdc5122e413c2a3023ed024b643/wsj.pkl -O
-
-  SAMPLE_ARRAY=()
-  NUM_RUNS=20
-
-  for (( i=1; i<=$NUM_RUNS; i++ )) do
-    runtime=$(get_runtime_of_command "python mini_sequence_labeler.py")
-    echo $runtime
-    SAMPLE_ARRAY+=(${runtime})
-  done
-
-  python ../compare_with_baseline.py ${FUNCNAME[0]} "${SAMPLE_ARRAY[@]}"
-}
-
-test_cpu_speed_mnist () {
-  echo "Testing: MNIST, CPU"
-
-  git clone https://github.com/yf225/examples.git -b benchmark_test
-
-  cd examples/mnist
-
-  pip install -r requirements.txt
-
-  # Download data
-  python main.py --epochs 0
-
-  SAMPLE_ARRAY=()
-  NUM_RUNS=20
-
-  for (( i=1; i<=$NUM_RUNS; i++ )) do
-    runtime=$(get_runtime_of_command "python main.py --epochs 1 --no-log")
-    echo $runtime
-    SAMPLE_ARRAY+=(${runtime})
-  done
-
-  cd ../..
-
-  python ../compare_with_baseline.py ${FUNCNAME[0]} "${SAMPLE_ARRAY[@]}"
-}
 
 echo "ENTERED_USER_LAND"
 
+# Include tests
+. ./test_cpu_speed_mini_sequence_labeler.sh
+. ./test_cpu_speed_mnist.sh
+
 # Run tests
-run_test test_cpu_speed_mini_sequence_labeler
-run_test test_cpu_speed_mnist
+run_test test_cpu_speed_mini_sequence_labeler compare_with_baseline
+run_test test_cpu_speed_mnist compare_with_baseline
 
 echo "EXITED_USER_LAND"
