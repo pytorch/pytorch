ecc8eb7740817ac065fa6da434b6b38e3b073149
Guillaume Dumont <dumont.guillaume@gmail.com>
2018-02-07 15:55:40 -0500

    - Added ATen exported targets for consuming projects
    - Modified THD CMakeLists.txt to handle the fact that
    ${ATEN_LIBRARIES} are now imported targets.
    
    - Made cpuinfo and tbb_static private dependencies

--- tools/run_aten_tests.sh
+++ tools/run_aten_tests.sh
@@ -2,7 +2,7 @@
 set -xe
 mkdir aten_build aten_install
 cd aten_build
-cmake ../aten -DNO_CUDA=1 -DCMAKE_INSTALL_PREFIX=../aten_install
+cmake ../aten -DNO_CUDA=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../aten_install
 make -j32 install
 ../aten/tools/run_tests.sh .
 cd ..
