c111cdfd1dc77682d45fa4794ebcdeabc871e022
Lu Fang <30275821+houseroad@users.noreply.github.com>
2018-02-07 07:54:30 -0800

    Add onnx support for InstanceNorm (#4626)
    * Add ONNX symbolic for instancenorm
    
    * Fix some bugs

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -534,6 +534,22 @@ def conv_tbc(g, input, weight, bias, pad):
     return g.op("ATen", input, weight, bias, operator_s="conv_tbc", pad_i=pad)
 
 
+def instance_norm(g, input, **kwargs):
+    input_type = input.type().scalarType()
+    weight = kwargs.get("weight", None)
+    bias = kwargs.get("bias", None)
+    eps = kwargs.get("eps", 1e-5)
+    if not weight:
+        weight = g.constant(1.0, [input.type().sizes()[1]], input_type)
+    else:
+        weight = g.op('Constant', value_t=weight)
+    if not bias:
+        bias = g.constant(0.0, [input.type().sizes()[1]], input_type)
+    else:
+        bias = g.op('Constant', value_t=bias)
+    return g.op("InstanceNormalization", input, weight, bias, epsilon_f=eps)
+
+
 def RNN_symbolic_builder(cell_type, *args, **kwargs):
     if cell_type == 'LSTM':
         return LSTM_symbolic_builder(*args, **kwargs)
