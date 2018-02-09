328985671f0eb6696821fdfcae6ee60f9db1357d
Hao Lu <hlu@fb.com>
2018-02-08 18:57:48 -0800

    Make NNApi build
    Summary:
    To build with tests and benchmarks
    `./scripts/build_android.sh -G Ninja -DBUILD_TEST=ON -DUSE_NNAPI=ON`
    To run unit test
    `adb push build_android/bin/nnapi_test data/local/tmp`
    `adb shell "cd data/local/tmp &&./nnapi_test`
    To run benchmark
    `adb push build_android/bin/nnapi_benchmark data/local/tmp`
    `adb shell "cd data/local/tmp &&./nnapi_benchmark`
    Tested on Google PIxel 2 XL with android 8.1
    Closes https://github.com/caffe2/caffe2/pull/1918
    
    Reviewed By: Maratyszcza
    
    Differential Revision: D6944604
    
    Pulled By: hlu1
    
    fbshipit-source-id: 462f010117ae4628b23bef506c41397de3817ad4

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -478,6 +478,11 @@ if (USE_METAL)
   endif()
 endif()
 
+if (USE_NNAPI AND NOT ANDROID)
+  message(WARNING "NNApi is only used in android builds.")
+  set(USE_NNAPI OFF)
+endif()
+
 if (USE_ATEN)
   list(APPEND Caffe2_EXTERNAL_DEPENDENCIES aten_build)
   list(APPEND Caffe2_DEPENDENCY_LIBS ATen)
