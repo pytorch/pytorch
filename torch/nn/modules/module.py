4c4a267817a14ab11b7364a65ecf406976fd8bc0
Kaiyu Shi <skyisno.1@gmail.com>
2018-02-10 23:31:12 +0800

    Fix PEP8

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -91,7 +91,7 @@ class Module(object):
         """
         if hasattr(self, name) and name not in self._buffers:
             raise KeyError("attribute '{}' already exists".format(name))
-        elif not tensor is None and not torch.is_tensor(tensor):
+        elif tensor is not None and not torch.is_tensor(tensor):
             raise TypeError("cannot assign '{}' object to buffer '{}' "
                             "(torch Tensor or None required)"
                             .format(torch.typename(tensor), name))
