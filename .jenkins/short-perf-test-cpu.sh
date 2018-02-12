63f31ccca428970b6dd66dce16145d39a3b5b749
Edward Z. Yang <ezyang@fb.com>
2018-02-12 15:38:08 -0800

    Modest refactor of .jenkins scripts
    - Create a new common.sh to put common bash stanzas in
    - Create a new enabled-configs.txt file, which you can use
      to selectively disable tests when running CI
    - Specify exited user land via trap, which means early successful
      exit will correctly print the end sigil.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .jenkins/short-perf-test-cpu.sh
+++ .jenkins/short-perf-test-cpu.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-set -ex
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 cd .jenkins/perf_test
 
 export PATH=/opt/conda/bin:$PATH
 
 echo "Running CPU perf test for PyTorch..."
-
-echo "ENTERED_USER_LAND"
 
 # Include tests
 . ./test_cpu_speed_mini_sequence_labeler.sh
@@ -17,5 +15,3 @@ echo "ENTERED_USER_LAND"
 # Run tests
 run_test test_cpu_speed_mini_sequence_labeler compare_with_baseline
 run_test test_cpu_speed_mnist compare_with_baseline
-
-echo "EXITED_USER_LAND"
