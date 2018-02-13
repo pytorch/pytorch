cb76a06ffc990df112f0a43d4e855a11b92d37d8
Yinghai Lu <yinghai@fb.com>
2018-02-12 21:40:15 -0800

    Fix cmake function to resolve libraries correctly
    Summary:
    Previous behavior may fail to resolve the correct library name. A rework of https://github.com/caffe2/caffe2/pull/1935 as it was messed up in the rebase...
    Closes https://github.com/caffe2/caffe2/pull/1950
    
    Reviewed By: bddppq
    
    Differential Revision: D6974530
    
    Pulled By: yinghai
    
    fbshipit-source-id: 924b653e8ac0b68c46341edfd3eb05d9cc0155f2

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -180,7 +180,7 @@ function(caffe_add_whole_archive_flag lib output_var)
     set(${output_var} -WHOLEARCHIVE:$<TARGET_FILE:${lib}> PARENT_SCOPE)
   else()
     # Assume everything else is like gcc
-    set(${output_var} -Wl,--whole-archive ${lib} -Wl,--no-whole-archive PARENT_SCOPE)
+    set(${output_var} -Wl,--whole-archive $<TARGET_FILE:${lib}> -Wl,--no-whole-archive PARENT_SCOPE)
   endif()
 endfunction()
 
