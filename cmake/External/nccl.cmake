cab91e3899803de5679d487d9bec9284a42cad20
Junjie Bai <bai@in.tum.de>
2018-02-14 15:54:18 -0800

    Fix nccl cmake files
    Summary: Closes https://github.com/caffe2/caffe2/pull/1963
    
    Differential Revision: D6994392
    
    Pulled By: bddppq
    
    fbshipit-source-id: 4ab6a8f7dcb4469bdd3e152559ff3474984776fc

--- cmake/External/nccl.cmake
+++ cmake/External/nccl.cmake
@@ -18,17 +18,32 @@ if (NOT __NCCL_INCLUDED)
     set(NCCL_CXX_FLAGS ${CMAKE_CXX_FLAGS} ${NCCL_EXTRA_COMPILER_FLAGS})
     set(NCCL_C_FLAGS ${CMAKE_C_FLAGS} ${NCCL_EXTRA_COMPILER_FLAGS})
 
-    ExternalProject_Add(nccl_external
-      SOURCE_DIR ${nccl_PREFIX}
-      BUILD_IN_SOURCE 1
-      CONFIGURE_COMMAND ""
-      BUILD_COMMAND
-        make
-        "CXX=${CMAKE_CXX_COMPILER}"
-        "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
-        "NVCC=${CUDA_NVCC_EXECUTABLE}"
-      INSTALL_COMMAND ""
-      )
+    if(${CMAKE_VERSION} VERSION_LESS "3.2")
+      ExternalProject_Add(nccl_external
+        SOURCE_DIR ${nccl_PREFIX}
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND
+          make
+          "CXX=${CMAKE_CXX_COMPILER}"
+          "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
+          "NVCC=${CUDA_NVCC_EXECUTABLE}"
+        INSTALL_COMMAND ""
+        )
+    else()
+      ExternalProject_Add(nccl_external
+        SOURCE_DIR ${nccl_PREFIX}
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND
+          make
+          "CXX=${CMAKE_CXX_COMPILER}"
+          "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
+          "NVCC=${CUDA_NVCC_EXECUTABLE}"
+        BUILD_BYPRODUCTS "${nccl_PREFIX}/build/lib/libnccl_static.a"
+        INSTALL_COMMAND ""
+        )
+    endif()
 
     set(NCCL_FOUND TRUE)
     set(NCCL_INCLUDE_DIRS ${nccl_PREFIX}/build/include)
