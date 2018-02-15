8543719d3513361f7029fdb78b61dd2b60660646
mdraw <mdraw.gh@gmail.com>
2018-02-15 16:25:20 +0100

    Remove superfluous backticks from LRN formula

--- torch/nn/modules/normalization.py
+++ torch/nn/modules/normalization.py
@@ -9,8 +9,8 @@ class LocalResponseNorm(Module):
 
     .. math::
 
-        `b_{c} = a_{c}\left(k + \frac{\alpha}{n}
-        \sum_{c'=\max(0, c-n/2)}^{\min(N-1,c+n/2)}a_{c'}^2\right)^{-\beta}`
+        b_{c} = a_{c}\left(k + \frac{\alpha}{n}
+        \sum_{c'=\max(0, c-n/2)}^{\min(N-1,c+n/2)}a_{c'}^2\right)^{-\beta}
 
     Args:
         size: amount of neighbouring channels used for normalization
