f4f5bad901996051da1711e94ab27f92be084bd2
Manoj Krishnan <manojk@fb.com>
2018-02-16 17:16:30 -0800

    Adding a new ReduceScatter Operator.
    Summary: Integrating Gloo's ReduceScatter operation with caffe2
    
    Reviewed By: pietern
    
    Differential Revision: D6970344
    
    fbshipit-source-id: 27762f940812eb0bf6c99afb4ff1a25914855b11

--- caffe2/contrib/gloo/gloo_test.py
+++ caffe2/contrib/gloo/gloo_test.py
@@ -361,6 +361,94 @@ class TestCase(hu.HypothesisTestCase):
                     tmpdir=tmpdir,
                     use_float16=use_float16)
 
+    def _test_reduce_scatter(self,
+                             comm_rank=None,
+                             comm_size=None,
+                             blob_size=None,
+                             num_blobs=None,
+                             tmpdir=None,
+                             use_float16=False
+                             ):
+        store_handler, common_world = self.create_common_world(
+            comm_rank=comm_rank,
+            comm_size=comm_size,
+            tmpdir=tmpdir)
+
+        blob_size = self.synchronize(
+            store_handler,
+            blob_size,
+            comm_rank=comm_rank)
+
+        num_blobs = self.synchronize(
+            store_handler,
+            num_blobs,
+            comm_rank=comm_rank)
+
+        blobs = []
+        for i in range(num_blobs):
+            blob = "blob_{}".format(i)
+            value = np.full(blob_size, (comm_rank * num_blobs) + i,
+                            np.float16 if use_float16 else np.float32)
+            workspace.FeedBlob(blob, value)
+            blobs.append(blob)
+
+        # Specify distribution among ranks i.e. number of elements
+        # scattered/distributed to each process.
+        recv_counts = np.zeros(comm_size, dtype=np.int32)
+        remaining = blob_size
+        chunk_size = (blob_size + comm_size - 1) / comm_size
+        for i in range(comm_size):
+            recv_counts[i] = min(chunk_size, remaining)
+            remaining = remaining - chunk_size if remaining > chunk_size else 0
+        recv_counts_blob = "recvCounts"
+        workspace.FeedBlob(recv_counts_blob, recv_counts)
+        blobs.append(recv_counts_blob)
+
+        net = core.Net("reduce_scatter")
+        net.ReduceScatter(
+            [common_world] + blobs,
+            blobs,
+            engine=op_engine)
+
+        workspace.CreateNet(net)
+        workspace.RunNet(net.Name())
+
+        for i in range(num_blobs):
+            np.testing.assert_array_equal(
+                np.resize(workspace.FetchBlob(blobs[i]), recv_counts[comm_rank]),
+                (num_blobs * comm_size) * (num_blobs * comm_size - 1) / 2)
+
+        # Run the net a few more times to check the operator
+        # works not just the first time it's called
+        for _tmp in range(4):
+            workspace.RunNet(net.Name())
+
+    @given(comm_size=st.integers(min_value=2, max_value=8),
+           blob_size=st.integers(min_value=1e3, max_value=1e6),
+           num_blobs=st.integers(min_value=1, max_value=4),
+           device_option=st.sampled_from([hu.cpu_do]),
+           use_float16=st.booleans())
+    def test_reduce_scatter(self, comm_size, blob_size, num_blobs,
+                            device_option, use_float16):
+        TestCase.test_counter += 1
+        if os.getenv('COMM_RANK') is not None:
+            self.run_test_distributed(
+                self._test_reduce_scatter,
+                blob_size=blob_size,
+                num_blobs=num_blobs,
+                use_float16=use_float16,
+                device_option=device_option)
+        else:
+            with TemporaryDirectory() as tmpdir:
+                self.run_test_locally(
+                    self._test_reduce_scatter,
+                    comm_size=comm_size,
+                    blob_size=blob_size,
+                    num_blobs=num_blobs,
+                    device_option=device_option,
+                    tmpdir=tmpdir,
+                    use_float16=use_float16)
+
     def _test_allgather(self,
                         comm_rank=None,
                         comm_size=None,
