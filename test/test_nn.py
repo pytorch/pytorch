fe918b0a773dbf9ecd80b286db493524bd19137d
justheuristic <justheuristic@gmail.com>
2018-02-19 00:54:21 +0300

    whitespaces

--- test/test_nn.py
+++ test/test_nn.py
@@ -1426,7 +1426,7 @@ class TestNN(NNTestCase):
         inputs = Variable(torch.randn(1, 3, 4, 4), requires_grad=True)
         self.assertTrue(gradcheck(lambda x: F.normalize(x, p=1, dim=-1), (inputs,)))
         self.assertTrue(gradcheck(lambda x: F.normalize(x, p=2, dim=-2), (inputs,)))
-
+    
     def test_to_one_hot(self):
         y = Variable(torch.LongTensor([3, 2, 1, 2, 3]))
         y_onehot = F.to_one_hot(y, depth=5)
@@ -1438,7 +1438,6 @@ class TestNN(NNTestCase):
             [0, 0, 0, 1, 0]
         ])
         self.assertTrue((y_onehot.data == reference).all())
-
         y = torch.LongTensor([
             [1, 2, 3, 2, 1, 2],
             [4, 3, 2, 2, 1, 0]
