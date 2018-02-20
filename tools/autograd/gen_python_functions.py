0509f26d41428d91761ed3415cc2542608c61bf0
Sam Gross <colesbury@gmail.com>
2018-02-19 19:47:20 -0500

    Speed-up nn.Linear for the 3d input case (#5279)
    This adds at::_unsafe_view and uses it in matmul. The _unsafe_view
    function is identical to view except that the output is not treated
    like a view by the automatic differentiation code. This avoids in-place
    modifications triggering the more expensive CopySlices/AsStridedBackward
    behavior.
    
    The _unsafe_view function is only safe to use on temporaries that will
    be immediately discarded and that do not alias other tensors. Otherwise,
    in-place modificatiions may trigger incorrect gradients. The funciton is
    not exposed to Python.
    
    See #5169

--- tools/autograd/gen_python_functions.py
+++ tools/autograd/gen_python_functions.py
@@ -16,7 +16,7 @@ CodeTemplate = import_module('code_template', 'aten/src/ATen/code_template.py').
 SKIP_PYTHON_BINDINGS = [
     'alias', 'contiguous', 'clamp.*', 'is_cuda', 'is_sparse', 'size', 'stride',
     '.*_backward', '.*_backward_out', '.*_forward', '.*_forward_out',
-    'sparse_raw_resize_',
+    'sparse_raw_resize_', '_unsafe_view',
 ]
 
 PY_VARIABLE_METHODS_CPP = CodeTemplate.from_file(template_path + '/python_variable_methods.cpp')
