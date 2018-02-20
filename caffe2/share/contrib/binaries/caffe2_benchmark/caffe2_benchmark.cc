372d6972ee9e991394824a0fbec63b528e80dd72
sf-wind <sf_wind@hotmail.com>
2018-02-20 14:45:53 -0800

    Allow more backends in caffe2_benchmark (#1979)
    * Remove OpenGL code from benchmark
    
    * Make it possible to print plot in the ipython notbook
    
    * Create the blob if the blob is not specified in the init net
    
    * Do not use gf library for MKL. Even after I install the entire MKL library it is still not found. After removing it, the MKL code can still run
    
    * Support more backends in Caffe2 Benchmark
    
    * Revert "Do not use gf library for MKL. Even after I install the entire MKL library it is still not found. After removing it, the MKL code can still run"
    
    This reverts commit 981b6693a94cbf63ad78d51bd806c7a0d7a5a2d3.
    
    * Build caffe2_benchmark using shared or static library depending on the flag

--- caffe2/share/contrib/binaries/caffe2_benchmark/caffe2_benchmark.cc
+++ caffe2/share/contrib/binaries/caffe2_benchmark/caffe2_benchmark.cc
@@ -13,9 +13,9 @@
 
 CAFFE2_DEFINE_string(
     backend,
-    "default",
+    "builtin",
     "The backend to use when running the model. The allowed "
-    "backend choices are: default, nnpack");
+    "backend choices are: builtin, default, nnpack, eigen, mkl");
 CAFFE2_DEFINE_string(
     init_net,
     "",
@@ -172,14 +172,16 @@ int main(int argc, char** argv) {
   // Run main network.
   caffe2::NetDef net_def;
   CAFFE_ENFORCE(ReadProtoFromFile(caffe2::FLAGS_net, &net_def));
-  if (caffe2::FLAGS_backend == "nnpack") {
+  if (caffe2::FLAGS_backend != "builtin") {
+    std::string engine = caffe2::FLAGS_backend == "nnpack" ? "NNPACK" :
+                         caffe2::FLAGS_backend == "eigen" ? "EIGEN" :
+                         caffe2::FLAGS_backend == "mkl" ? "MKLDNN" :
+                         caffe2::FLAGS_backend == "default" ? "" : "NONE";
+     CAFFE_ENFORCE(engine != "NONE", "Backend is not supported");
     for (int i = 0; i < net_def.op_size(); i++) {
       caffe2::OperatorDef* op_def = net_def.mutable_op(i);
-      op_def->set_engine("NNPACK");
+      op_def->set_engine(engine);
     }
-  } else {
-    CAFFE_ENFORCE(
-        caffe2::FLAGS_backend == "default", "Backend is not supported");
   }
 
   caffe2::NetBase* net = workspace->CreateNet(net_def);
