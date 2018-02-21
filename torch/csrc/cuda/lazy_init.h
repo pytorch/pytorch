923d7be85f3046157ef169ae2e7407a11a52334c
Gregory Chanan <gchanan@fb.com>
2018-02-21 11:08:46 -0800

    Move implementation of lazy_init to cpp.

--- torch/csrc/cuda/lazy_init.h
+++ torch/csrc/cuda/lazy_init.h
@@ -1,21 +1,9 @@
-#include <Python.h>
-#include <mutex>
-
-#include "torch/csrc/Exceptions.h"
-#include "torch/csrc/utils/object_ptr.h"
+#pragma once
 
 namespace torch {
 namespace cuda {
 
-static void lazy_init() {
-  static std::once_flag once;
-  std::call_once(once, []() {
-    auto module = THPObjectPtr(PyImport_ImportModule("torch.cuda"));
-    if (!module) throw python_error();
-    auto res = THPObjectPtr(PyObject_CallMethod(module.get(), "_lazy_init", ""));
-    if (!res) throw python_error();
-  });
-}
+void lazy_init();
 
 }
 }
