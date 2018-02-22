a16b767ec39b0520dd016bfe86002eedb5d60c7f
Orion Reblitz-Richardson <orionr@gmail.com>
2018-02-21 16:51:35 -0800

    Implementing Pow operator (this merges existing pow with a scalar and new pow with a tensor exponent). Second Try.
    The old pow operator has been deleted in math_ops.cc, math_ops.cu and math_ops.h, while the new operator supporting scalar and tensor exponent has been added in pow_op.cc, pow_op.h an elementwise_op.cu.

--- caffe2/python/operator_test/elementwise_ops_test.py
+++ caffe2/python/operator_test/elementwise_ops_test.py
@@ -75,6 +75,36 @@ class TestElementwiseOps(hu.HypothesisTestCase):
         self.assertGradientChecks(
             gc, op, [X], 0, [0], stepsize=1e-4, threshold=1e-2)
 
+    @given(n=st.integers(2, 10), m=st.integers(4, 6),
+           d=st.integers(2, 3), **hu.gcs)
+    def test_powt(self, n, m, d, gc, dc):
+        np.random.seed(101)
+        X = np.random.rand(n, m, d).astype(np.float32) + 1.0
+        Y = np.random.rand(n, m, d).astype(np.float32) + 2.0
+
+        def powt_op(X, Y):
+            return [np.power(X, Y)]
+
+        #two gradients Y*X^(Y-1) and X^Y * ln(X)
+        def powt_grad(g_out, outputs, fwd_inputs):
+            [X, Y] = fwd_inputs
+            Z = outputs[0]
+            return ([Y * np.power(X, Y - 1), Z * np.log(X)] * g_out)
+
+        op = core.CreateOperator(
+            "Pow",
+            ["X", "Y"],
+            ["Z"]
+        )
+
+        self.assertReferenceChecks(device_option=gc,
+                                   op=op,
+                                   inputs=[X, Y],
+                                   reference=powt_op,
+                                   output_to_grad="Z",
+                                   grad_reference=powt_grad)
+
+
     @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
     def test_sqr(self, n, m, gc, dc):
         X = np.random.rand(n, m).astype(np.float32)
