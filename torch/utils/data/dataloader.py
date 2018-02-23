73f28c58a13e21ce0ed46ea4e813dbc905a75a11
SsnL <tongzhou.wang.1994@gmail.com>
2018-02-23 12:46:55 -0500

    add faq on cuda memory management and dataloder

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -19,7 +19,7 @@ else:
 
 
 class ExceptionWrapper(object):
-    r"Wraps an exception plus traceback to communicate across threads"
+    r"""Wraps an exception plus traceback to communicate across threads"""
 
     def __init__(self, exc_info):
         self.exc_type = exc_info[0]
@@ -27,7 +27,7 @@ class ExceptionWrapper(object):
 
 
 _use_shared_memory = False
-"""Whether to use shared memory in default_collate"""
+r"""Whether to use shared memory in default_collate"""
 
 
 def _worker_loop(dataset, index_queue, data_queue, collate_fn, seed, init_fn, worker_id):
@@ -97,7 +97,7 @@ numpy_type_map = {
 
 
 def default_collate(batch):
-    "Puts each data field into a tensor with outer dimension batch size"
+    r"""Puts each data field into a tensor with outer dimension batch size"""
 
     error_msg = "batch must contain tensors, numbers, dicts or lists; found {}"
     elem_type = type(batch[0])
@@ -151,7 +151,7 @@ def pin_memory_batch(batch):
 
 
 _SIGCHLD_handler_set = False
-"""Whether SIGCHLD handler is set for DataLoader worker failures. Only one
+r"""Whether SIGCHLD handler is set for DataLoader worker failures. Only one
 handler needs to be set for all DataLoaders in a process."""
 
 
@@ -181,7 +181,7 @@ def _set_SIGCHLD_handler():
 
 
 class DataLoaderIter(object):
-    "Iterates once over the DataLoader's dataset, as specified by the sampler"
+    r"""Iterates once over the DataLoader's dataset, as specified by the sampler"""
 
     def __init__(self, loader):
         self.dataset = loader.dataset
@@ -334,7 +334,7 @@ class DataLoaderIter(object):
 
 
 class DataLoader(object):
-    """
+    r"""
     Data loader. Combines a dataset and a sampler, and provides
     single- or multi-process iterators over the dataset.
 
@@ -371,7 +371,7 @@ class DataLoader(object):
               this value in :attr:`worker_init_fn`, which can be used to set other seeds
               (e.g. NumPy) before data loading.
 
-    .. warning:: If ``spawn'' start method is used, :attr:`worker_init_fn` cannot be an
+    .. warning:: If ``spawn`` start method is used, :attr:`worker_init_fn` cannot be an
                  unpicklable object, e.g., a lambda function.
     """
 
