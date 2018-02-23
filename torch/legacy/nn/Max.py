e22441c7d8edd20473033ec8d50e657604b33d02
Sam Gross <sgross@fb.com>
2018-02-23 14:08:46 -0800

    Fix Max.py

--- torch/legacy/nn/Max.py
+++ torch/legacy/nn/Max.py
@@ -23,8 +23,7 @@ class Max(Module):
             self._output = self.output.new()
         if self._indices is None:
             self._indices = \
-                (torch.cuda.LongTensor() if self.output.dtype == torch.cuda.int64
-                 else torch.LongTensor())
+                (torch.cuda.LongTensor() if self.output.is_cuda else torch.LongTensor())
 
     def updateOutput(self, input):
         self._lazyInit()
