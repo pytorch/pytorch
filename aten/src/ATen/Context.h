e056d9cf88553335ee03f2c657954e411898f1ce
Carl Lemaire <c.lemaire@live.ca>
2018-02-27 15:30:41 -0500

    DataParallel: GPU imbalance warning (#5376)

--- aten/src/ATen/Context.h
+++ aten/src/ATen/Context.h
@@ -55,6 +55,7 @@ public:
 
   cudaStream_t getCurrentCUDAStream() const;
   cudaDeviceProp* getCurrentDeviceProperties() const;
+  cudaDeviceProp* getDeviceProperties(int device) const;
 
   bool setFlushDenormal(bool on);
 
