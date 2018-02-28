dbd17668ad4db48062314ef85a1fd87360ae9e51
Paul Jesse Hellemn <jesse.hellemn@gmail.com>
2018-02-28 12:35:51 -0800

    Removing leveldb for ubuntu (#2081)
    * Removing leveldb for ubuntu
    
    * changes

--- conda/cuda_full/build.sh
+++ conda/cuda_full/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This needs to run on Ubuntu 16.04 with gcc 5
 # Install script for Anaconda environments with CUDA on linux
 # This script is not supposed to be called directly, but should be run by:
 #
@@ -25,24 +26,33 @@ PYTHON_ARGS="$(python ./scripts/get_python_cmake_flags.py)"
 # Build with a big suite of libraries
 CMAKE_ARGS=()
 CMAKE_ARGS+=("-DUSE_CUDA=ON")
+CMAKE_ARGS+=("-DCUDA_ARCH_NAME=All")
 CMAKE_ARGS+=("-DUSE_GFLAGS=ON")
 CMAKE_ARGS+=("-DUSE_GLOG=ON")
 CMAKE_ARGS+=("-DUSE_GLOO=ON")
-CMAKE_ARGS+=("-DUSE_LEVELDB=ON")
+CMAKE_ARGS+=("-DUSE_IBVERBS=ON")
 CMAKE_ARGS+=("-DUSE_LMDB=ON")
 CMAKE_ARGS+=("-DUSE_MPI=ON")
 CMAKE_ARGS+=("-DUSE_NCCL=ON")
 CMAKE_ARGS+=("-DUSE_OPENCV=ON")
-CMAKE_ARGS+=("-DUSE_REDIS=ON")
+
+# cudnn comes from a module location
+#-DCUDNN_ROOT_DIR=/public/apps/cudnn/v7.0/cuda/ \
 
 # Explicitly turn unused packages off to prevent cmake from trying to find
 # system libraries. If conda packages are built with any system libraries then
 # they will not be relocatable.
+CMAKE_ARGS+=("-DUSE_LEVELDB=OFF")
+CMAKE_ARGS+=("-DUSE_REDIS=OFF")
 CMAKE_ARGS+=("-DUSE_ROCKSDB=OFF")
 
 # Install under specified prefix
 CMAKE_ARGS+=("-DCMAKE_INSTALL_PREFIX=$PREFIX")
 CMAKE_ARGS+=("-DCMAKE_PREFIX_PATH=$PREFIX")
+
+# No rpaths will work for anaconda?
+# -DCMAKE_SKIP_RPATH=ON \
+
 
 mkdir -p build
 cd build
