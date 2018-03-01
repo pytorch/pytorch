6b914cd8faa185f5f31318af4af056d43c3dc79a
Peter Goldsborough <peter@goldsborough.me>
2018-03-01 12:15:35 -0800

    Fix warnings in jit (#5499)

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -7,7 +7,15 @@ namespace script {
 
 using ResolutionCallback = std::function<py::function(std::string)>;
 
-struct PythonResolver : public Resolver {
+// The visibility attribute is to avoid a warning about storing a field in the
+// struct that has a different visibility (from pybind) than the struct.
+#ifdef _WIN32
+#define VISIBILITY_HIDDEN
+#else
+#define VISIBILITY_HIDDEN __attribute__((visibility("hidden")))
+#endif
+
+struct VISIBILITY_HIDDEN PythonResolver : public Resolver {
   PythonResolver(ResolutionCallback rcb) : rcb(rcb) {}
 
   Node* resolveCall(SourceRange location, Node* n) const override {
