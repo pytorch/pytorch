2fb8ee2551ab993cc1eea2b733459c0fae412360
Peter Goldsborough <psag@fb.com>
2018-02-28 17:00:19 -0800

    Remove rogue UnderlyingType.h

--- aten/src/ATen/UnderlyingType.h
+++
@@ -1,7 +0,0 @@
-template<typename T>
-struct UnderlyingType;
-
-#define SPECIALIZE_UNDERLYING_TYPE(underlying, type, _) \
-template<> UnderlyingType<type> { using type = underlying; }
-AT_FORALL_SCALAR_TYPES(SPECIALIZE_UNDERLYING_TYPE)
-#undef SPECIALIZE_UNDERLYING_TYPE
