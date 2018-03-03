e51d0130531cb34e9cc44d1aaecfa7cfaf86d3b6
Myle Ott <myleott@fb.com>
2018-03-02 19:04:56 -0800

    WIP

--- torch/lib/THD/base/data_channels/DataChannelGloo.cpp
+++ torch/lib/THD/base/data_channels/DataChannelGloo.cpp
@@ -36,7 +36,10 @@
     case ::at::ScalarType::Float: func<float>(args); break;                   \
     case ::at::ScalarType::Double: func<double>(args); break;                 \
     case ::at::ScalarType::Half: func<gloo::float16>(args); break;            \
+    case ::at::ScalarType::Char: func<int8_t>(args); break;                   \
     case ::at::ScalarType::Byte: func<uint8_t>(args); break;                  \
+    case ::at::ScalarType::Int: func<int32_t>(args); break;                   \
+    case ::at::ScalarType::Long: func<int64_t>(args); break;                  \
     default:                                                                  \
       throw std::runtime_error("Invalid " + std::string(#func) + " function type"); \
   }
