b4b2f0d2ccdde17a3c565a70b3579aea8edeb122
Kutta Srinivasan <kutta@fb.com>
2018-03-05 16:43:37 -0800

    Work on fp16 conv op

--- caffe2/operators/conv_op_cache_cudnn_test.cc
+++ caffe2/operators/conv_op_cache_cudnn_test.cc
@@ -28,37 +28,50 @@ namespace caffe2 {
 TEST(AlgorithmsCacheTest, CachesCorrectly) {
   AlgorithmsCache<int> cache;
   int result = cache.getAlgorithm(
-      std::vector<TIndex>(1), std::vector<TIndex>(1), []() { return 5; });
+      std::vector<TIndex>(1), std::vector<TIndex>(1), 0, []() { return 5; });
   EXPECT_EQ(result, 5);
 
   int res2 = cache.getAlgorithm(
-      std::vector<TIndex>(1), std::vector<TIndex>(1), []() { return 10; });
+      std::vector<TIndex>(1), std::vector<TIndex>(1), 0, []() { return 10; });
 
   EXPECT_EQ(res2, 5);
-}
-
-TEST(AlgorithmsCacheTest, DoesNotCacheEmptyKeys) {
-  AlgorithmsCache<int> cache;
-  int result = cache.getAlgorithm(
-      std::vector<TIndex>(), std::vector<TIndex>(), []() { return 5; });
-  EXPECT_EQ(result, 5);
-
-  int res2 = cache.getAlgorithm(
-      std::vector<TIndex>(), std::vector<TIndex>(), []() { return 10; });
-
-  EXPECT_EQ(res2, 10);
 }
 
 TEST(AlgorithmsCacheTest, KeysDifferIfOneVectorIsEmpty) {
   AlgorithmsCache<int> cache;
   int result = cache.getAlgorithm(
-      std::vector<TIndex>(1, 10), std::vector<TIndex>(), []() { return 5; });
+      std::vector<TIndex>(1, 10), std::vector<TIndex>(), 0, []() { return 5; });
   EXPECT_EQ(result, 5);
 
   int res2 = cache.getAlgorithm(
-      std::vector<TIndex>(), std::vector<TIndex>(1, 10), []() { return 10; });
+      std::vector<TIndex>(), std::vector<TIndex>(1, 10), 0, []() {
+        return 10;
+      });
 
   EXPECT_EQ(res2, 10);
+}
+
+TEST(AlgorithmsCacheTest, KeysDifferIfFlagsAreDifferent) {
+  AlgorithmsCache<int> cache;
+  int result = cache.getAlgorithm(
+      std::vector<TIndex>{2, 3, 4}, std::vector<TIndex>{5, 6}, 123, []() {
+        return 5;
+      });
+  EXPECT_EQ(result, 5);
+
+  int res2 = cache.getAlgorithm(
+      std::vector<TIndex>{2, 3, 4}, std::vector<TIndex>{5, 6}, 456, []() {
+        return 10;
+      });
+
+  EXPECT_EQ(res2, 10);
+
+  int res3 = cache.getAlgorithm(
+      std::vector<TIndex>{2, 3, 4}, std::vector<TIndex>{5, 6}, 456, []() {
+        return 15;
+      });
+
+  EXPECT_EQ(res3, 10);
 }
 
 } // namespace caffe2
