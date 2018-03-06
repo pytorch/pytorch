5179d0a07a41fc23dc9d7c79b3470683c590066b
Teng Li <tengli@fb.com>
2018-03-05 17:23:24 -0800

    Docstring formatting and comment addressing

--- torch/distributed/launch.py
+++ torch/distributed/launch.py
@@ -1,81 +1,87 @@
-"""
+r"""
 torch.distributed.launch is a helper module that spawns up multiple distributed
 training processes on each of the training nodes.
 
-The utility can be used for single-node distributed training, in which
+The utility can be used for single-node distributed training, in which one or
+more processes per node will be spawned. The utility can be used for either
+CPU training or GPU training. If the utility is used for GPU training,
 each distributed process will be operating on a single GPU. This can achieve
 well-improved single-node training performance. It can also be used in
 multi-node distributed training, by spawning up multiple processes on each node
 for well-improved multi-node distributed training performance as well.
 This will especially be benefitial for systems with multiple Infiniband
-interfaces since all of them can be utilized for aggregated communication
-bandwidth.
+interfaces that have direct-GPU support, since all of them can be utilized for
+aggregated communication bandwidth.
 
-In both cases, this utilily will launch a given number of processes
-per node (nproc_per_node, which defaults to the number of GPUs on the node).
-This number needs to be less or euqal to the number of GPUs on the current
-system, and each process will be operating on a single GPU from GPU 0 to
-GPU nproc_per_node - 1.
+In both cases of single-node distributed training or multi-node distributed
+training, this utility will launch the given number of processes per node
+(``--nproc_per_node``). If used for GPU training, this number needs to be less
+or euqal to the number of GPUs on the current system (``nproc_per_node``),
+and each process will be operating on a single GPU from *GPU 0 to
+GPU (nproc_per_node - 1)*.
 
-How to use this module:
+**How to use this module:**
 
-    (1) Single Node multi-proc distributed training:
-        python -m torch.distributed.launch YOUR_TRAINING_SCRIPT.py (--arg1
-        --arg2 --arg3 and all other arguments of your training script)
+1. Single-Node multi-process distributed training
 
-    (2) Multi Node multi-proc distributed training: (e.g. two nodes)
+  ``python -m torch.distributed.launch YOUR_TRAINING_SCRIPT.py (--arg1
+  --arg2 --arg3 and all other arguments of your training script)``
 
-    NODE1: (IP: 192.168.1.1, and has a free port: 1234)
-        python -m torch.distributed.launch --nproc_per_node=NUM_GPUS_YOU_HAVE
-        --num_node=2 --rank_node=0 --master_addr="192.168.1.1"
+2. Multi-Node multi-process distributed training: (e.g. two nodes)
+
+    **Node 1**: *(IP: 192.168.1.1, and has a free port: 1234)*
+        ``python -m torch.distributed.launch --nproc_per_node=NUM_GPUS_YOU_HAVE
+        --nnodes=2 --node_rank=0 --master_addr="192.168.1.1"
         --master_port=1234 YOUR_TRAINING_SCRIPT.py (--arg1 --arg2 --arg3 and
-        all other arguments of your training script)
-    NODE2:
-        python -m torch.distributed.launch --nproc_per_node=NUM_GPUS_YOU_HAVE
-        --num_node=2 --rank_node=1 --master_addr="192.168.1.1"
+        all other arguments of your training script)``
+    **Node 2**:
+        ``python -m torch.distributed.launch --nproc_per_node=NUM_GPUS_YOU_HAVE
+        --nnodes=2 --node_rank=1 --master_addr="192.168.1.1"
         --master_port=1234 YOUR_TRAINING_SCRIPT.py (--arg1 --arg2 --arg3 and
-        all other arguments of your training script)
+        all other arguments of your training script)``
 
-    (3) To look up what optional arguments this module offers:
-        python -m torch.distributed.launch --help
+3. To look up what optional arguments this module offers:
 
-Important Notices:
+        >>> python -m torch.distributed.launch --help
 
-(1) This utilty and multi-process distributed (single node or
+**Important Notices:**
+
+1. This utilty and multi-process distributed (single-node or
 multi-node) GPU training currently only achieves the best performance using
 the NCCL distributed backend. Thus NCCL backend is the recommended backend to
 use for GPU training.
 
-(2) In your training program, you are supposed to parse the command-line
-argument: --device=DEVICE_TO_RUN, (which will be provided with this module) and
-ensure that your code only runs on this device in your training program by:
+2. In your training program, you are supposed to parse the command-line
+argument: ``--device=DEVICE_TO_RUN``, (which will be provided with this module)
+and ensure that your code only runs on this device in your training program by:
 
-    torch.cuda.set_device(arg.device)  # before your code runs
+
+    >>> torch.cuda.set_device(arg.device)  # before your code runs
 
     or
 
-    with torch.cuda.device(arg.device):
-        # your code to run
+    >>> with torch.cuda.device(arg.device):
+    >>>    # your code to run
 
-(3) In your training program, you are supposed to call the following function
+3. In your training program, you are supposed to call the following function
 at the beginning to start the distributed backend. You need to make sure that
-the init_method uses "env://", which is the only supported init_method by this
-module:
+the init_method uses ``env://``, which is the only supported ``init_method``
+by this module:
 
-    torch.distributed.init_process_group(backend='YOUR BACKEND',
-                                         "init_method='env://')
+    ``torch.distributed.init_process_group(backend='YOUR BACKEND',
+    init_method='env://')``
 
-(4) In your training program, you are supposed to convert your model to
-DistributedDataParallel module using the following function. Please ensure
-that device_ids argument is set to be the only GPU device id that your code
-will be operating on. In other words, the device_ids needs to be [args.device]
-in order to use this utility.
+4. In your training program, you can either use regular distributed functions
+or use DistributedDataParallel module. If you would like to use
+DistributedDataParallel module, here is how to configure it.
 
-    model = torch.nn.parallel.DistributedDataParallel(model,
-                                                      device_ids=[arg.device])
+    ``model = torch.nn.parallel.DistributedDataParallel(model,
+    device_ids=[arg.device], output_device=arg.device)``
 
-(5) For multi-node training, current we only support nodes with identical number
-of GPUs. In other words, the number of GPUs on each node needs to be the same.
+Please ensure that ``device_ids`` argument is set to be the only GPU device id
+that your code will be operating on. In other words, the ``device_ids`` needs
+to be ``[args.device]``, and ``output_device`` needs to be ``args.devices`` in
+order to use this utility
 
 """
 
@@ -98,13 +104,13 @@ def parse_args():
                                         "multiple distributed processes")
 
     # Optional arguments for the launch helper
-    parser.add_argument("--num_node", type=int, default=1,
+    parser.add_argument("--nnodes", type=int, default=1,
                         help="The number of nodes to use for distributed "
                              "training")
-    parser.add_argument("--rank_node", type=int, default=0,
+    parser.add_argument("--node_rank", type=int, default=0,
                         help="The rank of the node for multi-node distributed "
                              "training")
-    parser.add_argument("--nproc_per_node", type=int, default=-1,
+    parser.add_argument("--nproc_per_node", type=int, default=1,
                         help="The number of processes to launch on each node, "
                              "will default to the number of GPUs on your "
                              "system if not specified")
@@ -131,21 +137,9 @@ def parse_args():
 
 
 args = parse_args()
-num_gpus = torch.cuda.device_count()
-
-if args.nproc_per_node == -1:
-    args.nproc_per_node = num_gpus
-
-if args.nproc_per_node > num_gpus:
-    raise RuntimeError("Found: {} GPUs on host: {} with rank: {}, the "
-                       "number of processes per node cannot be greater "
-                       "than the number of GPUs availble on the host."
-                       .format(num_gpus,
-                               socket.gethostname(),
-                               args.rank_node))
 
 # world size in terms of number of processes
-dist_world_size = args.nproc_per_node * args.num_node
+dist_world_size = args.nproc_per_node * args.nnodes
 
 # set PyTorch distributed related environmental variables
 current_env = os.environ.copy()
@@ -157,7 +151,7 @@ processes = []
 
 for local_rank in range(0, args.nproc_per_node):
     # each process's rank
-    dist_rank = args.nproc_per_node * args.rank_node + local_rank
+    dist_rank = args.nproc_per_node * args.node_rank + local_rank
     current_env["RANK"] = str(dist_rank)
 
     # spawn the processes
