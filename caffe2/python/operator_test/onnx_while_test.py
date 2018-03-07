0c6e843028673eedce1c809bc36cdc1a8e8664b5
ilia-cher <30845429+ilia-cher@users.noreply.github.com>
2018-03-07 12:34:11 -0800

    [caffe2] Add scopes into ONNX While op (#2149)
    Summary:
    Executing loop's body in a separate workspace, using WorkspaceStack to
    support saving and reusing of workspaces
    
    Test Plan:
    python caffe2/python/operator_test/onnx_while_test.py
    
    Reviewers: caffe2-review, jamesreed
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- caffe2/python/operator_test/onnx_while_test.py
+++ caffe2/python/operator_test/onnx_while_test.py
@@ -25,13 +25,16 @@ from caffe2.python import core, workspace
 from hypothesis import given
 from caffe2.proto import caffe2_pb2
 
+
 class TestONNXWhile(hu.HypothesisTestCase):
     @given(
         condition=st.booleans(),
         max_trip_count=st.integers(0, 100),
+        save_scopes=st.booleans(),
         seed=st.integers(0, 65535),
         **hu.gcs_cpu_only)
-    def test_onnx_while_fibb(self, condition, max_trip_count, seed, gc, dc):
+    def test_onnx_while_fibb(
+            self, condition, max_trip_count, save_scopes, seed, gc, dc):
         np.random.seed(seed)
 
         # Create body net
@@ -71,6 +74,7 @@ class TestONNXWhile(hu.HypothesisTestCase):
             body=body_net,
             has_cond=True,
             has_trip_count=True,
+            save_scopes=save_scopes,
         )
 
         condition_arr = np.array(condition).astype(np.bool)
@@ -98,6 +102,7 @@ class TestONNXWhile(hu.HypothesisTestCase):
             [max_trip_count_arr, condition_arr, first_init, second_init],
             ref,
         )
+        self.assertFalse(workspace.HasBlob("cond_new"))
 
 if __name__ == "__main__":
     unittest.main()
