4913ac665499058219922d74a84b6731236bb35d
Marat Dukhan <marat@fb.com>
2018-03-06 17:11:04 -0800

    Bump minimum CMake version to 3.2
    CMake 3.2 is required to properly track dependencies in projects imported as ExternalProject_Add (BUILD_BYPRODUCTS parameter).
    Users on Ubuntu 14.04 LTS would need to install and use cmake3 package for configurations. Users of other popular distributions generally have a recent enough CMake package.

--- cmake/External/nccl.cmake
+++ cmake/External/nccl.cmake
@@ -20,32 +20,18 @@ if (NOT __NCCL_INCLUDED)
     set(NCCL_CXX_FLAGS ${CMAKE_CXX_FLAGS} ${NCCL_EXTRA_COMPILER_FLAGS})
     set(NCCL_C_FLAGS ${CMAKE_C_FLAGS} ${NCCL_EXTRA_COMPILER_FLAGS})
 
-    if(${CMAKE_VERSION} VERSION_LESS "3.2")
-      ExternalProject_Add(nccl_external
-        SOURCE_DIR ${nccl_PREFIX}
-        BUILD_IN_SOURCE 1
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND
-          make
-          "CXX=${CMAKE_CXX_COMPILER}"
-          "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
-          "NVCC=${CUDA_NVCC_EXECUTABLE}"
-        INSTALL_COMMAND ""
-        )
-    else()
-      ExternalProject_Add(nccl_external
-        SOURCE_DIR ${nccl_PREFIX}
-        BUILD_IN_SOURCE 1
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND
-          make
-          "CXX=${CMAKE_CXX_COMPILER}"
-          "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
-          "NVCC=${CUDA_NVCC_EXECUTABLE}"
-        BUILD_BYPRODUCTS "${nccl_PREFIX}/build/lib/libnccl_static.a"
-        INSTALL_COMMAND ""
-        )
-    endif()
+    ExternalProject_Add(nccl_external
+      SOURCE_DIR ${nccl_PREFIX}
+      BUILD_IN_SOURCE 1
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND
+        make
+        "CXX=${CMAKE_CXX_COMPILER}"
+        "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}"
+        "NVCC=${CUDA_NVCC_EXECUTABLE}"
+      BUILD_BYPRODUCTS "${nccl_PREFIX}/build/lib/libnccl_static.a"
+      INSTALL_COMMAND ""
+      )
 
     set(NCCL_FOUND TRUE)
     add_library(__caffe2_nccl INTERFACE)
