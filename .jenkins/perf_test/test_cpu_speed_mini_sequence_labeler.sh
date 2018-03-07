07e69c18cbc69f7f97a1fc99bcab5cdf2e894c3a
Will Feng <yf225@cornell.edu>
2018-03-07 15:07:30 -0500

    Update perf test baseline with every master commit (#5605)
    * Update perf test baseline with every master commit
    
    * Get perf test data from repo for local runs

--- .jenkins/perf_test/test_cpu_speed_mini_sequence_labeler.sh
+++ .jenkins/perf_test/test_cpu_speed_mini_sequence_labeler.sh
@@ -15,7 +15,7 @@ test_cpu_speed_mini_sequence_labeler () {
   cd scripts/mini_sequence_labeler
 
   SAMPLE_ARRAY=()
-  NUM_RUNS=20
+  NUM_RUNS=$1
 
   for (( i=1; i<=$NUM_RUNS; i++ )) do
     runtime=$(get_runtime_of_command "python main.py")
@@ -28,8 +28,10 @@ test_cpu_speed_mini_sequence_labeler () {
   echo "Runtime stats in seconds:"
   echo $stats
 
-  if [ "$1" == "compare_with_baseline" ]; then
-    python ../compare_with_baseline.py ${FUNCNAME[0]} "${stats}"
+  if [ "$2" == "compare_with_baseline" ]; then
+    python ../compare_with_baseline.py --test-name ${FUNCNAME[0]} --sample-stats "${stats}"
+  elif [ "$2" == "compare_and_update" ]; then
+    python ../compare_with_baseline.py --test-name ${FUNCNAME[0]} --sample-stats "${stats}" --update
   fi
 }
 
--- .jenkins/short-perf-test-gpu.sh
+++ .jenkins/short-perf-test-gpu.sh
@@ -11,6 +11,22 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 echo "Running GPU perf test for PyTorch..."
 
+# Get last master commit hash
+export PYTORCH_COMMIT_ID=$(git log --format="%H" -n 1)
+
+# Get baseline file from https://github.com/yf225/perf-tests
+if [ -f /var/lib/jenkins/host-workspace/perf_test_numbers_gpu.json ]; then
+    cp /var/lib/jenkins/host-workspace/perf_test_numbers_gpu.json perf_test_numbers_gpu.json
+else
+    curl https://raw.githubusercontent.com/yf225/perf-tests/master/perf_test_numbers_gpu.json -O
+fi
+
+if [[ "$GIT_COMMIT" == *origin/master* ]]; then
+    # Prepare new baseline file
+    cp perf_test_numbers_gpu.json new_perf_test_numbers_gpu.json
+    python update_commit_hash.py new_perf_test_numbers_gpu.json ${PYTORCH_COMMIT_ID}
+fi
+
 # Include tests
 . ./test_gpu_speed_mnist.sh
 . ./test_gpu_speed_word_language_model.sh
@@ -19,8 +35,26 @@ echo "Running GPU perf test for PyTorch..."
 . ./test_gpu_speed_mlstm.sh
 
 # Run tests
-run_test test_gpu_speed_mnist compare_with_baseline
-run_test test_gpu_speed_word_language_model compare_with_baseline
-run_test test_gpu_speed_cudnn_lstm compare_with_baseline
-run_test test_gpu_speed_lstm compare_with_baseline
-run_test test_gpu_speed_mlstm compare_with_baseline
+if [[ "$GIT_COMMIT" == *origin/master* ]]; then
+    run_test test_gpu_speed_mnist 20 compare_and_update
+    run_test test_gpu_speed_word_language_model 20 compare_and_update
+    run_test test_gpu_speed_cudnn_lstm 20 compare_and_update
+    run_test test_gpu_speed_lstm 20 compare_and_update
+    run_test test_gpu_speed_mlstm 20 compare_and_update
+else
+    run_test test_gpu_speed_mnist 20 compare_with_baseline
+    run_test test_gpu_speed_word_language_model 20 compare_with_baseline
+    run_test test_gpu_speed_cudnn_lstm 20 compare_with_baseline
+    run_test test_gpu_speed_lstm 20 compare_with_baseline
+    run_test test_gpu_speed_mlstm 20 compare_with_baseline
+fi
+
+if [[ "$GIT_COMMIT" == *origin/master* ]]; then
+    # Push new baseline file
+    cp new_perf_test_numbers_gpu.json /var/lib/jenkins/host-workspace/perf_test_numbers_gpu.json
+    cd /var/lib/jenkins/host-workspace
+    git config --global user.email jenkins@ci.pytorch.org
+    git config --global user.name Jenkins
+    git add perf_test_numbers_gpu.json
+    git commit -m "New GPU perf test baseline from ${PYTORCH_COMMIT_ID}"
+fi
