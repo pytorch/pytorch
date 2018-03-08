cebf44e96078cde81e3224c2ac369eaa52537a8e
Joseph Spisak <spisakjo@gmail.com>
2018-03-08 07:51:45 -0800

    Element-wise tests now use or seeded with hypothesis (#2181)
    * Updated all element-wise tests to use hypothesis testing or at least use hypothesis seeds
    
    * Updated tests to add seed to sqr function

--- caffe2/python/operator_test/elementwise_ops_test.py
+++ caffe2/python/operator_test/elementwise_ops_test.py
@@ -28,8 +28,9 @@ import numpy as np
 class TestElementwiseOps(hu.HypothesisTestCase):
 
     @given(n=st.integers(2, 10), m=st.integers(4, 6),
-           d=st.integers(2, 3), **hu.gcs)
-    def test_div(self, n, m, d, gc, dc):
+           d=st.integers(2, 3), seed=st.integers(0, 1000), **hu.gcs)
+    def test_div(self, n, m, d, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m, d).astype(np.float32)
         Y = np.random.rand(n, m, d).astype(np.float32) + 5.0
 
@@ -52,8 +53,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
         self.assertGradientChecks(
             gc, op, [X, Y], 0, [0], stepsize=1e-4, threshold=1e-2)
 
-    @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
-    def test_log(self, n, m, gc, dc):
+    @given(n=st.integers(5, 6), m=st.integers(4, 6), seed=st.integers(0, 1000), **hu.gcs)
+    def test_log(self, n, m, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m).astype(np.float32) + 1.0
 
         def log_op(X):
@@ -76,9 +78,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
             gc, op, [X], 0, [0], stepsize=1e-4, threshold=1e-2)
 
     @given(n=st.integers(2, 10), m=st.integers(4, 6),
-           d=st.integers(2, 3), **hu.gcs)
-    def test_powt(self, n, m, d, gc, dc):
-        np.random.seed(101)
+           d=st.integers(2, 3), seed=st.integers(0, 1000), **hu.gcs)
+    def test_powt(self, n, m, d, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m, d).astype(np.float32) + 1.0
         Y = np.random.rand(n, m, d).astype(np.float32) + 2.0
 
@@ -105,8 +107,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
                                    grad_reference=powt_grad)
 
 
-    @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
-    def test_sqr(self, n, m, gc, dc):
+    @given(n=st.integers(5, 6), m=st.integers(4, 6), seed=st.integers(0, 1000), **hu.gcs)
+    def test_sqr(self, n, m, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m).astype(np.float32)
 
         def sqr_op(X):
@@ -171,8 +174,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
         self.assertGradientChecks(
             gc, op, [X], 0, [0], stepsize=1e-4, threshold=1e-2)
 
-    @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
-    def test_swish(self, n, m, gc, dc):
+    @given(n=st.integers(5, 6), m=st.integers(4, 6), seed=st.integers(0, 1000), **hu.gcs)
+    def test_swish(self, n, m, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m).astype(np.float32)
 
         def swish(X):
@@ -194,9 +198,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
         self.assertGradientChecks(
             gc, op, [X], 0, [0], stepsize=1e-4, threshold=1e-2)
 
-    @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
-    def test_swish_gradient_inplace(self, n, m, gc, dc):
-
+    @given(n=st.integers(5, 6), m=st.integers(4, 6), seed=st.integers(0, 1000), **hu.gcs)
+    def test_swish_gradient_inplace(self, n, m, gc, dc, seed):
+        np.random.seed(seed)
         def swish(X):
             return [np.divide(X, (1. + np.exp(-X)))]
 
@@ -219,8 +223,9 @@ class TestElementwiseOps(hu.HypothesisTestCase):
             reference=swish_gradient,
         )
 
-    @given(n=st.integers(5, 6), m=st.integers(4, 6), **hu.gcs)
-    def test_sigmoid(self, n, m, gc, dc):
+    @given(n=st.integers(5, 6), m=st.integers(4, 6), seed=st.integers(0, 1000), **hu.gcs)
+    def test_sigmoid(self, n, m, gc, dc, seed):
+        np.random.seed(seed)
         X = np.random.rand(n, m).astype(np.float32)
 
         def sigmoid(X):
