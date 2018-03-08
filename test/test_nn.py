4c4a42b3f901d0dea21521b1a82ed6a1a029b1d3
li-roy <8813817+li-roy@users.noreply.github.com>
2018-03-08 14:54:24 -0800

    implement CosineEmbeddingLoss as a native function and add reduce arg (#5646)
    * implement CosineEmbeddingLoss as a native function and add reduce=True arg to it
    
    * fix flake8
    
    * address comments
    
    * add reference function to tests
    
    * fix flake8

--- test/test_nn.py
+++ test/test_nn.py
@@ -3784,6 +3784,24 @@ class TestNN(NNTestCase):
         input2 = Variable(torch.randn(4, 4), requires_grad=True)
         self.assertTrue(gradcheck(lambda x, y: F.pairwise_distance(x, y), (input1, input2)))
 
+    def test_cosine_embedding_loss_no_reduce(self):
+        input1 = Variable(torch.randn(15, 10), requires_grad=True)
+        input2 = Variable(torch.randn(15, 10), requires_grad=True)
+        target = Variable(torch.randn(15).sign())
+        self.assertTrue(gradcheck(lambda x, y, z: F.cosine_embedding_loss(
+            x, y, z), (input1, input2, target)))
+        self.assertEqual(F.cosine_embedding_loss(input1, input2, target),
+                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target))
+
+    def test_cosine_embedding_loss_margin_no_reduce(self):
+        input1 = Variable(torch.randn(15, 10), requires_grad=True)
+        input2 = Variable(torch.randn(15, 10), requires_grad=True)
+        target = Variable(torch.randn(15).sign())
+        self.assertTrue(gradcheck(lambda x, y, z: F.cosine_embedding_loss(
+            x, y, z, margin=0.5), (input1, input2, target)))
+        self.assertEqual(F.cosine_embedding_loss(input1, input2, target, margin=0.5),
+                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target, margin=0.5))
+
     def test_triplet_margin_loss(self):
         input1 = Variable(torch.randn(4, 4), requires_grad=True)
         input2 = Variable(torch.randn(4, 4), requires_grad=True)
