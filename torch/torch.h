518cc7f7e36f8e4dadf458d2e1863157628bf199
Peter Goldsborough <psag@fb.com>
2018-03-09 11:20:13 -0800

    Add note about experimental API

--- torch/torch.h
+++ torch/torch.h
@@ -7,6 +7,10 @@
 #include <torch/csrc/utils/pybind.h>
 
 namespace torch {
+
+// NOTE: This API is currently highly experimental and may change drastically
+// in the near future.
+
 /// Returns a `Type` object for the given backend (e.g. `at::kCPU`) and
 /// `ScalarType` (e.g. `at::kDouble`).
 at::Type& getType(at::Backend backend, at::ScalarType type);
