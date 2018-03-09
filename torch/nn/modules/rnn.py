b543041e2119f31e4770c02190635fce82a7a564
Shagun Sodhani <shagunsodhani@users.noreply.github.com>
2018-03-09 16:03:25 -0500

    Corrected a typo in LSTM documentation. Fixes #5661 (#5662)

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -319,7 +319,7 @@ class LSTM(RNNBase):
             \begin{array}{ll}
             i_t = \sigma(W_{ii} x_t + b_{ii} + W_{hi} h_{(t-1)} + b_{hi}) \\
             f_t = \sigma(W_{if} x_t + b_{if} + W_{hf} h_{(t-1)} + b_{hf}) \\
-            g_t = \tanh(W_{ig} x_t + b_{ig} + W_{hc} h_{(t-1)} + b_{hg}) \\
+            g_t = \tanh(W_{ig} x_t + b_{ig} + W_{hg} h_{(t-1)} + b_{hg}) \\
             o_t = \sigma(W_{io} x_t + b_{io} + W_{ho} h_{(t-1)} + b_{ho}) \\
             c_t = f_t c_{(t-1)} + i_t g_t \\
             h_t = o_t \tanh(c_t)
