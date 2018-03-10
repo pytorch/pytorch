7239395dfc7709ea6a3768e1a84e8f1457313c88
li-roy <8813817+li-roy@users.noreply.github.com>
2018-03-09 20:52:22 -0800

    small fixes to CosineEmbeddingLoss tests (#5681)
    * small fixes to CosineEmbeddingLoss tests
    
    * fix test

--- test/test_nn.py
+++ test/test_nn.py
@@ -3789,18 +3789,18 @@ class TestNN(NNTestCase):
         input2 = Variable(torch.randn(15, 10), requires_grad=True)
         target = Variable(torch.randn(15).sign())
         self.assertTrue(gradcheck(lambda x, y, z: F.cosine_embedding_loss(
-            x, y, z), (input1, input2, target)))
-        self.assertEqual(F.cosine_embedding_loss(input1, input2, target),
-                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target))
+            x, y, z, reduce=False), (input1, input2, target)))
+        self.assertEqual(F.cosine_embedding_loss(input1, input2, target, reduce=False),
+                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target, reduce=False))
 
     def test_cosine_embedding_loss_margin_no_reduce(self):
         input1 = Variable(torch.randn(15, 10), requires_grad=True)
         input2 = Variable(torch.randn(15, 10), requires_grad=True)
         target = Variable(torch.randn(15).sign())
         self.assertTrue(gradcheck(lambda x, y, z: F.cosine_embedding_loss(
-            x, y, z, margin=0.5), (input1, input2, target)))
-        self.assertEqual(F.cosine_embedding_loss(input1, input2, target, margin=0.5),
-                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target, margin=0.5))
+            x, y, z, margin=0.5, reduce=False), (input1, input2, target)))
+        self.assertEqual(F.cosine_embedding_loss(input1, input2, target, margin=0.5, reduce=False),
+                         loss_reference_fns['CosineEmbeddingLoss'](input1, input2, target, margin=0.5, reduce=False))
 
     def test_triplet_margin_loss(self):
         input1 = Variable(torch.randn(4, 4), requires_grad=True)
