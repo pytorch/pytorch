2ef64e78f17a07af1bee0a22e65ec1b78feb2879
Edward Z. Yang <ezyang@mit.edu>
2018-03-12 12:04:00 -0400

    Copyedit

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -4171,8 +4171,9 @@ Returns a 1-D tensor of size :math:`\left\lfloor \frac{end - start}{step} \right
 with values from the interval ``[start, end)`` taken with common difference
 :attr:`step` beginning from `start`.
 
-Results may be inconsistent when using a non-integer step.
-This is due to floating point rounding errors, but can be avoided by adding a small value to `end`.
+Note that non-integer steps are subject to floating point rounding errors when
+comparing against end; to avoid inconsistency, we advise adding a small epsilon to `eend`
+in such cases.
 
 .. math::
     out_{i+1} = out_{i} + \mathrm{step}
