edd138ba00b649ef03bf53c2ede08215c37a33f0
Qinqing Zheng <enosair@users.noreply.github.com>
2018-03-13 13:20:26 -0700

    [C2] Support optional lengths input to ReduceFront/Back operators (#2250)

--- caffe2/operators/reduction_front_back_ops.cu
+++ caffe2/operators/reduction_front_back_ops.cu
@@ -21,70 +21,90 @@
 namespace caffe2 {
 
 namespace {
-template <typename T>
+template <typename T, bool NORMALIZE>
 __global__ void columnwise_fill_kernel(
     const int rows,
     const int cols,
-    const float alpha,
     const T* dY,
+    const int* lengths,
     T* dX) {
   CUDA_1D_KERNEL_LOOP(i, rows * cols) {
-    dX[i] = dY[i % cols] * alpha;
+    int row = i / cols;
+    int col = i % cols;
+    if (lengths == nullptr) {
+      dX[i] = NORMALIZE ? dY[col] / rows : dY[col];
+    } else if (row < lengths[col]) {
+      dX[i] = NORMALIZE ? dY[col] / lengths[col] : dY[col];
+    } else {
+      dX[i] = 0;
+    }
   }
 }
 
-template <typename T>
+template <typename T, bool NORMALIZE>
 __global__ void rowwise_fill_kernel(
     const int rows,
     const int cols,
-    const float alpha,
     const T* dY,
+    const int* lengths,
     T* dX) {
   CUDA_1D_KERNEL_LOOP(i, rows * cols) {
-    dX[i] = dY[i / cols] * alpha;
+    int row = i / cols;
+    int col = i % cols;
+    if (lengths == nullptr) {
+      dX[i] = NORMALIZE ? dY[row] / cols : dY[row];
+    } else if (col < lengths[row]) {
+      dX[i] = NORMALIZE ? dY[row] / lengths[row] : dY[row];
+    } else {
+      dX[i] = 0;
+    }
   }
 }
 
-template <typename T>
+template <typename T, bool NORMALIZE>
 __global__ void rowwise_sum_kernel(
     const int rows,
     const int cols,
-    const float alpha,
     const T* data,
+    const int* lengths,
     T* out) {
   typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
   __shared__ typename BlockReduce::TempStorage temp_storage;
   for (int rowIndex = blockIdx.x; rowIndex < rows; rowIndex += gridDim.x) {
     T sum = 0;
     const int rowOffset = rowIndex * cols;
-    for (int colIndex = threadIdx.x; colIndex < cols; colIndex += blockDim.x) {
+    const int length = lengths == nullptr ? cols : lengths[rowIndex];
+    for (int colIndex = threadIdx.x; colIndex < length;
+         colIndex += blockDim.x) {
       sum += data[rowOffset + colIndex];
     }
     sum = BlockReduce(temp_storage).Reduce(sum, cub::Sum());
     if (threadIdx.x == 0) {
-      out[rowIndex] = alpha * sum;
+      out[rowIndex] = NORMALIZE ? sum / length : sum;
     }
     __syncthreads();
   }
 }
 
-template <typename T>
+template <typename T, bool NORMALIZE>
 __global__ void columnwise_sum_kernel(
     const int rows,
     const int cols,
-    const float alpha,
     const T* data,
+    const int* lengths,
     T* out) {
   typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
   __shared__ typename BlockReduce::TempStorage temp_storage;
   for (int colIndex = blockIdx.x; colIndex < cols; colIndex += gridDim.x) {
     T sum = 0;
-    for (int rowIndex = threadIdx.x; rowIndex < rows; rowIndex += blockDim.x) {
+    const int length = lengths == nullptr ? rows : lengths[colIndex];
+    for (int rowIndex = threadIdx.x; rowIndex < length;
+         rowIndex += blockDim.x) {
       sum += data[rowIndex * cols + colIndex];
     }
     sum = BlockReduce(temp_storage).Reduce(sum, cub::Sum());
     if (threadIdx.x == 0) {
-      out[colIndex] = alpha * sum;
+      out[colIndex] = NORMALIZE ? sum / length : sum;
     }
     __syncthreads();
   }
@@ -103,12 +123,13 @@ void SumReduceDimsOp<CUDAContext, true, false>::Compute(
     int rows,
     int cols,
     const T* in_data,
+    const int* lengths_data,
     T* out_data) {
-  columnwise_sum_kernel<T>
+  columnwise_sum_kernel<T, false>
       <<<std::min(cols, CAFFE_MAXIMUM_NUM_BLOCKS),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0, in_data, out_data);
+         context_.cuda_stream()>>>(rows, cols, in_data, lengths_data, out_data);
 }
 
 // ReduceBackSum: rowwise sum
@@ -118,12 +139,13 @@ void SumReduceDimsOp<CUDAContext, false, false>::Compute(
     int rows,
     int cols,
     const T* in_data,
+    const int* lengths_data,
     T* out_data) {
-  rowwise_sum_kernel<T>
+  rowwise_sum_kernel<T, false>
       <<<std::min(rows, CAFFE_MAXIMUM_NUM_BLOCKS),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0, in_data, out_data);
+         context_.cuda_stream()>>>(rows, cols, in_data, lengths_data, out_data);
 }
 
 // ReduceFrontSumGradient
@@ -133,12 +155,13 @@ void SumReduceDimsGradientOp<CUDAContext, true, false>::Compute(
     int rows,
     int cols,
     const T* dYdata,
+    const int* lengths_data,
     T* dXdata) {
-  columnwise_fill_kernel<T>
+  columnwise_fill_kernel<T, false>
       <<<CAFFE_GET_BLOCKS(rows * cols),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0, dYdata, dXdata);
+         context_.cuda_stream()>>>(rows, cols, dYdata, lengths_data, dXdata);
 }
 
 // ReduceBackSumGradient
@@ -148,12 +171,13 @@ void SumReduceDimsGradientOp<CUDAContext, false, false>::Compute(
     int rows,
     int cols,
     const T* dYdata,
+    const int* lengths_data,
     T* dXdata) {
-  rowwise_fill_kernel<T>
+  rowwise_fill_kernel<T, false>
       <<<CAFFE_GET_BLOCKS(rows * cols),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0, dYdata, dXdata);
+         context_.cuda_stream()>>>(rows, cols, dYdata, lengths_data, dXdata);
 }
 
 REGISTER_CUDA_OPERATOR(
@@ -181,12 +205,13 @@ void SumReduceDimsOp<CUDAContext, true, true>::Compute(
     int rows,
     int cols,
     const T* in_data,
+    const int* lengths_data,
     T* out_data) {
-  columnwise_sum_kernel<<<
-      std::min(cols, CAFFE_MAXIMUM_NUM_BLOCKS),
-      CAFFE_CUDA_NUM_THREADS,
-      0,
-      context_.cuda_stream()>>>(rows, cols, 1.0 / rows, in_data, out_data);
+  columnwise_sum_kernel<T, true>
+      <<<std::min(cols, CAFFE_MAXIMUM_NUM_BLOCKS),
+         CAFFE_CUDA_NUM_THREADS,
+         0,
+         context_.cuda_stream()>>>(rows, cols, in_data, lengths_data, out_data);
 }
 
 // ReduceBackMean: rowwise mean
@@ -196,12 +221,13 @@ void SumReduceDimsOp<CUDAContext, false, true>::Compute(
     int rows,
     int cols,
     const T* in_data,
+    const int* lengths_data,
     T* out_data) {
-  rowwise_sum_kernel<<<
-      std::min(rows, CAFFE_MAXIMUM_NUM_BLOCKS),
-      CAFFE_CUDA_NUM_THREADS,
-      0,
-      context_.cuda_stream()>>>(rows, cols, 1.0 / cols, in_data, out_data);
+  rowwise_sum_kernel<T, true>
+      <<<std::min(rows, CAFFE_MAXIMUM_NUM_BLOCKS),
+         CAFFE_CUDA_NUM_THREADS,
+         0,
+         context_.cuda_stream()>>>(rows, cols, in_data, lengths_data, out_data);
 }
 
 // ReduceFrontMeanGradient
@@ -211,12 +237,13 @@ void SumReduceDimsGradientOp<CUDAContext, true, true>::Compute(
     int rows,
     int cols,
     const T* dYdata,
+    const int* lengths_data,
     T* dXdata) {
-  columnwise_fill_kernel<T>
+  columnwise_fill_kernel<T, true>
       <<<CAFFE_GET_BLOCKS(rows * cols),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0 / rows, dYdata, dXdata);
+         context_.cuda_stream()>>>(rows, cols, dYdata, lengths_data, dXdata);
 }
 
 // ReduceBackMeanGradient
@@ -226,12 +253,13 @@ void SumReduceDimsGradientOp<CUDAContext, false, true>::Compute(
     int rows,
     int cols,
     const T* dYdata,
+    const int* lengths_data,
     T* dXdata) {
-  rowwise_fill_kernel<T>
+  rowwise_fill_kernel<T, true>
       <<<CAFFE_GET_BLOCKS(rows * cols),
          CAFFE_CUDA_NUM_THREADS,
          0,
-         context_.cuda_stream()>>>(rows, cols, 1.0 / cols, dYdata, dXdata);
+         context_.cuda_stream()>>>(rows, cols, dYdata, lengths_data, dXdata);
 }
 
 REGISTER_CUDA_OPERATOR(
@@ -258,12 +286,15 @@ __global__ void columnwise_max_kernel(
     const int rows,
     const int cols,
     const float* data,
+    const int* lengths,
     float* out) {
   typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
   __shared__ typename BlockReduce::TempStorage temp_storage;
   for (int colIndex = blockIdx.x; colIndex < cols; colIndex += gridDim.x) {
     float mx = FLT_MIN;
-    for (int rowIndex = threadIdx.x; rowIndex < rows; rowIndex += blockDim.x) {
+    const int length = lengths == nullptr ? rows : lengths[colIndex];
+    for (int rowIndex = threadIdx.x; rowIndex < length;
+         rowIndex += blockDim.x) {
       mx = max(mx, data[rowIndex * cols + colIndex]);
     }
     mx = BlockReduce(temp_storage).Reduce(mx, cub::Max());
@@ -278,12 +309,15 @@ __global__ void rowwise_max_kernel(
     const int rows,
     const int cols,
     const float* data,
+    const int* lengths,
     float* out) {
   typedef cub::BlockReduce<float, CAFFE_CUDA_NUM_THREADS> BlockReduce;
   __shared__ typename BlockReduce::TempStorage temp_storage;
   for (int rowIndex = blockIdx.x; rowIndex < rows; rowIndex += gridDim.x) {
     float mx = FLT_MIN;
-    for (int colIndex = threadIdx.x; colIndex < cols; colIndex += blockDim.x) {
+    const int length = lengths == nullptr ? cols : lengths[rowIndex];
+    for (int colIndex = threadIdx.x; colIndex < length;
+         colIndex += blockDim.x) {
       mx = max(mx, data[rowIndex * cols + colIndex]);
     }
     mx = BlockReduce(temp_storage).Reduce(mx, cub::Max());
@@ -300,10 +334,16 @@ __global__ void columnwise_max_grad_kernel(
     const float* dYdata,
     const float* Xdata,
     const float* Ydata,
+    const int* lengths,
     float* dXdata) {
   CUDA_1D_KERNEL_LOOP(i, rows * cols) {
     int col = i % cols;
-    dXdata[i] = (Xdata[i] == Ydata[col]) * dYdata[col];
+    int row = i / cols;
+    if (lengths != nullptr && row >= lengths[col]) {
+      dXdata[i] = 0.0f;
+    } else {
+      dXdata[i] = (Xdata[i] == Ydata[col]) * dYdata[col];
+    }
   }
 }
 
@@ -313,10 +353,16 @@ __global__ void rowwise_max_grad_kernel(
     const float* dYdata,
     const float* Xdata,
     const float* Ydata,
+    const int* lengths,
     float* dXdata) {
   CUDA_1D_KERNEL_LOOP(i, rows * cols) {
+    int col = i % cols;
     int row = i / cols;
-    dXdata[i] = (Xdata[i] == Ydata[row]) * dYdata[row];
+    if (lengths != nullptr && col >= lengths[row]) {
+      dXdata[i] = 0.0f;
+    } else {
+      dXdata[i] = (Xdata[i] == Ydata[row]) * dYdata[row];
+    }
   }
 }
 } // anonymous namespace
@@ -327,12 +373,13 @@ void MaxReduceDimsOp<float, CUDAContext, true>::Compute(
     int rows,
     int cols,
     const float* data,
+    const int32_t* lengths_data,
     float* out_data) {
   columnwise_max_kernel<<<
       std::min(cols, CAFFE_MAXIMUM_NUM_BLOCKS),
       CAFFE_CUDA_NUM_THREADS,
       0,
-      context_.cuda_stream()>>>(rows, cols, data, out_data);
+      context_.cuda_stream()>>>(rows, cols, data, lengths_data, out_data);
 }
 
 // ReduceBackMax
@@ -341,12 +388,13 @@ void MaxReduceDimsOp<float, CUDAContext, false>::Compute(
     int rows,
     int cols,
     const float* data,
+    const int32_t* lengths_data,
     float* out_data) {
   rowwise_max_kernel<<<
       std::min(rows, CAFFE_MAXIMUM_NUM_BLOCKS),
       CAFFE_CUDA_NUM_THREADS,
       0,
-      context_.cuda_stream()>>>(rows, cols, data, out_data);
+      context_.cuda_stream()>>>(rows, cols, data, lengths_data, out_data);
 }
 
 // ReduceFrontMaxGradient
@@ -357,12 +405,14 @@ void MaxReduceDimsGradientOp<float, CUDAContext, true>::Compute(
     const float* dYdata,
     const float* Xdata,
     const float* Ydata,
+    const int32_t* lengths_data,
     float* dXdata) {
   columnwise_max_grad_kernel<<<
       CAFFE_GET_BLOCKS(rows * cols),
       CAFFE_CUDA_NUM_THREADS,
       0,
-      context_.cuda_stream()>>>(rows, cols, dYdata, Xdata, Ydata, dXdata);
+      context_.cuda_stream()>>>(
+      rows, cols, dYdata, Xdata, Ydata, lengths_data, dXdata);
 }
 
 // ReduceBackMaxGradient
@@ -373,12 +423,14 @@ void MaxReduceDimsGradientOp<float, CUDAContext, false>::Compute(
     const float* dYdata,
     const float* Xdata,
     const float* Ydata,
+    const int* lengths_data,
     float* dXdata) {
   rowwise_max_grad_kernel<<<
       CAFFE_GET_BLOCKS(rows * cols),
       CAFFE_CUDA_NUM_THREADS,
       0,
-      context_.cuda_stream()>>>(rows, cols, dYdata, Xdata, Ydata, dXdata);
+      context_.cuda_stream()>>>(
+      rows, cols, dYdata, Xdata, Ydata, lengths_data, dXdata);
 }
 
 REGISTER_CUDA_OPERATOR(
