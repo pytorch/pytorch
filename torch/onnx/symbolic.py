9632054dcdf6dc7d55f9211bd5a9dd19cfa55fb7
Yu-An Chen <yuandychen@gmail.com>
2018-03-13 16:38:37 +0800

    fix the number of spaces

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -424,10 +424,10 @@ def upsample_bilinear2d(g, input, output_size):
     w_scale = float(output_size[-1])/input.type().sizes()[-1]
     h_scale = float(output_size[-2])/input.type().sizes()[-2]
     return g.op("Upsample", input, width_scale_f=w_scale,
-                height_scale_f = h_scale, mode_s="bilinear")
+                height_scale_f=h_scale, mode_s="bilinear")
 
 def stack(g, *tensors, dim=0):
-    return g.op("ATen", *tensors, operator_s = "stack", dim_i=dim)
+    return g.op("ATen", *tensors, operator_s="stack", dim_i=dim)
 
 def nonzero(g, input):
     return g.op("ATen", input, operator_s="nonzero")
