e73169b9649610126e8801db11bc44745c80c576
Peter Goldsborough <psag@fb.com>
2018-03-14 00:16:31 -0700

    s/log/print_to_stderr

--- test/run_test.py
+++ test/run_test.py
@@ -50,10 +50,10 @@ DISTRIBUTED_TESTS_CONFIG = {
 }
 
 
-def log(message):
+def print_to_stderr(message):
     # Print to stderr because test output also goes to stderr. This ensures
     # synchronization between the two output sources.
-    print(message, file=sys.stdout)
+    print(message, file=sys.stderr)
 
 
 def shell(command, cwd):
@@ -94,7 +94,8 @@ def test_cpp_extensions(python, test_module, test_directory, options):
 def test_distributed(python, test_module, test_directory, options):
     mpi_available = subprocess.call('command -v mpiexec', shell=True) == 0
     if options.verbose and not mpi_available:
-        log('MPI not available -- MPI backend tests will be skipped')
+        print_to_stderr(
+            'MPI not available -- MPI backend tests will be skipped')
     for backend, env_vars in DISTRIBUTED_TESTS_CONFIG.items():
         if backend == 'mpi' and not mpi_available:
             continue
@@ -102,8 +103,9 @@ def test_distributed(python, test_module, test_directory, options):
             tmp_dir = tempfile.mkdtemp()
             with_init_message = ' with file init_method' if with_init else ''
             if options.verbose:
-                log('Running distributed tests for the {} backend{}'.format(
-                    backend, with_init_message))
+                print_to_stderr(
+                    'Running distributed tests for the {} backend{}'.format(
+                        backend, with_init_message))
             os.environ['TEMP_DIR'] = tmp_dir
             os.environ['BACKEND'] = backend
             os.environ['INIT_METHOD'] = 'env://'
@@ -207,7 +209,7 @@ def get_selected_tests(options):
     if sys.platform == 'win32' and not options.ignore_win_blacklist:
         for test in WINDOWS_BLACKLIST:
             if test in selected_tests:
-                log('Excluding {} on Windows'.format(test))
+                print_to_stderr('Excluding {} on Windows'.format(test))
                 selected_tests.remove(test)
 
     return selected_tests
@@ -219,14 +221,14 @@ def main():
     test_directory = os.path.dirname(os.path.abspath(__file__))
     selected_tests = get_selected_tests(options)
     if options.verbose:
-        log('Selected tests: {}'.format(', '.join(selected_tests)))
+        print_to_stderr('Selected tests: {}'.format(', '.join(selected_tests)))
 
     if options.coverage:
         shell('coverage erase')
 
     for test in selected_tests:
         test_module = 'test_{}.py'.format(test)
-        log('Running {} ...'.format(test_module))
+        print_to_stderr('Running {} ...'.format(test_module))
         handler = CUSTOM_HANDLERS.get(test, run_test)
         if not handler(python, test_module, test_directory, options):
             raise RuntimeError('{} failed!'.format(test_module))
