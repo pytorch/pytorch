9ede25a53def3d68b355cbf25f3f96bb85be4930
Roy Li <royboy@fb.com>
2018-03-16 12:23:05 -0700

    address comments

--- aten/src/ATen/native/Linear.cpp
+++ aten/src/ATen/native/Linear.cpp
@@ -5,26 +5,28 @@
 namespace at { namespace native {
 
 Tensor bilinear(const Tensor& input1, const Tensor& input2, const Tensor& weight, const Tensor& bias) {
-  if (input1.dim() != input2.dim()) {
-    throw std::runtime_error("Inputs should have the same number of dimensions");
-  }
+  AT_ASSERT(input1.dim() == input2.dim(), "bilinear(): input dimensions do not match: got %d and %d",
+            input1.dim(), input2.dim());
   for (int64_t i = 0; i < input1.dim() - 1; i++) {
-    if (input1.size(i) != input2.size(i)) {
-      throw std::runtime_error("Batch dimensions of inputs do not match");
-    }
+    AT_ASSERT(input1.size(i) == input2.size(i),
+              "bilinear(): input batch dimensions do not match at dim %d: got %d and %d",
+              i, input1.size(i), input2.size(i));
   }
-  if (input1.size(input1.dim() - 1) != weight.size(1) || input2.size(input2.dim() - 1) != weight.size(2)) {
-    throw std::runtime_error("Input sizes do not match weight sizes");
-  }
-  if (bias.defined() && bias.size(0) != weight.size(0)) {
-    throw std::runtime_error("Bias sizes does not match weight size");
-  }
+  AT_ASSERT(input1.size(input1.dim() - 1) == weight.size(1),
+            "bilinear(): input1 size does not match weight size: got %d but expected %d",
+            input1.size(input1.dim() - 1), weight.size(1));
+  AT_ASSERT(input2.size(input2.dim() - 1) == weight.size(2),
+            "bilinear(): input2 size does not match weight size: got %d but expected %d",
+            input2.size(input2.dim() - 1), weight.size(2));
+  AT_ASSERT(bias.defined() && bias.size(0) == weight.size(0),
+            "bilinear(): bias size does not match weight size: got %d but expected %d",
+            bias.size(0), weight.size(0));
 
   auto b_input1 = input1.unsqueeze(-2).unsqueeze(-2);
   auto b_input2 = input2.unsqueeze(-2).unsqueeze(-1);
 
   auto output = at::matmul(at::matmul(b_input1, weight), b_input2);
-  output = output.squeeze(-1).squeeze(-2).sum(-1);
+  output = output.squeeze(-1).squeeze(-1);
   if (bias.defined()) {
     output = output + bias;
   }
--- torch/nn/modules/linear.py
+++ torch/nn/modules/linear.py
@@ -73,10 +73,10 @@ class Bilinear(Module):
             Default: ``True``
 
     Shape:
-        - Input: :math:`(N, \text{in1_features})`, :math:`(N, \text{in2_features})`
+        - Input: :math:`(N, *, \text{in1_features})`, :math:`(N, *, \text{in2_features})`
           where :math:`*` means any number of additional dimensions. All but the last
           dimension of the inputs should be the same.
-        - Output: :math:`(N, \text{out_features})` where all but the last dimension
+        - Output: :math:`(N, *, \text{out_features})` where all but the last dimension
           are the same shape as the input.
 
     Attributes:
