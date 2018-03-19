8ccf21b423f7f01e270b0a9b1b899028acb05ea3
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2018-03-19 01:38:00 -0400

    Update docs

--- torch/distributions/logistic_normal.py
+++ torch/distributions/logistic_normal.py
@@ -3,28 +3,31 @@ from torch.distributions import constraints
 from torch.distributions.normal import Normal
 from torch.distributions.transformed_distribution import TransformedDistribution
 from torch.distributions.transforms import ComposeTransform, ExpTransform, StickBreakingTransform
-from torch.distributions.utils import _sum_rightmost
 
 
 class LogisticNormal(TransformedDistribution):
     r"""
-    Creates a logistic-normal distribution parameterized by
-    `mean` and `std` where::
+    Creates a logistic-normal distribution parameterized by `loc` and `scale`
+    that define the base `Normal` distribution transformed with the
+    `StickBreakingTransform` such that::
 
         X ~ LogisticNormal(loc, scale)
-        Y = log(X / (1 - X)) ~ Normal(loc, scale)
+        Y = log(X / (1 - X.cumsum(-1)))[..., :-1] ~ Normal(loc, scale)
 
     Example::
 
-        >>> m = LogisticNormal(torch.Tensor([0.0]), torch.Tensor([1.0]))
-        >>> m.sample()  # logistic-normal distributed with mean=0 and stddev=1
-         0.4655
-         0.5345
-        [torch.FloatTensor of size (2,)]
+        >>> m = LogisticNormal(torch.Tensor([0.0] * 3), torch.Tensor([1.0] * 3))
+        >>> m.sample()  # logistic-normal distributed with mean=(0, 0, 0) and
+                        # stddev=(1, 1, 1) of the base Normal distribution
+         0.4163
+         0.1386
+         0.3539
+         0.0912
+        [torch.FloatTensor of size (4,)]
 
     Args:
-        loc (float or Tensor): mean
-        scale (float or Tensor): standard deviation
+        loc (float or Tensor): mean of the base distribution
+        scale (float or Tensor): standard deviation of the base distribution
     """
     params = {'loc': constraints.real, 'scale': constraints.positive}
     support = constraints.simplex
