6f80023c29e0fb55f46a32c4931bc5d4ba749846
Luca Antiga <luca.antiga@orobix.com>
2018-03-19 21:09:43 +0100

    Port ATen and JIT C++ tests to Catch2 (#5788)
    This PR addresses #5648. In particular, following the discussion at #5648:
    
    - it adds Catch as a submodule (https://github.com/catchorg/Catch2) in torch/aten/utils
    - it ports all ATen tests to Catch
    - it ports torch/csrc/jit/test_jit.cpp to Catch (libtorch only, Python build is unaffected)

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -74,7 +74,7 @@ fi
 
 if [[ "$BUILD_ENVIRONMENT" != *cuda* ]]; then
   echo "Testing ATen"
-  time tools/run_aten_tests.sh
+  ( unset LD_PRELOAD; time tools/run_aten_tests.sh )
 fi
 
 # Test C FFI plugins
@@ -99,7 +99,8 @@ fi
 if [[ "$BUILD_ENVIRONMENT" == *pytorch-linux-xenial-cuda9-cudnn7-py3 ]] || \
    [[ "$BUILD_ENVIRONMENT" == *pytorch-linux-trusty-py3.6-gcc7.2 ]]; then
   echo "Building libtorch with NO_PYTHON"
-  pushd tools/cpp_build || exit 1
-  bash build_all.sh
+  LIBTORCH_INSTALL_PREFIX=`pwd`/../libtorch
+  pushd tools/cpp_build
+  bash build_all.sh "$LIBTORCH_INSTALL_PREFIX" || exit 1
   popd
 fi
