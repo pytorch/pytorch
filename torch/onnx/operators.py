b6da80b7e52f372f6297e72d637383b31a948520
Junjie Bai <bai@in.tum.de>
2018-03-19 16:47:11 -0700

    Add doc for torch/onnx/operators.py

--- torch/onnx/operators.py
+++ torch/onnx/operators.py
@@ -1,3 +1,7 @@
+r"""
+This file provides a location for operators that help exporting models via onnx. E.g. shape_as_tensor and reshape_from_tensor_shape are to make all dynamic sizes operations traceble.
+"""
+
 import torch
 import torch.onnx
 
