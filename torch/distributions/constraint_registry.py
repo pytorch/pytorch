03148cf2af42735cb7b3b291695c776bf9e1beb4
Fritz Obermeyer <fritzo@uber.com>
2018-03-20 12:14:17 -0700

    Updates per review

--- torch/distributions/constraint_registry.py
+++ torch/distributions/constraint_registry.py
@@ -184,9 +184,10 @@ def _transform_to_less_than(constraint):
 @transform_to.register(constraints.interval)
 def _transform_to_interval(constraint):
     # Handle the special case of the unit interval.
-    if isinstance(constraint.lower_bound, numbers.Number) and constraint.lower_bound == 0:
-        if isinstance(constraint.upper_bound, numbers.Number) and constraint.upper_bound == 1:
-            return transforms.SigmoidTransform()
+    lower_is_0 = isinstance(constraint.lower_bound, numbers.Number) and constraint.lower_bound == 0
+    upper_is_1 = isinstance(constraint.upper_bound, numbers.Number) and constraint.upper_bound == 1
+    if lower_is_0 and upper_is_1:
+        return transforms.SigmoidTransform()
 
     loc = constraint.lower_bound
     scale = constraint.upper_bound - constraint.lower_bound
