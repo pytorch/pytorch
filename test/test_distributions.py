39997f98d637ab4d5e397b0fb490abcd0fd5c26b
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-03-20 14:00:37 -0700

    [distributions] Support pickling of constraint objects (#5910)

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -35,21 +35,21 @@ from torch.autograd import Variable, grad, gradcheck, variable
 from torch.distributions import (Bernoulli, Beta, Binomial, Categorical,
                                  Cauchy, Chi2, Dirichlet, Distribution,
                                  Exponential, ExponentialFamily,
-                                 FisherSnedecor, Gamma, Geometric,
-                                 Gumbel, Laplace, LogNormal, Multinomial, MultivariateNormal,
-                                 Normal, OneHotCategorical, Pareto, Poisson,
-                                 RelaxedBernoulli, RelaxedOneHotCategorical, StudentT,
+                                 FisherSnedecor, Gamma, Geometric, Gumbel,
+                                 Laplace, LogNormal, Multinomial,
+                                 MultivariateNormal, Normal, OneHotCategorical,
+                                 Pareto, Poisson, RelaxedBernoulli,
+                                 RelaxedOneHotCategorical, StudentT,
                                  TransformedDistribution, Uniform, constraints,
                                  kl_divergence)
-from torch.distributions.kl import _kl_expfamily_expfamily
 from torch.distributions.constraint_registry import biject_to, transform_to
 from torch.distributions.constraints import Constraint, is_dependent
 from torch.distributions.dirichlet import _Dirichlet_backward
+from torch.distributions.kl import _kl_expfamily_expfamily
 from torch.distributions.transforms import (AbsTransform, AffineTransform,
-                                            BoltzmannTransform,
                                             ComposeTransform, ExpTransform,
                                             LowerCholeskyTransform,
-                                            SigmoidTransform,
+                                            SigmoidTransform, SoftmaxTransform,
                                             StickBreakingTransform,
                                             identity_transform)
 from torch.distributions.utils import _finfo, probs_to_logits, softmax
@@ -2944,7 +2944,7 @@ class TestTransforms(TestCase):
                 AffineTransform(variable(torch.Tensor(4, 5).normal_()),
                                 variable(torch.Tensor(4, 5).normal_()),
                                 cache_size=cache_size),
-                BoltzmannTransform(cache_size=cache_size),
+                SoftmaxTransform(cache_size=cache_size),
                 StickBreakingTransform(cache_size=cache_size),
                 LowerCholeskyTransform(cache_size=cache_size),
                 ComposeTransform([
@@ -3089,7 +3089,7 @@ class TestTransforms(TestCase):
 
     def test_transform_shapes(self):
         transform0 = ExpTransform()
-        transform1 = BoltzmannTransform()
+        transform1 = SoftmaxTransform()
         transform2 = LowerCholeskyTransform()
 
         self.assertEqual(transform0.event_dim, 0)
@@ -3101,7 +3101,7 @@ class TestTransforms(TestCase):
 
     def test_transformed_distribution_shapes(self):
         transform0 = ExpTransform()
-        transform1 = BoltzmannTransform()
+        transform1 = SoftmaxTransform()
         transform2 = LowerCholeskyTransform()
         base_dist0 = Normal(variable(torch.zeros(4, 4)), variable(torch.ones(4, 4)))
         base_dist1 = Dirichlet(variable(torch.ones(4, 4)))
