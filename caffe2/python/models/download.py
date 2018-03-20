efe1c2bd13824eec40be7ed707bf32ca616875f1
Yinghai Lu <yinghai@fb.com>
2018-03-20 08:52:54 -0700

    hypen as a valid part of model names (#2312)

--- caffe2/python/models/download.py
+++ caffe2/python/models/download.py
@@ -154,7 +154,7 @@ def validModelName(name):
     invalid_names = ['__init__']
     if name in invalid_names:
         return False
-    if not re.match("^[/0-9a-zA-Z_]+$", name):
+    if not re.match("^[/0-9a-zA-Z_-]+$", name):
         return False
     return True
 
