e9f144b3e84f03d6a507faac40bdfce58b68c1fb
cpuhrsch <cpuhrsch@googlemail.com>
2018-03-22 01:14:56 -0400

    parallel_for_2d fix and guarding avx/avx2 compilation (#5926)
    Fix for #5921.
    
    I'm adding support compilers that don't support -mavx -mavx2 by revisiting the dispatch code.

---
+++ aten/src/ATen/test/test_parallel.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "ATen/ATen.h"
+#include "ATen/DLConvertor.h"
+
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include "test_seed.h"
+
+using namespace at;
+
+TEST_CASE( "parallel", "[cpu]" ) {
+
+  manual_seed(123);
+  set_num_threads(1);
+
+  Tensor a = rand(CPU(at::kFloat), {1,3});
+  a[0][0] = 1;
+  a[0][1] = 0;
+  a[0][2] = 0;
+  Tensor as = rand(CPU(at::kFloat), {3});
+  as[0] = 1;
+  as[1] = 0;
+  as[2] = 0;
+  REQUIRE(a.sum(0).equal(as));
+}
+
