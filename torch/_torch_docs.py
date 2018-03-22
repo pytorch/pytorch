ae2c24e3b234376490fccea5f856c52744d1e8c1
su <sulixinict@gmail.com>
2018-03-22 19:57:59 +0800

    Update _torch_docs.py

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -3158,7 +3158,7 @@ of tensor :attr:`input`.
 Indices are ordered from left to right according to when each was sampled
 (first samples are placed in first column).
 
-If :attr:`input` is a vector, :attr:`out` is a vector of size `num_samples`.
+If :attr:`input` is a vector, :attr:`out` is a vector of size `num\_samples`.
 
 If :attr:`input` is a matrix with `m` rows, :attr:`out` is an matrix of shape
 :math:`(m \times num\_samples)`.
