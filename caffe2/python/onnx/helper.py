0e94de45df56fb5ef2612dc0c644a258f43c14ec
Yinghai Lu <yinghai@fb.com>
2018-03-22 11:59:03 -0700

    Remove Python onnx-caffe2 conversion code (#2362)
    * WIP
    
    * Remove Python onnx-caffe2 onversion code
    
    * Fix build
    
    * Comments
    
    * Add comments
    
    * Fix typo in comments

--- caffe2/python/onnx/helper.py
+++ caffe2/python/onnx/helper.py
@@ -24,6 +24,7 @@ from caffe2.proto import caffe2_pb2
 from onnx.backend.base import namedtupledict
 
 from caffe2.python.onnx.workspace import Workspace
+import caffe2.python._import_c_extension as C
 
 import io
 import logging
@@ -33,27 +34,12 @@ import time
 log = logging.getLogger(__name__)
 
 
-class _DummyNameFactory(object):
-    used_names = set()
-    counter = 0
-
-    @classmethod
-    def dummy_name(cls, used_names=None):
-        if used_names is not None:
-            cls.used_names.clear()
-            cls.used_names.update(used_names)
-            cls.counter = 0
-            return None
-        else:
-            while True:
-                name = 'OC2_DUMMY_{}'.format(cls.counter)
-                cls.counter += 1
-                if name not in cls.used_names:
-                    cls.used_names.add(name)
-                    return name
-
-dummy_name = _DummyNameFactory.dummy_name
-
+def dummy_name(used_names=None):
+    if used_names is None:
+        return C.new_dummy_name()
+    else:
+        C.reset_dummy_name(set(used_names))
+        return None
 
 def c2_native_run_op(op_def, inputs):
     ws = Workspace()
