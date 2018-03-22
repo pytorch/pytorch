f92c402a0e190da5d18eabb6682210fdff18cb43
Junjie Bai <bai@in.tum.de>
2018-03-22 00:34:34 -0700

    Specify outputs number in embedding_bag onnx export

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -247,6 +247,7 @@ def embedding_bag(g,
                 indices,
                 offsets,
                 operator_s="embedding_bag",
+                outputs=3,
                 scale_grad_by_freq_i=scale_grad_by_freq,
                 mode_i=mode,
                 sparse_i=sparse)
