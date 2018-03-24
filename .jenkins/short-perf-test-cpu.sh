f99787cd5de72a1fc49df9ff63087b98f1bafe4e
Will Feng <willfeng@fb.com>
2018-03-23 22:51:25 -0700

    Fixed issues

--- .jenkins/short-perf-test-cpu.sh
+++ .jenkins/short-perf-test-cpu.sh
@@ -9,20 +9,27 @@ echo "Running CPU perf test for PyTorch..."
 
 pip install awscli
 
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+# Set multipart_threshold to be sufficiently high, so that `aws s3 cp` is not a multipart read
+# More info at https://github.com/aws/aws-cli/issues/2321
+aws configure set default.s3.multipart_threshold 5GB
 
-if [[ "$COMMIT_SOURCE" == *master* ]]; then
+if [[ "$COMMIT_SOURCE" == master ]]; then
     # Get current master commit hash
     export MASTER_COMMIT_ID=$(git log --format="%H" -n 1)
 fi
 
-# Get baseline file from ossci-perf-test S3 bucket
-aws s3 cp s3://ossci-perf-test/pytorch/cpu_runtime/LATEST_TESTED_COMMIT LATEST_TESTED_COMMIT
-export LATEST_TESTED_COMMIT="$(cat LATEST_TESTED_COMMIT)"
+# Find the master commit to test against
+IFS=$'\n'
+master_commit_ids=($(git rev-list HEAD))
+for commit_id in "${master_commit_ids[@]}"; do
+    if aws s3 ls s3://ossci-perf-test/pytorch/cpu_runtime/${commit_id}.json; then
+        LATEST_TESTED_COMMIT=${commit_id}
+        break
+    fi
+done
 aws s3 cp s3://ossci-perf-test/pytorch/cpu_runtime/${LATEST_TESTED_COMMIT}.json cpu_runtime.json
 
-if [[ "$COMMIT_SOURCE" == *master* ]]; then
+if [[ "$COMMIT_SOURCE" == master ]]; then
     # Prepare new baseline file
     cp cpu_runtime.json new_cpu_runtime.json
     python update_commit_hash.py new_cpu_runtime.json ${MASTER_COMMIT_ID}
@@ -33,7 +40,7 @@ fi
 . ./test_cpu_speed_mnist.sh
 
 # Run tests
-if [[ "$COMMIT_SOURCE" == *master* ]]; then
+if [[ "$COMMIT_SOURCE" == master ]]; then
     run_test test_cpu_speed_mini_sequence_labeler 20 compare_and_update
     run_test test_cpu_speed_mnist 20 compare_and_update
 else
@@ -41,14 +48,8 @@ else
     run_test test_cpu_speed_mnist 20 compare_with_baseline
 fi
 
-if [[ "$COMMIT_SOURCE" == *master* ]]; then
+if [[ "$COMMIT_SOURCE" == master ]]; then
+    # This could cause race condition if we are testing the same master commit twice,
+    # but the chance of them executing this line at the same time is low.
     aws s3 cp new_cpu_runtime.json s3://ossci-perf-test/pytorch/cpu_runtime/${MASTER_COMMIT_ID}.json --acl public-read
-
-    # If current commit is newer, then we put it in LATEST_TESTED_COMMIT
-    aws s3 cp s3://ossci-perf-test/pytorch/cpu_runtime/LATEST_TESTED_COMMIT LATEST_TESTED_COMMIT
-    export LATEST_TESTED_COMMIT="$(cat LATEST_TESTED_COMMIT)"
-    if ! git merge-base --is-ancestor ${MASTER_COMMIT_ID} ${LATEST_TESTED_COMMIT}; then
-        echo "${MASTER_COMMIT_ID}" > LATEST_TESTED_COMMIT
-        aws s3 cp LATEST_TESTED_COMMIT s3://ossci-perf-test/pytorch/cpu_runtime/LATEST_TESTED_COMMIT --acl public-read
-    fi
 fi
