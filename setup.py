a172447b843704cac4995b953665104e851e7353
peter <peterghost86@gmail.com>
2018-03-26 12:55:21 +0800

    Add missing options

--- setup.py
+++ setup.py
@@ -464,7 +464,7 @@ extra_link_args = []
 if IS_WINDOWS:
     extra_compile_args = ['/Z7', '/EHa', '/DNOMINMAX', '/wd4267', '/wd4251', '/wd4522',
                           '/wd4522', '/wd4838', '/wd4305', '/wd4244', '/wd4190',
-                          '/wd4101', '/wd4996'
+                          '/wd4101', '/wd4996', '/wd4275'
                           # /Z7 turns on symbolic debugging information in .obj files
                           # /EHa is about native C++ catch support for asynchronous
                           # structured exception handling (SEH)
