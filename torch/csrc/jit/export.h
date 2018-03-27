45b276dd55c07d00a273f1ca8a744785c770ab6f
James Reed <jamesreed@fb.com>
2018-03-27 10:33:43 -0700

    Add comment

--- torch/csrc/jit/export.h
+++ torch/csrc/jit/export.h
@@ -4,6 +4,14 @@
 
 namespace torch { namespace jit {
 
+// This map is used to keep track of parameters that should be exported
+// externally. When `defer_weight_export` is true, the returned map contains
+// kv pairs that map {external reference name} -> {at::Tensor to be exported}.
+// It is the responsibility of the caller to export these appropriately.
+//
+// For example, when exporting to a zip archive, the caller may write out files
+// for each entry in the export map, with the filename being the key and the
+// file contents being the raw tensor data.
 using RawDataExportMap = std::unordered_map<std::string, at::Tensor>;
 
 std::tuple<std::string, RawDataExportMap> ExportGraph(
