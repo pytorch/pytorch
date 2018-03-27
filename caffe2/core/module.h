bd844af38a4b24ccc9de7fbd539f3606856245b1
Orion Reblitz-Richardson <orionr@gmail.com>
2018-03-26 21:17:12 -0700

    Remove Apache headers from source.
    * LICENSE file contains details, so removing from individual source files.

--- caffe2/core/module.h
+++ caffe2/core/module.h
@@ -1,20 +1,4 @@
 /**
- * Copyright (c) 2016-present, Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
  * A global dictionary that holds information about what Caffe2 modules have
  * been loaded in the current runtime, and also utility functions to load
  * modules.
