fe88d85815bc1d0d6d6adefbda5f92254c5935ac
Junjie Bai <bai@in.tum.de>
2018-03-26 23:30:14 -0700

    Fix use after free bug in peephole pass

--- torch/csrc/jit/passes/peephole.cpp
+++ torch/csrc/jit/passes/peephole.cpp
@@ -14,6 +14,7 @@ void PeepholeOptimize(Block * block) {
   for (auto it = block->nodes().begin(); it != block->nodes().end(); ++it) {
     auto* n = *it;
 
+    bool destroyed = false;
     // XXX: remember that if you want to simplify an expression by combining multiple nodes
     // into a different one, then you need to check that they all belong to the given block
     switch (n->kind()) {
@@ -23,6 +24,7 @@ void PeepholeOptimize(Block * block) {
         if (n->is(attr::size) == n->input()->type()->expect<TensorType>()->sizes()) {
           n->output()->replaceAllUsesWith(n->input());
           it.destroyCurrent();
+          destroyed = true;
         }
         break;
       case aten::t:
@@ -39,12 +41,15 @@ void PeepholeOptimize(Block * block) {
               input_node->destroy();
             }
           }
+          destroyed = true;
         }
         break;
     }
 
-    for (Block * sub_block : n->blocks()) {
-      PeepholeOptimize(sub_block);
+    if (!destroyed) {
+        for (Block * sub_block : n->blocks()) {
+            PeepholeOptimize(sub_block);
+        }
     }
   }
 }
