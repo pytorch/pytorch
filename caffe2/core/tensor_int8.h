62ea35eacf76ed230965eb40b42f8f1215f0bbd6
Yiming Wu <wyiming@fb.com>
2018-03-30 15:20:56 -0700

    [C2 Int8][C2 Core]fetch int8 blob
    Providing Python API to fetch Int8 tensors.
    
      data, scale. zero_point = workspace.FetchInt8Blob(blob_name)
    
    now returns a tuple if the blob contains a Int8TensorCPU
    
         'data' = int8 data array
         'scale' = fake quantization scale
         'zero_point' = fake quantization offset
    
    Although FetchBlob shares back-end implmentation with FetchInt8Blob, we raise
    error to prevent unexpected behavior of the same method

---
+++ caffe2/core/tensor_int8.h
@@ -0,0 +1,20 @@
+#ifndef CAFFE2_TENSOR_INT8_H_
+#define CAFFE2_TENSOR_INT8_H_
+
+#include "caffe2/core/context.h"
+#include "caffe2/core/tensor.h"
+
+namespace caffe2 {
+namespace int8 {
+
+struct Int8TensorCPU {
+  float scale{1.0};
+  int32_t zero_point{0};
+  // Generally stores uint8_t data, but sometimes int32_t (e.g. bias
+  // parameters).
+  TensorCPU t;
+};
+} // namespace int8
+} // namespace caffe2
+
+#endif // CAFFE2_TENSOR_INT8_H_
