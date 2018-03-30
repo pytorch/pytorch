c5f2cd907ca9bd448f828755d5da7feff70774f0
Kaiyu Shi <skyisno.1@gmail.com>
2018-03-31 00:01:50 +0800

    Change to extra_repr in some Modules

--- torch/nn/modules/fold.py
+++ torch/nn/modules/fold.py
@@ -68,13 +68,11 @@ class Fold(Module):
         return F.fold(input, self.output_size, self.kernel_size, self.dilation,
                       self.padding, self.stride)
 
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-            + 'output_size=' + str(self.output_size) \
-            + ', kernel_size=' + str(self.kernel_size) \
-            + ', dilation=' + str(self.dilation) \
-            + ', padding=' + str(self.padding) \
-            + ', stride=' + str(self.stride) + ')'
+    def extra_repr(self):
+        return 'output_size={output_size}, kernel_size={kernel_size}, ' \
+            'dilation={dilation}, padding={padding}, stride={stride}'.format(
+                **self.__dict__
+            )
 
 
 class Unfold(Module):
@@ -140,9 +138,6 @@ class Unfold(Module):
         return F.unfold(input, self.kernel_size, self.dilation,
                         self.padding, self.stride)
 
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-            + 'kernel_size=' + str(self.kernel_size) \
-            + ', dilation=' + str(self.dilation) \
-            + ', padding=' + str(self.padding) \
-            + ', stride=' + str(self.stride) + ')'
+    def extra_repr(self):
+        return 'kernel_size={kernel_size}, dilation={dilation}, padding={padding},' \
+            ' stride={stride}'.format(**self.__dict__)
