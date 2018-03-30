a8e8ef6a8d254a2059e31bcc2cf0b3753fff787b
Will Feng <willfeng@fb.com>
2018-03-30 00:08:16 -0400

    Set default for TEST_CUDA

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -13,6 +13,7 @@ from torch.utils.data import Dataset, TensorDataset, DataLoader, ConcatDataset
 from torch.utils.data.dataset import random_split
 from torch.utils.data.dataloader import default_collate, ExceptionWrapper
 from common import TestCase, run_tests, TEST_NUMPY, IS_WINDOWS
+TEST_CUDA = False
 if __name__ == '__main__':
     from common_nn import TEST_CUDA
 
