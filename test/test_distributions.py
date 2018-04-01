Add batched MVN test, and fix KL-D expression error

test/test_distributions.py
@@ -2630,11 +2630,8 @@ class TestKL(TestCase):
         n = 5  # Number of tests for multivariate_normal
         print('Testing KL(MultivariateNormal, MultivariateNormal) using Monte Carlo')
         for i in range(0, n):
-            loc = []
-            scale_tril = []
-            for _ in range(0, 2):
-                loc.append(torch.randn(4))
-                scale_tril.append(transform_to(constraints.lower_cholesky)(torch.randn(4, 4)))
+            loc = [torch.randn(4) for _ in range(0, 2)]
+            scale_tril = [transform_to(constraints.lower_cholesky)(torch.randn(4, 4)) for _ in range(0, 2)]
             p = MultivariateNormal(loc=loc[0], scale_tril=scale_tril[0])
             q = MultivariateNormal(loc=loc[1], scale_tril=scale_tril[1])
             actual = kl_divergence(p, q)
@@ -2653,6 +2650,18 @@ class TestKL(TestCase):
                  'Expected ({} Monte Carlo sample): {}'.format(denominator, expected),
                  'Actual (analytic): {}'.format(actual),
             ]))
+
+    def test_kl_multivariate_normal_batched(self):
+        b = 7  # Number of batches
+        loc = [torch.randn(b, 3) for _ in range(0, 2)]
+        scale_tril = [transform_to(constraints.lower_cholesky)(torch.randn(b, 3, 3)) for _ in range(0, 2)]
+        expected_kl = torch.stack([
+                        kl_divergence(MultivariateNormal(loc[0][i], scale_tril=scale_tril[0][i]),
+                                      MultivariateNormal(loc[1][i], scale_tril=scale_tril[1][i]))
+                        for i in range(0, b)])
+        actual_kl = kl_divergence(MultivariateNormal(loc[0], scale_tril=scale_tril[0]),
+                                  MultivariateNormal(loc[1], scale_tril=scale_tril[1]))
+        self.assertEqual(expected_kl, actual_kl)
 
     def test_kl_exponential_family(self):
         for (p, _), (_, q) in self.finite_examples: