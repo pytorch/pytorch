5f1a58d010b0fac3f51655536f4d77c0f407ed7b
Paul Jesse Hellemn <jesse.hellemn@gmail.com>
2018-04-02 16:35:27 -0700

    [caffe2] Fbcode to GitHub sync (#6208)
    * [easy] allow empty tensor in cuda relu op
    
    The diff has not enabled unit test of empty tensor, because MLKVersion of ReluOp need extra work to support
    
    * Make blob norm plotting work with distributed trainer when the old framework is used

--- caffe2/python/modeling/compute_histogram_for_blobs_test.py
+++ caffe2/python/modeling/compute_histogram_for_blobs_test.py
@@ -72,6 +72,45 @@ class ComputeHistogramForBlobsTest(unittest.TestCase):
             fc1_w_curr_normalized_hist - cur_hist), 0.0, delta=1e-5)
         self.assertEqual(len(model.net.Proto().op), 12)
 
+        assert model.net.output_record() is None
+
+    def test_compute_histogram_for_blobs_modify_output_record(self):
+        model = model_helper.ModelHelper(name="test")
+        data = model.net.AddExternalInput("data")
+        fc1 = brew.fc(model, data, "fc1", dim_in=4, dim_out=2)
+
+        # no operator name set, will use default
+        brew.fc(model, fc1, "fc2", dim_in=2, dim_out=1)
+
+        num_buckets = 20
+        lower_bound = 0.2
+        upper_bound = 0.8
+        accumulate = False
+        net_modifier = ComputeHistogramForBlobs(blobs=['fc1_w', 'fc2_w'],
+                                                logging_frequency=10,
+                                                num_buckets=num_buckets,
+                                                lower_bound=lower_bound,
+                                                upper_bound=upper_bound,
+                                                accumulate=accumulate)
+        net_modifier(model.net, modify_output_record=True)
+
+        workspace.FeedBlob('data', np.random.rand(10, 4).astype(np.float32))
+
+        workspace.RunNetOnce(model.param_init_net)
+        workspace.RunNetOnce(model.net)
+
+        fc1_w = workspace.FetchBlob('fc1_w')
+        fc1_w_curr_normalized_hist = workspace.FetchBlob('fc1_w_curr_normalized_hist')
+        cur_hist, acc_hist = self.histogram(fc1_w,
+                                            lower_bound=lower_bound,
+                                            upper_bound=upper_bound,
+                                            num_buckets=num_buckets)
+
+        self.assertEqual(fc1_w_curr_normalized_hist.size, num_buckets + 2)
+        self.assertAlmostEqual(np.linalg.norm(
+            fc1_w_curr_normalized_hist - cur_hist), 0.0, delta=1e-5)
+        self.assertEqual(len(model.net.Proto().op), 12)
+
         assert 'fc1_w' + net_modifier.field_name_suffix() in\
             model.net.output_record().field_blobs(),\
             model.net.output_record().field_blobs()
