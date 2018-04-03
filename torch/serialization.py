fdd88940b33ace1726b6816833cd9ec100b0e322
James Sutton <1892175+zeryx@users.noreply.github.com>
2018-04-03 14:08:26 -0300

    added portable model serialization utilities

--- torch/serialization.py
+++ torch/serialization.py
@@ -19,6 +19,7 @@ else:
     import pathlib
 
 DEFAULT_PROTOCOL = 2
+DEFAULT_MOD_PATH = 'portable-pytorch'
 
 LONG_SIZE = struct.Struct('=l').size
 INT_SIZE = struct.Struct('=i').size
@@ -130,6 +131,79 @@ def _is_real_file(f):
     except AttributeError:
         return False
 
+def save_portable(obj, mod_path, output_path):
+    """
+    Portably serializes a torch module file to a disk, along with the network definition module required to execute it.
+    Args:
+        obj: the torch module to save.
+        mod_path: the pythonic import path to your network definition module (it's recommended that this is
+        a separate module, that is self contained (IE does not use resources from other parts of your project).
+        output_path: the output file path where you'd like to save your model to.
+        
+    Example:
+        >>> # save portable version of your model
+        >>> from src.some_project.pytorch_defs.net import Net
+        >>> net = Net(...)
+        >>> output = net.forward()
+        >>> ...
+        >>> torch.save_portable(net, "src.some_project.pytorch_defs", "/tmp/myModel.zip")
+        
+"""
+    import zipfile
+    _, model_temp = tempfile.mkstemp()
+    input_system_path = input_mod_path.replace('.', '/')
+    _save_portable(model, input_mod_path, model_temp)
+    source_files = []
+    for root, dirs, files in os.walk(input_system_path):
+        for file in files:
+            true_path = os.path.join(root, file)
+            false_path = os.path.join(default_mod_path, file)
+            source_files.append((true_path, false_path))
+    with zipfile.ZipFile(output_path, "w") as zip:
+        zip.write(model_temp, "model.t7")
+        for true_path, false_path in source_files:
+            zip.write(true_path, false_path)
+    os.remove(model_temp)
+    return output_path
+
+def _save_portable(obj, in_mod_path, model_save_path):
+    serialized_model = pickle.dumps(obj, byref=False)
+    input_mod_path = input_mod_path.encode('utf-8')
+    output_mod_path = output_mod_path.encode('utf-8')
+    serialized_model = serialized_model.replace(input_mod_path, DEFAULT_MOD_PATH)
+    with open(model_save_path, 'wb') as f:
+        f.write(serialized_model)
+
+        
+def load_portable(local_file_path, temp_location="/tmp"):
+    """
+    Portably deserializes a portable serialized torch model saved with "save_portable". The magic here is 
+    you don't need the original network definition code in order to load this network and utilize it, as the
+    necessary source code is provided along with the network.
+    
+    Args:
+        local_file_path: The local system file path to your portable model object.
+        temp_location: a scratchspace location to use for the unzip/depickling process, if you're
+        using a non debian based kernel (or wholy different operating system) you'll want to change this.
+        
+    Example:
+        >>> # Load a portable model
+        >>> local_portable_model_path = "/tmp/myModel.zip"
+        >>> model = torch.load_portable(local_portable_model_path)
+        >>> output = model.forward(...)
+    """
+    import zipfile
+    with zipfile.ZipFile(local_file_path) as zip:
+        zip.extractall(temp_location)
+    sys.path.insert(0, temp_location)
+    model = _load_portable("{}/model.t7".format(str(temp_location)))
+    return model
+
+def _load_portable(model_path):
+    with open(filepath, 'rb') as f:
+        mod = pickle.load(f)
+    return mod
+    
 
 def save(obj, f, pickle_module=pickle, pickle_protocol=DEFAULT_PROTOCOL):
     """Saves an object to a disk file.
