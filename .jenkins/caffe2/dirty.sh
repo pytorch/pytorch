80ff36c9a4e3854ee923bc6d37ddcebc0dcaeaca
Edward Z. Yang <ezyang@mit.edu>
2018-04-03 15:12:58 -0400

    Print the diff files to aid in debugging when it's wrong. (#6238)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .jenkins/caffe2/dirty.sh
+++ .jenkins/caffe2/dirty.sh
@@ -2,4 +2,5 @@
 set -ex
 upstream="$1"
 pr="$2"
+git diff --name-only "$upstream" "$pr"
 git diff --name-only "$upstream" "$pr" | grep -Eq '^(CMakeLists.txt|Makefile|.gitmodules|.jenkins/caffe2|binaries|caffe|caffe2|cmake|conda|docker|docs/caffe2|modules|scripts|third_party)'
