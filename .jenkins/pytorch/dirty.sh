867c17eef6fed8da49a00ebef5fdfa7a5276ef51
Edward Z. Yang <ezyang@mit.edu>
2018-04-03 15:12:58 -0400

    Print the diff files to aid in debugging when it's wrong. (#6238)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .jenkins/pytorch/dirty.sh
+++ .jenkins/pytorch/dirty.sh
@@ -2,4 +2,5 @@
 set -ex
 upstream="$1"
 pr="$2"
+git diff --name-only "$upstream" "$pr"
 git diff --name-only "$upstream" "$pr" | grep -Eq '^(aten/|.jenkins/pytorch|docs/(make.bat|Makefile|requirements.txt|source)|mypy|requirements.txt|setup.py|test/|third_party/|tools/|\.gitmodules|torch/)'
