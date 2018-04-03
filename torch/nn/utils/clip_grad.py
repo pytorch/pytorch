f8a5370895ecaf51b7047934f96b551ed3d22a50
Tony Beltramelli <contact@tonybeltramelli.com>
2018-04-03 22:44:14 +0200

    remove DeprecationWarning in torch.nn.utils.clip_grad

--- torch/nn/utils/clip_grad.py
+++ torch/nn/utils/clip_grad.py
@@ -43,8 +43,7 @@ def clip_grad_norm(parameters, max_norm, norm_type=2):
         :func:`torch.nn.utils.clip_grad_norm_`.
     """
     warnings.warn("torch.nn.utils.clip_grad_norm is now deprecated in favor "
-                  "of torch.nn.utils.clip_grad_norm_.",
-                  category=DeprecationWarning, stacklevel=2)
+                  "of torch.nn.utils.clip_grad_norm_.", stacklevel=2)
     return clip_grad_norm_(parameters, max_norm, norm_type)
 
 
