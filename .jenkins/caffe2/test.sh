c3e5ef8fc5a9d7da952570f932222040ae5d7feb
pjh5 <hellemn@fb.com>
2018-04-04 11:09:28 -0700

    Moving conda test package installs into docker image

--- .jenkins/caffe2/test.sh
+++ .jenkins/caffe2/test.sh
@@ -25,23 +25,6 @@ if [[ "$BUILD_ENVIRONMENT" == conda* ]]; then
   # docker image
   PYTHON="/opt/conda/bin/python"
   INSTALL_PREFIX="/opt/conda/"
-
-  # Testing requires separate packages
-  if [[ $BUILD_ENVIRONMENT == *gcc4* ]]; then
-    # These are from conda-forge
-    conda install -yc conda-forge hypothesis tabulate pydot networkx==2.0 click pytest scipy
-    # These packages are from the default channels
-    conda install -y opencv=3.1.0=np112py27_1 pil=1.1.7=py27_2
-  else
-    conda install -y hypothesis tabulate pydot
-  fi
-
-  # This build will be tested against onnx tests, which needs onnx installed.
-  # Onnx should be built against the same protobuf that Caffe2 uses, which is
-  # only installed in the conda environment when Caffe2 is.
-  # This path comes from install_anaconda.sh which installs Anaconda into the
-  # docker image
-  PROTOBUF_INCDIR=/opt/conda/include pip install "${ROOT_DIR}/third_party/onnx"
 fi
 
 # Add the site-packages in the caffe2 install prefix to the PYTHONPATH
