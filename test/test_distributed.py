2819b8630970cbe1eb645b974fbc0fd937d52f06
Ailing <ailzhang@fb.com>
2018-04-10 09:38:34 -0700

    convert range object to list

--- test/test_distributed.py
+++ test/test_distributed.py
@@ -183,8 +183,8 @@ class _DistTestBase(object):
             apply_hack_for_nccl()
 
         nGPUs_per_process = nGPUs // world_size
-        rank_to_GPU = {i: visible_devices[i * nGPUs_per_process: (i + 1) * nGPUs_per_process]
-                       for i in list(range(world_size))}
+        rank_to_GPU = {i: list(visible_devices[i * nGPUs_per_process: (i + 1) * nGPUs_per_process])
+                       for i in range(world_size)}
         return rank_to_GPU
 
     # GET RANK
