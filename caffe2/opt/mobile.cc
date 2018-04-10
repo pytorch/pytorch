7bd398b3dbc9907be56e7123537f40b210e738ca
Bram Wasti <bwasti@fb.com>
2018-04-10 16:51:16 -0700

    Add fuseNNPACKConvRelu (#6439)

--- caffe2/opt/mobile.cc
+++ caffe2/opt/mobile.cc
@@ -1,8 +1,11 @@
 #include "mobile.h"
+#include "caffe2/core/logging.h"
 #include "nomnigraph/Converters/Caffe2.h"
 
 namespace caffe2 {
 namespace opt {
+
+using namespace nom;
 
 caffe2::NetDef addNNPACK(caffe2::NetDef net, bool low_memory) {
   auto nn = nom::converters::convertFromCaffe2Proto(net);
@@ -68,6 +71,87 @@ caffe2::NetDef addNNPACK(caffe2::NetDef net, bool low_memory) {
       precompute_argument->set_name("convolution_transform_strategy");
       precompute_argument->set_s("PRECOMPUTE");
     }
+  }
+  return nom::converters::convertToCaffe2Proto(nn);
+}
+
+namespace {
+
+inline bool isNNPACKConvReluEfficient(std::string algo, repr::Conv* conv) {
+  if (algo == "AUTO" || algo == "") {
+    for (auto stride : conv->getStrides()) {
+      if (stride > 1) {
+        return false;
+      }
+    }
+    for (auto kernel : conv->getKernelShape()) {
+      if (kernel < 2) {
+        return false;
+      }
+    }
+  } else if (!(algo == "WINOGRAD" || algo == "WINOGRAD_FP16" ||
+               algo == "FT8x8" || algo == "FT16x16")) {
+    return false;
+  }
+  return true;
+}
+
+} // namespace
+
+caffe2::NetDef fuseNNPACKConvRelu(caffe2::NetDef net) {
+  auto nn = nom::converters::convertFromCaffe2Proto(net);
+  for (auto node_pair : repr::nn::dataIterator<repr::Conv>(nn.dataFlow)) {
+    repr::NNGraph::NodeRef conv_node;
+    repr::Conv* conv;
+    std::tie(conv, conv_node) = node_pair;
+
+    auto conv_outputs = repr::nn::getOutputs(conv_node);
+    if (conv_outputs.size() != 1) {
+      continue;
+    }
+    auto conv_output = conv_outputs.front();
+
+    auto consumers = repr::nn::getConsumers(conv_output);
+    if (consumers.size() != 1) {
+      continue;
+    }
+    if (!repr::nn::is<repr::Relu>(consumers.front())) {
+      continue;
+    }
+    auto relu_node = consumers.front();
+
+    auto annotation = conv->getMutableAnnotation();
+    if (!annotation) {
+      continue;
+    }
+    auto* op = static_cast<caffe2::OperatorDef*>(annotation->getSaved());
+
+    // We only want to fuse for fast NNPACK convs
+    if (op->engine() != "NNPACK") {
+      continue;
+    }
+    caffe2::string algo = "AUTO";
+    for (auto arg : op->arg()) {
+      if (arg.name() == "algo") {
+        algo = arg.s();
+      }
+    }
+    if (!isNNPACKConvReluEfficient(algo, conv)) {
+      continue;
+    }
+
+    auto relu_outputs = repr::nn::getOutputs(relu_node);
+    if (relu_outputs.size() != 1) {
+      continue;
+    }
+    auto relu_output = relu_outputs.front();
+
+    nn.dataFlow.replaceNode(relu_output, conv_output);
+    nn.dataFlow.deleteNode(relu_node);
+    nn.dataFlow.deleteNode(relu_output);
+    auto* arg = op->add_arg();
+    arg->set_name("activation");
+    arg->set_s("Relu");
   }
   return nom::converters::convertToCaffe2Proto(nn);
 }
--- caffe2/python/transformations_test.py
+++ caffe2/python/transformations_test.py
@@ -20,7 +20,7 @@ from __future__ import unicode_literals
 
 from caffe2.python import core, workspace, test_util
 
-from caffe2.python.transformations import addNNPACK
+from caffe2.python.transformations import addNNPACK, fuseNNPACKConvRelu
 
 
 class TestTransformations(test_util.TestCase):
@@ -32,3 +32,74 @@ class TestTransformations(test_util.TestCase):
         net.Relu(["Y"], ["Y2"])
         addNNPACK(net)
         assert (net.Proto().op[0].engine == "NNPACK")
+
+
+    def test_fuseNNPACKConvRelu(self):
+        net = core.Net("net")
+        net.Conv(
+            ["X", "w", "b"], ["Y"], stride=1, pad=0, kernel=3, order="NCHW"
+        )
+        net.Relu(["Y"], ["Y2"])
+        addNNPACK(net) # get the NNPACK engine
+        assert (net.Proto().op[0].engine == "NNPACK")
+        fuseNNPACKConvRelu(net)
+        assert (len(net.Proto().op) == 1)
+        has_activation_arg = False
+        for arg in net.Proto().op[0].arg:
+            if arg.name == "activation":
+                assert (arg.s == "Relu")
+                has_activation_arg = True
+        assert has_activation_arg
+
+    def test_noFuseNNPACKConvRelu(self):
+        net = core.Net("net")
+        net.Conv(
+            ["X", "w", "b"], ["Y"], stride=1, pad=0, kernel=3, order="NCHW"
+        )
+        net.Relu(["Y"], ["Y2"])
+        net.Relu(["Y"], ["Y3"])
+        addNNPACK(net) # get the NNPACK engine
+        assert (net.Proto().op[0].engine == "NNPACK")
+        fuseNNPACKConvRelu(net)
+        assert (len(net.Proto().op) == 3)
+        has_activation_arg = False
+        for arg in net.Proto().op[0].arg:
+            if arg.name == "activation" and arg.s == "Relu":
+                has_activation_arg = True
+        assert not has_activation_arg
+
+    def test_fuseNNPACKConvReluNoInplace(self):
+        net = core.Net("net")
+        net.Conv(
+            ["X", "w", "b"], ["Y"], stride=1, pad=0, kernel=3, order="NCHW"
+        )
+        net.Relu(["Y"], ["X"])
+        addNNPACK(net) # get the NNPACK engine
+        assert (net.Proto().op[0].engine == "NNPACK")
+        fuseNNPACKConvRelu(net)
+        assert (len(net.Proto().op) == 1)
+        has_activation_arg = False
+        for arg in net.Proto().op[0].arg:
+            if arg.name == "activation":
+                assert (arg.s == "Relu")
+                has_activation_arg = True
+        assert has_activation_arg
+        assert net.Proto().op[0].output[0] != net.Proto().op[0].input[0]
+
+    def test_fuseNNPACKConvReluInplaceRelu(self):
+        net = core.Net("net")
+        net.Conv(
+            ["X", "w", "b"], ["Y"], stride=1, pad=0, kernel=3, order="NCHW"
+        )
+        net.Relu(["Y"], ["Y"])
+        addNNPACK(net) # get the NNPACK engine
+        assert (net.Proto().op[0].engine == "NNPACK")
+        fuseNNPACKConvRelu(net)
+        assert (len(net.Proto().op) == 1)
+        has_activation_arg = False
+        for arg in net.Proto().op[0].arg:
+            if arg.name == "activation":
+                assert (arg.s == "Relu")
+                has_activation_arg = True
+        assert has_activation_arg
+        assert net.Proto().op[0].output[0] != net.Proto().op[0].input[0]
