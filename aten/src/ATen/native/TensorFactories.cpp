dc0ca98bc7f09fc44c82421ea2b3b943cb0e8763
Naman Jain <naman1205jain@gmail.com>
2018-04-10 21:38:21 +0530

    Add torch.randint and torch.randint_like functions (#6136)
    Adds randint and randint_like to TensorFactories.cpp

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -178,6 +178,42 @@ Tensor rand_like(const Tensor& self, const Type& dtype) {
   return at::native::rand(dtype, self.sizes());
 }
 
+Tensor randint(const Type& dtype, int64_t high, IntList size, Generator* generator) {
+  Tensor result = dtype.tensor(size);
+  return result.random_(0, high, generator);
+}
+
+Tensor randint(const Type& dtype, int64_t low, int64_t high, IntList size, Generator* generator) {
+  Tensor result = dtype.tensor(size);
+  return result.random_(low, high, generator);
+}
+
+Tensor& randint_out(Tensor& result, int64_t high, IntList size, Generator* generator) {
+  result.resize_(size);
+  return result.random_(0, high, generator);
+}
+
+Tensor& randint_out(Tensor& result, int64_t low, int64_t high, IntList size, Generator* generator) {
+  result.resize_(size);
+  return result.random_(low, high, generator);
+}
+
+Tensor randint_like(const Tensor& self, int64_t high) {
+  return at::native::randint_like(self, high, self.type());
+}
+
+Tensor randint_like(const Tensor& self, int64_t low, int64_t high) {
+  return at::native::randint_like(self, low, high, self.type());
+}
+
+Tensor randint_like(const Tensor& self, int64_t high, const Type& dtype) {
+  return at::native::randint(dtype, high, self.sizes(), nullptr);
+}
+
+Tensor randint_like(const Tensor& self, int64_t low, int64_t high, const Type& dtype) {
+  return at::native::randint(dtype, low, high, self.sizes(), nullptr);
+}
+
 Tensor randn(const Type& dtype, IntList size, Generator* generator) {
   Tensor result = dtype.tensor(size);
   return result.normal_(0, 1, generator);
