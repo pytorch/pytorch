5ce6b97aee39eb04ff2359384341d07e15a4c05f
Tongzhou Wang <SsnL@users.noreply.github.com>
2018-04-11 13:41:23 -0400

    Use symbolizer in ASAN (#6506)

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -23,6 +23,7 @@ if [[ "$BUILD_ENVIRONMENT" == *asan* ]]; then
     export ASAN_OPTIONS=detect_leaks=0:symbolize=1
     export PYTORCH_TEST_WITH_ASAN=1
     # TODO: Figure out how to avoid hard-coding these paths
+    export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-5.0/bin/llvm-symbolizer
     export LD_PRELOAD=/usr/lib/llvm-5.0/lib/clang/5.0.0/lib/linux/libclang_rt.asan-x86_64.so
 fi
 
