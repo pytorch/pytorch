fb45e81fae4eb9e292c8d6afd36c1772d2f2658a
Peter Goldsborough <psag@fb.com>
2018-04-11 21:22:48 -0700

    Also set WERROR=1 for cpp_build in CI

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -63,5 +63,5 @@ if [[ "$BUILD_TEST_LIBTORCH" == "1" ]]; then
   echo "Building libtorch with NO_PYTHON"
   # NB: Install outside of source directory (at the same level as the root
   # pytorch folder) so that it doesn't get cleaned away prior to docker push.
-  VERBOSE=1 tools/cpp_build/build_all.sh "$PWD/../cpp-build"
+  WERROR=1 VERBOSE=1 tools/cpp_build/build_all.sh "$PWD/../cpp-build"
 fi
