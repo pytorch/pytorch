f0b49814f4c7360aa1666785914c26bd415c8b98
Thomas Viehmann <tv@beamnet.de>
2018-04-17 11:06:23 +0200

    better commentary in einsum and sumproduct_pair

--- aten/src/ATen/native/Linear.cpp
+++ aten/src/ATen/native/Linear.cpp
@@ -7,15 +7,19 @@ namespace at { namespace native {
 
 
 // sumproduct_pair computes `(left*right).sum(sumdims)` by means of permutation and
-// batch matrix multiplication // its main purpose is to provide a pairwise
-// reduction for einsum
+// batch matrix multiplication
+// its main purpose is to provide a pairwise reduction for einsum
 Tensor sumproduct_pair(const Tensor& left_, const Tensor& right_, IntList sum_dims_, bool keepdim) {
-  // assumes that tensors have been pre-unsqueezed
+  // assumes that tensors have been pre-unsqueezed (so that all dimensions match - after broadcasting)
+  // but makes no other assumptions on the order of dimensions
   AT_ASSERT(left_.dim()==right_.dim(), "number of dimensions must match");
   if (sum_dims_.size() == 0)
     return at::mul(left_, right_);
   int64_t dim = left_.dim();
   auto sum_dims = dim_list_to_bitset(sum_dims_, dim);
+  // dimensions that will be part of the output (i.e. not summed over) in three vectors
+  // dims in lro appear in left, right and output, similarly lo: left and output, ro: right and output
+  // also the sizes are kept track of for reshaping
   std::vector<int64_t> lro, lo, ro;
   int64_t lro_size = 1, lo_size = 1, ro_size = 1, sum_size = 1;
   Tensor left = left_;
@@ -23,20 +27,21 @@ Tensor sumproduct_pair(const Tensor& left_, const Tensor& right_, IntList sum_di
   for (int64_t i = 0; i < dim; i++) {
     auto sl = left.size(i)>1;
     auto sr = right.size(i)>1;
-    if (sum_dims[i]) {
-      if (sl && sr) {
-	AT_ASSERT(left.size(i)==right.size(i), "sum indexes must match");
+    if (sum_dims[i]) { // first dimensions that will be summed over after multiplication
+      if (sl && sr) {  // dimensions nontrivially in both left and right must be of the same size
+	AT_ASSERT(left.size(i)==right.size(i), "non-broadcast dimensions must match");
 	sum_size *= left.size(i);
-      } else if (sl) {
+      } else if (sl) { // if it is only in one of left and right, we can sum right away
 	left = left.sum(i, true);
       } else if (sr) {
 	right = right.sum(i, true);
       }
-    } else if (sl && sr) {
+    } else if (sl && sr) { // now deal with dimensions  dimensions that will be in the output
+      // dimensions nontrivially in both left and right must be of the same size
       AT_ASSERT(left.size(i)==right.size(i), "non-broadcast dimensions must match");
       lro.push_back(i);
       lro_size *= left.size(i);
-    } else if (sl) {
+    } else if (sl) { // keep track of dimensions appearing only once
       lo.push_back(i);
       lo_size *= left.size(i);
     } else {
@@ -44,6 +49,13 @@ Tensor sumproduct_pair(const Tensor& left_, const Tensor& right_, IntList sum_di
       ro_size *= right.size(i);
     }
   }
+  // we now work with the following permutations / shapes.
+  // the pipeline is permute inputs -> reshape inputs -> batch matrix mul -> reshape(view) output -> permute output
+  // output: "lro, lo, 1-for-summed-dims, ro" with orgiginal shape dimensions
+  // left:   "lro, lo, summed" permuted with lpermutation and the three flattened
+  // right:  "lro, summed, ro" permuted with rpermutation and the three flattened
+  // then the permuted output is a view of bmm(left, right)
+  // finally, opermutation reverts the permutation to the original order of dimensions
   std::vector<int64_t> out_size;
   for (auto& d : lro) out_size.push_back(left.size(d));
   for (auto& d : lo) out_size.push_back(left.size(d));
@@ -78,10 +90,13 @@ Tensor sumproduct_pair(const Tensor& left_, const Tensor& right_, IntList sum_di
   }
   }
 
+  // now we can execute the operations above
   left = left.permute(lpermutation).reshape({lro_size, lo_size, sum_size});
   right = right.permute(rpermutation).reshape({lro_size, sum_size, ro_size});
   Tensor result = at::bmm(left, right);
   result = result.view(out_size).permute(opermutation);
+
+  // finally squeeze summed dimensions if desired
   if (! keepdim) {
     for (int i = dim-1; i>=0; i--)
       if (sum_dims[i])
@@ -91,40 +106,51 @@ Tensor sumproduct_pair(const Tensor& left_, const Tensor& right_, IntList sum_di
 }
 
 Tensor einsum(String eqn, TensorList tensors) {
+  constexpr size_t number_of_letters = 26;
   std::string in_eqn;
   size_t pos;
-  std::vector<std::int64_t> number_of_occurences(26, 0);
-  std::vector<std::int64_t> last_occurence(26, -1);
-  std::vector<std::int64_t> sorted_position(26, -1);
-  if ((pos = eqn.find("->")) != std::string::npos) {
+  // we need are number of mappings (letter) index for analysing the equation. The index runs from 0='a' through 25='z'.
+  std::vector<std::int64_t> number_of_occurences(number_of_letters, 0); // number of occurence in the equation of this index
+  std::vector<std::int64_t> last_occurence(number_of_letters, -1);      // the last operator (left to right) using this index
+  std::vector<std::int64_t> sorted_position(number_of_letters, -1);     // the position of the index in the tensor dimensions
+  if ((pos = eqn.find("->")) != std::string::npos) { // check whether we have a right hand side. in_eq is the left hand side
     in_eqn = eqn.substr(0, pos);
   } else {
     in_eqn = eqn;
   }
 
+  // next we parse in_eq (the left hand side) by iterating. It is a string of comma separated terms per index
   int64_t operand = 0;
   std::stringstream eqn_stream(in_eqn);
   std::string term;
   while (! eqn_stream.eof()) {
-    std::getline(eqn_stream, term, ',');
+    std::getline(eqn_stream, term, ',');  // term = string with indices of current term
     int64_t dims_in_operand = 0;
-    for (auto &c : term) {
+    for (auto &c : term) {                // c    = character with a single index
       AT_ASSERT(('a' <= c) && (c <= 'z'), "only lowercase letters a-z allowed as indices");
-      int64_t index_num = c-'a';
+      int64_t index_num = c-'a';          // index_num  = index to be used in the vectors above
       number_of_occurences[index_num]++;
+      // when there are two occurences we need to take a diagonal with respect to the dimensions
+      // occuring multiple times before continuing the processing.
+      // e.g. einsum('ii->i', [A]) should return the diagonal
+      // This waits for the general diagonal handling discussed in #6479
+      // for now, we error out here
       AT_ASSERT(last_occurence[index_num] < operand, "diagonals (multiple occurences of the same index for one tensor) not implemented yet")
       last_occurence[index_num] = operand;
       dims_in_operand++;
     }
-    AT_ASSERT((int64_t) tensors.size()>operand, "more operands in equation than tensors");
+    AT_ASSERT((int64_t) tensors.size()>operand, "more operands in equation than tensors"); // we cannot have a longer equation than operands. We need to check here before we check the dimensions
     AT_ASSERT(dims_in_operand == tensors[operand].dim(), "dimension mismatch for operand %zd: equation %zd, tensor %zd", operand, dims_in_operand, tensors[operand].dim());
     operand++;
   }
-  AT_ASSERT((int64_t) tensors.size()==operand, "more tensors than operands in equation");
+  AT_ASSERT((int64_t) tensors.size()==operand, "more tensors than operands in equation");  // we need ==, but > is captured above, so the error message can be specific that it is <.
 
+  // the following parses or infers output (right hand side)
+  // it also assigns the sorted_positions ((letter) index -> dimension in Tensors) and position_labels (dimensions in Tensors -> index)
+  // for the output indices
   int64_t num_outputs = 0;
   std::vector<int64_t> position_labels;
-  if (pos != std::string::npos) {
+  if (pos != std::string::npos) {            // parse the user provided right hand side
     for (auto &c : eqn.substr(pos+2)) {
       AT_ASSERT(('a' <= c) && (c <= 'z'), "only lowercase letters a-z allowed as indices");
       int64_t index_num = c-'a';
@@ -133,8 +159,8 @@ Tensor einsum(String eqn, TensorList tensors) {
       position_labels.push_back(index_num);
       num_outputs++;
     }
-  } else {
-    for (size_t idx = 0; idx < 26; idx++) {
+  } else {                                   // create a right hand side: the indices that occur exactly once in alphabetic order
+    for (size_t idx = 0; idx < number_of_letters; idx++) {
       if (number_of_occurences[idx] == 1) {
 	sorted_position[idx] = num_outputs;
 	position_labels.push_back(idx);
@@ -142,19 +168,24 @@ Tensor einsum(String eqn, TensorList tensors) {
       }
     }
   }
-  int64_t position = num_outputs;
-  for (int64_t idx = 0; idx < 26; idx++) {
+  // now we assign the sorted_positions ((letter) index -> dimension in Tensors) and position_labels (dimensions in Tensors -> index)
+  // for the non-output indices - those that are eventually summed over
+  int64_t position = num_outputs;            // we now determine the porder of the remaining indices (in so far they are in the equation)
+  for (size_t idx = 0; idx < number_of_letters; idx++) {
     if ((number_of_occurences[idx] > 0) && (sorted_position[idx]==-1)) {
       sorted_position[idx] = position;
       position_labels.push_back(idx);
       position++;
     }
   }
+  // we now "homogenize the dimensions", i.e. create all dimensions in each tensor and sort the dimensions according to the mapping in
+  // sorted_postition / position_labels
+  // after this, all operands will have compatible shapes (i.e. all dimensions are aligned are broadcastable)
   std::vector<Tensor> permuted_ops;
   eqn_stream.clear();
   eqn_stream.seekg(0, std::ios_base::beg);
   for (int64_t op = 0; op < (int64_t) tensors.size(); op++) {
-    std::vector<int64_t> axes(26, -1);
+    std::vector<int64_t> axes(number_of_letters, -1);
     std::vector<int64_t> permutation;
     std::getline(eqn_stream, term, ',');
     int64_t dim = 0;
@@ -176,18 +207,23 @@ Tensor einsum(String eqn, TensorList tensors) {
       }
     }
   }
-  // we just go left to right. numpy allows to optimize the path...
+  // now we reduce the indices from left to right
+  // numpy allows to optimize the path using various
+  // algorithms (see eigen_path in numpy docs)
+  // we start with the leftmost operator and reduce indices that
+  // appear only there
   Tensor result = permuted_ops[0];
-  for (int64_t idx = 0; idx < 26; idx++) {
+  for (int64_t idx = 0; idx < number_of_letters; idx++) {
     if ((last_occurence[idx] == 0)
 	&& (sorted_position[idx]>=num_outputs)) {
       result = result.sum(sorted_position[idx], true);
     }
   }
 
+  // now we process each tensor using sumproduct_pair
   for (int64_t i = 1; i < (int64_t) permuted_ops.size(); i++) {
     std::vector<int64_t> sum_dims;
-    for (int64_t idx = 0; idx < 26; idx++) {
+    for (int64_t idx = 0; idx < number_of_letters; idx++) {
       if ((last_occurence[idx] == i)
 	  && (sorted_position[idx]>=num_outputs)) {
 	sum_dims.push_back(sorted_position[idx]);
@@ -195,6 +231,7 @@ Tensor einsum(String eqn, TensorList tensors) {
     }
     result = at::native::sumproduct_pair(result, permuted_ops[i], sum_dims, true);
   }
+  // finally, we squeeze out all non-result dimensions
   for (int64_t dim = position_labels.size()-1; dim >= num_outputs; dim--)
     result.squeeze_(dim);
   return result;
