7295cbc285ee34dae8facb6bc4b208fed49971cf
Zach DeVito <zdevito@fb.com>
2018-04-17 14:55:18 -0700

    Tuple literal and cat support
    * Support list and tuple literals: Adds support for [a, b], (a, b) and "a, "
    
    * Allow non-tensors to reach emitBuiltinCall, each SugaredValue::call
    is now responsible for checking the types of its inputs.
    
    Add support for calling cat with a tuple to emitBuiltinOp

--- torch/csrc/jit/type.h
+++ torch/csrc/jit/type.h
@@ -72,6 +72,10 @@ public:
   virtual ~Type() {}
 };
 
+inline bool operator!=(const Type & lhs, const Type & rhs) {
+  return !(lhs == rhs);
+}
+
 
 struct DynamicType : public Type {
   DynamicType()
@@ -190,15 +194,21 @@ struct TupleType : public Type {
   virtual bool operator==(const Type& rhs) const override {
     if(rhs.kind() != kind())
       return false;
-    return rhs.cast<TupleType>()->elements().equals(elements());
+    const auto & l_elements = elements();
+    const auto & r_elements = rhs.cast<TupleType>()->elements();
+    if(l_elements.size() != r_elements.size())
+      return false;
+    for(size_t i = 0; i < l_elements.size(); ++i) {
+      if(*l_elements[i] != *r_elements[i])
+        return false;
+    }
+    return true;
   }
 private:
   std::vector<TypePtr> elements_;
 };
 
-inline bool operator!=(const Type & lhs, const Type & rhs) {
-  return !(lhs == rhs);
-}
+
 
 std::ostream& operator<<(std::ostream & out, const Type & t);
 
