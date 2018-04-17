a60fd6a839b3f7061d20d4573aed9e8fa14dd857
Marat Dukhan <marat@fb.com>
2018-04-17 12:13:57 -0700

    Fix bug in NNPACK bindings for convolution in precomputed transform
    Caffe2-NNPACK integration created blobs for precomputed kernel transorms based on the name of Conv operator.
    When Conv operators have the same name (e.g. empty string), or the blobs for precomputed transforms get the same name and overwrite each other.
    This patch ensures that blobs for all precomputed transforms in the network get a unique name.

--- caffe2/share/contrib/nnpack/conv_op.cc
+++ caffe2/share/contrib/nnpack/conv_op.cc
@@ -232,11 +232,11 @@ bool NNPACKConvOp::RunOnDeviceWithOrderNCHW() {
             (transformedFilterSize + sizeof(float) - 1) / sizeof(float);
 
         for (auto g = 0; g < group_; g++) {
-          transformedFilters_[g] =
-              ws_->CreateBlob(
-                     "__transformed_kernel_" +
-                     to_string(__sync_fetch_and_add(&precomputed_transform_id, 1)))
-                  ->GetMutable<TensorCPU>();
+          transformedFilters_[g] = ws_->CreateBlob(
+                                          "__transformed_kernel_" +
+                                          to_string(__sync_fetch_and_add(
+                                              &precomputed_transform_id, 1)))
+                                       ->GetMutable<TensorCPU>();
           transformedFilters_[g]->Resize(transformedFilterElements);
 
           status = nnp_convolution_inference(
