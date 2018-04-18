e488ee708252937e1813631c68d381ca89354be5
Neeraj Pradhan <npradhan@uber.com>
2018-04-18 11:43:32 -0700

    fix unused imports

--- torch/distributions/binomial.py
+++ torch/distributions/binomial.py
@@ -1,10 +1,8 @@
 from numbers import Number
 import torch
-import math
 from torch.distributions import constraints
 from torch.distributions.distribution import Distribution
 from torch.distributions.utils import broadcast_all, probs_to_logits, lazy_property, logits_to_probs
-from torch.distributions.utils import clamp_probs
 
 
 class Binomial(Distribution):
