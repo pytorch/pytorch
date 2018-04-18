f5000c8ba22fd3fd0a875c82439e1d4627686bcb
Will Feng <willfeng@fb.com>
2018-04-18 12:56:28 -0400

    Don't test ATen in ASAN build

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -29,10 +29,12 @@ fi
 time python test/run_test.py --verbose
 
 # Test ATen
-echo "Testing ATen"
-TORCH_LIB_PATH=$(python -c "import site; print(site.getsitepackages()[0])")/torch/lib
-ln -s "$TORCH_LIB_PATH"/libATen.so aten/build/src/ATen/libATen.so
-aten/tools/run_tests.sh aten/build
+if [[ "$BUILD_ENVIRONMENT" != *asan* ]]; then
+  echo "Testing ATen"
+  TORCH_LIB_PATH=$(python -c "import site; print(site.getsitepackages()[0])")/torch/lib
+  ln -s "$TORCH_LIB_PATH"/libATen.so aten/build/src/ATen/libATen.so
+  aten/tools/run_tests.sh aten/build
+fi
 
 rm -rf ninja
 
