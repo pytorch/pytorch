fdfc01ffc19eea070028f392b923899c19b6ad51
Jongsoo Park <jongsoo.park@gmail.com>
2018-04-18 19:13:45 -0700

    ChannelShuffle with NHWC layout (#6667)
    * ChannelShuffle with NHWC layout
    
    * ChannelShuffle with NHWC layout

--- caffe2/operators/channel_shuffle_op.h
+++ caffe2/operators/channel_shuffle_op.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "caffe2/utils/math.h"
 #include "caffe2/operators/conv_pool_op_base.h"
 
 namespace caffe2 {
@@ -8,21 +9,17 @@ class ChannelShuffleOp final : public ConvPoolOpBase<Context> {
  public:
   USE_OPERATOR_FUNCTIONS(Context);
   ChannelShuffleOp(const OperatorDef& operator_def, Workspace* ws)
-      : ConvPoolOpBase<Context>(operator_def, ws) {
-    OPERATOR_NEEDS_FEATURE(
-        this->order_ == StorageOrder::NCHW,
-        "ChannelShuffleOp only supports NCHW order");
-  }
+      : ConvPoolOpBase<Context>(operator_def, ws) {}
 
   bool RunOnDeviceWithOrderNCHW() override {
     const auto& X = Input(0);
     auto* Y = Output(0);
     Y->ResizeLike(X);
     const auto C = X.dim32(1);
-    CAFFE_ENFORCE(C % this->group_ == 0, "");
-    const auto K = C / this->group_;
-    const auto S = X.dim32(2) * X.dim32(3);
     const auto G = this->group_;
+    CAFFE_ENFORCE(C % G == 0, "");
+    const auto K = C / G;
+    const auto S = X.dim32(2) * X.dim32(3);
     for (auto n = 0; n < X.dim32(0); ++n) {
       for (auto g = 0; g < G; ++g) {
         // Scatter the group g block (of size KxS) to output channels
@@ -41,6 +38,29 @@ class ChannelShuffleOp final : public ConvPoolOpBase<Context> {
     }
     return true;
   }
+
+  bool RunOnDeviceWithOrderNHWC() override {
+    const auto& X = Input(0);
+    auto* Y = Output(0);
+    Y->ResizeLike(X);
+    const auto C = X.dim32(3);
+    const auto G = this->group_;
+    CAFFE_ENFORCE(C % G == 0, "");
+    const auto K = C / G;
+    std::array<int, 2> dims = {G, K};
+    std::array<int, 2> axes = {1, 0};
+    for (auto i = 0; i < X.size(); i += C) {
+      // Transpose each C = GxK matrix
+      math::Transpose(
+          2,
+          dims.data(),
+          axes.data(),
+          X.template data<float>() + i,
+          Y->template mutable_data<float>() + i,
+          &context_);
+    }
+    return true;
+  }
 };
 
 template <typename Context>
@@ -48,21 +68,17 @@ class ChannelShuffleGradientOp final : public ConvPoolOpBase<Context> {
  public:
   USE_OPERATOR_FUNCTIONS(Context);
   ChannelShuffleGradientOp(const OperatorDef& operator_def, Workspace* ws)
-      : ConvPoolOpBase<Context>(operator_def, ws) {
-    OPERATOR_NEEDS_FEATURE(
-        this->order_ == StorageOrder::NCHW,
-        "ChannelShuffleOp only supports NCHW order");
-  }
+      : ConvPoolOpBase<Context>(operator_def, ws) {}
 
   bool RunOnDeviceWithOrderNCHW() override {
     const auto& dY = Input(0);
     auto* dX = Output(0);
     dX->ResizeLike(dY);
     const auto C = dY.dim32(1);
-    CAFFE_ENFORCE(C % this->group_ == 0, "");
-    const auto K = C / this->group_;
-    const auto S = dY.dim32(2) * dY.dim32(3);
     const auto G = this->group_;
+    CAFFE_ENFORCE(C % G == 0, "");
+    const auto K = C / G;
+    const auto S = dY.dim32(2) * dY.dim32(3);
     for (auto n = 0; n < dY.dim32(0); ++n) {
       for (auto g = 0; g < G; ++g) {
         // Gather the group g block (of size KxS) from output channels
@@ -81,5 +97,28 @@ class ChannelShuffleGradientOp final : public ConvPoolOpBase<Context> {
     }
     return true;
   }
+
+  bool RunOnDeviceWithOrderNHWC() override {
+    const auto& dY = Input(0);
+    auto* dX = Output(0);
+    dX->ResizeLike(dY);
+    const auto C = dY.dim32(3);
+    const auto G = this->group_;
+    CAFFE_ENFORCE(C % G == 0, "");
+    const auto K = C / G;
+    std::array<int, 2> dims = {K, G};
+    std::array<int, 2> axes = {1, 0};
+    for (auto i = 0; i < dY.size(); i += C) {
+      // Transpose each C = KxG matrix
+      math::Transpose(
+          2,
+          dims.data(),
+          axes.data(),
+          dY.template data<float>() + i,
+          dX->template mutable_data<float>() + i,
+          &context_);
+    }
+    return true;
+  }
 };
-}
+} // namespace caffe2
--- caffe2/operators/channel_shuffle_op_gpu.cu
+++ caffe2/operators/channel_shuffle_op_gpu.cu
@@ -3,7 +3,7 @@
 
 namespace caffe2 {
 
-__global__ void ChannelShuffleKernel(
+__global__ void ChannelShuffleNCHWKernel(
     const int N,
     const int S,
     const int C,
@@ -24,17 +24,34 @@ __global__ void ChannelShuffleKernel(
   }
 }
 
+__global__ void ChannelShuffleNHWCKernel(
+    const int N,
+    const int G,
+    const int K,
+    const float* Xdata,
+    float* Ydata) {
+  CUDA_1D_KERNEL_LOOP(i, N) {
+    const int out_g = i % G;
+    const int i_2 = i / G;
+    const int out_k = i_2 % K;
+    const int n = i_2 / K;
+
+    const int in_c = out_k + K * out_g;
+    Ydata[out_g + G * out_k + G * K * n] = Xdata[in_c + G * K * n];
+  }
+}
+
 template <>
 bool ChannelShuffleOp<CUDAContext>::RunOnDeviceWithOrderNCHW() {
   const auto& X = Input(0);
   auto* Y = Output(0);
   Y->ResizeLike(X);
   const auto C = X.dim32(1);
-  CAFFE_ENFORCE(C % this->group_ == 0, "");
-  const auto K = C / this->group_;
-  const auto S = X.dim32(2) * X.dim32(3);
   const auto G = this->group_;
-  ChannelShuffleKernel<<<
+  CAFFE_ENFORCE(C % G == 0, "");
+  const auto K = C / G;
+  const auto S = X.dim32(2) * X.dim32(3);
+  ChannelShuffleNCHWKernel<<<
       CAFFE_GET_BLOCKS(X.size()),
       CAFFE_CUDA_NUM_THREADS,
       0,
@@ -44,16 +61,34 @@ bool ChannelShuffleOp<CUDAContext>::RunOnDeviceWithOrderNCHW() {
 }
 
 template <>
+bool ChannelShuffleOp<CUDAContext>::RunOnDeviceWithOrderNHWC() {
+  const auto& X = Input(0);
+  auto* Y = Output(0);
+  Y->ResizeLike(X);
+  const auto C = X.dim32(3);
+  const auto G = this->group_;
+  CAFFE_ENFORCE(C % G == 0, "");
+  const auto K = C / G;
+  ChannelShuffleNHWCKernel<<<
+      CAFFE_GET_BLOCKS(X.size()),
+      CAFFE_CUDA_NUM_THREADS,
+      0,
+      context_.cuda_stream()>>>(
+      X.size(), G, K, X.data<float>(), Y->mutable_data<float>());
+  return true;
+}
+
+template <>
 bool ChannelShuffleGradientOp<CUDAContext>::RunOnDeviceWithOrderNCHW() {
   const auto& dY = Input(0);
   auto* dX = Output(0);
   dX->ResizeLike(dY);
   const auto C = dY.dim32(1);
-  CAFFE_ENFORCE(C % this->group_ == 0, "");
-  const auto K = C / this->group_;
-  const auto S = dY.dim32(2) * dY.dim32(3);
   const auto G = this->group_;
-  ChannelShuffleKernel<<<
+  CAFFE_ENFORCE(C % G == 0, "");
+  const auto K = C / G;
+  const auto S = dY.dim32(2) * dY.dim32(3);
+  ChannelShuffleNCHWKernel<<<
       CAFFE_GET_BLOCKS(dY.size()),
       CAFFE_CUDA_NUM_THREADS,
       0,
@@ -62,8 +97,26 @@ bool ChannelShuffleGradientOp<CUDAContext>::RunOnDeviceWithOrderNCHW() {
   return true;
 }
 
+template <>
+bool ChannelShuffleGradientOp<CUDAContext>::RunOnDeviceWithOrderNHWC() {
+  const auto& dY = Input(0);
+  auto* dX = Output(0);
+  dX->ResizeLike(dY);
+  const auto C = dY.dim32(3);
+  const auto G = this->group_;
+  CAFFE_ENFORCE(C % G == 0, "");
+  const auto K = C / G;
+  ChannelShuffleNHWCKernel<<<
+      CAFFE_GET_BLOCKS(dY.size()),
+      CAFFE_CUDA_NUM_THREADS,
+      0,
+      context_.cuda_stream()>>>(
+      dY.size(), K, G, dY.data<float>(), dX->mutable_data<float>());
+  return true;
+}
+
 REGISTER_CUDA_OPERATOR(ChannelShuffle, ChannelShuffleOp<CUDAContext>);
 REGISTER_CUDA_OPERATOR(
     ChannelShuffleGradient,
     ChannelShuffleGradientOp<CUDAContext>);
-}
+} // namespace caffe2
