b50d1fc0e2df3f28b4d090a3ffaa3c183ea50df0
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-04-19 11:45:49 +0200

    Update rnn.py

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -335,8 +335,8 @@ class LSTM(RNNBase):
             f_t = \sigma(W_{if} x_t + b_{if} + W_{hf} h_{(t-1)} + b_{hf}) \\
             g_t = \tanh(W_{ig} x_t + b_{ig} + W_{hg} h_{(t-1)} + b_{hg}) \\
             o_t = \sigma(W_{io} x_t + b_{io} + W_{ho} h_{(t-1)} + b_{ho}) \\
-            c_t = f_t c_{(t-1)} + i_t g_t \\
-            h_t = o_t \tanh(c_t)
+            c_t = f_t * c_{(t-1)} + i_t * g_t \\
+            h_t = o_t * \tanh(c_t)
             \end{array}
 
     where :math:`h_t` is the hidden state at time `t`, :math:`c_t` is the cell
@@ -607,7 +607,7 @@ class LSTMCell(RNNCellBase):
         g = \tanh(W_{ig} x + b_{ig} + W_{hc} h + b_{hg}) \\
         o = \sigma(W_{io} x + b_{io} + W_{ho} h + b_{ho}) \\
         c' = f * c + i * g \\
-        h' = o \tanh(c') \\
+        h' = o * \tanh(c') \\
         \end{array}
 
     where :math:`\sigma` is the sigmoid function.
