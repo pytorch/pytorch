80de0ceb7059607713c77a8117737bf50d54d68b
gchanan <gregchanan@gmail.com>
2018-04-19 14:28:48 -0400

    Revert "Terminate dataloader workers properly when parent process is SIGKILL'ed (#6606)" (#6772)
    This reverts commit 8d6a50aaeba2166ce870016da7488f879395ebb1.

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -11,14 +11,7 @@ import re
 import sys
 import threading
 import traceback
-import os
-import time
 from torch._six import string_classes, int_classes
-
-IS_WINDOWS = sys.platform == "win32"
-if IS_WINDOWS:
-    import ctypes
-    from ctypes.wintypes import DWORD, BOOL, HANDLE
 
 if sys.version_info[0] == 2:
     import Queue as queue
@@ -36,37 +29,6 @@ class ExceptionWrapper(object):
 
 _use_shared_memory = False
 r"""Whether to use shared memory in default_collate"""
-
-MANAGER_STATUS_CHECK_INTERVAL = 5.0
-
-if IS_WINDOWS:
-    class ManagerWatchdog(object):
-        def __init__(self):
-            self.manager_pid = os.getppid()
-
-            self.kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-            self.kernel32.OpenProcess.argtypes = (DWORD, BOOL, DWORD)
-            self.kernel32.OpenProcess.restype = HANDLE
-            self.kernel32.WaitForSingleObject.argtypes = (HANDLE, DWORD)
-            self.kernel32.WaitForSingleObject.restype = DWORD
-
-            # Value obtained from https://msdn.microsoft.com/en-us/library/ms684880.aspx
-            SYNCHRONIZE = 0x00100000
-            self.manager_handle = self.kernel32.OpenProcess(SYNCHRONIZE, 0, self.manager_pid)
-
-            if not self.manager_handle:
-                raise ctypes.WinError(ctypes.get_last_error())
-
-        def is_alive(self):
-            # Value obtained from https://msdn.microsoft.com/en-us/library/windows/desktop/ms687032.aspx
-            return self.kernel32.WaitForSingleObject(self.manager_handle, 0) != 0
-else:
-    class ManagerWatchdog(object):
-        def __init__(self):
-            self.manager_pid = os.getppid()
-
-        def is_alive(self):
-            return os.getppid() == self.manager_pid
 
 
 def _worker_loop(dataset, index_queue, data_queue, collate_fn, seed, init_fn, worker_id):
@@ -86,16 +48,8 @@ def _worker_loop(dataset, index_queue, data_queue, collate_fn, seed, init_fn, wo
     if init_fn is not None:
         init_fn(worker_id)
 
-    watchdog = ManagerWatchdog()
-
     while True:
-        try:
-            r = index_queue.get(timeout=MANAGER_STATUS_CHECK_INTERVAL)
-        except queue.Empty:
-            if watchdog.is_alive():
-                continue
-            else:
-                break
+        r = index_queue.get()
         if r is None:
             break
         idx, batch_indices = r
@@ -245,7 +199,7 @@ class _DataLoaderIter(object):
 
         if self.num_workers > 0:
             self.worker_init_fn = loader.worker_init_fn
-            self.index_queues = [multiprocessing.Queue() for _ in range(self.num_workers)]
+            self.index_queues = [multiprocessing.SimpleQueue() for _ in range(self.num_workers)]
             self.worker_queue_idx = 0
             self.worker_result_queue = multiprocessing.SimpleQueue()
             self.batches_outstanding = 0
