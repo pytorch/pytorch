b66d3b4ade80b29f2b6969db01af419c13fea1fa
Wei Yang <weiyang@fb.com>
2018-04-23 00:15:20 -0700

    cleaned up logs, added stress test for cuda flip
    added stress testing for flip

--- test/test_torch.py
+++ test/test_torch.py
@@ -5523,27 +5523,24 @@ class TestTorch(TestCase):
     def _test_flip(self, use_cuda=False):
         if use_cuda and torch.cuda.is_available():
             x = torch.Tensor([1,2,3,4,5,6,7,8]).view(2, 2, 2).cuda()
+            # stress testing
+            y = torch.arange(0, 100000000).view(10000, 10000).cuda()
+            y.flip([0])
         else:
             x = torch.Tensor([1,2,3,4,5,6,7,8]).view(2, 2, 2)
 
         x_flip_0 = x.flip([0])
-        print(x_flip_0)
-        # self.assertEqual(torch.Tensor([5,6,7,8,1,2,3,4]).view(2, 2, 2), x_flip_0)
+        self.assertEqual(torch.Tensor([5,6,7,8,1,2,3,4]).view(2, 2, 2), x_flip_0)
 
         x_flip_01 = x.flip([0,1])
-        # self.assertEqual(torch.Tensor([7,8,5,6,3,4,1,2]).view(2, 2, 2), x_flip_01)
+        self.assertEqual(torch.Tensor([7,8,5,6,3,4,1,2]).view(2, 2, 2), x_flip_01)
 
         x_flip_012 = x.flip([0,1,2])
-        # self.assertEqual(torch.Tensor([8,7,6,5,4,3,2,1]).view(2, 2, 2), x_flip_012)
+        self.assertEqual(torch.Tensor([8,7,6,5,4,3,2,1]).view(2, 2, 2), x_flip_012)
 
         # test duplicated dims
         x_flip_001 = x.flip([0,0,1])
-        # self.assertEqual(x_flip_01, x_flip_001)
-
-        try:
-            y = x.flip([])
-        except Exception as e:
-            print(e)
+        self.assertEqual(x_flip_01, x_flip_001)
 
     def test_flip(self):
         self._test_flip(self, use_cuda=False)
