335040f6ed3090d0c1d4f986a4924d30e2477bcd
Zach DeVito <zdevito@fb.com>
2018-04-23 14:24:46 -0700

    [jit][script] Fix a bug combining sizes/unsized tensors
    This add an isSubtypeOf method to reflect that sized tensors are a subtype
    of Dynamic[Tensors]. It updates the typechecking code to reflect this
    relationship.

--- test/test_jit.py
+++ test/test_jit.py
@@ -2541,7 +2541,7 @@ class TestScript(TestCase):
         v = torch.rand(10, 3)
         self.assertEqual(torch.chunk(v, dim=0, chunks=2)[0], foo(v))
 
-        with self.assertRaisesRegex(RuntimeError, r"variable 'a' previously has type \(Dynamic, Dynamic\)"):
+        with self.assertRaisesRegex(RuntimeError, r"variable 'a' previously has type \(Tensor, Tensor\)"):
             @torch.jit.script
             def mixtypes():
                 a = torch.chunk(1, dim=0, chunks=2)
@@ -2773,6 +2773,19 @@ class TestScript(TestCase):
         self.assertEqual(result, reference)
         self.assertExpected(torch.onnx._export_to_pretty_string(
             mte, (torch.ones(2, 3),), None, verbose=False))
+
+    def test_trace_with_size(self):
+        @torch.jit.trace(torch.zeros(1, 1))
+        def foo(x):
+            return x + 1
+
+        def bar(x):
+            y = foo(x)
+            if True:
+                y = 7
+            return y + 1
+
+        self.assertEqual(8, bar(torch.ones(1, 1)))
 
 
 # Smoke tests for export methods
