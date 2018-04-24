d985cf46f18d202756dc9ce2ddbebd9507b29d43
Zachary DeVito <zdevito@gmail.com>
2018-04-24 12:30:19 -0700

    Add workaround to fix include warnings in Python 2 builds. (#6716)

---
+++ torch/csrc/python_headers.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// workaround for Python 2 issue: https://bugs.python.org/issue17120
+#pragma push_macro("_XOPEN_SOURCE")
+#pragma push_macro("_POSIX_C_SOURCE")
+#undef _XOPEN_SOURCE
+#undef _POSIX_C_SOURCE
+
+#include <Python.h>
+
+#pragma pop_macro("_XOPEN_SOURCE")
+#pragma pop_macro("_POSIX_C_SOURCE")
