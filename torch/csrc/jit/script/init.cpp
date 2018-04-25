66300becef72318b6ec9bfd699634dcc8d9b2997
Zachary DeVito <zdevito@gmail.com>
2018-04-24 17:21:00 -0700

    Workaround to make PythonOps traced with torch.jit.trace work correctly. (#6738)
    The long-term fix is to remove the handling-creating pathways and
    remove all the modes from PythonOp making it into an op that simply
    calls a PyObject. Right now ONNX expects PythonOp to hold a
    nn.Function, not a generic callable, so completely removing the legacy
    pathway will also require changes to how ONNX symbolics are found.

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -43,7 +43,7 @@ struct VISIBILITY_HIDDEN PythonValue : public SugaredValue {
     py::object func = self;
     std::string cconv(inputs.size(), 't');
     Node* new_node = g.insertNode(g.createPythonOp(
-      THPObjectPtr(func.release().ptr()), cconv, false, {}, {}, false));
+      THPObjectPtr(func.release().ptr()), cconv, {}, {}, false));
     new_node->setSourceLocation(std::make_shared<SourceRange>(loc));
     for(auto i : inputs)
       new_node->addInput(i);
