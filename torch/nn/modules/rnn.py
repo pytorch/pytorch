8f7677d2a59661495ba96aeef91fc6a18c51a6d3
rolczynski <rafal.rolczynski@gmail.com>
2018-04-25 17:54:33 +0200

    fix typo in the LSTMCell math definition

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -605,7 +605,7 @@ class LSTMCell(RNNCellBase):
         \begin{array}{ll}
         i = \sigma(W_{ii} x + b_{ii} + W_{hi} h + b_{hi}) \\
         f = \sigma(W_{if} x + b_{if} + W_{hf} h + b_{hf}) \\
-        g = \tanh(W_{ig} x + b_{ig} + W_{hc} h + b_{hg}) \\
+        g = \tanh(W_{ig} x + b_{ig} + W_{hg} h + b_{hg}) \\
         o = \sigma(W_{io} x + b_{io} + W_{ho} h + b_{ho}) \\
         c' = f * c + i * g \\
         h' = o \tanh(c') \\
