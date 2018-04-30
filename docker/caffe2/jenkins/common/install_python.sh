764b91933a0202dfc8445954a4b6cce67b085799
pjh5 <hellemn@fb.com>
2018-04-30 11:05:24 -0700

    Location of pip package changed

--- docker/caffe2/jenkins/common/install_python.sh
+++ docker/caffe2/jenkins/common/install_python.sh
@@ -115,7 +115,7 @@ fi
 # The python-pip package on Ubuntu Trusty is old
 # and upon install numpy doesn't use the binary
 # distribution, and fails to compile it from source.
-curl -O https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
+curl -O https://files.pythonhosted.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
 tar zxf pip-9.0.1.tar.gz
 pushd pip-9.0.1
 "$PYTHON" setup.py install
