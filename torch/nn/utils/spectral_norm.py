76f8956fc3145d8e366e0a982bbd60bce812a280
crcrpar <masaki.kozuki.2014@gmail.com>
2018-04-30 20:54:04 +0900

    fix docs
    fix typos
    add comments related to power iteration and epsilon
    update link to the paper
    make some comments specific

--- torch/nn/utils/spectral_norm.py
+++ torch/nn/utils/spectral_norm.py
@@ -19,6 +19,9 @@ class SpectralNorm(object):
         height = weight.size(0)
         weight_mat = weight.view(height, -1)
         for _ in range(self.n_power_iterations):
+            # Spectral norm of weight equals to `u^T W v`, where `u` and `v`
+            # are the first left and right singular vecors.
+            # This power iteration produces approximations of `u` and `v`.
             v = normalize(torch.matmul(weight_mat.t(), u), dim=0, eps=self.eps)
             u = normalize(torch.matmul(weight_mat, v), dim=0, eps=self.eps)
 
@@ -44,7 +47,7 @@ class SpectralNorm(object):
         weight = module._parameters[name]
         height = weight.size(0)
 
-        u = normalize(weight.data.new(height).normal_(0, 1), dim=0, eps=fn.eps)
+        u = normalize(weight.new_empty(height).normal_(0, 1), dim=0, eps=fn.eps)
         module.register_parameter(fn.name + "_org", weight)
         module.register_buffer(fn.name + "_u", u)
 
@@ -60,21 +63,24 @@ def spectral_norm(module, name='weight', n_power_iterations=1, eps=1e-12):
          \sigma(\mathbf{W}) &= \max_{\mathbf{h}: \mathbf{h} \ne 0} \dfrac{\|\mathbf{W} \mathbf{h}\|_2}{\|\mathbf{h}\|_2}
 
     Spectral normalization stabilizes the training of discriminators (critics)
-    in GANs by rescaling the weight tensor with spectral norm :math:`\sigma` of
-    the weight matrix calculated using power iteration method. If the dimension
-    of the weight tensor is greater than 2, reshaped to 2D in power iteration method
-    to get spectral norm. This is implemented via a hook that calculates
-    spectral norm and rescales weight before every :meth:`~Module.forward`
-    call.
+    in Generaive Adversarial Networks (GANs) by rescaling the weight tensor
+    with spectral norm :math:`\sigma` of the weight matrix calculated using
+    power iteration method. If the dimension of the weight tensor is greater
+    than 2, it is reshaped to 2D in power iteration method to get spectral
+    norm. This is implemented via a hook that calculates spectral norm and
+    rescales weight before every :meth:`~Module.forward` call.
 
-    See https://arxiv.org/abs/1802.05957
+    See `Spectral Normalization for Generative Adversarial Networks`_ .
+
+    .. _`Spectral Normalization for Generative Adversarial Networks`: https://arxiv.org/abs/1802.05957
 
     Args:
         module (nn.Module): containing module
         name (str, optional): name of weight parameter
         n_power_iterations (int, optional): number of power iterations to
             calculate spectal norm
-        eps (float, optional): epsilon for numerical stability
+        eps (float, optional): epsilon for numerical stability in
+            calculating norms
 
     Returns:
         The original module with the spectal norm hook
@@ -95,7 +101,7 @@ def remove_spectral_norm(module, name='weight'):
     r"""Removes the spectral normalization reparameterization from a module.
 
     Args:
-        module (nn.Modue): containing module
+        module (nn.Module): containing module
         name (str, optional): name of weight parameter
 
     Example:
