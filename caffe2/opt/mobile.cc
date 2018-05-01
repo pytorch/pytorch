967c4a0c18dc6a18efc1afbe3f0afbca2cf496a3
Bram Wasti <bwasti@fb.com>
2018-05-01 16:26:54 -0700

    [caffe2][nomnigraph] Fix NNPACK relu fusion for inplace relu (#7124)

--- caffe2/opt/mobile.cc
+++ caffe2/opt/mobile.cc
@@ -149,9 +149,19 @@ caffe2::NetDef fuseNNPACKConvRelu(caffe2::NetDef net) {
     }
     auto relu_output = relu_outputs.front();
 
-    nn.dataFlow.replaceNode(relu_output, conv_output);
-    nn.dataFlow.deleteNode(relu_node);
-    nn.dataFlow.deleteNode(relu_output);
+    auto output_tensor = repr::nn::get<repr::Tensor>(relu_output);
+    auto input_tensor = repr::nn::get<repr::Tensor>(repr::nn::getInputs(conv_node).front());
+
+    if (output_tensor->getName() != input_tensor->getName()) {
+      nn.dataFlow.replaceNode(conv_output, relu_output);
+      nn.dataFlow.deleteNode(relu_node);
+      nn.dataFlow.deleteNode(conv_output);
+    } else {
+      nn.dataFlow.replaceNode(relu_output, conv_output);
+      nn.dataFlow.deleteNode(relu_node);
+      nn.dataFlow.deleteNode(relu_output);
+    }
+
     auto* arg = op->add_arg();
     arg->set_name("activation");
     arg->set_s("Relu");
