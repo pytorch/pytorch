6eebb4c2fb8fcd5c3536f5354a6bbf2fd209bd51
theweiho <theweiho@users.noreply.github.com>
2018-04-30 17:01:27 -0700

    Open-source extractMetaNetDef & runGlobalInitialization, add new Predictor constructor from db file, and add run_map_outputs (#7063)
    * Refactor extractMetaNetDef and runGlobalInitialization into open...
    
    * Fix test by making get output blobs optional
    
    * Update test instead of making output blobs optional

--- caffe2/core/predictor.h
+++ caffe2/core/predictor.h
@@ -15,15 +15,20 @@ class Predictor {
 
   // MetaNetDef contains 'init_net', 'run_net', and meta-info
   // The meta-info is used to verify inputs are correctly passed
-  Predictor(const MetaNetDef& net, Workspace* parent = nullptr);
+  Predictor(
+      const MetaNetDef& net,
+      Workspace* parent = nullptr,
+      bool run_init = true);
 
   // Runs the `init_net` once, then saves the `run_net` to be executed
   // in `::run`
   Predictor(
       const NetDef& init_net,
       const NetDef& run_net,
-      Workspace* parent = nullptr);
-  ~Predictor();
+      Workspace* parent = nullptr,
+      bool run_init = true);
+
+  ~Predictor() {}
 
   // Executes `run_net` on the inputs.
   // The first `inputs.size()` inputs from run_net::external_inputs
@@ -41,6 +46,10 @@ class Predictor {
   // Similar to run, but consumes a map of name to tensor as input
   bool run_map(const TensorMap& inputs, TensorVector* outputs);
 
+  // Similar to the other run fns, except inputs and outputs are both maps of
+  // string name to tensor.
+  bool run_map_outputs(const TensorMap& inputs, TensorMap* outputs);
+
   const NetDef& def() const {
     return run_net_;
   };
@@ -49,9 +58,22 @@ class Predictor {
     return &ws_;
   };
 
+  const std::unordered_set<std::string>& input_names() const {
+    return inputNames_;
+  }
+
+  const std::vector<std::string>& output_names() const {
+    return outputNames_;
+  }
+
  private:
+  bool run_map_workspace(const TensorMap& inputs);
+
   NetDef run_net_;
   Workspace ws_;
   std::unordered_set<std::string> inputNames_;
+  // Outputs need to be ordered since TensorVector outputs rely on the outputs
+  // being in a certain order.
+  std::vector<std::string> outputNames_;
 };
 }
