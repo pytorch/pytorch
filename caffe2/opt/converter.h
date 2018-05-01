fa943200523f88658e37c65c53093084067f9690
Bram Wasti <bwasti@fb.com>
2018-05-01 15:31:07 -0700

    [caffe2][nomnigraph] Add ability to pass the old net to convertToCaffe2Proto (#7149)

--- caffe2/opt/converter.h
+++ caffe2/opt/converter.h
@@ -45,6 +45,11 @@ nom::repr::NNModule convertToNNModule(caffe2::NetDef &net, std::unordered_map<st
 
 caffe2::NetDef convertToCaffe2Proto(nom::repr::NNModule&);
 
+// Pass in an oldNet to copy all the attributes of that network.
+// Be warned that transformations that modify the graph's inputs or outputs
+// are not reflected in changes to external_input or external_output.
+caffe2::NetDef convertToCaffe2Proto(nom::repr::NNModule&, const caffe2::NetDef& oldNet);
+
 std::unique_ptr<nom::repr::NeuralNetOperator> convertToOperatorDef(caffe2::OperatorDef op);
 
 } // namespace caffe2 
