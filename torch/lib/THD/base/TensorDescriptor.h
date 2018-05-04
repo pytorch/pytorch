d0bef52cbb3ded01d4f3acbd79f09fea89f5c88e
Gregory Chanan <gchanan@fb.com>
2018-05-04 09:15:31 -0700

    Remove THDTensorDescriptor_newFromTH{X}Tensor.
    They don't seem to be used and we are moving to a single TensorImpl model.

--- torch/lib/THD/base/TensorDescriptor.h
+++ torch/lib/THD/base/TensorDescriptor.h
@@ -10,21 +10,3 @@
 #include <ATen/ATen.h>
 using THDTensorDescriptor = at::Tensor;
 #endif
-
-THDTensorDescriptor THDTensorDescriptor_newFromTHDoubleTensor(THDoubleTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHFloatTensor(THFloatTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHLongTensor(THLongTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHIntTensor(THIntTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHShortTensor(THShortTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCharTensor(THCharTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHByteTensor(THByteTensor *tensor);
-#ifdef WITH_CUDA
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaDoubleTensor(THCudaDoubleTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaFloatTensor(THCudaTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaHalfTensor(THCudaHalfTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaLongTensor(THCudaLongTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaIntTensor(THCudaIntTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaShortTensor(THCudaShortTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaCharTensor(THCudaCharTensor *tensor);
-THDTensorDescriptor THDTensorDescriptor_newFromTHCudaByteTensor(THCudaByteTensor *tensor);
-#endif
