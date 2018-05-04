571e019c3ea48e3fb5bbf785b43fd624a20784f1
Tongzhou Wang <SsnL@users.noreply.github.com>
2018-05-04 16:38:38 -0400

    Fix onnx.symbolic.upsample_bilinear2d not considering align_corners (#7264)

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -505,7 +505,9 @@ def upsample_nearest2d(g, input, scale_factor):
                 height_scale_f=scale_factor, mode_s="nearest")
 
 
-def upsample_bilinear2d(g, input, output_size):
+def upsample_bilinear2d(g, input, output_size, align_corners):
+    if align_corners:
+        return _unimplemented("upsample_bilinear2d", "align_corners == True")
     w_scale = float(output_size[-1]) / input.type().sizes()[-1]
     h_scale = float(output_size[-2]) / input.type().sizes()[-2]
     return g.op("Upsample", input, width_scale_f=w_scale,
