pytorch/pytorch

commit 429d66549e893995938a54e8fba8b58a31c18106

If available try to use requests instead of urllib for model_zoo.load_url  (#3280)
* try to use requests instead of urllib for load_url if available

* add noqa to silence flake warnings

* remove urllib import into the except

torch/utils/model_zoo.py
@@ -6,12 +6,19 @@ import re
 import shutil
 import sys
 import tempfile
-if sys.version_info[0] == 2:
-    from urlparse import urlparse
-    from urllib2 import urlopen
-else:
-    from urllib.request import urlopen
-    from urllib.parse import urlparse
+
+try:
+    from requests.utils import urlparse
+    import requests.get as urlopen
+    requests_available = True
+except ImportError:
+    requests_available = False
+    if sys.version_info[0] == 2:
+        from urlparse import urlparse  # noqa f811
+        from urllib2 import urlopen  # noqa f811
+    else:
+        from urllib.request import urlopen
+        from urllib.parse import urlparse
 try:
     from tqdm import tqdm
 except ImportError:
@@ -60,11 +67,15 @@ def load_url(url, model_dir=None, map_location=None):
 
 def _download_url_to_file(url, dst, hash_prefix):
     u = urlopen(url)
-    meta = u.info()
-    if hasattr(meta, 'getheaders'):
-        file_size = int(meta.getheaders("Content-Length")[0])
+    if requests_available:
+        file_size = int(u.headers["Content-Length"])
+        u = u.raw
     else:
-        file_size = int(meta.get_all("Content-Length")[0])
+        meta = u.info()
+        if hasattr(meta, 'getheaders'):
+            file_size = int(meta.getheaders("Content-Length")[0])
+        else:
+            file_size = int(meta.get_all("Content-Length")[0])
 
     f = tempfile.NamedTemporaryFile(delete=False)
     try:

commit 3c6e46c89d7a30b2089a981ae632cd0f0d6f1cbc

Fix bug that introduced in pull #3280
Apparently get() is a function of requests, not a module (not sure if in
the past get() used to be a module). Therefore, the syntax in #3280 will
alway fail with ImportError, and requests lib will never be used (kind
of defeat the purpose of that pull request).
Also, if requests lib is used, should add stream=True parameter,
otherwise requests.get() will load the whole response into memory.

torch/utils/model_zoo.py
@@ -9,7 +9,7 @@ import tempfile
 
 try:
     from requests.utils import urlparse
-    import requests.get as urlopen
+    from requests import get as urlopen
     requests_available = True
 except ImportError:
     requests_available = False
@@ -67,11 +67,12 @@ def load_url(url, model_dir=None, map_location=None, progress=True):
 
 
 def _download_url_to_file(url, dst, hash_prefix, progress):
-    u = urlopen(url)
     if requests_available:
+        u = urlopen(url, stream=True)
         file_size = int(u.headers["Content-Length"])
         u = u.raw
     else:
+        u = urlopen(url)
         meta = u.info()
         if hasattr(meta, 'getheaders'):
             file_size = int(meta.getheaders("Content-Length")[0])