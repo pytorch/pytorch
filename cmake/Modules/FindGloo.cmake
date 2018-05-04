78801ff65e00ce2507cd525d63df6597db764644
Orion Reblitz-Richardson <orionr@gmail.com>
2018-05-04 12:27:20 -0700

    Move all Find*.cmake to cmake/Modules

--- cmake/Modules/FindGloo.cmake
+++ cmake/Modules/FindGloo.cmake
@@ -1,45 +1,22 @@
-# Try to find the Gloo library and headers.
-#  Gloo_FOUND        - system has Gloo lib
-#  Gloo_INCLUDE_DIRS - the Gloo include directory
-#  Gloo_LIBRARIES    - libraries needed to use Gloo
-
-find_path(Gloo_INCLUDE_DIR
-	NAMES gloo/common/common.h
-	DOC "The directory where Gloo includes reside"
-)
-
-find_library(Gloo_NATIVE_LIBRARY
-	NAMES gloo
-	DOC "The Gloo library (without CUDA)"
-)
-
-find_library(Gloo_CUDA_LIBRARY
-	NAMES gloo_cuda
-	DOC "The Gloo library (with CUDA)"
-)
-
-set(Gloo_INCLUDE_DIRS ${Gloo_INCLUDE_DIR})
-
-# use the CUDA library depending on the Gloo_USE_CUDA variable
-if (DEFINED Gloo_USE_CUDA)
-	if (${Gloo_USE_CUDA})
-		set(Gloo_LIBRARY ${Gloo_CUDA_LIBRARY})
-	else()
-		set(Gloo_LIBRARY ${Gloo_NATIVE_LIBRARY})
-	endif()
-else()
-	# else try to use the CUDA library if found
-	if (${Gloo_CUDA_LIBRARY} STREQUAL "Gloo_CUDA_LIBRARY-NOTFOUND")
-		set(Gloo_LIBRARY ${Gloo_NATIVE_LIBRARY})
-	else()
-		set(Gloo_LIBRARY ${Gloo_CUDA_LIBRARY})
-	endif()
-endif()
+IF(NOT GLOO_FOUND)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Gloo
-	FOUND_VAR Gloo_FOUND
-	REQUIRED_VARS Gloo_INCLUDE_DIR Gloo_LIBRARY
-)
 
-mark_as_advanced(Gloo_FOUND)
+SET(GLOO_LIBRARIES)
+SET(GLOO_INCLUDE_DIR)
+
+FIND_LIBRARY(GLOO_HOST_LIBRARIES gloo)
+FIND_LIBRARY(GLOO_CUDA_LIBRARIES gloo_cuda)
+FIND_PATH(GLOO_INCLUDE_DIR gloo)
+SET(GLOO_LIBRARIES ${GLOO_HOST_LIBRARIES} ${GLOO_CUDA_LIBRARIES})
+
+IF (GLOO_LIBRARIES)
+  SET(GLOO_FOUND TRUE)
+ELSE (GLOO_LIBRARIES)
+  SET(GLOO_FOUND FALSE)
+ENDIF (GLOO_LIBRARIES)
+
+MESSAGE(STATUS "Found Gloo: ${GLOO_FOUND}")
+
+ENDIF(NOT GLOO_FOUND)
+
--- cmake/Modules/FindNCCL.cmake
+++ cmake/Modules/FindNCCL.cmake
@@ -1,28 +1,20 @@
-# Find the nccl libraries
+# - Try to find NCCL
 #
 # The following variables are optionally searched for defaults
 #  NCCL_ROOT_DIR: Base directory where all NCCL components are found
-#  NCCL_INCLUDE_DIR: Directory where NCCL header is found
-#  NCCL_LIB_DIR: Directory where NCCL library is found
 #
 # The following are set after configuration is done:
 #  NCCL_FOUND
 #  NCCL_INCLUDE_DIRS
 #  NCCL_LIBRARIES
-#
-# The path hints include CUDA_TOOLKIT_ROOT_DIR seeing as some folks
-# install NCCL in the same location as the CUDA toolkit.
-# See https://github.com/caffe2/caffe2/issues/1601
 
-set(NCCL_ROOT_DIR "" CACHE PATH "Folder contains NVIDIA NCCL")
+set (NCCL_ROOT_DIR $ENV{NCCL_ROOT_DIR})
 
-find_path(NCCL_INCLUDE_DIRS
+find_path (NCCL_INCLUDE_DIR
   NAMES nccl.h
   HINTS
-  ${NCCL_INCLUDE_DIR}
   ${NCCL_ROOT_DIR}
-  ${NCCL_ROOT_DIR}/include
-  ${CUDA_TOOLKIT_ROOT_DIR}/include)
+  ${NCCL_ROOT_DIR}/include)
 
 IF ($ENV{USE_STATIC_NCCL})
   MESSAGE(STATUS "USE_STATIC_NCCL detected. Linking against static NCCL library")
@@ -31,20 +23,29 @@ ELSE()
   SET(NCCL_LIBNAME "nccl")
 ENDIF()
 
-find_library(NCCL_LIBRARIES
+find_library (NCCL_LIBRARY
   NAMES ${NCCL_LIBNAME}
   HINTS
-  ${NCCL_LIB_DIR}
   ${NCCL_ROOT_DIR}
   ${NCCL_ROOT_DIR}/lib
   ${NCCL_ROOT_DIR}/lib/x86_64-linux-gnu
-  ${NCCL_ROOT_DIR}/lib64
-  ${CUDA_TOOLKIT_ROOT_DIR}/lib64)
+  ${NCCL_ROOT_DIR}/lib64)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NCCL DEFAULT_MSG NCCL_INCLUDE_DIRS NCCL_LIBRARIES)
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (NCCL DEFAULT_MSG NCCL_INCLUDE_DIR NCCL_LIBRARY)
 
-if(NCCL_FOUND)
-  message(STATUS "Found NCCL (include: ${NCCL_INCLUDE_DIRS}, library: ${NCCL_LIBRARIES})")
-  mark_as_advanced(NCCL_ROOT_DIR NCCL_INCLUDE_DIRS NCCL_LIBRARIES)
-endif()
+if (NCCL_FOUND)
+  set (NCCL_HEADER_FILE "${NCCL_INCLUDE_DIR}/nccl.h")
+  message (STATUS "Determining NCCL version from the header file: ${NCCL_HEADER_FILE}")
+  file (STRINGS ${NCCL_HEADER_FILE} NCCL_MAJOR_VERSION_DEFINED
+        REGEX "^[ \t]*#define[ \t]+NCCL_MAJOR[ \t]+[0-9]+.*$" LIMIT_COUNT 1)
+  if (NCCL_MAJOR_VERSION_DEFINED)
+    string (REGEX REPLACE "^[ \t]*#define[ \t]+NCCL_MAJOR[ \t]+" ""
+            NCCL_MAJOR_VERSION ${NCCL_MAJOR_VERSION_DEFINED})
+    message (STATUS "NCCL_MAJOR_VERSION: ${NCCL_MAJOR_VERSION}")
+  endif ()
+  set (NCCL_INCLUDE_DIRS ${NCCL_INCLUDE_DIR})
+  set (NCCL_LIBRARIES ${NCCL_LIBRARY})
+  message (STATUS "Found NCCL (include: ${NCCL_INCLUDE_DIRS}, library: ${NCCL_LIBRARIES})")
+  mark_as_advanced (NCCL_ROOT_DIR NCCL_INCLUDE_DIRS NCCL_LIBRARIES)
+endif ()
