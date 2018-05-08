478bc10b92109f671586e460ae4d1abe3e1f6522
Paul Jesse Hellemn <jesse.hellemn@gmail.com>
2018-05-07 22:55:50 -0700

    [Caffe2] Pinning conda-numpy to 1.14 to avoid SVD issue (#7344)
    * Pinning conda-numpy to 1.14 to avoid SVD issue
    
    * Adding another leveldb test to conda's ignored tests, removing a mkl-test from this
    
    * Removing commented out section

--- .jenkins/caffe2/test.sh
+++ .jenkins/caffe2/test.sh
@@ -66,10 +66,6 @@ for test in $INSTALL_PREFIX/test/*; do
     mkl_utils_test)
       continue
       ;;
-    # TODO investigate conv_op_test failures when using MKL
-    conv_op_test)
-      continue
-      ;;
   esac
 
   "$test" --gtest_output=xml:"$TEST_DIR"/cpp/$(basename "$test").xml
@@ -90,10 +86,11 @@ if [[ "$BUILD_ENVIRONMENT" == *-cuda* ]]; then
   EXTRA_TESTS+=("$CAFFE2_PYPATH/contrib/nccl")
 fi
 
-# TODO find out why this breaks for conda builds
+conda_ignore_test=()
 if [[ $BUILD_ENVIRONMENT == conda* ]]; then
-  conda_ignore_test="--ignore $CAFFE2_PYPATH/python/tt_core_test.py"
-  conda_ignore_test="--ignore $CAFFE2_PYPATH/python/dataio_test.py"
+  # These tests both assume Caffe2 was built with leveldb, which is not the case
+  conda_ignore_test+=("--ignore $CAFFE2_PYPATH/python/dataio_test.py")
+  conda_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/checkpoint_test.py")
 fi
 
 # Python tests
@@ -107,7 +104,7 @@ echo "Running Python tests.."
   --ignore "$CAFFE2_PYPATH/python/operator_test/matmul_op_test.py" \
   --ignore "$CAFFE2_PYPATH/python/operator_test/pack_ops_test.py" \
   --ignore "$CAFFE2_PYPATH/python/mkl/mkl_sbn_speed_test.py" \
-  $conda_ignore_test \
+  ${conda_ignore_test[@]} \
   "$CAFFE2_PYPATH/python" \
   "${EXTRA_TESTS[@]}"
 
