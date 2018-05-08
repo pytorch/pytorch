f32e45614ab9fdec062a7abc9e29af62d2917948
Yinghai Lu <yinghai@fb.com>
2018-05-08 11:41:08 -0700

    Add conv+relu fusion for MKLDNN ops (IDEEP)

---
+++ caffe2/opt/optimize_ideep.cc
@@ -0,0 +1,46 @@
+#include "caffe2/opt/optimize_ideep.h"
+#include "caffe2/opt/converter.h"
+#include "caffe2/opt/fuse_conv_relu.h"
+
+namespace caffe2 {
+namespace opt {
+
+using namespace nom;
+caffe2::NetDef OptimizeForIdeep(caffe2::NetDef net) {
+  auto nn = convertToNNModule(net);
+
+  // Conv+Relu fusion
+  auto should_fuse = [](const repr::Conv& conv) {
+    const auto annotation = conv.getAnnotation();
+    if (!annotation || !isa<Caffe2Annotation>(annotation)) {
+      return false;
+    }
+    const auto* op = dyn_cast<Caffe2Annotation>(annotation)->getOperatorDef();
+
+    // We only want to fuse for fast NNPACK convs
+    if (op->device_option().device_type() != DeviceType::IDEEP) {
+      return false;
+    }
+
+    return true;
+  };
+  auto postprocess = [](repr::NNGraph::NodeRef conv_node) {
+    auto conv = repr::nn::get<repr::Conv>(conv_node);
+    auto annotation = conv->getMutableAnnotation();
+    if (!annotation || !isa<Caffe2Annotation>(annotation)) {
+      return;
+    }
+    auto* op = dyn_cast<Caffe2Annotation>(annotation)->getMutableOperatorDef();
+    op->set_type("ConvFusion");
+    auto* arg = op->add_arg();
+    arg->set_name("fusion_type");
+    // 1 means FUSION_CONV_RELU
+    arg->set_i(1);
+  };
+  fuseConvRelu(&nn, should_fuse, postprocess);
+
+  // Currently no more other optimization passes
+  return convertToCaffe2Proto(nn, net);
+}
+} // namespace opt
+} // namespace caffe2
--- caffe2/python/transformations.py
+++ caffe2/python/transformations.py
@@ -37,3 +37,8 @@ def sinkMaxPool(net):
     net.Proto().ParseFromString(
         C.transform_sinkMaxPool(net.Proto().SerializeToString())
     )
+
+def optimizeForIDEEP(net):
+    net.Proto().ParseFromString(
+        C.transform_optimizeForIDEEP(net.Proto().SerializeToString())
+    )
