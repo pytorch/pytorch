b1fbf29b52089fb4e7957be6cb7e55c794fa3982
Bram Wasti <bwasti@fb.com>
2018-05-08 17:43:51 -0700

    [caffe2][nomnigraph] Change the standard transform API to take in NNModule rather than NetDef (#7308)

--- caffe2/opt/sink.h
+++ caffe2/opt/sink.h
@@ -3,11 +3,12 @@
 
 #include "caffe2/core/common.h"
 #include "caffe2/proto/caffe2.pb.h"
+#include "nomnigraph/Representations/NeuralNet.h"
 
 namespace caffe2 {
 namespace opt {
 
-caffe2::NetDef sinkMaxPool(caffe2::NetDef net);
+void sinkMaxPool(nom::repr::NNModule* nn);
 
 } // namespace opt
 } // namespace caffe2
