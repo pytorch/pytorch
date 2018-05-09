cb037f774a6aa2c441c8af4f1eb601d4d8eea0c2
Jinghui <31264804+gujinghui@users.noreply.github.com>
2018-05-09 23:52:24 +0800

    [Caffe2] [feature request] Add gradient operators for IDEEP (#7234)
    * Add gradient operators for IDEEP
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Add gradient test cases for IDEEP
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Upgrade third_party/ideep
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Refine SumOp for IDEEP
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Share input buffer in fallback op if possible
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Fallback ConvTranspose op for IDEEP
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Fix bug introduced by the patch of sharing input buffer
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Share output buffer in fallback operators
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Remove IDEEP to resolve repo issue
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Reflash IDEEP repo
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Remove redundant lines in IDEEP
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>
    
    * Fallback operators for IDEEP
    (Flatten, ResizeLike, Transpose, and Reshape)
    
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>

--- caffe2/python/ideep/spatial_bn_op_test.py
+++ caffe2/python/ideep/spatial_bn_op_test.py
@@ -165,5 +165,40 @@ class TestSpatialBN(hu.HypothesisTestCase):
 
         workspace.SwitchWorkspace(old_ws_name)
 
+    @given(size=st.integers(7, 10),
+           input_channels=st.integers(1, 10),
+           batch_size=st.integers(1, 3),
+           seed=st.integers(0, 65535),
+           order=st.sampled_from(["NCHW"]),
+           epsilon=st.floats(min_value=1e-5, max_value=1e-2),
+           **mu.gcs)
+    @settings(deadline=None)
+    def test_spatialbn_train_mode_gradient_check(
+            self, size, input_channels, batch_size, seed, order, epsilon,
+            gc, dc):
+        op = core.CreateOperator(
+            "SpatialBN",
+            ["X", "scale", "bias", "mean", "var"],
+            ["Y", "mean", "var", "saved_mean", "saved_var"],
+            order=order,
+            is_test=False,
+            epsilon=epsilon,
+        )
+        np.random.seed(seed)
+        scale = np.random.rand(input_channels).astype(np.float32) + 0.5
+        bias = np.random.rand(input_channels).astype(np.float32) - 0.5
+        mean = np.random.randn(input_channels).astype(np.float32)
+        var = np.random.rand(input_channels).astype(np.float32) + 0.5
+        X = np.random.rand(
+            batch_size, input_channels, size, size).astype(np.float32) - 0.5
+        if order == "NHWC":
+            X = X.swapaxes(1, 2).swapaxes(2, 3)
+
+        for input_to_check in [0, 1, 2]:  # dX, dScale, dBias
+            self.assertGradientChecks(gc, op, [X, scale, bias, mean, var],
+                                      input_to_check, [0])
+
+
+
 if __name__ == "__main__":
     unittest.main()
