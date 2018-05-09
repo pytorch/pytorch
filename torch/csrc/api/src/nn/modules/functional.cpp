b35ff7ff3681e49a1a18c4ffcb76962946f23892
Peter Goldsborough <peter@goldsborough.me>
2018-05-09 11:18:01 -0700

    Add functional.cpp to CMakeLists.txt

--- torch/csrc/api/src/nn/modules/functional.cpp
+++ torch/csrc/api/src/nn/modules/functional.cpp
@@ -2,13 +2,13 @@
 
 namespace torch { namespace nn {
 Functional::Functional(std::function<variable_list(variable_list)> fun)
-    : fun_(fun) {}
+    : fun_(std::move(fun)) {}
 Functional::Functional(std::function<Variable(Variable)> fun)
     : fun_([fun](variable_list input) {
         return variable_list({fun(input[0])});
-      }){};
+      }) {}
 
-variable_list Functional::forward(variable_list input) override {
+variable_list Functional::forward(variable_list input) {
   return fun_(input);
-};
+}
 }} // namespace torch::nn
