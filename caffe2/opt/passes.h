76fd0d380243e94193c9342139557e521b800020
Bram Wasti <bwasti@fb.com>
2018-05-17 16:33:56 -0700

    [caffe2][nomnigraph] Add registry for optimization passes (#7656)

---
+++ caffe2/opt/passes.h
@@ -0,0 +1,67 @@
+#ifndef CAFFE2_OPT_OPT_PASSS_H
+#define CAFFE2_OPT_OPT_PASSS_H
+
+#include "caffe2/core/common.h"
+#include "caffe2/core/workspace.h"
+#include "caffe2/proto/caffe2.pb.h"
+
+#include "nomnigraph/Representations/NeuralNet.h"
+
+using namespace nom::repr;
+
+namespace caffe2 {
+
+/* This file sets up the optimization pass registry.
+ *
+ * You'll want to either create a class that inherits from OptimizationPass
+ * and implements run or use the REGISTER_OPT_PASS_FROM_FUNC(name, func)
+ * to register a function that takes in an NNModule*.
+ *
+ * If you need access to the workspace in the optimization you'll need to
+ * use a different registry and inherit from WorkspaceOptimizationPass.
+ */
+
+class OptimizationPass {
+ public:
+  OptimizationPass(NNModule* nn) : nn_(nn) {}
+  virtual void run() = 0;
+
+ protected:
+  NNModule* nn_;
+};
+
+class WorkspaceOptimizationPass : public OptimizationPass {
+ public:
+  WorkspaceOptimizationPass(NNModule* nn, Workspace* ws) : OptimizationPass(nn), ws_(ws) {}
+
+ protected:
+  Workspace* ws_;
+};
+
+CAFFE_DECLARE_REGISTRY(WorkspaceOptimizationPassRegistry, WorkspaceOptimizationPass, NNModule*, Workspace*);
+#define REGISTER_WS_OPT_PASS(clsname) \
+  CAFFE_REGISTER_CLASS(WorkspaceOptimizationPassRegistry, clsname, clsname)
+#define REGISTER_WS_OPT_PASS_FROM_FUNC(passname, funcname) \
+  class passname : public WorkspaceOptimizationPass { \
+   public: \
+    using WorkspaceOptimizationPass::WorkspaceOptimizationPass; \
+    void run() override { \
+      funcname(nn_, ws_); \
+    } \
+  };
+
+CAFFE_DECLARE_REGISTRY(OptimizationPassRegistry, OptimizationPass, NNModule*);
+#define REGISTER_OPT_PASS(clsname) \
+  CAFFE_REGISTER_CLASS(OptimizationPassRegistry, clsname, clsname)
+#define REGISTER_OPT_PASS_FROM_FUNC(passname, funcname) \
+  class passname : public OptimizationPass { \
+   public: \
+    using OptimizationPass::OptimizationPass; \
+    void run() override { \
+      funcname(nn_); \
+    } \
+  };
+
+} // namespace caffe2
+
+#endif // CAFFE2_OPT_OPT_PASSS_H
