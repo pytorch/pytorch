ec86443201e1c5b5edd3323124b3da12f25b6009
Orion Reblitz-Richardson <orionr@gmail.com>
2018-05-17 14:42:20 -0700

    Add tests back for ATen and fix TH build

--- tools/test_aten_install.sh
+++ tools/test_aten_install.sh
@@ -2,8 +2,7 @@
 set -xe
 mkdir aten_build aten_install
 cd aten_build
-# Tests only buildable via root right now, so disable here
-cmake ../aten -DNO_CUDA=1 -DATEN_NO_TEST=ON -DCMAKE_INSTALL_PREFIX=../aten_install
+cmake ../aten -DNO_CUDA=1 -DCMAKE_INSTALL_PREFIX=../aten_install
 NUM_JOBS="$(getconf _NPROCESSORS_ONLN)"
 make -j"$NUM_JOBS" install
 cd ..
