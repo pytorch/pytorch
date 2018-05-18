0ffca196afbd1f65df7f75f55290ce34afb521d2
Orion Reblitz-Richardson <orionr@gmail.com>
2018-05-18 06:04:00 -0700

    Fixes for aten/tools/run_tests.sh

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -31,12 +31,12 @@ time python test/run_test.py --verbose
 
 # Test ATen
 if [[ "$BUILD_ENVIRONMENT" != *asan* ]]; then
-  echo "Testing ATen"
+  echo "Running ATen tests with pytorch lib"
   TORCH_LIB_PATH=$(python -c "import site; print(site.getsitepackages()[0])")/torch/lib
   # NB: the ATen test binaries don't have RPATH set, so it's necessary to
   # put the dynamic libraries somewhere were the dynamic linker can find them.
   # This is a bit of a hack.
-  ln -s "$TORCH_LIB_PATH"/libcaffe2*.so build/lib
+  ln -s "$TORCH_LIB_PATH"/libcaffe2*.so build/bin
   aten/tools/run_tests.sh build/bin
 fi
 
