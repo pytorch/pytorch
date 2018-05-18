33776cb8aa4b276a8a34310e7c21445b8724f9aa
Will Feng <willfeng@fb.com>
2018-05-17 23:11:38 -0400

    Try to fix Windows error

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -266,7 +266,7 @@ Tensor randperm(const Type& dtype, int64_t n, Generator* generator) {
   return at::randperm_out(result, n, generator);
 }
 
-Tensor& randperm_out_cpu(Tensor& result, int64_t n, Generator* generator) {
+AT_API Tensor& randperm_out_cpu(Tensor& result, int64_t n, Generator* generator) {
   if (n < 0) {
     std::ostringstream oss;
     oss << "n must be non-negative, got " << n;
