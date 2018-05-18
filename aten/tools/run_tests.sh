f301ff65e4bac6b04653cb447635d45fddf826f1
Orion Reblitz-Richardson <orionr@gmail.com>
2018-05-18 14:02:13 -0700

    Attempt at fix for aten/tools/run_tests.sh

--- aten/tools/run_tests.sh
+++ aten/tools/run_tests.sh
@@ -2,28 +2,32 @@
 set -x
 set -e
 
+pushd $BUILD_ROOT
+
 VALGRIND=${VALGRIND:=ON}
 BUILD_ROOT=$1
-$BUILD_ROOT/basic
-$BUILD_ROOT/atest
-$BUILD_ROOT/scalar_test
-$BUILD_ROOT/broadcast_test
-$BUILD_ROOT/wrapdim_test
-$BUILD_ROOT/apply_utils_test
-$BUILD_ROOT/dlconvertor_test
-$BUILD_ROOT/native_test
-$BUILD_ROOT/scalar_tensor_test
-$BUILD_ROOT/undefined_tensor_test
-if [[ -x $BUILD_ROOT/cudnn_test ]]; then
-  $BUILD_ROOT/cudnn_test
+./basic
+./atest
+./scalar_test
+./broadcast_test
+./wrapdim_test
+./apply_utils_test
+./dlconvertor_test
+./native_test
+./scalar_tensor_test
+./undefined_tensor_test
+if [[ -x ./cudnn_test ]]; then
+  ./cudnn_test
 fi
-if [[ -x $BUILD_ROOT/cuda_rng_test ]]; then
-  $BUILD_ROOT/cuda_rng_test
+if [[ -x ./cuda_rng_test ]]; then
+  ./cuda_rng_test
 fi
-if [[ -x $BUILD_ROOT/apply_test ]]; then
-  $BUILD_ROOT/apply_test
+if [[ -x ./apply_test ]]; then
+  ./apply_test
 fi
 if [ "$VALGRIND" == "ON" ]
 then
-  valgrind --suppressions=`dirname $0`/valgrind.sup --error-exitcode=1 $BUILD_ROOT/basic "[cpu]"
+  valgrind --suppressions=`dirname $0`/valgrind.sup --error-exitcode=1 ./basic "[cpu]"
 fi
+
+popd
