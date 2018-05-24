35891c46dbdec46e55a5fb725418c2880f631dac
Mikhail Akimov <roveo@users.noreply.github.com>
2018-05-24 22:52:21 +0300

    Cosine Annealing with warm restarts
    Proposed in #1909, requested in #3790

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -216,6 +216,60 @@ class CosineAnnealingLR(_LRScheduler):
                 (1 + math.cos(math.pi * self.last_epoch / self.T_max)) / 2
                 for base_lr in self.base_lrs]
 
+    
+class CosineAnnealingWithRestartsLR(torch.optim.lr_scheduler._LRScheduler):
+
+    r"""Set the learning rate of each parameter group using a cosine annealing
+    schedule, where :math:`\eta_{max}` is set to the initial lr and
+    :math:`T_{cur}` is the number of epochs since the last restart in SGDR:
+
+    .. math::
+
+        \eta_t = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})(1 +
+        \cos(\frac{T_{cur}}{T_{max}}\pi))
+
+    When last_epoch=-1, sets initial lr as lr.
+
+    It has been proposed in
+    `SGDR: Stochastic Gradient Descent with Warm Restarts`_. This implements
+    the cosine annealing part of SGDR, the restarts and number of iterations multiplier.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        T_max (int): Maximum number of iterations.
+        T_mult (float): Multiply T_max by this number after each restart. Default: 1.
+        eta_min (float): Minimum learning rate. Default: 0.
+        last_epoch (int): The index of last epoch. Default: -1.
+
+    .. _SGDR\: Stochastic Gradient Descent with Warm Restarts:
+        https://arxiv.org/abs/1608.03983
+    """
+
+    def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1, T_mult=1):
+        self.T_max = T_max
+        self.T_mult = T_mult
+        self.restart_every = T_max
+        self.eta_min = eta_min
+        self.restarts = 0
+        self.restarted_at = 0
+        super().__init__(optimizer, last_epoch)
+    
+    def restart(self):
+        self.restart_every *= self.T_mult
+        self.restarted_at = self.last_epoch
+    
+    def cosine(self, base_lr):
+        return self.eta_min + (base_lr - self.eta_min) * (1 + math.cos(math.pi * self.step_n / self.restart_every)) / 2
+    
+    @property
+    def step_n(self):
+        return self.last_epoch - self.restarted_at
+
+    def get_lr(self):
+        if self.step_n >= self.restart_every:
+            self.restart()
+        return [self.cosine(base_lr) for base_lr in self.base_lrs]  
+  
 
 class ReduceLROnPlateau(object):
     """Reduce learning rate when a metric has stopped improving.
