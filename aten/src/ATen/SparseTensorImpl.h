139b8436ea1ed217933cc2ba828c8469ce17f960
Edward Z. Yang <ezyang@fb.com>
2018-05-31 07:38:43 -0700

    Split SparseTensorImpl off from TensorImpl.
    At the moment they have the same data layout, but with the upcoming refactor
    they will not, and we need a place to put all of the sparse tensor specific
    fields.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ aten/src/ATen/SparseTensorImpl.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+
+#include "ATen/TensorImpl.h"
+
+namespace at {
+struct SparseTensorImpl : public TensorImpl {
+  explicit SparseTensorImpl(Type * type)
+  : TensorImpl(type) {}
+};
+} // namespace at
