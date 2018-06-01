640c5def30f96ae38d46cc998f3dbd31ee3be4e3
Breno Arosa <breno.arosa@1sti.com.br>
2018-06-01 20:56:02 -0300

    Updates class_weight to Tensor/variable merge

--- torch/utils/class_weight.py
+++ torch/utils/class_weight.py
@@ -1,8 +1,6 @@
 import torch
-from torch.autograd import Variable
 
-
-def compute_sample_weight(y, n_classes=2, class_weight=None, eps=1e-8):
+def compute_sample_weight(y, class_weight=None, eps=1e-8):
     """Estimate sample weights by class for unbalanced datasets.
     Similar to sklearn.utils.class_weight.compute_sample_weight.
 
@@ -13,10 +11,8 @@ def compute_sample_weight(y, n_classes=2, class_weight=None, eps=1e-8):
     samples. This normalization isn't present in sklearn's version.
 
     Args:
-        y: 1D Tensor or Variable
+        y: 1D tensor
             Array of original class labels per sample.
-        n_classes: int
-            Number of unique classes
         class_weight: 1D FloatTensor
             Class weights where its idx correspond to class value
         eps: float
@@ -29,30 +25,17 @@ def compute_sample_weight(y, n_classes=2, class_weight=None, eps=1e-8):
     Examples:
     >>> y = torch.FloatTensor([1, 0, 0, 0])
     >>> compute_sample_weight(y)
-        0.5000
-        0.1667
-        0.1667
-        0.1667
-        [torch.FloatTensor of size 4]
+        tensor([ 2.0000,  0.6667,  0.6667,  0.6667])
 
-    >>> compute_sample_weight(y, n_classes=2,
-                              class_weight=torch.FloatTensor([.4, .6]))
-        1.3636
-        0.9091
-        0.9091
-        0.9091
-        0.9091
-        [torch.FloatTensor of size 5]
+    >>> compute_sample_weight(y, class_weight=torch.FloatTensor([.4, .6]))
+        tensor([ 1.3333,  0.8889,  0.8889,  0.8889])
 
     Computing weight in forward pass:
     >>> for i, (X, y) in enumerate(dataloader):
     >>>     y = y.float()
-    >>>     X = Variable(X)
-    >>>     y = Variable(y)
     >>>     optimizer.zero_grad()
     >>>     y_pred = model(X).squeeze()
-    >>>     weights = compute_sample_weight(y.data) # get y tensor to use as input
-    >>>     weights = Variable(weights)             # convert back to Variable to compute loss
+    >>>     weights = compute_sample_weight(y)
     >>>     loss = binary_cross_entropy(y_pred, y, weights)
     >>>     loss.backward()
     >>>     optimizer.step()
@@ -61,15 +44,12 @@ def compute_sample_weight(y, n_classes=2, class_weight=None, eps=1e-8):
     y = y.long()
     batch_size = y.size(0)
 
-    if isinstance(y, Variable):
-        weights = compute_sample_weight(y.data, n_classes, class_weight, eps)
-        return Variable(weights)
-
-    if y.is_cuda:
-        y_onehot = torch.cuda.zeros(batch_size, n_classes).float()
+    if class_weight is None:
+        n_classes = y.unique().size(0)
     else:
-        y_onehot = torch.zeros(batch_size, n_classes).float()
+        n_classes = class_weight.size(0)
 
+    y_onehot = torch.zeros(batch_size, n_classes, dtype=torch.float, device=y.device)
     y_onehot.scatter_(1, y.view(-1, 1), 1)
 
     if class_weight is None:
@@ -82,6 +62,5 @@ def compute_sample_weight(y, n_classes=2, class_weight=None, eps=1e-8):
         class_weight[class_weight > 1] = 0
 
     weights = torch.mm(y_onehot, class_weight.view(-1, 1)).squeeze()
-
     weights = batch_size * weights / (torch.sum(weights) + eps)
     return weights
