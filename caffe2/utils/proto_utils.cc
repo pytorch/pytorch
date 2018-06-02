e6308fc4c63a6b433f1fadeceeaa93c5975de973
Junjie Bai <bai@in.tum.de>
2018-06-01 21:36:50 -0700

    .

--- caffe2/utils/proto_utils.cc
+++ caffe2/utils/proto_utils.cc
@@ -30,7 +30,7 @@ const ::std::string& GetEmptyStringAlreadyInited() {
 
 namespace ONNX_NAMESPACE {
 
-// Caffe wrapper functions for protobuf's GetEmptyStringAlreadyInited() function
+// ONNX wrapper functions for protobuf's GetEmptyStringAlreadyInited() function
 // used to avoid duplicated global variable in the case when protobuf
 // is built with hidden visibility.
 const ::std::string& GetEmptyStringAlreadyInited() {
