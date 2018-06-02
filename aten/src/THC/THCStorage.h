b97e23a55ab94ef2e7bfab6ec4fc50707adce5bc
gchanan <gregchanan@gmail.com>
2018-06-02 11:05:02 -0400

    Have a single THStorage and THCStorage type. (#8030)
    No longer generate data-type specific Storage types, since all Storage types are now identical anyway.
    For (some) backwards compatibility and documentation purposes, the Real names, e.g. THLongStorage are now #defined as aliases to the single THStorage type

--- aten/src/THC/THCStorage.h
+++ aten/src/THC/THCStorage.h
@@ -4,14 +4,12 @@
 #include "THStorage.h"
 #include "THCGeneral.h"
 
-#define THCStorage        TH_CONCAT_3(TH,CReal,Storage)
 #define THCStorage_(NAME) TH_CONCAT_4(TH,CReal,Storage_,NAME)
-
-/* fast access methods */
-#define THC_STORAGE_GET(storage, idx) ((storage)->data[(idx)])
-#define THC_STORAGE_SET(storage, idx, value) ((storage)->data[(idx)] = (value))
 
 #include "generic/THCStorage.h"
 #include "THCGenerateAllTypes.h"
+
+// This exists to have a data-type independent way of freeing (necessary for THPPointer).
+THC_API void THCStorage_free(THCState *state, THCStorage *self);
 
 #endif
