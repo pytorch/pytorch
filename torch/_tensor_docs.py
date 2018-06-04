98b541c4c5420b1976677d672ca9ce6f4483a3cd
Ir1d <sirius.caffrey@gmail.com>
2018-06-05 07:49:53 +0800

    docs: Break the too long line

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -1560,8 +1560,10 @@ Unlike :meth:`~Tensor.expand`, this function copies the tensor's data.
 
 .. warning::
 
-    :func:`torch.repeat` behaves differently from `numpy.repeat <https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html>`_,
-    but is more similar to `numpy.tile <https://docs.scipy.org/doc/numpy/reference/generated/numpy.tile.html>`_.
+    :func:`torch.repeat` behaves differently from
+    `numpy.repeat <https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html>`_,
+    but is more similar to
+    `numpy.tile <https://docs.scipy.org/doc/numpy/reference/generated/numpy.tile.html>`_.
 
 Args:
     sizes (torch.Size or int...): The number of times to repeat this tensor along each
