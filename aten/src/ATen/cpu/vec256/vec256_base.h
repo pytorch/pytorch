5e681e53b46ee2d42739d487b9414ff4de5ca6bc
Christian Puhrsch <cpuhrsch@fb.com>
2018-06-05 10:15:52 -0600

    Abstract parallelization to faciliate using threadpools

--- aten/src/ATen/cpu/vec256/vec256_base.h
+++ aten/src/ATen/cpu/vec256/vec256_base.h
@@ -22,7 +22,7 @@ namespace {
 template <class T>
 struct Vec256 {
   static constexpr int size = 32 / sizeof(T);
-  __at_align32__ T values[32 / sizeof(T)];
+  T values[32 / sizeof(T)];
   Vec256() {}
   Vec256(T val) {
     for (int i = 0; i != size; i++) {
