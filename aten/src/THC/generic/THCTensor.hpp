fb2a6af321d2d4b78667bb6a18fa500ce036f8b7
Gregory Chanan <gchanan@fb.com>
2018-06-06 07:46:44 -0700

    Add comment about THCTensor struct.

--- aten/src/THC/generic/THCTensor.hpp
+++ aten/src/THC/generic/THCTensor.hpp
@@ -2,6 +2,8 @@
 #define THC_GENERIC_FILE "generic/THCTensor.hpp"
 #else
 
+// This allows one to write generic functions (i.e. functions that work across all THRealTensor types)
+// without having to explicitly cast the THRealTensor.
 typedef struct THCTensor : _THCTensor {
 } THCTensor;
 
