0465e41ffb15322520b25a562f95b5345943e576
llyfacebook <34827865+llyfacebook@users.noreply.github.com>
2018-06-06 17:04:54 -0700

    Change the benchmark log format and also log flops (#8215)
    as title

--- modules/observers/net_observer_reporter_print.cc
+++ modules/observers/net_observer_reporter_print.cc
@@ -1,5 +1,6 @@
 #include "observers/net_observer_reporter_print.h"
 
+#include <sstream>
 #include "caffe2/core/init.h"
 #include "observers/observer_config.h"
 
@@ -10,11 +11,44 @@ const std::string NetObserverReporterPrint::IDENTIFIER = "Caffe2Observer ";
 void NetObserverReporterPrint::report(
     NetBase* net,
     std::map<std::string, PerformanceInformation>& info) {
-  LOG(INFO) << IDENTIFIER << "Net Name - " << net->Name();
-  LOG(INFO) << IDENTIFIER << "Delay Start";
+  std::map<std::string, std::map<std::string, std::string>> caffe2_perf;
+  std::map<std::string, std::string> op_perf;
+  std::map<std::string, std::string> net_perf;
+
   for (auto& p : info) {
-    LOG(INFO) << IDENTIFIER << p.first << " - " << p.second.latency << "\t(ms)";
+    if ((p.first == "NET_DELAY") && (info.size() == 1)) {
+      // for Net_delay perf
+      net_perf["latency"] = caffe2::to_string(p.second.latency);
+      net_perf["flops"] = caffe2::to_string(-1);
+      caffe2_perf["NET"] = net_perf;
+    } else if (p.first != "NET_DELAY") {
+      // for operator perf
+      op_perf["latency"] = caffe2::to_string(p.second.latency);
+      op_perf["flops"] = caffe2::to_string(p.second.flops);
+      caffe2_perf[p.first] = op_perf;
+    }
   }
-  LOG(INFO) << IDENTIFIER << "Delay End";
+
+  std::stringstream buffer;
+  buffer << IDENTIFIER << "{";
+  for (auto it = caffe2_perf.begin(); it != caffe2_perf.end(); it++) {
+    buffer << "\"" << it->first << "\""
+           << ": {";
+    for (auto jt = it->second.begin(); jt != it->second.end(); jt++) {
+      buffer << "\"" << jt->first << "\""
+             << ": " << jt->second;
+      auto kt = jt;
+      if ((++kt) != it->second.end()) {
+        buffer << ", ";
+      }
+    }
+    buffer << "}";
+    auto lt = it;
+    if ((++lt) != caffe2_perf.end()) {
+      buffer << ", ";
+    }
+  }
+  buffer << "}";
+  LOG(INFO) << buffer.str();
 }
 }
