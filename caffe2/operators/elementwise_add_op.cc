09590ab55908d4dc858c7fe298dd98014f36ceb6
Zhishuai Zhang <zhishuaizhang@fb.com>
2018-06-12 10:04:44 -0700

    Support advanced pooling options in sum processor
    * support advanced pooling options in sum processor
    * remove redundant code
    * support attention in sum processor

--- caffe2/operators/elementwise_add_op.cc
+++ caffe2/operators/elementwise_add_op.cc
@@ -6,4 +6,10 @@ REGISTER_CPU_OPERATOR(
     Add,
     BinaryElementwiseOp<NumericTypes, CPUContext, AddFunctor<CPUContext>>);
 
+} // namespace
+
+REGISTER_GRADIENT(Add, GetAddGradient);
+
+#endif // !CAFFE2_MOBILE
+
 } // namespace caffe2
