d053c7849c92e05a0ce1d99016f1fc9f21799f8a
Anders Papitto <andersj@fb.com>
2018-06-12 13:41:16 -0700

    Back out "Back out "Add support for generating ATen files during fbcode build""
    Original commit changeset: 7b8de22d1613
    
    I'm re-sending this diff exactly as it was approved and
    committed. Fixes to support @mode/opt will be sent separately for ease
    of review.

--- tools/nnwrap/generate_wrappers.py
+++ tools/nnwrap/generate_wrappers.py
@@ -95,35 +95,41 @@ def wrap_function(name, type, arguments):
     return declaration
 
 
-def generate_wrappers(nn_root=None):
-    wrap_nn(os.path.join(nn_root, 'THNN', 'generic', 'THNN.h') if nn_root else None)
-    wrap_cunn(os.path.join(nn_root, 'THCUNN', 'generic', 'THCUNN.h') if nn_root else None)
+def generate_wrappers(nn_root=None, install_dir=None):
+    wrap_nn(os.path.join(nn_root, 'THNN', 'generic', 'THNN.h') if nn_root else None, install_dir)
+    wrap_cunn(os.path.join(nn_root, 'THCUNN', 'generic', 'THCUNN.h') if nn_root else None, install_dir)
 
 
-def wrap_nn(thnn_h_path):
+def wrap_nn(thnn_h_path, install_dir):
     wrapper = '#include <TH/TH.h>\n\n\n'
     nn_functions = thnn_utils.parse_header(thnn_h_path or thnn_utils.THNN_H_PATH)
     for fn in nn_functions:
         for t in ['Float', 'Double']:
             wrapper += wrap_function(fn.name, t, fn.arguments)
-    with open('torch/csrc/nn/THNN.cwrap', 'w') as f:
+    install_dir = install_dir or 'torch/csrc/nn'
+    try:
+        os.makedirs(install_dir)
+    except OSError:
+        pass
+    with open(os.path.join(install_dir, 'THNN.cwrap'), 'w') as f:
         f.write(wrapper)
-    cwrap('torch/csrc/nn/THNN.cwrap', plugins=[
+    cwrap(os.path.join(install_dir, 'THNN.cwrap'), plugins=[
         NNExtension('torch._C._THNN'),
         NullableArguments(),
     ])
 
 
-def wrap_cunn(thcunn_h_path=None):
+def wrap_cunn(thcunn_h_path, install_dir):
     wrapper = '#include <TH/TH.h>\n'
     wrapper += '#include <THC/THC.h>\n\n\n'
     cunn_functions = thnn_utils.parse_header(thcunn_h_path or thnn_utils.THCUNN_H_PATH)
     for fn in cunn_functions:
         for t in ['CudaHalf', 'Cuda', 'CudaDouble']:
             wrapper += wrap_function(fn.name, t, fn.arguments)
-    with open('torch/csrc/nn/THCUNN.cwrap', 'w') as f:
+    install_dir = install_dir or 'torch/csrc/nn'
+    with open(os.path.join(install_dir, 'THCUNN.cwrap'), 'w') as f:
         f.write(wrapper)
-    cwrap('torch/csrc/nn/THCUNN.cwrap', plugins=[
+    cwrap(os.path.join(install_dir, 'THCUNN.cwrap'), plugins=[
         NNExtension('torch._C._THCUNN'),
         NullableArguments(),
         AutoGPU(has_self=False),
