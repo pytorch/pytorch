c3ac213a9015af1d1b0fda30509fa7366522c616
James Reed <jamesreed@fb.com>
2018-06-12 22:59:45 -0700

    [ONNX] Add an ATen fallback pathway for ONNX export (#8273)
    * ATen fallback for ONNX export
    
    * Move to enum
    
    * Fix model test
    
    * Add comment
    
    * Address comments
    
    BC interface

--- torch/csrc/onnx/onnx.h
+++ torch/csrc/onnx/onnx.h
@@ -425,4 +425,11 @@ public:
   }
 };
 
+enum class OperatorExportTypes {
+  ONNX, // Strict ONNX export
+  ONNX_ATEN, // ONNX With ATen op everywhere
+  ONNX_ATEN_FALLBACK, // ONNX export with ATen fallback
+  RAW, // Raw export (no ONNX)
+};
+
 }} // namespace torch::onnx
