442e8e4449a155fe65d7d2af6ae6061137aecb63
Edward Z. Yang <ezyang@fb.com>
2018-06-15 06:37:29 -0700

    Clarify SparseTensorImpl comment.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/SparseTensorImpl.cpp
+++ aten/src/ATen/SparseTensorImpl.cpp
@@ -3,11 +3,21 @@
 
 namespace at {
 
-// SparseTensorImpl defaults to a [0] size tensor with one sparse dimension and
-// no dense dimensions.  This is kind of arbitrary but we want an empty tensor
-// to have one dimension (zero dimensions means its scalar), and we have
-// the invariant that dim == sparseDims + denseDims, so *one* of the inner dimensions
-// has to be one.
+
+// An empty dense tensor defaults to a 1-dimensional tensor of size [0]
+// (recall, it is not a 0-dimensional tensor, because such a tensor would
+// a scalar and have one element)
+//
+// Thus, an empty sparse tensor should be a 1-dimensional tensor of size [0].
+// Furthermore, we have dim == sparseDims + denseDims; since this is a sparse
+// tensor, let us say that an empty sparse tensor has sparseDims == 1 and
+// denseDims == 0.  (There is a degree of freedom here, but given that this
+// is a sparse dimension, it seems reasonable to demand that sparseDims > 0).
+//
+// In an ideal world, this would then mean we allocate a [1,0] size indices
+// tensor and a [0] size values tensor for such an empty tensor.  However,
+// we don't currently support zero-size dimensions, so we can't actually
+// do this; so we just allocate zero-size tensors for everything.
 SparseTensorImpl::SparseTensorImpl(Type * type)
     : TensorImpl(type)
     , size_{0}
