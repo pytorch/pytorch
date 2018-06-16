c5b2af6a950b67445ad8916d364fd24456d7aa39
Peter Goldsborough <psag@fb.com>
2018-06-15 22:19:29 -0700

    Fix flip functions

--- aten/src/ATen/native/TensorTransformations.cpp
+++ aten/src/ATen/native/TensorTransformations.cpp
@@ -1,6 +1,10 @@
-#include "TensorTransformations.h"
+#include "ATen/native/TensorTransformations.h"
 
-#include "ATen/NativeFunctions.h"
+#include <ATen/NativeFunctions.h>
+#include <ATen/Error.h>
+
+#include <algorithm>
+#include <vector>
 
 namespace at {
 namespace native {
@@ -15,7 +19,7 @@ Tensor flip_cpu(const Tensor& self, IntList dims) {
 
   auto indices = std::vector<at::Tensor>(flip_dims_size);
   for (int64_t i = 0; i < flip_dims_size; i++) {
-    indices[i] = at::arange(self.type().toScalarType(at::ScalarType::Long), self.size(i) - 1, -1, -1);
+    indices[i] = at::arange(self.size(i) - 1, -1, -1, self.type().toScalarType(at::kLong));
     // creates a meshgrid
     auto temp = std::vector<int64_t>(flip_dims_size, 1);
     temp[i] = indices[i].size(0);
