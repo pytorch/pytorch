2872cb5e120cc6a9225615069c1bf13d30c29b85
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2018-06-19 16:19:39 -0400

    Update gradcheck.py

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -161,15 +161,14 @@ def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, raise_exception=True
     """
     tupled_inputs = _as_tuple(inputs)
 
-    if any((t.requires_grad and t.dtype != torch.float64) for t in tupled_inputs):
-        warnings.warn(
-            'At least one of the inputs is of single precision. '
-            'The default values are designed for :attr:`input` of double precision. '
-            'This check will likely fail if :attr:`input` is of single precision. ')
-
     # Make sure that gradients are saved for all inputs
     for inp in tupled_inputs:
         if isinstance(inp, torch.Tensor):
+            if inp.requires_grad and inp.dtype != torch.float64:
+                warnings.warn(
+                    'At least one of the inputs is of single precision. '
+                    'The default values are designed for :attr:`input` of double precision. '
+                    'This check will likely fail if :attr:`input` is of single precision. ')
             inp.retain_grad()
 
     output = _differentiable_outputs(func(*inputs))
