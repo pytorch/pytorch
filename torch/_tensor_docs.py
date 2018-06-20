e8efbb7dc3edd296e2241d78ffb4431a18fa6cf9
li-roy <8813817+li-roy@users.noreply.github.com>
2018-06-20 09:53:06 -0700

    Implement flatten function (#8578)
    * Implement flatten function
    
    * address comments
    
    * allow start_dim=end_dim
    
    * undo submodule change

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -810,6 +810,13 @@ frac_() -> Tensor
 In-place version of :meth:`~Tensor.frac`
 """)
 
+add_docstr_all('flatten',
+               r"""
+flatten(input, start_dim=0, end_dim=-1) -> Tensor
+
+see :func:`torch.flatten`
+""")
+
 add_docstr_all('gather',
                r"""
 gather(dim, index) -> Tensor
