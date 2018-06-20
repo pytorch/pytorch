344f1197d3e20fc9527f6dc0fab986fd45443eb5
Edward Z. Yang <ezyang@fb.com>
2018-06-20 09:19:09 -0700

    Fix undefined ref differently
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cu
+++ aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cu
@@ -4,6 +4,7 @@
 #include <ATen/native/sparse/cuda/SparseCUDAApplyUtils.cuh>
 #include <ATen/AccumulateType.h>
 #include <ATen/cuda/CUDAApplyUtils.cuh>
+#include <ATen/cuda/CUDATensorMethods.cuh>
 
 #include <THC/THCThrustAllocator.cuh>
 #include <THC/THCTensorSort.cuh>
@@ -92,8 +93,8 @@ SparseTensor coalesce_sparse_cuda(const SparseTensor& self) {
         apply::coalesceValuesKernel<cuda_scalar_t, cuda_accscalar_t><<<grid, block, 0, stream>>>(
           uniqueOffsets.data<int64_t>(),
           origIndices.data<int64_t>(),
-          values.data<scalar_t>(),
-          newValues.data<scalar_t>(),
+          values.data<cuda_scalar_t>(),
+          newValues.data<cuda_scalar_t>(),
           nnz,
           newNnz,
           stride
