7844797b545f3b7328092d4f4c2e46e0dc571f32
anderspapitto <anderspapitto@gmail.com>
2018-06-20 14:45:26 -0700

    Build system changes (#8627)
    * All changes needed to get rid of process_github.sh
    
    * allow thnn_h_path

--- tools/nnwrap/__init__.py
+++ tools/nnwrap/__init__.py
@@ -1,5 +1,1 @@
-from .generate_wrappers import generate_wrappers, wrap_function
-try:
-    from .generate_wrappers import import_module
-except ImportError:
-    pass
+from .generate_wrappers import generate_wrappers, wrap_function, import_module
