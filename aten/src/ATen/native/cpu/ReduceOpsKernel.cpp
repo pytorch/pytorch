705894c6f36e13ce98613e215d59a41a249f3859
Christian Puhrsch <cpuhrsch@fb.com>
2018-06-21 15:43:31 -0600

    Specialize on stride 1

--- aten/src/ATen/native/cpu/ReduceOpsKernel.cpp
+++ aten/src/ATen/native/cpu/ReduceOpsKernel.cpp
@@ -67,52 +67,79 @@ struct Reduction {
     }
     int64_t batch = numel / (n * stride);
     bool paralellize = batch * n > internal::GRAIN_SIZE;
-    int64_t rows = n;
-    int64_t cols = stride;
-    int64_t cols_rounded = round_down(cols, WIDTH);
-    int64_t size = cols_rounded;
-    parallel_for(
-        0,
-        batch * (size / WIDTH),
-        1,
-        [out_, data_, n, stride, rows, cols, cols_rounded, size](
-            int64_t begin, int64_t end) {
-          for (int64_t bi = begin; bi < end; bi++) {
-            int64_t b = bi / (size / WIDTH);
-            int64_t i = bi % (size / WIDTH);
-            int64_t k = i * WIDTH;
-            reduce128(
-                &data_[b * n * stride + k],
-                &out_[b * stride + k],
-                rows,
-                stride);
+//    std::cout << "n: " << n << std::endl;
+//    std::cout << "stride: " << stride << std::endl;
+//    std::cout << "batch: " << batch << std::endl;
+    if (stride == 1) {
+      parallel_for(0, batch, 1, [=](int64_t begin, int64_t end) {
+        for (int64_t b = begin; b < end; b++) {
+          const scalar_t* data = &data_[b * n];
+          scalar_t* out = &out_[b];
+          scalar_t buf[WIDTH] = {ident};
+          int64_t a = n / WIDTH;
+//          std::cout << "a: " << a << std::endl;
+          reduce128(data, buf, a, WIDTH);
+          scalar_t result = ident;
+//          std::cout << "result0: " << result << std::endl;
+          for (int64_t i = 0; i < WIDTH; i++) {
+            result = ReduceScalar()(result, buf[i]);
           }
-        });
-
-    _parallel_for(batch, 1, paralellize, [=](int64_t b) {
-      const scalar_t* data = &data_[b * n * stride];
-      scalar_t* out = &out_[b * stride];
+//          std::cout << "result: " << result << std::endl;
+          for (int64_t row = a * WIDTH; row != n; row++) {
+            result = ReduceScalar()(result, data[row]);
+          }
+//          std::cout << "result2: " << result << std::endl;
+          out[0] = result;
+        }
+      });
+    } else {
       int64_t rows = n;
       int64_t cols = stride;
-
       int64_t cols_rounded = round_down(cols, WIDTH);
-      if (cols_rounded != cols) {
-        // Initializes the entire (tiny) arrays to avoid uninitialized
-        // warnings
-        scalar_t buf[WIDTH] = {0};
-        std::fill(std::begin(buf), std::end(buf), ident);
+      int64_t size = cols_rounded;
+      parallel_for(
+          0,
+          batch * (size / WIDTH),
+          1,
+          [out_, data_, n, stride, rows, cols, cols_rounded, size](
+              int64_t begin, int64_t end) {
+            for (int64_t bi = begin; bi < end; bi++) {
+              int64_t b = bi / (size / WIDTH);
+              int64_t i = bi % (size / WIDTH);
+              int64_t k = i * WIDTH;
+              reduce128(
+                  &data_[b * n * stride + k],
+                  &out_[b * stride + k],
+                  rows,
+                  stride);
+            }
+          });
 
-        for (int64_t row = 0; row != rows; row++) {
+      _parallel_for(batch, 1, paralellize, [=](int64_t b) {
+        const scalar_t* data = &data_[b * n * stride];
+        scalar_t* out = &out_[b * stride];
+        int64_t rows = n;
+        int64_t cols = stride;
+
+        int64_t cols_rounded = round_down(cols, WIDTH);
+        if (cols_rounded != cols) {
+          // Initializes the entire (tiny) arrays to avoid uninitialized
+          // warnings
+          scalar_t buf[WIDTH] = {0};
+          std::fill(std::begin(buf), std::end(buf), ident);
+
+          for (int64_t row = 0; row != rows; row++) {
+            for (int64_t j = 0; j != cols - cols_rounded; j++) {
+              auto val = data[row * stride + j + cols_rounded];
+              buf[j] = ReduceScalar()(buf[j], val);
+            }
+          }
           for (int64_t j = 0; j != cols - cols_rounded; j++) {
-            auto val = data[row * stride + j + cols_rounded];
-            buf[j] = ReduceScalar()(buf[j], val);
+            out[j + cols_rounded] = buf[j];
           }
         }
-        for (int64_t j = 0; j != cols - cols_rounded; j++) {
-          out[j + cols_rounded] = buf[j];
-        }
-      }
-    });
+      });
+    }
   }
 
   static scalar_t reduce_all(const scalar_t* data, int64_t size) {
