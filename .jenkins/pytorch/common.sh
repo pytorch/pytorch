b300934db690ee01843a7b6b436ac357691209bb
Will Feng <yf225@cornell.edu>
2018-06-21 12:58:28 -0400

    Add CUDA 9.2 + GCC 7 build and test to CI (#8592)

--- .jenkins/pytorch/common.sh
+++ .jenkins/pytorch/common.sh
@@ -113,7 +113,7 @@ else
 fi
 
 if [[ "$BUILD_ENVIRONMENT" == *pytorch-linux-xenial-cuda9-cudnn7-py3 ]] || \
-   [[ "$BUILD_ENVIRONMENT" == *pytorch-linux-trusty-py3.6-gcc7.2 ]]; then
+   [[ "$BUILD_ENVIRONMENT" == *pytorch-linux-trusty-py3.6-gcc7* ]]; then
   BUILD_TEST_LIBTORCH=1
 else
   BUILD_TEST_LIBTORCH=0
