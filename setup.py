6912b1ee59c70cf935f0d2223cd3bcc518f3bdb1
Orion Reblitz-Richardson <orionr@gmail.com>
2018-06-22 06:51:56 -0700

    Move nanopb-generated ONNX to unique file name (#8773)
    * Move nanopb-generated ONNX to unique file name
    
    * fix other places

--- setup.py
+++ setup.py
@@ -817,7 +817,7 @@ main_sources = [
     "torch/csrc/autograd/functions/init.cpp",
     "torch/csrc/nn/THNN.cpp",
     "torch/csrc/tensor/python_tensor.cpp",
-    "torch/csrc/onnx/onnx.pb.cpp",
+    "torch/csrc/onnx/onnx.npb.cpp",
     "torch/csrc/onnx/onnx.cpp",
     "torch/csrc/onnx/init.cpp",
 ]
