bd54eedeeab6d47ab5a66024ac645a145a01fcb8
James Sun <jamessun@fb.com>
2018-06-22 17:59:15 -0700

    A few fixups to multithread predictor benchmark
    Summary:
    (1) support perf on T6 server
    (2) remove dead code
    
    Reviewed By: Maratyszcza
    
    Differential Revision: D8559093
    
    fbshipit-source-id: a72f4a6f24a1efceff05a0c1f5c5743771ea0539

--- caffe2/utils/string_utils.cc
+++ caffe2/utils/string_utils.cc
@@ -16,6 +16,15 @@ std::vector<std::string> split(char separator, const std::string& string) {
   return pieces;
 }
 
+std::string trim(const std::string& str) {
+  size_t left = str.find_first_not_of(' ');
+  if (left == std::string::npos) {
+    return str;
+  }
+  size_t right = str.find_last_not_of(' ');
+  return str.substr(left, (right - left + 1));
+}
+
 size_t editDistance(
   const std::string& s1, const std::string& s2, size_t max_distance)
   {
@@ -104,5 +113,4 @@ int32_t editDistanceHelper(const char* s1,
 
     return current[s1_len];
   }
-
 } // namespace caffe2
