e4ff0b8aa1fac14035f903f166ff8c2f0d14eff3
ngimel <ngimelshein@nvidia.com>
2018-06-23 11:31:13 -0700

    remove unnecessary headers from SpectralOps, add cuda.h include to deviceutils (#8819)

--- aten/src/THC/THCDeviceUtils.cuh
+++ aten/src/THC/THCDeviceUtils.cuh
@@ -1,6 +1,7 @@
 #ifndef THC_DEVICE_UTILS_INC
 #define THC_DEVICE_UTILS_INC
 
+#include <cuda.h>
 /* The largest consecutive integer representable in float32 (2^24) */
 #define FLOAT32_MAX_CONSECUTIVE_INT 16777216.0f
 
