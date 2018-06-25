9928f90de09ed5c616f590087cce4c4cdb83c0ab
bstriner <bstriner@gmail.com>
2018-06-24 20:22:23 -0400

    Add file and line to CUDA_CHECK and CUDNN_CHECK

--- aten/src/ATen/cudnn/Exceptions.h
+++ aten/src/ATen/cudnn/Exceptions.h
@@ -1,43 +1,49 @@
 #pragma once
 
-#include "cudnn-wrapper.h"
-#include <string>
-#include <stdexcept>
 #include <sstream>
+#include <stdexcept>
+#include <string>
+#include "cudnn-wrapper.h"
 
 struct THCState;
 
-namespace at { namespace native {
+namespace at {
+namespace native {
 
 class cudnn_exception : public std::runtime_error {
-public:
+ public:
   cudnnStatus_t status;
   cudnn_exception(cudnnStatus_t status, const char* msg)
-      : std::runtime_error(msg)
-      , status(status) {}
+      : std::runtime_error(msg), status(status) {}
   cudnn_exception(cudnnStatus_t status, const std::string& msg)
-      : std::runtime_error(msg)
-      , status(status) {}
+      : std::runtime_error(msg), status(status) {}
 };
 
-inline void CUDNN_CHECK(cudnnStatus_t status)
-{
+inline void cudnnCheck(cudnnStatus_t status, const char* file, int line) {
   if (status != CUDNN_STATUS_SUCCESS) {
+    std::string msg("CuDNN error: ");
+    msg += cudnnGetErrorString(status);
+    msg = msg + " (" + file + ":" + std::to_string(line) + ")";
     if (status == CUDNN_STATUS_NOT_SUPPORTED) {
-        throw cudnn_exception(status, std::string(cudnnGetErrorString(status)) +
-                ". This error may appear if you passed in a non-contiguous input.");
+      msg += ". This error may appear if you passed in a non-contiguous input.";
+      throw cudnn_exception(status, msg);
     }
-    throw cudnn_exception(status, cudnnGetErrorString(status));
+    throw cudnn_exception(status, msg);
   }
 }
 
-inline void CUDA_CHECK(cudaError_t error)
-{
+#define CUDNN_CHECK(ERROR) ::at::native::cudnnCheck(ERROR, __FILE__, __LINE__)
+
+inline void cudaCheck(cudaError_t error, const char* file, int line) {
   if (error != cudaSuccess) {
     std::string msg("CUDA error: ");
     msg += cudaGetErrorString(error);
+    msg = msg + " (" + file + ":" + std::to_string(line) + ")";
     throw std::runtime_error(msg);
   }
 }
 
-}}  // namespace at::cudnn
+#define CUDA_CHECK(ERROR) ::at::native::cudaCheck(ERROR, __FILE__, __LINE__)
+
+} // namespace native
+} // namespace at
