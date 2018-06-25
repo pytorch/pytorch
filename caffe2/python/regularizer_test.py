750ac21ae21e34df9f150778408c95f557ca1c2a
Orion Reblitz-Richardson <orionr@gmail.com>
2018-06-25 16:52:49 -0700

    Import core in regularizer_test

--- caffe2/python/regularizer_test.py
+++ caffe2/python/regularizer_test.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from caffe2.python import schema
+from caffe2.python import core, schema
 from caffe2.python.regularizer_context import UseRegularizer, RegularizerContext
 from caffe2.python.regularizer import L1Norm
 from caffe2.python.optimizer import SgdOptimizer
