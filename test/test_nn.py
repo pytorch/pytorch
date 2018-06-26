df0bbe4ad3f294e26e484ed02ae2d34b139ef51f
Karan Dwivedi <karan.dwivedi42@gmail.com>
2018-06-26 18:36:43 -0400

    Fix lint

--- test/test_nn.py
+++ test/test_nn.py
@@ -898,12 +898,11 @@ class TestNN(NNTestCase):
 
     def test_register_buffer_raises_error_if_name_is_not_string(self):
         m = nn.Module()
-        with self.assertRaisesRegex(
-            TypeError, 'buffer name should be a string. Got int'):
-                m.register_buffer(1, torch.rand(5))
-        with self.assertRaisesRegex(
-            TypeError, 'buffer name should be a string. Got NoneType'):
-                m.register_buffer(None, torch.rand(5))
+        expected_error = 'buffer name should be a string. Got ' 
+        with self.assertRaisesRegex(TypeError, expected_error + 'int'):
+            m.register_buffer(1, torch.rand(5))
+        with self.assertRaisesRegex(TypeError, expected_error + 'NoneType'):
+            m.register_buffer(None, torch.rand(5))
 
     def test_register_buffer_raises_error_if_attr_exists(self):
         m = nn.Module()
@@ -940,12 +939,11 @@ class TestNN(NNTestCase):
 
     def test_register_parameter_raises_error_if_name_is_not_string(self):
         m = nn.Module()
-        with self.assertRaisesRegex(
-            TypeError, 'parameter name should be a string. Got int'):
-                m.register_parameter(1, nn.Parameter())
-        with self.assertRaisesRegex(
-            TypeError, 'parameter name should be a string. Got NoneType'):
-                m.register_parameter(None, nn.Parameter())
+        expected_error = 'parameter name should be a string. Got '
+        with self.assertRaisesRegex(TypeError, expected_error + 'int'):
+            m.register_parameter(1, nn.Parameter())
+        with self.assertRaisesRegex(TypeError, expected_error + 'NoneType'):
+            m.register_parameter(None, nn.Parameter())
 
     def test_register_parameter_raises_error_if_attr_exists(self):
         m = nn.Module()
@@ -1194,9 +1192,9 @@ class TestNN(NNTestCase):
         self.assertEqual(net.l, l3)
         self.assertRaises(TypeError, lambda: net.add_module('x', 'non-module'))
         self.assertRaisesRegex(TypeError, 'module name should be a string. Got int',
-            lambda: net.add_module(1, l))
+                               lambda: net.add_module(1, l))
         self.assertRaisesRegex(TypeError, 'module name should be a string. Got NoneType',
-            lambda: net.add_module(None, l))
+                               lambda: net.add_module(None, l))
 
     def test_module_to_argparse(self):
         net = nn.Sequential(nn.Linear(3, 3))
