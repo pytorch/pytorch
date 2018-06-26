8a6ce11b04f9309d5d22311b3028c9ede492b9ab
Nallamaddi, Rohith <rohith.nallamaddi@amd.com>
2018-06-26 13:57:14 -0700

    added ignore files feature

--- tools/amd_build/build_caffe2_amd.py
+++ tools/amd_build/build_caffe2_amd.py
@@ -4,17 +4,22 @@ import os
 import glob
 import subprocess
 
-
 caffe2_root = "../../caffe2"
 hipify_tool = "../tools/amd_build/hipify-perl"
 
 os.chdir(caffe2_root)
 
+ignore_file_list = {"depthwise_3x3_conv_op.cu",
+					"top_k.cu",
+					"top_k_radix_selection.cuh",
+					"top_k_heap_selection.cuh"}
+
 for folder in ["operators", "sgd", "image", "transforms", "video"]:
     for extension in ["/*.cu", "/*.cuh", "/*gpu.cc", "/*gpu.h"]:
         target = folder + extension
-        if glob.glob(target):
-            os.system(hipify_tool + ' --inplace ' + target)
+        for file in glob.glob(target):
+        	if file.split('/')[-1] not in ignore_file_list:
+        		os.system(hipify_tool + ' --inplace ' + file)
         os.chdir(folder)
         if glob.glob('*hip.cc'):
             os.system("mv -n *hip.cc hip")
