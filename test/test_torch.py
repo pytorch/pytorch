fc2bd03fec36f8c3909918b3368a6f1c7194efa7
Anders Papitto <andersj@fb.com>
2018-06-25 19:18:37 -0700

    add compile=False to pytorch tests, remove hack with pyc

--- test/test_torch.py
+++ test/test_torch.py
@@ -6668,10 +6668,7 @@ class TestTorch(TestCase):
             return module
 
         with filecontext_lambda() as checkpoint:
-            try:
-                fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network1.py')
-            except IOError:
-                fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network1.pyc')
+            fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network1.py')
             module = import_module(tmpmodule_name, fname)
             torch.save(module.Net(), checkpoint)
 
@@ -6684,10 +6681,7 @@ class TestTorch(TestCase):
                     self.assertEquals(len(w), 0)
 
             # Replace the module with different source
-            try:
-                fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network2.py')
-            except IOError:
-                fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network2.pyc')
+            fname = get_file_path_2(os.path.dirname(__file__), 'data', 'network2.py')
             module = import_module(tmpmodule_name, fname)
             checkpoint.seek(0)
             with warnings.catch_warnings(record=True) as w:
