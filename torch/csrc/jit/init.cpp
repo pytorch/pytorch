2de87d9bab92a6f45e2683e501808e28f1c3155c
Soumith Chintala <soumith@fb.com>
2018-06-25 22:17:43 -0600

    minor pybind for jit

--- torch/csrc/jit/init.cpp
+++ torch/csrc/jit/init.cpp
@@ -18,6 +18,7 @@
 #include "torch/csrc/jit/passes/shape_analysis.h"
 #include "torch/csrc/jit/passes/decompose_addmm.h"
 #include "torch/csrc/jit/passes/loop_unrolling.h"
+#include "torch/csrc/jit/passes/specialize_undef.h"
 #include "torch/csrc/jit/graph_executor.h"
 #include "torch/csrc/jit/script/init.h"
 #include "torch/csrc/jit/script/python_tree_views.h"
@@ -97,6 +98,7 @@ void initJITBindings(PyObject *module) {
    .def("_jit_pass_onnx_block", BlockToONNX)
    .def("_jit_pass_fixup_onnx_loops", FixupONNXLoops)
    .def("_jit_pass_decompose_addmm", DecomposeAddmm)
+    .def("_jit_pass_specialize_undef", specializeUndef)
    .def("_jit_differentiate", [](Graph &g, const std::vector<bool>& requires_grad) {
        // the python binding slightly differs in semantics
        // it makes a copy of the input Graph, and works on that
@@ -133,6 +135,9 @@ void initJITBindings(PyObject *module) {
     })
     .def_property_readonly("df", [](Gradient& m) {
       return m.df;
+    })
+    .def_property_readonly("f_real_outputs", [](Gradient& m) {
+      return m.f_real_outputs;
     })
     .def_property_readonly("df_input_vjps", [](Gradient& m) {
       return m.df_input_vjps;
