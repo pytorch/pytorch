03153539f0c30081726a5e2985b520ad50fe3650
Edward Z. Yang <ezyang@fb.com>
2018-06-26 14:26:05 -0700

    Axe THCStorage copy.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/THC/THCStorage.hpp
+++ aten/src/THC/THCStorage.hpp
@@ -4,6 +4,7 @@
 // read Note [TH abstraction violation]
 
 #include "THCStorage.h"
+#include <TH/THStorage.hpp>
 
 #include "ATen/ScalarType.h"
 #include "ATen/ScalarTypeUtils.h"
@@ -15,35 +16,6 @@ template <>
 struct CTypeToScalarType<__half> : public CTypeToScalarType<Half> {};
 
 }
-
-typedef struct THCStorage
-{
-    at::Backend backend;
-    at::ScalarType scalar_type;
-    void *data_ptr;
-    ptrdiff_t size;
-    std::atomic<int> refcount;
-    char flag;
-    void *allocatorVoidPtr;
-    void *allocatorContext;
-    struct THCStorage *view;
-    int device;
-
-    template <typename T>
-    inline T * data() const {
-      auto scalar_type_T = at::CTypeToScalarType<T>::to();
-      if (scalar_type != scalar_type_T) {
-        AT_ERROR("Attempt to access Storage having data type ", at::toString(scalar_type),
-                 " as data type ", at::toString(scalar_type_T));
-      }
-      return unsafe_data<T>();
-    }
-
-    template <typename T>
-    inline T * unsafe_data() const {
-      return static_cast<T*>(this->data_ptr);
-    }
-} THCStorage;
 
 THC_API THCStorage* THCStorage_new(THCState *state, at::ScalarType scalar_type);
 THC_API THCStorage* THCStorage_newWithSize(THCState *state, at::ScalarType scalar_type, ptrdiff_t size);
