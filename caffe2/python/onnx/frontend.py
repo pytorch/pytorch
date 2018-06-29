b75490414ca20f99e0047db4387a060a8838e39f
Lu Fang <lufang@fb.com>
2018-06-29 11:54:32 -0700

    Bump up the C2 onnx frontend opset to 8 (#9006)
    Summary:
    Now ONNX master has bump up to opset 8.
    Closes https://github.com/pytorch/pytorch/pull/9006
    
    Reviewed By: yinghai
    
    Differential Revision: D8685417
    
    Pulled By: houseroad
    
    fbshipit-source-id: f0c0a3682417b8803a856e232c2740cf3e68e554

--- caffe2/python/onnx/frontend.py
+++ caffe2/python/onnx/frontend.py
@@ -38,7 +38,7 @@ class Caffe2Frontend(object):
     # ONNX makes a BC breaking change to semantics of operators, having this set
     # to an accurate number will prevent our models form exporting.  However,
     # we should strive to keep this up-to-date as much as possible.
-    target_opset_version = 7
+    target_opset_version = 8
 
     _renamed_operators = {
         'SpatialBN': 'BatchNormalization',
