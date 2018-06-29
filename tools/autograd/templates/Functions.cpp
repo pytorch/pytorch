443d60d70f73dfb8676ec0b190574ea8d9b80b47
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-06-29 10:47:55 -0400

    fix select bwd when wrap dim

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -521,8 +521,14 @@ Tensor index_select_backward(Tensor grad, int64_t dim, Tensor indices, IntList s
 }
 
 Tensor slice_backward(Tensor grad, IntList input_sizes, int64_t dim, int64_t start, int64_t end, int64_t step) {
-  auto grad_input = at::zeros(input_sizes, grad.type());
+  auto grad_input = at::zeros(input_sizes, grad.options());
   grad_input.slice(dim, start, end, step).copy_(grad);
+  return grad_input;
+}
+
+Tensor select_backward(Tensor grad, IntList input_sizes, int64_t dim, int64_t index) {
+  auto grad_input = at::zeros(input_sizes, grad.options());
+  grad_input.select(dim, index).copy_(grad);
   return grad_input;
 }
 
