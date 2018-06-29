39ab5229c4b6a94683bb1e1b8af204af6ad35254
Junjie Bai <bai@in.tum.de>
2018-06-29 10:41:44 -0700

    .

--- test/test_jit.py
+++ test/test_jit.py
@@ -808,7 +808,7 @@ class TestJit(JitTestCase):
         self.assertExpected(str(g2))
 
     # @unittest.skipIf(IS_WINDOWS, "NYI: fuser support for Windows")
-    @unittest.skipIf(not RUN_CUDA, "cpp tests require CUDA")
+    # @unittest.skipIf(not RUN_CUDA, "cpp tests require CUDA")
     def test_cpp(self):
         # rather than rebuild assertExpected in cpp,
         # just glob all the cpp outputs into one file for now
