219e88878efe4b7f13cba97fab2ddeb9c665178e
Junjie Bai <bai@in.tum.de>
2018-07-03 17:20:46 -0600

    Only set -fvisibility-inlines-hidden when it is supported

--- cmake/MiscCheck.cmake
+++ cmake/MiscCheck.cmake
@@ -152,10 +152,6 @@ cmake_pop_check_state()
 # ---[ Checks if compiler supports -fvisibility=hidden
 check_cxx_compiler_flag("-fvisibility=hidden" COMPILER_SUPPORTS_HIDDEN_VISIBILITY)
 check_cxx_compiler_flag("-fvisibility-inlines-hidden" COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY)
-if (${COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY})
-  set(CAFFE2_VISIBILITY_FLAG "-fvisibility-inlines-hidden")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CAFFE2_VISIBILITY_FLAG}")
-endif()
 
 # ---[ If we are using msvc, set no warning flags
 # Note(jiayq): if you are going to add a warning flag, check if this is
