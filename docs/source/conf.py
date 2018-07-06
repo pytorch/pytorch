4673fdcc1c0ebda64568ebe70b0f939aac19c37a
Rob Kunkle <robkunkle@fb.com>
2018-07-06 12:59:36 -0700

    rolling back conf.py edits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,11 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+# import os
+# mport sys
 
 # source code directory, relative to this file, for sphinx-autobuild
-sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../..'))
 
 import torch
 try:
