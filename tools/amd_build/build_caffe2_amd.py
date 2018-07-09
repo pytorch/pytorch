a802bb32f6058b841db4a9d9ce47d4ecc1429e0f
root <rohith.nallamaddi@gmail.com>
2018-07-09 10:43:51 -0700

    enable static casts for caffe2

--- tools/amd_build/build_caffe2_amd.py
+++ tools/amd_build/build_caffe2_amd.py
@@ -19,6 +19,7 @@ include_dirs = [
     "video",
     "distributed"
 ]
+
 output_dir = os.path.join(os.path.dirname(os.path.dirname(amd_build_dir)), "caffe2_hip")
 
 file_extensions = ['cc','cu','h','cuh']
@@ -37,7 +38,8 @@ args = ["--project-directory", proj_dir,
         "--include-dirs"] + include_dirs + \
         ["--extensions"] + file_extensions + \
         ["--ignore_files"] + ignore_file_list + \
-        ["--hipify_caffe2", "True"]
+        ["--hipify_caffe2", "True"] + \
+        ["--add-static-casts", "True"]
 #os.execv(os.path.join(amd_build_dir, "pyHIPIFY", "hipify-python.py"), ['python'] + args)
 os.system("python " + os.path.join(amd_build_dir, "pyHIPIFY", "hipify-python.py") + " " + " ".join(args))
 
@@ -56,37 +58,4 @@ for dir in include_dirs:
                 shutil.copyfile(file,dest_filepath)
 
 shutil.rmtree(output_dir)
-
-
-"""
-import os
-import glob
-import subprocess
-
-caffe2_root = "../../caffe2"
-hipify_tool = "../tools/amd_build/hipify-perl"
-
-os.chdir(caffe2_root)
-
-ignore_file_list = {"depthwise_3x3_conv_op.cu",
-					"top_k.cu",
-					"top_k_radix_selection.cuh",
-					"top_k_heap_selection.cuh",
-                    "pool_op_cudnn.cu",
-                    "utility_ops.cu",
-                    "max_pool_with_index.cu"}#REVIST THIS FILE
-
-for folder in ["operators", "sgd", "image", "transforms", "video", "distributed"]:
-    for extension in ["/*.cu", "/*.cuh", "/*gpu.cc", "/*gpu.h"]:
-        target = folder + extension
-        for file in glob.glob(target):
-        	if file.split('/')[-1] not in ignore_file_list:
-        		os.system(hipify_tool + ' --inplace ' + file)
-        os.chdir(folder)
-        if glob.glob('*hip.cc'):
-            os.system("mv -n *hip.cc hip")
-            os.system("rm -f *hip.cc")
-        os.chdir(caffe2_root)
-
-"""
 
