05568cfac8aff51171bb1d16ec0454602ef647ee
Edward Z. Yang <ezyang@fb.com>
2018-07-09 10:15:57 -0400

    Warn about THPObjectPtr needing GIL.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/utils/object_ptr.h
+++ torch/csrc/utils/object_ptr.h
@@ -24,4 +24,15 @@ private:
   T *ptr = nullptr;
 };
 
+/**
+ * An RAII-style, owning pointer to a PyObject.  You must protect
+ * construction and destruction of this object with the GIL.
+ *
+ * WARNING: Think twice before putting this as a field in a C++
+ * struct.  This class does NOT take out the GIL on destruction,
+ * so if you will need to ensure that the destructor of your struct
+ * is either (a) always invoked when the GIL is taken or (b) takes
+ * out the GIL itself.  Easiest way to avoid this problem si to
+ * not use THPPointer if you can.
+ */
 typedef THPPointer<PyObject> THPObjectPtr;
