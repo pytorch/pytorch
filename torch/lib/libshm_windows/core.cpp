f6ba01e0a586647012535eb5213deba1b353b858
Edward Z. Yang <ezyang@fb.com>
2018-07-10 07:23:50 -0700

    Windows fixes
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/lib/libshm_windows/core.cpp
+++ torch/lib/libshm_windows/core.cpp
@@ -20,32 +20,8 @@ void libshm_context_free(libshm_context *ctx) {
   delete ctx;
 }
 
-void * libshm_alloc(void *_ctx, ptrdiff_t size) {
-  auto *ctx = (libshm_context*)_ctx;
-  return getTHRefcountedMapAllocator()->allocate(ctx->th_context, size);
-}
+THManagedSharedDeleter THManagedSharedDeleter::singleton_;
 
-void * libshm_realloc(void *_ctx, void *data, ptrdiff_t size) {
-  THError("cannot realloc shared memory");
-  return NULL;
-}
-
-void libshm_free(void *_ctx, void *data) {
-  auto *ctx = (libshm_context*)_ctx;
-  getTHRefcountedMapAllocator()->deallocate(ctx->th_context, data);
-  libshm_context_free(ctx);
-}
-
-struct THManagedSharedAllocator : public at::Allocator {
-  void* allocate(void* ctx, size_t size) const override {
-    return libshm_alloc(ctx, size);
-  }
-  void deallocate(void* ctx, void* data) const override {
-    return libshm_free(ctx, data);
-  }
-};
-
-static THManagedSharedAllocator th_managed_shared_allocator;
-at::Allocator* getTHManagedSharedAllocator() {
-  return &th_managed_shared_allocator;
+void THManagedSharedDeleter::deallocate(void* ctx, void* data) const {
+  return libshm_free(ctx, data);
 }
--- torch/lib/libshm_windows/libshm.h
+++ torch/lib/libshm_windows/libshm.h
@@ -17,13 +17,28 @@
 
 typedef struct {
   char *manager_handle;
+  // NB: th_context is a temporary field
   THMapAllocatorContext *th_context;
+  at::BoundDeleter th_deleter;
 } libshm_context;
 
 SHM_API void libshm_init(const char *manager_exec_path);
 SHM_API libshm_context * libshm_context_new(const char *manager_handle, const char *filename, int flags);
+SHM_API std::unique_ptr<void, at::BoundDeleter> libshm_alloc(void *_ctx, ptrdiff_t size);
 SHM_API void libshm_context_free(libshm_context *context);
 
-SHM_API at::Allocator* getTHManagedSharedAllocator();
+struct THManagedSharedDeleter : public at::Deleter {
+  void deallocate(void* ctx, void* data) const override;
+  static at::BoundDeleter make(libshm_context* ctx, at::BoundDeleter deleter) {
+    ctx->th_deleter = deleter;
+    return {&singleton_, ctx};
+  }
+  static libshm_context * getContext(at::BoundDeleter bd) {
+    if (bd.deleter_ != &singleton_) return nullptr;
+    return static_cast<libshm_context*>(bd.ctx_);
+  }
+private:
+  static THManagedSharedDeleter singleton_;
+};
 
 #endif
