4bca7fb2a78b99c7330ceba1ab94cfc1f5945cd9
Roy Li <royboy@fb.com>
2018-07-10 15:05:10 -0700

    Implement grid_sampler in aten (#8929)
    Summary:
    Partially addresses #8928.
    
    Maybe #7273?
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/8929
    
    Reviewed By: ezyang
    
    Differential Revision: D8668919
    
    Pulled By: li-roy
    
    fbshipit-source-id: 8ad07b224d2ab211c274c4c10f042501efaae32c

--- torch/nn/_functions/vision.py
+++ torch/nn/_functions/vision.py
@@ -5,19 +5,6 @@ from torch._thnn import type2backend
 from .thnn.auto import function_by_name
 import torch.backends.cudnn as cudnn
 
-MODE_ZEROS = 0
-MODE_BORDER = 1
-
-
-def grid_sampler(input, grid, padding_mode):
-    if cudnn.is_acceptable(input.data) \
-       and padding_mode == 'zeros' \
-       and input.dim() == 4 \
-       and input.size(1) <= 1024:  # as of cudnn 7102, will not work for larger than 1024
-        return torch.cudnn_grid_sampler(input, grid)
-    else:
-        return GridSampler.apply(input, grid, padding_mode)
-
 
 def affine_grid_generator(theta, size):
     if theta.data.is_cuda:
@@ -33,58 +20,6 @@ def affine_grid_generator(theta, size):
 
 
 # TODO: Port these completely into C++
-
-
-class GridSampler(Function):
-
-    @staticmethod
-    def forward(ctx, input, grid, padding_mode='zeros'):
-        ctx.save_for_backward(input, grid)
-
-        if input.device != grid.device:
-            raise RuntimeError(("input (device {}) and grid (device {}) must be on the same device" +
-                                "for grid_sampler").format(input.device, grid.device))
-        if padding_mode == 'zeros':
-            ctx.padding_mode = MODE_ZEROS
-        elif padding_mode == 'border':
-            ctx.padding_mode = MODE_BORDER
-        else:
-            raise ValueError("padding_mode needs to be 'zeros' or 'border', but got {}".format(padding_mode))
-
-        grid_sz = grid.size()
-        backend = type2backend[input.type()]
-        if input.dim() == 4:
-            output = input.new(grid_sz[0], input.size(1), grid_sz[1], grid_sz[2])
-            backend.SpatialGridSamplerBilinear_updateOutput(backend.library_state, input, grid,
-                                                            output, ctx.padding_mode)
-        elif input.dim() == 5:
-            output = input.new(grid_sz[0], input.size(1), grid_sz[1], grid_sz[2], grid_sz[3])
-            backend.VolumetricGridSamplerBilinear_updateOutput(backend.library_state, input, grid,
-                                                               output, ctx.padding_mode)
-        else:
-            raise ValueError("input has to be 4d or 5d but got input of shape: {}".format(input.shape))
-        return output
-
-    @staticmethod
-    @once_differentiable
-    def backward(ctx, grad_output):
-        input, grid = ctx.saved_tensors
-        padding_mode = ctx.padding_mode
-
-        backend = type2backend[input.type()]
-        grad_input = input.new(input.size())
-        grad_grid = grid.new(grid.size())
-        if input.dim() == 4:
-            backend.SpatialGridSamplerBilinear_updateGradInput(
-                backend.library_state, input, grad_input,
-                grid, grad_grid, grad_output, padding_mode)
-        elif input.dim() == 5:
-            backend.VolumetricGridSamplerBilinear_updateGradInput(
-                backend.library_state, input, grad_input,
-                grid, grad_grid, grad_output, padding_mode)
-        else:
-            raise ValueError("input has to be 4d or 5d but got input of shape: {}".format(input.shape))
-        return grad_input, grad_grid, None
 
 
 class AffineGridGenerator(Function):
