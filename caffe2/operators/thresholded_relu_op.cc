7f33ec55b266911ee29558cbef00adc4a623c52d
Orion Reblitz-Richardson <orionr@fb.com>
2018-07-11 13:52:48 -0700

    Fix Eigen issue on OS X with CUDA and nvcc compile (#9350)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9350
    
    Re-apply #9270
    
    Breaking this out of #8338
    
    This takes care of the Eigen failure we saw on Mac CUDA builds when BUILD_CAFFE2 and BUILD_ATEN were removed. Fix is to isolate Eigen from headers included by cu files and processed by nvcc. This was worked on with smessmer.
    
    Reviewed By: mingzhe09088
    
    Differential Revision: D8794431
    
    fbshipit-source-id: de656334af46c697802073f8e8d9a6aeb9ca65a7

--- caffe2/operators/thresholded_relu_op.cc
+++ caffe2/operators/thresholded_relu_op.cc
@@ -1,5 +1,6 @@
 #include "caffe2/operators/thresholded_relu_op.h"
 
+#include "caffe2/utils/eigen_utils.h"
 #include "caffe2/utils/math.h"
 
 namespace caffe2 {
--- caffe2/utils/math.h
+++ caffe2/utils/math.h
@@ -17,9 +17,6 @@ extern "C" {
 #include "caffe2/core/types.h"
 #include "caffe2/utils/math_utils.h"
 
-#include "Eigen/Core"
-#include "Eigen/Dense"
-
 namespace caffe2 {
 
 template <class Context>
@@ -28,30 +25,6 @@ class Tensor;
 // An empty class as a placeholder for a math function that has no specific
 // engine specified.
 class DefaultEngine {};
-
-// Common Eigen types that we will often use
-template <typename T>
-using EigenMatrixMap =
-    Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>;
-template <typename T>
-using EigenArrayMap =
-    Eigen::Map<Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>>;
-template <typename T>
-using EigenVectorMap = Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, 1>>;
-template <typename T>
-using EigenVectorArrayMap = Eigen::Map<Eigen::Array<T, Eigen::Dynamic, 1>>;
-template <typename T>
-using ConstEigenMatrixMap =
-    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>;
-template <typename T>
-using ConstEigenArrayMap =
-    Eigen::Map<const Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>>;
-template <typename T>
-using ConstEigenVectorMap =
-    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>;
-template <typename T>
-using ConstEigenVectorArrayMap =
-    Eigen::Map<const Eigen::Array<T, Eigen::Dynamic, 1>>;
 
 namespace math {
 
