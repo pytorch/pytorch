2bac7c4564014943b1225226a4ed4e2e9825cc3d
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-07-11 16:28:14 +0200

    Update convert_parameters.py

--- torch/nn/utils/convert_parameters.py
+++ torch/nn/utils/convert_parameters.py
@@ -31,6 +31,8 @@ def vector_to_parameters(vec, parameters):
         parameters (Iterable[Tensor]): an iterator of Tensors that are the
             parameters of a model.
     """
+    # Dimensionality check
+    assert vec.numel() == sum([p.numel() for p in parameters])
     # Ensure vec of type Tensor
     if not isinstance(vec, torch.Tensor):
         raise TypeError('expected torch.Tensor, but got: {}'
