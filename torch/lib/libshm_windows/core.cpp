fa68ceb0a73eed00415dfb9adb109034733abb83
Edward Z. Yang <ezyang@fb.com>
2018-07-11 15:07:33 -0700

    A stab at Windows
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/lib/libshm_windows/core.cpp
+++ torch/lib/libshm_windows/core.cpp
@@ -9,28 +9,16 @@
 void libshm_init(const char *manager_exec_path) {
 }
 
-libshm_context * libshm_context_new(const char *manager_handle, const char *filename, int flags) {
-  libshm_context *ctx = new libshm_context();
-  ctx->manager_handle = "no_manager";
-  ctx->th_context = THMapAllocatorContext_new(filename, flags);
-  return ctx;
+static void deleteTHManagedMapAllocator(void* ptr) {
+  delete static_cast<THManagedMapAllocator*>(ptr);
 }
 
-void libshm_context_free(libshm_context *ctx) {
-  delete ctx;
+at::SupervisedPtr THManagedMapAllocator::makeSupervisedPtr(const char* manager_handle, const char* filename, int flags, ptrdiff_t size) {
+  auto* supervisor = new THManagedMapAllocator(manager_handle, filename, flags, size);
+  return {supervisor->data(), {supervisor, &deleteTHManagedMapAllocator}};
 }
 
-THManagedSharedDeleter THManagedSharedDeleter::singleton_;
-
-
-at::SupervisedPtr libshm_alloc(void *_ctx, ptrdiff_t size) {
-  auto *ctx = (libshm_context*)_ctx;
-  return THRefcountedMapAllocator_alloc(ctx->th_context, size);
-}
-
-
-void THManagedSharedDeleter::deallocate(void* _ctx, void* data) const {
-  auto *ctx = (libshm_context*)_ctx;
-  ctx->th_deleter(data);
-  return libshm_context_free(ctx);
+THManagedMapAllocator* THManagedMapAllocator::fromSupervisedPtr(const at::SupervisedPtr& sptr) {
+  if (sptr.supervisor_.get_deleter() != &deleteTHManagedMapAllocator) return nullptr;
+  return static_cast<THManagedMapAllocator*>(sptr.supervisor_.get());
 }
