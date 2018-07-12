3b81072fbc80def3e272f76900585acc86873eda
vishwakftw <cs15btech11043@iith.ac.in>
2018-07-12 09:50:20 -0400

    Exclude check for Error and DelayedError

--- torch/csrc/autograd/python_cpp_function.cpp
+++ torch/csrc/autograd/python_cpp_function.cpp
@@ -2,6 +2,7 @@
 
 #include "torch/csrc/python_headers.h"
 #include <memory>
+#include <string>
 #include <stdio.h>
 #include <typeindex>
 #include <unordered_map>
@@ -28,7 +29,8 @@ PyObject* THPCppFunction_call(PyObject* self, PyObject* args, PyObject *kwargs)
 
   int num_inputs = PyTuple_GET_SIZE(args);
   int num_inputs_required = ((THPCppFunction*)self)->cdata->num_inputs();
-  if (num_inputs != num_inputs_required) {
+  std::string self_name = ((THPCppFunction*)self)->cdata->name();
+  if ((self_name.find("Error") == std::string::npos) && (num_inputs != num_inputs_required)) {
     return PyErr_Format(PyExc_TypeError, "expected %d arguments, got %d instead",
                         num_inputs, num_inputs_required);
   }
