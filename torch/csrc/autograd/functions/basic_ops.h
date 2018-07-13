5aaf3fcf6c7671aee5292daa2e98e77c48d0e60d
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2018-07-13 14:37:09 -0700

    Fix segmentation fault in grad_fn (#9292)
    Summary: Fixes #8774 .
    
    Reviewed By: soumith
    
    Differential Revision: D8836478
    
    Pulled By: apaszke
    
    fbshipit-source-id: f113bf47fe493be9f095a5a5490caf08dbb44e38

--- torch/csrc/autograd/functions/basic_ops.h
+++ torch/csrc/autograd/functions/basic_ops.h
@@ -25,8 +25,11 @@ struct Error : public Function {
 
 // Identity in forward, Error in backward. Used to implement @once_differentiable
 struct DelayedError : public Function {
-  DelayedError(std::string msg)
-    : msg(std::move(msg)) {};
+  DelayedError(std::string msg, int num_inputs)
+    : msg(std::move(msg)) {
+      for (int i = 0; i < num_inputs; i++)
+        add_input_metadata(Function::undefined_input());
+    }
 
   variable_list apply(variable_list&& inputs) override;
 
