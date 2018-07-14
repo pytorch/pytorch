05912413ed8721168a18d76df59aa2ef793043a9
Junjie Bai <bai@in.tum.de>
2018-07-13 21:02:52 -0700

    move rocm build steps together in build script

--- .jenkins/caffe2/build.sh
+++ .jenkins/caffe2/build.sh
@@ -155,6 +155,9 @@ if [[ $BUILD_ENVIRONMENT == *rocm* ]]; then
   export LANG=C.UTF-8
   export LC_ALL=C.UTF-8
   export HCC_AMDGPU_TARGET=gfx900
+
+  ########## HIPIFY Caffe2 operators
+  ${PYTHON} "${ROOT_DIR}/tools/amd_build/build_caffe2_amd.py"
 fi
 
 # Try to include Redis support for Linux builds
@@ -195,8 +198,7 @@ else
 fi
 
 
-########## Caffe2 HIP OP GENERATOR
-${PYTHON} "${ROOT_DIR}/tools/amd_build/build_caffe2_amd.py"
+
 ###############################################################################
 # Configure and make
 ###############################################################################
