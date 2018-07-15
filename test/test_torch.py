5eb9d40cc6b5f248db203b9b76de9b30f1c56d65
bhushan <bhushan.s.94@gmail.com>
2018-07-15 07:53:23 -0700

    Introducing IsInf (#9169)
    Summary:
    torch.isinf - checks element wise +/- inf implements #9132
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9169
    
    Reviewed By: SsnL
    
    Differential Revision: D8768614
    
    Pulled By: zou3519
    
    fbshipit-source-id: dd1b5f6c976deb421d626e22cdd25500ec04d796

--- test/test_torch.py
+++ test/test_torch.py
@@ -4711,6 +4711,10 @@ class TestTorch(TestCase):
         x = torch.Tensor([1, float('nan'), 2])
         self.assertEqual(torch.isnan(x), torch.ByteTensor([0, 1, 0]))
 
+    def test_isinf(self):
+        x = torch.Tensor([1, float('inf'), 2, float('-inf'), float('nan')])
+        self.assertEqual(torch.isinf(x), torch.ByteTensor([0, 1, 0, 1, 0]))
+
     def test_RNGState(self):
         state = torch.get_rng_state()
         stateCloned = state.clone()
