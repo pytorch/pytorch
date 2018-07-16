c923de1d17fbb3ac6dbb90ca92c61e0289e521e0
Junjie Bai <bai@in.tum.de>
2018-07-16 16:57:14 -0700

    Merge .cu and _gpu.cc files
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/9473
    
    Reviewed By: houseroad
    
    Differential Revision: D8865754
    
    Pulled By: bddppq
    
    fbshipit-source-id: 406eda6c145f03a0ee35c4643ec1ec0092fbce88

--- caffe2/operators/filler_op_gpu.cc
+++
@@ -1,9 +0,0 @@
-#include "caffe2/core/context_gpu.h"
-#include "caffe2/operators/filler_op.h"
-#include "caffe2/operators/operator_fallback_gpu.h"
-
-namespace caffe2 {
-REGISTER_CUDA_OPERATOR(
-    LengthsRangeFill,
-    GPUFallbackOp<LengthsRangeFillOp<CPUContext>>);
-}
