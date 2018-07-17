3418d71d6a1924810df352857190067105318c71
vishwakftw <cs15btech11043@iith.ac.in>
2018-07-17 15:53:50 -0400

    Fix byteordering issue in from_numpy

--- test/test_torch.py
+++ test/test_torch.py
@@ -7481,7 +7481,7 @@ class TestTorch(TestCase):
 
     @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
     def test_ctor_with_numpy_array(self):
-        dtypes = [
+        correct_dtypes = [
             np.double,
             np.float,
             np.float16,
@@ -7490,7 +7490,11 @@ class TestTorch(TestCase):
             np.int16,
             np.uint8
         ]
-        for dtype in dtypes:
+
+        incorrect_byteorder = '>' if sys.byteorder == 'little' else '<'
+        incorrect_dtypes = map(lambda t: incorrect_byteorder + t, ['d', 'f', 'i'])
+
+        for dtype in correct_dtypes:
             array = np.array([1, 2, 3, 4], dtype=dtype)
 
             # Upcast
@@ -7520,6 +7524,12 @@ class TestTorch(TestCase):
                 tensor = torch.cuda.HalfTensor(array)
                 for i in range(len(array)):
                     self.assertEqual(tensor[i], array[i])
+
+        for dtype in incorrect_dtypes:
+            array = np.array([1, 2, 3, 4], dtype=dtype)
+            error_string = "given numpy array has byte order different from the native byte order."
+            with self.assertRaisesRegex(ValueError, error_string):
+                tensor = torch.from_numpy(array)
 
     @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
     def test_numpy_index(self):
