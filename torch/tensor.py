254aae6288f1732fdfacc91d0e1b8005a10fe955
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-07-17 08:29:45 -0700

    change stft to have consistent signature with librosa (#9308)
    Summary:
    Fixes #7883 by using `rfft`.
    
    It's worth noting that this is BC breaking. And it's impossible to detect the change because the two signatures before and after this change supports a common subset of calling patterns, e.g., `stft(Tensor, int, int)`. (some other calling patterns will raise error).
    
    soumith and I plan to change the current `stft` interface because it is a bit messy and non-standard. rafaelvalle suggested us that `librosa` is a good reference API to align with. After discussing with soumith and ezyang , and given that `stft` is only out for 1 release, I decide to go with directly changing the signature. Also, my understanding is that most researchers in this field will welcome this change as `librosa` seems to be the golden-standard here. (it doesn't yet support all `pad_mode` but those will become available if added to `F.pad`.)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9308
    
    Differential Revision: D8806148
    
    fbshipit-source-id: 2e00c2105d039cb822ae2161c5ce2a9d46831355

--- torch/tensor.py
+++ torch/tensor.py
@@ -260,6 +260,17 @@ class Tensor(torch._C._TensorBase):
         else:
             return super(Tensor, self).btrifact(pivot=pivot)
 
+    def stft(self, n_fft, hop_length=None, win_length=None, window=None,
+             center=True, pad_mode='reflect', normalized=False, onesided=True):
+        r"""See :func:`torch.stft`
+
+        .. warning::
+          This function changed signature at version 0.4.1. Calling with
+          the previous signature may cause error or return incorrect result.
+        """
+        return torch.stft(self, n_fft, hop_length, win_length, window, center,
+                          pad_mode, normalized, onesided)
+
     def resize(self, *sizes):
         warnings.warn("non-inplace resize is deprecated")
         from torch.autograd._functions import Resize
