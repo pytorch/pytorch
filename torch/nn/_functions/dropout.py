b40e338bdd3aff27087d19dd8f1600ce0e74b495
tippisum <tippisum@gmail.com>
2018-07-17 17:09:05 -0700

    Implement 2D and 3D alpha_dropout (#9073)
    Summary:
    It implements per-channel alpha_dropout. It also creates corresponding function classes and unifies the process of dropout and alpha_dropout.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9073
    
    Differential Revision: D8727008
    
    Pulled By: ezyang
    
    fbshipit-source-id: 9d509f9c5db4e98f7b698cdfc4443505a4d2b331

--- torch/nn/_functions/dropout.py
+++ torch/nn/_functions/dropout.py
@@ -66,3 +66,65 @@ class FeatureDropout(Dropout):
     def _make_noise(input):
         return input.new().resize_(input.size(0), input.size(1),
                                    *repeat(1, input.dim() - 2))
+
+
+class AlphaDropout(Dropout):
+
+    @staticmethod
+    def symbolic(g, input, p=0.5, train=False, inplace=False):
+        # See Note [Export inplace]
+        # NB: In inference mode, FeatureDropout is exported as an identity op.
+        from torch.onnx.symbolic import _unimplemented
+        if train:
+            return _unimplemented("AlphaDropout", "training mode")
+        return input
+
+    @classmethod
+    def forward(cls, ctx, input, p=0.5, train=False, inplace=False):
+        if p < 0 or p > 1:
+            raise ValueError("dropout probability has to be between 0 and 1, "
+                             "but got {}".format(p))
+        ctx.p = p
+        ctx.train = train
+        ctx.inplace = inplace
+
+        if ctx.p == 0 or not ctx.train:
+            return input
+
+        if ctx.inplace:
+            ctx.mark_dirty(input)
+            output = input
+        else:
+            output = input.clone()
+
+        ctx.noise = cls._make_noise(input)
+        if ctx.p == 1:
+            a = 0
+            b = ctx.noise
+        else:
+            ctx.noise.bernoulli_(1 - ctx.p)
+            alpha = 1.7580993408473766
+            a = ((alpha ** 2 * ctx.p + 1) * (1 - ctx.p)) ** (-0.5)
+            b = ctx.noise.add(-1).mul_(alpha * a).add_(alpha * a * ctx.p)
+        ctx.noise = ctx.noise.mul_(a).expand_as(input)
+        b = b.expand_as(input)
+        output.mul_(ctx.noise).add_(b)
+
+        return output
+
+
+class FeatureAlphaDropout(AlphaDropout):
+
+    @staticmethod
+    def symbolic(g, input, p=0.5, train=False, inplace=False):
+        # See Note [Export inplace]
+        # NB: In inference mode, FeatureDropout is exported as an identity op.
+        from torch.onnx.symbolic import _unimplemented
+        if train:
+            return _unimplemented("FeatureAlphaDropout", "training mode")
+        return input
+
+    @staticmethod
+    def _make_noise(input):
+        return input.new().resize_(input.size(0), input.size(1),
+                                   *repeat(1, input.dim() - 2))
