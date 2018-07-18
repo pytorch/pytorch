add topk, softmax, argmax, fix for loop

test/test_jit.py
@@ -1184,6 +1184,70 @@ class TestBatched(TestCase):
         res = [torch.where(xs_cond[j], xs[j], xs2[j]) for j in range(4)]
         self.assertEqual(res, res_batch.examples())
 
+    def test_batch_argmax(self):
+        @torch.jit.batch(batch_size=4)
+        def argmax(a):
+            return torch.argmax(a, 1)
+
+        xs, batch = self.rand_batch(4, (True, 5), (True, 6))
+        res_batch = argmax(batch)
+        res = [torch.argmax(xs[j], 1) for j in range(4)]
+        self.assertEqual(res, res_batch.examples())
+
+        @torch.jit.batch(batch_size=4)
+        def argmax(a):
+            return torch.argmax(a, 1, False)
+
+        res_batch = argmax(batch)
+        res = [torch.argmax(xs[j], 1, False) for j in range(4)]
+        self.assertEqual(res, res_batch.examples())
+
+    def test_batch_topk(self):
+        @torch.jit.batch(batch_size=4)
+        def topk(a):
+            return torch.topk(a, 3, 1)
+
+        xs, batch = self.rand_batch(4, (False, 5), (True, 6))
+
+        # along static dim
+        res_batch = topk(batch)
+        res = [torch.topk(xs[j], 3, 1)[0] for j in range(4)]
+        res_idx = [torch.topk(xs[j], 3, 1)[1] for j in range(4)]
+        self.assertEqual(res, res_batch[0].examples())
+        self.assertEqual(res_idx, res_batch[1].examples())
+
+        @torch.jit.batch(batch_size=4)
+        def topk(a):
+            return torch.topk(a, 1, 2)
+
+        # along dynamic dim
+        res_batch = topk(batch)
+        res = [torch.topk(xs[j], 1, 2)[0] for j in range(4)]
+        res_idx = [torch.topk(xs[j], 1, 2)[1] for j in range(4)]
+        self.assertEqual(res, res_batch[0].examples())
+        self.assertEqual(res_idx, res_batch[1].examples())
+
+    def test_batch_softmax(self):
+        @torch.jit.batch(batch_size=4)
+        def softmax(a):
+            return torch.softmax(a, 1)
+
+        xs, batch = self.rand_batch(4, (False, 5), (True, 6))
+
+        # along static dim
+        res_batch = softmax(batch)
+        res = [torch.softmax(xs[j], 1) for j in range(4)]
+        self.assertEqual(res, res_batch.examples())
+
+        @torch.jit.batch(batch_size=4)
+        def softmax(a):
+            return torch.softmax(a, 2)
+
+        # along dynamic dim
+        res_batch = softmax(batch)
+        res = [torch.softmax(xs[j], 2) for j in range(4)]
+        self.assertEqual(res, res_batch.examples())
+
     def test_lstm(self):
         def LSTM(x_all, h, c, w_xi, w_xf, w_xo, w_xc, w_hi, w_hf, w_ho, w_hc, b_i, b_f, b_o, b_c):
             for i in range(x_all.size(1)):
@@ -1198,8 +1262,10 @@ class TestBatched(TestCase):
                 # cell computations
                 c_t = torch.matmul(x, w_xc) + torch.matmul(h, w_hc) + b_c
                 c_t = torch.tanh(c_t)
-                c_t = torch.mul(c, f_t) + torch.mul(i_t, c_t)
+                c_t = torch.mul(c_t, f_t) + torch.mul(i_t, c_t)
                 h_t = torch.mul(o_t, torch.tanh(c_t))
+                h = h_t
+                c = c_t
             return h
 
         @torch.jit.batch(batch_size=4)
@@ -1216,8 +1282,10 @@ class TestBatched(TestCase):
                 # cell computations
                 c_t = torch.matmul(x, w_xc) + torch.matmul(h, w_hc) + b_c
                 c_t = torch.tanh(c_t)
-                c_t = torch.mul(c, f_t) + torch.mul(i_t, c_t)
+                c_t = torch.mul(c_t, f_t) + torch.mul(i_t, c_t)
                 h_t = torch.mul(o_t, torch.tanh(c_t))
+                h = h_t
+                c = c_t
             return h
 
         batch_size, input_size, hidden_size = 4, 3, 2

torch/jit/batchop.py
@@ -95,9 +95,9 @@ def batch_select(data, mask, dims, dim, index):
     #     raise ValueError("Cannot select 0 dim in BatchTensor")
     data = data.select(dim, index)
     if dims[dim - 1]:
-        mask = mask.select(dim, 0)
-    else:
         mask = mask.select(dim, index)
+    else:
+        mask = mask.select(dim, 0)
     dims = torch.cat((dims[:dim - 1], dims[dim:dims.size(0)]))
     return data, mask, dims
 
@@ -109,6 +109,12 @@ def batch_where(data, mask, dims, data1, mask1, dims1, data2, mask2, dims2):
     res_mask = torch.where(data, mask1, mask2)
     res_dims = dims1 or dims2
     return res_data, res_mask, res_dims
+
+
+@torch.jit.script
+def batch_update(batch_data, batch_mask, batch_dims, new_data, new_mask, new_dims):
+    data = torch.where(new_mask, new_data, batch_data)
+    return data, new_mask, new_dims  # TODO: consider whether return new_mask and new_dims
 
 
 @torch.jit.script
@@ -127,6 +133,101 @@ def batch_gt(data, mask, dims, data1, mask1, dims1):
 
 
 @torch.jit.script
+def batch_size(data, mask, dims, dim):
+    return data.size(dim)
+
+
+@torch.jit.script
+def batch_argmax(data, mask, dims, dim, keepdim):
+    # if dim == 0:
+    #     raise ValueError("cannot do argmax along batch_dim")
+    batch_size = data.size(0)
+    res_data = None
+    for i in range(batch_size):
+        if dims[dim - 1]:
+            if dim - 1 != 0:
+                m = mask[i].transpose(0, dim - 1)
+            else:
+                m = mask[i]
+            valid_num = m.sum(0, keepdim=True)
+            while(valid_num.dim() >= 1):
+                valid_num = valid_num[0]
+            d = data[i].unsqueeze(0).narrow(dim, 0, valid_num)
+        else:
+            d = data[i].unsqueeze(0)
+        d = d.argmax(dim, keepdim)
+        if i == 0:
+            res_data = d
+        else:
+            res_data = torch.cat([res_data, d], 0)
+    if keepdim:
+        mask = mask
+    else:
+        mask = mask.select(dim, 0)
+        dims = torch.cat((dims[:dim - 1], dims[dim:dims.size(0)]))
+    return res_data, mask, dims
+
+
+@torch.jit.script
+def batch_topk(data, mask, dims, k, dim, largest, sorted):
+    # if dim == 0:
+    #     raise ValueError("cannot do topk along batch_dim")
+    batch_size = data.size(0)
+    res_data = None
+    res_index = None
+    for i in range(batch_size):
+        if dims[dim - 1]:
+            if dim - 1 != 0:
+                m = mask[i].transpose(0, dim - 1)
+            else:
+                m = mask[i]
+            valid_num = m.sum(0, keepdim=True)
+            while(valid_num.dim() >= 1):
+                valid_num = valid_num[0]
+            d = data[i].unsqueeze(0).narrow(dim, 0, valid_num)
+        else:
+            d = data[i].unsqueeze(0)
+        d, idx = d.topk(k, dim, largest, sorted)
+        if i == 0:
+            res_data = d
+            res_index = idx
+        else:
+            res_data = torch.cat([res_data, d], 0)
+            res_index = torch.cat([res_index, idx], 0)
+    if dims[dim - 1]:
+        mask = mask.narrow(dim, 0, k)
+    return res_data, mask, dims, res_index, mask, dims
+
+
+@torch.jit.script
+def batch_softmax(data, mask, dims, dim):
+    # if dim == 0:
+    #     raise ValueError("cannot do softmax along batch_dim")
+    batch_size = data.size(0)
+    max_len = data.size(dim)
+    res_data = None
+    for i in range(batch_size):
+        if dims[dim - 1]:
+            if dim - 1 != 0:
+                m = mask[i].transpose(0, dim - 1)
+            else:
+                m = mask[i]
+            valid_num = m.sum(0, keepdim=True)
+            while(valid_num.dim() >= 1):
+                valid_num = valid_num[0]
+            d = data[i].unsqueeze(0).narrow(dim, 0, valid_num).softmax(dim)
+            if valid_num < max_len:
+                d = torch.cat([d, data[i].unsqueeze(0).narrow(dim, valid_num, max_len - valid_num)], dim)
+        else:
+            d = data[i].unsqueeze(0).softmax(dim)
+        if i == 0:
+            res_data = d
+        else:
+            res_data = torch.cat([res_data, d], 0)
+    return res_data, mask, dims
+
+
+@torch.jit.script
 def batch_from_scalar_tensor(data):
     data = data.unsqueeze(0)
     mask = torch.ones([1], dtype=torch.uint8)
@@ -142,7 +243,12 @@ torch.register_batch_operator("matmul", batch_matmul.graph)
 torch.register_batch_operator("mm", batch_mm.graph)
 torch.register_batch_operator("select", batch_select.graph)
 torch.register_batch_operator("where", batch_where.graph)
+torch.register_batch_operator("update", batch_update.graph)
 torch.register_batch_operator("any", batch_any.graph)
 torch.register_batch_operator("type_as", batch_type_as.graph)
 torch.register_batch_operator("gt", batch_gt.graph)
+torch.register_batch_operator("size", batch_size.graph)
+torch.register_batch_operator("argmax", batch_argmax.graph)
+torch.register_batch_operator("topk", batch_topk.graph)
+torch.register_batch_operator("softmax", batch_softmax.graph)
 torch.register_batch_operator("batch_from_scalar_tensor", batch_from_scalar_tensor.graph)