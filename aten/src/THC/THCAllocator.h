4791003bd41444aee2604ef96ffb7901d196c577
Edward Yang <ezyang@fb.com>
2018-07-17 20:16:42 -0700

    Be strict prototypes clean. (#9516)
    Summary:
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9516
    
    Differential Revision: D8886493
    
    Pulled By: ezyang
    
    fbshipit-source-id: fea974fd96c7d81126a129eb5b8b06eb1b028526

--- aten/src/THC/THCAllocator.h
+++ aten/src/THC/THCAllocator.h
@@ -3,8 +3,8 @@
 
 #include "THCGeneral.h"
 
-THC_API THAllocator* getTHCudaHostAllocator();
-THC_API THAllocator* getTHCUVAAllocator();
+THC_API THAllocator* getTHCudaHostAllocator(void);
+THC_API THAllocator* getTHCUVAAllocator(void);
 // IPC doesn't support (re)allocation
 
 #ifdef __cplusplus
