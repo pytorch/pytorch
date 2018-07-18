176602feb8deb9e3974d28c475b30acf0a990858
vishwakftw <cs15btech11043@iith.ac.in>
2018-07-17 17:09:14 -0700

    Added hash for device (#9246)
    Summary:
    If this is good, I could write some tests to ensure collision doesn't occur within a given range.
    
    Closes #7228
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9246
    
    Differential Revision: D8872608
    
    Pulled By: ezyang
    
    fbshipit-source-id: 0ed29a73188f4167b42756f59a5c9a3d5cb37326

--- aten/src/ATen/Device.h
+++ aten/src/ATen/Device.h
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <iosfwd>
 #include <string>
+#include <functional>
 
 namespace at {
 /// Represents a a compute device on which a tensor is located. A device is
@@ -112,3 +113,16 @@ struct Device {
 
 std::ostream& operator<<(std::ostream& stream, at::Device::Type type);
 std::ostream& operator<<(std::ostream& stream, const at::Device& device);
+
+namespace std {
+  template<> struct hash<at::Device>
+  {
+    size_t operator()(const at::Device& device) const noexcept {
+      size_t hash_val = static_cast<size_t>(device.index() + 1);
+      if (device.is_cuda()) {
+        hash_val += 2;
+      }
+      return hash_val;
+    }
+  };
+} // namespace std
