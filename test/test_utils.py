62ebece3716f914a3b78c3268cd1f4c71ad7e15d
jithunnair-amd <jithun.nair@amd.com>
2018-07-18 10:32:40 -0500

    Remove OVERRIDE_DISABLED_UNIT_TESTS_ROCM define as one can just NOT define BUILT_WITH_ROCM to run all unit tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -19,8 +19,7 @@ from torch.utils.trainer.plugins import *
 from torch.utils.trainer.plugins.plugin import Plugin
 from torch.autograd._functions.utils import prepare_onnx_paddings
 from torch.autograd._functions.utils import check_onnx_broadcast
-from common import IS_WINDOWS, IS_PPC, BUILT_WITH_ROCM, OVERRIDE_DISABLED_UNIT_TESTS_ROCM
-
+from common import IS_WINDOWS, IS_PPC, BUILT_WITH_ROCM
 
 HAS_CUDA = torch.cuda.is_available()
 
@@ -413,7 +412,7 @@ class TestFFI(TestCase):
 
     @unittest.skipIf(not HAS_CFFI or not HAS_CUDA, "ffi tests require cffi package")
     @unittest.skipIf(IS_WINDOWS, "ffi doesn't currently work on Windows")
-    @unittest.skipIf(BUILT_WITH_ROCM and not OVERRIDE_DISABLED_UNIT_TESTS_ROCM, "Test disabled for ROCm")
+    @unittest.skipIf(BUILT_WITH_ROCM, "test doesn't currently work on the ROCm stack")
     def test_gpu(self):
         create_extension(
             name='gpulib',
@@ -617,7 +616,7 @@ class TestBottleneck(TestCase):
         self._check_cuda(out)
 
     @unittest.skipIf(not HAS_CUDA, 'No CUDA')
-    @unittest.skipIf(BUILT_WITH_ROCM and not OVERRIDE_DISABLED_UNIT_TESTS_ROCM, "Test disabled for ROCm")
+    @unittest.skipIf(BUILT_WITH_ROCM, "test doesn't currently work on the ROCm stack")
     def test_bottleneck_cuda(self):
         rc, out, err = self._run_bottleneck('bottleneck/test_cuda.py')
         self.assertEqual(rc, 0, 'Run failed with\n{}'.format(err))
