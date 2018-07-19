bcf0bf42a1727c8ee788f733c28579d0e36a387c
Sam Gross <sgross@fb.com>
2018-07-19 14:10:07 -0700

    Extend DispatchStub to support CUDA dispatch (#9579)
    Summary:
    This is a few files taken from https://github.com/pytorch/pytorch/pull/8919. They're unchanged from the latest versions of that PR.
    
    ```
    This is part of https://github.com/pytorch/pytorch/pull/8919. It's
    separated to make it easier to merge the PR in pieces.
    
    There are a few major changes to DispatchStub
    
     - The environment variable ATEN_CPU_CAPABILITY overrides the CPU
       capability detection code (Previous ATEN_DISABLE_AVX/AVX2)
    
     - DispatchStub is defined in the generic native code instead of the
       CPU_CAPABILITY_DEFAULT kernel.
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9579
    
    Differential Revision: D8909000
    
    Pulled By: colesbury
    
    fbshipit-source-id: fdeb606270b06acdab3c01dba97ec9d81584ecc0

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -44,13 +44,10 @@ if [[ "$BUILD_ENVIRONMENT" == *asan* ]]; then
     (cd test && ! get_exit_code python -c "import torch; torch._C._crash_if_aten_asan(3)")
 fi
 
-export ATEN_DISABLE_AVX=
-export ATEN_DISABLE_AVX2=
 if [[ "${JOB_BASE_NAME}" == *-NO_AVX-* ]]; then
-  export ATEN_DISABLE_AVX=1
-fi
-if [[ "${JOB_BASE_NAME}" == *-NO_AVX2-* ]]; then
-  export ATEN_DISABLE_AVX2=1
+  export ATEN_CPU_CAPABILITY=default
+elif [[ "${JOB_BASE_NAME}" == *-NO_AVX2-* ]]; then
+  export ATEN_CPU_CAPABILITY=avx
 fi
 
 test_python_nn() {
