45f0d05202f0506f39dad7e5cc537b5851dbb298
Marat Dukhan <marat@fb.com>
2018-07-18 19:23:52 -0700

    Adapt OnnxifiOp to removed suffix handling in ONNXIFI loader (#9571)
    Summary:
    Adapt to changes in onnx/onnx#1203
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9571
    
    Reviewed By: yinghai
    
    Differential Revision: D8907892
    
    Pulled By: bddppq
    
    fbshipit-source-id: 9f88471639dbe9050194e84340f335bece834d5d

--- caffe2/onnx/onnxifi_init.cc
+++ caffe2/onnx/onnxifi_init.cc
@@ -11,7 +11,7 @@ onnxifi_library* initOnnxifiLibrary() {
   static onnxifi_library core{};
   std::call_once(once, []() {
     auto ret =
-        onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, nullptr, nullptr, &core);
+        onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, nullptr, &core);
     if (!ret) {
       CAFFE_THROW("Cannot load onnxifi lib");
     }
