53ee104c8adb00b9a2ba688e30b7b698d3b4f544
Christian Puhrsch <cpuhrsch@fb.com>
2018-07-20 09:36:50 -0700

    Remove unnecessary functions from StorageDerived.h
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/9553
    
    Reviewed By: ezyang
    
    Differential Revision: D8915526
    
    Pulled By: cpuhrsch
    
    fbshipit-source-id: 32013d3aa58a1a68637f99ee619d06e27fadaad6

--- aten/src/ATen/templates/StorageDerived.cpp
+++ aten/src/ATen/templates/StorageDerived.cpp
@@ -11,17 +11,16 @@ $extra_cuda_headers
 namespace at {
 
 ${Storage}::${Storage}(Context* context):
-    storage(${THStorage}_new(${state})), context(context) {}
+    Storage(${THStorage}_new(${state})), context(context) {}
 
 ${Storage}::${Storage}(Context* context, THStorage* storage):
-    storage(storage), context(context) {}
+    Storage(storage), context(context) {}
 
 ${Storage}::${Storage}(Context* context, size_t storage_size)
-  : storage(${THStorage}_newWithSize(${state,} storage_size)), context(context) {}
+  : Storage(${THStorage}_newWithSize(${state,} storage_size)), context(context) {}
 
 ${Storage}::${Storage}(Context* context, size_t size, Allocator* allocator)
-  : storage(nullptr),
-    context(context) {
+  : Storage(nullptr), context(context) {
   storage = ${THStorage}_newWithAllocator(${state,} size, allocator);
   ${THStorage}_clearFlag(${state,} storage, TH_STORAGE_RESIZABLE);
 }
@@ -38,7 +37,7 @@ static int getPointerDevice(void* ptr) {
 
 ${Storage}::${Storage}(Context* context,
   void * data, size_t size, const std::function<void(void*)> & deleter)
-  : storage(${THStorage}_newWithDataAndAllocator(${state,}
+  : Storage(${THStorage}_newWithDataAndAllocator(${state,}
       InefficientStdFunctionContext::makeDataPtr(data, deleter,
 #if ${isCUDA}
       Device(kCUDA, getPointerDevice(data))
@@ -47,96 +46,18 @@ ${Storage}::${Storage}(Context* context,
 #endif
        ), size,
      /* allocator */ nullptr
-    )),
-    context(context) {
+    )), context(context) {
     ${THStorage}_clearFlag(${state,} storage, TH_STORAGE_RESIZABLE);
 }
 
-${Storage}::~${Storage}() {
-  ${THStorage}_free(${state,} storage);
-}
+${Storage}::~${Storage}() { }
 
 size_t ${Storage}::elementSize() const {
   return sizeof(${ScalarType});
 }
 
-size_t ${Storage}::size() const {
-  return storage->size;
-}
-
-void* ${Storage}::data() {
-  return storage->data_ptr.get();
-}
-
-const void* ${Storage}::data() const {
-  return storage->data_ptr.get();
-}
-
-auto ${Storage}::retain() -> ${Storage}& {
-  ${THStorage}_retain(${state,} storage);
-  return *this;
-}
-
-auto ${Storage}::free() -> ${Storage}& {
-  ${THStorage}_free(${state,} storage);
-  return *this;
-}
-
-void* ${Storage}::unsafeGetTH(bool retain) const {
-  if (retain) {
-    ${THStorage}_retain(${state,} storage);
-  }
-  return storage;
-}
-
-auto ${Storage}::resize(int64_t new_size) -> ${Storage}& {
-  ${THStorage}_resize(${state,} storage, new_size);
-  return *this;
-}
-
-auto ${Storage}::fill(Scalar value) -> ${Storage}& {
-  ${THStorage}_fill(${state,} storage, ${to_th_type}(value.to${ScalarName}()));
-  return *this;
-}
-
-auto ${Storage}::set(size_t ind, Scalar value) -> ${Storage}& {
-  ${THStorage}_set(${state,} storage, ind, ${to_th_type}(value.to${ScalarName}()));
-  return *this;
-}
-
-auto ${Storage}::fast_set(size_t ind, Scalar value) -> ${Storage}& {
-  throw std::runtime_error("unsupported operation 'fast_set'");
-}
-
-auto ${Storage}::get(size_t ind) -> Scalar {
-  // static cast to fix  long -> int64_t issues
-  return static_cast<${ScalarType}>(${to_at_type}(${THStorage}_get(${state,} storage, ind)));
-}
-
-auto ${Storage}::fast_get(size_t ind) -> Scalar {
-  if(${isCUDA})
-    throw std::runtime_error("unsupported operation 'fast_get'");
-  return static_cast<${ScalarType}>(${to_at_type}(storage->unsafe_data<${THScalarType}>()[ind]));
-}
-
-void ${Storage}::set_flag(char flag) {
-  ${THStorage}_setFlag(${state,} storage, flag);
-}
-
-void ${Storage}::clear_flag(char flag) {
-  ${THStorage}_clearFlag(${state,} storage, flag);
-}
-
-int ${Storage}::getDevice() const {
-  return storage->data_ptr.device().index();
-}
-
 Type& ${Storage}::type() const {
   return context->getType(Backend::${Backend},ScalarType::${ScalarName});
-}
-
-const char * ${Storage}::toString() const {
-  return "${Storage}";
 }
 
 const char * ${Storage}::typeString() {
