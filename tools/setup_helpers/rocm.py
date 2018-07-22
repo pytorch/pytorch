bd847cf2ed39d439dd1fd2f48d65d276dfe741dc
Jorghi12 <jorgsharp@gmail.com>
2018-07-22 18:52:06 -0400

    Update rocm.py

--- tools/setup_helpers/rocm.py
+++ tools/setup_helpers/rocm.py
@@ -3,6 +3,7 @@ from .env import check_env_flag, check_negative_env_flag
 
 # Get ROCm Home Path
 ROCM_HOME = check_env_flag("ROCM_HOME", "/opt/rocm")
+ROCM_VERSION = ""
 
 # Check if ROCm disabled.
 if check_negative_env_flag("USE_ROCM"):
