f5da30dd3eb391b846eabcaf89f98dd9bd0e2660
Jorghi12 <jorgsharp@gmail.com>
2018-07-22 19:40:08 -0400

    Transpiling to a separate directory.

--- tools/amd_build/build_pytorch_amd.py
+++ tools/amd_build/build_pytorch_amd.py
@@ -1,4 +1,3 @@
-"""Requires the hipify-python.py script (https://github.com/ROCm-Developer-Tools/pyHIPIFY)."""
 import shutil
 import subprocess
 import os
@@ -8,6 +7,11 @@ from functools import reduce
 
 amd_build_dir = os.path.dirname(os.path.realpath(__file__))
 proj_dir = os.path.dirname(os.path.dirname(amd_build_dir))
+out_dir = os.path.join(os.path.dirname(proj_dir), "pytorch_amd")
+
+# Create the pytorch_amd directory
+shutil.copytree(proj_dir, out_dir)
+
 includes = [
     "aten/*",
     "torch/*"
@@ -16,13 +20,13 @@ includes = [
 # List of operators currently disabled
 yaml_file = os.path.join(amd_build_dir, "disabled_features.yaml")
 
-# Apply patch files.
+# Apply patch files on output directory.
 patch_folder = os.path.join(amd_build_dir, "patches")
 for filename in os.listdir(os.path.join(amd_build_dir, "patches")):
-    subprocess.Popen(["git", "apply", os.path.join(patch_folder, filename)], cwd=proj_dir)
+    subprocess.Popen(["git", "apply", os.path.join(patch_folder, filename)], cwd=out_dir)
 
 # HIPCC Compiler doesn't provide host defines - Automatically include them.
-for root, _, files in os.walk(os.path.join(proj_dir, "aten/src/ATen")):
+for root, _, files in os.walk(os.path.join(out_dir, "aten/src/ATen")):
     for filename in files:
         if filename.endswith(".cu") or filename.endswith(".cuh"):
             filepath = os.path.join(root, filename)
@@ -42,7 +46,7 @@ for root, _, files in os.walk(os.path.join(proj_dir, "aten/src/ATen")):
 # Make various replacements inside AMD_BUILD/torch directory
 ignore_files = ["csrc/autograd/profiler.h", "csrc/autograd/profiler.cpp",
                 "csrc/cuda/cuda_check.h", "csrc/jit/fusion_compiler.cpp"]
-for root, _directories, files in os.walk(os.path.join(proj_dir, "torch")):
+for root, _directories, files in os.walk(os.path.join(out_dir, "torch")):
     for filename in files:
         if filename.endswith(".cpp") or filename.endswith(".h"):
             source = os.path.join(root, filename)
@@ -62,7 +66,7 @@ for root, _directories, files in os.walk(os.path.join(proj_dir, "torch")):
 
 # Execute the Hipify Script.
 args = (["--project-directory", proj_dir] +
-        ["--output-directory", proj_dir] +
+        ["--output-directory", out_dir] +
         ["--includes"] + includes +
         ["--yaml-settings", yaml_file] +
         ["--add-static-casts", "True"] +
