5e3b8f47f8e7d021dc746fd593ce0912c2895f7a
Zeming Lin <ebetica0@gmail.com>
2018-07-23 13:46:33 -0700

    public optimizer options

--- torch/csrc/api/src/optim/adagrad.cpp
+++ torch/csrc/api/src/optim/adagrad.cpp
@@ -12,10 +12,6 @@ namespace optim {
 AdagradOptions::AdagradOptions(double learning_rate)
     : learning_rate_(learning_rate) {}
 
-const AdagradOptions& Adagrad::options() const noexcept {
-  return options_;
-}
-
 /// Adapted from
 /// https://github.com/pytorch/pytorch/blob/master/torch/optim/adagrad.py
 void Adagrad::step() {
@@ -26,12 +22,12 @@ void Adagrad::step() {
       continue;
 
     auto d_p = Tensor(grad).data();
-    if (options_.weight_decay_ > 0) {
-      d_p.add_(p, options_.weight_decay_);
+    if (options.weight_decay_ > 0) {
+      d_p.add_(p, options.weight_decay_);
     }
     step_.at(i) += 1.0;
-    auto clr = options_.learning_rate_ /
-        (1.0 + (step_.at(i) - 1.0) * options_.lr_decay_);
+    auto clr = options.learning_rate_ /
+        (1.0 + (step_.at(i) - 1.0) * options.lr_decay_);
 
     auto sum = buffer_at(sum_, i);
     sum.data().addcmul_(d_p, d_p, 1.0);
