9cf76cfb4c712813a5a4622add2714eaa1d8c884
Jesse Hellemn <hellemn@fb.com>
2018-07-24 18:31:39 -0700

    Chaning conda build script to use current python version
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/9780
    
    Reviewed By: ml7
    
    Differential Revision: D8983501
    
    Pulled By: pjh5
    
    fbshipit-source-id: 79208796247433cbe271a2d06f66254587d96f80

--- scripts/build_anaconda.sh
+++ scripts/build_anaconda.sh
@@ -216,13 +216,8 @@ else
 fi
 
 # Read the python version
-PYTHON_VERSION="$(python --version 2>&1 | grep --only-matching '[0-9]\.[0-9]\.[0-9]*')"
-if [[ "$PYTHON_VERSION" == 3.6* ]]; then
-  # This is needed or else conda tries to move packages to python3/site-packages
-  # instead of python3.6/site-packages. Specifically 3.6 because that's what
-  # the latest Anaconda version is
-  conda_args+=(" --python 3.6")
-fi
+python_version="$(python --version 2>&1 | grep --only-matching '[0-9]\.[0-9]\.[0-9]*')"
+conda_args+=(" --python ${python_version:0:3}")
 
 
 ###########################################################
