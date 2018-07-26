5f9f2dc2d3b7f4872df73d598138d8ba6b323f1d
Achal <achalshah20@gmail.com>
2018-07-25 17:28:45 -0700

    OpenCV 4.0 Compatibility fix

--- binaries/convert_encoded_to_raw_leveldb.cc
+++ binaries/convert_encoded_to_raw_leveldb.cc
@@ -109,7 +109,7 @@ void ConvertToRawDataset(
     cv::Mat img = cv::imdecode(
         cv::Mat(1, &encoded_size, CV_8UC1,
         const_cast<char*>(encoded_image.data())),
-        caffe2::FLAGS_color ? CV_LOAD_IMAGE_COLOR : CV_LOAD_IMAGE_GRAYSCALE);
+        caffe2::FLAGS_color ? cv::IMREAD_COLOR : cv::IMREAD_GRAYSCALE);
     cv::Mat resized_img;
     int scaled_width, scaled_height;
     if (caffe2::FLAGS_warp) {
