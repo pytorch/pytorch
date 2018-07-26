d5051abcfb4f2eaf6314a4bfe90f61a8430486ca
Edward Yang <ezyang@fb.com>
2018-07-25 19:00:14 -0700

    Reimplement localScalar as a native function. (#9762)
    Summary:
    I split it into two parts, _local_scalar and _local_scalar_dense (unchecked)
    so I could reuse the sparse logic in both paths.
    
    _local_scalar became a method on Tensor to work around a circular
    include problem.
    
    This is resurrected copy of #9652
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9762
    
    Differential Revision: D8972348
    
    Pulled By: ezyang
    
    fbshipit-source-id: 2232dbfc8e1286b8a4a1c67d285c13a7771aad4c

--- aten/src/ATen/templates/TensorMethods.h
+++ aten/src/ATen/templates/TensorMethods.h
@@ -54,7 +54,7 @@ AT_FORALL_SCALAR_TYPES(DEFINE_CAST)
 #undef DEFINE_CAST
 
 #define DEFINE_TO_C_TYPE(T,name,_) \
-inline T Tensor::toC##name () const { return pImpl->localScalar().to##name (); }
+inline T Tensor::toC##name () const { return _local_scalar().to##name (); }
 
 AT_FORALL_SCALAR_TYPES(DEFINE_TO_C_TYPE)
 #undef DEFINE_TO_C_TYPE
