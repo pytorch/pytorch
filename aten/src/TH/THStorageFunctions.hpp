a20b4999a54f20be6edc9439e064dce69fdbafe0
Christian Puhrsch <cpuhrsch@fb.com>
2018-07-27 14:28:14 -0700

    Use Retainable as base class for StorageImpl

--- aten/src/TH/THStorageFunctions.hpp
+++ aten/src/TH/THStorageFunctions.hpp
@@ -35,8 +35,6 @@
 
 TH_API ptrdiff_t THStorage_size(const THStorage *self);
 
-TH_API void THStorage_setFlag(THStorage *storage, const char flag);
-TH_API void THStorage_clearFlag(THStorage *storage, const char flag);
 TH_API void THStorage_retain(THStorage *storage);
 TH_API void THStorage_resize(THStorage *storage, ptrdiff_t size);
 TH_API void THStorage_swap(THStorage *storage1, THStorage *storage2);
