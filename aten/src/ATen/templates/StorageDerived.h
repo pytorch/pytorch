7c9119478d4d30840cbef9c70dba57398214dcdf
Christian Puhrsch <cpuhrsch@fb.com>
2018-07-27 13:28:26 -0700

    Remove generated Storage files

--- aten/src/ATen/templates/StorageDerived.h
+++ aten/src/ATen/templates/StorageDerived.h
@@ -13,19 +13,17 @@ namespace at {
 
 struct Allocator;
 
-struct ${Storage} final : public Storage {
-  ${Storage}();
-  ${Storage}(StorageImpl* storage_impl) : Storage(storage_impl){};
-  ${Storage}(size_t size);
-  ${Storage}(size_t size, Allocator* allocator);
-  ${Storage}(
-      void* data,
-      size_t size,
-      const std::function<void(void*)>& deleter);
-  StorageImpl* storage_impl_;
-
- protected:
-  friend struct ${Type};
-};
+//struct ${Storage} final : public Storage {
+//  ${Storage}(StorageImpl* storage_impl) : Storage(storage_impl){};
+//  ${Storage}(size_t size, Allocator* allocator);
+//  ${Storage}(
+//      at::DataPtr,
+//      size_t size,
+//      const std::function<void(void*)>& deleter);
+//  StorageImpl* storage_impl_;
+//
+// protected:
+//  friend struct ${Type};
+//};
 
 } // namespace at
