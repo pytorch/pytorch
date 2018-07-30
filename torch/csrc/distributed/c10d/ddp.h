a726cb5b31cd23055cd46d50e6d9eea75b7fda21
Peter Goldsborough <psag@fb.com>
2018-07-30 10:57:03 -0700

    Add comments

--- torch/csrc/distributed/c10d/ddp.h
+++ torch/csrc/distributed/c10d/ddp.h
@@ -24,13 +24,19 @@ inline void distBroadcastCoalesced(
   flatTensors.reserve(tensorGroups.size());
   work.reserve(tensorGroups.size());
   for (const auto& group : tensorGroups) {
+    // Flatten each group of tensors (whose size equals `bufferSize`) into a
+    // single tensor.
     flatTensors.push_back({torch::utils::flatten_dense_tensors(group.tensors)});
     BroadcastOptions broadcastOptions;
     broadcastOptions.rootRank = 0;
     broadcastOptions.rootTensor = 0;
+    // Enqueue a work item and collect the `Work` (essntially a "future") so we
+    // can `wait()` for its completion after we have collected all `Work` items.
     work.push_back(
         processGroup.broadcast(flatTensors.back(), broadcastOptions));
   }
+  // Now loop through each group, wait for the broadcast to complete, and
+  // un-flatten the broadcast tensor back into device-local individual tensors.
   for (size_t group = 0; group < tensorGroups.size(); ++group) {
     auto& tensors = tensorGroups[group].tensors;
     work[group]->wait();
@@ -38,6 +44,7 @@ inline void distBroadcastCoalesced(
         torch::utils::unflatten_dense_tensors(flatTensors[group][0], tensors);
     AT_ASSERT(synced.size() == tensors.size());
     for (size_t i = 0; i < synced.size(); ++i) {
+      // Copy into the per-process tensors.
       tensors[i].copy_(synced[i]);
     }
   }
