4fc3a6daa046613e4c4aaf3a48f21d42f2b5c7f7
Edward Z. Yang <ezyang@fb.com>
2018-07-30 07:30:12 -0700

    Don't toggle on CAFFE2_BUILD_SHARED_LIBS
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/ATenGeneral.h
+++ aten/src/ATen/ATenGeneral.h
@@ -2,11 +2,7 @@
 
 #ifdef _WIN32
 # if defined(ATen_cpu_EXPORTS) || CAFFE2_BUILD_MAIN_LIB
-#  if defined(CAFFE2_BUILD_SHARED_LIBS)
-#   define AT_API __declspec(dllexport)
-#  else
-#   define AT_API
-#  endif
+#  define AT_API __declspec(dllexport)
 # else
 #  define AT_API __declspec(dllimport)
 # endif
