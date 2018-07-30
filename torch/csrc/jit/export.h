d74d434675bb0b2f2716ccaf7e756539abc6ba31
Roy Li <royboy@fb.com>
2018-07-30 13:57:00 -0700

    fix CI issues

--- torch/csrc/jit/export.h
+++ torch/csrc/jit/export.h
@@ -33,7 +33,7 @@ TORCH_API std::string PrettyPrintExportedGraph(
     ::torch::onnx::OperatorExportTypes operator_export_type
       = ::torch::onnx::OperatorExportTypes::ONNX);
 
-std::tuple<std::string, RawDataExportMap> ExportModule(
+TORCH_API std::tuple<std::string, RawDataExportMap> ExportModule(
     const std::shared_ptr<script::Module>& module,
     int64_t onnx_opset_version,
     ::torch::onnx::OperatorExportTypes operator_export_type
