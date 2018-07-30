a2fd0dd39847c7d4e7749f44c6920f7c1c7205d8
Edward Z. Yang <ezyang@fb.com>
2018-07-30 07:11:15 -0700

    Windows ATen Caffe2 try harder.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/ATenGeneral.h
+++ aten/src/ATen/ATenGeneral.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #ifdef _WIN32
-# if defined(ATen_cpu_EXPORTS) || (defined(CAFFE2_BUILD_SHARED_LIBS) && CAFFE2_BUILD_MAIN_LIB)
-#  define AT_API __declspec(dllexport)
+# if defined(ATen_cpu_EXPORTS) || CAFFE2_BUILD_MAIN_LIB
+#  if defined(CAFFE2_BUILD_SHARED_LIBS)
+#   define AT_API __declspec(dllexport)
+#  else
+#   define AT_API
+#  endif
 # else
 #  define AT_API __declspec(dllimport)
 # endif
