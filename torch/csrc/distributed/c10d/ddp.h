cff1d2daaf6410ed3725768dc911f5f8d69e1940
Peter Goldsborough <psag@fb.com>
2018-07-30 10:48:02 -0700

    Collect all futures before waiting on individual work

--- torch/csrc/distributed/c10d/ddp.h
+++ torch/csrc/distributed/c10d/ddp.h
@@ -7,6 +7,7 @@
 #include <ATen/ATen.h>
 
 #include <cstddef>
+#include <memory>
 #include <vector>
 
 namespace c10d {
@@ -14,18 +15,30 @@ inline void distBroadcastCoalesced(
     std::vector<at::Tensor>& tensors,
     int64_t bufferSize,
     ProcessGroup& processGroup) {
-  for (auto& group : torch::utils::take_tensors(tensors, bufferSize)) {
-    std::vector<at::Tensor> flatTensor = {
-        torch::utils::flatten_dense_tensors(group.tensors)};
+  auto tensorGroups = torch::utils::take_tensors(tensors, bufferSize);
+  // We store single-element vectors in `flatTensors` because
+  // `ProcessGroup::broadcast` takes a reference to a vector, which must be
+  // alive until the `wait()` call on the returned `Work` completes.
+  std::vector<std::vector<at::Tensor>> flatTensors;
+  std::vector<std::shared_ptr<ProcessGroup::Work>> work;
+  flatTensors.reserve(tensorGroups.size());
+  work.reserve(tensorGroups.size());
+  for (const auto& group : tensorGroups) {
+    flatTensors.push_back({torch::utils::flatten_dense_tensors(group.tensors)});
     BroadcastOptions broadcastOptions;
     broadcastOptions.rootRank = 0;
     broadcastOptions.rootTensor = 0;
-    processGroup.broadcast(flatTensor, broadcastOptions)->wait();
-    auto synced =
-        torch::utils::unflatten_dense_tensors(flatTensor[0], group.tensors);
-    AT_ASSERT(synced.size() == group.tensors.size());
+    work.push_back(
+        processGroup.broadcast(flatTensors.back(), broadcastOptions));
+  }
+  for (size_t group = 0; group < tensorGroups.size(); ++group) {
+    auto& tensors = tensorGroups[group].tensors;
+    work[group]->wait();
+    const auto synced =
+        torch::utils::unflatten_dense_tensors(flatTensors[group][0], tensors);
+    AT_ASSERT(synced.size() == tensors.size());
     for (size_t i = 0; i < synced.size(); ++i) {
-      group.tensors[i].copy_(synced[i]);
+      tensors[i].copy_(synced[i]);
     }
   }
 }
