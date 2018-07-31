7ec90da70521c241543bc20410561472c197e31b
Edward Z. Yang <ezyang@fb.com>
2018-07-31 13:45:59 -0700

    Move ArrayRef, Backtrace, Error, SmallVector, optional to ATen/core; add CoreAPI
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ aten/src/ATen/core/CoreAPI.h
@@ -0,0 +1,9 @@
+#ifdef _WIN32
+# if defined(ATen_cpu_EXPORTS) || defined(caffe2_EXPORTS)
+#  define AT_CORE_API __declspec(dllexport)
+# else
+#  define AT_CORE_API __declspec(dllimport)
+# endif
+#else
+# define AT_CORE_API
+#endif
