f88d096a48df97e573c5983500153c5d935255c6
Bram Wasti <bwasti@fb.com>
2018-07-31 20:43:04 -0700

    Enable Optimization on mobile by default
    Summary: Re-enable opt by default
    
    Reviewed By: Maratyszcza
    
    Differential Revision: D8525434
    
    fbshipit-source-id: a61253907251a44cfc59e0b50fb1906c5eb20558

--- caffe2/predictor/predictor.h
+++ caffe2/predictor/predictor.h
@@ -28,7 +28,7 @@ class Predictor {
       const NetDef& run_net,
       Workspace* parent = nullptr,
       bool run_init = true,
-      int optimization = 0);
+      int optimization = 1);
 
   ~Predictor() {}
 
