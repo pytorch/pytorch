036c292bcf9b508b0acf80ea9e471f386b2f5b78
Thomas Viehmann <tv.code@beamnet.de>
2018-08-01 23:19:14 +0200

    fix example to just use tensors now that we can

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -1152,8 +1152,8 @@ class CTCLoss(_Loss):
         >>> ctc_loss = nn.CTCLoss()
         >>> log_probs = torch.randn(50, 16, 20).log_softmax(2).detach().requires_grad_()
         >>> targets = torch.randint(1, 21, (16, 30), dtype=torch.long)
-        >>> input_lengths = torch.full((16,), 50, dtype=torch.long).toarray()
-        >>> target_lengths = torch.randint(10,30,(16,), dtype=torch.long).toarray()
+        >>> input_lengths = torch.full((16,), 50, dtype=torch.long)
+        >>> target_lengths = torch.randint(10,30,(16,), dtype=torch.long)
         >>> loss = ctc_loss(log_probs, targets, input_lengths, target_lengths)
         >>> loss.backward()
 
