29148d8669673e855649e70f6dfcbcc1f2258972
Lin Li <yyazll@fb.com>
2018-08-01 10:24:19 -0700

    Improve lars operator by applying clipping (#9905)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9905
    
    This diff improves lars operator in Caffe2 by applying clipping to the computed learning rate
    
    Differential Revision: D9020606
    
    fbshipit-source-id: c15a9902086e8845e6490e15e839685c51a3a3dd

--- caffe2/python/operator_test/lars_test.py
+++ caffe2/python/operator_test/lars_test.py
@@ -12,24 +12,34 @@ import numpy as np
 
 class TestLars(hu.HypothesisTestCase):
 
-    @given(offset=st.floats(min_value=0, max_value=100), **hu.gcs)
-    def test_lars(self, offset, dc, gc):
+    @given(offset=st.floats(min_value=0, max_value=100),
+    lr_min=st.floats(min_value=1e-8, max_value=1e-6),
+    **hu.gcs)
+    def test_lars(self, offset, lr_min, dc, gc):
         X = np.random.rand(6, 7, 8, 9).astype(np.float32)
         dX = np.random.rand(6, 7, 8, 9).astype(np.float32)
+        wd = np.array([1e-4]).astype(np.float32)
+        trust = np.random.rand(1).astype(np.float32)
+        lr_max = np.random.rand(1).astype(np.float32)
 
-        def ref_lars(X, dX):
-            return [1. / (np.linalg.norm(dX) / np.linalg.norm(X) + offset)]
+        def ref_lars(X, dX, wd, trust, lr_max):
+            rescale_factor = \
+                trust / (np.linalg.norm(dX) / np.linalg.norm(X) + wd + offset)
+            rescale_factor = np.minimum(rescale_factor, lr_max)
+            rescale_factor = np.maximum(rescale_factor, lr_min)
+            return [rescale_factor]
 
         op = core.CreateOperator(
             "Lars",
-            ["X", "dX"],
+            ["X", "dX", "wd", "trust", "lr_max"],
             ["rescale_factor"],
-            offset=offset
+            offset=offset,
+            lr_min=lr_min,
         )
 
         self.assertReferenceChecks(
             device_option=gc,
             op=op,
-            inputs=[X, dX],
+            inputs=[X, dX, wd, trust, lr_max],
             reference=ref_lars
         )
