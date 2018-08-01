57670207c163090e20149f477d609fd26ff3ce36
Pushkar Tripathi <pushkart@fb.com>
2018-08-01 10:53:30 -0700

    Allow mobile exporter to export string arrays (#10017)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10017
    
    Allow mobile exporter to export string arrays
    
    Differential Revision: D9061213
    
    fbshipit-source-id: 4e32eebe034b72e55eb0f8a364f0c21953c74e3e

--- caffe2/python/predictor/mobile_exporter_test.py
+++ caffe2/python/predictor/mobile_exporter_test.py
@@ -73,11 +73,15 @@ class TestMobileExporter(TestCase):
         model = ModelHelper(name="mobile_exporter_test_model")
         model.Copy("data_int", "out")
         model.params.append("data_int")
+        model.Copy("data_obj", "out_obj")
+        model.params.append("data_obj")
 
         # Create our mobile exportable networks
         workspace.RunNetOnce(model.param_init_net)
         np_data_int = np.random.randint(100, size=(1, 1, 28, 28), dtype=np.int32)
         workspace.FeedBlob("data_int", np_data_int)
+        np_data_obj = np.array(['aa', 'bb']).astype(np.dtype('O'))
+        workspace.FeedBlob("data_obj", np_data_obj)
 
         init_net, predict_net = mobile_exporter.Export(
             workspace, model.net, model.params
@@ -86,6 +90,7 @@ class TestMobileExporter(TestCase):
         workspace.CreateNet(model.net)
         workspace.RunNet(model.net)
         ref_out = workspace.FetchBlob("out")
+        ref_out_obj = workspace.FetchBlob("out_obj")
 
         # Clear the workspace
         workspace.ResetWorkspace()
@@ -97,9 +102,11 @@ class TestMobileExporter(TestCase):
         workspace.CreateNet(predict_net, True)
         workspace.RunNet(predict_net.name)
         manual_run_out = workspace.FetchBlob("out")
+        manual_run_out_obj = workspace.FetchBlob("out_obj")
         np.testing.assert_allclose(
             ref_out, manual_run_out, atol=1e-10, rtol=1e-10
         )
+        np.testing.assert_equal(ref_out_obj, manual_run_out_obj)
 
         # Clear the workspace
         workspace.ResetWorkspace()
@@ -109,11 +116,17 @@ class TestMobileExporter(TestCase):
             init_net.SerializeToString(), predict_net.SerializeToString()
         )
 
-        # Output is a vector of outputs but we only care about the first and only result
+        # Output is a vector of outputs.
         predictor_out = predictor.run([])
-        assert len(predictor_out) == 1
-        predictor_out = predictor_out[0]
-
+        assert len(predictor_out) == 2
+        predictor_out_int = predictor_out[1]
+        predictor_out_obj = predictor_out[0]
+        # The order in predictor_out is non-deterministic. Use type of the entry
+        # to figure out what to compare it to.
+        if isinstance(predictor_out[1][0], bytes):
+            predictor_out_int = predictor_out[0]
+            predictor_out_obj = predictor_out[1]
         np.testing.assert_allclose(
-            ref_out, predictor_out, atol=1e-10, rtol=1e-10
+            ref_out, predictor_out_int, atol=1e-10, rtol=1e-10
         )
+        np.testing.assert_equal(ref_out_obj, predictor_out_obj)
