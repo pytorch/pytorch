49e8cd34cdc9efe370e797b70707c0c0578170fa
Sergei Nikolaev <drnikolaev@users.noreply.github.com>
2018-08-02 11:18:28 -0700

    Update hypothesis_test.py
    More accurate deadline

--- caffe2/python/hypothesis_test.py
+++ caffe2/python/hypothesis_test.py
@@ -98,7 +98,7 @@ def _test_binary_broadcast(name, ref, filter_=None,
             elements=hu.elements_of_type(dtype, filter_=filter_))),
         in_place=(st.booleans() if allow_inplace else st.just(False)),
         **gcs)
-    @settings(max_examples=3, deadline=500)
+    @settings(max_examples=3, deadline=1000)
     def test_binary_broadcast(self, inputs, in_place, gc, dc):
         op = core.CreateOperator(
             name, ["X1", "X2"], ["X1" if in_place else "Y"], broadcast=1)
