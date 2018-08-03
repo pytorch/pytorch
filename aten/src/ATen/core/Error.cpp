2c7db4b95a688a4d947c4cd2b21dbeea84c9818e
Edward Yang <ezyang@fb.com>
2018-08-03 07:04:33 -0700

    Make at::Error look more like caffe2::EnforceNotMet
    Summary:
    - New concept of a message stack; you can add messages
      using AppendMessage
    - New concept of a caller; it's just a way to pass along
      some arbitrary extra information in the exception
    
    Coming soon is changing Caffe2 to use at::Error instead of
    EnforceNotMet
    
    Differential Revision: D9139996
    
    fbshipit-source-id: 87984f25c4f698567e45a752fc7d2f4f6998fc08

--- aten/src/ATen/core/Error.cpp
+++ aten/src/ATen/core/Error.cpp
@@ -3,21 +3,64 @@
 
 #include <iostream>
 #include <string>
+#include <numeric>
 
 namespace at {
+
+namespace detail {
+
+std::string StripBasename(const std::string &full_path) {
+  const char kSeparator = '/';
+  size_t pos = full_path.rfind(kSeparator);
+  if (pos != std::string::npos) {
+    return full_path.substr(pos + 1, std::string::npos);
+  } else {
+    return full_path;
+  }
+}
+
+} // namespace detail
+
 std::ostream& operator<<(std::ostream& out, const SourceLocation& loc) {
   out << loc.function << " at " << loc.file << ":" << loc.line;
   return out;
 }
 
-Error::Error(SourceLocation source_location, std::string err)
-    : what_without_backtrace_(err),
-      what_(
-          str(err,
-              " (",
-              source_location,
-              ")\n",
-              get_backtrace(/*frames_to_skip=*/2))) {}
+Error::Error(const std::string& new_msg, const std::string& backtrace, const void* caller)
+    : msg_stack_{new_msg}
+    , backtrace_(backtrace)
+    , caller_(caller) {
+  msg_ = msg();
+  msg_without_backtrace_ = msg_without_backtrace();
+}
+
+// PyTorch-style error message
+Error::Error(SourceLocation source_location, const std::string& msg)
+  : Error(msg, str(" (", source_location, ")\n", get_backtrace(/*frames_to_skip=*/2))) {}
+
+// Caffe2-style error message
+Error::Error(const char* file, const int line, const char* condition, const std::string& msg, const void* caller)
+  : Error(str("[enforce fail at ", detail::StripBasename(file), ":", line, "] ", condition, ". ", msg),
+          str("\n", get_backtrace(/*frames_to_skip=*/2)),
+          caller) {}
+
+std::string Error::msg() const {
+  return std::accumulate(msg_stack_.begin(), msg_stack_.end(), std::string("")) + backtrace_;
+}
+
+std::string Error::msg_without_backtrace() const {
+  return std::accumulate(msg_stack_.begin(), msg_stack_.end(), std::string(""));
+}
+
+void Error::AppendMessage(const std::string& new_msg) {
+  msg_stack_.push_back(new_msg);
+  // Refresh the cache
+  // TODO: Calling AppendMessage O(n) times has O(n^2) cost.  We can fix
+  // this perf problem by populating the fields lazily... if this ever
+  // actually is a problem.
+  msg_ = msg();
+  msg_without_backtrace_ = msg_without_backtrace();
+}
 
 void Warning::warn(SourceLocation source_location, std::string msg) {
   warning_handler_(source_location, msg.c_str());
