016293d5b486cdb833fccdcaee1cd244bf92f2db
wuhuikx <hui.h.wu@intel.com>
2018-08-06 16:44:21 -0700

    Add new mkldnn fallback operators (#10162)
    Summary:
    Add new ideep fallback operators.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10162
    
    Reviewed By: yinghai
    
    Differential Revision: D9169049
    
    Pulled By: wesolwsk
    
    fbshipit-source-id: 3bc30250d7340fea2c442f36d16b85241ceee6e7

--- caffe2/operators/distance_op.cc
+++ caffe2/operators/distance_op.cc
@@ -1,5 +1,9 @@
 #include "caffe2/operators/distance_op.h"
 #include "caffe2/utils/eigen_utils.h"
+#ifdef CAFFE2_USE_IDEEP
+#include <caffe2/ideep/operators/operator_fallback_ideep.h>
+#include <caffe2/ideep/utils/ideep_operator.h>
+#endif
 
 namespace caffe2 {
 
@@ -396,6 +400,11 @@ REGISTER_CPU_OPERATOR(L1Distance, L1DistanceOp<float, CPUContext>);
 REGISTER_CPU_OPERATOR(
     L1DistanceGradient,
     L1DistanceGradientOp<float, CPUContext>);
+#ifdef CAFFE2_USE_IDEEP
+REGISTER_IDEEP_OPERATOR(
+    L1DistanceGradient,
+    IDEEPFallbackOp<L1DistanceGradientOp<float, CPUContext>>);
+#endif
 
 OPERATOR_SCHEMA(L1Distance)
     .NumInputs(2)
