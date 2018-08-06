6f4d2aeffd45f71bf75f1d48980d079d024923eb
Christian Puhrsch <cpuhrsch@fb.com>
2018-08-06 14:40:51 -0700

    Add test

--- test/test_torch.py
+++ test/test_torch.py
@@ -358,6 +358,19 @@ class TestTorch(TestCase):
         check_non_contiguous((5, 7), torch.float)
         check_non_contiguous((1024,), torch.float)
 
+        def check_non_contiguous_expand(shape, dtype):
+            contig = torch.randn(shape, dtype=dtype)
+            non_contig = contig.clone().expand(3, -1, -1)
+            self.assertFalse(non_contig.is_contiguous())
+            contig = torchfn(contig)
+            non_contig = torchfn(non_contig)
+            for i in range(3):
+                self.assertEqual(contig, non_contig[i], 'non-contiguous expand[' + str(i) + ']')
+
+        # The size 1 case is special as it leads to 0 stride and needs to persists
+        check_non_contiguous_expand((1, 7), torch.double)
+        check_non_contiguous_expand((5, 7), torch.float)
+
         # If size(dim) == 1, stride(dim) is not defined.
         # The code needs to be able to handle this
         def check_contiguous_size1(dtype):
