cf1c63cb2bafe30710ebcdcda60ce86ed43169a5
Peter Goldsborough <psag@fb.com>
2018-08-06 18:39:51 -0700

    Automatically detect CUDA

--- tools/build_libtorch.py
+++ tools/build_libtorch.py
@@ -4,6 +4,8 @@ import shlex
 import subprocess
 import sys
 
+from setup_helpers.cuda import USE_CUDA
+
 # Placeholder for future interface. For now just gives a nice -h.
 parser = argparse.ArgumentParser(description='Build libtorch')
 parser.parse_args()
@@ -13,7 +15,10 @@ os.environ['ONNX_NAMESPACE'] = 'onnx_torch'
 
 tools_path = os.path.dirname(os.path.abspath(__file__))
 build_pytorch_libs = os.path.join(tools_path, 'build_pytorch_libs.sh')
-command = '{} --use-nnpack caffe2'.format(build_pytorch_libs)
+command = '{} --use-nnpack '.format(build_pytorch_libs)
+if USE_CUDA:
+    command += '--use-cuda '
+command += 'caffe2'
 
 sys.stdout.flush()
 sys.stderr.flush()
