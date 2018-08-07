c4360e5590e143065825514edab36e74f7e7dbb6
mruberry <mruberry@nvidia.com>
2018-08-07 12:18:15 -0700

    Extends type and shape tracing with device (#9796)
    Summary:
    This PR extends the existing type and shape metadata tracing and verification done in autograd with device information. This expansion of tracing is required for #8354, is likely useful in other scenarios, and is a healthy sanity check, just like type and shape tracing.
    
    The precise changes are:
    
    - TypeAndShape -> InputMetadata, now includes device()
    - Creating InputMetadata is simplified to just require a tensor, and callers were updated to use this simpler invocation wherever possible
    - The gradient accumulator of a variable is now reset when set_data() is called if either the type or device changes, and this reset now locks to avoid contention with acquiring the gradient accumulator
    - Mismatched devices during backward() will throw a runtime error, just like mismatched type and shape
    - (Bonus!) Two uninitialized pointers in THCReduce are now initialized (to nullptr) to prevent build warnings
    
    fyi colesbury
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9796
    
    Reviewed By: goldsborough
    
    Differential Revision: D9119325
    
    Pulled By: ezyang
    
    fbshipit-source-id: 76d1861b8d4f74db0575ff1f3bd965e18f9463de

--- torch/csrc/autograd/python_function.cpp
+++ torch/csrc/autograd/python_function.cpp
@@ -433,7 +433,7 @@ static void _wrap_outputs(THPFunction *self,
     // to set_history wins.
     auto var = as_variable(obj, i);
     if (cdata) {
-      auto output_nr = cdata->add_input_metadata(var.type(), var.sizes());
+      auto output_nr = cdata->add_input_metadata(var);
       AT_ASSERT(i == (int)output_nr);
     }
     set_history(var, i, is_input, is_modified, is_differentiable);
