f5cfbdd2993325a76afacd923a672c5fcbccafcd
Sam Gross <sgross@fb.com>
2018-08-10 10:21:54 -0700

    Don't call into Python during Storage destruction.
    This removes PyObjectFinalizer. We were seeing SIGSEGV at exit in some
    programs that use multiprocessing. The backtrace pointed to
    StorageRef.__del__ being called from subtype_dealloc. My guess is that
    the Python interpreter was shutdown before all C++ Storage objects were
    deallocated. Deallocating the C++ Storage called the finalizer which
    called back into Python after it was no longer safe to do so.
    
    This avoids a callback from C++ into Python during Storage finalization.
    Instead, dead Storage objects (expired weak references) are collected
    periodically when shared_cache exceeds a limit. The limit is scaled with
    2x the number of live references, which places an upper bound on the
    amount of extra memory held by dead Storage objects. In practice, this
    should be very small.

--- setup.py
+++ setup.py
@@ -772,7 +772,6 @@ main_sources = [
     "torch/csrc/autograd/python_variable.cpp",
     "torch/csrc/autograd/python_variable_indexing.cpp",
     "torch/csrc/byte_order.cpp",
-    "torch/csrc/finalizer.cpp",
     "torch/csrc/jit/batched/BatchTensor.cpp",
     "torch/csrc/jit/init.cpp",
     "torch/csrc/jit/ivalue.cpp",
