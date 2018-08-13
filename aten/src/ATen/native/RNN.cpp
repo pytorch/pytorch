492eecdec4630b82836961ac65187bfa67f1bab9
Adam Paszke <adam.paszke@gmail.com>
2018-08-13 13:52:35 -0700

    Old clang bug strike again

--- aten/src/ATen/native/RNN.cpp
+++ aten/src/ATen/native/RNN.cpp
@@ -496,7 +496,10 @@ std::tuple<Tensor, Tensor> unpack_hidden(const tpair_of<Tensor>& hidden) {
 }
 
 template<typename hidden_type>
-hidden_type pack_hidden(const Tensor& hx, const Tensor& cx) = delete;
+hidden_type pack_hidden(const Tensor& hx, const Tensor& cx) {
+  static_assert(std::is_same<hidden_type, void>::value, "pack_hidden not implemented for this type");
+  AT_ERROR("NOT IMPLEMENTED");
+}
 
 template<>
 Tensor pack_hidden<Tensor>(const Tensor& hx, const Tensor& cx) {
