07864da5366ed1d6efdea504efe2c36140f2e305
Gregory Chanan <gchanan@fb.com>
2018-08-13 13:02:00 -0700

    More merging.

--- aten/src/ATen/UndefinedTensor.cpp
+++ aten/src/ATen/UndefinedTensor.cpp
@@ -6,7 +6,7 @@ namespace at {
 
 // should this use the globalContext?  Can it get a context passed in somehow?
 UndefinedTensor::UndefinedTensor()
-: TensorImpl(UndefinedTensorId(), ScalarType::Undefined) {
+: TensorImpl(UndefinedTensorId(), ScalarType::Undefined,  /* is variable */ false) {
 }
 
 IntList UndefinedTensor::sizes() const {
