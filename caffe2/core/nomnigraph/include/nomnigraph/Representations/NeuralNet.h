582c392a0b4a101763ad9b6ab2a8a1eedcd86cfc
Duc Ngo <duc@fb.com>
2018-08-13 14:59:49 -0700

    Nomnigraph - add diagnostic ability for Subgraph matching API (#10267)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10267
    
    isSubtreeMatch now returns a SubtreeMatchResult which contains a match flag and a debugMessage string that contains the reason why a subtree is not matched (if requested).
    
    Differential Revision: D9182429
    
    fbshipit-source-id: d5d0ef7ba4eae1365d619a7d96e12eab156aae71

--- caffe2/core/nomnigraph/include/nomnigraph/Representations/NeuralNet.h
+++ caffe2/core/nomnigraph/include/nomnigraph/Representations/NeuralNet.h
@@ -19,6 +19,7 @@
 #include "nomnigraph/Support/Pointer.h"
 #include "nomnigraph/Transformations/SubgraphMatcher.h"
 
+#include <sstream>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -421,7 +422,20 @@ void coalesceInsertedDataDependencies(repr::NNModule* m);
 template <NNGraph* G>
 struct NodeHelper {};
 
-using NNNodeMatchCriteria = std::function<bool(NNGraph::NodeRef)>;
+struct NNNodeMatchCriteria {
+  const std::function<bool(NNGraph::NodeRef)> predicate;
+  const std::string debugString;
+
+  NNNodeMatchCriteria(
+      const std::function<bool(NNGraph::NodeRef)>& predicate,
+      const std::string& debugString = "No debug string specified")
+      : predicate(predicate), debugString(debugString){};
+};
+
+std::ostream& operator<<(
+    std::ostream& oss,
+    const NNNodeMatchCriteria& criteria);
+
 using NNSubtree = nom::matcher::SubtreeMatchCriteria<NNNodeMatchCriteria>;
 
 bool hasSingleOutputAndConsumer(NNGraph::NodeRef nodeRef);
@@ -429,8 +443,9 @@ bool hasSingleOutputAndConsumer(NNGraph::NodeRef nodeRef);
 template <typename NodeType>
 NNNodeMatchCriteria matchNodeTypeWithPredicate(
     const std::function<bool(NNGraph::NodeRef, const NodeType&)> predicate,
-    bool expectedSingleOutputAndConsumer = false) {
-  return
+    bool expectedSingleOutputAndConsumer = false,
+    const std::string& debugString = "matchNodeTypeWithPredicate") {
+  return NNNodeMatchCriteria(
       [&predicate, expectedSingleOutputAndConsumer](NNGraph::NodeRef nodeRef) {
         NOM_REQUIRE_OR_RET_FALSE(is<NodeType>(nodeRef));
         if (expectedSingleOutputAndConsumer) {
@@ -438,18 +453,22 @@ NNNodeMatchCriteria matchNodeTypeWithPredicate(
         }
         NodeType* node = get<NodeType>(nodeRef);
         return predicate(nodeRef, *node);
-      };
+      },
+      debugString);
 };
 
 template <typename NodeType>
 NNNodeMatchCriteria matchNodeType(
-    bool expectedSingleOutputAndConsumer = false) {
-  return [expectedSingleOutputAndConsumer](NNGraph::NodeRef nodeRef) {
-    if (expectedSingleOutputAndConsumer) {
-      NOM_REQUIRE_OR_RET_FALSE(hasSingleOutputAndConsumer(nodeRef));
-    }
-    return is<NodeType>(nodeRef);
-  };
+    bool expectedSingleOutputAndConsumer = false,
+    const std::string& debugString = "matchNodeType") {
+  return NNNodeMatchCriteria(
+      [expectedSingleOutputAndConsumer](NNGraph::NodeRef nodeRef) {
+        if (expectedSingleOutputAndConsumer) {
+          NOM_REQUIRE_OR_RET_FALSE(hasSingleOutputAndConsumer(nodeRef));
+        }
+        return is<NodeType>(nodeRef);
+      },
+      debugString);
 }
 
 NNNodeMatchCriteria matchAnyNode();
@@ -458,7 +477,7 @@ struct NNNodeMatch {
   static bool isMatch(
       const NNGraph::NodeRef& node,
       const NNNodeMatchCriteria& criteria) {
-    return criteria(node);
+    return criteria.predicate(node);
   }
 };
 
