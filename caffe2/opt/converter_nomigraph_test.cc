84a7bbaa19cf43330a06b60673cab4a905820175
Bram Wasti <bwasti@fb.com>
2018-08-13 21:04:59 -0700

    Add intrinsic support for external_input/output to nomnigraph (#10100)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10100
    
    nomnigraph has until this point tried to ignore external input and output, as they aren't very well defined (does order matter?).  but for DCE and some of Keren's work they are becoming necessary.  I went ahead and added this to the core nomnigraph converter
    
    Reviewed By: yinghai
    
    Differential Revision: D9105487
    
    fbshipit-source-id: e0759373d737444f01275c4a84c64e9e8b7cdae0

--- caffe2/opt/converter_nomigraph_test.cc
+++ caffe2/opt/converter_nomigraph_test.cc
@@ -48,3 +48,53 @@ TEST(Converter, UnknownType) {
   auto new_netdef = caffe2::convertToCaffe2Proto(nn);
 }
 
+caffe2::NetDef fakeNet() {
+  caffe2::NetDef net;
+
+  {
+    caffe2::OperatorDef* def = net.add_op();
+    def->set_type("Fake");
+    def->add_input("X");
+    def->add_output("Y");
+  }
+  {
+    caffe2::OperatorDef* def = net.add_op();
+    def->set_type("Fake");
+    def->add_input("Y");
+    def->add_output("Z");
+  }
+  {
+    caffe2::OperatorDef* def = net.add_op();
+    def->set_type("Fake");
+    def->add_input("Z");
+    def->add_input("X");
+    def->add_output("W");
+  }
+  net.add_external_input("X");
+  net.add_external_output("Y");
+  net.add_external_output("W");
+
+  return net;
+}
+
+TEST(Converter, ExternalInputs) {
+  auto net = fakeNet();
+
+  auto nn = caffe2::convertToNNModule(net);
+  auto new_netdef = caffe2::convertToCaffe2Proto(nn);
+  EXPECT_EQ(new_netdef.external_input().size(), net.external_input().size());
+  for (auto i = 0; i < net.external_input().size(); ++i) {
+    EXPECT_EQ(new_netdef.external_input(i), net.external_input(i));
+  }
+}
+
+TEST(Converter, ExternalOutputs) {
+  auto net = fakeNet();
+
+  auto nn = caffe2::convertToNNModule(net);
+  auto new_netdef = caffe2::convertToCaffe2Proto(nn);
+  EXPECT_EQ(new_netdef.external_output().size(), net.external_output().size());
+  for (auto i = 0; i < net.external_output().size(); ++i) {
+    EXPECT_EQ(new_netdef.external_output(i), net.external_output(i));
+  }
+}
