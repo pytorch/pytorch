17b63fb82b22efbf4142dc3f89c3c894a96f4690
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2018-08-13 21:33:10 -0700

    Fix byte ordering issue in from_numpy (#9508)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/3671 .
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/9508
    
    Differential Revision: D9307186
    
    Pulled By: soumith
    
    fbshipit-source-id: 39dcaa6fd2d330d7085802acd6f63c19270164fa

--- test/test_torch.py
+++ test/test_torch.py
@@ -7925,7 +7925,7 @@ class TestTorch(TestCase):
 
     @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
     def test_ctor_with_numpy_array(self):
-        dtypes = [
+        correct_dtypes = [
             np.double,
             np.float,
             np.float16,
@@ -7934,7 +7934,11 @@ class TestTorch(TestCase):
             np.int16,
             np.uint8
         ]
-        for dtype in dtypes:
+
+        incorrect_byteorder = '>' if sys.byteorder == 'little' else '<'
+        incorrect_dtypes = map(lambda t: incorrect_byteorder + t, ['d', 'f'])
+
+        for dtype in correct_dtypes:
             array = np.array([1, 2, 3, 4], dtype=dtype)
 
             # Upcast
