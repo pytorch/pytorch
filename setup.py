214627f44ba1a565219bb73da9ce1efadfd145a5
Orion Reblitz-Richardson <orionr@gmail.com>
2018-08-15 14:20:00 -0700

    Remove setup_requires and tests_require from setup.py for FULL_CAFFE2 (#10530)
    Summary:
    In my environment, it looks like setup.py hangs when running
    
    ```
    FULL_CAFFE2=1 python setup.py build_deps
    ```
    
    Removing this fixes things, but we might also want to look at `tests_require`, which came over from `setup_caffe2.py`.
    
    cc pjh5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10530
    
    Differential Revision: D9349597
    
    Pulled By: orionr
    
    fbshipit-source-id: 589145eca507dfaf16386884ee2fbe60299660b4

--- setup.py
+++ setup.py
@@ -1046,9 +1046,6 @@ install_requires = [
     'six',
 ] if FULL_CAFFE2 else []
 
-setup_requires = ['pytest-runner'] if FULL_CAFFE2 else []
-tests_require = ['pytest-cov', 'hypothesis'] if FULL_CAFFE2 else []
-
 entry_points = {}
 if FULL_CAFFE2:
     entry_points = {
@@ -1102,6 +1099,4 @@ if __name__ == '__main__':
             ]
         },
         install_requires=install_requires,
-        setup_requires=setup_requires,
-        tests_require=tests_require,
     )
