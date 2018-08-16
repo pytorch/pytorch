68b0b410785ff3d6c4138e219ed031b26a6683b0
Peter Goldsborough <psag@fb.com>
2018-08-15 19:19:19 -0700

    Remove duplicate _static and _templates folder from docs/cpp

--- docs/cpp/conf.py
+++ docs/cpp/conf.py
@@ -151,17 +151,17 @@ html_theme_options = {
     'logo_only': True,
 }
 
-html_logo = '_static/img/pytorch-logo-dark-unstable.png'
+html_logo = '../source/_static/img/pytorch-logo-dark-unstable.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../source/_static']
 
 # html_style_path = 'css/pytorch_theme.css'
 html_context = {
     'css_files': [
-        'https://fonts.googleapis.com/css?family=Roboto+Slab',
+        'https://fonts.googleapis.com/css?family=Lato',
         '_static/css/pytorch_theme.css'
     ],
 }
