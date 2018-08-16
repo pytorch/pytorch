436e810d29a20a42724e78212a152da341f596ba
Mike Ruberry <mruberry@nvidia.com>
2018-08-15 22:44:01 -0700

    cpu fuser reduction

--- torch/csrc/jit/fusers/fuser_interface.cpp
+++ torch/csrc/jit/fusers/fuser_interface.cpp
@@ -1,7 +1,7 @@
 #include "torch/csrc/jit/fusers/fuser_interface.h"
 
 #if !(defined _WIN32)
-  #include "torch/csrc/jit/fusers/cpu/fusion_compiler.h"
+  // #include "torch/csrc/jit/fusers/cpu/fusion_compiler.h"
   #include "torch/csrc/jit/fusers/cpu/graph_fuser.h"
 #endif // !(defined _WIN32)
 
@@ -13,7 +13,7 @@ void FuseCUDAGraph(std::shared_ptr<Graph>& graph) {
 
 void FuseCPUGraph(std::shared_ptr<Graph>& graph) {
   #if !(defined _WIN32)
-    if (cpufuser::getCompiler().canCompileOnCPU())
+    // if (cpufuser::getCompiler().canCompileOnCPU())
       cpufuser::GraphFuser{graph->block()}.run();
   #endif // !(defined _WIN32)
 }
