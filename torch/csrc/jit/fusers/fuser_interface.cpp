0b5b887b718ede5454e7a36c39c12c83ea428eea
Mike Ruberry <mruberry@nvidia.com>
2018-08-15 22:37:03 -0700

    cpu fuser attempt 2

--- torch/csrc/jit/fusers/fuser_interface.cpp
+++ torch/csrc/jit/fusers/fuser_interface.cpp
@@ -1,6 +1,9 @@
 #include "torch/csrc/jit/fusers/fuser_interface.h"
 
-#include "torch/csrc/jit/fusers/cpu/graph_fuser.h"
+#if !(defined _WIN32)
+  #include "torch/csrc/jit/fusers/cpu/fusion_compiler.h"
+  #include "torch/csrc/jit/fusers/cpu/graph_fuser.h"
+#endif // !(defined _WIN32)
 
 namespace torch { namespace jit { 
 
@@ -10,7 +13,8 @@ void FuseCUDAGraph(std::shared_ptr<Graph>& graph) {
 
 void FuseCPUGraph(std::shared_ptr<Graph>& graph) {
   #if !(defined _WIN32)
-    cpufuser::GraphFuser{graph->block()}.run();
+    if (cpufuser::getCompiler().canCompileOnCPU())
+      cpufuser::GraphFuser{graph->block()}.run();
   #endif // !(defined _WIN32)
 }
 
