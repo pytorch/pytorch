d754c1301b6f3a66ccb04f4b150abbace345aa7e
Mike Ruberry <mruberry@nvidia.com>
2018-08-15 21:37:14 -0700

    adds cpu fuser

--- torch/csrc/jit/fusers/cpu/cpu_fuser.cpp
+++ torch/csrc/jit/fusers/cpu/cpu_fuser.cpp
@@ -1,6 +1,7 @@
 #ifndef _WIN32
 
-#include "torch/csrc/jit/fusers/cpu/fuser_interface.h"
+#include "torch/csrc/jit/fusers/fuser_interface.h"
+#include "torch/csrc/jit/fusers/cpu/cpu_fuser_interface.h"
 #include "torch/csrc/jit/fusers/cpu/cpu_fuser.h"
 
 #include "torch/csrc/jit/ir.h"
