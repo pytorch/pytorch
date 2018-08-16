14c3f49532eac8c507adb4fca3b1db4ef460b7e9
Mike Ruberry <mruberry@nvidia.com>
2018-08-16 00:08:40 -0700

    adding simple cpp file

--- torch/csrc/jit/fusers/cpu/tensor_desc.cpp
+++ torch/csrc/jit/fusers/cpu/tensor_desc.cpp
@@ -1,25 +1,25 @@
-// #if !(defined _WIN32)
+#if !(defined _WIN32)
 
-// #include "torch/csrc/jit/fusers/cpu/tensor_desc.h"
+#include "torch/csrc/jit/fusers/cpu/tensor_desc.h"
 
-// #include "torch/csrc/jit/assertions.h"
+#include "torch/csrc/jit/assertions.h"
 
-// namespace torch { namespace jit { namespace cpufuser {
+namespace torch { namespace jit { namespace cpufuser {
 
-// std::vector<bool> TensorDesc::findContiguous(
-//     const at::IntList& sizes,
-//     const at::IntList& strides) {
-//   JIT_ASSERT(sizes.size() == strides.size());
-//   std::vector<bool> cont(sizes.size());
-//   for(size_t i = 0; i < sizes.size(); ++i) {
-//     int64_t expected_stride = (i + 1 < sizes.size()) ? sizes[i+1]*strides[i+1] : 1;
-//     cont[i] = strides[i] == expected_stride;
-//   }
-//   return cont;
-// }
+std::vector<bool> TensorDesc::findContiguous(
+    const at::IntList& sizes,
+    const at::IntList& strides) {
+  JIT_ASSERT(sizes.size() == strides.size());
+  std::vector<bool> cont(sizes.size());
+  for(size_t i = 0; i < sizes.size(); ++i) {
+    int64_t expected_stride = (i + 1 < sizes.size()) ? sizes[i+1]*strides[i+1] : 1;
+    cont[i] = strides[i] == expected_stride;
+  }
+  return cont;
+}
 
-// } // namespace cpufuser
-// } // namespace jit
-// } // namespace torch
+} // namespace cpufuser
+} // namespace jit
+} // namespace torch
 
-// #endif // !(defined _WIN32)
+#endif // !(defined _WIN32)
