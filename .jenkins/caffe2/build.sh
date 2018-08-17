8548c04d815c144806f1ba688580c6c25ed8519a
Mingzhe Li <mingzhe0908@fb.com>
2018-08-17 14:48:40 -0700

    limit build threads for rocm to avoid OOM issue

--- .jenkins/caffe2/build.sh
+++ .jenkins/caffe2/build.sh
@@ -186,10 +186,12 @@ fi
 # sccache will fail for CUDA builds if all cores are used for compiling
 if [[ "${BUILD_ENVIRONMENT}" == *-cuda* ]] && [ -n "${SCCACHE}" ]; then
   MAX_JOBS=`expr $(nproc) - 1`
+#ROCM builds often encounter OOM issue, so reduce the number of build threads
+elif [[ "${BUILD_ENVIRONMENT}" == *-rocm* ]]; then
+  MAX_JOBS=2
 else
   MAX_JOBS=$(nproc)
 fi
-
 
 
 ###############################################################################
