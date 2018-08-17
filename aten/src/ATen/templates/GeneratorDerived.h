d64709f76082062d41596d4a6236b9c0b187ad95
Orion Reblitz-Richardson <orionr@gmail.com>
2018-08-17 16:53:59 -0700

    Remove AT_API on Generator

--- aten/src/ATen/templates/GeneratorDerived.h
+++ aten/src/ATen/templates/GeneratorDerived.h
@@ -9,7 +9,7 @@
 namespace at {
 
 class Context;
-struct AT_API ${name}Generator : public Generator {
+struct ${name}Generator : public Generator {
   ${name}Generator(Context * context);
   virtual ~${name}Generator();
 
