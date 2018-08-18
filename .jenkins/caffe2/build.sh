745f99e0281bbc06eec5ec5faccaaeb168dbe18c
Junjie Bai <bai@in.tum.de>
2018-08-18 10:35:38 -0700

    Revert "export USE_ROCM=1"
    This reverts commit 9b4a4446eeaa09e453a7be5f6294b0c733df86fc.

--- .jenkins/caffe2/build.sh
+++ .jenkins/caffe2/build.sh
@@ -159,8 +159,6 @@ if [[ $BUILD_ENVIRONMENT == *rocm* ]]; then
   ########## HIPIFY Caffe2 operators
   ${PYTHON} "${ROOT_DIR}/tools/amd_build/build_pytorch_amd.py"
   ${PYTHON} "${ROOT_DIR}/tools/amd_build/build_caffe2_amd.py"
-
-  export USE_ROCM=1
 fi
 
 # Try to include Redis support for Linux builds
