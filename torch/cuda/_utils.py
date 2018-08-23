11b9fd26274852442a2fdf0922ce35e2139655fa
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-08-23 17:05:32 -0400

    Make torch.cuda.* take device objects

---
+++ torch/cuda/_utils.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def _get_device_index(device, optional=False):
+    if isinstane(device, torch.device):
+        dev_type = device.type
+        if device.type != 'cuda':
+            raise ValueError('Execpted a cuda device, but got: {}'.format(device))
+        device = device.index
+    if device is None:
+        if optional:
+            # default cuda device
+            return current_device()
+        else:
+            raise ValueError('Execpted a cuda device or a device index, but got: None')
+    return device
