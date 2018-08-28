12f3cb400a16e6ca891c3560e9a2a6ec438b82c2
Edward Z. Yang <ezyang@fb.com>
2018-08-28 08:03:53 -0700

    V2: rename to caffe2_pb.h
    Differential Revision: D9539945
    Differential Version: 56284757

--- caffe2/core/net_async_dag_gpu.cc
+++ caffe2/core/net_async_dag_gpu.cc
@@ -8,7 +8,7 @@
 #include "caffe2/core/operator.h"
 #include "caffe2/core/static_tracepoint.h"
 #include "caffe2/core/timer.h"
-#include "caffe2/proto/caffe2.pb.h"
+#include "caffe2/proto/caffe2_pb.h"
 #include "caffe2/utils/proto_utils.h"
 
 #include "caffe2/core/context_gpu.h"
