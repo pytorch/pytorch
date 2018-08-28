f679203a47f10b7ffa24b783409b98c637264bd5
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2018-08-28 23:41:29 +0200

    retain backward hooks when deserializing nn.Parameter

--- torch/nn/parameter.py
+++ torch/nn/parameter.py
@@ -18,6 +18,14 @@ class Parameter(torch.Tensor):
         requires_grad (bool, optional): if the parameter requires gradient. See
             :ref:`excluding-subgraphs` for more details. Default: `True`
     """
+
+    @classmethod
+    def _rebuild(cls, data, requires_grad, backward_hooks):
+            obj = cls(data, requires_grad)
+            obj._backward_hooks = backward_hooks
+
+            return obj
+
     def __new__(cls, data=None, requires_grad=True):
         if data is None:
             data = torch.Tensor()
@@ -27,4 +35,4 @@ class Parameter(torch.Tensor):
         return 'Parameter containing:\n' + super(Parameter, self).__repr__()
 
     def __reduce_ex__(self, proto):
-        return Parameter, (self.data, self.requires_grad)
+        return Parameter._rebuild, (self.data, self.requires_grad, self._backward_hooks)
