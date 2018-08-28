8e2efbd59d04a5fa5c34ef5ec7cb9c06130f3acf
Jerry Zhang <jerryzh@fb.com>
2018-08-28 10:45:47 -0700

    Split IsEmptyOp (#10918)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10918
    
    att
    
    Differential Revision: D9515040
    
    fbshipit-source-id: 53c05c160ba5dda92104aadc2e40801519a2cd28

--- caffe2/operators/utility_ops.cc
+++ caffe2/operators/utility_ops.cc
@@ -37,7 +37,6 @@ REGISTER_CPU_OPERATOR(
 REGISTER_CPU_OPERATOR(Copy, CopyOp<CPUContext, CPUContext, CPUContext>);
 REGISTER_CPU_OPERATOR(LengthsToShape, LengthsToShapeOp<CPUContext>);
 REGISTER_CPU_OPERATOR(HasElements, HasElementsOp<CPUContext>);
-REGISTER_CPU_OPERATOR(IsEmpty, IsEmptyOp<CPUContext>);
 REGISTER_CPU_OPERATOR(GatherRanges, GatherRangesOp<CPUContext>);
 REGISTER_CPU_OPERATOR(LengthsGather, LengthsGatherOp<CPUContext>);
 REGISTER_CPU_OPERATOR(LengthsToSegmentIds, LengthsToSegmentIdsOp<CPUContext>);
@@ -545,72 +544,6 @@ has_elements:  False
 )DOC")
     .Input(0, "tensor", "Input data tensor to check for elements.")
     .Output(0, "has_elements", "Output scalar boolean tensor. True if input has size > 0.");
-
-OPERATOR_SCHEMA(IsEmpty)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R"DOC(
-The *IsEmpty* op accepts a single input $tensor$, and produces a single boolean output $is\_empty$. The output is *True* if and only if $tensor$ has size == 0.
-
-Github Links:
-
-- https://github.com/caffe2/caffe2/blob/master/caffe2/operators/utility_ops.cc
-- https://github.com/caffe2/caffe2/blob/master/caffe2/operators/utility_ops.h
-
-
-<details>
-
-<summary> <b>Example</b> </summary>
-
-**Code**
-
-```
-
-workspace.ResetWorkspace()
-
-op = core.CreateOperator(
-    "IsEmpty",
-    ["tensor"],
-    ["is_empty"],
-)
-
-# Use a not-empty tensor
-workspace.FeedBlob("tensor", np.random.randn(2, 2).astype(np.float32))
-print("tensor:\n", workspace.FetchBlob("tensor"))
-
-workspace.RunOperatorOnce(op)
-print("is_empty: ", workspace.FetchBlob("is_empty"),"\n")
-
-# Use an empty tensor
-workspace.FeedBlob("tensor", np.empty(0))
-print("tensor:\n", workspace.FetchBlob("tensor"))
-
-workspace.RunOperatorOnce(op)
-print("is_empty: ", workspace.FetchBlob("is_empty"))
-
-```
-
-**Result**
-
-```
-
-tensor:
- [[ 0.26018378  0.6778789 ]
- [-1.3097627  -0.40083608]]
-is_empty:  False
-
-tensor:
- []
-is_empty:  True
-
-```
-
-</details>
-
-)DOC")
-    .ScalarType(::caffe2::TensorProto_DataType::TensorProto_DataType_BOOL)
-    .Input(0, "tensor", "Input data tensor to check if empty.")
-    .Output(0, "is_empty", "Output scalar boolean tensor. True if input has size == 0.");
 
 OPERATOR_SCHEMA(GatherRanges)
     .NumInputs(2)
