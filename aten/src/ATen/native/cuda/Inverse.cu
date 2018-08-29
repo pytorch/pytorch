639f938dabe64e74a6a7ed2929ab224818bb3893
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2018-08-29 09:09:42 +0530

    Fix CUDA build failure

--- aten/src/ATen/native/cuda/Inverse.cu
+++ aten/src/ATen/native/cuda/Inverse.cu
@@ -79,7 +79,7 @@ void magmaGetrfBatched<float>(
 
 #define ALLOCATE_ARRAY(name, type, size, dummy_tensor) \
   auto storage_##name = pin_memory<type>(size, dummy_tensor); \
-  name = reinterpret_cast<type*>(storage_##name->pImpl()->data());
+  name = static_cast<type*>(storage_##name.data());
 
 template <typename scalar_t>
 static void applyInverse(Tensor &self, Tensor &self_inv, std::vector<int64_t>& infos) {
