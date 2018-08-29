a5e9500a5672595f5ee8a242a411d80adb265dd7
Deyu Fu <deyuf@nvidia.com>
2018-08-29 16:38:07 -0700

    change clamp(min=0) to relu for better jit support

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -1721,7 +1721,7 @@ def binary_cross_entropy_with_logits(input, target, weight=None, size_average=No
     if not (target.size() == input.size()):
         raise ValueError("Target size ({}) must be the same as input size ({})".format(target.size(), input.size()))
 
-    max_val = (-input).clamp(min=0)
+    max_val = torch.relu(-input)
 
     if pos_weight is None:
         loss = input - input * target + max_val + ((-max_val).exp() + (-input - max_val).exp()).log()
