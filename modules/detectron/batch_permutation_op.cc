c755616e006efe011726105e2d7a1d7502c989b9
jgong5 <jiong.gong@intel.com>
2018-08-29 14:56:55 -0700

    Enable Detectron model inference for CPU and MKL-DNN paths (#10157)
    Summary:
    1. Support ops needed for inference of Faster-RCNN/Mask-RCNN needed in Detectron, mostly direct fallbacks.
    2. Use CPU device to hold 0-dim tensors and integer tensors in both fallback op and blob feeder, needed by Detectron models.
    3. Ignore 0-dim tensor in MKL-DNN concat operator.
    4. Generate dynamic library of Detectron module for CPU device.
    
    This PR obsoletes #9164.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10157
    
    Differential Revision: D9276837
    
    Pulled By: yinghai
    
    fbshipit-source-id: dc364932ae4a2e7fcefdee70b5fce3c0cee91b6f

--- modules/detectron/batch_permutation_op.cc
+++ modules/detectron/batch_permutation_op.cc
@@ -15,8 +15,18 @@
  */
 
 #include "batch_permutation_op.h"
+#ifdef CAFFE2_USE_IDEEP
+#include <caffe2/ideep/operators/operator_fallback_ideep.h>
+#include <caffe2/ideep/utils/ideep_operator.h>
+#endif
 
 namespace caffe2 {
+
+#ifdef CAFFE2_USE_IDEEP
+REGISTER_IDEEP_OPERATOR(
+    BatchPermutation,
+    IDEEPFallbackOp<BatchPermutationOp<float, CPUContext>>);
+#endif
 
 REGISTER_CPU_OPERATOR(BatchPermutation, BatchPermutationOp<float, CPUContext>);
 REGISTER_CPU_OPERATOR(
