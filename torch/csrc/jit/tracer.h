209dd538d4807b283136aa3af89a2b05376b39ea
Adam Paszke <adam.paszke@gmail.com>
2018-08-29 08:49:37 -0700

    Remove TORCH_API from templated functions

--- torch/csrc/jit/tracer.h
+++ torch/csrc/jit/tracer.h
@@ -246,19 +246,19 @@ void addInputs(Node *n, const char * name, std::array<bool, N> value) {
   throw std::runtime_error("Found an unsupported argument type in the JIT tracer. File a bug report.");
 }
 
-TORCH_API void addOutput(Node* node, const at::Tensor& tensor);
-TORCH_API void addOutput(Node* node, const std::vector<at::Tensor>& list);
-
 template <
     typename T,
     typename = torch::enable_if_t<
         (!std::is_convertible<torch::decay_t<T>, at::TensorList>::value &&
          !std::is_convertible<torch::decay_t<T>, at::Tensor>::value)>>
-TORCH_API void addOutput(Node* node, T&&) {
+void addOutput(Node* node, T&&) {
   AT_ERROR(
       "Found an unsupported argument type ", at::demangle_type<T>(),
       " in the JIT tracer. File a bug report.");
 }
+TORCH_API void addOutput(Node* node, const at::Tensor& tensor);
+TORCH_API void addOutput(Node* node, const std::vector<at::Tensor>& list);
+
 TORCH_API autograd::Variable getSizeOf(const autograd::Variable& var, int64_t dim);
 
 }}} // namespace torch::jit::tracer
