446a6f76d14bb38ffad60d2955091e23ebf84cfb
Erik Brinkman <ebrinkman@fb.com>
2018-08-30 11:40:53 -0700

    Add ATen pdist CPU kernel (#10782)
    Summary:
    Also add single grad whitelist to the jit test
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/10782
    
    Reviewed By: ezyang
    
    Differential Revision: D9583378
    
    Pulled By: erikbrinkman
    
    fbshipit-source-id: 069e5ae68ea7f3524dec39cf1d5fe9cd53941944

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -2299,6 +2299,30 @@ def pairwise_distance(x1, x2, p=2, eps=1e-6, keepdim=False):
     return torch.pairwise_distance(x1, x2, p, eps, keepdim)
 
 
+pdist = _add_docstr(torch.pdist, r"""
+pdist(input, p=2) -> Tensor
+
+Computes the p-norm distance between every pair of row vectors in the input.
+This is identical to the upper triangular portion, excluding the diagonal, of
+`torch.norm(input[:, None] - input, dim=2, p=p)`. This function will be faster
+if the rows are contiguous.
+
+If input has shape :math:`N \times M` then the output will have shape
+:math:`\frac{1}{2} N (N - 1)`.
+
+This function is equivalent to `scipy.spatial.distance.pdist(input,
+'minkowski', p=p)` if :math:`p \in (0, \infty)`. When :math:`p = 0` it is
+equivalent to `scipy.spatial.distance.pdist(input, 'hamming') * M`.
+When :math:`p = \infty`, the closest scipy function is
+`scipy.spatial.distance.pdist(xn, lambda x, y: np.abs(x - y).max())`.
+
+Args:
+    input: input tensor of shape :math:`N \times M`.
+    p: p value for the p-norm distance to calculate between each vector pair
+        :math:`\in [0, \infty]`.
+""")
+
+
 def cosine_similarity(x1, x2, dim=1, eps=1e-8):
     r"""Returns cosine similarity between x1 and x2, computed along dim.
 
