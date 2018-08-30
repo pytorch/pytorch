ec0d1427d35f015fe6a106c63330c5b4a9cdafab
striajan <stria.jan@gmail.com>
2018-08-30 15:46:33 +0200

    Implement cosine annealing schedule with warm restarts.

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -251,6 +251,70 @@ class CosineAnnealingLR(_LRScheduler):
                 for base_lr in self.base_lrs]
 
 
+class CosineAnnealingRestartsLR(_LRScheduler):
+    r"""Set the learning rate of each parameter group using a cosine annealing
+    schedule with warm restarts, where :math:`\eta_{max}` is set to the
+    initial learning rate, :math:`T_{cur}` is the number of epochs since the
+    last restart in SGDR and :math:`T_{i}` is the number of iterations in
+    :math:`i`-th run (after :math:`i-1` restarts):
+
+    .. math::
+
+        \eta_t = \eta_{min} + \frac{1}{2} \gamma^{i-1} (\eta_{max} - \eta_{min})
+        (1 + \cos(\frac{T_{cur}}{T_{i}-1}\pi))
+
+        T_{i} = T_{max} T_{mult}^{i-1}
+
+    When last_epoch=-1, sets initial lr as lr.
+
+    It has been proposed in
+    `SGDR: Stochastic Gradient Descent with Warm Restarts`_. Note that in the
+    paper the :math:`i`-th run takes :math:`T_{i}+1` iterations, while in this
+    implementation it takes :math:`T_{i}` iterations only. This implementation
+    also enables updating the range of learning rates by multiplicative factor
+    :math:`\gamma`.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        T_max (int): Maximum number of iterations in the first run.
+        eta_min (float): Minimum learning rate. Default: 0.
+        T_mult (float): Multiplicative factor adjusting number of iterations
+            after each restart. Default: 2
+        gamma (float): Multiplicative factor of decay in learning rate range
+            applied after each restart. Default: 1
+        last_epoch (int): The index of last epoch. Default: -1.
+
+    .. _SGDR\: Stochastic Gradient Descent with Warm Restarts:
+        https://arxiv.org/abs/1608.03983
+    """
+
+    def __init__(self, optimizer, T_max, eta_min=0, T_mult=2.0, gamma=1.0, last_epoch=-1):
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.T_mult = T_mult
+        self.gamma = gamma
+
+        super(CosineAnnealingRestartsLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if self.T_mult == 1:
+            num_restarts = self.last_epoch // self.T_max
+            last_restart = num_restarts * self.T_max
+            T_i = self.T_max
+
+        else:
+            # computation on the last restarting epoch is based on sum of geometric series:
+            # last_restart = T_max * (1 + T_mult + T_mult ** 2 + ... + T_mult ** num_restarts)
+            num_restarts = math.floor(math.log(1 - self.last_epoch * (1 - self.T_mult) / self.T_max, self.T_mult))
+            last_restart = self.T_max * (1 - self.T_mult ** num_restarts) / (1 - self.T_mult)
+            T_i = self.T_max * self.T_mult ** num_restarts
+
+        t = (self.last_epoch - last_restart) / (T_i - 1)
+        decay = 0.5 * (self.gamma ** num_restarts) * (1 + math.cos(t * math.pi))
+
+        return [decay * base_lr + (1 - decay) * self.eta_min for base_lr in self.base_lrs]
+
+
 class ReduceLROnPlateau(object):
     """Reduce learning rate when a metric has stopped improving.
     Models often benefit from reducing the learning rate by a factor
