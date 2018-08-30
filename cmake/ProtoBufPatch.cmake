ec430fc9b30a117ac2377b0003aac043f6bef839
Orion Reblitz-Richardson <orionr@gmail.com>
2018-08-30 14:48:53 -0700

    Inject GetEmptyStringAlreadyInited once for static proto (#11045)
    Summary:
    I've been seeing a lot of warnings about multiple declarations of this. Hopefully this fixes it.
    
    cc Yangqing mingzhe09088 ezyang
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11045
    
    Reviewed By: mingzhe09088
    
    Differential Revision: D9582756
    
    Pulled By: orionr
    
    fbshipit-source-id: 6171485609a2f2f357d6e1c44e26b4ecfcdb4ce6

--- cmake/ProtoBufPatch.cmake
+++ cmake/ProtoBufPatch.cmake
@@ -12,12 +12,25 @@ string(
   "${content}")
 
 foreach(ns ${NAMESPACES})
-  string(
-    REPLACE
-    "namespace ${ns} {"
-    "namespace ${ns} { const ::std::string& GetEmptyStringAlreadyInited(); "
-    content
-    "${content}")
+  # Insert "const ::std::string& GetEmptyStringAlreadyInited();" within
+  # the namespace and make sure we only do it once in the file. Unfortunately
+  # using string(REPLACE ...) doesn't work because it will replace at all
+  # locations and there might be multiple declarations of the namespace
+  # depending on how the proto is structured.
+  set(search "namespace ${ns} {")
+  string(LENGTH "${search}" search_len)
+  string(FIND "${content}" "${search}" pos)
+  if (${pos} GREATER -1)
+    math(EXPR pos "${pos}+${search_len}")
+    string(SUBSTRING "${content}" 0 ${pos} content_pre)
+    string(SUBSTRING "${content}" ${pos} -1 content_post)
+    string(
+      CONCAT
+      content
+      "${content_pre}"
+      " const ::std::string& GetEmptyStringAlreadyInited(); "
+      "${content_post}")
+  endif()
 endforeach()
 
 file(WRITE ${FILENAME} "${content}")
