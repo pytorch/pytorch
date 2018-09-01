341e40833a431162f6f26a4262f723d5b0406820
Shihao Xu <shihaoxu@fb.com>
2018-08-31 22:11:41 -0700

    Revert D9566744: [New Checkpoint] Kill the dummy TaskOutput when task.get_step() (#11164)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11164
    
    Revert D9566744
    
    Reviewed By: enosair
    
    Differential Revision: D9620272
    
    fbshipit-source-id: 6a78c46929f66bd11969840cb6b107f734be0c02

--- caffe2/python/task.py
+++ caffe2/python/task.py
@@ -150,7 +150,7 @@ def add_setup_steps(step, init_nets, exit_nets, name):
     if init_nets:
         steps.append(core.execution_step('%s:init' % name, init_nets))
     steps.append(step)
-    if exit_nets:
+    if len(exit_nets) > 0:
         steps.append(core.execution_step('%s:exit' % name, exit_nets))
     return core.execution_step(name, steps)
 
@@ -215,11 +215,10 @@ class TaskGroup(object):
         self._tasks.append(task)
 
     def tasks(self):
-        if not self._already_used:
-            for task in self._tasks_to_add:
-                self.add(task)
-            self._tasks_to_add = []
-            self._already_used = True
+        for task in self._tasks_to_add:
+            self.add(task)
+        self._tasks_to_add = []
+        self._already_used = True
         return self._tasks
 
     def num_registered_tasks(self):
@@ -260,8 +259,9 @@ class TaskGroup(object):
         # tasks_by_node can't be called twice because the setup won't
         # work properly a second time.
         node_map = {}
-        for node in self.used_nodes():
-            node_map[node] = node_remap(node) if node_remap else node
+        for task in self.tasks():
+            node_map[task.node] =\
+                node_remap(task.node) if node_remap else task.node
         if self._tasks_by_node is not None:
             tasks_by_node, prev_node_map = self._tasks_by_node
             assert prev_node_map == node_map, (
@@ -285,7 +285,11 @@ class TaskGroup(object):
         grouped_by_node = TaskGroup()
         for node, tasks in viewitems(tasks_by_node):
             report_steps = report_steps_by_node[node]
-
+            node_inits, node_exits = get_setup_nets(
+                TaskGroup.LOCAL_SETUP,
+                [t.get_step() for t in tasks] + report_steps,
+                self)
+            # shortcut for single task with no queue
             steps = report_steps
             outputs = []
             grouped_workspace_type = WorkspaceType.PRIVATE
@@ -307,15 +311,16 @@ class TaskGroup(object):
             else:
                 step = core.execution_step(
                     '%s:body' % node, steps, concurrent_substeps=True)
-
-            # Prepend and append setup nets.
-            node_inits, node_exits = get_setup_nets(
-                TaskGroup.LOCAL_SETUP,
-                [t.get_step() for t in tasks] + report_steps,
-                self,
-            )
-            step = add_setup_steps(step, node_inits, node_exits, node)
-
+            if len(node_inits) > 0 or len(node_exits) > 0:
+                steps = []
+                if len(node_inits) > 0:
+                    steps.append(
+                        core.execution_step('%s:init' % node, node_inits))
+                steps.append(step)
+                if len(node_exits) > 0:
+                    steps.append(
+                        core.execution_step('%s:exit' % node, node_exits))
+                step = core.execution_step(node, steps)
             Task(
                 node=node, step=step, outputs=outputs,
                 name='grouped_by_node',
@@ -577,6 +582,11 @@ class Task(object):
             Task.TASK_SETUP, [self._step] + report_steps, self)
         instance_init_nets, instance_exit_nets = get_setup_nets(
             Task.TASK_INSTANCE_SETUP, [self._step] + report_steps, self)
+        if len(self._outputs) == 0:
+            output_net = core.Net('%s:output' % self.name)
+            self.add_output(output_net.ConstantFill(
+                [], 1, dtype=core.DataType.INT32, value=0))
+            task_exit_nets.append(output_net)
 
         # Add instance-level report steps
         body = self._step if not report_steps else core.execution_step(
