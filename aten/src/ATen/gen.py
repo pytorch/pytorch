43fd6b234dba08a67d6bb5a3eb18969b0cc41ea1
Edward Yang <ezyang@fb.com>
2018-09-02 15:16:33 -0700

    Make Type a (mostly) pure virtual class; TypeDefault for impls (#11013) (#11013)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11013
    
    Previously, the parent class Type also contained a large number
    of implementations, for things like broadcasting and native
    functions that didn't need dispatch.  We'd like to be able
    to reference this interface from Tensor even when we don't
    have any of these implementations are available.
    
    To do this, we convert Type into a truly pure virtual interface,
    and move all of the implementations to TypeDefault.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11181
    
    Differential Revision: D9561478
    
    Pulled By: ezyang
    
    fbshipit-source-id: 13c49d80bc547551adf524b1cf1d691bfe311133

--- aten/src/ATen/gen.py
+++ aten/src/ATen/gen.py
@@ -107,7 +107,8 @@ TYPE_DERIVED_CPP = CodeTemplate.from_file(TEMPLATE_PATH + "/TypeDerived.cpp")
 SPARSE_TYPE_DERIVED_CPP = CodeTemplate.from_file(TEMPLATE_PATH + "/SparseTypeDerived.cpp")
 TYPE_DERIVED_H = CodeTemplate.from_file(TEMPLATE_PATH + "/TypeDerived.h")
 TYPE_H = CodeTemplate.from_file(TEMPLATE_PATH + "/Type.h")
-TYPE_CPP = CodeTemplate.from_file(TEMPLATE_PATH + "/Type.cpp")
+TYPE_DEFAULT_H = CodeTemplate.from_file(TEMPLATE_PATH + "/TypeDefault.h")
+TYPE_DEFAULT_CPP = CodeTemplate.from_file(TEMPLATE_PATH + "/TypeDefault.cpp")
 
 REGISTER_CPU_H = CodeTemplate.from_file(TEMPLATE_PATH + "/RegisterCPU.h")
 REGISTER_CPU_CPP = CodeTemplate.from_file(TEMPLATE_PATH + "/RegisterCPU.cpp")
@@ -166,6 +167,7 @@ top_env = {
     'cpu_type_headers': [],
     'cuda_type_registrations': [],
     'cuda_type_headers': [],
+    'pure_virtual_type_method_declarations': [],
     'type_method_declarations': [],
     'type_method_definitions': [],
     'type_method_inline_definitions': [],
@@ -329,7 +331,7 @@ def iterate_types():
 # so that the script runs quickly when we are just querying the
 # outputs
 def declare_outputs():
-    files = ['Declarations.yaml', 'Type.h', 'Type.cpp', 'Tensor.h',
+    files = ['Declarations.yaml', 'Type.h', 'TypeDefault.cpp', 'TypeDefault.h', 'Tensor.h',
              'TensorMethods.h', 'Functions.h',
              'CPUCopy.cpp', 'NativeFunctions.h',
              'RegisterCPU.cpp', 'RegisterCPU.h']
@@ -399,7 +401,8 @@ def generate_outputs():
             backend, density, scalar_type, declarations))
 
     file_manager.write('Type.h', TYPE_H, top_env)
-    file_manager.write('Type.cpp', TYPE_CPP, top_env)
+    file_manager.write('TypeDefault.h', TYPE_DEFAULT_H, top_env)
+    file_manager.write('TypeDefault.cpp', TYPE_DEFAULT_CPP, top_env)
 
     file_manager.write('RegisterCPU.h', REGISTER_CPU_H, top_env)
     file_manager.write('RegisterCPU.cpp', REGISTER_CPU_CPP, top_env)
