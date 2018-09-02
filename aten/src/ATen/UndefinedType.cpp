ebf29babce3ac7abbd8f681a065835eda2cd13da
Edward Yang <ezyang@fb.com>
2018-09-02 15:16:33 -0700

    Make Type a (mostly) pure virtual class; TypeDefault for impls (#11013) (#11013)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11013
    
    Previously, the parent class Type also contained a large number
    of implementations, for things like broadcasting and native
    functions that didn't need dispatch.  We'd like to be able
    to reference this interface from Tensor even when we don't
    have any of these implementations are available.
    
    To do this, we convert Type into a truly pure virtual interface,
    and move all of the implementations to TypeDefault.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11181
    
    Differential Revision: D9561478
    
    Pulled By: ezyang
    
    fbshipit-source-id: 13c49d80bc547551adf524b1cf1d691bfe311133

--- aten/src/ATen/UndefinedType.cpp
+++ aten/src/ATen/UndefinedType.cpp
@@ -4,7 +4,7 @@
 namespace at {
 
 UndefinedType::UndefinedType()
-    : Type(UndefinedTensorId(), /*is_variable=*/false, /*is_undefined=*/true) {}
+    : TypeDefault(UndefinedTensorId(), /*is_variable=*/false, /*is_undefined=*/true) {}
 ScalarType UndefinedType::scalarType() const {
   return ScalarType::Undefined;
 }
@@ -50,13 +50,13 @@ size_t UndefinedType::elementSizeInBytes() const {
 
 Type & UndefinedType::toBackend(Backend b) const {
   if (b == Backend::Undefined) {
-    return Type::toBackend(b);
+    return TypeDefault::toBackend(b);
   }
   AT_ERROR("toBackend not implemented for UndefinedType to non-UndefinedType");
 }
 Type & UndefinedType::toScalarType(ScalarType s) const {
   if (s == ScalarType::Undefined) {
-    return Type::toScalarType(s);
+    return TypeDefault::toScalarType(s);
   }
   AT_ERROR("toScalarType not implemented for UndefinedType to non-UndefinedType");
 }
--- aten/src/ATen/UndefinedType.h
+++ aten/src/ATen/UndefinedType.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "ATen/Type.h"
+#include "ATen/TypeDefault.h"
 #include "ATen/CheckGenerator.h"
 
 #ifdef _MSC_VER
@@ -11,7 +11,7 @@
 
 namespace at {
 
-struct UndefinedType final : public Type {
+struct UndefinedType final : public TypeDefault {
   explicit UndefinedType();
   virtual ScalarType scalarType() const override;
   virtual Backend backend() const override;
