882934b50ed7f591102383d726c36b58ce7b1bad
Will Feng <willfeng@fb.com>
2018-09-05 15:51:33 -0400

    Skip TestOperators.test_logit

--- caffe2/python/hypothesis_test.py
+++ caffe2/python/hypothesis_test.py
@@ -1825,6 +1825,7 @@ class TestOperators(hu.HypothesisTestCase):
         out, = self.assertReferenceChecks(gc, op, [a], ref)
         self.assertEqual(dst, out.dtype)
 
+    @unittest.skipIf("IN_CIRCLECI" in os.environ, "FIXME: flaky test in CircleCI")
     @given(a=hu.tensor(),
            eps=st.floats(min_value=1e-4, max_value=1e-2),
            a_grad=hu.tensor(elements=st.floats(min_value=0.01, max_value=0.99)),
