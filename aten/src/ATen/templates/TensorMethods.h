3f6c0584816d72f2d50604ff86634ffdc90a5d95
Gregory Chanan <gchanan@fb.com>
2018-09-04 18:01:08 -0700

    Pull Context out of TensorMethods.
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/11241
    
    Reviewed By: ezyang
    
    Differential Revision: D9645514
    
    Pulled By: gchanan
    
    fbshipit-source-id: 43e65d1d2fa3183264ed7e4752c1512df5f69175

--- aten/src/ATen/templates/TensorMethods.h
+++ aten/src/ATen/templates/TensorMethods.h
@@ -8,7 +8,6 @@
 #include "ATen/Type.h"
 #include "ATen/core/TensorOptions.h"
 #include "ATen/DeviceGuard.h"
-#include "ATen/Context.h"
 
 namespace at {
 
@@ -54,8 +53,11 @@ inline Tensor to(
   if (tensor.options() == options) {
     return tensor;
   }
+  AT_CHECK(tensor.is_variable() == options.is_variable(),
+           "cannot change is_variable, from: ", tensor.is_variable(),
+           " to: ", options.is_variable());
   DeviceGuard guard(options.device());
-  return at::getMaybeVariableType(options).copy(tensor, non_blocking);
+  return tensor.type().toBackend(options.backend()).toScalarType(options.dtype()).copy(tensor, non_blocking);
 }
 } // namespace detail
 
