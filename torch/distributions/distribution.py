d8ee34905f4d5d1ea006d069536520dc24ad6cda
Neeraj Pradhan <npradhan@uber.com>
2018-09-06 21:29:32 -0700

    Optional expand=True kwarg in distribution.enumerate_support (#11231)
    Summary:
    This adds an optional `expand=True` kwarg to the `distribution.expand_support()` method, to get a distribution's support without expanding the values over the distribution's `batch_shape`.
     - The default `expand=True` preserves the current behavior, whereas `expand=False` collapses the batch dimensions.
    
    e.g.
    ```python
    In [47]: d = dist.OneHotCategorical(torch.ones(3, 5) * 0.5)
    
    In [48]: d.batch_shape
    Out[48]: torch.Size([3])
    
    In [49]: d.enumerate_support()
    Out[49]:
    tensor([[[1., 0., 0., 0., 0.],
             [1., 0., 0., 0., 0.],
             [1., 0., 0., 0., 0.]],
    
            [[0., 1., 0., 0., 0.],
             [0., 1., 0., 0., 0.],
             [0., 1., 0., 0., 0.]],
    
            [[0., 0., 1., 0., 0.],
             [0., 0., 1., 0., 0.],
             [0., 0., 1., 0., 0.]],
    
            [[0., 0., 0., 1., 0.],
             [0., 0., 0., 1., 0.],
             [0., 0., 0., 1., 0.]],
    
            [[0., 0., 0., 0., 1.],
             [0., 0., 0., 0., 1.],
             [0., 0., 0., 0., 1.]]])
    
    In [50]: d.enumerate_support().shape
    Out[50]: torch.Size([5, 3, 5])
    
    In [51]: d.enumerate_support(expand=False)
    Out[51]:
    tensor([[[1., 0., 0., 0., 0.]],
    
            [[0., 1., 0., 0., 0.]],
    
            [[0., 0., 1., 0., 0.]],
    
            [[0., 0., 0., 1., 0.]],
    
            [[0., 0., 0., 0., 1.]]])
    
    In [52]: d.enumerate_support(expand=False).shape
    Out[52]: torch.Size([5, 1, 5])
    ```
    
    **Motivation:**
     - Currently `enumerate_support` builds up tensors of size `support + batch_shape + event_shape`, but the values are *repeated* over the `batch_shape` (adding little in the way of information). This can lead to expensive matrix operations over large tensors when `batch_shape` is large (see, example above), often leading to OOM issues. We use `expand=False` in Pyro for message passing inference. e.g. when enumerating over the state space in a Hidden Markov Model. This creates sparse tensors that capture the markov dependence, and allows for the possibility of using optimized matrix operations over these sparse tensors. `expand=True`, on the other hand, will create tensors that scale exponentially in size with the length of the Markov chain.
     - We have been using this in our [patch](https://github.com/uber/pyro/blob/dev/pyro/distributions/torch.py) of `torch.distributions` in Pyro. The interface has been stable, and it is already being used in a few Pyro algorithms. We think that this is more broadly applicable and will be of interest to the larger distributions community.
    
    cc. apaszke, fritzo, alicanb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11231
    
    Differential Revision: D9696290
    
    Pulled By: soumith
    
    fbshipit-source-id: c556f8ff374092e8366897ebe3f3b349538d9318

--- torch/distributions/distribution.py
+++ torch/distributions/distribution.py
@@ -142,7 +142,7 @@ class Distribution(object):
         """
         raise NotImplementedError
 
-    def enumerate_support(self):
+    def enumerate_support(self, expand=True):
         """
         Returns tensor containing all values supported by a discrete
         distribution. The result will enumerate over dimension 0, so the shape
@@ -150,8 +150,16 @@ class Distribution(object):
         (where `event_shape = ()` for univariate distributions).
 
         Note that this enumerates over all batched tensors in lock-step
-        `[[0, 0], [1, 1], ...]`. To iterate over the full Cartesian product
-        use `itertools.product(m.enumerate_support())`.
+        `[[0, 0], [1, 1], ...]`. With `expand=False`, enumeration happens
+        along dim 0, but with the remaining batch dimensions being
+        singleton dimensions, `[[0], [1], ..`.
+
+        To iterate over the full Cartesian product use
+        `itertools.product(m.enumerate_support())`.
+
+        Args:
+            expand (bool): whether to expand the support over the
+                batch dims to match the distribution's `batch_shape`.
 
         Returns:
             Tensor iterating over dimension 0.
