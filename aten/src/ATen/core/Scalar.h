4f6e8e3124b7703db6396aa2ea21af01e6e5bebc
Edward Z. Yang <ezyang@fb.com>
2018-09-07 09:29:55 -0700

    Windows fix some more
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/core/Scalar.h
+++ aten/src/ATen/core/Scalar.h
@@ -32,7 +32,10 @@ public:
 
 #define DEFINE_IMPLICIT_CTOR(type,name,member) \
   Scalar(type vv) \
-  : tag(Tag::HAS_##member), v{ .member = { convert<double>(vv.real()), convert<double>(vv.imag()) } } {}
+  : tag(Tag::HAS_##member) { \
+    v . member[0] = convert<double>(vv.real()); \
+    v . member[1] = convert<double>(vv.imag()); \
+  }
 
   DEFINE_IMPLICIT_CTOR(at::ComplexHalf,ComplexHalf,z)
   DEFINE_IMPLICIT_CTOR(std::complex<float>,ComplexFloat,z)
