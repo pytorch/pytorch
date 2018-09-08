02126c4122c476acf01ab16d771415047329251c
Edward Yang <ezyang@fb.com>
2018-09-08 09:58:55 -0700

    Move the type registry out of Context, into LegacyTypeDispatch. (#11274)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11274
    
    We don't want to put all of Context into ATen/core, but one
    particular part cannot be avoided: the type registry, because
    implementations of TensorMethods will need to get a Type,
    and then do a virtual call on it.
    
    I needed to do a little bit of (temporary) footwork to get this
    in without also moving Type, because unique_ptr<Type> expects
    to be able to see the destructor of Type (but it's forward declared
    right now).  So instead I put the destructor as an explicit functor.  We
    can get rid of this once Type actually moves in ATen/core
    
    Reviewed By: cpuhrsch
    
    Differential Revision: D9657449
    
    fbshipit-source-id: 940931493bf4f1f6a8dad03f34633cacdd63dd0b

--- aten/src/ATen/templates/RegisterCPU.cpp
+++ aten/src/ATen/templates/RegisterCPU.cpp
@@ -13,8 +13,7 @@ namespace at {
 
 void register_cpu_types(Context * context) {
   ${cpu_type_registrations}
-  context->type_registry[static_cast<int>(Backend::Undefined)]
-                        [static_cast<int>(ScalarType::Undefined)].reset(new UndefinedType());
+  context->registerType(Backend::Undefined, ScalarType::Undefined, new UndefinedType());
 }
 
 } // namespace at
