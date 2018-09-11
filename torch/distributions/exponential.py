2404f5e227d4eebeef0666b8c49169cbd96425bc
Neeraj Pradhan <npradhan@uber.com>
2018-09-11 06:50:33 -0700

    Add .expand() method to distribution classes (#11341)
    Summary:
    This adds a `.expand` method for distributions that is akin to the `torch.Tensor.expand` method for tensors. It returns a new distribution instance with batch dimensions expanded to the desired `batch_shape`. Since this calls `torch.Tensor.expand` on the distribution's parameters, it does not allocate new memory for the expanded distribution instance's parameters.
    
    e.g.
    ```python
    >>> d = dist.Normal(torch.zeros(100, 1), torch.ones(100, 1))
    >>> d.sample().shape
      torch.Size([100, 1])
    >>> d.expand([100, 10]).sample().shape
      torch.Size([100, 10])
    ```
    
    We have already been using the `.expand` method in Pyro in our [patch](https://github.com/uber/pyro/blob/dev/pyro/distributions/torch.py#L10) of `torch.distributions`. We use this in our models to enable dynamic broadcasting. This has also been requested by a few users on the distributions slack, and we believe will be useful to the larger community.
    
    Note that currently, there is no convenient and efficient way to expand distribution instances:
     - Many distributions use `TransformedDistribution` (or wrap over another distribution instance. e.g. `OneHotCategorical` uses a `Categorical` instance) under the hood, or have lazy parameters. This makes it difficult to collect all the relevant parameters, broadcast them and construct new instances.
     - In the few cases where this is even possible, the resulting implementation would be inefficient since we will go through a lot of broadcasting and args validation logic in `__init__.py` that can be avoided.
    
    The `.expand` method allows for a safe and efficient way to expand distribution instances. Additionally, this bypasses `__init__.py` (using `__new__` and populating relevant attributes) since we do not need to do any broadcasting or args validation (which was already done when the instance was first created). This can result in significant savings as compared to constructing new instances via `__init__` (that said, the `sample` and `log_prob` methods will probably be the rate determining steps in many applications).
    
    e.g.
    ```python
    >>> a = dist.Bernoulli(torch.ones([10000, 1]), validate_args=True)
    
    >>> %timeit a.expand([10000, 100])
    15.2 µs ± 224 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
    
    >>> %timeit dist.Bernoulli(torch.ones([10000, 100]), validate_args=True)
    11.8 ms ± 153 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
    ```
    
    cc. fritzo, apaszke, vishwakftw, alicanb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11341
    
    Differential Revision: D9728485
    
    Pulled By: soumith
    
    fbshipit-source-id: 3b94c23bc6a43ee704389e6287aa83d1e278d52f

--- torch/distributions/exponential.py
+++ torch/distributions/exponential.py
@@ -41,6 +41,14 @@ class Exponential(ExponentialFamily):
         batch_shape = torch.Size() if isinstance(rate, Number) else self.rate.size()
         super(Exponential, self).__init__(batch_shape, validate_args=validate_args)
 
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(Exponential, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new.rate = self.rate.expand(batch_shape)
+        super(Exponential, new).__init__(batch_shape, validate_args=False)
+        new._validate_args = self._validate_args
+        return new
+
     def rsample(self, sample_shape=torch.Size()):
         shape = self._extended_shape(sample_shape)
         return self.rate.new(shape).exponential_() / self.rate
