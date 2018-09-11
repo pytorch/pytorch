f3519e9d65b2f6727a6d95b2835eb57b7a16fc79
Edward Z. Yang <ezyang@fb.com>
2018-09-11 10:09:54 -0700

    V4: whack it again
    Differential Revision: D9771708
    Differential Version: 57500418

--- aten/src/ATen/templates/TypeExtendedInterface.h
+++ aten/src/ATen/templates/TypeExtendedInterface.h
@@ -6,7 +6,7 @@ namespace at {
 struct AT_API TypeExtendedInterface : public Type {
   explicit TypeExtendedInterface(TensorTypeId type_id, bool is_variable, bool is_undefined)
       : Type(type_id, is_variable, is_undefined) {}
-  ${pure_virtual_internal_type_method_declarations}
+  ${pure_virtual_extended_type_method_declarations}
 };
 
 } // namespace at
