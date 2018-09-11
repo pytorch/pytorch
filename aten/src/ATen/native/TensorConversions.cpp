3eb72da3b03dd81032d7b0e11eb91ba0185fe088
Adam Paszke <adam.paszke@gmail.com>
2018-09-11 12:18:16 -0700

    More fixes

--- aten/src/ATen/native/TensorConversions.cpp
+++ aten/src/ATen/native/TensorConversions.cpp
@@ -11,19 +11,24 @@ static void ensure_has_index(Device* device) {
   device->set_index(at::current_device());
 }
 
+static Tensor to_impl(const Tensor& self, const TensorOptions& options, bool non_blocking) {
+  return self.type().toBackend(options.backend()).toScalarType(options.dtype())
+                    .copy(self, non_blocking, options.device());
+}
+
 Tensor to(const Tensor& self, Device device, ScalarType dtype, bool non_blocking) {
   ensure_has_index(&device);
   if (self.device() == device && self.dtype() == dtype) {
     return self;
   }
-  return self.type().toScalarType(dtype).copy(self, non_blocking, device);
+  return to_impl(self, self.options().device(device).dtype(dtype), non_blocking);
 }
 
 Tensor to(const Tensor& self, ScalarType dtype, bool non_blocking) {
   if (self.dtype() == dtype) {
     return self;
   }
-  return self.type().toScalarType(dtype).copy(self, non_blocking, self.device());
+  return to_impl(self, self.options().dtype(dtype), non_blocking);
 }
 
 Tensor to(const Tensor& self, Device device, bool non_blocking) {
@@ -31,7 +36,7 @@ Tensor to(const Tensor& self, Device device, bool non_blocking) {
   if (self.device() == device) {
     return self;
   }
-  return self.type().copy(self, non_blocking, device);
+  return to_impl(self, self.options().device(device), non_blocking);
 }
 
 Tensor to(const Tensor& self, const Tensor& other, bool non_blocking) {
@@ -40,8 +45,7 @@ Tensor to(const Tensor& self, const Tensor& other, bool non_blocking) {
   if (self_options == options) {
     return self;
   }
-  return self.type().toBackend(options.backend()).toScalarType(options.dtype())
-                    .copy(self, non_blocking, options.device());
+  return to_impl(self, options, non_blocking);
 }
 
 }} // namespace at::native
