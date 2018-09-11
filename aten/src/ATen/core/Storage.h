541a900e9f9faf564438700c35267517b0e4d0ab
Sebastian Messmer <messmer@fb.com>
2018-09-11 13:56:31 -0700

    V2: Initial commit
    Differential Revision: D9778042
    Differential Version: 57535402

--- aten/src/ATen/core/Storage.h
+++ aten/src/ATen/core/Storage.h
@@ -7,9 +7,7 @@ namespace at {
 struct AT_API Storage {
 public:
   Storage() {}
-  Storage(StorageImpl* storage_impl) : storage_impl_(c10::intrusive_ptr<StorageImpl>::reclaim(storage_impl)) {}
-  Storage(const c10::intrusive_ptr<StorageImpl>& ptr) : storage_impl_(ptr) {}
-  Storage(c10::intrusive_ptr<StorageImpl>&& ptr) : storage_impl_(std::move(ptr)) {}
+  Storage(c10::intrusive_ptr<StorageImpl> ptr) : storage_impl_(std::move(ptr)) {}
   Storage(
       at::ScalarType,
       size_t size,
