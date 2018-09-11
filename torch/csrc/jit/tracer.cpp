a97ddd4b8b3146fdeb556f4bc7f5adf475d30aef
Adam Paszke <adam.paszke@gmail.com>
2018-09-10 18:53:07 -0700

    Add support for tracing strings

--- torch/csrc/jit/tracer.cpp
+++ torch/csrc/jit/tracer.cpp
@@ -39,8 +39,8 @@ void addInputs(Node *n, const char * name, int64_t value)            { detail::g
 void addInputs(Node *n, const char * name, bool value)               { detail::genericAddInput(n, value); }
 void addInputs(Node *n, const char * name, double value)             { detail::genericAddInput(n, value); }
 void addInputs(Node *n, const char * name, const at::Scalar& value)  { detail::genericAddInput(n, value); }
+void addInputs(Node *n, const char * name, const std::string& value) { detail::genericAddInput(n, value); }
 void addInputs(Node *n, const char * name, const at::Tensor& value)  { n->addInput(getValueTrace(value)); }
-void addInputs(Node *n, const char * name, const std::string& value)         { detail::badArgType(value); }
 void addInputs(Node *n, const char * name, const at::SparseTensorRef& value) { detail::badArgType(value); }
 void addInputs(Node *n, const char * name, at::Generator * value)            { detail::badArgType(value); }
 void addInputs(Node *n, const char * name, at::ScalarType value)             { detail::badArgType(value); }
