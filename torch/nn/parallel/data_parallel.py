de460c7ad312a1f30d6329406b9b2fb53c530a0a
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-09-11 08:39:20 -0700

    Improvements on conv/pool/fold/stft/ParamDict docs (#11106)
    Summary:
    Also fixes some incorrect formula rendering.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11106
    
    Differential Revision: D9752433
    
    Pulled By: SsnL
    
    fbshipit-source-id: 535fc8498638e8b645757fc7535d8771992b7d21

--- torch/nn/parallel/data_parallel.py
+++ torch/nn/parallel/data_parallel.py
@@ -36,9 +36,10 @@ class DataParallel(Module):
 
     This container parallelizes the application of the given module by
     splitting the input across the specified devices by chunking in the batch
-    dimension. In the forward pass, the module is replicated on each device,
-    and each replica handles a portion of the input. During the backwards
-    pass, gradients from each replica are summed into the original module.
+    dimension (other objects will be copied once per device). In the forward
+    pass, the module is replicated on each device, and each replica handles a
+    portion of the input. During the backwards pass, gradients from each replica
+    are summed into the original module.
 
     The batch size should be larger than the number of GPUs used.
 
