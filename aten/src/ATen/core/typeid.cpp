fdf90e0c103ab55b0d0c64d5fcbaccb1269e1356
Sebastian Messmer <messmer@fb.com>
2018-09-11 17:09:14 -0700

    V8: (no description)
    Differential Revision: D9763424
    Differential Version: 57563327

--- aten/src/ATen/core/typeid.cpp
+++ aten/src/ATen/core/typeid.cpp
@@ -44,4 +44,43 @@ TypeIdentifier TypeIdentifier::createTypeId() {
   return TypeIdentifier(new_value);
 }
 
+CAFFE_DEFINE_KNOWN_TYPE(float);
+CAFFE_DEFINE_KNOWN_TYPE(int);
+CAFFE_DEFINE_KNOWN_TYPE(std::string);
+CAFFE_DEFINE_KNOWN_TYPE(bool);
+CAFFE_DEFINE_KNOWN_TYPE(uint8_t);
+CAFFE_DEFINE_KNOWN_TYPE(int8_t);
+CAFFE_DEFINE_KNOWN_TYPE(uint16_t);
+CAFFE_DEFINE_KNOWN_TYPE(int16_t);
+CAFFE_DEFINE_KNOWN_TYPE(int64_t);
+CAFFE_DEFINE_KNOWN_TYPE(double);
+CAFFE_DEFINE_KNOWN_TYPE(char);
+CAFFE_DEFINE_KNOWN_TYPE(std::unique_ptr<std::mutex>);
+CAFFE_DEFINE_KNOWN_TYPE(std::unique_ptr<std::atomic<bool>>);
+CAFFE_DEFINE_KNOWN_TYPE(std::vector<int32_t>);
+CAFFE_DEFINE_KNOWN_TYPE(std::vector<int64_t>);
+CAFFE_DEFINE_KNOWN_TYPE(std::vector<unsigned long>);
+CAFFE_DEFINE_KNOWN_TYPE(bool*);
+CAFFE_DEFINE_KNOWN_TYPE(char*);
+CAFFE_DEFINE_KNOWN_TYPE(int*);
+
+#ifdef CAFFE2_UNIQUE_LONG_TYPEMETA
+CAFFE_DEFINE_KNOWN_TYPE(long);
+CAFFE_DEFINE_KNOWN_TYPE(std::vector<long>);
+#endif // CAFFE2_UNIQUE_LONG_TYPEMETA
+
+CAFFE_DEFINE_KNOWN_TYPE(_CaffeHighestPreallocatedTypeId);
+
+namespace {
+// This single registerer exists solely for us to be able to name a TypeMeta
+// for unintializied blob. You should not use this struct yourself - it is
+// intended to be only instantiated once here.
+struct UninitializedTypeNameRegisterer {
+  UninitializedTypeNameRegisterer() {
+    gTypeNames()[TypeIdentifier::uninitialized()] = "nullptr (uninitialized)";
+  }
+};
+static UninitializedTypeNameRegisterer g_uninitialized_type_name_registerer;
+
+} // namespace
 } // namespace caffe2
