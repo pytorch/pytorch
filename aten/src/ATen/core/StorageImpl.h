ac27459664e4ed8576f085e28f274a5b0dd91d07
Christian Puhrsch <cpuhrsch@fb.com>
2018-09-12 13:36:54 -0700

    move constructors

--- aten/src/ATen/core/StorageImpl.h
+++ aten/src/ATen/core/StorageImpl.h
@@ -17,13 +17,31 @@ struct AT_API StorageImpl : public c10::intrusive_ptr_target {
       int64_t numel,
       at::DataPtr data_ptr,
       at::Allocator* allocator,
-      bool resizable);
+      bool resizable)
+      : data_type_(data_type),
+        data_ptr_(std::move(data_ptr)),
+        numel_(numel),
+        resizable_(resizable),
+        allocator_(allocator) {
+    if (numel > 0) {
+      AT_ERROR(
+          data_type.id() != caffe2::TypeIdentifier::uninitialized(),
+          "Constructing a storage with meta of unknown type and non-zero \
+        numel");
+    }
+  }
 
   StorageImpl(
       caffe2::TypeMeta data_type,
       int64_t numel,
       at::Allocator* allocator,
-      bool resizable);
+      bool resizable)
+      : StorageImpl(
+            data_type,
+            numel,
+            allocator->allocate(data_type.itemsize() * numel),
+            allocator,
+            resizable) {}
 
   explicit StorageImpl(at::DeviceType device_type)
       : StorageImpl(device_type, caffe2::TypeMeta()) {}
