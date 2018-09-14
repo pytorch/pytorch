5ed06c130be8762713664c16ac456ac6d942516e
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-09-14 16:27:37 -0400

    final fix to expose _backend

--- torch/distributed/__init__.py
+++ torch/distributed/__init__.py
@@ -2,12 +2,16 @@ import torch
 
 
 def is_available():
-    return hasattr(torch._C, "_c10d_init")
+	return hasattr(torch._C, "_c10d_init")
 
 
 if is_available() and not torch._C._c10d_init():
-    raise RuntimeError("Failed to initialize PyTorch distributed support")
+	raise RuntimeError("Failed to initialize PyTorch distributed support")
 
 
 if is_available():
-    from .distributed_c10d import *
+	from .distributed_c10d import *
+	# Variables prefixed with underscore are not auto imported
+	# See the comment in `distributed_c10d.py` above `_backend` on why we expose
+	# this.
+	from .distributed_c10d import _backend
