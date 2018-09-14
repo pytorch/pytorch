70e68e755a8fdd94218e9b8e0abfb6b805354a08
David Riazati <davidriazati@fb.com>
2018-09-14 13:29:06 -0700

    Casting for binary ops (#11708)
    Summary:
    Fixes #11663
    
    `TensorIterator` was replacing the op tensors with type casted tensors
    which ended up producing side effects in binary ops like `a.float() * b`
    where `a` and `b` are `LongTensor`s.
    
    colesbury ezyang apaszke
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11708
    
    Differential Revision: D9834016
    
    Pulled By: driazati
    
    fbshipit-source-id: 4082eb9710b31dfc741161a0fbdb9a8eba8fe39d

--- aten/src/ATen/native/TensorIterator.cpp
+++ aten/src/ATen/native/TensorIterator.cpp
@@ -98,7 +98,8 @@ void TensorIterator::compute_common_type() {
       if (op.tensor->defined() && type != op.tensor->type()) {
         if (op.tensor->dim() == 0) {
           if (type.backend() != at::Backend::CUDA) {
-            *op.tensor = op.tensor->toType(type);
+            cast_tensors_.emplace_back(op.tensor->toType(type));
+            op.tensor = &(cast_tensors_.back());
           }
         } else {
           op.needs_cast = true;
