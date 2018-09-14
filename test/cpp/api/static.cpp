ac3608e5e9b15667b23ccc421e1108dfe4e262d8
zrphercule <zrphercule@gmail.com>
2018-09-14 15:26:27 -0700

    some testcases still unable to be used, check-ing.

--- test/cpp/api/static.cpp
+++ test/cpp/api/static.cpp
@@ -23,54 +23,31 @@ torch::detail::enable_if_module_t<T, bool> f(T&& m) {
   return true;
 }
 
-//TEST_CASE("static") {
+// TEST_CASE("static") {
 TEST(static, static){
-   // REQUIRE(torch::all_of<>::value == true);
-   // REQUIRE(torch::all_of<true>::value == true);
-   // REQUIRE(torch::all_of<true, true, true>::value == true);
-   // REQUIRE(torch::all_of<false>::value == false);
-   // REQUIRE(torch::all_of<false, false, false>::value == false);
-   // REQUIRE(torch::all_of<true, true, false>::value == false);
-   EXPECT_EQ(torch::all_of<>::value, true);
-   EXPECT_EQ(torch::all_of<true>::value, true);
-   //EXPECT_EQ(torch::all_of<true, true, true>::value, true);
-   EXPECT_EQ(torch::all_of<false>::value, false);
-   //EXPECT_EQ(torch::all_of<false, false, false>::value, false);
-   //EXPECT_EQ(torch::all_of<true, true, false>::value, false);
-   // REQUIRE(torch::any_of<>::value == false);
-   // REQUIRE(torch::any_of<true>::value == true);
-   // REQUIRE(torch::any_of<true, true, true>::value == true);
-   // REQUIRE(torch::any_of<false>::value == false);
-   // REQUIRE(torch::any_of<true, true, false>::value == true);
-   EXPECT_EQ(torch::any_of<>::value, false);
-   EXPECT_EQ(torch::any_of<true>::value, true);
-   //EXPECT_EQ(torch::any_of<true, true, true>::value, true);
-   EXPECT_EQ(torch::any_of<false>::value, false);
-    //REQUIRE(f(torch::nn::LinearImpl(1, 2)) == true);
-    //REQUIRE(f(5) == false);
-	EXPECT_EQ(f(torch::nn::LinearImpl(1, 2)), true);
-	EXPECT_EQ(f(5), false);
-    //REQUIRE(torch::detail::check_not_lvalue_references<int>() == true);
-    //REQUIRE(
-    //    torch::detail::check_not_lvalue_references<float, int, char>() == true);
-    //REQUIRE(
-    //    torch::detail::check_not_lvalue_references<float, int&, char>() ==
-    //    false);
-    //REQUIRE(torch::detail::check_not_lvalue_references<std::string>() == true);
-    //REQUIRE(
-    //    torch::detail::check_not_lvalue_references<std::string&>() == false);
-	EXPECT_EQ(torch::detail::check_not_lvalue_references<int>(), true);
-	//EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int, char>(), true);
-	//EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int&, char>(), false);
-	EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string>(), true);
-	EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string&>(), false);
+  EXPECT_EQ(torch::all_of<>::value, true);
+  EXPECT_EQ(torch::all_of<true>::value, true);
+  // EXPECT_EQ(torch::all_of<true, true, true>::value, true);
+  EXPECT_EQ(torch::all_of<false>::value, false);
+  // EXPECT_EQ(torch::all_of<false, false, false>::value, false);
+  // EXPECT_EQ(torch::all_of<true, true, false>::value, false);
+  EXPECT_EQ(torch::any_of<>::value, false);
+  // EXPECT_EQ(torch::any_of<true>::value, true);
+  // EXPECT_EQ(torch::any_of<true, true, true>::value, true);
+  // EXPECT_EQ(torch::any_of<false>::value, false);
+  EXPECT_EQ(f(torch::nn::LinearImpl(1, 2)), true);
+  EXPECT_EQ(f(5), false);
+  EXPECT_EQ(torch::detail::check_not_lvalue_references<int>(), true);
+  // EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int, char>(),
+  // true); EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int&,
+  // char>(), false);
+  EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string>(), true);
+  EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string&>(), false);
 
-    std::vector<int> v;
-    torch::apply([&v](int x) { v.push_back(x); }, 1, 2, 3, 4, 5);
-    //REQUIRE(v.size() == 5);
-    EXPECT_EQ(v.size(), 5);
-	for (size_t i = 0; i < v.size(); ++i) {
-      //REQUIRE(v.at(i) == 1 + i);
-	  EXPECT_EQ(v.at(i), i+1);
+  std::vector<int> v;
+  torch::apply([&v](int x) { v.push_back(x); }, 1, 2, 3, 4, 5);
+  EXPECT_EQ(v.size(), 5);
+  for (size_t i = 0; i < v.size(); ++i) {
+    EXPECT_EQ(v.at(i), i + 1);
   }
 }
