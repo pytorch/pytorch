2d6c2d782639a9a12e82eb3802714a709d05a669
zrphercule <zrphercule@gmail.com>
2018-09-14 16:43:25 -0700

    3 test cases skipped

--- test/cpp/api/static.cpp
+++ test/cpp/api/static.cpp
@@ -27,20 +27,21 @@ torch::detail::enable_if_module_t<T, bool> f(T&& m) {
 TEST(static, static){
   EXPECT_EQ(torch::all_of<>::value, true);
   EXPECT_EQ(torch::all_of<true>::value, true);
-  // EXPECT_EQ(torch::all_of<true, true, true>::value, true);
+  EXPECT_EQ((torch::all_of<true, true, true>::value), true);
   EXPECT_EQ(torch::all_of<false>::value, false);
-  // EXPECT_EQ(torch::all_of<false, false, false>::value, false);
-  // EXPECT_EQ(torch::all_of<true, true, false>::value, false);
+  EXPECT_EQ((torch::all_of<false, false, false>::value), false);
+  EXPECT_EQ((torch::all_of<true, true, false>::value), false);
   EXPECT_EQ(torch::any_of<>::value, false);
-  // EXPECT_EQ(torch::any_of<true>::value, true);
-  // EXPECT_EQ(torch::any_of<true, true, true>::value, true);
-  // EXPECT_EQ(torch::any_of<false>::value, false);
+  // EXPECT_EQ((torch::any_of<true>::value), true);
+  // EXPECT_EQ((torch::any_of<true, true, true>::value), true);
+  // EXPECT_EQ((torch::any_of<false>::value), false);
   EXPECT_EQ(f(torch::nn::LinearImpl(1, 2)), true);
   EXPECT_EQ(f(5), false);
   EXPECT_EQ(torch::detail::check_not_lvalue_references<int>(), true);
-  // EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int, char>(),
-  // true); EXPECT_EQ(torch::detail::check_not_lvalue_references<float, int&,
-  // char>(), false);
+  EXPECT_EQ(
+      (torch::detail::check_not_lvalue_references<float, int, char>()), true);
+  EXPECT_EQ(
+      (torch::detail::check_not_lvalue_references<float, int&, char>()), false);
   EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string>(), true);
   EXPECT_EQ(torch::detail::check_not_lvalue_references<std::string&>(), false);
 
