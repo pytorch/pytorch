cdc44e502e87cf50e6b911031abff8d8047a5a4f
Brian Johnson <brianjo@fb.com>
2018-09-18 08:47:15 -0700

    Updated C++ and requirements.txt

--- docs/cpp/conf.py
+++ docs/cpp/conf.py
@@ -136,14 +136,15 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'pytorch_sphinx_theme'
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
+    'pytorch_project': 'docs',
     'canonical_url': 'https://pytorch.org/docs/stable/',
     'collapse_navigation': False,
     'display_version': True,
@@ -158,12 +159,12 @@ html_logo = '../source/_static/img/pytorch-logo-dark-unstable.png'
 html_static_path = ['../source/_static']
 
 # html_style_path = 'css/pytorch_theme.css'
-html_context = {
-    'css_files': [
-        'https://fonts.googleapis.com/css?family=Lato',
-        '_static/css/pytorch_theme.css'
-    ],
-}
+# html_context = {
+#    'css_files': [
+#        'https://fonts.googleapis.com/css?family=Lato',
+#        '_static/css/pytorch_theme.css'
+#    ],
+#}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
