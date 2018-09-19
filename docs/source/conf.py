16076b7a1543893a5eb7050bd62419ec6e37931a
Brian Johnson <brianjo@fb.com>
2018-09-18 19:24:35 -0700

    Fixed lint issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,14 +142,6 @@ html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-#html_theme_options = {
-#    'pytorch_project': 'docs',
-#    'canonical_url': 'https://pytorch.org/docs/stable/',
-#    'collapse_navigation': False,
-#    'display_version': True,
-#    'logo_only': True,
-#}
 
 html_logo = '_static/img/pytorch-logo-dark-unstable.png'
 if RELEASE:
