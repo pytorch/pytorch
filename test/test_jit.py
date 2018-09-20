068eac255b088c55cfdff55e73d7d24517c5e801
Thomas Viehmann <tv.code@beamnet.de>
2018-09-20 14:27:02 -0700

    Jit fuse clamp (#11574)
    Summary:
    This patch adds fused forward and backward for clamp to the jit.
    This is one item of #11118 . If it's OK, I'd be happy to also add some more of #11118 .
    
    The patch depends on #11150 , which I merged into master as a base. I'll rebase it when that or #10981 is merged.
    
    This is first serious jit patch, thank you, ngimel and the others for their guidance. All errors are my own.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11574
    
    Differential Revision: D9943090
    
    Pulled By: apaszke
    
    fbshipit-source-id: c40954b8c28c374baab8d3bd89acc9250580dc67

--- test/test_jit.py
+++ test/test_jit.py
@@ -366,7 +366,10 @@ class JitTestCase(TestCase):
         return ge
 
     def assertAllFused(self, graph):
-        self.assertTrue(all(node.kind() in {'prim::Constant', 'prim::FusionGroup'} for node in graph.nodes()))
+        if [n.kind() for n in graph.nodes()] == ['prim::DifferentiableGraph']:
+            graph = next(graph.nodes()).g('Subgraph')
+        self.assertTrue(all(node.kind() in {'prim::Constant', 'prim::FusionGroup'} for node in graph.nodes()),
+                        'got {}'.format(graph))
         self.assertTrue([node.kind() for node in graph.nodes()].count('prim::FusionGroup') == 1)
 
     def assertExportImport(self, trace, inputs):
@@ -2690,6 +2693,23 @@ a")
         a = torch.rand(1, requires_grad=True)
         b = torch.rand(1, requires_grad=True)
         self.checkScript(func, (a, b), optimize=True)
+
+    @unittest.skipIf(IS_WINDOWS, "NYI: fuser support for Windows")
+    @unittest.skipIf(not RUN_CUDA, "fuser requires CUDA")
+    @skipIfRocm
+    def test_clamp_fusion(self):
+        def func(a, b):
+            return torch.clamp(a + b, min=0, max=2)
+
+        a = torch.randn(4, 4, dtype=torch.float, device='cuda', requires_grad=True)
+        b = torch.randn(4, 4, dtype=torch.float, device='cuda')
+
+        s = self.checkScript(func, (a, b))
+        self.assertAllFused(s.graph_for(a, b))
+
+        c = s(a, b)
+        c.sum().backward()
+        self.assertAllFused(backward_graph(s))
 
     def test_mul(self):
         def func(a, b):
