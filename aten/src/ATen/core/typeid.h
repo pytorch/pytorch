61f5de74c97ea30a62e7bdfae39b4166ffc33085
Sebastian Messmer <messmer@fb.com>
2018-09-21 14:17:23 -0700

    V29: (no description)
    Differential Revision: D9763422
    Differential Version: 58574565

--- aten/src/ATen/core/typeid.h
+++ aten/src/ATen/core/typeid.h
@@ -205,7 +205,7 @@ const char* __TypeName() noexcept;
 
 template <class T>
 const char* _TypeName() noexcept {
-  static constexpr const char* literal_name = __TypeName<T>();
+  static const char* literal_name = __TypeName<T>();
 #ifdef __GXX_RTTI
   static const std::string name = at::demangle(typeid(T).name());
   return name.c_str();
