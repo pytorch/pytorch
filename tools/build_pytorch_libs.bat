ae2aee67550ee1b7bb12a504e156a9243bc1c806
Zeming Lin <ebetica0@gmail.com>
2018-09-21 01:27:07 -0400

    Multithreaded windows build without Ninja

--- tools/build_pytorch_libs.bat
+++ tools/build_pytorch_libs.bat
@@ -74,7 +74,7 @@ IF NOT DEFINED BUILD_SHARED_LIBS (
 
 IF "%CMAKE_GENERATOR%"=="" (
   set CMAKE_GENERATOR_COMMAND=
-  set MAKE_COMMAND=msbuild INSTALL.vcxproj /p:Configuration=Release
+  set MAKE_COMMAND=msbuild INSTALL.vcxproj /p:Configuration=Release /m
 ) ELSE (
   set CMAKE_GENERATOR_COMMAND=-G "%CMAKE_GENERATOR%"
   IF "%CMAKE_GENERATOR%"=="Ninja" (
@@ -82,7 +82,7 @@ IF "%CMAKE_GENERATOR%"=="" (
     IF "%CXX%"== "" set CXX=cl.exe
     set MAKE_COMMAND=cmake --build . --target install --config %BUILD_TYPE% -- -j%MAX_JOBS%
   ) ELSE (
-    set MAKE_COMMAND=msbuild INSTALL.vcxproj /p:Configuration=%BUILD_TYPE%
+    set MAKE_COMMAND=msbuild INSTALL.vcxproj /p:Configuration=%BUILD_TYPE% /m
   )
 )
 
@@ -135,7 +135,7 @@ goto:eof
                   -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" ^
                   -DCMAKE_C_FLAGS="%C_FLAGS%" ^
                   -DCMAKE_SHARED_LINKER_FLAGS="%LINK_FLAGS%" ^
-                  -DCMAKE_CXX_FLAGS="%C_FLAGS% %CPP_FLAGS%" ^
+                  -DCMAKE_CXX_FLAGS="/MT %C_FLAGS% %CPP_FLAGS%" ^
                   -DCUDA_NVCC_FLAGS="%BASIC_CUDA_FLAGS%" ^
                   -Dcwrap_files="%CWRAP_FILES%" ^
                   -DTH_INCLUDE_PATH="%INSTALL_DIR%/include" ^
@@ -202,7 +202,7 @@ goto:eof
                   -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" ^
                   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ^
                   -DCMAKE_C_FLAGS="%USER_CFLAGS%" ^
-                  -DCMAKE_CXX_FLAGS="/EHa %USER_CFLAGS%" ^
+                  -DCMAKE_CXX_FLAGS="/EHa /MT %USER_CFLAGS%" ^
                   -DCMAKE_EXE_LINKER_FLAGS="%USER_LDFLAGS%" ^
                   -DCMAKE_SHARED_LINKER_FLAGS="%USER_LDFLAGS%" ^
                   -DUSE_ROCM=%USE_ROCM%
