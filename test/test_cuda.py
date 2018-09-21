1cf5b0c7c19f55411a01c936fb265dd6b7847392
Sam Gross <sgross@fb.com>
2018-09-21 13:49:16 -0700

    Fix casting logic for 0d CPU tensors in CUDA ops (#11808)
    Summary:
    Previously, we didn't cast any 0-dim tensors used in CUDA operations. We
    can only avoid the casts for 0-dim CPU tensors used in CUDA operations.
    
    Fixes #11795
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11808
    
    Differential Revision: D9922406
    
    Pulled By: colesbury
    
    fbshipit-source-id: 940b8a8534770aa5cd70d5d09b96be0f0f8146ff

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -161,6 +161,10 @@ def constant_tensor_add(a, b):
         return a + b
 
 
+def small_0d(t):
+    return make_tensor(t, (1,)).squeeze()
+
+
 def small_2d(t):
     return make_tensor(t, S, S)
 
@@ -258,6 +262,7 @@ tests = [
     ('mul', small_3d, lambda t: [number(3.14, 3, t)], '', types, False,
         "skipIfRocm:ByteTensor,CharTensor,HalfTensor,ShortTensor"),
     ('mul', small_3d, lambda t: [small_3d_positive(t)], 'tensor'),
+    ('mul', small_0d, lambda t: [small_0d(torch.IntTensor)], 'scalar', types, True),
     ('div', small_3d, lambda t: [number(3.14, 3, t)], '', types, False,
         "skipIfRocm:ByteTensor,CharTensor,FloatTensor,HalfTensor,ShortTensor"),
     ('div', small_3d, lambda t: [small_3d_positive(t)], 'tensor'),
@@ -902,6 +907,25 @@ class TestCuda(TestCase):
         self.assertIsInstance(y.cuda().float(), torch.cuda.FloatStorage)
         self.assertIsInstance(y.cuda().float().cpu(), torch.FloatStorage)
         self.assertIsInstance(y.cuda().float().cpu().int(), torch.IntStorage)
+
+    def test_mul_intertype_scalar(self):
+        x = torch.tensor(1.5, device='cuda')
+        y = torch.tensor(3, dtype=torch.int32, device='cuda')
+
+        self.assertEqual(x * y, 4.5)
+        self.assertEqual(y * x, 4.5)
+        with self.assertRaisesRegex(RuntimeError, 'expected type'):
+            y *= x
+        x *= y
+        self.assertEqual(x, 4.5)
+
+        x = torch.tensor(1.5, device='cuda', dtype=torch.float16)
+        self.assertEqual(x * y, 4.5)
+        # half * int currently promotes to double
+        with self.assertRaisesRegex(RuntimeError, 'expected type'):
+            x *= y
+        with self.assertRaisesRegex(RuntimeError, 'expected type'):
+            y *= x
 
     @unittest.skipIf(not TEST_MULTIGPU, "only one GPU detected")
     @skipIfRocm
