3ae6ee4ebded136da30aa53fd3873d84acfbc9f0
Jerry Zhang <jerryzh@fb.com>
2018-09-24 16:49:27 -0700

    Move CreateContext to global registry (#11688)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/11688
    
    As a first step to remove static context(merge with allocator), we'll create a
    global registries for context constructors, and remove CreateContext function from tensor.
    
    Reviewed By: ezyang, dzhulgakov
    
    Differential Revision: D9779821
    
    fbshipit-source-id: 8b239ea50af7a0556fde2382f58f79194f0e3dc1

--- aten/src/ATen/core/context_base.h
+++ aten/src/ATen/core/context_base.h
@@ -6,11 +6,12 @@
 #include <memory>
 #include <unordered_map>
 
-#include <ATen/core/DeviceType.h>
+#include <ATen/core/ATenGeneral.h>
+#include <ATen/core/Device.h>
 #include <ATen/core/Error.h>
+#include <ATen/core/Registry.h>
 #include <ATen/core/UniqueVoidPtr.h>
 #include <ATen/core/typeid.h>
-#include <ATen/core/ATenGeneral.h>
 
 namespace caffe2 {
 class Event;
@@ -30,11 +31,6 @@ class AT_CORE_API BaseStaticContext {
   virtual ~BaseStaticContext() noexcept {}
 
   virtual std::pair<void*, DeleterFnPtr> New(size_t nbytes) const = 0;
-
-  virtual std::unique_ptr<BaseContext> CreateContext() = 0;
-
-  virtual std::unique_ptr<BaseContext> CreateContext(
-      const caffe2::DeviceOption&) = 0;
 
   virtual DeviceType GetDeviceType() = 0;
 
@@ -183,6 +179,22 @@ class AT_CORE_API BaseContext {
     }
   }
 };
+
+// Context constructor registry
+AT_DECLARE_TYPED_REGISTRY(
+    ContextRegistry,
+    at::DeviceType,
+    BaseContext,
+    std::unique_ptr,
+    at::Device);
+
+#define REGISTER_CONTEXT(type, ...) \
+  AT_REGISTER_TYPED_CLASS(ContextRegistry, type, __VA_ARGS__)
+
+inline std::unique_ptr<at::BaseContext> CreateContext(
+    const at::Device& device) {
+  return ContextRegistry()->Create(device.type(), device);
+}
 
 } // namespace at
 
--- caffe2/core/tensor.h
+++ caffe2/core/tensor.h
@@ -130,12 +130,12 @@ class CAFFE2_API Tensor final {
     return impl_.get()->GetStaticContext();
   }
 
-  std::unique_ptr<BaseContext> CreateContext() const {
-    return impl_.get()->CreateContext();
-  }
-
   DeviceType GetDeviceType() const {
     return impl_.get()->GetDeviceType();
+  }
+
+  at::Device GetDevice() const {
+    return impl_.get()->GetDevice();
   }
 
   void CopyFrom(const Tensor& src, BaseContext* context = nullptr) const {
