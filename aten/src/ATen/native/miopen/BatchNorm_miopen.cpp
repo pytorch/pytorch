3b80983de0a379736bac62291e8300b4bfa87755
Gregory Chanan <gchanan@fb.com>
2018-09-24 15:11:00 -0700

    Fix typo.

--- aten/src/ATen/native/miopen/BatchNorm_miopen.cpp
+++ aten/src/ATen/native/miopen/BatchNorm_miopen.cpp
@@ -177,7 +177,7 @@ std::tuple<Tensor, Tensor, Tensor> miopen_batch_norm_backward(
     mode = miopenBNSpatial;
   }
 
-  auto grad_input_t  = at::empty(input->sizes(), input->optiions());
+  auto grad_input_t  = at::empty(input->sizes(), input->options());
   auto grad_weight_t = at::empty(weight->sizes(), weight->options());
   auto grad_bias_t   = at::empty(weight->sizes(), weight->options());
 
