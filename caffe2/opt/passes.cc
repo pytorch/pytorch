b3789a77366f83ca7071f6a385666aa81475a577
Edward Z. Yang <ezyang@fb.com>
2018-09-25 12:52:36 -0700

    V10: one more ns fix
    Differential Revision: D10032707
    Differential Version: 58834886

--- caffe2/opt/passes.cc
+++ caffe2/opt/passes.cc
@@ -1,4 +1,4 @@
 #include "caffe2/opt/passes.h"
 
-C10_DEFINE_REGISTRY(WorkspaceOptimizationPassRegistry, caffe2::WorkspaceOptimizationPass, caffe2::NNModule*, caffe2::Workspace*);
-C10_DEFINE_REGISTRY(OptimizationPassRegistry, caffe2::OptimizationPass, caffe2::NNModule*);
+C10_DEFINE_REGISTRY(WorkspaceOptimizationPassRegistry, caffe2::WorkspaceOptimizationPass, nom::repr::NNModule*, caffe2::Workspace*);
+C10_DEFINE_REGISTRY(OptimizationPassRegistry, caffe2::OptimizationPass, nom::repr::NNModule*);
