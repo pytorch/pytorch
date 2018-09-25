3deb4791c3fd6e4ce63b708006e75fa43fe3d971
Edward Yang <ezyang@fb.com>
2018-09-25 09:42:41 -0700

    Replace 'struct Tensor' with 'class Tensor'. (#12034)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12034
    
    We need ATen and Caffe2 to line up, and the rule is
    that if you have any private/protected members, you
    should declare it as a class.  Class we go.
    
    (There are some other obvious candidates for this treatment,
    but I've kept this patch just to Tensor)
    
    Reviewed By: gchanan, mingzhe09088
    
    Differential Revision: D10024467
    
    fbshipit-source-id: 17cfe2741ba9c3f56cb87d6f5d1afd3c61a8e4fe

--- aten/src/ATen/core/Tensor.h
+++ aten/src/ATen/core/Tensor.h
@@ -15,7 +15,7 @@
 namespace at {
 struct Generator;
 struct Type;
-struct Tensor;
+class Tensor;
 struct TensorOptions;
 } // namespace at
 
@@ -37,7 +37,8 @@ namespace at {
 //
 // Note that Tensor can also be NULL, i.e. it is not associated with any underlying TensorImpl, and
 // special care must be taken to handle this.
-struct CAFFE2_API Tensor {
+class CAFFE2_API Tensor {
+public:
   Tensor(){};
   Tensor(c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl> tensor_impl)
       : impl_(std::move(tensor_impl)) {
