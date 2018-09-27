732473e7484ae628b5e3998157ae6a25bc72a326
Jerry Zhang <jerryzh168@gmail.com>
2018-09-27 12:11:42 -0700

    V6: .
    Differential Revision: D10033396
    Differential Version: 59088508

--- aten/src/ATen/core/context_base.h
+++ aten/src/ATen/core/context_base.h
@@ -9,9 +9,9 @@
 #include <ATen/core/ATenGeneral.h>
 #include <ATen/core/Device.h>
 #include <ATen/core/Error.h>
-#include <ATen/core/Registry.h>
 #include <ATen/core/UniqueVoidPtr.h>
 #include <ATen/core/typeid.h>
+#include <c10/util/Registry.h>
 
 namespace caffe2 {
 class Event;
