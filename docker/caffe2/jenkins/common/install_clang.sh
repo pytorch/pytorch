5d045bc1ef5fb88bab7d405d01053bf46279c12f
Junjie Bai <bai@in.tum.de>
2018-09-28 14:41:17 -0700

    .

--- docker/caffe2/jenkins/common/install_clang.sh
+++ docker/caffe2/jenkins/common/install_clang.sh
@@ -4,15 +4,14 @@ set -ex
 
 [ -n "$CLANG_VERSION" ]
 
-apt-get update
-
 if [[ "$CLANG_VERSION" == "7" ]]; then
-  apt-get install -y --no-install-recommends software-properties-common
-  apt-get install wget
+  apt-get update
+  apt-get install -y --no-install-recommends software-properties-common wget
   wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
   apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
 fi
 
+apt-get update
 apt-get install -y --no-install-recommends clang-"$CLANG_VERSION"
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
