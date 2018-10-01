0d4a3cc45fcd3b6a52c027ecb86d1e08fee5c67f
Edward Z. Yang <ezyang@fb.com>
2018-10-01 15:14:59 -0700

    V3: one more buildfix
    Differential Revision: D10134319
    Differential Version: 59385053

--- aten/src/ATen/core/TensorImpl.cpp
+++ aten/src/ATen/core/TensorImpl.cpp
@@ -85,9 +85,9 @@ int64_t TensorImpl::size(int64_t d) const {
 }
 
 int64_t TensorImpl::stride(int64_t d) const {
-  AT_ASSERT(strides_.size() == sizes_.size(),
-            "Caffe2 tensors don't (yet) have meaningful strides and cannot "
-            "be used in PyTorch.");
+  AT_ASSERTM(strides_.size() == sizes_.size(),
+             "Caffe2 tensors don't (yet) have meaningful strides and cannot "
+             "be used in PyTorch.");
   d = at::maybe_wrap_dim(d, dim(), false);
   return strides_[d];
 }
