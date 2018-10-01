3010dc4208df1d2bd8058cfafeffe645f454595c
Rick Ratmansky <rratmansky@fb.com>
2018-10-01 12:09:39 -0700

    Revert D10123245: Back out "codemod cuda_gpu_id to device_id"
    Differential Revision:
    D10123245
    
    Original commit changeset: d83da8e00a12
    
    fbshipit-source-id: fca91fea58b7df208edc2e218a1d514f9821ec7b

--- caffe2/core/context_gpu.h
+++ caffe2/core/context_gpu.h
@@ -184,7 +184,7 @@ class CAFFE2_CUDA_API CUDAContext final : public BaseContext {
     }
   }
 
-  inline int cuda_gpu_id() const {
+  inline int device_id() const {
     return gpu_id_;
   }
 
@@ -283,7 +283,7 @@ class CAFFE2_CUDA_API CUDAContext final : public BaseContext {
   }
 
   static bool IsStreamFree(const DeviceOption& option, int stream_id) {
-    auto stream = CUDAContext::cuda_stream(option.cuda_gpu_id(), stream_id);
+    auto stream = CUDAContext::cuda_stream(option.device_id(), stream_id);
     return cudaStreamQuery(stream) == cudaSuccess;
   }
 
@@ -393,7 +393,7 @@ class CAFFE2_CUDA_API CUDAStaticContext final : public BaseStaticContext {
 
   void ExtractDeviceOption(DeviceOption* device, const void* data) override {
     device->set_device_type(TypeToProto(GetDeviceType()));
-    device->set_cuda_gpu_id(GetGPUIDForPointer(data));
+    device->set_device_id(GetGPUIDForPointer(data));
   }
 
  protected:
