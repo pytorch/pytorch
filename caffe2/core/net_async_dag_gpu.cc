26e1d386eb8a95fa526dfb6ab6a7594fdb5d6533
Junjie Bai <jbai@fb.com>
2018-10-09 13:54:20 -0700

    Rename cuda_gpu_id to device_id in DeviceOption (#12456)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12456
    
    codemod with 'Yes to all'
    codemod -d . --extensions h,cc,cpp,cu,py,proto,pbtxt,pb.txt,config cuda_gpu_id device_id
    
    Overload TextFormat::ParseFromString to do string replace when parsing from protobuf format
    
    Reviewed By: Yangqing
    
    Differential Revision: D10240535
    
    fbshipit-source-id: f1d6f208db36a89393625e4f7334242f10635272

--- caffe2/core/net_async_dag_gpu.cc
+++ caffe2/core/net_async_dag_gpu.cc
@@ -112,7 +112,7 @@ AsyncDAGNet::AsyncDAGNet(
 int AsyncDAGNet::stream(const DeviceOption& device_option) {
   int stream_id = 0;
   if (device_option.device_type() == PROTO_CUDA) {
-    int gpu_id = device_option.cuda_gpu_id();
+    int gpu_id = device_option.device_id();
     CAFFE_ENFORCE_GE(gpu_id, 0, "Invalid gpu id: " + caffe2::to_string(gpu_id));
     if ((unsigned)gpu_id >= stream_counters_.size()) {
       stream_counters_.resize(gpu_id + 1, 0);
