fc8c9fb4e6c842e8219c69d802dcf67b228ea282
Brian W. Hart <hartb@us.ibm.com>
2018-10-12 14:56:36 -0500

    test_proper_exit: fix indent to suit PEP8

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -650,7 +650,7 @@ class TestDataLoader(TestCase):
     @skipIfRocm
     def test_proper_exit(self):
         (r'''There might be ConnectionResetError or leaked semaphore warning '''
-        r'''(due to dirty process exit), but they are all safe to ignore''')
+         r'''(due to dirty process exit), but they are all safe to ignore''')
 
         # TODO: test the case where the pin_memory_thread triggers an
         #       error/fatal signal. I haven't found out how to properly do that.
