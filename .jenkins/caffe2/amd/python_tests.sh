da17731c9b16762de4fdc5f954111162d2267068
Ashish <ashish.farmer@amd.com>
2018-10-15 14:36:36 -0700

    Remove softmax test from ignore list

--- .jenkins/caffe2/amd/python_tests.sh
+++ .jenkins/caffe2/amd/python_tests.sh
@@ -14,7 +14,6 @@ rocm_ignore_test=()
 # need to debug
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/arg_ops_test.py")
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/piecewise_linear_transform_test.py")
-rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/softmax_ops_test.py")
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/unique_ops_test.py")
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/model_device_test.py")
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/data_parallel_model_test.py")
