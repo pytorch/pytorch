06c27ea59622ca7338cff7f57a1b57be520e36d6
Guillaume Huard <grj.huard@gmail.com>
2018-10-17 02:53:58 -0700

    Fix missing final activation in NLLLoss second example (#12703)
    Summary:
    Fixed the second example in NLLLoss.
    The LogSoftmax activation was missing after the convolution layer. Without this activation, the second example loss was sometimes negative.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12703
    
    Differential Revision: D10419694
    
    Pulled By: ezyang
    
    fbshipit-source-id: 98bfefd1050290dd5b29d3ce18fe075103db4674

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -185,10 +185,11 @@ class NLLLoss(_WeightedLoss):
         >>> loss = nn.NLLLoss()
         >>> # input is of size N x C x height x width
         >>> data = torch.randn(N, 16, 10, 10)
-        >>> m = nn.Conv2d(16, C, (3, 3))
+        >>> conv = nn.Conv2d(16, C, (3, 3))
+        >>> m = nn.LogSoftmax()
         >>> # each element in target has to have 0 <= value < C
         >>> target = torch.empty(N, 8, 8, dtype=torch.long).random_(0, C)
-        >>> output = loss(m(data), target)
+        >>> output = loss(m(conv(data)), target)
         >>> output.backward()
     """
 
