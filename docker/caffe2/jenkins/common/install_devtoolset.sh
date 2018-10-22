3d4e6e6ee0d14113c7fed16a1d6e487f725c3512
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-10-22 15:45:28 -0500

    Decouple CENTOS_VERSION from DEVTOOLSET_VERSION

--- docker/caffe2/jenkins/common/install_devtoolset.sh
+++ docker/caffe2/jenkins/common/install_devtoolset.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-[ -n "$CENTOS_VERSION" ]
+[ -n "$DEVTOOLSET_VERSION" ]
 
 yum install -y centos-release-scl
-yum install -y devtoolset-$CENTOS_VERSION
+yum install -y devtoolset-$DEVTOOLSET_VERSION
 
-echo "source scl_source enable devtoolset-$CENTOS_VERSION" >> /root/.bashrc
+echo "source scl_source enable devtoolset-$DEVTOOLSET_VERSION" >> /root/.bashrc
