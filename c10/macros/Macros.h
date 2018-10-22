963dcb436fc74b993f196c6a0c15bc40f528c02e
Edward Yang <ezyang@fb.com>
2018-10-22 16:24:03 -0700

    Add C10_LIKELY/C10_UNLIKELY macros (#12932)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12932
    
    I was looking at some assembly for some code I was working on,
    and felt a desire to have likely()/unlikely() macros.  I checked
    if we already had them, and we didn't.  This commit adds them,
    and fixes up all known use sites to make use of it.
    
    Reviewed By: Maratyszcza
    
    Differential Revision: D10488399
    
    fbshipit-source-id: 7476da208907480d49f02b37c7345c17d85c3db7

--- c10/macros/Macros.h
+++ c10/macros/Macros.h
@@ -61,4 +61,24 @@ namespace c10 {} // namespace c10
 #define C10_NORETURN __attribute__((noreturn))
 #endif
 
+// C10_LIKELY/C10_UNLIKELY
+//
+// These macros provide parentheses, so you can use these macros as:
+//
+//    if C10_LIKELY(some_expr) {
+//      ...
+//    }
+//
+// NB: static_cast to boolean is mandatory in C++, because __builtin_expect
+// takes a long argument, which means you may trigger the wrong conversion
+// without it.
+//
+#if defined(__GNUC__) || defined(__ICL) || defined(__clang__)
+#define C10_LIKELY(expr)    (__builtin_expect(static_cast<bool>(expr), 1))
+#define C10_UNLIKELY(expr)  (__builtin_expect(static_cast<bool>(expr), 0))
+#else
+#define C10_LIKELY(expr)    (expr)
+#define C10_UNLIKELY(expr)  (expr)
+#endif
+
 #endif // C10_MACROS_MACROS_H_
