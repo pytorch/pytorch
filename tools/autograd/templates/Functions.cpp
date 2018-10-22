5b087bbde56cdfc1008a6f9861c03a1d5ff91470
kshitij12345 <kshitijkalambarkar@gmail.com>
2018-10-22 21:41:20 +0530

    move embd_dense_double_backwar to templates/Functions

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -2083,6 +2083,21 @@ Tensor constant_pad_nd_backward(const Tensor& grad, IntArrayRef pad) {
   return at::constant_pad_nd(grad, negated_pad, 0);
 }
 
+Tensor embedding_dense_double_backward(const Tensor & grad, const Tensor & indices) {
+  // since first backward takes care of padding_idx
+  // and scaling by frequency, we don't need to worry
+  // about it here.
+  auto grad_weight = grad.index_select(0, indices.reshape(-1));
+
+  // reshape gradient as per the shape of indices
+  auto size = indices.sizes().vec();
+  size.push_back(-1);
+  grad_weight = grad_weight.view(size);
+
+
+  return grad_weight;
+}
+
 } // anonymous namespace
 
 ${autograd_function_definitions}
