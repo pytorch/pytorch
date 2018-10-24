240bd8286069726293a7671c2edf927244d197a9
Alex Şuhan <asuhan@google.com>
2018-10-24 13:49:49 -0700

    Add autodiff support for a few additional operators
    Added aten::{avg_pool2d, log_softmax, max_pool2d_with_indices, threshold},
    enabled aten::{expand, view}.

--- test/cpp/jit/tests.h
+++ test/cpp/jit/tests.h
@@ -40,6 +40,7 @@
 #include "torch/csrc/jit/interpreter.h"
 #include "torch/csrc/jit/ir.h"
 #include "torch/csrc/jit/operator.h"
+#include "torch/csrc/jit/passes/constant_propagation.h"
 #include "torch/csrc/jit/passes/create_autodiff_subgraphs.h"
 #include "torch/csrc/jit/passes/dead_code_elimination.h"
 #include "torch/csrc/jit/passes/lower_grad_of.h"
@@ -583,6 +584,12 @@ void testADFormulas() {
        unary_pointwise,
        [](const VL& v) -> VL { return {v[0].tanh()}; }},
       {"t", unary_pointwise_2d, [](const VL& v) -> VL { return {v[0].t()}; }},
+      {"view",
+        unary_pointwise_2d,
+        [](const VL& v) -> VL { return {v[0].view({3, 2})}; }},
+      {"expand",
+        {{2, 1}},
+        [](const VL& v) -> VL { return {v[0].expand({2, 3})}; }},
       {"mm",
        {{10, 12}, {12, 15}},
        [](const VL& v) -> VL { return {v[0].mm(v[1])}; }},
@@ -607,6 +614,7 @@ void testADFormulas() {
     // Trace and differentiate the op
     auto graph = trace(test, vars_in);
     EliminateDeadCode(graph); // Tracing of some ops depends on the DCE trick
+    ConstantPropagation(graph);
     auto grad_spec = differentiate(graph);
     LowerGradOf(*grad_spec.df);
     // Get outputs from the interpreter
