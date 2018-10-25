b225c0b6d11439034a75afb5e5d6f554ad5cd27f
Zach DeVito <zdevito@fb.com>
2018-10-25 15:38:51 -0700

    fbcode fix?

--- test/cpp/jit/no-gtest.cpp
+++ test/cpp/jit/no-gtest.cpp
@@ -29,19 +29,5 @@ std::string runJITCPPTests() {
   return out.str();
 }
 
-
-at::Tensor leaky_relu(at::Tensor tensor, double scalar) {
-  return at::leaky_relu(tensor, scalar);
-}
-at::Tensor cat(std::vector<at::Tensor> tensors) {
-  return at::cat(tensors);
-}
-
-static auto registry =
-    torch::jit::RegisterOperators()
-        .op("_test::leaky_relu(Tensor self, float v=0.01) -> Tensor", &leaky_relu)
-        .op("_test::cat(Tensor[] inputs) -> Tensor", &cat);
-
-
 } // namespace jit
 } // namespace torch
