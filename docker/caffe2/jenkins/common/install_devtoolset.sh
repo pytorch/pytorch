488172ec43459979113ee45c58ea2c36e6616749
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-10-25 15:01:08 -0500

    Source devtoolset also in /etc/profile.
    Remove scripts after use.
    
    use devtool_version

--- docker/caffe2/jenkins/common/install_devtoolset.sh
+++ docker/caffe2/jenkins/common/install_devtoolset.sh
@@ -8,3 +8,4 @@ yum install -y centos-release-scl
 yum install -y devtoolset-$DEVTOOLSET_VERSION
 
 echo "source scl_source enable devtoolset-$DEVTOOLSET_VERSION" >> /root/.bashrc
+echo "source scl_source enable devtoolset-$DEVTOOLSET_VERSION" >> /etc/profile
