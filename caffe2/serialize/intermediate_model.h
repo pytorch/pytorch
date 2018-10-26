9f1680dcd129adf36c5c0a40cac9ce531f04e651
Lu Fang <lufang@fb.com>
2018-10-26 14:39:37 -0700

    fix syntax errors

--- caffe2/serialize/intermediate_model.h
+++ caffe2/serialize/intermediate_model.h
@@ -2,6 +2,7 @@
 
 #include <unordered_map>
 #include <stack>
+#include <string>
 
 #include <ATen/core/Allocator.h>
 
@@ -22,11 +23,15 @@ class SharedData {
 
   // getters
   void* rawData() {
-    return dataPtr_->get();
+    return dataPtr_.get();
   }
 
   uint64_t recordId() const {
     return recordId_;
+  }
+
+  uint64_t size() const {
+    return size_;
   }
 
   // setters
@@ -38,11 +43,15 @@ class SharedData {
     recordId_ = record_id;
   }
 
+  void setSize(uint64_t size) {
+    size_ = size;
+  }
+
  private:
   uint64_t recordId_;
   at::DataPtr dataPtr_;
   uint64_t size_;
-}
+};
 
 struct IntermediateDeviceOption {
   int32_t deviceType = 0;
@@ -57,96 +66,95 @@ class IntermediateTensor final {
 
   // update functions
   void update(caffe2::TensorProto* tensor_proto,
-      std::unordered_map<uint64_t, std::shared_ptr<SharedData>>* id_tensor) {
+      std::unordered_map<uint64_t, std::shared_ptr<SharedData>>* id_data) {
     AT_ASSERTM(tensor_proto->has_data_type(), "no data_type in TensorProto!");
     dataType_ = tensor_proto->data_type();
-    for (int i = 0; i < tensor_proto->dims_size()) {
+    for (int i = 0; i < tensor_proto->dims_size(); ++i) {
       dims_.push_back(tensor_proto->dims(i));
     }
     AT_ASSERTM(tensor_proto->has_name(), "no name in TensorProto!");
     name_ = tensor_proto->name();
     if (tensor_proto->has_device_detail()) {
       const auto& device_detail = tensor_proto->device_detail();
-      deviceDetail_.deviceType = device_detail.device_type;
+      deviceDetail_.deviceType = device_detail.device_type();
       if (device_detail.has_device_id()) {
         deviceDetail_.hasDeviceId = true;
-        deviceDetail_.devicedId = device_detail.device_id();
+        deviceDetail_.deviceId = device_detail.device_id();
       }
       if (device_detail.has_random_seed()) {
-        deviceDetail_.hasRandomSeed = true;
-        deviceDetail_.randomSeed = device_detail.random_seed();
+        AT_ERROR("DeviceOption contains random seed!");
       }
       if (device_detail.has_node_name()) {
-        deviceDetail_.hasNodeName = true;
-        deviceDetail_.nodeName = device_detail.nodeName();
+        AT_ERROR("DeviceOption contains node name!");
       }
-      for (int i = 0; i < device_detail.extra_info_size(); ++i) {
-        deviceDetail_.extraInfo.emplace_back(device_detail.extra_info(i));
+      if (device_detail.extra_info_size() > 0) {
+        AT_ERROR("DeviceOption contains extra info!");
       }
     }
     AT_ASSERTM(tensor_proto->has_storage_type(), "no storage_type in TensorProto!");
     int64_t storage_type = tensor_proto->storage_type();
     switch (storage_type) {
-      case TensorProto_StorageType_TYPED:
+      case caffe2::TensorProto_StorageType_TYPED:
         // TODO
         AT_ERROR("Not implemented!");
-      case TensorProto_StorageType_RAW:
+      case caffe2::TensorProto_StorageType_RAW:
         // TODO
         AT_ERROR("Not implemented!");
-      case TensorProto_StorageType_EXTERNAL:
-        AT_ERROR(tensor_proto->has_external_data(), "storage type is EXTERNAL, "
-            "but no external_data in TensorProto!");
-        const auto& external_data = tensor_proto->external_data();
-        if (external_data->has_offset()) {
-          offset_ = external_data->offset();
-        }
-        for (int i = 0; i < external_data.strides_size(); ++i) {
-          strides_->push_back(external_data.strides(i));
-        }
-        AT_ASSERTM(external_data->has_source_type(), "no source_type in TensorProto!");
-        int64_t source_type = external_data->source_type();
-        if (source_type == ExternalDataProto_SourceType::ExternalDataProto_SourceType_INLINE_CONTAINER) {
-          AT_ASSERTM(external_data->has_record_id(), "no record_id in ExternalDataProto!");
-          int64_t record_id = external_data->record_id();
-          auto it = id_tensor.find(record_id);
-          if (it == id_tensor.end()) {
-            AT_ERROR("Tensor's data is missing, tensor name is ", name_);
+      case caffe2::TensorProto_StorageType_EXTERNAL:
+        {
+          AT_ASSERTM(tensor_proto->has_external_data(), "storage type is EXTERNAL, "
+              "but no external_data in TensorProto!");
+          auto& external_data = tensor_proto->external_data();
+          offset_ = external_data.offset();
+          for (int i = 0; i < external_data.strides_size(); ++i) {
+            strides_.push_back(external_data.strides(i));
           }
-          dataPtr_ = std::move(it->second);
-        } else if (source_type == ExternalDataProto_SourceType::ExternalDataProto_SourceType_SIMPLE_FILE) {
-          // TODO
-          AT_ERROR("Not implemented!");
-        } else {
-          // TODO
-          AT_ERROR("Not implemented!");
+          int64_t source_type = external_data.source_type();
+          if (source_type == caffe2::ExternalDataProto_SourceType_INLINE_CONTAINER) {
+            AT_ASSERTM(external_data.has_record_id(), "no record_id in ExternalDataProto!");
+            int64_t record_id = std::stoul(external_data.record_id());
+            auto it = id_data->find(record_id);
+            if (it == id_data->end()) {
+              AT_ERROR("Tensor's data is missing, tensor name is ", name_);
+            }
+            data_ = it->second;
+          } else if (source_type == caffe2::ExternalDataProto_SourceType_SIMPLE_FILE) {
+            // TODO
+            AT_ERROR("Not implemented!");
+          } else {
+            // TODO
+            AT_ERROR("Not implemented!");
+          }
+          break;
         }
-        break;
-      case TensorProto_StorageType_NO_CONTENT:
-        noContent_ = true;
-        break;
+      case caffe2::TensorProto_StorageType_NO_CONTENT:
+        {
+          noContent_ = true;
+          break;
+        }
       default:
         AT_ERROR("Uknown type %lld", storage_type);
     }
   }
 
   void updateData(std::shared_ptr<SharedData> data) {
-    this->data_ = data;
+    data_ = data;
   }
 
   void dump(caffe2::TensorProto* tensor_proto) {
     for (auto dim : dims_) {
       tensor_proto->add_dims(dim);
     }
-    tensor_proto->set_data_type(dataType_);
+    tensor_proto->set_data_type(static_cast<caffe2::TensorProto_DataType>(dataType_));
     tensor_proto->set_storage_type(caffe2::TensorProto_StorageType::TensorProto_StorageType_EXTERNAL);
-    caffe2::ExternalDataProto* data_proto = tensor_proto->mutable_exteranl_data();
+    caffe2::ExternalDataProto* data_proto = tensor_proto->mutable_external_data();
     data_proto->set_source_type(caffe2::ExternalDataProto_SourceType_INLINE_CONTAINER);
-    data_proto->set_record_id(std::to_string(data_->recordId));
+    data_proto->set_record_id(std::to_string(data_->recordId()));
     data_proto->set_offset(offset_);
     for (auto stride : strides_) {
       data_proto->add_strides(stride);
     }
-    caffe2::DeviceOption* device_detail = data_proto->mutable_device_detail();
+    caffe2::DeviceOption* device_detail = tensor_proto->mutable_device_detail();
     device_detail->set_device_type(deviceDetail_.deviceType);
     if (deviceDetail_.hasDeviceId) {
       device_detail->set_device_id(deviceDetail_.deviceId);
@@ -207,7 +215,7 @@ class IntermediateParameter final {
     isBuffer_ = param_def->is_buffer();
     requireGradient_ = param_def->require_gradient();
     if (param_def->has_tensor()) {
-      tensor_.update(param_def->mutable_tensor(), id_tensor);
+      tensor_.update(param_def->mutable_tensor(), id_data);
     } else {
       // TODO
       AT_ERROR("A ParameterDef does not contain any tensor!");
@@ -217,7 +225,7 @@ class IntermediateParameter final {
   void dump(torch::ParameterDef* param_def) {
     param_def->set_name(name_);
     param_def->set_is_buffer(isBuffer_);
-    param_def->set_is_gradient(isGradient_);
+    param_def->set_require_gradient(requireGradient_);
     caffe2::TensorProto* tensor_def = param_def->mutable_tensor();
     tensor_.dump(tensor_def);
   }
@@ -237,6 +245,10 @@ class IntermediateParameter final {
 
   IntermediateTensor* mutableTensor() {
     return &tensor_;
+  }
+
+  void setName(const std::string& name) {
+    name_ = name;
   }
 
   void setIsBuffer(bool is_buffer) {
@@ -265,7 +277,7 @@ class IntermediateMethod final {
     if (method_def->has_torch_script()) {
       torchScript_ = method_def->torch_script();
     } else if (method_def->has_graph()) {
-      graph_ = caffe2::make_unique(release_graph());
+      graph_.reset(method_def->release_graph());
     } else {
       // TODO throw exception
     }
@@ -286,6 +298,10 @@ class IntermediateModule final {
  public:
   // constructor
   IntermediateModule() = default;
+  explicit IntermediateModule(torch::ModuleDef* module_def,
+      std::unordered_map<uint64_t, std::shared_ptr<SharedData>>* id_data) {
+    update(module_def, id_data);
+  }
 
   // update functions
   void update(torch::ModuleDef* module_def,
@@ -297,12 +313,12 @@ class IntermediateModule final {
     }
     for (int i = 0; i < module_def->parameters_size(); ++i) {
       auto* param_def = module_def->mutable_parameters(i);
-      parameters_.emplace_back(param_def, id_tensor);
+      parameters_.emplace_back(param_def, id_data);
     }
 
     for (int i = 0; i < module_def->submodules_size(); ++i) {
       auto* sub_def = module_def->mutable_submodules(i);
-      submodules_.emplace_back(sub_def, id_tensor);
+      submodules_.emplace_back(sub_def, id_data);
     }
 
     for (int i = 0; i < module_def->methods_size(); ++i) {
@@ -317,19 +333,19 @@ class IntermediateModule final {
     for (int i = 0; i < parameters_.size(); ++i) {
       module_def->add_parameters();
       torch::ParameterDef* param_def = module_def->mutable_parameters(module_def->parameters_size() - 1);
-      parameters_.at(i)->dump(param_def);
+      parameters_.at(i).dump(param_def);
     }
 
     for (int i = 0; i < submodules_.size(); ++i) {
       module_def->add_submodules();
       torch::ModuleDef* sub_def = module_def->mutable_submodules(module_def->submodules_size() - 1);
-      submodules_.at(i)->dump(sub_def);
+      submodules_.at(i).dump(sub_def);
     }
 
     for (int i = 0; i < methods_.size(); ++i) {
       module_def->add_methods();
       torch::MethodDef* method_def = module_def->mutable_methods(module_def->methods_size() - 1);
-      methods_.at(i)->dump(method_def);
+      methods_.at(i).dump(method_def);
     }
   }
 
@@ -364,7 +380,7 @@ class IntermediateModel final {
 
   // update functions
   void update(torch::ModelDef* model_def,
-      std::unordered_map<uint64_t, std::shared_ptr<SharedData>* id_data) {
+      std::unordered_map<uint64_t, std::shared_ptr<SharedData>>* id_data) {
     if (model_def->has_name()) {
       name_ = model_def->name();
     } else {
@@ -382,13 +398,13 @@ class IntermediateModel final {
       AT_ERROR("ModelDef does not have proto_version.");
     }
     if (model_def->has_main_module()) {
-      mainModule.update(model_def->mutable_main_module(), id_data);
+      mainModule_.update(model_def->mutable_main_module(), id_data);
     } else {
       AT_ERROR("ModelDef does not have main_module.");
     }
   }
 
-  void dump(torch::Model* model_def) {
+  void dump(torch::ModelDef* model_def) {
     // TODO
   }
 
@@ -403,10 +419,6 @@ class IntermediateModel final {
 
   const std::string& producerVersion() const {
     return producerVersion_;
-  }
-
-  int64_t producerVersion() const {
-    return protoVersion_;
   }
 
   const IntermediateModule& mainModule() const {
@@ -448,27 +460,28 @@ void serializeIntermediateModel(IntermediateModel* imodel,
   std::unordered_map<void*, uint64_t> data_id;
   // TODO
   std::stack<IntermediateModule*> imodules;
-  imodules.push(&(imodel->mainModule()));
-  while (!modules.empty()) {
-    IntermediateModule* m = imodules.pop();
+  imodules.push(imodel->mutableMainModule());
+  while (!imodules.empty()) {
+    IntermediateModule* m = imodules.top();
+    imodules.pop();
     auto* params = m->mutableParameters();
     for (int i = 0; i < params->size(); ++i) {
-      std::shared_ptr<SharedData>* t = params->at(i)->mutableTensor()->mutableData();
-      void* data = t->rawData();
-      size_t size = t->size();
+      std::shared_ptr<SharedData> dataptr = params->at(i).mutableTensor()->data();
+      void* data = dataptr->rawData();
+      size_t size = dataptr->size();
       auto it = data_id.find(data);
       if (it != data_id.end()) {
-        t->setRecordId(it->second);
+        dataptr->setRecordId(it->second);
       } else {
         uint64_t id = writer->writeRecord(data, size);
-        t->setRecordId(id);
+        dataptr->setRecordId(id);
         data_id[data] = id;
       }
     }
 
     auto* subms = m->mutableSubmodules();
     for (int i = 0; i < subms->size(); ++i) {
-      imodules.push(subms->at(i));
+      imodules.push(&subms->at(i));
     }
   }
 
@@ -477,20 +490,20 @@ void serializeIntermediateModel(IntermediateModel* imodel,
   imodel->dump(&model_def);
   size_t proto_size = model_def.ByteSizeLong();
   void* buffer = malloc(proto_size);
-  model_def.SerializeToArray(buffer, size);
+  model_def.SerializeToArray(buffer, proto_size);
   writer->writeRecord(buffer, proto_size);
-  free(proto_size);
+  free(buffer);
 }
 
 // serialize functions
-void serializeIntermediateModel(IntermediateModel* imdoel, const std::string& filename) {
-  PyTorchFileWriter writer(filename);
+void serializeIntermediateModel(IntermediateModel* imodel, const std::string& filename) {
+  torch::jit::PyTorchFileWriter writer(filename);
   serializeIntermediateModel(imodel, &writer);
 }
 
 void deserializeIntermediateModel(IntermediateModel* imodel,
     torch::jit::PyTorchFileReader* reader) {
-  std::unordered_map<uint64_t, std::shared_ptr<SharedData> id_data;
+  std::unordered_map<uint64_t, std::shared_ptr<SharedData>> id_data;
   while (reader->hasNextRecord()) {
     at::DataPtr data_ptr;
     size_t data_key;
@@ -499,22 +512,22 @@ void deserializeIntermediateModel(IntermediateModel* imodel,
     if (!reader->hasNextRecord()) {
       auto it = id_data.find(data_key);
       if (it != id_data.end()) {
-        id_data->second->updateData(std::move(data_ptr));
+        it->second->setDataPtr(std::move(data_ptr));
       } else {
-        (*id_data)[data_key] = make_shared<SharedData>(
+        id_data[data_key] = std::make_shared<SharedData>(
             data_key, std::move(data_ptr));
       }
       continue;
     }
     torch::ModelDef model_def = torch::ModelDef();
-    AT_ASSERTM(model_def.ParsePartialFromArray(proto_ptr.get(),
-          proto_size), "Parsing proto from array failedl");
-    imodel->update(&model_def, id_data);
+    AT_ASSERTM(model_def.ParsePartialFromArray(data_ptr.get(),
+          data_size), "Parsing proto from array failedl");
+    imodel->update(&model_def, &id_data);
   }
 }
 
 void deserializeIntermediateModel(IntermediateModel* imodel, const std::string& filename) {
-  PyTorchFileReader reader(filename);
+  torch::jit::PyTorchFileReader reader(filename);
   deserializeIntermediateModel(imodel, &reader);
 }
 
--- caffe2/serialize/intermediate_model_test.cc
+++ caffe2/serialize/intermediate_model_test.cc
@@ -1,10 +1,9 @@
 #include <cstdio>
-#include <iostream>
 #include <string>
 
 #include <gtest/gtest.h>
 
-#include "caffe2/serialize/inline_container.h"
+#include "caffe2/serialize/intermediate_model.h"
 
 namespace at {
 namespace {
@@ -17,52 +16,53 @@ TEST(IntermediateModel, SerializeAndDeserialize) {
   std::string producer_version("Test-Producer-Version");
   int64_t proto_version = 1;
 
-  IntermediateModel imodel;
+  at::serialize::IntermediateModel imodel;
   imodel.setName(model_name);
   imodel.setProducerName(producer_name);
   imodel.setProducerVersion(producer_version);
   imodel.setProtoVersion(proto_version);
 
-  IntermediateModule* main_module = imodel.mutableMainModule();
+  at::serialize::IntermediateModule* main_module = imodel.mutableMainModule();
   std::string module_name("Test-Module-Name");
   main_module->setName(module_name);
-  std::vector<IntermediateModule>* subs = main_module->mutableSubmodules();
+  std::vector<serialize::IntermediateModule>* subs = main_module->mutableSubmodules();
   subs->resize(1);
-  IntermediateModule& sub_module = subs->at(0);
+  at::serialize::IntermediateModule& sub_module = subs->at(0);
   std::string sub_name("Test-Submodule-Name");
-  sub_module->setName(sub_name);
+  sub_module.setName(sub_name);
 
-  std::vector<IntermediateParameter>* params = main_module->mutableParameters();
+  std::vector<serialize::IntermediateParameter>* params = main_module->mutableParameters();
   params->resize(1);
-  IntermediateParameter& param = params->at(0);
+  at::serialize::IntermediateParameter& param = params->at(0);
   std::string param_name("Test-Parameter-Name");
-  param->setName(param_name);
+  param.setName(param_name);
   bool is_buffer = true;
-  param->setIsBuffer(is_buffer);
+  param.setIsBuffer(is_buffer);
   bool require_gradient = true;
-  param->setRequireGradient(require_gradient);
-  IntermediateParameter* tensor = param->mutableTensor();
-  vector<int64_t>* dims = tensor->mutableDims();
+  param.setRequireGradient(require_gradient);
+  at::serialize::IntermediateTensor* tensor = param.mutableTensor();
+  std::vector<int64_t>* dims = tensor->mutableDims();
   size_t raw_size = sizeof(float);
   for (size_t i = 2; i < 5; ++i) {
     raw_size *= i;
     dims->push_back(i);
   }
-  tensor->dataType_ = caffe2::TensorProto_DataType::TensorProto_DataType_FLOAT;
-  std::array<char, raw_size> data_array;
-  for (size_t i = 0; i < data_array.size(); ++i) {
-    data_array[i] = data_array.size() - i;
+  tensor->setDataType(caffe2::TensorProto_DataType_FLOAT);
+  std::vector<char> data_vector;
+  data_vector.resize(raw_size);
+  for (size_t i = 0; i < data_vector.size(); ++i) {
+    data_vector[i] = data_vector.size() - i;
   }
-  at::DataPtr data_ptr(data_array, at::kCPU);
-  std::shared_ptr<SharedData> data = make_shared<SharedData>(0, std::move(data_ptr));
-  tensor.setData(std::move(data));
-  auto& device_detail = tensor->mutableDeviceDetail();
+  at::DataPtr data_ptr(data_vector.data(), at::kCPU);
+  std::shared_ptr<at::serialize::SharedData> data = std::make_shared<at::serialize::SharedData>(0, std::move(data_ptr));
+  tensor->setData(data);
+  auto* device_detail = tensor->mutableDeviceDetail();
 
   std::string tmp_name = std::tmpnam(nullptr);
-  serializeIntermediateModel(imodel, tmp_name);
+  at::serialize::serializeIntermediateModel(&imodel, tmp_name);
 
-  IntermediateModel loaded_model;
-  deserializeIntermediateModel(&loaded_model, tmp_name);
+  at::serialize::IntermediateModel loaded_model;
+  at::serialize::deserializeIntermediateModel(&loaded_model, tmp_name);
 
   ASSERT_EQ(loaded_model.name(), model_name);
 }
