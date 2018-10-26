e95966ef4de06d22e3927ead419b35d86a4e2e14
Peter Goldsborough <psag@fb.com>
2018-10-26 00:06:24 -0700

    Hide c10::optional and nullopt in torch namespace (#12927)
    Summary:
    Does
    
    ```cpp
    namespace torch {
    using c10::optional;
    using c10::nullopt;
    }
    ```
    
    So that users can be oblivious of our changes with ATen/c10 happening in the background, and also don't have to deal with multiple namespaces (which is very confusing).
    
    ezyang
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12927
    
    Differential Revision: D10510630
    
    Pulled By: goldsborough
    
    fbshipit-source-id: e456264f2fbca3eda277712de11cdd8acc77fbd4

--- torch/csrc/api/include/torch/nn/parallel/data_parallel.h
+++ torch/csrc/api/include/torch/nn/parallel/data_parallel.h
@@ -14,7 +14,6 @@
 #include <ATen/Parallel.h>
 #include <ATen/core/TensorOptions.h>
 #include <c10/util/Exception.h>
-#include "c10/util/Optional.h"
 
 #include <cstddef>
 #include <exception>
@@ -72,7 +71,7 @@ template <typename ModuleType>
 std::vector<Tensor> parallel_apply(
     std::vector<ModuleType>& modules,
     const std::vector<Tensor>& inputs,
-    const c10::optional<std::vector<Device>>& devices = c10::nullopt) {
+    const optional<std::vector<Device>>& devices = nullopt) {
   AT_CHECK(
       modules.size() == inputs.size(), "Must have as many inputs as modules");
   if (devices) {
@@ -135,8 +134,8 @@ template <typename ModuleType>
 Tensor data_parallel(
     ModuleType module,
     Tensor input,
-    c10::optional<std::vector<Device>> devices = c10::nullopt,
-    c10::optional<Device> output_device = c10::nullopt,
+    optional<std::vector<Device>> devices = nullopt,
+    optional<Device> output_device = nullopt,
     int64_t dim = 0) {
   if (!devices) {
     const auto device_count = torch::cuda::device_count();
@@ -157,7 +156,7 @@ Tensor data_parallel(
   }
 
 #ifdef USE_CUDA
-  autograd::Scatter scatter(*devices, /*chunk_sizes=*/c10::nullopt, dim);
+  autograd::Scatter scatter(*devices, /*chunk_sizes=*/nullopt, dim);
   auto scattered_inputs = fmap<Tensor>(scatter.apply({std::move(input)}));
 
   auto replicas = replicate(module, *devices);
