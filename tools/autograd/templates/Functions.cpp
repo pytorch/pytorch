284c6e7682b26d3113361311a05778de76d3da6b
Lu Fang <lufang@fb.com>
2018-10-26 23:04:00 -0700

    Revert "Move batch_norm to ATen/native, speed up (#12368)" (#13191)
    Summary:
    Revert #12368 since it's causing onnx related test cases failing.
    https://github.com/pytorch/pytorch/pull/12368
    
    SsnL The controller you requested could not be found.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13191
    
    Reviewed By: BIT-silence
    
    Differential Revision: D12810778
    
    Pulled By: houseroad
    
    fbshipit-source-id: 1c373b92628580097cffcd237dccc5b3d8697577

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -1861,7 +1861,7 @@ std::tuple<Tensor, Tensor, Tensor> batchnorm_double_backward(
     bool training,
     double eps,
     const Tensor & save_mean,
-    const Tensor & save_invstd,
+    const Tensor & save_std,
     std::array<bool,3> output_mask) {
 
   bool affine = gamma.defined();
@@ -1885,12 +1885,9 @@ std::tuple<Tensor, Tensor, Tensor> batchnorm_double_backward(
   for (auto s : input.sizes().slice(2)) {
     M *= s;
   }
-  // for half inputs, save_mean, save_invstd are float (ideally, we would cast
-  // everything else, but not now)
-  auto mu = unsqueeze_dim1(training ? save_mean.to(input) : running_mean, input);
+  auto mu = unsqueeze_dim1(training ? save_mean : running_mean, input);
   auto input_sub_mu = input - mu;
-  auto sigma2_eps_neg_1_2 = unsqueeze_dim1(training ? save_invstd.to(input)
-					            : running_var.add(Scalar(eps)).pow(-0.5), input);
+  auto sigma2_eps_neg_1_2 = unsqueeze_dim1(training ? save_std : running_var.add(Scalar(eps)).pow(-0.5), input);
   auto sigma2_eps_neg_1 = sigma2_eps_neg_1_2.pow(2);
   auto sigma2_eps_neg_3_2 = sigma2_eps_neg_1_2.pow(3);
 
