4f834ddcab9ba5c4433e5f8597e52c899967262c
Gu, Jinghui <jinghui.gu@intel.com>
2018-10-29 10:47:25 -0700

    seperate mkl, mklml, and mkldnn (#12170)
    Summary:
    1. Remove avx2 support in mkldnn
    2. Seperate mkl, mklml, and mkldnn
    3. Fix convfusion test case
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/12170
    
    Reviewed By: yinghai
    
    Differential Revision: D10207126
    
    Pulled By: orionr
    
    fbshipit-source-id: 1e62eb47943f426a89d57e2d2606439f2b04fd51

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -109,6 +109,7 @@ test_aten() {
 
     ${SUDO} ln -s "$TORCH_LIB_PATH"/libc10* build/bin
     ${SUDO} ln -s "$TORCH_LIB_PATH"/libcaffe2* build/bin
+    ${SUDO} ln -s "$TORCH_LIB_PATH"/libmkldnn* build/bin
     ${SUDO} ln -s "$TORCH_LIB_PATH"/libnccl* build/bin
 
     ls build/bin
