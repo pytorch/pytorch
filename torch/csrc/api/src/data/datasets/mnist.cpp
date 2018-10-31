fcffd3ed821ceb3a529eba6bd49f39811d5bf6b0
serega <sergey.melderis@gmail.com>
2018-10-31 08:00:44 -0400

    Use torch::kByte instead of TensorOptions

--- torch/csrc/api/src/data/datasets/mnist.cpp
+++ torch/csrc/api/src/data/datasets/mnist.cpp
@@ -90,8 +90,7 @@ Tensor read_targets(const std::string& root, bool train) {
   expect_int32(targets, kTargetMagicNumber);
   expect_int32(targets, count);
 
-  at::TensorOptions options;
-  auto tensor = torch::empty(count, options.dtype(at::kByte));
+  auto tensor = torch::empty(count, torch::kByte);
   targets.read(reinterpret_cast<char*>(tensor.data_ptr()), count);
   return tensor.to(torch::kInt64);
 }
