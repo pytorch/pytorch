8275230472ff02ce54419dfc6e1fec3fc2738566
Edward Z. Yang <ezyang@fb.com>
2018-10-31 07:15:57 -0700

    V3: Also install c10/detail
    Differential Revision: D12849620
    Differential Version: 62160805

--- setup.py
+++ setup.py
@@ -1183,6 +1183,7 @@ if __name__ == '__main__':
                 'lib/include/c10/*.h',
                 'lib/include/c10/macros/*.h',
                 'lib/include/c10/util/*.h',
+                'lib/include/c10/detail/*.h',
                 'lib/include/caffe2/core/*.h',
                 'lib/include/torch/*.h',
                 'lib/include/torch/csrc/*.h',
