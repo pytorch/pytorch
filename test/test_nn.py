Wrong value of expected
In addition, flake8

test/test_nn.py
@@ -1952,7 +1952,7 @@ class TestNN(NNTestCase):
         self.assertEqual(input.grad, inputf.grad.to(dtype), prec=0)
 
     def _test_gumbel_softmax_st_shapes(self, dtype, shape, dim, count_expected):
-        logits = torch.randn(shape, dtype = dtype)
+        logits = torch.randn(shape, dtype=dtype)
         y_draw = F.gumbel_softmax(logits, hard=True, dim=dim)
 
         # All values positive
@@ -1995,8 +1995,8 @@ class TestNN(NNTestCase):
 
     @repeat_test_for_types(NO_HALF_TENSORTYPES)
     def test_gumbel_softmax(self, dtype=torch.float):
-        self._test_gumbel_softmax_st_shapes(dtype, shape=[5], dim=0, count_expected=5)
-        self._test_gumbel_softmax_st_shapes(dtype, shape=[5], dim=-1, count_expected=5)
+        self._test_gumbel_softmax_st_shapes(dtype, shape=[5], dim=0, count_expected=1)
+        self._test_gumbel_softmax_st_shapes(dtype, shape=[5], dim=-1, count_expected=1)
         self._test_gumbel_softmax_st_shapes(dtype, shape=[5, 4], dim=1, count_expected=5)
         self._test_gumbel_softmax_st_shapes(dtype, shape=[5, 4, 3], dim=1, count_expected=5 * 3)
         self._test_gumbel_softmax_st_shapes(dtype, shape=[5, 4, 3], dim=-1, count_expected=5 * 4)