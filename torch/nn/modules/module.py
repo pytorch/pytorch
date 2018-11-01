c82a3e581fe7a16d64f7b6b5e8b7a575dee76610
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-11-01 14:40:17 -0400

    conditionally add in __setstate__

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -500,8 +500,13 @@ class Module(object):
 
     def __setstate__(self, state):
         self.__dict__.update(state)
+        # Support loading old checkpoints that don't have the following attrs:
         if '_forward_pre_hooks' not in self.__dict__:
             self._forward_pre_hooks = OrderedDict()
+        if '_state_dict_hooks' not in self.__dict__:
+            self._state_dict_hooks = OrderedDict()
+        if '_load_state_dict_pre_hooks' not in self.__dict__:
+            self._load_state_dict_pre_hooks = OrderedDict()
 
     def __getattr__(self, name):
         if '_parameters' in self.__dict__:
