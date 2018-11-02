ab51f19e0c23f4e737f831494dcb18da35c440a3
Tongzhou Wang <tongzhou.wang.1994@gmail.com>
2018-11-01 19:04:17 -0700

    Fix pytest, make it work with run_test.py (#13416)
    Summary:
    Fixes #13326
    
    Also now you can use `run_test.py` with `pytest`. E.g.,
    ```
    python run_test.py -vci distributed -pt
    ```
    
    Yes it works with `distributed` and `cpp_extension`.
    
    cc zou3519 vishwakftw
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13416
    
    Differential Revision: D12895622
    
    Pulled By: SsnL
    
    fbshipit-source-id: 2d18106f3a118d642a666bfb1318f41c859c3df7

--- test/test_torch.py
+++ test/test_torch.py
@@ -94,7 +94,9 @@ class BytesIOContext(io.BytesIO):
         pass
 
 
-class TestTorchMixin(object):
+# This is intentionally prefixed by an underscore. Otherwise pytest will try to
+# run its methods as test cases.
+class _TestTorchMixin(object):
     def _check_sum_dim(tensors, dim):
         for tensor in tensors:
             expected = tensor.numpy().sum(dim)
@@ -2833,7 +2835,7 @@ class TestTorchMixin(object):
                      "spawn start method is not supported in Python 2, \
                      but we need it for for testing failure case for CPU RNG on Windows")
     def test_multinomial_invalid_probs(self):
-        test_method = TestTorchMixin._test_multinomial_invalid_probs
+        test_method = _TestTorchMixin._test_multinomial_invalid_probs
         self._spawn_method(test_method, torch.Tensor([1, -1, 1]))
         self._spawn_method(test_method, torch.Tensor([1, inf, 1]))
         self._spawn_method(test_method, torch.Tensor([1, -inf, 1]))
@@ -6289,7 +6291,7 @@ class TestTorchMixin(object):
         idx_size = [m, n, o]
         idx_size[dim] = elems_per_row
         idx = torch.LongTensor().resize_(*idx_size)
-        TestTorchMixin._fill_indices(self, idx, dim, src.size(dim), elems_per_row, m, n, o)
+        _TestTorchMixin._fill_indices(self, idx, dim, src.size(dim), elems_per_row, m, n, o)
 
         src = cast(src)
         idx = cast(idx)
@@ -6327,7 +6329,7 @@ class TestTorchMixin(object):
         idx_size = [m, n, o]
         idx_size[dim] = elems_per_row
         idx = cast(torch.LongTensor().resize_(*idx_size))
-        TestTorchMixin._fill_indices(self, idx, dim, ([m, n, o])[dim], elems_per_row, m, n, o)
+        _TestTorchMixin._fill_indices(self, idx, dim, ([m, n, o])[dim], elems_per_row, m, n, o)
 
         if is_scalar:
             src = random.random()
@@ -6547,7 +6549,7 @@ class TestTorchMixin(object):
         self.assertEqual(tensor.view(1, 6, 2, 1), contig_tensor.view(1, 6, 2, 1))
 
     def test_view(self):
-        TestTorchMixin._test_view(self, lambda x: x)
+        _TestTorchMixin._test_view(self, lambda x: x)
 
     def test_view_empty(self):
         x = torch.randn(0, 6)
@@ -9266,13 +9268,14 @@ def add_neg_dim_tests():
 
         test_name = 'test_' + name + '_neg_dim'
 
-        assert not hasattr(TestTorchMixin, test_name), "Duplicated test name: " + test_name
-        setattr(TestTorchMixin, test_name, make_neg_dim_test(name, tensor_arg, arg_constr, types, extra_dim))
+        assert not hasattr(_TestTorchMixin, test_name), "Duplicated test name: " + test_name
+        setattr(_TestTorchMixin, test_name, make_neg_dim_test(name, tensor_arg, arg_constr, types, extra_dim))
 
 add_neg_dim_tests()
 
-if __name__ == '__main__':
-    class TestTorch(TestCase, TestTorchMixin):
-        pass
 
+class TestTorch(TestCase, _TestTorchMixin):
+    pass
+
+if __name__ == '__main__':
     run_tests()
