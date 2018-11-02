c26c3e3dbeeb03926ea2d81fd2d60425bb206a2b
David Riazati <davidriazati@fb.com>
2018-11-02 13:15:35 -0700

    Fix py2 error

--- torch/nn/_VF.py
+++ torch/nn/_VF.py
@@ -4,7 +4,11 @@ import types
 
 
 class VFModule(types.ModuleType):
+    def __init__(self, name):
+        super(VFModule, self).__init__(name)
+        self.vf = torch._C._VariableFunctions
+
     def __getattr__(self, attr):
-        return getattr(torch._C._VariableFunctions, attr)
+        return getattr(self.vf, attr)
 
 sys.modules[__name__] = VFModule(__name__)
