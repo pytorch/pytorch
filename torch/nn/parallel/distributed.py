11b6ca1860414727f84cfe621e456faae34b46bf
Teng Li <tengli@fb.com>
2018-11-01 20:15:06 -0700

    [c10d] Mixed precision DDP hang fix and fine-grained option for perf improvement

--- torch/nn/parallel/distributed.py
+++ torch/nn/parallel/distributed.py
@@ -175,6 +175,8 @@ class DistributedDataParallel(Module):
         # This is a triply-nested list where the "dimensions" are: devices, buckets, bucket_elems
         param_buckets = []
         # Split the parameters into buckets and by types as well
+        # TODO: use take_tensor finegrained to provide better overlapping for
+        # mixed precision training
         param_buckets = [list(_take_tensors(m.parameters(), bucket_bytes_cap)) for m in self._module_copies]
 
         self.bucket_sizes = []
