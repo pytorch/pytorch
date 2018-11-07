289eaf6cfacb2c47d36eaafaa0ffa9e2e6d3385d
Jerry Zhang <jerryzh@fb.com>
2018-11-06 16:34:00 -0800

    Rename ndim() -> dim() - 5/6
    Summary:
    Codemod generated with clangr shard mode, 50 files per diff,
    clangr code(ndim()->dim()): diffusion/FBS/browse/master/fbcode/caffe2/caffe2/fb/codemods/TensorMethodRename.cpp
    
    Reviewed By: salexspb
    
    Differential Revision: D12935787
    
    fbshipit-source-id: 303d71d3eb050789af2ab9575e5dcc48f6037086

--- caffe2/operators/utility_ops_gpu_test.cc
+++ caffe2/operators/utility_ops_gpu_test.cc
@@ -44,7 +44,7 @@ TEST(UtilityOpGPUTest, testReshapeWithScalar) {
   EXPECT_TRUE(op->Run());
   Blob* XNew = ws.GetBlob("XNew");
   const Tensor& XNewTensor = XNew->Get<Tensor>();
-  EXPECT_EQ(1, XNewTensor.ndim());
+  EXPECT_EQ(1, XNewTensor.dim());
   EXPECT_EQ(1, XNewTensor.numel());
 }
 
