fd9aaa6b79b4a98d8b59fb98fc5820a1cae70637
peter <peterghost86@gmail.com>
2018-11-08 09:52:21 -0800

    Fix linking errors on Windows (#13100)
    Summary:
    1. Removes the flag "/FORCE:UNRESOLVED" that shouldn't be used.
    2. Fix the code logic for ONNX_BUILD_MAIN_LIBS on Windows
    3. Add a patch for protobuf using CMake
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13100
    
    Differential Revision: D12978950
    
    Pulled By: orionr
    
    fbshipit-source-id: db9eb8136acf5712cfb5a24ed228b7934d873331

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -931,12 +931,12 @@ if (CAFFE2_CMAKE_BUILDING_WITH_MAIN_REPO)
   endif()
   set(TEMP_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
   # We will build onnx as static libs and embed it directly into the binary.
-  set(BUILD_SHARED_LIBS OFF)
   if (MSVC AND BUILD_SHARED_LIBS)
     # That also means we want to export all symbols from the shared
     # library we are building
     set(ONNX_BUILD_MAIN_LIB ON)
   endif()
+  set(BUILD_SHARED_LIBS OFF)
   set(ONNX_USE_MSVC_STATIC_RUNTIME ${CAFFE2_USE_MSVC_STATIC_RUNTIME})
   set(ONNX_USE_LITE_PROTO ${CAFFE2_USE_LITE_PROTO})
   # If linking local protobuf, make sure ONNX has the same protobuf
