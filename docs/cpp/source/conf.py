c2ddfec61c2dde8b758dc755f6a240c0814c5ec2
Brian Johnson <brianjo@fb.com>
2018-11-09 16:41:34 -0800

    Fixed lint errors.

--- docs/cpp/source/conf.py
+++ docs/cpp/source/conf.py
@@ -23,7 +23,7 @@ import os
 import sys
 import textwrap
 
-import pytorch_sphinx_theme 
+import pytorch_sphinx_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -191,15 +191,15 @@ def setup(app):
     # NOTE: in Sphinx 1.8+ `html_css_files` is an official configuration value
     # and can be moved outside of this function (and the setup(app) function
     # can be deleted).
-     html_css_files = [
+    html_css_files = [
         'https://fonts.googleapis.com/css?family=Lato',
         'css/pytorch_theme.css'  # relative to paths in `html_static_path`
     ]
 
     # In Sphinx 1.8 it was renamed to `add_css_file`, 1.7 and prior it is
     # `add_stylesheet` (deprecated in 1.8).
-     add_css = getattr(app, 'add_css_file', getattr(app, 'add_stylesheet'))
-     for css_file in html_css_files:
+    add_css = getattr(app, 'add_css_file', getattr(app, 'add_stylesheet'))
+    for css_file in html_css_files:
         add_css(css_file)
 
 # -- Options for HTMLHelp output ------------------------------------------
