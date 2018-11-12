eeab738debdacf4d7b347fcd2c26c34b080af994
Edward Z. Yang <ezyang@fb.com>
2018-11-12 07:42:01 -0800

    V5: morefix
    Differential Revision: D13024368
    Differential Version: 63314717

--- aten/src/ATen/cuda/CUDAGuard.h
+++ aten/src/ATen/cuda/CUDAGuard.h
@@ -60,7 +60,7 @@ struct CUDAGuard {
 
  private:
   /// The guard for the current device.
-  c10::impl::InlineDeviceGuard<at::detail::CUDAGuardImpl> guard_;
+  c10::impl::InlineDeviceGuard<at::cuda::detail::CUDAGuardImpl> guard_;
 };
 
 /// A variant of OptionalDeviceGuard that is specialized for CUDA.  See
@@ -112,7 +112,7 @@ struct OptionalCUDAGuard {
   void reset() { guard_.reset(); }
 
 private:
-  c10::impl::InlineOptionalDeviceGuard<at::detail::CUDAGuardImpl> guard_;
+  c10::impl::InlineOptionalDeviceGuard<at::cuda::detail::CUDAGuardImpl> guard_;
 };
 
 /// A variant of StreamGuard that is specialized for CUDA.  See CUDAGuard
@@ -169,7 +169,7 @@ struct CUDAStreamGuard {
   Device original_device() const { return guard_.original_device(); }
 
 private:
-  c10::impl::InlineStreamGuard<at::detail::CUDAGuardImpl> guard_;
+  c10::impl::InlineStreamGuard<at::cuda::detail::CUDAGuardImpl> guard_;
 };
 
 /// A variant of OptionalStreamGuard that is specialized for CUDA.  See CUDAGuard
@@ -232,7 +232,7 @@ struct OptionalCUDAStreamGuard {
   void reset() { guard_.reset(); }
 
 private:
-  c10::impl::InlineOptionalStreamGuard<at::detail::CUDAGuardImpl> guard_;
+  c10::impl::InlineOptionalStreamGuard<at::cuda::detail::CUDAGuardImpl> guard_;
 };
 
 // TODO: Implement this generically in c10.  You'll need some way to get
