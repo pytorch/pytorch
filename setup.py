75bf87753484927cf0f4ce9b315c9b4ba17d1c96
David Brownell <david.brownell@microsoft.com>
2018-11-12 14:28:10 -0800

    Preventing error where ninja build files are overwritten when invokin… (#13698)
    Summary:
    …g clean and build together
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13698
    
    Differential Revision: D13030905
    
    Pulled By: soumith
    
    fbshipit-source-id: 234576ac92e0aa8c2d2409958d3cf85eb29ed1f3

--- setup.py
+++ setup.py
@@ -217,10 +217,8 @@ if not ONNX_NAMESPACE:
 try:
     import ninja
     USE_NINJA = True
-    ninja_global = NinjaBuilder('global')
 except ImportError:
     USE_NINJA = False
-    ninja_global = None
 
 # Constant known variables used throughout this file
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -633,12 +631,16 @@ class build_ext(build_ext_parent):
         else:
             print('-- Building without distributed package')
 
-        generate_code(ninja_global)
-
         if USE_NINJA:
+            ninja_builder = NinjaBuilder('global')
+
+            generate_code(ninja_builder)
+
             # before we start the normal build make sure all generated code
             # gets built
-            ninja_global.run()
+            ninja_builder.run()
+        else:
+            generate_code(None)
 
         # It's an old-style class in Python 2.7...
         setuptools.command.build_ext.build_ext.run(self)
