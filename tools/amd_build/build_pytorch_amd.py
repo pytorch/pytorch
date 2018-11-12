ef4ae1e30e99eb3443955763267f0d11705f1a36
Your Name <bai@in.tum.de>
2018-11-12 00:53:23 -0800

    revert some hipify cleanup

--- tools/amd_build/build_pytorch_amd.py
+++ tools/amd_build/build_pytorch_amd.py
@@ -27,6 +27,27 @@ patch_folder = os.path.join(amd_build_dir, "patches")
 for filename in os.listdir(os.path.join(amd_build_dir, "patches")):
     subprocess.Popen(["git", "apply", os.path.join(patch_folder, filename)], cwd=proj_dir)
 
+# Make various replacements inside AMD_BUILD/torch directory
+ignore_files = ["csrc/autograd/profiler.h", "csrc/autograd/profiler.cpp",
+                "csrc/cuda/cuda_check.h"]
+for root, _directories, files in os.walk(os.path.join(proj_dir, "torch")):
+    for filename in files:
+        if filename.endswith(".cpp") or filename.endswith(".h"):
+            source = os.path.join(root, filename)
+            # Disabled files
+            if reduce(lambda result, exclude: source.endswith(exclude) or result, ignore_files, False):
+                continue
+            # Update contents.
+            with open(source, "r+") as f:
+                contents = f.read()
+                contents = contents.replace("USE_CUDA", "USE_ROCM")
+                contents = contents.replace("CUDA_VERSION", "0")
+                f.seek(0)
+                f.write(contents)
+                f.truncate()
+                f.flush()
+                os.fsync(f)
+
 hipify_python.hipify(
     project_directory=proj_dir,
     output_directory=proj_dir,
--- tools/amd_build/pyHIPIFY/cuda_to_hip_mappings.py
+++ tools/amd_build/pyHIPIFY/cuda_to_hip_mappings.py
@@ -2211,7 +2211,6 @@ PYTORCH_SPECIFIC_MAPPINGS = collections.OrderedDict([
     ("cudaHostAllocator", ("hipHostAllocator", API_PYTORCH)),
     ("cudaDeviceAllocator", ("hipDeviceAllocator", API_PYTORCH)),
     ("define MAX_NUM_BLOCKS 200", ("define MAX_NUM_BLOCKS 64", API_PYTORCH)),
-    ("USE_CUDA", ("USE_ROCM", API_PYTORCH)),
 ])
 
 CAFFE2_SPECIFIC_MAPPINGS = collections.OrderedDict([
