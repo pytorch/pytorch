10d201f4b3587e8b243d7226b761ede95575b47d
Peter Goldsborough <psag@fb.com>
2018-11-12 09:44:29 -0800

    Document

--- torch/csrc/api/include/torch/data/datasets/shared.h
+++ torch/csrc/api/include/torch/data/datasets/shared.h
@@ -2,8 +2,6 @@
 
 #include <torch/data/datasets/base.h>
 
-#include <c10/util/Exception.h>
-
 #include <memory>
 #include <utility>
 
@@ -11,6 +9,14 @@ namespace torch {
 namespace data {
 namespace datasets {
 
+/// A dataset that wraps another dataset in a shared pointer and implements the
+/// `BatchDataset` API, delegating all calls to the shared instance. This is
+/// useful when you want all worker threads in the dataloader to access the same
+/// dataset instance. The dataset must take care of synchronization and
+/// thread-safe access itself.
+///
+/// Use `torch::data::datasets::make_shared_dataset()` to create a new
+/// `SharedDataset` like you would a `std::shared_ptr`.
 template <typename UnderlyingDataset>
 class SharedDataset : BatchDataset<
                           SharedDataset<UnderlyingDataset>,
@@ -20,32 +26,38 @@ class SharedDataset : BatchDataset<
   using BatchType = typename UnderlyingDataset::BatchType;
   using BatchRequestType = typename UnderlyingDataset::BatchRequestType;
 
+  /// Constructs a new `SharedDataset` from a `shared_ptr` to the
+  /// `UnderlyingDataset`.
   /* implicit */ SharedDataset(
       std::shared_ptr<UnderlyingDataset> shared_dataset)
       : dataset_(std::move(shared_dataset)) {}
 
+  /// Calls `get_batch` on the underlying dataset.
   BatchType get_batch(BatchRequestType request) override {
-    AT_ASSERT(dataset_ != nullptr);
     return dataset_->get_batch(std::move(request));
   }
 
+  /// Returns the `size` from the underlying dataset.
   optional<size_t> size() const override {
-    AT_ASSERT(dataset_ != nullptr);
     return dataset_->size();
   }
 
+  /// Accesses the underlying dataset.
   UnderlyingDataset& operator*() {
     return *dataset_;
   }
 
+  /// Accesses the underlying dataset.
   const UnderlyingDataset& operator*() const {
     return *dataset_;
   }
 
+  /// Accesses the underlying dataset.
   UnderlyingDataset* operator->() {
     return dataset_.get();
   }
 
+  /// Accesses the underlying dataset.
   const UnderlyingDataset* operator->() const {
     return dataset_.get();
   }
@@ -54,6 +66,9 @@ class SharedDataset : BatchDataset<
   std::shared_ptr<UnderlyingDataset> dataset_;
 };
 
+/// Constructs a new `SharedDataset` by creating a
+/// `shared_ptr<UnderlyingDatase>`. All arguments are forwarded to
+/// `make_shared<UnderlyingDataset>`.
 template <typename UnderlyingDataset, typename... Args>
 SharedDataset<UnderlyingDataset> make_shared_dataset(Args&&... args) {
   return std::make_shared<UnderlyingDataset>(std::forward<Args>(args)...);
