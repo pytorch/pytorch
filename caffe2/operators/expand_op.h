9d36c37bdb9e765dfb4abb07b9c923b6cb531220
Jerry Zhang <jerryzh@fb.com>
2018-11-13 12:21:44 -0800

    Tensor construction: combine Resize+mutable_data - 1/4 (#13853)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13853
    
    Codemod generated with clangr shard mode, 25 files per diff,
    motivation: https://github.com/pytorch/pytorch/pull/12407
    
    Reviewed By: smessmer
    
    Differential Revision: D13007246
    
    fbshipit-source-id: 230de42a3843d71599e812d5511f52f3af47f59b

--- caffe2/operators/expand_op.h
+++ caffe2/operators/expand_op.h
@@ -30,7 +30,6 @@ class ExpandOp final : public Operator<Context> {
         Y_shape_tensor.numel(),
         Y_shape_tensor.template data<int64_t>(),
         shape_dims.data());
-    auto* Y = Output(0);
 
     const int ndim = shape_dims.size();
     const std::vector<int> X_dims(X.sizes().cbegin(), X.sizes().cend());
@@ -49,7 +48,10 @@ class ExpandOp final : public Operator<Context> {
       Y_dims.push_back(std::max(shape_x, shape_y));
     }
     std::reverse(Y_dims.begin(), Y_dims.end());
-    Y->Resize(Y_dims);
+    // TODO: remove when the function in math are changed to use vector<int64_t>
+    std::vector<int64_t> Y_dims_int64;
+    std::copy(Y_dims.begin(), Y_dims.end(), std::back_inserter(Y_dims_int64));
+    auto* Y = Output(0, Y_dims_int64, at::dtype<T>());
     math::Broadcast<T, Context>(
         X_dims.size(),
         X_dims.data(),
