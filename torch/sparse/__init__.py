24ac4c7b39e3188e87423fd3891a29b9149c2ffe
Wei Yang <weiyang@fb.com>
2018-11-13 13:37:54 -0800

    update doc

--- torch/sparse/__init__.py
+++ torch/sparse/__init__.py
@@ -9,5 +9,15 @@ __all__ = [
 def addmm(mat, mat1, mat2, beta=1, alpha=1):
     r"""
     .. function:: torch.sparse.addmm(mat, mat1, mat2, beta=1, alpha=1) -> Tensor
+
+    This function does exact same thing as :meth:`~Torch.addmm` in the forward,
+    except that it supports backward for sparse matrix input `mat1`.
+
+    Args:
+        mat (Tensor): a dense matrix to be added
+        mat1 (Tensor): a sparse matrix to be multiplied
+        mat2 (Tensor): a dense matrix be multiplied
+        beta (Number, optional): multiplier for :attr:`mat` (:math:`\beta`)
+        alpha (Number, optional): multiplier for :math:`mat1 @ mat2` (:math:`\alpha`)
     """
     return torch._sparse_addmm(mat, mat1, mat2, beta=beta, alpha=alpha)
