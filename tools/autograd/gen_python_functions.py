e2899b9987029ae6e73a9706f02dcb4efeba05e8
Gregory Chanan <gchanan@fb.com>
2018-11-16 14:44:37 -0800

    Support 'python_module' of 'nn' in native functions.
    Also move mse_loss over to use it.

--- tools/autograd/gen_python_functions.py
+++ tools/autograd/gen_python_functions.py
@@ -168,6 +168,7 @@ def gen_py_variable_methods(out, declarations, template_path):
     def should_bind(declaration):
         return (should_generate_python_binding(declaration) and
                 declaration['mode'] != 'NN' and
+                declaration.get('python_module') != 'nn' and
                 'Tensor' in declaration['method_of'])
 
     py_variable_methods = group_declarations_by_name(declarations, should_bind)
@@ -184,7 +185,7 @@ def gen_py_nn_functions(out, declarations, template_path):
 
     def should_bind(declaration):
         return (should_generate_python_binding(declaration) and
-                declaration['mode'] == 'NN')
+                (declaration['mode'] == 'NN' or declaration.get('python_module') == 'nn'))
 
     py_nn_functions = group_declarations_by_name(declarations, should_bind)
 
@@ -201,6 +202,7 @@ def gen_py_torch_functions(out, declarations, template_path):
     def should_bind(declaration):
         return (should_generate_python_binding(declaration) and
                 declaration['mode'] != 'NN' and
+                declaration.get('python_module') != 'nn' and
                 'namespace' in declaration['method_of'])
 
     py_torch_functions = group_declarations_by_name(declarations, should_bind)
