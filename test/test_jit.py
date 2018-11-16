6be7fdd709f3ba414c001d8b0e8543cb0b1b5d37
David Riazati <davidriazati@fb.com>
2018-11-15 16:20:52 -0800

    Convert more weak functions (#14003)
    Summary:
    Same deal as #13707
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14003
    
    Differential Revision: D13076403
    
    Pulled By: driazati
    
    fbshipit-source-id: eb3cb3b2c31caf1de591b613bdc4c9a6ed4e1767

--- test/test_jit.py
+++ test/test_jit.py
@@ -9118,23 +9118,23 @@ EXCLUDE_SCRIPT = {
     'test_nn_affine_grid',
 
     # unknown builtin op
-    'test_nn_softmin',
-    'test_nn_local_response_norm',
-    'test_nn_poisson_nll_loss',
+    'test_nn_binary_cross_entropy',
+    'test_nn_binary_cross_entropy_size_average',
     'test_nn_cross_entropy',
     'test_nn_binary_cross_entropy_with_logits',
-    'test_nn_multilabel_soft_margin_loss',
-    'test_nn_pixel_shuffle',
     'test_nn_interpolate',
-    'test_nn_pad',
-    'test_nn_normalize',
     'test_nn_fold',
     'test_nn_max_unpool1d',
     'test_nn_lp_pool1d',
     'test_nn_lp_pool2d',
-    'test_nn_instance_norm',
-    'test_nn_grid_sample',
     'test_nn_gumbel_softmax',
+    'test_nn_poisson_nll_loss',
+    'test_nn_poisson_nll_loss_full',
+
+    # undefined tensors as constants
+    'test_nn_instance_norm',
+    'test_nn_normalize',
+    'test_nn_multilabel_soft_margin_loss',
 }
 
 DISABLE_AUTODIFF_SUBGRAPH_INLINING = {
@@ -9757,6 +9757,7 @@ nn_functional_tests = [
     ('local_response_norm', (S, S, S), (2, ),),
     ('nll_loss', F.log_softmax(torch.randn(3, 5), dim=0), (torch.tensor([1, 0, 4]), None, None),),
     ('poisson_nll_loss', (S, 2), ((S, 2),),),
+    ('poisson_nll_loss', (S, 2), ((S, 2), True, True), 'full'),
     ('kl_div', F.log_softmax(torch.randn(S, 10), 1), (F.softmax(torch.randn(S, 10), 1),),),
     ('cross_entropy', (3, S), (torch.randint(S, (3,), dtype=torch.int64),),),
     ('binary_cross_entropy_with_logits', (3,), (torch.empty(3).random_(2), ),),
@@ -9786,6 +9787,9 @@ nn_functional_tests = [
                                    1, 1, non_differentiable(torch.randn(S))),),
     ('binary_cross_entropy', torch.randn(3, 2).sigmoid(), (non_differentiable(torch.rand(3, 2)), \
                                                            non_differentiable(torch.randn(3, 2))),),
+    ('binary_cross_entropy', torch.randn(3, 2).sigmoid(),
+        (non_differentiable(torch.rand(3, 2)),
+         non_differentiable(torch.randn(3, 2)), True), 'size_average'),
     ('ctc_loss', torch.randn(S, S, S).log_softmax(2).detach().requires_grad_(), \
      (torch.randint(1, S, (S, S), dtype=torch.long), torch.full((S,), S, dtype=torch.long), \
       torch.randint(1, S, (S,), dtype=torch.long))),
@@ -9800,6 +9804,7 @@ nn_functional_single_grad = frozenset('test_nn_' + name for name in [
     'multi_margin_loss',
     'binary_cross_entropy',
     'ctc_loss',
+    'grid_sample',
 ])
 
 
