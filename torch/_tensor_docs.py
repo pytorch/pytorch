4df378add178bcc760d813aa5910393469d80bb0
Ailing Zhang <ailzhang@fb.com>
2018-11-15 20:06:08 -0800

    allow empty index for scatter_* methods

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -2008,7 +2008,8 @@ along the specified dimension :attr:`dim` must be unique.
 
 Args:
     dim (int): the axis along which to index
-    index (LongTensor): the indices of elements to scatter
+    index (LongTensor): the indices of elements to scatter, can be either empty or
+      the same size of `src`
     src (Tensor or float): the source element(s) to scatter
 
 Example::
