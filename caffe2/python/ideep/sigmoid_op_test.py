452be5e4c5a319f8ec9c813e5d2e7a65e3e094e9
Hui Wu <hui.h.wu@intel.com>
2018-11-19 14:39:07 +0800

    Add sigmoid op based on MKL-DNN

--- caffe2/python/ideep/sigmoid_op_test.py
+++ caffe2/python/ideep/sigmoid_op_test.py
@@ -9,9 +9,9 @@ from hypothesis import given
 import numpy as np
 from caffe2.python import core, workspace
 import caffe2.python.hypothesis_test_util as hu
-import caffe2.python.ideep_test_util as mu
 
-@unittest.skipIf(not workspace.C.use_ideep, "No IDEEP support.")
+
+@unittest.skipIf(not workspace.C.use_mkldnn, "No MKLDNN support.")
 class SigmoidTest(hu.HypothesisTestCase):
     @given(X=hu.tensor(dtype=np.float32),
            inplace=st.booleans(),
@@ -25,6 +25,7 @@ class SigmoidTest(hu.HypothesisTestCase):
 
         self.assertDeviceChecks(dc, op, [X], [0])
         self.assertGradientChecks(gc, op, [X], 0, [0])
+
 
 if __name__ == "__main__":
     unittest.main()
