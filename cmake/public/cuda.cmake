2ec0e2430bc087d9e97139d3d1686c732ce3cde2
Edward Yang <ezyang@fb.com>
2018-11-19 08:13:07 -0800

    Add c10 cuda library. (#13900)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13900
    
    Add c10 cuda library.
    
    Right now, this is not used by anything, and only tests if the CUDA
    headers are available (and not, e.g., that linking works.)
    
    Extra changes:
    - cmake/public/cuda.cmake now is correctly include guarded, so you
      can include it multiple times without trouble.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Reviewed By: smessmer
    
    Differential Revision: D13025313
    
    fbshipit-source-id: fda85b4c35783ffb48ddd6bbb98dbd9154119d86

--- cmake/public/cuda.cmake
+++ cmake/public/cuda.cmake
@@ -1,5 +1,10 @@
 # ---[ cuda
 
+# Poor man's include guard
+if(TARGET caffe2::cudart)
+  return()
+endif()
+
 # sccache is only supported in CMake master and not in the newest official
 # release (3.11.3) yet. Hence we need our own Modules_CUDA_fix to enable sccache.
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../Modules_CUDA_fix)
