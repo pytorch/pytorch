751b5ea94103105e9a2617a2eddafc830cd0104d
ArmenAg <armen.ag@live.com>
2018-11-19 09:18:45 -0800

    use at::Device throughout JIT (#14181)
    Summary:
    zdevito soumith
    
    Sorry about the previous PR, had some git issues. This is the same exact code as the previous PR but updated w.r.t pytorch/master.
    
    fixes #13254
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14181
    
    Differential Revision: D13117688
    
    Pulled By: soumith
    
    fbshipit-source-id: 044840b2c7a0101ef43dd16655fd9a0f9981f53f

--- torch/csrc/jit/import.cpp
+++ torch/csrc/jit/import.cpp
@@ -210,7 +210,7 @@ TypePtr MethodDecoder::buildType(const onnx::TypeProto& type_proto) {
     return DynamicType::get();
   } else if (kind == "TensorType") {
     auto dims = shape_proto.dim_size();
-    return TensorType::create(onnxTypeToATenType(tensortype_proto.elem_type()), -1, dims);
+    return TensorType::create(onnxTypeToATenType(tensortype_proto.elem_type()), at::kCPU, dims);
   } else if (kind == "CompleteTensorType") {
     // first half of the dims are sizes and the second half are strides
     auto total = shape_proto.dim_size();
@@ -221,7 +221,7 @@ TypePtr MethodDecoder::buildType(const onnx::TypeProto& type_proto) {
     for (int i = total / 2; i < total; i++) {
       strides.push_back(shape_proto.dim(i).dim_value());
     }
-    return CompleteTensorType::create(onnxTypeToATenType(tensortype_proto.elem_type()), -1, sizes, strides);
+    return CompleteTensorType::create(onnxTypeToATenType(tensortype_proto.elem_type()), at::kCPU, sizes, strides);
   } else if (kind == "TupleType") {
     std::vector<TypePtr> elems;
     for (auto &subkind : shape_proto.dim()) {
