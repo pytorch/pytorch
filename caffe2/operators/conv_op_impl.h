d68fe738c495312d7838354b0556c53c19b34f5e
Jerry Zhang <jerryzh@fb.com>
2018-11-19 12:07:31 -0800

    Tensor construction: combine Resize+mutable_data - 1/4 (#13942)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/13942
    
    Codemod generated with clangr shard mode, 25 files per diff,
    motivation: https://github.com/pytorch/pytorch/pull/12407
    
    Reviewed By: smessmer
    
    Differential Revision: D13054770
    
    fbshipit-source-id: be2d76996f78f60ba875234aa5eb7a7e081ab833

--- caffe2/operators/conv_op_impl.h
+++ caffe2/operators/conv_op_impl.h
@@ -540,8 +540,7 @@ bool ConvGradientOp<T, Context>::RunOnDeviceWithOrderNCHW() {
 
   T* dbias_data = nullptr;
   if (!no_bias_) {
-    auto* dbias = Output(BIAS_OR_INPUT_GRAD);
-    dbias->Resize(M);
+    auto* dbias = Output(BIAS_OR_INPUT_GRAD, {M}, at::dtype<T>());
     if (bias_multiplier_.numel() != output_image_size) {
       // If the helper bias multiplier is not M, reshape and fill it with one.
       bias_multiplier_.Resize(vector<int64_t>(1, output_image_size));
@@ -744,8 +743,7 @@ bool ConvGradientOp<T, Context>::RunOnDeviceWithOrderNHWC() {
 
   T* dbias_data = nullptr;
   if (!no_bias_) {
-    auto* dbias = Output(BIAS_OR_INPUT_GRAD);
-    dbias->Resize(M);
+    auto* dbias = Output(BIAS_OR_INPUT_GRAD, {M}, at::dtype<T>());
     dbias_data = dbias->template mutable_data<T>();
     math::Set<T, Context>(dbias->numel(), 0, dbias_data, &context_);
     if (bias_multiplier_.numel() != output_image_size) {
