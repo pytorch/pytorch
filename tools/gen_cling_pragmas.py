af4e32559924a1e3bfe461bea135dabd16c8938e
Peter Goldsborough <psag@fb.com>
2018-11-27 11:22:08 -0800

    Trying

--- tools/gen_cling_pragmas.py
+++ tools/gen_cling_pragmas.py
@@ -1,3 +1,9 @@
+"""
+Generates a single header file that may be included in cling/jupyter, which
+has all the necessary pragmas for cling to see all further headers and libraries
+of PyTorch.
+"""
+
 import argparse
 import os.path
 
@@ -14,38 +20,38 @@ FILE_TEMPLATE = """
 {}
 """
 
-INCLUDE_DIR_TEMPLATE = "#pragma cling add_include_path({})"
-LIBRARY_DIR_TEMPLATE = "#pragma cling add_library_path({})"
-LIBRARIES_TEMPLATE = "#pragma cling load({})"
+# Mainly taken from https://github.com/QuantStack/xeus-cling/issues/87#issuecomment-349053121
+INCLUDE_DIR_TEMPLATE = '#pragma cling add_include_path("{}")'
+LIBRARY_DIR_TEMPLATE = '#pragma cling add_library_path("{}")'
+LIBRARIES_TEMPLATE = '#pragma cling load("{}")'
 
 
-parser = argparse.ArgumentParser(description='Generate cling_pragmas.h')
-parser.add_argument('--include-dirs', nargs="+", required=True)
-parser.add_argument('--library-dirs', nargs="+", required=True)
-parser.add_argument('--libraries', nargs="+", required=True)
-parser.add_argument('-o', '--output')
+parser = argparse.ArgumentParser(description="Generate cling_pragmas.h")
+parser.add_argument("--root", required=True)
+parser.add_argument("--include-dirs", nargs="+", required=True)
+parser.add_argument("--library-dirs", nargs="+", required=True)
+parser.add_argument("--libraries", nargs="+", required=True)
+parser.add_argument("-o", "--output")
 options = parser.parse_args()
 
 
-def format_paths(template, paths):
+def format_paths(template, paths, relative=True):
     for path in paths:
-        relative_path = "{}/{}".format("${{CLING_PRAGMAS_ROOT}}", path)
-        yield template.format(relative_path)
-
+        if relative:
+            path = os.path.join(options.root, path)
+        yield template.format(path)
 
 
 include_dirs = format_paths(INCLUDE_DIR_TEMPLATE, options.include_dirs)
 library_dirs = format_paths(LIBRARY_DIR_TEMPLATE, options.library_dirs)
-libraries = format_paths(LIBRARIES_TEMPLATE, options.libraries)
+libraries = format_paths(LIBRARIES_TEMPLATE, options.libraries, relative=False)
 
 source = FILE_TEMPLATE.format(
-  "\n".join(include_dirs),
-  "\n".join(library_dirs),
-  "\n".join(libraries),
+    "\n".join(include_dirs), "\n".join(library_dirs), "\n".join(libraries)
 )
 
 if options.output:
-  with open(options.output, "w") as output_file:
-    output_file.write(source)
+    with open(options.output, "w") as output_file:
+        output_file.write(source)
 else:
-  print(source)
+    print(source)
