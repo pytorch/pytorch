5fc9ecc407d0332fd4e3a3eccab227a494100463
eellison <eellison@fb.com>
2018-11-26 18:32:36 -0800

    fix comments

--- aten/src/ATen/native/AffineGridGenerator.cpp
+++ aten/src/ATen/native/AffineGridGenerator.cpp
@@ -3,11 +3,11 @@
 
 namespace at { namespace native {
 
-at::Tensor linspace_from_neg_one(const Tensor& grid, int64_t val) {
-  if (val > 1) {
-    return at::linspace(-1, 1, val, grid.options());
+at::Tensor linspace_from_neg_one(const Tensor& grid, int64_t num_steps) {
+  if (num_steps > 1) {
+    return at::linspace(-1, 1, num_steps, grid.options());
   } else {
-    return at::empty({}, grid.options()).fill_(-1);
+    return at::full(-1, {}, grid.options());
   }
 }
 
@@ -17,8 +17,8 @@ Tensor make_base_grid_4D(const Tensor& theta, int64_t N, int64_t C, int64_t H, i
   base_grid.select(-1, 0) = linspace_from_neg_one(theta, W);
   base_grid.select(-1, 1) = linspace_from_neg_one(theta, H).unsqueeze_(-1);
   base_grid.select(-1, 2) = 1;
-  return base_grid;
 
+  return base_grid;
 }
 
 Tensor make_base_grid_5D(const Tensor& theta, int64_t N, int64_t C, int64_t D, int64_t H, int64_t W) {
@@ -33,55 +33,42 @@ Tensor make_base_grid_5D(const Tensor& theta, int64_t N, int64_t C, int64_t D, i
 }
 
 Tensor affine_grid_generator_4D(const Tensor& theta, int64_t N, int64_t C, int64_t H, int64_t W) {
-
   Tensor base_grid = make_base_grid_4D(theta, N, C, H, W);
   auto grid = base_grid.view({N, H * W, 3}).bmm(theta.transpose(1, 2));
-  grid = grid.view({N, H, W, 2});
-
-  return grid;
+  return grid.view({N, H, W, 2});
 }
 
 Tensor affine_grid_generator_5D(const Tensor& theta, int64_t N, int64_t C, int64_t D, int64_t H, int64_t W) {
-
   Tensor base_grid = make_base_grid_5D(theta, N, C, D, H, W);
   auto grid = base_grid.view({N, D * H * W, 4}).bmm(theta.transpose(1, 2));
-  grid = grid.view({N, D, H, W, 3});
-
-  return grid;
+  return grid.view({N, D, H, W, 3});
 }
 
 
 Tensor affine_grid_generator(const Tensor& theta, IntList size) {
-    AT_CHECK(size.size() == 4 || size.size() == 5,
-             "AffineGridGenerator needs 4d (spatial) or 5d (volumetric) inputs.");
-    if (size.size() == 4) {
-      return affine_grid_generator_4D(theta, size[0], size[1], size[2], size[3]);
-    } else {
-      return affine_grid_generator_5D(theta, size[0], size[1], size[2], size[3], size[4]);
-    }
+  AT_CHECK(size.size() == 4 || size.size() == 5,
+           "AffineGridGenerator needs 4d (spatial) or 5d (volumetric) inputs.");
+  if (size.size() == 4) {
+    return affine_grid_generator_4D(theta, size[0], size[1], size[2], size[3]);
+  } else {
+    return affine_grid_generator_5D(theta, size[0], size[1], size[2], size[3], size[4]);
+  }
 }
 
 Tensor affine_grid_generator_4D_backward(const Tensor& grad_grid,
       int64_t N, int64_t C, int64_t H, int64_t W) {
   auto base_grid = make_base_grid_4D(grad_grid, N, C, H, W);
-  IntList exp_size = {N, H, W, 2};
-  AT_ASSERT(grad_grid.sizes() == exp_size);
-
+  AT_ASSERT(grad_grid.sizes() == IntList({N, H, W, 2}));
   auto grad_theta = base_grid.view({N, H * W, 3}).transpose(1, 2).bmm(grad_grid.view({N, H * W, 2}));
-  grad_theta = grad_theta.transpose(1, 2);
-
-  return grad_theta;
+  return grad_theta.transpose(1, 2);
 }
 
 Tensor affine_grid_generator_5D_backward(const Tensor& grad_grid,
       int64_t N, int64_t C, int64_t D, int64_t H, int64_t W) {
   auto base_grid = make_base_grid_5D(grad_grid, N, C, D, H, W);
-  IntList exp_size = {N, D, H, W, 3};
-  AT_ASSERT(grad_grid.sizes() == exp_size);
+  AT_ASSERT(grad_grid.sizes() == IntList({N, D, H, W, 3}));
   auto grad_theta = base_grid.view({N, D * H * W, 4}).transpose(1, 2).bmm(grad_grid.view({N, D * H * W, 3}));
-  grad_theta = grad_theta.transpose(1, 2);
-
-  return grad_theta;
+  return grad_theta.transpose(1, 2);
 }
 
 Tensor affine_grid_generator_backward(const Tensor& grad, IntList size) {
