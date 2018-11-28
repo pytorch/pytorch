03469db938abcaaadf02597727267db9c9b8ffc7
Ailing Zhang <ailzhang@fb.com>
2018-11-28 08:05:43 -0800

    fix typo

--- test/common_nn.py
+++ test/common_nn.py
@@ -252,7 +252,7 @@ def kldivloss_reference(input, target, reduction='mean'):
     safe_target = target * (target > 0).type_as(target)
     safe_target_log = (safe_target + (target <= 0).type_as(target)).log()
     result = safe_target * (safe_target_log - input)
-    if reduction == 'mean' and reduction.dim() != 0:
+    if reduction == 'mean' and result.dim() != 0:
         return result.mean(0)
     elif reduction == 'sum':
         return result.sum()
