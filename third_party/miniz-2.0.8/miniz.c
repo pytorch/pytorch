211826d908bd134f364791b1efc6c5bbe1c3c6cb
Zach DeVito <zdevito@fb.com>
2018-11-29 16:35:28 -0800

    asan fix

--- third_party/miniz-2.0.8/miniz.c
+++ third_party/miniz-2.0.8/miniz.c
@@ -3250,7 +3250,8 @@ static MZ_FORCEINLINE mz_bool mz_zip_array_push_back(mz_zip_archive *pZip, mz_zi
     size_t orig_size = pArray->m_size;
     if (!mz_zip_array_resize(pZip, pArray, orig_size + n, MZ_TRUE))
         return MZ_FALSE;
-    memcpy((mz_uint8 *)pArray->m_p + orig_size * pArray->m_element_size, pElements, n * pArray->m_element_size);
+    if (n > 0) // zdevito: pElements may be null when n == 0 and ASAN complains
+      memcpy((mz_uint8 *)pArray->m_p + orig_size * pArray->m_element_size, pElements, n * pArray->m_element_size);
     return MZ_TRUE;
 }
 
