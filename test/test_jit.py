119f9ec291884c81ef5de745e3338f04ee1152f5
Wanchao Liang <wanchaol@users.noreply.github.com>
2018-12-03 20:38:53 -0800

    enable NoneValue parameter assignment for WeakScriptModule (#14715)
    Summary:
    This PR:
    
    1. Handle None value attr in the WeakScriptModuleProxy
    2. add back module tests that now passing
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14715
    
    Differential Revision: D13313573
    
    Pulled By: wanchaol
    
    fbshipit-source-id: a6b7892707350290a6d69b6f6270ad089bfc954b

--- test/test_jit.py
+++ test/test_jit.py
@@ -9689,23 +9689,9 @@ EXCLUDE_PYTHON_PRINT = {
 }
 
 EXCLUDE_SCRIPT_MODULES = {
-    'test_nn_LPPool2d_norm',
-    'test_nn_LPPool1d_norm',
-    'test_nn_BatchNorm1d_3d_input_not_affine',
-    'test_nn_BatchNorm1d_affine_simple_average',
-    'test_nn_BatchNorm1d_not_affine',
     'test_nn_BatchNorm1d_not_tracking_stats',
-    'test_nn_BatchNorm2d_2d_simple_average',
-    'test_nn_BatchNorm2d_not_affine',
     'test_nn_BatchNorm2d_not_tracking_stats',
-    'test_nn_BatchNorm3d_3d_simple_average',
-    'test_nn_BatchNorm3d_not_affine',
     'test_nn_BatchNorm3d_not_tracking_stats',
-    'test_nn_LayerNorm_1d_elementwise_affine',
-    'test_nn_LayerNorm_1d_no_elementwise_affine',
-    'test_nn_LayerNorm_3d_elementwise_affine',
-    'test_nn_LayerNorm_3d_no_elementwise_affine',
-    'test_nn_Linear_no_bias',
 }
 
 DISABLE_AUTODIFF_SUBGRAPH_INLINING = {
