8c5235943fcf3df0da5073cf11f4cfa2ba9b00f4
Chandler Zuo <chandlerzuo@fb.com>
2018-12-04 08:20:22 -0800

    Fix lint errors
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/14638
    
    Reviewed By: ezyang
    
    Differential Revision: D13281496
    
    fbshipit-source-id: 32721678a412e250d3c6a906125e6bf9ea5c5ef6

--- c10/Half.h
+++ c10/Half.h
@@ -96,35 +96,44 @@ namespace detail {
         uint32_t renorm_shift = __builtin_clz(nonsign);
 #endif
         renorm_shift = renorm_shift > 5 ? renorm_shift - 5 : 0;
-  	/*
-  	 * Iff half-precision number has exponent of 15, the addition overflows it into bit 31,
-  	 * and the subsequent shift turns the high 9 bits into 1. Thus
-  	 *   inf_nan_mask ==
-  	 *                   0x7F800000 if the half-precision number had exponent of 15 (i.e. was NaN or infinity)
-  	 *                   0x00000000 otherwise
-  	 */
-  	const int32_t inf_nan_mask = ((int32_t) (nonsign + 0x04000000) >> 8) & INT32_C(0x7F800000);
-  	/*
-  	 * Iff nonsign is 0, it overflows into 0xFFFFFFFF, turning bit 31 into 1. Otherwise, bit 31 remains 0.
-  	 * The signed shift right by 31 broadcasts bit 31 into all bits of the zero_mask. Thus
-  	 *   zero_mask ==
-  	 *                0xFFFFFFFF if the half-precision number was zero (+0.0h or -0.0h)
-  	 *                0x00000000 otherwise
-  	 */
-  	const int32_t zero_mask = (int32_t) (nonsign - 1) >> 31;
-  	/*
-  	 * 1. Shift nonsign left by renorm_shift to normalize it (if the input was denormal)
-  	 * 2. Shift nonsign right by 3 so the exponent (5 bits originally) becomes an 8-bit field and 10-bit mantissa
-  	 *    shifts into the 10 high bits of the 23-bit mantissa of IEEE single-precision number.
-  	 * 3. Add 0x70 to the exponent (starting at bit 23) to compensate the different in exponent bias
-  	 *    (0x7F for single-precision number less 0xF for half-precision number).
-  	 * 4. Subtract renorm_shift from the exponent (starting at bit 23) to account for renormalization. As renorm_shift
-  	 *    is less than 0x70, this can be combined with step 3.
-  	 * 5. Binary OR with inf_nan_mask to turn the exponent into 0xFF if the input was NaN or infinity.
-  	 * 6. Binary ANDNOT with zero_mask to turn the mantissa and exponent into zero if the input was zero.
-  	 * 7. Combine with the sign of the input number.
-  	 */
-  	return sign | ((((nonsign << renorm_shift >> 3) + ((0x70 - renorm_shift) << 23)) | inf_nan_mask) & ~zero_mask);
+        /*
+         * Iff half-precision number has exponent of 15, the addition overflows
+         * it into bit 31, and the subsequent shift turns the high 9 bits
+         * into 1. Thus inf_nan_mask == 0x7F800000 if the half-precision number
+         * had exponent of 15 (i.e. was NaN or infinity) 0x00000000 otherwise
+         */
+        const int32_t inf_nan_mask =
+            ((int32_t)(nonsign + 0x04000000) >> 8) & INT32_C(0x7F800000);
+        /*
+         * Iff nonsign is 0, it overflows into 0xFFFFFFFF, turning bit 31
+         * into 1. Otherwise, bit 31 remains 0. The signed shift right by 31
+         * broadcasts bit 31 into all bits of the zero_mask. Thus zero_mask ==
+         * 0xFFFFFFFF if the half-precision number was zero (+0.0h or -0.0h)
+         * 0x00000000 otherwise
+         */
+        const int32_t zero_mask = (int32_t)(nonsign - 1) >> 31;
+        /*
+         * 1. Shift nonsign left by renorm_shift to normalize it (if the input
+         * was denormal)
+         * 2. Shift nonsign right by 3 so the exponent (5 bits originally)
+         * becomes an 8-bit field and 10-bit mantissa shifts into the 10 high
+         * bits of the 23-bit mantissa of IEEE single-precision number.
+         * 3. Add 0x70 to the exponent (starting at bit 23) to compensate the
+         * different in exponent bias (0x7F for single-precision number less 0xF
+         * for half-precision number).
+         * 4. Subtract renorm_shift from the exponent (starting at bit 23) to
+         * account for renormalization. As renorm_shift is less than 0x70, this
+         * can be combined with step 3.
+         * 5. Binary OR with inf_nan_mask to turn the exponent into 0xFF if the
+         * input was NaN or infinity.
+         * 6. Binary ANDNOT with zero_mask to turn the mantissa and exponent
+         * into zero if the input was zero.
+         * 7. Combine with the sign of the input number.
+         */
+        return sign |
+            ((((nonsign << renorm_shift >> 3) + ((0x70 - renorm_shift) << 23)) |
+              inf_nan_mask) &
+             ~zero_mask);
   }
 
   /*
