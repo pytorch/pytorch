53194573e45a2e5886302c0b49ccac68d8ee4721
Wanchao Liang <wanchaol@users.noreply.github.com>
2018-12-03 20:38:53 -0800

    enable NoneValue parameter assignment for WeakScriptModule (#14715)
    Summary:
    This PR:
    
    1. Handle None value attr in the WeakScriptModuleProxy
    2. add back module tests that now passing
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14715
    
    Differential Revision: D13313573
    
    Pulled By: wanchaol
    
    fbshipit-source-id: a6b7892707350290a6d69b6f6270ad089bfc954b

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -1146,17 +1146,22 @@ if _enabled:
             self.__dict__['_initialized'] = False
             super(WeakScriptModuleProxy, self).__init__()
 
-            # Copy constants
             self.__dict__["_original"] = weakref.ref(original)
-            self.__dict__["_constants_set"] = set(getattr(original, "__constants__", []))
 
             # Copy Parameters / Modules / Buffers
             for name in dir(original):
                 item = getattr(original, name)
-                if isinstance(item, Parameter) or (isinstance(item, Module) and item is not self):
+                if item is None and name in original._parameters:
+                    # XXX: treat None value simply as module attributes instead of adding them to the parameter list
+                    # TODO: need to handle this more generally when non-tensor attributes added to module
+                    object.__setattr__(self, name, item)
+                elif isinstance(item, Parameter) or (isinstance(item, Module) and item is not self):
                     ScriptModule.__setattr__(self, name, item)
             for name in original._buffers:
                 self.register_buffer(name, original._buffers[name])
+
+            # Copy constants
+            self.__dict__["_constants_set"] = set(getattr(original, "__constants__", []))
 
             self.__dict__["_initialized"] = True
             _create_methods_from_stubs(self, stubs)
