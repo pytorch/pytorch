f89de64796104ca3a48a465a62017edea1cd7506
Adam Paszke <adam.paszke@gmail.com>
2018-12-05 00:07:51 -0800

    Use AT_WARN for warnings in the JIT (#14770)
    Summary:
    Previously their implementation dispatched to prim::Print, which kept
    printing the warnings.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14770
    
    Differential Revision: D13327629
    
    Pulled By: suo
    
    fbshipit-source-id: b9913f533d4530eb7c29146c39981ba7f72b6b68

--- aten/src/ATen/core/interned_strings.h
+++ aten/src/ATen/core/interned_strings.h
@@ -73,6 +73,7 @@ namespace c10 {
   _(prim, NoneGenerator)           \
   _(prim, MMTreeReduce)            \
   _(prim, MMBatchSide)             \
+  _(aten, warn)                    \
   _(aten, floordiv)                \
   _(aten, __round_to_zero_floordiv)\
   _(prim, fork)                    \
