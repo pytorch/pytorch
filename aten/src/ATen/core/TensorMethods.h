71c3b590b95265b3f75679b427adcf3e74e192bf
Sebastian Messmer <messmer@fb.com>
2018-12-06 15:52:15 -0800

    Move TensorOptions, DefaultTensorOptions to c10
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/14746
    
    Reviewed By: ezyang
    
    Differential Revision: D13318644
    
    fbshipit-source-id: b703d7dc67e75d9e9571c80d62a100c5fc4e84df

--- aten/src/ATen/core/TensorMethods.h
+++ aten/src/ATen/core/TensorMethods.h
@@ -2,6 +2,7 @@
 
 #include "ATen/core/Tensor.h"
 #include <c10/core/Scalar.h>
+#include <c10/macros/Macros.h>
 #include "ATen/core/SparseTensorRef.h"
 #include "ATen/core/Type.h"
 #include "ATen/core/TensorOptions.h"
