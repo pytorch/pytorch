22bac4e219e50b683ab2f5c934d0cb3c04ad75c1
Jerry Zhang <jerryzh@fb.com>
2018-12-06 10:56:14 -0800

    Move numa.{h, cc} to c10/util (#14393)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14393
    
    att
    
    Reviewed By: ezyang
    
    Differential Revision: D13205604
    
    fbshipit-source-id: 54166492d31827b0343ed070cc36a825dd86e2ed

---
+++ c10/util/numa.cpp
@@ -0,0 +1,131 @@
+#include "c10/util/numa.h"
+
+#if defined(__linux__) && !defined(C10_DISABLE_NUMA) && C10_MOBILE == 0
+#include <numa.h>
+#include <numaif.h>
+#include <unistd.h>
+#define C10_ENABLE_NUMA
+#endif
+
+namespace c10 {
+
+#ifdef C10_ENABLE_NUMA
+bool IsNUMAEnabled() {
+  return numa_available() >= 0;
+}
+
+void NUMABind(int numa_node_id) {
+  if (numa_node_id < 0) {
+    return;
+  }
+  if (!IsNUMAEnabled()) {
+    VLOG(1) << "NUMA is not enabled";
+    return;
+  }
+
+  AT_CHECK(
+      numa_node_id <= numa_max_node(),
+      "NUMA node id ", numa_node_id, " is unavailable");
+
+  auto bm = numa_allocate_nodemask();
+  numa_bitmask_setbit(bm, numa_node_id);
+  numa_bind(bm);
+  numa_bitmask_free(bm);
+}
+
+int GetNUMANode(const void* ptr) {
+  if (!IsNUMAEnabled()) {
+    VLOG(1) << "NUMA is not enabled";
+    return -1;
+  }
+  AT_ASSERT(ptr);
+
+  int numa_node = -1;
+  AT_CHECK(
+      get_mempolicy(
+          &numa_node, NULL, 0, const_cast<void*>(ptr), MPOL_F_NODE | MPOL_F_ADDR) == 0,
+      "Unable to get memory policy, errno:", errno);
+  return numa_node;
+}
+
+int GetNumNUMANodes() {
+  if (!IsNUMAEnabled()) {
+    VLOG(1) << "NUMA is not enabled";
+    return -1;
+  }
+
+  return numa_num_configured_nodes();
+}
+
+void NUMAMove(void* ptr, size_t size, int numa_node_id) {
+  if (numa_node_id < 0) {
+    return;
+  }
+  if (!IsNUMAEnabled()) {
+    VLOG(1) << "NUMA is not enabled";
+    return;
+  }
+  AT_ASSERT(ptr);
+
+  uintptr_t page_start_ptr = ((reinterpret_cast<uintptr_t>(ptr)) & ~(getpagesize() - 1));
+  ptrdiff_t offset = reinterpret_cast<uintptr_t>(ptr) - page_start_ptr;
+  // Avoid extra dynamic allocation and NUMA api calls
+  AT_ASSERT(numa_node_id >= 0 && static_cast<unsigned>(numa_node_id) < sizeof(unsigned long) * 8);
+  unsigned long mask = 1UL << numa_node_id;
+  AT_CHECK(
+      mbind(
+          reinterpret_cast<void*>(page_start_ptr),
+          size + offset,
+          MPOL_BIND,
+          &mask,
+          sizeof(mask) * 8,
+          MPOL_MF_MOVE | MPOL_MF_STRICT) == 0,
+      "Could not move memory to a NUMA node");
+}
+
+int GetCurrentNUMANode() {
+  if (!IsNUMAEnabled()) {
+    VLOG(1) << "NUMA is not enabled";
+    return -1;
+  }
+
+  auto n = numa_node_of_cpu(sched_getcpu());
+  return n;
+}
+
+#else // C10_ENABLE_NUMA
+
+bool IsNUMAEnabled() {
+  return false;
+}
+
+void NUMABind(int numa_node_id) {
+  if (numa_node_id >= 0) {
+    VLOG(1) << "NUMA is not enabled";
+  }
+}
+
+int GetNUMANode(const void* ptr) {
+  VLOG(1) << "NUMA is not enabled";
+  return -1;
+}
+
+int GetNumNUMANodes() {
+  VLOG(1) << "NUMA is not enabled";
+  return -1;
+}
+
+void NUMAMove(void* ptr, size_t size, int numa_node_id) {
+  if (numa_node_id >= 0) {
+    VLOG(1) << "NUMA is not enabled";
+  }
+}
+
+int GetCurrentNUMANode() {
+  VLOG(1) << "NUMA is not enabled";
+  return -1;
+}
+
+#endif // C10_NUMA_ENABLED
+
+} // namespace c10
