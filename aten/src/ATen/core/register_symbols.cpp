6955f8cc7a5ea508f93b2c0b8cc74f0ad01a1d92
Giuseppe Ottaviano <ott@fb.com>
2018-12-10 11:54:45 -0800

    Improve build time of register_symbols.cpp without compiler hacks (#14911)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14911
    
    In optimized modes the compiler tries to inline all the
    `unordered_map::operator[]` calls, creating a massive amount of code
    which takes several minutes to optimize. Instead, create a table of
    PODs and populate the maps using a simple loop.
    
    Reviewed By: soumith, luciang
    
    Differential Revision: D13382948
    
    fbshipit-source-id: b6752921e0f7213595d26b39e4397f6a3897960b

--- aten/src/ATen/core/register_symbols.cpp
+++ aten/src/ATen/core/register_symbols.cpp
@@ -1,16 +1,38 @@
 #include <ATen/core/interned_strings_class.h>
 
-// This file is compiled with -O0 because the fully-macro-expanded
-// function is huge and only called once at startup.
-
 namespace c10 {
+
+namespace {
+
+struct Entry {
+  const char* const qual_name;
+  const char* const unqual_name;
+  const Symbol sym;
+  const Symbol ns_sym;
+};
+
+constexpr Entry entries[] = {
+#define SYMBOL_ENTRY(n, s) {#n "::" #s, #s, n::s, namespaces::n},
+
+    FORALL_NS_SYMBOLS(SYMBOL_ENTRY)
+#undef SYMBOL_ENTRY
+};
+
+} // namespace
+
 InternedStrings::InternedStrings()
     : sym_to_info_(static_cast<size_t>(_keys::num_symbols)) {
-#define REGISTER_SYMBOL(n, s)        \
-  string_to_sym_[#n "::" #s] = n::s; \
-  sym_to_info_[n::s] = {namespaces::n, #n "::" #s, #s};
-
-  FORALL_NS_SYMBOLS(REGISTER_SYMBOL)
-#undef REGISTER_SYMBOL
+  // Instead of a loop, this could be done by expanding the
+  // assignments directly into FORALL_NS_SYMBOLS, but it would create
+  // a huge function (thanks to all the std::string constructors and
+  // operator[]s) which would take several minutes to optimize. A
+  // static C array of constexpr-constructible structs takes instead
+  // no time to compile.
+  for (const auto& entry : entries) {
+    string_to_sym_[entry.qual_name] = entry.sym;
+    sym_to_info_[entry.sym] = {
+        entry.ns_sym, entry.qual_name, entry.unqual_name};
+  }
 }
+
 } // namespace c10
