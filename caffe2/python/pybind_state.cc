a51fe386c8e5f3198c948fe0a08458892a7901c4
Jerry Zhang <jerryzh@fb.com>
2018-12-10 14:17:43 -0800

    caffe2/caffe2/contrib/script (#15007)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15007
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14979
    
    att
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D13286191
    
    fbshipit-source-id: b8a6bc7aea44487aea4dcf7f44c858fd30c6293c

--- caffe2/python/pybind_state.cc
+++ caffe2/python/pybind_state.cc
@@ -6,7 +6,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "caffe2/contrib/script/compiler.h"
 #include "caffe2/core/asan.h"
 #include "caffe2/core/blob_stats.h"
 #include "caffe2/core/db.h"
@@ -937,29 +936,6 @@ void addObjectMethods(py::module& m) {
               pyout.push_back(TensorFetcher().FetchTensor(t, true).obj);
             }
             return pyout;
-          });
-
-  py::class_<script::CompilationUnit>(m, "CompilationUnit")
-      .def(py::init<>())
-      .def("define", &script::CompilationUnit::define)
-      .def("get_proto", &script::CompilationUnit::getProto)
-      .def(
-          "create_net",
-          [](script::CompilationUnit* self, const std::string& name) {
-            auto net = self->createNet(gWorkspace, name);
-            CAFFE_ENFORCE(net);
-            return net;
-          })
-      .def(
-          "extern",
-          [](script::CompilationUnit* self,
-             const std::string& name,
-             py::object py_proto) {
-            py::bytes bytes = py_proto.attr("SerializeToString")();
-            std::unique_ptr<caffe2::NetDef> proto(new NetDef());
-            CAFFE_ENFORCE(ParseProtoFromLargeString(
-                bytes.cast<std::string>(), proto.get()));
-            self->defineExtern(name, std::move(proto));
           });
 }
 
