2588cc3fabcaff87fd5bb95f3b23428f980eebd8
Duc Ngo <duc@fb.com>
2018-12-10 13:51:05 -0800

    caffe2 - easy - test utils to create operator
    Summary:
    Test utils to create an operator
    
    On top of D13370461
    
    Differential Revision: D13382773
    
    fbshipit-source-id: 779d10329166242378c378a02f47e30b6a9afc1a

--- caffe2/core/test_utils.cc
+++ caffe2/core/test_utils.cc
@@ -13,9 +13,33 @@ const caffe2::Tensor& getTensor(
 }
 
 caffe2::Tensor* createTensor(
-    caffe2::Workspace& workspace,
-    const std::string& name) {
-  return BlobGetMutableTensor(workspace.CreateBlob(name), caffe2::CPU);
+    const std::string& name,
+    caffe2::Workspace* workspace) {
+  return BlobGetMutableTensor(workspace->CreateBlob(name), caffe2::CPU);
+}
+
+caffe2::OperatorDef* createOperator(
+    const std::string& type,
+    const std::vector<string>& inputs,
+    const std::vector<string>& outputs,
+    caffe2::NetDef* net) {
+  auto* op = net->add_op();
+  op->set_type(type);
+  for (const auto& in : inputs) {
+    op->add_input(in);
+  }
+  for (const auto& out : outputs) {
+    op->add_output(out);
+  }
+  return op;
+}
+
+NetMutator& NetMutator::newOp(
+    const std::string& type,
+    const std::vector<string>& inputs,
+    const std::vector<string>& outputs) {
+  createOperator(type, inputs, outputs, net_);
+  return *this;
 }
 
 } // namespace testing
--- caffe2/core/test_utils.h
+++ caffe2/core/test_utils.h
@@ -26,8 +26,28 @@ const caffe2::Tensor& getTensor(
 
 // Create a new tensor in the workspace.
 caffe2::Tensor* createTensor(
-    caffe2::Workspace& workspace,
-    const std::string& name);
+    const std::string& name,
+    caffe2::Workspace* workspace);
+
+// Create a new operator in the net.
+caffe2::OperatorDef* createOperator(
+    const std::string& type,
+    const std::vector<string>& inputs,
+    const std::vector<string>& outputs,
+    caffe2::NetDef* net);
+
+// Coincise util class to mutate a net in a chaining fashion.
+struct NetMutator {
+  explicit NetMutator(caffe2::NetDef* net) : net_(net) {}
+
+  NetMutator& newOp(
+      const std::string& type,
+      const std::vector<string>& inputs,
+      const std::vector<string>& outputs);
+
+ private:
+  caffe2::NetDef* net_;
+};
 
 } // namespace testing
 } // namespace caffe2
