b28890d19b77dec9a31d348d69665da5641405ec
Andy Chen <andersenchen@fb.com>
2018-12-10 10:15:31 -0800

    Actually remove method max_unpool3d

--- aten/src/ATen/core/TensorMethods.h
+++ aten/src/ATen/core/TensorMethods.h
@@ -1242,9 +1242,6 @@ inline Tensor Tensor::pow(const Tensor & exponent) const {
 inline Tensor Tensor::alias() const {
     return type().alias(*this);
 }
-inline Tensor Tensor::max_unpool3d(const Tensor & indices, IntList output_size, IntList stride, IntList padding) const {
-    return type().max_unpool3d(*this, indices, output_size, stride, padding);
-}
 
 inline bool Tensor::is_variable() const noexcept {
   return impl_->is_variable();
