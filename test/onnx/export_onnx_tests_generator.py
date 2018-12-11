560005fc9920f3067138b5e0b4aa2f3f74b24f2e
zrphercule <zrphercule@gmail.com>
2018-12-10 16:11:50 -0800

    update onnx coverage script for more accurate result

--- test/onnx/export_onnx_tests_generator.py
+++ test/onnx/export_onnx_tests_generator.py
@@ -33,7 +33,8 @@ def get_test_name(testcase):
 # Take a test case (a dict) as input, return the input for the module.
 def gen_input(testcase):
     if "input_size" in testcase:
-        if (testcase["input_size"] == ()):
+        if testcase["input_size"] == () and "desc" in testcase and testcase["desc"][-6:] == "scalar":
+            print()
             testcase["input_size"] = (1,);
         return Variable(torch.randn(*testcase["input_size"]))
     elif "input_fn" in testcase:
