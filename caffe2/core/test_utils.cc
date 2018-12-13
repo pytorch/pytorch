f466e5d70b12eff659063cddc28cf6d1d80af90d
Duc Ngo <duc@fb.com>
2018-12-13 13:20:03 -0800

    caffe2 - easy - test utils to create operator
    Summary:
    Test utils to create an operator
    
    On top of D13370461
    
    Differential Revision: D13382773
    
    fbshipit-source-id: f9c841f046b040ac9bca442b6793f085440c670f

--- caffe2/core/test_utils.cc
+++ caffe2/core/test_utils.cc
@@ -18,5 +18,29 @@ caffe2::Tensor* createTensor(
   return BlobGetMutableTensor(workspace->CreateBlob(name), caffe2::CPU);
 }
 
+caffe2::OperatorDef* createOperator(
+    const std::string& type,
+    const std::vector<string>& inputs,
+    const std::vector<string>& outputs,
+    caffe2::NetDef* net) {
+  auto* op = net->add_op();
+  op->set_type(type);
+  for (const auto& in : inputs) {
+    op->add_input(in);
+  }
+  for (const auto& out : outputs) {
+    op->add_output(out);
+  }
+  return op;
+}
+
+NetMutator& NetMutator::newOp(
+    const std::string& type,
+    const std::vector<string>& inputs,
+    const std::vector<string>& outputs) {
+  createOperator(type, inputs, outputs, net_);
+  return *this;
+}
+
 } // namespace testing
 } // namespace caffe2
