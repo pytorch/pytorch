9f08ea6966826f06a06deac2924ca12dbd90500c
rohithkrn <rohith.nallamaddi@gmail.com>
2018-12-12 17:05:56 -0800

    import caffe2_pb2

--- caffe2/python/operator_test/momentum_sgd_test.py
+++ caffe2/python/operator_test/momentum_sgd_test.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from caffe2.proto import caffe2_pb2
 from caffe2.python import core, workspace
 import caffe2.python.hypothesis_test_util as hu
 import caffe2.python.serialized_test.serialized_test_util as serial
