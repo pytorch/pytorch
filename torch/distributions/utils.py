df6533f2754b1399ca262fa7ed8618b099d9a946
vishwakftw <cs15btech11043@iith.ac.in>
2018-12-13 15:17:40 +0530

    Remove _finfo; replace _finfo usage with torch.finfo
    This PR removes the usage of _finfo defined in torch.distributions.utils and changes the call sites
    to use torch.finfo instead

--- torch/distributions/utils.py
+++ torch/distributions/utils.py
@@ -1,35 +1,8 @@
-from collections import namedtuple
 from functools import update_wrapper
 from numbers import Number
 import math
 import torch
 import torch.nn.functional as F
-
-# This follows semantics of numpy.finfo.
-_Finfo = namedtuple('_Finfo', ['eps', 'tiny'])
-_FINFO = {
-    torch.HalfStorage: _Finfo(eps=0.00097656, tiny=6.1035e-05),
-    torch.FloatStorage: _Finfo(eps=1.19209e-07, tiny=1.17549e-38),
-    torch.DoubleStorage: _Finfo(eps=2.22044604925e-16, tiny=2.22507385851e-308),
-    torch.cuda.HalfStorage: _Finfo(eps=0.00097656, tiny=6.1035e-05),
-    torch.cuda.FloatStorage: _Finfo(eps=1.19209e-07, tiny=1.17549e-38),
-    torch.cuda.DoubleStorage: _Finfo(eps=2.22044604925e-16, tiny=2.22507385851e-308),
-}
-
-
-def _finfo(tensor):
-    r"""
-    Return floating point info about a `Tensor`:
-    - `.eps` is the smallest number that can be added to 1 without being lost.
-    - `.tiny` is the smallest positive number greater than zero
-      (much smaller than `.eps`).
-
-    Args:
-        tensor (Tensor): tensor of floating point data.
-    Returns:
-        _Finfo: a `namedtuple` with fields `.eps` and `.tiny`.
-    """
-    return _FINFO[tensor.storage_type()]
 
 
 # promote numbers to tensors of dtype torch.get_default_dtype()
@@ -100,7 +73,7 @@ def logits_to_probs(logits, is_binary=False):
 
 
 def clamp_probs(probs):
-    eps = _finfo(probs).eps
+    eps = torch.finfo(probs.dtype).eps
     return probs.clamp(min=eps, max=1 - eps)
 
 
