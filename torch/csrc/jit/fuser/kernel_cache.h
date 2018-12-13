c6084f753f242fb3216977b0b93e0f780e0c2863
Richard Zou <zou3519@gmail.com>
2018-12-13 07:51:08 -0800

    Reuse KernelSpec for FusionGroups with equivalent graphs (#14541)
    Summary:
    Before this PR, loop unrolling + the graph fuser was creating multiple
    FusionGroups with the same bodies (with different variable names) for
    JIT LSTMs. Each FusionGroup got registered to a separate fusion key;
    each key resulted in a different compilation for the same
    specializations.
    
    This PR makes it so that when registering FusionGroups with the fusion
    compiler, the compiler first checks the KernelSpec cache to see if the
    FusionGroup's graph exists already. If it does, then return the
    corresponding KernelSpec's key to share compiled kernels.
    
    In addition, graphs in the KernelSpec cache are canonicalized before
    being cached. I added a flag to the canonicalize pass to remove unique
    names of values.
    
    This shortens the compile time for a JIT LSTM (seq_len of 100, loop
    unroll factor of 8) from 5.3s to 2.3s. Most of this compile time is
    running the graph fuser and/or fusion compiler; while this PR
    makes it so that there is only one unique kernel in the forward pass,
    there are a lot of different kernels (6) in the backward pass
    (after loop unrolling) that should be investigated.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/14541
    
    Differential Revision: D13324487
    
    Pulled By: zou3519
    
    fbshipit-source-id: b841d82ed35a959b5cfc72db033bf5a7b42cc4fb

--- torch/csrc/jit/fuser/kernel_cache.h
+++ torch/csrc/jit/fuser/kernel_cache.h
@@ -14,11 +14,21 @@ namespace torch { namespace jit { namespace fuser {
 
 // A thread-safe cache interface.
 
+// Normalizes the graph by canonicalizing and erasing shape information
+TORCH_API std::shared_ptr<Graph> normalizeGraphForCache(const std::shared_ptr<Graph>& graph);
+
 // Stores the given graph, returning the key used to access it
 TORCH_API int64_t store(std::shared_ptr<Graph> graph);
 
+// Given a graph, find a KernelSpec based on it
+TORCH_API at::optional<KernelSpec*> lookupGraph(std::shared_ptr<Graph> graph);
+
 // Returns the graph corresponding to the given key (if it exists)
 TORCH_API at::optional<KernelSpec*> retrieve(const int64_t key);
+
+// Returns the size of the fusion key -> KernelSpec cache.
+// Only used for testing.
+TORCH_API int64_t debugNumCachedKernelSpecs();
 
 } // namespace fuser
 } // namespace jit
--- torch/csrc/jit/passes/canonicalize.h
+++ torch/csrc/jit/passes/canonicalize.h
@@ -4,6 +4,7 @@
 
 namespace torch { namespace jit {
 
-TORCH_API std::shared_ptr<Graph> Canonicalize(const std::shared_ptr<Graph>& graph);
+TORCH_API std::shared_ptr<Graph> Canonicalize(
+    const std::shared_ptr<Graph>& graph, bool keep_unique_names=true);
 
 }}
