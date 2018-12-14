30f304e4e55d1cebeb33f43c4dfebbdb23bea19b
Duc Ngo <duc@fb.com>
2018-12-13 17:42:48 -0800

    caffe2 - easy - test utils for tensor assertion
    Summary: Add test utils for assertion of a tensor (sizes and values)
    
    Differential Revision: D13401146
    
    fbshipit-source-id: cdf410c691767c48678c79238783d5ad16847867

--- caffe2/core/test_utils.cc
+++ caffe2/core/test_utils.cc
@@ -18,6 +18,13 @@ void assertTensorEqualsWithType(
 namespace caffe2 {
 namespace testing {
 
+// Asserts that two float values are close within epsilon.
+void assertNear(float value1, float value2, float epsilon) {
+  // These two enforces will give good debug messages.
+  CAFFE_ENFORCE_LE(value1, value2 + epsilon);
+  CAFFE_ENFORCE_GE(value1, value2 - epsilon);
+}
+
 void assertTensorEquals(const TensorCPU& tensor1, const TensorCPU& tensor2) {
   CAFFE_ENFORCE_EQ(tensor1.sizes(), tensor2.sizes());
   if (tensor1.IsType<float>()) {
@@ -49,6 +56,7 @@ void assertTensorListEquals(
 const caffe2::Tensor& getTensor(
     const caffe2::Workspace& workspace,
     const std::string& name) {
+  CAFFE_ENFORCE(workspace.HasBlob(name));
   return workspace.GetBlob(name)->Get<caffe2::Tensor>();
 }
 
