f9866e52d5da9d466c8415e94be927687aa7b63a
Richard Zou <zou3519@gmail.com>
2018-12-16 18:58:23 -0800

    Fix ONNX tests

--- test/onnx/model_defs/rnn_model_with_packed_sequence.py
+++ test/onnx/model_defs/rnn_model_with_packed_sequence.py
@@ -10,7 +10,8 @@ class RnnModelWithPackedSequence(nn.Module):
 
     def forward(self, input, *args):
         args, seq_lengths = args[:-1], args[-1]
-        input = rnn_utils.pack_padded_sequence(input, seq_lengths, self.batch_first)
+        input = rnn_utils.pack_padded_sequence(
+            input, seq_lengths, self.batch_first, enforce_sorted=True)
         rets = self.model(input, *args)
         ret, rets = rets[0], rets[1:]
         ret, _ = rnn_utils.pad_packed_sequence(ret, self.batch_first)
