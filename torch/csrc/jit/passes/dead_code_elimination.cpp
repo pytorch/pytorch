516abe443fa9962671a7e55da1cc999afaa7af24
Ailing Zhang <ailzhang@fb.com>
2018-12-16 21:30:39 -0800

    resolve conflict

--- torch/csrc/jit/passes/dead_code_elimination.cpp
+++ torch/csrc/jit/passes/dead_code_elimination.cpp
@@ -205,7 +205,7 @@ class DeadCodeEliminator {
           sweep(block, true);
         }
       }
-      if (!marked_.count(node)) {
+      if (!(marked_.count(node) or node->hasUses())) {
         it.destroyCurrent();
       }
     }
