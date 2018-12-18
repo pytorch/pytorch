08fd6b62e5f522233cf3e3fb616a9ed8afa88b2a
Chandler Zuo <chandlerzuo@fb.com>
2018-12-18 15:57:07 -0800

    initial commit

--- aten/src/THNN/init.cpp
+++ aten/src/THNN/init.cpp
@@ -157,9 +157,6 @@
 #include <THNN/generic/SpatialAveragePooling.c>
 #include <TH/THGenerateFloatTypes.h>
 
-#include <THNN/generic/SpatialFractionalMaxPooling.c>
-#include <TH/THGenerateFloatTypes.h>
-
 #include <THNN/generic/SpatialDilatedMaxPooling.c>
 #include <TH/THGenerateFloatTypes.h>
 
