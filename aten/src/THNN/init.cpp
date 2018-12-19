b8353d505ddb709a6977cfefe37f172a74987097
Chandler Zuo <chandlerzuo@fb.com>
2018-12-19 11:54:34 -0800

    migrate CPU

--- aten/src/THNN/init.cpp
+++ aten/src/THNN/init.cpp
@@ -157,6 +157,9 @@
 #include <THNN/generic/SpatialAveragePooling.c>
 #include <TH/THGenerateFloatTypes.h>
 
+#include <THNN/generic/SpatialFractionalMaxPooling.c>
+#include <TH/THGenerateFloatTypes.h>
+
 #include <THNN/generic/SpatialDilatedMaxPooling.c>
 #include <TH/THGenerateFloatTypes.h>
 
