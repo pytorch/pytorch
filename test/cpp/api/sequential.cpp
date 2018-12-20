52283b7c7883f108e0ad9bfebc93c3eabf101e7e
Peter Goldsborough <psag@fb.com>
2018-12-19 17:31:50 -0800

    Change raw string literal to old style literal because gcc 4.8 cant

--- test/cpp/api/sequential.cpp
+++ test/cpp/api/sequential.cpp
@@ -334,12 +334,12 @@ TEST_F(SequentialTest, PrettyPrintSequential) {
       LSTM(4, 5));
   ASSERT_EQ(
       c10::str(sequential),
-R"(torch::nn::Sequential(
-  (0): torch::nn::Linear(in=10, out=3, with_bias=true)
-  (1): torch::nn::Conv2d(input_channels=1, output_channels=2, kernel_size=[3, 3], stride=[1, 1])
-  (2): torch::nn::Dropout(rate=0.5)
-  (3): torch::nn::BatchNorm(features=5, eps=1e-05, momentum=0.1, affine=true, stateful=true)
-  (4): torch::nn::Embedding(count=4, dimension=10)
-  (5): torch::nn::LSTM(input_size=4, hidden_size=5, layers=1, dropout=0)
-))");
+      "torch::nn::Sequential(\n"
+      "  (0): torch::nn::Linear(in=10, out=3, with_bias=true)\n"
+      "  (1): torch::nn::Conv2d(input_channels=1, output_channels=2, kernel_size=[3, 3], stride=[1, 1])\n"
+      "  (2): torch::nn::Dropout(rate=0.5)\n"
+      "  (3): torch::nn::BatchNorm(features=5, eps=1e-05, momentum=0.1, affine=true, stateful=true)\n"
+      "  (4): torch::nn::Embedding(count=4, dimension=10)\n"
+      "  (5): torch::nn::LSTM(input_size=4, hidden_size=5, layers=1, dropout=0)\n"
+      ")");
 }
