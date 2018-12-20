f984d8bde0bed2cf77b0cfdab6577be39f162ed1
Derek Kim <bluewhale8202@gmail.com>
2018-12-21 01:34:57 +0900

    Fixed the importing process, which had two problems - Importing path is not consistent with other modules in `tools` package. For example, other modules imports `from tools...` not `from setuptools...`. - Because this module is part of the tools package, it should be able to run with `python -m tools.build_libtorch.py` command.

--- tools/build_libtorch.py
+++ tools/build_libtorch.py
@@ -1,16 +1,23 @@
+# This module can be run either as a module, or as a standalone script.
+# cd <pytorch_root>; python -m tools.setup.py  # module
+# python ./.../build_libtorch.py             # standalone script
+
 import argparse
 import os
 import shlex
 import subprocess
 import sys
 
-from setup_helpers.env import check_env_flag, hotpatch_build_env_vars
+dirname = os.path.dirname
+pytorch_root = dirname(dirname(os.path.abspath(__file__)))
+sys.path.append(pytorch_root)
 
+from tools.setup_helpers.env import check_env_flag, hotpatch_build_env_vars
 
 hotpatch_build_env_vars()
 
-from setup_helpers.cuda import USE_CUDA
-from setup_helpers.dist_check import USE_DISTRIBUTED, USE_GLOO_IBVERBS, IS_LINUX
+from tools.setup_helpers.cuda import USE_CUDA
+from tools.setup_helpers.dist_check import USE_DISTRIBUTED, USE_GLOO_IBVERBS, IS_LINUX
 
 if __name__ == '__main__':
     # Placeholder for future interface. For now just gives a nice -h.
@@ -22,10 +29,16 @@ if __name__ == '__main__':
     os.environ['ONNX_NAMESPACE'] = 'onnx_torch'
     os.environ['PYTORCH_PYTHON'] = sys.executable
 
-    tools_path = os.path.dirname(os.path.abspath(__file__))
+    tools_path = os.path.join(pytorch_root, 'tools')
+    build_path = os.path.join(pytorch_root, 'build')
+
     if sys.platform == 'win32':
+        # TODO: handle cwd if needed
+        kwargs = {}
         build_pytorch_libs = os.path.join(tools_path, 'build_pytorch_libs.bat')
     else:
+        os.makedirs(build_path, exist_ok=True)
+        kwargs = {'cwd': build_path}
         build_pytorch_libs = os.path.join(tools_path, 'build_pytorch_libs.sh')
 
     command = [build_pytorch_libs, '--use-nnpack']
@@ -45,4 +58,5 @@ if __name__ == '__main__':
 
     sys.stdout.flush()
     sys.stderr.flush()
-    subprocess.check_call(command, universal_newlines=True)
+
+    subprocess.check_call(command, universal_newlines=True, **kwargs)
