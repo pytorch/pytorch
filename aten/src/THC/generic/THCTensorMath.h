d0a909dc6e01196582e3d9e07e0be53c3af3d72e
Gregory Chanan <gchanan@fb.com>
2018-12-20 10:51:09 -0800

    Move torch.logspace to ATen and parallelize on CPU.

--- aten/src/THC/generic/THCTensorMath.h
+++ aten/src/THC/generic/THCTensorMath.h
@@ -19,12 +19,6 @@ THC_API void THCTensor_(eye)(THCState *state, THCTensor *self, int64_t n, int64_
 
 THC_API accreal THCTensor_(trace)(THCState *state, THCTensor *self);
 
-#if defined(THC_REAL_IS_FLOAT) || defined(THC_REAL_IS_DOUBLE) || defined(THC_REAL_IS_HALF)
-
-THC_API void THCTensor_(logspace)(THCState *state, THCTensor *r_, scalar_t a, scalar_t b, int64_t n);
-
-#endif
-
 THC_API void THCTensor_(range)(THCState *state, THCTensor *r_, accreal xmin, accreal xmax, accreal step);
 THC_API void THCTensor_(arange)(THCState *state, THCTensor *r_, accreal xmin, accreal xmax, accreal step);
 
