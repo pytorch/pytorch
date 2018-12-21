a62c2ceffa6d528ea8b978cc4abaad7d619dc75d
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-12-21 01:56:29 -0500

    experiment

--- test/common_methods_invocations.py
+++ test/common_methods_invocations.py
@@ -982,6 +982,7 @@ def run_additional_tri_tests(self, device):
 
 def unpack_variables(args):
     print(type(args).__name__)
+    print(istuple(args))
     if istuple(args):
         return tuple(unpack_variables(elem) for elem in args)
     else:
