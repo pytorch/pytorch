70aafad08a9e01f4de04cfe6a486556c1c201a58
Ailing Zhang <ailzhang@fb.com>
2018-12-21 15:32:44 -0800

    AD support for adaptive_avg_pool2d (#15459)
    Summary:
    This adds AD support for adaptive_avg_pool2d, which is necessary for resnet50 in pytorch/vision:master. cc: soumith asuhan dlibenzi
    
    apaszke  I saw that autodiff bug you fixed in #15403 , as it doesn't prevent this PR from passing, so I'll leave it for your PR to fix it. :)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15459
    
    Differential Revision: D13534732
    
    Pulled By: ailzhang
    
    fbshipit-source-id: 4e48b93e35d5ecfe7bd64b6a132a55b07843f206

--- test/test_jit.py
+++ test/test_jit.py
@@ -9642,6 +9642,7 @@ EXCLUDE_SCRIPT_MODULES = {
 
 DISABLE_AUTODIFF_SUBGRAPH_INLINING = {
     'test_nn_avg_pool2d',
+    'test_nn_adaptive_avg_pool2d',
     'test_nn_log_softmax',
     'test_nn_threshold',
     'test_nn_nll_loss',
