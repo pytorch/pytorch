601e4eb3514d1e1ef13f1b96eeccae4dfbb0e101
derek <bluewhale8202@gmail.com>
2018-12-21 11:54:57 -0800

    Fixed trivial typos in Dropout2D and Dropout3D classes (#15200)
    Summary:
    Fixed trivial typos in Dropout2D and Dropout3D classes
    
    weiyangfb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15200
    
    Differential Revision: D13537888
    
    Pulled By: ezyang
    
    fbshipit-source-id: 8fb06027ca663a2e4bfa016af400698ae3c88ad1

--- torch/nn/modules/dropout.py
+++ torch/nn/modules/dropout.py
@@ -62,7 +62,7 @@ class Dropout(_DropoutNd):
 class Dropout2d(_DropoutNd):
     r"""Randomly zero out entire channels (a channel is a 2D feature map,
     e.g., the :math:`j`-th channel of the :math:`i`-th sample in the
-    batched input is a 2D tensor :math:`\text{input}[i, j]`) of the input tensor).
+    batched input is a 2D tensor :math:`\text{input}[i, j]`).
     Each channel will be zeroed out independently on every forward call.
     with probability :attr:`p` using samples from a Bernoulli distribution.
 
@@ -106,7 +106,7 @@ class Dropout2d(_DropoutNd):
 class Dropout3d(_DropoutNd):
     r"""Randomly zero out entire channels (a channel is a 3D feature map,
     e.g., the :math:`j`-th channel of the :math:`i`-th sample in the
-    batched input is a 3D tensor :math:`\text{input}[i, j]`) of the input tensor).
+    batched input is a 3D tensor :math:`\text{input}[i, j]`).
     Each channel will be zeroed out independently on every forward call.
     with probability :attr:`p` using samples from a Bernoulli distribution.
 
