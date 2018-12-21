cad713119c70050a2609e47bc2373fe5f0192dc2
davidriazati <davidriazati@fb.com>
2018-12-20 16:33:15 -0800

    Use mypy for all changed files

--- tools/run-mypy-in-ci.sh
+++ tools/run-mypy-in-ci.sh
@@ -10,32 +10,10 @@ if [[ $TRAVIS ]]; then
   BASE_BRANCH="upstream/$TRAVIS_BRANCH"
 fi
 
-if [[ ! -d build ]]; then
-  git submodule update --init --recursive
-
-  mkdir build
-  pushd build
-  # We really only need compile_commands.json, so no need to build!
-  time cmake -DBUILD_TORCH=ON ..
-  popd
-
-  # Generate ATen files.
-  time python aten/src/ATen/gen.py \
-    -s aten/src/ATen \
-    -d build/aten/src/ATen \
-    aten/src/ATen/Declarations.cwrap \
-    aten/src/THNN/generic/THNN.h \
-    aten/src/THCUNN/generic/THCUNN.h \
-    aten/src/ATen/nn.yaml \
-    aten/src/ATen/native/native_functions.yaml
-
-  # Generate PyTorch files.
-  time python tools/setup_helpers/generate_code.py            \
-    --declarations-path build/aten/src/ATen/Declarations.yaml \
-    --nn-path aten/src
-fi
-
-CHANGED_PYTHON_FILES=$(git diff --name-only master..mypy | grep -E ".py\$" | tr '\n' ' ')
+echo $BASE_BRANCH
+echo $TRAVIS_BRANCH
+git status
+CHANGED_PYTHON_FILES=$(git diff --name-only $BASE_BRANCH..$TRAVIS_BRANCH | grep -E ".py\$" | tr '\n' ' ')
 if [[ $CHANGED_PYTHON_FILES ]]; then
   time mypy $CHANGED_PYTHON_FILES
 else
