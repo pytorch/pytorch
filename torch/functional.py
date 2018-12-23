03f1289277302580fbcf19e6c3e419a093c357e6
vishwakftw <cs15btech11043@iith.ac.in>
2018-12-23 23:04:33 +0530

    Fix device issue in index_select indices

--- torch/functional.py
+++ torch/functional.py
@@ -114,7 +114,7 @@ def btriunpack(LU_data, LU_pivots, unpack_data=True, unpack_pivots=True):
             final_order = list(range(sz))
             for k, j in enumerate(LU_pivots[idx]):
                 final_order[k], final_order[j] = final_order[j], final_order[k]
-            P[idx] = P[idx].index_select(1, torch.tensor(final_order))
+            P[idx] = P[idx].index_select(1, torch.as_tensor(final_order, device=LU_pivots.device))
     else:
         P = None
 
