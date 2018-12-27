496ae5d7fa40a68da9f2db082170b26c4026a099
Dheeraj Rajaram Reddy <dheeraj98reddy@gmail.com>
2018-12-28 02:14:01 +0530

    Add whitespace around operator

--- torch/jit/annotations.py
+++ torch/jit/annotations.py
@@ -64,7 +64,7 @@ def get_num_params(fn):
         return None
     py_ast = ast.parse(source)
     if isinstance(py_ast.body[0], ast.ClassDef):
-        module_name = source[source.index('class')+6:source.index('(')]
+        module_name = source[source.index('class') + 6: source.index('(')]
         raise RuntimeError("cannot create a module (%s) inside jit.script" % module_name)
     if len(py_ast.body) != 1 or not isinstance(py_ast.body[0], ast.FunctionDef):
         raise RuntimeError("expected a single top-level function")
