pytorch/pytorch

commit 2ed95c58713b45a6a9dac4336135523555bc58a9

Fix the compare logic in function `overflows` for MSVC (#15499)
Summary:
Fixes https://github.com/pytorch/pytorch/issues/15497.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/15499

Differential Revision: D13547835

Pulled By: soumith

fbshipit-source-id: a674da93bf905a0b81f0cc60449ccb97c2746926

c10/Half.h
@@ -383,6 +383,13 @@ struct Converter<
   }
 };
 
+// In some versions of MSVC, there will be a compiler error when building.
+// C4146: unary minus operator applied to unsigned type, result still unsigned
+// It can be addressed by disabling the following warning. 
+#ifdef _MSC_VER
+#pragma warning( disable : 4146 )
+#endif
+
 // skip isnan and isinf check for integral types
 template <typename To, typename From>
 typename std::enable_if<std::is_integral<From>::value, bool>::type overflows(
@@ -398,6 +405,10 @@ typename std::enable_if<std::is_integral<From>::value, bool>::type overflows(
     return f < limit::lowest() || f > limit::max();
   }
 }
+
+#ifdef _MSC_VER
+#pragma warning( default : 4146 )
+#endif
 
 template <typename To, typename From>
 typename std::enable_if<std::is_floating_point<From>::value, bool>::type

commit 119efd52660856804c34f5414f75ed21d4428988

Make the warning suppression safer (#15560)
Summary:
Address the problem introduced in https://github.com/pytorch/pytorch/pull/15499#issuecomment-450038494.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/15560

Differential Revision: D13561346

Pulled By: soumith

fbshipit-source-id: 6abf622672bdcb77ae1a7188e8a3817fa97aecbc

c10/Half.h
@@ -387,6 +387,7 @@ struct Converter<
 // C4146: unary minus operator applied to unsigned type, result still unsigned
 // It can be addressed by disabling the following warning. 
 #ifdef _MSC_VER
+#pragma warning( push )
 #pragma warning( disable : 4146 )
 #endif
 
@@ -407,7 +408,7 @@ typename std::enable_if<std::is_integral<From>::value, bool>::type overflows(
 }
 
 #ifdef _MSC_VER
-#pragma warning( default : 4146 )
+#pragma warning( pop )
 #endif
 
 template <typename To, typename From>