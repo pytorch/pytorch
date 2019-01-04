72630cc993798ca5ae75fe9a32e646b76e40e1d4
Peter Goldsborough <psag@fb.com>
2019-01-03 17:43:58 -0800

    Move isnan to C++

--- aten/src/ATen/core/Type.h
+++ aten/src/ATen/core/Type.h
@@ -274,6 +274,7 @@ struct CAFFE2_API Type {
   virtual Tensor & index_put_(Tensor & self, TensorList indices, const Tensor & values, bool accumulate) const = 0;
   virtual Tensor inverse(const Tensor & self) const = 0;
   virtual Tensor isclose(const Tensor & self, const Tensor & other, double rtol, double atol, bool equal_nan) const = 0;
+  virtual Tensor isnan(const Tensor & self) const = 0;
   virtual bool is_distributed(const Tensor & self) const = 0;
   virtual bool is_floating_point(const Tensor & self) const = 0;
   virtual bool is_complex(const Tensor & self) const = 0;
