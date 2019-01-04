fc5c5a91d5df59d4de761cefe99c50ce1e475435
Will Feng <willfeng@fb.com>
2019-01-04 15:40:25 -0500

    use C10_DEFINE_string

--- aten/src/ATen/SparseTensorImpl.cpp
+++ aten/src/ATen/SparseTensorImpl.cpp
@@ -79,7 +79,7 @@ int64_t SparseTensorImpl::storage_offset() const {
   AT_ERROR("sparse tensors do not have storage");
 }
 void SparseTensorImpl::set_indices_and_values_unsafe(const Tensor& indices, const Tensor& values) {
-  AT_CHECK(allow_tensor_metadata_change(), "set_indices_and_values_unsafe " + err_msg_for_changing_metadata_on_derived_tensor);
+  AT_CHECK(allow_tensor_metadata_change(), "set_indices_and_values_unsafe " + FLAGS_err_msg_for_changing_metadata_on_derived_tensor);
   AT_ASSERT(!indices.is_variable() && !values.is_variable());  // They should be plain tensors!
 
   AT_CHECK(!indices.is_sparse(), "expected indices to be a dense tensor, but got indices of layout ", indices.layout());
