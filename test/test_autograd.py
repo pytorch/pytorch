f038e9475159dcedc718236a38d0e59befc6ad0b
Richard Zou <zou3519@gmail.com>
2019-01-07 08:55:39 -0800

    Fix sum_to behavior with zero dimensions
    Fixes #15223.
    
    This fixes an autograd bug where backprop either fails or produces
    gradients of incorrect sizes when tensors with zero-sized dimensions are
    involved.
    
    Previously, we were reducing along dimensions that had size greater than 1
    when summing to a size in autograd. This is incorrect because we should also reduce
    along dimensions with size 0 to produce a tensor of size 1 in that
    dimension that then gets viewed to the correct shape.
    
    Test Plan:
    - Added a test for this case.

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -202,6 +202,15 @@ class TestAutograd(TestCase):
         x_grad, x_grad_clone = compute_grad(create_graph=True)
         self.assertEqual(x_grad, x_grad_clone)
 
+    def test_sum_to_with_empty_dim_grad(self):
+        a = torch.rand(4, 0)
+        b = torch.rand(4, 1, requires_grad=True)
+        c = a + b
+        assert c.shape == (4, 0)
+        c.sum().backward()
+
+        self.assertEqual(b.grad, torch.zeros(4, 1))
+
     def test_hessian_vector(self):
         x = torch.randn(2, 2, requires_grad=True)
         y = torch.randn(2, 2, requires_grad=True)
