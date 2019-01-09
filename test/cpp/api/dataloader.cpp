5e83e2c39dcb9702405d2d669d482d27eddf2e63
Peter Goldsborough <psag@fb.com>
2019-01-09 11:42:19 -0800

    [C++ frontend] Add the normalize transform to the core library

--- test/cpp/api/dataloader.cpp
+++ test/cpp/api/dataloader.cpp
@@ -450,6 +450,56 @@ TEST(DataTest, TensorLambdaWorksforAnyTargetType) {
   ASSERT_EQ(batch[1].target, "2");
 }
 
+struct DummyTensorDataset
+    : datasets::Dataset<DummyTensorDataset, Example<torch::Tensor, int>> {
+  Example<torch::Tensor, int> get(size_t index) override {
+    const auto channels = static_cast<int64_t>(index);
+    torch::Tensor tensor =
+        (channels > 0) ? torch::ones({4, 4, channels}) : torch::ones({4, 4});
+    return {tensor, static_cast<int64_t>(channels)};
+  }
+
+  torch::optional<size_t> size() const override {
+    return 100;
+  }
+};
+
+TEST(DataTest, NormalizeTransform) {
+  auto dataset = DummyTensorDataset().map(transforms::Normalize<int>(0.5, 0.1));
+
+  // Works for zero (one implicit) channels
+  std::vector<Example<torch::Tensor, int>> output = dataset.get_batch(0);
+  ASSERT_EQ(output.size(), 1);
+  // (1 - 0.5) / 0.1 = 5
+  ASSERT_TRUE(output[0].data.allclose(torch::ones({4, 4}) * 5));
+
+  // Works for one explicit channel
+  output = dataset.get_batch(1);
+  ASSERT_EQ(output.size(), 1);
+  ASSERT_EQ(output[0].data.size(2), 1);
+  ASSERT_TRUE(output[0].data.allclose(torch::ones({4, 4, 1}) * 5));
+
+  // Works for two channels with different moments
+  dataset = DummyTensorDataset().map(
+      transforms::Normalize<int>({0.5, 1.5}, {0.1, 0.2}));
+  output = dataset.get_batch(2);
+  ASSERT_EQ(output.size(), 1);
+  ASSERT_EQ(output[0].data.size(2), 2);
+  ASSERT_TRUE(output[0]
+                  .data.slice(/*dim=*/2, /*start=*/0, /*end=*/1)
+                  .allclose(torch::ones({4, 4, 1}) * 5));
+  ASSERT_TRUE(output[0]
+                  .data.slice(/*dim=*/2, /*start=*/1)
+                  .allclose(torch::ones({4, 4, 1}) * -2.5));
+
+  // Works for three channels with one moment value
+  dataset = DummyTensorDataset().map(transforms::Normalize<int>(1.5, 0.2));
+  output = dataset.get_batch(3);
+  ASSERT_EQ(output.size(), 1);
+  ASSERT_EQ(output[0].data.size(2), 3);
+  ASSERT_TRUE(output[0].data.allclose(torch::ones({4, 4, 3}) * -2.5));
+}
+
 struct UnCopyableDataset : public datasets::Dataset<UnCopyableDataset> {
   UnCopyableDataset() = default;
 
--- torch/csrc/api/include/torch/data/transforms/tensor.h
+++ torch/csrc/api/include/torch/data/transforms/tensor.h
@@ -50,6 +50,23 @@ class TensorLambda : public TensorTransform<Target> {
  private:
   FunctionType function_;
 };
+
+/// Normalizes input tensors by subtracting the supplied mean and dividing by
+/// the given standard deviation.
+template <typename Target = Tensor>
+struct Normalize : public TensorTransform<Target> {
+
+  /// Constructs a `Normalize` transform. The mean and standard deviation can be
+  //anything that is broadcastable over the input tensors (like single scalars).
+  Normalize(ArrayRef<float> mean, ArrayRef<float> stddev)
+      : mean(torch::tensor(mean)), stddev(torch::tensor(stddev)) {}
+
+  torch::Tensor operator()(Tensor input) {
+    return input.sub(mean).div(stddev);
+  }
+
+  torch::Tensor mean, stddev;
+};
 } // namespace transforms
 } // namespace data
 } // namespace torch
