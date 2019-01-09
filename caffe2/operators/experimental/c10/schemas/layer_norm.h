d562840910b8743b6ea476a47c4df53a8531fd14
Sebastian Messmer <messmer@fb.com>
2019-01-08 20:22:41 -0800

    Use C10Tensor in the dispatcher (#15195)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15195
    
    This removes the use of caffe2::Tensor or at::Tensor in the c10 dispatcher and only uses C10::Tensor.
    It also changes output tensors to be passed as `const Tensor&` instead of `Tensor*` because we otherwise can't forward them in operator_c10wrapper.h.
    
    Reviewed By: ezyang
    
    Differential Revision: D13461640
    
    fbshipit-source-id: 7f79925a7d60f01660a24bbfda47391af0c70ed3

--- caffe2/operators/experimental/c10/schemas/layer_norm.h
+++ caffe2/operators/experimental/c10/schemas/layer_norm.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "caffe2/core/tensor.h"
+#include <c10/core/Tensor.h>
 #include <c10/util/Array.h>
+#include "caffe2/core/context_base.h"
+#include "caffe2/core/tensor.h"
 
 namespace caffe2 {
 namespace ops {
@@ -10,19 +12,23 @@ struct LayerNorm final {
   static constexpr const char* name = "LayerNorm";
 
   struct Cache final {
-    Tensor scale = empty({}, CPU);
-    Tensor bias = empty({}, CPU);
+      at::optional<C10Tensor> scale = at::nullopt;
+      at::optional<C10Tensor> bias = at::nullopt;
   };
 
   using Signature = void(
-      const Tensor& input,
-      Tensor* output,
-      Tensor* output_mean,
-      Tensor* output_stddev,
+      const C10Tensor& input,
+      const C10Tensor& output,
+      const C10Tensor& output_mean,
+      const C10Tensor& output_stddev,
       int axis,
       float epsilon,
       Cache* cache,
       BaseContext* context);
+
+  static constexpr size_t num_dispatch_args() {return 1;}
+
+  static constexpr size_t num_outputs() {return 3;}
 
   static constexpr c10::guts::array<const char*, 8> parameter_names = {
       {"input", "output", "output_mean", "output_stddev", "axis", "epsilon", "cache", "context"}};
