956d4d89c06edbd6c844875d29bcb76851ccdb5d
Shen Li <shenli@fb.com>
2019-01-09 15:07:07 -0800

    Wrap C10 CUDAStream instead of cudaStream_t in THCPStream
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/15833
    
    Differential Revision: D13608337
    
    Pulled By: mrshenli
    
    fbshipit-source-id: 4c66ef89fad0dc14a11ddb69da92907797cd2828

--- torch/csrc/cuda/Stream.cpp
+++ torch/csrc/cuda/Stream.cpp
@@ -4,7 +4,6 @@
 #include <torch/csrc/cuda/Module.h>
 
 #include <c10/cuda/CUDAGuard.h>
-#include <c10/cuda/CUDAStream.h>
 
 #include <structmember.h>
 #include <cuda_runtime_api.h>
@@ -38,27 +37,56 @@ static PyObject * THCPStream_pynew(PyTypeObject *type, PyObject *args, PyObject 
 
   THCPStream* self = (THCPStream *)ptr.get();
   self->cdata = stream.pack();
-  self->device = stream.device_index();
-  self->cuda_stream = stream.stream();
+  new (&self->cuda_stream) at::cuda::CUDAStream(stream);
+
   return (PyObject *)ptr.release();
+  END_HANDLE_TH_ERRORS
+}
+
+static void THCPStream_dealloc(THCPStream *self) {
+  self->cuda_stream.~CUDAStream();
+  Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+static PyObject * THPVariable_get_device(THCPStream *self) {
+  HANDLE_TH_ERRORS
+  return THPUtils_packInt64(self->cuda_stream.device_index());
+  END_HANDLE_TH_ERRORS
+}
+
+static PyObject * THPVariable_get_cuda_stream(THCPStream *self) {
+  HANDLE_TH_ERRORS
+  return PyLong_FromVoidPtr(self->cuda_stream.stream());
   END_HANDLE_TH_ERRORS
 }
 
 static PyObject * THCPStream_query(THCPStream *self) {
   HANDLE_TH_ERRORS
-  return PyBool_FromLong(at::cuda::CUDAStream::unpack(self->cdata).query());
+  return PyBool_FromLong(self->cuda_stream.query());
+  END_HANDLE_TH_ERRORS
+}
+
+static PyObject * THCPStream_eq(THCPStream *self, THCPStream *other) {
+  HANDLE_TH_ERRORS
+  return PyBool_FromLong(self->cuda_stream == other->cuda_stream);
   END_HANDLE_TH_ERRORS
 }
 
 static struct PyMemberDef THCPStream_members[] = {
   {(char*)"_cdata", T_ULONGLONG, offsetof(THCPStream, cdata), READONLY, nullptr},
-  {(char*)"device", T_INT, offsetof(THCPStream, device), READONLY, nullptr},
-  {(char*)"cuda_stream", T_ULONGLONG, offsetof(THCPStream, cuda_stream), READONLY, nullptr},
+  {nullptr}
+};
+
+static struct PyGetSetDef THPVariable_properties[] = {
+  {"device", (getter)THPVariable_get_device, nullptr, nullptr, nullptr},
+  {"cuda_stream",
+    (getter)THPVariable_get_cuda_stream, nullptr, nullptr, nullptr},
   {nullptr}
 };
 
 static PyMethodDef THCPStream_methods[] = {
   {(char*)"query", (PyCFunction)THCPStream_query, METH_NOARGS, nullptr},
+  {(char*)"__eq__", (PyCFunction)THCPStream_eq, METH_O, nullptr},
   {nullptr}
 };
 
@@ -67,7 +95,7 @@ PyTypeObject THCPStreamType = {
   "torch._C._CudaStreamBase",             /* tp_name */
   sizeof(THCPStream),                    /* tp_basicsize */
   0,                                     /* tp_itemsize */
-  0,                                     /* tp_dealloc */
+  (destructor)THCPStream_dealloc,        /* tp_dealloc */
   0,                                     /* tp_print */
   0,                                     /* tp_getattr */
   0,                                     /* tp_setattr */
@@ -92,7 +120,7 @@ PyTypeObject THCPStreamType = {
   0,                                     /* tp_iternext */
   THCPStream_methods,                    /* tp_methods */
   THCPStream_members,                    /* tp_members */
-  0,                                     /* tp_getset */
+  THPVariable_properties,                /* tp_getset */
   0,                                     /* tp_base */
   0,                                     /* tp_dict */
   0,                                     /* tp_descr_get */
