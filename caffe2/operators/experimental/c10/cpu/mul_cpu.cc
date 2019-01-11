6ec850789f9c9c45a42f0fd538ab9b883c901ca3
Sebastian Messmer <messmer@fb.com>
2019-01-10 16:06:29 -0800

    caffe2::Tensor::is_same() (#15407)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15407
    
    Don't ask the tensor for its intrusive pointer if we just want to check if two tensors are the same.
    This mirrors ATen APIs.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D13520389
    
    fbshipit-source-id: 681317f36f480ab60e532bb08a073f98f39770fd

--- caffe2/operators/experimental/c10/cpu/mul_cpu.cc
+++ caffe2/operators/experimental/c10/cpu/mul_cpu.cc
@@ -27,9 +27,8 @@ void mul_op_cpu_impl(
   std::vector<int> B_dims;
 
   if (legacy_broadcast) {
-    CAFFE_ENFORCE_NE(
-        C.getIntrusivePtr(),
-        B.getIntrusivePtr(),
+    CAFFE_ENFORCE(
+        !B.is_same(C),
         "In-place is allowed only with the first tensor when "
         "legacy-broadcasting");
     C.ResizeLike(A);
@@ -51,9 +50,9 @@ void mul_op_cpu_impl(
     const std::vector<int> C_dims =
         caffe2::elementwise_ops_utils::ComputeBinaryBroadcastForwardDims(
             A_dims, B_dims);
-    if (C.getIntrusivePtr() == A.getIntrusivePtr()) {
+    if (A.is_same(C)) {
       CAFFE_ENFORCE_EQ(C_dims, A_dims);
-    } else if (C.getIntrusivePtr() == B.getIntrusivePtr()) {
+    } else if (B.is_same(C)) {
       CAFFE_ENFORCE_EQ(C_dims, B_dims);
     } else {
       C.Resize(C_dims);
