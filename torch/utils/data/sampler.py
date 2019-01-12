bc61aa345c7ea7a8d63142c7a8710d194a0e1851
kyryl <truskovskiyk@gmail.com>
2019-01-12 16:51:56 +0200

    fix RandomSampler length

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -73,7 +73,7 @@ class RandomSampler(Sampler):
         return iter(torch.randperm(n).tolist())
 
     def __len__(self):
-        return len(self.data_source)
+        return self.num_samples
 
 
 class SubsetRandomSampler(Sampler):
