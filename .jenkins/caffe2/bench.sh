e04f3c50f8d1419f066f28035a7b0029fbddcf97
Junjie Bai <bai@in.tum.de>
2019-01-14 14:54:55 -0800

    Add num layers flag

--- .jenkins/caffe2/bench.sh
+++ .jenkins/caffe2/bench.sh
@@ -29,14 +29,14 @@ fi
 
 # ResNext
 if (( $num_gpus == 0 )); then
-    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --train_data null --batch_size 32 --epoch_size 3200 --num_epochs 2 --use_cpu
+    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --num_layers 101 --train_data null --batch_size 32 --epoch_size 3200 --num_epochs 2 --use_cpu
 fi
 if (( $num_gpus >= 1 )); then
-    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --train_data null --batch_size 32 --epoch_size 3200 --num_epochs 2 --num_gpus 1
+    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --num_layers 101 --train_data null --batch_size 32 --epoch_size 3200 --num_epochs 2 --num_gpus 1
 fi
 if (( $num_gpus >= 2 )); then
-    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --train_data null --batch_size 64 --epoch_size 6400 --num_epochs 2 --num_gpus 2
+    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --num_layers 101 --train_data null --batch_size 64 --epoch_size 6400 --num_epochs 2 --num_gpus 2
 fi
 if (( $num_gpus >= 4 )); then
-    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --train_data null --batch_size 128 --epoch_size 12800 --num_epochs 2 --num_gpus 4
+    "$PYTHON" "$CAFFE2_PYPATH/python/examples/resnet50_trainer.py" --resnext_num_groups 32 --resnext_width_per_group 4 --num_layers 101 --train_data null --batch_size 128 --epoch_size 12800 --num_epochs 2 --num_gpus 4
 fi
