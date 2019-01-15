ec5ed98e1ef9e3c369c140f31bf60e8b42cf6a2d
pjh5 <hellemn@fb.com>
2019-01-15 10:51:42 -0800

    Fixing missing cpp tests for Caffe2 setup.py builds

--- .jenkins/caffe2/bench.sh
+++ .jenkins/caffe2/bench.sh
@@ -2,7 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-# Anywhere except $ROOT_DIR should work
+# Anywhere except $ROOT_DIR should work. This is so the python import doesn't
+# get confused by any 'caffe2' directory in cwd
 cd "$INSTALL_PREFIX"
 
 if [[ $BUILD_ENVIRONMENT == *-cuda* ]]; then
@@ -13,7 +14,7 @@ else
     num_gpus=0
 fi
 
-caffe2_pypath="$(python -c 'import os; import caffe2; print(os.path.dirname(os.path.realpath(caffe2.__file__)))')"
+caffe2_pypath="$(cd /usr && python -c 'import os; import caffe2; print(os.path.dirname(os.path.realpath(caffe2.__file__)))')"
 cmd="$PYTHON $caffe2_pypath/python/examples/resnet50_trainer.py --train_data null --batch_size 64 --epoch_size 6400 --num_epochs 2"
 if (( $num_gpus == 0 )); then
     cmd="$cmd --use_cpu"
