f73984b5ebbebf69891da4ca19ced5cabdc3c30c
pjh5 <hellemn@fb.com>
2019-01-14 17:40:58 -0800

    Fix Python path finding for benchmark tests

--- .jenkins/caffe2/bench.sh
+++ .jenkins/caffe2/bench.sh
@@ -13,7 +13,8 @@ else
     num_gpus=0
 fi
 
-cmd="$PYTHON $CAFFE2_PYPATH/python/examples/resnet50_trainer.py --train_data null --batch_size 64 --epoch_size 6400 --num_epochs 2"
+caffe2_pypath="$(dirname $(python -c 'import caffe2; print(caffe2.__file__)'))"
+cmd="$PYTHON $caffe2_pypath/python/examples/resnet50_trainer.py --train_data null --batch_size 64 --epoch_size 6400 --num_epochs 2"
 if (( $num_gpus == 0 )); then
     cmd="$cmd --use_cpu"
 else
