a48489108d9e9e6d31557fabd10a87ffad446323
Gregory Chanan <gchanan@fb.com>
2019-01-16 15:02:41 -0800

    Skip all pdist NN tests under rocm.

--- test/test_nn.py
+++ test/test_nn.py
@@ -5330,6 +5330,7 @@ class TestNN(NNTestCase):
             inp = torch.randn(1, 3, dtype=torch.double, device=device, requires_grad=True)
             self.assertTrue(gradcheck(F.pdist, (inp,)))
 
+    @skipIfRocm
     def test_pdist_empty_col(self):
         for device in device_():
             inp = torch.randn(4, 0, dtype=torch.double, device=device, requires_grad=True)
@@ -5340,6 +5341,7 @@ class TestNN(NNTestCase):
         inp = torch.randn(4, 5, requires_grad=True)
         gradgradcheck(F.pdist, (inp,))
 
+    @skipIfRocm
     @unittest.expectedFailure
     def test_pdist_cuda_gradgrad_unimplemented(self):
         inp = torch.randn(4, 5, device='cuda', requires_grad=True)
