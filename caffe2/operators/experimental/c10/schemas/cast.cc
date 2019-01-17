4a9a8889d1859582457b8ed0b5aa04f1119c2fc9
Sebastian Messmer <messmer@fb.com>
2019-01-17 15:47:16 -0800

    Move c10 dispatcher back to ATen/core (#16050)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16050
    
    The c10 dispatcher will (soon) depend on IValue and IValue can't be moved to c10 yet because it depends on at::Tensor, which depends on legacy Type dispatch and we don't want the legacy dispatch in c10.
    
    So instead, we move the c10 dispatcher back to ATen/core until we can actually move at::Tensor to c10.
    
    Reviewed By: ezyang
    
    Differential Revision: D13684517
    
    fbshipit-source-id: 1125f4254223907c52f96ff73034f6d4ae9fd0a7

--- caffe2/operators/experimental/c10/schemas/cast.cc
+++ caffe2/operators/experimental/c10/schemas/cast.cc
@@ -1,5 +1,5 @@
 #include "caffe2/operators/experimental/c10/schemas/cast.h"
-#include <c10/core/dispatch/OpSchemaRegistration.h>
+#include <ATen/core/dispatch/OpSchemaRegistration.h>
 #include "caffe2/core/operator_c10wrapper.h"
 #include "caffe2/utils/cast.h"
 
