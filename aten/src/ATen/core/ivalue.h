40b3e4907c2b5b21da43de7b263952225a91644a
Sebastian Messmer <messmer@fb.com>
2019-01-17 15:53:52 -0800

    Fix export macros (#15856)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15856
    
    They seem to be wrong.
    cc zdevito to take a look but I think this is now more correct.
    
    It's weird this didn't cause linker errors. Probably, this functionality isn't used across library boundaries yet.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D13605257
    
    fbshipit-source-id: 7077ca9027c3ac79a4847ec15ead7ddb28696445

--- aten/src/ATen/core/ivalue.h
+++ aten/src/ATen/core/ivalue.h
@@ -39,7 +39,7 @@ struct CAFFE2_API ConstantString final : c10::intrusive_ptr_target {
 };
 
 template <typename Elem>
-struct C10_EXPORT List : c10::intrusive_ptr_target {
+struct CAFFE2_API List : c10::intrusive_ptr_target {
  private:
   std::vector<Elem> elements_;
 
@@ -67,7 +67,7 @@ struct C10_EXPORT List : c10::intrusive_ptr_target {
 
 struct Future;
 
-struct C10_EXPORT Tuple : public List<IValue> {
+struct CAFFE2_API Tuple : public List<IValue> {
   using List<IValue>::List;
   static c10::intrusive_ptr<Tuple> create(std::vector<IValue> elements_) {
     return c10::make_intrusive<Tuple>(std::move(elements_));
