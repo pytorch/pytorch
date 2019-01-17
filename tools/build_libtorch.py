1caa027951f100a5a2b342c42e930075a0d88ad6
Derek Kim <bluewhale8202@gmail.com>
2019-01-16 23:52:37 -0800

    Fix possible importing errors in build_libtorch.py (#15471)
    Summary:
    1. I fixed the importing process, which had some problems
        -  **I think `setup_helpers` should not be imported as the top level module. It can lead to many future errors. For example, what if `setup_helpers` imports another module from the upper level?** So we need to change it.
        - The code is not consistent with other modules in `tools` package. For example, other
        modules in the package imports `from tools.setuptools...` not `from setuptools...`.
        - **It should be able to run with `python -m tools.build_libtorch` command**  because this module is a part of the tools package. Currently, you cannot do that and I think it's simply wrong.
    
    ~~2. I Added platform specific warning messages.
        - I constantly forgot that I needed to define some environment variables in advance specific to my platform to build libtorch, especially when I'm working at a non pytorch root directory. So I thought adding warnings for common options would be helpful .~~
    
    ~~3. Made the build output path configurable. And a few other changes.~~
    
    orionr  ebetica
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/15471
    
    Differential Revision: D13709607
    
    Pulled By: ezyang
    
    fbshipit-source-id: 950d5727aa09f857d973538c50b1ab169d88da38

--- tools/build_libtorch.py
+++ tools/build_libtorch.py
@@ -1,13 +1,20 @@
 import argparse
 import os
+from os.path import dirname, abspath
 import shlex
 import subprocess
 import sys
 
+# By appending pytorch_root to sys.path, this module can import other torch
+# modules even when run as a standalone script. i.e., it's okay either you
+# do `python build_libtorch.py` or `python -m tools.build_libtorch`.
+pytorch_root = dirname(dirname(abspath(__file__)))
+sys.path.append(pytorch_root)
+
 # If you want to modify flags or environmental variables that is set when
 # building torch, you should do it in tools/setup_helpers/configure.py.
 # Please don't add it here unless it's only used in LibTorch.
-from setup_helpers.configure import get_libtorch_env_with_flags
+from tools.setup_helpers.configure import get_libtorch_env_with_flags, IS_WINDOWS
 
 if __name__ == '__main__':
     # Placeholder for future interface. For now just gives a nice -h.
@@ -15,7 +22,7 @@ if __name__ == '__main__':
     options = parser.parse_args()
 
     tools_path = os.path.dirname(os.path.abspath(__file__))
-    if sys.platform == 'win32':
+    if IS_WINDOWS:
         build_pytorch_libs = os.path.join(tools_path, 'build_pytorch_libs.bat')
     else:
         build_pytorch_libs = os.path.join(tools_path, 'build_pytorch_libs.sh')
