549db7d4ff431de3063ead635f7efa1054712ca7
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-01-21 12:28:56 -0800

    Unhide unique from C++, make unique partially scriptable (#15256)
    Summary:
    This PR does three things:
    
    ~~Allow `int64_t?` in function schema,  which provide an elegant way of implementing null-able int arguments, as discussed in https://github.com/pytorch/pytorch/pull/15208#pullrequestreview-185230081~~
    
    ~~Originally implemented in https://github.com/pytorch/pytorch/pull/15235~~
    
    ~~Example:~~
    
    ```yaml
    - func: myop(Tensor self, int64_t? dim=None) -> Tensor
      variants: function
    ```
    
    ~~cc: zou3519~~
    
    Edit: implemented in https://github.com/pytorch/pytorch/pull/15234
    
    Previously tried in https://github.com/pytorch/pytorch/pull/12064. There was a problem that C++ does not have kwarg support, which makes it confusing to know whether `unique(t, 1)` actually means `unique(t, dim=1)` or `unique(t, sorted=1)`.
    
    Now I think I have a better idea on how to implement this: there are two ATen operators: `unique` and `unique_dim`. `unique` has the same signature as in python, and exported to both python and C++. `unique_dim` has signature `unique_dim(tensor, dim, sorted=False, return_inverse=False)`, and only exported to C++, which could be used more naturally for a C++ user.
    
    Differential Revision: D13540278
    
    Pulled By: wanchaol
    
    fbshipit-source-id: 3768c76a90b0881f565a1f890459ebccbdfe6ecd

--- torch/tensor.py
+++ torch/tensor.py
@@ -343,23 +343,7 @@ class Tensor(torch._C._TensorBase):
 
         See :func:`torch.unique`
         """
-        if dim is not None:
-            output, inverse_indices = torch._unique_dim(
-                self,
-                sorted=sorted,
-                return_inverse=return_inverse,
-                dim=dim
-            )
-        else:
-            output, inverse_indices = torch._unique(
-                self,
-                sorted=sorted,
-                return_inverse=return_inverse
-            )
-        if return_inverse:
-            return output, inverse_indices
-        else:
-            return output
+        return torch.unique(self, sorted, return_inverse, dim)
 
     def __rsub__(self, other):
         return _C._VariableFunctions.rsub(self, other)
