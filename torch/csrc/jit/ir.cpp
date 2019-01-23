fc474fd1b9e525934dec57cbd242c9c1f455d3fd
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-01-23 02:28:00 -0500

    schema matching

--- torch/csrc/jit/ir.cpp
+++ torch/csrc/jit/ir.cpp
@@ -1161,9 +1161,9 @@ Node* Graph::createFusionGroup() {
   return n;
 }
 
-Node* Graph::createTuple(at::ArrayRef<Value*> values) {
+Node* Graph::createTuple(at::ArrayRef<Value*> values, c10::OptNameList field_names) {
   auto types = fmap(values, [](Value* v) { return v->type(); });
-  auto tt = TupleType::create(std::move(types));
+  auto tt = TupleType::create(std::move(types), field_names);
   auto n = create(prim::TupleConstruct, values);
   n->output()->setType(tt);
   return n;
