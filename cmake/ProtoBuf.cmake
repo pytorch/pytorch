7a0a7199d14fb4abf97ee3c73c2aaf6f92661274
Junjie Bai <bai@in.tum.de>
2019-01-22 21:13:02 -0800

    Update tbird_party protobuf to v3.6.1

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -63,7 +63,9 @@ macro(custom_protobuf_find)
   if (NOT TARGET protobuf::libprotobuf)
     add_library(protobuf::libprotobuf ALIAS libprotobuf)
     add_library(protobuf::libprotobuf-lite ALIAS libprotobuf-lite)
-    add_executable(protobuf::protoc ALIAS protoc)
+    if (NOT (ANDROID OR IOS))
+      add_executable(protobuf::protoc ALIAS protoc)
+    endif()
   endif()
 endmacro()
 
@@ -78,17 +80,10 @@ if (ANDROID OR IOS)
         "change in the future, and you will need to specify "
         "-DBUILD_CUSTOM_PROTOBUF=ON explicitly.")
   endif()
+  set(__caffe2_protobuf_BUILD_PROTOC_BINARIES ${protobuf_BUILD_PROTOC_BINARIES})
+  set(protobuf_BUILD_PROTOC_BINARIES OFF)
   custom_protobuf_find()
-  # Unfortunately, new protobuf does not support libprotoc and protoc
-  # cross-compilation so we will need to exclude it.
-  # The problem of using EXCLUDE_FROM_ALL is that one is not going to be able
-  # to run cmake install. A proper solution has to be implemented by protobuf
-  # since we derive our cmake files from there.
-  # TODO(jiayq): change this once https://github.com/google/protobuf/pull/3878
-  # merges.
-  set_target_properties(
-      libprotoc protoc PROPERTIES
-      EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+  set(protobuf_BUILD_PROTOC_BINARIES ${__caffe2_protobuf_BUILD_PROTOC_BINARIES})
 elseif (BUILD_CUSTOM_PROTOBUF)
   message(STATUS "Building using own protobuf under third_party per request.")
   custom_protobuf_find()
