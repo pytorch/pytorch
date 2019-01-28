6ac549b32192738b84e73759550a0a65179fd83d
James Reed <jamesreed@fb.com>
2019-01-28 15:22:08 -0800

    Move tracer impls into cpp file (#16410)
    Summary:
    Working on the tracer was really annoying because a lot of the implementations were in `tracer.h` and editing that file caused us to rebuild almost the whole world. So this moves all the implementations into tracer.cpp
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16410
    
    Differential Revision: D13847776
    
    Pulled By: jamesr66a
    
    fbshipit-source-id: ec8500da32b2d4cd990f293a0a96101d3e82f158

--- torch/csrc/jit/tracer.cpp
+++ torch/csrc/jit/tracer.cpp
@@ -39,6 +39,14 @@ thread_local std::shared_ptr<TracingState> tracing_state;
 
 } // namespace detail
 
+TORCH_API std::function<void()> pauseTracing() {
+  // NOLINTNEXTLINE
+  std::shared_ptr<tracer::TracingState> state = getTracingState();
+  tracer::setTracingState(nullptr);
+
+  return [state]() { tracer::setTracingState(state); };
+}
+
  void delValueTrace(const Variable& var) {
   AT_ASSERT(var.defined());
   auto& env_stack = getTracingState()->env_stack;
@@ -119,6 +127,134 @@ Value* getValueTrace(const IValue& var) {
     oss << "Unknown type used in value trace lookup!";
     throw std::runtime_error(oss.str());
   }
+}
+
+// allow tracing of tuples passed to List[Tensor] or Tuple[Tensor...] arguments
+// One might merge getValueTrace and getNestedValueTrace after checking that
+// casting to IValue instead  of Variable is OK
+Value* getNestedValueTrace(const IValue& v) {
+  auto& state = getTracingState();
+  if (v.isTensorList()) {
+    return state->graph
+        ->insertNode(state->graph->createList(
+            DynamicType::get(),
+            fmap(
+                v.toTensorListRef(),
+                [](const IValue& val) { return getNestedValueTrace(val); })))
+        ->output();
+  } else if (v.isTuple()) {
+    return state->graph
+        ->insertNode(state->graph->createTuple(fmap(
+            v.toTuple()->elements(),
+            [](const IValue& val) { return getNestedValueTrace(val); })))
+        ->output();
+  }
+  return getValueTrace(v.toTensor());
+}
+
+Value* getOutputTrace(
+    const std::shared_ptr<TracingState>& state,
+    const Variable& var) {
+  if (!var.defined()) {
+    Node* n = state->graph->createUndefined();
+    return state->graph->insertNode(n)->output();
+  }
+
+  auto& value_map = getTracingState()->env_stack.back().value_map;
+  auto it = value_map.find(var);
+  if (it == value_map.end()) {
+    std::ostringstream os;
+    os << "output of traced region did not have observable "
+       << "data dependence with trace inputs; this probably indicates your program "
+       << "cannot be understood by the tracer.";
+    throw std::runtime_error(os.str());
+  }
+  return it->second;
+}
+
+Value* getNestedOutputTrace(
+    const std::shared_ptr<TracingState>& state,
+    const IValue& iv) {
+  if (iv.isTensor()) {
+    return getOutputTrace(state, iv.toTensor());
+  } else if (iv.isTuple()) {
+    const auto& elems = iv.toTuple()->elements();
+    auto tuple_node = state->graph->createTuple(
+        fmap(elems, [&state](const IValue& iv) {
+          return getNestedOutputTrace(state, iv);
+        }));
+    state->graph->insertNode(tuple_node);
+    return tuple_node->output();
+  } else {
+    AT_ERROR(
+        "Only tensors or tuples of tensors can be output from traced functions");
+  }
+}
+
+// Start tracing, treating 'inputs' as inputs to the trace, which can be
+// varied on subsequent invocations of the trace.  Any other variables
+// will be treated as constants.
+std::pair<std::shared_ptr<TracingState>, Stack> enter(Stack inputs) {
+  if (isTracing()) {
+    AT_ERROR("Tracing can't be nested");
+  }
+  auto state = std::make_shared<TracingState>();
+  setTracingState(state);
+  // XXX: this function mutates input
+  const std::function<IValue(IValue, TypePtr, Value*)> add_input =
+      [&](IValue input, TypePtr type, Value* value) -> IValue {
+    value->setType(type);
+    if (type->isSubtypeOf(DynamicType::get())) {
+      auto input_tensor = input.toTensor();
+      auto name = Variable(input_tensor).name();
+      auto& value_map = state->env_stack.back().value_map;
+      if (value_map.find(input_tensor) != value_map.end()) {
+        input_tensor = input_tensor.view(input_tensor.sizes());
+      }
+      value->setUniqueName(name);
+      value_map[input_tensor] = value;
+      return input_tensor;
+    } else if (auto tuple_type = type->cast<TupleType>()) {
+      auto unpack_node =
+          state->graph->insertNode(state->graph->createTupleUnpack(value));
+      auto elem_values = unpack_node->outputs();
+      auto elem_types = tuple_type->elements();
+      Stack elems = input.toTuple()->elements();
+      size_t num_elems = elems.size();
+      AT_ASSERT(
+          elem_values.size() == num_elems && elem_types.size() == num_elems);
+      for (size_t i = 0; i < num_elems; ++i) {
+        elems[i] = add_input(elems[i], elem_types[i], elem_values[i]);
+      }
+      return Tuple::create(std::move(elems));
+    } else {
+      AT_ERROR(
+          "Only tensors or tuples of tensors can be inputs to traced functions");
+    }
+  };
+  for (IValue& input : inputs) {
+    input = add_input(
+        input, incompleteInferTypeFrom(input), state->graph->addInput());
+  }
+  return std::make_pair(state, inputs);
+}
+
+// Exit a trace, treating 'outputs' as the outputs of the trace.  These
+// are the variables whose values will be computed upon subsequent
+// invocations of the trace.
+void exit(const Stack& outputs) {
+  auto& state = getTracingState();
+  size_t i = 0;
+  for (auto& output : outputs) {
+    state->graph->registerOutput(getNestedOutputTrace(state, output));
+    i++;
+  }
+  setTracingState(nullptr);
+}
+
+// Abort tracing. Used to reset the state in case of errors.
+void abandon() {
+  setTracingState(nullptr);
 }
 
 void setValueTrace(const IValue& v, Value* value) {
@@ -336,6 +472,29 @@ autograd::Variable getSizeOf(const autograd::Variable& var, int64_t dim) {
       graph->insertNode(graph->createNumToTensor(node->output()))->output();
   setValueTrace(size_var, ten);
   return size_var;
+}
+
+void ensureUniqueIfOutOfPlaced(
+    const char* name,
+    const at::Tensor& tensor) {
+  auto& state = getTracingState();
+  if (state && state->force_outplace == false) {
+    // If we're not converting in-place ops to out-of-place, this check is
+    // unnecessary
+    return;
+  }
+  auto aliases = tensor.storage().use_count();
+  if (isTracing() && aliases > 1) {
+    std::stringstream ss;
+    ss << "There are " << aliases
+       << " live references to the data region being modified when tracing in-place operator "
+       << name
+       << ". This might cause the trace to be incorrect, because all other views "
+       << "that also reference this data will not not reflect this change in the trace! "
+       << "On the other hand, if all other views use the same memory chunk, but are disjoint (e.g. "
+       << "are outputs of torch.split), this might still be safe.";
+    warn(ss.str().c_str());
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
