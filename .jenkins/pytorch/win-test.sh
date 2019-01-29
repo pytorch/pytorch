81a52cbc9c05d8b6976a27c1fd45b7e5c9582492
peter <peterghost86@gmail.com>
2019-01-29 19:35:29 +0800

    Add some smoke tests for Windows

--- .jenkins/pytorch/win-test.sh
+++ .jenkins/pytorch/win-test.sh
@@ -82,6 +82,28 @@ EOL
 
 cat >ci_scripts/test_python_nn.bat <<EOL
 call ci_scripts/setup_pytorch_env.bat
+:: Some smoke tests
+cd test/
+:: Check caffe2.python importing works
+python -c "from caffe2.python import core"
+if ERRORLEVEL 1 exit /b 1
+:: Checking that MKL is available
+python -c "import torch; exit(0 if torch.backends.mkl.is_available() else 1)"
+if ERRORLEVEL 1 exit /b 1
+python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
+if ERRORLEVEL 1 exit /b 1
+:: Checking that CUDA archs are setup correctly
+python -c "import torch; torch.randn([3,5]).cuda()"
+if ERRORLEVEL 1 exit /b 1
+:: Checking that magma is available
+python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
+if ERRORLEVEL 1 exit /b 1
+:: Checking that CuDNN is available
+python -c "import torch; exit(0 if torch.backends.cudnn.is_available() else 1)"
+if ERRORLEVEL 1 exit /b 1
+cd ..
+
+:: Run nn tests
 cd test/ && python run_test.py --include nn --verbose && cd ..
 EOL
 
