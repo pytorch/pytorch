d6605e9a7226f99c59ec64840ab50dc9ff35e3f4
peter <peterghost86@gmail.com>
2019-01-29 19:38:38 +0800

    Minor changes

--- .jenkins/pytorch/win-test.sh
+++ .jenkins/pytorch/win-test.sh
@@ -84,13 +84,11 @@ cat >ci_scripts/test_python_nn.bat <<EOL
 call ci_scripts/setup_pytorch_env.bat
 :: Some smoke tests
 cd test/
-:: Check caffe2.python importing works
+:: Checking that caffe2.python is available
 python -c "from caffe2.python import core"
 if ERRORLEVEL 1 exit /b 1
 :: Checking that MKL is available
 python -c "import torch; exit(0 if torch.backends.mkl.is_available() else 1)"
-if ERRORLEVEL 1 exit /b 1
-python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
 if ERRORLEVEL 1 exit /b 1
 :: Checking that CUDA archs are setup correctly
 python -c "import torch; torch.randn([3,5]).cuda()"
