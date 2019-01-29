1dbaf07d4abcd97417dc6414062bb497759cf169
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2019-01-29 11:41:07 -0800

    Back out "Revert D13596031: Improve c2-aten tensor interop and add proper testing"
    Summary:
    Original commit changeset: dc371697f14b
    Relanding https://github.com/pytorch/pytorch/pull/15860 - the problem was that layer_norm was using at::empty which is not yet on mobile
    
    Differential Revision: D13861480
    
    fbshipit-source-id: 92d2fa72a693461c94ab6d8f4ba7ff312fbdeac0

--- caffe2/operators/layer_norm_op.cc
+++ caffe2/operators/layer_norm_op.cc
@@ -202,10 +202,10 @@ c10::IValue layer_norm_c10(c10::ArrayRef<c10::IValue> inputs, c10::KernelState* 
   caffe2::CPUContext context;
   State* cache = static_cast<State*>(state);
   if (!cache->scale.has_value()) {
-    cache->scale = at::Tensor(c10::C10Tensor(caffe2::Tensor{caffe2::CPU}));
+    cache->scale = at::Tensor(caffe2::empty({0}, at::dtype<float>()));
   }
   if (!cache->bias.has_value()) {
-    cache->bias = at::Tensor(c10::C10Tensor(caffe2::Tensor{caffe2::CPU}));
+    cache->bias = at::Tensor(caffe2::empty({0}, at::dtype<float>()));
   }
   caffe2::Tensor scale(*cache->scale);
   caffe2::Tensor bias(*cache->bias);
