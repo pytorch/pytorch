2325998d0f68a838dd0a4c4f312728b2fd606a57
Wanchao Liang <wanchaol@users.noreply.github.com>
2019-01-29 00:37:24 -0800

    fix code gen after the merge

--- torch/csrc/jit/tracer.h
+++ torch/csrc/jit/tracer.h
@@ -23,6 +23,21 @@ namespace torch {
 namespace jit {
 namespace tracer {
 
+using ::c10::ivalue::List;
+using ::c10::ivalue::Shared;
+
+using ::c10::IValue;
+using ::c10::ivalue::Future;
+using ::c10::ivalue::Tuple;
+
+using ::c10::ivalue::BoolList;
+using ::c10::ivalue::DoubleList;
+using ::c10::ivalue::GenericList;
+using ::c10::ivalue::IntList;
+using ::c10::ivalue::TensorList;
+
+using ::c10::ivalue::ConstantString;
+
 using torch::autograd::Variable;
 using variable_list = std::vector<Variable>;
 
