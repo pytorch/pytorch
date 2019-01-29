b1c24e2abf37f751d1c7c998c80f0336debdc546
Sebastian Messmer <messmer@fb.com>
2019-01-29 15:01:02 -0800

    V1: Initial commit
    Differential Revision: D13869275
    Differential Version: 70323184

--- aten/src/ATen/core/dispatch/Dispatcher.h
+++ aten/src/ATen/core/dispatch/Dispatcher.h
@@ -76,6 +76,23 @@ private:
 };
 
 /**
+ * Implement this interface and register your instance with the dispatcher
+ * to get notified when operators are registered or deregistered with
+ * the dispatcher.
+ */
+class CAFFE2_API OpRegistrationListener {
+public:
+  virtual ~OpRegistrationListener() = default;
+
+  virtual void onOperatorRegistered(const OperatorHandle& op) = 0;
+  virtual void onOperatorDeregistered(const OperatorHandle& op) = 0;
+};
+
+namespace detail {
+class RegistrationListenerList;
+}
+
+/**
  * Top-level dispatch interface for dispatching via the dynamic dispatcher.
  */
 class CAFFE2_API Dispatcher final {
@@ -90,52 +107,48 @@ public:
    * Register a new operator schema. The handle returned can be used to register
    * kernels to this operator or to call it.
    */
-  OperatorHandle registerSchema(FunctionSchema schema) {
-    // we need a lock to avoid concurrent writes
-    std::lock_guard<std::mutex> lock(mutex_);
+  OperatorHandle registerSchema(FunctionSchema schema);
 
-    operators_.emplace_back(std::move(schema));
-    return OperatorHandle(--operators_.end());
-  }
-
-  void deregisterSchema(OperatorHandle op) {
-    // we need a lock to avoid concurrent writes
-    std::lock_guard<std::mutex> lock(mutex_);
-
-    if (!op.dispatchTable_->isEmpty()) {
-      AT_ERROR("Tried to deregister op schema that still has kernels registered");
-    }
-    operators_.erase(op.dispatchTable_);
-  }
+  /**
+   * Remove a previously registered schema.
+   */
+  void deregisterSchema(OperatorHandle op);
 
   /**
    * Register an operator to the dispatch table for an operator.
    */
-  void registerKernel(const OperatorHandle& op, TensorTypeId dispatch_key, KernelFunction* kernel_func, KernelCacheCreatorFunction* cache_creator_func) {
-    // note: this doesn't need the mutex because write operations on the list keep iterators intact.
-    op.dispatchTable_->registerKernel(std::move(dispatch_key), DispatchTableEntry{kernel_func, cache_creator_func});
-  }
+  void registerKernel(const OperatorHandle& op, TensorTypeId dispatch_key, KernelFunction* kernel_func, KernelCacheCreatorFunction* cache_creator_func);
 
   /**
    * Remove an operator from the dispatch table for an operator.
    */
-  void deregisterKernel(const OperatorHandle& op, TensorTypeId dispatch_key) {
-    // note: this doesn't need the mutex because write operations on the list keep iterators intact.
-    op.dispatchTable_->deregisterKernel(dispatch_key);
-  }
+  void deregisterKernel(const OperatorHandle& op, TensorTypeId dispatch_key);
 
   /**
    * Perform a dynamic dispatch and get the kernel for an operator.
    */
-  OpKernel lookup(const OperatorHandle& op, const Stack* stack) const {
-    // note: this doesn't need the mutex because write operations on the list keep iterators intact.
-    const DispatchTableEntry& kernel = op.dispatchTable_->lookup(stack);
-    return OpKernel(kernel.kernel_func, kernel.cache_creator_func);
-  }
+  OpKernel lookup(const OperatorHandle& op, const Stack* stack) const;
+
+  /**
+   * Add a listener that gets called whenever a new op is registered or an existing
+   * op is deregistered. Immediately after registering, this listener gets called
+   * for all previously registered ops, so it can be used to keep track of ops
+   * registered with this dispatcher.
+   */
+  void addRegistrationListener(std::unique_ptr<OpRegistrationListener> listener);
 
 private:
+  Dispatcher();
+
   std::list<DispatchTable> operators_;
+  std::unique_ptr<detail::RegistrationListenerList> listeners_;
   std::mutex mutex_;
 };
+
+inline OpKernel Dispatcher::lookup(const OperatorHandle& op, const Stack* stack) const {
+  // note: this doesn't need the mutex because write operations on the list keep iterators intact.
+  const DispatchTableEntry& kernel = op.dispatchTable_->lookup(stack);
+  return OpKernel(kernel.kernel_func, kernel.cache_creator_func);
+}
 
 } // namespace c10
