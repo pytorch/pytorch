1948b0acfdb876907e82f03b7519da0341f67823
Chen, Jian Ping <jian.ping.chen@intel.com>
2019-01-29 13:32:10 +0800

    Optimize CPU RNN performance with MKL-DNN

---
+++ aten/src/ATen/mkldnn/TensorUtils.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <ATen/mkldnn/Runtime.h>
+
+namespace at { namespace native {
+
+using namespace mkldnn;
+
+inline memory::desc _format_md(const memory::dims& _dims, const memory::format& _format) {
+  return memory::desc(_dims, memory::data_type::f32, _format);
+}
+
+inline memory::desc _generic_md(const memory::dims& _dims) {
+  return _format_md(_dims, memory::format::any);
+}
+
+inline memory::primitive_desc _primitive_md(const memory::dims& _dims, const memory::format& _format) {
+  auto _engine = MKLDNNEngine::Instance().get_engine();
+  return memory::primitive_desc(_format_md(_dims, _format), _engine);
+}
+
+inline memory null_memory() {
+  auto _engine = MKLDNNEngine::Instance().get_engine();
+  return null_memory(_engine);
+}
+
+inline memory _new_mkldnn_memory(const memory::primitive_desc& pd, const Tensor& tensor) {
+  return memory(pd, tensor.data_ptr());
+}
+
+inline memory _new_mkldnn_memory(const memory::primitive_desc& pd) {
+  return memory(pd);
+}
+
+inline memory _reorder(const memory& from, const memory::primitive_desc& pd) {
+  if (pd == from.get_primitive_desc()) {
+    return from;
+  }
+  auto to = memory(pd);
+  MKLDNN_EXEC(reorder(from, to));
+  return to;
+}
+
+inline void  _reorder(const memory& from, const memory& to) {
+  if (from != to) {
+    MKLDNN_EXEC(reorder(from, to));
+  }
+}
+
+}} // namespace at::native
