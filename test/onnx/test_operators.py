f639c8c49e67bff44dbd130606542aa134cd1604
Lu Fang <lufang@fb.com>
2019-01-30 01:13:16 -0800

    Fix the dropout onnx symbolic, and ensure all exported models in test_operators.py are eval mode (#16547)
    Summary:
    In eval mode, skip dropout operator in onnx exporter.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16547
    
    Reviewed By: houseroad
    
    Differential Revision: D13877136
    
    Pulled By: dzhulgakov
    
    fbshipit-source-id: c366da156f83677bcf4989b79166aae5b6c36125

--- test/onnx/test_operators.py
+++ test/onnx/test_operators.py
@@ -3,7 +3,7 @@ from test_pytorch_common import TestCase, run_tests, skipIfNoLapack, flatten
 import torch
 import torch.onnx
 from torch.autograd import Variable, Function
-from torch.nn import Module
+from torch.nn import Module, functional
 import torch.nn as nn
 
 import itertools
@@ -62,6 +62,7 @@ class TestOperators(TestCase):
             m = f
         else:
             m = FuncModule(f, params)
+        m.eval()
         onnx_model_pbtxt = export_to_pbtxt(m, args, **kwargs)
         subname = kwargs.pop('subname', None)
         self.assertExpected(onnx_model_pbtxt, subname)
@@ -513,6 +514,11 @@ class TestOperators(TestCase):
     def test_erf(self):
         x = torch.randn(1, 2, 3, 4)
         self.assertONNX(lambda x: x.erf(), x)
+
+    def test_dropout(self):
+        x = torch.randn(3, 4, requires_grad=True)
+        self.assertONNX(lambda x: torch.max(functional.dropout(x, training=False)), x)
+
 
 if __name__ == '__main__':
     no_onnx_dep_flag = '--no-onnx'
