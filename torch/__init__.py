f75f1fda529fff20ffff16352e2682fcc3134db5
Zachary DeVito <zdevito@gmail.com>
2019-01-29 16:36:08 -0800

    try to get rid of tmp_install (#16414)
    Summary:
    Rehash of previous attempts. This tries a different approach where we accept the install as specified in cmake (leaving bin/ include/ and lib/ alone), and then try to adjust the rest of the files to this more standard layout.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16414
    
    Differential Revision: D13863635
    
    Pulled By: zdevito
    
    fbshipit-source-id: 23725f5c64d7509bf3ca8f472dcdcad074de9828

--- torch/__init__.py
+++ torch/__init__.py
@@ -240,7 +240,7 @@ _tensor_classes = set()
 def manager_path():
     if platform.system() == 'Windows':
         return b""
-    path = get_file_path('torch', 'lib', 'torch_shm_manager')
+    path = get_file_path('torch', 'bin', 'torch_shm_manager')
     prepare_multiprocessing_environment(get_file_path('torch'))
     if not os.path.exists(path):
         raise RuntimeError("Unable to find torch_shm_manager at " + path)
