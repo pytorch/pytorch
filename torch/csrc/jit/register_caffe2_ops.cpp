3f815f2c6b1941d862fb7ea6244d3a9d530024ff
Bram Wasti <bwasti@fb.com>
2019-01-31 12:50:57 -0800

    Expose BBoxTransform to pytorch
    Differential Revision: D13864292
    
    fbshipit-source-id: 94634c1b2da7236d4eed0c7ad6ed5e7302ee37f0

--- torch/csrc/jit/register_caffe2_ops.cpp
+++ torch/csrc/jit/register_caffe2_ops.cpp
@@ -2,6 +2,7 @@
 #include "caffe2/operators/layer_norm_op.h"
 #include "caffe2/operators/roi_align_op.h"
 #include "caffe2/operators/generate_proposals_op.h"
+#include "caffe2/operators/bbox_transform_op.h"
 
 #define REGISTER_CAFFE2_OP(name) \
   static caffe2::CAFFE2_STRUCT_OP_REGISTRATION_##name CAFFE2_STRUCT_OP_REGISTRATION_DEFN_TORCH_##name; \
@@ -10,3 +11,4 @@
 REGISTER_CAFFE2_OP(LayerNorm);
 REGISTER_CAFFE2_OP(RoIAlign);
 REGISTER_CAFFE2_OP(GenerateProposals);
+REGISTER_CAFFE2_OP(BBoxTransform);
