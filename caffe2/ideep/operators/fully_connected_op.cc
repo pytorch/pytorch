f1f114d05b9144e23cbac1295d9c808735cbde19
Gu, Jinghui <jinghui.gu@intel.com>
2019-02-01 14:40:12 +0800

    Impl INT8FC operator for DNNLOWP support
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>

--- caffe2/ideep/operators/fully_connected_op.cc
+++ caffe2/ideep/operators/fully_connected_op.cc
@@ -4,26 +4,6 @@ namespace caffe2 {
 
 USE_IDEEP_DEF_ALIASES();
 
-static inline itensor::dims CanonicalDims(itensor::dims adims, int32_t axis) {
-  CAFFE_ENFORCE(axis < (int32_t)adims.size(), "Invalid axis!");
-  CAFFE_ENFORCE(axis > (int32_t)-adims.size(), "Invalid axis!");
-  if (adims.size() == 2 || axis == 1) {
-    return adims;
-  }
-  if (axis < 0) {
-    axis += (int32_t)adims.size();
-  }
-
-  auto dim0 = std::accumulate(
-      adims.begin(),
-      adims.begin() + axis,
-      1,
-      std::multiplies<itensor::dim_t>());
-  auto dim1 = std::accumulate(
-      adims.begin() + axis, adims.end(), 1, std::multiplies<itensor::dim_t>());
-  return itensor::dims({dim0, dim1});
-}
-
 class IDEEPFullyConnectedOp final : public IDEEPOperator {
  public:
   USE_IDEEP_DEF_ALIASES();
