5bfb338a88678b11127b36c366d3f15b042b1c2c
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2019-02-01 20:47:06 +0900

    Fix a conditional expression of cutoffs.

--- torch/nn/modules/adaptive.py
+++ torch/nn/modules/adaptive.py
@@ -107,7 +107,7 @@ class AdaptiveLogSoftmaxWithLoss(Module):
 
         if (cutoffs != sorted(cutoffs)) \
                 or (min(cutoffs) <= 0) \
-                or (max(cutoffs) >= (n_classes - 1)) \
+                or (max(cutoffs) > (n_classes - 1)) \
                 or (len(set(cutoffs)) != len(cutoffs)) \
                 or any([int(c) != c for c in cutoffs]):
 
