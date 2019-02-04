d05dd0f32da9fcb62e97931781d12883ee944b04
Gregory Chanan <gchanan@fb.com>
2019-02-04 14:34:02 -0800

    Fix test to use storage constructor.

--- aten/src/ATen/test/xla_tensor_test.cpp
+++ aten/src/ATen/test/xla_tensor_test.cpp
@@ -4,12 +4,31 @@
 
 using namespace at;
 
+void XLAFree(void *ptr) {
+  free(ptr);
+}
+
+void* XLAMalloc(ptrdiff_t size) {
+  return malloc(size);
+}
+
+struct XLAAllocator final : public at::Allocator {
+  at::DataPtr allocate(size_t size) const override {
+    auto* ptr = XLAMalloc(size);
+    return {ptr, ptr, &XLAFree, at::DeviceType::XLA};
+  }
+  at::DeleterFnPtr raw_deleter() const override {
+    return &XLAFree;
+  }
+};
+
 TEST(XlaTensorTest, TestNoStorage) {
+  XLAAllocator allocator;
+  auto storage = Storage(caffe2::TypeMeta::Make<float>(), 0, &allocator, true);
   auto tensor_impl = c10::make_intrusive<TensorImpl, UndefinedTensorImpl>(
+      std::move(storage),
       XLATensorId(),
-      caffe2::TypeMeta::Make<float>(),
-      /*allocator=*/nullptr,
       /*is_variable=*/false);
   at::Tensor t(std::move(tensor_impl));
-  ASSERT_FALSE(t.storage());
+  ASSERT_TRUE(t.device() == DeviceType::XLA);
 }
