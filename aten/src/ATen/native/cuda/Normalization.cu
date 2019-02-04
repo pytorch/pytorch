e3e07459d1d9d084973cfb104a46e265554cbe8d
Adam Paszke <adam.paszke@gmail.com>
2019-02-04 23:58:57 +0100

    Add support for fusion of half batch norm with float stats

--- aten/src/ATen/native/cuda/Normalization.cu
+++ aten/src/ATen/native/cuda/Normalization.cu
@@ -27,10 +27,25 @@ std::tuple<Tensor, Tensor, Tensor> batch_norm_backward_cuda(const Tensor& grad_o
 std::tuple<Tensor, Tensor> batch_norm_update_stats_cuda(
         const Tensor& self, const Tensor& running_mean, const Tensor& running_var, double momentum) {
   return AT_DISPATCH_FLOATING_TYPES_AND_HALF(self.type(), "batch_norm_backward", [&] {
+      auto mean_st = running_mean.dtype();
+      auto var_st = running_var.dtype();
+      AT_CHECK(mean_st == var_st, "running_mean and running_var need to have the same data types");
+      // <sigh> Some workloads depend on passing in half input and float stats, which is
+      // usually handled by cuDNN. However, the JIT sometimes replaces cuDNN calls with this
+      // one so it needs to support the same case, or people start to complain.
+      bool is_half_float = std::is_same<scalar_t, at::Half>::value && mean_st == at::kFloat;
       if (cuda::detail::canUse32BitIndexMath(self)) {
-        return batch_norm_update_stats_cuda_template<scalar_t, int32_t>(self, running_mean, running_var, momentum);
+        if (is_half_float) {
+          return batch_norm_update_stats_cuda_template<at::Half, float, int32_t>(self, running_mean, running_var, momentum);
+        } else {
+          return batch_norm_update_stats_cuda_template<scalar_t, scalar_t, int32_t>(self, running_mean, running_var, momentum);
+        }
       } else {
-        return batch_norm_update_stats_cuda_template<scalar_t, int64_t>(self, running_mean, running_var, momentum);
+        if (is_half_float) {
+          return batch_norm_update_stats_cuda_template<at::Half, float, int64_t>(self, running_mean, running_var, momentum);
+        } else {
+          return batch_norm_update_stats_cuda_template<scalar_t, scalar_t, int64_t>(self, running_mean, running_var, momentum);
+        }
       }
     });
 }
