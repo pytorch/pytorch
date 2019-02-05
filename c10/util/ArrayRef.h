f9a5acc05d5a12a1e0cb416d03021440e9b84377
Edward Z. Yang <ezyang@fb.com>
2019-02-05 07:13:20 -0800

    V2: BC for IntList
    Differential Revision: D13954362
    Differential Version: 71072970

--- c10/util/ArrayRef.h
+++ c10/util/ArrayRef.h
@@ -267,4 +267,8 @@ bool operator!=(c10::ArrayRef<T> a1, std::vector<T> a2) {
 
 using IntArrayRef = ArrayRef<int64_t>;
 
+// This alias is deprecated because it doesn't make ownership
+// semantics obvious.  Use IntArrayRef instead!
+using IntList = ArrayRef<int64_t>;
+
 } // namespace c10
