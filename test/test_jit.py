bcc5f19c9243495448822bf938267487c6e3b48e
Edward Yang <ezyang@fb.com>
2019-02-05 14:39:43 -0800

    Rename IntList to IntArrayRef. (#16751)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16751
    
    This was made more complicated by the fact that ivalue::IntList
    is a thing.  So I had to fix all of the sites where we referring
    to IValue post facto.
    
    The following codemods were run, in this order:
    
    ```
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in IntList IntArrayRef
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in IntArrayRef::create IntList::create
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in ivalue::IntArrayRef ivalue::IntList
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in Tag::IntArrayRef Tag::IntList
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in isIntArrayRef isIntList
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in toIntArrayRef toIntList
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in 'Shared<IntArrayRef>' 'Shared<IntList>'
    codemod -m -d . --extensions cc,cpp,cu,cuh,h,hpp,py,cwrap,yaml,in 'intrusive_ptr<IntArrayRef>' 'intrusive_ptr<IntList>'
    ```
    
    Some manual fixups were done afterwards; they can be reviewed separately
    at https://github.com/pytorch/pytorch/pull/16752
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D13954363
    
    fbshipit-source-id: b5c40aacba042402155a2f5a229fa6db7992ac64

--- test/test_jit.py
+++ test/test_jit.py
@@ -3619,7 +3619,7 @@ a")
             return x.sum(dim=4)
 
         self.assertExpected(canonical(func.graph), subname='1')
-        # test that shape analysis is written correctly for sum with IntList[1] dim argument
+        # test that shape analysis is written correctly for sum with IntArrayRef[1] dim argument
         torch._C._jit_pass_shape_analysis(
             func2.graph, (torch.zeros(1, 1, 1, 1, 4),), False)
         self.assertExpected(canonical(func2.graph), subname='2')
