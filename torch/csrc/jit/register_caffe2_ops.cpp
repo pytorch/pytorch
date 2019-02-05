ef5ea8fdda750ff7df121d5856008abd3b940c24
Bram Wasti <bwasti@fb.com>
2019-02-05 12:30:29 -0800

    LayerNorm Registration Example (#16478)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16478
    
    This diff includes an example registration of a caffe2 op in torch.  A previous attempt ran into a static initialization order bug.
    
    Reviewed By: smessmer
    
    Differential Revision: D13854304
    
    fbshipit-source-id: ec463ce2272126d08a5163d1599361ee5b718bbc

--- torch/csrc/jit/register_caffe2_ops.cpp
+++ torch/csrc/jit/register_caffe2_ops.cpp
@@ -1,5 +1,8 @@
 #include <jit/custom_operator.h>
+#include "caffe2/operators/layer_norm_op.h"
 
 #define REGISTER_CAFFE2_OP(name) \
   static caffe2::CAFFE2_STRUCT_OP_REGISTRATION_##name CAFFE2_STRUCT_OP_REGISTRATION_DEFN_TORCH_##name; \
   static auto CAFFE2_OP_EXPORT_##name = torch::jit::RegisterOperators::Caffe2Operator(#name);
+
+REGISTER_CAFFE2_OP(LayerNorm);
