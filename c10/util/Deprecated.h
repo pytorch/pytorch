eae991f563f2c5e6ef081962787754582d179061
Edward Z. Yang <ezyang@fb.com>
2019-02-06 07:28:19 -0800

    V1: Initial commit
    Differential Revision: D13972298
    Differential Version: 71211094

--- c10/util/Deprecated.h
+++ c10/util/Deprecated.h
@@ -3,14 +3,14 @@
 // Largely from https://stackoverflow.com/questions/295120/c-mark-as-deprecated
 
 #if defined(__cplusplus) && __cplusplus > 201402L
-#define C10_DEPRECATED(function) [[deprecated]] function
+#define C10_DEPRECATED [[deprecated]]
 #else
 #if defined(__GNUC__)
-#define C10_DEPRECATED(function) __attribute__((deprecated)) function
+#define C10_DEPRECATED __attribute__((deprecated))
 #elif defined(_MSC_VER)
-#define C10_DEPRECATED(function) __declspec(deprecated) function
+#define C10_DEPRECATED __declspec(deprecated)
 #else
 #warning "You need to implement C10_DEPRECATED for this compiler"
-#define C10_DEPRECATED(function) function
+#define C10_DEPRECATED
 #endif // defined(__GNUC__)
 #endif // defined(__cplusplus) && __cplusplus > 201402L
