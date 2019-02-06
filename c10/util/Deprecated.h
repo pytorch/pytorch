99c75109faf1d684a5e28ce7cfaeedc850475cb5
Edward Z. Yang <ezyang@fb.com>
2019-02-06 08:10:30 -0800

    V3: try again
    Differential Revision: D13972297
    Differential Version: 71214728

--- c10/util/Deprecated.h
+++ c10/util/Deprecated.h
@@ -2,18 +2,17 @@
 
 // Largely from https://stackoverflow.com/questions/295120/c-mark-as-deprecated
 
-#if (defined(__cplusplus) && __cplusplus > 201402L) || defined(__CUDACC__) || defined(__HIPCC__)
+#if defined(_MSC_VER)
+// [[deprecated]] doesn't work on Windows, so try declspec first
+# define C10_DEPRECATED __declspec(deprecated)
+#elif (defined(__cplusplus) && __cplusplus > 201402L) || defined(__CUDACC__) || defined(__HIPCC__)
 // NB: We preferentially use [[deprecated]] for nvcc because
 // nvcc doesn't understand __attribute__((deprecated)) syntax
 // for 'using' declarations (even though the host compiler does.)
-#define C10_DEPRECATED [[deprecated]]
+# define C10_DEPRECATED [[deprecated]]
+#elif defined(__GNUC__)
+# define C10_DEPRECATED __attribute__((deprecated))
 #else
-#if defined(__GNUC__)
-#define C10_DEPRECATED __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#define C10_DEPRECATED __declspec(deprecated)
-#else
-#warning "You need to implement C10_DEPRECATED for this compiler"
-#define C10_DEPRECATED
-#endif // defined(__GNUC__)
-#endif // defined(__cplusplus) && __cplusplus > 201402L
+# warning "You need to implement C10_DEPRECATED for this compiler"
+# define C10_DEPRECATED
+#endif
