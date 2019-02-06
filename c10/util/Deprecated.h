1eb71709242f27d599b36ccee0044815909dfc61
Edward Z. Yang <ezyang@fb.com>
2019-02-06 07:28:28 -0800

    V1: Initial commit
    Differential Revision: D13972297
    Differential Version: 71211095

--- c10/util/Deprecated.h
+++ c10/util/Deprecated.h
@@ -2,7 +2,10 @@
 
 // Largely from https://stackoverflow.com/questions/295120/c-mark-as-deprecated
 
-#if defined(__cplusplus) && __cplusplus > 201402L
+#if (defined(__cplusplus) && __cplusplus > 201402L) || defined(__CUDACC__) || defined(__HIPCC__)
+// NB: We preferentially use [[deprecated]] for nvcc because
+// nvcc doesn't understand __attribute__((deprecated)) syntax
+// for 'using' declarations (even though the host compiler does.)
 #define C10_DEPRECATED [[deprecated]]
 #else
 #if defined(__GNUC__)
