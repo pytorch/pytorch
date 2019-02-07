d2c88ee20f6728d51325d89ab0306da8ad2e36e9
bddppq <bai@in.tum.de>
2019-02-07 01:25:11 +0000

    make the update of has_gpu_support, has_cuda_support and has_hip_support atomic

--- caffe2/python/_import_c_extension.py
+++ caffe2/python/_import_c_extension.py
@@ -11,17 +11,18 @@ from caffe2.python import extension_loader
 with extension_loader.DlopenGuard():
     has_hip_support = False
     has_cuda_support = False
+    has_gpu_support = False
 
     try:
         from caffe2.python.caffe2_pybind11_state_gpu import *  # noqa
         if num_cuda_devices():  # noqa
-            has_cuda_support = True
+            has_gpu_support = has_cuda_support = True
     except ImportError as gpu_e:
         logging.info('Failed to import cuda module: {}'.format(gpu_e))
         try:
             from caffe2.python.caffe2_pybind11_state_hip import *  # noqa
             if num_hip_devices():
-                has_hip_support = True
+                has_gpu_support = has_hip_support = True
                 logging.info('This caffe2 python run has AMD GPU support!')
         except ImportError as hip_e:
             logging.info('Failed to import AMD hip module: {}'.format(hip_e))
@@ -35,9 +36,6 @@ with extension_loader.DlopenGuard():
                 logging.critical(
                     'Cannot load caffe2.python. Error: {0}'.format(str(cpu_e)))
                 sys.exit(1)
-
-    # setting this variable to make this module backward compatible.
-    has_gpu_support = has_cuda_support or has_hip_support
 
 # libcaffe2_python contains a global Workspace that we need to properly delete
 # when exiting. Otherwise, cudart will cause segfaults sometimes.
