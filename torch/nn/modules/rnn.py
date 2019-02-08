87aabbcf2412ac500c6599d848538dbbd3c477e1
davidriazati <davidriazati@fb.com>
2019-02-07 16:10:08 -0800

    Fix extra self

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -167,7 +167,7 @@ class RNNBase(Module):
 
     def check_forward_args(self, input, hidden, batch_sizes):
         self.check_input(input, batch_sizes)
-        self.expected_hidden_size = self.self.get_expected_hidden_size(input, batch_sizes)
+        expected_hidden_size = self.get_expected_hidden_size(input, batch_sizes)
 
         self.check_hidden_size(hidden, expected_hidden_size)
 
