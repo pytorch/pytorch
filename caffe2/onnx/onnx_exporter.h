97d18bdabd3dfed22f511f98af6f91eac6e87af6
Kimish Patel <kimishpatel@fb.com>
2019-02-11 14:32:30 -0800

    Keep weights name unchanged during SsaRewrite (#16932)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/16932
    
    During onnxifi transformation net ssa is rewritten. At the last step the weight
    names are changed back to what they were before. The diff keeps the weight
    names unchanged thru the process.
    
    Reviewed By: yinghai
    
    Differential Revision: D13972597
    
    fbshipit-source-id: 7c29857f788a674edf625c073b345f2b44267b33

--- caffe2/onnx/onnx_exporter.h
+++ caffe2/onnx/onnx_exporter.h
@@ -28,7 +28,9 @@ using ConvertedResult =
 // output names for predict net.
 CAFFE2_API std::unordered_map<std::string, std::string> SsaRewrite(
     caffe2::NetDef* init_net,
-    caffe2::NetDef* pred_net);
+    caffe2::NetDef* pred_net,
+    const std::unordered_set<std::string>& exceptions =
+        std::unordered_set<std::string>());
 
 ::ONNX_NAMESPACE::TensorProto::DataType Caffe2TypeToOnnxType(
     caffe2::TensorProto::DataType t);
