b8dc6e26411216ca05dceade2742b4b772c80493
jayleverett <jay@aireverie.com>
2019-02-12 14:51:58 -0500

    fix indentation

--- torch/utils/data/dataset.py
+++ torch/utils/data/dataset.py
@@ -75,7 +75,7 @@ class ConcatDataset(Dataset):
     def __getitem__(self, idx):
         if idx < 0:
             if -idx > len(self):
-             raise ValueError("absolute value of index should not exceed dataset length")
+                raise ValueError("absolute value of index should not exceed dataset length")
             idx = len(self) + idx
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if dataset_idx == 0:
