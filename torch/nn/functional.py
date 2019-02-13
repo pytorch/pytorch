f69ef4e1a1830f139bad38c69d86fe3f5f1b31d6
Edward Z. Yang <ezyang@mit.edu>
2019-02-13 13:42:57 -0500

    format fix

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -247,7 +247,7 @@ Args:
 Examples::
 
     >>> # pool of square window of size=3, stride=2
-    >>> input = torch.tensor([[[1.,2,3,4,5,6,7]]])
+    >>> input = torch.tensor([[[1, 2, 3, 4, 5, 6, 7]]], dtype=torch.float32)
     >>> F.avg_pool1d(input, kernel_size=3, stride=2)
     tensor([[[ 2.,  4.,  6.]]])
 
