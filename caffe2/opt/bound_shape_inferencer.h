0a20ed302e1d9babf0f5aa2ecb10ac113d8b56d0
Ying Zhang <yingz@fb.com>
2019-02-12 16:37:50 -0800

    Extract ShapeInfo and some util functions into a separate file. (#17025)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17025
    
    Extract ShapeInfo and some util functions into a separate file.
    
    Reviewed By: yinghai
    
    Differential Revision: D14017432
    
    fbshipit-source-id: 201db46bce6d52d9355a1a86925aa6206d0336bf

--- caffe2/opt/bound_shape_inferencer.h
+++ caffe2/opt/bound_shape_inferencer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "caffe2/core/logging.h"
+#include "caffe2/opt/shape_info.h"
 #include "caffe2/proto/caffe2_pb.h"
 
 #include <sstream>
@@ -9,20 +10,6 @@
 #include <unordered_set>
 
 namespace caffe2 {
-
-struct CAFFE2_API ShapeInfo {
-  enum DimType : int8_t { UNKNOWN = 0, CONSTANT = 1, BATCH = 2, SEQ = 3 };
-  ShapeInfo() {}
-  ShapeInfo(DimType t, TensorShape&& s) : dim_type(t), shape(std::move(s)) {}
-  ShapeInfo(DimType t, const TensorShape& s) : dim_type(t), shape(s) {}
-
-  // type of the shape according its first dim
-  DimType dim_type{DimType::UNKNOWN};
-  TensorShape shape;
-};
-
-using ShapeInfoMap = std::unordered_map<std::string, ShapeInfo>;
-
 // This struct stores the max bound size for batch in the general sense. We have
 // the conventioal batch size and the look-up sequence, which is also batch in a
 // sense.
@@ -51,7 +38,7 @@ class CAFFE2_API BoundShapeInferencer {
       const NetDef& net,
       const std::unordered_map<std::string, ShapeInfo>& info);
 
-  const std::unordered_map<std::string, ShapeInfo>& shape_info() const {
+  const ShapeInfoMap& shape_info() const {
     return shape_info_;
   }
 
