416d289e88c1c50207489776064bec581bbc85d6
Jie <jiej@nvidia.com>
2019-02-13 12:38:57 -0800

    fixing test failures by updating layer data type

--- test/test_nn.py
+++ test/test_nn.py
@@ -5463,9 +5463,10 @@ class TestNN(NNTestCase):
             self.assertAlmostEqual(cudnn_input_grad, thnn_input_grad, delta=1e-3)
 
     @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
-    def test_batchnorm_large_batch(self):
-        bn = nn.BatchNorm1d(1).cuda()
-        data = torch.rand(131072, 1, device="cuda", dtype=torch.float)
+    @repeat_test_for_types([torch.float, torch.half])
+    def test_batchnorm_large_batch(self, dtype=torch.float):
+        bn = nn.BatchNorm1d(1).to('cuda', dtype)
+        data = torch.rand(131072, 1, device="cuda", dtype=dtype)
         out = bn(data).sum().backward()
 
     def _test_batchnorm_update_stats(self, device="cpu", dtype=torch.float):
