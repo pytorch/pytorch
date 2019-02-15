475d4876250a5610cba4e5e1a00e1677aa766125
Wanchao Liang <wanchaol@users.noreply.github.com>
2019-02-14 16:02:16 -0800

    add std to autodiff, and mean/var/std to operator set

--- torch/csrc/jit/symbolic_script.cpp
+++ torch/csrc/jit/symbolic_script.cpp
@@ -294,6 +294,26 @@ const std::vector<std::string> functions = {
 
             return torch.var(self, dim, unbiased, keepdim), backward
 
+        def std_0(self,
+                  unbiased: bool=True):
+            std_out = torch.std(self, unbiased)
+            def backward(grad_output):
+                grad_self = torch.var_backward(grad_output / (std_out * 2), self, unbiased)
+                return grad_self, None
+
+            return std_out, backward
+
+        def std_1(self,
+                  dim: List[int],
+                  unbiased: bool,
+                  keepdim: bool):
+            std_out = torch.std(self, dim, unbiased, keepdim)
+            def backward(grad_output):
+                grad_self = torch.var_backward(grad_output / (std_out * 2), self, dim, unbiased, keepdim)
+                return grad_self, None, None, None
+
+            return std_out, backward
+
         def view(self,
                  size: List[int]):
             self_size = self.size()
