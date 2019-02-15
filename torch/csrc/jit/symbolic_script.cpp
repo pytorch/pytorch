8d33eb450ef8f6de02ac999af7f88d4b45780249
Ailing Zhang <ailzhang@fb.com>
2019-02-15 13:50:10 -0800

    Fix avg pool2d api (#17166)
    Summary:
    Fix xla breakage (partially).
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17166
    
    Differential Revision: D14106954
    
    Pulled By: ailzhang
    
    fbshipit-source-id: 35ae6713272d0517b66da2ee9209f49015492b89

--- torch/csrc/jit/symbolic_script.cpp
+++ torch/csrc/jit/symbolic_script.cpp
@@ -323,10 +323,14 @@ const std::vector<std::string> functions = {
 
             return torch.view(self, size), backward
 
-        def _adaptive_avg_pool2d(self,
+        def adaptive_avg_pool2d(self,
                                 output_size: List[int]):
+            self_size = self.size()
             def backward(grad_output):
-                grad_self = torch._adaptive_avg_pool2d_backward(grad_output, self)
+                if output_size[0] == 1 and output_size[1] == 1:
+                    grad_self = grad_output.expand(self_size)
+                else:
+                    grad_self = torch._adaptive_avg_pool2d_backward(grad_output, self)
                 return grad_self, None
 
             return torch._adaptive_avg_pool2d(self, output_size), backward
