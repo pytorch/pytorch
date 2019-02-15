89e44836d418a7e6ccaa6c7a228074db6cbc9238
Ailing Zhang <ailzhang@fb.com>
2019-02-15 09:42:35 -0800

    fix ad for adaptive_avg_pool2d

--- torch/csrc/jit/symbolic_script.cpp
+++ torch/csrc/jit/symbolic_script.cpp
@@ -305,8 +305,12 @@ const std::vector<std::string> functions = {
 
         def adaptive_avg_pool2d(self,
                                 output_size: List[int]):
+            self_size = self.size()
             def backward(grad_output):
-                grad_self = torch.adaptive_avg_pool2d_backward(grad_output, self)
+                if output_size[0] == 1 and output_size[1] == 1:
+                    grad_self = grad_output.expand(self_size)
+                else:
+                    grad_self = torch._adaptive_avg_pool2d_backward(grad_output, self)
                 return grad_self, None
 
             return torch.adaptive_avg_pool2d(self, output_size), backward
