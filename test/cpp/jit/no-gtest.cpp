6c06b3255885f41e5837568038da5a86a26ccb3c
Mikhail Zolotukhin <mvz@fb.com>
2019-02-15 20:33:04 -0800

    Implement NetDef <--> JIT IR converters. Try 2. (#17123)
    Summary:
    Currently the converters are very straightforward, i.e. there is no code for trying to
    preserve semantics, we're purely perform conversion from one format to another.
    
    Two things that we might want to add/change:
    
    1. Add semantic conversion as well (but probably it would be a good idea to keep
       it separate as a temporary thing).
    2. Make sure we don't mess with value names, as they are crucial for current
       uses of NetDefs.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17123
    
    Differential Revision: D14090244
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 07175fa9235582e1d1da5f10a42a5c1280b1b394

--- test/cpp/jit/no-gtest.cpp
+++ test/cpp/jit/no-gtest.cpp
@@ -1,5 +1,6 @@
 #include <test/cpp/jit/test_alias_analysis.h>
 #include <test/cpp/jit/test_misc.h>
+#include <test/cpp/jit/test_netdef_converter.h>
 
 #include <sstream>
 #include <string>
@@ -37,6 +38,7 @@ std::string runJITCPPTests() {
   testRegisterFusionCachesKernel();
   testAliasAnalysis();
   testAliasTracker();
+  testNetDefConverter(out);
   return out.str();
 }
 
