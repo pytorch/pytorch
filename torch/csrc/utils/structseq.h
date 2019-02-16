e326d3742853b990b09235b0ea0d3955233042f4
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-02-16 17:46:22 -0500

    fix typo

--- torch/csrc/utils/structseq.h
+++ torch/csrc/utils/structseq.h
@@ -5,7 +5,7 @@
 namespace torch { namespace utils {
 
 #if PY_MAJOR_VERSION == 2
-PyObject structseq_slice(PyStructSequence *obj, Py_ssize_t low, Py_ssize_t high);
+PyObject *structseq_slice(PyStructSequence *obj, Py_ssize_t low, Py_ssize_t high);
 #endif
 
 PyObject *returned_structseq_repr(PyStructSequence *obj);
