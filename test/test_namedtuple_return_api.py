6fde89dcf0f76b739d0a84d340b39605187a9aea
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-02-16 09:28:29 -0500

    update namedtuple return api test

--- test/test_namedtuple_return_api.py
+++ test/test_namedtuple_return_api.py
@@ -9,6 +9,7 @@ aten_native_yaml = os.path.join(path, '../aten/src/ATen/native/native_functions.
 whitelist = [
     'max', 'max_out', 'min', 'min_out', 'median', 'median_out',
     'mode', 'mode_out', 'kthvalue', 'kthvalue_out', 'svd', 'svd_out',
+    'gesv', 'slogdet', 'sort', 'topk',
 ]
 
 
