fd7e15147e0b97d4077a22e450fa40c0b727c1e0
ZhuBaohe <ehoabuhz@gmail.com>
2019-02-17 13:44:37 +0800

    modify the punctuation

--- torch/nn/modules/adaptive.py
+++ torch/nn/modules/adaptive.py
@@ -75,7 +75,7 @@ class AdaptiveLogSoftmaxWithLoss(Module):
     Args:
         in_features (int): Number of features in the input tensor
         n_classes (int): Number of classes in the dataset
-        cutoffs (Sequence): Cutoffs used to assign targets to their buckets.
+        cutoffs (Sequence): Cutoffs used to assign targets to their buckets
         div_value (float, optional): value used as an exponent to compute sizes
             of the clusters. Default: 4.0
         head_bias (bool, optional): If ``True``, adds a bias term to the ‘head’ of the
