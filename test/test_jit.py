a91e056f2ac6fe4fd42d4936ade0694d1e0521a5
Nikolay Korovaiko <korovaikon@gmail.com>
2019-02-20 09:11:11 -0800

    add list methods: copy,extend (#17092)
    Summary:
    This PR adds the following methods to python's list.
    
    * copy
    * extend
    
    and tests
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17092
    
    Differential Revision: D14141817
    
    Pulled By: Krovatkin
    
    fbshipit-source-id: c89207f0f25f3d1d4ad903ee634745615d61d576

--- test/test_jit.py
+++ test/test_jit.py
@@ -4112,6 +4112,48 @@ a")
             return len(a) == 0
         self.checkScript(test_clear, ())
 
+    def test_extend_list_mutable(self):
+        @torch.jit.script
+        def extend_list(a, b):
+            # type: (List[Tensor], List[Tensor]) -> List[Tensor]
+
+            a.extend(b)
+            return a
+
+        for l in [[], [torch.rand(2)], [torch.rand(2), torch.rand(2), torch.rand(2)]]:
+            for r in [[], [torch.rand(2)], [torch.rand(2), torch.rand(2), torch.rand(2)]]:
+                self.assertEqual(extend_list(l, r), l + r)
+
+    def test_extend_list_immutable(self):
+        @torch.jit.script
+        def extend_list(a, b):
+            # type: (List[int], List[int]) -> List[int]
+
+            a.extend(b)
+            return a
+
+        for l in [[], [1], [1, 2, 3]]:
+            for r in [[], [1], [1, 2, 3]]:
+                self.assertEqual(extend_list(l, r), l + r)
+
+    def test_copy_list_mutable(self):
+        @torch.jit.script
+        def copy_list(a):
+            # type: (List[Tensor]) -> List[Tensor]
+            return a.copy()
+
+        for l in [[], [torch.rand(2)], [torch.rand(2), torch.rand(2), torch.rand(2)]]:
+            self.assertEqual(copy_list(l), l)
+
+    def test_copy_list_immutable(self):
+        @torch.jit.script
+        def copy_list(a):
+            # type: (List[int]) -> List[int]
+            return a.copy()
+
+        for l in [[], [1], [1, 2, 3]]:
+            self.assertEqual(copy_list(l), l)
+
     def test_func_call(self):
         script = '''
         def add(a, b):
