cf3fb90bd183efe91ba34d175129d5c3a638ef5b
Michael Suo <suo@fb.com>
2019-02-19 17:43:38 -0800

    clean up print from test
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/17279
    
    Differential Revision: D14144264
    
    Pulled By: suo
    
    fbshipit-source-id: eec837d29c46e96be37c54192a841046b486cb8b

--- test/test_jit.py
+++ test/test_jit.py
@@ -6101,8 +6101,6 @@ a")
 
         m = M()
         graph = str(m.graph)
-        print(graph)
-        return
         self.assertTrue(graph.count("aten::add") == 4)
         self.assertTrue("python" not in graph)
 
