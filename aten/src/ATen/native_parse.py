9101dfc57ccb6b6931b4e80233bbc64d9080d2e8
Christian Puhrsch <cpuhrsch@fb.com>
2019-02-21 14:31:24 -0800

    Revisit some native functions to increase number of jit matches (#17340)
    Summary:
    Adds about 30 matches due to new functions / misuse of double.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17340
    
    Differential Revision: D14161109
    
    Pulled By: cpuhrsch
    
    fbshipit-source-id: bb3333446b32551f7469206509b480db290f28ee

--- aten/src/ATen/native_parse.py
+++ aten/src/ATen/native_parse.py
@@ -69,6 +69,9 @@ def type_argument_translations(arg):
     # Enables str by translating to legacy std::string.
     elif t == 'str':
         t = 'std::string'
+    elif t == 'double':
+        raise RuntimeError("Please use float and not double. "
+                           "See [temp translations] for details.")
     # Enables int[x] by translating to legacy IntArrayRef[x]. See [temp translations]
     elif re.match(r'int\[(\d+)\]', t):
         match = re.match(r'int\[(\d+)\]', t)
