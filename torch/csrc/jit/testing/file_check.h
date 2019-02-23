d8d8371bd3d117d4c89df488d80760f0f44a98b8
eellison <elias_ellison@brown.edu>
2019-02-22 17:54:09 -0800

    Batch of Expect Files removal (#17414)
    Summary:
    Batch of removing expect files, and some tests that no longer test anything.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17414
    
    Differential Revision: D14196342
    
    Pulled By: eellison
    
    fbshipit-source-id: 75c45649d1dd1ce39958fb02f5b7a2622c1d1d01

--- torch/csrc/jit/testing/file_check.h
+++ torch/csrc/jit/testing/file_check.h
@@ -32,8 +32,12 @@ struct FileCheck {
   // previous match
   TORCH_API FileCheck* check_next(const std::string& str);
 
-  // Checks that the string occurs count number of times
-  TORCH_API FileCheck* check_count(const std::string& str, size_t count);
+  // Checks that the string occurs count number of times. If exactly is true,
+  // checks that there are exactly count many matches
+  TORCH_API FileCheck* check_count(
+      const std::string& str,
+      size_t count,
+      bool exactly = false);
 
   // A series of consecutive check_dags get turned into a group of checks
   // which can appear in any order relative to each other.
@@ -46,7 +50,6 @@ struct FileCheck {
   bool has_run = false;
   std::unique_ptr<FileCheckImpl> fcImpl;
 };
-
 } // namespace testing
 } // namespace jit
 } // namespace torch
