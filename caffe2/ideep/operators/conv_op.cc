cc875420229f92c5d7643ec2175cd83c6bd6e561
Gu, Jinghui <jinghui.gu@intel.com>
2019-02-25 11:42:39 +0800

    Remove duplicated code in Conv and ConvFusion
    Signed-off-by: Gu, Jinghui <jinghui.gu@intel.com>

--- caffe2/ideep/operators/conv_op.cc
+++ caffe2/ideep/operators/conv_op.cc
@@ -2,7 +2,7 @@
 
 namespace caffe2 {
 
-class IDEEPConvOp final : public IDEEPConvPoolOpBase {
+class IDEEPConvOp : public IDEEPConvPoolOpBase {
  public:
   USE_IDEEP_DEF_ALIASES();
   USE_IDEEP_CONV_POOL_BASE_FUNCTIONS();
@@ -14,6 +14,9 @@ class IDEEPConvOp final : public IDEEPConvPoolOpBase {
     OPERATOR_NEEDS_FEATURE(
         pad_l() == pad_r() && pad_t() == pad_b(),
         "Uneven padding not supported.");
+
+    fusion_type_ = FUSION_UNKNOWN;
+    last_input_ = BIAS_OR_INPUT_S;
 
     training_mode_ = OperatorBase::GetSingleArgument<int>("training_mode", 0);
     pk_ = training_mode_ ? iprop::forward_training : iprop::forward_inference;
@@ -28,11 +31,11 @@ class IDEEPConvOp final : public IDEEPConvPoolOpBase {
   virtual ~IDEEPConvOp() {}
 
   bool RunOnDeviceWithOrderNCHW() override {
-    const auto& X = Input(INPUT);
+    const auto& X = Input(INPUT_X);
     const auto& filter = Input(FILTER);
     auto* Y = Output(OUTPUT);
     auto grouped = filter.is_grouped() ? 1 : 0;
-    auto Y_dims = CalcOutputDims(
+    auto Y_dims_conv = CalcOutputDims(
         X,
         grouped ? (filter.get_dim(0) * filter.get_dim(1)) : filter.get_dim(0));
 
@@ -83,20 +86,13 @@ class IDEEPConvOp final : public IDEEPConvPoolOpBase {
       cached_X_descriptor_ = X.dup_descriptor();
     }
 
-    // NB: actually, in the case when `group_ > 1`, IDEEP will create
-    // an itermediate tensor for each run below. However, this tensor is merely
-    // a view of of the weights and there is no actual data copy, so I'll let it
-    // go now. If we encounter performance surprise when convoluting with group
-    // > 1, this is the first place to check and we need to do the same cache
-    // trick as above
-
-    if (InputSize() > BIAS) {
+    if (InputSize() > last_input_) {
       ideep::convolution_forward::compute(
           op_key_,
           X,
           training_mode_ ? filter : filter_,
-          Input(BIAS),
-          Y_dims,
+          Input(BIAS_OR_INPUT_S),
+          Y_dims_conv,
           *Y,
           stride_,
           dilation_,
@@ -114,7 +110,7 @@ class IDEEPConvOp final : public IDEEPConvPoolOpBase {
           op_key_,
           X,
           training_mode_ ? filter : filter_,
-          Y_dims,
+          Y_dims_conv,
           *Y,
           stride_,
           dilation_,
@@ -129,22 +125,128 @@ class IDEEPConvOp final : public IDEEPConvPoolOpBase {
           pk_);
     }
 
+    if (fusion_type_ == FUSION_CONV_SUM
+        && fusion_type_ == FUSION_CONV_SUM_RELU) {
+      CAFFE_ENFORCE(Y == &(Input(InputSize() - 1)),
+          "Convolution fusion op: InPlace is enforced for sum fusion.");
+    }
+
     return true;
   }
 
- private:
+ protected:
   iprop pk_;
   ialgo algo_;
   iattr attr_;
   ikey op_key_;
-  iscale dummy_scale_;
+  int last_input_;
   bool training_mode_;
+  FusionType fusion_type_;
   itensor filter_;
+  iscale dummy_scale_;
   itensor::descriptor cached_X_descriptor_, cached_weights_descriptor_;
 
-  INPUT_TAGS(INPUT, FILTER, BIAS);
+  INPUT_TAGS(INPUT_X, FILTER, BIAS_OR_INPUT_S, INPUT_S);
   OUTPUT_TAGS(OUTPUT);
 };
+
+class IDEEPConvFusionOp final : public IDEEPConvOp {
+ public:
+  USE_IDEEP_DEF_ALIASES();
+  USE_IDEEP_CONV_POOL_BASE_FUNCTIONS();
+
+  IDEEPConvFusionOp(const OperatorDef& operator_def, Workspace* ws)
+      : IDEEPConvOp(operator_def, ws) {
+    CAFFE_ENFORCE(OperatorBase::HasArgument("fusion_type"),
+          "You should specify the fusion type");
+    fusion_type_ = static_cast<FusionType>(
+        OperatorBase::GetSingleArgument<int>("fusion_type", FUSION_UNKNOWN));
+    OPERATOR_NEEDS_FEATURE(
+        fusion_type_ > FUSION_UNKNOWN && fusion_type_ < FUSION_MAX,
+        "Undefined Conv fusion type.",
+        fusion_type_);
+
+    if (fusion_type_ == FUSION_CONV_RELU) {
+      attr_ = iattr::fuse_relu();
+      last_input_ = BIAS_OR_INPUT_S;
+    } else if (fusion_type_ == FUSION_CONV_SUM) {
+      attr_ = iattr::fuse_sum();
+      last_input_ = INPUT_S;
+    } else if (fusion_type_ == FUSION_CONV_SUM_RELU) {
+      attr_ = iattr::residual();
+      last_input_ = INPUT_S;
+    }
+  }
+  virtual ~IDEEPConvFusionOp() {}
+};
+
+const char* kConvFusionDoc = R"DOC(
+Note that other parameters, such as the stride and
+kernel size, or the pads' sizes in each direction are not necessary for input
+because they are provided by the ConvPoolOpBase operator. Various dimension
+checks are done implicitly, and the sizes are specified in the Input docs for
+this operator. As is expected, the filter is convolved with a subset of the
+image and the bias is added; this is done throughout the image data and the
+output is computed. As a side note on the implementation layout:
+conv_op_impl.h is the templated implementation of the conv_op.h file, which is
+why they are separate files.
+)DOC";
+
+std::function<void(OpSchema&)> ConvFusionDocGenerator(const char* dim) {
+  return [=](OpSchema& schema) {
+    string doc = R"DOC(
+The convolution fusion operator consumes an input vector, a {dim}filter blob,
+a bias blob and another input vector and computes the output. This operator
+gives the chance to fuse the ReLU or element-wise Sum with a convolution
+operator. {conv_fusion_doc})DOC";
+    c10::ReplaceAll(doc, "{dim}", dim);
+    c10::ReplaceAll(doc, "{conv_fusion_doc}", kConvFusionDoc);
+    schema.SetDoc(doc);
+    schema.Input(
+        0,
+        "X",
+        "Input data blob from previous layer; has size (N x C x H x W), "
+        "where N is the batch size, C is the number of channels, "
+        "and H and W are the height and width. Note that this is for the NCHW "
+        "usage. On the other hand, the NHWC Op has a different set of "
+        "dimension constraints. ");
+    schema.Input(
+        1,
+        "filter",
+        "The filter blob that will be used in the "
+        "convolutions; has size (M x C x kH x kW), where C is the number of "
+        "channels, and kH and kW are the height and width of the kernel.");
+    schema.Input(
+        2,
+        "bias",
+        "The 1D bias blob that is added through the "
+        "convolution; has size (M).");
+    schema.Input(
+        3,
+        "S",
+        "Input data blob for element-wise Sum fusion from previous layer; "
+        "has the same size of convolution output. Its input index should "
+        "be 2 if no bias for this convolution, and it MUST be inplace with "
+        "output Y.");
+    schema.Output(
+        0,
+        "Y",
+        "Output data blob that contains the result of the "
+        "convolution fusion. The output dimensions are functions of the kernel "
+        "size, stride size, and pad lengths."
+        "");
+  };
+}
+
+OPERATOR_SCHEMA(ConvFusion)
+    .NumInputs(2, 4)
+    .NumOutputs(1)
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForConv)
+    .CostInferenceFunction(OpSchema::CostInferenceFunctionType(
+        ConvPoolOpBase<CPUContext>::CostInferenceForConv))
+    .Arg("fusion_type", "Which fusion type is used")
+    .AllowInplace({{2, 0}, {3, 0}})
+    .FillUsing(ConvFusionDocGenerator(""));
 
 class IDEEPConvGradientOp final : public IDEEPConvPoolOpBase {
  public:
@@ -224,6 +326,7 @@ class IDEEPConvGradientOp final : public IDEEPConvPoolOpBase {
 };
 
 REGISTER_IDEEP_OPERATOR(Conv, IDEEPConvOp);
+REGISTER_IDEEP_OPERATOR(ConvFusion, IDEEPConvFusionOp);
 REGISTER_IDEEP_OPERATOR(ConvGradient, IDEEPConvGradientOp);
 
 } // namespace caffe2
