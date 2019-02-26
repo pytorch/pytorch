5ea0a6e99f0b1b9fbdeb4ef9c265afa12b9c6221
Michael Suo <suo@fb.com>
2019-02-26 01:24:05 -0800

    user defined types (#17314)
    Summary:
    First pass at user defined types. The following is contained in this PR:
    - `UserType` type, which contains a reference to a module with all methods for the type, and a separate namespace for data attributes (map of name -> TypePtr).
    - `UserTypeRegistry`, similar to the operator registry
    - `UserObject` which is the runtime representation of the user type (just a map of names -> IValues)
    - `UserTypeValue` SugaredValue, to manage getattr and setattr while generating IR, plus compiler.cpp changes to make that work.
    - Frontend changes to get `torch.jit.script` to work as a class decorator
    - `ClassDef` node in our AST.
    - primitive ops for object creation, setattr, and getattr, plus alias analysis changes to make mutation safe.
    
    Things that definitely need to get done:
    - Import/export, python_print support
    - String frontend doesn't understand class definitions yet
    - Python interop (using a user-defined type outside TorchScript) is completely broken
    - Static methods (without `self`) don't work
    
    Things that are nice but not essential:
    - Method definition shouldn't matter (right now you can only reference a method that's already been defined)
    - Class definitions can only contain defs, no other expressions are supported.
    
    Things I definitely won't do initially:
    - Polymorphism/inheritance
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17314
    
    Differential Revision: D14194065
    
    Pulled By: suo
    
    fbshipit-source-id: c5434afdb9b39f84b7c85a9fdc2891f8250b5025

--- torch/csrc/jit/passes/dead_code_elimination.cpp
+++ torch/csrc/jit/passes/dead_code_elimination.cpp
@@ -193,7 +193,7 @@ class DeadCodeEliminator {
     if (!aliasDb_) {
       // If we don't have alias information, all mutable ops have unknown
       // effects and can't be considered for elimination.
-      if (!node->kind().is_aten()) {
+      if (!node->kind().is_aten() && !node->kind().is_prim()) {
         return false;
       }
       // onnx export calls EliminateDeadCode but sometimes passes invalid
