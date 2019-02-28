ca91631385a70538926bbbc90be811c46310cbe4
Sebastian Messmer <messmer@fb.com>
2019-02-28 08:23:47 -0800

    V3: (no description)
    Differential Revision: D14071130
    Differential Version: 73747283

--- caffe2/operators/generate_proposals_op.cu
+++ caffe2/operators/generate_proposals_op.cu
@@ -668,6 +668,6 @@ REGISTER_CUDA_OPERATOR(GenerateProposals, GenerateProposalsOp<CUDAContext>);
 } // namespace caffe2
 
 C10_REGISTER_CAFFE2_OPERATOR_CUDA(
-    GenerateProposalsOp,
+    GenerateProposals,
     caffe2::GenerateProposalsOp<caffe2::CUDAContext>
 );
