3c3779411d23bf9895eb8448cb8db0dfa85fa06c
Spandan Tiwari <sptiwari@microsoft.com>
2019-02-27 18:01:43 -0800

    Style fixes to assuage flake8.

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -234,7 +234,7 @@ def _model_to_graph(model, args, f, verbose=False, training=False,
         params = list(_unique_state_dict(model).values())
 
     input_and_param_names = [val.uniqueName() for val in graph.inputs()]
-    param_names = input_and_param_names[len(input_and_param_names)-len(params):]
+    param_names = input_and_param_names[len(input_and_param_names) - len(params):]
     params_dict = dict(zip(param_names, params))
 
     graph = _optimize_graph(graph, operator_export_type)
@@ -299,9 +299,9 @@ def _export(model, args, f, export_params=True, verbose=False, training=False,
         opset_version = _default_onnx_opset_version
     _set_opset_version(opset_version)
     graph, params_dict, torch_out = _model_to_graph(model, args, f, verbose,
-                                               training, input_names,
-                                               output_names, operator_export_type,
-                                               example_outputs, propagate)
+                                                    training, input_names,
+                                                    output_names, operator_export_type,
+                                                    example_outputs, propagate)
 
     # TODO: Don't allocate a in-memory string for the protobuf
     defer_weight_export = export_type is not ExportTypes.PROTOBUF_FILE
