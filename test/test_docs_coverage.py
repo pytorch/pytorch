b7717237fe8e7f08d64c5c99c036061459a93f94
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-02-28 22:29:30 -0500

    simplify

--- test/test_docs_coverage.py
+++ test/test_docs_coverage.py
@@ -10,10 +10,23 @@ path = os.path.dirname(os.path.realpath(__file__))
 rstpath = os.path.join(path, '../docs/source/')
 pypath = os.path.join(path, '../torch/_torch_docs.py')
 r1 = re.compile(r'\.\. autofunction:: (\w*)')
-r2 = re.compile(r'\.\. auto(method|attribute):: (\w*)')
+r2 = re.compile(r'\.\. auto(?:method|attribute):: (\w*)')
 
 
 class TestDocCoverage(unittest.TestCase):
+
+    @staticmethod
+    def parse_rst(filename, regex):
+        filename = os.path.join(rstpath, filename)
+        ret = set()
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            for l in lines:
+                l = l.strip()
+                name = regex.findall(l)
+                if name:
+                    ret.add(name[0])
+        return ret
 
     def test_torch(self):
         # get symbols documented in torch.rst
@@ -23,16 +36,7 @@ class TestDocCoverage(unittest.TestCase):
             'is_tensor', 'compiled_with_cxx11_abi', 'set_rng_state',
             'manual_seed'
         ]
-        everything = set()
-        filename = os.path.join(rstpath, 'torch.rst')
-        with open(filename, 'r') as f:
-            lines = f.readlines()
-            for l in lines:
-                l = l.strip()
-                name = r1.findall(l)
-                if name:
-                    everything.add(name[0])
-        everything -= set(whitelist)
+        everything = self.parse_rst('torch.rst', r1) - set(whitelist)
         # get symbols in functional.py and _torch_docs.py
         whitelist2 = ['product', 'inf', 'math', 'reduce', 'warnings', 'torch', 'annotate']
         everything2 = set()
@@ -63,22 +67,10 @@ class TestDocCoverage(unittest.TestCase):
 
     def test_tensor(self):
         # get symbols documented in tensors.rst
-        whitelist = []
-        everything = set()
-        filename = os.path.join(rstpath, 'tensors.rst')
-        with open(filename, 'r') as f:
-            lines = f.readlines()
-            for l in lines:
-                l = l.strip()
-                name = r2.findall(l)
-                if name:
-                    everything.add(name[0][1])
-        everything -= set(whitelist)
+        everything = self.parse_rst('tensors.rst', r2)
         # get symbols in tensor.py and _tensor_docs.py
         classes = [torch.FloatTensor, torch.LongTensor, torch.ByteTensor]
-        whitelist2 = []
         everything2 = set(x for c in classes for x in dir(c) if not x.startswith('_') and getattr(c, x).__doc__)
-        everything2 -= set(whitelist2)
         # assert they are equal
         for p in everything:
             self.assertIn(p, everything2, 'in tensors.rst but not in python')
