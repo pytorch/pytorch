5fba411d85bca47f0853be994ba1844284dbbf2d
Sebastian Messmer <messmer@fb.com>
2019-03-01 17:26:40 -0800

    V4: (no description)
    Differential Revision: D14270288
    Differential Version: 73956882

--- caffe2/operators/reduction_ops.h
+++ caffe2/operators/reduction_ops.h
@@ -80,12 +80,16 @@ class SumElementsGradientOp : public Operator<Context> {
  public:
   USE_OPERATOR_CONTEXT_FUNCTIONS;
 
-  template <class... Args>
-  explicit SumElementsGradientOp(Args&&... args)
-      : Operator<Context>(std::forward<Args>(args)...),
+  explicit SumElementsGradientOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<Context>(operator_def, ws),
         average_(this->template GetSingleArgument<bool>("average", false)) {}
   explicit SumElementsGradientOp(const OperatorDef& operator_def, Workspace* ws, bool average)
       : Operator<Context>(operator_def, ws), average_(average) {}
+  explicit SumElementsGradientOp(const c10::FunctionSchema& schema, std::vector<c10::IValue> inputs, std::vector<c10::IValue*> outputs)
+      : Operator<Context>(schema, std::move(inputs), std::move(outputs)),
+        average_(this->template GetSingleArgument<bool>("average", false)) {}
+  explicit SumElementsGradientOp(const c10::FunctionSchema& schema, std::vector<c10::IValue> inputs, std::vector<c10::IValue*> outputs, bool average)
+      : Operator<Context>(schema, std::move(inputs), std::move(outputs)), average_(average) {}
   ~SumElementsGradientOp() {}
 
   bool RunOnDevice() override;
