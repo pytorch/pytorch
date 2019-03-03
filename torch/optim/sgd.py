b5cfc9c7a513dbe458459d73f0a32d81af49b069
Reyhane Askari <r.askari.hemmat@gmail.com>
2019-03-03 17:15:38 -0500

    allow negative mom for sgd

--- torch/optim/sgd.py
+++ torch/optim/sgd.py
@@ -52,14 +52,12 @@ class SGD(Optimizer):
                  weight_decay=0, nesterov=False):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if momentum < 0.0:
-            raise ValueError("Invalid momentum value: {}".format(momentum))
         if weight_decay < 0.0:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
 
         defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
                         weight_decay=weight_decay, nesterov=nesterov)
-        if nesterov and (momentum <= 0 or dampening != 0):
+        if nesterov and (momentum == 0 or dampening != 0):
             raise ValueError("Nesterov momentum requires a momentum and zero dampening")
         super(SGD, self).__init__(params, defaults)
 
