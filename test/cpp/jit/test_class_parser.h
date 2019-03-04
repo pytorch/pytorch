760a9d5c5e69e3d51c7246282055f07f9e666e66
suo <suo@fb.com>
2019-03-04 10:48:11 -0800

    V4: (no description)
    Differential Revision: D14291654
    Differential Version: 74151164

--- test/cpp/jit/test_class_parser.h
+++ test/cpp/jit/test_class_parser.h
@@ -24,19 +24,10 @@ void testClassParser() {
   const auto classDef = ClassDef(p.parseClass());
   p.lexer().expect(TK_EOF);
 
-  const auto type = ClassType::create(classDef.name().name(), cu);
-  for (const auto& def : classDef.defs()) {
-    definitions.push_back(def);
-    resolvers.push_back(nativeResolver);
-  }
-  defineMethodsInModule(cu, definitions, resolvers, Self(type));
-
-  const auto classType = ClassType::get("FooTest");
-  ASSERT_TRUE(classType);
-  ASSERT_TRUE(classType->numAttributes() == 1);
-  ASSERT_TRUE(classType->getAttributeSlot("x") == 0);
-  auto method = classType->getMethod("get_x");
-  ASSERT_TRUE(method);
+  ASSERT_EQ(classDef.name().name(), "FooTest");
+  ASSERT_EQ(classDef.defs().size(), 2);
+  ASSERT_EQ(classDef.defs()[0].name().name(), "__init__");
+  ASSERT_EQ(classDef.defs()[1].name().name(), "get_x");
 }
 } // namespace script
 } // namespace jit
