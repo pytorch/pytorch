033f6ae578a27a825f9a0998bcce0e8d47947f2c
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2019-03-11 12:15:41 -0700

    Deprecate torch.pstrf (#17866)
    Summary:
    Changelog:
    - Add deprecation warning to torch.pstrf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17866
    
    Differential Revision: D14405527
    
    Pulled By: soumith
    
    fbshipit-source-id: 73f3b7d61c60eb57e4bffd08112e552ae3e6dfdc

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -3628,45 +3628,6 @@ Example::
     tensor([-0.2018, -0.2962, -0.0821, -1.1831])
 """.format(**single_dim_common))
 
-add_docstr(torch.pstrf, r"""
-pstrf(a, upper=True, out=None) -> (Tensor, Tensor)
-
-Computes the pivoted Cholesky decomposition of a positive semidefinite
-matrix :attr:`a`. returns a namedtuple (u, pivot) of matrice.
-
-If :attr:`upper` is ``True`` or not provided, `u` is upper triangular
-such that :math:`a = p^T u^T u p`, with `p` the permutation given by `pivot`.
-
-If :attr:`upper` is ``False``, `u` is lower triangular such that
-:math:`a = p^T u u^T p`.
-
-Args:
-    a (Tensor): the input 2-D tensor
-    upper (bool, optional): whether to return a upper (default) or lower triangular matrix
-    out (tuple, optional): namedtuple of `u` and `pivot` tensors
-
-Example::
-
-    >>> a = torch.randn(3, 3)
-    >>> a = torch.mm(a, a.t()) # make symmetric positive definite
-    >>> a
-    tensor([[ 3.5405, -0.4577,  0.8342],
-            [-0.4577,  1.8244, -0.1996],
-            [ 0.8342, -0.1996,  3.7493]])
-    >>> u,piv = torch.pstrf(a)
-    >>> u
-    tensor([[ 1.9363,  0.4308, -0.1031],
-            [ 0.0000,  1.8316, -0.2256],
-            [ 0.0000,  0.0000,  1.3277]])
-    >>> piv
-    tensor([ 2,  0,  1], dtype=torch.int32)
-    >>> p = torch.eye(3).index_select(0,piv.long()).index_select(0,piv.long()).t() # make pivot permutation
-    >>> torch.mm(torch.mm(p.t(),torch.mm(u.t(),u)),p) # reconstruct
-    tensor([[ 3.5405, -0.4577,  0.8342],
-            [-0.4577,  1.8244, -0.1996],
-            [ 0.8342, -0.1996,  3.7493]])
-""")
-
 add_docstr(torch.qr,
            r"""
 qr(input, out=None) -> (Tensor, Tensor)
