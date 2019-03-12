b0c4f45d66ae0cd298a39b268c237d01e16d8fb2
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-03-11 23:11:08 -0700

    flake8

--- torch/generator.py
+++ torch/generator.py
@@ -1,21 +1,26 @@
 import torch
 
 
- class Generator(torch._C._GeneratorBase):
+class Generator(torch._C._GeneratorBase):
     r"""Generator(device='cpu', default=False) -> Generator
-     Creates and returns a generator object which manages the state of the algorithm that
+
+    Creates and returns a generator object which manages the state of the algorithm that
     produces pseudo random numbers. Used as a keyword argument in many random tensors in
     :ref:`inplace-random-sampling`. Currently only creation of CPU Generator is supported through
     this API.
-     Arguments:
+
+    Arguments:
         device (:class:`torch.device`, optional): the desired device for the generator.
         default (bool, optional): If using the default CPU/CUDA generator.
-     Returns:
+
+    Returns:
         Generator: An ATen Generator object.
-     Example::
-         >>> g_cpu = torch.Generator()
+
+    Example::
+
+        >>> g_cpu = torch.Generator()
         >>> g_cpu_default = torch.Generator(default=True);
     """
 
-     def __new__(cls, device='cpu', default=False):
+    def __new__(cls, device='cpu', default=False):
         return super(Generator, cls).__new__(cls, device=device, default=default)
