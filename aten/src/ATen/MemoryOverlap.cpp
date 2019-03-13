09e412a75fcb8623abd9d63e6d3bbdf784016599
Richard Zou <zou3519@gmail.com>
2019-03-13 12:56:51 -0700

    Update on "Implement at::has_internal_overlap helper function"

--- aten/src/ATen/MemoryOverlap.cpp
+++ aten/src/ATen/MemoryOverlap.cpp
@@ -1,29 +1,28 @@
 #include <ATen/MemoryOverlap.h>
 #include <c10/core/Layout.h>
-#include <c10/core/Backend.h>
 
 namespace at {
 
 MemOverlap has_internal_overlap(const Tensor& tensor) {
   auto* t = tensor.unsafeGetTensorImpl();
 
-  AT_ASSERT(layout_from_backend(tensorTypeIdToBackend(t->type_id())) == kStrided);
+  AT_ASSERT(tensor.layout() == kStrided);
 
   if (t->is_contiguous()) {
-    return MemOverlap::kNo;
+    return MemOverlap::NO;
   }
 
   auto strides = t->strides();
   if (std::find_if(
         strides.begin(), strides.end(), [](int s) { return s == 0; })) {
-    return MemOverlap::kYes;
+    return MemOverlap::YES;
   }
 
-  return MemOverlap::kTooHard;
+  return MemOverlap::TOO_HARD;
 }
 
 void assert_no_internal_overlap(const Tensor& t, std::string op) {
-  if (has_internal_overlap(t) == MemOverlap::kYes) {
+  if (has_internal_overlap(t) == MemOverlap::YES) {
     AT_ERROR(
         op, ": unsupported operation: more than one element of the written-to "
         "tensor refers to a single memory location. Please clone() the tensor "
