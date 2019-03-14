82f33f1837e421e796f3b6573eff4f6b52c7f152
Edward Z. Yang <ezyang@fb.com>
2019-03-14 13:25:12 -0400

    Update on "Use std::isnan instead of self-comparison."

--- aten/src/ATen/native/NumericUtils.h
+++ aten/src/ATen/native/NumericUtils.h
@@ -3,17 +3,17 @@
 namespace at { namespace native {
 
 template <typename scalar_t>
-bool _isnan(scalar_t val) {
+inline bool _isnan(scalar_t val) {
   return false;
 }
 
 template <>
-bool _isnan(float val) {
+inline bool _isnan(float val) {
   return std::isnan(val);
 }
 
 template <>
-bool _isnan(double val) {
+inline bool _isnan(double val) {
   return std::isnan(val);
 }
 
