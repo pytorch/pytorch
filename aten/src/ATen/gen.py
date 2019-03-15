15baadfb15d08634f4536fb845bebfaf3da1b6b6
Junji Hashimoto <junji.hashimoto@gmail.com>
2019-03-15 11:04:18 +0900

    Increase line-width of Declarations.yaml to avoid insering line-break on schema_string

--- aten/src/ATen/gen.py
+++ aten/src/ATen/gen.py
@@ -249,7 +249,7 @@ def format_yaml(data):
     noalias_dumper.ignore_aliases = lambda self, data: True
     # Support serializing OrderedDict
     noalias_dumper.add_representer(OrderedDict, dict_representer)
-    return yaml.dump(data, default_flow_style=False, Dumper=noalias_dumper)
+    return yaml.dump(data, default_flow_style=False, Dumper=noalias_dumper, width=2048)
 
 
 def generate_storage_type_and_tensor(backend, density, scalar_type, declarations):
