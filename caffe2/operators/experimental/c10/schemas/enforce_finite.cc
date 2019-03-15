5e0c11849c39f076c8f97b245d65aa1bc46692df
Sebastian Messmer <messmer@fb.com>
2019-03-15 16:54:11 -0700

    Specify overload name in function schema (#18037)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18037
    
    The FunctionSchema can now store an overload name and the parser knows how to parse it. Specify like this:
    
        my_func.overload1(arg1: Tensor) -> Tensor
        my_func.overload2(arg1: Tensor, arg2: Tensor) -> Tensor
    
    Reviewed By: zdevito
    
    Differential Revision: D14467497
    
    fbshipit-source-id: 8832b32f07351bb61090357b17b77a6a2fed3650

--- caffe2/operators/experimental/c10/schemas/enforce_finite.cc
+++ caffe2/operators/experimental/c10/schemas/enforce_finite.cc
@@ -7,13 +7,13 @@ using caffe2::CPUContext;
 namespace caffe2 {
 namespace ops {
 // TODO Parse schema string instead of creating FunctionSchema manually
-C10_DEFINE_OP_SCHEMA(EnforceFinite, FunctionSchema(
-    "_c10_experimental::EnforceFinite",
-    (std::vector<c10::Argument>{
-      c10::Argument("input")
-    }), (std::vector<c10::Argument>{
-    })
-));
+C10_DEFINE_OP_SCHEMA(
+    EnforceFinite,
+    FunctionSchema(
+        "_c10_experimental::EnforceFinite",
+        "",
+        (std::vector<c10::Argument>{c10::Argument("input")}),
+        (std::vector<c10::Argument>{})));
 }
 }
 
