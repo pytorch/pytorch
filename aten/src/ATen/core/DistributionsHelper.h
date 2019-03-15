ebb2a0239f2e4428226b56d280a02415a70662c3
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-03-15 14:43:39 -0700

    Revert to previous uniform dist strategy and add logarithm guard

--- aten/src/ATen/core/DistributionsHelper.h
+++ aten/src/ATen/core/DistributionsHelper.h
@@ -24,9 +24,9 @@
 //       the Philox4_32_10 object directly and CPU take CPUGenerator object directly.
 namespace at {
 
-/*
-* Samples a uniform distribution in the range [0,1] of type T
-*/
+/**
+ * Samples a uniform distribution in the range [0,1) of type T
+ */
 template <typename T>
 struct uniform_real_distribution {
 
@@ -43,28 +43,9 @@ struct uniform_real_distribution {
   }
 
   template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, double>::value, double>::type
-  operator()(Philox4_32_10& engine){
-    uint64_t hi = static_cast<uint64_t>(engine()) << 32;
-    uint64_t lo = static_cast<uint64_t>(engine());
-    uint64_t random = hi | lo;
-    double x = static_cast<double>(random) / static_cast<double>(std::numeric_limits<uint64_t>::max());
-    return (x * (b - a) + a);
-
-  }
-
-  template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, float>::value
-                                          || std::is_same<U, at::Half>::value, float>::type
-  operator()(Philox4_32_10& engine){
-    float x = static_cast<float>(engine()) / static_cast<float>(std::numeric_limits<uint32_t>::max());
-    return (x * (b - a) + a);
-  }
-
-  template<typename U = T> 
   C10_HOST inline typename std::enable_if<std::is_same<U, double>::value, double>::type
   operator()(at::CPUGenerator* generator){
-    double x = static_cast<double>(generator->random64()) / static_cast<double>(std::numeric_limits<uint64_t>::max());
+    double x = (generator->random64() & ((1ULL << 53) - 1)) * ::ldexp(1.0, -53);
     return (x * (b - a) + a);
 
   }
@@ -73,7 +54,7 @@ struct uniform_real_distribution {
   C10_HOST inline typename std::enable_if<std::is_same<U, float>::value
                                           || std::is_same<U, at::Half>::value, float>::type
   operator()(at::CPUGenerator* generator){
-    float x = static_cast<float>(generator->random()) / static_cast<float>(std::numeric_limits<uint32_t>::max());
+    float x = (generator->random() & ((1 << 24) - 1)) * ::ldexp(1.0, -24);
     return (x * (b - a) + a);
   }
 
@@ -82,11 +63,11 @@ struct uniform_real_distribution {
     T b; 
 };
 
-/*
-* Samples a normal distribution using the Box-Muller method
-* Takes mean and standard deviation as inputs
-* Outputs two samples at a time
-*/
+/**
+ * Samples a normal distribution using the Box-Muller method
+ * Takes mean and standard deviation as inputs
+ * Outputs two samples at a time
+ */
 template <typename T>
 struct normal_distribution {
 
@@ -101,40 +82,17 @@ struct normal_distribution {
   }
 
   template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, double>::value, DOUBLE2>::type
-  operator()(Philox4_32_10& engine){
-    uniform_real_distribution<double> uniform(0.0, 1.0);
-    DOUBLE2 result;
-    double u2 = uniform(engine);
-    double u1 = uniform(engine);
-    double r = ::sqrt(-2.0 * ::log(1.0-u1));
-    result[0] = r * ::cos(2.0 * M_PI * u2) * stdv + mean;
-    result[1] = r * ::sin(2.0 * M_PI * u2) * stdv + mean;
-    return result;
-  }
-
-  template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, float>::value
-                                          || std::is_same<U, at::Half>::value, FLOAT2>::type
-  operator()(Philox4_32_10& engine){
-    uniform_real_distribution<float> uniform(0.0, 1.0);
-    FLOAT2 result;
-    float u2 = uniform(engine);
-    float u1 = uniform(engine);
-    float r = ::sqrt(-2.0 * ::log(1.0-u1));
-    result[0] = r * ::cos(2.0 * M_PI * u2) * stdv + mean;
-    result[1] = r * ::sin(2.0 * M_PI * u2) * stdv + mean;
-    return result;
-  }
-
-  template<typename U = T> 
   C10_HOST inline typename std::enable_if<std::is_same<U, double>::value, DOUBLE2>::type
   operator()(at::CPUGenerator* generator){
     uniform_real_distribution<double> uniform(0.0, 1.0);
     DOUBLE2 result;
     double u2 = uniform(generator);
     double u1 = uniform(generator);
-    double r = ::sqrt(-2.0 * ::log(1.0-u1));
+    // extra pre-caution to make sure log never gets zero
+    if (u1 == 0.0) {
+      u1 = std::numeric_limits<double>::min();
+    }
+    double r = ::sqrt(-2.0 * ::log(u1));
     result[0] = r * ::cos(2.0 * M_PI * u2) * stdv + mean;
     result[1] = r * ::sin(2.0 * M_PI * u2) * stdv + mean;
     return result;
@@ -148,7 +106,11 @@ struct normal_distribution {
     FLOAT2 result;
     float u2 = uniform(generator);
     float u1 = uniform(generator);
-    float r = ::sqrt(-2.0 * ::log(1.0-u1));
+    // extra pre-caution to make sure log never gets zero
+    if (u1 == 0.0f) {
+      u1 = std::numeric_limits<float>::min();
+    }
+    float r = ::sqrt(-2.0 * ::log(u1));
     result[0] = r * ::cos(2.0 * M_PI * u2) * stdv + mean;
     result[1] = r * ::sin(2.0 * M_PI * u2) * stdv + mean;
     return result;
@@ -159,9 +121,9 @@ struct normal_distribution {
     T stdv;
 };
 
-/*
-* Samples a bernoulli distribution given a probability input
-*/
+/**
+ * Samples a bernoulli distribution given a probability input
+ */
 template <typename T>
 struct bernoulli_distribution {
 
@@ -174,23 +136,18 @@ struct bernoulli_distribution {
     p = p_in;
   }
 
-  C10_DEVICE inline T operator()(Philox4_32_10& engine) { 
-    uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(uniform(engine) <= p);
-  }
-
   C10_HOST inline T operator()(at::CPUGenerator* generator) { 
     uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(uniform(generator) <= p);
+    return uniform(generator) <= p;
   }
 
   private:
     T p;
 };
 
-/*
-* Samples a geometric distribution given a probability input
-*/
+/**
+ * Samples a geometric distribution given a probability input
+ */
 template <typename T>
 struct geometric_distribution {
 
@@ -203,23 +160,23 @@ struct geometric_distribution {
     p = p_in;
   }
 
-  C10_DEVICE inline int operator()(Philox4_32_10& engine) {
-    uniform_real_distribution<T> uniform(0.0, 1.0);
-    return((int)(::log(1-uniform(engine)) / ::log(p)) + 1);
-  }
-
   C10_HOST inline int operator()(at::CPUGenerator* generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
-    return((int)(::log(1-uniform(generator)) / ::log(p)) + 1);
+    auto sample = uniform(generator);
+    // extra pre-caution to make sure log never gets zero
+    if (sample == static_cast<T>(0.0)) {
+      sample = std::numeric_limits<T>::min();
+    }
+    return static_cast<int>(::log(sample) / ::log(p)) + 1;
   }
 
   private:
     T p;
 };
 
-/*
-* Samples an exponential distribution given a lambda input
-*/
+/**
+ * Samples an exponential distribution given a lambda input
+ */
 template <typename T>
 struct exponential_distribution {
 
@@ -227,23 +184,23 @@ struct exponential_distribution {
     lambda = lambda_in;
   }
 
-  C10_DEVICE inline T operator()(Philox4_32_10& engine) {
-    uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(-1. / lambda * ::log(1-uniform(engine)));
-  }
-
   C10_HOST inline T operator()(at::CPUGenerator* generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(-1. / lambda * ::log(1-uniform(generator)));
+    auto sample = uniform(generator);
+    // extra pre-caution to make sure log never gets zero
+    if (sample == static_cast<T>(0.0)) {
+      sample = std::numeric_limits<T>::min();
+    }
+    return -1. / lambda * ::log(sample);
   }
 
   private:
     T lambda;
 };
 
-/*
-* Samples a cauchy distribution given median and sigma as inputs
-*/
+/**
+ * Samples a cauchy distribution given median and sigma as inputs
+ */
 template <typename T>
 struct cauchy_distribution {
 
@@ -252,14 +209,9 @@ struct cauchy_distribution {
     sigma = sigma_in;
   }
 
-  C10_DEVICE inline T operator()(Philox4_32_10& engine) {
-    uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(median + sigma * ::tan(M_PI*(uniform(engine)-0.5)));
-  }
-
   C10_HOST inline T operator()(at::CPUGenerator* generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
-    return(median + sigma * ::tan(M_PI*(uniform(generator)-0.5)));
+    return median + sigma * ::tan(M_PI*(uniform(generator)-0.5));
   }
 
   private:
@@ -267,11 +219,11 @@ struct cauchy_distribution {
     T sigma;
 };
 
-/*
-* Samples a lognormal distribution
-* Takes mean and standard deviation as inputs
-* Outputs two samples at a time
-*/
+/**
+ * Samples a lognormal distribution
+ * Takes mean and standard deviation as inputs
+ * Outputs two samples at a time
+ */
 template <typename T>
 struct lognormal_distribution {
 
@@ -283,29 +235,6 @@ struct lognormal_distribution {
     #endif
     mean = mean_in;
     stdv = stdv_in;
-  }
-
-  template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, double>::value, DOUBLE2>::type
-  operator()(Philox4_32_10& engine){
-    normal_distribution<double> normal(mean, stdv);
-    DOUBLE2 result;
-    DOUBLE2 normal_vals = normal(engine);
-    result[0] = ::exp(normal_vals[0]);
-    result[1] = ::exp(normal_vals[1]);
-    return result;
-  }
-
-  template<typename U = T> 
-  C10_DEVICE inline typename std::enable_if<std::is_same<U, float>::value
-                                          || std::is_same<U, at::Half>::value, FLOAT2>::type
-  operator()(Philox4_32_10& engine){
-    normal_distribution<float> normal(mean, stdv);
-    FLOAT2 result;
-    FLOAT2 normal_vals = normal(engine);
-    result[0] = ::exp(normal_vals[0]);
-    result[1] = ::exp(normal_vals[1]);
-    return result;
   }
 
   template<typename U = T> 
