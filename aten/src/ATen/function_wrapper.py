68b116453ee23d14a3ba82b82332cb074fa77cfc
Gregory Chanan <gchanan@fb.com>
2019-03-15 12:14:20 -0700

    properly device_guard IndexTensor and BoolTensor.
    Currently IndexTensor and BoolTensors do not have device_guards applied to them.
    This is bad in the case where the only tensor(s) are IndexTensors or BoolTensors, because no device guard is present.
    
    The only case this currently happens is with one_hot which ends up not mattering because of the way the implementation is written.  But I wanted to make sure we are covered here.

--- aten/src/ATen/function_wrapper.py
+++ aten/src/ATen/function_wrapper.py
@@ -731,14 +731,19 @@ def create_generic(top_env, declarations):
     def find_dispatch_tensor(formals):
         # type: (List[AtFormal]) -> Optional[str]
         # dispatch to self if it's a parameter
+        def is_any_tensor_type(formal):
+            return (formal['dynamic_type'] == 'Tensor' or formal['dynamic_type'] == 'BoolTensor'
+                    or formal['dynamic_type'] == 'IndexTensor')
+
         for formal in formals:
-            if formal['name'] == 'self' and formal['dynamic_type'] == 'Tensor' and not formal.get('is_nullable', False):
+            if formal['name'] == 'self' and is_any_tensor_type(formal) and not formal.get('is_nullable', False):
                 return formal['name']
         # otherwise dispatch to the first Tensor or TensorList
         for formal in formals:
-            if 'TensorList' == formal['dynamic_type'] or formal['dynamic_type'] == 'Tensor' and \
+            if 'TensorList' == formal['dynamic_type'] or is_any_tensor_type(formal) and \
                not formal.get('is_nullable', False):
                 return formal['name']
+
         return None
 
     def format_formal(f):
