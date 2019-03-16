8798ed4fd5b8b4acb4d345a2e0d4fd43143846c3
Guanheng Zhang <zhangguanheng@fb.com>
2019-03-16 14:22:17 -0400

    Add case 1

--- aten/src/TH/generic/THBlas.cpp
+++ aten/src/TH/generic/THBlas.cpp
@@ -400,7 +400,7 @@ void THBlas_(gemm)(
       // #pragma omp parallel for
       int64_t l;
       // #pragma omp parallel for if(k > TH_OMP_OVERHEAD_THRESHOLD) private(l)
-      // #pragma omp parallel for private(l)
+      #pragma omp parallel for private(l)
       for (l = 0; l < k; l++) {
         for (int64_t j = 0; j < n; j++) {
           scalar_t val = b[l + j * ldb] * alpha;
