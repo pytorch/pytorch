779e8a3a8b6a558f3f6b4bd2eca7344786796c8a
Guanheng Zhang <zhangguanheng@fb.com>
2019-03-18 09:32:41 -0400

    Move omp to the outermost for loop in case 3.

--- aten/src/TH/generic/THBlas.cpp
+++ aten/src/TH/generic/THBlas.cpp
@@ -463,23 +463,25 @@ void THBlas_(gemm)(
       // #pragma omp parallel for if(k > TH_OMP_OVERHEAD_THRESHOLD) private(l)
       // #pragma omp parallel for private(l)
 //       #pragma omp parallel for shared(c, a, b) private(i, j, l)
+      #pragma omp parallel for shared(c, a, b, alpha, lda, ldb, ldc, m, j, l) private(i)
       for (l = 0; l < k; l++) {
         for (j = 0; j < n; j++) {
-//           int64_t i_m = m / 4;
-//           for (int64_t i_i = 0; i_i < i_m; i_i++) {
-//             c[j * ldc + i_i * 4 + 0] += a[i_i * 4 + 0 + l * lda] //
-//             					* b[j + l * ldb] * alpha;
-//             c[j * ldc + i_i * 4 + 1] += a[i_i * 4 + 1 + l * lda] //
-//                         		* b[j + l * ldb] * alpha;
-//             c[j * ldc + i_i * 4 + 2] += a[i_i * 4 + 2 + l * lda] //
-//                         		* b[j + l * ldb] * alpha;
-//             c[j * ldc + i_i * 4 + 3] += a[i_i * 4 + 3 + l * lda] //
-//                         		* b[j + l * ldb] * alpha;
-//           }
-//           int64_t i = i_m * 4;
-//           for (; i < m; i++)
-          #pragma omp parallel for shared(c, a, b, alpha, lda, ldb, ldc, m, j, l) private(i)
-          for (i = 0; i < m; i++)
+          int64_t i_m = m / 4;
+          for (int64_t i_i = 0; i_i < i_m; i_i++) {
+            c[j * ldc + i_i * 4 + 0] += a[i_i * 4 + 0 + l * lda] //
+            					* b[j + l * ldb] * alpha;
+            c[j * ldc + i_i * 4 + 1] += a[i_i * 4 + 1 + l * lda] //
+                        		* b[j + l * ldb] * alpha;
+            c[j * ldc + i_i * 4 + 2] += a[i_i * 4 + 2 + l * lda] //
+                        		* b[j + l * ldb] * alpha;
+            c[j * ldc + i_i * 4 + 3] += a[i_i * 4 + 3 + l * lda] //
+                        		* b[j + l * ldb] * alpha;
+          }
+          int64_t i = i_m * 4;
+          for (; i < m; i++)
+
+//           #pragma omp parallel for shared(c, a, b, alpha, lda, ldb, ldc, m, j, l) private(i)
+//           for (i = 0; i < m; i++)
             c[j * ldc + i] += a[i + l * lda] * b[j + l * ldb] * alpha;
         }
       }
