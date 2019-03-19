3d44305e9d3f0ecb9eb126dbf0125a0cdc513b2d
David Riazati <davidriazati@fb.com>
2019-03-18 18:15:17 -0700

    Attribute serialization (#17423)
    Summary:
    Allows serialization/loading of attributes (`IValue`s of any type).
    * metadata (attribute name, type) is stored in the `model.json`
    * The binary format is a subset of the `pickle` module that supports the operations necessary for `IValue`s
        * Attributes are serialized in the order they are defined on a module to a list in a single `attributes` file, with submodule attributes coming first. This order directly matches the order attributes are listed in `model.json`
        * This can be inspected in Python with `pickle.load()` or with `pickletools` (PyTorch need not be installed for this to work)
            * A class is used to store a tensor's index into the tensor table of the model, so to unpickle the file you have to use a custom Unpickler:
            ```python
            class TensorID(object):
                def __setstate__(self, id):
                    self.id = id
    
            class JitUnpickler(pickle.Unpickler):
                def find_class(self, module, name):
                    if module == '__main__' and name == 'TensorID':
                        return TensorID
    
            JitUnpickler(open("my_model/attributes.pkl", "rb")).load()
            ```
        * pickle format: https://svn.python.org/projects/python/trunk/Lib/pickletools.py
    * It currently does not support/guarantee that anything saved out with `pickle` (i.e. if you edit `attributes` with `pickle` directly) instead of our tools will be imported correctly
    
    Also will fix #17683 and fix #16367
    
    Followup Work:
    * document format / choice of pickle: #17951
    * create an example
    * list specializations
    * int size specializations, large binputs
    * do a first pass over attributes to output only necessary `BINPUT` ops
    * attribute reassignment (e.g `self.my_attribute = new_value`)
    * `tensor.save("some_checkpoint.pkl")` support with tensors embedded in Pickle file
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17423
    
    Differential Revision: D14470965
    
    Pulled By: driazati
    
    fbshipit-source-id: 6a21a9939efdbe59b4bc57fd31d6d630bab5297e

--- torch/csrc/jit/import_source.cpp
+++ torch/csrc/jit/import_source.cpp
@@ -25,6 +25,9 @@ struct ModuleAccessorValue : public SugaredValue {
       return std::make_shared<SimpleValue>(m.get_or_add_parameter(v->slot()));
     } else if (NamedIValue* v = module->find_buffer(field)) {
       return std::make_shared<SimpleValue>(m.get_or_add_parameter(v->slot()));
+    } else if (script::NamedIValue* v = module->find_attribute(field)) {
+      return std::make_shared<script::SimpleValue>(
+          m.get_or_add_attribute(v->type, v->slot()));
     } else if (Method* m = module->find_method(field)) {
       return std::make_shared<MethodValue>(shared_from_this(), *m);
     } else {
