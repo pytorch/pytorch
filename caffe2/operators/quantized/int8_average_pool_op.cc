pytorch/pytorch

commit b5198d29d6c74e1d14fbdaf98910ed91c7ffa437

Open-source Caffe2 Int8 ops (#13065)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/13065

- Open-source Caffe2 Int8 (quantized) operators
- Update CMakeLists to have a dependency on QNNPACK, gemmlowp, and neon2sse

Differential Revision: D10524381

fbshipit-source-id: 16f41fdd55153368784de5235edcb5df86a6b4e7

caffe2/operators/quantized/int8_average_pool_op.cc
@@ -1,0 +1,65 @@
+#include "caffe2/operators/quantized/int8_average_pool_op.h"
+
+namespace caffe2 {
+
+REGISTER_CPU_OPERATOR(
+    Int8AveragePool,
+    int8::Int8AveragePoolOp<int8::Activation::NONE>);
+REGISTER_CPU_OPERATOR(
+    Int8AveragePoolRelu,
+    int8::Int8AveragePoolOp<int8::Activation::RELU>);
+
+const char kAveragePoolDoc_int8[] = R"DOC(
+consumes an input blob X and applies average pooling across the
+the blob according to kernel sizes, stride sizes, and pad lengths defined by the
+ConvPoolOpBase operator. Average pooling consisting of averaging all values of a
+subset of the input tensor according to the kernel size and downsampling the
+data into the output blob Y for further processing.
+)DOC";
+
+std::function<void(OpSchema&)> AveragePoolDocGenerator(
+    const char* dim,
+    bool relu_fused = false) {
+  auto suffix = relu_fused ? " Output will go through rectified linear "
+                             "function, where y = max(0, x)."
+                           : "";
+  return [=](OpSchema& schema) {
+    string doc = "AveragePool{dim} {pool_doc}";
+    c10::ReplaceAll(doc, "{dim}", dim);
+    c10::ReplaceAll(doc, "{pool_doc}", kAveragePoolDoc_int8);
+    schema.SetDoc(doc);
+    string output_doc =
+        "Output data tensor from average pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes.{suffix}";
+    c10::ReplaceAll(output_doc, "{suffix}", suffix);
+    schema.Input(
+        0,
+        "X",
+        "Input data tensor from the previous operator; dimensions depend on "
+        "whether the NCHW or NHWC operators are being used. For example, in "
+        "the former, the input has size (N x C x H x W), where N is the batch "
+        "size, C is the number of channels, and H and W are the height and the "
+        "width of the data. The corresponding permutation of dimensions is "
+        "used in the latter case.");
+    schema.Output(0, "Y", output_doc.c_str());
+  };
+}
+
+OPERATOR_SCHEMA(Int8AveragePool)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForPool)
+    .FillUsing(AveragePoolDocGenerator(""));
+
+OPERATOR_SCHEMA(Int8AveragePoolRelu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForPool)
+    .FillUsing(AveragePoolDocGenerator("", true));
+
+} // namespace caffe2

caffe2/operators/quantized/int8_conv_op.cc
@@ -1,0 +1,81 @@
+#include "caffe2/operators/quantized/int8_conv_op.h"
+
+namespace caffe2 {
+
+REGISTER_CPU_OPERATOR(Int8Conv, int8::Int8ConvOp<int8::Activation::NONE>);
+REGISTER_CPU_OPERATOR(Int8ConvRelu, int8::Int8ConvOp<int8::Activation::RELU>);
+
+const char kConvDoc_int8[] = R"DOC(
+[Only NHWC order is supported now]Note that other parameters, such as the stride and
+kernel size, or the pads' sizes in each direction are not necessary for input
+because they are provided by the ConvPoolOpBase operator. Various dimension
+checks are done implicitly, and the sizes are specified in the Input docs for
+this operator. As is expected, the filter is convolved with a subset of the
+image and the bias is added; this is done throughout the image data and the
+output is computed. As a side note on the implementation layout:
+conv_op_impl.h is the templated implementation of the conv_op.h file, which is
+why they are separate files.
+)DOC";
+
+std::function<void(OpSchema&)> ConvDocGenerator(
+    const char* dim,
+    bool relu_fused = false) {
+  auto suffix = relu_fused ? " Output will go through rectified linear "
+                             "function, where y = max(0, x)."
+                           : "";
+  return [=](OpSchema& schema) {
+    string doc = R"DOC(
+The convolution operator consumes an input vector, a {dim}filter blob
+and a bias blob and computes the output. {conv_doc})DOC";
+    c10::ReplaceAll(doc, "{dim}", dim);
+    c10::ReplaceAll(doc, "{conv_doc}", kConvDoc_int8);
+    schema.SetDoc(doc);
+    string output_doc =
+        "Output data blob that contains the result of the "
+        "convolution. The output dimensions are functions of the kernel size, "
+        "stride size, and pad lengths.{suffix}";
+    c10::ReplaceAll(output_doc, "{suffix}", suffix);
+    schema.Input(
+        0,
+        "X",
+        "Input data blob from previous layer; has size (N x C x H x W), "
+        "where N is the batch size, C is the number of channels, "
+        "and H and W are the height and width. Note that this is for the NCHW "
+        "usage. On the other hand, the NHWC Op has a different set of "
+        "dimension constraints. ");
+    schema.Input(
+        1,
+        "filter",
+        "The filter blob that will be used in the "
+        "convolutions; has size (M x C x kH x kW), where C is the number of "
+        "channels, and kH and kW are the height and width of the kernel.");
+    schema.Input(
+        2,
+        "bias",
+        "The 1D bias blob that is added through the "
+        "convolution; has size (M).");
+    schema.Output(0, "Y", output_doc.c_str());
+  };
+}
+
+OPERATOR_SCHEMA(Int8Conv)
+    .NumInputs(2, 3)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForConv)
+    .CostInferenceFunction(OpSchema::CostInferenceFunctionType(
+        ConvPoolOpBase<CPUContext>::CostInferenceForConv))
+    .FillUsing(ConvDocGenerator(""));
+
+OPERATOR_SCHEMA(Int8ConvRelu)
+    .NumInputs(2, 3)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForConv)
+    .CostInferenceFunction(OpSchema::CostInferenceFunctionType(
+        ConvPoolOpBase<CPUContext>::CostInferenceForConv))
+    .FillUsing(ConvDocGenerator("", true));
+
+} // namespace caffe2

caffe2/operators/quantized/int8_max_pool_op.cc
@@ -1,0 +1,63 @@
+#include "caffe2/operators/quantized/int8_max_pool_op.h"
+
+namespace caffe2 {
+
+REGISTER_CPU_OPERATOR(Int8MaxPool, int8::Int8MaxPoolOp<int8::Activation::NONE>);
+REGISTER_CPU_OPERATOR(
+    Int8MaxPoolRelu,
+    int8::Int8MaxPoolOp<int8::Activation::RELU>);
+
+const char kMaxPoolDoc_int8[] = R"DOC(
+consumes an input blob X and applies max pooling across the
+the blob according to kernel sizes, stride sizes, and pad lengths defined by the
+ConvPoolOpBase operator. Max pooling consisting of taking the maximum value of a
+subset of the input tensor according to the kernel size and downsampling the
+data into the output blob Y for further processing.
+)DOC";
+
+std::function<void(OpSchema&)> MaxPoolDocGenerator(
+    const char* dim,
+    bool relu_fused = false) {
+  auto suffix = relu_fused ? " Output will go through rectified linear "
+                             "function, where y = max(0, x)."
+                           : "";
+  return [=](OpSchema& schema) {
+    string doc = "MaxPool{dim} {pool_doc}";
+    c10::ReplaceAll(doc, "{dim}", dim);
+    c10::ReplaceAll(doc, "{pool_doc}", kMaxPoolDoc_int8);
+    string output_doc =
+        "Output data tensor from max pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes.{suffix}";
+    c10::ReplaceAll(output_doc, "{suffix}", suffix);
+    schema.SetDoc(doc);
+    schema.Input(
+        0,
+        "X",
+        "Input data tensor from the previous operator; dimensions depend on "
+        "whether the NCHW or NHWC operators are being used. For example, in "
+        "the former, the input has size (N x C x H x W), where N is the batch "
+        "size, C is the number of channels, and H and W are the height and the "
+        "width of the data. The corresponding permutation of dimensions is "
+        "used in the latter case.");
+    schema.Output(0, "Y", output_doc.c_str());
+  };
+}
+
+OPERATOR_SCHEMA(Int8MaxPool)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForPool)
+    .FillUsing(MaxPoolDocGenerator(""));
+
+OPERATOR_SCHEMA(Int8MaxPoolRelu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .TensorInferenceFunction(ConvPoolOpBase<CPUContext>::TensorInferenceForPool)
+    .FillUsing(MaxPoolDocGenerator("", true));
+
+} // namespace caffe2

caffe2/operators/quantized/init_qnnpack.cc
@@ -1,0 +1,17 @@
+#include <mutex>
+
+#include <qnnpack.h>
+
+#include "caffe2/core/logging.h"
+
+namespace caffe2 {
+
+void initQNNPACK() {
+  static std::once_flag once;
+  static enum qnnp_status qnnpackStatus = qnnp_status_uninitialized;
+  std::call_once(once, []() { qnnpackStatus = qnnp_initialize(); });
+  CAFFE_ENFORCE(
+      qnnpackStatus == qnnp_status_success, "failed to initialize QNNPACK");
+}
+
+} // namespace caffe2

caffe2/operators/quantized/int8_add_op.cc
@@ -1,0 +1,68 @@
+#include <climits>
+
+#include "caffe2/operators/quantized/int8_add_op.h"
+
+namespace caffe2 {
+
+REGISTER_CPU_OPERATOR(Int8Add, int8::Int8AddOp<int8::Activation::NONE>);
+REGISTER_CPU_OPERATOR(Int8AddRelu, int8::Int8AddOp<int8::Activation::RELU>);
+
+REGISTER_CPU_OPERATOR(Int8Sum, int8::Int8AddOp<int8::Activation::NONE>);
+REGISTER_CPU_OPERATOR(Int8SumRelu, int8::Int8AddOp<int8::Activation::RELU>);
+
+OPERATOR_SCHEMA(Int8Add)
+    .NumInputs(2)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}, {1, 0}})
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .SetDoc(R"DOC(
+    Performs element-wise binary Add (with no broadcast support).
+)DOC")
+    .Input(
+        0,
+        "A",
+        "First operand, should share the type with the second operand.")
+    .Input(1, "B", "Second operand. It should be of the same size as A.")
+    .Output(0, "C", "Result, has same dimensions and type as A");
+
+OPERATOR_SCHEMA(Int8AddRelu)
+    .NumInputs(2)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}, {1, 0}})
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset")
+    .SetDoc(R"DOC(
+    Performs element-wise binary Add (with no broadcast support). "
+    "Output will go through rectified linear "
+    "function, where y = max(0, x).
+)DOC")
+    .Input(
+        0,
+        "A",
+        "First operand, should share the type with the second operand.")
+    .Input(1, "B", "Second operand. It should be of the same size as A.")
+    .Output(0, "C", "Result, has same dimensions and type as A");
+
+/*
+ * These ops are defined as alias of Int8Add/Int8AddRelu for compatibility
+ * with current production models. In the future these ops will be changed
+ * to an equivalent of Sum op, which does reduction of a single argument.
+ * We deliberately omit schema for Int8Sum/Int8SumRelu so they can
+ * temporary use either legacy or the new semantics depending on the engine.
+ */
+OPERATOR_SCHEMA(Int8Sum)
+    .NumInputs(1, std::numeric_limits<int>::max())
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}, {1, 0}})
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset");
+
+OPERATOR_SCHEMA(Int8SumRelu)
+    .NumInputs(1, std::numeric_limits<int>::max())
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}, {1, 0}})
+    .Arg("Y_scale", "Output tensor quantization scale")
+    .Arg("Y_zero_point", "Output tensor quantization offset");
+
+} // namespace caffe2

caffe2/operators/quantized/int8_add_op.h
@@ -1,0 +1,222 @@
+#ifndef CAFFE2_OPERATORS_INT8_ADD_OP_H_
+#define CAFFE2_OPERATORS_INT8_ADD_OP_H_
+
+#include "caffe2/core/context.h"
+#include "caffe2/core/operator.h"
+#include "caffe2/core/tensor_int8.h"
+#include "caffe2/operators/quantized/int8_utils.h"
+
+namespace caffe2 {
+
+namespace int8 {
+
+namespace {
+
+/*
+ * Implementation based on TensorFlow Lite kernels:
+ * - Repo: https://github.com/tensorflow/tensorflow
+ * - Path: tensorflow/contrib/lite/kernels/internal/optimized/optimized_ops.h
+ * - Hash: d4ad9c73969c45d1a224ebfc43eb645b9860216b
+ */
+
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+constexpr size_t kAddLeftShift = 20;
+
+void Int8Add(
+    const uint8_t* X0_data,
+    size_t N,
+    size_t D,
+    int32_t X0_offset,
+    int32_t X0_multiplier,
+    int X0_shift,
+    const uint8_t* X1_data,
+    int32_t X1_offset,
+    int32_t X1_multiplier,
+    int X1_shift,
+    int32_t Y_offset,
+    int32_t Y_multiplier,
+    int Y_shift,
+    uint8_t* Y_data,
+    uint8_t Y_activation_min,
+    uint8_t Y_activation_max,
+    C2GEMMContext* gemm_context) {
+  CHECK_GT(X0_offset, -256);
+  CHECK_GT(X1_offset, -256);
+  CHECK_LT(X0_offset, 256);
+  CHECK_LT(X1_offset, 256);
+  static_assert(kAddLeftShift > 5, "");
+  static_assert(kAddLeftShift <= 20, "");
+
+  auto f = [&](int, size_t n) {
+    size_t d = 0;
+
+#ifdef INT8_NEON_SIMD
+    constexpr size_t kIntermediateAddLeftShift = 4;
+    const auto X0_offset_val =
+        vshlq_n_s16(vdupq_n_s16(X0_offset), kIntermediateAddLeftShift);
+    const auto X1_offset_val =
+        vshlq_n_s16(vdupq_n_s16(X1_offset), kIntermediateAddLeftShift);
+    const auto X0_shift_dup = vdupq_n_s32(-X0_shift);
+    const auto X1_shift_dup = vdupq_n_s32(-X1_shift);
+    const auto DUnroll = (D / 8) * 8;
+
+    for (; d < DUnroll; d += 8) {
+      const auto X0_val_original = vld1_u8(X0_data + n * D + d);
+      const auto X1_val_original = vld1_u8(X1_data + n * D + d);
+
+      // Load input
+      // Widen to int16
+      // Add int16 offset.
+      // Widen to int32
+      // Shift right by 20.
+      // Alternatively, we can widening shift X by 4, shifty X0_offset by 4,
+      // add, then shift by 16. Safe as X << 5 + X_offset << 5 can't overflow
+      // uint16, as X ~ 8 bit, X_offset ~ 10 bit, so 15 bits total from X +
+      // X_offset
+      const auto X0_val_s16 = vreinterpretq_s16_u16(
+          vshll_n_u8(X0_val_original, kIntermediateAddLeftShift));
+      const auto X1_val_s16 = vreinterpretq_s16_u16(
+          vshll_n_u8(X1_val_original, kIntermediateAddLeftShift));
+      const auto X0_val = vaddq_s16(X0_val_s16, X0_offset_val);
+      const auto X1_val = vaddq_s16(X1_val_s16, X1_offset_val);
+      const auto X0_val_high = vget_high_s16(X0_val);
+      const auto X0_val_low = vget_low_s16(X0_val);
+      const auto X1_val_high = vget_high_s16(X1_val);
+      const auto X1_val_low = vget_low_s16(X1_val);
+      auto x11 =
+          vshll_n_s16(X0_val_low, kAddLeftShift - kIntermediateAddLeftShift);
+      auto x12 =
+          vshll_n_s16(X0_val_high, kAddLeftShift - kIntermediateAddLeftShift);
+      auto x21 =
+          vshll_n_s16(X1_val_low, kAddLeftShift - kIntermediateAddLeftShift);
+      auto x22 =
+          vshll_n_s16(X1_val_high, kAddLeftShift - kIntermediateAddLeftShift);
+      x11 = vqrdmulhq_n_s32(x11, X0_multiplier);
+      x12 = vqrdmulhq_n_s32(x12, X0_multiplier);
+      x21 = vqrdmulhq_n_s32(x21, X1_multiplier);
+      x22 = vqrdmulhq_n_s32(x22, X1_multiplier);
+      x11 = vshlq_s32(x11, X0_shift_dup);
+      x12 = vshlq_s32(x12, X0_shift_dup);
+      x21 = vshlq_s32(x21, X1_shift_dup);
+      x22 = vshlq_s32(x22, X1_shift_dup);
+      auto s1 = vaddq_s32(x11, x21);
+      auto s2 = vaddq_s32(x12, x22);
+      s1 = vqrdmulhq_n_s32(s1, Y_multiplier);
+      s2 = vqrdmulhq_n_s32(s2, Y_multiplier);
+      using gemmlowp::RoundingDivideByPOT;
+      s1 = RoundingDivideByPOT(s1, Y_shift);
+      s2 = RoundingDivideByPOT(s2, Y_shift);
+      const auto s1_narrowed = vmovn_s32(s1);
+      const auto s2_narrowed = vmovn_s32(s2);
+      const auto s = vaddq_s16(
+          vcombine_s16(s1_narrowed, s2_narrowed), vdupq_n_s16(Y_offset));
+      auto ss = vqmovun_s16(s);
+      ss = vmin_u8(ss, vdup_n_u8(Y_activation_max));
+      ss = vmax_u8(ss, vdup_n_u8(Y_activation_min));
+      vst1_u8(Y_data + n * D + d, ss);
+    }
+#endif // NEON
+
+    for (; d < D; d++) {
+      const int32_t X0_val = X0_offset + X0_data[n * D + d];
+      const int32_t X1_val = X1_offset + X1_data[n * D + d];
+      const int32_t shifted_X0_val = X0_val * (1 << kAddLeftShift);
+      const int32_t shifted_X1_val = X1_val * (1 << kAddLeftShift);
+      const int32_t scaled_X0_val = MultiplyByQuantizedMultiplierSmallerThanOne(
+          shifted_X0_val, X0_multiplier, X0_shift);
+      const int32_t scaled_X1_val = MultiplyByQuantizedMultiplierSmallerThanOne(
+          shifted_X1_val, X1_multiplier, X1_shift);
+      const int32_t raw_sum = scaled_X0_val + scaled_X1_val;
+      const int32_t raw_Y = MultiplyByQuantizedMultiplierSmallerThanOne(
+                                raw_sum, Y_multiplier, Y_shift) +
+          Y_offset;
+      const int32_t clamped_Y = std::min<int32_t>(
+          Y_activation_max, std::max<int32_t>(Y_activation_min, raw_Y));
+      Y_data[n * D + d] = static_cast<uint8_t>(clamped_Y);
+    }
+  };
+  gemm_context->threadPool()->run(f, N);
+}
+
+} // namespace
+
+template <Activation Ac>
+class Int8AddOp final : public Operator<CPUContext> {
+ public:
+  Int8AddOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<CPUContext>(operator_def, ws),
+        gemm_context_(ws->GetThreadPool()) {}
+
+  bool RunOnDevice() override {
+    CAFFE_ENFORCE_EQ(Inputs().size(), 2);
+    const auto& X0 = Inputs()[0]->template Get<Int8TensorCPU>();
+    const auto& X1 = Inputs()[1]->template Get<Int8TensorCPU>();
+    auto* Y = Outputs()[0]->template GetMutable<Int8TensorCPU>();
+    auto X0_offset = -X0.zero_point;
+    auto X1_offset = -X1.zero_point;
+    int32_t Y_offset = this->template GetSingleArgument<int>("Y_zero_point", 0);
+    auto Y_scale = this->template GetSingleArgument<float>("Y_scale", 1);
+    const double twice_max_input_scale = 2 * std::max(X0.scale, X1.scale);
+    const double real_X0_multiplier = X0.scale / twice_max_input_scale;
+    const double real_X1_multiplier = X1.scale / twice_max_input_scale;
+    const double real_Y_multiplier =
+        twice_max_input_scale / ((1 << kAddLeftShift) * Y_scale);
+
+    Y->t.ResizeLike(X0.t);
+    Y->zero_point = Y_offset;
+    Y->scale = Y_scale;
+
+    int32_t X0_multiplier;
+    int X0_shift;
+    QuantizeMultiplierSmallerThanOne(
+        real_X0_multiplier, &X0_multiplier, &X0_shift);
+    int32_t X1_multiplier;
+    int X1_shift;
+    QuantizeMultiplierSmallerThanOne(
+        real_X1_multiplier, &X1_multiplier, &X1_shift);
+    int32_t Y_multiplier;
+    int Y_shift;
+    QuantizeMultiplierSmallerThanOne(
+        real_Y_multiplier, &Y_multiplier, &Y_shift);
+
+    Int8Add(
+        X0.t.template data<uint8_t>(),
+        X0.t.size() / X0.t.dim(X0.t.ndim() - 1),
+        X0.t.dim(X0.t.ndim() - 1),
+        X0_offset,
+        X0_multiplier,
+        X0_shift,
+        X1.t.template data<uint8_t>(),
+        X1_offset,
+        X1_multiplier,
+        X1_shift,
+        Y_offset,
+        Y_multiplier,
+        Y_shift,
+        Y->t.template mutable_data<uint8_t>(),
+        activationLimits(Y->scale, Y->zero_point, Ac).first,
+        activationLimits(Y->scale, Y->zero_point, Ac).second,
+        &gemm_context_);
+    return true;
+  }
+
+ private:
+  C2GEMMContext gemm_context_;
+};
+
+} // namespace int8
+
+} // namespace caffe2
+
+#endif // CAFFE2_OPERATORS_INT8_ADD_OP_H_

commit 15f2c0a72448c5bceda3747d2d545b17cfa6b070

Fix Caffe2 operator schemas (#15462) (#13229) (#18109)
Summary:
Maratyszcza harouwu yinghai

This is broken since #13065. `c_str()` returns a pointer that isn't permanent.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/18109

Differential Revision: D14516622

Pulled By: ezyang

fbshipit-source-id: 7113d92eac4f61479c4c7b323cf78cc8aa00b17e

caffe2/operators/quantized/int8_average_pool_op.cc
@@ -20,19 +20,11 @@ data into the output blob Y for further processing.
 std::function<void(OpSchema&)> AveragePoolDocGenerator(
     const char* dim,
     bool relu_fused = false) {
-  auto suffix = relu_fused ? " Output will go through rectified linear "
-                             "function, where y = max(0, x)."
-                           : "";
   return [=](OpSchema& schema) {
     string doc = "AveragePool{dim} {pool_doc}";
     c10::ReplaceAll(doc, "{dim}", dim);
     c10::ReplaceAll(doc, "{pool_doc}", kAveragePoolDoc_int8);
     schema.SetDoc(doc);
-    string output_doc =
-        "Output data tensor from average pooling across the input "
-        "tensor. Dimensions will vary based on various kernel, stride, and pad "
-        "sizes.{suffix}";
-    c10::ReplaceAll(output_doc, "{suffix}", suffix);
     schema.Input(
         0,
         "X",
@@ -42,7 +34,14 @@ std::function<void(OpSchema&)> AveragePoolDocGenerator(
         "size, C is the number of channels, and H and W are the height and the "
         "width of the data. The corresponding permutation of dimensions is "
         "used in the latter case.");
-    schema.Output(0, "Y", output_doc.c_str());
+    schema.Output(0, "Y", relu_fused ?
+        "Output data tensor from average pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes. Output will go through rectified linear "
+        "function, where y = max(0, x)." :
+        "Output data tensor from average pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes.");
   };
 }
 

caffe2/operators/quantized/int8_conv_op.cc
@@ -19,9 +19,6 @@ why they are separate files.
 std::function<void(OpSchema&)> ConvDocGenerator(
     const char* dim,
     bool relu_fused = false) {
-  auto suffix = relu_fused ? " Output will go through rectified linear "
-                             "function, where y = max(0, x)."
-                           : "";
   return [=](OpSchema& schema) {
     string doc = R"DOC(
 The convolution operator consumes an input vector, a {dim}filter blob
@@ -29,11 +26,6 @@ and a bias blob and computes the output. {conv_doc})DOC";
     c10::ReplaceAll(doc, "{dim}", dim);
     c10::ReplaceAll(doc, "{conv_doc}", kConvDoc_int8);
     schema.SetDoc(doc);
-    string output_doc =
-        "Output data blob that contains the result of the "
-        "convolution. The output dimensions are functions of the kernel size, "
-        "stride size, and pad lengths.{suffix}";
-    c10::ReplaceAll(output_doc, "{suffix}", suffix);
     schema.Input(
         0,
         "X",
@@ -53,7 +45,14 @@ and a bias blob and computes the output. {conv_doc})DOC";
         "bias",
         "The 1D bias blob that is added through the "
         "convolution; has size (M).");
-    schema.Output(0, "Y", output_doc.c_str());
+    schema.Output(0, "Y", relu_fused ?
+        "Output data blob that contains the result of the "
+        "convolution. The output dimensions are functions of the kernel size, "
+        "stride size, and pad lengths. Output will go through rectified linear "
+        "function, where y = max(0, x)." :
+        "Output data blob that contains the result of the "
+        "convolution. The output dimensions are functions of the kernel size, "
+        "stride size, and pad lengths.");
   };
 }
 

caffe2/operators/quantized/int8_max_pool_op.cc
@@ -18,18 +18,10 @@ data into the output blob Y for further processing.
 std::function<void(OpSchema&)> MaxPoolDocGenerator(
     const char* dim,
     bool relu_fused = false) {
-  auto suffix = relu_fused ? " Output will go through rectified linear "
-                             "function, where y = max(0, x)."
-                           : "";
   return [=](OpSchema& schema) {
     string doc = "MaxPool{dim} {pool_doc}";
     c10::ReplaceAll(doc, "{dim}", dim);
     c10::ReplaceAll(doc, "{pool_doc}", kMaxPoolDoc_int8);
-    string output_doc =
-        "Output data tensor from max pooling across the input "
-        "tensor. Dimensions will vary based on various kernel, stride, and pad "
-        "sizes.{suffix}";
-    c10::ReplaceAll(output_doc, "{suffix}", suffix);
     schema.SetDoc(doc);
     schema.Input(
         0,
@@ -40,7 +32,14 @@ std::function<void(OpSchema&)> MaxPoolDocGenerator(
         "size, C is the number of channels, and H and W are the height and the "
         "width of the data. The corresponding permutation of dimensions is "
         "used in the latter case.");
-    schema.Output(0, "Y", output_doc.c_str());
+    schema.Output(0, "Y", relu_fused ?
+        "Output data tensor from max pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes. Output will go through rectified linear"
+        "function, where y = max(0, x)." :
+        "Output data tensor from max pooling across the input "
+        "tensor. Dimensions will vary based on various kernel, stride, and pad "
+        "sizes. Output will go through rectified linear");
   };
 }
 