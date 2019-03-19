b605ecfa496959b6e84d249fead5ecba7541779d
Mikhail Zolotukhin <mvz@fb.com>
2019-03-18 17:57:27 -0700

    Initial implementation of InsertObserverNodes pass.
    gh-metadata: pytorch pytorch 18150 gh/zolotukhinm@gmail.com/2/head

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -18,8 +18,30 @@ void PropagateQuantInfo(std::shared_ptr<Graph>& graph) {
   throw std::runtime_error("Pass not implemented yet!");
 }
 
+static void addObserverFor(torch::jit::Value* v) {
+  torch::jit::Node* def = v->node();
+  torch::jit::Node* observer =
+      def->owningGraph()->create(at::Symbol::fromQualString("aten::observer"));
+  def->owningBlock()->appendNode(observer);
+  observer->moveAfter(def);
+  v->replaceAllUsesWith(observer->outputs()[0]);
+  observer->outputs()[0]->setUniqueName(v->uniqueName() + ".observed");
+  observer->addInput(v);
+}
+
 void InsertObserverNodes(std::shared_ptr<Graph>& graph) {
-  throw std::runtime_error("Pass not implemented yet!");
+  for (const auto& n : graph->nodes()) {
+    // Skip nodes that we've just added
+    if (n->kind().toQualString() == std::string("aten::observer")) {
+      continue;
+    }
+
+    // Add observers to all outputs of node N unless they are inside a subgraph
+    // that we expect to be fused.
+    for (torch::jit::Value* v : n->outputs()) {
+      addObserverFor(v);
+    }
+  }
 }
 
 void InsertFakeQuantNodes(std::shared_ptr<Graph>& graph) {
