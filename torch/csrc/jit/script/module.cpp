017f2507484f19a7b0d1f8eb5243030246f061e9
davidriazati <davidriazati@fb.com>
2019-03-19 11:18:12 -0700

    add todo

--- torch/csrc/jit/script/module.cpp
+++ torch/csrc/jit/script/module.cpp
@@ -55,6 +55,7 @@ Value* try_emit_call_to(
           << " attempting to call a method with parameters/attributes"
              " from a raw graph. File a bug report";
     }
+    // TODO: preserve the type information so we don't have to infer it here
     auto type = incompleteInferTypeFrom(*member);
     matched_schema->inputs.push_back(
         caller->get_or_add_attribute(type, member));
