f5716cdb0210d524c679e8b2b583d0c2e90af8b5
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-03-18 19:22:07 -0700

    try using c10_host_device conditionally

--- aten/src/ATen/core/Array.h
+++ aten/src/ATen/core/Array.h
@@ -17,10 +17,15 @@ struct alignas(16) Array {
   C10_HOST_DEVICE T& operator[](int i) {
     return data[i];
   }
-
+#ifdef __HIP_PLATFORM_HCC__
+  C10_HOST_DEVICE Array() = default;
+  C10_HOST_DEVICE Array(const Array&) = default;
+  C10_HOST_DEVICE Array& operator=(const Array&) = default;
+#else
   Array() = default;
   Array(const Array&) = default;
   Array& operator=(const Array&) = default;
+#endif
 
   // Fill the array with x.
   C10_HOST_DEVICE Array(T x) {
--- test/onnx/test_models.py
+++ test/onnx/test_models.py
@@ -123,7 +123,7 @@ class TestModels(TestCase):
     def test_inception(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 3, 299, 299) + 3.)
-        self.exportTest(toC(inception_v3()), toC(x))
+        self.exportTest(toC(inception_v3()), toC(x), rtol=5e-2, atol=1e-6)
 
     def test_squeezenet(self):
         # SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and
