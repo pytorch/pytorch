68c367ad0954d7eadedaa98d1991b202b3581dd8
jgong5 <jiong.gong@intel.com>
2019-03-20 23:34:20 +0800

    add test on unsupported functions

--- test/test_mkldnn.py
+++ test/test_mkldnn.py
@@ -12,5 +12,23 @@ class TestMkldnn(TestCase):
         self.assertEqual(mkldnn_tensor.size(), torch.Size([1, 2, 3, 4]))
 
 
+    def test_unsupported(self):
+        # unsupported types and unsupported types with gpu
+        for dtype in [ torch.double, torch.half, torch.uint8, torch.int8,
+                       torch.short, torch.int, torch.long ]:
+            with self.assertRaises(RuntimeError) as context:
+                torch.randn(1, 2, 3, 4, dtype=dtype, device=torch.device('cpu')).to_mkldnn()
+            if torch.cuda.is_available():
+                with self.assertRaises(RuntimeError) as context:
+                    torch.randn(1, 2, 3, 4, dtype=dtype, device=torch.device('cuda')).to_mkldnn()
+        # supported type with gpu
+        if torch.cuda.is_available():
+            with self.assertRaises(RuntimeError) as context:
+                torch.randn(1, 2, 3, 4, dtype=torch.float, device=torch.device('cuda')).to_mkldnn()
+        # some factory functions
+        for creator in [ torch.empty, torch.ones, torch.zeros, torch.randn, torch.rand ]:
+            with self.assertRaises(RuntimeError) as context:
+                creator(1, 2, 3, 4, dtype=torch.float, device=torch.device('cpu'), layout=torch.mkldnn)
+
 if __name__ == '__main__':
     run_tests()
