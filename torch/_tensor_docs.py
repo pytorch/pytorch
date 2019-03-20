532fdcefffad6b27d1dd50ad3d629eaf74516222
vishwakftw <cs15btech11043@iith.ac.in>
2019-03-20 13:07:11 +0530

    Rename trtrs to triangular_solve
    Changelog:
    - Renames `trtrs` to `triangular_solve` to remain consistent with `cholesky_solve` and `solve`.
    - Rename all tests, fix callsites
    - Create a tentative alias for `triangular_solve` under the name `trtrs`, and add a deprecation warning to not promote usage.
    - Move `isnan` to _torch_docs.py
    - Remove unnecessary imports
    
    Test Plan:
    - All tests should pass to confirm that the patch is correct.

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -2587,6 +2587,13 @@ transpose_(dim0, dim1) -> Tensor
 In-place version of :meth:`~Tensor.transpose`
 """)
 
+add_docstr_all('triangular_solve',
+               r"""
+triangular_solve(A, upper=True, transpose=False, unitriangular=False) -> (Tensor, Tensor)
+
+See :func:`torch.triangular_solve`
+""")
+
 add_docstr_all('tril',
                r"""
 tril(k=0) -> Tensor
@@ -2613,13 +2620,6 @@ add_docstr_all('triu_',
 triu_(k=0) -> Tensor
 
 In-place version of :meth:`~Tensor.triu`
-""")
-
-add_docstr_all('trtrs',
-               r"""
-trtrs(A, upper=True, transpose=False, unitriangular=False) -> (Tensor, Tensor)
-
-See :func:`torch.trtrs`
 """)
 
 add_docstr_all('trunc',
