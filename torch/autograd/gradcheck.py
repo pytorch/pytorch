88e25ad170e925664991ccccafbcf6af5022143a
Igor Fedan <ifedan@fb.com>
2019-03-21 09:35:15 -0400

    changed the way to verify that numerical gradient is 0

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -234,7 +234,7 @@ def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, raise_exception=True
                 return _as_tuple(func(*input))[i]
             numerical = get_numerical_jacobian(fn, tupled_inputs, eps=eps)
             for n in numerical:
-                if not n.sum().item() == 0:
+                if len(torch.nonzero(n)) > 0:
                     fail_test('Numerical gradient for function expected to be zero')
         return True
 
