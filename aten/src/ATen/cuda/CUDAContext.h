pytorch/pytorch

commit 272a48f6fee54d0ad9d6938ac380d55f46b35682

Enable autograd to recognize the XLA backend as one providing multiple devices (#17847)
Summary:
…e devices, while not being CUDA/HIP.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/17847

Differential Revision: D14545634

Pulled By: ezyang

fbshipit-source-id: 417181bf2ff4f8978544afe2fb6b042e787854ed

c10/core/impl/DeviceGuardImplInterface.h
@@ -131,4 +131,8 @@ inline const DeviceGuardImplInterface* getDeviceGuardImpl(DeviceType type) {
   return p;
 }
 
+inline bool hasDeviceGuardImpl(DeviceType type) {
+  return device_guard_impl_registry[static_cast<size_t>(type)].load();
+}
+
 }} // namespace c10::impl

test/cpp_extensions/msnpu_extension.cpp
@@ -9,7 +9,7 @@ static int test_int;
 Tensor get_dtype_tensor(caffe2::TypeMeta dtype) {
   auto tensor_impl = c10::make_intrusive<TensorImpl, UndefinedTensorImpl>(
       Storage(
-          dtype, 0, at::DataPtr(nullptr, Device(DeviceType::MSNPU, 1)), nullptr, false),
+          dtype, 0, at::DataPtr(nullptr, Device(DeviceType::MSNPU, 0)), nullptr, false),
       MSNPUTensorId(),
       false);
   return Tensor(std::move(tensor_impl));
@@ -90,6 +90,45 @@ void init_msnpu_extension() {
     "ones_like(Tensor self, TensorOptions options) -> Tensor",
     &ones_like_override);
 }
+
+// TODO: Extend this to exercise multi-device setting.  In that case,
+// we need to add a thread local variable to track the current device.
+struct MSNPUGuardImpl final : public c10::impl::DeviceGuardImplInterface {
+  static constexpr DeviceType static_type = DeviceType::MSNPU;
+  MSNPUGuardImpl() {}
+  MSNPUGuardImpl(DeviceType t) {
+    AT_ASSERT(t == DeviceType::MSNPU);
+  }
+  DeviceType type() const override {
+    return DeviceType::MSNPU;
+  }
+  Device exchangeDevice(Device d) const override {
+    AT_ASSERT(d.type() == DeviceType::MSNPU);
+    AT_ASSERT(d.index() == 0);
+    return d;
+  }
+  Device getDevice() const override {
+    return Device(DeviceType::MSNPU, 0);
+  }
+  void setDevice(Device d) const override {
+    AT_ASSERT(d.type() == DeviceType::MSNPU);
+    AT_ASSERT(d.index() == 0);
+  }
+  void uncheckedSetDevice(Device d) const noexcept override {
+  }
+  Stream getStream(Device d) const noexcept override {
+    return Stream(Stream::DEFAULT, Device(DeviceType::MSNPU, 0));
+  }
+  Stream exchangeStream(Stream s) const noexcept override {
+    return Stream(Stream::DEFAULT, Device(DeviceType::MSNPU, 0));
+  }
+  DeviceIndex deviceCount() const override {
+    return 1;
+  }
+};
+
+constexpr DeviceType MSNPUGuardImpl::static_type;
+C10_REGISTER_GUARD_IMPL(MSNPU, MSNPUGuardImpl);
 
 int get_test_int() {
   return test_int;

aten/src/ATen/Context.h
@@ -14,6 +14,7 @@
 #include <ATen/detail/HIPHooksInterface.h>
 #include <ATen/detail/ComplexHooksInterface.h>
 #include <c10/util/Exception.h>
+#include <c10/core/impl/DeviceGuardImplInterface.h>
 
 #include <memory>
 #include <mutex>
@@ -75,6 +76,9 @@ class CAFFE2_API Context {
   }
   bool hasHIP() const {
     return detail::getHIPHooks().hasHIP();
+  }
+  bool hasXLA() const {
+    return c10::impl::hasDeviceGuardImpl(at::DeviceType::XLA);
   }
   // defined in header so that getNonVariableType has ability to inline
   // call_once check. getNonVariableType is called fairly frequently
@@ -200,14 +204,28 @@ static inline bool hasHIP() {
   return globalContext().hasHIP();
 }
 
+static inline bool hasXLA() {
+  return globalContext().hasXLA();
+}
+
+// Despite its name, this function returns the number of *CUDA* GPUs.
 static inline size_t getNumGPUs() {
-  if (hasCUDA()) {
+  // WARNING: DO NOT ADD LOGIC TO HANDLE OTHER DEVICE TYPES TO THIS
+  // FUNCTION.  If you are interested in interrogating the number of
+  // devices for a specific device type, add that function to the
+  // relevant library (e.g., similar to at::cuda::device_count())
+  if (hasCUDA() && hasHIP()) {
+    throw std::runtime_error(
+        "Enabling both CUDA and HIP in ATen is not supported, as HIP masquerades "
+        "to be CUDA (e.g., when you say CUDA, on a HIP build of ATen, this actually "
+        "means HIP.  Rebuild PyTorch with one or the other disabled.");
+  } else if (hasCUDA()) {
     return detail::getCUDAHooks().getNumGPUs();
-  }
-  if (hasHIP()) {
+  } else if (hasHIP()) {
     return detail::getHIPHooks().getNumGPUs();
+  } else {
+    return 0;
   }
-  return 0;
 }
 
 static inline bool hasOpenMP() {

c10/core/DeviceType.h
@@ -34,6 +34,17 @@ enum class DeviceType : int16_t {
 // define explicit int constant
 constexpr int COMPILE_TIME_MAX_DEVICE_TYPES =
     static_cast<int>(DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES);
+
+static_assert(COMPILE_TIME_MAX_DEVICE_TYPES <= 16,
+    "Hey!  You seem to be adding a lot of new DeviceTypes.  The intent was "
+    "for this constant to reflect the actual number of DeviceTypes we support "
+    "in PyTorch; it's important that this number is not too large as we "
+    "use this to allocate stack arrays in some places in our code.  If you "
+    "are indeed just adding the 17th device type, feel free to change "
+    "the check to 32; but if you are adding some sort of extensible device "
+    "types registration, please be aware that you are affecting code that "
+    "this number is small.  Try auditing uses of this constant.");
+
 C10_API std::string DeviceTypeName(
     DeviceType d,
     bool lower_case = false);

test/test_cpp_extensions.py
@@ -639,7 +639,7 @@ class TestMSNPUTensor(common.TestCase):
         self.assertEqual(a.sum(), 0)
 
         b = torch.zeros(5, 5, device='msnpu')
-        self.assertEqual(b.device, torch.device('msnpu', 1))
+        self.assertEqual(b.device, torch.device('msnpu', 0))
         self.assertEqual(msnpu_extension.get_test_int(), 0)
         self.assertEqual(torch.get_default_dtype(), b.dtype)
 

torch/csrc/autograd/engine.cpp
@@ -203,22 +203,47 @@ Engine::Engine() = default;
 // This Engine's ReadyQueues and their corresponding threads are leaked here
 Engine::~Engine() = default;
 
-// TODO: Engine is not written in a way that it can deal with anything that's
-// not CUDA.
 auto Engine::thread_init(int device) -> void {
   THInferNumThreads();
+  // Note [Allocating GPUs to autograd threads]
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // What's our strategy here?  Originally, the autograd engine was written
+  // with only CUDA in mind.  We allocate one thread to handle all CPU
+  // operations, and a thread per CUDA device.
+  //
+  // But what if we have OTHER devices?  There are two plausible
+  // strategies:
+  //
+  //  - We can allocate threads equal to max(num_cuda_devices, num_xla_devices,
+  //    ...) and colocate cuda device 0 with xla device 0
+  //  - We can allocate threads equal to sum(num_cuda_devices, num_xla_devices,
+  //    ...) keeping everyone separate.
+  //
+  // We don't have any good reason to prefer one or the other, so we've
+  // arbitrarily picked to colocate devices.  Maybe the other approach is
+  // better.
+  //
   // NB: We MUST NOT construct the guard for device -1,
   // as in some settings we compile with cuda, but
   // have lazy stubs for CUDA functionality (so actually
   // attempting to setup a guard(-1) will cause an
   // error, because it will still query cudaGetDevice).
-  at::OptionalDeviceGuard guard;
+  //
+  // NB: These are not OptionalCUDAGuard/etc because engine.cpp
+  // is built as part of the CPU-only library; so we need to
+  // dynamic dispatch.
+  //
+  // NB: We need an array here since neither DeviceGuard nor OptionalDeviceGuard
+  // are movable.
+  std::array<c10::OptionalDeviceGuard,
+             static_cast<size_t>(c10::DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES)>
+      guards;  // Guards! Guards!
   if (device != -1) {
-    if (at::hasCUDA()) {
-      guard.reset_device(at::Device(at::DeviceType::CUDA, device));
-    }
-    if (at::hasHIP()) {
-      guard.reset_device(at::Device(at::DeviceType::HIP, device));
+    for (size_t i = 0; i < static_cast<size_t>(c10::DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES); i++) {
+      auto* impl = c10::impl::device_guard_impl_registry[i].load();
+      if (impl && device < impl->deviceCount()) {
+        guards[i].reset_device(at::Device(static_cast<c10::DeviceType>(i), device));
+      }
     }
   }
   worker_device = device;
@@ -276,7 +301,7 @@ auto Engine::thread_main(GraphTask *graph_task) -> void {
         if (--task.base->outstanding_tasks == 0) {
           // Synchronize outstanding_tasks with queue mutex
           std::atomic_thread_fence(std::memory_order_release);
-          ready_queue(base_owner).push(FunctionTask(task.base, nullptr, InputBuffer(0)));
+          ready_queue_by_index(base_owner).push(FunctionTask(task.base, nullptr, InputBuffer(0)));
         }
       }
     }
@@ -352,7 +377,7 @@ static void validate_outputs(const edge_list& edges, variable_list& grads, const
       ss << metadata.type() << " but got " << grads[i].type();
       AT_ERROR(format_error(ss.str()));
     }
-    const auto output_device = output.is_cuda() ? output.get_device() : -1;
+    auto output_device = output.device();
     if (output_device != metadata.device()) {
       std::stringstream ss;
       ss << "invalid gradient at index " << i << " - expected device ";
@@ -557,7 +582,7 @@ auto Engine::execute(const edge_list& roots,
   if (!outputs.empty()) {
     graph_task.init_to_execute(*graph_root, outputs);
   }
-  ready_queue(-1).push(FunctionTask(&graph_task, std::move(graph_root), InputBuffer(0)));
+  ready_queue(at::kCPU).push(FunctionTask(&graph_task, std::move(graph_root), InputBuffer(0)));
 
   // Not a worker
   if (worker_device == NO_DEVICE) {
@@ -627,13 +652,34 @@ bool Engine::is_checkpoint_valid() {
   return checkpoint_valid;
 }
 
-auto Engine::ready_queue(int device) -> ReadyQueue& {
-  return *ready_queues.at(device + 1);
+auto Engine::ready_queue(at::Device device) -> ReadyQueue& {
+  // See Note [Allocating GPUs to autograd threads]
+  if (device.type() == at::kCPU) {
+    return *ready_queues.at(0);
+  } else {
+    return *ready_queues.at(device.index() + 1);
+  }
+}
+
+// See Note [Allocating GPUs to autograd threads]
+// NB: This would become obsolete if we truly allocated a CPU thread
+// per device, rather than colocate.
+auto Engine::ready_queue_by_index(int device_index) -> ReadyQueue& {
+  return *ready_queues.at(device_index + 1);
 }
 
 auto Engine::start_threads() -> void {
-  int num_devices = at::getNumGPUs();
-  // One for CPU, plus one for every GPU device
+  // See Note [Allocating GPUs to autograd threads]
+  c10::DeviceIndex num_devices = 0;
+  for (const auto& impl_atomic : c10::impl::device_guard_impl_registry) {
+    auto* impl = impl_atomic.load();
+    if (impl) {
+      num_devices = std::max(num_devices, impl->deviceCount());
+    }
+  }
+
+  // One for CPU, plus one for every GPU device (but colocate GPUs of different
+  // types)
   int num_threads = num_devices + 1;
   ready_queues = std::vector<std::shared_ptr<ReadyQueue>>(num_threads);
   for (auto& queue : ready_queues)

torch/csrc/autograd/engine.h
@@ -54,7 +54,8 @@ struct TORCH_API Engine {
 protected:
   void compute_dependencies(Function* root, GraphTask& task);
   void evaluate_function(FunctionTask& task);
-  ReadyQueue& ready_queue(int device);
+  ReadyQueue& ready_queue(at::Device device);
+  ReadyQueue& ready_queue_by_index(int device_index);
   void start_threads();
   virtual void thread_init(int device);
   virtual void thread_main(GraphTask *graph_task);

torch/csrc/autograd/function.h
@@ -130,7 +130,7 @@ struct TORCH_API Function : std::enable_shared_from_this<Function> {
   uint32_t add_input_metadata(
     const at::Type& type
   , at::IntArrayRef shape
-  , const int64_t device) noexcept {
+  , at::Device device) noexcept {
     uint32_t input_nr = input_metadata_.size();
     input_metadata_.emplace_back(type, shape, device);
     return input_nr;

commit 98cf169394c69d2ab8481aba106bd9e4e909a4fb

Unify cudaGetDeviceCount implementations.
I went about doing this by searching for calls to cudaGetDeviceCount,
and then methodically replacing them with references to c10::cuda::device_count()
or at::cuda::device_count().

There is a point to doing this: the various implementations wildly differed
in their handling of what to do when cudaGetDeviceCount returns an error.
The final standardized behavior is that **all errors are swallowed** and
we return device count of zero.  This indirectly fixes running CUDA builds
on CPU, which was broken in #17847.

I added 'noexcept' to the 'deviceCount' virtual method on DeviceGuardImpl.
This is a BC-breaking change for anyone inheriting from DeviceGuardImpl
but all you need to do is put 'noexcept' on your method and it is backwards
compatible with older libtorch.

Signed-off-by: Edward Z. Yang <ezyang@fb.com>

aten/src/ATen/cuda/CUDAContext.h
@@ -36,22 +36,20 @@ It is expected that the modules whose functions compose this interface will
 manage their own state. There is only a single CUDA context/state.
 */
 
-/* Device info */
+/**
+ * DEPRECATED: use device_count() instead
+ */
 inline int64_t getNumGPUs() {
     return c10::cuda::device_count();
 }
 
 /**
- * In some situations, you may have compiled with CUDA, but no CUDA
- * device is actually available.  Test for this case using is_available().
+ * CUDA is available if we compiled with CUDA, and there are one or more
+ * devices.  If we compiled with CUDA but there is a driver problem, etc.,
+ * this function will report CUDA is not available (rather than raise an error.)
  */
 inline bool is_available() {
-    int count;
-    cudaError_t err = cudaGetDeviceCount(&count);
-    if (err == cudaErrorInsufficientDriver) {
-      return false;
-    }
-    return count > 0;
+    return c10::cuda::device_count() > 0;
 }
 
 CAFFE2_API cudaDeviceProp* getCurrentDeviceProperties();

aten/src/ATen/cuda/detail/CUDAHooks.cpp
@@ -46,12 +46,7 @@ std::unique_ptr<Generator> CUDAHooks::initCUDAGenerator(
 }
 
 bool CUDAHooks::hasCUDA() const {
-  int count;
-  cudaError_t err = cudaGetDeviceCount(&count);
-  if (err == cudaErrorInsufficientDriver) {
-    return false;
-  }
-  return true;
+  return at::cuda::is_available();
 }
 
 bool CUDAHooks::hasMAGMA() const {
@@ -152,15 +147,7 @@ void CUDAHooks::cuFFTClearPlanCache() const {
 }
 
 int CUDAHooks::getNumGPUs() const {
-  int count;
-  auto err = cudaGetDeviceCount(&count);
-  if (err == cudaErrorNoDevice) {
-    return 0;
-  } else if (err != cudaSuccess) {
-    AT_ERROR(
-        "CUDA error (", static_cast<int>(err), "): ", cudaGetErrorString(err));
-  }
-  return count;
+  return at::cuda::device_count();
 }
 
 // Sigh, the registry doesn't support namespaces :(

aten/src/ATen/detail/CPUGuardImpl.h
@@ -34,7 +34,7 @@ struct CPUGuardImpl final : public c10::impl::DeviceGuardImplInterface {
     // no-op
     return Stream(Stream::DEFAULT, Device(DeviceType::CPU, -1));
   }
-  DeviceIndex deviceCount() const override {
+  DeviceIndex deviceCount() const noexcept override {
     return 1;
   }
 };

aten/src/ATen/hip/impl/HIPGuardImplMasqueradingAsCUDA.h
@@ -89,7 +89,7 @@ struct HIPGuardImplMasqueradingAsCUDA final : public c10::impl::DeviceGuardImplI
     setCurrentHIPStreamMasqueradingAsCUDA(cs);
     return old_stream.unwrap();
   }
-  DeviceIndex deviceCount() const override {
+  DeviceIndex deviceCount() const noexcept override {
     int deviceCnt;
     C10_HIP_CHECK(hipGetDeviceCount(&deviceCnt));
     return deviceCnt;

c10/core/impl/DeviceGuardImplInterface.h
@@ -81,9 +81,11 @@ struct C10_API DeviceGuardImplInterface {
   virtual Stream exchangeStream(Stream) const noexcept = 0;
 
   /**
-   * Get the number of devices.
+   * Get the number of devices.  WARNING: This is REQUIRED to not raise
+   * an exception.  If there is some sort of problem, e.g., driver error,
+   * you should report that there are zero available devices.
    */
-  virtual DeviceIndex deviceCount() const = 0;
+  virtual DeviceIndex deviceCount() const noexcept = 0;
 
   /**
    * Intended use of this class is to leak the DeviceGuardImpl at program end.

c10/core/impl/FakeGuardImpl.h
@@ -54,7 +54,7 @@ struct FakeGuardImpl final : public DeviceGuardImplInterface {
     current_streams_[s.device_index()] = s.id();
     return Stream(Stream::UNSAFE, s.device(), old_id);
   }
-  DeviceIndex deviceCount() const override {
+  DeviceIndex deviceCount() const noexcept override {
     return 1;
   }
   // Convenience methods for testing

c10/core/impl/VirtualGuardImpl.h
@@ -40,7 +40,7 @@ public:
   Stream exchangeStream(Stream s) const noexcept override {
     return impl_->exchangeStream(s);
   }
-  DeviceIndex deviceCount() const override {
+  DeviceIndex deviceCount() const noexcept override {
     return impl_->deviceCount();
   }
 private:

c10/cuda/CUDACachingAllocator.cpp
@@ -2,6 +2,7 @@
 
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAFunctions.h>
 #include <c10/util/UniqueVoidPtr.h>
 
 #include <cuda_runtime_api.h>
@@ -624,9 +625,8 @@ std::mutex* getFreeMutex()
 }
 
 static inline void assertValidDevice(int device) {
-  int device_count;
-  C10_CUDA_CHECK(cudaGetDeviceCount(&device_count));
-  AT_ASSERTM(0 <= device && device < device_count, "Invalid device argument.");
+  int device_num = device_count();
+  AT_ASSERTM(0 <= device && device < device_num, "Invalid device argument.");
 }
 
 uint64_t currentMemoryAllocated(int device)

c10/cuda/CUDAFunctions.h
@@ -16,9 +16,21 @@
 namespace c10 {
 namespace cuda {
 
-inline DeviceIndex device_count() {
+inline DeviceIndex device_count() noexcept {
   int count;
-  C10_CUDA_CHECK(cudaGetDeviceCount(&count));
+  // NB: In the past, we were inconsistent about whether or not this reported
+  // an error if there were driver problems are not.  Based on experience
+  // interacting with users, it seems that people basically ~never want this
+  // function to fail; it should just return zero if things are not working.
+  // Oblige them.
+  cudaError_t err = cudaGetDeviceCount(&count);
+  if (err != cudaSuccess) {
+    // Clear out the error state, so we don't spuriously trigger someone else.
+    // (This shouldn't really matter, since we won't be running very much CUDA
+    // code in this regime.)
+    cudaGetLastError();
+    return 0;
+  }
   return static_cast<DeviceIndex>(count);
 }
 

c10/cuda/impl/CUDAGuardImpl.h
@@ -5,6 +5,7 @@
 
 #include <c10/cuda/CUDAException.h>
 #include <c10/cuda/CUDAStream.h>
+#include <c10/cuda/CUDAFunctions.h>
 
 #include <cuda_runtime_api.h>
 
@@ -54,10 +55,8 @@ struct CUDAGuardImpl final : public c10::impl::DeviceGuardImplInterface {
     setCurrentCUDAStream(cs);
     return old_stream.unwrap();
   }
-  DeviceIndex deviceCount() const override {
-    int deviceCnt;
-    C10_CUDA_CHECK(cudaGetDeviceCount(&deviceCnt));
-    return deviceCnt;
+  DeviceIndex deviceCount() const noexcept override {
+    return device_count();
   }
 };
 

torch/csrc/api/src/cuda.cpp
@@ -14,6 +14,7 @@ bool is_available() {
   // NB: the semantics of this are different from at::globalContext().hasCUDA();
   // ATen's function tells you if you have a working driver and CUDA build,
   // whereas this function also tells you if you actually have any GPUs.
+  // This function matches the semantics of at::cuda::is_available()
   return cuda::device_count() > 0;
 }
 

torch/csrc/autograd/profiler_cuda.cpp
@@ -43,8 +43,7 @@ struct CUDAMethods : public CUDAStubs {
   }
   void onEachDevice(std::function<void(int)> op) override {
     at::cuda::OptionalCUDAGuard device_guard;
-    int count;
-    TORCH_CUDA_CHECK(cudaGetDeviceCount(&count));
+    int count = at::cuda::device_count();
     for(int i = 0; i < count; i++) {
       device_guard.set_index(i);
       op(i);

torch/csrc/cuda/Module.cpp
@@ -57,12 +57,7 @@ PyObject * THCPModule_getDevice_wrap(PyObject *self)
 PyObject * THCPModule_getDeviceCount_wrap(PyObject *self)
 {
   HANDLE_TH_ERRORS
-  int ndevice;
-  if (cudaGetDeviceCount(&ndevice) != cudaSuccess) {
-    cudaGetLastError();
-    ndevice = 0;
-  }
-  return PyLong_FromLong(ndevice);
+  return PyLong_FromLong(at::cuda::device_count());
   END_HANDLE_TH_ERRORS
 }
 

test/cpp_extensions/msnpu_extension.cpp
@@ -122,7 +122,7 @@ struct MSNPUGuardImpl final : public c10::impl::DeviceGuardImplInterface {
   Stream exchangeStream(Stream s) const noexcept override {
     return Stream(Stream::DEFAULT, Device(DeviceType::MSNPU, 0));
   }
-  DeviceIndex deviceCount() const override {
+  DeviceIndex deviceCount() const noexcept override {
     return 1;
   }
 };