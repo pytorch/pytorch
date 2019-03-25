3afe34c77fc89e05e3dd2671cd358d199bb182be
WinticsAI <levi.viana@wintics.com>
2019-03-25 23:49:39 +0100

    [WIP] declaring Choice in TensorFactories

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -727,5 +727,133 @@ Tensor tensor_cuda(ArrayRef<T> values, const TensorOptions& options) {
   }
 AT_FORALL_SCALAR_TYPES_EXCEPT_HALF(TENSOR)
 #undef TENSOR
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ choice ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+template <typename scalar_t>
+void generate_keys(
+  scalar_t *keys,
+  scalar_t *weights,
+  int n,
+  THGenerator* generator
+){
+  std::lock_guard<std::mutex> lock(generator->mutex);
+
+  for(int i = 0; i < n; i++){
+    scalar_t u = THRandom_standard_uniform(generator);
+    keys[i] = (scalar_t) std::pow(u, 1/weights[i]);
+  }
+
+}
+
+void reservoir_generator_cpu(
+  int64_t* indices,
+  int n,
+  int k,
+  THGenerator* generator
+){
+  std::lock_guard<std::mutex> lock(generator->mutex);
+
+  for(int i = k; i < n; i++){
+    int64_t z = THRandom_random(generator) % (i + 1);
+    if (z < k) {
+        std::swap(indices[z], indices[i]);
+    }
+  }
+
+}
+
+Tensor reservoir_sampling_cpu(
+  Tensor& x,
+  Tensor &weights,
+  int k
+){
+
+  if (!x.is_contiguous()){
+    x = x.contiguous();
+  }
+
+  int n = x.numel();
+  auto options = x.options().dtype(at::kLong);
+  THGenerator* generator = THGenerator_new();
+
+  if (weights.numel() == 0){
+    Tensor indices_n = at::arange({n}, options);
+    int split, begin, end;
+    if(2 * k < n){
+      split = n - k;
+      begin = n - k;
+      end = n;
+    } else {
+      split = k;
+      begin = 0;
+      end = k;
+    }
+
+    reservoir_generator_cpu(
+      indices_n.data<int64_t>(),
+      n,
+      split,
+      generator);
+
+      THGenerator_free(generator);
+      return x.index_select(
+        0,
+        indices_n.index_select(
+          0,
+          at::arange(begin, end, options)
+        )
+      );
+
+  } else {
+    Tensor keys = at::empty({n}, weights.options());
+
+    AT_DISPATCH_FLOATING_TYPES(weights.scalar_type(), "generate keys", [&] {
+      generate_keys<scalar_t>(
+        keys.data<scalar_t>(),
+        weights.data<scalar_t>(),
+        n,
+        generator);
+    });
+
+    THGenerator_free(generator);
+    return x.index_select(0, std::get<1>(keys.topk(k)));
+  }
+
+}
+
+Tensor sampling_with_replacement(
+  Tensor& x,
+  Tensor &weights,
+  int k
+){
+  int n = x.numel();
+  Tensor samples;
+
+  if (weights.numel() == 0){
+    samples = at::randint(0, n, {k}, x.options().dtype(at::kLong));
+  } else {
+    Tensor uniform_samples = at::rand({k}, weights.options());
+    Tensor cdf = weights.cumsum(0);
+    cdf /= cdf[-1];
+    samples = (uniform_samples.unsqueeze(1) > cdf.unsqueeze(0)).sum(1);
+  }
+
+  return x.index_select(0, samples);
+}
+
+Tensor choice_cpu(
+  Tensor& input,
+  Tensor& weights,
+  bool replacement,
+  int k
+){
+  if (replacement){
+    return sampling_with_replacement(input, weights, k);
+  } else {
+    return reservoir_sampling_cpu(input, weights, k);
+  }
+}
+
 } // namespace native
 } // namespace at
