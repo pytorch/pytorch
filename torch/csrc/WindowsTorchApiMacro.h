c53db886a390efa6b675e5a09c33ce75e2801b1c
Karl Ostmo <kostmo@gmail.com>
2019-03-26 16:46:57 -0700

    fully fix windows issue

--- torch/csrc/WindowsTorchApiMacro.h
+++ torch/csrc/WindowsTorchApiMacro.h
@@ -1,11 +1,7 @@
 #pragma once
 
 #ifdef _WIN32
-#if !defined(TORCH_BUILD_STATIC_LIBS)
 #define TORCH_API CAFFE2_API
-#else
-#define TORCH_API
-#endif
 #elif defined(__GNUC__)
 #if defined(torch_EXPORTS)
 #define TORCH_API __attribute__((__visibility__("default")))
