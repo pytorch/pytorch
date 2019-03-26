2d655098aa3c644aa74e4bf22a5ae6e055af301d
Tongzhou Wang <SsnL@users.noreply.github.com>
2019-03-26 11:40:06 -0400

    fix https://github.com/pytorch/pytorch/issues/16448

--- test/common_utils.py
+++ test/common_utils.py
@@ -421,12 +421,7 @@ class TestCase(expecttest.TestCase):
                     if a.device.type == 'cpu' and a.dtype == torch.float16:
                         # CPU half tensors don't have the methods we need below
                         a = a.to(torch.float32)
-                    if TEST_WITH_ROCM:
-                        # Workaround for bug https://github.com/pytorch/pytorch/issues/16448
-                        # TODO: remove after the bug is resolved.
-                        b = b.to(a.dtype).to(a.device)
-                    else:
-                        b = b.to(a)
+                    b = b.to(a)
 
                     if x.dtype == torch.bool and y.dtype == torch.bool:
                         self.assertEqual(x.tolist(), y.tolist())
