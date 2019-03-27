03910ac1bbd38e7dc5a29ee1dac71804c5f775a8
Will Feng <willfeng@fb.com>
2019-03-27 12:37:09 -0400

    don't copy version_counter in shallow copy

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -882,8 +882,11 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
     return std::move(autograd_meta_);
   }
 
-  // NOTE: `shallow_copy_and_detach()` does not copy the AutogradMeta pointer
-  // because it is unique for each Variable.
+  // NOTE: `shallow_copy_and_detach()` does not copy the following TensorImpl fields:
+  // 1. the AutogradMeta pointer, because it is unique for each Variable.
+  // 2. the version counter, because we should decide whether the new TensorImpl should share version
+  // counter with the original TensorImpl case-by-case. See NOTE [ Version Counter Sharing ] for details.
+  //
   // NOTE: We don't set `allow_tensor_metadata_change_` to false here, because there are call sites
   // to this function that need to change the shallow copy's size or storage afterwards, and setting
   // `allow_tensor_metadata_change_` to false would prevent those changes from happening and is
@@ -894,7 +897,6 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
     impl->storage_offset_ = storage_offset_;
     impl->is_wrapped_number_ = is_wrapped_number_;
     impl->reserved_ = reserved_;
-    // impl->set_version_counter(version_counter());
     impl->refresh_numel();
     impl->refresh_contiguous();
     return impl;
