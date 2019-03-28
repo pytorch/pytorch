e9a35aff1cdf390f6ae4ee79c1960b5683ec346a
Will Feng <willfeng@fb.com>
2019-03-28 11:22:11 -0400

    const_cast is not allowed by clang-tidy

--- torch/csrc/api/src/nn/modules/named_any.cpp
+++ torch/csrc/api/src/nn/modules/named_any.cpp
@@ -6,8 +6,8 @@ namespace nn {
 torch::OrderedDict<std::string, AnyModule> modules_ordered_dict(
   std::initializer_list<NamedAnyModule> named_modules) {
   torch::OrderedDict<std::string, AnyModule> dict;
-  for (const auto& named_module : named_modules) {
-    dict.insert(named_module.name(), std::move(const_cast<NamedAnyModule&>(named_module).module()));
+  for (auto named_module : named_modules) {
+    dict.insert(named_module.name(), std::move(named_module.module()));
   }
   return dict;
 }
