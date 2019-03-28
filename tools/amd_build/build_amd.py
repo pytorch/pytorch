be0a26493a43bf5705f512cfaaa9f056ef3d74a1
Junjie Bai <bai@in.tum.de>
2019-03-28 10:30:00 -0700

    Registery ATen op for HIP

--- tools/amd_build/build_amd.py
+++ tools/amd_build/build_amd.py
@@ -56,6 +56,7 @@ includes = [
     "caffe2/video/*",
     "caffe2/distributed/*",
     "caffe2/queue/*",
+    "caffe2/contrib/aten/*",
     "binaries/*",
     "caffe2/**/*_test*",
     "caffe2/core/*",
