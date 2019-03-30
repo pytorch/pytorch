21de8a567c722d6fac1f3e88c5d2daf3c31d3c17
Sebastian Messmer <messmer@fb.com>
2019-03-30 00:03:46 -0700

    Fallback kernels (#18443)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18443
    
    Allow registering a kernel without a dispatch key. In this case, the kernel becomes a fallback kernel that is called whenever no other kernel matches.
    This is also useful for the legacy function based API (since that API doesn't know about dispatch keys) or any other custom ops that don't care about dispatch
    and just want one kernel to be called no matter the dispatch key.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D14603258
    
    fbshipit-source-id: 242dc8871dad2989ca25079854d0cc97429e7199

--- aten/src/ATen/core/op_registration/op_registration_test.cpp
+++ aten/src/ATen/core/op_registration/op_registration_test.cpp
@@ -16,15 +16,28 @@ using c10::FunctionSchema;
 using c10::Argument;
 using c10::kernel;
 using c10::dispatchKey;
+using c10::Dispatcher;
 using at::Tensor;
 
 namespace {
 
 C10_DECLARE_TENSOR_TYPE(TensorType1);
 C10_DEFINE_TENSOR_TYPE(TensorType1);
+C10_DECLARE_TENSOR_TYPE(TensorType2);
+C10_DEFINE_TENSOR_TYPE(TensorType2);
 
 struct DummyKernel final : OperatorKernel {
   void operator()(Tensor) {}
+};
+
+struct MockKernel final : OperatorKernel {
+  MockKernel(bool* called): called_(called) {}
+
+  void operator()(Tensor) {
+    *called_ = true;
+  }
+private:
+  bool* called_;
 };
 
 FunctionSchema dummySchema(
@@ -44,15 +57,122 @@ TEST(OperatorRegistrationTest, whenTryingToRegisterWithoutKernel_thenFails) {
   c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>(), dispatchKey(TensorType1()));
 }
 
-TEST(OperatorRegistrationTest, whenTryingToRegisterWithoutDispatchKey_thenFails) {
-  // make sure it crashes when dispatch key is absent
+TEST(OperatorRegistrationTest, givenOpWithoutFallbackKernel_whenCallingOpWithWrongDispatchKey_thenFails) {
+  auto registrar = c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>(), dispatchKey(TensorType1()));
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
   EXPECT_THROW(
-    c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>()),
+    callOp(*op, dummyTensor(TensorType2())),
     c10::Error
   );
+}
 
-  // but make sure it doesn't crash when dispatch key is present
-  c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>(), dispatchKey(TensorType1()));
+TEST(OperatorRegistrationTest, givenOpWithFallbackKernelOutOfScope_whenCallingOpWithWrongDispatchKey_thenFails) {
+  auto registrar = c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>(), dispatchKey(TensorType1()));
+  {
+    auto inner_registrar = c10::RegisterOperators().op(dummySchema, kernel<DummyKernel>());
+    // this registered a fallback kernel, but now that registration goes out of scope and deregisters it
+  }
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_THROW(
+    callOp(*op, dummyTensor(TensorType2())),
+    c10::Error
+  );
+}
+
+TEST(OperatorRegistrationTest, givenOpWithOnlyFallbackKernel_whenCallingOp_thenCallsFallbackKernel) {
+  bool called = false;
+  auto registrar = c10::RegisterOperators().op(dummySchema, kernel<MockKernel>(&called)); // note: no dispatch key means this is the fallback kernel
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called);
+  callOp(*op, dummyTensor(TensorType2()));
+  EXPECT_TRUE(called);
+}
+
+TEST(OperatorRegistrationTest, givenOpWithOnlyFallbackKernelAndOtherKernelOutOfScope_whenCallingOp_thenCallsFallbackKernel) {
+  bool called = false;
+  bool other_called = false;
+  auto registrar = c10::RegisterOperators().op(dummySchema, kernel<MockKernel>(&called)); // note: no dispatch key means this is the fallback kernel
+  {
+    auto inner_registrar = c10::RegisterOperators().op(dummySchema, kernel<MockKernel>(&other_called), dispatchKey(TensorType2()));
+  }
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called);
+  callOp(*op, dummyTensor(TensorType2()));
+  EXPECT_TRUE(called);
+  EXPECT_FALSE(other_called);
+}
+
+TEST(OperatorRegistrationTest, givenOpWithFirstFallbackAndThenOtherKernel_whenCallingWithCorrectDispatchKey_thenCallsCorrectKernel) {
+  bool called_kernel = false;
+  bool called_fallback = false;
+  auto registrar = c10::RegisterOperators()
+    .op(dummySchema, kernel<MockKernel>(&called_fallback)) // note: no dispatch key means this is the fallback kernel
+    .op(dummySchema, kernel<MockKernel>(&called_kernel), dispatchKey(TensorType1()));
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+  callOp(*op, dummyTensor(TensorType1()));
+  EXPECT_TRUE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+}
+
+TEST(OperatorRegistrationTest, givenOpWithFirstFallbackAndThenOtherKernel_whenCallingWithWrongDispatchKey_thenCallsFallbackKernel) {
+  bool called_kernel = false;
+  bool called_fallback = false;
+  auto registrar = c10::RegisterOperators()
+    .op(dummySchema, kernel<MockKernel>(&called_fallback)) // note: no dispatch key means this is the fallback kernel
+    .op(dummySchema, kernel<MockKernel>(&called_kernel), dispatchKey(TensorType1()));
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+  callOp(*op, dummyTensor(TensorType2()));
+  EXPECT_FALSE(called_kernel);
+  EXPECT_TRUE(called_fallback);
+}
+
+
+TEST(OperatorRegistrationTest, givenOpWithFirstOtherAndThenFallbackKernel_whenCallingWithCorrectDispatchKey_thenCallsCorrectKernel) {
+  bool called_kernel = false;
+  bool called_fallback = false;
+  auto registrar = c10::RegisterOperators()
+    .op(dummySchema, kernel<MockKernel>(&called_kernel), dispatchKey(TensorType1()))
+    .op(dummySchema, kernel<MockKernel>(&called_fallback)); // note: no dispatch key means this is the fallback kernel
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+  callOp(*op, dummyTensor(TensorType1()));
+  EXPECT_TRUE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+}
+
+TEST(OperatorRegistrationTest, givenOpWithFirstOtherAndThenFallbackKernel_whenCallingWithWrongDispatchKey_thenCallsFallbackKernel) {
+  bool called_kernel = false;
+  bool called_fallback = false;
+  auto registrar = c10::RegisterOperators()
+    .op(dummySchema, kernel<MockKernel>(&called_kernel), dispatchKey(TensorType1()))
+    .op(dummySchema, kernel<MockKernel>(&called_fallback)); // note: no dispatch key means this is the fallback kernel
+
+  auto op = Dispatcher::singleton().findSchema("_test::dummy", "");
+  ASSERT_TRUE(op.has_value());
+  EXPECT_FALSE(called_kernel);
+  EXPECT_FALSE(called_fallback);
+  callOp(*op, dummyTensor(TensorType2()));
+  EXPECT_FALSE(called_kernel);
+  EXPECT_TRUE(called_fallback);
 }
 
 }
