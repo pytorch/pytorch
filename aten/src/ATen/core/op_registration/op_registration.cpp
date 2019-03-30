aaaa8ee4b662e1cab7981d7186753f112990abb0
Sebastian Messmer <messmer@fb.com>
2019-03-29 18:02:01 -0700

    V16: (no description)
    Differential Revision: D14514231
    Differential Version: 77424609

--- aten/src/ATen/core/op_registration/op_registration.cpp
+++ aten/src/ATen/core/op_registration/op_registration.cpp
@@ -1,5 +1,7 @@
 #include <ATen/core/op_registration/op_registration.h>
 
+#ifndef C10_MOBILE
+
 namespace c10 {
 
 RegisterOperators::RegisterOperators() = default;
@@ -8,3 +10,5 @@ RegisterOperators::RegisterOperators(RegisterOperators&&) noexcept = default;
 RegisterOperators& RegisterOperators::operator=(RegisterOperators&&) = default;
 
 }
+
+#endif
