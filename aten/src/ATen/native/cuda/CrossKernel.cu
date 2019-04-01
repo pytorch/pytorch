477135f3d87f8404a58518945185bbc35cd29430
Igor Fedan <ifedan@fb.com>
2019-04-01 10:53:37 -0400

    _th_cross renamed to _th_cross_kernel

--- aten/src/ATen/native/cuda/CrossKernel.cu
+++ aten/src/ATen/native/cuda/CrossKernel.cu
@@ -6,7 +6,7 @@
 namespace at { namespace native {
 
 void cross_kernel_impl(Tensor& result, const Tensor& x1, const Tensor& x2, const int64_t dim) {
-  at::legacy::th::_th_cross_out(result, x1, x2, dim);
+  at::legacy::th::_th_cross_kernel_out(result, x1, x2, dim);
 }
 
 REGISTER_DISPATCH(cross_stub, &cross_kernel_impl);
