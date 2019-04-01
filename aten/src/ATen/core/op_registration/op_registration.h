723ce02a55ab638c1afeb5068e15a8a22e7d5a6e
Sebastian Messmer <messmer@fb.com>
2019-04-01 14:53:08 -0700

    deprecated function based API (#18444)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18444
    
    This adds the deprecated function based API to c10::RegisterOperators().
    This is the API currently exposed under jit::RegisterOperators() and we need to support it for backwards compatibility.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D14514218
    
    fbshipit-source-id: c77676851cfd431d66f18fd8038cf153a3a7d7cc

--- aten/src/ATen/core/op_registration/op_registration.h
+++ aten/src/ATen/core/op_registration/op_registration.h
@@ -70,7 +70,51 @@ public:
     return std::move(*this);
   }
 
-  // TODO Add deprecated function and lambda based kernel APIs
+  // TODO allow input schema to be just the operator name + overload name, in that case use schema generated from kernel function
+
+  /**
+   * Deprecated. For backwards compatibility only.
+   * Don't use this, it introduces a performance overhead on each kernel call
+   * due to the kernel being stored in the wrapper as a runtime function pointer.
+   *
+   * Given a kernel
+   *
+   * > namespace { Tensor my_kernel_cpu(Tensor a, Tensor b) {...} }
+   *
+   * This deprecated API looks like:
+   *
+   * > static auto registry = c10::RegisterOperators()
+   * >     .op("my_op", &my_kernel_cpu);
+   *
+   * But you should use the new API instead:
+   *
+   * > static auto registry = c10::RegisterOperators()
+   * >     .op("my_op", kernel<decltype(my_kernel_cpu), &my_kernel_cpu>());
+   *
+   * Or, alternatively, write your kernel as a functor:
+   *
+   * > namespace {
+   * >   class my_kernel_cpu final : public c10::OperatorKernel {
+   * >   public:
+   * >     Tensor operator()(Tensor a, Tensor b) {...}
+   * >   };
+   * > }
+   * >
+   * > static auto registry = c10::RegisterOperators()
+   * >     .op("my_op", c10::kernel<my_kernel_cpu>());
+   */
+   template<class FuncType>
+   C10_DEPRECATED_MESSAGE("Registering kernels via passing function pointers to op() directly is deprecated. " \
+                          "Please use the new c10::kernel() based API instead.")
+   // enable_if: only enable it if FuncType is actually a function, but not a stack based KernelFunction.
+   guts::enable_if_t<guts::is_function_type<FuncType>::value && !std::is_same<FuncType, KernelFunction>::value, RegisterOperators>
+   op(FunctionSchema schema, FuncType* func) && {
+    // We intentionally don't extend this deprecated API to support dispatch keys
+    // and the like to push people towards using the new API.
+    return std::move(*this).op(std::move(schema), kernel<detail::WrapRuntimeKernelFunctor<FuncType*>>(func));
+   }
+
+  // TODO Add deprecated lambda-based API
 
 private:
   void registerOp_(FunctionSchema&& schema, detail::KernelRegistrationConfig&& config);
