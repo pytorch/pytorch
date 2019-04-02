00f6505a53628472de22f4eb6ae7182a1e6ac876
Vitaly Fedyunin <vitalyf@fb.com>
2019-04-02 08:44:27 -0700

    Adding pin_memory kwarg to zeros, ones, empty, ... tensor constructors. (#18455)
    Summary:
    Make it possible to construct a pinned memory tensor without creating a storage first and without calling pin_memory() function. It is also faster, as copy operation is unnecessary.
    
    Supported functions:
    ```python
    torch.rand_like(t, pin_memory=True)
    torch.randn_like(t, pin_memory=True)
    torch.empty_like(t, pin_memory=True)
    torch.full_like(t, 4, pin_memory=True)
    torch.zeros_like(t, pin_memory=True)
    torch.ones_like(t, pin_memory=True)
    torch.tensor([10,11], pin_memory=True)
    torch.randn(3, 5, pin_memory=True)
    torch.rand(3, pin_memory=True)
    torch.zeros(3, pin_memory=True)
    torch.randperm(3, pin_memory=True)
    torch.empty(6, pin_memory=True)
    torch.ones(6, pin_memory=True)
    torch.eye(6, pin_memory=True)
    torch.arange(3, 5, pin_memory=True)
    ```
    
    Part of the bigger: `Remove Storage` plan.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18455
    
    Reviewed By: ezyang
    
    Differential Revision: D14672084
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: 9d0997ec00f59500ee018f8b851934d334012124

--- torch/csrc/utils/tensor_new.cpp
+++ torch/csrc/utils/tensor_new.cpp
@@ -193,12 +193,15 @@ Tensor internal_new_from_data(
     PyObject* data,
     bool copy_variables,
     bool copy_numpy,
-    bool type_inference) {
+    bool type_inference,
+    bool pin_memory = false) {
+
   if (THPUtils_checkString(data)) {
     throw TypeError("new(): invalid data type '%s'", Py_TYPE(data)->tp_name);
   }
 
   if (THPVariable_Check(data)) {
+    AT_CHECK(!pin_memory, "Can't pin tensor constructed from a variable");
     auto var = reinterpret_cast<THPVariable*>(data)->cdata;
     if (copy_variables) {
       var = var.detach();
@@ -214,6 +217,7 @@ Tensor internal_new_from_data(
 
 #ifdef USE_NUMPY
   if (PyArray_Check(data)) {
+    AT_CHECK(!pin_memory, "Can't pin tensor constructed from numpy");
     auto tensor = autograd::make_variable(tensor_from_numpy(data), /*requires_grad=*/false);
     const auto& scalar_type = type_inference ? tensor.scalar_type() : type.scalarType();
     auto device = device_opt.has_value() ? *device_opt : at::Device(type.device_type());
@@ -225,7 +229,7 @@ Tensor internal_new_from_data(
 
   auto sizes = compute_sizes(data);
   ScalarType scalar_type = type_inference ? infer_scalar_type(data) : type.scalarType();
-  auto tensor = autograd::make_variable(at::empty(sizes, at::initialTensorOptions().dtype(scalar_type)), /*requires_grad=*/false);
+  auto tensor = autograd::make_variable(at::empty(sizes, at::initialTensorOptions().dtype(scalar_type).pinned_memory(pin_memory)), /*requires_grad=*/false);
   recursive_store(
       (char*)tensor.data_ptr(), tensor.sizes(), tensor.strides(), 0,
       scalar_type, tensor.element_size(), data);
@@ -502,10 +506,10 @@ Tensor sparse_coo_tensor_ctor(const Type& default_type, PyObject* args, PyObject
 
 Tensor tensor_ctor(const Type& type, PyObject* args, PyObject* kwargs) {
   static PythonArgParser parser({
-    "tensor(PyObject* data, *, ScalarType dtype=None, Device? device=None, bool requires_grad=False)",
+    "tensor(PyObject* data, *, ScalarType dtype=None, Device? device=None, bool pin_memory=False, bool requires_grad=False)",
   });
 
-  ParsedArgs<4> parsed_args;
+  ParsedArgs<5> parsed_args;
   auto r = parser.parse(args, kwargs, parsed_args);
   if (r.idx == 0) {
     PyObject* data = r.pyobject(0);
@@ -516,14 +520,16 @@ Tensor tensor_ctor(const Type& type, PyObject* args, PyObject* kwargs) {
     }
 
     bool type_inference = r.isNone(1);
-    bool args_requires_grad = r.toBool(3);
+    bool pin_memory = r.toBool(3);
+    bool args_requires_grad = r.toBool(4);
     auto new_tensor = internal_new_from_data(
                typeWithDefault(r, 1, 2, type),
                r.deviceOptional(2),
                data,
                true,
                true,
-               type_inference);
+               type_inference,
+               pin_memory);
     new_tensor.detach_(); // ensure new_tensor a leaf node
     new_tensor.set_requires_grad(args_requires_grad);
     return new_tensor;
@@ -576,10 +582,10 @@ Tensor new_tensor(const Type& type, PyObject* args, PyObject* kwargs) {
 
 Tensor new_empty(const Type& type, PyObject* args, PyObject* kwargs) {
   static PythonArgParser parser({
-    "new_empty(IntArrayRef size, *, ScalarType dtype=None, Device? device=None, bool requires_grad=False)",
+    "new_empty(IntArrayRef size, *, ScalarType dtype=None, Device? device=None, bool pin_memory=False, bool requires_grad=False)",
   }, /*traceable=*/true);
 
-  ParsedArgs<4> parsed_args;
+  ParsedArgs<5> parsed_args;
   auto r = parser.parse(args, kwargs, parsed_args);
   if (r.idx == 0) {
     const auto& actual_type = typeWithDefault(r, 1, 2, type);
