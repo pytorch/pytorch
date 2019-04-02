7cc892d18176b5067534a2111a37ee18be1c3c27
eellison <eellison@fb.com>
2019-04-02 14:20:10 -0700

    wasn't included

---
+++ torch/csrc/jit/testing/utils.h
@@ -0,0 +1,73 @@
+#include <torch/csrc/WindowsTorchApiMacro.h>
+#include <torch/csrc/jit/ir.h>
+
+namespace torch {
+namespace jit {
+namespace testing {
+
+TORCH_API std::vector<Node*> findAllNodes(
+    c10::ArrayRef<torch::jit::Block*> blocks,
+    Symbol kind,
+    bool recurse = true) {
+  std::vector<Node*> ret;
+  for (Block* block : blocks) {
+    for (Node* n : block->nodes()) {
+      if (n->kind() == kind) {
+        ret.push_back(n);
+      }
+      if (recurse) {
+        auto nodes = findAllNodes(n->blocks(), kind, recurse);
+        ret.insert(ret.end(), nodes.begin(), nodes.end());
+      }
+    }
+  }
+  return ret;
+}
+
+TORCH_API std::vector<Node*> findAllNodes(
+    Block* block,
+    Symbol kind,
+    bool recurse = true) {
+  std::vector<Block*> blocks = {block};
+  return findAllNodes(blocks, kind, recurse);
+}
+
+TORCH_API Node* findNode(
+    c10::ArrayRef<torch::jit::Block*> blocks,
+    Symbol kind,
+    bool recurse = true) {
+  for (Block* block : blocks) {
+    for (Node* n : block->nodes()) {
+      if (n->kind() == kind) {
+        return n;
+      }
+      if (recurse) {
+        auto node = findNode(n->blocks(), kind, recurse);
+        if (node != nullptr) {
+          return node;
+        }
+      }
+    }
+  }
+  return nullptr;
+}
+
+TORCH_API Node* findNode(Block* block, Symbol kind, bool recurse = true) {
+  std::vector<Block*> blocks = {block};
+  return findNode(blocks, kind, recurse);
+}
+
+TORCH_API Node* findNode(Graph& g, Symbol kind, bool recurse = true) {
+  return findNode(g.block(), kind, recurse);
+}
+
+TORCH_API std::vector<Node*> findAllNodes(
+    Graph& g,
+    Symbol kind,
+    bool recurse = true) {
+  return findAllNodes(g.block(), kind, recurse);
+}
+
+} // namespace testing
+} // namespace jit
+} // namespace torch
