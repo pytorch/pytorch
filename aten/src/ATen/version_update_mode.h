379da62773fc6196018379db3fb39a932bc15c42
Will Feng <willfeng@fb.com>
2019-04-02 17:34:35 -0400

    better comment

--- aten/src/ATen/version_update_mode.h
+++ aten/src/ATen/version_update_mode.h
@@ -4,6 +4,13 @@
 
 namespace at {
 
+// This thread-local mode controls whether in-place operations on a Tensor
+// update its version.
+//
+// WARNING: Disabling version update and performing in-place operations on a
+// Tensor that is a saved variable in an autograd graph could result in
+// incorrect gradient calculations. You should make sure that no incorrect
+// gradient calculations could occur in your code, before disabling version update.
 struct CAFFE2_API VersionUpdateMode {
   static bool is_enabled();
   static void set_enabled(bool enabled);
