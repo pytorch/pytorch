98deb452fd5f644cfb0ad8c399df1dce37e5be07
Sepehr Sameni <Sepehr.Sameni@gmail.com>
2019-04-02 11:41:47 +0430

    add tests for load_state_dict's return value

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -756,7 +756,7 @@ class Module(object):
         def load(module, prefix=''):
             local_metadata = {} if metadata is None else metadata.get(prefix[:-1], {})
             module._load_from_state_dict(
-                state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
+                state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
             for name, child in module._modules.items():
                 if child is not None:
                     load(child, prefix + name + '.')
