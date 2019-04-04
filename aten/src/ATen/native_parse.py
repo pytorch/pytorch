f5fb37d51f5b46dec03eec86cde9c60e6b9bab99
Michael Suo <suo@fb.com>
2019-04-03 22:18:09 -0700

    fix flake8 lint (#18835)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18835
    ghimport-source-id: 7b1f433ae51232822704d62699233688072cbc23
    
    Stack from [ghstack](https://github.com/ezyang/ghstack):
    * **#18835 fix flake8 lint**
    * #18826 [jit] run cpp tests for non-cuda builds in test_jit.py
    
    ...again
    
    Reviewed By: ZolotukhinM
    
    Differential Revision: D14766790
    
    fbshipit-source-id: 29361a407589092831dfbc3c5d63d2834934cd02

--- aten/src/ATen/native_parse.py
+++ aten/src/ATen/native_parse.py
@@ -343,6 +343,7 @@ def has_sparse_dispatches(dispatches):
             return True
     return False
 
+
 def parse_native_yaml(path):
     with open(path, 'r') as f:
         return yaml.load(f, Loader=Loader)
