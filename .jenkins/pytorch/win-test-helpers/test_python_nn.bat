29be77816175a968bde1f8b8c6b69bd31e4c51ab
Karl Ostmo <kostmo@gmail.com>
2019-04-04 10:38:09 -0700

    U/kostmo/win test offload scripts
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/18694
    
    Differential Revision: D14766339
    
    Pulled By: kostmo
    
    fbshipit-source-id: a2300e72129979f866430ca5c09dd7fff6df0a89

---
+++ .jenkins/pytorch/win-test-helpers/test_python_nn.bat
@@ -0,0 +1,29 @@
+call %TMP_DIR%/ci_scripts/setup_pytorch_env.bat
+
+echo Some smoke tests
+cd test
+
+echo Checking that caffe2.python is available
+python -c "from caffe2.python import core"
+if ERRORLEVEL 1 exit /b 1
+
+echo Checking that MKL is available
+python -c "import torch; exit(0 if torch.backends.mkl.is_available() else 1)"
+if ERRORLEVEL 1 exit /b 1
+
+echo Checking that CUDA archs are setup correctly
+python -c "import torch; torch.randn([3,5]).cuda()"
+if ERRORLEVEL 1 exit /b 1
+
+echo Checking that magma is available
+python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
+if ERRORLEVEL 1 exit /b 1
+
+echo Checking that CuDNN is available
+python -c "import torch; exit(0 if torch.backends.cudnn.is_available() else 1)"
+if ERRORLEVEL 1 exit /b 1
+
+cd ..
+
+echo Run nn tests
+cd test && python run_test.py --include nn --verbose && cd ..
