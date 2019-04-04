9a11744d3261aa1356f1b5e941d5147d15387d75
Michael Suo <suo@fb.com>
2019-04-03 17:42:48 -0700

    [jit] run cpp tests for non-cuda builds in test_jit.py
    We did all the work of nicely separating our cpp tests that don't require
    CUDA, but they aren't run from test_jit.py if CUDA is missing.

--- test/test_jit.py
+++ test/test_jit.py
@@ -1484,13 +1484,22 @@ class TestJit(JitTestCase):
         for node in g.nodes():
             self.assertTrue(g2.findNode(node.kind()) is not None)
 
+    @unittest.skipIf(IS_WINDOWS, "NYI: JIT tests not yet supported on windows")
+    @skipIfRocm
+    def test_cpp(self):
+        from cpp.jit import tests_setup
+        tests_setup.setup()
+        torch._C._jit_run_cpp_tests(run_cuda=False)
+        tests_setup.shutdown()
+
     @unittest.skipIf(IS_WINDOWS, "NYI: fuser support for Windows")
     @unittest.skipIf(not RUN_CUDA, "cpp tests require CUDA")
     @skipIfRocm
     def test_cpp_cuda(self):
         from cpp.jit import tests_setup
         tests_setup.setup()
-        torch._C._jit_run_cpp_tests()
+        run_cuda = True
+        torch._C._jit_run_cpp_tests(run_cuda=True)
         tests_setup.shutdown()
 
     def test_batchnorm(self):
