01deaf47cd32f88bd6e64d4520691abe25954d83
Igor Fedan <ifedan@fb.com>
2019-04-05 11:28:45 -0400

    Revert "Fix for TensorMethods"
    This reverts commit bc1cc26b51c0a7bd8e187c1ba7abaa08c42969a3.

--- aten/src/ATen/core/TensorMethods.h
+++ aten/src/ATen/core/TensorMethods.h
@@ -593,10 +593,10 @@ inline Tensor Tensor::std(IntArrayRef dim, bool unbiased, bool keepdim) const {
     return dispatch_type().std(*this, dim, unbiased, keepdim);
 }
 inline std::tuple<Tensor,Tensor> Tensor::std_mean(bool unbiased) const {
-    return dispatch_type().std_mean(*this, unbiased);
+    return type().std_mean(*this, unbiased);
 }
 inline std::tuple<Tensor,Tensor> Tensor::std_mean(IntArrayRef dim, bool unbiased, bool keepdim) const {
-    return dispatch_type().std_mean(*this, dim, unbiased, keepdim);
+    return type().std_mean(*this, dim, unbiased, keepdim);
 }
 inline Tensor Tensor::prod(ScalarType dtype) const {
     return dispatch_type().prod(*this, dtype);
@@ -668,10 +668,10 @@ inline Tensor Tensor::var(IntArrayRef dim, bool unbiased, bool keepdim) const {
     return dispatch_type().var(*this, dim, unbiased, keepdim);
 }
 inline std::tuple<Tensor,Tensor> Tensor::var_mean(bool unbiased) const {
-    return dispatch_type().var_mean(*this, unbiased);
+    return type().var_mean(*this, unbiased);
 }
 inline std::tuple<Tensor,Tensor> Tensor::var_mean(IntArrayRef dim, bool unbiased, bool keepdim) const {
-    return dispatch_type().var_mean(*this, dim, unbiased, keepdim);
+    return type().var_mean(*this, dim, unbiased, keepdim);
 }
 inline Tensor Tensor::view_as(const Tensor & other) const {
     return dispatch_type().view_as(*this, other);
