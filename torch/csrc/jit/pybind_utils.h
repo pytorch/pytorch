d1884d24ce76356ab71e9efe3d57f5ed011f1290
Zachary DeVito <zdevito@fb.com>
2019-04-05 13:33:14 -0700

    Make Object hold its ClassType (#18467)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18467
    ghimport-source-id: d51bdd64d2529d08c634c58df1a0870b54ad49fb
    
    Stack from [ghstack](https://github.com/ezyang/ghstack):
    * #18469 Create Object that represents a Module
    * #18468 slots with explicit value/setValue make more sense in future patches
    * **#18467 Make Object hold its ClassType**
    * #18379 Enforce single parent for script submodules
    * #18378 Unify namespace of script::Module
    * #18314 Add ability to specialize class types to ArgumentSpec
    * #18226 Add Slot type to abstract the raw pointers being used for slots.
    
    Currently it holds a symbol whose unqualified name is the name of the
    class. This will get confusing when there are multiple possible registries,
    and it makes getting the class type from the object difficult.
    The pointer to the class is only 4 more bytes so this patch just puts
    it in the object.
    
    Reviewed By: suo
    
    Differential Revision: D14613510
    
    fbshipit-source-id: b35175ba4be83d2522deaa6dad5070d6ec691fed

--- torch/csrc/jit/pybind_utils.h
+++ torch/csrc/jit/pybind_utils.h
@@ -226,9 +226,8 @@ inline IValue toIValue(
     case TypeKind::ClassType: {
       auto classType = type->expect<ClassType>();
       // 1. create a bare ivalue
-      const auto name = Symbol::user(classType->name());
       const size_t numAttrs = classType->numAttributes();
-      auto userObj = c10::ivalue::Object::create(name, numAttrs);
+      auto userObj = c10::ivalue::Object::create(classType, numAttrs);
 
       // 2. copy all the contained types
       for (size_t slot = 0; slot < numAttrs; slot++) {
@@ -346,10 +345,10 @@ inline py::object toPyObject(IValue&& ivalue) {
     return std::move(py_dict);
   } else if (ivalue.isObject()) {
     const auto obj = ivalue.toObject();
-    const auto classType = ClassType::get(obj->name().toUnqualString());
+    const auto classType = ClassType::get(obj->name());
     AT_ASSERT(classType);
     auto pyClass = py::module::import("torch.jit")
-                       .attr("_get_script_class")(obj->name().toUnqualString());
+                       .attr("_get_script_class")(obj->name());
     auto pyObj = pyClass.attr("__new__")(pyClass);
 
 
