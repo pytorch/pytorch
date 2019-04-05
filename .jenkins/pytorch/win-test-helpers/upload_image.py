90b8552c9811ee6e9bbc9dff1f1df30c00b663dd
Karl Ostmo <kostmo@gmail.com>
2019-04-04 21:05:13 -0700

    U/kostmo/windows offload scripts 3
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/18754
    
    Differential Revision: D14794893
    
    Pulled By: kostmo
    
    fbshipit-source-id: 05187d9b53615ffbcc7253accdc692c4ecaf25d9

---
+++ .jenkins/pytorch/win-test-helpers/upload_image.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import boto3
+
+IMAGE_COMMIT_TAG = os.getenv('IMAGE_COMMIT_TAG')
+
+session = boto3.session.Session()
+s3 = session.resource('s3')
+with open(sys.argv[1], 'rb') as data:
+  s3.Bucket('ossci-windows-build').put_object(Key='pytorch/' + IMAGE_COMMIT_TAG + '.7z', Body=data)
+object_acl = s3.ObjectAcl('ossci-windows-build', 'pytorch/' + IMAGE_COMMIT_TAG + '.7z')
+response = object_acl.put(ACL='public-read')
+
