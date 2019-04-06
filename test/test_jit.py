b572e5883321e791d796cd6af45b14b4f7c17ce9
Elias Ellison <eellison@fb.com>
2019-04-05 17:52:12 -0700

    Fix interpolate trace (#18875)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/10654
    
    The issue is that in tracing `.size` returns an int tensor, and when an int tensor is multiplied by a scalar the int dominates and the scalar gets casted 0.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18875
    
    Differential Revision: D14814441
    
    Pulled By: eellison
    
    fbshipit-source-id: a4e96a2698f2fcbf3ec4b2bb4c43a30250f30ad9

--- test/test_jit.py
+++ test/test_jit.py
@@ -7518,6 +7518,24 @@ a")
             mte, (torch.zeros(1, 2, 3),), None, verbose=False,
             example_outputs=outputs))
 
+    def test_interpolate_trace(self):
+        class test(nn.Module):
+            def __init__(self):
+                super(test, self).__init__()
+                self.conv = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+
+            def forward(self, x):
+                y = self.conv(x)
+                w = nn.functional.interpolate(y, mode='bilinear', align_corners=False, scale_factor=0.5)
+                return w
+
+        f = test()
+        # no failure
+        g = torch.jit.trace(f, (torch.zeros(1, 1, 28, 28),))
+        x = torch.zeros(1, 1, 14, 14)
+        # constants not baked in
+        self.assertEqual(g(x), f(x))
+
     def test_trace_nested_datatypes(self):
         @torch.jit.script
         def foo(x):
