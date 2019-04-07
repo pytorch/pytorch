8cc153dc1afa6c0dafdbe3d378e0b66ba408d463
Roy Li <royboy@fb.com>
2019-04-07 01:35:11 -0700

    Remove tensorFromBlob() from Type (#18779)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18779
    ghimport-source-id: e7453b74fcce0e4f4a9cbce0324992a85272a426
    
    Stack from [ghstack](https://github.com/ezyang/ghstack):
    * #18780 Remove tensorWithAllocator() from Type
    * **#18779 Remove tensorFromBlob() from Type**
    
    Differential Revision: D14739335
    
    fbshipit-source-id: 8a0619a5b412332efa3b2d60c1edebd53d089d50

--- aten/src/ATen/UndefinedType.h
+++ aten/src/ATen/UndefinedType.h
@@ -18,7 +18,6 @@ struct UndefinedType final : public TypeDefault {
   virtual Backend backend() const override;
   virtual Allocator* allocator() const override;
   virtual Device getDeviceFromPtr(void* data) const override;
-  virtual Storage storageFromBlob(void * data, int64_t size, const std::function<void(void*)> & deleter) const override;
   virtual Storage storageWithAllocator(int64_t size, Allocator* allocator) const override;
   virtual std::unique_ptr<Generator> generator() const override;
   virtual const char * toString() const override;
