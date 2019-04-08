1b2f40e4b5fd10d21a3bde78f08006f43cd0e8fb
Zachary DeVito <zdevito@fb.com>
2019-04-08 14:56:26 -0700

    Fix interpolate tracing (#19034)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19034
    ghimport-source-id: 874e0b0a8685184416152a77fc1850d9a06516ae
    
    Differential Revision: D14837282
    
    Pulled By: zdevito
    
    fbshipit-source-id: b0ed82b607c288a54eecec3d6ed62c4626e5a563

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -2495,7 +2495,7 @@ def interpolate(input, size=None, scale_factor=None, mode='nearest', align_corne
 
         # make scale_factor a tensor in tracing so constant doesn't get baked in
         if torch._C._get_tracing_state():
-            return [(torch.floor(input.size(i + 2) * torch.tensor(scale_factors[i]))) for i in range(dim)]
+            return [(torch.floor(input.size(i + 2) * torch.tensor(float(scale_factors[i])))) for i in range(dim)]
         else:
             return [int(math.floor(int(input.size(i + 2)) * scale_factors[i])) for i in range(dim)]
 
