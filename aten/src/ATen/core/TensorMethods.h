b4de794fdf273103511ac1e55ee2d5b7e2c67a83
jgong5 <jiong.gong@intel.com>
2019-04-08 21:30:50 -0700

    Add MKL-DNN Tensor (#17748)
    Summary:
    This is a minimalist PR to add MKL-DNN tensor per discussion from Github issue: https://github.com/pytorch/pytorch/issues/16038
    
    Ops with MKL-DNN tensor will be supported in following-up PRs to speed up imperative path.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/17748
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D14614640
    
    Pulled By: bddppq
    
    fbshipit-source-id: c58de98e244b0c63ae11e10d752a8e8ed920c533

--- aten/src/ATen/core/TensorMethods.h
+++ aten/src/ATen/core/TensorMethods.h
@@ -775,6 +775,9 @@ inline Tensor Tensor::to_sparse(int64_t sparse_dim) const {
 inline Tensor Tensor::to_sparse() const {
     return dispatch_type().to_sparse(*this);
 }
+inline Tensor Tensor::to_mkldnn() const {
+    return dispatch_type().to_mkldnn(*this);
+}
 inline Tensor Tensor::quantize_linear(double scale, int64_t zero_point) const {
     return dispatch_type().quantize_linear(*this, scale, zero_point);
 }
