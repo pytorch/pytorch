49b5104a1579737ca067c1f1e41e57a882c44059
Will Feng <willfeng@fb.com>
2019-04-11 15:14:24 -0400

    improve comments

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -140,46 +140,39 @@ struct C10_API AutogradMetaInterface {
 
 // NOTE [ Version Counter Sharing ]
 //
-// Every Tensor has a version counter. Version counters are incremented
-// whenever the data or shape of a tensor changes through Variable operations.
-// These are typically in-place operations. Version counters are used to
-// detect modifications to saved variables which would result in incorrect
-// gradient calculations. Version counters may be shared between Variables:
+// Every Tensor has a version counter. Version counters are incremented whenever the
+// data or size of a tensor changes through in-place Variable operations. Version
+// counters are used to detect modifications to saved variables which would result in
+// incorrect gradient calculations. Version counters may be shared between Variables:
 //
 // 1. A view shares the version counter of the base Variable,
-// 2. Detached variables share the version counter of the source,
+// 2. `x.detach()` shares the version counter of `x`,
 // 3. Unpacked saved variables share the version counter of the source.
 //
-// Version counters are not shared when:
+// Version counters are not shared in these scenarios:
 //
-// 1. We replace a `Variable`'s underlying `Tensor` by calling `set_data(...)`.
+// 1. When we replace a `Variable`'s underlying `Tensor` by calling `set_data(...)`,
+// 2. `x.data` does not share the version counter of `x`. (See discussion at
+// https://github.com/pytorch/pytorch/issues/5396)
 //
-// Question 1: Why do we not increment the version counter in non-Variable
-// operations?
-//
-// Answer: We explicitly don't increment the version counter in non-Variable
-// operations, because it is a useful escape hatch when we want to change the
-// content of a Variable without invalidating it in the autograd graph.
-//
-// Question 2: Why do we put the version counter in TensorImpl instead of AutogradMeta?
+// Question: Why do we put the version counter in TensorImpl instead of AutogradMeta?
 //
 // Answer: After the Variable/Tensor merge, a tensor will not have AutogradMeta when
 // its `requires_grad_` is false, but when we use this tensor in the forward pass of
 // a function that requires saving this tensor for backward, we need to keep track of
 // this tensor's version to make sure it's always valid in the autograd graph.
 //
-// One logical way to achieve this goal is to initialize AutogradMeta and create the
-// version counter for the non-requires-grad tensor only when it's saved for backward.
-// However, since saving a tensor for backward happens in the forward pass, and our
-// invariant is that forward pass needs to be thread-safe, lazy-initializing AutogradMeta
+// To achieve this goal, we put the version counter in TensorImpl instead of AutogradMeta,
+// and have it always be available. This allows us to have the optimization of not
+// carrying AutogradMeta when a tensor doesn't require gradient.
+//
+// A hypothetical alternative way to achieve this goal is to initialize AutogradMeta and
+// create the version counter for the non-requires-grad tensor only when it's saved for
+// backward. However, since saving a tensor for backward happens in the forward pass, and
+// our invariant is that forward pass needs to be thread-safe, lazy-initializing AutogradMeta
 // when saving a tensor can introduce race conditions when we are running the forward
 // pass in multi-thread scenarios, thus making the forward pass not thread-safe anymore,
 // which breaks the invariant.
-//
-// Instead, we choose to put the version counter in TensorImpl instead of AutogradMeta,
-// and have it always be available. This allows us to have the optimization of not
-// carrying AutogradMeta when a tensor doesn't require gradient, and also keeps the
-// forward pass thread-safe.
 struct C10_API VariableVersion {
  public:
   // NOTE: As of C++11 and 14, default-constructing a std::atomic variable
