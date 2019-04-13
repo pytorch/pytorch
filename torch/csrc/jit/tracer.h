d1496eec911b9f31bfd770821971ae414936e3f0
Sacha <xsacha@gmail.com>
2019-04-14 00:57:12 +1000

    Resolve inconsistent dll linkage warnings on MSVC

--- torch/csrc/jit/tracer.h
+++ torch/csrc/jit/tracer.h
@@ -131,14 +131,14 @@ TORCH_API void addInputs(
     const std::unordered_map<K, V>& value);
 
 template<typename T>
-void addInputs(
+TORCH_API void addInputs(
     Node* n,
     const char* name,
     const std::vector<T>& value) {
   AT_ERROR("Tracing a list of arbitrary type is currently not supported!");
 }
 template<typename K, typename V>
-void addInputs(
+TORCH_API void addInputs(
     Node* n,
     const char* name,
     const std::unordered_map<K, V>& value) {
@@ -146,7 +146,7 @@ void addInputs(
 }
 
 template <size_t N>
-void addInputs(Node* n, const char* name, std::array<bool, N> value) {
+TORCH_API void addInputs(Node* n, const char* name, std::array<bool, N> value) {
   throw std::runtime_error(
       "Found an unsupported argument type in the JIT tracer. File a bug report.");
 }
