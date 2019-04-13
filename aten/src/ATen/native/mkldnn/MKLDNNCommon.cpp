a2a77f90189ddbb1275f9573e55762a8b86adc37
Will Feng <willfeng@fb.com>
2019-04-12 22:38:15 -0400

    fix

--- aten/src/ATen/native/mkldnn/MKLDNNCommon.cpp
+++ aten/src/ATen/native/mkldnn/MKLDNNCommon.cpp
@@ -78,7 +78,6 @@ Tensor new_with_sizes_mkldnn(IntArrayRef sizes, const TensorOptions& options) {
 ideep::tensor& itensor_from_mkldnn(const MKLDNNTensor& mkldnn_tensor) {
   AT_ASSERTM(mkldnn_tensor.type_id() == MkldnnCPUTensorId(),
              "mkldnn_to_dense expects MKL-DNN tensor input");
-  AT_ASSERTM(!mkldnn_tensor.is_variable(), "_internal_get_MKLDNNImpl: should not be a variable");
   MKLDNNTensorImpl *mklimpl = static_cast<MKLDNNTensorImpl *>(mkldnn_tensor.unsafeGetTensorImpl());
   return mklimpl->unsafe_opaque_handle()->get_target();
 }
