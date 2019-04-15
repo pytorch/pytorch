f4153e97410d2d80014d2bb09ce5a28d8c084401
Lara <lahaidar@microsoft.com>
2019-04-15 11:24:59 -0700

    add test_strip_doc_string

--- test/onnx/test_utility_funs.py
+++ test/onnx/test_utility_funs.py
@@ -4,7 +4,10 @@ from test_pytorch_common import TestCase, run_tests
 import torch
 import torch.onnx
 
+import onnx
+
 import io
+import copy
 
 
 class TestUtilityFuns(TestCase):
@@ -24,6 +27,27 @@ class TestUtilityFuns(TestCase):
             torch.onnx.export(MyModule(), x, f)
         except ValueError:
             self.assertFalse(torch.onnx.is_in_onnx_export())
+
+    def test_strip_doc_string(self):
+        class MyModule(torch.nn.Module):
+            def forward(self, input):
+                return torch.exp(input)
+        x = torch.randn(3, 4)
+
+        def is_model_stripped(f, strip_doc_string=None):
+            if strip_doc_string is None:
+                torch.onnx.export(MyModule(), x, f)
+            else:
+                torch.onnx.export(MyModule(), x, f, strip_doc_string=strip_doc_string)
+            model = onnx.load(io.BytesIO(f.getvalue()))
+            model_strip = copy.copy(model)
+            onnx.helper.strip_doc_string(model_strip)
+            return model == model_strip
+
+        # test strip_doc_string=True (default)
+        self.assertTrue(is_model_stripped(io.BytesIO()))
+        # test strip_doc_string=False
+        self.assertFalse(is_model_stripped(io.BytesIO(), False))
 
 
 if __name__ == '__main__':
