f6d12b250ec27fe76834863651fb6931e4f6164c
Karl Ostmo <kostmo@gmail.com>
2019-04-16 12:59:27 -0700

    invoke NN smoketests from a python loop instead of a batch file (#18756)
    Summary:
    I tried first to convert the `.bat` script to a Bash `.sh` script, but I got this error:
    ```
    [...]/build/win_tmp/ci_scripts/test_python_nn.sh: line 3: fg: no job control
    ```
    Line 3 was where `%TMP_DIR%/ci_scripts/setup_pytorch_env.bat` was invoked.
    
    I found a potential workaround on stack overflow of adding the `monitor` (`-m`) flag to the script, but hat didn't work either:
    
    ```
    00:58:00 /bin/bash: cannot set terminal process group (3568): Inappropriate ioctl for device
    00:58:00 /bin/bash: no job control in this shell
    00:58:00 + %TMP_DIR%/ci_scripts/setup_pytorch_env.bat
    00:58:00 /c/Jenkins/workspace/pytorch-builds/pytorch-win-ws2016-cuda9-cudnn7-py3-test1/build/win_tmp/ci_scripts/test_python_nn.sh: line 3: fg: no job control
    ```
    
    So instead I decided to use Python to replace the `.bat` script.  I believe this is an improvement in that it's both "table-driven" now and cross-platform.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18756
    
    Differential Revision: D14957570
    
    Pulled By: kostmo
    
    fbshipit-source-id: 87794e64b56ffacbde4fd44938045f9f68f7bc2a

--- .jenkins/pytorch/win-test-helpers/test_python_nn.bat
+++ .jenkins/pytorch/win-test-helpers/test_python_nn.bat
@@ -1,29 +1,16 @@
 call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
 
+
+pushd test
+
 echo Some smoke tests
-cd test
-
-echo Checking that caffe2.python is available
-python -c "from caffe2.python import core"
+python %SCRIPT_HELPERS_DIR%\run_python_nn_smoketests.py
 if ERRORLEVEL 1 exit /b 1
-
-echo Checking that MKL is available
-python -c "import torch; exit(0 if torch.backends.mkl.is_available() else 1)"
-if ERRORLEVEL 1 exit /b 1
-
-echo Checking that CUDA archs are setup correctly
-python -c "import torch; torch.randn([3,5]).cuda()"
-if ERRORLEVEL 1 exit /b 1
-
-echo Checking that magma is available
-python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
-if ERRORLEVEL 1 exit /b 1
-
-echo Checking that CuDNN is available
-python -c "import torch; exit(0 if torch.backends.cudnn.is_available() else 1)"
-if ERRORLEVEL 1 exit /b 1
-
-cd ..
 
 echo Run nn tests
-cd test && python run_test.py --include nn --verbose && cd ..
+python run_test.py --include nn --verbose
+if ERRORLEVEL 1 exit /b 1
+
+popd
+
+
