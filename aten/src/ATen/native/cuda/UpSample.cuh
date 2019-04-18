b0d8bbcc6a5c540b54abadc837aefd4c72cda4f1
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-04-17 20:14:00 -0400

    Porting in progress

--- aten/src/ATen/native/cuda/UpSample.cuh
+++ aten/src/ATen/native/cuda/UpSample.cuh
@@ -1,10 +1,22 @@
-#include <math.h>
-
 #include <ATen/ATen.h>
 #include <ATen/TensorUtils.h>
+#include <ATen/cuda/CUDAApplyUtils.cuh>
+
+#include <math.h>
 
 namespace at {
 namespace native {
+
+/* TODO: move this to a common place */
+template <typename scalar_t>
+__device__ inline scalar_t min(scalar_t a, scalar_t b) {
+  return ((a) < (b)) ? (a) : (b);
+}
+
+template <typename scalar_t>
+__device__ inline scalar_t max(scalar_t a, scalar_t b) {
+  return ((a) > (b)) ? (a) : (b);
+}
 
 static inline void upsample_1d_shape_check(
     const Tensor& input,
@@ -15,7 +27,7 @@ static inline void upsample_1d_shape_check(
     int64_t output_width) {
   AT_CHECK(
       input_width > 0 && output_width > 0,
-      "Input and output sizes should be greater than 0, but got input (W: ",
+      "input and output sizes should be greater than 0, but got input (W: ",
       input_width,
       ") and output (W: ",
       output_width,
@@ -24,7 +36,7 @@ static inline void upsample_1d_shape_check(
   if (input.defined()) {
     AT_CHECK(
         input.numel() != 0 && input.dim() == 3,
-        "Non-empty 3D data tensor expected but got a tensor with sizes ",
+        "non-empty 3D input tensor expected but got a tensor with sizes ",
         input.sizes());
   } else if (grad_output.defined()) {
     check_dim_size(grad_output, 3, 0, nbatch);
@@ -45,7 +57,7 @@ static inline void upsample_2d_shape_check(
   AT_CHECK(
       input_height > 0 && input_width > 0 && output_height > 0 &&
           output_width > 0,
-      "Input and output sizes should be greater than 0,"
+      "input and output sizes should be greater than 0,"
       " but got input (H: ",
       input_height,
       ", W: ",
@@ -59,7 +71,7 @@ static inline void upsample_2d_shape_check(
   if (input.defined()) {
     AT_CHECK(
         input.numel() != 0 && input.dim() == 4,
-        "Non-empty 4D data tensor expected but got a tensor with sizes ",
+        "non-empty 4D input tensor expected but got a tensor with sizes ",
         input.sizes());
   } else if (grad_output.defined()) {
     check_dim_size(grad_output, 4, 0, nbatch);
@@ -111,128 +123,121 @@ static inline void upsample_3d_shape_check(
   }
 }
 
-template <typename scalar_t>
-static inline scalar_t area_pixel_compute_scale(
+template <typename accscalar_t>
+__host__ __forceinline__ static accscalar_t area_pixel_compute_scale(
     int64_t input_size,
     int64_t output_size,
     bool align_corners) {
-  /* We view each pixel as an area, idx + 0.5 as its center index.
-   * Here is an example formula in 1D case.
-   * if align_corners: center of two corner pixel areas are preserved,
-   *     (0.5, 0.5) -> (0.5, 0.5),
-   *     (input_size - 0.5, 0.5) -> (output_size - 0.5)
-   *     scale = (input_size - 0.5 - 0.5) / (output_size - 0.5 - 0.5)
-   *     src_index + 0.5 - 0.5 = scale * (dst_index + 0.5 - 0.5)
-   * if not align_corners: the whole range is scaled accordingly
-   *     scale = input_size / output_size
-   *     src_idx + 0.5 = scale * (dst_index + 0.5)
-   */
   if (output_size > 1) {
-    return align_corners
-        ? static_cast<scalar_t>(input_size - 1) / (output_size - 1)
-        : static_cast<scalar_t>(input_size) / output_size;
+    return align_corners ? (accscalar_t)(input_size - 1) / (output_size - 1)
+                         : (accscalar_t)input_size / output_size;
   } else {
-    return scalar_t(0);
+    return static_cast<accscalar_t>(0);
   }
 }
 
-template <typename scalar_t>
-static inline scalar_t area_pixel_compute_source_index(
-    scalar_t scale,
+template <typename accscalar_t>
+__device__ __forceinline__ static accscalar_t area_pixel_compute_source_index(
+    accscalar_t scale,
     int64_t dst_index,
     bool align_corners,
     bool cubic) {
   if (align_corners) {
     return scale * dst_index;
   } else {
-    scalar_t src_idx = scale * (dst_index + 0.5) - 0.5;
-    // [Note] Follow Opencv resize logic:
-    // We allow negative src_idx here and later will use
-    //   dx = src_idx - floorf(src_idx)
-    // to compute the "distance"(which affects weights).
-    // For linear modes, weight distribution doesn't matter
-    // for negative indices as they use 2 pixels to interpolate.
-    // For example, [-1, 0], they both use pixel 0 value so it
-    // doesn't affect if we bound the src_idx to 0 or not.
-    // TODO: Our current linear mode impls use unbound indices
-    // where we should and then remove this cubic flag.
-    // This matters in cubic mode, as we might need [-1, 0, 1, 2]
-    // to interpolate and the weights can be affected.
-    return (!cubic && src_idx < 0) ? scalar_t(0) : src_idx;
+    accscalar_t src_idx = scale * (dst_index + static_cast<accscalar_t>(0.5)) -
+        static_cast<accscalar_t>(0.5);
+    // See Note[Follow Opencv resize logic]
+    return (!cubic && src_idx < static_cast<accscalar_t>(0))
+        ? static_cast<accscalar_t>(0)
+        : src_idx;
   }
 }
 
-static inline int64_t nearest_neighbor_compute_source_index(
+__device__ __forceinline__ static int64_t nearest_neighbor_compute_source_index(
     const float scale,
     int64_t dst_index,
     int64_t input_size) {
-  const int64_t src_index =
-      std::min(static_cast<int64_t>(floorf(dst_index * scale)), input_size - 1);
+  const int64_t src_index = min<int64_t>(
+      static_cast<int64_t>(floorf(dst_index * scale)), input_size - 1);
   return src_index;
 }
 
 template <typename scalar_t>
-static scalar_t upsample_get_value_bounded(
-    scalar_t* data,
+__device__ __forceinline__ static scalar_t upsampling_get_value_bounded(
+    const PackedTensorAccessor<scalar_t, 4> data,
+    int64_t channel,
+    int64_t batch,
     int64_t width,
     int64_t height,
     int64_t x,
     int64_t y) {
-  int64_t access_x = std::max(std::min(x, width - 1), static_cast<int64_t>(0));
-  int64_t access_y = std::max(std::min(y, height - 1), static_cast<int64_t>(0));
-  return data[access_y * width + access_x];
+  int64_t access_x =
+      max<int64_t>(min<int64_t>(x, width - 1), static_cast<int64_t>(0));
+  int64_t access_y =
+      max<int64_t>(min<int64_t>(y, height - 1), static_cast<int64_t>(0));
+  return data[batch][channel][access_y][access_x];
 }
 
-template <typename scalar_t>
-static void upsample_increment_value_bounded(
-    scalar_t* data,
+template <typename scalar_t, typename accscalar_t>
+__device__ __forceinline__ static void upsampling_increment_value_bounded(
+    PackedTensorAccessor<scalar_t, 4> data,
+    int64_t channel,
+    int64_t batch,
     int64_t width,
     int64_t height,
     int64_t x,
     int64_t y,
-    scalar_t value) {
-  int64_t access_x = std::max(std::min(x, width - 1), static_cast<int64_t>(0));
-  int64_t access_y = std::max(std::min(y, height - 1), static_cast<int64_t>(0));
-  data[access_y * width + access_x] += value;
+    accscalar_t value) {
+  int64_t access_x =
+      max<int64_t>(min<int64_t>(x, width - 1), static_cast<int64_t>(0));
+  int64_t access_y =
+      max<int64_t>(min<int64_t>(y, height - 1), static_cast<int64_t>(0));
+  atomicAdd(
+      &data[batch][channel][access_y][access_x], static_cast<scalar_t>(value));
 }
 
 // Based on
 // https://en.wikipedia.org/wiki/Bicubic_interpolation#Bicubic_convolution_algorithm
-template <typename scalar_t>
-static inline scalar_t cubic_convolution1(scalar_t x, scalar_t A) {
+template <typename accscalar_t>
+__device__ __forceinline__ static accscalar_t cubic_convolution1(
+    accscalar_t x,
+    accscalar_t A) {
   return ((A + 2) * x - (A + 3)) * x * x + 1;
 }
 
-template <typename scalar_t>
-static inline scalar_t cubic_convolution2(scalar_t x, scalar_t A) {
+template <typename accscalar_t>
+__device__ __forceinline__ static accscalar_t cubic_convolution2(
+    accscalar_t x,
+    accscalar_t A) {
   return ((A * x - 5 * A) * x + 8 * A) * x - 4 * A;
 }
 
-template <typename scalar_t>
-static inline void get_cubic_upsample_coefficients(
-    scalar_t coeffs[4],
-    scalar_t t) {
-  scalar_t A = -0.75;
+template <typename accscalar_t>
+__device__ __forceinline__ static void get_cubic_upsampling_coefficients(
+    accscalar_t coeffs[4],
+    accscalar_t t) {
+  accscalar_t A = -0.75;
 
-  scalar_t x1 = t;
-  coeffs[0] = cubic_convolution2<scalar_t>(x1 + 1.0, A);
-  coeffs[1] = cubic_convolution1<scalar_t>(x1, A);
+  accscalar_t x1 = t;
+  coeffs[0] = cubic_convolution2<accscalar_t>(x1 + 1.0, A);
+  coeffs[1] = cubic_convolution1<accscalar_t>(x1, A);
 
   // opposite coefficients
-  scalar_t x2 = 1.0 - t;
-  coeffs[2] = cubic_convolution1<scalar_t>(x2, A);
-  coeffs[3] = cubic_convolution2<scalar_t>(x2 + 1.0, A);
+  accscalar_t x2 = 1.0 - t;
+  coeffs[2] = cubic_convolution1<accscalar_t>(x2, A);
+  coeffs[3] = cubic_convolution2<accscalar_t>(x2 + 1.0, A);
 }
 
-template <typename scalar_t>
-static inline scalar_t cubic_interp1d(
+template <typename scalar_t, typename accscalar_t>
+__device__ __forceinline__ static accscalar_t cubic_interp1d(
     scalar_t x0,
     scalar_t x1,
     scalar_t x2,
     scalar_t x3,
-    scalar_t t) {
-  scalar_t coeffs[4];
-  get_cubic_upsample_coefficients<scalar_t>(coeffs, t);
+    accscalar_t t) {
+  accscalar_t coeffs[4];
+  get_cubic_upsampling_coefficients<accscalar_t>(coeffs, t);
 
   return x0 * coeffs[0] + x1 * coeffs[1] + x2 * coeffs[2] + x3 * coeffs[3];
 }
