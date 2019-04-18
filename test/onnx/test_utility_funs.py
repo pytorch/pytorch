a64cce326ffd923373fbe129d3296cc232019ff2
Spandan Tiwari <sptiwari@microsoft.com>
2019-04-18 00:06:59 -0700

    Add constant folding to ONNX graph during export (Resubmission) (#18698)
    Summary:
    Rewritten version of https://github.com/pytorch/pytorch/pull/17771 using graph C++ APIs.
    
    This PR adds the ability to do constant folding on ONNX graphs during PT->ONNX export. This is done mainly to optimize the graph and make it leaner. The two attached snapshots show a multiple-node LSTM model before and after constant folding.
    A couple of notes:
    1. Constant folding is by default turned off for now. The goal is to turn it on by default once we have validated it through all the tests.
    2. Support for folding in nested blocks is not in place, but will be added in the future, if needed.
    
    **Original Model:**
    ![multiple_lstm_original](https://user-images.githubusercontent.com/23646532/53987630-6ac53980-40d6-11e9-9702-1ccfee124a83.JPG)
    **Constant-folded model:**
    ![multiple_lstm_constant_folded](https://user-images.githubusercontent.com/23646532/53987632-6c8efd00-40d6-11e9-81c5-362c16f68861.JPG)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18698
    
    Differential Revision: D14889768
    
    Pulled By: houseroad
    
    fbshipit-source-id: b6616b1011de9668f7c4317c880cb8ad4c7b631a

--- test/onnx/test_utility_funs.py
+++ test/onnx/test_utility_funs.py
@@ -3,6 +3,8 @@ from test_pytorch_common import TestCase, run_tests
 
 import torch
 import torch.onnx
+from torch.onnx import utils
+from torch.onnx.symbolic import _set_opset_version
 
 import io
 
@@ -25,6 +27,115 @@ class TestUtilityFuns(TestCase):
         except ValueError:
             self.assertFalse(torch.onnx.is_in_onnx_export())
 
+    def test_constant_fold_transpose(self):
+        class TransposeModule(torch.nn.Module):
+            def forward(self, x):
+                a = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
+                b = torch.transpose(a, 1, 0)
+                return b + x
+
+        _set_opset_version(9)
+        x = torch.ones(3, 2)
+        graph, _, __ = utils._model_to_graph(TransposeModule(), (x, ), None,
+                                             do_constant_folding=True,
+                                             _disable_torch_constant_prop=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Transpose"
+            assert node.kind() != "onnx::Cast"
+            assert node.kind() != "onnx::Constant"
+        assert len(list(graph.nodes())) == 1
+
+    def test_constant_fold_slice(self):
+        class SliceModule(torch.nn.Module):
+            def forward(self, x):
+                a = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
+                b = torch.narrow(a, 0, 0, 1)
+                return b + x
+
+        _set_opset_version(9)
+        x = torch.ones(1, 3)
+        graph, _, __ = utils._model_to_graph(SliceModule(), (x, ), None,
+                                             do_constant_folding=True,
+                                             _disable_torch_constant_prop=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Slice"
+            assert node.kind() != "onnx::Cast"
+            assert node.kind() != "onnx::Constant"
+        assert len(list(graph.nodes())) == 1
+
+    def test_constant_fold_unsqueeze(self):
+        class UnsqueezeModule(torch.nn.Module):
+            def forward(self, x):
+                a = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
+                b = torch.unsqueeze(a, 0)
+                return b + x
+
+        _set_opset_version(9)
+        x = torch.ones(1, 2, 3)
+        graph, _, __ = utils._model_to_graph(UnsqueezeModule(), (x, ), None,
+                                             do_constant_folding=True,
+                                             _disable_torch_constant_prop=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Unsqueeeze"
+            assert node.kind() != "onnx::Cast"
+            assert node.kind() != "onnx::Constant"
+        assert len(list(graph.nodes())) == 1
+
+    def test_constant_fold_concat(self):
+        class ConcatModule(torch.nn.Module):
+            def forward(self, x):
+                a = torch.tensor([[1., 2., 3.]])
+                b = torch.tensor([[4., 5., 6.]])
+                c = torch.cat((a, b), 0)
+                return b + c
+
+        _set_opset_version(9)
+        x = torch.ones(2, 3)
+        graph, _, __ = utils._model_to_graph(ConcatModule(), (x, ), None,
+                                             do_constant_folding=True,
+                                             _disable_torch_constant_prop=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Concat"
+            assert node.kind() != "onnx::Cast"
+            assert node.kind() != "onnx::Constant"
+        assert len(list(graph.nodes())) == 1
+
+    def test_constant_fold_lstm(self):
+        class GruNet(torch.nn.Module):
+            def __init__(self):
+                super(GruNet, self).__init__()
+                self.mygru = torch.nn.GRU(7, 3, 1, bidirectional=False)
+
+            def forward(self, input, initial_state):
+                return self.mygru(input, initial_state)
+
+        _set_opset_version(9)
+        input = torch.randn(5, 3, 7)
+        h0 = torch.randn(1, 3, 3)
+        graph, _, __ = utils._model_to_graph(GruNet(), (input, h0), None,
+                                             do_constant_folding=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Slice"
+            assert node.kind() != "onnx::Concat"
+            assert node.kind() != "onnx::Unsqueeze"
+        assert len(list(graph.nodes())) == 3
+
+    def test_constant_fold_transpose_matmul(self):
+        class MatMulNet(torch.nn.Module):
+            def __init__(self):
+                super(MatMulNet, self).__init__()
+                self.B = torch.nn.Parameter(torch.ones(5, 3))
+
+            def forward(self, A):
+                return torch.matmul(A, torch.transpose(self.B, -1, -2))
+
+        _set_opset_version(9)
+        A = torch.randn(2, 3)
+        graph, _, __ = utils._model_to_graph(MatMulNet(), (A), None,
+                                             do_constant_folding=True)
+        for node in graph.nodes():
+            assert node.kind() != "onnx::Transpose"
+        assert len(list(graph.nodes())) == 1
 
 if __name__ == '__main__':
     run_tests()
