41dd1e3ad214c48dd023e69722f928f6010617eb
Sebastian Messmer <messmer@fb.com>
2019-04-18 02:00:49 -0700

    Use string based schema for exposing caffe2 ops (#19287)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19287
    
    Since we now have a string-schema-based op registration API, we can also use it when exposing caffe2 operators.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D14931925
    
    fbshipit-source-id: ec162469d2d94965e8c99d431c801ae7c43849c8

--- caffe2/operators/layer_norm_op.cc
+++ caffe2/operators/layer_norm_op.cc
@@ -186,13 +186,7 @@ to the end.)
 
 C10_REGISTER_CAFFE2_OPERATOR_CPU(
     LayerNorm,
-    (std::vector<c10::Argument>{
-        c10::Argument("input"),
-        c10::Argument("axis", c10::IntType::get()),
-        c10::Argument("epsilon", c10::FloatType::get())}),
-    (std::vector<c10::Argument>{c10::Argument("output"),
-                                c10::Argument("mean"),
-                                c10::Argument("stdev")}),
+    "_caffe2::LayerNorm(Tensor input, int axis, float epsilon) -> (Tensor output, Tensor mean, Tensor stdev)",
     caffe2::LayerNormOp<caffe2::CPUContext>)
 
 namespace caffe2 {
