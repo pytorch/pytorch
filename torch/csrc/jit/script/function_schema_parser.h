d5d264f5b58f842453c3d39ed792c75734c66548
Sebastian Messmer <messmer@fb.com>
2019-04-18 00:57:44 -0700

    Move function schema parser to ATen/core build target (#19282)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19282
    
    This is largely a hack because we need to use the function schema parser from ATen/core
    but aren't clear yet on how the final software architecture should look like.
    
    - Add function schema parser files from jit to ATen/core build target.
    - Also move ATen/core build target one directory up to allow this.
    
    We only change the build targets and don't move the files yet because this is likely
    not the final build set up and we want to avoid repeated interruptions
    for other developers. cc zdevito
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D14931922
    
    fbshipit-source-id: 26462e2e7aec9e0964706138edd3d87a83b964e3

--- torch/csrc/jit/script/function_schema_parser.h
+++ torch/csrc/jit/script/function_schema_parser.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <ATen/core/function_schema.h>
-#include <torch/csrc/WindowsTorchApiMacro.h>
+#include <ATen/core/Macros.h>
 #include <string>
 
 namespace torch {
 namespace jit {
 
-TORCH_API ::c10::FunctionSchema parseSchema(const std::string& schema);
+CAFFE2_API ::c10::FunctionSchema parseSchema(const std::string& schema);
 
 } // namespace jit
 } // namespace torch
