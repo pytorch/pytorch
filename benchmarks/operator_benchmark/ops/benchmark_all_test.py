8e68f487735222cb8f9b1b53fe5e64870a0bf1d1
Mingzhe Li <mingzhe0908@fb.com>
2019-04-19 15:41:31 -0700

    fix AI-PEP path error
    Differential Revision: D15018499
    
    fbshipit-source-id: 67f51780333130e94a2946d8ff743bf62a554103

--- benchmarks/operator_benchmark/ops/benchmark_all_test.py
+++ benchmarks/operator_benchmark/ops/benchmark_all_test.py
@@ -3,14 +3,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import importlib
-import os
 from benchmarks.operator_benchmark import benchmark_runner
+from benchmarks.operator_benchmark.ops import ( # noqa
+    add_test, # noqa
+    matmul_test) # noqa
+
 
 if __name__ == "__main__":
-    # TODO: current way of importing other tests are fragile, so we need to have a robust way
-    for module in os.listdir(os.path.dirname(__file__)):
-        if module == '__init__.py' or not module.endswith('_test.py'):
-            continue
-        importlib.import_module("benchmarks.operator_benchmark.ops." + module[:-3])
     benchmark_runner.main()
