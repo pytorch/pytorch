cdc6de289b9de4efbc81d53f7f06e3b401f39c8d
eellison <elias_ellison@brown.edu>
2019-04-19 16:04:01 -0700

    First step at container aliasing (#18710)
    Summary:
    First step at allowing container types within alias analysis.
    
    Since the current implementation hides the concept of Wildcards within alias analysis and does not expose it to memory dag, we cannot represent whether a container type holds a wildcard. As a result, only handle TupleConstruct, where we can directly inspect if any input values are wildcards, and don't handle nested containers.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18710
    
    Differential Revision: D15017068
    
    Pulled By: eellison
    
    fbshipit-source-id: 3ee76a5482cef1cc4a10f034593ca21019161c18

--- torch/csrc/jit/passes/alias_analysis.h
+++ torch/csrc/jit/passes/alias_analysis.h
@@ -45,6 +45,10 @@ class AliasDb {
   bool writesToAlias(Node* n, const ValueSet& vs, bool recurseBlocks = false)
       const;
 
+  // Does `a` and `b` potentially share a memory location or do either
+  // hold in memory any element that exists in the other
+  bool mayContainAlias(const Value* a, const Value* b) const;
+
   // Do `a` and `b` potentially share a memory location?
   bool mayAlias(const Value* a, const Value* b) const;
   // Do any values in group `a` potentially share a memory location with any
@@ -165,6 +169,10 @@ class AliasDb {
   // Returns nullopt if there are no wildcard nodes
   c10::optional<const Node*> getLastWildcard() const;
 
+  // Is the element a wildcard or an unhandled container type,
+  // or does the element contain an element for which that's true
+  bool cannotCheckAliasContainment(const Value* elem) const;
+
   /**
    * Special analysis methods
    */
@@ -182,6 +190,7 @@ class AliasDb {
   void analyzeFork(Node* node);
   void analyzeWait(Node* node);
   void analyzeSetAttr(Node* node);
+  void analyzeTupleConstruct(Node* node);
   void analyzeCustomOp(Node* node);
 
   /**
@@ -189,11 +198,16 @@ class AliasDb {
    */
   void makeAllAlias(const std::vector<Value*>& values);
   void makePointerTo(const Value* value, const Value* to);
+  void addToContainedElements(const Value* element, const Value* container);
   void mapAliases(at::ArrayRef<Value*> to, at::ArrayRef<Value*> from);
   void giveFreshAlias(const Value* value);
+  Element* getOrCreateElement(const Value* value);
 
   static bool shouldAnnotate(const Value* v);
   static bool shouldAnnotate(const TypePtr& type);
+
+  static bool isContainerType(const TypePtr& type);
+
   bool hasUsesAfter(Symbol alias, const Node* n) const;
   bool isBeforeSameGraph(const Node* lhs, const Node* rhs) const;
 
