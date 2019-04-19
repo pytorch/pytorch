3b2848a5f487bdae47e4280471e287f8eff214ab
Jerry Zhang <jerryzh168@gmail.com>
2019-04-18 19:05:07 -0700

    V21: (no description)
    Differential Revision: D13739657
    Differential Version: 80045872

--- aten/src/ATen/native/quantized/cpu/fake_quantize.cpp
+++ aten/src/ATen/native/quantized/cpu/fake_quantize.cpp
@@ -5,92 +5,43 @@
 
 namespace at { namespace native {
 namespace {
-namespace utils {
-// Borrowed from tensorflow/core/kernels/fake_quant_ops_functor.h.
-// Gymnastics with nudged zero point is to ensure that real zero maps to
-// an integer, which is required for e.g. zero-padding in convolutional layers.
-// Outputs nudged_min, nudged_max, nudged_scale.
-void nudge(
-    const double min,
-    const double max,
-    const long quant_min,
-    const long quant_max,
-    double* nudged_min,
-    double* nudged_max,
-    double* scale) {
-  const double quant_min_double = static_cast<double>(quant_min);
-  const double quant_max_double = static_cast<double>(quant_max);
-  *scale = (max - min) / (quant_max_double - quant_min_double);
-  const double zero_point_from_min = quant_min_double - min / *scale;
-
-  const int32_t nudged_zero_point = [zero_point_from_min,
-                                     quant_min,
-                                     quant_min_double,
-                                     quant_max,
-                                     quant_max_double] {
-    if (zero_point_from_min < quant_min_double) {
-      return static_cast<int32_t>(quant_min);
-    }
-    if (zero_point_from_min > quant_max_double) {
-      return static_cast<int32_t>(quant_max);
-    }
-    return static_cast<int32_t>(std::round(zero_point_from_min));
-  }();
-  *nudged_min = (quant_min_double - nudged_zero_point) * (*scale);
-  *nudged_max = (quant_max_double - nudged_zero_point) * (*scale);
-}
-}  // namespace utils
-
 /* Fake-quantizes the 'inputs' tensor.
 Args:
   X: Forward input tensor.
   dY: Backward input tensor (_backward op only).
-  min, max: Minimum and maximum for the clamping range.
+  scale: scale of per tensor affine quantization
+  zero_point: zero_point of per tensor affine quantization
   num_bits: Number of quantization bits.
   quant_delay: Count of global steps for which to delay the quantization.
                See note below.
   iter: The current quantization iteration used for `quant_delay`.
-  narrow_range: Use "narrow quantization range". See note below.
 Returns:
   Quantized tensor (double dtype).
 
 Notes:
   - quant_delay might be set to non-zero to help weights stabilize in the
     beginning of the training.
-  - narrow quantization range is [1, 2^bits - 1];
-    wide quantization range [0, 2^bits - 1]
+  - quantization range [0, 2^bits - 1]
 */
 
-/********* Begin MinMaxArgs ops (forward and backward). *********/
-class FakeQuantizeMinMaxArgsOp_forward : public c10::OperatorKernel {
+/********* Begin FakeQuant ops (forward and backward). *********/
+class FakeQuantizeOp_forward : public c10::OperatorKernel {
  public:
   at::Tensor operator()(
       at::Tensor X,
-      double min,
-      double max,
+      double scale,
+      int64_t zero_point,
       int64_t num_bits = 8,
       int64_t quant_delay = 0,
-      int64_t iter = 0,
-      bool narrow_range = false
+      int64_t iter = 0
     ) {
     // Sanity checks.
     if (num_bits > 32 || num_bits < 1) {
       throw std::invalid_argument("`num_bits` should be in the [1, 32] range.");
     }
-    if (min > 0.0f || max < 0.0f || min >= max) {
-      throw std::invalid_argument("`min`/`max` are malformed.");
-    }
     if (quant_delay < 0) {
       throw std::invalid_argument("`quant_delay` must be a positive integer.");
     }
-
-    // Prepare args.
-    long quant_min = int(narrow_range);
-    long quant_max = (1 << num_bits) - 1;
-
-    double nudge_min = 0.0, nudge_max = 0.0, nudge_scale;
-    utils::nudge(min, max, quant_min, quant_max,
-                 &nudge_min, &nudge_max, &nudge_scale);
 
     if (quant_delay != 0 && iter < 0) {
       throw std::invalid_argument(
@@ -104,9 +55,11 @@ class FakeQuantizeMinMaxArgsOp_forward : public c10::OperatorKernel {
       return Y;
     }
 
-    double inv_nudge_scale = 1.0f / nudge_scale;
-    Y = ((X.clamp_min(nudge_min).clamp_max(nudge_max) - nudge_min) *
-         inv_nudge_scale + 0.5f).floor() * nudge_scale + nudge_min;
+    double inv_scale = 1.0f / scale;
+    const auto quant_min = 0;
+    const auto quant_max = (1 << num_bits) - 1;
+    Y = (((X * inv_scale + 0.5f).floor() + zero_point)
+      .clamp_min(quant_min).clamp_max(quant_max) - zero_point) * scale;
     return Y;
   }
 };
@@ -116,38 +69,33 @@ class FakeQuantizeMinMaxArgsOp_forward : public c10::OperatorKernel {
 Args:
   X: Forward input tensor.
   dY: Backward input tensor.
-  min, max: Minimum and maximum for the clamping range.
+  scale: scale of per tensor affine quantization
+  zero_point: zero_point of per tensor affine quantization
   num_bits: Number of quantization bits.
   quant_delay: Count of global steps for which to delay the quantization.
                See note in forward.
   iter: The current quantization iteration used for `quant_delay`.
-  narrow_range: Use "narrow quantization range". See notes in forward.
 Returns:
   Quantized tensor (double dtype).
 
 Notes:
   - quant_delay might be set to non-zero to help weights stabilize in the
     beginning of the training.
-  - narrow quantization range is [1, 2^bits - 1];
-    wide quantization range [0, 2^bits - 1]
+  - quantization range [0, 2^bits - 1]
 */
-class FakeQuantizeMinMaxArgsOp_backward : public c10::OperatorKernel {
+class FakeQuantizeOp_backward : public c10::OperatorKernel {
  public:
   at::Tensor operator()(
       at::Tensor X,
       at::Tensor dY,
-      double min,
-      double max,
+      double scale,
+      int64_t zero_point,
       int64_t num_bits = 8,
       int64_t quant_delay = 0,
-      int64_t iter = 0,
-      bool narrow_range = false) {
+      int64_t iter = 0) {
     // Sanity checks.
     if (num_bits > 32 || num_bits < 1) {
       throw std::invalid_argument("`num_bits` should be in the [1, 32] range.");
-    }
-    if (min > 0.0f || max < 0.0f || min >= max) {
-      throw std::invalid_argument("`min`/`max` are malformed.");
     }
     if (quant_delay < 0) {
       throw std::invalid_argument("`quant_delay` must be a positive integer.");
@@ -158,14 +106,6 @@ class FakeQuantizeMinMaxArgsOp_backward : public c10::OperatorKernel {
     if (X.numel() != dY.numel()) {
       throw std::invalid_argument("`X` and `dY` are not the same size");
     }
-
-    // Prepare args.
-    const auto quant_min = int(narrow_range);
-    const auto quant_max = (1 << num_bits) - 1;
-
-    double nudge_min = 0.0, nudge_max = 0.0, nudge_scale;
-    utils::nudge(min, max, quant_min, quant_max,
-                 &nudge_min, &nudge_max, &nudge_scale);
 
     if (quant_delay != 0 && iter < 0) {
       throw std::invalid_argument(
@@ -178,46 +118,45 @@ class FakeQuantizeMinMaxArgsOp_backward : public c10::OperatorKernel {
       return dX;
     }
 
-    at::Tensor mask_min = (X >= nudge_min);
-    at::Tensor mask_max = (X <= nudge_max);
+    double inv_scale = 1.0f / scale;
+    const auto quant_min = 0;
+    const auto quant_max = (1 << num_bits) - 1;
+    at::Tensor Xq = (X * inv_scale + 0.5).floor() + zero_point;
+    at::Tensor mask_min = (Xq >= quant_min);
+    at::Tensor mask_max = (Xq <= quant_max);
     at::Tensor mask = mask_min * mask_max;
     dX = mask.type_as(dY) * dY;
-
     return dX;
   }
 };
 
 static auto registry = c10::RegisterOperators()
 .op(c10::FunctionSchema(
-      "quantized::fake_quantize_minmax_forward",
+      "quantized::fake_quantize_forward",
       "",
       {{"X", TensorType::get()},
-       {"min", FloatType::get()},
-       {"max", FloatType::get()},
-       {"num_bits", IntType::get(), /*N=*/c10::nullopt, /*defaul_valuet=*/8},
+       {"scale", FloatType::get()},
+       {"zero_point", IntType::get()},
+       {"num_bits", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/8},
        {"quant_delay", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0},
-       {"iter", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0},
-       {"narrow_range", BoolType::get(), /*N=*/c10::nullopt,
-                                         /*default_value=*/false}},
+       {"iter", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0}},
       {{"Y", TensorType::get()}}),
-  c10::kernel<FakeQuantizeMinMaxArgsOp_forward>(),
+  c10::kernel<FakeQuantizeOp_forward>(),
   c10::dispatchKey(CPUTensorId()))
 .op(c10::FunctionSchema(
-      "quantized::fake_quantize_minmax_backward",
+      "quantized::fake_quantize_backward",
       "",
       {{"X", TensorType::get()},
        {"dY", TensorType::get()},
-       {"min", FloatType::get()},
-       {"max", FloatType::get()},
-       {"num_bits", IntType::get(), /*N=*/c10::nullopt, /*defaul_valuet=*/8},
+       {"scale", FloatType::get()},
+       {"zero_point", IntType::get()},
+       {"num_bits", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/8},
        {"quant_delay", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0},
-       {"iter", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0},
-       {"narrow_range", BoolType::get(), /*N=*/c10::nullopt,
-                                         /*default_value=*/false}},
+       {"iter", IntType::get(), /*N=*/c10::nullopt, /*default_value=*/0}},
       {{"Y", TensorType::get()}}),
-  c10::kernel<FakeQuantizeMinMaxArgsOp_backward>(),
+  c10::kernel<FakeQuantizeOp_backward>(),
   c10::dispatchKey(CPUTensorId()));
-/********* End MinMaxArgs ops (forward and backward). *********/
+/********* End FakeQuant ops (forward and backward). *********/
 
 }  // namespace
 }}  // namespace at::native
--- test/test_quantized.py
+++ test/test_quantized.py
@@ -7,58 +7,26 @@ import unittest
 # from caffe2.python import core
 from common_utils import TestCase, run_tests
 
-
 def canonical(graph):
     return str(torch._C._jit_pass_canonicalize(graph))
 
 
-# Equivalent of the `nudge` method in the `::fake_quantize_ops.cpp`.
-def _nudge(min, max, num_bits, narrow_range):
-    quant_min = float(narrow_range)
-    quant_max = float(1 << num_bits) - 1.0
-    scale = (max - min) / (quant_max - quant_min)
-
-    zero_point_from_min = np.round(quant_min - min / scale)
-    nudged_zero_point = np.int(
-        np.fmin(
-            np.fmax(zero_point_from_min, quant_min),
-            quant_max))
-    nudged_min = (quant_min - nudged_zero_point) * scale
-    nudged_max = (quant_max - nudged_zero_point) * scale
-
-    return (nudged_min, nudged_max, scale)
-
-
 # Reference method for quantizing a tensor.
-def _quantize_min_max_args_reference(X, min, max, num_bits, narrow_range):
-    nudge_min, nudge_max, scale = _nudge(min, max, num_bits, narrow_range)
-    res = (np.round((np.clip(X, nudge_min, nudge_max) - nudge_min) / scale)
-           * scale + nudge_min)
+def _fake_quantize_reference(X, scale, zero_point, num_bits):
+    quant_min, quant_max = 0, 2 ** num_bits - 1
+    res = (np.clip(np.round(X / scale) + zero_point, quant_min, quant_max) - zero_point) * scale
     res = res.reshape(X.shape)
     return res
 
 
 # Reference method for the gradient of the quantizer.
-def _quantize_min_max_args_grad_reference(X, dY, min, max, num_bits, narrow_range):
-    nudge_min, nudge_max, scale = _nudge(min, max, num_bits, narrow_range)
-    mask = np.logical_and(X >= nudge_min, X <= nudge_max)
+def _fake_quantize_grad_reference(X, dY, scale, zero_point, num_bits):
+    Xq = np.round(X / scale) + zero_point
+    quant_min, quant_max = 0, 2 ** num_bits - 1
+    mask = np.logical_and(Xq >= quant_min, Xq <= quant_max)
     res = dY[mask].reshape(dY.shape)
     return res
 
-
-# Adjusts the input if too close to min or max.
-def _adjust_input_tensor(X, min, max, epsilon=1e-3):
-    def adjust(x):
-        if abs(x - min) < epsilon:
-            return min
-        if abs(x - max) < epsilon:
-            return max
-        return x
-
-    return np.array(
-        [adjust(x) for x in X.flatten()],
-        dtype=np.float32
-    ).reshape(X.shape)
 
 @unittest.skip("Skipping due to the protobuf dependency in the CI's")
 # @unittest.skipIf("Relu_ENGINE_DNNLOWP" not in core._REGISTERED_OPERATORS, "fbgemm-based Caffe2 ops are not linked")
@@ -114,47 +82,40 @@ class TestQuantizedRelu(unittest.TestCase):
         np.testing.assert_equal(Y_tensor, Y_hat_tensor)
 
 
-class TestFakeQuantizeMinMax(unittest.TestCase):
+class TestFakeQuantize(unittest.TestCase):
     """Tests the forward path of the fake quantization minMax args op."""
     def test_forward(self):
-        fake_quantize_minmax_args_forward = torch.ops.quantized.fake_quantize_minmax_forward
+        fake_quantize_forward = torch.ops.quantized.fake_quantize_forward
 
-        min = -10
-        max = 10
+        scale = 3
+        zero_point = 2
         num_bits = 8
-        narrow_range = False
-        X = np.random.rand(20, 20) * (max - min) + min
-        X = _adjust_input_tensor(X, min, max)
-        Y = _quantize_min_max_args_reference(X, min, max, num_bits,
-                                             narrow_range)
+        X = np.random.rand(20, 20) * 125
+        Y = _fake_quantize_reference(X, scale, zero_point, num_bits)
         X_torch = torch.from_numpy(X)
-        Y_prime = fake_quantize_minmax_args_forward(
-            X=X_torch, min=min, max=max, num_bits=num_bits,
-            quant_delay=0, iter=0, narrow_range=narrow_range)
+        Y_prime = fake_quantize_forward(
+            X=X_torch, scale=scale, zero_point=zero_point, num_bits=num_bits,
+            quant_delay=0, iter=0)
         tolerance = 1e-6
         np.testing.assert_allclose(Y, Y_prime, rtol=tolerance, atol=tolerance)
 
     """Tests the backward method. Note that this runs the reference quantization
     and thus the errors might be originating there."""
     def test_backward(self):
-        fake_quantize_minmax_args_backward = torch.ops.quantized.fake_quantize_minmax_backward
+        fake_quantize_backward = torch.ops.quantized.fake_quantize_backward
 
-        min = -10
-        max = 10
+        scale = 3
+        zero_point = 2
         num_bits = 8
-        narrow_range = False
-        X = np.random.rand(2, 2) * (max - min) + min
-        X = _adjust_input_tensor(X, min, max)
-        Y = _quantize_min_max_args_reference(X, min, max, num_bits,
-                                             narrow_range)
+        X = np.random.rand(20, 20) * 125
+        Y = _fake_quantize_reference(X, scale, zero_point, num_bits)
         dY = Y - X  # Fake gradient
-        dX = _quantize_min_max_args_grad_reference(X, dY, min, max, num_bits,
-                                                   narrow_range)
+        dX = _fake_quantize_grad_reference(X, dY, scale, zero_point, num_bits)
         X_torch = torch.from_numpy(X)
         dY_torch = torch.from_numpy(dY)
-        dX_prime = fake_quantize_minmax_args_backward(
-            X=X_torch, dY=dY_torch, min=min, max=max,
-            num_bits=num_bits, quant_delay=0, iter=0, narrow_range=narrow_range)
+        dX_prime = fake_quantize_backward(
+            X=X_torch, dY=dY_torch, scale=scale, zero_point=zero_point,
+            num_bits=num_bits, quant_delay=0, iter=0)
         tolerance = 1e-6
         np.testing.assert_allclose(dX, dX_prime, rtol=tolerance, atol=tolerance)
 
