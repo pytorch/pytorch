597127c4c5db0ad49f024a25d907fa0753a49a70
Mikhail Zolotukhin <mvz@fb.com>
2019-04-19 16:29:02 -0700

    Add minimalistic implementation of subgraph matcher. (#19322)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19322
    ghimport-source-id: 93c713f829d1b2a9aa5d104cb1f30148dd37c967
    
    Differential Revision: D14962182
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 3989fba06502011bed9c24f12648d0baa2a4480c

--- test/cpp/jit/test.cpp
+++ test/cpp/jit/test.cpp
@@ -25,6 +25,7 @@
 #include <test/cpp/jit/test_misc.h>
 #include <test/cpp/jit/test_netdef_converter.h>
 #include <test/cpp/jit/test_peephole_optimize.h>
+#include <test/cpp/jit/test_subgraph_matcher.h>
 #include <test/cpp/jit/test_subgraph_utils.h>
 
 using namespace torch::jit::script;
@@ -70,6 +71,7 @@ namespace jit {
   _(Profiler)                      \
   _(PeepholeOptimize)              \
   _(RecordFunction)                \
+  _(SubgraphMatching)              \
   _(ModuleDefine)
 
 #define TH_FORALL_TESTS_CUDA(_) \
