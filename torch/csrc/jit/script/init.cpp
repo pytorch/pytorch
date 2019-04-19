3c1a64175083f028988a54c810a0301e0fb23749
Michael Suo <suo@fb.com>
2019-04-19 12:48:39 -0700

    add resolveType to Resolver (#19237)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19237
    ghimport-source-id: 70777ec37155be37efef1b743d564752e4dff9de
    
    Differential Revision: D14928289
    
    Reviewed By: zdevito
    
    Pulled By: suo
    
    fbshipit-source-id: 46827da9ace16730669fc654bf781d83172d18b1

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -656,6 +656,10 @@ struct PythonResolver : public Resolver {
     return toSugaredValue(obj, m, loc);
   }
 
+  TypePtr resolveType(const std::string& name) const override {
+    return ClassType::get(name);
+  }
+
  private:
   ResolutionCallback rcb_;
 };
