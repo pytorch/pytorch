85606357183882483bcb34b0e9d600c031ab0b94
Stefan Krah <skrah@bytereef.org>
2019-04-21 19:51:37 +0200

    Port adaptive_max_pool3d() to ATen.

--- torch/nn/_functions/thnn/auto.py
+++ torch/nn/_functions/thnn/auto.py
@@ -285,7 +285,6 @@ def _generate_function_classes(scope_dict):
         'VolumetricMaxPooling',
         'VolumetricMaxUnpooling',
         'VolumetricAdaptiveAveragePooling',
-        'VolumetricAdaptiveMaxPooling',
         'VolumetricConvolution',
         'VolumetricFullConvolution',
         'VolumetricConvolutionMM',
