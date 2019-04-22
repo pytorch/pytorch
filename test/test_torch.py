89bdc7f0372699b7a6e410d0262e08414562caa2
Jerry Zhang <jerryzh168@gmail.com>
2019-04-22 13:30:17 -0700

    V2: Initial commit
    Differential Revision: D15008160
    Differential Version: 80386400

--- test/test_torch.py
+++ test/test_torch.py
@@ -2680,9 +2680,9 @@ class _TestTorchMixin(object):
         self.assertEqual(qr.item(), 1)
         self.assertEqual(qr[0].item(), 1)
         # assignment
-        # This calls _th_fill_
-        # qr[0] = 8 # float asignment
-        # self.assertEqual(qr.item(), 8)
+        self.assertTrue(qr[0].is_quantized)
+        qr[0] = 11.3 # float asignment
+        self.assertEqual(qr.item(), 11)
 
     def test_qtensor_quant_dequant(self):
         r = np.random.rand(3, 2) * 2 - 4
