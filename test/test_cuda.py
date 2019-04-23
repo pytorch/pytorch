20a76202841d571eaceea0f57ef31fed1909bf1a
SsnL <tongzhou.wang.1994@gmail.com>
2019-04-23 07:51:31 -0700

    add torch.cuda.synchronize(device=None) (#19573)
    Summary:
    fixes https://github.com/pytorch/pytorch/issues/19509
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19573
    
    Differential Revision: D15045730
    
    Pulled By: ezyang
    
    fbshipit-source-id: 732721b4b360fc4348ca7c87d4cd1386e7651bdd

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1497,6 +1497,21 @@ class TestCuda(TestCase):
 
     def test_cuda_synchronize(self):
         torch.cuda.synchronize()
+        torch.cuda.synchronize('cuda')
+        torch.cuda.synchronize('cuda:0')
+        torch.cuda.synchronize(0)
+        torch.cuda.synchronize(torch.device('cuda:0'))
+
+        if TEST_MULTIGPU:
+            torch.cuda.synchronize('cuda:1')
+            torch.cuda.synchronize(1)
+            torch.cuda.synchronize(torch.device('cuda:1'))
+
+        with self.assertRaisesRegex(ValueError, "Expected a cuda device, but"):
+            torch.cuda.synchronize(torch.device("cpu"))
+
+        with self.assertRaisesRegex(ValueError, "Expected a cuda device, but"):
+            torch.cuda.synchronize("cpu")
 
     @unittest.skipIf(not TEST_MULTIGPU, "detected only one GPU")
     def test_current_stream(self):
