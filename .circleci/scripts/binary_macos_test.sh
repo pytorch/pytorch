b55dfd565b8264e1cd46bf9a1550761453163000
Karl Ostmo <kostmo@gmail.com>
2019-04-24 10:05:07 -0700

    GCC ABI variants for nightly builds (#18888)
    Summary:
    closes #17492
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18888
    
    Differential Revision: D15065093
    
    Pulled By: pjh5
    
    fbshipit-source-id: 6abeabf68b91106fc8ae9df238f6a40613d40b57

---
+++ .circleci/scripts/binary_macos_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ex
+
+source "/Users/distiller/project/env"
+export "PATH=$workdir/miniconda/bin:$PATH"
+pkg="$workdir/final_pkgs/$(ls $workdir/final_pkgs)"
+
+# Don't test libtorch
+# TODO we should test libtorch
+if [[ "$PACKAGE_TYPE" == libtorch ]]; then
+  exit 0
+fi
+
+# Create a new test env
+# TODO cut all this out into a separate test job and have an entirely different
+# miniconda
+source deactivate || true
+conda create -qyn test python="$DESIRED_PYTHON"
+source activate test >/dev/null
+
+# Install the package
+if [[ "$PACKAGE_TYPE" == conda ]]; then
+  conda install -y "$pkg" --offline
+else
+  pip install "$pkg" --no-index --no-dependencies -v
+fi
+
+# Test
+pushd "$workdir/pytorch"
+$workdir/builder/run_tests.sh "$PACKAGE_TYPE" "$DESIRED_PYTHON" "$DESIRED_CUDA"
