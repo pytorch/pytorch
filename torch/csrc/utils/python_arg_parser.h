8f11bb788337e52e90a0f5564623b8d3cda394f4
Vitaly Fedyunin <vitalyf@fb.com>
2019-04-24 15:31:46 -0700

    Add torch.from_file function similar to the Storage.from_file, but returning tensor (#18688)
    Summary:
    Porting `torch.Storage.from_file(filename, shared, size)` function to `torch.from_file(filename, shared, size, dtype=torch.int)`
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18688
    
    Differential Revision: D15012644
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: 3f62ca9e414fad3847fe71b785ff97b5bdc2d2cd

--- torch/csrc/utils/python_arg_parser.h
+++ torch/csrc/utils/python_arg_parser.h
@@ -129,6 +129,7 @@ struct PythonArgs {
   inline c10::optional<at::ScalarType> scalartypeOptional(int i);
   inline c10::optional<at::Scalar> scalarOptional(int i);
   inline c10::optional<int64_t> toInt64Optional(int i);
+  inline c10::optional<bool> toBoolOptional(int i);
   inline const THPLayout& layout(int i);
   inline const THPLayout& layoutWithDefault(int i, const THPLayout& default_layout);
   inline at::Device device(int i);
@@ -411,6 +412,13 @@ inline c10::optional<int64_t> PythonArgs::toInt64Optional(int i) {
   if (!args[i])
     return c10::nullopt;
   return toInt64(i);
+}
+
+inline c10::optional<bool> PythonArgs::toBoolOptional(int i) {
+  if (!args[i]) {
+    return c10::nullopt;
+  }
+  return toBool(i);
 }
 
 inline double PythonArgs::toDouble(int i) {
