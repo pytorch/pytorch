d39621af31deae04ee1d1326e164c2b5c2f16b38
Tongzhou Wang <SsnL@users.noreply.github.com>
2019-04-25 14:20:07 -0400

    lint

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -2657,10 +2657,10 @@ def grid_sample(input, grid, mode='bilinear', padding_mode='zeros'):
     ``output[n, :, d, h, w]``. :attr:`mode` argument specifies ``nearest`` or
     ``bilinear`` interpolation method to sample the input pixels.
 
-    :attr:`grid` specifies the sampling pixel locations normalized by the 
-    :attr:`input` spatial dimensions. Therefore, it should have most values in 
-    the range of ``[-1, 1]``. For example, values ``x = -1, y = -1`` is the 
-    left-top pixel of :attr:`input`, and values  ``x = 1, y = 1`` is the 
+    :attr:`grid` specifies the sampling pixel locations normalized by the
+    :attr:`input` spatial dimensions. Therefore, it should have most values in
+    the range of ``[-1, 1]``. For example, values ``x = -1, y = -1`` is the
+    left-top pixel of :attr:`input`, and values  ``x = 1, y = 1`` is the
     right-bottom pixel of :attr:`input`.
 
     If :attr:`grid` has values outside the range of ``[-1, 1]``, the corresponding
@@ -2669,9 +2669,9 @@ def grid_sample(input, grid, mode='bilinear', padding_mode='zeros'):
         * ``padding_mode="zeros"``: use ``0`` for out-of-bound grid locations,
         * ``padding_mode="border"``: use border values for out-of-bound grid locations,
         * ``padding_mode="reflection"``: use values at locations reflected by
-          the border for out-of-bound grid locations. For location far away 
-          from the border, it will keep being reflected until becoming in bound, 
-          e.g., (normalized) pixel location ``x = -3.5`` reflects by border ``-1`` 
+          the border for out-of-bound grid locations. For location far away
+          from the border, it will keep being reflected until becoming in bound,
+          e.g., (normalized) pixel location ``x = -3.5`` reflects by border ``-1``
           and becomes ``x' = 1.5``, then reflects by border ``1`` and becomes
           ``x'' = -0.5``.
 
