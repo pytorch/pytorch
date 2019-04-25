9c102302fce876d122db5fd6e900c42a56984864
Sebastian Messmer <messmer@fb.com>
2019-04-25 14:50:45 -0700

    V1: Initial commit
    Differential Revision: D15090318
    Differential Version: 80756357

--- aten/src/ATen/core/dispatch/Operator.h
+++ aten/src/ATen/core/dispatch/Operator.h
@@ -9,10 +9,6 @@ namespace impl {
 // This is a private class used inside the Dispatcher to represent an operator
 // and it's dispatch table. This is not part of the public API.
 class Operator final {
-private:
-  struct Kernels final {
-    DispatchTable dispatchTable;
-  };
 public:
   explicit Operator(FunctionSchema&& schema);
 
@@ -26,8 +22,8 @@ public:
   }
 
   DispatchTableEntry lookupKernel(const Stack* stack) const {
-    return kernels_.read([&] (const Kernels& kernels) {
-      return kernels.dispatchTable.lookup(stack);
+    return dispatchTable_.read([&] (const DispatchTable& dispatchTable) {
+      return dispatchTable.lookup(stack);
     });
   }
 
@@ -37,11 +33,38 @@ public:
   RegistrationHandleRAII registerFallbackKernel(DispatchTableEntry kernel);
 
 private:
-  void deregisterKernel_(TensorTypeId dispatch_key);
+  void deregisterKernel_(TensorTypeId dispatch_key, std::list<DispatchTableEntry>::iterator kernel);
   void deregisterFallbackKernel_();
 
   FunctionSchema schema_;
-  LeftRight<Kernels> kernels_;
+
+  // The dispatchTable stores the current kernel for each dispatch key
+  LeftRight<DispatchTable> dispatchTable_;
+
+  // The kernels map stores all registered kernels for a certain dispatch key.
+  // If an operator library gets loaded that overwrites already existing kernels,
+  // both kernels will be in that list but only the newer one will be in
+  // dispatchTable. If any of the kernels go away (say the library gets
+  // unloaded), we remove the kernel from this list and update the
+  // dispatchTable if necessary.
+  // Kernels in the list are ordered by registration time descendingly,
+  // newer registrations are before older registrations.
+  // We do not combine dispatchTable and kernels into one hash map because
+  // kernels is a larger data structure and accessed quite infrequently
+  // while dispatchTable is accessed often and should be kept small to fit
+  // into CPU caches.
+  // Invariants:
+  //  - dispatchTable[dispatch_key] == kernels[dispatch_key].front()
+  //  - dispatchTable[dispatch_key] does not exist if and only if
+  //    kernels[dispatch_key] does not exist
+  //  - If kernels[dispatch_key] exists, then it has elements.
+  //    It is never an empty list.
+  ska::flat_hash_map<TensorTypeId, std::list<DispatchTableEntry>> kernels_;
+  std::mutex kernelsMutex_;
+
+  // This function re-establishes the invariant that dispatchTable
+  // contains the front element from the kernels list for a given dispatch key.
+  void updateDispatchTable_(TensorTypeId dispatch_key);
 };
 
 }
