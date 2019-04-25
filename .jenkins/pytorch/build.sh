dbcb428095f33134a7b9ee837c5e23f868a091b7
Jiakai Liu <liujiakai@fb.com>
2019-04-25 11:49:12 -0700

    [pytorch] CMakeLists changes to enable libtorch for Android
    Summary:
    Stole from https://github.com/pytorch/pytorch/pull/16242
    Only include CMakeLists changes to make it easer for reviewers.
    
    Change pytorch android CI job to build libtorch instead of caffe2 -
    turns out it's as simple as adding a few cmake macros. Will properly
    update tools/build_pytorch_libs.py in subsequent diffs.
    
    Test Plan:
    - Make sure pytorch android CI job succeeds;
    - Test libtorch.so on Android device;

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -65,10 +65,13 @@ fi
 if [[ "${BUILD_ENVIRONMENT}" == *-android* ]]; then
   export ANDROID_NDK=/opt/ndk
   build_args=()
-  build_args+=("-DBUILD_BINARY=ON")
-  build_args+=("-DBUILD_TEST=ON")
-  build_args+=("-DUSE_OBSERVERS=ON")
-  build_args+=("-DUSE_ZSTD=ON")
+  build_args+=("-DBUILD_TORCH=ON")
+  build_args+=("-DBUILD_TORCH_MOBILE=ON")
+  build_args+=("-DBUILD_CAFFE2_OPS=OFF")
+  build_args+=("-DUSE_DISTRIBUTED=OFF")
+  build_args+=("-DCMAKE_PREFIX_PATH=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')")
+  build_args+=("-DPYTHON_EXECUTABLE=$(python -c 'import sys; print(sys.executable)')")
+  build_args+=("-DPYTHON_INCLUDE_DIR=$(python -c 'from distutils.sysconfig import get_python_inc; print(get_python_inc())')")
   exec ./scripts/build_android.sh "${build_args[@]}" "$@"
 fi
 
