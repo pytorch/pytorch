44e1d82c29418b182ee91b53055e5cca38524d89
Karl Ostmo <kostmo@gmail.com>
2019-04-26 01:26:49 -0700

    C++ changes toward libtorch and libcaffe2 unification (#19554)
    Summary:
    * adds TORCH_API and AT_CUDA_API in places
    * refactor code generation Python logic to separate
      caffe2/torch outputs
    * fix hip and asan
    * remove profiler_cuda from hip
    * fix gcc warnings for enums
    * Fix PythonOp::Kind
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19554
    
    Differential Revision: D15082727
    
    Pulled By: kostmo
    
    fbshipit-source-id: 83a8a99717f025ab44b29608848928d76b3147a4

--- torch/csrc/jit/ir.h
+++ torch/csrc/jit/ir.h
@@ -231,7 +231,7 @@ struct Value {
   TORCH_API Value* copyMetadata(Value* from);
 };
 
-struct Node {
+struct TORCH_API Node {
   TH_DISALLOW_COPY_AND_ASSIGN(Node);
   friend struct Graph;
   friend struct Block;
@@ -259,7 +259,7 @@ struct Node {
   topo_position_t topo_position_ = 0;
 
  protected:
-  TORCH_API Node(Graph* graph_, NodeKind kind_); // defined after graph
+  Node(Graph* graph_, NodeKind kind_); // defined after graph
  public:
   // each node but Return/Param
   // is associated with exactly one place in the node list...
@@ -358,7 +358,7 @@ struct Node {
     return false;
   }
 
-  TORCH_API void replaceAllUsesWith(Node* n);
+  void replaceAllUsesWith(Node* n);
 
   // lots of things like chunk have a single input or single output, so we have
   // a helper to make accessing it easier
@@ -399,10 +399,10 @@ struct Node {
   bool is_constant(Symbol name) const {
     return static_cast<bool>(get(name));
   }
-  TORCH_API bool mustBeNone() const;
+  bool mustBeNone() const;
 
-  TORCH_API bool isNondeterministic() const;
-  TORCH_API bool hasSideEffects() const;
+  bool isNondeterministic() const;
+  bool hasSideEffects() const;
 
   // Graphs
 
@@ -424,11 +424,11 @@ struct Node {
   // Given:   %3 = f(%1, %2)
   // Execute: %3.addInput(%4)
   // Result:  %3 = f(%1, %2, %4)
-  TORCH_API Value* addInput(Value* value);
+  Value* addInput(Value* value);
 
   // Add 'value' as an input to 'this' at the specified position in the
   // arguments. Returns the added value for ease of chaining.
-  TORCH_API Value* insertInput(size_t i, Value* value);
+  Value* insertInput(size_t i, Value* value);
 
   // Replace the input of 'this' at position 'i' with
   // 'newValue', returning the old node.
@@ -436,7 +436,7 @@ struct Node {
   // Given:   %3 = f(%1, %2)
   // Execute: %3.replaceInput(1, %4)
   // Result:  %3 = f(%1, %4)
-  TORCH_API Value* replaceInput(size_t i, Value* newValue);
+  Value* replaceInput(size_t i, Value* newValue);
 
   // Replace all occurrences of 'from' in the inputs of this
   // node with 'to'. Corresponds to llvm's replaceUsesOfWith.
@@ -444,16 +444,16 @@ struct Node {
   // Given:   %3 = f(%1, %2, %1)
   // Execute: %3.replaceInputWith(%1, %4)
   // Result:  %3 = f(%4, %2, %4)
-  TORCH_API void replaceInputWith(Value* from, Value* to);
+  void replaceInputWith(Value* from, Value* to);
 
-  TORCH_API Value* addOutput();
+  Value* addOutput();
 
-  TORCH_API Value* insertOutput(size_t i);
+  Value* insertOutput(size_t i);
 
-  TORCH_API void eraseOutput(size_t i);
+  void eraseOutput(size_t i);
 
-  TORCH_API Block* addBlock();
-  TORCH_API void eraseBlock(size_t i);
+  Block* addBlock();
+  void eraseBlock(size_t i);
 
   // Each Node can have a list of subblocks. These are used to define structured
   // nested control flow operators such as If and Loop.
@@ -482,10 +482,10 @@ struct Node {
   }
 
   // Is 'this' before 'n' in the topological order?
-  TORCH_API bool isBefore(const Node* n) const;
+  bool isBefore(const Node* n) const;
 
   // Is 'this' after 'n' in the topological order?
-  TORCH_API bool isAfter(const Node* n) const;
+  bool isAfter(const Node* n) const;
 
   // Insert unattached 'this' node before 'n' in the topological order.
   // Returns this (for chaining).
@@ -497,7 +497,7 @@ struct Node {
   // Result:  %3 = f(%1, %2)
   //          %5 = h(%1)
   //          %4 = g(%3)
-  TORCH_API Node* insertBefore(Node* n);
+  Node* insertBefore(Node* n);
 
   // Insert unattached 'this' node after 'n' in the topological order.
   // Returns this (for chaining).
@@ -509,7 +509,7 @@ struct Node {
   // Result:  %3 = f(%1, %2)
   //          %4 = g(%3)
   //          %5 = h(%1)
-  TORCH_API Node* insertAfter(Node* n);
+  Node* insertAfter(Node* n);
 
   // Move 'this' (already in the graph) after 'n' in the topological order.
   //
@@ -522,7 +522,7 @@ struct Node {
   // Result: %3 = g(%1)
   //         %2 = f(%1)
   //
-  TORCH_API void moveAfter(Node* n);
+  void moveAfter(Node* n);
 
   // Move a node 'n' (already in the graph) before 'this' in the topological
   // order.
@@ -535,7 +535,7 @@ struct Node {
   // Execute: %3.moveBefore(%2)
   // Result: %3 = g(%1)
   //         %2 = f(%1)
-  TORCH_API void moveBefore(Node* n);
+  void moveBefore(Node* n);
 
   // Remove the input at 'i' from this node.
   //
@@ -545,14 +545,14 @@ struct Node {
   // Given: %3 = f(%1, %2)
   // Execute: %3.removeInput(1)
   // Result: %3 = f(%1)
-  TORCH_API void removeInput(size_t i);
+  void removeInput(size_t i);
 
   // Remove all inputs from a node.
   //
   // Given: %3 = f(%1, %2)
   // Execute: %3.removeAllInputs()
   // Result: %3 = f()
-  TORCH_API void removeAllInputs();
+  void removeAllInputs();
 
   // iterators of the node list starting at this node
   // useful for resuming a search starting at this node
@@ -577,7 +577,7 @@ struct Node {
   //        %3 = g(%1)
   // Execute: %2.destroy()
   // Result: %3 = g(%1)
-  TORCH_API void destroy();
+  void destroy();
 
   // Dynamically cast this node to the subclass indicated by the
   // template variable, returning nullptr if the cast is invalid..
@@ -604,11 +604,11 @@ struct Node {
   }
 
   // XXX: this function is meant to be used with string literals only!
-  TORCH_API bool matches(
+  bool matches(
       const char* signature_literal,
       at::ArrayRef<Symbol> const_inputs = {}) const;
 
-  TORCH_API const FunctionSchema& schema() const {
+  const FunctionSchema& schema() const {
     if (!schema_) {
       findSchema();
     }
@@ -782,15 +782,15 @@ struct Node {
   bool isBeforeOrAfter(const Node* n, MoveSide moveSide) const;
 
   std::pair<Value*, const Argument&> findInput(Symbol name);
-  TORCH_API void findSchema() const;
+  void findSchema() const;
   // Lookup iterator in use list of _input i_ that corresponds to its use of
   // _this_
-  TORCH_API use_list::iterator findUseForInput(size_t i);
+  use_list::iterator findUseForInput(size_t i);
 
   // remove the use of input i, this sets input i to nullptr, but
   // is only used internally to Node before setting it to a new value
   // or erasing the entry from the list.
-  TORCH_API Value* dropInput(size_t i);
+  Value* dropInput(size_t i);
 
   bool inBlockList() const {
     if (next() == nullptr) {
@@ -799,8 +799,8 @@ struct Node {
     return next() != nullptr;
   }
 
-  TORCH_API void removeFromList();
-  TORCH_API void lint() const;
+  void removeFromList();
+  void lint() const;
 
   void assignTopoPosition();
 
@@ -818,7 +818,7 @@ struct Node {
   // 'this' will be allocated with s->allocNewInstance(g) so it should have
   // the same concrete type as 's'
   //
-  TORCH_API virtual void cloneFrom(Node* s);
+  virtual void cloneFrom(Node* s);
 };
 
 struct Block {
@@ -1262,9 +1262,7 @@ struct ProfileOp : public Node {
 // which is not included in libtorch.so. We still include some bits and pieces
 // of PythonOp here to enable writing simple passes generically. In general,
 // python-aware bits need to be moved to the descendant classes.
-struct PythonOp : public Node {
-  static constexpr Symbol Kind = ::c10::prim::PythonOp;
-
+struct TORCH_API PythonOp : public Node {
   using Node::Node;
 
   // should this Python function be skipped over when exported (i.e. for
