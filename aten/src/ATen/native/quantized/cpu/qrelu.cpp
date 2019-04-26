02999f3eed906e499b3c742a039abfd08b568e09
Zafar Takhirov <zaf@fb.com>
2019-04-25 17:55:50 -0700

    Use the QTensor with QReLU (#19312)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19312
    
    Replaces the tuple hack with the QTensor. Please, note this can be landed ONLY after #18960 (D14810261) is landed.
    
    Reviewed By: raghuramank100
    
    Differential Revision: D14819460
    
    fbshipit-source-id: 75ca649304b1619cb3cfe845962c9f226b8f884a

--- aten/src/ATen/native/quantized/cpu/qrelu.cpp
+++ aten/src/ATen/native/quantized/cpu/qrelu.cpp
@@ -1,44 +1,35 @@
 #include <ATen/ATen.h>
+#include <ATen/core/Type.h>
 #include <ATen/core/op_registration/op_registration.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/cpu/Loops.h>
+#include <ATen/quantized/Quantizer.h>
 
 #include <algorithm>
-#include <tuple>
 
 namespace at { namespace native {
 namespace {
 
 class QReluInt8 final : public c10::OperatorKernel {
  public:
-  // The use of `double` and `int64_t` is dictated by the
-  // `op_registration/infer_schema.h`.
-  std::tuple<at::Tensor, double, int64_t>
-  operator()(const at::Tensor& x, double scale, int64_t zero_point) {
-    at::Tensor tensor = at::empty_like(x);
-    auto x_contig = x.contiguous();
-    const auto* x_data = x_contig.data<uint8_t>();
-    for (int idx = 0; idx < x.numel(); ++idx) {
-      tensor[idx] = static_cast<uint8_t>(std::max<int>(x_data[idx],
-                                                       zero_point));
-    }
-    return std::make_tuple(tensor, scale, zero_point);
+  Tensor operator()(Tensor qx) {
+    Tensor qy = at::_empty_affine_quantized(qx.sizes(),
+                                            at::device(kCPU).dtype(kQInt8),
+                                            qx.q_scale().toDouble(),
+                                            qx.q_zero_point().toLong());
+    auto iter = TensorIterator::unary_op(qy, qx);
+    const auto zero_point = qx.q_zero_point().toByte();
+    unary_kernel(*iter, [&](c10::qint8 value) -> c10::qint8 {
+      return c10::qint8(std::max(value.val_, zero_point));
+    });
+    return qy;
   }
 };
 
 static auto registry = c10::RegisterOperators().op(
-    // "quant::relu(x: Tensor, scale: float, zero_point: int)
-    //     -> ((Tensor, float, int))"
-    c10::FunctionSchema(
-        "quantized::relu",
-        "",
-        std::vector<c10::Argument>{c10::Argument("x", TensorType::get()),
-                                   c10::Argument("scale", FloatType::get()),
-                                   c10::Argument("zero_point", IntType::get())},
-        std::vector<c10::Argument>{
-          c10::Argument("y", TensorType::get()),
-          c10::Argument("scale_o", FloatType::get()),
-          c10::Argument("zero_point_o", IntType::get())
-        }),
+    "quantized::relu(Tensor qx) -> Tensor",
     c10::kernel<QReluInt8>(),
-    c10::dispatchKey(CPUTensorId()));
+    c10::dispatchKey(QuantizedCPUTensorId()));
+
 } // namespace
 }} // namespace at::native
