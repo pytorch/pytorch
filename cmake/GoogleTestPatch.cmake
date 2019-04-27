0ca1702b0aab9677a24b8bff9fcccc773727440f
peter <peterghost86@gmail.com>
2019-04-28 01:00:38 +0800

    .

--- cmake/GoogleTestPatch.cmake
+++ cmake/GoogleTestPatch.cmake
@@ -2,8 +2,14 @@
 # Usage example:
 #   cmake -DFILENAME=internal_utils.cmake -P GoogleTestPatch.cmake
 
+message("file name: ${FILENAME}")
+
 file(READ ${FILENAME} content)
 
+message("before change: ${content}")
+
 string(REGEX REPLACE "[-/]Z[iI]" "/Z7" ${content} "${content}")
+
+message("after change: ${content}")
 
 file(WRITE ${FILENAME} "${content}")
