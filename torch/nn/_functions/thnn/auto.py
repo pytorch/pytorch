8446965ba4e4c148166f06c730029f64e94aa736
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2019-04-29 10:19:03 +0900

    port Volumetric Adaptive Avg Pooling

--- torch/nn/_functions/thnn/auto.py
+++ torch/nn/_functions/thnn/auto.py
@@ -280,7 +280,6 @@ def _generate_function_classes(scope_dict):
         'SpatialMaxPooling',
         'SpatialDilatedMaxPooling',
         'SpatialMaxUnpooling',
-        'VolumetricAveragePooling',
         'VolumetricMaxPooling',
         'VolumetricMaxUnpooling',
         'VolumetricAdaptiveAveragePooling',
