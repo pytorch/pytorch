128542c9ab4873d5a08a554a9a2cb30a7dd485d9
Lucas Hosseini <hoss@fb.com>
2019-04-30 11:25:43 +0200

    [Do not merge] Testing changes to pytorch/builder.

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -31,7 +31,7 @@ elif [[ -n "$CIRCLE_SHA1" ]]; then
   git reset --hard "$CIRCLE_SHA1"
   git checkout -q -B master
 else
-  # nightly binary builds. These run at 05:05 UTC every day. 
+  # nightly binary builds. These run at 05:05 UTC every day.
   last_commit="$(git rev-list --before "$(date -u +%Y-%m-%d) 05:00" --max-count 1 HEAD)"
   git checkout "$last_commit"
 fi
@@ -41,10 +41,10 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+git clone -q https://github.com/beauby/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 git fetch origin
-git reset origin/master --hard
+git reset origin/fix-conda-rpath2 --hard
 echo "Using builder from "
 git --no-pager log --max-count 1
 popd
