1dfc0315b4ce28dc34038c1033ab05eddc66f1f8
Cheng Cheng <cc6@fb.com>
2019-05-01 10:47:21 -0700

    shape inference for learning rate op
    Summary: Add shape inference for LearningRate op. The output (lr) should have similar shape with input (iteration), but not the same type (float vs int).
    
    Differential Revision: D15112300
    
    fbshipit-source-id: 983a4067c5b59c44e31c5923f5667903d1be47e9

--- caffe2/python/operator_test/shape_inference_test.py
+++ caffe2/python/operator_test/shape_inference_test.py
@@ -539,6 +539,28 @@ class TestShapeInference(test_util.TestCase):
         # TODO: find a tighter bound
         assert(np.allclose(x, x_recovered, atol=1e-2))
 
+    def testLearningRateOp(self):
+        net = core.Net("lr_test")
+        iteration = net.ConstantFill(
+            [],
+            "iteration",
+            shape=[1],
+            value=0,
+            dtype=core.DataType.INT64,
+        )
+        lr = net.LearningRate(
+            [iteration],
+            net.NextScopedBlob("weight_decay"),
+            base_lr=0.5,
+            policy="constantWarmup",
+            multiplier=0.0,
+            num_iter=0,
+        )
+        (shapes, types) = workspace.InferShapesAndTypes(
+            [net],
+        )
+        self.assertEqual(shapes['weight_decay'], [1])
+
     def testShapeOp(self):
         model = model_helper.ModelHelper(name="shape_op_test")
         model.Shape('x', 'y')
@@ -567,6 +589,7 @@ class TestShapeInference(test_util.TestCase):
         correct_types = {}
         for b in workspace.Blobs():
             arr = workspace.FetchBlob(b)
+            print("arr is {}".format(arr))
             correct_shapes[b] = arr.shape
             if type(arr) is np.ndarray:
                 if arr.dtype == np.dtype('float32'):
