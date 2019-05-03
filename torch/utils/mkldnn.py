8ff1a5ddabb45cd75a3e55383e4f7b27b2049d11
Will Feng <willfeng@fb.com>
2019-05-03 14:38:56 -0400

    [WIP] try to fix test_mkldnn

--- torch/utils/mkldnn.py
+++ torch/utils/mkldnn.py
@@ -10,27 +10,29 @@ def to_mkldnn(module):
             return t.to_mkldnn()
 
     def m_fn(m):
+        for key, param in m._parameters.items():
+            if param is not None:
+                # yf225 TODO: explain the change here!
+                m._parameters[key] = t_fn(param)
+                if param._grad is not None:
+                    m._parameters[key]._grad = t_fn(param._grad)
+
+        for key, buf in m._buffers.items():
+            if buf is not None:
+                m._buffers[key] = t_fn(buf)
+
         # TODO: This is a temporary hack to work around the fact that
         # nn.Linear is decomposed into addmm/matmul. Later we will
         # change nn.Linear to directly call aten linear and we can
         # remove this patch
+        # yf225 TODO: add comment: this needs to happen after the conversion for param and buf, because
+        # the conversion can potentially change the values that `m.weight` and `m.bias` point to, which invalidates
+        # the original pointers
         if isinstance(m, torch.nn.Linear):
             m.forward = functools.partial(
                 torch._C._nn.linear,
                 weight=m.weight,
                 bias=m.bias)
-
-        for param in m._parameters.values():
-            if param is not None:
-                # Tensors stored in modules are graph leaves, and we don't
-                # want to create copy nodes, so we have to unpack the data.
-                param.data = t_fn(param.data)
-                if param._grad is not None:
-                    param._grad.data = t_fn(param._grad.data)
-
-        for key, buf in m._buffers.items():
-            if buf is not None:
-                m._buffers[key] = t_fn(buf)
 
         if isinstance(m, torch.nn.Conv2d):
             m.weight.data = torch._C._nn.mkldnn_reorder_conv2d_weight(
