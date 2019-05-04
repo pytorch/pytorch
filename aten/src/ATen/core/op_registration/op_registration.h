281423e090ca064285dca856727095dd973a5f85
Sebastian Messmer <messmer@fb.com>
2019-05-03 18:46:51 -0700

    V8: (no description)
    Differential Revision: D15090322
    Differential Version: 81204207

--- aten/src/ATen/core/op_registration/op_registration.h
+++ aten/src/ATen/core/op_registration/op_registration.h
@@ -42,7 +42,7 @@ public:
   RegisterOperators(const RegisterOperators&) = delete;
   RegisterOperators& operator=(const RegisterOperators&) = delete;
   RegisterOperators(RegisterOperators&&) noexcept;
-  RegisterOperators& operator=(RegisterOperators&&);
+  RegisterOperators& operator=(RegisterOperators&&) noexcept;
 
   /**
    * Register an operator based on a function schema and a set of configuration
