2b565e1d829153cd8c0d3e9b23aa0b0950782795
Sebastian Messmer <messmer@fb.com>
2019-05-05 01:03:21 -0700

    Allow overwriting kernels (#19777)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19777
    
    When used from iPython/Jupyter/Bento, the cell doing kernel registration might be executed multiple times.
    Also, there might be a kernel library that wants to overwrite one of the existing kernels.
    Let's allow this.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D15090318
    
    fbshipit-source-id: 09f842e8fd36646053c5c2f11325de4d31105b0c

--- aten/src/ATen/core/dispatch/OperatorEntry.cpp
+++ aten/src/ATen/core/dispatch/OperatorEntry.cpp
@@ -5,48 +5,72 @@ namespace impl {
 
 OperatorEntry::OperatorEntry(FunctionSchema&& schema)
 : schema_(std::move(schema))
-, kernels_(schema_) {}
+, dispatchTable_(schema_)
+, kernels_() {}
 
 void OperatorEntry::prepareForDeregistration() {
-  return kernels_.read([&] (const Kernels& kernels) {
-    if (!kernels.dispatchTable.isEmpty()) {
+  return dispatchTable_.read([&] (const DispatchTable& dispatchTable) {
+    if (!dispatchTable.isEmpty()) {
       std::ostringstream str;
       str << schema_;
       AT_ERROR("Tried to deregister op schema for an operator that still has kernels registered. The operator schema is ", str.str());
     }
   });
+  AT_ASSERTM(kernels_.size() == 0, "If the dispatch table is empty, then the invariant says there can't be any kernels");
 }
 
 RegistrationHandleRAII OperatorEntry::registerKernel(TensorTypeId dispatch_key, DispatchTableEntry kernel) {
-  kernels_.write([&] (Kernels& kernels) {
-    kernels.dispatchTable.registerKernel(dispatch_key, std::move(kernel));
-  });
+  std::unique_lock<std::mutex> lock(kernelsMutex_);
 
-  return RegistrationHandleRAII([this, dispatch_key] {
-    deregisterKernel_(dispatch_key);
+  // Add the kernel to the kernels list,
+  // possibly creating the list if this is the first kernel.
+  auto& k = kernels_[dispatch_key];
+  k.push_front(kernel);
+  std::list<DispatchTableEntry>::iterator inserted = k.begin();
+  // update the dispatch table, i.e. re-establish the invariant
+  // that the dispatch table points to the newest kernel
+  updateDispatchTable_(dispatch_key);
+
+  return RegistrationHandleRAII([this, dispatch_key, inserted] {
+    // list iterators stay valid even if the list changes,
+    // so we can use the iterator to remove the kernel from the list
+    deregisterKernel_(dispatch_key, inserted);
   });
 }
 
-void OperatorEntry::deregisterKernel_(TensorTypeId dispatch_key) {
-  kernels_.write([&] (Kernels& kernels) {
-    kernels.dispatchTable.deregisterKernel(std::move(dispatch_key));
-  });
+void OperatorEntry::updateDispatchTable_(TensorTypeId dispatch_key) {
+  // precondition: kernelsMutex_ is locked
+
+  auto k = kernels_.find(dispatch_key);
+
+  if (k == kernels_.end()) {
+    dispatchTable_.write([&] (DispatchTable& dispatchTable) {
+      dispatchTable.removeKernelIfExists(dispatch_key);
+    });
+  } else {
+    dispatchTable_.write([&] (DispatchTable& dispatchTable) {
+      dispatchTable.setKernel(dispatch_key, k->second.front());
+    });
+  }
+}
+
+void OperatorEntry::deregisterKernel_(TensorTypeId dispatch_key, std::list<DispatchTableEntry>::iterator kernel) {
+  std::unique_lock<std::mutex> lock(kernelsMutex_);
+
+  auto found = kernels_.find(dispatch_key);
+  AT_ASSERTM(found != kernels_.end(), "Tried to deregister a kernel but there are no kernels registered for this dispatch key.");
+  auto& k = found->second;
+  k.erase(kernel);
+  if (k.empty()) {
+    // the invariant says we don't want empty lists but instead remove the list from the map
+    kernels_.erase(found);
+  }
+
+  updateDispatchTable_(dispatch_key);
 }
 
 RegistrationHandleRAII OperatorEntry::registerFallbackKernel(DispatchTableEntry kernel) {
-  kernels_.write([&] (Kernels& kernels) {
-    kernels.dispatchTable.registerFallbackKernel(std::move(kernel));
-  });
-
-  return RegistrationHandleRAII([this] {
-    deregisterFallbackKernel_();
-  });
-}
-
-void OperatorEntry::deregisterFallbackKernel_() {
-  kernels_.write([&] (Kernels& kernels) {
-    kernels.dispatchTable.deregisterFallbackKernel();
-  });
+  return registerKernel(TensorTypeIds::undefined(), std::move(kernel));
 }
 
 }
