863818e05a80b970e58a9dbce09c114ecbc6879e
Sebastian Messmer <messmer@fb.com>
2019-05-05 01:03:21 -0700

    Allow overwriting kernels (#19777)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19777
    
    When used from iPython/Jupyter/Bento, the cell doing kernel registration might be executed multiple times.
    Also, there might be a kernel library that wants to overwrite one of the existing kernels.
    Let's allow this.
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D15090318
    
    fbshipit-source-id: 09f842e8fd36646053c5c2f11325de4d31105b0c

--- aten/src/ATen/core/dispatch/OperatorEntry.h
+++ aten/src/ATen/core/dispatch/OperatorEntry.h
@@ -2,6 +2,7 @@
 
 #include <ATen/core/dispatch/DispatchTable.h>
 #include <ATen/core/dispatch/RegistrationHandleRAII.h>
+#include <list>
 
 namespace c10 {
 namespace impl {
@@ -9,10 +10,6 @@ namespace impl {
 // This is a private class used inside the Dispatcher to represent an operator
 // and it's dispatch table. This is not part of the public API.
 class OperatorEntry final {
-private:
-  struct Kernels final {
-    DispatchTable dispatchTable;
-  };
 public:
   explicit OperatorEntry(FunctionSchema&& schema);
 
@@ -26,8 +23,8 @@ public:
   }
 
   DispatchTableEntry lookupKernel(const Stack* stack) const {
-    return kernels_.read([&] (const Kernels& kernels) {
-      return kernels.dispatchTable.lookup(stack);
+    return dispatchTable_.read([&] (const DispatchTable& dispatchTable) {
+      return dispatchTable.lookup(stack);
     });
   }
 
@@ -37,11 +34,38 @@ public:
   RegistrationHandleRAII registerFallbackKernel(DispatchTableEntry kernel);
 
 private:
-  void deregisterKernel_(TensorTypeId dispatch_key);
+  void deregisterKernel_(TensorTypeId dispatch_key, std::list<DispatchTableEntry>::iterator kernel);
   void deregisterFallbackKernel_();
 
   FunctionSchema schema_;
-  LeftRight<Kernels> kernels_;
+
+  // The dispatchTable stores the current kernel for each dispatch key
+  LeftRight<DispatchTable> dispatchTable_;
+
+  // The kernels map stores all registered kernels for a certain dispatch key.
+  // If an operator library gets loaded that overwrites already existing kernels,
+  // both kernels will be in that list but only the newer one will be in
+  // dispatchTable. If any of the kernels go away (say the library gets
+  // unloaded), we remove the kernel from this list and update the
+  // dispatchTable if necessary.
+  // Kernels in the list are ordered by registration time descendingly,
+  // newer registrations are before older registrations.
+  // We do not combine dispatchTable and kernels into one hash map because
+  // kernels is a larger data structure and accessed quite infrequently
+  // while dispatchTable is accessed often and should be kept small to fit
+  // into CPU caches.
+  // Invariants:
+  //  - dispatchTable[dispatch_key] == kernels[dispatch_key].front()
+  //  - dispatchTable[dispatch_key] does not exist if and only if
+  //    kernels[dispatch_key] does not exist
+  //  - If kernels[dispatch_key] exists, then it has elements.
+  //    It is never an empty list.
+  ska::flat_hash_map<TensorTypeId, std::list<DispatchTableEntry>> kernels_;
+  std::mutex kernelsMutex_;
+
+  // This function re-establishes the invariant that dispatchTable
+  // contains the front element from the kernels list for a given dispatch key.
+  void updateDispatchTable_(TensorTypeId dispatch_key);
 };
 
 }
