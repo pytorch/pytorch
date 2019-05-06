802b30bbfa82c83e1460b1d6bec2d5248c17a93e
Mikhail Zolotukhin <mvz@fb.com>
2019-05-06 13:11:10 -0700

    Cleanup includes in torch/csrc/* (#19924)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19924
    ghimport-source-id: f7248b16c8e263a7d0ba7975b1fc0b00cb2cf2c0
    
    Differential Revision: D15125018
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 322c7ca53e38ef8b43b5ac5bd747b28bc10379f1

--- torch/csrc/jit/script/builtin_functions.cpp
+++ torch/csrc/jit/script/builtin_functions.cpp
@@ -1,6 +1,7 @@
+#include <torch/csrc/jit/script/builtin_functions.h>
 #include <torch/csrc/api/include/torch/jit.h>
 #include <torch/csrc/jit/code_template.h>
-#include <torch/csrc/jit/script/builtin_functions.h>
+#include <torch/csrc/jit/script/resolver.h>
 
 namespace torch {
 namespace jit {
