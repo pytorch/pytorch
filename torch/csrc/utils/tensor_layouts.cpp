d74892f3137dafebcb406f0759d559e19bfb8446
Jerry Zhang <jerryzh@fb.com>
2019-05-06 12:28:44 -0700

    Add print function for QTensor (#19513)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19513
    
    Add support for printing a QTensor in python frontend
    
    Differential Revision: D15017168
    
    fbshipit-source-id: 312d1f18e6ca3c9eb4a5b8bb1c64f7cc8bc1dcf5

--- torch/csrc/utils/tensor_layouts.cpp
+++ torch/csrc/utils/tensor_layouts.cpp
@@ -25,6 +25,7 @@ void initializeLayouts() {
   registerLayoutObject((THPLayout*)strided_layout, at::Backend::CUDA);
   registerLayoutObject((THPLayout*)strided_layout, at::Backend::MSNPU);
   registerLayoutObject((THPLayout*)strided_layout, at::Backend::XLA);
+  registerLayoutObject((THPLayout*)strided_layout, at::Backend::QuantizedCPU);
 
   PyObject *sparse_coo_layout = THPLayout_New(at::Layout::Sparse, "torch.sparse_coo");
   Py_INCREF(sparse_coo_layout);
