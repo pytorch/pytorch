89de3f45dd015492145068e854e494162e7d31be
Thomas Viehmann <tv.code@beamnet.de>
2019-05-06 14:54:10 -0700

    Specialize Optional[T] to T (or subtype for Tensor) or None when executing graph (#18407)
    Summary:
    This patch specializes `Optional[Tensor]` graph inputs to either a `DimensionedTensorType` (if a Tensor is passed) or `NoneType`. Other `Optional[T]` are specialized to `T` or `None`.
    
    - For unwrapping (checked and unchecked) we need to keep the output type, as IR code that follows unwrapping may not work with NoneType (just as it doesn't deal with Optional). While it would not be hit during execution, it will run against the (legitimate) assumptions of the analysis passes.
    - Function lookup currently will not match NoneType when it expects optional (I'm not entirely sure why this doesn't lead to unhappyness currently, but hey), I amend this at the level of the function matching code (`operator.cpp`), but see Adam's comments. We would run into trouble if we needed to select between functions whose signature only differs in Optional types with different subtypes, but we would have the same problem when calling them directly, so I would think this is OK.
    
    - It would enable throwing away branches we can't hit. This also reduces the "blockyness" of the graph, so it may be easier to apply optimizations (e.g. fuse things in `if t is None: ...` and outside the `if`.
    - Arguments passed into `Optional[Tensor]` arguments will get shape information, which is very handy.
    - It get's rid of the problem that tensors passed into Optional arguments get requires_grad set erroneously #18270 (though that also affects lists, which aren't fixed here).
    - `Optional[List[int]]` is needed for #18697.
    
    - We're changing typing in a more subtle way than the `TensorType`->`DimensionedTensorType`.
    - In particular, specializing to NoneType loses the Type information captured in the `OptionalType` element type.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18407
    
    Reviewed By: zdevito
    
    Differential Revision: D15216808
    
    Pulled By: eellison
    
    fbshipit-source-id: 01f1a7643deaf4962c3f55eff2070d54b0e54b69

--- test/cpp/jit/test.cpp
+++ test/cpp/jit/test.cpp
@@ -80,6 +80,7 @@ namespace jit {
 
 #define TH_FORALL_TESTS_CUDA(_) \
   _(ArgumentSpec)               \
+  _(CompleteArgumentSpec)       \
   _(Fusion)                     \
   _(GraphExecutor)              \
   _(ModuleConversion)           \
