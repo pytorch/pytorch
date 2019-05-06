e22f1efc3e54d625d429d03462ef892cf4b9657b
BowenBao <semisqg@gmail.com>
2019-05-06 16:30:39 -0700

    fix case of empty default args

--- torch/onnx/symbolic.py
+++ torch/onnx/symbolic.py
@@ -129,8 +129,8 @@ def parse_args(*arg_descriptors):
             # some args may be optional, so the length may be smaller
             assert len(arg_descriptors) >= len(args)
             argspec = inspect.getfullargspec(fn)
-            args_names = argspec.args
-            args_defaults = argspec.defaults
+            args_names = argspec.args if argspec.args else []
+            args_defaults = argspec.defaults if argspec.defaults else []
             default_args_dict = dict(zip(args_names[-len(args_defaults):], args_defaults))
 
             fn_args = []
