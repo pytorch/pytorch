f44b102eb92b4df3e94f62566b8bc05b1154329f
Bram Wasti <bwasti@fb.com>
2019-05-06 14:55:02 -0700

    Add options to Operator to enable registration of alias analysis passes (#19382)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19382
    ghimport-source-id: aeaad3b84ea20dd95b38635ca28c5ff657187909
    
    Differential Revision: D14990873
    
    Pulled By: bwasti
    
    fbshipit-source-id: e1292ac8358ca8ff5bad8d8aeaddf06c23e66067

--- test/cpp/jit/test.cpp
+++ test/cpp/jit/test.cpp
@@ -61,6 +61,7 @@ namespace jit {
   _(SubgraphUtils)                 \
   _(AliasAnalysis)                 \
   _(ContainerAliasing)             \
+  _(AliasRegistration)             \
   _(WriteTracking)                 \
   _(Wildcards)                     \
   _(MemoryDAG)                     \
