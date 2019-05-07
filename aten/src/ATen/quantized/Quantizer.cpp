6bd2ad41e82b9039f740a2bdbf367c32e831889f
Jerry Zhang <jerryzh168@gmail.com>
2019-05-07 13:55:28 -0700

    V18: (no description)
    Differential Revision: D15094174
    Differential Version: 81328044

--- aten/src/ATen/quantized/Quantizer.cpp
+++ aten/src/ATen/quantized/Quantizer.cpp
@@ -100,12 +100,12 @@ Tensor dequantize_tensor(Tensor qtensor, Tensor rtensor, float scale, int32_t ze
 }
 #endif
 
-CAFFE2_API template qint8 quantize_val<qint8>(float scale, int32_t zero_point, float value);
-CAFFE2_API template qint32 quantize_val<qint32>(float scale, int32_t zero_point, float value);
-CAFFE2_API template Tensor quantize_tensor<qint8>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
-CAFFE2_API template Tensor quantize_tensor<qint32>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
-CAFFE2_API template Tensor dequantize_tensor<qint8>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
-CAFFE2_API template Tensor dequantize_tensor<qint32>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
+template CAFFE2_API qint8 quantize_val<qint8>(float scale, int32_t zero_point, float value);
+template CAFFE2_API qint32 quantize_val<qint32>(float scale, int32_t zero_point, float value);
+template CAFFE2_API Tensor quantize_tensor<qint8>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
+template CAFFE2_API Tensor quantize_tensor<qint32>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
+template CAFFE2_API Tensor dequantize_tensor<qint8>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
+template CAFFE2_API Tensor dequantize_tensor<qint32>(Tensor rtensor, Tensor qtensor, float scale, int32_t zero_point);
 
 QuantizerPtr make_per_tensor_affine_quantizer(
     double scale,
