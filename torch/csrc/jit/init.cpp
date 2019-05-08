3a813203e48c1987b9118653681546631d23ef00
Mikhail Zolotukhin <mvz@fb.com>
2019-05-08 11:18:52 -0700

    SubgraphRewriter: Rename pattern fusion to subgraph rewrite. (#20082)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/20082
    ghimport-source-id: f0594f4ad918288fb3158b4ecfa8010cf09dd0c2
    
    Differential Revision: D15190193
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 81b026398c94f2fbf7487cafbb86b7364a78d827

--- torch/csrc/jit/init.cpp
+++ torch/csrc/jit/init.cpp
@@ -27,13 +27,13 @@
 #include <torch/csrc/jit/passes/onnx/fixup_onnx_loop.h>
 #include <torch/csrc/jit/passes/onnx/peephole.h>
 #include <torch/csrc/jit/passes/onnx/prepare_division_for_onnx.h>
-#include <torch/csrc/jit/passes/pattern_fusion.h>
 #include <torch/csrc/jit/passes/peephole.h>
 #include <torch/csrc/jit/passes/quantization.h>
 #include <torch/csrc/jit/passes/remove_expands.h>
 #include <torch/csrc/jit/passes/remove_inplace_ops.h>
 #include <torch/csrc/jit/passes/shape_analysis.h>
 #include <torch/csrc/jit/passes/specialize_autogradzero.h>
+#include <torch/csrc/jit/passes/subgraph_rewrite.h>
 #include <torch/csrc/jit/passes/utils/check_alias_annotation.h>
 #include <torch/csrc/jit/pybind_utils.h>
 #include <torch/csrc/jit/python_arg_flatten.h>
@@ -172,21 +172,21 @@ void initJITBindings(PyObject* module) {
           "_jit_pass_quantlint",
           [](std::shared_ptr<Graph>& g) { return QuantLinting(g); })
       .def(
-          "_jit_pass_pattern_based_fusion",
-          [](std::shared_ptr<script::Module> m) {
-            return PatternBasedFusion(m);
+          "_jit_pass_pattern_based_rewrite",
+          [](std::shared_ptr<script::Module>& m) {
+            return PatternBasedRewrite(m);
           })
       .def(
-          "_jit_pass_custom_pattern_based_fusion",
+          "_jit_pass_custom_pattern_based_rewrite",
           [](const std::string& pattern,
              const std::string& fused_node_name,
              std::vector<std::string> inputs,
              std::vector<std::string> outputs,
              std::shared_ptr<script::Module> m) {
-            PatternFuser pattern_fuser;
-            pattern_fuser.RegisterFusionPattern(
+            SubgraphRewriter subgraph_rewriter;
+            subgraph_rewriter.RegisterRewritePattern(
                 pattern, fused_node_name, inputs, outputs);
-            pattern_fuser.runOnModule(m);
+            subgraph_rewriter.runOnModule(m);
           })
       .def(
           "_jit_pass_fold_quant_inputs",
