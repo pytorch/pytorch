2c57f1308b5d128e5413100ade0b6123e40fc487
Will Feng <willfeng@fb.com>
2019-05-10 16:58:23 -0400

    Remove addInputs(SparseTensorRef) from torch/csrc/jit/tracer.h

--- torch/csrc/jit/tracer.h
+++ torch/csrc/jit/tracer.h
@@ -136,10 +136,6 @@ TORCH_API void addInputs(Node* n, const char* name, const std::string& value);
 TORCH_API void addInputs(
     Node* n,
     const char* name,
-    const at::SparseTensorRef& value);
-TORCH_API void addInputs(
-    Node* n,
-    const char* name,
     const at::TensorOptions& value);
 TORCH_API void addInputs(Node* n, const char* name, at::Device value);
 TORCH_API void addInputs(Node* n, const char* name, at::Layout value);
