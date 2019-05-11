39c6d71cff804da9ddd48838ee5d93376b16101d
Will Feng <willfeng@fb.com>
2019-05-11 00:43:31 -0400

    Move at::NonVariableTypeMode to TensorImpl, and check it in is_variable()

--- aten/src/ATen/core/LegacyTypeDispatch.h
+++ aten/src/ATen/core/LegacyTypeDispatch.h
@@ -139,11 +139,6 @@ private:
 
 CAFFE2_API LegacyTypeDispatch& globalLegacyTypeDispatch();
 
-struct CAFFE2_API NonVariableTypeMode {
-  static bool is_enabled();
-  static void set_enabled(bool enabled);
-};
-
 // A RAII, thread local (!) guard that has the following effect:
 //
 // Upon construction: sets NonVariableTypeMode_enabled for the current thread to
