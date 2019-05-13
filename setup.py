c9917c4d5346ae6e8d2fd679cf4be25b516d2a8c
ilia-cher <iliacher@fb.com>
2019-05-13 14:45:57 -0700

    Restore TBB module
    Summary:
    This partially reverts https://github.com/pytorch/pytorch/pull/8255
    Test Plan:
    USE_TBB=1 USE_OPENMP=0 MKLDNN_THREADING=SEQ MKL_SEQ=1  BUILD_BINARY=1
    USE_EIGEN_THREADPOOL=1 USE_CUDA=0 PARALLEL_BACKEND=NATIVE BLAS=MKL
    USE_MKLDNN=1 USE_OPENCV=1 USE_FFMPEG=1 python setup.py develop --cmake

--- setup.py
+++ setup.py
@@ -289,6 +289,7 @@ def build_deps():
     check_file(os.path.join(third_party_path, "gloo", "CMakeLists.txt"))
     check_file(os.path.join(third_party_path, "pybind11", "CMakeLists.txt"))
     check_file(os.path.join(third_party_path, 'cpuinfo', 'CMakeLists.txt'))
+    check_file(os.path.join(third_party_path, 'tbb', 'Makefile'))
     check_file(os.path.join(third_party_path, 'onnx', 'CMakeLists.txt'))
     check_file(os.path.join(third_party_path, 'foxi', 'CMakeLists.txt'))
     check_file(os.path.join(third_party_path, 'QNNPACK', 'CMakeLists.txt'))
