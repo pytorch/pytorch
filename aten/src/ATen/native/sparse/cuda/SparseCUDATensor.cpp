9d09f5df6c8126440e0f7da1235b6eaf748698e8
Edward Yang <ezyang@fb.com>
2019-05-15 07:28:37 -0700

    Replace AT_CHECK with TORCH_CHECK [shard 7/10]
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/20432
    
    Reviewed By: jerryzh168
    
    Differential Revision: D15318289
    
    fbshipit-source-id: 6c443ac848fe28a1e3e8d7f33a12cd50f80b3e40

--- aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cpp
+++ aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cpp
@@ -9,13 +9,13 @@ namespace at { namespace native {
 using namespace at::sparse;
 
 SparseTensor& sparse_mask_out_cuda(SparseTensor& r, const Tensor& t, const SparseTensor& mask) {
-  AT_CHECK(mask.is_coalesced(), "sparse_mask: mask is uncoalesced");
-  AT_CHECK(mask.sizes().equals(t.sizes()), "sparse_mask: operands have incompatible sizes; self has size ",
+  TORCH_CHECK(mask.is_coalesced(), "sparse_mask: mask is uncoalesced");
+  TORCH_CHECK(mask.sizes().equals(t.sizes()), "sparse_mask: operands have incompatible sizes; self has size ",
       t.sizes(), " but mask has size ", mask.sizes());
   AT_ASSERT(t.is_cuda());  // dispatch argument
-  AT_CHECK(mask.is_cuda(), "sparse_mask: expected 'mask' to be CUDA, but got CPU");
-  AT_CHECK(r.is_cuda(), "sparse_mask: expected 'out' to be CUDA, but got CPU");
-  AT_CHECK(cuda::check_device({r, t, mask}),
+  TORCH_CHECK(mask.is_cuda(), "sparse_mask: expected 'mask' to be CUDA, but got CPU");
+  TORCH_CHECK(r.is_cuda(), "sparse_mask: expected 'out' to be CUDA, but got CPU");
+  TORCH_CHECK(cuda::check_device({r, t, mask}),
       "sparse_mask: arguments are located on different devices; self is on device ", t.get_device(),
       ", mask is on device ", mask.get_device(), ", out is on device ", r.get_device());
   resize_as_sparse_(r, mask);
