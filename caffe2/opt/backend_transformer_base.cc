e2ed0f850c5dc8fa5597253d4fb532eab4a32ad3
Rui Zhu <zrphercule@fb.com>
2019-05-15 16:48:52 -0700

    Change onnxifi workflow to support multi-group quantized & Add multi quantization info to caffe2.proto (#20439)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/20439
    
    This is the QTensorProto workflow for multi group quantization in C2 side.
    No DNNLOWP Tensor related thing is included in this pr, so once we finished glow side, we should be able to test this pr using resnet50.
    
    Reviewed By: yinghai
    
    Differential Revision: D15096919
    
    fbshipit-source-id: 0e615195d3beef89408a46467012248d40c7db99

--- caffe2/opt/backend_transformer_base.cc
+++ caffe2/opt/backend_transformer_base.cc
@@ -54,8 +54,16 @@ QTensorProto BackendTransformerBase::wrapShapeInfoIntoQTensorProto(
       "Only quantized shapeinfo can be extracted into QTensor!");
   t.set_name(name);
   t.set_data_type(shape_info.shape.data_type());
-  t.set_scale(shape_info.q_info.scale);
-  t.set_bias(shape_info.q_info.offset);
+  t.set_axis(shape_info.q_info.axis);
+  t.set_is_multiparam(true);
+  for (const auto i : shape_info.q_info.scale) {
+    t.add_scales(i);
+  }
+  t.set_scale(1.0);
+  for (const auto i : shape_info.q_info.offset) {
+    t.add_biases(i);
+  }
+  t.set_bias(0.0);
   // precision and is_signed is not used in onnxifi workflow, but it is required
   // field
   t.set_precision(0);
