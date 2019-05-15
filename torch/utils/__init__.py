a6726cfbd52a0b688caede753bc5aaecdef35cad
Gao, Xiang <qasdfgtyuiop@gmail.com>
2019-05-15 00:11:38 -0400

    Update __init__.py

--- torch/utils/__init__.py
+++ torch/utils/__init__.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from . import bottleneck  # noqa: F401
 from . import checkpoint  # noqa: F401
 from . import cpp_extension  # noqa: F401
 from . import data  # noqa: F401
 from . import dlpack  # noqa: F401
 from . import model_zoo  # noqa: F401
 from . import tensorboard  # noqa: F401
-from . import utils  # noqa: F401
+from . import mkldnn  # noqa: F401
