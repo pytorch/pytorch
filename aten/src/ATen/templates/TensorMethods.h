5b78a5eadb345a84f7f36d1c8c602bf608030e35
Vitaly Fedyunin <vitalyf@fb.com>
2019-05-16 07:15:34 -0700

    Memory format support for contiguous and is_contiguous (#20455)
    Summary:
    #19975 was separated by 2 PRs.
    
    This one:
    
    Introduce MemoryFormat argument to the `x.is_contiguous(memory_format=torch.channels_last)` and to the `y = x.contiguous(memory_format=torch.channels_last)` functions.
    
    At this moment both functions just operate with strides and doesn't store any tensor state.
    
    (Original RFC #19092)
    
    -----
    
    Expands functionality of two tensor functions `.is_contiguous` and `.contiguous` (both python and c++ api).
    
    Note: We had several complaints about `.to(memory_format)` function, and decided not to support it.
    
    1.  `.contiguous` now support optional keyword-only argument - `memory_format`, which can be either `torch.contiguous_format` or `torch.channels_last`.
    
        - Using `torch.contiguous_format` will preserve existing `.contiguous()` behavior.
    
        - Calling `x.contiguous(memory_format=torch.channels_last)` returns new tensor which maintain same semantical layout (NCHW), but have different memory allocation pattern.
    
            `x.contiguous(memory_format=torch.channels_last)` expects input tensor to be 3d, 4d or 5d; and fails otherwise.
    
    2. `.is_contiguous` now support optional keyword-only argument - `memory_format`, which can be either `torch.contiguous_format` or `torch.channels_last`.
    
        - `x.is_contiguous(memory_format=torch.contiguous_format)` preserves same functionality as `x.is_contiguous()` and remains unchanged.
    
        - `x.is_contiguous(memory_format=torch.channels_last)` returns true if A) input tensor is contiguous in memory AND B) allocated in the memory in NWHC (or similar for 3d,5d) format.
    
    Note: By the end of the phase one `x.is_contiguous(memory_format=torch.channels_last)` will calculate state of the Tensor on every call. This functionality going to be updated later.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/20455
    
    Differential Revision: D15341577
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: bbb6b4159a8a49149110ad321109a3742383185d

--- aten/src/ATen/templates/TensorMethods.h
+++ aten/src/ATen/templates/TensorMethods.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/core/Scalar.h>
+#include <c10/core/MemoryFormat.h>
 #include <c10/macros/Macros.h>
 #include <ATen/core/SparseTensorRef.h>
 #include <c10/core/TensorOptions.h>
--- torch/csrc/utils/python_arg_parser.h
+++ torch/csrc/utils/python_arg_parser.h
@@ -47,6 +47,7 @@
 #include <torch/csrc/DynamicTypes.h>
 #include <torch/csrc/Exceptions.h>
 #include <torch/csrc/Generator.h>
+#include <torch/csrc/MemoryFormat.h>
 #include <torch/csrc/autograd/python_variable.h>
 #include <torch/csrc/jit/tracer.h>
 #include <torch/csrc/tensor/python_tensor.h>
@@ -70,7 +71,7 @@ namespace torch {
 
 enum class ParameterType {
   TENSOR, SCALAR, INT64, DOUBLE, TENSOR_LIST, INT_LIST, GENERATOR,
-  BOOL, STORAGE, PYOBJECT, SCALARTYPE, LAYOUT, DEVICE, STRING
+  BOOL, STORAGE, PYOBJECT, SCALARTYPE, LAYOUT, MEMORY_FORMAT, DEVICE, STRING
 };
 
 struct FunctionParameter;
@@ -134,6 +135,7 @@ struct PythonArgs {
   inline at::Device device(int i);
   inline at::Device deviceWithDefault(int i, const at::Device& default_device);
   inline c10::optional<at::Device> deviceOptional(int i);
+  inline at::MemoryFormat toMemoryFormat(int i);
   inline std::string string(int i);
   inline PyObject* pyobject(int i);
   inline int64_t toInt64(int i);
@@ -386,6 +388,13 @@ inline c10::optional<at::Device> PythonArgs::deviceOptional(int i) {
   if (!args[i])
     return c10::nullopt;
   return device(i);
+}
+
+inline at::MemoryFormat PythonArgs::toMemoryFormat(int i) {
+  if (!args[i]) return at::MemoryFormat::Any;
+  AT_CHECK(THPMemoryFormat_Check(args[i]), "memory_format arg must be an instance of the torch.memory_format");
+  const auto memory_format = reinterpret_cast<THPMemoryFormat*>(args[i]);
+  return memory_format->memory_format;
 }
 
 inline std::string PythonArgs::string(int i) {
