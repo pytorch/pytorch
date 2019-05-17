220e6894c59bcac0eace168eeb1800be2261924c
Jerry Zhang <jerryzh@fb.com>
2019-05-16 18:02:18 -0700

    Rename qint8 data type (#19932)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19932
    
    In preparation to add int8_t data type for QTensor
    
    Reviewed By: zafartahirov
    
    Differential Revision: D15137838
    
    fbshipit-source-id: 59462c36d6fc5982986d4196bf3f32f49bb294d7

--- aten/src/ATen/native/quantized/Copy.cpp
+++ aten/src/ATen/native/quantized/Copy.cpp
@@ -9,8 +9,8 @@ namespace at {
 namespace native {
 Tensor& _s_copy__quantized(Tensor& self, const Tensor& src, bool /* unused */) {
   TORCH_CHECK(
-      self.scalar_type() == at::kQInt8,
-      "Quantized copy only works with kQInt8 as target Tensor");
+      self.scalar_type() == at::kQUInt8,
+      "Quantized copy only works with kQUInt8 as target Tensor");
   TORCH_CHECK(
       src.scalar_type() == at::kFloat,
       "Quantized copy only works with kFloat as source Tensor");
