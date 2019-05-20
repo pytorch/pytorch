2f9752bfe77a582496a2ecdf4daa38e41980622b
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2019-05-20 14:47:13 +0200

    Update transforms.py

--- torch/distributions/transforms.py
+++ torch/distributions/transforms.py
@@ -355,8 +355,6 @@ class SigmoidTransform(Transform):
         return torch.sigmoid(x)
 
     def _inverse(self, y):
-        finfo = torch.finfo(y.dtype)
-        y = y.clamp(min=finfo.tiny, max=1. - finfo.eps)
         return y.log() - (-y).log1p()
 
     def log_abs_det_jacobian(self, x, y):
