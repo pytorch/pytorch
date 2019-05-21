c6f0f84e698b7b1fda14ff702368dc28048ea0fc
Edward Z. Yang <ezyang@fb.com>
2019-05-21 17:20:35 -0400

    Reduce set of build/tests which run on PRs.
    I ran an analysis of historical build data to find out which
    build jobs success/failures where correlated with each other.
    The analysis can be found here:
    
      https://github.com/ezyang/pytorch-ci-deduplication/blob/master/PyTorch%20CI%20deduplication%20(1).pdf
    
    Based on this analysis, I've revamped our logic for whether or not to
    run a PR or not to only run builds in our default set.  This replaces
    our previous logic for doing [ci slow] or [ci xla]; now, you can place
    any string [ci foobar] and it will cause any build which matches that
    substring to be run on your run.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .circleci/scripts/setup_ci_environment.sh
+++ .circleci/scripts/setup_ci_environment.sh
@@ -4,40 +4,10 @@ set -ex -o pipefail
 # Check if we should actually run
 echo "BUILD_ENVIRONMENT: ${BUILD_ENVIRONMENT}"
 echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST:-}"
-if [[ "${BUILD_ENVIRONMENT}" == *-slow-* ]]; then
-  if ! [ -z "${CIRCLE_PULL_REQUEST:-}" ]; then
-    # It's a PR; test for [slow ci] tag on the TOPMOST commit
-    topmost_commit=$(git log --format='%B' -n 1 HEAD)
-    if !(echo $topmost_commit | grep -q -e '\[slow ci\]' -e '\[ci slow\]' -e '\[test slow\]' -e '\[slow test\]'); then
-      circleci step halt
-      exit
-    fi
-  fi
-fi
-if [[ "${BUILD_ENVIRONMENT}" == *xla* ]]; then
-  if ! [ -z "${CIRCLE_PULL_REQUEST:-}" ]; then
-    # It's a PR; test for [xla ci] tag on the TOPMOST commit
-    topmost_commit=$(git log --format='%B' -n 1 HEAD)
-    if !(echo $topmost_commit | grep -q -e '\[xla ci\]' -e '\[ci xla\]' -e '\[test xla\]' -e '\[xla test\]'); then
-      # NB: This doesn't halt everything, just this job.  So
-      # the rest of the workflow will keep going and you need
-      # to make sure you halt there too.  Blegh.
-      circleci step halt
-      exit
-    fi
-  fi
-fi
-if [[ "${BUILD_ENVIRONMENT}" == *namedtensor* ]]; then
-  if ! [ -z "${CIRCLE_PULL_REQUEST:-}" ]; then
-    # It's a PR; test for [namedtensor] tag on the TOPMOST commit
-    topmost_commit=$(git log --format='%B' -n 1 HEAD)
-    if !(echo $topmost_commit | grep -q -e '\[namedtensor\]' -e '\[ci namedtensor\]' -e '\[namedtensor ci\]'); then
-      # NB: This doesn't halt everything, just this job.  So
-      # the rest of the workflow will keep going and you need
-      # to make sure you halt there too.  Blegh.
-      circleci step halt
-      exit
-    fi
+if ! [ -z "${CIRCLE_PULL_REQUEST:-}" ]; then
+  if python .circleci/scripts/should_run_job.py "${BUILD_ENVIRONMENT}"; then
+    circleci step halt
+    exit
   fi
 fi
 
