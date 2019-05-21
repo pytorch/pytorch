66834425d08a781a98eb2733a02452983363d9d5
dawars <dawars00@gmail.com>
2019-05-21 22:26:45 +0200

    Example code for mesh plugin

--- torch/utils/tensorboard/writer.py
+++ torch/utils/tensorboard/writer.py
@@ -848,7 +848,7 @@ class SummaryWriter(object):
             tag (string): Data identifier
             vertices (torch.Tensor): List of the 3D coordinates of vertices.
             colors (torch.Tensor): Colors for each vertex
-            faces (torch.Tensor): Indices of vertices within each triangle.
+            faces (torch.Tensor): Indices of vertices within each triangle. (Optional)
             config_dict: Dictionary with ThreeJS classes names and configuration.
             global_step (int): Global step value to record
             walltime (float): Optional override default walltime (time.time())
@@ -858,6 +858,33 @@ class SummaryWriter(object):
             vertices: :math:`(B, N, 3)`. (batch, number_of_vertices, channels)
             colors: :math:`(B, N, 3)`. The values should lie in [0, 255] for type `uint8` or [0, 1] for type `float`.
             faces: :math:`(B, N, 3)`. The values should lie in [0, number_of_vertices] for type `uint8`.
+
+        Examples::
+            from torch.utils.tensorboard import SummaryWriter
+
+            vertices_tensor = torch.as_tensor([
+                [1, 1, 1],
+                [-1, -1, 1],
+                [1, -1, -1],
+                [-1, 1, -1],
+            ], dtype=torch.float).unsqueeze(0)
+            colors_tensor = torch.as_tensor([
+                [255, 0, 0],
+                [0, 255, 0],
+                [0, 0, 255],
+                [255, 0, 255],
+            ], dtype=torch.int).unsqueeze(0)
+            faces_tensor = torch.as_tensor([
+                [0, 2, 3],
+                [0, 3, 1],
+                [0, 1, 2],
+                [1, 3, 2],
+            ], dtype=torch.int).unsqueeze(0)
+
+            writer = SummaryWriter()
+            writer.add_mesh('my_mesh', vertices=vertices_tensor, colors=colors_tensor, faces=faces_tensor)
+
+            writer.close()
         """
         self._get_file_writer().add_summary(mesh(tag, vertices, colors, faces, config_dict), global_step, walltime)
 
