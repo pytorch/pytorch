7a90569892583466d813d60500f5a39b1c8eb7c7
Edward Z. Yang <ezyang@fb.com>
2019-05-21 18:03:19 -0400

    Update on "Reduce set of build/tests which run on PRs."
    Reduce set of build/tests which run on PRs.
    
    I ran an analysis of historical build data to find out which
    build jobs success/failures where correlated with each other.
    The analysis can be found here:
    
      https://github.com/ezyang/pytorch-ci-deduplication/blob/master/PyTorch%20CI%20deduplication%20(1).pdf
    
    Based on this analysis, I've revamped our logic for whether or not to
    run a PR or not to only run builds in our default set.  This replaces
    our previous logic for doing [ci slow] or [ci xla]; now, you can place
    any string [ci foobar] and it will cause any build which matches that
    substring to be run on your run.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    gh-metadata: pytorch pytorch 20775 gh/ezyang/134/head

--- .circleci/scripts/setup_ci_environment.sh
+++ .circleci/scripts/setup_ci_environment.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -ex -o pipefail
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Check if we should actually run
-echo "BUILD_ENVIRONMENT: ${BUILD_ENVIRONMENT}"
-echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST:-}"
-if ! [ -z "${CIRCLE_PULL_REQUEST:-}" ]; then
-  # Don't swallow "script doesn't exist
-  [ -e "$SCRIPT_DIR/should_run_job.py"  ]
-  if ! python "$SCRIPT_DIR/should_run_job.py" "${BUILD_ENVIRONMENT}"; then
-    circleci step halt
-    exit
-  fi
-fi
-
 # Set up NVIDIA docker repo
 curl -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 echo "deb https://nvidia.github.io/libnvidia-container/ubuntu16.04/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
