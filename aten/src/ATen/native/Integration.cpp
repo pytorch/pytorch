d6eb5a142b830619ced0c30d60613844c1d9dd87
Brennan Vincent <btv@fb.com>
2019-05-22 18:23:19 -0400

    implement numerical integration with trapezoid rule

---
+++ aten/src/ATen/native/Integration.cpp
@@ -0,0 +1,37 @@
+#include "ATen/ATen.h"
+#include "ATen/NativeFunctions.h"
+#include "ATen/WrapDimUtils.h"
+#include "c10/util/Exception.h"
+#include <ATen/Parallel.h>
+#include <tuple>
+
+
+namespace at {
+namespace native {
+namespace {
+
+template <typename T>
+static Tensor do_trapz(const Tensor& y, const T& dx, int64_t dim) {
+    Tensor left = y.slice(dim, 0, -1);
+    Tensor right = y.slice(dim, 1);
+
+    return (((left + right) * dx).sum(dim) / 2.);
+}
+
+
+
+}
+
+Tensor trapz(const Tensor& y, const Tensor& x, int64_t dim) {
+    Tensor x_left = x.slice(dim, 0, -1);
+    Tensor x_right = x.slice(dim, 1);
+
+    Tensor dx = x_right - x_left;
+    return do_trapz<Tensor>(y, dx, dim);
+}
+
+Tensor trapz(const Tensor& y, double dx, int64_t dim) {
+    return do_trapz<Scalar>(y, dx, dim);
+}
+
+}} // namespace at::native
