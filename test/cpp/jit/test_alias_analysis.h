a1514b5b0dbc42bfa6295adc5c5023b2896622af
Michael Suo <suo@fb.com>
2019-05-22 16:42:33 -0700

    Mark values entering containers as wildcards (#20556)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/20556
    ghimport-source-id: d7c62e38a2f6928f6f8d988c26a38ea8f8cff8b6
    
    Reviewed By: jamesr66a
    
    Differential Revision: D15447568
    
    Pulled By: suo
    
    fbshipit-source-id: 77ebc11b571b8517d3bad3ee1b3ee5ac037542b2

--- test/cpp/jit/test_alias_analysis.h
+++ test/cpp/jit/test_alias_analysis.h
@@ -680,6 +680,49 @@ graph():
     AT_ASSERT(!aliasDb.mayContainAlias(first_st, second_st));
     AT_ASSERT(!aliasDb.mayContainAlias(second_st, tup_st));
   }
+  {
+    // Test list container aliasing
+    auto graph = std::make_shared<Graph>();
+    std::unordered_map<std::string, Value*> vmap;
+    script::parseIR(
+        R"IR(
+graph():
+  %10 : bool? = prim::Constant()
+  %8 : Device? = prim::Constant()
+  %4 : int? = prim::Constant()
+  %0 : int = prim::Constant[value=2]()
+  %1 : int = prim::Constant[value=3]()
+  %2 : int[] = prim::ListConstruct(%0, %1)
+  %x : Tensor = aten::rand(%2, %4, %4, %8, %10)
+  %12 : int[] = prim::ListConstruct(%0, %1)
+  %y : Tensor = aten::rand(%12, %4, %4, %8, %10)
+  %22 : int[] = prim::ListConstruct(%0, %1)
+  %z : Tensor = aten::rand(%22, %4, %4, %8, %10)
+  %32 : int[] = prim::ListConstruct(%0, %1)
+  %fresh : Tensor = aten::rand(%32, %4, %4, %8, %10)
+  %foo : Tensor[] = prim::ListConstruct(%x, %y)
+  %43 : Tensor[] = aten::append(%foo, %z)
+  return ()
+)IR",
+        graph.get(),
+        vmap);
+    AliasDb aliasDb(graph);
+    auto x = vmap["x"];
+    auto y = vmap["y"];
+    auto z = vmap["z"];
+    // Tensors x, y, and z went into a list, so they all may alias each other.
+    ASSERT_TRUE(aliasDb.mayAlias(x, y));
+    ASSERT_TRUE(aliasDb.mayAlias(y, z));
+    ASSERT_TRUE(aliasDb.mayAlias(x, z));
+
+    // But we know `fresh` didn't go into a list, so x, y, and z should not
+    // alias it.
+
+    // auto fresh = vmap["fresh"];
+    // ASSERT_FALSE(aliasDb.mayAlias(x, fresh));
+    // ASSERT_FALSE(aliasDb.mayAlias(y, fresh));
+    // ASSERT_FALSE(aliasDb.mayAlias(z, fresh));
+  }
 }
 
 void testWildcards() {
