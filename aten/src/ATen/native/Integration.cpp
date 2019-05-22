1b08e4a6ec2f985e09a2b88a66a4ad5c1813cac0
Brennan Vincent <btv@fb.com>
2019-05-22 18:28:43 -0400

    do multiplication by scalar dx after sum

--- aten/src/ATen/native/Integration.cpp
+++ aten/src/ATen/native/Integration.cpp
@@ -10,15 +10,20 @@ namespace at {
 namespace native {
 namespace {
 
-template <typename T>
-static Tensor do_trapz(const Tensor& y, const T& dx, int64_t dim) {
+static Tensor do_trapz(const Tensor& y, const Tensor& dx, int64_t dim) {
     Tensor left = y.slice(dim, 0, -1);
     Tensor right = y.slice(dim, 1);
 
-    return (((left + right) * dx).sum(dim) / 2.);
+    return ((left + right) * dx).sum(dim) / 2.;
 }
 
-
+// special case for scalar dx, to move the multiplication out of the
+// loop
+static Tensor do_trapz(const Tensor& y, double dx, int64_t dim) {
+    Tensor left = y.slice(dim, 0, -1);
+    Tensor right = y.slice(dim, 1);
+    return (left + right).sum(dim) * (dx/2);
+}
 
 }
 
@@ -27,11 +32,11 @@ Tensor trapz(const Tensor& y, const Tensor& x, int64_t dim) {
     Tensor x_right = x.slice(dim, 1);
 
     Tensor dx = x_right - x_left;
-    return do_trapz<Tensor>(y, dx, dim);
+    return do_trapz(y, dx, dim);
 }
 
 Tensor trapz(const Tensor& y, double dx, int64_t dim) {
-    return do_trapz<Scalar>(y, dx, dim);
+    return do_trapz(y, dx, dim);
 }
 
 }} // namespace at::native
