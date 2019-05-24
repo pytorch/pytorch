89d0e0c180918406e5449db671e503b9369efb82
Brennan Vincent <btv@fb.com>
2019-05-24 15:57:36 -0400

    better version for constant dx; add comments

--- aten/src/ATen/native/Integration.cpp
+++ aten/src/ATen/native/Integration.cpp
@@ -10,6 +10,14 @@ namespace at {
 namespace native {
 namespace {
 
+// The estimated integral of a function y of x,
+// sampled at points (y_1, ..., y_n) that are separated by distance (dx_1, ..., dx_{n-1}),
+// is given by the trapezoid rule:
+//
+// \sum_{i=1}^{n-1}  dx_i * (y_i + y_{i+1}) / 2
+//
+// TODO: if we extend TensorIterator to accept 3 inputs,
+// we can probably make this a bit more performant.
 static Tensor do_trapz(const Tensor& y, const Tensor& dx, int64_t dim) {
     Tensor left = y.slice(dim, 0, -1);
     Tensor right = y.slice(dim, 1);
@@ -17,10 +25,10 @@ static Tensor do_trapz(const Tensor& y, const Tensor& dx, int64_t dim) {
     return ((left + right) * dx).sum(dim) / 2.;
 }
 
-// special case for scalar dx, to move the multiplication out of the
-// loop
+// When dx is constant, the above formula simplifies
+// to dx * [(\sum_{i=1}^n y_i) - (y_1 + y_n)/2]
 static Tensor do_trapz(const Tensor& y, double dx, int64_t dim) {
-    return (y * dx) - y.slice(dim, 0, 1) - y.slice(dim, -1);
+    return (y.sum(dim) - (y.select(dim, 0) + y.select(dim, -1)) * (0.5)) * dx;
 }
 
 }
