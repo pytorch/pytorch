ab58b2789f89fead1bb10bcdbb49f7feedb6cc53
Horace He <chilli@fb.com>
2019-05-28 16:14:27 -0700

    Fixed gcd to use 64 bit integers (#21041)
    Summary:
    Not much to say. Fixes implementation introduced here: https://github.com/pytorch/pytorch/pull/19115
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21041
    
    Differential Revision: D15528801
    
    Pulled By: Chillee
    
    fbshipit-source-id: bacd709eb711ca00156bd70480d6051b437517ed

--- test/test_jit.py
+++ test/test_jit.py
@@ -6009,7 +6009,11 @@ a")
             # type: (int, int) -> int
             return math.gcd(x, y)
 
-        for inputs in [(2, 4), (-5, -15), (-5, 15), (10, 0), (0, 10), (-5, 0), (0, -5), (0, 0), (0, -0)]:
+        max_int = 2147483647
+        min_int = -2147483647 - 1
+        int_vals = list(range(-5, 5, 1)) + [max_int + 5, max_int * 2, min_int - 5, min_int * 2]
+        vals = [(i, j) for i in int_vals for j in int_vals]
+        for inputs in vals:
             self.checkScript(test_gcd, inputs)
 
     def test_math_ops1(self):
