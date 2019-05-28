83a23e745e839e8db81cf58ee00a5755d7332a43
Vitaly Fedyunin <vitalyf@fb.com>
2019-05-28 11:20:35 -0700

    Add Vec256 instructions

--- aten/src/ATen/native/cpu/LerpKernel.cpp
+++ aten/src/ATen/native/cpu/LerpKernel.cpp
@@ -22,11 +22,18 @@ static void lerp_kernel(
 
   AT_DISPATCH_FLOATING_TYPES(ret.scalar_type(), "lerp_kernel", [&] {
     scalar_t weight_val = weight.to<scalar_t>();
-    at::native::binary_kernel(*iter, [=](scalar_t self_val, scalar_t end_val) {
-      return (weight_val < 0.5)
-          ? self_val + weight_val * (end_val - self_val)
-          : end_val - (end_val - self_val) * (1 - weight_val);
-    });
+    auto weight_vec = Vec256<scalar_t>(weight_val);
+    auto reversed_weight_vec = Vec256<scalar_t>(1 - weight_val);
+    at::native::binary_kernel_vec(
+        *iter,
+        [=](scalar_t self_val, scalar_t end_val) {
+          return (weight_val < 0.5)
+              ? self_val + weight_val * (end_val - self_val)
+              : end_val - (end_val - self_val) * (1 - weight_val);
+        },
+        [=](Vec256<scalar_t> start_vec, Vec256<scalar_t> end_vec) {
+          return reversed_weight_vec * start_vec + weight_vec * end_vec;
+        });
   });
 }
 
