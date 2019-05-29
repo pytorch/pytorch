57e005aff99c87217c41579f3d1b473ddac7c3a1
Thomas Viehmann <tv.code@beamnet.de>
2019-05-29 07:51:14 +0200

    add nondet_tol to test

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1589,7 +1589,7 @@ class TestAutograd(TestCase):
                 log_probs = torch.log_softmax(x_full, 2)
                 return torch.nn.functional.ctc_loss(log_probs, targets, input_lengths, target_lengths)
 
-            gradcheck(ctc_after_softmax, [x])
+            gradcheck(ctc_after_softmax, [x], nondet_tol=1e-7)
 
     def _test_sparse_gather(self, size_x, size_ind, dim):
         x = torch.randn(size_x, requires_grad=True)
