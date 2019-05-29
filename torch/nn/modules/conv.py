88f192a52cabdc451363691c199eed01db499e59
davidriazati <davidriazati@fb.com>
2019-05-29 11:13:47 -0700

    fix

--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -480,10 +480,6 @@ class Conv3d(_ConvNd):
 
 @weak_module
 class _ConvTransposeMixin(object):
-    __constants__ = ['stride', 'padding', 'kernel_size', 'dim_size',
-                     'output_padding', 'groups', 'dilation', 'transposed',
-                     'bias', 'padding_mode']
-
     @weak_script_method
     def forward(self, input, output_size=None):
         # type(Tensor, Optional[List[int]]) -> Tensor
