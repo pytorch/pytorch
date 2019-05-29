736bf7b46c0458875af2eee36072794f054c5bc4
davidriazati <davidriazati@fb.com>
2019-05-29 13:48:34 -0700

    Fix __constants__ for some nn modules (#21071)
    Summary:
    A bunch of modules were missing entries for `__constants__` which was making their `__repr__`s not work. Others had `__constants__` that were not necessary since it was provided by some parent class instead.
    
    Fixes #20978
    ](https://our.intern.facebook.com/intern/diff/15539518/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21071
    
    Pulled By: driazati
    
    Differential Revision: D15539518
    
    fbshipit-source-id: 24bdd1ef41ef636eefd5d2bad4ab2d79646ed4f0

--- test/test_jit.py
+++ test/test_jit.py
@@ -14874,16 +14874,22 @@ def add_nn_module_test(*args, **kwargs):
                 def __init__(self):
                     super(TheModule, self).__init__()
                     self.submodule = nn_module(*constructor_args)
+
+            def make_module(script):
+                module = TheModule()
+                # check __repr__
+                str(module)
+                module.define(script)
+                return module
+
             # module cannot be imported / exported
             if module_name in EXCLUDE_MODULE_EXPORT_IMPORT:
                 with self.disableEmitHook():
-                    module = TheModule()
-                    module.define(script)
+                    module = make_module(script)
                     create_script_module.last_graph = module.graph
                     mod = module(*args)
             else:
-                module = TheModule()
-                module.define(script)
+                module = make_module(script)
                 self.assertExportImportModule(module, tensors)
                 create_script_module.last_graph = module.graph
                 mod = module(*args)
