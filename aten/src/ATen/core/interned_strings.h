8322699450ed880b91bf77f2723ad182f74bb466
Zach DeVito <zdevito@fb.com>
2019-05-29 13:13:06 -0700

    Conservative alias analysis rules for CallFunction/CallMethod
    Summary: Uses the analyzeCustomOp rule (renamed analyzeConservative)
    to support CallFunction, assuming we do not know the function implementation.
    When we have a know function target, and a way to summarize alias info
    we can make the analysis more accurate.
    
    Cleans up seemingly dead code in alias analysis and fixes a bug in
    analyzeCustomOp where inputs are not marked as potentially aliasing the
    wildcard set.
    
    Test Plan: test_jit.py

--- aten/src/ATen/core/interned_strings.h
+++ aten/src/ATen/core/interned_strings.h
@@ -99,6 +99,7 @@ namespace c10 {
   _(prim, AddStatValue)            \
   _(prim, TimePoint)               \
   _(prim, CallFunction)            \
+  _(prim, CallMethod)              \
   _(aten, append)                  \
   _(aten, item)                    \
   _(aten, format)                  \
