285abf2ee45eae79308ca1bde34f0a957fc5bfc2
eellison <eellison@fb.com>
2019-05-30 11:14:37 -0700

    add handle to run all jit tests

--- test/run_test.py
+++ test/run_test.py
@@ -30,7 +30,6 @@ TESTS = [
     'expecttest',
     'indexing',
     'indexing_cuda',
-    'jit',
     'logging',
     'mkldnn',
     'multiprocessing',
@@ -47,10 +46,16 @@ TESTS = [
     'type_hints',
     'utils',
     'namedtuple_return_api',
-    'jit_fuser',
     'tensorboard',
     'namedtensor',
 ]
+
+JIT_TESTS = [
+    'jit',
+    'jit_fuser',
+]
+
+TESTS = TESTS + JIT_TESTS
 
 WINDOWS_BLACKLIST = [
     'distributed',
@@ -268,6 +273,11 @@ def parse_args():
         action='store_true',
         help='print verbose information and test-by-test results')
     parser.add_argument(
+        '--jit',
+        '--jit',
+        action='store_true',
+        help='run all jit tests')
+    parser.add_argument(
         '-pt', '--pytest', action='store_true',
         help='If true, use `pytest` to execute the tests. E.g., this runs '
              'TestTorch with pytest in verbose and coverage mode: '
@@ -412,6 +422,9 @@ def main():
 
     if options.coverage:
         shell(['coverage', 'erase'])
+
+    if options.jit:
+        selected_tests = JIT_TESTS
 
     for test in selected_tests:
         test_name = 'test_{}'.format(test)
