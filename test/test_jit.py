d240734ef666364a32dabfcec8481c3969c143f1
Horace He <chilli@fb.com>
2019-05-29 23:45:07 -0700

    Added remainder of high-priority trigonometric math ops

--- test/test_jit.py
+++ test/test_jit.py
@@ -6013,7 +6013,7 @@ a")
                     raise AssertionError("Failed on {func_name} with inputs {a} {b}. Python: {resf}, Script: {resfs}".format(func_name=func_name, a=a, b=b, resf=resf, resfs=resfs))
 
 
-        unary_float_ops = ["log", "log1p", "log10", "exp", "sqrt", "gamma", "lgamma", "erf", "erfc", "expm1", "fabs"]
+        unary_float_ops = ["log", "log1p", "log10", "exp", "sqrt", "gamma", "lgamma", "erf", "erfc", "expm1", "fabs", "acos", "asin", "atan", "cos", "sin", "tan"]
         for op in unary_float_ops:
             checkMathWrap(op)
 
