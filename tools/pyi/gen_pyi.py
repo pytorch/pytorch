241984a7fd0401f3428f4651e4fed4cfafcb8e58
Jerry Zhang <jerryzh168@gmail.com>
2019-05-30 21:23:02 -0700

    V6: (no description)
    Differential Revision: D15364354
    Differential Version: 83778341

--- tools/pyi/gen_pyi.py
+++ tools/pyi/gen_pyi.py
@@ -125,6 +125,7 @@ def type_to_python(typename, size=None):
         'void*': '_int',    # data_ptr
         'void': 'None',
         'std::string': 'str',
+        'QScheme': 'QScheme',
     }[typename]
 
     return typename
