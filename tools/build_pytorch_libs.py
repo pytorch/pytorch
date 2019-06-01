30a4ad7e1c504b8b828c6d0b43d5c7e0964c07ec
peter <peterghost86@gmail.com>
2019-06-01 15:49:23 +0800

    Fix `sccache not being used on Windows`

--- tools/build_pytorch_libs.py
+++ tools/build_pytorch_libs.py
@@ -86,9 +86,11 @@ def overlay_windows_vcvars(env):
         vc_arch = 'x64' if IS_64BIT else 'x86'
         vc_env = _get_vc_env(vc_arch)
         for k, v in env.items():
-            lk = k.lower()
-            if lk not in vc_env:
-                vc_env[lk] = v
+            # OS environ keys are always uppercase on Windows.
+            # https://stackoverflow.com/a/7797329
+            uk = k.upper()
+            if uk not in vc_env:
+                vc_env[uk] = v
         return vc_env
     else:
         return env
@@ -118,10 +120,8 @@ def create_build_env():
         # But it should be set to MSVC as the user's first choice.
         if USE_NINJA:
             my_env = overlay_windows_vcvars(my_env)
-            cc = my_env.get('CC', 'cl')
-            cxx = my_env.get('CXX', 'cl')
-            my_env['CC'] = cc
-            my_env['CXX'] = cxx
+            my_env.setdefault('CC', 'cl')
+            my_env.setdefault('CXX', 'cl')
     return my_env
 
 
