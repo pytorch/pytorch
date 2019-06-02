7c42c833ed00f82d3400d6f732b884fc243468f3
Xiaomeng Yang <yangxm@fb.com>
2019-06-02 09:36:14 -0700

    Add gelu gradient for pytorch (#21237)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21237
    
    Add gelu gradient for pytorch
    
    Reviewed By: zheng-xq
    
    Differential Revision: D15589816
    
    fbshipit-source-id: 76fda7c413afed5b6cc3abe3a26c258d393a53ce

--- aten/src/ATen/native/Activation.h
+++ aten/src/ATen/native/Activation.h
@@ -12,9 +12,12 @@ namespace native {
 
 using threshold_fn = void (*)(TensorIterator&, Scalar, Scalar);
 using activation_fn = void (*)(const Tensor& /* X */, Tensor* /* Y */);
+using activation_backward_fn =
+    void (*)(const Tensor& /* dY */, const Tensor& /* X */, Tensor* /* dX */);
 
 DECLARE_DISPATCH(threshold_fn, threshold_stub);
 DECLARE_DISPATCH(activation_fn, GeluKernel);
+DECLARE_DISPATCH(activation_backward_fn, GeluBackwardKernel);
 
 } // namespace native
 
