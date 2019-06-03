9b6a1a2a41ca324b41652fb3b290af12a0f29348
Richard Zou <zou3519@gmail.com>
2019-06-03 06:44:33 -0700

    [namedtensor] Added at::Dimname
    Dimname represents a dimension name for named tensors. Because we
    support special names (wildcard, tagged, and possibly more in the
    future) a single at::Symbol does not suffice and we need this struct.
    
    Test Plan:
    - build/bin/Dimname_test

---
+++ aten/src/ATen/core/Dimname.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifdef NAMEDTENSOR_ENABLED
+
+#include <ATen/core/interned_strings.h>
+
+namespace at {
+
+enum class NameType: uint8_t { NORMAL, WILDCARD, TAGGED };
+
+struct CAFFE2_API Dimname {
+  static Dimname fromSymbol(Symbol name);
+  static Dimname wildcard();
+
+  NameType type() const { return type_; }
+  Symbol name() const { return name_; }
+  Symbol untagged_name() const { return untagged_name_; }
+
+ private:
+  Dimname(Symbol name)
+    : untagged_name_(name), name_(name), type_(NameType::NORMAL) {}
+  Dimname(NameType type, Symbol name, Symbol untagged_name)
+    : untagged_name_(untagged_name), name_(name), type_(type) {}
+  Symbol untagged_name_;
+  Symbol name_;
+  NameType type_;
+  // Will need more fields for other special name types.
+};
+
+static Symbol kWildcard = Symbol::dimname("*");
+bool CAFFE2_API is_valid_identifier(const std::string& name);
+
+} // namespace at
+#endif
