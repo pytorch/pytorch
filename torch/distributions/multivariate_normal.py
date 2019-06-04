90d9d0ee636e3709ccb5573eea33dfcc49847abd
fehiepsi <fehiepsi@gmail.com>
2019-06-04 18:44:02 -0400

    use triangular solve for inverse of triangular matrix

--- torch/distributions/multivariate_normal.py
+++ torch/distributions/multivariate_normal.py
@@ -69,8 +69,10 @@ def _batch_mahalanobis(bL, bx):
 def _precision_to_scale_tril(P):
     # Ref: https://nbviewer.jupyter.org/gist/fehiepsi/5ef8e09e61604f10607380467eb82006#Precision-to-scale_tril
     Lf = torch.cholesky(torch.flip(P, (-2, -1)))
-    L = torch.inverse(torch.transpose(torch.flip(Lf, (-2, -1)), -2, -1))
-    return L.tril()  # torch.inverse of a triangular is not a triangular due to precision
+    L_inv = torch.transpose(torch.flip(Lf, (-2, -1)), -2, -1)
+    L = torch.triangular_solve(torch.eye(P.shape[-1], dtype=P.dtype, device=P.device),
+                               L_inv, upper=False)[0]
+    return L
 
 
 class MultivariateNormal(Distribution):
