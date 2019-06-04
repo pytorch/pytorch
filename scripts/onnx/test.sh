1ffd2fc9393ebd3dcff5c1e4fa8944c2a2fac4fe
BowenBao <semisqg@gmail.com>
2019-06-04 12:57:32 -0700

    add ninja to onnx test env

--- scripts/onnx/test.sh
+++ scripts/onnx/test.sh
@@ -23,7 +23,7 @@ do
 done
 set -- "${UNKNOWN[@]}" # leave UNKNOWN
 
-pip install pytest scipy hypothesis
+pip install pytest scipy hypothesis ninja
 if [[ $PARALLEL == 1 ]]; then
     pip install pytest-xdist
 fi
