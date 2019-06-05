1e998959ef1389fade5c84e9d52feef6465dfa9a
Edward Z. Yang <ezyang@fb.com>
2019-06-05 16:43:15 -0400

    Revert "Revert D15632268: [pytorch][PR] Continuation of Port max_unpool1d, max_unpool2d and max_unpool3d to ATen"
    This reverts commit 7a1c9076acd4a4fe20a8817ec3bfd6c68ab370a8.
    
    Same as the original commit, except there's now a static in front
    of the shape check.

--- torch/nn/_functions/thnn/auto.py
+++ torch/nn/_functions/thnn/auto.py
@@ -298,8 +298,6 @@ def _generate_function_classes(scope_dict):
     name_remap = {
         'TemporalConvolution': 'Conv1d',
         'SpatialDilatedConvolution': 'DilatedConv2d',
-        'SpatialMaxUnpooling': 'MaxUnpool2d',
-        'VolumetricMaxUnpooling': 'MaxUnpool3d',
         'HardTanh': 'Hardtanh',
         'HardShrink': 'Hardshrink',
         'SoftPlus': 'Softplus',
