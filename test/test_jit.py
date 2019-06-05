bb52b5cfe802a08aa35a2146cd4063c9e93f9f1e
Horace He <chilli@fb.com>
2019-06-04 19:41:04 -0700

    Update on "Added factorial"
    Added factorial
    
    gh-metadata: pytorch pytorch 21129 gh/chillee/7/head

--- test/test_jit.py
+++ test/test_jit.py
@@ -6138,7 +6138,7 @@ a")
             checkMathWrap("gcd", 2, is_float=False, ret_type="int")
         if PY37:
             checkMathWrap("remainder", 2)
-        checkMathWrap("factorial", 1, is_float=False, ret_type="int", vals=[(i,i) for i in range(-2, 10)])
+        checkMathWrap("factorial", 1, is_float=False, ret_type="int", vals=[(i, i) for i in range(-2, 10)])
 
     @unittest.skipIf(PY2, "Requires python 3")
     def test_math_gcd(self):
