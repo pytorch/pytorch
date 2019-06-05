f7bfc6b42af5d0803d62786f943c4a68f1279e11
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-06-04 19:10:36 -0700

    Move THCTensor_(exponential) to ATen (#21297)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21297
    ghimport-source-id: 5f45154e714ab44dec961dabf1c64e54aaa063a2
    
    Reviewed By: jerryzh168
    
    Differential Revision: D15632931
    
    Pulled By: ezyang
    
    fbshipit-source-id: 0367eec0a9ef6812b1b3ab7597817ee40a011bb8

--- aten/src/THC/THCTensorRandom.cu
+++ aten/src/THC/THCTensorRandom.cu
@@ -129,11 +129,6 @@ __global__ void NAME(curandStateMtgp32 *state, int size, T *result, ARG1, ARG2) 
   }                                                                                  \
 }
 
-GENERATE_KERNEL1(generate_exponential, float, double lambda, float, curand_uniform, (float)(-1. / lambda * log(x)))
-GENERATE_KERNEL1(generate_exponential, double, double lambda, double, curand_uniform_double, (double)(-1. / lambda * log(x)))
-
-GENERATE_KERNEL1(generate_exponential, at::Half, double lambda, float, curand_uniform, (ScalarConvert<float, at::Half>::to((float)(-1. / lambda * log(x)))))
-
 #include <THC/generic/THCTensorRandom.cu>
 #include <THC/THCGenerateAllTypes.h>
 
