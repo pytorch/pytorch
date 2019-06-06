8c426cd80d1b815b807fe0aa3575ff481c6246a1
davidriazati <davidriazati@fb.com>
2019-06-06 11:55:44 -0700

    Make ScriptModule.training an attribute instead of a parameter (#21078)
    Summary:
    Redo of #19587
    ](https://our.intern.facebook.com/intern/diff/15560540/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21078
    
    Pulled By: driazati
    
    Differential Revision: D15560540
    
    fbshipit-source-id: f415775d87c163f93b3bbdd5f87c9ff73f58b049

--- test/test_jit.py
+++ test/test_jit.py
@@ -3487,6 +3487,7 @@ def foo(x):
         self.assertEqual(4, w(3))
         w.train(False)
         self.assertEqual(7, w(3))
+        self.assertFalse("training" in w.state_dict())
 
     def test_jitter_bug(self):
         @torch.jit.script
