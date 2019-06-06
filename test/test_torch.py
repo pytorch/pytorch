3dd8f40b9f7be7fd1d5e8c4871be5eb7c042ceff
Gregory Chanan <gchanan@fb.com>
2019-06-06 15:26:00 -0700

    Fix size of histc return on CPU when input is 0-dimensional and bins=1.
    This triggers the wrong scalar_check unless it is explicitly overridden (because the heuristic uses the shape of the input).
    This wasn't an issue on CUDA because it didn't use TH, so doesn't have scalar checks.

--- test/test_torch.py
+++ test/test_torch.py
@@ -2646,6 +2646,13 @@ class _TestTorchMixin(object):
                 torch.tensor([0, 2, 1, 0], dtype=torch.float, device=device),
                 actual)
             self.assertEqual(actual.dtype, torch.float)
+            # scalar input and 1 bin -- should return a 1-dimensional tensor, not a scalar.
+            actual = torch.histc(
+                torch.tensor(0, dtype=torch.float, device=device),
+                bins=1, min=0, max=3)
+            self.assertEqual(
+                torch.tensor([1], dtype=torch.float, device=device),
+                actual)
 
         # test against numpy.histogram()
         def test_against_np(tensor, bins=100, min=0, max=0):
