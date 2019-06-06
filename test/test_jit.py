e1fc9307cb55fcbe3c9b4283dfd6c03abe3f4997
davidriazati <davidriazati@fb.com>
2019-06-06 13:40:23 -0700

    [jit] Move Python 3 tests to their own file

--- test/test_jit.py
+++ test/test_jit.py
@@ -16453,8 +16453,6 @@ for test in criterion_tests:
     add_nn_module_test(**test)
 
 if __name__ == '__main__':
-    run_tests()
     if not PY2:
-        import test_jit_py3
-        suite = unittest.findTestCases(test_jit_py3)
-        unittest.TextTestRunner().run(suite)
+        from test_jit_py3 import TestScriptPy3
+    run_tests()
