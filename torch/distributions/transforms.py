Fix numerically instability of `SigmoidTransform` (#19802)
Summary:
fix #18254 for numerically instability of `SigmoidTransform`
Pull Request resolved: https://github.com/pytorch/pytorch/pull/19802

Differential Revision: D15701837

Pulled By: ezyang

fbshipit-source-id: fe6c755c523487c8bbdcc3bfb8455801617c70a4

torch/distributions/transforms.py
@@ -3,6 +3,7 @@ import numbers
 import weakref
 
 import torch
+import torch.nn.functional as F
 from torch.distributions import constraints
 from torch.distributions.utils import (_sum_rightmost, broadcast_all,
                                        lazy_property)
@@ -359,7 +360,7 @@ class SigmoidTransform(Transform):
         return y.log() - (-y).log1p()
 
     def log_abs_det_jacobian(self, x, y):
-        return -(y.reciprocal() + (1 - y).reciprocal()).log()
+        return -F.softplus(-x) - F.softplus(x)
 
 
 class AbsTransform(Transform):