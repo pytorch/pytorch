085a99c47e2c5ba506275f295ed0102866903aa1
mal <mal@devgpu005.ash6.facebook.com>
2019-06-07 16:28:12 -0400

    Update on "Don't leak threads on exit"
    Don't leak threads on exit
    
    gh-metadata: pytorch pytorch 21438 gh/mal2147/2/head

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -3127,20 +3127,7 @@ class TestAutograd(TestCase):
         b.data = a
         self.assertTrue(b_id_saved == id(b))
 
-    def _run_and_capture_stderr(self, code):
-        import os
-        import subprocess
-
-        env = os.environ.copy()
-        env["PYTORCH_API_USAGE_STDERR"] = "1"
-        pipes = subprocess.Popen(
-            [sys.executable, '-c', code],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            env=env)
-        return pipes.communicate()[1].decode('ascii')
-
-    @unittest.skipIf(IS_WINDOWS, "Skip for windows")
+    @unittest.skipIf(IS_WINDOWS, "Skipping because doesn't work for windows")
     def test_thread_shutdown(self):
         code = """import torch
 from torch.autograd import Function
@@ -3157,7 +3144,7 @@ for shape in [(1,), ()]:
     v = torch.ones(shape, requires_grad=True)
     MyFunction.apply(v).backward()
 """
-        s = self._run_and_capture_stderr(code)
+        s = TestCase.runWithPytorchAPIUsageStderr(code)
         self.assertRegex(s, "PYTORCH_API_USAGE torch.autograd.thread_shutdown")
 
 def index_variable(shape, max_indices):
