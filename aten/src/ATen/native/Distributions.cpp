c820c8c322c5000fcf2b1ae1c96b879f1038b2ac
Abdullah Al Mamun <amamun@u.rochester.edu>
2019-06-06 21:47:48 -0400

    wrote new func for log_normal_cpu

--- aten/src/ATen/native/Distributions.cpp
+++ aten/src/ATen/native/Distributions.cpp
@@ -171,6 +171,17 @@ Tensor& bernoulli_scalar_cpu_(Tensor& self, double p, Generator* gen) {
   return self;
 }
 
+Tensor& log_normal_cpu_(Tensor& self, double mean, double stdv, Generator* gen) {
+  AT_DISPATCH_ALL_TYPES(self.scalar_type(), "log_normal_cpu_", [&] {
+    THGenerator* generator = get_generator(gen);
+    std::lock_guard<std::mutex> lock(generator->mutex);
+    CPU_tensor_apply1<scalar_t>(
+        self, [generator, mean, stdv](scalar_t& ret_val) {
+          ret_val = static_cast<scalar_t>(THRandom_logNormal(generator, mean, stdv));
+        });
+  });
+  return self;
+}
 
 Tensor _standard_gamma_grad_cpu(const Tensor& self, const Tensor& output) {
   Tensor ret = at::empty(self.sizes(), self.options());
