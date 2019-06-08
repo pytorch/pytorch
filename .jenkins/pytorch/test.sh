5756c83c4c394fe717aff17b06a2501841d429ff
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-06-08 21:02:14 +0800

    make tests deterministic

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -38,7 +38,7 @@ if [[ "$BUILD_ENVIRONMENT" != *ppc64le* ]]; then
   pip install -q ninja --user
   # ninja is installed in /var/lib/jenkins/.local/bin
   export PATH="/var/lib/jenkins/.local/bin:$PATH"
-
+  export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
   # TODO: move this to Docker
   pip install -q hypothesis --user
 
