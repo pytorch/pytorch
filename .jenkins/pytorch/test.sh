954d02fb9181a4dc740f078fa8f8af86beb299cf
Tzu-Wei Huang <huang.dexter@gmail.com>
2019-06-09 12:34:36 +0800

    export variable

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -103,7 +103,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *-NO_AVX2-* ]]; then
 fi
 
 test_python_nn() {
-  time PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python test/run_test.py --include tensorboard --verbose
+  export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+  time python test/run_test.py --include tensorboard --verbose
   assert_git_not_dirty
 }
 
