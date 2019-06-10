8085c70395429537e18d186bb37bcc51afe5268c
Sam Gross <colesbury@gmail.com>
2019-06-10 13:44:05 -0700

    [TESTING] Enable stream switching on test_advancedindex

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2409,7 +2409,6 @@ class TestCuda(TestCase):
     def test_index(self):
         _TestTorchMixin._test_index(self, lambda t: t.cuda())
 
-    @skipCUDANonDefaultStreamIf(True)
     def test_advancedindex(self):
         _TestTorchMixin._test_advancedindex(self, lambda t: t.cuda())
 
