8b9b215dc50915ade01611448e672ae4235f50bb
Stefan Krah <skrah@bytereef.org>
2019-06-10 15:10:08 -0700

    Add a 'dim' argument to nuclear norm (#21022)
    Summary:
    Addresses #18275.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21022
    
    Differential Revision: D15743515
    
    Pulled By: ezyang
    
    fbshipit-source-id: e4aaea0bd7f863a2abad45c4322d6a9fb02a88e3

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2674,6 +2674,15 @@ class TestCuda(TestCase):
     def test_norm(self):
         _TestTorchMixin._test_norm(self, device='cuda')
 
+    @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
+    @unittest.skipIf(not TEST_MAGMA, "no MAGMA library detected")
+    def test_nuclear_norm_axes_small_brute_force(self):
+        _TestTorchMixin._test_nuclear_norm_axes(self, device='cuda')
+
+    @unittest.skipIf(not TEST_MAGMA, "no MAGMA library detected")
+    def test_nuclear_norm_exceptions(self):
+        _TestTorchMixin._test_nuclear_norm_exceptions(self, device='cuda')
+
     def test_dist(self):
         _TestTorchMixin._test_dist(self, device='cuda')
 
