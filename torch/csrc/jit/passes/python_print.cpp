ea4caef03cafe7cadde34f97421bb15a24a99015
Michael Suo <suo@fb.com>
2019-06-10 16:31:18 -0700

    Split out Function into its own file (#21539)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21539
    ghimport-source-id: f1e4396a0bec6e30d3179f926ec4da68807942f7
    
    Differential Revision: D15741979
    
    Pulled By: suo
    
    fbshipit-source-id: 4cd0ed36bcbf8db0b36a101dda6f58975f806889

--- torch/csrc/jit/passes/python_print.cpp
+++ torch/csrc/jit/passes/python_print.cpp
@@ -1031,7 +1031,7 @@ struct PythonPrintPass {
   }
 
  public:
-  void printFunction(const script::Function& func) {
+  void printFunction(const Function& func) {
     const FunctionSchema& schema = func.getSchema();
     Graph& graph = *func.graph();
     used_names_.clear(); // each graph can reuse local names
@@ -1122,7 +1122,7 @@ struct PythonPrintPass {
 
 void PythonPrint(
     std::ostream& out,
-    const script::Function& func,
+    const Function& func,
     bool is_method,
     std::vector<at::Tensor>& tensor_table,
     std::vector<ClassTypePtr>& class_table,
