e370ff0b2b36998ef638dc95c424cc7726c2d005
Bram Wasti <bwasti@fb.com>
2019-06-11 13:50:31 -0700

    [fix] Fix up repr on scripted modules

--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -13,8 +13,8 @@ from ..._jit_internal import weak_module, weak_script_method, List
 class _ConvNd(Module):
 
     __constants__ = ['stride', 'padding', 'dilation', 'groups', 'bias',
-                     'padding_mode', 'output_padding', 'in_channels',
-                     'out_channels', 'kernel_size']
+                     'padding_mode', 'output_padding', 'transposed',
+                     'kernel_size', 'in_channels', 'out_channels']
 
     def __init__(self, in_channels, out_channels, kernel_size, stride,
                  padding, dilation, transposed, output_padding,
