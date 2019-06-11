50b49647dcbadcd54697de5f6a01b5cf6e38303e
Ejaaz Merali <emerali@users.noreply.github.com>
2019-06-11 15:07:08 -0700

    Fix momentum bug in CyclicLR (#20401)
    Summary:
    Resolves issue https://github.com/pytorch/pytorch/issues/19003
    
    The author of this issue also asked that `cycle_momentum` default to `False` if the optimizer does not have a momentum parameter, but I'm not sure what the best way to do this would be. Silently changing the value based on the optimizer may confuse the user in some cases (say the user explicitly set `cycle_momentum=True` but doesn't know that the Adam optimizer doesn't use momentum).
    
    Maybe printing a warning when switching this argument's value would suffice?
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/20401
    
    Differential Revision: D15765463
    
    Pulled By: ezyang
    
    fbshipit-source-id: 88ddabd9e960c46f3471f37ea46013e6b4137eaf

--- test/test_optim.py
+++ test/test_optim.py
@@ -971,6 +971,34 @@ class TestLRScheduler(TestCase):
                              mode='exp_range', gamma=gamma)
         self._test_cycle_lr(scheduler, lr_targets, momentum_targets, len(lr_target))
 
+    def test_cycle_lr_with_momentumless_optimizer(self):
+        # Note [Temporarily set optimizer to Adam]
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # The TestLRScheduler object carries around an SGD optimizer to avoid having to
+        # instantiate one for every test. This gets in the way for our very specific case
+        # in which we need to use Adam (or really any optimizer that doesn't use momentum)
+        # in order to test that the momentum bug in CyclicLR is fixed (the bug is described
+        # in more detail in https://github.com/pytorch/pytorch/issues/19003 ).
+        old_opt = self.opt
+        self.opt = optim.Adam(
+            [{'params': self.net.conv1.parameters()}, {'params': self.net.conv2.parameters(), 'lr': 0.5}],
+            lr=0.05)
+
+        lr_target = [1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3]
+        lr_targets = [lr_target, lr_target]
+        momentum_target = [None] * len(lr_target)
+        momentum_targets = [momentum_target, momentum_target]
+        scheduler = CyclicLR(self.opt, base_lr=1, max_lr=5, step_size_up=4,
+                             cycle_momentum=False, mode='triangular')
+        self._test_cycle_lr(scheduler, lr_targets, momentum_targets, len(lr_target))
+
+        self.opt = old_opt  # set optimizer back to SGD
+
+    def test_cycle_lr_cycle_momentum_fail_with_momentumless_optimizer(self):
+        with self.assertRaises(ValueError):
+            adam_opt = optim.Adam(self.net.parameters())
+            scheduler = CyclicLR(adam_opt, base_lr=1, max_lr=5, cycle_momentum=True)
+
     def test_lambda_lr(self):
         epochs = 10
         self.opt.param_groups[0]['lr'] = 0.05
@@ -1168,17 +1196,23 @@ class TestLRScheduler(TestCase):
         for batch_num in range(batch_iterations):
             scheduler.step(batch_num)
             if verbose:
-                print('batch{}:\tlr={},momentum={}'.format(batch_num, self.opt.param_groups[0]['lr'],
-                                                           self.opt.param_groups[0]['momentum']))
+                if 'momentum' in self.opt.param_groups[0].keys():
+                    print('batch{}:\tlr={},momentum={}'.format(batch_num, self.opt.param_groups[0]['lr'],
+                                                               self.opt.param_groups[0]['momentum']))
+                else:
+                    print('batch{}:\tlr={}'.format(batch_num, self.opt.param_groups[0]['lr']))
+
             for param_group, lr_target, momentum_target in zip(self.opt.param_groups, lr_targets, momentum_targets):
                 self.assertAlmostEqual(
                     lr_target[batch_num], param_group['lr'],
                     msg='LR is wrong in batch_num {}: expected {}, got {}'.format(
                         batch_num, lr_target[batch_num], param_group['lr']), delta=1e-5)
-                self.assertAlmostEqual(
-                    momentum_target[batch_num], param_group['momentum'],
-                    msg='Momentum is wrong in batch_num {}: expected {}, got {}'.format(
-                        batch_num, momentum_target[batch_num], param_group['momentum']), delta=1e-5)
+
+                if 'momentum' in param_group.keys():
+                    self.assertAlmostEqual(
+                        momentum_target[batch_num], param_group['momentum'],
+                        msg='Momentum is wrong in batch_num {}: expected {}, got {}'.format(
+                            batch_num, momentum_target[batch_num], param_group['momentum']), delta=1e-5)
 
 if __name__ == '__main__':
     run_tests()
