4343dd1d4bf0cd4c306de87cca3f5ba9b381f1c7
Richard Zou <zou3519@gmail.com>
2019-06-12 12:20:58 -0700

    Update on "Expose torch.empty(sizes, *, names, ...) to Python"
    Expose torch.empty(sizes, *, names, ...) to Python
    
    There are a few main components to this:
    
    Implement `tensor.names` in Python.
    - This results a `List[Optional[str]]`. In the future it'll return a
    `List[Optional[torch.Dimname]]` when we implement torch.Dimname objects,
    but now it is like this for testing.
    
    PythonArgParser learns how to handle `DimnameList?`
    - PythonArgParser stores an InternedStringsTable that maps interned
    PyObject* to at::Dimname objects. In cpp-land we use at::Dimname, in
    python land we work with strings.
    - PythonArgParser converts input Nones and strings to at::Dimname.
    - None gets converted to Dimname::wildcard()
    - PythonArgParser interns a python string if it is not interned (the
    mapping only stores interned strings).
    - If the interned string is not in the table, PythonArgParser parses it
    to get an at::Dimname and adds the (interned string, Dimname) mapping to
    the table.
    
    ![image](https://user-images.githubusercontent.com/5652049/59299353-7175c680-8c5a-11e9-8f57-220cce9cbec7.png)
    
    unpacking `optional<DimnameList>`.
    - We get an `optional<vector<Dimname>>` from `PythonArgs`. However, this
    needs to be converted to an `optional<ArrayRef<Dimname>>` (because that
    is what `at::empty` takes as an argument).
    - To do this, I added extra codegen to unwrap
    `optional<vector<Dimname>>` to `vector<Dimname>>`, convert that to
    `ArrayRef<Dimname>`, and then rewrap it to
    `optional<ArrayRef<Dimname>>`.
    
    Test Plan:
    - `python test/test_namedtensor.py -v`
    - New tests [namedtensor ci]
    
    gh-metadata: pytorch pytorch 21648 gh/zou3519/48/head

--- test/test_namedtensor.py
+++ test/test_namedtensor.py
@@ -2,6 +2,7 @@ import unittest
 from common_utils import TestCase, run_tests
 from common_cuda import TEST_CUDA
 import torch
+import sys
 
 
 def namedtensor_enabled():
@@ -10,6 +11,10 @@ def namedtensor_enabled():
 skipIfNamedTensorDisabled = \
     unittest.skipIf(not namedtensor_enabled(),
                     'PyTorch not compiled with namedtensor support')
+
+def pass_name_to_python_arg_parser(name):
+    x = torch.empty(2, names=(name,))
+
 
 class TestNamedTensor(TestCase):
     @skipIfNamedTensorDisabled
@@ -52,6 +57,38 @@ class TestNamedTensor(TestCase):
     def test_empty_cuda(self):
         self._test_factory(torch.empty, 'cuda')
 
+    @skipIfNamedTensorDisabled
+    def test_using_unseen_interned_string_bumps_refcount_permanently(self):
+        # Please don't use this as a name in a different test.
+        unseen_name = 'abcdefghi'
+        old_refcnt = sys.getrefcount(unseen_name)
+
+        pass_name_to_python_arg_parser(unseen_name)
+
+        new_refcnt = sys.getrefcount(unseen_name)
+        self.assertEqual(new_refcnt, old_refcnt + 1)
+
+    @skipIfNamedTensorDisabled
+    def test_using_unseen_uninterned_string_refcounts(self):
+        # Please don't use this as a name in a different test.
+        # non-compile-time constants are not interned
+        unseen_name = ''.join(['abc', 'def', 'ghi', 'jkl'])
+        interned_unseen_name = 'abcdefghijkl'
+        self.assertFalse(unseen_name is interned_unseen_name)
+
+        old_uninterned_refcnt = sys.getrefcount(unseen_name)
+        old_interned_refcnt = sys.getrefcount(interned_unseen_name)
+
+        pass_name_to_python_arg_parser(unseen_name)
+
+        new_uninterned_refcnt = sys.getrefcount(unseen_name)
+        new_interned_refcnt = sys.getrefcount(interned_unseen_name)
+
+        # Internally, PyTorch should not hold a reference to the uninterned string
+        self.assertEqual(new_uninterned_refcnt, old_uninterned_refcnt)
+
+        # Instead, we should hold a new reference to the interned version.
+        self.assertEqual(new_interned_refcnt, old_interned_refcnt + 1)
 
 if __name__ == '__main__':
     run_tests()
