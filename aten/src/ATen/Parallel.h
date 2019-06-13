5bf8346a89fc0c9b9135198b4162d39600fa5165
ilia-cher <iliacher@fb.com>
2019-06-13 16:46:42 -0700

    Future interface for ATen/Parallel
    Summary:
    Adding std::future interface into ATen/Parallel for convenience

--- aten/src/ATen/Parallel.h
+++ aten/src/ATen/Parallel.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <ATen/ATen.h>
 
+#include <future>
+
 namespace at {
 namespace internal {
 // This parameter is heuristically chosen to determine the minimum number of
@@ -106,6 +108,9 @@ CAFFE2_API void launch(std::function<void()> func);
 
 // Launches intra-op parallel task
 CAFFE2_API void intraop_launch(std::function<void()> func);
+
+// Launches intra-op parallel task, returns a future
+CAFFE2_API std::future<void> intraop_launch_future(std::function<void()> func);
 
 // Returns number of intra-op threads used by default
 CAFFE2_API int intraop_default_num_threads();
