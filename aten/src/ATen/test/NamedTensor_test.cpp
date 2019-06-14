4cb34be89b0b1110b78f70928ea209c40ef44e41
Richard Zou <zou3519@gmail.com>
2019-06-14 08:35:26 -0700

    Move at::internal_set_names_inplace to ATen/NamedTensorUtils.h
    I'm planning on dumping helper functions here in the near future.
    
    Test Plan:
    - [namedtensor ci]

--- aten/src/ATen/test/NamedTensor_test.cpp
+++ aten/src/ATen/test/NamedTensor_test.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 #include <ATen/ATen.h>
-#include <ATen/NamedTensor.h>
+#include <ATen/NamedTensorUtils.h>
 #include <c10/util/Exception.h>
 #include <torch/csrc/utils/memory.h>
 
