2c4afda83fc9c77c750671ddb3becc644508dd18
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-06-14 00:17:01 -0500

    Port vol2col

--- aten/src/THNN/generic/VolumetricFullDilatedConvolution.c
+++ aten/src/THNN/generic/VolumetricFullDilatedConvolution.c
@@ -1,88 +1,8 @@
+#include <ATen/native/vol2col.h>
+
 #ifndef TH_GENERIC_FILE
 #define TH_GENERIC_FILE "THNN/generic/VolumetricFullDilatedConvolution.c"
 #else
-
-static void THNN_(vol2col)(
-  const scalar_t *data_vol, const int64_t channels,
-  const int64_t depth, const int64_t height, const int64_t width,
-  const int64_t depth_col, const int64_t height_col, const int64_t width_col,
-  const int64_t kT, const int64_t kH, const int64_t kW,
-  const int64_t pT, const int64_t pH, const int64_t pW,
-  const int64_t dT, const int64_t dH, const int64_t dW,
-  const int64_t dilationT, const int64_t dilationH, const int64_t dilationW,
-  scalar_t *data_col)
-{
-  int64_t c, t, h, w;
-  int64_t channels_col = channels * kT * kH * kW;
-  for (c = 0; c < channels_col; ++c)
-  {
-    int64_t w_offset = c % kW;
-    int64_t h_offset = (c / kW) % kH;
-    int64_t t_offset = (c / kW / kH) % kT;
-    int64_t c_vol = c / kT / kH / kW;
-    for (t = 0; t < depth_col; ++t)
-    {
-      int64_t t_pad = t * dT - pT + t_offset * dilationT;
-      for (h = 0; h < height_col; ++h)
-      {
-        int64_t h_pad = h * dH - pH + h_offset * dilationH;
-        for (w = 0; w < width_col; ++w)
-        {
-          int64_t w_pad = w * dW - pW + w_offset * dilationW;
-          if (t_pad >= 0 && t_pad < depth &&
-              h_pad >= 0 && h_pad < height &&
-              w_pad >= 0 && w_pad < width)
-            data_col[((c * depth_col + t) * height_col + h) * width_col + w] =
-              data_vol[((c_vol * depth + t_pad) * height + h_pad) * width + w_pad];
-          else
-            data_col[((c * depth_col + t) * height_col + h) * width_col + w] = 0;
-        }
-      }
-    }
-  }
-}
-
-static void THNN_(col2vol)(
-  const scalar_t* data_col, const int64_t channels,
-  const int64_t depth, const int64_t height, const int64_t width,
-  const int64_t out_depth, const int64_t out_height, const int64_t out_width,
-  const int64_t kT, const int64_t kH, const int64_t kW,
-  const int64_t pT, const int64_t pH, const int64_t pW,
-  const int64_t dT, const int64_t dH, const int64_t dW,
-  const int64_t dilationT, const int64_t dilationH, const int64_t dilationW,
-  scalar_t* data_vol)
-{
-  int64_t c, t, h, w;
-  memset(data_vol, 0, sizeof(scalar_t) * depth * height * width * channels);
-  int64_t depth_col  = out_depth;
-  int64_t height_col = out_height;
-  int64_t width_col  = out_width;
-  int64_t channels_col = channels * kT * kH * kW;
-  for (c = 0; c < channels_col; ++c)
-  {
-    int64_t w_offset = c % kW;
-    int64_t h_offset = (c / kW) % kH;
-    int64_t t_offset = (c / kW / kH) % kT;
-    int64_t c_vol = c / kT / kH / kW;
-    for (t = 0; t < depth_col; ++t)
-    {
-      int64_t t_pad = t * dT - pT + t_offset * dilationT;
-      for (h = 0; h < height_col; ++h)
-      {
-        int64_t h_pad = h * dH - pH + h_offset * dilationH;
-        for (w = 0; w < width_col; ++w)
-        {
-          int64_t w_pad = w * dW - pW + w_offset * dilationW;
-          if (t_pad >= 0 && t_pad < depth &&
-              h_pad >= 0 && h_pad < height &&
-              w_pad >= 0 && w_pad < width)
-            data_vol[((c_vol * depth + t_pad) * height + h_pad) * width + w_pad] +=
-              data_col[((c * depth_col + t) * height_col + h) * width_col + w];
-        }
-      }
-    }
-  }
-}
 
 static inline void THNN_(VolumetricFullDilatedConvolution_shapeCheck)(
                          THTensor *input, THTensor *gradOutput,
@@ -255,7 +175,7 @@ void THNN_(VolumetricFullDilatedConvolution_updateOutput)(
     );
 
     // Unpack columns back into input:
-    THNN_(col2vol)(
+    at::native::col2vol<scalar_t>(
       columns->data<scalar_t>(),
       nOutputPlane, outputDepth, outputHeight, outputWidth,
       inputDepth, inputHeight, inputWidth,
@@ -370,7 +290,7 @@ void THNN_(VolumetricFullDilatedConvolution_updateGradInput)(
     THTensor_(select)(gradOutput_n, gradOutput, 0, elt);
 
     // Extract columns:
-    THNN_(vol2col)(
+    at::native::vol2col<scalar_t>(
       gradOutput_n->data<scalar_t>(),
       nOutputPlane, outputDepth, outputHeight, outputWidth,
       inputDepth, inputHeight, inputWidth,
@@ -506,7 +426,7 @@ void THNN_(VolumetricFullDilatedConvolution_accGradParameters)(
       THTensor_(select)(input_n, input, 0, elt);
 
       // Extract columns:
-      THNN_(vol2col)(
+      at::native::vol2col<scalar_t>(
         gradOutput_n->data<scalar_t>(), nOutputPlane,
         outputDepth, outputHeight, outputWidth,
         inputDepth, inputHeight, inputWidth,
