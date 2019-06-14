5ee6de05de5c766bc46f3bd6edce640b63bff18a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-06-14 16:33:39 -0500

    Applied recommendations from review

--- aten/src/ATen/native/Col2Im.cpp
+++ aten/src/ATen/native/Col2Im.cpp
@@ -5,6 +5,7 @@
 #include <ATen/div_rtn.h>
 
 #include <ATen/native/im2col.h>
+#include <ATen/native/im2col_shape_check.h>
 
 // Note [im2col/col2im output padding]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -62,119 +63,9 @@ namespace at {
 namespace native {
 namespace {
 
-static inline void col2im_shape_check(
-    const Tensor& input,
-    const Tensor& grad_output,
-    int64_t output_height,
-    int64_t output_width,
-    int64_t kernel_height,
-    int64_t kernel_width,
-    int64_t dilation_height,
-    int64_t dilation_width,
-    int64_t pad_height,
-    int64_t pad_width,
-    int64_t stride_height,
-    int64_t stride_width) {
-  TORCH_CHECK(
-      kernel_width > 0 && kernel_height > 0,
-      "kernel size should be greater than zero, but got kernel_height: ",
-      kernel_height,
-      " kernel_width: ",
-      kernel_width);
-  TORCH_CHECK(
-      stride_width > 0 && stride_height > 0,
-      "stride should be greater than zero, but got stride_height: ",
-      stride_height,
-      " stride_width: ",
-      stride_width);
-  TORCH_CHECK(
-      dilation_width > 0 && dilation_height > 0,
-      "dilation should be greater than zero, but got dilation_height: ",
-      dilation_height,
-      " dilation_width: ",
-      dilation_width);
-
-  int64_t ndim = input.ndimension();
-  TORCH_CHECK(
-      input.numel() != 0 && (ndim == 2 || ndim == 3),
-      "Expected non-empty 2D or 3D input tensor, but got input of sizes",
-      input.sizes());
-
-  int64_t batch_dim = (ndim == 3) ? 0 : -1;
-  int64_t n_input_plane = input.size(batch_dim + 1);
-
-  if (n_input_plane % (kernel_width * kernel_height) != 0) {
-    AT_ERROR(
-        "Expected size of input's dimension 1 to be divisible by the "
-        "product of kernel_size, but got input.size(1)=",
-        (long long)n_input_plane,
-        " and kernel_size=(",
-        kernel_height,
-        ", ",
-        kernel_width,
-        ").");
-  }
-
-  int64_t input_length = input.size(batch_dim + 2);
-  int64_t nBlockstride_height =
-      div_rtn<int64_t>(
-          output_height + 2 * pad_height -
-              dilation_height * (kernel_height - 1) - 1,
-          stride_height) +
-      1;
-  int64_t nBlockstride_width = div_rtn<int64_t>(
-                                   output_width + 2 * pad_width -
-                                       dilation_width * (kernel_width - 1) - 1,
-                                   stride_width) +
-      1;
-
-  if (input_length != (nBlockstride_height * nBlockstride_width)) {
-    AT_ERROR(
-        "Given output_size=(",
-        output_height,
-        ", ",
-        output_width,
-        "), kernel_size=(",
-        kernel_height,
-        ", ",
-        kernel_width,
-        "), dilation=(",
-        dilation_height,
-        ", ",
-        dilation_width,
-        "), padding=(",
-        pad_height,
-        ", ",
-        pad_width,
-        "), stride=(",
-        stride_height,
-        ", ",
-        stride_width,
-        "), expected size of input's dimension 2 to match the calculated number of ",
-        "sliding blocks ",
-        (long long)nBlockstride_height,
-        " * ",
-        (long long)nBlockstride_width,
-        " = ",
-        (long long)(nBlockstride_height * nBlockstride_width),
-        ", but got input.size(2)=",
-        (long long)input_length,
-        ".");
-  }
-
-  if (output_width < 1 || output_height < 1) {
-    AT_ERROR(
-        "Expected output spatial size to be positive, but got: output_size=(",
-        output_height,
-        ", ",
-        output_width,
-        ").");
-  }
-}
-
 static void col2im_out_cpu_template(
     Tensor& output,
-    Tensor& input_,
+    const Tensor& input_,
     IntArrayRef output_size,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
@@ -246,69 +137,82 @@ static void col2im_out_cpu_template(
   output.resize_({batch_size, n_output_plane, output_height, output_width});
   output.zero_();
 
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.scalar_type(), "col2im_out_cpu", [&] {
-    Tensor input_n = Tensor();
-    Tensor output_n = Tensor();
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+      input.scalar_type(), "col2im_out_cpu", [&] {
+        Tensor input_n = Tensor();
+        Tensor output_n = Tensor();
 
-    int64_t height_col = (output_height + 2 * pad_height -
-                          (dilation_height * (kernel_height - 1) + 1)) /
-            stride_height +
-        1;
-    int64_t width_col = (output_width + 2 * pad_width -
-                         (dilation_width * (kernel_width - 1) + 1)) /
-            stride_width +
-        1;
+        int64_t height_col = (output_height + 2 * pad_height -
+                              (dilation_height * (kernel_height - 1) + 1)) /
+                stride_height +
+            1;
+        int64_t width_col = (output_width + 2 * pad_width -
+                             (dilation_width * (kernel_width - 1) + 1)) /
+                stride_width +
+            1;
 
-    for (int64_t elt = 0; elt < batch_size; elt++) {
-      input_n = input.select(0, elt);
-      output_n = output.select(0, elt);
+        for (int64_t elt = 0; elt < batch_size; elt++) {
+          input_n = input.select(0, elt);
+          output_n = output.select(0, elt);
 
-      col2im<scalar_t>(
-          input_n.data<scalar_t>(),
-          n_output_plane,
-          output_height,
-          output_width,
-          height_col,
-          width_col,
-          kernel_height,
-          kernel_width,
-          pad_height,
-          pad_width,
-          stride_height,
-          stride_width,
-          dilation_height,
-          dilation_width,
-          output_n.data<scalar_t>());
-    }
+          col2im<scalar_t>(
+              input_n.data<scalar_t>(),
+              n_output_plane,
+              output_height,
+              output_width,
+              height_col,
+              width_col,
+              kernel_height,
+              kernel_width,
+              pad_height,
+              pad_width,
+              stride_height,
+              stride_width,
+              dilation_height,
+              dilation_width,
+              output_n.data<scalar_t>());
+        }
 
-    if (!batched_input) {
-      output.resize_({n_output_plane, output_height, output_width});
-    }
-  });
+        if (!batched_input) {
+          output.resize_({n_output_plane, output_height, output_width});
+        }
+      });
 }
 
 void col2im_backward_out_cpu_template(
     Tensor& grad_input,
-    Tensor& grad_output,
+    const Tensor& grad_output,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
     IntArrayRef stride) {
   // im2col_out_cpu checks size of kernel_size, dilation, padding and stride
-  grad_input = im2col_cpu(
-      grad_output, kernel_size, dilation, padding, stride);
+  im2col_out_cpu(
+      grad_input, grad_output, kernel_size, dilation, padding, stride);
 }
 
 } // namespace
 
-Tensor col2im_cpu(
-    const Tensor& input_,
+Tensor& col2im_out_cpu(
+    Tensor& output,
+    const Tensor& input,
     IntArrayRef output_size,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
     IntArrayRef stride) {
-  Tensor input = input_;
+  col2im_out_cpu_template(
+      output, input, output_size, kernel_size, dilation, padding, stride);
+  return output;
+}
+
+Tensor col2im_cpu(
+    const Tensor& input,
+    IntArrayRef output_size,
+    IntArrayRef kernel_size,
+    IntArrayRef dilation,
+    IntArrayRef padding,
+    IntArrayRef stride) {
   Tensor output = at::empty_like(input);
 
   col2im_out_cpu_template(
@@ -316,13 +220,24 @@ Tensor col2im_cpu(
   return output;
 }
 
-Tensor col2im_backward_cpu(
-    const Tensor& grad_output_,
+Tensor& col2im_backward_out_cpu(
+    Tensor& grad_input,
+    const Tensor& grad_output,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
     IntArrayRef stride) {
-  Tensor grad_output = grad_output_;
+  col2im_backward_out_cpu_template(
+      grad_input, grad_output, kernel_size, dilation, padding, stride);
+  return grad_input;
+}
+
+Tensor col2im_backward_cpu(
+    const Tensor& grad_output,
+    IntArrayRef kernel_size,
+    IntArrayRef dilation,
+    IntArrayRef padding,
+    IntArrayRef stride) {
   Tensor grad_input = at::empty_like(grad_output);
 
   col2im_backward_out_cpu_template(
--- aten/src/ATen/native/cuda/Im2Col.cu
+++ aten/src/ATen/native/cuda/Im2Col.cu
@@ -9,106 +9,15 @@
 #include <ATen/cuda/CUDAApplyUtils.cuh>
 
 #include <ATen/native/cuda/im2col.cuh>
+#include <ATen/native/im2col_shape_check.h>
 
 namespace at {
 namespace native {
 namespace {
 
-static inline void im2col_shape_check(
-    const Tensor& input,
-    const Tensor& grad_output,
-    int64_t kernel_height,
-    int64_t kernel_width,
-    int64_t dilation_height,
-    int64_t dilation_width,
-    int64_t pad_height,
-    int64_t pad_width,
-    int64_t stride_height,
-    int64_t stride_width) {
-  TORCH_CHECK(
-      kernel_width > 0 && kernel_height > 0,
-      "kernel size should be greater than zero, but got kernel_height: ",
-      kernel_height,
-      " kernel_width: ",
-      kernel_width);
-
-  TORCH_CHECK(
-      dilation_width > 0 && dilation_height > 0,
-      "dilation should be greater than zero, but got dilation_height: ",
-      dilation_height,
-      " dilation_width: ",
-      dilation_width);
-  
-  TORCH_CHECK(
-      pad_width >= 0 && pad_height >= 0,
-      "padding should be non-negative, but got pad_height: ",
-      pad_height,
-      " pad_width: ",
-      pad_width);
-  
-  TORCH_CHECK(
-      stride_width > 0 && stride_height > 0,
-      "stride should be greater than zero, but got stride_height: ",
-      stride_height,
-      " stride_width: ",
-      stride_width);
-
-  int64_t ndim = input.ndimension();
-
-  TORCH_CHECK(
-      input.numel() != 0 && (ndim == 3 || ndim == 4),
-      "Expected non-empty 3D or 4D input tensor, but got input of size ",
-      input.sizes());
-
-  int64_t dim_batch = 0;
-
-  if (ndim == 3) {
-    dim_batch = -1;
-  }
-
-  int64_t n_input_plane = input.size(dim_batch + 1);
-  int64_t input_height = input.size(dim_batch + 2);
-  int64_t input_width = input.size(dim_batch + 3);
-  int64_t output_height = div_rtn<int64_t>(
-                              input_height + 2 * pad_height -
-                                  (dilation_height * (kernel_height - 1) + 1),
-                              stride_height) +
-      1;
-  int64_t output_width = div_rtn<int64_t>(
-                             input_width + 2 * pad_width -
-                                 (dilation_width * (kernel_width - 1) + 1),
-                             stride_width) +
-      1;
-
-  if (output_height < 1 || output_width < 1) {
-    AT_ERROR(
-        "Given input with spatial size (",
-        input_height,
-        ", ",
-        input_width,
-        "), kernel_size=(",
-        kernel_height,
-        ", ",
-        kernel_width,
-        "), dilation=(",
-        dilation_height,
-        ", ",
-        dilation_width,
-        "), padding=(",
-        pad_height,
-        ", ",
-        pad_width,
-        "), calculated shape of the array of sliding blocks as (",
-        output_height,
-        ", ",
-        output_width,
-        "), which is too small (non-positive).");
-  }
-}
-
 static void im2col_out_cuda_template(
     Tensor& output,
-    Tensor& input_,
+    const Tensor& input_,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
@@ -222,7 +131,7 @@ static void im2col_out_cuda_template(
 
 static void im2col_backward_out_cuda_template(
     Tensor& grad_input,
-    Tensor& grad_output,
+    const Tensor& grad_output,
     IntArrayRef input_size,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
@@ -233,7 +142,8 @@ static void im2col_backward_out_cuda_template(
       "It is expected input_size equals to 2, but got size ",
       input_size.size());
   // col2im_out_cuda checks size of kernel_size, dilation, padding and stride
-  grad_input = col2im_cuda(
+  col2im_out_cuda(
+      grad_input,
       grad_output,
       input_size,
       kernel_size,
@@ -244,27 +154,56 @@ static void im2col_backward_out_cuda_template(
 
 } // namespace
 
-Tensor im2col_cuda(
-    const Tensor& input_,
+Tensor& im2col_out_cuda(
+    Tensor& output,
+    const Tensor& input,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
     IntArrayRef stride) {
-  Tensor input = input_;
+  im2col_out_cuda_template(
+      output, input, kernel_size, dilation, padding, stride);
+  return output;
+}
+
+Tensor im2col_cuda(
+    const Tensor& input,
+    IntArrayRef kernel_size,
+    IntArrayRef dilation,
+    IntArrayRef padding,
+    IntArrayRef stride) {
   Tensor output = at::empty_like(input);
   im2col_out_cuda_template(
       output, input, kernel_size, dilation, padding, stride);
   return output;
 }
 
-Tensor im2col_backward_cuda(
-    const Tensor& grad_output_,
+Tensor& im2col_backward_out_cuda(
+    Tensor& grad_input,
+    const Tensor& grad_output,
     IntArrayRef input_size,
     IntArrayRef kernel_size,
     IntArrayRef dilation,
     IntArrayRef padding,
     IntArrayRef stride) {
-  Tensor grad_output = grad_output_;
+  im2col_backward_out_cuda_template(
+      grad_input,
+      grad_output,
+      input_size,
+      kernel_size,
+      dilation,
+      padding,
+      stride);
+  return grad_input;
+}
+
+Tensor im2col_backward_cuda(
+    const Tensor& grad_output,
+    IntArrayRef input_size,
+    IntArrayRef kernel_size,
+    IntArrayRef dilation,
+    IntArrayRef padding,
+    IntArrayRef stride) {
   Tensor grad_input = at::empty_like(grad_output);
   im2col_backward_out_cuda_template(
       grad_input,
