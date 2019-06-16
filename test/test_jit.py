cf1ca5a502ec0ca90f0ed8ffd97b15d5e904576c
Ailing Zhang <ailzhang@fb.com>
2019-06-16 09:45:14 -0700

    high pri Jit builtins (#21451)
    Summary:
    bin/hex/oct/round/chr
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21451
    
    Differential Revision: D15702863
    
    Pulled By: ailzhang
    
    fbshipit-source-id: 9f69896b79e7584f12353e9f2ee2969dbe1ec6d6

--- test/test_jit.py
+++ test/test_jit.py
@@ -12175,6 +12175,46 @@ a")
         s = u'\u00a3'.encode('utf8')[:1]
         self.checkScript(index_str_to_tensor, (s,))
 
+    def test_chr(self):
+        def fn(x):
+            # type: (int) -> str
+            return chr(x)
+
+        self.checkScript(fn, (1,))
+        self.checkScript(fn, (97,))
+
+    def test_round(self):
+        def round_float(x):
+            # type: (float) -> float
+            return round(x)
+
+        def round_int(x):
+            # type: (int) -> float
+            return round(x)
+
+        self.checkScript(round_float, (1.5,))
+        self.checkScript(round_int, (2,))
+
+    @unittest.skipIf(PY2, "oct() format changed from PY2 to PY3")
+    def test_convert_base(self):
+        def test_hex(x):
+            # type: (int) -> str
+            return hex(x)
+
+        def test_oct(x):
+            # type: (int) -> str
+            return oct(x)
+
+        def test_bin(x):
+            # type: (int) -> str
+            return bin(x)
+
+        numbers = [-1000, -10, 0, 1, 10, 2343]
+        for n in numbers:
+            self.checkScript(test_bin, (n,))
+            self.checkScript(test_oct, (n,))
+            self.checkScript(test_hex, (n,))
+
     @unittest.skipIf(IS_WINDOWS or IS_SANDCASTLE, "NYI: TemporaryFileName support for Windows or Sandcastle")
     def test_get_set_state(self):
         class M(torch.jit.ScriptModule):
