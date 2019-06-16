b0447c4a70fdc652f6ec9c7bcec6ed0e1b315bb1
Michael Suo <suo@fb.com>
2019-06-16 02:33:56 -0700

    change ClassType::compilation_unit to return owning ptr (#21787)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21787
    ghimport-source-id: eed7b98b0f02745066164b8ef3906291931e2ecb
    
    Test Plan: Imported from OSS
    
    Differential Revision: D15831353
    
    Pulled By: suo
    
    fbshipit-source-id: 50695c35dba8ffea710cbc9aca8aba6a75512fa0

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -300,7 +300,7 @@ void addFunctionToModule(
   auto graph = func->graph()->copy();
   auto v = graph->insertInput(0, "self");
   v->setType(module.module_object()->type());
-  module.module_object()->type()->compilation_unit().create_function(
+  module.module_object()->type()->compilation_unit()->create_function(
       "forward", graph);
 }
 
@@ -342,7 +342,7 @@ void initJitScriptBindings(PyObject* module) {
              const std::string& script,
              ResolutionCallback rcb) {
             c10::optional<Self> self;
-            m->class_compilation_unit().define(
+            m->class_compilation_unit()->define(
                 script, pythonResolver(rcb), moduleSelf(m, py_m));
             didFinishEmitModule(m);
           })
@@ -358,13 +358,13 @@ void initJitScriptBindings(PyObject* module) {
             for (auto& callback : rcbs) {
               resolvers.push_back(pythonResolver(callback));
             }
-            m->class_compilation_unit().define(
+            m->class_compilation_unit()->define(
                 defs, resolvers, moduleSelf(m, py_m));
             // Stitch in default arguments for each Def if provided
             auto defaults_it = defaults.begin();
             auto defs_it = defs.begin();
             while (defs_it != defs.end()) {
-              auto& method = m->class_compilation_unit().get_function(
+              auto& method = m->class_compilation_unit()->get_function(
                   (*defs_it).name().name());
               method.setSchema(getSchemaWithNameAndDefaults(
                   defs_it->range(),
@@ -386,7 +386,7 @@ void initJitScriptBindings(PyObject* module) {
       .def(
           "_get_functions",
           [](Module& self) {
-            return self.class_compilation_unit().get_functions();
+            return self.class_compilation_unit()->get_functions();
           })
       .def(
           "_register_attribute",
@@ -492,7 +492,7 @@ void initJitScriptBindings(PyObject* module) {
             auto typed_inputs = toTypedStack(input_tuple);
             auto graph = tracer::createGraphByTracing(
                 func, typed_inputs, var_lookup_fn, force_outplace, self);
-            self->module_object()->type()->compilation_unit().create_function(
+            self->module_object()->type()->compilation_unit()->create_function(
                 name, graph);
             didFinishEmitModule(self);
           })
@@ -514,7 +514,7 @@ void initJitScriptBindings(PyObject* module) {
             std::vector<ClassTypePtr> classes;
             PythonPrint(
                 ss,
-                self.class_compilation_unit(),
+                *self.class_compilation_unit(),
                 true,
                 tensors,
                 classes,
@@ -750,7 +750,7 @@ void initJitScriptBindings(PyObject* module) {
     std::vector<ClassTypePtr> classes;
     if (auto self = as_module(obj)) {
       PythonPrint(
-          ss, self->class_compilation_unit(), true, constants, classes, true);
+          ss, *self->class_compilation_unit(), true, constants, classes, true);
     } else if (auto self = as_function(obj)) {
       PythonPrint(ss, *self, false, constants, classes, true);
     } else {
