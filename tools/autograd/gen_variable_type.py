1719069b25af6bd2f7885844a629d84e543d70a1
Jerry Zhang <jerryzh168@gmail.com>
2019-06-16 22:48:42 -0700

    V4: (no description)
    Differential Revision: D15793427
    Differential Version: 85027709

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -281,8 +281,6 @@ def should_trace(declaration):
     # Operations involving Storage or Type are not traceable at the moment
     if any(arg['simple_type'] in {'Storage', 'Type', 'ConstQuantizerPtr'} for arg in declaration['arguments']):
         return False
-    for arg in declaration['arguments']:
-        print(arg)
     # We can't trace functions which don't have any Tensor or TensorList returns
     if 'Tensor' not in declaration['return_type']:
         return False
