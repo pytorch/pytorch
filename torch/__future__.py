0983de760f8be689cd6feb92e560f286800d7f55
Will Feng <willfeng@fb.com>
2019-06-18 14:20:25 -0400

    improve comment for _overwrite_module_params_on_conversion

--- torch/__future__.py
+++ torch/__future__.py
@@ -3,8 +3,8 @@ This global flag controls whether to assign new tensors to the parameters
 instead of changing the existing parameters in-place when converting an `nn.Module`
 using the following methods:
 1. `module.cuda()` / `.cpu()` (for moving `module` between devices)
-2. `module.type()` / `.float()` / `.double()` / `.half()` (for converting `module` to a different dtype)
-3. `module.to()` (for changing `module`'s device or dtype)
+2. `module.float()` / `.double()` / `.half()` (for converting `module` to a different dtype)
+3. `module.to()` / `.type()` (for changing `module`'s device or dtype)
 4. `module._apply(fn)` (for generic functions applied to `module`)
 
 Default: False
