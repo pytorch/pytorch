c669e69967699ac1bacd171e82d4e550c86d26c1
Zachary DeVito <zdevito@fb.com>
2019-06-17 18:11:22 -0700

    Make script::Method a value type (#21675)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21675
    ghimport-source-id: 90ee7ba00e58b0151ca4c17e91fd17303c9d5d08
    
    Test Plan: Imported from OSS
    
    Reviewed By: suo
    
    Differential Revision: D15777725
    
    Pulled By: zdevito
    
    fbshipit-source-id: 8482cd2e1dcd7dd77a9cacbb76743bd190c7c4cf

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -378,10 +378,10 @@ void initJitScriptBindings(PyObject* module) {
           })
       .def(
           "_get_method",
-          [](Module& self, const std::string& name) -> const Method& {
+          [](Module& self, const std::string& name) -> Method {
             return self.get_method(name);
           },
-          py::return_value_policy::reference_internal)
+          py::keep_alive<0, 1>())
       .def("_register_parameter", &Module::register_parameter)
       .def(
           "_get_functions",
@@ -474,10 +474,9 @@ void initJitScriptBindings(PyObject* module) {
       .def(
           "_method_names",
           [](Module& self) {
-            return fmap(
-                self.get_methods(), [](const std::unique_ptr<Method>& method) {
-                  return method->name();
-                });
+            return fmap(self.get_methods(), [](const Method& method) {
+              return method.name();
+            });
           })
       .def(
           "_create_method_from_trace",
@@ -499,9 +498,8 @@ void initJitScriptBindings(PyObject* module) {
       .def(
           "get_debug_state",
           [](Module& self) {
-            if (self.find_method("forward")) {
-              Method& m = self.get_method("forward");
-              return m.get_executor().getDebugState();
+            if (auto m = self.find_method("forward")) {
+              return m->get_executor().getDebugState();
             }
             throw std::runtime_error(
                 "Attempted to call get_debug_state on a Module without a compiled forward()");
@@ -629,6 +627,7 @@ void initJitScriptBindings(PyObject* module) {
       [](const Def& def, ResolutionCallback rcb, FunctionDefaults defaults) {
         C10_LOG_API_USAGE_ONCE("torch.script.compile");
         CompilationUnit cu;
+
         cu.define({def}, {pythonResolver(rcb)}, nullptr);
         std::shared_ptr<Function> defined = cu.get_functions().at(0);
         defined->setSchema(getSchemaWithNameAndDefaults(
