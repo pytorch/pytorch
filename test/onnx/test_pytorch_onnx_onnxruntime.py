62d22c6f1fa9c55b89f7d5d162848c694f60f628
liqunfu <liqun_fu@hotmail.com>
2019-06-18 10:22:50 -0700

    add ORT test draft

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -7,12 +7,37 @@ import unittest
 import sys
 import onnxruntime  # noqa
 
+import torch
 
 class TestONNXRuntime(unittest.TestCase):
 
     def test_onnxruntime_installed(self):
         self.assertTrue('onnxruntime' in sys.modules)
 
+    def test_sort(self):
+        class SortModel(torch.nn.Module):
+            def __init__(self, dim, descending):
+                super(SortModel, self).__init__()
+                self.dim = dim
+                self.descending = descending
+
+            def forward(self, x):
+                return torch.sort(x, dim=self.dim, descending=self.descending)
+
+        dim = 1
+        decending = False
+        model = SortModel(dim, decending)
+
+        x = torch.randn(3, 4)
+        pt_sorted, pt_indices = model(x)
+
+        # f = io.BytesIO()
+        # torch.onnx._export(model, (x,), f)
+        # ort_sess = onnxruntime.InferenceSession(f.getvalue())
+        # ort_sorted, ort_indices = ort_sess.run(None, {ort_sess.get_inputs()[0].name: x.numpy()})
+
+        # np.allclose(ort_sorted, pt_sorted)
+        # np.allclose(ort_indices, pt_indices)
 
 if __name__ == '__main__':
     unittest.main()
