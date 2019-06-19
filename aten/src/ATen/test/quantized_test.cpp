44be061ab13de85e2fba7337db109c24dcdb8196
Jerry Zhang <jerryzh168@gmail.com>
2019-06-19 09:05:45 -0700

    V13: (no description)
    Differential Revision: D15793003
    Differential Version: 85209962

--- aten/src/ATen/test/quantized_test.cpp
+++ aten/src/ATen/test/quantized_test.cpp
@@ -16,8 +16,8 @@ using namespace at;
 TEST(TestQTensor, QuantDequantAPIs) {
   auto num_elements = 10;
   Tensor r = at::ones({num_elements});
-  const float scale = 1.0;
-  const int32_t zero_point = 2;
+  const double scale = 1.0;
+  const int64_t zero_point = 2;
   Tensor qr = at::quantize_linear(r, scale, zero_point, kQUInt8);
   ASSERT_EQ(qr.q_scale(), scale);
   ASSERT_EQ(qr.q_zero_point(), zero_point);
--- test/test_quantized.py
+++ test/test_quantized.py
@@ -24,7 +24,7 @@ def _quantize(x, scale, zero_point, qmin=None, qmax=None, dtype=np.uint8):
         qmin = np.iinfo(dtype).min
     if qmax is None:
         qmax = np.iinfo(dtype).max
-    qx = (np.round(x / scale) + zero_point).astype(np.int64)
+    qx = (np.round(x / scale + zero_point)).astype(np.int64)
     qx = np.clip(qx, qmin, qmax)
     qx = qx.astype(dtype)
     return qx
