b510df12791384bd43a1e48f8eae477b24fdf098
Jerry Zhang <jerryzh168@gmail.com>
2019-06-18 20:19:48 -0700

    V10: (no description)
    Differential Revision: D15793003
    Differential Version: 85190167

--- test/test_quantized.py
+++ test/test_quantized.py
@@ -166,8 +166,8 @@ class TestQuantizedOps(TestCase):
         qY_hat = relu(qX)
 
         Y[Y < 0] = 0
-        qY = _quantize(Y, scale, zero_point, dtype=np_type)
-        np.testing.assert_equal(qY, qY_hat.int_repr())
+        qY = torch.quantize_linear(Y, scale=scale, zero_point=zero_point, dtype=torch_type)
+        np.testing.assert_equal(qY.int_repr(), qY_hat.int_repr())
 
     """Tests the correctness of the add and add_relu op."""
     def test_qadd_relu_same_qparams(self):
