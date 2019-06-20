365de7bda1a8c9acbe813052daaa5de50fb7279d
Pieter Noordhuis <pietern@fb.com>
2019-06-20 06:59:53 -0700

    Support sparse gradients in DistributedDataParallel (#19443)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/19443
    
    This adds support for sparse gradients to the reducer as well as to
    the DistributedDataParallel wrapper. Note that an out of band signal
    is needed whether or not a dense parameter (e.g. an embedding) is
    expected to receive a sparse gradient or not. This information is
    passed to the bucket assignment computation routine and the reducer as
    a vector of booleans. Every parameter for which we expect a sparse
    gradient is assigned its own bucket, as we cannot easily group
    multiple unrelated sparse tensors.
    
    Reviewed By: mrshenli
    
    Differential Revision: D15007365
    
    fbshipit-source-id: f298e83fd3ca828fae9e80739e1db89d045c99ac

--- torch/csrc/distributed/c10d/reducer.h
+++ torch/csrc/distributed/c10d/reducer.h
@@ -21,6 +21,7 @@ class Reducer {
   // variables list for **a single replica** (i.e. `variables[0]`).
   explicit Reducer(
       std::vector<std::vector<torch::autograd::Variable>> replicas,
+      std::vector<std::vector<bool>> expect_sparse_gradients,
       std::vector<std::vector<size_t>> bucket_indices,
       std::shared_ptr<c10d::ProcessGroup> process_group);
 
@@ -47,8 +48,12 @@ class Reducer {
   }
 
  protected:
+  // Forward declaration.
+  struct Bucket;
+
   std::mutex mutex_;
   std::vector<std::vector<torch::autograd::Variable>> replicas_;
+  std::vector<std::vector<bool>> expect_sparse_gradients_;
   std::shared_ptr<c10d::ProcessGroup> process_group_;
 
   std::vector<std::vector<std::shared_ptr<torch::autograd::Function>>>
@@ -62,12 +67,20 @@ class Reducer {
   bool has_marked_unused_parameters_;
   size_t next_bucket_;
 
+  void mark_variable_ready_dense(size_t replica_index, size_t variable_index);
+
+  void mark_variable_ready_sparse(size_t replica_index, size_t variable_index);
+
   void mark_variable_ready(
       size_t replica_index,
       size_t variable_index,
       bool called_from_autograd = false);
 
   void mark_bucket_ready(size_t bucket_index);
+
+  void finalize_bucket_dense(Bucket& replica);
+
+  void finalize_bucket_sparse(Bucket& replica);
 
   void finalize_backward();
 
@@ -119,6 +132,10 @@ class Reducer {
 
     // Keep work handle around when this set of buckets is being reduced.
     std::shared_ptr<c10d::ProcessGroup::Work> work;
+
+    // If this bucket should expect a single sparse gradient.
+    // Implies: replicas[i].variables.size() == 1.
+    bool expect_sparse_gradient = false;
   };
 
   std::vector<Bucket> buckets_;
@@ -146,6 +163,7 @@ class Reducer {
 
 std::vector<std::vector<size_t>> compute_bucket_assignment_by_size(
     const std::vector<at::Tensor>& tensors,
-    std::vector<size_t> bucket_size);
+    const std::vector<size_t>& bucket_size,
+    const std::vector<bool>& expect_sparse_gradient = {});
 
 } // namespace c10d
