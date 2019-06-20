51c68c5218cf493fce0b9ddbf8cd8986d16feff5
Vitaly Fedyunin <vitalyf@fb.com>
2019-06-20 13:00:15 -0700

    Leaving broadcasting up to the TensorIterator

--- aten/src/ATen/native/Lerp.cpp
+++ aten/src/ATen/native/Lerp.cpp
@@ -11,61 +11,41 @@ namespace native {
 
 Tensor& lerp_cpu_tensor_out(Tensor& result, const Tensor& self,
                             const Tensor& end, const Tensor& weight) {
-  Tensor b_self, b_end, b_weight;
   TORCH_CHECK(weight.dim() <= std::max(self.dim(), end.dim()),
            "weight should be of dimension max(self.dim(), end.dim()) or lesser");
-  std::tie(b_self, b_end, b_weight) = expand_outplace(self, end, weight, "lerp_out_cpu");
-  result.resize_as_(b_self);
-  lerp_kernel_tensor_weight(kCPU, result, b_self, b_end, b_weight);
+  lerp_kernel_tensor_weight(kCPU, result, self, end, weight);
   return result;
 }
 
 Tensor& lerp_cpu_scalar_out(Tensor& result, const Tensor& self,
                             const Tensor& end, Scalar weight) {
-  Tensor b_self, b_end;
-  std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_out_cpu");
-  result.resize_as_(b_self);
-  lerp_kernel_scalar_weight(kCPU, result, b_self, b_end, weight);
+  lerp_kernel_scalar_weight(kCPU, result, self, end, weight);
   return result;
 }
 
 Tensor& lerp_cpu_tensor_(Tensor& self, const Tensor& end, const Tensor& weight) {
-  Tensor b_self, b_end, b_weight;
-  std::tie(b_self, b_end, b_weight) = expand_outplace(self, end, weight, "lerp__cpu");
-  TORCH_CHECK(b_self.sizes() == self.sizes(),
-           "output with shape ", self.sizes(),
-           " doesn't match the broadcast shape ", b_self.sizes());
   TORCH_CHECK(weight.dim() <= std::max(self.dim(), end.dim()),
            "weight should be of dimension max(self.dim(), end.dim()) or lesser");
-  lerp_kernel_tensor_weight(kCPU, self, b_self, b_end, b_weight);
+  lerp_kernel_tensor_weight(kCPU, self, self, end, weight);
   return self;
 }
 
 Tensor& lerp_cpu_scalar_(Tensor& self, const Tensor& end, Scalar weight) {
-  Tensor b_self, b_end;
-  std::tie(b_self, b_end) = expand_outplace(self, end, "lerp__cpu");
-  TORCH_CHECK(b_self.sizes() == self.sizes(),
-           "output with shape ", self.sizes(),
-           " doesn't match the broadcast shape ", b_self.sizes());
-  lerp_kernel_scalar_weight(kCPU, self, b_self, b_end, weight);
+  lerp_kernel_scalar_weight(kCPU, self, self, end, weight);
   return self;
 }
 
 Tensor lerp_cpu_tensor(const Tensor& self, const Tensor& end, const Tensor& weight) {
-  Tensor b_self, b_end, b_weight;
   TORCH_CHECK(weight.dim() <= std::max(self.dim(), end.dim()),
            "weight should be of dimension max(self.dim(), end.dim()) or lesser");
-  std::tie(b_self, b_end, b_weight) = expand_outplace(self, end, weight, "lerp_cpu");
-  Tensor result = at::empty_like(b_self);
-  lerp_kernel_tensor_weight(kCPU, result, b_self, b_end, b_weight);
+  Tensor result = at::empty({0}, self.options());
+  lerp_kernel_tensor_weight(kCPU, result, self, end, weight);
   return result;
 }
 
 Tensor lerp_cpu_scalar(const Tensor& self, const Tensor& end, Scalar weight) {
-  Tensor b_self, b_end;
-  std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_cpu");
-  Tensor result = at::empty_like(b_self);
-  lerp_kernel_scalar_weight(kCPU, result, b_self, b_end, weight);
+  Tensor result = at::empty({0}, self.options());
+  lerp_kernel_scalar_weight(kCPU, result, self, end, weight);
   return result;
 }
 
--- aten/src/ATen/native/cpu/LerpKernel.cpp
+++ aten/src/ATen/native/cpu/LerpKernel.cpp
@@ -19,20 +19,14 @@ static void lerp_kernel_scalar(
   builder.add_input(self);
   builder.add_input(end);
   auto iter = builder.build();
-
   AT_DISPATCH_FLOATING_TYPES(ret.scalar_type(), "lerp_kernel_scalar", [&] {
     scalar_t weight_val = weight.to<scalar_t>();
-    auto weight_vec = Vec256<scalar_t>(weight_val);
-    auto reversed_weight_vec = Vec256<scalar_t>(1 - weight_val);
-    at::native::binary_kernel_vec(
+    at::native::cpu_kernel(
         *iter,
         [=](scalar_t self_val, scalar_t end_val) {
           return (weight_val < 0.5)
               ? self_val + weight_val * (end_val - self_val)
               : end_val - (end_val - self_val) * (1 - weight_val);
-        },
-        [=](Vec256<scalar_t> start_vec, Vec256<scalar_t> end_vec) {
-          return reversed_weight_vec * start_vec + weight_vec * end_vec;
         });
   });
 }
@@ -49,7 +43,7 @@ static void lerp_kernel_tensor(
   builder.add_input(weights);
   auto iter = builder.build();
   AT_DISPATCH_FLOATING_TYPES(ret.scalar_type(), "lerp_kernel_tensor", [&] {
-    at::native::ternary_kernel(
+    at::native::cpu_kernel(
         *iter,
         [=](scalar_t self_val, scalar_t end_val, scalar_t weight_val) {
           return (weight_val < 0.5)
