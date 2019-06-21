38383245394c5c1deeabc38527f2f594b9f285cf
Jerry Zhang <jerryzh@fb.com>
2019-06-20 20:48:57 -0700

    Add max/min/argmax/argmin/sort/argsort for quantized Tensor (#21546)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/21546
    
    Added following methods for QTensor:
    - max, min
    - argmax, argmin
    - sort, argsort
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D15718117
    
    fbshipit-source-id: 746b978d5722cb75e216fc65585bf206d45a7969

--- aten/src/ATen/native/quantized/QTensor.cpp
+++ aten/src/ATen/native/quantized/QTensor.cpp
@@ -86,11 +86,12 @@ Tensor int_repr_quant(const Tensor& self) {
 
 Tensor per_tensor_affine_qtensor_cpu(const Tensor& self, double scale, int64_t zero_point) {
   Tensor dst = at::_empty_affine_quantized(self.sizes(), self.options().dtype(toQIntType(self.scalar_type())), scale, zero_point);
+  Tensor self_contig = self.contiguous();
   AT_DISPATCH_QINT_TYPES(dst.scalar_type(), "per_tensor_affine_qtensor", [&]() {
-    underlying_t* self_data = self.data<underlying_t>();
+    underlying_t* self_data = self_contig.data<underlying_t>();
     underlying_t* dst_data = reinterpret_cast<underlying_t *>(dst.data<scalar_t>());
     if (self.numel() > 0) {
-      memcpy(dst_data, self_data, self.numel());
+      memcpy(dst_data, self_data, self.nbytes());
     }
   });
   return dst;
