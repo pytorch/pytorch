b40d9987c09c453c43d40b835044e02a855bd4a1
royboy <royboy@fb.com>
2019-06-21 15:46:52 -0700

    Update on "Remove many usages of Type"
    Remove many usages of Type
    
    gh-metadata: pytorch pytorch 21941 gh/li-roy/30/head

--- torch/csrc/utils/tensor_types.h
+++ torch/csrc/utils/tensor_types.h
@@ -7,7 +7,6 @@
 namespace torch { namespace utils {
 
 std::string type_to_string(const at::DeprecatedTypeProperties& type);
-std::string options_to_string(const at::TensorOptions& options);
 at::DeprecatedTypeProperties* type_from_string(const std::string& str);
 
 // return a vector of all "declared" types, even those that weren't compiled
