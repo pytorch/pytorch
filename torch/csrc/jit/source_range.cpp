52c23e53162709ae40943dc8d8bb73aeebffe4fe
James Reed <jamesreed@fb.com>
2019-06-24 23:52:43 -0700

    [JIT] Load original SourceRanges on import

--- torch/csrc/jit/source_range.cpp
+++ torch/csrc/jit/source_range.cpp
@@ -1,7 +1,45 @@
 #include <torch/csrc/jit/source_range.h>
 
+#include <torch/csrc/jit/pickler.h>
+
 namespace torch {
 namespace jit {
+
+C10_EXPORT std::shared_ptr<SourceRange> DebugInfo::query(const SourceRange& q) {
+  deserialize();
+
+  auto query = SourceRangeRecord{q.start(), nullptr};
+  auto entry = std::lower_bound(
+      deserialized_records_->begin(),
+      deserialized_records_->end(),
+      query,
+      [](const SourceRangeRecord& a, const SourceRangeRecord& b) -> bool {
+        return std::get<0>(a) < std::get<0>(b);
+      });
+
+  if (entry != deserialized_records_->end()) {
+    return std::get<1>(*entry);
+  }
+
+  return nullptr;
+}
+
+void DebugInfo::deserialize() {
+  if (deserialized_records_) {
+    return;
+  }
+
+  Unpickler up(std::get<0>(info_).get(), std::get<1>(info_), nullptr);
+  auto ivalues = up.parse_ivalue_list();
+
+  deserialized_records_ = SourceRangeRecords();
+  for (auto& val : ivalues) {
+    auto tup_elems = val.toTuple()->elements();
+    int64_t offset = tup_elems[0].toInt();
+    std::shared_ptr<SourceRange> sr = SourceRange::__setstate__(tup_elems[1]);
+    deserialized_records_->emplace_back(offset, std::move(sr));
+  }
+}
 
 // a range of a shared string 'file_' with
 C10_EXPORT void SourceRange::highlight(std::ostream& out) const {
@@ -56,6 +94,13 @@ C10_EXPORT void SourceRange::highlight(std::ostream& out) const {
   out << str.substr(end_line, end_highlight - end_line);
   if (!str.empty() && str.back() != '\n')
     out << "\n";
+  // Retrieve original SourceRange, if present.
+  if (source_) {
+    if (auto orig_source_range = orig_range()) {
+      out << "Compiled from code ";
+      orig_source_range->highlight(out);
+    }
+  }
 }
 
 } // namespace jit
