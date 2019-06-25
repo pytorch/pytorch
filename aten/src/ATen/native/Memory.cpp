b9bb07d0eb33e99503bf91308192389c25a257a2
SsnL <tongzhou.wang.1994@gmail.com>
2019-06-25 16:24:50 -0400

    Use existing primary context if available in pin_memory. Fix OptionalDeviceGuard comments.

--- aten/src/ATen/native/Memory.cpp
+++ aten/src/ATen/native/Memory.cpp
@@ -1,10 +1,10 @@
 #include <ATen/ATen.h>
 #include <ATen/MemoryOverlap.h>
 #include <ATen/NativeFunctions.h>
+#include <ATen/TensorUtils.h>
 #include <ATen/detail/CUDAHooksInterface.h>
 #include <c10/util/Exception.h>
 #include <c10/core/Storage.h>
-#include <ATen/TensorUtils.h>
 
 namespace at {
 namespace native {
@@ -12,6 +12,16 @@ namespace native {
 Tensor pin_memory(const Tensor& self) {
   if (self.type().backend() != Backend::CPU) {
     AT_ERROR("cannot pin '", self.type().toString(), "' only dense CPU tensors can be pinned");
+  }
+  at::OptionalDeviceGuard device_guard;
+  // Pinned memory pointers allocated by any device can be directly used by any
+  // other device, regardless of the current device at the time of allocation,
+  // since we assume unified addressing.
+  // So we grab any existing primary context, if available.
+  // See pytorch/pytorch#21081.
+  auto primary_ctx_device = detail::getCUDAHooks().getDeviceWithPrimaryContext();
+  if (primary_ctx_device >= 0) {
+    device_guard.reset_device(at::Device(at::DeviceType::CUDA, primary_ctx_device));
   }
   auto* allocator = detail::getCUDAHooks().getPinnedMemoryAllocator();
   auto storage = Storage(
