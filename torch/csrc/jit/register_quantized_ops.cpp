fe21464486bb5e536af4575942b7a4d40081b736
Will Feng <willfeng@fb.com>
2019-06-25 14:15:09 -0400

    don't rename tensor_data() yet

--- torch/csrc/jit/register_quantized_ops.cpp
+++ torch/csrc/jit/register_quantized_ops.cpp
@@ -28,10 +28,10 @@ using caffe2::int8::Int8TensorCPU;
 namespace {
 
 caffe2::Tensor from_at_tensor(const c10::IValue& v) {
-  // We use `._tensor_data_deprecated()` here, because Caffe2 can't handle Variables yet.
+  // We use `.tensor_data()` here, because Caffe2 can't handle Variables yet.
   // After the Variable/Tensor merge, Caffe2 will be able to handle non-requires-grad Variables,
-  // and we will remove `._tensor_data_deprecated()` here.
-  return caffe2::Tensor(autograd::Variable(std::move(v).toTensor())._tensor_data_deprecated());
+  // and we will remove `.tensor_data()` here.
+  return caffe2::Tensor(autograd::Variable(std::move(v).toTensor()).tensor_data());
 }
 
 Int8TensorCPU from_proxy(const c10::IValue& proxy) {
