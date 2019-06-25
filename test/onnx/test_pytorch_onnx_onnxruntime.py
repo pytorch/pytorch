f7a62af0ee1ebd29240daecd06b877499d58d42a
Lara <lahaidar@microsoft.com>
2019-06-25 10:10:15 -0700

    add helper fct for onnxruntime tests

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import unittest
-import sys
 import onnxruntime  # noqa
 import torch
 import numpy as np
@@ -12,10 +11,25 @@ import io
 
 class TestONNXRuntime(unittest.TestCase):
 
-    def test_onnxruntime_installed(self):
-        self.assertTrue('onnxruntime' in sys.modules)
+    def run_test(self, model, input, output, opset_version=None, example_outputs=None):
+        # export the model to ONNX
+        f = io.BytesIO()
+        torch.onnx.export(model, input, f,
+                          opset_version=opset_version,
+                          example_outputs=example_outputs)
 
-    def test_full(self):
+        # compute onnxruntime output prediction
+        ort_sess = onnxruntime.InferenceSession(f.getvalue())
+        ort_outs = ort_sess.run(None, {ort_sess.get_inputs()[0].name: input.numpy()})
+
+        # compare onnxruntime and PyTorch results 
+        if output.requires_grad:
+            output = output.detach().numpy()
+        else:
+            output = output.numpy()
+        np.allclose(output, ort_outs[0])
+
+    def test_full_trace(self):
         class FullModel(torch.nn.Module):
             def forward(self, x):
                 return torch.full((3, 4), x, dtype=torch.long)
@@ -23,28 +37,18 @@ class TestONNXRuntime(unittest.TestCase):
         x = torch.tensor(12)
         model = FullModel()
         output = model(x)
+        self.run_test(model, x, output)
 
-        f = io.BytesIO()
-        torch.onnx.export(model, x, f)
-
-        ort_sess = onnxruntime.InferenceSession(f.getvalue())
-        ort_outs = ort_sess.run(None, {ort_sess.get_inputs()[0].name: x.numpy()})
-        np.allclose(output.numpy(), ort_outs[0])
-
+    def test_full_script(self):
         class FullModelScripting(torch.jit.ScriptModule):
             @torch.jit.script_method
             def forward(self, x):
                 return torch.full((3, 4), x, dtype=torch.long)
 
-        model_scripting = FullModelScripting()
-        output_scripting = model_scripting(x)
-
-        f_scripting = io.BytesIO()
-        torch.onnx.export(model_scripting, x, f_scripting, example_outputs=output_scripting)
-
-        ort_sess_scripting = onnxruntime.InferenceSession(f_scripting.getvalue())
-        ort_outs_scripting = ort_sess_scripting.run(None, {ort_sess_scripting.get_inputs()[0].name: x.numpy()})
-        np.allclose(output_scripting.numpy(), ort_outs_scripting[0])
+        x = torch.tensor(12)
+        model = FullModelScripting()
+        output = model(x)
+        self.run_test(model, x, output, example_outputs=output)
 
 
 if __name__ == '__main__':
