c2b118bd98bb9c611e5b49bc11c562560392730e
shihongzhi <shihzh@rd.netease.com>
2019-06-26 10:08:59 +0800

    change docs

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -720,7 +720,7 @@ add_docstr_all('fill_diagonal_',
                r"""
 fill_diagonal_(fill_value, wrap=False) -> Tensor
 
-Fill the main diagonal of the given tensor of any dimensionality.
+Fill the main diagonal of a tensor that has at least 2-dimensions.
 Tensor at least 2-dims. When dims>2, all dimensions of input must be of equal length
 This function modifies the input tensor in-place, and returns the input tensor.
 
