a82278b23d2595e011131804aaf235487ac7a04c
Horace He <horacehe2007@yahoo.com>
2019-06-27 12:26:22 -0700

    moved attributes to init

--- torch/nn/modules/flatten.py
+++ torch/nn/modules/flatten.py
@@ -21,6 +21,13 @@ class Flatten(Module):
         >>>     nn.Flatten()
         >>> )
     """
+    __constants__ = ['start_dim', 'end_dim']
+
+    def __init__(self, start_dim=1, end_dim=-1):
+        super(Flatten, self).__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
     @weak_script_method
     def forward(self, input, start_dim=1, end_dim=-1):
         return input.flatten(start_dim, end_dim)
