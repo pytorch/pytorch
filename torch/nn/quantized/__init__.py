53931141773b4c94bb9569cce54e67def66ea25b
Jerry Zhang <jerryzh168@gmail.com>
2019-06-26 18:13:03 -0700

    V24: (no description)
    Differential Revision: D15554224
    Differential Version: 85688352

--- torch/nn/quantized/__init__.py
+++ torch/nn/quantized/__init__.py
@@ -1,6 +1,3 @@
-r"""
-The quantized module contains the data structures and ops for developing
-the quantized models.
-"""
+from __future__ import absolute_import, division, print_function, unicode_literals
 from .modules import *  # noqa: F401
 from . import functional  # noqa: F401
