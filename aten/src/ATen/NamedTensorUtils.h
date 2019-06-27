2e291a90652b44b9d09f582763bc7e26b346851e
Richard Zou <zou3519@gmail.com>
2019-06-27 12:37:55 -0700

    Implement unify_from_right (#22223)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22223
    ghimport-source-id: b88bd2a13c1c9c699945a69ec05300c6e598e95a
    
    Test Plan:
    - `build/bin/NamedTensor_test` [namedtensor ci]
    
    gh-metadata: pytorch pytorch 22223 gh/zou3519/62/head
    
    Imported from OSS
    
    Differential Revision: D16030551
    
    Pulled By: zou3519
    
    fbshipit-source-id: f3d53e3f9b2428a4926c61a02631e6cd29f89e4b

--- aten/src/ATen/NamedTensorUtils.h
+++ aten/src/ATen/NamedTensorUtils.h
@@ -3,6 +3,7 @@
 
 #include <ATen/NamedTensor.h>
 #include <ATen/core/Tensor.h>
+#include <functional>
 
 namespace at {
 
@@ -17,6 +18,17 @@ CAFFE2_API void internal_set_names_inplace(Tensor& tensor, optional<DimnameList>
 // Converts dim to an positional index. Errors if `dim` cannot be used to
 // refer to any dimension of tensor.
 CAFFE2_API int64_t dimname_to_position(const Tensor& tensor, Dimname dim);
+
+// Unifies two DimnameList to produce a third. This is useful for implementing
+// the named inference rule for binary broadcasting operations like add.
+//
+// There are three main constraints:
+// 1) Check matching: Names must match positionally from the right.
+// 2) Check misaligned: If a name `n` is in `names`, then it must appear at
+//    the same index from the right in other.
+// 3) The output names are obtained by unifying the names individually from the right.
+CAFFE2_API optional<std::vector<Dimname>>
+unify_from_right(optional<DimnameList> names, optional<DimnameList> other);
 
 namespace namedinference {
 
