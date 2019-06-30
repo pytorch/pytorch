7867c45b577d201182d53b9921a648b6e60f3273
vishwakftw <cs15btech11043@iith.ac.in>
2019-06-30 08:32:50 +0530

    CR

--- tools/setup_helpers/nnpack.py
+++
@@ -1,3 +0,0 @@
-from .env import check_negative_env_flag
-
-USE_NNPACK = not check_negative_env_flag('USE_NNPACK')
--- tools/setup_helpers/qnnpack.py
+++
@@ -1,3 +0,0 @@
-from .env import check_negative_env_flag
-
-USE_QNNPACK = not check_negative_env_flag('USE_QNNPACK')
