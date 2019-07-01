6bd5be0b052bd43c35b729f92c3e13b3347db898
dylanbespalko <dylan.bespalko@me.com>
2019-07-01 00:05:00 -0600

    Debugging pytorch-complex1

--- aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
@@ -14,7 +14,7 @@ namespace {
 using namespace vec256;
 
 void add_kernel(TensorIterator& iter, Scalar alpha_scalar) {
-  AT_DISPATCH_ALL_TYPES(iter.type(), "add", [&]() {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.type(), "add", [&]() {
     auto alpha = alpha_scalar.to<scalar_t>();
     auto alpha_vec = Vec256<scalar_t>(alpha);
     binary_kernel_vec(iter,
@@ -30,7 +30,7 @@ void sub_kernel(TensorIterator& iter, Scalar alpha_scalar) {
 }
 
 void mul_kernel(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES(iter.type(), "mul", [&]() {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.type(), "mul", [&]() {
     binary_kernel_vec(iter,
       [=](scalar_t a, scalar_t b) -> scalar_t { return a * b; },
       [=](Vec256<scalar_t> a, Vec256<scalar_t> b) {
@@ -43,7 +43,7 @@ void div_kernel(TensorIterator& iter) {
   if (isIntegralType(iter.type().scalarType())) {
     // There's no SIMD integer division, so don't try to vectorize it.
     // TODO: if the divisor is a scalar, rewrite as multiplication by a constant.
-    AT_DISPATCH_INTEGRAL_TYPES(iter.type(), "div", [&]() {
+	AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.type(), "div", [&]() {
       binary_kernel(iter, [](scalar_t a, scalar_t b) -> scalar_t {
         return a / b;
       });
