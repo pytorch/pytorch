402b9f9a6da4a3d2e9c19f838e79fc7131816b02
Mingzhe Li <mingzhe0908@fb.com>
2019-07-01 16:28:16 -0700

    add PT chunk op to the benchmark (#22409)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22409
    
    as title
    
    Reviewed By: hl475
    
    Differential Revision: D16079031
    
    fbshipit-source-id: 109060ffc953f2357b2783b13f9b9dc87bd3f98a

---
+++ benchmarks/operator_benchmark/pt/chunk_test.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import operator_benchmark as op_bench
+import torch
+
+
+"""Microbenchmarks for Chunk operator"""
+
+
+# Configs for PT Chunk operator
+chunks_short_configs = op_bench.cross_product_configs(
+    M=[8, 64, 128],
+    N=range(2, 10, 3),
+    chunks=[2, 3],
+    tags=["short"]
+)
+
+
+class ChunkBenchmark(op_bench.TorchBenchmarkBase):
+    def init(self, M, N, chunks): 
+        self.input_one = torch.rand(M, N) 
+        self.chunks = chunks
+        self.set_module_name("chunks")
+
+    def forward(self):
+        return torch.chunk(self.input_one, self.chunks)
+
+
+op_bench.generate_pt_test(chunks_short_configs, ChunkBenchmark)
+
+
+if __name__ == "__main__":
+    op_bench.benchmark_runner.main()
