09f507e50b25ad10b5d5f6b0780ef30ff8d5d8fd
Pavel Belevich <pbelevich@fb.com>
2019-07-02 11:53:03 -0700

    hardshrink_cpu and hardshrink_backward_cpu refactoring with at::native::cpu_kernel

--- aten/src/ATen/native/Activation.cpp
+++ aten/src/ATen/native/Activation.cpp
@@ -5,6 +5,7 @@
 #include <ATen/Dispatch.h>
 #include <ATen/NativeFunctions.h>
 #include <ATen/native/TensorIterator.h>
+#include <ATen/native/cpu/Loops.h>
 #include <ATen/Parallel.h>
 
 
@@ -341,13 +342,12 @@ Tensor hardshrink_cpu(const Tensor & self, Scalar lambd) {
   auto out_tensor = at::empty_like(self);
   AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "hardshrink_cpu", [&] {
     auto lambd_val = lambd.to<scalar_t>();
-    at::CPU_tensor_apply2<scalar_t, scalar_t>(
-      self,
-      out_tensor,
-      [&](
-        scalar_t& self_val,
-        scalar_t& out_tensor_val) {
-          out_tensor_val = (self_val >= -lambd_val && self_val <= lambd_val) ? scalar_t(0) : self_val;
+    auto builder = at::TensorIterator::Builder();
+    builder.add_output(out_tensor);
+    builder.add_input(self);
+    auto iter = builder.build();
+    at::native::cpu_kernel(*iter, [=](scalar_t self_val) {
+      return (self_val >= -lambd_val && self_val <= lambd_val) ? scalar_t(0) : self_val;
     });
   });
   return out_tensor;
@@ -357,15 +357,13 @@ Tensor hardshrink_backward_cpu(const Tensor & grad, const Tensor & self, Scalar 
   auto out_tensor = at::empty_like(self);
   AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "hardshrink_backward_cpu", [&] {
     auto lambd_val = lambd.to<scalar_t>();
-    at::CPU_tensor_apply3<scalar_t, scalar_t, scalar_t>(
-      self,
-      grad,
-      out_tensor,
-      [&](
-        scalar_t& self_val,
-        scalar_t& grad_val,
-        scalar_t& out_tensor_val) {
-          out_tensor_val = (self_val >= -lambd_val && self_val <= lambd_val) ? scalar_t(0) : grad_val;
+    auto builder = at::TensorIterator::Builder();
+    builder.add_output(out_tensor);
+    builder.add_input(self);
+    builder.add_input(grad);
+    auto iter = builder.build();
+    at::native::cpu_kernel(*iter, [=](scalar_t self_val, scalar_t grad_val) {
+      return (self_val >= -lambd_val && self_val <= lambd_val) ? scalar_t(0) : grad_val;
     });
   });
   return out_tensor;
