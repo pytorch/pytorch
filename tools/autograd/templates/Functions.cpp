dff2c07183e0a94ba9707e110661cd42413ab9fb
Wanchao Liang <wanchaol@fb.com>
2019-07-01 19:53:05 -0700

    Manual revert of D16012838
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/22412
    
    Reviewed By: nairbv, houseroad
    
    Differential Revision: D16079809
    
    fbshipit-source-id: ee0d805ff7a2bc5f98bcc65f90b8199751c840f6

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -398,7 +398,7 @@ Tensor cumprod_backward(const Tensor &grad, const Tensor &input, int64_t dim) {
   return grad_input;
 }
 
-Tensor cumprod_backward(const Tensor &grad, const Tensor &input, int64_t dim, optional<ScalarType> dtype) {
+Tensor cumprod_backward(const Tensor &grad, const Tensor &input, int64_t dim, ScalarType dtype) {
   return cumprod_backward(grad.to(input.scalar_type()), input, dim);
 }
 
@@ -423,6 +423,10 @@ Tensor cumsum_backward(const Tensor & x, int64_t dim) {
   ret -= ret_sum.expand(ret.sizes());
   ret += x;
   return ret;
+}
+
+Tensor cumsum_backward(const Tensor &x, int64_t dim, ScalarType input_dtype) {
+  return cumsum_backward(x.to(input_dtype), dim);
 }
 
 Tensor logsumexp_backward(Tensor grad, const Tensor & self, Tensor result, IntArrayRef dim, bool keepdim) {
