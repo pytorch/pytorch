4b2922a19cf52092ca76845b2b23177451cc847d
Alyssa Wang <alyssawqq@fb.com>
2019-07-02 15:17:57 -0700

    Create C2 operator to replace values in embedding table (#22279)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22279
    
    This new operator is used for embedding table weight re-init. After we get the evicted indices, they will be the rows need reseting in embedding table. Then we can create a 1d tensor with default values, and apply this operator to copy the tensor to all evicted rows in embedding table
    
    Will add gradient op in next diff
    
    Reviewed By: itomatik
    
    Differential Revision: D15709866
    
    fbshipit-source-id: 2297b70a7326591524d0be09c73a588da245cc08

---
+++ caffe2/operators/copy_rows_to_tensor_op.cc
@@ -0,0 +1,34 @@
+#include "copy_rows_to_tensor_op.h"
+
+namespace caffe2 {
+namespace {
+REGISTER_CPU_OPERATOR(CopyRowsToTensor, CopyRowsToTensorOp<CPUContext>);
+
+OPERATOR_SCHEMA(CopyRowsToTensor)
+    .NumInputs(3)
+    .NumOutputs(1)
+    .EnforceInplace({{0, 0}})
+    .SetDoc(R"DOC(
+      This operator takes in a 2d tensor, a list of indices, and a 1d tensor
+      with the same width of the 2d tensor. It will replace the rows in 2d
+      tensor specified in indices with the 2d tensor. The operator does an
+      in-place change to the input tensor.
+      Example:
+        INPUT_TENSOR = [[1, 2], [3, 4], [5, 6]]
+        INDICES = [1]
+        ROW = [9, 0]
+        OUTPUT_TENSOR = [[1, 2], [9, 0], [5, 6]]
+      )DOC")
+    .Input(0, "input_tensor", "Input tensor needs to be modified.")
+    .Input(1, "indices", "Indices of rows need to be copied")
+    .Input(2, "row", "1-d tensor that is going to replace the rows")
+    .Output(0, "output_tensor", "updated tensor")
+    .TensorInferenceFunction([](const OperatorDef& /*def*/,
+                                const vector<TensorShape>& in) {
+      vector<TensorShape> out(1);
+      out[0] = in[0];
+      return out;
+    });
+
+} // namespace
+} // namespace caffe2
