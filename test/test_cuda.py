8feb88a93c3c68da02293d42f212dbeb839c0e7e
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2019-07-02 16:40:49 -0500

    Document the failures as being python 3.6 related.

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2339,6 +2339,7 @@ class TestCuda(TestCase):
                             self.assertEqual(torch.backends.cuda.cufft_plan_cache.max_size, 10)  # default is cuda:0
                         self.assertEqual(torch.backends.cuda.cufft_plan_cache.max_size, 11)  # default is cuda:1
 
+    # passes on ROCm w/ python 2.7, fails w/ python 3.6
     @skipIfRocm
     def test_stft(self):
         _TestTorchMixin._test_stft(self, device=torch.device('cuda'))
