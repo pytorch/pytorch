f5b3f9ecd9c21a720d90012cd4d71071b37c24df
Hong Xu <hong@topbug.net>
2019-07-02 14:37:22 -0700

    Remove unnecessary ROCm detection code in Python scripts. (#22464)
    Summary:
    ROCm is already detected in cmake/public/LoadHIP.cmake. No need to
    detect twice. Plus, the Python script read environment variable
    ROCM_HOME, but what is really used in CMake scripts is ROCM_PATH -- A
    user must specify both environment variables right. Since ROCM_HOME is
    undocumented, this commit completely eradicates it.
    
     ---
    
    ezyang A remake of https://github.com/pytorch/pytorch/issues/22228 because its dependency has been dismissed.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22464
    
    Differential Revision: D16096833
    
    Pulled By: bddppq
    
    fbshipit-source-id: fea461e80ee61ec77fa3a7b476f7aec4fc453d5d

--- tools/setup_helpers/cmake.py
+++ tools/setup_helpers/cmake.py
@@ -20,7 +20,6 @@ from .dist_check import USE_DISTRIBUTED, USE_GLOO_IBVERBS
 from .nccl import (USE_SYSTEM_NCCL, NCCL_INCLUDE_DIR, NCCL_ROOT_DIR,
                    NCCL_SYSTEM_LIB, USE_NCCL)
 from .numpy_ import USE_NUMPY, NUMPY_INCLUDE_DIR
-from .rocm import USE_ROCM
 
 
 def _which(thefile):
@@ -281,7 +280,6 @@ class CMake:
             'USE_NCCL': USE_NCCL,
             'USE_SYSTEM_NCCL': USE_SYSTEM_NCCL,
             'USE_NUMPY': USE_NUMPY,
-            'USE_ROCM': USE_ROCM,
             'USE_SYSTEM_EIGEN_INSTALL': 'OFF'
         })
 
