80a1df7163fd4dd9a5dccb171a70bac388566fb7
Sebastian Messmer <messmer@fb.com>
2019-07-03 12:32:46 -0700

    V1: Initial commit
    Differential Revision: D16111969
    Differential Version: 86024667

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -351,7 +351,7 @@ def _model_to_graph(model, args, verbose=False, training=False,
 
     if do_constant_folding and _export_onnx_opset_version == 9:
         params_dict = torch._C._jit_pass_onnx_constant_fold(graph, params_dict)
-        torch._C._jit_pass_dce(graph)
+        torch._C._jit_pass_dce_allow_deleting_nodes_with_side_effects(graph)
 
     if verbose:
         print(graph)
