7c815216e03521178a50dfd4f0664371ae2413d1
Will Feng <willfeng@fb.com>
2019-07-03 16:42:05 -0400

    add final

--- torch/csrc/api/include/torch/grad_mode.h
+++ torch/csrc/api/include/torch/grad_mode.h
@@ -7,7 +7,7 @@ using autograd::AutoGradMode;
 
 // A RAII, thread local (!) guard that stops future operations from building
 // gradients.
-struct TORCH_API NoGradGuard : public AutoGradMode {
+struct TORCH_API NoGradGuard final : public AutoGradMode {
   NoGradGuard() : AutoGradMode(/*enabled=*/false) {}
 };
 
