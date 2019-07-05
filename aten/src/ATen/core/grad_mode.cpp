794a1e0569d93c91a75c01f954b6708d7fe7ff4d
Will Feng <willfeng@fb.com>
2019-07-05 16:06:00 -0400

    special-case CAFFE2_FB_LIMITED_MOBILE_CAPABILITY in grad_mode.cpp

--- aten/src/ATen/core/grad_mode.cpp
+++ aten/src/ATen/core/grad_mode.cpp
@@ -2,6 +2,11 @@
 
 namespace at {
 
+/// In the CAFFE2_FB_LIMITED_MOBILE_CAPABILITY build setting,
+/// thread_local is not supported. In that case, we don't provide
+/// `at::GradMode`.
+#ifndef CAFFE2_FB_LIMITED_MOBILE_CAPABILITY
+
 thread_local bool GradMode_enabled = true;
 
 bool GradMode::is_enabled() {
@@ -11,4 +16,15 @@ bool GradMode::is_enabled() {
 void GradMode::set_enabled(bool enabled) {
   GradMode_enabled = enabled;
 }
+
+#else // defined(CAFFE2_FB_LIMITED_MOBILE_CAPABILITY)
+
+bool GradMode::is_enabled() {
+  throw std::runtime_error("GradMode is not supported on mobile");
 }
+
+void GradMode::set_enabled(bool enabled) {
+  throw std::runtime_error("GradMode is not supported on mobile");
+}
+
+#endif
