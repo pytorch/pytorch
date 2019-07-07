465ec989f4878b66199d797803e0e940fa7d86fd
Will Feng <willfeng@fb.com>
2019-07-06 22:50:59 -0400

    fix bug

--- caffe2/core/tensor.h
+++ caffe2/core/tensor.h
@@ -192,7 +192,7 @@ class CAFFE2_API Tensor final {
    */
   void CopyFrom(const Tensor& src, bool async = false) {
     // TODO: only check `!impl_->requires_grad()` after Variable and Tensor are merged
-    AT_ASSERT(!impl_->is_variable() || !(impl_->requires_grad() && torch::autograd::GradMode::is_enabled()));
+    AT_ASSERT(!impl_->is_variable() || !(impl_->requires_grad() && at::GradMode::is_enabled()));
     AT_ASSERTM(
         src.impl_->is_contiguous(),
         "Right now only copy of contiguous source Tensor is supported.");
