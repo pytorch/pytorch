352c8b161a721d1b12ca668ad4fb2620b83df0f1
Jerry Zhang <jerryzh@fb.com>
2019-07-09 10:42:35 -0700

    torch.quantization conversion utilities, observers for eager mode quantization (#22010)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22010
    
    torch.quantization module with observers and conversion routines
    
    Reviewed By: zafartahirov
    
    Differential Revision: D15554183
    
    fbshipit-source-id: 05a3fabe28dd701978b8ecebf5bfc3a4c044ba5c

---
+++ torch/quantization/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .quantize import *  # noqa: F401
+from .observer import *  # noqa: F401
+from .QConfig import *  # noqa: F401
+
+def default_eval_fn(model, calib_data):
+    r"""
+    Default evaluation function takes a torch.utils.data.Dataset or a list of
+    input Tensors and run the model on the dataset
+    """
+    for data in calib_data:
+        model(data)
+
+_all__ = [
+    'QuantWrapper', 'QuantStub', 'DeQuantStub', 'DEFAULT_MODULE_MAPPING',
+    # Top level API for quantizing a float model
+    'quantize',
+    # Sub functions called by quantize
+    'prepare', 'convert',
+    # Sub functions for `prepare` and `swap_module`
+    'propagate_qconfig', 'add_quant_dequant', 'add_observer', 'swap_module',
+    'default_eval_fn',
+    # Observers
+    'Observer', 'WeightObserver', 'observer', 'default_observer',
+    'default_weight_observer',
+    # QConfig
+    'QConfig', 'default_qconfig'
+]
