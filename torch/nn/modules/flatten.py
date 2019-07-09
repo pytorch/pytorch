60c9ff5ee71d7699bb62a78c16aa140632c92219
Horace He <horacehe2007@yahoo.com>
2019-07-09 11:09:58 -0700

    Updated docs again

--- torch/nn/modules/flatten.py
+++ torch/nn/modules/flatten.py
@@ -5,14 +5,15 @@ from ..._jit_internal import weak_module, weak_script_method
 @weak_module
 class Flatten(Module):
     r"""
-    Flattens a contiguous range of dims into a tensor. For use with nn.Sequential.
+    Flattens a contiguous range of dims into a tensor. For use with :class:`~nn.Sequential`.
     Args:
-        start_dim: first dim to flatten
-        end_dim: last dim to flatten
+        start_dim: first dim to flatten (default = 1).
+        end_dim: last dim to flatten (default = -1).
 
     Shape:
         - Input: :math:`(N, *dims)
-        - Output: :math:`(N, product of dims)`
+        - Output: :math:`(N, \prod_{i=\text{start\_dim}}^{\text{end\_dim+1}} \text{dims[i]}, \text{*dims[end\_dim+1:]})`
+
 
     Examples::
         >>> m = nn.Sequential(
