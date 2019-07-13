3b260c21a41b1ffc0fa4e7fa5a125c92b738ef2f
Karl Ostmo <kostmo@fb.com>
2019-07-12 20:48:01 -0700

    Revert D16224780: [pytorch][PR] [ROCm] MIOpen integration into pytorch RNN operators
    Differential Revision:
    D16224780
    
    Original commit changeset: 331dafbb7689
    
    fbshipit-source-id: a6b5eb3f4820fbb58d4a329aa4c93b40a111ff27

--- aten/src/ATen/core/aten_interned_strings.h
+++ aten/src/ATen/core/aten_interned_strings.h
@@ -462,8 +462,6 @@ _(aten, miopen_depthwise_convolution) \
 _(aten, miopen_depthwise_convolution_backward) \
 _(aten, miopen_depthwise_convolution_backward_input) \
 _(aten, miopen_depthwise_convolution_backward_weight) \
-_(aten, miopen_rnn) \
-_(aten, miopen_rnn_backward) \
 _(aten, mkldnn_convolution) \
 _(aten, mkldnn_convolution_backward) \
 _(aten, mkldnn_convolution_backward_input) \
