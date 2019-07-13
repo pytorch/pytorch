cb152537c4f32f8d9cad89073ee3c90177f04928
zaf <zaf@devvm6485.prn2.facebook.com>
2019-07-12 18:47:16 -0700

    V8: (no description)
    Differential Revision: D16102608
    Differential Version: 86438103

--- torch/nn/quantized/modules/pooling.py
+++ torch/nn/quantized/modules/pooling.py
@@ -3,4 +3,4 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from torch.nn.modules.pooling import MaxPool2d
+from torch.nn.modules.pooling import MaxPool2d  # noqa: F401
