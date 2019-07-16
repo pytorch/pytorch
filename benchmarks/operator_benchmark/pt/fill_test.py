ad15cd5dc05900832c69d8c319cea7e9badaaff0
Mingzhe Li <mingzhe0908@fb.com>
2019-07-16 11:41:44 -0700

    replace ByteTensor with bool in fill_test (#22913)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/22913
    
    as title
    
    Reviewed By: hl475
    
    Differential Revision: D16285248
    
    fbshipit-source-id: 78b13d48d547760e59e0e5c8875ab09a3cd24828

--- benchmarks/operator_benchmark/pt/fill_test.py
+++ benchmarks/operator_benchmark/pt/fill_test.py
@@ -7,7 +7,7 @@ import torch
 fill_short_configs = op_bench.cross_product_configs(
     N=[10, 1000],
     device=torch.testing.get_all_device_types(),
-    dtype=[torch.ByteTensor, torch.int8, torch.uint8, torch.int16, torch.int32,
+    dtype=[torch.bool, torch.int8, torch.uint8, torch.int16, torch.int32,
            torch.int64, torch.half, torch.float, torch.double],
     tags=["short"]
 )
