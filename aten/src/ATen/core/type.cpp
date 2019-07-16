57a3cd2eea293a2ff174fd2d98da5d65a5621981
Will Feng <willfeng@fb.com>
2019-07-15 19:41:23 -0700

    Revert D16197608: [jit] Make classtypes hold a weak_ptr to their CU
    Differential Revision:
    D16197608
    
    Original commit changeset: 22250d6f0d24
    
    fbshipit-source-id: 47a8cdeb62b1033252070ecb92906358014b551a

--- aten/src/ATen/core/type.cpp
+++ aten/src/ATen/core/type.cpp
@@ -5,6 +5,14 @@
 #include <c10/macros/Macros.h>
 namespace c10 {
 
+namespace ivalue {
+Object::~Object() {
+  if (on_delete_) {
+    on_delete_(this);
+  }
+}
+} // namespace ivalue
+
 std::ostream& operator<<(std::ostream & out, const Type & t) {
   if(auto value = t.cast<CompleteTensorType>()) {
     out << toString(value->scalarType()) << "(";
