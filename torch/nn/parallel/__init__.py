44166ec39f5cd31accdbda72564bcdf2566620ab
Shen Li <cs.shenli@gmail.com>
2019-07-17 12:15:59 -0700

    fix indent

--- torch/nn/parallel/__init__.py
+++ torch/nn/parallel/__init__.py
@@ -8,7 +8,7 @@ __all__ = ['replicate', 'scatter', 'parallel_apply', 'gather', 'data_parallel',
            'DataParallel', 'DistributedDataParallel']
 
 def DistributedDataParallelCPU(*args, **kwargs):
-  import warnings
-  warnings.warn("torch.nn.parallel.DistributedDataParallelCPU is deprecated, "
-                "please use torch.nn.parallel.DistributedDataParallel instead.")
-  return DistributedDataParallel(*args, **kwargs)
+    import warnings
+    warnings.warn("torch.nn.parallel.DistributedDataParallelCPU is deprecated, "
+                  "please use torch.nn.parallel.DistributedDataParallel instead.")
+    return DistributedDataParallel(*args, **kwargs)
