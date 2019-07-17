0721c784e5b7ed2747b82974d73e7335559e70b3
Igor Fedan <ifedan@fb.com>
2019-07-17 14:14:30 -0400

    Fix for ceil mode pooling size calculation

--- aten/src/ATen/native/Pool.h
+++ aten/src/ATen/native/Pool.h
@@ -24,7 +24,7 @@ template<typename T>
 static inline T pooling_output_shape(
         T inputSize, T kernelSize, T pad, T stride, T dilation, bool ceil_mode) {
   T size = inputSize + 2 * pad - dilation * (kernelSize - 1) - 1;
-  T outputSize = ((size + (size >= 0 ? 1 : -1) * (ceil_mode ? stride - 1 : 0)) /RegistrationDeclarations stride + 1);
+  T outputSize = ((size + (size >= 0 ? 1 : -1) * (ceil_mode ? stride - 1 : 0)) / stride + 1);
   if (pad) {
     // ensure that the last pooling starts inside the image
     // needed to avoid problems in ceil mode
