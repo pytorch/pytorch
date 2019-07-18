a68cfe10f8f5e5f5148b4df752e0382c803462be
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-07-18 09:53:46 -0700

    Bring back c++ extension

---
+++ torch/csrc/tensortypes/nestedtensor/nestedtensor.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <torch/csrc/python_headers.h>
+
+namespace torch {
+namespace tensortypes {
+namespace nestedtensor {
+
+PyMethodDef* python_functions();
+
+} // namespace nestedtensor
+} // namespace tensortypes
+} // namespace torch
