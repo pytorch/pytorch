8af23905b10d8d062e4660cb321247efbef65b9d
Hong Xu <hong@topbug.net>
2019-07-18 00:39:04 -0700

    Update on "Deprecate function variants of in-place operators. (not those in the nn module)"
    Deprecate function variants of in-place operators.
    
    gh-metadata: pytorch pytorch 22720 gh/xuhdev/9/head

--- tools/autograd/gen_python_functions.py
+++ tools/autograd/gen_python_functions.py
@@ -708,12 +708,13 @@ def create_python_bindings(python_functions, has_self, is_module=False):
 
         # In-place operator, should be method only. Deprecate it.
         if not is_module and not has_self and name.endswith('_') and not name.startswith('_'):
-            deprecated_functions = ['abs_', 'acos_', 'addmv_', 'as_strided_', 'asin_', 'atan_', 'ceil_', 'celu_',
+            deprecated_functions = ('dropout_', 'feature_dropout_', 'alpha_dropout_', 'feature_alpha_dropout_',
+                                    'abs_', 'acos_', 'addmv_', 'as_strided_', 'asin_', 'atan_', 'ceil_', 'celu_',
                                     'clamp_', 'clamp_max_', 'clamp_min_', 'cos_', 'cosh_', 'detach_',
                                     'embedding_renorm_', 'erf_', 'erfc_', 'exp_', 'expm1_', 'fill_', 'floor_', 'frac_',
                                     'index_put_', 'log10_', 'log1p_', 'log2_', 'log_', 'neg_', 'reciprocal_', 'relu_',
                                     'resize_as_', 'round_', 'rrelu_', 'rsqrt_', 'selu_', 'sigmoid_', 'sin_', 'sinh_',
-                                    'sqrt_', 'tan_', 'tanh_', 'threshold_', 'trunc_', 'zero_']
+                                    'sqrt_', 'tan_', 'tanh_', 'threshold_', 'trunc_', 'zero_')
             if name not in deprecated_functions:  # This is an in-place function variant that was later added
                 raise AssertionError('{} must not have a function variant.'.format(name))
 
