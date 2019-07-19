f19aa12ae5724d70d54c302eb7fb37bb8f268c5f
Soumith Chintala <soumith@fb.com>
2019-07-19 03:06:44 -0700

    Revert D16274792: [qat] Conv module
    Differential Revision:
    D16274792
    
    Original commit changeset: 1da10194123b
    
    fbshipit-source-id: 71b34774b463f2350289bd39b8cfd798e095ffa5

--- torch/nn/quantized/modules/linear.py
+++ torch/nn/quantized/modules/linear.py
@@ -26,13 +26,13 @@ class Quantize(Module):
 
     def __init__(self, out_scale, out_zero_point, out_dtype):
         super(Quantize, self).__init__()
-        self.register_buffer('_scale', torch.tensor([out_scale]))
-        self.register_buffer('_zero_point', torch.tensor([out_zero_point], dtype=torch.long))
-        self._dtype = out_dtype
+        self.register_buffer('out_scale', torch.tensor([out_scale]))
+        self.register_buffer('out_zero_point', torch.tensor([out_zero_point], dtype=torch.long))
+        self.out_dtype = out_dtype
 
     def forward(self, X):
-        return torch.quantize_linear(X, self._scale.item(),
-                                     self._zero_point.item(), self._dtype)
+        return torch.quantize_linear(X, self.out_scale.item(),
+                                     self.out_zero_point.item(), self.out_dtype)
 
     @staticmethod
     def from_float(mod):
@@ -108,10 +108,10 @@ class Linear(NNLinear):
             dtype=torch.qint8)
         self.register_buffer('_packed_weight',
                              torch.ops.quantized.fbgemm_linear_prepack(qweight))
-        self.register_buffer('scale',
-                             torch.tensor([1.0], dtype=torch.double))
-        self.register_buffer('zero_point',
-                             torch.tensor([0], dtype=torch.long))
+        self.register_buffer('out_scale',
+                             torch.Tensor([1.0]).to(torch.double))
+        self.register_buffer('out_zero_point',
+                             torch.Tensor([0]).to(torch.long))
 
     @property
     def weight(self):
@@ -126,8 +126,8 @@ class Linear(NNLinear):
         Y_q = torch.ops.quantized.fbgemm_linear(
             x, self._packed_weight,
             self.bias,
-            self.scale,
-            self.zero_point)
+            self.out_scale,
+            self.out_zero_point)
         return Y_q
 
     def _save_to_state_dict(self, destination, prefix, keep_vars):
@@ -165,14 +165,14 @@ class Linear(NNLinear):
             weight_observer = mod.qconfig.weight()
             weight_observer(mod.weight)
         activation_observer = mod.observer
-        act_scale, act_zp = activation_observer.calculate_qparams()
-        wt_scale, wt_zp = weight_observer.calculate_qparams()
-        bias_scale = (wt_scale * act_scale).float()
-        qweight = torch.quantize_linear(mod.weight.float(), wt_scale, wt_zp.long().item(), torch.qint8)
+        act_qparams = activation_observer.calculate_qparams()
+        wt_qparams = weight_observer.calculate_qparams()
+        bias_scale = (wt_qparams[0] * act_qparams[0]).float()
+        qweight = torch.quantize_linear(mod.weight.float(), wt_qparams[0], wt_qparams[1].long().item(), torch.qint8)
         qbias = torch.quantize_linear(mod.bias.float(), bias_scale, 0, torch.qint32)
         qlinear = Linear(mod.in_features, mod.out_features)
         qlinear._packed_weight = torch.ops.quantized.fbgemm_linear_prepack(qweight)
         qlinear.bias = qbias
-        qlinear.scale = torch.tensor([act_scale], dtype=torch.double)
-        qlinear.zero_point = torch.tensor([act_zp], dtype=torch.long)
+        qlinear.out_scale = torch.tensor([act_qparams[0]])
+        qlinear.out_zero_point = torch.tensor([act_qparams[1]])
         return qlinear
--- torch/quantization/quantize.py
+++ torch/quantization/quantize.py
@@ -57,6 +57,7 @@ def _observer_forward_hook(self, input, output):
 def _fake_quant_forward_hook(self, input, output):
     return self.observer(output)
 
+
 # TODO(jerryzh): remove_observer?
 def add_observer(module):
     r"""Add observer for the leaf child of the module.
@@ -231,13 +232,11 @@ DEFAULT_MODULE_MAPPING = {
     DeQuantStub: nnq.DeQuantize,
     # QAT modules:
     qat.Linear: nnq.Linear,
-    qat.Conv2d: nnq.Conv2d,
 }
 
 # Map for swapping float module to qat modules
 DEFAULT_QAT_MODULE_MAPPING = {
     torch.nn.Linear: qat.Linear,
-    torch.nn.Conv2d: qat.Conv2d,
 }
 
 def convert(module, mapping=DEFAULT_MODULE_MAPPING):
