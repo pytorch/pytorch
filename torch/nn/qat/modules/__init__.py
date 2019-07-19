50b4afcf8c6dce48c9031ffe35160ab57b6c59dc
Soumith Chintala <soumith@fb.com>
2019-07-19 03:06:44 -0700

    Revert D16274792: [qat] Conv module
    Differential Revision:
    D16274792
    
    Original commit changeset: 1da10194123b
    
    fbshipit-source-id: 71b34774b463f2350289bd39b8cfd798e095ffa5

--- torch/nn/qat/modules/__init__.py
+++ torch/nn/qat/modules/__init__.py
@@ -1,9 +1,5 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 from .linear import Linear
-from .conv import Conv2d
 
 __all__ = [
     'Linear',
-    'Conv2d',
 ]
