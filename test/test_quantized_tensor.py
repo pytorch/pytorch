31457784df1f4cf315900af3b816f189c103d404
Jerry Zhang <jerryzh168@gmail.com>
2019-07-19 11:35:06 -0700

    Update on "support at::view and at::reshape for quantized tensor"
    att
    
    Differential Revision: [D16368897](https://our.internmc.facebook.com/intern/diff/D16368897/)

--- test/test_quantized_tensor.py
+++ test/test_quantized_tensor.py
@@ -221,6 +221,8 @@ class TestQuantizedTensor(TestCase):
         err_str = "view size is not compatible with input tensor's size and stride*"
         with self.assertRaisesRegex(RuntimeError, err_str):
             b.view(1, 4, 2, 3)
+        # view on contiguous tensor is fine
+        b.contiguous().view(1, 4, 2, 3)
 
 
     def test_qtensor_reshape(self):
@@ -236,6 +238,8 @@ class TestQuantizedTensor(TestCase):
         b = a.transpose(1, 2)  # swaps 2nd and 3rd dimension
         c = a.reshape(1, 3, 2, 4)  # does not change tensor layout
         self.assertEqual(b.size(), c.size())
+        self.assertEqual(b.q_scale(), c.q_scale())
+        self.assertEqual(b.q_zero_point(), c.q_zero_point())
         self.assertNotEqual(b.int_repr(), c.int_repr())
 
         # we can use reshape for non-contiguous Tensor
