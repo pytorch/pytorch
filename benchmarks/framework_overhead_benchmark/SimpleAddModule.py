adc1eae39488c2cde5bbdbaa79c3010a36926df9
Kimish Patel <kimishpatel@fb.com>
2019-07-19 08:21:29 -0700

    Added add op framework overhead benchmark for C2
    Summary: C2 benchmark.
    
    Differential Revision: D16122337
    
    fbshipit-source-id: 2f6d342771f6e2cfcb39a42ef659ad6e3a14ac31

--- benchmarks/framework_overhead_benchmark/SimpleAddModule.py
+++ benchmarks/framework_overhead_benchmark/SimpleAddModule.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import torch
-from utils import NUM_PT_LOOP_ITERS
+from utils import NUM_LOOP_ITERS
 
 def add_tensors_loop(x, y):
     z = torch.add(x, y)
-    for i in range(NUM_PT_LOOP_ITERS):
+    for i in range(NUM_LOOP_ITERS):
         z = torch.add(z, x)
     return z
 
