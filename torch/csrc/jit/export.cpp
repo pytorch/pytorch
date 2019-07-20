ff678d76b658f1043cdc24265118fb3d62856734
Spandan Tiwari <sptiwari@microsoft.com>
2019-07-19 18:04:10 -0700

    Updating producer_version in exported ONNX models to pytorch 1.2.

--- torch/csrc/jit/export.cpp
+++ torch/csrc/jit/export.cpp
@@ -209,7 +209,7 @@ EncoderBase::EncoderBase(
   // stable. only bump it when it's necessary
   model_proto_.set_ir_version(4);
   // TODO: set the producer version using appropriate function call
-  model_proto_.set_producer_version("1.1");
+  model_proto_.set_producer_version("1.2");
 }
 
 void EncoderBase::EncodeValueInfo(
