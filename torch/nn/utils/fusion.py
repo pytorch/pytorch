fff62f6b8b18987902ba75bc78c82f0abfdc9f21
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-07-23 15:30:13 -0700

    Optimizing out the division in the fusion

--- torch/nn/utils/fusion.py
+++ torch/nn/utils/fusion.py
@@ -12,7 +12,7 @@ def fuse_conv_bn_eval(conv, bn):
     b_conv = fused_conv.bias
 
     bn_mean = bn.running_mean
-    bn_var_sqrt = torch.sqrt(bn.running_var + bn.eps)
+    bn_var_sqrt = torch.rsqrt(bn.running_var + bn.eps)
 
     bn_weight = bn.weight
     bn_bias = bn.bias
@@ -20,8 +20,8 @@ def fuse_conv_bn_eval(conv, bn):
     if b_conv is None:
         b_conv = bn_mean.new_zeros(bn_mean.shape)
 
-    w_conv = w_conv * (bn_weight / bn_var_sqrt).reshape([-1, 1, 1, 1])
-    b_conv = (b_conv - bn_mean) / bn_var_sqrt * bn_weight + bn_bias
+    w_conv = w_conv * (bn_weight * bn_var_sqrt).reshape([-1, 1, 1, 1])
+    b_conv = (b_conv - bn_mean) * bn_var_sqrt * bn_weight + bn_bias
 
     fused_conv.weight = torch.nn.Parameter(w_conv)
     fused_conv.bias = torch.nn.Parameter(b_conv)
