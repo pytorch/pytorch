c5885d08539809bbb54c3ded1c9eee84f176a376
Zachary DeVito <zdevito@fb.com>
2019-07-23 14:11:35 -0700

    pickler.cpp: respect __getstate__/__setstate__
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/23190
    
    Test Plan: Imported from OSS
    
    Differential Revision: D16431553
    
    Pulled By: zdevito
    
    fbshipit-source-id: 680ea1507c12727fd17aedb3067f522cf490e306

--- test/test_jit.py
+++ test/test_jit.py
@@ -12227,10 +12227,11 @@ a")
 
             @torch.jit.export
             def __getstate__(self):
-                pass
+                return None
 
             @torch.jit.export
-            def __setstate__(self):
+            def __setstate__(self, _):
+                # type: (None) -> None
                 self.buffer1 = torch.ones(2, 2) + 10
                 self.buffer2 = torch.ones(2, 2) + 10
 
@@ -13413,6 +13414,22 @@ class TestRecursiveScript(JitTestCase):
                 self.a = 4
                 self.inner = Inner()
 
+        @torch.jit.script
+        class SFoo(object):
+            def __init__(self):
+                self.a = 4
+                self.inner = Inner()
+
+            def __setstate__(self, obj):
+                # type: (Tuple[int, Inner]) -> None
+                a, inner = obj
+                self.a = a
+                self.inner = inner
+
+            def __getstate__(self):
+                return (self.a, self.inner)
+
+
         untyped_values = (
             ('my_dict', {"I": "am", "a test": "test"}),
             ('my_float', 2.3),
@@ -13432,6 +13449,7 @@ class TestRecursiveScript(JitTestCase):
             ('my_empty_dict', {}),
             ('my_none', None),
             ('my_object', Foo()),
+            ('my_object2', SFoo()),
         )
 
         class M(torch.nn.Module):
@@ -13461,6 +13479,8 @@ class TestRecursiveScript(JitTestCase):
                     self.my_none,
                     self.my_object.a,
                     self.my_object.inner.b,
+                    self.my_object.a,
+                    self.my_object2.inner.b,
                 )
 
         # TODO: as a followup, fix this test
@@ -13477,6 +13497,7 @@ class TestRecursiveScript(JitTestCase):
             'my_empty_dict': Dict[str, int],
             'my_none': Optional[int],
             'my_object': Foo,
+            'my_object2': SFoo,
         }
 
         m = M()
