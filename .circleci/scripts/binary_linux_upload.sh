15876768ccb429bbe28ac8edeb9d05009443a6a4
pjh5 <hellemn@fb.com>
2019-07-24 16:53:31 -0700

    Removing empty foo file

--- .circleci/scripts/binary_linux_upload.sh
+++ .circleci/scripts/binary_linux_upload.sh
@@ -30,7 +30,7 @@ if [[ "$PACKAGE_TYPE" == conda ]]; then
 elif [[ "$PACKAGE_TYPE" == libtorch ]]; then
   retry pip install -q awscli
   s3_dir="s3://pytorch/libtorch/${PIP_UPLOAD_FOLDER}${DESIRED_CUDA}/"
-  retry aws s3 cp "$pkg" "$s3_dir" --acl public-read
+  retry aws s3 cp "$(ls)" "$s3_dir" --acl public-read
 else
   retry pip install -q awscli
   s3_dir="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}${DESIRED_CUDA}/"
