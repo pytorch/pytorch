803abc6513c34e2625e38e699485b430d1eba1d0
Igor Fedan <ifedan@fb.com>
2019-07-24 18:12:26 -0400

    Fix cdist test

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -2438,8 +2438,8 @@ class TestAutograd(TestCase):
             devices = torch.testing.get_all_device_types()
             for p in [0, 1, 2, 3, 1.5, 2.5, float('inf')]:
                 for device in devices:
-                    f_args_variable = (torch.randn(sizes, device=device, requires_grad=True, dtype=torch.double),
-                                       torch.randn(sizes, device=device, requires_grad=True, dtype=torch.double))
+                    f_args_variable = (torch.randn(sizes, device=device, requires_grad=True),
+                                       torch.randn(sizes, device=device, requires_grad=True))
 
                     def f(a, b):
                         return torch.cdist(a, b, p)
@@ -3566,10 +3566,14 @@ def gradgradcheck_method_precision_override(test_name):
             override = {'atol': override['atol'] * S * S, 'rtol': override['atol'] * S * S}
     return override
 
+GRADCHECK_EPS_OVERRIDE = {
+    'test_cdist': 1e-3,
+}
 
 def run_grad_and_gradgrad_checks(test_case, name, test_name, apply_method, output_variable,
                                  input_variables, run_gradgradcheck=True):
-    test_case.assertTrue(gradcheck(apply_method, input_variables, eps=1e-6, atol=PRECISION))
+    eps = 1e-6 if test_name not in GRADCHECK_EPS_OVERRIDE else GRADCHECK_EPS_OVERRIDE[test_name]
+    test_case.assertTrue(gradcheck(apply_method, input_variables, eps=eps, atol=PRECISION))
     if name in EXCLUDE_GRADGRADCHECK or test_name in EXCLUDE_GRADGRADCHECK_BY_TEST_NAME:
         return
     gradgradcheck_precision_override = gradgradcheck_method_precision_override(test_name)
