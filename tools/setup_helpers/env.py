9f7b2421af6aa12d78dcf0f79cabbac65761d603
Hong Xu <hong@topbug.net>
2019-07-24 12:57:37 -0700

    Update on "[WIP] For second-time build, let build_type be inferred from CMakeCache.txt."
    This leads to more consistent build outcome.

--- tools/setup_helpers/env.py
+++ tools/setup_helpers/env.py
@@ -83,18 +83,20 @@ class BuildType(object):
     def __init__(self, cmake_build_type_env=None):
         if cmake_build_type_env is not None:
             self.build_type_string = cmake_build_type_env
+            return
+
+        cmake_cache_txt = os.path.join(BUILD_DIR, 'CMakeCache.txt')
+        if os.path.isfile(cmake_cache_txt):
+            # Found CMakeCache.txt. Use the build type specified in it.
+            from .cmake import get_cmake_cache_variables_from_file
+            with open(cmake_cache_txt) as f:
+                cmake_cache_vars = get_cmake_cache_variables_from_file(f)
+            # Normally it is anti-pattern to determine build type from CMAKE_BUILD_TYPE because it is not used for
+            # multi-configuration build tools, such as Visual Studio and XCode. But since we always communicate with
+            # CMake using CMAKE_BUILD_TYPE from our Python scripts, this is OK here.
+            self.build_type_string = cmake_cache_vars['CMAKE_BUILD_TYPE']
         else:
-            cmake_cache_txt = os.path.join(BUILD_DIR, 'CMakeCache.txt')
-            if os.path.isfile(cmake_cache_txt):
-                # Found CMakeCache.txt. Use the build type specified in it.
-                from .cmake import CMake
-                cmake_cache_vars = CMake.get_cmake_cache_variables_from_file(cmake_cache_txt)
-                # Normally it is anti-pattern to determine build type from CMAKE_BUILD_TYPE because it is not used for
-                # multi-configuration build tools, such as Visual Studio and XCode. But since we always communicate with
-                # CMake using CMAKE_BUILD_TYPE from our Python scripts, this is OK here.
-                self.build_type_string = cmake_cache_vars['CMAKE_BUILD_TYPE']
-            else:
-                self.build_type_string = os.environ.getattr('CMAKE_BUILD_TYPE', 'Release')
+            self.build_type_string = os.environ.get('CMAKE_BUILD_TYPE', 'Release')
 
     def is_debug(self):
         "Checks Debug build."
@@ -118,4 +120,4 @@ if 'CMAKE_BUILD_TYPE' not in os.environ:
     else:
         os.environ['CMAKE_BUILD_TYPE'] = 'Release'
 
-build_type = BuildType(os.environ['CMAKE_BUILD_TYPE'])
+build_type = BuildType()
