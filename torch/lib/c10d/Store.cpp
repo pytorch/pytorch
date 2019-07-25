8b07d67143de39ecaf3191f95b18d56c12ed18bc
Pieter Noordhuis <pcnoordhuis@gmail.com>
2019-07-25 03:02:09 -0700

    Remove superfluous check
    This check is not needed. Even if it were, the assignment is clobbered anyway.
    
    Closes #23300.
    
    Differential Revision: [D16485329](https://our.internmc.facebook.com/intern/diff/D16485329/)

--- torch/lib/c10d/Store.cpp
+++ torch/lib/c10d/Store.cpp
@@ -2,17 +2,11 @@
 
 namespace c10d {
 
-constexpr std::chrono::milliseconds Store::kDefaultTimeout;
-constexpr std::chrono::milliseconds Store::kNoTimeout;
-
 // Define destructor symbol for abstract base class.
 Store::~Store() {}
 
 // Set timeout function
 void Store::setTimeout(const std::chrono::milliseconds& timeout) {
-  if (timeout.count() == 0) {
-    timeout_ = kNoTimeout;
-  }
   timeout_ = timeout;
 }
 
