bbe3cb1638517a9d7fd07342ef754890abe25c17
Jesse Hellemn <hellemn@fb.com>
2019-07-25 10:35:31 -0700

    Fix lint
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/23381
    
    Differential Revision: D16496327
    
    Pulled By: pjh5
    
    fbshipit-source-id: 529029544a5f8c8106bcb7cebdc71aee33e3b86c

--- tools/setup_helpers/cmake.py
+++ tools/setup_helpers/cmake.py
@@ -12,7 +12,7 @@ import distutils.sysconfig
 from distutils.version import LooseVersion
 
 from . import escape_path, which
-from .env import (BUILD_DIR, IS_64BIT, IS_DARWIN, IS_WINDOWS, check_env_flag, check_negative_env_flag, build_type)
+from .env import (BUILD_DIR, IS_64BIT, IS_DARWIN, IS_WINDOWS, check_negative_env_flag, build_type)
 from .cuda import USE_CUDA
 from .dist_check import USE_DISTRIBUTED, USE_GLOO_IBVERBS
 from .numpy_ import USE_NUMPY, NUMPY_INCLUDE_DIR
