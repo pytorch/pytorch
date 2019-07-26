b924f97204b2caf53dd889db116a2bcbb5661cdb
Igor Fedan <ifedan@fb.com>
2019-07-26 12:13:23 -0400

    Debug info

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -3620,7 +3620,7 @@ def gradgradcheck_method_precision_override(test_name):
     return override
 
 GRADCHECK_EPS_OVERRIDE = {
-    'test_cdist': 1e-5,
+    'test_cdist': 1e-3,
 }
 
 GRADCHECK_PRECISION_OVERRIDE = {
@@ -3635,7 +3635,7 @@ def run_grad_and_gradgrad_checks(test_case, name, test_name, apply_method, outpu
     printDebug = False
     if "test_cdist" in test_name:
         printDebug = True
-        eps = 1e-4
+        eps = 1e-3
         atol = 1e-4
 
     test_case.assertTrue(gradcheck(apply_method, input_variables, eps=eps, atol=atol, printDebug=printDebug))
