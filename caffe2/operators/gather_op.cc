a4d9a3333a619424e42620809aec28b86ecc4bcb
Shali Jiang <shalijiang@fb.com>
2019-07-26 18:48:43 -0700

    support Gather different indices for different examples in one batch (#23285)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23285
    
    for example:
    
    Inputs:
      data:
       [[[2 4 2 0],
         [0 1 2 0],
         [1 1 0 0]],
        [[3 4 1 3],
         [0 3 2 2],
         [4 1 0 4]]]
    
      idx:
        [[0 2],
         [0 1]]
    
    outputs:
      [[[2 4 2 0],
        [1 1 0 0]],
       [[3 4 1 3],
        [0 3 2 2]]]
    
    data and idx must have the same first dimension
    
    Must call Gather with arg axis=1, match_outer=True
    
    Differential Revision: D16452539
    
    fbshipit-source-id: 8760320c69b4172deeceadfeb89b26d6f5e09110

--- caffe2/operators/gather_op.cc
+++ caffe2/operators/gather_op.cc
@@ -79,12 +79,14 @@ OUTPUT:
                                 const vector<TensorShape>& in) {
       ArgumentHelper helper(def);
       const int axis = helper.GetSingleArgument<int>("axis", 0);
+      const bool match_outer =
+          helper.GetSingleArgument<bool>("match_outer", false);
       const auto& data_dims = GetDimsVector(in[0]);
       const auto& indices_dims = GetDimsVector(in[1]);
 
       vector<int> output_dims =
           caffe2::gather_helper::calc_output_shape_vector<int>(
-              data_dims, indices_dims, axis);
+              data_dims, indices_dims, axis, match_outer);
       vector<TensorShape> out(1);
       out[0] = CreateTensorShape(output_dims, in[0].data_type());
       return out;
