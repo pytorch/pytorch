c28bf032dfb7e92b0c15781360138a6c2a3c3110
Horace He <chilli@fb.com>
2019-07-29 16:22:05 -0700

    Fixed clang format

--- torch/csrc/jit/pybind_utils.cpp
+++ torch/csrc/jit/pybind_utils.cpp
@@ -11,7 +11,7 @@ getClassConverter() {
 }
 
 c10::optional<py::object> tryToConvertToCustomClass(
-    c10::intrusive_ptr<c10::ivalue::Object> obj) {
+    const c10::intrusive_ptr<c10::ivalue::Object>& obj) {
   if (obj->name().find("__torch__.torch.classes") == 0) {
     auto objPtr = (void*)obj->getSlot(0).toCapsule().release();
     auto classConverter = getClassConverter()[obj->name()];
