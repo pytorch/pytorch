f7b53c9a1e349e75f5e499afe065a109d87ebcb0
Mikhail Zolotukhin <mvz@fb.com>
2019-07-30 11:18:50 -0700

    Cleanup interface of inlineCallTo.
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/23539
    
    Test Plan: Imported from OSS
    
    Differential Revision: D16555365
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 6cfcde7a7600315e73e083284c80f876509489a5

--- torch/csrc/jit/passes/inliner.cpp
+++ torch/csrc/jit/passes/inliner.cpp
@@ -9,31 +9,18 @@ namespace prim {
 using namespace ::c10::prim;
 }
 
-static void replace(
-    Node* to_replace,
-    const Function* fn,
-    at::ArrayRef<Value*> inputs) {
-  WithInsertPoint guard(to_replace);
-  auto new_output =
-      inlineCallTo(*to_replace->owningGraph(), *fn->graph(), inputs).at(0);
-  if (to_replace->output()->hasDebugName()) {
-    new_output->setDebugName(to_replace->output()->debugName());
-  }
-  to_replace->output()->replaceAllUsesWith(new_output);
-}
-
 void inlineCalls(Block* block) {
   for (auto it = block->nodes().begin(), end = block->nodes().end();
        it != end;) {
     Node* cur = *it++;
     switch (cur->kind()) {
       case prim::CallFunction: {
-        AT_ASSERT(cur->inputs().at(0)->node()->kind() == prim::Constant);
-        auto function_constant = cur->inputs().at(0)->node();
+        AT_ASSERT(cur->input(0)->node()->kind() == prim::Constant);
+        auto function_constant = cur->input(0)->node();
         auto fun_type =
             function_constant->output()->type()->expect<FunctionType>();
-        replace(cur, fun_type->function(), cur->inputs().slice(1));
-        cur->destroy();
+        cur->removeInput(0);
+        inlineCallTo(cur, *fun_type->function()->graph());
         if (!function_constant->hasUses()) {
           function_constant->destroy();
         }
@@ -41,9 +28,8 @@ void inlineCalls(Block* block) {
       case prim::CallMethod: {
         const std::string& name = cur->s(attr::name);
         auto function =
-            cur->inputs().at(0)->type()->expect<ClassType>()->getMethod(name);
-        replace(cur, function, cur->inputs());
-        cur->destroy();
+            cur->input(0)->type()->expect<ClassType>()->getMethod(name);
+        inlineCallTo(cur, *function->graph());
       } break;
       default: {
         for (auto b : cur->blocks()) {
