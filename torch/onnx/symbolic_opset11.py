f1d94d487df3fb98f6ed39ba49dd4c83055dff97
neginraoof <neginmr@utexas.edu>
2019-08-02 10:15:55 -0700

    fixed symbolic file notes

--- torch/onnx/symbolic_opset11.py
+++ torch/onnx/symbolic_opset11.py
@@ -1,20 +1,10 @@
-import torch
-from torch.nn.modules.utils import _single, _pair, _triple
-import torch.onnx
-# This import monkey-patches graph manipulation methods on Graph, used for the
-# ONNX symbolics
-import torch.onnx.utils
-
-import torch.onnx.symbolic_helper as sym_help
 from torch.onnx.symbolic_helper import parse_args, _unimplemented
 
 
 # EDITING THIS FILE? READ THIS FIRST!
 # see Note [Edit Symbolic Files] in symbolic_helper.py
 
-# This file exports ONNX ops for opset 10
-# Opset 10 is supported by ONNX release 1.5.0
-# release on 04/24/19
+# This file exports ONNX ops for opset 11
 
 @parse_args('v', 'i')
 def pixel_shuffle(g, self, upscale_factor):
