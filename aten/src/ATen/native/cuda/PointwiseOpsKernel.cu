2e751ab6eaf533c2b45e8c84143e700d2d69a1fa
Igor Fedan <ifedan@fb.com>
2019-08-02 17:53:50 -0400

    CUDA implementation

--- aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
+++ aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
@@ -8,14 +8,13 @@
 
 namespace at { namespace native {
 
-static void addcdiv_cuda_kernel(TensorIterator& iter, Scalar value) {
-    AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "addcdiv_cuda", [&]() {
-      auto alpha = value.to<scalar_t>();
-      gpu_kernel(iter, [alpha]GPU_LAMBDA(scalar_t a, scalar_t b, scalar_t c) -> scalar_t {
-        return THCNumerics<scalar_t>::add(a, THCNumerics<scalar_t>::mul(alpha,THCNumerics<scalar_t>::div(b, c)));
-      });
+void addcdiv_cuda_kernel(TensorIterator& iter, Scalar value) {
+  AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "addcdiv_cuda", [&]() {
+    auto alpha = value.to<scalar_t>();
+    gpu_kernel(iter, [alpha]GPU_LAMBDA(scalar_t a, scalar_t b, scalar_t c) -> scalar_t {
+      return THCNumerics<scalar_t>::add(a, THCNumerics<scalar_t>::mul(alpha,THCNumerics<scalar_t>::div(b, c)));
     });
-  }
+  });
 }
 
 REGISTER_DISPATCH(addcdiv_stub, &addcdiv_cuda_kernel);
