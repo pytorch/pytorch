e93b9746cd1ec9fcdfdcf33258c276e67cb792a8
James Reed <jamesreed@fb.com>
2019-08-03 20:49:08 -0700

    Update on "[wip][quantization] Enable OSS quantization tests"
    Changes:
    
    - Enable tests for quantization test files in `run_tests.py`
    - Remove `__future__` imports from `torch/nn/qat/modules/__init__.py`, since `unicode_literals` messes up imports on python2 because the elements in `__all__` will be Unicode and not string
    - Skip PostTrainingQuantTests if the build doesn't have FBGEMM (only a small subset of targets in tests) or if testing under UBSAN (the suppression file doesn't seem to work)
    - Initialize the CUDA runtime in a static class method on `TestFakeQuantizePerTensorAffine`. This prevents CUDA test cases from timing out because the runtime initialization time is accounted for in a test
    
    Outstanding issues:
    
    - [ ] MKLDNN corruption, see https://github.com/pytorch/pytorch/issues/23763#issuecomment-517958683
    - [x] There is an issue with the CUDA fake quantization tests causing flaky failures
    
    Differential Revision: [D16639467](https://our.internmc.facebook.com/intern/diff/D16639467)

--- test/test_quantized_conv.py
+++ test/test_quantized_conv.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import unittest
 import numpy as np
 import torch
 import torch.nn.quantized.functional as qF
