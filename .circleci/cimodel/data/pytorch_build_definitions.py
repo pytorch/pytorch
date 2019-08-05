07e9e3fc89e73f1a699ce04f5375ea7dc46cfcda
Edward Z. Yang <ezyang@fb.com>
2019-08-05 13:45:20 -0400

    Update on "Add CUDA 10.1 to CI."
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .circleci/cimodel/data/pytorch_build_definitions.py
+++ .circleci/cimodel/data/pytorch_build_definitions.py
@@ -222,7 +222,7 @@ def instantiate_configs():
             if compiler_name == "clang":
                 parms_list.append("asan")
 
-        if cuda_version in ["9.2", "10"]:
+        if cuda_version in ["9.2", "10", "10.1"]:
             # TODO The gcc version is orthogonal to CUDA version?
             parms_list.append("gcc7")
 
--- .circleci/scripts/should_run_job.py
+++ .circleci/scripts/should_run_job.py
@@ -15,7 +15,7 @@ default_set = [
     # PyTorch CUDA
     'pytorch-linux-xenial-cuda9-cudnn7-py3',
     # PyTorch CUDA 10.1 (testing!)
-    'pytorch-linux-xenial-cuda10.1-cudnn7-py3-gcc7',
+    'pytorch-linux-xenial-cuda10.1-cudnn7-py3',
     # PyTorch ASAN
     'pytorch-linux-xenial-py3-clang5-asan',
     # PyTorch DEBUG
