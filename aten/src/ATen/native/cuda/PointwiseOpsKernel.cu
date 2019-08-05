3d5c2c50f5f83fce60881133048c05d8021efba4
Igor Fedan <ifedan@fb.com>
2019-08-05 14:19:55 -0400

    Fix

--- aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
+++ aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
@@ -12,7 +12,7 @@ void addcdiv_cuda_kernel(TensorIterator& iter, Scalar value) {
   AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "addcdiv_cuda", [&]() {
     auto alpha = value.to<scalar_t>();
     gpu_kernel(iter, [alpha]GPU_LAMBDA(scalar_t a, scalar_t b, scalar_t c) -> scalar_t {
-      return a + alpha * (b / c)
+      return a + alpha * (b / c);
     });
   });
 }
