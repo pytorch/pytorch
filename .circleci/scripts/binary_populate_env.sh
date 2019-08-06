0cf6bc7474c05bd2b3a0a60578bcbc12266fc5f9
Edward Z. Yang <ezyang@fb.com>
2019-08-06 12:53:43 -0400

    Attempt a 1.2.0 final build.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- .circleci/scripts/binary_populate_env.sh
+++ .circleci/scripts/binary_populate_env.sh
@@ -53,9 +53,9 @@ fi
 # We put this here so that OVERRIDE_PACKAGE_VERSION below can read from it
 export DATE="$(date -u +%Y%m%d)"
 if [[ "$(uname)" == 'Darwin' ]] || [[ "$DESIRED_CUDA" == "cu100" ]]; then
-  export PYTORCH_BUILD_VERSION="1.2.0.dev$DATE"
+  export PYTORCH_BUILD_VERSION="1.2.0"
 else
-  export PYTORCH_BUILD_VERSION="1.2.0.dev$DATE+$DESIRED_CUDA"
+  export PYTORCH_BUILD_VERSION="1.2.0+$DESIRED_CUDA"
 fi
 export PYTORCH_BUILD_NUMBER=1
 
@@ -79,7 +79,7 @@ export OVERRIDE_PACKAGE_VERSION="$PYTORCH_BUILD_VERSION"
 
 # TODO: We don't need this anymore IIUC
 export TORCH_PACKAGE_NAME='torch'
-export TORCH_CONDA_BUILD_FOLDER='pytorch-nightly'
+export TORCH_CONDA_BUILD_FOLDER='pytorch-1.2.0'
 
 export USE_FBGEMM=1
 export PIP_UPLOAD_FOLDER="$PIP_UPLOAD_FOLDER"
