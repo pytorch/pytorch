f0ceea46bcb507cb2dd86b9efb4adf1f0d8219c7
Will Feng <willfeng@fb.com>
2019-08-07 11:41:57 -0400

    improve libtorch zip installation in test

--- .circleci/scripts/binary_linux_test.sh
+++ .circleci/scripts/binary_linux_test.sh
@@ -11,7 +11,7 @@ if [[ "$PACKAGE_TYPE" == conda ]]; then
   source activate testenv >/dev/null
 elif [[ "$DESIRED_PYTHON" == 2.7mu ]]; then
   export PATH="/opt/python/cp27-cp27mu/bin:\$PATH"
-else
+elif [[ "$PACKAGE_TYPE" != libtorch ]]; then
   python_nodot="\$(echo $DESIRED_PYTHON | tr -d m.u)"
   export PATH="/opt/python/cp\$python_nodot-cp\${python_nodot}m/bin:\$PATH"
 fi
@@ -22,25 +22,31 @@ fi
 # TODO there is duplicated and inconsistent test-python-env setup across this
 #   file, builder/smoke_test.sh, and builder/run_tests.sh, and also in the
 #   conda build scripts themselves. These should really be consolidated
-pkg="/final_pkgs/\$(ls /final_pkgs)"
-if [[ "$PACKAGE_TYPE" == conda ]]; then
-  conda install -y "\$pkg" --offline
-  if [[ "$DESIRED_CUDA" == 'cpu' ]]; then
-    conda install -y cpuonly -c pytorch
-  fi
-  retry conda install -yq future numpy protobuf six
-  if [[ "$DESIRED_CUDA" != 'cpu' ]]; then
-    # DESIRED_CUDA is in format cu90 or cu100
-    if [[ "${#DESIRED_CUDA}" == 4 ]]; then
-      cu_ver="${DESIRED_CUDA:2:1}.${DESIRED_CUDA:3}"
-    else
-      cu_ver="${DESIRED_CUDA:2:2}.${DESIRED_CUDA:4}"
-    fi
-    retry conda install -yq -c pytorch "cudatoolkit=\${cu_ver}"
-  fi
+if [[ "$PACKAGE_TYPE" == libtorch ]]; then
+  pkg="\$(ls /final_pkgs/*-latest.zip)"
+  unzip "\$pkg" -d /tmp
+  cd /tmp/libtorch
 else
-  pip install "\$pkg"
-  retry pip install -q future numpy protobuf six
+  pkg="/final_pkgs/\$(ls /final_pkgs)"
+  if [[ "$PACKAGE_TYPE" == conda ]]; then
+    conda install -y "\$pkg" --offline
+    if [[ "$DESIRED_CUDA" == 'cpu' ]]; then
+      conda install -y cpuonly -c pytorch
+    fi
+    retry conda install -yq future numpy protobuf six
+    if [[ "$DESIRED_CUDA" != 'cpu' ]]; then
+      # DESIRED_CUDA is in format cu90 or cu100
+      if [[ "${#DESIRED_CUDA}" == 4 ]]; then
+        cu_ver="${DESIRED_CUDA:2:1}.${DESIRED_CUDA:3}"
+      else
+        cu_ver="${DESIRED_CUDA:2:2}.${DESIRED_CUDA:4}"
+      fi
+      retry conda install -yq -c pytorch "cudatoolkit=\${cu_ver}"
+    fi
+  else
+    pip install "\$pkg"
+    retry pip install -q future numpy protobuf six
+  fi
 fi
 
 # Test the package
