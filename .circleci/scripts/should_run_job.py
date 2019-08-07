586715cc6083d140573f0708184d2a1450ecdabb
Will Feng <willfeng@fb.com>
2019-08-07 18:20:50 -0400

    don't run unrelated jobs

--- .circleci/scripts/should_run_job.py
+++ .circleci/scripts/should_run_job.py
@@ -9,49 +9,6 @@ import sys
 # the set of jobs to run via the Workflows filters in CircleCI config.
 
 default_set = [
-    # PyTorch CPU
-    # Selected oldest Python 2 version to ensure Python 2 coverage
-    'pytorch-linux-trusty-py2.7.9',
-    # PyTorch CUDA
-    'pytorch-linux-xenial-cuda9-cudnn7-py3',
-    # PyTorch ASAN
-    'pytorch-linux-xenial-py3-clang5-asan',
-    # PyTorch DEBUG
-    'pytorch-linux-trusty-py3.6-gcc5.4',
-
-    # Caffe2 CPU
-    'caffe2-py2-mkl-ubuntu16.04',
-    # Caffe2 CUDA
-    'caffe2-py2-cuda9.1-cudnn7-ubuntu16.04',
-    # Caffe2 ONNX
-    'caffe2-onnx-py2-gcc5-ubuntu16.04',
-    'caffe2-onnx-py3.6-clang7-ubuntu16.04',
-    # Caffe2 Clang
-    'caffe2-py2-clang7-ubuntu16.04',
-    # Caffe2 CMake
-    'caffe2-cmake-cuda9.0-cudnn7-ubuntu16.04',
-
-    # Binaries
-    'manywheel 2.7mu cpu devtoolset7',
-    'libtorch 2.7m cpu devtoolset7',
-    'libtorch 2.7m cpu gcc5.4_cxx11-abi',
-
-    # Caffe2 Android
-    'caffe2-py2-android-ubuntu16.04',
-    # Caffe2 OSX
-    'caffe2-py2-system-macos10.13',
-    # PyTorch OSX
-    'pytorch-macos-10.13-cuda9.2-cudnn7-py3',
-    # PyTorch Android
-    'pytorch-linux-xenial-py3-clang5-android-ndk-r19c',
-
-    # XLA
-    'pytorch-xla-linux-trusty-py3.6-gcc5.4',
-
-    # Other checks
-    'pytorch-short-perf-test-gpu',
-    'pytorch-python-doc-push',
-    'pytorch-cpp-doc-push',
 ]
 
 # Takes in commit message to analyze via stdin
