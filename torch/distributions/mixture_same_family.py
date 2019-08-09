5709e5ea87455927cab09448be3759719376d34d
Nicki Skafte <nugginea@gmail.com>
2019-08-09 11:15:39 +0200

    fixed syntax error for python2.7

--- torch/distributions/mixture_same_family.py
+++ torch/distributions/mixture_same_family.py
@@ -157,7 +157,7 @@ class MixtureSameFamily(Distribution):
     def _pad(self, x):
         d = len(x.shape) - self._event_ndims
         s = x.shape
-        x = x.reshape(*(s[:d]), 1, *(s[d:]))
+        x = x.reshape(s[:d] + (1,) + s[d:])
         return x
 
     def _pad_mixture_dimensions(self, x):
@@ -166,8 +166,8 @@ class MixtureSameFamily(Distribution):
         pad_ndims = 0 if cat_batch_ndims == 1 else \
             dist_batch_ndims - cat_batch_ndims
         xs = x.shape
-        x = x.reshape(*xs[:-1], *(pad_ndims * [1]),
-                      *xs[-1:], *(self._event_ndims * [1]))
+        x = x.reshape(xs[:-1] + torch.Size(pad_ndims * [1]) +
+                      xs[-1:] + torch.Size(self._event_ndims * [1]))
         return x
 
     def __repr__(self):
