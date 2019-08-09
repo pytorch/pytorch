e6fe1e95d396b0416e328139e8b5803ffef0f4c6
Hong Xu <hong@topbug.net>
2019-08-09 08:13:30 -0700

    Refactor randperm test (#23526)
    Summary:
    CPU and CUDA testing code are largely the same.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23526
    
    Reviewed By: ezyang
    
    Differential Revision: D16586271
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: 91c70c05789120fde4718ce955de243087a8c993

--- test/test_torch.py
+++ test/test_torch.py
@@ -4656,33 +4656,58 @@ class _TestTorchMixin(object):
         torch.zeros(5, 6).copy_(torch.zeros(6))
         self.assertRaises(RuntimeError, lambda: torch.zeros(5, 6).copy_(torch.zeros(30)))
 
-    def test_randperm(self):
-        # Test core functionality. Ensure both integer and floating-point numbers are tested.
-        for dtype in (torch.long, torch.half):
-            _RNGState = torch.get_rng_state()
-            res1 = torch.randperm(100)
-            res2 = torch.empty(0, dtype=dtype)
-            torch.set_rng_state(_RNGState)
-            torch.randperm(100, out=res2)
-            self.assertEqual(res1, res2)
+    @staticmethod
+    def _test_randperm(self, device):
+        if device == 'cpu':
+            rng_device = None
+        else:
+            rng_device = [0]
+
+        # Test core functionality. On CUDA, for small n, randperm is offloaded to CPU instead. For large n, randperm is
+        # executed on GPU.
+        for n in (100, 50000, 100000):
+            # Ensure both integer and floating-point numbers are tested. Half follows an execution path that is
+            # different from others on CUDA.
+            for dtype in (torch.long, torch.half, torch.float):
+                if n > 2049 and dtype == torch.half:  # Large n for torch.half will raise an exception, do not test here.
+                    continue
+                with torch.random.fork_rng(devices=rng_device):
+                    res1 = torch.randperm(n, dtype=dtype, device=device)
+                res2 = torch.empty(0, dtype=dtype, device=device)
+                torch.randperm(n, out=res2, dtype=dtype, device=device)
+                self.assertEqual(res1, res2, 0)
 
         # Default type is long
-        self.assertIsInstance(torch.randperm(100), torch.LongTensor)
+        for n in (100, 10000):
+            self.assertEqual(torch.randperm(n, device=device).dtype, torch.long)
 
         # randperm of 0 elements is an empty tensor
         res1 = torch.randperm(0)
-        res2 = torch.LongTensor(5)
+        res2 = torch.tensor(5, dtype=dtype, device=device)
         torch.randperm(0, out=res2)
         self.assertEqual(res1.numel(), 0)
         self.assertEqual(res2.numel(), 0)
 
         # Test exceptions when n is too large for a floating point type
-        for res, small_n, large_n in ((torch.HalfTensor(), 2**11 + 1, 2**11 + 2),
-                                      (torch.FloatTensor(), 2**24 + 1, 2**24 + 2),
-                                      (torch.DoubleTensor(), 2**25,  # 2**53 + 1 is too large to run
-                                       2**53 + 2)):
+        for dtype, small_n, large_n in ((torch.half, 2**11 + 1, 2**11 + 2),
+                                        (torch.float, 2**24 + 1, 2**24 + 2),
+                                        (torch.double, 2**25,  # 2**53 + 1 is too large to run
+                                         2**53 + 2)):
+            res = torch.empty(0, dtype=dtype, device=device)
             torch.randperm(small_n, out=res)  # No exception expected
-            self.assertRaises(RuntimeError, lambda: torch.randperm(large_n, out=res))
+            self.assertRaises(RuntimeError, lambda: torch.randperm(large_n, out=res, device=device))
+
+        # Test non-contiguous tensors
+        for n in (4, 5, 6, 10, 20):
+            non_contiguous_tensor = torch.zeros((2, 3), dtype=torch.long, device=device).t()
+            self.assertFalse(non_contiguous_tensor.is_contiguous())
+            with torch.random.fork_rng(devices=rng_device):
+                res = torch.randperm(n, dtype=torch.long, device=device)
+            torch.randperm(n, out=non_contiguous_tensor)
+            self.assertEqual(non_contiguous_tensor, res)
+
+    def test_randperm(self):
+        self._test_randperm(self, 'cpu')
 
     def test_random(self):
         # This test is flaky with p<=(2/(ub-lb))^200=6e-36
