838caaf204cfc92a929838be93a5a99bd90dff79
Negin Raoof <neginmr@utexas.edu>
2019-08-08 23:03:07 -0700

    exclude test_clip since the op is updated in opset 11

--- caffe2/python/onnx/tests/onnx_backend_test.py
+++ caffe2/python/onnx/tests/onnx_backend_test.py
@@ -72,6 +72,7 @@ backend_test.exclude(r'(test_hardsigmoid'  # Does not support Hardsigmoid.
                      '|test_bitshift.*'  # Needs implementation
                      '|test_round.*'  # Needs implementation
                      '|test_cumsum.*'  # Needs implementation
+                     '|test_clip.*'  # opset 11 is not supported yet
                      ')')
 
 # Quick patch to unbreak master CI, is working on the debugging.
