14ffb6679e0ae4d7d051122a1ba602c07e0310a3
neginraoof <neginmr@utexas.edu>
2019-08-09 11:57:06 -0700

    fixed unused state/issue for LegacyTracedModule

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -319,7 +319,8 @@ class LegacyTracedModule(Module):
         in_vars, in_desc = _flatten(args)
         # NOTE: use full state, because we need it for BatchNorm export
         # This differs from the compiler path, which doesn't support it at the moment.
-        module_state = list(_unique_state_dict(self, keep_vars=True).values())
+        state_dict = _unique_state_dict(self, keep_vars=True)
+        module_state = list(_remove_unused_state_batchnorm(self, state_dict).values())
         try:
             trace, all_trace_inputs = torch._C._tracer_enter(*(in_vars + module_state))
         except Exception as e:
