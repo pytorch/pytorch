782e2d0df4144f2351b908a7b5fa2b8061a4f1f1
Zhang Zhi <zhangzhi2018@email.szu.edu.cn>
2019-08-11 17:18:23 +0800

    Add test case

--- test/test_namedtensor.py
+++ test/test_namedtensor.py
@@ -82,6 +82,12 @@ class TestNamedTensor(TestCase):
         self.assertTrue(partially_named.has_names())
         self.assertTrue(fully_named.has_names())
 
+    def test_contains(self):
+        self.assertTrue(0 in torch.LongTensor([0, 1, 2]))
+        with self.assertRaises(NotImplementedError):
+            'foo' in torch.LongTensor([0, 1, 2])
+            False in torch.LongTensor([0, 1, 2])
+        
     def test_repr(self):
         named_tensor = torch.zeros(2, 3).set_names_(['N', 'C'])
         expected = "tensor([[0., 0., 0.],\n        [0., 0., 0.]], names=('N', 'C'))"
