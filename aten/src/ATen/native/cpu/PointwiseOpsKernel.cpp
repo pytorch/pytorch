68214f7026c019a461687cc7340c075f3a21fe99
Will Feng <willfeng@fb.com>
2019-08-11 14:39:47 -0400

    Revert "Port addcdiv operator from the TH code to Aten"
    This reverts commit 1d3d92e7705e3b15081b63bbc71f6ab09bdced77.

--- aten/src/ATen/native/cpu/PointwiseOpsKernel.cpp
+++ aten/src/ATen/native/cpu/PointwiseOpsKernel.cpp
@@ -28,28 +28,9 @@ static void addcmul_cpu_kernel(TensorIterator& iter, Scalar value) {
   });
 }
 
-static void addcdiv_cpu_kernel(TensorIterator& iter, Scalar value) {
-  ScalarType dtype = iter.dtype(0);
-  AT_DISPATCH_ALL_TYPES(dtype, "addcdiv_cpu_out", [&] {
-    scalar_t scalar_val = value.to<scalar_t>();
-    auto scalar_vec = Vec256<scalar_t>(scalar_val);
-    cpu_kernel_vec(
-        iter,
-        [=](scalar_t self_val, scalar_t t1_val, scalar_t t2_val) -> scalar_t {
-          return self_val + scalar_val * t1_val / t2_val;
-        },
-        [=](Vec256<scalar_t> self_vec,
-            Vec256<scalar_t> t1_vec,
-            Vec256<scalar_t> t2_vec) {
-          return self_vec + scalar_vec * t1_vec / t2_vec;
-        });
-  });
-}
-
 } // anonymous namespace
 
 REGISTER_DISPATCH(addcmul_stub, &addcmul_cpu_kernel);
-REGISTER_DISPATCH(addcdiv_stub, &addcdiv_cpu_kernel);
 
 } // namespace native
 } // namespace at
