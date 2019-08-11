69fe09e6316e476b2a114acefc0d2004b0a447ca
Will Feng <willfeng@fb.com>
2019-08-11 14:39:50 -0400

    Revert "Revert D16627924: [pytorch][PR] Port addcdiv operator from the TH code to Aten"
    This reverts commit 21ea0a115cd14f8bb5eb35943be188c7b9428782.

--- aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
+++ aten/src/ATen/native/cuda/PointwiseOpsKernel.cu
@@ -8,6 +8,15 @@
 
 namespace at { namespace native {
 
+void addcdiv_cuda_kernel(TensorIterator& iter, Scalar value) {
+  AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "addcdiv_cuda", [&]() {
+    auto alpha = value.to<scalar_t>();
+    gpu_kernel(iter, [alpha]GPU_LAMBDA(scalar_t a, scalar_t b, scalar_t c) -> scalar_t {
+      return a + alpha * (b / c);
+    });
+  });
+}
+
 void addcmul_cuda_kernel(TensorIterator& iter, Scalar value) {
   AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "addcmul_cuda", [&]() {
     auto alpha = value.to<scalar_t>();
@@ -17,6 +26,7 @@ void addcmul_cuda_kernel(TensorIterator& iter, Scalar value) {
   });
 }
 
+REGISTER_DISPATCH(addcdiv_stub, &addcdiv_cuda_kernel);
 REGISTER_DISPATCH(addcmul_stub, &addcmul_cuda_kernel);
 
 }} // namespace at::native
