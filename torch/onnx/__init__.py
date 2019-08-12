857c5246b4442d850b63b734e8f679fa72fed70f
Junjie Bai <bai@in.tum.de>
2019-08-12 11:05:06 -0700

    One more place to set default behavior to be the same as before

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -31,7 +31,7 @@ def export(model, args, f, export_params=True, verbose=False, training=False,
            input_names=None, output_names=None, aten=False, export_raw_ir=False,
            operator_export_type=None, opset_version=None, _retain_param_name=True,
            do_constant_folding=False, example_outputs=None, strip_doc_string=True, 
-           dynamic_axes=None, keep_initializers_as_inputs=None):
+           dynamic_axes=None, keep_initializers_as_inputs=True):
     r"""
     Export a model into ONNX format.  This exporter runs your model
     once in order to get a trace of its execution to be exported;
