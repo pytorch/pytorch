9f93cc5337d5bf4478a97a11abaddad2433237f6
Max Kalinin <makalini@microsoft.com>
2019-08-12 16:42:08 -0700

    Fix lint warning

--- test/onnx/test_utility_funs.py
+++ test/onnx/test_utility_funs.py
@@ -37,7 +37,7 @@ class TestUtilityFuns(TestCase):
             warnings.simplefilter("always")
             utils._validate_dynamic_axes({'input1': {}, 'output': {},
                                          'invalid_name1': {}, 'invalid_name2': {}},
-                                          None, ['input1', 'input2'], ['output'])
+                                         None, ['input1', 'input2'], ['output'])
             messages = [str(warning.message) for warning in w]
         assert "Provided key invalid_name1 for dynamic axes is not a valid input/output name" in messages
         assert "Provided key invalid_name2 for dynamic axes is not a valid input/output name" in messages
