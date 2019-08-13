5b3bb7dc1f01721abecc9d28bd89e26b76dba06d
Will Feng <willfeng@fb.com>
2019-08-13 18:07:55 -0400

    update

--- test/cpp/api/random.cpp
+++ test/cpp/api/random.cpp
@@ -23,12 +23,12 @@ TEST_F(RandomTest, GetAndSetRNGState_CUDA) {
   auto state = torch::cuda::get_rng_state();
   auto stateCloned = state.clone();
 
-  auto before = torch::rand({1000}, torch::Device(torch::kCUDA)); // auto before = torch::rand(1000, torch::TensorOptions(torch::Device(torch::kCUDA)));
+  auto before = torch::rand({1000}, torch::Device(torch::kCUDA));
 
   ASSERT_TRUE(torch::equal(state, stateCloned));
   ASSERT_FALSE(torch::equal(state, torch::cuda::get_rng_state()));
 
   torch::cuda::set_rng_state(state);
-  auto after = torch::rand({1000}, torch::Device(torch::kCUDA)); // auto after = torch::rand(1000, torch::TensorOptions(torch::Device(torch::kCUDA)));
+  auto after = torch::rand({1000}, torch::Device(torch::kCUDA));
   ASSERT_TRUE(torch::equal(before, after));
 }
