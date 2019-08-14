3e1ae81365175a67fe2625f2b2bac0dec13ab029
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-08-14 10:23:21 -0700

    Add _load_from_state_dict func

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -726,6 +726,18 @@ class MultiheadAttention(Module):
         if self.bias_v is not None:
             xavier_normal_(self.bias_v)
 
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        if prefix + '_qkv_same_embed_dim' is not in state_dict.keys():
+            # In the early version of torch.nn.MultiheadAttention, q_proj_weight, k_proj_weight,
+            # and v_proj_weight are saved as in_proj_weight.
+            # If so, there is no attribute _qkv_same_embed_dim.
+            # To resolve the BC breaking, we just need to add the attribute _qkv_same_embed_dim.
+            state_dict[prefix + '_qkv_same_embed_dim'] = False
+        super(MultiheadAttention, self)._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict,
+            missing_keys, unexpected_keys, error_msgs)
+
     def forward(self, query, key, value, key_padding_mask=None,
                 need_weights=True, attn_mask=None):
         r"""
@@ -756,7 +768,7 @@ class MultiheadAttention(Module):
         - attn_output_weights: :math:`(N, L, S)` where N is the batch size,
           L is the target sequence length, S is the source sequence length.
         """
-        if hasattr(self, '_qkv_same_embed_dim') and self._qkv_same_embed_dim is False:
+        if not self._qkv_same_embed_dim:
             return F.multi_head_attention_forward(
                 query, key, value, self.embed_dim, self.num_heads,
                 self.in_proj_weight, self.in_proj_bias,
@@ -768,11 +780,6 @@ class MultiheadAttention(Module):
                 q_proj_weight=self.q_proj_weight, k_proj_weight=self.k_proj_weight,
                 v_proj_weight=self.v_proj_weight)
         else:
-            if not hasattr(self, '_qkv_same_embed_dim'):
-                warnings.warn('A new version of MultiheadAttention module has been implemented. \
-                    Please re-train your model with the new module',
-                              UserWarning)
-
             return F.multi_head_attention_forward(
                 query, key, value, self.embed_dim, self.num_heads,
                 self.in_proj_weight, self.in_proj_bias,
