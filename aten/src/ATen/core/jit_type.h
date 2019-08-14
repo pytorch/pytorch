ffa7bf60eef25ab1f3971cb2cd8829410c05416f
Michael Suo <suo@fb.com>
2019-08-13 18:35:49 -0700

    Update on "[jit] simplify NamedType interface"
    We had a lot of redundant methods. Killing them.

--- aten/src/ATen/core/jit_type.h
+++ aten/src/ATen/core/jit_type.h
@@ -567,7 +567,6 @@ struct CAFFE2_API ProfiledTensorType : public TensorType {
   c10::optional<size_t> dim() const {
     return sizes().size();
   }
-  
 
   const VaryingShape& sizes() const {
     return sizes_;
@@ -648,7 +647,6 @@ struct CAFFE2_API ProfiledTensorType : public TensorType {
     copy->strides_ = VaryingShape(sizes().size());
     return copy;
   }
-  
 
   ProfiledTensorTypePtr merge(ProfiledTensorTypePtr other) {
     auto scalar_type = merge_primitive(scalarType(), other->scalarType());
@@ -1557,7 +1555,10 @@ struct CAFFE2_API ClassType : public NamedType {
   DEFINE_IS_SUBCLASS(ClassType);
   bool operator==(const Type& rhs) const override {
     if (auto user_rhs = rhs.cast<ClassType>()) {
-      return name()->qualifiedName() == user_rhs->name()->qualifiedName();
+      const auto& lhs_name = name().value();
+      const auto& rhs_name = user_rhs->name().value();
+
+      return lhs_name == rhs_name;
     }
     return false;
   }
