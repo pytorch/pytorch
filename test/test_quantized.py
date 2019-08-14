6b3e28506bc705e07b7f575872cddfed80518ffb
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-08-14 16:18:59 -0700

    Update on "Enables `inplace` in the quantized relu"
    This is a duplicate to bring back #23704 with diff revision D16634539
    
    Differential Revision: [D16818664](https://our.internmc.facebook.com/intern/diff/D16818664)

--- test/test_quantized.py
+++ test/test_quantized.py
@@ -106,14 +106,14 @@ class TestQuantizedOps(TestCase):
             qY_hat = op(qX)
             self.assertEqual(qY, qY_hat, message="{} relu failed".format(name))
 
-        ops_under_test_inline = {
-            'inline native': torch.relu_,
-            'inline nn.functional': torch.nn.functional.relu_,
+        ops_under_test_inplace = {
+            'inplace native': torch.relu_,
+            'inplace nn.functional': torch.nn.functional.relu_,
         }
 
-        for name, op in ops_under_test_inline.items():
+        for name, op in ops_under_test_inplace.items():
             qY_hat = qX.clone()
-            op(qY_hat)
+            op_(qY_hat)
             self.assertEqual(qY, qY_hat, message="{} relu failed".format(name))
 
     """Tests the correctness of the add and add_relu op."""
