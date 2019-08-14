a9e60f779b4beaeec64c49fa3b1e8ac6e3964696
Michael Suo <suo@fb.com>
2019-08-14 11:21:42 -0700

    make FunctionType a NamedType (#24280)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24280
    
    This simplifies the groundwork for serializing functions.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D16800560
    
    Pulled By: suo
    
    fbshipit-source-id: 129b32dddb39494daeade33c87d76248486a86b2

--- torch/csrc/jit/script/class_type.cpp
+++ torch/csrc/jit/script/class_type.cpp
@@ -6,6 +6,10 @@ namespace c10 {
 
 // This file exists because we need to reference module.h, which we can't from
 // c10. Sigh...
+FunctionType::FunctionType(Function* function)
+    : NamedType(TypeKind::FunctionType),
+      function_(function),
+      name_(function->qualname()) {}
 
 Function* ClassType::getMethod(const std::string& name) const {
   for (auto method : methods_) {
