712d1359de19e380a4ccb96529cc952d8abf411a
Will Feng <willfeng@fb.com>
2019-08-15 12:05:51 -0400

    avoid ambiguity

--- torch/csrc/api/include/torch/random.h
+++ torch/csrc/api/include/torch/random.h
@@ -19,13 +19,13 @@ void TORCH_API set_rng_state(const Tensor & new_state);
 namespace cuda {
 
 /// Returns the random number generator state of the specified GPU as a ByteTensor.
-Tensor TORCH_API get_rng_state(torch::DeviceIndex device_index = -1);
+Tensor TORCH_API get_rng_state(torch::DeviceIndex device_index);
 
 /// Returns the random number generator state of the specified GPU as a ByteTensor.
 Tensor TORCH_API get_rng_state(torch::Device device = torch::Device(torch::kCUDA));
 
 /// Sets the random number generator state of the specified GPU.
-void TORCH_API set_rng_state(const Tensor & new_state, torch::DeviceIndex device_index = -1);
+void TORCH_API set_rng_state(const Tensor & new_state, torch::DeviceIndex device_index);
 
 /// Sets the random number generator state of the specified GPU.
 void TORCH_API set_rng_state(const Tensor & new_state, torch::Device device = torch::Device(torch::kCUDA));
