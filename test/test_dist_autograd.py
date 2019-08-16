c9c2d74d0deb75b73d0e2e337d0d21764d878ab8
Pritam Damania <pritam.damania@fb.com>
2019-08-16 14:51:15 -0700

    Basic framework for Distributed Autograd context. (#24020)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24020
    
    As per https://github.com/pytorch/pytorch/issues/23110, each autograd pass
    would be assigned a unique autograd_context_id. In this change we introduce a
    DistAutogradContainer per worker which holds information for each autograd pass
    currently running.
    
    DistAutogradContainer has a map from the autograd_context_id to
    DistAutogradContext (which holds all the relevant information for the autograd
    pass). DistAutogradContext currently only stores the autograd_context_id and
    more information would be added to it later as we build out the rest of the
    framework.
    
    The autograd_context_id is a 64 bit globally unique integer where the first 16
    bits are the worker_id and next 48 bits are auto-incrementing for uniqueness.
    
    Sample python code on how this would be used for distributed autograd:
    
    ```
    import torch.distributed.autograd as dist_autograd
    worker_id = 0
    dist_autograd.init(worker_id)
    with dist_autograd.context() as context_id:
         # forward pass...
         # backward pass...
         # optimizer step...
    ```
    
    Differential Revision: D16356694
    
    fbshipit-source-id: 0c2e93b22d892e3220a1197cb544e4d8f8c0990a

---
+++ test/test_dist_autograd.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+import torch.distributed.autograd as dist_autograd
+import unittest
+
+@unittest.skipIf(sys.version_info < (3, 0), "Pytorch distributed autograd package "
+                 "does not support python2")
+class TestDistAutograd(unittest.TestCase):
+
+    def setUp(self):
+        self.worker_id = 16
+        # Using private init method here since 'init_model_parallel' currently
+        # requires ProcessGroupGloo which in turns requires at least two
+        # processes. We'd like to avoid multiprocessing in this unit test to
+        # keep it simple and hence we use the private _init method.
+        dist_autograd._init(self.worker_id)
+
+    def test_autograd_context(self):
+        context_ids = []
+        for i in range(1000):
+            with dist_autograd.context() as context_id:
+                self.assertEqual(context_id, dist_autograd._retrieve_context(context_id)._context_id())
+                # First 16 bits should be worker_id.
+                self.assertEqual(self.worker_id, context_id >> 48)
+                context_ids.append(context_id)
+
+        for context_id in context_ids:
+            with self.assertRaises(RuntimeError):
+                dist_autograd._retrieve_context(context_id)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ torch/distributed/autograd/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+class context(object):
+    '''
+        Autograd context object to wrap forward and backward passes when using
+        distributed autograd. The context_id generated in the 'with' is required
+        to uniquely identify a distributed autograd pass on all workers. Each
+        worker stores metadata associated with this context_id, which is required
+        to correctly execute a distributed autograd pass.
+
+        Example::
+            >> import torch.distributed.autograd as dist_autograd
+            >> with dist_autograd.context() as context_id:
+            >>      forward pass...
+            >>      backward pass...
+            >>      optimizer step...
+    '''
+    def __enter__(self):
+        self.autograd_context = _new_context()
+        return self.autograd_context._context_id()
+
+    def __exit__(self, type, value, traceback):
+        _release_context(self.autograd_context._context_id())
