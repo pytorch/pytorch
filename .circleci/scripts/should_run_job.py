51f253873c026e95fb9313530e077eb9d8ade66d
James Reed <jamesreed@fb.com>
2019-08-16 13:26:52 -0700

    Update on "[CI] Enable trusty with GCC 7 on diffs"
    Differential Revision: [D16862005](https://our.internmc.facebook.com/intern/diff/D16862005)
    
    This will give us diff signal for CPU-only builds with FBGEMM enabled, ensuring quantization gets timely signal

--- .circleci/scripts/should_run_job.py
+++ .circleci/scripts/should_run_job.py
@@ -17,7 +17,7 @@ default_set = [
     # PyTorch ASAN
     'pytorch-linux-xenial-py3-clang5-asan',
     # PyTorch DEBUG
-    'pytorch-linux-trusty-py3.6-gcc7',
+    'pytorch_linux_xenial_py3_gcc7',
 
     # Caffe2 CPU
     'caffe2-py2-mkl-ubuntu16.04',
