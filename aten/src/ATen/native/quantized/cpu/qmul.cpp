11c4056d1d89915468bd75e5a80b5f1bff100f61
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-08-19 16:22:01 -0700

    Rewrite of the scalar mul

--- aten/src/ATen/native/quantized/cpu/qmul.cpp
+++ aten/src/ATen/native/quantized/cpu/qmul.cpp
@@ -51,22 +51,41 @@ Tensor _mul_out(Tensor& out, const Tensor& self, const Tensor& other) {
 
 template <bool ReLUFused = false>
 Tensor _mul_scalar_out(Tensor& out, const Tensor& self, Scalar other) {
-  int64_t zero_point = out.q_zero_point();
-  double scale = out.q_scale();
-  int64_t self_zero_point = self.q_zero_point();
-  double self_scale = self.q_scale();
+  // int64_t zero_point = out.q_zero_point();
+  // double scale = out.q_scale();
+  // int64_t self_zero_point = self.q_zero_point();
+  // double self_scale = self.q_scale();
 
-  auto iter = TensorIterator::unary_op(out, self);
-  AT_DISPATCH_QINT_TYPES(out.scalar_type(), "qmul", [&]() {
-    cpu_kernel(iter, [&](scalar_t a) -> scalar_t {
-      const auto da = at::dequantize_val(self_scale, self_zero_point, a);
-      float c = da * other.toFloat();
-      if (ReLUFused) {
-        c = std::max<float>(c, 0.0);
+  // auto iter = TensorIterator::unary_op(out, self);
+  // AT_DISPATCH_QINT_TYPES(out.scalar_type(), "qmul", [&]() {
+  //   cpu_kernel(iter, [&](scalar_t a) -> scalar_t {
+  //     const auto da = at::dequantize_val(self_scale, self_zero_point, a);
+  //     float c = da * other.toFloat();
+  //     if (ReLUFused) {
+  //       c = std::max<float>(c, 0.0);
+  //     }
+  //     return at::quantize_val<scalar_t>(scale, zero_point, c);
+  //   });
+  // });
+  // return out;
+  auto other_float = other.toFloat();
+  switch (out.qscheme()) {
+    case kPerTensorAffine:
+      double new_scale = self.q_scale() * other_float;
+      out.set_quantizer(make_per_tensor_affine_quantizer(
+        new_scale, self.q_zero_point(), self.axis(), self.scalar_type()));
+      break;
+    case kPerChannelAffine:
+      auto new_scales = self.q_scales();
+      for (int idx = 0; idx < new_scales.size(); ++idx) {
+        new_scales[idx] *= other.toFloat() * other_float;
       }
-      return at::quantize_val<scalar_t>(scale, zero_point, c);
-    });
-  });
+      out.set_quantizer(make_per_channel_affine_quantizer(
+        new_scales(), self.q_zero_points(), self.scalar_type()));
+      break;
+    default:
+      TORCH_CHECK(false, "Only affine is supported!");
+  }
   return out;
 }
 
