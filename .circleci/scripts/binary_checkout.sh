fce42f2ef3715f0307739f7145f9146ed9efbf12
Will Feng <willfeng@fb.com>
2019-08-19 14:31:53 -0400

    use pytorch/builder libtorch_more_binary_tests branch

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -39,7 +39,7 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+git clone -q https://github.com/yf225/builder.git -b libtorch_more_binary_tests "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
