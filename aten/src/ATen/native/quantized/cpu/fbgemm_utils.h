fabd90974390a29207af879b481ca9d29884e6f6
dskhudia <dskhudia@fb.com>
2019-08-20 14:54:38 -0700

    [quant] per channel quantization support
    Per channel quantization support in qconv2d + tests
    
    Differential Revision: [D16909378](https://our.internmc.facebook.com/intern/diff/D16909378/)

--- aten/src/ATen/native/quantized/cpu/fbgemm_utils.h
+++ aten/src/ATen/native/quantized/cpu/fbgemm_utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <c10/core/QScheme.h>
+
 #ifdef USE_FBGEMM
 #include "fbgemm/Fbgemm.h"
 #include "fbgemm/QuantUtils.h"
@@ -24,8 +26,9 @@ struct FBGEMM_API PackedConvWeight {
   std::unique_ptr<fbgemm::PackWeightsForConv<2>> w;
   std::vector<int32_t> col_offsets;
   std::vector<int64_t> kernel;
-  double w_scale;
-  int64_t w_zp;
+  std::vector<float> w_scale;
+  std::vector<int32_t> w_zp;
+  c10::QScheme q_scheme;
 };
 
 // PackWeight: Convert the weight from uint8 to int8.
