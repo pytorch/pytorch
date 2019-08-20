d3f8b76ce0fd1d56486b68b2533d6145ea886c89
Barak Nehoran <bnehoran@users.noreply.github.com>
2019-08-20 16:10:20 -0400

    Check for proper dtype and sizes in affine_grid

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -2715,6 +2715,25 @@ def affine_grid(theta, size, align_corners=None):
                       "align_corners=True if the old behavior is desired. "
                       "See the documentation of grid_sample for details.")
         align_corners = False
+
+    # enforce floating point dtype on theta
+    if not theta.is_floating_point():
+        raise ValueError("Expected theta to have floating point type, but got {}"
+                         .format(theta.dtype))
+    # check that shapes and sizes match
+    if len(size) == 4:
+        if theta.dim() != 3 or theta.shape[-2] != 2 or theta.shape[-1] != 3:
+            raise ValueError("Expected a batch of 2D affine matrices of shape Nx2x3 "
+                             "for size {}. Got {}.".format(size, theta.shape))
+    elif len(size) == 5:
+        if theta.dim() != 3 or theta.shape[-2] != 3 or theta.shape[-1] != 4:
+            raise ValueError("Expected a batch of 3D affine matrices of shape Nx3x4 "
+                             "for size {}. Got {}.".format(size, theta.shape))
+    else:
+        raise NotImplementedError("affine_grid only supports 4D and 5D sizes, "
+                                  "for 2D and 3D affine transforms, respectively. "
+                                  "Got size {}.".format(size))
+
     return torch.affine_grid_generator(theta, size, align_corners)
 
 
