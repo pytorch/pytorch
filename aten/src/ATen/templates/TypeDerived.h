bd6cf5099bd352494807a2711fcab91d94927bf8
Lucian Grijincu <lucian@fb.com>
2019-08-20 16:24:00 -0700

    Revert D16048264: Add static dispatch mode to reduce mobile code size
    Differential Revision:
    D16048264
    
    Original commit changeset: ad1e50951273
    
    fbshipit-source-id: 69d302929e183e2da26b64dcc24c69c3b7de186b

--- aten/src/ATen/templates/TypeDerived.h
+++ aten/src/ATen/templates/TypeDerived.h
@@ -2,35 +2,14 @@
 
 // ${generated_comment}
 
-#include <c10/core/TensorOptions.h>
-#include <c10/core/Scalar.h>
-#include <c10/core/QScheme.h>
-#include <c10/core/MemoryFormat.h>
-#include <c10/util/ArrayRef.h>
-#include <c10/util/intrusive_ptr.h>
-#include <torch/csrc/WindowsTorchApiMacro.h>
+#include <ATen/Context.h>
+#include <ATen/Utils.h>
 
 $extra_cuda_headers
 
-namespace c10 {
-struct Storage;
-}
-
 namespace at {
 
-class Tensor;
-using TensorList = ArrayRef<Tensor>;
-
-class Context;
-struct Generator;
-
-struct Quantizer;
-// This is temporary typedef to enable Quantizer in aten native function API
-// we'll remove them when we are actually exposing Quantizer class
-// to frontend
-using ConstQuantizerPtr = const c10::intrusive_ptr<Quantizer>&;
-
-struct TORCH_API ${Type} final {
+struct ${Type} final {
   ${type_derived_method_declarations}
 };
 
