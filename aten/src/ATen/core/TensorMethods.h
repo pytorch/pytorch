c61fafb94b4574bd4fc9ef64219343cfea1e6671
Richard Zou <zou3519@gmail.com>
2019-08-20 17:09:23 -0700

    Implement name inference for t(), transpose(...)
    Test Plan
    - [namedtensor ci]

--- aten/src/ATen/core/TensorMethods.h
+++ aten/src/ATen/core/TensorMethods.h
@@ -2099,6 +2099,16 @@ inline Tensor Tensor::transpose(int64_t dim0, int64_t dim1) const {
     return table->getOp<Tensor (const Tensor &, int64_t, int64_t)>(tensorTypeIdToBackend(type_id()), is_variable())(const_cast<Tensor&>(*this), dim0, dim1);
 #endif
 }
+#ifdef BUILD_NAMEDTENSOR
+inline Tensor Tensor::transpose(Dimname dim0, Dimname dim1) const {
+#ifdef USE_STATIC_DISPATCH
+    return TypeDefault::transpose(const_cast<Tensor&>(*this), dim0, dim1);
+#else
+    static auto table = globalATenDispatch().getOpTable("aten::transpose(Tensor(a) self, Dimname dim0, Dimname dim1) -> Tensor(a)");
+    return table->getOp<Tensor (const Tensor &, Dimname, Dimname)>(tensorTypeIdToBackend(type_id()), is_variable())(const_cast<Tensor&>(*this), dim0, dim1);
+#endif
+}
+#endif
 inline Tensor & Tensor::transpose_(int64_t dim0, int64_t dim1) const {
 #ifdef USE_STATIC_DISPATCH
     return TypeDefault::transpose_(const_cast<Tensor&>(*this), dim0, dim1);
--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -627,6 +627,27 @@ static inline Tensor & sparse_transpose_(Tensor & self, int64_t dim0, int64_t di
   return self;
 }
 
+#ifdef BUILD_NAMEDTENSOR
+static Tensor& propagate_transposed_names(
+    Tensor& result,
+    const Tensor& other,
+    int64_t dim0,
+    int64_t dim1) {
+  if (other.names()) {
+    auto names = other.names()->vec();
+    std::swap(names[dim0], names[dim1]);
+    namedinference::propagate_names(result, names);
+  }
+  return result;
+}
+
+Tensor transpose(const Tensor& self, Dimname dim0, Dimname dim1) {
+  return at::transpose(
+      self, dimname_to_position(self, dim0), dimname_to_position(self, dim1));
+}
+
+#endif
+
 Tensor & transpose_(Tensor & self, int64_t dim0, int64_t dim1) {
   auto ndims = self.dim();
   dim0 = maybe_wrap_dim(dim0, ndims);
@@ -671,7 +692,11 @@ Tensor transpose(const Tensor & self, int64_t dim0, int64_t dim1) {
   auto sizes = self.sizes().vec();
   std::swap(strides[dim0], strides[dim1]);
   std::swap(sizes[dim0], sizes[dim1]);
-  return self.as_strided(sizes, strides);
+  auto result = self.as_strided(sizes, strides);
+#ifdef BUILD_NAMEDTENSOR
+  propagate_transposed_names(result, self, dim0, dim1);
+#endif
+  return result;
 }
 
 static void check_t(const Tensor& self, const char *fn) {
