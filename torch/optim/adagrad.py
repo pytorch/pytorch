75df9931903bbc9d2a79281a217f48453c6fbbd1
royboy <royboy@fb.com>
2019-08-21 12:29:01 -0700

    Add epsilon argument to Adagrad optimizer

--- torch/optim/adagrad.py
+++ torch/optim/adagrad.py
@@ -12,6 +12,8 @@ class Adagrad(Optimizer):
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         lr (float, optional): learning rate (default: 1e-2)
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-10)
         lr_decay (float, optional): learning rate decay (default: 0)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
 
@@ -19,17 +21,19 @@ class Adagrad(Optimizer):
         Optimization: http://jmlr.org/papers/v12/duchi11a.html
     """
 
-    def __init__(self, params, lr=1e-2, lr_decay=0, weight_decay=0, initial_accumulator_value=0):
+    def __init__(self, params, lr=1e-2, lr_decay=0, eps=1e-10, weight_decay=0, initial_accumulator_value=0):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= lr_decay:
             raise ValueError("Invalid lr_decay value: {}".format(lr_decay))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
         if not 0.0 <= weight_decay:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
         if not 0.0 <= initial_accumulator_value:
             raise ValueError("Invalid initial_accumulator_value value: {}".format(initial_accumulator_value))
 
-        defaults = dict(lr=lr, lr_decay=lr_decay, weight_decay=weight_decay,
+        defaults = dict(lr=lr, lr_decay=lr_decay, eps=eps, weight_decay=weight_decay,
                         initial_accumulator_value=initial_accumulator_value)
         super(Adagrad, self).__init__(params, defaults)
 
@@ -86,11 +90,11 @@ class Adagrad(Optimizer):
                         return constructor(grad_indices, values, size)
                     state['sum'].add_(make_sparse(grad_values.pow(2)))
                     std = state['sum'].sparse_mask(grad)
-                    std_values = std._values().sqrt_().add_(1e-10)
+                    std_values = std._values().sqrt_().add_(group['eps'])
                     p.data.add_(-clr, make_sparse(grad_values / std_values))
                 else:
                     state['sum'].addcmul_(1, grad, grad)
-                    std = state['sum'].sqrt().add_(1e-10)
+                    std = state['sum'].sqrt().add_(group['eps'])
                     p.data.addcdiv_(-clr, grad, std)
 
         return loss
