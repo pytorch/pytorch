91e402886eccd0e324d7cda35a989eb6483f848c
Igor Fedan <ifedan@fb.com>
2019-08-21 21:08:55 -0400

    Debug info

--- aten/src/ATen/native/cuda/DistanceKernel.cu
+++ aten/src/ATen/native/cuda/DistanceKernel.cu
@@ -132,12 +132,12 @@ template <typename scalar_t, typename F>
 __global__ static void cdist_backward_kernel_cuda_impl(scalar_t * buffer, const scalar_t * grad, const scalar_t * x1, const scalar_t * x2, const scalar_t * dist, int64_t gs,
                                                        const scalar_t p, const int64_t r1, const int64_t r2, const int64_t m, const int64_t count, const int64_t r_size, const int64_t l1_size, const int64_t l2_size) {
   const int y = blockIdx.y * blockDim.y + threadIdx.y;
-  if (y >= count) {
+  const int init = blockIdx.x * blockDim.x + threadIdx.x;
+  if (y >= count || init >= m) {
     return;
   }
   const int l = y / r_size;
   const int k = y % r_size;
-  const int init = blockIdx.x * blockDim.x + threadIdx.x;
   const int stride = blockDim.x * gridDim.x;
   const int l_size = r_size * m;
 
@@ -326,17 +326,17 @@ void cdist_backward_kernel_impl(Tensor& result, const Tensor& grad, const Tensor
   const int block_x = 64;
   const int block_y = 16;
   const int grid_x = (m + block_x * 8 - 1) / (block_x * 8);
-  const int grid_y = ((r1 * r2 * d) + block_y - 1) / block_y;
+  const int grid_y = (dist.numel() + block_y - 1) / block_y; //CHANGED
 
   const dim3 grid(grid_x, grid_y);
   const dim3 block(block_x, block_y);
 
-  const int64_t count = r1 * r2 * d;
+  const int64_t count = dist.numel(); //CHANGED
   const int64_t r_size = r1 * r2;
   const int64_t l1_size = r1 * m;
   const int64_t l2_size = r2 * m;
 
-  Tensor buffer = (x1.dim() > 2) ? at::empty({d, r2, r1, m}, result.options()) : at::empty({r2, r1, m}, result.options());
+  Tensor buffer = (x1.dim() > 2) ? at::empty({d, r2, r1, m}, result.options()) : at::empty({r2, r1, m}, result.options());  //CHANGED
   AT_DISPATCH_FLOATING_TYPES(result.scalar_type(), "cdist_cuda_backward", [&] {
     if (p == 1.0) {
       cdist_backward_kernel_cuda_impl<scalar_t, dists<scalar_t>::one><<<grid, block>>>(buffer.data<scalar_t>(), grad.data<scalar_t>(), x1.data<scalar_t>(), x2.data<scalar_t>(), dist.data<scalar_t>(), grad.stride(-1), p, r1, r2, m, count, r_size, l1_size, l2_size);
