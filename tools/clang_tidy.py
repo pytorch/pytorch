18ece196414fbbdd2a30e9ca64ef0c7bbe3e2f5f
Michael Suo <suo@fb.com>
2019-08-22 20:56:45 -0700

    [wip] show clang-tidy as github annotations

--- tools/clang_tidy.py
+++ tools/clang_tidy.py
@@ -24,6 +24,8 @@ import shlex
 import subprocess
 import sys
 import tempfile
+import itertools
+from xml.sax.saxutils import escape
 
 try:
     from shlex import quote
@@ -31,6 +33,105 @@ except ImportError:
     from pipes import quote
 
 Patterns = collections.namedtuple("Patterns", "positive, negative")
+ErrorDescription = collections.namedtuple(
+    'ErrorDescription', 'file line column error error_identifier description')
+
+
+# Adapted from https://github.com/PSPDFKit-labs/clang-tidy-to-junit. License elsewhere.
+class ClangTidyConverter:
+    # All the errors encountered.
+    errors = []
+
+    # Parses the error.
+    # Group 1: file path
+    # Group 2: line
+    # Group 3: column
+    # Group 4: error message
+    # Group 5: error identifier
+    error_regex = re.compile(
+        r"^([\w\/\.\-\ ]+):(\d+):(\d+): (.+) (\[[\w\-,\.]+\])$")
+
+    # This identifies the main error line (it has a [the-warning-type] at the end)
+    # We only create a new error when we encounter one of those.
+    main_error_identifier = re.compile(r'\[[\w\-,\.]+\]$')
+
+    def __init__(self, basename, input_str):
+        self.basename = basename
+        input_lines = input_str.split('\n')
+        # Collect all lines related to one error.
+        current_error = []
+        for line in input_lines:
+            # If the line starts with a `/`, it is a line about a file.
+            if line[0] == '/':
+                # Look if it is the start of a error
+                if self.main_error_identifier.search(line, re.M):
+                    # If so, process any `current_error` we might have
+                    self.process_error(current_error)
+                    # Initialize `current_error` with the first line of the error.
+                    current_error = [line]
+                else:
+                    # Otherwise, append the line to the error.
+                    current_error.append(line)
+            elif len(current_error) > 0:
+                # If the line didn't start with a `/` and we have a `current_error`, we simply append
+                # the line as additional information.
+                current_error.append(line)
+            else:
+                pass
+
+        # If we still have any current_error after we read all the lines,
+        # process it.
+        if len(current_error) > 0:
+            self.process_error(current_error)
+
+    def print_junit_file(self, output_file):
+        # Write the header.
+        output_file.write("""
+<?xml version="1.0" encoding="UTF-8" ?>
+<testsuites id="1"
+ name="Clang-Tidy"
+ tests="{error_count}"
+ errors="{error_count}"
+ failures="0"
+ time="0">""".format(error_count=len(self.errors)))
+
+        sorted_errors = sorted(self.errors, key=lambda x: x.file)
+
+        # Iterate through the errors, grouped by file.
+        for file, errorIterator in itertools.groupby(sorted_errors, key=lambda x: x.file):
+            errors = list(errorIterator)
+            error_count = len(errors)
+
+            # Each file gets a test-suite
+            output_file.write("""\n    <testsuite errors="{error_count}" name="{file}" tests="{error_count}" failures="0" time="0">\n"""
+                              .format(error_count=error_count, file=file))
+            for error in errors:
+                # Write each error as a test case.
+                output_file.write("""
+        <testcase id="{id}" name="{id}" time="0">
+            <failure message="{message}">
+{htmldata}
+            </failure>
+        </testcase>""".format(id="[{}/{}] {}".format(error.line, error.column, error.error_identifier), message=escape(error.error),
+                              htmldata=escape(error.description)))
+            output_file.write("\n    </testsuite>\n")
+        output_file.write("</testsuites>\n")
+
+    def process_error(self, error_array):
+        if len(error_array) == 0:
+            return
+
+        result = self.error_regex.match(error_array[0])
+        if result is None:
+            logging.warning(
+                'Could not match error_array to regex: %s', error_array)
+            return
+
+        # We remove the `basename` from the `file_path` to make prettier filenames in the JUnit file.
+        file_path = result.group(1).replace(self.basename, "")
+        error = ErrorDescription(file_path, int(result.group(2)), int(
+            result.group(3)), result.group(4), result.group(5), "\n".join(error_array[1:]))
+        self.errors.append(error)
 
 
 # NOTE: Clang-tidy cannot lint headers directly, because headers are not
@@ -259,6 +360,10 @@ def parse_options():
         help="Run clang tidy in parallel per-file (requires ninja to be installed).",
     )
     parser.add_argument(
+        "--junit",
+        help="Output results in junit format to file",
+    )
+    parser.add_argument(
         "extra_args", nargs="*", help="Extra arguments to forward to clang-tidy"
     )
     return parser.parse_args()
@@ -290,7 +395,15 @@ def main():
     if options.diff:
         line_filters = [get_changed_lines(options.diff, f) for f in files]
 
-    print(run_clang_tidy(options, line_filters, files), file=sys.stderr)
+    raw_output = run_clang_tidy(options, line_filters, files)
+    output = ClangTidyConverter("", raw_output)
+    if len(output.errors) > 0:
+        if options.junit:
+            output_file = open(options.junit, "w")
+            output.print_junit_file(output_file)
+
+        print(raw_output)
+        exit(1)
 
 
 if __name__ == "__main__":
