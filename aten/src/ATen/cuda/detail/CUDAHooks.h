6bb87dd0dfb7ad48a4ed6ca87a145b7dae45412b
Mike Ruberry <mruberry@devfair044.maas>
2019-08-23 14:59:27 -0700

    dummy commit to refresh ci

--- aten/src/ATen/cuda/detail/CUDAHooks.h
+++ aten/src/ATen/cuda/detail/CUDAHooks.h
@@ -1,6 +1,7 @@
 #include <ATen/detail/CUDAHooksInterface.h>
 
 #include <ATen/Generator.h>
+#include <c10/util/Optional.h>
 
 // TODO: No need to have this whole header, we can just put it all in
 // the cpp file
