a98136cc21a5bd945aed6ed172c02065254e621b
Martin Yuan <martin.yuan@oculus.com>
2019-08-24 07:39:09 -0700

    Pull instruction definitions out of interpreter.cpp.

---
+++ torch/csrc/jit/instruction.cpp
@@ -0,0 +1,42 @@
+#include <torch/csrc/jit/instruction.h>
+#include <iostream>
+
+namespace torch {
+namespace jit {
+std::ostream& operator<<(std::ostream& out, OperatorCode op) {
+  switch (op) {
+#define OP_STRING(x, _) \
+  case x:               \
+    return out << #x;
+    FORALL_OPCODES(OP_STRING)
+#undef OP_STRING
+  }
+  return out;
+}
+
+const char* OpInfo(OperatorCode op) {
+  switch (op) {
+#define OP_INFO(x, info) \
+  case x:                \
+    return info;
+    FORALL_OPCODES(OP_INFO)
+#undef OP_INFO
+  }
+  return nullptr;
+}
+
+static_assert(sizeof(Instruction) == 8, "Instructions should be 8 bytes");
+std::ostream& operator<<(std::ostream& out, Instruction inst) {
+  // TODO: use op info to print out the op in a more user-friendly way
+  int nargs = strlen(OpInfo(inst.op));
+  out << inst.op;
+  if (nargs > 0) {
+    out << " " << inst.X;
+  }
+  if (nargs > 1) {
+    out << " " << inst.N;
+  }
+  return out;
+}
+}
+}
--- torch/csrc/jit/interpreter.cpp
+++ torch/csrc/jit/interpreter.cpp
@@ -12,6 +12,7 @@
 #include <torch/csrc/jit/graph_executor.h>
 #include <torch/csrc/jit/ir.h>
 #include <torch/csrc/jit/operator.h>
+#include <torch/csrc/jit/instruction.h>
 #include <torch/csrc/jit/passes/bailout_graph.h>
 #include <torch/csrc/jit/script/compilation_unit.h>
 #include <torch/csrc/jit/script/jit_exception.h>
@@ -195,6 +196,8 @@ void insertLastUses(Graph& g) {
 }
 } // namespace
 
+std::ostream& operator<<(std::ostream& out, Instruction inst);
+
 /*
 This is an optimization that reduces the number of store/load/move nodes needed
 by recognizing that parts of the graph are simple trees like a*x + b*y. When
@@ -293,86 +296,6 @@ struct PreprocessGraph {
   std::unordered_map<Node*, bool> can_emit_inline;
 };
 
-// instructs look like:
-// op_code X, N
-// meaning of X, N depend on the op:
-// O - index into operator table
-// R - index into register table
-// I - literal integer
-// C - index into constant table
-// P - jump offset relative to beginning of current instruction
-// F - index into function table
-// T - index into the type table, used for guard instructions
-
-#define FORALL_OPCODES(_)                                                   \
-  _(OP, "O") /* invoke operator X */                                        \
-  _(LOAD, "R") /* push a value from a register X */                         \
-  _(MOVE, "R") /* push a value from register X, clearing the register */    \
-  _(STOREN, "RI") /* store N values to registers [X, X+N) */                \
-  _(STORE, "R") /* store 1 value to registers X */                          \
-  _(DROP, "") /* drop 1 value from the top of the stack */                  \
-  _(DROPR, "R") /* clear register X */                                      \
-  _(LOADC, "C") /* push the constant X */                                   \
-  _(JF, "P") /* pop the top of the stack, if false, branch to P */          \
-  _(JMP, "P") /* unconditional branch to X */                               \
-  _(LOOP, "PI") /* perform a loop, X is where to branch if cond is false */ \
-  _(RET, "") /* exit execution */                                           \
-  _(WAIT, "") /* wait for a future to be complete */                        \
-  _(CALL, "F") /* call function X */                                        \
-  _(GUARD, "T") /* check guard against type_table, true if passes */        \
-  _(TAIL_CALL, "F") /* replace current frame with function F */
-
-enum OpCode : uint8_t {
-#define DEFINE_OP(op, _) op,
-  FORALL_OPCODES(DEFINE_OP)
-#undef DEFINE_OP
-};
-
-std::ostream& operator<<(std::ostream& out, OpCode op) {
-  switch (op) {
-#define OP_STRING(x, _) \
-  case x:               \
-    return out << #x;
-    FORALL_OPCODES(OP_STRING)
-#undef OP_STRING
-  }
-  return out;
-}
-
-const char* OpInfo(OpCode op) {
-  switch (op) {
-#define OP_INFO(x, info) \
-  case x:                \
-    return info;
-    FORALL_OPCODES(OP_INFO)
-#undef OP_INFO
-  }
-  return nullptr;
-}
-
-struct Instruction {
-  OpCode op;
-  uint8_t padding; // currently unused
-  uint16_t N;
-  int32_t X;
-  // TODO: check for overflow
-  Instruction(OpCode op, int32_t X, uint16_t N)
-      : op(op), padding(0), N(N), X(X) {}
-};
-
-static_assert(sizeof(Instruction) == 8, "Instructions should be 8 bytes");
-std::ostream& operator<<(std::ostream& out, Instruction inst) {
-  // TODO: use op info to print out the op in a more user-friendly way
-  int nargs = strlen(OpInfo(inst.op));
-  out << inst.op;
-  if (nargs > 0) {
-    out << " " << inst.X;
-  }
-  if (nargs > 1) {
-    out << " " << inst.N;
-  }
-  return out;
-}
 
 // for keeping track of the current node
 struct WithCurrentNode {
@@ -453,7 +376,7 @@ struct CodeImpl {
     insertBailoutBlocks();
   }
 
-  void insertInstruction(OpCode op, int64_t X = 0, uint64_t N = 0) {
+  void insertInstruction(OperatorCode op, int64_t X = 0, uint64_t N = 0) {
     instructions_.emplace_back(op, X, N);
     instructions_source_.emplace_back(current_node_);
 
@@ -516,7 +439,7 @@ struct CodeImpl {
       int reg = registerFor(input);
       bool moved = input->uses().size() == ++use_count_[input];
 
-      OpCode op;
+      OperatorCode op;
       if (input->node()->kind() == prim::Constant) {
         op = LOADC;
       } else if (drop) {
