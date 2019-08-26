5afe13e4ff406929875be1386ae3cb63ae718f17
Richard Zou <zou3519@gmail.com>
2019-08-26 15:01:15 -0700

    Implement name inference for addmv, addmv_, mv (#24471)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24471
    
    mv(Tensor[M, N], Tensor[O]) ignores the names of N and O and returns a
    tensor with names [M].
    
    Test Plan: - new tests [namedtensor ci]
    
    Differential Revision: D16915805
    
    Pulled By: zou3519
    
    fbshipit-source-id: d7d47903f249f85ef3be8a188d51993834bf5f55

--- aten/src/ATen/NamedTensorUtils.h
+++ aten/src/ATen/NamedTensorUtils.h
@@ -61,6 +61,12 @@ void propagate_names_for_addmm(
     /*const*/TensorImpl* m2,
     /*const*/TensorImpl* bias);
 
+void propagate_names_for_addmv(
+    TensorImpl* result,
+    TensorImpl* mat,
+    TensorImpl* vec,
+    TensorImpl* bias);
+
 void propagate_names_for_expand(Tensor& result, const Tensor& self);
 
 } // namespace namedinference
