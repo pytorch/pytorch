a1bf4d7ee1a4839be358f49c51647747f7c601f6
Jerry Zhang <jerryzh@fb.com>
2019-08-26 15:34:45 -0700

    Integration tests for initial quantization graph mode (#24428)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24428
    
    att
    
    Test Plan:
    python test/test_quantizer.py
    
    Imported from OSS
    
    Differential Revision: D17001136
    
    fbshipit-source-id: b0c6cd433efdcbc6b54b429a29677fc509221937

--- test/test_quantizer.py
+++ test/test_quantizer.py
@@ -4,9 +4,12 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import torch.jit
+from jit_utils import _inline_everything
+from torch._jit_internal import Optional
 import torch.nn as nn
-import torch.nn.functional as F
-from common_utils import TestCase
+from common_utils import TestCase, run_tests
+from torch.quantization import QuantStub, DeQuantStub, \
+    quantize, default_eval_fn, QConfig
 # TODO : Quantizer tests to be integrated with CI once quantizer intf hardened
 
 r"""
@@ -148,77 +151,87 @@ class QuantTemplate:
 
 
 class QuantizerTestCase(TestCase):
+    @_inline_everything
     def test_compare_qparam_eager_script_default(self):
-        # Simple test case with conv->relu->maxpool
+        class Observer(torch.nn.Module):
+            __annotations__ = {'scale' : Optional[torch.Tensor], 'zero_point': Optional[torch.Tensor]}
+
+            def __init__(self):
+                super(Observer, self).__init__()
+                self.dtype = torch.quint8
+                self.qscheme = torch.per_tensor_affine
+                self.scale, self.zero_point = None, None
+
+            def forward(self, x):
+                self.scale = torch.tensor([2.0])
+                self.zero_point = torch.tensor([3])
+                return x
+
+            @torch.jit.export
+            def calculate_qparams(self):
+                return self.scale, self.zero_point
+
+        class WeightObserver(Observer):
+            def __init__(self):
+                super(WeightObserver, self).__init__()
+                self.dtype = torch.qint8
+
+        class TestM(nn.Module):
+            def __init__(self, qconfig):
+                super(TestM, self).__init__()
+                self.conv = nn.Conv2d(3, 1, 3).float()
+                self.conv.weight.data.fill_(1.0)
+                self.conv.bias.data.fill_(0.01)
+                self.qconfig = qconfig
+                self.quant = QuantStub()
+                self.dequant = DeQuantStub()
+
+            def forward(self, x):
+                return self.dequant(self.conv(self.quant(x)))
+
         class TestScriptM(torch.jit.ScriptModule):
-            def __init__(self, init_weight=None):
+            def __init__(self):
                 super(TestScriptM, self).__init__()
-                self.conv1 = nn.Conv2d(1, 20, 5, 1)
-                self.conv1.weight.data.fill_(1.0)
-                self.conv1.bias.data.fill_(0.01)
+                self.conv = nn.Conv2d(3, 1, 3).float()
+                self.conv.bias.data.fill_(0.01)
 
             @torch.jit.script_method
             def forward(self, x):
-                y = F.relu(self.conv1(x))
-                z = F.max_pool2d(y, 2, 2)
-                return z
-
-        class TestM(nn.Module):
-            def __init__(self, quantObj=None):
-                super(TestM, self).__init__()
-                self.conv1 = nn.Conv2d(1, 20, 5, 1)
-                self.conv1.weight.data.fill_(1.0)
-                self.conv1.bias.data.fill_(0.01)
-                self.quantObj = quantObj
-
-            def forward(self, x):
-                y = F.relu(self.conv1(x))
-                if self.quantObj is not None:
-                    self.quantObj.observer(y, "y")
-                z = F.max_pool2d(y, 2, 2)
-                if self.quantObj is not None:
-                    self.quantObj.observer(z, "z")
-                return z
+                y = self.conv(x)
+                return y
 
         # Test Data
-        data = torch.ones(1, 1, 28, 28)
+        data = [(torch.randn(10, 3, 10, 10, dtype=torch.float), 1)]
 
         # Eager mode
-
-        # Create QuantConfig object for eager mode
-        eagerQuantObj = QuantTemplate(qscheme='per_tensor_quant',
-                                      observerImpl=activationObserver,
-                                      calcQParamImpl=calcQParamFunc)
-        eagerM = TestM(quantObj=eagerQuantObj)
-
-        # Run EagerMode Model and Collect stats
-        eagerM.forward(data)
-        eagerM.quantObj.calcQParam()
-
+        fake_qconfig = QConfig(activation=Observer, weight=WeightObserver)
+        eager_module = TestM(fake_qconfig)
         # Script mode
-        scriptM = TestScriptM()
+        script_module = TestScriptM()
+        script_module.conv.weight = torch.nn.Parameter(eager_module.conv.weight.detach())
+        quantized_eager_module = quantize(eager_module, default_eval_fn, data)
 
-        # Create QuantConfig object for script mode
-        activationQuantObj = QuantTemplate(qscheme='per_tensor_quant',
-                                           observerImpl=activationObserver,
-                                           calcQParamImpl=calcQParamFunc)
+        def get_forward(m):
+            return m._c._get_method('forward')
+        # TODO: test jit.script as well
+        torch._C._jit_pass_constant_propagation(get_forward(script_module).graph)
 
-        # This performs type analysis to identify tensors from other
-        # types. This info needed for further quantizer passes
-        torch._C._jit_pass_constant_propagation(scriptM.graph)
-
-        # Insert observers
-        torch._C._jit_pass_insert_observers(scriptM._c, "forward", activationQuantObj.observer)
-
+        ScriptedObserver = torch.jit.script(Observer())
+        ScriptedWeightObserver = torch.jit.script(WeightObserver())
+        torch._C._jit_pass_prepare_quant(script_module._c,
+                                         "forward",
+                                         ScriptedObserver._c, ScriptedWeightObserver._c)
         # Run ScriptM Model and Collect statistics
-        scriptM.forward(data)
-        activationQuantObj.calcQParam()
+        get_forward(script_module)(data[0][0])
 
-        # Compare results for eager and graph mode
-        eagerDict = eagerQuantObj.getQParamDict()
-        activationDict = activationQuantObj.getQParamDict()
+        # Insert quantize and dequantize calls
+        script_module._c = torch._C._jit_pass_insert_quant_dequant(script_module._c, "forward")
+        # Note that observer modules are not removed right now
+        torch._C._jit_pass_quant_fusion(script_module._c._get_method('forward').graph)
+        get_forward(script_module)(data[0][0])
+        eager_result = quantized_eager_module(data[0][0])
+        script_result = get_forward(script_module)(data[0][0])
+        self.assertEqual(eager_result, script_result)
 
-        # TODO - fix @eellison
-        self.assertTrue('z' in eagerDict and 'z.1' in activationDict)
-        self.assertAlmostEqual(eagerDict["z"][0], activationDict["z.1"][0], places=15)
-        self.assertAlmostEqual(eagerDict["z"][1], activationDict["z.1"][1], places=15)
+if __name__ == '__main__':
+    run_tests()
