44bd63c7a146422ac0dd2d94d79750cc0984ecf0
Elias Ellison <eellison@fb.com>
2019-08-28 15:32:36 -0700

    don't throw in constant prop (#25270)
    Summary:
    Don't throw in constant propagation, since the op we're running may not be reached. Previously we would only only catch `C10::Error`; however it's hard to maintain that the entire codebase doesn't throw any other types of errors, and some errors map nicely to python errors, like `std::index_error` to IndexError.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25270
    
    Differential Revision: D17102545
    
    Pulled By: eellison
    
    fbshipit-source-id: 9fd485821743ad882e5c6fc912ca47b0b001b0e9

--- test/test_jit.py
+++ test/test_jit.py
@@ -2205,6 +2205,18 @@ graph(%Ra, %Rb):
         # testing that 1 // 0 error is not thrownn
         self.run_pass('constant_propagation', constant_prop.graph)
 
+    def test_constant_prop_exception(self):
+        # checking y = a[4] does not error in constant propagation
+        def bad_index(x):
+            # type: (bool)
+            y = 0
+            if x:
+                a = [1, 2, 3]
+                y = a[4]
+            return y
+
+        self.checkScript(bad_index, (False,))
+
     def test_short_circuit_optimization(self):
         @torch.jit.script
         def const_expressions(x):
@@ -17107,14 +17119,14 @@ class TestList(JitTestCase):
             a = [1, 2, 3]
             return a[4]
 
-        self.checkScriptRaisesRegex(bad_index, (), IndexError,
+        self.checkScriptRaisesRegex(bad_index, (), Exception,
                                     "list index out of range")
 
         def bad_negative_index():
             a = [1, 2, 3]
             return a[-5]
 
-        self.checkScriptRaisesRegex(bad_negative_index, (), IndexError,
+        self.checkScriptRaisesRegex(bad_negative_index, (), Exception,
                                     "list index out of range")
 
     def test_list_len(self):
