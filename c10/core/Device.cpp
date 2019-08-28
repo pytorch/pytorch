d5b9b0d9fd7d160e12db15c57569730f07f0037e
Joseph Courtley <joseph@withtab.com>
2019-08-28 10:42:44 +0100

    random commit

--- c10/core/Device.cpp
+++ c10/core/Device.cpp
@@ -8,37 +8,27 @@
 #include <ostream>
 #include <string>
 #include <tuple>
-#include <vector>
-
-namespace c10 {
-namespace {
-DeviceType parse_type(const std::string& device_string) {
-  static const std::array<std::pair<std::string, DeviceType>, 9> types = {{
-      {"cpu", DeviceType::CPU},
-      {"cuda", DeviceType::CUDA},
-      {"mkldnn", DeviceType::MKLDNN},
+#include <vectorgex(
+//     "(cuda|cpu)|(cuda|cpu):([0-9]+)|([0-9]+)",
+//     std::regex_constants::basic);
+// std::smatch match;
+// const bool ok = std::regex_match(device_string, match, regex);
+// TORCH_CHECK(ok, "Invalid device string: '", device_string, "'");
+// if (match[1].matched) {
+//   type_ = parse_type_from_string(maype::MKLDNN},
       {"opengl", DeviceType::OPENGL},
       {"opencl", DeviceType::OPENCL},
       {"ideep", DeviceType::IDEEP},
       {"hip", DeviceType::HIP},
       {"msnpu", DeviceType::MSNPU},
-      {"xla", DeviceType::XLA},
-  }};
-  auto device = std::find_if(
-      types.begin(),
-      types.end(),
-      [device_string](const std::pair<std::string, DeviceType>& p) {
-        return p.first == device_string;
-      });
-  if (device != types.end()) {
-    return device->second;
-  }
-  AT_ERROR(
-      "Expected one of cpu, cuda, mkldnn, opengl, opencl, ideep, hip, msnpu device type at start of device string: ", device_string);
-}
-} // namespace
-
-// `std::regex` is still in a very incomplete state in GCC 4.8.x,
+      {"xla", DeviceTygex(
+//     "(cuda|cpu)|(cuda|cpu):([0-9]+)|([0-9]+)",
+//     std::regex_constants::basic);
+// std::smatch match;
+// const bool ok = std::regex_match(device_string, match, regex);
+// TORCH_CHECK(ok, "Invalid device string: '", device_string, "'");
+// if (match[1].matched) {
+//   type_ = parse_type_from_string(man a very incomplete state in GCC 4.8.x,
 // so we have to do our own parsing, like peasants.
 // https://stackoverflow.com/questions/12530406/is-gcc-4-8-or-earlier-buggy-about-regular-expressions
 //
@@ -68,15 +58,14 @@ Device::Device(const std::string& device_string) : Device(Type::CPU) {
     type_ = parse_type(device_string);
   } else {
     std::string s;
-    s = device_string.substr(0, index);
-    TORCH_CHECK(!s.empty(), "Device string must not be empty");
-    type_ = parse_type(s);
-
-    std::string device_index = device_string.substr(index + 1);
-    try {
-      index_ = c10::stoi(device_index);
-    } catch (const std::exception &) {
-      AT_ERROR("Could not parse device index '", device_index,
+    s = device_stgex(
+//     "(cuda|cpu)|(cuda|cpu):([0-9]+)|([0-9]+)",
+//     std::regex_constants::basic);
+// std::smatch match;
+// const bool ok = std::regex_match(device_string, match, regex);
+// TORCH_CHECK(ok, "Invalid device string: '", device_string, "'");
+// if (match[1].matched) {
+//   type_ = parse_type_from_string(mat parse device index '", device_index,
                "' in device string '", device_string, "'");
     }
     TORCH_CHECK(index_ >= 0,
