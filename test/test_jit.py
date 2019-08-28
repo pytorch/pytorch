a7cd40548d6f354ee10b83d0013e4f047bebd002
Gregory Chanan <gchanan@fb.com>
2019-08-28 13:03:15 -0700

    [JIT] Attempt to enable CrossMapLRN2d, as it no longer uses Module._backend.

--- test/test_jit.py
+++ test/test_jit.py
@@ -15443,9 +15443,6 @@ EXCLUDE_SCRIPT_MODULES = {
     'test_nn_AdaptiveAvgPool3d_tuple_none',
     'test_nn_AdaptiveMaxPool2d_tuple_none',
     'test_nn_AdaptiveMaxPool3d_tuple_none',
-
-    # Uses Module._backend, so this is not supported
-    'test_nn_CrossMapLRN2d',
 }
 
 
