2621d6d4938358132750b79be562d2da0f689842
Haixin Liu <haixin@fb.com>
2019-08-28 13:11:17 -0700

    Refactor MinMax observer (#23902)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23902
    
    Copied from Daya's diff in pytorch/pytorch #23191
    
    Refactor MinMax observer and create the base observer class to prepare for future observers such as histogram observer.
    ghstack-source-id: 89146014
    
    Test Plan:
    Added a test test_minmax_observer
    
    buck test mode/dev caffe2/test:quantization -- 'test_minmax_observer'
    
    ```
    Running 1 tests
    Started new test run: https://our.intern.facebook.com/intern/testinfra/testrun/2533274797931635
          ✓ caffe2/test:quantization - test_minmax_observer (test_quantization.ObserverTest) 0.055 1/1 (passed)
    Finished test run: https://our.intern.facebook.com/intern/testinfra/testrun/2533274797931635
    Summary (total time 4.26s):
      PASS: 1
      FAIL: 0
      SKIP: 0
      FATAL: 0
      TIMEOUT: 0
      OMIT: 0
    ```
    
    buck test mode/dev caffe2/test:quantization -- 'test_observer_scriptable'
    ```
    Running 1 tests
    Started new test run: https://our.intern.facebook.com/intern/testinfra/testrun/5348024563344195
          ✓ caffe2/test:quantization - test_observer_scriptable (test_quantization.ObserverTest) 1.762 1/1 (passed)
    Finished test run: https://our.intern.facebook.com/intern/testinfra/testrun/5348024563344195
    Summary (total time 6.02s):
      PASS: 1
      FAIL: 0
      SKIP: 0
      FATAL: 0
      TIMEOUT: 0
      OMIT: 0
    ```
    
    Differential Revision: D16663221
    
    fbshipit-source-id: 3d0e1aa9e4d27808e61b10604782606de067a34a

--- test/test_quantization.py
+++ test/test_quantization.py
@@ -11,7 +11,7 @@ from torch.quantization import \
     QConfig_dynamic, default_weight_observer, \
     quantize, prepare, convert, prepare_qat, quantize_qat, fuse_modules, \
     quantize_dynamic, default_qconfig, default_qat_qconfig, \
-    default_dynamic_qconfig, Observer, QuantWrapper
+    default_dynamic_qconfig, MinMaxObserver, QuantWrapper
 
 from common_utils import run_tests, tempfile
 from common_quantization import QuantizationTestCase, SingleLayerLinearModel, \
@@ -28,7 +28,6 @@ from common_quantization import AnnotatedTwoLayerLinearModel, AnnotatedNestedMod
 from hypothesis import given
 from hypothesis import strategies as st
 import io
-
 
 @unittest.skipIf(
     not torch.fbgemm_is_cpu_supported(),
@@ -629,11 +628,12 @@ class FusionTest(QuantizationTestCase):
         model = quantize(model, test_only_eval_fn, self.img_data)
         checkQuantized(model)
 
+
 class ObserverTest(QuantizationTestCase):
     @given(qdtype=st.sampled_from((torch.qint8, torch.quint8)),
            qscheme=st.sampled_from((torch.per_tensor_affine, torch.per_tensor_symmetric)))
-    def test_observer(self, qdtype, qscheme):
-        myobs = Observer(dtype=qdtype, qscheme=qscheme)
+    def test_minmax_observer(self, qdtype, qscheme):
+        myobs = MinMaxObserver(dtype=qdtype, qscheme=qscheme)
         x = torch.tensor([1.0, 2.0, 2.0, 3.0, 4.0, 5.0, 6.0])
         y = torch.tensor([4.0, 5.0, 5.0, 6.0, 7.0, 8.0])
         result = myobs(x)
@@ -666,7 +666,6 @@ class ObserverTest(QuantizationTestCase):
         buf.seek(0)
         loaded = torch.jit.load(buf)
         self.assertEqual(obs.calculate_qparams(), loaded.calculate_qparams())
-
 
 if __name__ == '__main__':
     run_tests()
