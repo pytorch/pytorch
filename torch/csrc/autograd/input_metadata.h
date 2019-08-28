3790fce64efe634c8aac06fce01cbfb644a6c725
Edward Yang <ezyang@fb.com>
2019-08-28 08:23:05 -0700

    Revert D17052534: [pytorch][PR] Creates Torch-friendly Event class and adds Stream tracking to autograd
    Test Plan: revert-hammer
    
    Differential Revision:
    D17052534
    
    Original commit changeset: d91b308ad0f7
    
    fbshipit-source-id: dacc7e70a835a8fa6ae71246999b4eff3383f3f3

--- torch/csrc/autograd/input_metadata.h
+++ torch/csrc/autograd/input_metadata.h
@@ -1,29 +1,19 @@
 #pragma once
 
 #include <ATen/ATen.h>
-#include "c10/core/Device.h"
-#include "c10/core/DeviceType.h"
-#include "c10/core/Stream.h"
-#include "c10/core/impl/DeviceGuardImplInterface.h"
 
 #include <cstdint>
 
 namespace torch { namespace autograd {
 
-/**
- * Records type, shape, and device of tensor and, where applicable,
- * the stream the correspondingoperation took place on.
- *
- * If is_valid() is false, then the corresponding input is not used and may be
- * an undefined tensor.
- */
+/// A tensor's type and shape. Each Function records the required type and
+/// shape of its inputs. If is_valid() is false, then the corresponding input
+/// is not used and may be an undefined tensor.
 struct InputMetadata {
   InputMetadata() = default;
 
   InputMetadata(const at::DeprecatedTypeProperties& type, at::IntArrayRef shape, at::Device device)
-  : type_{&type}, shape_{shape}, device_{device} {
-    stream_ = c10::impl::getDeviceGuardImpl(device_.type())->getStream(device_);
-  }
+  : type_{&type} , shape_{shape}, device_{device} { }
 
   InputMetadata(const at::Tensor& t)
   : InputMetadata(t.type(), t.sizes(), t.device()) { }
@@ -45,10 +35,6 @@ struct InputMetadata {
     return device_;
   }
 
-  c10::Stream stream() const {
-    return stream_;
-  }
-
   at::Tensor zeros_like() const {
     return at::zeros(shape_, type_->options(device_));
   }
@@ -57,7 +43,6 @@ private:
   const at::DeprecatedTypeProperties* type_ = nullptr;
   at::DimVector shape_;
   at::Device device_ = at::kCPU;
-  c10::Stream stream_ = c10::Stream(c10::Stream::Default::DEFAULT, device_);
 };
 
-}} // torch::autograd
+}}
