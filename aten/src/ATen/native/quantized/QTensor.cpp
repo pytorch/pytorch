44cc8dc9591431f6104a399bffd3ed32199e8716
Jerry Zhang <jerryzh168@gmail.com>
2019-08-29 15:06:47 -0700

    [fix] quantized_tensor tests
    Summary:
    Previously we are using empty to generate test tensors, this PR changes the test tensors to use
    randint so that we can test things properly
    Also added a set_sizes_and_strides and removed .contiguous() in int_repr function to preserve the
    original size and strides
    
    Test Plan:
    python test/test_quantized_tensor.py
    
    Reviewers:
    pt1quant
    Subscribers:
    
    Tasks:
    
    Tags:

--- aten/src/ATen/native/quantized/QTensor.cpp
+++ aten/src/ATen/native/quantized/QTensor.cpp
@@ -100,17 +100,17 @@ Quantizer* quantizer(const Tensor& self) {
 
 Tensor int_repr_quant(const Tensor& self) {
   Tensor dst;
-  // TODO: replace with TensorIterator
-  auto self_c = self.contiguous();
   AT_DISPATCH_QINT_TYPES(self.scalar_type(), "int_repr", [&]() {
     dst = at::empty(self.sizes(), self.options().dtype(UNDERLYING_TYPE));
     underlying_t* self_data =
-        reinterpret_cast<underlying_t*>(self_c.data_ptr<scalar_t>());
+        reinterpret_cast<underlying_t*>(self.data_ptr<scalar_t>());
     underlying_t* dst_data = dst.data_ptr<underlying_t>();
     if (self.numel() > 0) {
       memcpy(dst_data, self_data, self.nbytes());
     }
   });
+  self.unsafeGetTensorImpl()->set_sizes_and_strides(self.sizes(), self.strides());
+
   return dst;
 }
 
