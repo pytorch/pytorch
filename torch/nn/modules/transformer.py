138cf7c911110fc6dcd4caed9fa75ad9020f9090
Kyeongpil Kang <rudvlf0413@korea.ac.kr>
2019-09-01 23:03:59 +0900

    change the method type of generate_square_subsequent_mask
    Change the method type of generate_square_subsequent_mask to static method in order to be accessed by nn.Transformer.generate_square_subsequent_mask(sz)
    
    instead of
    model = nn.Transformer(args)
    model.generate_square_subsequent_mask(sz)

--- torch/nn/modules/transformer.py
+++ torch/nn/modules/transformer.py
@@ -118,7 +118,8 @@ class Transformer(Module):
                               memory_key_padding_mask=memory_key_padding_mask)
         return output
 
-    def generate_square_subsequent_mask(self, sz):
+    @staticmethod
+    def generate_square_subsequent_mask(sz):
         r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
             Unmasked positions are filled with float(0.0).
         """
