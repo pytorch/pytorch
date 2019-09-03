ba9b851edd7a2372342b5cf17d313d87be4a6c0d
Theo <theo.matussiere@gmail.com>
2019-09-03 16:53:56 +0200

    V1: partial type of init

--- torch/nn/__init__.pyi
+++ torch/nn/__init__.pyi
@@ -1,5 +1,10 @@
+import builtins
 from .modules import * 
 from .parameter import Parameter as Parameter
 from .parallel import DataParallel as DataParallel
 from . import init as init
 from . import utils as utils
+
+class Parameter:
+    @overload
+    def __init__(self, data, requires_grad: builtins.bool): ...
