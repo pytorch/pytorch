b0527b8e1766e42ff8a4c131c4376cb8f5789f75
Theo <theo.matussiere@gmail.com>
2019-09-03 17:42:14 +0200

    Unnecessary overload, full typing

--- torch/nn/parameter.pyi
+++ torch/nn/parameter.pyi
@@ -1,9 +1,7 @@
 from .. import Tensor
-from typing import overload
 import builtins
 
 class Parameter(Tensor):
-    @overload
-    def __init__(self, data, requires_grad: builtins.bool): ...
+    def __init__(self, data: Tensor, requires_grad: builtins.bool): ...
 
     ...
