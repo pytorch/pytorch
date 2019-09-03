bb7c034e8f1887323766c7fcab96a6b4ba272159
Pavel Belevich <pbelevich@fb.com>
2019-09-03 16:44:42 -0400

    Fixed tensor^tensor for integral types

--- aten/src/ATen/native/cuda/PowKernel.cu
+++ aten/src/ATen/native/cuda/PowKernel.cu
@@ -9,12 +9,33 @@ namespace at { namespace native {
 
 namespace {
 
+template <typename T>
+static inline __host__ __device__ T powi(T a, T b) {
+  T result = 1;
+  while (b) {
+    if (b & 1) {
+       result *= a;
+    }
+    b /= 2;
+    a *= a;
+  }
+  return result;
+}
+
 void pow_tensor_tensor_kernel(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "pow_cuda", [&]() {
-    gpu_kernel(iter, []GPU_LAMBDA(scalar_t base, scalar_t exp) -> scalar_t {
-      return std::pow(base, exp);
+  if (isFloatingType(iter.dtype())) {
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "pow_cuda", [&]() {
+      gpu_kernel(iter, []GPU_LAMBDA(scalar_t base, scalar_t exp) -> scalar_t {
+        return std::pow(base, exp);
+      });
     });
-  });
+  } else {
+    AT_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "pow_cuda", [&]() {
+      gpu_kernel(iter, []GPU_LAMBDA(scalar_t base, scalar_t exp) -> scalar_t {
+        return powi(base, exp);
+      });
+    });
+  }
 }
 
 void pow_tensor_scalar_kernel(TensorIterator& iter, Scalar exp_scalar) {
