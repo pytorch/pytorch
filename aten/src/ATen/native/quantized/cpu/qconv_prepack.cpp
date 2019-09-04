0f071f501e598f930c3f01047f9de60222fd8264
Edward Yang <ezyang@fb.com>
2019-09-04 14:57:14 -0700

    Revert D17097735: [quantization] Rename fbgemm quantized operators to generic `quantized` ops
    Test Plan: revert-hammer
    
    Differential Revision:
    D17097735
    
    Original commit changeset: 447112a7a421
    
    fbshipit-source-id: 78368b6f84d96cea70692fb000cebe99602a08c1

--- aten/src/ATen/native/quantized/cpu/qconv_prepack.cpp
+++ aten/src/ATen/native/quantized/cpu/qconv_prepack.cpp
@@ -132,7 +132,7 @@ class QConvPackWeightInt8 final : public c10::OperatorKernel {
 };
 
 static auto registry = c10::RegisterOperators().op(
-    "quantized::conv_prepack",
+    "quantized::fbgemm_conv_prepack",
     c10::RegisterOperators::options().kernel<QConvPackWeightInt8>(
         TensorTypeId::QuantizedCPUTensorId));
 
