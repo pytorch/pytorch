48d791d74dd4cb12f72c41fead75350c30ef9e40
Pieter Noordhuis <pietern@fb.com>
2019-09-04 07:49:52 -0700

    Run clang-format on torch/csrc/distributed
    TSIA
    
    Differential Revision: [D17182909](https://our.internmc.facebook.com/intern/diff/D17182909/)

--- torch/csrc/distributed/rpc/python_functions.cpp
+++ torch/csrc/distributed/rpc/python_functions.cpp
@@ -31,28 +31,28 @@ std::shared_ptr<FutureMessage> py_rpc_builtin(
     const std::string& opName,
     const py::args& args,
     const py::kwargs& kwargs) {
-    // builtin operators.
-    Symbol symbol = Symbol::fromQualString(opName);
-    if (symbol.is_aten()) {
-      Stack stack;
-      for (const auto& op : torch::jit::getAllOperatorsFor(symbol)) {
-        try {
-          // FIXME: This is temporary solution. We should at least refactor
-          // ``createStackForSchema`` to avoid throwing an error.
-          stack = torch::jit::createStackForSchema(
-              op->schema(), args, kwargs, c10::nullopt);
+  // builtin operators.
+  Symbol symbol = Symbol::fromQualString(opName);
+  if (symbol.is_aten()) {
+    Stack stack;
+    for (const auto& op : torch::jit::getAllOperatorsFor(symbol)) {
+      try {
+        // FIXME: This is temporary solution. We should at least refactor
+        // ``createStackForSchema`` to avoid throwing an error.
+        stack = torch::jit::createStackForSchema(
+            op->schema(), args, kwargs, c10::nullopt);
 
-        } catch (std::runtime_error& e) {
-          VLOG(1) << "Couldn't match schema: " << op->schema()
-                  << " to args: " << args << " and kwargs: " << kwargs
-                  << ", reason: " << e.what();
-          continue;
-        }
-
-        // Found the right op! Send it along...
-        return agent.send(dst, ScriptCall(op, std::move(stack)).toMessage());
+      } catch (std::runtime_error& e) {
+        VLOG(1) << "Couldn't match schema: " << op->schema()
+                << " to args: " << args << " and kwargs: " << kwargs
+                << ", reason: " << e.what();
+        continue;
       }
+
+      // Found the right op! Send it along...
+      return agent.send(dst, ScriptCall(op, std::move(stack)).toMessage());
     }
+  }
 
   AT_ERROR(
       "Failed to match operator name ",
@@ -72,10 +72,10 @@ std::shared_ptr<FutureMessage> py_rpc_python_udf(
   std::vector<char> data(pickledPythonUDF.begin(), pickledPythonUDF.end());
   std::vector<torch::Tensor> tensor_table;
 
-  return agent.send(dst,
-                    Message(std::move(data),
-                            std::move(tensor_table),
-                            MessageType::PYTHON_CALL));
+  return agent.send(
+      dst,
+      Message(
+          std::move(data), std::move(tensor_table), MessageType::PYTHON_CALL));
 }
 
 } // namespace rpc
