9e0db9bab8e75a624b7683b4187ad3d7777da4a8
yanlizhao <yanlizhao@fb.com>
2019-09-04 11:25:23 -0700

    Update on "make rpc multiprocess test to use spawn instead of fork"
    1. current fork unit tests did not catch rpc exit crash issue, but spawn caught it. most of use cases also uses spawn,
    so changing multiprocessing start method from fork to spawn
    
    2. it did not change fork to spawn for c10d tests for now -- if we need to make changes for c10d, it may need another PR as c10d has a lot of unit tests, may need some more changes to make spawn work for test_c10d
    
    3. fix the real segment exit issue caused by python rpc handler temporarily by removing module_ global variable, separate diff D17097999 is to change python rpc handler to be singletion class for deterministic destruction order of variables
    
    Differential Revision: [D17086007](https://our.internmc.facebook.com/intern/diff/D17086007/)

--- torch/csrc/distributed/rpc/python_rpc_handler.cpp
+++ torch/csrc/distributed/rpc/python_rpc_handler.cpp
@@ -4,45 +4,43 @@ namespace torch {
 namespace distributed {
 namespace rpc {
 namespace {
-  py::object runUDFFunction_ = py::none();
-  py::object loadResultFunction_ = py::none();
+py::object runUDFFunction_ = py::none();
+py::object loadResultFunction_ = py::none();
 } // anonymous namespace
 
 namespace PythonRpcHandler {
-  void init() {
-    AutoGIL ag;
-    py::object module
-      = py::module::import("torch.distributed.internal_rpc_utils");
-    TORCH_CHECK(!module.is(py::none()), "module is None.");
-    if (runUDFFunction_.is(py::none())) {
-      runUDFFunction_ = module.attr("run_python_udf_internal");
-    }
-    if (loadResultFunction_.is(py::none())) {
-      loadResultFunction_ = module.attr("load_python_udf_result_internal");
-    }
+void init() {
+  AutoGIL ag;
+  py::object module =
+      py::module::import("torch.distributed.internal_rpc_utils");
+  TORCH_CHECK(!module.is(py::none()), "module is None.");
+  if (runUDFFunction_.is(py::none())) {
+    runUDFFunction_ = module.attr("run_python_udf_internal");
   }
-
-  std::vector<char> generatePythonUDFResult(
-    const Message& request) {
-    AutoGIL ag;
-    auto pargs = py::bytes(request.payload().data(), request.payload().size());
-    TORCH_CHECK(!runUDFFunction_.is(py::none()), "runUDFFunction_ is None.");
-    py::bytes pres = runUDFFunction_(pargs);
-    const auto& presStr = static_cast<std::string>(pres);
-    std::vector<char> payload(presStr.begin(), presStr.end());
-    return payload;
+  if (loadResultFunction_.is(py::none())) {
+    loadResultFunction_ = module.attr("load_python_udf_result_internal");
   }
+}
 
-  py::object loadPythonUDFResult(const Message& message) {
-    AutoGIL ag;
-    auto pargs = py::bytes(message.payload().data(), message.payload().size());
-    TORCH_CHECK(
-        !loadResultFunction_.is(py::none()), "loadResultFunction_ is None.");
-    return loadResultFunction_(pargs);
-  }
-} // PythonRpcHandler
+std::vector<char> generatePythonUDFResult(const Message& request) {
+  AutoGIL ag;
+  auto pargs = py::bytes(request.payload().data(), request.payload().size());
+  TORCH_CHECK(!runUDFFunction_.is(py::none()), "runUDFFunction_ is None.");
+  py::bytes pres = runUDFFunction_(pargs);
+  const auto& presStr = static_cast<std::string>(pres);
+  std::vector<char> payload(presStr.begin(), presStr.end());
+  return payload;
+}
 
+py::object loadPythonUDFResult(const Message& message) {
+  AutoGIL ag;
+  auto pargs = py::bytes(message.payload().data(), message.payload().size());
+  TORCH_CHECK(
+      !loadResultFunction_.is(py::none()), "loadResultFunction_ is None.");
+  return loadResultFunction_(pargs);
+}
+} // namespace PythonRpcHandler
 
-} // rpc
-} // distributed
-} // torch
+} // namespace rpc
+} // namespace distributed
+} // namespace torch
