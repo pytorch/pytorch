f21a09f866183bd950943a86d69264c63fd0ac48
neginraoof <neginmr@utexas.edu>
2019-09-04 11:37:50 -0700

    fix for gather axis in caffe2 backend

--- caffe2/onnx/backend.cc
+++ caffe2/onnx/backend.cc
@@ -709,10 +709,16 @@ Caffe2Ops Caffe2Backend::CreateGather(
   std::vector<std::string> inputs;
   inputs.emplace_back(node.input(0));
   inputs.emplace_back(node.input(1));
+
+  auto axis = onnx_node->attributes.get<int64_t>("axis", 1L);
+  caffe2::Argument arg_axis;
+  arg_axis.set_name("axis");
+  arg_axis.set_i(axis);
+
   std::vector<std::string> outputs;
   outputs.emplace_back(node.output(0));
 
-  BuildOperator(c2_op, "Gather", inputs, outputs);
+  BuildOperator(c2_op, "Gather", inputs, outputs, {arg_axis});
 
   return ret;
 }
