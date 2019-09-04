9e6b654ae9a0cff61d955d4d9ebee38810f68a16
Theo <theo.matussiere@gmail.com>
2019-09-04 09:43:37 +0200

    Revert changes

--- torch/nn/__init__.pyi
+++ torch/nn/__init__.pyi
@@ -1,7 +1,6 @@
-import builtins
 from .modules import * 
 from .parameter import Parameter as Parameter
 from .parallel import DataParallel as DataParallel
 from . import init as init
-from . import functional as functional
 from . import utils as utils
+from . import functional as functional
