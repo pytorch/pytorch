4b5b5658a516be014e5d4033d3919fc46f75e99e
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-09-06 02:30:15 -0400

    Remove complex dispatch

--- aten/src/ATen/native/ScatterGather.cpp
+++ aten/src/ATen/native/ScatterGather.cpp
@@ -32,7 +32,7 @@ Tensor & gather_out_cpu(Tensor & result, const Tensor & self, int64_t dim, const
     }
   }
 
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND2(ScalarType::Bool, ScalarType::Half, self.scalar_type(), "gather_out_cpu", [&](){
+  AT_DISPATCH_ALL_TYPES_AND2(ScalarType::Bool, ScalarType::Half, self.scalar_type(), "gather_out_cpu", [&](){
     scalar_t *result_data = result.data<scalar_t>();
     scalar_t *self_data = self.data<scalar_t>();
     int64_t *index_data = index.data<int64_t>();
