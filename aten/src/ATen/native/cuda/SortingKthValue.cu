1101128c01b90f9cbc8dfc3d2da12b72abc828bb
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2019-09-09 16:25:09 -0500

    Don't double include.

--- aten/src/ATen/native/cuda/SortingKthValue.cu
+++ aten/src/ATen/native/cuda/SortingKthValue.cu
@@ -21,7 +21,6 @@
 #include <THC/THCThrustAllocator.cuh>
 #include <ATen/native/cuda/SortingCommon.cuh>
 #include <ATen/native/cuda/SortingRadixSelect.cuh>
-#include <c10/macros/Macros.h>
 
 namespace at {
 namespace native {
