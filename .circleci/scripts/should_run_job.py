03cab892dafcec594c904e43feb3aba2b24a482e
Tao Xu <taox@fb.com>
2019-09-08 17:26:58 -0700

    Update on "[DO NOT MERGE][iOS][circle-ci] PR to test iOS builds"
    This PR is for testing the CI jobs for iOS buid.

--- .circleci/scripts/should_run_job.py
+++ .circleci/scripts/should_run_job.py
@@ -47,8 +47,8 @@ default_set = set([
     # PyTorch Android gradle
     'pytorch-linux-xenial-py3-clang5-android-ndk-r19c-gradle-build-only-x86_32',
     # PyTorch iOS build
-    'pytorch-ios-10-2-1-x86_64_build',
-    'pytorch-ios-10-2-1-arm64_build',
+    'pytorch-ios-10.2.1-x86_64_build',
+    'pytorch-ios-10.2.1-arm64_build',
 
     # XLA
     'pytorch-xla-linux-xenial-py3.6-clang7',
