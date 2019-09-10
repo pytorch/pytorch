c60dddbb9ffe1732834bad4750b04b6d90ef8ed2
Supriya Rao <supriyar@fb.com>
2019-09-10 08:39:07 -0700

    Store bias in PackedConvWeight in fbgemm (#25626)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25626
    
    Add bias as an optional parameter in the packed conv weight struct.
    ghstack-source-id: 89780639
    
    Test Plan: python test/run_test.py --exclude nn --verbose --bring-to-front quantization quantized quantized_tensor quantized_nn_mods quantizer
    
    Reviewed By: raghuramank100
    
    Differential Revision: D17177723
    
    fbshipit-source-id: e502f2196cb1c002db8b691124db740368944c92

--- torch/nn/_intrinsic/quantized/modules/conv_relu.py
+++ torch/nn/_intrinsic/quantized/modules/conv_relu.py
@@ -24,29 +24,13 @@ class ConvReLU2d(nnq.Conv2d):
                                          stride=stride, padding=padding, dilation=dilation,
                                          groups=groups, bias=bias, padding_mode=padding_mode)
 
-    def weight(self):
-        return torch.ops.quantized.conv_unpack(self._packed_weight).permute([0, 3, 1, 2])
-
-    def set_weight(self, w):
-        self._packed_weight = torch.ops.quantized.conv_prepack(w.permute([0, 2, 3, 1]),
-                                                               self.stride,
-                                                               self.padding,
-                                                               self.dilation,
-                                                               self.groups)
-        self.weight_scale = w.q_scale()
-
     def forward(self, input):
         # Temporarily using len(shape) instead of ndim due to JIT issue
         # https://github.com/pytorch/pytorch/issues/23890
         if len(input.shape) != 4:
             raise ValueError("Input shape must be `(N, C, H, W)`!")
-        # Temporary work around for bias
-        # see Issue:https://github.com/pytorch/pytorch/issues/23874
-        bias = self.bias
-        if bias is not None:
-            bias = torch.quantize_linear(bias.dequantize(), float(self.weight_scale) * input.q_scale(), 0, torch.qint32)
         output = torch.ops.quantized.conv2d_relu(input.permute([0, 2, 3, 1]),
-                                                 self._packed_weight, bias,
+                                                 self._packed_params,
                                                  self.stride, self.padding,
                                                  self.dilation, self.groups,
                                                  float(self.scale), int(self.zero_point))
