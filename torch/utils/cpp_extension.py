fd3014e0b6f15f63fb334ac6f2367eedc554cac6
bildad namawa <bildadnamawa@gmail.com>
2019-09-10 22:41:42 +0300

    fix the error  UnicodeDecodeError: 'ascii' codec can't decode byte 0xe2 in position 1199: ordinal not in range(128) #25891

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1039,7 +1039,7 @@ def _build_extension_module(name, build_directory, verbose):
         # error.output contains the stdout and stderr of the build attempt.
         message = "Error building extension '{}'".format(name)
         if hasattr(error, 'output') and error.output:
-            message += ": {}".format(error.output.decode())
+            message += ": {}".format(error.output.decode(sys.stdout.encoding))
         raise RuntimeError(message)
 
 
