d8433822be7ef41299555cfe2b61ad2c1a45a96f
Supriya Rao <supriyar@fb.com>
2019-09-10 14:12:29 -0700

    Add new API for Convolution operators in QNNPACK. Tested using QNNPACK unit tests

--- aten/src/ATen/native/quantized/cpu/qnnpack/include/qnnpack_func.h
+++ aten/src/ATen/native/quantized/cpu/qnnpack/include/qnnpack_func.h
@@ -2,6 +2,35 @@
 #include <conv_utils.h>
 
 namespace qnnpack {
+class PrePackConvWeights final {
+ public:
+  PrePackConvWeights(const conv_param_t& conv_param, const uint8_t* kernel, const int32_t* bias);
+
+  void* getPackedWeights() const
+  {
+    return packed_weights_;
+  }
+
+  int64_t getOutputChannels() const
+  {
+    return output_channels_;
+  }
+
+  ~PrePackConvWeights()
+  {
+    if (packed_weights_ != nullptr) {
+      free(packed_weights_);
+    }
+  }
+
+  PrePackConvWeights() = delete;
+  PrePackConvWeights(const PrePackConvWeights&) = delete;
+  PrePackConvWeights& operator=(const PrePackConvWeights&) = delete;
+
+ private:
+  void* packed_weights_ = nullptr;
+  int64_t output_channels_;
+};
 
 class PackBMatrix final {
  public:
@@ -13,17 +42,17 @@ class PackBMatrix final {
       const uint8_t* kernel,
       const int32_t* bias);
 
-  void* getPackedWeights()
+  void* getPackedWeights() const
   {
     return packed_weights_;
   }
 
-  size_t getInputChannels()
+  size_t getInputChannels() const
   {
     return input_channels_;
   }
 
-  size_t getOutputChannels()
+  size_t getOutputChannels() const
   {
     return output_channels_;
   }
@@ -46,22 +75,35 @@ class PackBMatrix final {
 };
 
 enum pytorch_qnnp_status qnnpackLinear(
-    size_t batch_size,
-    size_t input_channels,
-    size_t output_channels,
-    uint8_t input_zero_point,
-    float input_scale,
-    uint8_t kernel_zero_point,
-    float kernel_scale,
-    uint8_t output_zero_point,
-    float output_scale,
-    uint8_t output_min,
-    uint8_t output_max,
+    const size_t batch_size,
+    const size_t input_channels,
+    const size_t output_channels,
+    const uint8_t input_zero_point,
+    const float input_scale,
+    const uint8_t kernel_zero_point,
+    const float kernel_scale,
+    const uint8_t output_zero_point,
+    const float output_scale,
+    const uint8_t output_min,
+    const uint8_t output_max,
     const uint8_t* input,
-    size_t input_stride,
+    const size_t input_stride,
     void* packed_weights,
     uint8_t* output,
-    size_t output_stride,
+    const size_t output_stride,
     pthreadpool_t threadpool);
 
+enum pytorch_qnnp_status qnnpackConv(
+    const conv_param_t& conv_p,
+    void* packed_weights,
+    const size_t batch_size,
+    const size_t input_height,
+    const size_t input_width,
+    const float input_scale,
+    const uint8_t input_zero_point,
+    const uint8_t* input,
+    const float output_scale,
+    const uint8_t output_zero_point,
+    uint8_t* output,
+    pthreadpool_t threadpool);
 } // namespace qnnpack
