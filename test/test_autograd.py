f63ffdbf3df7e7b64bf749d39987a2b26d920e3d
J M Dieterich <dieterich@ogolem.org>
2019-09-11 13:37:14 -0500

    Works on ROCm

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1953,7 +1953,6 @@ class TestAutograd(TestCase):
         self.assertEqual(device[0], 1)
 
     @unittest.skipIf(torch.cuda.device_count() < 2, "no multi-GPU")
-    @skipIfRocm
     def test_inputbuffer_add_multigpu(self):
         input = torch.randn(1).cuda(0).requires_grad_()
         output = input.cuda(1) + input.cuda(1)
