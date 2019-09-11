a074df570b0e803dee261429c8408178f45aaefa
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2019-09-11 13:32:33 -0700

    Prepend all PyTorch QNNPACK symbols with pytorch_ to avoid symbol collision.

--- aten/src/ATen/native/quantized/cpu/qnnpack/src/qnnpack/requantization-stubs.h
+++ aten/src/ATen/native/quantized/cpu/qnnpack/src/qnnpack/requantization-stubs.h
@@ -19,7 +19,7 @@
 extern "C" {
 #endif
 
-typedef void (*requantization_function)(
+typedef void (*pytorch_requantization_function)(
     size_t n,
     const int32_t* input,
     float scale,
@@ -28,7 +28,7 @@ typedef void (*requantization_function)(
     uint8_t qmax,
     uint8_t* output);
 
-#define DECLARE_REQUANTIZATION_FUNCTION(fn_name) \
+#define PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(fn_name) \
   void fn_name(                                  \
       size_t n,                                  \
       const int32_t* input,                      \
@@ -38,36 +38,36 @@ typedef void (*requantization_function)(
       uint8_t qmax,                              \
       uint8_t* output);
 
-DECLARE_REQUANTIZATION_FUNCTION(
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(
     pytorch_qnnp_requantize_precise__scalar_unsigned32)
-DECLARE_REQUANTIZATION_FUNCTION(
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(
     pytorch_qnnp_requantize_precise__scalar_unsigned64)
-DECLARE_REQUANTIZATION_FUNCTION(
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(
     pytorch_qnnp_requantize_precise__scalar_signed64)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__sse2)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__ssse3)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__sse4)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__neon)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__psimd)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__sse2)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__ssse3)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__sse4)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__neon)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_precise__psimd)
 
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__scalar_lrintf)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__scalar_magic)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__sse2)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__neon)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__psimd)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__scalar_lrintf)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__scalar_magic)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__sse2)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__neon)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_fp32__psimd)
 
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__scalar)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__sse2)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__ssse3)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__sse4)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__neon)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__psimd)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__scalar)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__sse2)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__ssse3)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__sse4)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__neon)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_q31__psimd)
 
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__scalar)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__sse2)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__ssse3)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__sse4)
-DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__neon)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__scalar)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__sse2)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__ssse3)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__sse4)
+PYTORCH_DECLARE_REQUANTIZATION_FUNCTION(pytorch_qnnp_requantize_gemmlowp__neon)
 
 #ifdef __cplusplus
 } /* extern "C" */
