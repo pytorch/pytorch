d270b3ac8bfb15da60263b3b1cd30e13814eb1bc
J M Dieterich <dieterich@ogolem.org>
2019-09-11 13:40:26 -0500

    Works on ROCm

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1924,7 +1924,6 @@ class TestAutograd(TestCase):
         torch.autograd.grad(y, x)  # should not error!
 
     @unittest.skipIf(torch.cuda.device_count() < 2, "no multi-GPU")
-    @skipIfRocm
     def test_unused_output_gpu(self):
         from torch.nn.parallel._functions import Broadcast
         x = Variable(torch.randn(5, 5).float().cuda(), requires_grad=True)
