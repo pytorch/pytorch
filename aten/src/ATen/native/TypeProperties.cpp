1f2aa02657b28434cc616885af96e6b5b8017cee
swchen1234 <swchen0215@gmail.com>
2019-09-11 15:53:00 -0400

    Update TypeProperties.cpp

--- aten/src/ATen/native/TypeProperties.cpp
+++ aten/src/ATen/native/TypeProperties.cpp
@@ -48,8 +48,4 @@ bool _can_Cast(at::ScalarType from, at::ScalarType to) {
   return at::canCast(from, to);
 }  
 
-bool _can_Cast(at::ScalarType from, at::ScalarType to) {
-  return at::canCast(from, to);
-}
-  
 }} // namespace at::native
