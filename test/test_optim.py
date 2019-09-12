1c596433ec58b77add6b72a8e8cd5c7aaa552a2d
Vincent Quenneville-Belair <vincentqb@gmail.com>
2019-09-12 12:57:19 -0400

    fix cyclic lr.
    ghstack-source-id: 78068b53522adba476ef9abe7510a99d688345c9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26101

--- test/test_optim.py
+++ test/test_optim.py
@@ -1283,5 +1283,26 @@ class TestLRScheduler(TestCase):
                         msg='Momentum is wrong in batch_num {}: expected {}, got {}'.format(
                             batch_num, momentum_target[batch_num], param_group['momentum']), delta=1e-5)
 
+    def test_cosine_then_cyclic(self):
+        # https://github.com/pytorch/pytorch/issues/21965
+
+        model = torch.nn.Linear(2, 1)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+        lr_scheduler_1 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20, eta_min=0.1)
+        lr_scheduler_2 = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.1, max_lr=0.3, step_size_up=1, step_size_down=3)
+
+        lrs = []
+
+        for i in range(40):
+            if i <= lr_scheduler_1.T_max:
+                lr_scheduler_1.step()
+            else:
+                lr_scheduler_2.step()
+            lrs.append(
+                optimizer.param_groups[0]["lr"]
+            )
+
+        self.assertLessEqual(lrs[-1], 0.3)
+
 if __name__ == '__main__':
     run_tests()
