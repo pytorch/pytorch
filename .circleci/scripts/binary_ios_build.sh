865eb74d0323b428262b8a8431715ee3e569c765
Tao Xu <taox@fb.com>
2019-09-12 00:05:57 -0700

    Update on "[DO NOT MERGE][IOS][CIRCLE CI] Test Binary build for iOS"

--- .circleci/scripts/binary_ios_build.sh
+++ .circleci/scripts/binary_ios_build.sh
@@ -28,22 +28,22 @@ echo "########################################################"
 echo "IOS_ARCH: ${IOS_ARCH}"
 echo "IOS_PLATFORM: ${IOS_PLATFORM}"
 BUILD_PYTORCH_MOBILE=1
-# unbuffer ${PROJ_ROOT}/scripts/build_ios.sh 2>&1 | ts
-CMAKE_ARGS=() 
-if [ -n "${IOS_ARCH:-}" ]; then
-  CMAKE_ARGS+=("-DIOS_ARCH=${IOS_ARCH}")
-  export IOS_ARCH=${IOS_ARCH}
-fi
-if [ -n "${IOS_PLATFORM:-}" ]; then
-  CMAKE_ARGS+=("-DIOS_PLATFORM=${IOS_PLATFORM}")
-fi
-if [ -n "${USE_NNPACK:-}" ]; then 
-  CMAKE_ARGS+=("-DUSE_NNPACK=${USE_NNPACK}")
-fi 
-CMAKE_ARGS+=("-DBUILD_CAFFE2_MOBILE=OFF")
-CMAKE_ARGS+=("-DCMAKE_PREFIX_PATH=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')")
-CMAKE_ARGS+=("-DPYTHON_EXECUTABLE=$(python -c 'import sys; print(sys.executable)')")
-unbuffer ${PROJ_ROOT}/scripts/build_ios.sh ${CMAKE_ARGS[@]} 2>&1 | ts
+unbuffer ${PROJ_ROOT}/scripts/build_ios.sh 2>&1 | ts
+# CMAKE_ARGS=() ruffjjffkjlvldgitujelnivkbktdifi
+# if [ -n "${IOS_ARCH:-}" ]; then
+#   CMAKE_ARGS+=("-DIOS_ARCH=${IOS_ARCH}")
+#   export IOS_ARCH=${IOS_ARCH}
+# fi
+# if [ -n "${IOS_PLATFORM:-}" ]; then
+#   CMAKE_ARGS+=("-DIOS_PLATFORM=${IOS_PLATFORM}")
+# fi
+# if [ -n "${USE_NNPACK:-}" ]; then 
+#   CMAKE_ARGS+=("-DUSE_NNPACK=${USE_NNPACK}")
+# fi 
+# CMAKE_ARGS+=("-DBUILD_CAFFE2_MOBILE=OFF")
+# CMAKE_ARGS+=("-DCMAKE_PREFIX_PATH=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')")
+# CMAKE_ARGS+=("-DPYTHON_EXECUTABLE=$(python -c 'import sys; print(sys.executable)')")
+# unbuffer ${PROJ_ROOT}/scripts/build_ios.sh ${CMAKE_ARGS[@]} 2>&1 | ts
 
 #store the binary
 cd ${WORKSPACE}
