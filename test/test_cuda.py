79ed58fdf3bbed502fa13ca4998319caec808ab5
vishwakftw <vishwaks@cs.cmu.edu>
2019-09-12 16:02:44 -0400

    Skip testing triangular_solve_batched on non-default CUDA stream

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2784,7 +2784,7 @@ class TestCuda(TestCase):
         _TestTorchMixin._test_triangular_solve(self, lambda t: t.cuda())
 
     @unittest.skipIf(not TEST_MAGMA, "no MAGMA library detected")
-    @unittest.skip("Spuriously failing")
+    @skipCUDANonDefaultStreamIf(True)
     def test_triangular_solve_batched(self):
         _TestTorchMixin._test_triangular_solve_batched(self, lambda t: t.cuda())
 
