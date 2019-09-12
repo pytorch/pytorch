c78650f862b96ddb87370521ae507b580c1fa0cc
Tao Xu <taox@fb.com>
2019-09-12 02:34:29 -0700

    Update on "[DO NOT MERGE][IOS][CIRCLE CI] Test Binary build for iOS"

--- .circleci/scripts/binary_ios_upload.sh
+++ .circleci/scripts/binary_ios_upload.sh
@@ -9,7 +9,7 @@ mkdir -p ${ZIP_DIR}/install/lib
 mkdir -p ${ZIP_DIR}/src
 # copy header files
 cp -R ${ARTIFACTS_DIR}/arm64/include ${ZIP_DIR}/install/
-# archvie to FAT bianry
+# build a FAT bianry
 cd ${ZIP_DIR}/install/lib
 target_libs=(libc10.a libclog.a libcpuinfo.a libqnnpack.a libtorch.a)
 for lib in ${target_libs[*]}
@@ -20,22 +20,17 @@ done
 # for nnpack, we only support arm64/armv7s build
 lipo -create ${ARTIFACTS_DIR}/arm64/lib/libnnpack.a ${ARTIFACTS_DIR}/armv7s/lib/libnnpack.a -o ${ZIP_DIR}/install/lib/libnnpack.a
 lipo -i ${ZIP_DIR}/install/lib/*.a
-# copy the umbrella header
-if [ -e ${PROJ_ROOT}/ios/LibTorch.h ]; then
-    cp ${PROJ_ROOT}/ios/LibTorch.h ${ZIP_DIR}/src/
-    cp ${PROJ_ROOT}/ios/README.md ${ZIP_DIR}/src/
-else
-    echo "LibTorch.h not found!"
-    touch ${ZIP_DIR}/src/LibTorch.h
-    echo "import <torch/script.h>" > ${ZIP_DIR}/src/LibTorch.h
-fi
+# copy the umbrella header and license
+cp ${PROJ_ROOT}/ios/LibTorch.h ${ZIP_DIR}/src/
+cp ${PROJ_ROOT}/LICENSE ${ZIP_DIR}/
 # zip the library
 timestamp=`date +"%Y-%m-%d_%H-%M-%S"`
 ZIPFILE=libtorch_ios_nightly_build.zip
 cd ${ZIP_DIR}
+#for testing
 touch version.txt
 echo `date +%s` > version.txt
-zip -r ${ZIPFILE} install src version.txt
+zip -r ${ZIPFILE} install src version.txt LICENSE
 # upload to aws
 brew install awscli
 set +x
