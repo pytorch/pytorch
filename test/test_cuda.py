5376ee51fd58671a58de9bcf11f6e400576a824e
J M Dieterich <dieterich@ogolem.org>
2019-09-11 17:51:43 -0700

    Enable more mGPU tests (#26055)
    Summary:
    Enable mGPU tests that pass on ROCm as of 2.7.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26055
    
    Differential Revision: D17331484
    
    Pulled By: bddppq
    
    fbshipit-source-id: 51f956a84a6c14a1a41473d322950994fa29c25c

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1894,7 +1894,6 @@ class TestCuda(TestCase):
             c2p.put(sync_func(self, TestCuda.FIFTY_MIL_CYCLES))
 
     @unittest.skipIf(not TEST_MULTIGPU, "detected only one GPU")
-    @skipIfRocm
     def test_stream_event_nogil(self):
         for sync_func in [TestCuda._stream_synchronize,
                           TestCuda._event_synchronize,
@@ -1957,7 +1956,6 @@ class TestCuda(TestCase):
         self.assertTrue(s1.query())
 
     @unittest.skipIf(not TEST_MULTIGPU, "detected only one GPU")
-    @skipIfRocm
     def test_events_multi_gpu_query(self):
         d0 = torch.device('cuda:0')
         d1 = torch.device('cuda:1')
