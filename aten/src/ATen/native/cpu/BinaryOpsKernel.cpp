beffd6e7b48172571895954c676089ead743874b
Igor Fedan <ifedan@fb.com>
2019-09-12 17:45:52 -0400

    Migrate lt and lt_ from the TH to Aten

--- aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
@@ -104,9 +104,9 @@ void logical_xor_kernel(TensorIterator& iter) {
 }
 
 void lt_kernel(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Bool, iter.dtype(), "lt_cpu_result_", [&] {
+  AT_DISPATCH_ALL_TYPES_AND2(kBool, kBFloat16, iter.dtype(), "lt_cpu_result_", [&] {
     using return_t = scalar_t;
-    AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Bool, iter.input_dtype(), "lt_cpu", [&]() {
+    AT_DISPATCH_ALL_TYPES_AND2(kBool, kBFloat16, iter.input_dtype(), "lt_cpu", [&]() {
       cpu_kernel(iter,
        [=](scalar_t a, scalar_t b) -> return_t {
          return a < b;
