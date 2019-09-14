2ac98b57a7ae0ced191fdabec27d0fcea640745b
Mike Ruberry <mruberry@devfair044.maas>
2019-09-13 23:32:43 -0700

    Creates generic device type testing framework (#25967)
    Summary:
    This PR addresses https://github.com/pytorch/pytorch/issues/24851 by...
    
    1. lets device types easily register themselves for testing
    2. lets tests be written to run on multiple devices and with multiple dtypes
    3. provides a mechanism to instantiate those tests so they are discoverable and filterable by unittest and pytest
    
    It refactors three tests from test_torch.py to demonstrate how to use it.
    
    `test_diagonal` is the simplest example. Most tests just need to be modified to accept 'device' as an argument. The framework will then instantiate `test_diagonal_cpu` and `test_diagonal_cuda` (when CUDA is available) which call `test_diagonal` with the appropriate 'device' argument.
    
    `test_neg` also has dtype variants. It accepts both 'device' and 'dtype' as arguments, and the dtypes it runs with are specified with the 'dtypes' decorator. Dtypes can be specified for all device types and particular device types. The framework instantiates tests like `test_neg_cpu_torch.float`.
    
    `test_inverse` has device-specific dependencies. These dependencies are expressed with the sugary 'skipCUDAIfNoMagma' and 'skipCPUIfNoLapack' decorators. These decorators are device-specific so CPU testing is not skipped if Magma is not installed, and there conditions may be checked after or before the test case has been initialized. This means that skipCUDAIfNoMagma does not initialize CUDA. In fact, CUDA is only initialized if a CUDA test is run.
    
    These instantiated tests may be run as usual and with pytest filtering it's easy to run one test on all device types, run all the tests for a particular device type, or run a device type and dtype combination.
    
    See the note "Generic Device-Type Testing" for more detail.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25967
    
    Differential Revision: D17381987
    
    Pulled By: mruberry
    
    fbshipit-source-id: 4a639641130f0a59d22da0efe0951b24b5bc4bfb

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1086,9 +1086,6 @@ class TestCuda(TestCase):
             for num in abs_zeros:
                 self.assertGreater(math.copysign(1.0, num), 0.0)
 
-    def test_neg(self):
-        _TestTorchMixin._test_neg(self, lambda t: t.cuda())
-
     def test_bitwise_not(self):
         _TestTorchMixin._test_bitwise_not(self, 'cuda')
 
@@ -2198,10 +2195,6 @@ class TestCuda(TestCase):
     def _select_broadcastable_dims(dims_full=None):
         return _TestTorchMixin._select_broadcastable_dims(dims_full)
 
-    @unittest.skipIf(not TEST_MAGMA, "no MAGMA library detected")
-    def test_inverse(self):
-        _TestTorchMixin._test_inverse(self, lambda t: t.cuda())
-
     @slowTest
     @unittest.skipIf(not TEST_MAGMA, "no MAGMA library detected")
     def test_inverse_many_batches(self):
@@ -2747,9 +2740,6 @@ class TestCuda(TestCase):
 
     def test_lerp(self):
         _TestTorchMixin._test_lerp(self, lambda t: t.cuda())
-
-    def test_diagonal(self):
-        _TestTorchMixin._test_diagonal(self, dtype=torch.float32, device='cuda')
 
     def test_diagflat(self):
         _TestTorchMixin._test_diagflat(self, dtype=torch.float32, device='cuda')
