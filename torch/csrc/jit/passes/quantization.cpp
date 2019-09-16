981d2815eb1de815b611535afef1ae2e4cdc24b2
Jerry Zhang <jerryzh@fb.com>
2019-09-16 10:43:12 -0700

    Whiltelist and fusion support for quantized::linear - addmm (#26208)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26208
    
    Supporing `addmm` -> `quantized::linear` quant fusion
    
    Test Plan:
    python test/test_jit.py 'TestJit.test_quant_fusion'
    
    Imported from OSS
    
    Differential Revision: D17380074
    
    fbshipit-source-id: fae88f118f85663d777648695768b0504ed7ccf9

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -81,7 +81,7 @@ bool nodeQuantizable(Node* n) {
       "linear",
       "relu",
   };
-  std::vector<Symbol> aten_funcs;
+  std::vector<Symbol> aten_funcs = {Symbol::aten("addmm")};
   std::transform(
       call_funcs.begin(),
       call_funcs.end(),
@@ -651,8 +651,6 @@ void FoldQuantNodesIntoInputsOutputs(std::shared_ptr<Graph>& graph) {
 }
 
 void QuantFusion(std::shared_ptr<Graph>& graph) {
-  // First fuse aten::linear op
-  FuseLinear(graph);
   const std::unordered_map<std::string, std::string> pattern_and_replacements =
       {// quantized::conv2d
        {R"(
@@ -680,27 +678,28 @@ graph(%a_quant, %w_quant, %b_quant, %a_scale, %a_zero_point, %a_dtype, %w_scale,
         %out_param : int[] = prim::ListConstruct(%0, %3, %1, %2)
         %r_perm = aten::permute(%r, %out_param)
         return (%r_perm))"},
-       // quantized::linear
+       // addmm -> quantized::linear
        {R"(
-graph(%a_quant, %w_quant, %b_quant, %a_scale, %a_zero_point, %a_dtype, %w_scale, %w_zero_point, %w_dtype, %b_scale, %b_zero_point, %b_dtype, %r_scale, %r_zero_point, %r_dtype):
+graph(%a_quant, %w_quant, %b_quant, %a_scale, %a_zero_point, %a_dtype, %w_scale, %w_zero_point, %w_dtype, %b_scale, %b_zero_point, %b_dtype, %r_scale, %r_zero_point, %r_dtype, %4):
         %a_intrepr = aten::int_repr(%a_quant)
         %a_dequant = aten::_dequantize_linear(%a_intrepr, %a_scale, %a_zero_point, %a_dtype)
         %w_intrepr = aten::int_repr(%w_quant)
         %w_dequant = aten::_dequantize_linear(%w_intrepr, %w_scale, %w_zero_point, %w_dtype)
         %b_intrepr = aten::int_repr(%b_quant)
         %b_dequant = aten::_dequantize_linear(%b_intrepr, %b_scale, %b_zero_point, %b_dtype)
-        %r = aten::linear(%a_dequant, %w_dequant, %b_dequant)
+        %r = aten::addmm(%b_dequant, %a_dequant, %w_dequant, %4, %4)
         %r_quant = aten::quantize_linear(%r, %r_scale, %r_zero_point, %r_dtype)
         return (%r_quant))",
         R"(
-graph(%a_quant, %w_quant, %b_quant, %a_scale, %a_zero_point, %a_dtype, %w_scale, %w_zero_point, %w_dtype, %b_scale, %b_zero_point, %b_dtype, %r_scale, %r_zero_point, %r_dtype):
+graph(%a_quant, %w_quant, %b_quant, %a_scale, %a_zero_point, %a_dtype, %w_scale, %w_zero_point, %w_dtype, %b_scale, %b_zero_point, %b_dtype, %r_scale, %r_zero_point, %r_dtype, %4):
         %0 : int = prim::Constant[value=0]()
         %1 : int = prim::Constant[value=1]()
         %2 : int = prim::Constant[value=2]()
         %3 : int = prim::Constant[value=3]()
         %in_param : int[] = prim::ListConstruct(%0, %2, %3, %1)
         %a_perm : Tensor = aten::permute(%a_quant, %in_param)
-        %w_perm : Tensor = aten::permute(%w_quant, %in_param)
+        %w_quant_t = aten::t(%w_quant)
+        %w_perm : Tensor = aten::permute(%w_quant_t, %in_param)
         %w_packed = quantized::fbgemm_linear_prepack(%w_perm)
         %r = quantized::fbgemm_linear(%a_perm, %w_packed, %b_quant, %r_scale, %r_zero_point)
         %out_param : int[] = prim::ListConstruct(%0, %3, %1, %2)
