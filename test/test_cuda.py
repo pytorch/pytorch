4d36423752d1335d37b5e84d08f6d16dc956956f
Mike Ruberry <mruberry@devfair044.maas>
2019-09-17 15:12:41 -0700

    removes torchtest, adds generic device test class to test_nn, moves more tests to generic device torch tests

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2246,10 +2246,6 @@ class TestCuda(TestCase):
                             self.assertEqual(torch.backends.cuda.cufft_plan_cache.max_size, 10)  # default is cuda:0
                         self.assertEqual(torch.backends.cuda.cufft_plan_cache.max_size, 11)  # default is cuda:1
 
-    # passes on ROCm w/ python 2.7, fails w/ python 3.6
-    @skipIfRocm
-    def test_stft(self):
-        _TestTorchMixin._test_stft(self, device=torch.device('cuda'))
 
     def test_multinomial(self):
         _TestTorchMixin._test_multinomial(self, torch.cuda.FloatTensor)
