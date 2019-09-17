77cbba28e3e0ec950ff7b9134064d89f12774a00
Edward Z. Yang <ezyang@fb.com>
2019-09-17 18:19:44 -0400

    Update on "Add boxed fallback"
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- aten/src/ATen/core/Boxer.h
+++ aten/src/ATen/core/Boxer.h
@@ -6,24 +6,38 @@
 
 namespace at {
 
-template <class R, class... Ts>
-using are_all_constructible = c10::guts::conjunction<std::is_constructible<R, guts::decay_t<Ts>>...>;
+// Assume T is decayed
+template <typename T>
+using not_ok_to_box =
+  c10::guts::disjunction<
+    c10::guts::negation<std::is_constructible<IValue, T>>,
+    // some constructors are templated (and therefore pass
+    // is_constructible), but do not actually work with all
+    // template arguments, so we must blacklist them explicitly
+    // TODO: The correct fix is to sfinae based on is_constructible of T
+    std::is_same<optional<ArrayRef<Dimname>>, T>,
+    std::is_same<optional<ScalarType>, T>,
+    std::is_same<optional<MemoryFormat>, T>
+  >;
+
+template <class... Args>
+using all_ok_to_box = c10::guts::negation<c10::guts::disjunction<not_ok_to_box<guts::decay_t<Args>>...>>;
 
 struct Boxer {
   Boxer(torch::jit::Stack* stack) : stack_(stack) {}
 
   template <
       typename... Args,
-      typename c10::guts::enable_if_t<!are_all_constructible<IValue, Args...>::value, std::nullptr_t> = nullptr>
+      typename c10::guts::enable_if_t<!all_ok_to_box<Args...>::value, std::nullptr_t> = nullptr>
   bool operator()(Args&&... as) {
     return false;
   }
 
   template <
       typename... Args,
-      typename c10::guts::enable_if_t<are_all_constructible<IValue, Args...>::value, std::nullptr_t> = nullptr>
+      typename c10::guts::enable_if_t<all_ok_to_box<Args...>::value, std::nullptr_t> = nullptr>
   bool operator()(Args&&... as) {
-    // torch::jit::push(*stack_, std::forward<Args>(as)...);
+    torch::jit::push(*stack_, std::forward<Args>(as)...);
     return true;
   }
 
