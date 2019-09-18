1a36ad4b5e39a1954b96aa68d08e0dc9e446bbd4
Your Name <davidriazati@fb.com>
2019-09-18 15:41:25 -0700

    cleanup

--- test/cpp/jit/torch_python_test.cpp
+++ test/cpp/jit/torch_python_test.cpp
@@ -11,7 +11,7 @@ void runJITCPPTests(bool runCuda) {
 #else
 #define JIT_TEST(name) test##name();
 TORCH_API void runJITCPPTests(bool runCuda) {
-  // TH_FORALL_TESTS(JIT_TEST)
+  TH_FORALL_TESTS(JIT_TEST)
   if (runCuda) {
     TH_FORALL_TESTS_CUDA(JIT_TEST)
   }
@@ -20,7 +20,7 @@ TORCH_API void runJITCPPTests(bool runCuda) {
   // So it is not part of the general test list (which is shared between the gtest
   // and python test runners), but is instead invoked manually by the
   // torch_python_test.cpp
-  // testEvalModeForLoadedModule();
+  testEvalModeForLoadedModule();
   testSerializationInterop();
 }
 #undef JIT_TEST
