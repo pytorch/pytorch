947141ad64198c60202a0b275c1c98f468a1af15
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2019-09-19 13:36:51 -0700

    Fold activation permutation inside quantized conv operator (#26242)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26242
    
    According to https://github.com/pytorch/pytorch/issues/19092 we always keep NCHW order and do handling inside the kernels. This PR fixes it for activations of the qconv by using MemoryLayout mechanism - activations stay logically as NCHW but strided as NHWC.
    
    Note, that this version is more aggressive than eventual MemoryLayout mechanism - the QConv's output is always NHWC regardless of the input striding. I think it's ok as we don't have NCHW quantized kernels anyway - so the very first conv would magically switch the order, but I'm open to suggestions. Btw, it doesn't change behavior - same happens today in master because of the explicit permute() call.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D17443218
    
    Pulled By: dzhulgakov
    
    fbshipit-source-id: cfd136ae0465acd8d8c26ffad87385dac9c88726

--- test/test_quantized_nn_mods.py
+++ test/test_quantized_nn_mods.py
@@ -56,7 +56,6 @@ class FunctionalAPITest(QuantizationTestCase):
         dilation = (1, 1)
 
         X = torch.randn(N, iC, H, W, dtype=torch.float32)
-        X = X.permute([0, 2, 3, 1]).contiguous()
         qX = torch.quantize_linear(X, scale=scale, zero_point=128, dtype=torch.quint8)
 
         w = torch.randn(oC, iC // g, kH, kW, dtype=torch.float32)
@@ -72,10 +71,10 @@ class FunctionalAPITest(QuantizationTestCase):
                                                          g)
 
 
-        ref_result = torch.ops.quantized.conv2d(qX.permute([0, 2, 3, 1]), q_filters_ref,
+        ref_result = torch.ops.quantized.conv2d(qX, q_filters_ref,
                                                 stride,
                                                 i_padding, dilation,
-                                                g, scale, zero_point).permute([0, 3, 1, 2])
+                                                g, scale, zero_point)
 
         q_result = torch.nn.quantized.functional.conv2d(qX,
                                                         qw,
@@ -348,7 +347,6 @@ class ModuleAPITest(QuantizationTestCase):
         scale, zero_point = 1.0 / 255, 128
 
         X = torch.randn(N, iC, H, W, dtype=torch.float32)
-        X = X.permute([0, 2, 3, 1]).contiguous()
         qX = torch.quantize_linear(X, scale=scale, zero_point=128, dtype=torch.quint8)
 
         w = torch.randn(oC, iC // g, kH, kW, dtype=torch.float32)
