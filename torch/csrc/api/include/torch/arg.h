3a0b830460be05d5ec67bc4a93ecb65cc46dbd60
Will Feng <willfeng@fb.com>
2019-09-19 11:12:40 -0400

    inline arg functions

--- torch/csrc/api/include/torch/arg.h
+++ torch/csrc/api/include/torch/arg.h
@@ -4,15 +4,15 @@
 
 #define TORCH_ARG(T, name)                                       \
  public:                                                         \
-  auto name(const T& new_##name)->decltype(*this) { /* NOLINT */ \
+  inline auto name(const T& new_##name)->decltype(*this) { /* NOLINT */ \
     this->name##_ = new_##name;                                  \
     return *this;                                                \
   }                                                              \
-  auto name(T&& new_##name)->decltype(*this) { /* NOLINT */      \
+  inline auto name(T&& new_##name)->decltype(*this) { /* NOLINT */      \
     this->name##_ = std::move(new_##name);                       \
     return *this;                                                \
   }                                                              \
-  const T& name() const noexcept { /* NOLINT */                  \
+  inline const T& name() const noexcept { /* NOLINT */                  \
     return this->name##_;                                        \
   }                                                              \
  private:                                                        \
