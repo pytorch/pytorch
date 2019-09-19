b97748db9c200ed32d188286c8ca267e945e8571
Jerry Zhang <jerryzh168@gmail.com>
2019-09-19 11:25:43 -0700

    [quant] Expose supportedQEngines to python
    Summary:
    att
    
    Test Plan:
    python test/test_torch.py
    
    Reviewers:
    pt1quant
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -502,6 +502,20 @@ PyObject *THPModule_qEngine(PyObject */* unused */)
   return THPQEngine_New(at::globalContext().qEngine(), toString(at::globalContext().qEngine()));
 }
 
+PyObject *THPModule_supportedQEngines(PyObject */* unused */)
+{
+  auto qengines = at::globalContext().supportedQEngines();
+  auto list = THPObjectPtr(PyList_New(qengines.size()));
+  for (auto i = 0; i < qengines.size(); ++i) {
+    PyObject *i64 = THPUtils_packInt64(static_cast<int>(qengines[i]));
+    if (!i64) {
+      throw python_error();
+    }
+    PyList_SET_ITEM(list.get(), i, i64);
+  }
+  return list.release();
+}
+
 static PyMethodDef TorchMethods[] = {
   {"_initExtension",  (PyCFunction)THPModule_initExtension,   METH_O,       nullptr},
   {"_autograd_init",  (PyCFunction)THPAutograd_initExtension, METH_NOARGS,  nullptr},
@@ -540,6 +554,7 @@ static PyMethodDef TorchMethods[] = {
   {"_get_default_device", (PyCFunction)THPModule_getDefaultDevice, METH_NOARGS,   nullptr},
   {"_get_qengine", (PyCFunction)THPModule_qEngine, METH_NOARGS, nullptr},
   {"_set_qengine", (PyCFunction)THPModule_setQEngine, METH_O, nullptr},
+  {"_supported_qengines", (PyCFunction)THPModule_supportedQEngines, METH_NOARGS, nullptr},
   {nullptr, nullptr, 0, nullptr}
 };
 
