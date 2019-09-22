605e466bb1d643707eba76078553d88db94c5e9d
Raghu Krishnamoorthi <raghuramank@gmail.com>
2019-09-22 01:46:09 -0700

    Emulate weight and activation only quant with fake quant, numerics test
    Differential Revision: [D17520342](https://our.internmc.facebook.com/intern/diff/D17520342/)
    
    [ghstack-poisoned]

--- torch/quantization/QConfig.py
+++ torch/quantization/QConfig.py
@@ -19,3 +19,7 @@ default_dynamic_qconfig = QConfig_dynamic(weight=default_weight_observer())
 
 default_qat_qconfig = QConfig(activation=default_fake_quant(),
                               weight=default_weight_fake_quant())
+# Configs for simulating weight only quantization for debugging
+# Use only to analyze accuracy tradeoffs
+default_weight_only_quant_qconfig = QConfig(activation=observer(torch.nn.Identity), weight=default_weight_fake_quant())
+default_activation_only_quant_qconfig = QConfig(activation=default_fake_quant(), weight=observer(torch.nn.Identity))
