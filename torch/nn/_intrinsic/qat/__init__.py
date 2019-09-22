843318763535368809146188843c152f9464233d
Raghu Krishnamoorthi <raghuramank@gmail.com>
2019-09-22 01:45:56 -0700

    Quantization aware training: Freeze batch norm support
    For QAT we need to be able to control batch norm for all modules from the top. Adding helper functions to enable/disable batch norm freezing during training
    
    Differential Revision: [D17512199](https://our.internmc.facebook.com/intern/diff/D17512199/)
    
    [ghstack-poisoned]

--- torch/nn/_intrinsic/qat/__init__.py
+++ torch/nn/_intrinsic/qat/__init__.py
@@ -3,6 +3,7 @@ from .modules import LinearReLU
 from .modules import ConvReLU2d
 from .modules import ConvBn2d
 from .modules import ConvBnReLU2d
+from .modules import update_bn_stats, freeze_bn_stats
 
 __all__ = [
     'ConvBn2d',
--- torch/nn/_intrinsic/qat/modules/conv_fused.py
+++ torch/nn/_intrinsic/qat/modules/conv_fused.py
@@ -80,13 +80,21 @@ class ConvBn2d(nn.Conv2d):
             self.reset_bn_parameters()
 
     def enable_fake_quant(self):
-        self.observer.enable()
-        self.weight_fake_quant.enable()
+        self.observer.enable_fake_quant()
+        self.weight_fake_quant.enable_fake_quant()
         return self
 
     def disable_fake_quant(self):
-        self.observer.disable()
-        self.weight_fake_quant.disable()
+        self.observer.disable_fake_quant()
+        self.weight_fake_quant.disable_fake_quant()
+        return self
+
+    def update_bn_stats(self):
+        self.freeze_bn = False
+        return self
+
+    def freeze_bn_stats(self):
+        self.freeze_bn = True
         return self
 
     def _forward(self, input):
@@ -219,6 +227,14 @@ class ConvBnReLU2d(ConvBn2d):
     def forward(self, input):
         return self.observer(F.relu(super(ConvBnReLU2d, self)._forward(input)))
 
+    def update_bn_stats(self):
+        self.freeze_bn = False
+        return self
+
+    def freeze_bn_stats(self):
+        self.freeze_bn = True
+        return self
+
     @classmethod
     def from_float(cls, mod, qconfig=None):
         return super(ConvBnReLU2d, cls).from_float(mod, qconfig)
@@ -261,3 +277,11 @@ class ConvReLU2d(nnqat.Conv2d):
     @classmethod
     def from_float(cls, mod, qconfig=None):
         return super(ConvReLU2d, cls).from_float(mod, qconfig)
+
+def update_bn_stats(mod):
+    if type(mod) in set([ConvBnReLU2d,ConvBn2d]):
+        mod.update_bn_stats()
+
+def freeze_bn_stats(mod):
+    if type(mod) in set([ConvBnReLU2d, ConvBn2d]):
+        mod.freeze_bn_stats()
