b10a23e70998109906969ea7a47b0efac7a20327
Iurii Zdebskyi <iuriiz@fb.com>
2019-09-23 09:17:26 -0700

    Enabled bfloat16 dtype on CUDA
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/26407
    
    Differential Revision: D17450502
    
    Pulled By: izdeby
    
    fbshipit-source-id: 0a5acc5fe1b1555c61ebe038aee9eaaae9dac228

--- aten/src/THC/THCTensorMath.h
+++ aten/src/THC/THCTensorMath.h
@@ -10,6 +10,9 @@
 #include <THC/generic/THCTensorMath.h>
 #include <THC/THCGenerateBoolType.h>
 
+#include <THC/generic/THCTensorMath.h>
+#include <THC/THCGenerateBFloat16Type.h>
+
 #include <THC/generic/THCTensorMathBlas.h>
 #include <THC/THCGenerateAllTypes.h>
 
@@ -34,17 +37,26 @@
 #include <THC/generic/THCTensorMathReduce.h>
 #include <THC/THCGenerateBoolType.h>
 
+#include <THC/generic/THCTensorMathReduce.h>
+#include <THC/THCGenerateBFloat16Type.h>
+
 #include <THC/generic/THCTensorMathCompare.h>
 #include <THC/THCGenerateAllTypes.h>
 
 #include <THC/generic/THCTensorMathCompare.h>
 #include <THC/THCGenerateBoolType.h>
 
+#include <THC/generic/THCTensorMathCompare.h>
+#include <THC/THCGenerateBFloat16Type.h>
+
 #include <THC/generic/THCTensorMathCompareT.h>
 #include <THC/THCGenerateAllTypes.h>
 
 #include <THC/generic/THCTensorMathCompareT.h>
 #include <THC/THCGenerateBoolType.h>
+
+#include <THC/generic/THCTensorMathCompareT.h>
+#include <THC/THCGenerateBFloat16Type.h>
 
 #include <THC/generic/THCTensorMathScan.h>
 #include <THC/THCGenerateAllTypes.h>
@@ -57,6 +69,9 @@
 
 #include <THC/generic/THCTensorMasked.h>
 #include <THC/THCGenerateBoolType.h>
+
+#include <THC/generic/THCTensorMasked.h>
+#include <THC/THCGenerateBFloat16Type.h>
 
 #include <THC/generic/THCTensorScatterGather.h>
 #include <THC/THCGenerateAllTypes.h>
