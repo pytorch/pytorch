96f044f0433b145ce5e18e14753f28cb9ba117af
Rohan Varma <rvarm1@fb.com>
2019-09-23 13:26:30 -0700

    remove default constructor

--- torch/csrc/distributed/rpc/rpc_agent.h
+++ torch/csrc/distributed/rpc/rpc_agent.h
@@ -22,8 +22,6 @@ struct WorkerId {
         " out of bound of int16_t.");
   }
 
-  WorkerId() : name_(""), id_(-1){};
-
   WorkerId(std::string name, worker_id_t id) : name_(std::move(name)), id_(id) {
     bool validSize = name_.length() < MAX_NAME_LEN && name_.length() > 0;
     bool validChar =
