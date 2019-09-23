4137ca0a1657f8f24f6655711e9291b28d8aa1ca
lingyiliu <lingyiliu@fb.com>
2019-09-22 20:48:57 -0700

    add the handling of nhwc

--- aten/src/ATen/native/quantized/cpu/qupsample_nearest2d.cpp
+++ aten/src/ATen/native/quantized/cpu/qupsample_nearest2d.cpp
@@ -70,6 +70,62 @@ static void upsample_nearest2d_out_frame(
   }
 }
 
+template <typename scalar_t>
+static void upsample_nearest2d_out_frame_nhwc(
+    scalar_t* odata,
+    scalar_t* idata,
+    int64_t input_height,
+    int64_t input_width,
+    int64_t output_height,
+    int64_t output_width,
+    int64_t nbatch,
+    int64_t channels) {
+  const float height_scale = (float)input_height / (float)output_height;
+  const float width_scale = (float)input_width / (float)output_width;
+  for (int b = 0; b < nbatch; b++) {
+    auto* i_p = reinterpret_cast<typename scalar_t::underlying*>(idata + b * input_height * input_width * channels);
+    auto* o_p = reinterpret_cast<typename scalar_t::underlying*>(odata + b * output_height * output_width * channels);
+    // special case: just copy
+    if (input_height == output_height && input_width == output_width) {
+      for (int64_t h2 = 0; h2 < output_height; ++h2) {
+        const int64_t h1 = h2;
+
+        for (int64_t w2 = 0; w2 < output_width; ++w2) {
+          const int64_t w1 = w2;
+          const auto* pos1 = &i_p[(h1 * input_width + w1) * channels];
+          auto* pos2 = &o_p[(h2 * output_width + w2) * channels];
+
+          for (int64_t c = 0; c < channels; ++c) {
+            pos2[0] = pos1[0];
+            pos1++;
+            pos2++;;
+          }
+        }
+      }
+      return;
+    }
+
+    for (int64_t h2 = 0; h2 < output_height; ++h2) {
+      const int64_t h1 =
+          nearest_neighbor_compute_source_index(height_scale, h2, input_height);
+
+      for (int64_t w2 = 0; w2 < output_width; ++w2) {
+        const int64_t w1 =
+            nearest_neighbor_compute_source_index(width_scale, w2, input_width);
+
+        const auto* pos1 = &i_p[(h1 * input_width + w1)*channels];
+        auto* pos2 = &o_p[(h2 * output_width + w2)*channels];
+
+        for (int64_t c = 0; c < channels; ++c) {
+          pos2[0] = pos1[0];
+          pos1++;
+          pos2++;;
+        }
+      }
+    }
+  }
+}
+
 Tensor quantized_upsample_nearest2d_cpu(
     const Tensor& input,
     IntArrayRef output_size) {
@@ -90,30 +146,53 @@ Tensor quantized_upsample_nearest2d_cpu(
   int64_t channels = input.size(1);
   int64_t input_height = input.size(2);
   int64_t input_width = input.size(3);
+    AT_ASSERT(input_width > 0 && output_width > 0);
+  if (input.is_contiguous(c10::MemoryFormat::ChannelsLast)) {
+    Tensor output = at::_empty_affine_quantized(
+        {nbatch, channels, output_height, output_width},
+        input.options(),
+        input.q_scale(),
+        input.q_zero_point(),
+        input.suggest_memory_format());
 
-  Tensor output = at::_empty_affine_quantized(
-      {nbatch, channels, output_height, output_width},
-      input.options(),
-      input.q_scale(),
-      input.q_zero_point());
+    AT_DISPATCH_QINT_TYPES(input.scalar_type(), "upsample_nearest2d", [&] {
+      auto* idata = static_cast<scalar_t*>(input.data_ptr());
+      auto* odata = static_cast<scalar_t*>(output.data_ptr());
+      upsample_nearest2d_out_frame_nhwc<scalar_t>(
+          odata,
+          idata,
+          input_height,
+          input_width,
+          output_height,
+          output_width,
+          nbatch,
+          channels);
+    });
+    return output;
+  } else {
+    Tensor output = at::_empty_affine_quantized(
+        {nbatch, channels, output_height, output_width},
+        input.options(),
+        input.q_scale(),
+        input.q_zero_point());
 
-  auto input_contig = input.contiguous();
-  AT_ASSERT(input_width > 0 && output_width > 0);
+    auto input_contig = input.contiguous();
 
-  AT_DISPATCH_QINT_TYPES(input_contig.scalar_type(), "upsample_nearest2d", [&] {
-    auto* idata = static_cast<scalar_t*>(input_contig.data_ptr());
-    auto* odata = static_cast<scalar_t*>(output.data_ptr());
-    upsample_nearest2d_out_frame<scalar_t>(
-        odata,
-        idata,
-        input_height,
-        input_width,
-        output_height,
-        output_width,
-        nbatch,
-        channels);
-  });
-  return output;
+    AT_DISPATCH_QINT_TYPES(input_contig.scalar_type(), "upsample_nearest2d", [&] {
+      auto* idata = static_cast<scalar_t*>(input_contig.data_ptr());
+      auto* odata = static_cast<scalar_t*>(output.data_ptr());
+      upsample_nearest2d_out_frame<scalar_t>(
+          odata,
+          idata,
+          input_height,
+          input_width,
+          output_height,
+          output_width,
+          nbatch,
+          channels);
+    });
+    return output;
+  }
 }
 
 } // namespace native
