12438edf050748cdc898983597c2a0731d6441c2
Supriya Rao <supriyar@fb.com>
2019-09-23 11:31:08 -0700

    Update on "Update qengine flag in python to string"
    Summary: This change updates torch.backend.quantized.engine to accept string ("fbgemm"/"qnnpack"/"none" for now).
    set_qengine and get_qengine return an int which represents the at::QEngine enum
    
    Test Plan:
    python test/test_torch.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/common_quantized.py
+++ test/common_quantized.py
@@ -65,12 +65,12 @@ def _calculate_dynamic_qparams(X, dtype):
 
 @contextmanager
 def enable_mobile_quantized_engine():
-    torch.backends.quantized.engine = "qnnpack"
+    torch.backends.quantized.engine = 'qnnpack'
     try:
         yield
     finally:
-        qengines = torch._C._supported_qengines()
-        if 2 in qengines:
-            torch.backends.quantized.engine = "fbgemm"
+        qengines = torch.backends.quantized.get_supported_qengines()
+        if 'fbgemm' in qengines:
+            torch.backends.quantized.engine = 'fbgemm'
         else:
-            torch.backends.quantized.engine = "none"
+            torch.backends.quantized.engine = 'none'
