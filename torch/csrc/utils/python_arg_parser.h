7537bb23c0f7019aa5794a6814d3690b170bb6d8
Prasun Anand <prasunanand.bitsp@gmail.com>
2019-09-24 22:44:01 +0530

    Parse modified

--- torch/csrc/utils/python_arg_parser.h
+++ torch/csrc/utils/python_arg_parser.h
@@ -97,11 +97,14 @@ struct PythonArgParser {
 
   template<int N>
   inline PythonArgs parse(PyObject* args, PyObject* kwargs, ParsedArgs<N>& dst);
+  template<int N>
+  inline PythonArgs parse2(PyObject* args, PyObject* kwargs, ParsedArgs<N>& dst);
 
 private:
   [[noreturn]]
   void print_error(PyObject* args, PyObject* kwargs, PyObject* parsed_args[]);
   PythonArgs raw_parse(PyObject* args, PyObject* kwargs, PyObject* parsed_args[]);
+  PythonArgs raw_parse2(PyObject* args, PyObject* kwargs, PyObject* parsed_args[]);
 
   std::vector<FunctionSignature> signatures_;
   std::string function_name;
@@ -173,7 +176,8 @@ private:
 struct FunctionSignature {
   explicit FunctionSignature(const std::string& fmt);
 
-  bool parse(PyObject* args, PyObject* kwargs, PyObject* dst[], PyObject* overloaded_args[], bool raise_exception);
+  bool parse(PyObject* args, PyObject* kwargs, PyObject* dst[], bool raise_exception);
+  bool parse2(PyObject* args, PyObject* kwargs, PyObject* dst[], PyObject* overloaded_args[], bool raise_exception);
 
   std::string toString() const;
 
@@ -224,6 +228,15 @@ inline PythonArgs PythonArgParser::parse(PyObject* args, PyObject* kwargs, Parse
         (int)max_args, N);
   }
   return raw_parse(args, kwargs, dst.args);
+}
+
+template<int N>
+inline PythonArgs PythonArgParser::parse2(PyObject* args, PyObject* kwargs, ParsedArgs<N>& dst) {
+  if (N < max_args) {
+    throw ValueError("PythonArgParser: dst ParsedArgs buffer does not have enough capacity, expected %d (got %d)",
+        (int)max_args, N);
+  }
+  return raw_parse2(args, kwargs, dst.args);
 }
 
 bool PythonArgs::has_torch_function(){
