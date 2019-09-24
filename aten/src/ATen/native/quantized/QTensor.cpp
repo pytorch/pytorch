8fd5bfcab31e92da86d348d55ebe93c879d0a733
Jerry Zhang <jerryzh168@gmail.com>
2019-09-24 14:35:43 -0700

    Update on "[fix] quantized_tensor tests"
    Summary:
    Previously we are using empty to generate test tensors, this PR changes the test tensors to use
    randint so that we can test things properly
    Also added a set_sizes_and_strides and removed .contiguous() in int_repr function to preserve the
    original size and strides
    
    Test Plan:
    python test/test_quantized_tensor.py
    
    Reviewers:
    pt1quant
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/QTensor.cpp
+++ aten/src/ATen/native/quantized/QTensor.cpp
@@ -102,19 +102,23 @@ IntArrayRef q_per_channel_axis_quant(const Tensor& self) {
   return static_cast<PerChannelAffineQuantizer*>(quantizer.get())->axis();
 }
 
+// When input Tensor is non-dense, i.e. the allocated memory
+// is larger than the memory used by all the elements, we'll
+// convert it to dense tensor, otherwise we'll keep the memory
+// format of the output the same as input
 Tensor int_repr_quant(const Tensor& self) {
   Tensor dst;
   AT_DISPATCH_QINT_TYPES(self.scalar_type(), "int_repr", [&]() {
-    dst = at::empty(self.sizes(), self.options().dtype(UNDERLYING_TYPE));
-    dst.unsafeGetTensorImpl()->set_sizes_and_strides(self.sizes(), self.strides());
+    dst = at::empty(
+        self.sizes(),
+        self.options().dtype(UNDERLYING_TYPE),
+        self.suggest_memory_format());
     auto iter = TensorIterator();
     iter.add_output(dst);
     iter.add_input(self);
     iter.dont_compute_common_dtype();
     iter.build();
-    cpu_kernel(iter, [&](scalar_t value) -> underlying_t {
-       return value.val_;
-    });
+    cpu_kernel(iter, [](scalar_t value) -> underlying_t { return value.val_; });
   });
   return dst;
 }
