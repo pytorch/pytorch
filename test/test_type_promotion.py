b508aaf9e6999173851f5e78f3fb303edb69ef3e
Igor Fedan <ifedan@fb.com>
2019-09-25 17:52:33 -0400

    Changes based on PR review

--- test/test_type_promotion.py
+++ test/test_type_promotion.py
@@ -271,9 +271,9 @@ class TestTypePromotion(TestCase):
     def test_lt_with_type_promotion(self):
         for dt in torch.testing.get_all_math_dtypes(self.device):
             actual = torch.tensor([0], dtype=dt) < 0.5
-            expected = torch.tensor([1], dtype=dt)
+            expected = torch.tensor([True], dtype=dt)
             self.assertTrue(actual, expected)
-            self.assertTrue(actual.dtype == dt)
+            self.assertTrue(actual.dtype == torch.bool)
 
 @unittest.skipIf(not torch.cuda.is_available(), "no cuda")
 class TestTypePromotionCuda(TestTypePromotion):
