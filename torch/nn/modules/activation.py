cefc4d5c4fa03ae77305cfba4628559c92bb5f6a
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-25 10:47:47 -0700

    Fix DDP incompatibility issue with nn.MultiheadAttention.

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -687,12 +687,13 @@ class MultiheadAttention(Module):
         self.head_dim = embed_dim // num_heads
         assert self.head_dim * num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
 
-        self.in_proj_weight = Parameter(torch.empty(3 * embed_dim, embed_dim))
-
         if self._qkv_same_embed_dim is False:
             self.q_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))
             self.k_proj_weight = Parameter(torch.Tensor(embed_dim, self.kdim))
             self.v_proj_weight = Parameter(torch.Tensor(embed_dim, self.vdim))
+            self.register_parameter('in_proj_weight', None)
+        else:
+            self.in_proj_weight = Parameter(torch.empty(3 * embed_dim, embed_dim))
 
         if bias:
             self.in_proj_bias = Parameter(torch.empty(3 * embed_dim))
