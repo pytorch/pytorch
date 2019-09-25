9573c768757f626a64ec6ea811e13eb2cba8ba71
Mikhail Zolotukhin <mvz@fb.com>
2019-09-24 17:51:21 -0700

    Update on "Improve error message in IR parser when accessing undefined variable."
    Differential Revision: [D17562853](https://our.internmc.facebook.com/intern/diff/D17562853)
    
    [ghstack-poisoned]

--- torch/csrc/jit/irparser.cpp
+++ torch/csrc/jit/irparser.cpp
@@ -55,6 +55,8 @@ class IRParser {
       int end,
       const std::function<void()>& callback);
 
+  Value* findValueInVMap(const std::string& name);
+
   torch::jit::script::Lexer L;
   torch::jit::Graph* g = nullptr;
   std::unordered_map<std::string, Value*>& vmap;
@@ -255,11 +257,7 @@ void IRParser::parseOperatorInputs(Node* n) {
   }
   parseList('(', ',', ')', [&] {
     std::string var_name = parseVar();
-    if (!vmap.count(var_name)) {
-      throw ErrorReport(L.cur().range)
-          << "Cannot find a variable with name '" << var_name << "'";
-    }
-    n->addInput(vmap[var_name]);
+    n->addInput(findValueInVMap(var_name));
   });
 }
 
@@ -285,11 +283,7 @@ void IRParser::parseBlockOutputs(Block* b) {
   L.expect(TK_ARROW);
   parseList('(', ',', ')', [&] {
     std::string var_name = parseVar();
-    if (!vmap.count(var_name)) {
-      throw ErrorReport(L.cur().range)
-          << "Cannot find a variable with name '" << var_name << "'";
-    }
-    b->registerOutput(vmap[var_name]);
+    b->registerOutput(findValueInVMap(var_name));
   });
   L.expect(TK_NEWLINE);
   L.expect(TK_DEDENT);
@@ -391,13 +385,7 @@ void IRParser::parseReturnOperator() {
   // Parse output names and types
   parseList('(', ',', ')', [&] {
     std::string var_name = parseVar();
-    // Outputs should already be in VMAP, otherwise we're trying to return
-    // undefined value.
-    if (!vmap.count(var_name)) {
-      throw ErrorReport(L.cur().range)
-          << "Cannot find a variable with name '" << var_name << "'";
-    }
-    g->registerOutput(vmap.at(var_name));
+    g->registerOutput(findValueInVMap(var_name));
   });
 
   // Consume ending tokens
@@ -448,6 +436,15 @@ void IRParser::parseList(
     L.expect(end);
   }
 }
+
+Value* IRParser::findValueInVMap(const std::string& name) {
+  if (!vmap.count(name)) {
+    throw ErrorReport(L.cur().range)
+        << "Cannot find a variable with name '" << name << "'";
+  }
+  return vmap.at(name);
+}
+
 } // namespace script
 } // namespace jit
 } // namespace torch
