39e9ca388d3b08777e2cb6a13f5d4bdbbed738a5
Edward Z. Yang <ezyang@fb.com>
2019-09-25 14:46:40 -0400

    Switch nightly jobs to trigger on 'nightly' branch rather than cron.
    Fixes #26817
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- .circleci/cimodel/data/binary_build_definitions.py
+++ .circleci/cimodel/data/binary_build_definitions.py
@@ -62,6 +62,7 @@ class Conf(object):
         job_def["name"] = self.gen_build_name(phase)
         job_def["build_environment"] = miniutils.quote(" ".join(self.gen_build_env_parms()))
         job_def["requires"] = ["setup"]
+        job_def["filters"] = {"branches": {"only": "nightly"}}
         if self.libtorch_variant:
             job_def["libtorch_variant"] = miniutils.quote(self.libtorch_variant)
         if phase == "test":
@@ -121,18 +122,6 @@ def predicate_exclude_nonlinux_and_libtorch(config):
     return config.os == "linux"
 
 
-def gen_schedule_tree(cron_timing):
-    return [{
-        "schedule": {
-            "cron": miniutils.quote(cron_timing),
-            "filters": {
-                "branches": {
-                    "only": ["master"],
-                },
-            },
-        },
-    }]
-
 def get_nightly_uploads():
     configs = gen_build_env_list(False)
     mylist = []
@@ -165,7 +154,6 @@ def add_jobs_and_render(jobs_dict, toplevel_key, smoke, cron_schedule):
         jobs_list.append(build_config.gen_workflow_job(phase))
 
     jobs_dict[toplevel_key] = OrderedDict(
-        triggers=gen_schedule_tree(cron_schedule),
         jobs=jobs_list,
     )
 
