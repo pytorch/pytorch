d5490c662ef8d2255b8129d77e7d3ac171b17a75
Jongsoo Park <jongsoo@fb.com>
2019-09-26 16:05:40 -0700

    batch size 0 tests in BatchMatMul ops (#26874)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26874
    
    Add batch_size == 0 testings of BatchMatMul DNNLOWP operator.
    
    Test Plan: CI
    
    Reviewed By: jianyuh
    
    Differential Revision: D17596117
    
    fbshipit-source-id: 029e29e6c2bd7894d83dac46e8ce8484cc92b1c0

--- caffe2/utils/math_cpu.cc
+++ caffe2/utils/math_cpu.cc
@@ -272,8 +272,9 @@ C10_EXPORT void Gemm<float, CPUContext>(
     float* C,
     CPUContext* /*context*/,
     TensorProto::DataType /*math_type*/) {
-  const int lda = (trans_A == CblasNoTrans) ? K : M;
-  const int ldb = (trans_B == CblasNoTrans) ? N : K;
+  // MKL expects ld? >= 1
+  const int lda = std::max((trans_A == CblasNoTrans) ? K : M, 1);
+  const int ldb = std::max((trans_B == CblasNoTrans) ? N : K, 1);
   cblas_sgemm(
       CblasRowMajor,
       trans_A,
@@ -367,9 +368,10 @@ C10_EXPORT void GemmBatched<float, CPUContext>(
     TensorProto::DataType /* math_type */) {
 #ifdef CAFFE2_USE_MKL
   (void)context;
-  const int lda = (trans_A == CblasNoTrans) ? K : M;
-  const int ldb = (trans_B == CblasNoTrans) ? N : K;
-  const int ldc = N;
+  // MKL expects ld? >= 1
+  const int lda = std::max((trans_A == CblasNoTrans) ? K : M, 1);
+  const int ldb = std::max((trans_B == CblasNoTrans) ? N : K, 1);
+  const int ldc = std::max(N, 1);
   cblas_sgemm_batch(
       CblasRowMajor,
       &trans_A,
@@ -416,9 +418,10 @@ C10_EXPORT void GemmStridedBatched<float, CPUContext>(
     TensorProto::DataType /* math_type */) {
 #ifdef CAFFE2_USE_MKL
   (void)context;
-  const int lda = (trans_A == CblasNoTrans) ? K : M;
-  const int ldb = (trans_B == CblasNoTrans) ? N : K;
-  const int ldc = N;
+  // MKL expects ld? >= 1
+  const int lda = std::max((trans_A == CblasNoTrans) ? K : M, 1);
+  const int ldb = std::max((trans_B == CblasNoTrans) ? N : K, 1);
+  const int ldc = std::max(N, 1);
   std::vector<const float*> A_array(batch_size);
   std::vector<const float*> B_array(batch_size);
   std::vector<float*> C_array(batch_size);
@@ -427,6 +430,8 @@ C10_EXPORT void GemmStridedBatched<float, CPUContext>(
     B_array[i] = B + i * B_stride;
     C_array[i] = C + i * C_stride;
   }
+  LOG(INFO) << "M " << M << " N " << N << " K " << K << " lda " << lda
+            << " ldb " << ldb << " ldc " << ldc << " batch_size " << batch_size;
   cblas_sgemm_batch(
       CblasRowMajor,
       &trans_A,
