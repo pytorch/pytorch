ac1d9fddbfb39be5b3cb5f29326b75de51adb25b
Rohan Varma <rvarm1@fb.com>
2019-09-26 15:54:33 -0700

    Update on "[pytorch] Add clip_grad_norm_ to c++ api"
    Per https://github.com/pytorch/pytorch/issues/25883, we want to work towards C++/Python API parity. This diff adds `clip_grad_norm_` to the C++ API to improve parity. 
    
    The implementation and tests are ported over directly from the python api (see https://pytorch.org/docs/stable/_modules/torch/nn/utils/clip_grad.html)
    
    
    Differential Revision: [D17312367](https://our.internmc.facebook.com/intern/diff/D17312367/)
    
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -828,6 +828,7 @@ if __name__ == '__main__':
                 'include/torch/csrc/api/include/torch/nn/modules/*.h',
                 'include/torch/csrc/api/include/torch/nn/modules/container/*.h',
                 'include/torch/csrc/api/include/torch/nn/parallel/*.h',
+                'include/torch/csrc/api/include/torch/nn/utils/*.h',
                 'include/torch/csrc/api/include/torch/optim/*.h',
                 'include/torch/csrc/api/include/torch/serialize/*.h',
                 'include/torch/csrc/autograd/*.h',
