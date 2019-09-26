81cbc64cb3ada87bb3840809d71b9a0e424be974
Edward Z. Yang <ezyang@fb.com>
2019-09-26 17:27:44 -0400

    Separate libtorch tests from libtorch build.
    When we build a "normal" copy of PyTorch, we internally build a copy
    of libtorch.  If we want to test libtorch: we have a choice:
    test against the regular PyTorch build, or test against the libtorch
    only build.  All of our libtorch tests require Python-side PyTorch
    to run.  So it makes more sense to test the regular PyTorch build.
    
    There is probably still utility in making sure that it is still
    possible to build libtorch only, but in that case we should endeavour
    to run tests that ONLY require libtorch build, and not Python side
    stuff.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -138,37 +138,32 @@ test_torchvision() {
 }
 
 test_libtorch() {
-  if [[ "$BUILD_TEST_LIBTORCH" == "1" ]]; then
-    echo "Testing libtorch"
-    python test/cpp/jit/tests_setup.py setup
-    CPP_BUILD="$PWD/../cpp-build"
-    if [[ "$BUILD_ENVIRONMENT" == *cuda* ]]; then
-      "$CPP_BUILD"/caffe2/build/bin/test_jit
-    else
-      "$CPP_BUILD"/caffe2/build/bin/test_jit "[cpu]"
-    fi
-    python test/cpp/jit/tests_setup.py shutdown
-    python tools/download_mnist.py --quiet -d test/cpp/api/mnist
-    OMP_NUM_THREADS=2 TORCH_CPP_TEST_MNIST_PATH="test/cpp/api/mnist" "$CPP_BUILD"/caffe2/build/bin/test_api
-    assert_git_not_dirty
+  echo "Testing libtorch"
+  python test/cpp/jit/tests_setup.py setup
+  if [[ "$BUILD_ENVIRONMENT" == *cuda* ]]; then
+    build/bin/test_jit
+  else
+    build/bin/test_jit "[cpu]"
   fi
+  python test/cpp/jit/tests_setup.py shutdown
+  python tools/download_mnist.py --quiet -d test/cpp/api/mnist
+  OMP_NUM_THREADS=2 TORCH_CPP_TEST_MNIST_PATH="test/cpp/api/mnist" build/bin/test_api
+  assert_git_not_dirty
 }
 
 test_custom_script_ops() {
-  if [[ "$BUILD_TEST_LIBTORCH" == "1" ]]; then
-    echo "Testing custom script operators"
-    CUSTOM_OP_BUILD="$PWD/../custom-op-build"
-    pushd test/custom_operator
-    cp -a "$CUSTOM_OP_BUILD" build
-    # Run tests Python-side and export a script module.
-    python test_custom_ops.py -v
-    python test_custom_classes.py -v
-    python model.py --export-script-module=model.pt
-    # Run tests C++-side and load the exported script module.
-    build/test_custom_ops ./model.pt
-    popd
-    assert_git_not_dirty
-  fi
+  echo "Testing custom script operators"
+  CUSTOM_OP_BUILD="$PWD/../custom-op-build"
+  pushd test/custom_operator
+  cp -a "$CUSTOM_OP_BUILD" build
+  # Run tests Python-side and export a script module.
+  python test_custom_ops.py -v
+  python test_custom_classes.py -v
+  python model.py --export-script-module=model.pt
+  # Run tests C++-side and load the exported script module.
+  build/test_custom_ops ./model.pt
+  popd
+  assert_git_not_dirty
 }
 
 test_xla() {
