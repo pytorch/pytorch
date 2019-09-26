e2257d3ae661c719f3f5942fdd26ad9f50470c40
BowenBao <bowbao@microsoft.com>
2019-09-26 16:21:12 -0700

    Export index_fill and index_copy, fix caffe2 scatter
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/23052
    
    Reviewed By: hl475
    
    Differential Revision: D16428486
    
    Pulled By: houseroad
    
    fbshipit-source-id: 8c5905052763fd70197c67aba5f28eeff0790721

--- torch/onnx/symbolic_opset8.py
+++ torch/onnx/symbolic_opset8.py
@@ -41,7 +41,8 @@ import warnings
 
 black_listed_operators = [
     "nonzero", "where", "scatter", "scatter_add", "erf", "sign", "isnan", "gather",
-    "arange", "masked_fill"
+    "arange", "masked_fill",
+    "index_fill", "index_copy"
 ]
 
 for black_listed_op in black_listed_operators:
