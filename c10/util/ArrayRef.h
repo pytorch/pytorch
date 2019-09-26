0cb8af361349236bbc41d0ca9e9511f3a7df5977
Sebastian Messmer <messmer@fb.com>
2019-09-26 14:55:22 -0700

    Switch internal CUDA build to C++14 (#26757)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26757
    
    This doesn't switch any open source builds or CI.
    The internal fbcode build is C++17 already for quite some time, but in CUDA code, we had it restricted to C++11.
    This diff changes that to C++14.
    
    Because this doesn't change anything open source, the risk of this is low.
    ghstack-source-id: 90728524
    
    Test Plan: waitforsandcastle
    
    Differential Revision: D17558142
    
    fbshipit-source-id: 9cfd47e38e71d5a2fdae2f535c01f281bf007d9a

--- c10/util/ArrayRef.h
+++ c10/util/ArrayRef.h
@@ -145,13 +145,13 @@ class ArrayRef final {
   }
 
   /// front - Get the first element.
-  AT_CPP14_CONSTEXPR const T& front() const {
+  C10_CPP14_HOST_CONSTEXPR const T& front() const {
     TORCH_CHECK(!empty(), "ArrayRef: attempted to access front() of empty list");
     return Data[0];
   }
 
   /// back - Get the last element.
-  AT_CPP14_CONSTEXPR const T& back() const {
+  C10_CPP14_HOST_CONSTEXPR const T& back() const {
     TORCH_CHECK(!empty(), "ArrayRef: attempted to access back() of empty list");
     return Data[Length - 1];
   }
@@ -163,7 +163,7 @@ class ArrayRef final {
 
   /// slice(n, m) - Chop off the first N elements of the array, and keep M
   /// elements in the array.
-  AT_CPP14_CONSTEXPR ArrayRef<T> slice(size_t N, size_t M) const {
+  C10_CPP14_HOST_CONSTEXPR ArrayRef<T> slice(size_t N, size_t M) const {
     TORCH_CHECK(
         N + M <= size(),
         "ArrayRef: invalid slice, N = ",
@@ -188,7 +188,7 @@ class ArrayRef final {
   }
 
   /// Vector compatibility
-  AT_CPP14_CONSTEXPR const T& at(size_t Index) const {
+  C10_CPP14_HOST_CONSTEXPR const T& at(size_t Index) const {
     TORCH_CHECK(
         Index < Length,
         "ArrayRef: invalid index Index = ",
