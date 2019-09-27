f9253a64ef24523dec22ec355021bc8b433fa0d3
Shihao Xu <shihaoxu@fb.com>
2019-09-27 01:08:28 -0700

    RPC Backend Registry (#26919)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26919
    
    Make the names more systematic
    
    ghstack-source-id: 90870882
    
    Differential Revision: D17262797
    
    fbshipit-source-id: 5a2e513a0d0cca5b699b40cbf530f51776392a2a

--- torch/distributed/rpc.py
+++ torch/distributed/rpc.py
@@ -5,7 +5,7 @@ from . import init_rref_context
 from . import ProcessGroupAgent
 from . import WorkerId
 from .internal_rpc_utils import serialize, PythonUDF
-from .rpc_backend_handler import is_backend_registered, registered_init_rpc
+from .rpc_backend_registry import is_rpc_backend_registered, init_rpc_backend
 
 import sys
 import torch
@@ -76,11 +76,12 @@ def _init_rpc(backend=RpcBackend.PROCESS_GROUP,
         # TODO: add try-except and destroy _agent in all processes if any fails.
         _agent = ProcessGroupAgent(self_name, group, num_send_recv_threads)
         init_rref_context(_agent)
-    elif is_backend_registered(backend):
-        _agent = registered_init_rpc(backend,
-                                     self_rank=self_rank,
-                                     self_name=self_name,
-                                     init_method=init_method)
+    elif is_rpc_backend_registered(backend):
+        _agent = init_rpc_backend(
+            backend,
+            self_name=self_name,
+            init_method=init_method,
+        )
         init_rref_context(_agent)
     else:
         raise RuntimeError("Unrecognized RPC backend ", backend)
