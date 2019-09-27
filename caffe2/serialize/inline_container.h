663875c28c1aacdaa268cb72b0ee953638488ee6
Basil Hosmer <bhosmer@fb.com>
2019-09-27 16:41:01 -0700

    Add some extra info to PyTorchStreamReader/Writer errors
    Prompted by the lack of filename info in the error message for
    torch.jit.save() to an unwriteable location... looks like most
    serialization code paths have upstream checks that include more
    info, but adding the extra info here as well seemed simple and
    cheap enough, so went ahead and did it more generally.
    
    [ghstack-poisoned]

--- caffe2/serialize/inline_container.h
+++ caffe2/serialize/inline_container.h
@@ -111,7 +111,7 @@ class CAFFE2_API PyTorchStreamReader final {
  private:
   void init();
   size_t read(uint64_t pos, char* buf, size_t n);
-  void valid(const char* what);
+  void valid(const char* what, const char* info = "");
   size_t getRecordID(const std::string& name);
 
   friend size_t
@@ -141,7 +141,7 @@ class CAFFE2_API PyTorchStreamWriter final {
   ~PyTorchStreamWriter();
 
  private:
-   void valid(const char* what);
+   void valid(const char* what, const char* info = "");
    size_t current_pos_ = 0;
    std::unique_ptr<mz_zip_archive> ar_;
    std::string archive_name_;
