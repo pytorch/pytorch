4b6b12949a4d0f98d43bcaa5045255a441df7c88
Negin Raoof <neginmr@utexas.edu>
2019-09-26 22:19:47 -0700

    flake8 fix

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -38,7 +38,7 @@ def ort_test_with_input(ort_sess, input, output, rtol, atol):
 
     # compare onnxruntime and PyTorch results
     assert len(outputs) == len(ort_outs), "number of outputs differ"
-    
+
     # compare onnxruntime and PyTorch results
     [np.testing.assert_allclose(out, ort_out, rtol=rtol, atol=atol) for out, ort_out in zip(outputs, ort_outs)]
 
