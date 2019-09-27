e3b0b658c831273bf6da7ade233833c302e32dca
Shen Li <cs.shenli@gmail.com>
2019-09-27 15:10:03 -0700

    Acquire GIL before creating py::object in RPC python handler
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/26988
    
    Test Plan: Imported from OSS
    
    Differential Revision: D17635297
    
    Pulled By: mrshenli
    
    fbshipit-source-id: 43c93e44fe0dceba9a41a292c53a665c612843e9

--- torch/csrc/distributed/rpc/python_functions.cpp
+++ torch/csrc/distributed/rpc/python_functions.cpp
@@ -53,7 +53,12 @@ py::object toPyObj(const Message& message) {
       ScriptRet ret = ScriptRet::fromMessage(message);
       Stack stack;
       stack.push_back(ret.value());
-      return torch::jit::createPyObjectForStack(std::move(stack));
+      {
+        AutoGIL ag;
+        // The createPyObjectForStack does not acquire GIL, but creating a new
+        // py::object requires GIL.
+        return torch::jit::createPyObjectForStack(std::move(stack));
+      }
     }
     case MessageType::PYTHON_RET: {
       return PythonRpcHandler::getInstance().loadPythonUDFResult(message);
