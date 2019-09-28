pytorch/pytorch

commit 3a2e88ba93da6762e63a03960e0647986da481fc

Use std::mutex instead of std::call_once in Function when we initialize GraphExecutor. (#26571)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/26571

We will need a mutex for computing optimized graph too, which will be
implemented in subsequent commits.

Test Plan: Imported from OSS

Differential Revision: D17510883

Pulled By: ZolotukhinM

fbshipit-source-id: 273b25426785e50f67a103204de98f6ed14182db

torch/csrc/jit/function.h
@@ -2,6 +2,7 @@
 #include <torch/csrc/jit/graph_executor.h>
 #include <torch/csrc/jit/ir.h>
 #include <torch/csrc/utils/memory.h>
+#include <mutex>
 
 namespace torch {
 namespace jit {
@@ -87,10 +88,12 @@ struct TORCH_API Function {
 
   GraphExecutor& get_executor() {
     ensure_defined();
-    std::call_once(executor_init_, [&] {
-      check_single_output();
-      executor_ = GraphExecutor(graph());
-    });
+    if (executor_) {
+      return executor_;
+    }
+    std::lock_guard<std::mutex> lock(compile_mutex);
+    check_single_output();
+    executor_ = GraphExecutor(graph());
     return executor_;
   }
 
@@ -98,9 +101,11 @@ struct TORCH_API Function {
   c10::QualifiedName name_;
   std::shared_ptr<Graph> graph_; // for debugging and for inlining
 
-  GraphExecutor executor_; // for execution
+  // Functions are invokable from multiple threads, so this lock needs to be
+  // held when we're initializing graph executor for the first time.
+  mutable std::mutex compile_mutex;
 
-  std::once_flag executor_init_;
+  GraphExecutor executor_; // for execution
 
   // an optional function that actually creates the method when
   // ensure_defined() is called. This is used by the compiler so

commit 904426a11beac20fcdf268ea90a2c48c8303c6cd

Fix race condition in torch::jit::Function
Summary:
JIT can be called concurrently from two threads, so even the read from GraphExecutor has to be guarded by the lock.

This was a recent regression introduced by https://github.com/pytorch/pytorch/pull/26571/files#diff-40af5094abe4f522e8a78adb591dde19

Reviewed By: jamesr66a, wanchaol

Differential Revision: D17645407

fbshipit-source-id: 0244437b0b3e4ba30f5c518971195692f89d6934

torch/csrc/jit/function.h
@@ -94,10 +94,10 @@ struct TORCH_API Function {
 
   GraphExecutor& get_executor() {
     ensure_defined();
+    std::lock_guard<std::recursive_mutex> lock(compile_mutex);
     if (executor_) {
       return executor_;
     }
-    std::lock_guard<std::recursive_mutex> lock(compile_mutex);
     check_single_output();
     executor_ = GraphExecutor(optimized_graph());
     return executor_;