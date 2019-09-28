771c5d0caa0250bd0e6dd57df80dedf02c2383c2
Chris Gottbrath <gottbrath@fb.com>
2019-09-28 13:23:44 -0700

    fixes for a variety of docstring formatting issues. Issues 26464, 26469, 26470, 26834.

--- torch/quantization/quantize.py
+++ torch/quantization/quantize.py
@@ -109,12 +109,10 @@ def add_observer_(module):
     has a valid qconfig attribute.
 
     Args:
-        module: input module with qconfig attributes for all the leaf modules
-        that we want to quantize
+        module: input module with qconfig attributes for all the leaf modules that we want to quantize
 
     Return:
-        None, module is modified inplace with added observer modules and
-            forward_hooks
+        None, module is modified inplace with added observer modules and forward_hooks
     """
     for child in module.children():
         if type(child) == nnq.FloatFunctional:
@@ -162,7 +160,7 @@ def add_quant_dequant(module):
 
     Args:
         module: input module with qconfig attributes for all the leaf modules
-        that we want to quantize
+                that we want to quantize
 
     Return:
         Either the inplace modified module with submodules wrapped in
@@ -343,10 +341,10 @@ def quantize_qat(model, run_fn, run_args, inplace=False):
 def convert(module, mapping=DEFAULT_MODULE_MAPPING, inplace=False):
     r"""Converts the float module with observers (where we can get quantization
     parameters) to a quantized module.
+
     Args:
         module: calibrated module with observers
-        mapping: a dictionary that maps from float module type to quantized
-           module type, can be overwrritten to allow swapping user defined Modules
+        mapping: a dictionary that maps from float module type to quantized module type, can be overwrritten to allow swapping user defined Modules
         inplace: carry out model transformations in-place, the original module is mutated
     """
     if not inplace:
