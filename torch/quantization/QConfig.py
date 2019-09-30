a8083e18e87bb376d5d38b4ca705382bd87b51f5
Raghuraman Krishnamoorthi <raghuraman@fb.com>
2019-09-30 00:35:23 -0700

    Default observer and fake-quant for backends (#26627)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26627
    
    ghstack-source-id: 91008337
    
    Test Plan: buck test caffe2/test:quantization -- --print-passing-details
    
    Differential Revision: D17518194
    
    fbshipit-source-id: 1eb8a7a85dc811c4ee5228d68563abb157613ceb

--- torch/quantization/QConfig.py
+++ torch/quantization/QConfig.py
@@ -9,9 +9,11 @@ class QConfig(namedtuple('QConfig', ['activation', 'weight'])):
     Describes how to quantize a layer or a part of the network by providing
     settings (observer classes) for activations and weights respectively.
 
+
     Note that QConfig needs to contain observer **classes** (like MinMaxObserver) or a callable that returns
     instances on invocation, not the concrete observer instances themselves.
     Quantization preparation function will instantiate observers multiple times for each of the layers.
+
 
     Observer classes have usually reasonable default arguments, but they can be overwritten with `with_args`
     method (that behaves like functools.partial):
@@ -69,3 +71,33 @@ default_weight_only_quant_qconfig = QConfig(activation=torch.nn.Identity,
                                             weight=default_weight_fake_quant)
 default_activation_only_quant_qconfig = QConfig(activation=default_fake_quant,
                                                 weight=torch.nn.Identity)
+
+def get_default_qconfig(backend='fbgemm'):
+    if backend == 'fbgemm':
+        qconfig = QConfig(activation=HistogramObserver.with_args(reduce_range=True),
+                          weight=default_per_channel_weight_observer)
+    elif backend == 'qnnpack':
+        qconfig = QConfig(activation=HistogramObserver.with_args(reduce_range=False),
+                          weight=default_weight_observer)
+    else:
+        raise ValueError("Unknown backend, please specify qconfig manually")
+    return qconfig
+
+def get_default_qat_qconfig(backend='fbgemm'):
+    # Histogram observer is too slow for quantization aware training
+    if backend == 'fbgemm':
+        qconfig = QConfig(activation=FakeQuantize.with_args(observer=MinMaxObserver,
+                                                            quant_min=0,
+                                                            quant_max=255,
+                                                            reduce_range=True),
+                          weight=default_per_channel_weight_fake_quant)
+    elif backend == 'qnnpack':
+        qconfig = QConfig(activation=FakeQuantize.with_args(observer=MinMaxObserver,
+                                                            quant_min=0,
+                                                            quant_max=255,
+                                                            reduce_range=False),
+                          weight=default_weight_fake_quant)
+    else:
+        raise ValueError("Unknown backend, please specify qconfig manually")
+
+    return qconfig
