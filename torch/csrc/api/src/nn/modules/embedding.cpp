18f6a6e802827c788760d68d40b28fdcd8b684bd
Will Feng <willfeng@fb.com>
2019-10-01 13:11:24 -0400

    try to fix Windows build error C2338: You are trying to default construct a module which has no default constructor

--- torch/csrc/api/src/nn/modules/embedding.cpp
+++ torch/csrc/api/src/nn/modules/embedding.cpp
@@ -90,7 +90,7 @@ namespace nn {
       } else {
         options = EmbeddingOptions(embeddings.size(0), embeddings.size(1));
       }
-      Embedding embedding = Embedding((*options)._weight(embeddings));
+      Embedding embedding((*options)._weight(embeddings));
       embedding->weight.set_requires_grad(!freeze);
       return embedding;
     }
@@ -189,7 +189,7 @@ namespace nn {
       } else {
         options = EmbeddingBagOptions(embeddings.size(0), embeddings.size(1));
       }
-      EmbeddingBag embeddingbag = EmbeddingBag((*options)._weight(embeddings));
+      EmbeddingBag embeddingbag((*options)._weight(embeddings));
       embeddingbag->weight.set_requires_grad(!freeze);
       return embeddingbag;
     }
