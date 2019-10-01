1688902310fefd4bb06867acf0f961c48058c32d
Jerry Zhang <jerryzh@fb.com>
2019-10-01 12:08:30 -0700

    Enable tests (#27103)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27103
    
    att
    
    Test Plan:
    python test/test_quantization.py 'GraphModePostTrainingQuantTest'
    
    Imported from OSS
    
    Differential Revision: D17678261
    
    fbshipit-source-id: 5caa7512c6ff4a613980c86b5b221e0cfbe0a173

--- test/test_quantization.py
+++ test/test_quantization.py
@@ -644,15 +644,20 @@ class EagerModeQuantizationAwareTrainingTest(QuantizationTestCase):
     " Quantized operations require FBGEMM. FBGEMM is only optimized for CPUs"
     " with instruction set support avx2 or newer.",
 )
-@unittest.skip("temoprarily disable the test - enable after known issues are fixed")
 class GraphModePostTrainingQuantTest(QuantizationTestCase):
     @_tmp_donotuse_dont_inline_everything
     def test_single_layer(self):
-        r"""Quantize SingleLayerLinearModel which has one Linear module, make sure it is swapped
-        to nnq.Linear which is the quantized version of the module
+        r"""Compare the result of quantizing single linear layer in
+        eager mode and graph mode
         """
         # eager mode
-        model_eager = quantize(AnnotatedSingleLayerLinearModel(), test_only_eval_fn,
+        annotated_linear_model = AnnotatedSingleLayerLinearModel()
+        linear_model = SingleLayerLinearModel()
+        # copy the weight from eager mode so that we can
+        # compare the result of the two quantized models later
+        linear_model.fc1.weight = torch.nn.Parameter(annotated_linear_model.fc1.module.weight.detach())
+        linear_model.fc1.bias = torch.nn.Parameter(annotated_linear_model.fc1.module.bias.detach())
+        model_eager = quantize(annotated_linear_model, test_only_eval_fn,
                                self.calib_data)
 
         qconfig_dict = {
@@ -661,11 +666,13 @@ class GraphModePostTrainingQuantTest(QuantizationTestCase):
                 weight=default_weight_observer)
         }
         model_script = quantize_script(
-            torch.jit.script(SingleLayerLinearModel()),
+            torch.jit.script(linear_model),
             qconfig_dict,
             test_only_eval_fn,
-            [self.calib_data])
+            [self.calib_data],
+            inplace=False)
         result_eager = model_eager(self.calib_data[0][0])
+        torch._C._jit_pass_quant_fusion(model_script._c._get_module('fc1')._get_method('forward').graph)
         result_script = model_script._c._get_method('forward')(self.calib_data[0][0])
         self.assertEqual(result_eager, result_script)
 
