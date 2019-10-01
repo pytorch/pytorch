02ad4a5c43cfeed3352fedde0cabf44dd594a12b
Shen Li <cs.shenli@gmail.com>
2019-10-01 13:22:10 -0700

    Rename distributed autograd test to avoid "test" prefix
    If the base class starts with "Test", pytest would detect it as a
    test class to run. As a result, when I execute the following
    command, I run into test failures:
    
    py.test test_dist_autograd_fork.py -k test -vs
    
    outputs:
    
    test/test_dist_autograd_fork.py::TestDistAutograd::test_autograd_context FAILED
    test/test_dist_autograd_fork.py::TestDistAutograd::test_autograd_send_function FAILED
    test/test_dist_autograd_fork.py::TestDistAutograd::test_rpc_complex_args FAILED
    test/test_dist_autograd_fork.py::DistAutogradTestWithFork::test_autograd_context PASSED
    test/test_dist_autograd_fork.py::DistAutogradTestWithFork::test_autograd_send_function PASSED
    test/test_dist_autograd_fork.py::DistAutogradTestWithFork::test_rpc_complex_args PASSED

--- test/test_dist_autograd_spawn.py
+++ test/test_dist_autograd_spawn.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from dist_autograd_test import TestDistAutograd
+from dist_autograd_test import DistAutogradTest
 from common_distributed import MultiProcessTestCase
 from common_utils import TEST_WITH_ASAN, run_tests
 
 import unittest
 
 @unittest.skipIf(TEST_WITH_ASAN, "Skip ASAN as torch + multiprocessing spawn have known issues")
-class TestDistAutogradWithSpawn(MultiProcessTestCase, TestDistAutograd):
+class DistAutogradWithSpawnTest(MultiProcessTestCase, DistAutogradTest):
 
     def setUp(self):
-        super(TestDistAutogradWithSpawn, self).setUp()
+        super(DistAutogradWithSpawnTest, self).setUp()
         self._spawn_processes()
 
 if __name__ == '__main__':
