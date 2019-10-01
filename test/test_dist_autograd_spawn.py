c436975b6c301bc8b64f0147a32d2da06e645381
Shen Li <cs.shenli@gmail.com>
2019-10-01 14:44:24 -0700

    Enable distributed autograd tests for >py36
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/27166
    
    Test Plan: Imported from OSS
    
    Reviewed By: zhaojuanmao
    
    Differential Revision: D17694691
    
    Pulled By: mrshenli
    
    fbshipit-source-id: 6e7b740645891fd3cc67600de26346f7b336773b

--- test/test_dist_autograd_spawn.py
+++ test/test_dist_autograd_spawn.py
@@ -3,11 +3,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from dist_autograd_test import TestDistAutograd
 from common_distributed import MultiProcessTestCase
-from common_utils import run_tests
+from common_utils import TEST_WITH_ASAN, run_tests
 
 import unittest
 
-@unittest.skip("Test is flaky, see https://github.com/pytorch/pytorch/issues/27157")
+@unittest.skipIf(TEST_WITH_ASAN, "Skip ASAN as torch + multiprocessing spawn have known issues")
 class TestDistAutogradWithSpawn(MultiProcessTestCase, TestDistAutograd):
 
     def setUp(self):
