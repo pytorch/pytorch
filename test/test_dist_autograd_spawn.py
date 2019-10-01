ee11c8adc63bc9b4843ab24d4834022deba26eb3
Shen Li <cs.shenli@gmail.com>
2019-10-01 12:47:24 -0700

    Update on "Disable flaky distributed autograd test under spawn mode"
    Differential Revision: [D17693184](https://our.internmc.facebook.com/intern/diff/D17693184)

--- test/test_dist_autograd_spawn.py
+++ test/test_dist_autograd_spawn.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from dist_autograd_test import TestDistAutograd
 from common_distributed import MultiProcessTestCase
-from common_utils import TEST_WITH_ASAN, run_tests
+from common_utils import run_tests
 
 import unittest
 
