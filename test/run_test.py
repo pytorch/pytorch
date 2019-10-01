d976e13ff8059e416c375f9531cc4dff5382da5c
Shen Li <cs.shenli@gmail.com>
2019-10-01 13:59:58 -0700

    Update on "Enable distributed autograd tests for >py36"
    Differential Revision: [D17694691](https://our.internmc.facebook.com/intern/diff/D17694691)

--- test/run_test.py
+++ test/run_test.py
@@ -66,27 +66,27 @@ TESTS = [
 
 # skip < 3.6 b/c fstrings added in 3.6
 if PY36:
-    TESTS.append('jit_py3', 'rpc_spawn', 'dist_autograd_spawn')
+    TESTS.append('dist_autograd_spawn', 'jit_py3', 'rpc_spawn')
 
 WINDOWS_BLACKLIST = [
+    'dist_autograd_fork',
+    'dist_autograd_spawn',
     'distributed',
     'rpc_fork',
     'rpc_spawn',
-    'dist_autograd_fork',
-    'dist_autograd_spawn',
 ]
 
 ROCM_BLACKLIST = [
     'c10d',
     'cpp_api_parity',
     'cpp_extensions',
+    'dist_autograd_fork',
+    'dist_autograd_spawn',
     'distributed',
     'multiprocessing',
     'nccl',
     'rpc_fork',
     'rpc_spawn',
-    'dist_autograd_fork',
-    'dist_autograd_spawn',
 ]
 
 DISTRIBUTED_TESTS_CONFIG = {}
