3b809830c4f064b56a3b43bcb13ec665b7e2a803
albanD <desmaison.alban@gmail.com>
2019-10-01 12:09:14 -0700

    Handle warning in torchscript
    [ghstack-poisoned]

--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -622,6 +622,7 @@ void initJitScriptBindings(PyObject* module) {
       .def(
           "__call__",
           [](py::args args, py::kwargs kwargs) {
+            HANDLE_TH_ERRORS
             // see: [pybind11 varargs]
             auto strongPtr = py::cast<StrongFunctionPtr>(args[0]);
             Function& callee = *strongPtr.function_;
@@ -635,6 +636,7 @@ void initJitScriptBindings(PyObject* module) {
               tracer::getTracingState()->graph->pop_scope();
             }
             return result;
+            END_HANDLE_TH_ERRORS_PYBIND
           })
       .def(
           "save",
