bb51980766182b42f631e3f156233d0fdcc881bc
Egor Peshkov <egor96@hotmail.com>
2019-10-02 11:27:58 -0700

    make default string arguments in schemas human readable (#27088)
    Summary:
    [jit] String default args get printed as ascii values https://github.com/pytorch/pytorch/issues/25804
    https://github.com/pytorch/pytorch/issues/25804
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27088
    
    Differential Revision: D17689732
    
    Pulled By: Krovatkin
    
    fbshipit-source-id: f385b2fe44c5a2387bfcb6484edf7faa92bc8edf

--- test/test_jit.py
+++ test/test_jit.py
@@ -19250,6 +19250,18 @@ class TestClassType(JitTestCase):
                 def set_non_initialized(self, y):
                     self.bar = y  # can't assign to non-initialized attr
 
+    def test_schema_human_readable(self):
+        """ 
+        Make sure that the schema is human readable, ie the mode parameter should read "nearest" instead of being displayed in octal
+        aten::__interpolate(Tensor input, int? size=None, float[]? scale_factor=None, 
+        str mode='\156\145\141\162\145\163\164', bool? align_corners=None) -> (Tensor): 
+        Expected a value of type 'Optional[int]' for argument 'size' but instead found type 'Tensor'.
+        """
+        with self.assertRaisesRegex(RuntimeError, "nearest"):
+            @torch.jit.script
+            def FooTest(x):
+                return torch.nn.functional.interpolate(x, 'bad')
+
     def test_type_annotations(self):
         with self.assertRaisesRegex(RuntimeError, "Expected a value of type \'bool"):
             @torch.jit.script  # noqa: B903
