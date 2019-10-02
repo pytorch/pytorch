dbe002f8bbd9f9e738785414dc2a67c90bf3aafc
Anjali Chourdia <chourdiaanjali@fb.com>
2019-10-02 15:39:45 -0400

    renaming output

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -906,34 +906,34 @@ Tensor binary_cross_entropy_double_backward(const Tensor & grad_output, const Te
   auto eps = 1e-12;
   auto inp_pl_eps = input + eps;
   auto one_m_inp_pl_eps = 1 - input + eps;
-  auto output = (input * input - 2 * input * target + target) / (inp_pl_eps.pow(2) * one_m_inp_pl_eps.pow(2));
-  output *= (grad * grad_output);
+  auto gI = (input * input - 2 * input * target + target) / (inp_pl_eps.pow(2) * one_m_inp_pl_eps.pow(2));
+  gI *= (grad * grad_output);
 
   if (weight.defined()) {
-    output *= weight;
+    gI *= weight;
   }
   if (reduction == Reduction::Mean) {
-    return output / input.numel();
+    return gI / input.numel();
   } else if (reduction == Reduction::Sum) {
-    return output.sum();
+    return gI.sum();
   }
-  return output;
+  return gI;
 }
 
 Tensor binary_cross_entropy_double_backward_grad_output(const Tensor & grad, const Tensor & input, const Tensor & target, const Tensor& weight, int64_t reduction) {
   auto eps = 1e-12;
-  auto output = (input - target) / ((input + eps) * (1 - input + eps));
-  output *= grad;
+  auto ggO = (input - target) / ((input + eps) * (1 - input + eps));
+  ggO *= grad;
 
   if (weight.defined()) {
-    output *= weight;
+    ggO *= weight;
   }
   if (reduction == Reduction::Mean) {
-    return output / input.numel();
+    return ggO / input.numel();
   } else if (reduction == Reduction::Sum) {
-    return output.sum();
+    return ggO.sum();
   }
-  return output;
+  return ggO;
 }
 
 Tensor l1_loss_double_backward_grad_output(const Tensor & grad, const Tensor & input, const Tensor & target, int64_t reduction) {
