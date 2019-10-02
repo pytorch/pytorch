5cac7387135ccf8db5b4b1ed9f52f80c924c2bc8
Pavel Belevich <pbelevich@fb.com>
2019-10-01 19:27:48 -0700

    C++ API parity: MaxUnpool2d
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/26915
    
    Test Plan: Imported from OSS
    
    Differential Revision: D17627826
    
    Pulled By: pbelevich
    
    fbshipit-source-id: 04a5a7e7d19b1610cafaaa0bd329d4d228ab4be5

--- torch/csrc/api/src/nn/modules/pooling.cpp
+++ torch/csrc/api/src/nn/modules/pooling.cpp
@@ -183,7 +183,13 @@ Tensor MaxUnpool1dImpl::forward(const Tensor& input, const Tensor& indices,
   return F::max_unpool1d(input, indices, options, output_size);
 }
 
+Tensor MaxUnpool2dImpl::forward(const Tensor& input, const Tensor& indices,
+    const c10::optional<IntArrayRef>& output_size) {
+  return F::max_unpool2d(input, indices, options, output_size);
+}
+
 template class MaxUnpoolImpl<1, MaxUnpool1dImpl>;
+template class MaxUnpoolImpl<2, MaxUnpool2dImpl>;
 
 } // namespace nn
 } // namespace torch
