2e91fc4462fae98c12fb2391944bfa7d534e7e2c
albanD <desmaison.alban@gmail.com>
2019-10-02 15:15:00 -0700

    Update on "Handle warning in torchscript"
    Fix for #25859
    
    
    Differential Revision: [D17692504](https://our.internmc.facebook.com/intern/diff/D17692504)
    
    [ghstack-poisoned]

--- torch/csrc/Exceptions.h
+++ torch/csrc/Exceptions.h
@@ -7,6 +7,7 @@
 
 #include <torch/csrc/THP_export.h>
 #include <torch/csrc/utils/auto_gil.h>
+#include <torch/csrc/jit/script/jit_exception.h>
 #include <torch/csrc/WindowsTorchApiMacro.h>
 #include "c10/util/StringUtil.h"
 #include "c10/util/Exception.h"
@@ -60,6 +61,12 @@
       e.set_error(); \
       throw;                                                 \
     }                                                                \
+    catch (torch::jit::JITException & e) {\
+      /* Special case for JITException that are explicitely unpacked by pybind */ \
+      /* Set a temporary python error to be detectable by warning code */ \
+      PyErr_SetString(PyExc_RuntimeError, "JITException");              \
+      throw; \
+    } \
     CATCH_TH_ERRORS(throw)                                          \
   }                                                                  \
   catch (py::error_already_set & e) {                                       \
@@ -70,6 +77,12 @@
     /* Repack already stored error */ \
     throw py::error_already_set();                                                 \
   }                                                                \
+  catch (torch::jit::JITException & e) {\
+    /* Special case for JITException that are explicitely unpacked by pybind */ \
+    /* Clear the temporary error message we used */ \
+    PyErr_Clear(); \
+    throw; \
+  } \
   CATCH_TH_ERRORS(throw py::error_already_set())
 
 #define END_HANDLE_TH_ERRORS_RET(retval)                             \
