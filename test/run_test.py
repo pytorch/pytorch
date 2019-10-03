812e7ead4b8ae1ac2f260503b98683cdadb4defa
Yanli Zhao <yanlizhao@fb.com>
2019-10-02 23:03:58 -0700

    skip all rpc and dist autograd spawn tests for <PY36 (#27191)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27191
    
    skip rpc and distautograd spawns tests for <python 3.6
    ghstack-source-id: 91231565
    
    close #27157
    
    Test Plan: unit tests
    
    Differential Revision: D17697368
    
    fbshipit-source-id: bb8cf1f47de41f9d350fd60afe37fece293d8680

--- test/run_test.py
+++ test/run_test.py
@@ -28,7 +28,6 @@ TESTS = [
     'cuda_primary_ctx',
     'dataloader',
     'dist_autograd_fork',
-    'dist_autograd_spawn',
     'distributed',
     'distributions',
     'docs_coverage',
@@ -64,12 +63,19 @@ TESTS = [
     'function_schema',
 ]
 
-# skip < 3.6 b/c fstrings added in 3.6
+# skip < 3.6 b/c fstrings added in 3.6 for jit_py3
+# skip < 3.6 for rpc_spawn and dist_autograd_spawn temporarily because
+# a segmenation fault was triggered on python 3.5,
+# rpc_spawn and dist_autograd_spawn tests were added in
+# https://github.com/pytorch/pytorch/pull/25656
+# skip < 3.6 for rpc_fork as it imports mock that is only available in 3.6, mock
+# was added to rpc_fork in https://github.com/pytorch/pytorch/pull/26997
 if PY36:
     TESTS.extend([
         'jit_py3',
         'rpc_fork',
         'rpc_spawn',
+        'dist_autograd_spawn',
     ])
 
 WINDOWS_BLACKLIST = [
