3ed42f85ab3a6db33003eff307216af3bb010246
davidriazati <davidriazati@fb.com>
2019-10-04 12:28:11 -0700

    Fix some return std::move warnings (#27384)
    Summary:
    clang-tidy was complaining about these
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27384
    
    Pulled By: driazati
    
    Differential Revision: D17767412
    
    fbshipit-source-id: 03e2630790edf3f6bbf9064e754156613032b464

--- torch/csrc/jit/passes/bailout_graph.cpp
+++ torch/csrc/jit/passes/bailout_graph.cpp
@@ -23,7 +23,7 @@ static std::unordered_set<Value *> collectLoopCounts(Node *n) {
     it = outerNode->owningBlock();
   }
 
-  return std::move(loopCounts);
+  return loopCounts;
 }
 
 struct BailOutGraphBuilderForNode {
