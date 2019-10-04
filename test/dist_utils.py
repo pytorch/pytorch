f99ff960d4b6e2df22f60237274f006fc5d465b8
Shihao Xu <shihaoxu@fb.com>
2019-10-04 09:18:29 -0700

    Make RpcTest re-usable by other RPC backends by using init_method to initialize a RPC backend (#27320)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27320
    
    https://github.com/pytorch/pytorch/pull/27208/
    
    # Problem
    
    Other RPC backends take init_method.
    
    # Solution
    
    Set up init_method in rpc tests.
    ghstack-source-id: 91335127
    
    Differential Revision: D17709219
    
    fbshipit-source-id: 3184c6e9b922a6ff9f4d1cb9abfa118b23f43eeb

--- test/dist_utils.py
+++ test/dist_utils.py
@@ -1,36 +1,49 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from os import getenv
 from functools import wraps
+from os import getenv
+
 import torch.distributed as dist
 from torch.distributed.rpc_api import RpcBackend
+
 
 if not dist.is_available():
     print("c10d not available, skipping tests")
     sys.exit(0)
 
 
-BACKEND = getenv('RPC_BACKEND', RpcBackend.PROCESS_GROUP)
-RPC_INIT_URL = getenv('RPC_INIT_URL', '')
+class TestConfig:
+    __slots__ = ['backend']
 
-def dist_init(func):
+    def __init__(self, *args, **kwargs):
+        assert len(args) == 0, "TestConfig only takes kwargs."
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+TEST_CONFIG = TestConfig(backend=getenv("RPC_BACKEND", RpcBackend.PROCESS_GROUP))
+INIT_METHOD_TEMPLATE = "file://{file_name}?rank={rank}&world_size={world_size}"
+
+
+def dist_init(test_method):
     """
     We use this decorator for setting up and tearing down state since
     MultiProcessTestCase runs each `test*` method in a separate process and
     each process just runs the `test*` method without actually calling
     'setUp' and 'tearDown' methods of unittest.
     """
-    @wraps(func)
-    def wrapper(self):
+
+    @wraps(test_method)
+    def wrapper(self, *arg, **kwargs):
         self.worker_id = self.rank
-        store = dist.FileStore(self.file_name, self.world_size)
-        dist.init_process_group(backend='gloo', rank=self.rank,
-                                world_size=self.world_size, store=store)
-        dist.init_model_parallel(self_name='worker%d' % self.rank,
-                                 backend=BACKEND,
-                                 self_rank=self.rank,
-                                 init_method=RPC_INIT_URL)
-        func(self)
+        dist.init_process_group(backend="gloo", init_method=self.init_method)
+        dist.init_model_parallel(
+            self_name="worker%d" % self.rank,
+            backend=TEST_CONFIG.backend,
+            self_rank=self.rank,
+            init_method=self.init_method,
+        )
+        test_method(self, *arg, **kwargs)
         dist.join_rpc()
 
     return wrapper
