ff57e1c969dc0ba788e9ca3f3b48cccc2cd5ef02
Tao Xu <taox@fb.com>
2019-10-03 17:25:18 -0700

    Update on "[DO NOT MERGE][IOS] add test job for iOS simulator build"

--- .circleci/scripts/binary_ios_test.sh
+++ .circleci/scripts/binary_ios_test.sh
@@ -2,14 +2,17 @@
 
 echo ""
 echo "PWD: $(pwd)"
-
 echo "IOS_ARCH: ${IOS_ARCH}"
 echo "IOS_PLATFORM: ${IOS_PLATFORM}"
-export IOS_ARCH=${IOS_ARCH}
-export IOS_PLATFORM=${IOS_PLATFORM}
-WORKSPACE=/Users/distiller/workspace
 PROJ_ROOT=/Users/distiller/project
-
+if ! [ -x "$(command -v xcodebuild)" ]; then
+    echo 'Error: xcodebuild is not installed.'
+    exit 1
+fi 
 if [ ${IOS_PLATFORM} = "SIMULAOTR" ]; then 
     ruby ${PROJ_ROOT}/scripts/xcode_ios_x86_build.rb -i ${PROJ_ROOT}/build_ios/install/lib -x ${PROJ_ROOT}/ios/TestApp/TestApp.xcodeproj
+    if [ "$?" -eq "0" ]; then
+        echo 'xcodebuild failed!'
+        exit 1
+    fi
 fi
