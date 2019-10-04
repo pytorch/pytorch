6195437e57dcc80a718f23bf04a763af3b38d988
Pearu Peterson <pearu.peterson@gmail.com>
2019-10-04 08:07:44 -0700

    Implement pickle support for sparse tensors and torch.layout instances (#27062)
    Summary:
    Resolves issue https://github.com/pytorch/pytorch/issues/16667 and https://github.com/OpenMined/PySyft/issues/2326
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27062
    
    Differential Revision: D17762932
    
    Pulled By: ezyang
    
    fbshipit-source-id: dd99c1f4ac8eb2286eb55aa20ce973f60ce7b7e1

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -3,6 +3,7 @@ import torch
 import itertools
 import functools
 import random
+import sys
 import unittest
 from common_utils import TestCase, run_tests, skipIfRocm, do_test_dtypes, do_test_empty_full, load_tests
 from common_cuda import TEST_CUDA
@@ -2053,6 +2054,39 @@ class TestSparse(TestCase):
         v.transpose_(0, 1)
         self.assertEqual(list(t.coalesce().indices().size()), [2, 1])
         self.assertEqual(list(t.coalesce().values().size()), [1, 3])
+
+    def test_pickle(self):
+        if sys.version_info[0] == 2:
+            import cPickle as pickle
+        else:
+            import pickle
+
+        shape_sparse_dim_nnz = [
+            ((), 0, 2),
+            ((0,), 0, 10),
+            ((2,), 0, 3),
+            ((100, 3), 1, 3),
+            ((100, 20, 3), 2, 0),
+            ((10, 0, 3), 0, 3),
+            ((10, 0, 3), 0, 0),
+        ]
+
+        for shape, sparse_dim, nnz in shape_sparse_dim_nnz:
+            indices_shape = torch.Size((sparse_dim, nnz))
+            values_shape = torch.Size((nnz,) + shape[sparse_dim:])
+            indices = torch.arange(indices_shape.numel(), dtype=self.index_tensor(0).dtype,
+                                   device=self.device).view(indices_shape)
+            for d in range(sparse_dim):
+                indices[d].clamp_(max=(shape[d] - 1))  # make it valid index
+            if self.is_uncoalesced and indices.numel() > 0:
+                indices[:, -1] = indices[:, 0]  # make it uncoalesced
+            values_numel = values_shape.numel()
+            values = torch.arange(values_numel, dtype=self.value_dtype,
+                                  device=self.device).view(values_shape).div_(values_numel / 2.)
+            sp_tensor = self.sparse_tensor(indices, values, shape)
+            serialized = pickle.dumps(sp_tensor)
+            sp_tensor_loaded = pickle.loads(serialized)
+            self.assertEqual(sp_tensor, sp_tensor_loaded)
 
 
 class TestUncoalescedSparse(TestSparse):
