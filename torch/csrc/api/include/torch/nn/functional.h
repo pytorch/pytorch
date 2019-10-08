40978cf8cb67d22b141e3da30b3b07db77800695
Gaurav Tamba <gaurav.tamba@gmail.com>
2019-10-08 14:19:05 +0530

    Sort functional.h header imports alphabetically

--- torch/csrc/api/include/torch/nn/functional.h
+++ torch/csrc/api/include/torch/nn/functional.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <torch/nn/functional/distance.h>
+#include <torch/nn/functional/embedding.h>
 #include <torch/nn/functional/loss.h>
 #include <torch/nn/functional/normalization.h>
 #include <torch/nn/functional/pooling.h>
-#include <torch/nn/functional/embedding.h>
