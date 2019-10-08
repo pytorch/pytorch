5465622efbc7bc5d8fee1a1ed691186c6ebef7c8
Igor Fedan <ifedan@fb.com>
2019-10-08 00:06:08 -0400

    Fix for 25030: torch.multinomial samples same elements multiple times when replacement=False

--- aten/src/ATen/native/cpu/MultinomialKernel.cpp
+++ aten/src/ATen/native/cpu/MultinomialKernel.cpp
@@ -37,87 +37,191 @@ void multinomial_apply(Tensor& result, const Tensor& self, const int64_t n_sampl
   for (int64_t i = 0; i < n_dist; i++) {
     /* Get normalized cumulative distribution from prob distribution */
     scalar_t sum = 0;
+    scalar_t prev_sum = 0;
     scalar_t val;
     int n_zeros = 0;
+    bool lost_precision = false;
     for (int64_t j = 0; j < n_categories; j++) {
       val = self_ptr[i * self_stride_0 + j * self_stride_1];
       TORCH_CHECK(val >= 0, "invalid multinomial distribution (encountering probability entry < 0)");
       TORCH_CHECK(std::isfinite(val), "invalid multinomial distribution (encountering probability entry = infinity or NaN)");
 
+      prev_sum = sum;
       sum += val;
+      if (prev_sum == sum && val != 0) {
+        // We tried to summarize very large and small numbers and lost precision.
+        // We will use extra memory that will hold integer and fractional part of the sum separately.
+        lost_precision = true;
+        break;
+      }
       if (val == 0) {
         n_zeros += 1;
       }
       cum_dist_ptr[j * cum_dist_stride_0] = sum;
     }
 
-    TORCH_CHECK(sum > 0, "invalid multinomial distribution (sum of probabilities <= 0)");
-    TORCH_CHECK(with_replacement || (n_categories - n_zeros >= n_sample),
-        "invalid multinomial distribution (with replacement=False, not enough non-negative category to sample)");
+    if (!exceed_precision) {
+      TORCH_CHECK(sum > 0, "invalid multinomial distribution (sum of probabilities <= 0)");
+      TORCH_CHECK(with_replacement || (n_categories - n_zeros >= n_sample),
+                  "invalid multinomial distribution (with replacement=False, not enough non-negative category to sample)");
 
-    /* normalize cumulative probability distribution so that last val is 1
-    i.e. doesn't assume original self row sums to one */
-    if ((sum > 0) || ((sum < 1.00001) && (sum > 0.99999))) {
-      for (int64_t j = 0; j < n_categories; j++) {
-        cum_dist_ptr[j * cum_dist_stride_0] /= sum;
-      }
-    }
-
-    for (int64_t j = 0; j < n_sample; j++) {
-      /* sample a probability mass from a uniform distribution */
-      at::uniform_real_distribution<double> uniform(0, 1);
-      double uniform_sample = uniform(gen);
-      /* Do a binary search for the slot in which the prob falls
-      ie cum_dist[row][slot-1] < uniform_prob < cum_distr[row][slot] */
-      int left_pointer = 0;
-      int right_pointer = n_categories;
-      int mid_pointer;
-      scalar_t cum_prob;
-      int sample_idx;
-      /* Make sure the last cumulative distribution bucket sums to 1 */
-      cum_dist_ptr[(n_categories - 1) * cum_dist_stride_0] = 1;
-
-      while(right_pointer - left_pointer > 0) {
-        mid_pointer = left_pointer + (right_pointer - left_pointer) / 2;
-        cum_prob = cum_dist_ptr[mid_pointer * cum_dist_stride_0];
-        if (cum_prob < uniform_sample) {
-          left_pointer = mid_pointer + 1;
-        }
-        else {
-          right_pointer = mid_pointer;
+      /* normalize cumulative probability distribution so that last val is 1
+      i.e. doesn't assume original self row sums to one */
+      if ((sum > 0) || ((sum < 1.00001) && (sum > 0.99999))) {
+        for (int64_t j = 0; j < n_categories; j++) {
+          cum_dist_ptr[j * cum_dist_stride_0] /= sum;
         }
       }
-      sample_idx = left_pointer;
 
-      /* store in result tensor (will be incremented for lua compat by wrapper) */
-      result_ptr[i * result_dist_stride_0 + j * result_dist_stride_1] = sample_idx;
+      for (int64_t j = 0; j < n_sample; j++) {
+        /* sample a probability mass from a uniform distribution */
+        at::uniform_real_distribution<double> uniform(0, 1);
+        double uniform_sample = uniform(gen);
+        /* Do a binary search for the slot in which the prob falls
+        ie cum_dist[row][slot-1] < uniform_prob < cum_distr[row][slot] */
+        int left_pointer = 0;
+        int right_pointer = n_categories;
+        int mid_pointer;
+        scalar_t cum_prob;
+        int sample_idx;
+        /* Make sure the last cumulative distribution bucket sums to 1 */
+        cum_dist_ptr[(n_categories - 1) * cum_dist_stride_0] = 1;
 
-      /* Once a sample is drawn, it cannot be drawn again. ie sample without replacement */
-      if (!with_replacement && j < n_sample - 1) {
-        /* update cumulative distribution so that sample cannot be drawn again */
-        scalar_t diff;
-        scalar_t new_val = 0;
-        scalar_t sum;
-
-        if (sample_idx != 0) {
-          new_val = cum_dist_ptr[(sample_idx - 1) * cum_dist_stride_0];
-        }
-        /* marginal cumulative mass (i.e. original probability) of sample */
-        diff = cum_dist_ptr[sample_idx * cum_dist_stride_0] - new_val;
-        /* new sum of marginals is not one anymore... */
-        sum = 1.0 - diff;
-        for (int64_t k = 0; k < n_categories; k++) {
-          new_val = cum_dist_ptr[k * cum_dist_stride_0];
-          if (k >= sample_idx) {
-            /* remove sampled probability mass from later cumulative probabilities */
-            new_val -= diff;
+        while(right_pointer - left_pointer > 0) {
+          mid_pointer = left_pointer + (right_pointer - left_pointer) / 2;
+          cum_prob = cum_dist_ptr[mid_pointer * cum_dist_stride_0];
+          if (cum_prob < uniform_sample) {
+            left_pointer = mid_pointer + 1;
           }
-          /* make total marginals sum to one */
-          new_val /= sum;
-          cum_dist_ptr[k * cum_dist_stride_0] = new_val;
+          else {
+            right_pointer = mid_pointer;
+          }
+        }
+        sample_idx = left_pointer;
+
+        /* store in result tensor (will be incremented for lua compat by wrapper) */
+        result_ptr[i * result_dist_stride_0 + j * result_dist_stride_1] = sample_idx;
+
+        /* Once a sample is drawn, it cannot be drawn again. ie sample without replacement */
+        if (!with_replacement && j < n_sample - 1) {
+          /* update cumulative distribution so that sample cannot be drawn again */
+          scalar_t diff;
+          scalar_t new_val = 0;
+          scalar_t sum;
+
+          if (sample_idx != 0) {
+            new_val = cum_dist_ptr[(sample_idx - 1) * cum_dist_stride_0];
+          }
+          /* marginal cumulative mass (i.e. original probability) of sample */
+          diff = cum_dist_ptr[sample_idx * cum_dist_stride_0] - new_val;
+          /* new sum of marginals is not one anymore... */
+          sum = 1.0 - diff;
+          for (int64_t k = 0; k < n_categories; k++) {
+            new_val = cum_dist_ptr[k * cum_dist_stride_0];
+            if (k >= sample_idx) {
+              /* remove sampled probability mass from later cumulative probabilities */
+              new_val -= diff;
+            }
+            /* make total marginals sum to one */
+            new_val /= sum;
+            cum_dist_ptr[k * cum_dist_stride_0] = new_val;
+          }
+        }
+      }
+    } else {
+      AT_WARN("To calculate cumulative distribution pytorch will use extra memory");
+      // We will use second Tensor cum_dist_high to hold integer part of the cum_dist
+      // cum_dist will be used to hold fractional part
+      Tensor cum_dist_high = at::empty({n_categories}, self.options());
+      scalar_t * const cum_dist_high_ptr = cum_dist_high.data_ptr<scalar_t>();
+      scalar_t * const cum_dist_low_ptr = cum_dist.data_ptr<scalar_t>();
+
+      scalar_t sum_high = 0;
+      scalar_t sum_low = 0;
+      scalar_t val;
+      for (int64_t j = 0; j < n_categories; j++) {
+        val = self_ptr[i * self_stride_0 + j * self_stride_1];
+        TORCH_CHECK(val >= 0, "invalid multinomial distribution (encountering probability entry < 0)");
+        TORCH_CHECK(std::isfinite(val), "invalid multinomial distribution (encountering probability entry = infinity or NaN)");
+
+        scalar_t h = (scalar_t)((long)val);
+        scalar_t l = val - h;
+        sum_high += h;
+        sum_low += l;
+        if (val == 0) {
+          n_zeros += 1;
+        }
+        cum_dist_high_ptr[j * cum_dist_stride_0] = sum_high;
+        cum_dist_low_ptr[j * cum_dist_stride_0] = sum_low;
+      }
+
+      TORCH_CHECK((sum_high + sum_low) > 0, "invalid multinomial distribution (sum of probabilities <= 0)");
+      TORCH_CHECK(with_replacement || (n_categories - n_zeros >= n_sample),
+                  "invalid multinomial distribution (with replacement=False, not enough non-negative category to sample)");
+
+      for (int64_t j = 0; j < n_sample; j++) {
+        at::uniform_real_distribution<double> uniform(0, 1);
+        double uniform_sample = uniform(gen);
+        /* Do a binary search for the slot in which the prob falls
+        ie cum_dist[row][slot-1] < uniform_prob < cum_distr[row][slot] */
+        int left_pointer = 0;
+        int right_pointer = n_categories;
+        int mid_pointer;
+        scalar_t cum_prob_high;
+        scalar_t cum_prob_low;
+        int sample_idx;
+
+        while(right_pointer - left_pointer > 0) {
+          mid_pointer = left_pointer + (right_pointer - left_pointer) / 2;
+          cum_prob_high = cum_dist_high_ptr[mid_pointer * cum_dist_stride_0];
+          cum_prob_low = cum_dist_low_ptr[mid_pointer * cum_dist_stride_0];
+          scalar_t diff = cum_prob_high - uniform_sample * sum_high + cum_prob_low - uniform_sample * sum_low;
+          if (diff < 0) {
+            left_pointer = mid_pointer + 1;
+          }
+          else {
+            right_pointer = mid_pointer;
+          }
+        }
+        sample_idx = left_pointer;
+
+        /* store in result tensor (will be incremented for lua compat by wrapper) */
+        result_ptr[i * result_dist_stride_0 + j * result_dist_stride_1] = sample_idx;
+
+        /* Once a sample is drawn, it cannot be drawn again. ie sample without replacement */
+        if (!with_replacement && j < n_sample - 1) {
+          /* update cumulative distribution so that sample cannot be drawn again */
+          scalar_t diff_high;
+          scalar_t diff_low;
+          scalar_t new_val_high = 0;
+          scalar_t new_val_low = 0;
+
+          if (sample_idx != 0) {
+            new_val_high = cum_dist_high_ptr[(sample_idx - 1) * cum_dist_stride_0];
+            new_val_low = cum_dist_low_ptr[(sample_idx - 1) * cum_dist_stride_0];
+          }
+          /* marginal cumulative mass (i.e. original probability) of sample */
+          diff_high = cum_dist_high_ptr[sample_idx * cum_dist_stride_0] - new_val_high;
+          diff_low = cum_dist_low_ptr[sample_idx * cum_dist_stride_0] - new_val_low;
+
+          sum_high -= diff_high;
+          sum_low -= diff_low;
+          for (int64_t k = 0; k < n_categories; k++) {
+            new_val_high = cum_dist_high_ptr[k * cum_dist_stride_0];
+            new_val_low = cum_dist_low_ptr[k * cum_dist_stride_0];
+            if (k >= sample_idx) {
+              /* remove sampled probability mass from later cumulative probabilities */
+              new_val_high -= diff_high;
+              new_val_low -= diff_low;
+            }
+            cum_dist_high_ptr[k * cum_dist_stride_0] = new_val_high;
+            cum_dist_low_ptr[k * cum_dist_stride_0] = new_val_low;
+          }
         }
       }
     }
+
+
   }
 }
 
