fe8aa4e926217e854cf35cfd70d6aeddd8c21935
Edward Yang <ezyang@fb.com>
2019-10-09 12:18:55 -0700

    Tests for fallback boxed dispatch (including TLS mode) (#26719)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26719
    
    This PR adds a pair of tests for fallback boxed dispatch, exercising two different ways you might use it: (1) to implement a "wrapper" tensor type (e.g., LazyTensor, NestedTensor), and (2) to implement a toggleable "mode" (e.g., Profiling, Tracing). Both implement the most trivial possible implementations of their type: they "wrap" a real tensor simply forward along to the real implementation. This PR also adds the necessary feature support for toggleable mode, which is in the original generic dispatch abstraction design, but was not previously implemented. I had not originally intended to add this, but it turns out writing a new "mode" is a lot simpler than writing a "wrapper" type, so I ended up writing the mode version first.
    
    General structure of the PR:
    
    * Add two new testing tensor type ids, `TESTING_ONLY_GenericWrapperTensorId` and `TESTING_ONLY_GenericModeTensorId`, which our tests use. They might find other use in other tests if necessary.
    * Add support for toggling the availability of `TESTING_ONLY_GenericModeTensorId`. Introduces a new thread local variable accessible by `tls_local_tensor_type_set()` which is considered as part of dispatch.
    * The mode fallback is very simple: it increments a counter and then passes on the call to the underlying kernel by invoking the JIT.
    * The wrapper fallback is more complex: it parses the arguments, unwrapping any wrapped tensor arguments, then invokes the JIT, and then rewraps the outputs.
    
    The examples here are somewhat simplistic; there are a number of engineering improvements that could be applied. We could save these for later (landing this patch to get immediate testing), or incorporate them into this patch:
    
    * `getOperator` is horrible. Bram Wasti and I discussed a plan for how to make this easier, by simply refactoring the JIT interface.
    * `GenericWrapperTensorImpl` doesn't populate all of its fields accurately. Most notably, size is not setup correctly.
    * `generic_wrapper_fallback` should handle tensor lists in arguments and returns properly.
    
    One pitfall: fallback dispatch only works with non-c10 code. That's why I test using `batch_norm`.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Differential Revision: D17549624
    
    Test Plan: Imported from OSS
    
    Pulled By: ezyang
    
    fbshipit-source-id: 57dbdd8d6812a66082aa6db2934c8edcda340ea6

--- c10/core/impl/LocalTensorTypeSet.h
+++ c10/core/impl/LocalTensorTypeSet.h
@@ -1,22 +1,80 @@
+#pragma once
+
 #include <c10/core/TensorTypeSet.h>
 
-// TLS management for TensorTypeSet
+// TLS management for TensorTypeSet (the "local" TensorTypeSet(s))
 //
-// This manages thread-local TensorTypeSet of excluded keys which disqualify
-// tensor types from dispatch.  Keys which are in this set, even if they appear
-// in a list of potential valid keys on a tensor, are not considered for
-// dispatch.  This is used to, for example, turn off autograd after we have
-// handled autograd for a top-level element.
+// This manages two thread-local TensorTypeSets:
 //
-// Originally, I implemented this as storing the inverted set, but
-// TLS is defined to be zero-initialized, so this doesn't actually work
-// (you want the set to be -1 initialized).
+//  - The included type set, which adds a tensor type for consideration
+//    in dispatch.  (For example, you might add ProfilingTensorId to
+//    the included type set to turn on profiling on all tensor operations.)
+//
+//  - The excluded type set, which disqualifies a tensor type from dispatch.
+//    (For example, after redispatching on variable, we disqualify
+//    VariableTensorId so we don't attempt to handle variable again.)
+//    (Exclusion wins over inclusion.)
+//
+// NB: Originally, I implemented the excluded type set as storing the inverted
+// set, but TLS is defined to be zero-initialized, so this doesn't actually work
+// (if it's inverted, you want the set to be -1 initialized).
 
 namespace c10 {
 namespace impl {
 
-C10_API bool tls_variable_is_enabled();
-C10_API void tls_variable_set_enabled(bool enabled);
-C10_API TensorTypeSet tls_excluded_tensor_type_set();
+// POD version of LocalTensorTypeSet.  Declared here just so that
+// we can put it in the guards.
+struct C10_API PODLocalTensorTypeSet {
+  uint64_t included_;
+  uint64_t excluded_;
+
+  TensorTypeSet included() const {
+    return TensorTypeSet(TensorTypeSet::RAW, included_);
+  }
+  TensorTypeSet excluded() const {
+    return TensorTypeSet(TensorTypeSet::RAW, excluded_);
+  }
+
+  void set_included(TensorTypeSet x) {
+    included_ = x.raw_repr();
+  }
+  void set_excluded(TensorTypeSet x) {
+    excluded_ = x.raw_repr();
+  }
+};
+static_assert(std::is_pod<PODLocalTensorTypeSet>::value, "PODLocalTensorTypeSet must be a POD type.");
+
+struct C10_API LocalTensorTypeSet {
+  /* implicit */ LocalTensorTypeSet(PODLocalTensorTypeSet x)
+    : included_(x.included()), excluded_(x.excluded()) {}
+  TensorTypeSet included_;
+  TensorTypeSet excluded_;
+};
+
+C10_API LocalTensorTypeSet tls_local_tensor_type_set();
+
+class C10_API IncludeTensorTypeIdGuard {
+public:
+  IncludeTensorTypeIdGuard(TensorTypeId);
+  ~IncludeTensorTypeIdGuard();
+private:
+  // A little micro-optimization to save us from tls_get_addr call
+  // on destruction
+  PODLocalTensorTypeSet* tls_;
+  TensorTypeId id_;
+  bool prev_state_;
+};
+
+class C10_API ExcludeTensorTypeIdGuard {
+public:
+  ExcludeTensorTypeIdGuard(TensorTypeId);
+  ~ExcludeTensorTypeIdGuard();
+private:
+  // A little micro-optimization to save us from tls_get_addr call
+  // on destruction
+  PODLocalTensorTypeSet* tls_;
+  TensorTypeId id_;
+  bool prev_state_;
+};
 
 }} // namespace c10::impl
