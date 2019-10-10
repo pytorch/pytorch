5cbd451580c4aa25200358857b1873e169fef017
Negin Raoof <neginmr@utexas.edu>
2019-10-09 23:48:45 -0700

    fix for comments

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -1973,11 +1973,13 @@ def group_norm(g, input, num_groups, weight, bias, eps, cudnn_enabled):
     if sym_help._operator_export_type == torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK:
         return g.op("ATen", input, weight, bias, num_groups_i=num_groups,
                     eps_f=eps, cudnn_enabled_i=cudnn_enabled, operator_s="group_norm")
-    
+
     input_sizes = input.type().sizes()
+    # 0 in the shape list keeps dimension value unchanged.
     shape = [0, num_groups, -1]
     input_reshaped = g.op('Reshape', input, g.op('Constant', value_t=torch.LongTensor(shape)))
 
+    # C is always divisible by num_groups
     # Due to shape difference. we need to apply weight and bias after
     # instance norm computation and reshape
     weight_ = g.op("Constant", value_t=torch.tensor([1.] * num_groups).type(
