25fe70a594f967da0ac2ac468ec6f44c1496ae7e
Will Feng <willfeng@fb.com>
2019-10-13 18:15:19 -0400

    add TORCH_API to arg.h

--- torch/csrc/api/include/torch/arg.h
+++ torch/csrc/api/include/torch/arg.h
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <torch/csrc/WindowsTorchApiMacro.h>
+
 #include <utility>
 
 #define TORCH_ARG(T, name)                                       \
  public:                                                         \
-  inline auto name(const T& new_##name)->decltype(*this) { /* NOLINT */ \
+  TORCH_API inline auto name(const T& new_##name)->decltype(*this) { /* NOLINT */ \
     this->name##_ = new_##name;                                  \
     return *this;                                                \
   }                                                              \
-  inline auto name(T&& new_##name)->decltype(*this) { /* NOLINT */      \
+  TORCH_API inline auto name(T&& new_##name)->decltype(*this) { /* NOLINT */      \
     this->name##_ = std::move(new_##name);                       \
     return *this;                                                \
   }                                                              \
-  inline const T& name() const noexcept { /* NOLINT */                  \
+  TORCH_API inline const T& name() const noexcept { /* NOLINT */                  \
     return this->name##_;                                        \
   }                                                              \
  private:                                                        \
