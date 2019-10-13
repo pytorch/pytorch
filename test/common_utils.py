975fda373ee7f0b1d29d9ff9fd2bce0ce6c27bba
suo <suo@fb.com>
2019-10-12 22:21:20 -0700

    Remove sharding code from tests
    This has been turned off since january. Might as well clean it up. I want to do a bit of refactoring in this area.
    
    Differential Revision: [D17898077](https://our.internmc.facebook.com/intern/diff/D17898077/)
    
    [ghstack-poisoned]

--- test/common_utils.py
+++ test/common_utils.py
@@ -1247,32 +1247,11 @@ def check_test_defined_in_running_script(test_case):
             test_case.id(), running_script_path, test_case_class_file)
 
 
-num_shards = os.environ.get('TEST_NUM_SHARDS', None)
-shard = os.environ.get('TEST_SHARD', None)
-if num_shards is not None and shard is not None:
-    num_shards = int(num_shards)
-    shard = int(shard)
-
-    def load_tests(loader, tests, pattern):
-        set_running_script_path()
-        test_suite = unittest.TestSuite()
-        for test_group in tests:
-            for test in test_group:
-                check_test_defined_in_running_script(test)
-                name = test.id().split('.')[-1]
-                if name in THESE_TAKE_WAY_TOO_LONG:
-                    continue
-                hash_id = int(hashlib.sha256(str(test).encode('utf-8')).hexdigest(), 16)
-                if hash_id % num_shards == shard:
-                    test_suite.addTest(test)
-        return test_suite
-else:
-
-    def load_tests(loader, tests, pattern):
-        set_running_script_path()
-        test_suite = unittest.TestSuite()
-        for test_group in tests:
-            for test in test_group:
-                check_test_defined_in_running_script(test)
-                test_suite.addTest(test)
-        return test_suite
+def load_tests(loader, tests, pattern):
+    set_running_script_path()
+    test_suite = unittest.TestSuite()
+    for test_group in tests:
+        for test in test_group:
+            check_test_defined_in_running_script(test)
+            test_suite.addTest(test)
+    return test_suite
