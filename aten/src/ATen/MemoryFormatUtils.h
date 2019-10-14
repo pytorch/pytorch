7fdfb8458dbffc2425d371fa3c451f803ad66299
Igor Fedan <ifedan@fb.com>
2019-10-14 18:07:50 -0400

    Update on "Added MemoryFormatUtils.h that will give the ability to clone Tensor"
    [ghstack-poisoned]

--- aten/src/ATen/MemoryFormatUtils.h
+++ aten/src/ATen/MemoryFormatUtils.h
@@ -4,7 +4,7 @@
 
 namespace at {
 
-static Tensor clone_if_possible_with_memory_format(const Tensor& self) {
+Tensor try_clone_as_contiguous(const Tensor& self) {
   if (self.is_sparse()) {
     return self.clone();
   } else if (self.is_mkldnn()) {
