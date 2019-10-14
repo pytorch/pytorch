ec8883885d93ad2730af2c79f8b54d1b7fb38153
Igor Fedan <ifedan@fb.com>
2019-10-14 17:06:16 -0400

    Added MemoryFormatUtils.h that will give the ability to clone Tensor
    [ghstack-poisoned]

---
+++ aten/src/ATen/MemoryFormatUtils.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <ATen/ATen.h>
+
+namespace at {
+
+static Tensor clone_if_possible_with_memory_format(const Tensor& self) {
+  if (self.is_sparse()) {
+    return self.clone();
+  } else if (self.is_mkldnn()) {
+    return self.clone();
+  } else {
+    return self.clone(at::MemoryFormat::Contiguous);
+  }
+}
+
+}
---
+++ aten/src/ATen/test/memory_format_utils_test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include <ATen/MemoryFormatUtils.h>
+
+using namespace at;
+
+TEST(MemoryFormatUtilsTest, CheckSpareMemoryFormat) {
+  auto spare_tensor = at::empty({2, 2}, at::dtype<float>().layout(at::kSparse));
+  ASSERT_TRUE(spare_tensor.is_sparse());
+  auto spare_clone = clone_if_possible_with_memory_format(spare_tensor);
+  ASSERT_TRUE(spare_clone.is_sparse());
+}
+
+TEST(MemoryFormatUtilsTest, CheckMkldnnMemoryFormat) {
+  auto mkldnn_tensor = at::empty({2, 2}, at::dtype<float>().layout(at::kMkldnn));
+  ASSERT_TRUE(mkldnn_tensor.is_mkldnn());
+  auto mkldnn_clone = clone_if_possible_with_memory_format(mkldnn_tensor);
+  ASSERT_TRUE(mkldnn_clone.is_mkldnn());
+}
+
+TEST(MemoryFormatUtilsTest, CheckContiguousMemoryFormat) {
+  auto tensor_contiguous = at::empty({2, 2});
+  ASSERT_TRUE(tensor_contiguous.is_contiguous());
+  auto contiguous_clone = clone_if_possible_with_memory_format(tensor_contiguous);
+  ASSERT_TRUE(contiguous_clone.is_contiguous());
+}
+
+TEST(MemoryFormatUtilsTest, CheckNonContiguousMemoryFormat) {
+  auto tensor_noncontiguous = at::empty({2, 2}).t();
+  ASSERT_FALSE(tensor_noncontiguous.is_contiguous());
+  auto noncontiguous_clone = clone_if_possible_with_memory_format(tensor_noncontiguous);
+  ASSERT_TRUE(noncontiguous_clone.is_contiguous());
+}
+
