fdfe85b05125d6793a994244c2440a905b1c5f77
Igor Fedan <ifedan@fb.com>
2019-10-14 10:47:14 -0700

    Update on "clone() changed to clone(at::MemoryFormat::Contiguous) at ProcessGroupGloo.cpp"
    [ghstack-poisoned]

--- aten/src/ATen/MemoryFormatUtils.h
+++ aten/src/ATen/MemoryFormatUtils.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <ATen/ATen.h>
-#include <ATen/core/Tensor.h>
 
-static Tensor clone_if_possible_with_memory_format(const Tensor& src) {
+namespace at {
+
+static Tensor clone_if_possible_with_memory_format(const Tensor& self) {
   if (self.is_sparse()) {
     return self.clone();
-  } else if (input.is_mkldnn()) {
+  } else if (self.is_mkldnn()) {
     return self.clone();
   } else {
     return self.clone(at::MemoryFormat::Contiguous);
   }
 }
+
+}
+
