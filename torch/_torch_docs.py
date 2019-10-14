21b0fb5fd974211dad3c04e1e04e02235fa4d9fb
vishwakftw <vishwaks@cs.cmu.edu>
2019-10-14 17:09:04 -0400

    Code review - I : fix wording

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -3973,25 +3973,25 @@ add_docstr(torch.polygamma,
            r"""
 polygamma(n, input, out=None) -> Tensor
 
-Computes the :math:`n^{th}` derivative of the digamma function on :attr:`input`, where :attr:`n \geq 0`
-is called the order of the polygamma function.
+Computes the :math:`n^{th}` derivative of the digamma function on :attr:`input`.
+:math:`n \geq 0` is called the order of the polygamma function.
 
 .. math::
-    \psi^{(n)}(x) = \frac{d}{dx^{(n)}} \psi(x)
+    \psi^{(n)}(x) = \frac{d^{(n)}}{dx^{(n)}} \psi(x)
 
 .. note::
     This function is not implemented for :math:`n \geq 2`.
-
+""" + """
 Args:
     n (int): the order of the polygamma function
-    input (Tensor): the input tensor
-    out (Tensor, optional): the output tensor
+    {input}
+    {out}
 
 Example::
     >>> a = torch.tensor([1, 0.5])
     >>> torch.polygamma(1, a)
     tensor([1.64493, 4.9348])
-""")
+""".format(**common_args))
 
 add_docstr(torch.pow,
            r"""
