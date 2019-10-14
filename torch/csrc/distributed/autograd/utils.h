301c4344f8406855c17f7245a9e1903a3cd7ebb9
Rohan Varma <rvarm1@fb.com>
2019-10-14 10:41:34 -0700

    add known worker ids to distributed autograd context (#26324)
    Summary:
    Per https://github.com/pytorch/pytorch/issues/25525 we want to clean up distributed autograd context on all nodes, in addition to the local one. To do this, we want to send async RPCs to the other nodes telling them to clean up the context.
    
    The first step for this is for a node's context to know about the other workers. This PR does two things:
    
    1) Adds the necessary data structures and getter functions to `DistAutogradContext`
    2) Refactors calls to `addSendRpcBackward` to take in the `worker_id` as an additional argument
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26324
    
    Differential Revision: D17769411
    
    Pulled By: rohan-varma
    
    fbshipit-source-id: b7327d1209a574e2e88cb197edff3103024d51ad

--- torch/csrc/distributed/autograd/utils.h
+++ torch/csrc/distributed/autograd/utils.h
@@ -16,7 +16,8 @@ namespace autograd {
 TORCH_API void addSendRpcBackward(
     DistAutogradContext& autogradContext,
     const AutogradMetadata& autogradMetadata,
-    std::vector<torch::Tensor>& tensors);
+    std::vector<torch::Tensor>& tensors,
+    const rpc::worker_id_t dst);
 
 // This method is used to attach the 'recv' autograd function to the autograd
 // graph when we use RPC. This method creates a new 'recv' autograd function
