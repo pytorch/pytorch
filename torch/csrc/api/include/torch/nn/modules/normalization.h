ffbdcf9037efb21d9afd7777de662b68a2785e69
Anjali Chourdia <chourdiaanjali@fb.com>
2019-10-14 11:30:22 -0400

    updated the header file and function implementations

--- torch/csrc/api/include/torch/nn/modules/normalization.h
+++ torch/csrc/api/include/torch/nn/modules/normalization.h
@@ -7,19 +7,57 @@
 namespace torch {
 namespace nn {
 
-/// Options for the `BatchNorm` module.
+/// Options for the `LayerNorm` module.
 struct TORCH_API LayerNormOptions {
-  /* implicit */ LayerNormOptions(int64_t normalized_shape);
-  /// The number of features of the input tensor.
-  /// Changing this parameter after construction __has no effect__.
-  TORCH_ARG(int64_t, normalized_shape);
+  /* implicit */ LayerNormOptions(torch::IntArrayRef normalized_shape);
+  /// input shape from an expected input.
+  TORCH_ARG(torch::IntArrayRef, normalized_shape);
   /// a boolean value that when set to ``True``, this module
   /// has learnable per-element affine parameters initialized to ones (for weights)
   /// and zeros (for biases).
   TORCH_ARG(bool, elementwise_affine) = true;
-  /// The epsilon value added for numerical stability.
-  /// Changing this parameter after construction __is effective__.
+  /// a value added to the denominator for numerical stability.
   TORCH_ARG(double, eps) = 1e-5;
+
+  class TORCH_API LayerNormImpl : public torch::nn::Cloneable<LayerNormImpl> {
+   public:
+    explicit LayerNormImpl(torch::IntArrayRef normalized_shape)
+        : LayerNormImpl(LayerNormOptions(normalized_shape)) {}
+    explicit LayerNormImpl(const LayerNormOptions& options_);
+
+    void reset() override;
+
+    /// Pretty prints the `LayerNorm` module into the given `stream`.
+    void pretty_print(std::ostream& stream) const override;
+
+    /// Applies layer normalization over a mini-batch of inputs as described in
+    /// the paper `Layer Normalization`_ .
+    ///
+    /// The mean and standard-deviation are calculated separately over the last
+    /// certain number dimensions which have to be of the shape specified by
+    /// input `normalized_shape`.
+    ///
+    /// `Layer Normalization`: https://arxiv.org/abs/1607.06450
+    Tensor forward(const Tensor& input);
+
+    /// The options with which this module was constructed.
+    LayerNormOptions options;
+
+    /// The learned weight.
+    /// Initialized to ones if the `elementwise_affine` option is set to `true` upon construction.
+    Tensor weight;
+
+    /// The learned bias.
+    /// Initialized to zeros `elementwise_affine` option is set to `true` upon construction.
+    Tensor bias;
+  };
+
+  //todo- update description
+  /// A `ModuleHolder` subclass for `LayerNormImpl`.
+  /// See the documentation for `LayerNormImpl` class to learn what methods it
+  /// provides, or the documentation for `ModuleHolder` to learn about PyTorch's
+  /// module storage semantics.
+  TORCH_MODULE(LayerNorm);
 };
 
 } // namespace nn
--- torch/csrc/api/src/nn/modules/normalization.cpp
+++ torch/csrc/api/src/nn/modules/normalization.cpp
@@ -12,89 +12,34 @@
 namespace torch {
 namespace nn {
 
-  LayerNorm::LayerNorm() {
+LayerNormImpl::LayerNormImpl(const LayerNormOptions& options_) : options(options_) {
+  reset();
+}
 
+// todo - how to store the shape? checks based on elementwise_affine?
+void LayerNormImpl::reset() {
+  options.elementwise_affine(elementwise_affine);
+  options.eps(eps);
+  if(options.elementwise_affine()) {
+    weight = register_parameter("weight", torch::ones(options.normalized_shape()));
+    bias = register_parameter("bias", torch::zeros(options.normalized_shape()));
+  } else {
+    weight = register_parameter("weight", torch::empty(options.normalized_shape()));
+    bias = register_parameter("bias", torch::empty(options.normalized_shape()));
   }
+}
 
-  void LayerNorm::reset() {
+void LayerNormImpl::pretty_print(std::ostream& stream) const {
+  stream << std::boolalpha
+         << "torch::nn::LayerNorm(normalized_shape=" << options.normalized_shape()
+         << ", elementwise_affine=" << options.elementwise_affine() << ", eps=" << options.eps()
+         << ")";
+}
 
-  }
-
-  void LayerNorm::pretty_print(std::ostream& stream) const {
-
-  }
-
-  torch::Tensor EmbeddingImpl::forward(const Tensor& input) {
-
-  }
-  // EmbeddingImpl::EmbeddingImpl(const EmbeddingOptions& options_) : options(options_) { // NOLINT(modernize-pass-by-value)
-  //   reset();
-  // }
-
-  // void EmbeddingImpl::reset() {
-  //   if (options.padding_idx() != c10::nullopt) {
-  //     if (*options.padding_idx() > 0) {
-  //       TORCH_CHECK(*options.padding_idx() < options.num_embeddings(), "Padding_idx must be within num_embeddings");
-  //     }
-  //     else if (*options.padding_idx() < 0) {
-  //       TORCH_CHECK(*options.padding_idx() >= -(options.num_embeddings()), "Padding_idx must be within num_embedding");
-  //       options.padding_idx(options.num_embeddings() + *options.padding_idx());
-  //     }
-  //   }
-  //
-  //   if (options._weight() == c10::nullopt) {
-  //     weight = register_parameter(
-  //         "weight", torch::empty({options.num_embeddings(), options.embedding_dim()}));
-  //     torch::nn::init::normal_(weight);
-  //     if (options.padding_idx() != c10::nullopt) {
-  //       torch::NoGradGuard no_grad;
-  //       weight[*options.padding_idx()].fill_(0);
-  //     }
-  //   } else {
-  //     TORCH_CHECK((*options._weight()).sizes() == torch::IntArrayRef({options.num_embeddings(), options.embedding_dim()}), "Shape of _weight does not match num_embeddings and embedding_dim");
-  //     weight = register_parameter("weight", *options._weight());
-  //   }
-  // }
-  //
-  // void EmbeddingImpl::pretty_print(std::ostream& stream) const {
-  //   stream << "torch::nn::Embedding(num_embeddings=" << options.num_embeddings()
-  //          << ", embedding_dim=" << options.embedding_dim();
-  //   if (options.padding_idx() != c10::nullopt) {
-  //     stream << ", padding_idx=" << *options.padding_idx();
-  //   }
-  //   if (options.max_norm() != c10::nullopt) {
-  //     stream << ", max_norm=" << *options.max_norm();
-  //   }
-  //   if (options.norm_type() != 2) {
-  //     stream << ", norm_type=" << options.norm_type();
-  //   }
-  //   if (options.scale_grad_by_freq()) {
-  //     stream << ", scale_grad_by_freq=" << std::boolalpha << options.scale_grad_by_freq();
-  //   }
-  //   if (options.sparse()) {
-  //     stream << ", sparse=" << std::boolalpha << options.sparse();
-  //   }
-  //   stream << ")";
-  // }
-  //
-  // torch::Tensor EmbeddingImpl::forward(const Tensor& input) {
-  //   if (options.padding_idx() != c10::nullopt) {
-  //     if (*options.padding_idx() > 0) {
-  //       TORCH_CHECK(*options.padding_idx() < weight.size(0), "Padding_idx must be within num_embeddings");
-  //     }
-  //     else if (*options.padding_idx() < 0) {
-  //       TORCH_CHECK(*options.padding_idx() >= -weight.size(0), "Padding_idx must be within num_embedding");
-  //       options.padding_idx(weight.size(0) + *options.padding_idx());
-  //     }
-  //   } else {
-  //     options.padding_idx(-1);
-  //   }
-  //
-  //   if (options.max_norm() != c10::nullopt) {
-  //     torch::NoGradGuard no_grad;
-  //     torch::embedding_renorm_(weight, input.contiguous(), *options.max_norm(), options.norm_type());
-  //   }
-  //   return torch::embedding(weight, input.contiguous(), *options.padding_idx(), options.scale_grad_by_freq(), options.sparse());
-  // }
+//todo- where to get this: torch.backends.cudnn.enabled value from
+torch::Tensor LayerNormImpl::forward(const Tensor& input) {
+  return torch::layer_norm(input, options.normalized_shape(), weight, bias, options.eps(),
+                          false/*torch.backends.cudnn.enabled*/)
+}
 } // namespace nn
 } // namespace torch
