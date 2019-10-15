a68831284b7cc1e845973952bbc03a47f59cb09d
jeremyl <jeremyl@devvm5626.prn2.facebook.com>
2019-10-15 14:15:31 -0700

    [pytorch] Make JIT Serialization support arbitrary std::function<> IO
    Right now, torch::save() uses std::ostream, which results in unnecessary
    data copies in practice. Similar for torch::load().
    
    Adding a std::function<size_t(const void*, size_t)> as an output option,
    parallel to the existing filename and std::ostream apis, gives users the
    flexibility to emit directly to a backing store.
    
    For a simple case of appending the output to a std::string, we observe
    significant benchmark savings (on order of -50%), even with the
    minor std::function<> dispatch overhead. The main reason is that
    std::ostringstream effectively requires 2 extra copies of the data
    beyond a simple string.append lambda.
    
    We also provide a parallel api for the load(), though this one is
    slightly more complex due to the need to do arbitrary position reads.
    
    Differential Revision: [D17939034](https://our.internmc.facebook.com/intern/diff/D17939034/)
    
    [ghstack-poisoned]

--- caffe2/serialize/inline_container.h
+++ caffe2/serialize/inline_container.h
@@ -123,11 +123,15 @@ class CAFFE2_API PyTorchStreamReader final {
 
 class CAFFE2_API PyTorchStreamWriter final {
  public:
-  PyTorchStreamWriter(std::string archive_name, std::ostream* out=nullptr);
-  PyTorchStreamWriter(std::ostream* out)
-  : PyTorchStreamWriter("archive", out) {}
+  explicit PyTorchStreamWriter(std::string archive_name);
+  explicit PyTorchStreamWriter(
+      const std::function<size_t(const void*, size_t)>& writer_func);
 
-  void writeRecord(const std::string& name, const void* data, size_t size, bool compress = false);
+  void writeRecord(
+      const std::string& name,
+      const void* data,
+      size_t size,
+      bool compress = false);
   void writeEndOfFile();
 
   bool finalized() const {
@@ -141,13 +145,16 @@ class CAFFE2_API PyTorchStreamWriter final {
   ~PyTorchStreamWriter();
 
  private:
+  void setup(const string& file_name);
   void valid(const char* what, const char* info = "");
   size_t current_pos_ = 0;
   std::unique_ptr<mz_zip_archive> ar_;
   std::string archive_name_;
-  std::ostream* out_;
+  std::string archive_name_plus_slash_;
   std::ofstream file_stream_;
+  std::function<size_t(const void*, size_t)> writer_func_;
   bool finalized_ = false;
+  bool err_seen_ = false;
   friend size_t ostream_write_func(
       void* pOpaque,
       uint64_t file_ofs,
