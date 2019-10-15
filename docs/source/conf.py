77d8864eb22c53b463e2d6d7be4f79263bb7700d
Richard Zou <zou3519@gmail.com>
2019-10-15 07:46:41 -0700

    Fix Sphinx warning about '_images' not existing (#27927)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27927
    
    This fixes
    `WARNING: html_static_path entry '_images' does not exist`
    by removing '_images' from conf.py. As far as I can tell, '_images' in
    `html_static_path` is only necessary if images already exist in the
    `_images` folder; otherwise, sphinx is able to auto-generate _images
    into the build directory and populate it correctly.
    
    Test Plan: - build and view the docs locally.
    
    Differential Revision: D17915109
    
    Pulled By: zou3519
    
    fbshipit-source-id: ebcc1f331475f52c0ceadd3e97c3a4a0d606e14b

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -158,7 +158,7 @@ if RELEASE:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_images']
+html_static_path = ['_static']
 
 
 # Called automatically by Sphinx, making this `conf.py` an "extension".
