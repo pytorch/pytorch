a6e0b6a834a2d6547cbcceb6649043e4f6def3f9
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2019-10-15 10:01:23 -0500

    Add a uint24_t type with some fast computations for ROCm

---
+++ aten/src/ATen/rocm/ROCm_uint24.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#ifdef __HIP_PLATFORM_HCC
+template<typename T>
+struct __attribute__((packed)) uint24_t {
+    T x : 24;
+};
+
+template<typename T>
+inline
+T mul24(T x, T y) noexcept
+{
+    return uint24_t<T>{x}.x * uint24_t<T>{y}.x;
+}
+
+template long long mul24<long long>(long long, long long);
+
+template<typename T>
+inline
+T mad24(T x, T y, T z) noexcept
+{
+    return uint24_t<T>{x}.x * uint24_t<T>{y}.x + uint24_t<T>{z}.x;
+}
+
+template long long mad24<long long>(long long, long long, long long);
+
+template<typename T>
+inline
+T div24(T x, T y) noexcept
+{
+    return uint24_t<T>{x}.x / uint24_t<T>{y}.x;
+}
+
+template long long div24<long long>(long long, long long);
+
+template<typename T>
+inline
+T mod24(T x, T y) noexcept
+{
+    return uint24_t<T>{x}.x % uint24_t<T>{y}.x;
+}
+
+template long long mod24<long long>(long long, long long);
+#endif
