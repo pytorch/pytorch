7f9cd27cf4d4ceb7a07f467e41f3404fae9f2209
jeremyl <jeremyl@devvm5626.prn2.facebook.com>
2019-10-15 22:21:51 -0700

    [pytorch] In ProcessGroupAgent, use non-iostream torch::load()/save().
    Avoid using the iostream versions of torch::load()/torch::save(), which
    incur at least one additional full data copy.
    
    Differential Revision: [D17945206](https://our.internmc.facebook.com/intern/diff/D17945206/)
    
    [ghstack-poisoned]

--- torch/csrc/distributed/rpc/process_group_agent.cpp
+++ torch/csrc/distributed/rpc/process_group_agent.cpp
@@ -12,7 +12,7 @@ namespace rpc {
 namespace {
 
 // Write the message into the given ostream
-void serialize(const Message& message, std::ostream& os) {
+std::string serialize(const Message& message) {
   // We cast const void* to void* here because we need to create a tensor using
   // that memory space. If is fine as that tensor stays function-local, and will
   // not be modified during its lifetime.
@@ -27,13 +27,18 @@ void serialize(const Message& message, std::ostream& os) {
   // append id and autograd metadata as a tensor
   tensors.push_back(torch::tensor({message.id()}, {torch::kInt64}));
 
-  torch::save(tensors, os);
+  std::string out;
+  torch::save(tensors, [&](const void* buf, size_t n) -> size_t {
+    out.append(static_cast<const char*>(buf), n);
+    return n;
+  });
+  return out;
 }
 
-Message deserialize(MessageType type, std::istream& is) {
+Message deserialize(MessageType type, const char* buf, size_t nbytes) {
   std::vector<torch::Tensor> tensors;
 
-  torch::load(tensors, is);
+  torch::load(tensors, buf, nbytes);
 
   TORCH_CHECK(tensors.size() >= 2, "Failed to deserialize a message.");
   auto idTensor = std::move(tensors.back());
@@ -44,12 +49,8 @@ Message deserialize(MessageType type, std::istream& is) {
   TORCH_INTERNAL_ASSERT(1, idTensor.numel());
   int64_t id = idTensor.storage().data<int64_t>()[0];
 
-  std::vector<char> payload(payloadTensor.numel());
-
-  if (payloadTensor.numel() > 0) {
-    std::memcpy(
-        payload.data(), payloadTensor.storage().data(), payloadTensor.numel());
-  }
+  const char* data = payloadTensor.storage().data<char>();
+  std::vector<char> payload(data, data + payloadTensor.numel());
 
   return Message(std::move(payload), std::move(tensors), type, id);
 }
@@ -292,9 +293,7 @@ void ProcessGroupAgent::enqueueSend(SendWork work) {
   // NB: this can be changed to use a native move capture when moved to C++14
   threadPool_.run(std::bind(
       [&](const SendWork& work) {
-        std::stringstream ss;
-        serialize(work.message_, ss);
-        std::string serializedPayload = ss.str();
+        std::string serializedPayload = serialize(work.message_);
 
         std::vector<torch::Tensor> preamble = {torch::tensor(
             {(int64_t)pg_->getRank(),
@@ -342,10 +341,10 @@ void ProcessGroupAgent::enqueueRecv(RecvWork work) {
   threadPool_.run(std::bind(
       [&](RecvWork& work) {
         torch::Tensor& payload = work.payload_;
-        std::stringstream ss(std::string(
-            (char*)payload.storage().data<signed char>(), payload.numel()));
-
-        Message message = deserialize(work.type_, ss);
+        Message message = deserialize(
+            work.type_,
+            (char*)payload.storage().data<signed char>(),
+            payload.numel());
         if (message.isRequest()) {
           send(work.from_, cb_->operator()(message));
         } else if (message.isResponse()) {
