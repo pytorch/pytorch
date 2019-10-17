4f64d8535e8c930c36574c6000feaef847d9e294
Sebastian Messmer <messmer@fb.com>
2019-10-17 14:26:55 -0700

    Update on "Remove confusing torch::jit::RegisterOperators for custom ops"
    We have `torch::RegisterOperators` for custom ops. `torch::jit::RegisterOperators` had a dual state of being able to register custom ops if called one way and being able to register pure JIT ops if called another way.
    This is confusing because you end up in different operator libraries depending on which API exactly you're using.
    
    This PR removes the ability for torch::jit::RegisterOperators to register custom ops and forces people to use the new torch::RegisterOperators.
    
    This was already deprecated before but we now remove it.
    
    Differential Revision: [D17981895](https://our.internmc.facebook.com/intern/diff/D17981895/)
    
    [ghstack-poisoned]

--- torch/csrc/jit/custom_operator.h
+++ torch/csrc/jit/custom_operator.h
@@ -2,6 +2,7 @@
 
 #include <torch/csrc/jit/operator.h>
 #include <ATen/core/stack.h>
+#include <ATen/core/op_registration/op_registration.h>
 
 namespace torch {
 namespace jit {
@@ -10,6 +11,8 @@ namespace jit {
 /// `torch::jit::registerOperator` for every supplied operator, but allows doing
 /// so in the global scope when a `RegisterOperators` object is assigned to a
 /// static variable.
+/// Note: This is *not* the custom operator API. If you want to register custom
+/// operators, take a look at torch::RegisterOperators.
 struct TORCH_API RegisterOperators {
   RegisterOperators() = default;
 
