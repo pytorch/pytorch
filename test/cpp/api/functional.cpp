56567c11a0ab81d373f8de97c166f0d435bf98e6
Anjali Chourdia <chourdiaanjali@fb.com>
2019-10-17 11:09:57 -0400

    added F::layer_norm and functionality test

--- test/cpp/api/functional.cpp
+++ test/cpp/api/functional.cpp
@@ -595,10 +595,10 @@ TEST_F(FunctionalTest, Normalize) {
   const auto expected = torch::tensor(
     {{{0.00000000, 0.10000000, 0.2000, 0.30000000, 0.40000000},
       {0.14285715, 0.17142858, 0.2000, 0.22857143, 0.25714287}}}, torch::requires_grad().dtype(torch::kFloat));
-  { // Test #1 
+  { // Test #1
     auto input = torch::tensor({{{0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}}}, torch::dtype(torch::kFloat).requires_grad(true));
     auto norm = F::normalize(input, NormalizeOptions().p(1).dim(-1));
-    
+
     // reduce to scalar to call .backward()
     torch::Tensor s = norm.sum();
     s.backward();
@@ -606,6 +606,16 @@ TEST_F(FunctionalTest, Normalize) {
     ASSERT_EQ(s.ndimension(), 0);
     ASSERT_EQ(input.grad().numel(), 10);
     ASSERT_TRUE(torch::allclose(norm, expected));
+  }
+
+  TEST_F(FunctionalTest, LayerNorm) {
+    const Tensor& input = torch::randn(2, 2);
+    const LayerNormOptions& options = LayerNormOptions({2, 2}).elementwise_affine(false).eps(2e-5);
+    const Tensor& weight = torch::randn(2, 2);;
+    const Tensor& bias = torch::randn(2, 2);
+    auto y = F::layer_norm(input, options, weight, bias);
+    auto y_exp = torch::layer_norm(input, options.normalized_shape(), weight, bias, options.eps());
+    ASSERT_TRUE(torch::allclose(y, y_exp));
   }
 
   { // Test #2 Check variations of optional arguments
@@ -618,7 +628,7 @@ TEST_F(FunctionalTest, Normalize) {
 
     ASSERT_TRUE(torch::allclose(output, expected));
   }
-  
+
   { // Test #3 Base case of scalar tensor
     auto input = torch::randn({}, torch::requires_grad());
     torch::Tensor norm = F::normalize(input, NormalizeOptions().p(1).dim(-1));
