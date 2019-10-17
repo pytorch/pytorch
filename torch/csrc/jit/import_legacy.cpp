2265cddbd2896e6ecf2a257b29cc141097164f43
Mikhail Zolotukhin <mvz@fb.com>
2019-10-16 21:30:44 -0700

    Cleanup torch::jit::script::Module API for accessing attributes/parameters/submodules. (#27260)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27260
    
    This PR has the following changes:
    - Slot class is removed. In all use cases except `lower_graph` we really
    just needed the attribute name and thus having an extra layer of
    abstraction through Slot only made the code harder to understand.
    - get_parameters, get_attributes, get_modules, and get_slots now return
    a list of <name, item> pairs instead of a list of Slots.
    
    Differential Revision: D17728910
    
    Test Plan: Imported from OSS
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 94781611752dd88e7fddfe8b8e0252d6ec32ba68

--- torch/csrc/jit/import_legacy.cpp
+++ torch/csrc/jit/import_legacy.cpp
@@ -348,17 +348,17 @@ script::Module ScriptModuleDeserializer::LEGACY_convertModule(
         LEGACY_pickled_ivalues_.at(module_def.get_state_attribute_id()));
   }
 
-  for (const auto& slot : module.get_attributes()) {
+  for (const script::NameValue& slot : module.get_attributes()) {
     // Verify that all the non-optional attributes have been initialized
     // TODO: Issue #20497
-    if (slot.type()->kind() != TypeKind::OptionalType) {
+    if (slot.value.type()->kind() != TypeKind::OptionalType) {
       TORCH_CHECK(
-          !slot.value().isNone(),
+          !slot.value.isNone(),
           "The field '",
-          slot.name(),
+          slot.name,
           "' was left unitialized after __setstate__, but expected a ",
           "value of type '",
-          slot.type()->python_str(),
+          slot.value.type()->python_str(),
           "'");
     }
   }
--- torch/csrc/jit/script/init.cpp
+++ torch/csrc/jit/script/init.cpp
@@ -513,8 +513,9 @@ void initJitScriptBindings(PyObject* module) {
           [](Module& self, const std::string& name, py::object value) {
             auto attr = self.find_attribute(name);
             TORCH_CHECK(attr, "Could not find attribute '", name, "'");
-            auto ivalue = toIValue(value, attr->type());
-            attr->setValue(ivalue);
+            auto ivalue =
+                toIValue(std::move(value), self.type()->getAttribute(name));
+            self.set_attribute(name, ivalue);
           })
       .def("_set_parameter", &Module::set_parameter)
       .def("_get_parameter", &Module::get_parameter)
@@ -525,8 +526,8 @@ void initJitScriptBindings(PyObject* module) {
           "_get_modules",
           [](Module& self) {
             std::vector<std::pair<std::string, Module>> modules;
-            for (Slot s : self.get_module_slots()) {
-              modules.emplace_back(s.name(), s.to_module());
+            for (const NameModule& s : self.get_modules()) {
+              modules.emplace_back(std::make_pair(s.name, s.module));
             }
             return modules;
           })
@@ -536,10 +537,10 @@ void initJitScriptBindings(PyObject* module) {
             auto parameters = self.get_parameters();
             py::tuple result(parameters.size());
             auto i = 0;
-            for (Slot p : parameters) {
+            for (const NameValue& p : parameters) {
               py::tuple r(2);
               result[i++] = std::make_tuple(
-                  p.name(), autograd::as_variable_ref(p.value().toTensor()));
+                  p.name, autograd::as_variable_ref(p.value.toTensor()));
             }
             return result;
           })
@@ -549,11 +550,11 @@ void initJitScriptBindings(PyObject* module) {
             auto attributes = self.get_attributes();
             py::tuple result(attributes.size());
             size_t i = 0;
-            for (Slot buffer : attributes) {
+            for (const NameValue& attr : attributes) {
               py::tuple r(3);
-              IValue v = buffer.value();
+              IValue v = attr.value;
               result[i++] = std::make_tuple(
-                  buffer.name(), buffer.type(), toPyObject(std::move(v)));
+                  attr.name, attr.value.type(), toPyObject(std::move(v)));
             }
             return result;
           })
