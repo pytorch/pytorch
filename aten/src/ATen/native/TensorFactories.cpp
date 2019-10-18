7f94d8853c516590a10be9f0088adbcbcdf26070
eellison <eellison@fb.com>
2019-10-18 16:00:43 -0700

    address comments

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -50,6 +50,7 @@ void window_function_checks(
       window_length);
 }
 
+// bool inputs are considered integral
 static inline bool allIntegral(std::initializer_list<std::reference_wrapper<Scalar>> l) {
   for (Scalar& s : l) {
     if (!s.isIntegral(true)) {
@@ -77,12 +78,9 @@ Tensor arange(
     Scalar step,
     const TensorOptions& options) {
   bool set_to_integral_dtype = !options.has_dtype() && allIntegral({start, end, step});
-  Tensor result;
-  if (set_to_integral_dtype) {
-      result = at::empty({0}, options.dtype(at::ScalarType::Long));
-  } else {
-      result = at::empty({0}, options);
-  }
+  Tensor result = set_to_integral_dtype
+      ? at::empty({0}, options.dtype(at::ScalarType::Long))
+      : at::empty({0}, options);
   return at::arange_out(result, start, end, step);
 }
 
