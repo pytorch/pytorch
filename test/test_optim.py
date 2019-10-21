9b112e39fe6d1326a396c022db3a9b34e00cf5b8
Timothy Man <timothy.man@biognosys.com>
2019-10-21 10:29:37 +0200

    Adding test for div_factor and final_div_factor

--- test/test_optim.py
+++ test/test_optim.py
@@ -1493,5 +1493,30 @@ class TestLRScheduler(TestCase):
 
         self.assertLessEqual(last_lr, max_lr)
 
+    def test_onecycle_lr_div_factors(self):
+        # https://github.com/pytorch/pytorch/issues/28216
+
+        model = torch.nn.Linear(2, 1)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+        div_factors = [1, 10, 25, 100]
+        final_div_factors = [1, 1E2, 1E4, 1E6]
+
+        for div_factor, final_div_factor in zip(div_factors, final_div_factors):
+            for strategy in ['linear', 'cos']:
+                scheduler = OneCycleLR(optimizer, max_lr=25, total_steps=10,
+                                       anneal_strategy=strategy,
+                                       cycle_momentum=False,
+                                       div_factor=div_factor,
+                                       final_div_factor=final_div_factor
+                                       )
+                lrs = []
+                for i in range(10):
+                    lrs.append(optimizer.param_groups[0]["lr"])
+                    scheduler.step()
+
+                initial_lr, max_lr, last_lr = lrs[0], max(lrs), lrs[-1]
+                self.assertAlmostEqual(max_lr / initial_lr, div_factor)
+                self.assertAlmostEqual(initial_lr, final_div_factor * last_lr)
+
 if __name__ == '__main__':
     run_tests()
