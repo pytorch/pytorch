b0a4c31b0f3dc74d302114fd2fc1d2a3889138df
Hong Xu <hong@topbug.net>
2019-10-22 13:58:35 -0700

    Move the CUDA implementation of log1p to ATen. (#26923)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26923
    
    Fix #24588
    
    Test Plan: Imported from OSS
    
    Differential Revision: D17984184
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: 3bc2be4f08e800b1de274940f2bd3d5b418b45ee

--- aten/src/ATen/native/cuda/UnaryOpsKernel.cu
+++ aten/src/ATen/native/cuda/UnaryOpsKernel.cu
@@ -73,6 +73,14 @@ void log10_kernel_cuda(TensorIterator& iter) {
   });
 }
 
+void log1p_kernel_cuda(TensorIterator& iter) {
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "log1p_cuda", [&]() {
+    gpu_kernel(iter, []GPU_LAMBDA(scalar_t a) -> scalar_t {
+      return ::log1p(a);
+    });
+  });
+}
+
 void log2_kernel_cuda(TensorIterator& iter) {
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "log2_cuda", [&]() {
     gpu_kernel(iter, []GPU_LAMBDA(scalar_t a) -> scalar_t {
@@ -198,6 +206,7 @@ REGISTER_DISPATCH(floor_stub, &floor_kernel_cuda);
 REGISTER_DISPATCH(log_stub, &log_kernel_cuda);
 REGISTER_DISPATCH(log10_stub, &log10_kernel_cuda);
 REGISTER_DISPATCH(log2_stub, &log2_kernel_cuda);
+REGISTER_DISPATCH(log1p_stub, &log1p_kernel_cuda);
 REGISTER_DISPATCH(neg_stub, &neg_kernel_cuda);
 REGISTER_DISPATCH(round_stub, &round_kernel_cuda);
 REGISTER_DISPATCH(rsqrt_stub, &rsqrt_kernel_cuda);
