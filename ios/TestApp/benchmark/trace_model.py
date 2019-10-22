2e2882bf29875ad62822646abedf380f4ff1e73d
Tao Xu <taox@fb.com>
2019-10-22 15:18:26 -0700

    [iOS] Scripts for setting up benchmark projects

---
+++ ios/TestApp/benchmark/trace_model.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+
+model = torchvision.models.resnet18(pretrained=True)
+model.eval()
+example = torch.rand(1, 3, 224, 224)
+traced_script_module = torch.jit.trace(model, example)
+traced_script_module.save("model.pt")
