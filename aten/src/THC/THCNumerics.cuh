6ec162fda659de482acdf7716c66f0000fee72d0
Hong Xu <hong@topbug.net>
2019-10-23 11:19:10 -0700

    Migrate `sinh` and `sinh_` from the TH to Aten (CUDA)
    Benchmark (Debian Buster, CUDA 9.2, Quadro P400, turbo off, Release):
    
    ```python
    import timeit
    
    for n, t in [(10_000, 20000),
                 (100_000, 20000)]:
        for dtype in ('torch.half', 'torch.float', 'torch.double'):
            print(f'torch.sinh(a) a.numel() == {n} for {t} times {dtype}')
            print(timeit.timeit(f'torch.sinh(a); torch.cuda.synchronize()', setup=f'import torch; a=torch.arange({n}, dtype={dtype}, device="cuda")', number=t))
    ```
    
    Before:
    
    ```
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.half
    0.3807680979998622
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.float
    0.37430476099962107
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.double
    1.0580407639999976
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.half
    0.7996397469996737
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.float
    1.010930432999885
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.double
    7.310400856999877
    ```
    
    After:
    
    ```
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.half
    0.3720399889998589
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.float
    0.3694016069994177
    torch.sinh(a) a.numel() == 10000 for 20000 times torch.double
    1.0551542660004998
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.half
    0.7431191599998783
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.float
    0.9953043630002867
    torch.sinh(a) a.numel() == 100000 for 20000 times torch.double
    7.3146168890007175
    ```
    
    Close #24628
    
    [ghstack-poisoned]

--- aten/src/THC/THCNumerics.cuh
+++ aten/src/THC/THCNumerics.cuh
@@ -208,7 +208,6 @@ struct THCNumerics<at::Half> {
   static inline __host__ __device__ at::Half sqrt(at::Half a) { return ::sqrt(a); }
   static inline __host__ __device__ at::Half acos(at::Half a) { return ::acos(a); }
   static inline __host__ __device__ at::Half cosh(at::Half a) { return ::cosh(a); }
-  static inline __host__ __device__ at::Half sinh(at::Half a) { return ::sinh(a); }
   static inline __host__ __device__ at::Half tan(at::Half a) { return ::tan(a); }
   static inline __host__ __device__ at::Half atan(at::Half a) { return ::atan(a); }
   static inline __host__ __device__ at::Half tanh(at::Half a) { return ::tanh(a); }
@@ -278,7 +277,6 @@ struct THCNumerics<float> {
   static inline __host__ __device__  float acos (float a) { return  acosf(a); }
   static inline __host__ __device__  float cosh (float a) { return  coshf(a); }
   static inline __host__ __device__  float acosh(float a) { return acoshf(a); }
-  static inline __host__ __device__  float sinh (float a) { return  sinhf(a); }
   static inline __host__ __device__  float asinh(float a) { return asinhf(a); }
   static inline __host__ __device__  float tan  (float a) { return   tanf(a); }
   static inline __host__ __device__  float atan (float a) { return  atanf(a); }
@@ -322,7 +320,6 @@ struct THCNumerics<double> {
   static inline __host__ __device__  double acos (double a) { return  ::acos(a); }
   static inline __host__ __device__  double cosh (double a) { return  ::cosh(a); }
   static inline __host__ __device__  double acosh(double a) { return ::acosh(a); }
-  static inline __host__ __device__  double sinh (double a) { return  ::sinh(a); }
   static inline __host__ __device__  double asinh(double a) { return ::asinh(a); }
   static inline __host__ __device__  double tan  (double a) { return   ::tan(a); }
   static inline __host__ __device__  double atan (double a) { return  ::atan(a); }
