7b3e610b2735418c9997c95edf4dba9d8165d4b9
Will Feng <willfeng@fb.com>
2019-10-23 16:13:10 -0400

    Update on "C++ nn::ZeroPad2d"
    C++ nn::ZeroPad2d
    
    gh-metadata: pytorch pytorch 26604 gh/yf225/4/head

--- torch/csrc/api/src/nn/modules/padding.cpp
+++ torch/csrc/api/src/nn/modules/padding.cpp
@@ -65,7 +65,7 @@ void ZeroPad2dImpl::pretty_print(std::ostream& stream) const {
 }
 
 Tensor ZeroPad2dImpl::forward(const Tensor& input) {
-  return F::pad(input, PadOptions(*options.padding()).mode(torch::kConstant).value(0));
+  return F::pad(input, PadOptions(at::ArrayRef<int64_t>(options.padding()).vec()).mode(torch::kConstant).value(0));
 }
 
 } // namespace nn
