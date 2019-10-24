58873776ff695b13cec391bf28af61a9c6bddff9
Shen Li <cs.shenli@gmail.com>
2019-10-23 17:05:29 -0700

    Make RRef::toHere() return a jit::Future (#27943)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27943
    
    This is step 1 to make PyRRef::toHere() non-blocking on caller.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D17936747
    
    Pulled By: mrshenli
    
    fbshipit-source-id: 7cf60e5804e72bdc28f0135fed4d7fdce05ea38a

--- torch/csrc/distributed/rpc/py_rref.cpp
+++ torch/csrc/distributed/rpc/py_rref.cpp
@@ -1,5 +1,7 @@
 #include <torch/csrc/distributed/rpc/py_rref.h>
 
+#include <torch/csrc/distributed/rpc/python_functions.h>
+#include <torch/csrc/distributed/rpc/python_rpc_handler.h>
 #include <torch/csrc/distributed/rpc/rref_context.h>
 #include <torch/csrc/jit/pybind_utils.h>
 
@@ -58,18 +60,21 @@ py::object PyRRef::toHere() {
     }
   } else {
     if (rref_->isPyObj()) {
-      // UserRRef<py::object>::toHere() calls python_rpc_handler which acquires
-      // GIL.
-      return std::static_pointer_cast<UserRRef<py::object>>(rref_)->toHere();
+      auto userRRef = std::static_pointer_cast<UserRRef<py::object>>(rref_);
+      auto future = userRRef->toHere();
+      future->wait();
+      // PythonRpcHandler acquires the GIL on creating the py::object
+      return PythonRpcHandler::getInstance().deserialize(
+          SerializedPyObj::fromIValues(future->value().toTuple()->elements()));
     } else {
-      IValue value =
-          std::static_pointer_cast<UserRRef<IValue>>(rref_)->toHere();
-
+      auto userRRef = std::static_pointer_cast<UserRRef<IValue>>(rref_);
+      auto future = userRRef->toHere();
+      future->wait();
       {
         // acquiring GIL as torch::jit::toPyObject creates new py::object
         // without grabbing the GIL.
         AutoGIL ag;
-        return torch::jit::toPyObject(std::move(value));
+        return torch::jit::toPyObject(future->value());
       }
     }
   }
