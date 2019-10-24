303527d733cb307b7e19c22ddc7c2565fd1cc40d
Will Feng <willfeng@fb.com>
2019-10-24 12:47:40 -0700

    C++ nn::ReplicationPad{1,2,3}d
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/28539
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18115609
    
    Pulled By: yf225
    
    fbshipit-source-id: 15f4ab6a114279bb06bf62f1265b62aa12f8700f

--- torch/csrc/api/src/nn/options/padding.cpp
+++ torch/csrc/api/src/nn/options/padding.cpp
@@ -8,5 +8,9 @@ PadOptions::PadOptions(std::vector<int64_t> pad) : pad_(std::move(pad)) {}
 template struct ReflectionPadOptions<1>;
 template struct ReflectionPadOptions<2>;
 
+template struct ReplicationPadOptions<1>;
+template struct ReplicationPadOptions<2>;
+template struct ReplicationPadOptions<3>;
+
 } // namespace nn
 } // namespace torch
