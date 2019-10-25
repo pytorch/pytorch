9a5c9c9b3d56ad21b2ce578fec42e92735f9e7a0
Igor Fedan <ifedan@fb.com>
2019-10-25 14:47:09 -0400

    explicitly provide memory format when calling to clone() at observer.py
    [ghstack-poisoned]

--- torch/quantization/observer.py
+++ torch/quantization/observer.py
@@ -834,7 +834,7 @@ class RecordingObserver(_ObserverBase):
         self.tensor_val = []
 
     def forward(self, x):
-        self.tensor_val.append(x.clone())
+        self.tensor_val.append(x.clone(memory_format=torch.contiguous_format))
         return x
 
     @torch.jit.export
