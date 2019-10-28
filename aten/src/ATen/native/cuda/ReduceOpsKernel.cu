e2a1ff456bf8dc0677a77b61656d139b0547c21e
Pavel Belevich <pbelevich@fb.com>
2019-10-28 13:04:12 -0700

    Update on "argmax for half datatype fix"
    Differential Revision: [D18175646](https://our.internmc.facebook.com/intern/diff/D18175646)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/ReduceOpsKernel.cu
+++ aten/src/ATen/native/cuda/ReduceOpsKernel.cu
@@ -141,32 +141,40 @@ void or_kernel_cuda(TensorIterator& iter) {
     }), false);
 }
 
-template <typename scalar_t>
+template <typename scalar_t, typename acc_t=scalar_t>
 void max_values_kernel_cuda_impl(TensorIterator& iter) {
   gpu_reduce_kernel<scalar_t, scalar_t>(
-    iter, func_wrapper<scalar_t> ([]GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-      return (THCNumerics<scalar_t>::isnan(a) || a > b) ? a : b;
-    }), at::numeric_limits<scalar_t>::lower_bound());
+    iter, func_wrapper<acc_t> ([]GPU_LAMBDA(acc_t a, acc_t b) -> acc_t {
+      return (THCNumerics<acc_t>::isnan(a) || a > b) ? a : b;
+    }), at::numeric_limits<acc_t>::lower_bound());
 }
 
-template <typename scalar_t>
+template <typename scalar_t, typename acc_t=scalar_t>
 void min_values_kernel_cuda_impl(TensorIterator& iter) {
   gpu_reduce_kernel<scalar_t, scalar_t>(
-    iter, func_wrapper<scalar_t> ([]GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-      return (THCNumerics<scalar_t>::isnan(a) || a < b) ? a : b;
-    }), at::numeric_limits<scalar_t>::upper_bound());
+    iter, func_wrapper<acc_t> ([]GPU_LAMBDA(acc_t a, acc_t b) -> acc_t {
+      return (THCNumerics<acc_t>::isnan(a) || a < b) ? a : b;
+    }), at::numeric_limits<acc_t>::upper_bound());
 }
 
 void max_values_kernel_cuda(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES(iter.dtype(), "max_values_cuda", [&]() {
-    max_values_kernel_cuda_impl<scalar_t>(iter);
-  });
+  if (iter.dtype(1) == kHalf) {
+    max_values_kernel_cuda_impl<at::Half, float>(iter);
+  } else {
+    AT_DISPATCH_ALL_TYPES(iter.dtype(), "max_values_cuda", [&]() {
+      max_values_kernel_cuda_impl<scalar_t>(iter);
+    });
+  }
 }
 
 void min_values_kernel_cuda(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES(iter.dtype(), "min_values_cuda", [&]() {
-    min_values_kernel_cuda_impl<scalar_t>(iter);
-  });
+  if (iter.dtype(1) == kHalf) {
+    min_values_kernel_cuda_impl<at::Half, float>(iter);
+  } else {
+    AT_DISPATCH_ALL_TYPES(iter.dtype(), "min_values_cuda", [&]() {
+      min_values_kernel_cuda_impl<scalar_t>(iter);
+    });
+  }
 }
 
 template <typename scalar_t, typename acc_t=scalar_t>
@@ -187,20 +195,26 @@ void argmin_kernel_cuda_impl(TensorIterator& iter) {
 
 void argmax_kernel_cuda(TensorIterator& iter) {
   if (iter.dtype(1) == kHalf) {
+    // Instead of implementing is_nan and warp_shfl_down 
+    // we can convert halfs to float and do all the operations in float
     argmax_kernel_cuda_impl<at::Half, float>(iter);
+  } else {
+    AT_DISPATCH_ALL_TYPES(iter.dtype(1), "argmax_cuda", [&]() {
+      argmax_kernel_cuda_impl<scalar_t>(iter);
+    });
   }
-  AT_DISPATCH_ALL_TYPES(iter.dtype(1), "argmax_cuda", [&]() {
-    argmax_kernel_cuda_impl<scalar_t>(iter);
-  });
 }
 
 void argmin_kernel_cuda(TensorIterator& iter) {
   if (iter.dtype(1) == kHalf) {
+    // Instead of implementing is_nan and warp_shfl_down 
+    // we can convert halfs to float and do all the operations in float
     argmin_kernel_cuda_impl<at::Half, float>(iter);
+  } else {
+    AT_DISPATCH_ALL_TYPES(iter.dtype(1), "argmin_cuda", [&]() {
+      argmin_kernel_cuda_impl<scalar_t>(iter);
+    });
   }
-  AT_DISPATCH_ALL_TYPES(iter.dtype(1), "argmin_cuda", [&]() {
-    argmin_kernel_cuda_impl<scalar_t>(iter);
-  });
 }
 
 REGISTER_DISPATCH(std_var_stub, &std_var_kernel_cuda);
