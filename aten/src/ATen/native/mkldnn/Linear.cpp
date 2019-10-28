juliusshufan/pytorch-1

Add aten mkldnn backward ops: relu, linear and reshape

aten/src/ATen/native/mkldnn/Linear.cpp
@@ -14,6 +14,22 @@ Tensor mkldnn_linear(
   AT_ERROR("mkldnn_linear: ATen not compiled with MKLDNN support");
 }
 
+Tensor mkldnn_linear_backward_input(
+    IntArrayRef input_size, const Tensor& grad_output, const Tensor& weight) {
+  AT_ERROR("mkldnn_linear_backward_input: ATen not compiled with MKLDNN support");
+}
+
+std::tuple<Tensor, Tensor> mkldnn_linear_backward_weights(
+    const Tensor& grad_output, const Tensor& input, bool bias_defined) {
+  AT_ERROR("mkldnn_linear_backward_weights: ATen not compiled with MKLDNN support");
+}
+
+std::tuple<Tensor, Tensor, Tensor> mkldnn_linear_backward(
+    const Tensor& input, const Tensor& grad_output_t,
+    const Tensor& weight, std::array<bool,3> output_mask) {
+  AT_ERROR("mkldnn_linear_backward: ATen not compiled with MKLDNN support");
+}
+
 } // namespace native
 } // namespace at
 
@@ -40,6 +56,45 @@ Tensor mkldnn_linear(
   }
 
   return new_with_itensor_mkldnn(std::move(y), self.options());
+}
+
+Tensor mkldnn_linear_backward_input(
+    IntArrayRef input_size, const Tensor& grad_output, const Tensor& weight){
+  ideep::tensor& grady = itensor_from_mkldnn(grad_output);
+  ideep::tensor& w = itensor_from_mkldnn(weight);
+
+  ideep::tensor gradx;
+  ideep::inner_product_backward_data::compute(grady, w, {input_size.begin(), input_size.end()}, gradx);
+
+  return new_with_itensor_mkldnn(std::move(gradx), grad_output.options());
+}
+
+std::tuple<Tensor, Tensor> mkldnn_linear_backward_weights(
+    const Tensor& grad_output, const Tensor& input, bool bias_defined) {
+  ideep::tensor& grady = itensor_from_mkldnn(grad_output);
+  ideep::tensor& x = itensor_from_mkldnn(input);
+  ideep::tensor gradw, gradb;
+  if (bias_defined) {
+    ideep::inner_product_backward_weights::compute(x, grady, gradw, gradb);
+  } else {
+    ideep::inner_product_backward_weights::compute(x, grady, gradw);
+  }
+
+  return std::tuple<Tensor, Tensor>{new_with_itensor_mkldnn(std::move(gradw), grad_output.options()),
+    new_with_itensor_mkldnn(std::move(gradb), grad_output.options())};
+}
+
+std::tuple<Tensor, Tensor, Tensor> mkldnn_linear_backward(
+    const Tensor& input, const Tensor& grad_output,
+    const Tensor& weight, std::array<bool,3> output_mask) {
+  Tensor grad_input, grad_weight, grad_bias;
+  if (output_mask[0]) {
+    grad_input = at::mkldnn_linear_backward_input(input.sizes(), grad_output, weight);
+  }
+  if (output_mask[1] || output_mask[2]) {
+    std::tie(grad_weight, grad_bias) = at::mkldnn_linear_backward_weights(grad_output, input, output_mask[2]);
+  }
+  return std::tuple<Tensor, Tensor, Tensor>{grad_input, grad_weight, grad_bias};
 }
 
 } // namespace native

pytorch/pytorch

aten/src/ATen/native/mkldnn/Linear.cpp
@@ -11,7 +11,23 @@ Tensor mkldnn_linear(
     const Tensor& self,
     const Tensor& weight,
     const Tensor& bias) {
-  AT_ERROR("mkldnn_linear: ATen not compiled with MKLDNN support");
+  TORCH_CHECK(false, "mkldnn_linear: ATen not compiled with MKLDNN support");
+}
+
+Tensor mkldnn_linear_backward_input(
+    IntArrayRef input_size, const Tensor& grad_output, const Tensor& weight) {
+  TORCH_CHECK(false, "mkldnn_linear_backward_input: ATen not compiled with MKLDNN support");
+}
+
+std::tuple<Tensor, Tensor> mkldnn_linear_backward_weights(
+    const Tensor& grad_output, const Tensor& input, const Tensor& weight, bool bias_defined) {
+  TORCH_CHECK(false, "mkldnn_linear_backward_weights: ATen not compiled with MKLDNN support");
+}
+
+std::tuple<Tensor, Tensor, Tensor> mkldnn_linear_backward(
+    const Tensor& input, const Tensor& grad_output_t,
+    const Tensor& weight, std::array<bool,3> output_mask) {
+  TORCH_CHECK(false, "mkldnn_linear_backward: ATen not compiled with MKLDNN support");
 }
 
 } // namespace native
@@ -32,17 +48,15 @@ Tensor mkldnn_linear(
       "mkldnn_linear: input needs to has dim at least 2, input dim ", self.dim());
   TORCH_CHECK(self.is_mkldnn(),
       "mkldnn_linear: input needs to be mkldnn layout");
-  TORCH_CHECK(weight.is_mkldnn() && bias.is_mkldnn(),
-      "mkldnn_linear: weight and bias need to be mkldnn layout");
 
   // reshape first if input dim is greater than 2 and the reshape will cost a memory copy.
   auto self_reshaped = self.dim() > 2 ? self.reshape({-1, self.size(self.dim() - 1)}) : self;
   const ideep::tensor x = itensor_from_mkldnn(self_reshaped);
-  const ideep::tensor w = itensor_from_mkldnn(weight);
+  const ideep::tensor w = itensor_from_tensor(weight);
 
   ideep::tensor y;
   if (bias.defined()) {
-    const ideep::tensor b = itensor_from_mkldnn(bias);
+    const ideep::tensor b = itensor_from_tensor(bias);
     ideep::inner_product_forward::compute(x, w, b, y);
   } else {
     ideep::inner_product_forward::compute(x, w, y);
@@ -56,6 +70,65 @@ Tensor mkldnn_linear(
     return new_with_itensor_mkldnn(std::move(y), self.options()).reshape(output_size);
   }
   return new_with_itensor_mkldnn(std::move(y), self.options());
+}
+
+Tensor mkldnn_linear_backward_input(
+    IntArrayRef input_size, const Tensor& grad_output, const Tensor& weight){
+  auto grad_output_reshaped = grad_output.dim() > 2 ?
+    grad_output.reshape({-1, grad_output.size(grad_output.dim() - 1)}) : grad_output;
+  ideep::tensor& grady = itensor_from_mkldnn(grad_output_reshaped);
+  const ideep::tensor w = itensor_from_tensor(weight);
+
+  std::vector<int64_t> input_reshaped_size;
+  input_reshaped_size.push_back(grad_output_reshaped.size(0));
+  input_reshaped_size.push_back(weight.size(1));
+
+  ideep::tensor gradx;
+  ideep::inner_product_backward_data::compute(
+    grady, w, {input_reshaped_size.begin(), input_reshaped_size.end()}, gradx);
+
+  if (input_size.size() > 2) {
+    return new_with_itensor_mkldnn(std::move(gradx), grad_output.options()).reshape(input_size);
+  }
+  return new_with_itensor_mkldnn(std::move(gradx), grad_output.options());
+}
+
+std::tuple<Tensor, Tensor> mkldnn_linear_backward_weights(
+    const Tensor& grad_output, const Tensor& input, const Tensor& weight, bool bias_defined) {
+  auto grad_output_reshaped = grad_output.dim() > 2 ?
+    grad_output.reshape({-1, grad_output.size(grad_output.dim() - 1)}) : grad_output;
+  auto input_reshaped = input.dim() > 2 ? input.reshape({-1, input.size(input.dim() - 1)}) : input;
+
+  ideep::tensor& grady = itensor_from_mkldnn(grad_output_reshaped);
+  ideep::tensor& x = itensor_from_mkldnn(input_reshaped);
+  ideep::tensor gradw, gradb;
+  if (bias_defined) {
+    ideep::inner_product_backward_weights::compute(x, grady, gradw, gradb);
+  } else {
+    ideep::inner_product_backward_weights::compute(x, grady, gradw);
+  }
+
+  if (weight.is_mkldnn()) {
+    return std::tuple<Tensor, Tensor>{new_with_itensor_mkldnn(std::move(gradw), grad_output.options()),
+      new_with_itensor_mkldnn(std::move(gradb), grad_output.options())};
+  } else {
+    return std::tuple<Tensor, Tensor>{
+      mkldnn_to_dense(new_with_itensor_mkldnn(std::move(gradw), grad_output.options())),
+      mkldnn_to_dense(new_with_itensor_mkldnn(std::move(gradb), grad_output.options()))};
+  }
+}
+
+std::tuple<Tensor, Tensor, Tensor> mkldnn_linear_backward(
+    const Tensor& input, const Tensor& grad_output,
+    const Tensor& weight, std::array<bool,3> output_mask) {
+  Tensor grad_input, grad_weight, grad_bias;
+  if (output_mask[0]) {
+    grad_input = at::mkldnn_linear_backward_input(input.sizes(), grad_output, weight);
+  }
+  if (output_mask[1] || output_mask[2]) {
+    std::tie(grad_weight, grad_bias) = at::mkldnn_linear_backward_weights(grad_output, input, weight, output_mask[2]);
+  }
+  return std::tuple<Tensor, Tensor, Tensor>{grad_input, grad_weight, grad_bias};
 }
 
 } // namespace native