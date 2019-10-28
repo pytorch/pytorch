1ee844954c8b5d91adc19dcf67bc8764401bcf2c
Will Feng <willfeng@fb.com>
2019-10-28 17:40:12 -0400

    Update on "[BC-breaking] Fix bugs in torch::tensor constructor"
    Fix bugs in torch::tensor constructor
    
    gh-metadata: pytorch pytorch 28523 gh/yf225/16/head

--- test/cpp/api/tensor.cpp
+++ test/cpp/api/tensor.cpp
@@ -682,7 +682,7 @@ TEST(TensorTest, Data) {
 }
 
 TEST(TensorTest, BackwardAndGrad) {
-  auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   auto y = x * x;
   y.backward();
   ASSERT_EQ(x.grad().item<float>(), 10.0);
@@ -694,7 +694,7 @@ TEST(TensorTest, BackwardAndGrad) {
 }
 
 TEST(TensorTest, BackwardCreatesOnesGrad) {
-  const auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  const auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   x.backward();
   ASSERT_TRUE(torch::equal(x.grad(),
               torch::ones_like(x)));
@@ -708,7 +708,7 @@ TEST(TensorTest, BackwardNonScalarOutputs) {
 }
 
 TEST(TensorTest, IsLeaf) {
-  auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   auto y = x * x;
   ASSERT_TRUE(x.is_leaf());
   ASSERT_FALSE(y.is_leaf());
@@ -721,7 +721,7 @@ TEST(TensorTest, IsLeaf) {
 }
 
 TEST(TensorTest, OutputNr) {
-  auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   auto y = x * x;
   ASSERT_EQ(x.output_nr(), 0);
   ASSERT_EQ(y.output_nr(), 0);
@@ -751,7 +751,7 @@ TEST(TensorTest, Version) {
 }
 
 TEST(TensorTest, Detach) {
-  auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   auto y = x * x;
   const auto y_detached = y.detach();
   ASSERT_FALSE(y.is_leaf());
@@ -766,7 +766,7 @@ TEST(TensorTest, Detach) {
 }
 
 TEST(TensorTest, DetachInplace) {
-  auto x = torch::tensor({5.}, at::TensorOptions().requires_grad(true));
+  auto x = torch::tensor({5}, torch::dtype(torch::kFloat).requires_grad(true));
   auto y = x * x;
   auto y_detached = y.detach_();
   ASSERT_TRUE(y.is_leaf());
