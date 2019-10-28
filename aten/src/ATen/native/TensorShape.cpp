1e5b2559ac2e4e2d70144d15e7a728e9d9341283
Gregory Chanan <gchanan@fb.com>
2019-10-28 08:16:01 -0700

    Write out some set_ overloads instead of relying on code binding generation.
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/28595
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18116720
    
    Pulled By: gchanan
    
    fbshipit-source-id: a917e03aeb8d5513ad3882163642b800ae35dabe

--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -34,6 +34,18 @@ Tensor _shape_as_tensor(const Tensor& self) {
   return at::tensor(self.sizes(), options);
 }
 
+Tensor& set_(Tensor& result, Storage source) {
+  return result.set_(source, 0, static_cast<int64_t>(source.size()), {});
+}
+
+// this needs to be split along CPU/CUDA lines because we don't have a consistent
+// way of getting the allocator to use for a device (c10::GetAllocator is not
+// the same as at::cuda::getCUDADeviceAllocator().
+Tensor& set_cpu_(Tensor& result) {
+  Storage storage(result.dtype(), 0, c10::GetAllocator(kCPU), true);
+  return result.set_(storage, 0, {0}, {});
+}
+
 std::vector<Tensor> broadcast_tensors(TensorList tensors) {
   return expand_outplace(tensors);
 }
