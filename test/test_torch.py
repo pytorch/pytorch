7ac114dfd7ef31a18f782816cbe91cc0b91a9f2b
Pavel Belevich <pbelevich@fb.com>
2019-10-28 11:15:50 -0700

    Update on "argmax for half datatype fix"
    [ghstack-poisoned]

--- test/test_torch.py
+++ test/test_torch.py
@@ -8272,7 +8272,7 @@ class TestTorchDeviceType(TestCase):
                  torch.int64,
                  torch.int32,
                  torch.int16]
-        if device.startswith('cuda'):  # 'cpu' and 'xla' do not support half
+        if torch.device(device).type == 'cuda':  # 'cpu' and 'xla' do not support half
             types.append(torch.half)
 
         # This won't test for 256bit instructions, since we usually
