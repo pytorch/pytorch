5718be7479e980278de0c5d116a5aee50f90ac8b
Shihao Xu <xsh6528@gmail.com>
2019-10-29 12:38:30 -0700

    Update on "Add enum type to rpc registry for consolidating RPC initialization code path"
    Consolidate code paths of ProcessGroupAgent construction and other RPC Backend construction.
    
    Differential Revision: [D5516188](https://our.internmc.facebook.com/intern/diff/D5516188/)
    
    [ghstack-poisoned]

--- test/rpc_test.py
+++ test/rpc_test.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import concurrent.futures
 import sys
-import tempfile
 import unittest
 from collections import namedtuple
 from unittest import mock
