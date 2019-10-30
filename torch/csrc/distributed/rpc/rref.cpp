d635dc94a24b09c37d698942008b7ad982f64de8
Shen Li <cs.shenli@gmail.com>
2019-10-29 19:37:14 -0700

    Support remote for builtin operators in distributed autograd (#28630)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/28630
    
    This includes:
    1. Respect autograd context in rpc.remote for builtin ops
    2. Force setting autograd context in RRef.to_here() even if the
    message for to_here() does not contain any tensor.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18138562
    
    Pulled By: mrshenli
    
    fbshipit-source-id: a39ec83e556d19130f22eb317927241a017000ba

--- torch/csrc/distributed/rpc/rref.cpp
+++ torch/csrc/distributed/rpc/rref.cpp
@@ -1,7 +1,10 @@
 #include <torch/csrc/distributed/rpc/rref.h>
 
+#include <torch/csrc/distributed/autograd/rpc_messages/rpc_with_autograd.h>
+#include <torch/csrc/distributed/autograd/utils.h>
 #include <torch/csrc/distributed/rpc/rref_context.h>
 #include <torch/csrc/distributed/rpc/rref_proto.h>
+#include <torch/csrc/distributed/rpc/utils.h>
 
 namespace torch {
 namespace distributed {
@@ -133,14 +136,36 @@ template <>
 std::shared_ptr<ivalue::Future> UserRRef<IValue>::toHere() {
   auto future = std::make_shared<ivalue::Future>(nullptr);
   auto agent = RpcAgent::getDefaultRpcAgent();
-  auto futureResponse = agent->send(
+
+  // ScriptRRefFetchCall message always carries autograd context id even if
+  // the message itself does not contain any tensor, because the response would
+  // potentially contain tensors.
+  auto futureResponse = autograd::sendMessageWithAutograd(
+      *agent,
       agent->getWorkerInfo(ownerId_),
-      ScriptRRefFetchCall(rrefId()).toMessage());
+      ScriptRRefFetchCall(ownerId_, rrefId()).toMessage(),
+      true /* forceGradRecording */);
 
   futureResponse->addCallback([future](const Message& message) {
     RRefContext::handleException(message);
-    auto rfr = ScriptRRefFetchRet::fromMessage(message);
-    future->markCompleted(rfr->values().front());
+    auto response = deserializeResponse(message);
+    if (message.type() == MessageType::FORWARD_AUTOGRAD_RESP) {
+      auto& rpcWithAutograd =
+          static_cast<autograd::RpcWithAutograd&>(*response);
+
+      // Attach 'recv' autograd function.
+      addRecvRpcBackward(
+          rpcWithAutograd.autogradMetadata(),
+          rpcWithAutograd.tensors(),
+          rpcWithAutograd.fromWorkerId());
+
+      auto& wrappedRpc = rpcWithAutograd.wrappedRpc();
+      auto& rfr = static_cast<ScriptRRefFetchRet&>(wrappedRpc);
+      future->markCompleted(rfr.values().front());
+    } else {
+      auto& rfr = static_cast<ScriptRRefFetchRet&>(*response);
+      future->markCompleted(rfr.values().front());
+    }
   });
   return future;
 }
