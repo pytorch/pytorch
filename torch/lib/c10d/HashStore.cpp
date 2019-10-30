164e69cbccbff43977d585104a1ae34b3e42ac7e
jeremyl <jeremyl@devvm5626.prn2.facebook.com>
2019-10-30 14:32:38 -0700

    [pytorch] Add HashStore to c10d
    This implementation is quite similar to the HashStore in gloo -
    an ephemeral in-process store with a lock and unordered_map<>
    There are just small tweaks for the differences (e.g. add/check methods,
    uint8_t vs char)
    
    It's potentially a better choice for some cases than FileStore when we
    don't need cross-process access, or care about the backing file.
    
    Differential Revision: [D18233713](https://our.internmc.facebook.com/intern/diff/D18233713/)
    
    [ghstack-poisoned]

---
+++ torch/lib/c10d/HashStore.cpp
@@ -0,0 +1,84 @@
+#include <c10d/HashStore.hpp>
+
+#include <stdint.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <chrono>
+#include <cstdio>
+#include <system_error>
+
+namespace c10d {
+
+void HashStore::set(const std::string& key, const std::vector<uint8_t>& data) {
+  std::unique_lock<std::mutex> lock(m_);
+  if (map_.find(key) != map_.end()) {
+    throw std::runtime_error(("Key '" + key) + "' already set");
+  }
+  map_[key] = data;
+  cv_.notify_all();
+}
+
+std::vector<uint8_t> HashStore::get(const std::string& key) {
+  std::unique_lock<std::mutex> lock(m_);
+  auto it = map_.find(key);
+  if (it == map_.end()) {
+    return std::vector<uint8_t>();
+  }
+
+  return it->second;
+}
+
+void HashStore::wait(
+    const std::vector<std::string>& keys,
+    const std::chrono::milliseconds& timeout) {
+  const auto end = std::chrono::steady_clock::now() + timeout;
+  auto pred = [&]() {
+    auto done = true;
+    for (const auto& key : keys) {
+      if (map_.find(key) == map_.end()) {
+        done = false;
+        break;
+      }
+    }
+    return done;
+  };
+
+  std::unique_lock<std::mutex> lock(m_);
+  if (timeout == kNoTimeout) {
+    cv_.wait(lock, pred);
+  } else {
+    if (!cv_.wait_until(lock, end, pred)) {
+      throw std::system_error(
+          ETIMEDOUT, std::system_category(), "Wait timeout");
+    }
+  }
+}
+
+int64_t HashStore::add(const std::string& key, int64_t i) {
+  std::unique_lock<std::mutex> lock(m_);
+  const auto& value = map_[key];
+  int64_t ti = i;
+  if (!value.empty()) {
+    auto buf = reinterpret_cast<const char*>(value.data());
+    auto len = value.size();
+    ti += std::stoll(std::string(buf, len));
+  }
+
+  auto str = std::to_string(ti);
+  const uint8_t* strB = reinterpret_cast<const uint8_t*>(str.c_str());
+  map_[key] = std::vector<uint8_t>(strB, strB + str.size());
+  return ti;
+}
+
+bool HashStore::check(const std::vector<std::string>& keys) {
+  std::unique_lock<std::mutex> lock(m_);
+  for (const auto& key : keys) {
+    if (map_.find(key) == map_.end()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+} // namespace c10d
---
+++ torch/lib/c10d/test/HashStoreTest.cpp
@@ -0,0 +1,53 @@
+#include <c10d/test/StoreTestCommon.hpp>
+
+#include <unistd.h>
+
+#include <iostream>
+#include <thread>
+
+#include <c10d/HashStore.hpp>
+#include <c10d/PrefixStore.hpp>
+
+void testHelper(const std::string prefix = "") {
+  // Basic set/get
+  {
+    c10d::HashStore hashStore;
+    c10d::PrefixStore store(prefix, hashStore);
+    c10d::test::set(store, "key0", "value0");
+    c10d::test::set(store, "key1", "value1");
+    c10d::test::set(store, "key2", "value2");
+    c10d::test::check(store, "key0", "value0");
+    c10d::test::check(store, "key1", "value1");
+    c10d::test::check(store, "key2", "value2");
+  }
+
+  // Hammer on HashStore#add
+  std::vector<std::thread> threads;
+  const auto numThreads = 4;
+  const auto numIterations = 100;
+  c10d::test::Semaphore sem1, sem2;
+  c10d::HashStore hashStore;
+  c10d::PrefixStore store(prefix, hashStore);
+  for (auto i = 0; i < numThreads; i++) {
+    threads.push_back(std::thread([&] {
+      sem1.post();
+      sem2.wait();
+      for (auto j = 0; j < numIterations; j++) {
+        store.add("counter", 1);
+      }
+    }));
+  }
+  sem1.wait(numThreads);
+  sem2.post(numThreads);
+  for (auto& thread : threads) {
+    thread.join();
+  }
+  std::string expected = std::to_string(numThreads * numIterations);
+  c10d::test::check(store, "counter", expected);
+}
+
+int main(int argc, char** argv) {
+  testHelper();
+  testHelper("testPrefix");
+  std::cout << "Test succeeded" << std::endl;
+}
