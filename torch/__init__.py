a7211b2182d59988442cfe5c498d10e4ffe91036
Peter Bell <peterbell10@live.co.uk>
2019-11-01 13:51:33 +0000

    Use absolute_import

--- torch/__init__.py
+++ torch/__init__.py
@@ -9,6 +9,7 @@ Tensors and arbitrary types, and other useful utilities.
 It has a CUDA counterpart, that enables you to run your tensor computations
 on an NVIDIA GPU with compute capability >= 3.0.
 """
+from __future__ import absolute_import
 
 import os
 import sys
