1584289bfb05c0118a9be3a2a809cf555af87e8d
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2019-11-02 15:24:10 +0900

    Raise ValueError if CUDA device is specified without specifying the :
    add print functions
    
    fixed

--- torch/serialization.py
+++ torch/serialization.py
@@ -107,6 +107,9 @@ def validate_cuda_device(location):
         location = str(location)
     if not isinstance(location, _string_classes):
         raise ValueError("location should be a string or torch.device")
+    if len(location) > 4 and location[4] != ':':
+        raise ValueError("location should be specified in the format 'cuda:<device ID>'")
+    
     if location[5:] == '':
         device = 0
     else:
