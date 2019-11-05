a5f9997a2488d13b6b062ccf28525f47bfc433fb
albanD <desmaison.alban@gmail.com>
2019-11-05 13:44:04 -0800

    Update on "Buffer python warning to avoid deadlocks"
    Differential Revision: [D18249633](https://our.internmc.facebook.com/intern/diff/D18249633)
    
    [ghstack-poisoned]

--- c10/util/Exception.h
+++ c10/util/Exception.h
@@ -84,7 +84,7 @@ class C10_API Error : public std::exception {
 
 class C10_API WarningHandler {
   public:
-  virtual ~WarningHandler() noexcept(false) {};
+  virtual ~WarningHandler() noexcept(false) {}
   /// The default warning handler. Prints the message to stderr.
   virtual void process(
       const SourceLocation& source_location,
