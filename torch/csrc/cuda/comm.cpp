9b833af8659f1c75f5923a3e7a1b1194455a61c2
Edward Z. Yang <ezyang@fb.com>
2019-11-06 11:52:50 -0500

    Update on "Delete all trivial uses of make_variable."
    A trivial use of make_variable is one where requires_grad=False.  This
    transformation is not technically semantics preserving, as make_variable
    will create a shallow copy of the tensor in question; however, I
    am guessing that we have the invariant that we don't actually make
    use of this shallow copy in a nontrivial way.
    
    There were some cases where the surrounding code expected a Variable proper
    to be returned; I retained those sites.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- torch/csrc/cuda/comm.cpp
+++ torch/csrc/cuda/comm.cpp
@@ -86,9 +86,11 @@ std::vector<Tensor> broadcast(const Tensor& tensor, IntArrayRef devices) {
 // one of them is modified in-place during `forward` but the other is needed in
 // backward, autograd engine will complain.
 //
-// We thus re-wrap these Variables after broadcasting (i.e., effetively doing
+// We thus re-wrap these Variables after broadcasting (i.e., effectively doing
 // what is equivalent to .data in Python), and give them individual version
 // counters.
+//
+// NB: Just calling detach() on the variables is not sufficient
 //
 // NB: For `device[0]` in broadcast_coalesced, the input Variables are always
 //     returned as-is, so **do not** re-wrap them.
@@ -132,7 +134,7 @@ tensor_list2d broadcast_coalesced(TensorList tensors, IntArrayRef devices, size_
           // See NOTE [ Version Counter in comm.*_coalesced ]
           AT_ASSERT(t.is_variable());
           Variable var = t;
-          device_outputs.push_back(var.tensor_data());
+          device_outputs.push_back(make_variable(var.tensor_data(), false));
         }
       }
     } else {
@@ -145,7 +147,7 @@ tensor_list2d broadcast_coalesced(TensorList tensors, IntArrayRef devices, size_
           // See NOTE [ Version Counter in comm.*_coalesced ]
           AT_ASSERT(t.is_variable());
           Variable var = t;
-          device_outputs.push_back(var.tensor_data());
+          device_outputs.push_back(make_variable(var.tensor_data(), false));
         }
       }
     }
