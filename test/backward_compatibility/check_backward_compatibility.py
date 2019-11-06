7ac9c05fb84b0734f1513486dab61cd3467bd16a
Edward Z. Yang <ezyang@fb.com>
2019-11-06 08:22:37 -0800

    Back out "Revert D18299298: [pytorch][PR] Migrate conv3d from TH to ATen (CPU)"
    Original commit changeset: 33057d5a91d1
    
    Differential Revision: [D18349945](https://our.internmc.facebook.com/intern/diff/D18349945/)
    
    [ghstack-poisoned]

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -22,6 +22,8 @@ white_list = [
     ('thnn_conv2d_backward', datetime.date(2019, 10, 30)),
     ('thnn_conv_depthwise2d_backward', datetime.date(2019, 10, 30)),
     ('thnn_conv3d_backward', datetime.date(2019, 10, 30)),
+    ('thnn_conv3d', datetime.date(2019, 11, 4)),
+    ('thnn_conv3d.out', datetime.date(2019, 11, 6)),
     ('empty_like', datetime.date(2019, 10, 30)),
     ('rand_like', datetime.date(2019, 11, 11)),
     ('ones_like', datetime.date(2019, 11, 11)),
