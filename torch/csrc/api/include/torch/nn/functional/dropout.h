66acac61f6051198e4953471856e20d48a54288e
Gaurav Tamba <gaurav.tamba@gmail.com>
2019-11-06 23:19:34 +0530

    correct tensor reference

--- torch/csrc/api/include/torch/nn/functional/dropout.h
+++ torch/csrc/api/include/torch/nn/functional/dropout.h
@@ -9,25 +9,25 @@ namespace functional {
 
 inline Tensor dropout(Tensor input, const DropoutOptions& options) {
   if (options.inplace()) {
-    return torch::dropout_(input, options.p(), this->is_training());
+    return torch::dropout_(input, options.p(), input->is_training());
   } else {
-    return torch::dropout(input, options.p(), this->is_training()); 
+    return torch::dropout(input, options.p(), input->is_training()); 
   }
 }
 
 inline Tensor dropout2d(Tensor input, const Dropout2dOptions& options) {
   if (options.inplace()) {
-    return torch::feature_dropout_(input, options.p(), this->is_training());
+    return torch::feature_dropout_(input, options.p(), input->is_training());
   } else {
-    return torch::feature_dropout(input, options.p(), this->is_training()); 
+    return torch::feature_dropout(input, options.p(), input->is_training()); 
   }
 }
 
 inline Tensor dropout3d(Tensor input, const Dropout3dOptions& options) {
   if (options.inplace()) {
-    return torch::feature_dropout_(input, options.p(), this->is_training());
+    return torch::feature_dropout_(input, options.p(), input->is_training());
   } else {
-    return torch::feature_dropout(input, options.p(), this->is_training()); 
+    return torch::feature_dropout(input, options.p(), input->is_training()); 
   }
 }
 
