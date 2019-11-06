480efe3402ef0c5371e715c4be45db0fa95e835f
Vitaly Fedyunin <vitalyf@fb.com>
2019-11-06 13:28:44 -0800

    Revert cudnn chnages #23861
    ```python
    x = torch.randn(192,16,50).cuda()
    x = x.permute(0,2,1).contiguous().permute(0,2,1)
    m = torch.nn.Conv1d(
           in_channels=16,
           out_channels=32,
           kernel_size=2,
           bias=True,
      ).cuda()
    
    m(x)
    ```
    
    This reverts commit 8160f390cf678b3b98e0c1f73bd289ee3c96afcb.

--- aten/src/ATen/cudnn/Descriptors.h
+++ aten/src/ATen/cudnn/Descriptors.h
@@ -140,8 +140,8 @@ public:
   void set(const at::Tensor &t, int64_t pad = 0);
 
 private:
-  void set(cudnnDataType_t dataType, int dim, int* size, cudnnTensorFormat_t filter_format) {
-    AT_CUDNN_CHECK(cudnnSetFilterNdDescriptor(mut_desc(), dataType, filter_format, dim, size));
+  void set(cudnnDataType_t dataType, int dim, int* size) {
+    AT_CUDNN_CHECK(cudnnSetFilterNdDescriptor(mut_desc(), dataType, CUDNN_TENSOR_NCHW, dim, size));
   }
 };
 
