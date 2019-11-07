0515002ee18fcb50f97fbb415931d88e1b88c19b
Anjali Chourdia <chourdiaanjali@fb.com>
2019-11-07 10:46:42 -0500

    clang tidy default constructor error fix

--- torch/csrc/api/src/nn/options/embedding.cpp
+++ torch/csrc/api/src/nn/options/embedding.cpp
@@ -2,11 +2,11 @@
 
 namespace torch {
 namespace nn {
-EmbeddingOptions::EmbeddingOptions() {}
+EmbeddingOptions::EmbeddingOptions() = default;
 EmbeddingOptions::EmbeddingOptions(int64_t num_embeddings, int64_t embedding_dim) :
  num_embeddings_(num_embeddings), embedding_dim_(embedding_dim) {}
 
-EmbeddingBagOptions::EmbeddingBagOptions() {}
+EmbeddingBagOptions::EmbeddingBagOptions() = default;
 EmbeddingBagOptions::EmbeddingBagOptions(int64_t num_embeddings, int64_t embedding_dim) :
  num_embeddings_(num_embeddings), embedding_dim_(embedding_dim) {}
 } // namespace nn
