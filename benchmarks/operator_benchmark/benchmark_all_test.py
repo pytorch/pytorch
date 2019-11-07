960fdf69144c505207961e2e6acae78d43312fae
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-11-07 14:55:18 -0800

    Factoring out quantized benchmark imports

--- benchmarks/operator_benchmark/benchmark_all_test.py
+++ benchmarks/operator_benchmark/benchmark_all_test.py
@@ -8,9 +8,11 @@ from pt import ( # noqa
     add_test, batchnorm_test, cat_test, chunk_test, conv_test,  # noqa
     gather_test, linear_test, matmul_test, pool_test,  # noqa
     softmax_test, split_test, unary_test, fill_test, as_strided_test,  # noqa
-    # Quantized tests
-    qactivation_test, qconv_test, qlinear_test,
 )
+
+# Quantized tests
+from benchmark_all_quantized_test import *  # noqa
+
 
 if __name__ == "__main__":
     op_bench.benchmark_runner.main()
