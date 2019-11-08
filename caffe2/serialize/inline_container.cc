fa7c78c456d7cbdc222bdf48e8688917331b2691
Jeremy Lilley <jeremyl@fb.com>
2019-11-08 14:24:35 -0800

    Pickler: convert std::stringstream cases. (#29351)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29351
    
    When torch::save()ing a smallish tensor, we spend ~5% of the time
    still in std::stringstream constructors.
    
    This removes the last couple of cases. Benchmark shows ~5% improvement:
      TorchSaveSmallTensor Pre: 13.12us
      TorchSaveSmallTensor Post: 12.48us
    ghstack-source-id: 93517928
    
    Test Plan:
    buck build mode/opt experimental/jeremyl/c2:
       buck-out/opt/gen/experimental/jeremyl/c2/SerializationBench  --bm_regex=TorchSaveSmallTensor
    
    Differential Revision: D18365066
    
    fbshipit-source-id: a3284bec004751cedae1cdadf27f969422faff8e

--- caffe2/serialize/inline_container.cc
+++ caffe2/serialize/inline_container.cc
@@ -284,9 +284,9 @@ void PyTorchStreamWriter::setup(const string& file_name) {
   mz_zip_writer_init_v2(ar_.get(), 0, MZ_ZIP_FLAG_WRITE_ZIP64);
   valid("initializing archive ", file_name.c_str());
 
-  std::ostringstream version;
-  version << kProducedFileFormatVersion << "\n";
-  writeRecord("version", version.str().c_str(), version.str().size());
+  std::string version = c10::to_string(kProducedFileFormatVersion);
+  version.push_back('\n');
+  writeRecord("version", version.c_str(), version.size());
 }
 
 void PyTorchStreamWriter::writeRecord(
