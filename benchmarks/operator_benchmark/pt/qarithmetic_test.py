4874120804d702dde293ebddfad26c5fe4993f9c
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-11-09 14:48:21 -0800

    Added all binary arithmetic tests in QFunctional
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/29424
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18385689
    
    Pulled By: z-a-f
    
    fbshipit-source-id: 5947e0edfcbe2b6eba984dc9da187e9fce5cd40f

--- benchmarks/operator_benchmark/pt/qarithmetic_test.py
+++ benchmarks/operator_benchmark/pt/qarithmetic_test.py
@@ -11,6 +11,18 @@ qarithmetic_binary_configs = op_bench.cross_product_configs(
     N=(2, 8, 64, 512),
     dtype=(torch.quint8, torch.qint8, torch.qint32),
     contig=(False, True),
+    tags=('short',)
+)
+
+qarithmetic_binary_ops = op_bench.op_list(
+    attrs=(
+        ('add', 'add'),
+        ('add_scalar', 'add_scalar'),
+        ('add_relu', 'add_relu'),
+        ('mul', 'mul'),
+        ('mul_scalar', 'mul_scalar'),
+    ),
+    attr_names=('op_name', 'op_func'),
 )
 
 
@@ -21,32 +33,39 @@ I.e. `self.qop = 'add'`
 """
 class _QFunctionalBinaryArithmeticBenchmarkBase(op_bench.TorchBenchmarkBase):
     def setup(self, N, dtype, contig):
+        self.qfunctional = torch.nn.quantized.QFunctional()
+
         # TODO: Consider more diverse shapes
         f_input = (torch.rand(N, N) - 0.5) * 256
         scale = 1.0
         zero_point = 0
 
-        self.q_input = torch.quantize_per_tensor(f_input, scale=scale,
-                                                 zero_point=zero_point,
-                                                 dtype=dtype)
+        self.q_input_a = torch.quantize_per_tensor(f_input, scale=scale,
+                                                   zero_point=zero_point,
+                                                   dtype=dtype)
 
         if not contig:
             permute_dims = list(range(f_input.ndim))[::-1]
-            self.q_input = self.q_input.permute(permute_dims)
-
-        self.qfunctional = QFunctional()
+            self.q_input_a = self.q_input_a.permute(permute_dims)
 
     def forward(self):
-        return getattr(self.qfunctional, self.qop)(self.q_input, self.q_input)
+        return getattr(self.qfunctional, self.qop)(self.q_input_a,
+                                                   self.q_input_b)
 
 
 class QFunctionalAddBenchmarkBase(_QFunctionalBinaryArithmeticBenchmarkBase):
-    def init(self, N, dtype, contig):
+    def init(self, N, dtype, contig, op_func):
         super(QFunctionalAddBenchmarkBase, self).setup(N, dtype, contig)
-        self.qop = 'add'
+        self.qop = op_func
+        if self.qop.endswith('_scalar'):
+            self.q_input_b = 42
+        else:
+            self.q_input_b = self.q_input_a
 
 
-op_bench.generate_pt(qarithmetic_binary_configs, QFunctionalAddBenchmarkBase)
+op_bench.generate_pt_tests_from_op_list(qarithmetic_binary_ops,
+                                        qarithmetic_binary_configs,
+                                        QFunctionalAddBenchmarkBase)
 
 
 if __name__ == '__main__':
