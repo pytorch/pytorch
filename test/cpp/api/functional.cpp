604fc9ec41aaa243460090dc93a02ed26f10505c
Anjali Chourdia <chourdiaanjali@fb.com>
2019-11-11 11:50:01 -0800

    F::embedding, F::embedding_bag, moved Embedding and EmbeddingBag options to embedding.h in options
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/28669
    
    Differential Revision: D18377609
    
    Pulled By: anjali411
    
    fbshipit-source-id: 6a2c547368849ebd1a2f8828cfbe7252152b26a2

--- test/cpp/api/functional.cpp
+++ test/cpp/api/functional.cpp
@@ -971,6 +971,32 @@ TEST_F(FunctionalTest, Linear) {
   }
 }
 
+TEST_F(FunctionalTest, Embedding) {
+  const auto input = torch::tensor({{1,2,4,5}, {4,3,2,9}}, torch::kLong);
+  auto weight = torch::empty({10, 3});
+  torch::nn::init::normal_(weight);
+  auto y = F::embedding(input, weight);
+  auto y_exp = torch::embedding(weight, input.contiguous(), -1, false, false);
+  ASSERT_TRUE(torch::allclose(y, y_exp));
+}
+
+TEST_F(FunctionalTest, EmbeddingBag) {
+  const auto input = torch::tensor({1,2,4,5,4,3,2,9}, torch::kLong);
+  auto offsets = torch::tensor({0,4}, torch::kLong);
+  auto weight = torch::empty({10, 3});
+  torch::nn::init::normal_(weight);
+  auto y = F::embedding_bag(input, weight, EmbeddingBagOptions().mode(torch::kSum), offsets);
+  auto y_exp = std::get<0>(torch::embedding_bag(weight, input, offsets, false, 0, false, torch::Tensor()));
+  ASSERT_TRUE(torch::allclose(y, y_exp));
+
+  // no options test
+  const auto input_ = torch::tensor({{1,2,4,5}, {4,3,2,9}}, torch::kLong);
+  auto offsets_ = torch::arange(0, input_.numel(), input_.size(1), torch::TensorOptions().dtype(torch::kLong).device(input.device()));
+  y = F::embedding_bag(input_, weight);
+  y_exp = std::get<0>(torch::embedding_bag(weight, input_.reshape(-1), offsets_, false, 1, false, torch::Tensor()));
+  ASSERT_TRUE(torch::allclose(y, y_exp));
+}
+
 TEST_F(FunctionalTest, Bilinear) {
   auto input1 = torch::tensor({{1, 2, 3}, {7, 6, 5}});
   auto input2 = torch::tensor({{7, 4}, {8 ,9}});
