211c5d692a7ca989eea878a552a42d7f0e469fe2
Will Feng <willfeng@fb.com>
2019-11-11 14:34:36 -0500

    use unique instead of shared
    gh-metadata: pytorch pytorch 29569 gh/yf225/58/head

--- torch/csrc/api/include/torch/optim/optimizer.h
+++ torch/csrc/api/include/torch/optim/optimizer.h
@@ -32,26 +32,33 @@ namespace torch {
 namespace optim {
 namespace detail {
 
-class TORCH_API OptimizerParamStateBase {};
+class TORCH_API OptimizerParamStateBase {
+ public:
+  virtual std::unique_ptr<OptimizerParamStateBase> clone() const = 0;
+};
 
-class TORCH_API OptimizerOptionsBase {};
+class TORCH_API OptimizerOptionsBase {
+ public:
+  virtual std::unique_ptr<OptimizerOptionsBase> clone() const = 0;
+};
 
 class TORCH_API OptimizerParamGroup {
  public:
+  OptimizerParamGroup(const OptimizerParamGroup& param_group) : params_(param_group.params()), options_(param_group.has_options() ? param_group.options()->clone() : nullptr) {}
   OptimizerParamGroup(std::vector<Tensor> params) : params_(params) {}
-  OptimizerParamGroup(std::vector<Tensor> params, std::shared_ptr<OptimizerOptionsBase> options) : params_(params), options_(options) {}
+  OptimizerParamGroup(std::vector<Tensor> params, std::unique_ptr<OptimizerOptionsBase> options) : params_(params), options_(std::move(options)) {}
 
   bool has_options() const {
     return options_ != nullptr;
   }
 
-  std::shared_ptr<OptimizerOptionsBase> options() const {
+  OptimizerOptionsBase* options() const {
     TORCH_CHECK(has_options());
-    return options_;
+    return options_.get();
   }
 
-  void set_options(std::shared_ptr<OptimizerOptionsBase> options) {
-    options_ = options;
+  void set_options(std::unique_ptr<OptimizerOptionsBase> options) {
+    options_ = std::move(options);
   }
 
   std::vector<Tensor>& params() {
@@ -64,7 +71,7 @@ class TORCH_API OptimizerParamGroup {
 
  protected:
   std::vector<Tensor> params_;
-  std::shared_ptr<OptimizerOptionsBase> options_;
+  std::unique_ptr<OptimizerOptionsBase> options_;
 };
 
 /// Base class for all optimizers, that does not yet define a `step()`
@@ -73,25 +80,34 @@ class TORCH_API OptimizerParamGroup {
 /// shall have, such as `zero_grad`.
 class TORCH_API OptimizerBase {
  public:
+  // The copy constructor is deleted, because the user should rely on the
+  // `state_dict` / `load_state_dict` API to copy an optimizer instead.
+  OptimizerBase(const OptimizerBase& optimizer_base) = delete;
+  OptimizerBase(OptimizerBase&& optimizer_base) = default;
+
   /// Constructs the `Optimizer` from a vector of parameters.
   explicit OptimizerBase(std::vector<Tensor> parameters);
 
   //todo
-  explicit OptimizerBase(std::vector<OptimizerParamGroup> param_groups, std::shared_ptr<OptimizerOptionsBase> defaults) : defaults_(defaults) {
-    for (auto& param_group : param_groups) {
+  explicit OptimizerBase(std::vector<OptimizerParamGroup> param_groups, std::unique_ptr<OptimizerOptionsBase> defaults) : defaults_(std::move(defaults)) {
+    for (const auto& param_group : param_groups) {
       add_param_group(param_group);
     }
   }
 
-  void add_param_group(OptimizerParamGroup param_group) {
+  void add_param_group(const OptimizerParamGroup& param_group) {
     for (const auto& param : param_group.params()) {
       TORCH_CHECK(param.is_leaf(), "can't optimize a non-leaf Tensor");
     }
+
+    OptimizerParamGroup param_group_(param_group.params());
     if (!param_group.has_options()) {
-      param_group.set_options(defaults_);
+      param_group_.set_options(defaults_->clone());
+    } else {
+      param_group_.set_options(param_group.options()->clone());
     }
     // TODO: check "some parameters appear in more than one parameter group"
-    param_groups_.push_back(param_group);
+    param_groups_.push_back(std::move(param_group_));
   }
 
   virtual ~OptimizerBase() = default;
@@ -143,9 +159,9 @@ class TORCH_API OptimizerBase {
   /// The parameters this optimizer optimizes.
   std::vector<Tensor> parameters_;
   //to do-description
-  std::shared_ptr<OptimizerOptionsBase> defaults_;
+  std::unique_ptr<OptimizerOptionsBase> defaults_;
   std::vector<OptimizerParamGroup> param_groups_;
-  ska::flat_hash_map<at::TensorImpl*, std::shared_ptr<OptimizerParamStateBase>> state_;
+  ska::flat_hash_map<at::TensorImpl*, std::unique_ptr<OptimizerParamStateBase>> state_;
 };
 
 /// Serializes an `OptimizerBase` into an `OutputArchive`.
