42ea0eb8d43951b9b365e4dece7d471b7d9af165
Will Feng <willfeng@fb.com>
2019-11-11 14:33:09 -0500

    updated step function and added state
    gh-metadata: pytorch pytorch 29559 gh/yf225/48/head

--- torch/csrc/api/src/optim/adagrad.cpp
+++ torch/csrc/api/src/optim/adagrad.cpp
@@ -11,37 +11,48 @@
 namespace torch {
 namespace optim {
 
-AdagradOptions::AdagradOptions(double learning_rate)
+AdagraddefaultOptions::AdagraddefaultOptions(double learning_rate)
     : learning_rate_(learning_rate) {}
 
 /// Adapted from
 /// https://github.com/pytorch/pytorch/blob/master/torch/optim/adagrad.py
 void Adagrad::step() {
-  for (size_t i = 0; i < parameters_.size(); ++i) {
-    Tensor p = parameters_.at(i);
-    if (!p.grad().defined()) {
-      continue;
-    }
-    auto grad = p.grad().data();
-    // at::IValue curr_state = state[""]
-    // state = self.state[p]
-    // state['step'] += 1
-    if (options.weight_decay() != 0) {
-      //TORCH_CHECK(!p.grad().data().is_sparse(), "weight_decay option is not compatible with sparse gradients");
+  for (auto group : param_groups) {
+    for (auto p : group.at("params")) {
+      if (!p.grad().defined()) {
+        continue;
+      }
+      auto grad = p.grad().data();
+      auto& state_ = state.at(p.unsafeGetTensorImpl());
+      state_.at("step").toInt()+=1;
+
+      if(group.at("options").weight_decay() != 0) {
+        TORCH_CHECK(!p.grad().data().is_sparse(), "weight_decay option is not compatible with sparse gradients");
+
+        NoGradGuard guard;
+        grad += group.at("options").weight_decay() * p.data();
+      }
+
+      const auto clr =  group.at("options").learning_rate() /
+          (1.0 + (state_.at("step").toInt() - 1.0) * group.at("options").lr_decay());
+
+      if(grad.is_sparse()) {
+        grad = grad.coalesce();
+        auto grad_indices = grad._indices();
+        auto grad_values = grad._values();
+        auto size = grad.size();
+        //add a lambda fn
+        //ad makesparse fn
+      }
+      else {
+        //fill up
+      }
+      auto& sum = state_.at("sum").toTensor();
+      sum.addcmul_(grad, grad, 1.0);
+      const auto std = state_.at("sum").toTensor().sqrt().add_(1e-10);
       NoGradGuard guard;
-      p.grad() = p.grad() + options.weight_decay() * p;
+      p.addcdiv_(grad, std, -clr);
     }
-
-    buffer_at(step_buffers, i) += 1.0;
-    const auto clr = options.learning_rate() /
-        (1.0 + (buffer_at(step_buffers, i) - 1.0) * options.lr_decay());
-
-    auto& sum = buffer_at(sum_buffers, i);
-    sum.addcmul_(p.grad(), p.grad(), 1.0);
-    const auto std = buffer_at(sum_buffers, i).sqrt().add_(1e-10);
-
-    NoGradGuard guard;
-    p.addcdiv_(p.grad(), std, -clr);
   }
 }
 
