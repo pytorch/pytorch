e46d0d3ecfa58e4b8c7cf2c476b8d43ce5ca82eb
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-11-12 00:43:42 -0800

    Update on "Benchmarking per channel quantization"
    Differential Revision: [D18443929](https://our.internmc.facebook.com/intern/diff/D18443929)

--- benchmarks/operator_benchmark/pt/quantization_test.py
+++ benchmarks/operator_benchmark/pt/quantization_test.py
@@ -12,24 +12,34 @@ import torch.quantization as tq
 
 """Microbenchmarks for general quantization operations."""
 
-quantize_configs_short = op_bench.config_list(
-    # mode is used to show the direction of the benchmark:
-    # if 'Q', benchmark quantization, else dequantization
-    attr_names=['C', 'M', 'N', 'dtype', 'mode'],
-    attrs=[
+# mode is used to show the direction of the benchmark:
+# if 'Q', benchmark quantization, else dequantization
+
+quantize_configs_short_dict = {
+    'attr_names': ['C', 'M', 'N', 'dtype', 'mode'],
+    'attrs': [
         [3, 512, 512, torch.quint8, 'Q'],
         [3, 512, 512, torch.quint8, 'D'],
     ],
-    tags=['short']
+    'tags': ['short'],
+}
+
+quantize_configs_long_dict = {
+    'C': [3, 5, 8],  # this is reused for per-channel: avoid single channel test
+    'M': [256, 1024],
+    'N': [256, 1024],
+    'dtype': [torch.quint8, torch.qint8, torch.qint32],
+    'mode': ['D', 'Q'],
+    'tags': ['long'],
+}
+
+
+quantize_per_tensor_configs_short = op_bench.config_list(
+    **quantize_configs_short_dict
 )
 
-quantize_configs_long = op_bench.cross_product_configs(
-    C=[1, 3, 8],
-    M=[256, 1024],
-    N=[256, 1024],
-    dtype=[torch.quint8, torch.qint8, torch.qint32],
-    mode=['D', 'Q'],
-    tags=['long']
+quantize_per_tensor_configs_long = op_bench.cross_product_configs(
+    **quantize_configs_long_dict
 )
 
 
@@ -51,28 +61,38 @@ class QuantizePerTensorBenchmark(op_bench.TorchBenchmarkBase):
         return self.op(self.input)
 
 
-op_bench.generate_pt_test(quantize_configs_short + quantize_configs_long,
-                          QuantizePerTensorBenchmark)
+op_bench.generate_pt_test(
+    quantize_per_tensor_configs_short + quantize_per_tensor_configs_long,
+    QuantizePerTensorBenchmark)
 
+# === Per Channel quantization ===
+
+quantize_per_channel_configs_short = op_bench.config_list(
+    cross_product_configs={
+        'axis': (0,)
+    },
+    **quantize_configs_short_dict
+)
+
+quantize_per_channel_configs_long = op_bench.cross_product_configs(
+    axis=(0, 1, 2),
+    **quantize_configs_long_dict
+)
 
 class QuantizePerChannelBenchmark(op_bench.TorchBenchmarkBase):
     r"""Benchmarks both quantization and dequantization."""
-    def init(self, C, M, N, dtype, mode):
+    def init(self, C, M, N, dtype, axis, mode):
         assert(mode in ('Q', 'D'))
         self.input = torch.rand(C, M, N)
-        self.dtype = dtype
-
         self.op = torch.quantize_per_channel
 
-        scales = torch.tensor([1.0] * C)
-        zero_points = torch.tensor([0] * C)
-        dtype = dtype
+        channel_len = (C, M, N)[axis]
 
         self.kwargs = {
-            'scales': scales,
-            'zero_points': zero_points,
+            'scales': torch.tensor([1.0] * channel_len),
+            'zero_points': torch.tensor([0] * channel_len),
             'dtype': dtype,
-            'axis': 0
+            'axis': axis
         }
 
         self.set_module_name('QuantizePerChannel')
@@ -87,9 +107,11 @@ class QuantizePerChannelBenchmark(op_bench.TorchBenchmarkBase):
         return self.op(self.input, **self.kwargs)
 
 
-op_bench.generate_pt_test(quantize_configs_short + quantize_configs_long,
-                          QuantizePerChannelBenchmark)
+op_bench.generate_pt_test(
+    quantize_per_channel_configs_short + quantize_per_channel_configs_long,
+    QuantizePerChannelBenchmark)
 
+# === Fake Quantization ===
 
 fake_quantize_configs_short = op_bench.config_list(
     # mode is used to show the direction of the benchmark:
@@ -121,8 +143,9 @@ class FakeQuantizeBenchmark(op_bench.TorchBenchmarkBase):
         return self.op(self.input)
 
 
-op_bench.generate_pt_test(fake_quantize_configs_short + fake_quantize_configs_long,
-                          FakeQuantizeBenchmark)
+op_bench.generate_pt_test(
+    fake_quantize_configs_short + fake_quantize_configs_long,
+    FakeQuantizeBenchmark)
 
 
 if __name__ == "__main__":
