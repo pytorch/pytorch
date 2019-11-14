4bd2d89f0d5e33e354eb22dc98b6134fd2a47ada
jeremyl <jeremyl@devvm5626.prn2.facebook.com>
2019-11-14 10:36:30 -0800

    Update on "[pytorch] Improve process_group_agent() serialization speed"
    TLDR: This change improves process_group's serialization speed:
    
    ```
      Serialize_Tensor64:     12.38us ->   1.99us  (~-84%)
      Deserialize_Tensor64:   33.89us ->   5.62us  (~-84%)
      Serialize_Tensor1M:    525.74us -> 285.43us  (~-45%)
      Deserialize_Tensor1M:  892.61us -> 273.68us  (~-70%)
    ```
    
    After speaking with the jit team, we had consensus that torch::save()/load()
    are somewhat high-overhead for RPC serialization, mostly intended for
    persistent disk data.
    
    (Particularly, for large tensors, 35% of the time is spent in CRC checking, even
    with the fb-side changes to subsitute 40x faster SSE-accelerated crc checking;
    Also, for small tensors, the zip container overhead is considerable, as is the
    overhead of lexing/parsing an embedded text python program for each RPC).
    
    The jit team encouraged us to use jit::pickler, with the WriteableTensorData
    way of outputting result tensors (not the default side-tensor table, or
    with pickling the actual tensors). This ends up just pickling some tensor
    metadata, and giving us some tensor blobs that we can mindlessly
    blit over the wire (they copy to cpu memory if needed).
    
    There is yet no standardized container format for the pickled data
    (there is jit::pickle_save() checked in, but but it's experimental,
    no load function is yet provided), but they encouraged us to just use
    something sensible for this, and possibly revisit later. For now, I made
    the directory headers slightly http-inspired.
    
    Note that serialization is just one component of the pipeline, but that
    said, we also see reasonable reductions in end-to-end echo times (noisier):
    
    ```
       ProcessGroupAgent_Echo(Tensor_Small)   855.25us -> 492.65us  (~-42%)
       ProcessGroupAgent_Echo(Tensor_1M)       10.82ms -> 6.94ms    (~-35%)
       ProcessGroupAgent_Echo(Small_NoTensor) 688.82us -> 301.72us  (~-56%)
       ProcessGroupAgent_Echo(1MB_NoTensor)     4.65ms -> 3.71ms    (~-20%)
    ```
    
    I moved the "wire serialization" logic to a separate file to assist with
    unittesting.
    
    Differential Revision: [D18493938](https://our.internmc.facebook.com/intern/diff/D18493938/)
    
    [ghstack-poisoned]

--- test/cpp/api/serialize.cpp
+++ test/cpp/api/serialize.cpp
@@ -2,7 +2,6 @@
 
 #include <c10/util/tempfile.h>
 
-#include <torch/csrc/distributed/rpc/utils.h>
 #include <torch/torch.h>
 
 #include <test/cpp/api/support.h>
@@ -494,33 +493,4 @@ TEST(SerializeTest, UnserializableSubmoduleIsIgnoredWhenLoadingModule) {
   // `output` should equal to the sum of the values we manually assigned to "b.foo" before
   // serialization.
   ASSERT_EQ(output, 5);
-}
-
-TEST(SerializeTest, WireSerialize) {
-  auto run = [](const std::string& payload,
-                const std::vector<at::Tensor>& tensors) {
-    std::string serialized;
-    {
-      std::vector<char> mpayload(payload.begin(), payload.end());
-      std::vector<at::Tensor> mtensors = tensors;
-      serialized = torch::distributed::rpc::wireSerialize(
-          std::move(mpayload), std::move(mtensors));
-    }
-    auto deser = torch::distributed::rpc::wireDeserialize(
-        serialized.data(), serialized.size());
-    EXPECT_EQ(payload.size(), deser.first.size());
-    EXPECT_EQ(tensors.size(), deser.second.size());
-    if (payload.size() > 0) {
-      EXPECT_TRUE(
-          memcmp(deser.first.data(), payload.data(), payload.size()) == 0);
-    }
-    for (size_t i = 0; i < tensors.size(); ++i) {
-      EXPECT_TRUE(torch::equal(tensors[i], deser.second[i]));
-    }
-  };
-  run("", {});
-  run("hi", {});
-  run("", {torch::randn({5, 5})});
-  run("hi", {torch::randn({5, 5})});
-  run("more", {torch::randn({5, 5}), torch::rand({10, 10})});
 }
