d43e98e0813ba2a682d645a030757fda8c9e2fed
yanlizhao <yanlizhao@fb.com>
2019-11-15 14:56:10 -0800

    make python remote exception to rethrow when using remote reference to itself
    Right now, python call remote exception rethrown is coupled with deserializtiaon.
    For owner ref, the setValue() and getValue() do not use serialization and deserialization, so when users create a ref to itself, and call ownerRef.to_here(), python call remote exception will not be rethrown.
    
    This diff is to move remote exception rethrown out of deserialization, and exception can be handled for owner ref getValue()
    
    close #29924
    
    Differential Revision: [D18541916](https://our.internmc.facebook.com/intern/diff/D18541916/)
    
    [ghstack-poisoned]

--- torch/csrc/distributed/rpc/rref.cpp
+++ torch/csrc/distributed/rpc/rref.cpp
@@ -199,11 +199,20 @@ template class UserRRef<py::object>;
 
 //////////////////////////  OwnerRRef  /////////////////////////////////////
 
-template <typename T>
-const T& OwnerRRef<T>::getValue() const {
+template <>
+const IValue& OwnerRRef<IValue>::getValue() const {
   // TODO: use callback to make this non-blocking
   std::unique_lock<std::mutex> lock(mutex_);
   valueCV_.wait(lock, [this] { return value_.has_value(); });
+  return value_.value();
+}
+
+template <>
+const py::object& OwnerRRef<py::object>::getValue() const {
+  // TODO: use callback to make this non-blocking
+  std::unique_lock<std::mutex> lock(mutex_);
+  valueCV_.wait(lock, [this] { return value_.has_value(); });
+  PythonRpcHandler::getInstance().handleException(value_.value());
   return value_.value();
 }
 
