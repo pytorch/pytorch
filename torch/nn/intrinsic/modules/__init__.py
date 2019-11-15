037f245bb798d728823cc904a567057c0f8891ae
Xiaomeng Yang <yangxm@fb.com>
2019-11-14 21:30:08 -0800

    Add nn.quantized.Conv3d (#29813)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29813
    
    Add nn.quantized.Conv3d
    
    Test Plan: buck test mode/dev-nosan //caffe2/test:quantized -- "conv"
    
    Reviewed By: jianyuh
    
    Differential Revision: D18467749
    
    fbshipit-source-id: 3f992de8f4100f3bd7ba696a6e8943d027d8b2f8

--- torch/nn/intrinsic/modules/__init__.py
+++ torch/nn/intrinsic/modules/__init__.py
@@ -3,11 +3,13 @@
 from .fused import ConvBn2d
 from .fused import ConvBnReLU2d
 from .fused import ConvReLU2d
+from .fused import ConvReLU3d
 from .fused import LinearReLU
 
 __all__ = [
     'ConvBn2d',
     'ConvBnReLU2d',
     'ConvReLU2d',
+    'ConvReLU3d',
     'LinearReLU',
 ]
