9c2e19eb684df6cdd9cdb825017ed304cb22bfb3
Michael Suo <suo@fb.com>
2019-11-15 10:08:34 -0800

    Update on "[ci] test reporting"
    This PR makes our test scripts output artifacts that CircleCI can
    understand. This has a few benefits:
    1. We can actually see failed tests and their output in the job screen
    (instead of having to scroll through logs)
    2. We can use the CircleCI test metadata API to track failed tests
    programmatically.
    
    it looks like this (old ui):
    https://circleci.com/gh/pytorch/pytorch/3546584?pipelines-ui-opt-out
    or this (new ui):
    https://app.circleci.com/jobs/github/pytorch/pytorch/3546584/tests

--- test/run_test.py
+++ test/run_test.py
@@ -105,17 +105,17 @@ if dist.is_available():
     if dist.is_mpi_available():
         DISTRIBUTED_TESTS_CONFIG['mpi'] = {
             'WORLD_SIZE': '3',
-            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-mpi'
+            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-mpi.xml'
         }
     if dist.is_nccl_available():
         DISTRIBUTED_TESTS_CONFIG['nccl'] = {
             'WORLD_SIZE': '2' if torch.cuda.device_count() == 2 else '3',
-            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-nccl'
+            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-nccl.xml'
         }
     if dist.is_gloo_available():
         DISTRIBUTED_TESTS_CONFIG['gloo'] = {
             'WORLD_SIZE': '2' if torch.cuda.device_count() == 2 else '3',
-            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-gloo'
+            'TEST_REPORT_FILE_OVERRIDE': 'test-reports/TestDistBackend-gloo.xml'
         }
 
 # https://stackoverflow.com/questions/2549939/get-signal-names-from-numbers-in-python
