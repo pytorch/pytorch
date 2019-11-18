602ce651d3348118ba11ed73d376779ae770fd1e
Vitaly Fedyunin <vitalyf@fb.com>
2019-11-18 08:50:13 -0800

    explicitly provide memory format when calling to *_like operators (Redo of 81bf7364)
    [ghstack-poisoned]

--- torch/csrc/distributed/autograd/engine/dist_engine.cpp
+++ torch/csrc/distributed/autograd/engine/dist_engine.cpp
@@ -49,7 +49,7 @@ void DistEngine::validateRootsAndRetrieveEdges(
 
     // Compute the root edges and generate the appropriate gradients.
     rootEdges.push_back(root.gradient_edge());
-    grads.push_back(at::ones_like(root, at::MemoryFormat::Contiguous));
+    grads.push_back(at::ones_like(root, LEGACY_CONTIGUOUS_MEMORY_FORMAT));
   }
 
   // Validate rootEdges and grads.
