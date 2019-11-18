74f1c4bd81f9bec0e6a7d56307e15ec6a37566cf
Will Feng <willfeng@fb.com>
2019-11-18 18:21:06 -0500

    fix test

--- torch/csrc/api/src/nn/modules/instancenorm.cpp
+++ torch/csrc/api/src/nn/modules/instancenorm.cpp
@@ -23,11 +23,11 @@ template <size_t D, typename Derived>
 void InstanceNormImpl<D, Derived>::pretty_print(std::ostream& stream) const {
   stream << std::boolalpha
          << "torch::nn::InstanceNorm" << D << "d("
-         << options.num_features() << ", "
-         << "eps=" << options.eps() << ", "
-         << "momentum=" << options.momentum().value() << ", "
-         << "affine=" << options.affine() << ", "
-         << "track_running_stats=" << options.track_running_stats() << ")";
+         << this->options.num_features() << ", "
+         << "eps=" << this->options.eps() << ", "
+         << "momentum=" << this->options.momentum().value() << ", "
+         << "affine=" << this->options.affine() << ", "
+         << "track_running_stats=" << this->options.track_running_stats() << ")";
 }
 
 void InstanceNorm1dImpl::_check_input_dim(const Tensor& input) {
