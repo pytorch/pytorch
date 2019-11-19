3158ab72324f43a7a9eb85eb8b515eb51dbe8c16
Mingbo Wan <mingbo@fb.com>
2019-11-19 15:29:50 -0800

    replace tab with space

--- .circleci/docker/common/install_android.sh
+++ .circleci/docker/common/install_android.sh
@@ -24,21 +24,21 @@ rm -rf /tmp/*
 # https://hub.docker.com/r/picoded/ubuntu-openjdk-8-jdk/dockerfile/
 
 sudo apt-get update && \
-	apt-get install -y openjdk-8-jdk && \
-	apt-get install -y ant && \
-	apt-get clean && \
-	rm -rf /var/lib/apt/lists/* && \
-	rm -rf /var/cache/oracle-jdk8-installer;
+    apt-get install -y openjdk-8-jdk && \
+    apt-get install -y ant && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /var/cache/oracle-jdk8-installer;
 
 # Fix certificate issues, found as of
 # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/983302
 
 sudo apt-get update && \
-	apt-get install -y ca-certificates-java && \
-	apt-get clean && \
-	update-ca-certificates -f && \
-	rm -rf /var/lib/apt/lists/* && \
-	rm -rf /var/cache/oracle-jdk8-installer;
+    apt-get install -y ca-certificates-java && \
+    apt-get clean && \
+    update-ca-certificates -f && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /var/cache/oracle-jdk8-installer;
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 
