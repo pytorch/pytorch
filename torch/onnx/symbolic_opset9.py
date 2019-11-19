28a5f8f5ea74c1991762c0ed4eb64f0664179fbb
neginraoof <neginmr@utexas.edu>
2019-11-18 21:50:29 -0800

    Fix for batch norm 2D with affine=False (#29458)
    Summary:
    This is a fix for batch norm 2D with affine=False.
    Repro: https://github.com/pytorch/pytorch/issues/29271
    Error is because the output of the unsqueeze op does not have scalar type information. So I moved the references to scalar type after the unsqueeze line.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29458
    
    Reviewed By: hl475
    
    Differential Revision: D18400975
    
    Pulled By: houseroad
    
    fbshipit-source-id: f5c5633857c584edcef3b9e9946861dcfccccd75

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -883,9 +883,6 @@ def _convolution(g, input, weight, bias, stride, padding, dilation,
 @parse_args('v', 'v', 'v', 'v', 'v', 'i', 'f', 'f', 'i')
 def batch_norm(g, input, weight, bias, running_mean, running_var, training, momentum, eps, cudnn_enabled):
     input_sizes = input.type().sizes()
-    if len(input_sizes) == 2:
-        # batchnorm1d accepts 2d and 3d array, but ONNX only accepts 3d
-        input = g.op("Unsqueeze", input, axes_i=[2])
 
     if weight is None or sym_help._is_none(weight):
         assert len(input_sizes) > 1
@@ -897,13 +894,12 @@ def batch_norm(g, input, weight, bias, running_mean, running_var, training, mome
         bias_value = torch.tensor([0.] * input_sizes[1]).type(
             'torch.' + input.type().scalarType() + 'Tensor')
         bias = g.op("Constant", value_t=bias_value)
+
     out = g.op("BatchNormalization", input, weight, bias, running_mean, running_var,
                epsilon_f=eps,
                momentum_f=1 - momentum,
                outputs=1 if not training else 5)
     if not training:
-        if len(input_sizes) == 2:
-            out = g.op("Squeeze", out, axes_i=[2])
         return out
     else:
         res, new_running_mean, new_running_var, saved_mean, saved_var = out
@@ -911,8 +907,6 @@ def batch_norm(g, input, weight, bias, running_mean, running_var, training, mome
         new_running_var.setType(running_var.type())
         saved_mean.setDebugName("batch_norm_dead_output-" + saved_mean.debugName())
         saved_var.setDebugName("batch_norm_dead_output-" + saved_var.debugName())
-        if len(input_sizes) == 2:
-            res = g.op("Squeeze", res, axes_i=[2])
         return res
 
 
