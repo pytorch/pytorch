07ba7a73ba83d6953678c05f7e9c19e4177d588b
Jerry Zhang <jerryzh168@gmail.com>
2019-11-20 12:02:44 -0800

    Update on "[jit] Add `clone_instance` for Module"
    Summary:
    Previous implementation of `clone` in `script::Module` copies both the module instance and the
    class type, after we enabled type sharing https://github.com/pytorch/pytorch/pull/26666 we also
    need to have a function to clone instance only and share the underlying class type.
    
    Test Plan:
    tbd
    
    Reviewers:
    mvz
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/cpp/jit/tests.h
+++ test/cpp/jit/tests.h
@@ -54,6 +54,7 @@ namespace jit {
   _(ThreadLocalDebugInfo)              \
   _(SubgraphMatching)                  \
   _(SubgraphRewriter)                  \
+  _(ModuleCloneInstance)               \
   _(ModuleDefine)                      \
   _(QualifiedName)                     \
   _(ClassImport)                       \
