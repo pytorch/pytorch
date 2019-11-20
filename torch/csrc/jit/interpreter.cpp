ef08fb2d47d171f7081630b48409b42fc4a1a97c
Mikhail Zolotukhin <mvz@fb.com>
2019-11-19 17:55:42 -0800

    Show full call stack in TorchScript exception even when calls were inlined.
    Summary:
    This uses newly added InlinedCallStack to print the original call stack
    even if the real call stack is shallower because of inlining.
    This change also makes torchscript stacktraces look like python ones.
    
    Example:
    ```
    torch.jit.script
    def baz(c, b):
        return c + b
    
    torch.jit.script
    def foo(c, b):
        return baz(c, b)
    
    torch.jit.script
    def bar(c, b):
        return foo(c, b)
    
    bar(torch.rand(10), torch.rand(9))
    ```
    
    Output before:
    ```
    Traceback (most recent call last):
      File "fail.py", line 25, in <module>
        bar(torch.rand(10), torch.rand(9))
    RuntimeError: The size of tensor a (10) must match the size of tensor b (9) at non-singleton dimension 0
    The above operation failed in interpreter, with the following stack trace:
    at fail.py:15:11
    torch.jit.script
    def baz(c, b):
        return c + b
               ~~~~~ <--- HERE
    ```
    
    Output after:
    ```
    Traceback (most recent call last):
      File "fail.py", line 41, in <module>
        bar(torch.rand(10), torch.rand(9))
    RuntimeError: The size of tensor a (10) must match the size of tensor b (9) at non-singleton dimension 0
    The above operation failed in interpreter.
    Traceback (most recent call last):
      File "fail.py", line 33
    torch.jit.script
    def bar(c, b):
        return foo(c, b)
               ~~~ <--- HERE
      File "fail.py", line 29, in foo
    torch.jit.script
    def foo(c, b):
        return baz(c, b)
               ~~~ <--- HERE
      File "fail.py", line 25, in baz
    torch.jit.script
    def baz(c, b):
        return c + b
               ~~~~~ <--- HERE
    ```
    
    Output of non-scripted python code:
    ```
    Traceback (most recent call last):
      File "fail.py", line 36, in <module>
        bar(torch.rand(10), torch.rand(9))
      File "fail.py", line 21, in bar
        return foo(c, b)
      File "fail.py", line 18, in foo
        return baz(c, b)
      File "fail.py", line 15, in baz
        return c + b
    RuntimeError: The size of tensor a (10) must match the size of tensor b (9) at non-singleton dimension 0
    ```
    
    Differential Revision: D18532812
    
    Test Plan: Imported from OSS
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: e7e5ba5e4a8f1c7086406271d0f1685d9db8541a

--- torch/csrc/jit/interpreter.cpp
+++ torch/csrc/jit/interpreter.cpp
@@ -1044,23 +1044,38 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
   }
 
   void formatStackTrace(std::ostream& out) {
-    for (size_t i = 0; i < frames.size(); ++i) {
+    std::string previous_fn_name = "";
+    for (int64_t i = frames.size() - 1; i >= 0; i--) {
       const Frame& frame = frames[frames.size() - 1 - i];
       size_t pc = (i == 0) ? frame.pc
                            : frame.pc -
               1; // make sure we report the call node, not the node after it
       Node* node = frame.function->instructions_source_[pc];
-      if (i > 0) {
-        out << "during call ";
+      if (node->callstack()) {
+        for (const auto& p : (*node->callstack())->vec()) {
+          p.second.print_with_context(
+              out, /*context=*/3, /*highlight=*/true, previous_fn_name);
+          previous_fn_name = p.first->name();
+        }
       }
-      node->sourceRange().highlight(out);
+      node->sourceRange().print_with_context(
+          out, /*context=*/3, /*highlight=*/true, previous_fn_name);
+      if (node->kind() == prim::CallFunction) {
+        previous_fn_name = node->inputs()
+                               .at(0)
+                               ->type()
+                               ->expect<FunctionType>()
+                               ->function()
+                               ->name();
+      }
     }
   }
 
   void handleError(const ExceptionMessage& msg, bool is_jit_exception) {
     std::stringstream ss;
     ss << msg << "\n";
-    ss << "The above operation failed in interpreter, with the following stack trace:\n";
+    ss << "The above operation failed in interpreter.\n";
+    ss << "Traceback (most recent call last):\n";
     formatStackTrace(ss);
     if (future_) {
       future_->markCompleted(Future::FutureError(ss.str()));
