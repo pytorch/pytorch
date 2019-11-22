47a914012274b7ce9c9e9b5423678402b52af1a9
Sebastian Messmer <messmer@fb.com>
2019-11-21 17:51:23 -0800

    [no land] Disable LeftRight
    this makes stuff not threadsafe. Just for perf measurements.
    
    Differential Revision: [D18652172](https://our.internmc.facebook.com/intern/diff/D18652172/)
    
    [ghstack-poisoned]

--- c10/util/LeftRight.h
+++ c10/util/LeftRight.h
@@ -61,9 +61,9 @@ public:
 
     template <typename F>
     auto read(F&& readFunc) const -> typename std::result_of<F(const T&)>::type {
-        detail::IncrementRAII _increment_counter(&_counters[_foregroundCounterIndex.load()]);
+        //detail::IncrementRAII _increment_counter(&_counters[_foregroundCounterIndex.load()]);
 
-        return readFunc(_data[_foregroundDataIndex.load()]);
+        return readFunc(_data[0]);
     }
 
     // Throwing an exception in writeFunc is ok but causes the state to be either the old or the new state,
