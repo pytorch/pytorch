11b7bc362f9c7d1588167e1730809b1aaeefc13c
Jerry Zhang <jerryzh168@gmail.com>
2019-11-22 12:07:31 -0800

    Update on "[quant][refactor] Add `get_qparams` and revert the changes to `calculate_qparams`"
    Summary:
    `get_qparams` returns all parameters that's needed to call quantize function
    
    Test Plan:
    python test/test_jit.py
    
    Reviewers:
    pt1quant
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D18645047](https://our.internmc.facebook.com/intern/diff/D18645047)
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -512,7 +512,7 @@ void checkGetQParamsResult(const IValue& qparams) {
   auto tp = qparams.toTuple();
   TORCH_CHECK(
       tp->elements().size() == 2 || tp->elements().size() == 3,
-      "`get_qparams` function is expected to reutrn a "
+      "`get_qparams` function is expected to return a "
       "Tuple of size 2 or 3, got Tuple of size ",
       tp->elements().size());
   // Expect first two elements of the tuple to be Tensor
@@ -543,11 +543,11 @@ std::tuple<IValue, IValue> QuantizeHelper::getQParams(Value* v) {
       "getQParams expects the corresponding observer for ",
       v->debugName(),
       " exists.");
-  auto om = module_.attr(observer_name.value()).toModule();
-  auto get_qparams = om.get_method("get_qparams");
+  auto observer_module = module_.attr(observer_name.value()).toModule();
+  auto get_qparams = observer_module.get_method("get_qparams");
   IValue qparams = get_qparams(std::vector<IValue>());
   checkGetQParamsResult(qparams);
-  auto scalar_type = om.attr("dtype");
+  auto scalar_type = observer_module.attr("dtype");
   TORCH_CHECK(scalar_type.toScalarType() != at::ScalarType::Undefined,
               "dtype of observer can't be undefined");
   return std::make_tuple(qparams, scalar_type);
--- torch/nn/quantized/modules/utils.py
+++ torch/nn/quantized/modules/utils.py
@@ -1,13 +1,14 @@
 import torch
 
 def _quantize_weight(float_wt, observer):
-    wt_scale, wt_zp = observer.calculate_qparams()
+    qparams = observer.get_qparams()
     if observer.qscheme in [torch.per_tensor_symmetric, torch.per_tensor_affine]:
+        wt_scale, wt_zp = qparams
         qweight = torch.quantize_per_tensor(
             float_wt,
             float(wt_scale), int(wt_zp), torch.qint8)
     elif observer.qscheme in [torch.per_channel_symmetric, torch.per_channel_affine]:
-        wt_axis = observer.ch_axis
+        wt_scale, wt_zp, wt_axis = qparams
         qweight = torch.quantize_per_channel(
             float_wt,
             wt_scale.to(torch.double), wt_zp.to(torch.int64), wt_axis, torch.qint8)
