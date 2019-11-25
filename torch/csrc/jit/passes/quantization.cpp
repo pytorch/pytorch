b8f50d9cc860460c7e9b6d3b370cab546e9f9583
Lingyi Liu <lingyiliu@fb.com>
2019-11-25 14:30:46 -0800

    Support to add dequant for each use of Value (#30145)
    Summary:
    In this PR, we mainly handle the case there are multiple usage of a Value when inserting the quant-dequant pair. This change will add one dequant for each usage of the Value.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30145
    
    Differential Revision: D18671600
    
    Pulled By: lly-zero-one
    
    fbshipit-source-id: 61324a98861da85b80dcf7e930381311118ae53b

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -457,7 +457,7 @@ void InsertObserversHelper::insertObservers(
   }
 }
 
-Node* insertQuantDeQuantCall(
+void insertQuantDeQuantCall(
     Value* v,
     const IValue& qparams,
     const IValue& scalar_type) {
@@ -487,14 +487,25 @@ Node* insertQuantDeQuantCall(
 
   Node* quant = g->create(at::Symbol::aten(quantize_func), inputs);
   quant->output()->setDebugName(v->debugName() + ".quant");
-
-  Node* dequant = g->create(at::Symbol::aten("dequantize"), {quant->output()});
-  dequant->output()->setDebugName(v->debugName() + ".dequant");
-
   g->insertNode(quant);
-  g->insertNode(dequant);
 
-  return dequant;
+  // two passes to insert the dequant for every usage
+  // in first pass, identify all the nodes using "v"
+  std::vector<Node*> use_nodes;
+  for (const auto& use : v->uses()) {
+    auto cur = use.user;
+    if(cur != quant) {
+      use_nodes.push_back(cur);
+    }
+  }
+
+  // in second pass, replace the input "v" with dequant output
+  for (size_t i = 0; i < use_nodes.size(); ++i) {
+    Node* dequant = g->create(at::Symbol::aten("dequantize"), {quant->output()});
+    dequant->output()->setDebugName(v->debugName() + ".dequant." + c10::guts::to_string(i));
+    use_nodes[i]->replaceInputWith(v, dequant->output());
+    g->insertNode(dequant);
+  }
 }
 
 // find the observer for Value `v` and return the name of the observer
@@ -532,7 +543,6 @@ class QuantizeHelper {
   std::vector<Value*> values_to_quantize_;
   std::unordered_map<Value*, std::tuple<IValue, IValue> > values_to_qparams_;
 };
-
 
 void QuantizeHelper::collectObserverNodesAndValueToQuantize(Value* v) {
   auto observer_name = findObserverName(v);
@@ -582,15 +592,7 @@ void QuantizeHelper::quantizeTensors() {
     auto tp = values_to_qparams_[v];
     auto qparams = std::get<0>(tp);
     auto scalar_type = std::get<1>(tp);
-    // NB: v is updated here, since removeObserver replaces
-    // v with the input to the observer call
-    Node* dequant;
-    dequant = insertQuantDeQuantCall(v, qparams, scalar_type);
-    v->replaceAllUsesWith(dequant->output());
-    Node* q = dequant->input(0)->node();
-    // replaceAllUsesWith rewrote all uses of V, but we want to keep one: the one
-    // used in quant node. Restore it here:
-    q->replaceInputWith(dequant->output(), v);
+    insertQuantDeQuantCall(v, qparams, scalar_type);
   }
   // no need to clear the vector or map
 }
