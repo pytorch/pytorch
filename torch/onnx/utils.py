a2f95cd1fcab216d5bb71960e7c904746b8b3f15
BowenBao <bowbao@microsoft.com>
2019-11-26 15:24:56 -0800

    move inplace onnx pass

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -89,8 +89,6 @@ def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=Fa
     # Inline everyting
     torch._C._jit_pass_inline(graph)
 
-    if operator_export_type != OperatorExportTypes.RAW:
-        torch._C._jit_pass_onnx_prepare_inplace_ops_for_onnx(graph)
     # Remove fork/wait nodes
     torch._C._jit_pass_inline_fork_wait(graph)
     torch._C._jit_pass_dce(graph)
@@ -116,6 +114,8 @@ def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=Fa
     torch._C._jit_pass_lint(graph)
 
     if operator_export_type != OperatorExportTypes.RAW:
+        torch._C._jit_pass_onnx_prepare_inplace_ops_for_onnx(graph)
+
         # onnx does not support tuples, so try to remove them
         torch._C._jit_pass_lower_all_tuples(graph)
         torch._C._jit_pass_peephole(graph, True)
