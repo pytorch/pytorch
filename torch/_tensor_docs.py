bfd49267676c9ff8719b3e73fb8b0bb084e1fd5a
Will Price <will.price94@gmail.com>
2019-11-26 16:06:24 +0000

    Fix Tensor.argsort -> torch.argsort documentation link

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -2522,7 +2522,7 @@ add_docstr_all('argsort',
                r"""
 argsort(dim=-1, descending=False) -> LongTensor
 
-See :func: `torch.argsort`
+See :func:`torch.argsort`
 """)
 
 add_docstr_all('sparse_dim',
