424664a3ed56926ab6b9f8c32e9b2205a1791dff
Nathan Goldbaum <nathan12343@gmail.com>
2019-11-26 17:59:26 -0600

    make InputMetadata wrap a TensorOptions instead of a TensorOptions*

--- torch/csrc/autograd/input_metadata.h
+++ torch/csrc/autograd/input_metadata.h
@@ -20,21 +20,16 @@ namespace torch { namespace autograd {
 struct InputMetadata {
   InputMetadata() = default;
 
-  InputMetadata(const at::TensorOptions& options, at::IntArrayRef shape, at::Device device)
-  : options_{&options}, shape_{shape}, device_{device} {
+  InputMetadata(const at::TensorOptions options, at::IntArrayRef shape, at::Device device)
+  : options_{options}, shape_{shape}, device_{device} {
     stream_ = c10::impl::getDeviceGuardImpl(device_.type())->getStream(device_);
   }
 
   InputMetadata(const at::Tensor& t)
   : InputMetadata(t.options(), t.sizes(), t.device()) { }
 
-  bool is_valid() const {
-    return options_ != nullptr;
-  }
-
-  const at::TensorOptions& options() const {
-    AT_ASSERT(options_);
-    return *options_;
+  const at::TensorOptions options() const {
+    return options_;
   }
 
   at::IntArrayRef shape() const {
@@ -50,11 +45,11 @@ struct InputMetadata {
   }
 
   at::Tensor zeros_like() const {
-    return at::zeros(shape_, *options_);
+    return at::zeros(shape_, options_);
   }
 
 private:
-  const at::TensorOptions* options_ = nullptr;
+  const at::TensorOptions options_;
   at::DimVector shape_;
   at::Device device_ = at::kCPU;
   c10::Stream stream_ = c10::Stream(c10::Stream::Default::DEFAULT, device_);
