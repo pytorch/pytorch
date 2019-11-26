84b1a793b7cc4b34665e9fec06588abfa58757fd
Spandan Tiwari <sptiwari@microsoft.com>
2019-11-25 22:30:23 -0800

    Provide names for operator nodes in ONNX exported graph. (#27342)
    Summary:
    The PyTorch exporter does not add any name to the ONNX operators in the exported graph. A common request is to add names to op nodes by default. This helps the readability of the graph in visualization tools such a Netron, or when the ONNX graph is printed as a string. Also, it helps with the debuggability of the ONNX graph.
    
    Therefore this PR adds name to operators in the exporters. The names follow a simple format, <op_type>_<index>. Expect files for tests in `test/onnx/test_operators.py` have been updated.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27342
    
    Reviewed By: hl475
    
    Differential Revision: D17790979
    
    Pulled By: houseroad
    
    fbshipit-source-id: 1eaae88b5f51f152735a2ff96e22827837e34d9d

--- torch/csrc/jit/export.h
+++ torch/csrc/jit/export.h
@@ -28,7 +28,8 @@ TORCH_API std::tuple<std::string, RawDataExportMap> export_onnx(
     ::torch::onnx::OperatorExportTypes operator_export_type =
         ::torch::onnx::OperatorExportTypes::ONNX,
     bool strip_doc_string = true,
-    bool keep_initializers_as_inputs = true);
+    bool keep_initializers_as_inputs = true,
+    bool add_node_names = true);
 
 // For testing purposes
 TORCH_API std::string pretty_print_onnx(
@@ -39,7 +40,8 @@ TORCH_API std::string pretty_print_onnx(
     ::torch::onnx::OperatorExportTypes operator_export_type =
         ::torch::onnx::OperatorExportTypes::ONNX,
     bool google_printer = false,
-    bool keep_initializers_as_inputs = true);
+    bool keep_initializers_as_inputs = true,
+    bool add_node_names = true);
 
 TORCH_API void ExportModule(
     const script::Module& module,
