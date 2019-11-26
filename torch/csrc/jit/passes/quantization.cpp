44f06126cf375a1b7577e41b49e13a71b5f2a366
Jerry Zhang <jerryzh168@gmail.com>
2019-11-26 11:12:48 -0800

    [quant][graphmode] Invoke more passes in `insertObservers`
    Summary:
    Invoked `ConstantPooling` and `FuseLinear` pass before
    `insertObservers`.
    `ConstantPooling` is for cleanning up traced graph, e.g. when we
    have to constant node that has the same value, this pass will merge them,
    this allows us to have less quantization patterns
    `FuseLinear` is to merge the exploded linear function into `aten::linear` so
    that we can quantize this function properly. We need to fuse it because right now
    the way we recognize weight and bias is by matching the argument position in certain function
    calls, e.g. 1st argument of aten::conv2d is weight. Therefore we have to preserve
    the bounary of the linear function to recognize the weight of linear. Since in the exploded
    linear code, input of addmm is transposed weight rather than the original weight of linear.
    
    Test Plan:
    This is needed for quantizing traced model tests to pass
    Reviewers:
    mvz
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -1,4 +1,5 @@
 #include <torch/csrc/jit/passes/quantization.h>
+#include <torch/csrc/jit/passes/constant_pooling.h>
 #include <torch/csrc/jit/passes/constant_propagation.h>
 #include <torch/csrc/jit/passes/fuse_linear.h>
 #include <torch/csrc/jit/passes/quantization_patterns.h>
@@ -365,8 +366,12 @@ void InsertObserversHelper::insertObservers(
   script::Method method = module.get_method(method_name);
   auto graph = method.graph();
   ConstantPropagation(graph);
+  // To cleanup traced graph
+  ConstantPooling(graph);
   // must do constant propagation first before replacement
   replaceConvolutionWithConv2d(graph);
+  // fuse exploded linear into aten::linear
+  FuseLinear(graph);
   addIntermediateValuesToSkipObserver(module, method_name);
   // For storing all values that need to be instrumented with an observer call.
   std::vector<Value*> values_to_observe;
