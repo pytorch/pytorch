cf28321dba5801fbbf8b09051ad2273318bb83ac
Your Name <davidriazati@fb.com>
2019-11-26 18:17:19 -0800

    re-add num_layers

--- torch/nn/modules/transformer.py
+++ torch/nn/modules/transformer.py
@@ -157,6 +157,7 @@ class TransformerEncoder(Module):
     def __init__(self, encoder_layer, num_layers, norm=None):
         super(TransformerEncoder, self).__init__()
         self.layers = _get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
         self.norm = norm
 
     def forward(self, src, mask=None, src_key_padding_mask=None):
