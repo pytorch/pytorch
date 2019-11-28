ecd4b4a76aa17de12d04e36e394a3f470849afc6
Jerry Zhang <jerryzh168@gmail.com>
2019-11-27 17:09:58 -0800

    [quant][graphmode] InsertObservers for shared class types
    Summary:
    ClassTypes can be shared among different module instances, but previously we assumed
    they would be unique, this PR enables the insert_observers pass to work with shared class types
    Test Plan:
    python test/test_jit.py
    python test/test_quantization.py
    
    Reviewers:
    mvz
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -176,6 +176,8 @@ class InsertObserversHelper {
   int uid_ = 0;
   // Set of observer forward call nodes
   std::unordered_set<Node*> observer_nodes_;
+  // Map from type of the module to the vector of observer modules we added to the class
+  std::unordered_map<Graph*, std::vector<std::tuple<std::string, script::Module>>> graph_observer_map_;
 
   // These are the IR patterns we match to skip inserting observers.
   // They are compiled once on construction and used repeatedly within
@@ -347,12 +349,13 @@ void InsertObserversHelper::insertObserverFor(
     observer_module = std::get<0>(qconfig);
   }
 
-  script::Module observer = observer_module.clone();
+  script::Module observer = observer_module.clone_instance();
   std::string observer_name = "_observer_" + c10::to_string(uid_++);
   while (module.hasattr(observer_name)) {
     observer_name = "_observer_" + c10::to_string(uid_++);
   }
   module.register_module(observer_name, observer);
+  graph_observer_map_[g].push_back({observer_name, observer});
 
   // Get handle of observer module
   Node* observer_instance =
@@ -418,6 +421,18 @@ void InsertObserversHelper::insertObservers(
 
   script::Method method = module.get_method(method_name);
   auto graph = method.graph();
+
+  if (graph_observer_map_.count(graph.get())) {
+    // instance clone of observer module and setAttr
+    for (const auto& observer_attrs : graph_observer_map_.at(graph.get())) {
+      const auto& name = std::get<0>(observer_attrs);
+      const auto& observer = std::get<1>(observer_attrs);
+      module._ivalue()->setAttr(name, observer.clone_instance()._ivalue());
+    }
+    insertObserversForInvokedMethods(module, method_name);
+    return;
+  }
+
   ConstantPropagation(graph);
   // must do constant propagation first before replacement
   replaceConvolutionWithConv2d(graph);
