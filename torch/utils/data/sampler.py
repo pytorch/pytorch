74060d1f1e444100748f6e5a658ebd298da3035a
ea409 <33399273+ea409@users.noreply.github.com>
2019-11-29 11:05:13 +0000

    Update sampler.py
    Just added a weighted subset sampler and a sequential subset sampler - it might be easier to make these a super class of sequential and random samplers and use the range(0, len(dataset)) as indices - useful for people who don't load their train and test as separate datasets.

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -125,6 +125,50 @@ class SubsetRandomSampler(Sampler):
 
     def __len__(self):
         return len(self.indices)
+ 
+class SequentialSubsetSampler(Sampler):
+    """Samples elements sequentially given a list of indices,
+
+    Arguments:
+        indices (sequence): a sequence of indices
+        
+    """
+    def __init__(self, indices):
+        self.indices = indices
+
+    def __iter__(self):
+        return iter(self.indices.tolist())
+
+    def __len__(self):
+        return len(self.indices)
+
+class WeightedSubsetSampler(Sampler):
+    """Samples elements randomly given a weighting, from a defined subset.
+    Arguments:
+        weights (sequence): a sequence of weights, not necessary summing up to one
+        indices (sequence): a sequence of indices
+        replacement (bool): samples are drawn with replacement if ``True``, default=``False``
+        num_samples (int): number of samples to draw, default=`len(dataset)`. This argument
+            is supposed to be specified only when `replacement` is ``True``.
+    """
+    def __init__(self, weights, indices, num_samples, replacement=True):
+        if not isinstance(num_samples, _int_classes) or isinstance(num_samples, bool) or \
+                num_samples <= 0:
+            raise ValueError("num_samples should be a positive integer "
+                             "value, but got num_samples={}".format(num_samples))
+        if not isinstance(replacement, bool):
+            raise ValueError("replacement should be a boolean value, but got "
+                             "replacement={}".format(replacement))
+        self.weights = torch.as_tensor(weights, dtype=torch.double)
+        self.num_samples = num_samples
+        self.indices=indices
+        self.replacement = replacement
+
+    def __iter__(self):
+        return iter(self.indices[torch.multinomial(self.weights[self.indices], self.num_samples, self.replacement)])
+
+    def __len__(self):
+        return self.num_samples
 
 
 class WeightedRandomSampler(Sampler):
