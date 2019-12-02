af4ba9cc8da7be70ada915644d9dcb9dff49ea4d
Martin Yuan <iseeyuan@yahoo.com>
2019-12-02 13:54:29 -0800

    Update on "Move prim ops from JIT registration to C10"
    The first version to move prim ops to c10 registration. After the reviewers are fine with the initial changes, more operators will be moved in the same style.

--- torch/csrc/jit/register_prim_ops_c10.cpp
+++ torch/csrc/jit/register_prim_ops_c10.cpp
@@ -13,21 +13,21 @@ using at::Scalar;
 using c10::IValue;
 
 static auto registry_prim = torch::RegisterOperators().op("aten::Int.Tensor(Tensor a) -> int",
-  torch::RegisterOperators::options().kernel(c10::TensorTypeId::CPUTensorId,
+  torch::RegisterOperators::options().catchAllKernel(
   [](at::Tensor a) -> int64_t {
     return a.item<int64_t>();
 })
-).op(  "aten::Int.bool(bool a) -> int",
+).op("aten::Int.bool(bool a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](bool b) -> int64_t {
-    return (int64_t)b;
+    return static_cast<int64_t>(b);
 })
-).op(  "aten::Int.float(float a) -> int",
+).op("aten::Int.float(float a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](double d) -> int64_t {
-    return (int64_t)d;
+    return static_cast<int64_t>(d);
 })
-).op(  "aten::Int.Scalar(Scalar a) -> int",
+).op("aten::Int.Scalar(Scalar a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](Scalar scalar) -> int64_t {
     return scalar.toInt();
