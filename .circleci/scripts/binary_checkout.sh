01221c157c371a69dfe8b838545fd31f931af9e6
Mingbo Wan <mingbo@fb.com>
2019-12-02 18:46:45 -0800

    no way

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -27,20 +27,21 @@ export CIRCLE_SHA1=${SHA1}
 retry git clone https://github.com/mingbowan/pytorch.git "$PYTORCH_ROOT" -b check_size
 cp /pytorch/.circleci/scripts/upload_binary_size_to_scuba.py /tmp/
 pushd "$PYTORCH_ROOT"
-if [[ -n "${CIRCLE_PR_NUMBER:-}" ]]; then
-  # "smoke" binary build on PRs
-  git fetch --force origin "pull/${CIRCLE_PR_NUMBER}/head:remotes/origin/pull/${CIRCLE_PR_NUMBER}"
-  git reset --hard "$CIRCLE_SHA1"
-  git checkout -q -B "$CIRCLE_BRANCH"
-  git reset --hard "$CIRCLE_SHA1"
-elif [[ -n "${CIRCLE_SHA1:-}" ]]; then
-  # Scheduled workflows & "smoke" binary build on master on PR merges
-  git reset --hard "$CIRCLE_SHA1"
-  git checkout -q -B master
-else
-  echo "Can't tell what to checkout"
-  exit 1
-fi
+git reset --hard "$CIRCLE_SHA1"
+# if [[ -n "${CIRCLE_PR_NUMBER:-}" ]]; then
+#   # "smoke" binary build on PRs
+#   git fetch --force origin "pull/${CIRCLE_PR_NUMBER}/head:remotes/origin/pull/${CIRCLE_PR_NUMBER}"
+#   git reset --hard "$CIRCLE_SHA1"
+#   git checkout -q -B "$CIRCLE_BRANCH"
+#   git reset --hard "$CIRCLE_SHA1"
+# elif [[ -n "${CIRCLE_SHA1:-}" ]]; then
+#   # Scheduled workflows & "smoke" binary build on master on PR merges
+#   git reset --hard "$CIRCLE_SHA1"
+#   git checkout -q -B master
+# else
+#   echo "Can't tell what to checkout"
+#   exit 1
+# fi
 retry git submodule update --init --recursive --quiet
 echo "Using Pytorch from "
 git --no-pager log --max-count 1
