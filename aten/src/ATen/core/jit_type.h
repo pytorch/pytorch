bd0c340db4074f310f5c33932daa9830cc8d1758
Jerry Zhang <jerryzh@fb.com>
2019-12-04 14:15:30 -0800

    AddConstant and findConstant for ClassType (#29217)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29217
    
    We want to preserve constant information in ClassType so that
    users can access the constants in the module by name.
    This is also used later for freezing some attribute(converting
    attributes to constant)
    
    Test Plan:
    tbd
    
    Imported from OSS
    
    Differential Revision: D18799955
    
    fbshipit-source-id: fbfbcd5d3f7f560368b96e2a87e270c822a3d03a

--- aten/src/ATen/core/jit_type.h
+++ aten/src/ATen/core/jit_type.h
@@ -1435,6 +1435,8 @@ struct CAFFE2_API ClassType : public NamedType {
     return attributeNames_[slot];
   }
 
+  void checkNotExist(const std::string& name, const std::string& what) const;
+
   // Attributes are stored in a specific slot at runtime for effiency.
   // When emitting instructions we specify the slot so that attribute access is
   // a constant lookup
@@ -1471,7 +1473,7 @@ struct CAFFE2_API ClassType : public NamedType {
 
   size_t addAttribute(
       const std::string& name,
-      TypePtr type,
+      const TypePtr& type,
       bool is_parameter = false);
 
   // [Internal Only] Remove attribute from the ClassType,
@@ -1516,6 +1518,39 @@ struct CAFFE2_API ClassType : public NamedType {
 
   at::ArrayRef<TypePtr> containedTypes() const override {
     return attributeTypes_;
+  }
+
+  bool hasConstant(const std::string& name) const {
+    return std::find_if(
+               constantNames_.cbegin(),
+               constantNames_.cend(),
+               [&](const std::string& constant) { return constant == name; }) !=
+        constantNames_.cend();
+  }
+
+  size_t addConstant(
+      const std::string& name,
+      const IValue& value);
+
+  const std::string& getConstantName(size_t slot) const {
+    TORCH_CHECK(constantNames_.size() == constantValues_.size());
+    TORCH_CHECK(slot < constantNames_.size());
+    return constantNames_[slot];
+  }
+
+  c10::optional<IValue> getConstant(const std::string& name) const;
+
+  size_t numConstants() const {
+    TORCH_INTERNAL_ASSERT(constantNames_.size() == constantValues_.size());
+    return constantNames_.size();
+  }
+
+  at::ArrayRef<std::string> constantNames() const {
+    return constantNames_;
+  }
+
+  at::ArrayRef<IValue> constantValues() const {
+    return constantValues_;
   }
 
   TypePtr createWithContained(std::vector<TypePtr> contained_types) const override {
@@ -1573,6 +1608,9 @@ struct CAFFE2_API ClassType : public NamedType {
   // available from c10
   std::vector<std::string> attributeNames_;
   std::vector<TypePtr> attributeTypes_;
+  // Mapping of constant names -> their value.
+  std::vector<std::string> constantNames_;
+  std::vector<IValue> constantValues_;
   // Holds method attributes
   std::weak_ptr<CompilationUnit> compilation_unit_;
 
--- torch/csrc/jit/script/script_type_parser.cpp
+++ torch/csrc/jit/script/script_type_parser.cpp
@@ -332,6 +332,37 @@ FunctionSchema ScriptTypeParser::parseSchemaFromDef(
       name, "", std::move(args), std::move(returns), false, false);
 }
 
+c10::IValue ScriptTypeParser::parseClassConstant(const Assign& assign) {
+  if (assign.lhs().kind() != TK_VAR) {
+    throw ErrorReport(assign.range())
+      << "Expected to a variable for class constant";
+  }
+  const auto final_type = assign.type().get();
+  auto expr = assign.rhs().get();
+  if (final_type.kind() != TK_SUBSCRIPT) {
+    throw ErrorReport(assign.range())
+      << "Expected subscripted type for class constant";
+  }
+  auto subscript = Subscript(final_type);
+  auto value_name = parseBaseTypeName(subscript.value());
+  if (!value_name) {
+    throw ErrorReport(subscript.value().range())
+      << "Subscripted type must be a type identifier";
+  }
+  if (*value_name != "Final") {
+    throw ErrorReport(subscript.range())
+      << "Base type must be Final for class constant";
+  }
+  if (subscript.subscript_exprs().size() != 1) {
+    throw ErrorReport(subscript)
+      << " expected exactly one element type but found "
+      << subscript.subscript_exprs().size();
+  }
+  auto type = *subscript.subscript_exprs().begin();
+  auto default_val = evaluateDefaults(expr.range(), {type}, {expr});
+  return *default_val.begin();
+}
+
 } // namespace script
 } // namespace jit
 } // namespace torch
