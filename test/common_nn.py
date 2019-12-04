d164a22e456f27965b6ad1ca7f5c3139680dbb77
Gregory Chanan <gchanan@fb.com>
2019-12-04 15:44:36 -0800

    [BC-Breaking] Fix scalar check of MultiLabelMarginLoss.
    The behavior didn't match the documentation, because the documentation (for 'none' reduction) reads:
    input X target -> output
    (N, C) X (N, C) -> (N,)
    (C,) X (C,) -> ()
    
    but the later case would output (1,).  This also changes the case to:
    () X (C,) -> ()
    from:
    () X (C,) -> (C,)
    which makes more sense with the above formulas.
    
    Restacked version of: https://github.com/pytorch/pytorch/pull/30748
    
    [ghstack-poisoned]

--- test/common_nn.py
+++ test/common_nn.py
@@ -2606,9 +2606,6 @@ def smoothl1loss_reference(input, target, reduction='mean'):
 
 
 def _multilabelmarginloss_reference(input, target):
-    if input.dim() == 0:
-        return 0
-
     targets = []
     for target_index in target:
         if target_index < 0:
@@ -2625,27 +2622,29 @@ def _multilabelmarginloss_reference(input, target):
 
 
 def multilabelmarginloss_reference(input, target, reduction='mean'):
-    if input.dim() == 0:
-        dim = 1
-        output = torch.empty_like(input)
-        output.fill_(_multilabelmarginloss_reference(input, target))
-    elif input.dim() == 1:
-        n = 1
-        dim = input.size(0)
-        output = input.new(n).zero_()
-        output[0] = _multilabelmarginloss_reference(input, target)
-    else:
-        n = input.size(0)
-        dim = input.size(1)
-        output = input.new(n).zero_()
-        for i in range(0, n):
-            output[i] = _multilabelmarginloss_reference(input[i], target[i])
+    # make everything 2-dimensional
+    input_dim = input.dim()
+    if input.dim() < 2:
+        assert target.dim() < 2
+        input = input.unsqueeze(0) if input.dim() == 1 else input.unsqueeze(0).unsqueeze(0)
+        target = target.unsqueeze(0) if target.dim() == 1 else target.unsqueeze(0).unsqueeze(0)
+
+    n = input.size(0)
+    dim = input.size(1)
+    output = input.new(n).zero_()
+    for i in range(0, n):
+        output[i] = _multilabelmarginloss_reference(input[i], target[i])
 
     if reduction == 'mean':
         return output.mean() / dim
     elif reduction == 'sum':
         return output.sum() / dim
-    return output / dim
+    elif input_dim < 2:
+        # we know we have (1, C) X (1, C) -> (1,), so squeeze will get us
+        # back to correct dimensionality
+        return output.squeeze() / dim
+    else:
+        return output / dim
 
 
 def hingeembeddingloss_reference(input, target, margin=1.0, reduction='mean'):
