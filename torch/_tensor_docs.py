a68b790293441c00e35979dab4fe1e8681dd2d70
Tongzhou Wang <ssnl@users.noreply.github.com>
2019-12-04 07:25:17 -0800

    fix ref to nonexistent torch.repeat
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/30614
    
    Differential Revision: D18808517
    
    Pulled By: ezyang
    
    fbshipit-source-id: 27f9bda6fbbd1c3c751a0e96fdc336bf724c0b31

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -2158,7 +2158,7 @@ Unlike :meth:`~Tensor.expand`, this function copies the tensor's data.
 
 .. warning::
 
-    :func:`torch.repeat` behaves differently from
+    :meth:`~Tensor.repeat` behaves differently from
     `numpy.repeat <https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html>`_,
     but is more similar to
     `numpy.tile <https://docs.scipy.org/doc/numpy/reference/generated/numpy.tile.html>`_.
