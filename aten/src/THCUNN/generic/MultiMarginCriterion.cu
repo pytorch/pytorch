20361281aece8d95783ca82f5ce46d5eb4f80d19
Gregory Chanan <gchanan@fb.com>
2019-12-05 09:33:55 -0800

    MultiMarginCriterion: move scalar_check from codegen to code.
    [ghstack-poisoned]

--- aten/src/THCUNN/generic/MultiMarginCriterion.cu
+++ aten/src/THCUNN/generic/MultiMarginCriterion.cu
@@ -25,7 +25,7 @@ void THNN_(MultiMarginCriterion_updateOutput)(
                "inconsistent target size");
     dim3 blocks(1);
     dim3 threads(MULTIMARGIN_THREADS);
-    THCTensor_(resize1d)(state, output, 1);
+    THCTensor_(resizeAs)(state, output, target);
     if (p == 1)
     {
       cunn_MultiMarginCriterion_updateOutput_kernel<1, scalar_t, accreal> <<<blocks,threads, 0, THCState_getCurrentStream(state)>>>(
@@ -91,7 +91,7 @@ void THNN_(MultiMarginCriterion_updateOutput)(
     }
     else
     {
-      THCTensor_(resize1d)(state, output, 1);
+      THCTensor_(resize0d)(state, output);
       THCTensor *output_ = THCTensor_(newWithSize1d)(state, input->size(0));  // tmp output buffer
       if (p == 1)
       {
@@ -119,7 +119,7 @@ void THNN_(MultiMarginCriterion_updateOutput)(
       }
       THCudaCheck(cudaGetLastError());
       float sum = THCTensor_(sumall)(state, output_);
-      THCTensor_(set1d)(state, output, 0, ScalarConvert<accreal, scalar_t>::to(sum));
+      THCTensor_(set0d)(state, output, 0, ScalarConvert<accreal, scalar_t>::to(sum));
       THCTensor_(free)(state, output_);
     }
   }
