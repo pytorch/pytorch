a92e03dbd7c021cead35f0a422de7b707b038103
vfdev <vfdev.5@gmail.com>
2019-12-05 23:04:14 +0100

    Updated example of WeightedRandomSampler
    Previous example
    ```python
    >>> list(WeightedRandomSampler([0.1, 0.9, 0.4, 0.7, 3.0, 0.6], 5, replacement=True))
            [0, 0, 0, 1, 0]
    ```
    may seem misleading according to provided weights

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -139,7 +139,7 @@ class WeightedRandomSampler(Sampler):
 
     Example:
         >>> list(WeightedRandomSampler([0.1, 0.9, 0.4, 0.7, 3.0, 0.6], 5, replacement=True))
-        [0, 0, 0, 1, 0]
+        [4, 4, 1, 4, 5]
         >>> list(WeightedRandomSampler([0.9, 0.4, 0.05, 0.2, 0.3, 0.1], 5, replacement=False))
         [0, 1, 4, 3, 2]
     """
