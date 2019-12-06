47b47894a134108aae7fca86d3697c88416d8213
take-cheeze <takechi101010@gmail.com>
2019-12-06 12:41:25 +0900

    Use provided upper_bound/lower_bound instead

--- aten/src/ATen/native/cpu/ReduceOpsKernel.cpp
+++ aten/src/ATen/native/cpu/ReduceOpsKernel.cpp
@@ -157,26 +157,6 @@ static void or_kernel_impl(TensorIterator& iter) {
     /*ident=*/false);
 }
 
-static void min_values_kernel_impl(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.dtype(), "min_values_cpu", [&iter] {
-    binary_kernel_reduce_vec(
-      iter,
-      [](scalar_t a, scalar_t b) -> scalar_t { return min_impl(a, b); },
-      [](Vec256<scalar_t> a, Vec256<scalar_t> b) { return minimum(a, b); },
-      std::numeric_limits<scalar_t>::max());
-  });
-}
-
-static void max_values_kernel_impl(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.dtype(), "max_values_cpu", [&iter] {
-    binary_kernel_reduce_vec(
-      iter,
-      [](scalar_t a, scalar_t b) -> scalar_t { return max_impl(a, b); },
-      [](Vec256<scalar_t> a, Vec256<scalar_t> b) { return maximum(a, b); },
-      std::numeric_limits<scalar_t>::min());
-  });
-}
-
 // Maximum and minimum possible scalar values, including infinities
 
 template <typename scalar_t>
@@ -189,6 +169,26 @@ template <typename scalar_t>
 constexpr scalar_t lower_bound() {
   using lim = std::numeric_limits<scalar_t>;
   return lim::has_infinity ? -lim::infinity() : lim::lowest();
+}
+
+static void min_values_kernel_impl(TensorIterator& iter) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.dtype(), "min_values_cpu", [&iter] {
+    binary_kernel_reduce_vec(
+      iter,
+      [](scalar_t a, scalar_t b) -> scalar_t { return min_impl(a, b); },
+      [](Vec256<scalar_t> a, Vec256<scalar_t> b) { return minimum(a, b); },
+      upper_bound<scalar_t>());
+  });
+}
+
+static void max_values_kernel_impl(TensorIterator& iter) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.dtype(), "max_values_cpu", [&iter] {
+    binary_kernel_reduce_vec(
+      iter,
+      [](scalar_t a, scalar_t b) -> scalar_t { return max_impl(a, b); },
+      [](Vec256<scalar_t> a, Vec256<scalar_t> b) { return maximum(a, b); },
+      lower_bound<scalar_t>());
+  });
 }
 
 static void argmax_kernel_impl(TensorIterator &iter) {
