872e6851db89b99bb63af4d88821b506c0231181
Zachary DeVito <zdevito@fb.com>
2019-12-05 17:06:13 -0800

    Revert D18621773: we should have a config-based way to skip flaky tests
    Test Plan: revert-hammer
    
    Differential Revision:
    D18621773
    
    Original commit changeset: 5532f1d5fa3f
    
    fbshipit-source-id: 22239b88a6f9551938e6e2178bf9162e3385b011

--- tools/update_disabled_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-EXTRACTED_REPO=https://$USERNAME:$API_KEY@github.com/zdevito/pytorch_disabled_tests.git
-git clone $EXTRACTED_REPO
-cd pytorch_disabled_tests
-curl 'https://api.github.com/search/issues?q=is%3Aissue+is%3Aopen+label%3A%22topic%3A+flaky-tests%22+repo:pytorch/pytorch+in%3Atitle+DISABLED' \
-| sed 's/"score": [0-9\.]*/"score": 0.0/g' > result.json
-# score changes every request, so we strip it out to avoid creating a commit every time we query.
-git commit -a -m 'update'
-git push
