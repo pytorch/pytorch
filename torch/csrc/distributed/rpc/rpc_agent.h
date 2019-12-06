3985f2b36b757bbfd86aff78306b2147e2244682
Satendra Gera <sgera@fb.com>
2019-12-05 23:49:52 -0800

    Add get_metrics and get_debug_info to rpc agent (#30833)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30833
    
    [rpc] Add get_metrics and get_debug_info to rpc agent
    
    Test Plan: UT and builds
    
    Reviewed By: mrshenli
    
    Differential Revision: D18835068
    
    fbshipit-source-id: f552cf196bb6d54ccd38a44ba981e7d5b15513f0

--- torch/csrc/distributed/rpc/rpc_agent.h
+++ torch/csrc/distributed/rpc/rpc_agent.h
@@ -139,6 +139,12 @@ class TORCH_API RpcAgent {
   // Retrieve the default rpc agent.
   static std::shared_ptr<RpcAgent> getDefaultRpcAgent();
 
+  // Retrive metrics as KV map
+  virtual std::unordered_map<std::string, std::string> getMetrics() = 0;
+
+  // Retrive debug info in addition to metrics as KV map
+  virtual std::unordered_map<std::string, std::string> getDebugInfo() = 0;
+
  protected:
   const WorkerInfo workerInfo_;
   const std::string workerName_;
