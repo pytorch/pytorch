3eea3b8519cb402d7495f3d089d9de127b666849
BowenBao <bowbao@microsoft.com>
2019-12-06 22:46:51 -0800

    Support exporting aten::copy_ and aten::index_put to ONNX opset 11 (#26941)
    Summary:
    - [x] Add more comments and refactor the logic of `ReshapeToAdvancedIndexingFormat`
    - [x] Add more description here. Cases that are/aren't supported, and how they are supported.
    - [x] Need to merge this PR https://github.com/pytorch/pytorch/issues/27186 to enable testing inplace operators.
    
    We are now supporting exporting aten::copy_ and aten::index_put to ONNX.
    Here's a breakdown of the different cases in PyTorch code.
    
    ```
    # Case 1: Scalar Indices
    x[0, 1, 2] = data
    
    # Case 2: Slice Indices
    x[1:3, :, ::2] = data
    
    # Case 3: Ellipsis Indices
    x[..., 0] = data
    
    # Case 4: Tensor Indices
    ind1 = torch.tensor([0, 2])
    ind2 = torch.tensor([1, 1])
    x[ind1, ind2] = data
    
    # Case 5: Mixing all the above cases
    ind1 = torch.tensor([0, 2])
    ind2 = torch.tensor([1, 1])
    x[1:3, ind1, ind2, ..., 3] = data
    ```
    
    Limitations:
    
    Tensor indices must be consecutive, and 1-d tensors.
    
    ```
    # Supported
    ind1 = torch.tensor([0, 2])
    ind2 = torch.tensor([1, 1])
    x[ind1, ind2] = data
    
    # Not supported
    ind1 = torch.tensor([0, 2])
    ind2 = torch.tensor([1, 1])
    ind3 = torch.tensor([[0], [1]])
    x[ind1, :, ind2] = data
    x[ind3] = data
    ```
    
    Negative indices are not supported.
    ```
    # Not supported
    x[-1] = data
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26941
    
    Differential Revision: D17951030
    
    Pulled By: houseroad
    
    fbshipit-source-id: 4357777072f53aa0bc4b297aa1ee53457a7f8dec

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -114,6 +114,8 @@ def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=Fa
     torch._C._jit_pass_lint(graph)
 
     if operator_export_type != OperatorExportTypes.RAW:
+        torch._C._jit_pass_onnx_prepare_inplace_ops_for_onnx(graph)
+
         # onnx does not support tuples, so try to remove them
         torch._C._jit_pass_lower_all_tuples(graph)
         torch._C._jit_pass_peephole(graph, True)
