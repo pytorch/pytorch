9d45c15c0c34725313b78b78bd0ef27765b3c657
Sebastian Messmer <messmer@fb.com>
2019-12-06 16:29:00 -0800

    Remove c++14-conditional constexpr
    These macros said "make it constexpr if we're in C++14". Since we're now always C++14, we can just say "constexpr" isntead.
    
    Differential Revision: [D18869635](https://our.internmc.facebook.com/intern/diff/D18869635/)
    
    [ghstack-poisoned]

--- c10/macros/Macros.h
+++ c10/macros/Macros.h
@@ -222,23 +222,6 @@ constexpr uint32_t CUDA_THREADS_PER_BLOCK_FALLBACK = 256;
 #define C10_IS_TRIVIALLY_COPYABLE(T) std::is_trivially_copyable<T>::value
 #endif
 
-// AT_CPP14_CONSTEXPR: Make it constexpr if we're in C++14 or later
-#if defined(_MSC_VER) && defined(__CUDACC__) && \
-    (__CUDACC_VER_MAJOR__ >= 10 ||              \
-     (__CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ >= 2))
-// workaround: CUDA >= v9.2 compiler cannot compile correctly on Windows.
-#define AT_CPP14_CONSTEXPR
-#define AT_IS_CPP14_CONSTEXPR 0
-#else
-#if defined(__cpp_constexpr) && __cpp_constexpr >= 201304
-#define AT_CPP14_CONSTEXPR constexpr
-#define AT_IS_CPP14_CONSTEXPR 1
-#else
-#define AT_CPP14_CONSTEXPR
-#define AT_IS_CPP14_CONSTEXPR 0
-#endif
-#endif
-
 // We need --expt-relaxed-constexpr in CUDA because of Eigen. This flag allows
 // device code in CUDA to call host constexpr functions. Unfortunately,
 // the CUDA compiler (at least for CUDA 9.0, 9.1 and 9.2) isn't compatible
@@ -256,11 +239,9 @@ constexpr uint32_t CUDA_THREADS_PER_BLOCK_FALLBACK = 256;
 #if defined(__CUDA_ARCH__)
 #define C10_HOST_CONSTEXPR __host__
 #define C10_HOST_CONSTEXPR_VAR
-#define C10_CPP14_HOST_CONSTEXPR __host__
 #else
 #define C10_HOST_CONSTEXPR constexpr
 #define C10_HOST_CONSTEXPR_VAR constexpr
-#define C10_CPP14_HOST_CONSTEXPR AT_CPP14_CONSTEXPR
 #endif
 
 #if !defined(__clang__) && !defined(_MSC_VER) && defined(__GNUC__) && \
@@ -268,8 +249,8 @@ constexpr uint32_t CUDA_THREADS_PER_BLOCK_FALLBACK = 256;
 #define CONSTEXPR_EXCEPT_GCC5
 #define IS_NOT_GCC5_CONSTEXPR 0
 #else
-#define CONSTEXPR_EXCEPT_GCC5 AT_CPP14_CONSTEXPR
-#define IS_NOT_GCC5_CONSTEXPR AT_IS_CPP14_CONSTEXPR
+#define CONSTEXPR_EXCEPT_GCC5 constexpr
+#define IS_NOT_GCC5_CONSTEXPR 1
 #endif
 
 #endif // C10_MACROS_MACROS_H_
