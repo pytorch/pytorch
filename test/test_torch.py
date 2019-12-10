a1b2756d48ff82a09a76b73c639728f3f2765156
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-12-10 11:54:04 -0800

    no prints

--- test/test_torch.py
+++ test/test_torch.py
@@ -11799,23 +11799,19 @@ class TestTorchDeviceType(TestCase):
         out = torch.empty((2,), dtype=dtype, device=device)
         alpha = 6
         beta = 3
-        print(torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta))
         self.assertEqual(torch.full((2,), beta * value, dtype=dtype, device=device),
                          torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta))
         self.assertEqual(torch.full((2,), beta * value, dtype=dtype, device=device),
                          torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta, out=out))
-        print(out)
 
         # torch.addmm
         input = torch.full((2, 3), value, dtype=dtype, device=device)
         mat2 = torch.ones((0, 3), dtype=dtype, device=device)
         out = torch.empty((2, 3), dtype=dtype, device=device)
-        print(torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta))
         self.assertEqual(torch.full((2, 3), beta * value, dtype=dtype, device=device),
                          torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta))
         self.assertEqual(torch.full((2, 3), beta * value, dtype=dtype, device=device),
                          torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta, out=out))
-        print(out)
 
     @skipCUDAIfRocm
     def test_unique_dim(self, device):
