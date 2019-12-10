670209ae83434185ea556e3c00855ddb68232bd0
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2019-12-10 11:42:14 +0530

    update test

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -805,9 +805,19 @@ class TestCuda(TestCase):
 
     def test_specify_improper_device_name(self):
         with tempfile.NamedTemporaryFile() as f:
-            torch.save([torch.nn.Parameter(torch.randn(1000, 1000))], f)
+            torch.save([torch.nn.Parameter(torch.randn(10, 10))], f,
+                       _use_new_zipfile_serialization=True)
             with self.assertRaisesRegex(ValueError, "Expected a cuda device"):
                 torch.load(f, 'cuda0')
+
+    def test_get_device_index(self):
+        from torch.cuda._utils import _get_device_index
+        with self.assertRaiseRegex(RuntimeError, "Expected one of cpu"):
+            _get_device_index('cuda0', optional=True)
+
+        with self.assertRaiseRegex(ValueError, "Expected a cuda device"):
+            cpu_device = torch.device('cpu')
+            _get_device_index(cpu_device, optional=True)
 
     def test_serialization_array_with_empty(self):
         x = [torch.randn(4, 4).cuda(), torch.cuda.FloatTensor()]
