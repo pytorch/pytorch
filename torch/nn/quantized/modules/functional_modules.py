7270781074738d2a7ada0c4678e8ed99b463a8c9
dskhudia <dskhudia@fb.com>
2019-12-09 17:07:05 -0800

    Update on "[pt][quant] Adding quantized clamp kernel"
    Differential Revision: [D18739628](https://our.internmc.facebook.com/intern/diff/D18739628/)
    
    [ghstack-poisoned]

--- torch/nn/quantized/modules/functional_modules.py
+++ torch/nn/quantized/modules/functional_modules.py
@@ -101,7 +101,6 @@ class QFunctional(torch.nn.Module):
     Valid operation names:
         - add
         - cat
-        - clamp
         - mul
         - add_relu
         - add_scalar
@@ -156,11 +155,6 @@ class QFunctional(torch.nn.Module):
         # type: (List[Tensor], int) -> Tensor
         return ops.quantized.cat(x, scale=self.scale,
                                  zero_point=self.zero_point, dim=dim)
-
-    r"""Operation equivalent to ``torch.ops.quantized.clamp``"""
-    def clamp(self, x, min_, max_):
-        # type: (Tensor, float, float) -> Tensor
-        return ops.quantized.clamp(x, min_, max_)
 
     r"""Operation equivalent to ``torch.ops.quantized.add_relu``"""
     def add_relu(self, x, y):
