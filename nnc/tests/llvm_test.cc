b80148b210a9ca738f8add20a5574fac70464bfb
Bert Maher <bertrand@fb.com>
2019-12-13 11:53:32 -0800

    Test all arithmetic ops with LLVM

--- nnc/tests/llvm_test.cc
+++ nnc/tests/llvm_test.cc
@@ -20,3 +20,30 @@ TEST(ExprTest, IntAddTest) {
   c.accept(&cg);
   EXPECT_EQ(cg.value(), 5);
 }
+
+TEST(ExprTest, IntSubTest) {
+  auto a = IntImm::make(2);
+  auto b = IntImm::make(3);
+  auto c = Sub::make(a, b);
+  LLVMCodeGen cg;
+  c.accept(&cg);
+  EXPECT_EQ(cg.value(), -1);
+}
+
+TEST(ExprTest, IntMulTest) {
+  auto a = IntImm::make(2);
+  auto b = IntImm::make(3);
+  auto c = Mul::make(a, b);
+  LLVMCodeGen cg;
+  c.accept(&cg);
+  EXPECT_EQ(cg.value(), 6);
+}
+
+TEST(ExprTest, IntDivTest) {
+  auto a = IntImm::make(6);
+  auto b = IntImm::make(3);
+  auto c = Div::make(a, b);
+  LLVMCodeGen cg;
+  c.accept(&cg);
+  EXPECT_EQ(cg.value(), 2);
+}
