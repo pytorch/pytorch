6a56a591d76d34aced8f024e7203cd1d2e3bbe3d
Ivan Kobzarev <ivankobzarev@fb.com>
2019-12-13 20:38:40 -0800

    Loading module from android asset (#30378)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30378
    
    Loading module directly from android assets. Iteration on https://github.com/pytorch/pytorch/pull/30109
    Loading Module:
    ```
    mModule = AndroidUtils.loadModuleFromAsset(assetName, getAssets());
    ```
    
    `org.pytorch.AndroidUtils` is excluded from pytorch_jni host build
    
    Testing:
    test_app module load switched to this approach and works fine
    ```
    gradle test_app:installMobNet2QuantDebug -PABI_FILTERS=x86 && adb shell am start -n org.pytorch.testapp.mobNet2Quant/org.pytorch.testapp.MainActivity
    ```
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18893269
    
    Pulled By: IvanKobzarev
    
    fbshipit-source-id: a7c73776f40e9c67bef233da05db56cc6efbe76a

--- android/pytorch_android/src/main/java/org/pytorch/PyTorchAndroid.java
+++ android/pytorch_android/src/main/java/org/pytorch/PyTorchAndroid.java
@@ -1,5 +1,7 @@
 package org.pytorch;
 
+import android.content.res.AssetManager;
+
 import com.facebook.soloader.nativeloader.NativeLoader;
 import com.facebook.soloader.nativeloader.SystemDelegate;
 
@@ -9,6 +11,17 @@ public final class PyTorchAndroid {
       NativeLoader.init(new SystemDelegate());
     }
     NativeLoader.loadLibrary("pytorch_jni");
+  }
+
+  /**
+   * Attention:
+   * This is not recommended way of loading production modules, as prepackaged assets increase apk size etc.
+   * For production usage consider using loading from file on the disk {@link org.pytorch.Module#load(String)}.
+   *
+   * This method is meant to use in tests and demos.
+   */
+  public static Module loadModuleFromAsset(final AssetManager assetManager, final String assetName) {
+    return new Module(new NativePeer(assetName, assetManager));
   }
 
   /**
