58eb15f41cdfbdc274ec8345876c0533a9af5cad
Martin Yuan <iseeyuan@yahoo.com>
2019-12-14 00:16:30 -0800

    JIT Type parser for mobile (#30391)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30391
    
    A Type parser to parse the python string of a Type. For example,
    "Tuple[str, Optional[float], Dict[str, List[Tensor]], int]".
    Please refer to test_type_parser.cpp for the usage.
    
    One of the use cases is in lite interpreter, types needs to be serialized (directly calling the python_str() of the Type) and deserialized (calling parseType(str)).
    
    Test Plan: Imported from OSS
    
    Differential Revision: D18924268
    
    Pulled By: iseeyuan
    
    fbshipit-source-id: 830d411563abfbeec023f01e7f8f4a1796f9a59a

--- test/cpp/jit/tests.h
+++ test/cpp/jit/tests.h
@@ -76,7 +76,8 @@ namespace jit {
   _(LiteInterpreterTuple)              \
   _(LiteInterpreterPrimOverload)       \
   _(CommonAncestor)                    \
-  _(AutogradSymbols)
+  _(AutogradSymbols)                   \
+  _(MobileTypeParser)
 
 #define TH_FORALL_TESTS_CUDA(_) \
   _(ArgumentSpec)               \
