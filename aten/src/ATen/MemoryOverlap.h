bd1d1d665ead8b509148caaa79c138de04e2e804
Peter Bell <peterbell10@live.co.uk>
2019-12-14 13:59:43 +0000

    Fix linking and zero-size tensor errors

--- aten/src/ATen/MemoryOverlap.h
+++ aten/src/ATen/MemoryOverlap.h
@@ -21,8 +21,8 @@ CAFFE2_API MemOverlap has_internal_overlap(TensorImpl* t);
 CAFFE2_API void assert_no_internal_overlap(const Tensor& t);
 CAFFE2_API void assert_no_internal_overlap(TensorImpl* t);
 
-MemOverlapStatus get_overlap_status(const Tensor& a, const Tensor& b);
-MemOverlapStatus get_overlap_status(TensorImpl* a, TensorImpl* b);
+CAFFE2_API MemOverlapStatus get_overlap_status(const Tensor& a, const Tensor& b);
+CAFFE2_API MemOverlapStatus get_overlap_status(TensorImpl* a, TensorImpl* b);
 
 void assert_no_partial_overlap(const Tensor& a, const Tensor& b);
 void assert_no_partial_overlap(TensorImpl* a, TensorImpl* b);
