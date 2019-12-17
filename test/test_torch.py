bfee9edad58456672bb14f8f3d290b0237f19403
pritam <pritam.damania@fb.com>
2019-12-16 18:23:25 -0800

    Update on "Move pytorch distributed tests to separate folder for contbuild."
    Create distributed and rpc directories under caffe/test for better management
    of unit tests.
    
    For this purpose, we've created a new `torch.testing._internal` package where
    we install all of our test libraries. This ensures we don't assume that all test files
    are in the test/ folder (which enables them to use test libraries without imports)
    
    Differential Revision: [D18702786](https://our.internmc.facebook.com/intern/diff/D18702786/)
    
    [ghstack-poisoned]

--- test/test_torch.py
+++ test/test_torch.py
@@ -4286,7 +4286,8 @@ class _TestTorchMixin(object):
             return module
 
         with filecontext_lambda() as checkpoint:
-            fname = get_file_path_2(os.path.dirname(os.path.dirname(torch.__file__)), 'torch', 'testing', '_internal', 'data', 'network1.py')
+            fname = get_file_path_2(os.path.dirname(os.path.dirname(torch.__file__)), 'torch', 'testing',
+                                    '_internal', 'data', 'network1.py')
             module = import_module(tmpmodule_name, fname)
             torch.save(module.Net(), checkpoint)
 
@@ -4299,7 +4300,8 @@ class _TestTorchMixin(object):
                     self.assertEquals(len(w), 0)
 
             # Replace the module with different source
-            fname = get_file_path_2(os.path.dirname(os.path.dirname(torch.__file__)), 'torch', 'testing', '_internal', 'data', 'network2.py')
+            fname = get_file_path_2(os.path.dirname(os.path.dirname(torch.__file__)), 'torch', 'testing',
+                                    '_internal', 'data', 'network2.py')
             module = import_module(tmpmodule_name, fname)
             checkpoint.seek(0)
             with warnings.catch_warnings(record=True) as w:
