a5c27caf6753db5e755ad651b32baee8938dd194
Bert Maher <bertrand@fb.com>
2019-12-18 13:08:11 -0800

    Refactor JIT class to isolate no-rtti pieces

--- torch/csrc/jit/compiler/include/llvm_jit.h
+++ torch/csrc/jit/compiler/include/llvm_jit.h
@@ -1,97 +1,31 @@
 #ifndef NNC_LIB_LLVM_JIT_H_
 #define NNC_LIB_LLVM_JIT_H_
 
-#include <algorithm>
+#include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/Target/TargetMachine.h"
+
 #include <memory>
 #include <string>
-#include <vector>
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/ExecutionEngine/JITSymbol.h"
-#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
-#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
-#include "llvm/ExecutionEngine/Orc/LambdaResolver.h"
-#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
-#include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
-#include "llvm/ExecutionEngine/SectionMemoryManager.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/Mangler.h"
-#include "llvm/Support/DynamicLibrary.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Target/TargetMachine.h"
 
 namespace llvm {
 namespace orc {
 
-// Lightly modified implementation from LLVM's Kaleidoscope JIT tutorial:
-// https://llvm.org/docs/tutorial/BuildingAJIT1.html
+class PytorchLLVMJITImpl;
+
 class PytorchLLVMJIT {
- private:
-#if LLVM_VERSION_MAJOR == 8 || LLVM_VERSION_MAJOR == 9
-  using JITLinkingLayer = LegacyRTDyldObjectLinkingLayer;
-  template <typename B, typename C>
-  using JITCompileLayer = LegacyIRCompileLayer<B, C>;
-#elif LLVM_VERSION_MAJOR == 7
-  using JITLinkingLayer = RTDyldObjectLinkingLayer;
-  template <typename B, typename C>
-  using JITCompileLayer = IRCompileLayer<B, C>;
-#else
-#error "Supported LLVM versions: 7, 8"
-#endif
-
-  ExecutionSession ES;
-  std::shared_ptr<SymbolResolver> Resolver;
-  std::unique_ptr<TargetMachine> TM;
-  const DataLayout DL;
-  JITLinkingLayer ObjectLayer;
-  JITCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
-
  public:
-  PytorchLLVMJIT()
-      : Resolver(createLegacyLookupResolver(
-            ES,
-            [this](const std::string& Name) -> JITSymbol {
-              if (auto Sym = CompileLayer.findSymbol(Name, false))
-                return Sym;
-              else if (auto Err = Sym.takeError())
-                return std::move(Err);
-              if (auto SymAddr = RTDyldMemoryManager::getSymbolAddressInProcess(Name))
-                return JITSymbol(SymAddr, JITSymbolFlags::Exported);
-              return nullptr;
-            },
-            [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); })),
-        TM(EngineBuilder().selectTarget()),
-        DL(TM->createDataLayout()),
-        ObjectLayer(
-            ES,
-            [this](VModuleKey) {
-              return JITLinkingLayer::Resources{std::make_shared<SectionMemoryManager>(), Resolver};
-            }),
-        CompileLayer(ObjectLayer, SimpleCompiler(*TM)) {
-    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
-  }
-
-  TargetMachine& getTargetMachine() { return *TM; }
-
-  VModuleKey addModule(std::unique_ptr<Module> M) {
-    // Add the module to the JIT with a new VModuleKey.
-    auto K = ES.allocateVModule();
-    cantFail(CompileLayer.addModule(K, std::move(M)));
-    return K;
-  }
-
-  JITSymbol findSymbol(const std::string Name) {
-    std::string MangledName;
-    raw_string_ostream MangledNameStream(MangledName);
-    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
-    return CompileLayer.findSymbol(MangledNameStream.str(), true);
-  }
-
-  JITTargetAddress getSymbolAddress(const std::string Name) {
-    return cantFail(findSymbol(Name).getAddress());
-  }
-
-  void removeModule(VModuleKey K) { cantFail(CompileLayer.removeModule(K)); }
+  PytorchLLVMJIT();
+  ~PytorchLLVMJIT();
+  TargetMachine& getTargetMachine();
+  VModuleKey addModule(std::unique_ptr<Module> M);
+  JITSymbol findSymbol(const std::string Name);
+  JITTargetAddress getSymbolAddress(const std::string Name);
+  void removeModule(VModuleKey K);
+  
+ private:
+  // Use PImpl idiom here to hide the no-rtti parts of the JIT structure.
+  std::unique_ptr<PytorchLLVMJITImpl> impl_;
 };
 
 }  // end namespace orc
---
+++ torch/csrc/jit/compiler/src/llvm_jit.cc
@@ -0,0 +1,112 @@
+#include "llvm_jit.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/LambdaResolver.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Mangler.h"
+#include "llvm/Support/DynamicLibrary.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+namespace orc {
+
+// Lightly modified implementation from LLVM's Kaleidoscope JIT tutorial:
+// https://llvm.org/docs/tutorial/BuildingAJIT1.html
+class PytorchLLVMJITImpl {
+ private:
+#if LLVM_VERSION_MAJOR == 8 || LLVM_VERSION_MAJOR == 9
+  using JITLinkingLayer = LegacyRTDyldObjectLinkingLayer;
+  template <typename B, typename C>
+  using JITCompileLayer = LegacyIRCompileLayer<B, C>;
+#elif LLVM_VERSION_MAJOR == 7
+  using JITLinkingLayer = RTDyldObjectLinkingLayer;
+  template <typename B, typename C>
+  using JITCompileLayer = IRCompileLayer<B, C>;
+#else
+#error "Supported LLVM versions: 7, 8"
+#endif
+
+  ExecutionSession ES;
+  std::shared_ptr<SymbolResolver> Resolver;
+  std::unique_ptr<TargetMachine> TM;
+  const DataLayout DL;
+  JITLinkingLayer ObjectLayer;
+  JITCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
+
+ public:
+  PytorchLLVMJITImpl()
+      : Resolver(createLegacyLookupResolver(
+            ES,
+            [this](const std::string& Name) -> JITSymbol {
+              if (auto Sym = CompileLayer.findSymbol(Name, false))
+                return Sym;
+              else if (auto Err = Sym.takeError())
+                return std::move(Err);
+              if (auto SymAddr = RTDyldMemoryManager::getSymbolAddressInProcess(Name))
+                return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+              return nullptr;
+            },
+            [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); })),
+        TM(EngineBuilder().selectTarget()),
+        DL(TM->createDataLayout()),
+        ObjectLayer(
+            ES,
+            [this](VModuleKey) {
+              return JITLinkingLayer::Resources{std::make_shared<SectionMemoryManager>(), Resolver};
+            }),
+        CompileLayer(ObjectLayer, SimpleCompiler(*TM)) {
+    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+  }
+
+  TargetMachine& getTargetMachine() { return *TM; }
+
+  VModuleKey addModule(std::unique_ptr<Module> M) {
+    // Add the module to the JIT with a new VModuleKey.
+    auto K = ES.allocateVModule();
+    cantFail(CompileLayer.addModule(K, std::move(M)));
+    return K;
+  }
+
+  JITSymbol findSymbol(const std::string Name) {
+    std::string MangledName;
+    raw_string_ostream MangledNameStream(MangledName);
+    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
+    return CompileLayer.findSymbol(MangledNameStream.str(), true);
+  }
+
+  JITTargetAddress getSymbolAddress(const std::string Name) {
+    return cantFail(findSymbol(Name).getAddress());
+  }
+
+  void removeModule(VModuleKey K) { cantFail(CompileLayer.removeModule(K)); }
+};
+
+PytorchLLVMJIT::PytorchLLVMJIT() : impl_(std::make_unique<PytorchLLVMJITImpl>()) {}
+
+
+PytorchLLVMJIT::~PytorchLLVMJIT() = default;
+
+TargetMachine& PytorchLLVMJIT::getTargetMachine() { return impl_->getTargetMachine(); }
+
+VModuleKey PytorchLLVMJIT::addModule(std::unique_ptr<Module> M) { return impl_->addModule(std::move(M)); }
+
+JITSymbol PytorchLLVMJIT::findSymbol(const std::string Name) { return impl_->findSymbol(Name); }
+
+JITTargetAddress PytorchLLVMJIT::getSymbolAddress(const std::string Name) { return impl_->getSymbolAddress(Name); }
+
+void PytorchLLVMJIT::removeModule(VModuleKey K) { impl_->removeModule(K); }
+
+}  // end namespace orc
+}  // end namespace llvm
