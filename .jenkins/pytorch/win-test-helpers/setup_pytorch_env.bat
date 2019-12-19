b382a52cb42f8eb2c13dd09cb206fb5e5134a2e0
Mingbo Wan <mingbo@fb.com>
2019-12-19 13:50:48 -0800

    debug why future is not installed

--- .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
+++ .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
@@ -23,7 +23,7 @@ if NOT "%BUILD_ENVIRONMENT%"=="" (
     call conda install -y -q python=3.6.7 numpy mkl cffi pyyaml boto3 protobuf numba==0.44.0
 )
 :: The version is fixed to avoid flakiness: https://github.com/pytorch/pytorch/issues/31136
-pip install -q ninja future "hypothesis==4.53.2" "librosa>=0.6.2" psutil pillow
+pip install ninja future "hypothesis==4.53.2" "librosa>=0.6.2" psutil pillow
 :: No need to install faulthandler since we only test Python >= 3.6 on Windows
 :: faulthandler is builtin since Python 3.3
 
