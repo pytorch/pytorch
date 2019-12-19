d3a8f2b5f33c0403d8074044f85277c953b0a504
Jeremy Lilley <jeremyl@fb.com>
2019-12-18 19:21:58 -0800

    Avoid sending large unneeded data over wire in process_group_agent. (#31357)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31357
    
    If a user selects a subset of a Tensor and sends it in an RPC, we were sending
    the whole original Tensor Storage over the network.
    
    While this sounds reasonable, in practice, we observed view-like Tensors being sent
    over rpc, where only 1% of the data in the provided Tensor's Storage was
    actually used/needed.
    
    The simple solution here is to just force a clone in the serializer code if we see that
    less than (arbitrary) half the bits are used, and the tensor is more than a nominal few KB.
    Add related tests to ensure this doesn't break.
    
    An alternate approach would be to modify the Pickler. That said, since Pickler is shared by more
    components, the logic might be harder to tailor appropriately at that layer (particularly
    given that the Pickler has explicit logic to share a single Storage* among several Tensors
    that commonly point to the same Storage*).
    
    It's possible that we might want to further refine the basic thresholds in this change.
    In practice, we've seen a mostly bimodal distribution thus far for the percent of Tensor
    Storage referred by a Tensor in observed rpcs (i.e. either 90%+ or sub-10% of the Storage
    referenced), hence the existing 50% threshold here is probably not an unreasonable
    starting point.
    ghstack-source-id: 95925474
    
    Test Plan: buck test mode/dev caffe2/test/cpp/rpc/...
    
    Differential Revision: D19137056
    
    fbshipit-source-id: e2b3a4dd0cc6e1de820fd0740aa1d59883dbf8d4

--- torch/csrc/distributed/rpc/utils.cpp
+++ torch/csrc/distributed/rpc/utils.cpp
@@ -173,6 +173,27 @@ static const char* kMeta = "meta";
 static const char* kPayload = "payload";
 }; // namespace
 
+c10::List<at::Tensor> cloneSparseTensors(
+    const std::vector<at::Tensor>& tensors) {
+  // Sanity-check: If the majority of bits don't need to go over the wire,
+  // force a clone(). Some Tensors are effectively small views, only using
+  // ~1% of the underlying Storage.
+  auto worthRecopying = [](const at::Tensor& t) -> bool {
+    auto storageSize = t.storage().elementSize() * t.storage().numel();
+    auto usefulSize = t.element_size() * t.numel();
+    constexpr size_t kMinMultiple = 2;
+    constexpr size_t kMinRecopyBytes = 8 * 1024;
+    return storageSize >= kMinRecopyBytes &&
+        storageSize >= usefulSize * kMinMultiple;
+  };
+  c10::List<at::Tensor> pTensors;
+  pTensors.reserve(tensors.size());
+  for (const auto& t : tensors) {
+    pTensors.push_back(worthRecopying(t) ? t.clone() : t);
+  }
+  return pTensors;
+}
+
 std::string wireSerialize(
     const std::vector<char>& payload,
     const std::vector<at::Tensor>& tensors) {
@@ -197,7 +218,7 @@ std::string wireSerialize(
         },
         nullptr);
     pickler.protocol();
-    pickler.pushIValue(tensors);
+    pickler.pushIValue(cloneSparseTensors(tensors));
     pickler.stop();
     // tensorData is in function scope so that the data() pointers stay valid.
     tensorData = pickler.tensorData();
