52846376c89a725088ff101fd6999e13cae8b5c2
Mingbo Wan <mingbo@fb.com>
2019-12-20 15:20:21 -0800

    binary build for rc 1.4.0

--- .circleci/cimodel/data/binary_build_definitions.py
+++ .circleci/cimodel/data/binary_build_definitions.py
@@ -68,8 +68,6 @@ class Conf(object):
             job_def["requires"].append("update_s3_htmls_for_nightlies")
             job_def["requires"].append("update_s3_htmls_for_nightlies_devtoolset7")
             job_def["filters"] = {"branches": {"only": "postnightly"}}
-        else:
-            job_def["filters"] = {"branches": {"only": "nightly"}}
         if self.libtorch_variant:
             job_def["libtorch_variant"] = miniutils.quote(self.libtorch_variant)
         if phase == "test":
