f65ca5204838690edd73c5f520cb8bf6691a08d9
peterjc123 <peterghost86@gmail.com>
2019-12-24 14:50:31 +0800

    Wait for vs_installer.exe to finish

--- .circleci/scripts/vs_install.bat
+++ .circleci/scripts/vs_install.bat
@@ -21,6 +21,7 @@ curl -k -L %VS_DOWNLOAD_LINK% --output vs_installer.exe
 if errorlevel 1 exit /b 1
 
 start /wait .\vs_installer.exe %VS_INSTALL_ARGS%
+powershell -Command Wait-Process -Name vs_installer.exe
 if not errorlevel 0 exit /b 1
 if errorlevel 1 if not errorlevel 3010 exit /b 1
 if errorlevel 3011 exit /b 1
