3e2892564caea4b97107b1bd1a5663b2ed49ea79
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2019-12-27 09:15:26 -0600

    convolution

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -537,7 +537,7 @@ at::Tensor _convolution(
     IntArrayRef stride_, IntArrayRef padding_, IntArrayRef dilation_,
     bool transposed_, IntArrayRef output_padding_, int64_t groups_,
     bool benchmark, bool deterministic, bool cudnn_enabled) {
-    std::cout << "calling _convolution.\n";
+  //    std::cout << "calling _convolution.\n";
   const bool input_is_mkldnn = input_r.is_mkldnn();
   auto input = input_r;
   auto weight = weight_r;
@@ -646,24 +646,24 @@ at::Tensor _convolution(
                                       params.padding, params.stride, params.dilation, params.groups);
     } else {
       // do not call contiguous on mkldnn tensor
-      std::cout << "if mkldnn_convolution.\n";
+      //      std::cout << "if mkldnn_convolution.\n";
       output = at::mkldnn_convolution(input, weight, bias,
                                       params.padding, params.stride, params.dilation, params.groups);
     }
 #endif
   } else if (input.device().type() == c10::DeviceType::CPU || input.device().type() == c10::DeviceType::CUDA) {
-    std::cout << "if input dev: CPU\n";
+    //    std::cout << "if input dev: CPU\n";
     if (params.use_cpu_depthwise3x3_winograd(input, weight)) {
-      std::cout << "\tif use cpu depth wise\n";
+      //      std::cout << "\tif use cpu depth wise\n";
       output = convolution_depthwise3x3_winograd_stub(
         input.device().type(), input, weight, bias, params.stride, params.padding, params.groups);
     } else if (params.groups == 1) {
-       std::cout << "\telse if params groups\n";
+      //       std::cout << "\telse if params groups\n";
       output = at::_convolution_nogroup(
           input.contiguous(), weight, bias, params.stride, params.padding, params.dilation, params.transposed, params.output_padding);
-      std::cout << "\toutput grad_fn: " << output.grad_fn() << std::endl;
+      //      std::cout << "\toutput grad_fn: " << output.grad_fn() << std::endl;
     } else {
-      std::cout << "if input device type CPU.\n";
+      //      std::cout << "if input device type CPU.\n";
       std::vector<Tensor> outputs(params.groups);
       input = input.contiguous();
       for (int g = 0; g < params.groups; ++g) {
