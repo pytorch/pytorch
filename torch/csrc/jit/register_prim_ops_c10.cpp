0c980f74fe746e0786451f19483f7724de14aefd
Martin Yuan <iseeyuan@yahoo.com>
2019-12-27 08:15:47 -0800

    Update on "Move prim ops from JIT registration to C10"
    The first version to move prim ops to c10 registration. After the reviewers are fine with the initial changes, more operators will be moved in the same style.

--- torch/csrc/jit/register_prim_ops_c10.cpp
+++ torch/csrc/jit/register_prim_ops_c10.cpp
@@ -16,21 +16,21 @@ static auto registry_prim = torch::RegisterOperators().op("aten::Int.Tensor(Tens
   torch::RegisterOperators::options().catchAllKernel(
   [](at::Tensor a) -> int64_t {
     return a.item<int64_t>();
-})
+}).aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA)
 ).op("aten::Int.bool(bool a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](bool b) -> int64_t {
     return static_cast<int64_t>(b);
-})
+}).aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA)
 ).op("aten::Int.float(float a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](double d) -> int64_t {
     return static_cast<int64_t>(d);
-})
+}).aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA)
 ).op("aten::Int.Scalar(Scalar a) -> int",
   torch::RegisterOperators::options().catchAllKernel(
   [](Scalar scalar) -> int64_t {
     return scalar.toInt();
-})
+}).aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA)
 );
 
