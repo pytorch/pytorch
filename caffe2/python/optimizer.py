4983ef8de19cd5f9e52f2c9b9a779d63c94c5fba
Jiyan Yang <chocjy@fb.com>
2019-12-26 17:14:44 -0800

    Integrating MaskedAdagrad
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/31640
    
    Test Plan: unit test
    
    Reviewed By: ellie-wen
    
    Differential Revision: D18805278
    
    fbshipit-source-id: 1def4a89b7e4e04385c762bf127d95c5e513180e

--- caffe2/python/optimizer.py
+++ caffe2/python/optimizer.py
@@ -519,7 +519,8 @@ class AdagradOptimizer(Optimizer):
     def __init__(self, alpha=0.01, epsilon=1e-4, decay=1, policy="fixed",
                  sparse_dedup_aggregator=None, rowWise=False, engine='',
                  lars=None, output_effective_lr=False,
-                 output_effective_lr_and_update=False, **kwargs):
+                 output_effective_lr_and_update=False,
+                 mask_tensor=None, **kwargs):
         super(AdagradOptimizer, self).__init__()
         self.alpha = alpha
         self.epsilon = epsilon
@@ -531,7 +532,13 @@ class AdagradOptimizer(Optimizer):
         self.lars = lars
         self.output_effective_lr = output_effective_lr
         self.output_effective_lr_and_update = output_effective_lr_and_update
+        self.mask_tensor = mask_tensor
         self.init_kwargs = kwargs
+
+        self.use_mask = False
+        if self.mask_tensor is not None:
+            assert type(mask_tensor) is np.ndarray, "mask_tensor must be a numpy array!"
+            self.use_mask = True
 
     def _run(self, net, param_init_net, param_info):
         param = param_info.blob
@@ -614,6 +621,11 @@ class AdagradOptimizer(Optimizer):
                     value=0.0
                 )
 
+        # Adding masked Adagrad for dense parameter for now
+        if not isinstance(grad, core.GradientSlice) and self.mask_tensor is not None:
+            # Assuming np array for the mask tensor
+            mask_blob = param_init_net.GivenTensorFill([], [str(param) + "_mask"], values=self.mask_tensor, shape=self.mask_tensor.shape)
+
         self._aux_params.local.append(param_squared_sum)
 
         if self.rowWise:
@@ -638,18 +650,31 @@ class AdagradOptimizer(Optimizer):
         else:
             output_args = [param, param_squared_sum]
             if self.output_effective_lr_and_update:
+                assert self.use_mask is False, \
+                    "MaskedAdagrad doesn't support outputting effective_lr_and_update"
                 output_args.append(str(param) + '_effective_lr')
                 output_args.append(str(param) + '_update')
             elif self.output_effective_lr:
+                assert self.use_mask is False, \
+                    "MaskedAdagrad doesn't support outputting effective_lr"
                 output_args.append(str(param) + '_effective_lr')
 
-            net.Adagrad(
-                [param, param_squared_sum, grad, lr],
-                output_args,
-                epsilon=self.epsilon,
-                decay=float(self.decay),
-                engine=self.engine
-            )
+            if self.use_mask:
+                net.MaskedAdagrad(
+                    [param, param_squared_sum, grad, lr, mask_blob],
+                    output_args,
+                    epsilon=self.epsilon,
+                    decay=float(self.decay),
+                    engine=self.engine
+                )
+            else:
+                net.Adagrad(
+                    [param, param_squared_sum, grad, lr],
+                    output_args,
+                    epsilon=self.epsilon,
+                    decay=float(self.decay),
+                    engine=self.engine
+                )
 
     def scale_learning_rate(self, scale):
         self.alpha *= scale
