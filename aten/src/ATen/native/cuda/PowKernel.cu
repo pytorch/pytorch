52fb4864f86a7d4d2c5f0544e80e1c941732a8b9
peter <peterghost86@gmail.com>
2019-12-30 20:24:22 +0800

    Fix inverse sqrt

--- aten/src/ATen/native/cuda/PowKernel.cu
+++ aten/src/ATen/native/cuda/PowKernel.cu
@@ -22,52 +22,81 @@ inline __host__ __device__ T powi(T a, T b) {
   return result;
 }
 
-// SFINAE doesn't work well with NVCC under Windows for pow.
+// SFINAE doesn't work well with NVCC under Windows for math functions like pow and sqrt.
 // So we need to define the functions with the explicit function signatures.
+// As for pow, the following signatures are defined as the device function:
+//   pow(float, int)
+//   pow(double, int)
+//   pow(float, float)
+//   pow(double, double)
+// As for sqrt, the following signatures are defined as the device function:
+//   sqrt(float)
+//   sqrt(double)
+// As for inverse sqrt, we must define it explicitly in MSVC, otherwise the static cast will be
+// applied to the result of the inline function, and thus the result is incorrect.
+//   e.g. if we use 1.0 / sqrt(2) for 2 ^ (-0.5) in MSVC, we get
+//          int(2 ^ (-0.5)) = int(1.0 / sqrt(2)) = int(1.0 / int(1.414)) = int(1.0 / 1) = 1
+//        However, the correct result is 
+//          int(2 ^ (-0.5)) = int(1.0 / 1.414) = 0
 #ifdef _MSC_VER
-  // Functions for pow
-  // pow for at::Half
-  inline __host__ __device__ at::Half pow_(at::Half base, at::Half exp) {
-    return static_cast<at::Half>(std::pow(static_cast<double>(base), static_cast<double>(exp)));
-  }
-  // pow (floating, floating/int)
-  template <typename Base_type, typename Exp_type>
-  inline __host__ __device__ typename std::enable_if<std::is_floating_point<Base_type>::value && (std::is_same<Base_type, Exp_type>::value || std::is_same<Exp_type, int>::value), Base_type>::type
-    pow_(Base_type base, Exp_type exp) {
-    return std::pow(base, exp);
-  }
-  // pow (integral, integral)
-  template <typename Base_type, typename Exp_type>
-  inline __host__ __device__ typename std::enable_if<std::is_integral<Base_type>::value && std::is_same<Base_type, Exp_type>::value, Base_type>::type
-    pow_(Base_type base, Exp_type exp) {
-    return powi(base, exp);
-  }
-  // pow (Otherwise)
-  template <typename Base_type, typename Exp_type>
-  inline __host__ __device__ typename std::enable_if<!std::is_same<Base_type, Exp_type>::value && !std::is_same<Exp_type, int>::value, Base_type>::type
-    pow_(Base_type base, Exp_type exp) {
-    return static_cast<Base_type>(std::pow(static_cast<double>(base), static_cast<double>(exp)));
-  }
-  // Functions for sqrt
-  // sqrt (floating)
-  template <typename T>
-  inline __host__ __device__ typename std::enable_if<std::is_floating_point<T>::value, T>::type sqrt_(T x) {
-    return std::sqrt(x);
-  }
-  // sqrt (integral)
-  template <typename T>
-  inline __host__ __device__ typename std::enable_if<!std::is_floating_point<T>::value, T>::type sqrt_(T x) {
-    return static_cast<T>(std::sqrt(static_cast<double>(x)));
-  }
+// Functions for pow
+// pow for at::Half
+inline __host__ __device__ at::Half pow_(at::Half base, at::Half exp) {
+  return static_cast<at::Half>(std::pow(static_cast<double>(base), static_cast<double>(exp)));
+}
+// pow (floating, floating/int)
+template <typename Base_type, typename Exp_type>
+inline __host__ __device__ typename std::enable_if<std::is_floating_point<Base_type>::value && (std::is_same<Base_type, Exp_type>::value || std::is_same<Exp_type, int>::value), Base_type>::type
+  pow_(Base_type base, Exp_type exp) {
+  return std::pow(base, exp);
+}
+// pow (integral, integral)
+template <typename Base_type, typename Exp_type>
+inline __host__ __device__ typename std::enable_if<std::is_integral<Base_type>::value && std::is_same<Base_type, Exp_type>::value, Base_type>::type
+  pow_(Base_type base, Exp_type exp) {
+  return powi(base, exp);
+}
+// pow (Otherwise)
+template <typename Base_type, typename Exp_type>
+inline __host__ __device__ typename std::enable_if<!std::is_same<Base_type, Exp_type>::value && !std::is_same<Exp_type, int>::value, Base_type>::type
+  pow_(Base_type base, Exp_type exp) {
+  return static_cast<Base_type>(std::pow(static_cast<double>(base), static_cast<double>(exp)));
+}
+// Functions for sqrt
+// sqrt (floating)
+template <typename T>
+inline __host__ __device__ typename std::enable_if<std::is_floating_point<T>::value, T>::type sqrt_(T x) {
+  return std::sqrt(x);
+}
+// sqrt (integral)
+template <typename T>
+inline __host__ __device__ typename std::enable_if<!std::is_floating_point<T>::value, T>::type sqrt_(T x) {
+  return static_cast<T>(std::sqrt(static_cast<double>(x)));
+}
+// Function for inverse sqrt
+// invsqrt (floating)
+template <typename T>
+inline __host__ __device__ typename std::enable_if<std::is_floating_point<T>::value, T>::type invsqrt_(T x) {
+  return 1.0 / std::sqrt(x);
+}
+// invsqrt (integral)
+template <typename T>
+inline __host__ __device__ typename std::enable_if<!std::is_floating_point<T>::value, T>::type invsqrt_(T x) {
+  return static_cast<T>(1.0 / std::sqrt(static_cast<double>(x)));
+}
 #else
-  template <typename Base_type, typename Exp_type>
-  inline __host__ __device__ Base_type pow_(Base_type base, Exp_type exp) {
-    return std::pow(base, exp);
-  }
-  template <typename T>
-  inline __host__ __device__ T sqrt_(T x) {
-    return std::sqrt(x);
-  }
+template <typename Base_type, typename Exp_type>
+inline __host__ __device__ Base_type pow_(Base_type base, Exp_type exp) {
+  return std::pow(base, exp);
+}
+template <typename T>
+inline __host__ __device__ T sqrt_(T x) {
+  return std::sqrt(x);
+}
+template <typename T>
+inline __host__ __device__ T invsqrt_(T x) {
+  return 1.0 / std::sqrt(x);
+}
 #endif
 
 void pow_tensor_tensor_kernel(TensorIterator& iter) {
@@ -104,7 +133,7 @@ void pow_tensor_scalar_kernel_impl(TensorIterator& iter,
     });
   } else if (d_exp == -0.5) {
     gpu_kernel(iter, [=]GPU_LAMBDA(Base_type base) -> Base_type {
-      return 1.0 / sqrt_(base);
+      return invsqrt_(base);
     });
   } else if (d_exp == -1) {
     gpu_kernel(iter, [=]GPU_LAMBDA(Base_type base) -> Base_type {
