8936e4ed33646ebf020f74d9d7248589f91bc30f
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos061.prn3.facebook.com>
2020-01-05 00:09:33 -0800

    2020-01-05 nightly release (3f0b330736a2af1331e49e8e12e880dffd1e5974)

--- torch/csrc/jit/script/class_type.cpp
+++ torch/csrc/jit/script/class_type.cpp
@@ -149,6 +149,12 @@ c10::optional<IValue> ClassType::findConstant(const std::string& name) const {
   return constantValues_[pos];
 }
 
+void ClassType::unsafeRemoveConstant(const std::string& name) {
+  auto slot = getConstantSlot(name);
+  constantNames_.erase(constantNames_.begin() + slot);
+  constantValues_.erase(constantValues_.begin() + slot);
+}
+
 std::shared_ptr<CompilationUnit> ClassType::compilation_unit() {
   auto cu = compilation_unit_.lock();
   TORCH_INTERNAL_ASSERT(cu);
