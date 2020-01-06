7bb3c7085cb6bbc34100de6b4e38c5b2d3cb4092
Peter Bell <peterbell10@live.co.uk>
2020-01-06 15:19:27 -0800

    no_grad, enable_grad: support for decorating generator functions (#31792)
    Summary:
    Closes https://github.com/pytorch/pytorch/issues/31497
    
    This allows `torch.no_grad` and `torch.enable_grad` to be used as decorators for generator functions. In which case it disables/enables grad only inside the body of the generator and restores the context outside of the generator.
    
    https://github.com/pytorch/pytorch/issues/31497 doesn't include a complete reproducer but the included test with `torch.is_grad_enabled` show this is working where it failed before.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31792
    
    Differential Revision: D19274971
    
    Pulled By: albanD
    
    fbshipit-source-id: fde6d3fd95d76c8d324ad02db577213a4b68ccbe

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -880,6 +880,27 @@ class TestAutograd(TestCase):
             w = adder(x, y)
             self.assertFalse(torch.is_grad_enabled())
 
+    def test_set_grad_generator_functions(self):
+        @torch.no_grad()
+        def gen_no_grad():
+            for i in range(10):
+                self.assertEqual(torch.is_grad_enabled(), False)
+                yield i
+
+        with torch.enable_grad():
+            for _ in gen_no_grad():
+                self.assertEqual(torch.is_grad_enabled(), True)
+
+        @torch.enable_grad()
+        def gen_enable_grad():
+            for i in range(10):
+                self.assertEqual(torch.is_grad_enabled(), True)
+                yield i
+
+        with torch.no_grad():
+            for _ in gen_enable_grad():
+                self.assertEqual(torch.is_grad_enabled(), False)
+
     def test_no_grad_python_function(self):
         """Python Functions should respect grad mode."""
         x = torch.ones(5, 5, requires_grad=True)
