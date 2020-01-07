5abf0d84723564b57663537f44e8c96f4a70cfdd
lixinyu <lixinyu@devvm1329.atn1.facebook.com>
2020-01-07 12:00:59 -0800

    named tensor max pooling support
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/31669
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19240348
    
    Pulled By: glaringlee
    
    fbshipit-source-id: 004387aa753e4e41afdede66647abbb0bcbd9808

--- test/test_namedtensor.py
+++ test/test_namedtensor.py
@@ -272,6 +272,26 @@ class TestNamedTensor(TestCase):
         self.assertEqual(named_tensor.diagonal(outdim='E', dim1='B', dim2='D').names,
                          ['A', 'C', 'E'])
 
+    def test_max_pooling(self):
+        def check_tuple_return(op, inputs, expected_names):
+            values, indices = op(*inputs)
+            self.assertEqual(values.names, expected_names)
+            self.assertEqual(indices.names, expected_names)
+
+        for device in torch.testing.get_all_device_types():
+
+            named_tensor_1d = torch.zeros(2, 3, 5, device=device, names=list('ABC'))
+            named_tensor_2d = torch.zeros(2, 3, 5, 7, device=device, names=list('ABCD'))
+            named_tensor_3d = torch.zeros(2, 3, 5, 7, 9, device=device, names=list('ABCDE'))
+
+            self.assertEqual(F.max_pool1d(named_tensor_1d, 2).names, named_tensor_1d.names)
+            self.assertEqual(F.max_pool2d(named_tensor_2d, [2, 2]).names, named_tensor_2d.names)
+            self.assertEqual(F.max_pool3d(named_tensor_3d, [2, 2, 2]).names, named_tensor_3d.names)
+
+            check_tuple_return(F.max_pool1d_with_indices, [named_tensor_1d, 2], named_tensor_1d.names)
+            check_tuple_return(F.max_pool2d_with_indices, [named_tensor_2d, [2, 2]], named_tensor_2d.names)
+            check_tuple_return(F.max_pool3d_with_indices, [named_tensor_3d, [2, 2, 2]], named_tensor_3d.names)
+
     def test_no_save_support(self):
         named_tensor = torch.zeros(2, 3, names=('N', 'C'))
         buf = io.BytesIO()
