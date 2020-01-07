c87ae63a4c0f05a8cb2d70e393bf5dbc4d664315
Peter Bell <peterbell10@live.co.uk>
2020-01-07 11:28:46 -0800

    Raise an error if torch.cat is given `out` as one of the input tensors (#30577)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/30562 for both cpu and cuda.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30577
    
    Differential Revision: D19298732
    
    Pulled By: ezyang
    
    fbshipit-source-id: ea539c97493ee17d8f60b1134d100a44c8717578

--- aten/src/TH/generic/THTensor.cpp
+++ aten/src/TH/generic/THTensor.cpp
@@ -6,6 +6,7 @@
 #include <ATen/NativeFunctions.h>
 #include <new>
 #include <ATen/NamedTensorUtils.h>
+#include <ATen/MemoryOverlap.h>
 
 /**** access methods ****/
 THStorage *THTensor_(storage)(const THTensor *self)
@@ -623,6 +624,16 @@ void THTensor_(catArray)(THTensor *result, THTensor **inputs, int numInputs, int
   bool allSkipped= true;
   int64_t nDims = 0;
   THTensor *notSkippedTensor;  // non-owning reference
+
+  // Inputs cannot alias the output tensor
+  for (int i = 0; i < numInputs; i++) {
+    auto lap = at::get_overlap_status(result, inputs[i]);
+    THArgCheck(lap != at::MemOverlapStatus::PARTIAL &&
+        lap != at::MemOverlapStatus::FULL, 0,
+        "unsupported operation: the input tensors cannot refer to any of the "
+        "output memory locations. Found overlap in input tensor %d.", i);
+  }
+
   auto should_skip = [](THTensor *t) { return t->is_empty() && t->dim() == 1; };
   for (int i = 0; i < numInputs; i++) {
     if (should_skip(inputs[i])) {
