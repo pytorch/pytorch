062933635e94e97618513593052e6b4953f0c711
Sebastian Messmer <messmer@fb.com>
2020-01-08 15:50:51 -0800

    Update on "[wip] Don't use codegen'ed boxing wrappers in c10 for factory ops"
    Differential Revision: [D19320662](https://our.internmc.facebook.com/intern/diff/D19320662/)
    
    [ghstack-poisoned]

--- tools/jit/templates/register_aten_ops.cpp
+++ tools/jit/templates/register_aten_ops.cpp
@@ -113,7 +113,8 @@ public:
         schema,
         Operation([boxed_kernel_wrapper = std::move(boxed_kernel_wrapper)] (Stack& stack) -> int {
           return boxed_kernel_wrapper(&stack);
-        })
+        }),
+        atenOperatorOptions()
       )
     );
     return std::move(*this);
