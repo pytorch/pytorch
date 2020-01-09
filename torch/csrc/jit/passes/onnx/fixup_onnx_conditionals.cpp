bce520a330997116c38cefa97565788ae8c310d4
Brian Stark <brstark@microsoft.com>
2020-01-09 11:27:40 -0800

    changes based on comments.

--- torch/csrc/jit/passes/onnx/fixup_onnx_conditionals.cpp
+++ torch/csrc/jit/passes/onnx/fixup_onnx_conditionals.cpp
@@ -7,20 +7,20 @@ namespace onnx{
 using namespace ::c10::onnx;
 }
 
-bool FixupONNXIfs(Block* block) {
-  bool has_nodes = false;
+void FixupONNXIfs(Block* block) {
   for (auto* node : block->nodes()) {
-    has_nodes = true;
     if (node->kind() == ::c10::onnx::If) {
       auto* if_node = node;
       auto* graph = if_node->owningGraph();
       for (Block* block : node->blocks()) {
-        if (!FixupONNXIfs(block)) {
+        FixupONNXIfs(block);
+        if (block->nodes().begin() == block->nodes().end()) {
           //ONNX does not support empty blocks, must use some op which does nothing
           Value* output = block->outputs()[0];
           Node* id_node = graph->create(onnx::Identity);
           id_node->insertBefore(block->return_node());
           id_node->addInput(output);
+          id_node->output()->copyMetadata(output);
           block->return_node()->replaceInputWith(output, id_node->output());
         }
       }
@@ -31,7 +31,6 @@ bool FixupONNXIfs(Block* block) {
       }
     }
   }
-  return has_nodes;
 }
 
 void FixupONNXConditionals(std::shared_ptr<Graph>& graph) {
