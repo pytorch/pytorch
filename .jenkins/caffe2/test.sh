44c1bf0a89207b87cd92b8e26b011ea4dfd05173
Negin Raoof <neginmr@utexas.edu>
2020-01-08 21:02:27 -0800

    Added torchvision tests as part of ORT tests (#31835)
    Summary:
    Added torchvision tests as part of ORT tests
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31835
    
    Reviewed By: hl475
    
    Differential Revision: D19278607
    
    Pulled By: houseroad
    
    fbshipit-source-id: 18a6a85ce3019bcc9aee9517af1378964b585afd

--- .jenkins/caffe2/test.sh
+++ .jenkins/caffe2/test.sh
@@ -136,7 +136,7 @@ pip install --user pytest-sugar
 # torchvision tests #
 #####################
 if [[ "$BUILD_ENVIRONMENT" == *onnx* ]]; then
-  pip install -q --user git+https://github.com/pytorch/vision.git
+  pip install -q --user git+https://github.com/pytorch/vision.git@v0.5.0
   pip install -q --user ninja
   # JIT C++ extensions require ninja, so put it into PATH.
   export PATH="/var/lib/jenkins/.local/bin:$PATH"
@@ -144,7 +144,7 @@ if [[ "$BUILD_ENVIRONMENT" == *onnx* ]]; then
     # default pip version is too old(9.0.2), unable to support tag `manylinux2010`.
     # Fix the pip error: Couldn't find a version that satisfies the requirement
     sudo pip install --upgrade pip
-    pip install -q --user -i https://test.pypi.org/simple/ ort-nightly==1.0.0.dev1123
+    pip install -q --user -i https://test.pypi.org/simple/ ort-nightly==1.1.0.dev1228
   fi
   "$ROOT_DIR/scripts/onnx/test.sh"
 fi
