66c5cb09462da54f6ea1c826f492ff1475973ab6
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-10 15:56:22 -0800

    fix nccl

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -17,7 +17,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 if [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda10.1-* ]]; then
   # TODO: move this to Docker
   sudo apt-get -qq update
-  sudo apt-get -qq install --allow-downgrades --allow-change-held-packages libnccl-dev=2.2.13-1+cuda10.1 libnccl2=2.2.13-1+cuda10.1
+  sudo apt-get -qq install --allow-downgrades --allow-change-held-packages libnccl-dev=2.5.6-1+cuda10.1 libnccl2=2.5.6-1+cuda10.1
 fi
 
 if [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda9*gcc7* ]] || [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda10.1-* ]] || [[ "$BUILD_ENVIRONMENT" == *-trusty-py2.7.9* ]]; then
