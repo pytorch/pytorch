45c88298cf9048872c8ec33e01f93a0e55a1448a
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-09 16:44:02 -0800

    Revert "try fix cuda9"
    This reverts commit 1509a138cfe8cb74fa0ee44d78d574697e151e99.

--- aten/src/ATen/core/Array.h
+++ aten/src/ATen/core/Array.h
@@ -17,11 +17,12 @@ struct alignas(16) Array {
   C10_HOST_DEVICE T& operator[](int i) {
     return data[i];
   }
-  C10_HOST_DEVICE Array() {};
 #ifdef __HIP_PLATFORM_HCC__
+  C10_HOST_DEVICE Array() = default;
   C10_HOST_DEVICE Array(const Array&) = default;
   C10_HOST_DEVICE Array& operator=(const Array&) = default;
 #else
+  Array() = default;
   Array(const Array&) = default;
   Array& operator=(const Array&) = default;
 #endif
