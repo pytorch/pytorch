6663aff4ea08e27b60d63ad5cae77b18adea47e8
Tzu-Wei Huang <huang.dexter@gmail.com>
2020-01-11 20:06:19 +0800

    update tensorboard warning message

--- torch/utils/tensorboard/__init__.py
+++ torch/utils/tensorboard/__init__.py
@@ -2,5 +2,5 @@ try:
     from tensorboard.summary.writer.record_writer import RecordWriter  # noqa F401
 except ImportError:
     raise ImportError('TensorBoard logging requires TensorBoard with Python summary writer installed. '
-                      'This should be available in 1.14 or above.')
+                      'This should be available in 1.15 or above.')
 from .writer import FileWriter, SummaryWriter  # noqa F401
