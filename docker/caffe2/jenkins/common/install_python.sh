cee89b585e24156786acb6366499d8340b1fedbe
J M Dieterich <dieterich@ogolem.org>
2020-01-12 14:02:06 -0800

    Revert "Support PyTorch ROCm CI on Ubuntu18.04 (#31886)" (#31946)
    Summary:
    This reverts commit 4ee9c562188ae930cb2520cfce7805f55acaf968.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31946
    
    Differential Revision: D19368391
    
    Pulled By: bddppq
    
    fbshipit-source-id: 63d032a5256ff4da7247fb1092be314c5b133eb6

--- docker/caffe2/jenkins/common/install_python.sh
+++ docker/caffe2/jenkins/common/install_python.sh
@@ -32,13 +32,11 @@ install_ubuntu() {
       ;;
     3.6)
       install_ubuntu_deadsnakes python3.6-dev
-      apt-get install -y python3-distutils
       PYTHON=python3.6
       INSTALL_SETUPTOOLS=yes
       ;;
     3.7)
       install_ubuntu_deadsnakes python3.7-dev
-      apt-get install -y python3-distutils
       PYTHON=python3.7
       INSTALL_SETUPTOOLS=yes
       ;;
