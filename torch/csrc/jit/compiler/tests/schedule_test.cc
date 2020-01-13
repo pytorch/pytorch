c6f8ecc326a1d204b06a8afb5f270fe005428b37
Xiaoqiang Zheng <zhengxq@fb.com>
2020-01-13 08:20:18 +0000

    Add a reference tests for Split

--- torch/csrc/jit/compiler/tests/schedule_test.cc
+++ torch/csrc/jit/compiler/tests/schedule_test.cc
@@ -50,10 +50,10 @@ TEST(TensorExpr, Lower01) {
 }
 
 TEST(TensorExpr, Simple02) {
-  Tensor tensor = Compute(
-      "f", {Expr(26), Expr(5)}, {"x", "y"}, [](const Var& x, const Var& y) {
-        return Expr(1.0f) + cast<float>(x) * x + cast<float>(y) * y;
-      });
+  auto func = [](const Expr& x, const Expr& y) {
+    return Expr(1.0f) + cast<float>(x) * x + cast<float>(y) * y;
+  };
+  Tensor tensor = Compute("f", {Expr(26), Expr(5)}, {"x", "y"}, func);
   Var x = tensor.function().arg(0);
   Var y = tensor.function().arg(1);
   Schedule sch = Schedule::make({tensor});
@@ -67,6 +67,37 @@ TEST(TensorExpr, Simple02) {
   std::ostringstream oss;
   oss << stmt;
   // TODO: switch to a better check
+
   ASSERT_GT(oss.str().size(), 200);
   ASSERT_LT(oss.str().size(), 500);
+
+  {
+    Var x_outer("x.outer", kInt32);
+    Var x_inner("x.inner", kInt32);
+    Var y("y", kInt32);
+    Var x_tail("x.tail", kInt32);
+    Var f("f", kHandle);
+    Expr x_1 = x_outer * 4 + x_inner;
+    Stmt stmt1 = For::make(
+        x_outer,
+        0,
+        6,
+        For::make(
+            x_inner,
+            0,
+            4,
+            For::make(
+                y, 0, 5, Store::make(f, x_1 * 5 + y * 1, func(x_1, y), 1))));
+    Expr x_2 = x_tail + Expr(6) * 4;
+    Stmt stmt2 = For::make(
+        x_tail,
+        0,
+        2,
+        For::make(y, 0, 5, Store::make(f, x_2 * 5 + y * 1, func(x_2, y), 1)));
+    Stmt stmt = Block::make({stmt1, stmt2});
+
+    std::ostringstream oss_ref;
+    oss_ref << stmt;
+    ASSERT_EQ(oss.str(), oss_ref.str());
+  }
 }
