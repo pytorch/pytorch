412b340861ece7adde60cb2839d58f11dff3ff57
Owen Anderson <owen.anderson@oculus.com>
2020-01-13 11:39:53 -0800

    Add support for Float immediates.

--- torch/csrc/jit/compiler/include/llvm_codegen.h
+++ torch/csrc/jit/compiler/include/llvm_codegen.h
@@ -21,7 +21,10 @@ class LLVMCodeGen : public IRVisitor {
   llvm::Function* fn_;
   llvm::BasicBlock* bb_;
   llvm::Value* value_;
+
   llvm::Type* int32Ty_;
+  llvm::Type* floatTy_;
+
   std::unordered_map<const BaseExprNode*, int> varToArg_;
   std::unordered_map<const Variable*, llvm::Value*> varToVal_;
 
@@ -45,8 +48,43 @@ class LLVMCodeGen : public IRVisitor {
   void visit(const Store* v) override;
   void visit(const Broadcast* v) override;
 
-  int value();
-  int value(std::vector<void*>& args);
+  void optimize(llvm::TargetMachine& TM, llvm::Module& M);
+
+
+  template<typename T> T value() {
+    std::vector<void*> args;
+    return value<T>(args);
+  }
+
+  template<typename T>
+  T value(std::vector<void*>& args) {
+    irb_.CreateRet(value_);
+    assert(!llvm::verifyFunction(*fn_, &llvm::outs()));
+    optimize(jit_->getTargetMachine(), *module_);
+
+  #if DEBUG_PRINT
+    llvm::errs() << *module_;
+    llvm::SmallVector<char, 0> asmBuffer;
+    llvm::raw_svector_ostream asmStream(asmBuffer);
+    llvm::legacy::PassManager PM;
+    jit_->getTargetMachine().addPassesToEmitFile(
+        PM,
+        asmStream,
+        nullptr,
+        llvm::TargetMachine::CodeGenFileType::CGFT_AssemblyFile);
+    PM.run(*module_);
+    llvm::errs() << asmStream.str();
+  #endif
+
+    auto key = jit_->addModule(std::move(module_));
+    auto sym = jit_->findSymbol("wrapper");
+    auto addr = sym.getAddress();
+    assert(addr);
+    T (*fp)(void**) = (T (*)(void**))addr.get();
+    T rv = fp(args.data());
+    jit_->removeModule(key);
+    return rv;
+  }
 };
 
 } // namespace compiler
