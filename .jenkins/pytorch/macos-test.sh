1972ebd6ed0c6301dc208f66e5b1f33532c3e4b8
Will Feng <willfeng@fb.com>
2020-01-13 14:09:36 -0800

    Revert D19373615: Fix typo in config script to re-enable libtorch build and test in macOS CI
    Test Plan: revert-hammer
    
    Differential Revision:
    D19373615
    
    Original commit changeset: 28686ef58953
    
    fbshipit-source-id: 432b04adfd9d010e1965846a386f117ebc80e013

--- .jenkins/pytorch/macos-test.sh
+++ .jenkins/pytorch/macos-test.sh
@@ -68,20 +68,22 @@ test_libtorch() {
 
     echo "Testing libtorch"
 
-    python test/cpp/jit/tests_setup.py setup
-    if [[ "$BUILD_ENVIRONMENT" == *cuda* ]]; then
-      build/bin/test_jit
-    else
-      build/bin/test_jit "[cpu]"
-    fi
-    python test/cpp/jit/tests_setup.py shutdown
+    CPP_BUILD="$PWD/../cpp-build"
+    rm -rf $CPP_BUILD
+    mkdir -p $CPP_BUILD/caffe2
+
+    BUILD_LIBTORCH_PY=$PWD/tools/build_libtorch.py
+    pushd $CPP_BUILD/caffe2
+    VERBOSE=1 DEBUG=1 python $BUILD_LIBTORCH_PY
+    popd
+
     python tools/download_mnist.py --quiet -d test/cpp/api/mnist
 
     # Unfortunately it seems like the test can't load from miniconda3
     # without these paths being set
     export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/miniconda3/lib"
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/miniconda3/lib"
-    OMP_NUM_THREADS=2 TORCH_CPP_TEST_MNIST_PATH="test/cpp/api/mnist" build/bin/test_api
+    TORCH_CPP_TEST_MNIST_PATH="test/cpp/api/mnist" "$CPP_BUILD"/caffe2/bin/test_api
 
     assert_git_not_dirty
   fi
