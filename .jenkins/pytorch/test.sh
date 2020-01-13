a3622492f77f22216eb1158fa9885aeb09a72fac
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-13 08:44:47 -0800

    CUDA 8 cleanup (#32013)
    Summary:
    CUDA 8 is no longer supported
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32013
    
    Differential Revision: D19372963
    
    Pulled By: ezyang
    
    fbshipit-source-id: e584d7d5d5908933221ea4400234b3e6e7c32e7a

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -21,7 +21,7 @@ if [ -n "${IN_CIRCLECI}" ]; then
     sudo apt-get -qq install --allow-downgrades --allow-change-held-packages libnccl-dev=2.2.13-1+cuda9.0 libnccl2=2.2.13-1+cuda9.0
   fi
 
-  if [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda8-* ]] || [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda9-cudnn7-py2* ]]; then
+  if [[ "$BUILD_ENVIRONMENT" == *-xenial-cuda9-cudnn7-py2* ]]; then
     # TODO: move this to Docker
     sudo apt-get -qq update
     sudo apt-get -qq install --allow-downgrades --allow-change-held-packages openmpi-bin libopenmpi-dev
