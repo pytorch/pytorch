f368ee4dc7efa088b31f3eb4d34b6538db7809c7
Rohan Varma <rvarm1@fb.com>
2020-01-14 11:54:22 -0800

    [do not review][test] in summary
    foo
    
    Differential Revision: [D19396925](https://our.internmc.facebook.com/intern/diff/D19396925/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D19396925/)!
    
    [ghstack-poisoned]

--- test/rpc_test.py
+++ test/rpc_test.py
@@ -1254,6 +1254,7 @@ class RpcTest(RpcAgentTestFixture):
         self.assertIn("num_owner_rrefs", info)
         self.assertIn("num_pending_users", info)
         # RRef on local value is not added to context until shared across RPC
+        # foo
         self.assertEqual(0, int(info["num_owner_rrefs"]))
         self.assertEqual(0, int(info["num_pending_users"]))
         # barrier after the check 1
