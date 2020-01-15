372b0aed780a53ff7111fc67323779c42438e8e2
peterjc123 <peterghost86@gmail.com>
2020-01-15 23:27:38 +0800

    Apply changes to caffe2

--- caffe2/python/__init__.py
+++ caffe2/python/__init__.py
@@ -15,20 +15,33 @@ caffe2_pb2.COMPILE_TIME_MAX_DEVICE_TYPES = caffe2_pb2.PROTO_COMPILE_TIME_MAX_DEV
 caffe2_pb2.ONLY_FOR_TEST = caffe2_pb2.PROTO_ONLY_FOR_TEST
 
 if platform.system() == 'Windows':
-    # first get nvToolsExt PATH
-    def get_nvToolsExt_path():
-        NVTOOLEXT_HOME = os.getenv('NVTOOLSEXT_PATH', 'C:\\Program Files\\NVIDIA Corporation\\NvToolsExt')
-
-        if os.path.exists(NVTOOLEXT_HOME):
-            return os.path.join(NVTOOLEXT_HOME, 'bin', 'x64')
-        else:
-            return ''
-
     py_dll_path = os.path.join(os.path.dirname(sys.executable), 'Library', 'bin')
     th_root = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'torch')
     th_dll_path = os.path.join(th_root, 'lib')
 
-    dll_paths = [th_dll_path, py_dll_path, get_nvToolsExt_path(), os.environ['PATH']]
+    if not os.path.exists(os.path.join(th_dll_path, 'nvToolsExt64_1.dll')):
+        nvtoolsext_dll_path = os.path.join(
+            os.getenv('NVTOOLSEXT_PATH', 'C:\\Program Files\\NVIDIA Corporation\\NvToolsExt'), 'bin', 'x64')
+    else:
+        nvtoolsext_dll_path = None
+    
+    import torch.version
+    import glob
+    if torch.version.cuda and len(glob.glob(os.path.join(th_dll_path, 'cudart64*.dll'))) == 0:
+        cuda_version = torch.version.cuda
+        cuda_version_1 = cuda_version.replace('.', '_')
+        cuda_path_var = 'CUDA_PATH_V' + cuda_version_1
+        default_path = 'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v' + cuda_version
+        cuda_path = os.path.join(os.getenv(cuda_path_var, default_path), 'bin')
+    else:
+        cuda_path = None
 
-    # then add the path to env
-    os.environ['PATH'] = ';'.join(dll_paths)
+    if sys.version_info >= (3, 8):
+        dll_paths = list(filter(None, [th_dll_path, py_dll_path, nvtoolsext_dll_path, cuda_path]))
+        
+        for dll_path in dll_paths:
+            os.add_dll_directory(dll_path)
+    else:
+        dll_paths = list(filter(None, [th_dll_path, py_dll_path, nvtoolsext_dll_path, cuda_path, os.environ['PATH']]))
+
+        os.environ['PATH'] = ';'.join(dll_paths)
