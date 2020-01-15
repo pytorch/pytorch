7a0c618dd6dd3e820fb580be4d57e4b9d560fc4e
Tao Xu <taox@fb.com>
2020-01-14 21:16:13 -0800

    [iOS][CI]Add File existence checking

--- .circleci/scripts/binary_ios_upload.sh
+++ .circleci/scripts/binary_ios_upload.sh
@@ -17,8 +17,12 @@ cd ${ZIP_DIR}/install/lib
 target_libs=(libc10.a libclog.a libcpuinfo.a libeigen_blas.a libpytorch_qnnpack.a libtorch_cpu.a libtorch.a)
 for lib in ${target_libs[*]}
 do
-    libs=(${ARTIFACTS_DIR}/x86_64/lib/${lib} ${ARTIFACTS_DIR}/arm64/lib/${lib})
-    lipo -create "${libs[@]}" -o ${ZIP_DIR}/install/lib/${lib}
+    lib_x86="${ARTIFACTS_DIR}/x86_64/lib/${lib}"
+    lib_arm64="${ARTIFACTS_DIR}/arm64/lib/${lib}"
+    if [ -f "${lib_x86}" ] && [ -f "${lib_arm64}" ]; then
+        libs=(${lib_x86} ${lib_arm64})
+        lipo -create "${libs[@]}" -o ${ZIP_DIR}/install/lib/${lib}
+    fi
 done
 # for nnpack, we only support arm64 build
 cp ${ARTIFACTS_DIR}/arm64/lib/libnnpack.a ./
