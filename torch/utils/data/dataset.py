e2b567cfb221391625cdba1cfdc41ec76f49610c
Andreas Triantafyllopoulos <atriant@audeering.com>
2020-01-15 10:11:14 +0100

    added get_idxs method to concatdataset

--- torch/utils/data/dataset.py
+++ torch/utils/data/dataset.py
@@ -194,6 +194,15 @@ class ConcatDataset(Dataset):
     def __len__(self):
         return self.cumulative_sizes[-1]
 
+    def get_idxs(self, idx):
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+
+        return sample_idx, dataset_idx
+
     def __getitem__(self, idx):
         if idx < 0:
             if -idx > len(self):
