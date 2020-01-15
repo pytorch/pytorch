ac068c1aee0fac53a25973442be890b2d70a9f18
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-15 14:30:01 -0800

    Revert "SCCACHE_RECACHE=1"
    This reverts commit 9920928450541c1be3b7a05a825edeeeffd0e2b3.

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -4,8 +4,6 @@
 # (This is set by default in the Docker images we build, so you don't
 # need to set it yourself.
 
-export SCCACHE_RECACHE=1
-
 # shellcheck disable=SC2034
 COMPACT_JOB_NAME="${BUILD_ENVIRONMENT}"
 
