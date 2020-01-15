48a1a14251738ca0e8866772a37aa5fc38ff9f82
peter <peterghost86@gmail.com>
2020-01-15 16:51:49 +0800

    Fix dll load logic for Python 3.8 on Windows

--- torch/__init__.py
+++ torch/__init__.py
@@ -36,20 +36,36 @@ __all__ = [
 ################################################################################
 
 if platform.system() == 'Windows':
-    NVTOOLSEXT_PATH = os.getenv('NVTOOLSEXT_PATH', 'C:\\Program Files\\NVIDIA Corporation\\NvToolsExt')
-
-    if os.path.exists(NVTOOLSEXT_PATH):
-        nvtoolsext_lib_path = os.path.join(NVTOOLSEXT_PATH, 'bin', 'x64')
-    else:
-        nvtoolsext_lib_path = ''
-
     py_dll_path = os.path.join(sys.exec_prefix, 'Library', 'bin')
     th_dll_path = os.path.join(os.path.dirname(__file__), 'lib')
 
-    dll_paths = [th_dll_path, py_dll_path, nvtoolsext_lib_path, os.environ['PATH']]
+    if not os.path.exists(os.path.join(th_dll_path, 'nvToolsExt64_1.dll')):
+        nvtoolsext_dll_path = os.path.join(
+            os.getenv('NVTOOLSEXT_PATH', 'C:\\Program Files\\NVIDIA Corporation\\NvToolsExt'), 'bin', 'x64')
+    else:
+        nvtoolsext_dll_path = None
+    
+    import torch.version
+    import glob
+    if len(glob.glob(os.path.join(th_dll_path, 'cudart64*.dll'))) == 0:
+        cuda_version = torch.version.cuda
+        cuda_version_1 = cuda_version.replace('.', '_')
+        cuda_path_var = 'CUDA_PATH_V' + cuda_version_1
+        default_path = 'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v' + cuda_version
+        cuda_path = os.path.join(os.getenv(cuda_path_var, default_path), 'bin')
+    else:
+        cuda_path = None
 
-    # then add the path to env
-    os.environ['PATH'] = ';'.join(dll_paths)
+    if sys.version_info >= (3, 8):
+        dll_paths = list(filter(None, [th_dll_path, py_dll_path, nvtoolsext_dll_path, cuda_path]))
+        
+        for dll_path in dll_paths:
+            os.add_dll_directory(dll_path)
+    else:
+        dll_paths = list(filter(None, [th_dll_path, py_dll_path, nvtoolsext_dll_path, cuda_path, os.environ['PATH']]))
+
+        os.environ['PATH'] = ';'.join(dll_paths)
+    
 
 
 # See Note [Global dependencies]
