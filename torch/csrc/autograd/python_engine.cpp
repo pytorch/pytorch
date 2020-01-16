65bc885994e149b4af3192793b2e26e225b6ba0b
albanD <desmaison.alban@gmail.com>
2020-01-16 09:53:46 -0800

    Update on "Raise error for code that risk deadlock"
    Fix for https://github.com/pytorch/pytorch/issues/32045
    
    
    Calling into the engine with the GIL can deadlock because:
    - worker thread initialization acquires the GIL
    - Any Node / hook can be a python function that will acquire the GIL
    
    The choice was made here to raise an error as one of the advantage of using cpp extensions with python is to be able to release the GIL. So we prefer to educate users to do it rather than doing it under the hook.
    
    Differential Revision: [D19430979](https://our.internmc.facebook.com/intern/diff/D19430979)
    
    [ghstack-poisoned]

--- torch/csrc/autograd/python_engine.cpp
+++ torch/csrc/autograd/python_engine.cpp
@@ -61,7 +61,7 @@ variable_list PythonEngine::execute(
     bool keep_graph,
     bool create_graph,
     const edge_list& outputs) {
-  TORCH_CHECK(!PyGILState_Check(), "The autograd engine is called while holding the GIL. If you are using the C++ "
+  TORCH_CHECK(!PyGILState_Check(), "The autograd engine was called while holding the GIL. If you are using the C++ "
                                    "API, the autograd engine is an expensive operation that does not require the "
                                    "GIL to be held so you should release it with 'pybind11::gil_scoped_release no_gil;'"
                                    ". If you are not using the C++ API, please report a bug to the pytorch team.")
