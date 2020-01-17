6284702909a5334d7e5ae422b90a91032ff159c3
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-01-17 13:33:47 -0800

    Update test.

--- test/test_nn.py
+++ test/test_nn.py
@@ -3484,21 +3484,33 @@ class TestNN(NNTestCase):
         test_multihead_attn_all_arguments3()  # Test MultiheadAttention with all the argument.
 
     def test_multihead_attn_3d_attn_mask(self):
-        embed_dim = 8
-        num_heads = 4
-        batch_size = 5
-        src_len = 3
+        embed_dim = 8                                                                                                         
+        num_heads = 4                                                                                                         
+        batch_size = 8          
+        src_len = 3  
         tgt_len = 2
 
-        query = torch.rand(tgt_len, batch_size, embed_dim)
-        key = torch.rand(src_len, batch_size, embed_dim)
-        value = key
-        mta_model = torch.nn.MultiheadAttention(embed_dim, num_heads)
-        attn_mask_2d = torch.randint(0, 2, (tgt_len, src_len)).float()
-        attn_mask_2d = attn_mask_2d.masked_fill(attn_mask_2d == 0, float('-inf')).masked_fill(attn_mask_2d == 1, float(0.0)) 
-        attn_mask_3d = torch.stack([attn_mask_2d] * (batch_size * num_heads), dim=0)
-        self.assertEqual(mta_model(query, key, value, attn_mask=attn_mask_2d)[0],
-                         mta_model(query, key, value, attn_mask=attn_mask_3d)[0])
+        query = torch.rand(batch_size, tgt_len, embed_dim)  # [N, T, D]
+        key = torch.rand(batch_size, src_len, embed_dim)  # [N, S, D]                                                                      
+        value = key  # [N, S, D]           
+        attn_mask = torch.randint(0, 2, (batch_size, tgt_len, src_len)).float()  # [N, T, S]
+        attn_mask = attn_mask.masked_fill(attn_mask == 0, float('-inf')).masked_fill(attn_mask == 1, float(0.0))  
+
+        mta_model = torch.nn.MultiheadAttention(embed_dim, num_heads)    
+
+        # Generate 3D results
+        attn_mask_3d = torch.repeat_interleave(attn_mask, num_heads, dim=0)  # [N * H, T, S]    
+        output_3d = mta_model(query.transpose(0, 1), key.transpose(0, 1), value.transpose(0, 1), attn_mask=attn_mask_3d)[0]
+        output_3d = output_3d.transpose(0, 1)  # [N, T, D]
+
+        for i in range(0, batch_size):
+            output_2d = mta_model(query[i].unsqueeze(0).transpose(0, 1),
+                                  key[i].unsqueeze(0).transpose(0, 1),
+                                  value[i].unsqueeze(0).transpose(0, 1),
+                                  attn_mask=attn_mask[i])[0]
+
+            # output_2d in shape of [T, 1, D]
+            self.assertEqual(output_3d[i].unsqueeze(0).transpose(0, 1), output_2d)
 
     def test_normalize(self):
         inputs = torch.randn(1, 3, 4, 4, requires_grad=True)
