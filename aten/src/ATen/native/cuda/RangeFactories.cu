afad60b6416fe72c28b6f79f715e1e5bc5a0ef70
Enealor <53704971+Enealor@users.noreply.github.com>
2020-01-17 13:58:27 -0800

    Reverts cuda float linspace

--- aten/src/ATen/native/cuda/RangeFactories.cu
+++ aten/src/ATen/native/cuda/RangeFactories.cu
@@ -47,7 +47,7 @@ Tensor& linspace_cuda_out(Tensor& result, Scalar start, Scalar end, int64_t step
     AT_DISPATCH_FLOATING_TYPES_AND_HALF(r.scalar_type(), "linspace_cuda", [&]() {
       scalar_t scalar_start = start.to<scalar_t>();
       scalar_t scalar_end = end.to<scalar_t>();
-      scalar_t step = (scalar_end - scalar_start) / static_cast<scalar_t>(steps - 1);
+      float step = static_cast<float>(scalar_end - scalar_start) / (steps - 1);
 
       auto iter = TensorIterator::nullary_op(r);
       gpu_kernel_with_index(iter, [scalar_start, step]GPU_LAMBDA(int ind) -> scalar_t {
@@ -56,10 +56,10 @@ Tensor& linspace_cuda_out(Tensor& result, Scalar start, Scalar end, int64_t step
       });
     });
   } else {
-    AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Half, r.scalar_type(), "linspace_cuda", [&]() {
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(r.scalar_type(), "linspace_cuda", [&]() {
       scalar_t scalar_start = start.to<scalar_t>();
       scalar_t scalar_end = end.to<scalar_t>();
-      double step = static_cast<double>(scalar_end - scalar_start) / (steps - 1);
+      scalar_t step = (scalar_end - scalar_start) / static_cast<scalar_t>(steps - 1);
 
       auto iter = TensorIterator::nullary_op(r);
       gpu_kernel_with_index(iter, [scalar_start, step]GPU_LAMBDA(int ind) -> scalar_t {
