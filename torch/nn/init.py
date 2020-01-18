Corrects trunc_norm computation

torch/nn/init.py
@@ -25,24 +25,25 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
         return (1+math.erf(x/math.sqrt(2.)))/2.
     
     with torch.no_grad():
-        #Compute upper and lower cdf values
+        # Get upper and lower cdf values
         l = norm_cdf((a-mean)/std)
         u = norm_cdf((b-mean)/std)
-        sqrt_two = math.sqrt(2.)
         
-        #First, fill with uniform values
-        tensor.uniform_(0., 1.)
+        # Fill tensor with uniform values from [l, u]
+        tensor.uniform_(l, u)
 
-        #Scale by 2(u-l), shift by 2l-1
-        tensor.mul_(2*(u-l))
-        tensor.add_(2*l-1)
-        
-        #Ensure that the values are strictly between -1 and 1
+        # Use inverse cdf transform from normal distribution
+        tensor.mul_(2)
+        tensor.sub_(1)
+
+        #Ensure that the values are strictly between -1 and 1 for erfinv
         eps = torch.finfo(tensor.dtype).eps
         tensor.clamp_(min=-(1.-eps), max=(1.-eps))
-
-        #Now use the inverse erf to get distributed values
         tensor.erfinv_()
+        
+        # Transform to proper mean, std
+        tensor.mul_(std*math.sqrt(2.))
+        tensor.add_(mean)
 
         #Clamp one last time to ensure it's still in the proper range
         tensor.clamp_(min=a, max=b)
@@ -135,11 +136,12 @@ def normal_(tensor, mean=0., std=1.):
 
 def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
     # type: (Tensor, float, float, float, float) -> Tensor
-    r"""Fills the input `Tensor` with values drawn from a truncated
+    r"""Fills the input Tensor with values drawn from a truncated
     normal distribution. The values are effectively drawn from the
     normal distribution :math:`\mathcal{N}(\text{mean}, \text{std}^2)`
     with values outside :math:`[a, b]` redrawn until they are within
-    the bounds.
+    the bounds. The method used works best if :math:`\text{mean}` is
+    near the center of the interval.
 
     Args:
         tensor: an n-dimensional `torch.Tensor`