d31c39b3476396edc942bbd0b945819aff4c8066
Mike Ruberry <mruberry@devfair044.maas>
2020-01-22 15:26:05 -0800

    randn -> ones

--- test/test_torch.py
+++ test/test_torch.py
@@ -14632,135 +14632,135 @@ class TestDevicePrecision(TestCase):
 class TestViewOps(TestCase):
 
     def test_diagonal_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = torch.diagonal(t)
         v[0] = 0
         self.assertEqual(t[0, 0], v[0])
 
-        t = torch.randn((3, 3, 3))
+        t = torch.ones((3, 3, 3))
         v = torch.diagonal(t, offset=1, dim1=1, dim2=2)
         v[0, 0] = 0
         self.assertEqual(t[0, 0, 1], v[0, 0])
 
     def test_select_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = t.select(0, 2)
         v[0] = 0
         self.assertEqual(t[2, 0], v[0])
 
     def test_unbind_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = torch.unbind(t)
         v[0][0] = 0
         self.assertEqual(t[0, 0], v[0][0])
 
     def test_expand_view(self, device):
-        t = torch.randn((5, 1))
+        t = torch.ones((5, 1))
         v = t.expand(5, 5)
         v[2, 2] = 0
         self.assertEqual(t[2, 0], v[2, 2])
 
     def test_expand_as_view(self, device):
-        t = torch.randn((5, 1))
+        t = torch.ones((5, 1))
         e = torch.empty((5, 5))
         v = t.expand_as(e)
         v[2, 2] = 0
         self.assertEqual(t[2, 0], v[2, 2])
 
     def test_narrow_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = torch.narrow(t, 1, 2, 2)
         v[0, 0] = 0
         self.assertEqual(t[0, 2], v[0, 0])
 
     def test_permute_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = t.permute(1, 0)
         v[0, 1] = 0
         self.assertEqual(t[1, 0], v[0, 1])
 
     def test_transpose_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = torch.transpose(t, 0, 1)
         v[0, 1] = 0
         self.assertEqual(t[1, 0], v[0, 1])
 
     def test_t_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = t.t()
         v[0, 1] = 0
         self.assertEqual(t[1, 0], v[0, 1])
 
     def test_T_view(self, device):
-        t = torch.randn((5, 5))
+        t = torch.ones((5, 5))
         v = t.T
         v[0, 1] = 0
         self.assertEqual(t[1, 0], v[0, 1])
 
     def test_unfold_view(self, device):
-        t = torch.randn(10)
+        t = torch.ones(10)
         v = t.unfold(0, 3, 2)
         v[1, 0] = 0
         self.assertEqual(t[2], v[1, 0])
 
     def test_squeeze_view(self, device):
-        t = torch.randn(5, 1, 5)
+        t = torch.ones(5, 1, 5)
         v = torch.squeeze(t)
         v[0, 1] = 0
         self.assertEqual(t[0, 0, 1], v[0, 1])
 
     def test_unsqueeze_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = torch.unsqueeze(t, 1)
         v[0, 0, 1] = 0
         self.assertEqual(t[0, 1], v[0, 0, 1])
 
     def test_as_strided_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = torch.as_strided(t, (25,), (1,))
         v[6] = 0
         self.assertEqual(t[1, 1], v[6])
 
     def test_view_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = t.view(25)
         v[6] = 0
         self.assertEqual(t[1, 1], v[6])
 
     def test_view_as_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         e = torch.empty((25,))
         v = t.view_as(e)
         v[6] = 0
         self.assertEqual(t[1, 1], v[6])
 
     def test_contiguous_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = t.contiguous()
         v[0, 0] = 0
         self.assertEqual(t[0, 0], v[0, 0])
 
     def test_contiguous_nonview(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = t.t().contiguous()
         v[0, 0] = 0
         self.assertNotEqual(t[0, 0], v[0, 0])
 
     def test_reshape_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = torch.reshape(t, (25,))
         v[6] = 0
         self.assertEqual(t[1, 1], v[6])
 
     def test_reshape_as_view(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         e = torch.empty((25,))
         v = t.reshape_as(e)
         v[6] = 0
         self.assertEqual(t[1, 1], v[6])
 
     def test_reshape_nonview(self, device):
-        t = torch.randn(5, 5)
+        t = torch.ones(5, 5)
         v = torch.reshape(t.t(), (25,))
         v[6] = 0
         self.assertNotEqual(t[1, 1], v[6])
