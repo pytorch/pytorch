4a4c98dae13443ccf83bb42ff2154d1ebe889928
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-22 01:08:31 -0800

    fix

--- aten/src/ATen/native/cuda/FillKernel.cu
+++ aten/src/ATen/native/cuda/FillKernel.cu
@@ -3,13 +3,15 @@
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/Fill.h>
+#include <ATen/native/cuda/zmath.cuh>
 
 namespace at { namespace native {
 
 void fill_kernel_cuda(TensorIterator& iter, Scalar value) {
   AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(at::ScalarType::Bool, at::ScalarType::Half, at::ScalarType::BFloat16, iter.dtype(), "fill_cuda", [&]() {
-    auto value_converted = value.to<scalar_t>();
-    gpu_kernel(iter, [value_converted]GPU_LAMBDA() -> scalar_t {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    auto value_converted = thrust_t(value.to<scalar_t>());
+    gpu_kernel(iter, [value_converted]GPU_LAMBDA() -> thrust_t {
       return value_converted;
     });
   });
