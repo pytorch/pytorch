c62c8445fc649d1748a1fd49a42711dcfa14132a
James Reed <jamesreed@fb.com>
2020-01-22 15:57:05 -0800

    Update on "[JIT] Add torch.classes.load_library"
    Differential Revision: [D19525175](https://our.internmc.facebook.com/intern/diff/D19525175)
    
    [ghstack-poisoned]

--- torch/_classes.py
+++ torch/_classes.py
@@ -1,8 +1,5 @@
-import ctypes
 import types
 import torch._C
-
-from torch._ops import dl_open_guard
 
 class _Classes(types.ModuleType):
     def __init__(self):
@@ -16,30 +13,7 @@ class _Classes(types.ModuleType):
         return proxy
 
     def load_library(self, path):
-        """
-        Loads a shared library from the given path into the current process.
-
-        The library being loaded may run global initialization code to register
-        custom operators with the PyTorch JIT runtime. This allows dynamically
-        loading custom operators. For this, you should compile your operator
-        and the static registration code into a shared library object, and then
-        call ``torch.ops.load_library('path/to/libcustom.so')`` to load the
-        shared object.
-
-        After the library is loaded, it is added to the
-        ``torch.ops.loaded_libraries`` attribute, a set that may be inspected
-        for the paths of all libraries loaded using this function.
-
-        Arguments:
-            path (str): A path to a shared library to load.
-        """
-        path = torch._utils_internal.resolve_library_path(path)
-        with dl_open_guard():
-            # Import the shared library into the process, thus running its
-            # static (global) initialization code in order to register custom
-            # operators with the JIT.
-            ctypes.CDLL(path)
-        self.loaded_libraries.add(path)
+        torch.ops.load_library(path)
 
 # The classes "namespace"
 classes = _Classes()
