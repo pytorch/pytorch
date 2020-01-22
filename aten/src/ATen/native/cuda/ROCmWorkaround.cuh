53bd583190ba2de4a3bd8f2ca6d8e41a6a947cef
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-22 12:54:07 -0800

    try

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -1,17 +1,15 @@
 #pragma once
 
-#include <ATen/native/cuda/MemoryAccess.cuh>
-
 namespace rocm { namespace workaround {
 
 template<typename T>
 struct enable_default_constructor {
   struct { char bytes[sizeof(T)]; } value;
-  T operator T() {
-    return typeless_cast<T>(value);
+  T &operator T() {
+    return *reinterpret_cast<T *>(&value);
   }
   enable_default_constructor &operator=(T x) {
-    value = typeless_cast<decltype(value)>(x);
+    value = *reinterpret_cast<decltype(&value)>(&x);
     return *this;
   }
 };
