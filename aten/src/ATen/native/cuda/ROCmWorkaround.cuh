b141ee2d2536459c1695ef5050faadce52a72686
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-01-22 14:51:07 -0800

    debug ROCm for #32383 (#4)
    * test
    
    * clear ci
    
    * no gh
    
    * fix
    
    * test
    
    * test
    
    * fix
    
    * test
    
    * try
    
    * fix
    
    * test
    
    * test
    
    * revert unneeded changes
    
    * revert ci changes

---
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace rocm { namespace workaround {
+
+template<typename T>
+struct enable_default_constructor {
+  struct { char bytes[sizeof(T)]; } value;
+  operator T&() {
+    return *reinterpret_cast<T *>(&value);
+  }
+  enable_default_constructor &operator=(T x) {
+    value = *reinterpret_cast<decltype(&value)>(&x);
+    return *this;
+  }
+};
+
+}}  // namespace rocm::workaround
