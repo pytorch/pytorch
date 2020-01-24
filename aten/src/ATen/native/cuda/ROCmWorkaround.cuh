10f5ef435feeb8de5747741ca5b1e313b571df24
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-24 15:26:03 -0800

    improve

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -7,10 +7,10 @@ struct enable_default_constructor {
   T value;
   enable_default_constructor() : T(0) {}
   operator T&() {
-    return *reinterpret_cast<T *>(&value);
+    return *value;
   }
   enable_default_constructor &operator=(T x) {
-    value = *reinterpret_cast<decltype(&value)>(&x);
+    value = x;
     return *this;
   }
 };
