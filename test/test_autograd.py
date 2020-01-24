79c7b1548d61e1db73b20fb0a072d678e30c9295
albanD <desmaison.alban@gmail.com>
2020-01-24 14:29:11 -0800

    Make autogen functions correct for multiple outputs and views (#31990)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31990
    
    This PR does three things:
    - Add a new `allow_rebase_history` flag to the differentiable views. If set, trying to rebase their history will raise an error.
    - Make sure that the codegen functions verify this flag before doing inplace operations so that they fail before doing the inplace modification.
    - Make sure the codegen functions set this flag properly when we don't support rebasing the history of the output.
    
    The codegen change can be found [here](https://github.com/pytorch/pytorch/commit/4bf180caa0e655111b036c8b0419da89fc75671b).
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19409649
    
    Pulled By: albanD
    
    fbshipit-source-id: a2b41c2d231e952ecfe162bdb6bad620ac595703

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -3472,7 +3472,7 @@ for shape in [(1,), ()]:
         run_test(grad_mode=True, requires_grad=True, is_view=True,
                  should_raise_tuple=(None, None, None))
         # TODO: views require gradients when created in no_grad mode but their grad_fn is not populated
-        leaf_grad_err = "a leaf Variable that requires grad has been used in an in-place operation."
+        leaf_grad_err = "a leaf Variable that requires grad is being used in an in-place operation."
         run_test(grad_mode=False, requires_grad=True, is_view=True,
                  should_raise_tuple=(leaf_grad_err, leaf_grad_err, leaf_grad_err))
         run_test(grad_mode=False, requires_grad=False, is_view=True,
@@ -3797,6 +3797,32 @@ for shape in [(1,), ()]:
         c = torch.sum(s**b)
         c.backward()
         self.assertEqual(b.grad, torch.tensor([-inf, 0., 0.]), allow_inf=True)
+
+    def test_multi_view_methods(self):
+        # This list should math the PURE_VIEW_FUNCTIONS in `tools/autograd/gen_autograd.py
+        # It maps a function its arguments for an input of size [3,]
+        fn_to_test = {
+            'split': (2,),
+            'split_with_sizes': ((2, 1),),
+            'unbind': (0,)
+        }
+
+        for fn, arg in fn_to_test.items():
+            inp = torch.rand(3, dtype=torch.double, requires_grad=True)
+
+            def foo(inp, inplace=False):
+                x = inp * 2
+                x = getattr(x, fn)(*arg)
+                res = 0.
+                for i, el in enumerate(x):
+                    if inplace:
+                        el *= 42
+                    res += (i + 1) * el.sum()
+                return res
+            self.assertTrue(gradcheck(foo, (inp,)))
+            self.assertTrue(gradgradcheck(foo, (inp,)))
+            self.assertTrue(gradcheck(foo, (inp, True)))
+            self.assertTrue(gradgradcheck(foo, (inp, True)))
 
 
 def index_variable(shape, max_indices):
