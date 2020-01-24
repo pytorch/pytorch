37607ad2fa7f09cd9b7b80ec88ef660a82fcdb29
Jerry Zhang <jerryzh@fb.com>
2020-01-24 13:02:13 -0800

    [quant] Re-enable fold_convbn in quantize_script (#32302)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32302
    
    att
    
    Test Plan:
    .
    
    Imported from OSS
    
    Differential Revision: D19508035
    
    fbshipit-source-id: 2ac26585396ec8a115acd0e1d7ccb84098a76824

--- torch/quantization/_quantize_script.py
+++ torch/quantization/_quantize_script.py
@@ -99,11 +99,7 @@ def quantize_script(model, qconfig_dict, run_fn, run_args, inplace=False):
     if not inplace:
         model = model.copy()
     scripted_qconfig_dict = {k: script_qconfig(v) for k, v in qconfig_dict.items()}
-    # We are not going to run fold_convbn pass right now
-    # since it is not able to work correctly, we will
-    # revisit after constants is properly handled in
-    # JIT
-    # torch._C._jit_pass_fold_convbn(model._c)
+    torch._C._jit_pass_fold_convbn(model._c)
     model = prepare_script(model, scripted_qconfig_dict, True)
     run_fn(model._c._get_method('forward'), *run_args)
     model = convert_script(model, True)
