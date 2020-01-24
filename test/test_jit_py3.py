29287c2393652e70640a56d441a2e227f0912096
Michael Suo <suo@fb.com>
2020-01-23 23:29:47 -0800

    Update on "[jit] Fix dict type serialization"
    If the dict's contained types cannot be inferred from its contents (for
    example, `Dict[str, Tensor]` vs. `Dict[str, Optional[Tensor]]`), we must
    explicitly annotate the type.
    
    Also this removes some special handling that omits annotations on empty
    containers that have the default type. It makes the code more complex
    for not too much value, and was wrong for dicts anyway.
    
    Differential Revision: [D19551016](https://our.internmc.facebook.com/intern/diff/D19551016)
    
    [ghstack-poisoned]

--- test/test_jit_py3.py
+++ test/test_jit_py3.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 class TestScriptPy3(JitTestCase):
     def test_optional_dict_construct(self):
         class M(torch.nn.Module):
-            def use(self, buffer: Dict[str, Optional[torch.Tensor]]):
+            def foo(self, buffer: Dict[str, Optional[torch.Tensor]]):
                 return buffer["prev_key"]
 
             def forward(self, x):
@@ -23,7 +23,7 @@ class TestScriptPy3(JitTestCase):
                     "next_key": next_key,
                 }
 
-                return self.use(saved_state)
+                return self.foo(saved_state)
 
         self.checkModule(M(), (torch.rand(2, 2),))
 
