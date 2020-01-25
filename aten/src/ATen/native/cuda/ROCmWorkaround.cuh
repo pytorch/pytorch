48c6580eec54b09dd0102a84334790e5f5b49645
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-24 16:13:24 -0800

    __device__

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -5,10 +5,10 @@ namespace rocm { namespace workaround {
 template<typename T>
 struct enable_default_constructor {
   struct { char bytes[sizeof(T)]; } value;
-  operator T&() {
+  __device__ operator T&() {
     return *reinterpret_cast<T *>(&value);
   }
-  enable_default_constructor &operator=(T x) {
+  __device__ enable_default_constructor &operator=(T x) {
     value = *reinterpret_cast<decltype(&value)>(&x);
     return *this;
   }
