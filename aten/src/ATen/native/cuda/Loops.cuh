c114f5e171eee6cdf3451aaffa551adf9fb9bbf1
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2020-01-24 20:49:16 -0600

    Adjust elementwise_kernel settings on ROCm

--- aten/src/ATen/native/cuda/Loops.cuh
+++ aten/src/ATen/native/cuda/Loops.cuh
@@ -56,8 +56,8 @@
 
 #ifdef __HIP_PLATFORM_HCC__
 static constexpr int launch_size_1d = 1024;
-static constexpr int launch_size_nd = 1024;
-static constexpr int launch_bound2 = 1;
+static constexpr int launch_size_nd = 128;
+static constexpr int launch_bound2 = 4;
 #else
 static constexpr int launch_size_1d = 512;
 static constexpr int launch_size_nd = 128;
