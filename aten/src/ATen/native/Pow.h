4c11392a5eeacdfc949e98cb6eb112033639ebed
Brian Vaughan <bvaughan@fb.com>
2020-01-27 11:19:17 -0800

    Update on "Fix issues with `pow` type promotion"
    Summary:
    
    Fixes: https://github.com/pytorch/pytorch/issues/32436
    
    The issue caused incorrect handling of dtypes for scalar ** tensor.
    e.g. before this change:
    ```
    >>> 5.5 ** torch.ones(5, dtype=torch.int32)
    tensor([5, 5, 5, 5, 5], dtype=torch.int32)
    ```
    should return a float tensor.
    
    Also fixes a number of incorrect cases:
         * tensors to negative powers were giving incorrect results (1 instead
            of 0 or error)
         * Behavior wasn't consistent between cuda/cpu
         * large_value ** 1 in some cases gave a result not equal
            to large_value because of truncation in conversion to double and back.
    
    BC-breaking:
    
    Previously incorrect behavior (in 1.4):
    ```
    >>> a
    tensor([1, 1, 1, 1, 1], dtype=torch.int32)
    >>> a.pow_(.5)
    tensor([1, 1, 1, 1, 1], dtype=torch.int32)
    ```
    
    After this change:
    `RuntimeError: result type Float can't be cast to the desired output type Int`
    
    [ghstack-poisoned]

--- aten/src/ATen/native/Pow.h
+++ aten/src/ATen/native/Pow.h
@@ -9,22 +9,15 @@ struct TensorIterator;
 
 namespace native {
 
-template <typename T>
-static inline
 #ifdef __CUDACC__
- __host__ __device__
+#define HOST_DEVICE __host__ __device__
+#else
+#define HOST_DEVICE
 #endif
-T powi(T a, T b) {
-  if ( std::numeric_limits<T>::is_signed && b < 0 ) {
-      if ( a == 1 ) {
-          return 1;
-      } else if ( a == -1 ) {
-          auto negative = std::abs(b) % 2;
-          return negative ? -1 : 1;
-      } else {
-          return 0;
-      }
-  }
+
+template <class T,
+  typename std::enable_if<std::is_integral<T>::value, T>::type* = nullptr>
+static inline HOST_DEVICE T powi_impl(T a, T b) {
   T result = 1;
   while (b) {
     if (b & 1) {
@@ -34,6 +27,28 @@ T powi(T a, T b) {
     a *= a;
   }
   return result;
+}
+
+template <class T,
+  typename std::enable_if<std::is_integral<T>::value && !std::is_signed<T>::value, T>::type* = nullptr>
+static inline HOST_DEVICE T powi(T a, T b) {
+  return powi_impl(a, b);
+}
+
+template <class T,
+  typename std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value, T>::type* = nullptr>
+static inline HOST_DEVICE T powi(T a, T b) {
+  if ( b < 0 ) {
+      if ( a == 1 ) {
+          return 1;
+      } else if ( a == -1 ) {
+          auto negative = (-b) % static_cast<T>(2);
+          return negative ? -1 : 1;
+      } else {
+          return 0;
+      }
+  }
+  return powi_impl(a, b);
 }
 
 using pow_tensor_tensor_fn = void (*)(TensorIterator&);
