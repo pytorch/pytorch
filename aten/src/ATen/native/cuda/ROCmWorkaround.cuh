7d53aea395290117c209d77cae788d691474f96b
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-27 10:43:05 -0800

    review

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <type_traits>
+
 namespace rocm { namespace workaround {
 
 template<typename T>
 struct enable_default_constructor {
+  static_assert(std::is_trivially_copyable<T>{} && std::is_trivially_destructible<T>{}, "Not trivial type");
   struct { char bytes[sizeof(T)]; } value;
   __device__ operator T&() {
-    return *reinterpret_cast<T *>(&value);
+    return *std::launder(reinterpret_cast<T *>(&value));
   }
   __device__ enable_default_constructor &operator=(T x) {
     value = *reinterpret_cast<decltype(&value)>(&x);
