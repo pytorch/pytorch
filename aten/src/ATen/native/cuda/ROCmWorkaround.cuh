e08e4648e0b9b247d90e2c17f8c1ff4d0a688e8b
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-27 11:06:25 -0800

    revert launder

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -10,7 +10,7 @@ struct enable_default_constructor {
   static_assert(std::is_trivially_copyable<T>{} && std::is_trivially_destructible<T>{}, "Not trivial type");
   struct { char bytes[sizeof(T)]; } value;
   __device__ operator T&() {
-    return *std::launder(reinterpret_cast<T *>(&value));
+    return *reinterpret_cast<T *>(&value);
   }
   __device__ enable_default_constructor &operator=(T x) {
     value = *reinterpret_cast<decltype(&value)>(&x);
