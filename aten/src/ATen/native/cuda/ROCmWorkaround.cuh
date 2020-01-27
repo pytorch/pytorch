67b69fde27b74fbf7e505405465ac9b5d7a90b3b
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-01-27 11:39:04 -0800

    revert static assert

--- aten/src/ATen/native/cuda/ROCmWorkaround.cuh
+++ aten/src/ATen/native/cuda/ROCmWorkaround.cuh
@@ -1,12 +1,9 @@
 #pragma once
 
-#include <type_traits>
-
 namespace rocm { namespace workaround {
 
 template<typename T>
 struct enable_default_constructor {
-  static_assert(std::is_trivially_copyable<T>{} && std::is_trivially_destructible<T>{}, "Not trivial type");
   struct { char bytes[sizeof(T)]; } value;
   __device__ operator T&() {
     return *reinterpret_cast<T *>(&value);
