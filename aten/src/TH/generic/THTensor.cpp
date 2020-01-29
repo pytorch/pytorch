fa3b4a3c5b07521e8a1953e3948a4774c48882eb
Edward Yang <ezyang@fb.com>
2020-01-29 13:23:13 -0800

    Expunge last two sites of resize_dim (#32112)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32112
    
    It turns out we already removed these from the CPU version; copy
    the changes over.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19579874
    
    Pulled By: ezyang
    
    fbshipit-source-id: e40efbf94e128fd81421b227b76dd9c9c0256d96

--- aten/src/TH/generic/THTensor.cpp
+++ aten/src/TH/generic/THTensor.cpp
@@ -321,8 +321,8 @@ void THTensor_(select)(THTensor *self, THTensor *src, int dimension, int64_t sli
   THTensor_(set)(self, src);
   THTensor_(narrow)(self, NULL, dimension, sliceIndex, 1);
 
-  std::vector<int64_t> newSize(self->dim()-1);
-  std::vector<int64_t> newStride(self->dim()-1);
+  at::DimVector newSize(static_cast<size_t>(self->dim()-1));
+  at::DimVector newStride(static_cast<size_t>(self->dim()-1));
   for (d = 0; d < dimension; d++)
   {
     newSize[d] = self->size(d);
@@ -373,8 +373,8 @@ void THTensor_(squeeze1d)(THTensor *self, THTensor *src, int dimension)
 
   if(src->size(dimension) == 1)
   {
-    std::vector<int64_t> newSize(self->dim() - 1);
-    std::vector<int64_t> newStride(self->dim() - 1);
+    at::DimVector newSize(static_cast<size_t>(self->dim() - 1));
+    at::DimVector newStride(static_cast<size_t>(self->dim() - 1));
     for (d = 0; d < dimension; d++)
     {
       newSize[d] = self->size(d);
@@ -401,8 +401,8 @@ void THTensor_(unsqueeze1d)(THTensor *self, THTensor *src, int dimension)
 
   THTensor_(set)(self, src);
 
-  std::vector<int64_t> newSize(/* size */ self->dim()+1);
-  std::vector<int64_t> newStride(/* size */ self->dim()+1);
+  at::DimVector newSize(static_cast<size_t>(/* size */ self->dim()+1));
+  at::DimVector newStride(static_cast<size_t>(/* size */ self->dim()+1));
 
   for(d = self->dim(); d > dimension; d--)
   {
@@ -665,7 +665,7 @@ void THTensor_(catArray)(THTensor *result, THTensor **inputs, int numInputs, int
   }
 
   // Compute the size of the result
-  std::vector<int64_t> size(nDims);
+  at::DimVector size(static_cast<size_t>(nDims));
   for (int dim = 0; dim < nDims; dim++) {
     int64_t result_dim_size = notSkippedTensor->size(dim);
     if (dim == dimension) {
