b9202de0ba4085956ae8e2a8630470aae6cdc8ae
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos053.prn3.facebook.com>
2020-01-29 04:07:32 -0800

    2020-01-29 postnightly release (5e2311033ebc4bb6009f70db68e4d63ba73a1633)

--- tools/autograd/gen_autograd.py
+++ tools/autograd/gen_autograd.py
@@ -39,8 +39,7 @@ VIEW_FUNCTIONS = {
     'as_strided': 'self',
     'diagonal': 'self',
     'expand': 'self',
-    'split': 'self',
-    'split_with_sizes': 'self',
+    'narrow': 'self',
     'permute': 'self',
     'select': 'self',
     'slice': 'self',
@@ -66,7 +65,7 @@ VIEW_FUNCTIONS = {
 # this list contains both the root view functions and any that are purely composed
 # of viewing functions, and is used by the JIT to determine when an operator
 # returns a view of its inputs
-RETURNS_VIEWS_OF_INPUT = set(VIEW_FUNCTIONS.keys()).union({'chunk', 'narrow'})
+RETURNS_VIEWS_OF_INPUT = set(VIEW_FUNCTIONS.keys()).union({'chunk', 'split'})
 
 
 def format_return_type(returns):
