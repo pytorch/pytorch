05099eae258412296c3eafb809a7a606c4e01a85
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-29 15:01:08 -0800

    debug

--- aten/src/ATen/native/cuda/RangeFactories.cu
+++ aten/src/ATen/native/cuda/RangeFactories.cu
@@ -96,7 +96,7 @@ Tensor& logspace_cuda_out(Tensor& result, Scalar start, Scalar end, int64_t step
       auto iter = TensorIterator::nullary_op(r);
       gpu_kernel_with_index(iter, [scalar_start, step, scalar_base]GPU_LAMBDA(int ind) -> scalar_t {
           scalar_t inc = step * ind;
-          scalar_t val = std::pow(scalar_base, scalar_start + inc);
+          scalar_t val = ::pow(scalar_base, scalar_start + inc);
           return val;
         });
     });
