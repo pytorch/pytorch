dbcfed05b38aee575513cf78db4aa81ce1ba9229
James Reed <jamesreed@fb.com>
2020-01-29 14:47:56 -0800

    [quantization] Remove incorrect fp16 dynamic linear/relu op
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/32774
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19624471
    
    Pulled By: jamesr66a
    
    fbshipit-source-id: eb6cb11fabf2ddd5edf345aff35b86b83c3af94c

--- aten/src/ATen/native/quantized/cpu/qlinear_dynamic.cpp
+++ aten/src/ATen/native/quantized/cpu/qlinear_dynamic.cpp
@@ -405,10 +405,7 @@ static auto registry =
                 .kernel<QLinearDynamicInt8<true>>(DispatchKey::CPUTensorId))
         .op("quantized::linear_dynamic_fp16(Tensor X, Tensor W_prepack) -> Tensor Y",
             torch::RegisterOperators::options()
-                .kernel<QLinearDynamicFp16<true>>(DispatchKey::CPUTensorId))
-        .op("quantized::linear_relu_dynamic_fp16(Tensor X, Tensor W_prepack) -> Tensor Y",
-            torch::RegisterOperators::options()
-                .kernel<QLinearDynamicFp16<true>>(DispatchKey::CPUTensorId));
+                .kernel<QLinearDynamicFp16<false>>(DispatchKey::CPUTensorId));
 
 } // namespace
 } // namespace native
