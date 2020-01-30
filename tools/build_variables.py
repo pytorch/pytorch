d62ac9282161d307b1ede10f4b708667241181ab
Shihao Xu <shihaoxu@fb.com>
2020-01-30 01:14:18 -0800

    Reduce RPC branches for Python/BuiltinOp/TorchScript (#32689)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32689
    
    As described in https://github.com/pytorch/pytorch/issues/32565
    ghstack-source-id: 97440343
    
    Test Plan:
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork -- test_script_functions_not_supported
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork
    
    buck-out/gen/caffe2/test/distributed/rpc/rpc_fork\#binary.par -r test_script_functions_not_supported
    ```
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_fork
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_fork
    
    buck-out/gen/caffe2/test/distributed/rpc/dist_autograd_fork\#binary.par -r test_backward_simple_script_call
    ```
    
    Differential Revision: D5721814
    
    fbshipit-source-id: 9079e81764be1e7c7b85dd72a18c76f3ecfd2547

--- tools/build_variables.py
+++ tools/build_variables.py
@@ -309,7 +309,7 @@ def add_torch_libs():
         "torch/csrc/distributed/rpc/request_callback_impl.cpp",
         "torch/csrc/distributed/rpc/rref_context.cpp",
         "torch/csrc/distributed/rpc/rref_impl.cpp",
-        "torch/csrc/distributed/rpc/script_functions.cpp",
+        "torch/csrc/distributed/rpc/torchscript_functions.cpp",
         "torch/csrc/jit/init.cpp",
         "torch/csrc/jit/passes/inline_fork_wait.cpp",
         "torch/csrc/jit/passes/onnx.cpp",
