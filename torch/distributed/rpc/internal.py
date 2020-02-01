eac86150847cb2b3cd97cc195cc3e887a187b8a8
Rohan Varma <rvarm1@fb.com>
2020-01-31 17:53:24 -0800

    [rpc] don't crash callee when function does not exist on it, instead return Exception (#32726)
    Summary:
    Closes https://github.com/pytorch/pytorch/issues/27368.
    Previously, if a function `'func` did not exist on worker A but existed in B, and the user ran `rpc.rpc_sync(A,  func)`, A would crash with a segmentation fault since it is not able to find the function. B would eventually timeout since RPCs by default time out in 60s.
    
    At the root this comes from an unhandled exception when trying to deserialize the `PythonUDF` to run.
    
    This PR makes it so that we can recover from this error, and A reports back a `RemoteException` to B indicating that the function was not found. Now, A will no longer crash and B can handle the exception appropriately and with more information.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32726
    
    Differential Revision: D19648825
    
    Pulled By: rohan-varma
    
    fbshipit-source-id: 53847f4bfb68187db41c61d69ddac13613e814b4

--- torch/distributed/rpc/internal.py
+++ torch/distributed/rpc/internal.py
@@ -90,7 +90,15 @@ class _InternalRPCPickler:
             old_recv_tables = None
         _thread_local_tensor_tables.recv_tables = tensor_table
 
-        ret = pickle.loads(binary_data)
+        try:
+            ret = pickle.loads(binary_data)
+        except AttributeError as e:
+            # Occurs when function is not found on module/class during
+            # unpickling.
+            except_str = str(e) + """ Default RPC pickler does not serialize
+            function code. Ensure that UDFs are defined on both caller and
+            callee modules."""
+            raise AttributeError(except_str)
 
         # restore _thread_local_tensor_tables.recv_tables if return
         # from nested call, otherwise clean up the table
@@ -118,8 +126,8 @@ def _run_function(binary_data, tensor_table):
     Runs a Python UDF and returns its return value.
     Wraps any exception in ``RemoteException`` if the function raises.
     """
-    python_udf = _internal_rpc_pickler.deserialize(binary_data, tensor_table)
     try:
+        python_udf = _internal_rpc_pickler.deserialize(binary_data, tensor_table)
         result = python_udf.func(*python_udf.args, **python_udf.kwargs)
     except Exception as e:
         # except str = exception info + traceback string
