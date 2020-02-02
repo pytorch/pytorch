e07bca2c583c46d6631a2c6d55814ee4fe720307
ludwigwinkler <paul.ludwig.winkler@gmail.com>
2020-02-02 15:00:01 +0100

    Docfix

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -978,7 +978,7 @@ class Module(object):
                 yield name, module
 
     def modules(self):
-        r"""Returns an iterator over all modules in the network.
+        r"""Returns an iterator over all modules in the network including ``self`` if called inside a ``torch.nn.Module``.
 
         Yields:
             Module: a module in the network
