ecbcbe0fd8be9bdd77ba9716ae06411a33b2a994
Wojciech Baranowski <wbaranowski@protonmail.com>
2020-02-03 10:11:31 -0800

    min, max: check that operand and outputs are on the same device type (#32862)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/32001
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32862
    
    Differential Revision: D19695935
    
    Pulled By: ezyang
    
    fbshipit-source-id: bb37eb7a187214aa69259828024366f479a258d7

--- aten/src/ATen/native/TensorCompare.cpp
+++ aten/src/ATen/native/TensorCompare.cpp
@@ -213,6 +213,12 @@ static std::tuple<Tensor &,Tensor &> max_out_impl(Tensor& max, Tensor& max_indic
               "max only supports CPU AND CUDA device type, got: ", self.device().type());
   TORCH_CHECK(self.layout() == Layout::Strided,
               "max only supports strided layout, got: ", self.layout());
+  TORCH_CHECK(self.device() == max.device(),
+              "expected device ", self.device(), " but got ",
+              max.device(), " for max values output");
+  TORCH_CHECK(self.device() == max_indices.device(),
+              "expected device ", self.device(), " but got ",
+              max_indices.device(), " for indices output");
   dim = maybe_wrap_dim(dim, self.dim());
   if (_dimreduce_return_trivial_no_ident(max, self, dim, keepdim, "max")) {
     AT_ASSERT(max.dim() == 0);
@@ -271,6 +277,12 @@ static std::tuple<Tensor &,Tensor &> min_out_impl(Tensor& min, Tensor& min_indic
               "min only supports CPU AND CUDA device type, got: ", self.device().type());
   TORCH_CHECK(self.layout() == Layout::Strided,
               "min only supports strided layout, got: ", self.layout());
+  TORCH_CHECK(self.device() == min.device(),
+              "expected device ", self.device(), " but got ",
+              min.device(), " for min values output");
+  TORCH_CHECK(self.device() == min_indices.device(),
+              "expected device ", self.device(), " but got ",
+              min_indices.device(), " for indices output");
   dim = maybe_wrap_dim(dim, self.dim());
   if (_dimreduce_return_trivial_no_ident(min, self, dim, keepdim, "min")) {
     AT_ASSERT(min.dim() == 0);
