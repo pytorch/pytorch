b40f73156bc0631aaed2413d13eda45f2da76131
Pierre Fenoll <pierrefenoll@gmail.com>
2020-02-03 11:10:53 -0800

    raise when jit-load.ing a folder (#27836)
    Summary:
    Very similar to https://github.com/pytorch/pytorch/issues/16267 but handling directories.
    
    Stoked to contribute!
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27836
    
    Differential Revision: D19698398
    
    Pulled By: ezyang
    
    fbshipit-source-id: eabc3a44d258124f860babb47ab91e22c2c3d6cc

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -216,6 +216,8 @@ def load(f, map_location=None, _extra_files=DEFAULT_EXTRA_FILES_MAP):
     if isinstance(f, string_classes):
         if not os.path.exists(f):
             raise ValueError("The provided filename {} does not exist".format(f))
+        if os.path.isdir(f):
+            raise ValueError("The provided filename {} is a directory".format(f))
     if isinstance(map_location, string_classes):
         map_location = torch.device(map_location)
     elif not (map_location is None or
