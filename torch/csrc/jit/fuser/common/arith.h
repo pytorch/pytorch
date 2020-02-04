8bf81b063463dd01ae67f4941ad95fbc93132e08
Christian Sarofeen <csarofeen@nvidia.com>
2020-02-04 12:45:32 -0500

    Comment arith.h

--- torch/csrc/jit/fuser/common/arith.h
+++ torch/csrc/jit/fuser/common/arith.h
@@ -4,9 +4,18 @@
 #include <torch/csrc/jit/fuser/common/type.h>
 #include <c10/util/Exception.h>
 
+/*
+ * Creating an Expr node returns the node that was created. This is useful
+ * if you already have things connected and are directly modifying Exprs.
+ * However, creating Expr nodes directly requires you to instantiate all
+ * intermediate values. Arith is intended to cover all Exprs, however,
+ * its goal is to return a value that is the output of the Expr.
+ */ 
+
 namespace torch{
 namespace jit{
 namespace fuser{
+
 //Return new value of type that v1 and v2 promotes to
 TORCH_API Val* promote_new(Val *v1, Val* v2);
 
