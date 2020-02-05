bda874b480c35f8baa511f709d906d69ef052005
Rohan Varma <rvarm1@fb.com>
2020-02-04 16:05:47 -0800

    [rpc] throw correct Exception on local client based on the RemoteException (#32936)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32936
    
    Closes https://github.com/pytorch/pytorch/issues/32732. Currently if a
    UDF run in RPC throws an exception such as ValueError or TypeError, we wrap
    this in a RemoteException on the callee side. When raising this on the caller
    side, we currently raise a vanilla Exception. This diff changes it so that the
    correct exception is thrown. Tested by changing the current rpc tests to assert
    on the right type of error rather than just the base `Exception`.
    ghstack-source-id: 97706957
    
    Test Plan: Modified unit test.
    
    Differential Revision: D19700434
    
    fbshipit-source-id: e451b772ea6aecc1d2e109e67e7f932eb9151f15

--- torch/testing/_internal/distributed/rpc/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -821,7 +821,7 @@ class RpcTest(RpcAgentTestFixture):
     def test_py_function_exception(self):
         n = self.rank + 1
         dst_rank = n % self.world_size
-        with self.assertRaisesRegex(Exception, "TypeError"):
+        with self.assertRaises(TypeError):
             ret = rpc.rpc_sync("worker{}".format(dst_rank), no_result, args=(10,))
 
     @dist_init
@@ -829,7 +829,7 @@ class RpcTest(RpcAgentTestFixture):
         n = self.rank + 1
         dst_rank = n % self.world_size
         fut = rpc.rpc_async("worker{}".format(dst_rank), raise_func)
-        with self.assertRaisesRegex(Exception, "ValueError"):
+        with self.assertRaises(ValueError):
             fut.wait()
 
     @dist_init
@@ -843,12 +843,11 @@ class RpcTest(RpcAgentTestFixture):
     @dist_init
     def test_torchscript_function_exception(self):
         dst_worker_name = "worker{}".format((self.rank + 1) % self.world_size)
-
-        with self.assertRaisesRegex(Exception, r"one_arg\(\) expected at most"):
+        with self.assertRaisesRegex(RuntimeError, r"one_arg\(\) expected at most"):
             ret = rpc.rpc_sync(dst_worker_name, one_arg, args=(10, 20))
 
         with self.assertRaisesRegex(
-            Exception, r"one_arg\(\) expected at most"
+            RuntimeError, r"one_arg\(\) expected at most"
         ):
             rref = rpc.remote(dst_worker_name, one_arg, args=(10, 20))
 
@@ -1102,11 +1101,11 @@ class RpcTest(RpcAgentTestFixture):
         dst_rank = n % self.world_size
         # check ref to other workers
         rref = rpc.remote("worker{}".format(dst_rank), raise_func)
-        with self.assertRaisesRegex(Exception, "ValueError"):
+        with self.assertRaises(ValueError):
             rref.to_here()
         # check ref to itself
         rref = rpc.remote("worker{}".format(self.rank), no_result, args=(10,))
-        with self.assertRaisesRegex(Exception, "TypeError"):
+        with self.assertRaises(TypeError):
             rref.to_here()
 
     @dist_init
@@ -1703,11 +1702,11 @@ class RpcTest(RpcAgentTestFixture):
 
         if self.rank == 0:
             # func exists on caller, but not callee.
-            # TODO: Need to enhance RemoteException to return the correct
-            # Exception subclass: https://github.com/pytorch/pytorch/issues/32732
             # wait for remote end to remove the binding of foo_add func.
             wait_for_value_future()
             # Ensure that we have the attribute on this module. Otherwise, the test could fail due to a caller-side pickling error.
             self.assertTrue(hasattr(this_module, "foo_add"))
-            with self.assertRaisesRegex(Exception, "AttributeError"):
+            with self.assertRaisesRegex(
+                AttributeError, "RPC pickler does not serialize"
+            ):
                 rpc.rpc_sync(callee_worker, foo_add, args=())
