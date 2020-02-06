f6c7f408e735309db7dbd582bdb9d53b1a9cdd16
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2020-02-06 14:38:45 -0800

    added dtype argument to expm1 function, changes helper functions as well

--- aten/src/ATen/native/UnaryOps.cpp
+++ aten/src/ATen/native/UnaryOps.cpp
@@ -35,9 +35,9 @@ namespace native {
 // macro that implements everything, because the former allows some simple preprocessing that are unique to some
 // operators (more is foreseeable) and is more flexible and elegant than the latter.
 template <typename Stub>
-static inline Tensor& unary_op_impl_out(Tensor& result, const Tensor& self, Stub& stub) {
+static inline Tensor& unary_op_impl_out(Tensor& result, const Tensor& self, Stub& stub, bool strategy_promote=false) {
   auto iter = TensorIterator::unary_op(result, self,
-    /*check_mem_overlap=*/true);
+    /*check_mem_overlap=*/true, /*promoting=*/strategy_promote);
   stub(iter.device_type(), iter);
   return result;
 }
@@ -49,6 +49,13 @@ template <typename OutImpl>
 static inline Tensor unary_op_impl(const Tensor& self, OutImpl& out_impl) {
   Tensor result = at::empty({0}, self.options());
   return out_impl(result, self);
+}
+
+template <typename OutImpl>
+static inline Tensor unary_op_impl(const Tensor& self, OutImpl& out_impl, c10::ScalarType dtype) {
+  Tensor result = at::empty({0}, self.options().dtype(dtype));
+  out_impl(result, self, true); // true for dtype promotion
+  result; 
 }
 
 template <typename OutImpl>
@@ -89,7 +96,9 @@ Tensor ceil(const Tensor& self) { return unary_op_impl(self, at::ceil_out); }
 Tensor& ceil_(Tensor& self) { return unary_op_impl_(self, at::ceil_out); }
 
 Tensor& expm1_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, expm1_stub); }
+Tensor& expm1_out_promoting(Tensor& result, const Tensor& self, bool promoting=false) { return unary_op_impl_out(result, self, expm1_stub, promoting); }
 Tensor expm1(const Tensor& self) { return unary_op_impl(self, at::expm1_out); }
+Tensor expm1(const Tensor& self, c10::ScalarType dtype) { return unary_op_impl(self, at::native::expm1_out_promoting, dtype); }
 Tensor& expm1_(Tensor& self) { return unary_op_impl_(self, at::expm1_out); }
 
 Tensor& frac_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, frac_stub); }
