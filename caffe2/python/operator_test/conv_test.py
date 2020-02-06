064b11685a07a7b78364ea1f99e83be41bbae2b4
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2020-02-06 17:40:28 -0600

    Enable test

--- caffe2/python/operator_test/conv_test.py
+++ caffe2/python/operator_test/conv_test.py
@@ -569,8 +569,7 @@ class TestConvolution(serial.SerializedTestCase):
         force_algo_fwd=_cudnn_convolution_algo_count("fwd"),
         force_algo_dgrad=_cudnn_convolution_algo_count("dgrad"),
         force_algo_wgrad=_cudnn_convolution_algo_count("wgrad"),
-        **hu.gcs_no_hip
-    )  # MIOPEN doesn't support 3D conv yet
+    )
     def test_3d_convolution_cudnn_nchw(
         self,
         op_type,
