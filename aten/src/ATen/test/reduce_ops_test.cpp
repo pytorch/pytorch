5fc49a4e6f7929d099fe85adb641f2f6c7c274fe
take-cheeze <takechi101010@gmail.com>
2020-02-06 12:04:52 +0900

    Add CPU tests

--- aten/src/ATen/test/reduce_ops_test.cpp
+++ aten/src/ATen/test/reduce_ops_test.cpp
@@ -8,9 +8,18 @@ using namespace at;
 TEST(ReduceOpsTest, MaxValuesAndMinValues) {
   const int W = 10;
   const int H = 10;
-  if (hasCUDA()) {
-    for (const auto dtype : {kHalf, kFloat, kDouble, kShort, kInt, kLong}) {
-      auto a = at::rand({H, W}, TensorOptions(kCUDA).dtype(at::kHalf));
+
+  for (const auto dev : {kCPU, kCUDA}) {
+    std::vector<at::ScalarType> dtypes = {kFloat, kDouble, kShort, kInt, kLong};
+    if (hasCUDA()) {
+      dtypes.push_back(kHalf);
+    } else if (dev == kCUDA) {
+      // Skip CUDA test in non cuda env
+      continue;
+    }
+
+    for (const auto dtype : dtypes) {
+      auto a = at::rand({H, W}, TensorOptions(dev).dtype(dtype));
       ASSERT_FLOAT_EQ(
         a.max_values(c10::IntArrayRef{0, 1}).item<double>(),
         a.max().item<double>()
