05d18ffaf5ccc5a19245afafe3998fc3731b570d
Pritam Damania <pritam.damania@fb.com>
2020-02-06 23:24:42 -0800

    Distributed Autograd: Allow multiple backward passes to accumulate gradients. (#32506)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32506
    
    In this PR, we've introduced a `retain_graph` parameter to distributed
    autograd similar to `torch.autograd.backward`.
    
    In terms of design, this parameter is sent over RPC to all nodes and is used to
    create the GraphTask on the local nodes. This enables us to run
    `dist_autograd.backward()` multiple times in the same context.
    
    The use case currently for this is to benchmark only the backward pass for
    distributed autograd. We'd like to measure the QPS for the backward pass and as
    a result, running a single forward pass and multiple backward passes in a loop
    is one way to benchmark backward pass performance.
    ghstack-source-id: 97868900
    
    Test Plan: waitforbuildbot
    
    Differential Revision: D19521288
    
    fbshipit-source-id: 7ad8521059fd400d7b5a6ab77ce56e1927ced90a

--- torch/csrc/distributed/autograd/functions/recvrpc_backward.cpp
+++ torch/csrc/distributed/autograd/functions/recvrpc_backward.cpp
@@ -30,9 +30,20 @@ variable_list RecvRpcBackward::apply(variable_list&& grads) {
     }
   }
 
+  auto sharedContext = autogradContext_.lock();
+  TORCH_CHECK(
+      sharedContext,
+      c10::str(
+          "Autograd context no longer valid! This usually ",
+          "means the autograd context was cleaned up by a different thread due ",
+          "to an error before RecvRcpBackward had a chance to run"));
+
   // Send the gradients over the wire and record the future in the autograd
   // context.
-  PropagateGradientsReq gradCall(autogradMetadata_, outputGrads);
+  PropagateGradientsReq gradCall(
+      autogradMetadata_,
+      outputGrads,
+      sharedContext->retrieveGraphTask()->keep_graph_);
 
   // Send the gradients over to the appropriate node.
   auto rpcAgent = rpc::RpcAgent::getCurrentRpcAgent();
@@ -40,11 +51,7 @@ variable_list RecvRpcBackward::apply(variable_list&& grads) {
       rpcAgent->getWorkerInfo(fromWorkerId_), std::move(gradCall).toMessage());
 
   // Record the future in the context.
-  if (auto sharedContext = autogradContext_.lock()) {
-    sharedContext->addOutstandingRpc(futureMessage);
-  } else {
-    C10_THROW_ERROR(Error, "Autograd context no longer valid");
-  }
+  sharedContext->addOutstandingRpc(futureMessage);
 
   // 'recv' function sends the gradients over the wire using RPC, it doesn't
   // need to return anything for any downstream autograd function.
