43cb85a7f347e14bedab1695025082f5a2258524
Edward Z. Yang <ezyang@fb.com>
2020-02-07 07:46:24 -0800

    Delete redundant aliasAnalysis annotations from VariableType.
    A lot of people look at VariableType to work out how they should
    write registrations, so it's important to not put extra goop in
    them.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -184,14 +184,12 @@ UNBOXEDONLY_WRAPPER_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
   .schema("${schema_string}")
   .impl_unboxedOnlyKernel<${return_type} (${formal_types}), &VariableType::${api_name}>(DispatchKey::VariableTensorId)
-  .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
 """)
 
 WRAPPER_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
   .schema("${schema_string}")
   .kernel<${return_type} (${formal_types})>(DispatchKey::VariableTensorId, &VariableType::${api_name})
-  .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
 """)
 
 UNPACK_TENSOR = CodeTemplate("""\
