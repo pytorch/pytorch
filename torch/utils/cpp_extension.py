763d90117615b369e4bbbc9ac9fedfb79a48b80a
peter <peterghost86@gmail.com>
2020-02-08 18:26:29 +0800

    fix 4

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1515,6 +1515,7 @@ def _write_ninja_file(path,
         rule = 'cuda_compile' if is_cuda_source else 'compile'
         if IS_WINDOWS:
             source_file = source_file.replace(':', '$:')
+            object_file = object_file.replace(':', '$:')
         source_file = source_file.replace(" ", "$ ")
         build.append('build {}: {} {}'.format(object_file, rule, source_file))
 
