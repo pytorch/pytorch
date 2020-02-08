a7ccb6a9a88b678f70293081fa4a6555013e26fd
Jithun Nair <jithun.nair@amd.com>
2020-02-08 08:54:50 +0000

    Lint fixes

--- torch/backends/cudnn/__init__.py
+++ torch/backends/cudnn/__init__.py
@@ -81,7 +81,7 @@ def _libcudnn():
             miopen_patch = ctypes.c_size_t()
             # miopen version is MAJOR*1000000 + MINOR*1000 + PATCH
             lib.miopenGetVersion(ctypes.byref(miopen_major), ctypes.byref(miopen_minor), ctypes.byref(miopen_patch))
-            __cudnn_version = miopen_major.value*1000000 + miopen_minor.value*1000 + miopen_patch.value
+            __cudnn_version = miopen_major.value * 1000000 + miopen_minor.value * 1000 + miopen_patch.value
         else:
             lib = None
     return lib
@@ -189,28 +189,28 @@ if cuda is not None:
             ptr = ctypes.c_void_p()
             check_error(lib.cudnnCreate(ctypes.byref(ptr)))
             self._as_parameter_ = ptr
-    
+
         def __del__(self):
             check_error(lib.cudnnDestroy(self))
-    
-    
+
+
     class CuDNNError(RuntimeError):
         def __init__(self, status):
             self.status = status
             msg = '{}: {}'.format(status, get_error_string(status))
             super(CuDNNError, self).__init__(msg)
-    
-    
+
+
     class TensorDescriptor(object):
         def __init__(self):
             ptr = ctypes.c_void_p()
             check_error(lib.cudnnCreateTensorDescriptor(ctypes.byref(ptr)))
             self._as_parameter_ = ptr
-    
+
         def __del__(self):
             check_error(lib.cudnnDestroyTensorDescriptor(self._as_parameter_))
             del self._as_parameter_
-    
+
         def set(self, tensor):
             self._type = tensor.type()
             self._size = tensor.size()
@@ -218,11 +218,11 @@ if cuda is not None:
             check_error(lib.cudnnSetTensorNdDescriptor(
                 self, _typemap[tensor.type()], tensor.dim(),
                 int_array(tensor.size()), int_array(tensor.stride())))
-    
+
         def as_tuple(self):
             return (self._type, tuple(self._size), tuple(self._stride))
-    
-    
+
+
     class TensorDescriptorArray(object):
         def __init__(self, N):
             self.ptrs = (ctypes.c_void_p * N)()
@@ -230,14 +230,14 @@ if cuda is not None:
                 ptr = ctypes.byref(self.ptrs, i * ctypes.sizeof(ctypes.c_void_p))
                 check_error(lib.cudnnCreateTensorDescriptor(ptr))
             self._as_parameter_ = self.ptrs
-    
+
         def __del__(self):
             for ptr in self.ptrs:
                 check_error(lib.cudnnDestroyTensorDescriptor(ctypes.c_void_p(ptr)))
-    
+
         def __getitem__(self, key):
             return ctypes.c_void_p(self.ptrs[key])
-    
+
         def set_all(self, tensor):
             _type = _typemap[tensor.type()]
             _ndim = tensor.dim()
@@ -246,50 +246,50 @@ if cuda is not None:
             for ptr in self.ptrs:
                 check_error(lib.cudnnSetTensorNdDescriptor(
                     ctypes.c_void_p(ptr), _type, _ndim, _size, _stride))
-    
+
         def set_raw(self, i, _type, _ndim, _size, _stride):
             ptr = self.ptrs[i]
             check_error(lib.cudnnSetTensorNdDescriptor(
                 ctypes.c_void_p(ptr), _type, _ndim, _size, _stride))
-    
-    
+
+
     class FilterDescriptor(object):
         def __init__(self):
             ptr = ctypes.c_void_p()
             check_error(lib.cudnnCreateFilterDescriptor(ctypes.byref(ptr)))
             self._as_parameter_ = ptr
-    
+
         def __del__(self):
             check_error(lib.cudnnDestroyFilterDescriptor(self._as_parameter_))
             del self._as_parameter_
-    
+
         def set(self, weight):
             self._size = weight.size()
             datatype = _typemap[weight.type()]
             check_error(lib.cudnnSetFilterNdDescriptor(
                 self, datatype, CUDNN_TENSOR_NCHW, weight.ndimension(),
                 int_array(weight.size())))
-    
+
         def as_tuple(self):
             return tuple(self._size)
-    
-    
+
+
     class DropoutDescriptor(object):
         def __init__(self, handle, dropout, seed):
             ptr = ctypes.c_void_p()
             check_error(lib.cudnnCreateDropoutDescriptor(ctypes.byref(ptr)))
-    
+
             self._as_parameter_ = ptr
             self.state = None
             self.dropout = dropout
             self.handle = handle
-    
+
             self._set(dropout, seed)
-    
+
         def set_dropout(self, dropout, seed):
             if dropout != self.dropout:
                 self._set(dropout, seed)
-    
+
         def _set(self, dropout, seed):
             if self.state is None and dropout > 0:
                 dropout_states_size = ctypes.c_long()
@@ -302,7 +302,7 @@ if cuda is not None:
             else:
                 state_ptr = None
                 state_size = 0
-    
+
             check_error(lib.cudnnSetDropoutDescriptor(
                 self,
                 self.handle,
@@ -311,13 +311,13 @@ if cuda is not None:
                 ctypes.c_size_t(state_size),
                 ctypes.c_ulonglong(seed),
             ))
-    
+
             self.dropout = dropout
-    
+
         def __del__(self):
             check_error(lib.cudnnDestroyDropoutDescriptor(self))
-    
-    
+
+
     class RNNDescriptor(object):
         def __init__(self, handle, hidden_size, num_layers, dropout_desc, input_mode,
                      bidirectional, mode, datatype):
@@ -353,20 +353,20 @@ if cuda is not None:
                     mode,
                     datatype
                 ))
-    
+
         def __del__(self):
             check_error(lib.cudnnDestroyRNNDescriptor(self))
-    
-    
+
+
     def check_error(status):
         if status != 0:
             raise CuDNNError(status)
-    
-    
+
+
     def get_error_string(status):
         return lib.cudnnGetErrorString(status)
-    
-    
+
+
     def get_handle():
         if _libcudnn() is None:
             raise RuntimeError('cuDNN not available')
@@ -376,21 +376,21 @@ if cuda is not None:
             handle = CuDNNHandle()
             _handles[current_device] = handle
         return handle
-    
-    
+
+
     _typemap = {
         'torch.cuda.HalfTensor': CUDNN_DATA_HALF,
         'torch.cuda.FloatTensor': CUDNN_DATA_FLOAT,
         'torch.cuda.DoubleTensor': CUDNN_DATA_DOUBLE,
     }
-    
+
     _sizeofmap = {
         CUDNN_DATA_HALF: 2,
         CUDNN_DATA_FLOAT: 4,
         CUDNN_DATA_DOUBLE: 8,
     }
-    
-    
+
+
     def c_type(tensor):
         if isinstance(tensor, torch.cuda.HalfTensor):
             return ctypes.c_float
@@ -400,13 +400,13 @@ if cuda is not None:
             return ctypes.c_double
         else:
             raise ValueError("unknown type '{}'".format(type(tensor)))
-    
-    
+
+
     def int_array(itr):
         array_type = ctypes.c_int * len(itr)
         return array_type(*itr)
-    
-    
+
+
     def descriptor(tensor, N=None):
         padded_size = tensor.size() + ((1,) * (5 - tensor.dim()))
         tensor = tensor.view(padded_size)
@@ -417,8 +417,8 @@ if cuda is not None:
             descriptor = TensorDescriptor()
             descriptor.set(tensor)
         return descriptor
-    
-    
+
+
     def descriptor_sequence(tensor, batch_sizes):
         descriptors = TensorDescriptorArray(len(batch_sizes))
         _type = _typemap[tensor.type()]
@@ -430,8 +430,8 @@ if cuda is not None:
             _size[0] = batch_size
             descriptors.set_raw(i, _type, _ndim, _size, _stride)
         return descriptors
-    
-    
+
+
     def add_tensor(*args):
         check_error(lib.cudnnAddTensor(*args))
 
