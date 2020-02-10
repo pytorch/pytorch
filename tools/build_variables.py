8301f980df24125e655086e833714d427b438b8a
Rohan Varma <rvarm1@fb.com>
2020-02-10 14:18:59 -0800

    Update

--- tools/build_variables.py
+++ tools/build_variables.py
@@ -77,6 +77,7 @@ libtorch_sources = [
     "torch/csrc/distributed/rpc/script_resp.cpp",
     "torch/csrc/distributed/rpc/types.cpp",
     "torch/csrc/distributed/rpc/utils.cpp",
+    "torch/csrc/distributed/rpc/process_group_agent.cpp",
     "torch/csrc/jit/autodiff.cpp",
     "torch/csrc/jit/attributes.cpp",
     "torch/csrc/jit/argument_spec.cpp",
