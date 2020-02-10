d821644fc8c31c03dc5650488021f0b00750b6ed
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2020-02-10 12:36:56 -0800

    Enable test_distributed for ROCm but only with nccl backend [REDUX] (#32551)
    Summary:
    This is a redux of the original PR https://github.com/pytorch/pytorch/issues/28814 which was reverted in PR https://github.com/pytorch/pytorch/issues/29736 due to test_DistributedDataParallel being suspected as being flaky. Further investigation revealed it wasn't flakiness, but a bug in the PyTorch source code which has been now fixed in PR https://github.com/pytorch/pytorch/issues/32356. This PR is another attempt at enabling the test_distributed unit test suite only for the nccl backend.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32551
    
    Differential Revision: D19729966
    
    Pulled By: bddppq
    
    fbshipit-source-id: 12a0d850991a903cc7723d63693b6157071d7115

--- test/run_test.py
+++ test/run_test.py
@@ -95,7 +95,6 @@ ROCM_BLACKLIST = [
     'test_cpp_extensions_aot',
     'test_cpp_extensions_aot_no_ninja',
     'test_cpp_extensions_jit',
-    'distributed/test_distributed',
     'test_multiprocessing',
     'distributed/rpc/test_rpc_spawn',
     'distributed/rpc/test_dist_autograd_spawn',
@@ -105,7 +104,7 @@ DISTRIBUTED_TESTS_CONFIG = {}
 
 
 if dist.is_available():
-    if dist.is_mpi_available():
+    if not TEST_WITH_ROCM and dist.is_mpi_available():
         DISTRIBUTED_TESTS_CONFIG['mpi'] = {
             'WORLD_SIZE': '3',
             'TEST_REPORT_SOURCE_OVERRIDE': 'dist-mpi'
@@ -115,7 +114,7 @@ if dist.is_available():
             'WORLD_SIZE': '2' if torch.cuda.device_count() == 2 else '3',
             'TEST_REPORT_SOURCE_OVERRIDE': 'dist-nccl'
         }
-    if dist.is_gloo_available():
+    if not TEST_WITH_ROCM and dist.is_gloo_available():
         DISTRIBUTED_TESTS_CONFIG['gloo'] = {
             'WORLD_SIZE': '2' if torch.cuda.device_count() == 2 else '3',
             'TEST_REPORT_SOURCE_OVERRIDE': 'dist-gloo'
