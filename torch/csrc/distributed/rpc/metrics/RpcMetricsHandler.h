4f60cca6fa0a86bf97ac82bbe17a014020995e71
Rohan Varma <rvarm1@fb.com>
2020-02-10 12:13:12 -0800

    [rpc][metrics] add initial metric handler classes.
    These are the initial classes and unit tests for RPC metrics. They contain:
    `RpcMetrics.h` - the abstract interface from which metrics handler will inherit (i.e. the ServiceDataMetricsHandler will inherit from this)
    `ConsoleMetricsHandler` - a dummy metrics handler that keeps track of aggregate and individual metrics. Not used in prod but will help with debugging and can also be a simple OSS interface.
    
    If these interfaces look okay, we can export them to OSS in a later diff and remove the interfaces from internal-FB code.
    
    Per the
    quip design doc https://fb.quip.com/s0u3AffEJzbO, the next steps will be to implement logging to ODS via fbData
    and explore using Logger to log to both ODS and Scuba. We will then also
    integrate this with ThriftRpcAgent and ensure we are capturing a desired set of
    metrics.
    
    Differential Revision: [D19615364](https://our.internmc.facebook.com/intern/diff/D19615364/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D19615364/)!
    
    [ghstack-poisoned]

---
+++ torch/csrc/distributed/rpc/metrics/RpcMetricsHandler.h
@@ -0,0 +1,29 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+#include <string>
+
+namespace torch {
+namespace distributed {
+namespace rpc {
+// All metrics are prefixed with the following  key.
+const std::string kRPCMetricsKeyPrefix = "torch.distributed.rpc.";
+// Thread-safe API for logging time-series metrics for RPC-based model parallel
+// training. This class provides thread safety so that metrics can be logged
+// from multiple threads without the user needing to coordinate serialization.
+// The class provides two main functions - accumulateMetric which can be used
+// for statistical aggregations and setMetric for setting/modifying single
+// values.
+class RpcMetricsHandler {
+ public:
+  // Log value corresponding to metric name given by name to the time-series
+  // metric stream.
+  virtual void accumulateMetric(const std::string& name, double value) = 0;
+  // Increment a count for the metric given by the name.
+  virtual void bumpMetric(const std::string& name) = 0;
+  virtual ~RpcMetricsHandler() {}
+};
+
+} // namespace rpc
+} // namespace distributed
+} // namespace torch
