b1fb79b3642620506d57a1e2da94bee21236097d
Hong Xu <hong@topbug.net>
2020-02-11 10:59:27 -0800

    Add zero_mask function for vectorized functions. (#32985)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32985
    
    This can be useful in many situations to decide whether all elements are
    zeros or non-zeros, such as elu as shown in #32986 .
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19794549
    
    Pulled By: VitalyFedyunin
    
    fbshipit-source-id: 1be1c863d69b9a19fdcfcdd7cb52343066f740d3

--- aten/src/ATen/cpu/vec256/vec256_float.h
+++ aten/src/ATen/cpu/vec256/vec256_float.h
@@ -92,6 +92,11 @@ public:
   }
   const float& operator[](int idx) const  = delete;
   float& operator[](int idx) = delete;
+  int zero_mask() const {
+    // returns an integer mask where all zero elements are translated to 1-bit and others are translated to 0-bit
+    __m256 cmp = _mm256_cmp_ps(values, _mm256_set1_ps(0.0f), _CMP_EQ_OQ);
+    return _mm256_movemask_ps(cmp);
+  }
   Vec256<float> map(float (*f)(float)) const {
     __at_align32__ float tmp[8];
     store(tmp);
