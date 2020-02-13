f2671c54bc658f1837926f9882889db634123555
Hao Lu <hlu@fb.com>
2020-02-12 22:27:37 -0800

    [TVM] Add clip op to c2_frontend (#33257)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/33257
    
    Test Plan: buck test caffe2/caffe2/fb/tvm:test_tvm_transform
    
    Reviewed By: yinghai
    
    Differential Revision: D19866406
    
    fbshipit-source-id: e903e15178af323d0bd1f804e09919023c0a2989

--- caffe2/opt/tvm_transformer.cc
+++ caffe2/opt/tvm_transformer.cc
@@ -217,7 +217,8 @@ NetDef TvmTransformer::applyTvmTransform(
         "DotProduct", "Transpose",
         "Mul",        "Tanh",
         "Logit",      "Cast",
-        "Copy",       "ReplaceNaN"};
+        "Copy",       "ReplaceNaN",
+        "Clip"};
 
     try {
       // If the op position is black listed, return false
