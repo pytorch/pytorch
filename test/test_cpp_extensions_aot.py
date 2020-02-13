e59c0380b4c978db17fdc4a3f15325e85f328560
Will Feng <willfeng@fb.com>
2020-02-13 13:08:16 -0800

    Disable flaky test TestCppExtensionAOT.test_cuda_extension in Windows CI (#33282)
    Summary:
    See https://github.com/pytorch/pytorch/issues/33270 for details.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33282
    
    Differential Revision: D19886975
    
    Pulled By: yf225
    
    fbshipit-source-id: 7e6756095b1bb8c55fc5acb8fc2cb02c1e89b032

--- test/test_cpp_extensions_aot.py
+++ test/test_cpp_extensions_aot.py
@@ -55,6 +55,7 @@ class TestCppExtensionAOT(common.TestCase):
         self.assertEqual(tensor.grad, expected_tensor_grad)
 
     @unittest.skipIf(not TEST_CUDA, "CUDA not found")
+    @unittest.skipIf(IS_WINDOWS, "Flaky on Windows, see issue #33270")
     def test_cuda_extension(self):
         import torch_test_cpp_extension.cuda as cuda_extension
 
