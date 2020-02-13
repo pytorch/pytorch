0068cd947aecc3b185e5864d310c821fd0555c20
neginraoof <neginmr@utexas.edu>
2020-02-13 11:36:59 -0800

    Adding jobs

--- .circleci/scripts/should_run_job.py
+++ .circleci/scripts/should_run_job.py
@@ -27,7 +27,8 @@ default_set = set([
     'caffe2-py3.5-cuda10.1-cudnn7-ubuntu16.04',
     # Caffe2 ONNX
     'caffe2-onnx-py2-gcc5-ubuntu16.04',
-    'caffe2-onnx-py3.6-clang7-ubuntu16.04',
+    'caffe2-onnx-py3.6-lower-clang7-ubuntu16.04',
+    'caffe2-onnx-py3.6-higher-clang7-ubuntu16.04',
     # Caffe2 Clang
     'caffe2-py2-clang7-ubuntu16.04',
     # Caffe2 CMake
