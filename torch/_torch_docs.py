9959b65c428c337bd9620d04d38de2da0b8f9cc7
anjali411 <chourdiaanjali123@gmail.com>
2020-02-12 17:41:23 -0800

    Added tensor.is_complex(), is_complex and dtype.is_complex py binding, tensor printing, and dixed the scalar type returned for complex float
    [ghstack-poisoned]

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -2585,6 +2585,17 @@ Args:
     input (Tensor): the PyTorch tensor to test
 """)
 
+add_docstr(torch.is_complex,
+           r"""
+is_complex(input) -> (bool)
+
+Returns True if the data type of :attr:`input` is a floating point data type i.e.,
+one of ``torch.complex64``, and ``torch.float128``.
+
+Args:
+    input (Tensor): the PyTorch tensor to test
+""")
+
 add_docstr(torch.kthvalue,
            r"""
 kthvalue(input, k, dim=None, keepdim=False, out=None) -> (Tensor, LongTensor)
