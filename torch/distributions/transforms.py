b1552010b01d3cdd65a4c878a169ab6332e5b4a4
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2020-02-14 14:57:07 +0100

    Update transforms.py

--- torch/distributions/transforms.py
+++ torch/distributions/transforms.py
@@ -411,7 +411,7 @@ class TanhTransform(Transform):
 
     def log_abs_det_jacobian(self, x, y):
         # We use a formula that is more numerically stable, see details in the following link
-        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
+        # https://github.com/tensorflow/probability/blob/master/tensorflow_probability/python/bijectors/tanh.py#L69-L80
         return 2. * (math.log(2.) - x - softplus(-2. * x))
 
 
