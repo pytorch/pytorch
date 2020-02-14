b8f6df994e7dd42ff040d6269de86effc8068134
Ahmad Salim Al-Sibahi <ahmad@di.ku.dk>
2020-02-14 07:14:20 -0800

    Second try on Von Mises: Make it JIT compatible (#33177)
    Summary:
    Follow up from https://github.com/pytorch/pytorch/issues/17168 .
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33177
    
    Differential Revision: D19899550
    
    Pulled By: ezyang
    
    fbshipit-source-id: fbcdd9bc91438164bcb2b1cbc314c765520754e1

--- torch/distributions/__init__.py
+++ torch/distributions/__init__.py
@@ -107,6 +107,7 @@ from .studentT import StudentT
 from .transformed_distribution import TransformedDistribution
 from .transforms import *
 from .uniform import Uniform
+from .von_mises import VonMises
 from .weibull import Weibull
 
 __all__ = [
@@ -142,6 +143,7 @@ __all__ = [
     'StudentT',
     'Poisson',
     'Uniform',
+    'VonMises',
     'Weibull',
     'TransformedDistribution',
     'biject_to',
