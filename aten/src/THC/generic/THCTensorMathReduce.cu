5e07a9b2c87197224b86a2681ce2e1976c82e2d9
xiaobing.zhang <xiaobing.zhang@intel.com>
2020-02-14 10:34:16 -0800

    remove THC dist code which nerver be used (#33283)
    Summary:
    Remove THC dist code which nerver be used.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33283
    
    Differential Revision: D19905361
    
    Pulled By: gchanan
    
    fbshipit-source-id: 367fd31e2209d36b30af31511554fdbdd67c98e4

--- aten/src/THC/generic/THCTensorMathReduce.cu
+++ aten/src/THC/generic/THCTensorMathReduce.cu
@@ -178,61 +178,6 @@ accreal THCTensor_(var_all)(THCState *state, THCTensor *self, bool unbiased)
   return val;
 }
 
-accreal THCTensor_(dist)(THCState *state, THCTensor *self,
-                         THCTensor *src, scalar_t _value)
-{
-  const accreal value = scalar_cast<accreal>(_value);
-  THCAssertSameGPU(THCTensor_(checkGPU)(state, 2, self, src));
-  self = THCTensor_(newContiguous)(state, self);
-  ptrdiff_t size = THCTensor_(nElement)(state, self);
-  src = THCTensor_(newContiguous)(state, src);
-  thrust::device_ptr<scalar_t> self_data(THCTensor_(data)(state, self));
-  thrust::device_ptr<scalar_t> src_data(THCTensor_(data)(state, src));
-
-  THCThrustAllocator thrustAlloc(state);
-  accreal result;
-
-  if (THCNumerics<accreal>::eq(value, scalar_cast<accreal>(INFINITY))) {
-    result = thrust::inner_product(
-#if CUDA_VERSION >= 7000 || defined __HIP_PLATFORM_HCC__
-      thrust::cuda::par(thrustAlloc).on(THCState_getCurrentStream(state)),
-#endif
-      self_data, self_data+size, src_data, scalar_cast<accreal>(0),
-      ReduceMax<accreal>(),
-      ThrustTensorDistOp<scalar_t, accreal>(scalar_cast<scalar_t>(1)));
-  } else if (THCNumerics<accreal>::eq(value, scalar_cast<accreal>(-INFINITY))) {
-    result = thrust::inner_product(
-#if CUDA_VERSION >= 7000 || defined __HIP_PLATFORM_HCC__
-      thrust::cuda::par(thrustAlloc).on(THCState_getCurrentStream(state)),
-#endif
-      self_data, self_data+size, src_data, scalar_cast<accreal>(INFINITY),
-      ReduceMin<accreal>(),
-      ThrustTensorDistOp<scalar_t, accreal>(scalar_cast<scalar_t>(1)));
-  } else if (THCNumerics<accreal>::eq(value, scalar_cast<accreal>(0))) {
-    result = thrust::inner_product(
-#if CUDA_VERSION >= 7000 || defined __HIP_PLATFORM_HCC__
-      thrust::cuda::par(thrustAlloc).on(THCState_getCurrentStream(state)),
-#endif
-      self_data, self_data+size, src_data, scalar_cast<accreal>(0),
-      thrust::plus<accreal>(),
-      ThrustTensorDistOp<scalar_t, accreal>(scalar_cast<scalar_t>(0)));
-  } else {
-    result = thrust::inner_product(
-#if CUDA_VERSION >= 7000 || defined __HIP_PLATFORM_HCC__
-      thrust::cuda::par(thrustAlloc).on(THCState_getCurrentStream(state)),
-#endif
-      self_data, self_data+size, src_data, scalar_cast<accreal>(0),
-      thrust::plus<accreal>(),
-      ThrustTensorDistOp<scalar_t, accreal>(value));
-
-    result = THCNumerics<accreal>::pow(result, static_cast<accreal>(1) / value);
-  }
-  THCTensor_(free)(state, src);
-  THCTensor_(free)(state, self);
-
-  return result;
-}
-
 #endif
 
 accreal THCTensor_(meanall)(THCState *state, THCTensor *self)
