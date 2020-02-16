ee931a19a27fe43b28aee966e10c244c5aa766d0
Boyuan Chen <boyuanchen@berkeley.edu>
2020-02-16 13:50:06 -0800

    add cdf to mixture of same family

--- torch/distributions/mixture_same_family.py
+++ torch/distributions/mixture_same_family.py
@@ -135,12 +135,19 @@ class MixtureSameFamily(Distribution):
                                   dim=-1 - self._event_ndims)
         return mean_cond_var + var_cond_mean
 
+    def cdf(self, x):
+        x = self._pad(x)
+        cdf_x = self.component_distribution.cdf(x)
+        mix_prob = self.mixture_distribution.probs
+
+        return torch.sum(cdf_x * mix_prob[None], dim=-1)
+
     def log_prob(self, x):
         x = self._pad(x)
         log_prob_x = self.component_distribution.log_prob(x)  # [S, B, k]
         log_mix_prob = torch.log_softmax(self.mixture_distribution.logits,
                                          dim=-1)  # [B, k]
-        return torch.logsumexp(log_prob_x + log_mix_prob, dim=-1)  # [S, B]
+        return torch.logsumexp(log_prob_x + log_mix_prob[None], dim=-1)  # [S, B]
 
     def sample(self, sample_shape=torch.Size()):
         with torch.no_grad():
