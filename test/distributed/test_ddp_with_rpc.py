4f768bd830850e15ef21e3822c78fe5cf3510cb7
HC Zhu <hczhu.mtv@gmail.com>
2020-02-18 14:02:09 -0800

    [RFC] Add trainer and remote worker setup

--- test/distributed/test_ddp_with_rpc.py
+++ test/distributed/test_ddp_with_rpc.py
@@ -1,9 +1,13 @@
+from torch.distributed import rpc
+from torch.distributed.optim import DistributedOptimizer
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.testing._internal.common_utils import TestCase, run_tests
 import torch
 import torch.distributed as dist
 import torch.nn as nn
-from torch.distributed import rpc
 
-from torch.nn.parallel import DistributedDataParallel as DDP
+from torch._utils_internal import TEST_MASTER_ADDR as MASTER_ADDR
+from torch._utils_internal import TEST_MASTER_PORT as MASTER_PORT
 
 
 def _remote_method(method, rref, *args, **kwargs):
@@ -31,3 +35,40 @@ class DdpModelWithRpc(nn.Module):
         x = self.net1(x)
         x = _remote_method(SimpleNet.forward, self.rref, x)
         return self.net2(x)
+
+class TestDdpWithRpc(TestCase):
+    TRAINER_NAME_TEMPLATE = 'trainer.{:02d}'
+    REMOTE_WORKER_NAME = 'remote_worker'
+    TRAINER_GROUP = 'trainer_group'
+    NUM_TRAINERS = 4
+
+    def setUp(self):
+        super(TestDdpWithRpc, self).setUp()
+        self.model = DdpModelWithRpc()
+        os.environ["MASTER_ADDR"] = str(MASTER_ADDR)
+        os.environ["MASTER_PORT"] = str(MASTER_PORT)
+        # os.environ["WORLD_SIZE"] = str(self.NUM_TRAINERS + 1)
+
+
+    def run_trainer(self, rank):
+        trainer_name = self.TRAINER_NAME_TEMPLATE.format(rank)
+        #  For DDP communications
+        dist.init_process_group("gloo", group_name=self.TRAINER_GROUP, rank=rank, world_size=self.NUM_TRAINERS)
+        # This group includes the remote worker
+        rpc.init_rpc(
+            name=trainer_name,
+            rank=rank,
+            world_size=self.NUM_TRAINERS + 1,
+        )
+
+    def run_remote_worker(self):
+        # This group includes the remote worker
+        rpc.init_rpc(
+            name=self.REMOTE_WORKER_NAME,
+            rank=self.NUM_TRAINERS,
+            world_size=self.NUM_TRAINERS + 1,
+        )
+
+
+if __name__ == "__main__":
+    print("Running as main")
