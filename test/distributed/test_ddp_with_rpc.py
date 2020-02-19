ee4b154c9132506fe209faa5860a49d19bcf8d5b
HC Zhu <hczhu.mtv@gmail.com>
2020-02-19 09:56:31 -0800

    Make a RPC test case work

--- test/distributed/test_ddp_with_rpc.py
+++ test/distributed/test_ddp_with_rpc.py
@@ -1,8 +1,7 @@
+import logging
 import multiprocessing
 import os
 import sys
-import logging
-import time
 
 import torch.distributed.autograd as dist_autograd
 from torch.distributed import rpc
@@ -17,41 +16,66 @@ from torch._utils_internal import TEST_MASTER_ADDR as MASTER_ADDR
 from torch._utils_internal import TEST_MASTER_PORT as MASTER_PORT
 
 
+def init_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s %(filename)s:%(lineno)s %(levelname)s: %(message)s"
+    )
+    console.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(console)
+    logger.info("Set up a logger.")
+    return logger
+
+
+gLogger = init_logger()
+
+
 def _call_method(method, rref, *args, **kwargs):
     return method(rref.local_value(), *args, **kwargs)
 
 
 def _remote_method(method, rref, *args, **kwargs):
-    args = [method, rref] + list(args)
-    return rpc.rpc_sync(rref.owner(), _call_method, args=args, kwargs=kwargs)
+    args_tup = tuple([method, rref] + list(args))
+    return rpc.rpc_sync(
+        rref.owner(), _call_method, args=args_tup, kwargs=kwargs
+    )
 
 
 class SimpleNet(nn.Module):
     def __init__(self, d_in, d_out):
+        gLogger.info(f"Initing SimpleNet with {d_in} {d_out}")
         super(SimpleNet, self).__init__()
         self.net = nn.Linear(d_in, d_out)
         self.relu = nn.ReLU()
 
     def forward(self, input):
-        print("Running simple net: {input}")
+        gLogger.info(f"Running SimpleNet.forward on: {input}")
         return self.relu(self.net(input))
 
 
 class DdpModelWithRpc(nn.Module):
-    def __init__(self, remote_server, process_group_name):
+    def __init__(self, remote_server, process_group_name=None):
+        ddp_transform = (
+            lambda net, process_group_name: net if process_group_name is None else DDP
+        )
         super(DdpModelWithRpc, self).__init__()
-        self.net1 = DDP(SimpleNet(5, 8), process_group=process_group_name)
-        self.rref = rpc.remote(remote_server, SimpleNet, 8, 5)
-        self.net2 = DDP(SimpleNet(5, 3), process_group=process_group_name)
+        self.net1 = ddp_transform(SimpleNet(4, 3), process_group_name)
+        self.rref = rpc.remote(remote_server, SimpleNet, args=(3, 2))
+        self.net2 = ddp_transform(SimpleNet(2, 1), process_group_name)
 
     def forward(self, x):
+        gLogger.info(f"Running DdpModelWithRpc.forward on {x}")
         x = self.net1(x)
         x = _remote_method(SimpleNet.forward, self.rref, x)
         return self.net2(x)
 
 
 class TestDdpWithRpc(TestCase):
-    TRAINER_NAME_TEMPLATE = "trainer:{:02d}"
+    TRAINER_NAME_TEMPLATE = "trainer{:02d}"
     REMOTE_WORKER_NAME = "remote_worker"
     TRAINER_GROUP = "trainer_group"
     NUM_TRAINERS = 3
@@ -72,6 +96,7 @@ class TestDdpWithRpc(TestCase):
         for p in self.processes:
             p.join()
 
+    # TODO: make it work
     def run_one_mini_batch(self):
         with dist_autograd.context() as context_id:
             # Forward pass (create references on remote nodes).
@@ -90,23 +115,23 @@ class TestDdpWithRpc(TestCase):
             # Run the distributed optimizer step.
             dist_optim.step()
 
-    def run_trainer(self, rank, func):
+    def run_trainer(self, rank, func, use_ddp=True):
         trainer_name = self.TRAINER_NAME_TEMPLATE.format(rank)
-        print(f"Starting trainer: {trainer_name}...")
-        #  For DDP communications
-        dist.init_process_group(
-            # TODO: test other rpc backend.
-            "gloo",
-            group_name=self.TRAINER_GROUP,
-            rank=rank,
-            world_size=self.NUM_TRAINERS,
-        )
+        gLogger.info(f"Starting trainer: {trainer_name}...")
+        if use_ddp:
+            dist.init_process_group(
+                # TODO: test other rpc backend.
+                "gloo",
+                group_name=self.TRAINER_GROUP,
+                rank=rank,
+                world_size=self.NUM_TRAINERS,
+            )
         # This group includes the remote worker
         rpc.init_rpc(
             name=trainer_name, rank=rank, world_size=self.NUM_TRAINERS + 1
         )
         self.model = DdpModelWithRpc(
-            self.REMOTE_WORKER_NAME, self.TRAINER_GROUP
+            self.REMOTE_WORKER_NAME, self.TRAINER_GROUP if use_ddp else None
         )
         func()
         rpc.shutdown()
@@ -122,15 +147,12 @@ class TestDdpWithRpc(TestCase):
         )
         rpc.shutdown()
 
-    def spawn_processes(self):
-        def trainer_func():
-            print(f"Forward pass on {self.model(torch.randn((3, 5)))}")
-
+    def spawn_processes(self, func, use_ddp=True):
         for rank in range(self.NUM_TRAINERS):
             process = multiprocessing.Process(
                 target=self.run_trainer,
                 name=self.TRAINER_NAME_TEMPLATE.format(rank),
-                args=(rank, trainer_func),
+                args=(rank, func, use_ddp),
             )
             process.start()
             self.processes.append(process)
@@ -140,21 +162,14 @@ class TestDdpWithRpc(TestCase):
         )
         remote_worker_process.start()
         self.processes.append(remote_worker_process)
-        # time.sleep(30)
 
-    def test_forward_pass(self):
-        self.spawn_processes()
+    def test_rpc(self):
+        def run_one_forward():
+            gLogger.info(f"Forward pass on {self.model(torch.randn((2, 4)))}")
+
+        self.spawn_processes(run_one_forward, False)
         self.join_processes()
 
 
-def init_logger():
-    FORMAT = "%(asctime)s %(filename)s:%(lineno)s %(levelname)s: %(message)s"
-    logging.basicConfig(format=FORMAT, stream=sys.stderr, level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    print("Got a logger.")
-
-
 if __name__ == "__main__":
-    init_logger()
     run_tests()
