48af69cfd716293b535dca7f65a1ac2810429476
Will Feng <willfeng@fb.com>
2020-02-18 23:33:18 -0500

    use handle_simple_type lambda

--- torch/csrc/autograd/python_variable_indexing.cpp
+++ torch/csrc/autograd/python_variable_indexing.cpp
@@ -252,23 +252,27 @@ PyObject* THPVariable_getitem(PyObject* self, PyObject* index) {
     return THPVariable_NewWithVar((PyTypeObject *)THPVariableClass, std::move(var));
   };
 
+  auto handle_simple_type = [&](at::indexing::TensorIndex&& tensor_index) {
+    return wrap_var(std::move(at::indexing::get_item(self_, {std::move(tensor_index)}, is_tracing)));
+  };
+
   // handle simple types: integers, slices, none, ellipsis
   if (THPUtils_checkLong(index)) {
     if (is_tracing && THPVariable_Check(index)) {
       recordSelectTrace(THPVariable_Unpack(index));
     }
-    return wrap_var(std::move(at::indexing::get_item(self_, {at::indexing::TensorIndex(THPUtils_unpackLong(index))}, is_tracing)));
+    return handle_simple_type(at::indexing::TensorIndex(THPUtils_unpackLong(index)));
   } else if (PySlice_Check(index)) {
     Py_ssize_t start, stop, step;
     checkUnpackSlice(index, &start, &stop, &step);
     if (is_tracing) {
       recordSliceTrace(index);
     }
-    return wrap_var(std::move(at::indexing::get_item(self_, {at::indexing::TensorIndex({start, stop, step})}, is_tracing)));
+    return handle_simple_type(at::indexing::TensorIndex({start, stop, step}));
   } else if (index == Py_None) {
-    return wrap_var(std::move(at::indexing::get_item(self_, {at::indexing::TensorIndex(at::indexing::None)}, is_tracing)));
+    return handle_simple_type(at::indexing::TensorIndex(at::indexing::None));
   } else if (index == Py_Ellipsis) {
-    return wrap_var(std::move(at::indexing::get_item(self_, {at::indexing::TensorIndex(at::indexing::Ellipsis)}, is_tracing)));
+    return handle_simple_type(at::indexing::TensorIndex(at::indexing::Ellipsis));
   }
 
   // wrap index in a tuple if it's not already one
