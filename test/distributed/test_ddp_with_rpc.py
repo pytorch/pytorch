7069f5d44a312b8db99132fe9f46e655a613d596
HC Zhu <hczhu.mtv@gmail.com>
2020-02-18 18:18:53 -0800

    Tried to make it work, but there is a master port conflict.

--- test/distributed/test_ddp_with_rpc.py
+++ test/distributed/test_ddp_with_rpc.py
@@ -1,5 +1,8 @@
 import multiprocessing
 import os
+import sys
+import logging
+import time
 
 import torch.distributed.autograd as dist_autograd
 from torch.distributed import rpc
@@ -30,6 +33,7 @@ class SimpleNet(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, input):
+        print("Running simple net: {input}")
         return self.relu(self.net(input))
 
 
@@ -37,7 +41,7 @@ class DdpModelWithRpc(nn.Module):
     def __init__(self, remote_server, process_group_name):
         super(DdpModelWithRpc, self).__init__()
         self.net1 = DDP(SimpleNet(5, 8), process_group=process_group_name)
-        self.rref = rpc.remote(remote_server, 8, 5)
+        self.rref = rpc.remote(remote_server, SimpleNet, 8, 5)
         self.net2 = DDP(SimpleNet(5, 3), process_group=process_group_name)
 
     def forward(self, x):
@@ -47,22 +51,26 @@ class DdpModelWithRpc(nn.Module):
 
 
 class TestDdpWithRpc(TestCase):
-    TRAINER_NAME_TEMPLATE = "trainer.{:02d}"
+    TRAINER_NAME_TEMPLATE = "trainer:{:02d}"
     REMOTE_WORKER_NAME = "remote_worker"
     TRAINER_GROUP = "trainer_group"
-    NUM_TRAINERS = 4
+    NUM_TRAINERS = 3
 
     def setUp(self):
         super(TestDdpWithRpc, self).setUp()
         self.processes = []
         os.environ["MASTER_ADDR"] = str(MASTER_ADDR)
         os.environ["MASTER_PORT"] = str(MASTER_PORT)
-        # os.environ["WORLD_SIZE"] = str(self.NUM_TRAINERS + 1)
+        os.environ["WORLD_SIZE"] = str(self.NUM_TRAINERS + 1)
 
     def tearDown(self):
         super(TestDdpWithRpc, self).tearDown()
         for p in self.processes:
             p.terminate()
+
+    def join_processes(self):
+        for p in self.processes:
+            p.join()
 
     def run_one_mini_batch(self):
         with dist_autograd.context() as context_id:
@@ -84,6 +92,7 @@ class TestDdpWithRpc(TestCase):
 
     def run_trainer(self, rank, func):
         trainer_name = self.TRAINER_NAME_TEMPLATE.format(rank)
+        print(f"Starting trainer: {trainer_name}...")
         #  For DDP communications
         dist.init_process_group(
             # TODO: test other rpc backend.
@@ -100,23 +109,22 @@ class TestDdpWithRpc(TestCase):
             self.REMOTE_WORKER_NAME, self.TRAINER_GROUP
         )
         func()
-        self.clean_up()
-
-    def clean_up(self):
-        dist.destroy_process_group()
         rpc.shutdown()
+        dist.destroy_process_group()
 
     def run_remote_worker(self):
+        print(f"Starting the remote worker...")
         # This group includes the remote worker
         rpc.init_rpc(
             name=self.REMOTE_WORKER_NAME,
             rank=self.NUM_TRAINERS,
             world_size=self.NUM_TRAINERS + 1,
         )
+        rpc.shutdown()
 
     def spawn_processes(self):
         def trainer_func():
-            print(self.model(torch.randn((3, 5))))
+            print(f"Forward pass on {self.model(torch.randn((3, 5)))}")
 
         for rank in range(self.NUM_TRAINERS):
             process = multiprocessing.Process(
@@ -126,14 +134,27 @@ class TestDdpWithRpc(TestCase):
             )
             process.start()
             self.processes.append(process)
+
         remote_worker_process = multiprocessing.Process(
             target=self.run_remote_worker, name=self.REMOTE_WORKER_NAME
         )
+        remote_worker_process.start()
         self.processes.append(remote_worker_process)
+        # time.sleep(30)
 
     def test_forward_pass(self):
         self.spawn_processes()
+        self.join_processes()
+
+
+def init_logger():
+    FORMAT = "%(asctime)s %(filename)s:%(lineno)s %(levelname)s: %(message)s"
+    logging.basicConfig(format=FORMAT, stream=sys.stderr, level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    print("Got a logger.")
 
 
 if __name__ == "__main__":
+    init_logger()
     run_tests()
