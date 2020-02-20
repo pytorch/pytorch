e591d5a114133039eeb7523968315568fa5f962d
Gregory Chanan <gchanan@fb.com>
2020-02-20 11:46:40 -0800

    Minimize the cases where we have to cpu_zero.
    In this PR, we are a bit more careful about avoiding zero-ing the output.  Analysis as follows:
    1) `mm` doesn't need zero_ because it never calls scal, which is the underlying problem.
    2) for `mv`, which does call scal (in certain cases), we can just move the zeroing to where it would actually be a problem, namely when the scalar value is 0.
    In this case we just run the non-BLAS version of the code.

--- aten/src/ATen/native/BlasWrappersCPU.cpp
+++ aten/src/ATen/native/BlasWrappersCPU.cpp
@@ -9,9 +9,6 @@ namespace native {
 
 Tensor & mv_cpu_out(Tensor & result, const Tensor & self, const Tensor & vec) {
   result.resize_({ self.size(0) });
-  // we likely don't need to do this, see [NOTE: cpu_zero].
-  // We should do a full accounting that all cases are handled correctly, without it, though.
-  result.zero_();
   return legacy::cpu::_th_addmv_out(result, result, self, vec, 0, 1);
 }
 
@@ -22,9 +19,6 @@ Tensor mv_cpu(const Tensor & self, const Tensor & vec) {
 
 Tensor & mm_cpu_out(Tensor & result, const Tensor & self, const Tensor & mat2) {
   result.resize_({ self.size(0), mat2.size(1) });
-  // we likely don't need to do this, see [NOTE: cpu_zero].
-  // We should do a full accounting that all cases are handled correctly, without it, though.
-  result.zero_();
   return legacy::cpu::_th_addmm_out(result, result, self, mat2, 0, 1);
 }
 
