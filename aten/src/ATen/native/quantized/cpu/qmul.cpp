d498588cf397b660d3bfd9284ed414583d731750
Lingyi Liu <lingyiliu@fb.com>
2020-02-19 16:50:34 -0800

    Support broadcast for quantized mul kernel (#30442)
    Summary:
    Since the tensor iterator supports the broadcast, we will just remove the assertion on input shapes.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30442
    
    Differential Revision: D19976562
    
    Pulled By: lly-zero-one
    
    fbshipit-source-id: 91b27fc8b2570f29d110c6df26eacdd16f587b9f

--- aten/src/ATen/native/quantized/cpu/qmul.cpp
+++ aten/src/ATen/native/quantized/cpu/qmul.cpp
@@ -14,12 +14,10 @@ namespace {
 inline void check_inputs(const Tensor& qa, const Tensor& qb) {
   TORCH_CHECK(qa.qscheme() == kPerTensorAffine,
               "Only per tensor quantization is supported in Mul.");
-  TORCH_CHECK(qa.qscheme() == qb.qscheme(),
-              "Both inputs to Mul must have the same quantization scheme.");
-  TORCH_CHECK(qa.numel() == qb.numel(),
-              "Mul operands must be the same size!");
   TORCH_CHECK(qa.scalar_type() == qb.scalar_type(),
               "Mul operands should have same data type.");
+  TORCH_CHECK(qa.qscheme() == qb.qscheme(),
+              "Both inputs to Mul must have the same quantization shceme.");
 }
 
 // Note: out is assumed to be the same size as self and other.
