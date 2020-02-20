1fba66198b8d20f1182ef1333df3be40203eff57
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-02-20 13:39:04 -0800

    update implementation to take into account ignore_index

--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -15,5 +15,35 @@ def einsum(g, equation, tensor_list):
     return g.op("Einsum", *tensors, equation_s=equation)
 
 @parse_args('s', 'v', 'v')
-def crossentropyloss(g, reduction, input, target):
-    return g.op("SoftmaxCrossEntropyLoss", input, target, reduction=reduction)
+def crossentropyloss(g, input, target, weight, reduction, ignore_index):
+    
+    loss = g.op("SoftmaxCrossEntropyLoss", input, target, reduction_s='none')
+    if not sym_help._is_none(weight):
+        loss = g.op("SoftmaxCrossEntropyLoss", input, target, weight, reduction_s='none')
+
+    # when ignore_index is not specified, ignore_index == onnx::Constant[value={-100}]
+    if sym_help._maybe_get_const(ignore_index, 'i') == -100:
+        return loss
+
+    # if ignore_index
+    zeros = zeros_like(g, loss)
+    ignored_mask = eq(g, target, ignore_index)
+    loss = where(g, ignored_mask, zeros, loss)
+
+    if reduction == 'sum' or reduction == 'mean':
+        zeros = zeros_like(g, target)
+        ones = ones_like(g, target)
+        nb_elem = where(g, ignored_mask, zeros, ones)
+        if not sym_help._is_none(weight):
+            weight = index_select(g, weight_flattened, 0, target)
+            weight = reshape_as(g, weight, target)
+
+            nb_elem = g.op("Mul", nb_elem, weight)
+        
+        nb_elem = g.op("ReduceSum", nb_elem)
+        loss = g.op("ReduceSum", nllloss)
+
+        if reduction == 'mean':
+            loss = g.op("Div", nllloss, nb_elem)
+
+    return loss
