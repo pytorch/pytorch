e4d6a61c29f7fbb789849cf8e79f11d0b74a397f
Martin Yuan <iseeyuan@yahoo.com>
2020-02-20 15:03:40 -0800

    Add instructions and operators for new bytecode format of PyText model (#33555)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33555
    
    A quick fix for the PyText model (in internal production) on the new bytecode format.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20008266
    
    Pulled By: iseeyuan
    
    fbshipit-source-id: 1916bd0bf41093898713c567c7f6fa546b9ea440

--- torch/csrc/jit/mobile/register_mobile_ops.cpp
+++ torch/csrc/jit/mobile/register_mobile_ops.cpp
@@ -8,6 +8,7 @@ using torch::jit::drop;
 using torch::jit::pack;
 using torch::jit::push;
 using torch::jit::pop;
+using torch::jit::last;
 using at::Tensor;
 using at::Scalar;
 
@@ -130,6 +131,37 @@ void upsample_nearest2d_kernel(const c10::OperatorHandle& op, Stack* stack) {
 void warn_kernel(const c10::OperatorHandle& op, Stack* stack) {
   drop(*stack, 1);
   pop(*stack);
+}
+
+void tupleunpack_kernel(const c10::OperatorHandle& op, Stack* stack) {
+  auto tuple = pop(*stack).toTuple();
+  stack->insert(
+     stack->end(),
+     tuple->elements().begin(),
+     tuple->elements().end());
+}
+
+void format_kernel(const c10::OperatorHandle& op, Stack* stack) {
+  size_t num_inputs = pop(*stack).toInt();
+  auto format = peek(*stack, 0, num_inputs).toStringRef();
+  auto args = last(*stack, num_inputs - 1);
+  std::stringstream ss;
+  for (size_t begin = 0, used_args = 0; true; ++used_args) {
+    size_t loc = format.find("{}", begin);
+    if (loc == std::string::npos) {
+      ss << format.substr(begin);
+      break;
+    }
+    ss << format.substr(begin, loc - begin);
+    if (used_args >= args.size()) {
+      AT_ERROR("Too few arguments for format string: ", format);
+    }
+    ss << args[used_args];
+    begin = loc + 2;
+  }
+
+  drop(*stack, num_inputs);
+  push(*stack, ss.str());
 }
 
 template <typename T>
@@ -411,15 +443,12 @@ static auto registry = torch::RegisterOperators().op(
   []() {
   })
 ).op(
-  "_prim::TupleUnpack",
-  torch::RegisterOperators::options().catchAllKernel(
-  []() {
-  })
+  "_prim::TupleUnpack(any self) -> any",
+  torch::RegisterOperators::options().catchAllKernel<&tupleunpack_kernel>()
 ).op(
-  "_aten::format",
-  torch::RegisterOperators::options().catchAllKernel(
-  []() {
-  })
+  "_aten::format(str self, ...) -> str",
+  torch::RegisterOperators::options().catchAllKernel<&format_kernel>()
+      .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA)
 ).op(
   "_aten::append.Tensor(Tensor self) -> void",
   torch::RegisterOperators::options().kernel<&listAppend<at::Tensor>>(c10::DispatchKey::CPUTensorId)
