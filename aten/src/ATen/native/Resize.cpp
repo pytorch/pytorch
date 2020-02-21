22963f42eccf0b1dc9cb356b9bae0d1b66e871fe
Edward Yang <ezyang@fb.com>
2020-02-21 14:22:02 -0800

    Delete unnecessary aliasAnalysis specification from operator registrations. (#33093)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33093
    
    In #30187 the aliasAnalysis field on operator registration was updated
    so that alias analysis could be specified in only some registration call
    sites, rather than requiring it be consistently specified in all call
    sites.  With this change, we can eliminate the requirement that all
    registrations specify aliasAnalysis; as long as we know *one* site
    specifies the correct aliasAnalysis, we don't have to specify it
    any of the other sites.
    
    In this patch, the "one site" is TypeDefault.cpp (previously we only
    generated these stub declarations for manually registered functions,
    but now we generate the stubs for everything).  Then I delete aliasAnalysis
    anywhere we register an op for an existing function (which is a lot
    of places).
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19837897
    
    Pulled By: ezyang
    
    fbshipit-source-id: 26a7fbc809ec1553da89ea5c0361f3e81526d4c2

--- aten/src/ATen/native/Resize.cpp
+++ aten/src/ATen/native/Resize.cpp
@@ -82,12 +82,10 @@ Tensor& resize_(
 static auto registry = torch::RegisterOperators()
   .op(torch::RegisterOperators::options()
     .schema("aten::resize_(Tensor(a!) self, int[] size, *, MemoryFormat? memory_format=None) -> Tensor(a!)")
-    .impl_unboxedOnlyKernel<decltype(resize_), &resize_>(DispatchKey::CPUTensorId)
-    .aliasAnalysis(AliasAnalysisKind::FROM_SCHEMA))
+    .impl_unboxedOnlyKernel<decltype(resize_), &resize_>(DispatchKey::CPUTensorId))
   .op(torch::RegisterOperators::options()
     .schema("aten::resize_as_(Tensor(a!) self, Tensor the_template, *, MemoryFormat? memory_format=None) -> Tensor(a!)")
-    .impl_unboxedOnlyCatchAllKernel<decltype(resize_as_), &resize_as_>()
-    .aliasAnalysis(AliasAnalysisKind::FROM_SCHEMA))
+    .impl_unboxedOnlyCatchAllKernel<decltype(resize_as_), &resize_as_>())
   ;
 
 } // namespace native
--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -183,15 +183,13 @@ ${return_type} ${api_name}(${type_method_formals}) {
 UNBOXEDONLY_WRAPPER_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
   .schema("${schema_string}")
-  .impl_unboxedOnlyKernel<${return_type} (${formal_types}), &VariableType::${api_name}>(DispatchKey::VariableTensorId)
-  .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+  .impl_unboxedOnlyKernel<${return_type} (${formal_types}), &VariableType::${api_name}>(DispatchKey::VariableTensorId))
 """)
 
 WRAPPER_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
   .schema("${schema_string}")
-  .kernel<${return_type} (${formal_types})>(DispatchKey::VariableTensorId, &VariableType::${api_name})
-  .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+  .kernel<${return_type} (${formal_types})>(DispatchKey::VariableTensorId, &VariableType::${api_name}))
 """)
 
 UNPACK_TENSOR = CodeTemplate("""\
