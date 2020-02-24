bd04f693f7c09ed004b866002770a9370e070cbb
Gregory Chanan <gchanan@fb.com>
2020-02-24 07:43:41 -0800

    Fix NaN handling in torch.mv. (#31666)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31666
    
    List of changes:
    1) Fix a case where torch.mv was not handling NaNs correctly.  In particular, with a transposed tensor and expanded vector, NaNs in the output are kept, even if beta = 0.
    This is handled in the `out=` case by zero-ing out the passed-in Tensor, but this can happen just the same with the non-out variant if the allocated tensor happens to have a NaN.
    Also adds tests for this case.
    NOTE: we zero out the output tensor in all cases for mv and mm, even though this is probably overkill.  I didn't find another case where this would be a problem, but the old code at least
    attempted to do this for all mv and mm calls and I didn't add comprehensive testing to be sure that it's not a problem.
    
    2) on CPU: move mv, mv_out, mm, mm_out to be direct wrappers on _th_addmv, _th_addmm, rather than having their own wrappers in Declarations.cwrap.
    Ths is to remove the magic around cpu_zero from the codegen, which simplifies the codegen and makes testing this easier.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D19239953
    
    Pulled By: gchanan
    
    fbshipit-source-id: 27d0748d215ad46d17a8684696d88f4cfd8a917e

--- aten/src/TH/generic/THBlas.cpp
+++ aten/src/TH/generic/THBlas.cpp
@@ -87,6 +87,17 @@ void THBlas_(scal)(int64_t n, scalar_t a, scalar_t *x, int64_t incx)
     int i_n = (int)n;
     int i_incx = (int)incx;
 
+    // [NOTE: cpu_zero]
+    // at least on the following version of BLAS this does not folllow the same semantics
+    // when a == 0 and there exists a NaN in the input.  Namely, the non-BLAS code below results
+    // in a value of 0, whereas this results in a value of NaN.  This is problematic because a
+    // NaN in an output tensor needs to be zero'ed explicitly through a separate mechanism.
+    // At the ATen/TH binding layer, this was via "cpu_zero", which would zero out the output
+    // tensor.  This probably isn't necessary if we avoid these calls, but I haven't done a
+    // full analysis of the code.
+    // BLAS version:
+    // [conda] blas                      1.0                         mkl
+    // [conda] mkl                       2019.4                      243
 #if defined(TH_REAL_IS_DOUBLE)
     dscal_(&i_n, &a, x, &i_incx);
 #else
