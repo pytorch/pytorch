26d1363fb16c5d53146d85887cd0676cf96f3819
Shihao Xu <shihaoxu@fb.com>
2020-02-24 21:38:07 -0800

    Add missing test launchers for JitRpcTest and JitDistAutogradTest (#32891)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32891
    
    - Add JitDistAutoGradTest into fork/spawn test launcher
    - Add JitRpcTest into fork/spawn test launcher
    
    ghstack-source-id: 98900090
    
    Test Plan:
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork
    
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:rpc_spawn
    ```
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_fork
    
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_spawn
    ```
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:rpc_fork
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:rpc_fork_thrift
    
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:rpc_spawn
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:rpc_spawn_thrift
    ```
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_fork
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_fork_thrift
    
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_spawn
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_spawn_thrift
    ```
    
    Differential Revision: D5785394
    
    fbshipit-source-id: 335a85424d22f1a83874be81a8139499c9a68ce2

--- test/distributed/rpc/jit/test_dist_autograd_spawn.py
+++ test/distributed/rpc/jit/test_dist_autograd_spawn.py
@@ -3,15 +3,17 @@ import unittest
 
 from torch.testing._internal.common_distributed import MultiProcessTestCase
 from torch.testing._internal.common_utils import TEST_WITH_ASAN, run_tests
-from torch.testing._internal.distributed.rpc.rpc_test import RpcTest
+from torch.testing._internal.distributed.rpc.jit.dist_autograd_test import (
+    JitDistAutogradTest,
+)
 
 
 @unittest.skipIf(
     TEST_WITH_ASAN, "Skip ASAN as torch + multiprocessing spawn have known issues"
 )
-class RpcTestWithSpawn(MultiProcessTestCase, RpcTest):
+class JitDistAutogradTestWithSpawn(MultiProcessTestCase, JitDistAutogradTest):
     def setUp(self):
-        super(RpcTestWithSpawn, self).setUp()
+        super(JitDistAutogradTestWithSpawn, self).setUp()
         self._spawn_processes()
 
 
