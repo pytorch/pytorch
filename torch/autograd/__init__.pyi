b3557a8f6aa48d58bab97853638cd9c91e842084
JeongUkJae <jeongukjae@gmail.com>
2020-02-26 00:28:33 +0900

    add missing imports in type stubs (torch.autograd.profiler)

--- torch/autograd/__init__.pyi
+++ torch/autograd/__init__.pyi
@@ -2,6 +2,7 @@ from typing import Any, Callable, Union, Tuple, Sequence, Optional
 from .. import Tensor
 from .grad_mode import no_grad as no_grad, enable_grad as enable_grad, \
     set_grad_enabled as set_grad_enabled
+from . import profiler
 
 # TODO make Variable and Function more precise
 class Variable:
