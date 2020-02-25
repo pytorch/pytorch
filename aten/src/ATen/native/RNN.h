e5ab41a92ae086ec51319f748b51440a6bb89b02
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-02-24 21:09:50 -0500

    Update Packed LSTM prototype

--- aten/src/ATen/native/RNN.h
+++ aten/src/ATen/native/RNN.h
@@ -7,7 +7,7 @@ namespace at { namespace native {
 
 using lstm_fn = void(*)(Tensor&, Tensor&, Tensor&, const Tensor&, TensorList, TensorList, bool, int64_t, double, bool, bool, bool, bool);
 using rnn_fn = void(*)(Tensor&, Tensor&, const Tensor&, const Tensor&, TensorList, bool, int64_t, double, bool, bool, bool);
-using lstm_packed_fn = void(*)(Tensor&, Tensor&, Tensor&, const Tensor&, const Tensor&, TensorList, TensorList, bool, int64_t, double, bool, bool);
+using lstm_packed_fn = void(*)(Tensor&, Tensor&, Tensor&, const Tensor&, const Tensor&, TensorList, TensorList, bool, int64_t, double, bool, bool, bool);
 using rnn_packed_fn = void(*)(Tensor&, Tensor&, const Tensor&, const Tensor&, const Tensor&, TensorList, bool, int64_t, double, bool, bool);
 
 DECLARE_DISPATCH(lstm_fn, lstm_cudnn_stub);
