d2595a034e01189a83776354ae1c92835dc3902f
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2020-02-25 10:33:27 -0800

    Testing with minor changes...

--- aten/src/ATen/native/UnaryOps.cpp
+++ aten/src/ATen/native/UnaryOps.cpp
@@ -35,125 +35,93 @@ namespace native {
 namespace {
   enum class TypePromotionStrategy {
     None,  // No implicit dtype promotion
-    Type1, // int8 - fp16, int16 - fp32, int32 - fp64, int64 - fp64, bool - fp16
-    Type2, // (int8, int16, int32, int64, bool) - fp64
-    Type3, // bool - int8
-    Type4  // bool - fp16
+    IntBoolToFloats, // int8 - fp16, int16 - fp32, int32 - fp64, int64 - fp64, bool - fp16
+    IntBoolToFloat64, // (int8, int16, int32, int64, bool) - fp64
+    BoolToInt8, // bool - int8
+    BoolToFloat16  // bool - fp16
   };
 
-  // promoteToFloatType[1/2/3/4] functions are helper functions to support input dtype to Float implicit promotions based on NumPy's conversion rules
+  // promote* functions are helper functions to support input dtype to Float implicit promotions based on NumPy's conversion rules
   // For discussion, check https://github.com/pytorch/pytorch/pull/33322 and https://github.com/pytorch/pytorch/issues/28703
   // There are 4 type of dtype promotions in NumPy: (float16 is replaced by float32 for CPU devices)
-  // Type - 1: int8 - float16, int16 - float32, int32 - float64, int64 - float64, bool - float16
-  // Type - 2: (int8, int16, int32, int64, bool) - float64
-  // Type - 3: bool - int8
-  // Type - 4: bool - float16
-  static inline ScalarType promoteToFloatType1(const Tensor& self) {
-    // This promotes dtype based on Type 1 strategy
-    ScalarType dtype;
-
+  // IntBoolToFloats: int8 - float16, int16 - float32, int32 - float64, int64 - float64, bool - float16
+  // IntBoolToFloat64: (int8, int16, int32, int64, bool) - float64
+  // BoolToInt8: bool - int8
+  // BoolToFloat16: bool - float16
+  static inline ScalarType promoteIntBoolToFloats(const Tensor& self) {
+    // This promotes dtype based on IntBoolToFloats strategy
     switch(self.scalar_type()) {
       case kChar:
-        dtype = (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
-        break;
+        return (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
       case kShort:
-        dtype = kFloat;
-        break;
+        return kFloat;
       case kInt:
-        dtype = kDouble;
-        break;
+        return kDouble;
       case kLong:
-        dtype = kDouble;
-        break;
+        return kDouble;
       case kBool:
-        dtype = (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
-        break;
+        return (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
       default:
-        dtype = ScalarType::Undefined;
+        return ScalarType::Undefined;
     }
-
-    return dtype;
   }
 
-  static inline ScalarType promoteToFloatType2(const Tensor& self) {
-    // This promotes dtype based on Type 2 strategy
-    ScalarType dtype;
-
+  static inline ScalarType promoteIntBoolToFloat64(const Tensor& self) {
+    // This promotes dtype based on IntBoolToFloat64 strategy
     switch(self.scalar_type()) {
       case kChar:
       case kShort:
       case kInt:
       case kLong:
       case kBool:
-        dtype = kDouble;
-        break;
+        return kDouble;
       default:
-        dtype = ScalarType::Undefined;
+        return ScalarType::Undefined;
     }
-
-    return dtype;
   }
 
-  static inline ScalarType promoteToFloatType3(const Tensor& self) {
-    // This promotes dtype based on Type 3 strategy
-    ScalarType dtype;
-
+  static inline ScalarType promoteBoolToInt8(const Tensor& self) {
+    // This promotes dtype based on BoolToInt8 strategy
     switch(self.scalar_type()) {
       case kBool:
-        dtype = kChar;
-        break;
+        return kChar;
       default:
-        dtype = ScalarType::Undefined;
+        return ScalarType::Undefined;
     }
-
-    return dtype;
   }
 
-  static inline ScalarType promoteToFloatType4(const Tensor& self) {
-    // This promotes dtype based on Type 4 strategy
-    ScalarType dtype;
-
+  static inline ScalarType promoteBoolToFloat16(const Tensor& self) {
+    // This promotes dtype based on BoolToFloat16 strategy
     switch(self.scalar_type()) {
       case kBool:
-        dtype = (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
-        break;
+        return (self.device().type() == DeviceType::CPU) ? kFloat : kHalf;
       default:
-        dtype = ScalarType::Undefined;
+        return ScalarType::Undefined;
     }
-
-    return dtype;
   }
 
   // This function returns the dtype to promote
   // typeStrategy is the type of implicit dtype promotion
   ScalarType get_promoted_dtype(const Tensor& self, TypePromotionStrategy typeStrategy) {
     // typePromotionStrategy argument defaults to TypePromotionStrategy::None (no implicit dtype upcasting)
-    // and is set to TypePromotionStrategy::Type1/Type2/Type3/Type4 depending on the type of implicit
+    // and is set to TypePromotionStrategy::IntBoolToFloats/IntBoolToFloat64/BoolToInt8/BoolToFloat16 depending on the type of implicit
     // dtype promotion
-    ScalarType promoted_dtype = ScalarType::Undefined;
-
     if (typeStrategy != TypePromotionStrategy::None) {
       // This enables int-to-float implicit dtype conversions
       switch(typeStrategy) {
-        case TypePromotionStrategy::Type1:
-          promoted_dtype = promoteToFloatType1(self);
-          break;
-        case TypePromotionStrategy::Type2:
-          promoted_dtype = promoteToFloatType2(self);
-          break;
-        case TypePromotionStrategy::Type3:
-          promoted_dtype = promoteToFloatType3(self);
-          break;
-        case TypePromotionStrategy::Type4:
-          promoted_dtype = promoteToFloatType4(self);
-          break;
+        case TypePromotionStrategy::IntBoolToFloats:
+          return promoteIntBoolToFloats(self);
+        case TypePromotionStrategy::IntBoolToFloat64:
+          return promoteIntBoolToFloat64(self);
+        case TypePromotionStrategy::BoolToInt8:
+          return promoteBoolToInt8(self);
+        case TypePromotionStrategy::BoolToFloat16:
+          return promoteBoolToFloat16(self);
         default:
           // dtype is set to Undefined if no dtype-to-float conversion
-          promoted_dtype = ScalarType::Undefined;
+          return ScalarType::Undefined;
       }
     }
-
-    return promoted_dtype;
   }
 } // end anonymous namespace
 
@@ -178,7 +146,6 @@ template <typename OutImpl>
 static inline Tensor unary_op_impl(const Tensor& self, OutImpl& out_impl, TypePromotionStrategy typeStrategy=TypePromotionStrategy::None) {
   // Calculate the dtype for type promotion
   ScalarType promoted_dtype = get_promoted_dtype(self, typeStrategy);
-
   if (promoted_dtype != ScalarType::Undefined) {
     Tensor result = at::empty({0}, self.options().dtype(promoted_dtype));
     return out_impl(result, self.to(promoted_dtype));
@@ -206,7 +173,7 @@ Tensor abs(const Tensor& self) { return unary_op_impl(self, at::abs_out); }
 Tensor& abs_(Tensor& self) { return unary_op_impl_(self, at::abs_out); }
 
 Tensor& angle_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, angle_stub); }
-Tensor angle(const Tensor& self) { return unary_op_impl(self, at::angle_out, TypePromotionStrategy::Type2); }
+Tensor angle(const Tensor& self) { return unary_op_impl(self, at::angle_out, TypePromotionStrategy::IntBoolToFloat64); }
 
 Tensor& real_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, real_stub); }
 Tensor real(const Tensor& self) { return unary_op_impl(self, at::real_out); }
@@ -215,14 +182,14 @@ Tensor& imag_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(
 Tensor imag(const Tensor& self) { return unary_op_impl(self, at::imag_out); }
 
 Tensor& conj_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, conj_stub); }
-Tensor conj(const Tensor& self) { return unary_op_impl(self, at::conj_out, TypePromotionStrategy::Type3); }
+Tensor conj(const Tensor& self) { return unary_op_impl(self, at::conj_out, TypePromotionStrategy::BoolToInt8); }
 
 Tensor& bitwise_not_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, bitwise_not_stub); }
 Tensor bitwise_not(const Tensor& self) { return unary_op_impl(self, at::bitwise_not_out); }
 Tensor& bitwise_not_(Tensor& self) { return unary_op_impl_(self, at::bitwise_not_out); }
 
 Tensor& ceil_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, ceil_stub); }
-Tensor ceil(const Tensor& self) { return unary_op_impl(self, at::ceil_out, TypePromotionStrategy::Type1); }
+Tensor ceil(const Tensor& self) { return unary_op_impl(self, at::ceil_out, TypePromotionStrategy::IntBoolToFloats); }
 Tensor& ceil_(Tensor& self) { return unary_op_impl_(self, at::ceil_out); }
 
 Tensor& expm1_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, expm1_stub); }
@@ -254,7 +221,7 @@ Tensor log2(const Tensor& self) { return unary_op_impl(self, at::log2_out); }
 Tensor& log2_(Tensor& self) { return unary_op_impl_(self, at::log2_out); }
 
 Tensor& round_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, round_stub); }
-Tensor round(const Tensor& self) { return unary_op_impl(self, at::round_out, TypePromotionStrategy::Type4); }
+Tensor round(const Tensor& self) { return unary_op_impl(self, at::round_out, TypePromotionStrategy::BoolToFloat16); }
 Tensor& round_(Tensor& self) { return unary_op_impl_(self, at::round_out); }
 
 Tensor& digamma_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, digamma_stub); }
