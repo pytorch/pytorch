bde545361e72b7e3f50be83c795898ea1b884a29
Shihao Xu <shihaoxu@fb.com>
2020-02-26 22:22:52 -0800

    [JIT] Support calling Tensor.element_size() in TorchScript (#33808)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33808
    
    # Problem
    
    https://github.com/pytorch/pytorch/issues/33620
    ghstack-source-id: 99073701
    
    Test Plan:
    ```
    buck test mode/dev-nosan //caffe2/test:jit -- test_numel
    
    buck test mode/dev-nosan //caffe2/test:jit -- test_element_size
    
    buck build mode/dev-nosan //caffe2/test:jit \
    && buck-out/gen/caffe2/test/jit\#binary.par -r test_numel
    
    buck build mode/dev-nosan //caffe2/test:jit \
    && buck-out/gen/caffe2/test/jit\#binary.par -r test_element_size
    ```
    
    Compile error
    
    P126667043
    
    Generated code,
    ```
    buck-out/dev/gen/caffe2/generate-code=register_aten_ops_0.cpp/register_aten_ops_0.cpp
    
    buck-out/dev/gen/caffe2/generate-code=register_aten_ops_2.cpp/register_aten_ops_2.cpp
    ```
    P126667064
    
    Differential Revision: D7050644
    
    fbshipit-source-id: 20dbdb9c500b6d7683c23e3049d43ed0ca06d831

--- test/test_jit.py
+++ test/test_jit.py
@@ -697,6 +697,24 @@ class TestJit(JitTestCase):
 
         self.assertTrue(check(x, y))
 
+    def test_numel(self):
+        @torch.jit.script
+        def get_numel_script(x):
+            return x.numel()
+
+        x = torch.rand(3, 4)
+        numel = get_numel_script(x)
+        self.assertEqual(numel, x.numel())
+
+    def test_element_size(self):
+        @torch.jit.script
+        def get_element_size_script(x):
+            return x.element_size()
+
+        x = torch.rand(3, 4)
+        element_size = get_element_size_script(x)
+        self.assertEqual(element_size, x.element_size())
+
     def test_index(self):
         x = torch.tensor([0.4], requires_grad=True)
         y = torch.tensor([0], dtype=torch.int64)
