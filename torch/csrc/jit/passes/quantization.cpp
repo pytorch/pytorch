c2852f53d871b329dc691cca7d7a5784545b2487
Jerry Zhang <jerryzh@fb.com>
2020-02-27 12:16:33 -0800

    [quant][graphmode] Swap dequantize after inline for ops that doesn't require observation (#33173)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33173
    
    How to deal with ops that’s defined for both floating point and quantized Tensor?
    
    Category of ops: the ones that doesn’t require observers, which means the quantization parameters(scale/zero_point) of the output of this op can be inferred from the quantization parameters of inputs.
    For example:
    avg_pool, max_pool, flatten, transpose, upsample
    
    Another related topic to previous one is how do we deal with things like adaptive_avg_pool2d that does not require to be observed and it works with quantized tensor as well? If we insert quant/dequant for them, even the quant fusion becomes a numerically changing operation because the scale/zero_point for input and output are different.
    
    Proposal
    
    We can swap the operator with dequantize whenever we see it. For example, for pattern
    Let’s say aten::general_op is defined for both floating point and quantized
    
    %r = aten::conv(...)
    %q = quantize(%r)
    %dq = dequantize(%q)
    %f = aten::general_op(%dq)
    ...
    
    We detect that all inputs of aten::general_op is produced by dequantize, we’ll first delete all the dequantize for the inputs and then insert dequantize for each use of the output of the aten::general_op, note that this should work generally for all the case we might encounter.
    
    After transformation we’ll have:
    
    %r = aten::conv(...)
    %q = quantize(%r)
    %x = aten::general_op(%q)
    %f = dequantize(%x)
    ...
    
    1. Multiple inputs
        1. We need to make sure all inputs of the aten::general_op are produced by dequantize before we do this transformation
    2. Input used by multiple operators
        1. We already did this by inserting dequantize for each use of the value
    3. Output used by multiple operators
        1. We’ll reuse the code that inserts dequantize(might need some refactor)
    
    Note that current concat does not belong to this category right now since it does not inherit quantization parameters from inputs.
    
    Test Plan:
    python test/test_jit.py
    
    Imported from OSS
    
    Differential Revision: D20123590
    
    fbshipit-source-id: de2febe1f37e4079457a23acaeccbc6d9c9e1f8a

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -128,6 +128,50 @@ bool isFunctionNode(Node* n,
   return is_quantizable;
 }
 
+// If the op doesn't require observation, return
+// the the list of input indexes that we should check to see
+// if they are observed/quantized, if so, we can say the output
+// of this op is observed/quantized as well, since for these ops we can derive
+// the quantization parameters for output given inputs
+std::vector<size_t> getGeneralOpTensorInputIndexes(Node* n) {
+  std::vector<std::string> single_input_aten_funcs = {
+    "adaptive_avg_pool2d",
+    "max_pool2d",
+    "avg_pool2d",
+    "flatten",
+    "max",
+    "min",
+    "mean",
+    // TODO: sort returns a tuple of Tensors, we have
+    // to extend the API to support that
+    // "sort",
+    "__interpolate",
+    "__upsample",
+    "__upsample_bilinear",
+    "__upsample_nearest",
+  };
+  std::vector<std::string> single_input_call_funcs = {
+    "adaptive_avg_pool2d",
+    "_max_pool2d",
+  };
+  if (isFunctionNode(
+          n,
+      // We don't have call functions
+      // after inline
+      /* call_funcs = */ single_input_call_funcs,
+      /* aten_funcs = */ {})) {
+    return {1};
+  } else if (isFunctionNode(
+                 n,
+                 // We don't have call functions
+                 // after inline
+                 /* call_funcs = */ {},
+                 /* aten_funcs = */ single_input_aten_funcs)) {
+    return {0};
+  }
+  return {};
+}
+
 bool nodeQuantizable(Node* n) {
   return isFunctionNode(
       n,
@@ -1704,6 +1748,53 @@ void ReplicateDeQuant(std::shared_ptr<Graph>& graph) {
   }
   for (Node* n : dequant_nodes_to_rewrite) {
     n->destroy();
+  }
+}
+
+// This is the pass to handle ops that does not require observation
+// for example: flatten, average_pool, upsample
+// This is called after inline and before graph execution
+void SwapDeQuant(std::shared_ptr<Graph>& graph) {
+  std::stack<Block*> blocks_to_visit;
+  blocks_to_visit.push(graph->block());
+  while (!blocks_to_visit.empty()) {
+    Block* b = blocks_to_visit.top();
+    blocks_to_visit.pop();
+    for (Node* n : b->nodes()) {
+      auto input_indexes = getGeneralOpTensorInputIndexes(n);
+      if (input_indexes.size() > 0) {
+        bool is_dequantized = true;
+        for (auto i : input_indexes) {
+          is_dequantized &= n->inputs()[i]->node()->kind() == Symbol::aten("dequantize");
+        }
+        if (!is_dequantized) {
+          continue;
+        }
+        // Delete dequantize node, we have one dequantize
+        // for each use of the value
+        for (auto i : input_indexes) {
+          auto* dequantized_val = n->inputs()[i];
+          auto* dequantize_node = dequantized_val->node();
+          TORCH_INTERNAL_ASSERT(dequantized_val->uses().size() == 1,
+                                "Expect to have one dequantize node for each use");
+          // Replace useses of dequantized_val with the input of
+          // dequantize node
+          dequantized_val->replaceAllUsesWith(dequantize_node->inputs()[0]);
+          dequantize_node->removeAllInputs();
+          dequantize_node->destroy();
+        }
+        TORCH_CHECK(n->outputs().size() == 1, "We only support dequantize swapping for ops"
+                    " with one output right now");
+        auto* output = n->output();
+        WithInsertPoint ins(n->next());
+        std::vector<Use> uses = output->uses();
+        // Insert new dequantize node for each use of the output
+        insertDeQuantCall(graph.get(), output, output, uses);
+      }
+      for (Block* subblock : n->blocks()) {
+        blocks_to_visit.push(subblock);
+      }
+    }
   }
 }
 
