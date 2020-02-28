pytorch/pytorch

commit 3c5677a6760ba8df633987263b387e2a5a881c23

Use codegen'ed unboxing wrappers (#32521)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/32521

Not all ops support the templated unboxing wrappers yet. For the ones that don't,
let's use the codegen'ed unboxing wrappers from register_aten_ops.cpp, but register
them with c10 directly instead of JIT.

The `use_c10_dispatcher` setting in `native_functions.yaml` now has a new option 'with_codegenerated_unboxing_wrapper' which means we take the codegened unboxing wrapper from register_aten_ops.cpp and stuff it into c10. This new argument is the default, 'unboxed_only' is not the default anymore. For the (very few) ops that don't support boxed dispatch yet (i.e. ops taking TensorOptions arguments), we set them to 'unboxed_only' and they follow the old behavior of having register_aten_ops.cpp register the jit op.

Next steps here are (1) to make TensorOptions work with boxed dispatch and remove the `unboxed_only` option from `use_c10_dispatcher`, so that all ops go through the new path and (2) make the new path template-only and remove codegen from it (see https://github.com/pytorch/pytorch/issues/32366).

First experiments show that
- For a small JITted model that calls add (i.e. a op with just two arguments that are both tensors) on two tensors in a loop, we see a 2-4% performance improvement (~35-50ns) when compared to the old path. This is a simple op that takes two tensor arguments and no non-tensor arguments, so iterating over it in boxed dispatch is cheap.
- For a small JITted model that calls avgpool1d (i.e. an op that has one tensor arg and 5 non-tensor args) on a tensor in a loop, we see a 3-4% performance regression (~60ns) when compared to the old path. This is an op that takes only one tensor argument and then 6 non-tensor arguments. Unboxed dispatch doesn’t have to look at those but boxed dispatch still needs to iterate over them.

tools/jit/templates/register_aten_ops.cpp
@@ -1,5 +1,7 @@
+
 #include "torch/csrc/jit/runtime/operator.h"
 #include "torch/csrc/jit/runtime/custom_operator.h"
+#include "torch/csrc/jit/frontend/function_schema_parser.h"
 
 #include "torch/csrc/autograd/profiler.h"
 #include "torch/csrc/autograd/generated/variable_factories.h"
@@ -44,8 +46,20 @@ using at::MemoryFormat;
 
 using ::c10::fmap;
 using ::c10::filter;
+using c10::OperatorKernel;
+using c10::OperatorHandle;
+using c10::KernelFunction;
+using c10::RegistrationHandleRAII;
+using c10::Stack;
 
 namespace {
+
+template<class Return, class... Args>
+Return callUnboxedKernel(OperatorKernel* unboxedKernel, Args... args) {
+  using FuncType = Return (Args...);
+  auto* typedUnboxedKernel = static_cast<c10::detail::WrapRuntimeKernelFunctor<FuncType*>*>(unboxedKernel);
+  return (*typedUnboxedKernel)(std::forward<Args>(args)...);
+}
 
 // TODO: remove the toOptionalTensor and toListOfOptionalTensor
 // when we remove the undefined tensor semantic from TH
@@ -91,43 +105,70 @@ int (*DUMMY_OPERATION)(Stack&) = [](Stack& stack) -> int {
   return 0;
 };
 
-RegisterOperators reg(
-    {Operator(
-         "aten::get_device(Tensor self) -> int",
-         [](Stack& stack) {
-           RECORD_FUNCTION("get_device", std::vector<c10::IValue>());
-           auto result =
-               at::get_device((std::move(peek(stack, 0, 1))).toTensor());
-           drop(stack, 1);
-           pack(stack, std::move(result));
-           return 0;
-         },
-         atenOperatorOptions()),
-     Operator(
-         "aten::storage_offset(Tensor self) -> int",
-         [](Stack& stack) {
-           RECORD_FUNCTION("storage_offset", std::vector<c10::IValue>());
-           auto result =
-               ((std::move(peek(stack, 0, 1))).toTensor()).storage_offset();
-           drop(stack, 1);
-           pack(stack, std::move(result));
-           return 0;
-         },
-         atenOperatorOptions()),
-     Operator(
-         "aten::is_contiguous(Tensor self) -> bool",
-         [](Stack& stack) {
-           RECORD_FUNCTION("is_contiguous", std::vector<c10::IValue>());
-           auto result =
-               ((std::move(peek(stack, 0, 1))).toTensor()).is_contiguous();
-           drop(stack, 1);
-           pack(stack, std::move(result));
-           return 0;
-         },
-         atenOperatorOptions()),
+class Registerer final {
+public:
+  Registerer&& op(const std::string& schema, KernelFunction::InternalBoxedKernelFunction* boxed_kernel_wrapper) && {
+    static auto& dispatcher = c10::Dispatcher::singleton();
+    std::pair<RegistrationHandleRAII, OperatorHandle> registration = dispatcher.registerSchema(parseSchema(schema), atenOperatorOptions());
+    registrationHandles_.push_back(std::move(registration.first));
+    dispatcher.setManuallyBoxedKernelFor_(registration.second, boxed_kernel_wrapper);
+    return std::move(*this);
+  }
 
-     // Generated operators
-     ${constructors}});
+  Registerer&& jitOnlyOp(const std::string& schema, std::function<int (Stack*)> boxed_kernel_wrapper) && {
+    torch::jit::registerOperator(
+      torch::jit::Operator(
+        schema,
+        Operation([boxed_kernel_wrapper = std::move(boxed_kernel_wrapper)] (Stack& stack) -> int {
+          return boxed_kernel_wrapper(&stack);
+        }),
+        atenOperatorOptions()
+      )
+    );
+    return std::move(*this);
+  }
+
+  Registerer() = default;
+  Registerer(const Registerer&) = delete;
+  Registerer& operator=(const Registerer&) = delete;
+  Registerer(Registerer&&) noexcept = default;
+  Registerer& operator=(Registerer&&) noexcept = default;
+private:
+  std::vector<RegistrationHandleRAII> registrationHandles_;
+};
+
+static auto registry = Registerer()
+  .jitOnlyOp("aten::get_device(Tensor self) -> int",
+    [](Stack* stack) {
+      RECORD_FUNCTION("get_device", std::vector<c10::IValue>());
+      auto result =
+          at::get_device((std::move(peek(*stack, 0, 1))).toTensor());
+      drop(*stack, 1);
+      pack(*stack, std::move(result));
+      return 0;
+    })
+  .jitOnlyOp("aten::storage_offset(Tensor self) -> int",
+    [](Stack* stack) {
+      RECORD_FUNCTION("storage_offset", std::vector<c10::IValue>());
+      auto result =
+          ((std::move(peek(*stack, 0, 1))).toTensor()).storage_offset();
+      drop(*stack, 1);
+      pack(*stack, std::move(result));
+       return 0;
+    })
+  .jitOnlyOp("aten::is_contiguous(Tensor self) -> bool",
+    [](Stack* stack) {
+      RECORD_FUNCTION("is_contiguous", std::vector<c10::IValue>());
+      auto result =
+          ((std::move(peek(*stack, 0, 1))).toTensor()).is_contiguous();
+      drop(*stack, 1);
+      pack(*stack, std::move(result));
+      return 0;
+    })
+
+  // Generated operators
+  ${constructors}
+  ;
 
 } // anon namespace
 

aten/src/ATen/core/OpsAlreadyMovedToC10.h
@@ -9,6 +9,7 @@ struct OperatorName;
 namespace at {
 
 // list of ATen ops that come from native_functions.yaml
-CAFFE2_API bool is_aten_op(const c10::OperatorName& opName);
+CAFFE2_API bool is_aten_op_and_unboxing_is_already_handled_by_c10(const c10::OperatorName& opName);
+CAFFE2_API bool is_aten_op_and_unboxing_is_not_handled_by_c10_yet(const c10::OperatorName& opName);
 
 }

aten/src/ATen/core/boxing/KernelFunction.h
@@ -209,6 +209,12 @@ public:
   template<bool AllowLegacyTypes = false, class Lambda>
   static KernelFunction makeFromUnboxedLambda(Lambda&& lambda);
 
+  // This function is a temporary hack that allows register_aten_ops.cpp to register its codegen'ed
+  // unboxing wrapper for aten operators. We still need those for some operators because not all work
+  // with the templated unboxing logic yet.
+  // TODO Delete setManuallyBoxedKernel_ once all operators work with the templated boxing logic. This can be done once https://github.com/pytorch/pytorch/issues/32366 is fixed.
+  void setManuallyBoxedKernel_(InternalBoxedKernelFunction* func);
+
 private:
 
   explicit KernelFunction(std::function<std::unique_ptr<OperatorKernel>()> functorFactory, std::unique_ptr<OperatorKernel> functor, InternalBoxedKernelFunction* boxed_kernel_func, void* unboxed_kernel_func);

aten/src/ATen/core/boxing/KernelFunction_impl.h
@@ -187,4 +187,10 @@ inline KernelFunction KernelFunction::makeFromUnboxedLambda(Lambda&& lambda) {
     );
 }
 
+inline void KernelFunction::setManuallyBoxedKernel_(InternalBoxedKernelFunction* func) {
+    TORCH_INTERNAL_ASSERT(boxed_kernel_func_ == nullptr, "Tried to set a manually boxed kernel for a kernel that already has a boxed kernel set.");
+    TORCH_INTERNAL_ASSERT(unboxed_kernel_func_ != nullptr, "Tried to set a manually boxed kernel for an invalid KernelFunction.");
+    boxed_kernel_func_ = func;
+}
+
 }

aten/src/ATen/core/dispatch/DispatchTable.h
@@ -62,6 +62,10 @@ public:
     return kernels_[static_cast<uint8_t>(dispatchKey)];
   }
 
+  KernelFunction& operator[](DispatchKey dispatchKey) {
+    return kernels_[static_cast<uint8_t>(dispatchKey)];
+  }
+
   size_t size() const {
     return kernelCount_;
   }
@@ -95,6 +99,9 @@ class DispatchTable final {
    * @param kernel Concrete kernel function implementation to register
    */
   void setKernel(DispatchKey dispatchKey, KernelFunction kernel) {
+    if (manuallyBoxedKernel_.has_value()) {
+      kernel.setManuallyBoxedKernel_(*manuallyBoxedKernel_);
+    }
     auto result = kernels_.setKernel(dispatchKey, std::move(kernel));
     dispatchKeyExtractor_.setOperatorHasKernelForBackend(dispatchKey, true);
     if (result == impl::KernelFunctionTable::SetKernelResult::OVERWROTE_EXISTING_KERNEL) {
@@ -121,6 +128,9 @@ class DispatchTable final {
   void setCatchallKernel(KernelFunction kernel) {
     if (catchallKernel_.isValid()) {
       TORCH_WARN("Registered a catch-all kernel for operator ", operatorName_," that overwrote a previously registered catch-all kernel for the same operator.");
+    }
+    if (manuallyBoxedKernel_.has_value()) {
+      kernel.setManuallyBoxedKernel_(*manuallyBoxedKernel_);
     }
     catchallKernel_ = std::move(kernel);
   }
@@ -188,12 +198,32 @@ class DispatchTable final {
     return operatorName_;
   }
 
+  // This function is a temporary hack, see comment at manuallyBoxedKernel_ member
+  void setManuallyBoxedKernel_(KernelFunction::InternalBoxedKernelFunction* func) {
+    TORCH_INTERNAL_ASSERT(!manuallyBoxedKernel_.has_value(), "Cannot set multiple manually boxed kernels for the same operator ", operatorName_);
+    manuallyBoxedKernel_ = func;
+
+    // make sure that all previously registered kernels get this manually boxed kernel
+    for (uint8_t iter = 0; iter != static_cast<uint8_t>(DispatchKey::NumDispatchKeys); ++iter) {
+      auto& kernel = kernels_[static_cast<DispatchKey>(iter)];
+      if (kernel.isValid()) {
+        kernel.setManuallyBoxedKernel_(func);
+      }
+    }
+  }
+
 private:
 
   impl::KernelFunctionTable kernels_;
   KernelFunction catchallKernel_;
   DispatchKeyExtractor dispatchKeyExtractor_;
   std::string operatorName_;
+
+  // This manuallyBoxedKernel_ member is a temporary hack that allows register_aten_ops.cpp to register its codegen'ed
+  // unboxing wrapper for aten operators. We still need those for some operators because not all work
+  // with the templated unboxing logic yet.
+  // TODO Delete manuallyBoxedKernel_ once all operators work with the templated boxing logic
+  c10::optional<KernelFunction::InternalBoxedKernelFunction*> manuallyBoxedKernel_;
 };
 
 } // namespace c10

aten/src/ATen/core/dispatch/Dispatcher.cpp
@@ -180,4 +180,17 @@ void Dispatcher::addRegistrationListener(std::unique_ptr<OpRegistrationListener>
           dispatchTable.listAllDispatchKeys(), ".");
 }
 
+void Dispatcher::setManuallyBoxedKernelFor_(const OperatorHandle& op, KernelFunction::InternalBoxedKernelFunction* func) {
+  op.operatorIterator_->op.setManuallyBoxedKernel_(func);
+}
+
+bool Dispatcher::isValid(const OperatorHandle& op) const {
+  for (auto iter = operators_.begin(); iter != operators_.end(); ++iter) {
+    if (iter == op.operatorIterator_) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }

aten/src/ATen/core/dispatch/Dispatcher.h
@@ -88,6 +88,14 @@ public:
   OperatorHandle findSchemaOrThrow(const char* name, const char* overload_name);
 
   /**
+   * Returns true, iff the given operator handle is still valid,
+   * i.e. the operator was not deregistered.
+   * Note that this function is somewhat expensive to call,
+   * so don't do it in a hotpath.
+   */
+  bool isValid(const OperatorHandle& op) const;
+
+  /**
    * Register a kernel to the dispatch table for an operator.
    * If dispatch_key is nullopt, then this registers a fallback kernel.
    *
@@ -113,6 +121,12 @@ public:
    * fallback kernel for the given dispatch key and, if yes, call that one.
    */
   RegistrationHandleRAII registerBackendFallbackKernel(DispatchKey dispatch_key, KernelFunction kernel);
+
+  // This function is a temporary hack that allows register_aten_ops.cpp to register its codegen'ed
+  // unboxing wrapper for aten operators. We still need those for some operators because not all work
+  // with the templated unboxing logic yet.
+  // TODO Delete setBoxedKernelFor_ once all operators work with the templated boxing logic
+  void setManuallyBoxedKernelFor_(const OperatorHandle& op, KernelFunction::InternalBoxedKernelFunction* func);
 
   template<class Return, class... Args>
   Return callUnboxed(const OperatorHandle& op, Args... args) const;
@@ -163,6 +177,16 @@ public:
   OperatorHandle& operator=(OperatorHandle&&) noexcept = default;
   OperatorHandle(const OperatorHandle&) = default;
   OperatorHandle& operator=(const OperatorHandle&) = default;
+
+  /**
+   * Returns true iff the operator handle is still valid,
+   * i.e. the operator was not deregistered.
+   * Note that this function is somewhat expensive to call,
+   * so don't do it in a hotpath.
+   */
+  bool isValid() const {
+    return c10::Dispatcher::singleton().isValid(*this);
+  }
 
   const FunctionSchema& schema() const {
     return operatorIterator_->op.schema();

aten/src/ATen/core/dispatch/OperatorEntry.h
@@ -44,6 +44,14 @@ public:
     options_.setAliasAnalysis(a);
   }
 
+  // This function is a temporary hack that allows register_aten_ops.cpp to register its codegen'ed
+  // unboxing wrapper for aten operators. We still need those for some operators because not all work
+  // with the templated unboxing logic yet.
+  // TODO Delete setManuallyBoxedKernel_ once all operators work with the templated boxing logic
+  void setManuallyBoxedKernel_(KernelFunction::InternalBoxedKernelFunction* func) {
+    dispatchTable_.setManuallyBoxedKernel_(func);
+  }
+
 private:
   void deregisterKernel_(DispatchKey dispatch_key, std::list<KernelFunction>::iterator kernel);
   void deregisterCatchallKernel_(std::list<KernelFunction>::iterator kernel);

commit 2a74a46c1d545420ad185ad5c23f6ac66a36608e

Fix mobile build
This was broken by https://github.com/pytorch/pytorch/pull/32521 but only showed up in master CI builds

Differential Revision: [D20172782](https://our.internmc.facebook.com/intern/diff/D20172782/)

[ghstack-poisoned]

tools/jit/templates/register_aten_ops.cpp
@@ -100,10 +100,10 @@ c10::OperatorOptions atenOperatorOptions() {
   return result;
 }
 
-int (*DUMMY_OPERATION)(Stack&) = [](Stack& stack) -> int {
-  TORCH_CHECK(false, "Operator has been stripped in the custom build.")
-  return 0;
-};
+KernelFunction::InternalBoxedKernelFunction *DUMMY_OPERATION =
+  [](c10::OperatorKernel *, const c10::OperatorHandle &, std::vector<c10::IValue> *) -> void {
+    TORCH_CHECK(false, "Operator has been stripped in the custom build.")
+  };
 
 class Registerer final {
 public: