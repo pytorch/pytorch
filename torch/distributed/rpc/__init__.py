118e4f020837b19c2ca2b4299a8414bfb2401708
Shihao Xu <xsh6528@gmail.com>
2020-02-28 15:00:16 -0800

    Remove RPC TorchScript private API
    We can directly pass user_callbale to rpc_async API in TorchScript. There is no need to have private API for taking qualified name.
    
    Differential Revision: [D7420993](https://our.internmc.facebook.com/intern/diff/D7420993/)
    
    [ghstack-poisoned]

--- torch/distributed/rpc/__init__.py
+++ torch/distributed/rpc/__init__.py
@@ -19,7 +19,6 @@ if is_available() and not torch._C._rpc_init():
 
 if is_available():
     from . import api
-    from .api import _rpc_sync_torchscript, _rpc_async_torchscript, _remote_torchscript
     from .api import *  # noqa: F401
     import torch.distributed.autograd as dist_autograd
 
