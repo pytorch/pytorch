7bb48590c88682139a3e0a4f2fb3a8c9f8914ae6
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-02-29 14:06:59 -0800

    Migrate lerp from CUDA_tensor_apply3 to TensorIterator
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/Lerp.cu
+++ aten/src/ATen/native/cuda/Lerp.cu
@@ -2,6 +2,8 @@
 #include <ATen/cuda/CUDAApplyUtils.cuh>
 #include <ATen/Dispatch.h>
 #include <ATen/ExpandUtils.h>
+#include <ATen/native/cuda/Loops.cuh>
+#include <ATen/native/TensorIterator.h>
 
 namespace {
 template <typename scalar_t>
@@ -19,16 +21,16 @@ void lerp_cuda(at::Tensor& ret, const at::Tensor& self, const at::Tensor& end, c
 }
 
 template <typename scalar_t>
-void lerp_cuda(at::Tensor& ret, const at::Tensor& self, const at::Tensor& end, scalar_t weight_val) {
-  at::cuda::CUDA_tensor_apply3<scalar_t, scalar_t, scalar_t>(
-      ret, self, end,
-      [=] __device__(
-         scalar_t& ret_val,
-         const scalar_t& self_val,
-         const scalar_t& end_val) {
-        ret_val = (weight_val < 0.5) ?
-            self_val + weight_val * (end_val - self_val) : end_val - (end_val - self_val) * (1 - weight_val);
-      });
+void lerp_scalar_cuda(at::Tensor& ret, const at::Tensor& self, const at::Tensor& end, scalar_t weight_val) {
+  at::TensorIterator iter;
+  iter.add_output(ret);
+  iter.add_input(self);
+  iter.add_input(end);
+  iter.build();
+  at::native::gpu_kernel(iter,
+    [=] GPU_LAMBDA (scalar_t self_val, scalar_t end_val) {
+      return (weight_val < 0.5) ? self_val + weight_val * (end_val - self_val) : end_val - (end_val - self_val) * (1 - weight_val);
+    });
 }
 } // namespace
 
@@ -54,7 +56,7 @@ Tensor& lerp_cuda_scalar_out(Tensor& result, const Tensor& self,
   std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_out_cuda");
   result.resize_as_(b_self);
   AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp_out_cuda", [&]{
-    lerp_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
+    lerp_scalar_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
   });
   return result;
 }
@@ -80,7 +82,7 @@ Tensor& lerp_cuda_scalar_(Tensor& self, const Tensor& end, Scalar weight) {
            "output with shape ", self.sizes(),
            " doesn't match the broadcast shape ", b_self.sizes());
   AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp__cuda", [&]{
-    lerp_cuda<scalar_t>(self, b_self, b_end, weight.to<scalar_t>());
+    lerp_scalar_cuda<scalar_t>(self, b_self, b_end, weight.to<scalar_t>());
   });
   return self;
 }
@@ -102,7 +104,7 @@ Tensor lerp_cuda_scalar(const Tensor& self, const Tensor& end, Scalar weight) {
   std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_cuda");
   Tensor result = at::empty_like(b_self, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
   AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp_cuda", [&]{
-    lerp_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
+    lerp_scalar_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
   });
   return result;
 }
