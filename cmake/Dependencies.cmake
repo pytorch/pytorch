0e52627358c4ec60243f9d8e19b722b7b2265ffc
Kimish Patel <kimishpatel@fb.com>
2020-02-28 21:20:42 -0800

    Fixing pthreadpool symbol conflict issue. (#33869)
    Summary:
    Mainly renaming pthread_create of C2, the only one referred internally in NNPACK, that
    is conflicting, to pthread_create_c2.
    Removed 2 other conflicting symbols that are not used internally at all.
    Pointing XNNPACK to original repo instead of the fork.
    
    Copy pasted the new interface and implementation to
    caff2/utils/threadpool, so that for internal builds we compile against
    this.
    
    When threadpool is unified this will be removed.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33869
    
    Differential Revision: D20140580
    
    Pulled By: kimishpatel
    
    fbshipit-source-id: de70df0af9c7d6bc065e85ede0e1c4dd6a9e6be3

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -294,6 +294,26 @@ if(INTERN_BUILD_MOBILE AND NOT BUILD_CAFFE2_MOBILE AND (USE_QNNPACK OR USE_NNPAC
   ENDIF()
 endif()
 
+# XNNPACK has not option of like QNNPACK_CUSTOM_THREADPOOL
+# that allows us to hijack pthreadpool interface.
+# Thus not doing this ends up building pthreadpool as well as
+# the internal implemenation of pthreadpool which results in symbol conflicts.
+if (USE_XNNPACK)
+  if(NOT DEFINED PTHREADPOOL_SOURCE_DIR)
+    set(CAFFE2_THIRD_PARTY_ROOT "${PROJECT_SOURCE_DIR}/third_party")
+    set(PTHREADPOOL_SOURCE_DIR "${CAFFE2_THIRD_PARTY_ROOT}/pthreadpool" CACHE STRING "pthreadpool source directory")
+  endif()
+
+  IF(NOT TARGET pthreadpool)
+    SET(PTHREADPOOL_BUILD_TESTS OFF CACHE BOOL "")
+    SET(PTHREADPOOL_BUILD_BENCHMARKS OFF CACHE BOOL "")
+    ADD_SUBDIRECTORY(
+      "${PTHREADPOOL_SOURCE_DIR}"
+      "${CONFU_DEPENDENCIES_BINARY_DIR}/pthreadpool"
+      EXCLUDE_FROM_ALL)
+  ENDIF()
+endif()
+
 # ---[ QNNPACK
 if(USE_QNNPACK)
   set(CAFFE2_THIRD_PARTY_ROOT "${PROJECT_SOURCE_DIR}/third_party")
