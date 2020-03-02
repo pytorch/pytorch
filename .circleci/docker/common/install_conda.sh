65a5a9a42015b791e472cb0060f72768e935a2ad
Enealor <53704971+Enealor@users.noreply.github.com>
2020-03-02 09:45:52 -0800

    Add testing packages to conda install

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -86,7 +86,7 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   # scikit-learn is pinned because of
   # https://github.com/scikit-learn/scikit-learn/issues/14485 (affects gcc 5.5
   # only)
-  as_jenkins pip install --progress-bar off pytest scipy==1.1.0 scikit-learn==0.20.3 scikit-image librosa>=0.6.2 psutil numba==0.46.0 llvmlite==0.30.0
+  as_jenkins pip install --progress-bar off pytest hypothesis==4.53.2 unittest-xml-reporting scipy==1.1.0 scikit-learn==0.20.3 scikit-image librosa>=0.6.2 psutil numba==0.46.0 llvmlite==0.30.0
 
   popd
 fi
