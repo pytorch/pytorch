a224fc75a24a28999cf2b890165076d926f58527
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-03-02 22:23:17 -0500

    Use auto?

--- aten/src/ATen/native/RNN.cpp
+++ aten/src/ATen/native/RNN.cpp
@@ -1070,8 +1070,8 @@ std::tuple<Tensor, Tensor, Tensor> lstm(
         std::cout << "c_fwd type: " << c_fwd.device().type() << "\n";
         std::cout << "h_bwd type: " << h_bwd.device().type() << "\n";
         std::cout << "c_bwd type: " << c_bwd.device().type() << "\n";
-        TensorList _fwd_hx = {h_fwd, c_fwd};
-        TensorList _bwd_hx = {h_bwd, c_bwd};
+        auto _fwd_hx = {h_fwd, c_fwd};
+        auto _bwd_hx = {h_bwd, c_bwd};
         std::cout << "_fwd_hx[0] type: " << _fwd_hx[0].device().type() << "\n";
         std::cout << "_fwd_hx[1] type: " << _fwd_hx[1].device().type() << "\n";
 
