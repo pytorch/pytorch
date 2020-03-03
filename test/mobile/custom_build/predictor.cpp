96f44febb4d1135157c741fcecf80fa3ab1e4031
Jiakai Liu <liujiakai@fb.com>
2020-03-03 10:46:57 -0800

    [pytorch][CI] end-to-end custom build script (#34012)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34012
    
    Today some mobile simulator tests only run on landed PRs and it requires
    setting up special build environment to repro errors locally.
    
    The goal of the PR is to do end-to-end mobile custom build & integration
    tests with host toolchain (using same CMake options as mobile build). This
    way, non-mobile engineers can capture & debug mobile related build issues
    much more easily.
    
    There are three custom build types that this script supports:
    
    1. `TEST_DEFAULT_BUILD=1 ./build.sh` - it is similar to the prebuilt libtorch
    libraries released for Android and iOS (same CMake build options + host
    toolchain), which doesn't contain autograd function nor backward ops thus is
    smaller than full LibTorch.
    
    2. `TEST_CUSTOM_BUILD_STATIC=1 ./build.sh` - it further optimizes libtorch
    size by only including ops used by a specific model.
    
    3. `TEST_CUSTOM_BUILD_DYNAMIC=1 ./build.sh` - similar as 2) except that it
    relies on the op dependency graph (instead of static dispatch) to calculate
    and keep all transitively dependent ops by the model.
    
    Type 2) will be deprecated by type 3) in the future.
    Type 3) custom build has not been fully supported yet so it's expected to fail.
    
    Replacing existing mobile build CI to run Type 1) build & integration test.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20193328
    
    Pulled By: ljk53
    
    fbshipit-source-id: 48c14cae849fde86e27123f00f9911996c1cf40e

---
+++ test/mobile/custom_build/predictor.cpp
@@ -0,0 +1,52 @@
+// This is a simple predictor binary that loads a TorchScript CV model and runs
+// a forward pass with fixed input `torch::ones({1, 3, 224, 224})`.
+// It's used for end-to-end integration test for custom mobile build.
+
+#include <iostream>
+#include <string>
+#include <torch/script.h>
+
+using namespace std;
+
+namespace {
+
+struct MobileCallGuard {
+  // AutoGrad is disabled for mobile by default.
+  torch::autograd::AutoGradMode no_autograd_guard{false};
+  // Disable graph optimizer to ensure list of unused ops are not changed for
+  // custom mobile build.
+  torch::jit::GraphOptimizerEnabledGuard no_optimizer_guard{false};
+};
+
+void init() {
+  at::globalContext().setQEngine(at::QEngine::QNNPACK);
+}
+
+torch::jit::script::Module loadModel(const std::string& path) {
+  MobileCallGuard guard;
+  auto module = torch::jit::load(path);
+  module.eval();
+  return module;
+}
+
+} // namespace
+
+int main(int argc, const char* argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <model_path>\n";
+    return 1;
+  }
+  init();
+  auto module = loadModel(argv[1]);
+  auto input = torch::ones({1, 3, 224, 224});
+  auto output = [&]() {
+    MobileCallGuard guard;
+    return module.forward({input}).toTensor();
+  }();
+
+  std::cout << std::setprecision(3) << std::fixed;
+  for (int i = 0; i < 5; i++) {
+    std::cout << output.data_ptr<float>()[i] << std::endl;
+  }
+  return 0;
+}
