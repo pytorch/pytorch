daf94575ea30460350450c82f156f90373b7ab82
ljk53 <liujiakai@fb.com>
2020-03-03 18:16:01 -0800

    [pytorch][CI] add e2e mobile custom build jobs to CI
    Summary:
    Add mobile custom build with static dispatch & dynamic dispatch to CI.
    Most of mobile code analysis CI should be covered by the custom build +
    dynamic dispatch flow, so changing it to running on master only.
    
    [ghstack-poisoned]

--- .jenkins/pytorch/build-mobile.sh
+++ .jenkins/pytorch/build-mobile.sh
@@ -15,8 +15,10 @@ pip install torch torchvision --progress-bar off
 
 # Run end-to-end process of building mobile library, linking into the predictor
 # binary, and running forward pass with a real model.
-if [[ "$BUILD_ENVIRONMENT" == *-mobile-custom-build* ]]; then
+if [[ "$BUILD_ENVIRONMENT" == *-mobile-custom-build-static* ]]; then
   TEST_CUSTOM_BUILD_STATIC=1 test/mobile/custom_build/build.sh
+elif [[ "$BUILD_ENVIRONMENT" == *-mobile-custom-build-dynamic* ]]; then
+  TEST_CUSTOM_BUILD_DYNAMIC=1 test/mobile/custom_build/build.sh
 else
   TEST_DEFAULT_BUILD=1 test/mobile/custom_build/build.sh
 fi
