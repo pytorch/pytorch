7ae0af0108f9d7a9875c6b928d0404fa138a9d1b
Kurt Mohler <kmohler@quansight.com>
2020-03-04 15:12:41 -0600

    Add comment about remainder type promotion for scalar argument

--- aten/src/ATen/native/BinaryOps.cpp
+++ aten/src/ATen/native/BinaryOps.cpp
@@ -243,16 +243,25 @@ Tensor& div_(Tensor& self, Scalar other) {
 
 Tensor remainder(const Tensor& self, Scalar other) {
   Tensor other_tensor = wrapped_scalar_tensor(other);
+  // FIXME: 'other' is converted to match the dtype of 'self' to retain
+  //   BC with TH, but in the future, we should use normal type promotion,
+  //   like in numpy
   return native::remainder(self, other_tensor.toType(self.scalar_type()));
 }
 
 Tensor& remainder_(Tensor& self, Scalar other) {
   Tensor other_tensor = wrapped_scalar_tensor(other);
+  // FIXME: 'other' is converted to match the dtype of 'self' to retain
+  //   BC with TH, but in the future, we should use normal type promotion,
+  //   like in numpy
   return native::remainder_(self, other_tensor.toType(self.scalar_type()));
 }
 
 Tensor& remainder_out(Tensor& result, const Tensor& self, Scalar other) {
   Tensor other_tensor = wrapped_scalar_tensor(other);
+  // FIXME: 'other' is converted to match the dtype of 'self' to retain
+  //   BC with TH, but in the future, we should use normal type promotion,
+  //   like in numpy
   return native::remainder_out(result, self, other_tensor.toType(self.scalar_type()));
 }
 
