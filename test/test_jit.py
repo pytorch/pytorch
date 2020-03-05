11b1fc0f8ecdd797b4af0aa38ebeb052659eac43
Your Name <davidriazati@fb.com>
2020-03-05 13:40:08 -0800

    [jit] Make `ModuleList`s a sugared value
    Previously when emitting subscripts we only emitted actual values, but
    now they may sometimes emit a `ModuleValue`, so it should stay as a
    `SugaredValue`. This allows for the result of the subscript to be
    treated as a real module (i.e. you can just do `self.modlist[1](inputs)`
    instead of `self.modlist[1].forward(inputs)`)

--- test/test_jit.py
+++ test/test_jit.py
@@ -10393,8 +10393,6 @@ a")
                 return thing - self.i
 
         class M(torch.nn.Module):
-            __constants__ = ['mods']
-
             def __init__(self):
                 super(M, self).__init__()
                 self.mods = nn.ModuleList([Sub(i) for i in range(10)])
@@ -10407,6 +10405,19 @@ a")
 
         x = torch.tensor(1)
         self.checkModule(M(), (x,))
+
+        class MForward(torch.nn.Module):
+            def __init__(self):
+                super(MForward, self).__init__()
+                self.mods = nn.ModuleList([Sub(i) for i in range(10)])
+
+            def forward(self, v):
+                v = self.mods[4](v)
+                v = self.mods[-1](v)
+                v = self.mods[-9](v)
+                return v
+
+        self.checkModule(MForward(), (torch.tensor(1),))
 
         class M2(M):
             def __init__(self):
