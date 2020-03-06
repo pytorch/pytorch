417f213af3c26db8cf9b2256c4b36060f59d0a27
eellison <eellison@fb.com>
2020-03-06 14:35:10 -0800

    add BC ignore

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -114,6 +114,8 @@ white_list = [
     ('aten::confirmed_by_owner', datetime.date(2020, 3, 17)),
     ('aten::owner', datetime.date(2020, 3, 27)),
     ('aten::owner_name', datetime.date(2020, 3, 27)),
+    ('_aten', datetime.date(2020, 4, 1)),
+    ("prim::shape", datetime.date(2020, 4, 1)),
 ]
 
 
