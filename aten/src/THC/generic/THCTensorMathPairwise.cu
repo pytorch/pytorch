72f9e590fb9e49f647d075ad005f8b21f602ee59
Kurt Mohler <kmohler@quansight.com>
2020-03-11 07:02:59 -0700

    Port `remainder` from TH to ATen (CPU and CUDA) (#34136)
    Summary:
    CPU issue https://github.com/pytorch/pytorch/issues/24753
    CUDA issue https://github.com/pytorch/pytorch/issues/24615
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34136
    
    Differential Revision: D20375458
    
    Pulled By: ezyang
    
    fbshipit-source-id: 1a9fb39a7e2d17a0d31bd14b211eaacea060e834

--- aten/src/THC/generic/THCTensorMathPairwise.cu
+++ aten/src/THC/generic/THCTensorMathPairwise.cu
@@ -97,24 +97,6 @@ void THCTensor_(fmod)(THCState *state, THCTensor *self_, THCTensor *src_, scalar
   THCudaCheck(cudaGetLastError());
 }
 
-void THCTensor_(remainder)(THCState *state, THCTensor *self_, THCTensor *src_, scalar_t value)
-{
-  THCAssertSameGPU(THCTensor_(checkGPU)(state, 2, self_, src_));
-  if (self_ == src_) {
-    if (!THC_pointwiseApply1<scalar_t>(state, self_, TensorRemainderOp<scalar_t>(value))) {
-      THArgCheck(false, 2, CUTORCH_DIM_WARNING);
-    }
-  } else {
-    THCTensor_(resizeAs)(state, self_, src_);
-
-    if (!THC_pointwiseApply2<scalar_t, scalar_t>(state, self_, src_, TensorRemainderOp<scalar_t>(value))) {
-      THArgCheck(false, 2, CUTORCH_DIM_WARNING);
-    }
-  }
-
-  THCudaCheck(cudaGetLastError());
-}
-
 void THCTensor_(triu)(THCState *state, THCTensor *self_, THCTensor *src_, int64_t k)
 {
   THCAssertSameGPU(THCTensor_(checkGPU)(state, 2, self_, src_));
