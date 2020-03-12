944ea4c334bd44e01ebcc21e6a89d60d1a8e16ab
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-03-12 11:39:56 -0700

    ONNX Export Support for CrossEntropyLoss (#33767)
    Summary:
    Add ONNX export support for torch.nn.CrossEntropyLoss.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33767
    
    Reviewed By: hl475
    
    Differential Revision: D20121169
    
    Pulled By: houseroad
    
    fbshipit-source-id: 7b56617e8c60617b922949fc8b4ecc626eedf7ed

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -2692,6 +2692,63 @@ class TestONNXRuntime(unittest.TestCase):
         x = torch.randn(3, 4)
         self.run_test(EinsumModelTranspose(), input=(x,))
 
+    @unittest.skip("Enable this once ORT version is updated")
+    @skipIfUnsupportedMinOpsetVersion(12)
+    def test_crossentropyloss(self):
+        class CrossEntropyLossNone(torch.nn.Module):
+            def forward(self, input, target):
+                loss = torch.nn.CrossEntropyLoss(reduction='none')
+                return loss(input, target)
+
+        x = torch.randn(3, 5)
+        y = torch.empty(3, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossNone(), input=(x, y))
+
+        class CrossEntropyLossNoneWeight(torch.nn.Module):
+            def forward(self, input, target):
+                loss = torch.nn.CrossEntropyLoss(reduction='none', weight=torch.randn(5))
+                return loss(input, target)
+
+        x = torch.randn(3, 5)
+        y = torch.empty(3, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossNoneWeight(), input=(x, y))
+
+        class CrossEntropyLossSum(torch.nn.Module):
+            def forward(self, input, target):
+                loss = torch.nn.CrossEntropyLoss(reduction='sum')
+                return loss(input, target)
+
+        x = torch.randn(3, 5, 2)
+        y = torch.empty(3, 2, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossSum(), input=(x, y))
+
+        class CrossEntropyLossSumWeight(torch.nn.Module):
+            def forward(self, input, target, weight):
+                loss = torch.nn.CrossEntropyLoss(reduction='sum', weight=torch.randn(5))
+                return loss(input, target)
+
+        x = torch.randn(3, 5, 2)
+        y = torch.empty(3, 2, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossSumWeight(), input=(x, y))
+
+        class CrossEntropyLossMean(torch.nn.Module):
+            def forward(self, input, target):
+                loss = torch.nn.CrossEntropyLoss()
+                return loss(input, target)
+
+        x = torch.randn(3, 5, 2)
+        y = torch.empty(3, 2, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossMean(), input=(x, y))
+
+        class CrossEntropyLossMeanWeight(torch.nn.Module):
+            def forward(self, input, target, weight):
+                loss = torch.nn.CrossEntropyLoss(weight=torch.randn(5))
+                return loss(input, target)
+
+        x = torch.randn(3, 5, 2)
+        y = torch.empty(3, 2, dtype=torch.long).random_(5)
+        self.run_test(CrossEntropyLossMeanWeight(), input=(x, y))
+
     def test_empty_branch(self):
         class EmptyBranchModel(torch.jit.ScriptModule):
             @torch.jit.script_method
