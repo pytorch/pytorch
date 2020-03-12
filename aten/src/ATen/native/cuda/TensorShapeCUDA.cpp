a6dcfbd20293daf9f32ca0a97d55765e47f3fac6
Gregory Chanan <gchanan@fb.com>
2020-03-11 17:26:02 -0700

    Update on "Port set_ to ATen."
    Differential Revision: [D20342300](https://our.internmc.facebook.com/intern/diff/D20342300)

--- aten/src/ATen/native/cuda/TensorShapeCUDA.cpp
+++ aten/src/ATen/native/cuda/TensorShapeCUDA.cpp
@@ -51,7 +51,7 @@ Tensor& set_storage_cuda_(Tensor& result, Storage storage, int64_t storage_offse
 
   c10::optional<IntArrayRef> stride_opt = stride.data() != nullptr ?
                                               c10::optional<IntArrayRef>(stride) : c10::nullopt;
-  at::native::cuda::resize_impl_cuda_(result.unsafeGetTensorImpl(), size, stride_opt);
+  at::native::resize_impl_cuda_(result.unsafeGetTensorImpl(), size, stride_opt);
   return result;
 }
 
