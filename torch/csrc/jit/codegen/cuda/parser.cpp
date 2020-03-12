65f8f6dbf2af6c95002be8d66a848ba845979acb
Jie <jiej@nvidia.com>
2020-03-12 02:09:32 -0700

    update build files

--- torch/csrc/jit/codegen/cuda/parser.cpp
+++ torch/csrc/jit/codegen/cuda/parser.cpp
@@ -132,6 +132,7 @@ protected:
     // Register parse-function for each JIT operator;
     // This is a one-time look up, our hash registry indexes on the pointer in
     // OperatorRegistry.
+/*
     const char* BinaryOpWithAlpha[4] = {
         "aten::add(Tensor self, Tensor other, *, Scalar alpha) -> Tensor",
         "aten::add(Tensor self, Scalar other, Scalar alpha) -> Tensor",
@@ -143,7 +144,6 @@ protected:
       //assert(registerParseRule(ptr_op, &parseBinaryOpWithAlpha));
     }
 
-/*
     const char* BinaryOp[4] = {
         "aten::div(Tensor self, Tensor other) -> Tensor",
         "aten::div(Tensor self, Scalar other) -> Tensor",
