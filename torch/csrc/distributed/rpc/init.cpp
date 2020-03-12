8cfb407078f474772af72170b3861ba40a70b7c8
Shihao Xu <shihaoxu@fb.com>
2020-03-12 10:09:38 -0700

    Delete all user forks tracked in RRefContext before graceful shutting down (#31893)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31893
    
    In order to resolve the issue summarized in https://github.com/pytorch/pytorch/issues/31325.
    
    The overal solution is to proactively send out delete fork messages from user nodes, before user nodes detecting rref leaks.
    
    As the first step, we want to have a weak ref tracker to track all user rrefs.
    ghstack-source-id: 100023142
    
    Test Plan:
    V22 is the version that make User to wait on delete UseerRRef message.
    
    # Unit tests
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork
    
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork -- test_nested_rref_stress --stress-runs 100
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork \
    && buck-out/gen/caffe2/test/distributed/rpc/rpc_fork\#binary.par -r test_nested_rref_stress
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork \
    && buck-out/gen/caffe2/test/distributed/rpc/rpc_fork\#binary.par - r test_rref_forward_chain
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:rpc_fork \
    && buck-out/gen/caffe2/test/distributed/rpc/rpc_fork\#binary.par -r test_non_garbage_collected_user_rref_due_to_local_circular_dependency
    ```
    
    Reviewed By: mrshenli
    
    Differential Revision: D19292254
    
    fbshipit-source-id: 92c3e8d0b00f183c5e22f163bdca482cc25a1ce9

--- torch/csrc/distributed/rpc/init.cpp
+++ torch/csrc/distributed/rpc/init.cpp
@@ -23,6 +23,8 @@ namespace rpc {
 
 namespace {
 
+constexpr std::chrono::milliseconds kDeleteAllUsersTimeout(100000);
+
 template <typename T>
 using shared_ptr_class_ = py::class_<T, std::shared_ptr<T>>;
 
@@ -329,6 +331,13 @@ If the future completes with an error, an exception is thrown.
   module.def("_reset_current_rpc_agent", []() {
     RpcAgent::setCurrentRpcAgent(nullptr);
   });
+
+  module.def(
+      "_delete_all_user_rrefs",
+      [](std::chrono::milliseconds timeoutMillis) {
+        RRefContext::getInstance().delAllUsers(timeoutMillis);
+      },
+      py::arg("timeout") = kDeleteAllUsersTimeout);
 
   module.def("_destroy_rref_context", [](bool ignoreRRefLeak) {
     // NB: do not release GIL in the function. The destroyInstance() method
