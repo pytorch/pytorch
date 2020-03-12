e41ff765a73c4fe0388b228b8ea6e810cf8edfee
Christian Sarofeen <csarofeen@nvidia.com>
2020-03-12 10:37:30 -0400

    Quick interface header to get all needed items for codegen.

---
+++ torch/csrc/jit/codegen/cuda/interface.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Grab all headers intended as the user interface for CUDA codegen.
+#include <torch/csrc/jit/codegen/cuda/tensor.h>
+#include <torch/csrc/jit/codegen/cuda/iriostream.h>
+#include <torch/csrc/jit/codegen/cuda/arith.h>
+#include <torch/csrc/jit/codegen/cuda/code_write.h>
