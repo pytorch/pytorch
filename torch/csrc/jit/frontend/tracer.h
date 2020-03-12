6eef2004425991a0d75c0aafcd6530f06857f823
peter <peterghost86@gmail.com>
2020-03-12 14:58:39 +0800

    Attempt 2

--- torch/csrc/jit/frontend/tracer.h
+++ torch/csrc/jit/frontend/tracer.h
@@ -272,22 +272,30 @@ TORCH_API void addInputs(
     const c10::optional<at::MemoryFormat>& value);
 TORCH_API void addInputs(Node* n, const char* name, at::Generator* value);
 
+inline void addInputs(
+    Node* n,
+    const char* name,
+    const std::vector<bool>& value) {
+  AT_ERROR("Tracing a list of bool type is currently not supported!");
+}
+
 template <typename T>
-TORCH_API void addInputs(Node* n, const char* name, ArrayRef<T> value);
-
+void addInputs(Node* n, const char* name, ArrayRef<T> value) {
+  AT_ERROR("Tracing a list of arbitrary type is currently not supported!");
+}
 template <typename K, typename V>
-TORCH_API void addInputs(
+void addInputs(
     Node* n,
     const char* name,
-    const std::unordered_map<K, V>& value);
-
-TORCH_API void addInputs(
-    Node* n,
-    const char* name,
-    const std::vector<bool>& value);
+    const std::unordered_map<K, V>& value) {
+  AT_ERROR("Tracing a dict of arbitrary types is currently not supported!");
+}
 
 template <size_t N>
-TORCH_API void addInputs(Node* n, const char* name, std::array<bool, N> value);
+void addInputs(Node* n, const char* name, std::array<bool, N> value) {
+  throw std::runtime_error(
+      "Found an unsupported argument type in the JIT tracer. File a bug report.");
+}
 
 TORCH_API void addInputs(
     Node* n,
