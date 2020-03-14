da176309df1f3357c2de9ed3f0ac60413922af08
James Reed <jamesreed@fb.com>
2020-03-13 18:21:21 -0700

    [JIT][mobile] Support built-in Function call in lite interpreter (#34676)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/34676
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20427938
    
    Pulled By: jamesr66a
    
    fbshipit-source-id: 79eebfa858776f26da55ffd49d3f78fa7ae0df9b

--- torch/csrc/jit/mobile/interpreter.cpp
+++ torch/csrc/jit/mobile/interpreter.cpp
@@ -1,6 +1,8 @@
 #include "interpreter.h"
-#include <torch/csrc/jit/mobile/function.h>
+#include <ATen/core/function.h>
+#include <ATen/core/jit_type.h>
 #include <ATen/core/operator_name.h>
+#include <torch/csrc/jit/mobile/function.h>
 #include <torch/csrc/jit/runtime/vararg_functions.h>
 
 #if defined(PYTORCH_MOBILE_OPERATOR_OBSERVER)
@@ -52,6 +54,15 @@ bool InterpreterState::run(Stack& stack) {
       case OPN: {
         stack.push_back(inst.N);
         code_->operators_[inst.X](stack);
+        ++pc;
+      } break;
+      case INTERFACE_CALL: {
+        torch::jit::Function* method =
+            peek(stack, 0, inst.N)
+                .toObject()
+                ->type()
+                ->getMethod(code_->constants_[inst.X].toStringRef());
+        method->run(stack);
         ++pc;
       } break;
       case LOAD:
