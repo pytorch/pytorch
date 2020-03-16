779d73a9013c1adb153b574e5f45542516a1c7d5
Kurt Mohler <kmohler@quansight.com>
2020-03-16 14:30:00 -0500

    Add half and bfloat16 types to mm cuda dispatch

--- aten/src/ATen/native/cuda/LinearAlgebra.cu
+++ aten/src/ATen/native/cuda/LinearAlgebra.cu
@@ -45,6 +45,26 @@ Tensor prepare_tensor_for_cublas(Tensor& tensor, char& transpose_tensor) {
   return tensor_;
 }
 
+// Check https://github.com/pytorch/pytorch/issues/22078
+// for information about the bug. We don't know the exact conditions that trigger it,
+// but using Sgemm or Hgemm on Maxwell or Pascal seems to be a
+// necessary condition.
+static void checkCuda90Bug(int i_m, int i_n, int i_k)
+{
+#if CUDA_VERSION < 9200 && CUDA_VERSION >= 9000
+  static std::once_flag alreadyWarned;
+  const int LIMIT = 1 << 21;
+  if (i_m > LIMIT || i_n > LIMIT || i_k > LIMIT) {
+    cudaDeviceProp* prop = at::cuda::getCurrentDeviceProperties();
+    if (prop->major == 5 || prop->major == 6) {
+      std::call_once(alreadyWarned, []() {
+        TORCH_WARN("Matrix multiplication for dimensions larger than 2^21 has known bugs on your combination of CUDA version and device type. Please consider upgrading to CUDA 9.2 or later.");
+      });
+    }
+  }
+#endif
+}
+
 Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& mat1, const Tensor& mat2, Scalar beta, Scalar alpha) {
   TORCH_CHECK(
     (mat1.dim() == 2) && (mat2.dim() == 2) &&
@@ -76,55 +96,34 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
   mat1_ = prepare_tensor_for_cublas(mat1_, transpose_mat1);
   mat2_ = prepare_tensor_for_cublas(mat2_, transpose_mat2);
 
-  AT_DISPATCH_FLOATING_TYPES(
-    result_.scalar_type(), "addmm_cuda", [&] {
-      scalar_t alpha_val = alpha.to<scalar_t>();
-      scalar_t beta_val = beta.to<scalar_t>();
-      scalar_t* mat1_ptr = mat1_.data_ptr<scalar_t>();
-      scalar_t* mat2_ptr = mat2_.data_ptr<scalar_t>();
-      scalar_t* result_ptr = result_.data_ptr<scalar_t>();
+  int64_t m = mat1_.size(0);
+  int64_t k = mat1_.size(1);
+  int64_t n = mat2_.size(1);
+  int64_t mat1_ld = (transpose_mat1 == 'n') ? mat1_.stride(1) : mat1_.stride(0);
+  int64_t mat2_ld = (transpose_mat2 == 'n') ? mat2_.stride(1) : mat2_.stride(0);
+  int64_t result_ld = (transpose_result == 'n') ? result_.stride(1) : result_.stride(0);
 
-      int64_t m = mat1_.size(0);
-      int64_t k = mat1_.size(1);
-      int64_t n = mat2_.size(1);
-
-      int64_t mat1_ld = (transpose_mat1 == 'n') ? mat1_.stride(1) : mat1_.stride(0);
-      int64_t mat2_ld = (transpose_mat2 == 'n') ? mat2_.stride(1) : mat2_.stride(0);
-      int64_t result_ld = (transpose_result == 'n') ? result_.stride(1) : result_.stride(0);
-
-      /*
-      std::cout << "mat1_ = " << std::endl;
-      std::cout << mat1_ << std::endl;
-      std::cout << "mat2_ = " << std::endl;
-      std::cout << mat2_ << std::endl;
-      std::cout << "result_ = " << std::endl;
-      std::cout << result_ << std::endl;
-      std::cout << "transpose_mat1 = " << transpose_mat1 << std::endl;
-      std::cout << "transpose_mat2 = " << transpose_mat1 << std::endl;
-      std::cout << "transpose_result = " << transpose_result << std::endl;
-      std::cout << "m = " << m << std::endl;
-      std::cout << "n = " << n << std::endl;
-      std::cout << "k = " << k << std::endl;
-      std::cout << "mat1_ld = " << mat1_ld << std::endl;
-      std::cout << "mat2_ld = " << mat2_ld << std::endl;
-      std::cout << "result_ld = " << result_ld << std::endl;
-      */
-
-      at::cuda::blas::gemm<scalar_t>(
-        transpose_mat1,
-        transpose_mat2,
-        m, n, k,
-        alpha_val,
-        mat1_ptr, mat1_ld,
-        mat2_ptr, mat2_ld,
-        beta_val,
-        result_ptr, result_ld
-      );
+  AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, self.scalar_type(), "addmm_cuda", [&] {
+    if (self.scalar_type() == at::ScalarType::BFloat16 || self.scalar_type() == at::ScalarType::Int) {
+      checkCuda90Bug(static_cast<int>(m), static_cast<int>(n), static_cast<int>(k));
     }
-  );
-
+    scalar_t alpha_val = alpha.to<scalar_t>();
+    scalar_t beta_val = beta.to<scalar_t>();
+    scalar_t* mat1_ptr = mat1_.data_ptr<scalar_t>();
+    scalar_t* mat2_ptr = mat2_.data_ptr<scalar_t>();
+    scalar_t* result_ptr = result_.data_ptr<scalar_t>();
+    at::cuda::blas::gemm<scalar_t>(
+      transpose_mat1,
+      transpose_mat2,
+      m, n, k,
+      alpha_val,
+      mat1_ptr, mat1_ld,
+      mat2_ptr, mat2_ld,
+      beta_val,
+      result_ptr, result_ld
+    );
+  });
   result.copy_(result_);
-
   return result;
 }
 
