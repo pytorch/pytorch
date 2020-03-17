0fc10fa91a731564383eeec0829de51d521cd012
Lara Haidar <haidar.lara@gmail.com>
2020-03-17 11:05:05 -0700

    Update symbolic_opset12.py

--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import torch.onnx.symbolic_helper as sym_help
 from torch.onnx.symbolic_helper import parse_args
+import torch
 
 
 # EDITING THIS FILE? READ THIS FIRST!
