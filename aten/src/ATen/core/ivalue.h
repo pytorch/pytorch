0216c76e1273e9deeb708cc8e6cf7bd7b7ea5fbc
Terence Feng <tfeng@fb.com>
2020-03-17 15:33:16 -0700

    SNIFAE Template Constructors of IValue (#34647) (#34843)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34843
    
    Currently, we use not_ok_to_boxing to filter Dimname that can not be
    converted/constructed to IValue. The correct way should be SNIFAE the
    constructor of IValue.
    
    (Note: this ignores all push blocking failures!)
    
    Test Plan:
    PyTorch compiled after the code change.
    
    All unit test passed
    
    Imported from OSS
    
    Differential Revision: D20494886
    
    fbshipit-source-id: 91dfba6a41a3ae2d6ceba9d4124cbf612ea3f080

--- aten/src/ATen/core/ivalue.h
+++ aten/src/ATen/core/ivalue.h
@@ -321,11 +321,23 @@ struct CAFFE2_API IValue final {
   c10::List<IValue> toList() const &;
   c10::ArrayRef<IValue> toListRef() const;
 
+  // Some template constructors of IValue calls another constructor recursively.
+  // This SNIFAEs the called constructor exists.
   template<class T>
+  using enable_if_ivalue_constructible =
+      std::enable_if_t<std::is_constructible<IValue, T>::value, std::nullptr_t>;
+
+  template <
+      class T,
+      enable_if_ivalue_constructible<T> = nullptr>
   IValue(c10::List<T> v);
-  template<class T>
+  template <
+      class T,
+      enable_if_ivalue_constructible<T> = nullptr>
   IValue(at::ArrayRef<T> v);
-  template<class T>
+  template <
+      class T,
+      enable_if_ivalue_constructible<T> = nullptr>
   IValue(const std::vector<T>& v);
 
   // GenericDict
@@ -343,7 +355,9 @@ struct CAFFE2_API IValue final {
   /// \endcond
   IValue(std::unordered_map<Key, Value> v);
 
-  template<class T>
+  template <
+      class T,
+      enable_if_ivalue_constructible<T> = nullptr>
   IValue(c10::optional<T> v);
   IValue(c10::nullopt_t);
 
