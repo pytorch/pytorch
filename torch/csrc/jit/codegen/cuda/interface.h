115447343e2d0bf24c961612bbb25967309eff2c
Christian Sarofeen <csarofeen@nvidia.com>
2020-03-17 14:14:45 -0400

    Missed a few that can't be defined in .cpp files.

--- torch/csrc/jit/codegen/cuda/interface.h
+++ torch/csrc/jit/codegen/cuda/interface.h
@@ -25,17 +25,17 @@ struct CudaFuserInterface {
 };
 
 // Get interface, this is used by registration and user facing API internally
-TORCH_CUDA_API CudaFuserInterface* getFuserInterface();
+TORCH_API CudaFuserInterface* getFuserInterface();
 
 // Customer facing APIs vvv
 
 // Query if node is fusable for cuda codegen
-TORCH_CUDA_API bool isFusable(const Node* const node);
-TORCH_CUDA_API bool isFusable(const Node* const fusion, const Node* const node);
+TORCH_API bool isFusable(const Node* const node);
+TORCH_API bool isFusable(const Node* const fusion, const Node* const node);
 
 // redirect to compileCudaFusionGroup (manager.h)
-TORCH_CUDA_API void compileFusionGroup(Node* fusion_node);
+TORCH_API void compileFusionGroup(Node* fusion_node);
 // redirect to runCudaFusionGroup (manager.h)
-TORCH_CUDA_API void runFusionGroup(const Node* const fusion_node, Stack& stack);
+TORCH_API void runFusionGroup(const Node* const fusion_node, Stack& stack);
 
 }}}} // namespace torch::jit::fuser::cuda
