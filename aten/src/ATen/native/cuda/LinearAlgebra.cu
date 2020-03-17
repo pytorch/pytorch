5957e5b90c54bac1909ff20b99bbddc735a25c5c
Kurt Mohler <kmohler@quansight.com>
2020-03-17 08:50:56 -0500

    Improve performance and code readability of MM cuda

--- aten/src/ATen/native/cuda/LinearAlgebra.cu
+++ aten/src/ATen/native/cuda/LinearAlgebra.cu
@@ -24,21 +24,19 @@ Tensor& bmm_out_cuda(Tensor &result, const Tensor& batch1, const Tensor& batch2)
   return legacy::cuda::_th_bmm_out(result, batch1, batch2);
 }
 
-Tensor prepare_tensor_for_cublas(Tensor& tensor, char& transpose_tensor) {
+Tensor prepare_matrix_for_cublas(Tensor& tensor, bool& transpose_tensor) {
   Tensor tensor_;
+  IntArrayRef tensor_strides = tensor.strides();
 
-  if ((tensor.stride(0) == 1) && (tensor.stride(1) != 0)) {
+  if ((tensor_strides[0] == 1) && (tensor_strides[1] != 0)) {
     tensor_ = tensor;
-    transpose_tensor = 'n';
-  } else if ((tensor.stride(1) == 1) && (tensor.stride(0) != 0)) {
+    transpose_tensor = false;
+  } else if ((tensor_strides[1] == 1) && (tensor_strides[0] != 0)) {
     tensor_ = tensor;
-    transpose_tensor = 't';
+    transpose_tensor = true;
   } else {
-    // transpose_result = 'n';
-    // result_ = result.transpose().clone(at::MemoryFormat::Contiguous).transpose();
-
     // This way is better, right?
-    transpose_tensor = 't';
+    transpose_tensor = true;
     tensor_ = tensor.clone(at::MemoryFormat::Contiguous);
   }
 
@@ -71,40 +69,52 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
     (self.dim() == 2) && (result.dim() == 2),
     "tensors must be 2-D"
   );
-  TORCH_CHECK(mat1.size(1) == mat2.size(0), "mat1 dim 1 must match mat2 dim 0");
-  TORCH_CHECK(self.size(0) == mat1.size(0), "self dim 0 must match mat1 dim 0");
-  TORCH_CHECK(self.size(1) == mat2.size(1), "self dim 1 must match mat2 dim 1");
+  IntArrayRef mat1_sizes = mat1.sizes();
+  IntArrayRef mat2_sizes = mat2.sizes();
+  IntArrayRef self_sizes = self.sizes();
+  TORCH_CHECK(mat1_sizes[1] == mat2_sizes[0], "mat1 dim 1 must match mat2 dim 0");
+  TORCH_CHECK(self_sizes[0] == mat1_sizes[0], "self dim 0 must match mat1 dim 0");
+  TORCH_CHECK(self_sizes[1] == mat2_sizes[1], "self dim 1 must match mat2 dim 1");
 
-  if (!result.is_set_to(self)) {
+  // If self and result either point to the same data or if beta is zero,
+  // we can avoid copying self into result. Otherwise, we need to copy.
+  if (result.data_ptr() != self.data_ptr()) {
     if (beta.to<double>() != 0.0) {
       result.copy_(self);
     }
   }
 
-  if ((result.size(0) == 0) || (result.size(1) == 0)) {
+  IntArrayRef result_sizes = result.sizes();
+  if ((result_sizes[0] == 0) || (result_sizes[1] == 0)) {
     return result;
   }
 
-  char transpose_result;
-  Tensor result_ = prepare_tensor_for_cublas(result, transpose_result);
+  bool transpose_result;
+  Tensor result_ = prepare_matrix_for_cublas(result, transpose_result);
+  bool transpose_mat1;
+  bool transpose_mat2;
+  Tensor mat1_ = transpose_result ? mat2 : mat1;
+  Tensor mat2_ = transpose_result ? mat1 : mat2;
+  mat1_ = prepare_matrix_for_cublas(mat1_, transpose_mat1);
+  mat2_ = prepare_matrix_for_cublas(mat2_, transpose_mat2);
 
-  char transpose_mat1;
-  char transpose_mat2;
-  Tensor mat1_ = (transpose_result == 'n') ? mat1 : mat2.transpose(0, 1);
-  Tensor mat2_ = (transpose_result == 'n') ? mat2 : mat1.transpose(0, 1);
+  if (transpose_result) {
+    transpose_mat1 = !transpose_mat1;
+    transpose_mat2 = !transpose_mat2;
+    mat1_sizes = mat1_.sizes();
+    mat2_sizes = mat2_.sizes();
+  }
 
-  mat1_ = prepare_tensor_for_cublas(mat1_, transpose_mat1);
-  mat2_ = prepare_tensor_for_cublas(mat2_, transpose_mat2);
+  int64_t m = mat1_sizes[transpose_result ? 1 : 0];
+  int64_t k = mat1_sizes[transpose_result ? 0 : 1];
+  int64_t n = mat2_sizes[transpose_result ? 0 : 1];
+  int64_t mat1_ld = mat1_.stride((transpose_mat1 == transpose_result) ? 1 : 0);
+  int64_t mat2_ld = mat2_.stride((transpose_mat2 == transpose_result) ? 1 : 0);
+  int64_t result_ld = result_.stride(transpose_result ? 0 : 1);
+  at::ScalarType scalar_type = self.scalar_type();
 
-  int64_t m = mat1_.size(0);
-  int64_t k = mat1_.size(1);
-  int64_t n = mat2_.size(1);
-  int64_t mat1_ld = (transpose_mat1 == 'n') ? mat1_.stride(1) : mat1_.stride(0);
-  int64_t mat2_ld = (transpose_mat2 == 'n') ? mat2_.stride(1) : mat2_.stride(0);
-  int64_t result_ld = (transpose_result == 'n') ? result_.stride(1) : result_.stride(0);
-
-  AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, self.scalar_type(), "addmm_cuda", [&] {
-    if (self.scalar_type() == at::ScalarType::BFloat16 || self.scalar_type() == at::ScalarType::Int) {
+  AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, scalar_type, "addmm_cuda", [&] {
+    if (scalar_type == at::ScalarType::BFloat16 || scalar_type == at::ScalarType::Int) {
       checkCuda90Bug(static_cast<int>(m), static_cast<int>(n), static_cast<int>(k));
     }
     scalar_t alpha_val = alpha.to<scalar_t>();
@@ -113,8 +123,8 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
     scalar_t* mat2_ptr = mat2_.data_ptr<scalar_t>();
     scalar_t* result_ptr = result_.data_ptr<scalar_t>();
     at::cuda::blas::gemm<scalar_t>(
-      transpose_mat1,
-      transpose_mat2,
+      transpose_mat1 ? 't' : 'n',
+      transpose_mat2 ? 't' : 'n',
       m, n, k,
       alpha_val,
       mat1_ptr, mat1_ld,
@@ -123,7 +133,9 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
       result_ptr, result_ld
     );
   });
-  result.copy_(result_);
+  if (result.data_ptr() != result_.data_ptr()) {
+    result.copy_(result_);
+  }
   return result;
 }
 
