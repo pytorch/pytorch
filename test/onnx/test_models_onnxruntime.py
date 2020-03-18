f6570893fbb589c8985b4d66cf98961d96541aba
Bowen Bao <semisqg@gmail.com>
2020-03-17 17:04:34 -0700

    Enable onnx model tests for opset 11

--- test/onnx/test_models_onnxruntime.py
+++ test/onnx/test_models_onnxruntime.py
@@ -11,9 +11,10 @@ from test_pytorch_onnx_onnxruntime import run_model_test
 
 
 def exportTest(self, model, inputs, rtol=1e-2, atol=1e-7, opset_versions=None):
-    opset_versions = opset_versions if opset_versions else [7, 8, 9, 10]
+    opset_versions = opset_versions if opset_versions else [7, 8, 9, 10, 11]
     for opset_version in opset_versions:
         self.opset_version = opset_version
+        self.keep_initializers_as_inputs = True
         run_model_test(self, model, False,
                        input=inputs, rtol=rtol, atol=atol)
 
