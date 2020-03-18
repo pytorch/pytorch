c2c8e390b8b2f0611c578c0344a63da83b8a6ea4
Pavel Belevich <pbelevich@fb.com>
2020-03-18 13:17:41 -0700

    Update on "Replace Generator* with Generator that holds std::shared_ptr<GeneratorImpl>"
    This PR prepares `at::Generator` for pybind11's `type_caster<at::Generator>` which is required to implement custom RNG in python. The following changes are done:
    1. `at::Generator` was moved to `c10::GeneratorImpl` (similar to `c10::TensorImpl`)
    2. `at::Generator` was recreated as a holder of `std::shared_ptr<c10::GeneratorImpl>` (similar to `at::Tensor` that holds `c10::intrusive_ptr<c10::TensorImpl>`)
    3. Most of `at::Generator*` usages were replaced with `at::Generator`
    
    TBD: replacing `Generator generator = nullptr` with `{}` requires JIT changes(adding Generator to IValue?)
    
    
    
    [ghstack-poisoned]

--- aten/src/ATen/core/Generator.h
+++ aten/src/ATen/core/Generator.h
@@ -61,8 +61,8 @@ struct CAFFE2_API Generator {
     }
   }
 
-  // // TODO(pbelevich): delete this after replace Generator generator = nullptr with {}
-  Generator(nullptr_t gen_impl) {}
+  // TODO(pbelevich): delete this after replace Generator generator = nullptr with c10::optional<at::Generator> = c10::nullopt
+  Generator(std::nullptr_t gen_impl) {}
 
   bool operator==(const Generator& rhs) const {
     return (!(this->impl_) && !(rhs.impl_)) || (this->impl_ == rhs.impl_);
