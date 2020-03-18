9a03b6dc3b0708117c3d2be8f0f35eace8703521
Pavel Izmailov <izmailovpavel@gmail.com>
2020-03-17 21:42:06 -0400

    added scheduler + finished averaged model

--- torch/optim/swa_utils.py
+++ torch/optim/swa_utils.py
@@ -1,32 +1,39 @@
-from torch.nn.module import Module
+import torch
+from torch.nn import Module
 from copy import deepcopy
+from torch.optim.lr_scheduler import _LRScheduler
+
 
 class AveragedModel(Module):
     """
-	.. _Averaging Weights Leads to Wider Optima and Better Generalization:
+    ToDo
+    .. _Averaging Weights Leads to Wider Optima and Better Generalization:
         https://arxiv.org/abs/1803.05407
     .. _There Are Many Consistent Explanations of Unlabeled Data: Why You Should
-		Average:
+        Average:
         https://arxiv.org/abs/1806.05594
-	.. _SWALP: Stochastic Weight Averaging in Low-Precision Training:
-		https://arxiv.org/abs/1904.11943
-	.. _Stochastic Weight Averaging in Parallel: Large-Batch Training That 
-	    Generalizes Well:
-	    https://arxiv.org/abs/2001.02312
+    .. _SWALP: Stochastic Weight Averaging in Low-Precision Training:
+        https://arxiv.org/abs/1904.11943
+    .. _Stochastic Weight Averaging in Parallel: Large-Batch Training That 
+        Generalizes Well:
+        https://arxiv.org/abs/2001.02312
     """
-    def __init__(self, model):
+    def __init__(self, model, device=None):
         super(AveragedModel, self).__init__()
         self.module = deepcopy(model)
-        self.register_buffer('n_averaged', , torch.tensor(0, dtype=torch.long))
+        if device is not None:
+            self.module = self.module.to(device)
+        self.register_buffer('n_averaged', torch.tensor(0, dtype=torch.long))
         
     def forward(self, *args, **kwargs):
-        self.module = self.module(*args, **kwargs)
+        return self.module(*args, **kwargs)
 
     def update_parameters(self, model):
         for p_swa, p_model in zip(self.parameters(), model.parameters()):
+            device = p_swa.device
             virtual_decay = 1 / float(self.n_averaged + 1)
-            diff = (p_model.data - p_swa.data) * virtual_decay
-                p_swa.data.add_(diff)
+            diff = (p_model.data.to(device) - p_swa.data) * virtual_decay
+            p_swa.data.add_(diff)
         self.n_averaged += 1
 
     
@@ -97,3 +104,22 @@ def _get_momenta(module, momenta):
 def _set_momenta(module, momenta):
     if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
         module.momentum = momenta[module]
+
+
+class SWALR(_LRScheduler):
+    """
+    ToDo
+    """
+    def __init__(self, optimizer, swa_lr, start_epoch=None, last_epoch=-1):
+        self.swa_lr = swa_lr
+        self.start_epoch = start_epoch
+        super(SWALR, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.", UserWarning)
+        
+        if self.start_epoch and self._step_count >= self.start_epoch:
+            return [self.swa_lr for group in self.optimizer.param_groups]
+        return [max(group['lr'], self.swa_lr) for group in self.optimizer.param_groups]
