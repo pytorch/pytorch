db15fe287cecd85dbfef0ba9a567f7c35fe9decb
huangzhiyuan <zhiyuan.huang@intel.com>
2020-03-19 21:21:17 +0800

    remove Zombie Code

--- aten/src/TH/generic/THBlas.h
+++ aten/src/TH/generic/THBlas.h
@@ -3,7 +3,6 @@
 #else
 
 /* Level 1 */
-TH_API void THBlas_(swap)(int64_t n, scalar_t *x, int64_t incx, scalar_t *y, int64_t incy);
 TH_API void THBlas_(scal)(int64_t n, scalar_t a, scalar_t *x, int64_t incx);
 TH_API void THBlas_(copy)(int64_t n, scalar_t *x, int64_t incx, scalar_t *y, int64_t incy);
 TH_API void THBlas_(axpy)(int64_t n, scalar_t a, scalar_t *x, int64_t incx, scalar_t *y, int64_t incy);
