c8f665dcb626378ca9c51d7b70fc242524105441
anjali411 <chourdiaanjali123@gmail.com>
2020-03-18 23:29:55 -0700

    Added type promotion logic for complex numbers (#34093)
    Summary:
    Issue: https://github.com/pytorch/pytorch/issues/33780
    After this PR:
    1. dtype promotion logic will correctly work for ops involving complex scalars
    2. torch.ComplexFloatTensor, torch.ComplexDoubleTensor works
    3. added alias for complex64 (cfloat) and complex128 (cdouble)
    4. added an internal function get_complex_default_dtype (consciously not exposed in public API)
    
    >>> 1j*torch.ones(2)
    tensor([(0.0000 + 1.0000j), (0.0000 + 1.0000j)], dtype=torch.complex64)
    
    >>> torch.set_default_dtype(torch.float64)
    >>> 1j*torch.ones(2)
    tensor([(0.0000 + 1.0000j), (0.0000 + 1.0000j)], dtype=torch.complex128)
    
    >>> 1j + torch.ones(2)
    tensor([(1.0000 + 1.0000j), (1.0000 + 1.0000j)], dtype=torch.complex128)
    
    >>> torch.tensor(1j) + torch.ones(2,2)
    tensor([[(1.0000 + 1.0000j), (1.0000 + 1.0000j)],
            [(1.0000 + 1.0000j), (1.0000 + 1.0000j)]], dtype=torch.complex128)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34093
    
    Differential Revision: D20312366
    
    Pulled By: anjali411
    
    fbshipit-source-id: 90f00a1a916d9c8eeda101eb6e9d250fce569815

--- c10/core/DefaultDtype.cpp
+++ c10/core/DefaultDtype.cpp
@@ -3,12 +3,21 @@
 
 namespace c10 {
 static auto default_dtype = caffe2::TypeMeta::Make<float>();
+static auto default_complex_dtype = caffe2::TypeMeta::Make<std::complex<float>>();
 
 void set_default_dtype(caffe2::TypeMeta dtype) {
   default_dtype = std::move(dtype);
+  if(dtype == caffe2::TypeMeta::Make<double>()) {
+    default_complex_dtype = std::move(caffe2::TypeMeta::Make<std::complex<double>>());
+  } else {
+    default_complex_dtype = std::move(caffe2::TypeMeta::Make<std::complex<float>>());
+  }
 }
 
 const caffe2::TypeMeta& get_default_dtype() {
   return default_dtype;
+}
+const caffe2::TypeMeta& get_default_complex_dtype() {
+  return default_complex_dtype;
 }
 } // namespace c10
