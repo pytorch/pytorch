d05492b012265b723478aa527c9e7dd930e4d237
shihongzhi <shi65881583@gmail.com>
2020-03-20 23:20:40 +0800

    port diag_out

--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -1389,49 +1389,57 @@ Tensor unfold(const Tensor& self, int64_t dimension, int64_t size, int64_t step)
   return self.as_strided(new_size, new_stride);
 }
 
-Tensor diag(const Tensor& self, int64_t dimension) {
+template <typename scalar_t>
+void apply_diag(Tensor& result, const Tensor& self, int64_t dimension) {
   TORCH_CHECK(self.dim() == 1 || self.dim() == 2, "matrix or a vector expected");
 
-  Tensor result = at::empty({0}, self.options());
-  AT_DISPATCH_ALL_TYPES(self.scalar_type(), "diag", [&] {
-    int64_t i;
+  int64_t i;
+  auto self_data = self.data_ptr<scalar_t>();
+  if (self.dim() == 1) {
+    auto self_size = self.size(0);
+    auto self_stride = self.stride(0);
+    int64_t sz = self_size + std::abs(dimension);
+
+    result.resize_({sz, sz});
+    auto r_data = result.data_ptr<scalar_t>();
+    auto r_stride_0 = result.stride(0);
+    auto r_stride_1 = result.stride(1);
+    r_data += (dimension >= 0 ? dimension*r_stride_0 : -dimension*r_stride_1);
     
-    auto self_data = self.data_ptr<scalar_t>();
-    if (self.dim() == 1) {
-      auto self_size = self.size(0);
-      auto self_stride = self.stride(0);
-      int64_t sz = self_size + std::abs(dimension);
-
-      result.resize_({sz, sz});
-      auto r_data = result.data_ptr<scalar_t>();
-      auto r_stride_0 = result.stride(0);
-      auto r_stride_1 = result.stride(1);
-      r_data += (dimension >= 0 ? dimension*r_stride_0 : -dimension*r_stride_1);
-      
-      for (i = 0; i < self_size; i++) {
-        r_data[i * (r_stride_0 + r_stride_1)] = self_data[i * self_stride];
-      }
-    } else {
-      auto self_stride_0 = self.stride(0);
-      auto self_stride_1 = self.stride(1);
-
-      int64_t sz;
-      if (dimension >= 0) {
-        sz = std::min(self.size(0), self.size(1) - dimension);
-      } else {
-        sz = std::min(self.size(0) + dimension, self.size(1));
-      }
-
-      result.resize_({sz});
-      auto r_data = result.data_ptr<scalar_t>();
-      auto r_stride_0 = result.stride(0);
-      self_data += (dimension >= 0 ? dimension * self_stride_1 : -dimension * self_stride_0);
-      for (i = 0; i < sz; i++) {
-        r_data[i * r_stride_0] = self_data[i * (self_stride_0 + self_stride_1)];
-      }
+    for (i = 0; i < self_size; i++) {
+      r_data[i * (r_stride_0 + r_stride_1)] = self_data[i * self_stride];
     }
-  });
+  } else {
+    auto self_stride_0 = self.stride(0);
+    auto self_stride_1 = self.stride(1);
 
+    int64_t sz;
+    if (dimension >= 0) {
+      sz = std::min(self.size(0), self.size(1) - dimension);
+    } else {
+      sz = std::min(self.size(0) + dimension, self.size(1));
+    }
+
+    result.resize_({sz});
+    auto r_data = result.data_ptr<scalar_t>();
+    auto r_stride_0 = result.stride(0);
+    self_data += (dimension >= 0 ? dimension * self_stride_1 : -dimension * self_stride_0);
+    for (i = 0; i < sz; i++) {
+      r_data[i * r_stride_0] = self_data[i * (self_stride_0 + self_stride_1)];
+    }
+  }
+}
+
+Tensor diag(const Tensor& self, int64_t dimension) {
+  Tensor result = at::empty({0}, self.options());
+  at::diag_out(result, self, dimension);
+  return result;
+}
+
+Tensor& diag_out(Tensor &result, const Tensor& self, int64_t dimension) {
+  AT_DISPATCH_ALL_TYPES(self.scalar_type(), "diag", [&] {
+    apply_diag<scalar_t>(result, self, dimension);
+  });
   return result;
 }
 
