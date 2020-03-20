5714052a2fe2b367520a4c33486122e8c5dd696f
Zafar Takhirov <cc.rafaz@zafar.cc>
2020-03-20 16:26:01 -0700

    Update on "quantized Conv1d"
    Differential Revision: [D20555070](https://our.internmc.facebook.com/intern/diff/D20555070)

--- torch/nn/quantized/modules/conv.py
+++ torch/nn/quantized/modules/conv.py
@@ -162,7 +162,7 @@ class Conv1d(_ConvNd):
 
         >>> m = nn.quantized.Conv1d(16, 33, 3, stride=2)
         >>> input = torch.randn(20, 16, 100)
-        >>> # quantize input to qint8
+        >>> # quantize input to quint8
         >>> q_input = torch.quantize_per_tensor(input, scale=1.0, zero_point=0,
                                                 dtype=torch.quint32)
         >>> output = m(q_input)
