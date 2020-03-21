6b945b9471f645302ee58f5ab00f19792ad4d337
Will Feng <willfeng@fb.com>
2020-03-20 21:45:37 -0700

    Fix Conv and ConvTranspose implementation (#35023)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35023
    
    This PR fixes Conv and ConvTranspose implementation to match the Python API implementation.
    
    **TODO**: cherry-pick this PR into v1.5 release branch.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20559889
    
    Pulled By: yf225
    
    fbshipit-source-id: 53783a7398ef968ec6d25b6f568fde44907417c5

--- torch/csrc/api/include/torch/nn/modules/utils.h
+++ torch/csrc/api/include/torch/nn/modules/utils.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <c10/util/ArrayRef.h>
 #include <c10/util/Optional.h>
 
@@ -7,6 +9,21 @@ namespace torch {
 namespace nn {
 namespace modules {
 namespace utils {
+
+// Repeat each element of `t` for `n` times.
+// This can be used to translate padding arg used by Conv and Pooling modules
+// to the ones used by `F::pad`.
+//
+// This mirrors `_repeat_tuple` in `torch/nn/modules/utils.py`.
+inline std::vector<int64_t> _repeat_vector(at::ArrayRef<int64_t> t, int64_t n) {
+  std::vector<int64_t> ret;
+  for (int64_t elem : t) {
+    for (int64_t i = 0; i < n; i++) {
+      ret.emplace_back(elem);
+    }
+  }
+  return ret;
+}
 
 inline std::vector<int64_t> _list_with_default(
   torch::ArrayRef<c10::optional<int64_t>> out_size, torch::IntArrayRef defaults) {
