85c357c2ff890ab793cdff1388bcc6fec4dd654a
Protonu Basu <protonu@fb.com>
2020-03-20 17:15:18 -0700

    use nullptr to address clang-tidy

--- torch/csrc/jit/tensorexpr/cuda_codegen.cpp
+++ torch/csrc/jit/tensorexpr/cuda_codegen.cpp
@@ -934,7 +934,7 @@ void CudaCodeGen::call(const std::vector<CallArg>& args) {
 void CudaCodeGen::CompileToNVRTC(
     const std::string& code,
     const std::string& func_name) {
-  CUcontext pctx = nullptr;
+  CUcontext pctx = 0;
   AT_CUDA_DRIVER_CHECK(nvrtc().cuCtxGetCurrent(&pctx));
   // Note: hacked at::DeviceGuard since at::DeviceGuard was failing to work
   // properly in some scenarios
@@ -947,7 +947,7 @@ void CudaCodeGen::CompileToNVRTC(
   if (!pctx) {
     std::unique_lock<std::mutex> cudaFreeMutexLock(
         *(c10::cuda::CUDACachingAllocator::getFreeMutex()));
-    cudaFree(0);
+    cudaFree(nullptr);
     AT_CUDA_DRIVER_CHECK(nvrtc().cuCtxGetCurrent(&pctx));
   }
   // Acquires device and NVRTC properties (for compile arch and occupancy
