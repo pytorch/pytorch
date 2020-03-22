5306713a362e60e2111cc6a6ab736b45ab7d5d0f
Pavel Belevich <pbelevich@fb.com>
2020-03-21 17:28:33 -0700

    Replace Generator* with Generator that holds std::shared_ptr<GeneratorImpl> (#34468)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34468
    
    This PR prepares `at::Generator` for pybind11's `type_caster<at::Generator>` which is required to implement custom RNG in python. The following changes are done:
    1. `at::Generator` was moved to `c10::GeneratorImpl` (similar to `c10::TensorImpl`)
    2. `at::Generator` was recreated as a holder of `std::shared_ptr<c10::GeneratorImpl>` (similar to `at::Tensor` that holds `c10::intrusive_ptr<c10::TensorImpl>`)
    3. Most of `at::Generator*` usages were replaced with `at::Generator`
    
    TBD: replacing `Generator generator = nullptr` with `{}` requires JIT changes(adding Generator to IValue?)
    
    Differential Revision: D20549420
    
    Pulled By: pbelevich
    
    fbshipit-source-id: 4c92a40eab8f033b359bb6c93f4cd84b07ee8d4e

--- torch/csrc/Generator.cpp
+++ torch/csrc/Generator.cpp
@@ -24,22 +24,19 @@ using namespace torch;
 
 PyObject *THPGeneratorClass = nullptr;
 
-PyObject * THPGenerator_initDefaultGenerator(at::Generator* cdata)
+PyObject * THPGenerator_initDefaultGenerator(at::Generator cdata)
 {
   auto type = (PyTypeObject*)THPGeneratorClass;
   auto self = THPObjectPtr{type->tp_alloc(type, 0)};
   if (!self) throw python_error();
   auto self_ = reinterpret_cast<THPGenerator*>(self.get());
   self_->cdata = cdata;
-  self_->owner = false;
   return self.release();
 }
 
 static void THPGenerator_dealloc(THPGenerator* self)
 {
-  if (self->owner) {
-    delete self->cdata;
-  }
+  self->cdata.~Generator();
   Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
@@ -56,9 +53,9 @@ static PyObject * THPGenerator_pynew(PyTypeObject *type, PyObject *args, PyObjec
   THPGeneratorPtr self((THPGenerator *)type->tp_alloc(type, 0));
 #ifdef USE_CUDA
   if (device.type() == at::kCPU) {
-    self->cdata = new CPUGenerator();
+    self->cdata = make_generator<CPUGenerator>();
   } else if (device.type() == at::kCUDA){
-    self->cdata = new CUDAGenerator(device.index());
+    self->cdata = make_generator<CUDAGenerator>(device.index());
   } else {
     AT_ERROR("Device type ", c10::DeviceTypeName(device.type()),
              " is not supported for torch.Generator() api.");
@@ -67,9 +64,8 @@ static PyObject * THPGenerator_pynew(PyTypeObject *type, PyObject *args, PyObjec
   TORCH_CHECK(device.type() == at::kCPU,
               "Device type ", c10::DeviceTypeName(device.type()),
               " is not supported for torch.Generator() api.");
-  self->cdata = new CPUGenerator();
+  self->cdata = make_generator<CPUGenerator>();
 #endif
-  self->owner = true;
   return (PyObject*)self.release();
   END_HANDLE_TH_ERRORS
 }
