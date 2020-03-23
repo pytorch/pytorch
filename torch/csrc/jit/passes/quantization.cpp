f7c826e23775320af7d8425f721ccaa9a1f00faa
Jerry Zhang <jerryzh@fb.com>
2020-03-23 08:03:10 -0700

    [quant][graphmode][fix] preserve the type of original value when inserting dequant node (#34349)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34349
    
    Set the output type of dequantize node to the type of original value
    this is to fix swap dequantize tensor list
    
    Test Plan:
    .
    
    Imported from OSS
    
    Differential Revision: D20504456
    
    fbshipit-source-id: 9064d7d598a4310e27e2914a072097526448a02c

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -1082,7 +1082,8 @@ void insertDeQuantCall(Graph* graph,
     Node* dequant =
       graph->create(Symbol::aten("dequantize"), {quantized_val});
     dequant->output()->setDebugName(
-        original_val->debugName() + ".dequant." + c10::guts::to_string(i));
+        original_val->debugName() + ".dequant." + c10::guts::to_string(i))
+      ->setType(original_val->type());
     uses[i].user->replaceInputWith(original_val, dequant->output());
     graph->insertNode(dequant);
   }
