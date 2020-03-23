1f1b52ced9fc104bfa9f3e9696587a3375701f48
Taylor Robie <taylorrobie@fb.com>
2020-03-23 13:01:37 -0700

    replace index_select with gather

--- aten/src/ATen/native/Repeat.cpp
+++ aten/src/ATen/native/Repeat.cpp
@@ -17,6 +17,12 @@ static void compute_cpu(int64_t *repeat_ptr, int64_t *cumsum_ptr, int64_t *resul
 
 namespace at { namespace native {
 
+static inline IntArrayRef to_intarrayref(const Tensor& t){
+    TORCH_CHECK(t.dim() == 1, "shape tensor should be a vector");
+    TORCH_CHECK(t.scalar_type() == at::kLong, "shape has to be Long tensor");
+    return IntArrayRef(t.data_ptr<int64_t>(), t.size(0));
+}
+
 Tensor repeat_interleave_cpu(const Tensor &repeat) {
     return repeat_interleave_common<compute_cpu>(repeat);
 }
@@ -37,7 +43,17 @@ Tensor repeat_interleave(const Tensor &self, const Tensor &repeats, c10::optiona
         AT_ERROR("repeats must be 0-dim or 1-dim tensor");
     }
 
-    return input.index_select(dim.value(), at::repeat_interleave(repeats_));
+    Tensor indices = at::repeat_interleave(repeats_);
+
+    Tensor indices_sizes = at::ones({self.dim()}, TensorOptions(at::kLong));
+    indices_sizes[dim.value()] = indices.size(0);
+    indices = indices.reshape(to_intarrayref(indices_sizes));
+
+    Tensor expanded_indices_shape = at::_shape_as_tensor(self).clone();
+    expanded_indices_shape[dim.value()] = -1;
+    indices = indices.expand(to_intarrayref(expanded_indices_shape));
+
+    return input.gather(dim.value(), indices);
 }
 
 Tensor repeat_interleave(const Tensor &self, int64_t repeats, c10::optional<int64_t> dim) {
