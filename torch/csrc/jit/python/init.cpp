0d70db9c6c343e362f86910b5b755669f6e1532d
Meghan Lele <meghanl@fb.com>
2020-03-23 12:28:56 -0700

    [JIT] clang-format JIT code
    Summary:
    This commit runs the newly added tools/clang_format.py on the JIT
    codebase and includes all of the formatting changes thus produced.
    
    Testing:
    Ran the script, CI.
    
    ghstack-source-id: 92a841afaa062b8a36a41f148714794f5f512b5b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35115

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -1,14 +1,11 @@
 #include <torch/csrc/utils/pybind.h>
 
-#include <torch/csrc/jit/runtime/argument_spec.h>
-#include <torch/csrc/jit/runtime/autodiff.h>
-#include <torch/csrc/jit/serialization/export.h>
+#include <torch/csrc/jit/api/module.h>
 #include <torch/csrc/jit/codegen/fuser/interface.h>
 #include <torch/csrc/jit/codegen/fuser/kernel_cache.h>
-#include <torch/csrc/jit/runtime/graph_executor.h>
-#include <torch/csrc/jit/serialization/import.h>
+#include <torch/csrc/jit/frontend/ir_emitter.h>
+#include <torch/csrc/jit/frontend/tracer.h>
 #include <torch/csrc/jit/ir/irparser.h>
-#include <torch/csrc/jit/runtime/operator.h>
 #include <torch/csrc/jit/passes/canonicalize.h>
 #include <torch/csrc/jit/passes/canonicalize_ops.h>
 #include <torch/csrc/jit/passes/common_subexpression_elimination.h>
@@ -16,12 +13,13 @@
 #include <torch/csrc/jit/passes/constant_propagation.h>
 #include <torch/csrc/jit/passes/create_autodiff_subgraphs.h>
 #include <torch/csrc/jit/passes/create_functional_graphs.h>
+#include <torch/csrc/jit/passes/cuda_graph_fuser.h>
 #include <torch/csrc/jit/passes/dead_code_elimination.h>
 #include <torch/csrc/jit/passes/decompose_ops.h>
 #include <torch/csrc/jit/passes/erase_number_types.h>
+#include <torch/csrc/jit/passes/freeze_module.h>
 #include <torch/csrc/jit/passes/fuse_linear.h>
 #include <torch/csrc/jit/passes/graph_fuser.h>
-#include <torch/csrc/jit/passes/cuda_graph_fuser.h>
 #include <torch/csrc/jit/passes/inline_fork_wait.h>
 #include <torch/csrc/jit/passes/inliner.h>
 #include <torch/csrc/jit/passes/loop_unrolling.h>
@@ -46,20 +44,22 @@
 #include <torch/csrc/jit/passes/subgraph_rewrite.h>
 #include <torch/csrc/jit/passes/tensorexpr_fuser.h>
 #include <torch/csrc/jit/passes/utils/check_alias_annotation.h>
-#include <torch/csrc/jit/passes/freeze_module.h>
 #include <torch/csrc/jit/passes/xnnpack_rewrite.h>
-#include <torch/csrc/jit/runtime/print_handler.h>
 #include <torch/csrc/jit/python/pybind_utils.h>
 #include <torch/csrc/jit/python/python_arg_flatten.h>
 #include <torch/csrc/jit/python/python_custom_class.h>
 #include <torch/csrc/jit/python/python_ir.h>
 #include <torch/csrc/jit/python/python_tracer.h>
-#include <torch/csrc/jit/python/script_init.h>
-#include <torch/csrc/jit/frontend/ir_emitter.h>
-#include <torch/csrc/jit/runtime/jit_exception.h>
-#include <torch/csrc/jit/api/module.h>
 #include <torch/csrc/jit/python/python_tree_views.h>
-#include <torch/csrc/jit/frontend/tracer.h>
+#include <torch/csrc/jit/python/script_init.h>
+#include <torch/csrc/jit/runtime/argument_spec.h>
+#include <torch/csrc/jit/runtime/autodiff.h>
+#include <torch/csrc/jit/runtime/graph_executor.h>
+#include <torch/csrc/jit/runtime/jit_exception.h>
+#include <torch/csrc/jit/runtime/operator.h>
+#include <torch/csrc/jit/runtime/print_handler.h>
+#include <torch/csrc/jit/serialization/export.h>
+#include <torch/csrc/jit/serialization/import.h>
 #include <torch/csrc/jit/tensorexpr/execution_counter.h>
 #include <torch/csrc/jit/tensorexpr/kernel.h>
 
@@ -176,10 +176,11 @@ void initJITBindings(PyObject* module) {
              const py::dict& qconfig_dict,
              bool inplace,
              bool is_dynamic) {
-            auto dict = py::cast<std::unordered_map<
-                std::string,
-                std::tuple<Module, Module>>>(qconfig_dict);
-            return InsertObservers(module, method_name, dict, inplace, is_dynamic);
+            auto dict = py::cast<
+                std::unordered_map<std::string, std::tuple<Module, Module>>>(
+                qconfig_dict);
+            return InsertObservers(
+                module, method_name, dict, inplace, is_dynamic);
           },
           py::arg("module"),
           py::arg("method_name"),
@@ -487,19 +488,13 @@ void initJITBindings(PyObject* module) {
           })
       .def(
           "_jit_pass_insert_prepacked_ops",
-          [](script::Module& module) {
-            return insertPrePackedOps(module);
-          })
+          [](script::Module& module) { return insertPrePackedOps(module); })
       .def(
           "_jit_pass_fold_prepacking_ops",
-          [](script::Module& module) {
-            return FoldPrePackingOps(module);
-          })
+          [](script::Module& module) { return FoldPrePackingOps(module); })
       .def(
           "_jit_pass_optimize_for_mobile",
-          [](script::Module& module) {
-            return optimizeForMobile(module);
-          })
+          [](script::Module& module) { return optimizeForMobile(module); })
       .def(
           "_jit_pass_onnx_unpack_quantized_weights",
           [](std::shared_ptr<Graph>& graph,
@@ -572,26 +567,31 @@ void initJITBindings(PyObject* module) {
 
   py::class_<PyTorchStreamWriter>(m, "PyTorchFileWriter")
       .def(py::init<std::string>())
-      .def(py::init([](const py::object &buffer) {
-        auto writer_func = [=](const void *data, size_t size) {
-          auto bytes = py::bytes(reinterpret_cast<const char *>(data), size);
+      .def(py::init([](const py::object& buffer) {
+        auto writer_func = [=](const void* data, size_t size) {
+          auto bytes = py::bytes(reinterpret_cast<const char*>(data), size);
           buffer.attr("write")(std::move(bytes));
           return size;
         };
         return std::make_unique<PyTorchStreamWriter>(std::move(writer_func));
       }))
-      .def(py::init<const std::function<size_t(const void *, size_t)> &>())
-      .def("write_record",
-           [](PyTorchStreamWriter &self, const std::string &name,
-              const char *data,
-              size_t size) { return self.writeRecord(name, data, size); })
+      .def(py::init<const std::function<size_t(const void*, size_t)>&>())
+      .def(
+          "write_record",
+          [](PyTorchStreamWriter& self,
+             const std::string& name,
+             const char* data,
+             size_t size) { return self.writeRecord(name, data, size); })
       .def("write_end_of_file", &PyTorchStreamWriter::writeEndOfFile)
-      .def("write_record",
-           [](PyTorchStreamWriter &self, const std::string &name,
-              uintptr_t data, size_t size) {
-             return self.writeRecord(name, reinterpret_cast<const char *>(data),
-                                     size);
-           });
+      .def(
+          "write_record",
+          [](PyTorchStreamWriter& self,
+             const std::string& name,
+             uintptr_t data,
+             size_t size) {
+            return self.writeRecord(
+                name, reinterpret_cast<const char*>(data), size);
+          });
 
   // This allows PyTorchStreamReader to read from a Python buffer. It requires
   // that the buffer implement `seek()`, `tell()`, and `read()`.
@@ -666,12 +666,14 @@ void initJITBindings(PyObject* module) {
         auto adapter = std::make_unique<BufferAdapter>(std::move(buffer));
         return std::make_unique<PyTorchStreamReader>(std::move(adapter));
       }))
-      .def("get_record", [](PyTorchStreamReader& self, const std::string& key) {
-        at::DataPtr data;
-        size_t size;
-        std::tie(data, size) = self.getRecord(key);
-        return py::bytes(reinterpret_cast<const char*>(data.get()), size);
-      })
+      .def(
+          "get_record",
+          [](PyTorchStreamReader& self, const std::string& key) {
+            at::DataPtr data;
+            size_t size;
+            std::tie(data, size) = self.getRecord(key);
+            return py::bytes(reinterpret_cast<const char*>(data.get()), size);
+          })
       .def("get_all_records", [](PyTorchStreamReader& self) {
         return self.getAllRecords();
       });
@@ -721,14 +723,16 @@ void initJITBindings(PyObject* module) {
           "arguments", [](FunctionSchema& self) { return self.arguments(); })
       .def_property_readonly(
           "returns", [](FunctionSchema& self) { return self.returns(); })
-      .def("is_backward_compatible_with",
+      .def(
+          "is_backward_compatible_with",
           [](const FunctionSchema& self, const FunctionSchema& old_schema) {
             return self.isBackwardCompatibleWith(old_schema);
           })
-      .def("__eq__", [](const FunctionSchema& self,
-            const FunctionSchema& other) {
-          return self == other;
-        })
+      .def(
+          "__eq__",
+          [](const FunctionSchema& self, const FunctionSchema& other) {
+            return self == other;
+          })
       .def("__str__", [](FunctionSchema& self) {
         std::stringstream ss;
         ss << self;
@@ -743,14 +747,14 @@ void initJITBindings(PyObject* module) {
             return (self.N()) ? py::cast(*self.N()) : py::none();
           })
       .def_property_readonly("default_value", [](Argument& self) -> py::object {
-          if (!self.default_value())
-            return py::none();
-          IValue v = *self.default_value();
-          return toPyObject(std::move(v));
-        });
-  m.def(
-      "_jit_get_all_schemas", []() {
-    const std::vector<std::shared_ptr<Operator>>& operations = getAllOperators();
+        if (!self.default_value())
+          return py::none();
+        IValue v = *self.default_value();
+        return toPyObject(std::move(v));
+      });
+  m.def("_jit_get_all_schemas", []() {
+    const std::vector<std::shared_ptr<Operator>>& operations =
+        getAllOperators();
     return fmap(operations, [](const std::shared_ptr<Operator>& op) {
       return op->schema();
     });
--- torch/csrc/jit/runtime/vararg_functions.h
+++ torch/csrc/jit/runtime/vararg_functions.h
@@ -1,8 +1,8 @@
 #pragma once
+#include <ATen/core/List.h>
+#include <ATen/core/functional.h>
 #include <ATen/core/ivalue.h>
 #include <ATen/core/stack.h>
-#include <ATen/core/functional.h>
-#include <ATen/core/List.h>
 
 namespace torch {
 namespace jit {
@@ -26,9 +26,7 @@ void dictConstruct(Stack& stack, at::DictTypePtr type, size_t num_inputs);
 
 void createObject(Stack& stack, at::ClassTypePtr type);
 
-void isinstance(
-    Stack& stack,
-    at::ArrayRef<at::TypePtr> types);
+void isinstance(Stack& stack, at::ArrayRef<at::TypePtr> types);
 
 void tupleSlice(Stack& stack, size_t begin, size_t end);
 
