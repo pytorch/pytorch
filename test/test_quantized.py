f1efe5102876958976b648691ffde422d85b6a0e
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-03-24 15:12:15 -0700

    add quantized version of hardswish operator (#34820)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34820
    
    Adds quantized version of hardswish, for common quantized operator coverage.
    
    Note:
    * we carry over scale and zero_point from the input to the output, because the
      range of the output is unbounded if x > 0
    * we also skip the .out function to not allow the user to specify a custom
      scale+zp (flexible on this).
    
    Test Plan:
    ```
    python test/test_quantized.py
    
    https://gist.github.com/vkuzo/f9b579315ed7f5fdb24839e3218d8465
    ```
    
    Imported from OSS
    
    Differential Revision: D20472905
    
    fbshipit-source-id: 0f2a83e9f5f7b43485fa46caf30e756dc5d492a9

--- test/test_quantized.py
+++ test/test_quantized.py
@@ -364,6 +364,36 @@ class TestQuantizedOps(TestCase):
             op_(qY_hat, min_val, max_val, inplace=True)
             self.assertEqual(qY, qY_hat, message="{} hardtanh failed".format(name))
 
+    """Tests the correctness of the quantized::hardswish op."""
+    @given(X=hu.tensor(shapes=hu.array_shapes(1, 8, 1, 8),
+                       elements=hu.floats(-1e6, 1e6, allow_nan=False, allow_infinity=False),
+                       qparams=hu.qparams()))
+    def test_hardswish(self, X):
+        X, (scale, zero_point, torch_type) = X
+        X = torch.from_numpy(X)
+        qX = torch.quantize_per_tensor(X, scale=scale, zero_point=zero_point,
+                                       dtype=torch_type)
+        dqX = qX.dequantize()
+
+        output_scale = scale
+        output_zero_point = zero_point
+
+        dqY_hat = F.hardswish(dqX)
+        qY_hat = torch.quantize_per_tensor(dqY_hat, scale=output_scale,
+                                           zero_point=output_zero_point,
+                                           dtype=torch_type)
+
+        # regular
+        qY = torch.nn.quantized.functional.hardswish(qX)
+        self.assertEqual(qY, qY_hat,
+                         message="Hardswish failed: {} vs {}".format(qY, qY_hat))
+
+        # inplace
+        qX_copy = qX.clone().detach()
+        torch.nn.quantized.functional.hardswish(qX_copy, inplace=True)
+        self.assertEqual(qX_copy, qY_hat,
+                         message="inplace Hardswish failed: {} vs {}".format(qY, qY_hat))
+
     """Tests the correctness of the scalar addition."""
     @given(A=hu.tensor(shapes=hu.array_shapes(1, 4, 1, 5),
                        elements=hu.floats(-1e6, 1e6, allow_nan=False),
