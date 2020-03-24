1cd2d9767cf50afc9d4703007c3957fd80f7212c
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos581.prn2.facebook.com>
2020-03-24 04:00:53 -0700

    2020-03-24 postnightly release (618c6214aaab1fc478ac576bee8d212b7c35f482)

--- torch/nn/intrinsic/quantized/modules/__init__.py
+++ torch/nn/intrinsic/quantized/modules/__init__.py
@@ -2,9 +2,12 @@
 
 from .linear_relu import LinearReLU
 from .conv_relu import ConvReLU2d, ConvReLU3d
+from .bn_relu import BNReLU2d, BNReLU3d
 
 __all__ = [
     'LinearReLU',
     'ConvReLU2d',
     'ConvReLU3d',
+    'BNReLU2d',
+    'BNReLU3d',
 ]
