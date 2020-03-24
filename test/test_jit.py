44622bbda953abe40eb5620a9131cf75b44e41ad
davidriazati <davidriazati@fb.com>
2020-03-24 13:39:12 -0700

    [jit] Add lazy script decorator (#34935)
    Summary:
    Stacked PRs
     * #34938 - [jit] Remove stray `script`
     * **#34935 - [jit] Add lazy script decorator**
    
    Some users maintain libraries of code that is largely trace-able but not
    script-able. However, some functions may need to be `torch.jit.script`ed if
    they contain control flow so the tracer will use the compiler version.
    This however impacts library start up time as in #33418, so this PR adds
    a workaround in the form of a `torch.jit._lazy_script_while_tracing`
    that will only initialize the compiler if the function is called while
    actually tracing.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34935
    
    Pulled By: driazati
    
    Differential Revision: D20569778
    
    fbshipit-source-id: d87c88c02b1abc86b283729ab8db94285d7d4853

--- test/test_jit.py
+++ test/test_jit.py
@@ -4945,6 +4945,35 @@ def foo(x):
         # shouldn't throw a type error
         torch.jit.script(MyMod())
 
+    @_inline_everything
+    def test_lazy_script(self):
+        def untraceable(x):
+            if x.ndim > 2:
+                print("hello")
+            else:
+                print("goodbye")
+            return x + 2
+
+        # Non-working example
+        def fn(x):
+            return untraceable(x)
+
+        with self.capture_stdout():
+            traced_bad = torch.jit.trace(fn, [torch.ones(2, 2)])
+
+        FileCheck().check_not("goodbye").check_not("hello").run(traced_bad.graph)
+
+        # Working example
+        untraceable = torch.jit._script_if_tracing(untraceable)
+
+        def fn2(x):
+            return untraceable(x)
+
+        with self.capture_stdout():
+            traced = torch.jit.trace(fn, [torch.ones(2, 2)])
+
+        FileCheck().check("goodbye").check("hello").run(traced.graph)
+
     def test_big_int_literals(self):
         def ok():
             # signed 64 bit max
