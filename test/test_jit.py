facbea126d7c93527a3d7138e4c38f95aa8e69bd
Jerry Zhang <jerryzh@fb.com>
2020-03-23 22:28:05 -0700

    [quant][graphmode] quantization support for view, transpose, contiguos (#34854)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/34854
    
    Test Plan:
    python test/test_jit.py
    
    Imported from OSS
    
    Differential Revision: D20524456
    
    fbshipit-source-id: e6e8fc3db6cccbd32c210d04f921274d81996fe2

--- test/test_jit.py
+++ test/test_jit.py
@@ -2155,6 +2155,9 @@ graph(%input, %weight):
                 x = torch.min(x)
                 x = torch.mean(x)
                 x = x.reshape([-1])
+                x = x.view(-1)
+                x = x.transpose(1, 2)
+                x = x.contiguous()
                 x, y = torch.chunk(x, 2)
                 x = F.dropout(x)
                 x = self.dropout(x)
@@ -2178,6 +2181,9 @@ graph(%input, %weight):
                    .check("aten::min") \
                    .check("aten::mean") \
                    .check("aten::reshape") \
+                   .check("aten::view") \
+                   .check("aten::transpose") \
+                   .check("aten::contiguous") \
                    .check("aten::chunk") \
                    .check("prim::ListUnpack") \
                    .check("aten::dropout") \
@@ -2192,6 +2198,9 @@ graph(%input, %weight):
                    .check("aten::min") \
                    .check("aten::mean") \
                    .check("aten::reshape") \
+                   .check("aten::view") \
+                   .check("aten::transpose") \
+                   .check("aten::contiguous") \
                    .check("aten::chunk") \
                    .check("prim::ListUnpack") \
                    .check("aten::dropout") \
