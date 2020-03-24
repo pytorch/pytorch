82308bb502fec1a891a619292c3db4a96c5240e6
neginraoof <neginmr@utexas.edu>
2020-03-23 23:34:26 -0700

    Separate utils tests

--- scripts/onnx/test.sh
+++ scripts/onnx/test.sh
@@ -54,6 +54,8 @@ pytest "${args[@]}" \
   --ignore "$top_dir/test/onnx/test_pytorch_onnx_onnxruntime.py" \
   --ignore "$top_dir/test/onnx/test_custom_ops.py" \
   --ignore "$top_dir/test/onnx/test_models_onnxruntime.py" \
+  --ignore "$top_dir/test/onnx/test_utility_funs.py" \
+  --ignore "$top_dir/test/onnx/test_pytorch_onnx_caffe2_quantized.py" \
   "${test_paths[@]}"
 
 # onnxruntime only support py3
@@ -64,12 +66,14 @@ if [[ "$BUILD_ENVIRONMENT" == *ort1-py3.6* ]]; then
     "$top_dir/test/onnx/test_pytorch_onnx_onnxruntime.py::TestONNXRuntime_opset8" \
     "$top_dir/test/onnx/test_pytorch_onnx_onnxruntime.py::TestONNXRuntime" \
     "$top_dir/test/onnx/test_custom_ops.py" \
-    "$top_dir/test/onnx/test_models_onnxruntime.py"
+    "$top_dir/test/onnx/test_models_onnxruntime.py" \
+    "$top_dir/test/onnx/test_pytorch_onnx_caffe2_quantized.py"
 fi
 if [[ "$BUILD_ENVIRONMENT" == *ort2-py3.6* ]]; then
   # Update the loop for new opsets
   for i in $(seq 10 12); do
     pytest "${args[@]}" \
       "$top_dir/test/onnx/test_pytorch_onnx_onnxruntime.py::TestONNXRuntime_opset$i"
-  done
+  done \
+  "$top_dir/test/onnx/test_utility_funs.py"
 fi
