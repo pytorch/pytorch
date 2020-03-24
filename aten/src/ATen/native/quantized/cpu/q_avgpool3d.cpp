4c3fb380f72b1df8d3cd5e3d46419643342fa194
bhosmer <bhosmer@fb.com>
2020-03-23 22:51:07 -0700

    Update on "Calls to _empty_affine_quantized pass MemoryFormat by TensorOptions"
    This argument will no longer exist in positional form when MemoryFormat
    is moved into TensorOptions by codegen, so we must stop using it when
    we make calls from C++.  This diff eliminates all direct positional
    calls, making them be passed in using TensorOptions.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cpu/q_avgpool3d.cpp
+++ aten/src/ATen/native/quantized/cpu/q_avgpool3d.cpp
@@ -126,7 +126,7 @@ Tensor q_avg_pool3d(
       input_nhwc.options().memory_format(input_nhwc.suggest_memory_format()),
       input_nhwc.q_scale(),
       input_nhwc.q_zero_point(),
-      c10:nullopt);
+      c10::nullopt);
   // fast path for channel last: qavg_pool_2d_nhwc_stub
   if (output_shape.size() == 4) {
     qavg_pool3d_nhwc_stub(
