f8976066306e2673d2b7a937e9269ab6ab61877e
Alban Desmaison <albandes@fb.com>
2020-03-25 17:04:13 -0400

    Implement first draft of autograd benchmark.

---
+++ benchmarks/functional_autograd_benchmark/ppl_models.py
@@ -0,0 +1,134 @@
+import torch
+import torch.distributions as dist
+
+def get_simple_regression(device):
+    N = 10
+    K = 10
+
+    loc_beta = 0.
+    scale_beta = 1.
+
+    beta_prior = dist.Normal(loc_beta, scale_beta)
+
+    X = torch.rand(N, K+1, device=device)
+    Y = torch.rand(N, 1, device=device)
+
+    # X.shape: (N, K+1), Y.shape: (N, 1), beta_value.shape: (K+1, 1)
+    beta_value = beta_prior.sample((K+1, 1))
+    beta_value.requires_grad_(True)
+
+    def forward(beta_value):
+        mu = X.mm(beta_value)
+
+        # We need to compute the first and second gradient of this score with respect
+        # to beta_value.
+        score = dist.Bernoulli(logits=mu).log_prob(Y).sum() + beta_prior.log_prob(beta_value).sum()
+        return score
+
+    return forward, (beta_value.to(device),)
+
+
+def get_robust_regression(device):
+    N = 10
+    K = 10
+
+    # X.shape: (N, K+1), Y.shape: (N, 1)
+    X = torch.rand(N, K+1, device=device)
+    Y = torch.rand(N, 1, device=device)
+
+    # Predefined nu_alpha and nu_beta, nu_alpha.shape: (1, 1), nu_beta.shape: (1, 1)
+    nu_alpha = torch.randn(1, 1, device=device)
+    nu_beta = torch.rand(1, 1, device=device)
+    nu = dist.Gamma(nu_alpha, nu_beta)
+
+    # Predefined sigma_rate: sigma_rate.shape: (N, 1)
+    sigma_rate = torch.rand(N, 1, device=device)
+    sigma = dist.Exponential(sigma_rate)
+
+    # Predefined beta_mean and beta_sigma: beta_mean.shape: (K+1, 1), beta_sigma.shape: (K+1, 1)
+    beta_mean = torch.rand(K+1, 1, device=device)
+    beta_sigma = torch.rand(K+1, 1, device=device)
+    beta = dist.Normal(beta_mean, beta_sigma)
+
+    nu_value = nu.sample()
+    nu_value.requires_grad_(True)
+
+    sigma_value = sigma.sample()
+    sigma_unconstrained_value = sigma_value.log()
+    sigma_unconstrained_value.requires_grad_(True)
+
+    beta_value = beta.sample()
+    beta_value.requires_grad_(True)
+
+    def forward(nu_value, sigma_unconstrained_value, beta_value):
+        sigma_constrained_value = sigma_unconstrained_value.exp()
+        mu = X.mm(beta_value)
+
+        # For this model, we need to compute the following three scores:
+        # We need to compute the first and second gradient of this score with respect
+        # to nu_value.
+        nu_score = dist.StudentT(nu_value, mu, sigma_constrained_value).log_prob(Y).sum() \
+                    + nu.log_prob(nu_value)
+
+
+
+        # We need to compute the first and second gradient of this score with respect
+        # to sigma_unconstrained_value.
+        sigma_score = dist.StudentT(nu_value, mu, sigma_constrained_value).log_prob(Y).sum() \
+                            + sigma.log_prob(sigma_constrained_value) \
+                            + sigma_unconstrained_value
+
+
+
+        # We need to compute the first and second gradient of this score with respect
+        # to beta_value.
+        beta_score = dist.StudentT(nu_value, mu, sigma_constrained_value).log_prob(Y).sum() \
+                            + beta.log_prob(beta_value)
+
+        return nu_score.sum() + sigma_score.sum() + beta_score.sum()
+
+    return forward, (nu_value.to(device), sigma_unconstrained_value.to(device), beta_value.to(device))
+
+
+def get_test_flakyness():
+    # With predefined N, R, F and Y, where N, R and F are just numbers
+    N = 10
+    R = 10
+    F = 10
+
+    Y = torch.rand(N, 1, device=device)
+
+    NUM_OF_FAILS = torch.arange(0, F + 1, step=1).to(dtype=torch.float32, device=device).expand(R, N, F + 1).transpose(0, -1).transpose(1, -1)
+
+    bad_state_prob = dist.Beta(torch.tensor([1.] * N, device=device), torch.tensor([1.] * N, device=device))
+
+
+    good_state_failure_prob = dist.Beta(torch.tensor([2.] * N, device=device), torch.tensor([8.] * N, device=device))
+
+
+    probs = (good_state_failure_prob().expand(F + 1, R, N)).pow(NUM_OF_FAILS) * (1 - bad_state_prob()).expand(F + 1, R, N)
+    probs[:F, :, :] *= (1 - good_state_failure_prob()).expand(F, R, N)
+    probs[F, :, :] += bad_state_prob().expand(R, N)
+    probs = probs.transpose(0, -1)
+
+    bad_state_prob_value = bad_state_prob.sample()
+    bad_state_prob_value.requires_grad_(True)
+
+    good_state_failure_prob_value = good_state_failure_prob.sample()
+    good_state_failure_prob_value.requires_grad_(True)
+
+    def forward(bad_state_prob_value, good_state_failure_prob_value):
+        # We need to compute the first and second gradient of this score with respect
+        # to bad_state_prob_value.
+        score = dist.Categorical(probs).log_prob(Y) \
+                    + bad_state_prob.log_prob(bad_state_prob_value)
+
+        # We need to compute the first and second gradient of this score with respect
+        # to good_state_failure_prob.
+        score2 = dist.Categorical(probs).log_prob(Y) \
+                    + good_state_failure_prob.log_prob(good_state_failure_prob_value)
+
+        return score + score2
+
+    return forward, (bad_state_prob_value.to(device), good_state_failure_prob_value.to(device))
+
---
+++ benchmarks/functional_autograd_benchmark/torchvision_models.py
@@ -0,0 +1,398 @@
+# Taken from https://github.com/pytorch/vision
+# So that we don't need torchvision to be installed
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from torch.jit.annotations import Dict
+from collections import OrderedDict
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+def _resnet(arch, block, layers, pretrained, progress, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    # if pretrained:
+    #     state_dict = load_state_dict_from_url(model_urls[arch],
+    #                                           progress=progress)
+    #     model.load_state_dict(state_dict)
+    return model
+
+def resnet18(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-18 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained, progress,
+                   **kwargs)
+
+def resnet50(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-50 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
+                   **kwargs)
+
+class IntermediateLayerGetter(nn.ModuleDict):
+    """
+    Module wrapper that returns intermediate layers from a model
+    It has a strong assumption that the modules have been registered
+    into the model in the same order as they are used.
+    This means that one should **not** reuse the same nn.Module
+    twice in the forward if you want this to work.
+    Additionally, it is only able to query submodules that are directly
+    assigned to the model. So if `model` is passed, `model.feature1` can
+    be returned, but not `model.feature1.layer2`.
+    Arguments:
+        model (nn.Module): model on which we will extract the features
+        return_layers (Dict[name, new_name]): a dict containing the names
+            of the modules for which the activations will be returned as
+            the key of the dict, and the value of the dict is the name
+            of the returned activation (which the user can specify).
+    Examples::
+        >>> m = torchvision.models.resnet18(pretrained=True)
+        >>> # extract layer1 and layer3, giving as names `feat1` and feat2`
+        >>> new_m = torchvision.models._utils.IntermediateLayerGetter(m,
+        >>>     {'layer1': 'feat1', 'layer3': 'feat2'})
+        >>> out = new_m(torch.rand(1, 3, 224, 224))
+        >>> print([(k, v.shape) for k, v in out.items()])
+        >>>     [('feat1', torch.Size([1, 64, 56, 56])),
+        >>>      ('feat2', torch.Size([1, 256, 14, 14]))]
+    """
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model, return_layers):
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+    def forward(self, x):
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+class _SimpleSegmentationModel(nn.Module):
+    __constants__ = ['aux_classifier']
+
+    def __init__(self, backbone, classifier, aux_classifier=None):
+        super(_SimpleSegmentationModel, self).__init__()
+        self.backbone = backbone
+        self.classifier = classifier
+        self.aux_classifier = aux_classifier
+
+    def forward(self, x):
+        input_shape = x.shape[-2:]
+        # contract: features is a dict of tensors
+        features = self.backbone(x)
+
+        result = OrderedDict()
+        x = features["out"]
+        x = self.classifier(x)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        result["out"] = x
+
+        if self.aux_classifier is not None:
+            x = features["aux"]
+            x = self.aux_classifier(x)
+            x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+            result["aux"] = x
+
+        return result
+
+class FCN(_SimpleSegmentationModel):
+    """
+    Implements a Fully-Convolutional Network for semantic segmentation.
+    Arguments:
+        backbone (nn.Module): the network used to compute the features for the model.
+            The backbone should return an OrderedDict[Tensor], with the key being
+            "out" for the last feature map used, and "aux" if an auxiliary classifier
+            is used.
+        classifier (nn.Module): module that takes the "out" element returned from
+            the backbone and returns a dense prediction.
+        aux_classifier (nn.Module, optional): auxiliary classifier used during training
+    """
+    pass
+
+class FCNHead(nn.Sequential):
+    def __init__(self, in_channels, channels):
+        inter_channels = in_channels // 4
+        layers = [
+            nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(inter_channels, channels, 1)
+        ]
+
+        super(FCNHead, self).__init__(*layers)
+
+def _segm_resnet(name, backbone_name, num_classes, aux, pretrained_backbone=True):
+    # backbone = resnet.__dict__[backbone_name](
+    #     pretrained=pretrained_backbone,
+    #     replace_stride_with_dilation=[False, True, True])
+    # Hardcoded resnet 50
+    assert backbone_name == "resnet50"
+    backbone = resnet50(
+        pretrained=pretrained_backbone,
+        replace_stride_with_dilation=[False, True, True])
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    if aux:
+        inplanes = 1024
+        aux_classifier = FCNHead(inplanes, num_classes)
+
+    model_map = {
+        # 'deeplabv3': (DeepLabHead, DeepLabV3), # Not used
+        'fcn': (FCNHead, FCN),
+    }
+    inplanes = 2048
+    classifier = model_map[name][0](inplanes, num_classes)
+    base_model = model_map[name][1]
+
+    model = base_model(backbone, classifier, aux_classifier)
+    return model
+
+def _load_model(arch_type, backbone, pretrained, progress, num_classes, aux_loss, **kwargs):
+    if pretrained:
+        aux_loss = True
+    model = _segm_resnet(arch_type, backbone, num_classes, aux_loss, **kwargs)
+    # if pretrained:
+    #     arch = arch_type + '_' + backbone + '_coco'
+    #     model_url = model_urls[arch]
+    #     if model_url is None:
+    #         raise NotImplementedError('pretrained {} is not supported as of now'.format(arch))
+    #     else:
+    #         state_dict = load_state_dict_from_url(model_url, progress=progress)
+    #         model.load_state_dict(state_dict)
+    return model
+
+def fcn_resnet50(pretrained=False, progress=True,
+                 num_classes=21, aux_loss=None, **kwargs):
+    """Constructs a Fully-Convolutional Network model with a ResNet-50 backbone.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on COCO train2017 which
+            contains the same classes as Pascal VOC
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _load_model('fcn', 'resnet50', pretrained, progress, num_classes, aux_loss, **kwargs)
