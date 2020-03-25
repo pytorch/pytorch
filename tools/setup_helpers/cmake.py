340bec282297136a5e103735eb3faa6978b55188
Eli Uriegas <eliuriegas@fb.com>
2020-03-24 18:27:19 -0700

    DO NOT MERGE, getting full signal on #34863
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- tools/setup_helpers/cmake.py
+++ tools/setup_helpers/cmake.py
@@ -207,7 +207,8 @@ class CMake:
         # Store build options that are directly stored in environment variables
         build_options = {
             # The default value cannot be easily obtained in CMakeLists.txt. We set it here.
-            'CMAKE_PREFIX_PATH': distutils.sysconfig.get_python_lib()
+            'CMAKE_PREFIX_PATH': distutils.sysconfig.get_python_lib(),
+            'CUDA_SEPARABLE_COMPILATION': 'YES',
         }
         # Build options that do not start with "BUILD_", "USE_", or "CMAKE_" and are directly controlled by env vars.
         # This is a dict that maps environment variables to the corresponding variable name in CMake.
