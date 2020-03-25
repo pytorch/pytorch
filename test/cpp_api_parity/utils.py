1c04cd6e4b422c7e979d664fdfe0ce89698c72d4
Will Feng <willfeng@fb.com>
2020-03-25 15:18:46 -0400

    Update on "Refactor C++ API parity test mechanism and turn it on in CI again"
    Turn on C++ API parity test again
    
    gh-metadata: pytorch pytorch 35190 gh/yf225/106/head

--- test/cpp_api_parity/utils.py
+++ test/cpp_api_parity/utils.py
@@ -139,7 +139,8 @@ def compute_cpp_args_construction_stmts_and_forward_arg_symbols(test_params):
 
 def serialize_arg_dict_as_script_module(arg_dict):
     arg_dict_flat = {arg_name: arg_value
-            for arg_name, arg_value in arg_dict['input'] + arg_dict['target'] + arg_dict['extra_args'] + arg_dict['other']}
+                     for arg_name, arg_value in
+                     arg_dict['input'] + arg_dict['target'] + arg_dict['extra_args'] + arg_dict['other']}
     arg_dict_module = torch.nn.Module()
     for arg_name, arg_value in arg_dict_flat.items():
         assert isinstance(arg_value, torch.Tensor)
