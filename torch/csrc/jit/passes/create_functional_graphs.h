1e65de742f593040e8e00f7886e23383966b1b90
Elias Ellison <eellison@fb.com>
2020-03-24 23:39:49 -0700

    [JIT] Pass To Safely Remove Aten Inplace Ops (#33186)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33186
    
    This helps create larger functional graphs. It has the potential to increase memory use, so in order to land this on by default we would probably also do a reuse of buffers pass.
    
    This is currently O(n * | Removed Nodes | ) because we have to rebuild the alias Db each time we make a change. This pass is critical to creating functional graphs, so this might be a compelling use case to build incremental updates to alias Db.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20603189
    
    Pulled By: eellison
    
    fbshipit-source-id: 105db52bf38e02188ca6df6d36294466d3309a0a

--- torch/csrc/jit/passes/create_functional_graphs.h
+++ torch/csrc/jit/passes/create_functional_graphs.h
@@ -10,5 +10,9 @@ TORCH_API void CreateFunctionalGraphs(const std::shared_ptr<Graph>& graph);
 
 TORCH_API void InlineFunctionalGraphs(const std::shared_ptr<Graph>& graph);
 
+// Replaces in-place aten ops with their functional equivalents when it can
+// be proven that this does not change graph semantics.
+TORCH_API void RemoveMutation(const std::shared_ptr<Graph>& graph);
+
 } // namespace jit
 } // namespace torch
