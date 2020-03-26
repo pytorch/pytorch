e104c1efcc705344fd287d457320f016f4b31233
Supriya Rao <supriyar@fb.com>
2020-03-26 09:51:26 -0700

    Update on "[quant][graph] Add a new observer type for dynamic quantization"
    Summary:
    In graph mode we need to observer the activation tensor for dynamic quantization. This observer should behave the same way as the quantization functions called in the dynamic operator.
    Currently for qlinear_dynamic we call quant_utils::ChooseQuantizationParams which has its own logic for calculating scale and zero_point.
    We mimic those calculations in the new observer.
    
    Test Plan:
    python test/test_quantization.py ObserverTest
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D20664586](https://our.internmc.facebook.com/intern/diff/D20664586)
    
    [ghstack-poisoned]

--- torch/quantization/observer.py
+++ torch/quantization/observer.py
@@ -495,14 +495,16 @@ class MinMaxDynamicQuantObserver(MinMaxObserver):
             scale = torch.tensor(0.1).to(dtype=torch.float)
             zero_point = 0
 
-        zero_point_from_min = qmin - min_val / float(scale)
-        zero_point_from_max = qmax - max_val / float(scale)
-        zero_point_from_min_error = abs(qmin) - abs(min_val / float(scale))
-        zero_point_from_max_error = abs(qmax) - abs(max_val / float(scale))
+        zero_point_from_min = qmin - min_val / scale.to(dtype=torch.double)
+        zero_point_from_max = qmax - max_val / scale.to(dtype=torch.double)
+        zero_point_from_min_error = abs(qmin) - abs(min_val / scale.to(dtype=torch.double))
+        zero_point_from_max_error = abs(qmax) - abs(max_val / scale.to(dtype=torch.double))
+
         if zero_point_from_min_error < zero_point_from_max_error:
             initial_zero_point = zero_point_from_min
         else:
             initial_zero_point = zero_point_from_max
+
         nudged_zero_point = 0
 
         if initial_zero_point < qmin:
