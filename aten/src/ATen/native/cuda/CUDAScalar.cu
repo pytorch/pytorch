5576e6a988056381e591d3d831df632149d678ea
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2020-03-26 11:48:21 -0500

    Try to pacify Windows.

--- aten/src/ATen/native/cuda/CUDAScalar.cu
+++ aten/src/ATen/native/cuda/CUDAScalar.cu
@@ -13,18 +13,24 @@ namespace native {
 
 Scalar _local_scalar_dense_cuda(const Tensor& self) {
   Scalar r;
+#if HIP_VERSION >= 301
   AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
     at::ScalarType::Half, at::ScalarType::Bool, at::ScalarType::BFloat16, self.scalar_type(), "_local_scalar_dense_cuda", [&] {
         scalar_t value;
         cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-#if HIP_VERSION >= 301
         AT_CUDA_CHECK(hipMemcpyWithStream(&value, self.data_ptr<scalar_t>(), sizeof(scalar_t), cudaMemcpyDeviceToHost, stream));
-#else
-        AT_CUDA_CHECK(cudaMemcpyAsync(&value, self.data_ptr<scalar_t>(), sizeof(scalar_t), cudaMemcpyDeviceToHost, stream));
-        AT_CUDA_CHECK(cudaStreamSynchronize(stream));
-#endif
         r = Scalar(value);
       });
+#else
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
+    at::ScalarType::Half, at::ScalarType::Bool, at::ScalarType::BFloat16, self.scalar_type(), "_local_scalar_dense_cuda", [&] {
+        scalar_t value;
+        cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+        AT_CUDA_CHECK(cudaMemcpyAsync(&value, self.data_ptr<scalar_t>(), sizeof(scalar_t), cudaMemcpyDeviceToHost, stream));
+        AT_CUDA_CHECK(cudaStreamSynchronize(stream));
+        r = Scalar(value);
+      });
+#endif
   return r;
 }
 
