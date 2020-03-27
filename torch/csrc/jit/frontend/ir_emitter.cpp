f18b7c9b6534b6a9564f36991e0d3fd3b78d86e8
Zachary DeVito <zdevito@fb.com>
2020-03-27 14:12:24 -0700

    Fix reporting of error message in toBool
    [ghstack-poisoned]

--- torch/csrc/jit/frontend/ir_emitter.cpp
+++ torch/csrc/jit/frontend/ir_emitter.cpp
@@ -1132,7 +1132,7 @@ struct to_ir {
             }
           }
         }
-        auto expr_out = emitToBool(emitExpr(expr));
+        auto expr_out = emitToBool(expr.range(), emitExpr(expr));
         c10::optional<bool> static_if = c10::nullopt;
         if (expr_out->node()->kind() == aten::is_scripting) {
           static_if = true;
@@ -1292,8 +1292,7 @@ struct to_ir {
 
     return expr_value;
   }
-  Value* emitToBool(Value* v) {
-    SourceRange loc = v->node()->sourceRange();
+  Value* emitToBool(const SourceRange& loc, Value* v) {
     Value* out;
     try {
       auto bool_cast = environment_stack->getSugaredVar("bool", loc);
@@ -1622,7 +1621,7 @@ struct to_ir {
       Value* out;
       if (cond) {
         WithInsertPoint insert(condition_block);
-        out = emitToBool(emitExpr(cond.value()));
+        out = emitToBool(cond.value().range(), emitExpr(cond.value()));
       } else {
         WithInsertPoint insert(n);
         out = graph->insertConstant(true, range);
