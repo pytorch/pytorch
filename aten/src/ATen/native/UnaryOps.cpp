d3c9d69cad7a1f297e4830bc6aab1829a4815d29
Mike Ruberry <mruberry@devfair044.maas>
2020-03-27 21:13:21 -0700

    typo

--- aten/src/ATen/native/UnaryOps.cpp
+++ aten/src/ATen/native/UnaryOps.cpp
@@ -70,7 +70,7 @@ Tensor& abs_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(r
 Tensor abs(const Tensor& self) { return unary_op_impl(self, at::abs_out); }
 Tensor& abs_(Tensor& self) { return unary_op_impl_(self, at::abs_out); }
 
-Tensor& angle_out(Tensor& result, const Tensor& self) { Ureturn unary_op_impl_out(result, self, angle_stub); }
+Tensor& angle_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, angle_stub); }
 Tensor angle(const Tensor& self) { return unary_op_impl(self, at::angle_out); }
 
 Tensor& real_out(Tensor& result, const Tensor& self) { return unary_op_impl_out(result, self, real_stub); }
