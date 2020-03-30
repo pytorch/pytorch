d76bf478ae4cdec6ed8a945ab39049388814fad5
Wanchao Liang <wanchaol@users.noreply.github.com>
2020-03-30 15:14:38 -0700

    [rpc] fix backward compatibility test (#35703)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/35703
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20746388
    
    Pulled By: wanchaol
    
    fbshipit-source-id: 4a336fd4a05a606c5b209e5e62c35c70d35614d4

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -117,7 +117,7 @@ white_list = [
     ('aten::_conv2d_prepack', datetime.date(2020, 4, 1)),
     ('aten::dequantize', datetime.date(2020, 4, 1)),
     ('aten::confirmed_by_owner', datetime.date(2020, 3, 17)),
-    ('aten::owner', datetime.date(2020, 3, 27)),
+    ('aten::owner', datetime.date(2020, 4, 27)),
     ('aten::owner_name', datetime.date(2020, 3, 27)),
     ('_aten', datetime.date(2020, 4, 1)),
     ("prim::shape", datetime.date(2020, 4, 1)),
