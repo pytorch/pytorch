01b8c45dec78e6c2a0c34bd7a49fb5a2143417b4
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2020-03-30 10:45:45 -0500

    Make use of hdot version dependent.

--- caffe2/operators/elementwise_ops.cu
+++ caffe2/operators/elementwise_ops.cu
@@ -9,8 +9,11 @@
 #include "caffe2/utils/conversions.h"
 
 #ifdef __HIPCC__
+#include <hip/hip_version.h>
+#if HIP_VERSION < 210
 // rocblas doesn't fully support fp16 yet
 #define ROCBLAS_FP16 0
+#endif
 #endif
 
 namespace caffe2 {
@@ -116,7 +119,7 @@ void device_reduce<at::Half>(
     int N,
     Tensor* buffer,
     CUDAContext* context) {
-#if defined(__HIPCC__)
+#if HIP_VERSION >= 210
   auto buffer_size = 1;
 
   if (buffer->numel() != buffer_size) {
@@ -137,6 +140,8 @@ void device_reduce<at::Half>(
       reinterpret_cast<const rocblas_half*>(buffer->data<at::Half>()),
       0,
       reinterpret_cast<rocblas_half*>(out)));
+#elif HIP_VERSION < 210
+   CAFFE_THROW("HIP rocblas doesn't fully support fp16 device_reduce yet.");
 #else
   auto buffer_size = 1;
 
