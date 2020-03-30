6f33e279f5c91ef037ec2784f0a37663b800d81d
Aayush Naik <aayushnaik17@gmail.com>
2020-03-30 14:54:51 -0700

    Fix torch complex exp CPU implementation (#35532)

--- test/test_complex.py
+++ test/test_complex.py
@@ -9,5 +9,62 @@ class TestComplexTensor(TestCase):
         # there's no garbage value in the resultant list
         self.assertEqual(torch.zeros((2, 2), dtype=torch.complex64).tolist(), [[0j, 0j], [0j, 0j]])
 
+    def test_exp_with_complex_64(self):
+        import numpy as np
+
+        w = torch.tensor([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=torch.complex64)
+        w = torch.exp(w)
+
+        x = np.array([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=np.complex64)
+        x = np.exp(x)
+
+        self.assertEqual(w * 2j, x * 2j)
+
+    def test_exp_with_complex_128(self):
+        import numpy as np
+
+        w = torch.tensor([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=torch.complex128)
+        w = torch.exp(w)
+
+        x = np.array([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=np.complex128)
+        x = np.exp(x)
+
+        self.assertEqual(w * 2j, x * 2j)
+
+    def test_addition_and_mult_with_complex_64(self):
+        import numpy as np
+
+        w = torch.tensor([3 + 3j], dtype=torch.complex64)
+        w = 1.323j * w + 1.0
+
+        x = np.array([3 + 3j], dtype=np.complex64)
+        x = 1.323j * x + 1.0
+
+        self.assertEqual(w, x)
+
+    def test_exp_and_addition_with_complex_64(self):
+        import numpy as np
+
+        w = torch.tensor([3 + 3j], dtype=torch.complex64)
+        w = torch.exp(w + 1.0)
+
+        x = np.array([3 + 3j], dtype=np.complex64)
+        x = np.exp(x + 1.0)
+
+        self.assertEqual(w, x)
+
+    def test_exp_and_addition_with_complex_128(self):
+        import numpy as np
+
+        w = torch.tensor([3 + 3j], dtype=torch.complex128)
+        w = torch.exp(w + 1.0)
+
+        x = np.array([3 + 3j], dtype=np.complex128)
+        x = np.exp(x + 1.0)
+
+        self.assertEqual(w, x)
+
+
+
 if __name__ == '__main__':
     run_tests()
