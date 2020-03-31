c1ef3ef15792ddb17ffc66afb12ab896238607f8
neginraoof <neginmr@utexas.edu>
2020-03-31 09:04:56 -0700

    clang

--- torch/csrc/jit/passes/onnx/preinline_onnx.cpp
+++ torch/csrc/jit/passes/onnx/preinline_onnx.cpp
@@ -1,9 +1,9 @@
 #include <torch/csrc/jit/passes/onnx/preinline_onnx.h>
-#include <torch/csrc/jit/jit_log.h>
-#include <torch/csrc/jit/frontend/error_report.h>
-#include <torch/csrc/jit/api/module.h>
-#include <torch/csrc/jit/passes/onnx/helper.h>
 #include <c10/util/Exception.h>
+#include <torch/csrc/jit/api/module.h>
+#include <torch/csrc/jit/frontend/error_report.h>
+#include <torch/csrc/jit/jit_log.h>
+#include <torch/csrc/jit/passes/onnx/helper.h>
 
 #include <c10/util/Optional.h>
 #include <algorithm>
@@ -23,7 +23,10 @@ using namespace ::c10::onnx;
 void replaceFunctions(Node* to_replace, Function* callee) {
   if (callee->name() == "interpolate") {
     to_replace->removeInput(0);
-    Node* interpolate_node =  to_replace->owningGraph()->create(Symbol::fromQualString("aten::__interpolate"), {to_replace->inputs()}, to_replace->outputs().size());
+    Node* interpolate_node = to_replace->owningGraph()->create(
+        Symbol::fromQualString("aten::__interpolate"),
+        {to_replace->inputs()},
+        to_replace->outputs().size());
     interpolate_node->output()->copyMetadata(to_replace->output());
     interpolate_node->insertAfter(to_replace);
     to_replace->replaceAllUsesWith(interpolate_node);
@@ -32,7 +35,6 @@ void replaceFunctions(Node* to_replace, Function* callee) {
     return;
   }
 }
-
 
 void PreInlineCalls(Block* block) {
   for (auto it = block->nodes().begin(), end = block->nodes().end();
