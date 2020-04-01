f5872696cbafe1860729a98d14b9cb66ee1eaa3f
Aayush Naik <aayushnaik17@gmail.com>
2020-03-31 19:05:25 -0700

    Update exp() test case

--- test/test_complex.py
+++ test/test_complex.py
@@ -1,5 +1,13 @@
+import math
 import torch
 from torch.testing._internal.common_utils import TestCase, run_tests
+import unittest
+
+TEST_NUMPY = True
+try:
+    import numpy as np
+except ImportError:
+    TEST_NUMPY = False
 
 devices = (torch.device('cpu'), torch.device('cuda:0'))
 
@@ -9,61 +17,17 @@ class TestComplexTensor(TestCase):
         # there's no garbage value in the resultant list
         self.assertEqual(torch.zeros((2, 2), dtype=torch.complex64).tolist(), [[0j, 0j], [0j, 0j]])
 
-    def test_exp_with_complex_64(self):
-        import numpy as np
-
-        w = torch.tensor([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=torch.complex64)
-        w = torch.exp(w)
-
-        x = np.array([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=np.complex64)
-        x = np.exp(x)
-
-        self.assertEqual(w * 2j, x * 2j)
-
-    def test_exp_with_complex_128(self):
-        import numpy as np
-
-        w = torch.tensor([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=torch.complex128)
-        w = torch.exp(w)
-
-        x = np.array([0 + 0j, 1 + 1j, 2 + 2j, 3 + 3j], dtype=np.complex128)
-        x = np.exp(x)
-
-        self.assertEqual(w * 2j, x * 2j)
-
-    def test_addition_and_mult_with_complex_64(self):
-        import numpy as np
-
-        w = torch.tensor([3 + 3j], dtype=torch.complex64)
-        w = 1.323j * w + 1.0
-
-        x = np.array([3 + 3j], dtype=np.complex64)
-        x = 1.323j * x + 1.0
-
-        self.assertEqual(w, x)
-
-    def test_exp_and_addition_with_complex_64(self):
-        import numpy as np
-
-        w = torch.tensor([3 + 3j], dtype=torch.complex64)
-        w = torch.exp(w + 1.0)
-
-        x = np.array([3 + 3j], dtype=np.complex64)
-        x = np.exp(x + 1.0)
-
-        self.assertEqual(w, x)
-
-    def test_exp_and_addition_with_complex_128(self):
-        import numpy as np
-
-        w = torch.tensor([3 + 3j], dtype=torch.complex128)
-        w = torch.exp(w + 1.0)
-
-        x = np.array([3 + 3j], dtype=np.complex128)
-        x = np.exp(x + 1.0)
-
-        self.assertEqual(w, x)
-
+    @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
+    def test_exp(self):
+        def exp_fn(dtype):
+            a = (1j * torch.tensor([0, 1 + 1j, 2 + 2j, 3 + 3j], dtype=dtype) /
+                 3 * math.pi)
+            expected = np.exp(a.numpy())
+            actual = torch.exp(a)
+            self.assertEqual(actual, torch.from_numpy(expected))
+        
+        exp_fn(torch.complex64)
+        exp_fn(torch.complex128)
 
 
 if __name__ == '__main__':
