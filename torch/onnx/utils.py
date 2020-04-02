2ee652679132868c98d2bdcb0f47a7de5ac3377b
neginraoof <neginmr@utexas.edu>
2020-04-02 11:58:47 -0700

    Add pass to utils

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -113,6 +113,7 @@ def _split_tensor_list_constants(g, block):
 
 def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=False, fixed_batch_size=False, params_dict=None):
     # Inline everyting
+    torch._C._jit_pass_onnx_preinline(graph)
     torch._C._jit_pass_inline(graph)
 
     # Remove fork/wait nodes
