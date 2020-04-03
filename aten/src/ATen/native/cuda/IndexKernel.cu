61e71e0315656166179bbb983bff97b77c6ff99d
Kurt Mohler <kmohler@quansight.com>
2020-04-03 15:05:16 -0500

    Cleanup unnecessary code

--- aten/src/ATen/native/cuda/IndexKernel.cu
+++ aten/src/ATen/native/cuda/IndexKernel.cu
@@ -6,9 +6,6 @@
 #include <ATen/native/cuda/Loops.cuh>
 #include <ATen/core/Array.h>
 #include <ATen/ExpandUtils.h>
-#include <thrust/scan.h>
-#include <thrust/execution_policy.h>
-#include <thrust/device_ptr.h>
 
 namespace at { namespace native {
 
@@ -95,46 +92,6 @@ static void index_put_kernel(TensorIterator& iter, IntArrayRef index_size, IntAr
     using dtype = OpaqueType<sizeof(scalar_t)>;
     index_put_kernel_impl<dtype>(iter, index_size, index_stride);
   });
-}
-
-template<typename scalar_t, typename mask_t>
-__global__ void masked_select_out_cuda_kernel(
-  scalar_t* result_ptr,
-  scalar_t* self_ptr,
-  mask_t* mask_ptr,
-  int64_t* mask_inclusive_scan_ptr,
-  int64_t num_input_elements
-) {
-  mask_t mask[thread_work_size];
-  int64_t base_index = block_work_size * blockIdx.x;
-  int remaining = std::min<int64_t>(num_input_elements - base_index, block_work_size);
-
-  // load data into registers
-  int thread_idx = threadIdx.x;
-  #pragma unroll
-  for(int i = 0; i < thread_work_size; i++) {
-    if (thread_idx >= remaining) {
-      break;
-    }
-    int64_t input_idx = thread_idx + base_index;
-    mask[i] = mask_ptr[input_idx];
-    thread_idx += num_threads;
-  }
-
-  // compute and store
-  thread_idx = threadIdx.x;
-  #pragma unroll
-  for(int i = 0; i < thread_work_size; i++) {
-    if (thread_idx >= remaining) {
-      break;
-    }
-    if (mask[i]) {
-      int64_t input_idx = thread_idx + base_index;
-      int64_t result_idx = mask_inclusive_scan_ptr[input_idx] - 1;
-      result_ptr[result_idx] = self_ptr[input_idx];
-    }
-    thread_idx += num_threads;
-  }
 }
 
 static Tensor & masked_select_out_cuda_impl(Tensor & result, const Tensor & self, const Tensor & mask) {
