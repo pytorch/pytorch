f2b39823f6800dd351bf4acebed7b32878f4543e
Christian Sarofeen <csarofeen@nvidia.com>
2020-04-03 18:06:37 -0400

    Don't remove guards around gpu tests.

--- test/cpp/jit/test_gpu.cpp
+++ test/cpp/jit/test_gpu.cpp
@@ -1,4 +1,4 @@
-//#if defined(USE_CUDA)
+#if defined(USE_CUDA)
 #include <test/cpp/jit/test_base.h>
 
 #include <torch/csrc/jit/codegen/cuda/arith.h>
@@ -1004,4 +1004,4 @@ void testGPU_Fusion() {}
 
 } // namespace jit
 } // namespace torch
-//#endif // #if defined(USE_CUDA)
+#endif // #if defined(USE_CUDA)
