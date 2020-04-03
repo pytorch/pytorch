4d0f51d3a27d24a4cb393607eac732a28bee8b2f
Pavel Belevich <pbelevich@fb.com>
2020-04-03 16:31:42 -0700

    Move uniform_() to DistributionTemplates(Migrate uniform_ from TH to ATen) (#35580)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35580
    
    `uniform_kernel_cpu` is based on https://github.com/pytorch/pytorch/pull/30954
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20820221
    
    Pulled By: pbelevich
    
    fbshipit-source-id: 13f9fc8fc75b0e9fb48021f2ac08dcb38212a53f

--- aten/src/ATen/native/DistributionTemplates.h
+++ aten/src/ATen/native/DistributionTemplates.h
@@ -92,8 +92,6 @@ static void check_from_to_in_range(int64_t from, int64_t to_inc, caffe2::TypeMet
   }
 }
 
-#undef CHECK_OUT_OF_BOUNDS_AND_SHOW_WARNING
-
 template<template<typename> class random_from_to_kernel, typename RNG>
 at::Tensor& random_from_to_impl(at::Tensor& self, int64_t from, c10::optional<int64_t> to_opt, at::Generator generator) {
   uint64_t range = 0;
@@ -261,5 +259,30 @@ Tensor normal_impl(const Tensor& mean, const Tensor& std, Generator gen) {
   normal_out_impl<normal_kernel, RNG>(ret, mean, std, gen);
   return ret;
 }
+
+// ==================================================== Uniform =======================================================
+
+template<template<typename> class uniform_kernel, typename RNG>
+at::Tensor& uniform_impl_(at::Tensor& self, double from, double to, at::Generator generator) {
+  AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, self.scalar_type(), "check_uniform_bounds", [&] {
+    const auto dtype = self.dtype();
+    const auto min = static_cast<double>(std::numeric_limits<scalar_t>::lowest());
+    const auto max = static_cast<double>(std::numeric_limits<scalar_t>::max());
+    CHECK_OUT_OF_BOUNDS_AND_SHOW_WARNING(from, "from", min, max, dtype);
+    CHECK_OUT_OF_BOUNDS_AND_SHOW_WARNING(to, "to", min, max, dtype);
+    TORCH_CHECK(from <= to, "uniform_ expects to return a [from, to) range, but found from=", from, " > to=", to);
+    TORCH_CHECK((to - from) <= std::numeric_limits<scalar_t>::max(),
+          "uniform_ expects to-from <= std::numeric_limits<", toString(self.scalar_type()),
+          ">::max(), but found to=", to, " and from=", from,
+          " which result in to-from to exceed the limit");
+    from = std::min(std::max(from, min), max);
+    to = std::max(std::min(to, max), min);
+  });
+  auto iter = at::TensorIterator::nullary_op(self);
+  uniform_kernel<RNG>()(iter, from, to, generator);
+  return self;
+}
+
+#undef CHECK_OUT_OF_BOUNDS_AND_SHOW_WARNING
 
 }}}
