34f8d3fa23ba182425bfc6dc9bc524e61aecd777
Raghu Krishnamoorthi <raghuramank@gmail.com>
2020-04-03 18:23:47 -0700

    Fix NaN error in dynamic quantization in qLinear, re-enable test_quantized_rnn
    When scale is very small (less than float eps, but greater than minimum double precision value), computation of reciprocal of scale in floating point precision within FBGEMM returns inf, while QuantUtils does not. Changed computation in QuantUtils to occur with floating point precision to re-enable tests.
    
    Differential Revision: [D20853000](https://our.internmc.facebook.com/intern/diff/D20853000/)
    
    [ghstack-poisoned]

--- test/quantization/test_quantization.py
+++ test/quantization/test_quantization.py
@@ -575,7 +575,6 @@ class PostTrainingDynamicQuantTest(QuantizationTestCase):
         model = quantize_dynamic(NestedModel().eval(), qconfig_dict)
         checkQuantized(model)
 
-    @unittest.skip("temporarily disable the test")
     @given(qengine=st.sampled_from(("fbgemm",)))
     def test_quantized_rnn(self, qengine):
         d_in, d_hid = 2, 2
