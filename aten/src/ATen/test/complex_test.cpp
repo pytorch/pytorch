a3f465ea93f4ec1da1c34f18f33e640029b9af13
Hong Xu <hong@topbug.net>
2020-04-03 19:55:04 -0700

    Remove nonexistent op variable in complex tests. (#35722)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/35722
    
    Differential Revision: D20851653
    
    Pulled By: EscapeZero
    
    fbshipit-source-id: 42f23f4150d0bf501e0e03f0802dd3e3c5fa60f5

--- aten/src/ATen/test/complex_test.cpp
+++ aten/src/ATen/test/complex_test.cpp
@@ -17,10 +17,10 @@ static void TestBinaryOpsForIntType(T real, T img, int_t num) {
 
 template<typename T>
 static void TestBinaryOpsForAllIntTypes(T real, T img, int8_t i) {
-  TestBinaryOpsForIntType<T, int8_t>(real, img, i, op);
-  TestBinaryOpsForIntType<T, int16_t>(real, img, i, op);
-  TestBinaryOpsForIntType<T, int32_t>(real, img, i, op);
-  TestBinaryOpsForIntType<T, int64_t>(real, img, i, op);
+  TestBinaryOpsForIntType<T, int8_t>(real, img, i);
+  TestBinaryOpsForIntType<T, int16_t>(real, img, i);
+  TestBinaryOpsForIntType<T, int32_t>(real, img, i);
+  TestBinaryOpsForIntType<T, int64_t>(real, img, i);
 }
 
 TEST(ComplexTest, Integer) {
