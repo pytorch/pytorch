92a5d4f1c91ddb82415d4aacfc760c0ddb21c28d
Sebastian Messmer <messmer@fb.com>
2020-04-04 14:11:13 -0700

    Rename boxing/unboxing files and utilities (#35411)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35411
    
    The file and class names in ATen/core/boxing were quite confusing.
    Let's rename them for readability.
    
    Also move function schema inference out of the boxing logic into op_registration.h where it belongs.
    ghstack-source-id: 101539206
    
    Test Plan: waitforsandcastle
    
    Differential Revision: D20653621
    
    fbshipit-source-id: 6a79c73d5758bee1e072d543c030913b18a69c7c

--- aten/src/ATen/core/boxing/KernelFunction_test.cpp
+++ aten/src/ATen/core/boxing/KernelFunction_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <ATen/core/boxing/KernelFunction.h>
-#include <ATen/core/boxing/test_helpers.h>
+#include <ATen/core/boxing/impl/test_helpers.h>
 #include <ATen/core/op_registration/op_registration.h>
 
 using std::vector;
