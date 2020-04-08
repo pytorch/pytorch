e197a54e9f5eb723d9836456a640a175a146c3ff
Eli Uriegas <eliuriegas@fb.com>
2020-04-08 16:37:24 -0700

    .circleci: Add binary builds/tests to run on release branches (#36283)
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .circleci/cimodel/data/binary_build_definitions.py
+++ .circleci/cimodel/data/binary_build_definitions.py
@@ -67,9 +67,14 @@ class Conf(object):
             job_def["requires"].append("update_s3_htmls_for_nightlies_devtoolset7")
             job_def["filters"] = {"branches": {"only": "postnightly"}}
         else:
+            filter_branches = ["nightly"]
+            # we only want to add the release branch filter if we aren't
+            # uploading
+            if phase not in ["upload"]:
+                filter_branches.append(r"/release\/.*/")
             job_def["filters"] = {
                 "branches": {
-                    "only": "nightly"
+                    "only": filter_branches
                 },
                 # Will run on tags like v1.5.0-rc1, etc.
                 "tags": {
