b3709f0f8f23ad0964ab3b95dc0e688e7f203053
Christian Sarofeen <csarofeen@nvidia.com>
2020-04-07 22:08:12 -0400

    Clang again.

--- torch/csrc/jit/codegen/cuda/type.cpp
+++ torch/csrc/jit/codegen/cuda/type.cpp
@@ -9,8 +9,12 @@ namespace fuser {
 
 // Return highest on list (smallest enum val)
 DataType promote_type(const DataType& t1, const DataType& t2) {
-  TORCH_CHECK(DataType::Null != t1 && DataType::Null != t2,
-      "Expected promotable DataTypes but got: ", t1, " and ", t2);
+  TORCH_CHECK(
+      DataType::Null != t1 && DataType::Null != t2,
+      "Expected promotable DataTypes but got: ",
+      t1,
+      " and ",
+      t2);
   return t1 < t2 ? t1 : t2;
 }
 
@@ -18,7 +22,10 @@ DataType promote_type(const DataType& t1, const DataType& t2) {
 ValType promote_type(const ValType& t1, const ValType& t2) {
   TORCH_CHECK(
       t1 >= ValType::TensorView && t2 >= ValType::TensorView,
-      "Expected promotable ValTypes but got: ", t1, " and ", t2);
+      "Expected promotable ValTypes but got: ",
+      t1,
+      " and ",
+      t2);
   // Check that it's a promotable type (with dtype)
   // static_assert??
   return t1 < t2 ? t1 : t2;
