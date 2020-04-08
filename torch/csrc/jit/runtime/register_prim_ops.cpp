pytorch/pytorch

commit 4b916b6b7504c6a2c166bb463a0b28ad6fb73c0e

Mark every frame with a unique id (#33788)
Summary:
This PR introduces frame ids that will allow us to associate profiling information with its corresponding run.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/33788

Differential Revision: D20164897

Pulled By: Krovatkin

fbshipit-source-id: 8172ff9f4d188b339e2ff98a80bbe4a2b306a8aa

torch/csrc/jit/runtime/register_prim_ops.cpp
@@ -673,17 +673,17 @@ RegisterOperators reg(
          aliasAnalysisSpecialCase()),
      Operator(
          "prim::BroadcastSizes(...) -> int[]",
-         [](Stack& stack) {
-           auto num_inputs = pop(stack).toInt();
-           std::vector<int64_t> size;
-           size.reserve(8);
-           for (auto i = 0; i < num_inputs; ++i) {
-             size =
-                 at::infer_size(size, peek(stack, i, num_inputs).toIntVector());
-           }
-           drop(stack, num_inputs);
-           push(stack, IValue(std::move(size)));
-           return 0;
+          [](Stack& stack) {
+             auto num_inputs = pop(stack).toInt();
+             std::vector<int64_t> size;
+             size.reserve(8);
+             for (auto i = 0; i < num_inputs; ++i) {
+               size = at::infer_size(
+                   size, peek(stack, i, num_inputs).toIntVector());
+             }
+             drop(stack, num_inputs);
+             push(stack, IValue(std::move(size)));
+             return 0;
          },
          aliasAnalysisSpecialCase()),
      Operator(
@@ -719,9 +719,9 @@ RegisterOperators reg(
      Operator(
          "aten::warn(str message, int stacklevel=2) -> ()",
          [](Stack& stack) {
-           TORCH_CHECK(
+            TORCH_CHECK(
                false, "warn is implemented directly in the interpreter");
-           return 0;
+            return 0;
          },
          aliasAnalysisFromSchema()),
 
@@ -754,31 +754,31 @@ RegisterOperators reg(
          aliasAnalysisSpecialCase()),
      Operator(
          "prim::AutogradAnyNonZero(...) -> bool",
-         [](Stack& stack) {
-           auto num_inputs = pop(stack).toInt();
-           bool result = false;
-           for (const IValue& v : last(stack, num_inputs)) {
-             if (v.isTensor()) {
-               if (v.toTensor().defined()) {
-                 result = true;
-                 break;
-               }
-             } else if (v.isTensorList()) {
-               for (const at::Tensor& t : v.toTensorVector()) {
-                 if (t.defined()) {
+           [](Stack& stack) {
+             auto num_inputs = pop(stack).toInt();
+             bool result = false;
+             for (const IValue& v : last(stack, num_inputs)) {
+               if (v.isTensor()) {
+                 if (v.toTensor().defined()) {
                    result = true;
+                   break;
                  }
+               } else if (v.isTensorList()) {
+                 for (const at::Tensor& t : v.toTensorVector()) {
+                   if (t.defined()) {
+                     result = true;
+                   }
+                 }
+                 if (result) {
+                   break;
+                 }
+               } else {
+                 TORCH_INTERNAL_ASSERT(false);
                }
-               if (result) {
-                 break;
-               }
-             } else {
-               TORCH_INTERNAL_ASSERT(false);
              }
-           }
-           drop(stack, num_inputs);
-           stack.emplace_back(result);
-           return 0;
+             drop(stack, num_inputs);
+             stack.emplace_back(result);
+             return 0;
          },
          aliasAnalysisFromSchema()),
      Operator(
@@ -828,8 +828,7 @@ RegisterOperators reg(
          },
          aliasAnalysisFromSchema()),
      Operator(
-         // note the compiler knows to type TupleIndex more accurately than it
-         // is listed here.
+         // note the compiler knows to type TupleIndex more accurately than it is listed here.
          "prim::TupleIndex(Any tup, int i) -> Any",
          [](Stack& stack) {
            int64_t index = pop(stack).toInt();
@@ -843,11 +842,10 @@ RegisterOperators reg(
            return 0;
          },
          aliasAnalysisSpecialCase()),
-     Operator(
-         "prim::TupleUnpack(Any tup) -> ...",
+      Operator("prim::TupleUnpack(Any tup) -> ...",
          [](Stack& stack) {
-           tupleUnpack(stack);
-           return 0;
+             tupleUnpack(stack);
+             return 0;
          },
          aliasAnalysisSpecialCase()),
      Operator(
@@ -983,10 +981,7 @@ RegisterOperators reg(
          "prim::unchecked_unwrap_optional(t(a)? optional) -> t(a)",
          noop,
          aliasAnalysisFromSchema()),
-     Operator(
-         "prim::unchecked_cast(t x) -> t",
-         noop,
-         aliasAnalysisSpecialCase()),
+     Operator("prim::unchecked_cast(t x) -> t", noop, aliasAnalysisSpecialCase()),
      Operator(
          "aten::wait(Future(t) self) -> t",
          [](Stack& stack) {

torch/csrc/jit/runtime/instruction.h
@@ -36,6 +36,7 @@ namespace jit {
   _(CALL, "F") /* call function X */                                        \
   _(GUARD, "T") /* check a guard against type_table, true if passes */      \
   _(FAIL_GUARD, "T") /* fail a guard, patch back to GUARD */                \
+  _(PROFILE_OP, "F") /* get a callback from profile_function_table at X */  \
   _(TAIL_CALL, "F") /* replace current frame with function F */             \
   _(INTERFACE_CALL, "CI") /* call method X on the first argument (of N) */  \
   _(GET_ATTR, "S") /* get attribute from slot X in an Object */             \

torch/csrc/jit/runtime/interpreter.cpp
@@ -373,6 +373,8 @@ struct CodeImpl {
   std::vector<Function*> function_table_;
   std::vector<TypePtr> type_table_;
   std::vector<Code> code_table_;
+  std::vector<std::function<void(std::vector<IValue>&)>>
+      profile_function_table_;
 
   int register_size_ = 0;
   size_t n_outputs;
@@ -681,6 +683,12 @@ struct CodeImpl {
     createBailoutBlock(jf_index);
   }
 
+  void emitProfile(Node* node) {
+    emitLoadInputs(node->inputs());
+    insertInstruction(PROFILE_OP, profile_function_table_.size());
+    profile_function_table_.push_back(node->cast<ProfileOp>()->getCallback());
+  }
+
   void emitGetAttr(Node* node) {
     emitLoadInputs(node->inputs());
     const auto type = node->input()->type()->expect<ClassType>();
@@ -812,6 +820,9 @@ struct CodeImpl {
       case prim::BailOut:
         emitBailOut(node);
         break;
+      case prim::profile:
+        emitProfile(node);
+        break;
       case prim::GetAttr:
         emitGetAttr(node);
         break;
@@ -926,6 +937,10 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
     // to replace the current frame
     // with a frame of a bailout graph
     size_t base_pointer;
+
+    // unique to every frame with prim::profile across all threads
+    c10::optional<size_t> id;
+    static std::atomic<size_t> num_frames;
   };
 
   // saved-by-value stuff that can exist on the stack inside runInterpreter
@@ -935,6 +950,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
     IValue* constants;
     Operation* operators;
     Function** functions;
+    std::function<void(std::vector<IValue>&)>* profile_functions;
     TypePtr* types;
 
     ActiveFrame(const Frame& frame)
@@ -943,6 +959,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
           constants(frame.function->constant_table_.data()),
           operators(frame.function->operator_table_.data()),
           functions(frame.function->function_table_.data()),
+          profile_functions(frame.function->profile_function_table_.data()),
           types(frame.function->type_table_.data()) {}
   };
 
@@ -954,7 +971,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
   }
 
   void enterFrame(const Code& code, size_t base_pointer) {
-    frames.emplace_back(Frame{code.pImpl, 0, base_pointer});
+    frames.emplace_back(Frame{code.pImpl, 0, base_pointer, c10::nullopt});
     registers.resize(registers.size() + code.pImpl->register_size_);
     // frames.back().function->dump(std::cout);
   }
@@ -1195,6 +1212,17 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
             stack.emplace_back(future->value());
             ++af.pc;
           } break;
+          case PROFILE_OP: {
+            auto& frame_id_ref = frames.back().id;
+            if (!frame_id_ref.has_value()) {
+              frame_id_ref = Frame::num_frames++;
+            }
+            auto callback = af.profile_functions[inst.X];
+            push(stack, c10::IValue{static_cast<int64_t>(*frame_id_ref)});
+            callback(stack);
+            ++af.pc;
+            break;
+          }
           case FAIL_GUARD: {
             // patch FAIL_GUARD back to GUARD
             GRAPH_DEBUG(
@@ -1388,6 +1416,8 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
     }
   }
 };
+
+std::atomic<size_t> InterpreterStateImpl::Frame::num_frames;
 
 std::ostream& operator<<(std::ostream& out, const Code& code) {
   out << *code.pImpl->graph_ << "\n";

torch/csrc/jit/runtime/profiling_record.cpp
@@ -28,6 +28,8 @@ void ProfilingRecord::insertShapeProfile(Node* n, Value* i) {
   pno->setType(TensorType::get());
   std::function<void(Stack&)> shape_profiler =
       [this, pno, first](Stack& stack) mutable {
+        int64_t frame_id;
+        pop(stack, frame_id);
         IValue t;
         pop(stack, t);
         if (t.isTensor()) {
@@ -92,7 +94,9 @@ std::unique_ptr<ProfilingRecord> ProfilingRecord::instrumentGraph(
   ClearProfilingInformation(new_g);
   pr->instrumentBlock(new_g->block());
 
-  std::function<void(Stack&)> counter = [raw_pr](Stack&) {
+  std::function<void(Stack&)> counter = [raw_pr](Stack& stack) {
+    int64_t frame_id;
+    pop(stack, frame_id);
     std::lock_guard<std::mutex> lock(raw_pr->mutex_);
     if (raw_pr->profiling_count_ > 0) {
       raw_pr->profiling_count_--;

torch/csrc/jit/runtime/register_prim_ops_fulljit.cpp
@@ -69,8 +69,9 @@ RegisterOperators reg({
         prim::profile,
         [](const Node* node) -> Operation {
           auto callback = node->cast<ProfileOp>()->getCallback();
-          return [callback](Stack& stack) {
-            callback(stack);
+          return [](Stack& stack) {
+            AT_ERROR(
+                "Must be lowered to Interpreter's PROFILE instruction"); // NOLINT
             return 0;
           };
         },

commit 51d15521e94166554699ef70d9888e6f5bb722db

Fix clang-format broken by https://github.com/pytorch/pytorch/pull/33788
Test Plan: CI

torch/csrc/jit/runtime/register_prim_ops.cpp
@@ -673,17 +673,17 @@ RegisterOperators reg(
          aliasAnalysisSpecialCase()),
      Operator(
          "prim::BroadcastSizes(...) -> int[]",
-          [](Stack& stack) {
-             auto num_inputs = pop(stack).toInt();
-             std::vector<int64_t> size;
-             size.reserve(8);
-             for (auto i = 0; i < num_inputs; ++i) {
-               size = at::infer_size(
-                   size, peek(stack, i, num_inputs).toIntVector());
-             }
-             drop(stack, num_inputs);
-             push(stack, IValue(std::move(size)));
-             return 0;
+         [](Stack& stack) {
+           auto num_inputs = pop(stack).toInt();
+           std::vector<int64_t> size;
+           size.reserve(8);
+           for (auto i = 0; i < num_inputs; ++i) {
+             size =
+                 at::infer_size(size, peek(stack, i, num_inputs).toIntVector());
+           }
+           drop(stack, num_inputs);
+           push(stack, IValue(std::move(size)));
+           return 0;
          },
          aliasAnalysisSpecialCase()),
      Operator(
@@ -719,9 +719,9 @@ RegisterOperators reg(
      Operator(
          "aten::warn(str message, int stacklevel=2) -> ()",
          [](Stack& stack) {
-            TORCH_CHECK(
+           TORCH_CHECK(
                false, "warn is implemented directly in the interpreter");
-            return 0;
+           return 0;
          },
          aliasAnalysisFromSchema()),
 
@@ -754,31 +754,31 @@ RegisterOperators reg(
          aliasAnalysisSpecialCase()),
      Operator(
          "prim::AutogradAnyNonZero(...) -> bool",
-           [](Stack& stack) {
-             auto num_inputs = pop(stack).toInt();
-             bool result = false;
-             for (const IValue& v : last(stack, num_inputs)) {
-               if (v.isTensor()) {
-                 if (v.toTensor().defined()) {
-                   result = true;
-                   break;
-                 }
-               } else if (v.isTensorList()) {
-                 for (const at::Tensor& t : v.toTensorVector()) {
-                   if (t.defined()) {
-                     result = true;
-                   }
-                 }
-                 if (result) {
-                   break;
-                 }
-               } else {
-                 TORCH_INTERNAL_ASSERT(false);
+         [](Stack& stack) {
+           auto num_inputs = pop(stack).toInt();
+           bool result = false;
+           for (const IValue& v : last(stack, num_inputs)) {
+             if (v.isTensor()) {
+               if (v.toTensor().defined()) {
+                 result = true;
+                 break;
                }
+             } else if (v.isTensorList()) {
+               for (const at::Tensor& t : v.toTensorVector()) {
+                 if (t.defined()) {
+                   result = true;
+                 }
+               }
+               if (result) {
+                 break;
+               }
+             } else {
+               TORCH_INTERNAL_ASSERT(false);
              }
-             drop(stack, num_inputs);
-             stack.emplace_back(result);
-             return 0;
+           }
+           drop(stack, num_inputs);
+           stack.emplace_back(result);
+           return 0;
          },
          aliasAnalysisFromSchema()),
      Operator(
@@ -828,7 +828,8 @@ RegisterOperators reg(
          },
          aliasAnalysisFromSchema()),
      Operator(
-         // note the compiler knows to type TupleIndex more accurately than it is listed here.
+         // note the compiler knows to type TupleIndex more accurately than it
+         // is listed here.
          "prim::TupleIndex(Any tup, int i) -> Any",
          [](Stack& stack) {
            int64_t index = pop(stack).toInt();
@@ -842,10 +843,11 @@ RegisterOperators reg(
            return 0;
          },
          aliasAnalysisSpecialCase()),
-      Operator("prim::TupleUnpack(Any tup) -> ...",
+     Operator(
+         "prim::TupleUnpack(Any tup) -> ...",
          [](Stack& stack) {
-             tupleUnpack(stack);
-             return 0;
+           tupleUnpack(stack);
+           return 0;
          },
          aliasAnalysisSpecialCase()),
      Operator(
@@ -981,7 +983,10 @@ RegisterOperators reg(
          "prim::unchecked_unwrap_optional(t(a)? optional) -> t(a)",
          noop,
          aliasAnalysisFromSchema()),
-     Operator("prim::unchecked_cast(t x) -> t", noop, aliasAnalysisSpecialCase()),
+     Operator(
+         "prim::unchecked_cast(t x) -> t",
+         noop,
+         aliasAnalysisSpecialCase()),
      Operator(
          "aten::wait(Future(t) self) -> t",
          [](Stack& stack) {