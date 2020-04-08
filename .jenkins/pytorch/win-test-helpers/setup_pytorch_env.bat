c2bc5c56c5457224ae5d1b65e9c36b0aa539798e
Nikita Shulga <nshulga@fb.com>
2020-04-07 21:06:55 -0700

    Use `repo.anaconda.com` instead of `repo.continuum.io` (#36201)
    Summary:
    Per https://github.com/conda/conda/issues/6886  `repo.anaconda.com` should have been used since Feb 2019
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36201
    
    Test Plan: CI
    
    Differential Revision: D20910667
    
    Pulled By: malfet
    
    fbshipit-source-id: 3a191e2cae293e6f96dbb323853e84c07cd7aabc

--- .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
+++ .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
@@ -13,7 +13,7 @@ if "%BUILD_ENVIRONMENT%"=="" (
 )
 if NOT "%BUILD_ENVIRONMENT%"=="" (
     IF EXIST %CONDA_PARENT_DIR%\Miniconda3 ( rd /s /q %CONDA_PARENT_DIR%\Miniconda3 )
-    curl --retry 3 https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
+    curl --retry 3 https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
     %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
 )
 call %CONDA_PARENT_DIR%\Miniconda3\Scripts\activate.bat %CONDA_PARENT_DIR%\Miniconda3
