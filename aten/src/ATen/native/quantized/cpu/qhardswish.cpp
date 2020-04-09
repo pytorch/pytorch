394960ce415a0b837f3bed4206bff95279be5028
vasiliy <vasiliy@fb.com>
2020-04-09 10:26:35 -0700

    qnnpack hardswish - pytorch op integration
    Summary:
    
    Hooks up the aten quantized hardswish op to the QNNPACK
    path added in the previous PR.
    
    Test Plan:
    
    tests pass
    
    will run benchmarking on mobile to confirm
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cpu/qhardswish.cpp
+++ aten/src/ATen/native/quantized/cpu/qhardswish.cpp
@@ -3,6 +3,9 @@
 #include <ATen/core/op_registration/op_registration.h>
 #include <ATen/quantized/Quantizer.h>
 #include <ATen/native/quantized/cpu/quantized_ops.h>
+#include <ATen/native/quantized/cpu/init_qnnpack.h>
+#include <ATen/native/quantized/cpu/qnnpack_utils.h>
+#include <caffe2/utils/threadpool/ThreadPoolMobile.h>
 
 #include <algorithm>
 
@@ -11,7 +14,70 @@ namespace native {
 
 DEFINE_DISPATCH(qhardswish_stub);
 
+namespace {
+
+#ifdef USE_PYTORCH_QNNPACK
+Tensor qnnpack_hardswish(Tensor input) {
+  TORCH_CHECK(input.ndimension() > 0, "qnnpack_hardswish(): Got empty input tensor");
+  initQNNPACK();
+
+  Tensor input_contig = input.contiguous(input.suggest_memory_format());
+  size_t num_elems = input_contig.numel() / input_contig.size(0);
+  const auto i_zero_point = input_contig.q_zero_point();
+  const auto i_scale = input_contig.q_scale();
+  const auto o_zero_point = i_zero_point;
+  const auto o_scale = i_scale;
+
+  pytorch_qnnp_operator_t hardswish_op{nullptr};
+  const pytorch_qnnp_status createStatus = pytorch_qnnp_create_hardswish_nc_q8(
+    num_elems, // channels
+    i_zero_point,
+    i_scale,
+    o_zero_point,
+    o_scale,
+    std::numeric_limits<uint8_t>::min(), // output min
+    std::numeric_limits<uint8_t>::max(), // output max
+    0, // flags
+    &hardswish_op);
+  TORCH_INTERNAL_ASSERT(createStatus == pytorch_qnnp_status_success,
+                        "failed to create QNNPACK Hardswish operator");
+  Tensor qy = at::_empty_affine_quantized(
+    input_contig.sizes(),
+    input_contig.options(),
+    o_scale,
+    o_zero_point);
+
+  const pytorch_qnnp_status setupStatus = pytorch_qnnp_setup_hardswish_nc_q8(
+    hardswish_op,
+    input_contig.size(0), // batch size
+    (uint8_t*)input_contig.data_ptr<c10::quint8>(), // input data
+    num_elems, // input stride
+    (uint8_t*)qy.data_ptr<c10::quint8>(), // output data
+    num_elems); // output stride
+  TORCH_INTERNAL_ASSERT(setupStatus == pytorch_qnnp_status_success,
+                        "failed to setup QNNPACK Hardswish operator");
+
+  pthreadpool_t threadpool = caffe2::mobile_pthreadpool();
+
+  const pytorch_qnnp_status runStatus =
+    pytorch_qnnp_run_operator(hardswish_op, threadpool);
+
+  TORCH_INTERNAL_ASSERT(
+    runStatus == pytorch_qnnp_status_success,
+    "failed to run QNNPACK Hardswish operator");
+  return qy;
+}
+#endif // USE_PYTORCH_QNNPACK
+
+} // namespace
+
 Tensor quantized_hardswish(const Tensor& qx) {
+#ifdef USE_PYTORCH_QNNPACK
+  if (at::globalContext().qEngine() == at::QEngine::QNNPACK &&
+      qx.scalar_type() == kQUInt8) {
+    return qnnpack_hardswish(qx);
+  }
+#endif  // USE_PYTORCH_QNNPACK
   Tensor qy = at::_empty_affine_quantized(qx.sizes(), qx.options(),
       qx.q_scale(), qx.q_zero_point());
   qhardswish_stub(qx.device().type(), qx, qy);
@@ -19,6 +85,14 @@ Tensor quantized_hardswish(const Tensor& qx) {
 }
 
 Tensor& quantized_hardswish_(Tensor& qx) {
+#ifdef USE_PYTORCH_QNNPACK
+  if (at::globalContext().qEngine() == at::QEngine::QNNPACK &&
+      qx.scalar_type() == kQUInt8) {
+    Tensor qy = qnnpack_hardswish(qx);
+    qx.copy_(qy);
+    return qx;
+  }
+#endif  // USE_PYTORCH_QNNPACK
   qhardswish_stub(qx.device().type(), qx, qx);
   return qx;
 }
