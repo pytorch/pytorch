42457e634dbae9833cc12fac03d6b45c9dfefde2
Nick Gibson <nickg@fb.com>
2020-04-10 11:54:32 -0700

    [TensorExpr] add support for Reduction Ops (#35866)
    Summary:
    Second attempt at the reduction frontend for the TensorExpr compiler. Has two APIs, a simple version for common reduction types and a customizable Reducer fronted which allows specifying initializer, reduction interaction via lambda and body via lambda.
    
    Simple API looks like so:
    ```
    Buffer b(BufHandle("b", {10}), kInt);
    Tensor* c = Reduce("sum", {}, Sum(b), {{10, "m"}});
    ```
    
    An example of specializing a Sum to do Matmul:
    ```
    Buffer tA(BufHandle("tA", {M, K}), kFloat);
    Buffer tB(BufHandle("tB", {K, N}), kFloat);
    Sum matmul([&](ParameterList& v) {
      ExprHandle m = v[0];
      ExprHandle n = v[1];
      ExprHandle k = v[2];
      return tA(m, k) * tB(k, n);
    });
    Tensor* mm = Reduce("mm", {{M, "m"}, {N, "n"}}, matmul, {{K, "k"}});
    ```
    
    A fully specialized Reduction:
    ```
    VarHandle searchValue("searchValue", kInt);
    Buffer b(BufHandle("b", {4, 10}), kInt);
    
    Reducer anyEqSV(
        ExprHandle(0),
        [](ExprHandle a, ExprHandle b) {
          return CompareSelect::make(a, 1, 1, b, kEQ);
        },
        [&](ParameterList& v) {
          return CompareSelect::make(b.call(v), searchValue, kEQ);
        });
    
    Tensor* any = Reduce("anyEqual", {{4, "i"}}, anyEqSV, {{10, "j"}});
    ```
    
     ---
    
    Until lowering, Reductions are held in a compound form for easier optimization:
    ```
      VarHandle m("m", kInt);
      Buffer b(BufHandle("b", {2, 3, m}), kFloat);
    
      Tensor* c = Reduce("sum", {{2, "l"}, {3, "n"}}, Sum(b), {{m, "m"}});
      LoopNest loop({c});
      std::cout << *loop.root_stmt() << "\n";
    ```
    ```
    for (int l = 0; l < 2; l++) {
      for (int n = 0; n < 3; n++) {
        for (int m = 0; m < m_1; m++) {
          sum[l, n] = ReduceOp(sum[l, n] = float(0);, (sum[l, n]) + (b[l, n, m]), {m});
        }
      }
    }
    ```
    ```
      loop.prepareForCodegen();
      std::cout << *loop.root_stmt() << "\n";
    ```
    ```
    for (int l = 0; l < 2; l++) {
      for (int n = 0; n < 3; n++) {
        sum[(0 + l * (1 * 3)) + n * 1] = float(0);
        for (int m = 0; m < m_1; m++) {
          sum[(0 + l * (1 * 3)) + n * 1] = (sum[(0 + l * (1 * 3)) + n * 1]) + (b[((0 + l * ((1 * m_1) * 3)) + n * (1 * m_1)) + m * 1]);
        }
      }
    }
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35866
    
    Differential Revision: D20965577
    
    Pulled By: nickgg
    
    fbshipit-source-id: afe506c90db794447180056417013bcaf0e2c049

--- torch/csrc/jit/tensorexpr/reduction.h
+++ torch/csrc/jit/tensorexpr/reduction.h
@@ -1,92 +1,231 @@
 #pragma once
 
-#include <functional>
-#include <vector>
-
+#include <torch/csrc/jit/tensorexpr/buffer.h>
+#include <torch/csrc/jit/tensorexpr/dim_arg.h>
 #include <torch/csrc/jit/tensorexpr/expr.h>
 #include <torch/csrc/jit/tensorexpr/ir.h>
+#include <torch/csrc/jit/tensorexpr/types.h>
+
+#include <functional>
+#include <vector>
 
 namespace torch {
 namespace jit {
 namespace tensorexpr {
 
-class Function : public KernelScopedObject {
+using ParameterList = const std::vector<VarHandle>;
+using ReduceInteraction = std::function<ExprHandle(ExprHandle, ExprHandle)>;
+
+// An expression representing a Reduction operation (e.g. Sum, Max) broken into
+// it's component parts: initialization, accumulation var, acquisition of value
+// to be reduced and interaction.
+//
+// This is intended to be expanded in the loopnest and not make it to codegen.
+class ReduceOp : public ExprNode<ReduceOp> {
  public:
-  Function(
-      const std::string& func_name,
-      const std::vector<const Expr*>& dims,
-      const std::vector<const Var*>& args,
-      const Expr* body)
-      : func_vars_({VarHandle(func_name, kHandle).node()}),
-        dims_(dims),
-        args_(args),
-        bodies_({body}) {}
-  Function(
-      const std::vector<std::string>& func_names,
-      const std::vector<const Expr*>& dims,
-      const std::vector<const Var*>& args,
-      const std::vector<const Expr*>& bodies)
-      : func_vars_(func_names.size()),
-        dims_(dims),
-        args_(args),
-        bodies_(bodies) {
-    for (size_t i = 0; i < func_names.size(); i++) {
-      func_vars_[i] = new Var(func_names[i], kHandle);
-    }
+  ReduceOp(
+      ExprHandle accum,
+      Stmt* init,
+      ExprHandle body,
+      ReduceInteraction c,
+      const std::vector<const Var*>& reduce_args)
+      : ExprNodeBase(body.dtype()),
+        accumulator_(accum),
+        initializer_(init),
+        body_(body),
+        interaction_(c),
+        reduce_args_(reduce_args) {}
+
+  // return the accumulation load expression.
+  ExprHandle accumulator() const {
+    return accumulator_;
   }
 
-  int ndim() const {
-    return dims_.size();
-  }
-  const Expr* dim(int index) const {
-    if (index < 0 || index >= ndim()) {
-      throw out_of_range_index();
-    }
-
-    return dims_[index];
-  }
-  const std::vector<const Expr*>& dims() const {
-    return dims_;
-  }
-  const Var* arg(int index) const {
-    if (index < 0 || index >= ndim()) {
-      throw out_of_range_index();
-    }
-
-    return args_[index];
-  }
-  const std::vector<const Var*>& args() const {
-    return args_;
+  // return a Statement which stores the initializer into the accumulation
+  // buffer.
+  Stmt* initializer() const {
+    return initializer_;
   }
 
-  std::vector<const Expr*> bodies() const {
-    return bodies_;
-  }
-  const Expr* body(size_t index) const {
-    if (index >= bodies_.size()) {
-      throw out_of_range_index();
-    }
-
-    return bodies_[index];
+  // return the body expression which obtains the value to be reduced.
+  ExprHandle body() const {
+    return body_;
   }
 
-  std::vector<const Var*> func_vars() const {
-    return func_vars_;
-  }
-  const Var* func_var(size_t index) const {
-    if (index >= func_vars_.size()) {
-      throw out_of_range_index();
-    }
-    return func_vars_[index];
+  // returns a function encoding the interaction between accumulator and the
+  // reduction value.
+  ReduceInteraction interaction() const {
+    return interaction_;
   }
 
-  Stmt* ElementStmt(size_t index);
+  // returns variables associated with the axes of reduction.
+  const std::vector<const Var*>& reduce_args() const {
+    return reduce_args_;
+  }
+
+  // Completes the reduction operator by applying the interaction function to
+  // the accumulation and the body expression.
+  ExprHandle complete() const {
+    return interaction_(accumulator_, body_);
+  }
 
  private:
-  std::vector<const Var*> func_vars_;
-  std::vector<const Expr*> dims_;
-  std::vector<const Var*> args_;
-  std::vector<const Expr*> bodies_;
+  ExprHandle accumulator_;
+  Stmt* initializer_;
+  ExprHandle body_;
+  ReduceInteraction interaction_;
+  std::vector<const Var*> reduce_args_;
+};
+
+// A Reducer is a user interface describing a particular reduction operation. It
+// has three components: An initializtion value, a way of interacting each value
+// with the accumulation, and a method for obtaining the current value to be
+// reduced. It is materialized into a ReduceOp when loop variables are known.
+class Reducer {
+ public:
+  Reducer(ExprHandle init, ReduceInteraction& interaction)
+      : init_(init.node()), interaction_(interaction) {}
+
+  Reducer(ExprHandle init, ReduceInteraction& interaction, Buffer& buf)
+      : init_(init.node()), interaction_(interaction) {}
+
+  template <typename RI>
+  Reducer(ExprHandle init, RI interaction) : init_(init.node()) {
+    interaction_ = interaction;
+  }
+
+  ReduceOp* operator()(
+      Buf* result_buf,
+      ExprHandle body,
+      std::vector<const Var*> outer,
+      std::vector<const Var*> inner) const {
+    std::vector<const Expr*> indices;
+    for (size_t i = 0; i < outer.size(); i++) {
+      indices.push_back(outer[i]);
+    }
+
+    ExprHandle accum =
+        ExprHandle(new Load(body.dtype(), result_buf, indices, new IntImm(1)));
+    Stmt* init = new Store(
+        result_buf, indices, new Cast(body.dtype(), init_), new IntImm(1));
+
+    return new ReduceOp(accum, init, body, interaction_, inner);
+  }
+
+  // Polymorphic handling of Body functions with a variety of parameters.
+  static ExprHandle getReduceBody(
+      const std::function<ExprHandle(ParameterList&)>& func,
+      const std::vector<VarHandle>& vars) {
+    return func(vars);
+  }
+
+  static ExprHandle getReduceBody(
+      const std::function<ExprHandle(const VarHandle&)>& func,
+      const std::vector<VarHandle>& vars) {
+    if (vars.size() != 1) {
+      throw malformed_input("mismatch between reduce body and arg size (1)");
+    }
+
+    return func(vars[0]);
+  }
+
+  static ExprHandle getReduceBody(
+      const std::function<ExprHandle(const VarHandle&, const VarHandle&)>& func,
+      const std::vector<VarHandle>& vars) {
+    if (vars.size() != 2) {
+      throw malformed_input("mismatch between reduce body and arg size (2)");
+    }
+    return func(vars[0], vars[1]);
+  }
+
+  static ExprHandle getReduceBody(
+      const std::function<
+          ExprHandle(const VarHandle&, const VarHandle&, const VarHandle&)>&
+          func,
+      const std::vector<VarHandle>& vars) {
+    if (vars.size() != 3) {
+      throw malformed_input("mismatch between reduce body and arg size (3)");
+    }
+    return func(vars[0], vars[1], vars[2]);
+  }
+
+  static ExprHandle getReduceBody(
+      const std::function<ExprHandle(
+          const VarHandle&,
+          const VarHandle&,
+          const VarHandle&,
+          const VarHandle&)>& func,
+      const std::vector<VarHandle>& vars) {
+    if (vars.size() != 4) {
+      throw malformed_input("mismatch between reduce body and arg size (4)");
+    }
+    return func(vars[0], vars[1], vars[2], vars[3]);
+  }
+
+ private:
+  const Expr* init_;
+  ReduceInteraction interaction_;
+};
+
+class Sum : public Reducer {
+ public:
+  Sum()
+      : Reducer(ExprHandle(0), [](ExprHandle a, ExprHandle b) {
+          return a + b;
+        }) {}
+};
+
+namespace {
+ExprHandle maximumVal(ScalarType type) {
+  switch (type) {
+#define MAX_BY_TYPE_CASE(Type, Name) \
+  case ScalarType::Name:             \
+    return ExprHandle(std::numeric_limits<Type>::max());
+    AT_FORALL_SCALAR_TYPES_AND2(Bool, Half, MAX_BY_TYPE_CASE)
+#undef MAX_BY_TYPE_CASE
+    default:
+      throw unsupported_dtype();
+  }
+  return ExprHandle();
+}
+
+static ExprHandle minimumVal(ScalarType type) {
+  switch (type) {
+#define MAX_BY_TYPE_CASE(Type, Name) \
+  case ScalarType::Name:             \
+    return ExprHandle(std::numeric_limits<Type>::min());
+    AT_FORALL_SCALAR_TYPES_AND2(Bool, Half, MAX_BY_TYPE_CASE)
+#undef MAX_BY_TYPE_CASE
+    default:
+      throw unsupported_dtype();
+  }
+}
+} // namespace
+
+class Maximum : public Reducer {
+ public:
+  // TODO possible to remove this arg by deferring the init value until we know
+  // the dtype of the body.
+  Maximum(Dtype dtype)
+      : Reducer(
+            minimumVal(dtype.scalar_type()),
+            [](ExprHandle a, ExprHandle b) { return Max::make(a, b, true); }) {}
+  Maximum(ExprHandle initializer)
+      : Reducer(initializer, [](ExprHandle a, ExprHandle b) {
+          return Max::make(a, b, true);
+        }) {}
+};
+
+class Minimum : public Reducer {
+ public:
+  Minimum(Dtype dtype)
+      : Reducer(
+            maximumVal(dtype.scalar_type()),
+            [](ExprHandle a, ExprHandle b) { return Min::make(a, b, true); }) {}
+  Minimum(ExprHandle initializer)
+      : Reducer(initializer, [](ExprHandle a, ExprHandle b) {
+          return Min::make(a, b, true);
+        }) {}
 };
 
 } // namespace tensorexpr
