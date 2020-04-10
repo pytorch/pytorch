ca0dc1fcdcb4a696218a131d27b70d2730d7073e
Soumith Chintala <soumith@gmail.com>
2020-04-10 11:06:47 -0700

    skip test in 3.8 because of inconsistent regex

--- test/test_jit.py
+++ test/test_jit.py
@@ -4915,7 +4915,7 @@ def foo(x):
 
         self.checkModule(Add(), [torch.randn(2, 2)])
 
-    @unittest.skipIf(IS_WINDOWS and sys.version_info >= (3, 8), 'TODO: need to fix the test case')
+    @unittest.skipIf(sys.version_info >= (3, 8), 'TODO: need to fix the test case')
     def test_unmatched_type_annotation(self):
         message1 = re.escape("Number of type annotations (2) did not match the number of function parameters (1):")
         message2 = re.escape("""
