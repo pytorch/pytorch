25ae08bc86e19fe4b742eabbbaafbec6ebf55514
Shihao Xu <shihaoxu@fb.com>
2020-04-11 02:48:49 -0700

    [DistAutograd x JIT] Capture global state, dist autograd current context id, before thread switching triggered by JIT future.wait() (#36395)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36395
    
    titled
    
    Test Plan:
    # Unit tests
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_fork -- test_restore_context_after_swtich_to_jit_thread
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc/jit:dist_autograd_fork
    
    buck-out/gen/caffe2/test/distributed/rpc/jit/dist_autograd_fork\#binary.par -r test_restore_context_after_swtich_to_jit_thread
    ```
    
    ```
    buck test mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_fork -- test_backward_simple_script_call
    
    buck build mode/dev-nosan //caffe2/test/distributed/rpc:dist_autograd_fork
    
    buck-out/gen/caffe2/test/distributed/rpc/dist_autograd_fork\#binary.par -r test_backward_simple_script_call
    ```
    
    Differential Revision: D7857991
    
    fbshipit-source-id: 168e0e3846a50ea92d4f9450a30ccc6c13e2fcec

--- torch/testing/_internal/distributed/rpc/jit/dist_autograd_test.py
+++ torch/testing/_internal/distributed/rpc/jit/dist_autograd_test.py
@@ -1,32 +1,38 @@
 import unittest
+from typing import Tuple
 
 import torch
 import torch.distributed.autograd as dist_autograd
+import torch.distributed.rpc as rpc
+from torch import Tensor
+from torch.distributed.rpc import rpc_async
 from torch.testing import FileCheck
 from torch.testing._internal.dist_utils import dist_init, worker_name
 from torch.testing._internal.distributed.rpc.rpc_agent_test_fixture import (
     RpcAgentTestFixture,
 )
-from torch.distributed.rpc import rpc_async
+
 
 @torch.jit.script
 def local_add(t1, t2):
     return torch.add(t1, t2)
 
+
 @torch.jit.script
 def remote_add(t1, t2, dst: str):  # noqa: E999
     return rpc_async(dst, local_add, (t1, t2)).wait()
+
 
 @torch.jit.script
 def fork_add(t1, t2, dst: str):
     fut = torch.jit._fork(remote_add, t1, t2, dst)
     return torch.jit._wait(fut)
 
+
 @unittest.skipIf(
     not torch._six.PY3, "Pytorch distributed autograd package does not support python2"
 )
 class JitDistAutogradTest(RpcAgentTestFixture):
-
     @dist_init
     def test_get_gradients(self):
         dst_rank = self.rank
@@ -65,3 +71,32 @@ class JitDistAutogradTest(RpcAgentTestFixture):
             self.assertEqual(2, len(grads))
             self.assertIn(t1, grads)
             self.assertIn(t2, grads)
+
+    @dist_init
+    def test_restore_context_after_swtich_to_jit_thread(self):
+        if self.rank != 0:
+            return
+
+        @torch.jit.script
+        def forward_script(
+            context_id: int, dst_worker_name: str, t1: Tensor, t2: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+            res1_fut = rpc.rpc_async(dst_worker_name, local_add, (t1, t1))
+            res1 = res1_fut.wait()  # After this, the script runs in a new JIT thread.
+            loss1 = res1.sum()
+
+            # SendRpcBackward is not attched, since DistAutogradContext is lost here.
+            res2_fut = rpc.rpc_async(dst_worker_name, local_add, (t2, t2))
+            res2 = res2_fut.wait()
+            loss2 = res2.sum()
+
+            return loss1, loss2
+
+        with dist_autograd.context() as context_id:
+            t1 = torch.ones((2, 3), requires_grad=True)
+            t2 = torch.ones((2, 3), requires_grad=True)
+            dst_worker_name = worker_name((self.rank + 1) % self.world_size)
+            loss0, loss1 = forward_script(context_id, dst_worker_name, t1, t2)
+            dist_autograd.backward(context_id, [loss0, loss1])
+            grad0, grad1 = dist_autograd.get_gradients(context_id)
+            self.assertEqual(grad0, grad1)
