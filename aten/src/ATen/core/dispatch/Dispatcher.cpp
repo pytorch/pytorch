f691b2169e4c5729aa6c305c5055523d71db83a3
Edward Z. Yang <ezyang@fb.com>
2020-04-14 18:55:50 -0400

    Add locks to fallback register/deregister.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- aten/src/ATen/core/dispatch/Dispatcher.cpp
+++ aten/src/ATen/core/dispatch/Dispatcher.cpp
@@ -218,6 +218,8 @@ void Dispatcher::cleanup(const OperatorHandle& op, const OperatorName& op_name) 
 }
 
 RegistrationHandleRAII Dispatcher::registerFallback(DispatchKey dispatchKey, KernelFunction kernel) {
+  std::lock_guard<std::mutex> lock(mutex_);
+
   // TODO: fallbacks clobber each other completely unsafely, unlike regular
   // kernels
   backendFallbackKernels_.setKernel(dispatchKey, std::move(kernel));
@@ -231,6 +233,8 @@ RegistrationHandleRAII Dispatcher::registerFallback(DispatchKey dispatchKey, Ker
 }
 
 void Dispatcher::deregisterFallback_(DispatchKey dispatchKey) {
+  std::lock_guard<std::mutex> lock(mutex_);
+
   backendFallbackKernels_.removeKernelIfExists(dispatchKey);
   backendsWithoutFallthrough_ = backendsWithoutFallthrough_.add(dispatchKey);
 }
