7d4bc1a098dff6e0422a8487fa537847925347ae
Nick Korovaiko <villedepommes@fb.com>
2020-04-14 10:34:51 -0700

    run 100 times with diag

--- torch/csrc/jit/passes/liveness.cpp
+++ torch/csrc/jit/passes/liveness.cpp
@@ -3,6 +3,7 @@
 #include <torch/csrc/jit/ir/ir_views.h>
 #include <torch/csrc/jit/passes/constant_pooling.h>
 #include <memory>
+#include <torch/csrc/jit/jit_log.h>
 
 namespace torch {
 namespace jit {
@@ -14,10 +15,14 @@ struct LivenessAnalyzer {
       : graph_(std::move(graph)), changed_(false) {}
 
   std::unordered_map<Node*, std::vector<Value*>> run() {
+
+    GRAPH_DUMP("Liveness Graph: ", graph_);
     // we implement the canonical fixed-point liveness
     // the analysis is run until there are no more changes
     // to liveness sets for each node
+    size_t i = 0;
     do {
+      GRAPH_DEBUG("Running iteration ", i++);
       changed_ = false;
       processBlock(graph_->block(), SparseBitVector{});
     } while (changed_);
@@ -82,44 +87,66 @@ struct LivenessAnalyzer {
   SparseBitVector processBlock(Block* b, SparseBitVector liveness) {
     // block outputs are the uses
     auto block_outputs = toSparseBitVector(b->outputs());
+    GRAPH_DEBUG("@#$processBlock Liveness", liveness);
+    GRAPH_DEBUG("block_outputs : ", block_outputs);
     liveness |= block_outputs;
+    GRAPH_DEBUG("Processing block ", b);
+    GRAPH_DEBUG("@#$Liveness with block outputs: ", liveness);
 
     SparseBitVector defs;
     for (Node* it : b->nodes().reverse()) {
       // kill outputs
+      GRAPH_DEBUG("Processing node ", getHeader(it));
       liveness -= toSparseBitVector(it->outputs());
+      GRAPH_DEBUG("@#$After removing outputs: ", liveness);
       if (it->kind() == prim::Loop) {
         LoopView lv(it);
         // N.B. merge in changes from the loop header
         auto loop_header = *lv.bodyBlock()->nodes().begin();
+        GRAPH_DEBUG("@#$Liveness from loop header: ", liveness_sets_[loop_header]);
         auto loop_block = liveness | liveness_sets_[loop_header];
+        GRAPH_DEBUG("@#$loop_block liveness before loop: ", loop_block);
         // loop's outputs aren't live inside the loop
         // loop's block outputs, OTOH, will be considered
         // as uses
         WithInsertPoint guard(*lv.bodyBlock()->nodes().end());
         // temporary make loop counts live for the duration of the loop
         // as they are needed by BailOuts in the loop
+        
         auto ctc = graph_->create(prim::Store, {lv.currentTripCount()}, 0);
         graph_->insertNode(ctc);
+        GRAPH_DEBUG("@#$Creating a store for ctc : ", ctc);
         auto mtc = graph_->create(prim::Store, {lv.maxTripCount()}, 0);
         graph_->insertNode(mtc);
+        GRAPH_DEBUG("@#$Creating a store for mtc : ", mtc);
         loop_block = processBlock(lv.bodyBlock(), loop_block);
+        GRAPH_DEBUG("@#$loop_block liveness after loop: ", loop_block);
         ctc->destroy();
         mtc->destroy();
         // loop block's inputs die outside loop's block
         loop_block -= toSparseBitVector(lv.bodyBlock()->inputs());
+        GRAPH_DEBUG("@#$loop_block liveness after removing block inputs: ", loop_block);
         liveness |= loop_block;
+        GRAPH_DEBUG("@#$liveness |= loop_block : ", liveness);
       } else if (it->kind() == prim::If) {
         IfView iv(it);
         auto true_liveness = processBlock(iv.thenBlock(), liveness);
+        GRAPH_DEBUG("@#$true_liveness: ", true_liveness);
         auto false_liveness = processBlock(iv.elseBlock(), liveness);
+        GRAPH_DEBUG("@#$false_liveness: ", false_liveness);
         liveness |= true_liveness;
         liveness |= false_liveness;
+        GRAPH_DEBUG("@#$liveness | true_liveness | false_liveness: ", liveness);
       }
       liveness |= toSparseBitVector(it->inputs());
+      GRAPH_DEBUG("@#$Liveness after adding node inputs: ", liveness);
       // `|=` returns true if new bits were set in LHS
       // after or/union with `liveness`
+
+      GRAPH_DEBUG("@#$liveness_sets_[it] : ", liveness_sets_[it]);
       auto changed = liveness_sets_[it] |= liveness;
+      GRAPH_DEBUG("changed = ", changed);
+      GRAPH_DEBUG("liveness_sets_[it] = ", liveness_sets_[it]);
       changed_ = changed_ | changed;
     }
     return liveness;
