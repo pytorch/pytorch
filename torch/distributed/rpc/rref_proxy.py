ea29ed122bc1feb658b600fa91fe55f0c94b3f2a
Shen Li <shenli@devfair017.maas>
2020-04-14 15:02:23 -0700

    Update on "Add RRef Python Helper to launch function on the referenced object"
    With this PR, applications no longer need to create dedicated helpers
    to run functions on the object referenced by an RRef. Instead,
    `rref.rpc_sync().some_func()` will use `rpc_sync` to run `some_func`
    on the owner of the RRef using the object referenced by the RRef.
    Similar helpers for `rref.rpc_async().some_func()` and
    `rref.remote().some_func()` are also added.
    
    [ghstack-poisoned]

--- torch/distributed/rpc/rref_proxy.py
+++ torch/distributed/rpc/rref_proxy.py
@@ -1,14 +1,11 @@
-import enum
 from functools import partial
-
-from .api import rpc_sync, rpc_async, remote, get_worker_info
 
 
 def _local_invoke(rref, func_name, args, kwargs):
     return getattr(rref.local_value(), func_name)(*args, **kwargs)
 
 
-def _invoke_rpc_sync(rref, rpc_api, func_name, *args, **kwargs):
+def _invoke_rpc(rref, rpc_api, func_name, *args, **kwargs):
     return rpc_api(
         rref.owner(),
         _local_invoke,
@@ -22,6 +19,4 @@ class RRefProxy:
         self.rpc_api = rpc_api
 
     def __getattr__(self, func_name):
-        return partial(_invoke_rpc_sync, self.rref, self.rpc_api, func_name)
-
-
+        return partial(_invoke_rpc, self.rref, self.rpc_api, func_name)
