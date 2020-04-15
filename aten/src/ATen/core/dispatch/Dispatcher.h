84f4061a6723a4999e5824e026a7b9968b6bf7d4
Sebastian Messmer <messmer@fb.com>
2020-04-14 22:13:59 -0700

    Back out "Revert D20147487: Refactor jit::Operator to more clearly distinguish the two possible states" (#36634)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36634
    
    Original commit changeset: e69432dc3d03
    ghstack-source-id: 102163937
    
    Test Plan: waitforsandcastle
    
    Differential Revision: D21023952
    
    fbshipit-source-id: d1bad395cb0b4eda91a5d815291ac9b7bdb04573

--- aten/src/ATen/core/dispatch/Dispatcher.h
+++ aten/src/ATen/core/dispatch/Dispatcher.h
@@ -143,14 +143,6 @@ public:
   RegistrationHandleRAII registerDef(FunctionSchema schema);
 
   /**
-   * Returns true, iff the given operator handle is still valid,
-   * i.e. the operator was not deregistered.
-   * Note that this function is somewhat expensive to call,
-   * so don't do it in a hotpath.
-   */
-  bool isValid(const OperatorHandle& op) const;
-
-  /**
    * Register a kernel to the dispatch table for an operator.
    * If dispatch_key is nullopt, then this registers a fallback kernel.
    *
@@ -240,16 +232,6 @@ public:
 
   bool hasSchema() const {
     return operatorIterator_->op.hasSchema();
-  }
-
-  /**
-   * Returns true iff the operator handle is still valid,
-   * i.e. the operator was not deregistered.
-   * Note that this function is somewhat expensive to call,
-   * so don't do it in a hotpath.
-   */
-  bool isValid() const {
-    return c10::Dispatcher::singleton().isValid(*this);
   }
 
   const FunctionSchema& schema() const {
