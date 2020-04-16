c9840e3d07ec547c48351da96727ba12662f912b
David Riazati <davidriazati@fb.com>
2020-04-15 18:13:24 -0700

    update

--- caffe2/serialize/crc.cc
+++
@@ -1,9 +0,0 @@
-#include "miniz.h"
-#include <iostream>
-
-extern "C" {
-mz_ulong mz_crc32(mz_ulong crc, const mz_uint8* ptr, size_t buf_len) {
-  // std::cout << "Custom crc!\n";
-  return 0;
-}
-}
