f90ae08d421b2134c64533171aa6febf95c8b87a
Edward Yang <ezyang@fb.com>
2020-04-16 16:15:09 -0700

    Update reference to RegisterOperators in error message in Convolution (#36389)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36389
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Differential Revision: D20964193
    
    Pulled By: ezyang
    
    fbshipit-source-id: 27aeea01ccf5dfcebb8f043cde009a14dde3958e

--- aten/src/ATen/native/quantized/cpu/qlinear_prepack.cpp
+++ aten/src/ATen/native/quantized/cpu/qlinear_prepack.cpp
@@ -299,19 +299,19 @@ static auto registry =
     c10::RegisterOperators()
         .op("quantized::linear_prepack(Tensor W, Tensor? B=None) -> Tensor W_prepack",
             c10::RegisterOperators::options()
-            .aliasAnalysis(at::AliasAnalysisKind::PURE_FUNCTION)
+            .aliasAnalysis(at::AliasAnalysisKind::FROM_SCHEMA)
             .kernel<QLinearPackWeightInt8>(DispatchKey::QuantizedCPU))
         .op("quantized::linear_prepack_fp16(Tensor W, Tensor? B=None) -> Tensor W_prepack",
             c10::RegisterOperators::options()
-            .aliasAnalysis(at::AliasAnalysisKind::PURE_FUNCTION)
+            .aliasAnalysis(at::AliasAnalysisKind::FROM_SCHEMA)
             .kernel<QLinearPackWeightFp16>(DispatchKey::CPU))
         .op("_quantized::linear_prepack(Tensor W, Tensor? B=None) -> Tensor W_prepack",
             c10::RegisterOperators::options()
-            .aliasAnalysis(at::AliasAnalysisKind::PURE_FUNCTION)
+            .aliasAnalysis(at::AliasAnalysisKind::FROM_SCHEMA)
             .kernel<QLinearPackWeightInt8>(DispatchKey::QuantizedCPU))
         .op("_quantized::linear_prepack_fp16(Tensor W, Tensor? B=None) -> Tensor W_prepack",
             c10::RegisterOperators::options()
-            .aliasAnalysis(at::AliasAnalysisKind::PURE_FUNCTION)
+            .aliasAnalysis(at::AliasAnalysisKind::FROM_SCHEMA)
             .kernel<QLinearPackWeightFp16>(DispatchKey::CPU));
 
 } // namespace
