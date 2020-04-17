4dc357f03ad99d9569d8dd9a6cd5c78eb7e9359c
Nikolay Korovaiko <korovaikon@gmail.com>
2020-04-17 12:28:11 -0700

    make simple executor the default for OSS

--- torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
+++ torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
@@ -29,7 +29,7 @@ static std::atomic<bool> executor_mode{false};
 static std::atomic<bool> profiling_mode{false};
 #else
 static std::atomic<bool> executor_mode{true};
-static std::atomic<bool> profiling_mode{true};
+static std::atomic<bool> profiling_mode{false};
 #endif
 
 static std::atomic<size_t> num_profiled_runs{1};
