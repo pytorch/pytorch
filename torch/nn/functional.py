e90c9a7689dbcb66bc0cde00b3f83758a301b2fe
Kimish Patel <kimishpatel@fb.com>
2020-04-17 11:26:38 -0700

    Add channel shuffle op fp32 + quantized.
    Pytorch does not have native channel shuffle op.
    This diff adds that for both fp and quantized tensors.
    For FP implementation is inefficient one. For quantized there is a native
    QNNPACK op for this.
    
    Differential Revision: [D21093841](https://our.internmc.facebook.com/intern/diff/D21093841/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D21093841/)!
    
    [ghstack-poisoned]

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -2764,6 +2764,44 @@ Examples::
     torch.Size([1, 1, 12, 12])
 """)
 
+channel_shuffle = _add_docstr(torch.channel_shuffle, r"""
+Divide the channels in a tensor of shape :math:`(*, C , H, W)`
+into g groups and rearrange them as :math:`(*, C \frac g, g, H, W)`,
+while keeping the original tensor shape.
+
+See :class:`~torch.nn.ChannelShuffle` for details.
+
+Args:
+    input (Tensor): the input tensor
+    groups (int): number of groups to divide channels in and rearrange.
+
+Examples::
+
+    >>> channel_shuffle = nn.ChannelShuffle(3)
+    >>> input = torch.randn(1, 4, 2, 2)
+    >>> print(input)
+    [[[[1, 2],
+       [3, 4]],
+      [[5, 6],
+       [7, 8]],
+      [[9, 10],
+       [11, 12]],
+      [[13, 14],
+       [15, 16]],
+     ]]
+    >>> output = channel_shuffle(input)
+    >>> print(output)
+    [[[[1, 2],
+       [3, 4]],
+      [[9, 10],
+       [11, 12]],
+      [[5, 6],
+       [7, 8]],
+      [[13, 14],
+       [15, 16]],
+     ]]
+""")
+
 @_overload  # noqa: F811
 def upsample(input, size=None, scale_factor=None, mode='nearest', align_corners=None):  # noqa: F811
     # type: (Tensor, Optional[int], Optional[float], str, Optional[bool]) -> Tensor
