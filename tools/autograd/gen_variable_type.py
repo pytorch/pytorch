60c7d0cc33be1a40bd8d0a92d631e8964a28a8b4
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos076.prn3.facebook.com>
2020-04-17 04:00:58 -0700

    2020-04-17 postnightly release (32bbf12aa76b53c51bd17bd299ba73d9bfcb591c)

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -183,15 +183,11 @@ ${return_type} ${type_wrapper_name}(${type_method_formals}) {
 """)
 
 UNBOXEDONLY_WRAPPER_REGISTRATION = CodeTemplate("""\
-.impl_UNBOXED("${operator_name_with_overload}", torch::kAutograd,
-        VariableType::${type_wrapper_name}
-      )
+m.impl_UNBOXED("${unqual_operator_name_with_overload}", VariableType::${type_wrapper_name});
 """)
 
 WRAPPER_REGISTRATION = CodeTemplate("""\
-.impl("${operator_name_with_overload}", torch::kAutograd,
-        VariableType::${type_wrapper_name}
-     )
+m.impl("${unqual_operator_name_with_overload}", VariableType::${type_wrapper_name});
 """)
 
 UNPACK_TENSOR = CodeTemplate("""\
