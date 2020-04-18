daa4d93534cf3806046411d2bdfeaac373320504
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-04-18 11:14:13 -0700

    Generator -> c10::optional<Generator>

--- aten/src/ATen/native/cuda/Distributions.cu
+++ aten/src/ATen/native/cuda/Distributions.cu
@@ -192,7 +192,7 @@ Tensor _s_poisson_cuda(const Tensor& lambda, c10::optional<Generator> gen_) {
   return ret;
 }
 
-Tensor _s_binomial_cuda(const Tensor& count, const Tensor& prob, Generator gen_) {
+Tensor _s_binomial_cuda(const Tensor& count, const Tensor& prob, c10::optional<Generator> gen_) {
   auto gen = get_generator_or_default<CUDAGeneratorImpl>(gen_, cuda::detail::getDefaultCUDAGenerator());
   std::pair<uint64_t, uint64_t> rng_engine_inputs;
   {
