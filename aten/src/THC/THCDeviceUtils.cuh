99f9d661dc2b30eea6b7ba44c2935bcf749dddff
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-17 17:41:58 -0700

    Update on "debug ROCm"
    [ghstack-poisoned]

--- aten/src/THC/THCDeviceUtils.cuh
+++ aten/src/THC/THCDeviceUtils.cuh
@@ -36,7 +36,12 @@ __device__ __forceinline__ T doLdg(const T* p) {
 
 __device__ __forceinline__ unsigned int ACTIVE_MASK()
 {
+#if CUDA_VERSION >= 9000
     return __activemask();
+#else
+// will be ignored anyway
+    return 0xffffffff;
+#endif
 }
 
 #if defined(__HIP_PLATFORM_HCC__)
@@ -47,26 +52,42 @@ __device__ __forceinline__ unsigned long long int WARP_BALLOT(int predicate)
 #else
 __device__ __forceinline__ unsigned int WARP_BALLOT(int predicate, unsigned int mask = 0xffffffff)
 {
+#if CUDA_VERSION >= 9000
     return __ballot_sync(mask, predicate);
+#else
+    return __ballot(predicate);
+#endif
 }
 #endif
 
 template <typename T>
 __device__ __forceinline__ T WARP_SHFL_XOR(T value, int laneMask, int width = warpSize, unsigned int mask = 0xffffffff)
 {
+#if CUDA_VERSION >= 9000
     return __shfl_xor_sync(mask, value, laneMask, width);
+#else
+    return __shfl_xor(value, laneMask, width);
+#endif
 }
 
 template <typename T>
 __device__ __forceinline__ T WARP_SHFL(T value, int srcLane, int width = warpSize, unsigned int mask = 0xffffffff)
 {
+#if CUDA_VERSION >= 9000
     return __shfl_sync(mask, value, srcLane, width);
+#else
+    return __shfl(value, srcLane, width);
+#endif
 }
 
 template <typename T>
 __device__ __forceinline__ T WARP_SHFL_UP(T value, unsigned int delta, int width = warpSize, unsigned int mask = 0xffffffff)
 {
+#if CUDA_VERSION >= 9000
     return __shfl_up_sync(mask, value, delta, width);
+#else
+    return __shfl_up(value, delta, width);
+#endif
 }
 
 #ifdef __HIP_PLATFORM_HCC__
@@ -82,7 +103,11 @@ __device__ __forceinline__ int64_t WARP_SHFL_DOWN(int64_t value, unsigned int de
 template <typename T>
 __device__ __forceinline__ T WARP_SHFL_DOWN(T value, unsigned int delta, int width = warpSize, unsigned int mask = 0xffffffff)
 {
+#if CUDA_VERSION >= 9000
     return __shfl_down_sync(mask, value, delta, width);
+#else
+    return __shfl_down(value, delta, width);
+#endif
 }
 
 
