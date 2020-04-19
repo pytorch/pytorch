382290c9801f87b3a7eb4086a75294dbc29dde7a
peter <peterghost86@gmail.com>
2020-04-19 11:40:00 +0800

    Use lld-link over link.exe

--- .jenkins/pytorch/win-test-helpers/build_pytorch.bat
+++ .jenkins/pytorch/win-test-helpers/build_pytorch.bat
@@ -4,7 +4,7 @@ if "%DEBUG%" == "1" (
   set BUILD_TYPE=release
 )
 
-set PATH=C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;%PATH%
+set PATH=C:\Program Files\LLVM\bin;C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;%PATH%
 
 :: This inflates our log size slightly, but it is REALLY useful to be
 :: able to see what our cl.exe commands are (since you can actually
@@ -15,6 +15,7 @@ set CMAKE_VERBOSE_MAKEFILE=1
 
 set INSTALLER_DIR=%SCRIPT_HELPERS_DIR%\installation-helpers
 
+call %INSTALLER_DIR%\install_llvm.bat
 call %INSTALLER_DIR%\install_mkl.bat
 call %INSTALLER_DIR%\install_magma.bat
 call %INSTALLER_DIR%\install_sccache.bat
@@ -87,6 +88,7 @@ sccache --start-server
 sccache --zero-stats
 set CC=sccache-cl
 set CXX=sccache-cl
+set LD=lld-link
 
 set CMAKE_GENERATOR=Ninja
 
