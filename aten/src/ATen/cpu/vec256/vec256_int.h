a53395e3a41f37ab7bb25caea0843f6edc1b46b7
Mike Ruberry <38511765+mruberry@users.noreply.github.com>
2020-04-19 03:04:07 -0700

    Updates ubsan macro placement for function template

--- aten/src/ATen/cpu/vec256/vec256_int.h
+++ aten/src/ATen/cpu/vec256/vec256_int.h
@@ -2,6 +2,7 @@
 
 #include <ATen/cpu/vec256/intrinsics.h>
 #include <ATen/cpu/vec256/vec256_base.h>
+#include <c10/macros/Macros.h>
 
 namespace at {
 namespace vec256 {
@@ -594,9 +595,8 @@ Vec256<int64_t> inline emulate(const Vec256<int64_t>& a, const Vec256<int64_t>& 
 // This is also technically avx compatible, but then we'll need AVX
 // code for add as well.
 // Note: intentionally ignores undefined behavior like (-lowest * -1).
-__ubsan_ignore_undefined__
 template <>
-Vec256<int64_t> inline operator*(const Vec256<int64_t>& a, const Vec256<int64_t>& b) {
+__ubsan_ignore_undefined__ Vec256<int64_t> inline operator*(const Vec256<int64_t>& a, const Vec256<int64_t>& b) {
   return emulate(a, b, [](int64_t a_point, int64_t b_point){return a_point * b_point;});
 }
 
