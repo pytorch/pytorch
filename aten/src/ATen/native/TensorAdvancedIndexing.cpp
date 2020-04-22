19232898efedb089f5f129a642ae02b3993b7ab3
Mike Ruberry <mruberry@devfair044.maas>
2020-04-22 12:34:30 -0700

    Adds missing file

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -564,7 +564,7 @@ Tensor masked_scatter(const Tensor & self, const Tensor & mask, const Tensor & s
 static Tensor & masked_fill_impl_cpu(Tensor & self, const Tensor & mask, Scalar value) {
   NoNamesGuard guard;
   if (mask.dtype() == ScalarType::Byte) {
-    TORCH_WARN("masked_fill_ received a mask with dtype torch.uint8, this behavior is now deprecated," \
+    TORCH_WARN_ONCE("masked_fill_ received a mask with dtype torch.uint8, this behavior is now deprecated," \
             "please use a mask with dtype torch.bool instead.");
   }
 
