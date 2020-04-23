5ffb9267754675ad620231ff0a483264634309d9
Sharvil Nanavati <sharvil.nanavati@gmail.com>
2020-04-23 14:00:58 -0700

    Add unit test to ensure multiple pruning calls preserve hook order.

--- test/test_nn.py
+++ test/test_nn.py
@@ -2063,6 +2063,20 @@ class TestNN(NNTestCase):
         hook = next(iter(m._forward_pre_hooks.values()))
         self.assertEqual(hook._tensor_name, 'weight')
 
+    def test_muliple_pruning_calls_preserve_hooks(self):
+        # Initial setup: a module with 3 pre-forward hooks where the middle one is
+        # the hook for pruning.
+        m = nn.Conv3d(2, 2, 2)
+        m.register_forward_pre_hook(lambda x, y: y)
+        prune.l1_unstructured(m, name='weight', amount=0.1)
+        m.register_forward_pre_hook(lambda x, y: y)
+
+        original_hooks = list(m._forward_pre_hooks.keys())
+        prune.l1_unstructured(m, name='weight', amount=0.1)
+        new_hooks = list(m._forward_pre_hooks.keys())
+
+        self.assertEqual(original_hooks, new_hooks)
+
     def test_pruning_container(self):
         # create an empty container
         container = prune.PruningContainer()
