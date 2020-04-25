84a31fb4e7fb1b5dbe9e42f5e1e30be4a0440189
Mike Ruberry <mruberry@fb.com>
2020-04-25 14:20:33 -0700

    Revert D18927221: Boxing uses if_constexpr instead of SFINAE
    Test Plan: revert-hammer
    
    Differential Revision:
    D18927221
    
    Original commit changeset: 70d99025b45e
    
    fbshipit-source-id: a4b650bbb6d76dda6086d88eb554f3c3077b0f76

--- aten/src/ATen/core/boxing/impl/boxing.h
+++ aten/src/ATen/core/boxing/impl/boxing.h
@@ -36,28 +36,35 @@ using supports_boxing =
   >>;
 
 template<class Result, class... Args>
-decltype(auto) boxAndCallBoxedFunc(KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func, OperatorKernel* functor, const OperatorHandle& opHandle, Args... args) {
-  return guts::if_constexpr<!supports_boxing<Result, Args...>::value>([] () -> Result {
-    TORCH_INTERNAL_ASSERT(false, "Tried to call KernelFunction::callUnboxed() for a kernel that only has a boxed kernel and doesn't support calling from an unboxed API yet.");
-  }, /* else */ [&] (auto __) -> Result {
-    (void)__; // silence gcc warning about unused arg
-    // TODO Reuse stack vector instead of allocating?
-    torch::jit::Stack stack;
-    torch::jit::push(stack, std::forward<Args>(args)...);
+Result boxAndCallBoxedFunc(KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func, OperatorKernel* functor, const OperatorHandle& opHandle, Args... args, std::enable_if_t<!supports_boxing<Result, Args...>::value, int> = 0) {
+  TORCH_INTERNAL_ASSERT(false, "Tried to call KernelFunction::callUnboxed() for a kernel that only has a boxed kernel and doesn't support calling from an unboxed API yet.");
+}
 
-    (*boxed_kernel_func)(functor, opHandle, &stack);
+// SFINAE version for ops with returns
+template<class Result, class... Args>
+std::enable_if_t<supports_boxing<Result, Args...>::value && !std::is_same<void, Result>::value, Result>
+boxAndCallBoxedFunc(KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func, OperatorKernel* functor, const OperatorHandle& opHandle, Args... args) {
+  // TODO Reuse stack vector instead of allocating?
+  torch::jit::Stack stack;
+  torch::jit::push(stack, std::forward<Args>(args)...);
 
-    return guts::if_constexpr<!std::is_same<void, Result>::value>([&] (auto _) -> Result {
-      // Result == Result_ but the compiler only knows that once it's evaluating this lambda, so it can't error early for Result == void.
-      using Result_ = typename decltype(_)::template type_identity<typename decltype(__)::template type_identity<Result>>;
-      static_assert(std::is_same<Result, Result_>::value, "");
+  (*boxed_kernel_func)(functor, opHandle, &stack);
 
-      TORCH_INTERNAL_ASSERT(stack.size() == 1, "A boxed kernel should only push one return to the stack");
-      return std::move(stack[0]).to<Result_>();
-    }, /* else */ [&] {
-      TORCH_INTERNAL_ASSERT(stack.size() == 0, "A boxed kernel returned a value but when we called it with KernelFunction::callUnboxed, we expected it to return void.");
-    });
-  });
+  TORCH_INTERNAL_ASSERT(stack.size() == 1, "A boxed kernel should only push one return to the stack");
+  return std::move(stack[0]).to<Result>();
+}
+
+// SFINAE version for ops without returns
+template<class Result, class... Args>
+std::enable_if_t<supports_boxing<Result, Args...>::value && std::is_same<void, Result>::value, Result>
+boxAndCallBoxedFunc(KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func, OperatorKernel* functor, const OperatorHandle& opHandle, Args... args) {
+  // TODO Reuse stack vector instead of allocating?
+  torch::jit::Stack stack;
+  torch::jit::push(stack, std::forward<Args>(args)...);
+
+  (*boxed_kernel_func)(functor, opHandle, &stack);
+
+  TORCH_INTERNAL_ASSERT(stack.size() == 0, "A boxed kernel returned a value but when we called it with KernelFunction::callUnboxed, we expected it to return void.");
 }
 
 }
