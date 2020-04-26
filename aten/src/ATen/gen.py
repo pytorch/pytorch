006d605c38efe2b988c972c46d53e7b68abecda6
Dylan Bespalko <dylanbespalko@me.com>
2020-04-25 18:15:11 -0600

    Changed gen.py so that it can be called by out-of-tree device code

--- aten/src/ATen/gen.py
+++ aten/src/ATen/gen.py
@@ -368,10 +368,11 @@ def generate_storage_type_and_tensor(backend, density, declarations, per_op_regi
     if env['DeviceType'] == 'CPU':
         top_env['cpu_type_headers'].append(
             '#include <ATen/{}.h>'.format(env['Type']))
-    else:
-        assert env['DeviceType'] == 'CUDA'
+    elif env['DeviceType'] == 'CUDA':
         top_env['cuda_type_headers'].append(
             '#include <ATen/{}.h>'.format(env['Type']))
+    else:  # Allow gen.py to be called by out-of-tree devices
+        pass
 
 
 # yields (backend, density) tuples
