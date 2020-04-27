c92aed5b50a8fcda32d69e58390cd227800c3a33
peter <peterghost86@gmail.com>
2020-04-27 17:34:59 +0800

    Test CUDA separate compilation on Windows

--- .jenkins/pytorch/win-test-helpers/build_pytorch.bat
+++ .jenkins/pytorch/win-test-helpers/build_pytorch.bat
@@ -62,6 +62,7 @@ set CUDA_TOOLKIT_ROOT_DIR=%CUDA_PATH%
 set CUDNN_ROOT_DIR=%CUDA_PATH%
 set NVTOOLSEXT_PATH=C:\Program Files\NVIDIA Corporation\NvToolsExt
 set PATH=%CUDA_PATH%\bin;%CUDA_PATH%\libnvvp;%PATH%
+set CUDA_SEPARABLE_COMPILATION=YES
 
 :cuda_build_end
 
