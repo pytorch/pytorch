64b0a19bba6c1f621a8c6d4874b27095efd146e6
Zafar <cc.rafaz@zafar.cc>
2020-04-28 00:58:54 -0700

    Update on "[quant] Refactor QNNPACK conv for deconv"
    Differential Revision: [D21110112](https://our.internmc.facebook.com/intern/diff/D21110112)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cpu/qnnpack/include/qnnpack_func.h
+++ aten/src/ATen/native/quantized/cpu/qnnpack/include/qnnpack_func.h
@@ -107,20 +107,6 @@ enum pytorch_qnnp_status qnnpackConv(
     uint8_t* output,
     pthreadpool_t threadpool);
 
-enum pytorch_qnnp_status qnnpackDeConv(
-    const deconv_param_t& conv_p,
-    void* packed_weights,
-    const size_t batch_size,
-    const size_t input_height,
-    const size_t input_width,
-    const float input_scale,
-    const uint8_t input_zero_point,
-    const uint8_t* input,
-    const float output_scale,
-    const uint8_t output_zero_point,
-    uint8_t* output,
-    pthreadpool_t threadpool);
-
 enum pytorch_qnnp_status qnnpackLinearDynamic(
     const size_t batch_size,
     const size_t input_channels,
