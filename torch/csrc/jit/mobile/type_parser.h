6a651567c564da6ff7b5084bb9c352fa40ee592a
Michael Suo <suo@fb.com>
2020-04-27 21:56:08 -0700

    Fix for mobile type parser
    The mobile type resolver that we inject into the unpickler currently
    creates a dummy type for everything, even built-in types like
    `List[int]`. This PR makes the necessary changes so that we first check
    if a primitive type is parseable, but otherwise fall back to the dummy
    type thing.
    
    [ghstack-poisoned]

--- torch/csrc/jit/mobile/type_parser.h
+++ torch/csrc/jit/mobile/type_parser.h
@@ -2,4 +2,11 @@
 
 namespace c10 {
 TORCH_API TypePtr parseType(const std::string& pythonStr);
+TORCH_API TypePtr parseType(
+    const std::string& pythonStr,
+    // Function to resolve non-primitive types to a TypePtr, typically by
+    // looking them up in some sort of environment. Similar to
+    // `torch::jit::Resolver`, which we don't use here because it pulls in some
+    // JIT headers.
+    std::function<TypePtr(const std::string&)> resolver);
 } // namespace c10
