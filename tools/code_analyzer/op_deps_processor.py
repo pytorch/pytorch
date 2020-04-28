b0103ce3cd0dfedc22c009fc0c50275429917dc5
ljk53 <liujiakai@fb.com>
2020-04-27 21:40:10 -0700

    [pytorch] remove unused flags from code analyzer & move format support to python
    Summary:
    Simplify the code analyzer by removing some unused flags and moving the
    different format printer logic to python script. It's easier to add other
    post processing logic to adapt to different BUCK build configs.
    
    [ghstack-poisoned]

---
+++ tools/code_analyzer/op_deps_processor.py
@@ -0,0 +1,109 @@
+"""
+This util is used to parse op_deps_pass output (in yaml) and convert it into
+other formats for downstream use cases.
+
+To run this file by hand from the root of the PyTorch
+repository, run:
+
+python -m tools.code_analyzer.op_deps_processor \
+  --op-dependency build_code_analyzer/work/torch_result.yaml \
+  --output pt_deps.bzl
+"""
+
+import argparse
+import yaml
+
+from ..autograd.utils import CodeTemplate
+
+BAZEL_OUTPUT = CodeTemplate("""\
+# Generated for selective build without using static dispatch.
+# Manually run the script to update:
+# ANALYZE_TORCH=1 DEPLOY=1 tools/code_analyzer/build.sh
+TORCH_DEPS = {
+${ops}
+}
+""")
+
+BAZEL_OP = CodeTemplate("""\
+    "${op_name}": [
+${op_deps}
+    ],
+""")
+
+BAZEL_OP_DEP = CodeTemplate("""\
+        "${dep_name}",
+""")
+
+DOT_OUTPUT = CodeTemplate("""\
+digraph {
+    layout="circo";
+${ops}
+}
+""")
+
+DOT_OP = CodeTemplate("""\
+${op_deps}
+""")
+
+DOT_OP_DEP = CodeTemplate("""\
+    "${op_name}" -> "${dep_name}";
+""")
+
+def load_op_deps(fname):
+    with open(fname, 'r') as stream:
+        return yaml.safe_load(stream)
+
+
+def convert(fname, graph, OUTPUT, OP, OP_DEP):
+    ops = []
+    for op in graph:
+        op_name = op['name']
+        op_deps = []
+
+        for dep in op.get('depends', []):
+            dep_name = dep['name']
+            if dep_name == op_name:
+                # skip itself reference
+                continue
+            op_deps.append(
+                OP_DEP.substitute(
+                    op_name=op_name,
+                    dep_name=dep_name))
+
+        if not op_deps:
+            # skip ops without any fanout
+            continue
+
+        ops.append(
+            OP.substitute(
+                op_name=op_name,
+                op_deps=op_deps))
+
+    with open(fname, 'w') as out:
+        out.write(OUTPUT.substitute(ops=ops))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Util to produce transitive dependencies for custom build')
+    parser.add_argument(
+        '--op-dependency',
+        required=True,
+        help='input yaml file of op dependency graph produced by op_deps_pass')
+    parser.add_argument(
+        '--format',
+        default='bazel',
+        help='output file format [bazel, dot]')
+    parser.add_argument(
+        '--output',
+        required=True,
+        help='output file')
+    args = parser.parse_args()
+
+    deps = load_op_deps(args.op_dependency)
+    if args.format == 'bazel':
+        convert(args.output, deps, BAZEL_OUTPUT, BAZEL_OP, BAZEL_OP_DEP)
+    elif args.format == 'dot':
+        convert(args.output, deps, DOT_OUTPUT, DOT_OP, DOT_OP_DEP)
+    else:
+        raise Exception("Unknown output format: " + args.format)
