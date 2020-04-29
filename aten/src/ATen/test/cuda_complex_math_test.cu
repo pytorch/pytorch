68e2b96daf6356b5bc5f4d6dd1551aa486ba2aa9
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-28 21:09:45 -0700

    SKIP_IF_NO_GPU

--- aten/src/ATen/test/cuda_complex_math_test.cu
+++ aten/src/ATen/test/cuda_complex_math_test.cu
@@ -1,14 +1,31 @@
 #include <gtest/gtest.h>
 
+int safeDeviceCount() {
+  int count;
+  cudaError_t err = cudaGetDeviceCount(&count);
+  if (err == cudaErrorInsufficientDriver || err == cudaErrorNoDevice) {
+    return 0;
+  }
+  return count;
+}
+
+#define SKIP_IF_NO_GPU()                    \
+  do {                                      \
+    if (safeDeviceCount() == 0) {           \
+      return;                               \
+    }                                       \
+  } while(0)
+
 #define C10_ASSERT_NEAR(a, b, tol) assert(abs(a - b) < tol)
-#define C10_DEFINE_TEST(a, b) \
-__global__ void CUDA##a##b(); \
-TEST(a##Device, b) {\
-  cudaDeviceSynchronize();\
-  CUDA##a##b<<<1, 1>>>();\
-  cudaDeviceSynchronize();\
-  ASSERT_EQ(cudaGetLastError(), cudaSuccess);\
-}\
+#define C10_DEFINE_TEST(a, b)                       \
+__global__ void CUDA##a##b();                       \
+TEST(a##Device, b) {                                \
+  SKIP_IF_NO_GPU();                                 \
+  cudaDeviceSynchronize();                          \
+  CUDA##a##b<<<1, 1>>>();                           \
+  cudaDeviceSynchronize();                          \
+  ASSERT_EQ(cudaGetLastError(), cudaSuccess);       \
+}                                                   \
 __global__ void CUDA##a##b()
 #include <c10/test/util/complex_math_test_common.h>
 
