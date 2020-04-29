dc0f434ca5a6d015c5ce78a7cde24b080e986d2d
peter <peterghost86@gmail.com>
2020-04-29 12:40:16 +0800

    Try reverting the change

--- cmake/MiscCheck.cmake
+++ cmake/MiscCheck.cmake
@@ -202,25 +202,21 @@ endif()
 cmake_pop_check_state()
 
 # ---[ Checks if compiler supports -fvisibility=hidden
-if(NOT MSVC)
-  check_cxx_compiler_flag("-fvisibility=hidden" COMPILER_SUPPORTS_HIDDEN_VISIBILITY)
-  check_cxx_compiler_flag("-fvisibility-inlines-hidden" COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY)
-  if (${COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY})
-    set(CAFFE2_VISIBILITY_FLAG "-fvisibility-inlines-hidden")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CAFFE2_VISIBILITY_FLAG}")
-  endif()
+check_cxx_compiler_flag("-fvisibility=hidden" COMPILER_SUPPORTS_HIDDEN_VISIBILITY)
+check_cxx_compiler_flag("-fvisibility-inlines-hidden" COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY)
+if(${COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY})
+  set(CAFFE2_VISIBILITY_FLAG "-fvisibility-inlines-hidden")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CAFFE2_VISIBILITY_FLAG}")
 endif()
 
 # ---[ Checks if linker supports -rdynamic. `-rdynamic` tells linker
 # -to add all (including unused) symbols into the dynamic symbol
 # -table. We need this to get symbols when generating backtrace at
 # -runtime.
-if(NOT MSVC)
-  check_cxx_compiler_flag("-rdynamic" COMPILER_SUPPORTS_RDYNAMIC)
-  if (${COMPILER_SUPPORTS_RDYNAMIC})
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -rdynamic")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -rdynamic")
-  endif()
+check_cxx_compiler_flag("-rdynamic" COMPILER_SUPPORTS_RDYNAMIC)
+if(${COMPILER_SUPPORTS_RDYNAMIC})
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -rdynamic")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -rdynamic")
 endif()
 
 # ---[ If we are using msvc, set no warning flags
