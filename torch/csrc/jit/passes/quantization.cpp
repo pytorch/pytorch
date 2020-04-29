0334b0cfca6f2e71f0ba856ec365a51cd836677c
vasiliy <vasiliy@fb.com>
2020-04-29 12:59:35 -0700

    graph mode: add hardswish op
    Summary:
    
    Adds graph mode handling for the hardswish op.
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestQuantizeScriptPTSQOps.test_hardswish
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -51,6 +51,7 @@ std::vector<std::string> _quantizable_call_funcs = {
     "conv2d",
     "linear",
     "batch_norm",
+    "hardswish",
 };
 
 std::vector<std::string> _quantizable_aten_funcs = {
@@ -65,6 +66,7 @@ std::vector<std::string> _quantizable_aten_funcs = {
     "lstm",
     "mul",
     "mul_",
+    "hardswish",
 };
 
 // These are the prim::CallFunctions that doesn't require observation and
