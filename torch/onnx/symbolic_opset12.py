e09b3da27b8eac4c295c9c83ec66181e85069f1d
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-04-30 16:01:48 -0700

    fix flake8 error

--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -27,7 +27,7 @@ def dropout(g, input, p, train):
     # in eval mode, dropout is non-op - if the node's train param is set to False, dropout is non-op
     if not sym_help._training_mode:
         return input
-    
+
     t = g.op("Constant", value_t=torch.tensor(True))
     r, _ = g.op("Dropout", input, p, outputs=2)
     return r
