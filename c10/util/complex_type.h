404cb53de60955fa83c8bf9c899a37a6bacaeaca
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-30 13:04:20 -0700

    Overload c10::complex operators inside c10 namespace

--- c10/util/complex_type.h
+++ c10/util/complex_type.h
@@ -290,7 +290,6 @@ constexpr complex<double> operator"" _id(unsigned long long imag) {
 
 } // namespace complex_literals
 
-} // namespace c10
 
 template<typename T>
 constexpr c10::complex<T> operator+(const c10::complex<T>& val) {
@@ -415,6 +414,8 @@ std::basic_istream<CharT, Traits>& operator>>(std::basic_istream<CharT, Traits>&
   x = tmp;
   return is;
 }
+
+} // namespace c10
 
 // std functions
 //
