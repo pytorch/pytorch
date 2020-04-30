f7e95fc744d3ef96ba7a440ad0f731101fc76c91
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-04-29 22:46:11 -0700

    Update operator_visibility_test.cpp

--- aten/src/ATen/test/operator_visibility_test.cpp
+++ aten/src/ATen/test/operator_visibility_test.cpp
@@ -1,12 +1,16 @@
-#include <ATen/ATen.h>
-
 class A {};
 void operator+(A, A) {}
 
+#include <ATen/ATen.h>
+
+class B {};
+void operator+(B, B) {}
+
 namespace at { namespace native {
 
-void f(A a) {
+void f(A a, B b) {
   a + a;
+  b + b;
 }
 
 }}
