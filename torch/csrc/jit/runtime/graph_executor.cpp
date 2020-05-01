a410f142221e84aef24fb962c5c808bdbd434bfd
Protonu Basu <protonu@fb.com>
2020-05-01 15:45:50 -0700

    changes to enable CI test to run with NNC

--- torch/csrc/jit/runtime/graph_executor.cpp
+++ torch/csrc/jit/runtime/graph_executor.cpp
@@ -779,9 +779,16 @@ void runNondiffOptimization(
   // Fuse the dequant - op - quant patterns into quantized ops
   QuantFusion(graph);
 
-  FuseGraph(graph, strict_fuser_check);
-
-  FuseTensorExprs(graph);
+  //FuseGraph(graph, strict_fuser_check);
+  // strict_fuser_check is synomous with ProfilingExecutor on
+  // if `strict_fuser_check` is set to `true`, run TE by default
+  // otherwise fallback to the legacy executor and legacy fuser
+  if (strict_fuser_check) {
+    fuseTensorExprs(graph);
+  }
+  else {
+    FuseGraph(graph, strict_fuser_check);
+  }
 
   // Run custom post-fusion passes
   for (const auto& passPair : getCustomPostPasses()) {
