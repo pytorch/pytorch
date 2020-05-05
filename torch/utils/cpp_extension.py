dcc5b0c2632b3caf37bbbc71b6a46fa8ef2a6cb2
ashishfarmer <ashish.farmer@amd.com>
2020-05-05 00:03:52 +0000

    add appropriate flags for rocm compilation

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -428,7 +428,11 @@ class BuildExtension(build_ext, object):
                     cuda_post_cflags = extra_postargs['nvcc']
                 else:
                     cuda_post_cflags = list(extra_postargs)
-                cuda_post_cflags = unix_cuda_flags(cuda_post_cflags)
+                if IS_HIP_EXTENSION:
+                    cuda_post_cflags = cuda_post_cflags + _get_rocm_arch_flags(cuda_post_cflags)
+                    cuda_post_cflags = cuda_post_cflags + COMMON_HIPCC_FLAGS
+                else:
+                    cuda_post_cflags = unix_cuda_flags(cuda_post_cflags)
                 append_std14_if_no_std_present(cuda_post_cflags)
 
             _write_ninja_file_and_compile_objects(
