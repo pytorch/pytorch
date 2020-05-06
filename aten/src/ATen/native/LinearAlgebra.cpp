17c842d647a37f9069c9f2f84118d6db5f5e3032
Gregory Chanan <gchanan@fb.com>
2020-05-05 17:09:36 -0700

    Move addmm broadcasting code from codegen layer to native layer. (#37613)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/37613
    
    Test Plan: Imported from OSS
    
    Differential Revision: D21337341
    
    Pulled By: gchanan
    
    fbshipit-source-id: 064e983e0dc4334c5eed9df1af57bd7fc29d7a81

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -183,6 +183,18 @@ Tensor& addbmm_cpu_out(Tensor& result, const Tensor& self, const Tensor& batch1,
   return legacy::cpu::_th_addbmm_out(result, b_self, batch1, batch2, beta, alpha);
 }
 
+Tensor addmm_cpu(const Tensor& self, const Tensor& mat1, const Tensor& mat2, Scalar beta, Scalar alpha) {
+  Tensor b_self;
+  std::tie(b_self) = expand_size(self, {mat1.size(0), mat2.size(1)}, "addmm");
+  return legacy::cpu::_th_addmm(b_self, mat1, mat2, beta, alpha);
+}
+
+Tensor& addmm_cpu_out(Tensor &result, const Tensor& self, const Tensor& mat1, const Tensor& mat2, Scalar beta, Scalar alpha) {
+  Tensor b_self;
+  std::tie(b_self) = expand_size(self, {mat1.size(0), mat2.size(1)}, "addmm_out");
+  return legacy::cpu::_th_addmm_out(result, b_self, mat1, mat2, beta, alpha);
+}
+
 template <typename scalar_t, bool is_bmm>
 inline void baddbmm_cpu_kernel(const Tensor& result, const Tensor& self, const Tensor& mat2, Scalar beta_, Scalar alpha_) {
   int64_t bs = result.size(0);
