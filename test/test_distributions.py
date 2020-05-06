05c60b8bf3c9cb92d6dad0944fc33b04d039cffd
Protonu Basu <protonu@fb.com>
2020-05-06 13:15:16 -0700

    remmoving comments to skip tests that were segfaulting

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -778,7 +778,7 @@ class TestDistributions(TestCase):
                 dist = Dist(**param)
                 self.assertTrue(repr(dist).startswith(dist.__class__.__name__))
 
-    @unittest.skip("this segfaults")
+    #
     def test_sample_detached(self):
         for Dist, params in EXAMPLES:
             for i, param in enumerate(params):
@@ -804,7 +804,7 @@ class TestDistributions(TestCase):
                                 msg='{} example {}/{}, .rsample() does not require grad'.format(
                                     Dist.__name__, i + 1, len(params)))
 
-    @unittest.skip("this segfaults")
+
     def test_enumerate_support_type(self):
         for Dist, params in EXAMPLES:
             for i, param in enumerate(params):
@@ -849,7 +849,7 @@ class TestDistributions(TestCase):
                 self.assertIn(Dist, distributions_with_examples,
                               "Please add {} to the EXAMPLES list in test_distributions.py".format(Dist.__name__))
 
-    @unittest.skip("this segfaults")
+
     def test_distribution_expand(self):
         shapes = [torch.Size(), torch.Size((2,)), torch.Size((2, 1))]
         for Dist, params in EXAMPLES:
@@ -877,7 +877,7 @@ class TestDistributions(TestCase):
                     except NotImplementedError:
                         pass
 
-    @unittest.skip("this segfaults")
+
     def test_distribution_subclass_expand(self):
         expand_by = torch.Size((2,))
         for Dist, params in EXAMPLES:
@@ -1402,7 +1402,7 @@ class TestDistributions(TestCase):
         high.grad.zero_()
 
     @unittest.skipIf(not TEST_NUMPY, "NumPy not found")
-    @unittest.skip("this segfaults")
+
     def test_vonmises_sample(self):
         for loc in [0.0, math.pi / 2.0]:
             for concentration in [0.03, 0.3, 1.0, 10.0, 100.0]:
@@ -2469,7 +2469,7 @@ class TestDistributions(TestCase):
                          (2, 5, 2, 3, 5))
         self.assertEqual(ContinuousBernoulli(p).sample((2,)).size(), (2, 2, 3, 5))
 
-    @unittest.skip("this segfaults")
+
     def test_independent_shape(self):
         for Dist, params in EXAMPLES:
             for param in params:
@@ -2498,7 +2498,7 @@ class TestDistributions(TestCase):
                     except NotImplementedError:
                         pass
 
-    @unittest.skip("this segfaults")
+
     def test_independent_expand(self):
         for Dist, params in EXAMPLES:
             for param in params:
@@ -2516,7 +2516,7 @@ class TestDistributions(TestCase):
                         self.assertEqual(expanded.event_shape, indep_dist.event_shape)
                         self.assertEqual(expanded.batch_shape, expanded_shape)
 
-    @unittest.skip("this segfaults")
+
     def test_cdf_icdf_inverse(self):
         # Tests the invertibility property on the distributions
         for Dist, params in EXAMPLES:
@@ -2536,7 +2536,7 @@ class TestDistributions(TestCase):
                     'icdf(cdf(x)) = {}'.format(actual),
                 ]))
 
-    @unittest.skip("this segfaults")
+
     def test_cdf_log_prob(self):
         # Tests if the differentiation of the CDF gives the PDF at a given value
         for Dist, params in EXAMPLES:
@@ -3232,7 +3232,7 @@ class TestDistributionShapes(TestCase):
         self.assertEqual(gumbel.log_prob(self.tensor_sample_1).size(), torch.Size((3, 2)))
         self.assertEqual(gumbel.log_prob(self.tensor_sample_2).size(), torch.Size((3, 2, 3)))
 
-    @unittest.skip("this segfaults")
+
     def test_vonmises_shape_tensor_params(self):
         von_mises = VonMises(torch.tensor([0., 0.]), torch.tensor([1., 1.]))
         self.assertEqual(von_mises._batch_shape, torch.Size((2,)))
@@ -3242,7 +3242,7 @@ class TestDistributionShapes(TestCase):
         self.assertEqual(von_mises.log_prob(self.tensor_sample_1).size(), torch.Size((3, 2)))
         self.assertEqual(von_mises.log_prob(torch.ones(2, 1)).size(), torch.Size((2, 2)))
 
-    @unittest.skip("this segfaults")
+
     def test_vonmises_shape_scalar_params(self):
         von_mises = VonMises(0., 1.)
         self.assertEqual(von_mises._batch_shape, torch.Size())
@@ -3769,7 +3769,7 @@ class TestConstraints(TestCase):
                         Dist.__name__, i + 1, len(params), name, value)
                     self.assertTrue(constraint.check(value).all(), msg=message)
 
-    @unittest.skip("this segfaults")
+
     def test_support_constraints(self):
         for Dist, params in EXAMPLES:
             self.assertIsInstance(Dist.support, Constraint)
@@ -4774,7 +4774,7 @@ class TestJit(TestCase):
             sample = Dist(**param).sample()
             return values, sample
 
-    @unittest.skip("this segfaults")
+
     def test_sample(self):
         for Dist, keys, values, sample in self._examples():
 
@@ -4804,7 +4804,7 @@ class TestJit(TestCase):
             if Dist not in xfail:
                 self.assertTrue(any(n.isNondeterministic() for n in traced_f.graph.nodes()))
 
-    @unittest.skip("this segfaults")
+
     def test_rsample(self):
         for Dist, keys, values, sample in self._examples():
             if not Dist.has_rsample:
@@ -4856,7 +4856,7 @@ class TestJit(TestCase):
             self.assertEqual(expected, actual,
                              message='{}\nExpected:\n{}\nActual:\n{}'.format(Dist.__name__, expected, actual))
 
-    @unittest.skip("this segfaults")
+
     def test_enumerate_support(self):
         for Dist, keys, values, sample in self._examples():
             # FIXME traced functions produce incorrect results
@@ -4881,7 +4881,7 @@ class TestJit(TestCase):
             self.assertEqual(expected, actual,
                              message='{}\nExpected:\n{}\nActual:\n{}'.format(Dist.__name__, expected, actual))
 
-    @unittest.skip("this segfaults")
+
     def test_mean(self):
         for Dist, keys, values, sample in self._examples():
 
@@ -4904,7 +4904,7 @@ class TestJit(TestCase):
             self.assertEqual(expected, actual, allow_inf=True,
                              message='{}\nExpected:\n{}\nActual:\n{}'.format(Dist.__name__, expected, actual))
 
-    @unittest.skip("this segfaults")
+
     def test_variance(self):
         for Dist, keys, values, sample in self._examples():
             if Dist in [Cauchy, HalfCauchy]:
@@ -4953,7 +4953,7 @@ class TestJit(TestCase):
             self.assertEqual(expected, actual, allow_inf=True,
                              message='{}\nExpected:\n{}\nActual:\n{}'.format(Dist.__name__, expected, actual))
 
-    @unittest.skip("this segfaults")
+
     def test_cdf(self):
         for Dist, keys, values, sample in self._examples():
 
