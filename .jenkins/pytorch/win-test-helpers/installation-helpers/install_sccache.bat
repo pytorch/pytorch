aee4a400542c8eb6c44549ea60efef75cbc5bac7
peter <peterghost86@gmail.com>
2020-05-07 00:59:01 +0800

    Test

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -7,10 +7,10 @@ if "%REBUILD%"=="" (
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
     del %TMP_DIR_WIN%\bin\sccache-cl.exe || ver > nul
     if "%BUILD_ENVIRONMENT%"=="" (
-      curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20200506.exe --output %TMP_DIR_WIN%\bin\sccache.exe
+      curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20200507.exe --output %TMP_DIR_WIN%\bin\sccache.exe
       curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %TMP_DIR_WIN%\bin\sccache-cl.exe
     ) else (
-      aws s3 cp s3://ossci-windows/sccache-20200506.exe %TMP_DIR_WIN%\bin\sccache.exe
+      aws s3 cp s3://ossci-windows/sccache-20200507.exe %TMP_DIR_WIN%\bin\sccache.exe
       aws s3 cp s3://ossci-windows/sccache-cl.exe %TMP_DIR_WIN%\bin\sccache-cl.exe
     )
     goto :check_sccache
