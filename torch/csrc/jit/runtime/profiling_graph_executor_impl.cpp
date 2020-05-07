84b63ed92b4c661cfe24cdffb932fcaddc27dff9
Mikhail Zolotukhin <mvz@fb.com>
2020-05-07 14:17:29 -0700

    Enable PE too. on "[TensorExpr] Turn on TensorExpr fuser and with fallbacks disabled."
    Differential Revision: [D21463714](https://our.internmc.facebook.com/intern/diff/D21463714)
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
+++ torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
@@ -36,10 +36,10 @@ namespace jit {
 // TODO: keep the else clause for trial runs
 #if defined(FBCODE_CAFFE2) || defined(C10_MOBILE)
 static std::atomic<bool> executor_mode{true};
-static std::atomic<bool> profiling_mode{false};
+static std::atomic<bool> profiling_mode{true};
 #else
 static std::atomic<bool> executor_mode{true};
-static std::atomic<bool> profiling_mode{false};
+static std::atomic<bool> profiling_mode{true};
 #endif
 
 static std::atomic<size_t> num_profiled_runs{1};
