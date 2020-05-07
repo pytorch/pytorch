e933e07c841769952142fbd9128e2760e55d87d5
Protonu Basu <protonu@fb.com>
2020-05-07 14:14:36 -0700

    adding the raise back in test/run_test.py

--- test/run_test.py
+++ test/run_test.py
@@ -679,8 +679,8 @@ def main():
                 # return code -N, where N is the signal number.
                 signal_name = SIGNALS_TO_NAMES_DICT[-return_code]
                 message += ' Received signal: {}'.format(signal_name)
-            print(message, file=sys.stderr)
-            #raise RuntimeError(message)
+            #print(message, file=sys.stderr)
+            raise RuntimeError(message)
     if options.coverage:
         shell(['coverage', 'combine'])
         shell(['coverage', 'html'])
