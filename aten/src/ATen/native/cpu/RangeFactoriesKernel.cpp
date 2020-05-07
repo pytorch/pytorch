27b625819678299684eeb4e5f2552608d5a28065
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-07 10:55:27 -0700

    Update on "Migrate CPU tensor factories to c10::complex"
    [ghstack-poisoned]

--- aten/src/ATen/native/cpu/RangeFactoriesKernel.cpp
+++ aten/src/ATen/native/cpu/RangeFactoriesKernel.cpp
@@ -19,23 +19,23 @@ static void linspace_kernel(TensorIterator& iter, Scalar scalar_start, Scalar sc
     int64_t idx(0);
     scalar_t start = scalar_start.to<scalar_t>();
     scalar_t end = scalar_end.to<scalar_t>();
-    step_t step = static_cast<step_t>(end - start) / (steps - 1);
+    step_t step = static_cast<step_t>(end - start) / static_cast<step_t>(steps - 1);
     int64_t halfway = steps / 2;
     cpu_serial_kernel_vec(
         iter,
         [start, end, step, halfway, steps, &idx]() -> scalar_t {
           if (idx < halfway) {
-            return start + step * (idx++);
+            return start + step * static_cast<step_t>(idx++);
           } else {
-            return end - step * (steps - (idx++) - 1);
+            return end - step * static_cast<step_t>(steps - (idx++) - 1);
           }
         },
         [start, end, step, halfway, steps, &idx]() -> Vec256<scalar_t> {
           Vec256<scalar_t> res;
           if (idx < halfway) {
-            res = Vec256<scalar_t>::arange(start + step * idx, step);
+            res = Vec256<scalar_t>::arange(start + step * static_cast<step_t>(idx), step);
           } else {
-            res = Vec256<scalar_t>::arange(end - step * (steps - idx - 1), step);
+            res = Vec256<scalar_t>::arange(end - step * static_cast<step_t>(steps - idx - 1), step);
           }
           idx += Vec256<scalar_t>::size();
           return res;
