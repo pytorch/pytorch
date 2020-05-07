04038abac8d055a264d9581e52a488c3be4b8f1a
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-06 19:43:55 -0700

    Move complex utilities out of Half.h (#37676)
    Summary:
    There is no reason to put complex utilities to half header.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37676
    
    Differential Revision: D21440270
    
    Pulled By: anjali411
    
    fbshipit-source-id: bbed5fcb5be33f6a4aedcc9932595d43d97672f6

--- c10/util/Half.h
+++ c10/util/Half.h
@@ -379,35 +379,6 @@ struct alignas(4) complex<Half> {
   }
 };
 
-template <typename T>
-struct is_complex_t : public std::false_type {};
-
-template <typename T>
-struct is_complex_t<std::complex<T>> : public std::true_type {};
-
-template <typename T>
-struct is_complex_t<c10::complex<T>> : public std::true_type {};
-
-
-// Extract double from std::complex<double>; is identity otherwise
-// TODO: Write in more idiomatic C++17
-template <typename T>
-struct scalar_value_type {
-  using type = T;
-};
-template <typename T>
-struct scalar_value_type<std::complex<T>> {
-  using type = T;
-};
-template <typename T>
-struct scalar_value_type<c10::complex<T>> {
-  using type = T;
-};
-template <>
-struct scalar_value_type<complex<Half>> {
-  using type = Half;
-};
-
 // In some versions of MSVC, there will be a compiler error when building.
 // C4146: unary minus operator applied to unsigned type, result still unsigned
 // C4804: unsafe use of type 'bool' in operation
