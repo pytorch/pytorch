927eca7a790f4c2ebae7a081a22d93631feaac43
Ralf Gommers <ralf.gommers@gmail.com>
2020-05-08 10:07:46 -0700

    Replace torch.is_tensor usages with isinstance checks. (#38062)
    Summary:
    `is_tensor` doesn't really have a reason to exist anymore (other than
    backwards compatibility) and is worse for typechecking with mypy (see
    gh-32824). Given that it may not be obvious what the fix is once mypy
    gives an error, make the change in a number of places at once, and add
    a note on this to the `is_tensor` docstring.
    
    Recommending an isinstance check instead has been done for quite a
    while, e.g. https://github.com/pytorch/pytorch/pull/7769#discussion_r190458971
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38062
    
    Differential Revision: D21470963
    
    Pulled By: ezyang
    
    fbshipit-source-id: 98dd60d32ca0650abd2de21910b541d32b0eea41

--- test/test_torch.py
+++ test/test_torch.py
@@ -13735,7 +13735,7 @@ class TestTorchDeviceType(TestCase):
 
     def _test_unique_with_expects(self, device, dtype, f, x, expected_unique, expected_inverse, expected_counts, additional_shape):
         def ensure_tuple(x):
-            if torch.is_tensor(x):
+            if isinstance(x, torch.Tensor):
                 return (x,)
             return x
 
@@ -13764,7 +13764,7 @@ class TestTorchDeviceType(TestCase):
             return  # CPU does not have half support
 
         def ensure_tuple(x):
-            if torch.is_tensor(x):
+            if isinstance(x, torch.Tensor):
                 return (x,)
             return x
 
@@ -17718,13 +17718,13 @@ def generate_test_function(cls,
 
         # Converts CPU tensors to device tensors
         device_tensor = cpu_tensor.to(dtype=dtype, device=device)
-        device_args = [arg.to(device=device) if torch.is_tensor(arg) else arg for arg in cpu_args]
+        device_args = [arg.to(device=device) if isinstance(arg, torch.Tensor) else arg for arg in cpu_args]
 
         # Converts float device tensors to half/bfloat16 when the dtype is half/bfloat16
         # Note: CPU half tensors don't support many operations.
         if dtype in {torch.half, torch.bfloat16}:
             device_args = [arg.to(dtype=dtype) if
-                           (torch.is_tensor(arg) and arg.dtype == torch.float) else arg
+                           (isinstance(arg, torch.Tensor) and arg.dtype == torch.float) else arg
                            for arg in device_args]
 
         # Runs the tensor op on CPU and device
