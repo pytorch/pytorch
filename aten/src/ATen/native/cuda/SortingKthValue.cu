34502e60a290580e690f616f22db1b56513f1217
Emilio Castillo <ecastill@preferred.jp>
2020-05-11 08:12:51 +0000

    Fix nan comparison in `torch.median`

--- aten/src/ATen/native/cuda/SortingKthValue.cu
+++ aten/src/ATen/native/cuda/SortingKthValue.cu
@@ -73,8 +73,9 @@ __global__ void gatherKthValue(
     bool inRange = (i < inputSliceSize);
     scalar_t v = inRange ? doLdg(&inputSliceStart[i * inputWithinSliceStride])
                          : static_cast<scalar_t>(0);
-    bool isKValue = inRange && THCNumerics<scalar_t>::eq_with_nan(v, kValue);
-
+    // Check for NaN values, comparing two different NaNs using eq_with_nan
+    // returns always false, even if both values are nan
+    bool isKValue = inRange && ( (v == kValue) || (v != v) && (kValue != kValue));
     if (isKValue) {
       kValueIndex = i;
       foundKValue = true;
