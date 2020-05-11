6ab149598486ed7576197b5ffb90999ae644bcd3
Gregory Chanan <gchanan@fb.com>
2020-05-11 13:15:19 -0700

    Update on "Split up BinaryAritmeticKernel.cu to speed up compilation time."
    On my machine, compilation went from 4m8sec to the maximum of the files being compiled in 2m22sec.
    
    Differential Revision: [D21508985](https://our.internmc.facebook.com/intern/diff/D21508985)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/BinaryRemainderKernel.cu
+++ aten/src/ATen/native/cuda/BinaryRemainderKernel.cu
@@ -10,43 +10,6 @@
 
 namespace at { namespace native {
 
-void div_kernel_cuda(TensorIterator& iter) {
-  if (!isIntegralType(iter.common_dtype(), /*includeBool*/ false) && iter.is_cpu_scalar(2)) {
-    // optimization for floating-point types: if the second operand is a CPU
-    // scalar, compute a * reciprocal(b). Note that this may lose one bit of
-    // precision compared to computing the division.
-    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16, iter.common_dtype(), "div_cuda", [&]() {
-      using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
-      auto inv_b = thrust_t(1.0) / thrust_t(iter.scalar_value<scalar_t>(2));
-      iter.remove_operand(2);
-      gpu_kernel(iter, [inv_b]GPU_LAMBDA(thrust_t a) -> thrust_t {
-        return a * inv_b;
-      });
-    });
-  } else {
-    AT_DISPATCH_ALL_TYPES_AND_C10_COMPLEX_AND2(kHalf, kBFloat16, iter.common_dtype(), "div_cuda", [&]() {
-      gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-        return a / b;
-      });
-    });
-  }
-}
-
-void mul_kernel_cuda(TensorIterator& iter) {
-  if (iter.common_dtype() == ScalarType::Bool) {
-    // Workaround for the error: '*' in boolean context, suggest '&&' instead [-Werror=int-in-bool-context]
-    gpu_kernel_with_scalars(iter, []GPU_LAMBDA(bool a, bool b) -> bool {
-      return a && b;
-    });
-  } else {
-    AT_DISPATCH_ALL_TYPES_AND_C10_COMPLEX_AND2(kHalf, kBFloat16, iter.common_dtype(), "mul_cuda", [&]() {
-      gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-        return a * b;
-      });
-    });
-  }
-}
-
 void remainder_kernel_cuda(TensorIterator& iter) {
   if (isIntegralType(iter.dtype(), /*includeBool*/ false)) {
     AT_DISPATCH_INTEGRAL_TYPES(iter.dtype(), "remainder_cuda", [&]() {
