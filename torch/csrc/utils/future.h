649b83ffd064bf89001e841c4356bc107a4b1c7b
jeremyl <jeremyl@devvm096.ftw0.facebook.com>
2020-05-12 14:33:32 -0700

    [future] Make new IValue future constValue semantics match torch::utils counterpart
    The torch::utils::Future api which this api was copied from last week
    intentionally does not throw. Harmonize the semantics and comment
    appropriately.
    
    Differential Revision: [D21533016](https://our.internmc.facebook.com/intern/diff/D21533016/)
    
    [ghstack-poisoned]

--- torch/csrc/utils/future.h
+++ torch/csrc/utils/future.h
@@ -47,6 +47,8 @@ class TORCH_API Future final {
     return value_;
   }
 
+  // These constValue/moveValue accessors should only be used if
+  // we know that the future is completed() with no error.
   const T& constValue() const {
     std::unique_lock<std::mutex> lock(mutex_);
     AT_ASSERT(completed_);
