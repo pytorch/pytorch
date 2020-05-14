16696186e1f6a84cf2e925a12c71222c69f7e137
Jerry Zhang <jerryzh@fb.com>
2020-05-13 17:46:15 -0700

    [quant][graphmode] Move elu to general value ops map (#38165)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/38165
    
    Test Plan: Imported from OSS
    
    Differential Revision: D21559812
    
    fbshipit-source-id: 55bc28d71d0b8a1c33e05bce20a802db1015ea0b

--- torch/csrc/jit/passes/quantization/helper.cpp
+++ torch/csrc/jit/passes/quantization/helper.cpp
@@ -64,8 +64,6 @@ std::vector<std::string> _single_input_general_shape_call_funcs = {
     "relu_",
     "sigmoid",
     "tanh",
-    "elu",
-    "elu_",
     "hardsigmoid",
     "hardsigmoid_",
     "leaky_relu",
@@ -78,16 +76,15 @@ std::vector<std::string> _single_input_general_shape_call_funcs = {
 // operation only depends on the shape of the Tensor
 // e.g. `aten::flatten(%input_tensor, ...)`
 std::vector<std::string> _single_input_general_shape_aten_funcs = {
-    "max_pool1d",  "max_pool2d",   "max_pool3d",
-    "flatten",     "max",          "min",
-    "dropout",     "reshape",
+    "max_pool1d", "max_pool2d",  "max_pool3d",
+    "flatten",    "max",         "min",
+    "dropout",    "reshape",
     "resize_", // Non-inplace resize is deprecated
-    "chunk",       "view",         "transpose",
-    "contiguous",  "permute",      "repeat_interleave",
-    "relu",        "relu_",        "sigmoid",
-    "tanh",        "elu",          "elu_",
-    "hardsigmoid", "hardsigmoid_", "leaky_relu",
-    "leaky_relu_",
+    "chunk",      "view",        "transpose",
+    "contiguous", "permute",     "repeat_interleave",
+    "relu",       "relu_",       "sigmoid",
+    "tanh",       "hardsigmoid", "hardsigmoid_",
+    "leaky_relu", "leaky_relu_",
 };
 
 // Theses are prim::CallFunctions for ops that doesn't require observation and
@@ -107,6 +104,7 @@ std::vector<std::string> _single_input_general_value_call_funcs = {
     "upsample_bilinear",
     "upsample_nearest",
     "hardtanh",
+    "elu",
 };
 
 // Theses are aten functions for ops that doesn't require observation and
@@ -132,6 +130,8 @@ std::vector<std::string> _single_input_general_value_aten_funcs = {
     // "clamp_",  // Enable when quantized `clamp_` is ready
     "hardtanh",
     "hardtanh_",
+    "elu",
+    "elu_",
 };
 
 // Special checks for ops that do not require observers for all input tensors.
