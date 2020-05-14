2dc57c85a089b64985fd254a34c8ae68f7c24c5a
Wanchao Liang <wanchaol.home@gmail.com>
2020-05-14 14:35:59 -0400

    Update on "[jit] register distributed backward"
    This register distributed.autograd.backward to jit
    
    [ghstack-poisoned]

--- torch/jit/_builtins.py
+++ torch/jit/_builtins.py
@@ -127,7 +127,6 @@ def _get_builtin_table():
     import torch.distributed.autograd as dist_autograd
     if dist_autograd.is_available():
         _builtin_ops.append((dist_autograd.get_gradients, "aten::get_gradients"))
-        # _builtin_ops.append((dist_autograd.backward, "aten::dist_backward"))
         _builtin_ops.append((dist_autograd.backward, "aten::backward"))
 
     # populate the _builtin_table from _builtin_ops
