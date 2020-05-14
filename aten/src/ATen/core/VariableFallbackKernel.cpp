e038351e735034ce39d8f3666eb69cb0c8f50156
ljk53 <liujiakai@fb.com>
2020-05-14 10:17:55 -0700

    Update on "[WIP][pytorch] move tracing logic from VariableType to TraceType"
    Differential Revision: [D21570684](https://our.internmc.facebook.com/intern/diff/D21570684/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D21570684/)!
    
    [ghstack-poisoned]

--- aten/src/ATen/core/VariableFallbackKernel.cpp
+++ aten/src/ATen/core/VariableFallbackKernel.cpp
@@ -32,4 +32,8 @@ TORCH_LIBRARY_IMPL(_, Autograd, m) {
   m.fallback(torch::CppFunction::makeFallthrough());
 }
 
+TORCH_LIBRARY_IMPL(_, Tracer, m) {
+  m.fallback(torch::CppFunction::makeFallthrough());
+}
+
 }
--- aten/src/ATen/core/dispatch/DispatchKeyExtractor.h
+++ aten/src/ATen/core/dispatch/DispatchKeyExtractor.h
@@ -15,7 +15,11 @@ namespace impl {
 // Some keys are ALWAYS considered for inclusion by default, so they are
 // included in the set here.  (const appears to be sufficient for
 // always_included to get inlined, constexpr not necessary)
-const DispatchKeySet always_included{DispatchKey::Autograd, DispatchKey::BackendSelect};
+const DispatchKeySet always_included{
+    DispatchKey::Autograd,
+    DispatchKey::BackendSelect,
+    DispatchKey::Tracer
+};
 
 // Take a DispatchKeySet for a Tensor and determine what the actual dispatch
 // DispatchKey should be, taking into account TLS, and skipping backends which
