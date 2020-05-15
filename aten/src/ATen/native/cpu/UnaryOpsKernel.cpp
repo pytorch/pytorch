91c30b2aa653636e50c6b0765004ac083cd0b9aa
Gregory Chanan <gchanan@fb.com>
2020-05-15 14:21:37 -0700

    Update on "Avoid double dispatch in logical_not for compilation speed reasons."
    Also note this turns on "-Wno-unused-local-typedefs" because we are using dispatch macros for error checking.
    
    Differential Revision: [D21598478](https://our.internmc.facebook.com/intern/diff/D21598478)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cpu/UnaryOpsKernel.cpp
+++ aten/src/ATen/native/cpu/UnaryOpsKernel.cpp
@@ -129,12 +129,14 @@ static void frac_kernel(TensorIterator& iter) {
 }
 
 static void logical_not_kernel(TensorIterator& iter) {
-  // error check -- this is just ensuring we don't dispatch on types that aren't in ALL_TYPES_AND2(...)
-  // so we don't have to maintain a separate list or to do double dispatch.
-  AT_DISPATCH_ALL_TYPES_AND2(kBool, kHalf, iter.dtype(0), "logical_not_cpu", [&]() {});
-
+  // NOTE: this implementation differs from the CUDA implementation which only does single dispatch
+  // (to avoid expensive compilation) because CPU kernels don't handle dynamic_casting
+  // (see needs_dynamic_casting).
   AT_DISPATCH_ALL_TYPES_AND2(kBool, kHalf, iter.dtype(1), "logical_not_cpu", [&]() {
-    cpu_kernel(iter, [](scalar_t a) -> bool { return !a; });
+    using self_t = scalar_t;
+    AT_DISPATCH_ALL_TYPES_AND2(kBool, kHalf, iter.dtype(0), "logical_not_cpu", [&]() {
+      cpu_kernel(iter, [](self_t a) -> scalar_t { return static_cast<scalar_t>(!a); });
+    });
   });
 }
 
