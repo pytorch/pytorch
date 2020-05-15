cf574d83059071efb03daed236fb1bcfba907e35
Rohan Varma <rvarm1@fb.com>
2020-05-15 15:49:41 -0700

    Update on "Infinite timeout for operations against ProcessGroup for RPC"
    We don't want to limit a timeout to 30 min since there could be no
    operations within that time frame. Bump the timeout to the maximum allowed by
    std::chrono::milliseconds, which is 2^44 - 1
    
    closes https://github.com/pytorch/pytorch/issues/33107
    closes https://github.com/pytorch/pytorch/issues/38531
    
    Differential Revision: [D21602425](https://our.internmc.facebook.com/intern/diff/D21602425/)
    
    [ghstack-poisoned]

--- torch/distributed/rpc/constants.py
+++ torch/distributed/rpc/constants.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from . import (
     _DEFAULT_RPC_TIMEOUT_SEC,
     _UNSET_RPC_TIMEOUT,
@@ -13,6 +14,6 @@ DEFAULT_INIT_METHOD = _DEFAULT_INIT_METHOD
 DEFAULT_NUM_SEND_RECV_THREADS = _DEFAULT_NUM_SEND_RECV_THREADS
 # Ensure that we don't time out when there are long periods of time without
 # any operations against the underlying ProcessGroup.
-DEFAULT_PROCESS_GROUP_TIMEOUT = (2**44) - 1
+DEFAULT_PROCESS_GROUP_TIMEOUT = timedelta(milliseconds=(2**44) - 1)
 # Value indicating that timeout is not set for RPC call, and the default should be used.
 UNSET_RPC_TIMEOUT = _UNSET_RPC_TIMEOUT
