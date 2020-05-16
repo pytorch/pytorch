8877889e8bcf1efe3a8ab1a21ba16fc854b5b284
krshrimali <kushashwaravishrimali@gmail.com>
2020-05-16 21:36:49 +0530

    Revert back, complex is enabled for acosh

--- test/test_torch.py
+++ test/test_torch.py
@@ -12310,9 +12310,7 @@ class TestTorchDeviceType(TestCase):
                     non_zero_rand((2, 2), dtype=dtype, device=device))
 
     # This function tests that a nan value is returned for input values not in domain
-    @dtypesIfCUDA(torch.half, torch.float, torch.double)
-    @dtypes(torch.float, torch.double)
-    def test_acosh_domain(self, device, dtype):
+    def test_acosh_domain(self, device):
         # Domain of acosh is [1, inf)
         sample = torch.tensor([-1.23, -0.06, 0.98], device=device, dtype=dtype)
         self.assertEqual(torch.isnan(torch.acosh(sample)), torch.BoolTensor([1, 1, 1]))
