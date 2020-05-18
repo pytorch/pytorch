8bc00bd1ec125aedc5279a38ad1870047600ee04
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-05-18 13:39:03 +0530

    address comment
    * update docs.

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -5825,58 +5825,50 @@ add_docstr(torch.fliplr,
            r"""
 fliplr(input) -> Tensor
 
-Flip array in the left/right direction.
+Flip array in the left/right direction, returning a new tensor.
 
 Flip the entries in each row in the left/right direction. 
 Columns are preserved, but appear in a different order than before.
 
+Notes: Equivalent to input[:,::-1]. Requires the array to be at least 2-D.
+
 Args:
-    {input}
+    input (Tensor): Must be at least 2-dimensional.
 
 Example::
 
-    >>> x = torch.arange(8).view(2, 2, 2)
+    >>> x = torch.arange(4).view(2, 2)
     >>> x
-    tensor([[[ 0,  1],
-             [ 2,  3]],
-
-            [[ 4,  5],
-             [ 6,  7]]])
+    tensor([[0, 1],
+            [2, 3]])
     >>> torch.fliplr(x)
-    tensor([[[2, 3],
-             [0, 1]],
-
-            [[6, 7],
-             [4, 5]]])
+    tensor([[1, 0],
+            [3, 2]])
 """.format(**common_args))
 
 add_docstr(torch.flipud,
            r"""
 flipud(input) -> Tensor
 
-Flip array in the up/down direction.
+Flip array in the up/down direction, returning a new tensor.
 
 Flip the entries in each column in the up/down direction.
 Rows are preserved, but appear in a different order than before.
 
+Notes: Equivalent to input[::-1,...]. Requires the array to be at least 1-D.
+
 Args:
-    {input}
+    input (Tensor): Must be at least 1-dimensional.
 
 Example::
 
-    >>> x = torch.arange(8).view(2, 2, 2)
+    >>> x = torch.arange(4).view(2, 2)
     >>> x
-    tensor([[[ 0,  1],
-             [ 2,  3]],
-
-            [[ 4,  5],
-             [ 6,  7]]])
+    tensor([[0, 1],
+            [2, 3]])
     >>> torch.flipud(x)
-    tensor([[[4, 5],
-             [6, 7]],
-
-            [[0, 1],
-             [2, 3]]])
+    tensor([[2, 3],
+            [0, 1]])
 """.format(**common_args))
 
 add_docstr(torch.roll,
