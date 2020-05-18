ed87827d97fdcb5138a9af64511b339581238220
Nikita Shulga <nshulga@fb.com>
2020-05-18 15:59:45 -0700

    Docker constants refactor (#38676)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/38676
    
    Differential Revision: D21629431
    
    Pulled By: malfet
    
    fbshipit-source-id: 17503c80ceb3aa30a45c29742e719226658e5ca5

--- .circleci/cimodel/data/simple/util/docker_constants.py
+++ .circleci/cimodel/data/simple/util/docker_constants.py
@@ -1,21 +1,22 @@
 AWS_DOCKER_HOST = "308535385114.dkr.ecr.us-east-1.amazonaws.com"
 
+DOCKER_IMAGE_TAG = ":9a3986fa-7ce7-4a36-a001-3c9bef9892e2"
 DOCKER_IMAGE_BASIC = "/".join([
     AWS_DOCKER_HOST,
     "pytorch",
-    "pytorch-linux-xenial-py3.6-gcc5.4:9a3986fa-7ce7-4a36-a001-3c9bef9892e2",
+    "pytorch-linux-xenial-py3.6-gcc5.4" + DOCKER_IMAGE_TAG,
 ])
 
 DOCKER_IMAGE_CUDA_10_2 = "/".join([
     AWS_DOCKER_HOST,
     "pytorch",
-    "pytorch-linux-xenial-cuda10.2-cudnn7-py3-gcc7:9a3986fa-7ce7-4a36-a001-3c9bef9892e2",
+    "pytorch-linux-xenial-cuda10.2-cudnn7-py3-gcc7" + DOCKER_IMAGE_TAG,
 ])
 
-DOCKER_IMAGE_BAZEL = "/".join([
+DOCKER_IMAGE_GCC7 = "/".join([
     AWS_DOCKER_HOST,
     "pytorch",
-    "pytorch-linux-xenial-py3.6-gcc7:f990c76a-a798-42bb-852f-5be5006f8026",
+    "pytorch-linux-xenial-py3.6-gcc7" + DOCKER_IMAGE_TAG,
 ])
 
 
@@ -24,7 +25,7 @@ def gen_mobile_docker_name(specifier):
     final_path_part = "".join([
         "pytorch-linux-xenial-py3-clang5-",
         specifier,
-        ":9a3986fa-7ce7-4a36-a001-3c9bef9892e2",
+        DOCKER_IMAGE_TAG,
     ])
 
     parts = [
