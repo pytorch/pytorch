d33de8b9e18b7156c8fd6997913ed6b258ba5937
krshrimali <kushashwaravishrimali@gmail.com>
2020-05-19 10:18:15 +0530

    tanh backward support (cuda) for complex types

--- aten/src/ATen/native/cuda/BinaryMiscBackwardOpsKernels.cu
+++ aten/src/ATen/native/cuda/BinaryMiscBackwardOpsKernels.cu
@@ -20,7 +20,7 @@ void sigmoid_backward_kernel_cuda(TensorIterator& iter) {
 }
 
 void tanh_backward_kernel_cuda(TensorIterator& iter) {
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "tanh_backward_cuda", [&]() {
+  AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(iter.dtype(), "tanh_backward_cuda", [&]() {
     gpu_kernel(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
       return a * (scalar_t(1.) - b * b);
     });
