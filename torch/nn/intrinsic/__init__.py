331987b49ff0dea36a815ec265b59baf5ea32230
Natalia Gimelshein <ngimel@fb.com>
2020-05-19 15:19:36 -0700

    Revert D21632878: [quant] Support for fused ConvBn1d and ConvBnRelu1d modules
    Test Plan: revert-hammer
    
    Differential Revision:
    D21632878
    
    Original commit changeset: 0d73398b95d7
    
    fbshipit-source-id: c4dd18a4220d175237f31f741a782f2596228009

--- torch/nn/intrinsic/__init__.py
+++ torch/nn/intrinsic/__init__.py
@@ -1,8 +1,6 @@
 
-from .modules import ConvBn1d
 from .modules import ConvBn2d
 from .modules import ConvBn3d
-from .modules import ConvBnReLU1d
 from .modules import ConvBnReLU2d
 from .modules import ConvBnReLU3d
 from .modules import ConvReLU1d
@@ -13,11 +11,9 @@ from .modules import BNReLU2d
 from .modules import BNReLU3d
 
 __all__ = [
-    'ConvBn1d',
     'ConvBn2d',
     'ConvBn3d',
     'ConvBnReLU2d',
-    'ConvBnReLU1d',
     'ConvBnReLU3d',
     'ConvReLU1d',
     'ConvReLU2d',
