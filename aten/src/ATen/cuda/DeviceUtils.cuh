80de335c58b128a633fd503cce610ba88fdd2663
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-19 14:42:33 -0700

    try fix

--- aten/src/ATen/cuda/DeviceUtils.cuh
+++ aten/src/ATen/cuda/DeviceUtils.cuh
@@ -58,26 +58,6 @@ __device__ __forceinline__ T WARP_SHFL_UP(T value, unsigned int delta, int width
 #endif
 }
 
-#ifdef __HIP_PLATFORM_HCC__
-template<>
-__device__ __forceinline__ int64_t WARP_SHFL_DOWN<int64_t>(int64_t value, unsigned int delta, int width , unsigned int mask)
-{
-  //(HIP doesn't support int64_t). Trick from https://devblogs.nvidia.com/faster-parallel-reductions-kepler/
-  int2 a = *reinterpret_cast<int2*>(&value);
-  a.x = __shfl_down(a.x, delta);
-  a.y = __shfl_down(a.y, delta);
-  return *reinterpret_cast<int64_t*>(&a);
-}
-template<>
-__device__ __forceinline__ __half WARP_SHFL_DOWN<__half>(__half value, unsigned int delta, int width, unsigned int mask)
-{
-  //(HIP doesn't support c10::Half). Trick from https://devblogs.nvidia.com/faster-parallel-reductions-kepler/
-  short a = *reinterpret_cast<short*>(&value);
-  a = __shfl_down(a, delta);
-  return *reinterpret_cast<__half*>(&a);
-}
-#endif
-
 template <typename T>
 __device__ __forceinline__ T WARP_SHFL_DOWN(T value, unsigned int delta, int width = warpSize, unsigned int mask = 0xffffffff)
 {
@@ -107,3 +87,23 @@ __device__ __forceinline__ c10::complex<T> WARP_SHFL_DOWN(c10::complex<T> value,
         __shfl_down(value.storage[1], delta, width));
 #endif
 }
+
+#ifdef __HIP_PLATFORM_HCC__
+template<>
+__device__ __forceinline__ int64_t WARP_SHFL_DOWN<int64_t>(int64_t value, unsigned int delta, int width , unsigned int mask)
+{
+  //(HIP doesn't support int64_t). Trick from https://devblogs.nvidia.com/faster-parallel-reductions-kepler/
+  int2 a = *reinterpret_cast<int2*>(&value);
+  a.x = __shfl_down(a.x, delta);
+  a.y = __shfl_down(a.y, delta);
+  return *reinterpret_cast<int64_t*>(&a);
+}
+template<>
+__device__ __forceinline__ __half WARP_SHFL_DOWN<__half>(__half value, unsigned int delta, int width, unsigned int mask)
+{
+  //(HIP doesn't support c10::Half). Trick from https://devblogs.nvidia.com/faster-parallel-reductions-kepler/
+  short a = *reinterpret_cast<short*>(&value);
+  a = __shfl_down(a, delta);
+  return *reinterpret_cast<__half*>(&a);
+}
+#endif
