978db48e7c4e61a1a1362c08d9c69d424353cb71
jjsjann123 <jiej@nvidia.com>
2020-05-18 18:43:50 -0700

    Fix memory usage increase reported in #38568 (#38674)
    Summary:
    update to in-place version for bias add in convolution, this saves unnecessary memory allocation.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38674
    
    Differential Revision: D21626080
    
    Pulled By: ngimel
    
    fbshipit-source-id: 4f52a3ae2e5aefae372d8ea5188336216f910da3

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -609,7 +609,7 @@ at::Tensor _convolution(
     auto weight_view = at::_unsafe_view(weight, -1);
     auto out = input*weight_view[0];
     if (bias.defined())
-      out = out + bias[0];
+      out.add_(bias[0]);
     return out.view(o);
   }
 
@@ -639,7 +639,7 @@ at::Tensor _convolution(
             input.contiguous(cudnn_memory_format), weight,
             padding, stride, dilation, params.groups, params.benchmark, params.deterministic);
         if (bias.defined()) {
-          output = output + reshape_bias(input.dim(), bias);
+          output.add_(reshape_bias(input.dim(), bias));
         }
 
       } else if (params.use_miopen(input, bias.defined())){
@@ -662,14 +662,14 @@ at::Tensor _convolution(
           input.contiguous(cudnn_memory_format), weight,
           params.padding, params.output_padding, params.stride, params.dilation, params.groups, params.benchmark, params.deterministic);
       if (bias.defined()) {
-        output = output + reshape_bias(input.dim(), bias);
+        output.add_(reshape_bias(input.dim(), bias));
       }
     } else {
       output = at::cudnn_convolution(
           input.contiguous(cudnn_memory_format), weight,
           params.padding, params.stride, params.dilation, params.groups, params.benchmark, params.deterministic);
       if (bias.defined()) {
-        output = output + reshape_bias(input.dim(), bias);
+        output.add_(reshape_bias(input.dim(), bias));
       }
     }
   } else if (params.use_miopen(input, bias.defined())) {
