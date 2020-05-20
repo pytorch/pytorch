4adb0ddbb9ac8749bb392d943cee3fe3e61ae023
Shawn Zhong <github@shawnzhong.com>
2020-05-20 08:59:15 -0500

    Update Generator.cpp

--- torch/csrc/Generator.cpp
+++ torch/csrc/Generator.cpp
@@ -222,7 +222,7 @@ bool THPGenerator_init(PyObject *module)
   Py_INCREF(&THPGeneratorType);
   PyModule_AddObject(module, "Generator", (PyObject *)&THPGeneratorType);
   auto defaultGenerator = at::detail::getDefaultCPUGenerator();
-  PyModule_AddObject(module, "Generator", (PyObject *)THPGenerator_initDefaultGenerator(defaultGenerator));
+  PyModule_AddObject(module, "default_generator", (PyObject *)THPGenerator_initDefaultGenerator(defaultGenerator));
   return true;
 }
 
