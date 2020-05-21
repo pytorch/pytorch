a668af0cc3dbc53e96d6a78ede5c023c892ac9c2
Alban Desmaison <albandes@fb.com>
2020-05-21 12:22:57 -0400

    fix comments in gradcheck

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -266,7 +266,11 @@ def gradcheck(
                     'This check will likely fail if all the inputs are '
                     'not of double precision floating point or complex. ')
             content = inp._values() if inp.is_sparse else inp
-            if content.layout is not torch._mkldnn and any([s == 0 for s in content.stride()]):
+            # TODO: To cover more problematic cases, replace stride = 0 check with
+            # "any overlap in memory" once we have a proper function to check it.
+            if content.layout is not torch._mkldnn and \
+               content.nelement() > 1 and \
+               any([s == 0 for s in content.stride()]):
                 raise RuntimeError(
                     'The {}th input has a dimension with stride 0. gradcheck only '
                     'supports inputs that are non-overlapping to be able to '
