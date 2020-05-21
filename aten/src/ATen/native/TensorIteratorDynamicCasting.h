4bbae25589e7da9244d097689f9630cb9c0e31db
Gregory Chanan <gchanan@fb.com>
2020-05-21 13:11:11 -0700

    Update on "Add dynamic_cast asserts to CPU Loops."
    On CUDA, we currently support casting loops dynamically (i.e. when the argument or return types of the lamba don't match the dtypes of the TensorIterator).
    On CPU, before this change we would essentially reinterpret_cast, now we internal assert.  We could add dynamic_casting support in the future on CPU.
    
    Differential Revision: [D21670828](https://our.internmc.facebook.com/intern/diff/D21670828)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/TensorIteratorDynamicCasting.h
+++ aten/src/ATen/native/TensorIteratorDynamicCasting.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <complex>
 #include <type_traits>
 #include <c10/core/ScalarType.h>
 #include <c10/util/C++17.h>
 #include <ATen/detail/FunctionTraits.h>
 #include <ATen/native/TensorIterator.h>
+
+#if defined(__CUDACC__) || defined(__HIPCC__)
+#include <thrust/complex.h>
+#endif
+
 
 // This file includes utilties for dynamic_casting done by TensorIterator, see CUDALoops.cuh and Loops.h.
 
@@ -15,6 +21,50 @@
 
 namespace at { namespace native {
 
+namespace {
+
+// Note: the complex (number) handling code is here while we transition to using c10::complex
+// universally; we currently have a mix of kernels that use std::complex, c10::complex, and thrust::complex.
+// CPPTypeAndStdComplexToScalarType is equivalent to CPPTypeToScalarType, but also includes mappings
+// from all the complex types.
+template <typename>
+struct CPPTypeAndStdComplexToScalarType {
+};
+
+#define SPECIALIZE_CPPTypeAndStdComplexToScalarType(cpp_type, scalar_type)                  \
+  template <>                                                                               \
+  struct CPPTypeAndStdComplexToScalarType<cpp_type> {                                       \
+    constexpr static c10::ScalarType value = c10::ScalarType::scalar_type;                  \
+  };
+
+AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(SPECIALIZE_CPPTypeAndStdComplexToScalarType)
+
+#undef SPECIALIZE_CPPTypeeAndStdComplexToScalarType
+
+template<>
+struct CPPTypeAndStdComplexToScalarType<std::complex<float>> {
+  constexpr static c10::ScalarType value = c10::ScalarType::ComplexFloat;
+};
+
+template<>
+struct CPPTypeAndStdComplexToScalarType<std::complex<double>> {
+  constexpr static c10::ScalarType value = c10::ScalarType::ComplexDouble;
+};
+
+#if defined(__CUDACC__) || defined(__HIPCC__)
+template<>
+struct CPPTypeAndStdComplexToScalarType<thrust::complex<float>> {
+  constexpr static c10::ScalarType value = c10::ScalarType::ComplexFloat;
+};
+
+template<>
+struct CPPTypeAndStdComplexToScalarType<thrust::complex<double>> {
+  constexpr static c10::ScalarType value = c10::ScalarType::ComplexDouble;
+};
+#endif
+
+}
+
 // `needs_dynamic_casting` compares the types expected by iterator
 // (i.e. dtypes of the operands) with the actual type of the arguments
 // (and returns) of func_t
@@ -22,7 +72,8 @@ template<typename func_t, int nargs=function_traits<func_t>::arity>
 struct needs_dynamic_casting {
   static bool check(TensorIterator& iter) {
     using traits = function_traits<func_t>;
-    if (iter.input_dtype(nargs-1) != c10::impl::CPPTypeToScalarType<typename traits::template arg<nargs - 1>::type>::value) {
+
+    if (iter.input_dtype(nargs-1) != CPPTypeAndStdComplexToScalarType<typename traits::template arg<nargs - 1>::type>::value) {
       return true;
     }
     return needs_dynamic_casting<func_t, nargs - 1>::check(iter);
@@ -37,8 +88,8 @@ struct needs_dynamic_casting<func_t, 0> {
     // we could assert output numbers are correct here, but checks
     // (including arity) are currently pushed outside of this struct.
     return c10::guts::if_constexpr<std::is_void<typename traits::result_type>::value>(
-      [&]() { return false; },
-      [&]() { return iter.dtype(0) != c10::impl::CPPTypeToScalarType<typename traits::result_type>::value;}
+      [&](auto _) { return false; },
+      [&](auto _) { return iter.dtype(0) != _(CPPTypeAndStdComplexToScalarType<typename traits::result_type>::value);}
     );
   }
 };
