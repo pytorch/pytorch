36f62f3ff06914124c1b172995f4fe41ed2736ac
Donna Choi <choidong@amazon.com>
2020-05-22 12:52:10 -0700

    Add not drop_last condition

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -313,7 +313,7 @@ class DataLoader(object):
             # `DataLoader`, save the returned value in `self._len_called`, and warn
             # if the iterator ends up yielding more than this number of samples.
             length = self._IterableDataset_len_called = len(self.dataset)
-            if self.batch_size is not None:
+            if self.batch_size is not None and not self.drop_last:
                 length = length // self.batch_size
             return length
         else:
