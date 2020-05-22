5d98c34ab9412eee055b4ea73be343990e5fd8e8
Donna Choi <choidong@amazon.com>
2020-05-22 09:56:39 -0700

    Add if condition

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -313,7 +313,9 @@ class DataLoader(object):
             # `DataLoader`, save the returned value in `self._len_called`, and warn
             # if the iterator ends up yielding more than this number of samples.
             length = self._IterableDataset_len_called = len(self.dataset)
-            return length // self.batch_size
+            if self.batch_size is not None:
+                length = length // self.batch_size
+            return length
         else:
             return len(self._index_sampler)
 
