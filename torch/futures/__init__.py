0ba4b30c73bf06fc1093aa652eada1b79db8cab2
Shen Li <cs.shenli@gmail.com>
2020-05-26 09:08:37 -0700

    Update on "Expose torch.futures.Future"
    This commit adds a `torch.futures.Future` type and exposes its ctor,
    `wait`, `then`, and `set_result` APIs. This type is currently a
    wrapper of `c10::ivalue::Future` and mainly used by RPC for now. Later,
    we could revamp c10d APIs to return this `Future` type as well. More
    utils will be added into `torch.futures` package in followup PRs.
    
    Differential Revision: [D21723022](https://our.internmc.facebook.com/intern/diff/D21723022)
    
    [ghstack-poisoned]

--- torch/futures/__init__.py
+++ torch/futures/__init__.py
@@ -24,7 +24,11 @@ class Future(torch._C.Future):
     def then(self, callback):
         r"""
         Append the given callback function to this ``Future``, which will be run
-        in order when the ``Future`` is completed.
+        when the ``Future`` is completed.  Multiple callbacks can be added to
+        the same ``Future``, and will be invoked in the same order as they were
+        added. The callback must take one argument, which is the reference to
+        this ``Future``. The callback function can use the ``Future.wait()`` API
+        to get the value.
 
         Argument:
             callback(``Callable``): a ``Callable`` that takes this ``Future`` as
@@ -34,11 +38,48 @@ class Future(torch._C.Future):
             A new ``Future`` object that holds the return value of the
             ``callback`` and will be marked as completed when the given
             ``callback`` finishes.
+
+        Example::
+            >>> import torch
+            >>>
+            >>> def callback(fut):
+            >>>     print(f"RPC return value is {fut.wait()}.")
+            >>>
+            >>> fut = torch.futures.Future()
+            >>> # The inserted callback will print the return value when
+            >>> # receiving the response from "worker1"
+            >>> cb_fut = fut.then(callback)
+            >>> chain_cb_fut = cb_fut.then(lambda x : print(f"Chained cb done. {x.wait()}"))
+            >>> fut.set_result(5)
+            >>>
+            >>> # Outputs are:
+            >>> # RPC return value is 5.
+            >>> # Chained cb done. None
         """
         return super(Future, self).then(callback)
 
     def set_result(self, result):
         r"""
-        Mark this future as completed using the provided result object.
+        Set the result for this ``Future``, which will mark this ``Future`` as
+        completed and trigger all attached callbacks. Note that a ``Future``
+        cannot be marked completed twice.
+
+        Arguments:
+            result (object): the result object of this ``Future``.
+
+        Example::
+            >>> from torch.distributed import rpc
+            >>> import torch
+            >>>
+            >>> fut = rpc.Future()
+            >>> rpc_fut = rpc.async(
+            >>>     "worker1",
+            >>>     torch.add,
+            >>>     args=(torch.ones(2), 1)
+            >>> )
+            >>> rpc_fut.add_done_callback(
+            >>>     lambda rpc_fut : fut.set_result(rpc_fut.wait() + 1)
+            >>> )
+            >>> print(fut.wait())  # tensor([3., 3.])
         """
         super(Future, self).set_result(result)
