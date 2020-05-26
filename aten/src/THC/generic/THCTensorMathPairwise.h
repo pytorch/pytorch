9e5fcc4d1a9a75a118a072d0a3652b0f8628687a
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-26 13:46:38 -0700

    Revert "clean up more"
    This reverts commit de1b9014aea790d5113d574af3e8508bcdfb57d6.

--- aten/src/THC/generic/THCTensorMathPairwise.h
+++ aten/src/THC/generic/THCTensorMathPairwise.h
@@ -2,6 +2,8 @@
 #define THC_GENERIC_FILE "THC/generic/THCTensorMathPairwise.h"
 #else
 
+THC_API int THCTensor_(equal)(THCState *state, THCTensor *self, THCTensor *src);
+
 #if !defined(THC_REAL_IS_BOOL)
 
 THC_API void THCTensor_(mul)(THCState *state, THCTensor *self, THCTensor *src, scalar_t value);
