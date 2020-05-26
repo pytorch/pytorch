2e6ee853abe8b3cab2b5b603ff174159f0da2b46
mattip <matti.picus@gmail.com>
2020-05-26 16:09:07 -0700

    make onnx expect tests resiliant to producer_version changes (#39002)
    Summary:
    closes gh-32561 closes gh-38545. As part of the fallout from gh-36797, this PR
    - replaces the producer_version: "1.6" in onnx expect tests with `producer_version: "XXX"
    - adapts `testing/_internal/common_utils.py` with a regex to change the onnx producer_version so tests still pass
    
    The consistency of the torch version and the onnx `producer_version` is tested in gh-36797, so there is no reason to test it again in the expect tests.
    
    xref gh-38629 which documented how to run the onnx tests and at the same time refactored the Community documentation.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39002
    
    Differential Revision: D21723062
    
    Pulled By: ezyang
    
    fbshipit-source-id: 1bd6a8ed37d5383e69d017226dc09c0645a69aff

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1230,6 +1230,11 @@ class TestCase(expecttest.TestCase):
             expected = re.sub(r'CppOp\[(.+?)\]', 'CppOp[]', expected)
             s = re.sub(r'CppOp\[(.+?)\]', 'CppOp[]', s)
 
+        # Adjust for producer_version
+        expected = expected.replace(
+            'producer_version: "XXX"',
+            'producer_version: "{}"'.format(torch.onnx.producer_version)
+        )
         if expecttest.ACCEPT:
             if expected != s:
                 return accept_output("updated output")
