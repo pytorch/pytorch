b0190806ef1735304c39ecd0b081145a2af74d59
leon yeu <me@ulenium.com>
2020-05-26 14:52:42 +0800

    fix #38032 to allow specifying num_samples to RandomSampler when replacement is False

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -72,8 +72,7 @@ class RandomSampler(Sampler):
     Arguments:
         data_source (Dataset): dataset to sample from
         replacement (bool): samples are drawn with replacement if ``True``, default=``False``
-        num_samples (int): number of samples to draw, default=`len(dataset)`. This argument
-            is supposed to be specified only when `replacement` is ``True``.
+        num_samples (int): number of samples to draw, default=`len(dataset)`
     """
 
     def __init__(self, data_source, replacement=False, num_samples=None):
@@ -84,11 +83,8 @@ class RandomSampler(Sampler):
         if not isinstance(self.replacement, bool):
             raise ValueError("replacement should be a boolean value, but got "
                              "replacement={}".format(self.replacement))
-
-        if self._num_samples is not None and not replacement:
-            raise ValueError("With replacement=False, num_samples should not be specified, "
-                             "since a random permute will be performed.")
-
+        if self._num_samples is None or not isinstance(self._num_samples, int) or self._num_samples > len(self.data_source):
+            self._num_samples = len(self.data_source)
         if not isinstance(self.num_samples, int) or self.num_samples <= 0:
             raise ValueError("num_samples should be a positive integer "
                              "value, but got num_samples={}".format(self.num_samples))
@@ -104,7 +100,7 @@ class RandomSampler(Sampler):
         n = len(self.data_source)
         if self.replacement:
             return iter(torch.randint(high=n, size=(self.num_samples,), dtype=torch.int64).tolist())
-        return iter(torch.randperm(n).tolist())
+        return iter(torch.randperm(n).tolist()[:self._num_samples])
 
     def __len__(self):
         return self.num_samples
