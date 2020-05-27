775d24440e92f699e7a1e0a3cf4f9018247bb218
Gregory Chanan <gchanan@fb.com>
2020-05-27 07:36:06 -0700

    Move needs_dynamic_casting to a non-CUDA specific file. (#38813)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38813
    
    We are going to apply this check to CPU (with some changes), so just moving this in preparation.
    
    The code is just cut-pasted here, no behavioral change.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D21670554
    
    Pulled By: gchanan
    
    fbshipit-source-id: c7e07f67bb4c6524fde12237e35892e42557103e

---
+++ aten/src/ATen/native/TensorIteratorDynamicCasting.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <c10/core/ScalarType.h>
+#include <ATen/detail/FunctionTraits.h>
+#include <ATen/native/TensorIterator.h>
+
+// This file includes utilties for dynamic_casting done by TensorIterator, see CUDALoops.cuh and Loops.h.
+
+// dynamic_casting handles when the types expected by the iterator do not match the types of the arguments
+// to the function that is being called.
+// On CUDA, the cast is currently pushed down into the kernel (for performance reasons).
+// On CPU, there is currently an internal assert that a dynamic_cast is not needed.
+
+namespace at { namespace native {
+
+// `needs_dynamic_casting` compares the types expected by iterator
+// (i.e. dtypes of the operands) with the actual type of the arguments
+// of func_t
+template<typename func_t, int nargs=function_traits<func_t>::arity>
+struct needs_dynamic_casting {
+  static bool check(TensorIterator& iter) {
+    using traits = function_traits<func_t>;
+    if (iter.dtype(nargs) != c10::impl::CPPTypeToScalarType<typename traits::template arg<nargs - 1>::type>::value) {
+      return true;
+    }
+    return needs_dynamic_casting<func_t, nargs - 1>::check(iter);
+  }
+};
+
+template<typename func_t>
+struct needs_dynamic_casting<func_t, 0> {
+  static bool check(TensorIterator& iter) {
+    using traits = function_traits<func_t>;
+    return iter.dtype(0) != c10::impl::CPPTypeToScalarType<typename traits::result_type>::value;
+  }
+};
+
+}} //namespace at::native
