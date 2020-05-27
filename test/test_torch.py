7cdf77b0e0b24062f9b61fa69d370553f81fa58a
Hong Xu <hong@topbug.net>
2020-05-27 14:49:18 -0700

    Renable memory format test for some unary functions
    Many of them have already been migrated to ATen

--- test/test_torch.py
+++ test/test_torch.py
@@ -14145,19 +14145,19 @@ class TestTorchDeviceType(TestCase):
             x.is_contiguous(memory_format=torch.channels_last_3d), x_rep.is_contiguous(memory_format=torch.channels_last_3d))
 
     def test_memory_format_operators(self, device):
-        def chunk_op(x, y):
+        def _chunk_op(x, y):
             x1, x2 = x.chunk(2, dim=1)
             y1, y2 = x.chunk(2, dim=1)
             y1 = y1.contiguous()
             return y1 + x1
 
-        def unsqueeze_op_add(x, y):
+        def _unsqueeze_op_add(x, y):
             return x[0].unsqueeze(0) + 3
 
-        def unsqueeze_op_clone(x, y):
+        def _unsqueeze_op_clone(x, y):
             return x[0].unsqueeze(0).clone()
 
-        def test_helper(x, y, memory_format):
+        def _test_helper(x, y, memory_format):
             fns = [
                 lambda x, y: x.clone(),
                 lambda x, y: x + 3,
@@ -14186,26 +14186,26 @@ class TestTorchDeviceType(TestCase):
                 lambda x, y: x.atan2_(y),
                 lambda x, y: x.ceil(),
                 lambda x, y: x.ceil_(),
-                # lambda x, y: x.clamp(-1, 1), # https://github.com/pytorch/pytorch/issues/24544
-                # lambda x, y: x.cos(), # https://github.com/pytorch/pytorch/issues/24545
-                # lambda x, y: x.cosh(), # https://github.com/pytorch/pytorch/issues/24546
+                lambda x, y: x.clamp(-1, 1),
+                lambda x, y: x.cos(),
+                lambda x, y: x.cosh(),
                 lambda x, y: x.div(0.5),
                 lambda x, y: x.div_(0.5),
                 lambda x, y: x.div(y),
                 lambda x, y: x.div_(y),
                 lambda x, y: x.digamma(),
                 lambda x, y: x.digamma_(),
-                # lambda x, y: x.erf(), # https://github.com/pytorch/pytorch/issues/24558
+                lambda x, y: x.erf(),
                 lambda x, y: x.erfc(),
                 lambda x, y: x.erfinv(),
                 lambda x, y: x.erfinv_(),
-                # lambda x, y: x.exp(), # https://github.com/pytorch/pytorch/issues/24561
+                lambda x, y: x.exp(),
                 lambda x, y: x.expm1(),
                 lambda x, y: x.expm1_(),
                 lambda x, y: x.floor(),
                 lambda x, y: x.floor_(),
                 # lambda x, y: x.fmod(2), # https://github.com/pytorch/pytorch/issues/24565
-                # lambda x, y: x.frac(), # https://github.com/pytorch/pytorch/issues/24566
+                lambda x, y: x.frac(),
                 # lambda x, y: x.lerp(y, 0.5), #  Need to update Lerp.cu with TensorIterator
                 lambda x, y: x.log(),
                 lambda x, y: x.log_(),
@@ -14221,10 +14221,10 @@ class TestTorchDeviceType(TestCase):
                 lambda x, y: x.neg_(),
                 lambda x, y: x.pow(3),
                 lambda x, y: x.pow_(3),
-                # lambda x, y: x.pow(0.0), # Need to make resize_as_ memory format aware
-                # lambda x, y: x.pow(1.0), # Need to make resize_as_ memory format aware
-                # lambda x, y: x.reciprocal(), # Not migrated for CUDA
-                # lambda x, y: x.remainder(2),  # https://github.com/pytorch/pytorch/issues/24615
+                lambda x, y: x.pow(0.0),
+                lambda x, y: x.pow(1.0),
+                lambda x, y: x.reciprocal(),
+                lambda x, y: x.remainder(2),
                 lambda x, y: x.round(),
                 lambda x, y: x.round_(),
                 lambda x, y: x.rsqrt(),
@@ -14239,13 +14239,13 @@ class TestTorchDeviceType(TestCase):
                 lambda x, y: x.sinh_(),
                 lambda x, y: x.sqrt(),
                 lambda x, y: x.sqrt_(),
-                # lambda x, y: x.tan(), # https://github.com/pytorch/pytorch/issues/24641
-                # lambda x, y: x.tanh(), # https://github.com/pytorch/pytorch/issues/24642
+                lambda x, y: x.tan(),
+                lambda x, y: x.tanh(),
                 lambda x, y: x.trunc(),
                 lambda x, y: x.trunc_(),
-                chunk_op,
-                unsqueeze_op_add,
-                # unsqueeze_op_clone,
+                _chunk_op,
+                _unsqueeze_op_add,
+                _unsqueeze_op_clone,
             ]
             for fn in fns:
                 x_c = x.contiguous()
@@ -14257,11 +14257,11 @@ class TestTorchDeviceType(TestCase):
                     result.is_contiguous(memory_format=memory_format),
                     "result of the '{}' is not in '{}' format".format(inspect.getsource(fn).strip(), memory_format))
 
-        test_helper(
+        _test_helper(
             torch.randn((4, 3, 8, 8), device=device).contiguous(memory_format=torch.channels_last),
             abs(torch.randn((4, 3, 8, 8), device=device)) + 1,
             torch.channels_last)
-        test_helper(
+        _test_helper(
             torch.randn((4, 3, 8, 8, 8), device=device).contiguous(memory_format=torch.channels_last_3d),
             abs(torch.randn((4, 3, 8, 8, 8), device=device)) + 1,
             torch.channels_last_3d)
