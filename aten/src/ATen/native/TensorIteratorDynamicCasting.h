d627f2b174136b720dcc99380e2b98f535d1f169
Gregory Chanan <gchanan@fb.com>
2020-05-27 07:36:06 -0700

    Support void return type in TensorIteratorDynamicCasting checks. (#38815)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38815
    
    Some CPU kernels have void return types and the currently implementation segfaults on these cases.
    
    Test Plan: Imported from OSS
    
    Differential Revision: D21670717
    
    Pulled By: gchanan
    
    fbshipit-source-id: bc17b8330195601ca231a985ee44319447ba6cf0

--- aten/src/ATen/native/TensorIteratorDynamicCasting.h
+++ aten/src/ATen/native/TensorIteratorDynamicCasting.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <type_traits>
 #include <c10/core/ScalarType.h>
+#include <c10/util/C++17.h>
 #include <ATen/detail/FunctionTraits.h>
 #include <ATen/native/TensorIterator.h>
 
@@ -15,12 +17,12 @@ namespace at { namespace native {
 
 // `needs_dynamic_casting` compares the types expected by iterator
 // (i.e. dtypes of the operands) with the actual type of the arguments
-// of func_t
+// (and returns) of func_t
 template<typename func_t, int nargs=function_traits<func_t>::arity>
 struct needs_dynamic_casting {
   static bool check(TensorIterator& iter) {
     using traits = function_traits<func_t>;
-    if (iter.dtype(nargs) != c10::impl::CPPTypeToScalarType<typename traits::template arg<nargs - 1>::type>::value) {
+    if (iter.input_dtype(nargs-1) != c10::impl::CPPTypeToScalarType<typename traits::template arg<nargs - 1>::type>::value) {
       return true;
     }
     return needs_dynamic_casting<func_t, nargs - 1>::check(iter);
@@ -31,7 +33,13 @@ template<typename func_t>
 struct needs_dynamic_casting<func_t, 0> {
   static bool check(TensorIterator& iter) {
     using traits = function_traits<func_t>;
-    return iter.dtype(0) != c10::impl::CPPTypeToScalarType<typename traits::result_type>::value;
+
+    // we could assert output numbers are correct here, but checks
+    // (including arity) are currently pushed outside of this struct.
+    return c10::guts::if_constexpr<std::is_void<typename traits::result_type>::value>(
+      [&]() { return false; },
+      [&]() { return iter.dtype(0) != c10::impl::CPPTypeToScalarType<typename traits::result_type>::value;}
+    );
   }
 };
 
