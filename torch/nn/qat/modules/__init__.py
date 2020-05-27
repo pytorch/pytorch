b7daee898f19c9cb10fb80bf3999be06ea29cc96
vasiliy <vasiliy@fb.com>
2020-05-27 14:25:53 -0700

    instancenorm: eager mode QAT support
    Summary:
    
    Adds eager mode QAT support for instancenorm
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestQuantizationAwareTraining.test_normalization
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/nn/qat/modules/__init__.py
+++ torch/nn/qat/modules/__init__.py
@@ -2,11 +2,15 @@
 from .linear import Linear
 from .conv import Conv2d
 from .activations import Hardswish
-from .normalization import GroupNorm
+from .normalization import GroupNorm, InstanceNorm1d, InstanceNorm2d, \
+    InstanceNorm3d
 
 __all__ = [
     'Linear',
     'Conv2d',
     'Hardswish',
     'GroupNorm',
+    'InstanceNorm1d',
+    'InstanceNorm2d',
+    'InstanceNorm3d',
 ]
