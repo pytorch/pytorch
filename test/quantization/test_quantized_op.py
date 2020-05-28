33a0d0232b9d561062f3547e6c797052736ce0f2
durumu <jpgraham@fb.com>
2020-05-28 14:17:11 -0700

    Update on "Dummy operator building correctly"
    Differential Revision: [D21771202](https://our.internmc.facebook.com/intern/diff/D21771202)
    
    [ghstack-poisoned]

--- test/quantization/test_quantized_op.py
+++ test/quantization/test_quantized_op.py
@@ -240,11 +240,36 @@ class TestQuantizedOps(TestCase):
 
     """Tests the correctness of the quantized::celu op."""
     @given(X=hu.tensor(shapes=hu.array_shapes(1, 5, 1, 5),
-                       elements=hu.floats(-1e3, 1e3, allow_nan=False, allow_infinity=False),
-                       qparams=hu.qparams()),
-           alpha=st.floats(0.01, 10.0, allow_nan=False, allow_infinity=False))
+                       elements=hu.floats(-1e2, 1e2, allow_nan=False, allow_infinity=False),
+                       qparams=hu.qparams(scale_max=9.999999747378752e-06)),
+           alpha=st.floats(0.01, 100.0, allow_nan=False, allow_infinity=False))
     def test_qcelu(self, X, alpha):
-        pass  # TODO: add some real tests
+        X, (scale, zero_point, torch_type) = X
+
+        X = torch.from_numpy(X)
+        qX = torch.quantize_per_tensor(X, scale=scale, zero_point=zero_point,
+                                       dtype=torch_type)
+        op = torch.nn.quantized.functional.celu
+
+        # calculate ELU(dqX) and quantize
+        dqX = qX.dequantize()
+        dqY_hat = dqX.clone()
+        dqY_hat[dqX < 0] = alpha * (torch.exp(dqY_hat[dqX < 0] / alpha) - 1.)
+        qY_hat = torch.quantize_per_tensor(dqY_hat, scale=scale, zero_point=zero_point,
+                                           dtype=torch_type)
+
+        # test regular
+        qY = op(qX, alpha=alpha)
+        self.assertEqual(qY, qY_hat,
+                         msg="F.celu failed ({} vs {})".format(qY, qY_hat))
+
+
+        # test inplace
+        qXcopy = qX.clone()
+        op(qXcopy, alpha=alpha, inplace=True)
+        self.assertEqual(qXcopy, qY_hat,
+                         msg="F.celu_ failed ({} vs {})".format(qXcopy, qY_hat))
+
 
     """Tests the correctness of the quantized::qnnpack_sigmoid op."""
     @given(X=hu.tensor(shapes=hu.array_shapes(1, 5, 1, 5),
