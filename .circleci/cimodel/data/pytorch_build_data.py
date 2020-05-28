89bac2de7d76e7bdcca812eaa8d383fcc79a1523
Rohan Varma <rvarm1@fb.com>
2020-05-27 17:39:14 -0700

    Revert D21752017: [pytorch][PR] Test PyTorch using python-3.8 + GCC-9 on Bionic
    Test Plan: revert-hammer
    
    Differential Revision:
    D21752017
    
    Original commit changeset: 56c841636349
    
    fbshipit-source-id: adf08e03ba9610050fc5440ef453789f805fdc6b

--- .circleci/cimodel/data/pytorch_build_data.py
+++ .circleci/cimodel/data/pytorch_build_data.py
@@ -55,7 +55,11 @@ CONFIG_TREE_DATA = [
             ]),
         ]),
         ("gcc", [
-            ("9", [XImportant("3.8")]),
+            ("9", [
+                ("3.8", [
+                    ("build_only", [XImportant(True)]),
+                ]),
+            ]),
         ]),
     ]),
 ]
