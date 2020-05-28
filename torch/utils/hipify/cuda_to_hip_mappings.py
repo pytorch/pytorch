5d01f87e581fd977d42cfd44ccacd832bfd8e4e0
Natalia Gimelshein <ngimel@fb.com>
2020-05-28 15:41:32 -0700

    fix asserts in cuda code (#39047)
    Summary:
    Gets rid of some in-kernel asserts where they can be replaced with static_asserts
    Replaces bare in-kernel `assert` in one case with `CUDA_KERNEL_ASSERT` where necessary
    replaces host code `assert`s with `TORCH_INTERNAL_ASSERT`
    Another group of asserts is in fractional max pooling kernels which should be fixed regardless https://github.com/pytorch/pytorch/issues/39044, the problems there are not just asserts.
    I've audited remaining cases of in-kernel asserts, and they are more like `TORCH_INTERNAL_ASSERT`, so they should not happen with invalid user data. I think it's ok to leave them as is.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39047
    
    Differential Revision: D21750392
    
    Pulled By: ngimel
    
    fbshipit-source-id: e9417523a2c672284de3515933cb7ed166e56719

--- torch/utils/hipify/cuda_to_hip_mappings.py
+++ torch/utils/hipify/cuda_to_hip_mappings.py
@@ -8010,6 +8010,9 @@ CAFFE2_SPECIFIC_MAPPINGS = collections.OrderedDict(
         ("curandGenerateUniform", ("hiprandGenerateUniform", API_CAFFE2)),
         ("curand_generator", ("hiprand_generator", API_CAFFE2)),
         ("CaffeCudaGetDevice", ("CaffeHipGetDevice", API_CAFFE2)),
+        # do not rename CUDA_KERNEL_ASSERT,
+        # the ordered dict guarantees this pattern will match first, before "CUDA"
+        ("CUDA_KERNEL_ASSERT", ("CUDA_KERNEL_ASSERT", API_CAFFE2)),
         ("CUDA", ("HIP", API_CAFFE2)),
         ("Cuda", ("Hip", API_CAFFE2)),
         ("cuda_", ("hip_", API_CAFFE2)),
