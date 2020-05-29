1d05da6247898345dca205608fc5c81cd1fabb1e
fnst-yule <yule@cn.fujitsu.com>
2020-05-29 09:28:17 +0800

    Allow specifying num_samples to RandomSampler in any case(#38032)
    Summary
    Modify iterator of RandomSampler to return a valid value
    while the parameter "replacement" is specified as false.

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -85,10 +85,6 @@ class RandomSampler(Sampler):
             raise ValueError("replacement should be a boolean value, but got "
                              "replacement={}".format(self.replacement))
 
-        if self._num_samples is not None and not replacement:
-            raise ValueError("With replacement=False, num_samples should not be specified, "
-                             "since a random permute will be performed.")
-
         if not isinstance(self.num_samples, int) or self.num_samples <= 0:
             raise ValueError("num_samples should be a positive integer "
                              "value, but got num_samples={}".format(self.num_samples))
@@ -96,15 +92,14 @@ class RandomSampler(Sampler):
     @property
     def num_samples(self):
         # dataset size might change at runtime
-        if self._num_samples is None:
-            return len(self.data_source)
-        return self._num_samples
+        n = len(self.data_source)
+        return n if self._num_samples is None or self._num_samples > n else self._num_samples
 
     def __iter__(self):
         n = len(self.data_source)
         if self.replacement:
             return iter(torch.randint(high=n, size=(self.num_samples,), dtype=torch.int64).tolist())
-        return iter(torch.randperm(n).tolist())
+        return iter(torch.randperm(n).tolist()[:self.num_samples])
 
     def __len__(self):
         return self.num_samples
