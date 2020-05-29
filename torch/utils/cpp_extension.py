c7f8c6f412e547ab1369c1634ab14b90b2f688cd
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-29 15:18:10 -0700

    Initial support for building on Ampere GPU, CUDA 11, cuDNN 8
    This PR contains initial changes that makes PyTorch build with Ampere GPU, CUDA 11, and cuDNN 8.
    TF32 related features will not be included in this PR.
    
    [ghstack-poisoned]

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1373,7 +1373,7 @@ def _get_cuda_arch_flags(cflags=None):
     ])
 
     supported_arches = ['3.5', '3.7', '5.0', '5.2', '5.3', '6.0', '6.1', '6.2',
-                        '7.0', '7.2', '7.5']
+                        '7.0', '7.2', '7.5', '8.0']
     valid_arch_strings = supported_arches + [s + "+PTX" for s in supported_arches]
 
     # The default is sm_30 for CUDA 9.x and 10.x
