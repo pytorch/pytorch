ef47098b4483806558da3da5a3a1102854068479
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-05-30 01:51:54 -0700

    Revert "Migrate pow kernel to c10::complex"
    This reverts commit 162657acd6ee79bcba71cf2f60309e7fccaaeeb4.

--- aten/src/ATen/Dispatch.h
+++ aten/src/ATen/Dispatch.h
@@ -329,21 +329,6 @@ inline void deprecated_AT_DISPATCH_ALL_TYPES_AND_HALF_AND_COMPLEX() {}
     }                                                                       \
   }()
 
-#define _AT_DISPATCH_C10_COMPLEX_TYPES(TYPE, NAME, ...)                     \
-  [&] {                                                                     \
-    const auto& the_type = TYPE;                                            \
-    /* don't use TYPE again in case it is an expensive or side-effect op */ \
-    at::ScalarType _st = ::detail::scalar_type(the_type);                   \
-    switch (_st) {                                                          \
-      AT_PRIVATE_CASE_TYPE(                                                 \
-          at::ScalarType::ComplexFloat, std::complex<float>, __VA_ARGS__)   \
-      AT_PRIVATE_CASE_TYPE(                                                 \
-          at::ScalarType::ComplexDouble, std::complex<double>, __VA_ARGS__) \
-      default:                                                              \
-        AT_ERROR(#NAME, " not implemented for '", toString(_st), "'");      \
-    }                                                                       \
-  }()
-
 #define AT_DISPATCH_QINT_TYPES(TYPE, NAME, ...)                             \
   [&] {                                                                     \
     const auto& the_type = TYPE;                                            \
