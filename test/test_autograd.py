c52c7741dc2dfccf915b3ac149eef93a6c42dcd5
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-06-02 13:00:01 -0700

    support flip and rot90 for complex dtype (#37826)
    Summary:
    Closes https://github.com/pytorch/pytorch/issues/37698
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37826
    
    Differential Revision: D21657697
    
    Pulled By: mruberry
    
    fbshipit-source-id: 16a3899d5de280da692a52bd0ce85d5ebe14cc31

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -4326,7 +4326,7 @@ complex_list = ['t', 'view', 'reshape', 'reshape_as', 'view_as', 'zero_', 'clone
                 'tril', 'triu', 'fill_', 'eq_', 'ne_', 'permute', 'squeeze', 'unsqueeze',
                 'chunk', 'split', 'split_with_sizes', 'resize', 'resize_as', 'sin', 'cos',
                 '__rmul__', '__rdiv__', 'sum', 'transpose', 'round', 'add', 'roll',
-                '__radd__', 'repeat', 'expand', 'mul', 'tanh'] + separate_complex_tests
+                '__radd__', 'repeat', 'expand', 'mul', 'tanh', 'flip', 'rot90'] + separate_complex_tests
 
 def add_test(
         name,
