c691056094e972debebe51894288dedee2d809b9
Edward Z. Yang <ezyang@fb.com>
2020-06-03 07:08:09 -0700

    Update on "Un-inline Functions.h into Functions.cpp"
    In my unscientific testing, this reduces startup time by 50% on gcc 8.3.
    That's a big fucking deal.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- aten/src/ATen/templates/Functions.cpp
+++ aten/src/ATen/templates/Functions.cpp
@@ -2,18 +2,7 @@
 
 // ${generated_comment}
 
-#include <c10/core/Scalar.h>
-#include <ATen/Tensor.h>
-#include <c10/core/Storage.h>
-#include <ATen/core/Generator.h>
-#include <c10/util/Deprecated.h>
-#include <ATen/NativeFunctions.h>
-#include <ATen/DeviceGuard.h>
-#include <c10/core/TensorOptions.h>
-#include <ATen/core/Reduction.h>
-#include <c10/util/Optional.h>
-#include <ATen/TensorUtils.h>
-#include <ATen/Context.h>
+#include <ATen/Functions.h>
 
 #include <ATen/core/dispatch/Dispatcher.h>
 #include <ATen/TypeDefault.h>
