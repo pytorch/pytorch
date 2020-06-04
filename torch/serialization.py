d5ac90a8e15272cfdb8a7c8da24f473106443686
KushajveerSingh <kushajreal@gmail.com>
2020-06-05 00:51:55 +0530

    remove white spaces

--- torch/serialization.py
+++ torch/serialization.py
@@ -338,10 +338,10 @@ def save(obj, f, pickle_module=pickle, pickle_protocol=DEFAULT_PROTOCOL, _use_ne
            containing a file name
         pickle_module: module used for pickling metadata and objects
         pickle_protocol: can be specified to override the default protocol
-    
-    Note:
+
+    .. note::
         A common PyTorch convention is to save tensors using .pt file extension.
-    
+
     .. warning::
         If you are using Python 2, :func:`torch.save` does NOT support :class:`StringIO.StringIO`
         as a valid file-like object. This is because the write method should return
