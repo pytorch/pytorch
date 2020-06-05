96facfe8864b63ce19edc9c342c5731ed6f449ce
Sebastian Messmer <messmer@fb.com>
2020-06-05 16:40:12 -0700

    Unify TensorOptions signatures
    A few ops have been taking non-optional ScalarType, Device and Layout. That isn't supported by the hacky wrapper that makes those
    kernels work with the c10 operator library. This PR unifies the signatures and makes those ops c10-full.
    
    Differential Revision: [D21915788](https://our.internmc.facebook.com/intern/diff/D21915788/)
    
    [ghstack-poisoned]

--- aten/src/ATen/templates/TensorMethods.h
+++ aten/src/ATen/templates/TensorMethods.h
@@ -10,6 +10,7 @@
 #include <ATen/core/dispatch/Dispatcher.h>
 #include <ATen/core/NamedTensor.h>
 #include <ATen/core/LegacyTypeDispatch.h>
+#include <ATen/core/op_registration/hacky_wrapper_for_legacy_signatures.h>
 
 #ifdef USE_STATIC_DISPATCH
 #include <ATen/TypeDefault.h>
