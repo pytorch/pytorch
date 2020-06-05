e72fb8c1a13c074cff5a08a26ac81a869b4bf817
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-06-05 07:56:50 -0700

    add comment for the pass

--- torch/csrc/jit/passes/onnx/eliminate_unused_items.h
+++ torch/csrc/jit/passes/onnx/eliminate_unused_items.h
@@ -5,6 +5,9 @@
 namespace torch {
 namespace jit {
 
+// EliminateUnusedItemsONNX pass is removing unused
+// initializers and inputs, this is needed because
+// dce pass is only removing unused fork inputs
 void EliminateUnusedItemsONNX(
     Block* b,
     std::map<std::string, IValue>& paramDict);
