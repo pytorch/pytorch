73d740126abd20ecfb6748a7d96b43f007fc5af6
Christian Sarofeen <csarofeen@nvidia.com>
2020-06-05 09:59:27 -0400

    Minor cleanup.

--- torch/csrc/jit/codegen/cuda/ir_graphviz.cpp
+++ torch/csrc/jit/codegen/cuda/ir_graphviz.cpp
@@ -90,13 +90,11 @@ class IrNodeLabel : private OptInConstDispatch {
   }
 
   void handle(const Split* split) override {
-    label_ << "Split(IterDomain=" << split->in()
-           << ", factor=" << IrNodeLabel::gen(split->factor()) << ")";
+    label_ << "Split( factor=" << IrNodeLabel::gen(split->factor()) << ")";
   }
 
   void handle(const Merge* merge) override {
-    label_ << "Merge(IterDomainOuter=" << merge->outer()
-           << ", IterDomainInner=" << merge->inner() << ")";
+    label_ << "Merge";
   }
 
  private:
