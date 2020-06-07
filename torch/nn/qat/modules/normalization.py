952deba828bfd1941429ca28cceb6471974314bb
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-06-07 13:32:54 -0700

    layernorm: eager mode qat support (#39094)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39094
    
    Adds eager mode QAT handling for LayerNorm
    
    Test Plan:
    ```
    python test/test_quantization.py TestQuantizationAwareTraining.test_normalization
    ```
    
    Imported from OSS
    
    Differential Revision: D21885260
    
    fbshipit-source-id: 4f4c84a8bb8ba15dd78494f92569ed3a30d89169

--- torch/nn/qat/modules/normalization.py
+++ torch/nn/qat/modules/normalization.py
@@ -179,3 +179,47 @@ class InstanceNorm3d(nn.InstanceNorm3d):
             mod.num_features, mod.eps, mod.momentum, mod.affine,
             mod.track_running_stats, qconfig=qconfig)
         return qat_instancenorm
+
+class LayerNorm(nn.LayerNorm):
+    r"""
+    A LayerNorm module attached with FakeQuantize modules for both output
+    activation and weight, used for quantization aware training.
+
+    Similar to `torch.nn.LayerNorm`, with FakeQuantize modules initialized to
+    default.
+
+    Attributes:
+        activation_post_process: fake quant module for output activation
+        weight: fake quant module for weight
+    """
+    _FLOAT_MODULE = nn.LayerNorm
+
+    def __init__(self, normalized_shape, eps=1e-5, elementwise_affine=True,
+                 qconfig=None):
+        super(LayerNorm, self).__init__(normalized_shape, eps, elementwise_affine)
+        assert qconfig, 'qconfig must be provided for QAT module'
+        self.qconfig = qconfig
+        self.activation_post_process = qconfig.activation()
+
+    def forward(self, input):
+        return self.activation_post_process(
+            super(LayerNorm, self).forward(input))
+
+    @classmethod
+    def from_float(cls, mod, qconfig=None):
+        r"""Create a qat module from a float module or qparams_dict
+
+            Args: `mod` a float module, either produced by
+            torch.quantization utilities or directly from user
+        """
+        assert type(mod) == cls._FLOAT_MODULE, ' qat.' + cls.__name__ + '.from_float only works for ' + \
+            cls._FLOAT_MODULE.__name__
+        if not qconfig:
+            assert hasattr(mod, 'qconfig'), 'Input float module must have qconfig defined'
+            assert mod.qconfig, 'Input float module must have a valid qconfig'
+
+        qconfig = mod.qconfig
+        qat_layernorm = cls(
+            mod.normalized_shape, mod.eps, mod.elementwise_affine,
+            qconfig=qconfig)
+        return qat_layernorm
