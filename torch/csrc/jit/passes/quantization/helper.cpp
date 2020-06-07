61a9383920272b8a7f272ac631c80706a39cc572
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos576.prn2.facebook.com>
2020-06-07 00:01:59 -0700

    2020-06-07 nightly release (e4627e5dba1516afe0c12bf748ebc2200b6e378d)

--- torch/csrc/jit/passes/quantization/helper.cpp
+++ torch/csrc/jit/passes/quantization/helper.cpp
@@ -492,5 +492,65 @@ Module getInvokedModule(Module& module, Node* n, Value* self) {
   return findChildModule(module, path);
 }
 
+// ==================== filter functions for matches ==============
+bool is_int_constant(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap,
+    const std::string& vname,
+    int value) {
+  const auto& match_vmap = match.values_map;
+  auto v = toIValue(match_vmap.at(vmap.at(vname)));
+  return v && v->isInt() && v->toInt() == value;
+}
+
+bool is_functional(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap,
+    const std::string& vname,
+    const std::string& functional) {
+  const auto& match_vmap = match.values_map;
+  Value* v = match_vmap.at(vmap.at(vname));
+  return v->type()->cast<FunctionType>() &&
+    getFuncName(v) == functional;
+}
+
+bool is_module(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap,
+    const std::string& vname,
+    const std::string& module_qualified_name) {
+  const auto& match_vmap = match.values_map;
+  Value* relu = match_vmap.at(vmap.at(vname));
+  auto type = relu->type()->cast<ClassType>();
+  if (type && type->name()) {
+    static std::regex mangle_re("\\.___torch_mangle_\\d+");
+    auto qualified_name =
+        std::regex_replace(type->name()->qualifiedName(), mangle_re, "");
+    return qualified_name == module_qualified_name;
+  }
+  return false;
+};
+
+bool aten_add_alpha_is_one(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap) {
+  return is_int_constant(match, vmap, "alpha", 1);
+}
+
+bool is_functional_relu(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap) {
+  return is_functional(match, vmap, "relu", "relu");
+}
+
+bool is_relu_module(
+    const Match& match,
+    const std::unordered_map<std::string, Value*>& vmap) {
+  return is_module(match,
+                   vmap,
+                   "relu",
+                   "__torch__.torch.nn.modules.activation.ReLU");
+}
+
 } // namespace jit
 } // namespace torch
