b28422d44426375649e5058bc8c4eafb5ea3de16
Linbin Yu <linbin@fb.com>
2020-06-06 23:19:33 -0700

    add overload name for str cmp (#39607)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39607
    
    add overload name for strcmp macro to prevent duplicated op names in lite interpreter
    
    also reformatted some other files
    
    Test Plan:
    verified these op schema are changed
    
    ```
    -aten::eq(str a, str b) -> (bool)
    +aten::eq.str(str a, str b) -> (bool)
    
    -aten::ne(str a, str b) -> (bool)
    +aten::ne.str(str a, str b) -> (bool)
    
    -aten::lt(str a, str b) -> (bool)
    +aten::lt.str(str a, str b) -> (bool)
    
    -aten::gt(str a, str b) -> (bool)
    +aten::gt.str(str a, str b) -> (bool)
    
    -aten::le(str a, str b) -> (bool)
    +aten::le.str(str a, str b) -> (bool)
    
    -aten::ge(str a, str b) -> (bool)
    +aten::ge.str(str a, str b) -> (bool)
    ```
    
    Reviewed By: iseeyuan
    
    Differential Revision: D21913049
    
    fbshipit-source-id: 518db068c8c5b0efd19223f0bd94fc3351335dc4

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -85,8 +85,13 @@ white_list = [
     ('quantized::linear', datetime.date(2020, 6, 1)),
     ('_aten::*', datetime.date(2020, 6, 1)),
     ('_prim::*', datetime.date(2020, 6, 1)),
+    ('aten::eq', datetime.date(2020, 6, 30)),
+    ('aten::nq', datetime.date(2020, 6, 30)),
+    ('aten::lt', datetime.date(2020, 6, 30)),
+    ('aten::gt', datetime.date(2020, 6, 30)),
+    ('aten::le', datetime.date(2020, 6, 30)),
+    ('aten::ge', datetime.date(2020, 6, 30)),
 ]
-
 
 # The nightly will fail to parse newly added syntax to schema declarations
 # Add new schemas that will fail the nightly here
