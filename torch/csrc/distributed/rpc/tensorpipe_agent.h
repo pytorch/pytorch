0147216a46a67b110334ae246e1b032611099b19
Omkar Salpekar <osalpekar@fb.com>
2020-06-08 15:14:51 -0700

    [TensorPipe Agent] Documentation fixes and nits (#39467)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39467
    
    Mainly cleaned up some docs and spelling/grammar nits
    ghstack-source-id: 105457210
    
    Test Plan: Sandcastle/CI
    
    Differential Revision: D21755354
    
    fbshipit-source-id: 28e7b925ace7813548a1bf8cdcf96cd423a227aa

--- torch/csrc/distributed/rpc/tensorpipe_agent.h
+++ torch/csrc/distributed/rpc/tensorpipe_agent.h
@@ -37,10 +37,10 @@ struct AggregatedNetworkData {
   uint64_t totalErrors{0};
 };
 
-// TensorPipeAgent leverages tensorpipe (https://github.com/pytorch/tensorpipe)
-// to move tensors and payload through fatested transport and channel
-// transparently. We can see it as a hybrid RPC transport, providing
-// shared memory (linux) and tcp (linux & mac). CUDA will be supported next.
+// TensorPipeAgent leverages TensorPipe (https://github.com/pytorch/tensorpipe)
+// to transparently move tensors and payloads through the fastest available
+// transport or channel. It acts like a hybrid RPC transport, providing shared
+// memory (linux) and TCP (linux & mac) support. CUDA support is in progress.
 class TensorPipeAgent : public RpcAgent {
  public:
   TensorPipeAgent(
@@ -79,10 +79,13 @@ class TensorPipeAgent : public RpcAgent {
   using NetworkDataDict =
       std::unordered_map<std::string, AggregatedNetworkData>;
 
+  // Returns metrics tracked by the NetworkDataDict
   NetworkDataDict getNetworkData();
+  // Returns NetworkSourceInfo struct
   NetworkSourceInfo getNetworkSourceInfo();
 
  private:
+  // Populates workerIdToInfo_ and workerNameToInfo_ using addressStore_
   void collectNames();
 
   const std::string& findWorkerURL(const WorkerInfo& worker) const;
@@ -139,15 +142,16 @@ class TensorPipeAgent : public RpcAgent {
     std::atomic_flag isComplete = ATOMIC_FLAG_INIT;
   };
 
-  // State per client pipe to keep tracking of pending response message
-  // and error sate. pendingResponseMessage_ should be protected by
-  // mutex since it can be raced with user send() call.
+  // Maintains state per client pipe to track pending response messages and
+  // error states. pendingResponseMessage_ should be protected by a mutex since
+  // it can be raced with user send() call.
   // TODO: To achieve better performance we can have a pipe pool per
-  // client and work together with RpcBackendOptions to configure.
+  // client that can be configured using RpcBackendOptions.
   struct ClientPipe {
     explicit ClientPipe(std::shared_ptr<tensorpipe::Pipe> pipe) : pipe_(pipe) {}
     std::shared_ptr<tensorpipe::Pipe> pipe_;
     bool readError_{false};
+    // Map from Message Request ID's to corresponding futures.
     std::unordered_map<uint64_t, std::shared_ptr<AtomicFutureMessage>>
         pendingResponseMessage_;
   };
@@ -158,7 +162,7 @@ class TensorPipeAgent : public RpcAgent {
   std::shared_ptr<tensorpipe::Listener> listener_;
   std::unordered_map<worker_id_t, ClientPipe> connectedPipes_;
 
-  // We need map one keyed on name and one on id for easy lookup.
+  // Maps keyed on name and id for easy WorkerInfo lookup.
   std::unordered_map<worker_id_t, WorkerInfo> workerIdToInfo_;
   std::unordered_map<std::string, WorkerInfo> workerNameToInfo_;
   std::unordered_map<std::string, std::string> workerNameToURL_;
@@ -232,7 +236,7 @@ class TensorPipeAgent : public RpcAgent {
 
   // Map to Track Network Data
   NetworkDataDict networkData_;
-  // Mutex to guarg networkData_
+  // Mutex to guard networkData_
   std::mutex networkDataMutex_;
 
   // A mutex and a cv to guard access to the call counts and watch for changes.
