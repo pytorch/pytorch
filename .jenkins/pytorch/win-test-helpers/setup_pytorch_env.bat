85017806a4403164f34f813e570f5cc0984704b0
peter <peterghost86@gmail.com>
2020-06-08 21:29:42 +0800

    Append PDB search locations

--- .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
+++ .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
@@ -46,6 +46,7 @@ if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
 set ASAN_OPTIONS=detect_leaks=0:symbolize=1:strict_init_order=true
 set ASAN_SYMBOLIZER_PATH=%VCToolsInstallDir%bin\HostX64\x64
+set _NT_ALT_SYMBOL_PATH=%TMP_DIR_WIN%\build\torch\lib
 set PYTORCH_TEST_WITH_ASAN=1
 
 set DISTUTILS_USE_SDK=1
