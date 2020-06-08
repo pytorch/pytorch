e67aa93e1219e03dfba046a807d95836584bfc2b
peter <peterghost86@gmail.com>
2020-06-08 19:41:43 +0800

    Some fixes

--- .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
+++ .jenkins/pytorch/win-test-helpers/setup_pytorch_env.bat
@@ -43,8 +43,10 @@ pip install "ninja==1.9.0" future "hypothesis==4.53.2" "librosa>=0.6.2" psutil p
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
 :: No need to install faulthandler since we only test Python >= 3.6 on Windows
 :: faulthandler is builtin since Python 3.3
-set ASAN_OPTIONS=symbolize=1:detect_leaks=0
-set ASAN_SYMBOLIZER_PATH=%VCToolsInstallDir%\bin\HostX64\x64
+
+set ASAN_OPTIONS=detect_leaks=0:symbolize=1:strict_init_order=true
+set ASAN_SYMBOLIZER_PATH=%VCToolsInstallDir%bin\HostX64\x64
+set PYTORCH_TEST_WITH_ASAN=1
 
 set DISTUTILS_USE_SDK=1
 
