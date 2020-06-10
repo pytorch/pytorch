94b08de7f4158f79966a05dafb9ad7e9a876869b
Daniel Hollinger <dhollinger@fb.com>
2020-06-10 14:01:19 -0700

    Fix cut and paste error

--- torch/csrc/cuda/nccl.cpp
+++ torch/csrc/cuda/nccl.cpp
@@ -221,7 +221,7 @@ ncclComm_t comm_init_rank(
 #ifdef USE_NCCL
   using namespace torch::cuda::nccl::detail;
   ncclComm_t comm;
-  ncclUniqueId* nccl_id = reinterpret_cast<ncclUniqueId*>(&id);
+  ncclUniqueId* nccl_id = reinterpret_cast<ncclUniqueId*>(&comm_id);
   NCCL_CHECK(
       static_cast<torchNcclResult_t>(ncclCommInitRank(&comm, nranks, nccl_id, rank)));
   return comm;
