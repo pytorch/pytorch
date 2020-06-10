defdff45830d8c0b570d383333b3f5a2718ad412
Daiming Yang <ydaiming@amazon.com>
2020-06-10 14:59:45 +0000

    change _test_shuffle to allow disabled batching

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -771,7 +771,7 @@ class TestDataLoader(TestCase):
         found_labels = {i: 0 for i in range(self.labels.size(0))}
         batch_size = loader.batch_size
         for i, (batch_samples, batch_targets) in enumerate(loader):
-            for sample, target in zip(batch_samples, batch_targets):
+            if batch_size is None:
                 for data_point_idx, data_point in enumerate(self.data):
                     if data_point.eq(sample).all():
                         self.assertFalse(found_data[data_point_idx])
@@ -779,6 +779,15 @@ class TestDataLoader(TestCase):
                         break
                 self.assertEqual(target, self.labels[data_point_idx])
                 found_labels[data_point_idx] += 1
+            else:
+                for sample, target in zip(batch_samples, batch_targets):
+                    for data_point_idx, data_point in enumerate(self.data):
+                        if data_point.eq(sample).all():
+                            self.assertFalse(found_data[data_point_idx])
+                            found_data[data_point_idx] += 1
+                            break
+                    self.assertEqual(target, self.labels[data_point_idx])
+                    found_labels[data_point_idx] += 1
             self.assertEqual(sum(found_data.values()), (i + 1) * batch_size)
             self.assertEqual(sum(found_labels.values()), (i + 1) * batch_size)
         self.assertEqual(i, math.floor((len(self.dataset) - 1) / batch_size))
