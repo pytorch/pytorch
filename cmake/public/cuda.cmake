812519eba04088201b43b701598d06ad9063841f
peter <peterghost86@gmail.com>
2020-06-12 11:47:20 +0800

    lineinfo only

--- cmake/public/cuda.cmake
+++ cmake/public/cuda.cmake
@@ -494,7 +494,7 @@ if(MSVC)
   else()
     list(APPEND CUDA_NVCC_FLAGS "-Xcompiler" "-MD$<$<CONFIG:Debug>:d>")
   endif()
-  list(APPEND CUDA_NVCC_FLAGS "-G" "-lineinfo")
+  list(APPEND CUDA_NVCC_FLAGS "-lineinfo")
 elseif(CUDA_DEVICE_DEBUG)
   list(APPEND CUDA_NVCC_FLAGS "-g" "-G")  # -G enables device code debugging symbols
 endif()
