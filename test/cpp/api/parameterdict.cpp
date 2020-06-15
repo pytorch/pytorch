6b5207c413cc53cee454f9df9ab09916e6f7ef78
yyn19951228 <yiningyang1228@gmail.com>
2020-06-14 23:00:30 -0400

    Impl for Parameter Dict

---
+++ test/cpp/api/parameterdict.cpp
@@ -0,0 +1,113 @@
+#include <gtest/gtest.h>
+#include <torch/torch.h>
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+#include <test/cpp/api/support.h>
+
+using namespace torch::nn;
+using namespace torch::test;
+
+struct ParameterDictTest : torch::test::SeedingFixture {};
+
+TEST_F(ParameterDictTest, ConstructFromTensor) {
+  ParameterDict dict;
+  dict->insert("A", torch::tensor({1.0}));
+  dict->insert("B", torch::tensor({2.0}));
+  dict->insert("C", torch::tensor({3.3}));
+  ASSERT_EQ(dict->size(), 3);
+}
+
+TEST_F(ParameterDictTest, ConstructFromOrderedDict) {
+  torch::OrderedDict<std::string, torch::Tensor> params = {
+      {"a", torch::tensor({1.0})}, {"b", torch::tensor({2.0})}};
+  auto dict = torch::nn::ParameterDict(params);
+  ASSERT_EQ(dict->size(), 2);
+}
+
+TEST_F(ParameterDictTest, InsertAndContains) {
+  ParameterDict dict;
+  dict->insert("A", torch::tensor({1.0}));
+  ASSERT_EQ(dict->size(), 1);
+  ASSERT_TRUE(dict->contains("A"));
+  ASSERT_FALSE(dict->contains("C"));
+}
+
+TEST_F(ParameterDictTest, InsertAndClear) {
+  ParameterDict dict;
+  dict->insert("A", torch::tensor({1.0}));
+  ASSERT_EQ(dict->size(), 1);
+  dict->clear();
+  ASSERT_EQ(dict->size(), 0);
+}
+
+TEST_F(ParameterDictTest, InsertAndPop) {
+  ParameterDict dict;
+  dict->insert("A", torch::tensor({1.0}));
+  ASSERT_EQ(dict->size(), 1);
+  ASSERT_THROWS_WITH(dict->pop("B"), "No Parameter with name B is registered");
+  torch::Tensor p = dict->pop("A");
+  ASSERT_EQ(dict->size(), 0);
+  ASSERT_TRUE(torch::eq(p, torch::tensor({1.0})).item<bool>());
+}
+
+TEST_F(ParameterDictTest, SimpleUpdate) {
+  ParameterDict dict;
+  ParameterDict otherDict;
+  dict->insert("A", torch::tensor({1.0}));
+  dict->insert("B", torch::tensor({2.0}));
+  dict->insert("C", torch::tensor({3.0}));
+  otherDict->insert("A", torch::tensor({5.0}));
+  otherDict->insert("D", torch::tensor({5.0}));
+  dict->update(*otherDict);
+  ASSERT_EQ(dict->size(), 4);
+  ASSERT_TRUE(torch::eq(dict["A"], torch::tensor({5.0})).item<bool>());
+}
+
+TEST_F(ParameterDictTest, InsertAndGetTest) {
+  ParameterDict dict;
+  dict->insert("A", torch::tensor({1.0}));
+  ASSERT_EQ(dict->size(), 1);
+  ASSERT_TRUE(torch::eq(dict["A"], torch::tensor({1.0})).item<bool>());
+}
+
+TEST_F(ParameterDictTest, Keys) {
+  torch::OrderedDict<std::string, torch::Tensor> params = {
+      {"a", torch::tensor({1.0})},
+      {"b", torch::tensor({2.0})},
+      {"c", torch::tensor({1.0, 2.0})}};
+  auto dict = torch::nn::ParameterDict(params);
+  std::vector<std::string> keys = dict->keys();
+  std::vector<std::string> true_keys{"a", "b", "c"};
+  ASSERT_EQ(keys, true_keys);
+}
+
+TEST_F(ParameterDictTest, Values) {
+  torch::OrderedDict<std::string, torch::Tensor> params = {
+      {"a", torch::tensor({1.0})},
+      {"b", torch::tensor({2.0})},
+      {"c", torch::tensor({3.0})}};
+  auto dict = torch::nn::ParameterDict(params);
+  std::vector<torch::Tensor> values = dict->values();
+  std::vector<torch::Tensor> true_values{
+      torch::tensor({1.0}), torch::tensor({2.0}), torch::tensor({3.0})};
+  for (auto i = 0; i < values.size(); i += 1) {
+    ASSERT_TRUE(torch::eq(values[i], true_values[i]).item<bool>());
+  }
+}
+
+TEST_F(ParameterDictTest, PrettyPrintParameterDict) {
+  torch::OrderedDict<std::string, torch::Tensor> params = {
+      {"a", torch::tensor({1.0})},
+      {"b", torch::tensor({2.0})},
+      {"c", torch::tensor({3.0})}};
+  auto dict = torch::nn::ParameterDict(params);
+  ASSERT_EQ(
+      c10::str(dict),
+      "torch::nn::ParameterDict(\n"
+      "(a): Parameter containing: [CPUFloatType of size [1]]\n"
+      "(b): Parameter containing: [CPUFloatType of size [1]]\n"
+      "(c): Parameter containing: [CPUFloatType of size [1]]\n"
+      ")");
+}
---
+++ torch/csrc/api/include/torch/nn/modules/container/parameterdict.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include <torch/nn/cloneable.h>
+#include <torch/nn/pimpl.h>
+#include <torch/ordered_dict.h>
+
+#include <vector>
+
+namespace torch {
+namespace nn {
+
+class ParameterDictImpl : public Cloneable<ParameterDictImpl> {
+ public:
+  using Iterator = OrderedDict<std::string, Tensor>::Iterator;
+  using ConstIterator = OrderedDict<std::string, Tensor>::ConstIterator;
+
+  ParameterDictImpl() = default;
+
+  explicit ParameterDictImpl(
+      torch::OrderedDict<std::string, torch::Tensor> params) {
+    parameters_.reserve(params.size());
+    for (const auto& item : params) {
+      insert(std::move(item.key()), std::move(item.value()));
+    }
+  }
+
+  /// `reset()` is empty for `ParameterDict`, since it does not have parameters
+  /// of its own.
+  void reset() override {}
+
+  /// Pretty prints the `ParameterDict` module into the given `stream`.
+  void pretty_print(std::ostream& stream) const override {
+    stream << "torch::nn::ParameterDict(" << std::endl;
+    for (const auto& pair : parameters_) {
+      stream << "(" << pair.key() << ")"
+             << ": Parameter containing: [" << pair.value().type()
+             << " of size " << pair.value().sizes() << "]";
+      ;
+      stream << std::endl;
+    }
+    stream << ")";
+  }
+
+  /// Insert the parameter along with the key into ParameterDict
+  /// The parameter is set to be require grad by default
+  Tensor& insert(std::string key, Tensor param, bool requires_grad = true) {
+    return register_parameter(key, param, requires_grad);
+  }
+
+  /// Remove key from the ParameterDict, and return its value
+  Tensor pop(const std::string& key) {
+    TORCH_CHECK(
+        parameters_.contains(key),
+        "No Parameter with name `",
+        key,
+        "` is registered");
+    torch::Tensor v = parameters_[key].clone();
+    parameters_.erase(key);
+    return v;
+  }
+
+  /// Return the keys in the dict
+  ::std::vector<std::string> keys() const {
+    std::vector<std::string> keys;
+    keys.reserve(parameters_.size());
+    for (const auto& param : parameters_) {
+      keys.push_back(param.key());
+    }
+    return keys;
+  }
+
+  /// Return the Values in the dict
+  ::std::vector<torch::Tensor> values() const {
+    std::vector<torch::Tensor> values;
+    values.reserve(parameters_.size());
+    for (const auto& param : parameters_) {
+      values.push_back(param.value());
+    }
+    return values;
+  }
+
+  /// Returns an `OrderedDict` with the parameters of this `Module` along with
+  /// their keys, and if `recurse` is true also recursively of every submodule.
+  OrderedDict<std::string, Tensor> named_parameters(bool recurse = true) const {
+    return named_parameters(recurse);
+  }
+
+  /// Return an iterator to the start of ParameterDict
+  Iterator begin() {
+    return parameters_.begin();
+  }
+
+  /// Return a const iterator to the start of ParameterDict
+  ConstIterator begin() const {
+    return parameters_.begin();
+  }
+
+  /// Return an iterator to the end of ParameterDict
+  Iterator end() {
+    return parameters_.end();
+  }
+
+  /// Return a const iterator to the end of ParameterDict
+  ConstIterator end() const {
+    return parameters_.end();
+  }
+
+  /// Return the number of items currently stored in the ParameterDict
+  size_t size() const noexcept {
+    return parameters_.size();
+  }
+
+  /// Return true if the ParameterDict is empty, otherwise return false
+  bool empty() const noexcept {
+    return parameters_.is_empty();
+  }
+
+  /// Update the ParameterDict with the key-value pairs from
+  /// another ParameterDict, overwriting existing key
+  template <typename Container>
+  void update(const Container& container) {
+    parameters_.reserve(parameters_.size() + container.size());
+    for (auto& item : container) {
+      // erase and overwrite the duplicate keys
+      if (contains(item.key())) {
+        pop(item.key());
+      }
+      insert(std::move(item.key()), std::move(item.value()));
+    }
+  }
+
+  /// Remove all parameters in the ParameterDict
+  void clear() {
+    parameters_.clear();
+  }
+
+  /// Check if the centain parameter with the key in the ParameterDict
+  bool contains(const std::string& key) {
+    return parameters_.contains(key);
+  }
+
+  Tensor& get(std::string key) {
+    return parameters_[key];
+  }
+
+  Tensor& operator[](const std::string& key) {
+    return parameters_[key];
+  }
+
+  const Tensor& operator[](const std::string& key) const {
+    return parameters_[key];
+  }
+};
+
+TORCH_MODULE(ParameterDict);
+
+} // namespace nn
+} // namespace torch
