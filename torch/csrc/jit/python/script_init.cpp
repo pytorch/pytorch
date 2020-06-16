189a78185996a4b7eba7454dd4d4828a657d6a6c
James Reed <jamesreed@fb.com>
2020-06-15 17:57:54 -0700

    [JIT] Expose `__deepcopy__` on script::Object
    [ghstack-poisoned]

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -846,6 +846,11 @@ void initJitScriptBindings(PyObject* module) {
     }
   }
 
+  object_class.def("__deepcopy__", [](const Object& self, py::dict memo) {
+    // TODO: Figure out if memoization makes any sense in this context
+    return Object(IValue(self._ivalue()).deepcopy().toObject());
+  });
+
   // torch.jit.ScriptModule is a subclass of this C++ object.
   // Methods here are prefixed with _ since they should not be
   // public.
