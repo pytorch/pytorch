8ff7f67a30c8a9fbefccdee5709587d03a0e1d41
Jeff Daily <jeff.daily@amd.com>
2020-06-17 19:32:51 +0000

    thunk upgrade needed sudo for cp command

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -136,8 +136,9 @@ if [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
     wget http://repo.radeon.com/rocm/apt/3.5/pool/main/h/hsakmt-roct-dev3.5.0/hsakmt-roct-dev3.5.0_1.0.9-347-gd4b224f_amd64.deb
     dpkg-deb -vx hsakmt-roct3.5.0_1.0.9-347-gd4b224f_amd64.deb .
     dpkg-deb -vx hsakmt-roct-dev3.5.0_1.0.9-347-gd4b224f_amd64.deb .
-    cp -r opt/rocm-3.5.0/* /opt/rocm-3.3.0/
+    sudo cp -r opt/rocm-3.5.0/* /opt/rocm-3.3.0/
     popd
+    rm -rf rocm3.5-thunk
   fi
 
   # ROCm CI is using Caffe2 docker images, which needs these wrapper
