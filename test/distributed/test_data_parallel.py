08227fea4f10037d8625f5d1c0dd5fca718a3813
Alban Desmaison <albandes@fb.com>
2020-06-17 10:14:18 -0700

    Revert D22079377: [pytorch][PR] [RELAND] Change AccumulateGrad to yield `.grad`s that match weights' memory layout
    Test Plan: revert-hammer
    
    Differential Revision:
    D22079377
    
    Original commit changeset: 9bd2b7e0c34f
    
    fbshipit-source-id: c22cc349d790caa574eace0d63980854c33e5a59

--- test/distributed/test_data_parallel.py
+++ test/distributed/test_data_parallel.py
@@ -3,7 +3,6 @@ import io
 import unittest
 from copy import deepcopy
 from collections import OrderedDict
-from itertools import product
 
 import torch
 from torch import nn
@@ -16,7 +15,6 @@ from torch.testing._internal.common_utils import skipIfRocm
 import torch.nn.functional as F
 
 torch.set_default_dtype(torch.double)
-
 
 class TestDataParallel(TestCase):
 
@@ -682,69 +680,6 @@ class TestDataParallel(TestCase):
         torch.save(dpm, data)
         dpm = torch.nn.parallel.replicate(module, devices=[0, 1], detach=True)
         torch.save(dpm, data)
-
-    @unittest.skipIf(not TEST_MULTIGPU, "multi-GPU not supported")
-    def test_strided_grad_layout(self):
-        class ConvNet(nn.Module):
-            def __init__(self, layouts, dtypes):
-                super(ConvNet, self).__init__()
-                self.dtypes = dtypes
-                self.conv0 = torch.nn.Conv2d(8, 16, (2, 2)).to(memory_format=layouts[0], dtype=dtypes[0])
-                self.conv1 = torch.nn.Conv2d(16, 32, (2, 2)).to(memory_format=layouts[1], dtype=dtypes[1])
-                self.conv2 = torch.nn.Conv2d(32, 16, (2, 2)).to(memory_format=layouts[2], dtype=dtypes[2])
-                self.conv3 = torch.nn.Conv2d(16, 8, (2, 2)).to(memory_format=layouts[3], dtype=dtypes[3])
-
-            def forward(self, x):
-                x = x.to(self.dtypes[0])
-                x = self.conv0(x).to(self.dtypes[1])
-                x = self.conv1(x).to(self.dtypes[2])
-                x = self.conv2(x).to(self.dtypes[3])
-                x = self.conv3(x)
-                return x
-
-        layer_formats = ([torch.contiguous_format] * 4,
-                         [torch.channels_last] * 2 + [torch.contiguous_format] * 2,
-                         [torch.channels_last] * 4,)
-        layer_dtypes = ([torch.float] * 4,
-                        [torch.float] * 2 + [torch.half] * 2,
-                        [torch.half] * 4,)
-
-        ndevs = torch.cuda.device_count()
-        input = torch.randn(ndevs * 8, 8, 8, 8, device="cuda:0", dtype=torch.float)
-        target = torch.randn(ndevs * 8, 8, 4, 4, device="cuda:0", dtype=torch.float)
-        device_ids = list(range(ndevs))
-
-        with torch.backends.cudnn.flags(enabled=True, deterministic=True, benchmark=False):
-            for formats, dtypes in product(layer_formats, layer_dtypes):
-                model_msg = "formats = {} dtypes = {}".format(formats, dtypes)
-                try:
-                    m = ConvNet(formats, dtypes).cuda(device="cuda:0")
-                    m_dp = dp.DataParallel(deepcopy(m), device_ids=device_ids)
-                    has_half = any(p.dtype is torch.half for p in m.parameters())
-                    tol = 1.e-3 if has_half else 1.e-5
-                except BaseException:
-                    # Prints case-specific debugging info to narrow down failing case.
-                    print("Caught exception during model creation for " + model_msg, flush=True)
-                    raise
-                # 2 iters:  First iter creates grads, second iter tries zeroed grads.
-                for it in range(2):
-                    iter_msg = "iter = {} ".format(it) + model_msg
-                    try:
-                        F.mse_loss(m(input).float(), target).backward()
-                        F.mse_loss(m_dp(input).float(), target).backward()
-                        for i, ((layer_name, m_child), m_dp_child) in enumerate(zip(m.named_children(),
-                                                                                    m_dp.module.children())):
-                            named_msg = layer_name + ".weight " + iter_msg
-                            self.assertTrue(m_child.weight.grad.is_contiguous(memory_format=formats[i]), named_msg)
-                            self.assertTrue(m_dp_child.weight.grad.is_contiguous(memory_format=formats[i]), named_msg)
-                            for j, ((param_name, p), p_dp) in enumerate(zip(m_child.named_parameters(),
-                                                                            m_dp_child.parameters())):
-                                named_msg = layer_name + "." + param_name + " " + iter_msg
-                                self.assertEqual(p.grad, p_dp.grad, msg=named_msg, rtol=tol, atol=tol)
-                    except BaseException:
-                        # Makes sure we still get info if an error occurred somewhere other than the asserts.
-                        print("Caught exception during iterations at " + iter_msg, flush=True)
-                        raise
 
 
 if __name__ == '__main__':
