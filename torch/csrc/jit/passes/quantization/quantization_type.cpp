4648e987b09e1c1a8e893b2052f1228cb3968eb3
Jerry Zhang <jerryzh168@gmail.com>
2020-06-17 15:20:36 -0700

    [quant][graphmode] Clean up and add more logging
    Summary:
    - separate passes in insert observers to make it more robust
    - added print for quantization type
    - added more logging for insert observers
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

---
+++ torch/csrc/jit/passes/quantization/quantization_type.cpp
@@ -0,0 +1,15 @@
+#include "quantization_type.h"
+
+namespace torch {
+namespace jit {
+
+std::ostream& operator<<(std::ostream& os, QuantType t) {
+  switch(t) {
+    case QuantType::DYNAMIC: os << "dynamic"; break;
+    case QuantType::STATIC: os << "static"; break;
+    default: os.setstate(std::ios_base::failbit);
+  }
+  return os;
+}
+
+}}
