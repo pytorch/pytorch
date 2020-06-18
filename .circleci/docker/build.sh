befff680f1525228c29616f872f37e9d4f4c823d
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-06-18 12:07:38 -0700

    more

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -125,12 +125,12 @@ case "$image" in
     VISION=yes
     KATEX=yes
     ;;
-  pytorch-linux-bionic-cuda11.0-cudnn8-py3-gcc9)
+  pytorch-linux-xenial-cuda11.0-cudnn8-py3-gcc7)
     UBUNTU_VERSION=18.04-rc
     CUDA_VERSION=11.0
     CUDNN_VERSION=8
     ANACONDA_PYTHON_VERSION=3.6
-    GCC_VERSION=9
+    GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
     VISION=yes
@@ -192,6 +192,28 @@ case "$image" in
     PROTOBUF=yes
     DB=yes
     VISION=yes
+    ;;
+  pytorch-linux-bionic-cuda11.0-cudnn8-py3.6-gcc9)
+    UBUNTU_VERSION=18.04-rc
+    CUDA_VERSION=11.0
+    CUDNN_VERSION=8
+    ANACONDA_PYTHON_VERSION=3.6
+    GCC_VERSION=9
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    KATEX=yes
+    ;;
+  pytorch-linux-bionic-cuda11.0-cudnn8-py3.8-gcc9)
+    UBUNTU_VERSION=18.04-rc
+    CUDA_VERSION=11.0
+    CUDNN_VERSION=8
+    ANACONDA_PYTHON_VERSION=3.8
+    GCC_VERSION=9
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    KATEX=yes
     ;;
   pytorch-linux-xenial-rocm3.3-py3.6)
     ANACONDA_PYTHON_VERSION=3.6
