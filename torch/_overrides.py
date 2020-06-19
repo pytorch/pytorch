2a4d312ce656e46af94abb2cb94987255ae12370
Kimish Patel <kimishpatel@fb.com>
2020-06-19 11:50:16 -0700

    Update on "Add fused add_relu op."
    Summary:
    Many networks such as resnet have adds followed by relu. This op is the
    first step in enabling this fused implementation.
    Once we have the fused add_relu op, a JIT pass will be written to
    replace add + relu patterns with add_relu.
    
    Test Plan:
    python test/test_nn.py TestAddRelu
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D21822397](https://our.internmc.facebook.com/intern/diff/D21822397)
    
    [ghstack-poisoned]

--- torch/_overrides.py
+++ torch/_overrides.py
@@ -169,6 +169,7 @@ def get_testing_overrides():
         torch.adaptive_max_pool1d: lambda inputs, output_size: -1,
         torch.acos: lambda input, out=None: -1,
         torch.acosh: lambda input, out=None: -1,
+        torch.add_relu: lambda input, other, out=None: -1,
         torch.add: lambda input, other, out=None: -1,
         torch.addbmm: lambda input, batch1, batch2, alpha=1, beta=1, out=None: -1,
         torch.addcdiv: lambda input, tensor1, tensor2, value=1, out=None: -1,
