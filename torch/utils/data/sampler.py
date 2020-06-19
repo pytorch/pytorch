f3e43c4fd3aa69308e5884ca1cbdc4c8a972a5fe
Daiming Yang <ydaiming@amazon.com>
2020-06-19 17:33:12 +0000

    fix a typo

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -106,7 +106,7 @@ class RandomSampler(Sampler):
         n = len(self.data_source)
         if self.generator is None:
             generator = torch.Generator()
-            generator.manual_seed(torch.empty((), dtype=troch.int64).random_().item())
+            generator.manual_seed(torch.empty((), dtype=torch.int64).random_().item())
         else:
             generator = self.generator
         if self.replacement:
