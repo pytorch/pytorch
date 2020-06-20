e390a5fc28d417ea985ede1f27dcc1d2fd02d97b
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-06-20 10:36:39 -0700

    quant docs: add and clean up hardsigmoid
    Adds and simplifies quantization docs for hardsigmoid
    
    Test plan:
    * build docs on Mac OS
    * inspect
    
    [ghstack-poisoned]

--- torch/nn/quantized/functional.py
+++ torch/nn/quantized/functional.py
@@ -490,17 +490,7 @@ def elu(input, alpha=1., inplace=False, scale=None, zero_point=None):
 
 def hardsigmoid(input):
     # type: (Tensor) -> Tensor
-    r"""
-    Applies the quantized element-wise function
-
-    .. math::
-        \text{Hardsigmoid}(x) = \begin{cases}
-            0 & \text{if~} x \le -3, \\
-            1 & \text{if~} x \ge +3, \\
-            x / 6 & \text{otherwise}
-        \end{cases}
-
-    See :class:`~torch.nn.Hardsigmoid` for more details.
+    r"""This is the quantized version of :meth:`~torch.nn.functional.hardsigmoid`.
     """
     if not input.is_quantized:
         raise ValueError("Input to 'quantized.hardsigmoid' must be quantized!")
