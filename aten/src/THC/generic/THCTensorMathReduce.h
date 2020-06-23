ffd7f05d3ae650d25767568e3579a17ede9e2e71
Vitaly Fedyunin <vitalyf@fb.com>
2020-06-23 16:57:24 -0700

    Revert D22162469: [pytorch][PR] Migrate `var` & `std` to ATen
    Test Plan: revert-hammer
    
    Differential Revision:
    D22162469 (https://github.com/pytorch/pytorch/commit/7a3c223bbb711c7a93910ce406a0126b8000b43b)
    
    Original commit changeset: 8d901c779767
    
    fbshipit-source-id: 9e0fa439732478349c0ac6c7baafba063edfac5d

--- aten/src/THC/generic/THCTensorMathReduce.h
+++ aten/src/THC/generic/THCTensorMathReduce.h
@@ -9,7 +9,9 @@
 THC_API void THCTensor_(renorm)(THCState *state, THCTensor* self, THCTensor* src, scalar_t value, int dimension, scalar_t max_norm);
 THC_API void THCTensor_(norm)(THCState *state, THCTensor* self, THCTensor* src, scalar_t value, int dimension, int keepdim);
 
+THC_API accreal THCTensor_(std_all)(THCState *state, THCTensor *self, bool unbiased);
 THC_API accreal THCTensor_(normall)(THCState *state, THCTensor *self, scalar_t value);
+THC_API accreal THCTensor_(var_all)(THCState *state, THCTensor *self, bool unbiased);
 
 #endif
 
