32983859f3a780d5d06d675bfead154d5677137c
Jerry Zhang <jerryzh@fb.com>
2020-06-22 21:28:31 -0700

    [quant][graphmode][refactor] InsertQuantDeQuantHelper (#40384)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/40384
    
    Test Plan: Imported from OSS
    
    Differential Revision: D22164072
    
    fbshipit-source-id: 0ca86265cfef1afa99dd860a452f3dd76e31792a

--- torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
+++ torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
@@ -456,15 +456,10 @@ class SubGraphCloneHelper {
 
 class InsertQuantDeQuantHelper {
  public:
+  InsertQuantDeQuantHelper(QuantType quant_type, bool debug)
+      : quant_type_(quant_type), debug_(debug) {}
+
   void run(Module& module, const std::string& method_name);
-
-  void setQuantType(QuantType quant_type) {
-    quant_type_ = quant_type;
-  }
-
-  void setDebug(bool debug) {
-    debug_ = debug;
-  }
 
   // Cleanup observer nodes from graph and observer modules
   // from module object and ClassType
@@ -1075,6 +1070,10 @@ void InsertQuantDeQuantHelper::propagateQuantizationOps(Block* block) {
 void InsertQuantDeQuantHelper::runWeightObserver(
     Module& module,
     const std::string& method_name) {
+  if (quant_type_ != QuantType::DYNAMIC) {
+    return;
+  }
+
   for (auto& invoked_methods : getInvokedMethods(module, method_name)) {
     auto& invoked_module = std::get<0>(invoked_methods);
     const auto& invoked_method_name = std::get<1>(invoked_methods);
@@ -1329,12 +1328,8 @@ Module InsertQuantDeQuant(
     bool debug,
     QuantType quant_type) {
   Module module = input_module.clone(inplace);
-  InsertQuantDeQuantHelper h;
-  h.setQuantType(quant_type);
-  if (quant_type == QuantType::DYNAMIC) {
-    h.runWeightObserver(module, method_name);
-  }
-  h.setDebug(debug);
+  InsertQuantDeQuantHelper h(quant_type, debug);
+  h.runWeightObserver(module, method_name);
   h.run(module, method_name);
   h.cleanup(module);
   h.propagateQuantizationOps(module);
