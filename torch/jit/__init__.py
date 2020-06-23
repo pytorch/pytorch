8b84fd4747002caf0924975bc64d366e9cf08a2e
eellison <eellison@fb.com>
2020-06-23 12:43:09 -0700

    address comments

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -794,19 +794,18 @@ def wrap_check_inputs(check_inputs):
 def freeze(mod, preserved_attrs : Optional[List[str]] = None):
     r"""
     Freezing a :class:`ScriptModule` will clone it and attempt to inline the cloned
-    module's submodules, parameters, and attributes as constants
-    in the TorchScript IR Graph. By default, `forward` will be preserved,
-    as well as attributes specified in `preserved_attrs`. Additionally, attributes which
-    are modified in the `forward` or in a method specified in `preserved_attrs`
-    will be preserved.
+    module's submodules, parameters, and attributes as constants in the TorchScript IR Graph.
+    By default, `forward` will be preserved, as well as attributes & methods specified in
+    `preserved_attrs`. Additionally, any attribute that is modified within a preserved
+    method will be preserved.
 
-    Freezing currently only accepts ScriptModules which are in eval mode.
+    Freezing currently only accepts ScriptModules that are in eval mode.
 
     Arguments:
         mod (:class:`ScriptModule`): a module to be frozen
 
-        preserved_attrs (Optional[List[str]]): a list of additional attributes to `forward` to preserve.
-        Attributes modified in methods specified in `preserved_attrs` will also be preserved.
+        preserved_attrs (Optional[List[str]]): a list of attributes to preserve in addition to the forward method.
+        Attributes modified in preserved methods will also be preserved.
 
     Returns:
         Frozen :class:`ScriptModule`.
@@ -840,7 +839,6 @@ def freeze(mod, preserved_attrs : Optional[List[str]] = None):
                 super(MyModule2, self).__init__()
                 self.modified_tensor = torch.tensor(10.)
                 self.version = 1
-                self.unused_tensor = torch.tensor(5.)
 
             def forward(self, input):
                 self.modified_tensor += 1
@@ -849,7 +847,7 @@ def freeze(mod, preserved_attrs : Optional[List[str]] = None):
 
         scripted_module = torch.jit.script(MyModule2().eval())
         frozen_module = torch.jit.freeze(scripted_module, preserved_attrs=["version"])
-        # we've manually preserved `version`, soit still exists on the frozen module and can be modified
+        # we've manually preserved `version`, so it still exists on the frozen module and can be modified
         assert frozen_module.version == 1
         frozen_module.version = 2
         # `modified_tensor` is detected as being mutated in the forward, so freezing preserves it to
@@ -860,8 +858,8 @@ def freeze(mod, preserved_attrs : Optional[List[str]] = None):
 
     Note:
         If you're not sure why an attribute is not being inlined as a constant, you can run
-        `dump_alias_db` on frozen_module.forward.graph to try to investigate if freezing has
-        detected has detected the attribute is being modified.
+        `dump_alias_db` on frozen_module.forward.graph to see if freezing has detected the
+        attribute is being modified.
     """
     if not isinstance(mod, ScriptModule):
         raise RuntimeError("Freezing expects a ScriptModule as input. "
