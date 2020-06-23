20738669e60b35a9632dbeb14c4661c47433c88c
ilia-cher <iliacher@fb.com>
2020-06-22 18:13:48 -0700

    Update on "Destroy CUDA events after profiling"
    Summary:
    Using shared_ptr for cuda event and
    destroying cuda event after the last copy is destroyed
    
    Differential Revision: [D22027092](https://our.internmc.facebook.com/intern/diff/D22027092)
    
    [ghstack-poisoned]

--- test/test_profiler.py
+++ test/test_profiler.py
@@ -1,4 +1,5 @@
 import collections
+import gc
 import unittest
 
 import torch
@@ -6,23 +7,39 @@ from torch.testing._internal.common_utils import (
     TestCase, run_tests, TEST_WITH_ASAN)
 from torch.autograd.profiler import profile
 
+try:
+    import psutil
+    HAS_PSUTIL = True
+except ImportError:
+    HAS_PSUTIL = False
 
+
+@unittest.skipIf(not HAS_PSUTIL, "Requires psutil to run")
 @unittest.skipIf(TEST_WITH_ASAN, "Cannot test with ASAN")
 @unittest.skipIf(not torch.cuda.is_available(), "CUDA is required")
 class TestProfiler_cuda(TestCase):
-    def test_cuda_mem_leak(self):
-        """Checks that there's no CUDA memory leak when using the profiler
+    def test_mem_leak(self):
+        """Checks that there's no memory leak when using profiler with CUDA
         """
-        device = torch.device('cuda:0')
-        t = torch.rand(1, 1).cuda(device=device)
-        stats = torch.cuda.memory_stats(device)
-        print("stats before: ", stats)
-        with profile(use_cuda=True):
-            for _ in range(1024):
-                t = torch.mm(t, t)
-        stats = torch.cuda.memory_stats(device)
-        print("\nstats after: ", stats)
+        t = torch.rand(1, 1).cuda()
+        p = psutil.Process()
+        last_rss = collections.deque(maxlen=5)
+        for outer_idx in range(10):
+            with profile(use_cuda=True):
+                for _ in range(1024):
+                    t = torch.mm(t, t)
 
+            gc.collect()
+            torch.cuda.empty_cache()
+            last_rss.append(p.memory_info().rss)
+
+        max_diff = -1
+        for idx in range(1, len(last_rss)):
+            max_diff = max(max_diff, last_rss[idx] - last_rss[idx-1])
+
+        # with CUDA events leaking the increase in memory was ~7 MB,
+        # using much smaller threshold but not zero to reduce flakiness
+        self.assertTrue(max_diff < 100 * 1024)
 
 if __name__ == '__main__':
     run_tests()
