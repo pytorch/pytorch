6c0ad6b5e403a96bdc0980003677a53f4fe88ec7
Mike Ruberry <mruberry@devfair044.maas>
2020-06-23 00:59:43 -0700

    TESTING 2

--- caffe2/serialize/inline_container.cc
+++ caffe2/serialize/inline_container.cc
@@ -306,9 +306,9 @@ void PyTorchStreamWriter::setup(const string& file_name) {
   mz_zip_writer_init_v2(ar_.get(), 0, MZ_ZIP_FLAG_WRITE_ZIP64);
   valid("initializing archive ", file_name.c_str());
 
-  std::string version = c10::to_string(kProducedFileFormatVersion);
-  version.push_back('\n');
-  writeRecord("version", version.c_str(), version.size());
+  // std::string version = c10::to_string(kProducedFileFormatVersion);
+  // version.push_back('\n');
+  // writeRecord("version", version.c_str(), version.size());
 }
 
 void PyTorchStreamWriter::setMinVersion(const uint64_t version) {
@@ -349,6 +349,9 @@ void PyTorchStreamWriter::writeEndOfFile() {
   // std::string version_string = c10::to_string(version_);
   // version_string.push_back('\n');
   // writeRecord("version", version_string.c_str(), version_string.size());
+  std::string version = c10::to_string(kProducedFileFormatVersion);
+  version.push_back('\n');
+  writeRecord("version", version.c_str(), version.size());
 
   AT_ASSERT(!finalized_);
   finalized_ = true;
