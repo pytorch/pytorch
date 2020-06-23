d15fcc7e491c69ba6170ff4caafa0aafb59e195b
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-06-23 08:56:45 -0700

    quant docs: add and clean up LayerNorm (#40342)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40342
    
    Cleans up the docstrings for quantized LayerNorm, and adds it to the docs.
    
    Test Plan: * build on Mac OS and inspect
    
    Differential Revision: D22152639
    
    Pulled By: vkuzo
    
    fbshipit-source-id: 38adf14b34675d1983ac4ed751938aa396e5400b

--- torch/nn/quantized/modules/normalization.py
+++ torch/nn/quantized/modules/normalization.py
@@ -7,59 +7,12 @@ import torch
 import torch.nn.quantized.functional
 
 class LayerNorm(torch.nn.LayerNorm):
-    r"""Applies Layer Normalization over a mini-batch of inputs as described in
-    the paper `Layer Normalization <https://arxiv.org/abs/1607.06450>`__
+    r"""This is the quantized version of :class:`~torch.nn.LayerNorm`.
 
-    .. math::
-        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
+    Additional args:
+        * **scale** - quantization scale of the output, type: double.
+        * **zero_point** - quantization zero point of the output, type: long.
 
-    The mean and standard-deviation are calculated separately over the last
-    certain number dimensions which have to be of the shape specified by
-    :attr:`normalized_shape`.
-    :math:`\gamma` and :math:`\beta` are learnable affine transform parameters of
-    :attr:`normalized_shape` if :attr:`elementwise_affine` is ``True``.
-
-    .. note::
-        Unlike Batch Normalization and Instance Normalization, which applies
-        scalar scale and bias for each entire channel/plane with the
-        :attr:`affine` option, Layer Normalization applies per-element scale and
-        bias with :attr:`elementwise_affine`.
-
-    This layer uses statistics computed from input data in both training and
-    evaluation modes.
-
-    Args:
-        normalized_shape (int or list or torch.Size): input shape from an expected input
-            of size
-
-            .. math::
-                [* \times \text{normalized\_shape}[0] \times \text{normalized\_shape}[1]
-                    \times \ldots \times \text{normalized\_shape}[-1]]
-
-            If a single integer is used, it is treated as a singleton list, and this module will
-            normalize over the last dimension which is expected to be of that specific size.
-        eps: a value added to the denominator for numerical stability. Default: 1e-5
-        elementwise_affine: a boolean value that when set to ``True``, this module
-            has learnable per-element affine parameters initialized to ones (for weights)
-            and zeros (for biases). Default: ``True``.
-
-    Shape:
-        - Input: :math:`(N, *)`
-        - Output: :math:`(N, *)` (same shape as input)
-
-    Examples::
-
-        >>> input = torch.randn(20, 5, 10, 10)
-        >>> # With Learnable Parameters
-        >>> m = nn.LayerNorm(input.size()[1:])
-        >>> # Without Learnable Parameters
-        >>> m = nn.LayerNorm(input.size()[1:], elementwise_affine=False)
-        >>> # Normalize over last two dimensions
-        >>> m = nn.LayerNorm([10, 10])
-        >>> # Normalize over last dimension of size 10
-        >>> m = nn.LayerNorm(10)
-        >>> # Activating the module
-        >>> output = m(input)
     """
 
     def __init__(self, normalized_shape, weight, bias, scale, zero_point, eps=1e-5,
