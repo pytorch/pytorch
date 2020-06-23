f652abc1dd653265cc8befa03a21a7ecb5226900
Jerry Zhang <jerryzh@fb.com>
2020-06-23 09:15:35 -0700

    [jit] Enable `copy.deepcopy` and `copy.copy` for RecursiveScriptModule (#32685)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32685
    
    att
    
    Test Plan:
    .
    
    Imported from OSS
    
    Differential Revision: D21220755
    
    fbshipit-source-id: 5c71e9bb9f43032cf60563a9e67579118a8d7e33

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -1895,6 +1895,12 @@ if _enabled:
                 "Mixed serialization of script and non-script modules is not supported. " +
                 "For purely script modules use my_script_module.save(<filename>) instead.")
 
+        def __copy__(self):
+            return torch.jit._recursive.wrap_cpp_module(copy.copy(self._c))
+
+        def __deepcopy__(self, memo):
+            return torch.jit._recursive.wrap_cpp_module(copy.deepcopy(self._c, memo))
+
         # Python magic methods do method lookups on an object's class type, instead of looking up
         # the method defines on the class instance. In order to continue to expose the magic methods
         # of builtin-containers (ModuleList, Sequential, ModuleDict) to python we
