b78a6fd2229e5cbd013f7c7d0de043e3ab263338
Richard Zou <zou3519@gmail.com>
2020-06-25 10:57:05 -0700

    Enable `out_dims` for vmap frontend API
    `out_dims` specifies where in the output tensors the vmapped dimension
    should appear. We implement this by simply creating a view with the
    batch dimension moved to the desired position.
    
    `out_dims` must either:
    - be int (use the same value for all outputs)
    - be Tuple[int] (so the user specifies one out_dim per output).
    (See the vmap docstring for what we advertise out_dims to do).
    
    I also renamed `TestVmap` to `TestVmapAPI` to make it clearer that we
    are testing the API here and not specific operators (which will go into
    their own test class).
    
    Test Plan:
    - `pytest test/test_vmap.py -v`
    
    [ghstack-poisoned]

--- aten/src/ATen/native/Batching.cpp
+++ aten/src/ATen/native/Batching.cpp
@@ -1,4 +1,5 @@
 #include <ATen/BatchedTensorImpl.h>
+#include <ATen/WrapDimUtils.h>
 #include <ATen/VmapTransforms.h>
 
 namespace at { namespace native {
@@ -20,6 +21,66 @@ static bool has_level(const Tensor& self, int64_t level) {
   return it != bdims.end();
 }
 
+// Returns a Tensor with batch dim with level `level` turned into a regular tensor,
+// as well as a logical dim index of where said dimension is in the returned tensor.
+//
+// For example, given
+// self=BatchedTensor([2, 3, 5], bdims=[(lvl=0, dim=1), (lvl=1, dim=2)]), level=1
+// we would return pair(BatchedTensor([2, 3, 5], bdims=[(lvl=0,dim=1)]), 1)
+// because we'd turn the batch dim at (physical) dim 2 into a regular tensor dimension.
+// Said tensor dimension has a logical dim of 1 in the returned Tensor.
+static std::pair<Tensor,int64_t> remove_existing_batch_dim(const Tensor& self, int64_t level) {
+  const auto* batched = maybeGetBatched(self);
+  TORCH_INTERNAL_ASSERT(batched != nullptr);
+  auto bdims = batched->bdims();
+  if (bdims.size() == 1) {
+    TORCH_INTERNAL_ASSERT(bdims[0].level() == level);
+    return std::make_pair(batched->value(), bdims[0].dim());
+  }
+  BatchDims new_bdims;
+  int64_t newly_exposed_physical_dim;
+  new_bdims.reserve(bdims.size() - 1);
+  for (const auto& bdim : bdims) {
+    if (bdim.level() == level) {
+      newly_exposed_physical_dim = bdim.dim();
+    } else {
+      new_bdims.push_back(bdim);
+    }
+  }
+  int64_t num_batch_dims_before_newly_exposed_physical_dim = std::count_if(
+      new_bdims.begin(), new_bdims.end(),
+      [&](const BatchDim& bdim) {
+        return bdim.dim() < newly_exposed_physical_dim;
+      });
+  int64_t newly_exposed_logical_dim =
+      newly_exposed_physical_dim - num_batch_dims_before_newly_exposed_physical_dim;
+  auto result_tensor = makeBatched(batched->value(), std::move(new_bdims));
+  return std::make_pair(std::move(result_tensor), newly_exposed_logical_dim);
+}
+
+// Poor man's version of np.moveaxis. Moves the dimension at `dst` to `src`
+// while preserving the order of other existing dimensions.
+// We should probably add np.moveaxis (it is more general) to PyTorch. (#36048)
+// When we do, replace the following with it.
+static Tensor movedim(const Tensor& self, int64_t src, int64_t dst) {
+  auto logical_dim = self.dim();
+  src = maybe_wrap_dim(src, logical_dim);
+  dst = maybe_wrap_dim(dst, logical_dim);
+  if (src == dst) {
+    return self;
+  }
+  VmapDimVector permutation;
+  permutation.reserve(logical_dim);
+  for (int64_t dim = 0; dim < logical_dim; dim++) {
+    if (dim == src) {
+      continue;
+    }
+    permutation.push_back(dim);
+  }
+  permutation.insert(permutation.begin() + dst, src);
+  return self.permute(permutation);
+}
+
 // Removes the batch dim with level `level` from `self`. If this causes the
 // last batch dim to be removed from a BatchedTensor, then this returns a
 // regular Tensor.
@@ -37,7 +98,6 @@ static bool has_level(const Tensor& self, int64_t level) {
 //
 // `out_dim` controls where we should put the batch dimension in the output tensor.
 Tensor _remove_batch_dim(const Tensor& self, int64_t level, int64_t batch_size, int64_t out_dim) {
-  TORCH_INTERNAL_ASSERT(out_dim == 0);
   if (!has_level(self, level)) {
     auto self_sizes = self.sizes();
     VmapDimVector expanded_sizes(self_sizes.begin(), self_sizes.end());
@@ -45,17 +105,10 @@ Tensor _remove_batch_dim(const Tensor& self, int64_t level, int64_t batch_size, 
     return self.expand(expanded_sizes);
   }
 
-  const auto* batched = maybeGetBatched(self);
-  TORCH_INTERNAL_ASSERT(batched != nullptr);
-  auto bdims = batched->bdims();
-  if (bdims.size() == 1) {
-    return batched->value();
-  }
-  BatchDims new_bdims;
-  new_bdims.reserve(bdims.size() - 1);
-  std::copy_if(bdims.begin(), bdims.end(), std::back_inserter(new_bdims),
-               [&](const BatchDim& bdim) { return bdim.level() != level; });
-  return makeBatched(batched->value(), std::move(new_bdims));
+  Tensor self_without_bdim;
+  int64_t newly_exposed_logical_dim;
+  std::tie(self_without_bdim, newly_exposed_logical_dim) = remove_existing_batch_dim(self, level);
+  return movedim(self_without_bdim, newly_exposed_logical_dim, out_dim);
 }
 
 } // namespace native
