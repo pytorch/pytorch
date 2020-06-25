68b86d427f0d85f2bbecb35cf898ed4219c9c8fb
Sebastian Messmer <messmer@fb.com>
2020-06-25 16:17:17 -0700

    Back out "Revert D21581908: Move TensorOptions ops to c10"
    Differential Revision: [D22247729](https://our.internmc.facebook.com/intern/diff/D22247729/)
    
    [ghstack-poisoned]

--- tools/autograd/gen_autograd.py
+++ tools/autograd/gen_autograd.py
@@ -96,6 +96,26 @@ def get_simple_type(arg):
         simple_type = '{}?'.format(opt_match.group(1))
     return simple_type
 
+def has_tensoroptions_argument(declaration):
+    for argument in declaration['arguments']:
+        if 'TensorOptions' == argument['dynamic_type']:
+            return True
+    return False
+
+def process_schema_order_arg(schema_order_arg):
+    if schema_order_arg == 'dtype':
+        return 'optTypeMetaToScalarType(options.dtype_opt())'
+    elif schema_order_arg == 'layout':
+        return 'options.layout_opt()'
+    elif schema_order_arg == 'device':
+        return 'options.device_opt()'
+    elif schema_order_arg == 'pin_memory':
+        return 'options.pinned_memory_opt()'
+    elif schema_order_arg == 'memory_format':
+        return 'c10::impl::process_memory_format(options, memory_format)'
+    else:
+        return schema_order_arg
+
 
 def load_aten_declarations(path):
     with open(path, 'r') as f:
@@ -114,7 +134,12 @@ def load_aten_declarations(path):
 
         declaration['formals'] = [arg['type'] + ' ' + arg['name']
                                   for arg in declaration['arguments']]
+        declaration['schema_order_formals'] = [arg['type'] + ' ' + arg['name']
+                                               for arg in declaration['schema_order_arguments']]
         declaration['args'] = [arg['name'] for arg in declaration['arguments']]
+        declaration['schema_order_args'] = [arg['name'] for arg in declaration['schema_order_arguments']]
+        if has_tensoroptions_argument(declaration):
+            declaration['schema_order_args'] = [process_schema_order_arg(arg) for arg in declaration['schema_order_args']]
         declaration['api_name'] = declaration['name']
         # NB: keep this in sync with common_with_cwrap.py
         if declaration.get('overload_name'):
