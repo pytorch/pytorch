aa9b88c4e5e9cbbf9b0642852c7e3b98ad6c7074
davinnovation <davinnovation@gmail.com>
2020-06-25 14:56:17 +0900

    Update __init__.py
    fix the condition for checking `_c10d_init` availability

--- torch/distributed/__init__.py
+++ torch/distributed/__init__.py
@@ -15,7 +15,7 @@ def is_available():
     return hasattr(torch._C, "_c10d_init")
 
 
-if is_available() and not torch._C._c10d_init():
+if not (is_available() and torch._C._c10d_init()):
     raise RuntimeError("Failed to initialize torch.distributed")
 
 
