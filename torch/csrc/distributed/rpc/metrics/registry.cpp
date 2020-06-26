cb3f0622e16f7167872f8c833261098009d07b9c
Rohan Varma <rvarm1@fb.com>
2020-06-25 19:07:27 -0700

    Update on "Configure RPC metrics handlers"
    Differential Revision: [D22250592](https://our.internmc.facebook.com/intern/diff/D22250592/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D22250592/)!
    
    [ghstack-poisoned]

--- torch/csrc/distributed/rpc/metrics/registry.cpp
+++ torch/csrc/distributed/rpc/metrics/registry.cpp
@@ -1,11 +1,10 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-#include "RpcMetricsHandler.h"
+#include <torch/csrc/distributed/rpc/metrics/RpcMetricsHandler.h>
 namespace torch {
 namespace distributed {
 namespace rpc {
 C10_DEFINE_REGISTRY(
     RpcMetricsHandlerRegistry,
     torch::distributed::rpc::RpcMetricsHandler);
-}
+} // namespace rpc
 } // namespace distributed
 } // namespace torch
