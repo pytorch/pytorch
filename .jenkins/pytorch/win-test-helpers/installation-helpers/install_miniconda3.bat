61afe16297a829ac6fb728e988da968dd3a22f04
peter <peterghost86@gmail.com>
2020-06-28 14:19:46 +0800

    Unpin the version of ninja and scipy

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_miniconda3.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_miniconda3.bat
@@ -4,12 +4,15 @@ if "%BUILD_ENVIRONMENT%"=="" (
   set CONDA_PARENT_DIR=C:\Jenkins
 )
 if "%REBUILD%"=="" (
-  IF EXIST %CONDA_PARENT_DIR%\Miniconda3 ( rd /s /q %CONDA_PARENT_DIR%\Miniconda3 )
+  if exist %CONDA_PARENT_DIR%\Miniconda3 ( rd /s /q %CONDA_PARENT_DIR%\Miniconda3 )
   curl --retry 3 -k https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
   %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
+  if %errorlevel% neq 0 ( exit /b %errorlevel% )
 )
 call %CONDA_PARENT_DIR%\Miniconda3\Scripts\activate.bat %CONDA_PARENT_DIR%\Miniconda3
 if "%REBUILD%"=="" (
   call conda install -y -q python=%PYTHON_VERSION% numpy cffi pyyaml boto3
+  if %errorlevel% neq 0 ( exit /b %errorlevel% )
   call conda install -y -q -c conda-forge cmake
+  if %errorlevel% neq 0 ( exit /b %errorlevel% )
 )
