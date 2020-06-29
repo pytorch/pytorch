32601ef57459ade966fa406c0e97aa41802c3b61
Jerry Zhang <jerryzh168@gmail.com>
2020-06-29 16:03:43 -0700

    [quant][graphmode] Support quantization for `aten::apend`
    Summary:
    `aten::append` modifies input inplace and the output is ignored, these ops are not
    supported right now, so we'll need to first make `aten::append` non-inplace
    by change
    ```
    ignored = aten::append(list, x)
    ```
    to
    ```
    x_list = aten::ListConstruct(x)
    result = aten::add(list, x_list)
    ```
    and then quantize the aten::add instead.
    
    Test Plan:
    
    TestQuantizeJitOps.test_general_shape_ops
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/quantization/test_quantize_jit.py
+++ test/quantization/test_quantize_jit.py
@@ -2335,10 +2335,12 @@ class TestQuantizeJitOps(QuantizationTestCase):
         # observers and also successfully fused two quantized::conv2d
         # patterns
         # one quantize_per_tensor for input
-        # TODO: the checks are problematic, we need to split all checks
+        print(m.graph)
         FileCheck().check_count("aten::quantize_per_tensor", 1, exactly=True) \
-                   .check_count("quantized::conv2d", 2, exactly=True) \
-                   .check("aten::dequantize") \
+                   .run(m.graph)
+        FileCheck().check_count("quantized::conv2d", 2, exactly=True) \
+                   .run(m.graph)
+        FileCheck().check_count("aten::dequantize", 1, exactly=True) \
                    .run(m.graph)
 
         FileCheck().check("quantized::add_scalar") \
