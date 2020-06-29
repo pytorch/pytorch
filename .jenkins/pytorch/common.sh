5f52fa622a308d856d2b21f353186a5cab7b817e
Nikita Shulga <nshulga@fb.com>
2020-06-29 10:54:27 -0700

    Tweak `file_diff_from_base` for release/1.6 branch

--- .jenkins/pytorch/common.sh
+++ .jenkins/pytorch/common.sh
@@ -185,9 +185,9 @@ function get_exit_code() {
 function file_diff_from_base() {
   # The fetch may fail on Docker hosts, but it's not always necessary.
   set +e
-  git fetch origin master --quiet
+  git fetch origin release/1.6 --quiet
   set -e
-  git diff --name-only "$(git merge-base origin/master HEAD)" > "$1"
+  git diff --name-only "$(git merge-base origin/release/1.6 HEAD)" > "$1"
 }
 
 function get_bazel() {
