bb55f834f77adc9cbf13f12fd139064d8b134c7c
Luca Wehrstedt <lcw@fb.com>
2020-06-30 13:55:17 -0700

    [PATCH] [RPC tests] Remove world_size and init_method from TensorPipe fixture
    This prepares the stack by simplifying the TensorPipe fixture. A comment says that the TensorPipe fixture cannot subclass the generic fixture class as that would lead to a diamond class hierarchy which Python doesn't support (whereas in fact it does), and therefore it copies over two properties that are defined on the generic fixture. However, each class that uses the TensorPipe fixture also inherits from the generic fixture, so there's no need to redefine those properties. And, in fact, by not redefining it we save ourselves some trouble when the TensorPipe fixture would end up overriding another override.

--- test/distributed/rpc/tensorpipe/test_ddp_under_dist_autograd.py
+++ test/distributed/rpc/tensorpipe/test_ddp_under_dist_autograd.py
@@ -16,10 +16,6 @@ import torch.distributed.rpc as rpc
 )
 class TestDdpUnderDistAutogradTensorPipe(TensorPipeRpcAgentTestFixture, ddp_under_dist_autograd_test.TestDdpUnderDistAutograd):
 
-    @property
-    def world_size(self) -> int:
-        return ddp_under_dist_autograd_test.WORLD_SIZE
-
     @dist_init
     def test_verify_backend_options(self):
         self.assertEqual(self.rpc_backend, rpc.backend_registry.BackendType.TENSORPIPE)
@@ -28,10 +24,6 @@ class TestDdpUnderDistAutogradTensorPipe(TensorPipeRpcAgentTestFixture, ddp_unde
     TEST_WITH_ASAN, "Skip ASAN as torch + multiprocessing spawn have known issues"
 )
 class TestDdpComparisonTensorPipe(TensorPipeRpcAgentTestFixture, ddp_under_dist_autograd_test.TestDdpComparison):
-
-    @property
-    def world_size(self) -> int:
-        return ddp_under_dist_autograd_test.WORLD_SIZE
 
     @dist_init
     def test_verify_backend_options(self):
