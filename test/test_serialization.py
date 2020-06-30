e7ce02f0c8ee50eb78aedea61e5ddd54da3065d5
James Reed <jamesreed@fb.com>
2020-06-29 19:14:29 -0700

    Fix zip serialization for file > 2GiB (#40722)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/40722
    
    Test Plan: Imported from OSS
    
    Differential Revision: D22294016
    
    Pulled By: jamesr66a
    
    fbshipit-source-id: 0288882873d4b59bdef37d018c030519c4be7f03

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -673,6 +673,18 @@ class TestSerialization(TestCase, SerializationMixin):
 
         test(io.BytesIO())
 
+    # Ensure large zip64 serialization works properly
+    @unittest.skipIf(IS_WINDOWS,
+                     '<built-in method read of BytesIOContext object at 0x0000022C21F2B468> returned a result'
+                     ' with an error set')
+    def test_serialization_2gb_file(self):
+        big_model = torch.nn.Conv2d(20000, 3200, kernel_size=3)
+
+        with BytesIOContext() as f:
+            torch.save(big_model, f)
+            f.seek(0)
+            state = torch.load(f)
+
     def run(self, *args, **kwargs):
         with serialization_method(use_zip=True):
             return super(TestSerialization, self).run(*args, **kwargs)
