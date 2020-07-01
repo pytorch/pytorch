pytorch/pytorch

commit 145df306aee0884810aa7660db8e46b1daf90bf7

Avoid using default process group in ProcessGroupAgent. (#39909)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/39909

As described in https://github.com/pytorch/pytorch/issues/33583,
ProcessGroupAgent initializes the default process group and this causes issues
if the user initializes the default process group themsleves. Either the RPC
initialization would fail or the user's process group initialization would
fail.

To avoid this, I've changed ProcessGroupAgent init to create its own
ProcessGroupGloo and not use the default one at all.

Closes: https://github.com/pytorch/pytorch/issues/33583
ghstack-source-id: 105953303

Test Plan: waitforbuildbot

Differential Revision: D22011868

fbshipit-source-id: 7346a3fcb2821a0bc08e0bdc0625947abb5ae16f

torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -2942,6 +2942,56 @@ class RpcTest(RpcAgentTestFixture):
 
         wait_until_owners_and_forks_on_rank(1, 1, rank=1)
 
+    @dist_init(setup_rpc=False)
+    def test_init_pg_then_rpc(self):
+        dist.init_process_group(
+            backend="gloo",
+            init_method=self.init_method,
+            rank=self.rank,
+            world_size=self.world_size,
+        )
+
+        rpc.init_rpc(
+            name=worker_name(self.rank),
+            backend=self.rpc_backend,
+            rank=self.rank,
+            world_size=self.world_size,
+            rpc_backend_options=self.rpc_backend_options,
+        )
+
+        # Test RPC.
+        next_rank = (self.rank + 1) % self.world_size
+        ret = rpc.rpc_sync(worker_name(next_rank), torch.add, args=(torch.ones(2, 2), 1))
+        self.assertEqual(ret, torch.ones(2, 2) + 1)
+
+        # Test PG
+        dist.barrier()
+
+    @dist_init(setup_rpc=False)
+    def test_init_rpc_then_pg(self):
+        rpc.init_rpc(
+            name=worker_name(self.rank),
+            backend=self.rpc_backend,
+            rank=self.rank,
+            world_size=self.world_size,
+            rpc_backend_options=self.rpc_backend_options,
+        )
+
+        dist.init_process_group(
+            backend="gloo",
+            init_method=self.init_method,
+            rank=self.rank,
+            world_size=self.world_size,
+        )
+
+        # Test RPC.
+        next_rank = (self.rank + 1) % self.world_size
+        ret = rpc.rpc_sync(worker_name(next_rank), torch.add, args=(torch.ones(2, 2), 1))
+        self.assertEqual(ret, torch.ones(2, 2) + 1)
+
+        # Test PG
+        dist.barrier()
+
 
 class FaultyAgentRpcTest(FaultyRpcAgentTestFixture):
 

test/distributed/test_ddp_under_dist_autograd.py
@@ -4,6 +4,7 @@ from typing import NamedTuple
 import enum
 import logging
 import os
+import threading
 
 import torch
 from torch.distributed import rpc
@@ -17,14 +18,13 @@ from torch.testing._internal.common_utils import (
     TEST_WITH_ASAN,
 )
 from torch.testing._internal.dist_utils import dist_init
-from torch._utils_internal import TEST_MASTER_ADDR as MASTER_ADDR
-from torch._utils_internal import TEST_MASTER_PORT as MASTER_PORT
 import torch.distributed as dist
 import torch.distributed.autograd as dist_autograd
-import torch.distributed.distributed_c10d as dist_c10d
 import torch.nn as nn
 import unittest
-
+from torch.testing._internal.distributed.rpc.rpc_agent_test_fixture import (
+    RpcAgentTestFixture,
+)
 
 NUM_EM_ROW = 2
 D_SPARSE = 3
@@ -157,8 +157,8 @@ class HybridModel(nn.Module):
             gLogger.info("Use DDP for the second local net.")
             self.fc2 = DistributedDataParallel(
                 self.fc2,
-                process_group=process_group_for_ddp,
                 check_reduction=True,
+                process_group=process_group_for_ddp,
             )
 
         gLogger.info(
@@ -187,17 +187,14 @@ class Trainer:
         ddp_mode: DdpMode,
         rank: int,
     ):
-        gLogger.info(
-            f"Initing trainer process group by trainer #{rank} with ranks {TRAINER_RANKS}"
-        )
-        self.process_group_for_ddp = dist_c10d.new_group(ranks=TRAINER_RANKS)
-
+        self.rank = rank
+        self.trainer_group = dist.new_group(TRAINER_RANKS)
         self.remote_em_rref = remote_em_rref
         self.remote_net_rref = remote_net_rref
         self.hybrid_module = HybridModel(
             self.remote_em_rref,
             self.remote_net_rref,
-            self.process_group_for_ddp
+            self.trainer_group
             if ddp_mode in (DdpMode.INSIDE,)
             else None,
         )
@@ -211,8 +208,8 @@ class Trainer:
             self.non_ddp_params = ()
             self.hybrid_module = DistributedDataParallel(
                 self.hybrid_module,
-                process_group=self.process_group_for_ddp,
                 check_reduction=True,
+                process_group=self.trainer_group,
             )
         gLogger.info(
             f"Succeeded in creating a HybridModel instance with "
@@ -220,8 +217,8 @@ class Trainer:
             f"other local params."
         )
 
-    def __del__(self):
-        dist.destroy_process_group(self.process_group_for_ddp)
+    def destroy_pg(self):
+        dist.destroy_process_group(self.trainer_group)
 
     def train_batch(self, mini_batch: FeatureSet):
         grads_dict = None
@@ -280,13 +277,18 @@ def get_training_examples():
     ]
 
 
+shutdown_signal = threading.Condition()
+
+def set_shutdown_signal():
+    global shutdown_signal
+    with shutdown_signal:
+        shutdown_signal.notify()
+
 @unittest.skipIf(
     TEST_WITH_ASAN,
     "Skip ASAN as torch + multiprocessing spawn have known issues",
 )
-class TestDdpUnderDistAutograd(MultiProcessTestCase):
-    rpc_backend = rpc.backend_registry.BackendType.PROCESS_GROUP
-    rpc_backend_options = None
+class TestDdpUnderDistAutograd(MultiProcessTestCase, RpcAgentTestFixture):
 
     @property
     def world_size(self) -> int:
@@ -303,25 +305,50 @@ class TestDdpUnderDistAutograd(MultiProcessTestCase):
     def setUp(self):
         super(TestDdpUnderDistAutograd, self).setUp()
 
-        os.environ["MASTER_ADDR"] = str(MASTER_ADDR)
-        os.environ["MASTER_PORT"] = str(MASTER_PORT)
         self._spawn_processes()
 
     def tearDown(self):
         super(TestDdpUnderDistAutograd, self).tearDown()
 
     def _remote_worker_process(self):
-        process_group_for_ddp = dist_c10d.new_group(ranks=TRAINER_RANKS)
         gLogger.info("The remote worker is running.")
-        dist.destroy_process_group(process_group_for_ddp)
+        dist.init_process_group(
+            backend="gloo",
+            init_method="file://{}".format(self.file_name),
+            world_size=self.world_size,
+            rank=self.rank)
+        global shutdown_signal
+        with shutdown_signal:
+            shutdown_signal.wait()
         gLogger.info("Exiting remote worker.")
+        dist.destroy_process_group()
 
     def _trainer_process(self, rank: int):
         gLogger.info(f"Running the trainer #{rank}...")
+        gLogger.info(
+            f"Initing trainer process group by trainer #{rank} with ranks {TRAINER_RANKS}"
+        )
+        dist.init_process_group(
+            backend="gloo",
+            init_method="file://{}".format(self.file_name),
+            world_size=self.world_size,
+            rank=self.rank)
+
+        gLogger.info(f"Waiting for shutdown signal on trainer #{rank}...")
+
+        global shutdown_signal
+        with shutdown_signal:
+            shutdown_signal.wait()
+        gLogger.info(f"Exiting the trainer #{rank}...")
+        dist.destroy_process_group()
 
     def _master_process(self, ddp_mode: DdpMode):
         gLogger.info("Running the master process...")
-        process_group_for_ddp = dist_c10d.new_group(ranks=TRAINER_RANKS)
+        dist.init_process_group(
+            backend="gloo",
+            init_method="file://{}".format(self.file_name),
+            world_size=self.world_size,
+            rank=self.rank)
         remote_em_rref = rpc.remote(
             self.remote_worker_name(), RemoteEM, args=(NUM_EM_ROW, D_SPARSE)
         )
@@ -332,7 +359,6 @@ class TestDdpUnderDistAutograd(MultiProcessTestCase):
         )
         gLogger.info("Created remote rrefs on master")
         self.do_test_on_master(ddp_mode, remote_em_rref, remote_net_rref)
-        dist.destroy_process_group(process_group_for_ddp)
 
     def do_test_on_master(
         self,
@@ -379,6 +405,20 @@ class TestDdpUnderDistAutograd(MultiProcessTestCase):
                         msg="The grad for any non-ddp parameter shouldn't be zeros",
                     )
 
+        # Destroy process groups
+        for idx, trainer_rref in enumerate(trainer_rrefs):
+            _remote_method_async(
+                Trainer.destroy_pg,
+                trainer_rref,
+            ).wait()
+
+        # Send shutdown signals.
+        for rank in TRAINER_RANKS:
+            trainer = self.trainer_name(rank)
+            rpc.rpc_sync(trainer, set_shutdown_signal, args=())
+
+        rpc.rpc_sync(self.remote_worker_name(), set_shutdown_signal, args=())
+
     def _do_test(self, ddp_mode):
         if self.rank == MASTER_RANK:
             self._master_process(ddp_mode)
@@ -409,9 +449,7 @@ class TestDdpUnderDistAutograd(MultiProcessTestCase):
     TEST_WITH_ASAN,
     "Skip ASAN as torch + multiprocessing spawn have known issues",
 )
-class TestDdpComparison(MultiProcessTestCase):
-    rpc_backend = rpc.backend_registry.BackendType.PROCESS_GROUP
-    rpc_backend_options = None
+class TestDdpComparison(MultiProcessTestCase, RpcAgentTestFixture):
 
     @property
     def world_size(self) -> int:
@@ -424,8 +462,6 @@ class TestDdpComparison(MultiProcessTestCase):
     def setUp(self):
         super(TestDdpComparison, self).setUp()
 
-        os.environ["MASTER_ADDR"] = str(MASTER_ADDR)
-        os.environ["MASTER_PORT"] = str(MASTER_PORT)
         self._spawn_processes()
 
     def tearDown(self):
@@ -440,10 +476,14 @@ class TestDdpComparison(MultiProcessTestCase):
         # therefore grads. That means the grads will be the same before and
         # after DDP's all-reduce.
         torch.manual_seed(self.rank)
-        process_group_for_ddp = dist_c10d.new_group(ranks=TRAINER_RANKS)
+        dist.init_process_group(
+            backend="gloo",
+            init_method="file://{}".format(self.file_name),
+            world_size=self.world_size,
+            rank=self.rank)
         net = nn.Linear(2, 3)
         ddp_net = DistributedDataParallel(
-            net, process_group=process_group_for_ddp
+            net
         )
         inputs = torch.rand((3, 2))
 
@@ -471,7 +511,7 @@ class TestDdpComparison(MultiProcessTestCase):
                 msg=f"The grads for param {param} are different under local "
                 f"and dist autograd: {param.grad} \n---\n {grads_dict[param]}",
             )
-        dist.destroy_process_group(process_group_for_ddp)
+        dist.destroy_process_group()
 
 
 if __name__ == "__main__":

torch/distributed/rpc/__init__.py
@@ -35,9 +35,7 @@ if is_available():
         and distributed autograd.
 
         Initializes the local RPC agent which immediately makes the current
-        process ready to send and receive RPCs. This method also properly
-        initializes a default process group backend that uses Gloo for
-        communication.
+        process ready to send and receive RPCs.
 
         Arguments:
             backend (Enum): type of RPC backend implementation. Currently,

torch/distributed/rpc/backend_registry.py
@@ -5,7 +5,6 @@ from datetime import timedelta
 import enum
 
 import torch.distributed as dist
-import torch.distributed.distributed_c10d as dc10d
 
 from . import constants as rpc_constants
 
@@ -99,52 +98,42 @@ def _process_group_construct_rpc_backend_options_handler(
         num_send_recv_threads=num_send_recv_threads
     )
 
+def _init_process_group(store, rank, world_size):
+    # Initialize ProcessGroup.
+    process_group_timeout = rpc_constants.DEFAULT_PROCESS_GROUP_TIMEOUT
+
+    # We're using a bunch of private APIs here since `new_group` requires the
+    # default group to be initialized.
+    group = dist.ProcessGroupGloo(store, rank, world_size, process_group_timeout)
+
+    assert group is not None, "Failed to initialize default ProcessGroup."
+
+    if (rank != -1) and (rank != group.rank()):
+        raise RuntimeError(
+            "rank argument {} doesn't match pg rank {}".format(rank, group.rank())
+        )
+    if (world_size != -1) and (world_size != group.size()):
+        raise RuntimeError(
+            "world_size argument {} doesn't match pg size {}".format(
+                world_size, group.size()
+            )
+        )
+    return group
 
 def _process_group_init_backend_handler(
     store, name, rank, world_size, rpc_backend_options
 ):
     from . import ProcessGroupAgent
 
-    # Initialize ProcessGroup.
-    if dist.is_initialized():
-        raise RuntimeError(
-            "Default process group must not be initialized before init_rpc."
-        )
+    group = _init_process_group(store, rank, world_size)
 
-    process_group_timeout = rpc_constants.DEFAULT_PROCESS_GROUP_TIMEOUT
-
-    dist.init_process_group(
-        backend=dist.Backend.GLOO,
-        store=store,
-        rank=rank,
-        world_size=world_size,
-        timeout=process_group_timeout,
+    # TODO: add try-except and destroy _agent in all processes if any fails.
+    return ProcessGroupAgent(
+        name,
+        group,
+        rpc_backend_options.num_send_recv_threads,
+        timedelta(seconds=rpc_backend_options.rpc_timeout),
     )
-
-    try:
-        group = dc10d._get_default_group()
-        assert group is not None, "Failed to initialize default ProcessGroup."
-
-        if (rank != -1) and (rank != group.rank()):
-            raise RuntimeError(
-                "rank argument {} doesn't match pg rank {}".format(rank, group.rank())
-            )
-        if (world_size != -1) and (world_size != group.size()):
-            raise RuntimeError(
-                "world_size argument {} doesn't match pg size {}".format(
-                    world_size, group.size()
-                )
-            )
-        # TODO: add try-except and destroy _agent in all processes if any fails.
-        return ProcessGroupAgent(
-            name,
-            group,
-            rpc_backend_options.num_send_recv_threads,
-            timedelta(seconds=rpc_backend_options.rpc_timeout),
-        )
-    except Exception as ex:
-        dist.destroy_process_group()
-        raise ex
 
 
 register_backend(
@@ -186,43 +175,12 @@ def _tensorpipe_init_backend_handler(store, name, rank, world_size, rpc_backend_
     # collective operations, for which it relies on a process group, instead of
     # re-implementing this on top of RPCs.
 
-    # Initialize ProcessGroup.
-    if dist.is_initialized():
-        raise RuntimeError(
-            "Default process group must not be initialized before init_rpc."
-        )
+    group = _init_process_group(store, rank, world_size)
 
-    process_group_timeout = rpc_constants.DEFAULT_PROCESS_GROUP_TIMEOUT
-
-    dist.init_process_group(
-        backend=dist.Backend.GLOO,
-        store=store,
-        rank=rank,
-        world_size=world_size,
-        timeout=process_group_timeout,
+    # TODO: add try-except and destroy _agent in all processes if any fails.
+    return TensorPipeAgent(
+        store, name, rank, world_size, group, rpc_backend_options
     )
-
-    try:
-        group = dc10d._get_default_group()
-        assert group is not None, "Failed to initialize default ProcessGroup."
-
-        if (rank != -1) and (rank != group.rank()):
-            raise RuntimeError(
-                "rank argument {} doesn't match pg rank {}".format(rank, group.rank())
-            )
-        if (world_size != -1) and (world_size != group.size()):
-            raise RuntimeError(
-                "world_size argument {} doesn't match pg size {}".format(
-                    world_size, group.size()
-                )
-            )
-        # TODO: add try-except and destroy _agent in all processes if any fails.
-        return TensorPipeAgent(
-            store, name, rank, world_size, group, rpc_backend_options
-        )
-    except Exception as ex:
-        dist.destroy_process_group()
-        raise ex
 
 
 register_backend(

commit 84799a7755f2dfdedf3cde5df14337f15ff39284

[RPC tests] Fix @_skip_if_tensorpipe always skipping for all agents
It turns out that the `@_skip_if_tensorpipe_agent` decorator was written in such a way that it accidentally caused the test to become a no-op (and thus always succeed) for all agents. What this means is that all tests wrapped by that decorator were never ever being run, for any agent.

My understanding of the root cause is that the following code:
```
@_skip_if_tensorpipe_agent
def test_foo(self):
    self.assertEqual(2 + 2, 4)
```
ended up behaving somewhat like this:
```
def test_foo(self):
    def original_test_func(self):
        self.assertEqual(2 + 2, 4)
    return unittest.skipIf(self.agent == "TENSORPIPE")(original_test_func)
```
which means that the test body of the decorated method was not actually calling the original test method.

This issue probably came from the `@_skip_if_tensorpipe_agent` being copy-pasted from `@requires_process_group_agent` (which, however, is not a decorator but rather a decorator *factory*). An unfortunate naming (calling `decorator` what was in fact the wrapped method) then hindered readability and hid the issue.

Note that a couple of tests had become legitimately broken in the meantime and no one had noticed. The breakages have been introduced in #39909 (a.k.a., D22011868).

Differential Revision: [D22332611](https://our.internmc.facebook.com/intern/diff/D22332611/)

[ghstack-poisoned]

torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -6,7 +6,7 @@ from threading import Lock
 import time
 import unittest
 from collections import namedtuple
-from functools import partial
+from functools import partial, wraps
 from unittest import mock
 
 import torch
@@ -472,14 +472,18 @@ load_tests = load_tests
 
 
 class RpcTest(RpcAgentTestFixture):
-    def _skip_if_tensorpipe_agent(old_func):  # noqa: B902
-        def decorator(self):
-            return unittest.skipIf(
-                self.rpc_backend == rpc.backend_registry.BackendType.TENSORPIPE,
-                "This test is not yet supported in the Tensorpipe Agent"
-            )(old_func)
+    def _skip_if_tensorpipe_agent(test_func):  # noqa: B902
+        @wraps(test_func)
+        def wrapped_test_func(self):
+            if self.rpc_backend == rpc.backend_registry.BackendType.TENSORPIPE:
+                # In theory we should call self.skipTest(reason), but this does
+                # not play well with the test runner, which sees an exception
+                # being raised and marks the test as failed. Thus instead we
+                # make the test a successful no-op.
+                return
+            test_func(self)
 
-        return decorator
+        return wrapped_test_func
 
     @dist_init
     def test_worker_id(self):
@@ -2045,6 +2049,14 @@ class RpcTest(RpcAgentTestFixture):
     @requires_process_group_agent("PROCESS_GROUP rpc backend specific test, skip")
     @_skip_if_tensorpipe_agent
     def test_single_threaded_rref_owner(self):
+        # We need a process group in order to perform a barrier at the end.
+        dist.init_process_group(
+            backend="gloo",
+            init_method=self.init_method,
+            rank=self.rank,
+            world_size=self.world_size,
+        )
+
         # This test aims to verify if the server can handle all internal RPC
         # messages using just one thread.
         caller_rank = 0
@@ -2110,6 +2122,14 @@ class RpcTest(RpcAgentTestFixture):
     @requires_process_group_agent("PROCESS_GROUP rpc backend specific test, skip")
     @_skip_if_tensorpipe_agent
     def test_single_threaded_rref_to_here(self):
+        # We need a process group in order to perform a barrier at the end.
+        dist.init_process_group(
+            backend="gloo",
+            init_method=self.init_method,
+            rank=self.rank,
+            world_size=self.world_size,
+        )
+
         # This test aims to verify if the server can handle all internal RPC
         # messages using just one thread.
         caller_rank = 0