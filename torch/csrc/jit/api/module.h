4dd37bfbf7373fad4fa3bf04c4982673207dc9c0
Jerry Zhang <jerryzh@fb.com>
2020-07-02 10:22:27 -0700

    [jit] Remove unnecessary clone APIs for script::Module and RecursiveScriptModule (#40297) (#40748)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/40297
    
    Test Plan: Imported from OSS
    
    Differential Revision: D22191660
    
    fbshipit-source-id: 4b338ca82caaca04784bffe01fdae3d180c192f4

--- torch/csrc/jit/api/module.h
+++ torch/csrc/jit/api/module.h
@@ -237,10 +237,6 @@ struct TORCH_API Module : public Object {
   // will be preserved as well
   Module clone(bool inplace = false) const;
 
-  // Clones the module instance but shares the underlying type with the
-  // the current instance, it doesn't create new `ClassType`
-  Module clone_instance() const;
-
   void clone_method(const Module& orig, const std::string& name);
 
   template <typename... Types>
