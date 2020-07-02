a888941992c70edad1e94e9e729355e3d9aa2ee1
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-07-02 14:52:52 -0700

    add buildParamMap func

--- torch/csrc/jit/passes/onnx/initializer_based_peephole.cpp
+++ torch/csrc/jit/passes/onnx/initializer_based_peephole.cpp
@@ -116,6 +116,15 @@ static void fuseConvBatchNorm(Block* b, ValueToParamPairMap& valsToParamsMap) {
   }
 }
 
+void buildParamsMapFromValueToParamsMap(
+    const ValueToParamPairMap& valsToParamsMap,
+    ParamMap& paramsDict) {
+  paramsDict.clear();
+  for (const auto& nameTensorParamPair : valsToParamsMap) {
+    paramsDict.insert(nameTensorParamPair.second);
+  }
+}
+
 void InitializerPeepholeONNX(Block* b, ParamMap& paramsDict) {
   auto valsToParamsMap = buildValueToParamsMap(b, paramsDict);
   fuseConvBatchNorm(b, valsToParamsMap);
