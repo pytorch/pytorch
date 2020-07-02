ad37336351a6637309d1fda3c578087a8ef39054
Yi Huang (Feed) <huangyi@fb.com>
2020-07-02 14:44:07 -0700

    Migrate 'torch.dot' from TH to Aten (CUDA) (#40646)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40646
    
    Support double, float, at::Half.
    Avoid creating output result on CPU.
    
    Both of two tensors must be on GPU.
    
    Reviewed By: ngimel
    
    Differential Revision: D22258840
    
    fbshipit-source-id: 95f4747477f09b40b1d682cd1f76e4c2ba28c452

--- aten/src/ATen/cuda/CUDABlas.h
+++ aten/src/ATen/cuda/CUDABlas.h
@@ -7,8 +7,10 @@
 
     gemv<Dtype>(transa, m, n, alpha, a, lda, x, incx, beta, y, incy)
 
-  where Dtype is double, float, at::Half or at::BFloat16(ROCm). The functions are
-  available in at::cuda::blas namespace.
+    dot<Dtype>(n, x, incx, y, incy, result)
+
+  where Dtype is double, float, at::Half or at::BFloat16 (ROCm, NOT for dot).
+  The functions are available in at::cuda::blas namespace.
  */
 
 #include <ATen/cuda/CUDAContext.h>
@@ -19,10 +21,10 @@ namespace blas {
 
 /* LEVEL 3 BLAS FUNCTIONS */
 
-#define CUDABLAS_GEMM_ARGTYPES(Dtype)                                      \
-      char transa, char transb, int64_t m, int64_t n,                      \
-      int64_t k, Dtype alpha, const Dtype *a, int64_t lda, const Dtype *b, \
-      int64_t ldb, Dtype beta, Dtype *c, int64_t ldc
+#define CUDABLAS_GEMM_ARGTYPES(Dtype)                                       \
+  char transa, char transb, int64_t m, int64_t n, int64_t k, Dtype alpha,   \
+      const Dtype *a, int64_t lda, const Dtype *b, int64_t ldb, Dtype beta, \
+      Dtype *c, int64_t ldc
 
 template <typename Dtype>
 inline void gemm(CUDABLAS_GEMM_ARGTYPES(Dtype)) {
@@ -50,10 +52,9 @@ void gemm<at::BFloat16>(CUDABLAS_GEMM_ARGTYPES(at::BFloat16));
 
 /* LEVEL 2 BLAS FUNCTIONS */
 
-#define CUDABLAS_GEMV_ARGTYPES(Dtype)                                        \
-      char trans, int64_t m, int64_t n, Dtype alpha,                         \
-      const Dtype *a, int64_t lda, const Dtype *x, int64_t incx, Dtype beta, \
-      Dtype *y, int64_t incy
+#define CUDABLAS_GEMV_ARGTYPES(Dtype)                                         \
+  char trans, int64_t m, int64_t n, Dtype alpha, const Dtype *a, int64_t lda, \
+      const Dtype *x, int64_t incx, Dtype beta, Dtype *y, int64_t incy
 
 template <typename Dtype>
 inline void gemv(CUDABLAS_GEMV_ARGTYPES(Dtype)) {
@@ -65,10 +66,10 @@ void gemv<double>(CUDABLAS_GEMV_ARGTYPES(double));
 template <>
 void gemv<float>(CUDABLAS_GEMV_ARGTYPES(float));
 #ifndef __HIP_PLATFORM_HCC__
-  template <>
-  void gemv<c10::complex<double>>(CUDABLAS_GEMV_ARGTYPES(c10::complex<double>));
-  template <>
-  void gemv<c10::complex<float>>(CUDABLAS_GEMV_ARGTYPES(c10::complex<float>));
+template <>
+void gemv<c10::complex<double>>(CUDABLAS_GEMV_ARGTYPES(c10::complex<double>));
+template <>
+void gemv<c10::complex<float>>(CUDABLAS_GEMV_ARGTYPES(c10::complex<float>));
 #endif
 template <>
 void gemv<at::Half>(CUDABLAS_GEMV_ARGTYPES(at::Half));
@@ -77,8 +78,35 @@ template <>
 void gemv<at::BFloat16>(CUDABLAS_GEMV_ARGTYPES(at::BFloat16));
 #endif
 
-template<typename Dtype>
-void ger(int64_t m, int64_t n, Dtype alpha, Dtype *x, int64_t incx, Dtype *y, int64_t incy, Dtype *a, int64_t lda);
+template <typename Dtype>
+void ger(
+    int64_t m,
+    int64_t n,
+    Dtype alpha,
+    Dtype* x,
+    int64_t incx,
+    Dtype* y,
+    int64_t incy,
+    Dtype* a,
+    int64_t lda);
+
+/* LEVEL 1 BLAS FUNCTIONS */
+
+#define CUDABLAS_DOT_ARGTYPES(Dtype)                                      \
+  cublasHandle_t handle, int n, const Dtype *x, int incx, const Dtype *y, \
+      int incy, Dtype *result
+
+template <typename Dtype>
+inline void dot(CUDABLAS_DOT_ARGTYPES(Dtype)) {
+  AT_ERROR("at::cuda::blas::dot: not implemented for ", typeid(Dtype).name());
+}
+
+template <>
+void dot<double>(CUDABLAS_DOT_ARGTYPES(double));
+template <>
+void dot<float>(CUDABLAS_DOT_ARGTYPES(float));
+template <>
+void dot<at::Half>(CUDABLAS_DOT_ARGTYPES(at::Half));
 
 } // namespace blas
 } // namespace cuda
