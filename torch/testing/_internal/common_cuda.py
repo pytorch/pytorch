0d6be4b54cfca3f96773a5094938319edc46ed2f
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-07-06 16:33:31 -0700

    check cuda version in tf32_is_not_fp32

--- torch/testing/_internal/common_cuda.py
+++ torch/testing/_internal/common_cuda.py
@@ -42,6 +42,8 @@ def tf32_is_not_fp32():
         return False
     if torch.cuda.get_device_properties(torch.cuda.current_device()).major < 8:
         return False
+    if int(torch.version.cuda.split('.')[0]) < 11:
+        return False
     return True
 
 
