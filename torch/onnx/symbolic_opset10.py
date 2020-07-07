14e276b3210900875296e334d95d94a550fdff5e
Hao Wu <skyw@nvidia.com>
2020-07-06 17:10:41 -0700

    change fake quant op boundary check to runtime error

--- torch/onnx/symbolic_opset10.py
+++ torch/onnx/symbolic_opset10.py
@@ -181,9 +181,8 @@ def fmod(g, input, other):
 
 @parse_args('v', 't', 'i', 'i', 'i')
 def fake_quantize_per_tensor_affine(g, inputs, scale, zero_point, quant_min=-128, quant_max=127):
-    if quant_min not in [0, -128] or quant_max not in [127, 255]:
-        _unimplemented(
-            "fake_quantize_per_tensor_affine",
+    if quant_min not in [0, -127] or quant_max not in [127, 255]:
+        raise RuntimeError(
             "ONNX defines [0, 255] for quint8 and [-128, 127] for qint8, got [{}, {}]".format(quant_min, quant_max))
     scale = scale.float().data  # Avoid exporter generating double type
     zero_point_dtype = torch.int8 if quant_min == -128 else torch.uint8
