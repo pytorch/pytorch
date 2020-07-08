42356a99714d8f6d440caf8055c78a76a196e486
xueht-fnst <xueht.fnst@cn.fujitsu.com>
2020-07-08 16:43:43 +0800

    improve test case & adding values for bfloat16

--- test/test_type_info.py
+++ test/test_type_info.py
@@ -14,11 +14,11 @@ if TEST_NUMPY:
 class TestDTypeInfo(TestCase):
 
     def test_invalid_input(self):
-        for dtype in [torch.float16, torch.float32, torch.float64]:
+        for dtype in [torch.float16, torch.float32, torch.float64, torch.bfloat16, torch.complex64, torch.complex128, torch.bool]:
             with self.assertRaises(TypeError):
                 _ = torch.iinfo(dtype)
 
-        for dtype in [torch.int64, torch.int32, torch.int16, torch.int8, torch.uint8]:
+        for dtype in [torch.int64, torch.int32, torch.int16, torch.int8, torch.uint8, torch.bool]:
             with self.assertRaises(TypeError):
                 _ = torch.finfo(dtype)
 
@@ -32,6 +32,7 @@ class TestDTypeInfo(TestCase):
             self.assertEqual(xinfo.bits, xninfo.bits)
             self.assertEqual(xinfo.max, xninfo.max)
             self.assertEqual(xinfo.min, xninfo.min)
+            self.assertEqual(xinfo.dtype, xninfo.dtype)
 
     @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
     def test_finfo(self):
@@ -47,9 +48,24 @@ class TestDTypeInfo(TestCase):
             self.assertEqual(xinfo.eps, xninfo.eps)
             self.assertEqual(xinfo.tiny, xninfo.tiny)
             self.assertEqual(xinfo.resolution, xninfo.resolution)
+            self.assertEqual(xinfo.dtype, xninfo.dtype)
             if not dtype.is_complex:
                 torch.set_default_dtype(dtype)
                 self.assertEqual(torch.finfo(dtype), torch.finfo())
+
+        # Special test case for BFloat16 type
+        x = torch.zeros((2, 2), dtype=torch.bfloat16)
+        xinfo = torch.finfo(x.dtype)
+        self.assertEqual(xinfo.bits, 16)
+        self.assertEqual(xinfo.max, 3.38953e+38)
+        self.assertEqual(xinfo.min, -3.38953e+38)
+        self.assertEqual(xinfo.eps, 0.0078125)
+        self.assertEqual(xinfo.tiny, 1.17549e-38)
+        self.assertEqual(xinfo.resolution, 0.01)
+        self.assertEqual(xinfo.dtype, "bfloat16")
+        torch.set_default_dtype(x.dtype)
+        self.assertEqual(torch.finfo(x.dtype), torch.finfo())
+
         # Restore the default type to ensure that the test has no side effect
         torch.set_default_dtype(initial_default_type)
 
--- torch/csrc/TypeInfo.cpp
+++ torch/csrc/TypeInfo.cpp
@@ -112,7 +112,7 @@ static PyObject* THPDTypeInfo_bits(THPDTypeInfo* self, void*) {
 }
 
 static PyObject* THPFInfo_eps(THPFInfo* self, void*) {
-  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(at::kHalf,
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16,
       self->type, "epsilon", [] {
         return PyFloat_FromDouble(
             std::numeric_limits<
@@ -121,14 +121,14 @@ static PyObject* THPFInfo_eps(THPFInfo* self, void*) {
 }
 
 static PyObject* THPFInfo_max(THPFInfo* self, void*) {
-  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(at::kHalf, self->type, "max", [] {
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16, self->type, "max", [] {
     return PyFloat_FromDouble(
         std::numeric_limits<at::scalar_value_type<scalar_t>::type>::max());
   });
 }
 
 static PyObject* THPFInfo_min(THPFInfo* self, void*) {
-  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(at::kHalf, self->type, "min", [] {
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16, self->type, "lowest", [] {
     return PyFloat_FromDouble(
         std::numeric_limits<at::scalar_value_type<scalar_t>::type>::lowest());
   });
@@ -158,37 +158,44 @@ static PyObject* THPIInfo_min(THPIInfo* self, void*) {
   });
 }
 
+static PyObject* THPIInfo_dtype(THPIInfo* self, void*) {
+  std::string primary_name, legacy_name;
+  std::tie(primary_name, legacy_name) = torch::utils::getDtypeNames(self->type);
+  return AT_DISPATCH_INTEGRAL_TYPES(self->type, "dtype", [primary_name] {
+    return PyUnicode_FromString((char*)primary_name.data());
+  });
+}
+
 static PyObject* THPFInfo_tiny(THPFInfo* self, void*) {
-  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(at::kHalf, self->type, "min", [] {
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16, self->type, "min", [] {
     return PyFloat_FromDouble(
         std::numeric_limits<at::scalar_value_type<scalar_t>::type>::min());
   });
 }
 
 static PyObject* THPFInfo_resolution(THPFInfo* self, void*) {
-  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(at::kHalf, self->type, "digits10", [] {
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16, self->type, "digits10", [] {
     return PyFloat_FromDouble(
         std::pow(10, -std::numeric_limits<at::scalar_value_type<scalar_t>::type>::digits10));
   });
 }
 
-PyObject* THPFInfo_str(THPFInfo* self) {
-  auto type = self->type;
+static PyObject* THPFInfo_dtype(THPFInfo* self, void*) {
   std::string primary_name, legacy_name;
-  std::tie(primary_name, legacy_name) = torch::utils::getDtypeNames(type);
+  std::tie(primary_name, legacy_name) = torch::utils::getDtypeNames(self->type);
+  return AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(at::kHalf, at::ScalarType::BFloat16, self->type, "dtype", [primary_name] {
+    return PyUnicode_FromString((char*)primary_name.data());
+  });
+}
+
+PyObject* THPFInfo_str(THPFInfo* self) {
   std::ostringstream oss;
-
-  if (type == at::ScalarType::BFloat16) {
-    oss << "finfo(dtype=" << primary_name << ")";
-    return THPUtils_packString(oss.str().c_str());
-  }
-
   oss << "finfo(resolution=" << PyFloat_AsDouble(THPFInfo_resolution(self, nullptr));
   oss << ", min=" << PyFloat_AsDouble(THPFInfo_min(self, nullptr));
   oss << ", max=" << PyFloat_AsDouble(THPFInfo_max(self, nullptr));
   oss << ", eps=" << PyFloat_AsDouble(THPFInfo_eps(self, nullptr));
   oss << ", tiny=" << PyFloat_AsDouble(THPFInfo_tiny(self, nullptr));
-  oss << ", dtype=" << primary_name << ")";
+  oss << ", dtype=" << PyUnicode_AsUTF8(THPFInfo_dtype(self, nullptr)) << ")";
 
   return THPUtils_packString(oss.str().c_str());
 }
@@ -201,7 +208,7 @@ PyObject* THPIInfo_str(THPIInfo* self) {
 
   oss << "iinfo(min=" << PyFloat_AsDouble(THPIInfo_min(self, nullptr));
   oss << ", max=" << PyFloat_AsDouble(THPIInfo_max(self, nullptr));
-  oss << ", dtype=" << primary_name << ")";
+  oss << ", dtype=" << PyUnicode_AsUTF8(THPIInfo_dtype(self, nullptr)) << ")";
 
   return THPUtils_packString(oss.str().c_str());
 }
@@ -213,6 +220,7 @@ static struct PyGetSetDef THPFInfo_properties[] = {
     {"min", (getter)THPFInfo_min, nullptr, nullptr, nullptr},
     {"tiny", (getter)THPFInfo_tiny, nullptr, nullptr, nullptr},
     {"resolution", (getter)THPFInfo_resolution, nullptr, nullptr, nullptr},
+    {"dtype", (getter)THPFInfo_dtype, nullptr, nullptr, nullptr},
     {nullptr}};
 
 static PyMethodDef THPFInfo_methods[] = {
@@ -263,6 +271,7 @@ static struct PyGetSetDef THPIInfo_properties[] = {
     {"bits", (getter)THPDTypeInfo_bits, nullptr, nullptr, nullptr},
     {"max", (getter)THPIInfo_max, nullptr, nullptr, nullptr},
     {"min", (getter)THPIInfo_min, nullptr, nullptr, nullptr},
+    {"dtype", (getter)THPIInfo_dtype, nullptr, nullptr, nullptr},
     {nullptr}};
 
 static PyMethodDef THPIInfo_methods[] = {
