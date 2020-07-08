dde18041a62784ec9e8a56b98de2bf2c0671e6de
Supriya Rao <supriyar@fb.com>
2020-07-08 10:29:16 -0700

    [quant][graphmode] Refactor quantization patterns (#40894)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/40894
    
    Test Plan:
    python test/test_quantization.py
    
    Imported from OSS
    
    Differential Revision: D22403901
    
    fbshipit-source-id: e0bcf8a628c6a1acfe6fa10a52912360a619bc62

--- torch/csrc/jit/passes/quantization/finalize.cpp
+++ torch/csrc/jit/passes/quantization/finalize.cpp
@@ -8,48 +8,13 @@
 namespace torch {
 namespace jit {
 
-struct PatternReplaceInfo {
-  std::string pattern;
-  std::string replacement;
-  std::vector<MatchFilter> filters = {};
-};
-
 namespace {
 
 using graph_rewrite_helper::PatternInfo;
 
 void insertPrepackUnpackForLinear(std::shared_ptr<Graph>& graph) {
-  std::string linear_with_quant = R"(
-graph(%a_dequant, %w_quant, %b):
-        %w_dequant = aten::dequantize(%w_quant)
-        %r = aten::linear(%a_dequant, %w_dequant, %b)
-        return (%r) )";
-
-  std::string linear_with_quant_prepack = R"(
-graph(%a_dequant, %w_quant, %b):
-        %packed_params = quantized::linear_prepack(%w_quant, %b)
-        %w_quant_unpacked : Tensor, %b_unpacked : Tensor? = quantized::linear_unpack(%packed_params)
-        %w_dequant = aten::dequantize(%w_quant_unpacked)
-        %r = aten::linear(%a_dequant, %w_dequant, %b_unpacked)
-        return (%r) )";
-  std::string linear_fp16_with_cast = R"(
-graph(%w, %a_dq, %b, %dtype_fp16, %dtype_fp32, %false):
-        %fp16_tensor = aten::to(%w, %dtype_fp16, %false, %false)
-        %fp32_tensor = aten::to(%fp16_tensor, %dtype_fp32, %false, %false)
-        %r = aten::linear(%a_dq, %fp32_tensor, %b)
-        return (%r) )";
-  std::string linear_fp16_with_prepack = R"(
-graph(%w, %a_dq, %b, %dtype_fp16, %dtype_fp32, %false):
-        %packed_params = quantized::linear_prepack_fp16(%w, %b)
-        %w_unpacked : Tensor, %b_unpacked : Tensor? = quantized::linear_unpack_fp16(%packed_params)
-        %r = aten::linear(%a_dq, %w_unpacked, %b_unpacked)
-        return (%r) )";
-
-  std::vector<PatternReplaceInfo> patterns_and_replacements = {
-      {linear_with_quant, linear_with_quant_prepack},
-      {linear_fp16_with_cast,
-       linear_fp16_with_prepack,
-       {is_half_dtype, is_float_dtype, is_false_value}}};
+  std::vector<QuantFusionInfo> patterns_and_replacements =
+      linear_prepack_unpack_patterns();
 
   for (const auto& entry : patterns_and_replacements) {
     SubgraphRewriter rewriter;
@@ -59,52 +24,8 @@ graph(%w, %a_dq, %b, %dtype_fp16, %dtype_fp32, %false):
 }
 
 void insertPrepackUnpackForConv(std::shared_ptr<Graph>& graph) {
-  std::string conv1d_with_quant = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %w_dequant = aten::dequantize(%w_quant)
-        %r = aten::conv1d(%a_dequant, %w_dequant, %b, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::string conv1d_with_quant_prepack = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %packed_params : __torch__.torch.classes.quantized.Conv2dPackedParamsBase = quantized::conv1d_prepack(%w_quant, %b, %stride, %padding, %dilation, %groups)
-        %w_quant_unpacked : Tensor, %b_unpacked : Tensor? = quantized::conv1d_unpack(%packed_params)
-        %w_dequant = aten::dequantize(%w_quant_unpacked)
-        %r = aten::conv1d(%a_dequant, %w_dequant, %b_unpacked, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::string conv2d_with_quant = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %w_dequant = aten::dequantize(%w_quant)
-        %r = aten::conv2d(%a_dequant, %w_dequant, %b, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::string conv2d_with_quant_prepack = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %packed_params : __torch__.torch.classes.quantized.Conv2dPackedParamsBase = quantized::conv2d_prepack(%w_quant, %b, %stride, %padding, %dilation, %groups)
-        %w_quant_unpacked : Tensor, %b_unpacked : Tensor? = quantized::conv2d_unpack(%packed_params)
-        %w_dequant = aten::dequantize(%w_quant_unpacked)
-        %r = aten::conv2d(%a_dequant, %w_dequant, %b_unpacked, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::string conv3d_with_quant = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %w_dequant = aten::dequantize(%w_quant)
-        %r = aten::conv3d(%a_dequant, %w_dequant, %b, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::string conv3d_with_quant_prepack = R"(
-graph(%a_dequant, %w_quant, %b, %stride, %padding, %dilation, %groups):
-        %packed_params : __torch__.torch.classes.quantized.Conv3dPackedParamsBase = quantized::conv3d_prepack(%w_quant, %b, %stride, %padding, %dilation, %groups)
-        %w_quant_unpacked : Tensor, %b_unpacked : Tensor? = quantized::conv3d_unpack(%packed_params)
-        %w_dequant = aten::dequantize(%w_quant_unpacked)
-        %r = aten::conv3d(%a_dequant, %w_dequant, %b_unpacked, %stride, %padding, %dilation, %groups)
-        return (%r) )";
-
-  std::vector<PatternReplaceInfo> patterns_and_replacements = {
-      {conv1d_with_quant, conv1d_with_quant_prepack},
-      {conv2d_with_quant, conv2d_with_quant_prepack},
-      {conv3d_with_quant, conv3d_with_quant_prepack}};
+  std::vector<QuantFusionInfo> patterns_and_replacements =
+      conv_prepack_unpack_patterns();
 
   for (const auto& entry : patterns_and_replacements) {
     SubgraphRewriter rewriter;
