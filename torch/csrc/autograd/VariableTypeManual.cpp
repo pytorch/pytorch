514283bbe188be71fc3d0fed930669f4e4af10c1
Wojciech Baranowski <wbaranowski@protonmail.com>
2020-07-09 11:01:29 +0300

    Warning in detach_()

--- torch/csrc/autograd/VariableTypeManual.cpp
+++ torch/csrc/autograd/VariableTypeManual.cpp
@@ -251,7 +251,18 @@ Tensor detach(const Tensor & self) {
 Tensor & detach_(Tensor & self) {
   RECORD_FUNCTION("detach_", std::vector<c10::IValue>({self}));
   if (self.is_view()) {
-    AT_ERROR("Can't detach views in-place. Use detach() instead");
+    // NB: is_view() ==> get_autograd_meta()
+    auto diff_view_meta = static_cast<torch::autograd::DifferentiableViewMeta*>(torch::autograd::impl::get_autograd_meta(self));
+    // See NOTE [ View + Inplace detection ]
+    if (diff_view_meta->creation_meta == CreationMeta::MULTI_OUTPUT_SAFE) {
+        TORCH_WARN("This view is the output of a function that "
+                   "returns multiple views. Inplace operators on such "
+                   "views are being deprecated and will be forbidden "
+                   "starting from version 1.8. Consider using `unsafe_` "
+                   "version of the function that produced this view.");
+    } else {
+      AT_ERROR("Can't detach views in-place. Use detach() instead");
+    }
   }
   // I think the choice here is conservative.  In principle, doing
   // an in-place detach should give us the ability to just clear
