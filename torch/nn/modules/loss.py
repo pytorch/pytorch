f60d911d0fdcc055335247b9d4a74a5d33f878c2
SsnL <tongzhou.wang.1994@gmail.com>
2020-07-08 23:32:43 -0400

    make IterableDataset DataLoader.__len__ warning clearer

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -1316,7 +1316,7 @@ class CTCLoss(_Loss):
         >>> # Initialize random batch of input vectors, for *size = (T,N,C)
         >>> input = torch.randn(T, N, C).log_softmax(2).detach().requires_grad_()
         >>> input_lengths = torch.full(size=(N,), fill_value=T, dtype=torch.long)
-        >>> 
+        >>>
         >>> # Initialize random batch of targets (0 = blank, 1:C = classes)
         >>> target_lengths = torch.randint(low=1, high=T, size=(N,), dtype=torch.long)
         >>> target = torch.randint(low=1, high=C, size=(sum(target_lengths),), dtype=torch.long)
