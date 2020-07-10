ca487a57510d2b6edb663768e359c45e12df5f23
Will Gan <wjgan@berkeley.edu>
2020-07-09 22:52:25 -0700

    Fix CUDA type promotion issue

--- aten/src/ATen/native/cuda/ComplexHelperKernel.cu
+++ aten/src/ATen/native/cuda/ComplexHelperKernel.cu
@@ -10,21 +10,33 @@ namespace at { namespace native {
 namespace {
 
 void complex_kernel_cuda(TensorIterator& iter) {
-  AT_DISPATCH_FLOATING_TYPES(iter.input_dtype(), "complex_cuda", [&]() {
-    gpu_kernel_with_scalars(iter,
-      []GPU_LAMBDA(scalar_t a, scalar_t b) -> c10::complex<scalar_t> {
-        return c10::complex<scalar_t>(a, b);
+  // Necessary since comparison_op doesn't actually promote types for CUDA
+  // tensors
+  AT_DISPATCH_FLOATING_TYPES(
+      promote_types(iter.input_dtype(0), iter.input_dtype(1)),
+      "complex_cuda",
+      [&]() {
+        gpu_kernel_with_scalars(
+            iter,
+            [] GPU_LAMBDA(scalar_t a, scalar_t b) -> c10::complex<scalar_t> {
+              return c10::complex<scalar_t>(a, b);
+            });
       });
-  });
 }
 
 void complex_polar_kernel_cuda(TensorIterator& iter) {
-  AT_DISPATCH_FLOATING_TYPES(iter.input_dtype(), "complex_polar_cuda", [&]() {
-    gpu_kernel_with_scalars(iter,
-      []GPU_LAMBDA(scalar_t a, scalar_t b) -> c10::complex<scalar_t> {
-        return c10::complex<scalar_t>(a * std::cos(b), a * std::sin(b));
+  // Necessary since comparison_op doesn't actually promote types for CUDA
+  // tensors
+  AT_DISPATCH_FLOATING_TYPES(
+      promote_types(iter.input_dtype(0), iter.input_dtype(1)),
+      "complex_polar_cuda",
+      [&]() {
+        gpu_kernel_with_scalars(
+            iter,
+            [] GPU_LAMBDA(scalar_t a, scalar_t b) -> c10::complex<scalar_t> {
+              return c10::complex<scalar_t>(a * std::cos(b), a * std::sin(b));
+            });
       });
-  });
 }
 
 } // anonymous namespace
