f8441dd8ffa99500513c09b1bb77e8170ecef54f
Ivan Kobzarev <ivankobzarev@fb.com>
2020-07-09 19:47:26 -0700

    [vulkan][asan] Fix Invalid Memory ops
    [ghstack-poisoned]

--- aten/src/ATen/native/vulkan/VulkanOps.cpp
+++ aten/src/ATen/native/vulkan/VulkanOps.cpp
@@ -261,12 +261,20 @@ VBuffer kernelNCHW_OCHW_repack_O4C4HWi4o4(
 
 VBuffer bufferFromOptionalHostData(
     c10::optional<const float*> data,
-    const uint32_t size) {
+    const uint32_t dataSize,
+    const uint32_t bufferSize) {
+  TORCH_INTERNAL_ASSERT(
+      dataSize <= bufferSize,
+      "buffer size(",
+      bufferSize,
+      ") is not enough for data(",
+      dataSize,
+      ")");
   const auto sizeAligned =
-      ROUND_UP(size, context().limits().minStorageBufferOffsetAlignment);
+      ROUND_UP(bufferSize, context().limits().minStorageBufferOffsetAlignment);
   VBuffer buffer{sizeAligned};
   if (data.has_value()) {
-    buffer.copy_from_host_to_device((void*)*data, size);
+    buffer.copy_from_host_to_device((void*)*data, dataSize);
   } else {
     buffer.set_zeros();
   }
@@ -277,10 +285,6 @@ VBuffer bufferZeros(const uint32_t size) {
   VBuffer buffer{size};
   buffer.set_zeros();
   return buffer;
-}
-
-uint32_t conv2d_biasBufferSize(uint32_t oc) {
-  return sizeof(float) * ALIGN_UP4(oc);
 }
 
 void conv2d_depthwise(
@@ -370,7 +374,10 @@ void conv2d_depthwise(
       output,
       input,
       weight,
-      bufferFromOptionalHostData(bias, conv2d_biasBufferSize(params.OC)),
+      bufferFromOptionalHostData(
+          bias,
+          sizeof(float) * params.OC,
+          sizeof(float) * ALIGN_UP4(params.OC)),
       params,
       output_min,
       output_max);
@@ -390,7 +397,10 @@ void conv2d_depthwise(
       output,
       input,
       weightTensor,
-      bufferFromOptionalHostData(bias, conv2d_biasBufferSize(params.OC)),
+      bufferFromOptionalHostData(
+          bias,
+          sizeof(float) * params.OC,
+          sizeof(float) * ALIGN_UP4(params.OC)),
       params,
       output_min,
       output_max);
@@ -590,7 +600,10 @@ void conv2d(
       output,
       input,
       kernelImage,
-      bufferFromOptionalHostData(bias, conv2d_biasBufferSize(params.OC)),
+      bufferFromOptionalHostData(
+          bias,
+          sizeof(float) * params.OC,
+          sizeof(float) * ALIGN_UP4(params.OC)),
       params,
       output_min,
       output_max);
@@ -609,7 +622,10 @@ void conv2d(
         output,
         input,
         weight_prepacked,
-        bufferFromOptionalHostData(bias, conv2d_biasBufferSize(params.OC)),
+        bufferFromOptionalHostData(
+            bias,
+            sizeof(float) * params.OC,
+            sizeof(float) * ALIGN_UP4(params.OC)),
         params,
         output_min,
         output_max);
