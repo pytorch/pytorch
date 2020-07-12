65286e9880b8a1f2894060232a279745ec1bd18e
Paul Shao <pshao@fb.com>
2020-07-11 23:36:14 -0700

    Fake Quantization Per Tensor Kernel Core Implementation (CPU) (#41029)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41029
    
    This diff contains the core implementation for the fake quantizer per tensor kernel that supports back propagation on the scale and zero point.
    
    Test Plan:
    On a devvm, use:
    - `buck test //caffe2/test:quantization -- learnable_forward_per_tensor`
    - `buck test //caffe2/test:quantization -- learnable_backward_per_tensor`
    
    Reviewed By: z-a-f
    
    Differential Revision: D22394145
    
    fbshipit-source-id: f5ec0bf9d76948b938a55543418fe8ec8ee28af4

--- aten/src/ATen/native/quantized/fake_quant_affine.h
+++ aten/src/ATen/native/quantized/fake_quant_affine.h
@@ -28,6 +28,8 @@ using fake_quant_grad_tensor_fn = void (*)(
 
 DECLARE_DISPATCH(fake_quant_tensor_fn, fake_quant_tensor_stub);
 DECLARE_DISPATCH(fake_quant_grad_tensor_fn, fake_quant_grad_tensor_stub);
+DECLARE_DISPATCH(fake_quant_grad_tensor_fn, fake_quant_grad_learnable_scale_tensor_stub);
+DECLARE_DISPATCH(fake_quant_grad_tensor_fn, fake_quant_grad_learnable_zero_point_tensor_stub);
 
 using fake_quant_per_channel_fn = void (*)(
     TensorIterator &iter,
