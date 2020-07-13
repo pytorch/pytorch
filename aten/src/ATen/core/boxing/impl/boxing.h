b6849cad51d6a6d0293d66efe0f9f63c201a2626
bhosmer <bhosmer@fb.com>
2020-07-12 21:54:53 -0700

    Update on "Improved coverage for unboxed->boxed kernel wrappers"
    Adds boxing for inplace and outplace kernels, itemizes
    remaining unsupported cases, and fails compilation when
    new unsupported types are introduced in op signatures.
    
    Reviewer notes:
    1. `QuantizerBase.h` is just the classdef for `Quantizer` snipped out of `Quantizer.h` and placed in its own file, so `Quantizer` can be fully defined in `TensorMethods.h` without introducing a circularity. (The definition was not previously needed bc the relevant ops didn't get autoboxing support.)
    2. there are some general utilities at the top of `boxing.h` which don't comfortably fit in any of the existing headers, but I didn't want to make a new one... is there a better home for them?
    3. Best to leave custom class boxing tests out of this PR, I think.
    
    Differential Revision: [D21718547](https://our.internmc.facebook.com/intern/diff/D21718547)
    
    [ghstack-poisoned]

--- aten/src/ATen/core/boxing/impl/boxing.h
+++ aten/src/ATen/core/boxing/impl/boxing.h
@@ -38,7 +38,7 @@ template<class T, class Enable = void>
 struct has_ivalue_to : std::false_type {};
 
 template<class T>
-struct has_ivalue_to<T, guts::void_t<decltype(IValue::to<T>())>> : std::true_type {};
+struct has_ivalue_to<T, guts::void_t<decltype(IValue::to<T>)>> : std::true_type {};
 
 //
 // boxing predicates
