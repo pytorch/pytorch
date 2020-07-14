665b9ce16cb66c42e4ef904d860aceada1f8938d
Hameer Abbasi <hameerabbasi@yahoo.com>
2020-07-14 10:40:02 -0500

    Revert "Add TensorList benchmarks to overrides benchmark."
    This reverts commit 5a94109af0d0dd29b4d8faf924d6f27807f30e9f.

--- benchmarks/overrides_benchmark/bench.py
+++ benchmarks/overrides_benchmark/bench.py
@@ -6,15 +6,14 @@ from common import SubTensor, WithTorchFunction, SubWithTorchFunction
 
 NUM_REPEATS = 1000
 NUM_REPEAT_OF_REPEATS = 1000
-NUM_ARGS_IN_LIST = 2
 
 
-def bench(f, *args):
+def bench(t1, t2):
     bench_times = []
     for _ in range(NUM_REPEAT_OF_REPEATS):
         time_start = time.time()
         for _ in range(NUM_REPEATS):
-            f(*args)
+            torch.add(t1, t2)
         bench_times.append(time.time() - time_start)
 
     bench_time = float(torch.min(torch.Tensor(bench_times))) / 1000
@@ -26,7 +25,6 @@ def bench(f, *args):
 def main():
     global NUM_REPEATS
     global NUM_REPEAT_OF_REPEATS
-    global NUM_ARGS_IN_LIST
 
     parser = argparse.ArgumentParser(
         description="Run the __torch_function__ benchmarks."
@@ -45,18 +43,10 @@ def main():
         default=NUM_REPEAT_OF_REPEATS,
         help="The number of measurements.",
     )
-    parser.add_argument(
-        "--nlist",
-        "-l",
-        type=int,
-        default=NUM_REPEAT_OF_REPEATS,
-        help="The number of tensors in a tensor list.",
-    )
     args = parser.parse_args()
 
     NUM_REPEATS = args.nreps
     NUM_REPEAT_OF_REPEATS = args.nrepreps
-    NUM_ARGS_IN_LIST = args.nlist
 
     types = torch.Tensor, SubTensor, WithTorchFunction, SubWithTorchFunction
 
@@ -64,19 +54,10 @@ def main():
         tensor_1 = t(1)
         tensor_2 = t(2)
 
-        bench_min, bench_std = bench(torch.add, tensor_1, tensor_2)
+        bench_min, bench_std = bench(tensor_1, tensor_2)
         print(
             "Type {0} had a minimum time of {1} us"
-            " and a standard deviation of {2} us for torch.add.".format(
-                t.__name__, (10 ** 6 * bench_min), (10 ** 6) * bench_std
-            )
-        )
-
-        tensor_list = [tensor_1] * NUM_ARGS_IN_LIST
-        bench_min, bench_std = bench(torch.cat, tensor_list)
-        print(
-            "Type {0} had a minimum time of {1} us"
-            " and a standard deviation of {2} us for torch.cat.".format(
+            " and a standard deviation of {2} us.".format(
                 t.__name__, (10 ** 6 * bench_min), (10 ** 6) * bench_std
             )
         )
