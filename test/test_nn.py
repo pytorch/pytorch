20f3051f7d6aa85a778c5f1a8b88ffb706da01ca
Wojciech Baranowski <wbaranowski@protonmail.com>
2020-07-14 21:49:43 -0700

    [adaptive_]max_pool{1,2,3}d: handle edge case when input is filled with -inf (#40665)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/40131
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40665
    
    Differential Revision: D22463538
    
    Pulled By: ezyang
    
    fbshipit-source-id: 7e08fd0205926911d45aa150012154637e64a8d4

--- test/test_nn.py
+++ test/test_nn.py
@@ -11045,7 +11045,7 @@ class TestNNDeviceType(NNTestCase):
 
     @dtypesIfCUDA(torch.half, torch.float, torch.double)
     @dtypes(torch.float)
-    @onlyCUDA   # TODO: fix CPU adaptive_maxpool_2d
+    @onlyOnCPUAndCUDA  # TODO: Fails on XLA
     def test_max_pool_nan_inf(self, device, dtype):
         for adaptive in ['', 'adaptive_']:
             for num_dim in [1, 2, 3]:
@@ -11063,7 +11063,7 @@ class TestNNDeviceType(NNTestCase):
 
     @dtypesIfCUDA(torch.half, torch.float, torch.double)
     @dtypes(torch.float)
-    @onlyCUDA   # TODO: fix CPU fractional_maxpool_2d
+    @onlyOnCPUAndCUDA  # TODO: Fails on XLA
     def test_fractional_max_pool_nan_inf(self, device, dtype):
         for num_dim in [2, 3]:
             fn_name = 'FractionalMaxPool{}d'.format(num_dim)
