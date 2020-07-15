1cff489abfc936dab8c70f7ef78fa1dd82f4fd50
Pritam Damania <pritam.damania@fb.com>
2020-07-15 12:54:13 -0700

    Add C++ end to end test for RPC and distributed autograd. (#36893)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36893
    
    Adding an end to end test for running a simple training loop in C++
    for the distributed RPC framework.
    
    The goal of this change is to enable LeakSanitizer and potentially catch memory
    leaks in the Future. Enabling LSAN with python multiprocessing is tricky and we
    haven't found a solution for this. As a result, adding a C++ test that triggers
    most of the critical codepaths would be good for now.
    
    As an example, this unit test would've caught the memory leak fixed by:
    https://github.com/pytorch/pytorch/pull/31030
    ghstack-source-id: 107781167
    
    Test Plan:
    1) Verify the test catches memory leaks.
    2) waitforbuildbot
    
    Reviewed By: mrshenli
    
    Differential Revision: D21112208
    
    fbshipit-source-id: 4eb2a6b409253108f6b6e14352e593d250c7a64d

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -85,7 +85,7 @@ fi
 # ASAN test is not working
 if [[ "$BUILD_ENVIRONMENT" == *asan* ]]; then
     # Suppress vptr violations arising from multiple copies of pybind11
-    export ASAN_OPTIONS=detect_leaks=0:symbolize=1:strict_init_order=true
+    export ASAN_OPTIONS=detect_leaks=0:symbolize=1:strict_init_order=true:detect_odr_violation=0
     export UBSAN_OPTIONS=print_stacktrace=1:suppressions=$PWD/ubsan.supp
     export PYTORCH_TEST_WITH_ASAN=1
     export PYTORCH_TEST_WITH_UBSAN=1
