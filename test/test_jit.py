7e96d7c46ad5a611007b57228cce0c2ac7cf36b8
eellison <eellison@fb.com>
2020-07-15 15:37:50 -0700

    move remove mutation to its own file
    [ghstack-poisoned]

--- test/test_jit.py
+++ test/test_jit.py
@@ -24,6 +24,7 @@ from jit.test_freezing import TestFreezing  # noqa: F401
 from jit.test_save_load import TestSaveLoad  # noqa: F401
 from jit.test_python_ir import TestPythonIr  # noqa: F401
 from jit.test_functional_blocks import TestFunctionalBlocks  # noqa: F401
+from jit.test_remove_mutation import TestRemoveMutation  # noqa: F401
 from jit.test_torchbind import TestTorchbind  # noqa: F401
 from jit.test_op_normalization import TestOpNormalization  # noqa: F401
 from jit.test_module_interface import TestModuleInterface  # noqa: F401
