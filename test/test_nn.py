55e8cfc7a24246bf0d8db3babdfb3af99bd668c1
Kurt Mohler <kmohler@quansight.com>
2020-07-14 20:32:24 -0700

    Change BCELoss size mismatch warning into an error (#41426)
    Summary:
    BCELoss currently uses different broadcasting semantics than numpy. Since previous versions of PyTorch have thrown a warning in these cases telling the user that input sizes should match, and since the CUDA and CPU results differ when sizes do not match, it makes sense to upgrade the size mismatch warning to an error.
    
    We can consider supporting numpy broadcasting semantics in BCELoss in the future if needed.
    
    Closes https://github.com/pytorch/pytorch/issues/40023
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41426
    
    Reviewed By: zou3519
    
    Differential Revision: D22540841
    
    Pulled By: ezyang
    
    fbshipit-source-id: 6c6d94c78fa0ae30ebe385d05a9e3501a42b3652

--- test/test_nn.py
+++ test/test_nn.py
@@ -6052,6 +6052,13 @@ class TestNN(NNTestCase):
         with self.assertRaisesRegex(RuntimeError, 'between 0 and 1'):
             loss_too_positive = bceloss(output_too_positive, target)
 
+    def test_bce_loss_size_mismatch(self):
+        bceloss = nn.BCELoss()
+        a = torch.rand(25)
+        b = torch.rand(25, 1)
+        with self.assertRaisesRegex(ValueError, r'Using a target size \('):
+            bceloss(a, b)
+
     def test_bce_with_logits_gives_same_result_as_sigmoid_and_bce_loss_large_tensors_with_grad(self):
         x_size = 1024
         y_size = 256
