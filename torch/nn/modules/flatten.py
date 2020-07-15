78adc4854a82db3d8bbfa0d050774865bc584d8c
Alvaro <alvgaona@gmail.com>
2020-07-14 22:23:39 -0300

    Add unflatten module

--- torch/nn/modules/flatten.py
+++ torch/nn/modules/flatten.py
@@ -2,6 +2,7 @@ from .module import Module
 
 from torch import Tensor
 
+
 class Flatten(Module):
     r"""
     Flattens a contiguous range of dims into a tensor. For use with :class:`~nn.Sequential`.
@@ -31,3 +32,48 @@ class Flatten(Module):
 
     def forward(self, input: Tensor) -> Tensor:
         return input.flatten(self.start_dim, self.end_dim)
+
+    def extra_repr(self) -> str:
+        return 'start_dim={}, end_dim={}'.format(
+            self.start_dim, self.end_dim
+        )
+
+
+class Unflatten(Module):
+    r"""
+    Unflattens a tensor into another tensor of shape (N, C, H, W). For use with :class:`~nn.Sequential`.
+    Args:
+        channels: numbers of channels or depth of the output tensor
+        height: dimension corresponding to the height of the output tensor.
+        width: dimension corresponding to the width of the output tensor.
+
+    Shape:
+        - Input: :math:`(N, *dims)`
+        - Output: :math:`(N, C_out, H_out, W_out)`
+
+
+    Examples::
+        >>> m = nn.Sequential(
+        >>>     nn.Linear(49152, 49152),
+        >>>     nn.Unflatten(3, 128, 128)
+        >>> )
+    """
+    __constants__ = ['channels', 'height', 'width']
+    channels: int
+    height: int
+    width: int
+
+    def __init__(self, channels: int, height: int, width: int) -> None:
+        super(Unflatten, self).__init__()
+        self.channels = channels
+        self.height = height
+        self.width = width
+
+    def forward(self, input: Tensor) -> Tensor:
+        input = input.refine_names('N', 'features')
+        return input.unflatten('features', (('C', self.channels), ('H', self.height), ('W', self.width)))
+
+    def extra_repr(self) -> str:
+        return 'channels={}, height={}, width={}'.format(
+            self.channels, self.height, self.width
+        )
