8fd319ba505767de77dba8610465492dc4a25834
Natalia Gimelshein <ngimel@fb.com>
2020-07-15 20:02:58 -0700

    add non-scalar branch

--- aten/src/ATen/native/cuda/BinaryMulDivKernel.cu
+++ aten/src/ATen/native/cuda/BinaryMulDivKernel.cu
@@ -38,21 +38,28 @@ void mul_kernel_cuda(TensorIterator& iter) {
     gpu_kernel_with_scalars(iter, []GPU_LAMBDA(bool a, bool b) -> bool {
       return a && b;
     });
-  } else if (!isIntegralType(iter.common_dtype(), /*includeBool*/ false) && (iter.is_cpu_scalar(1) ||
-iter.is_cpu_scalar(2))) {
+  } else if (!isIntegralType(iter.common_dtype(), /*includeBool*/ false)) {
+       if (iter.is_cpu_scalar(1) || iter.is_cpu_scalar(2)) {
   //if common dtype is half the scalar constant can overflow in half precision, and yet the result can
   //still be representable in the half dtype. Cast scalar to acc_type to have better accuracy
-  AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16, iter.common_dtype(), "mul_cuda", [&]() {
-    using accscalar_t = at::acc_type<scalar_t, true>;
-    int scalar_arg = iter.is_cpu_scalar(1) ? 1 : 2;
-    auto b = iter.scalar_value<accscalar_t>(scalar_arg);
-    iter.remove_operand(scalar_arg);
-    gpu_kernel(iter, [b]GPU_LAMBDA(scalar_t a) -> scalar_t {
-      return a * b;
-    });
-  });
+          AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16, iter.common_dtype(), "mul_cuda", [&]() {
+            using accscalar_t = at::acc_type<scalar_t, true>;
+            int scalar_arg = iter.is_cpu_scalar(1) ? 1 : 2;
+            auto b = iter.scalar_value<accscalar_t>(scalar_arg);
+            iter.remove_operand(scalar_arg);
+            gpu_kernel(iter, [b]GPU_LAMBDA(scalar_t a) -> scalar_t {
+              return a * b;
+            });
+          });
+     } else {
+      AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16, iter.common_dtype(), "mul_cuda", [&]() {
+        gpu_kernel(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
+          return a * b;
+        });
+      });
+     }
   } else {
-    AT_DISPATCH_INTEGRAL_TYPES(kHalf, kBFloat16, iter.common_dtype(), "mul_cuda", [&]() {
+    AT_DISPATCH_INTEGRAL_TYPES(iter.common_dtype(), "mul_cuda", [&]() {
       gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
         return a * b;
       });
