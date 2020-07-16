5bba973afd81b8df5c4dd85fa6f2ec38e3549f04
Wojciech Baranowski <wbaranowski@protonmail.com>
2020-07-16 09:00:09 -0700

    Reland split unsafe version (#41484)
    Summary:
    Reland of https://github.com/pytorch/pytorch/pull/39299
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41484
    
    Reviewed By: glaringlee
    
    Differential Revision: D22552377
    
    Pulled By: albanD
    
    fbshipit-source-id: 1d1b713d2429ae162e04bda845ef0838c52df789

--- test/test_torch.py
+++ test/test_torch.py
@@ -211,6 +211,7 @@ class AbstractTestCases:
                            'smm',
                            'softmax',
                            'split_with_sizes',
+                           'unsafe_split_with_sizes',
                            'sspaddmm',
                            'to_dense',
                            'sparse_resize_',
