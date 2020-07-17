e048c5fee796856a68c0d7990cc8acbe71cedd7f
Alexander <aocsa.cs@gmail.com>
2020-07-17 10:31:05 -0500

    sparse matmul using a coalesce form

--- aten/src/ATen/native/sparse/SparseMatMul.cpp
+++ aten/src/ATen/native/sparse/SparseMatMul.cpp
@@ -21,6 +21,86 @@ std::vector<int64_t> sort_indices(const TensorAccessor<scalar_t, 1> &v) {
   return indices;
 }
 
+template<size_t N>
+std::vector<int64_t> indices2csr(const TensorAccessor<int64_t,N>& indices, int64_t dim) {
+  auto nnz = indices.size(0);
+  std::vector<int64_t> csr(dim + 1, int64_t(0));
+  int64_t last_i = 0;
+  for (int64_t index = 0; index < nnz; index++) {
+    int64_t i = indices[index]; 
+    if (i == last_i) {
+      csr[last_i + 1] += 1;
+    } else {
+      for (int64_t j = last_i; j < i + 1; j++) {
+        csr[j + 1] = csr[last_i + 1];
+      }
+      last_i = i;
+      csr[last_i + 1] += 1;
+    }
+  }
+  for (int64_t j = last_i; j < dim; j++) {
+    csr[j + 1] = csr[last_i + 1];
+  } 
+  return csr;
+}
+
+template<typename scalar_t>
+void sparse_coalesce_matmul_kernel(Tensor& result, const Tensor& self, const Tensor& other) {
+  auto n = self.size(0);
+  auto p = self.size(1);
+  auto indices_a = self._indices().contiguous();
+  auto values_a = self._values().contiguous();
+
+  auto m = other.size(1);
+  auto indices_b = other._indices().contiguous();
+  auto values_b = other._values().contiguous();
+
+  auto nnz_a = values_a.size(0);
+  auto nnz_b = values_b.size(0);
+  auto indices_accessor_a = indices_a.accessor<int64_t, 2>();
+  auto indices_accessor_b = indices_b.accessor<int64_t, 2>();
+
+  auto values_accessor_a = values_a.accessor<scalar_t, 1>();
+  auto values_accessor_b = values_b.accessor<scalar_t, 1>();
+
+  auto result_indices = result._indices().contiguous();
+  auto result_values = result._values().contiguous();
+ 
+  std::map<std::pair<int64_t, int64_t>, scalar_t> d;
+  if (self.is_coalesced() && other.is_coalesced()) {
+    for (int64_t n1 = 0; n1 < nnz_a; n1++) {
+      auto r2 = indices2csr(indices_accessor_b[0], other.size(0));
+      for (int n2 = r2[indices_accessor_a[1][n1]]; n2 < r2[indices_accessor_a[1][n1] + 1]; n2++) {
+        d[std::make_pair(indices_accessor_a[0][n1], indices_accessor_b[1][n2]) ] += values_accessor_a[n1] * values_accessor_b[n2];
+      }    
+    }
+  } else {
+    for (int64_t n1 = 0; n1 < nnz_a; n1++) {
+      for (int n2 = 0; n2 < nnz_b; n2++) {
+        if (indices_accessor_b[0][n2] == indices_accessor_a[1][n1]) {
+            d[std::make_pair(indices_accessor_a[0][n1], indices_accessor_b[1][n2]) ] += values_accessor_a[n1] * values_accessor_b[n2];
+        }
+      }    
+    }
+  }
+  int64_t nnz_result = d.size();
+  std::vector<int64_t> values_size = {nnz_result};
+  result_indices.resize_({2, nnz_result});
+  result_values.resize_(values_size);
+
+  auto result_indices_accessor = result_indices.accessor<int64_t, 2>();
+  auto result_values_accessor = result_values.accessor<scalar_t, 1>();
+
+  int64_t index = 0;
+  for (auto kv : d) {
+    auto idx = kv.first; 
+    result_indices_accessor[0][index] = idx.first;
+    result_indices_accessor[1][index] = idx.second;
+    result_values[index] = kv.second;
+    index++;
+  }
+}
+
 template<typename scalar_t>
 void sparse_matmul_kernel(Tensor& result, const Tensor& self, const Tensor& other) {
   auto n = self.size(0);
@@ -181,6 +261,8 @@ void sparse_matmul_kernel_grad(Tensor& result, const Tensor& x) {
   }
 }
 
+
+
 } // end anonymous namespace
 
 Tensor sparse_matmul(const Tensor& self_, const Tensor& other_) {
@@ -195,7 +277,7 @@ Tensor sparse_matmul(const Tensor& self_, const Tensor& other_) {
 
   AT_DISPATCH_FLOATING_TYPES(
     self_.scalar_type(), "sparse_matmul", [&] {
-      sparse_matmul_kernel<scalar_t>(output, self, other);
+      sparse_coalesce_matmul_kernel<scalar_t>(output, self, other);
     });
   return output;
 }
