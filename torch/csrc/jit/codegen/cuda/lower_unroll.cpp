85a7f1509f902627942444a65f762eaac0693e8b
Kevin Stephano <kevin.stephano@gmail.com>
2020-07-18 06:41:29 -0700

    Fix to loop unrolling where a second loop at the same nesting level was missing predication. (#181)
    Fix to loop unrolling where a second loop at the same nesting level was missing predication.

--- torch/csrc/jit/codegen/cuda/lower_unroll.cpp
+++ torch/csrc/jit/codegen/cuda/lower_unroll.cpp
@@ -118,7 +118,7 @@ void UnrollPass::handle(ForLoop* fl) {
 
   bool has_TV_op = out != nullptr;
 
-  if (within_unroll && has_TV_op && has_global) {
+  if (within_unroll && has_TV_op) {
     // Setup unrolled loop information:
 
     // Indices used to detect when we can unroll a loop safely
