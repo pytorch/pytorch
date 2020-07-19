87ed0bbfb0f3ca297232ee37ea8a45ab06d507e1
maokaiyu <maokaiyu@devvm1090.prn0.facebook.com>
2020-07-19 16:47:32 -0700

    Add CUDAFunctions.h to some other contexts.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torch/csrc/CudaIPCTypes.h
+++ torch/csrc/CudaIPCTypes.h
@@ -2,6 +2,7 @@
 #ifdef USE_CUDA
 #include <c10/core/Allocator.h>
 #include <c10/cuda/CUDACachingAllocator.h>
+#include <c10/cuda/CUDAFunctions.h>
 #include <c10/cuda/CUDAException.h>
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/cuda/CUDAStream.h>
