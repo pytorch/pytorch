b520740fa81b6081669032b01ab072202ef599f8
Rohan Varma <rvarm1@fb.com>
2020-07-20 18:42:49 -0700

    Enable test_distributed to work with spawn mode
    Currently the tests in `test_distributed` only work with the `fork` mode multiprocessing, this PR introduces support for `spawn` mode multiprocessing as well (while keeping the `fork` mode intact).
    
    Motivations for the change:
    1) Spawn multiprocessing is the default on MacOS, so it better emulates how MacOS users would use distributed
    2) With python 3.8+, spawn is the default on linux, so we should have test coverage for this
    3) PT multiprocessing suggests using spawn/forkserver over fork, for sharing cuda tensors: https://pytorch.org/docs/stable/multiprocessing.html
    4) Spawn is better supported with respect to certain sanitizers such as TSAN, so adding this sanitizer coverage may help us uncover issues.
    
    Differential Revision: [D22408023](https://our.internmc.facebook.com/intern/diff/D22408023/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D22408023/)!
    
    [ghstack-poisoned]

---
+++ test/distributed/test_distributed_spawn.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import sys
+
+import torch.distributed as dist
+from torch._utils_internal import (
+    TEST_MASTER_ADDR as MASTER_ADDR,
+    TEST_MASTER_PORT as MASTER_PORT,
+)
+from torch.testing._internal.common_distributed import (
+    TEST_SKIPS,
+    MultiProcessTestCase,
+    initialize_temp_directories,
+)
+from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.distributed.distributed_test import Barrier, _DistTestBase
+
+
+BACKEND = os.environ["BACKEND"]
+INIT_METHOD = os.getenv("INIT_METHOD", "env://")
+
+
+if BACKEND == "gloo" or BACKEND == "nccl":
+    WORLD_SIZE = os.environ["WORLD_SIZE"]
+
+    class TestDistBackendWithSpawn(MultiProcessTestCase, _DistTestBase):
+        @property
+        def init_method(self):
+            return "file://{file_name}".format(file_name=self.file_name)
+
+        # Needed since MultiProcessTestCase assumes a world_size of 4, but we
+        # run these tests under other various world_sizes.
+        @property
+        def world_size(self):
+            return os.environ["WORLD_SIZE"]
+
+        @classmethod
+        def setUpClass(cls):
+            os.environ["MASTER_ADDR"] = str(MASTER_ADDR)
+            os.environ["MASTER_PORT"] = str(MASTER_PORT)
+            os.environ["WORLD_SIZE"] = str(WORLD_SIZE)
+            super().setUpClass()
+
+        def setUp(self):
+            super().setUp()
+            # initialize Barrier.
+            initialize_temp_directories()
+            Barrier.init()
+            self._spawn_processes()
+
+        def tearDown(self):
+            super(MultiProcessTestCase, self).tearDown()
+            super(TestDistBackendWithSpawn, self).tearDown()
+
+        @classmethod
+        def _run(cls, rank, test_name, file_name):
+            self = cls(test_name)
+            self.rank = rank
+            self.file_name = file_name
+            try:
+                dist.init_process_group(
+                    init_method=self.init_method,
+                    backend=BACKEND,
+                    world_size=int(WORLD_SIZE),
+                    rank=self.rank,
+                )
+            except RuntimeError as e:
+                if "recompile" in e.args[0]:
+                    sys.exit(TEST_SKIPS["backend_unavailable"].exit_code)
+
+                raise
+
+            # Execute barrier prior to running test to ensure that every process
+            # has finished initialization and that the following test
+            # immediately exiting due to a skip doesn't cause flakiness.
+            self._barrier()
+
+            # self.id() == e.g. '__main__.TestDistributed.test_get_rank'
+            # We're retreiving a corresponding test and executing it.
+            getattr(self, test_name)()
+            self._barrier()
+            dist.destroy_process_group()
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    run_tests()
--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -124,7 +124,7 @@ def skip_if_rocm(func):
 
     return wrapper
 
-TIMEOUT_DEFAULT = 100
+TIMEOUT_DEFAULT = 25
 TIMEOUT_OVERRIDE = {}
 
 
@@ -174,6 +174,22 @@ def simple_sparse_reduce_tests(rank, world_size, num_inputs=1):
         ]
     ]
 
+tmp_dir = None
+def initialize_temp_directories(init_method=None):
+    global tmp_dir
+    tmp_dir = tempfile.TemporaryDirectory()
+    os.environ["TEMP_DIR"] = tmp_dir.name
+    os.mkdir(os.path.join(tmp_dir.name, "barrier"))
+    os.mkdir(os.path.join(tmp_dir.name, "test_dir"))
+    init_dir_path = os.path.join(tmp_dir.name, "init_dir")
+    os.mkdir(init_dir_path)
+    # Set init method if specified.
+    if init_method is not None:
+        os.environ["INIT_METHOD"] = init_method
+    else:
+        os.environ["INIT_METHOD"] = "file://" + os.path.join(
+            init_dir_path, "shared_init_file"
+        )
 
 # [How does MultiProcessTestCase work?]
 # Each MultiProcessTestCase instance uses 1 + `world_size()` processes, by
