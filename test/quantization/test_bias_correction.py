5d81f2ef2ef296a3bf91fe0c8c881c1d01a82759
Edmund Williams Jr <edmundjr@fb.com>
2020-07-22 01:06:33 -0700

    Update on "Bias Correction Implementation"
    Differential Revision: [D22661503](https://our.internmc.facebook.com/intern/diff/D22661503)
    
    [ghstack-poisoned]

---
+++ test/quantization/test_bias_correction.py
@@ -0,0 +1,6 @@
+import torch
+from torch.testing._internal.common_quantization import QuantizationTestCase
+import torch.quantization._correct_bias as _correct_bias
+
+class TestBiasCorrection(QuantizationTestCase):
+    pass
