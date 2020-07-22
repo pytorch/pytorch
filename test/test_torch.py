4da9c4ef4fb91d545af94446cbdf463457ab6128
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-07-21 18:14:55 -0700

    add max_and_min function and cpu kernel to speed up observers (#41570)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41570
    
    For min/max based quantization observers, calculating min and max of a tensor
    takes most of the runtime. Since the calculation of min and max is done
    on the same tensor, we can speed this up by only reading the tensor
    once, and reducing with two outputs.
    
    One question I had is whether we should put this into the quantization
    namespace, since the use case is pretty specific.
    
    This PR implements the easier CPU path to get an initial validation.
    There is some needed additional work in future PRs, which durumu will
    take a look at:
    * CUDA kernel and tests
    * making this work per channel
    * benchmarking on observer
    * benchmarking impact on QAT overhead
    
    Test Plan:
    ```
    python test/test_torch.py TestTorch.test_min_and_max
    ```
    
    quick bench (not representative of real world use case):
    https://gist.github.com/vkuzo/7fce61c3456dbc488d432430cafd6eca
    ```
    (pytorch) [vasiliy@devgpu108.ash6 ~/local/pytorch] OMP_NUM_THREADS=1 python ~/nfs/pytorch_scripts/observer_bench.py
    tensor(5.0390) tensor(-5.4485) tensor([-5.4485,  5.0390])
    min and max separate 11.90243935585022
    min and max combined 6.353186368942261
    % decrease 0.466228209277153
    (pytorch) [vasiliy@devgpu108.ash6 ~/local/pytorch] OMP_NUM_THREADS=4 python ~/nfs/pytorch_scripts/observer_bench.py
    tensor(5.5586) tensor(-5.3983) tensor([-5.3983,  5.5586])
    min and max separate 3.468616485595703
    min and max combined 1.8227086067199707
    % decrease 0.4745142294372342
    (pytorch) [vasiliy@devgpu108.ash6 ~/local/pytorch] OMP_NUM_THREADS=8 python ~/nfs/pytorch_scripts/observer_bench.py
    tensor(5.2146) tensor(-5.2858) tensor([-5.2858,  5.2146])
    min and max separate 1.5707778930664062
    min and max combined 0.8645427227020264
    % decrease 0.4496085496757899
    ```
    
    Imported from OSS
    
    Reviewed By: supriyar
    
    Differential Revision: D22589349
    
    fbshipit-source-id: c2e3f1b8b5c75a23372eb6e4c885f842904528ed

--- test/test_torch.py
+++ test/test_torch.py
@@ -275,7 +275,7 @@ class AbstractTestCases:
             self.assertIsNotNone(torch.Tensor([0, 0, 0]).nonzero().storage())
             self.assertIsNotNone(torch.Tensor().new().storage())
 
-        def _testSelection(self, torchfn, mathfn):
+        def _testSelection(self, torchfn, mathfn, skip_indices=False):
             # contiguous
             m1 = torch.randn(100, 100)
             res1 = torchfn(m1)
@@ -294,20 +294,21 @@ class AbstractTestCases:
             self.assertEqual(res1, res2)
 
             # with indices
-            m1 = torch.randn(100, 100)
-            res1val, res1ind = torchfn(m1, 1, False)
-            res2val = m1[:, 0:1].clone().squeeze()
-            res2ind = res1ind.clone().fill_(0)
-            for i, j in iter_indices(m1):
-                if mathfn(res2val[i], m1[i, j]) != res2val[i]:
-                    res2val[i] = m1[i, j]
-                    res2ind[i] = j
+            if not skip_indices:
+                m1 = torch.randn(100, 100)
+                res1val, res1ind = torchfn(m1, 1, False)
+                res2val = m1[:, 0:1].clone().squeeze()
+                res2ind = res1ind.clone().fill_(0)
+                for i, j in iter_indices(m1):
+                    if mathfn(res2val[i], m1[i, j]) != res2val[i]:
+                        res2val[i] = m1[i, j]
+                        res2ind[i] = j
 
-            maxerr = 0
-            for i in range(res1val.size(0)):
-                maxerr = max(maxerr, abs(res1val[i] - res2val[i]))
-                self.assertEqual(res1ind[i], res2ind[i])
-            self.assertLessEqual(abs(maxerr), 1e-5)
+                maxerr = 0
+                for i in range(res1val.size(0)):
+                    maxerr = max(maxerr, abs(res1val[i] - res2val[i]))
+                    self.assertEqual(res1ind[i], res2ind[i])
+                self.assertLessEqual(abs(maxerr), 1e-5)
 
             # NaNs
             for index in (0, 4, 99):
@@ -327,11 +328,31 @@ class AbstractTestCases:
                 res2 = mathfn(res2, m1[i])
             self.assertEqual(res1, res2)
 
+            # Long
+            m1 = torch.LongTensor(100).random_(-1000, 1000)
+            res1 = torchfn(m1)
+            res2 = m1[0]
+            for i in iter_indices(m1):
+                res2 = mathfn(res2, m1[i])
+            self.assertEqual(res1, res2)
+
+
         def test_max(self):
             self._testSelection(torch.max, max)
 
         def test_min(self):
             self._testSelection(torch.min, min)
+
+        def test_min_max(self):
+            # TODO: implement indices, in a future PR
+            # min correctness
+            self._testSelection(lambda x: torch._min_max(x)[0],
+                                lambda x, y: min(x, y),
+                                skip_indices=True)
+            # max correctness
+            self._testSelection(lambda x: torch._min_max(x)[1],
+                                lambda x, y: max(x, y),
+                                skip_indices=True)
 
         def test_dim_reduction_uint8_overflow(self):
             example = [[-1, 2, 1], [5, 3, 6]]
