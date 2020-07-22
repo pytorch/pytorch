8df3e329b96eec6574ce73e828a53b33b6653302
Natalia Gimelshein <ngimel@fb.com>
2020-07-22 09:54:08 -0700

    use parallel kernel for potentially permuted inputs

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -710,14 +710,19 @@ static Tensor & masked_select_out_impl_cpu(Tensor & result, const Tensor & self,
   auto result_strided = result.as_strided(shape, strides);
 
   // serial kernel
-  bool use_serial_kernel = self.numel() < at::internal::GRAIN_SIZE || at::get_num_threads() == 1;
+  // serial kernel requires that src is traversed in its logical order. However, TensorIterator might
+  // have reordered dimensions so that src would be traversed in its physical order, producing wrong
+  // answers. A sufficient condition that no reorder happened is that both _self and _mask is contiguous.
+  // If it is not satisfied, use parallel kernel that handles permutations correctly
+  bool use_serial_kernel = (self.numel() < at::internal::GRAIN_SIZE || at::get_num_threads() == 1 ) &&
+  _self.is_contiguous() && _mask.is_contiguous();
   if (use_serial_kernel) {
     auto iter = TensorIteratorConfig()
       .check_all_same_dtype(false)
       .resize_outputs(false)
       .add_output(result_strided)
-      .add_input(_self.contiguous())
-      .add_input(_mask.contiguous())
+      .add_input(_self)
+      .add_input(_mask)
       .build();
 
     masked_select_serial_stub(iter.device_type(), iter, orig_stride);
