71ecf3fe7b2bbe90e083dd7205ade036baa4d228
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-07-23 13:41:24 -0700

    linter

--- torch/csrc/jit/passes/shape_analysis.cpp
+++ torch/csrc/jit/passes/shape_analysis.cpp
@@ -1267,10 +1267,9 @@ class ShapePropagator {
                       type->withScalarType(maybe_dtype_option->toScalarType())};
                 }
                 if (type->scalarType()) {
-                  return {
-                      at::isFloatingType(*type->scalarType())
-                          ? type
-                          : type->withScalarType(at::kLong)};
+                  return {at::isFloatingType(*type->scalarType())
+                              ? type
+                              : type->withScalarType(at::kLong)};
                 } else {
                   return {type};
                 }
