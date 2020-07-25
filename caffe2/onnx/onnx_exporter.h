6e088031aebb322c215d8f4a2317b067186e1a6d
byeongminP <byeongmin@berkeley.edu>
2020-07-25 05:59:35 -0700

    better-engineering: replace blacklist in caffe2/onnx/onnx_exporter.h

--- caffe2/onnx/onnx_exporter.h
+++ caffe2/onnx/onnx_exporter.h
@@ -112,9 +112,9 @@ class CAFFE2_API OnnxExporter {
       const caffe2::OperatorDef& def,
       const std::unordered_map<std::string, caffe2::TensorShape>& shapes);
 
-  // \brief Check black listed arguments where we won't pass down when
+  // \brief Check blocklisted arguments where we won't pass down when
   // converting to ONNX node
-  bool IsBlackListed(const caffe2::Argument& arg);
+  bool IsBlockListed(const caffe2::Argument& arg);
 
   // \brief Convert Caffe2 argument to Onnx attribute
   void CopyCaffe2ArgToOnnxAttr(
