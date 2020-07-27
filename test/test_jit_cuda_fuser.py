e48d4486148a3ed1b4e0c3c7a1829fc7c26a4f73
Jie <jiej@nvidia.com>
2020-07-27 15:45:37 -0700

    marking failing tests

--- test/test_jit_cuda_fuser.py
+++ test/test_jit_cuda_fuser.py
@@ -133,6 +133,7 @@ class TestCudaFuser(JitTestCase):
         self.assertEqual(o, jit_o)
         self.assertGraphContains(t_jit.graph_for(x, y, 2.0), FUSION_GROUP)
 
+    @unittest.expectedFailure
     @unittest.skipIf(not RUN_CUDA, "requires CUDA")
     @unittest.skipIf(GRAPH_EXECUTOR != ProfilingMode.PROFILING and GRAPH_EXECUTOR !=
                      ProfilingMode.LEGACY, "Requires fusion optimization pass to be effective")
@@ -143,7 +144,6 @@ class TestCudaFuser(JitTestCase):
             o = o + z
             return o
         t_jit = torch.jit.script(t)
-        '''
         x = torch.randn(4, 8, 32, 32, dtype=torch.float, device="cuda")
         y = torch.randn(32, 32, dtype=torch.float, device="cuda")
         jit_o = t_jit(x, y, 2.0)
@@ -151,7 +151,6 @@ class TestCudaFuser(JitTestCase):
         o = t(x, y, 2.0)
         self.assertEqual(o, jit_o)
         self.assertGraphContains(t_jit.graph_for(x, y, 2.0), FUSION_GROUP)
-        '''
         t_jit2 = torch.jit.script(t)
         x = torch.randn(4, 8, 32, 32, dtype=torch.float, device="cuda")
         y = torch.randn(1, 32, 32, dtype=torch.float, device="cuda")
@@ -160,7 +159,6 @@ class TestCudaFuser(JitTestCase):
         o = t(x, y, 2.0)
         self.assertEqual(o, jit_o)
         self.assertGraphContains(t_jit2.graph_for(x, y, 2.0), FUSION_GROUP)
-        '''
         x = torch.randn(4, 1, 32, 32, dtype=torch.float, device="cuda")
         y = torch.randn(8, 32, 32, dtype=torch.float, device="cuda")
         t_jit3 = torch.jit.script(t)
@@ -169,7 +167,6 @@ class TestCudaFuser(JitTestCase):
         o = t(x, y, 2.0)
         self.assertEqual(o, jit_o)
         self.assertGraphContains(t_jit3.graph_for(x, y, 2.0), FUSION_GROUP)
-        '''
 
     @unittest.skipIf(True, "Broadcast with different output not supported yet")
     @unittest.skipIf(not RUN_CUDA, "requires CUDA")
