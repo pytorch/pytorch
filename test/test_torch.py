7c11ad108df9514a600d8f61c350eb8d476c4c64
krshrimali <kushashwaravishrimali@gmail.com>
2020-07-28 08:38:56 +0530

    Add tests for int and complex types

--- test/test_torch.py
+++ test/test_torch.py
@@ -6436,8 +6436,8 @@ class TestTorchDeviceType(TestCase):
 
     @unittest.skipIf(not TEST_NUMPY, 'NumPy not found')
     @onlyOnCPUAndCUDA
-    @dtypesIfCUDA(torch.half, torch.float, torch.double)
-    @dtypes(torch.float, torch.double)
+    @dtypesIfCUDA(torch.int32, torch.int64, torch.complex64, torch.half, torch.float, torch.double)
+    @dtypes(torch.int32, torch.int64, torch.complex64, torch.float, torch.double)
     def test_nanprod(self, device, dtype):
         x = ([[float('nan'), 1.23], [2.06, float('nan')]], [[float('nan'), float('nan')], [float('nan'), float('nan')]])
 
