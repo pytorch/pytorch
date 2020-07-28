6bd88f581a6323d026e08b65ffee75bfe162501f
Mike Ruberry <mruberry@fb.com>
2020-07-28 06:09:02 -0700

    Revert D22790238: [caffe2][tpx] Use logger instead of print
    Test Plan: revert-hammer
    
    Differential Revision:
    D22790238 (https://github.com/pytorch/pytorch/commit/3c6fae6567230da0e08788b968cfb466ec629d65)
    
    Original commit changeset: c0a801cdf7f0
    
    fbshipit-source-id: cadfbd22f7d3ce656624483c9a19062f7c9a5b61

--- test/distributed/test_nccl.py
+++ test/distributed/test_nccl.py
@@ -18,13 +18,7 @@ load_tests = load_tests
 
 nGPUs = torch.cuda.device_count()
 if not TEST_CUDA:
-    import logging
-    import sys
-
-    logger = logging.getLogger('test_nccl')
-    logger.setLevel(logging.INFO)
-    log_handler.setStream(sys.stderr)
-    logger.info('CUDA not available, skipping tests')
+    print('CUDA not available, skipping tests')
     TestCase = object  # noqa: F811
 
 
