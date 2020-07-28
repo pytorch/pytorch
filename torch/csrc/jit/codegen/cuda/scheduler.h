e7766945f0b48c569a9de5aa52843df3f564fa03
Kevin Stephano <kevin.stephano@gmail.com>
2020-07-28 11:47:30 -0700

    Adding capabilities to reduction heuristic to enable kernel caching (#229)
    * Apply reordering of Reduction Schedule in the magic scheduler on unrolling dimensions.  This allows the unroll to be split last but put at the highest dimension of the tensor for easier application of computeAt()
    
    * Fix clang format.
    
    * Added better computeAt() statements for rFactored Tensors of reductions and removed unneeded bindings.  Added thorough comments.
    
    * Adding a new struct LaunchParam that has boolean indicating whether a param is mutable.  This is so the reduction heuristic can properly mark which blocking constants need to be constant during comparison between structs.
    
    * Add equal and hash functions for caching of ReductionParams.
    
    * Fix clang formatting.
    
    * Forgot to only check Launch Param equivalence for Reduction Parameters when the params are not mutable.
    
    * Fixed some debug prints
    
    * Made changes in style based on Leonard's suggestions.
    
    * Made clang format fixes.

--- torch/csrc/jit/codegen/cuda/scheduler.h
+++ torch/csrc/jit/codegen/cuda/scheduler.h
@@ -2,6 +2,7 @@
 
 #include <ATen/core/ivalue.h>
 #include <torch/csrc/jit/codegen/cuda/fusion.h>
+#include <torch/csrc/jit/codegen/cuda/ir_all_nodes.h>
 
 namespace torch {
 namespace jit {
@@ -13,20 +14,62 @@ TORCH_CUDA_API bool scheduleFusion(
     Fusion* fusion,
     const at::ArrayRef<c10::IValue> inputs);
 
+// This struct communicates whether a launch param
+// should be calculated per kernel instance if a new set of elements
+// matches the kernel.
+//
+// is_mutable : indicates whether the param should be matched
+// value : launch param integer
+
+struct LaunchParam {
+  bool is_mutable = false;
+  int value = 1;
+
+  bool operator==(const LaunchParam& other) const {
+    // If the params are mutable, we don't care about the value
+    return (is_mutable && other.is_mutable) || (other.value == value);
+  }
+};
+
 // Parameters the Reduction Heuristic Generates to describe
 // the optimial schedule
 struct ReductionParams {
   // Reduction Blocking
-  int grid_dim_x_ = 1;
-  int grid_dim_y_ = 1;
-  int block_dim_x_ = 1;
-  int block_dim_y_ = 1;
+  LaunchParam gdimx;
+  LaunchParam gdimy;
+  LaunchParam bdimx;
+  LaunchParam bdimy;
 
   // Reduction Attributes
-  bool fastest_dim_ = true;
-  bool cross_block_ = false;
-  bool cross_grid_ = false;
-  bool mul_reds_per_blk_ = false;
+  bool fastest_dim = true;
+  bool cross_block = false;
+  bool cross_grid = false;
+  bool mul_reds_per_blk = false;
+
+  bool operator==(const ReductionParams& other) const {
+    bool lp_equal = other.gdimx == gdimx && other.gdimy == gdimy &&
+        other.bdimx == bdimx && other.bdimy == bdimy;
+    bool attr_equal = other.fastest_dim == fastest_dim &&
+        other.cross_block == cross_block && other.cross_grid == cross_grid &&
+        other.mul_reds_per_blk == mul_reds_per_blk;
+    return attr_equal && lp_equal;
+  }
+};
+
+class ReductionParamsHash {
+ public:
+  size_t operator()(const ReductionParams& rp) const {
+    size_t lp_hash = (rp.gdimx.is_mutable ? 0 : rp.gdimx.value) ^
+        (rp.gdimy.is_mutable ? 0 : rp.gdimy.value) ^
+        (rp.bdimx.is_mutable ? 0 : rp.bdimx.value) ^
+        (rp.bdimy.is_mutable ? 0 : rp.bdimy.value);
+    constexpr size_t bits = sizeof(std::size_t) * 8;
+    size_t attr_hash = static_cast<size_t>(rp.fastest_dim) << (bits - 1) |
+        static_cast<size_t>(rp.cross_block) << (bits - 2) |
+        static_cast<size_t>(rp.cross_grid) << (bits - 3) |
+        static_cast<size_t>(rp.mul_reds_per_blk) << (bits - 4);
+    return lp_hash | attr_hash;
+  }
 };
 
 // TODO: This function is currently a redundant API as I populate a more
