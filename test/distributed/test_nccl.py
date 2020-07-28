a43da99cf2a24e3ca55827a9b779a2730d343af9
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos047.prn3.facebook.com>
2020-07-28 06:01:43 -0700

    2020-07-28 postnightly release (3c6fae6567230da0e08788b968cfb466ec629d65)

--- test/distributed/test_nccl.py
+++ test/distributed/test_nccl.py
@@ -18,7 +18,13 @@ load_tests = load_tests
 
 nGPUs = torch.cuda.device_count()
 if not TEST_CUDA:
-    print('CUDA not available, skipping tests')
+    import logging
+    import sys
+
+    logger = logging.getLogger('test_nccl')
+    logger.setLevel(logging.INFO)
+    log_handler.setStream(sys.stderr)
+    logger.info('CUDA not available, skipping tests')
     TestCase = object  # noqa: F811
 
 
