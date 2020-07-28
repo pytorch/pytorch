075ab10b954bc11a7b17e99228803b316a9e449c
izdeby <iuriiz@fb.com>
2020-07-28 12:39:19 -0700

    utils

---
+++ 1.py
@@ -0,0 +1,3 @@
+import torch
+
+print(torch.cuda.is_available())
---
+++ aten/src/ATen/native/cuda/Utils.cu
@@ -0,0 +1,21 @@
+#pragma once
+#include <ATen/ATen.h>
+#include <ATen/native/cuda/Loops.cuh>
+#include <ATen/native/cuda/MemoryAccess.cuh>
+namespace {
+static constexpr int64_t kILP = 4;
+static constexpr int64_t kChunkSize = 65536;
+static constexpr int64_t kBlockSize = 1024;
+
+template<typename T>
+__device__ __forceinline__ bool is_aligned(T* p){
+  return ((uint64_t)p) % (kILP * sizeof(T)) == 0;
+}
+
+template<typename T>
+__device__ __forceinline__ void load_store(T* dst, T* src, int dst_offset, int src_offset){
+  using LT = at::native::memory::aligned_vector<T, kILP>;
+  ((LT*)dst)[dst_offset] = ((LT*)src)[src_offset];
+}
+
+} // namespace
