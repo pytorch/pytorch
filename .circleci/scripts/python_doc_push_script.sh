622feb650cbd97428440ed68ddb823155937aa34
mattip <matti.picus@gmail.com>
2020-07-28 14:39:19 +0300

    remove unused code

--- .circleci/scripts/python_doc_push_script.sh
+++ .circleci/scripts/python_doc_push_script.sh
@@ -52,11 +52,7 @@ rm -rf pytorch || true
 
 # Get all the documentation sources, put them in one place
 pushd "$pt_checkout"
-# Do not do this, since torchvision is a separate project.
-# checkout_install_torchvision
 pushd docs
-# rm -rf source/torchvision
-# cp -a ../vision/docs/source source/torchvision
 
 # Build the docs
 pip -q install -r requirements.txt
