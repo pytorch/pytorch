6b49cbfe05b7e3fceff8dd55c73767e740f66344
James Reed <jamesreed@fb.com>
2020-07-29 12:26:42 -0700

    Update on "[JIT] Interface for overriding object conversion during script compilation"
    Given any object undergoing conversion during TorchScript compilation, if the object exposes that method and it returns an object that is convertible to IValue, TorchScript will use that object in place of the original object
    
    This can help facilitate use cases like in the TorchText Vocab/Vector classes, where potentially we want to have different objects in Python and TorchScript to--for example--avoid FFI overhead of moving between types in a given runtime
    
    Differential Revision: [D22823202](https://our.internmc.facebook.com/intern/diff/D22823202)
    
    [ghstack-poisoned]

--- torch/csrc/jit/python/pybind_utils.h
+++ torch/csrc/jit/python/pybind_utils.h
@@ -498,7 +498,11 @@ inline IValue toIValue(
     c10::optional<int32_t> N) {
   if (py::hasattr(obj, "__to_ivalue__")) {
     auto method = obj.attr("__to_ivalue__");
-    return toTypeInferredIValue(method());
+    // Precondition here is that `type` has already been set up to refer to the
+    // type of the result of `__to_ivalue__` instead of the type of `obj`
+    //
+    // Currently, this is done in the recursive scripting API on the frontend
+    return toIValue(method(), type, N);
   }
   switch (type->kind()) {
     case TypeKind::TensorType: {
