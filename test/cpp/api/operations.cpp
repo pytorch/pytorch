f435fdf074bb64a644cf50e4035a00be0879795d
Yujun Zhao <yujunzhao@fb.com>
2020-07-29 22:27:56 -0700

    Add test to cross function
    Summary: function `cross_kernel_scalar` is not covered in `Aten/native/cpu/CrossKernel.cpp`, add tests to cover it
    
    Test Plan:
    1. Test locally to check new lines are covered
    2. CI
    
    https://pxl.cl/1fZjG
    
    Reviewed By: malfet
    
    Differential Revision: D22834122
    
    fbshipit-source-id: 7bdbaff516da2cf62f7ca1d261f938638e87fec2

--- test/cpp/api/operations.cpp
+++ test/cpp/api/operations.cpp
@@ -5,10 +5,15 @@
 #include <test/cpp/api/support.h>
 using namespace torch::nn;
 using namespace std;
-struct OperationTest : torch::test::SeedingFixture {};
+struct OperationTest : torch::test::SeedingFixture {
+ protected:
+  void SetUp() override {}
+
+  const int TEST_AMOUNT = 10;
+};
 
 TEST_F(OperationTest, Lerp) {
-  for (auto i = 0; i < 10; i++) {
+  for (auto i = 0; i < TEST_AMOUNT; i++) {
     // test lerp_kernel_scalar
     auto start = torch::rand({3, 5});
     auto end = torch::rand({3, 5});
@@ -28,5 +33,27 @@ TEST_F(OperationTest, Lerp) {
     // compare
     ASSERT_EQ(out.dtype(), tensor_expected.dtype());
     ASSERT_TRUE(out.allclose(tensor_expected));
+  }
+}
+
+TEST_F(OperationTest, Cross) {
+  for (auto i = 0; i < TEST_AMOUNT; i++) {
+    // input
+    auto a = torch::rand({10, 3});
+    auto b = torch::rand({10, 3});
+    // expected
+    auto exp = torch::empty({10, 3});
+    for (int j = 0; j < 10; j++) {
+      auto u1 = a[j][0], u2 = a[j][1], u3 = a[j][2];
+      auto v1 = b[j][0], v2 = b[j][1], v3 = b[j][2];
+      exp[j][0] = u2 * v3 - v2 * u3;
+      exp[j][1] = v1 * u3 - u1 * v3;
+      exp[j][2] = u1 * v2 - v1 * u2;
+    }
+    // actual
+    auto out = torch::cross(a, b);
+    // compare
+    ASSERT_EQ(out.dtype(), exp.dtype());
+    ASSERT_TRUE(out.allclose(exp));
   }
 }
