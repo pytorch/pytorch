76796189f9ccf155b6ad8d2dfde7d3d5f34af49d
ziab <danilviaz@gmail.com>
2020-07-29 21:29:02 -0700

    Move device_count() definition to the cpp file

--- c10/cuda/CUDAFunctions.cpp
+++ c10/cuda/CUDAFunctions.cpp
@@ -3,6 +3,25 @@
 namespace c10 {
 namespace cuda {
 
+DeviceIndex device_count() noexcept {
+  int count;
+  // NB: In the past, we were inconsistent about whether or not this reported
+  // an error if there were driver problems are not.  Based on experience
+  // interacting with users, it seems that people basically ~never want this
+  // function to fail; it should just return zero if things are not working.
+  // Oblige them.
+  cudaError_t err = cudaGetDeviceCount(&count);
+  if (err != cudaSuccess) {
+    // Clear out the error state, so we don't spuriously trigger someone else.
+    // (This shouldn't really matter, since we won't be running very much CUDA
+    // code in this regime.)
+    cudaError_t last_err = cudaGetLastError();
+    (void)last_err;
+    return 0;
+  }
+  return static_cast<DeviceIndex>(count);
+}
+
 DeviceIndex current_device() {
   int cur_device;
   C10_CUDA_CHECK(cudaGetDevice(&cur_device));
--- c10/cuda/CUDAFunctions.h
+++ c10/cuda/CUDAFunctions.h
@@ -17,33 +17,18 @@
 namespace c10 {
 namespace cuda {
 
-inline DeviceIndex device_count() noexcept {
-  int count;
-  // NB: In the past, we were inconsistent about whether or not this reported
-  // an error if there were driver problems are not.  Based on experience
-  // interacting with users, it seems that people basically ~never want this
-  // function to fail; it should just return zero if things are not working.
-  // Oblige them.
-  cudaError_t err = cudaGetDeviceCount(&count);
-  if (err != cudaSuccess) {
-    // Clear out the error state, so we don't spuriously trigger someone else.
-    // (This shouldn't really matter, since we won't be running very much CUDA
-    // code in this regime.)
-    cudaError_t last_err = cudaGetLastError();
-    (void)last_err;
-    return 0;
-  }
-  return static_cast<DeviceIndex>(count);
-}
+C10_CUDA_API DeviceIndex device_count() noexcept;
 
 C10_CUDA_API DeviceIndex current_device();
 
 C10_CUDA_API void set_device(DeviceIndex device);
 
-// Returns a pair of an int containing the version number and a string containing an error description,
-// if the string is not empty then the function has failed and the integer value should be discarded
+// Returns a pair of an int containing the version number and a string
+// containing an error description, if the string is not empty then the function
+// has failed and the integer value should be discarded
 C10_CUDA_API std::pair<int32_t, std::string> driver_version();
 
 C10_CUDA_API void device_synchronize();
 
-}} // namespace c10::cuda
+} // namespace cuda
+} // namespace c10
