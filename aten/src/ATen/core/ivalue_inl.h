pytorch/pytorch

commit 160d1e1823840eb89354d719381f37c73a99df22

[NCCL] DDP communication hook: getFuture() (#41596)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/41596

We've modified the previous design of `convert_dist_work_to_future` API in the GH Issue [#39272](https://github.com/pytorch/pytorch/issues/39272).

1. Whenever we create a `WorkNCCL` object, create a `Future` associated with `WorkNCCL` and store it with the object.
2. Add an API `c10::intrusive_ptr<c10::ivalue::Future> getFuture()` to `c10d::ProcessGroup::Work`.
3. This API will only be supported by NCCL in the first version, the default implementation will throw UnsupportedOperation.
4. To mark the future associated with WorkNCCL completed, implement a `cudaStreamCallback` function.

`cudaStreamAddCallback` is marked as deprecated. An alternative is `cudaLaunchHostFunc`, but it is supported for CUDA > 10 and may not be deprecated until there's a reasonable alternative available according to [this discussion](https://stackoverflow.com/questions/56448390/how-to-recover-from-cuda-errors-when-using-cudalaunchhostfunc-instead-of-cudastr).
ghstack-source-id: 108409748

Test Plan:
Run old  python test/distributed/test_c10d.py.
Some additional tests:
`test_ddp_comm_hook_allreduce_hook_nccl`: This unit test verifies whether a DDP communication hook that just calls allreduce gives the same result result with the case of no hook registered.  Without the then callback, the future_value in reducer is no longer a PyObject, and this unit test verifies future_value is properly checked.
`test_ddp_comm_hook_allreduce_then_mult_ten_hook_nccl`: This unit test verifies whether a DDP communication hook that calls allreduce and then multiplies the result by ten gives the expected result.

As of v10:
```
........................s.....s.....................................................s...............................
----------------------------------------------------------------------
Ran 116 tests

OK (skipped=3)
```

torch/csrc/distributed/c10d/init.cpp
@@ -24,6 +24,7 @@
 #include <torch/csrc/Exceptions.h>
 #include <torch/csrc/distributed/c10d/comm.h>
 #include <torch/csrc/distributed/c10d/reducer.h>
+#include <torch/csrc/jit/python/pybind_utils.h>
 #include <torch/csrc/utils/object_ptr.h>
 #include <torch/csrc/utils/pybind.h>
 
@@ -700,7 +701,23 @@ They are used in specifying strategies for reduction collectives, e.g.,
           "wait",
           &::c10d::ProcessGroup::Work::wait,
           py::arg("timeout") = kNoTimeout,
-          py::call_guard<py::gil_scoped_release>());
+          py::call_guard<py::gil_scoped_release>())
+      .def(
+          "get_future",
+          [](::c10d::ProcessGroup::Work& work)
+              -> std::shared_ptr<jit::PythonFutureWrapper> {
+            return std::make_shared<jit::PythonFutureWrapper>(work.getFuture());
+          },
+          py::call_guard<py::gil_scoped_release>(),
+          R"(
+            ``get_future`` retrieves a future associated with the completion of
+            ``c10d.ProcessGroup.work``. As an example, a future object can be set
+            by `future_work = dist.allreduce(tensors).get_future()`. `future_work`
+            will be marked as completed once ``dist.allreduce`` work is finished.
+
+            .. warning ::
+                ``get_future`` API supports only NCCL backend.
+           )");
 
   module.def(
       "_compute_bucket_assignment_by_size",

torch/lib/c10d/ProcessGroupNCCL.hpp
@@ -8,6 +8,7 @@
 #include <c10d/ProcessGroup.hpp>
 #include <c10d/Store.hpp>
 
+#include <ATen/Parallel.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <ATen/cuda/CUDAEvent.h>
 
@@ -77,9 +78,15 @@ class ProcessGroupNCCL : public ProcessGroup {
     // completion.
     void synchronize() override;
 
+    // Synchronize streams by blocking each on the NCCL stream
+    void synchronizeStreams();
+
     // Helper function that checks if the NCCL kernels have finished
     // execution on the GPUs
     bool finishedGPUExecution();
+
+    // Get a Future object that will be marked as completed internally.
+    c10::intrusive_ptr<c10::ivalue::Future> getFuture() override;
 
    protected:
     // The cached list of CUDA devices to operate on
@@ -125,7 +132,68 @@ class ProcessGroupNCCL : public ProcessGroup {
     // to the store.
     std::shared_ptr<Store> store_;
 
+    // Store a Future work associated with WorkNCCL.
+    c10::intrusive_ptr<c10::ivalue::Future> futureWork_;
+
     friend class ProcessGroupNCCL;
+  };
+
+  // CheckFutureWork is used by a cudaStremCallback function called
+  // ncclKernelCompletionCallback. The purpose of CheckFutureWork is to mark
+  // workNCCL's Future work when, all NCCL streams created in collective
+  // operation are finished. We use enable_shared_from_this to safely call
+  // shared_ptr<CheckFutureWork> (this) to remove its pointer from
+  // ProcessGroupNCCL's checkFutObjs after at::launch operation of
+  // markFutureCompleted is done. markFutureCompleted increments streamCounter
+  // by one and once it is called by number of replicas times, it marks Future
+  // work completed by setting outputs vector as its value.
+  struct CheckFutureWork : std::enable_shared_from_this<CheckFutureWork> {
+   public:
+    CheckFutureWork(
+        std::shared_ptr<ProcessGroupNCCL::WorkNCCL> work,
+        std::vector<at::Tensor>& outputs,
+        std::shared_ptr<std::unordered_set<std::shared_ptr<CheckFutureWork>>>
+            checkFutObjs,
+        std::mutex& checkFutObjMutex_)
+        : work_(work),
+          outputs_(std::make_shared<std::vector<at::Tensor>>(outputs)),
+          streamCounter_(0),
+          checkFutObjs_(checkFutObjs),
+          checkFutObjMutex_(checkFutObjMutex_) {}
+
+    std::shared_ptr<CheckFutureWork> getPtr() {
+      return shared_from_this();
+    }
+
+    void markFutureCompleted() {
+      // Passing `this` here as the capture is fine because we store a
+      // `shared_ptr` to `this` in ProcessGroupNCCL's `checkFutObjs_` to
+      // ensure that the object is alive when the lambda function is called.
+      at::launch(([this]() {
+        if (++streamCounter_ == (*outputs_).size()) {
+          // Need to synchronize before passing outputs to Future because
+          // operations using the outputs might be running on different streams
+          work_->synchronizeStreams();
+
+          TORCH_CHECK(
+              !work_->getFuture()->completed(),
+              "Future work of workNCCL can only be marked as "
+              "completed by ncclKernelCompletionCallback.")
+          work_->getFuture()->markCompleted(at::IValue(*outputs_));
+
+          std::unique_lock<std::mutex> lock(checkFutObjMutex_);
+          checkFutObjs_->erase(getPtr());
+        }
+      }));
+    }
+
+   private:
+    std::shared_ptr<ProcessGroupNCCL::WorkNCCL> work_;
+    std::shared_ptr<std::vector<at::Tensor>> outputs_;
+    std::atomic<int> streamCounter_;
+    std::shared_ptr<std::unordered_set<std::shared_ptr<CheckFutureWork>>>
+        checkFutObjs_;
+    std::mutex& checkFutObjMutex_;
   };
 
   // If you wish to create multiple process groups, each with a potentially
@@ -383,6 +451,15 @@ class ProcessGroupNCCL : public ProcessGroup {
   // for this map since only the watchdog thread accesses this set. The
   // set contains the string representation of ncclUniqueId.
   std::unordered_set<std::string> abortedComms_;
+
+  // The set of CheckFutureWork pointers ensures that the CheckFutureWork object
+  // is not deleted before the at::launch operation inside CheckFutureWork's
+  // markFutureCompleted called by cudaStremCallback function.
+  std::shared_ptr<std::unordered_set<std::shared_ptr<CheckFutureWork>>>
+      checkFutObjs_;
+
+  // Mutex to guard the unordered set checkFutObjs_.
+  mutable std::mutex checkFutObjMutex_;
 };
 
 } // namespace c10d

test/distributed/test_c10d.py
@@ -3015,17 +3015,11 @@ class DistributedDataParallelTest(MultiProcessTestCase):
         # Register DDP Communication Hook
         cpu_model._register_comm_hook(None, self._simple_hook)
 
-        self._run_and_verify_simple_hook(cpu_model)
+        # check whether the grads are equal to what then callback returns.
+        # without the comm_hook, result would be 0.25 * torch.ones(2, 2).
+        self._run_and_verify_hook(cpu_model, 8, 2 * torch.ones(2, 2))
 
-    def _test_ddp_comm_hook_future_passing_gpu(self, c10d_process_group):
-        """
-        This unit test verifies whether the Future object is passed properly.
-        The callback function creates a Future object and sets a value to it.
-        """
-        store = c10d.FileStore(self.file_name, self.world_size)
-        process_group = c10d_process_group(store, self.rank, self.world_size)
-
-        # Test on GPU
+    def _gpu_model_with_ddp_comm_hook(self, process_group, hook=None):
         device_id = gpus_for_rank(self.world_size)[self.rank][0]
         gpu_model = DistributedDataParallel(
             TestDdpCommHook().to(device_id),
@@ -3033,10 +3027,20 @@ class DistributedDataParallelTest(MultiProcessTestCase):
             process_group=process_group
         )
 
-        # Register DDP Communication Hook
-        gpu_model._register_comm_hook(None, self._simple_hook)
+        # Register DDP Communication Hook if defined
+        if hook is not None:
+            gpu_model._register_comm_hook(None, hook)
 
-        self._run_and_verify_simple_hook(gpu_model)
+        return gpu_model
+
+    def _run_and_verify_hook(self, model, input, expected_grad):
+        # Run forward
+        output = model(input, self.rank)
+
+        # Run backward
+        output.mean().backward()
+
+        [self.assertEqual(p.grad, expected_grad) for p in model.parameters()]
 
     def _simple_hook(self, state: object, bucket: dist.GradBucket) -> torch.futures.Future:
         fut = torch.futures.Future()
@@ -3048,32 +3052,86 @@ class DistributedDataParallelTest(MultiProcessTestCase):
 
         return fut.then(fut_then)
 
-    def _run_and_verify_simple_hook(self, model):
-        # Run forward
-        output = model(8, self.rank)
-
-        # Run backward
-        output.mean().backward()
-
-        # check whether the grads are equal to what then callback returns.
-        # without the comm_hook, result would be 0.25 * torch.ones(2, 2).
-        [self.assertEqual(p.grad, 2 * torch.ones(2, 2)) for p in model.parameters()]
-
     @requires_gloo()
     @skip_if_lt_x_gpu(2)
     def test_ddp_comm_hook_future_passing_gpu_gloo(self):
         """
-        This unit test executes _test_ddp_comm_hook_future_passing_gpu using gloo backend.
+        This unit test verifies whether the Future object is passed properly using gloo backend.
+        The hook callback function creates a Future object and sets a value to it.
         """
-        self._test_ddp_comm_hook_future_passing_gpu(c10d.ProcessGroupGloo)
+        store = c10d.FileStore(self.file_name, self.world_size)
+        process_group = c10d.ProcessGroupGloo(store, self.rank, self.world_size)
+
+        # Get GPU model with simple_hook registered.
+        gpu_model = self._gpu_model_with_ddp_comm_hook(process_group, self._simple_hook)
+
+        # check whether the grads are equal to what simple_hook's then callback returns.
+        # without the comm_hook, result would be 0.25 * torch.ones(2, 2).
+        self._run_and_verify_hook(gpu_model, 8 , 2 * torch.ones(2, 2))
 
     @requires_nccl()
     @skip_if_lt_x_gpu(2)
     def test_ddp_comm_hook_future_passing_gpu_nccl(self):
         """
-        This unit test executes _test_ddp_comm_hook_future_passing_gpu using nccl backend.
+        This unit test verifies whether the Future object is passed properly using nccl backend.
+        The hook callback function creates a Future object and sets a value to it.
         """
-        self._test_ddp_comm_hook_future_passing_gpu(c10d.ProcessGroupNCCL)
+        store = c10d.FileStore(self.file_name, self.world_size)
+        process_group = c10d.ProcessGroupNCCL(store, self.rank, self.world_size)
+
+        # Get GPU model with simple_hook registered.
+        gpu_model = self._gpu_model_with_ddp_comm_hook(process_group, self._simple_hook)
+
+        # check whether the grads are equal to what simple_hook's then callback returns.
+        # without the comm_hook, result would be 0.25 * torch.ones(2, 2).
+        self._run_and_verify_hook(gpu_model, 8 , 2 * torch.ones(2, 2))
+
+    @requires_nccl()
+    @skip_if_lt_x_gpu(2)
+    def test_ddp_comm_hook_allreduce_hook_nccl(self):
+        """
+        This unit test verifies whether a DDP communication hook that just calls
+        allreduce gives the same result result with the case of no hook registered.
+        Without the then callback, the future_value in reducer is no longer
+        a PyObject, and this unit test verifies future_value is properly checked.
+        """
+        store = c10d.FileStore(self.file_name, self.world_size)
+        process_group = c10d.ProcessGroupNCCL(store, self.rank, self.world_size)
+
+        def allreduce_hook(state: object, bucket: dist.GradBucket) -> torch.futures.Future:
+            return process_group.allreduce(bucket.get_tensors()).get_future()
+
+        # Get GPU model with allreduce_hook registered.
+        gpu_model = self._gpu_model_with_ddp_comm_hook(process_group, allreduce_hook)
+
+        # check whether the grads are equal to what DDP without hook would return.
+        self._run_and_verify_hook(gpu_model, 8, 0.25 * torch.ones(2, 2))
+
+    @requires_nccl()
+    @skip_if_lt_x_gpu(2)
+    def test_ddp_comm_hook_allreduce_then_mult_hook_nccl(self):
+        """
+        This unit test verifies whether a DDP communication hook that calls allreduce and then
+        multiplies the result by ten gives the expected result.
+        """
+        store = c10d.FileStore(self.file_name, self.world_size)
+        process_group = c10d.ProcessGroupNCCL(store, self.rank, self.world_size)
+
+        def allreduce_then_mult_hook(state: object, bucket: dist.GradBucket) -> torch.futures.Future:
+            fut = process_group.allreduce(bucket.get_tensors()).get_future()
+
+            def fut_then(fut):
+                # Multiply result by 10.
+                return [10 * t for t in fut.wait()]
+
+            return fut.then(fut_then)
+
+        # Get GPU model with allreduce_then_mult_hook registered.
+        gpu_model = self._gpu_model_with_ddp_comm_hook(process_group, allreduce_then_mult_hook)
+
+        # check whether the grads are equal to what allreduce returns multuplied by 10.
+        # without the comm_hook, result would be still 0.25 * torch.ones(2, 2).
+        self._run_and_verify_hook(gpu_model, 8, 2.5 * torch.ones(2, 2))
 
     @requires_gloo()
     def test_ddp_invalid_comm_hook_init(self):

torch/csrc/distributed/c10d/comm.cpp
@@ -113,14 +113,18 @@ c10::intrusive_ptr<torch::jit::Future> PythonCommHook::runHook(
 
 std::vector<at::Tensor> PythonCommHook::processFuture(
     c10::IValue future_value) {
-  // Since we have a Python hook, future_value is a PyObject. Therefore, we
-  // first convert it to an IValue that contains a TensorVector.
-  py::object obj =
-      py::reinterpret_borrow<py::object>(future_value.toPyObject());
-  auto value =
-      torch::jit::toIValue(obj, c10::ListType::create(c10::TensorType::get()));
+  // Since we have a Python hook, future_value can be a PyObject.
+  if (future_value.isPyObject()) {
+    // We first convert it to an IValue that contains a TensorVector.
+    py::object obj =
+        py::reinterpret_borrow<py::object>(future_value.toPyObject());
+    auto value = torch::jit::toIValue(
+        obj, c10::ListType::create(c10::TensorType::get()));
 
-  return value.toTensorVector();
+    return value.toTensorVector();
+  }
+
+  return future_value.toTensorVector();
 }
 
 } // namespace c10d

torch/csrc/distributed/c10d/comm.h
@@ -21,6 +21,7 @@ void broadcast_coalesced(
 class GradBucket {
  public:
   explicit GradBucket(std::vector<at::Tensor> tensors);
+
   const std::vector<at::Tensor>& getTensors();
 
  private:

torch/lib/c10d/ProcessGroup.cpp
@@ -55,8 +55,11 @@ bool ProcessGroup::Work::wait(std::chrono::milliseconds timeout) {
   return true;
 }
 
-void ProcessGroup::Work::abort() {
-  TORCH_CHECK(false, "ProcessGroup::Work::abort not implemented.")
+void ProcessGroup::Work::abort(){
+    TORCH_CHECK(false, "ProcessGroup::Work::abort not implemented.")}
+
+c10::intrusive_ptr<c10::ivalue::Future> ProcessGroup::Work::getFuture() {
+  TORCH_CHECK(false, "ProcessGroup::Work::getFuture not implemented.")
 }
 
 void ProcessGroup::Work::finish(std::exception_ptr exception) {

torch/lib/c10d/ProcessGroup.hpp
@@ -89,6 +89,10 @@ class ProcessGroup {
 
     virtual void abort();
 
+    // Returns a Future object that will be marked as completed once work is
+    // completed. Only NCCL backend is currently supported.
+    virtual c10::intrusive_ptr<c10::ivalue::Future> getFuture();
+
    protected:
     // Completes the work object and optionally sets the exception in a
     // thread-safe manner. Notifies all waiting condition variables as well.

torch/lib/c10d/ProcessGroupNCCL.cpp
@@ -143,6 +143,9 @@ ProcessGroupNCCL::WorkNCCL::WorkNCCL(const std::vector<at::Device>& devices)
   // DEFAULT_FLAGS = cudaEventDisableTiming.
   cudaEvents_.resize(devices.size());
   ncclComms_.resize(devices.size());
+
+  futureWork_ = c10::make_intrusive<c10::ivalue::Future>(
+      c10::ListType::create(c10::TensorType::get()));
 }
 
 ProcessGroupNCCL::WorkNCCL::~WorkNCCL() {}
@@ -208,14 +211,18 @@ void ProcessGroupNCCL::WorkNCCL::synchronize() {
   synchronizeInternal(kNoTimeout);
 }
 
-// Waiting on the work's corresponding CUDA events
-void ProcessGroupNCCL::WorkNCCL::synchronizeInternal(
-    std::chrono::milliseconds timeout) {
+void ProcessGroupNCCL::WorkNCCL::synchronizeStreams() {
   for (size_t i = 0; i < devices_.size(); ++i) {
     auto currentStream = at::cuda::getCurrentCUDAStream(devices_[i].index());
     // Block the current stream on the NCCL stream
     cudaEvents_[i].block(currentStream);
   }
+}
+
+// Waiting on the work's corresponding CUDA events
+void ProcessGroupNCCL::WorkNCCL::synchronizeInternal(
+    std::chrono::milliseconds timeout) {
+  synchronizeStreams();
 
   // In case of blocking, wait for the operation to complete.
   if (blockingWait_) {
@@ -299,7 +306,10 @@ ProcessGroupNCCL::ProcessGroupNCCL(
       store_(store),
       ncclCommCounter_(0),
       terminateWatchdog_(false),
-      opTimeout_(opTimeout) {
+      opTimeout_(opTimeout),
+      checkFutObjs_(
+          std::make_shared<std::unordered_set<
+              std::shared_ptr<ProcessGroupNCCL::CheckFutureWork>>>(0)) {
   try {
     parseNcclBlockingWait();
   } catch (std::exception& e) {
@@ -660,6 +670,24 @@ std::shared_ptr<ProcessGroupNCCL::WorkNCCL> ProcessGroupNCCL::initWork(
   return std::make_shared<ProcessGroupNCCL::WorkNCCL>(devices);
 }
 
+c10::intrusive_ptr<c10::ivalue::Future> ProcessGroupNCCL::WorkNCCL::
+    getFuture() {
+  return futureWork_;
+}
+
+void ncclKernelCompletionCallback(
+    cudaStream_t /* unused */,
+    cudaError_t /* unused */,
+    void* userData) {
+  auto castedUserData =
+      static_cast<ProcessGroupNCCL::CheckFutureWork*>(userData);
+
+  TORCH_CHECK(
+      castedUserData, "Null castedUserData in ncclKernelCompletionCallback.");
+
+  castedUserData->markFutureCompleted();
+};
+
 template <typename Fn, typename PreProcess, typename PostProcess>
 std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::collective(
     std::vector<at::Tensor>& inputs,
@@ -697,6 +725,14 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::collective(
         inputs[i].storage().data_ptr(), ncclStream);
   }
 
+  // Create and store a CheckFutureWork object to be used in
+  // ncclKernelCompletionCallback function.
+  auto checkFutObj = std::make_shared<ProcessGroupNCCL::CheckFutureWork>(
+      work, outputs, checkFutObjs_, checkFutObjMutex_);
+
+  std::unique_lock<std::mutex> lock(checkFutObjMutex_);
+  checkFutObjs_->insert(checkFutObj);
+
   {
     AutoNcclGroup nccl_group_guard;
     for (size_t i = 0; i < inputs.size(); ++i) {
@@ -704,6 +740,13 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::collective(
       at::cuda::CUDAStream& ncclStream = ncclStreams_[key][i];
       C10D_NCCL_CHECK(
           fn(inputs[i], outputs[i], ncclComms[i]->getNcclComm(), ncclStream));
+
+      // Add a cudaStreamCallback on the same stream. CUDA will automatically
+      // call ncclKernelCompletionCallback once the appropriate NCCL kernel
+      // is done and workNCCL's Future work will be marked as completed
+      // using this callback.
+      cudaStreamAddCallback(
+          ncclStream, ncclKernelCompletionCallback, checkFutObj.get(), 0);
     }
   }
 

commit 1748b6a77beae257708f47f7d904bfe683264fe4

Update on "[NCCL] DDP communication hook: getFuture() without cudaStreamAddCallback"
**Main goal:** For DDP communication hook, provide an API called "get_future" to retrieve a future associated with the completion of c10d.ProcessGroupNCCL.work. Enable NCCL support for this API in this diff.

We add an API `c10::intrusive_ptr<c10::ivalue::Future> getFuture()` to `c10d::ProcessGroup::Work`. This API will only be supported by NCCL in the first version, the default implementation will throw UnsupportedOperation.

We no longer consider a design that involves cudaStreamAddCallback which potentially was causing performance regression in [#41596](https://github.com/pytorch/pytorch/pull/41596). See the following approach:

**New design:**
1. As we create a `WorkNCCL` object inside `ProcessGroupNCCL::collective`, we also create a `FutureNCCL` (sub class of `Future`) associated with `WorkNCCL` and store it with the object. This future has a reference to the original work object.
2. The Future is marked as completed when its created (to allow for async execution of callbacks).
3. fut.wait() simply synchronizes the streams (synchronizeStreams() in Sinan's diff) and returns the result. This preserves the async execution model for CUDA.
4. When we add a callback to this future (.then()), we synchronizeStreams() and invoke the callback inline.

Differential Revision: [D22833298](https://our.internmc.facebook.com/intern/diff/D22833298/)

[ghstack-poisoned]

aten/src/ATen/core/ivalue_inl.h
@@ -268,7 +268,7 @@ struct C10_EXPORT ivalue::Future : c10::intrusive_ptr_target {
   /**
    * Explicitly mark the future as completed with the output value.
    */
-  void markCompleted(IValue value) {
+  virtual void markCompleted(IValue value) {
     std::unique_lock<std::mutex> lock(mutex_);
     TORCH_CHECK(
         !completed(),
@@ -402,7 +402,10 @@ struct C10_EXPORT ivalue::Future : c10::intrusive_ptr_target {
   }
 
  protected:
+  mutable std::mutex mutex_;
   std::atomic_bool completed_ = {false}; // is this future complete
+
+  IValue value_; // when finished the value
 
  private:
   void setErrorInternal(
@@ -422,10 +425,8 @@ struct C10_EXPORT ivalue::Future : c10::intrusive_ptr_target {
     }
   }
 
-  mutable std::mutex mutex_;
   std::condition_variable finished_cv_;
 
-  IValue value_; // when finished the value
   TypePtr type_;
   std::vector<std::function<void(void)>> callbacks_;
   c10::optional<FutureError> error_;

torch/csrc/distributed/c10d/init.cpp
@@ -711,13 +711,21 @@ They are used in specifying strategies for reduction collectives, e.g.,
           py::call_guard<py::gil_scoped_release>(),
           R"(
             ``get_future`` retrieves a future associated with the completion of
-            ``c10d.ProcessGroup.work``. As an example, a future object can be set by
-            `future_work = dist.allreduce(tensors).get_future()`. `future_work` wait()
-            and then() callbacks will return once ``dist.allreduce`` is finished.
+            ``c10d.ProcessGroup.work``. As an example, a future object can be set
+            by `fut = dist.allreduce(tensors).get_future()`.
+
+            In the example above, if `dist.allreduce` work was done on GPU,
+            `fut.wait()` would return after synchronizing the correct GPU streams
+            to ensure we can have async CUDA execution and it does not wait for
+            the entire operation to complete on GPU. If NCCL_BLOCKING_WAIT is enabled,
+            in that case, it would wait for the entire operation to complete before
+            returning. In addition, if a callback function was added by `fut.then()`,
+            it will synchronize the appropriate streams and invoke the callback inline.
+
+            Note that `fut.done()` actually returns if the work was completed on the GPU.
 
             .. warning ::
-                ``get_future`` API supports only NCCL backend. For NCCL backend,
-                it returns a FutureNCCL which is a sub class of Future.
+                ``get_future`` API supports only NCCL backend.
            )");
 
   module.def(

torch/lib/c10d/ProcessGroupNCCL.hpp
@@ -139,44 +139,50 @@ class ProcessGroupNCCL : public ProcessGroup {
   };
 
   // FutureNCCL is a subclass of c10d ivalue's Future. FutureNCCL has a
-  // reference to WorkNCCL and our goal is to use this class in getFuture API of
-  // WorkNCCL. Once NCCL kernel is done executing, this future will have NCCL
-  // collective's outputs as its value. Inside FutureNCCL's constructor, we mark
-  // it as completed with NCCL collective's unfinished outputs to allow async
-  // execution of callbacks. We next set its completed false until NCCL streams
-  // are synchronized in wait() or addCallback() (called by Future's then()).
+  // reference to WorkNCCL and NCCL collective's outputs. The goal is to
+  // use this class in getFuture API of WorkNCCL. Once NCCL kernel is done
+  // executing, this future will have NCCL collective's outputs as its value.
+  // Inside FutureNCCL's constructor, we mark it as completed with NCCL
+  // collective's unfinished outputs to allow async execution of callbacks. We
+  // next set its completed false until NCCL streams are synchronized in wait()
+  // or addCallback() (called by Future's then()).
   //
-  // We override wait() and addCallback() of Future for our purpose.
-  // Inside wait(), we simply synchronize the streams using WorkNCCL's
-  // synchronizeStreams(). This preserves the async execution model for CUDA.
-  // When we add a callback to this future (.then()), we first
-  // synchronizeStreams() and invoke the callback inline.
-  // Callbacks return a Future (not FutureNCCL).
+  // It overrides wait(), markCompleted(), and addCallback() methods of Future.
   struct FutureNCCL : at::ivalue::Future {
    public:
     explicit FutureNCCL(
         std::shared_ptr<ProcessGroupNCCL::WorkNCCL> work,
         std::vector<at::Tensor>& outputs)
         : at::ivalue::Future(c10::ListType::create(c10::TensorType::get())),
-          work_(work) {
-      markCompleted(at::IValue(outputs));
-      completed_ = false;
+          work_(work),
+          outputs_(std::make_shared<std::vector<at::Tensor>>(outputs)) {}
+
+    // Simply calls WorkNCCL's wait(). After WorkNCCL's wait() returns it calls
+    // markCompleted using collective operation's outputs. It will return after
+    // synchronizing the correct GPU streams to ensure we can have async CUDA
+    // execution and it does not wait for the entire operation to complete on
+    // GPU. If NCCL_BLOCKING_WAIT is enabled, in that case, it will wait for the
+    // entire operation to complete before returning.
+    void wait() override {
+      work_->wait();
+      markCompleted(at::IValue(*outputs_));
     }
 
-    // Synchronizes the streams using WorkNCCL's synchronizeStreams(). After
-    // that, it sets completed true. No need to write the value again, since it
-    // is already set in constructor.
-    void wait() override {
-      work_->synchronizeStreams();
+    // Just sets the value without calling then callbacks, because callbacks
+    // were invoked inline by addCallback().
+    void markCompleted(at::IValue value) override {
       completed_ = true;
-    };
+
+      std::unique_lock<std::mutex> lock(mutex_);
+      value_ = std::move(value);
+    }
 
     // Add a callback to FutureNCCL. FutureNCCL invokes the callback inline
-    // after NCCL streams are synchronized. Callbacks return a Future (not
-    // FutureNCCL).
-    void addCallback(std::function<void(void)> callback) {
-      work_->synchronizeStreams();
-      completed_ = true;
+    // after WorkNCCL wait() returns.
+    // Callbacks return a Future (not FutureNCCL).
+    void addCallback(std::function<void(void)> callback) override {
+      work_->wait();
+      markCompleted(at::IValue(*outputs_));
 
       // Run callback inline after synchronizeStreams.
       callback();
@@ -184,6 +190,7 @@ class ProcessGroupNCCL : public ProcessGroup {
 
    private:
     std::shared_ptr<ProcessGroupNCCL::WorkNCCL> work_;
+    std::shared_ptr<std::vector<at::Tensor>> outputs_;
   };
 
   // If you wish to create multiple process groups, each with a potentially