3670d5a9650851ef7f9853f204f90c71fb62ea4f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-07-31 12:31:22 -0700

    Update Loops.cuh

--- aten/src/ATen/native/cuda/Loops.cuh
+++ aten/src/ATen/native/cuda/Loops.cuh
@@ -148,7 +148,7 @@ template <int num_outputs, typename func_t, typename array_t, typename inp_calc_
 C10_LAUNCH_BOUNDS_1(num_threads)
 __global__ void unrolled_elementwise_kernel_for_multi_outputs(int N, func_t f, array_t data, inp_calc_t ic, out_calc_t oc) {
   int remaining = N - block_work_size * blockIdx.x;
-  modern::elementwise_kernel_helper(f, memory::policies::multi_outputs_unroll<array_t, inp_calc_t, out_calc_t, num_outputs>(data, remaining, ic, oc));
+  elementwise_kernel_helper(f, memory::policies::multi_outputs_unroll<array_t, inp_calc_t, out_calc_t, num_outputs>(data, remaining, ic, oc));
 }
 
 template <int num_outputs, typename func_t, typename array_t, typename inp_calc_t, typename out_calc_t>
