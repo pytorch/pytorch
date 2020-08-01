42706143d851fc3a258fa9862e44db613db82a95
Ivan Kobzarev <ivankobzarev@fb.com>
2020-07-31 17:24:38 -0700

    Update on "[docker][android] use aws instead of google repository"
    [ghstack-poisoned]

--- .circleci/docker/common/install_android.sh
+++ .circleci/docker/common/install_android.sh
@@ -10,7 +10,7 @@ apt-get autoclean && apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 pushd /tmp
-curl -Os --retry 3 https://ossci-android.s3.amazonaws.com/android-ndk-${ANDROID_NDK}-linux-x86_64.zip
+curl -Os --retry 3 $_https_amazon_aws/android-ndk-${ANDROID_NDK}-linux-x86_64.zip
 popd
 _ndk_dir=/opt/ndk
 mkdir -p "$_ndk_dir"
@@ -44,13 +44,14 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 
 # Installing android sdk
 # https://github.com/circleci/circleci-images/blob/staging/android/Dockerfile.m4
+_https_amazon_aws=https://s3.amazonaws.com/ossci-android/
 
 _tmp_sdk_zip=/tmp/android-sdk-linux.zip
 _android_home=/opt/android/sdk
 
 rm -rf $_android_home
 sudo mkdir -p $_android_home
-curl --silent --show-error --location --fail --retry 3 --output /tmp/android-sdk-linux.zip https://ossci-android.s3.amazonaws.com/android-sdk-linux.zip
+curl --silent --show-error --location --fail --retry 3 --output /tmp/android-sdk-linux.zip $_https_amazon_aws/android-sdk-linux.zip
 sudo unzip -q $_tmp_sdk_zip -d $_android_home
 rm $_tmp_sdk_zip
 
@@ -68,8 +69,7 @@ _gradle_home=/opt/gradle
 sudo rm -rf $gradle_home
 sudo mkdir -p $_gradle_home
 
-wget --no-verbose --output-document=/tmp/gradle.zip \
-"https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip"
+curl -Os --retry 3 $_https_amazon_aws/gradle-${GRADLE_VERSION}-bin.zip
 
 sudo unzip -q /tmp/gradle.zip -d $_gradle_home
 rm /tmp/gradle.zip
