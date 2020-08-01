637feb86cdd5235c1f92cbaa72c01d8d4ad0367e
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-07-31 17:01:53 -0700

    Vulkan pipeline cache.
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/vulkan/api/Pipeline.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace detail {
+namespace api {
+
+struct Pipeline final {
+  class Cache final {
+   public:
+   private:
+  };
+
+  VkPipeline
+};
+
+} // namespace api
+} // namespace detail
+} // namespace vulkan
+} // namespace native
+} // namespace at
