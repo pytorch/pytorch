1be6451667ff46ebb32353610293ec747f3115f1
aviloria <aviloria@yahoo.com>
2020-08-03 16:28:09 +0200

    Fix variable type
    `dim_` and `last_idx` should be of the same type.
    Fixing it, some annoying warnings are fixed

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -1380,7 +1380,7 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
         auto dim_ = dim();
         strides_.resize(dim_);
         if (dim_ > 0) {
-          int last_idx = dim_ - 1;
+          auto last_idx = dim_ - 1;
           strides_[last_idx] = 1;
           for (auto i = last_idx - 1; i >= 0; --i) {
             strides_[i] = strides_[i + 1] * std::max<int64_t>(sizes_[i + 1], 1);
