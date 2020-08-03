f70c56144701143ac58a61e0775feed2e6ed9c8d
peter <peterghost86@gmail.com>
2020-08-03 22:23:58 +0800

    Add CUDA 10.1 builds

--- .circleci/cimodel/data/windows_build_definitions.py
+++ .circleci/cimodel/data/windows_build_definitions.py
@@ -120,6 +120,11 @@ def TruePred(_):
 
 WORKFLOW_DATA = [
     # VS2019 CUDA-10.1
+    WindowsJob(None, VcSpec(2019), CudaVersion(10, 1)),
+    WindowsJob(1, VcSpec(2019), CudaVersion(10, 1)),
+    WindowsJob(2, VcSpec(2019), CudaVersion(10, 1)),
+    WindowsJob("-jit-profiling-tests", VcSpec(2019), CudaVersion(10, 1), master_only_pred=FalsePred),
+    # VS2019 CUDA-11.0
     WindowsJob(None, VcSpec(2019), CudaVersion(11, 0)),
     WindowsJob(1, VcSpec(2019), CudaVersion(11, 0)),
     WindowsJob(2, VcSpec(2019), CudaVersion(11, 0)),
@@ -128,6 +133,7 @@ WORKFLOW_DATA = [
     WindowsJob(None, VcSpec(2019), None),
     WindowsJob(1, VcSpec(2019), None, master_only_pred=TruePred),
     WindowsJob(2, VcSpec(2019), None, master_only_pred=TruePred),
+    WindowsJob(1, VcSpec(2019), CudaVersion(10, 1), force_on_cpu=True, master_only_pred=TruePred),
     WindowsJob(1, VcSpec(2019), CudaVersion(11, 0), force_on_cpu=True, master_only_pred=TruePred),
 ]
 
