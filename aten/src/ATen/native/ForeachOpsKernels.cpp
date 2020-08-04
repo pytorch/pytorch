8e6256a92ce5b70220ed3de3a936ebca9936f64c
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-04 09:52:53 -0700

    Update on "[IGNORE] Added add_scalar_"
    **Motivation**
    [GitHub issue](https://github.com/pytorch/pytorch/issues/38655) 
    Current PyTorch optimizer implementations are not efficient in cases when we work with a lot of small feature tensors. Starting a lot of kernels slows down the whole process. We need to reduce the number of kernels that we start. 
    As an example, we should be looking at [NVIDIAs Apex](https://github.com/NVIDIA/apex). 
    In order to track progress, we will pick PyTorchs DCGAN model with Adam optimizer and once the optimizer is reimplemented with tensor lists, benchmark the model performance against original model version, Apexs version with original Adam optimizer and it’s FusedAdam optimizer.
    
    **In this PR**
    - Adding a `std::vector<Tensor> _foreach_add_(TensorList tensors, Scalar scalar)` API
    
    **Tests**
    Tested via unit tests
    
    **Plan for the next PRs**
    1. APIs
    - Binary Ops for list with Scalar
    - Binary Ops for list with list
    - Unary Ops for list
    
    2. Rewrite PyTorch optimizers to use for-each operators in order to get performance gains.
    
    [ghstack-poisoned]

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -5,8 +5,8 @@ std::vector<Tensor> foreach_add_scalar_kernel_fallback(TensorList tensors, Scala
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
 
   std::vector<Tensor> result;
-  for (int i = 0; i < tensors.size(); i++) {
-    auto temp = tensors[i].add(scalar);
+  for (auto& t : tensors) {
+    auto temp = t.add(scalar);
     result.emplace_back(temp);
   }
   return result;
@@ -15,8 +15,8 @@ std::vector<Tensor> foreach_add_scalar_kernel_fallback(TensorList tensors, Scala
 std::vector<Tensor> foreach_add_scalar__kernel_fallback(TensorList tensors, Scalar scalar) {
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
 
-  for (int i = 0; i < tensors.size(); i++) {
-    tensors[i].add_(scalar);
+  for (auto& t : tensors) {
+    t.add_(scalar);
   }
 
   return tensors.vec();
