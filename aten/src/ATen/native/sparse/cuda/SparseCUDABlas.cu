pytorch/pytorch

commit 2ae1490c37ff85ddda7b5a881c701095d15b96ef

Relax cusparse windows guard on cuda 11 (#42412)
Summary:
Fixes https://github.com/pytorch/pytorch/issues/42406

### cusparse Xcsrmm2 API:

(https://github.com/pytorch/pytorch/issues/37202)

- new: https://docs.nvidia.com/cuda/cusparse/index.html#cusparse-generic-function-spmm
- old (deprecated in cuda 11): https://docs.nvidia.com/cuda/archive/10.2/cusparse/index.html#csrmm2

Before:

|cuda ver | windows | linux |
|--|--|--|
| 10.1 | old api | old api  |
| 10.2 | old api | new api |
| 11    | old api (build error claimed in https://github.com/pytorch/pytorch/issues/42406) | new api |

After:

|cuda ver | windows | linux |
|--|--|--|
| 10.1 | old api | old api  |
| 10.2 | old api | **old api** |
| 11    | **new api** | new api |

### cusparse bmm-sparse-dense API

<details><summary>reverted, will be revisited in the future</summary>
(cc kurtamohler https://github.com/pytorch/pytorch/issues/33430)

- new: https://docs.nvidia.com/cuda/cusparse/index.html#cusparse-generic-function-spmm

Before:

|cuda ver | windows | linux |
|--|--|--|
| 10.1 | not supported | new api  |
| 10.2 | not supported | new api |
| 11    | not supported | new api |

After:

|cuda ver | windows | linux |
|--|--|--|
| 10.1 | not supported | new api  |
| 10.2 | not supported | new api |
| 11    | **new api** | new api |

</details>

Pull Request resolved: https://github.com/pytorch/pytorch/pull/42412

Reviewed By: agolynski

Differential Revision: D22892032

Pulled By: ezyang

fbshipit-source-id: cded614af970f0efdc79c74e18e1d9ea8a46d012

aten/src/ATen/native/sparse/cuda/SparseCUDABlas.cu
@@ -9,16 +9,11 @@
 
 #include <cusparse.h>
 
-#if !defined(_MSC_VER) && defined(__CUDACC__) && CUSPARSE_VERSION >= 10301 // CUDA release >= 10.2 and not windows
+#if defined(__CUDACC__) && CUSPARSE_VERSION >= 11000
 #include <library_types.h>
 #endif
 
-// LIMITATION (cusparseSpMM): 
-// The generic APIs are currently (CUDA 10.1) available for all platforms except Windows. 
-// Using these APIs in any other systems will result in compile-time or run-time failures. 
-// Their support will be extended in the next releases. 
-
-#if !defined(CUSPARSE_VERSION) || (CUSPARSE_VERSION < 10200)
+#if !defined(CUSPARSE_VERSION) || (CUSPARSE_VERSION < 10100)
 const char* cusparseGetErrorString(cusparseStatus_t status) {
   switch(status)
   {
@@ -81,7 +76,7 @@ cusparseOperation_t convertTransToCusparseOperation(char trans) {
   }
 }
 
-#if !defined(_MSC_VER) && defined(__CUDACC__) && CUSPARSE_VERSION >= 10301 // CUDA release >= 10.2 and not windows
+#if defined(__CUDACC__) && CUSPARSE_VERSION >= 11000
 
 template<typename T> 
 void csrmm2(

commit 4ed34a7d9425250811af506fac87d5c795347546

Re-enable cusparseSpMM on CUDA-10.x on Linux
This fixes feature regression introduced by #42412 which limited all the use of the API to CUDA-11.0+

aten/src/ATen/native/sparse/cuda/SparseCUDABlas.cu
@@ -9,7 +9,17 @@
 
 #include <cusparse.h>
 
-#if defined(__CUDACC__) && CUSPARSE_VERSION >= 11000
+// LIMITATION (cusparseSpMM):
+// The generic APIs are available on all platforms on CUDA 11.0
+// For CUDA 10.1+ it is available for all platforms except Windows.
+// Using these APIs in any other systems will result in compile-time or run-time failures.
+// Their support will be extended in the next releases.
+
+#define IS_SPMM_AVAILABLE() (defined(__CUDACC__) &&                              \
+                             (CUSPARSE_VERSION >= 11000 ||                       \
+                              (!defined(_MSC_VER) && CUSPARSE_VERSION >= 10301)))
+
+#if IS_SPMM_AVAILABLE()
 #include <library_types.h>
 #endif
 
@@ -76,7 +86,7 @@ cusparseOperation_t convertTransToCusparseOperation(char trans) {
   }
 }
 
-#if defined(__CUDACC__) && CUSPARSE_VERSION >= 11000
+#if IS_SPMM_AVAILABLE()
 
 template<typename T> 
 void csrmm2(