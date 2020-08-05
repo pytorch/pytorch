ef9f6576de638d43d02ddce298794540f1eaade8
Bhavishya Samriya <bhavishyasam69@gmail.com>
2020-08-05 17:14:31 +0530

    Corrected Grammatical Mistakes

--- torch/__init__.py
+++ torch/__init__.py
@@ -1,12 +1,12 @@
 
 r"""
 The torch package contains data structures for multi-dimensional
-tensors and defines mathematical operations over these tensors.
+tensors. It also defines mathematical operations that can be performed over these tensors.
 Additionally, it provides many utilities for efficient serializing of
 Tensors and arbitrary types, and other useful utilities.
 
 It has a CUDA counterpart, that enables you to run your tensor computations
-on an NVIDIA GPU with compute capability >= 3.0.
+on an NVIDIA GPU which has a compute capability >= 3.0.
 """
 
 import os
