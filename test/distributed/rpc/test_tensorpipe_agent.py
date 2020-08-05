4da602b00422ddfd89f08cffc2c7cc41a743816b
Luca Wehrstedt <lcw@fb.com>
2020-08-05 15:01:13 -0700

    [RPC tests] Generate test classes automatically (#42527)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42527
    
    ghstack-source-id: 109229468
    
    Test Plan: CI
    
    Reviewed By: pritamdamania87
    
    Differential Revision: D22864698
    
    fbshipit-source-id: 6a55f3201c544f0173493b38699a2c7e95ac1bbc

--- test/distributed/rpc/test_tensorpipe_agent.py
+++ test/distributed/rpc/test_tensorpipe_agent.py
@@ -1,68 +1,26 @@
 #!/usr/bin/env python3
-import unittest
 
-from torch.testing._internal.common_distributed import MultiProcessTestCase
-from torch.testing._internal.common_utils import TEST_WITH_ASAN, run_tests
-from torch.testing._internal.distributed.ddp_under_dist_autograd_test import (
-    DdpComparisonTest,
-    DdpUnderDistAutogradTest,
-)
-from torch.testing._internal.distributed.nn.api.remote_module_test import (
-    RemoteModuleTest,
-)
-from torch.testing._internal.distributed.rpc.dist_autograd_test import DistAutogradTest
-from torch.testing._internal.distributed.rpc.dist_optimizer_test import (
-    DistOptimizerTest,
-)
-from torch.testing._internal.distributed.rpc.jit.dist_autograd_test import (
-    JitDistAutogradTest,
-)
-from torch.testing._internal.distributed.rpc.jit.rpc_test import JitRpcTest
-from torch.testing._internal.distributed.rpc.rpc_test import TensorPipeAgentRpcTest
+from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.distributed.rpc.tensorpipe_rpc_agent_test_fixture import (
     TensorPipeRpcAgentTestFixture,
 )
-
-
-@unittest.skipIf(
-    TEST_WITH_ASAN, "Skip ASAN as torch + multiprocessing spawn have known issues"
+from torch.testing._internal.distributed.rpc_utils import (
+    GENERIC_TESTS,
+    TENSORPIPE_TESTS,
+    MultiProcess,
+    generate_tests,
 )
-class SpawnHelper(TensorPipeRpcAgentTestFixture, MultiProcessTestCase):
-    def setUp(self):
-        super().setUp()
-        self._spawn_processes()
 
 
-class TensorPipeRpcTestWithSpawn(TensorPipeAgentRpcTest, SpawnHelper):
-    pass
-
-
-class TensorPipeDistAutogradTestWithSpawn(DistAutogradTest, SpawnHelper):
-    pass
-
-
-class TensorPipeDistOptimizerTestWithSpawn(DistOptimizerTest, SpawnHelper):
-    pass
-
-
-class TensorPipeJitRpcTestWithSpawn(JitRpcTest, SpawnHelper):
-    pass
-
-
-class TensorPipeJitDistAutogradTestWithSpawn(JitDistAutogradTest, SpawnHelper):
-    pass
-
-
-class TensorPipeRemoteModuleTestWithSpawn(RemoteModuleTest, SpawnHelper):
-    pass
-
-
-class TensorPipeDdpUnderDistAutogradTest(DdpUnderDistAutogradTest, SpawnHelper):
-    pass
-
-
-class TensorPipeDdpComparisonTest(DdpComparisonTest, SpawnHelper):
-    pass
+globals().update(
+    generate_tests(
+        "TensorPipe",
+        TensorPipeRpcAgentTestFixture,
+        GENERIC_TESTS + TENSORPIPE_TESTS,
+        MultiProcess.SPAWN,
+        __name__,
+    )
+)
 
 
 if __name__ == "__main__":
