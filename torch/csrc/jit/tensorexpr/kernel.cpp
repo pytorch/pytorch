8b24904794846d4dd5c6510cb15e278575b24e74
Mikhail Zolotukhin <mvz@fb.com>
2020-08-05 14:08:01 -0700

    [TensorExpr] Support shape inference in TE for aten::cat. (#42387)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/42387
    
    Test Plan: Imported from OSS
    
    Reviewed By: bertmaher
    
    Differential Revision: D22879281
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 775e46a4cfd91c63196b378ee587cc4434672c89

--- torch/csrc/jit/tensorexpr/kernel.cpp
+++ torch/csrc/jit/tensorexpr/kernel.cpp
@@ -273,7 +273,30 @@ std::vector<ExprHandle> TensorExprKernel::inferSizesForValue(
       return shape;
     }
 
-    case aten::cat:
+    case aten::cat: {
+      // In JIT IR, aten::cat usually appears with the following nodes around
+      // it:
+      //   %dim : int = prim::Constant[value=0]()
+      //   %inputs : Tensor[] = prim::ListConstruct(%a, %b, ...)
+      //   %cat_output : Tensor = aten::cat(%inputs, %dim)
+      // Shapes of the input tensors could only differ at the dimension %dim.
+      // The sizes of the output tensor on that dimension is a sum of the
+      // corresponding sizes of the input tensors, the other dimension have the
+      // same sizes.
+      auto const& n = v->node();
+      auto inputs = n->input(0)->node()->inputs();
+      TORCH_INTERNAL_ASSERT(n->input(1)->node()->kind() == prim::Constant);
+      int64_t dim = n->input(1)->node()->i(attr::value);
+
+      ExprHandle concat_size = IntImm::make(0);
+      for (auto input : inputs) {
+        concat_size = concat_size + sizesForValue(input)[dim];
+      }
+      concat_size = IRSimplifier::simplify(concat_size);
+      auto shape = sizesForValue(inputs[0]);
+      shape[dim] = concat_size;
+      return shape;
+    }
     case aten::slice:
       throw std::runtime_error(
           "Shape info is not implemented for this kind of node");
