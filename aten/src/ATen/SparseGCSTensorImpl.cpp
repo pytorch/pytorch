00a11648e1b12086b738311d2b7fdd8e362752ec
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-08-05 05:11:47 -0500

    sparseGCS compiles with new constructor

--- aten/src/ATen/SparseGCSTensorImpl.cpp
+++ aten/src/ATen/SparseGCSTensorImpl.cpp
@@ -1,14 +1,35 @@
 #include <ATen/ATen.h>
-#include <ATen/SparseTensorImpl.h>
+#include <ATen/SparseGCSTensorImpl.h>
 #include <ATen/InitialTensorOptions.h>
 #include <ATen/core/LegacyTypeDispatch.h>
 
 namespace at {
 
-SparseGCSTensorImpl::SparseGCSTensorImpl(at::DispatchKeySet key_set, const caffe2::TypeMeta& data_type) {
-  hello
+namespace {
+  DeviceType sparseGCSTensorSetToDeviceType(DispatchKeySet key_set) {
+    if (key_set.has(DispatchKey::SparseCPU)) {
+      return kCPU;
+    } else if (key_set.has(DispatchKey::SparseCUDA)) {
+      return kCUDA;
+    } else {
+      AT_ERROR("Cannot construct SparseTensor with non-sparse tensor type ID ", key_set);
+    }
+  }
+}
+
+
+SparseGCSTensorImpl::SparseGCSTensorImpl(at::DispatchKeySet key_set, const caffe2::TypeMeta& data_type)
+  :   SparseGCSTensorImpl(key_set, data_type
+      , at::empty({1, 0}, at::initialTensorOptions().device(sparseGCSTensorSetToDeviceType(key_set)).dtype(ScalarType::Long))
+      , at::empty({0}, at::initialTensorOptions().device(sparseGCSTensorSetToDeviceType(key_set)).dtype(data_type))
+      , at::empty({0}, at::initialTensorOptions().device(sparseGCSTensorSetToDeviceType(key_set)).dtype(data_type))) {
+  
 }
 
 SparseGCSTensorImpl::SparseGCSTensorImpl(at::DispatchKeySet key_set, const caffe2::TypeMeta& data_type,
-                                   at::Tensor pointers, at::Tensor indices, at::Tensor values) {}
+                                         at::Tensor pointers, at::Tensor indices, at::Tensor values)
+  :
+  TensorImpl(key_set, data_type, values.device()) {
+}
+
 }
--- aten/src/ATen/SparseGCSTensorImpl.h
+++ aten/src/ATen/SparseGCSTensorImpl.h
@@ -8,8 +8,15 @@ namespace at {
 struct CAFFE2_API SparseGCSTensorImpl : public TensorImpl {
  public:
   explicit SparseGCSTensorImpl(at::DispatchKeySet, const caffe2::TypeMeta&);
-
+  
   void resize_and_clear_(ArrayRef<int64_t>& size) {
   }
+
+ private :
+  
+  explicit SparseGCSTensorImpl(at::DispatchKeySet key_set, const caffe2::TypeMeta& data_type,
+                               at::Tensor pointers, at::Tensor indices, at::Tensor values);
+
+
 };
 }
