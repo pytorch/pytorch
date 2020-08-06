b9595da653a3d5a608ff734adbc08f2a237eb813
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-08-06 11:44:30 -0700

    revert

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -56,7 +56,7 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-retry git clone -q https://github.com/zasdfgbnm/builder.git -b cuda110-win "$BUILDER_ROOT"
+retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
