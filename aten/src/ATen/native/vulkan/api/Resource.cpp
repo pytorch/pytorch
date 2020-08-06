bbd36103bf577fa8bae26d62662d8a85f94665e1
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-06 15:36:33 -0700

    Vulkan resource cache.
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/vulkan/api/Resource.cpp
@@ -0,0 +1,12 @@
+#include <ATen/native/vulkan/api/Resource.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
---
+++ aten/src/ATen/native/vulkan/api/Resource.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+#include <ATen/native/vulkan/api/Allocator.h>
+#include <ATen/native/vulkan/api/Cache.h>
+#include <c10/util/hash.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+struct Resource final {
+  VmaAllocator allocator;
+};
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
