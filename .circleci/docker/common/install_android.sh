c1f3434cafed4c6074036f127bfd0f978218e9c8
Ivan Kobzarev <ivankobzarev@fb.com>
2020-08-05 20:25:51 -0700

    Update on "[WIP] VulkanSdk and SwiftShader to Android docker image"
    [ghstack-poisoned]

--- .circleci/docker/common/install_android.sh
+++ .circleci/docker/common/install_android.sh
@@ -4,10 +4,6 @@ set -ex
 
 [ -n "${ANDROID_NDK}" ]
 
-retry () {
-    $*  || (sleep 1 && $*) || (sleep 2 && $*) || (sleep 4 && $*) || (sleep 8 && $*)
-}
-
 _https_amazon_aws=https://ossci-android.s3.amazonaws.com
 
 apt-get update
@@ -67,53 +63,6 @@ export ADB_INSTALL_TIMEOUT=120
 
 export PATH="${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:${PATH}"
 echo "PATH:${PATH}"
-
-# Installing Vulkan Sdk
-_vulkansdk_dir=/var/lib/jenkins/vulkansdk
-mkdir -p $_vulkansdk_dir
-_tmp_vulkansdk_targz=/tmp/vulkansdk.tar.gz
-curl --silent --show-error --location --fail --retry 3 --output "$_tmp_vulkansdk_targz" "$_https_amazon_aws/vulkansdk-linux-x86_64-1.2.148.0.tar.gz"
-tar -C "$_vulkansdk_dir" -xzf "$_tmp_vulkansdk_targz"
-export VULKAN_SDK="$_vulkansdk_dir/1.2.148.0/"
-rm "$_tmp_vulkansdk_targz"
-
-
-# Installing SwiftShader for Vulkan
-# XCB libs
-apt-get update
-sudo apt-get install -y libx11-dev libxcb1-dev libx11-xcb-dev
-
-# CMake >= 3.13 is required by SwiftShader
-_cmake_dir=/var/lib/jenkins/swiftshader-cmake
-mkdir -p $_cmake_dir
-_tmp_cmake_targz=/tmp/cmake.tar.gz
-curl --silent --show-error --location --fail --retry 3 --output $_tmp_cmake_targz https://cmake.org/files/v3.16/cmake-3.16.8-Linux-x86_64.tar.gz
-tar -C "$_cmake_dir" -xzf "$_tmp_cmake_targz"
-_cmake_bin_path="$_cmake_dir/cmake-3.16.8-Linux-x86_64/bin/cmake"
-rm "$_tmp_cmake_targz"
-
-# SwiftShader
-_swiftshader_root_dir=/var/lib/jenkins
-_swiftshader_dir="$_swiftshader_root_dir/swiftshader"
-retry git clone https://github.com/google/swiftshader.git "$_swiftshader_dir"
-pushd "$_swiftshader_dir"
-git submodule sync && git submodule update -q --init --recursive
-popd
-#_tmp_swiftshader_zip=/tmp/swiftshader-master-200805-1128.zip
-#curl --silent --show-error --location --fail --retry 3 --output "$_tmp_swiftshader_zip" "$_https_amazon_aws/swiftshader-master-200805-1128.zip"
-#unzip -qo "$_tmp_swiftshader_zip" -d "$_swiftshader_root_dir"
-#_swiftshader_dir="$_swiftshader_root_dir/swiftshader-master"
-#rm "$_tmp_swiftshader_zip"
-
-pushd "$_swiftshader_dir/build"
-
-$_cmake_bin_path .. -DSWIFTSHADER_BUILD_PVR=0 -DSWIFTSHADER_BUILD_TESTS=0
-make --jobs=8
-./vk-unittests
-
-popd
-
-export VK_ICD_FILENAMES="$_swiftshader_dir/build/Linux/vk_swiftshader_icd.json"
 
 # Installing Gradle
 echo "GRADLE_VERSION:${GRADLE_VERSION}"
