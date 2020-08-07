962dec2c62983a4ce7f0ddb0feb137f159321a33
Mike Ruberry <mruberry@devfair044.maas>
2020-08-06 17:03:52 -0700

    fft->linalg

--- torch/linalg/__init__.py
+++ torch/linalg/__init__.py
@@ -5,8 +5,8 @@ from torch._C import _add_docstr, _linalg  # type: ignore
 
 Tensor = torch.Tensor
 
-# Note: This not only adds the doc strings for the spectral ops, but
-# connects the torch.fft Python namespace to the torch._C._fft builtins.
+# Note: This not only adds doc strings for the linear algebra ops, but
+# also connects the torch.linalg Python namespace to the torch._C._linalg builtins.
 
 outer = _add_docstr(_linalg.linalg_outer, r"""
 linalg.outer(input, vec2, *, out=None) -> Tensor
