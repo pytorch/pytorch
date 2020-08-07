c9346ad3b82e749b4faf506e5a1c3dc03566e4b8
anjali411 <chourdiaanjali123@gmail.com>
2020-08-07 10:00:25 -0700

    [CPU] Added torch.bmm for complex tensors (#42383)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42383
    
    Test Plan - Updated existing tests to run for complex dtypes as well.
    
    Also added tests for `torch.addmm`, `torch.badmm`
    
    Test Plan: Imported from OSS
    
    Reviewed By: ezyang
    
    Differential Revision: D22960339
    
    Pulled By: anjali411
    
    fbshipit-source-id: 0805f21caaa40f6e671cefb65cef83a980328b7d

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -470,15 +470,16 @@ static inline Tensor& bmm_out_or_baddbmm_(Tensor& self_or_result, const Tensor& 
 
   if (contraction_size * res_rows * res_cols < 400) {
     if (is_bmm_out) {
-      AT_DISPATCH_ALL_TYPES(batch1.scalar_type(), "bmm", [&] {
+      AT_DISPATCH_ALL_TYPES_AND_COMPLEX(batch1.scalar_type(), "bmm", [&] {
           baddbmm_cpu_kernel<scalar_t, true>(self_or_result, batch1, batch2, beta, alpha);
         });
     } else {
-      AT_DISPATCH_ALL_TYPES(batch1.scalar_type(), "baddbmm", [&] {
+      AT_DISPATCH_ALL_TYPES_AND_COMPLEX(batch1.scalar_type(), "baddbmm", [&] {
           baddbmm_cpu_kernel<scalar_t, false>(self_or_result, batch1, batch2, beta, alpha);
         });
     }
-  } else if (at::hasMKL() && at::native::is_floating_point(self_or_result)
+  } else if (at::hasMKL() && (at::native::is_floating_point(self_or_result) ||
+            at::native::is_complex(self_or_result))
             && batch_items_contiguous_or_transposed(batch1)
             && batch_items_contiguous_or_transposed(batch2)
             && self_or_result.is_contiguous()) {
