37fff60141bf4d357cfe24aa62416788cc7d2ae1
vasiliy <vasiliy@fb.com>
2020-08-07 12:39:41 -0700

    fix nits on "fix asan failure for module freezing in conv bn folding"
    Summary:
    
    This is a test case which fails with ASAN on at the module freezing
    step.
    
    Test Plan:
    
    ```
    USE_ASAN=1 USE_CUDA=0 python setup.py develop
    LD_PRELOAD=/usr/lib64/libasan.so.4 python test/test_mobile_optimizer.py TestOptimizer.test_optimize_for_mobile_asan
    
    // output tail: https://gist.github.com/vkuzo/7a0018b9e10ffe64dab0ac7381479f23
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D23005962](https://our.internmc.facebook.com/intern/diff/D23005962)
    
    [ghstack-poisoned]

--- test/test_mobile_optimizer.py
+++ test/test_mobile_optimizer.py
@@ -255,48 +255,5 @@ class TestOptimizer(unittest.TestCase):
         bi_module_lint_list = generate_mobile_module_lints(bi_module)
         self.assertEqual(len(bi_module_lint_list), 0)
 
-    @unittest.skipUnless(torch.backends.xnnpack.enabled,
-                         " XNNPACK must be enabled for these tests."
-                         " Please build with USE_XNNPACK=1.")
-    def test_quantized_conv_no_asan_failures(self):
-        # There were ASAN failures when fold_conv_bn was run on
-        # already quantized conv modules. Verifying that this does
-        # not happen again.
-
-        class Child(nn.Module):
-            def __init__(self):
-                super(Child, self).__init__()
-                self.conv2 = nn.Conv2d(1, 1, 1)
-
-            def forward(self, x):
-                x = self.conv2(x)
-                return x
-
-        class Parent(nn.Module):
-            def __init__(self):
-                super(Parent, self).__init__()
-                self.quant = torch.quantization.QuantStub()
-                self.conv1 = nn.Conv2d(1, 1, 1)
-                self.child = Child()
-                self.dequant = torch.quantization.DeQuantStub()
-
-            def forward(self, x):
-                x = self.quant(x)
-                x = self.conv1(x)
-                x = self.child(x)
-                x = self.dequant(x)
-                return x
-
-        model = Parent()
-        model.qconfig = torch.quantization.get_default_qconfig('qnnpack')
-        torch.quantization.prepare(model, inplace=True)
-        model(torch.randn(4, 1, 4, 4))
-        torch.quantization.convert(model, inplace=True)
-        model = torch.jit.script(model)
-        # this line should not have ASAN failures
-        model_optim = optimize_for_mobile(model)
-        self.assertFalse(hasattr(model_optim.conv1, "bias"))
-        self.assertFalse(hasattr(model_optim.child.conv2, "bias"))
-
 if __name__ == '__main__':
     unittest.main()
