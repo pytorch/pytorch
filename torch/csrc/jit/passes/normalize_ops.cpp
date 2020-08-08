64c0fadc322b03d5a1e640c25e614c4596959bf2
Mike Ruberry <mruberry@devfair044.maas>
2020-08-07 18:28:26 -0700

    format

--- torch/csrc/jit/passes/normalize_ops.cpp
+++ torch/csrc/jit/passes/normalize_ops.cpp
@@ -11,8 +11,7 @@ static const std::unordered_map<Symbol, Symbol> alias_map = {
     {aten::absolute, aten::abs},
     {aten::absolute_, aten::abs_},
     {aten::clip, aten::clamp},
-    {aten::clip_, aten::clamp_}
-};
+    {aten::clip_, aten::clamp_}};
 
 void replaceNodeWithNewSymbol(Node* node, Symbol new_symbol) {
   WithInsertPoint insert_guard{node};
