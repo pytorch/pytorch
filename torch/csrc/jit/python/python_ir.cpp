a0d11161567bba0b388ce8db31ec649a6be9e879
Yael Dekel <yaeld@microsoft.com>
2020-08-08 10:09:19 +0300

    clang format

--- torch/csrc/jit/python/python_ir.cpp
+++ torch/csrc/jit/python/python_ir.cpp
@@ -4,7 +4,6 @@
 #include <torch/csrc/jit/ir/alias_analysis.h>
 #include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/passes/canonicalize.h>
-#include <torch/csrc/jit/passes/shape_analysis.h>
 #include <torch/csrc/jit/passes/onnx/helper.h>
 #include <torch/csrc/jit/passes/shape_analysis.h>
 #include <torch/csrc/jit/python/pybind.h>
