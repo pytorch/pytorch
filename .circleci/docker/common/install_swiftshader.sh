369acdb3048fd8989e80184b25c550c954c6a4f3
Ivan Kobzarev <ivankobzarev@fb.com>
2020-08-08 14:37:06 -0700

    Update on "[WIP] VulkanSdk and SwiftShader to Android docker image"
    [ghstack-poisoned]

--- .circleci/docker/common/install_swiftshader.sh
+++ .circleci/docker/common/install_swiftshader.sh
@@ -39,7 +39,20 @@ popd
 
 pushd "$_swiftshader_dir/build"
 
+_cc="$CC"
+_cxx="$CXX"
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+echo "CC=$CC"
+echo "CXX=$CXX"
+/usr/bin/clang --version
+/usr/bin/clang++ --version
+
 $_cmake_bin_path .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_PARALLEL_LINK_JOBS=1 \
+  -DLLVM_USE_LINKER=gold \
   -DSWIFTSHADER_BUILD_VULKAN=1 \
   -DSWIFTSHADER_BUILD_EGL=0 \
   -DSWIFTSHADER_BUILD_GLESv2=0 \
@@ -51,6 +64,8 @@ make --jobs=8
 
 ./vk-unittests
 
+export CC="$_cc"
+export CXX="$_cxx"
 popd
 
 export VK_ICD_FILENAMES="$_swiftshader_dir/build/Linux/vk_swiftshader_icd.json"
