b23705ee02d51f8497fe5b8a1ed03980c5c46e05
anjali411 <chourdiaanjali123@gmail.com>
2020-08-10 08:13:34 -0700

    Update on "Add dot product for complex tensors"
    TODO: potentially add a fast path for complex dot
    
    [ghstack-poisoned]

--- aten/src/ATen/native/BlasKernel.cpp
+++ aten/src/ATen/native/BlasKernel.cpp
@@ -6,8 +6,8 @@
 
 #if AT_BUILD_WITH_BLAS()
 extern "C" double ddot_(int *n, double *x, int *incx, double *y, int *incy);
-extern "C" std::complex<double> zdotu_(int *n, std::complex<double> *x, int *incx, std::complex<double> *y, int *incy);
-extern "C" std::complex<double> cdotu_(int *n, std::complex<float> *x, int *incx, std::complex<float> *y, int *incy);
+extern "C" void zdotu_(std::complex<double> *res, int *n, std::complex<double> *x, int *incx, std::complex<double> *y, int *incy);
+extern "C" void cdotu_(std::complex<float> *res, int *n, std::complex<float> *x, int *incx, std::complex<float> *y, int *incy);
 extern "C" void dscal_(int *n, double *a, double *x, int *incx);
 extern "C" void sscal_(int *n, float *a, float *x, int *incx);
 extern "C" void dgemv_(char *trans, int *m, int *n, double *alpha, double *a, int *lda, double *x, int *incx, double *beta, double *y, int *incy);
@@ -200,13 +200,15 @@ double dot_fast_path(int n, double* x, int incx, double* y, int incy) {
 }
 
 c10::complex<double> dot_fast_path(int n, c10::complex<double>* x, int incx, c10::complex<double>* y, int incy) {
-  auto result = zdotu_(&n, reinterpret_cast<std::complex<double>*>(x), &incx, reinterpret_cast<std::complex<double>*>(y), &incy);
-  return *reinterpret_cast<c10::complex<double>*>(&result);
+  c10::complex<double> result;
+  zdotu_(reinterpret_cast<std::complex<double>* >(&result), &n, reinterpret_cast<std::complex<double>*>(x), &incx, reinterpret_cast<std::complex<double>*>(y), &incy);
+  return result;
 }
 
 c10::complex<float> dot_fast_path(int n, c10::complex<float>* x, int incx, c10::complex<float>* y, int incy) {
-  auto result = cdotu_(&n, reinterpret_cast<std::complex<float>*>(x), &incx, reinterpret_cast<std::complex<float>*>(y), &incy);
-  return *reinterpret_cast<c10::complex<float>*>(&result);
+  c10::complex<float> result;
+  cdotu_(reinterpret_cast<std::complex<float>* >(&result), &n, reinterpret_cast<std::complex<float>*>(x), &incx, reinterpret_cast<std::complex<float>*>(y), &incy);
+  return result;
 }
 #endif
 
