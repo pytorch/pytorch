d28639a08054b6662693d1c81c734dd69938592c
Paul Shao <pshao@fb.com>
2020-08-11 08:40:21 -0700

    Optimization with Backward Implementation of Learnable Fake Quantize Per Channel Kernel (CPU and GPU) (#42810)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42810
    
    In this diff, the original backward pass implementation is sped up by merging the 3 iterations computing dX, dScale, and dZeroPoint separately. In this case, a native loop is directly used on a byte-wise level (referenced by `strides`). In addition, vectorization is used such that scale and zero point are expanded to share the same shape and the element-wise corresponding values to X along the channel axis.
    
    In the benchmark test on the operators, for an input of shape `3x3x256x256`, we have observed the following improvement in performance:
    **Speedup from python operator**: ~10x
    **Speedup from original learnable kernel**: ~5.4x
    **Speedup from non-backprop kernel**: ~1.8x
    
    Test Plan:
    To assert correctness of the new kernel, on a devvm, enter the command
    
    `buck test //caffe2/test:quantization -- learnable_backward_per_channel`
    
    To benchmark the operators, on a devvm, enter the command
    1. Set the kernel size to 3x3x256x256 or a reasonable input size.
    2. Run `buck test //caffe2/benchmarks/operator_benchmark/pt:quantization_test`
    3. The relevant outputs for CPU are as follows:
    
    ```
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cpu_op_typepy_module
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: py_module
    Backward Execution Time (us) : 989024.686
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cpu_op_typelearnable_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: learnable_kernel
    Backward Execution Time (us) : 95654.079
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cpu_op_typeoriginal_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: original_kernel
    Backward Execution Time (us) : 176948.970
    ```
    4. The relevant outputs for GPU are as follows:
    The relevant outputs are as follows
    
    **Pre-optimization**:
    
    ```
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typepy_module
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: py_module
    Backward Execution Time (us) : 6795.173
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typelearnable_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: learnable_kernel
    Backward Execution Time (us) : 4321.351
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typeoriginal_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: original_kernel
    Backward Execution Time (us) : 1052.066
    ```
    
    **Post-optimization**:
    ```
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typepy_module
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: py_module
    Backward Execution Time (us) : 6737.106
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typelearnable_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: learnable_kernel
    Backward Execution Time (us) : 2112.484
    
    # Benchmarking PyTorch: FakeQuantizePerChannelOpBenchmark
    # Mode: Eager
    # Name: FakeQuantizePerChannelOpBenchmark_N3_C3_H256_W256_cuda_op_typeoriginal_kernel
    # Input: N: 3, C: 3, H: 256, W: 256, device: cpu, op_type: original_kernel
    Backward Execution Time (us) : 1078.79
    
    Reviewed By: vkuzo
    
    Differential Revision: D22946853
    
    fbshipit-source-id: 1a01284641480282b3f57907cc7908d68c68decd

--- aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
+++ aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
@@ -2121,50 +2121,41 @@ void fake_quant_grad_per_channel_cpu(
       });
 }
 
-void fake_quantize_learnable_scale_grad_channel_kernel(
-    Tensor& input_grad,
-    const Tensor& input,
-    const Tensor& output_grad,
-    float scale,
-    int64_t zero_point,
+void fake_quantize_learnable_channel_grad_kernel_cpu(
+    TensorIterator& iter,
     int64_t quant_min,
     int64_t quant_max) {
-  float inv_scale = 1.0f / scale;
-  float grad_small = quant_min - zero_point;
-  float grad_big = quant_max - zero_point;
-  auto iter_scale = TensorIterator::binary_op(input_grad, input, output_grad);
-  // TODO: Implement the vectorized per channel version for the learnable backprop kernel on scale.
-  cpu_kernel(iter_scale, [&](float x, float dy) -> float {
-    int64_t xq = static_cast<int64_t>(zero_point + std::nearbyint(x * inv_scale));
-    xq = std::max(std::min(xq, quant_max), quant_min);
-    float x_fq = static_cast<float>((xq - zero_point) * scale);
-    if (xq == quant_min) {
-      return dy * grad_small;
-    } else if (xq == quant_max) {
-      return dy * grad_big;
-    }
-    return dy * (x_fq - x) * inv_scale;
-  });
-}
+  iter.for_each([&](char** data, const int64_t* strides, int64_t n) {
+    /*  To see how the input and outputs are referenced and assigned,
+        please see the implemenetation of
+        fake_quantize_learnable_tensor_grad_kernel_cpu.
+    */
+    for (int64_t i = 0; i < n; i++) {
+      float* dx_output = (float*)(data[0] + i * strides[0]);
+      float* dscale_output = (float*)(data[1] + i * strides[1]);
+      float* dzero_point_output = (float*)(data[2] + i * strides[2]);
+      float* x_input = (float*)(data[3] + i * strides[3]);
+      float* dy_input = (float*)(data[4] + i * strides[4]);
+      float* scale_input = (float*)(data[5] + i * strides[5]);
+      float* zero_point_input = (float*)(data[6] + i * strides[6]);
 
-void fake_quantize_learnable_zero_point_grad_channel_kernel(
-    Tensor& input_grad,
-    const Tensor& input,
-    const Tensor& output_grad,
-    float scale,
-    int64_t zero_point,
-    int64_t quant_min,
-    int64_t quant_max) {
-  float inv_scale = 1.0f / scale;
-  auto iter_zero_point = TensorIterator::binary_op(input_grad, input, output_grad);
-  // TODO: Implement the vectorized per channel version for the learnable backprop kernel on zero point.
-  cpu_kernel(iter_zero_point, [&](float x, float dy) -> float {
-    int64_t xq = static_cast<int64_t>(zero_point + std::nearbyint(x * inv_scale));
-    xq = std::max(std::min(xq, quant_max), quant_min);
-    if (xq == quant_min || xq == quant_max) {
-      return dy * (-1) * scale;
+      float inv_scale = 1.0f / (*scale_input);
+      float dscale_small = quant_min - (*zero_point_input);
+      float dscale_big = quant_max - (*zero_point_input);
+      // Calculate gradients for X.
+      int64_t xqi = std::nearbyint((*zero_point_input) + (*x_input) * inv_scale);
+      *dx_output = (*dy_input) * (xqi >= quant_min && xqi <= quant_max);
+      // Calculate gradients for scale and zero point.
+      xqi = std::max(std::min(xqi, quant_max), quant_min);
+      float xfqi = static_cast<float>((xqi - (*zero_point_input)) * (*scale_input));
+      if (xqi == quant_min || xqi == quant_max) {
+        *dzero_point_output = (*dy_input) * (-1) * (*scale_input);
+        *dscale_output = (xqi == quant_min) ? ((*dy_input) * dscale_small) : ((*dy_input) * dscale_big);
+      } else {
+        *dzero_point_output = 0;
+        *dscale_output = (*dy_input) * (xfqi - (*x_input)) * inv_scale;
+      }
     }
-    return 0;
   });
 }
 
@@ -2595,12 +2586,8 @@ REGISTER_DISPATCH(dequantize_tensor_per_channel_affine_stub,
                   &dequantize_tensor_per_channel_affine_cpu);
 REGISTER_DISPATCH(dequantize_tensor_per_tensor_affine_stub,
                   &dequantize_tensor_per_tensor_affine_cpu);
-REGISTER_DISPATCH(fake_quant_grad_learnable_scale_channel_stub,
-                  &fake_quantize_learnable_scale_grad_channel_kernel);
 REGISTER_DISPATCH(fake_quant_grad_learnable_tensor_stub,
                   &fake_quantize_learnable_tensor_grad_kernel_cpu);
-REGISTER_DISPATCH(fake_quant_grad_learnable_zero_point_channel_stub,
-                  &fake_quantize_learnable_zero_point_grad_channel_kernel);
 REGISTER_DISPATCH(fake_quant_grad_per_channel_stub,
                   &fake_quant_grad_per_channel_cpu);
 REGISTER_DISPATCH(fake_quant_grad_tensor_stub,
@@ -2635,10 +2622,13 @@ REGISTER_DISPATCH(qsigmoid_stub, &qsigmoid_kernel);
 REGISTER_DISPATCH(qtanh_stub, &qtanh_kernel);
 REGISTER_DISPATCH(qthreshold_stub, &qthreshold_kernel);
 REGISTER_DISPATCH(qtopk_stub, &qtopk_kernel);
-REGISTER_DISPATCH(quantize_tensor_per_channel_affine_stub,
-                  &quantize_tensor_per_channel_affine_cpu);
-REGISTER_DISPATCH(quantize_tensor_per_tensor_affine_stub,
-                  &quantize_tensor_per_tensor_affine_cpu);
+REGISTER_DISPATCH(fake_quant_grad_learnable_channel_stub, &fake_quantize_learnable_channel_grad_kernel_cpu);
+REGISTER_DISPATCH(
+    quantize_tensor_per_tensor_affine_stub,
+    &quantize_tensor_per_tensor_affine_cpu);
+REGISTER_DISPATCH(
+    quantize_tensor_per_channel_affine_stub,
+    &quantize_tensor_per_channel_affine_cpu);
 REGISTER_DISPATCH(quantized_normalize_stub, &quantized_normalize_kernel);
 REGISTER_DISPATCH(qupsample_bilinear2d_nhwc_stub,
                   &qupsample_bilinear2d_nhwc_kernel);
