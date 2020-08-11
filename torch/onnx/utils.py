d2ebe51d352cc5b040a5c4242e063ad120112a07
neginraoof <neginmr@utexas.edu>
2020-08-11 14:28:59 -0700

    Fix the pass name

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -130,10 +130,10 @@ def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=Fa
     torch._C._jit_pass_inline_fork_wait(graph)
     torch._C._jit_pass_lint(graph)
 
-    if not use_new_jit_passes:
-        torch._C._jit_pass_remove_inplace_ops(graph)
-    else:
+    if use_new_jit_passes:
         torch._C._jit_pass_lower_all_tuples(graph)
+        torch._C._jit_pass_onnx_remove_inplace_ops_for_onnx(graph)
+    else:
         torch._C._jit_pass_remove_inplace_ops(graph)
 
     # we record now record some ops like ones/zeros
