088a0989b8ff04404d426dcdd105350ae7c2408f
Will Constable <whc@fb.com>
2020-08-12 13:33:42 -0700

    WIP debug loader.py

--- torchpy/loader.py
+++ torchpy/loader.py
@@ -11,12 +11,13 @@ class Loader:
         self.fresh = True
 
     def __getattr__(self, name):
+        if name[:7] == "_ResNet":
+            name = name[7:]
+        # if name == '_ResNet___torch_mangle_29':
+            # import ipdb; ipdb.set_trace()
         print("{} -> {}".format(self.base, name))
-        # if name in self.globals:
-        #     import ipdb; ipdb.set_trace()
-            # return self.globals[name]
-        filename = os.path.join(self.base, name + '.py')
         dirname = os.path.join(self.base, name)
+        filename = dirname + '.py'
         if filename in self.zf.namelist():
             # the Loader for 'module' is loading 'linear.py' which should define some stuff (Linear? others?).  
             # We would like to eval it into a 'module' object and setattr the new module
@@ -26,19 +27,38 @@ class Loader:
             setattr(self, name, ldr) # Temporary class to allow annotations to work
             self.globals[name] = ldr
             f = self.zf.read(filename)
+            print("   Compile: ", name)
             exec(compile(f, name, 'exec'), ldr.globals)
             ldr.fresh = False
+            print("   Return finished loader for module {}".format(name))
             return ldr
 
         if any([dirname in x for x in self.zf.namelist()]):
             attr = Loader(dirname, self.zf, self.globals)
             setattr(self, name, attr)
+            print("   Return loader for dir {}".format(name))
             return attr
 
+
+        # Need to find a way to return a dummy that 
+        # - can be used recursively
+        #   - e.g. while compiling 'resnet' module, 
+        #          dummy 'resnet._Sequential___torch_mangle_13' gets returned
+        # .        and BasicBlock gets looked up on that dummy
+        # - could also be terminal
+        #   - I am replacing the terminal ones above, which will break the recursive part
+
+        # i'm looking up 'Sequential' class correctly from 'container.py' module
+        # but failing to treat '_ResNet___torch_mangle_29' as a module name in 'container' dir
+        # since container already exists as a .py module
+        # how do I make a correct distinction?
         if self.fresh:
-            return types.new_class("Dummy Class")
+            # return types.new_class("Dummy Class")
+            print("   Return sub- loader for {}".format(name))
+            return Loader(dirname, self.zf, self.globals)
         else:
             if name in self.globals:
+                print("   Return globals for {}".format(name))
                 return self.globals[name]
             raise AttributeError("{} not found in {}".format(name, self.base))
 
@@ -82,7 +102,6 @@ def load(filename):
 
         for src in ['data.pkl']: #['constants.pkl', 'data.pkl']:
             fname = os.path.join(filename_no_ext, src)
-            import ipdb; ipdb.set_trace()
             upk = TorchUnpickler(resolver, f.open(fname))
             upk.load()
             print("blah")
