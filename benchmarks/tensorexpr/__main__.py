835df737fdd985facd940357083d96e0e7478a36
Bert Maher <bertrand@fb.com>
2020-08-11 22:39:49 -0700

    Add a microbenchmark for LSTM elementwise portion
    [ghstack-poisoned]

--- benchmarks/tensorexpr/__main__.py
+++ benchmarks/tensorexpr/__main__.py
@@ -13,6 +13,7 @@ from . import matmul         # noqa: F401
 # from . import pooling        # noqa: F401
 # from . import reduction      # noqa: F401
 # from . import softmax        # noqa: F401
+from . import rnn_eltwise    # noqa: F401
 from . import swish          # noqa: F401
 
 
