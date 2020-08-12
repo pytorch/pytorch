99852d54b3aad51ea53275aec849bc67aa777351
Rohan Varma <rvarm1@fb.com>
2020-08-12 11:45:21 -0700

    Rename test_distributed to test_distributed_fork
    Follow up from https://github.com/pytorch/pytorch/pull/41769, rename `test_distributed` to `test_distributed_fork` to make it explicit that it forks.
    
    New command to run test:
    `python test/run_test.py -i distributed/test_distributed_fork -v`
    
    Differential Revision: [D23072201](https://our.internmc.facebook.com/intern/diff/D23072201/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D23072201/)!
    
    [ghstack-poisoned]

--- test/run_test.py
+++ test/run_test.py
@@ -35,7 +35,7 @@ TESTS = [
     'test_cuda_primary_ctx',
     'test_dataloader',
     'distributed/test_data_parallel',
-    'distributed/test_distributed',
+    'distributed/test_distributed_fork',
     'distributed/test_distributed_spawn',
     'test_distributions',
     'test_expecttest',
@@ -93,7 +93,7 @@ WINDOWS_BLOCKLIST = [
     'distributed/rpc/test_faulty_agent',
     'distributed/rpc/test_process_group_agent',
     'distributed/rpc/test_tensorpipe_agent',
-    'distributed/test_distributed',
+    'distributed/test_distributed_fork',
     'distributed/test_distributed_spawn',
 ]
 
@@ -137,7 +137,7 @@ SLOW_TESTS = [
     'test_jit_profiling',
     'test_torch',
     'distributed/nn/jit/test_instantiator',
-    'distributed/test_distributed',
+    'distributed/test_distributed_fork',
     'distributed/rpc/test_process_group_agent',
     'distributed/rpc/test_tensorpipe_agent',
     'distributed/test_distributed_spawn',
@@ -314,7 +314,7 @@ def test_distributed(test_module, test_directory, options):
             os.environ['INIT_METHOD'] = 'env://'
             os.environ.update(env_vars)
             if with_init_file:
-                if test_module in ["test_distributed", "test_distributed_spawn"]:
+                if test_module in ["test_distributed_fork", "test_distributed_spawn"]:
                     init_method = 'file://{}/'.format(tmp_dir)
                 else:
                     init_method = 'file://{}/shared_init_file'.format(tmp_dir)
@@ -346,7 +346,7 @@ CUSTOM_HANDLERS = {
     'test_cuda_primary_ctx': test_cuda_primary_ctx,
     'test_cpp_extensions_aot_no_ninja': test_cpp_extensions_aot_no_ninja,
     'test_cpp_extensions_aot_ninja': test_cpp_extensions_aot_ninja,
-    'distributed/test_distributed': test_distributed,
+    'distributed/test_distributed_fork': test_distributed,
     'distributed/test_distributed_spawn': test_distributed,
 }
 
