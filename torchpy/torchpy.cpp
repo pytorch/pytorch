db0fc128e9b3595216710e2b83266a1c55a2f580
Will Constable <whc@fb.com>
2020-08-12 13:34:07 -0700

    Add load/inputs functions to go with test_resnet

--- torchpy/torchpy.cpp
+++ torchpy/torchpy.cpp
@@ -1,5 +1,7 @@
 #include <torchpy.h>
 #include <Python.h>
+#include <torch/csrc/jit/api/module.h>
+#include <torch/script.h>
 #include <torch/torch.h>
 #include <iostream>
 
@@ -9,4 +11,14 @@ void torchpy::init() {
       "from time import time,ctime\n"
       "print('Today is',ctime(time()))\n");
   Py_Finalize();
+}
+
+Module torchpy::load(char* filename) {
+  return torch::jit::load(filename);
+}
+
+std::vector<torch::jit::IValue> torchpy::inputs() {
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back(torch::ones({1, 3, 224, 224}));
+  return inputs;
 }
--- torchpy/torchpy.h
+++ torchpy/torchpy.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 
 namespace torchpy {
 void init();
+std::vector<torch::jit::IValue> inputs();
 }
