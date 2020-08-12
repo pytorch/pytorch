d0c6d5874b3cb56e0bc987778380b0a050f7b6a5
Nikita Shulga <nshulga@fb.com>
2020-08-12 16:52:58 -0700

    run_test.py code cleanup
    Delete `from __future__ import print_function` as it supposed to be run on python-3 only
    Use `f'{foo}'` rather than `'{}'.format(foo)`
    Move test_module logic into its own function (that returns optional error message if module have failed

--- test/run_test.py
+++ test/run_test.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import argparse
 from datetime import datetime
 import modulefinder
@@ -17,6 +15,7 @@ import torch._six
 from torch.utils import cpp_extension
 from torch.testing._internal.common_utils import TEST_WITH_ROCM, shell
 import torch.distributed as dist
+from typing import Dict, Optional
 
 TESTS = [
     'test_autograd',
@@ -157,7 +156,7 @@ SLOW_TESTS = [
     'test_determination',
     'test_futures',
 ]
-_DEP_MODULES_CACHE = {}
+_DEP_MODULES_CACHE: Dict[str, set] = {}
 
 DISTRIBUTED_TESTS_CONFIG = {}
 
@@ -200,7 +199,7 @@ def print_to_stderr(message):
 
 def get_executable_command(options, allow_pytest):
     if options.coverage:
-        executable = ['coverage', 'run', '--parallel-mode', '--source torch']
+        executable = ['coverage', 'run', '--source=torch']
     else:
         executable = [sys.executable]
     if options.pytest:
@@ -564,7 +563,7 @@ def log_test_reason(file_type, filename, test, options):
 
 
 def get_dep_modules(test):
-    # Cache results in case of repitition
+    # Cache results in case of repetition
     if test in _DEP_MODULES_CACHE:
         return _DEP_MODULES_CACHE[test]
 
@@ -611,7 +610,7 @@ def determine_target(test, touched_files, options):
     # Some tests are faster to execute than to determine.
     if test not in SLOW_TESTS:
         if options.verbose:
-            print_to_stderr('Running {} without determination'.format(test))
+            print_to_stderr(f'Running {test} without determination')
         return True
     # HACK: "no_ninja" is not a real module
     if test.endswith('_no_ninja'):
@@ -649,10 +648,30 @@ def determine_target(test, touched_files, options):
 
     # If nothing has determined the test has run, don't run the test.
     if options.verbose:
-        print_to_stderr('Determination is skipping {}'.format(test))
+        print_to_stderr(f'Determination is skipping {test}')
 
     return False
 
+
+def run_test_module(test: str, test_directory: str, options) -> Optional[str]:
+    test_module = parse_test_module(test)
+
+    # Printing the date here can help diagnose which tests are slow
+    print_to_stderr('Running {} ... [{}]'.format(test, datetime.now()))
+    handler = CUSTOM_HANDLERS.get(test, run_test)
+    return_code = handler(test_module, test_directory, options)
+    assert isinstance(return_code, int) and not isinstance(
+        return_code, bool), 'Return code should be an integer'
+    if return_code == 0:
+        return None
+
+    message = f'{test} failed!'
+    if return_code < 0:
+        # subprocess.Popen returns the child process' exit signal as
+        # return code -N, where N is the signal number.
+        signal_name = SIGNALS_TO_NAMES_DICT[-return_code]
+        message += f' Received signal: {signal_name}'
+    return message
 
 def main():
     options = parse_args()
@@ -684,33 +703,20 @@ def main():
 
     has_failed = False
     failure_messages = []
-    for test in selected_tests:
-
-        test_module = parse_test_module(test)
-
-        # Printing the date here can help diagnose which tests are slow
-        print_to_stderr('Running {} ... [{}]'.format(test, datetime.now()))
-        handler = CUSTOM_HANDLERS.get(test, run_test)
-        return_code = handler(test_module, test_directory, options)
-        assert isinstance(return_code, int) and not isinstance(
-            return_code, bool), 'Return code should be an integer'
-        if return_code != 0:
+    try:
+        for test in selected_tests:
+            err_message = run_test_module(test, test_directory, options)
+            if err_message is None:
+                continue
             has_failed = True
-            message = '{} failed!'.format(test)
-            if return_code < 0:
-                # subprocess.Popen returns the child process' exit signal as
-                # return code -N, where N is the signal number.
-                signal_name = SIGNALS_TO_NAMES_DICT[-return_code]
-                message += ' Received signal: {}'.format(signal_name)
-            err = RuntimeError(message)
-            failure_messages.append(err)
-            if options.continue_through_error:
-                print_to_stderr(err)
-            else:
-                raise RuntimeError(err)
-    if options.coverage:
-        shell(['coverage', 'combine'])
-        shell(['coverage', 'html'])
+            failure_messages.append(err_message)
+            if not options.continue_through_error:
+                raise RuntimeError(err_message)
+            print_to_stderr(err_message)
+    finally:
+        if options.coverage:
+            shell(['coverage', 'combine'])
+            shell(['coverage', 'json'])
 
     if options.continue_through_error and has_failed:
         for err in failure_messages:
