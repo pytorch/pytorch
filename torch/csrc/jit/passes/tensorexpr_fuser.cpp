3d242ee1364345197575c730470bb7c1f49d221d
Mikhail Zolotukhin <mvz@fb.com>
2020-08-13 23:45:51 -0700

    [TensorExpr] Wrap fuser in a class. (#42936)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/42936
    
    Test Plan: Imported from OSS
    
    Reviewed By: eellison
    
    Differential Revision: D23084407
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: f622874efbcbf8d4e49c8fa519a066161ebe4877

--- torch/csrc/jit/passes/tensorexpr_fuser.cpp
+++ torch/csrc/jit/passes/tensorexpr_fuser.cpp
@@ -9,6 +9,7 @@
 #include <torch/csrc/jit/runtime/custom_operator.h>
 #include <torch/csrc/jit/runtime/operator_options.h>
 #include <torch/csrc/jit/tensorexpr/kernel.h>
+#include <torch/csrc/utils/memory.h>
 
 namespace torch {
 namespace jit {
@@ -161,7 +162,7 @@ bool allShapesAreKnown(Node* node) {
   return true;
 }
 
-bool canHandle(Node* node, AliasDb& aliasDb) {
+bool canHandle(Node* node) {
   if (node->kind() == prim::Constant) {
     if (node->output()->type()->cast<TensorType>()) {
       // TODO: add support for tensor constants.
@@ -188,64 +189,6 @@ bool canHandle(Node* node, AliasDb& aliasDb) {
   return tensorexpr::isSupported(node);
 }
 
-#define REQ(cond)                           \
-  if (!(cond)) {                            \
-    GRAPH_DEBUG("Failed cond " #cond "\n"); \
-    return false;                           \
-  }
-
-bool canMerge(Node* consumer, Node* producer, AliasDb& aliasDb) {
-  // Only handle complete tensor types
-  for (torch::jit::Value* output : consumer->outputs()) {
-    REQ(output->isCompleteTensor());
-  }
-
-  // Only fuse within a block
-  REQ(consumer->owningBlock() == producer->owningBlock());
-
-  // Symbolic checks
-  REQ(canHandle(producer, aliasDb));
-  REQ(
-      (canHandle(consumer, aliasDb) ||
-       consumer->kind() == getTensorExprSymbol()));
-
-  // Alias checks
-  REQ(aliasDb.couldMoveBeforeTopologically(producer, consumer));
-
-  // Ops that return aliases can only be folded if this is the only use.
-  if (producer->kind() == aten::slice || producer->kind() == aten::unsqueeze ||
-      producer->kind() == prim::ConstantChunk) {
-    for (auto& use : producer->output(0)->uses()) {
-      REQ(use.user == consumer);
-    }
-  }
-
-  if (!consumer->hasAttribute(attr::Subgraph) &&
-      consumer->kind() != getTensorExprSymbol()) {
-    // Don't initiate a fusion group from prim::ListConstruct
-    REQ(consumer->kind() != prim::ListConstruct);
-    REQ(consumer->kind() != aten::slice);
-    REQ(consumer->kind() != aten::unsqueeze);
-    REQ(consumer->kind() != prim::ConstantChunk);
-
-    // Don't initiate a fusion group just for a constant operand
-    REQ(producer->kind() != prim::Constant);
-  }
-
-  if (producer->kind() == aten::cat) {
-    REQ(producer->inputs()[0]->node()->kind() == prim::ListConstruct);
-    REQ(producer->inputs()[0]->uses().size() == 1);
-    REQ(producer->inputs()[1]->node()->kind() == prim::Constant);
-  } else if (consumer->kind() == aten::cat) {
-    REQ(consumer->inputs()[0]->node()->kind() == prim::ListConstruct);
-    REQ(consumer->inputs()[0]->uses().size() == 1);
-    REQ(consumer->inputs()[1]->node()->kind() == prim::Constant);
-  }
-
-  return true;
-}
-#undef REQ
-
 Node* getOrCreateTensorExprSubgraph(Node* n) {
   if (n->hasAttribute(attr::Subgraph) && n->kind() == getTensorExprSymbol()) {
     return n;
@@ -256,65 +199,174 @@ Node* getOrCreateTensorExprSubgraph(Node* n) {
   return te_group;
 }
 
-c10::optional<Node*> tryMerge(
-    Node* consumer,
-    Node* producer,
-    AliasDb& aliasDb) {
-  GRAPH_DEBUG(
-      "Trying producer ",
-      getHeader(producer),
-      " and consumer ",
-      getHeader(consumer),
-      ":\n");
+class TensorExprFuser {
+ public:
+  TensorExprFuser(std::shared_ptr<Graph> graph) : graph_(std::move(graph)) {}
 
-  if (!canMerge(consumer, producer, aliasDb)) {
-    return c10::nullopt;
+  void run() {
+    aliasDb_ = torch::make_unique<AliasDb>(graph_);
+    createFusionGroups();
   }
 
-  consumer = getOrCreateTensorExprSubgraph(consumer);
+ private:
+  // Merge fusible nodes into subgraphs in tensorexpr::Group nodes.
+  void createFusionGroups() {
+    auto block = graph_->block();
 
-  if (producer->kind() == aten::cat) {
-    Node* listconstruct = producer->inputs()[0]->node();
+    std::vector<std::pair<graph_node_list_iterator, graph_node_list_iterator>>
+        worklist;
+    std::unordered_set<torch::jit::Block*> visited_blocks;
 
-    aliasDb.moveBeforeTopologicallyValid(producer, consumer);
-    GRAPH_UPDATE(
-        "Merging ", getHeader(producer), " into ", getHeader(consumer));
-    SubgraphUtils::mergeNodeIntoSubgraph(producer, consumer);
+    bool any_changed = true;
+    while (any_changed) {
+      any_changed = false;
+      worklist.push_back({block->nodes().rbegin(), block->nodes().rend()});
 
-    aliasDb.moveBeforeTopologicallyValid(listconstruct, consumer);
-    GRAPH_UPDATE(
-        "Merging ", getHeader(listconstruct), " into ", getHeader(consumer));
-    SubgraphUtils::mergeNodeIntoSubgraph(listconstruct, consumer);
-  } else {
-    aliasDb.moveBeforeTopologicallyValid(producer, consumer);
-    GRAPH_UPDATE(
-        "Merging ", getHeader(producer), " into ", getHeader(consumer));
-    SubgraphUtils::mergeNodeIntoSubgraph(producer, consumer);
-  }
+      while (worklist.size()) {
+        auto& it = worklist.back().first;
+        auto end = worklist.back().second;
 
-  return consumer;
-}
+        if (it->blocks().size()) {
+          Node* n = *it;
+          ++it;
 
-std::pair<graph_node_list::iterator, bool> scanNode(
-    Node* consumer,
-    AliasDb& aliasDb) {
-  auto inputs =
-      sortReverseTopological(consumer->inputs(), consumer->owningBlock());
+          if (it == end) {
+            worklist.pop_back();
+          }
 
-  // Grab the iterator below consumer.  We'll use that to determine
-  // where to resume iteration, even if consumer gets relocated within
-  // the block.
-  auto iter = --consumer->reverseIterator();
-  for (auto input : inputs) {
-    if (auto group = tryMerge(consumer, input->node(), aliasDb)) {
-      // Resume iteration from where consumer is/used to be.
-      return {++iter, true};
+          for (auto b : n->blocks()) {
+            if (!visited_blocks.count(b)) {
+              worklist.push_back({b->nodes().rbegin(), b->nodes().rend()});
+              visited_blocks.insert(b);
+            }
+          }
+        } else {
+          bool changed;
+          std::tie(it, changed) = scanNode(*it);
+          any_changed |= changed;
+          if (it == end) {
+            worklist.pop_back();
+          }
+        }
+      }
     }
   }
 
-  // We know consumer didn't move, so skip over it.
-  return {++(++iter), false};
-}
+  std::pair<graph_node_list::iterator, bool> scanNode(Node* consumer) {
+    auto inputs =
+        sortReverseTopological(consumer->inputs(), consumer->owningBlock());
+
+    // Grab the iterator below consumer.  We'll use that to determine
+    // where to resume iteration, even if consumer gets relocated within
+    // the block.
+    auto iter = --consumer->reverseIterator();
+    for (auto input : inputs) {
+      if (auto group = tryMerge(consumer, input->node())) {
+        // Resume iteration from where consumer is/used to be.
+        return {++iter, true};
+      }
+    }
+
+    // We know consumer didn't move, so skip over it.
+    return {++(++iter), false};
+  }
+
+  c10::optional<Node*> tryMerge(Node* consumer, Node* producer) {
+    GRAPH_DEBUG(
+        "Trying producer ",
+        getHeader(producer),
+        " and consumer ",
+        getHeader(consumer),
+        ":\n");
+
+    if (!canMerge(consumer, producer)) {
+      return c10::nullopt;
+    }
+
+    consumer = getOrCreateTensorExprSubgraph(consumer);
+
+    if (producer->kind() == aten::cat) {
+      Node* listconstruct = producer->inputs()[0]->node();
+
+      aliasDb_->moveBeforeTopologicallyValid(producer, consumer);
+      GRAPH_UPDATE(
+          "Merging ", getHeader(producer), " into ", getHeader(consumer));
+      SubgraphUtils::mergeNodeIntoSubgraph(producer, consumer);
+
+      aliasDb_->moveBeforeTopologicallyValid(listconstruct, consumer);
+      GRAPH_UPDATE(
+          "Merging ", getHeader(listconstruct), " into ", getHeader(consumer));
+      SubgraphUtils::mergeNodeIntoSubgraph(listconstruct, consumer);
+    } else {
+      aliasDb_->moveBeforeTopologicallyValid(producer, consumer);
+      GRAPH_UPDATE(
+          "Merging ", getHeader(producer), " into ", getHeader(consumer));
+      SubgraphUtils::mergeNodeIntoSubgraph(producer, consumer);
+    }
+
+    return consumer;
+  }
+
+#define REQ(cond)                           \
+  if (!(cond)) {                            \
+    GRAPH_DEBUG("Failed cond " #cond "\n"); \
+    return false;                           \
+  }
+
+  bool canMerge(Node* consumer, Node* producer) {
+    // Only handle complete tensor types
+    for (torch::jit::Value* output : consumer->outputs()) {
+      REQ(output->isCompleteTensor());
+    }
+
+    // Only fuse within a block
+    REQ(consumer->owningBlock() == producer->owningBlock());
+
+    // Symbolic checks
+    REQ(canHandle(producer));
+    REQ((canHandle(consumer) || consumer->kind() == getTensorExprSymbol()));
+
+    // Alias checks
+    REQ(aliasDb_->couldMoveBeforeTopologically(producer, consumer));
+
+    // Ops that return aliases can only be folded if this is the only use.
+    if (producer->kind() == aten::slice ||
+        producer->kind() == aten::unsqueeze ||
+        producer->kind() == prim::ConstantChunk) {
+      for (auto& use : producer->output(0)->uses()) {
+        REQ(use.user == consumer);
+      }
+    }
+
+    if (!consumer->hasAttribute(attr::Subgraph) &&
+        consumer->kind() != getTensorExprSymbol()) {
+      // Don't initiate a fusion group from prim::ListConstruct
+      REQ(consumer->kind() != prim::ListConstruct);
+      REQ(consumer->kind() != aten::slice);
+      REQ(consumer->kind() != aten::unsqueeze);
+      REQ(consumer->kind() != prim::ConstantChunk);
+
+      // Don't initiate a fusion group just for a constant operand
+      REQ(producer->kind() != prim::Constant);
+    }
+
+    if (producer->kind() == aten::cat) {
+      REQ(producer->input(0)->node()->kind() == prim::ListConstruct);
+      REQ(producer->input(0)->uses().size() == 1);
+      REQ(producer->input(1)->node()->kind() == prim::Constant);
+    } else if (consumer->kind() == aten::cat) {
+      REQ(consumer->input(0)->node()->kind() == prim::ListConstruct);
+      REQ(consumer->input(0)->uses().size() == 1);
+      REQ(consumer->input(1)->node()->kind() == prim::Constant);
+    }
+
+    return true;
+  }
+#undef REQ
+
+  std::shared_ptr<Graph> graph_;
+  std::unique_ptr<AliasDb> aliasDb_ = nullptr;
+};
 
 void FuseTensorExprs(std::shared_ptr<Graph>& graph) {
   GRAPH_DUMP("Before TExprFuser: ", graph);
@@ -322,46 +374,8 @@ void FuseTensorExprs(std::shared_ptr<Graph>& graph) {
   // Get rid of dead code so that we don't waste effort fusing it.
   EliminateDeadCode(graph);
 
-  AliasDb aliasDb(graph);
-  auto block = graph->block();
-
-  std::vector<std::pair<graph_node_list_iterator, graph_node_list_iterator>>
-      worklist;
-  std::unordered_set<torch::jit::Block*> visited_blocks;
-
-  bool any_changed = true;
-  while (any_changed) {
-    any_changed = false;
-    worklist.push_back({block->nodes().rbegin(), block->nodes().rend()});
-
-    while (worklist.size()) {
-      auto& it = worklist.back().first;
-      auto end = worklist.back().second;
-
-      if (it->blocks().size()) {
-        Node* n = *it;
-        ++it;
-
-        if (it == end) {
-          worklist.pop_back();
-        }
-
-        for (auto b : n->blocks()) {
-          if (!visited_blocks.count(b)) {
-            worklist.push_back({b->nodes().rbegin(), b->nodes().rend()});
-            visited_blocks.insert(b);
-          }
-        }
-      } else {
-        bool changed;
-        std::tie(it, changed) = scanNode(*it, aliasDb);
-        any_changed |= changed;
-        if (it == end) {
-          worklist.pop_back();
-        }
-      }
-    }
-  }
+  TensorExprFuser fuser(graph);
+  fuser.run();
 
   EliminateCommonSubexpression(graph);
   EliminateDeadCode(graph);
