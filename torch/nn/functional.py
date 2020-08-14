8c6d36638e2cc666f6f20d1921f6c29d9293702e
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-08-14 17:17:01 -0500

    Remove inline comments

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -1163,7 +1163,6 @@ def glu(input, dim=-1):
 
 
 def hardtanh(input: Tensor, min_val: float = -1., max_val: float = 1., inplace: bool = False):
-    # type: (Tensor, float, float, bool) -> Tensor
     r"""
     hardtanh(input, min_val=-1., max_val=1., inplace=False) -> Tensor
 
@@ -1283,7 +1282,6 @@ In-place version of :func:`~celu`.
 
 
 def leaky_relu(input: Tensor, negative_slope: float = 0.01, inplace: bool = False):
-    # type: (Tensor, float, bool) -> Tensor
     r"""
     leaky_relu(input, negative_slope=0.01, inplace=False) -> Tensor
 
