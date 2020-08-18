3c7227f63fbef9efc18f53ae3ee82b859205d9e7
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2020-08-17 20:19:54 -0700

    C++ APIs TransformerEncoder
    [ghstack-poisoned]

--- test/cpp/api/transformer.cpp
+++ test/cpp/api/transformer.cpp
@@ -9,31 +9,39 @@ using namespace torch::test;
 
 struct TransformerTest : torch::test::SeedingFixture {};
 
-void transformer_encoder_layer_test_helper(bool is_cuda) {
-  // this is a deterministic test for TransformerEncoderLayer
+// a generic function to provide consistent encoder/decoder layer for all the transformer tests
+template<typename T_LAYER, typename T_OPTIONS>
+T_LAYER get_a_test_layer(const torch::TensorOptions& tensor_options) {
   int64_t d_model = 4;
   int64_t nhead = 2;
   int64_t dim_feedforward = 16;
   double dropout = 0.0;
 
-  TransformerEncoderLayer model(
-    TransformerEncoderLayerOptions(
-      d_model, nhead).dim_feedforward(dim_feedforward).dropout(dropout));
-  if (is_cuda) {
-    model->to(torch::kCUDA);
+  // activation is always ReLU here and it can be adjusted later depending on the usage
+  T_LAYER layer(T_OPTIONS(d_model, nhead).dim_feedforward(dim_feedforward).dropout(dropout));
+  if (tensor_options.device() == torch::kCUDA) {
+    layer->to(torch::kCUDA);
   }
-
-  torch::Device device = is_cuda ? torch::kCUDA : torch::kCPU;
-  torch::TensorOptions tensor_options = torch::TensorOptions().dtype(torch::kFloat32).device(device);
 
   // set constant weights of the model
   {
     torch::NoGradGuard guard;
-    for (auto& p : model->parameters()) {
+    for (auto& p : layer->parameters()) {
       auto sz = p.view(-1).size(0);
       p.copy_(torch::cos(torch::arange(0, sz, tensor_options).view(p.sizes())));
     }
   }
+
+  return layer;
+}
+
+void transformer_encoder_layer_test_helper(bool is_cuda) {
+  // this is a deterministic test for TransformerEncoderLayer
+  torch::Device device = is_cuda ? torch::kCUDA : torch::kCPU;
+  torch::TensorOptions tensor_options = torch::TensorOptions().dtype(torch::kFloat32).device(device);
+
+  TransformerEncoderLayer model =
+    get_a_test_layer<TransformerEncoderLayer, TransformerEncoderLayerOptions>(tensor_options);
 
   // relu test case 1
   torch::Tensor encoder_input = torch::tensor({{{20, 30, 40, 50}}}, tensor_options);
@@ -150,33 +158,13 @@ TEST_F(TransformerTest, TransformerEncoderLayer_CUDA) {
 }
 
 void transformer_decoder_layer_test_helper(bool is_cuda){
-  int64_t d_model = 4;
-  int64_t nhead = 2;
-  int64_t dim_feedforward = 16;
-  double dropout = 0.0;
-
-  TransformerDecoderLayer model = TransformerDecoderLayer(
-    TransformerDecoderLayerOptions(d_model, nhead)
-    .dim_feedforward(dim_feedforward).dropout(dropout));
-  if (is_cuda) {
-    model->to(torch::kCUDA);
-  }
 
   torch::Device device = is_cuda ? torch::kCUDA : torch::kCPU;
   torch::TensorOptions tensor_options = torch::TensorOptions()
     .dtype(torch::kFloat32).device(device);
 
-  //set constant weights of the model
-  {
-    torch::NoGradGuard guard;
-    for (const auto& parameter : model->parameters()) {
-      at::Tensor x = parameter.data();
-      auto sz = x.view(-1).size(0);
-      auto shape = x.sizes();
-      x = torch::cos(torch::arange(0, sz, tensor_options).view(shape));
-      parameter.data().copy_(x);
-    }
-  }
+  TransformerDecoderLayer model =
+    get_a_test_layer<TransformerDecoderLayer, TransformerDecoderLayerOptions>(tensor_options);
 
   // deterministic input
   at::Tensor decoder_input = torch::tensor({{{20, 30, 40, 50}}}, tensor_options);
@@ -324,36 +312,14 @@ TEST_F(TransformerTest, TransformerDecoderLayer_CUDA){
 }
 
 void transformer_decoder_layer_test_helper_gelu(bool is_cuda) {
-  int64_t d_model = 4;
-  int64_t nhead = 2;
-  int64_t dim_feedforward = 16;
-  double dropout = 0.0;
-
-
-  TransformerDecoderLayer model = TransformerDecoderLayer(
-    TransformerDecoderLayerOptions(d_model, nhead)
-    .dim_feedforward(dim_feedforward)
-    .dropout(dropout)
-    .activation(torch::kGELU));
-  if (is_cuda) {
-    model->to(torch::kCUDA);
-  }
 
   torch::Device device = is_cuda ? torch::kCUDA : torch::kCPU;
   torch::TensorOptions tensor_options = torch::TensorOptions()
     .dtype(torch::kFloat32).device(device);
 
-  //set constant weights of the model
-  {
-    torch::NoGradGuard guard;
-    for (const auto& parameter : model->parameters()) {
-      at::Tensor x = parameter.data();
-      auto sz = x.view(-1).size(0);
-      auto shape = x.sizes();
-      x = torch::cos(torch::arange(0, sz, tensor_options).view(shape));
-      parameter.data().copy_(x);
-    }
-  }
+  TransformerDecoderLayer model =
+    get_a_test_layer<TransformerDecoderLayer, TransformerDecoderLayerOptions>(tensor_options);
+  model.get()->options.activation(torch::kGELU);
 
   // deterministic input
   at::Tensor decoder_input = torch::tensor({{{20, 30, 40, 50}}},
@@ -436,7 +402,70 @@ TEST_F(TransformerTest, TransformerDecoderLayer_gelu) {
 }
 
 TEST_F(TransformerTest, TransformerDecoderLayer_gelu_CUDA) {
-    transformer_decoder_layer_test_helper_gelu(true);
+  transformer_decoder_layer_test_helper_gelu(true);
+}
+
+void transformer_encoder_test_helper(bool is_cuda) {
+  // this is a deterministic test for TransformerEncoderLayer
+  torch::Device device = is_cuda ? torch::kCUDA : torch::kCPU;
+  torch::TensorOptions tensor_options = torch::TensorOptions().dtype(torch::kFloat32).device(device);
+
+  TransformerEncoderLayer encoder_layer =
+    get_a_test_layer<TransformerEncoderLayer, TransformerEncoderLayerOptions>(tensor_options);
+
+  TransformerEncoder model(TransformerEncoderOptions(encoder_layer, 1));
+  if (is_cuda) {
+    model->to(torch::kCUDA);
+  }
+
+  torch::Tensor encoder_input = torch::tensor({
+    {{0.7462, 0.6653, 0.5679, 0.4891}, {0.5387, 0.1655, 0.3565, 0.0471}},
+    {{0.8335, 0.2799, 0.5031, 0.2947}, {0.1402, 0.0318, 0.7636, 0.1346}},
+    {{0.6333, 0.9344, 0.1376, 0.9938}, {0.8924, 0.2872, 0.6692, 0.2944}},
+    {{0.9897, 0.6915, 0.3154, 0.1733}, {0.8645, 0.3513, 0.3064, 0.0767}},
+    {{0.8117, 0.2366, 0.4838, 0.7881}, {0.3718, 0.4945, 0.9511, 0.0864}}}, tensor_options);
+  torch::Tensor result = model(encoder_input).detach();
+  torch::Tensor ref_output = torch::tensor({
+    {{2.428589, 0.020835, -0.602055, -0.085249}, {2.427987, 0.021213, -0.602496, -0.084103}},
+    {{2.424689, 0.019155, -0.604793, -0.085672}, {2.413863, 0.022211, -0.612486, -0.072490}},
+    {{2.433774, 0.021598, -0.598343, -0.087548}, {2.425104, 0.019748, -0.604515, -0.084839}},
+    {{2.436185, 0.022682, -0.596625, -0.087261}, {2.433556, 0.021891, -0.598509, -0.086832}},
+    {{2.416246, 0.017512, -0.610712, -0.082961}, {2.422901, 0.024187, -0.606178, -0.074929}}}, tensor_options);
+  ASSERT_EQ(result.sizes(), ref_output.sizes());
+  ASSERT_TRUE(torch::allclose(result, ref_output, 1e-7, 1e-5, /*equal_nan=*/true));
+
+  // all 0 values are NOT masked
+  torch::Tensor mask = torch::zeros({2, 5}, tensor_options) == 1;
+  result = model(encoder_input, /*src_mask=*/torch::Tensor{}, /*src_key_padding_mask=*/mask).detach();
+  ASSERT_EQ(result.sizes(), ref_output.sizes());
+  ASSERT_TRUE(torch::allclose(result, ref_output, 1e-7, 1e-5, /*equal_nan=*/true));
+
+  // mask with 0s and 1s
+  mask[0][1] = 1;
+  mask[1][3] = 1;
+  mask[1][4] = 1;
+  result = model(encoder_input, /*src_mask=*/torch::Tensor{}, /*src_key_padding_mask=*/mask).detach();
+  ref_output = torch::tensor({
+    {{2.429026, 0.020793, -0.601741, -0.085642}, {2.428811, 0.021445, -0.601912, -0.084252}},
+    {{2.425009, 0.019155, -0.604566, -0.085899}, {2.415408, 0.02249 , -0.611415, -0.073}},
+    {{2.434199, 0.021682, -0.598039, -0.087699}, {2.42598, 0.019941, -0.603896, -0.085091}},
+    {{2.436457, 0.022736, -0.59643 , -0.08736},  {2.434021, 0.022093, -0.598179, -0.08679}},
+    {{2.416531, 0.017498, -0.610513, -0.083181}, {2.4242, 0.024653, -0.605266, -0.074959}}}, tensor_options);
+  ASSERT_EQ(result.sizes(), ref_output.sizes());
+  ASSERT_TRUE(torch::allclose(result, ref_output, 1e-7, 1e-5, /*equal_nan=*/true));
+
+  // test case 2, multiple layers no norm
+  model = TransformerEncoder(TransformerEncoderOptions(encoder_layer, 3));
+
+
+}
+
+TEST_F(TransformerTest, TransformerEncoder) {
+  transformer_encoder_test_helper(false);
+}
+
+TEST_F(TransformerTest, TransformerEncoder_CUDA) {
+  transformer_encoder_test_helper(true);
 }
 
 TEST_F(TransformerTest, PrettyPrintTransformerDecoderLayer) {
