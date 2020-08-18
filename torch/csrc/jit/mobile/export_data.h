dd194c16122a11c7f7263e7e3dfe29cd3a2bdd5a
Ann Shan <annshan@fb.com>
2020-08-18 14:53:51 -0700

    add _save_parameters to serialize map (#43163)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/43163
    
    Test Plan: Imported from OSS
    
    Reviewed By: iseeyuan
    
    Differential Revision: D23175287
    
    Pulled By: ann-ss
    
    fbshipit-source-id: ddfd734513c07e8bdbec108f26d1ca1770d098a6

--- torch/csrc/jit/mobile/export_data.h
+++ torch/csrc/jit/mobile/export_data.h
@@ -11,5 +11,14 @@ TORCH_API void _save_data(const Module& module, std::ostream& out);
 TORCH_API void _save_data(const Module& module, const std::string& filename);
 
 } // namespace mobile
+
+TORCH_API void _save_parameters(
+    const std::map<std::string, at::Tensor>& map,
+    std::ostream& out);
+
+TORCH_API void _save_parameters(
+    const std::map<std::string, at::Tensor>& map,
+    const std::string& filename);
+
 } // namespace jit
 } // namespace torch
