pytorch/pytorch

commit b3bda943932496572f4835862f3ec9e1d9b3f22e

[NVFuser] Enable E2E BCast-PWise-Reduction fusions (#43129)
Summary:
Had a bunch of merged commits that shouldn't have been there, reverted them to prevent conflicts. Lots of new features, highlights listed below.

**Overall:**

- Enables pointwise fusion, single (but N-D) broadcast -- pointwise fusion, single (but N-D) broadcast -- pointwise -- single (but N-D) reduction fusion.

**Integration:**

- Separate "magic scheduler" logic that takes a fusion and generates code generator schedule
- Reduction fusion scheduling with heuristics closely matching eagermode (unrolling supported, but no vectorize support)
- 2-Stage caching mechanism, one on contiguity, device, type, and operations, the other one is input size->reduction heuristic

**Code Generation:**

- More generic support in code generation for computeAt
- Full rework of loop nest generation and Indexing to more generically handle broadcast operations
- Code generator has automatic kernel launch configuration (including automatic allocation of grid reduction buffers)
- Symbolic (runtime) tilling on grid/block dimensions is supported
- Simplified index generation based on user-defined input contiguity
- Automatic broadcast support (similar to numpy/pytorch semantics)
- Support for compile time constant shared memory buffers
- Parallelized broadcast support (i.e. block reduction -> block broadcast support)

Pull Request resolved: https://github.com/pytorch/pytorch/pull/43129

Reviewed By: mrshenli

Differential Revision: D23162207

Pulled By: soumith

fbshipit-source-id: 16deee4074c64de877eed7c271d6a359927111b2

torch/csrc/jit/codegen/cuda/lower_thread_predicate.h
@@ -2,6 +2,7 @@
 #include <torch/csrc/WindowsTorchApiMacro.h>
 
 #include <torch/csrc/jit/codegen/cuda/ir_all_nodes.h>
+#include <torch/csrc/jit/codegen/cuda/lower_utils.h>
 
 #include <bitset>
 
@@ -9,34 +10,50 @@ namespace torch {
 namespace jit {
 namespace fuser {
 
-class TORCH_CUDA_API ThreadPredicates {
+/*
+ * Map from tensorview to bit set represnting <BIDx, BIDy, BIDz, TIDx, TIDy,
+ * TIDz> If any dependency of TV had a parallelized reduction, we will track
+ * it here. This will be used for predicate generation to prevent
+ * parallelization on that axis. This is important if we have a reduction on
+ * for example TIDx, as the reduced value is only valid on threadIdx.x == 0
+ * therefore if we use that value later in the kernel we have that predicate.
+ * If we follow a reduction parallelized on TIDx with a broadcast on TIDx we
+ * no longer need the predicate and can reset the bit accordingly
+ */
+class TORCH_CUDA_API ThreadPredicateMap {
+ public:
+  using SourceMapType =
+      std::unordered_map<ParallelType, std::unordered_set<const TensorView*>>;
+  using MapType = std::unordered_map<
+      const TensorView*,
+      std::pair<ir_utils::ParallelTypeBitmap, SourceMapType>>;
+  using const_iterator = MapType::const_iterator;
+
+  explicit ThreadPredicateMap(Fusion* _fusion);
+
+  const_iterator find(const TensorView* tv) const;
+  const_iterator end() const;
+  const MapType::mapped_type& at(const TensorView* tv) const;
+  MapType::mapped_type& at(const TensorView* tv);
+  MapType::mapped_type& operator[](const TensorView* tv);
+
+  void duplicate(const TensorView* copy, const TensorView* origin);
+
+  // Returns a Bool predicate expression for a given TensorView.
+  kir::Bool* getExpr(const TensorView* tv) const;
+
  private:
   Fusion* fusion_;
-
-  /*
-   * Map from tensorview to bit set represnting <BIDx, BIDy, BIDz, TIDx, TIDy,
-   * TIDz> If any dependency of TV had a parallelized reduction, we will track
-   * it here. This will be used for predicate generation to prevent
-   * parallelization on that axis. This is important if we have a reduction on
-   * for example TIDx, as the reduced value is only valid on threadIdx.x == 0
-   * therefore if we use that value later in the kernel we have that predicate.
-   * If we follow a reduction parallelized on TIDx with a broadcast on TIDx we
-   * no longer need the predicate and can reset the bit accordingly
-   */
-  std::unordered_map<const TensorView*, std::bitset<6>> thread_predicates;
+  MapType thread_predicates_;
 
   // Update the thread_predicates bitset based on provided Expr
   void updateBitSet(Expr*);
 
-  // Safety wrapper to access thread_predicates
-  std::bitset<6> getThreadPredicates(const TensorView*);
-
-  ThreadPredicates(Fusion* _fusion);
-
- public:
-  // Computes any thread predicates that need to be applied when computing a
-  // TensorView.
-  static std::unordered_map<const TensorView*, Bool*> compute(Fusion* fusion);
+  void insert(
+      const TensorView* tv,
+      const ir_utils::ParallelTypeBitmap& pred,
+      const SourceMapType& src_map);
+  void insert(const TensorView* tv, const MapType::mapped_type& pred_and_src);
 };
 
 } // namespace fuser

torch/csrc/jit/codegen/cuda/lower_utils.cpp
@@ -1,5 +1,11 @@
 #include <torch/csrc/jit/codegen/cuda/lower_utils.h>
+#include <torch/csrc/jit/codegen/cuda/arith.h>
 #include <torch/csrc/jit/codegen/cuda/ir_iostream.h>
+#include <torch/csrc/jit/codegen/cuda/ir_utils.h>
+#include <torch/csrc/jit/codegen/cuda/lower_thread_predicate.h>
+
+#include <algorithm>
+
 namespace torch {
 namespace jit {
 namespace fuser {
@@ -11,63 +17,37 @@ namespace {
 
 class Loops : private OptInDispatch {
  private:
-  std::deque<ForLoop*> loops;
-  void handle(ForLoop* fl) final {
+  std::deque<kir::ForLoop*> loops;
+  void handle(kir::ForLoop* fl) final {
     loops.insert(loops.begin(), fl);
   }
 
-  void handle(IfThenElse* ite) final {}
+  void handle(kir::IfThenElse* ite) final {}
 
   void handle(Expr* expr) final {
     OptInDispatch::handle(expr);
   }
 
  public:
-  static std::vector<ForLoop*> getLoops(Expr* scope) {
+  static std::vector<kir::ForLoop*> getLoops(Expr* scope) {
     Loops loops;
     Expr* it = scope;
     while (it != nullptr) {
       loops.handle(it);
       it = scope_utils::getParent(it);
     }
-    return std::vector<ForLoop*>(loops.loops.begin(), loops.loops.end());
-  }
-};
-
-class forLoopCount : private OptInDispatch {
- private:
-  unsigned int count_ = 0;
-
-  void handle(ForLoop* fl) final {
-    count_++;
-  }
-
-  void handle(IfThenElse* ite) final {}
-
-  void handle(Expr* expr) final {
-    OptInDispatch::handle(expr);
-  }
-
- public:
-  static unsigned int get(Expr* scope) {
-    forLoopCount flc;
-    Expr* it = scope;
-    while (it != nullptr) {
-      flc.handle(it);
-      it = scope_utils::getParent(it);
-    }
-    return flc.count_;
+    return std::vector<kir::ForLoop*>(loops.loops.begin(), loops.loops.end());
   }
 };
 
 class scopePushBack : private OptInDispatch {
  private:
   Expr* expr_;
-  void handle(ForLoop* fl) final {
+  void handle(kir::ForLoop* fl) final {
     fl->body().push_back(expr_);
   }
 
-  void handle(IfThenElse* ite) final {
+  void handle(kir::IfThenElse* ite) final {
     ite->body().push_back(expr_);
   }
 
@@ -91,11 +71,11 @@ class scopeInsertBefore : private OptInDispatch {
  private:
   Expr* ref_;
   Expr* expr_;
-  void handle(ForLoop* fl) final {
+  void handle(kir::ForLoop* fl) final {
     fl->body().insert_before(ref_, expr_);
   }
 
-  void handle(IfThenElse* ite) final {
+  void handle(kir::IfThenElse* ite) final {
     ite->body().insert_before(ref_, expr_);
   }
 
@@ -115,15 +95,49 @@ class scopeInsertBefore : private OptInDispatch {
   }
 };
 
+class ExprInScope : private OptInDispatch {
+ private:
+  Expr* expr_;
+  bool contains_ = false;
+
+  void handle(kir::ForLoop* fl) final {
+    if (fl->body().contains(expr_)) {
+      contains_ = true;
+    }
+  }
+
+  void handle(kir::IfThenElse* ite) final {
+    if (ite->body().contains(expr_)) {
+      contains_ = true;
+    }
+  }
+
+  void handle(Expr* expr) final {
+    OptInDispatch::handle(expr);
+  }
+
+  ExprInScope(Expr* expr) : expr_(expr) {}
+
+ public:
+  static bool find(Expr* scope, Expr* expr) {
+    ExprInScope eis(expr);
+    TORCH_INTERNAL_ASSERT(
+        expr != nullptr && scope != nullptr,
+        "Cannot push back, scope or expr is a nullptr.");
+    eis.handle(scope);
+    return eis.contains_;
+  }
+};
+
 class parentScope : private OptInDispatch {
  private:
   Expr* parent_ = nullptr;
 
-  void handle(ForLoop* fl) final {
+  void handle(kir::ForLoop* fl) final {
     parent_ = fl->parentScope();
   }
 
-  void handle(IfThenElse* ite) final {
+  void handle(kir::IfThenElse* ite) final {
     parent_ = ite->parentScope();
   }
 
@@ -139,29 +153,6 @@ class parentScope : private OptInDispatch {
   }
 };
 
-class scopeClearExprs : private OptInDispatch {
- private:
-  void handle(ForLoop* fl) final {
-    fl->body().clear();
-  }
-
-  void handle(IfThenElse* ite) final {
-    ite->body().clear();
-  }
-
-  void handle(Expr* expr) final {
-    OptInDispatch::handle(expr);
-  }
-
- public:
-  static void clear(Expr* scope) {
-    scopeClearExprs sce;
-    TORCH_INTERNAL_ASSERT(
-        scope != nullptr, "Cannot clear scope, scope is a nullptr.");
-    sce.handle(scope);
-  }
-};
-
 void assertScope(Expr* expr) {
   TORCH_INTERNAL_ASSERT(
       expr->getExprType() == ExprType::ForLoop ||
@@ -174,15 +165,15 @@ class CloneLoopNest : public OptOutMutator {
   Expr* parent_scope_ = nullptr;
   Expr* to_clone_ = nullptr;
 
-  Statement* mutate(ForLoop* fl) final {
+  Statement* mutate(kir::ForLoop* fl) final {
     std::vector<Expr*> mutated_exprs;
     for (Expr* expr : fl->body().exprs()) {
       mutated_exprs.push_back(ir_utils::asExpr(OptOutMutator::mutate(expr)));
     }
     if (fl == to_clone_)
-      return new ForLoop(
+      return new kir::ForLoop(
           fl->index(), fl->iter_domain(), mutated_exprs, parent_scope_);
-    return new ForLoop(
+    return new kir::ForLoop(
         fl->index(), fl->iter_domain(), mutated_exprs, fl->parentScope());
   }
 
@@ -190,7 +181,7 @@ class CloneLoopNest : public OptOutMutator {
       : parent_scope_(_parent_scope), to_clone_(_to_clone) {}
 
  public:
-  static ForLoop* getClone(ForLoop* _to_clone, Expr* _parent_scope) {
+  static kir::ForLoop* getClone(kir::ForLoop* _to_clone, Expr* _parent_scope) {
     TORCH_INTERNAL_ASSERT(
         _to_clone != nullptr,
         "Tried to clone a scope, but received a nullptr.");
@@ -212,7 +203,7 @@ class ReplaceExprsInScope : public OptOutDispatch {
   explicit ReplaceExprsInScope(std::unordered_map<Expr*, Expr*> replacement_map)
       : replacement_map_(std::move(replacement_map)) {}
 
-  void handleScope(Scope& scope) {
+  void handleScope(kir::Scope& scope) {
     for (size_t i = 0; i < scope.size(); ++i) {
       const auto it = replacement_map_.find(scope[i]);
       if (it == replacement_map_.end()) {
@@ -227,11 +218,11 @@ class ReplaceExprsInScope : public OptOutDispatch {
     OptOutDispatch::handle(expr);
   }
 
-  void handle(ForLoop* fl) final {
+  void handle(kir::ForLoop* fl) final {
     handleScope(fl->body());
   }
 
-  void handle(IfThenElse* ite) final {
+  void handle(kir::IfThenElse* ite) final {
     handleScope(ite->body());
     handleScope(ite->elseBody());
   }
@@ -244,7 +235,7 @@ class FirstInnerMostScope : private OptInDispatch {
  private:
   Expr* active_scope = nullptr;
 
-  void handle(ForLoop* fl) final {
+  void handle(kir::ForLoop* fl) final {
     for (auto expr : fl->body().exprs()) {
       if (ir_utils::isScope(expr)) {
         active_scope = expr;
@@ -254,7 +245,7 @@ class FirstInnerMostScope : private OptInDispatch {
     active_scope = nullptr;
   }
 
-  void handle(IfThenElse* ite) final {
+  void handle(kir::IfThenElse* ite) final {
     for (auto expr : ite->body().exprs()) {
       if (ir_utils::isScope(expr)) {
         active_scope = expr;
@@ -297,19 +288,11 @@ class FirstInnerMostScope : private OptInDispatch {
 } // namespace
 
 // Grab the ForLoop starting from scope working out
-std::vector<ForLoop*> getLoops(Expr* scope) {
+std::vector<kir::ForLoop*> getLoops(Expr* scope) {
   if (scope == nullptr)
-    return std::vector<ForLoop*>();
+    return std::vector<kir::ForLoop*>();
   assertScope(scope);
   return Loops::getLoops(scope);
-}
-
-// Track how far our for loop scope is
-unsigned int computeForDepth(Expr* scope) {
-  if (scope == nullptr)
-    return 0;
-  assertScope(scope);
-  return forLoopCount::get(scope);
 }
 
 // Push back an expr to scope
@@ -325,6 +308,10 @@ void insertBefore(Expr* scope, Expr* ref, Expr* expr) {
   scopeInsertBefore::insert(scope, ref, expr);
 }
 
+bool exprInScope(Expr* scope, Expr* expr) {
+  return ExprInScope::find(scope, expr);
+}
+
 // Return the parent of the active scope
 Expr* getParent(Expr* scope) {
   TORCH_INTERNAL_ASSERT(
@@ -335,38 +322,23 @@ Expr* getParent(Expr* scope) {
 }
 
 // Open a new inner most for loop
-ForLoop* openFor(Expr* scope, IterDomain* id) {
-  ForLoop* new_scope = nullptr;
+kir::ForLoop* openFor(Expr* scope, IterDomain* id) {
+  const auto kir_id = kir::lowerValue(id)->as<kir::IterDomain>();
+  kir::ForLoop* new_scope = nullptr;
   if (id->isThread()) {
     std::stringstream ss;
-    ss << id->parallel_method();
-    new_scope =
-        new ForLoop(new NamedScalar(ss.str(), DataType::Int), id, {}, scope);
+    ss << id->getParallelType();
+    new_scope = new kir::ForLoop(
+        new kir::NamedScalar(ss.str(), DataType::Int), kir_id, {}, scope);
   } else {
-    new_scope = new ForLoop(new Int(), id, {}, scope);
+    new_scope = new kir::ForLoop(new kir::Int(c10::nullopt), kir_id, {}, scope);
   }
   if (scope != nullptr)
     pushBack(scope, new_scope);
   return new_scope;
 }
 
-// Close the inner most for loop
-Expr* closeScope(Expr* scope) {
-  TORCH_INTERNAL_ASSERT(
-      scope != nullptr, "Tried to close a scope but got a nullptr.");
-  return getParent(scope);
-}
-
-// Clear all expressions from the scope
-Expr* clearScope(Expr* scope) {
-  TORCH_INTERNAL_ASSERT(
-      scope != nullptr, "Tried to clear a scope but got a nullptr.");
-  assertScope(scope);
-  scopeClearExprs::clear(scope);
-  return scope;
-}
-
-ForLoop* cloneLoopNest(ForLoop* to_clone, Expr* parent_scope) {
+kir::ForLoop* cloneLoopNest(kir::ForLoop* to_clone, Expr* parent_scope) {
   return CloneLoopNest::getClone(to_clone, parent_scope);
 }
 
@@ -387,20 +359,51 @@ Expr* firstInnerMostScope(Expr* scope) {
 
 namespace ir_utils {
 
-std::vector<Val*> indices(std::vector<ForLoop*> loops) {
-  std::vector<Val*> inds(loops.size());
-  std::transform(loops.begin(), loops.end(), inds.begin(), [](ForLoop* fl) {
-    return fl->index();
-  });
-  return inds;
+TVDomainGuard::TVDomainGuard(TensorView* _tv, TensorDomain* td)
+    : tv_(_tv), prev_domain(tv_->domain()) {
+  tv_->setDomain(td);
 }
 
-std::vector<IterDomain*> iterDomains(std::vector<ForLoop*> loops) {
-  std::vector<IterDomain*> ids(loops.size());
-  std::transform(loops.begin(), loops.end(), ids.begin(), [](ForLoop* fl) {
-    return fl->iter_domain();
-  });
-  return ids;
+TVDomainGuard::~TVDomainGuard() {
+  tv_->setDomain(prev_domain);
+}
+
+std::vector<IterDomain*> iterDomainInputsOf(
+    const std::vector<IterDomain*>& input_ids) {
+  auto inputs = IterVisitor::getInputsTo({input_ids.begin(), input_ids.end()});
+  std::vector<IterDomain*> id_inputs(
+      ir_utils::filterByType<IterDomain>(inputs).begin(),
+      ir_utils::filterByType<IterDomain>(inputs).end());
+  return id_inputs;
+}
+
+std::vector<IterDomain*> iterDomainInputsOfOrderedAs(
+    const std::vector<IterDomain*>& of,
+    const std::vector<IterDomain*>& order) {
+  auto inputs_vec = iterDomainInputsOf(of);
+
+  std::unordered_set<IterDomain*> inputs_set(
+      inputs_vec.begin(), inputs_vec.end());
+
+  std::vector<IterDomain*> ordered_inputs;
+  std::copy_if(
+      order.begin(),
+      order.end(),
+      std::back_inserter(ordered_inputs),
+      [&inputs_set](const auto& id) {
+        return inputs_set.find(id) != inputs_set.end();
+      });
+
+  return ordered_inputs;
+}
+
+std::vector<Val*> indices(std::vector<kir::ForLoop*> loops) {
+  std::vector<Val*> inds(loops.size());
+  std::transform(
+      loops.begin(), loops.end(), inds.begin(), [](kir::ForLoop* fl) {
+        return fl->index();
+      });
+  return inds;
 }
 
 bool isTV(const Val* val) {
@@ -419,6 +422,15 @@ bool isTVOp(const Expr* expr) {
   return false;
 }
 
+TensorView* getTVOutput(const Expr* expr) {
+  for (auto out : expr->outputs()) {
+    if (out->getValType().value() == ValType::TensorView) {
+      return out->as<TensorView>();
+    }
+  }
+  return nullptr;
+}
+
 bool isScalarOp(const Expr* expr) {
   for (auto out : expr->outputs())
     if (!out->isScalar())
@@ -435,12 +447,12 @@ void ASSERT_EXPR(Statement* stmt) {
 
 Expr* asExpr(Statement* stmt) {
   ASSERT_EXPR(stmt);
-  return static_cast<Expr*>(stmt);
+  return stmt->as<Expr>();
 }
 
 TensorView* asTV(Val* val) {
   TORCH_INTERNAL_ASSERT(isTV(val));
-  return static_cast<TensorView*>(val);
+  return val->as<TensorView>();
 }
 
 bool isScope(const Expr* expr) {
@@ -448,26 +460,247 @@ bool isScope(const Expr* expr) {
       expr->getExprType() == ExprType::IfThenElse;
 }
 
-ForLoop* asForLoop(Statement* stmt) {
+kir::ForLoop* asForLoop(Statement* stmt) {
   Expr* expr = asExpr(stmt);
   TORCH_INTERNAL_ASSERT(expr->getExprType() == ExprType::ForLoop);
-  return static_cast<ForLoop*>(expr);
+  return expr->as<kir::ForLoop>();
 }
 
 const TensorView* asConstTV(const Val* val) {
   TORCH_INTERNAL_ASSERT(isTV(val));
-  return static_cast<const TensorView*>(val);
+  return val->as<TensorView>();
 }
 
 bool isUnrolledFor(const Expr* expr) {
   if (expr->getExprType() != ExprType::ForLoop) {
     return false;
   }
-  return static_cast<const ForLoop*>(expr)->iter_domain()->parallel_method() ==
+  return expr->as<kir::ForLoop>()->iter_domain()->getParallelType() ==
       ParallelType::Unroll;
 }
 
+const std::unordered_map<ParallelType, int> ParallelTypeBitmap::pt_to_offset_{
+    {ParallelType::BIDx, 0},
+    {ParallelType::BIDy, 1},
+    {ParallelType::BIDz, 2},
+    {ParallelType::TIDx, 3},
+    {ParallelType::TIDy, 4},
+    {ParallelType::TIDz, 5}};
+
+const std::unordered_map<int, ParallelType> ParallelTypeBitmap::offset_to_pt_ =
+    {{0, ParallelType::BIDx},
+     {1, ParallelType::BIDy},
+     {2, ParallelType::BIDz},
+     {3, ParallelType::TIDx},
+     {4, ParallelType::TIDy},
+     {5, ParallelType::TIDz}};
+
+bool ParallelTypeBitmap::get(ParallelType pt) const {
+  if (pt_to_offset_.find(pt) == pt_to_offset_.end()) {
+    TORCH_INTERNAL_ASSERT(false, "Could not recognize parallel type.");
+  }
+  return bitset_[pt_to_offset_.at(pt)];
+}
+
+bool ParallelTypeBitmap::set(ParallelType pt, bool new_val) {
+  if (pt_to_offset_.find(pt) == pt_to_offset_.end()) {
+    TORCH_INTERNAL_ASSERT(false, "Could not recognize parallel type.");
+  }
+  bool old_val = bitset_[pt_to_offset_.at(pt)];
+  bitset_[pt_to_offset_.at(pt)] = new_val;
+  return old_val;
+}
+
+ParallelTypeBitmap ParallelTypeBitmap::operator&=(
+    const ParallelTypeBitmap& other) {
+  bitset_ &= other.bitset_;
+  return *this;
+}
+
+ParallelTypeBitmap ParallelTypeBitmap::operator|=(
+    const ParallelTypeBitmap& other) {
+  bitset_ |= other.bitset_;
+  return *this;
+}
+
+ParallelTypeBitmap ParallelTypeBitmap::operator^=(
+    const ParallelTypeBitmap& other) {
+  bitset_ ^= other.bitset_;
+  return *this;
+}
+
+ParallelTypeBitmap ParallelTypeBitmap::operator~() const {
+  return ParallelTypeBitmap(~bitset_);
+}
+
+bool ParallelTypeBitmap::none() const {
+  return bitset_.none();
+}
+
+bool ParallelTypeBitmap::any() const {
+  return bitset_.any();
+}
+
+bool ParallelTypeBitmap::all() const {
+  return bitset_.all();
+}
+
+bool ParallelTypeBitmap::operator[](size_t pos) const {
+  TORCH_INTERNAL_ASSERT(
+      pos < num_p_type, "Invalid index to ParallelTypeBitset: ", pos);
+  return bitset_[pos];
+}
+
+std::map<ParallelType, bool> ParallelTypeBitmap::getMap() const {
+  std::map<ParallelType, bool> map;
+  for (const auto& pt_offset : pt_to_offset_) {
+    map.emplace(std::make_pair(pt_offset.first, bitset_[pt_offset.second]));
+  }
+  return map;
+}
+
+ParallelTypeBitmap operator&(
+    const ParallelTypeBitmap& lhs,
+    const ParallelTypeBitmap& rhs) {
+  auto x = lhs;
+  x &= rhs;
+  return x;
+}
+
+ParallelTypeBitmap operator|(
+    const ParallelTypeBitmap& lhs,
+    const ParallelTypeBitmap& rhs) {
+  auto x = lhs;
+  x |= rhs;
+  return x;
+}
+
+ParallelTypeBitmap operator^(
+    const ParallelTypeBitmap& lhs,
+    const ParallelTypeBitmap& rhs) {
+  auto x = lhs;
+  x ^= rhs;
+  return x;
+}
+
+ParallelTypeBitmap getParallelBroadcastDomains(
+    const Val* bop_out,
+    const ThreadPredicateMap& preds) {
+  if (bop_out->getValType().value() == ValType::TensorIndex) {
+    bop_out = bop_out->as<kir::TensorIndex>()->view()->fuserTv();
+  }
+  TORCH_INTERNAL_ASSERT(
+      bop_out->getValType().value() == ValType::TensorView,
+      "Out is not tensor view");
+  auto out_tv = bop_out->as<TensorView>();
+  // If no pred is found for out_tv, no predicate is necessary
+  if (preds.find(out_tv) == preds.end()) {
+    return ParallelTypeBitmap();
+  }
+  const ParallelTypeBitmap& out_pred = preds.at(out_tv).first;
+
+  ParallelTypeBitmap parallel_broadcast;
+  const auto& iter_domains = out_tv->domain()->domain();
+  for (auto id : iter_domains) {
+    if (id->isBroadcast() && id->isThread()) {
+      parallel_broadcast.set(id->getParallelType(), true);
+    }
+  }
+
+  return parallel_broadcast & out_pred;
+}
+
 } // namespace ir_utils
+
+namespace loop_utils {
+
+std::pair<kir::ForLoop*, int64_t> getAllocPoint(
+    TensorView* tv,
+    const std::vector<kir::ForLoop*>& loops) {
+  // If in global memory, it can be all the way outside the loops.
+  if (tv->getMemoryType() == MemoryType::Global) {
+    return {nullptr, 0};
+  }
+
+  // Figure out where we want to place alloc/reduction initialization. We want
+  // outside an unroll loop, or inside our computeAt point.
+  kir::ForLoop* alloc_loop = nullptr;
+
+  auto loops_it = loops.begin();
+
+  // Look at each axis individually in out's domain
+  for (int64_t tv_i = 0; tv_i < (int64_t)tv->getThisComputeAtAxis(); tv_i++) {
+    // Grab the axis ID
+
+    auto ca_id = tv->getComputeAtAxis(tv_i).first;
+    auto kir_ca_id = kir::lowerValue(ca_id)->as<kir::IterDomain>();
+
+    loops_it =
+        std::find_if(loops_it, loops.end(), [&kir_ca_id](const auto& loop) {
+          return kir_ca_id == loop->iter_domain() ||
+              loop->iter_domain()->getParallelType() == ParallelType::Unroll;
+        });
+
+    if (loops_it == loops.end()) {
+      for (auto loop : loops) {
+        std::cout << loop->iter_domain() << "  ";
+      }
+      std::cout << std::endl;
+    }
+    TORCH_INTERNAL_ASSERT(
+        loops_it != loops.end(),
+        "Could not find all required axes for indexing when trying to index into ",
+        tv);
+
+    if (kir_ca_id->getParallelType() == ParallelType::Unroll) {
+      return {alloc_loop, tv_i};
+    }
+
+    alloc_loop = *loops_it;
+    ++loops_it;
+  }
+
+  return {alloc_loop, (int64_t)tv->getThisComputeAtAxis()};
+}
+
+std::unordered_map<IterDomain*, IterDomain*> p2cRootMap(
+    const std::vector<Expr*>& exprs) {
+  std::unordered_map<IterDomain*, IterDomain*> p2c_root_map;
+
+  for (auto expr : exprs) {
+    auto out_tv = ir_utils::getTVOutput(expr);
+    for (auto inp : expr->inputs()) {
+      if (inp->getValType().value() != ValType::TensorView) {
+        continue;
+      }
+
+      auto root_p2c = TensorDomain::mapRootPtoC(
+          inp->as<TensorView>()->domain(), out_tv->domain());
+      for (auto entry : root_p2c) {
+        auto p_id = entry.first;
+        auto c_id = entry.second;
+        // Careful we don't allow circular references
+        if (p_id != c_id) {
+          p2c_root_map[p_id] = c_id;
+        }
+      }
+    }
+  }
+
+  return p2c_root_map;
+}
+
+IterDomain* getTermIDInMap(
+    IterDomain* root_id,
+    std::unordered_map<IterDomain*, IterDomain*> p2c_root_map) {
+  auto entry = root_id;
+  while (p2c_root_map.find(entry) != p2c_root_map.end()) {
+    entry = p2c_root_map.at(entry);
+  }
+  return entry;
+}
+
+} // namespace loop_utils
 
 } // namespace fuser
 } // namespace jit

commit c5e3550521c350d021f1b1bf7bb91a1fe21a3027

Fix `codegen/cuda` gcc-5.4 compilation issues
Most of the fixes is the same old enum-is-not-hasheable error
In manager.cpp use std::unordered_map::emplace rather than `insert` to avoid error triggered by missed copy elision
This regression was introduced by https://github.com/pytorch/pytorch/pull/43129

torch/csrc/jit/codegen/cuda/lower_thread_predicate.h
@@ -22,8 +22,10 @@ namespace fuser {
  */
 class TORCH_CUDA_API ThreadPredicateMap {
  public:
-  using SourceMapType =
-      std::unordered_map<ParallelType, std::unordered_set<const TensorView*>>;
+  using SourceMapType = std::unordered_map<
+      ParallelType,
+      std::unordered_set<const TensorView*>,
+      TypeHash>;
   using MapType = std::unordered_map<
       const TensorView*,
       std::pair<ir_utils::ParallelTypeBitmap, SourceMapType>>;

torch/csrc/jit/codegen/cuda/lower_utils.cpp
@@ -479,13 +479,13 @@ bool isUnrolledFor(const Expr* expr) {
       ParallelType::Unroll;
 }
 
-const std::unordered_map<ParallelType, int> ParallelTypeBitmap::pt_to_offset_{
-    {ParallelType::BIDx, 0},
-    {ParallelType::BIDy, 1},
-    {ParallelType::BIDz, 2},
-    {ParallelType::TIDx, 3},
-    {ParallelType::TIDy, 4},
-    {ParallelType::TIDz, 5}};
+const std::unordered_map<ParallelType, int, TypeHash>
+    ParallelTypeBitmap::pt_to_offset_{{ParallelType::BIDx, 0},
+                                      {ParallelType::BIDy, 1},
+                                      {ParallelType::BIDz, 2},
+                                      {ParallelType::TIDx, 3},
+                                      {ParallelType::TIDy, 4},
+                                      {ParallelType::TIDz, 5}};
 
 const std::unordered_map<int, ParallelType> ParallelTypeBitmap::offset_to_pt_ =
     {{0, ParallelType::BIDx},
@@ -554,7 +554,7 @@ bool ParallelTypeBitmap::operator[](size_t pos) const {
 std::map<ParallelType, bool> ParallelTypeBitmap::getMap() const {
   std::map<ParallelType, bool> map;
   for (const auto& pt_offset : pt_to_offset_) {
-    map.emplace(std::make_pair(pt_offset.first, bitset_[pt_offset.second]));
+    map.emplace(pt_offset.first, bitset_[pt_offset.second]);
   }
   return map;
 }

torch/csrc/jit/codegen/cuda/lower_utils.h
@@ -125,7 +125,7 @@ class ParallelTypeBitmap {
  private:
   ParallelTypeBitmap(const std::bitset<num_p_type>& bs) : bitset_(bs) {}
   std::bitset<num_p_type> bitset_;
-  const static std::unordered_map<ParallelType, int> pt_to_offset_;
+  const static std::unordered_map<ParallelType, int, TypeHash> pt_to_offset_;
   const static std::unordered_map<int, ParallelType> offset_to_pt_;
 };
 

torch/csrc/jit/codegen/cuda/manager.cpp
@@ -91,7 +91,7 @@ class CudaFusionManager {
       int32_t kernel_id = getNextUniqueID();
       graph_cache_ids_[repr] = kernel_id;
       TORCH_CHECK(
-          graph_cache_.insert({kernel_id, std::make_unique<GraphCache>(graph)})
+          graph_cache_.emplace(kernel_id, std::make_unique<GraphCache>(graph))
               .second);
     }
     return graph_cache_ids_[repr];