7a53c864a7cb48cedbbf1fbf91bd6b27ec095a6c
Eli Uriegas <eliuriegas@fb.com>
2020-08-19 10:46:51 -0700

    .circleci: Remove manual docker installation
    Docker added native support for GPUs with the release of 19.03 and
    CircleCI's infrastructure is all on Docker 19.03 as of now.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .circleci/scripts/setup_ci_environment.sh
+++ .circleci/scripts/setup_ci_environment.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -ex -o pipefail
 
-# Set up NVIDIA docker repo
-if [[ "${BUILD_ENVIRONMENT}" == *cu* ]]; then
-  curl -s -L --retry 3 https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-  echo "deb https://nvidia.github.io/libnvidia-container/ubuntu16.04/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-  echo "deb https://nvidia.github.io/nvidia-container-runtime/ubuntu16.04/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-  echo "deb https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-else
-  # Explicitly remove nvidia docker apt repositories if not building for cuda
-  sudo rm -rf /etc/apt/sources.list.d/nvidia-docker.list
-fi
-
 # Remove unnecessary sources
 sudo rm -f /etc/apt/sources.list.d/google-chrome.list
 sudo rm -f /etc/apt/heroku.list
@@ -27,42 +16,9 @@ retry () {
 # This is better than retrying the whole apt-get command
 echo "APT::Acquire::Retries \"3\";" | sudo tee /etc/apt/apt.conf.d/80-retries
 
-sudo apt-get -y update
-sudo apt-get -y remove linux-image-generic linux-headers-generic linux-generic docker-ce
-# WARNING: Docker version is hardcoded here; you must update the
-# version number below for docker-ce and nvidia-docker2 to get newer
-# versions of Docker.  We hardcode these numbers because we kept
-# getting broken CI when Docker would update their docker version,
-# and nvidia-docker2 would be out of date for a day until they
-# released a newer version of their package.
-#
-# How to figure out what the correct versions of these packages are?
-# My preferred method is to start a Docker instance of the correct
-# Ubuntu version (e.g., docker run -it ubuntu:16.04) and then ask
-# apt what the packages you need are.  Note that the CircleCI image
-# comes with Docker.
-#
-# Using 'retry' here as belt-and-suspenders even though we are
-# presumably retrying at the single-package level via the
-# apt.conf.d/80-retries technique.
-if [[ "${BUILD_ENVIRONMENT}" == *cu* ]]; then
-  retry sudo apt-get -y install \
-    linux-headers-$(uname -r) \
-    linux-image-generic \
-    moreutils \
-    docker-ce=5:18.09.4~3-0~ubuntu-xenial \
-    nvidia-container-runtime=2.0.0+docker18.09.4-1 \
-    nvidia-docker2=2.0.3+docker18.09.4-1 \
-    expect-dev
-else
-  retry sudo apt-get -y install \
-    moreutils \
-    docker-ce=5:18.09.4~3-0~ubuntu-xenial \
-    expect-dev
-fi
-
-sudo pkill -SIGHUP dockerd
-
+retry sudo apt-get -y install \
+  moreutils \
+  expect-dev
 
 retry sudo pip -q install awscli==1.16.35
 
