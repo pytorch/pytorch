pytorch/pytorch

commit 66fc237c9a691359d445974627c72768ee2c807f

Autocast support for cudnn RNNs (#42385)
Summary:
Should close https://github.com/pytorch/pytorch/issues/36428.

The cudnn RNN API expects weights to occupy a flat buffer in memory with a particular layout.  This PR implements a "speed of light" fix:  [`_cudnn_rnn_cast_reflatten`](https://github.com/pytorch/pytorch/pull/42385/files#diff-9ef93b6a4fb5a06a37c562b83737ac6aR327) (the autocast wrapper assigned to `_cudnn_rnn`) copies weights to the right slices of a flat FP16 buffer with a single read/write per weight (as opposed to casting them to FP16 individually then reflattening the individual FP16 weights, which would require 2 read/writes per weight).

It isn't pretty but IMO it doesn't make rnn bindings much more tortuous than they already are.

The [test](https://github.com/pytorch/pytorch/pull/42385/files#diff-e68a7bc6ba14f212e5e7eb3727394b40R2683) tries a forward under autocast and a backward for the full cross product of RNN options and input/weight/hidden dtypes.  As for all FP16list autocast tests, forward output and backward grads are checked against a control where inputs (including RNN module weights in this case) are precasted to FP16 on the python side.

Not sure who to ask for review, tagging ezyang and ngimel because Ed wrote this file (almost 2 years ago) and Natalia did the most recent major [surgery](https://github.com/pytorch/pytorch/pull/12600).

Side quests discovered:
- Should we update [persistent RNN heuristics](https://github.com/pytorch/pytorch/blob/dbdd28207c5cf6c4a35ceb1de0811c4812e8882c/aten/src/ATen/native/cudnn/RNN.cpp#L584) to include compute capability 8.0?  Could be another PR but seems easy enough to include.
- Many (maybe all?!) the raw cudnn API calls in [RNN.cpp](https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/cudnn/RNN.cpp) are deprecated in cudnn 8.  I don't mind taking the AI to update them since my mental cache is full of rnn stuff, but that would be a substantial separate PR.

aten/src/ATen/native/cudnn/RNN.cpp
@@ -59,10 +59,7 @@ Tensor _cudnn_init_dropout_state(double dropout, bool train, int64_t dropout_see
 
 #else // AT_CUDNN_ENABLED()
 
-#include <ATen/cudnn/cudnn-wrapper.h>
-#include <ATen/cudnn/Descriptors.h>
-#include <ATen/cudnn/Types.h>
-#include <ATen/cudnn/Utils.h>
+#include <ATen/native/cudnn/RNNUtils.h>
 
 namespace at { namespace native {
 
@@ -406,7 +403,8 @@ namespace {
       const RNNDescriptor& rnn_desc,
       const TensorDescriptor& x_desc,
       const FilterDescriptor& w_desc,
-      const Tensor& weight_buf
+      const Tensor& weight_buf,
+      bool include_bias=true
   ) {
     auto cudnn_methods = { cudnnGetRNNLinLayerMatrixParams, cudnnGetRNNLinLayerBiasParams };
     std::vector<Tensor> params;
@@ -462,13 +460,19 @@ namespace {
           // might as well merge the CUDNN ones into a single tensor as well
           int mat_numel = *filter_dim_a.prod(at::ScalarType::Int).data_ptr<int>();
           if (linear_id == 0 || linear_id == num_linear_layers / 2) {
-            std::initializer_list<int64_t> size = {
-              mat_numel * num_linear_layers / 2, 1};
-            // Generate a new parameter tensor which is a view into the
-            // weight_buf.
-            Tensor param = at::empty({0}, weight_buf.options()).set_(weight_buf.storage(), offset, size);
-            params.emplace_back(std::move(param));
-            layer_params_count++;
+            // We could also exclude bias params by restricting cudnn_methods to just { cudnnGetRNNLinLayerMatrixParams }
+            // at the very top.  However, to do so would throw off the cur_offset account, which is currently a strict
+            // and informative check that all params are laid out the way we think they are.  If include_bias is false,
+            // I'd rather keep full cur_offset checks rather than save some CPU overhead by skipping the cudnn_method =
+            // cudnnGetRNNLinLayerBiasParams iteration.
+            if (include_bias || cudnn_method != cudnnGetRNNLinLayerBiasParams) {
+              // Generate a new parameter tensor which is a view into the weight_buf.
+              std::initializer_list<int64_t> size = {
+                mat_numel * num_linear_layers / 2, 1};
+              Tensor param = at::empty({0}, weight_buf.options()).set_(weight_buf.storage(), offset, size);
+              params.emplace_back(std::move(param));
+              layer_params_count++;
+            }
           } else {
             AT_ASSERTM(cur_offset == offset, "cur_offset = ", cur_offset, "; offset = ", offset);
           }
@@ -526,7 +530,8 @@ namespace {
     return data_ptrs;
   }
 
-  void _viewOrCopyParams(MatrixRef<Tensor> params_from, MatrixRef<Tensor> params_to, bool copy) {
+  void _viewOrCopyParams(MatrixRef<Tensor> params_from, MatrixRef<Tensor> params_to,
+                         bool copy, bool allow_type_change=false) {
     AT_ASSERTM(params_from.size(0) == params_to.size(0), "number of layers mismatch");
     for (size_t i = 0; i < params_from.size(0); i++) {
       auto layer_params_from = params_from[i];
@@ -538,7 +543,12 @@ namespace {
            a != layer_params_from.end() && b != layer_params_to.end();
            ++a, ++b) {
         auto param_from = *a, param_to = *b;
-        AT_ASSERTM(param_from.type() == param_to.type(), "parameter types mismatch");
+        // if copying, allow_type_change may be true or false.
+        // if viewing, allow_type_change must be false.
+        TORCH_INTERNAL_ASSERT(copy || !allow_type_change,
+                              "if viewing, type change is not allowed.");
+        TORCH_INTERNAL_ASSERT(allow_type_change || (param_from.scalar_type() == param_to.scalar_type()),
+                              "parameter types mismatch");
         if (copy) {
             param_to.copy_(param_from.view_as(param_to));
         } else {
@@ -607,6 +617,80 @@ namespace {
 
 } // anonymous namespace
 
+// Utilities exposed in RNNUtils.h
+namespace cudnn_rnn {
+
+  TORCH_CUDA_API std::tuple<Tensor, std::vector<Tensor>> copy_weights_to_flat_buf_views(
+      TensorList weight_arr,
+      int64_t weight_stride0,
+      int64_t input_size,
+      int64_t mode,
+      int64_t hidden_size,
+      int64_t num_layers,
+      bool batch_first,
+      bool bidirectional,
+      const cudnnDataType_t flat_buf_datatype,
+      const TensorOptions& flat_buf_options,
+      bool set_orig_weights_to_flat_buf,
+      bool allow_type_change/*=false*/,
+      bool include_bias/*=true*/) {
+    // flat_buf_datatype is accepted as a separate argument (rather than extracted from flat_buf_options)
+    // because to extract flat_buf_datatype from flat_buf_options, we'd need to say
+    // auto flat_buf_datatype = getCudnnDataTypeFromScalarType(typeMetaToScalarType(options.dtype()));
+    // typeMetaToScalarType is a surprisingly nontrivial function.  We should avoid it if we can.
+
+    TORCH_CHECK(weight_arr.size() > 0,
+                "copy_weights_to_flat_buf_views: cannot flatten empty weight list");
+
+    RNNDescriptorParams rnn;
+    rnn.set(mode, hidden_size, num_layers, bidirectional, promote_rnn_math_type(flat_buf_datatype), flat_buf_datatype);
+
+    auto handle = getCudnnHandle();
+    RNNDescriptor rnn_desc = rnn.descriptor(handle);
+
+    TensorGeometry x_geom({1, input_size});
+    TensorDescriptor x_desc;
+    // Why do we pad to 5 dims here (and elsewhere)?
+    // https://docs.nvidia.com/deeplearning/sdk/cudnn-api/index.html#cudnnRNNForwardTraining
+    // expects descriptors padded to 3 dimensions.
+    x_desc.set(flat_buf_datatype, x_geom.sizes(), x_geom.strides(), 5);
+
+    auto num_weights = get_num_weights(handle, rnn_desc, x_desc, flat_buf_datatype);
+    auto weight_buf = at::zeros(num_weights, flat_buf_options);
+
+    FilterDescriptor w_desc;
+    w_desc.set(weight_buf, 3);
+
+    // Slice off views into weight_buf
+    std::vector<Tensor> params_arr;
+    size_t params_stride0;
+    std::tie(params_arr, params_stride0) = get_parameters(handle, rnn, rnn_desc, x_desc, w_desc, weight_buf, include_bias);
+
+    MatrixRef<Tensor> weight{weight_arr, static_cast<size_t>(weight_stride0)},
+                      params{params_arr, params_stride0};
+
+    // Copy weights
+    _viewOrCopyParams(weight, params, /*copy=*/true, allow_type_change);
+
+    if (set_orig_weights_to_flat_buf) {
+      // Update the storage
+      for (size_t i = 0; i < weight.size(0); i++) {
+        for (auto orig_param_it = weight[i].begin(), new_param_it = params[i].begin();
+             orig_param_it != weight[i].end() && new_param_it != params[i].end();
+             orig_param_it++, new_param_it++) {
+          auto orig_param = *orig_param_it, new_param = *new_param_it;
+          orig_param.set_(new_param.view_as(orig_param));
+        }
+      }
+    }
+
+    return {weight_buf, params_arr};
+  }
+
+} // namespace cudnn_rnn
+
+using namespace cudnn_rnn;
+
 // NB: does inplace update into TensorList
 // It would be a relatively simple matter to refactor this into multiple
 // functions, only one of which does an inplace update, but we leave this
@@ -618,52 +702,25 @@ Tensor _cudnn_rnn_flatten_weight(
     int64_t fn_num_layers, bool batch_first,
     bool fn_bidirectional
     ) {
-
-  TORCH_CHECK(weight_arr.size() > 0,
-           "_cudnn_rnn_flatten_weight_: cannot flatten empty weight list");
-
-  auto any_param = weight_arr[0];
-  auto datatype = getCudnnDataType(any_param);
-
-  RNNDescriptorParams rnn;
-  rnn.set(fn_mode, fn_hidden_size, fn_num_layers, fn_bidirectional, promote_rnn_math_type(datatype), datatype);
-
-  auto handle = getCudnnHandle();
-  RNNDescriptor rnn_desc = rnn.descriptor(handle);
-
-  TensorGeometry x_geom({1, input_size});
-  TensorDescriptor x_desc;
-  x_desc.set(getCudnnDataType(any_param), x_geom.sizes(), x_geom.strides(), 5);
-
-  auto num_weights = get_num_weights(handle, rnn_desc, x_desc, datatype);
-  auto weight_buf = at::zeros(num_weights, any_param.options());
-
-  FilterDescriptor w_desc;
-  w_desc.set(weight_buf, 3);
-
-  // Slice off views into weight_buf
-  std::vector<Tensor> params_arr;
-  size_t params_stride0;
-  std::tie(params_arr, params_stride0) = get_parameters(handle, rnn, rnn_desc, x_desc, w_desc, weight_buf);
-
-  MatrixRef<Tensor> weight{weight_arr, static_cast<size_t>(weight_stride0)},
-                    params{params_arr, params_stride0};
-
-  // Copy weights
-  _copyParams(weight, params);
-
-  // Update the storage
-  for (size_t i = 0; i < weight.size(0); i++) {
-    for (auto orig_param_it = weight[i].begin(), new_param_it = params[i].begin();
-         orig_param_it != weight[i].end() && new_param_it != params[i].end();
-         orig_param_it++, new_param_it++) {
-      auto orig_param = *orig_param_it, new_param = *new_param_it;
-      orig_param.set_(new_param.view_as(orig_param));
-    }
-  }
-
-  return weight_buf;
+  // returns flat weight_buf
+  return std::get<0>(copy_weights_to_flat_buf_views(
+      weight_arr,
+      weight_stride0,
+      input_size,
+      fn_mode,
+      fn_hidden_size,
+      fn_num_layers,
+      batch_first,
+      fn_bidirectional,
+      /*flat_buf_datatype=*/getCudnnDataType(weight_arr[0]),
+      /*flat_buf_options=*/weight_arr[0].options(),
+      /*set_orig_weights_to_flat_buf=*/true));
 }
+
+const char * WEIGHT_FORMAT_WARN = "RNN module weights are not part of single contiguous "
+                                  "chunk of memory. This means they need to be compacted "
+                                  "at every call, possibly greatly increasing memory usage. "
+                                  "To compact weights again call flatten_parameters().";
 
 // NB: when fn_batch_sizes is empty, that means no batch sizes was specified
 std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor> _cudnn_rnn(
@@ -676,9 +733,12 @@ std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor> _cudnn_rnn(
     const Tensor& fn_dropout_state
     ) {
 
-  check_device(input_r, weight, {hx, cx});
+  check_attributes(input_r, weight, {hx, cx}, /*check_dtype=*/true);
   auto input = input_r;
   auto weight_buf = weight_buf_r;
+  if (!weight_buf.defined()) {
+    TORCH_WARN(WEIGHT_FORMAT_WARN);
+  }
   if (fn_dropout_state.defined()) {
       auto input_arg = TensorArg(input, "input", 1);
       auto dropout_state_arg = TensorArg(fn_dropout_state, "dropout_states", 15);
@@ -1172,9 +1232,11 @@ DropoutState& get_dropout_state(double dropout_p, bool train, TensorOptions opti
 Tensor try_get_weight_buf(
       const Tensor& input, TensorList parameters, bool has_biases,
       cudnnRNNMode_t mode, int64_t hidden_size, int64_t num_layers, bool bidirectional) {
+
   // Prepare all relevant descriptors
   auto handle = getCudnnHandle();
-  auto datatype = getCudnnDataType(input);
+  auto & any_param = parameters.at(0);
+  auto datatype = getCudnnDataType(any_param);
 
   RNNDescriptorParams rnn;
   rnn.set(mode, hidden_size, num_layers, bidirectional, promote_rnn_math_type(datatype), datatype);
@@ -1182,12 +1244,14 @@ Tensor try_get_weight_buf(
 
   TensorGeometry x_geom ({1, input.size(-1)});
   TensorDescriptor x_desc;
+  // datatype for x_desc comes from any_param, not input.
+  // try_get_weight_buf's job is to check "is the weight buffer correctly laid out
+  // for us to run it with input of the same datatype?"
   x_desc.set(datatype, x_geom.sizes(), x_geom.strides(), 5);
 
   auto num_params = get_num_weights(handle, rnn_desc, x_desc, datatype);
 
   // Try to get parameter storage
-  auto & any_param = parameters.at(0);
   auto param_storage = any_param.storage();
   auto weight_buf = at::empty({0}, any_param.options()).set_(param_storage);
   if (weight_buf.size(0) < num_params) {
@@ -1214,11 +1278,6 @@ Tensor try_get_weight_buf(
   return weight_buf;
 }
 
-const char * WEIGHT_FORMAT_WARN = "RNN module weights are not part of single contiguous "
-                                  "chunk of memory. This means they need to be compacted "
-                                  "at every call, possibly greatly increasing memory usage. "
-                                  "To compact weights again call flatten_parameters().";
-
 template<typename hidden_type>
 std::pair<Tensor, hidden_type> _cudnn_impl(
       const Tensor& input, const Tensor& _batch_sizes, const hidden_type& hidden,
@@ -1228,11 +1287,11 @@ std::pair<Tensor, hidden_type> _cudnn_impl(
   std::tie(hx, cx) = unpack_hidden(hidden);
   int64_t hidden_size = hx.size(2);
 
+  // TODO:  try_get_weight_buf returns a Tensor, but _cudnn_rnn below takes a c10::optional<Tensor>
+  // in weight_buf's slot.  Do we want try_get_weight_buf to return a c10::optional<Tensor>
+  // instead of a defined or undefined Tensor?
   auto weight_buf = try_get_weight_buf(
       input, params, has_biases, mode, hidden_size, num_layers, bidirectional);
-  if (!weight_buf.defined()) {
-    TORCH_WARN(WEIGHT_FORMAT_WARN);
-  }
 
   TORCH_CHECK(_batch_sizes.dim() == 1, "batch_sizes tensor should be 1D");
   IntArrayRef batch_sizes { _batch_sizes.data_ptr<int64_t>(), static_cast<size_t>(_batch_sizes.size(0)) };
@@ -1260,9 +1319,6 @@ std::pair<Tensor, hidden_type> _cudnn_impl(
 
   auto weight_buf = try_get_weight_buf(
       input, params, has_biases, mode, hidden_size, num_layers, bidirectional);
-  if (!weight_buf.defined()) {
-    TORCH_WARN(WEIGHT_FORMAT_WARN);
-  }
 
   auto & dropout_state = get_dropout_state(dropout_p, train, input.options());
   std::unique_lock<DropoutState> lock { dropout_state };

aten/src/ATen/native/RNN.cpp
@@ -1182,7 +1182,7 @@ bool _use_cudnn_rnn_flatten_weight() {
           batch_first);                                                     \
       return std::make_tuple(std::move(output), std::move(hy));             \
     }                                                                       \
-    check_device(_input, _params, hx);                                      \
+    check_attributes(_input, _params, hx);                                  \
     auto input = batch_first ? _input.transpose(0, 1) : _input;             \
     auto params = gather_params(_params, has_biases);                       \
     auto results =                                                          \
@@ -1410,7 +1410,7 @@ std::tuple<Tensor, Tensor, Tensor> lstm(
               num_layers, dropout_p, train, bidirectional, batch_first);
     return std::make_tuple(std::move(output), std::move(hy), std::move(cy));
   }
-  check_device(_input, _params, hx);
+  check_attributes(_input, _params, hx);
   auto input = batch_first ? _input.transpose(0, 1) : _input;
   auto params = gather_params(_params, has_biases);
   auto results = _lstm_impl<FullLayer, FullBidirectionalLayer>(

BUILD.bazel
@@ -340,6 +340,7 @@ filegroup(
         "aten/src/ATen/cuda/CublasHandlePool.cpp",
         "aten/src/ATen/cuda/PinnedMemoryAllocator.cpp",
         "aten/src/ATen/cuda/detail/CUDAHooks.cpp",
+        "aten/src/ATen/cudnn/AutocastRNN.cpp",
         "aten/src/ATen/cudnn/Descriptors.cpp",
         "aten/src/ATen/cudnn/Handle.cpp",
         "aten/src/ATen/cudnn/Types.cpp",

commit 2c7097ebb7715f53b53f67cb3c1c0768c9edc1fc

In copy_weights_to_flat_buf_views() explicitly construct tuple
In some versions of GCC, tuple constructor from initializer list  is marked as explicit, which results in the following compilation error:
```
/var/lib/jenkins/workspace/aten/src/ATen/native/cudnn/RNN.cpp: In function 'std::tuple<at::Tensor, std::vector<at::Tensor, std::allocator<at::Tensor> > > at::native::cudnn_rnn::copy_weights_to_flat_buf_views(at::TensorList, int64_t, int64_t, int64_t, int64_t, int64_t, bool, bool, cudnnDataType_t, const c10::TensorOptions&, bool, bool, bool)':
/var/lib/jenkins/workspace/aten/src/ATen/native/cudnn/RNN.cpp:687:35: error: converting to 'std::tuple<at::Tensor, std::vector<at::Tensor, std::allocator<at::Tensor> > >' from initializer list would use explicit constructor 'constexpr std::tuple<_T1, _T2>::tuple(_U1&&, _U2&&) [with _U1 = at::Tensor&; _U2 = std::vector<at::Tensor>&; <template-parameter-2-3> = void; _T1 = at::Tensor; _T2 = std::vector<at::Tensor>]'
     return {weight_buf, params_arr};
```
This regression was introduced by https://github.com/pytorch/pytorch/pull/42385

aten/src/ATen/native/cudnn/RNN.cpp
@@ -684,7 +684,7 @@ namespace cudnn_rnn {
       }
     }
 
-    return {weight_buf, params_arr};
+    return std::make_tuple(weight_buf, params_arr);
   }
 
 } // namespace cudnn_rnn