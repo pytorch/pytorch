16ebd1148dc09d9dd70881f0012219633090373b
Michael Suo <suo@fb.com>
2020-08-20 13:17:04 -0700

    Update on "[jit] Better match behavior of loaded ScriptModules vs. freshly created ones"
    IR emitter uses `ModuleValue` to represent ScriptModules and emit IR for
    attribute access, submodule access, etc.
    
    `ModuleValue` relies on two pieces of information, the JIT type of the
    module, and the `ConcreteModuleType`, which encapsulates Python-only
    information about the module.
    
    ScriptModules loaded from a package used to create a dummy
    ConcreteModuleType without any info in it. This led to divergences in
    behavior during compilation.
    
    This PR makes the two ways of constructing a ConcreteModuleType equivalent,
    modulo any py-only information (which, by definition, is never present in
    packaged files anyway).
    
    Differential Revision: [D23228738](https://our.internmc.facebook.com/intern/diff/D23228738)
    
    [ghstack-poisoned]

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -1522,7 +1522,9 @@ void initJitScriptBindings(PyObject* module) {
           "add_constant",
           [](ConcreteModuleTypeBuilder& self,
              std::string name,
-             py::object value) { self.addConstant(name, value); })
+             py::object value) {
+            self.addConstant(std::move(name), std::move(value));
+          })
       .def("add_attribute", &ConcreteModuleTypeBuilder::addAttribute)
       .def(
           "add_function_attribute",
