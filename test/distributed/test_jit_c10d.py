3366e237ed6ad28b2bcf7294474d7078ae89d2d1
Wanchao Liang <wanchaol@users.noreply.github.com>
2020-08-20 14:07:53 -0700

    Update on "[WIP][c10d][jit] torchbind ProcessGroup"
    Differential Revision: [D23228682](https://our.internmc.facebook.com/intern/diff/D23228682)
    
    [ghstack-poisoned]

--- test/distributed/test_jit_c10d.py
+++ test/distributed/test_jit_c10d.py
@@ -1,39 +1,15 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import copy
-import math
-import os
-import random
-import signal
-import sys
-import tempfile
-import threading
-import time
-import traceback
+
 import unittest
-from datetime import timedelta
+import traceback
 from sys import platform
-from contextlib import contextmanager
-
-from itertools import groupby, product
-from functools import reduce
-import operator
-
 import torch
-import torch.testing._internal.common_utils as common
-from torch import nn
-import torch.nn.functional as F
 import torch.distributed as c10d
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel
 
 from torch.testing._internal.common_distributed import MultiProcessTestCase, \
-    requires_gloo, requires_nccl, requires_nccl_version, \
-    skip_if_not_multigpu, skip_if_lt_x_gpu, get_timeout, skip_if_rocm, \
-    simple_sparse_reduce_tests
-
-from torch.testing._internal.common_utils import TestCase, load_tests, run_tests, \
-    retry_on_connect_failures, ADDRESS_IN_USE, CONNECT_TIMEOUT, TEST_WITH_TSAN
+    requires_gloo
+from torch.testing._internal.common_utils import TEST_WITH_TSAN
 
 
 if not c10d.is_available():
