5ea62d4643fe2f2d42f60e840411b1a3eab4ee4d
Brian Hirsh <hirsheybar@fb.com>
2020-08-21 11:12:43 -0700

    Update on "PR feedback. fixed DLConverter test with no deleter to make it more realistic. Custom function that doesn't give the DLMTensor a deleter in the first place, rather than assigning and removing one."
    [ghstack-poisoned]

--- aten/src/ATen/test/dlconvertor_test.cpp
+++ aten/src/ATen/test/dlconvertor_test.cpp
@@ -62,11 +62,19 @@ TEST(TestDlconvertor, TestDlconvertorNullDeleter) {
   manual_seed(123);
 
   Tensor a = rand({3, 4});
-  DLManagedTensor* dlMTensor = toDLPackNoDeleter(a);
 
-  Tensor b = fromDLPack(dlMTensor);
+  // no deleter necessary since dlManagedTensor is initialized on the stack
+  DLManagedTensor dlMTensor;
+  dlMTensor.deleter = nullptr;
+  dlMTensor.dl_tensor.data = a.data_ptr();
+  dlMTensor.dl_tensor.ctx = getDLContext(a, 0);
+  dlMTensor.dl_tensor.ndim = a.dim();
+  dlMTensor.dl_tensor.dtype = getDLDataType(a);
+  dlMTensor.dl_tensor.shape = const_cast<int64_t*>(a.sizes().data());
+  dlMTensor.dl_tensor.strides = const_cast<int64_t*>(a.strides().data());
+  dlMTensor.dl_tensor.byte_offset = 0;
 
-  // fromDLPack doesn't clean up the DLMTensor manager context since the deleter wasn't set
+  Tensor b = fromDLPack(&dlMTensor);
+
   ASSERT_TRUE(a.equal(b));
-  EXPECT_NE(dlMTensor->manager_ctx, nullptr);
 }
