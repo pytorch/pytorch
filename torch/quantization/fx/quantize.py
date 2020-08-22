81cb53b6a11c3774250751bc40bddde68cb99426
Jerry Zhang <jerryzh@fb.com>
2020-08-21 23:04:42 -0700

    [quant][graphmode][fx] Add support for general shape ops (#43438)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43438
    
    Porting op tests from test_quantize_jit.py
    
    Test Plan:
    TestQuantizeFxOps
    
    Imported from OSS
    
    Reviewed By: raghuramank100
    
    Differential Revision: D23278583
    
    fbshipit-source-id: 34b73390d47c7ce60528444da77c4096432ea2cb

--- torch/quantization/fx/quantize.py
+++ torch/quantization/fx/quantize.py
@@ -455,7 +455,9 @@ class ELU(QuantizeHandler):
 @register_quant_pattern(torch.nn.AvgPool2d)
 @register_quant_pattern(torch.nn.Dropout)
 @register_quant_pattern(torch.nn.Hardtanh)
+@register_quant_pattern(torch.nn.MaxPool1d)
 @register_quant_pattern(torch.nn.MaxPool2d)
+@register_quant_pattern(torch.nn.MaxPool3d)
 @register_quant_pattern(torch.nn.ReLU)
 @register_quant_pattern(torch.nn.ReLU6)
 @register_quant_pattern(torch.nn.functional.adaptive_avg_pool2d)
@@ -463,22 +465,44 @@ class ELU(QuantizeHandler):
 @register_quant_pattern(torch.nn.functional.hardtanh)
 @register_quant_pattern(torch.nn.functional.hardtanh_)
 @register_quant_pattern(torch.nn.functional.max_pool2d)
+@register_quant_pattern(torch.nn.functional.relu)
 @register_quant_pattern(torch.nn.functional.relu6)
 @register_quant_pattern(torch._C._nn.avg_pool2d)
+@register_quant_pattern(torch.chunk)
 @register_quant_pattern(torch.clamp)
 @register_quant_pattern(torch.flatten)
 @register_quant_pattern(torch.transpose)
+@register_quant_pattern(torch.max)
 @register_quant_pattern(torch.mean)
+@register_quant_pattern(torch.min)
+@register_quant_pattern(torch.repeat_interleave)
+@register_quant_pattern(torch.sort)
+@register_quant_pattern(torch.squeeze)
+@register_quant_pattern(torch.stack)
 @register_quant_pattern(torch.unsqueeze)
 @register_quant_pattern(operator.getitem)
 @register_quant_pattern(operator.floordiv)
 @register_quant_pattern('chunk')
 @register_quant_pattern('clamp')
 @register_quant_pattern('contiguous')
+@register_quant_pattern('detach')
+@register_quant_pattern('detach_')
 @register_quant_pattern('mean')
+@register_quant_pattern('numel')
+@register_quant_pattern('permute')
+@register_quant_pattern('relu')
+@register_quant_pattern('relu_')
+@register_quant_pattern('repeat')
+@register_quant_pattern('repeat_interleave')
 @register_quant_pattern('reshape')
+@register_quant_pattern('resize_')
 @register_quant_pattern('shape')
 @register_quant_pattern('size')
+@register_quant_pattern('squeeze')
+@register_quant_pattern('squeeze_')
+@register_quant_pattern('unsqueeze')
+@register_quant_pattern('unsqueeze_')
+@register_quant_pattern('transpose')
 @register_quant_pattern('view')
 class CopyNode(QuantizeHandler):
     def convert(self, quantizer, node, load_arg, debug=False):
@@ -646,8 +670,14 @@ class Quantizer:
                         'call_function',
                         'call_method'], \
                         'CopyNode of type ' + node.op + ' is not handled'
+
+                    def is_observed(input_arg):
+                        if isinstance(input_arg, Node):
+                            return input_arg.name in observed
+                        elif isinstance(input_arg, list):
+                            return all(map(is_observed, input_arg))
                     # propagate observed property from input
-                    if node.args[0].name in observed:
+                    if is_observed(node.args[0]):
                         observed.add(node.name)
                 elif (isinstance(obj, Add) or isinstance(obj, Mul)) and not obj.all_nodes:
                     if node.args[0].name in observed:
@@ -745,13 +775,19 @@ class Quantizer:
             return load_arg_impl
 
         def is_quantized(node):
-            assert node.name in env or node.name in quant_env, 'Expecting node to be in the environment'
-            # there might be nodes appearing in both environemnts, but quant_env will take
-            # precedence
-            if node.name in quant_env:
-                return True
-            elif node.name in env:
-                return False
+            if isinstance(node, Node):
+                assert node.name in env or node.name in quant_env, 'Expecting node to be in the environment'
+                # there might be nodes appearing in both environemnts, but quant_env will take
+                # precedence
+                if node.name in quant_env:
+                    return True
+                elif node.name in env:
+                    return False
+            elif isinstance(node, list):
+                if all(map(is_quantized, node)):
+                    return True
+                else:
+                    raise Exception("partially quantized inputs in list not handled yet")
 
         for node in observed_graph.nodes:
             root_node, matched, obj, qconfig = matches.get(node.name, (None, None, None, None))
