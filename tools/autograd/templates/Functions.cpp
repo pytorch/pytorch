348e78b08600e4652e57ed892ba0cf9c7a1661a0
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-08-25 16:34:37 -0700

    Evenly distribute output grad into all matching inputs for min/max/median (#43519)
    Summary:
    cc: ngimel mruberry
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43519
    
    Reviewed By: albanD
    
    Differential Revision: D23312235
    
    Pulled By: ngimel
    
    fbshipit-source-id: 678bda54996df7f29acf96add928bb7042fc2069

--- tools/autograd/templates/Functions.cpp
+++ tools/autograd/templates/Functions.cpp
@@ -726,20 +726,15 @@ Tensor _fused_dropout_backward(Tensor grad, Tensor mask, double p1m) {
   }
 }
 
-Tensor select_first_equal_backward(Tensor grad, const Tensor & input, const Tensor & value) {
-  auto grad_input = at::zeros_like(input);
-
-  // find indices of the first element for which input[idx] == value
-  auto first_value_idx = (input == value).nonzero().select(0, 0);
-
-  if (grad_input.dim() == 0) {
-    grad_input.copy_(grad);
+Tensor evenly_distribute_backward(Tensor grad, const Tensor & input, const Tensor & value) {
+  auto mask = (input == value);
+  auto count = mask.sum();
+  auto grad_input = grad / count;
+  if (input.is_cuda()) {
+    return mask * grad_input;
+  } else {
+    return at::zeros_like(input).masked_fill_(mask, grad_input);
   }
-  else {
-    grad_input.index_put_(at::chunk(first_value_idx, grad_input.dim()), grad);
-  }
-
-  return grad_input;
 }
 
 Tensor index_select_backward(Tensor grad, int64_t dim, Tensor indices, IntArrayRef sizes, bool keepdim) {
