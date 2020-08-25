f22a81e81a7d97ac680794bd59bdaa353214fdbe
Jerry Zhang <jerryzh168@gmail.com>
2020-08-25 15:21:39 -0700

    Update on "[quant][graphmode][fx] Add top level APIs"
    Summary:
    Add similar APIs like eager and graph mode on torchscript
    - fuse_fx
    - quantize_fx (for both post training static and qat)
    - quantize_dynamic_fx (for post training dynamic)
    - prepare_fx (for both post training static and qat)
    - prepare_dynamic_fx (for post training dynamic)
    - convert_fx (for all modes)
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/quantization/fx/__init__.py
+++ torch/quantization/fx/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-from .quantize import Quantizer, QuantType
-from .fuse import fuse
+from .quantize import Quantizer
+from .fuse import Fuser
