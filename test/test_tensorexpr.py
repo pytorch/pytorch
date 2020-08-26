ab7f07d3c70d744d25167a17098e5dd51cfc7cec
Mikhail Zolotukhin <mvz@fb.com>
2020-08-25 18:09:55 -0700

    [TensorExpr] Fuser: disallow aten::slice nodes. (#43365)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43365
    
    We don't have shape inference for them yet.
    
    Test Plan: Imported from OSS
    
    Reviewed By: eellison
    
    Differential Revision: D23253418
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: 9c38778b8a616e70f6b2cb5aab03d3c2013b34b0

--- test/test_tensorexpr.py
+++ test/test_tensorexpr.py
@@ -1072,6 +1072,7 @@ class TestTensorExprFuser(BaseTestClass):
 #    r = test(x, y, z)
 #    assert llvm.elapsed_value == 1 or interp.elapsed_value() > 1
 
+    @unittest.skip("no shape inference for aten::slice yet")
     def test_slice(self):
         def easy(x, y):
             a = x[0:512:2]
