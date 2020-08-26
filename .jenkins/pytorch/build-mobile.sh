6b9f5783d27c1925b8a582ef105a6430d2e9c548
ljk53 <liujiakai@fb.com>
2020-08-25 17:12:32 -0700

    [pytorch] upload mobile code analysis result to AWS
    Summary:
    Use the mobile custom build CI job to upload the static analysis result
    to AWS per github PR.
    
    Mobile selective build script could fetch the latest version from AWS
    according to local commit hashes.
    
    [ghstack-poisoned]

--- .jenkins/pytorch/build-mobile.sh
+++ .jenkins/pytorch/build-mobile.sh
@@ -20,6 +20,26 @@ retry pip install --pre torch torchvision \
   -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html \
   --progress-bar off
 
+upload_code_analysis_result() {
+  RESULT_PATH="build_test_custom_build/build_analyzer/work/torch_result.yaml"
+  S3_PATH="s3://pytorch-mobile-build/op_deps/${CIRCLE_SHA1}.yaml"
+
+  echo "Uploading the mobile code analysis result to AWS S3..."
+  echo "CIRCLE_BRANCH: ${CIRCLE_BRANCH}"
+  echo "CIRCLE_SHA1: ${CIRCLE_SHA1}"
+  echo "RESULT_PATH: ${RESULT_PATH}"
+  echo "S3_PATH: ${S3_PATH}"
+
+  if [ ! -f "${RESULT_PATH}" ]; then
+    echo "Cannot find the mobile code analysis result."
+    exit 1
+  fi
+
+  # Running the CI job multiple times for the same commit should be idempotent.
+  # TODO: should maintain an index file for downloading.
+  retry aws s3 cp "${RESULT_PATH}" "${S3_PATH}"
+}
+
 # Run end-to-end process of building mobile library, linking into the predictor
 # binary, and running forward pass with a real model.
 if [[ "$BUILD_ENVIRONMENT" == *-mobile-custom-build-static* ]]; then
@@ -28,6 +48,7 @@ elif [[ "$BUILD_ENVIRONMENT" == *-mobile-custom-build-dynamic* ]]; then
   export LLVM_DIR="$(llvm-config-5.0 --prefix)"
   echo "LLVM_DIR: ${LLVM_DIR}"
   TEST_CUSTOM_BUILD_DYNAMIC=1 test/mobile/custom_build/build.sh
+  upload_code_analysis_result
 else
   TEST_DEFAULT_BUILD=1 test/mobile/custom_build/build.sh
 fi
