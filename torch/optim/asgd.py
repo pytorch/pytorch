c73ad5c2bc90aaf9968ef4fc74fde3fddb160239
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-27 11:03:31 -0700

    WIP asgd
    [ghstack-poisoned]

--- torch/optim/asgd.py
+++ torch/optim/asgd.py
@@ -32,19 +32,7 @@ class ASGD(Optimizer):
                         weight_decay=weight_decay)
         super(ASGD, self).__init__(params, defaults)
 
-    @torch.no_grad()
-    def step(self, closure=None):
-        """Performs a single optimization step.
-
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
-
+    def v1(self):
         for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None:
@@ -83,4 +71,80 @@ class ASGD(Optimizer):
                                 math.pow((1 + group['lambd'] * group['lr'] * state['step']), group['alpha']))
                 state['mu'] = 1 / max(1, state['step'] - group['t0'])
 
+    def v2(self):
+        grads = []
+        params_with_grad = []
+        states = []
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is not None:
+                    if p.grad.is_sparse:
+                        raise RuntimeError('ASGD does not support sparse gradients')
+
+                    grads.append(p.grad)
+                    params_with_grad.append(p)
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        state['eta'] = group['lr']
+                        state['mu'] = 1
+                        state['ax'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    state['step'] += 1
+                    states.append(state)
+
+            if group['weight_decay'] != 0:
+                #grad = grad.add(p, alpha=group['weight_decay'])
+                torch._foreach_add_(grads, p, alpha=group['weight_decay'])
+
+            # decay term
+            torch._foreach_mul_(params_with_grad, 1 - group['lambd'] * state['eta'])
+
+            # update parameter
+            torch._foreach_add_(params_with_grad, grads, alpha=-state['eta'])
+
+
+            # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! 
+
+            # FIX!!!! 
+            # FIX!!!! 
+            # FIX!!!! 
+            # averaging
+            for i in range(len(states)):
+                if states[i]['mu'] != 1:
+                    states[i]['ax'].add_(params_with_grad[i].sub(states[i]['ax']).mul(states[i]['mu']))
+                else:
+                    states[i]['ax'].copy_(params_with_grad[i])
+
+            # update eta and mu
+            for state in states:
+                state['eta'] = (group['lr'] /
+                                math.pow((1 + group['lambd'] * group['lr'] * state['step']), group['alpha']))
+                state['mu'] = 1 / max(1, state['step'] - group['t0'])
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        self.v2()
+ 
         return loss
