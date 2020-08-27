225f22211326474c36cff5018143d25cd97920d3
mfkasim91 <firman.kasim@gmail.com>
2020-08-27 18:44:58 +0100

    Removed outdated comments

--- torch/csrc/autograd/anomaly_mode.h
+++ torch/csrc/autograd/anomaly_mode.h
@@ -26,8 +26,6 @@ struct TORCH_API AnomalyMetadata {
   virtual ~AnomalyMetadata();
   virtual void store_stack() = 0;
   virtual void print_stack(const std::string& current_node_name) = 0;
-  // using shared_ptr<void> instead of shared_ptr<Node> to avoid circular
-  // dependency between this file and "function.h"
   virtual void assign_parent(const std::shared_ptr<Node>& parent_node) = 0;
 };
 
