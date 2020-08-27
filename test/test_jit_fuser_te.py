758294cb4171943b5fd2b59941d65c462a520930
Elias Ellison <eellison@fb.com>
2020-08-27 14:31:53 -0700

    refactor tests (#43631)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43631
    
    I added a new test for just profiler stuff - I don't think the test should go in test_jit.py. Maybe this should just go in test_tensorexpr_fuser, but I'm not really testing tensorexpr stuff either... LMK
    
    Test Plan: Imported from OSS
    
    Reviewed By: bertmaher
    
    Differential Revision: D23358810
    
    Pulled By: eellison
    
    fbshipit-source-id: 074238e1b60e4c4a919a052b7a5312b790ad5d82

--- test/test_jit_fuser_te.py
+++ test/test_jit_fuser_te.py
@@ -21,7 +21,7 @@ torch._C._jit_set_profiling_mode(True)
 from torch.testing._internal.common_utils import run_tests, IS_SANDCASTLE, ProfilingMode, GRAPH_EXECUTOR, \
     enable_profiling_mode_for_profiling_tests
 from torch.testing._internal.jit_utils import JitTestCase, _inline_everything, \
-    RUN_CUDA, RUN_CUDA_HALF, RUN_CUDA_MULTI_GPU
+    RUN_CUDA, RUN_CUDA_HALF, RUN_CUDA_MULTI_GPU, warmup_backward
 
 from textwrap import dedent
 from itertools import product, permutations
@@ -37,27 +37,12 @@ def strip_profiling_nodes(nodes):
     profiling_opcodes = set(['prim::BailoutTemplate', 'prim::BailOut'])
     return [n for n in nodes if n.kind() not in profiling_opcodes]
 
-
-def warmup_backward(f, *args):
-    profiling_count = 2
-    results = []
-    for i in range(profiling_count):
-        if len(args) > 0:
-            r = torch.autograd.grad(f, *args)
-            results.append(r)
-        else:
-            f.backward(retain_graph=True)
-
-    return results
-
-
 def warmup_forward(f, *args):
     profiling_count = 2
     for i in range(profiling_count):
         results = f(*args)
 
     return results
-
 
 class TestTEFuser(JitTestCase):
     def setUp(self):
