a57009e7821aa808d536c05181c7c2b4dfff7318
Elias Ellison <eellison@fb.com>
2020-08-27 14:31:53 -0700

    Specialize optionals for grad_sum_to_size (#43633)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43633
    
    In the backward graph, _grad_sum_to_size is inserted whenever a possibly broadcasting op is called:"
    `"aten::_grad_sum_to_size(Tensor(a) self, int[]? size) -> Tensor(a)"`
     If a broadcast occurred, a sum is called, otherwise the second input is None and it is a no-op. Most of the time, it's a no-op (in the fast RNNs benchmark > 90% of the time).
    
    We can get rid of this op by profiling the optionality of the second input. I added `prim::profile_optional` to do this, which counts the number of times it saw a None value and the number of times it saw a value present. When specializing the backward graph, we insert checks for values we profiled as None, and in the optimized block can remove the grad_sum_to_size calls that use those values.
    
    In the future we may revisit this when NNC supports reductions and we want to replace grad_sum_to_size with sums as well, but I think this is worth landing now.
    
    Test Plan: Imported from OSS
    
    Reviewed By: bwasti, ZolotukhinM
    
    Differential Revision: D23358809
    
    Pulled By: eellison
    
    fbshipit-source-id: a30a148ca581370789d57ba082d23cbf7ef2cd4d

--- torch/csrc/jit/ir/ir.h
+++ torch/csrc/jit/ir/ir.h
@@ -437,7 +437,8 @@ struct TORCH_API Node {
 
   // instructions lowered by the interpreter and not run in the optimized graph
   bool notExecutedOp() const {
-    return kind_ == prim::Constant || kind_ == prim::profile;
+    return kind_ == prim::Constant || kind_ == prim::profile ||
+        kind_ == prim::profile_optional;
   }
 
   // Graphs
@@ -1326,6 +1327,28 @@ struct ProfileOp : public Node {
   static constexpr Symbol Kind = ::c10::prim::profile;
   ProfileOp(Graph* graph, std::function<void(std::vector<IValue>&)> callback)
       : Node(graph, ::c10::prim::profile), callback_(callback) {}
+
+  void cloneFrom(Node* other_) override;
+  Node* allocNewInstance(Graph* g) override;
+
+  const std::function<void(std::vector<IValue>&)>& getCallback() const {
+    return callback_;
+  }
+
+  void setCallback(std::function<void(std::vector<IValue>&)> callback) {
+    callback_ = callback;
+  }
+
+ private:
+  std::function<void(std::vector<IValue>&)> callback_;
+};
+
+struct TORCH_API ProfileOptionalOp : public Node {
+  static constexpr Symbol Kind = ::c10::prim::profile_optional;
+  ProfileOptionalOp(
+      Graph* graph,
+      std::function<void(std::vector<IValue>&)> callback)
+      : Node(graph, ::c10::prim::profile_optional), callback_(callback) {}
 
   void cloneFrom(Node* other_) override;
   Node* allocNewInstance(Graph* g) override;
