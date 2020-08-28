919b7a2531330d94e493a5fbdb6fdf6a4e40d63d
Yujun Zhao <yujunzhao@fb.com>
2020-08-28 10:32:45 -0700

    Land code coverage tool for OSS (#43778)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43778
    
    Move code_coverage_tool from experimental folder to caffe2/tools folder.
    
    Delete `TODO` and fb-related code.
    
    Test Plan: Test locally
    
    Differential Revision: D23399983
    
    fbshipit-source-id: c6a114f0b5472d876b54036be992459ee0d8eca8

---
+++ tools/code_coverage/package/tool/parser/coverage_record.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2004-present Facebook.  All rights reserved.
+
+import typing as t
+
+
+class CoverageRecord(t.NamedTuple):
+    filepath: str
+    covered_lines: t.List[int]
+    uncovered_lines: t.Optional[t.List[int]] = None
+
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        return {
+            "filepath": self.filepath,
+            "covered_lines": self.covered_lines,
+            "uncovered_lines": self.uncovered_lines,
+        }
