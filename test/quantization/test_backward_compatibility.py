221d0d63304e39e69a069b9626c57a51011e70b7
Vasiliy Kuznetsov <vasiliy@fb.com>
2020-08-28 15:37:00 -0700

    quantized conv: add support for graph mode BC testing, and increase coverage (#43524)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43524
    
    1. adds support for testing BC on data format and numerics for graph mode
    quantized modules
    2. using the above, adds coverage for quantized conv2d on graph mode
    
    Test Plan:
    ```
    python test/test_quantization.py TestSerialization.test_conv2d_nobias
    python test/test_quantization.py TestSerialization.test_conv2d_graph
    python test/test_quantization.py TestSerialization.test_conv2d_nobias_graph
    ```
    
    Imported from OSS
    
    Reviewed By: supriyar
    
    Differential Revision: D23335222
    
    fbshipit-source-id: 0c9e93a940bbf6c676c2576eb62fcc725247588b

--- test/quantization/test_backward_compatibility.py
+++ test/quantization/test_backward_compatibility.py
@@ -5,6 +5,7 @@ import os
 
 # torch
 import torch
+import torch.nn as nn
 import torch.nn.quantized as nnq
 import torch.nn.quantized.dynamic as nnqd
 import torch.nn.intrinsic.quantized as nniq
@@ -12,6 +13,35 @@ import torch.nn.intrinsic.quantized as nniq
 # Testing utils
 from torch.testing._internal.common_utils import TestCase
 from torch.testing._internal.common_quantized import override_qengines, qengine_is_fbgemm
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def get_filenames(self, subname):
+    # NB: we take __file__ from the module that defined the test
+    # class, so we place the expect directory where the test script
+    # lives, NOT where test/common_utils.py lives.
+    module_id = self.__class__.__module__
+    munged_id = remove_prefix(self.id(), module_id + ".")
+    test_file = os.path.realpath(sys.modules[module_id].__file__)
+    base_name = os.path.join(os.path.dirname(test_file),
+                             "serialized",
+                             munged_id)
+
+    subname_output = ""
+    if subname:
+        base_name += "_" + subname
+        subname_output = " ({})".format(subname)
+
+    input_file = base_name + ".input.pt"
+    state_dict_file = base_name + ".state_dict.pt"
+    scripted_module_file = base_name + ".scripted.pt"
+    traced_module_file = base_name + ".traced.pt"
+    expected_file = base_name + ".expected.pt"
+
+    return input_file, state_dict_file, scripted_module_file, traced_module_file, expected_file
 
 class TestSerialization(TestCase):
     """ Test backward compatiblity for serialization and numerics
@@ -23,30 +53,8 @@ class TestSerialization(TestCase):
         with current code, make sure we don't break backward compatibility for the
         serialization of quantized modules
         """
-        def remove_prefix(text, prefix):
-            if text.startswith(prefix):
-                return text[len(prefix):]
-            return text
-        # NB: we take __file__ from the module that defined the test
-        # class, so we place the expect directory where the test script
-        # lives, NOT where test/common_utils.py lives.
-        module_id = self.__class__.__module__
-        munged_id = remove_prefix(self.id(), module_id + ".")
-        test_file = os.path.realpath(sys.modules[module_id].__file__)
-        base_name = os.path.join(os.path.dirname(test_file),
-                                 "serialized",
-                                 munged_id)
-
-        subname_output = ""
-        if subname:
-            base_name += "_" + subname
-            subname_output = " ({})".format(subname)
-
-        input_file = base_name + ".input.pt"
-        state_dict_file = base_name + ".state_dict.pt"
-        scripted_module_file = base_name + ".scripted.pt"
-        traced_module_file = base_name + ".traced.pt"
-        expected_file = base_name + ".expected.pt"
+        input_file, state_dict_file, scripted_module_file, traced_module_file, expected_file = \
+            get_filenames(self, subname)
 
         # only generate once.
         if generate and qengine_is_fbgemm():
@@ -66,6 +74,51 @@ class TestSerialization(TestCase):
         qmodule_traced = torch.jit.load(traced_module_file)
         expected = torch.load(expected_file)
         self.assertEqual(qmodule(input_tensor), expected, atol=prec)
+        self.assertEqual(qmodule_scripted(input_tensor), expected, atol=prec)
+        self.assertEqual(qmodule_traced(input_tensor), expected, atol=prec)
+
+    def _test_op_graph(self, qmodule, subname=None, input_size=None, input_quantized=True,
+                       generate=False, prec=None, new_zipfile_serialization=False):
+        r"""
+        Input: a floating point module
+
+        If generate == True, traces and scripts the module and quantizes the results with
+        PTQ, and saves the results.
+
+        If generate == False, traces and scripts the module and quantizes the results with
+        PTQ, and compares to saved results.
+        """
+        input_file, state_dict_file, scripted_module_file, traced_module_file, expected_file = \
+            get_filenames(self, subname)
+
+        # only generate once.
+        if generate and qengine_is_fbgemm():
+            input_tensor = torch.rand(*input_size).float()
+            torch.save(input_tensor, input_file)
+
+            # convert to TorchScript
+            scripted = torch.jit.script(qmodule)
+            traced = torch.jit.trace(qmodule, input_tensor)
+
+            # quantize
+
+            def _eval_fn(model, data):
+                model(data)
+
+            qconfig_dict = {'': torch.quantization.default_qconfig}
+            scripted_q = torch.quantization.quantize_jit(
+                scripted, qconfig_dict, _eval_fn, [input_tensor])
+            traced_q = torch.quantization.quantize_jit(
+                traced, qconfig_dict, _eval_fn, [input_tensor])
+
+            torch.jit.save(scripted_q, scripted_module_file)
+            torch.jit.save(traced_q, traced_module_file)
+            torch.save(scripted_q(input_tensor), expected_file)
+
+        input_tensor = torch.load(input_file)
+        qmodule_scripted = torch.jit.load(scripted_module_file)
+        qmodule_traced = torch.jit.load(traced_module_file)
+        expected = torch.load(expected_file)
         self.assertEqual(qmodule_scripted(input_tensor), expected, atol=prec)
         self.assertEqual(qmodule_traced(input_tensor), expected, atol=prec)
 
@@ -92,7 +145,30 @@ class TestSerialization(TestCase):
         module = nnq.Conv2d(3, 3, kernel_size=3, stride=1, padding=0, dilation=1,
                             groups=1, bias=True, padding_mode="zeros")
         self._test_op(module, input_size=[1, 3, 6, 6], generate=False)
-        # TODO: graph mode quantized conv2d module
+
+    @override_qengines
+    def test_conv2d_nobias(self):
+        module = nnq.Conv2d(3, 3, kernel_size=3, stride=1, padding=0, dilation=1,
+                            groups=1, bias=False, padding_mode="zeros")
+        self._test_op(module, input_size=[1, 3, 6, 6], generate=False)
+
+    @override_qengines
+    def test_conv2d_graph(self):
+        module = nn.Sequential(
+            torch.quantization.QuantStub(),
+            nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=0, dilation=1,
+                      groups=1, bias=True, padding_mode="zeros"),
+        )
+        self._test_op_graph(module, input_size=[1, 3, 6, 6], generate=False)
+
+    @override_qengines
+    def test_conv2d_nobias_graph(self):
+        module = nn.Sequential(
+            torch.quantization.QuantStub(),
+            nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=0, dilation=1,
+                      groups=1, bias=False, padding_mode="zeros"),
+        )
+        self._test_op_graph(module, input_size=[1, 3, 6, 6], generate=False)
 
     @override_qengines
     def test_conv2d_relu(self):
