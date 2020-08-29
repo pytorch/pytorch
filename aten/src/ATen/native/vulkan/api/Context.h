6ea94b9a6658eab7b051b820fbdd49579936e8ba
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-29 12:37:44 -0700

    Revert "Revert D23252335: Refactor Vulkan context into its own files. Use RAII." (#43628)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43628
    
    This reverts commit 6c772515ed1a87ec676382492ff3c019c6d194c3.
    
    Test Plan: Imported from OSS
    
    Reviewed By: IvanKobzarev
    
    Differential Revision: D23356714
    
    Pulled By: AshkanAliabadi
    
    fbshipit-source-id: a44af3b3c7b00a097eae1b0c9a00fdabc7ab6f86

---
+++ aten/src/ATen/native/vulkan/api/Context.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+//
+// Vulkan Context holds onto all relevant Vulkan state as it pertains to our
+// use of Vulkan in PyTorch.  The context is currently a global object, but
+// technically it does not need to be if we were to make it explicit to the
+// user.
+//
+
+class C10_EXPORT Context final {
+ public:
+  explicit Context(bool enable_validation_layers);
+  ~Context() = default;
+
+  inline VkInstance instance() const {
+    return instance_.get();
+  }
+
+  inline VkPhysicalDevice physical_device() const {
+    return physical_device_;
+  }
+
+  inline const VkPhysicalDeviceLimits& physical_device_limits() const {
+    return physical_device_limits_;
+  }
+
+  inline VkDevice device() const {
+    return device_.get();
+  }
+
+  inline VkQueue queue() const {
+    return queue_;
+  }
+
+ private:
+  class Debug final {
+   public:
+    explicit Debug(VkInstance instance);
+    void operator()(VkDebugReportCallbackEXT debug_report_callback) const;
+
+   private:
+    VkInstance instance_;
+  };
+
+ private:
+  // Construction and destruction order matters.  Do not move members around.
+  Handle<VkInstance, decltype(&VK_DELETER(Instance))> instance_;
+  Handle<VkDebugReportCallbackEXT, Debug> debug_report_callback_;
+  VkPhysicalDevice physical_device_;
+  VkPhysicalDeviceLimits physical_device_limits_;
+  uint32_t compute_queue_family_index_;
+  Handle<VkDevice, decltype(&VK_DELETER(Device))> device_;
+  VkQueue queue_;
+};
+
+C10_EXPORT bool available();
+C10_EXPORT Context& context();
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
