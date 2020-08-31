73977c4898434f48bf5d9f426a89d65ad9acb03d
albanD <desmaison.alban@gmail.com>
2020-08-31 15:55:00 -0400

    Update on "Add fw_grad field and accessors"
    [ghstack-poisoned]

--- torch/overrides.py
+++ torch/overrides.py
@@ -783,6 +783,7 @@ def get_testing_overrides() -> Dict[Callable, Callable]:
         Tensor._cdata.__get__: lambda self: -1,
         Tensor.grad.__get__: lambda self: -1,
         Tensor._grad.__get__: lambda self: -1,
+        Tensor._fw_grad.__get__: lambda self: -1,
         Tensor._grad_fn.__get__: lambda self: -1,
         Tensor.grad_fn.__get__: lambda self: -1,
         Tensor._version.__get__: lambda self: -1,
