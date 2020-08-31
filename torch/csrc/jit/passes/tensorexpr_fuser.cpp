a7e7981c0b3da031a33699a7170f5e2c62cd463f
Elias Ellison <eellison@fb.com>
2020-08-31 11:45:27 -0700

    Use prim::TensorExprGroup interned symbol (#43635)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43635
    
    Intern the symbol, no functional changes. Aliasing need to be looked at but this should be done in a separate PR; this PR is just changing the symbol.
    
    Test Plan: Imported from OSS
    
    Reviewed By: bertmaher
    
    Differential Revision: D23358806
    
    Pulled By: eellison
    
    fbshipit-source-id: f18bcd142a0daf514136f019ae607e4c3f45d9f8

--- torch/csrc/jit/passes/tensorexpr_fuser.cpp
+++ torch/csrc/jit/passes/tensorexpr_fuser.cpp
@@ -134,11 +134,6 @@ bool tensorExprFuserEnabled() {
   return true;
 }
 
-const Symbol& getTensorExprSymbol() {
-  static Symbol s = Symbol::fromQualString("tensorexpr::Group");
-  return s;
-}
-
 struct nodesComparator {
   bool operator()(Node* a, Node* b) const {
     return a->isAfter(b);
@@ -165,13 +160,13 @@ class TensorExprFuser {
 
  private:
   Node* getOrCreateTensorExprSubgraph(Node* n) {
-    if (n->hasAttribute(attr::Subgraph) && n->kind() == getTensorExprSymbol()) {
+    if (n->hasAttribute(attr::Subgraph) && n->kind() == prim::TensorExprGroup) {
       return n;
     }
     std::unordered_map<Value*, Value*> vmap;
-    GRAPH_UPDATE("Creating a tensorexpr::Group node from: ", *n);
+    GRAPH_UPDATE("Creating a prim::TensorExprGroup node from: ", *n);
     auto te_group =
-        SubgraphUtils::createSingletonSubgraph(n, getTensorExprSymbol(), vmap);
+        SubgraphUtils::createSingletonSubgraph(n, prim::TensorExprGroup, vmap);
     updateTypeinfoMapWithVmap(vmap);
     return te_group;
   }
@@ -226,7 +221,7 @@ class TensorExprFuser {
     }
 
     // Update typeinfo for inputs and outputs of the fusion group
-    if (fusion_group->kind() == getTensorExprSymbol()) {
+    if (fusion_group->kind() == prim::TensorExprGroup) {
       auto subgraph = SubgraphUtils::getSubgraph(fusion_group);
       for (size_t idx = 0; idx < subgraph->inputs().size(); idx++) {
         if (typeinfo_map_.count(subgraph->inputs()[idx])) {
@@ -258,12 +253,12 @@ class TensorExprFuser {
 
   static void debugDumpFusionGroup(const std::string& msg, Node* n) {
     GRAPH_DEBUG(msg, *n);
-    if (n->kind() == getTensorExprSymbol()) {
+    if (n->kind() == prim::TensorExprGroup) {
       GRAPH_DEBUG(*n->g(attr::Subgraph));
     }
   }
 
-  // Merge fusible nodes into subgraphs in tensorexpr::Group nodes.
+  // Merge fusible nodes into subgraphs in prim::TensorExprGroup nodes.
   void createFusionGroups(Block* block) {
     std::vector<Node*> fusion_groups;
     auto reverse_iter = block->nodes().reverse();
@@ -318,7 +313,7 @@ class TensorExprFuser {
   }
 
   bool inlineIfTooSmall(Node* n) {
-    if (n->kind() != getTensorExprSymbol()) {
+    if (n->kind() != prim::TensorExprGroup) {
       return false;
     }
     auto subgraph = SubgraphUtils::getSubgraph(n);
@@ -433,7 +428,7 @@ class TensorExprFuser {
     // Symbolic checks
     REQ(canHandle(producer));
     TORCH_INTERNAL_ASSERT(
-        canHandle(consumer) || consumer->kind() == getTensorExprSymbol());
+        canHandle(consumer) || consumer->kind() == prim::TensorExprGroup);
 
     // Alias checks
     REQ(aliasDb_->couldMoveBeforeTopologically(producer, consumer));
@@ -448,7 +443,7 @@ class TensorExprFuser {
     }
 
     if (!consumer->hasAttribute(attr::Subgraph) &&
-        consumer->kind() != getTensorExprSymbol()) {
+        consumer->kind() != prim::TensorExprGroup) {
       // Don't initiate a fusion group from prim::ListConstruct
       REQ(consumer->kind() != prim::ListConstruct);
       REQ(consumer->kind() != aten::slice);
@@ -608,7 +603,7 @@ class TensorExprFuser {
       for (Block* b : n->blocks()) {
         guardFusionGroups(b);
       }
-      if (n->kind() == getTensorExprSymbol()) {
+      if (n->kind() == prim::TensorExprGroup) {
         fusion_groups.push_back(n);
       }
     }
@@ -672,9 +667,9 @@ Operation createTensorExprOp(const Node* node) {
 
 RegisterOperators TensorExprOps({
     torch::jit::Operator(
-        getTensorExprSymbol(),
+        prim::TensorExprGroup,
         createTensorExprOp,
-        AliasAnalysisKind::PURE_FUNCTION),
+        AliasAnalysisKind::INTERNAL_SPECIAL_CASE),
 });
 
 } // namespace jit
