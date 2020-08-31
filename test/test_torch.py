417e20225b477b6b09bc51e48a641c2e6d8a4b2a
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-08-31 14:11:56 -0700

    fix

--- test/test_torch.py
+++ test/test_torch.py
@@ -16194,21 +16194,17 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         for use_out, row_major, incx, incy, lda_tail in product((False, True), (False, True), (1, 2), (1, 2), (0, 1)):
             _test(use_out, row_major, incx, incy, lda_tail)
 
+    @precisionOverride({torch.double: 1e-8, torch.float: 1e-4, torch.bfloat16: 0.6,
+                        torch.half: 1e-1, torch.cfloat: 1e-4, torch.cdouble: 1e-8})
     @dtypesIfCUDA(*torch.testing.get_all_complex_dtypes(), *torch.testing.get_all_fp_dtypes(include_bfloat16=False))
     @dtypes(*torch.testing.get_all_complex_dtypes(), *torch.testing.get_all_fp_dtypes())
     def test_addmm(self, device, dtype):
-        prec = {
-            torch.double: 1e-8,
-            torch.float: 1e-4,
-            torch.bfloat16: 1e-1,
-            torch.half: 1e-1,
-            torch.cfloat: 1e-4,
-            torch.cdouble: 1e-8
-        }[dtype]
-
-        if False and dtype.is_complex:  # bug to be fixed in another PR
-            alpha = 1.2 + 0.3j
-            beta = 0.5 + 1.6j
+        numpy_dtype = dtype
+        if dtype in {torch.bfloat16}:
+            numpy_dtype = torch.float
+        if dtype.is_complex:
+            alpha = 0.9 + 0.3j
+            beta = 0.5 + 0.6j
         else:
             alpha = 1.2
             beta = 0.8
@@ -16219,14 +16215,11 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         res1 = torch.addmm(M, m1, m2, alpha=alpha, beta=beta)
         res2 = torch.full_like(res1, math.nan)
         torch.addmm(M, m1, m2, alpha=alpha, beta=beta, out=res2)
-        res3 = (beta * M).cpu().tolist()
-        for i in range(10):
-            for j in range(25):
-                for k in range(50):
-                    res3[i][j] += alpha * m1[i, k] * m2[k, j]
-        res3 = torch.tensor(res3, device=device, dtype=dtype)
-        self.assertEqual(res1, res2, atol=prec, rtol=0)
-        self.assertEqual(res1, res3, atol=prec, rtol=0)
+        res3 = (beta * M).to(numpy_dtype).cpu().numpy() + alpha * (
+            m1.to(numpy_dtype).cpu().numpy() @ m2.to(numpy_dtype).cpu().numpy())
+        res3 = torch.from_numpy(res3).to(dtype)
+        self.assertEqual(res1, res2)
+        self.assertEqual(res1, res3)
 
         # Test 0-strided
         M = torch.randn(10, 1).to(device=device, dtype=dtype).expand(10, 25)
@@ -16235,41 +16228,38 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         res1 = torch.addmm(M, m1, m2, alpha=alpha, beta=beta)
         res2 = torch.full_like(res1, math.nan)
         torch.addmm(M, m1, m2, alpha=alpha, beta=beta, out=res2)
-        res3 = (beta * M).cpu().tolist()
-        for i in range(10):
-            for j in range(25):
-                for k in range(50):
-                    res3[i][j] += alpha * m1[i, k] * m2[k, j]
-        res3 = torch.tensor(res3, device=device, dtype=dtype)
-        self.assertEqual(res1, res2, atol=prec, rtol=0)
-        self.assertEqual(res1, res3, atol=prec, rtol=0)
+        res3 = (beta * M).to(numpy_dtype).cpu().numpy() + alpha * (
+            m1.to(numpy_dtype).cpu().numpy() @ m2.to(numpy_dtype).cpu().numpy())
+        res3 = torch.from_numpy(res3).to(dtype)
+        self.assertEqual(res1, res2)
+        self.assertEqual(res1, res3)
 
     @dtypes(torch.float, torch.double)
     @dtypesIfCUDA(*([torch.float, torch.double] +
                     ([] if TEST_WITH_ROCM else torch.testing.get_all_complex_dtypes())))
     @tf32_on_and_off(0.005)
     def test_addmm_sizes(self, device, dtype):
+        numpy_dtype = dtype
+        if dtype in {torch.bfloat16}:
+            numpy_dtype = torch.float
+        if dtype.is_complex:
+            alpha = 1.2 + 0.3j
+            beta = 0.5 + 1.6j
+        else:
+            alpha = 1.2
+            beta = 0.8
         for m in [0, 1, 25]:
             for n in [0, 1, 10]:
                 for k in [0, 1, 8]:
-                    if False and dtype.is_complex:  # bug to be fixed in another PR
-                        alpha = 1.2 + 0.3j
-                        beta = 0.5 + 1.6j
-                    else:
-                        alpha = 1.2
-                        beta = 0.8
                     M = torch.randn(n, m, device=device, dtype=dtype)
                     m1 = torch.randn(n, k, device=device, dtype=dtype)
                     m2 = torch.randn(k, m, device=device, dtype=dtype)
                     res1 = torch.addmm(M, m1, m2, alpha=alpha, beta=beta)
                     res2 = torch.full_like(res1, math.nan)
                     torch.addmm(M, m1, m2, alpha=alpha, beta=beta, out=res2)
-                    res3 = (beta * M).cpu().tolist()
-                    for i in range(n):
-                        for j in range(m):
-                            for l in range(k):
-                                res3[i][j] += (alpha * m1[i, l] * m2[l, j]).item()
-                    res3 = torch.tensor(res3, device=device, dtype=dtype).view_as(res1)
+                    res3 = (beta * M).to(numpy_dtype).cpu().numpy() + alpha * (
+                        m1.to(numpy_dtype).cpu().numpy() @ m2.to(numpy_dtype).cpu().numpy())
+                    res3 = torch.from_numpy(res3).to(dtype)
                     self.assertEqual(res1, res2)
                     self.assertEqual(res1, res3)
 
