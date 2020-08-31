5712ca0970e4ef85b453b037628fca4662a23668
Elias Ellison <eellison@fb.com>
2020-08-31 11:45:27 -0700

    Use prim::TensorExprGroup interned symbol (#43635)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43635
    
    Intern the symbol, no functional changes. Aliasing need to be looked at but this should be done in a separate PR; this PR is just changing the symbol.
    
    Test Plan: Imported from OSS
    
    Reviewed By: bertmaher
    
    Differential Revision: D23358806
    
    Pulled By: eellison
    
    fbshipit-source-id: f18bcd142a0daf514136f019ae607e4c3f45d9f8

--- torch/csrc/jit/runtime/interpreter.cpp
+++ torch/csrc/jit/runtime/interpreter.cpp
@@ -318,6 +318,7 @@ struct CanEmitInline {
         // instruction stack
         // by the later BailOut in createBailoutBlock and its jf_index
         // will become invalid.
+        v->node()->kind() != prim::TensorExprGroup &&
         v->node()->kind() != prim::CudaFusionGroup &&
         v->node()->kind() != prim::FusionGroup &&
         v->node()->kind() != prim::BailOut && v->uses().size() == 1 &&
