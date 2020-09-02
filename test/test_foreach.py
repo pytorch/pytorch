297c938729a0adbd98a235a5970c972f6148adfe
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-09-02 12:15:40 -0700

    Add _foreach_add(TensorList tl1, TensorList tl2) and _foreach_add_(TensorList tl1, TensorList tl2) APIs (#42533)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42533
    
    [First PR: Add private API to support tensor lists: _foreach_add(TensorList tensors, Scalar scalar)](https://github.com/pytorch/pytorch/pull/41554).
    
    **Motivation**
    [GitHub issue](https://github.com/pytorch/pytorch/issues/38655)
    Current PyTorch optimizer implementations are not efficient in cases when we work with a lot of small feature tensors. Starting a lot of kernels slows down the whole process. We need to reduce the number of kernels that we start.
    As an example, we should be looking at [NVIDIAs Apex](https://github.com/NVIDIA/apex).
    In order to track progress, we will pick PyTorchs DCGAN model with Adam optimizer and once the optimizer is reimplemented with tensor lists, benchmark the model performance against original model version, Apexs version with original Adam optimizer and it’s FusedAdam optimizer.
    
    **Current API restrictions**
    - List can't be empty (will fixed in upcoming PRs).
    - All tensors in the list must have the same dtype, device and size.
    
    **Broadcasting**
    At this point we don't support broadcasting.
    
    **What is 'Fast' and 'Slow' route**
    In particular cases, we cant process an op with a fast list CUDA kernel. Still, we can do with a regular for-loop where the op will be applied to each tensor individually through the dispatch mechanisms. There are a few checks that decide whether the op will be performed via a 'fast' or 'slow' path.
    To go the fast route,
    - All tensors must have strided layout
    - All tensors must be dense and not have overlapping memory
    - The resulting tensor type must be the same.
    
    ----------------
    **In this PR**
    - Adding a `_foreach_add(TensorList tl1, TensorList tl2)` API
    - Adding a `_foreach_add_(TensorList tl1, TensorList tl2)` API
    
    **Tests**
    Tested via unit tests
    
    **TODO**
    1. Properly handle empty lists
    
    **Plan for the next PRs**
    1. APIs
    - Binary Ops for list with Scalar
    - Binary Ops for list with list
    - Unary Ops for list
    - Pointwise Ops
    
    2. Complete tasks from TODO
    3. Rewrite PyTorch optimizers to use for-each operators for performance gains.
    
    Test Plan: Imported from OSS
    
    Reviewed By: zou3519
    
    Differential Revision: D23331894
    
    Pulled By: izdeby
    
    fbshipit-source-id: 876dd1bc82750f609b9e3ba23c8cad94d8d6041c

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -1,4 +1,5 @@
 import torch
+import unittest
 from torch.testing._internal.common_utils import TestCase, run_tests
 from torch.testing._internal.common_device_type import instantiate_device_type_tests, dtypes
 
@@ -18,7 +19,8 @@ class TestForeach(TestCase):
         self.assertEqual(res, expected)
 
         if dtype in [torch.bool]:
-            with self.assertRaisesRegex(RuntimeError, "result type Long can't be cast to the desired output type Bool"):
+            with self.assertRaisesRegex(RuntimeError, 
+                                        "result type Long can't be cast to the desired output type Bool"):
                 torch._foreach_add_(tensors, int_scalar)
         else:
             torch._foreach_add_(tensors, int_scalar)
@@ -118,6 +120,96 @@ class TestForeach(TestCase):
         tensors = [torch.tensor([1.1], dtype=torch.float, device=device), 
                    torch.tensor([1], dtype=torch.long, device=device)]
         self.assertRaises(RuntimeError, lambda: torch._foreach_add(tensors, 1))
+
+    def test_add_list_error_cases(self, device):
+        tensors1 = []
+        tensors2 = []
+
+        # Empty lists
+        with self.assertRaises(RuntimeError):
+            torch._foreach_add(tensors1, tensors2)
+        with self.assertRaises(RuntimeError):
+            torch._foreach_add_(tensors1, tensors2)
+
+        # One empty list
+        tensors1.append(torch.tensor([1], device=device))
+        with self.assertRaisesRegex(RuntimeError, "Tensor list must have at least one tensor."):
+            torch._foreach_add(tensors1, tensors2)
+        with self.assertRaisesRegex(RuntimeError, "Tensor list must have at least one tensor."):
+            torch._foreach_add_(tensors1, tensors2)
+
+        # Lists have different amount of tensors
+        tensors2.append(torch.tensor([1], device=device))
+        tensors2.append(torch.tensor([1], device=device))
+        with self.assertRaisesRegex(RuntimeError, "Tensor lists must have the same number of tensors, got 1 and 2"):
+            torch._foreach_add(tensors1, tensors2)
+        with self.assertRaisesRegex(RuntimeError, "Tensor lists must have the same number of tensors, got 1 and 2"):
+            torch._foreach_add_(tensors1, tensors2)
+
+        # Different dtypes
+        tensors1 = [torch.zeros(10, 10, device=device, dtype=torch.float) for _ in range(10)]
+        tensors2 = [torch.ones(10, 10, device=device, dtype=torch.int) for _ in range(10)]
+
+        with self.assertRaisesRegex(RuntimeError, "All tensors in the tensor list must have the same dtype."):
+            torch._foreach_add(tensors1, tensors2)
+        with self.assertRaisesRegex(RuntimeError, "All tensors in the tensor list must have the same dtype."):
+            torch._foreach_add_(tensors1, tensors2)
+
+        # different devices
+        if torch.cuda.is_available():
+            tensor1 = torch.zeros(10, 10, device="cuda:0")
+            tensor2 = torch.ones(10, 10, device="cuda:1")
+            with self.assertRaisesRegex(RuntimeError, "Expected all tensors to be on the same device"):
+                torch._foreach_add([tensor1], [tensor2])
+            with self.assertRaisesRegex(RuntimeError, "Expected all tensors to be on the same device"):
+                torch._foreach_add_([tensor1], [tensor2])
+
+        # Coresponding tensors with different sizes 
+        tensors1 = [torch.zeros(10, 10, device=device) for _ in range(10)]
+        tensors2 = [torch.ones(11, 11, device=device) for _ in range(10)]
+        with self.assertRaisesRegex(RuntimeError, "Corresponding tensors in lists must have the same size"):
+            torch._foreach_add(tensors1, tensors2)
+        with self.assertRaisesRegex(RuntimeError, r", got \[10, 10\] and \[11, 11\]"):
+            torch._foreach_add_(tensors1, tensors2)
+
+    @dtypes(*torch.testing.get_all_dtypes())
+    def test_add_list_same_size(self, device, dtype):
+        tensors1 = [torch.zeros(10, 10, device=device, dtype=dtype) for _ in range(10)]
+        tensors2 = [torch.ones(10, 10, device=device, dtype=dtype) for _ in range(10)]
+
+        res = torch._foreach_add(tensors1, tensors2)
+        torch._foreach_add_(tensors1, tensors2)
+        self.assertEqual(res, tensors1)
+        self.assertEqual(res[0], torch.ones(10, 10, device=device, dtype=dtype))
+
+    @dtypes(*torch.testing.get_all_dtypes())
+    def test_add_list_different_sizes(self, device, dtype):
+        tensors1 = [torch.zeros(10 + n, 10 + n, device=device, dtype=dtype) for n in range(10)]
+        tensors2 = [torch.ones(10 + n, 10 + n, device=device, dtype=dtype) for n in range(10)]
+
+        res = torch._foreach_add(tensors1, tensors2)
+        torch._foreach_add_(tensors1, tensors2)
+        self.assertEqual(res, tensors1)
+        self.assertEqual(res, [torch.ones(10 + n, 10 + n, device=device, dtype=dtype) for n in range(10)])
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA not found")
+    @dtypes(*torch.testing.get_all_dtypes())
+    def test_add_list_slow_path(self, device, dtype):
+        # different strides
+        tensor1 = torch.zeros(10, 10, device=device, dtype=dtype)
+        tensor2 = torch.ones(10, 10, device=device, dtype=dtype)
+        res = torch._foreach_add([tensor1], [tensor2.t()])
+        torch._foreach_add_([tensor1], [tensor2])
+        self.assertEqual(res, [tensor1])
+
+        # non contiguous 
+        tensor1 = torch.randn(5, 2, 1, 3, device=device)[:, 0]
+        tensor2 = torch.randn(5, 2, 1, 3, device=device)[:, 0]
+        self.assertFalse(tensor1.is_contiguous())
+        self.assertFalse(tensor2.is_contiguous())
+        res = torch._foreach_add([tensor1], [tensor2])
+        torch._foreach_add_([tensor1], [tensor2])
+        self.assertEqual(res, [tensor1])
 
 instantiate_device_type_tests(TestForeach, globals())
 
