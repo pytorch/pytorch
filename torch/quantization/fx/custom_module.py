510a368173d4386abb81ae2ec14069eb788550db
Jerry Zhang <jerryzh168@gmail.com>
2020-09-02 16:04:37 -0700

    [quant][graphmode][fx] Support quantization for custom module
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

---
+++ torch/quantization/fx/custom_module.py
@@ -0,0 +1,13 @@
+CUSTOM_MODULES = set()
+
+def register_custom_module_class(custom_module_class):
+    ''' Register a module as custom module, when the module
+    appear in the code, we will observe and quantize it as one
+    unit
+    '''
+    CUSTOM_MODULES.insert(custom_module_class)
+
+def is_custom_module(module):
+    ''' Check if a module is a custom module or not
+    '''
+    return type(module) in CUSTOM_MODULES
