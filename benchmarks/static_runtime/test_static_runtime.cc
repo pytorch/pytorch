575e37696d595cd6793bf6475f841c58c59e855b
bwasti <bwasti@fb.com>
2020-09-03 18:30:15 +0000

    Add Deep and wide to test and flatten/tranpose for good measure
    [ghstack-poisoned]

--- benchmarks/static_runtime/test_static_runtime.cc
+++ benchmarks/static_runtime/test_static_runtime.cc
@@ -18,3 +18,27 @@ TEST(StaticRuntime, TrivialModel) {
   at::Tensor output_2 = runtime.run(input_tensors)[0];
   EXPECT_TRUE(output_1.equal(output_2));
 }
+
+TEST(StaticRuntime, DeepWide) {
+  const int embedding_size = 32;
+  const int num_features = 50;
+  torch::jit::Module mod = getDeepAndWideSciptModel();
+  torch::jit::StaticRuntime runtime(mod);
+
+  for (int batch_size : {1, 8, 32}) {
+    for (int i = 0; i < 5; ++i) {
+      auto ad_emb_packed = torch::randn({batch_size, 1, embedding_size});
+      auto user_emb = torch::randn({batch_size, 1, embedding_size});
+      auto wide = torch::randn({batch_size, num_features});
+
+      // run jit graph executor
+      std::vector<at::IValue> inputs({ad_emb_packed, user_emb, wide});
+      at::Tensor output_1 = mod.forward(inputs).toTensor();
+
+      // run static runtime
+      std::vector<at::Tensor> input_tensors({ad_emb_packed, user_emb, wide});
+      at::Tensor output_2 = runtime.run(input_tensors)[0];
+      EXPECT_TRUE(output_1.equal(output_2));
+    }
+  }
+}
