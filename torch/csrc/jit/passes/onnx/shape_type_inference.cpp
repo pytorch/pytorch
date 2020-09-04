47795adde457dbce513c0981048f36f8c25cc76d
BowenBao <bowbao@microsoft.com>
2020-09-04 15:29:03 -0700

    clang

--- torch/csrc/jit/passes/onnx/shape_type_inference.cpp
+++ torch/csrc/jit/passes/onnx/shape_type_inference.cpp
@@ -35,8 +35,8 @@ TypePtr MergeInferredType(TypePtr existing_type, TypePtr inferred_type) {
 
   if (new_tensor_type && old_tensor_type) {
     if (!old_tensor_type->device()) {
-      // device not avaible means this is an invalid tensor type (most likely an empty one)
-      // return inferred type directly.
+      // device not avaible means this is an invalid tensor type (most likely an
+      // empty one) return inferred type directly.
       return new_tensor_type;
     }
     auto type = old_tensor_type;
