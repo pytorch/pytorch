3d7c22a2ced626f906a9c43d56e15f42d4698877
neginraoof <neginmr@utexas.edu>
2020-09-04 15:19:37 -0700

    [ONNX] Enable new scripting passes for functionalization and remove_mutation (#43791)
    Summary:
    Duplicate of https://github.com/pytorch/pytorch/issues/41413
    This PR initiates the process of updating the torchsciprt backend interface used by ONNX exporter.
    
    Replace jit lower graph pass by freeze module pass
    
    Enable ScriptModule tests for ONNX operator tests (ORT backend) and model tests by default.
    
    Replace jit remove_inplace_ops pass with remove_mutation and consolidation all passes for handling inplace ops.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43791
    
    Reviewed By: houseroad
    
    Differential Revision: D23421872
    
    Pulled By: bzinodev
    
    fbshipit-source-id: a98710c45ee905748ec58385e2a232de2486331b

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -39,8 +39,8 @@
 #include <torch/csrc/jit/passes/onnx/function_substitution.h>
 #include <torch/csrc/jit/passes/onnx/peephole.h>
 #include <torch/csrc/jit/passes/onnx/prepare_division_for_onnx.h>
-#include <torch/csrc/jit/passes/onnx/prepare_inplace_ops_for_onnx.h>
 #include <torch/csrc/jit/passes/onnx/preprocess_for_onnx.h>
+#include <torch/csrc/jit/passes/onnx/remove_inplace_ops_for_onnx.h>
 #include <torch/csrc/jit/passes/onnx/scalar_type_analysis.h>
 #include <torch/csrc/jit/passes/onnx/shape_type_inference.h>
 #include <torch/csrc/jit/passes/onnx/unpack_quantized_weights.h>
@@ -184,6 +184,8 @@ void initJITBindings(PyObject* module) {
           },
           pybind11::return_value_policy::move)
       .def("_jit_pass_onnx_scalar_type_analysis", ScalarTypeAnalysisForONNX)
+      .def(
+          "_jit_pass_onnx_remove_inplace_ops_for_onnx", RemoveInplaceOpsForONNX)
       .def(
           "_jit_pass_onnx_prepare_inplace_ops_for_onnx",
           PrepareInplaceOpsForONNX)
