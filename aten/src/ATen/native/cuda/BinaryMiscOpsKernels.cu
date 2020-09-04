5caf3e8ac74d869bd14a4a228b4ad8dac1e48502
Brian Hirsh <hirsheybar@fb.com>
2020-09-04 07:35:59 -0700

    adding a beta parameter to the smooth_l1 loss fn
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/BinaryMiscOpsKernels.cu
+++ aten/src/ATen/native/cuda/BinaryMiscOpsKernels.cu
@@ -19,11 +19,12 @@ void atan2_kernel_cuda(TensorIterator& iter) {
   });
 }
 
-void smooth_l1_kernel_cuda(TensorIterator& iter) {
+void smooth_l1_kernel_cuda(TensorIterator& iter, Scalar beta) {
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "smooth_l1_cuda", [&]() {
+    auto beta_val = beta.to<scalar_t>();
     gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
       auto z = ::abs(a - b);
-      return z < scalar_t(1.) ? scalar_t(0.5) * z * z : z - scalar_t(0.5);
+      return z < beta_val ? scalar_t(0.5) * z * z / beta_val : z - scalar_t(0.5) * beta_val;
     });
   });
 }
