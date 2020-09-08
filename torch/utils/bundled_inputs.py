ac92e5d58be11ce2bbad4619b418e1e9ce7960ae
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-09-08 13:59:35 -0500

    Merge with newest changes

--- torch/utils/bundled_inputs.py
+++ torch/utils/bundled_inputs.py
@@ -54,8 +54,8 @@ def augment_model_with_bundled_inputs(
         raise Exception("Only ScriptModule is supported.")
 
     forward_arg_types = [arg.type for arg in model.forward.schema.arguments[1:]]
-    deflated_inputs_type: ListType[TupleType] = ListType(TupleType(forward_arg_types))
-    inflated_inputs_type: OptionalType[ListType[TupleType]] = OptionalType(deflated_inputs_type)
+    deflated_inputs_type: ListType = ListType(TupleType(forward_arg_types))
+    inflated_inputs_type: OptionalType[ListType] = OptionalType(deflated_inputs_type)
     model._c._register_attribute("_bundled_inputs_deflated", deflated_inputs_type, [])
     model._c._register_attribute("_bundled_inputs_inflated", inflated_inputs_type, None)
 
