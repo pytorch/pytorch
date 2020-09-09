350130a69d7a3515a200f7b5d02998a02160c113
Bert Maher <bertrand@fb.com>
2020-09-09 11:06:53 -0700

    Prevent the TE fuser from getting datatypes it can't handle (#44160)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/44160
    
    Test Plan: Imported from OSS
    
    Reviewed By: navahgar
    
    Differential Revision: D23528508
    
    Pulled By: bertmaher
    
    fbshipit-source-id: 03b22725fb2666f441cb504b35397ea6d155bb85

--- torch/csrc/jit/passes/tensorexpr_fuser.cpp
+++ torch/csrc/jit/passes/tensorexpr_fuser.cpp
@@ -586,6 +586,17 @@ class TensorExprFuser {
       if (input->node()->kind() == prim::Constant) {
         REQ(!input->type()->cast<TensorType>())
       }
+      if (auto const& tt = input->type()->cast<TensorType>()) {
+        auto st = tt->scalarType();
+        if (!st) {
+          // All tensor types should be known.
+          return false;
+        }
+        if (c10::isComplexType(*st) || c10::isQIntType(*st) ||
+            *st == c10::ScalarType::BFloat16) {
+          return false;
+        }
+      }
     }
     if (node->kind() == aten::cat) {
       REQ(node->input(0)->node()->kind() == prim::ListConstruct);
