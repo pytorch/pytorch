16b5358936aa57310abf68ae5793977ef0d9f608
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-09-08 18:00:42 -0700

    Revert "positive dims"
    This reverts commit ab9c92c9317557d1c8fd2bc48d76f3846fb786c7.

--- aten/src/ATen/native/AdaptiveAveragePooling.cpp
+++ aten/src/ATen/native/AdaptiveAveragePooling.cpp
@@ -332,7 +332,7 @@ namespace {
     if (!input.is_quantized() && output_size[0] == 1 && output_size[1] == 1) {
       // in this case, adaptive pooling is just computing mean over hw
       // dimensions, which can be done more efficiently
-      Tensor out = input.mean({input.dim()-1, input.dim()-2}, /* keepdim = */ true);
+      Tensor out = input.mean({-1, -2}, /* keepdim = */ true);
       if (input.suggest_memory_format() == at::MemoryFormat::ChannelsLast) {
         // assert ndim == 4, since ndim = 3 doesn't give channels_last
         const int n = input.size(0);
