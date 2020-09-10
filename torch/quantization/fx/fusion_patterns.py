80b423e069d2911b908b04612aa2ea58659ef152
Jerry Zhang <jerryzh168@gmail.com>
2020-09-10 11:54:28 -0700

    Update on "[quant][eagermode][refactor] Add set/get method for quantization and fusion mappings"
    Summary:
    Allow user to register custom quantization and fusion patterns
    
    Custom quantization API:
    - register_static_quant_module_class
    - get_static_quant_module_mapping
    - get_static_quant_module_class
    
    - register_dynamic_quant_module_class
    - get_dynamic_quant_module_mapping
    
    - register_qat_module_class
    - get_qat_module_mapping
    
    for fx graph mode quantization
    - register_quantized_operator
    - get_quantized_operator
    
    Custom fusion API:
    - register_fuser_method
    - get_fuser_method
    
    Example:
    
    ```
    register_static_quant_module_class(torch.nn.Conv2d, torch.nn.quantized.Conv2d)
    register_dynamic_quant_module_class(torch.nn.Conv2d, torch.nn.quantized.dynamic.Conv2d)
    register_qat_module_class(torch.nn.Conv2d, torch.nn.qat.Conv2d)
    register_fuser_method((torch.nn.Conv2d, torch.nn.BatchNorm2d), fuse_conv_bn)
    
    >> get_static_quant_module_class(torch.nn.Conv2d)
    torch.nn.quantized.Conv2d
    
    >> get_fuser_method((torch.nn.Conv2d, torch.nn.BatchNorm2d))
    fuser_conv_bn
    ```
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D23485344](https://our.internmc.facebook.com/intern/diff/D23485344)
    
    [ghstack-poisoned]

--- torch/quantization/fx/fusion_patterns.py
+++ torch/quantization/fx/fusion_patterns.py
@@ -105,7 +105,5 @@ class ModuleReLUFusion():
         op_type_list = tuple(type(m) for m in op_list)
         module_parent_name, module_name = _parent_name(self.module_node.target)
         fuser_method = get_fuser_method(op_type_list)
-        if fuser_method is None:
-            raise NotImplementedError("Cannot fuse modules: {}".format(types))
         setattr(quantizer.modules[module_parent_name], module_name, fuser_method(*op_list))
         return quantizer.fused_graph.node_copy(self.module_node, load_arg)
