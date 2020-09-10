f12c737748a209778b25e51997f4833f6f7c1047
Eli Uriegas <eliuriegas@fb.com>
2020-09-10 14:33:35 -0700

    Update on "packaging: Add wheel packaging scripts"
    Moves the wheel packaging scripts from their previous home in
    `pytorch/builder` to a place in the main repository.
    
    Should make it easier to test any changes we might need to make to these
    scripts.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- packaging/wheel/cuda_helpers.sh
+++ packaging/wheel/cuda_helpers.sh
@@ -32,8 +32,8 @@ fi
 
 # Searching only in /usr can be problematic but this find command
 # can also fail if it reaches a spot where it gets a permission denied
-LIBGOMP_PATH=$(find /usr -name libgomp.so.1 -type f | head -1)
-if ! find / -name libgomp.so.1 | head -1 >/dev/null 2>/dev/null; then
+LIBGOMP_PATH=$(find /usr -name libgomp.so.1 | head -1)
+if ! find /usr -name libgomp.so.1 | head -1 >/dev/null 2>/dev/null; then
     echo "ERROR: libgomp.so.1 not found, exiting"
     exit 1
 fi
