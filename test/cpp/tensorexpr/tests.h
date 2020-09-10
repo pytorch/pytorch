865b77848763ba5febdcabe6743d6b376ca691ae
Mikhail Zolotukhin <mvz@fb.com>
2020-09-10 15:32:16 -0700

    [Do not commit] Cherry pick PR44231 (cuda error fix 1).
    ghstack-source-id: 8b9640d9a5a4841315bf1945e64d73efbc57aa6d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44515

--- test/cpp/tensorexpr/tests.h
+++ test/cpp/tensorexpr/tests.h
@@ -261,7 +261,6 @@ namespace jit {
   _(FuserPass_UnfusibleDevice)              \
   _(FuserPass_UnknownShapes)                \
   _(FuserPass_Multidevice)                  \
-  _(FuserPass_MergeGroups)                  \
   _(TrainBasic)
 
 #define TH_FORALL_TENSOREXPR_TESTS_LLVM(_) \
