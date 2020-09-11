f3f197c222a9b39bad183813091fb49eaf7028b3
Mikhail Zolotukhin <mvz@fb.com>
2020-09-11 12:56:35 -0700

    Update on "[TensorExpr] Do not inline autodiff graphs if they contain prim::TypeCheck nodes."
    Before this change we sometimes inlined autodiff subgraph containing
    fusion groups. This happened because we didn't look for 'unsupported'
    nodes recursively, but fusion groups were inside if-nodes. With this
    change we start to look into sub-blocks and also block-list
    prim::TypeCheck - if we were to inline those node we would need to
    update the expected types and such logic doesn't exist yet.
    
    The problem was detected by @bertmaher in 'LearningToPaint' benchmark
    investigation where this bug caused us to keep constantly hitting
    fallback paths of the graph.
    
    Differential Revision: [D23657049](https://our.internmc.facebook.com/intern/diff/D23657049)
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/inline_autodiff_subgraphs.cpp
+++ torch/csrc/jit/passes/inline_autodiff_subgraphs.cpp
@@ -13,6 +13,12 @@ namespace jit {
 // Autograd-aware
 bool canRunWithAutograd(Node* node) {
   auto kind = node->kind();
+  for (Block* block : node->blocks()) {
+    if (!std::all_of(
+            block->nodes().begin(), block->nodes().end(), canRunWithAutograd)) {
+      return false;
+    }
+  }
   return kind != prim::FusionGroup && kind != prim::CudaFusionGroup &&
       kind != prim::TypeCheck && kind != prim::TensorExprGroup &&
       (kind.is_aten() || kind.is_prim());
@@ -21,6 +27,17 @@ bool canRunWithAutograd(Node* node) {
 namespace {
 
 void InlineAutodiffSubgraphs(Block* block, size_t threshold);
+
+size_t blockSize(Block* block) {
+  size_t num = 0;
+  for (Node* n : block->nodes()) {
+    for (Block* b : n->blocks()) {
+      num += blockSize(b);
+    }
+    num++;
+  }
+  return num;
+}
 
 graph_node_list::iterator scanNode(Node* node, size_t threshold) {
   auto next_node = ++node->iterator();
@@ -34,9 +51,8 @@ graph_node_list::iterator scanNode(Node* node, size_t threshold) {
   }
 
   auto subgraph = node->g(attr::Subgraph);
-  int64_t subgraph_size =
-      std::distance(subgraph->nodes().begin(), subgraph->nodes().end());
-  if (subgraph_size >= static_cast<int64_t>(threshold)) {
+  size_t subgraph_size = blockSize(subgraph->block());
+  if (subgraph_size >= threshold) {
     return next_node;
   }
 
