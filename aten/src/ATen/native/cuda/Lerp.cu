3e028b05259cdf297a06c18837c08a0bd123c1ac
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-09-10 21:48:43 -0700

    Enable lerp on half type; fix output memory format (#43541)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/43541
    
    Reviewed By: zou3519
    
    Differential Revision: D23499592
    
    Pulled By: ezyang
    
    fbshipit-source-id: 9efdd6cbf0a334ec035ddd467667ba874b892549

--- aten/src/ATen/native/cuda/Lerp.cu
+++ aten/src/ATen/native/cuda/Lerp.cu
@@ -16,7 +16,7 @@ inline void lerp_cuda(at::Tensor& ret, const at::Tensor& self, const at::Tensor&
       .add_input(end)
       .add_input(weights)
       .build();
-  AT_DISPATCH_FLOATING_TYPES(iter.common_dtype(), "lerp_cuda", [&]{
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.common_dtype(), "lerp_cuda", [&]{
     at::native::gpu_kernel(iter,
       [] GPU_LAMBDA (
           scalar_t self_val,
@@ -52,7 +52,7 @@ Tensor& lerp_cuda_tensor_out(Tensor& result, const Tensor& self,
   TORCH_CHECK(weight.dim() <= std::max(self.dim(), end.dim()),
            "weight should be of dimension max(self.dim(), end.dim()) or lesser");
   std::tie(b_self, b_end, b_weight) = expand_outplace(self, end, weight, "lerp_out_cuda");
-  result.resize_as_(b_self);
+  result.resize_as_(b_self, b_self.suggest_memory_format());
   lerp_cuda(result, b_self, b_end, b_weight);
   return result;
 }
@@ -61,8 +61,8 @@ Tensor& lerp_cuda_scalar_out(Tensor& result, const Tensor& self,
                             const Tensor& end, Scalar weight) {
   Tensor b_self, b_end;
   std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_out_cuda");
-  result.resize_as_(b_self);
-  AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp_out_cuda", [&]{
+  result.resize_as_(b_self, b_self.suggest_memory_format());
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(self.scalar_type(), "lerp_out_cuda", [&]{
     lerp_scalar_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
   });
   return result;
@@ -86,7 +86,7 @@ Tensor& lerp_cuda_scalar_(Tensor& self, const Tensor& end, Scalar weight) {
   TORCH_CHECK(b_self.sizes() == self.sizes(),
            "output with shape ", self.sizes(),
            " doesn't match the broadcast shape ", b_self.sizes());
-  AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp__cuda", [&]{
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(self.scalar_type(), "lerp__cuda", [&]{
     lerp_scalar_cuda<scalar_t>(self, b_self, b_end, weight.to<scalar_t>());
   });
   return self;
@@ -97,7 +97,7 @@ Tensor lerp_cuda_tensor(const Tensor& self, const Tensor& end, const Tensor& wei
   TORCH_CHECK(weight.dim() <= std::max(self.dim(), end.dim()),
            "weight should be of dimension max(self.dim(), end.dim()) or lesser");
   std::tie(b_self, b_end, b_weight) = expand_outplace(self, end, weight, "lerp_cuda");
-  Tensor result = at::empty_like(b_self, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
+  Tensor result = at::empty_like(b_self, b_self.suggest_memory_format());
   lerp_cuda(result, b_self, b_end, b_weight);
   return result;
 }
@@ -105,8 +105,8 @@ Tensor lerp_cuda_tensor(const Tensor& self, const Tensor& end, const Tensor& wei
 Tensor lerp_cuda_scalar(const Tensor& self, const Tensor& end, Scalar weight) {
   Tensor b_self, b_end;
   std::tie(b_self, b_end) = expand_outplace(self, end, "lerp_cuda");
-  Tensor result = at::empty_like(b_self, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
-  AT_DISPATCH_FLOATING_TYPES(self.scalar_type(), "lerp_cuda", [&]{
+  Tensor result = at::empty_like(b_self, b_self.suggest_memory_format());
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(self.scalar_type(), "lerp_cuda", [&]{
     lerp_scalar_cuda<scalar_t>(result, b_self, b_end, weight.to<scalar_t>());
   });
   return result;
