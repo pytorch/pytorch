59fc5cc336361d9171cf5156717bb6dd92d7b1e2
taivu <taivu@devvm2522.vll0.facebook.com>
2020-09-11 01:38:45 -0700

    Augment inlined call stack with module type/instance info.
    Summary:
    Motivation:
    During inlinng we lose information about where the node/op is coming from in module
    hierarchy. The SourceDebug info is useful in providing where in the
    source code the definition of the node exist. One can follow the
    callstack to find the relevant module hierarchy. However, this is less
    accessible, particularly in the context of aggregating node level op
    latencies to produce module level latencies. Facilitiating this
    enables identifying which module might be expensive and can be
    targeted either for optimization or redesign. This also aids network
    architecture search approaches.
    Approach:
    This PR adds module type/instance information to inlined call stack
    during inlining. During inlining where the subsequent CallMethods are
    being replaced with their corresponding graph, we find the module
    instance information corresponding to the CallMethod following the
    GetAttr node that produces the module type input to CallMethod.
    This approach has limitations in that such information will not be
    avaiable to CallFunctions and hence we will only be able to annotate
    type information and not instance information.
    But the intuition is that, it should cover majority of the cases. And we
    should be able to address this in another way.
    
    Test Plan:
    python test/test_jit.py TestScopeWithInlinedCallSTack
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/test_jit.py
+++ test/test_jit.py
@@ -21,6 +21,7 @@ from jit.test_class_type import TestClassType  # noqa: F401
 from jit.test_builtins import TestBuiltins, TestTensorBuiltins  # noqa: F401
 from jit.test_unsupported_ops import TestUnsupportedOps  # noqa: F401
 from jit.test_freezing import TestFreezing  # noqa: F401
+from jit.test_scope_with_inlined_callstack import TestScopeWithInlinedCallSTack # noqa: F401
 from jit.test_save_load import TestSaveLoad  # noqa: F401
 from jit.test_module_containers import TestModuleContainers  # noqa: F401
 from jit.test_python_ir import TestPythonIr  # noqa: F401
--- torch/csrc/jit/ir/scope.cpp
+++ torch/csrc/jit/ir/scope.cpp
@@ -90,12 +90,30 @@ InlinedCallStack::InlinedCallStack(Function* fn, SourceRange source_range)
     : fn_(fn), source_range_(std::move(source_range)) {}
 
 InlinedCallStack::InlinedCallStack(
+    Function* fn,
+    SourceRange source_range,
+    c10::optional<ModuleInstanceInfo> module_instance_info)
+    : fn_(fn),
+      source_range_(std::move(source_range)),
+      module_instance_info_(std::move(module_instance_info)){}
+
+InlinedCallStack::InlinedCallStack(
     InlinedCallStackPtr callee,
     Function* fn,
     SourceRange source_range)
     : callee_(std::move(callee)),
       fn_(fn),
       source_range_(std::move(source_range)) {}
+
+InlinedCallStack::InlinedCallStack(
+    InlinedCallStackPtr callee,
+    Function* fn,
+    SourceRange source_range,
+    c10::optional<ModuleInstanceInfo> module_instance_info)
+    : callee_(std::move(callee)),
+      fn_(fn),
+      source_range_(std::move(source_range)),
+      module_instance_info_(std::move(module_instance_info)){}
 
 c10::optional<InlinedCallStackPtr> InlinedCallStack::callee() const {
   return callee_;
@@ -110,5 +128,26 @@ std::vector<InlinedCallStackEntry> InlinedCallStack::vec() {
   }
   return r;
 }
+
+std::vector<InlinedCallStackWithModuleInfo>
+  InlinedCallStack::vec_with_module_info() {
+  std::vector<InlinedCallStackWithModuleInfo> r;
+  c10::optional<InlinedCallStackPtr> current = intrusive_from_this();
+  while (current) {
+    r.emplace_back(
+        std::make_tuple((*current)->fn_,
+          (*current)->source_range_,
+          (*current)->module_instance_info_));
+    current = (*current)->callee_;
+  }
+  return r;
+}
+
+ModuleInstanceInfo::ModuleInstanceInfo(
+    c10::ClassTypePtr module_type,
+    std::string instance_name)
+    : module_type_(module_type),
+      instance_name_(std::move(instance_name)) {}
+
 } // namespace jit
 } // namespace torch
