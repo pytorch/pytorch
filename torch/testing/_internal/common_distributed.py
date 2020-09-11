888e0948d1c809a1b2e3269de397a95ea031607d
Rohan Varma <rvarm1@fb.com>
2020-09-10 17:55:15 -0700

    In common_distributed, fix TEST_SKIPS multiprocessing manager
    Since `TEST_SKIPS` is a global multiprocessing.manager, this was causing
    issues when one test would fail and make the rest of the tests fail during
    setup due to networking errors.
    
    See the failed CI job: https://app.circleci.com/pipelines/github/pytorch/pytorch/212491/workflows/0450151d-ca09-4cf6-863d-272de6ed917f/jobs/7389065 for an example, where `test_ddp_backward` failed but then caused the rest of the tests to fail at the line `test_skips.update(TEST_SKIPS)`.
    
    To fix this issue, at the end of every test we revert `TEST_SKIPS` back to a regular dict, and redo the conversion to a `mulitiprocessing.Manager` in the next test, which prevents these errors.
    
    Differential Revision: [D23641618](https://our.internmc.facebook.com/intern/diff/D23641618/)
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -285,6 +285,7 @@ class MultiProcessTestCase(TestCase):
         test_skips_manager = Manager()
         test_skips = test_skips_manager.dict()
         global TEST_SKIPS
+        self.old_test_skips = TEST_SKIPS.copy()
         test_skips.update(TEST_SKIPS)
         TEST_SKIPS = test_skips
 
@@ -321,41 +322,45 @@ class MultiProcessTestCase(TestCase):
         timeout = get_timeout(self.id())
         start_time = time.time()
         subprocess_error = False
-        while True:
-            # check to see if any subprocess exited with an error early.
-            for (i, p) in enumerate(self.processes):
-                # This is the exit code processes exit with if they
-                # encountered an exception.
-                if p.exitcode == MultiProcessTestCase.TEST_ERROR_EXIT_CODE:
-                    print("Process {} terminated with exit code {}, terminating remaining processes.".format(i, p.exitcode))
-                    active_children = torch.multiprocessing.active_children()
-                    for ac in active_children:
-                        ac.terminate()
-                    subprocess_error = True
+        try:
+            while True:
+                # check to see if any subprocess exited with an error early.
+                for (i, p) in enumerate(self.processes):
+                    # This is the exit code processes exit with if they
+                    # encountered an exception.
+                    if p.exitcode == MultiProcessTestCase.TEST_ERROR_EXIT_CODE:
+                        print("Process {} terminated with exit code {}, terminating remaining processes.".format(i, p.exitcode))
+                        active_children = torch.multiprocessing.active_children()
+                        for ac in active_children:
+                            ac.terminate()
+                        subprocess_error = True
+                        break
+                if subprocess_error:
                     break
-            if subprocess_error:
-                break
-            # All processes have joined cleanly if they all a valid exitcode
-            if all([p.exitcode is not None for p in self.processes]):
-                break
-            # Check if we should time out the test. If so, we terminate each process.
-            elapsed = time.time() - start_time
-            if elapsed > timeout:
-                print(
-                    "Timing out after {} seconds and killing subprocesses.".format(
-                        timeout
+                # All processes have joined cleanly if they all a valid exitcode
+                if all([p.exitcode is not None for p in self.processes]):
+                    break
+                # Check if we should time out the test. If so, we terminate each process.
+                elapsed = time.time() - start_time
+                if elapsed > timeout:
+                    print(
+                        "Timing out after {} seconds and killing subprocesses.".format(
+                            timeout
+                        )
                     )
-                )
-                for p in self.processes:
-                    p.terminate()
-                break
-            # Sleep to avoid excessive busy polling.
-            time.sleep(0.1)
-        elapsed_time = time.time() - start_time
-        if fn in self.skip_return_code_checks:
-            self._check_no_test_errors(elapsed_time)
-        else:
-            self._check_return_codes(elapsed_time)
+                    for p in self.processes:
+                        p.terminate()
+                    break
+                # Sleep to avoid excessive busy polling.
+                time.sleep(0.1)
+            elapsed_time = time.time() - start_time
+            if fn in self.skip_return_code_checks:
+                self._check_no_test_errors(elapsed_time)
+            else:
+                self._check_return_codes(elapsed_time)
+        finally:
+            global TEST_SKIPS
+            TEST_SKIPS = self.old_test_skips
 
     def _check_no_test_errors(self, elapsed_time):
         """
