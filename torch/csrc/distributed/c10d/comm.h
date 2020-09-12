89421e8cb527b63962485fb3ec53b81c7b08a32b
Yi Wang <wayi@fb.com>
2020-09-11 17:59:15 -0700

    Move the inline implementations of GradBucket class to the header. (#44339)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44339
    
    Moved the inline implementations of GradBucket class to the header for
    succinctness and readability. This coding style is also consistent with
    reducer.h under the same directory.
    
    Test Plan: buck test caffe2/torch/lib/c10d:ProcessGroupGlooTest
    
    Reviewed By: pritamdamania87
    
    Differential Revision: D23569701
    
    fbshipit-source-id: 237d9e2c5f63a6bcac829d0fcb4a5ba3bede75e5

--- torch/csrc/distributed/c10d/comm.h
+++ torch/csrc/distributed/c10d/comm.h
@@ -20,12 +20,13 @@ void broadcast_coalesced(
 // mappings as well.
 class GradBucket {
  public:
-  explicit GradBucket(std::vector<at::Tensor> tensors);
+  explicit GradBucket(std::vector<at::Tensor> tensors) :
+    tensors_(std::move(tensors)) {}
   // Each tensor in the list that getTensors returns refers to the replica on
   // each device. There will be multiple replicas only in the case of single
   // process multiple device mode. In the single process single device mode,
   // this list would consist of only a single tensor.
-  const std::vector<at::Tensor>& getTensors() const;
+  const std::vector<at::Tensor>& getTensors() const { return tensors_; }
 
  private:
   std::vector<at::Tensor> tensors_;
