0e71328836065e57e16e5c6a6d5816d2064f3339
Nikitha Malgi <nikithamalgi@fb.com>
2020-09-13 15:30:50 -0700

    Adding support for bitwise augassignment operators

--- torch/jit/frontend.py
+++ torch/jit/frontend.py
@@ -321,6 +321,12 @@ class StmtBuilder(Builder):
         ast.Mult: '*',
         ast.Div: '/',
         ast.Mod: '%',
+        ast.BitOr: '|',
+        ast.BitAnd: '&',
+        ast.BitXor: '^',
+        ast.LShift: '<<',
+        ast.RShift: '>>',
+        ast.Pow: '**',
     }
 
     @staticmethod
