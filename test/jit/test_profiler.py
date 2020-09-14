b8b6f0e5c5b58cfc142d3f23be3dd0df26ce521a
Elias Ellison <eellison@devfair044.maas>
2020-09-14 14:22:38 -0700

    Update on "Fix fallback graph in specialize autogradzero"
    Previously we weren't creating a fallback graph as intended in specialize autograd zero, so if a Tensor failed one of our undefinedness checks we would run the backward normally without reprofiling & optimizing. 
    
    [ghstack-poisoned]

--- test/jit/test_profiler.py
+++ test/jit/test_profiler.py
@@ -178,3 +178,5 @@ class TestProfiler(JitTestCase):
 
         g = torch.jit.last_executed_optimized_graph()
         FileCheck().check("fallback_function").check_next("CallFunction").run(g)
+        
+        
