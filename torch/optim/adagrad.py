6a5f209783692a8b7c3d166bf66803c36142e624
Wanchao Liang <wanchaol@users.noreply.github.com>
2020-09-15 11:51:08 -0400

    [WIP][optimizer] introduce functional API for optimizer
    [ghstack-poisoned]

--- torch/optim/adagrad.py
+++ torch/optim/adagrad.py
@@ -1,4 +1,5 @@
 import torch
+from . import functional as F
 from .optimizer import Optimizer
 
 
@@ -70,33 +71,12 @@ class Adagrad(Optimizer):
                 grad = p.grad
                 state = self.state[p]
 
-                state['step'] += 1
-
-                if group['weight_decay'] != 0:
-                    if p.grad.is_sparse:
-                        raise RuntimeError("weight_decay option is not compatible with sparse gradients")
-                    grad = grad.add(p, alpha=group['weight_decay'])
-
-                clr = group['lr'] / (1 + (state['step'] - 1) * group['lr_decay'])
-
-                if grad.is_sparse:
-                    grad = grad.coalesce()  # the update is non-linear so indices must be unique
-                    grad_indices = grad._indices()
-                    grad_values = grad._values()
-                    size = grad.size()
-
-                    def make_sparse(values):
-                        constructor = grad.new
-                        if grad_indices.dim() == 0 or values.dim() == 0:
-                            return constructor().resize_as_(grad)
-                        return constructor(grad_indices, values, size)
-                    state['sum'].add_(make_sparse(grad_values.pow(2)))
-                    std = state['sum'].sparse_mask(grad)
-                    std_values = std._values().sqrt_().add_(group['eps'])
-                    p.add_(make_sparse(grad_values / std_values), alpha=-clr)
-                else:
-                    state['sum'].addcmul_(grad, grad, value=1)
-                    std = state['sum'].sqrt().add_(group['eps'])
-                    p.addcdiv_(grad, std, value=-clr)
+                F.adagrad(p, grad,
+                          state['step'],
+                          state['sum'],
+                          group['lr'],
+                          group['weight_decay'],
+                          group['lr_decay'],
+                          group['eps'])
 
         return loss
---
+++ torch/optim/functional.py
@@ -0,0 +1,27 @@
+def adagrad(param, grad, step, state_sum, lr, weight_decay, lr_decay, eps): 
+    if weight_decay != 0:
+        if grad.is_sparse:
+            raise RuntimeError("weight_decay option is not compatible with sparse gradients")
+        grad = grad.add(param, alpha=weight_decay)
+
+    clr = lr / (1 + (step - 1) * lr_decay)
+
+    if grad.is_sparse:
+        grad = grad.coalesce()  # the update is non-linear so indices must be unique
+        grad_indices = grad._indices()
+        grad_values = grad._values()
+        size = grad.size()
+
+        def make_sparse(values):
+            constructor = grad.new
+            if grad_indices.dim() == 0 or values.dim() == 0:
+                return constructor().resize_as_(grad)
+            return constructor(grad_indices, values, size)
+        state_sum.add_(make_sparse(grad_values.pow(2)))
+        std = state_sum.sparse_mask(grad)
+        std_values = std._values().sqrt_().add_(eps)
+        param.add_(make_sparse(grad_values / std_values), alpha=-clr)
+    else:
+        state_sum.addcmul_(grad, grad, value=1)
+        std = state_sum.sqrt().add_(eps)
+        param.addcdiv_(grad, std, value=-clr)
