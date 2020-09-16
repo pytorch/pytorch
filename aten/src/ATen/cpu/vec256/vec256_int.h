c82435b910447d0d643e46439f50c6eedef7c0d9
Hong Xu <hong@topbug.net>
2020-09-16 11:31:37 -0700

    Update aten/src/ATen/cpu/vec256/vec256_int.h
    `++ i` --> `++i`

--- aten/src/ATen/cpu/vec256/vec256_int.h
+++ aten/src/ATen/cpu/vec256/vec256_int.h
@@ -691,7 +691,7 @@ public:
     // Ensure uninitialized memory does not change the output value See https://github.com/pytorch/pytorch/issues/32502
     // for more details. We do not initialize arrays to zero using "={0}" because gcc would compile it to two
     // instructions while a loop would be compiled to one instruction.
-    for (size_t i = 0; i < size(); ++ i) {
+    for (size_t i = 0; i < size(); ++i) {
       tmp_values[i] = 0;
     }
     std::memcpy(tmp_values, ptr, count * sizeof(int8_t));
