76e04e78cc4fa905d3948ac36af03ff9b23b236e
Eli Uriegas <eliuriegas@fb.com>
2020-09-17 09:08:03 -0700

    DO NOT MERGE, testing CX11 ABI = 1
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .circleci/cimodel/data/dimensions.py
+++ .circleci/cimodel/data/dimensions.py
@@ -1,10 +1,10 @@
 PHASES = ["build", "test"]
 
 CUDA_VERSIONS = [
-    "92",
-    "101",
+    # "92",
+    # "101",
     "102",
-    "110",
+    # "110",
 ]
 
 ROCM_VERSIONS = [
@@ -14,7 +14,7 @@ ROCM_VERSIONS = [
 GPU_VERSIONS = [None] + ["cuda" + v for v in CUDA_VERSIONS] + ["rocm" + v for v in ROCM_VERSIONS]
 
 STANDARD_PYTHON_VERSIONS = [
-    "3.6",
-    "3.7",
+    # "3.6",
+    # "3.7",
     "3.8",
 ]
