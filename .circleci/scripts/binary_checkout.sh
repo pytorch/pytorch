a66b4147741f767bec3dafee5389d49e2fa0112b
Joe Zhu <zhuganghua_onnet@hotmail.com>
2020-09-17 10:53:02 +0800

    try fix windows libtorch build failure

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -56,8 +56,9 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+retry git clone -q https://github.com/gunandrose4u/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
+git.exe checkout -b jozh/fix_build remotes/origin/jozh/fix_build
 popd
