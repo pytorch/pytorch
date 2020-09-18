cd37f309673dd1b1db6013ab5997564655142b7e
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-09-17 21:56:03 -0700

    save

--- c10/util/BFloat16-inl.h
+++ c10/util/BFloat16-inl.h
@@ -29,16 +29,16 @@ inline C10_HOST_DEVICE BFloat16::operator float() const {
 inline C10_HOST_DEVICE BFloat16::BFloat16(const __nv_bfloat16& value) {
   x = *reinterpret_cast<const unsigned short*>(&value);
 }
-// inline C10_HOST_DEVICE BFloat16::operator __nv_bfloat16() const {
-//   return *reinterpret_cast<const __nv_bfloat16*>(&x);
-// }
+inline C10_HOST_DEVICE BFloat16::operator __nv_bfloat16() const {
+  return *reinterpret_cast<const __nv_bfloat16*>(&x);
+}
 #endif
 
 // CUDA intrinsics
 
 // #if defined(__CUDA_ARCH__) && defined(CUDA_VERSION) && CUDA_VERSION >= 11000
 // inline __device__ BFloat16 __ldg(const BFloat16* ptr) {
-//     return __ldg(reinterpret_cast<const __nv_bfloat16*>(ptr));
+//     return __ldg(reinterpret_cast<const __nv_bfloat16 *>(ptr));
 // }
 // #endif
 
