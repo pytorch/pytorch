8128ab7ca329c2a6bde50b945a89c73da7bceae2
ilia-cher <iliacher@fb.com>
2020-09-18 14:05:43 -0700

    replace callgrind macro control with pybind trampoline

--- setup.py
+++ setup.py
@@ -651,10 +651,9 @@ def configure_extension_build():
         extensions.append(
             Extension(
                 "torch.utils._benchmark.utils.valgrind_wrapper.bindings",
-                ["torch/utils/_benchmark/utils/valgrind_wrapper/control_bindings.cpp"],
+                ["torch/utils/_benchmark/utils/valgrind_wrapper/callgrind_trampoline.cpp"],
                 include_dirs=[
                     "third_party/pybind11/include",
-                    "torch/utils/_benchmark/utils/valgrind_wrapper/",
                 ],
                 language='c++'
             ),
