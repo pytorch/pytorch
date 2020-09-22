2b59731abeb6fe0113c97438832883afcca9a621
Ivan Kobzarev <ivankobzarev@fb.com>
2020-09-21 18:42:36 -0700

    [vulkan][circleci] Disable vulkan wrapper for vulkan ci build and test
    [ghstack-poisoned]

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -123,6 +123,7 @@ fi
 
 if [[ "$BUILD_ENVIRONMENT" != *android* && "$BUILD_ENVIRONMENT" == *vulkan-linux* ]]; then
   export USE_VULKAN=1
+  export USE_VULKAN_WRAPPER=0
   export VULKAN_SDK=/var/lib/jenkins/vulkansdk/
 fi
 
