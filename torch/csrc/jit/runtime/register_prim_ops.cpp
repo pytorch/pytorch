84e02c3dfb6348d274b4d4348e7e87cb47f043e2
razy <razy@devvm1661.atn0.facebook.com>
2020-09-22 14:07:11 -0700

    Update on "Moves prim ops from C10 back to JIT"
    Moves prim ops from C10 back to JIT.
    
    These were originally moved to C10 from JIT in D19237648
    
    Differential Revision: [D23697598](https://our.internmc.facebook.com/intern/diff/D23697598/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D23697598/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/register_prim_ops.cpp
+++ torch/csrc/jit/runtime/register_prim_ops.cpp
@@ -287,8 +287,8 @@ RegisterOperators reg(
              push(stack, val);
            } else {
              std::stringstream error_str;
-             error_str << "could not convert string "
-                       << "to int: '" << s->string() << "'";
+             error_str << "invalid literal for int() "
+                       << "with base 10: '" << s->string() << "'";
              throw std::runtime_error(error_str.str());
            }
          },
@@ -339,8 +339,8 @@ RegisterOperators reg(
              push(stack, b);
            } else {
              std::stringstream error_str;
-             error_str << "invalid literal for int() "
-                       << "with base 10: '" << s->string() << "'";
+             error_str << "could not convert string "
+                       << "to float: '" << s->string() << "'";
              throw std::runtime_error(error_str.str());
            }
          },
