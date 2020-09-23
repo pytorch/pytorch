2c0269483981012dcb4ab319fe902a059e5bdb4f
Dhruv Matani <dhruvbird@fb.com>
2020-09-22 22:19:25 -0700

    [RFC] Add method SelectiveBuildOperator.combine()
    When combining operator lists from multiple models, we want a way to be able to combine individual operators. This diff adds that method and tests for the same.
    
    Differential Revision: [D23862001](https://our.internmc.facebook.com/intern/diff/D23862001/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D23862001/)!
    
    [ghstack-poisoned]

--- tools/codegen/selective_build/operator.py
+++ tools/codegen/selective_build/operator.py
@@ -14,3 +14,22 @@ class SelectiveBuildOperator():
     # use the information to generate code in VariableType_N.cpp for registration
     # of training related operators
     is_used_for_training: bool
+
+    @staticmethod
+    def combine(
+        lhs: 'SelectiveBuildOperator',
+        rhs: 'SelectiveBuildOperator',
+    ) -> 'SelectiveBuildOperator':
+        if str(lhs.name) != str(rhs.name):
+            raise Exception(
+                "Expected both arguments to have the same name, but got '{}' and '{}' instead".format(
+                    str(lhs.name),
+                    str(rhs.name),
+                )
+            )
+
+        return SelectiveBuildOperator(
+            lhs.name,
+            lhs.is_root_operator or rhs.is_root_operator,
+            lhs.is_used_for_training or rhs.is_used_for_training,
+        )
