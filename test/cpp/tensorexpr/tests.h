c9445e4cf6047cb20ddd0f07b4b07a5ce3cf06f3
Bram Wasti <bwasti@fb.com>
2020-09-22 18:15:56 -0700

    [tensorexpr] Add flag to fuse with unknown shapes (#44401)
    Summary:
    This flag simply allows users to get fusion groups that will *eventually* have shapes (such that `getOperation` is a valid).
    
    This is useful for doing early analysis and compiling just in time.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44401
    
    Reviewed By: ZolotukhinM
    
    Differential Revision: D23656140
    
    Pulled By: bwasti
    
    fbshipit-source-id: 9a26c202752399d1932ad7d69f21c88081ffc1e5

--- test/cpp/tensorexpr/tests.h
+++ test/cpp/tensorexpr/tests.h
@@ -291,6 +291,7 @@ namespace jit {
   _(FuserPass_0DimInput)                    \
   _(FuserPass_UnfusibleDevice)              \
   _(FuserPass_UnknownShapes)                \
+  _(FuserPass_UnknownShapesIgnored)         \
   _(FuserPass_Multidevice)                  \
   _(FuserPass_MergeGroups)                  \
   _(TrainBasic)
