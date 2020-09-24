d786d8f72d9d80b4ffe050a38a1db38857d7765a
Xinyu Li <lixinyu@fb.com>
2020-09-24 11:53:58 -0700

    Revert D23753711: [pytorch][PR] Add foreach APIs for binary ops with ScalarList
    Test Plan: revert-hammer
    
    Differential Revision:
    D23753711 (https://github.com/pytorch/pytorch/commit/71d1b5b0e227e407e60c0a3dd6a4caabdcd6c89a)
    
    Original commit changeset: bf3e8c54bc07
    
    fbshipit-source-id: 192692e0d3fff4cade9983db0a1760fedfc9674c

--- tools/autograd/gen_python_functions.py
+++ tools/autograd/gen_python_functions.py
@@ -281,7 +281,6 @@ UNPACK_METHODS = {
     'c10::optional<bool>': 'toBoolOptional',
     'c10::optional<double>': 'toDoubleOptional',
     'c10::optional<ArrayRef<double>>': 'doublelistOptional',
-    'ArrayRef<double>': 'doublelist',
     'IntArrayRef': 'intlist',
     'Scalar': 'scalar',
     'ScalarType': 'scalartype',
