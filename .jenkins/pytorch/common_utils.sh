adebcc545b72eb0dfb64577241806267de643c90
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2020-09-25 12:31:16 -0400

    Update torchvision version to current latest master

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -66,7 +66,7 @@ function get_bazel() {
   chmod +x tools/bazel
 }
 
-TORCHVISION_COMMIT=c2e8a00885e68ae1200eb6440f540e181d9125de
+TORCHVISION_COMMIT=662373f6057bb0d39eaf6e5fde3083639ed93af3
 
 function install_torchvision() {
   # Check out torch/vision at Jun 11 2020 commit
