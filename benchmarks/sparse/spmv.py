b860c1de7450a68adea5934fc54e378006c0b3da
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-09-24 20:33:18 -0500

    update spmv tests

--- benchmarks/sparse/spmv.py
+++ benchmarks/sparse/spmv.py
@@ -1,5 +1,39 @@
 import argparse
 import sys
+import torch
+from .utils import gen_sparse_gcs, gen_sparse_coo, Event
+
+def test_sparse_gcs(m, nnz, test_count):
+    start_timer = Event(enable_timing=True)
+    stop_timer = Event(enable_timing=True)
+
+    gcs = gen_sparse_gcs((m, m), nnz)
+    vector = torch.randn(m, dtype=torch.double)
+
+    times = []
+    for _ in range(test_count):
+        start_timer.record()
+        gcs.matmul(vector)
+        stop_timer.record()
+        times.append(start_timer.elapsed_time(stop_timer))
+
+    return sum(times) / len(times)
+
+def test_sparse_coo(m, nnz, test_count):
+    start_timer = Event(enable_timing=True)
+    stop_timer = Event(enable_timing=True)
+
+    coo = gen_sparse_coo((m, m), nnz)
+    vector = torch.randn(m, dtype=torch.double)
+
+    times = []
+    for _ in range(test_count):
+        start_timer.record()
+        coo.matmul(vector)
+        stop_timer.record()
+        times.append(start_timer.elapsed_time(stop_timer))
+
+    return sum(times) / len(times)    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SpMV")
@@ -8,6 +42,7 @@ if __name__ == "__main__":
     parser.add_argument("--m", default='1000', type=int)
     parser.add_argument("--nnz_ratio", default='0.1', type=float)
     parser.add_argument("--outfile", default='stdout', type=str)
+    parser.add_argument("--test_count", default='10', type=int)
 
     args = parser.parse_args()
 
@@ -16,7 +51,18 @@ if __name__ == "__main__":
     elif args.outfile == 'stderr':
         outfile = sys.stderr
     else:
-        outfile = args.outfile
+        outfile = open(args.outfile, "a")
 
     print(args, file=outfile)
-        
+    test_count = args.test_count
+    m = args.m
+    nnz_ratio = args.nnz_ratio
+
+    nnz = int(nnz_ratio * m * m)
+    if args.format == 'gcs':
+        time = test_sparse_gcs(m, nnz, test_count)
+    elif args.format == 'coo':
+        time = test_sparse_coo(m, nnz, test_count)
+
+    print("format=", args.format, " nnz_ratio=", nnz_ratio, " m=", m,
+          " time=", time, file=outfile)
