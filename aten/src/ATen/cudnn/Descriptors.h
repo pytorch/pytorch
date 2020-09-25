c3a671997d39c8cd9329750a8821fa6730007455
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-09-25 14:40:52 -0700

    remove TORCH_CUDA_API

--- aten/src/ATen/cudnn/Descriptors.h
+++ aten/src/ATen/cudnn/Descriptors.h
@@ -164,7 +164,7 @@ private:
   }
 };
 
-TORCH_CUDA_API std::ostream& operator<<(std::ostream & out, const FilterDescriptor& d);
+std::ostream& operator<<(std::ostream & out, const FilterDescriptor& d);
 
 struct TORCH_CUDA_API ConvolutionDescriptor
   : public Descriptor<cudnnConvolutionStruct,
