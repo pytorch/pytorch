0dbf476fb1b3c803d471add0ae5a2b67586b68e2
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-09-27 22:14:26 -0700

    Revert "fix lint?"
    This reverts commit 252402a13ca7e2ffc96d4a95faad2c60f0d9a2c8.

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -371,9 +371,9 @@ class TestForeach(TestCase):
     @dtypes(*torch.testing.get_all_dtypes())
     def test_bool_scalar(self, device, dtype):
         for N in [30, 300]:
-            for foreach_bin_op, foreach_bin_op_, torch_bin_op in zip(self.foreach_bin_ops, \
-                                                                     self.foreach_bin_ops_, \
-                                                                     self.torch_bin_ops): \
+            for foreach_bin_op, foreach_bin_op_, torch_bin_op in zip(self.foreach_bin_ops,
+                                                                     self.foreach_bin_ops_,
+                                                                     self.torch_bin_ops,):
                 tensors = self._get_test_data(device, dtype, N)
                 scalar = True
 
