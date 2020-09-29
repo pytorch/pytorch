9561198ee0cf2e985925b571a35c089eb49b1a8b
Yi Zhang <zhanyi@microsoft.com>
2020-09-28 19:43:59 -0700

    minor fix

--- caffe2/operators/roi_align_op.cc
+++ caffe2/operators/roi_align_op.cc
@@ -84,7 +84,7 @@ std::vector<BilinearInterpolationParam<T>> MakeBilinearInterpolationParams(
 } // namespace
 
 template <>
-bool RoIAlignOp<float, CPUContext>::RunOnDeviceWithOrderNCHW(
+C10_EXPORT bool RoIAlignOp<float, CPUContext>::RunOnDeviceWithOrderNCHW(
     int64_t N,
     int64_t C,
     int64_t H,
