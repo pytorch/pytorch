56af12265975606e16e222e007872a25640ddd12
Mike Ruberry <mruberry@fb.com>
2020-09-29 04:31:34 -0700

    Revert D23966878: [pytorch][PR] This PR flips a switch to enable PE + TE
    Test Plan: revert-hammer
    
    Differential Revision:
    D23966878 (https://github.com/pytorch/pytorch/commit/dddb685c114a94df01a1919c7238f331fa63fc6d)
    
    Original commit changeset: 2010a0b07c59
    
    fbshipit-source-id: 132556039730fd3e4babd0d7ca8daf9c8d14f728

--- torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
+++ torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
@@ -45,7 +45,7 @@ static std::atomic<bool> executor_mode{true};
 static std::atomic<bool> profiling_mode{false};
 #else
 static std::atomic<bool> executor_mode{true};
-static std::atomic<bool> profiling_mode{true};
+static std::atomic<bool> profiling_mode{false};
 #endif
 
 static std::atomic<size_t> num_profiled_runs{1};
