67d3f3b9b335d2c9386f20d628f9f42334f7730b
Your Name <you@example.com>
2020-09-29 07:19:15 +0000

    Fix cholesky TF32 tests

--- test/test_torch.py
+++ test/test_torch.py
@@ -7767,6 +7767,7 @@ class TestTorchDeviceType(TestCase):
     @skipCUDAIfNoMagma
     @skipCPUIfNoLapack
     @dtypes(torch.float32, torch.float64, torch.complex64, torch.complex128)
+    @tf32_on_and_off(0.01)
     def test_cholesky(self, device, dtype):
         from torch.testing._internal.common_utils import \
             (random_symmetric_pd_matrix,
