f451d06444ce57427fae508ef06b272603c660e1
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-09-29 10:59:20 -0700

    Updated `cholesky_backward` for complex inputs (#45267)
    Summary:
    Updated `cholesky_backward` to work correctly for complex input.
    Note that the current implementation gives the conjugate of what JAX would return. anjali411 is that correct thing to do?
    Ref. https://github.com/pytorch/pytorch/issues/44895
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45267
    
    Reviewed By: bwasti
    
    Differential Revision: D23975269
    
    Pulled By: anjali411
    
    fbshipit-source-id: 9908b0bb53c411e5ad24027ff570c4f0abd451e6

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -715,15 +715,15 @@ Tensor cholesky_backward(Tensor grad, bool upper, Tensor L) {
   // leads to stable gradient updates, and retains symmetry of the updated matrix if it
   // were updated by a gradient based algorithm.
   if (upper) {
-    L = L.transpose(-1, -2);
-    grad = grad.transpose(-1, -2);
+    L = L.transpose(-1, -2).conj();
+    grad = grad.transpose(-1, -2).conj();
   }
   auto L_inverse = std::get<0>(at::triangular_solve(at::eye(L.size(-1), L.options()), L, /*upper=*/false));
-  auto phi = at::matmul(L.transpose(-1, -2), grad);
+  auto phi = at::matmul(L.transpose(-1, -2).conj(), grad);
   phi.tril_().diagonal(/*offset=*/0, /*dim1=*/-2, /*dim2=*/-1).mul_(0.5);
 
-  auto grad_input = at::matmul(at::matmul(L_inverse.transpose(-1, -2), phi), L_inverse);
-  return grad_input.add(grad_input.transpose(-1, -2)).mul_(0.5);  // Symmetrizing the gradient
+  auto grad_input = at::matmul(at::matmul(L_inverse.transpose(-1, -2).conj(), phi), L_inverse);
+  return grad_input.add(grad_input.transpose(-1, -2).conj()).mul_(0.5);  // Symmetrizing the gradient
 }
 
 Tensor cholesky_inverse_backward(Tensor grad, Tensor L, bool upper, Tensor inverse) {
