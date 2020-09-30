88e635c4f922f99f1e044235341234edd40b7eda
Mikhail Zolotukhin <mvz@fb.com>
2020-09-29 17:40:42 -0700

    Update on "[WIP][TensorExpr] Add a tensor expressions tutorial."
    Differential Revision: [D23998787](https://our.internmc.facebook.com/intern/diff/D23998787)
    
    [ghstack-poisoned]

--- test/cpp/tensorexpr/tutorial.cpp
+++ test/cpp/tensorexpr/tutorial.cpp
@@ -5,17 +5,28 @@
 #include <torch/csrc/jit/tensorexpr/expr.h>
 #include <torch/csrc/jit/tensorexpr/ir.h>
 #include <torch/csrc/jit/tensorexpr/ir_printer.h>
+#include <torch/csrc/jit/tensorexpr/loopnest.h>
 #include <torch/csrc/jit/tensorexpr/stmt.h>
 #include <torch/csrc/jit/tensorexpr/tensor.h>
 
 using namespace torch::jit::tensorexpr;
 
 int main(int argc, char* argv[]) {
-  // TODO: Describe what KernelScope is
+  // Memory management for tensor expressions is currently done with memory
+  // arenas. That is, whenever an object is created it registers itself in an
+  // arena and the object is kept alive as long as the arena is alive. When the
+  // arena gets destructed, it deletes all objects registered in it.
+  //
+  // The easiest way to set up a memory arena is to use `KernelScope` class - it
+  // is a resource guard that creates a new arena on construction and restores
+  // the previously set arena on destruction.
+  //
+  // We will create a kernel scope here, and thus we'll set up a mem arena for
+  // the entire tutorial.
   KernelScope kernel_scope;
+
+  // *** Structure of tensor expressions ***
   {
-    // *** Structure of tensor expressions ***
-    //
     // A tensor expression is a tree of expressions. Each expression has a type,
     // and that type defines what sub-expressions it the current expression has.
     // For instance, an expression of type 'Mul' would have a type 'kMul' and
@@ -67,9 +78,9 @@ int main(int argc, char* argv[]) {
     std::cout << "Tensor expression: " << *load.node() << std::endl;
     // Prints: Tensor expression: A[i, j]
   }
+
+  // *** Tensors, Functions, and Placeholders ***
   {
-    // *** Tensors, Functions, and Placeholders ***
-    //
     // A tensor computation is represented by objects of Tensor class and
     // consists of the following pieces:
     //   - domain, which is specified by a Buf expression
@@ -115,21 +126,149 @@ int main(int argc, char* argv[]) {
     std::cout << "Tensor computation: " << *Z << std::endl;
     // Prints: Tensor computation: Tensor Z(i[64], j[32]) = i / j
 
-    BufHandle ttt("A", {64, 32}, kFloat);
-    Placeholder a_buf(ttt);
-    // TODO: Show how reductions are represented and constructed
+    // Tensors might access other tensors and external placeholders in their
+    // expressions. It can be done like so:
+    Placeholder P("P", kFloat, {64, 32});
     Tensor* R = Compute(
-        "Z",
+        "R",
         {{64, "i"}, {32, "j"}},
         [&](const VarHandle& i, const VarHandle& j) {
-          return Z->call(i, j) * Load::make(ttt, {i, j}, 1);
+          return Z->call(i, j) * P.load(i, j);
         });
     std::cout << "Tensor computation: " << *R << std::endl;
+    // Prints: Tensor computation: Tensor R(i[64], j[32]) = Z(i, j) * P[i, j]
+
+    // Placeholders could be thought of as external tensors, i.e. tensors for
+    // which we don't have the element expression.
+    // Also note that we use 'call' to construct an access to an element of a
+    // Tensor and we use 'load' for accessing elements of an external tensor
+    // through its Placeholder. This is an implementation detail and could be
+    // changed in future.
+
+    // TODO: Show how reductions are represented and constructed
   }
-  // TODO: Describe statements
-  // TODO: Describe Loop Nests and loop transformations
+
+  // *** Loopnests and Statements ***
+  {
+    // Creating a tensor expression is the first step to generate an executable
+    // code for it. A next step is to represent it as a loop nest and apply
+    // various loop transformations in order to get an optimal implementation.
+    // In Halide's or TVM's terms the first step was to define the algorithm of
+    // computation (what to compute?) and now we are getting to the schedule of
+    // the computation (how to compute?).
+    //
+    // Let's create a simple tensor expression and construct a loop nest for it.
+    Placeholder A("A", kFloat, {64, 32});
+    Placeholder B("B", kFloat, {64, 32});
+    Tensor* X = Compute(
+        "X",
+        {{64, "i"}, {32, "j"}},
+        [&](const VarHandle& i, const VarHandle& j) {
+          return A.load(i, j) + B.load(i, j);
+        });
+    Tensor* Y = Compute(
+        "Y",
+        {{64, "i"}, {32, "j"}},
+        [&](const VarHandle& i, const VarHandle& j) {
+          return sigmoid(X->call(i, j));
+        });
+    std::cout << "Tensor computation X: " << *X
+              << "Tensor computation Y: " << *Y << std::endl;
+    // Prints:
+    // Tensor computation X: Tensor X(i[64], j[32]) = (A[i, j]) + (B[i, j])
+    // Tensor computation Y: Tensor Y(i[64], j[32]) = sigmoid(X(i, j))
+
+    // Creating a loop nest is as quite simple, we just need to specify what are
+    // the output tensors in our computation and LoopNest object will
+    // automatically pull all tensor dependencies:
+    LoopNest loopnest({Y});
+
+    // An IR used in LoopNest is based on tensor statements, represented by
+    // `Stmt` class. Statements are used to specify the loop nest structure, and
+    // to take a sneak peek at them, let's print out what we got right after
+    // creating our LoopNest object:
+    std::cout << *loopnest.root_stmt() << std::endl;
+    // Prints:
+    // {
+    //   for (int i = 0; i < 64; i++) {
+    //     for (int j = 0; j < 32; j++) {
+    //       X[i, j] = (A[i, j]) + (B[i, j]);
+    //     }
+    //   }
+    //   for (int i_1 = 0; i_1 < 64; i_1++) {
+    //     for (int j_1 = 0; j_1 < 32; j_1++) {
+    //       Y[i_1, j_1] = sigmoid(X(i_1, j_1));
+    //     }
+    //   }
+    // }
+
+    // To introduce statements let's first look at their three main types (in
+    // fact, there are more than 3 types, but the other types would be easy to
+    // understand once the overall structure is clear):
+    //  1) Block
+    //  2) For
+    //  3) Store
+    //
+    // A `Block` statement is simply a list of other statements.
+    // A `For` is a statement representing one axis of computation. It contains
+    // an index variable (Var), boundaries of the axis (start and end - both are
+    // `Expr`s), and a `Block` statement body.
+    // A `Store` represents an assignment to a tensor element. It contains a Buf
+    // representing the target tensor, a list of expressions for indices of the
+    // element, and the value to be stored, which is an arbitrary expression.
+
+    // Once we've constructed the loop nest, we can apply various tranformations
+    // to it. To begin with, let's inline computation of X into computation of Y
+    // and see what happens to our statements.
+    loopnest.computeInline(loopnest.getLoopBodyFor(X));
+    std::cout << *loopnest.root_stmt() << std::endl;
+    // Prints:
+    // {
+    //   for (int i = 0; i < 64; i++) {
+    //     for (int j = 0; j < 32; j++) {
+    //       Y[i, j] = sigmoid((A[i, j]) + (B[i, j]));
+    //     }
+    //   }
+    // }
+    //
+    // As you can see, the first two loops have disappeared and the expression
+    // for X[i,j] has been inserted into the Y[i,j] computation.
+
+    // Loop transformations can be composed, so we can do something else with
+    // our loop nest now. Let's split the inner loop with a factor of 9, for
+    // instance.
+    std::vector<For*> loops = loopnest.getLoopStmtsFor(Y);
+    For* j_outer;
+    For* j_inner;
+    For* j_tail;
+    loopnest.splitWithTail(
+        loops[1], // loops[0] is the outer loop, loops[1] is inner
+        9,
+        &j_outer, // These are handles that we would be using for
+        &j_inner, // further transformations
+        &j_tail);
+    std::cout << *loopnest.root_stmt() << std::endl;
+    // Prints:
+    // {
+    //   for (int i = 0; i < 64; i++) {
+    //     for (int j_outer = 0; j_outer < (32 - 0) / 9; j_outer++) {
+    //       for (int j_inner = 0; j_inner < 9; j_inner++) {
+    //         Y[i, j_outer * 9 + j_inner] = sigmoid((A[i, j_outer * 9 +
+    //         j_inner]) + (B[i, j_outer * 9 + j_inner]));
+    //       }
+    //     }
+    //     for (int j_tail = 0; j_tail < (32 - 0) % 9; j_tail++) {
+    //       Y[i, j_tail + ((32 - 0) / 9) * 9] = sigmoid((A[i, j_tail + ((32 -
+    //       0) / 9) * 9]) + (B[i, j_tail + ((32 - 0) / 9) * 9]));
+    //     }
+    //   }
+    // }
+
+    // TODO: List all available transformations
+    // TODO: Show how statements can be constructed manually
+  }
+
   // TODO: Describe codegen
-  //
   // TODO: Show how TorchScript IR is translated to TE
   return 0;
 }
