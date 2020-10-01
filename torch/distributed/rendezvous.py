3cc39409ecde897da12bfad42ae24c3fa47f89b8
xzhao <i@xuzhao.net>
2020-10-01 17:23:22 -0400

    Fix run_test errors on various platforms.

--- torch/distributed/rendezvous.py
+++ torch/distributed/rendezvous.py
@@ -11,11 +11,11 @@ import sys
 from datetime import timedelta
 from typing import Optional, Dict, Union
 
-from . import FileStore
+from torch._C._distributed_c10d import FileStore
 from .constants import default_pg_timeout
 
 if sys.platform != 'win32':
-    from . import TCPStore
+    from torch._C._distributed_c10d import TCPStore
 
 _rendezvous_handlers = {}
 
