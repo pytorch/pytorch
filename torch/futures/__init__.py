7d543ebd112d760991e20c5b7f7b5519d2ab9ed5
Brian Hirsh <hirsheybar@fb.com>
2020-10-02 07:44:10 -0700

    Update on "fix #45552 - adding add_done_callback(fn) to torch.futures.Future"
    Differential Revision: [D24055353](https://our.internmc.facebook.com/intern/diff/D24055353)
    
    [ghstack-poisoned]

--- torch/futures/__init__.py
+++ torch/futures/__init__.py
@@ -82,7 +82,7 @@ class Future(torch._C.Future, Generic[T], metaclass=_PyFutureMeta):
         return cast(Future[S], super().then(callback))
 
     # Have to use string annotations because  PEP-0563 is not available in 3.6
-    def add_done_callback(self, callback): # type: (Callable[[Future[T]], None]) -> None
+    def add_done_callback(self, callback):  # type: (Callable[[Future[T]], None]) -> None
         r"""
         Add a callback to this ``Future``.
         Multiple callbacks can be added to the same ``Future``, and all
