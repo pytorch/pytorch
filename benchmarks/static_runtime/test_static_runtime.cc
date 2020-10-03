2b48dd168d846c864d4e12f9d66dc61be67fa811
Hao Lu <hlu@fb.com>
2020-10-02 23:01:09 -0700

    [StaticRuntime] Integrate Static Runtime into PyTorchPredictor (#45640)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/45640
    
    Reviewed By: dzhulgakov
    
    Differential Revision: D23996656
    
    fbshipit-source-id: 63d88c89d1df61a04deadc472319607ed83867e5

--- benchmarks/static_runtime/test_static_runtime.cc
+++ benchmarks/static_runtime/test_static_runtime.cc
@@ -44,3 +44,55 @@ TEST(StaticRuntime, DeepWide) {
     }
   }
 }
+
+TEST(StaticRuntime, KWargsAPI_1) {
+  const int embedding_size = 32;
+  const int num_features = 50;
+  auto module = getDeepAndWideSciptModel();
+  torch::jit::StaticRuntime runtime(module);
+
+  for (int batch_size : {1, 8, 32}) {
+    for (int i = 0; i < 5; ++i) {
+      auto ad_emb_packed = torch::randn({batch_size, 1, embedding_size});
+      auto user_emb = torch::randn({batch_size, 1, embedding_size});
+      auto wide = torch::randn({batch_size, num_features});
+
+      // run jit graph executor
+      std::vector<at::IValue> inputs({ad_emb_packed, user_emb, wide});
+      at::Tensor output_1 = module.forward(inputs).toTensor();
+
+      // run static runtime
+      at::Tensor output_2 = runtime.run(inputs, {}).toTensor();
+      EXPECT_TRUE(output_1.equal(output_2));
+    }
+  }
+}
+
+TEST(StaticRuntime, KWargsAPI_2) {
+  const int embedding_size = 32;
+  const int num_features = 50;
+  auto module = getDeepAndWideSciptModel();
+  auto g = torch::jit::PrepareForStaticRuntime(module);
+  torch::jit::StaticRuntime runtime(module);
+
+  for (int batch_size : {1, 8, 32}) {
+    for (int i = 0; i < 5; ++i) {
+      auto ad_emb_packed = torch::randn({batch_size, 1, embedding_size});
+      auto user_emb = torch::randn({batch_size, 1, embedding_size});
+      auto wide = torch::randn({batch_size, num_features});
+
+      // run jit graph executor
+      std::vector<at::IValue> args({ad_emb_packed, user_emb, wide});
+      at::Tensor output_1 = module.forward(args).toTensor();
+
+      std::unordered_map<std::string, c10::IValue> kwargs(
+          {{"ad_emb_packed", ad_emb_packed},
+           {"user_emb", user_emb},
+           {"wide", wide}});
+
+      // run static runtime
+      at::Tensor output_2 = runtime.run({}, kwargs).toTensor();
+      EXPECT_TRUE(output_1.equal(output_2));
+    }
+  }
+}
