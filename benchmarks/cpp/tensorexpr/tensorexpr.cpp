8b05b1f0568692cbd9fdf018a43813a25bdeb484
Bert Maher <bertrand@fb.com>
2020-10-06 16:51:59 -0700

    [te] Tiled (m=32 x n=32) gemm benchmark (#45905)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/45905
    
    Test Plan: Imported from OSS
    
    Reviewed By: SplitInfinity
    
    Differential Revision: D24142402
    
    Pulled By: bertmaher
    
    fbshipit-source-id: b39e18b6985ee1c1f654fba4498ed91ff14d8d5f

--- benchmarks/cpp/tensorexpr/tensorexpr.cpp
+++ benchmarks/cpp/tensorexpr/tensorexpr.cpp
@@ -1,8 +1,8 @@
 #include <benchmark/benchmark.h>
-#include "torch/torch.h"
 #include "torch/csrc/jit/tensorexpr/ir_simplifier.h"
 #include "torch/csrc/jit/tensorexpr/loopnest.h"
 #include "torch/csrc/jit/tensorexpr/tensor.h"
+#include "torch/torch.h"
 
 namespace te = torch::jit::tensorexpr;
 
@@ -18,9 +18,9 @@ class Gemm : public benchmark::Fixture {
   }
 
   void TearDown(benchmark::State& state) {
-    state.counters["GFLOPS"] =
-      benchmark::Counter(uint64_t(state.iterations()) * 2 * M * N * K,
-                         benchmark::Counter::kIsRate);
+    state.counters["GFLOPS"] = benchmark::Counter(
+        uint64_t(state.iterations()) * 2 * M * N * K,
+        benchmark::Counter::kIsRate);
   }
 
   int M;
@@ -46,9 +46,9 @@ BENCHMARK_DEFINE_F(Gemm, TensorExprNoopt)(benchmark::State& state) {
       "gemm",
       {{M, "M"}, {N, "N"}},
       te::Sum(),
-      [&](const te::ExprHandle& m, const te::ExprHandle& n, const te::ExprHandle& k) {
-        return AP.load(m, k) * BP.load(k, n);
-      },
+      [&](const te::ExprHandle& m,
+          const te::ExprHandle& n,
+          const te::ExprHandle& k) { return AP.load(m, k) * BP.load(k, n); },
       {{K, "K"}});
   te::LoopNest loop({CT});
   loop.prepareForCodegen();
@@ -61,7 +61,72 @@ BENCHMARK_DEFINE_F(Gemm, TensorExprNoopt)(benchmark::State& state) {
   }
 }
 
+BENCHMARK_DEFINE_F(Gemm, TensorExprTile32x32)(benchmark::State& state) {
+  te::KernelScope ks;
+
+  te::Placeholder AP(te::BufHandle("A", {M, K}, te::kFloat));
+  te::Placeholder BP(te::BufHandle("B", {K, N}, te::kFloat));
+  te::Tensor* CT = te::Reduce(
+      "gemm",
+      {{M, "M"}, {N, "N"}},
+      te::Sum(),
+      [&](const te::ExprHandle& m,
+          const te::ExprHandle& n,
+          const te::ExprHandle& k) { return AP.load(m, k) * BP.load(k, n); },
+      {{K, "K"}});
+  te::LoopNest loop({CT});
+
+  {
+    auto const& loops = loop.getLoopStmtsFor(CT);
+    te::For* m = loops[0];
+    te::For* mo;
+    te::For* mi;
+    loop.splitWithMask(m, 32, &mo, &mi);
+  }
+  {
+    auto const& loops = loop.getLoopStmtsFor(CT);
+    te::For* n = loops[2];
+    te::For* no;
+    te::For* ni;
+    loop.splitWithMask(n, 32, &no, &ni);
+  }
+  // mo, mi, no, ni, k ->
+  // mo, no, mi, ni, k
+  {
+    auto const& loops = loop.getLoopStmtsFor(CT);
+    te::For* mi = loops[1];
+    te::For* no = loops[2];
+    loop.reorderAxis(mi, no);
+  }
+  // mo, no, mi, ni, k ->
+  // mo, no, mi, k, ni
+  {
+    auto const& loops = loop.getLoopStmtsFor(CT);
+    te::For* ni = loops[3];
+    te::For* k = loops[4];
+    loop.reorderAxis(ni, k);
+  }
+  // mo, no, mi, k, ni ->
+  // mo, no, k, mi, ni
+  {
+    auto const& loops = loop.getLoopStmtsFor(CT);
+    te::For* mi = loops[2];
+    te::For* k = loops[3];
+    loop.reorderAxis(mi, k);
+  }
+
+  loop.prepareForCodegen();
+  te::Stmt* s = loop.root_stmt();
+  s = te::IRSimplifier::simplify(s);
+  auto cg = CreateCodeGen("llvm_codegen", s, {AP, BP, CT});
+
+  for (auto _ : state) {
+    cg->call({A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>()});
+  }
+}
+
 BENCHMARK_REGISTER_F(Gemm, Torch)->Args({128, 128, 128});
 BENCHMARK_REGISTER_F(Gemm, TensorExprNoopt)->Args({128, 128, 128});
+BENCHMARK_REGISTER_F(Gemm, TensorExprTile32x32)->Args({128, 128, 128});
 
 BENCHMARK_MAIN();
