bb403bdb136fcab92dcb31235871c00577de0711
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-10-06 14:02:26 -0500

    update type annotation

--- torch/nn/modules/batchnorm.py
+++ torch/nn/modules/batchnorm.py
@@ -109,8 +109,8 @@ class _BatchNorm(_NormBase):
 
         if self.training and self.track_running_stats:
             # TODO: if statement only here to tell the jit to skip emitting this when it is None
-            if self.num_batches_tracked is not None:  # type: ignore[has-type]
-                self.num_batches_tracked = self.num_batches_tracked + 1  # type: ignore[has-type]
+            if self.num_batches_tracked is not None:  # type: ignore
+                self.num_batches_tracked = self.num_batches_tracked + 1  # type: ignore
                 if self.momentum is None:  # use cumulative moving average
                     exponential_average_factor = 1.0 / float(self.num_batches_tracked)
                 else:  # use exponential moving average
