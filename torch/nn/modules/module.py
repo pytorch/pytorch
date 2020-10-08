0c4642d7a855028250d12175322ad9c4a6652f90
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-10-08 04:28:22 -0500

    Remove extra space

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -606,7 +606,7 @@ class Module:
             if dtype.is_complex:
                 warnings.warn(
                     "Complex modules are a new feature, and some modules might not work as expected "
-                    "when using complex tensors as parameters or buffers. ")
+                    "when using complex tensors as parameters or buffers.")
 
         def convert(t):
             if convert_to_format is not None and t.dim() == 4:
