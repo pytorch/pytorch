262e9bfabef6edebf9f25530b2cb9b60a54a706b
Supriya Rao <supriyar@fb.com>
2020-10-08 09:56:18 -0700

    Update on "[quant][pyper] Set sparse to False for embedding_bag ops in graph mode"
    Summary:
    The current sparse field using in the float module is for sparse gradients, which is not applicable
    to inference. The sparse field in the quantizd ops denotes pruned weights.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeDynamicJitOps.test_embedding_bag
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D24176543](https://our.internmc.facebook.com/intern/diff/D24176543)
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
+++ torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
@@ -342,7 +342,9 @@ Node* insertEmbeddingBagOps(Node* observer, const std::string& op_name) {
     for (auto i = 1; i < inputs_size - 1; ++i) {
       qembedding_bag_inputs.push_back(embedding_bag_inputs[i]);
     }
-    // Set the sparse field to 0 for inference.
+    // The sparse field in the float operator denotes sparse gradients.
+    // For inference this stands for pruned weights. We currently don't support
+    // pruning in graph mode API so we set the field to 0 for inference.
     qembedding_bag_inputs[5] = sparse_const;
   } else {
     TORCH_CHECK(
@@ -353,7 +355,7 @@ Node* insertEmbeddingBagOps(Node* observer, const std::string& op_name) {
     qembedding_bag_inputs.push_back(
         embedding_bag_inputs[6]); // scale_grad_by_freq
     qembedding_bag_inputs.push_back(zero); // mode
-    qembedding_bag_inputs.push_back(sparse_const); // sparse
+    qembedding_bag_inputs.push_back(sparse_const); // pruned_weights
     qembedding_bag_inputs.push_back(
         embedding_bag_inputs[9]); // per_sample_weights
   }
