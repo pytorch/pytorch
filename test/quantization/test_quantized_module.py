31888b2e7749e4082a151151bb7010d1d28f2183
Supriya Rao <supriyar@fb.com>
2020-10-08 16:11:18 -0700

    [quant][pyper] Rename the sparse argument for embedding_bag ops (#46003)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46003
    
    sparse is confusing because itt is used in training for sparse gradients
    
    Test Plan: Imported from OSS
    
    Reviewed By: radkris-git, qizzzh
    
    Differential Revision: D24178248
    
    fbshipit-source-id: 0a2b595f3873d33b2ce25839b6eee31d2bfd3b0d

--- test/quantization/test_quantized_module.py
+++ test/quantization/test_quantized_module.py
@@ -747,7 +747,7 @@ class TestStaticQuantizedModule(QuantizationTestCase):
         module_out = qemb(indices)
 
         # Call the qembedding operator directly
-        ref = torch.ops.quantized.embedding_byte(w_packed, indices, sparse=False)
+        ref = torch.ops.quantized.embedding_byte(w_packed, indices, pruned_weights=False)
         self.assertEqual(module_out, ref)
         self.checkEmbeddingSerialization(qemb, num_embeddings, embedding_dim, indices, None, set_qconfig=False, is_emb_bag=False)
 
