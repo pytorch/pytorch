bc9d67a6a4653ba9ed176693d4dce0d43327729e
Yi Zhang <zhanyi@microsoft.com>
2020-10-10 05:35:23 -0700

    minor change

--- .circleci/cimodel/data/windows_build_definitions.py
+++ .circleci/cimodel/data/windows_build_definitions.py
@@ -131,6 +131,8 @@ WORKFLOW_DATA = [
     WindowsJob(None, _VC2019, CudaVersion(10, 1)),
     WindowsJob(1, _VC2019, CudaVersion(10, 1)),
     WindowsJob(2, _VC2019, CudaVersion(10, 1)),
+    # temp support caffe2 ops test
+    WindowsJob(3, _VC2019, CudaVersion(10, 1)),
     # VS2019 CUDA-11.0
     WindowsJob(None, _VC2019, CudaVersion(11, 0)),
     WindowsJob(1, _VC2019, CudaVersion(11, 0), master_only_pred=TruePred),
@@ -140,8 +142,6 @@ WORKFLOW_DATA = [
     WindowsJob(1, _VC2019, None, master_only_pred=TruePred),
     WindowsJob(2, _VC2019, None, master_only_pred=TruePred),
     WindowsJob(1, _VC2019, CudaVersion(10, 1), force_on_cpu=True, master_only_pred=TruePred),
-    # temp support caffe2 ops test
-    WindowsJob(3, _VC2019, None, master_only_pred=TruePred),
 ]
 
 
