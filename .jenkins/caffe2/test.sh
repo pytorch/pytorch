55ebf734e1260e52d7bf7c4e25570b105c321785
Yi Zhang <zhanyi@microsoft.com>
2020-10-11 19:39:41 -0700

    revert common.sh

--- .jenkins/caffe2/test.sh
+++ .jenkins/caffe2/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# shellcheck disable=SC2034
+COMPACT_JOB_NAME="${BUILD_ENVIRONMENT}"
+
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 # Skip tests in environments where they are not built/applicable
