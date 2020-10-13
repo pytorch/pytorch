2719ba8d6c2e9dbfb936eb9f30518e72714e202e
Yuxin Wu <ppwwyyxxc@gmail.com>
2020-10-13 10:41:37 -0700

    Update TensorIndexing.h

--- aten/src/ATen/TensorIndexing.h
+++ aten/src/ATen/TensorIndexing.h
@@ -341,6 +341,8 @@ static inline IntArrayRef slicePrefix1sSize(const IntArrayRef& sizes) {
 static inline void copy_to(const Tensor& dst, const Tensor& src) {
   if (dst.sizes().equals(src.sizes())) {
     // A shortcut to avoid generating hard-coded constant sizes during tracing.
+    // This is not a perfect solution: when src & dst have different shapes, constants will still
+    // appear. Users can workaround that case by dst[index..] = src.reshape(..)
     dst.copy_(src);
     return;
   }
