df0172e78593c86e35d9790da21d4ead3d1033ec
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-10-14 05:58:05 -0500

    Added a docstring for random_hermitian_pd_matrix

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1528,9 +1528,14 @@ def random_symmetric_pd_matrix(matrix_size, *batch_dims, **kwargs):
         + torch.eye(matrix_size, dtype=dtype, device=device) * 1e-5
 
 
-def random_hermitian_pd_matrix(matrix_size, *batch_dims, **kwargs):
-    dtype = kwargs.get('dtype', torch.double)
-    device = kwargs.get('device', 'cpu')
+def random_hermitian_pd_matrix(matrix_size, *batch_dims, dtype, device):
+    """
+    Returns a batch of random Hermitian positive-definite matrices.
+    The shape of the result is batch_dims + (matrix_size, matrix_size)
+
+    The following example creates a tensor of size 2 x 4 x 3 x 3
+    >>> matrices = random_hermitian_pd_matrix(3, 2, 4, dtype=dtype, device=device)
+    """
     A = torch.randn(*(batch_dims + (matrix_size, matrix_size)),
                     dtype=dtype, device=device)
     return torch.matmul(A, A.transpose(-2, -1).conj())
