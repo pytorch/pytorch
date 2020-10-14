pytorch/pytorch

commit 0b22318742d594619c044e674eb4800eee99c4a1

[caffe2] Enable fp16 for SparseNormalize op (#45551)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/45551

The FP16 version of SparseNormalize op in Caffe2 is missing. This Diff adds FP16 support to unblock MC process of adding FP16 to Dper3.

Check https://fb.quip.com/L0T2AXGwUY3n#EReACAeifk3 .

One question is whether the pure FP16 Sparse Normalized op will affect the accuracy? Maybe we should do it in FP32 domain.
ghstack-source-id: 114184398

Test Plan:
```
 buck run mode/opt //caffe2/caffe2/python/operator_test:sparse_normalize_test
```

```
buck run mode/opt -c python.package_style=inplace mode/no-gpu //caffe2/caffe2/python/benchmarks:sparse_normalize_benchmark -- --fp16
```

Reviewed By: jspark1105

Differential Revision: D24005618

fbshipit-source-id: 8b918ec4063fdaafa444779b95206ba2b7b38537

caffe2/operators/sparse_normalize_op.cc
@@ -2,6 +2,12 @@
 #include "caffe2/core/tensor.h"
 #include "caffe2/utils/eigen_utils.h"
 
+#include "caffe2/utils/cpuid.h"
+
+#ifdef USE_FBGEMM
+#include "fbgemm/FbgemmConvert.h"
+#endif
+
 namespace caffe2 {
 
 template <>
@@ -46,7 +52,66 @@ bool SparseNormalizeOp<float, CPUContext>::DoRunWithType() {
   return true;
 }
 
+template <>
+bool SparseNormalizeOp<c10::Half, CPUContext>::RunOnDevice() {
+  return DispatchHelper<TensorTypes<int32_t, int64_t>>::call(
+      this, Input(INDICES));
+}
+
+inline void Float16ToFloat_ref(const at::Half* in, float* out, size_t N) {
+  for (size_t i = 0; i < N; ++i) {
+    out[i] = in[i];
+  }
+}
+
+template <>
+template <typename SIndex>
+bool SparseNormalizeOp<c10::Half, CPUContext>::DoRunWithType() {
+  const auto* indices = Input(INDICES).template data<SIndex>();
+  const auto* paramIn = Input(PARAM).template data<c10::Half>();
+  auto* paramOut = Output(OUTPUT_PARAM)->template mutable_data<c10::Half>();
+  const float kEps = 1e-12f;
+
+  // n: number of sparse embeddings to be normalized
+  auto n = Input(INDICES).numel();
+  if (n == 0) {
+    return true;
+  }
+  // embedding length, e.g. 32, 64, 128
+  auto block_size = Input(PARAM).size_from_dim(1);
+  vector<float> row_vec_fp32(block_size);
+  auto out_data = row_vec_fp32.data();
+  for (int i = 0; i < n; ++i) {
+    auto idx = indices[i];
+    auto offsetIdx = idx * block_size;
+#ifdef USE_FBGEMM
+    if (GetCpuId().avx2()) {
+      fbgemm::Float16ToFloat_avx2(
+          reinterpret_cast<const fbgemm::float16*>(paramIn + offsetIdx),
+          out_data,
+          block_size);
+    } else {
+      Float16ToFloat_ref(paramIn + offsetIdx, out_data, block_size);
+    }
+#else
+    Float16ToFloat_ref(paramIn + offsetIdx, out_data, block_size);
+#endif
+    ConstEigenVectorMap<float> xVec_fp32(row_vec_fp32.data(), block_size);
+    float norm = xVec_fp32.template lpNorm<2>();
+    if (use_max_norm_ && norm <= norm_) {
+      continue;
+    }
+    auto Y = paramOut + offsetIdx;
+    EigenVectorArrayMap<c10::Half>(Y, block_size) *=
+        static_cast<float>(norm_ / (norm + kEps));
+  }
+  return true;
+}
+
 REGISTER_CPU_OPERATOR(SparseNormalize, SparseNormalizeOp<float, CPUContext>);
+REGISTER_CPU_OPERATOR(
+    Float16SparseNormalize,
+    SparseNormalizeOp<c10::Half, CPUContext>);
 OPERATOR_SCHEMA(SparseNormalize)
     .NumInputs(2, 3)
     .NumOutputs(1)

caffe2/python/benchmarks/sparse_normalize_benchmark.py
@@ -1,0 +1,121 @@
+import argparse
+import datetime
+
+# import hypothesis.strategies as st
+import numpy as np
+from caffe2.python import core, workspace
+
+
+def benchmark_sparse_normalize(
+    categorical_limit,
+    embedding_size,
+    average_len,
+    batch_size,
+    iterations,
+    flush_cache,
+    fp16,
+):
+    print("Preparing lookup table. " + str(datetime.datetime.now()))
+
+    # We will use a constant, but non-trivial value so we save initialization
+    # time.
+    data = np.ones([categorical_limit, embedding_size], dtype=np.float32)
+    data *= 17.01
+
+    init_net = core.Net("init_net")
+    if fp16:
+        op = core.CreateOperator("FloatToHalf", "X", "X_fp16")
+        init_net.Proto().op.extend([op])
+    l3_cache_size = 30 * 2 ** 20 // 4
+
+    # In order to produce truly random lengths and indices, we will embed a
+    # Python operator in the net to generate them.
+    def f(_, outputs):
+        lengths = np.random.randint(
+            int(average_len * 0.75), int(average_len * 1.25), batch_size
+        ).astype(np.int32)
+        indices = np.random.randint(0, categorical_limit, np.sum(lengths)).astype(
+            np.int64
+        )
+        outputs[0].feed(indices)
+
+    workspace.FeedBlob("X", data)
+    workspace.FeedBlob("huge_blob", np.random.randn(l3_cache_size).astype(np.float32))
+
+    print("Data has shape {} {}".format(data.shape, datetime.datetime.now()))
+
+    init_net.Python(f)([], ["indices"])
+    workspace.RunNetOnce(init_net)
+
+    net = core.Net("mynet")
+    op = core.CreateOperator(
+        "Float16SparseNormalize" if fp16 else "SparseNormalize",
+        ["X_fp16", "indices"] if fp16 else ["X", "indices"],
+        "X_fp16" if fp16 else "X",
+    )
+    net.Proto().external_input.append("X")
+    net.Proto().external_input.append("X_fp16")
+    net.Proto().external_input.append("indices")
+    net.Proto().op.extend([op])
+    if flush_cache:
+        net.Scale("huge_blob", "huge_blob_2x", value=2.0)
+
+    workspace.CreateNet(net)
+
+    # Set random seed, so that repeated runs will keep the same sequence of
+    # random indices.
+    np.random.seed(1701)
+
+    print("Preparation finished. " + str(datetime.datetime.now()))
+
+    runtimes = workspace.BenchmarkNet(net.Name(), 1, iterations, True)
+
+    print("{} ms".format(runtimes[2 if flush_cache else 1]))
+    print("indice_size: " + str(workspace.FetchBlob("indices").size))
+    print(
+        "{} GB/sec".format(
+            (2 if fp16 else 4)
+            * embedding_size
+            * workspace.FetchBlob("indices").size
+            / runtimes[2 if flush_cache else 1]
+            / 1e6
+        )
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="minimal benchmark for sparse lengths sum."
+    )
+    parser.add_argument(
+        "-e", "--embedding-size", type=int, default=600000, help="Lookup table size."
+    )
+    parser.add_argument(
+        "--embedding-dim", type=int, default=128, help="Embedding dimension."
+    )
+    parser.add_argument(
+        "--average-len",
+        type=int,
+        default=27,
+        help="Sparse feature average lengths, default is 27",
+    )
+    parser.add_argument("--batch_size", type=int, default=100, help="The batch size.")
+    parser.add_argument(
+        "-i", "--iteration", type=int, default=100, help="The number of iterations."
+    )
+    parser.add_argument(
+        "--flush-cache", action="store_true", help="If true, flush cache"
+    )
+    parser.add_argument("--fp16", action="store_true", help="If true, use fp16")
+    args, extra_args = parser.parse_known_args()
+    core.GlobalInit(["python"] + extra_args)
+
+    benchmark_sparse_normalize(
+        args.embedding_size,
+        args.embedding_dim,
+        args.average_len,
+        args.batch_size,
+        args.iteration,
+        args.flush_cache,
+        args.fp16,
+    )

caffe2/python/operator_test/sparse_normalize_test.py
@@ -1,19 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-
-
-
-
+import caffe2.python.hypothesis_test_util as hu
 import hypothesis
-from hypothesis import given, settings, HealthCheck
 import hypothesis.strategies as st
 import numpy as np
-
 from caffe2.python import core
-import caffe2.python.hypothesis_test_util as hu
+from hypothesis import HealthCheck, given, settings
 
 
 class TestSparseNormalize(hu.HypothesisTestCase):
-
     @staticmethod
     def ref_normalize(param_in, use_max_norm, norm):
         param_norm = np.linalg.norm(param_in) + 1e-12
@@ -24,31 +19,44 @@ class TestSparseNormalize(hu.HypothesisTestCase):
     # Suppress filter_too_much health check.
     # Likely caused by `assume` call falling through too often.
     @settings(suppress_health_check=[HealthCheck.filter_too_much])
-    @given(inputs=hu.tensors(n=2, min_dim=2, max_dim=2),
-           use_max_norm=st.booleans(),
-           norm=st.floats(min_value=1.0, max_value=4.0),
-           data_strategy=st.data(),
-           **hu.gcs_cpu_only)
-    def test_sparse_normalize(self, inputs, use_max_norm, norm,
-                              data_strategy, gc, dc):
+    @given(
+        inputs=hu.tensors(n=2, min_dim=2, max_dim=2),
+        use_max_norm=st.booleans(),
+        norm=st.floats(min_value=1.0, max_value=4.0),
+        data_strategy=st.data(),
+        use_fp16=st.booleans(),
+        **hu.gcs_cpu_only
+    )
+    def test_sparse_normalize(
+        self, inputs, use_max_norm, norm, data_strategy, use_fp16, gc, dc
+    ):
         param, grad = inputs
         param += 0.02 * np.sign(param)
         param[param == 0.0] += 0.02
 
+        if use_fp16:
+            param = param.astype(np.float16)
+            grad = grad.astype(np.float16)
+
         # Create an indexing array containing values that are lists of indices,
         # which index into param
         indices = data_strategy.draw(
-            hu.tensor(dtype=np.int64, min_dim=1, max_dim=1,
-                      elements=st.sampled_from(np.arange(param.shape[0]))),
+            hu.tensor(
+                dtype=np.int64,
+                min_dim=1,
+                max_dim=1,
+                elements=st.sampled_from(np.arange(param.shape[0])),
+            )
         )
-        hypothesis.note('indices.shape: %s' % str(indices.shape))
+        hypothesis.note("indices.shape: %s" % str(indices.shape))
 
         # For now, the indices must be unique
-        hypothesis.assume(np.array_equal(np.unique(indices.flatten()),
-                                         np.sort(indices.flatten())))
+        hypothesis.assume(
+            np.array_equal(np.unique(indices.flatten()), np.sort(indices.flatten()))
+        )
 
         op1 = core.CreateOperator(
-            "SparseNormalize",
+            "Float16SparseNormalize" if use_fp16 else "SparseNormalize",
             ["param", "indices"],
             ["param"],
             use_max_norm=use_max_norm,
@@ -59,7 +67,7 @@ class TestSparseNormalize(hu.HypothesisTestCase):
         grad = grad[indices]
 
         op2 = core.CreateOperator(
-            "SparseNormalize",
+            "Float16SparseNormalize" if use_fp16 else "SparseNormalize",
             ["param", "indices", "grad"],
             ["param"],
             use_max_norm=use_max_norm,
@@ -69,20 +77,22 @@ class TestSparseNormalize(hu.HypothesisTestCase):
         def ref_sparse_normalize(param, indices, grad=None):
             param_out = np.copy(param)
             for _, index in enumerate(indices):
-                param_out[index] = self.ref_normalize(
-                    param[index],
-                    use_max_norm,
-                    norm,
-                )
+                param_out[index] = self.ref_normalize(param[index], use_max_norm, norm)
             return (param_out,)
 
         # self.assertDeviceChecks(dc, op, [param, indices], [0])
         self.assertReferenceChecks(
-            gc, op1, [param, indices],
-            ref_sparse_normalize
+            gc,
+            op1,
+            [param, indices],
+            ref_sparse_normalize,
+            threshold=1e-2 if use_fp16 else 1e-4,
         )
 
         self.assertReferenceChecks(
-            gc, op2, [param, indices, grad],
-            ref_sparse_normalize
+            gc,
+            op2,
+            [param, indices, grad],
+            ref_sparse_normalize,
+            threshold=1e-2 if use_fp16 else 1e-4,
         )

commit cb23d1b992c1a0f040dcd33c912caa8cdb7e2d32

[caffe2] Add operator schema for FP16SparseNorm
Fixes regression introduced by https://github.com/pytorch/pytorch/pull/45551

caffe2/operators/sparse_normalize_op.cc
@@ -109,9 +109,6 @@ bool SparseNormalizeOp<c10::Half, CPUContext>::DoRunWithType() {
 }
 
 REGISTER_CPU_OPERATOR(SparseNormalize, SparseNormalizeOp<float, CPUContext>);
-REGISTER_CPU_OPERATOR(
-    Float16SparseNormalize,
-    SparseNormalizeOp<c10::Half, CPUContext>);
 OPERATOR_SCHEMA(SparseNormalize)
     .NumInputs(2, 3)
     .NumOutputs(1)
@@ -139,4 +136,33 @@ Given a sparse matrix, apply max_norm or constant_norm sparse regularization.
 )DOC");
 
 SHOULD_NOT_DO_GRADIENT(SparseNormalize);
+
+REGISTER_CPU_OPERATOR(Float16SparseNormalize, SparseNormalizeOp<c10::Half, CPUContext>);
+OPERATOR_SCHEMA(Float16SparseNormalize)
+    .NumInputs(2, 3)
+    .NumOutputs(1)
+    .Input(0, "param", "Parameters to be normalized")
+    .Input(1, "indices", "Sparse indices")
+    .Input(
+        2,
+        "grad",
+        "Gradient computed (optional - not used, this argument is for backwards compatibility)")
+    .Output(0, "output_param", "Normalized parameters")
+    .EnforceOneToOneInplace()
+    .Arg(
+        "use_max_norm",
+        "A bool variable to control whether to use max norm \
+    or constant norm. When use_max_norm = false, constant norm is used so that \
+    all the embedding vectors are scaled to have a L2 norm equals to A \
+    (see blow argument norm=A). If use_max_norm = true, \
+    max norm is used so that embedding is scaled so that its l2 norm is no larger \
+    than A. If an embedding's norm is less than A originally, \
+    the embedding is left unchanged.\
+    The default is True.")
+    .Arg("norm", "L2 norm of the embedding. The default is 1.0.")
+    .SetDoc(R"DOC(
+Given a sparse matrix, apply max_norm or constant_norm sparse regularization.
+)DOC");
+
+SHOULD_NOT_DO_GRADIENT(Float16SparseNormalize);
 } // namespace caffe2