167cc54a8392da8d42889d22403436063a315c06
Yi Zhang <zhanyi@microsoft.com>
2020-10-14 01:11:48 -0700

    rm useless code

--- .jenkins/pytorch/win-test-helpers/test_python_nn.bat
+++ .jenkins/pytorch/win-test-helpers/test_python_nn.bat
@@ -3,12 +3,6 @@ call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
 
 pushd test
 
-echo %cd%
-python -c 'import os; import torch; print(os.path.realpath(torch.__file__))'
-python -c 'import os; import caffe2; print(os.path.realpath(caffe2.__file__))'
-python -m pip show torch
-echo %python_installation%
-
 echo Some smoke tests
 "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe" /i python.exe +sls
 python %SCRIPT_HELPERS_DIR%\run_python_nn_smoketests.py
