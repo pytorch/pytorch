ffcfb864a49b29ea3638e89aed0ac87144e34c99
Zachary DeVito <zdevito@fb.com>
2020-10-13 21:31:05 -0700

    [fx] make sure args/kwargs are immutable (#46121)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46121
    
    Otherwise, mutating them would make the uses/users lists inaccurate.
    You can still mutate the node by assigning a new value to .args or .kwargs
    
    Test Plan: Imported from OSS
    
    Reviewed By: jamesr66a
    
    Differential Revision: D24232288
    
    Pulled By: zdevito
    
    fbshipit-source-id: c95b1a73ae55ad9bdb922ca960c8f744ff732100

--- test/test_fx.py
+++ test/test_fx.py
@@ -10,6 +10,8 @@ from torch.fx.experimental import GraphManipulation
 from torch.fx.experimental import shape_prop
 from torch.fx.experimental.Partitioner import DAG, Partitioner
 from torch.fx.experimental.subgraph_creation_example import split_module
+from torch.fx.immutable_collections import immutable_dict, immutable_list
+from copy import deepcopy
 
 from torch.fx.proxy import TraceError
 
@@ -865,7 +867,7 @@ class TestFX(JitTestCase):
         to_erase = []
         for node in rn18_traced.graph.nodes:
             if node.op == 'call_function' and node.target in [torch.relu, torch.nn.functional.relu]:
-                kwargs = node.kwargs
+                kwargs = node.kwargs.copy()
                 # Neg doesn't have in-place
                 kwargs.pop('inplace')
                 with rn18_traced.graph.inserting_before(node):
@@ -919,6 +921,13 @@ class TestFX(JitTestCase):
             if node.target in [operator.add, torch.relu]:
                 with self.assertRaisesRegex(RuntimeError, 'but it still had .* users in the graph'):
                     traced.graph.erase_node(node)
+
+    def test_copy_it(self):
+        d = immutable_dict([(3, 4), (5, 6)])
+        l = immutable_list([(3, 4), (5, 6)])
+
+        self.assertEqual(d, deepcopy(d))
+        self.assertEqual(l, deepcopy(l))
 
     def test_find_uses(self):
         graph = torch.fx.Graph()
