fcd9cd2678e80c59a96b013686947a31ed8eb3b9
Yi Zhang <zhanyi@microsoft.com>
2020-10-14 00:09:39 -0700

    add maxfail

--- .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
+++ .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
@@ -22,7 +22,7 @@ echo Run caffe2 ops tests
 pip list
 pip install pytest
 rem for /F "usebackq" %f IN (`python -c "import os; import caffe2; print(os.path.dirname(os.path.realpath(caffe2.__file__)))"`) DO set caffe2_dir=%f
-python -m pytest -x -v --disable-warnings --junit-xml="resulst.xml" C:\Users\circleci\project\build\win_tmp\build\caffe2\python\operator_test -G
+python -m pytest --maxfail=100 -x -v --disable-warnings --junit-xml="resulst.xml" C:\Users\circleci\project\build\win_tmp\build\caffe2\python\operator_test -G
 
 rem pytorch_installation="$(dirname $(dirname $(cd $TMP_DIR && python -c 'import os; import torch; print(os.path.realpath(torch.__file__))')))"
 rem python_installation="$(dirname $(dirname $(cd $TMP_DIR && python -c 'import os; import caffe2; print(os.path.realpath(caffe2.__file__))')))"
