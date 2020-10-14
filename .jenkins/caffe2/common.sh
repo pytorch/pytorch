9a9b9ead34dd67fbf1ae01697af47b9bf2138743
Yi Zhang <zhanyi@microsoft.com>
2020-10-14 00:49:52 -0700

    rm useless files

--- .jenkins/caffe2/common.sh
+++ .jenkins/caffe2/common.sh
@@ -8,11 +8,11 @@ pytest_reports_dir="${TEST_DIR}/python"
 
 # Figure out which Python to use
 PYTHON="$(which python)"
+if [[ "${BUILD_ENVIRONMENT}" =~ py((2|3)\.?[0-9]?\.?[0-9]?) ]]; then
+  PYTHON=$(which "python${BASH_REMATCH[1]}")
+fi
 
-# Figure out which Python to use for ROCm
-if [[ "${BUILD_ENVIRONMENT}" == *rocm* ]] && [[ "${BUILD_ENVIRONMENT}" =~ py((2|3)\.?[0-9]?\.?[0-9]?) ]]; then
-    PYTHON=$(which "python${BASH_REMATCH[1]}")
-    alias python="$PYTHON"
+if [[ "${BUILD_ENVIRONMENT}" == *rocm* ]]; then
     if which sccache > /dev/null; then
         # Save sccache logs to file
         sccache --stop-server || true
@@ -24,14 +24,12 @@ if [[ "${BUILD_ENVIRONMENT}" == *rocm* ]] && [[ "${BUILD_ENVIRONMENT}" =~ py((2|
     fi
 fi
 
-if [[ "$BUILD_ENVIRONMENT" != *pytorch-win-* ]]; then
-    # /usr/local/caffe2 is where the cpp bits are installed to in in cmake-only
-    # builds. In +python builds the cpp tests are copied to /usr/local/caffe2 so
-    # that the test code in .jenkins/test.sh is the same
-    INSTALL_PREFIX="/usr/local/caffe2"
-    mkdir -p "$INSTALL_PREFIX" || true
-fi
+# /usr/local/caffe2 is where the cpp bits are installed to in in cmake-only
+# builds. In +python builds the cpp tests are copied to /usr/local/caffe2 so
+# that the test code in .jenkins/test.sh is the same
+INSTALL_PREFIX="/usr/local/caffe2"
 
 mkdir -p "$gtest_reports_dir" || true
 mkdir -p "$pytest_reports_dir" || true
+mkdir -p "$INSTALL_PREFIX" || true
 
--- .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
+++ .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
@@ -1,14 +1,7 @@
 call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
 
-
+@echo on
 pushd test
-
-echo %cd%
-python -c "import os; import torch; print(os.path.realpath(torch.__file__))"
-python -c "import os; import caffe2; print(os.path.realpath(caffe2.__file__))"
-python -c "import os; import caffe2; print(os.path.dirname(os.path.realpath(caffe2.__file__)))"
-
-echo %python_installation%
 
 echo Some smoke tests
 "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe" /i python.exe +sls
@@ -21,15 +14,11 @@ if ERRORLEVEL 1 exit /b 1
 echo Run caffe2 ops tests
 pip list
 pip install pytest
-rem for /F "usebackq" %f IN (`python -c "import os; import caffe2; print(os.path.dirname(os.path.realpath(caffe2.__file__)))"`) DO set caffe2_dir=%f
-python -m pytest --maxfail=100 -x -v --disable-warnings --junit-xml="resulst.xml" C:\Users\circleci\project\build\win_tmp\build\caffe2\python\operator_test -G
+python -m pytest --maxfail=100 -x -v --disable-warnings --junit-xml="resulst.xml" %PYTHONPATH%\caffe2\python\operator_test -G
 
-rem pytorch_installation="$(dirname $(dirname $(cd $TMP_DIR && python -c 'import os; import torch; print(os.path.realpath(torch.__file__))')))"
-rem python_installation="$(dirname $(dirname $(cd $TMP_DIR && python -c 'import os; import caffe2; print(os.path.realpath(caffe2.__file__))')))"
-rem caffe2_pypath="$python_installation/caffe2"
-rem python run_test.py --include test_nn --verbose --determine-from="%1"
 if ERRORLEVEL 1 exit /b 1
 
 popd
+
 
 
