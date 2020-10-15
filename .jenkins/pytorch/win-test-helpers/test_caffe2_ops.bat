11491f467c6aea9c3c6ca6470e721d9335bd7458
Yi Zhang <zhanyi@microsoft.com>
2020-10-14 22:11:19 -0700

    simpliy test_caffe2_ops

--- .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
+++ .jenkins/pytorch/win-test-helpers/test_caffe2_ops.bat
@@ -3,17 +3,7 @@ call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
 @echo on
 pushd test
 
-echo Some smoke tests
-"C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe" /i python.exe +sls
-python %SCRIPT_HELPERS_DIR%\run_python_nn_smoketests.py
-if ERRORLEVEL 1 exit /b 1
-
-"C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe" /i python.exe -sls
-if ERRORLEVEL 1 exit /b 1
-
 echo Run caffe2 ops tests
-pip list
-pip install pytest
 cd %TMP_DIR_WIN%\build\caffe2\python\operator_test
 python -m pytest -x -v --disable-warnings ^
 --ignore adam_test.py ^
@@ -33,7 +23,7 @@ python -m pytest -x -v --disable-warnings ^
 --ignore roi_align_rotated_op_test.py ^
 --ignore sequence_ops_test.py ^
 --ignore torch_integration_test.py ^
-%TMP_DIR_WIN%\build\caffe2\python\operator_test -G
+. -G
 
 if ERRORLEVEL 1 exit /b 1
 
