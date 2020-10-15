f2df0ce51b31136e9ed6bc54dac96c84f707e79b
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-10-15 23:58:44 +0530

    update commit related for precision relaxation

--- test/test_unary_ufuncs.py
+++ test/test_unary_ufuncs.py
@@ -285,7 +285,8 @@ class TestUnaryUfuncs(TestCase):
                 exact_dtype = False
 
                 if dtype in [torch.uint8, torch.int8, torch.bool]:
-                    # Facing tolerance issue with above dtypes.
+                    # For the above dtypes,
+                    # `torch` prmotes and computes output in `float` while numpy in `float16`
                     self.assertEqualHelper(actual, expected, msg, dtype=dtype,
                                            exact_dtype=exact_dtype, rtol=1e-4, atol=1e-3)
                     continue
