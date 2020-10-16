35be7498b9e8bab092a6dac673daa3fd29ac29f2
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-10-16 11:18:57 -0700

    Add Vulkan tensor copy.
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/vulkan/ops/Copy.h
@@ -0,0 +1,13 @@
+#include <ATen/native/vulkan/ops/Common.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace ops {
+
+Tensor& copy_(Tensor& self, const Tensor& src);
+
+} // namespace ops
+} // namespace vulkan
+} // namespace native
+} // namespace at
--- aten/src/ATen/test/vulkan_api_test.cpp
+++ aten/src/ATen/test/vulkan_api_test.cpp
@@ -15,6 +15,22 @@ TEST(VulkanAPITest, empty) {
   ASSERT_NO_THROW(at::empty({1, 3, 64, 64}, at::device(at::kVulkan).dtype(at::kFloat)));
 }
 
+TEST(VulkanAPITest, copy) {
+  if (!at::native::vulkan::api::available()) {
+    return;
+  }
+
+  {
+    const auto vulkan = at::empty({1, 3, 64, 64}, at::device(at::kVulkan).dtype(at::kFloat));
+    const auto cpu = vulkan.cpu();
+  }
+
+  {
+    const auto cpu = at::empty({1, 3, 64, 64}, at::device(at::kCPU).dtype(at::kFloat));
+    const auto vulkan = cpu.vulkan();
+  }
+}
+
 } // namespace
 
 #endif /* USE_VULKAN_API */
