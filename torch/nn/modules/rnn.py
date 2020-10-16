f34e3e5acba60a287dedc44791f75e3bf1fa6a62
Guilherme Leobas <gleobas@quansight.com>
2020-10-16 00:56:46 +0000

    flake8

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -205,7 +205,10 @@ class RNNBase(Module):
             return hx
         return apply_permutation(hx, permutation)
 
-    def forward(self, input: Union[Tensor, PackedSequence], hx: Optional[Tensor] = None) -> Tuple[Union[Tensor, PackedSequence], Tensor]:
+    def forward(self,
+                input: Union[Tensor, PackedSequence],
+                hx: Optional[Tensor] = None
+    ) -> Tuple[Union[Tensor, PackedSequence], Tensor]:
         is_packed = isinstance(input, PackedSequence)
         if is_packed:
             input, batch_sizes, sorted_indices, unsorted_indices = input
