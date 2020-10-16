50d65254b24e7753bba3e8bbac1299dd534834a3
jiej <jiej@nvidia.com>
2020-10-16 01:52:54 -0700

    clang format

--- torch/csrc/jit/codegen/cuda/lower_alias_memory.cpp
+++ torch/csrc/jit/codegen/cuda/lower_alias_memory.cpp
@@ -179,8 +179,8 @@ class AllocateReuseModifier final : private OptOutDispatch {
           auto inferred_register_size =
               eval_evaluator_.inferValue(allocation->size());
           if (inferred_register_size.has_value()) {
-            local_valid =
-                inferred_register_size.value() > static_cast<int64_t>(register_size_threshold_);
+            local_valid = inferred_register_size.value() >
+                static_cast<int64_t>(register_size_threshold_);
           }
         }
 
