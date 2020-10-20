c406a489e8cd16fb430e8e1be4b918dd258e8b39
Nik Ved <nik@pytorch-dev.c.qhub-279316.internal>
2020-10-20 16:13:54 +0000

    add note on the derivation

--- torch/_autograd_functions.py
+++ torch/_autograd_functions.py
@@ -10,6 +10,55 @@ class _LU(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, LU_grad, pivots_grad, infors_grad):
+        """
+        Here we derive the gradients for the LU decomposition.
+        LIMITATIONS: square inputs of full rank.
+        If not stated otherwise, for tensors A and B,
+        `A B` means the matrix product of A and B.
+
+        Forward AD:
+        Note that PyTorch returns packed LU, it is a mapping
+        A -> (B:= L + U - I, P), such that A = P L U, and
+        P is a permutation matrix, and is non-differentiable.
+
+        Using B = L + U - I, A = P L U, we get
+        
+        dB = dL + dU and     (*)
+        P^T dA = dL U + L dU (**)
+
+        By left/right multiplication of (**) with L^{-1}/U^{-1} we get:
+        L^{-1} P^T dA U^{-1} = L^{-1} dL + dU U^{-1}.
+
+        Note that L^{-1} dL is lower-triangular with zero diagonal,
+        and dU U^{-1} is upper-triangular.
+        Define 1_U := triu(ones(n, n)), and 1_L := ones(n, n) - 1_U, so
+
+        L^{-1} dL = 1_L o (L^{-1} P^T dA U^{-1}),
+        dU U^{-1} = 1_U o (L^{-1} P^T dA U^{-1}), where o denotes the Hadamard product.
+
+        Hence we finally get:
+        dL = L 1_L o (L^{-1} P^T dA U^{-1}),
+        dU = 1_U o (L^{-1} P^T dA U^{-1}) U
+
+        Backward AD:
+        The backward sensitivity is then:
+        Tr(B_grad^T dB) = Tr(B_grad^T dL) + Tr(B_grad^T dU) = [1] + [2].
+
+        [1] = Tr(B_grad^T dL) = Tr(B_grad^T L 1_L o (L^{-1} P^T dA U^{-1}))
+            = [using Tr(A (B o C)) = Tr((A o B^T) C)]
+            = Tr((B_grad^T L o 1_L^T) L^{-1} P^T dA U^{-1})
+            = [cyclic property of trace]
+            = Tr(U^{-1} (B_grad^T L o 1_L^T) L^{-1} P^T dA)
+            = Tr((P L^{-T} (L^T B_grad o 1_L) U^{-T})^T dA).
+        Similary [2] can be rewritten as:
+        [2] = Tr(P L^{-T} (B_grad U^T o 1_U) U^{-T})^T dA, hence
+        Tr(A_grad^T dA) = [1] + [2]
+                        = Tr((P L^{-T} (L^T B_grad o 1_L + B_grad U^T o 1_U) U^{-T})^T dA), so
+        A_grad = P L^{-T} (L^T B_grad o 1_L + B_grad U^T o 1_U) U^{-T}.
+
+        In the code below we use `LU` name instead of `B`, so that there is no confusion
+        in the derivation above between the matrix product and a two-letter variable name.
+        """
         LU, pivots = ctx.saved_tensors
         P, L, U = torch.lu_unpack(LU, pivots)
 
