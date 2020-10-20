946f9d8775e92fd5c494332c7098aceb77a04206
Nik Ved <nik@pytorch-dev.c.qhub-279316.internal>
2020-10-20 14:44:41 +0000

    replace inverse with triangular_solve

--- torch/_autograd_functions.py
+++ torch/_autograd_functions.py
@@ -13,8 +13,11 @@ class _LU(torch.autograd.Function):
         LU, pivots = ctx.saved_tensors
         P, L, U = torch.lu_unpack(LU, pivots)
 
-        Lt_inv = L.inverse().transpose(-1, -2)
-        Ut_inv = U.inverse().transpose(-1, -2)
+        I = LU_grad.new_zeros(LU_grad.shape)
+        I.diagonal(dim1=-2, dim2=-1).fill_(1)
+
+        Lt_inv = torch.triangular_solve(I, L, upper=False).solution.transpose(-1, -2)
+        Ut_inv = torch.triangular_solve(I, U, upper=True).solution.transpose(-1, -2)
 
         phi_L = (L.transpose(-1, -2) @ LU_grad).tril_()
         phi_L.diagonal(dim1=-2, dim2=-1).mul_(0.0)
@@ -22,4 +25,3 @@ class _LU(torch.autograd.Function):
 
         self_grad_perturbed = Lt_inv @ (phi_L + phi_U) @ Ut_inv
         return P @ self_grad_perturbed, None, None
-
