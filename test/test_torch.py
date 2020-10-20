63c2e60f46351fda0496a408c7d8c9ea225f136d
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-10-20 16:05:28 -0700

    lint

--- test/test_torch.py
+++ test/test_torch.py
@@ -6148,9 +6148,9 @@ class TestTorchDeviceType(TestCase):
             x = torch.eye(3, 3, dtype=torch.float, device=device).reshape((1, 3, 3)).repeat(batch_dim, 1, 1)
             x[n, -1, -1] = 0
 
-            with self.assertRaisesRegex(RuntimeError, f'For batch {n}: U\(3,3\) is zero'):
+            with self.assertRaisesRegex(RuntimeError, rf'For batch {n}: U\(3,3\) is zero'):
                 torch.inverse(x)
-        
+
         for params in [(1, 0), (2, 0), (2, 1), (4, 0), (4, 2), (10, 2)]:
             helper(*params)
 
