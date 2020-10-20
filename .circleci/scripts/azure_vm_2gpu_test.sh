16596a3873b33624f312ed0b615ba90f6c0e0021
Joe Zhu <jozh@microsoft.com>
2020-10-20 19:22:51 +0800

    update

--- .circleci/scripts/azure_vm_2gpu_test.sh
+++ .circleci/scripts/azure_vm_2gpu_test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 az login --service-principal -u 144f73e6-2a64-47bc-8cfd-f6cbf04d9a69 -p jTn5Cz_b6qFQ-XF.eD9JoLQXSKbH4VwMq. --tenant e81f979e-21ba-433a-9fe1-673ae9641a5f
 az disk create -g PyTorchCiTestGroup -n pytorch-test-vm-os-disk --source pytorch-ci-test-base-image --location SouthCentralUS
 az vm create --name jozh-pytorch-test-vm-01 -g PyTorchCiTestGroup --attach-os-disk pytorch-test-vm-os-disk --location SouthCentralUS --os-type Windows
