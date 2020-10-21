402069dd92b112b45d593f9da80e1ca964a75ce1
Tao Xu <taox@fb.com>
2020-10-21 09:48:56 -0700

    Update on "[CI][IOS] Add unit tests for Metal"
    [ghstack-poisoned]

--- .circleci/cimodel/data/simple/ios_definitions.py
+++ .circleci/cimodel/data/simple/ios_definitions.py
@@ -63,7 +63,7 @@ class IOSJob:
 WORKFLOW_DATA = [
     IOSJob(XCODE_VERSION, ArchVariant("x86_64"), is_org_member_context=False),
     IOSJob(XCODE_VERSION, ArchVariant("arm64")),
-    IOSJob(XCODE_VERSION, ArchVariant("arm64", "metal"), extra_props={"use_metal": "ON"}),
+    IOSJob(XCODE_VERSION, ArchVariant("arm64", "metal"), extra_props={"use_metal": "1"}),
     IOSJob(XCODE_VERSION, ArchVariant("arm64", "custom"), extra_props={"op_list": "mobilenetv2.yaml"}),
 ]
 
