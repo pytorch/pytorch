4e96ee3d9af9343fb8a3444fba1f18a164cefe49
Joe Zhu <jozh@microsoft.com>
2020-10-21 20:04:31 +0800

    update

--- .circleci/scripts/script.ps1
+++ .circleci/scripts/script.ps1
@@ -1,13 +1,27 @@
+param(
+    [string]$sp_id,
+    [string]$sp_secret,
+    [string]$tenant_id,
+    [string]$pkg_name,
+    [string]$pull_branch,
+    [string]$commit_id
+)
+
 C:\pytorch_test\EnableCondaInPS.ps1
 conda env list
 pip list
 d:
 git clone https://github.com/gunandrose4u/pytorch.git
-az storage blob download --container-name pytorch --account-key 69yY6ZGqj0AqLB4w7i8IbzDd/e6FbgUp3vhnhk5bkTp0LR6xS3WvOayIgy9SdYRdI6yKBoIWKmx5FtKsG4pUvg== --account-name pytorchteststorage -n torch_7z.7z -f torch.7z
-7z x torch.7z -otmp -r -aou
+
+az login --service-principal -u $sp_id -p $sp_secret --tenant $tenant_id
+az storage blob download --account-name pytorchbuildsto --container-name pytorch -n $pkg_name -f $pkg_name
+7z x $pkg_name -otmp -r -aou
 echo $env:CONDA_PREFIX
 Copy-Item -Path d:\tmp\caffe2 -Destination $env:CONDA_PREFIX\Lib\site-packages\caffe2 -recurse -Force
 Copy-Item -Path d:\tmp\torch -Destination $env:CONDA_PREFIX\Lib\site-packages\torch -recurse -Force
 cd pytorch
-git checkout -b jozh/enable_2gpu_win_ci remotes/origin/jozh/enable_2gpu_win_ci
+git reset --hard $commit_id
+git checkout -q -B $pull_branch
+git reset --hard $commit_id
+
 python test/run_test.py --verbose -i distributed/test_c10d
