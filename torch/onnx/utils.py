c9a5fd266643507dba47f07cdd1b919ed0a70d0e
Negin Raoof <neginmr@utexas.edu>
2020-10-21 00:54:38 -0700

    Update utils.py

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -433,13 +433,13 @@ def _model_to_graph(model, args, verbose=False,
     # NB: ONNX requires complete information about output types, which might be
     # erased by some optimizations, so we need to set it explicitly again.
     if torch_out is not None:
-        output_tensors, out_desc = torch._C._jit_flatten(torch_out)
-        torch._C._jit_pass_onnx_assign_output_shape(graph, output_tensors, out_desc, _onnx_shape_inference)
-
         if isinstance(torch_out, list):
             torch_out = tuple(torch_out)
         elif not isinstance(torch_out, tuple):
             torch_out = tuple([torch_out])
+
+        output_tensors, out_desc = torch._C._jit_flatten(torch_out)
+        torch._C._jit_pass_onnx_assign_output_shape(graph, output_tensors, out_desc, _onnx_shape_inference)
 
     _set_input_and_output_names(graph, input_names, output_names)
 
