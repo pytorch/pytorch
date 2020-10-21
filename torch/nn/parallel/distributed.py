69e152fc488a1bf8bb3e7aefc7274f1366322b97
Shen Li <cs.shenli@gmail.com>
2020-10-21 13:19:15 -0700

    Update on "Skip broadcast buffers for SyncBN in DDP"
    Differential Revision: [D24457141](https://our.internmc.facebook.com/intern/diff/D24457141)
    
    [ghstack-poisoned]

--- torch/nn/parallel/distributed.py
+++ torch/nn/parallel/distributed.py
@@ -492,20 +492,22 @@ class DistributedDataParallel(Module):
             for m in self._module_copies
         ]
 
-        # Add buffers from SyncBN to parameters_to_ignore. These buffers can
+        # Collect names of buffers from SyncBN layers. These buffers can
         # be ignored in _sync_params() as the SyncBN layers have already
         # synchronized them.
+        buffers_to_ignore = []
         for m in self._module_copies:
             for m_name, m_layer in m.named_modules():
                 if isinstance(m_layer, torch.nn.SyncBatchNorm):
                     for b_name, _ in m_layer.named_buffers():
-                        self.parameters_to_ignore += f"{m_name}.{b_name}"
+                        buffers_to_ignore.append(f"{m_name}.{b_name}")
 
+        buffers_to_ignore.extend(self.parameters_to_ignore)
         self.modules_buffers = [
             [
                 buffer
                 for (buffer, buffer_name) in module_buffers
-                if buffer_name not in self.parameters_to_ignore
+                if buffer_name not in buffers_to_ignore
             ]
             for module_buffers in named_module_buffers
         ]
