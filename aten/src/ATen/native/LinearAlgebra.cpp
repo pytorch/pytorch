00369d7b15afa969d64f53e7b923f8f6882c514a
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-10-22 18:29:01 +0000

    Make non out function primary back, fixes autograd

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -1602,7 +1602,7 @@ Tensor& linalg_norm_out(Tensor& result, const Tensor& self, std::string ord, opt
   return linalg_norm_out_impl(result, self, c10::nullopt, ord, opt_dim, keepdim, opt_dtype);
 }
 
-Tensor& linalg_tensorinv_out(Tensor& result, const Tensor& self, int64_t ind) {
+Tensor linalg_tensorinv(const Tensor& self, int64_t ind) {
   /*
   The idea is to reduce the problem to 2D square matrix inversion.
   Step 1. Calculate the shape of the result and the shape of the intermediate 2D matrix.
@@ -1613,8 +1613,6 @@ Tensor& linalg_tensorinv_out(Tensor& result, const Tensor& self, int64_t ind) {
           Note that for CUDA this causes cross-device memory synchronization that can be slow.
   Step 4. reshape the result.
   */
-  TORCH_CHECK(result.scalar_type() == self.scalar_type(),
-    "result dtype ", result.scalar_type(), " does not match self dtype ", self.scalar_type());
   TORCH_CHECK(ind > 0, "Expected a strictly positive integer for 'ind', but got ", ind);
 
   // self[ind:]
@@ -1635,20 +1633,24 @@ Tensor& linalg_tensorinv_out(Tensor& result, const Tensor& self, int64_t ind) {
   shape_ind_end.insert(shape_ind_end.end(), shape_start_ind.begin(), shape_start_ind.end());
 
   // If the reshaped self is not invertible catch this error
+  Tensor result;
   try {
-    at::native::resize_output(result, shape_ind_end);
-    result.resize_({prod_ind_end, prod_ind_end});
-    result = at::inverse_out(result, self.reshape({prod_ind_end, prod_ind_end}));
+    result = at::inverse(self.reshape({prod_ind_end, prod_ind_end}));
   } catch (...) {
     TORCH_CHECK(false, "Failed to invert the input tensor, because it is singular.");
   }
-  result.resize_(shape_ind_end);
-  return result;
+
+  return result.reshape(shape_ind_end);
 }
 
-Tensor linalg_tensorinv(const Tensor& self, int64_t ind) {
-  Tensor result = at::empty({0}, self.options());
-  return at::linalg_tensorinv_out(result, self, ind);
+Tensor& linalg_tensorinv_out(Tensor& result, const Tensor& self, int64_t ind) {
+  TORCH_CHECK(result.scalar_type() == self.scalar_type(),
+    "result dtype ", result.scalar_type(), " does not match self dtype ", self.scalar_type());
+
+  Tensor result_tmp = at::linalg_tensorinv(self, ind);
+  at::native::resize_output(result, result_tmp.sizes());
+  result.copy_(result_tmp);
+  return result;
 }
 
 static inline Tensor _chain_matmul_general(TensorList matrices, std::vector<std::vector<int64_t>>& order, int64_t i, int64_t j) {
