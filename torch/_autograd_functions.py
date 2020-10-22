b1e79c94969491f789e07162d011ce86e414942d
Nik Ved <nik@pytorch-dev.c.qhub-279316.internal>
2020-10-22 14:20:08 +0000

    replace mul(0) with fill(0)

--- torch/_autograd_functions.py
+++ torch/_autograd_functions.py
@@ -72,7 +72,7 @@ class _LU(torch.autograd.Function):
         Ut_inv = torch.triangular_solve(I, U, upper=True).solution.transpose(-1, -2)
 
         phi_L = (L.transpose(-1, -2) @ LU_grad).tril_()
-        phi_L.diagonal(dim1=-2, dim2=-1).mul_(0.0)
+        phi_L.diagonal(dim1=-2, dim2=-1).fill_(0.0)
         phi_U = (LU_grad @ U.transpose(-1, -2)).triu_()
 
         self_grad_perturbed = Lt_inv @ (phi_L + phi_U) @ Ut_inv
