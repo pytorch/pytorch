3527c00254a5cf2449b69e909a74c214bc27498f
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-10-26 15:27:39 -0700

    Update on "Add Vulkan op Add."
    Differential Revision: [D23820826](https://our.internmc.facebook.com/intern/diff/D23820826)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/vulkan/glsl/add_.glsl
+++ aten/src/ATen/native/vulkan/glsl/add_.glsl
@@ -11,17 +11,17 @@ layout(set = 0, binding = 1)          uniform PRECISION          sampler3D uInpu
 layout(set = 0, binding = 2)          uniform           restrict Block {
   ivec3 WHC;
   float alpha;
-} block;
+} uBlock;
 
 layout(local_size_x_id = 1, local_size_y_id = 2, local_size_z_id = 3) in;
 
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
 
-  if (all(lessThan(pos, block.WHC))) {
+  if (all(lessThan(pos, uBlock.WHC))) {
     imageStore(
         uOutput,
         pos,
-        imageLoad(uOutput, pos) + block.alpha * texelFetch(uInput0, pos, 0));
+        imageLoad(uOutput, pos) + uBlock.alpha * texelFetch(uInput0, pos, 0));
   }
 }
--- aten/src/ATen/native/vulkan/glsl/add_scalar.glsl
+++ aten/src/ATen/native/vulkan/glsl/add_scalar.glsl
@@ -11,17 +11,17 @@ layout(set = 0, binding = 1)          uniform PRECISION           sampler3D uInp
 layout(set = 0, binding = 2)          uniform           restrict  Block {
   ivec3 WHC;
   float other;
-} block;
+} uBlock;
 
 layout(local_size_x_id = 1, local_size_y_id = 2, local_size_z_id = 3) in;
 
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
 
-  if (all(lessThan(pos, block.WHC))) {
+  if (all(lessThan(pos, uBlock.WHC))) {
     imageStore(
         uOutput,
         pos,
-        texelFetch(uInput, pos, 0) + block.other);
+        texelFetch(uInput, pos, 0) + uBlock.other);
   }
 }
