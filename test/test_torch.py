Update on "Fix ops that use type promotion + broadcasting (in TensorIterator)"
This PR does a few things (fixing a few related bugs):

1) updates all comparison/logical ops to use TensorIterator's type promotion logic to `cast_common_dtype_to_outputs()`. This fixes what used to be an internal assertion error whenever you tried to call comparison/logical op with an `out` tensor with a dtype that didn't match the type-promoted common input dtype (except when the output dtype is `torch.bool`, which was special-cased to work). e.g. The below used to fail with the internal assertion, and now works.

```
>>> a = torch.ones(1, dtype=torch.bfloat16)
>>> b = torch.ones(1, dtype=torch.float32)
>>> c = torch.ones(1, dtype=torch.float64)
>>> output = torch.eq(a, b, out=c)
>>> tensor([1, dtype=torch.float64)
```

2) This actually exposed an existing issue in TensorIterator that became more widespread as part of fix 1, which is also fixed in this PR. Specifically, calling `cast_common_dtype_to_outputs()` causes TensorIterator to create a temp output tensor [here](https://github.com/pytorch/pytorch/blob/343260a1cc278c213ba78a65492de75397b705e4/aten/src/ATen/native/TensorIterator.cpp#L408) with dtype=common_dtype_ (the type-promoted input dtype), and is supposed to later copy the contents of the temp output tensor to the actual output tensor (performing the cast to the output dtype). This currently does not happen when the output tensor also needs to be resized (and should be fixed as part of this PR), because `target_dtype` wasn't properly updated when creating the temp output tensor. For example, after only implementing the first fix we would see the following:
```
>>> torch.eq(torch.ones(2), torch.ones(1)).shape
>>> # incorrect, not resized properly
>>> torch.Size([0]) 
```

3) Fixing the implementation of Vec256<double/float/bfloat16> to work properly with NAN. We expect `nan != nan` to return True (since nan is defined such that `nan == nan` is False). Fixed

test/test_torch.py
@@ -3901,6 +3901,32 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
                 self.assertEqual(output_64.dtype, torch.float64)
                 self.assertEqual(output_64.shape, (2,))
 
+        def test_functional_comparison_ops_return_bool_outputs(self):
+            for dtype in torch.testing.get_all_dtypes():
+                for op in [torch.eq, torch.ne,
+                        torch.logical_and, torch.logical_or, torch.logical_xor]:
+                    input1 = torch.ones(8, dtype=dtype)
+                    self.assertEqual(op(input1, input1).dtype, torch.bool)
+
+        def test_ne_eq_vectorized_treats_nan_properly(self):
+            # issue #42660
+            # use a large enough tensor to call vec256::ne
+            shape = (32, 32)
+
+            for dtype in [torch.bfloat16, torch.float32, torch.float64,
+                    torch.complex64, torch.complex128]:
+                input_nan = torch.full(shape, np.nan, dtype=dtype)
+                input_not_nan = torch.zeros(shape, dtype=dtype)
+                # nan != nan returns true
+                self.assertTrue((input_nan != input_nan).all())
+                # nan != <anything> returns true
+                self.assertTrue((input_nan != input_not_nan).all())
+                # nan == nan returns false
+                self.assertFalse((input_nan == input_nan).any())
+                # nan == <anything> returns false
+                self.assertFalse((input_nan == input_not_nan).any())
+
+
         def test_inplace_comparison_ops_require_inputs_have_same_dtype(self):
             with self.assertRaisesRegex(RuntimeError, 'Expected object of scalar type'):
                 for op in ['lt_', 'le_', 'gt_', 'ge_', 'eq_', 'ne_', 'logical_xor_', 'logical_and_', 'logical_or_']:
@@ -6399,6 +6425,9 @@ class TestTorchDeviceType(TestCase):
         b = torch.tensor(b_, dtype=dtypes[1], device=device)
 
         # new tensor
+        x1 = expected_res.bool()
+        x2 = getattr(a, op)(b)
+        self.assertEqual(x1, x2)
         self.assertEqual(expected_res.bool(), getattr(a, op)(b))
         # out
         c = torch.empty(0, dtype=torch.bool, device=device)
@@ -18752,7 +18781,7 @@ else:
                                             exact_dtype=True, with_keepdim=False):
         # Test 0-d to 3-d tensors.
         for ndims in range(0, 4):
-            shape = self._rand_shape(ndims, min_size=5, max_size=10)
+            shape = self._rand_shape(ndims, min_size=2, max_size=2)
             for n in range(ndims + 1):
                 for c in combinations(list(range(ndims)), n):
                     for count_dim in permutations(c):