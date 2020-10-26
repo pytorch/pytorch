Update on "Fix ops that use type promotion + broadcasting (in TensorIterator)"
This PR does a few things (fixing a few related bugs):

1) updates all comparison/logical ops to use TensorIterator's type promotion logic to `cast_common_dtype_to_outputs()`. This fixes what used to be an internal assertion error whenever you tried to call comparison/logical op with an `out` tensor with a dtype that didn't match the type-promoted common input dtype (except when the output dtype is `torch.bool`, which was special-cased to work). e.g. The below used to fail with the internal assertion, and now works.

```
>>> a = torch.ones(1, dtype=torch.bfloat16)
>>> b = torch.ones(1, dtype=torch.float32)
>>> c = torch.ones(1, dtype=torch.float64)
>>> output = torch.eq(a, b, out=c)
>>> tensor([1, dtype=torch.float64)
```

2) This actually exposed an existing issue in TensorIterator that became more widespread as part of fix 1, which is also fixed in this PR. Specifically, calling `cast_common_dtype_to_outputs()` causes TensorIterator to create a temp output tensor [here](https://github.com/pytorch/pytorch/blob/343260a1cc278c213ba78a65492de75397b705e4/aten/src/ATen/native/TensorIterator.cpp#L408) with dtype=common_dtype_ (the type-promoted input dtype), and is supposed to later copy the contents of the temp output tensor to the actual output tensor (performing the cast to the output dtype). This currently does not happen when the output tensor also needs to be resized (and should be fixed as part of this PR), because `target_dtype` wasn't properly updated when creating the temp output tensor. For example, after only implementing the first fix we would see the following:
```
>>> torch.eq(torch.ones(2), torch.ones(1)).shape
>>> # incorrect, not resized properly
>>> torch.Size([0]) 
```

3) Performance fix in TensorIterator. Adds a flag in `cast_common_dtype_to_outputs()` to special-case `torch.bool` output tensors to not create a temporary output tensor. This is to preven

test/test_torch.py
@@ -6425,9 +6425,6 @@ class TestTorchDeviceType(TestCase):
         b = torch.tensor(b_, dtype=dtypes[1], device=device)
 
         # new tensor
-        x1 = expected_res.bool()
-        x2 = getattr(a, op)(b)
-        self.assertEqual(x1, x2)
         self.assertEqual(expected_res.bool(), getattr(a, op)(b))
         # out
         c = torch.empty(0, dtype=torch.bool, device=device)
@@ -18781,7 +18778,7 @@ else:
                                             exact_dtype=True, with_keepdim=False):
         # Test 0-d to 3-d tensors.
         for ndims in range(0, 4):
-            shape = self._rand_shape(ndims, min_size=2, max_size=2)
+            shape = self._rand_shape(ndims, min_size=5, max_size=10)
             for n in range(ndims + 1):
                 for c in combinations(list(range(ndims)), n):
                     for count_dim in permutations(c):