Update on "Fix ops that use type promotion + broadcasting (in TensorIterator)"
This PR does a few things (fixing a few related bugs):

1) updates all comparison/logical ops to use TensorIterator's type promotion logic to `cast_common_dtype_to_outputs()`. This fixes what used to be an internal assertion error whenever you tried to call comparison/logical op with an `out` tensor with a dtype that didn't match the type-promoted common input dtype (except when the output dtype is `torch.bool`, which was special-cased to work). e.g. The below used to fail with the internal assertion, and now works.

```
>>> a = torch.ones(1, dtype=torch.bfloat16)
>>> b = torch.ones(1, dtype=torch.float32)
>>> c = torch.ones(1, dtype=torch.float64)
>>> output = torch.eq(a, b, out=c)
>>> tensor([1, dtype=torch.float64)
```

2) This actually exposed an existing issue in TensorIterator that became more widespread as part of fix 1, which is also fixed in this PR. Specifically, calling `cast_common_dtype_to_outputs()` causes TensorIterator to create a temp output tensor [here](https://github.com/pytorch/pytorch/blob/343260a1cc278c213ba78a65492de75397b705e4/aten/src/ATen/native/TensorIterator.cpp#L408) with dtype=common_dtype_ (the type-promoted input dtype), and is supposed to later copy the contents of the temp output tensor to the actual output tensor (performing the cast to the output dtype). This currently does not happen when the output tensor also needs to be resized (and should be fixed as part of this PR), because `target_dtype` wasn't properly updated when creating the temp output tensor. For example, after only implementing the first fix we would see the following:
```
>>> torch.eq(torch.ones(2), torch.ones(1)).shape
>>> # incorrect, not resized properly
>>> torch.Size([0]) 
```

3) Avoid an unnecessary copy in the functional version of logical/comparison ops. This is part of the PR mainly to prevent a perf regression for the functional version of logical/compariso

test/test_torch.py
@@ -5902,16 +5902,19 @@ class TestTorchDeviceType(TestCase):
             torch.isinf(1)  # Parameter must be a tensor
 
     @unittest.skipIf(not TEST_NUMPY, "Numpy not found")
-    @dtypes(*tuple(itertools.combinations_with_replacement(torch.testing.get_all_dtypes(include_bfloat16=False), 2)))
+    @dtypes(*tuple(itertools.combinations_with_replacement(torch.testing.get_all_dtypes(), 2)))
     def test_comparison_ops_type_promotion_and_broadcasting(self, device, dtypes):
         # issue #42660
         # testing all combinations of broadcasting and type promotion
         # with a range of dtypes and input shapes, and with extremal values
         def compare_with_numpy_bin_op(torch_fn, np_fn, x, y, out=None):
-            y_np = y.cpu().numpy()
+            # working around the fact that numpy doesn't support bfloat16
+            # by letting numpy treat them as float32's
+            x_np = x if x.dtype != torch.bfloat16 else x.to(torch.float32)
+            y_np = y.cpu().numpy() if y.dtype != torch.bfloat16 else y.to(torch.float32).cpu().numpy()
             self.compare_with_numpy(lambda inp: torch_fn(inp, y, out=out) if out else torch_fn(inp, y),
                                     lambda inp: np_fn(inp, y_np, out=out) if out else np_fn(inp, y_np),
-                                    x)
+                                    x_np)
 
         complex_op_denylist = [torch.lt, torch.le, torch.gt, torch.ge] # complex not supported
         input_sizes = [
@@ -18768,7 +18771,12 @@ else:
             x = torch.tensor((), dtype=dtype, device=device)
         else:
             if dtype.is_floating_point or dtype.is_complex:
-                x = torch.randn(*shape, dtype=dtype, device=device) * random.randint(30, 100)
+                # work around torch.randn not being implemented for bfloat16
+                if dtype == torch.bfloat16:
+                    x = torch.randn(*shape, device=device) * random.randint(30, 100)
+                    x = x.to(torch.bfloat16)
+                else:
+                    x = torch.randn(*shape, dtype=dtype, device=device) * random.randint(30, 100)
                 x[torch.randn(*shape) > 0.5] = 0
                 if with_extremal and dtype.is_floating_point:
                     # Use extremal values