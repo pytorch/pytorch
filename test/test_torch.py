Update on "Fix ops that use type promotion + broadcasting (in TensorIterator)"
This PR does a few things (fixing a few related bugs):

1) updates all comparison/logical ops to use TensorIterator's type promotion logic to `cast_common_dtype_to_outputs()`. This fixes what used to be an internal assertion error whenever you tried to call comparison/logical op with an `out` tensor with a dtype that didn't match the type-promoted common input dtype (except when the output dtype is `torch.bool`, which was special-cased to work). e.g. The below used to fail with the internal assertion, and now works.

```
>>> a = torch.ones(1, dtype=torch.bfloat16)
>>> b = torch.ones(1, dtype=torch.float32)
>>> c = torch.ones(1, dtype=torch.float64)
>>> output = torch.eq(a, b, out=c)
>>> tensor([1, dtype=torch.float64)
```

2) This actually exposed an existing issue in TensorIterator that became more widespread as part of fix 1, which is also fixed in this PR. Specifically, calling `cast_common_dtype_to_outputs()` causes TensorIterator to create a temp output tensor [here](https://github.com/pytorch/pytorch/blob/343260a1cc278c213ba78a65492de75397b705e4/aten/src/ATen/native/TensorIterator.cpp#L408) with dtype=common_dtype_ (the type-promoted input dtype), and is supposed to later copy the contents of the temp output tensor to the actual output tensor (performing the cast to the output dtype). This currently does not happen when the output tensor also needs to be resized (and should be fixed as part of this PR), because `target_dtype` wasn't properly updated when creating the temp output tensor. For example, after only implementing the first fix we would see the following:
```
>>> torch.eq(torch.ones(2), torch.ones(1)).shape
>>> # incorrect, not resized properly
>>> torch.Size([0]) 
```

3) Avoid an unnecessary copy in the functional version of logical/comparison ops. This is part of the PR mainly to prevent a perf regression for the functional version of logical/compariso

test/test_torch.py
@@ -3891,20 +3891,35 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
 
         def test_out_comparison_ops_type_promotion_and_broadcasting(self):
             # issue #42660
-            for op in [torch.lt, torch.le, torch.gt, torch.ge, torch.eq, torch.ne,
-                       torch.logical_and, torch.logical_or, torch.logical_xor]:
-                input1_16 = torch.ones(2, dtype=torch.bfloat16)
-                input2_32 = torch.ones(1, dtype=torch.float32)
-                output_64 = torch.zeros(1, dtype=torch.float64)
+            input1_16 = torch.ones((2, 2), dtype=torch.bfloat16)
+            input2_32 = torch.ones(2, dtype=torch.float32)
+            output_64 = torch.zeros(1, dtype=torch.float64)
 
+            expected_ones = torch.ones((2, 2,), dtype=torch.float64)
+            expected_zeros = torch.zeros((2, 2,), dtype=torch.float64)
+            op_map = {
+                    torch.lt: expected_zeros,
+                    torch.le: expected_ones,
+                    torch.gt: expected_zeros,
+                    torch.ge: expected_ones,
+                    torch.eq: expected_ones,
+                    torch.ne: expected_zeros,
+                    torch.logical_and: expected_ones,
+                    torch.logical_or: expected_ones,
+                    torch.logical_xor: expected_zeros
+                    }
+            for op, expected_output in op_map.items():
                 op(input1_16, input2_32, out=output_64)
-                self.assertEqual(output_64.dtype, torch.float64)
-                self.assertEqual(output_64.shape, (2,))
+                self.assertEqual(output_64, expected_output)
+
 
         def test_functional_comparison_ops_return_bool_outputs(self):
+            complex_op_denylist = [torch.lt, torch.le, torch.gt, torch.ge]
             for dtype in torch.testing.get_all_dtypes():
-                for op in [torch.eq, torch.ne,
+                for op in [torch.lt, torch.le, torch.gt, torch.ge, torch.eq, torch.ne,
                         torch.logical_and, torch.logical_or, torch.logical_xor]:
+                    if dtype.is_complex and op in complex_op_denylist:
+                        continue
                     input1 = torch.ones(8, dtype=dtype)
                     self.assertEqual(op(input1, input1).dtype, torch.bool)
 
@@ -3917,14 +3932,20 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
                     torch.complex64, torch.complex128]:
                 input_nan = torch.full(shape, np.nan, dtype=dtype)
                 input_not_nan = torch.zeros(shape, dtype=dtype)
+                output = torch.zeros(0, dtype=dtype)
+
                 # nan != nan returns true
-                self.assertTrue((input_nan != input_nan).all())
+                torch.ne(input_nan, input_nan, out=output)
+                self.assertTrue(output.to(bool).all())
                 # nan != <anything> returns true
-                self.assertTrue((input_nan != input_not_nan).all())
+                torch.ne(input_nan, input_not_nan, out=output)
+                self.assertTrue(output.to(bool).all())
                 # nan == nan returns false
-                self.assertFalse((input_nan == input_nan).any())
+                torch.eq(input_nan, input_nan, out=output)
+                self.assertFalse(output.to(bool).any())
                 # nan == <anything> returns false
-                self.assertFalse((input_nan == input_not_nan).any())
+                torch.eq(input_nan, input_not_nan, out=output)
+                self.assertFalse(output.to(bool).any())
 
 
         def test_inplace_comparison_ops_require_inputs_have_same_dtype(self):