aa5b457d5a7557a72d36cb3982369f25c8e93bd7
Scott Wolchok <swolchok@fb.com>
2020-10-28 13:39:06 -0700

    [Pytorch] Specialize guts of c10::optional for 32-bit scalars
    c10::optional has non-trivial copy and move operations always. This change specializes it for 32-bit scalars so that it has trivial copy and move operations in that case. Ideally, we would instead rely on P0602 "variant and optional should propagate copy/move triviality" and use `std::optional` (or implement that functionality ourselves). We can't use `std::optional` because we are stuck with C++14. Implementing the full P0602 ourselves would add even more complexity. We could do it, but this should be a helpful first step.
    
    Differential Revision: [D24552280](https://our.internmc.facebook.com/intern/diff/D24552280/)
    
    [ghstack-poisoned]

--- c10/util/Optional.cpp
+++ c10/util/Optional.cpp
@@ -1,1 +1,6 @@
 #include <c10/util/Optional.h>
+
+#include <type_traits>
+
+static_assert(std::is_trivially_copyable<c10::optional<int>>::value, "c10::optional<int> should be trivially copyable");
+static_assert(std::is_trivially_copyable<c10::optional<bool>>::value, "c10::optional<bool> should be trivially copyable");
