b0852ad6b673b6b852783df0b45bf5de81809af5
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-10-28 18:16:55 -0700

    sync bn channels-last test

--- torch/testing/_internal/distributed/distributed_test.py
+++ torch/testing/_internal/distributed/distributed_test.py
@@ -2453,11 +2453,13 @@ class DistributedTest:
             return global_bs, input_cpu, target, loss
 
         # END TO END TEST FOR DISTRIBUTEDDATAPARALLEL
-        def _test_DDP_helper(self, model, input_var, target, loss, scale_factor=1.0):
+        def _test_DDP_helper(self, model, input_var, target, loss, scale_factor=1.0, check_memory_format=None):
             model.train()
             output = model(input_var)
             l = loss(output, target) * scale_factor
             l.backward()
+            if check_memory_format is not None:
+                self.assertTrue(input_var.is_contiguous(memory_format=check_memory_format))
 
         def _assert_equal_param(self, param_gpu, param_DDP):
             self.assertEqual(len(param_gpu), len(param_DDP))
@@ -2466,11 +2468,11 @@ class DistributedTest:
 
         def _test_DDP_5iter(
             self, model_base, model_DDP, input, target, loss, local_bs, rank, batch_size, test_save,
-            offset=None, world_size=0, zero_grad=False
+            offset=None, world_size=0, zero_grad=False, check_memory_format=None
         ):
             for idx in range(5):
                 # single cpu/gpu training
-                self._test_DDP_helper(model_base, input, target, loss)
+                self._test_DDP_helper(model_base, input, target, loss, check_memory_format=check_memory_format)
 
                 if offset is None:
                     offset = rank * local_bs
@@ -2482,6 +2484,7 @@ class DistributedTest:
                     target[offset: offset + local_bs],
                     loss,
                     world_size * local_bs / batch_size if world_size != 0 else 1,
+                    check_memory_format=check_memory_format
                 )
 
                 # Update weights and run a second iteration to shake out errors
@@ -2725,6 +2728,44 @@ class DistributedTest:
                 True,
                 offset,
                 dist.get_world_size()
+            )
+            self._barrier()
+
+        @unittest.skipIf(BACKEND != 'nccl' and BACKEND != 'gloo',
+                         "Only Nccl & Gloo backend support DistributedDataParallel")
+        @skip_if_no_gpu
+        def test_DistributedDataParallel_SyncBatchNorm_Channels_Last(self):
+            group, group_id, rank = self._init_global_test()
+            num_processes = dist.get_world_size()
+            local_bs = 2
+            bs_offset = int(rank * 2)
+            global_bs = int(num_processes * 2)
+
+            model = ONLY_SBN_NET
+            model_gpu = copy.deepcopy(model).cuda(rank)
+            model_DDP = nn.parallel.DistributedDataParallel(
+                model_gpu, device_ids=[rank]
+            )
+
+            memory_format = torch.channels_last
+            input_gpu = torch.randn(global_bs, 2, 4, 4, dtype=torch.float).cuda(rank).to(memory_format=memory_format)
+            target_gpu = torch.randn(global_bs, 2, 4, 4, dtype=torch.float).cuda(rank).to(memory_format=memory_format)
+            loss = nn.MSELoss()
+
+            # check two model parameters over 5 iterations
+            self._test_DDP_5iter(
+                model_gpu,
+                model_DDP,
+                input_gpu,
+                target_gpu,
+                loss,
+                local_bs,
+                rank,
+                global_bs,
+                True,
+                bs_offset,
+                dist.get_world_size(),
+                check_memory_format=memory_format
             )
             self._barrier()
 
