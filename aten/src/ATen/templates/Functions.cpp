50f900035874a093381eeffbf8261c26218fc41e
Edward Yang <ezyang@fb.com>
2020-10-29 14:31:56 -0700

    Delete Vulkan from code generator. (#46938)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46938
    
    It turns out that after https://github.com/pytorch/pytorch/pull/42194
    landed we no longer actually generate any registrations into this
    file.  That means it's completely unnecessary.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Reviewed By: IvanKobzarev
    
    Differential Revision: D24573518
    
    Pulled By: ezyang
    
    fbshipit-source-id: b41ada9e394b780f037f5977596a36b896b5648c

--- aten/src/ATen/templates/Functions.cpp
+++ aten/src/ATen/templates/Functions.cpp
@@ -7,9 +7,6 @@
 #include <ATen/CPUType.h>
 #include <ATen/QuantizedCPUType.h>
 #include <ATen/core/op_registration/hacky_wrapper_for_legacy_signatures.h>
-#ifdef USE_VULKAN
-#include <ATen/VulkanType.h>
-#endif
 
 namespace at {
 
