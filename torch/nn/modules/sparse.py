630a3d56aa6f5919b232258fb9396ce9eb47f210
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-11-02 07:44:17 -0600

    fix behavior of from_pretrained with padding_idx argument

--- torch/nn/modules/sparse.py
+++ torch/nn/modules/sparse.py
@@ -129,10 +129,14 @@ class Embedding(Module):
             assert list(_weight.shape) == [num_embeddings, embedding_dim], \
                 'Shape of weight does not match num_embeddings and embedding_dim'
             self.weight = Parameter(_weight)
+            self._fill_padding_idx_with_zero()
         self.sparse = sparse
 
     def reset_parameters(self) -> None:
         init.normal_(self.weight)
+        self._fill_padding_idx_with_zero()
+
+    def _fill_padding_idx_with_zero(self) -> None:
         if self.padding_idx is not None:
             with torch.no_grad():
                 self.weight[self.padding_idx].fill_(0)
