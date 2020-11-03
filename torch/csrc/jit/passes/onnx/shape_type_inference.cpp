58a5749236581d51e262cf9beaa1912ba7452782
neginraoof <neginmr@utexas.edu>
2020-11-02 22:59:56 -0800

    Fix for feedback

--- torch/csrc/jit/passes/onnx/shape_type_inference.cpp
+++ torch/csrc/jit/passes/onnx/shape_type_inference.cpp
@@ -489,21 +489,24 @@ void ONNXAssignOutputShape(
           PyObject* list_elem = PyList_GET_ITEM(elem, j);
           TORCH_INTERNAL_ASSERT(THPVariable_Check(list_elem));
           auto& new_var = reinterpret_cast<THPVariable*>(list_elem)->cdata;
-          TORCH_CHECK(var.scalar_type() == new_var.scalar_type(), "Unsupported sequence type in model outputs. ONNX supports sequences of elements of the same data type.");
+          TORCH_CHECK(
+              var.scalar_type() == new_var.scalar_type(),
+              "Unsupported sequence type in model outputs. ONNX supports sequences of elements of the same data type.");
         }
         outputs_index += list_len;
         graph->outputs()[i]->setType(ListType::create(
             TensorType::create(var.scalar_type(), at::kCPU, {}, {})));
-        ONNXUpdateTypeFromTensor(
-            graph->outputs()[i], var, onnx_shape_inference);
       }
     } else if (PyTuple_Check(elem)) {
       size_t tuple_len = PyTuple_GET_SIZE(elem);
       if (tuple_len > 0) {
-        at::Tensor var =
-            reinterpret_cast<THPVariable*>(PyTuple_GET_ITEM(elem, 0))->cdata;
-        ONNXUpdateTypeFromTensor(
-            graph->outputs()[i], var, onnx_shape_inference);
+        for (size_t j = 0; j < tuple_len; ++j) {
+          PyObject* tuple_elem = PyTuple_GET_ITEM(elem, j);
+          TORCH_INTERNAL_ASSERT(THPVariable_Check(tuple_elem));
+          auto& var = reinterpret_cast<THPVariable*>(tuple_elem)->cdata;
+          ONNXUpdateTypeFromTensor(
+            graph->outputs()[i+j], var, onnx_shape_inference);
+        }
         outputs_index += tuple_len;
       }
     } else if (THPVariable_Check(elem)) {
@@ -511,8 +514,9 @@ void ONNXAssignOutputShape(
       ONNXUpdateTypeFromTensor(graph->outputs()[i], var, onnx_shape_inference);
       outputs_index++;
     } else { // Dict
-      // Support for dict data type is limited to fixed size dictionaries in ONNX.
-      //Dictionary values are unrolled and keys are not preserved.
+      // Support for dict data type is limited to fixed size dictionaries in
+      // ONNX.
+      // Dictionary values are unrolled and keys are not preserved.
       TORCH_INTERNAL_ASSERT(PyDict_Check(elem));
       auto dict_items = py::reinterpret_borrow<py::list>(PyDict_Items(elem));
       for (size_t j = 0; j < dict_items.size(); ++j) {
