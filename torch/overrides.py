843c07fa70544e5261cf2a8623df54e8f435a942
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-11-03 05:48:08 -0600

    Added overrides.py entry

--- torch/overrides.py
+++ torch/overrides.py
@@ -284,6 +284,7 @@ def get_testing_overrides() -> Dict[Callable, Callable]:
         torch.combinations: lambda input, r=2, with_replacement=False: -1,
         torch.complex: lambda real, imag: -1,
         torch.polar: lambda abs, ang: -1,
+        torch.linalg.cond: lambda input, ord=None: -1,
         torch.conj: lambda input, out=None: -1,
         torch.constant_pad_nd: lambda input, pad, value=0: -1,
         torch.conv1d: lambda input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1: -1,
