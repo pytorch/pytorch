4383c71b2dfb4c1637dd90824dcd7eee8059efd2
Nikita Shulga <nshulga@fb.com>
2020-11-03 11:06:18 -0800

    Add comments + break after libiomp5.dylib has been copied in

--- setup.py
+++ setup.py
@@ -370,33 +370,37 @@ class build_ext(setuptools.command.build_ext.build_ext):
         libtorch_cpu_path = os.path.join(lib_dir, 'libtorch_cpu.dylib')
         libtorch_path = os.path.join(lib_dir, 'libtorch.dylib')
         libtorch_python_path = os.path.join(lib_dir, 'libtorch_python.dylib')
-        otool_cmds = subprocess.check_output(['otool', '-l', libtorch_cpu_path]).decode("utf-8").split("\n")
+        # Parse libtorch_cpu load commands
+        otool_cmds = subprocess.check_output(['otool', '-l', libtorch_cpu_path]).decode('utf-8').split('\n')
         rpaths, libs = [], []
         for idx, line in enumerate(otool_cmds):
-          if line.strip() == 'cmd LC_LOAD_DYLIB':
-              lib_name = otool_cmds[idx + 2].strip()
-              assert lib_name.startswith('name ')
-              libs.append(lib_name.split(' ', 1)[1].rsplit('(', 1)[0][:-1])
+            if line.strip() == 'cmd LC_LOAD_DYLIB':
+                lib_name = otool_cmds[idx + 2].strip()
+                assert lib_name.startswith('name ')
+                libs.append(lib_name.split(' ', 1)[1].rsplit('(', 1)[0][:-1])
 
-          if line.strip() == 'cmd LC_RPATH':
-              rpath = otool_cmds[idx + 2].strip()
-              assert rpath.startswith('path ')
-              rpaths.append(rpath.split(' ',1)[1].rsplit('(', 1)[0][:-1])
+            if line.strip() == 'cmd LC_RPATH':
+                rpath = otool_cmds[idx + 2].strip()
+                assert rpath.startswith('path ')
+                rpaths.append(rpath.split(' ', 1)[1].rsplit('(', 1)[0][:-1])
 
         omp_lib_name = 'libiomp5.dylib'
         if os.path.join('@rpath', omp_lib_name) not in libs:
             return
+
+        # Copy libiomp5 from rpath locations
         for rpath in rpaths:
             source_lib = os.path.join(rpath, omp_lib_name)
             if not os.path.exists(source_lib):
                 continue
             target_lib = os.path.join(self.build_lib, 'torch', 'lib', omp_lib_name)
             self.copy_file(source_lib, target_lib)
+            break
+
         # Delete rpath from those libs
         for rpath in rpaths:
             for lib in [libtorch_cpu_path, libtorch_path, libtorch_python_path]:
                 subprocess.check_call(['install_name_tool', '-delete_rpath', rpath, lib])
-
 
     def run(self):
         # Report build options. This is run after the build completes so # `CMakeCache.txt` exists and we can get an
