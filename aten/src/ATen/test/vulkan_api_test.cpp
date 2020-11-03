60a8be4ee0a0321ec5a0ca730e7321ed777eeda0
ssjia <ssjia@fb.com>
2020-11-02 20:10:14 -0500

    Add vulkan shape op (hack)
    [ghstack-poisoned]

--- aten/src/ATen/test/vulkan_api_test.cpp
+++ aten/src/ATen/test/vulkan_api_test.cpp
@@ -129,6 +129,39 @@ TEST(VulkanAPITest, clamp_) {
   ASSERT_TRUE(almostEqual(a_cpu, a_vulkan.cpu()));
 }
 
+/*
+TEST(VulkanAPITest, view) {
+  const auto a_cpu = at::rand({1,3,2,2}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto a_vulkan = a_cpu.vulkan();
+
+  const auto c_cpu = at::view(a_cpu, {2,3,1,2});
+  const auto c_vulkan = at::view(a_vulkan, {2,3,1,2});
+
+  ASSERT_TRUE(almostEqual(c_cpu, c_vulkan.cpu()));
+}
+*/
+
+TEST(VulkanAPITest, reshape) {
+  const auto a_cpu = at::rand({1,3,2,2}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto a_vulkan = a_cpu.vulkan();
+
+  const auto c_cpu = at::reshape(a_cpu, {2,3,1,2});
+  const auto c_vulkan = at::reshape(a_vulkan, {2,3,1,2});
+
+  ASSERT_TRUE(almostEqual(c_cpu, c_vulkan.cpu()));
+}
+
+
+TEST(VulkanAPITest, reshape_) {
+  const auto a_cpu = at::rand({1,3,2,2}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto a_vulkan = a_cpu.vulkan();
+
+  a_cpu.reshape({2,3,1,2});
+  a_vulkan.reshape({2,3,1,2});
+
+  ASSERT_TRUE(almostEqual(a_cpu, a_vulkan.cpu()));
+}
+
 TEST(VulkanAPITest, copy) {
   const auto cpu = at::rand({13, 17, 37, 19}, at::device(at::kCPU).dtype(at::kFloat));
   ASSERT_TRUE(exactlyEqual(cpu, cpu.vulkan().cpu()));
