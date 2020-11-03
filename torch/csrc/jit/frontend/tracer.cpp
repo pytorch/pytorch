46d28e4d3095bbf03368f18f383da0554e2b9285
Brian Hirsh <hirsheybar@fb.com>
2020-11-03 15:27:35 -0800

    add a backend fallback for tracing, remove the tracing logic from jit
    [ghstack-poisoned]

--- torch/csrc/jit/frontend/tracer.cpp
+++ torch/csrc/jit/frontend/tracer.cpp
@@ -18,6 +18,8 @@
 #include <torch/csrc/jit/passes/remove_expands.h>
 #include <torch/csrc/utils/variadic.h>
 #include <torch/custom_class.h>
+#include <torch/library.h>
+
 
 #include <memory>
 #include <sstream>
@@ -964,11 +966,144 @@ void setWarn(warn_fn_type fn) {
   warn_callback.store(fn);
 }
 } // namespace tracer
+
+void tracingFallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+    const auto input_size = op.schema().arguments().size();
+    const auto output_size = op.schema().returns().size();
+
+    Node* node = nullptr;
+    std::shared_ptr<jit::tracer::TracingState> tracer_state;
+
+    // trace the input before unwrapping, otherwise we may lose
+    // the input information
+    if (jit::tracer::isTracing()) {
+      tracer_state = jit::tracer::getTracingState();
+      auto symbol = Symbol::fromQualString(op.schema().name());
+      const auto& graph = tracer::getTracingState()->graph;
+      node = graph->create(symbol, 0);
+      tracer::recordSourceLocation(node);
+      const auto& args = op.schema().arguments();
+      int i = 0;
+      for (auto iter = stack->end() - input_size; iter != stack->end();
+           ++iter, ++i) {
+        // TODO we need to refactor graph APIs (e.g., addInputs)
+        // appropriately; after that, we can get rid of the giant if-else
+        // block we will clean this tech debt together in the following PRs
+        auto type = args[i].type();
+        if (type->kind() == TypeKind::OptionalType) {
+          if (iter->isNone()) {
+            Value* none = graph->insertNode(graph->createNone())->output();
+            node->addInput(none);
+            continue;
+          } else {
+            type = type->expect<OptionalType>()->getElementType();
+          }
+        }
+        if (type->isSubtypeOf(TensorType::get())) {
+          AT_ASSERT(iter->isTensor());
+          tracer::addInputs(node, args[i].name().c_str(), iter->toTensor());
+        } else if (type->kind() == TypeKind::FloatType) {
+          AT_ASSERT(iter->isDouble());
+          tracer::addInputs(node, args[i].name().c_str(), iter->toDouble());
+        } else if (type->kind() == TypeKind::IntType) {
+          AT_ASSERT(iter->isInt());
+          tracer::addInputs(node, args[i].name().c_str(), iter->toInt());
+        } else if (type->kind() == TypeKind::BoolType) {
+          AT_ASSERT(iter->isBool());
+          tracer::addInputs(node, args[i].name().c_str(), iter->toBool());
+        } else if (type->kind() == TypeKind::StringType) {
+          AT_ASSERT(iter->isString());
+          tracer::addInputs(node, args[i].name().c_str(), iter->toStringRef());
+        } else if (type->kind() == TypeKind::NumberType) {
+          tracer::addInputs(node, args[i].name().c_str(), iter->toScalar());
+        } else if (type->kind() == TypeKind::ListType) {
+          const auto& elem_type = type->expect<ListType>()->getElementType();
+          if (elem_type->isSubtypeOf(TensorType::get())) {
+            AT_ASSERT(iter->isTensorList());
+            auto list = iter->toTensorVector();
+            tracer::addInputs(node, args[i].name().c_str(), list);
+          } else if (auto class_type = elem_type->cast<ClassType>()) {
+            AT_ASSERT(iter->isList());
+            auto list = iter->toList();
+            std::vector<c10::intrusive_ptr<c10::ivalue::Object>> objects;
+            for (IValue iv : list) {
+              objects.emplace_back(std::move(iv).toObject());
+            }
+            tracer::addInputs(
+                node, args[i].name().c_str(), objects, class_type);
+          } else if (elem_type->kind() == TypeKind::FloatType) {
+            AT_ASSERT(iter->isDoubleList());
+            // NB: now, tracer doesn't support tracing double list. We add
+            // special handling here, since in our case, we assume that all the
+            // doubles in the list are constants
+            auto value = iter->toDoubleVector();
+            std::vector<Value*> info(value.size());
+            for (size_t value_index = 0; value_index < value.size();
+                 ++value_index) {
+              info[value_index] = graph->insertConstant(value[value_index]);
+              tracer::recordSourceLocation(info[value_index]->node());
+            }
+            node->addInput(
+                graph
+                    ->insertNode(graph->createList(jit::FloatType::get(), info))
+                    ->output());
+          } else if (elem_type->kind() == TypeKind::IntType) {
+            AT_ASSERT(iter->isIntList());
+            tracer::addInputs(
+                node,
+                args[i].name().c_str(),
+                c10::IntArrayRef(iter->toIntVector()));
+          } else if (elem_type->kind() == TypeKind::BoolType) {
+            AT_ASSERT(iter->isBoolList());
+            tracer::addInputs(
+                node, args[i].name().c_str(), iter->toBoolList().vec());
+          } else {
+            throw std::runtime_error(
+                "unsupported input list type: " + elem_type->str());
+          }
+        } else if (iter->isObject()) {
+          tracer::addInputs(node, args[i].name().c_str(), iter->toObject());
+        } else {
+          throw std::runtime_error("unsupported input type: " + type->str());
+        }
+      }
+      graph->insertNode(node);
+
+      jit::tracer::setTracingState(nullptr);
+    }
+
+    op.callBoxed(stack);
+
+    if (tracer_state) {
+      jit::tracer::setTracingState(std::move(tracer_state));
+      int i = 0;
+      for (auto iter = stack->end() - output_size; iter != stack->end();
+           ++iter, ++i) {
+        const auto& type = op.schema().returns()[i].type();
+        if (type->isSubtypeOf(TensorType::get())) {
+          AT_ASSERT(iter->isTensor());
+          tracer::addOutput(node, iter->toTensor());
+        } else if (type->kind() == TypeKind::ListType) {
+          const auto& elem_type = type->expect<ListType>()->getElementType();
+          if (elem_type->isSubtypeOf(TensorType::get())) {
+            AT_ASSERT(iter->isTensorList());
+            tracer::addOutput(node, iter->toTensorList());
+          } else {
+            throw std::runtime_error(
+                "unsupported ouptut list type: " + elem_type->str());
+          }
+        } else if (type->kind() == TypeKind::ClassType) {
+          AT_ASSERT(iter->isObject());
+          tracer::addOutput(node, iter->toObject());
+        } else {
+          throw std::runtime_error("unsupported output type: " + type->str());
+        }
+      }
+    }
+}
 } // namespace jit
 } // namespace torch
 
 TORCH_LIBRARY_IMPL(_, Tracer, m) {
-  // TODO: register fallback kernel with tracing function from
-  // `torch/csrc/jit/runtime/register_c10_ops.cpp`.
-  m.fallback(torch::CppFunction::makeFallthrough());
+  m.fallback(torch::CppFunction::makeFromBoxedFunction<&torch::jit::tracingFallback>());
 }
