6c287a793ba7c71ea0f41f70cacc123f126c15da
Martin Yuan <iseeyuan@yahoo.com>
2020-11-04 22:18:15 -0800

    Support extra files in _load_for_mobile

--- torch/csrc/jit/mobile/import.h
+++ torch/csrc/jit/mobile/import.h
@@ -11,17 +11,21 @@ namespace jit {
 using caffe2::serialize::FileAdapter;
 using caffe2::serialize::IStreamAdapter;
 using caffe2::serialize::ReadAdapterInterface;
+static ExtraFilesMap default_extra_files;
 
 TORCH_API mobile::Module _load_for_mobile(
     std::istream& in,
-    c10::optional<at::Device> device = c10::nullopt);
+    c10::optional<at::Device> device = c10::nullopt,
+    ExtraFilesMap& extra_files = default_extra_files);
 
 TORCH_API mobile::Module _load_for_mobile(
     const std::string& filename,
-    c10::optional<at::Device> device = c10::nullopt);
+    c10::optional<at::Device> device = c10::nullopt,
+    ExtraFilesMap& extra_files = default_extra_files);
 
 TORCH_API mobile::Module _load_for_mobile(
     std::unique_ptr<ReadAdapterInterface> rai,
-    c10::optional<c10::Device> device = c10::nullopt);
+    c10::optional<c10::Device> device = c10::nullopt,
+    ExtraFilesMap& extra_files = default_extra_files);
 } // namespace jit
 } // namespace torch
