e8596eae01adcd9400c8cad9b20bcc2374bc789d
vasiliy <vasiliy@fb.com>
2020-11-04 21:03:44 -0800

    FixedQParamsFakeQuantize: adjust default quant_min and quant_max
    Summary:
    
    Since the dtype of this fake_quant is `quint8`, the output range should be
    from 0 to 255.  Fixing.  This should address the numerical inaccuracies with
    sigmoid and hardsigmoid with `FixedQParamsFakeQuantize` attached compared
    to their quantized counterparts.
    
    In a future PR, might be safer to also make the activation functions
    using `FixedQParamsFakeQuantize` to explicitly specify their expected
    output range and zero_point.  Leaving that for later, as this bugfix
    should be landed urgently.
    
    Test Plan:
    
    Manual script which gives low SQNR before this PR and high SQNR after
    this PR: https://gist.github.com/vkuzo/9906bae29223da72b10d6b6aafadba42
    
    https://github.com/pytorch/pytorch/pull/47376, which can be landed after
    this, adds a proper test.
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/quantization/fake_quantize.py
+++ torch/quantization/fake_quantize.py
@@ -243,10 +243,13 @@ default_fake_quant = FakeQuantize.with_args(observer=MovingAverageMinMaxObserver
                                             dtype=torch.quint8, qscheme=torch.per_tensor_affine, reduce_range=True)
 default_weight_fake_quant = FakeQuantize.with_args(observer=MovingAverageMinMaxObserver, quant_min=-128, quant_max=127,
                                                    dtype=torch.qint8, qscheme=torch.per_tensor_symmetric, reduce_range=False)
+
+# TODO(future PR): remove these defaults and enforce activation functions
+# to explicitly specify their output range
 default_symmetric_fixed_qparams_fake_quant = FixedQParamsFakeQuantize.with_args(
     scale=2.0 / 256.0, zero_point=128, dtype=torch.quint8)
 default_affine_fixed_qparams_fake_quant = FixedQParamsFakeQuantize.with_args(
-    scale=1.0 / 256.0, zero_point=0, dtype=torch.quint8, quant_min=-128, quant_max=127)
+    scale=1.0 / 256.0, zero_point=0, dtype=torch.quint8, quant_min=0, quant_max=255)
 
 default_per_channel_weight_fake_quant = FakeQuantize.with_args(observer=MovingAveragePerChannelMinMaxObserver,
                                                                quant_min=-128,
