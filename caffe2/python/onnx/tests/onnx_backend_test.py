a7a6dd0052ade1a67139209a2e03d881b55404a2
shubhambhokare1 <shubhambhokare1@gmail.com>
2020-11-06 11:12:39 -0800

    Skipped some ONNX backend tests

--- caffe2/python/onnx/tests/onnx_backend_test.py
+++ caffe2/python/onnx/tests/onnx_backend_test.py
@@ -129,6 +129,21 @@ backend_test.exclude('(test_pow_bcast'
                      '|test_pow_types.*'
                      ')')
 
+# Temporarily skip some ONNX backend tests due to updates in opset 13.
+backend_test.exclude('(test_if_.*'  # added support for sequence type inputs
+                     '|test_if_seq_.*'  # added support for sequence type inputs
+                     '|test_logsoftmax_.*'  # axis attr default value changed from 1 to -1
+                     '|test_loop11_.*'  # seg fault issue
+                     '|test_loop13_seq_.*'  # no support for sequence inputs for scan input
+                     '|test_reduce_sum_.*'  # axes is now an input (not attr), added noop_with_empty_axes
+                     '|test_softmax_.*'  # axis attr default value changed from 1 to -1
+                     '|test_split_variable_parts_.*'  # axes is now an input (not attr)
+                     '|test_squeeze_.*'  # axes is now an input (not attr)
+                     '|test_unsqueeze_.*'  # axes is now an input (not attr)
+                     '|test_MaxPool1d_stride_padding_dilation_.*'
+                     '|test_MaxPool2d_stride_padding_dilation_.*'
+                     ')') 
+
 # Skip vgg to speed up CI
 if 'JENKINS_URL' in os.environ:
     backend_test.exclude(r'(test_vgg19|test_vgg)')
