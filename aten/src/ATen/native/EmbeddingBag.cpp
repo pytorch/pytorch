cfe3123fdbbbca4f0668094db5ce405fc71f3bdc
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-11-05 23:17:59 -0800

    Disable checkContiguous for _embedding_bag_backward for NestedTensorImpl

--- aten/src/ATen/native/EmbeddingBag.cpp
+++ aten/src/ATen/native/EmbeddingBag.cpp
@@ -614,7 +614,9 @@ Tensor _embedding_bag_backward(const Tensor &grad, const Tensor &indices,
                               const Tensor& per_sample_weights) {
   auto indices_arg = TensorArg(indices, "indices", 1);
   checkScalarTypes("embedding_bag", indices_arg, {kLong, kInt});
-  checkContiguous("embedding_bag", indices_arg);
+  if (!is_nested_tensor_impl(indices)) {
+    checkContiguous("embedding_bag", indices_arg);
+  }
   auto offsets_arg = TensorArg(offsets, "offsets", 1);
   checkScalarTypes("embedding_bag", offsets_arg, {kLong, kInt});
   checkSameType("embedding_bag", indices_arg, offsets_arg);
