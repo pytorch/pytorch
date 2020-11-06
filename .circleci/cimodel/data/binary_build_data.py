fb3f5569530a1e098321285036b1055ff8bfec63
Eli Uriegas <eliuriegas@fb.com>
2020-11-06 12:54:08 -0800

    Update on ".circleci: Add python 3.9 to binary build matrix"
    Depends on https://github.com/pytorch/builder/pull/565
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .circleci/cimodel/data/binary_build_data.py
+++ .circleci/cimodel/data/binary_build_data.py
@@ -33,44 +33,44 @@ def get_processor_arch_name(gpu_version):
 
 LINUX_PACKAGE_VARIANTS = OrderedDict(
     manywheel=[
-        "3.6m",
-        "3.7m",
-        "3.8m",
+        # "3.6m",
+        # "3.7m",
+        # "3.8m",
         "3.9m"
     ],
     conda=dimensions.STANDARD_PYTHON_VERSIONS,
-    libtorch=[
-        "3.7m",
-    ],
+    # libtorch=[
+    #     "3.7m",
+    # ],
 )
 
 # TODO: There's an issue with current Python 3.9 builds that only occurs during
 #       windows builds, let's just not build 3.9 for windows and figure out how
 #       to resolve afterwards
-WINDOWS_PYTHON_VERSIONS=[
+PYTHON_VERSIONS_NO_39=[
     v for v in dimensions.STANDARD_PYTHON_VERSIONS if v not in ['3.9']
 ]
 
 CONFIG_TREE_DATA = OrderedDict(
     linux=(dimensions.GPU_VERSIONS, LINUX_PACKAGE_VARIANTS),
-    macos=([None], OrderedDict(
-        wheel=dimensions.STANDARD_PYTHON_VERSIONS,
-        conda=dimensions.STANDARD_PYTHON_VERSIONS,
-        libtorch=[
-            "3.7",
-        ],
-    )),
-    # Skip CUDA-9.2 builds on Windows
-    windows=(
-        [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
-        OrderedDict(
-            wheel=WINDOWS_PYTHON_VERSIONS,
-            conda=WINDOWS_PYTHON_VERSIONS,
-            libtorch=[
-                "3.7",
-            ],
-        )
-    ),
+    # macos=([None], OrderedDict(
+    #     wheel=PYTHON_VERSIONS_NO_39,
+    #     conda=PYTHON_VERSIONS_NO_39,
+    #     libtorch=[
+    #         "3.7",
+    #     ],
+    # )),
+    # # Skip CUDA-9.2 builds on Windows
+    # windows=(
+    #     [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
+    #     OrderedDict(
+    #         wheel=PYTHON_VERSIONS_NO_39,
+    #         conda=PYTHON_VERSIONS_NO_39,
+    #         libtorch=[
+    #             "3.7",
+    #         ],
+    #     )
+    # ),
 )
 
 # GCC config variants:
