566b25c87a78dc4cdb3c0ad8ce3fb42552e25a17
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-11-07 00:48:03 -0600

    make clang-format happy

--- torch/csrc/jit/tensorexpr/kernel.cpp
+++ torch/csrc/jit/tensorexpr/kernel.cpp
@@ -913,8 +913,9 @@ Tensor* TensorExprKernel::computeValue(const torch::jit::Value* v) {
     } break;
 
     case aten::sigmoid: {
-      return computeOneOperand(
-          "aten_sigmoid", v, [](const ExprHandle& a) { return sigmoid(promoteIntegerToFloat(a)); });
+      return computeOneOperand("aten_sigmoid", v, [](const ExprHandle& a) {
+        return sigmoid(promoteIntegerToFloat(a));
+      });
     } break;
 
     case aten::reciprocal: {
