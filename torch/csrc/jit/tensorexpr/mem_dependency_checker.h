a1be92fd90ca4988076565ff7787c69a1572bcc9
Nick Gibson <nickg@fb.com>
2020-11-10 23:01:22 +0000

    [NNC] Make ReduceOp no ltonger deferred

--- torch/csrc/jit/tensorexpr/mem_dependency_checker.h
+++ torch/csrc/jit/tensorexpr/mem_dependency_checker.h
@@ -240,7 +240,6 @@ class TORCH_API MemDependencyChecker : public IRVisitor {
   void visit(const Store* v) override;
   void visit(const Load* v) override;
   void visit(const FunctionCall* v) override;
-  void visit(const ReduceOp* v) override;
   void visit(const For* v) override;
   void visit(const Cond* v) override;
   void visit(const IfThenElse* v) override;
