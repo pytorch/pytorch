cdb1a93c2c6484ceb98b841e9b43baa8354bd2bb
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-11-11 15:56:25 -0800

    Update windows_build_definitions.py

--- .circleci/cimodel/data/windows_build_definitions.py
+++ .circleci/cimodel/data/windows_build_definitions.py
@@ -131,7 +131,7 @@ WORKFLOW_DATA = [
     WindowsJob(None, _VC2019, CudaVersion(10, 1)),
     WindowsJob(1, _VC2019, CudaVersion(10, 1)),
     WindowsJob(2, _VC2019, CudaVersion(10, 1)),
-    # VS2019 CUDA-11.0
+    # VS2019 CUDA-11.1
     WindowsJob(None, _VC2019, CudaVersion(11, 1)),
     WindowsJob(1, _VC2019, CudaVersion(11, 1), master_only_pred=TruePred),
     WindowsJob(2, _VC2019, CudaVersion(11, 1), master_only_pred=TruePred),
