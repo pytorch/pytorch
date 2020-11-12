702e279848208269d75f1fc4878fdd4344b81496
Eli Uriegas <eliuriegas@fb.com>
2020-11-12 09:53:07 -0800

    DO NOT MERGE, testing 3.9 for 1.7.1
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .circleci/cimodel/data/binary_build_data.py
+++ .circleci/cimodel/data/binary_build_data.py
@@ -33,15 +33,15 @@ def get_processor_arch_name(gpu_version):
 
 LINUX_PACKAGE_VARIANTS = OrderedDict(
     manywheel=[
-        "3.6m",
-        "3.7m",
-        "3.8m",
+        # "3.6m",
+        # "3.7m",
+        # "3.8m",
         "3.9m"
     ],
-    conda=dimensions.STANDARD_PYTHON_VERSIONS,
-    libtorch=[
-        "3.7m",
-    ],
+    conda=['3.9'],
+    # libtorch=[
+    #     "3.7m",
+    # ],
 )
 
 # TODO: There's an issue with current Python 3.9 builds that only occurs during
@@ -53,24 +53,24 @@ PYTHON_VERSIONS_NO_39 = [
 
 CONFIG_TREE_DATA = OrderedDict(
     linux=(dimensions.GPU_VERSIONS, LINUX_PACKAGE_VARIANTS),
-    macos=([None], OrderedDict(
-        wheel=PYTHON_VERSIONS_NO_39,
-        conda=PYTHON_VERSIONS_NO_39,
-        libtorch=[
-            "3.7",
-        ],
-    )),
-    # Skip CUDA-9.2 builds on Windows
-    windows=(
-        [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
-        OrderedDict(
-            wheel=PYTHON_VERSIONS_NO_39,
-            conda=PYTHON_VERSIONS_NO_39,
-            libtorch=[
-                "3.7",
-            ],
-        )
-    ),
+    # macos=([None], OrderedDict(
+    #     wheel=PYTHON_VERSIONS_NO_39,
+    #     conda=PYTHON_VERSIONS_NO_39,
+    #     libtorch=[
+    #         "3.7",
+    #     ],
+    # )),
+    # # Skip CUDA-9.2 builds on Windows
+    # windows=(
+    #     [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
+    #     OrderedDict(
+    #         wheel=PYTHON_VERSIONS_NO_39,
+    #         conda=PYTHON_VERSIONS_NO_39,
+    #         libtorch=[
+    #             "3.7",
+    #         ],
+    #     )
+    # ),
 )
 
 # GCC config variants:
