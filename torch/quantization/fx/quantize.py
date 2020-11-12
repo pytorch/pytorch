e55e8591f9ac2ed1e6e0ed384e9d45b0f4bc1eb4
Jerry Zhang <jerryzh168@gmail.com>
2020-11-12 11:51:05 -0800

    [quant][graphmode][fx][refactor] insert_quantize_node
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/quantization/fx/quantize.py
+++ torch/quantization/fx/quantize.py
@@ -716,6 +716,28 @@ class Quantizer:
 
             return quantized
 
+        def insert_quantize_node(node):
+            """ Given a activation_post_process module call node, insert a quantize node"""
+            observer_module = self.modules[node.target]
+            prev_node = node.args[0]
+            if observer_module.dtype == torch.float16:
+                # activations are not quantized for
+                # fp16 dynamic quantization
+                # copy the activaiton_post_process node here
+                # since we may need it when we insert prepack
+                # op for weight of linear, this will be removed
+                # later in a separate pass
+                env[node.name] = self.quantized_graph.node_copy(node, load_non_quantized)
+            elif prev_node.name in quant_env:
+                # if previous node is already quantized, we'll just remove the activation_post_process
+                quant_env[node.name] = quant_env[prev_node.name]
+            else:
+                # replace activation post process with quantization ops
+                root_module = self.modules[""]
+                quant_env[node.name] = quantize_node(
+                    root_module, self.quantized_graph,
+                    load_non_quantized(node.args[0]), observer_module)
+
         for node in model.graph.nodes:
             if node.op == 'output':
                 if is_standalone_module:
@@ -743,31 +765,9 @@ class Quantizer:
                 continue
 
             # handle activation post process calls
-            if node.op == 'call_module':
-                if is_activation_post_process(self.modules[node.target]):
-                    observer_module = self.modules[node.target]
-                    prev_node = node.args[0]
-                    if observer_module.dtype == torch.float16:
-                        # activations are not quantized for
-                        # fp16 dynamic quantization
-                        # copy the activaiton_post_process node here
-                        # since we may need it when we insert prepack
-                        # op for weight of linear, this will be removed
-                        # later in a separate pass
-                        env[node.name] = self.quantized_graph.node_copy(node, load_non_quantized)
-                        continue
-                    if prev_node.name in quant_env:
-                        # if previous node is already quantized, we'll just remove the activation_post_process
-                        quant_env[node.name] = quant_env[prev_node.name]
-                        continue
-                    # replace activation post process with quantization ops
-                    root_module = self.modules['']
-                    quant_env[node.name] = quantize_node(
-                        root_module, self.quantized_graph,
-                        load_non_quantized(node.args[0]), observer_module)
-                    continue
-
-            if is_standalone_module and node.op == 'placeholder' and \
+            if node.op == 'call_module' and is_activation_post_process(self.modules[node.target]):
+                insert_quantize_node(node)
+            elif is_standalone_module and node.op == 'placeholder' and \
                graph_inputs.index(node.name) in model._standalone_module_observed_input_idxs:
                 # the node is quantized in parent module
                 quant_env[node.name] = self.quantized_graph.node_copy(node, load_non_quantized)
