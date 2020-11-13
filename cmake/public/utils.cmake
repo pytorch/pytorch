64c456c3e7456c27746abca2f3a7ab0b96530651
Rong Rong <rongr@fb.com>
2020-11-13 13:19:23 -0800

    clean up more unused cmake functions and marcos

--- cmake/public/utils.cmake
+++ cmake/public/utils.cmake
@@ -82,38 +82,6 @@ macro(caffe_parse_header FILENAME FILE_VAR)
 endmacro()
 
 ################################################################################################
-# Reads single version define from the header file and parses it
-# Usage:
-#   caffe_parse_header_single_define(<library_name> <file> <define_name>)
-function(caffe_parse_header_single_define LIBNAME HDR_PATH VARNAME)
-  set(${LIBNAME}_H "")
-  if(EXISTS "${HDR_PATH}")
-    file(STRINGS "${HDR_PATH}" ${LIBNAME}_H REGEX "^#define[ \t]+${VARNAME}[ \t]+\"[^\"]*\".*$" LIMIT_COUNT 1)
-  endif()
-
-  if(${LIBNAME}_H)
-    string(REGEX REPLACE "^.*[ \t]${VARNAME}[ \t]+\"([0-9]+).*$" "\\1" ${LIBNAME}_VERSION_MAJOR "${${LIBNAME}_H}")
-    string(REGEX REPLACE "^.*[ \t]${VARNAME}[ \t]+\"[0-9]+\\.([0-9]+).*$" "\\1" ${LIBNAME}_VERSION_MINOR  "${${LIBNAME}_H}")
-    string(REGEX REPLACE "^.*[ \t]${VARNAME}[ \t]+\"[0-9]+\\.[0-9]+\\.([0-9]+).*$" "\\1" ${LIBNAME}_VERSION_PATCH "${${LIBNAME}_H}")
-    set(${LIBNAME}_VERSION_MAJOR ${${LIBNAME}_VERSION_MAJOR} ${ARGN} PARENT_SCOPE)
-    set(${LIBNAME}_VERSION_MINOR ${${LIBNAME}_VERSION_MINOR} ${ARGN} PARENT_SCOPE)
-    set(${LIBNAME}_VERSION_PATCH ${${LIBNAME}_VERSION_PATCH} ${ARGN} PARENT_SCOPE)
-    set(${LIBNAME}_VERSION_STRING "${${LIBNAME}_VERSION_MAJOR}.${${LIBNAME}_VERSION_MINOR}.${${LIBNAME}_VERSION_PATCH}" PARENT_SCOPE)
-
-    # append a TWEAK version if it exists:
-    set(${LIBNAME}_VERSION_TWEAK "")
-    if("${${LIBNAME}_H}" MATCHES "^.*[ \t]${VARNAME}[ \t]+\"[0-9]+\\.[0-9]+\\.[0-9]+\\.([0-9]+).*$")
-      set(${LIBNAME}_VERSION_TWEAK "${CMAKE_MATCH_1}" ${ARGN} PARENT_SCOPE)
-    endif()
-    if(${LIBNAME}_VERSION_TWEAK)
-      set(${LIBNAME}_VERSION_STRING "${${LIBNAME}_VERSION_STRING}.${${LIBNAME}_VERSION_TWEAK}" ${ARGN} PARENT_SCOPE)
-    else()
-      set(${LIBNAME}_VERSION_STRING "${${LIBNAME}_VERSION_STRING}" ${ARGN} PARENT_SCOPE)
-    endif()
-  endif()
-endfunction()
-
-################################################################################################
 # Parses a version string that might have values beyond major, minor, and patch
 # and set version variables for the library.
 # Usage:
@@ -210,34 +178,6 @@ function(pycmd outvar cmd)
   string(STRIP "${_output}" _output)
   set(${outvar} "${_output}" PARENT_SCOPE)
 endfunction()
-
-###
-# Helper function to print out everything that cmake knows about a target
-#
-# Copied from https://stackoverflow.com/questions/32183975/how-to-print-all-the-properties-of-a-target-in-cmake
-# This isn't called anywhere, but it's very useful when debugging cmake
-# NOTE: This doesn't work for INTERFACE_LIBRARY or INTERFACE_LINK_LIBRARY targets
-
-function(print_target_properties tgt)
-  if(NOT TARGET ${tgt})
-    message("There is no target named '${tgt}'")
-    return()
-  endif()
-
-  # Get a list of all cmake properties TODO cache this lazily somehow
-  execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
-  string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-  string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-
-  foreach(prop ${CMAKE_PROPERTY_LIST})
-    string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" prop ${prop})
-    get_property(propval TARGET ${tgt} PROPERTY ${prop} SET)
-    if(propval)
-      get_target_property(propval ${tgt} ${prop})
-      message("${tgt} ${prop} = ${propval}")
-    endif()
-  endforeach(prop)
-endfunction(print_target_properties)
 
 
 ##############################################################################
@@ -398,21 +338,6 @@ function(caffe2_hip_binary_target target_name_or_src)
   target_include_directories(${__target} PRIVATE ${Caffe2_HIP_INCLUDE})
 endfunction()
 
-##############################################################################
-# Multiplex between loading executables for CUDA versus HIP (AMD Software Stack).
-# Usage:
-#   torch_cuda_based_add_executable(cuda_target)
-#
-macro(torch_cuda_based_add_executable cuda_target)
-  if(USE_ROCM)
-    hip_add_executable(${cuda_target} ${ARGN})
-  elseif(USE_CUDA)
-    cuda_add_executable(${cuda_target} ${ARGN})
-  else()
-
-  endif()
-endmacro()
-
 
 ##############################################################################
 # Multiplex between adding libraries for CUDA versus HIP (AMD Software Stack).
@@ -559,3 +484,4 @@ function(torch_set_target_props libname)
     set_target_properties(${libname} PROPERTIES STATIC_LIBRARY_FLAGS_DEBUG "/NODEFAULTLIB:${VCOMP_LIB}d")
   endif()
 endfunction()
+
