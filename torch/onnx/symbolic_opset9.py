bb1410495b195d8c8d5eac0d32144d2d8ed03dcf
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-11-12 20:32:44 -0800

    [ONNX] Add export of prim::dtype, prim::tolist (#46019)
    Summary:
    Add export of prim::dtype, prim::tolist.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46019
    
    Reviewed By: malfet
    
    Differential Revision: D24870870
    
    Pulled By: bzinodev
    
    fbshipit-source-id: 7f59e2c8f5ac2dbf83c889c73bd61f96587a296e

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -2254,6 +2254,22 @@ def is_floating_point(g, self):
     return g.op("Constant", value_t=torch.BoolTensor([0]))
 
 
+def prim_dtype(g, self):
+    dtype = sym_help._try_get_scalar_type(self)
+    dtype = sym_help.scalar_type_to_onnx.index(sym_help.cast_pytorch_to_onnx[dtype])
+    return g.op("Constant", value_t=torch.IntTensor([dtype]))
+
+
+# tolist is currently supported only for 1D input tensors.
+# dim_val and elem_ty_val represent dimension and type annotations
+# that need to match dimension and type of the input tensor.
+def prim_tolist(g, input, dim_val, elem_ty_val):
+    dim = sym_help._maybe_get_const(dim_val, 'i')
+    if dim > 1:
+        return _unimplemented("prim_tolist", "dim_val > 1")
+    return input
+
+
 @parse_args('v', 'i')
 def one_hot(g, self, num_classes):
     values = g.op("Constant", value_t=torch.LongTensor([0, 1]))
