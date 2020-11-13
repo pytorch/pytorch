c71519a66742e0dd995f23530b387e9d0d47df90
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-11-13 16:19:50 -0600

    remove one sigmoid event

--- torch/testing/_internal/distributed/rpc/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -67,7 +67,6 @@ EXPECTED_REMOTE_EVENTS = [
     "aten::relu",
     "aten::threshold",
     "aten::sigmoid",
-    "aten::sigmoid",
 ]
 
 # Remote operations are prefixed with the following string for RPC profiling.
