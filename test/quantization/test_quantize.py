pytorch/pytorch

commit 1478e5ec2aa42b2a9742257642c7c1d3203d7309

[quant] Remove nn.quantized.ReLU module and nn.quantized.functional.relu (#47415)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/47415

nn.ReLU works for both float and quantized input, we don't want to define an nn.quantized.ReLU
that does the same thing as nn.ReLU, similarly for nn.quantized.functional.relu

this also removes the numerical inconsistency for models quantizes nn.ReLU independently in qat mode

Test Plan: Imported from OSS

Reviewed By: z-a-f

Differential Revision: D24747035

fbshipit-source-id: b8fdf13e513a0d5f0c4c6c9835635bdf9fdc2769

torch/quantization/quantize.py
@@ -1,4 +1,3 @@
-
 import copy
 import itertools
 import warnings
@@ -137,7 +136,8 @@ def add_observer_(module, qconfig_propagation_list=None, non_leaf_module_list=No
             m._forward_hooks.move_to_end(handle.id, last=False)
 
     for name, child in module.named_children():
-        if type(child) == nnq.FloatFunctional or type(child) == nnq.QFunctional:
+        if type(child) in [nnq.FloatFunctional, nnq.QFunctional] or \
+           isinstance(child, _FusedModule):
             if needs_observation(child):
                 child.activation_post_process = get_activation_post_process(child.qconfig, device)
         elif _has_special_act_post_process(child):

test/quantization/test_quantize.py
@@ -307,8 +307,8 @@ class TestPostTrainingStatic(QuantizationTestCase):
                     self.checkQuantDequant(model.sub)
                     self.checkQuantizedLinear(model.sub.module.fc1)
                     self.checkQuantizedLinear(model.sub.module.fc2)
-                    self.assertEqual(type(model.sub.module.relu1), nnq.ReLU)
-                    self.assertEqual(type(model.sub.module.relu2), nnq.ReLU)
+                    self.assertEqual(type(model.sub.module.relu1), nn.ReLU)
+                    self.assertEqual(type(model.sub.module.relu2), nn.ReLU)
                     self.checkScriptable(model, self.calib_data)
                     self.checkNoQconfig(model)
 
@@ -1248,6 +1248,9 @@ class TestEagerModeOps(QuantizationTestCase):
     def test_leaky_relu(self):
         self._test_activation_op_impl(nn.LeakyReLU, nnq.LeakyReLU, {'negative_slope': 0.1, 'inplace': False})
 
+    def test_relu(self):
+        self._test_activation_op_impl(nn.ReLU, nn.ReLU, {'inplace': False})
+
 
 class TestEagerModeQATOps(QuantizationTestCase):
     def _test_activation_convert_numerics_impl(self, Act, data):
@@ -1324,6 +1327,25 @@ class TestEagerModeQATOps(QuantizationTestCase):
     def test_leaky_relu(self):
         data = torch.randn(1, 3, 2, 4)
         self._test_activation_convert_numerics_impl(nn.LeakyReLU, data)
+
+    def test_relu(self):
+        class M(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.relu = nn.ReLU()
+
+            def forward(self, x):
+                x = self.relu(x)
+                return x
+
+        m = M().train()
+        m.qconfig = default_qconfig
+        m = prepare_qat(m)
+        # make sure no activation_post_process is inserted for relu
+        self.assertFalse(hasattr(m, "activation_post_process"))
+        m = convert(m)
+        # make sure ReLU module is not changed
+        self.assertTrue(type(m.relu), nn.ReLU)
 
 class TestFunctionalModule(QuantizationTestCase):
     # Histogram Observers are slow, so have no-deadline to ensure test doesn't time out

test/quantization/test_quantized_functional.py
@@ -26,7 +26,7 @@ class TestQuantizedFunctional(QuantizationTestCase):
         zero_point = 1
         qX = torch.quantize_per_tensor(X, scale=scale, zero_point=zero_point, dtype=torch.quint8)
         qY = torch.relu(qX)
-        qY_hat = qF.relu(qX)
+        qY_hat = F.relu(qX)
         self.assertEqual(qY, qY_hat)
 
     def _test_conv_api_impl(

test/quantization/test_quantized_module.py
@@ -39,7 +39,8 @@ test please see `caffe2/test/test_quantized_op.py`.
 
 class TestStaticQuantizedModule(QuantizationTestCase):
     def test_relu(self):
-        relu_module = nnq.ReLU()
+        relu_module = nn.ReLU()
+        # TODO: remove nnq.ReLU6 and remove this test
         relu6_module = nnq.ReLU6()
 
         x = torch.arange(-10, 10, dtype=torch.float)
@@ -304,10 +305,11 @@ class TestStaticQuantizedModule(QuantizationTestCase):
             check_save_load=True)
 
         # Test from_float
-        conv_module.qconfig = torch.quantization.default_qconfig
-        torch.quantization.prepare(conv_module, inplace=True)
-        conv_module(X.float())
-        converted_qconv_module = torch.nn.Sequential(conv_module)
+        fused_conv_module = torch.nn.intrinsic._FusedModule(conv_module)
+        fused_conv_module.qconfig = torch.quantization.default_qconfig
+        torch.quantization.prepare(fused_conv_module, inplace=True)
+        fused_conv_module(X.float())
+        converted_qconv_module = fused_conv_module
         torch.quantization.convert(converted_qconv_module, inplace=True)
 
         # Smoke test to make sure the module actually runs

test/quantization/test_quantized_op.py
@@ -216,34 +216,6 @@ class TestQuantizedOps(TestCase):
                         fn_name, q_op, qY, qY_hat
                     ))
 
-    """Tests the correctness of the quantized::relu op."""
-    @override_qengines
-    @given(X=hu.tensor(shapes=hu.array_shapes(1, 5, 1, 5),
-                       qparams=hu.qparams()))
-    def test_qrelu(self, X):
-        relu_test_configs = [
-            {
-                'quantized_fn': [
-                    torch.relu,
-                    torch.relu_,
-                    torch.nn.functional.relu,
-                    torch.nn.quantized.functional.relu,
-                ],
-                'reference_fn': torch.nn.functional.relu
-            },
-            {
-                'quantized_fn': [
-                    torch.nn.functional.relu,
-                    torch.nn.quantized.functional.relu,
-                ],
-                'reference_fn': torch.nn.functional.relu,
-                'extra_kwargs': {
-                    'inplace': True
-                }
-            }
-        ]
-        self._test_activation_function(X, 'relu', relu_test_configs)
-
     """Tests the correctness of the quantized::relu6 op."""
     @override_qengines
     @given(X=hu.tensor(shapes=hu.array_shapes(1, 5, 1, 5),

torch/nn/quantized/functional.py
@@ -410,23 +410,6 @@ def celu(input: Tensor, scale: float, zero_point: int, alpha: Optional[float] = 
     return torch.ops.quantized.celu(input, scale, zero_point, alpha)
 
 
-def relu(input: Tensor, inplace: bool = False) -> Tensor:
-    r"""relu(input, inplace=False) -> Tensor
-
-    Applies the rectified linear unit function element-wise.
-    See :class:`~torch.nn.quantized.ReLU` for more details.
-
-    Args:
-        input: quantized input
-        inplace: perform the computation inplace
-    """
-    if not input.is_quantized:
-        raise ValueError("Input to 'quantized.relu' must be quantized!")
-    if inplace:
-        return torch.relu_(input)
-    else:
-        return torch.relu(input)
-
 def leaky_relu(input: Tensor, negative_slope: float = 0.01, inplace: bool = False,
                scale: float = None, zero_point: int = None):
     r"""

torch/nn/quantized/modules/__init__.py
@@ -2,7 +2,7 @@
 import torch
 from torch.nn.modules.pooling import MaxPool2d
 
-from .activation import ReLU, ReLU6, Hardswish, ELU, LeakyReLU, Sigmoid
+from .activation import ReLU6, Hardswish, ELU, LeakyReLU, Sigmoid
 from .batchnorm import BatchNorm2d, BatchNorm3d
 from .normalization import LayerNorm, GroupNorm, InstanceNorm1d, \
     InstanceNorm2d, InstanceNorm3d
@@ -106,7 +106,6 @@ __all__ = [
     'Linear',
     'MaxPool2d',
     'Quantize',
-    'ReLU',
     'ReLU6',
     'Sigmoid',
     # Wrapper modules

torch/nn/quantized/modules/activation.py
@@ -1,44 +1,6 @@
 import torch
 import torch.nn.quantized.functional
 
-class ReLU(torch.nn.ReLU):
-    r"""Applies quantized rectified linear unit function element-wise:
-
-    :math:`\text{ReLU}(x)= \max(x_0, x)`, where :math:`x_0` is the zero point.
-
-    Please see https://pytorch.org/docs/stable/nn.html#torch.nn.ReLU
-    for more documentation on ReLU.
-
-    Args:
-        inplace: (Currently not supported) can optionally do the operation in-place.
-
-    Shape:
-        - Input: :math:`(N, *)` where `*` means, any number of additional
-          dimensions
-        - Output: :math:`(N, *)`, same shape as the input
-
-    Examples::
-
-        >>> m = nn.quantized.ReLU()
-        >>> input = torch.randn(2)
-        >>> input = torch.quantize_per_tensor(input, 1.0, 0, dtype=torch.qint32)
-        >>> output = m(input)
-    """
-    def __init__(self, inplace=False):
-        super(ReLU, self).__init__(inplace)
-        self.inplace = inplace
-
-    def forward(self, input):
-        return torch.nn.quantized.functional.relu(input, inplace=self.inplace)
-
-    def _get_name(self):
-        return 'QuantizedReLU'
-
-    @staticmethod
-    def from_float(mod):
-        return ReLU(mod.inplace)
-
-
 class ReLU6(torch.nn.ReLU):
     r"""Applies the element-wise function:
 

commit a34a09ef2dfa129a8260343bac5d7045507fd2b6

eager QAT: fix numerics issue introduced by #47415
Summary:

After https://github.com/pytorch/pytorch/pull/47415, numerics for
QAT fused modules in Eager mode regressed.  The reason is that
activations for fused `conv-bn-relu` modules no longer received
calibration data, because they were moved outside of the hook
path.

Fixing by moving these back to the path which adds observers via
the forward hook.

Test Plan:

OSS:
```
python test/test_quantization.py TestQuantizationAwareTraining.test_fused_modules_activations_get_observed
```

FB - verified this fixes the numerical discrepancies between Eager and
FX for reference models.

Reviewers:

Subscribers:

Tasks:

Tags:

[ghstack-poisoned]

torch/quantization/quantize.py
@@ -136,8 +136,7 @@ def add_observer_(module, qconfig_propagation_list=None, non_leaf_module_list=No
             m._forward_hooks.move_to_end(handle.id, last=False)
 
     for name, child in module.named_children():
-        if type(child) in [nnq.FloatFunctional, nnq.QFunctional] or \
-           isinstance(child, _FusedModule):
+        if type(child) in [nnq.FloatFunctional, nnq.QFunctional]:
             if needs_observation(child):
                 child.activation_post_process = get_activation_post_process(child.qconfig, device)
         elif _has_special_act_post_process(child):

test/quantization/test_quantize.py
@@ -1219,6 +1219,30 @@ class TestQuantizationAwareTraining(QuantizationTestCase):
         torch.quantization.convert(model, inplace=True)
         checkHooksIsPresent(model, False)
 
+    @skipIfNoFBGEMM
+    def test_fused_modules_activations_get_observed(self):
+        # TODO(future PR): extend this check, for any model without control flow
+        # all observers must have values populated after data is fed through
+        m = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 1, 1),
+            torch.nn.BatchNorm2d(1),
+            torch.nn.ReLU(),
+        )
+        m = torch.quantization.fuse_modules(m, [['0', '1', '2']], inplace=True)
+        m.qconfig = torch.quantization.default_qconfig
+        m = torch.quantization.prepare_qat(m)
+        # feed some data through
+        input_data = torch.randn(4, 1, 4, 4)
+        m(input_data)
+        # verify all activations received values
+        for name, child in m.named_modules():
+            if isinstance(child, torch.quantization.MinMaxObserver) and \
+                    not isinstance(child, torch.quantization.PerChannelMinMaxObserver):
+                self.assertTrue(child.min_val.item() != float('inf'),
+                                "observer not receiving data")
+                self.assertTrue(child.max_val.item() != float('-inf'),
+                                "observer not receiving data")
+
 class TestEagerModeOps(QuantizationTestCase):
     def _test_activation_op_impl(
             self, float_module_class, quantized_module_class, extra_module_kwargs):