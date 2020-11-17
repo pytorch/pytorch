60340dbab3ec60034f92c200f59b0471857fd852
annieguan <annieguan@devvm2014.vll0.facebook.com>
2020-11-16 23:57:48 -0800

    Update on "Compare Weights FX Implementation"
    FX Compare weights
    
    Differential Revision: [D24940516](https://our.internmc.facebook.com/intern/diff/D24940516/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D24940516/)!
    
    [ghstack-poisoned]

--- test/quantization/test_numeric_suite_fx.py
+++ test/quantization/test_numeric_suite_fx.py
@@ -1,47 +1,34 @@
-import torch
 import copy
 
+import torch
+from torch.quantization import get_default_qconfig
 from torch.quantization._numeric_suite_fx import (
     compare_weights_fx,
     remove_qconfig_observer_fx,
 )
-
-from torch.quantization import (
-    get_default_qconfig,
-)
-
+from torch.quantization.fx.quantize import is_activation_post_process
+from torch.quantization.quantize_fx import convert_fx, fuse_fx, prepare_fx
 from torch.testing._internal.common_quantization import (
-    ConvModel,
     ConvBnModel,
     ConvBNReLU,
+    ConvModel,
     QuantizationTestCase,
-    SingleLayerLinearModel,
     SingleLayerLinearDynamicModel,
-)
-
-from torch.quantization.quantize_fx import (
-    prepare_fx,
-    convert_fx,
-    fuse_fx
-)
-
-from torch.quantization.fx.quantize import (
-    is_activation_post_process
+    SingleLayerLinearModel,
 )
 
 
 class TestGraphModeNumericSuite(QuantizationTestCase):
-
+    @override_qengines
     def test_remove_qconfig_observer_fx(self):
-        r"""Remove activation_post_process node from fx prepred model
-        """
+        r"""Remove activation_post_process node from fx prepred model"""
         float_model = SingleLayerLinearModel()
         float_model.eval()
 
         model_to_quantize = copy.deepcopy(float_model)
         model_to_quantize.eval()
 
-        qconfig = get_default_qconfig('fbgemm')
+        qconfig = get_default_qconfig("fbgemm")
         qconfig_dict = {"": qconfig}
 
         prepared_model = prepare_fx(model_to_quantize, qconfig_dict)
@@ -53,13 +40,12 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
 
         modules = dict(model.named_modules())
         for node in model.graph.nodes:
-            if node.op == 'call_module':
+            if node.op == "call_module":
                 self.assertFalse(is_activation_post_process(modules[node.target]))
 
-
+    @override_qengines
     def test_compare_weights_conv_static_fx(self):
-        r"""Compare the weights of float and static quantized conv layer
-        """
+        r"""Compare the weights of float and static quantized conv layer"""
 
         def calibrate(model, calib_data):
             model.eval()
@@ -75,7 +61,7 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
             for k, v in weight_dict.items():
                 self.assertTrue(v["float"].shape == v["quantized"].shape)
 
-        qconfig = get_default_qconfig('fbgemm')
+        qconfig = get_default_qconfig("fbgemm")
         qconfig_dict = {"": qconfig}
 
         model_list = [ConvModel(), ConvBnModel(), ConvBNReLU()]
@@ -94,10 +80,9 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
 
             compare_and_validate_results(fused, q_model)
 
-
+    @override_qengines
     def test_compare_weights_linear_static_fx(self):
-        r"""Compare the weights of float and static quantized linear layer
-        """
+        r"""Compare the weights of float and static quantized linear layer"""
 
         def calibrate(model, calib_data):
             model.eval()
@@ -119,7 +104,7 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
         model_to_quantize = copy.deepcopy(float_model)
         model_to_quantize.eval()
 
-        qconfig = get_default_qconfig('fbgemm')
+        qconfig = get_default_qconfig("fbgemm")
         qconfig_dict = {"": qconfig}
 
         prepared_model = prepare_fx(model_to_quantize, qconfig_dict)
@@ -135,9 +120,9 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
 
         compare_and_validate_results(model, q_model)
 
+    @override_qengines
     def test_compare_weights_linear_dynamic_fx(self):
-        r"""Compare the weights of float and dynamic quantized linear layer
-        """
+        r"""Compare the weights of float and dynamic quantized linear layer"""
 
         def compare_and_validate_results(float_model, q_model):
             weight_dict = compare_weights_fx(
@@ -146,7 +131,7 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
             self.assertEqual(len(weight_dict), 1)
             for k, v in weight_dict.items():
                 self.assertTrue(len(v["float"]) == len(v["quantized"]))
-                for i, val in enumerate(v['quantized']):
+                for i, val in enumerate(v["quantized"]):
                     self.assertTrue(v["float"][i].shape == v["quantized"][i].shape)
 
         float_model = SingleLayerLinearDynamicModel()
