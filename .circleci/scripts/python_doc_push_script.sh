a216163636320d41ef04bca820443d8e28077a97
Randall Hunt <ranman@fb.com>
2020-11-17 11:33:53 -0800

    speed up docs build with shallow clone of website

--- .circleci/scripts/python_doc_push_script.sh
+++ .circleci/scripts/python_doc_push_script.sh
@@ -42,7 +42,7 @@ fi
 
 echo "install_path: $install_path  version: $version"
 
-git clone https://github.com/pytorch/pytorch.github.io -b $branch
+git clone https://github.com/pytorch/pytorch.github.io -b $branch --depth 1
 pushd pytorch.github.io
 
 export LC_ALL=C
