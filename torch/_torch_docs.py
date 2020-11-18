f877cee6698dcd4a4ab79a04470135287691cb85
kshitij12345 <kshitijkalambarkar@gmail.com>
2020-11-18 11:26:31 -0800

    Add `torch.swapdims` and `torch.swapaxes` (#46041)
    Summary:
    Reference https://github.com/pytorch/pytorch/issues/38349
    
    Delegates to `torch.transpose` (not sure what is the best way to alias)
    
    TODO:
    * [x] Add test
    * [x] Add documentation
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46041
    
    Reviewed By: gchanan
    
    Differential Revision: D25022816
    
    Pulled By: mruberry
    
    fbshipit-source-id: c80223d081cef84f523ef9b23fbedeb2f8c1efc5

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -5648,6 +5648,66 @@ Examples::
             [[-0.8437,  0.1727, -0.1398]]])
 """.format(**common_args))
 
+add_docstr(torch.swapdims, r"""
+swapdims(input, dim0, dim1) -> Tensor
+
+Alias for :func:`torch.transpose`.
+
+This function is equivalent to NumPy's swapaxes function.
+
+Examples::
+
+    >>> x = torch.tensor([[[0,1],[2,3]],[[4,5],[6,7]]])
+    >>> x
+    tensor([[[0, 1],
+            [2, 3]],
+
+            [[4, 5],
+            [6, 7]]])
+    >>> torch.swapdims(x, 0, 1)
+    tensor([[[0, 1],
+            [4, 5]],
+
+            [[2, 3],
+            [6, 7]]])
+    >>> torch.swapdims(x, 0, 2)
+    tensor([[[0, 4],
+            [2, 6]],
+
+            [[1, 5],
+            [3, 7]]])    
+""".format(**common_args))
+
+add_docstr(torch.swapaxes, r"""
+swapaxes(input, axis0, axis1) -> Tensor
+
+Alias for :func:`torch.transpose`.
+
+This function is equivalent to NumPy's swapaxes function.
+
+Examples::
+
+    >>> x = torch.tensor([[[0,1],[2,3]],[[4,5],[6,7]]])
+    >>> x
+    tensor([[[0, 1],
+            [2, 3]],
+
+            [[4, 5],
+            [6, 7]]])
+    >>> torch.swapaxes(x, 0, 1)
+    tensor([[[0, 1],
+            [4, 5]],
+
+            [[2, 3],
+            [6, 7]]])
+    >>> torch.swapaxes(x, 0, 2)
+    tensor([[[0, 4],
+            [2, 6]],
+
+            [[1, 5],
+            [3, 7]]])    
+""".format(**common_args))
+
 add_docstr(torch.narrow,
            r"""
 narrow(input, dim, start, length) -> Tensor
