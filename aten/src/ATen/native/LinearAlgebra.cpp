91e61f92def5219bff1fe6bbc452ca5e27ace2f2
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-11-19 02:44:23 -0600

    Added a todo comment for _out variant

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -147,6 +147,7 @@ Tensor linalg_matrix_rank(const Tensor& self, optional<double> tol, bool hermiti
   }
 }
 
+// TODO: implement _out variant avoiding copy and using already allocated storage directly
 Tensor& linalg_matrix_rank_out(Tensor& result, const Tensor& self, optional<double> tol, bool hermitian) {
   TORCH_CHECK(result.scalar_type() == ScalarType::Long,
     "result dtype ", result.scalar_type(), " does not match the expected dtype ", ScalarType::Long);
