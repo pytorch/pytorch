4a0edd1b7693d7a5a85162889fe274507aa04c8f
Rong Rong <rongr@fb.com>
2020-11-19 09:49:55 -0800

    first commit test

--- aten/src/ATen/test/cuda_optional_test.cu
+++ aten/src/ATen/test/cuda_optional_test.cu
@@ -13,11 +13,15 @@ TEST(OptionalTest, OptionalTestCUDA) {
   if (!at::cuda::is_available()) return;
   c10::optional<int64_t> trivially_destructible;
   c10::optional<std::vector<int64_t>> non_trivially_destructible;
+  c10::optional<std::string> nvcc_compiler_copy_move;
   ASSERT_FALSE(trivially_destructible.has_value());
   ASSERT_FALSE(non_trivially_destructible.has_value());
+  ASSERT_FALSE(nvcc_compiler_copy_move.has_value());
 
   trivially_destructible = {5};
   non_trivially_destructible = std::vector<int64_t>{5, 10};
+  nvcc_compiler_copy_move = std::string("foo");
   ASSERT_TRUE(trivially_destructible.has_value());
   ASSERT_TRUE(non_trivially_destructible.has_value());
+  ASSERT_TRUE(nvcc_compiler_copy_move.has_value());
 }
