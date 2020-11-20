835abbb6d23e3c3b3ecbf4edd152d1ae5857c419
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-11-19 20:50:01 -0600

    add GCS constructor to overrides

--- torch/overrides.py
+++ torch/overrides.py
@@ -132,6 +132,7 @@ def get_ignored_functions() -> Set[Callable]:
         torch.result_type,
         torch.scalar_tensor,
         torch.sparse_coo_tensor,
+        torch.sparse_gcs_tensor,
         torch.tril_indices,
         torch.triu_indices,
         torch.vander,
