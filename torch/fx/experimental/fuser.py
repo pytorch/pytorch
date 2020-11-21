pytorch/pytorch

commit 0a3db1d460aea21a6f09bd7234efd802417734b9

[FX] Prototype Conv/BN fuser in FX (#47657)
Summary:
Some interesting stuff going on. All benchmarks are tested with both my implementation as well as the current quantized fuser.

For these benchmarks, things like using MKLDNN/FBGEMM make a big differene.

## Manual compilation (everything turned off)
In the small case, things look good
```
non-fused:  1.174886703491211
fused:  0.7494957447052002
```

However, for `torchvision.resnet18`, we see
```
non-fused:  1.2272708415985107
fused:  3.7183213233947754
```

This is because Conv (no bias) -> Batch Norm is actually faster than Conv (bias) if you don't have any libraries...

## Nightly (CPU)
```
Toy
non-fused:  0.45807552337646484
fused:  0.34779977798461914

resnet18
non-fused:  0.14216232299804688
fused:  0.13438796997070312

resnet50
non-fused:  0.2999534606933594
fused:  0.29364800453186035

densenet161
non-fused:  0.6558926105499268
fused:  0.6190280914306641

inception_v3
non-fused:  1.2804391384124756
fused:  1.181272029876709
```
with MKLDNN.

We see a small performance gain across the board, with more significant performance gains for smaller models.

## Nightly (CUDA)

```
M
non-fused:  1.2220964431762695
fused:  1.0833759307861328

resnet18
non-fused:  0.09721899032592773
fused:  0.09089207649230957

resnet50
non-fused:  0.2053072452545166
fused:  0.19138741493225098

densenet161
non-fused:  0.6830024719238281
fused:  0.660109281539917
```

Pull Request resolved: https://github.com/pytorch/pytorch/pull/47657

Reviewed By: eellison

Differential Revision: D25127546

Pulled By: Chillee

fbshipit-source-id: ecdf682038def046045fcc09faf9aeb6c459b5e3

torch/fx/experimental/fuser.py
@@ -1,0 +1,63 @@
+import torch.fx as fx
+from torch.nn.utils.fusion import fuse_conv_bn_eval
+from typing import Type, Dict, Any, Tuple, Iterable
+import torch
+import copy
+
+def _parent_name(target : str) -> Tuple[str, str]:
+    """
+    Splits a qualname into parent path and last atom.
+    For example, `foo.bar.baz` -> (`foo.bar`, `baz`)
+    """
+    *parent, name = target.rsplit('.', 1)
+    return parent[0] if parent else '', name
+
+# Works for length 2 patterns with 2 modules
+def matches_module_pattern(pattern: Iterable[Type], node: fx.Node, modules: Dict[str, Any]):
+    if len(node.args) == 0:
+        return False
+    nodes: Tuple[Any, fx.Node] = (node.args[0], node)
+    for expected_type, current_node in zip(pattern, nodes):
+        if not isinstance(current_node, fx.Node):
+            return False
+        if current_node.op != 'call_module':
+            return False
+        if not isinstance(current_node.target, str):
+            return False
+        if current_node.target not in modules:
+            return False
+        if type(modules[current_node.target]) is not expected_type:
+            return False
+    return True
+
+
+def replace_node_module(node: fx.Node, modules: Dict[str, Any], new_module: torch.nn.Module):
+    parent_name, name = _parent_name(node.target)
+    setattr(modules[parent_name], name, new_module)
+
+def fuse(model: torch.nn.Module, inplace=True) -> torch.nn.Module:
+    """
+    Fuses convolution/BN layers for inference purposes. Will deepcopy your
+    model by default, but can modify the model inplace as well.
+    """
+    patterns = [(torch.nn.Conv1d, torch.nn.BatchNorm1d),
+                (torch.nn.Conv2d, torch.nn.BatchNorm2d),
+                (torch.nn.Conv3d, torch.nn.BatchNorm3d)]
+    if not inplace:
+        model = copy.deepcopy(model)
+    fx_model = fx.symbolic_trace(model)
+    modules = dict(fx_model.named_modules())
+    new_graph = copy.deepcopy(fx_model.graph)
+
+    for pattern in patterns:
+        for node in new_graph.nodes:
+            if matches_module_pattern(pattern, node, modules):
+                if len(node.args[0].users) > 1:  # Output of conv is used by other nodes
+                    continue
+                conv = modules[node.args[0].target]
+                bn = modules[node.target]
+                fused_conv = fuse_conv_bn_eval(conv, bn)
+                replace_node_module(node.args[0], modules, fused_conv)
+                node.replace_all_uses_with(node.args[0])
+                new_graph.erase_node(node)
+    return fx.GraphModule(fx_model, new_graph)

test/test_fx_experimental.py
@@ -18,6 +18,7 @@ from torch.fx.experimental.partitioner_utils import (
     PartitionerConfig,
     PartitionMode
 )
+from torch.fx.experimental.fuser import fuse
 from typing import Union, Callable
 
 try:
@@ -474,6 +475,19 @@ class TestFXExperimental(JitTestCase):
                 new_target=operator.mul,
             )
             assert (input1 * input2) == traced(input1, input2)
+
+    @skipIfNoTorchVision
+    def test_conv_bn_fusion(self):
+        rn18 = resnet18().eval()
+        traced = symbolic_trace(rn18)
+        fused = fuse(traced)
+
+        self.assertTrue(all(not isinstance(m, torch.nn.BatchNorm2d) for m in fused.modules()))
+
+        N, C, H, W = 20, 3, 224, 224
+        inp = torch.randn(N, C, H, W)
+
+        self.assertEqual(fused(inp), rn18(inp))
 
     def test_call_to_assert_no_msg(self):
         class M(torch.nn.Module):

torch/fx/__init__.pyi
@@ -1,0 +1,5 @@
+from .graph import Graph as Graph
+from .graph_module import GraphModule as GraphModule
+from .node import Node as Node, map_arg as map_arg
+from .proxy import Proxy as Proxy
+from .symbolic_trace import Tracer as Tracer, symbolic_trace as symbolic_trace

commit 08092ab02bb1d0649fa1b46d67fef39e2b27f86c

Fix mypy error (#48359)
Summary:
Fixes error introduced in https://github.com/pytorch/pytorch/pull/47657

`node.target` can be either a str or a callable, but this is checked in the pattern matching portion.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/48359

Reviewed By: ilia-cher

Differential Revision: D25141885

Pulled By: Chillee

fbshipit-source-id: 94365a5a3dd351652ea7337077cd0e71b6ffe203

torch/fx/experimental/fuser.py
@@ -32,6 +32,7 @@ def matches_module_pattern(pattern: Iterable[Type], node: fx.Node, modules: Dict
 
 
 def replace_node_module(node: fx.Node, modules: Dict[str, Any], new_module: torch.nn.Module):
+    assert(isinstance(node.target, str))
     parent_name, name = _parent_name(node.target)
     setattr(modules[parent_name], name, new_module)
 