59f3e50f03b769a29a925d1431c4137094d167b2
wayi <wayi@devgpu227.prn2.facebook.com>
2020-11-20 17:43:14 -0800

    Update on "[Gradient Compression] clang-format test_c10d.py"
    Apply clang-format only.
    
    Original PR issue: Investigate Applying PowerSGD to Communication Hook for Gradient Compression #47202
    
    Differential Revision: [D25138833](https://our.internmc.facebook.com/intern/diff/D25138833/)
    
    [ghstack-poisoned]

--- test/distributed/test_c10d.py
+++ test/distributed/test_c10d.py
@@ -3544,7 +3544,7 @@ class DistributedDataParallelTest(MultiProcessTestCase):
         self._test_grad_layout(replica_devices, layer_devs, local_batch_size)
 
     @unittest.skipIf(
-        True, "Reenable when DDP with multiple GPUs per process is confirmed to work"
+        True, "Re-enable when DDP with multiple GPUs per process is confirmed to work"
     )
     @requires_nccl()
     @skip_if_lt_x_gpu(4)
