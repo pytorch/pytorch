750f2ce35a32e172bfcd3be8c84ff86172177ee6
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-11-30 14:21:44 -0800

    Tweak Vulkan memory use. (#47728)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/47728
    
    Test Plan: Imported from OSS
    
    Reviewed By: SS-JIA
    
    Differential Revision: D25032740
    
    Pulled By: AshkanAliabadi
    
    fbshipit-source-id: 7eb72538dc1aa3feb4e2f8c4ff9c675eb8e97057

--- aten/src/ATen/native/vulkan/glsl/conv2d_pw.glsl
+++ aten/src/ATen/native/vulkan/glsl/conv2d_pw.glsl
@@ -6,7 +6,7 @@ layout(std430) uniform;
 
 /* Qualifiers: layout - storage - precision - memory */
 
-layout(set = 0, binding = 0, rgba32f) uniform PRECISION restrict writeonly image3D   uOutput;
+layout(set = 0, binding = 0, rgba16f) uniform PRECISION restrict writeonly image3D   uOutput;
 layout(set = 0, binding = 1)          uniform PRECISION                    sampler3D uInput;
 layout(set = 0, binding = 2)          uniform PRECISION                    sampler3D uKernel;
 layout(set = 0, binding = 3)          buffer  PRECISION restrict readonly  Bias {
@@ -20,7 +20,7 @@ layout(set = 0, binding = 4)          uniform PRECISION restrict           Block
   int W;
 } uBlock;
 
-layout(local_size_x_id = 1, local_size_y_id = 2, local_size_z_id = 3) in;
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
