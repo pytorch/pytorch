118c96fd6433abca9f13dd9e5c47753259021d8b
Jerry Zhang <jerryzh168@gmail.com>
2020-12-01 15:15:40 -0800

    Update on "[reland][quant][fix] Add bias once in conv_fused (#48593)"
    Summary:
    Previously _conv_forward will add self.bias to the result, so bias is added twice in qat ConvBn module
    this PR added a bias argument to _conv_forward and _conv_forward is called with zero bias
    in ConvBn module
    
    fixes: https://github.com/pytorch/pytorch/issues/48514
    
    Test Plan: Imported from OSS
    
    Reviewed By: raghuramank100
    
    Differential Revision: [D25249175](https://our.internmc.facebook.com/intern/diff/D25249175)
    
    [ghstack-poisoned]

--- test/quantization/test_qat_module.py
+++ test/quantization/test_qat_module.py
@@ -110,7 +110,10 @@ class _ReferenceConvBnNd(torch.nn.Conv2d, torch.nn.modules.conv._ConvNd):
         running_std = torch.sqrt(self.running_var + self.eps)
         scale_factor = self.gamma / running_std
         scaled_weight = self.weight * scale_factor.reshape([-1, 1, 1, 1])
-        zero_bias = torch.zeros_like(self.bias)
+        if self.bias:
+            zero_bias = torch.zeros_like(self.bias)
+        else:
+            zero_bias = torch.zeros(self.out_channels, device=scaled_weight.device())
         conv = self._conv_forward(input, self.weight_fake_quant(scaled_weight), zero_bias)
 
         if self.training and not self.freeze_bn:
