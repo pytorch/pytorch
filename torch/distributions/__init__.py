fa5cffa18fd20272c579d74504694ee70f93506b
Qifan Lu <lqf.1996121@gmail.com>
2020-12-02 15:37:56 -0800

    Add reparameterization support to `OneHotCategorical` (#46610)
    Summary:
    Add reparameterization support to the `OneHotCategorical` distribution. Samples are reparameterized based on the straight-through gradient estimator, which is proposed in the paper [Estimating or Propagating Gradients Through Stochastic Neurons for Conditional Computation](https://arxiv.org/abs/1308.3432).
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46610
    
    Reviewed By: neerajprad
    
    Differential Revision: D25272883
    
    Pulled By: ezyang
    
    fbshipit-source-id: 8364408fe108a29620694caeac377a06f0dcdd84

--- torch/distributions/__init__.py
+++ torch/distributions/__init__.py
@@ -101,7 +101,7 @@ from .multinomial import Multinomial
 from .multivariate_normal import MultivariateNormal
 from .negative_binomial import NegativeBinomial
 from .normal import Normal
-from .one_hot_categorical import OneHotCategorical
+from .one_hot_categorical import OneHotCategorical, OneHotCategoricalStraightThrough
 from .pareto import Pareto
 from .poisson import Poisson
 from .relaxed_bernoulli import RelaxedBernoulli
@@ -144,6 +144,7 @@ __all__ = [
     'NegativeBinomial',
     'Normal',
     'OneHotCategorical',
+    'OneHotCategoricalStraightThrough',
     'Pareto',
     'RelaxedBernoulli',
     'RelaxedOneHotCategorical',
