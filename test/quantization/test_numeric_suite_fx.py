8aace4a88aed20f5dbdf19bb2d7ddcdc3f60c195
annieguan <annieguan@devvm1759.ftw0.facebook.com>
2020-12-02 15:02:15 -0800

    compare_model_stub_fx API
    compare_model_stub_fx API implementation
    
    Differential Revision: [D25265534](https://our.internmc.facebook.com/intern/diff/D25265534/)
    
    [ghstack-poisoned]

--- test/quantization/test_numeric_suite_fx.py
+++ test/quantization/test_numeric_suite_fx.py
@@ -1,10 +1,12 @@
 import copy
 
 import torch
+import torch.nn as nn
 from torch.quantization import get_default_qconfig
 from torch.quantization._numeric_suite_fx import (
     compare_weights_fx,
     remove_qconfig_observer_fx,
+    compare_model_stub_fx,
 )
 from torch.quantization.fx.quantize import is_activation_post_process
 from torch.quantization.quantize_fx import convert_fx, fuse_fx, prepare_fx
@@ -142,3 +144,90 @@ class TestGraphModeNumericSuite(QuantizationTestCase):
         q_model = convert_fx(prepared_model)
 
         compare_and_validate_results(backup_prepared_model, q_model)
+
+    @skipIfNoFBGEMM
+    def test_compare_model_stub_conv_static_fx(self):
+        r"""Compare the output of static quantized conv layer and its float shadow module"""
+
+        def calibrate(model, calib_data):
+            model.eval()
+            with torch.no_grad():
+                for inp in calib_data:
+                    model(*inp)
+
+        def compare_and_validate_results(float_model, q_model, module_swap_list, data):
+            ob_dict = compare_model_stub_fx(
+                float_model, q_model, module_swap_list, data
+            )
+            self.assertEqual(len(ob_dict), 1)
+            for k, v in ob_dict.items():
+                self.assertTrue(len(v["float"]) == len(v["quantized"]))
+                for i, val in enumerate(v["quantized"]):
+                    self.assertTrue(v["float"][i].shape == v["quantized"][i].shape)
+
+        qengine = torch.backends.quantized.engine
+        qconfig = get_default_qconfig(qengine)
+        qconfig_dict = {"": qconfig}
+
+        model_list = [ConvModel(), ConvBNReLU()]
+
+        for float_model in model_list:
+            float_model.eval()
+
+            prepared_model = prepare_fx(float_model, qconfig_dict)
+
+            backup_prepared_model = copy.deepcopy(prepared_model)
+
+            # Run calibration
+            calibrate(prepared_model, self.img_data_2d)
+            q_model = convert_fx(prepared_model)
+
+            module_swap_list = [nn.Conv2d, nn.intrinsic.modules.fused.ConvReLU2d]
+            compare_and_validate_results(
+                backup_prepared_model,
+                q_model,
+                module_swap_list,
+                self.img_data_2d[0][0],
+            )
+
+    @skipIfNoFBGEMM
+    def test_compare_model_stub_linear_static_fx(self):
+        r"""Compare the output of static quantized linear layer and its float shadow module"""
+
+        def calibrate(model, calib_data):
+            model.eval()
+            with torch.no_grad():
+                for inp in calib_data:
+                    model(*inp)
+
+        def compare_and_validate_results(float_model, q_model, module_swap_list, data):
+            ob_dict = compare_model_stub_fx(
+                float_model, q_model, module_swap_list, data
+            )
+            self.assertEqual(len(ob_dict), 1)
+            for k, v in ob_dict.items():
+                self.assertTrue(len(v["float"]) == len(v["quantized"]))
+                for i, val in enumerate(v["quantized"]):
+                    self.assertTrue(v["float"][i].shape == v["quantized"][i].shape)
+
+        float_model = SingleLayerLinearModel()
+        float_model.eval()
+
+        qengine = torch.backends.quantized.engine
+        qconfig = get_default_qconfig(qengine)
+        qconfig_dict = {"": qconfig}
+
+        prepared_model = prepare_fx(float_model, qconfig_dict)
+
+        backup_prepared_model = copy.deepcopy(prepared_model)
+
+        # Run calibration
+        calibrate(prepared_model, self.calib_data)
+        q_model = convert_fx(prepared_model)
+
+        linear_data = self.calib_data[0][0]
+        module_swap_list = [nn.Linear]
+
+        compare_and_validate_results(
+            backup_prepared_model, q_model, module_swap_list, linear_data
+        )
