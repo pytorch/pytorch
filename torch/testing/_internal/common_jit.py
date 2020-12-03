6dc47ae1b26b62e139971f9e02b535bb95eb747f
Lillian Johnson <lillianjohnson@fb.com>
2020-12-02 19:52:39 -0800

    Refactored assertAutodiff test to have better error message (#48567)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/48567
    
    Test Plan: Imported from OSS
    
    Reviewed By: eellison
    
    Differential Revision: D25212435
    
    Pulled By: Lilyjjo
    
    fbshipit-source-id: eab3933bf4248dbfa20cd956d4a0106b10db5fc4

--- torch/testing/_internal/common_jit.py
+++ torch/testing/_internal/common_jit.py
@@ -143,18 +143,78 @@ class JitCommonTestCase(TestCase):
             torch.jit.save(imported, fname)
             return torch.jit.load(fname, map_location=map_location)
 
+    def autoDiffErrorMessage(self, should_autodiff_node, nodes_not_in_diff_graph, 
+                             fusion_nodes_not_found, non_fusible_nodes_being_fused, 
+                             fusion_nodes_found, nodes_in_diff_graph):
+        err_msg = "\nFailure in testing nodes' autodifferentiation, "
+        if should_autodiff_node:
+            err_msg += "one or more nodes were expected to be autodiffed, " \
+                "but were not found in specified fusible/nonfusible " \
+                "DifferentiableGraph groups. \nSpecifically:"
+            for node in nodes_not_in_diff_graph:
+                err_msg += f"\n  {node} was not in one of the DifferentiableGraphs " \
+                    "when it was expected to be. Did you intend for this node to be " \
+                    "autodiffed? If not, remove it from the list of nonfusible nodes."
+                if node in non_fusible_nodes_being_fused:
+                    err_msg += "Additionally, This node was found in a FusionGroup " \
+                        "in a DifferentiableGraph. If that was intended, " \
+                        "reclassify this node as a fusible node. If not, your " \
+                        "autodifferention logic might be wrong."
+            for node in fusion_nodes_not_found:
+                err_msg += f"\n  {node} was not in one of the DifferentiableGraphs' " \
+                    "fusion groups when it was expected to be. " \
+                    "Did you intend for this node to be fused? If not, you should " \
+                    "move this node into the test's non-fusible nodes."
+        else: 
+            err_msg += "one or more nodes were not expected to be autodiffed, " \
+                "but were found in a fused/nonfused DifferentiableGraph group. " \
+                "Did you intend for these nodes to be autodiffed? " \
+                "If so, change this test to expect autodifferention. " \
+                "\nSpecifically:"
+            for node in fusion_nodes_found:
+                err_msg += f"\n  {node} was not expected to in one of the " \
+                    "DifferentiableGraph's fusion groups but was. "
+            for node in nodes_in_diff_graph:
+                err_msg += f"\n  {node} was not expected to be in a " \
+                    "DifferentiableGraph but was."
+        return err_msg
+
     def assertAutodiffNode(self, graph, should_autodiff_node, nonfusible_nodes, fusible_nodes):
         diff_nodes = graph.findAllNodes('prim::DifferentiableGraph')
         diff_subgraphs = [node.g('Subgraph') for node in diff_nodes]
 
-        # For any non-fusible nde, it must show up in one of the DifferentiableGraph.
-        found_all_nonfusible_nodes = (len(diff_subgraphs) == 0 and len(nonfusible_nodes) == 0) \
-            or all([any(g.findNode(n) is not None for g in diff_subgraphs) for n in nonfusible_nodes])
-
-        # For any fusible node, it must show up in one of the FusionGroup in the DifferentiableGraph.
+        # Note: currently no tests have fusible_nodes
         fusion_nodes = list(chain.from_iterable([g.findAllNodes('prim::FusionGroup') for g in diff_subgraphs]))
         fusion_subgraphs = [node.g('Subgraph') for node in fusion_nodes]
-        found_all_fusible_nodes = (len(fusion_nodes) == 0 and len(fusible_nodes) == 0) \
-            or all([any(g.findNode(n) is not None for g in fusion_subgraphs) for n in fusible_nodes])
 
-        self.assertEqual(should_autodiff_node, found_all_nonfusible_nodes and found_all_fusible_nodes)
+        # For any non-fusible node, it must show up in one of the DifferentiableGraphs.
+        nodes_in_diff_graph = []
+        nodes_not_in_diff_graph = []
+        non_fusible_nodes_being_fused = []
+        for node in nonfusible_nodes:
+            if any(g.findNode(node) is not None for g in diff_subgraphs):
+                nodes_in_diff_graph.append(node)
+            else: 
+                nodes_not_in_diff_graph.append(node)
+            if any(g.findNode(node) is not None for g in fusion_subgraphs):
+                non_fusible_nodes_being_fused.append(node)
+        found_all_nonfusible_nodes = len(nodes_in_diff_graph) == len(nonfusible_nodes)
+
+        # For any fusible node, it must show up in one of the FusionGroups in one of the DifferentiableGraphs.
+        fusion_nodes_found = []
+        fusion_nodes_not_found = []
+        for node in fusible_nodes:
+            if any(g.findNode(node) is not None for g in fusion_subgraphs):
+                fusion_nodes_found.append(node)
+            else:
+                fusion_nodes_not_found.append(node) 
+        found_all_fusible_nodes = len(fusion_nodes_found) == len(fusible_nodes)    
+
+        err_msg = self.autoDiffErrorMessage(should_autodiff_node, 
+                                            nodes_not_in_diff_graph, 
+                                            fusion_nodes_not_found, 
+                                            non_fusible_nodes_being_fused,
+                                            fusion_nodes_found, 
+                                            nodes_in_diff_graph)
+        self.assertEqual(should_autodiff_node, 
+                         found_all_nonfusible_nodes and found_all_fusible_nodes, err_msg)  
