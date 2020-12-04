692d67ed64c93f12e1be4948c2ee26054b557fba
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2020-12-04 00:04:47 -0800

    cusolver gesvd impl done [benchmark slower than magma]

--- aten/src/ATen/cuda/CUDASolver.h
+++ aten/src/ATen/cuda/CUDASolver.h
@@ -42,6 +42,24 @@ template<>
 void getrs<c10::complex<float>>(CUDASOLVER_GETRS_ARGTYPES(c10::complex<float>));
 
 
+#define CUDASOLVER_GESVD_ARGTYPES(Dtype, Vtype)  \
+    cusolverDnHandle_t handle, char jobu, char jobvt, int m, int n, Dtype* A, int lda, Vtype* S, Dtype* U, int ldu, Dtype* VT, \
+    int ldvt, int* devInfo
+
+template<class Dtype, class Vtype>
+void gesvd(CUDASOLVER_GESVD_ARGTYPES(Dtype, Vtype)) {
+  TORCH_CHECK(false, "at::cuda::solver::gesvd: not implemented for ", typeid(Dtype).name());
+}
+template<>
+void gesvd<float>(CUDASOLVER_GESVD_ARGTYPES(float, float));
+template<>
+void gesvd<double>(CUDASOLVER_GESVD_ARGTYPES(double, double));
+template<>
+void gesvd<c10::complex<float>>(CUDASOLVER_GESVD_ARGTYPES(c10::complex<float>, float));
+template<>
+void gesvd<c10::complex<double>>(CUDASOLVER_GESVD_ARGTYPES(c10::complex<double>, double));
+
+
 } // namespace solver
 } // namespace cuda
 } // namespace at
--- aten/src/ATen/native/cuda/BatchLinearAlgebraLib.cu
+++ aten/src/ATen/native/cuda/BatchLinearAlgebraLib.cu
@@ -116,16 +116,56 @@ Tensor _inverse_helper_cuda_lib(const Tensor& self) {
   return self_inv_working_copy;
 }
 
-
 template<typename scalar_t>
-static void apply_svd_lib(Tensor& self, Tensor& U, Tensor& S, Tensor& VT, char jobchar, std::vector<int64_t>& infos) {
+static void _apply_svd_lib_gesvd(Tensor& self, Tensor& U, Tensor& S, Tensor& VT, char jobchar, Tensor& infos) {
+  using value_t = typename c10::scalar_value_type<scalar_t>::type;
+  auto self_data = self.data_ptr<scalar_t>();
+  auto U_data = U.data_ptr<scalar_t>();
+  auto S_data = S.data_ptr<value_t>();
+  auto VT_data = VT.data_ptr<scalar_t>();
+  auto self_stride = matrixStride(self);
+  auto U_stride = matrixStride(U);
+  auto S_stride = S.size(-1);
+  auto VT_stride = matrixStride(VT);
+  auto batchsize = batchCount(self);
 
+  int m = cuda_int_cast(self.size(-2), "m");
+  int n = cuda_int_cast(self.size(-1), "n");
+  TORCH_INTERNAL_ASSERT(m >= n, "apply_svd_lib requires m >= n, but got m = ", m, ", n = ", n);
+
+  CUDA_PARALLEL_STREAM_LAUNCH(i, batchsize, [&] {
+    auto handle = at::cuda::getCurrentCUDASolverDnHandle();
+    at::cuda::solver::gesvd<scalar_t>(handle, jobchar, jobchar, m, n, self_data + i * self_stride,
+      m, S_data + i * S_stride, U_data + i * U_stride, m, VT_data + i * VT_stride, n, infos.data_ptr<int>() + i);
+  });
+}
+
+// cusolver gesvd with CUDA_PARALLEL_STREAM_LAUNCH
+inline static void apply_svd_lib_gesvd(const Tensor& self, Tensor& U, Tensor& S, Tensor& VT, char jobchar, Tensor& infos) {
+  const int64_t m = self.size(-2), n = self.size(-1);
+  if (m >= n) {
+    Tensor self_working_copy = cloneBatchedColumnMajor(self);
+
+    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(self.scalar_type(), "svd_cuda", [&] {
+      _apply_svd_lib_gesvd<scalar_t>(self_working_copy, U, S, VT, jobchar, infos);
+    });
+  } else {
+    // cusolver gesvd requires the matrix dimensions m, n satisfy m >= n
+    Tensor self_working_copy = cloneBatchedColumnMajor(self.transpose(-2, -1));
+
+    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(self.scalar_type(), "svd_cuda", [&] {
+      _apply_svd_lib_gesvd<scalar_t>(self_working_copy, VT, S, U, jobchar, infos);
+    });
+
+    VT = VT.transpose(-2, -1);
+    U = U.transpose(-2, -1);
+  }
 }
 
 std::tuple<Tensor, Tensor, Tensor> _svd_helper_cuda_lib(const Tensor& self, bool some, bool compute_uv) {
-  std::vector<int64_t> infos(batchCount(self), 0);
-  int64_t m = self.size(-2), n = self.size(-1);
-  int64_t k = std::min(m, n);
+  at::Tensor infos = at::zeros({batchCount(self)}, self.options().dtype(at::kInt));
+  const int64_t m = self.size(-2), n = self.size(-1);
+  const int64_t k = std::min(m, n);
 
   char jobchar = compute_uv ? (some ? 'S' : 'A') : 'N';
 
@@ -135,17 +175,10 @@ std::tuple<Tensor, Tensor, Tensor> _svd_helper_cuda_lib(const Tensor& self, bool
   // U, S, V working copies are already column majored now
 
   if (self.numel() > 0) {
-    Tensor self_working_copy = cloneBatchedColumnMajor(self);
 
-    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(self.scalar_type(), "svd_cuda", [&] {
-      apply_svd_lib<scalar_t>(self_working_copy, U_working_copy, S_working_copy, VT_working_copy, jobchar, infos);
-    });
+    apply_svd_lib_gesvd(self, U_working_copy, S_working_copy, VT_working_copy, jobchar, infos);
 
-    if (self.dim() > 2) {
-      batchCheckErrors(infos, "svd_cuda");
-    } else {
-      singleCheckErrors(infos[0], "svd_cuda");
-    }
+    batchCheckErrors(infos, "svd_cuda");
 
     if (compute_uv) {
       if (some) {
