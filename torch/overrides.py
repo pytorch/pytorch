cb285080b0f82e8133a2e8a35c9ecdb182458d09
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-12-04 02:21:55 -0800

    Added computing matrix condition numbers (linalg.cond) (#45832)
    Summary:
    This PR adds `torch.linalg.cond` for NumPy compatibility.
    
    Ref https://github.com/pytorch/pytorch/issues/42666.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45832
    
    Reviewed By: ngimel
    
    Differential Revision: D25183690
    
    Pulled By: mruberry
    
    fbshipit-source-id: a727959bfec2bc2dc36df59d9ef79c0534b68194

--- torch/overrides.py
+++ torch/overrides.py
@@ -318,6 +318,7 @@ def get_testing_overrides() -> Dict[Callable, Callable]:
         torch.complex: lambda real, imag: -1,
         torch.copysign: lambda input, other, out=None: -1,
         torch.polar: lambda abs, ang: -1,
+        torch.linalg.cond: lambda input, ord=None: -1,
         torch.conj: lambda input, out=None: -1,
         torch.constant_pad_nd: lambda input, pad, value=0: -1,
         torch.conv1d: lambda input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1: -1,
