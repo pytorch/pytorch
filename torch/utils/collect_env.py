54579ca11d9753abad1144f30e8609517994e22b
peterjc123 <peterghost86@gmail.com>
2020-12-08 23:38:47 +0800

    Using os.path and Doing path refactor

--- torch/utils/collect_env.py
+++ torch/utils/collect_env.py
@@ -163,7 +163,11 @@ def get_nvidia_smi():
     # Note: nvidia-smi is currently available only on Windows and Linux
     smi = 'nvidia-smi'
     if get_platform() == 'win32':
-        smis = [f'C:\\Program Files\\NVIDIA Corporation\\NVSMI\\{smi}', f'C:\\Windows\\System32\\{smi}']
+        system_root = os.environ.get('SYSTEMROOT', 'C:\\Windows')
+        program_files_root = os.environ.get('PROGRAMFILES', 'C:\\Program Files')
+        legacy_path = os.path.join(program_files_root, 'NVIDIA Corporation', 'NVSMI', smi)
+        new_path = os.path.join(system_root, 'System32', smi)
+        smis = [new_path, legacy_path]
         for candidate_smi in smis:
             if os.path.exists(candidate_smi):
                 smi = f'"{candidate_smi}"'
@@ -189,7 +193,7 @@ def get_mac_version(run_lambda):
 
 
 def get_windows_version(run_lambda):
-    system_root = os.environ.get('SystemRoot', 'C:\\Windows')
+    system_root = os.environ.get('SYSTEMROOT', 'C:\\Windows')
     wmic_cmd = os.path.join(system_root, 'System32', 'Wbem', 'wmic')
     findstr_cmd = os.path.join(system_root, 'System32', 'findstr')
     return run_and_read_all(run_lambda, '{} os get Caption | {} /v Caption'.format(wmic_cmd, findstr_cmd))
