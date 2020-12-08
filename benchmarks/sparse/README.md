2a36dbb6ae13c78e6c7bc376609e151055bba827
Alexander <aocsa.cs@gmail.com>
2020-12-08 09:42:41 -0500

    minor update
    add description  to the bench test
    
    
    minor fix

--- benchmarks/sparse/README.md
+++
@@ -1,5 +0,0 @@
-# Sparse benchmarks
-
-These benchmarks are for the sparse matrix functionality. They exist for  
-comparing the performance of sparse matrix routines `torch.sparse.mm(sparse, sparse)` with different backends (CPU/CUDA)
-and with other frameworks such as scipy. 
--- benchmarks/sparse/matmul_dlmc_bench.py
+++ benchmarks/sparse/matmul_dlmc_bench.py
@@ -1,3 +1,10 @@
+# Sparse benchmarks
+
+# These benchmarks are for the sparse matrix functionality. 
+# They exist for comparing the performance of sparse matrix routines
+# torch.sparse.mm(sparse, sparse)` with different backends (CPU/CUDA)
+# and with other frameworks such as scipy. 
+
 import sys
 from scipy import sparse
 import numpy as np
