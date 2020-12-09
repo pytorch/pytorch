ce13a8bf7539d119f51f36be86463ba9ca03e858
Marian Ivanov <majo.ivanov@gmail.com>
2020-12-09 22:58:33 +0100

    Removed dead code

--- aten/src/ATen/native/SummaryOps.cpp
+++ aten/src/ATen/native/SummaryOps.cpp
@@ -3,7 +3,6 @@
 #include <ATen/ATen.h>
 #include <ATen/Dispatch.h>
 #include <ATen/NumericUtils.h>
-#include <ATen/native/SummaryOpsUtils.h>
 
 #include <tuple>
 
@@ -108,6 +107,31 @@ Tensor _histogram_cpu_template_uniform_bins(
 
   return hist;
 
+}
+
+// A helper function to promote the histogram to the correct type if needed and
+// normalize it when density is set to true
+Tensor _histogram_normalize_density(
+    const Tensor& self,
+    const Tensor& edges,
+    bool uniform_bins) {
+  auto scalar_target = c10::get_default_dtype();
+  if (self.is_floating_point()) {
+    scalar_target = self.scalar_type();
+  }
+  Tensor output = self.to(scalar_target);
+  if (uniform_bins) {
+    int64_t nbins = self.size(0);
+    double bin_volume =
+        (edges[nbins].item<double>() - edges[0].item<double>()) /
+        static_cast<double>(nbins);
+    output /= bin_volume * output.sum();
+  } else {
+    output /= output.sum() *
+        (edges.slice(0, 1, edges.numel()) - edges.slice(0, 0, -1))
+            .to(scalar_target);
+  }
+  return output;
 }
 
 } // namespace
