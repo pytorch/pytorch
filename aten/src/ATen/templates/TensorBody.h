9b98647ff8a49530bbb9564700b33f684fc0136f
James Donald <jdonald@fb.com>
2020-12-11 06:40:07 -0800

    [caffe2][a10] Move down pragma pop to properly suppress warning 4522
    Summary:
    As the comments on line 160, say we should suppress this overly aggressive warning with MSVC:
    ```
    caffe2\tensorbody.h_ovrsource#header-mode-symlink-tree-only,headers\aten\core\tensorbody.h(1223): warning C4522: 'at::Tensor': multiple assignment operators specified
    ```
    
    However, in order to remove the warning, the closing brace of the class must be between the`#pragma warning` push and its corresponding pop. Move the pop down to ensure that.
    
    Test Plan: Built locally using clang for Windows without buck cache, confirmed the warning resolved
    
    Differential Revision: D25422447
    
    fbshipit-source-id: 369b27fafb106662d70e79d83c2c914deaef454b

--- aten/src/ATen/templates/TensorBody.h
+++ aten/src/ATen/templates/TensorBody.h
@@ -194,10 +194,6 @@ class CAFFE2_API Tensor {
   Tensor& operator=(const Tensor&) &&;
   Tensor& operator=(Tensor&&) &&;
 
-  #ifdef _MSC_VER
-  #pragma warning( pop )
-  #endif
-
   bool is_same(const Tensor& other) const noexcept {
     return impl_ == other.impl_;
   }
@@ -729,6 +725,10 @@ protected:
   void enforce_invariants();
   c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl> impl_;
 };
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
 
 int64_t get_device(Tensor self);
 
