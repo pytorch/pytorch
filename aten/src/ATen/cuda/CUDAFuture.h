5ab90b2fda9a94e45c26be6e567e8d3fbf0e6dfa
Luca Wehrstedt <lcw@fb.com>
2020-12-11 03:33:58 -0800

    Make CUDAFuture remember and restore current device in callback (#48789)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48789
    
    CUDAFuture aims to "capture" the current state of CUDA-related stuff when the future is marked complete (e.g., by looking at current streams and recording events on them) and then "replicate" a similar state when users synchronize with the result of the future (by synchronizing the current streams with these events).
    
    However, one "contextual" aspect of CUDA that we weren't capturing/replicating was the current device. This diff tries to fix that. I must mention that we can only do this for callbacks, while we cannot do it for the wait() method. I don't know if such a discrepancy between the two actually makes the overall behavior _worse_. I'd love to hear people's opinions on this.
    ghstack-source-id: 118081338
    
    Test Plan: Unit tests
    
    Reviewed By: mrshenli
    
    Differential Revision: D25210335
    
    fbshipit-source-id: 1d1a3f80b1cc42e5114bc88554ed50617f1aaa90

--- aten/src/ATen/cuda/CUDAFuture.h
+++ aten/src/ATen/cuda/CUDAFuture.h
@@ -44,6 +44,8 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
   }
 
   void postMarkCompletedHook(const at::IValue& value) override {
+    currentDevice_ = c10::cuda::current_device();
+
     // Extract them once and cache them for later uses.
     dataPtrs_ = extractDataPtrs(value);
 
@@ -98,6 +100,8 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
         }
       }
 
+      c10::cuda::CUDAGuard deviceGuard(currentDevice_);
+
       callback();
     };
   }
@@ -122,6 +126,10 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
   // Once WorkNCCL is gone (as part of the Future and Work merge) this should be
   // fixed.
  protected:
+  // The device that was current when markCompleted was called, which we'll
+  // restore when invoking callbacks.
+  c10::DeviceIndex currentDevice_;
+
   // The events that correspond to the completion of the async I/O kernels. They
   // are recorded on the appropriate streams when the future is marked completed
   // and can then be queried/waited/blocked on. There is one event for each
