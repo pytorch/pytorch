d3b87fe05fff72899637f66562e2bf5faa70ccd8
peter <peterghost86@gmail.com>
2020-12-12 18:48:06 +0800

    Revert "PyLong_{As/From}{Long/UnsignedLong} lint checks"
    This reverts commit b00f39ad59933dc139613148df7b1d6667382a87.

--- torch/csrc/python_headers.h
+++ torch/csrc/python_headers.h
@@ -17,16 +17,3 @@
 #error "Python 2 has reached end-of-life and is no longer supported by PyTorch."
 #endif
 
-// Check for Python APIs that interact with long types on Windows.
-// As those usages will lead to potential errors since `sizeof(long) == 4`.
-#ifdef _WIN32
-#undef PyLong_FromLong
-#undef PyLong_AsLong
-#undef PyLong_FromUnsignedLong
-#undef PyLong_AsUnsignedLong
-
-#define PyLong_FromLong(...)         static_assert(false, "Usage of PyLong_FromLong may cause problems on Windows. Please use THPUtils_packInt64 in torch/csrc/utils/python_numbers.h instead.")
-#define PyLong_AsLong(...)           static_assert(false, "Usage of PyLong_AsLong may cause problems on Windows. Please use THPUtils_unpackLong in torch/csrc/utils/python_numbers.h instead.")
-#define PyLong_FromUnsignedLong(...) static_assert(false, "Usage of PyLong_FromUnsignedLong may cause problems on Windows. Please use THPUtils_packUInt64 in torch/csrc/utils/python_numbers.h instead.")
-#define PyLong_AsUnsignedLong(...)   static_assert(false, "Usage of PyLong_AsUnsignedLong may cause problems on Windows. Please use THPUtils_unpackUInt64 in torch/csrc/utils/python_numbers.h instead.")
-#endif
