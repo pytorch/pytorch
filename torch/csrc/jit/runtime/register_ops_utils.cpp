38f3b387ef2b49515eb45df353d32ecbc3dae3c8
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2020-12-14 12:17:07 -0800

    Update on "Striding for lists Part 2"
    Summary: In this PR, we replace all definitions of slice to take None parameters for the start, end, and step. This will simplify the compiler logic
    
    Test Plan: test_jit test cases
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/register_ops_utils.cpp
+++ torch/csrc/jit/runtime/register_ops_utils.cpp
@@ -428,14 +428,13 @@ void listMulIntRight(Stack* stack) {
 }
 
 void listSlice(Stack* stack) {
-
   auto step_val = pop(stack);
   auto end_val = pop(stack);
   auto start_val = pop(stack);
 
-  int64_t step = step_val.isInt()? step_val.to<int64_t>() : 1;
-  int64_t end = end_val.isInt()? end_val.to<int64_t>() : INT64_MAX;
-  int64_t start = start_val.isInt()? start_val.to<int64_t>() : INT64_MAX;
+  int64_t step = step_val.isInt() ? step_val.to<int64_t>() : 1;
+  int64_t end = end_val.isInt() ? end_val.to<int64_t>() : INT64_MAX;
+  int64_t start = start_val.isInt() ? start_val.to<int64_t>() : INT64_MAX;
 
   c10::List<IValue> list = pop(stack).to<c10::List<IValue>>();
 
