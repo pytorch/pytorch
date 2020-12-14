3c6dcaf4d8e9e965749ccf924f8f2efef111a6e1
nikithamalgifb <70345919+nikithamalgifb@users.noreply.github.com>
2020-12-14 11:39:50 -0800

    Update torch/csrc/jit/runtime/register_cuda_ops.cpp
    Co-authored-by: SplitInfinity <meghanl@fb.com>

--- torch/csrc/jit/runtime/register_cuda_ops.cpp
+++ torch/csrc/jit/runtime/register_cuda_ops.cpp
@@ -1,6 +1,6 @@
 #ifdef USE_CUDA
 
-// This file registers special JIT operators used to implement the CUDA PyTorch API in TorchScript.
+// This file registers special JIT operators used to implement the PyTorch CUDA API in TorchScript.
 #include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/api/include/torch/utils.h>
 #include <torch/csrc/jit/runtime/custom_operator.h>
