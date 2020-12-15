5fd9ab2995833d9d8e31c8f98fb366ea423a0d42
Yi Zhang <zhanyi@microsoft.com>
2020-12-14 21:49:59 -0800

    improve major version extraction

--- .circleci/scripts/windows_cudnn_install.sh
+++ .circleci/scripts/windows_cudnn_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eux -o pipefail
 
-if [[ "$CUDA_VERSION" =~ ^10.* ]]; then
+cuda_major_version=${CUDA_VERSION%.*}
+
+if [[ "$cuda_major_version" == "10" ]]; then
     cudnn_installer_name="cudnn-${CUDA_VERSION}-windows10-x64-v7.6.4.38"
-elif [[ "$CUDA_VERSION" =~ ^11.* ]]; then
+elif [[ "$cuda_major_version" == "11" ]]; then
     cudnn_installer_name="cudnn-${CUDA_VERSION}-windows-x64-v8.0.5.39"
 else
     echo "CUDNN for CUDA_VERSION $CUDA_VERSION is not supported yet"
