260a6076dc2f91539b947210fb20f5724737b4f8
Stas Bekman <stas@stason.org>
2020-12-15 17:32:57 -0800

    fix is_ninja_available

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1405,13 +1405,12 @@ def is_ninja_available():
     Returns ``True`` if the `ninja <https://ninja-build.org/>`_ build system is
     available on the system, ``False`` otherwise.
     '''
-    with open(os.devnull, 'wb') as devnull:
-        try:
-            subprocess.check_call('ninja --version'.split(), stdout=devnull)
-        except OSError:
-            return False
-        else:
-            return True
+    try:
+        subprocess.check_output('ninja --version'.split())
+    except:
+        return False
+    else:
+        return True
 
 
 def verify_ninja_availability():
