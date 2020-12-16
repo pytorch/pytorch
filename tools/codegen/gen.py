98e93d790300ccd132e68796b4f002a85c22bf03
Edward Z. Yang <ezyang@fb.com>
2020-12-16 12:35:52 -0800

    Move default or no default logic into native.argument
    Previously, it was done at a use site, but that meant other use
    sites don't get the right logic.  Pushing it in makes sure everyone
    gets it.
    
    I also fixed one case of confusion where defn() was used to define a decl().
    If you want to define a declaration with no defaults, say no_default().decl()
    which is more direct and will give us code reviewers a clue if you should
    have pushed this logic in.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- tools/codegen/gen.py
+++ tools/codegen/gen.py
@@ -724,17 +724,7 @@ struct CAFFE2_API structured_{n} : public at::meta::{meta_name} {{
                 if is_structured_dispatch_key(k):
                     continue
                 seen.add(n)
-                if f.func.is_out_fn() and local.use_c10_dispatcher() is UseC10Dispatcher.full:
-                    # out overloads don't get default arguments because
-                    # defaulted arguments would be before the out argument
-                    # in the argument list and that doesn't work.
-                    # TODO We should consider if we just want to remove
-                    # default arguments from all at::native functions
-                    # but that would be a larger change because we need
-                    # to change a lot of call sites
-                    args_str = ', '.join(a.defn() for a in args)
-                else:
-                    args_str = ', '.join(a.decl() for a in args)
+                args_str = ', '.join(a.decl() for a in args)
                 rs.append(f"CAFFE2_API {returns_type} {n}({args_str});")
 
         return rs
@@ -1088,7 +1078,7 @@ def compute_registration_declarations(f: NativeFunction) -> str:
     name = dispatcher.name(f.func)
     returns_type = dispatcher.returns_type(f.func.returns)
     args = dispatcher.arguments(f.func)
-    args_str = ', '.join(a.defn() for a in args)
+    args_str = ', '.join(a.no_default().decl() for a in args)
     comment_data : Dict[str, str] = {
         'schema': f'aten::{f.func}',
         # TODO: What exactly is the semantics of the 'dispatch' field?
