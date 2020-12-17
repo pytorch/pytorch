8d6bce81b6ddf7e70733ae719ae705f828209c81
Martin Yuan <myuan@fb.com>
2020-12-17 14:01:54 -0800

    Revert D25152559: T66557700 Support default argument values of a method
    Test Plan: revert-hammer
    
    Differential Revision:
    D25152559 (https://github.com/pytorch/pytorch/commit/6bde0ca6d35bb42e1dee67da802ee0ea4ebdb6e1)
    
    Original commit changeset: bbf52f1fbdbf
    
    fbshipit-source-id: 592fdb3078b1ac86cd394adc6c1bfd6b10d829e1

--- caffe2/serialize/inline_container.h
+++ caffe2/serialize/inline_container.h
@@ -133,20 +133,14 @@ constexpr uint64_t kMaxSupportedFileFormatVersion = 0x5L;
 //      when given bool or integer fill values.
 constexpr uint64_t kProducedFileFormatVersion = 0x3L;
 
-// The version we write when the archive contains bytecode.
+// the version we write when the archive contains bytecode.
 // It must be higher or eq to kProducedFileFormatVersion.
 // Because torchscript changes is likely introduce bytecode change.
 // If kProducedFileFormatVersion is increased, kProducedBytecodeVersion
 // should be increased too. The relationship is:
 // kMaxSupportedFileFormatVersion >= (most likely ==) kProducedBytecodeVersion
 //   >= kProducedFileFormatVersion
-// Versions:
-//  0x1L: Initial version
-//  0x2L: (Comment missing)
-//  0x3L: (Comment missing)
-//  0x4L: (Comment missing)
-//  0x5L: Added schema to function tuple
-constexpr uint64_t kProducedBytecodeVersion = 0x5L;
+constexpr uint64_t kProducedBytecodeVersion = 0x4L;
 
 static_assert(kProducedBytecodeVersion >= kProducedFileFormatVersion,
     "kProducedBytecodeVersion must be higher or equal to kProducedFileFormatVersion.");
