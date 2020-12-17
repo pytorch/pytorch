08fd21f768a345859f133e71b51c7bcc4911bb42
Elias Ellison <eellison@fb.com>
2020-12-16 18:28:56 -0800

    Add fusion support of aten::to (#48976)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/48976
    
    Test Plan: Imported from OSS
    
    Reviewed By: ZolotukhinM
    
    Differential Revision: D25413164
    
    Pulled By: eellison
    
    fbshipit-source-id: 0c31787e8b5e1368b0cba6e23660799b652389cd

--- torch/csrc/jit/tensorexpr/llvm_codegen.cpp
+++ torch/csrc/jit/tensorexpr/llvm_codegen.cpp
@@ -826,13 +826,19 @@ void LLVMCodeGenImpl::visit(const BoolImm* v) {
   value_ = llvm::ConstantInt::get(BoolTy_, v->value());
 }
 
+llvm::Type* llvmTypeToVec(llvm::Type* type, int lanes) {
+  if (lanes > 1) {
+    return llvm::VectorType::get(type, ElementCount(lanes));
+  } else {
+    return type;
+  }
+}
+
 void LLVMCodeGenImpl::visit(const Cast* v) {
   v->src_value()->accept(this);
 
-  llvm::Type* dstType = dtypeToLLVM(v->dtype());
-  if (v->dtype().lanes() > 1) {
-    dstType = llvm::VectorType::get(dstType, ElementCount(v->dtype().lanes()));
-  }
+  llvm::Type* dstType =
+      llvmTypeToVec(dtypeToLLVM(v->dtype()), v->dtype().lanes());
   llvm::Type* srcType = dtypeToLLVM(v->src_value()->dtype());
 
   if (srcType == dstType) {
@@ -846,8 +852,27 @@ void LLVMCodeGenImpl::visit(const Cast* v) {
   // Scalar casts
   if (srcType->isFPOrFPVectorTy()) {
     if (dstType->isFPOrFPVectorTy()) {
+      // as with eager, convert from Double -> Half by Converting to Float then
+      // Half. TODO: __truncdfhf2
+      if (v->dtype().scalar_type() == ScalarType::Half &&
+          v->src_value()->dtype().scalar_type() == ScalarType::Double) {
+        value_ = irb_.CreateFPCast(
+            value_, llvmTypeToVec(FloatTy_, v->dtype().lanes()));
+      }
       value_ = irb_.CreateFPCast(value_, dstType);
     } else if (dstType->isIntOrIntVectorTy()) {
+      // Strictly casting from Float -> i8 doesnt give correct results
+      // set one bit true if the input float is not 0
+      if (v->dtype().scalar_type() == ScalarType::Bool) {
+        llvm::Value* zero =
+            toVec(llvm::ConstantFP::get(srcType, 0.), v->dtype().lanes());
+        value_ = irb_.CreateFCmp(llvm::FCmpInst::FCMP_UNO, value_, zero);
+        value_ = irb_.CreateICmpEQ(
+            value_, llvm::ConstantInt::get(value_->getType(), 0));
+        value_ = irb_.CreateIntCast(value_, dstType, !destUnsigned);
+        return;
+      }
+
       if (destUnsigned) {
         value_ = irb_.CreateFPToUI(value_, dstType);
       } else {
