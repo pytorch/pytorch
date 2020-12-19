2e0162266a4235083e4e98408f59faa92ff33f99
Hei Law <heilaw@umich.edu>
2020-12-18 22:30:42 -0500

    fixed a bug which causes AttributeError if we construct CosineAnnealingWarmRestarts with last_epoch greater than or equal to 0

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -962,9 +962,8 @@ class CosineAnnealingWarmRestarts(_LRScheduler):
         self.T_mult = T_mult
         self.eta_min = eta_min
 
+        self.T_cur = 0 if last_epoch < 0 else last_epoch
         super(CosineAnnealingWarmRestarts, self).__init__(optimizer, last_epoch, verbose)
-
-        self.T_cur = self.last_epoch
 
     def get_lr(self):
         if not self._get_lr_called_within_step:
