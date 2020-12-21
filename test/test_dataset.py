b41031566912bdbfe317d240db97b835ba19aacb
Erjia Guan <erjia@fb.com>
2020-12-21 07:02:31 -0800

    [WIP][DataLoader] CollateIterableDataset prototype (#48933)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48933
    
    Prototype for CollateIterableDataset.
    Move `collate_batch_fn` to BatchIterableDataset
    
    - CollateIterableDataset
      - [x] Prototype
      - [x] Tests
    - BatchIterableDataset
      - [x] Prototype
      - [x] Tests
    - SamplerIterableDataset
      - [x] Prototype
      - [x] Tests
    
    Test Plan: Imported from OSS
    
    Reviewed By: mrshenli
    
    Differential Revision: D25623635
    
    Pulled By: ejguan
    
    fbshipit-source-id: 99ba077619f672551ac15367baaba985db35a9c2

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,9 +1,12 @@
 import tempfile
 import warnings
 
+import torch
 from torch.testing._internal.common_utils import (TestCase, run_tests)
+from torch.utils.data import IterableDataset
+from torch.utils.data.datasets import \
+    (CollateIterableDataset, ListDirFilesIterableDataset, LoadFilesFromDiskIterableDataset)
 
-from torch.utils.data.datasets import (ListDirFilesIterableDataset, LoadFilesFromDiskIterableDataset)
 
 def create_temp_dir_and_files():
     # The temp dir and files within it will be released and deleted in tearDown().
@@ -44,6 +47,56 @@ class TestIterableDatasetBasic(TestCase):
         for rec in dataset2:
             self.assertTrue(rec[0] in self.temp_files)
             self.assertTrue(rec[1].read() == open(rec[0], 'rb').read())
+
+
+class IterDatasetWithoutLen(IterableDataset):
+    def __init__(self, ds):
+        super().__init__()
+        self.ds = ds
+
+    def __iter__(self):
+        for i in self.ds:
+            yield i
+
+
+class IterDatasetWithLen(IterableDataset):
+    def __init__(self, ds):
+        super().__init__()
+        self.ds = ds
+        self.length = len(ds)
+
+    def __iter__(self):
+        for i in self.ds:
+            yield i
+
+    def __len__(self):
+        return self.length
+
+
+class TestFunctionalIterableDataset(TestCase):
+    def test_collate_dataset(self):
+        arrs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        ds_len = IterDatasetWithLen(arrs)
+        ds_nolen = IterDatasetWithoutLen(arrs)
+
+        def _collate_fn(batch):
+            return torch.tensor(sum(batch), dtype=torch.float)
+
+        collate_ds = CollateIterableDataset(ds_len, collate_fn=_collate_fn)
+        self.assertEqual(len(ds_len), len(collate_ds))
+        ds_iter = iter(ds_len)
+        for x in collate_ds:
+            y = next(ds_iter)
+            self.assertEqual(x, torch.tensor(sum(y), dtype=torch.float))
+
+        collate_ds_nolen = CollateIterableDataset(ds_nolen)
+        with self.assertRaises(NotImplementedError):
+            len(collate_ds_nolen)
+        ds_nolen_iter = iter(ds_nolen)
+        for x in collate_ds_nolen:
+            y = next(ds_nolen_iter)
+            self.assertEqual(x, torch.tensor(y))
+
 
 if __name__ == '__main__':
     run_tests()
