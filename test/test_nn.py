1d0aa2d0921162a70acd36141b2e53f9615a12da
M.L. Croci <mlc70@eng.cam.ac.uk>
2020-12-22 19:00:04 +0000

    Add loss description in docstring

--- test/test_nn.py
+++ test/test_nn.py
@@ -4678,7 +4678,7 @@ class TestNN(NNTestCase):
         input = torch.tensor([[0.5, 1.5, 2.5], [2., 4., 6.]])
         target = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
         var = torch.tensor([[0.5, 1., 1.5], [1., 1.5, 2.]])
-        component_wise_loss = 0.5*(torch.sum(torch.log(var) + (input - target)**2/var, dim=1))
+        component_wise_loss = 0.5*(torch.sum(torch.log(var) + (input - target)**2/var, dim=1)) + 1.5*math.log(2*math.pi)
         self.assertEqual(component_wise_loss,
                          F.gaussian_nll_loss(input, target, var,
                          reduction='none'))
