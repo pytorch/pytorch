7425656cc20100ff1e47ad7fb85b953913d2c470
Qifan Lu <lqf.1996121@gmail.com>
2020-12-24 19:21:27 -0800

    Pickling support for `torch.Generator`

--- torch/csrc/Generator.cpp
+++ torch/csrc/Generator.cpp
@@ -119,8 +119,43 @@ void initGeneratorBindings(PyObject* module) {
       .def_property_readonly(
           "device",
           [](const Generator& gen) {
-              return py::handle((PyObject*)THPDevice_New(gen.device()));
-          });
+            return py::handle((PyObject*)THPDevice_New(gen.device()));
+          })
+      .def(py::pickle(
+          /* __getstate__ */
+          [](Generator& gen) {
+            HANDLE_TH_ERRORS
+            // See Note [Acquire lock when using random generators]
+            std::lock_guard<std::mutex> lock(gen.mutex());
+            py::handle py_device = (PyObject*)THPDevice_New(gen.device());
+            auto state_tensor = gen.state();
+            // `__getstate__` currently returns a 3-tuple: (state_version, device, state_tensor)
+            // `state_version` is added for backward compatibility when changes are made to
+            // the state tuple. Currently it is always set to 0.
+            return py::make_tuple((uint64_t)0, py_device, state_tensor);
+            END_HANDLE_TH_ERRORS_PYBIND
+          },
+          /* __setstate__ */
+          [](std::tuple<uint64_t, py::object, Tensor&> state_tuple) {
+            HANDLE_TH_ERRORS
+            auto state_version = std::get<0>(state_tuple);
+            auto py_device = std::get<1>(state_tuple).ptr();
+            // Currently only state version 0 is supported
+            TORCH_CHECK(state_version == 0, "unsupported RNG state version ", state_version);
+            TORCH_CHECK_TYPE(
+              THPDevice_Check(py_device),
+              "expect torch.device for state tuple element 0, got ", Py_TYPE(py_device)->tp_name
+            );
+
+            auto& device = ((THPDevice*)py_device)->device;
+            auto new_state_tensor = std::get<2>(state_tuple);
+            // FIXME support state restore beyond CPU and CUDA generators
+            auto gen = pyCreateGenerator(device);
+            // No need to lock because we are the sole owner of the generator
+            gen.set_state(new_state_tensor);
+            return gen;
+            END_HANDLE_TH_ERRORS_PYBIND
+          }));
 }
 
 } // namespace torch
