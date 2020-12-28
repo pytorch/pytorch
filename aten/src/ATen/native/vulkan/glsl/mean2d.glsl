6c7a11a6defdf6b81deeff714c45658a659cd01f
ssjia <ssjia@fb.com>
2020-12-28 17:01:15 -0500

    Update on "[vulkan] Efficient gemm implementation"
    [ghstack-poisoned]

--- aten/src/ATen/native/vulkan/glsl/mean2d.glsl
+++ aten/src/ATen/native/vulkan/glsl/mean2d.glsl
@@ -13,25 +13,42 @@ layout(set = 0, binding = 2)          uniform PRECISION restrict           Block
   ivec2 isize;
 } uBlock;
 
-layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
+shared vec4 sh_mem[64];
 
-// This implementation is suboptimal and should be revisted.
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
+  const ivec3 tid = ivec3(gl_LocalInvocationID);
+  const ivec3 group_size = ivec3(gl_WorkGroupSize);
 
-  if (all(lessThan(pos, uBlock.size.xyz))) {
+  if (pos.z < uBlock.size.z) {
     vec4 sum = vec4(0);
 
-    for (int y = 0; y < uBlock.isize.y; ++y) {
-      for (int x = 0; x < uBlock.isize.x; ++x) {
+    for (int y = tid.y; y < uBlock.isize.y; y+=group_size.y) {
+      for (int x = tid.x; x < uBlock.isize.x; x+=group_size.x) {
         sum += texelFetch(uInput, ivec3(x, y, pos.z), 0);
       }
     }
 
-    imageStore(
-        uOutput,
-        pos,
-        vec4(sum / uBlock.size.w));
+    sh_mem[tid.z * group_size.y * group_size.x + tid.y * group_size.x + tid.x] = sum;
   }
+  memoryBarrierShared();
+  barrier();
+
+  if (tid.y > 0 || tid.x > 0 || pos.z >= uBlock.size.z) {
+    return;
+  }
+
+  vec4 total = vec4(0);
+  for (int y = 0; y < group_size.y; ++y) {
+    for (int x = 0; x < group_size.x; ++x) {
+      total += sh_mem[tid.z * group_size.y * group_size.x + y * group_size.x + x];
+    }
+  }
+
+  imageStore(
+      uOutput,
+      pos,
+      total / uBlock.size.w);
 }
