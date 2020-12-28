f9647eb3e6191f5ae15951a8f7700cf8611dbe30
Yuxin Wu <yuxinwu@fb.com>
2020-12-28 11:05:21 -0800

    [pytorch] make is_tracing scriptable (#49853)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49853
    
    fix https://github.com/pytorch/pytorch/issues/47379
    
    Test Plan: buck test mode/dev-nosan //caffe2/test:jit -- 'test_script_is_tracing'
    
    Differential Revision: D25704315
    
    fbshipit-source-id: 4c53b11368685270dc2e268aa1cf4ff96e712d02

--- torch/jit/_trace.py
+++ torch/jit/_trace.py
@@ -20,7 +20,7 @@ from typing import Any, Dict, List, Optional, Set
 
 from torch.jit._state import _python_cu, _enabled
 from torch.jit._script import ScriptModule, _CachedForward, script
-from torch._jit_internal import _qualified_name
+from torch._jit_internal import _qualified_name, is_scripting
 from torch.autograd import function
 from torch.nn import Module
 
@@ -976,6 +976,8 @@ def is_tracing():
     Returns ``True`` in tracing (if a function is called during the tracing of
     code with ``torch.jit.trace``) and ``False`` otherwise.
     """
+    if is_scripting():
+        return False
     return torch._C._is_tracing()
 
 
