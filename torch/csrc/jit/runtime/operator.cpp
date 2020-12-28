938556acb25ed8bbed797d30ef2a6aeb43d73220
Nikolay Korovaiko <korovaikon@gmail.com>
2020-12-28 11:54:33 -0800

    add RequiresGradCheck

--- torch/csrc/jit/runtime/operator.cpp
+++ torch/csrc/jit/runtime/operator.cpp
@@ -7,6 +7,7 @@
 #include <queue>
 #include <utility>
 #include <vector>
+#include "ATen/core/interned_strings.h"
 
 namespace torch {
 namespace jit {
@@ -249,6 +250,7 @@ bool printerHasSpecialCaseFor(Symbol sym) {
       prim::profile_optional, // used in interpreter only
       prim::profile_ivalue, // used in interpreter only
       prim::TypeCheck, // used in interpreter only
+      prim::RequiresGradCheck, // used in interpreter only
       prim::FallbackGraph, // converted into prim::CallFunction
 
   };
@@ -309,6 +311,7 @@ bool aliasAnalysisHasSpecialCaseFor(Symbol symbol) {
       prim::profile_optional,
       prim::profile_ivalue,
       prim::TypeCheck,
+      prim::RequiresGradCheck,
       prim::Print,
       prim::CallFunction,
       prim::CallMethod,
