9f135235c56e6459f49226ab7f4a0f144f6d1386
jonykarki <dev.jonykarki@gmail.com>
2020-12-30 12:30:10 -0600

    ModuleAttributeError changed to AttributeError

--- test/test_jit_py3.py
+++ test/test_jit_py3.py
@@ -675,7 +675,9 @@ class TestScriptPy3(JitTestCase):
         mod = ModuleWithProperties(3)
         scripted_mod = torch.jit.script(mod)
 
-        with self.assertRaisesRegex(torch.nn.modules.module.ModuleAttributeError, "has no attribute"):
+        with self.assertRaisesRegex(
+                AttributeError, 
+                r"'RecursiveScriptModule' object has no attribute 'ignored_attr'"):
             scripted_mod.ignored_attr
 
     def test_ignoring_module_attributes(self):
