557ef176e99c8f2ab6725f98f708d90d1f9126f6
Michael Suo <suo@fb.com>
2021-01-04 09:44:53 -0800

    Update on "[package] mangle imported module names"
    Rationale and implementation immortalized in a big comment in
    _mangling.py
    
    [ghstack-poisoned]

--- torch/package/importer.py
+++ torch/package/importer.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Dict, Optional, Any, Union, ClassVar
+from typing import List, Callable, Dict, Optional, Any, Union
 import builtins
 import importlib
 from torch.serialization import _load
