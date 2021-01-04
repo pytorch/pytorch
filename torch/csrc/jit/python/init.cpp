fb4854af4b5afdf551ba445b68a690942d0b4cfb
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2021-01-04 12:35:50 -0800

    fix clang-tidy

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -948,7 +948,7 @@ void initJITBindings(PyObject* module) {
           "get_record",
           [](PyTorchStreamReader& self, const std::string& key) {
             at::DataPtr data;
-            size_t size;
+            size_t size = 0;
             std::tie(data, size) = self.getRecord(key);
             return py::bytes(reinterpret_cast<const char*>(data.get()), size);
           })
@@ -1203,7 +1203,7 @@ void initJITBindings(PyObject* module) {
       auto fork_node = graph->insertNode(graph->create(prim::TracedFork, 1));
       auto body_block = fork_node->addBlock();
 
-      Value* node_output;
+      Value* node_output = nullptr;
       py::object py_func_output;
       // Insert new trace ops into the fork op's sub-block
       WithInsertPoint guard(body_block);
