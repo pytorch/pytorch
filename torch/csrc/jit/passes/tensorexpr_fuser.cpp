8263eed5558003cd11939888ee0987576ff0d103
Heitor Schueroff <heitorschueroff@fb.com>
2021-01-05 08:44:12 -0800

    Revert D25763758: [pytorch][PR] introduce a flag to disable aten::cat in TE
    Test Plan: revert-hammer
    
    Differential Revision:
    D25763758 (https://github.com/pytorch/pytorch/commit/9e0b4a96e48132190220820684033a77a92e8a33)
    
    Original commit changeset: c4f4a8220964
    
    fbshipit-source-id: 98775ad9058b81541a010e646b0cf4864854be3e

--- torch/csrc/jit/passes/tensorexpr_fuser.cpp
+++ torch/csrc/jit/passes/tensorexpr_fuser.cpp
@@ -15,10 +15,6 @@
 #include <torch/csrc/jit/tensorexpr/kernel.h>
 #include <torch/csrc/utils/memory.h>
 
-C10_DEFINE_bool(
-    torch_jit_disable_cat,
-    false,
-    "disable aten::cat in TE fusion groups");
 namespace torch {
 namespace jit {
 
@@ -204,10 +200,6 @@ bool isSupported(Node* node) {
           return false;
         }
       }
-    }
-
-    if (FLAGS_torch_jit_disable_cat && node->kind() == aten::cat) {
-      return false;
     }
 
     return true;
