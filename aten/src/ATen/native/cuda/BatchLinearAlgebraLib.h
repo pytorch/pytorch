72edde61d803fd917343c676b013805da2ab2b43
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2021-01-05 01:03:15 -0800

    add at::parallel_for for parallel stream launch

--- aten/src/ATen/native/cuda/BatchLinearAlgebraLib.h
+++ aten/src/ATen/native/cuda/BatchLinearAlgebraLib.h
@@ -20,24 +20,28 @@
 
 #ifdef _USE_PARALLEL_LAUNCH
 
-#define CUDA_PARALLEL_STREAM_LAUNCH(_i, _batch_size, _stmt) \
-do {                                                        \
-  auto _main_stream = at::cuda::getCurrentCUDAStream();     \
-  at::cuda::CUDAEvent _main_event;                          \
-  _main_event.record(_main_stream);                         \
-  for (int _i = 0; _i < _batch_size; _i++) {                \
-    auto _stream = at::cuda::getStreamFromPool();           \
-    at::cuda::CUDAStreamGuard _guard(_stream);              \
-    _main_event.block(_stream);                             \
-    _stmt();                                                \
-    at::cuda::CUDAEvent _finished;                          \
-    _finished.record(_stream);                              \
-    _finished.block(_main_stream);                          \
-  }                                                         \
+// Launch multiple operations in different CUDA stream in parallel, using at::parallel_for to speed up.
+#define CUDA_PARALLEL_STREAM_LAUNCH(_i, _batch_size, _stmt)                         \
+do {                                                                                \
+  auto _main_stream = at::cuda::getCurrentCUDAStream();                             \
+  at::cuda::CUDAEvent _main_event;                                                  \
+  _main_event.record(_main_stream);                                                 \
+  at::parallel_for(0, _batch_size, _batch_size / 4 + 1, [&](int _start, int _end){  \
+    auto _stream = at::cuda::getStreamFromPool();                                   \
+    at::cuda::CUDAStreamGuard _guard(_stream);                                      \
+    _main_event.block(_stream);                                                     \
+    for (int _i = _start; _i < _end; _i++) {                                        \
+      _stmt();                                                                      \
+    }                                                                               \
+    at::cuda::CUDAEvent _finished;                                                  \
+    _finished.record(_stream);                                                      \
+    _finished.block(_main_stream);                                                  \
+  });                                                                               \
 } while(0)
 
 #else // _USE_PARALLEL_LAUNCH is not defined
 
+// Plain for-loop.
 #define CUDA_PARALLEL_STREAM_LAUNCH(_i, _batch_size, _stmt) \
 do {                                                        \
   for (int _i = 0; _i < _batch_size; _i++) {                \
