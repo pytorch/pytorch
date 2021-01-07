306339522a5489cbc1f518192481d7e152a08ccb
Shen Li <cs.shenli@gmail.com>
2021-01-07 14:51:50 -0800

    Apply clang-format to rpc cpp files
    [ghstack-poisoned]

--- torch/csrc/distributed/rpc/py_rref.cpp
+++ torch/csrc/distributed/rpc/py_rref.cpp
@@ -137,8 +137,7 @@ c10::intrusive_ptr<JitFuture> PyRRef::getFuture() const {
   // Marking hasValue to false, as this Future is only used for signaling
   // profiler to update profiling result and the profiler does not retrieve
   // any value from it.
-  return toPyJitFuture(
-      rref_->getOwnerCreationFuture(), false /* hasValue */);
+  return toPyJitFuture(rref_->getOwnerCreationFuture(), false /* hasValue */);
 }
 
 c10::intrusive_ptr<JitFuture> PyRRef::getProfilingFuture() const {
