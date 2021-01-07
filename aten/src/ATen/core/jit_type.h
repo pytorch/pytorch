3823d97d5a60b817651c1db403af89e5ccd9df4d
Meghan Lele <meghanl@fb.com>
2021-01-07 00:19:14 -0800

    [JIT] Print out CU address in `ClassType::repr_str()`
    **Summary**
    `ClassType::repr_str()` prints out only the name of a `ClassType`, which
    is not always enough to disambiguate it. In some situations, two
    `ClassTypes` are compared and do not match despite having identical
    names because they are in separate compilation units. In such cases, the
    error message can seem nonsensical (e.g. `expected type T but found type
    T`). This commit modifies `ClassType::repr_str()` so that it prints out
    the address of the type's compilation unit to make these messages less
    puzzling.
    
    **Test Plan**
    This commit adds a unit test, `ClassTypeTest.IdenticalTypesDifferentCus`
    that reproduces this situation.
    
    **Fixes**
    This commit fixes #46212.
    
    [ghstack-poisoned]

--- aten/src/ATen/core/jit_type.h
+++ aten/src/ATen/core/jit_type.h
@@ -1869,7 +1869,13 @@ struct TORCH_API ClassType : public NamedType {
 
   std::string str() const override {
      return annotation_str();
-   }
+  }
+
+  std::string repr_str() const override {
+    std::stringstream ss;
+    ss << str() << "(" << compilation_unit().get() << ")";
+    return ss.str();
+  }
 
   const std::vector<torch::jit::Function*>& methods() const;
 
