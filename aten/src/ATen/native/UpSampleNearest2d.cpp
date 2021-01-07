8fdcd7e58b6a1d5783ee7812b5809c6bad0840ea
Jeffrey Wan <jw3468@fb.com>
2021-01-06 23:11:28 -0500

    Make upsample_nearest2d use structured kernels

--- aten/src/ATen/native/UpSampleNearest2d.cpp
+++ aten/src/ATen/native/UpSampleNearest2d.cpp
@@ -3,43 +3,78 @@
 #include <ATen/native/UpSample.h>
 
 namespace at {
-namespace native {
-namespace {
+namespace meta {
 
-static void upsample_nearest2d_out_cpu_template(
-    Tensor& output,
-    const Tensor& input,
-    IntArrayRef output_size,
-    c10::optional<double> scales_h,
-    c10::optional<double> scales_w) {
+static std::array<int64_t, 4> upsample_nearest2d_common_check(IntArrayRef input_size, IntArrayRef output_size) {
   TORCH_CHECK(
       output_size.size() == 2,
-      "It is expected output_size equals to 2, but got size ",
+      "It is expected output_size equals to 1, but got size ",
       output_size.size());
 
-  int64_t output_height = output_size[0];
-  int64_t output_width = output_size[1];
+  TORCH_CHECK(
+      input_size.size() == 4,
+      "It is expected input_size equals to 3, but got size ",
+      input_size.size());
 
-  int64_t nbatch = input.size(0);
-  int64_t channels = input.size(1);
-  int64_t input_height = input.size(2);
-  int64_t input_width = input.size(3);
+  int64_t output_width = output_size[0];
+  int64_t output_height = output_size[1];
 
-  upsample_2d_shape_check(
-      input,
-      Tensor(),
-      nbatch,
-      channels,
+  int64_t nbatch = input_size[0];
+  int64_t channels = input_size[1];
+  int64_t input_width = input_size[2];
+  int64_t input_height = input_size[3];
+
+  TORCH_CHECK(
+      input_height > 0 && input_width > 0 && output_height > 0 &&
+          output_width > 0,
+      "Input and output sizes should be greater than 0,"
+      " but got input (H: ",
       input_height,
+      ", W: ",
       input_width,
+      ") output (H: ",
       output_height,
-      output_width);
+      ", W: ",
+      output_width,
+      ")");
 
-  output.resize_({nbatch, channels, output_height, output_width}, input.suggest_memory_format());
-
-  AT_ASSERT(input_width > 0 && output_width > 0);
-  upsample_nearest2d_kernel(kCPU, output, input, scales_h, scales_w);
+  return {nbatch, channels, output_width, output_height};
 }
+
+TORCH_META_FUNC(upsample_nearest2d) (
+  const Tensor& input, IntArrayRef output_size, c10::optional<double> scales_h, c10::optional<double> scales_w
+) {
+  auto full_output_size = upsample_nearest2d_common_check(input.sizes(), output_size);
+
+  // Allow for empty batch size but not other dimensions
+  TORCH_CHECK(
+      (input.numel() != 0 ||
+        (input.size(1) != 0 && input.size(2) != 0 && input.size(3) != 0)
+        ) &&
+      input.dim() == 4,
+      "Non-empty 4D data tensor expected but got a tensor with sizes ",
+      input.sizes());
+
+  set_output(full_output_size, input.options());
+}
+
+TORCH_META_FUNC(upsample_nearest2d_backward) (
+  const Tensor& grad_output, IntArrayRef input_size, IntArrayRef output_size, c10::optional<double> scales_h, c10::optional<double> scales_w
+) {
+  auto full_output_size = upsample_nearest2d_common_check(input_size, output_size);
+
+  check_dim_size(grad_output, 4, 0, full_output_size[0]);
+  check_dim_size(grad_output, 4, 1, full_output_size[1]);
+  check_dim_size(grad_output, 4, 2, full_output_size[2]);
+  check_dim_size(grad_output, 4, 3, full_output_size[3]);
+
+  set_output(input_size, grad_output.options());
+}
+
+} // namespace meta
+
+namespace native {
+namespace {
 
 static void upsample_nearest2d_backward_out_cpu_template(
     Tensor& grad_input,
@@ -83,48 +118,24 @@ static void upsample_nearest2d_backward_out_cpu_template(
 }
 } // namespace
 
-Tensor& upsample_nearest2d_out_cpu(
-    Tensor& output,
+TORCH_IMPL_FUNC(upsample_nearest2d_out_cpu) (
     const Tensor& input,
     IntArrayRef output_size,
     c10::optional<double> scales_h,
-    c10::optional<double> scales_w) {
-  upsample_nearest2d_out_cpu_template(output, input, output_size, scales_h, scales_w);
-  return output;
+    c10::optional<double> scales_w,
+    Tensor& output
+) {
+  upsample_nearest2d_kernel(kCPU, output, input, scales_h, scales_w);
 }
 
-Tensor upsample_nearest2d_cpu(
-    const Tensor& input,
-    IntArrayRef output_size,
-    c10::optional<double> scales_h,
-    c10::optional<double> scales_w) {
-  auto output = at::empty({0}, input.options());
-  upsample_nearest2d_out_cpu_template(output, input, output_size, scales_h, scales_w);
-  return output;
-}
-
-Tensor& upsample_nearest2d_backward_out_cpu(
-    Tensor& grad_input,
+TORCH_IMPL_FUNC(upsample_nearest2d_backward_out_cpu) (
     const Tensor& grad_output,
     IntArrayRef output_size,
     IntArrayRef input_size,
     c10::optional<double> scales_h,
-    c10::optional<double> scales_w) {
-  upsample_nearest2d_backward_out_cpu_template(
-      grad_input, grad_output, output_size, input_size, scales_h, scales_w);
-  return grad_input;
-}
-
-Tensor upsample_nearest2d_backward_cpu(
-    const Tensor& grad_output,
-    IntArrayRef output_size,
-    IntArrayRef input_size,
-    c10::optional<double> scales_h,
-    c10::optional<double> scales_w) {
-  auto grad_input = at::zeros(input_size, grad_output.options());
-  upsample_nearest2d_backward_out_cpu_template(
-      grad_input, grad_output, output_size, input_size, scales_h, scales_w);
-  return grad_input;
+    c10::optional<double> scales_w,
+    Tensor& grad_input) {
+  upsample_nearest2d_backward_kernel(kCPU, grad_input, grad_output, scales_h, scales_w);
 }
 
 using at::native::upsample::compute_output_size;
@@ -134,12 +145,10 @@ Tensor upsample_nearest2d_cpu(
     const Tensor& input,
     c10::optional<IntArrayRef> output_size,
     c10::optional<ArrayRef<double>> scale_factors) {
-  auto output = at::empty({0}, input.options());
   auto osize = compute_output_size(input.sizes(), output_size, scale_factors);
   auto scale_h = get_scale_value(scale_factors, 0);
   auto scale_w = get_scale_value(scale_factors, 1);
-  upsample_nearest2d_out_cpu_template(output, input, osize, scale_h, scale_w);
-  return output;
+  return at::upsample_nearest2d(input, osize, scale_h, scale_w);
 }
 
 Tensor upsample_nearest2d_backward_cpu(
