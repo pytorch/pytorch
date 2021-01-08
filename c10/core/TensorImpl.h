d32d8157a7563da9c719f5ed007d1a9abadce1d9
Scott Wolchok <swolchok@fb.com>
2021-01-07 18:58:29 -0800

    [PyTorch] Devirtualize TensorImpl::dim() with macro (#49770)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49770
    
    Seems like the performance cost of making this commonly-called method virtual isn't worth having use of undefined tensors crash a bit earlier (they'll still fail to dispatch).
    ghstack-source-id: 119528065
    
    Test Plan: framework overhead benchmarks
    
    Reviewed By: ezyang
    
    Differential Revision: D25687465
    
    fbshipit-source-id: 89aabce165a594be401979c04236114a6f527b59

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -402,7 +402,14 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
    * Return the number of dimensions of this tensor.  Note that 0-dimension
    * represents a Tensor that is a Scalar, e.g., one that has a single element.
    */
-  virtual int64_t dim() const;
+  TENSORIMPL_MAYBE_VIRTUAL int64_t dim() const
+#ifdef C10_DISABLE_TENSORIMPL_EXTENSIBILITY
+  {
+    return sizes_.size();
+  }
+#else
+  ;
+#endif
 
   /**
    * True if this tensor has storage. See storage() for details.
