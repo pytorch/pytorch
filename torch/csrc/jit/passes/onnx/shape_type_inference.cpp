807beecd112ba2f602911e03a9a90b36a187cfd3
BowenBao <bowbao@microsoft.com>
2021-01-08 10:04:30 -0800

    add helper function

--- torch/csrc/jit/passes/onnx/shape_type_inference.cpp
+++ torch/csrc/jit/passes/onnx/shape_type_inference.cpp
@@ -223,20 +223,10 @@ Value* CloneValueFromListConstruct(
     // order to be consumed as inputs
     std::vector<Value*> unsqueezed;
     for (auto* input : lc_node->inputs()) {
-      Node* unsqueezed_node =
-          n_graph->insertNode(n_graph->create(::c10::onnx::Unsqueeze, 1));
       auto new_input = n_graph->addInput();
       new_input->copyMetadata(input);
-      unsqueezed_node->addInput(new_input);
-      if (opset_version >= 13) {
-        Node* unsqueeze_axes = n_graph->create(::c10::onnx::Constant, 1);
-        unsqueeze_axes->insertBefore(unsqueezed_node);
-        unsqueeze_axes->t_(
-            attr::value, at::unsqueeze(at::scalar_to_tensor(at::Scalar(0)), 0));
-        unsqueezed_node->addInput(unsqueeze_axes->output());
-      } else {
-        unsqueezed_node->is_(attr::axes, {0});
-      }
+      Node* unsqueezed_node = createONNXUnsqueeze(
+          n_graph.get(), n_graph->return_node(), new_input, 0, opset_version);
       unsqueezed.emplace_back(unsqueezed_node->output());
     }
     Node* concat_node =
