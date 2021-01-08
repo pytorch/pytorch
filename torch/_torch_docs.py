20e74a6b1755ee274e5ff8fcd60a44804da8c2d1
Nikita Vedeneev <nik@quansight.com>
2021-01-08 11:38:39 +0000

    update the doc

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -7640,7 +7640,7 @@ A namedtuple of (values, indices) is returned, where the `values` are the
 sorted values and `indices` are the indices of the elements in the original
 `input` tensor.
 
-.. warning:: `stable=True` only works on CPU for now, it is ignored for CUDA.
+.. warning:: `stable=True` only works on the CPU for now.
 
 Args:
     {input}
