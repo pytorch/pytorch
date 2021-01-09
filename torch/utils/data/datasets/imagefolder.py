b8025f209cb2ff5627f7ee1b41fb6044817fcaae
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2021-01-08 16:12:45 -0800

    Update on "[WIP] webdataset prototype - class to preprocess image folder"
    This is for now a super simple class to preprocess image folder.
    The only functionality it currently support is to pair each image file with a label file (with category_id set to the name of the folder) and convert everything into one tar file.
    
    [ghstack-poisoned]

--- torch/utils/data/datasets/imagefolder.py
+++ torch/utils/data/datasets/imagefolder.py
@@ -2,6 +2,7 @@ import io
 import os
 import time
 import tarfile
+from typing import Union, List
 from pathlib import Path
 
 from torch.utils.data.datasets.common import get_file_pathnames_from_root
@@ -19,8 +20,8 @@ class ImageFolder:
 
     """
 
-    def __init__(self, root: str = '.'):
-        self.root : str = root
+    def __init__(self, root: Union[str, List[str]] = '.'):
+        self.root_list : List[str] = [root] if isinstance(root, str) else root
 
 
     def to_tar(self, tar_pathname: str, create_label: bool = True):
@@ -28,37 +29,38 @@ class ImageFolder:
         # always compress for now
         tarstream = tarfile.open(tar_pathname, mode="w:gz")
 
-        for pathname in get_file_pathnames_from_root(self.root, ''):
-            filename = os.path.basename(pathname)
-            splits = os.path.splitext(filename)
-            basename = splits[0]
-            ext = splits[1]
+        for root_path in self.root_list:
+            for pathname in get_file_pathnames_from_root(root_path, ''):
+                filename = os.path.basename(pathname)
+                splits = os.path.splitext(filename)
+                basename = splits[0]
+                ext = splits[1]
 
-            # do not allow any non image file exist in the folder
-            if not is_img_ext(ext):
-                try:
-                    tarstream.close()
-                    os.remove(tar_pathname)
-                except Exception as e:
-                    pass
-                raise TypeError("Image folder {} should only contain image file, but got non-image file {}".format(
-                    self.root, pathname))
+                # do not allow any non image file exist in the folder
+                if not is_img_ext(ext):
+                    try:
+                        tarstream.close()
+                        os.remove(tar_pathname)
+                    except Exception as e:
+                        pass
+                    raise TypeError("Image folder {} should only contain image file, but got non-image file {}".format(
+                        root_path, pathname))
 
-            # no encoding at the moment, store the raw image binary into tar file
-            tarstream.add(pathname, arcname=filename)
+                # no encoding at the moment, store the raw image binary into tar file
+                tarstream.add(pathname, arcname=filename)
 
-            if create_label:
-                category_id = os.path.basename(os.path.normpath(self.root))
-                bio = io.BytesIO()
-                bio.write(str.encode('{{"category_id": "{}"}}'.format(category_id)))
-                path_info = Path(pathname)
-                tinfo = tarfile.TarInfo(basename + ".json")
-                tinfo.size = bio.tell()
-                bio.seek(0)
-                tinfo.mtime = int(time.time())
-                tinfo.uname = path_info.owner()
-                tinfo.gname = path_info.group()
-                tinfo.mode = path_info.stat().st_mode & 0o0777
-                tarstream.addfile(tinfo, bio)
+                if create_label:
+                    category_id = os.path.basename(os.path.normpath(root_path))
+                    bio = io.BytesIO()
+                    bio.write(str.encode('{{"category_id": "{}"}}'.format(category_id)))
+                    path_info = Path(pathname)
+                    tinfo = tarfile.TarInfo(basename + ".json")
+                    tinfo.size = bio.tell()
+                    bio.seek(0)
+                    tinfo.mtime = int(time.time())
+                    tinfo.uname = path_info.owner()
+                    tinfo.gname = path_info.group()
+                    tinfo.mode = path_info.stat().st_mode & 0o0777
+                    tarstream.addfile(tinfo, bio)
 
         tarstream.close()
