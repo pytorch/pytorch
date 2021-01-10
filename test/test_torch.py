1eb2e7c5e69fa9e3cd7a9ba03a53779c156b6acf
peter <peterghost86@gmail.com>
2021-01-10 13:24:16 +0800

    Use assertIn

--- test/test_torch.py
+++ test/test_torch.py
@@ -512,7 +512,7 @@ class AbstractTestCases:
             e = cm.exception
             self.assertNotEqual(e.returncode, 0)
             self.assertNotEqual(e.output, None)
-            self.assertTrue(b'Unhandled exception caught in c10/util/AbortHandler.h' in e.output)
+            self.assertIn(b'Unhandled exception caught in c10/util/AbortHandler.h', e.output)
 
         def _spawn_method(self, method, arg):
             try:
