73c203e0f2dbd46cd722fc8d488d8c2b19588a34
M.L. Croci <mlc70@eng.cam.ac.uk>
2021-01-13 10:41:37 +0000

    Update class init to have kwargs

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -357,7 +357,7 @@ class GaussianNLLLoss(_Loss):
     full: bool
     eps: float
 
-    def __init__(self, full: bool = False, eps: float = 1e-6, reduction: str = 'mean') -> None:
+    def __init__(self, *, full: bool = False, eps: float = 1e-6, reduction: str = 'mean') -> None:
         super(GaussianNLLLoss, self).__init__(None, None, reduction)
         self.full = full
         self.eps = eps
