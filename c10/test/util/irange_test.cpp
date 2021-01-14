e7aa64da3b62baf5dfb9d8042baf844b4e665cef
Richard Barnes <rbarnes@fb.com>
2021-01-13 19:48:03 -0800

    Move irange to c10 (#46414)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46414
    
    For loops are often written with mismatched data types which causes silent type and sign coercion in the absence of integer conversion warnings. Getting around this in templated code requires convoluted patterns such as
    ```
    for(auto i=decltype(var){0};i<var;i++)
    ```
    with this diff we can instead write
    ```
    for(const auto i = c10::irange(var))
    ```
    Note that this loop is type-safe and const-safe.
    
    The function introduced here (`c10::irange`) allows for type-safety and const-ness within for loops, which prevents the accidental truncation or modification of integers and other types, improving code safety.
    
    Test Plan:
    ```
    buck test //caffe2/c10:c10_test_0
    ```
    
    Differential Revision: D24334732
    
    fbshipit-source-id: 595d03de995288da83d0e4325e269511a7f122f7

---
+++ c10/test/util/irange_test.cpp
@@ -0,0 +1,58 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <c10/util/irange.h>
+
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+TEST(irange_test, range_test) {
+    std::vector<int> test_vec;
+    for(const auto i : c10::irange(4, 11)){
+        test_vec.push_back(i);
+    }
+    const std::vector<int> correct = {{4,5,6,7,8,9,10}};
+    ASSERT_EQ(test_vec, correct);
+}
+
+TEST(irange_test, end_test) {
+    std::vector<int> test_vec;
+    for(const auto i : c10::irange(5)){
+        test_vec.push_back(i);
+    }
+    const std::vector<int> correct = {{0, 1, 2, 3, 4}};
+    ASSERT_EQ(test_vec, correct);
+}
+
+TEST(irange_test, neg_range_test) {
+    std::vector<int> test_vec;
+    for(const auto i : c10::irange(-2, 3)){
+        test_vec.push_back(i);
+    }
+    const std::vector<int> correct = {{-2,-1,0,1,2}};
+    ASSERT_EQ(test_vec, correct);
+}
+
+TEST(irange, empty_reverse_range_two_inputs){
+    std::vector<int> test_vec;
+    for(const auto i : c10::irange(3, -3)){
+        test_vec.push_back(i);
+        if(i>20){ //Cap the number of elements we add if something goes wrong
+            break;
+        }
+    }
+    const std::vector<int> correct = {};
+    ASSERT_EQ(test_vec, correct);
+}
+
+TEST(irange, empty_reverse_range_one_input){
+    std::vector<int> test_vec;
+    for(const auto i : c10::irange(-3)){
+        test_vec.push_back(i);
+        if(i>20){ //Cap the number of elements we add if something goes wrong
+            break;
+        }
+    }
+    const std::vector<int> correct = {};
+    ASSERT_EQ(test_vec, correct);
+}
