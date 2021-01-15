87d5823e25ee93bdef99612b24ed16c6e9149914
Kimish Patel <kimishpatel@fb.com>
2021-01-15 08:53:35 -0800

    [QNNPACK, Sparsity] Sparse kernel with 4x8 blocking
    Summary:
    Larger blocking across M dim such as 8 in previous PR is likely
    introducing wasted compute on the shapes being benchmarked.
    Here we introduced 4x8 blocking of mrxnr. This helps 1) in packing
    smaller data for small values of M and 2) for compute kernel it writes
    same number of bytes but more contiguously. It is not certain but it
    likely helps.
    
    Test Plan:
    q8gemm-sparse-test
    fully-connected-sparse-test
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cpu/qnnpack/test/gemm-block-sparse-microkernel-tester.h
+++ aten/src/ATen/native/quantized/cpu/qnnpack/test/gemm-block-sparse-microkernel-tester.h
@@ -356,7 +356,7 @@ class GemmBlockSparseMicrokernelTester {
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
       std::generate(a.begin(), a.end(), std::ref(u8rng));
       std::fill(bias.begin(), bias.end(), 0);
-      std::fill(c.begin(), c.end(), 0.0f);
+      std::fill(c.begin(), c.end(), 2.0f);
       size_t num_zero_points_padded = n() + 8;
       std::vector<uint8_t> kernel_zero_points
         (num_zero_points_padded, bZeroPoint());
