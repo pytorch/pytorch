bbedae96b80efb8890e4c0205729f25fe4860dd6
Mikhail Zolotukhin <mvz@fb.com>
2021-01-14 21:00:53 -0800

    [TensorExpr] Add python bindings. (#49698)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49698
    
    Reincarnation of #47620 by jamesr66a.
    
    It's just an initial bunch of things that we're exposing to python, more
    is expected to come in future. Some things can probably be done better,
    but I'm putting this out anyway, since some other people were interested
    in using and/or developing this.
    
    Differential Revision: D25668694
    
    Test Plan: Imported from OSS
    
    Reviewed By: bertmaher
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: fb0fd1b31e851ef9ab724686b9ac2d172fa4905a

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -86,6 +86,7 @@
 #include <torch/csrc/jit/serialization/import.h>
 #include <torch/csrc/jit/tensorexpr/execution_counter.h>
 #include <torch/csrc/jit/tensorexpr/kernel.h>
+#include <torch/csrc/jit/tensorexpr/tensorexpr_init.h>
 
 #include <c10/macros/Export.h>
 #include <caffe2/serialize/inline_container.h>
@@ -94,6 +95,7 @@
 
 #include <pybind11/functional.h>
 #include <pybind11/iostream.h>
+#include <pybind11/operators.h>
 
 #include <memory>
 #include <sstream>
@@ -1300,6 +1302,7 @@ void initJITBindings(PyObject* module) {
   initJitScriptBindings(module);
   initJitBackendBindings(module);
   initStaticRuntimeBindings(module);
+  initTensorExprBindings(module);
 
   setPrintHandler([](const std::string& str) {
     py::gil_scoped_acquire acquire;
