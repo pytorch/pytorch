0c3eb18973a9a8f8340f547c42aaf2bc2331e747
neginraoof <neginmr@utexas.edu>
2021-01-15 11:39:55 -0800

    mypy

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -466,9 +466,9 @@ def _model_to_graph(model, args, verbose=False,
     # erased by some optimizations, so we need to set it explicitly again.
     if torch_out is not None:
         if not (isinstance(torch_out, list) or isinstance(torch_out, tuple)):
-            output_wrapped = [torch_out]
+            output_wrapped = [torch_out]  # type: ignore
         else:
-            output_wrapped = torch_out
+            output_wrapped = torch_out  # type: ignore
 
         output_tensors, out_desc = torch._C._jit_flatten(tuple(output_wrapped))
         torch._C._jit_pass_onnx_assign_output_shape(graph, output_tensors, out_desc, _onnx_shape_inference)
