0864f5e15fe32b4075324359d5f9d0f3c1597286
Richard Zou <zou3519@gmail.com>
2021-01-15 11:14:05 -0800

    Update on "Add instructional error message for cudnn RNN double backward workaround"
    Mitigates https://github.com/pytorch/pytorch/issues/5261.
    
    It's not possible for us to support cudnn RNN double backwards due to
    limitations in the cudnn API. This PR makes it so that we raise an error
    message if users try to get the double backward on a cudnn RNN; in the
    error message we suggest using the non-cudnn RNN.
    
    Test Plan:
    - added some tests to check the error message
    
    Differential Revision: [D20143544](https://our.internmc.facebook.com/intern/diff/D20143544)
    
    [ghstack-poisoned]

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -33,7 +33,7 @@ using at::Scalar;
 using at::IntArrayRef;
 using at::TensorList;
 
-extern const char* kCudnnDoubleBackwardMsg = "Double backwards is not supported for CuDNN RNNs due to limitations in the CuDNN API. To run double backwards, please disable the CuDNN backend temporarily while running the forward pass of your RNN. For example: \nwith torch.backends.cudnn.flags(enabled=False):\n    output = model(inputs)";
+const char* kCudnnDoubleBackwardMsg = "Double backwards is not supported for CuDNN RNNs due to limitations in the CuDNN API. To run double backwards, please disable the CuDNN backend temporarily while running the forward pass of your RNN. For example: \nwith torch.backends.cudnn.flags(enabled=False):\n    output = model(inputs)";
 
 
 bool isDefined(const c10::optional<Tensor>& t) {
