bd2d24bf293efd9b39b7fe2d97a8369a66089c96
Fabian <reach.fmk@gmail.com>
2021-01-16 11:49:51 +0100

    Add fix proposed in issue 37738
    Add fix proposed in issue 37738

--- torch/utils/tensorboard/writer.py
+++ torch/utils/tensorboard/writer.py
@@ -264,7 +264,7 @@ class SummaryWriter(object):
         return self.log_dir
 
     def add_hparams(
-        self, hparam_dict, metric_dict, hparam_domain_discrete=None, run_name=None
+        self, hparam_dict, metric_dict, hparam_domain_discrete=None, run_name=None, global_step=None
     ):
         """Add a set of hyperparameters to be compared in TensorBoard.
 
@@ -282,6 +282,7 @@ class SummaryWriter(object):
               contains names of the hyperparameters and all discrete values they can hold
             run_name (str): Name of the run, to be included as part of the logdir.
               If unspecified, will use current timestamp.
+            global_step (int): Current time step
 
         Examples::
 
@@ -310,7 +311,7 @@ class SummaryWriter(object):
             w_hp.file_writer.add_summary(ssi)
             w_hp.file_writer.add_summary(sei)
             for k, v in metric_dict.items():
-                w_hp.add_scalar(k, v)
+                w_hp.add_scalar(k, v, global_step)
 
     def add_scalar(self, tag, scalar_value, global_step=None, walltime=None):
         """Add scalar data to summary.
