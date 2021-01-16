e57f31b98ced6ef85b0d99ccf65e4b075c947d0f
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2021-01-16 01:59:31 +0000

    update test/distributed/test_c10d.py
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- test/distributed/test_c10d.py
+++ test/distributed/test_c10d.py
@@ -34,7 +34,6 @@ from torch.testing._internal.common_distributed import (
     skip_if_lt_x_gpu,
     get_timeout,
     skip_if_rocm,
-    skip_if_rocm_single_process,
     simple_sparse_reduce_tests,
     skip_if_win32,
     create_device,
