52326412b6fecedf55b44921d490d62cd4a2d89f
peter <peterghost86@gmail.com>
2021-01-17 13:47:16 +0800

    Target comment

--- c10/util/AbortHandler.h
+++ c10/util/AbortHandler.h
@@ -4,16 +4,60 @@
 #include <exception>
 
 namespace c10 {
+class AbortHandlerHelper {
+public:
+  static AbortHandlerHelper& getInstance() {
+#ifdef _WIN32
+    thread_local
+#endif // _WIN32
+    static AbortHandlerHelper instance;
+    return instance;
+  }
+
+  void set(terminate_handler handler) {
+    if (!inited) {
+      prev = set_terminate(handler);
+      curr = get_terminate();
+      inited = true;
+    }
+  }
+
+  terminate_handler getPrev() {
+    return prev;
+  }
+
+private:
+  terminate_handler prev = nullptr;
+  terminate_handler curr = nullptr;
+  bool inited = false;
+  AbortHandlerHelper() = default;
+  ~AbortHandlerHelper() {
+    // Only restore the handler if we are the current one
+    if (inited && curr == get_terminate()) {
+      set_terminate(prev);
+    }
+  }
+
+public:
+  AbortHandlerHelper(AbortHandlerHelper const&) = delete;
+  void operator=(AbortHandlerHelper const&) = delete;
+};
+
 namespace detail {
 void terminate_handler() {
   std::cout << "Unhandled exception caught in c10/util/AbortHandler.h" << std::endl;
   auto backtrace = get_backtrace();
   std::cout << backtrace << std::endl;
-  std::abort();
+  auto prev_handler = AbortHandlerHelper::getInstance().getPrev();
+  if (prev_handler) {
+    prev_handler();
+  } else {
+    std::abort();
+  }
 }
-}
+} // namespace detail
 
 void set_terminate_handler() {
-  std::set_terminate(detail::terminate_handler);
+  AbortHandlerHelper::getInstance().set(detail::terminate_handler);
 }
-}
+} // namespace c10
