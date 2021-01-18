a181e7a2510fbf5273a3b0d5fbe264c6f3ff7bea
Shen Li <cs.shenli@gmail.com>
2021-01-17 20:27:52 -0800

    Enable TensorPipe CUDA sending to self
    [ghstack-poisoned]

--- torch/csrc/distributed/rpc/tensorpipe_agent.cpp
+++ torch/csrc/distributed/rpc/tensorpipe_agent.cpp
@@ -85,6 +85,7 @@ constexpr int64_t kBasicChannelPriority = 0;
 
 #if TENSORPIPE_HAS_CUDA_IPC_CHANNEL && defined(USE_CUDA_NOT_ROCM)
 constexpr int64_t kCudaIpcChannelPriority = 300;
+constexpr int64_t kCudaXthChannelPriority = 400;
 #endif
 
 std::unique_ptr<TransportRegistration> makeUvTransport() {
@@ -200,6 +201,23 @@ C10_REGISTER_CREATOR(
     TensorPipeCudaChannelRegistry,
     cuda_ipc,
     makeCudaIpcChannel);
+
+#endif
+
+#ifdef USE_CUDA_NOT_ROCM
+
+std::unique_ptr<CudaChannelRegistration> makeCudaXthChannel() {
+  auto context = std::make_shared<tensorpipe::channel::cuda_xth::Context>();
+  return std::make_unique<CudaChannelRegistration>(
+      CudaChannelRegistration{std::move(context), kCudaXthChannelPriority});
+}
+
+// The cuda_xth channel supports same-process GPU-to-GPU comm
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+C10_REGISTER_CREATOR(
+    TensorPipeCudaChannelRegistry,
+    cuda_xth,
+    makeCudaXthChannel);
 
 #endif
 
