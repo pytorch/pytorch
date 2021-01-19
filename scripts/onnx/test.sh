1907d933b3c6b0ac4529c299409cd4fa9a81f599
BowenBao <bowbao@microsoft.com>
2021-01-19 09:17:44 -0800

    skip ort cuda test in CI

--- scripts/onnx/test.sh
+++ scripts/onnx/test.sh
@@ -57,6 +57,7 @@ pytest "${args[@]}" \
   --ignore "$top_dir/test/onnx/test_models_onnxruntime.py" \
   --ignore "$top_dir/test/onnx/test_utility_funs.py" \
   --ignore "$top_dir/test/onnx/test_pytorch_onnx_shape_inference.py" \
+  --ignore "$top_dir/test/onnx/test_pytorch_onnx_onnxruntime_cuda.py" \
   "${test_paths[@]}"
 
 # onnxruntime only support py3
