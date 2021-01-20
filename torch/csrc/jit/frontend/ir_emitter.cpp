d5499cb749a4e1f8caec36d85f38d48c737dd1f8
Lillian Johnson <lillianjohnson@fb.com>
2021-01-20 09:04:11 -0800

    [WIP] JIT Static Hooks: schema checking logic (#49975)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/49975
    
    Test Plan: Imported from OSS
    
    Reviewed By: heitorschueroff
    
    Differential Revision: D25771120
    
    Pulled By: Lilyjjo
    
    fbshipit-source-id: 262892cec45b6894bd8c0c20b9cfee43065abc7c

--- torch/csrc/jit/frontend/ir_emitter.cpp
+++ torch/csrc/jit/frontend/ir_emitter.cpp
@@ -4273,6 +4273,21 @@ void CompilationUnit::define_hooks(
     return existing_hook;
   };
 
+  // build_schema for checking
+  auto build_schema = [&](const Def& hook_def,
+                          const ResolverPtr& hook_res) -> FunctionSchema {
+    ScriptTypeParser typeParser(hook_res);
+    FunctionSchema schema =
+        typeParser.parseSchemaFromDef(hook_def, true /* skip_self*/);
+    // need to add self as the first because we skipped it
+    std::vector<Argument> arguments;
+    arguments.emplace_back(Argument(
+        hook_def.decl().params()[0].ident().name(), self->getClassType()));
+    arguments.insert(
+        arguments.end(), schema.arguments().begin(), schema.arguments().end());
+    return schema.cloneWithArguments(arguments);
+  };
+
   // define hooks
   for (size_t i = 0; i < hookDefs.size(); i++) {
     // check to see if already defined this hook
@@ -4295,6 +4310,8 @@ void CompilationUnit::define_hooks(
     function_table[fn->name()] = fn.get();
     functions.emplace_back(fn.get());
     this->register_function(std::move(fn));
+    self->getClassType()->checkForwardHookSchema(
+        i, build_schema(hookDefs[i], hookResolvers[i]));
     functions.back()->ensure_defined();
   }
 
@@ -4320,6 +4337,8 @@ void CompilationUnit::define_hooks(
     function_table[fn->name()] = fn.get();
     functions.emplace_back(fn.get());
     this->register_function(std::move(fn));
+    self->getClassType()->checkForwardPreHookSchema(
+        i, build_schema(preHookDefs[i], preHookResolvers[i]));
     functions.back()->ensure_defined();
   }
 }
