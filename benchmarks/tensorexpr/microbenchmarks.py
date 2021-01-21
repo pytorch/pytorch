078f3ecf68ae257535ac9ee004190131146e11dd
Horace He <horacehe2007@yahoo.com>
2021-01-21 00:07:53 -0800

    allowed for return code to be used

--- benchmarks/tensorexpr/microbenchmarks.py
+++ benchmarks/tensorexpr/microbenchmarks.py
@@ -57,7 +57,7 @@ def gen_unary_nnc_fun(nnc_name):
 def gen_unary_torch_fun(torch_op):
     def torch_fun(a, b, out):
         def fun():
-            torch_op(a, out=out)
+            return torch_op(a, out=out)
         return fun
     return torch_fun
 
@@ -105,8 +105,10 @@ def nnc_relu(A, B):
                                       torch._C.te.ExprHandle.float(0), A.load([i, j]))
     return f
 
+def pt_relu(a, b, c):
+    return torch.relu(a)
 custom_ops = [
-    ('relu', nnc_relu, lambda a, b, c: c.copy_(a).relu_()),
+    ('relu', nnc_relu, pt_relu),
     # ('nnc_mul_relu', nnc_mul_relu, pt_mul_relu)
     # ('manual_sigmoid', nnc_manual_sigmoid, lambda a, b, c: torch.sigmoid(a, out=c))
 ]
@@ -151,73 +153,72 @@ def run_benchmarks(benchmarks, sizes):
     df = pd.DataFrame(columns=['name', 'N', 'M', 'nnc_time', 'torch_time', 'ratio'])
     with torch.no_grad():
         for name, nnc_fun, torch_fun, shape_fn in benchmarks:
-            for N in sizes:
-                for M in sizes:
-                    iters = int(1e6 / (N + M))
-                    with kernel_arena_scope():
-                        if shape_fn is None:
-                            tA = torch.rand(M, N).clamp(0.01, 0.99)
-                            tB = torch.rand(M, N).clamp(0.01, 0.99)
-                            tX = torch.empty(M, N)
-                            tR = torch.empty(M, N)
-                        else:
-                            tA, tB, tX = shape_fn(M, N)
-                            tR = tX.clone()
+            for N, M in sizes:
+                iters = int(1e6 / (N + M))
+                with kernel_arena_scope():
+                    if shape_fn is None:
+                        tA = torch.rand(M, N).clamp(0.01, 0.99)
+                        tB = torch.rand(M, N).clamp(0.01, 0.99)
+                        tX = torch.empty(M, N)
+                        tR = torch.empty(M, N)
+                    else:
+                        tA, tB, tX = shape_fn(M, N)
+                        tR = tX.clone()
 
-                        def get_nnc_type(dtype):
-                            if dtype == torch.float:
-                                return torch._C.te.Dtype.Float
-                            elif dtype == torch.long:
-                                return torch._C.te.Dtype.Long
+                    def get_nnc_type(dtype):
+                        if dtype == torch.float:
+                            return torch._C.te.Dtype.Float
+                        elif dtype == torch.long:
+                            return torch._C.te.Dtype.Long
 
-                        dtype = get_nnc_type(tA.dtype)
+                    dtype = get_nnc_type(tA.dtype)
 
-                        dM = torch._C.te.ExprHandle.int(M)
-                        dN = torch._C.te.ExprHandle.int(N)
+                    dM = torch._C.te.ExprHandle.int(M)
+                    dN = torch._C.te.ExprHandle.int(N)
 
-                        A = torch._C.te.Placeholder('A', dtype, [dM, dN])
-                        B = torch._C.te.Placeholder('B', dtype, [dM, dN])
+                    A = torch._C.te.Placeholder('A', dtype, [dM, dN])
+                    B = torch._C.te.Placeholder('B', dtype, [dM, dN])
 
-                        dim_args = [torch._C.te.DimArg(*args) for args in [(dM, 'm'), (dN, 'n')]]
+                    dim_args = [torch._C.te.DimArg(*args) for args in [(dM, 'm'), (dN, 'n')]]
 
-                        compute = nnc_fun(A, B)
-                        X = torch._C.te.Compute('X', dim_args, compute)
-                        loopnest = torch._C.te.LoopNest([X])
-                        loopnest.prepare_for_codegen()
-                        stmt = torch._C.te.simplify(loopnest.root_stmt())
-                        cg = torch._C.te.construct_codegen('llvm', stmt, [torch._C.te.BufferArg(x) for x in [A, B, X]])
-
-
-                        # warmup
-                        for _ in range(10):
-                            cg.call([tA, tB, tX])
-                        start = time.time()
-                        for it in range(iters):
-                            cg.call([tA, tB, tX])
-                        time1 = time.time() - start
+                    compute = nnc_fun(A, B)
+                    X = torch._C.te.Compute('X', dim_args, compute)
+                    loopnest = torch._C.te.LoopNest([X])
+                    loopnest.prepare_for_codegen()
+                    stmt = torch._C.te.simplify(loopnest.root_stmt())
+                    cg = torch._C.te.construct_codegen('llvm', stmt, [torch._C.te.BufferArg(x) for x in [A, B, X]])
 
 
-                        fn = torch_fun(tA, tB, tR)
-                        # warmup
-                        for _ in range(10):
-                            fn()
-                        start = time.time()
-                        for it in range(iters):
-                            fn()
-                        time2 = time.time() - start
+                    # warmup
+                    for _ in range(10):
+                        cg.call([tA, tB, tX])
+                    start = time.time()
+                    for it in range(iters):
+                        cg.call([tA, tB, tX])
+                    time1 = time.time() - start
 
-                        df = df.append({'name': name, 'N': N, 'M': M, 'nnc_time': time1,
-                                        'torch_time': time2, 'ratio': time2 / time1}, ignore_index=True)
-                        print(name, N, M)
 
-                        print(time2 / time1, time1, time2)
-                        print()
+                    fn = torch_fun(tA, tB, tR)
+                    # warmup
+                    for _ in range(10):
+                        tR = fn()
+                    start = time.time()
+                    for it in range(iters):
+                        tR = fn()
+                    time2 = time.time() - start
 
-                        def check_correctness(a, b):
-                            if not np.allclose(a, b):
-                                print(name)
-                                assert(np.allclose(a, b))
-                        check_correctness(tX, tR)
+                    df = df.append({'name': name, 'N': N, 'M': M, 'nnc_time': time1,
+                                    'torch_time': time2, 'ratio': time2 / time1}, ignore_index=True)
+                    print(name, N, M)
+
+                    print(time2 / time1, time1, time2)
+                    print()
+
+                    def check_correctness(a, b):
+                        if not np.allclose(a, b):
+                            print(name)
+                            assert(np.allclose(a, b))
+                    check_correctness(tX, tR)
     return df
 
 def dump_plot(df, sizes):
@@ -252,5 +253,5 @@ if __name__ == "__main__":
         torch.set_num_threads(1)
 
     sizes = [1, 4, 16, 64, 256, 1024]
-    df = run_benchmarks(benchmarks, sizes)
+    df = run_benchmarks(benchmarks, [(i, i) for i in sizes])
     dump_plot(df, sizes)
