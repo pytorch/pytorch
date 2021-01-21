96b923752ec789d4bedfaab0be76993b4890ee0f
Wanchao Liang <wanchaol@users.noreply.github.com>
2021-01-21 10:54:17 -0800

    [optimizer] refactor RMSProp to use functional API (#50410)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/50410
    
    Test Plan: Imported from OSS
    
    Reviewed By: izdeby
    
    Differential Revision: D25932779
    
    Pulled By: wanchaol
    
    fbshipit-source-id: b0d6007ea83d77e2d70d04681163ea7e4632c5cd

--- torch/optim/functional.py
+++ torch/optim/functional.py
@@ -156,3 +156,43 @@ def adadelta(params: List[Tensor],
         delta = acc_delta.add(eps).sqrt_().div_(std).mul_(grad)
         param.add_(delta, alpha=-lr)
         acc_delta.mul_(rho).addcmul_(delta, delta, value=1 - rho)
+
+
+def rmsprop(params: List[Tensor],
+            grads: List[Tensor],
+            square_avgs: List[Tensor],
+            grad_avgs: List[Tensor],
+            momentum_buffer_list: List[Tensor],
+            lr: float,
+            alpha: float,
+            eps: float,
+            weight_decay: float,
+            momentum: float,
+            centered: bool):
+    r"""Functional API that performs rmsprop algorithm computation.
+
+    See :class:`~torch.optim.RMSProp` for details.
+    """
+
+    for i, param in enumerate(params):
+        grad = grads[i]
+        square_avg = square_avgs[i]
+
+        if weight_decay != 0:
+            grad = grad.add(param, alpha=weight_decay)
+
+        square_avg.mul_(alpha).addcmul_(grad, grad, value=1 - alpha)
+
+        if centered:
+            grad_avg = grad_avgs[i]
+            grad_avg.mul_(alpha).add_(grad, alpha=1 - alpha)
+            avg = square_avg.addcmul(grad_avg, grad_avg, value=-1).sqrt_().add_(eps)
+        else:
+            avg = square_avg.sqrt().add_(eps)
+
+        if momentum > 0:
+            buf = momentum_buffer_list[i]
+            buf.mul_(momentum).addcdiv_(grad, avg)
+            param.add_(buf, alpha=-lr)
+        else:
+            param.addcdiv_(grad, avg, value=-lr)
