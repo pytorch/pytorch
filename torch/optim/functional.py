51c87ff44c9f0ed6c91622d88f5f60a57f42258e
Wanchao Liang <wanchaol@users.noreply.github.com>
2021-01-21 10:54:17 -0800

    [optimizer] refactor SGD to use functional API (#45597)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/45597
    
    Test Plan: Imported from OSS
    
    Reviewed By: izdeby
    
    Differential Revision: D25932773
    
    Pulled By: wanchaol
    
    fbshipit-source-id: bc5f830d6812f847475b9bdcc67865d9968e3282

--- torch/optim/functional.py
+++ torch/optim/functional.py
@@ -2,7 +2,7 @@ r"""Functional interface"""
 import math
 import torch
 from torch import Tensor
-from typing import List
+from typing import List, Optional
 
 # TODO: use foreach API in optim.functional to do all the computation
 
@@ -96,3 +96,39 @@ def adam(params: List[Tensor],
         step_size = lr / bias_correction1
 
         param.addcdiv_(exp_avg, denom, value=-step_size)
+
+
+def sgd(params: List[Tensor],
+        d_p_list: List[Tensor],
+        momentum_buffer_list: List[Optional[Tensor]],
+        weight_decay: float,
+        momentum: float,
+        lr: float,
+        dampening: float,
+        nesterov: bool):
+    r"""Functional API that performs SGD algorithm computation.
+
+    See :class:`~torch.optim.SGD` for details.
+    """
+
+    for i, param in enumerate(params):
+
+        d_p = d_p_list[i]
+        if weight_decay != 0:
+            d_p = d_p.add(param, alpha=weight_decay)
+
+        if momentum != 0:
+            buf = momentum_buffer_list[i]
+
+            if buf is None:
+                buf = torch.clone(d_p).detach()
+                momentum_buffer_list[i] = buf
+            else:
+                buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
+
+            if nesterov:
+                d_p = d_p.add(buf, alpha=momentum)
+            else:
+                d_p = buf
+
+        param.add_(d_p, alpha=-lr)
