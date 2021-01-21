[Gradient Compression] Fix warm-start for PowerSGD laywerwise compression (#50283)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/50283

Realize that for the layerwise compression, the previous warm-start implementation only skips memory allocations, but does not skip filling random values for Qs.

Also fix the unit test in distributed_test.py. Previously the process group was not created correctly, and not communication occurred in the test_DistributedDataParallel_powerSGD_ddp_comm_hook.

Original PR issue: Investigate Applying PowerSGD to Communication Hook for Gradient Compression #47202
ghstack-source-id: 120101220

Test Plan:
Verified the fix by adding added some loggings locally.

Also verified no NE diff on Ads 1x.

Reviewed By: rohan-varma

Differential Revision: D25846222

fbshipit-source-id: 1ebeeb55ceba64d4d904ea6ac1bb42b1b2241520

torch/testing/_internal/distributed/distributed_test.py
@@ -2861,20 +2861,18 @@ class DistributedTest:
         @skip_if_lt_x_gpu(int(os.environ["WORLD_SIZE"]))
         @skip_if_rocm
         def test_DistributedDataParallel_powerSGD_ddp_comm_hook(self):
-            stream = torch.cuda.Stream(self.rank)
             rank = self.rank
-            rank_to_GPU = self._init_multigpu_helper()
-            gpus = list(rank_to_GPU[rank])
-            with torch.cuda.stream(stream):
+            for warm_start in [True, False]:
                 net = torch.nn.parallel.DistributedDataParallel(
                     torch.nn.Linear(1, 5).to(rank), device_ids=[rank]
                 )
-                process_group = torch.distributed.new_group(gpus)
                 state = powerSGD.PowerSGDState(
-                    process_group=process_group, matrix_approximation_rank=1
+                    # Use the default process group (dist.group.WORLD) instead of creating a new one.
+                    process_group=None, matrix_approximation_rank=1, warm_start=warm_start
                 )
                 net.register_comm_hook(state=state, hook=powerSGD.powerSGD_hook)
-                # NOTE: batched_powerSGD_hook cannot pass the following test, because it has a lower accuracy.
+                # NOTE: batched_powerSGD_hook cannot pass the following test, because it has a much lower accuracy.
+                # E.g., after the compression of batched_powerSGD_hook, a gradient of 0.5 can become 0.8335.
                 for i in range(1000):
                     # Clear gradients manually.
                     grad = net.module.weight.grad
@@ -2887,19 +2885,12 @@ class DistributedTest:
                     loss.backward()
                     # For each worker, the gradient on the weight should be worker_rank.
                     grad = net.module.weight.grad
-                    avg = grad.clone()
-                    # All-reducing the gradient averages should give us the gradient
-                    # average. If not, then one of the workers has not correctly
-                    # written back the averaged gradient before this all-reduce call.
-                    dist.all_reduce(avg)
                     world_size = int(os.environ["WORLD_SIZE"])
-                    avg.div_(world_size)
                     expected_grad = sum(i for i in range(world_size)) / world_size
-                    self.assertEqual(
-                        avg[0, 0],
-                        expected_grad,
-                        msg=f"Expected gradient of {expected_grad} but got {avg} on rank {self.rank}",
-                    )
+                    # Cannot use exact match here due to a very small accuracy loss, e.g., 1e-05.
+                    torch.testing.assert_allclose(
+                        grad[0, 0], expected_grad,
+                        msg=f"Expected gradient of {expected_grad} but got {grad} on rank {self.rank}")
 
 
         @unittest.skipIf(BACKEND != 'nccl' and BACKEND != 'gloo',