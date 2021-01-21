a91654cc05072b235c933f220b28a4113d749a37
Jay Zhang <jiz@microsoft.com>
2021-01-21 14:52:45 +0800

    Update comment in test file as well.

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -40,6 +40,7 @@ def convert_to_onnx(model, input=None, opset_version=9, example_outputs=None,
                     fixed_batch_size=False, training=None,
                     onnx_shape_inference=False,
                     use_new_jit_passes=False):
+
     # export the model to ONNX
     f = io.BytesIO()
     input_copy = copy.deepcopy(input)
