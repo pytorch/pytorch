86380894166d85d3e0e382ea091c4ed76d0855da
mattip <matti.picus@gmail.com>
2021-01-21 13:54:49 +0200

    fixes from review

--- test/test_torch.py
+++ test/test_torch.py
@@ -2895,17 +2895,6 @@ class TestTorchDeviceType(TestCase):
         self.assertEqual(a.size(), deepcopy(a).size())
         self.assertEqual(a, deepcopy(a))
 
-    @dtypes(torch.float32)
-    @onlyCPU
-    def test_deepcopy_gradient(self, device, dtype):
-        from copy import deepcopy
-        a = torch.zeros(10)
-        a.grad = torch.ones(10)
-        self.assertEqual(a.grad, deepcopy(a).grad)
-        s = torch.zeros(10).to_sparse()
-        s.grad = torch.ones(10).to_sparse()
-        self.assertEqual(s.grad, deepcopy(s).grad)
-
     def check_internal_mem_overlap(self, inplace_op, num_inputs,
                                    dtype, device,
                                    expected_failure=False):
@@ -6995,6 +6984,16 @@ class TestTensorDeviceOps(TestCase):
 
 class TestTorch(AbstractTestCases._TestTorchMixin):
     exact_dtype = True
+
+    def test_deepcopy_gradient(self):
+        from copy import deepcopy
+        a = torch.zeros(10)
+        a.grad = torch.ones(10)
+        self.assertEqual(a.grad, deepcopy(a).grad)
+        s = torch.zeros(10).to_sparse()
+        s.grad = torch.ones(10).to_sparse()
+        self.assertEqual(s.grad, deepcopy(s).grad)
+
 
 # TODO: this empy class is temporarily instantiated for XLA compatibility
 #   once XLA updates their test suite it should be removed
