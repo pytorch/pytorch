8b11ab78482787d2e2a79ff9ac0fce0d7e243ad9
Jane Xu <janeyx@fb.com>
2021-01-22 08:09:04 -0800

    Split torch_cuda into torch_cuda_cu and torch_cuda_cpp; update API accordingly

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -876,7 +876,8 @@ def CUDAExtension(name, sources, *args, **kwargs):
     else:
         libraries.append('cudart')
         libraries.append('c10_cuda')
-        libraries.append('torch_cuda')
+        libraries.append('torch_cuda_cu')
+        libraries.append('torch_cuda_cpp')
     kwargs['libraries'] = libraries
 
     include_dirs = kwargs.get('include_dirs', [])
@@ -1431,8 +1432,11 @@ def _prepare_ldflags(extra_ldflags, with_cuda, verbose, is_standalone):
             extra_ldflags.append('c10_cuda.lib')
         extra_ldflags.append('torch_cpu.lib')
         if with_cuda:
-            extra_ldflags.append('torch_cuda.lib')
-            # /INCLUDE is used to ensure torch_cuda is linked against in a project that relies on it.
+            extra_ldflags.append('torch_cuda_cu.lib')
+            # /INCLUDE is used to ensure torch_cuda_cu is linked against in a project that relies on it.
+            extra_ldflags.append('-INCLUDE:?searchsorted_cuda@native@at@@YA?AVTensor@2@AEBV32@0_N1@Z')
+            extra_ldflags.append('torch_cuda_cpp.lib')
+            # /INCLUDE is used to ensure torch_cuda_cpp is linked against in a project that relies on it.
             # Related issue: https://github.com/pytorch/pytorch/issues/31611
             extra_ldflags.append('-INCLUDE:?warp_size@cuda@at@@YAHXZ')
         extra_ldflags.append('torch.lib')
@@ -1448,7 +1452,7 @@ def _prepare_ldflags(extra_ldflags, with_cuda, verbose, is_standalone):
             extra_ldflags.append('-lc10_hip' if IS_HIP_EXTENSION else '-lc10_cuda')
         extra_ldflags.append('-ltorch_cpu')
         if with_cuda:
-            extra_ldflags.append('-ltorch_hip' if IS_HIP_EXTENSION else '-ltorch_cuda')
+            extra_ldflags.append('-ltorch_hip' if IS_HIP_EXTENSION else '-ltorch_cuda_cu -ltorch_cuda_cpp')
         extra_ldflags.append('-ltorch')
         if not is_standalone:
             extra_ldflags.append('-ltorch_python')
