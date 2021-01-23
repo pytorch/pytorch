cd2067539e55253d954bba5a4a8999de39d144f4
Wanchao Liang <wanchaol@users.noreply.github.com>
2021-01-23 00:58:56 -0800

    [dist_optim] add distributed functional sgd optimizer (#50618)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/50618
    
    Test Plan: Imported from OSS
    
    Reviewed By: rohan-varma
    
    Differential Revision: D25932778
    
    Pulled By: wanchaol
    
    fbshipit-source-id: 8df3567b477bc5ba3556b8c5294cd3da5db963ad

--- torch/distributed/optim/optimizer.py
+++ torch/distributed/optim/optimizer.py
@@ -8,6 +8,7 @@ from torch import Tensor
 from torch.distributed.rpc import RRef
 from .functional_adagrad import _FunctionalAdagrad
 from .functional_adam import _FunctionalAdam
+from .functional_sgd import _FunctionalSGD
 import torch.distributed.autograd as dist_autograd
 
 
@@ -183,6 +184,7 @@ class DistributedOptimizer:
     functional_optim_map = {
         optim.Adagrad: _FunctionalAdagrad,
         optim.Adam: _FunctionalAdam,
+        optim.SGD: _FunctionalSGD,
     }
 
     def __init__(self, optimizer_class, params_rref, *args, **kwargs):
--- torch/testing/_internal/distributed/rpc/dist_optimizer_test.py
+++ torch/testing/_internal/distributed/rpc/dist_optimizer_test.py
@@ -200,6 +200,7 @@ class DistOptimizerTest(RpcAgentTestFixture):
 
     @dist_init()
     def test_dist_optim(self):
-        self._test_dist_optim_base(optim.SGD, lr=0.05)
         self._test_dist_optim_base(optim.Adagrad, lr=0.05)
         self._test_dist_optim_base(optim.Adam, lr=1e-2, amsgrad=True)
+        self._test_dist_optim_base(optim.SGD, lr=0.05)
+        self._test_dist_optim_base(optim.SGD, lr=1e-3, momentum=1, weight_decay=1, nesterov=True)
