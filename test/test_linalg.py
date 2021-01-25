7fc28c5ebe5c810d082050dea75c0875cc96cdbb
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-01-25 07:16:08 -0600

    Added test_svd_complex to check that V is returned, not V.conj()

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -1873,6 +1873,20 @@ class TestLinalg(TestCase):
         actual_rank, size, batches = 2, (17, 4), ()
         run_subtest(actual_rank, size, batches, device, jitted)
 
+    @skipCUDAIfNoMagmaAndNoCusolver
+    @skipCPUIfNoLapack
+    @dtypes(torch.cfloat)
+    def test_svd_complex(self, device, dtype):
+        # this test verifies that torch.svd really returns V and not V.conj()
+        # see: https://github.com/pytorch/pytorch/issues/45821
+        t = torch.randn((10, 10), dtype=dtype, device=device)
+        U, S, V = torch.svd(t, some=False)
+        # verify that t ≈ t2
+        # t2 = U @ diag(S) @ Vᴴ
+        # Vᴴ is the conjugate transpose of V
+        t2 = U @ torch.diag(S).type(dtype) @ V.conj().T
+        self.assertEqual(t, t2)
+
     def _test_svd_helper(self, shape, some, col_maj, device, dtype):
         cpu_tensor = torch.randn(shape, device='cpu').to(dtype)
         device_tensor = cpu_tensor.to(device=device)
