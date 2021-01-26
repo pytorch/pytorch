7279b82118d33ee1a1efddbde852e6c79ea99dbb
Mike Ruberry <mruberry@fb.com>
2021-01-26 02:05:07 -0800

    Revert D25850783: Add torch::deploy, an embedded torch-python interpreter
    Test Plan: revert-hammer
    
    Differential Revision:
    D25850783 (https://github.com/pytorch/pytorch/commit/3192f9e4fea9bf88d131542c53963c140f4fc7c1)
    
    Original commit changeset: a4656377caff
    
    fbshipit-source-id: 1c7133627da28fb12848da7a9a46de6d3b2b67c6

--- torch/utils/__init__.py
+++ torch/utils/__init__.py
@@ -2,7 +2,6 @@
 from .throughput_benchmark import ThroughputBenchmark
 
 import os.path as _osp
-import sys
 
 # Set the module for a given object for nicer printing
 def set_module(obj, mod):
@@ -10,8 +9,5 @@ def set_module(obj, mod):
         raise TypeError("The mod argument should be a string")
     obj.__module__ = mod
 
-if sys.executable == "torch_deploy":
-    # not valid inside torch_deploy interpreter, no paths exists for frozen modules
-    cmake_prefix_path = None
-else:
-    cmake_prefix_path = _osp.join(_osp.dirname(_osp.dirname(__file__)), 'share', 'cmake')
+#: Path to folder containing CMake definitions for Torch package
+cmake_prefix_path = _osp.join(_osp.dirname(_osp.dirname(__file__)), 'share', 'cmake')
