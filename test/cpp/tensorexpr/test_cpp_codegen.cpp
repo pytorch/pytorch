46a65456e29d8d59cdadcd542fd8fc49d247a38e
Cheng Chang <xcc@fb.com>
2021-01-26 19:02:03 -0800

    use Block and FileCheck

--- test/cpp/tensorexpr/test_cpp_codegen.cpp
+++ test/cpp/tensorexpr/test_cpp_codegen.cpp
@@ -2,6 +2,7 @@
 
 #include <test/cpp/tensorexpr/test_base.h>
 
+#include <torch/csrc/jit/testing/file_check.h>
 #include "torch/csrc/jit/tensorexpr/cpp_codegen.h"
 #include "torch/csrc/jit/tensorexpr/mem_arena.h"
 #include "torch/csrc/jit/tensorexpr/stmt.h"
@@ -17,12 +18,17 @@ TEST(CppPrinter, AllocateOnStackThenFree) {
   VarHandle var("x", kHandle);
   Allocate* alloc = Allocate::make(var, kInt, {dim0, dim1});
   Free* free = Free::make(var);
+  Block* block = Block::make({alloc, free});
 
   std::stringstream ss;
   CppPrinter printer(&ss);
-  printer.visit(alloc);
-  printer.visit(free);
-  ASSERT_EQ(ss.str(), "int x[6];\n");
+  printer.visit(block);
+  const std::string expected = R"(
+    # CHECK: {
+    # CHECK:   int x[6];
+    # CHECK: }
+  )";
+  torch::jit::testing::FileCheck().run(expected, ss.str());
 }
 
 TEST(CppPrinter, AllocateOnHeapThenFree) {
@@ -31,16 +37,20 @@ TEST(CppPrinter, AllocateOnHeapThenFree) {
   VarHandle var("y", kHandle);
   Allocate* alloc = Allocate::make(var, kLong, {dim0, dim1, dim2});
   Free* free = Free::make(var);
+  Block* block = Block::make({alloc, free});
 
   std::stringstream ss;
   CppPrinter printer(&ss);
-  printer.visit(alloc);
-  printer.visit(free);
+  printer.visit(block);
   // size(long) = 8;
   // dim0 * dim1 * dim2 * size(long) = 24000.
-  ASSERT_EQ(
-      ss.str(),
-      "int64_t* y = static_cast<int64_t*>(malloc(24000));\nfree(y);\n");
+  const std::string expected = R"(
+    # CHECK: {
+    # CHECK:   int64_t* y = static_cast<int64_t*>(malloc(24000));
+    # CHECK:   free(y);
+    # CHECK: }
+  )";
+  torch::jit::testing::FileCheck().run(expected, ss.str());
 }
 
 } // namespace jit
