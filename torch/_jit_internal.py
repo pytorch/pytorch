3947e78dcb7fc4b3358f5d7dcaea1630daf6e00e
Nikita Shulga <nshulga@fb.com>
2021-01-27 10:49:10 -0800

    Fix more JIT tests under Python-3.9 (#51182)
    Summary:
    Mostly replace `global Foo` with `make_global(Foo)`
    The only real fix is generating Subscript annotation, which is a follow up from https://github.com/pytorch/pytorch/pull/48676
    
    Fixes https://github.com/pytorch/pytorch/issues/49617
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51182
    
    Reviewed By: gmagogsfm
    
    Differential Revision: D26095244
    
    Pulled By: malfet
    
    fbshipit-source-id: 0e043d9a2cf43fff71dfbb341f708cd7af87c39a

--- torch/_jit_internal.py
+++ torch/_jit_internal.py
@@ -241,7 +241,9 @@ def get_annotation_str(annotation):
     elif isinstance(annotation, ast.Attribute):
         return '.'.join([get_annotation_str(annotation.value), annotation.attr])
     elif isinstance(annotation, ast.Subscript):
-        return f"{get_annotation_str(annotation.value)}[{get_annotation_str(annotation.slice.value)}]"  # type: ignore
+        # In Python3.9+ subscript indicies are not wrapped in ast.Index
+        subscript_slice = annotation.slice if sys.version_info >= (3, 9) else annotation.slice.value  # type: ignore
+        return f"{get_annotation_str(annotation.value)}[{get_annotation_str(subscript_slice)}]"
     elif isinstance(annotation, ast.Tuple):
         return ','.join([get_annotation_str(elt) for elt in annotation.elts])
     elif isinstance(annotation, ast.Constant) or isinstance(annotation, ast.NameConstant):
