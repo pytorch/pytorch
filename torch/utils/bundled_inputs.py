e1c3c9ef9f55129b5b7ef055171e0723c66e3433
jakeszwe <jakeszwe@devvm1677.vll0.facebook.com>
2021-01-27 10:06:20 -0800

    Update on "[Pytorch] Expanded Bundled Inputs To Any Public Function"
    Enabled bundled inputs for all public functions that the user wants in a torchscript module. An important caveat here is that you cant add bundled inputs to functions that were in the nn.module but weren't caught in the scripting/tracing process that brought the model to torchscript.
    
    
    Old Api is exactly the same. Still only works on forward, return types the same, etc.
    
    -----------New API-------------
    
    Attachment of inputs:
    
    ***augment_model_with_bundled_inputs*** : works the same as before but added the option to specify an info dictionary.
    
    ***augment_many_model_functions_with_bundled_inputs*** : Similar to the above function but allows the user to specify a Dict[Callable, List[<inputs>]] (mapping function references to the bundled inputs for that function) to attach bundled inputs to many functions
    
    Consumption of inputs:
    
    ***get_all_bundled_inputs_for_<function_name>()*** : Works exactly like get_all_bundled_inputs does, but can be used for functions other then forward if you know ahead of time what they are called, and if they have bundled inputs.
    
    ***get_bundled_inputs_functions_and_info()*** : This is easily the hackiest function. Returns a Dict['str', 'str'] mapping function_names to get_all_bundled_inputs_for_<function_name>. A user can then execute the functions specified in the values with something like
        all_info = model.get_bundled_inputs_functions_and_info()
        for func_name in all_info.keys():
            input_func_name = all_info[func_name]['get_inputs_function_name'][0]
            func_to_run = getattr(loaded, input_func_name)
    The reason its done this way is because torchscript doesn't support 'Any' type yet meaning I can't return the bundled inputs directly because they could be different types for each function. Torchscript also doesn't support callable so I can't return a function reference directly either.
    
    Differential Revision: [D25931961](https://our.internmc.facebook.com/intern/diff/D25931961/)
    
    [ghstack-poisoned]

--- torch/utils/bundled_inputs.py
+++ torch/utils/bundled_inputs.py
@@ -19,7 +19,7 @@ def augment_model_with_bundled_inputs(
         model: torch.jit.ScriptModule,
         inputs: Optional[Sequence[Tuple[Any, ...]]] = None,
         _receive_inflate_expr: Optional[List[str]] = None,  # For debugging.
-        info: Optional[Dict[str, List[str]]] = None,  # Optional argument to provide info about forward or its inputs
+        info: Optional[List[str]]] = None,  # Optional argument to provide info about forward or its inputs
 ) -> None:
     """ Wrapper around augment_many_model_functions_with_bundled_inputs to provide a streamlined api for forward
     which is the only function the vast majority of models need bundled inputs for.
@@ -43,7 +43,7 @@ def augment_model_with_bundled_inputs(
 
 def augment_many_model_functions_with_bundled_inputs(
         model: torch.jit.ScriptModule,
-        inputs: Dict[Callable, Optional[Sequence[Tuple[Any]]]],
+        inputs: Dict[Callable, Optional[Sequence[Tuple[Any, ...]]]],
         _receive_inflate_expr: Optional[List[str]] = None,  # For debugging.
         info: Optional[Dict[Callable, List[str]]] = None,  # Optional argument to provide info about the function or its inputs
 ) -> None:
@@ -108,8 +108,7 @@ def augment_many_model_functions_with_bundled_inputs(
     for function, input_list in inputs.items():
         function_name = function.__name__
 
-        assert(hasattr(function, 'schema'))
-        function_arg_types = [arg.type for arg in function.schema.arguments[1:]]
+        function_arg_types = [arg.type for arg in function.schema.arguments[1:]] # type: ignore
         deflated_inputs_type: ListType = ListType(TupleType(function_arg_types))
         inflated_inputs_type: OptionalType[ListType] = OptionalType(deflated_inputs_type)
         model._c._register_attribute("_bundled_inputs_deflated_{name}".format(name=function_name), deflated_inputs_type, [])
