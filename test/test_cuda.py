776ed5c371344fedcd780cb8ab599e831ef2f704
Nikita Shulga <nshulga@fb.com>
2021-01-27 17:10:36 -0800

    Move the test to TestCuda vs TestCudaComm class

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -3113,6 +3113,16 @@ t2.start()
         self.assertEqual(mean, torch.ones(3, device='cuda'))
         self.assertEqual(invstd, torch.ones(3, device='cuda'))
 
+    @unittest.skipIf(not TEST_MULTIGPU, "Test needs multiple GPUs")
+    def test_cuda_device_memory_allocated(self):
+        from torch.cuda import memory_allocated
+        device_count = torch.cuda.device_count()
+        current_alloc = [memory_allocated(idx) for idx in range(device_count)]
+        x = torch.ones(10, device="cuda:0")
+        self.assertTrue(memory_allocated(0) > current_alloc[0])
+        self.assertTrue(all(memory_allocated(torch.cuda.device(idx)) == current_alloc[idx] for idx in range(1, device_count)))
+
+
 class TestCudaComm(TestCase):
     def _test_broadcast(self, input):
         if not TEST_MULTIGPU:
@@ -3495,15 +3505,6 @@ class TestCudaComm(TestCase):
             expected_b = b_tensors_for_gpu[i]
             self.assertEqual(expected_a, x.a)
             self.assertEqual(expected_b, x.b)
-
-    @unittest.skipIf(not TEST_MULTIGPU, "Test needs multiple GPUs")
-    def test_cuda_device_memory_allocated(self):
-        from torch.cuda import memory_allocated
-        device_count = torch.cuda.device_count()
-        current_alloc = [memory_allocated(idx) for idx in range(device_count)]
-        x = torch.ones(10, device="cuda:0")
-        self.assertTrue(memory_allocated(0) > current_alloc[0])
-        self.assertTrue(all(memory_allocated(torch.cuda.device(idx)) == current_alloc[idx] for idx in range(1, device_count)))
 
 
 if __name__ == '__main__':
