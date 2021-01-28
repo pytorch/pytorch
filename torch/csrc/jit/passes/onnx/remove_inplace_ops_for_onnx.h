dc81f3da522d83559d46fd2f165f90257552aa4e
neginraoof <neginmr@utexas.edu>
2021-01-28 11:39:47 -0800

    Fix for feedback

--- torch/csrc/jit/passes/onnx/remove_inplace_ops_for_onnx.h
+++ torch/csrc/jit/passes/onnx/remove_inplace_ops_for_onnx.h
@@ -5,7 +5,7 @@
 namespace torch {
 namespace jit {
 
-TORCH_API void RemoveInplaceOpsForONNX(const std::shared_ptr<Graph>& graph);
+TORCH_API void RemoveInplaceOpsForONNX(const std::shared_ptr<Graph>& graph, Module* model);
 
 TORCH_API void PrepareInplaceOpsForONNX(const std::shared_ptr<Graph>& graph);
 
