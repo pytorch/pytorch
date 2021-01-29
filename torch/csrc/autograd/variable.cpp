88ae6ef2cff27c08cad00224398a723a3f7b028b
Joel Benjamin Schlosser <jbschlosser@fb.com>
2021-01-29 10:19:22 -0500

    Use RAII for temporarily setting grad mode

--- torch/csrc/autograd/variable.cpp
+++ torch/csrc/autograd/variable.cpp
@@ -452,14 +452,14 @@ const std::shared_ptr<torch::autograd::Node>& VariableHooks::grad_fn(const Tenso
           fn->storage_offset = self.storage_offset();
 
           // If we are here, the tensor we are operating on is a differentiable view, meaning
-          // that a valid graph exists and its validity should be maintained. To ensure this,
-          // we temporarily enable grad mode here while collecting edges. Without this logic,
-          // the collected edges will be an empty set, and the graph will be invalidated by
-          // connecting the new node to nowhere.
-          auto grad_enabled = GradMode::is_enabled();
-          GradMode::set_enabled(true);
-          fn->set_next_edges(torch::autograd::collect_next_edges(view_info.base_));
-          GradMode::set_enabled(grad_enabled);
+          // that a valid graph exists to which we should attach the new node. To do this
+          // correctly, we temporarily disable grad mode here while collecting edges. Without
+          // this logic, the collected edges will be an empty set, and the graph will be
+          // invalidated by connecting the new node to nowhere.
+          {
+            AutoGradMode grad_mode(true);
+            fn->set_next_edges(torch::autograd::collect_next_edges(view_info.base_));
+          }
 
           fn->add_input_metadata(
             view_info.base_.options(),
