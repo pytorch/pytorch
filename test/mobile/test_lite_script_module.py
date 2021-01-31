51aea228fef97a3a98aae3987722e30fc47ebbfb
myuan <myuan@fb.com>
2021-01-31 12:10:21 -0800

    [PyTorch Mobile] Support torchbind custom classes in lite interpreter
    Differential Revision: [D26168913](https://our.internmc.facebook.com/intern/diff/D26168913/)
    
    [ghstack-poisoned]

--- test/mobile/test_lite_script_module.py
+++ test/mobile/test_lite_script_module.py
@@ -190,7 +190,7 @@ class TestLiteScriptModule(TestCase):
         mobile_module_result = mobile_module.forward(*bundled_inputs[0])
         torch.testing.assert_allclose(script_module_result, mobile_module_result)
 
-    def test_unsupported_createobject(self):
+    def test_unsupported_classtype(self):
         class Foo():
             def __init__(self):
                 return
@@ -205,7 +205,6 @@ class TestLiteScriptModule(TestCase):
 
         script_module = torch.jit.script(MyTestModule())
         with self.assertRaisesRegex(RuntimeError,
-                                    r"^CREATE_OBJECT is not supported in mobile module\. "
                                     r"Workaround: instead of using arbitrary class type \(class Foo\(\)\), "
                                     r"define a pytorch class \(class Foo\(torch\.nn\.Module\)\)\.$"):
             script_module._save_to_buffer_for_lite_interpreter()
