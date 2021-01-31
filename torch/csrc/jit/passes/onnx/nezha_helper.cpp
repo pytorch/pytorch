9137bc9a57ba8a858ddc64274daad30ec82cf051
Jay Zhang <jiz@microsoft.com>
2021-02-01 00:14:01 +0800

    Update the way to update block.

--- torch/csrc/jit/passes/onnx/nezha_helper.cpp
+++ torch/csrc/jit/passes/onnx/nezha_helper.cpp
@@ -18,7 +18,14 @@ void NeZha_TryUpdateGraph(
     std::shared_ptr<Graph>& src_graph) {
     
     printf("------ Start NeZha_TryUpdateGraph ------");
-    dst_graph = src_graph;
+    dst_graph = std::make_shared<Graph>();
+    auto env = [](Value* v) -> Value* {
+    AT_ERROR(
+        "Graph::copy() encountered a use of a value " + v->debugName() +
+        " not in scope. Run lint!");
+    };
+    printf("------ NeZha_TryUpdateGraph: Clone block to dst_graph ------");
+    dst_graph->block()->cloneFrom(src_graph->block(), env);
 }
 
 
