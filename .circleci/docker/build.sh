63afa884fb89ddea39378f01b615a7c3e4cc2845
Jeff Daily <jeff.daily@amd.com>
2021-02-01 23:12:21 +0000

    [ROCm] add 4.0.1 docker image
    Add a ROCm 4.0.1 docker image for CI. Keep the 3.10 image and remove the
    3.9 image. Plan is to keep two ROCm versions at a time.

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -274,19 +274,19 @@ case "$image" in
     VISION=yes
     KATEX=yes
     ;;
-  pytorch-linux-bionic-rocm3.9-py3.6)
-    ANACONDA_PYTHON_VERSION=3.6
-    PROTOBUF=yes
-    DB=yes
-    VISION=yes
-    ROCM_VERSION=3.9
-    ;;
   pytorch-linux-bionic-rocm3.10-py3.6)
     ANACONDA_PYTHON_VERSION=3.6
     PROTOBUF=yes
     DB=yes
     VISION=yes
     ROCM_VERSION=3.10
+    ;;
+  pytorch-linux-bionic-rocm4.0.1-py3.6)
+    ANACONDA_PYTHON_VERSION=3.6
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    ROCM_VERSION=4.0.1
     ;;
   *)
     # Catch-all for builds that are not hardcoded.
