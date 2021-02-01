d9f0a16a3689ddd81507819faacdf37e49c85960
Nikita Shulga <nshulga@fb.com>
2021-01-31 20:09:01 -0800

    Modify test_type_promotion
    ceil from int64 to int64 should not throw

--- test/test_type_promotion.py
+++ test/test_type_promotion.py
@@ -920,16 +920,12 @@ class TestTypePromotion(TestCase):
         out = torch.empty(0, dtype=dtypes[1], device=device)
 
         ops = (torch.neg, torch.floor, torch.ceil)
-        float_only_ops = {torch.floor, torch.ceil}
         real_only_ops = {torch.floor, torch.ceil}
         for op in ops:
             if dtypes[0] is not dtypes[1]:
                 with self.assertRaises(RuntimeError):
                     op(t, out=out)
             elif op in real_only_ops and dtypes[0].is_complex:
-                with self.assertRaises(RuntimeError):
-                    op(t, out=out)
-            elif op in float_only_ops and (not dtypes[0].is_floating_point and not dtypes[0].is_complex):
                 with self.assertRaises(RuntimeError):
                     op(t, out=out)
             else:
