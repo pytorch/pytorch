831b8764c012f06a9997e24e42a6234d625f7906
ErjiaGuan <erjia@fb.com>
2021-02-01 15:10:41 -0500

    [DataLoader] Rename Functional DataSet to DataPipe
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/__init__.py
+++ torch/utils/data/datapipes/iter/__init__.py
@@ -1,4 +1,10 @@
 from torch.utils.data.datapipes.iter.listdirfiles import ListDirFilesIterDataPipe as ListDirFiles
 from torch.utils.data.datapipes.iter.loadfilesfromdisk import LoadFilesFromDiskIterDataPipe as LoadFilesFromDisk
 
-__all__ = ['ListDirFiles', 'LoadFilesFromDisk']
+# Functional DataPipe
+from torch.utils.data.datapipes.iter.batch import BatchIterDataPipe as Batch, BucketBatchIterDataPipe as BucketBatch
+from torch.utils.data.datapipes.iter.callable import CallableIterDataPipe as Callable, CollateIterDataPipe as Collate
+from torch.utils.data.datapipes.iter.sampler import SamplerIterDataPipe as Sampler
+
+__all__ = ['ListDirFiles', 'LoadFilesFromDisk',
+           'Batch', 'BucketBatch', 'Callable', 'Collate', 'Sampler']
