2eba2f7237486f30d133b68e8fdbc0847417253f
Eli Uriegas <eliuriegas@fb.com>
2021-02-02 11:23:50 -0800

    Update on "DO NOT MERGE, testing smaller conda images"
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -61,7 +61,7 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+retry git clone -b fix39_conda_builds -q https://github.com/seemethere/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
