077ed73a5586ce0f28cf5702e214e2a53b2d9f52
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2021-02-02 15:56:20 -0800

    move ZipDataset to Zip DataPipe
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/__init__.py
+++ torch/utils/data/datapipes/iter/__init__.py
@@ -1,5 +1,6 @@
 from torch.utils.data.datapipes.iter.listdirfiles import ListDirFilesIterDataPipe as ListDirFiles
 from torch.utils.data.datapipes.iter.loadfilesfromdisk import LoadFilesFromDiskIterDataPipe as LoadFilesFromDisk
 from torch.utils.data.datapipes.iter.readfilesfromtar import ReadFilesFromTarIterDataPipe as ReadFilesFromTar
+from torch.utils.data.datapipes.iter.readfilesfromzip import ReadFilesFromZipIterDataPipe as ReadFilesFromZip
 
-__all__ = ['ListDirFiles', 'LoadFilesFromDisk', 'ReadFilesFromTar']
+__all__ = ['ListDirFiles', 'LoadFilesFromDisk', 'ReadFilesFromTar', 'ReadFilesFromZip']
