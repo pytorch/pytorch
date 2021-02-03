648cdb7d0ae058aa6ac582711d5f5f5ebe7931d0
Edward Yang <ezyang@fb.com>
2021-02-03 13:57:56 -0800

    Relax type signature for tools.codegen.api.translate (#51477)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51477
    
    Passing in a full binding is still OK, but if you have less
    (e.g., an Expr/CType), that will do too. I'll need this for
    some codegen patches I'm doing later.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Reviewed By: bhosmer
    
    Differential Revision: D26179560
    
    Pulled By: ezyang
    
    fbshipit-source-id: 5730dfb2c91bf5325496e57b0c91eb6823c9194d

--- tools/codegen/api/translate.py
+++ tools/codegen/api/translate.py
@@ -1,4 +1,4 @@
-from typing import Dict, Sequence, List, NoReturn
+from typing import Dict, Sequence, List, NoReturn, Union
 from tools.codegen.api.types import *
 
 # This file implements a small program synthesis engine that implements
@@ -45,13 +45,39 @@ class UnsatError(RuntimeError):
 #
 # and you need to generate "exprs".
 #
-# TODO: Don't need full Binding for goals, CType will do
-# TODO: Don't need full Binding for bindings, list of Expr will do
-def translate(bindings: Sequence[Binding], goals: Sequence[Binding], *, method: bool = False) -> List[Expr]:
+# Typically, a list of Bindings is convenient to get (you usually call something
+# like arguments() to get them); but technically you only need less information:
+# for 'bindings' an (un-ordered) list of Exprs is sufficient; similarly, for
+# 'goals', an (ordered) list of CType goals is sufficient.  If you are doing
+# something more complicated, e.g., tracking the set of bindings in a context,
+# you may find using these smaller types more convenient.
+def translate(
+    bindings: Sequence[Union[Expr, Binding]],
+    goals: Sequence[Union[CType, Binding]],
+    *, method: bool = False
+) -> List[Expr]:
+
+    binding_exprs: List[Expr] = []
+    for b in bindings:
+        if isinstance(b, Binding):
+            binding_exprs.append(Expr(
+                expr=b.name,
+                type=b.ctype,
+            ))
+        else:
+            binding_exprs.append(b)
+
+    goal_ctypes: List[CType] = []
+    for g in goals:
+        if isinstance(g, Binding):
+            goal_ctypes.append(g.ctype)
+        else:
+            goal_ctypes.append(g)
+
     # Add all the bindings to the context
     ctx: Dict[CType, str] = {}
-    for b in bindings:
-        ctx[b.ctype] = b.name
+    for b in binding_exprs:
+        ctx[b.type] = b.expr
 
     # Add implicit bindings if the generated code is inside a Tensor method
     if method:
@@ -63,15 +89,7 @@ def translate(bindings: Sequence[Binding], goals: Sequence[Binding], *, method: 
     def unsat(goal: CType) -> NoReturn:
         ctx_desc = '\n'.join(f"  {t.cpp_type()} {e};" for t, e in ctx.items())
         raise UnsatError(f'''
-Failed to synthesize the expression "{goal.cpp_type()} {goal.name}"
-while trying to translate from:
-
-  from_func({', '.join(b.defn() for b in bindings)})
-
-to:
-
-  to_func({', '.join(g.defn() for g in goals)})
-
+Failed to synthesize the expression "{goal.cpp_type()} {goal.name}".
 When I failed, the following bindings were available in the context:
 
 {ctx_desc}
@@ -140,4 +158,4 @@ Check this module for more information.
 
         unsat(goal)
 
-    return [Expr(solve(g.ctype, direct=False), g.ctype) for g in goals]
+    return [Expr(solve(g, direct=False), g) for g in goal_ctypes]
