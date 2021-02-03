eb571b33fe08b2b7d81d47c2678eda669d2522c8
Kimish Patel <kimishpatel@fb.com>
2021-02-02 21:28:00 -0800

    [QNNPACK Sparse] Create fc sparse operator (#50586)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/50586
    
    Creates sparse operator for fully connected layer.
    
    Test Plan:
    ./build/local/fully-connected-sparse-test
    
    Imported from OSS
    
    Reviewed By: AshkanAliabadi
    
    Differential Revision: D25925503
    
    fbshipit-source-id: 49042158ba3bf26a716a6d68258fc7ead85ce9d8

---
+++ aten/src/ATen/native/quantized/cpu/qnnpack/src/fully-connected-sparse.c
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <pytorch_qnnpack.h>
+#include <qnnpack/log.h>
+#include <qnnpack/math.h>
+#include <qnnpack/operator.h>
+#include <qnnpack/pack.h>
+#include <qnnpack/params.h>
+#include <qnnpack/requantization.h>
+
+enum pytorch_qnnp_status pytorch_qnnp_create_fully_connected_sparse_dq_nc_q8(
+    size_t input_channels,
+    size_t output_channels,
+    uint8_t input_zero_point,
+    const uint8_t* kernel_zero_points,
+    const uint32_t* kernel_col_indices,
+    const uint32_t* kernel_row_values,
+    const uint8_t* kernel_values,
+    const uint32_t kernel_col_block_size,
+    uint8_t output_zero_point,
+    uint8_t output_min,
+    uint8_t output_max,
+    uint32_t flags,
+    const float* requantization_scales,
+    pytorch_qnnp_operator_t* fully_connected_out) {
+  pytorch_qnnp_operator_t fully_connected = NULL;
+  enum pytorch_qnnp_status status = pytorch_qnnp_status_uninitialized;
+
+  if (!pytorch_qnnp_params.initialized) {
+    pytorch_qnnp_log_error(
+        "pytorch_qnnp_create_fully_connected_sparse_dq_nc_q8 failed because QNNPACK is not properly initialized");
+    goto error;
+  }
+
+  status = pytorch_qnnp_status_unsupported_parameter;
+
+  for (int i = 0; i < output_channels; ++i) {
+    if (requantization_scales[i] <= 0.0f ||
+        !isnormal(requantization_scales[i])) {
+      pytorch_qnnp_log_error(
+          "failed to create fully connected operator with %.7g requantization scale: scale must be finite and positive",
+          requantization_scales[i]);
+      goto error;
+    }
+  }
+
+  status = pytorch_qnnp_status_out_of_memory;
+
+  fully_connected = calloc(1, sizeof(struct pytorch_qnnp_operator));
+  if (fully_connected == NULL) {
+    pytorch_qnnp_log_error(
+        "failed to allocate %zu bytes for pytorch_qnnp_operator structure",
+        sizeof(struct pytorch_qnnp_operator));
+    goto error;
+  }
+
+  fully_connected->sparse_matrix.col_indices = kernel_col_indices;
+  fully_connected->sparse_matrix.row_values = kernel_row_values;
+  fully_connected->sparse_matrix.values = kernel_values;
+  fully_connected->sparse_matrix.col_block_size = kernel_col_block_size;
+
+  fully_connected->groups = 1;
+  fully_connected->group_input_channels = input_channels;
+  fully_connected->group_output_channels = output_channels;
+
+  fully_connected->kernel_zero_point = kernel_zero_points[0];
+
+  fully_connected->dynamic_conv_quantization_params.input_zero_point =
+    input_zero_point;
+  fully_connected->dynamic_conv_quantization_params.kernel_zero_points =
+    kernel_zero_points;
+  fully_connected->dynamic_conv_quantization_params.multipliers =
+    requantization_scales;
+
+  fully_connected->ukernel_type = pytorch_qnnp_ukernel_type_gemm_sparse_dq;
+  fully_connected->format = pytorch_qnnp_format_quint8;
+
+  *fully_connected_out = fully_connected;
+  return pytorch_qnnp_status_success;
+
+error:
+  pytorch_qnnp_delete_operator(fully_connected);
+  return status;
+}
+
+enum pytorch_qnnp_status pytorch_qnnp_setup_fully_connected_sparse_dq_nc_q8(
+    pytorch_qnnp_operator_t fully_connected,
+    size_t batch_size,
+    const uint8_t* input,
+    size_t input_stride,
+    const float* bias,
+    float* output,
+    size_t output_stride) {
+  if (!pytorch_qnnp_params.initialized) {
+    pytorch_qnnp_log_error(
+        "pytorch_qnnp_setup_fully_connected_nc_q8 failed because QNNPACK is not properly initialized");
+    return pytorch_qnnp_status_uninitialized;
+  }
+
+  if (batch_size == 0) {
+    fully_connected->batch_size = 0;
+    return pytorch_qnnp_status_success;
+  }
+
+  fully_connected->batch_size = 1;
+  fully_connected->input_height = batch_size;
+  fully_connected->input_width = 1;
+  fully_connected->input = input;
+  fully_connected->input_pixel_stride = input_stride;
+
+  fully_connected->bias = bias;
+
+  fully_connected->output_height = batch_size;
+  fully_connected->output_width = 1;
+  fully_connected->output = output;
+  fully_connected->output_pixel_stride = output_stride;
+
+  return pytorch_qnnp_status_success;
+}
---
+++ aten/src/ATen/native/quantized/cpu/qnnpack/test/fully-connected-sparse.cc
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "fully-connected-sparse-operator-tester.h"
+
+TEST(FULLY_CONNECTED_SPARSE_OP, integration_test_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(4)
+      .inputChannels(4)
+      .outputChannels(4)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, zero_batch_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(0)
+      .inputChannels(2)
+      .outputChannels(2)
+      .iterations(1)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, unit_batch_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(1)
+      .inputChannels(23)
+      .outputChannels(19)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, unit_batch_with_qmin_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(1)
+      .inputChannels(23)
+      .outputChannels(19)
+      .qmin(128)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, unit_batch_with_qmax_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(1)
+      .inputChannels(23)
+      .outputChannels(19)
+      .qmax(128)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, unit_batch_with_input_stride_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(1)
+      .inputChannels(23)
+      .inputStride(28)
+      .outputChannels(19)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, unit_batch_with_output_stride_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(1)
+      .inputChannels(23)
+      .outputChannels(19)
+      .outputStride(29)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, small_batch_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(12)
+      .inputChannels(23)
+      .outputChannels(19)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, small_batch_with_qmin_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(12)
+      .inputChannels(23)
+      .outputChannels(19)
+      .qmin(128)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
+
+TEST(FULLY_CONNECTED_SPARSE_OP, small_batch_with_qmax_dynamic) {
+  FullyConnectedSparseOperatorTester()
+      .batchSize(12)
+      .inputChannels(23)
+      .outputChannels(19)
+      .qmax(128)
+      .iterations(3)
+      .testQ8(FullyConnectedSparseOperatorTester::Mode::Dynamic);
+}
