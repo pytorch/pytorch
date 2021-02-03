7c3881900b012a6257e28d5c8c40a9e73e7ede26
Chen Lai <chenlai@fb.com>
2021-02-03 01:28:12 -0800

    Update on "[PyTorch] update CMake to build libtorch lite"
    [ghstack-poisoned]

--- test/cpp/demos/main.cpp
+++ test/cpp/demos/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
-#include "torch/csrc/jit/mobile/import.h"
+#include <torch/csrc/jit/mobile/import.h>
+#include <ATen/core/ivalue.h>
+#include <torch/csrc/autograd/generated/variable_factories.h>
 
 int main(int argc, char* argv[]) {
     std::cout << "test start" << std::endl;
     std::string file_path = argv[1];
+    std::vector<c10::IValue> inputs;
+
     torch::jit::mobile::Module bc = torch::jit::_load_for_mobile(file_path);
+    // inputs.push_back(c10::IValue(1));
+    inputs.push_back(torch::ones({1, 1, 4, 4}));
+
+    at::Tensor output = bc.forward(inputs).toTensor();
+    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
     return 0;
 }
