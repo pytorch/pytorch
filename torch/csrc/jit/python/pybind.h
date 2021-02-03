31815286f917f03f606b5e0713081d489bd996bb
Thomas Viehmann <tv.code@beamnet.de>
2021-02-03 22:26:25 +0100

    clang format

--- torch/csrc/jit/python/pybind.h
+++ torch/csrc/jit/python/pybind.h
@@ -52,13 +52,14 @@ class unwrapping_shared_ptr {
   }
   // we need to disable the overloaded & for PyBind11 < 2.3 due.
   // see https://github.com/pybind/pybind11/pull/1435
-#if (PYBIND11_VERSION_MAJOR > 2) || ((PYBIND11_VERSION_MAJOR == 2) && (PYBIND11_VERSION_MINOR >= 3))
+#if (PYBIND11_VERSION_MAJOR > 2) || \
+    ((PYBIND11_VERSION_MAJOR == 2) && (PYBIND11_VERSION_MINOR >= 3))
   T** operator&() {
     if (!impl->elem) {
       throw std::logic_error("has been invalidated");
     }
     return &(impl->elem);
-    }
+  }
 #endif
 };
 
