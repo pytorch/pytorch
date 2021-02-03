bea0519b0bbe0006428cd2fa3a729746651c93be
Erjia Guan <erjia@fb.com>
2021-02-03 06:51:44 -0800

    [WIP][DataLoader] Implement BucketBatchIterableDataset (#51126)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51126
    
    BucketBatch:
    Get a chunk of data as a bucket, and sort the bucket by the specified key, then batching.
    If sort key is not specified, directly use batchIterableDS..
    
    1. Implement BucketBatch for bucket sampler
    2. Improve BatchDS tests
    
    Test Plan: Imported from OSS
    
    Reviewed By: H-Huang
    
    Differential Revision: D26209890
    
    Pulled By: ejguan
    
    fbshipit-source-id: 8519e2e49da158b3fe32913c8f3cadfa6f3ff1fc

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,4 +1,5 @@
 import pickle
+import random
 import tempfile
 import warnings
 
@@ -6,7 +7,7 @@ import torch
 from torch.testing._internal.common_utils import (TestCase, run_tests)
 from torch.utils.data import IterableDataset, RandomSampler
 from torch.utils.data.datasets import \
-    (CallableIterableDataset, CollateIterableDataset, BatchIterableDataset,
+    (CallableIterableDataset, CollateIterableDataset, BatchIterableDataset, BucketBatchIterableDataset,
      ListDirFilesIterableDataset, LoadFilesFromDiskIterableDataset, SamplerIterableDataset)
 from typing import List, Tuple, Dict, Any, Type
 
@@ -146,46 +147,81 @@ class TestFunctionalIterableDataset(TestCase):
             self.assertEqual(x, torch.tensor(y))
 
     def test_batch_dataset(self):
-        arrs = range(10)
+        arrs = list(range(10))
         ds = IterDatasetWithLen(arrs)
         with self.assertRaises(AssertionError):
             batch_ds0 = BatchIterableDataset(ds, batch_size=0)
 
         # Default not drop the last batch
-        batch_ds1 = BatchIterableDataset(ds, batch_size=3)
+        bs = 3
+        batch_ds1 = BatchIterableDataset(ds, batch_size=bs)
         self.assertEqual(len(batch_ds1), 4)
-        batch_iter = iter(batch_ds1)
-        value = 0
-        for i in range(len(batch_ds1)):
-            batch = next(batch_iter)
-            if i == 3:
-                self.assertEqual(len(batch), 1)
-                self.assertEqual(batch, [9])
-            else:
-                self.assertEqual(len(batch), 3)
-                for x in batch:
-                    self.assertEqual(x, value)
-                    value += 1
+        for i, batch in enumerate(batch_ds1):
+            self.assertEqual(len(batch), 1 if i == 3 else bs)
+            self.assertEqual(batch, arrs[i * bs: i * bs + len(batch)])
 
         # Drop the last batch
-        batch_ds2 = BatchIterableDataset(ds, batch_size=3, drop_last=True)
-        self.assertEqual(len(batch_ds2), 3)
-        value = 0
-        for batch in batch_ds2:
-            self.assertEqual(len(batch), 3)
-            for x in batch:
-                self.assertEqual(x, value)
-                value += 1
+        bs = 4
+        batch_ds2 = BatchIterableDataset(ds, batch_size=bs, drop_last=True)
+        self.assertEqual(len(batch_ds2), 2)
+        for i, batch in enumerate(batch_ds2):
+            self.assertEqual(len(batch), bs)
+            self.assertEqual(batch, arrs[i * bs: i * bs + len(batch)])
 
-        batch_ds3 = BatchIterableDataset(ds, batch_size=2)
-        self.assertEqual(len(batch_ds3), 5)
-        batch_ds4 = BatchIterableDataset(ds, batch_size=2, drop_last=True)
-        self.assertEqual(len(batch_ds4), 5)
-
-        ds_nolen = IterDatasetWithoutLen(arrs)
-        batch_ds_nolen = BatchIterableDataset(ds_nolen, batch_size=5)
+        ds_nl = IterDatasetWithoutLen(range(10))
+        batch_ds_nl = BatchIterableDataset(ds_nl, batch_size=2)
         with self.assertRaises(NotImplementedError):
-            len(batch_ds_nolen)
+            len(batch_ds_nl)
+
+    def test_bucket_batch_dataset(self):
+        ds = IterDatasetWithLen(range(20))
+        with self.assertRaises(AssertionError):
+            BucketBatchIterableDataset(ds, batch_size=0)
+
+        ds_nl = IterDatasetWithoutLen(range(20))
+        bucket_ds_nl = BucketBatchIterableDataset(ds_nl, batch_size=7)
+        with self.assertRaises(NotImplementedError):
+            len(bucket_ds_nl)
+
+        # Test Bucket Batch without sort_key
+        def _helper(**kwargs):
+            arrs = list(range(100))
+            random.shuffle(arrs)
+            ds = IterDatasetWithLen(arrs)
+            bucket_ds = BucketBatchIterableDataset(ds, **kwargs)
+            if kwargs["sort_key"] is None:
+                # BatchDataset as reference
+                ref_ds = BatchIterableDataset(ds, batch_size=kwargs['batch_size'], drop_last=kwargs['drop_last'])
+                for batch, rbatch in zip(bucket_ds, ref_ds):
+                    self.assertEqual(batch, rbatch)
+            else:
+                bucket_size = bucket_ds.bucket_size
+                bucket_num = (len(ds) - 1) // bucket_size + 1
+                it = iter(bucket_ds)
+                for i in range(bucket_num):
+                    ref = sorted(arrs[i * bucket_size: (i + 1) * bucket_size])
+                    bucket: List = []
+                    while len(bucket) < len(ref):
+                        try:
+                            batch = next(it)
+                            bucket += batch
+                        # If drop last, stop in advance
+                        except StopIteration:
+                            break
+                    if len(bucket) != len(ref):
+                        ref = ref[:len(bucket)]
+                    # Sorted bucket
+                    self.assertEqual(bucket, ref)
+
+        _helper(batch_size=7, drop_last=False, sort_key=None)
+        _helper(batch_size=7, drop_last=True, bucket_size_mul=5, sort_key=None)
+
+        # Test Bucket Batch with sort_key
+        def _sort_fn(data):
+            return data
+
+        _helper(batch_size=7, drop_last=False, bucket_size_mul=5, sort_key=_sort_fn)
+        _helper(batch_size=7, drop_last=True, bucket_size_mul=5, sort_key=_sort_fn)
 
     def test_sampler_dataset(self):
         arrs = range(10)
