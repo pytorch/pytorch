92b2759681146036dd6ea5047438823ee47d1228
Erjia Guan <erjia@fb.com>
2021-02-03 06:51:44 -0800

    [DataLoader] Rename Functional DataSet to DataPipe (#51488)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/51488
    
    Test Plan: Imported from OSS
    
    Reviewed By: H-Huang
    
    Differential Revision: D26209888
    
    Pulled By: ejguan
    
    fbshipit-source-id: cb8bc852b1e4d72be81e0297308a43954cd95332

--- torch/utils/data/datapipes/iter/batch.py
+++ torch/utils/data/datapipes/iter/batch.py
@@ -1,42 +1,42 @@
 import warnings
-from torch.utils.data import IterableDataset
+from torch.utils.data import IterDataPipe
 from typing import TypeVar, Optional, Iterator, List, Sized, Callable
 
 T_co = TypeVar('T_co', covariant=True)
 
 
-class BatchIterableDataset(IterableDataset[List[T_co]]):
-    r""" :class:`BatchIterableDataset`.
+class BatchIterDataPipe(IterDataPipe[List[T_co]]):
+    r""" :class:`BatchIterDataPipe`.
 
-    IterableDataset to create mini-batches of data. An outer dimension will be added as
+    Iterable DataPipe to create mini-batches of data. An outer dimension will be added as
     `batch_size` if `drop_last` is set to `True`, or `length % batch_size` for the
     last batch if `drop_last` is set to `False`.
     args:
-        dataset: IterableDataset being batched
+        datapipe: Iterable DataPipe being batched
         batch_size: The size of each batch
         drop_last: Option to drop the last batch if it's not full
     """
-    dataset: IterableDataset[T_co]
+    datapipe: IterDataPipe[T_co]
     batch_size: int
     drop_last: bool
     length: Optional[int]
 
     def __init__(self,
-                 dataset: IterableDataset[T_co],
+                 datapipe: IterDataPipe[T_co],
                  *,
                  batch_size: int,
                  drop_last: bool = False,
                  ) -> None:
         assert batch_size > 0, "Batch size is required to be larger than 0!"
-        super(BatchIterableDataset, self).__init__()
-        self.dataset = dataset
+        super().__init__()
+        self.datapipe = datapipe
         self.batch_size = batch_size
         self.drop_last = drop_last
         self.length = None
 
     def __iter__(self) -> Iterator[List[T_co]]:
         batch: List[T_co] = []
-        for x in self.dataset:
+        for x in self.datapipe:
             batch.append(x)
             if len(batch) == self.batch_size:
                 yield batch
@@ -49,29 +49,29 @@ class BatchIterableDataset(IterableDataset[List[T_co]]):
     def __len__(self) -> int:
         if self.length is not None:
             return self.length
-        if isinstance(self.dataset, Sized) and len(self.dataset) >= 0:
+        if isinstance(self.datapipe, Sized) and len(self.datapipe) >= 0:
             if self.drop_last:
-                self.length = len(self.dataset) // self.batch_size
+                self.length = len(self.datapipe) // self.batch_size
             else:
-                self.length = (len(self.dataset) + self.batch_size - 1) // self.batch_size
+                self.length = (len(self.datapipe) + self.batch_size - 1) // self.batch_size
             return self.length
         raise NotImplementedError
 
 
-class BucketBatchIterableDataset(IterableDataset[List[T_co]]):
-    r""" :class:`BucketBatchIterableDataset`.
+class BucketBatchIterDataPipe(IterDataPipe[List[T_co]]):
+    r""" :class:`BucketBatchIterDataPipe`.
 
-    IterableDataset to create mini-batches of data from sorted bucket. An outer
+    Iterable DataPipe to create mini-batches of data from sorted bucket. An outer
     dimension will be added as `batch_size` if `drop_last` is set to `True`,
     or `length % batch_size` for the last batch if `drop_last` is set to `False`.
         args:
-        dataset: IterableDataset being batched
+        datapipe: Iterable DataPipe being batched
         batch_size: The size of each batch
         drop_last: Option to drop the last batch if it's not full
         bucket_size_mul: The multiplier to specify the size of bucket
         sort_key: Callable to specify the comparison key for sorting within bucket
     """
-    dataset: IterableDataset[T_co]
+    datapipe: IterDataPipe[T_co]
     batch_size: int
     drop_last: bool
     bucket_size_mul: int
@@ -79,7 +79,7 @@ class BucketBatchIterableDataset(IterableDataset[List[T_co]]):
     length: Optional[int]
 
     def __init__(self,
-                 dataset: IterableDataset[T_co],
+                 datapipe: IterDataPipe[T_co],
                  *,
                  batch_size: int,
                  drop_last: bool = False,
@@ -87,8 +87,8 @@ class BucketBatchIterableDataset(IterableDataset[List[T_co]]):
                  sort_key: Optional[Callable] = None,
                  ) -> None:
         assert batch_size > 0, "Batch size is required to be larger than 0!"
-        super(BucketBatchIterableDataset, self).__init__()
-        self.dataset = dataset
+        super().__init__()
+        self.datapipe = datapipe
         self.batch_size = batch_size
         self.drop_last = drop_last
         self.bucket_size = batch_size * bucket_size_mul
@@ -96,13 +96,13 @@ class BucketBatchIterableDataset(IterableDataset[List[T_co]]):
         if sort_key is not None and sort_key.__name__ == '<lambda>':
             warnings.warn("Lambda function is not supported for pickle, "
                           "please use regular python function instead.")
-        self.bucket_ds = BatchIterableDataset(dataset, batch_size=self.bucket_size, drop_last=False)
+        self.bucket_ds = BatchIterDataPipe(datapipe, batch_size=self.bucket_size, drop_last=False)
         self.length = None
 
     def __iter__(self) -> Iterator[List[T_co]]:
         # Bucket without sorting remains same order, directly returns BatchDataset
         if self.sort_key is None:
-            yield from BatchIterableDataset(self.dataset, batch_size=self.batch_size, drop_last=self.drop_last)
+            yield from BatchIterDataPipe(self.datapipe, batch_size=self.batch_size, drop_last=self.drop_last)
         else:
             bucket: List[T_co]
             batch: List[T_co] = []
@@ -117,10 +117,10 @@ class BucketBatchIterableDataset(IterableDataset[List[T_co]]):
     def __len__(self) -> int:
         if self.length is not None:
             return self.length
-        if isinstance(self.dataset, Sized) and len(self.dataset) >= 0:
+        if isinstance(self.datapipe, Sized) and len(self.datapipe) >= 0:
             if self.drop_last:
-                self.length = len(self.dataset) // self.batch_size
+                self.length = len(self.datapipe) // self.batch_size
             else:
-                self.length = (len(self.dataset) + self.batch_size - 1) // self.batch_size
+                self.length = (len(self.datapipe) + self.batch_size - 1) // self.batch_size
             return self.length
         raise NotImplementedError
