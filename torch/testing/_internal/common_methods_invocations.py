8e32eed36c8440e5e140ae793beec794e503b5b0
vfdev-5 <vfdev-5@gmail.com>
2021-02-03 05:16:32 -0600

    Fixes according to the review - added code explanation - more inputs for clamp

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1117,13 +1117,17 @@ def sample_inputs_fliplr_flipud(op_info, device, dtype, requires_grad):
 def sample_inputs_clamp(op_info, device, dtype, requires_grad):
     tensors = (
         make_tensor((S, M, S), device, dtype, low=None, high=None, requires_grad=requires_grad),
-        make_tensor((S, 0, M), device, dtype, low=None, high=None, requires_grad=requires_grad)
+        make_tensor((S, 0, M), device, dtype, low=None, high=None, requires_grad=requires_grad),        
     )
     if dtype is torch.uint8:
         min_max_vals = ((2, 5), (3, 7))
     else:
         min_max_vals = ((0, 1), (-1, 1))
-    return [SampleInput(tensor, args=vals) for tensor, vals in product(tensors, min_max_vals)]
+    output = [SampleInput(tensor, args=vals) for tensor, vals in product(tensors, min_max_vals)]    
+    output += [SampleInput(tensors[0], args=(0.5, None)), SampleInput(tensors[0], args=(None, 0.5))]
+    empty_tensor = make_tensor((), device, dtype, low=None, high=None, requires_grad=requires_grad)
+    output += [SampleInput(empty_tensor, args=(0.0, 1.0)), ]
+    return output
 
 def sample_inputs_diag(op_info, device, dtype, requires_grad):
     vec_sample = SampleInput(make_tensor((M, ), device, dtype, low=None, high=None, requires_grad=requires_grad))
