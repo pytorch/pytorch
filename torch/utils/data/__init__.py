52de407b4b675d71174d24dd653f55b29d9c756f
Erjia Guan <erjia@fb.com>
2021-02-03 06:51:44 -0800

    [DataLoader] Rename Functional DataSet to DataPipe (#51488)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/51488
    
    Test Plan: Imported from OSS
    
    Reviewed By: H-Huang
    
    Differential Revision: D26209888
    
    Pulled By: ejguan
    
    fbshipit-source-id: cb8bc852b1e4d72be81e0297308a43954cd95332

--- torch/utils/data/__init__.py
+++ torch/utils/data/__init__.py
@@ -4,13 +4,10 @@ from .dataset import (Dataset, IterableDataset, TensorDataset, ConcatDataset, Ch
 from .dataset import IterableDataset as IterDataPipe
 from .distributed import DistributedSampler
 from .dataloader import DataLoader, _DatasetKind, get_worker_info
-from .datasets import (BatchIterableDataset, BucketBatchIterableDataset, CallableIterableDataset, CollateIterableDataset,
-                       SamplerIterableDataset)
 
 __all__ = ['Sampler', 'SequentialSampler', 'RandomSampler',
            'SubsetRandomSampler', 'WeightedRandomSampler', 'BatchSampler',
            'DistributedSampler', 'Dataset', 'IterableDataset', 'TensorDataset',
            'ConcatDataset', 'ChainDataset', 'BufferedShuffleDataset', 'Subset',
            'random_split', 'DataLoader', '_DatasetKind', 'get_worker_info',
-           'BatchIterableDataset', 'BucketBatchIterableDataset', 'CallableIterableDataset',
-           'CollateIterableDataset', 'SamplerIterableDataset', 'IterDataPipe']
+           'IterDataPipe']
--- torch/utils/data/datasets/__init__.py
+++ torch/utils/data/datasets/__init__.py
@@ -1,9 +1,4 @@
-from .batchdataset import BatchIterableDataset, BucketBatchIterableDataset
-from .callabledataset import CallableIterableDataset, CollateIterableDataset
-from .samplerdataset import SamplerIterableDataset
 from .listdirfilesdataset import ListDirFilesIterableDataset
 from .loadfilesfromdiskdataset import LoadFilesFromDiskIterableDataset
 
-__all__ = ['BatchIterableDataset', 'BucketBatchIterableDataset', 'CallableIterableDataset',
-           'CollateIterableDataset', 'ListDirFilesIterableDataset', 'LoadFilesFromDiskIterableDataset',
-           'SamplerIterableDataset']
+__all__ = ['ListDirFilesIterableDataset', 'LoadFilesFromDiskIterableDataset']
