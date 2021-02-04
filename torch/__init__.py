8f314fac6c08198e539962efcb78c9e24ad4bf96
Philip Meier <github.pmeier@posteo.de>
2021-02-04 09:35:22 +0100

    Revert "test dummy import [skip ci]"
    This reverts commit fbe8c3ace4c2accb8f58fdd1e2c6134e7d66ad32.

--- torch/__init__.py
+++ torch/__init__.py
@@ -543,8 +543,6 @@ if TYPE_CHECKING:
     # PR #43339 for details.
     from torch._C._VariableFunctions import *  # type: ignore
 
-from . import tensor
-
 for name in dir(_C._VariableFunctions):
     if name.startswith('__'):
         continue
