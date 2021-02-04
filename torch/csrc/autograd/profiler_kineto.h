03f33d58b7099805716b34da7844efa43beb7f92
Xu Zhao <xzhao9@fb.com>
2021-02-04 14:24:53 -0800

    Fix a bug in flops initialization.

--- torch/csrc/autograd/profiler_kineto.h
+++ torch/csrc/autograd/profiler_kineto.h
@@ -170,7 +170,7 @@ struct TORCH_API KinetoEvent {
   uint8_t activity_type_;
   c10::optional<std::vector<std::vector<int64_t>>> shapes_;
   c10::optional<std::vector<std::string>> stack_;
-  uint64_t flops_;
+  uint64_t flops_ = 0;
 
   std::string name_;
   uint64_t device_index_ = 0;
