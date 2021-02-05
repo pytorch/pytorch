9f1709a4e2b7299a2a1fd0b7d7bfc34b95d12846
Xu Zhao <xzhao9@fb.com>
2021-02-04 19:51:29 -0500

    Fix the full path in comment.

--- .github/scripts/build_publish_nightly_docker.sh
+++ .github/scripts/build_publish_nightly_docker.sh
@@ -7,7 +7,7 @@ PYTORCH_NIGHTLY_COMMIT=$(git log -1 --pretty=%B origin/nightly | head -1 | \
                              sed 's,.*(\([[:xdigit:]]*\)),\1,' | head -c 7)
 
 # Build PyTorch Nightly Docker
-# Full name: ghcr.io/pytorch-nightly/pytorch:${PYTORCH_NIGHTLY_COMMIT}
+# Full name: ghcr.io/pytorch/pytorch-nightly:${PYTORCH_NIGHTLY_COMMIT}
 make -f docker.Makefile \
      DOCKER_REGISTRY=ghcr.io \
      DOCKER_ORG=pytorch \
