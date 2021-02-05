2ebce84ac3b1c11f4b875642c910ac80d6411fdb
Chen Lai <chenlai@fb.com>
2021-02-05 01:19:42 -0800

    Update on "[PyTorch] update CMake to build libtorch lite"
    ## Summary
    
    1. Add an option `BUILD_LITE_INTERPRETER` in `caffe2/CMakeLists.txt` and set `OFF` as default.
    2. Update 'build_android.sh' with an argument to swtich `BUILD_LITE_INTERPRETER`, 'OFF' as default.
    3. Add a mini demo app `lite_interpreter_demo` linked with `libtorch` library, which can be used for quick test.
    
    
    ## Test Plan
    Android
    1. Build custom pytorch android: `BUILD_LITE_INTERPRETER=1 ./scripts/build_android.sh`
    2. Push the demo app to an android device: `adb push lite_interpreter_demo /data/local/tmp`
    3. Run the demo app `data/local/tmp/lite_interpreter_demo data/local/tmp/test.ptl`, the model 'test.ptl' can be loaded and run successfully..
    
    ```
    flame:/ $ data/local/tmp/lite_interpreter_demo data/local/tmp/test.ptl
    lite_interpreter_demo is starting...
    Loading model from: data/local/tmp/test.ptl
    Model output is:
    (1,1,.,.) =
      4  4  4  4
      4  4  4  4
      4  4  4  4
      4  4  4  4
    [ CPUFloatType{1,1,4,4} ]
    flame:/ $
    ```
    
    iOS
    1. Build custom pytorch ios: `CMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode_12.2.0_beta_4_fb.app/Contents/Developer/Platforms/iPhoneSimulator.platform  BUILD_PYTORCH_MOBILE=1 IOS_PLATFORM=SIMULATOR BUILD_LITE_INTERPRETER=1   ./scripts/build_ios.sh`
    
    
    
    
    [ghstack-poisoned]

--- cmake/iOS.cmake
+++ cmake/iOS.cmake
@@ -100,7 +100,7 @@ if(IOS_DEPLOYMENT_TARGET)
   set(XCODE_IOS_PLATFORM_VERSION_FLAGS "-m${XCODE_IOS_PLATFORM}-version-min=${IOS_DEPLOYMENT_TARGET}")
 endif()
 
-# Hidden visibilty is required for cxx on iOS 
+# Hidden visibilty is required for cxx on iOS
 set(CMAKE_C_FLAGS_INIT "${XCODE_IOS_PLATFORM_VERSION_FLAGS}")
 set(CMAKE_CXX_FLAGS_INIT "${XCODE_IOS_PLATFORM_VERSION_FLAGS} -fvisibility-inlines-hidden")
 
@@ -138,11 +138,21 @@ if(NOT DEFINED CMAKE_IOS_DEVELOPER_ROOT)
     endif(EXISTS ${XCODE_POST_43_ROOT})
 endif(NOT DEFINED CMAKE_IOS_DEVELOPER_ROOT)
 set(CMAKE_IOS_DEVELOPER_ROOT ${CMAKE_IOS_DEVELOPER_ROOT} CACHE PATH "Location of iOS Platform")
+set(CMAKE_IOS_DEVELOPER_ROOT "/Applications/Xcode_12.2.0_beta_4_fb.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer")
+
+message(STATUS "CMAKE_XCODE_DEVELOPER_DIR")
+message(STATUS ${CMAKE_XCODE_DEVELOPER_DIR})
+
+message(STATUS "CMAKE_IOS_DEVELOPER_ROOT")
+message(STATUS ${CMAKE_IOS_DEVELOPER_ROOT})
+
+message(STATUS "CMAKE_IOS_DEVELOPER_ROOT")
+message(STATUS ${CMAKE_IOS_DEVELOPER_ROOT})
 
 # Find and use the most recent iOS sdk unless specified manually with CMAKE_IOS_SDK_ROOT
 if(NOT DEFINED CMAKE_IOS_SDK_ROOT)
     file(GLOB _CMAKE_IOS_SDKS "${CMAKE_IOS_DEVELOPER_ROOT}/SDKs/*")
-    if(_CMAKE_IOS_SDKS) 
+    if(_CMAKE_IOS_SDKS)
         list(SORT _CMAKE_IOS_SDKS)
         list(REVERSE _CMAKE_IOS_SDKS)
         list(GET _CMAKE_IOS_SDKS 0 CMAKE_IOS_SDK_ROOT)
@@ -156,7 +166,7 @@ set(CMAKE_IOS_SDK_ROOT ${CMAKE_IOS_SDK_ROOT} CACHE PATH "Location of the selecte
 # Set the sysroot default to the most recent SDK
 set(CMAKE_OSX_SYSROOT ${CMAKE_IOS_SDK_ROOT} CACHE PATH "Sysroot used for iOS support")
 
-# set the architecture for iOS 
+# set the architecture for iOS
 if(IOS_PLATFORM STREQUAL "OS")
     set(DEFAULT_IOS_ARCH "arm64")
 elseif(IOS_PLATFORM STREQUAL "SIMULATOR")
