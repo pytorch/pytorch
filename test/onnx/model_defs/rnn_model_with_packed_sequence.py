c442881cbbf585f7af5682ea3a270c88b4375ca1
Jay Zhang <jiz@microsoft.com>
2021-02-06 17:05:31 +0800

    Update tests for test_gru_* at this moment.

--- test/onnx/model_defs/rnn_model_with_packed_sequence.py
+++ test/onnx/model_defs/rnn_model_with_packed_sequence.py
@@ -8,10 +8,17 @@ class RnnModelWithPackedSequence(nn.Module):
         self.model = model
         self.batch_first = batch_first
 
-    def forward(self, input, *args):
-        args, seq_lengths = args[:-1], args[-1]
+    # def forward(self, input, *args):
+    #     args, seq_lengths = args[:-1], args[-1]
+    #     input = rnn_utils.pack_padded_sequence(input, seq_lengths, self.batch_first)
+    #     rets = self.model(input, *args)
+    #     ret, rets = rets[0], rets[1:]
+    #     ret, _ = rnn_utils.pad_packed_sequence(ret, self.batch_first)
+    #     return tuple([ret] + list(rets))
+
+    def forward(self, input, seq_lengths):
         input = rnn_utils.pack_padded_sequence(input, seq_lengths, self.batch_first)
-        rets = self.model(input, *args)
+        rets = self.model(input)
         ret, rets = rets[0], rets[1:]
         ret, _ = rnn_utils.pad_packed_sequence(ret, self.batch_first)
-        return tuple([ret] + list(rets))
+        return tuple([ret] + list(rets))    
