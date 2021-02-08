c083a5eec0457ef5ce33bed62570b567d268b8fc
ErjiaGuan <erjia@fb.com>
2021-02-08 11:14:52 -0500

    [DataLoader] Rename files of functional datapipes
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/__init__.py
+++ torch/utils/data/datapipes/iter/__init__.py
@@ -4,13 +4,14 @@ from torch.utils.data.datapipes.iter.readfilesfromtar import ReadFilesFromTarIte
 from torch.utils.data.datapipes.iter.readfilesfromzip import ReadFilesFromZipIterDataPipe as ReadFilesFromZip
 
 # Functional DataPipe
-from torch.utils.data.datapipes.iter.batch import BatchIterDataPipe as Batch, BucketBatchIterDataPipe as BucketBatch
 from torch.utils.data.datapipes.iter.callable import \
     (MapIterDataPipe as Map, CollateIterDataPipe as Collate)
+from torch.utils.data.datapipes.iter.combinatorics import \
+    (SamplerIterDataPipe as Sampler)
+from torch.utils.data.datapipes.iter.grouping import \
+    (BatchIterDataPipe as Batch, BucketBatchIterDataPipe as BucketBatch)
 from torch.utils.data.datapipes.iter.selecting import \
     (FilterIterDataPipe as Filter)
-from torch.utils.data.datapipes.iter.sampler import \
-    (SamplerIterDataPipe as Sampler)
 
 
 __all__ = ['ListDirFiles', 'LoadFilesFromDisk', 'ReadFilesFormTar', 'ReadFilesFromZip'
