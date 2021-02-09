10da5a19795c629d702a9a1312031f455c9bda3f
Xu Zhao <xzhao9@fb.com>
2021-02-09 15:13:39 -0500

    Use CUDA 11.2 for nightly docker build.

--- .github/scripts/build_publish_nightly_docker.sh
+++ .github/scripts/build_publish_nightly_docker.sh
@@ -9,6 +9,7 @@ PYTORCH_DOCKER_TAG=$(git describe --tags --always)-devel
 make -f docker.Makefile \
      DOCKER_REGISTRY=ghcr.io \
      DOCKER_ORG=pytorch \
+     CUDA_VERSION=11.2.0 \
      DOCKER_IMAGE=pytorch-nightly \
      DOCKER_TAG=${PYTORCH_DOCKER_TAG} \
      INSTALL_CHANNEL=pytorch-nightly BUILD_TYPE=official devel-image
