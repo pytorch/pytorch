cc2655b75e4c574a86584cc92fd17627d2ec46eb
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2021-02-09 03:28:04 -0800

    move RoutedDecoder Dataset to DataPipe (#51704)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/51704
    
    Test Plan: Imported from OSS
    
    Reviewed By: ejguan
    
    Differential Revision: D26245910
    
    Pulled By: glaringlee
    
    fbshipit-source-id: 91e3c9f8a6c1209c1a1a752ba29a80dbd9bf4119

--- torch/utils/data/datapipes/iter/__init__.py
+++ torch/utils/data/datapipes/iter/__init__.py
@@ -2,11 +2,12 @@ from torch.utils.data.datapipes.iter.listdirfiles import ListDirFilesIterDataPip
 from torch.utils.data.datapipes.iter.loadfilesfromdisk import LoadFilesFromDiskIterDataPipe as LoadFilesFromDisk
 from torch.utils.data.datapipes.iter.readfilesfromtar import ReadFilesFromTarIterDataPipe as ReadFilesFromTar
 from torch.utils.data.datapipes.iter.readfilesfromzip import ReadFilesFromZipIterDataPipe as ReadFilesFromZip
+from torch.utils.data.datapipes.iter.routeddecoder import RoutedDecoderIterDataPipe as RoutedDecoder
 
 # Functional DataPipe
 from torch.utils.data.datapipes.iter.batch import BatchIterDataPipe as Batch, BucketBatchIterDataPipe as BucketBatch
 from torch.utils.data.datapipes.iter.callable import CallableIterDataPipe as Callable, CollateIterDataPipe as Collate
 from torch.utils.data.datapipes.iter.sampler import SamplerIterDataPipe as Sampler
 
-__all__ = ['ListDirFiles', 'LoadFilesFromDisk', 'ReadFilesFormTar', 'ReadFilesFromZip'
+__all__ = ['ListDirFiles', 'LoadFilesFromDisk', 'ReadFilesFormTar', 'ReadFilesFromZip', 'RoutedDecoder',
            'Batch', 'BucketBatch', 'Callable', 'Collate', 'Sampler']
