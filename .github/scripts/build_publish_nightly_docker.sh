73a6bac9f4b71df854b14a0a53ec1f015cc2c357
Xu Zhao <xzhao9@fb.com>
2021-02-09 17:58:21 -0500

    Adds CUDA_VERSION to Dockerfile and specify conda-forge as CUDA package source.

--- .github/scripts/build_publish_nightly_docker.sh
+++ .github/scripts/build_publish_nightly_docker.sh
@@ -10,6 +10,7 @@ make -f docker.Makefile \
      DOCKER_REGISTRY=ghcr.io \
      DOCKER_ORG=pytorch \
      CUDA_VERSION=11.2.0 \
+     CUDA_CHANNEL=conda-forge \
      DOCKER_IMAGE=pytorch-nightly \
      DOCKER_TAG=${PYTORCH_DOCKER_TAG} \
      INSTALL_CHANNEL=pytorch-nightly BUILD_TYPE=official devel-image
