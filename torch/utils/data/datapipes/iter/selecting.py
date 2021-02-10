104371e1dcb3c5e4b7f0f08c93612a897543a0ca
Erjia Guan <erjia@fb.com>
2021-02-09 16:59:23 -0800

    [DataLoader] Implement FilterIterDataPipe (#51783)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/51783
    
    Test Plan: Imported from OSS
    
    Reviewed By: glaringlee
    
    Differential Revision: D26277688
    
    Pulled By: ejguan
    
    fbshipit-source-id: 25ed7da9da88c030b29627142c2f04fed26cdcda

---
+++ torch/utils/data/datapipes/iter/selecting.py
@@ -0,0 +1,36 @@
+from torch.utils.data import IterDataPipe
+from typing import Callable, TypeVar, Iterator
+
+from .callable import CallableIterDataPipe
+
+T_co = TypeVar('T_co', covariant=True)
+
+
+class FilterIterDataPipe(CallableIterDataPipe[T_co]):
+    r""" :class:`FilterIterDataPipe`.
+
+    Iterable DataPipe to filter elements from datapipe according to filter_fn.
+    args:
+        datapipe: Iterable DataPipe being filterd
+        filter_fn: Customized function mapping an element to a boolean.
+    """
+    def __init__(self,
+                 datapipe: IterDataPipe[T_co],
+                 *args,
+                 filter_fn: Callable[..., bool],
+                 **kwargs,
+                 ) -> None:
+        super().__init__(datapipe, *args, fn=filter_fn, **kwargs)
+
+    def __iter__(self) -> Iterator[T_co]:
+        res: bool
+        for data in self.datapipe:
+            res = self.fn(data, *self.args, **self.kwargs)
+            if not isinstance(res, bool):
+                raise ValueError("Boolean output is required for "
+                                 "`filter_fn` of FilterIterDataPipe")
+            if res:
+                yield data
+
+    def __len__(self):
+        raise(NotImplementedError)
