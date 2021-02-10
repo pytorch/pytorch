2314de3f035b301e94e1954c9a9b07068e252390
Erjia Guan <erjia@fb.com>
2021-02-09 16:59:23 -0800

    [DataLoader] Rename Callable to Map IterDataPipe (#51879)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/51879
    
    Test Plan: Imported from OSS
    
    Reviewed By: VitalyFedyunin
    
    Differential Revision: D26314775
    
    Pulled By: ejguan
    
    fbshipit-source-id: ee77909eae97092155ed6a6c794540e68a04d754

--- torch/utils/data/datapipes/iter/selecting.py
+++ torch/utils/data/datapipes/iter/selecting.py
@@ -1,26 +1,29 @@
 from torch.utils.data import IterDataPipe
-from typing import Callable, TypeVar, Iterator
+from typing import Callable, TypeVar, Iterator, Optional, Tuple, Dict
 
-from .callable import CallableIterDataPipe
+from .callable import MapIterDataPipe
 
 T_co = TypeVar('T_co', covariant=True)
 
 
-class FilterIterDataPipe(CallableIterDataPipe[T_co]):
+class FilterIterDataPipe(MapIterDataPipe[T_co]):
     r""" :class:`FilterIterDataPipe`.
 
     Iterable DataPipe to filter elements from datapipe according to filter_fn.
     args:
         datapipe: Iterable DataPipe being filterd
         filter_fn: Customized function mapping an element to a boolean.
+        fn_args: Positional arguments for `filter_fn`
+        fn_kwargs: Keyword arguments for `filter_fn`
     """
     def __init__(self,
                  datapipe: IterDataPipe[T_co],
-                 *args,
+                 *,
                  filter_fn: Callable[..., bool],
-                 **kwargs,
+                 fn_args: Optional[Tuple] = None,
+                 fn_kwargs: Optional[Dict] = None,
                  ) -> None:
-        super().__init__(datapipe, *args, fn=filter_fn, **kwargs)
+        super().__init__(datapipe, fn=filter_fn, fn_args=fn_args, fn_kwargs=fn_kwargs)
 
     def __iter__(self) -> Iterator[T_co]:
         res: bool
