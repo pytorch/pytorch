e3eca4cf79b1b8bd7d5bfbc6cb99ce3c790c4c88
jiej <jiej@nvidia.com>
2021-02-11 03:39:23 -0800

    flake8

--- torch/jit/_fuser.py
+++ torch/jit/_fuser.py
@@ -76,23 +76,22 @@ def _graph_for(self, *args, **kwargs):
     dbs = self.get_debug_state()
     eps = list(dbs.execution_plans.values())
     if len(eps) == 1:
-      graph = eps[0].graph.copy()
+        graph = eps[0].graph.copy()
 
-      # graph_executor_states for differentiable node
-      fw_states = eps[0].code.differentiable_op_executor_states()
-      diff_nodes = []
-      for n in graph.nodes():
-          _get_differentiable_graph_node(n, diff_nodes)
+        # graph_executor_states for differentiable node
+        fw_states = eps[0].code.differentiable_op_executor_states()
+        diff_nodes = []
+        for n in graph.nodes():
+            _get_differentiable_graph_node(n, diff_nodes)
 
-      if len(fw_states) == len(diff_nodes):
-          # swap each differentiable graph with optimized graph in their execution plan
-          for n, state in zip(diff_nodes, fw_states):
-              #ep = list(fw_states[0].execution_plans.values())[0]
-              fw_execution_plans = list(state.execution_plans.values())
-              if len(fw_execution_plans) == 1:
-                  n.g_('Subgraph', fw_execution_plans[0].graph)
+        if len(fw_states) == len(diff_nodes):
+            # swap each differentiable graph with optimized graph in their execution plan
+            for n, state in zip(diff_nodes, fw_states):
+                fw_execution_plans = list(state.execution_plans.values())
+                if len(fw_execution_plans) == 1:
+                    n.g_('Subgraph', fw_execution_plans[0].graph)
 
-      return graph
+        return graph
 
     # fallback approach, we just ran the graph and return the recorded optimized
     # graph
