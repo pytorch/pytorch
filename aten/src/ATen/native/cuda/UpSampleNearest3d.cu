2ffc91fac14c15b446d312bf9d71b22c8b4d8969
Brian Hirsh <hirsheybar@fb.com>
2021-02-11 15:03:37 -0800

    Update on "port upsample_nearest3d and upsample_trilinear3d to structured"
    Porting `upsample_nearest3d` and `upsample_trilinear3d` to be structured kernels.
    
    One annoying thing: since the `vec` overload of `upsample_nearest3d` has a `QuantizedCPU` kernel, we can't make it a `DefaultBackend` op without first updating the structured kernel codegen to handle `QuantizedCPU`. I still ended up updating that overload to redispatch and hit the structured version, since that still gets rid of a lot of code duplication (the shape checking), just not all of it.
    
    A quick look showed 84 ops that have `QuantizedCPU` kernels. So it probably isn't a blocker to push off trying to get them to work with structured kernels.
    
    A quick thought on updating structured kernels to deal with QuantizedCPU (and other keys). It's not as simple as adding another path to create the `out` Quantized tensor, because the arguments used to create the tensor are different than CPU/CUDA (there are extra quantized fields pulled off the tensor). Two quick options that come to mind:
    - Update the signature of `MetaBase:set_output()` and `{op_name}:meta()` to be generic enough to handle all the cases: just take in a tensor, and leave it up to the op writers to pull the right fields off the tensor to generate the output. They would also need to plumb that tensor into `set_output`. **Downsides:** It shifts the onus of more logic from the codegen to kernel writers, since the interface is more generic. It would also require fixing up the existing meta kernels, so we'd have to do it sooner rather than later. **Upside:** We could probably maintain a single meta function per op, instead of making kernel writers implement a separate meta function for QuantizedCPU (and potential other keys later)
    - Make an alternate base class `QuantizedMetaBase:set_output()` that has the corresponding signature required to set up out quantized tensors. And alternate classes `{op_name}_quantized:meta()` for each op with the correct `meta()` signature that all ops with a quantized kernel will need to implement. **Downsides:** Mildly more confusing class hierarchy, might need to expand it further for other keys. Some ops will need to implement two meta functions, although that's not really a huge deal since they can factor out the shared logic. **Upside:** Won't require a refactor of existing structured kernels.
    
    I kind of like option 2 more, but I think they're both viable options. It also wouldn't require implementing immediately.
    
    Also, a quick "gotcha" I hit when migrating to structured: If you forget to add `structured: True` in the right spot in native_functions.yaml, you get compiler errors about a missing class. I don't think it's a huge deal, still better than linker errors anyway. 
    
    
    Differential Revision: [D26373027](https://our.internmc.facebook.com/intern/diff/D26373027)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/UpSampleNearest3d.cu
+++ aten/src/ATen/native/cuda/UpSampleNearest3d.cu
@@ -301,6 +301,7 @@ Tensor upsample_nearest3d_backward_cuda(
   auto scale_d = get_scale_value(scale_factors, 0);
   auto scale_h = get_scale_value(scale_factors, 1);
   auto scale_w = get_scale_value(scale_factors, 2);
+  auto grad_input = at::empty_like(grad_output, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
   return at::upsample_nearest3d_backward_out_cuda_template(
       grad_input, grad_output, osize, input_size, scale_d, scale_h, scale_w);
   return grad_input;
