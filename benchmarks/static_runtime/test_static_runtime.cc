1dcabaaeda17676934730aa51aa039c3e1ba7d13
Hao Lu <hlu@fb.com>
2021-02-12 01:07:49 -0800

    [StaticRuntime] Clean up output references and remove dead code (#51991)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51991
    
    - Clean up references of outputs, including Tuples/Lists, by using move semantics
    - Clean up references of elements in output Tuples/Lists by adding them to `unmanaged_values_` in MemoryPlanner. Check for corner case of Tuple/List element being inputs.
    - Modify unit tests to check for use_counts of outputs
    - Clean up dead code. A bit overlap with D25592967, but shouldn't be a problem.
    
    This diff does not try to fix the alias problem with the MemoryPlanner.
    
    (Note: this ignores all push blocking failures!)
    
    Test Plan:
    ```
    buck test //caffe2/benchmarks/static_runtime:static_runtime_cpptest
    buck test mode/opt-clang caffe2/caffe2/fb/predictor:ptvsc2_predictor_bench_test
    ```
    
    Reviewed By: bwasti
    
    Differential Revision: D26333953
    
    fbshipit-source-id: cadc0595ad6ab754c4f1f7a5a3733b2c16b3102f

--- benchmarks/static_runtime/test_static_runtime.cc
+++ benchmarks/static_runtime/test_static_runtime.cc
@@ -220,8 +220,16 @@ TEST(StaticRuntime, KWargsAPI_1) {
         at::Tensor output_1 = getTensor(module.forward(inputs));
 
         // run static runtime
-        at::Tensor output_2 = getTensor(runtime.run(inputs, {}));
+        c10::IValue output_ivalue = runtime.run(inputs, {});
+
+        at::Tensor output_2 = getTensor(output_ivalue);
         EXPECT_TRUE(output_1.equal(output_2));
+
+        // check for output aliasing
+        EXPECT_EQ(output_ivalue.use_count(), 1);
+        output_ivalue = IValue();
+
+        EXPECT_EQ(output_2.getIntrusivePtr().use_count(), 1);
       }
 
       // check for input aliasing (deep & wide does not have ops
@@ -256,8 +264,16 @@ TEST(StaticRuntime, KWargsAPI_2) {
              {"wide", wide}});
 
         // run static runtime
-        at::Tensor output_2 = getTensor(runtime.run({}, kwargs));
+        c10::IValue output_ivalue = runtime.run({}, kwargs);
+
+        at::Tensor output_2 = getTensor(output_ivalue);
         EXPECT_TRUE(output_1.equal(output_2));
+
+        // check for output aliasing
+        EXPECT_EQ(output_ivalue.use_count(), 1);
+        output_ivalue = IValue();
+
+        EXPECT_EQ(output_2.getIntrusivePtr().use_count(), 1);
       }
 
       EXPECT_EQ(ad_emb_packed.getIntrusivePtr().use_count(), 1);
