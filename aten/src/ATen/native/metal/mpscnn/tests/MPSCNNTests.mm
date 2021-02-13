388c38505cc71bce1305c193cdbdef41c8561d3f
Stephen Jia <ssjia@fb.com>
2021-02-12 18:38:37 -0800

    [Metal] Add concat op for metal
    Summary: Add concat op to enable models such as SqueezeNet.
    
    Test Plan:
    Test on device:
    ```
    arc focus2 pp-ios
    ```
    Test on mac
    ```
    buck test pp-macos
    ```
    
    Reviewed By: xta0
    
    Differential Revision: D26029029
    
    fbshipit-source-id: b0d621f2069a722f0770218c435b22feac4fb873

--- aten/src/ATen/native/metal/mpscnn/tests/MPSCNNTests.mm
+++ aten/src/ATen/native/metal/mpscnn/tests/MPSCNNTests.mm
@@ -425,6 +425,120 @@ bool test_view() {
   });
 }
 
+bool test_cat_dim0() {
+  __block std::vector<int64_t> x1{3, 9, 221, 193};
+  __block std::vector<int64_t> x2{5, 9, 221, 193};
+  __block std::vector<int64_t> x3{7, 9, 221, 193};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat))*100;
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat))*100;
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat))*100;
+    auto Y = at::cat({X1, X2, X3}, 0);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 0).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
+bool test_cat_dim0_nonarray() {
+  __block std::vector<int64_t> x1{1, 3, 90, 77};
+  __block std::vector<int64_t> x2{1, 3, 90, 77};
+  __block std::vector<int64_t> x3{1, 3, 90, 77};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto Y = at::cat({X1, X2, X3}, 0);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 0).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
+bool test_cat_dim1_0() {
+  __block std::vector<int64_t> x1{4, 10, 271, 333};
+  __block std::vector<int64_t> x2{4, 15, 271, 333};
+  __block std::vector<int64_t> x3{4, 16, 271, 333};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto Y = at::cat({X1, X2, X3}, 1);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 1).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
+bool test_cat_dim1_1() {
+  __block std::vector<int64_t> x1{3, 11, 271, 333};
+  __block std::vector<int64_t> x2{3, 17, 271, 333};
+  __block std::vector<int64_t> x3{3, 21, 271, 333};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto Y = at::cat({X1, X2, X3}, 1);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 1).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
+bool test_cat_dim1_nonarray_0() {
+  __block std::vector<int64_t> x1{1, 3, 22, 33};
+  __block std::vector<int64_t> x2{1, 2, 22, 33};
+  __block std::vector<int64_t> x3{1, 1, 22, 33};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto Y = at::cat({X1, X2, X3}, 1);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 1).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
+bool test_cat_dim1_nonarray_1() {
+  __block std::vector<int64_t> x1{1, 9, 53, 67};
+  __block std::vector<int64_t> x2{1, 2, 53, 67};
+  __block std::vector<int64_t> x3{1, 3, 53, 67};
+  return TEST(x1, __PRETTY_FUNCTION__, ^bool {
+    auto X1 = at::rand(x1, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X2 = at::rand(x2, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto X3 = at::rand(x3, at::TensorOptions(at::kCPU).dtype(at::kFloat));
+    auto Y = at::cat({X1, X2, X3}, 1);
+
+    auto MX1 = X1.metal();
+    auto MX2 = X2.metal();
+    auto MX3 = X3.metal();
+    auto MY = mpscnn::cat({MX1, MX2, MX3}, 1).cpu();
+
+    return almostEqual(Y, MY);
+  });
+}
+
 bool test_softmax() {
   __block std::vector<int64_t> size{2, 3, 1, 1};
   return TEST(size, __PRETTY_FUNCTION__, ^bool {
