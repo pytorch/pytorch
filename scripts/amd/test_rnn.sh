61b135aa650046440f01adb0b520009e8718c598
Michael Melesse <micmelesse@gmail.com>
2021-02-15 14:51:10 -0500

    update test rnn

--- scripts/amd/test_rnn.sh
+++ scripts/amd/test_rnn.sh
@@ -1,2 +1,3 @@
-cd scripts/amd
-python3 Pytorch_LSTMModel.py --batch_size=32 --warm_up=2 --num_test=16 --distributed=False
+# python3 scripts/amd/Pytorch_LSTMModel.py --batch_size=32 --warm_up=2 --num_test=16 --distributed=False
+
+PYTORCH_TEST_WITH_ROCM=1 python test/test_nn.py #--verbose TestFFTCUDA.test_batch_istft_cuda
