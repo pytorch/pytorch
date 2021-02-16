cce87493d76eddc6feb518cbe593f0289bdb7484
Jacob Hepkema <41913625+jacobhepkema@users.noreply.github.com>
2021-02-16 17:10:32 +0000

    Update transforms.py

--- torch/distributions/transforms.py
+++ torch/distributions/transforms.py
@@ -607,18 +607,18 @@ class SoftplusTransform(Transform):
     """
     domain = constraints.real_vector
     codomain = constraints.positive
-    
+
     def __eq__(self, other):
         return isinstance(other, SoftplusTransform)
 
     def _call(self, x):
-        return (1+x.exp()).log()
+        return (1 + x.exp()).log()
 
     def _inverse(self, y):
         return y.expm1().log()
-      
+
     def log_abs_det_jacobian(self, x, y):
-        return -(1+(-x).exp()).log()
+        return -(1 + (-x).exp()).log()
 
 
 class TanhTransform(Transform):
