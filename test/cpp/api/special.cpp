fe10d65c65c462ac1b82be3eece30f39a10d4b4a
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-02-17 01:22:46 -0600

    update test and add test file for build

--- test/cpp/api/special.cpp
+++ test/cpp/api/special.cpp
@@ -5,12 +5,9 @@
 
 #include <test/cpp/api/support.h>
 
-// NOTE: Visual Studio and ROCm builds don't understand complex literals
-//   as of August 2020
-
 // Simple test that verifies the special namespace is registered properly
 //   properly in C++
 TEST(SpecialTest, special) {
-    auto t = torch::randn(128, torch::dtype(torch::kDouble));
+    auto t = torch::randn(128, torch::kDouble);
     torch::special::lgamma(t);
 }
