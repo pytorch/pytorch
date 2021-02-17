4250ced02884f4c034c2bd96d900c9639dc7be31
Guilherme Leobas <gleobas@quansight.com>
2021-02-17 20:22:05 +0000

    Initial support for numpy like test infrastructure for type annotations

--- test/test_typing.py
+++ test/test_typing.py
@@ -1,12 +1,10 @@
-# based on NumPy test_typing.py
+# based on NumPy numpy/typing/tests/test_typing.py
 
-import importlib.util
 import itertools
 import os
 import re
 import shutil
-from collections import defaultdict
-from typing import Optional, IO, Dict, List
+from typing import IO, Dict, List
 
 import pytest
 import numpy as np
@@ -20,10 +18,8 @@ else:
 
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "typing")
-PASS_DIR = os.path.join(DATA_DIR, "pass")
-FAIL_DIR = os.path.join(DATA_DIR, "fail")
 REVEAL_DIR = os.path.join(DATA_DIR, "reveal")
-MYPY_INI = os.path.join(DATA_DIR, os.pardir, "mypy.ini")
+MYPY_INI = os.path.join(DATA_DIR, os.pardir, os.pardir, "mypy.ini")
 CACHE_DIR = os.path.join(DATA_DIR, ".mypy_cache")
 
 #: A dictionary with file names as keys and lists of the mypy stdout as values.
@@ -51,17 +47,20 @@ def run_mypy() -> None:
     if os.path.isdir(CACHE_DIR):
         shutil.rmtree(CACHE_DIR)
 
-    for directory in (PASS_DIR, REVEAL_DIR, FAIL_DIR):
+    for directory in (REVEAL_DIR,):
         # Run mypy
-        stdout, stderr, _ = api.run([
-            "--config-file",
-            MYPY_INI,
-            "--cache-dir",
-            CACHE_DIR,
-            directory,
-        ])
+        stdout, stderr, _ = api.run(
+            [
+                "--show-absolute-path",
+                "--config-file",
+                MYPY_INI,
+                "--cache-dir",
+                CACHE_DIR,
+                directory,
+            ]
+        )
         assert not stderr, directory
-        stdout = stdout.replace('*', '')
+        stdout = stdout.replace("*", "")
 
         # Parse the output
         iterator = itertools.groupby(stdout.split("\n"), key=_key_func)
@@ -83,68 +82,13 @@ def get_test_cases(directory):
                 )
 
 
-@pytest.mark.skipif(NO_MYPY, reason="Mypy is not installed")
-@pytest.mark.parametrize("path", get_test_cases(PASS_DIR))
-def test_success(path):
-    # Alias `OUTPUT_MYPY` so that it appears in the local namespace
-    output_mypy = OUTPUT_MYPY
-    if path in output_mypy:
-        raise AssertionError("\n".join(v for v in output_mypy[path].values()))
+def _construct_format_dict():
+    return {}
 
 
-@pytest.mark.skipif(NO_MYPY, reason="Mypy is not installed")
-@pytest.mark.parametrize("path", get_test_cases(FAIL_DIR))
-def test_fail(path):
-    __tracebackhide__ = True
-
-    with open(path) as fin:
-        lines = fin.readlines()
-
-    errors = defaultdict(lambda: "")
-
-    output_mypy = OUTPUT_MYPY
-    assert path in output_mypy
-    for error_line in output_mypy[path]:
-        match = re.match(
-            r"^.+\.py:(?P<lineno>\d+): (error|note): .+$",
-            error_line,
-        )
-        if match is None:
-            raise ValueError(f"Unexpected error line format: {error_line}")
-        lineno = int(match.group('lineno'))
-        errors[lineno] += error_line
-
-    for i, line in enumerate(lines):
-        lineno = i + 1
-        if line.startswith('#') or (" E:" not in line and lineno not in errors):
-            continue
-
-        target_line = lines[lineno - 1]
-        if "# E:" in target_line:
-            marker = target_line.split("# E:")[-1].strip()
-            expected_error = errors.get(lineno)
-            _test_fail(path, marker, expected_error, lineno)
-        else:
-            pytest.fail(f"Error {repr(errors[lineno])} not found")
-
-
-_FAIL_MSG1 = """Extra error at line {}
-
-Extra error: {!r}
-"""
-
-_FAIL_MSG2 = """Error mismatch at line {}
-
-Expected error: {!r}
-Observed error: {!r}
-"""
-
-
-def _test_fail(path: str, error: str, expected_error: Optional[str], lineno: int) -> None:
-    if expected_error is None:
-        raise AssertionError(_FAIL_MSG1.format(lineno, error))
-    elif error not in expected_error:
-        raise AssertionError(_FAIL_MSG2.format(lineno, expected_error, error))
+#: A dictionary with all supported format keys (as keys)
+#: and matching values
+FORMAT_DICT: Dict[str, str] = _construct_format_dict()
 
 
 def _parse_reveals(file: IO[str]) -> List[str]:
@@ -187,7 +131,7 @@ def test_reveal(path):
         )
         if match is None:
             raise ValueError(f"Unexpected reveal line format: {error_line}")
-        lineno = int(match.group('lineno')) - 1
+        lineno = int(match.group("lineno")) - 1
         assert "Revealed type is" in error_line
 
         marker = lines[lineno]
@@ -204,13 +148,3 @@ Observed reveal: {!r}
 def _test_reveal(path: str, reveal: str, expected_reveal: str, lineno: int) -> None:
     if reveal not in expected_reveal:
         raise AssertionError(_REVEAL_MSG.format(lineno, expected_reveal, reveal))
-
-
-@pytest.mark.skipif(NO_MYPY, reason="Mypy is not installed")
-@pytest.mark.parametrize("path", get_test_cases(PASS_DIR))
-def test_code_runs(path):
-    path_without_extension, _ = os.path.splitext(path)
-    dirname, filename = path.split(os.sep)[-2:]
-    spec = importlib.util.spec_from_file_location(f"{dirname}.{filename}", path)
-    test_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(test_module)
