0851cc42b0bfc3c5cecd86ee1f75ee7eacf7339f
eellison <elias_ellison@brown.edu>
2021-02-18 15:36:51 -0800

    Update freezing API - changes from 52337 (#52392)
    Co-authored-by: eellison <eellison@fb.com>

--- torch/csrc/jit/passes/frozen_graph_optimizations.cpp
+++ torch/csrc/jit/passes/frozen_graph_optimizations.cpp
@@ -8,12 +8,16 @@
 namespace torch {
 namespace jit {
 
-void OptimizeFrozenGraph(std::shared_ptr<Graph>& graph) {
+void OptimizeFrozenGraph(
+    std::shared_ptr<Graph>& graph,
+    bool optimize_numerics) {
   // run a couple times to capture Conv -> Mul -> Add etc
-  for (size_t i = 0; i < 2; i++) {
-    FoldFrozenConvBatchnorm(graph);
-    FoldFrozenConvAddOrSub(graph);
-    FoldFrozenConvMulOrDiv(graph);
+  if (optimize_numerics) {
+    for (size_t i = 0; i < 2; i++) {
+      FoldFrozenConvBatchnorm(graph);
+      FoldFrozenConvAddOrSub(graph);
+      FoldFrozenConvMulOrDiv(graph);
+    }
   }
 }
 
