29a661b6b4e64dafc983a5e016a54435c6e83bc7
Eli Uriegas <eliuriegas@fb.com>
2021-02-17 16:35:05 -0800

    DO NOT MERGE, testing CUDA 11.1 for v1.8
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .circleci/cimodel/data/dimensions.py
+++ .circleci/cimodel/data/dimensions.py
@@ -1,19 +1,20 @@
 PHASES = ["build", "test"]
 
 CUDA_VERSIONS = [
-    "101",
-    "102",
+    # "101",
+    # "102",
     "111",
 ]
 
 ROCM_VERSIONS = [
-    "3.10",
-    "4.0.1",
+    # "3.10",
+    # "4.0.1",
 ]
 
 ROCM_VERSION_LABELS = ["rocm" + v for v in ROCM_VERSIONS]
 
-GPU_VERSIONS = [None] + ["cuda" + v for v in CUDA_VERSIONS] + ROCM_VERSION_LABELS
+# GPU_VERSIONS = [None] + ["cuda" + v for v in CUDA_VERSIONS] + ROCM_VERSION_LABELS
+GPU_VERSIONS = ["cuda" + v for v in CUDA_VERSIONS]
 
 STANDARD_PYTHON_VERSIONS = [
     "3.6",
