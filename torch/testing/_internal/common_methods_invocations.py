fc247290664e6b5a6e2c2c98f8699559d4c958c0
iramazanli <iramazanli@facebook.com>
2021-02-19 06:53:32 -0800

    add test cases for complex supporting unfold

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1852,6 +1852,7 @@ def sample_inputs_rsub(op_info, device, dtype, requires_grad, variant='tensor'):
 
     return samples
 
+<<<<<<< HEAD
 
 def sample_inputs_cumsum(op_info, device, dtype, requires_grad):
     def _make_tensor_helper(shape, low=None, high=None):
@@ -1867,6 +1868,46 @@ def sample_inputs_cumsum(op_info, device, dtype, requires_grad):
     )
 
     return samples
+=======
+def sample_inputs_unfold(op_info, device, dtype, requires_grad):
+    test_cases = (
+        ((), (0, 1, 1), 'scalar'),
+        ((S, S, S, S), (0, 3, 1), '4d_dim0_step1'),
+        ((S, S, S, S), (1, 3, 1), '4d_dim1_step1'),
+        ((S, S, S, S), (2, 3, 1), '4d_dim2_step1'),
+        ((S, S, S, S), (3, 3, 1), '4d_dim3_step1'),
+        ((S, S, S, S), (0, 3, 2), '4d_dim0_step2'),
+        ((S, S, S, S), (1, 3, 2), '4d_dim1_step2'),
+        ((S, S, S, S), (2, 3, 2), '4d_dim2_step2'),
+        ((S, S, S, S), (3, 3, 2), '4d_dim3_step2'),
+        ((S, S, S, S), (0, 4, 1), '4d_dim0_size4'),
+        ((S, S, S, S), (1, 4, 1), '4d_dim1_size4'),
+        ((S, S, S, S), (2, 4, 1), '4d_dim2_size4'),
+        ((S, S, S, S), (3, 4, 1), '4d_dim3_size4'),
+        ((M,), (0, 3, 1), '1d_step1'),
+        ((M,), (0, 3, 2), '1d_step2'),
+        ((M,), (0, 3, 3), '1d_step3'),
+        ((1000,), (0, 3, 11), '1d_step_gt_size'),
+        ((1000,), (0, 2, 27), '1d_step_gt_size2'),
+        ((10, 10), (0, 1, 2), '2d_step_gt_size'),
+        ((10, 10), (1, 2, 3), '2d_step_gt_size2'),
+        ((10, 10), (1, 2, 2), '2d_step_ge_size2'),
+        ((S, S, S), (2, 3, 2), 'lastdim'),
+    )
+
+    sample_inputs = []
+    for dim, size, step in test_cases:
+        args = (make_tensor(dim, device, dtype,
+                            low=None, high=None,
+                            requires_grad=requires_grad),
+                make_tensor(size, device, dtype,
+                            low=None, high=None,
+                            requires_grad=requires_grad),
+                step)
+        sample_inputs += [SampleInput(args)]
+
+    return sample_inputs
+>>>>>>> c31542f078 (add test cases for complex supporting unfold)
 
 def sample_inputs_lerp(op_info, device, dtype, requires_grad):
     def _make_tensor_helper(shape, low=None, high=None):
@@ -3490,6 +3531,18 @@ op_db: List[OpInfo] = [
                # hstack does not correctly warn when resizing out= inputs
                SkipInfo('TestCommon', 'test_out')),
            sample_inputs_func=sample_inputs_hstack_dstack_vstack),
+    OpInfo('unfold',
+           # gradcheck expects the input arguments as a flat list
+           op=lambda *args: torch.unfold([*args]),
+           dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16),
+           test_complex_grad=True,
+           test_inplace_grad=False,
+           supports_tensor_out=False,
+           skips=(
+               SkipInfo('TestCommon', 'test_variant_consistency_jit',
+                        dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16)),
+           ),
+           sample_inputs_func=sample_inputs_unfold),
     OpInfo('vstack',
            # gradcheck expects the input arguments as a flat list
            op=lambda *args, **kwargs: torch.vstack([*args], **kwargs),
@@ -4072,28 +4125,6 @@ def method_tests():
         ('cummin', (), (0,), 'dim0_scalar', (), [0]),
         ('cumsum', (S, S, S), (1,), 'dim1_cast', (), [0], (), ident, {'dtype': torch.float64}),
         ('log_softmax', (S, S, S), (1, torch.float64,), 'kwarg_dtype_would_break_jit_loader', (True,)),
-        ('unfold', (), (0, 1, 1), 'scalar', (), [0]),
-        ('unfold', (S, S, S, S), (0, 3, 1), '4d_dim0_step1', (), [0]),
-        ('unfold', (S, S, S, S), (1, 3, 1), '4d_dim1_step1', (), [0]),
-        ('unfold', (S, S, S, S), (2, 3, 1), '4d_dim2_step1', (), [0]),
-        ('unfold', (S, S, S, S), (3, 3, 1), '4d_dim3_step1', (), [0]),
-        ('unfold', (S, S, S, S), (0, 3, 2), '4d_dim0_step2', (), [0]),
-        ('unfold', (S, S, S, S), (1, 3, 2), '4d_dim1_step2', (), [0]),
-        ('unfold', (S, S, S, S), (2, 3, 2), '4d_dim2_step2', (), [0]),
-        ('unfold', (S, S, S, S), (3, 3, 2), '4d_dim3_step2', (), [0]),
-        ('unfold', (S, S, S, S), (0, 4, 1), '4d_dim0_size4', (), [0]),
-        ('unfold', (S, S, S, S), (1, 4, 1), '4d_dim1_size4', (), [0]),
-        ('unfold', (S, S, S, S), (2, 4, 1), '4d_dim2_size4', (), [0]),
-        ('unfold', (S, S, S, S), (3, 4, 1), '4d_dim3_size4', (), [0]),
-        ('unfold', (M,), (0, 3, 1), '1d_step1', (), [0]),
-        ('unfold', (M,), (0, 3, 2), '1d_step2', (), [0]),
-        ('unfold', (M,), (0, 3, 3), '1d_step3', (), [0]),
-        ('unfold', (1000,), (0, 3, 11), '1d_step_gt_size', (), [0]),
-        ('unfold', (1000,), (0, 2, 27), '1d_step_gt_size2', (), [0]),
-        ('unfold', (10, 10), (0, 1, 2), '2d_step_gt_size', (), [0]),
-        ('unfold', (10, 10), (1, 2, 3), '2d_step_gt_size2', (), [0]),
-        ('unfold', (10, 10), (1, 2, 2), '2d_step_ge_size2', (), [0]),
-        ('unfold', (S, S, S), (2, 3, 2), 'lastdim', (), [0]),
         ('addmm', (S, M), ((S, S), (S, M)), '', (True, ['aten::add', 'aten::mm'])),
         ('addmm', (1,), ((S, S), (S, M)), 'broadcast_lhs', (True, ['aten::add', 'aten::mm'])),
         ('addmm', (S, M), ((S, S), (S, M)), 'coef', (True,), (), (), ident, {'beta': 0.2, 'alpha': 0.6}),
