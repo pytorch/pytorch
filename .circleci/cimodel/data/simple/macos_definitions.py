e0472a593aa874fdc3780c8a76dbe0a668620503
Chen Lai <chenlai@fb.com>
2021-02-18 17:25:37 -0800

    [PyTorch] update CMake to build libtorch lite
    ghstack-source-id: 15bfa0ff5a7892a77da35c679309fef1bc7e60f8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51419

--- .circleci/cimodel/data/simple/macos_definitions.py
+++ .circleci/cimodel/data/simple/macos_definitions.py
@@ -1,7 +1,10 @@
+import cimodel.lib.miniutils as miniutils
+
 class MacOsJob:
-    def __init__(self, os_version, is_test=False):
+    def __init__(self, os_version, is_test=False, extra_props=None):
         self.os_version = os_version
         self.is_test = is_test
+        self.extra_props = extra_props
 
     def gen_tree(self):
         non_phase_parts = ["pytorch", "macos", self.os_version, "py3"]
@@ -9,6 +12,8 @@ class MacOsJob:
         phase_name = "test" if self.is_test else "build"
 
         full_job_name = "_".join(non_phase_parts + [phase_name])
+        if self.extra_props:
+            full_job_name = "_".join([full_job_name] + list(self.extra_props.keys()))
 
         test_build_dependency = "_".join(non_phase_parts + ["build"])
         extra_dependencies = [test_build_dependency] if self.is_test else []
@@ -18,10 +23,22 @@ class MacOsJob:
         # for the YAML output to work.
         props_dict = {"requires": job_dependencies, "name": full_job_name}
 
+        # if self.extra_props:
+        #     props_dict.update(self.extra_props)
+
         return [{full_job_name: props_dict}]
 
 
-WORKFLOW_DATA = [MacOsJob("10_15"), MacOsJob("10_13"), MacOsJob("10_13", True)]
+WORKFLOW_DATA = [
+    MacOsJob("10_15"),
+    MacOsJob("10_13"),
+    MacOsJob("10_13", True),
+    MacOsJob(
+        "10_13",
+        True,
+        extra_props={"build_lite_interpreter": miniutils.quote(str(int(True)))}
+    )
+]
 
 
 def get_workflow_jobs():
