752d808fa080d4da7cb86a0c60fb0ecb5c194050
Elias Ellison <eellison@devfair044.maas>
2021-02-19 10:12:05 -0800

    Trace linear as aten::linear (#51897)
    Summary:
    https://github.com/pytorch/pytorch/pull/51613 made `torch.nn.functional.linear` compile as `aten::linear`, extend the same behavior with tracing.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51897
    
    Reviewed By: albanD
    
    Differential Revision: D26320711
    
    Pulled By: eellison
    
    fbshipit-source-id: a26d3c37323a0706313c6ebb210bad60eec6a64b

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -6296,7 +6296,7 @@ class TestONNXRuntime(unittest.TestCase):
 
         x = torch.randn(32, 3)
         f = io.BytesIO()
-        torch.onnx._export(test_model, (x,), f, _retain_param_name=True)
+        torch.onnx._export(test_model, (x,), f, _retain_param_name=True, do_constant_folding=False)
         loaded_model = onnx.load_from_string(f.getvalue())
 
         actual_list = [p.name for p in loaded_model.graph.initializer]
@@ -6346,7 +6346,7 @@ class TestONNXRuntime(unittest.TestCase):
         example_output = (test_model(x, y),)
         f = io.BytesIO()
 
-        torch.onnx.export(test_model, (x, y), f, example_outputs=example_output, _retain_param_name=True)
+        torch.onnx.export(test_model, (x, y), f, example_outputs=example_output, _retain_param_name=True, do_constant_folding=False)
         loaded_model = onnx.load_from_string(f.getvalue())
 
         actual_list = [p.name for p in loaded_model.graph.initializer]
