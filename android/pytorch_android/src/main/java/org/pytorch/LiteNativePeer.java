24c9df553f4549b6854bc95436e3460675388db8
Chen Lai <chenlai@fb.com>
2021-02-19 14:09:11 -0800

    [PyTorch] update CMake to build libtorch lite
    ghstack-source-id: fcad58e391c6364232deca89cab71c8b4eaacb02
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51419

--- android/pytorch_android/src/main/java/org/pytorch/LiteNativePeer.java
+++ android/pytorch_android/src/main/java/org/pytorch/LiteNativePeer.java
@@ -2,9 +2,11 @@ package org.pytorch;
 
 import com.facebook.jni.HybridData;
 import com.facebook.soloader.nativeloader.NativeLoader;
+import com.facebook.soloader.nativeloader.SystemDelegate;
 
 class LiteNativePeer implements INativePeer {
   static {
+    NativeLoader.init(new SystemDelegate());
     NativeLoader.loadLibrary("pytorch_jni_lite");
     PyTorchCodegenLoader.loadNativeLibs();
   }
