c8558cca50d0f818810af26756e5bf855f15be3d
Haichuan Yang <haichuan@fb.com>
2021-02-20 12:19:02 -0800

    fix zero_point rounding for _fake_quantize_learnable_per_channel_affine (#52290)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52290
    
    _fake_quantize_learnable_per_channel_affine should allow taking non-integer zero_point as input, and perform rounding and clamp before doing forward/backward. In this diff, we make _fake_quantize_learnable_per_channel_affine to round and clamp zero_point beforehand as in _fake_quantize_learnable_per_tensor_affine.
    ghstack-source-id: 122148099
    
    Test Plan: `buck test mode/dev-nosan -c fbcode.platform=platform009 //caffe2/test:quantization -- test_learnable`
    
    Reviewed By: raghuramank100
    
    Differential Revision: D26446342
    
    fbshipit-source-id: fc9b6832fa247cc9d41265eb4fd1575a2d2ed12c

--- test/quantization/test_workflow_module.py
+++ test/quantization/test_workflow_module.py
@@ -148,10 +148,10 @@ def _fake_quantize_learnable_per_channel_affine_grad_reference(
     - https://arxiv.org/pdf/1902.08153.pdf
     - https://arxiv.org/pdf/1903.08066.pdf
     """
+    per_channel_zero_point = ((per_channel_zero_point.detach() + 0.5).clamp(quant_min, quant_max)).type(torch.int64)
     grad_X = _fake_quantize_per_channel_affine_grad_reference(
         dY, X, per_channel_scale, per_channel_zero_point, axis, quant_min, quant_max).to(device)
     per_channel_scale = per_channel_scale.detach().type(torch.float)
-    per_channel_zero_point = ((per_channel_zero_point.detach() + 0.5).clamp(quant_min, quant_max)).type(torch.int64)
 
     grad_scale = torch.zeros([per_channel_scale.size(0)]).to(device)
     grad_zero_point = torch.zeros([per_channel_zero_point.size(0)]).to(device)
@@ -1265,14 +1265,14 @@ class TestFakeQuantize(TestCase):
             quant_min, quant_max = 0, 2 ** (n_bits) - 1
 
             scale_base = scale_base.to(device)
-            zero_point_base = zero_point_base.clamp(quant_min, quant_max)
+            zero_point_base = zero_point_base.to(device)
 
             X_curr = X_base.clone()
             scale_curr = scale_base.clone()
-            zero_point_curr = zero_point_base.to(dtype=torch.int64, device=device)
+            zero_point_curr = zero_point_base.clone()
 
             Y = _fake_quantize_per_channel_affine_reference(
-                X_curr, scale_curr, zero_point_curr, axis, quant_min, quant_max).to(device)
+                X_curr, scale_curr, zero_point_curr.round().clamp(quant_min, quant_max), axis, quant_min, quant_max).to(device)
             for grad_factor in [0.1, 1.0, 10.0]:
                 Y_prime = torch._fake_quantize_learnable_per_channel_affine(
                     X_curr, scale_curr, zero_point_curr, axis, quant_min, quant_max, grad_factor).to(device)
@@ -1369,7 +1369,7 @@ class TestFakeQuantize(TestCase):
             X_curr.requires_grad_()
             scale_curr = scale_base.clone()
             scale_curr.requires_grad_()
-            zero_point_curr = zero_point_base.clamp(quant_min, quant_max)
+            zero_point_curr = zero_point_base.clone()
             zero_point_curr.requires_grad_()
 
             for grad_factor in [0.1, 1.0, 10.0]:
