616463d850315acd75e2f485d7766306f6f23b24
Jay Zhang <jiz@microsoft.com>
2021-02-20 18:36:59 +0800

    Update it to supports split 1 module into 2.

--- torch/csrc/jit/passes/onnx/nezha_helper.cpp
+++ torch/csrc/jit/passes/onnx/nezha_helper.cpp
@@ -22,7 +22,11 @@ static void releaseGraph(std::shared_ptr<Graph> graph){
     }
 
     for (auto node : graph->block()->nodes()) {
-        printf("\n------ NeZha_TryUpdateModule:appendNode %s, inputs: %ld, outputs: %ld------", node->kind().toQualString(), node->inputs().size(), node->outputs().size());
+        printf("\n------ NeZha_TryUpdateModule:releaseGraph %s, scopeName: %s, debugName: %s ------", node->kind().toQualString(), node->scopeName().c_str(), node->inputs()[0]->debugName().c_str());
+        auto tempVal = node->get(Symbol::fromQualString("prim::GetAttr"));
+        if (tempVal != nullptr){
+            printf("\n------ more: %s", node->get(Symbol::prim("name")).value().toString()->string().c_str());
+        }
     }
 
     auto temporary_nodes = graph->block()->nodes();
@@ -36,21 +40,124 @@ static void releaseGraph(std::shared_ptr<Graph> graph){
     //         ++it) {
     //     IValue ival(true);
     //     auto new_constant = it->owningGraph()->insertConstant(ival);
-
+        // if (std::strcmp(it->kind().toQualString(), "prim::CallMethod") == 0){
+        //     printf("\n------ updateGraph:removeAllInputs - %s------", it->kind().toQualString());
+        //     it->removeAllInputs();
+        // }
     //     it->output()->replaceAllUsesWith(new_constant);
     //     it.destroyCurrent();
     // }
 
+
     printf("\n------ Release graph ------");
 } 
 
-void NeZha_TryMergeModule(
-    Module& dst_module,
-    std::shared_ptr<Graph>& src_graph_01,
-    std::shared_ptr<Graph>& src_graph_02) {
-
-    // Try to merge src_graph_01 and src_graph_02 together and return it into dst_module.
+static void updateGraph(std::shared_ptr<Graph> graph){
+    if (graph == nullptr){
+        return;
+    }
     
+    auto temporary_nodes = graph->block()->nodes();
+    generic_graph_node_list_iterator<Node> keyNode;
+
+    int delNodesCount = 0;
+    for (auto it = temporary_nodes.rbegin(); it != temporary_nodes.rend();
+            it++) {
+        printf("\n------ delNodesCount: %d ------\n", delNodesCount);
+
+        if (delNodesCount < 3) {
+            keyNode = it;
+            keyNode++;
+
+            it->output()->replaceAllUsesWith(keyNode->outputs()[0]);
+            it.destroyCurrent();
+            delNodesCount++;
+        }
+
+        if (std::strcmp(it->kind().toQualString(), "prim::GetAttr") == 0){
+            if (!it->output()->hasUses()){
+                it.destroyCurrent();
+            }
+        }
+
+        printf("\n------ check graph: ------\n");
+        graph->dump();
+    }
+}
+
+static void update2ndGraph(Module& module){
+    // auto allGraphInputs = graph->inputs();
+    // printf("\n------ all inputs: %ld ------\n", allGraphInputs.size());
+    // for (int i = 0; i < allGraphInputs.size(); i++){
+    //     printf("\n------ input: %d, %s ------\n", i, allGraphInputs[i]->debugName().c_str());
+    // }
+
+    // auto graph_input_node = allGraphInputs[1]->node();
+    // printf("\n------ input.1 node: ------\n");
+    // allGraphInputs[1]->node()->dump();
+
+    std::shared_ptr<Graph> graph = module.get_method("forward").graph()->copy();
+    auto temporary_nodes = graph->block()->nodes();
+
+    int delNodesCount = 0;
+
+    for (auto it = temporary_nodes.begin(); it != temporary_nodes.end();
+            it++) {
+        printf("\n------ delNodesCount: %d ------\n", delNodesCount);
+
+        if (std::strcmp(it->kind().toQualString(), "prim::GetAttr") == 0){
+            continue;
+        }
+
+        if (delNodesCount < 2) {
+            it->output()->replaceAllUsesWith(graph->inputs()[1]);
+            it.destroyCurrent();
+            delNodesCount++;
+        }
+
+        printf("\n------ check graph: ------\n");
+        graph->dump();
+    }
+
+    for (auto it = temporary_nodes.begin(); it != temporary_nodes.end();
+        it++) {
+        if (std::strcmp(it->kind().toQualString(), "prim::GetAttr") == 0){
+            if (!it->output()->hasUses()){
+                it.destroyCurrent();
+            }
+        }
+    }
+
+    printf("\n------ check graph: ------\n");
+    graph->dump();
+
+    const auto method_name = QualifiedName(*module.type()->name(), "forward");
+    module.type()->unsafeRemoveMethod("forward");
+    module._ivalue()->compilation_unit()->unsafeRemoveMethod(method_name);
+    auto fn = module._ivalue()->compilation_unit()->create_function(method_name, graph);
+    module.type()->addMethod(fn);
+    printf("\n------ Print second module after: \n%s ------", module.dump_to_str(true, false, false, 0).c_str());
+    printf("\n------ Print second module details: %s ------\n", module.type()->name()->qualifiedName().c_str());    
+}
+
+void NeZha_TrySplitModule(
+    Module& module_1st,
+    Module& module_2nd) {
+
+    printf("\n------ Start NeZha_TrySplitModule ------");
+
+    // printf("\n------ Print first module before: \n%s ------", module_1st.dump_to_str(true, false, false, 0).c_str());
+    auto graph_bak = module_1st.get_method("forward").graph()->copy();
+    updateGraph(graph_bak);
+    const auto method_name = QualifiedName(*module_1st.type()->name(), "forward");
+    module_1st.type()->unsafeRemoveMethod("forward");
+    module_1st._ivalue()->compilation_unit()->unsafeRemoveMethod(method_name);
+    auto fn = module_1st._ivalue()->compilation_unit()->create_function(
+        method_name, graph_bak);
+    module_1st.type()->addMethod(fn);
+    // printf("\n------ Print first module after: \n%s ------", module_1st.dump_to_str(true, true, true, 0).c_str());
+
+    update2ndGraph(module_2nd);
 }
 
 void NeZha_TryUpdateModule(
@@ -58,14 +165,20 @@ void NeZha_TryUpdateModule(
     std::shared_ptr<Graph>& src_graph) {
 
     printf("\n------ Start NeZha_TryUpdateModule ------");
-    releaseGraph(dst_module.get_method("forward").graph());
 
+    // printf("\n------ Print module before: \n%s ------", dst_module.dump_to_str(true, false, false, 0).c_str());
+    auto graph_bak = dst_module.get_method("forward").graph()->copy();
+    updateGraph(graph_bak);
+    
+    // releaseGraph(dst_module.get_method("forward").graph());
+    
     const auto method_name = QualifiedName(*dst_module.type()->name(), "forward");
     dst_module.type()->unsafeRemoveMethod("forward");
     dst_module._ivalue()->compilation_unit()->unsafeRemoveMethod(method_name);
     auto fn = dst_module._ivalue()->compilation_unit()->create_function(
-        method_name, src_graph);
+        method_name, graph_bak);
     dst_module.type()->addMethod(fn);
+    printf("\n------ Print module after: \n%s ------", dst_module.dump_to_str(true, false, false, 0).c_str());
 }
 
 void NeZha_TryUpdateGraph(
