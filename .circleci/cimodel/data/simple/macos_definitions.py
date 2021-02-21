56cf9dc599807b04afc7c3c321899a797effa85c
Chen Lai <chenlai@fb.com>
2021-02-20 22:13:20 -0800

    [PyTorch][CI] Enable test_lite_interpreter_runtime unittest in CI (macos)
    [ghstack-poisoned]

--- .circleci/cimodel/data/simple/macos_definitions.py
+++ .circleci/cimodel/data/simple/macos_definitions.py
@@ -1,14 +1,25 @@
+import cimodel.lib.miniutils as miniutils
+
 class MacOsJob:
-    def __init__(self, os_version, is_test=False):
+    def __init__(self, os_version, is_build=False, is_test=False, extra_props={}):
         self.os_version = os_version
+        self.is_build = is_build
         self.is_test = is_test
+        self.extra_props = extra_props
 
     def gen_tree(self):
         non_phase_parts = ["pytorch", "macos", self.os_version, "py3"]
 
-        phase_name = "test" if self.is_test else "build"
+        extra_name_list = [name for name, exist in self.extra_props.items() if exist]
+        full_job_name_list = non_phase_parts + extra_name_list + [
+            'build' if self.is_build else None,
+            'test' if self.is_test else None,
+        ]
 
-        full_job_name = "_".join(non_phase_parts + [phase_name])
+        full_job_name = "_".join(list(filter(None, full_job_name_list)))
+
+        # phase_name = "test" if self.is_test else "build"
+        # full_job_name = "_".join(non_phase_parts + [extra_name])
 
         test_build_dependency = "_".join(non_phase_parts + ["build"])
         extra_dependencies = [test_build_dependency] if self.is_test else []
@@ -18,10 +29,29 @@ class MacOsJob:
         # for the YAML output to work.
         props_dict = {"requires": job_dependencies, "name": full_job_name}
 
+        # if self.extra_props:
+        #     props_dict.update(self.extra_props)
+
         return [{full_job_name: props_dict}]
 
 
-WORKFLOW_DATA = [MacOsJob("10_15"), MacOsJob("10_13"), MacOsJob("10_13", True)]
+WORKFLOW_DATA = [
+    MacOsJob("10_15", is_build=True),
+    MacOsJob("10_13", is_build=True),
+    MacOsJob(
+        "10_13",
+        is_build=False,
+        is_test=True,
+    ),
+    MacOsJob(
+        "10_13",
+        is_build=True,
+        is_test=True,
+        extra_props={
+            "lite_interpreter": True,
+        },
+    )
+]
 
 
 def get_workflow_jobs():
