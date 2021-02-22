929c948db7997bbccfffc9005d5f3d7665f746b4
Jay Zhang <jiz@microsoft.com>
2021-02-22 22:45:56 +0800

    ONNX exporter could success.

--- nezha/separate_models.py
+++ nezha/separate_models.py
@@ -17,6 +17,16 @@ def prof_meth_call(*args, **kwargs):
 
 torch._C.ScriptMethod.__call__ = prof_meth_call
 
+import onnxruntime as ort
+import onnx
+
+old_call = torch._C.ScriptMethod.__call__
+
+def prof_meth_call(*args, **kwargs):
+    return old_call(*args, **kwargs)
+
+torch._C.ScriptMethod.__call__ = prof_meth_call
+
 def to_numpy(tensor):
     if tensor.requires_grad:
         return tensor.detach().cpu().numpy()
@@ -182,53 +192,48 @@ print('----End normal model.')
 print('----Start script model.')
 with torch.no_grad():
     trace_m_all_01 = torch.jit.trace(NeuralNet_All(total_input_size, total_hidden_size, total_num_classes), x)
-    print('----Finish trace_m_all_01.')
-
-    trace_m_all_02 = torch.jit.trace(NeuralNet_All(total_input_size, total_hidden_size, total_num_classes), x)
-    print('----Finish trace_m_all_02.')
-
-    trace_m_1 = torch.jit.trace(NeuralNet_1st(total_input_size, total_hidden_size), x)
-    print('----Finish trace_m_1.')
-
     trace_m_all_01.load_state_dict(all_params, strict=False)
     trace_m_all_01.eval()
-
-    trace_m_all_02.load_state_dict(all_params, strict=False)
-    trace_m_all_02.eval()    
-
-    trace_m_1.load_state_dict(all_params, strict=False)
-    trace_m_1.eval()
-
     trace_results_all_before = trace_m_all_01(dummy_input)
 
+    print('----Finish trace_m_all_01.')
+
+    # torch.onnx.export(trace_m_all_01, (x, ), 'test_model_before.onnx', example_outputs=trace_results_all_before)
+    # ort_sess = ort.InferenceSession('test_model_before.onnx')
+    # input_name = ort_sess.get_inputs()[0].name
+    # label_name = ort_sess.get_outputs()[0].name
+    # my_input, _ = torch.jit._flatten(x)
+    # my_inputs = [to_numpy(inp) for inp in my_input]
+
+    # ort_outs = ort_sess.run([label_name], {input_name: my_inputs[0]})
+    # [np.testing.assert_allclose(trace_results_all_before, ort_outs[0], rtol=1e-03, atol=1e-05)]
+
+    trace_m_all_02 = torch.jit.trace(NeuralNet_All(total_input_size, total_hidden_size, total_num_classes), x)
+    trace_m_all_02.load_state_dict(all_params, strict=False)
+    trace_m_all_02.eval()    
+    print('----Finish trace_m_all_02.')
+
+    # Split the overall module into 2 parts. This is an in-place operation.
     torch._C._jit_nezha_update_graph(trace_m_all_01._c, trace_m_all_02._c)
 
-    # trace_results_1 = trace_m_1(x)
-    # print('----Finish trace_m_1 inference.')
+    trace_m_1 = torch.jit.trace(NeuralNet_1st(total_input_size, total_hidden_size), x)
+    trace_m_1.load_state_dict(all_params, strict=False)
+    trace_m_1.eval()
+    print('----Finish trace_m_1.')
 
-    # f = 'model_before.pt'
-    # torch.jit.save(trace_m_all, f)
-    # loaded_m_before = torch.jit.load(f)
-    # print('----Save and Load model.')
-    # torch._C._jit_nezha_update_graph(trace_m_all._c, trace_m_1.graph)
-
-    # f = 'model_after.pt'
-    # torch.jit.save(trace_m_all, f)
-    # loaded_m_after = torch.jit.load(f)
-
-    trace_results_new_01 = trace_m_all_01(dummy_input)
+    # Compare results of different modules
     trace_results_1 = trace_m_1(dummy_input)
-    # [np.testing.assert_allclose(trace_results_new_01, trace_results_1, rtol=1e-03, atol=inliner::inlineCalls(),1e-05)]
+    # trace_results_new_01 = trace_m_all_01.forward(dummy_input)
+    # [np.testing.assert_allclose(trace_results_new_01, trace_results_1, rtol=1e-03, atol=1e-05)]
+    # print('----trace_m_1 results are same as first splitte module.')
 
-    # Construct the second part of the module
-    trace_m_2 = torch.jit.trace(NeuralNet_2nd(total_hidden_size, total_num_classes), trace_results_1)
-    trace_m_2.load_state_dict(all_params, strict=False)
-    trace_m_2.eval()
+    # Save the first part into ONNX and get the result via ORT.
+    torch.onnx.export(trace_m_all_01, (dummy_input, ), 'test_model_01.onnx', example_outputs=trace_results_1)
+    ort_sess = ort.InferenceSession('test_model_01.onnx')
+    input_name = ort_sess.get_inputs()[0].name
+    label_name = ort_sess.get_outputs()[0].name
 
-    trace_results_new_02 = trace_m_all_02(trace_results_new_01)
-    trace_results_2 = trace_m_2(trace_results_1)
-    [np.testing.assert_allclose(trace_results_new_02, trace_results_2, rtol=1e-03, atol=1e-05)]
-
-    # [np.testing.assert_allclose(trace_results_all, trace_results_2, rtol=1e-03, atol=1e-05)]
+    my_input, _ = torch.jit._flatten(dummy_input)
+    my_inputs = [to_numpy(inp) for inp in my_input]
 
 print('End')
