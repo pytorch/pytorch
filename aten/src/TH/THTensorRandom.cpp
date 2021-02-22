0aa9743d61d3b03b9a569955c767d166d890b2b1
XiaobingSuper <xiaobing.zhang@intel.com>
2021-02-22 12:16:32 +0800

    remove aliasMultinomial decode from TH and THC
    [ghstack-poisoned]

--- aten/src/TH/THTensorRandom.cpp
+++
@@ -1,8 +0,0 @@
-#include <TH/THTensor.hpp>
-#include <TH/THVector.h>
-
-#include <TH/generic/THTensorRandom.cpp>
-#include <TH/THGenerateAllTypes.h>
-
-#include <TH/generic/THTensorRandom.cpp>
-#include <TH/THGenerateBoolType.h>
