9c15147833fb2d3e722ffb7bafd77d94219b1299
Michael Carilli <mcarilli@gmail.com>
2021-02-23 14:31:42 -0700

    maybe?

--- c10/cuda/CUDAGraphsC10Utils.h
+++ c10/cuda/CUDAGraphsC10Utils.h
@@ -13,7 +13,7 @@ using CaptureId_t = unsigned long long;
 // RAII guard for "cudaStreamCaptureMode", a thread-local value
 // that controls the error-checking strictness of a capture.
 #if CUDA_VERSION >= 11000
-struct TORCH_CUDA_CPP_API CUDAStreamCaptureModeGuard {
+struct C10_CUDA_API CUDAStreamCaptureModeGuard {
   CUDAStreamCaptureModeGuard(cudaStreamCaptureMode desired) {
     strictness_ = desired;
     C10_CUDA_CHECK(cudaThreadExchangeStreamCaptureMode(&strictness_));
