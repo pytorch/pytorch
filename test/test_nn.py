e94a2fcce469c5c96c6358c14a58752e769d609f
Nikita Shulga <nshulga@fb.com>
2021-02-23 18:00:25 -0800

    Update on "`maybe_resize_storage_cpu` to error on negative numbers"
    Current code is written with the assumption that `new_size` is unsigned value,
    and when function is called with negative value it silently returns a nullptr rather than raise an exception.
    Fix abovementioned logic by converting new_size to unsigned type and let cpu_allocator raise exception on negative alloc.
    Add `TestNN.test_adaptive_pooling_size_overflow` to indirecty validate the fix.
    
    Fixes https://github.com/pytorch/pytorch/issues/50960
    
    Differential Revision: [D26607549](https://our.internmc.facebook.com/intern/diff/D26607549)
    
    [ghstack-poisoned]

--- test/test_nn.py
+++ test/test_nn.py
@@ -3816,6 +3816,7 @@ class TestNN(NNTestCase):
                 output = module(input)
                 self.assertEqual(output.size(), (4,) + (2,) * (numel - 1) + (4,))
 
+    @unittest.skipIf(TEST_WITH_UBSAN, "signed integer overflow error with UBSAN")
     def test_adaptive_pooling_size_overflow(self):
         # 0x0x3fffffffffffffff * 2 * 2 = 0xfffffffffffffffc = -4 as int64_t
         # Tensor::numel() return int64_t, so following check that negative allocs are correctly handled
