pytorch/pytorch

commit 2a2cb3af253a94fa69b06fbb38f8fa33f4e17146

Use finer-grained mutexes in TensorPipe RPC agent (#52749)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/52749

TensorPipe has recently changed some implementation details in how it schedules callbacks and this has exposed an issue in the RPC agent. Previously the callbacks of each pipe were executed independently and possibly simultaneously. For safety reasons (especially during shutdown) TensorPipe now synchronizes the pipes and thus invokes one callback at a time. Another characteristic of TensorPipe is that it "hijacks" some user threads to run some callbacks inline (e.g., if a low-level event loop completes an operation while a pipe is already busy, this completion is queued up and the user callback could be invoked later by a different thread, including the user's own thread).

These two effects combined caused a "reentrancy" phenomenon, where calling `context->connect` (formerly on line 850) to create a new client-side pipe could cause invoking a read callback on another pipe. Since we were holding `mutex_` when calling `context->connect`, and we were trying to re-acquire `mutex_` inside the read callback, this lead to a deadlock.

One solution to this problem is using finer-grained mutexes. In particular, introduce a mutex for each outgoing pipe (rather than a global one), which thus becomes the only one we need to acquire inside callbacks. At this point, the old `mutex_` is only guarding the vector of ClientPipes, thus we can rename it and release it earlier.

I also fixed the agent not acquiring any mutex when it set a message to error after a failed write (and also not removing the message from the timeout map).
ghstack-source-id: 122410367

Test Plan: Ran CI in #52677 together with the TensorPipe submodule update.

Reviewed By: mrshenli

Differential Revision: D26636345

fbshipit-source-id: d36da989f2aab51f4acb92d2e81bb15b76088df1

torch/csrc/distributed/rpc/tensorpipe_agent.cpp
@@ -3,6 +3,8 @@
 #ifdef USE_TENSORPIPE
 
 #include <limits>
+#include <tuple>
+#include <utility>
 
 #include <fmt/format.h>
 #include <tensorpipe/tensorpipe.h>
@@ -840,26 +842,34 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
 
   const auto& url = findWorkerURL(toWorkerInfo);
 
-  std::unique_lock<std::mutex> lock(mutex_);
+  decltype(connectedPipes_)::iterator it;
+  {
+    std::unique_lock<std::mutex> lock(connectedPipesMutex_);
 
-  // See if we already have a connection to this address or not
-  auto it = connectedPipes_.find(toWorkerInfo.id_);
-  if (it == connectedPipes_.end()) {
-    std::tie(it, std::ignore) = connectedPipes_.emplace(
-        toWorkerInfo.id_,
-        ClientPipe(context_->connect(
-            url, tensorpipe::PipeOptions().remoteName(toWorkerInfo.name_))));
+    // See if we already have a connection to this address or not
+    it = connectedPipes_.find(toWorkerInfo.id_);
+    if (it == connectedPipes_.end()) {
+      // An instance of ClientPipe cannot be copied or moved as it contains a
+      // mutex, and to force in-place construction in GCC 5 we need piecewise
+      // construction in order to work around an issue.
+      std::tie(it, std::ignore) = connectedPipes_.emplace(
+          std::piecewise_construct,
+          std::forward_as_tuple(toWorkerInfo.id_),
+          std::forward_as_tuple(context_->connect(
+              url, tensorpipe::PipeOptions().remoteName(toWorkerInfo.name_))));
+    }
   }
   ClientPipe& clientPipe = it->second;
-  auto& pendingResponseMessage = clientPipe.pendingResponseMessage_;
 
   auto futureResponseMessage = std::make_shared<AtomicJitFuture>(
       reverseDeviceMaps_.empty() && opts_.deviceMaps.empty());
   uint64_t messageId = nextMessageID_++;
   requestMessage.setId(messageId);
-  pendingResponseMessage[messageId] = futureResponseMessage;
 
-  lock.unlock();
+  {
+    std::unique_lock<std::mutex> lock(clientPipe.mutex_);
+    clientPipe.pendingResponseMessage_[messageId] = futureResponseMessage;
+  }
 
   // Get devices for tensors in the request message. This can throw if device
   // maps are not configured properly for this request.
@@ -928,11 +938,21 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
                          << clientPipe.pipe_->getRemoteName() << ": "
                          << error.what();
           }
-          auto pendingFutIt =
-              clientPipe.pendingResponseMessage_.find(messageId);
-          if (pendingFutIt != clientPipe.pendingResponseMessage_.end()) {
-            markFutureWithError(pendingFutIt->second, error.what());
+          std::shared_ptr<AtomicJitFuture> futureResponseMessage;
+          {
+            std::lock_guard<std::mutex> lock(clientPipe.mutex_);
+            auto pendingFutIt =
+                clientPipe.pendingResponseMessage_.find(messageId);
+            TORCH_INTERNAL_ASSERT(
+                pendingFutIt != clientPipe.pendingResponseMessage_.end(),
+                "message ID ",
+                messageId,
+                " is not recognized");
+            futureResponseMessage = std::move(pendingFutIt->second);
+            clientPipe.pendingResponseMessage_.erase(pendingFutIt);
           }
+          markFutureWithError(futureResponseMessage, error.what());
+          removeFromTimeoutMap(messageId);
           return;
         }
 
@@ -961,7 +981,7 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
                 // error state.
                 decltype(clientPipe.pendingResponseMessage_) pendingMsgs;
                 {
-                  std::lock_guard<std::mutex> lock(mutex_);
+                  std::lock_guard<std::mutex> lock(clientPipe.mutex_);
                   std::swap(clientPipe.pendingResponseMessage_, pendingMsgs);
                   clientPipe.readError_ = true;
                 }
@@ -985,7 +1005,7 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
 
               std::shared_ptr<AtomicJitFuture> futureResponseMessage;
               {
-                std::lock_guard<std::mutex> lock(mutex_);
+                std::lock_guard<std::mutex> lock(clientPipe.mutex_);
                 // A read error will lead all following callbacks to be
                 // invoked with error, and shouldn't reach here.
                 TORCH_INTERNAL_ASSERT(
@@ -1360,13 +1380,10 @@ size_t TensorPipeAgent::timeoutMapSize() {
 }
 
 size_t TensorPipeAgent::numPendingResponses() {
-  size_t totalPending = 0;
-  std::unique_lock<std::mutex> lock(mutex_);
-  for (const auto& entry : connectedPipes_) {
-    totalPending += entry.second.pendingResponseMessage_.size();
-  }
-  return totalPending;
+  std::unique_lock<std::mutex> lock(callCountMutex_);
+  return clientActiveCalls_;
 }
+
 size_t TensorPipeAgent::messageIdToTimeoutMapSize() {
   std::unique_lock<std::mutex> lock(timeoutMapMutex_);
   return messageIdToTimeout_.size();

torch/csrc/distributed/rpc/tensorpipe_agent.h
@@ -336,6 +336,7 @@ class TensorPipeAgent : public RpcAgent {
   struct ClientPipe {
     explicit ClientPipe(std::shared_ptr<tensorpipe::Pipe> pipe) : pipe_(pipe) {}
     std::shared_ptr<tensorpipe::Pipe> pipe_;
+    mutable std::mutex mutex_;
     bool readError_{false};
     // Map from Message Request ID's to corresponding futures.
     std::unordered_map<uint64_t, std::shared_ptr<AtomicJitFuture>>
@@ -348,6 +349,8 @@ class TensorPipeAgent : public RpcAgent {
   ThreadPool threadPool_;
   std::shared_ptr<tensorpipe::Context> context_;
   std::shared_ptr<tensorpipe::Listener> listener_;
+
+  mutable std::mutex connectedPipesMutex_;
   std::unordered_map<worker_id_t, ClientPipe> connectedPipes_;
 
   // Maps keyed on name and id for easy WorkerInfo lookup.
@@ -364,8 +367,7 @@ class TensorPipeAgent : public RpcAgent {
   // group, but probably one day we might want to re-implement them using RPCs.
   const c10::intrusive_ptr<::c10d::ProcessGroup> processGroup_;
 
-  mutable std::mutex mutex_;
-  uint64_t nextMessageID_{0};
+  std::atomic<uint64_t> nextMessageID_{0};
 
   // Metadata used for tracking of whether certain RPCs have timed out or not.
   struct TimeoutMessageMetadata {

commit 976221f546576947ca55ea3cd842ae1ae8f7d9fb

Fix TensorPipe agent trying to double-set error
After https://github.com/pytorch/pytorch/pull/52749 we started seeing an increased flakiness of the TensorPipeDistAutogradTestWithSpawn.test_backward_node_failure_python_udf test, with failures like this one:

https://app.circleci.com/pipelines/github/pytorch/pytorch/277824/workflows/cfcbef5a-544e-43bd-b3b0-ebc7b95134fe/jobs/11145394

https://gist.github.com/lw/a0b48900673b5ae0f5d03aca1e72ffff

The logs are very clear and point to the changes in the error handling code upon a write error. Namely, the bug is triggered when a incoming read fails while there is an outgoing write, in which case the read callback (invoked first) will flush all pending futures, which then causes the write callback (invoked after) to not find the future it's looking for.

In a sense this bug wasn't introduced by https://github.com/pytorch/pytorch/pull/52749, however that PR introduced a check for whether the outgoing message was found, whereas before we would silence such a condition.

A fix for this could be to just resume silencing the error. However, I'm trying to go a bit further: when an outgoing write fails, we know that all subsequent callbacks will fail too, and thus all pending operations should be flushed. Hence we can do so, instead of just trying to flush a single given operation. This allows us to merge the error-handling code of both the read and write paths.

Differential Revision: [D26663448](https://our.internmc.facebook.com/intern/diff/D26663448/)

[ghstack-poisoned]

torch/csrc/distributed/rpc/tensorpipe_agent.cpp
@@ -938,21 +938,7 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
                          << clientPipe.pipe_->getRemoteName() << ": "
                          << error.what();
           }
-          std::shared_ptr<AtomicJitFuture> futureResponseMessage;
-          {
-            std::lock_guard<std::mutex> lock(clientPipe.mutex_);
-            auto pendingFutIt =
-                clientPipe.pendingResponseMessage_.find(messageId);
-            TORCH_INTERNAL_ASSERT(
-                pendingFutIt != clientPipe.pendingResponseMessage_.end(),
-                "message ID ",
-                messageId,
-                " is not recognized");
-            futureResponseMessage = std::move(pendingFutIt->second);
-            clientPipe.pendingResponseMessage_.erase(pendingFutIt);
-          }
-          markFutureWithError(futureResponseMessage, error.what());
-          removeFromTimeoutMap(messageId);
+          handleClientError(clientPipe, error);
           return;
         }
 
@@ -976,23 +962,7 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
                       << clientPipe.pipe_->getRemoteName() << ": "
                       << error.what();
                 }
-                // We may get garbage content in responseMessage upon error.
-                // Flushing all future messages belonging to this pipe due to
-                // error state.
-                decltype(clientPipe.pendingResponseMessage_) pendingMsgs;
-                {
-                  std::lock_guard<std::mutex> lock(clientPipe.mutex_);
-                  std::swap(clientPipe.pendingResponseMessage_, pendingMsgs);
-                  clientPipe.readError_ = true;
-                }
-                std::string errorMsg = error.what();
-                for (auto& p : pendingMsgs) {
-                  markFutureWithError(std::move(p.second), errorMsg);
-
-                  // Remove entry from timeoutMap_.
-                  removeFromTimeoutMap(p.first);
-                }
-
+                handleClientError(clientPipe, error);
                 return;
               }
 
@@ -1009,7 +979,7 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
                 // A read error will lead all following callbacks to be
                 // invoked with error, and shouldn't reach here.
                 TORCH_INTERNAL_ASSERT(
-                    !clientPipe.readError_, "Shouldn't in error state");
+                    !clientPipe.inError_, "Shouldn't be in error state");
                 auto it = clientPipe.pendingResponseMessage_.find(messageId);
                 TORCH_INTERNAL_ASSERT(
                     it != clientPipe.pendingResponseMessage_.end(),
@@ -1040,6 +1010,27 @@ std::shared_ptr<JitFuture> TensorPipeAgent::send(
       deviceMap);
 
   return futureResponseMessage->jitFuture;
+}
+
+void TensorPipeAgent::handleClientError(
+    ClientPipe& clientPipe,
+    const tensorpipe::Error& error) {
+  // When an error occurs on a pipe all pending operations will be aborted and
+  // all callbacks invoked with error, hence we immediately flush all future
+  // messages belonging to this pipe.
+  decltype(clientPipe.pendingResponseMessage_) pendingMsgs;
+  {
+    std::lock_guard<std::mutex> lock(clientPipe.mutex_);
+    std::swap(clientPipe.pendingResponseMessage_, pendingMsgs);
+    clientPipe.inError_ = true;
+  }
+  std::string errorMsg = error.what();
+  for (auto& p : pendingMsgs) {
+    markFutureWithError(std::move(p.second), errorMsg);
+
+    // Remove entry from timeoutMap_.
+    removeFromTimeoutMap(p.first);
+  }
 }
 
 void TensorPipeAgent::pollTimeoutRpcs() {

torch/csrc/distributed/rpc/tensorpipe_agent.h
@@ -337,7 +337,7 @@ class TensorPipeAgent : public RpcAgent {
     explicit ClientPipe(std::shared_ptr<tensorpipe::Pipe> pipe) : pipe_(pipe) {}
     std::shared_ptr<tensorpipe::Pipe> pipe_;
     mutable std::mutex mutex_;
-    bool readError_{false};
+    bool inError_{false};
     // Map from Message Request ID's to corresponding futures.
     std::unordered_map<uint64_t, std::shared_ptr<AtomicJitFuture>>
         pendingResponseMessage_;
@@ -410,6 +410,11 @@ class TensorPipeAgent : public RpcAgent {
     return std::chrono::time_point_cast<std::chrono::milliseconds>(
         std::chrono::steady_clock::now() + timeout);
   }
+
+  // Handle error on an outgoing pipe
+  void handleClientError(
+      ClientPipe& clientPipe,
+      const tensorpipe::Error& error);
 
   // This is a generic struct for capturing Time-Series Metrics. It keeps a
   // running sum and count of data points (observations), and can return an