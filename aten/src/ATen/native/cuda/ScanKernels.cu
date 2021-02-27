4c3180f4b57d3f38c89eaf5c0eb30d7dd49cc24e
Natalia Gimelshein <ngimel@fb.com>
2021-02-26 22:56:06 -0800

    try fixing build

--- aten/src/ATen/native/cuda/ScanKernels.cu
+++ aten/src/ATen/native/cuda/ScanKernels.cu
@@ -2,6 +2,7 @@
 #include <ATen/cuda/NumericLimits.cuh>
 #include <ATen/Dispatch.h>
 #include <ATen/TensorUtils.h>
+#include <ATen/NumericUtils.h>
 #include <c10/util/accumulate.h>
 #include <THC/THCGeneral.h>
 #include <THC/THCNumerics.cuh>
@@ -560,8 +561,8 @@ Tensor& _logcumsumexp_out_cuda(Tensor& result, const Tensor& self, int64_t dim) 
     self.scalar_type(), "logcumsumexp_cuda", [&]() {
     scalar_t init = -std::numeric_limits<scalar_t>::infinity();
     auto log_add_exp = [] C10_HOST_DEVICE (const scalar_t x, const scalar_t y) -> scalar_t {
-      scalar_t min = std::isnan(y) ? y : std::min(x,y); //std::min returns first arg if one of the args is nan
-      scalar_t max = std::isnan(y) ? y : std::max(x,y); //std::max returns first arg if one of the args is nan
+      scalar_t min = at::_isnan(y) ? y : std::min<scalar_t>(x,y); //std::min returns first arg if one of the args is nan
+      scalar_t max = at::_isnan(y) ? y : std::max<scalar_t>(x,y); //std::max returns first arg if one of the args is nan
       if (min != max) {
       // nan will be propagated here
           return ::log1p(std::exp(min - max)) + std::max(x, y);
