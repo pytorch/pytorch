0b720f8fddf2bc778e180b5adfe15a8a3ce8c972
Shubham Bhokare <sbhokare@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-03-03 13:09:03 +0000

    Fixed flake issues

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -1990,14 +1990,14 @@ def repeat_interleave(g, self, repeats, dim=None):
         raise RuntimeError("repeats must be 0-dim or 1-dim tensor")
 
     final_splits = list()
-    r_splits = g.op("Split", repeats, split_i=[1]*reps, axis_i=0, outputs=reps)
-    i_splits = g.op("Split", input, split_i=[1]*reps, axis_i=dim, outputs=reps)
+    r_splits = g.op("Split", repeats, split_i=[1] * reps, axis_i=0, outputs=reps)
+    i_splits = g.op("Split", input, split_i=[1] * reps, axis_i=dim, outputs=reps)
     input_sizes[dim], input_sizes_temp[dim] = -1, 1
     for idx, r_split in enumerate(r_splits):
         i_split = unsqueeze(g, i_splits[idx], dim + 1)
-        r_concat = [g.op("Constant", value_t=torch.LongTensor(input_sizes_temp[:dim+1])), 
+        r_concat = [g.op("Constant", value_t=torch.LongTensor(input_sizes_temp[:dim + 1])), 
                     r_split, 
-                    g.op("Constant", value_t=torch.LongTensor(input_sizes_temp[dim+1:]))]
+                    g.op("Constant", value_t=torch.LongTensor(input_sizes_temp[dim + 1:]))]
         r_concat = g.op("Concat", *r_concat, axis_i=0)
         i_split = expand(g, i_split, r_concat, None)
         i_split = reshape(g, i_split, g.op("Constant", value_t=torch.LongTensor(input_sizes)))
