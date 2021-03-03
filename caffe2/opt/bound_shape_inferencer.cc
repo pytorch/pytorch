3a03415a506ec729248f8123880b2f09cfbb9201
Yinghai Lu <yinghai@fb.com>
2021-03-03 09:17:19 -0800

    Add shape inference for SparseLengthsSumSparseLookup
    Summary: Just copy whatever corresponding input shape info. Or we will miss the shape info of output of SparseLengthsSumSparseLookup, which will be infered as the input of downstream SparseLengthsSum op, whose int64/int32 mode is undetermined.
    
    Test Plan:
    ```
    buck test caffe2/caffe2/opt:bound_shape_inference_test
    ```
    
    Reviewed By: khabinov, ChunliF
    
    Differential Revision: D26769226
    
    fbshipit-source-id: 4032bc4643a125095a48fa8c23ca4ebcf26dc29c

--- caffe2/opt/bound_shape_inferencer.cc
+++ caffe2/opt/bound_shape_inferencer.cc
@@ -167,6 +167,8 @@ void BoundShapeInferencer::InferOps(
     InferUnPackRecords(op);
   } else if (op.type() == "Tile") {
     InferTile(op);
+  } else if (op.type() == "SparseLengthsSumSparseLookup") {
+    InferSparseLengthsSumSparseLookup(op);
   } else {
     InferCommonOp(op);
   }
@@ -353,6 +355,37 @@ void BoundShapeInferencer::InferLengthsRangeFill(const OperatorDef& op) {
       TensorProto_DataType_INT32,
       false);
   current_dim_type_ = TensorBoundShape_DimType_BATCH_OF_FEATURE_MAX_DEFAULT;
+}
+
+void BoundShapeInferencer::InferSparseLengthsSumSparseLookup(
+    const OperatorDef& op) {
+  CAFFE_ENFORCE_GT(
+      op.input_size(),
+      2,
+      "SparseLengthsSumSparseLookup must have more than 2 input");
+  CAFFE_ENFORCE_GT(
+      op.output_size(),
+      1,
+      "SparseLengthsSumSparseLookup must have more than 1 output");
+  if (shape_info_.find(op.input(2)) != shape_info_.end()) {
+    LOG(WARNING)
+        << "Shape of COMPRESSED_INDICES_MAPPING input of SparseLengthsSumSparseLookup "
+        << op.input(2) << " needs to be presented";
+  }
+  for (int i = 0; i < 2; ++i) {
+    const auto it = shape_info_.find(op.input(i));
+    if (it != shape_info_.end()) {
+      shape_info_[op.output(i)] = it->second;
+    }
+  }
+  // Handle the weights
+  if (op.input_size() == 4) {
+    CAFFE_ENFORCE_EQ(op.output_size(), 3);
+    const auto it = shape_info_.find(op.input(3));
+    if (it != shape_info_.end()) {
+      shape_info_[op.output(2)] = it->second;
+    }
+  }
 }
 
 void BoundShapeInferencer::InferSparseLengthsSum(const OperatorDef& op) {
