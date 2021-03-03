dedaf5a755c84466a66a9016906082f167f4190d
Shen Li <cs.shenli@gmail.com>
2021-03-03 14:27:42 -0800

    Update on "Use Store collect and verify names in all RPC agents"
    closes #40048
    
    Differential Revision: [D26791524](https://our.internmc.facebook.com/intern/diff/D26791524)
    
    [ghstack-poisoned]

--- torch/_C/_distributed_rpc_testing.pyi
+++ torch/_C/_distributed_rpc_testing.pyi
@@ -1,7 +1,8 @@
-from ._distributed_c10d import ProcessGroup
+from ._distributed_c10d import ProcessGroup, Store
 from ._distributed_rpc import ProcessGroupAgent, ProcessGroupRpcBackendOptions, WorkerInfo
 from typing import List, Dict, overload
 from datetime import timedelta
+
 
 # This module is defined in torch/csrc/distributed/rpc/testing/init.cpp
 
