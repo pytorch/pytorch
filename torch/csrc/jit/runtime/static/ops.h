49a699ef0f7211c61070910e59716e4b4317ab2e
Hao Lu <hlu@fb.com>
2021-03-04 19:17:47 -0800

    [PyPer] More at::empty -> at::detail::empty_cpu
    Summary:
    - Add more variants to `create_empty_from` to take more args, like dtype/layout/device.
    - Clean up stray at::empty uses, mostly in the out variants.
    
    Reviewed By: ajyu
    
    Differential Revision: D26799900
    
    fbshipit-source-id: bee8277a1158e04f3ad3713ecc046395cceda460

--- torch/csrc/jit/runtime/static/ops.h
+++ torch/csrc/jit/runtime/static/ops.h
@@ -66,6 +66,38 @@ inline at::Tensor create_empty_from(const at::Tensor& t) {
       c10::nullopt);
 }
 
+inline at::Tensor create_empty(c10::ScalarType dtype) {
+  return at::detail::empty_cpu(
+      {0}, dtype, c10::nullopt, c10::nullopt, c10::nullopt, c10::nullopt);
+}
+
+inline at::Tensor create_empty_from(
+    const at::Tensor& t,
+    c10::ScalarType dtype) {
+  return at::detail::empty_cpu(
+      {0}, dtype, t.layout(), t.device(), c10::nullopt, c10::nullopt);
+}
+
+inline at::Tensor create_empty_from(const at::Tensor& t, c10::Layout layout) {
+  return at::detail::empty_cpu(
+      {0},
+      c10::typeMetaToScalarType(t.dtype()),
+      layout,
+      t.device(),
+      c10::nullopt,
+      c10::nullopt);
+}
+
+inline at::Tensor create_empty_from(const at::Tensor& t, c10::Device device) {
+  return at::detail::empty_cpu(
+      {0},
+      c10::typeMetaToScalarType(t.dtype()),
+      t.layout(),
+      device,
+      c10::nullopt,
+      c10::nullopt);
+}
+
 inline bool checkResizedDataPtr(at::Tensor& t) {
   auto const prev_data_ptr = t.data_ptr();
   t.resize_({0});
