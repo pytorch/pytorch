894c3a60a0a3e3182b75597f8c29859c026a716b
Chen Lai <chenlai@fb.com>
2021-03-06 12:20:52 -0800

    Update on "[PyTorch][CI] Enable building test_lite_interpreter_runtime unittest in CI (macos)"
    ## Summary
    
    1. Enable building libtorch (lite) in CI (macos)
    2. Run `test_lite_interpreter_runtime` unittest in CI (macos)
    
    ![image](https://user-images.githubusercontent.com/16430979/110189039-b2b8ed00-7dd2-11eb-8fa1-be2d9e23792a.png)
    
    ![image](https://user-images.githubusercontent.com/16430979/110189119-e3008b80-7dd2-11eb-9e80-7c2ae6862468.png)
    
    
    
    Differential Revision: [D26601585](https://our.internmc.facebook.com/intern/diff/D26601585)
    
    [ghstack-poisoned]

--- .jenkins/pytorch/macos-lite-interpreter-build-test.sh
+++ .jenkins/pytorch/macos-lite-interpreter-build-test.sh
@@ -16,16 +16,16 @@ echo "BUILD_LITE_INTERPRETER: ${BUILD_LITE_INTERPRETER}"
 if [ "${BUILD_LITE_INTERPRETER}" == 1 ]; then
     echo "Testing libtorch (lite interpreter)."
 
-    CPP_BUILD="$PWD/../cpp-build"
-    rm -rf "${CPP_BUILD}"
-    mkdir -p "${CPP_BUILD}/caffe2"
+    # CPP_BUILD="$PWD/../cpp-build"
+    # rm -rf "${CPP_BUILD}"
+    # mkdir -p "${CPP_BUILD}/caffe2"
 
     BUILD_LIBTORCH_PY=$PWD/tools/build_libtorch.py
-    pushd "${CPP_BUILD}/caffe2" ... || exit
+    # pushd "${CPP_BUILD}/caffe2" ... || exit
     VERBOSE=1 DEBUG=1 python "${BUILD_LIBTORCH_PY}"
-    popd ... || exit
+    # popd ... || exit
 
-    python tools/download_mnist.py --quiet -d test/cpp/api/mnist
+    # python tools/download_mnist.py --quiet -d test/cpp/api/mnist
 
     # Unfortunately it seems like the test can't load from miniconda3
     # without these paths being set
