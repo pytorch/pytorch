660ad138e566872b384792fbefe52be4b7331ac9
Chen Lai <chenlai@fb.com>
2021-03-06 00:06:52 -0800

    Update on "[PyTorch][CI] Enable building test_lite_interpreter_runtime unittest in CI (macos)"
    ## Summary
    
    1. Enable building libtorch (lite) in CI (macos)
    2. Run `test_lite_interpreter_runtime` unittest in CI (macos)
    
    ![image](https://user-images.githubusercontent.com/16430979/110189039-b2b8ed00-7dd2-11eb-8fa1-be2d9e23792a.png)
    
    ![image](https://user-images.githubusercontent.com/16430979/110189119-e3008b80-7dd2-11eb-9e80-7c2ae6862468.png)
    
    
    
    Differential Revision: [D26601585](https://our.internmc.facebook.com/intern/diff/D26601585)
    
    [ghstack-poisoned]

--- .jenkins/pytorch/macos-lite-interpreter-build-test.sh
+++ .jenkins/pytorch/macos-lite-interpreter-build-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# shellcheck disable=SC2034
+# shellcheck disable=SC1090
 source "$(dirname "${BASH_SOURCE[0]}")/macos-common.sh"
 
 git submodule sync --recursive
@@ -17,13 +17,13 @@ if [ "${BUILD_LITE_INTERPRETER}" == 1 ]; then
     echo "Testing libtorch (lite interpreter)."
 
     CPP_BUILD="$PWD/../cpp-build"
-    rm -rf $CPP_BUILD
-    mkdir -p $CPP_BUILD/caffe2
+    rm -rf "${CPP_BUILD}"
+    mkdir -p "${CPP_BUILD}/caffe2"
 
     BUILD_LIBTORCH_PY=$PWD/tools/build_libtorch.py
-    pushd $CPP_BUILD/caffe2
-    VERBOSE=1 DEBUG=1 python $BUILD_LIBTORCH_PY
-    popd
+    pushd "${CPP_BUILD}/caffe2" ... || exit
+    VERBOSE=1 DEBUG=1 python "${BUILD_LIBTORCH_PY}"
+    popd ... || exit
 
     python tools/download_mnist.py --quiet -d test/cpp/api/mnist
 
@@ -41,11 +41,11 @@ if [ "${BUILD_LITE_INTERPRETER}" == 1 ]; then
     # Copy the unittest binary test_lite_interpreter_runtime to the path
     # where the dynamic library libtorch.dylib locates, otherwise it binary
     # can't find the library
-    cp ${HOME}/project/torch/bin/test_lite_interpreter_runtime ${WORKSPACE_DIR}//miniconda3/lib/python3.7/site-packages/torch/lib
-    ${WORKSPACE_DIR}//miniconda3/lib/python3.7/site-packages/torch/lib/test_lite_interpreter_runtime
+    cp "${HOME}/project/torch/bin/test_lite_interpreter_runtime" "${WORKSPACE_DIR}//miniconda3/lib/python3.7/site-packages/torch/lib"
+    "${WORKSPACE_DIR}//miniconda3/lib/python3.7/site-packages/torch/lib/test_lite_interpreter_runtime"
 
     # Change the permission manually from 755 to 644 to keep git clean
-    chmod 644 ${HOME}/project/.jenkins/pytorch/macos-lite-interpreter-build-test.sh
+    chmod 644 "${HOME}/project/.jenkins/pytorch/macos-lite-interpreter-build-test.sh"
     assert_git_not_dirty
 else
     echo "Skipping libtorch (lite interpreter)."
