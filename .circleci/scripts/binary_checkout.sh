38b2c973634eac19134a3284586002c55b56522b
Eli Uriegas <eliuriegas@fb.com>
2021-03-08 10:27:17 -0800

    DO NOT MERGE, testing new CUDA 10.2 builds
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -61,7 +61,7 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+retry git clone -b fix_cuda_issues -q https://github.com/seemethere/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
