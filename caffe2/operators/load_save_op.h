b9cc603f30ae103a6f81f0536ae40fce63fc878b
Adam Simpkins <simpkins@fb.com>
2021-03-08 19:15:15 -0800

    Update on "[caffe2] use snprintf() instead of sprintf() in the Checkpoint operator"
    Use `snprintf()` to avoid buffer overflows.
    Also only throw an exception on error, instead of crashing the entire
    application.  A failure can occur if the caller supplies an invalid format
    string.
    
    Differential Revision: [D26863355](https://our.internmc.facebook.com/intern/diff/D26863355/)
    
    [ghstack-poisoned]

--- caffe2/operators/load_save_op.h
+++ caffe2/operators/load_save_op.h
@@ -333,7 +333,7 @@ std::string FormatString(const std::string& pattern, Ts... values) {
   // Start with an initial buffer size that is probably enough most of the time.
   std::string buffer(256, '\0');
   auto bytes_written =
-      snprintf(buffer.data(), buffer.size(), pattern.c_str(), values...);
+      snprintf(&buffer[0], buffer.size(), pattern.c_str(), values...);
   if (bytes_written < 0) {
     throw std::runtime_error("FormatString failed");
   }
@@ -341,7 +341,7 @@ std::string FormatString(const std::string& pattern, Ts... values) {
     // Our initial buffer size wasn't enough, resize and run again.
     buffer.resize(bytes_written + 1);
     bytes_written =
-        snprintf(buffer.data(), buffer.size(), pattern.c_str(), values...);
+        snprintf(&buffer[0], buffer.size(), pattern.c_str(), values...);
     if (bytes_written < 0) {
       throw std::runtime_error("FormatString failed");
     }
