49cf8a36e63e4dc1c2340319191ab954033ce92f
Jay Zhang <jiz@microsoft.com>
2021-03-11 00:07:55 +0800

    Output debug information.

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -197,7 +197,9 @@ def _optimize_graph(graph, operator_export_type, _disable_torch_constant_prop=Fa
             input_names = [] if input_names is None else input_names
             dynamic_axes = {} if dynamic_axes is None else dynamic_axes
             torch._C._jit_pass_onnx_set_dynamic_input_shape(graph, dynamic_axes, input_names)
+        print('------aten Graph:', graph)
         graph = torch._C._jit_pass_onnx(graph, operator_export_type)
+        print('------onnx Graph:', graph)
         torch._C._jit_pass_lint(graph)
 
         torch._C._jit_pass_onnx_scalar_type_analysis(graph)
