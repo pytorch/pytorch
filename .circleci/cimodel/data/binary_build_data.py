76f93963b2f5d778fbe2f88c3f927a4dda6860aa
Jane Xu <janeyx@fb.com>
2021-03-10 10:29:13 -0500

    testing wheel package name for mac arm64

--- .circleci/cimodel/data/binary_build_data.py
+++ .circleci/cimodel/data/binary_build_data.py
@@ -44,33 +44,33 @@ LINUX_PACKAGE_VARIANTS = OrderedDict(
 )
 
 CONFIG_TREE_DATA = OrderedDict(
-    linux=(dimensions.GPU_VERSIONS, LINUX_PACKAGE_VARIANTS),
-    macos=([None], OrderedDict(
-        wheel=dimensions.STANDARD_PYTHON_VERSIONS,
-        conda=dimensions.STANDARD_PYTHON_VERSIONS,
-        libtorch=[
-            "3.7",
-        ],
-    )),
+    # linux=(dimensions.GPU_VERSIONS, LINUX_PACKAGE_VARIANTS),
+    # macos=([None], OrderedDict(
+    #     wheel=dimensions.STANDARD_PYTHON_VERSIONS,
+    #     conda=dimensions.STANDARD_PYTHON_VERSIONS,
+    #     libtorch=[
+    #         "3.7",
+    #     ],
+    # )),
     macos_arm64=([None], OrderedDict(
         wheel=[
             "3.8",
         ],
-        conda=[
-            "3.8",
-        ],
+        # conda=[
+        #     "3.8",
+        # ],
     )),
     # Skip CUDA-9.2 builds on Windows
-    windows=(
-        [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
-        OrderedDict(
-            wheel=dimensions.STANDARD_PYTHON_VERSIONS,
-            conda=dimensions.STANDARD_PYTHON_VERSIONS,
-            libtorch=[
-                "3.7",
-            ],
-        )
-    ),
+    # windows=(
+    #     [v for v in dimensions.GPU_VERSIONS if v not in ['cuda92'] + dimensions.ROCM_VERSION_LABELS],
+    #     OrderedDict(
+    #         wheel=dimensions.STANDARD_PYTHON_VERSIONS,
+    #         conda=dimensions.STANDARD_PYTHON_VERSIONS,
+    #         libtorch=[
+    #             "3.7",
+    #         ],
+    #     )
+    # ),
 )
 
 # GCC config variants:
