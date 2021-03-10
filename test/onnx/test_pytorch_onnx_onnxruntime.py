d944ead831b59d4b96f434b208e596c028db41e1
hwangdeyu <deyhuang@qq.com>
2021-03-11 00:11:42 +0800

    fix different data types comments

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -5321,6 +5321,18 @@ class TestONNXRuntime(unittest.TestCase):
         y = torch.arange(1, 4)
         self.run_test(Outer(), input=(x, y))
 
+        x = torch.arange(1, 6).to(dtype=torch.float32)
+        y = torch.arange(1, 4).to(dtype=torch.long)
+        self.run_test(Outer(), input=(x, y))
+
+        x = torch.arange(2, 5).to(dtype=torch.float32)
+        y = torch.arange(2, 4).to(dtype=torch.float64)
+        self.run_test(Outer(), input=(x, y))
+
+        x = torch.arange(3, 6).to(dtype=torch.int32)
+        y = torch.arange(4, 7).to(dtype=torch.long)
+        self.run_test(Outer(), input=(x, y))
+
     @skipIfUnsupportedMinOpsetVersion(12)
     def test_einsum(self):
         class EinsumModelBatchDiagonal(torch.nn.Module):
--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -16,6 +16,9 @@ def einsum(g, equation, tensor_list):
 
 @parse_args('v', 'v')
 def outer(g, input, other):
+    # make sure to cast other to self's type
+    if other.type().scalarType() != input.type().scalarType():
+        other = g.op("Cast", other, to_i=sym_help.cast_pytorch_to_onnx[input.type().scalarType()])
     return g.op("Einsum", input, other, equation_s='i,j->ij')
 
 @parse_args('v', 'f', 'i')
