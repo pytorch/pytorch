d973723d111c75400840569c8ca028d7bb57a06d
wayi <wayi@devgpu227.prn2.facebook.com>
2021-03-11 23:50:10 -0800

    [Model Averaging] Provide some util functions for model averaging
    All the code under model_averaging directory will be purely experimental at this moment.
    
    Starting with some util functions.
    
    Differential Revision: [D27004887](https://our.internmc.facebook.com/intern/diff/D27004887/)
    
    [ghstack-poisoned]

---
+++ torch/distributed/algorithms/model_averaging/utils.py
@@ -0,0 +1,49 @@
+import logging
+
+import torch
+import torch.distributed as dist
+
+
+def _init_per_machine_process_groups(
+    num_gpus_per_machine: int,
+    process_group: dist.ProcessGroup = dist.group.WORLD,
+    backend: str = "nccl",
+):
+    """
+    Initalizes all the per-machine process groups, even if only one group will be eventually used.
+    This is a requirement of :meth:`torch.distributed.new_group` API.
+    Returns the per-machine process group that contains the current rank.
+    """
+    for machine_rank in range(
+        dist.get_world_size(group=process_group) // num_gpus_per_machine
+    ):
+        global_rank = dist.get_rank(group=process_group)
+        start_rank = machine_rank * num_gpus_per_machine
+        end_rank = start_rank + num_gpus_per_machine
+        per_machine_ranks = list(range(start_rank, end_rank))
+        per_machine_process_group = dist.new_group(per_machine_ranks, backend=backend)
+        if global_rank >= start_rank and global_rank < end_rank:
+            logging.info(
+                "Global rank {} is assigned to per-machine process group {}".format(
+                    global_rank, per_machine_ranks
+                )
+            )
+            return per_machine_process_group
+
+
+def _average_parameters(
+    model: torch.nn.Module, process_group: dist.ProcessGroup = dist.group.WORLD
+):
+    """
+    Averages all the parameters of a given model.
+    For allreduce efficiency, all the parameters are flattened into a contiguous buffer.
+    Thus, it requires extra memory of the same size as the model’s parameters.
+    """
+    flat_params = torch.cat([p.data.view(-1) for p in model.parameters()])
+    dist.all_reduce(flat_params, group=process_group)
+    flat_params /= float(dist.get_world_size(group=process_group))
+
+    offset = 0
+    for p in model.parameters():
+        p.data = flat_params[offset : offset + p.numel()].view_as(p)
+        offset += p.numel()
