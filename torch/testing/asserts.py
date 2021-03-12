28cbde6a4b4189e6d266cf0672351c01efcc20e1
Philip Meier <github.pmeier@posteo.de>
2021-03-12 11:59:02 +0100

    a, b = tensor1, tensor2

--- torch/testing/asserts.py
+++ torch/testing/asserts.py
@@ -22,8 +22,8 @@ class UsageWarning(Warning):
     pass
 
 
-def _get_default_rtol_and_atol(tensor1: torch.Tensor, tensor2: torch.Tensor) -> Tuple[float, float]:
-    return _TestCase()._getDefaultRtolAndAtol(tensor1.dtype, tensor2.dtype)
+def _get_default_rtol_and_atol(a: torch.Tensor, b: torch.Tensor) -> Tuple[float, float]:
+    return _TestCase()._getDefaultRtolAndAtol(a.dtype, b.dtype)
 
 
 def _hide_internal_traceback(fn):
@@ -47,9 +47,9 @@ DEFAULT_CHECKED_ATTRIBUTES = (
 )
 
 
-def _compare_tensors_meta(tensor1: torch.Tensor, tensor2: torch.Tensor, **attributes: bool) -> None:
-    if not (isinstance(tensor1, torch.Tensor) and isinstance(tensor2, torch.Tensor)):
-        raise AssertionError(f"Both inputs have to be tensors, but got {type(tensor1)} and {type(tensor2)} instead.")
+def _compare_tensors_meta(a: torch.Tensor, b: torch.Tensor, **attributes: bool) -> None:
+    if not (isinstance(a, torch.Tensor) and isinstance(b, torch.Tensor)):
+        raise AssertionError(f"Both inputs have to be tensors, but got {type(a)} and {type(b)} instead.")
 
     for attr in DEFAULT_CHECKED_ATTRIBUTES:
         attributes.setdefault(attr, True)
@@ -59,49 +59,49 @@ def _compare_tensors_meta(tensor1: torch.Tensor, tensor2: torch.Tensor, **attrib
             continue
 
         try:
-            val1 = getattr(tensor1, attr)
-            val2 = getattr(tensor2, attr)
-            if val1 != val2:
-                raise AssertionError(f"The values for attribute '{attr}' do not match: {val1} != {val2}.")
+            val_a = getattr(a, attr)
+            val_b = getattr(b, attr)
+            if val_a != val_b:
+                raise AssertionError(f"The values for attribute '{attr}' do not match: {val_a} != {val_b}.")
         except AttributeError as error:
             raise UsageError(f"At least one of the inputs does not have an attribute '{attr}'.") from error
 
 
 def _compare_tensors(
-    tensor1: torch.Tensor,
-    tensor2: torch.Tensor,
+    a: torch.Tensor,
+    b: torch.Tensor,
     asserter: Callable[[torch.Tensor, torch.Tensor], None],
     **strict_attributes: bool,
 ) -> None:
-    if any(tensor.dtype in (torch.complex32, torch.complex64, torch.complex128) for tensor in (tensor1, tensor2)):
+    if any(tensor.dtype in (torch.complex32, torch.complex64, torch.complex128) for tensor in (a, b)):
         raise UsageError("Comparison for complex tensors is not supported yet.")
-    if any(tensor.is_quantized for tensor in (tensor1, tensor2)):
+    if any(tensor.is_quantized for tensor in (a, b)):
         raise UsageError("Comparison for quantized tensors is not supported yet.")
-    if any(tensor.is_sparse for tensor in (tensor1, tensor2)):
+    if any(tensor.is_sparse for tensor in (a, b)):
         raise UsageError("Comparison for sparse tensors is not supported yet.")
 
-    _compare_tensors_meta(tensor1, tensor2, **strict_attributes)
+    _compare_tensors_meta(a, b, **strict_attributes)
 
-    if tensor1.device != tensor2.device:
-        tensor1 = tensor1.cpu()
-        tensor2 = tensor2.cpu()
+    if a.device != b.device:
+        a = a.cpu()
+        b = b.cpu()
 
-    if tensor1.dtype != tensor2.dtype:
-        dtype = _get_comparison_dtype(tensor1, tensor2)
-        tensor1 = tensor1.to(dtype)
-        tensor2 = tensor2.to(dtype)
+    if a.dtype != b.dtype:
+        dtype = _get_comparison_dtype(a, b)
+        a = a.to(dtype)
+        b = b.to(dtype)
 
-    asserter(tensor1, tensor2)
+    asserter(a, b)
 
 
-def _tensors_equal_asserter(tensor1: torch.Tensor, tensor2: torch.Tensor):
-    if not torch.all(torch.eq(tensor1, tensor2)):
+def _tensors_equal_asserter(a: torch.Tensor, b: torch.Tensor):
+    if not torch.all(torch.eq(a, b)):
         raise AssertionError("ADDME")
 
 
 @_hide_internal_traceback
 def assert_tensors_equal(
-    tensor1: torch.Tensor, tensor2: torch.Tensor, *, warn_floating_point: bool = True, **strict_attributes: bool
+    a: torch.Tensor, b: torch.Tensor, *, warn_floating_point: bool = True, **strict_attributes: bool
 ) -> None:
     """Assert that two tensors are equal.
 
@@ -115,8 +115,8 @@ def assert_tensors_equal(
         :func:`torch.promote_types`.
 
     Args:
-        tensor1 (torch.Tensor): First tensor.
-        tensor2 (torch.Tensor): Second tensor.
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
         warn_floating_point (bool): Emit a warning if at least one of the inputs is a floating point tensor.
         **strict_attributes (bool): Set strictness for attribute equality checking. If omitted, this defaults to
             ``shape=True, dtype=True``.
@@ -125,7 +125,7 @@ def assert_tensors_equal(
         AssertionError: If the specified attributes do not match.
         AssertionError: If the tensors are not equal.
     """
-    is_floating_point = tensor1.dtype.is_floating_point or tensor2.dtype.is_floating_point
+    is_floating_point = a.dtype.is_floating_point or b.dtype.is_floating_point
     if is_floating_point and warn_floating_point:
         warnings.warn(
             "Due to the limitations of floating point arithmetic, comparing floating-point tensors for equality is not "
@@ -133,24 +133,22 @@ def assert_tensors_equal(
             UsageWarning,
         )
 
-    _compare_tensors(tensor1, tensor2, _tensors_equal_asserter, **strict_attributes)
+    _compare_tensors(a, b, _tensors_equal_asserter, **strict_attributes)
 
 
-def _tensors_allclose_asserter(
-    tensor1: torch.Tensor, tensor2: torch.Tensor, rtol: Optional[float], atol: Optional[float]
-) -> None:
-    default_rtol, default_atol = _get_default_rtol_and_atol(tensor1, tensor2)
+def _tensors_allclose_asserter(a: torch.Tensor, b: torch.Tensor, rtol: Optional[float], atol: Optional[float]) -> None:
+    default_rtol, default_atol = _get_default_rtol_and_atol(a, b)
     rtol = rtol if rtol is not None else default_rtol
     atol = atol if atol is not None else default_atol
 
-    if not torch.allclose(tensor1, tensor2, rtol=rtol, atol=atol):
+    if not torch.allclose(a, b, rtol=rtol, atol=atol):
         raise AssertionError("ADDME")
 
 
 @_hide_internal_traceback
 def assert_tensors_allclose(
-    tensor1: torch.Tensor,
-    tensor2: torch.Tensor,
+    a: torch.Tensor,
+    b: torch.Tensor,
     *,
     rtol: Optional[float] = None,
     atol: Optional[float] = None,
@@ -168,8 +166,8 @@ def assert_tensors_allclose(
         :func:`torch.promote_types`.
 
     Args:
-        tensor1 (torch.Tensor): First tensor.
-        tensor2 (torch.Tensor): Second tensor.
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
         rtol (Optional[float]): Relative tolerance. If omitted, a default value is determined by the ``dtype`` of the
             tensors.
         atol (Optional[float]): Absolute tolerance. If omitted, a default value is determined by the ``dtype`` of the
@@ -181,6 +179,4 @@ def assert_tensors_allclose(
         AssertionError: If the specified attributes do not match.
         AssertionError: If the tensors are not equal up to the desired precision.
     """
-    _compare_tensors(
-        tensor1, tensor2, functools.partial(_tensors_allclose_asserter, rtol=rtol, atol=atol), **strict_attributes
-    )
+    _compare_tensors(a, b, functools.partial(_tensors_allclose_asserter, rtol=rtol, atol=atol), **strict_attributes)
