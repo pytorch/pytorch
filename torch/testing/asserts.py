94f588c1ef726f497d2e143b960ae184ba67a9e1
Philip Meier <github.pmeier@posteo.de>
2021-03-15 11:46:16 +0100

    switch to explicit checks for tensor attributes

--- torch/testing/asserts.py
+++ torch/testing/asserts.py
@@ -2,7 +2,7 @@ import functools
 import inspect
 import sys
 import warnings
-from typing import Callable, Optional, Tuple
+from typing import Any, Callable, Optional, Tuple
 
 import torch
 from torch.testing._internal.common_utils import get_comparison_dtype as _get_comparison_dtype, TestCase as _TestCase
@@ -24,7 +24,10 @@ class UsageWarning(Warning):
 
 
 def _get_default_rtol_and_atol(a: torch.Tensor, b: torch.Tensor) -> Tuple[float, float]:
-    return _TestCase()._getDefaultRtolAndAtol(a.dtype, b.dtype)
+    try:
+        return _TestCase()._getDefaultRtolAndAtol(a.dtype, b.dtype)
+    except KeyError:
+        return 0.0, 0.0
 
 
 def _hide_internal_traceback_pytest(fn):
@@ -54,40 +57,148 @@ def _hide_internal_traceback_pytest(fn):
     return wrapper
 
 
-# These attributes will be checked by default in every call to assert_tensors_(equal|allclose) and
-# assert_(equal|allclose) if the inputs are tensors.
-DEFAULT_CHECKED_ATTRIBUTES = (
-    "shape",
-    "dtype",
-)
+def _assert_are_tensors(a: Any, b: Any) -> None:
+    """Asserts that both inputs are tensors.
 
+    Args:
+        a (Any): First input.
+        b (Any): Second input.
 
-def _compare_tensors_meta(a: torch.Tensor, b: torch.Tensor, **attributes: bool) -> None:
+    Raises:
+        AssertionError: If :attr:`a` or :attr:`b` is not a :class:`~torch.Tensor`.
+    """
     if not (isinstance(a, torch.Tensor) and isinstance(b, torch.Tensor)):
         raise AssertionError(f"Both inputs have to be tensors, but got {type(a)} and {type(b)} instead.")
 
-    for attr in DEFAULT_CHECKED_ATTRIBUTES:
-        attributes.setdefault(attr, True)
 
-    for attr, check in attributes.items():
-        if not check:
-            continue
+def _assert_attributes_equal(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    /,
+    *,
+    check_device: bool = True,
+    check_dtype: bool = True,
+    check_stride: bool = True,
+) -> None:
+    """Asserts that attributes of two tensors match.
 
-        try:
-            val_a = getattr(a, attr)
-            val_b = getattr(b, attr)
-            if val_a != val_b:
-                raise AssertionError(f"The values for attribute '{attr}' do not match: {val_a} != {val_b}.")
-        except AttributeError as error:
-            raise UsageError(f"At least one of the inputs does not have an attribute '{attr}'.") from error
+    Always checks the :attr:`~torch.Tensor.shape`. Other checks are optional and can be disabled.
+
+    Args:
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
+        check_device (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` live in the same
+            :attr:`~torch.Tensor.device` memory.
+        check_dtype (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :attr:`~torch.Tensor.dtype`.
+        check_stride (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :meth:`~torch.Tensor.stride`.
+
+    Raises:
+        AssertionError: If :attr:`a` and :attr:`b` do not have the same :attr:`~torch.Tensor.shape`.
+        AssertionError: If :attr:`check_device`, but :attr:`a` and :attr:`b` do not live in the same
+            :attr:`~torch.Tensor.device` memory.
+        AssertionError: If :attr:`check_dtype`, but :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`.
+        AssertionError: If :attr:`check_stride`, but :attr:`a` and :attr:`b` do not have the same
+            :meth:`~torch.Tensor.stride`.
+    """
+    msg_fmtstr = "The values for attribute '{}' do not match: {} != {}."
+
+    if a.shape != b.shape:
+        raise AssertionError(msg_fmtstr.format("shape", a.shape, b.shape))
+
+    if check_device and a.device != b.device:
+        raise AssertionError(msg_fmtstr.format("device", a.device, b.device))
+
+    if check_dtype and a.dtype != b.dtype:
+        raise AssertionError(msg_fmtstr.format("dtype", a.dtype, b.dtype))
+
+    if check_stride and a.stride() != b.stride():
+        raise AssertionError(msg_fmtstr.format("stride()", a.stride(), b.stride()))
+
+
+def _assert_values_equal(a: torch.Tensor, b: torch.Tensor):
+    """Asserts that the values of two tensors are bitwise equal.
+
+    Args:
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
+
+    Raises:
+         AssertionError: If the values of :attr:`a` and :attr:`b` are not bitwise equal.
+
+    .. seealso::
+
+        Internally :func:`torch.eq` is used to check for bitwise equality.
+    """
+    if not torch.all(torch.eq(a, b)):
+        raise AssertionError("ADDME")
+
+
+def _assert_values_allclose(
+    a: torch.Tensor, b: torch.Tensor, /, *, rtol: Optional[float] = None, atol: Optional[float] = None
+) -> None:
+    """Asserts that the values of two tensors are equal up to a desired precision.
+
+    Args:
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
+        rtol (Optional[float]): Relative tolerance. If omitted, a default value based on the
+            :attr:`~torch.Tensor.dtype` is selected with the table from :func:`assert_tensors_allclose`.
+        atol (Optional[float]): Absolute tolerance. If omitted, a default value based on the
+            :attr:`~torch.Tensor.dtype` is selected with the table from :func:`assert_tensors_allclose`.
+
+    Raises:
+         AssertionError: If the values of :attr:`a` and :attr:`b` are equal up to the desired precision.
+
+    .. seealso::
+
+        Internally :func:`torch.allclose` is used to check for bitwise equality.
+    """
+    default_rtol, default_atol = _get_default_rtol_and_atol(a, b)
+    rtol = rtol if rtol is not None else default_rtol
+    atol = atol if atol is not None else default_atol
+
+    if not torch.allclose(a, b, rtol=rtol, atol=atol):
+        raise AssertionError("ADDME")
 
 
 def _compare_tensors(
     a: torch.Tensor,
     b: torch.Tensor,
-    asserter: Callable[[torch.Tensor, torch.Tensor], None],
-    **strict_attributes: bool,
+    /,
+    values_asserter: Callable[[torch.Tensor, torch.Tensor], None],
+    check_device: bool = True,
+    check_dtype: bool = True,
+    check_stride: bool = True,
 ) -> None:
+    """Compare values and attributes and values.
+
+    Args:
+        a (torch.Tensor): First tensor.
+        b (torch.Tensor): Second tensor.
+        values_asserter (Callable[[torch.Tensor, torch.Tensor], None]): Will be called with :attr:`a` and :attr:`a`
+            after the attribute checks. Must raise an :class:`AssertionError` if the values do not match.
+        check_device (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` live in the same
+            :attr:`~torch.Tensor.device` memory.
+        check_dtype (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :attr:`~torch.Tensor.dtype`.
+        check_stride (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :meth:`~torch.Tensor.stride`.
+
+    Raises:
+        AssertionError: If :attr:`a` and :attr:`b` do not have the same :attr:`~torch.Tensor.shape`.
+        AssertionError: If :attr:`check_device`, but :attr:`a` and :attr:`b` do not live in the same
+            :attr:`~torch.Tensor.device` memory.
+        AssertionError: If :attr:`check_dtype`, but :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`.
+        AssertionError: If :attr:`check_stride`, but :attr:`a` and :attr:`b` do not have the same
+            :meth:`~torch.Tensor.stride`.
+        AssertionError: If the values of :attr:`a` and :attr:`b` do not match according to :attr:`values_asserter`.
+        UsageError: If :attr:`a` or :attr:`b` is complex, quantized, or sparse. This is a temporary restriction and
+            will be relaxed in the future.
+    """
     if any(tensor.dtype in (torch.complex32, torch.complex64, torch.complex128) for tensor in (a, b)):
         raise UsageError("Comparison for complex tensors is not supported yet.")
     if any(tensor.is_quantized for tensor in (a, b)):
@@ -95,7 +206,7 @@ def _compare_tensors(
     if any(tensor.is_sparse for tensor in (a, b)):
         raise UsageError("Comparison for sparse tensors is not supported yet.")
 
-    _compare_tensors_meta(a, b, **strict_attributes)
+    _assert_attributes_equal(a, b, check_device=check_device, check_dtype=check_dtype, check_stride=check_stride)
 
     if a.device != b.device:
         a = a.cpu()
@@ -106,12 +217,7 @@ def _compare_tensors(
         a = a.to(dtype)
         b = b.to(dtype)
 
-    asserter(a, b)
-
-
-def _tensors_equal_asserter(a: torch.Tensor, b: torch.Tensor):
-    if not torch.all(torch.eq(a, b)):
-        raise AssertionError("ADDME")
+    values_asserter(a, b)
 
 
 @_hide_internal_traceback_pytest
@@ -120,49 +226,64 @@ def assert_tensors_equal(
     b: torch.Tensor,
     /,
     *,
-    warn_floating_point: bool = True,
-    **strict_attributes: bool,
+    warn_floating_point_dtype: bool = True,
+    check_device: bool = True,
+    check_dtype: bool = True,
+    check_stride: bool = True,
 ) -> None:
-    """Assert that two tensors are equal.
+    """Asserts that the values of two tensors are bitwise equal.
 
-    Before the values are compared, the ``shape`` and the ``dtype`` of the tensors are checked for equality. You can
-    change this behavior with the ``strict_attributes`` parameter.
-
-    Depending on the strictness some additional steps may be performed before the value comparison:
-
-    - If the tensors don't live in the same device memory they are moved to CPU memory.
-    - If the tensors don't have the same ``dtype``, they are converted to the one determined by
-        :func:`torch.promote_types`.
+    Optionally, checks that some attributes of both tensors are equal.
 
     Args:
         a (torch.Tensor): First tensor.
         b (torch.Tensor): Second tensor.
-        warn_floating_point (bool): Emit a warning if at least one of the inputs is a floating point tensor.
-        **strict_attributes (bool): Set strictness for attribute equality checking. If omitted, this defaults to
-            ``shape=True, dtype=True``.
+        warn_floating_point_dtype (bool): If ``True`` (default), emit a warning if :attr:`a` or :attr:`b` is a
+            floating point tensor.
+        check_device (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` live in the same
+            :attr:`~torch.Tensor.device` memory. If this check is disabled **and** :attr:`a` and :attr:`b` do not live
+            in the same memory :attr:`~torch.Tensor.device`, they are moved to the CPU memory.
+        check_dtype (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :attr:`~torch.Tensor.dtype`. If this check is disabled **and** :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`, the comparison :attr:`~torch.Tensor.dtype` is determined by
+            :func:`torch.promote_types`.
+        check_stride (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :meth:`~torch.Tensor.stride`.
 
     Raises:
-        AssertionError: If the specified attributes do not match.
-        AssertionError: If the tensors are not equal.
+        AssertionError: If :attr:`a` and :attr:`b` do not have the same :attr:`~torch.Tensor.shape`.
+        AssertionError: If :attr:`check_device`, but :attr:`a` and :attr:`b` do not live in the same
+            :attr:`~torch.Tensor.device` memory.
+        AssertionError: If :attr:`check_dtype`, but :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`.
+        AssertionError: If :attr:`check_stride`, but :attr:`a` and :attr:`b` do not have the same
+            :meth:`~torch.Tensor.stride`.
+        AssertionError: If the values of :attr:`a` and :attr:`b` are not bitwise equal.
+        UsageError: If :attr:`a` or :attr:`b` is complex, quantized, or sparse. This is a temporary restriction and
+            will be relaxed in the future.
+
+    .. seealso::
+
+        To compare tensors for value proximity, :func:`assert_tensors_allclose` can be used.
     """
+    _assert_are_tensors(a, b)
+
     is_floating_point = a.dtype.is_floating_point or b.dtype.is_floating_point
-    if is_floating_point and warn_floating_point:
+    if is_floating_point and warn_floating_point_dtype:
         warnings.warn(
-            "Due to the limitations of floating point arithmetic, comparing floating-point tensors for equality is not "
-            "recommended. Use the respective 'allclose' variant instead.",
+            "If one tensor has an floating-point dtype, "
+            "it is recommended to use the respective 'allclose' variant instead.",
             UsageWarning,
         )
 
-    _compare_tensors(a, b, _tensors_equal_asserter, **strict_attributes)
-
-
-def _tensors_allclose_asserter(a: torch.Tensor, b: torch.Tensor, rtol: Optional[float], atol: Optional[float]) -> None:
-    default_rtol, default_atol = _get_default_rtol_and_atol(a, b)
-    rtol = rtol if rtol is not None else default_rtol
-    atol = atol if atol is not None else default_atol
-
-    if not torch.allclose(a, b, rtol=rtol, atol=atol):
-        raise AssertionError("ADDME")
+    _compare_tensors(
+        a,
+        b,
+        values_asserter=_assert_values_equal,
+        check_device=check_device,
+        check_dtype=check_dtype,
+        check_stride=check_stride,
+    )
 
 
 @_hide_internal_traceback_pytest
@@ -171,33 +292,88 @@ def assert_tensors_allclose(
     b: torch.Tensor,
     /,
     *,
+    warn_integer_dtypes: bool = True,
     rtol: Optional[float] = None,
     atol: Optional[float] = None,
-    **strict_attributes: bool,
+    check_device: bool = True,
+    check_dtype: bool = True,
+    check_stride: bool = True,
 ) -> None:
-    """Assert that two tensors are equal up to a desired precision.
+    """Asserts that the values of two tensors are equal up to a desired precision.
 
-    Before the values are compared, the ``shape`` and the ``dtype`` of the tensors are checked for equality. You can
-    change this behavior with the ``strict_attributes`` parameter.
-
-    Depending on the strictness some additional steps may be performed before the value comparison:
-
-    - If the tensors don't live in the same device memory they are moved to CPU memory.
-    - If the tensors don't have the same ``dtype``, they are converted to the one determined by
-        :func:`torch.promote_types`.
+    Optionally, checks that some attributes of both tensors are equal.
 
     Args:
         a (torch.Tensor): First tensor.
         b (torch.Tensor): Second tensor.
-        rtol (Optional[float]): Relative tolerance. If omitted, a default value is determined by the ``dtype`` of the
+        warn_integer_dtypes (bool): If ``True`` (default), emit a warning if :attr:`a` and :attr:`b` are integer
             tensors.
-        atol (Optional[float]): Absolute tolerance. If omitted, a default value is determined by the ``dtype`` of the
-            tensors.
-        **strict_attributes (bool): Set strictness for attribute equality checking. If omitted, this defaults to
-            ``shape=True, dtype=True``.
+        rtol (Optional[float]): Relative tolerance. If omitted, a default value based on the
+            :attr:`~torch.Tensor.dtype` is selected with the below table.
+        atol (Optional[float]): Absolute tolerance. If omitted, a default value based on the
+            :attr:`~torch.Tensor.dtype` is selected with the below table.
+        check_device (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` live in the same
+            :attr:`~torch.Tensor.device` memory. If this check is disabled **and** :attr:`a` and :attr:`b` do not live
+            in the same memory :attr:`~torch.Tensor.device`, they are moved to the CPU memory.
+        check_dtype (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :attr:`~torch.Tensor.dtype`. If this check is disabled **and** :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`, the comparison :attr:`~torch.Tensor.dtype` is determined by
+            :func:`torch.promote_types`.
+        check_stride (bool): If ``True`` (default), asserts that both :attr:`a` and :attr:`b` have the same
+            :meth:`~torch.Tensor.stride`.
 
     Raises:
-        AssertionError: If the specified attributes do not match.
-        AssertionError: If the tensors are not equal up to the desired precision.
+        AssertionError: If :attr:`a` and :attr:`b` do not have the same :attr:`~torch.Tensor.shape`.
+        AssertionError: If :attr:`check_device`, but :attr:`a` and :attr:`b` do not live in the same
+            :attr:`~torch.Tensor.device` memory.
+        AssertionError: If :attr:`check_dtype`, but :attr:`a` and :attr:`b` do not have the same
+            :attr:`~torch.Tensor.dtype`.
+        AssertionError: If :attr:`check_stride`, but :attr:`a` and :attr:`b` do not have the same
+            :meth:`~torch.Tensor.stride`.
+        AssertionError: If the values of :attr:`a` and :attr:`b` are equal up to the desired precision.
+        UsageError: If :attr:`a` or :attr:`b` is complex, quantized, or sparse. This is a temporary restriction and
+            will be relaxed in the future.
+
+
+    The following table displays the default ``rtol`` and ``atol`` for floating point :attr:`~torch.Tensor.dtype`'s.
+    For integer :attr:`~torch.Tensor.dtype`'s, ``rtol = atol = 0.0`` is used.
+
+    +===========================+============+==========+
+    | :class:`~torch.dtype`     | ``rtol``   | ``atol`` |
+    +===========================+============+==========+
+    | :attr:`~torch.float16`    | ``1e-3``   | ``1e-5`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.bfloat16`   | ``1.6e-2`` | ``1e-5`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.float32`    | ``1.3e-6`` | ``1e-5`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.float64`    | ``1e-7``   | ``1e-7`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.complex32`  | ``1e-3``   | ``1e-5`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.complex64`  | ``1.3e-6`` | ``1e-5`` |
+    +---------------------------+------------+----------+
+    | :attr:`~torch.complex128` | ``1e-7``   | ``1e-7`` |
+    +---------------------------+------------+----------+
+
+    .. seealso::
+
+        To compare tensors for bitwise equality, :func:`assert_tensors_equal` can be used.
     """
-    _compare_tensors(a, b, functools.partial(_tensors_allclose_asserter, rtol=rtol, atol=atol), **strict_attributes)
+    _assert_are_tensors(a, b)
+
+    are_integer_dtypes = not a.dtype.is_floating_point and not b.dtype.is_floating_point
+    if are_integer_dtypes and warn_integer_dtypes:
+        warnings.warn(
+            "If both tensors have an integer dtype, it is recommended to use the respective 'equal' variant instead.",
+            UsageWarning,
+        )
+
+    _compare_tensors(
+        a,
+        b,
+        values_asserter=functools.partial(_assert_values_allclose, rtol=rtol, atol=atol),
+        check_device=check_device,
+        check_dtype=check_dtype,
+        check_stride=check_stride,
+    )
