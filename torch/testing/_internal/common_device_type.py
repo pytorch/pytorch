9f71770ec4583f545b7ca8e838e3a72aabbffc2c
Guilherme Leobas <gleobas@quansight.com>
2021-03-15 16:48:22 +0000

    Revert "replace --tb=short by --tb=native"
    This reverts commit 52c4e5eaa0305b8f0d45dab8b697ea5f389abfe6.

--- torch/testing/_internal/common_device_type.py
+++ torch/testing/_internal/common_device_type.py
@@ -346,12 +346,6 @@ class CPUTestBase(DeviceTypeTestBase):
     def _should_stop_test_suite(self):
         return False
 
-class MetaTestBase(DeviceTypeTestBase):
-    device_type = 'meta'
-
-    def _should_stop_test_suite(self):
-        return False
-
 class CUDATestBase(DeviceTypeTestBase):
     device_type = 'cuda'
     _do_cuda_memory_leak_check = True
@@ -405,10 +399,8 @@ def get_device_type_test_bases():
                 test_bases.append(CUDATestBase)
         else:
             test_bases.append(CPUTestBase)
-            test_bases.append(MetaTestBase)
     else:
         test_bases.append(CPUTestBase)
-        test_bases.append(MetaTestBase)
         if torch.cuda.is_available():
             test_bases.append(CUDATestBase)
 
