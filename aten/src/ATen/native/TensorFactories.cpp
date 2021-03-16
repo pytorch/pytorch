bf046ee1e42d0d7a53d5993a4a639dad03e60cc5
Wenlei Xie <wxie@fb.com>
2021-03-15 23:13:12 -0700

    Pass Scalar by reference (#53583)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/53583
    
    `Scalar` takes 32 bytes due to `c10::complex<double>`
    requires aligning to 16 bytes. Passing Scalar by reference
    shows about 1% improvements on instruction count.
    
    All the changes in this commit are codemoded except for
    the following 4 files (which code-gen signatures):
    ```
    tools/codegen/api/cpp.py
    tools/codegen/api/native.py
    tools/codegen/api/structured.py
    caffe2/contrib/aten/gen_op.py
    ```
    
    # Codemode
    
    ## Main Step
    
    For the codemod part, here is the main command used:
    ```
    fastmod --extensions h '([a-zA-Z_+]\([^)]*,?\s*)Scalar (\w+)' '${1}const Scalar& ${2}'
    fastmod --extensions h '([a-zA-Z_+]\([^)]*,?\s*)optional<Scalar> (\w+)' '${1}const optional<Scalar>& ${2}'
    fastmod --extensions cpp '([a-zA-Z_+]\([^)]*,?\s*)Scalar (\w+)' '${1}const Scalar& ${2}'
    fastmod --extensions cpp '([a-zA-Z_+]\([^)]*,?\s*)optional<Scalar> (\w+)' '${1}const optional<Scalar>& ${2}'
    ```
    
    As you can tell, it codemods both `Scalar` and `optional<Scalar>`.  Apply these commands iteratively until reaching a fix-point (since one method signature might contain multiple `Scalar` parameter).
    
    In retrospect, excluding `thrid_party` and `torch/csrc/jit` would be a good idea. (I revert it manually later, see https://github.com/pytorch/pytorch/pull/53479 as an reference).
    
    ## Pre-Step
    
    Prior to applying the main command,  as some `Scalar` are presented as `at::Scalar` or `c10::Scalar`, so I codemod some of them in advance. Here is an incomplete list:
    ```
    fastmod --extensions h '([a-zA-Z_+]\([^)]*,?\s*)at::Scalar (\w+)' '${1}const at::Scalar& ${2}'
    fastmod --extensions cpp '([a-zA-Z_+]\([^)]*,?\s*)at::Scalar (\w+)' '${1}const at::Scalar& ${2}'
    fastmod --extensions h '([a-zA-Z_+]\([^)]*,?\s*)c10::optional<Scalar> (\w+)' '${1}const c10::optional<Scalar>& ${2}'
    fastmod --extensions cpp '([a-zA-Z_+]\([^)]*,?\s*)c10::optional<Scalar> (\w+)' '${1}const c10::optional<Scalar>& ${2}'
    ```
    
    ## Fixup
    There are a couple of post codemod fixup. For example, `const Scalar` will be codemoded into `const const Scalar&`. `at:Scalar` will be codemoded into `at::const Scalar&`  (if `Pre-step` is not done comprehensively). Here is an incomplete list:
    ```
    fastmod --extensions cpp 'const const Scalar' 'const Scalar'
    fastmod --extensions h 'const const c10::optional<Scalar>' 'const c10::optional<Scalar>'
    fastmod --extensions cpp 'const const c10::optional<Scalar>' 'const c10::optional<Scalar>'
    fastmod 'at::const Scalar&' 'const at::Scalar&'
    ```
    
    ## Supplementary
    
    `cu` and `mm` files also need to be codemoded, for example:
    
    ```
    fastmod --extensions cu 'at::const Scalar&' 'const at::Scalar&'
    fastmod --extensions mm '([a-zA-Z_+]\([^)]*,?\s*)Scalar (\w+)' '${1}const Scalar& ${2}'
    ```
    
    Function pointers are not codemoded. Here is an incomplete list:
    
    ```
    # Cover case: using index_fill_fn = void(*)(TensorIterator & iter, int64_t dim, int64_t self_dim_size, int64_t self_dim_stride, Scalar source);
    fastmod --extensions h '(void\s*\(\s*\*\s*\)\([^)]*,?\s*)Scalar (\w+)' '${1}const Scalar& ${2}'
    
    # Cover case: using softplus_fn = void (*)(TensorIterator&, Scalar, Scalar);
    fastmod --extensions h '(void\s*\(\s*\*\s*\)\([^)]*,?\s*)Scalar([, \)])' '${1}const Scalar&${2}'
    fastmod --extensions cpp '(void\s*\(\s*\*\s*\)\([^)]*,?\s*)Scalar([, \)])' '${1}const Scalar&${2}'
    fastmod --extensions h '(void\s*\(\s*\*\s*\)\([^)]*,?\s*)optional<Scalar>([, \)])' '${1}const optional<Scalar>&${2}'
    ```
    
    Some corner cases needs to be manually fixed.
    
    ghstack-source-id: 123970306
    
    Test Plan: Imported from OSS
    
    Reviewed By: smessmer
    
    Differential Revision: D26904445
    
    fbshipit-source-id: 8d8a002af4b5125f153a32f03c6956be7ae5671d

--- aten/src/ATen/native/TensorFactories.cpp
+++ aten/src/ATen/native/TensorFactories.cpp
@@ -52,18 +52,18 @@ DEFINE_DISPATCH(polar_stub);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ arange ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Tensor arange(Scalar end, const TensorOptions& options) {
+Tensor arange(const Scalar& end, const TensorOptions& options) {
   return native::arange(/*start=*/0, end, options);
 }
 
-Tensor arange(Scalar start, Scalar end, const TensorOptions& options) {
+Tensor arange(const Scalar& start, const Scalar& end, const TensorOptions& options) {
   return native::arange(start, end, /*step=*/1, options);
 }
 
 Tensor arange(
-    Scalar start,
-    Scalar end,
-    Scalar step,
+    const Scalar& start,
+    const Scalar& end,
+    const Scalar& step,
     const TensorOptions& options) {
   bool set_to_integral_dtype = !options.has_dtype() &&
        // bool inputs are considered integral
@@ -77,11 +77,11 @@ Tensor arange(
   return at::arange_out(result, start, end, step);
 }
 
-Tensor& arange_out(Tensor& result, Scalar end) {
+Tensor& arange_out(Tensor& result, const Scalar& end) {
   return at::arange_out(result, /*start=*/0, end);
 }
 
-Tensor& arange_out(Tensor& result, Scalar start, Scalar end) {
+Tensor& arange_out(Tensor& result, const Scalar& start, const Scalar& end) {
   return at::arange_out(result, start, end, /*step=*/1);
 }
 
@@ -389,7 +389,7 @@ namespace {
 
 // Performs dtype inference for full
 TensorOptions infer_full_options(
-  Scalar fill_value,
+  const Scalar& fill_value,
   const TensorOptions& options) {
 
   if (!options.has_dtype()) {
@@ -412,7 +412,7 @@ TensorOptions infer_full_options(
 
 } // anonymous namespace
 
-Tensor full(IntArrayRef size, Scalar fill_value, const TensorOptions& options) {
+Tensor full(IntArrayRef size, const Scalar& fill_value, const TensorOptions& options) {
   TORCH_CHECK(options.layout() != kSparse,
     "full(...) is not implemented for sparse layout");
 
@@ -420,7 +420,7 @@ Tensor full(IntArrayRef size, Scalar fill_value, const TensorOptions& options) {
   return result.fill_(fill_value);
 }
 
-Tensor& full_out(Tensor& result, IntArrayRef size, Scalar fill_value) {
+Tensor& full_out(Tensor& result, IntArrayRef size, const Scalar& fill_value) {
   TORCH_CHECK(!result.is_sparse(),
     "full(...) is not implemented for sparse layout");
 
@@ -430,7 +430,7 @@ Tensor& full_out(Tensor& result, IntArrayRef size, Scalar fill_value) {
 
 Tensor full_like(
     const Tensor& self,
-    Scalar fill_value,
+    const Scalar& fill_value,
     const TensorOptions& options,
     c10::optional<c10::MemoryFormat> optional_memory_format) {
   auto result = at::empty_like(self, options, optional_memory_format);
@@ -440,7 +440,7 @@ Tensor full_like(
 Tensor new_full(
     const Tensor& self,
     IntArrayRef size,
-    Scalar fill_value,
+    const Scalar& fill_value,
     const TensorOptions& options
     ) {
   return at::full(size, fill_value, self.options().merge_in(options));
@@ -448,8 +448,8 @@ Tensor new_full(
 
 namespace {
 TensorOptions linspace_logspace_infer_options(
-    Scalar start,
-    Scalar end,
+    const Scalar& start,
+    const Scalar& end,
     const TensorOptions& options) {
   auto result_options = options;
   if (start.isComplex() || end.isComplex()) {
@@ -475,8 +475,8 @@ TensorOptions linspace_logspace_infer_options(
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ linspace ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Tensor linspace(
-    Scalar start,
-    Scalar end,
+    const Scalar& start,
+    const Scalar& end,
     c10::optional<int64_t> steps,
     const TensorOptions& options) {
   const auto steps_ = steps.value_or(100);
@@ -489,8 +489,8 @@ Tensor linspace(
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ logspace ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Tensor logspace(
-    Scalar start,
-    Scalar end,
+    const Scalar& start,
+    const Scalar& end,
     c10::optional<int64_t> steps,
     double base,
     const TensorOptions& options) {
@@ -521,7 +521,7 @@ Tensor ones_like(
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ scalar_tensor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Tensor scalar_tensor(Scalar s, const TensorOptions& options) {
+Tensor scalar_tensor(const Scalar& s, const TensorOptions& options) {
   if (options.device() == at::kCPU) {
     // This is a fast track to skip device dispatch for making scalar tensor on CPU.
     // See https://github.com/pytorch/pytorch/pull/29915 for more detailed perf
@@ -741,17 +741,17 @@ Tensor& randperm_out_cpu(Tensor& result, int64_t n, c10::optional<Generator> gen
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ range ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Tensor range(
-    Scalar start,
-    Scalar end,
-    Scalar step,
+    const Scalar& start,
+    const Scalar& end,
+    const Scalar& step,
     const TensorOptions& options) {
   Tensor result = at::empty({0}, options);
   return at::range_out(result, start, end, step);
 }
 
 Tensor range(
-    Scalar start,
-    Scalar end,
+    const Scalar& start,
+    const Scalar& end,
     const TensorOptions& options) {
   return at::native::range(start, end, 1, options);
 }
@@ -1125,7 +1125,7 @@ Tensor clone(const Tensor& src, c10::optional<c10::MemoryFormat> optional_memory
 
 Tensor full(
     IntArrayRef size,
-    Scalar fill_value,
+    const Scalar& fill_value,
     optional<DimnameList> names,
     const TensorOptions& options) {
 
