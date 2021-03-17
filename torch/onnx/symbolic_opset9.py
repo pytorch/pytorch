95c64a89fccb3a424cb8fb47bf6d5526af3ec8e2
whiteking64 <whiteking64@gmail.com>
2021-03-17 15:00:59 +0900

    add hardsigmoid to opset 9

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -1876,6 +1876,20 @@ def hardswish(g, self):
     hardtanh_ = g.op("Div", hardtanh_, g.op('Constant', value_t=torch.tensor(6, dtype=torch.float)))
     return g.op("Mul", self, hardtanh_)
 
+
+@parse_args('v')
+def hardsigmoid(g, self):
+    input_ = g.op("Div", self, g.op('Constant', value_t=torch.tensor(3, dtype=torch.float)))
+    hardsigmoid_ = sym_help._hardtanh_helper(
+        g,
+        input_,
+        g.op('Constant', value_t=torch.tensor(-1, dtype=torch.float)),
+        g.op('Constant', value_t=torch.tensor(1, dtype=torch.float))
+    )
+    hardsigmoid_ = g.op("Add", hardsigmoid_, g.op('Constant', value_t=torch.tensor(1, dtype=torch.float)))
+    return g.op("Div", hardsigmoid_, g.op('Constant', value_t=torch.tensor(2, dtype=torch.float)))
+
+
 def alias(g, self):
     return self
 
