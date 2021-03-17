b14b3f48b94e75ffd25a8bad7efedbea06512f1d
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared18551.02.frc2.facebook.com>
2021-03-17 00:02:03 -0700

    2021-03-17 nightly release (b936abd840d8fa3122d3a6166c0d7344d8352901)

--- aten/src/ATen/native/ForeachUtils.h
+++ aten/src/ATen/native/ForeachUtils.h
@@ -64,7 +64,7 @@ void check_foreach_api_restrictions(TensorList tensors1, TensorList tensors2, Te
 // - All tensors must be non-overlapping and dense
 // - Resulting tensor must have the same dtype as the input one
 
-bool will_promote_tensor(const Tensor& tensor, Scalar scalar, bool does_op_promote_integer_inputs_to_float = false) {
+bool will_promote_tensor(const Tensor& tensor, const Scalar& scalar, bool does_op_promote_integer_inputs_to_float = false) {
   // In case of division, integer inputs will result in float
   if (does_op_promote_integer_inputs_to_float) {
     if (at::isIntegralType(tensor.scalar_type(), /*includeBool*/ true)) {
