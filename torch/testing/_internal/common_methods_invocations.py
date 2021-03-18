840dc5212438212cbf8b7f91d559e61d75f81d59
Arindam Roy <rarindam@gmail.com>
2021-03-18 14:51:22 -0700

    [ROCM] Enable bloat16 for dTypes
    Add bfloat16 in the list of dtypes for ROCM
    
    Signed-off-by: Arindam Roy <rarindam@gmail.com>

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1705,8 +1705,7 @@ op_db: List[OpInfo] = [
            # BFloat16 support on CUDA requires CUDA 11 and SM53
            dtypesIfCUDA=floating_types_and(torch.float16, torch.complex64, torch.complex128,
                                            *[torch.bfloat16] if CUDA11OrLater else []),
-           dtypesIfROCM=floating_types_and(torch.float16, torch.complex64, torch.complex128,
-                                           *[torch.bfloat16] if CUDA11OrLater else []),
+           dtypesIfROCM=floating_types_and(torch.float16, torch.complex64, torch.complex128, torch.bfloat16),
            assert_autodiffed=True,
            autodiff_nonfusible_nodes=['aten::add', 'aten::mm'],
            skips=(
