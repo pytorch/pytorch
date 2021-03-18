485174b24222b05024dc1e36134b14453f04255e
anjali411 <chourdiaanjali123@gmail.com>
2021-03-17 17:49:24 -0700

    Update on "Add JIT support for cmath unary ops"
    This PR adds:
    1. support for the following cmath functions:
         - Power and logarithmic functions (`cmath.{exp, log, log10, sqrt}`) 
         - Trigonometric functions (`cmath.{sin, cos, tan, asin, acos, atan}`)
         - Hyperbolic functions (`cmath.{sinh, cos, tanh, asinh, acosh, atanh}`)
         - `cmath.phase`
    
    Future work:
    1. support
        - `cmath.{polar, rect}` 
        - classification functions (`cmath.{isfinite, isnan, isinf, isclose}`)
        - constants (`cmath.{pi, e, inf, nan, infj, nanj}`)
    
    [ghstack-poisoned]

--- test/jit/test_complex.py
+++ test/jit/test_complex.py
@@ -115,7 +115,8 @@ class TestComplex(JitTestCase):
                            .format(func_name=func_name, a=a, res_python=res_python, res_script=res_script))
                     self.assertEqual(res_python, res_script, msg=msg)
 
-        unary_ops = ['log', 'log10', 'sqrt', 'exp', 'sin', 'cos', 'asin', 'acos', 'atan', 'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh']
+        unary_ops = ['log', 'log10', 'sqrt', 'exp', 'sin', 'cos', 'asin', 'acos', 'atan', 'sinh', 'cosh',
+                     'tanh', 'asinh', 'acosh', 'atanh']
         for op in unary_ops:
             checkMath(op, 1)
 
