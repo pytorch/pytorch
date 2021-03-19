c34b0b98e84e4d82f3e9ee8a964888e4178fdacb
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-18 17:08:28 -0700

    save

--- aten/src/ATen/native/cuda/TensorFactories.cu
+++ aten/src/ATen/native/cuda/TensorFactories.cu
@@ -97,6 +97,8 @@ Tensor empty_strided_cuda(IntArrayRef size, IntArrayRef stride, c10::optional<Sc
 //
 // Reference
 // [1] https://arxiv.org/abs/xxxxx.xxxx
+// ^^^^ I uploaded that doc to arxiv, and it will be public next Monday,
+// I will wait and update the link here.
 Tensor& randperm_out_cuda(Tensor& result, int64_t n, c10::optional<Generator> generator) {
   TORCH_CHECK(n >= 0, "n must be non-negative, got", n);
   TORCH_CHECK(!generator.has_value() || (generator.has_value() && result.device() == generator->device()), "Expected a '", result.device(), "' generator device but found '", generator->device(), "'");
