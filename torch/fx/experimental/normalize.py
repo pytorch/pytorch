Update on "[WIP][FX] Fix overload resolution in callsite normalization"
I didn't realize that `inspect.Signature.bind` doesn't check the arguments against type annotations, so roll our own type checking (unfortunately)

Differential Revision: [D27177171](https://our.internmc.facebook.com/intern/diff/D27177171/)

[ghstack-poisoned]

torch/fx/experimental/normalize.py
@@ -1,7 +1,8 @@
 import torch
 import torch.fx
 import inspect
-from typing import Any, Dict, List, Optional, Tuple
+import typing
+from typing import Any, Dict, List, Optional, Tuple, Union, get_args
 from torch.fx.node import Argument, Target
 from torch._jit_internal import boolean_dispatched
 
@@ -54,6 +55,7 @@ class NormalizeArgs(Transformer):
             assert callable(target)
             torch_op_schemas = get_signature_for_torch_op(target)
             if torch_op_schemas:
+                print('!' * 20, 'trying for op', target)
                 signature = self._find_overload_with_type_check(torch_op_schemas, args, kwargs)
                 if signature:
                     new_kwargs = self._args_kwargs_to_normalized_kwargs(signature, args, kwargs)
@@ -121,8 +123,49 @@ class NormalizeArgs(Transformer):
         found_signature = None
         for candidate_signature in candidate_signatures:
             try:
-                bound_args = candidate_signature.bind(*args, **kwargs)
+                # For the purposes of schema matching, assume Proxy's are Tensors
+                # This is safe because `__torch_function__` can only dispatch proxie
+                # args for Tensors or containers containing Tensors
+                def proxy_to_tensor(a):
+                    if isinstance(a, torch.fx.Proxy):
+                        return torch.Tensor()
+                args_for_analysis = torch.fx.node.map_aggregate(args, proxy_to_tensor)
+                kwargs_for_analysis = torch.fx.node.map_aggregate(kwargs, proxy_to_tensor)
+                bound_args = candidate_signature.bind(*args_for_analysis, **kwargs_for_analysis)
                 bound_args.apply_defaults()
+
+                def does_arg_match_param_type_annotation(arg, type):
+                    origin_type = getattr(parameter.annotation, '__origin__', parameter.annotation)
+
+                    if origin_type is Tuple:
+                        if not isinstance(arg, tuple):
+                            return False
+                        contained_types = get_args(type)
+                        assert len(contained_types) == 1
+                        return all(does_arg_match_param_type_annotation(a, contained_types[0]) for a in arg)
+                    elif origin_type is List:
+                        # PythonArgParser accepts tuples for `List`-typed arguments
+                        if not isinstance(arg, (list, tuple)):
+                            return False
+                        contained_types = get_args(type)
+                        assert len(contained_types) == 1
+                        return all(does_arg_match_param_type_annotation(a, contained_types[0]) for a in arg)
+                    elif origin_type is Union:
+                        contained_types = get_args(type)
+                        return any(does_arg_match_param_type_annotation(arg, t) for t in contained_types)
+                    elif origin_type is None:
+                        if type is torch.Tensor:
+                            # Python arg parser accepts int, float and complex for Tensor-typed
+                            # parameters
+                            # https://github.com/pytorch/pytorch/blob/19792b45dbf30b4555c4a87512e624cdd4aa6e4c/torch/csrc/utils/python_arg_parser.cpp#L1077-L1100?  # noqa
+                            return type(bound_arg) in {torch.Tensor, int, float, complex}
+                        else:
+                            # Primitive types
+                            return issubclass(type(arg), type)
+                    else:
+                        raise RuntimeError(f'Unsupported schema matching type {type}. Please '
+                                           f'report a bug to PyTorch!')
+
 
                 # Signature.bind does not check types. Do secondary checking here
                 for k, parameter in candidate_signature.parameters.items():
@@ -132,21 +175,11 @@ class NormalizeArgs(Transformer):
                         # string type annotations, so we should be good here
                         assert not isinstance(parameter.annotation, str)
 
-                        # Assuming that all proxied arguments are of Tensor type because that's the only
-                        # thing __torch_function__ currently supports
-                        if isinstance(bound_arg, torch.fx.Proxy):
-                            if parameter.annotation is not torch.Tensor:
-                                raise TypeError()
-                        elif parameter.annotation is torch.Tensor:
-                            # Python arg parser accepts int, float and complex for Tensor-typed
-                            # parameters
-                            # https://github.com/pytorch/pytorch/blob/19792b45dbf30b4555c4a87512e624cdd4aa6e4c/torch/csrc/utils/python_arg_parser.cpp#L1077-L1100?  # noqa
-                            if type(bound_arg) not in {torch.Tensor, int, float, complex}:
-                                raise TypeError()
-                        elif not issubclass(type(bound_arg), parameter.annotation):
+                        if not does_arg_match_param_type_annotation(bound_arg, parameter.annotation):
                             raise TypeError()
+
                 found_signature = candidate_signature
                 break
-            except TypeError:
+            except TypeError as e:
                 continue
         return found_signature