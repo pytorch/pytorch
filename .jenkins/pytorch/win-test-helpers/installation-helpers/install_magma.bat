93b01d655f4818d47ce7ecf0cf88af936fb02b5e
Yi Zhang <zhanyi@microsoft.com>
2021-03-22 23:34:20 +0800

    minor ifx

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.bat
@@ -1,8 +1,6 @@
 rem remove dot in cuda_version, fox example 11.1 to 111
 set VERSION_SUFFIX=%CUDA_VERSION:.=%
-IF "%VERSION_SUFFIX%"=="cpu" (
-    set CUDA_SUFFIX=""
-) ELSE (
+IF NOT "%VERSION_SUFFIX%"=="cpu"
     set CUDA_SUFFIX=cuda%VERSION_SUFFIX%
 )
 
