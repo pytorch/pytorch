f07770fc76c77fc0a40feb0c18a158c2adc26d2c
Yi Zhang <zhanyi@microsoft.com>
2021-03-23 21:21:45 +0800

    cuda extension installation path

--- .circleci/scripts/windows_cuda_install.sh
+++ .circleci/scripts/windows_cuda_install.sh
@@ -42,11 +42,8 @@ set +e
 
 set -e
 
-if [[ "${VC_YEAR}" == "2017" ]]; then
-    cp -r ${msbuild_project_dir}/* "C:/Program Files (x86)/Microsoft Visual Studio/2017/${VC_PRODUCT}/Common7/IDE/VC/VCTargets/BuildCustomizations/"
-else
-    cp -r ${msbuild_project_dir}/* "C:/Program Files (x86)/Microsoft Visual Studio/2019/${VC_PRODUCT}/MSBuild/Microsoft/VC/v160/BuildCustomizations/"
-fi
+
+cp -r ${msbuild_project_dir}/* "C:/Program Files (x86)/Microsoft Visual Studio/2019/${VC_PRODUCT}/Common7/IDE/VC/VCTargets/BuildCustomizations/"
 
 if ! ls "/c/Program Files/NVIDIA Corporation/NvToolsExt/bin/x64/nvToolsExt64_1.dll"
 then
