dc41378038b4995a9d4efea2c731ffa358777516
Taylor Robie <taylorrobie@fb.com>
2021-03-22 22:19:07 -0700

    Revert "Revert D26907093: Add repeats to Timer.collect_callgrind(...)"
    This reverts commit 0dc5abfaa9cac9266791788839d896b14600d123.
    
    [ghstack-poisoned]

--- test/benchmark_utils/test_benchmark_utils.py
+++ test/benchmark_utils/test_benchmark_utils.py
@@ -1,3 +1,4 @@
+import collections
 import json
 import os
 import re
@@ -504,12 +505,25 @@ class TestBenchmarkUtils(TestCase):
             }
         )
 
-        # Don't collect baseline to speed up unit test by ~30 seconds.
-        stats = timer.collect_callgrind(number=1000, collect_baseline=False)
+        stats = timer.collect_callgrind(number=1000)
         counts = stats.counts(denoise=False)
 
         self.assertIsInstance(counts, int)
         self.assertGreater(counts, 0)
+
+        # There is some jitter with the allocator, so we use a simpler task to
+        # test reproducibility.
+        timer = benchmark_utils.Timer(
+            "x += 1",
+            setup="x = torch.ones((1,))",
+        )
+
+        stats = timer.collect_callgrind(number=1000, repeats=20)
+        assert isinstance(stats, tuple)
+
+        # Check that the repeats are at least somewhat repeatable.
+        counts = collections.Counter([s.counts(denoise=True) for s in stats])
+        self.assertGreater(max(counts.values()), 1, f"Every instruction count total was unique: {counts}")
 
         from torch.utils.benchmark.utils.valgrind_wrapper.timer_interface import wrapper_singleton
         self.assertIsNone(
@@ -542,6 +556,14 @@ class TestBenchmarkUtils(TestCase):
             self.assertEqual(
                 s.counts(denoise=True), s.counts(denoise=False),
                 "De-noising should not apply to C++.")
+
+        stats = timer.collect_callgrind(number=1000, repeats=20)
+        assert isinstance(stats, tuple)
+
+        # NB: Unlike the example above, there is no expectation that all
+        #     repeats will be identical.
+        counts = collections.Counter([s.counts(denoise=True) for s in stats])
+        self.assertGreater(max(counts.values()), 1, repr(counts))
 
     def test_manipulate_callgrind_stats(self):
         stats_no_data, stats_with_data = load_callgrind_artifacts()
