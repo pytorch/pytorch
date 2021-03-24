f9ca0d87a7cd2afb3ae5866acf7a77af54c5ab91
anjali411 <chourdiaanjali123@gmail.com>
2021-03-24 08:10:58 -0700

    Teach Python TS frontend to parse complex literals (#52881)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52881
    
    **This PR adds:**
    1. logic to parse complex constants (complex literals of the form `bj`)
    2. logic to parse complex lists
    3. support for complex constructors: `complex(tensor/int/float/bool, tensor/int/float/bool)`
    4. Limited operator support
         - `add`, `sub`, `mul`, `torch.tensor`, `torch.as_tensor`
    
    **Follow-up work:**
    1. Add complex support for unary and other registered ops.
    2. support complex constructor with string as input (this is supported in Python eager mode).
    3. Test all emitXYZ for all XYZ in `ir_emitter.cpp` (currently only emitConst, emitValueToTensor are tested). e.g., test loops etc.
    4. onnx doesn't support complex tensors, so we should error out with a clear and descriptive error message.
    
    Test Plan: Imported from OSS
    
    Reviewed By: bdhirsh
    
    Differential Revision: D27245059
    
    Pulled By: anjali411
    
    fbshipit-source-id: af043b5159ae99a9cc8691b5a8401503fa8d6f05

--- test/test_jit.py
+++ test/test_jit.py
@@ -1137,7 +1137,7 @@ class TestJit(JitTestCase):
         out = torch.empty([2, 5], dtype=torch.complex128)
         self.checkScript(fn_out, (real, img, out, ))
 
-    def test_torch_complex_scalar(self):
+    def test_complex_constructor(self):
         # Test all scalar types
         def fn_int(real: int, img: int):
             return complex(real, img)
@@ -1211,45 +1211,41 @@ class TestJit(JitTestCase):
         self.checkScript(fn_int_float, (-3, 0.0, ))
         self.checkScript(fn_int_float, (2, -8.9, ))
 
-    def test_torch_complex_fails(self):
-        # The following operations are not allowed
-        # in Torchscript
+    def test_torch_complex_constructor_with_tensor(self):
+        tensors = ([torch.rand(1), torch.randint(-5, 5, (1, )), torch.tensor([False])])
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_tensor_float(real, img: float):
-                return complex(real, img)
-            fn_tensor_float(torch.rand(1), 1.2)
+        def fn_tensor_float(real, img: float):
+            return complex(real, img)
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_tensor_float(real: float, img):
-                return complex(real, img)
-            fn_tensor_float(2.4, torch.rand(1))
+        def fn_tensor_int(real, img: int):
+            return complex(real, img)
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_int_tensor(real: int, img):
-                return complex(real, img)
-            fn_int_tensor(6, torch.rand(1))
+        def fn_tensor_bool(real, img: bool):
+            return complex(real, img)
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_bool_tensor(real: bool, img):
-                return complex(real, img)
-            fn_bool_tensor(True, torch.rand(1))
+        def fn_float_tensor(real: float, img):
+            return complex(real, img)
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_int_tensor(real: int, img):
-                return complex(real, img)
-            fn_int_tensor(4, torch.rand(1))
+        def fn_int_tensor(real: int, img):
+            return complex(real, img)
 
-        with self.assertRaisesRegex(RuntimeError, "Cannot input a tensor of dimension"):
-            @torch.jit.script
-            def fn_bool_tensor(real, img: bool):
-                return complex(real, img)
-            fn_bool_tensor(torch.rand(1), False)
+        def fn_bool_tensor(real: bool, img):
+            return complex(real, img)
+
+        for tensor in tensors:
+            self.checkScript(fn_tensor_float, (tensor, 1.2))
+            self.checkScript(fn_tensor_int, (tensor, 3))
+            self.checkScript(fn_tensor_bool, (tensor, True))
+
+            self.checkScript(fn_float_tensor, (1.2, tensor))
+            self.checkScript(fn_int_tensor, (3, tensor))
+            self.checkScript(fn_bool_tensor, (True, tensor))
+
+        def fn_tensor_tensor(real, img):
+            return complex(real, img) + complex(2)
+
+        for x, y in product(tensors, tensors):
+            self.checkScript(fn_tensor_tensor, (x, y, ))
 
     def test_torch_sum(self):
         def fn(x):
@@ -7529,7 +7525,8 @@ a")
 
         dtypes = ["", ", dtype=torch.float", ", dtype=torch.double", ", dtype=torch.half",
                   ", dtype=torch.uint8", ", dtype=torch.int8", ", dtype=torch.short",
-                  ", dtype=torch.int", ", dtype=torch.long"]
+                  ", dtype=torch.int", ", dtype=torch.long", ", dtype=torch.cfloat",
+                  ", dtype=torch.cdouble"]
 
         ops = ['tensor', 'as_tensor']
         devices = ['', ", device='cpu'"]
@@ -7557,10 +7554,11 @@ a")
                     self.assertEqual(t1.device, t2.device)
 
         def test_as_tensor_tensor_input(input):
-            # type: (Tensor) -> Tuple[Tensor, Tensor]
-            return torch.as_tensor(input, dtype=torch.float), torch.as_tensor(input, dtype=torch.int32)
+            # type: (Tensor) -> Tuple[Tensor, Tensor, Tensor]
+            return torch.as_tensor(input, dtype=torch.cfloat), torch.as_tensor(input, dtype=torch.float), \
+                torch.as_tensor(input, dtype=torch.int32)
 
-        inp = torch.randn(3, 4)
+        inp = torch.randn(3, 4, dtype=torch.cfloat)
         self.checkScript(test_as_tensor_tensor_input, (inp,))
 
     def test_torch_tensor_dtype(self):
