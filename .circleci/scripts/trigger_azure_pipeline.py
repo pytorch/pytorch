d1242b7304987144c57272cf22b5b86a34fe25b9
Chester Liu <skyline75489@outlook.com>
2021-03-25 10:53:51 +0800

    Feedback

--- .circleci/scripts/trigger_azure_pipeline.py
+++ .circleci/scripts/trigger_azure_pipeline.py
@@ -5,26 +5,26 @@ import requests
 import time
 
 AZURE_PIPELINE_BASE_URL = "https://aiinfra.visualstudio.com/PyTorch/"
-AZURE_DEVOPS_PAT_BASE64 = "LW46YnZoNWhwM2xnZ3MzdWVqM2VteDR0ZnV4cHJ6djVwc2xuNWg0emZqaDdsa2Y0ZjdzN2lzcQ=="
+AZURE_DEVOPS_PAT_BASE64 = os.environ.get("AZURE_DEVOPS_PAT_BASE64_SECRET ", "")
 PIPELINE_ID = "911"
 PROJECT_ID = "0628bce4-2d33-499e-bac5-530e12db160f"
 
 s = requests.Session()
 s.headers.update({"Authorization": "Basic " + AZURE_DEVOPS_PAT_BASE64})
 
-build_base_url = AZURE_PIPELINE_BASE_URL + "_apis/build/builds?api-version=6.0"
+build_base_url = f"{AZURE_PIPELINE_BASE_URL}_apis/build/builds?api-version=6.0"
 
 TARGET_BRANCH = os.environ.get("CIRCLE_BRANCH", "master")
 
 # Convert the branch name for Azure DevOps
-if TARGET_BRANCH.startswith('pull/'):
-    match = re.search(r'pull/(\d+)', TARGET_BRANCH)
+match = re.search(r'pull/(\d+)', TARGET_BRANCH)
+if match is not None:
     pr_num = match.group(1)
-    SOURCE_BRANCH = 'refs/pull/' + str(pr_num) + '/head'
+    SOURCE_BRANCH = f'refs/pull/{pr_num}/head'
 else:
-    SOURCE_BRANCH = "refs/heads/" + TARGET_BRANCH
+    SOURCE_BRANCH = f'refs/heads/{TARGET_BRANCH}'
 
-print("Submitting build for branch: " + SOURCE_BRANCH)
+print(f"Submitting build for branch: {SOURCE_BRANCH}")
 
 run_build_raw = s.post(build_base_url, json={
     "definition": {"id": PIPELINE_ID},
@@ -35,18 +35,18 @@ run_build_raw = s.post(build_base_url, json={
 run_build_json = run_build_raw.json()
 build_id = run_build_json['id']
 
-print("Submitted Build: " + str(build_id))
-print("Build URL: " + run_build_json['url'])
+print(f"Submitted Build: {build_id}")
+print(f"Build URL: {run_build_json['url']}")
 
 
 def get_build(_id):
-    get_build_url = AZURE_PIPELINE_BASE_URL + "/_apis/build/builds/{}?api-version=6.0".format(_id)
+    get_build_url = f"{AZURE_PIPELINE_BASE_URL}/_apis/build/builds/{_id}?api-version=6.0"
     get_build_raw = s.get(get_build_url)
     return get_build_raw.json()
 
 
 def get_build_logs(_id):
-    get_build_logs_url = AZURE_PIPELINE_BASE_URL + "/_apis/build/builds/{}/logs?api-version=6.0".format(_id)
+    get_build_logs_url = f"{AZURE_PIPELINE_BASE_URL}/_apis/build/builds/{_id}/logs?api-version=6.0"
     get_build_logs_raw = s.get(get_build_logs_url)
     return get_build_logs_raw.json()
 
@@ -60,7 +60,7 @@ build_detail = get_build(build_id)
 build_status = build_detail['status']
 
 while build_status == 'notStarted':
-    print('Waiting for run to start: ' + str(build_id))
+    print(f'Waiting for run to start: {build_id}')
     sys.stdout.flush()
     try:
         build_detail = get_build(build_id)
@@ -69,7 +69,7 @@ while build_status == 'notStarted':
         pass
     time.sleep(60)
 
-print("Build started: ", str(build_id))
+print(f"Build started: {build_id}")
 
 handled_logs = set()
 while build_status == 'inProgress':
@@ -92,8 +92,8 @@ while build_status == 'inProgress':
 
 build_result = build_detail['result']
 
-print("Build status: " + build_status)
-print("Build result: " + build_result)
+print(f"Build status: {build_status}")
+print(f"Build result: {build_result}")
 
 if build_result != 'succeeded':
     sys.exit(-1)
