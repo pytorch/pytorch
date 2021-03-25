68c601691b6889ce968289ffcab3481e627d3a7a
haozhe.zhu <haozhezh.zhu@intel.com>
2021-03-24 22:44:57 -0700

    enable_and_enhance_bf16_threshold (#54384)
    Summary:
    enable_and_enhance_bf16_threshold
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54384
    
    Reviewed By: ngimel
    
    Differential Revision: D27286323
    
    Pulled By: mruberry
    
    fbshipit-source-id: 517fa94764d8202bbcbf94011d2d48f716fbd01b

--- test/test_nn.py
+++ test/test_nn.py
@@ -3735,6 +3735,13 @@ class TestNN(NNTestCase):
         expected = torch.tensor([99, 99, 99, 99, 1, 2, 3])
         self.assertEqual(F.threshold(x, 0, 99), expected)
 
+    def test_threshold_bfloat16(self):
+        x = torch.randn(100)
+        for threshold in [0, -0.5, 0.5, float('inf'), float('-inf'), float('nan')]:
+            expected = F.threshold(x, threshold, 0).bfloat16().float()
+            res_bf16 = F.threshold(x.bfloat16(), threshold, 0).float()
+            self.assertEqual(res_bf16, expected)
+
     @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
     def test_embedding_max_norm_unsorted_repeating_indices(self):
         def create_embedding(device):
