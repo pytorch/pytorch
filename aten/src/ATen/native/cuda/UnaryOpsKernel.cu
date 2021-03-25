f1edaabc35f2b672fd89fabdc8e5c6af62fc1a84
Hameer Abbasi <einstein.edison@gmail.com>
2021-03-25 11:06:59 -0700

    Simplify creation of unary structured kernels. (#54592)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/54592
    
    Test Plan: Imported from OSS
    
    Reviewed By: nikithamalgifb
    
    Differential Revision: D27293362
    
    Pulled By: bdhirsh
    
    fbshipit-source-id: 805f4e321645bc1ad7b8811f4b6daf96775eac9f

--- aten/src/ATen/native/cuda/UnaryOpsKernel.cu
+++ aten/src/ATen/native/cuda/UnaryOpsKernel.cu
@@ -104,7 +104,7 @@ void sigmoid_kernel_cuda(TensorIterator& iter) {
   });
 }
 
-void sinc_kernel_cuda(TensorIterator& iter) {
+void sinc_kernel_cuda(TensorIteratorBase& iter) {
   AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(ScalarType::Half, iter.common_dtype(), "sinc_cuda", [&]() {
     gpu_kernel(iter, []GPU_LAMBDA(scalar_t a) -> scalar_t {
       if (a == scalar_t(0)) {
