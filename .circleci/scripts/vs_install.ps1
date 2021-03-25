1a59632d02dac2ba0845269ec7938a56d44d6fbb
peter <peterghost86@gmail.com>
2021-03-25 22:52:59 +0800

    Fix VS install script

--- .circleci/scripts/vs_install.ps1
+++ .circleci/scripts/vs_install.ps1
@@ -1,9 +1,9 @@
 # For the VS download link, we could get a rather new one from `curl -vvv "http://aka.ms/vs/16/release/vs_Community.exe"`
-# As long as the installer don't update itself, we could pin the version of a toolchain to a revision version level 
+# As long as the installer don't update itself, we could pin the version of a toolchain to the revision level
 $VS_DOWNLOAD_LINK = "https://download.visualstudio.microsoft.com/download/pr/1192d0de-5c6d-4274-b64d-c387185e4f45/b6bf2954c37e1caf796ee06436a02c79f7b13ae99c89b8a3b3b023d64a5935e4/vs_Community.exe"
 $COLLECT_DOWNLOAD_LINK = "https://aka.ms/vscollect.exe"
 $VS_INSTALL_ARGS = @("modify","--nocache","--quiet","--wait", "--installPath ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community""",
-                                                              "--add Microsoft.VisualStudio.Component.VC.14.27.x86.x64")
+                                                              "--add Microsoft.VisualStudio.Component.VC.14.28.x86.x64")
 
 curl.exe --retry 3 -kL $VS_DOWNLOAD_LINK --output vs_installer.exe
 if ($LASTEXITCODE -ne 0) {
