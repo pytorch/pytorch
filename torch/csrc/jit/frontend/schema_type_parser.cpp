8d54aba47b4a4c378dabd6ce7d973c6b5735f2f9
James Reed <jamesreed@fb.com>
2021-03-24 20:22:17 -0700

    Update base for Update on "[WIP][FX] Make arg normalization a method on Node and not a pass"
    Instead of rewriting `GraphModule`s by default, expose normalized arguments as a method on Node that a user can call to get the normalized arguments structure. Also add exhaustive (as far as OpInfo gets us) tests for operator normalization.
    
    Differential Revision: [D27284432](https://our.internmc.facebook.com/intern/diff/D27284432)
    
    [ghstack-poisoned]

--- torch/csrc/jit/frontend/schema_type_parser.cpp
+++ torch/csrc/jit/frontend/schema_type_parser.cpp
@@ -60,13 +60,14 @@ TypePtr SchemaTypeParser::parseBaseType() {
       {"int", IntType::get()},
       {"bool", BoolType::get()},
       {"None", NoneType::get()},
+      {"NoneType", NoneType::get()},
       {"Capsule", CapsuleType::get()},
       {"Any", at::AnyType::get()},
       {"AnyClassType", at::AnyClassType::get()},
       {"AnyEnumType", at::AnyEnumType::get()},
   };
   auto tok = L.cur();
-  if (!L.nextIf(TK_NONE)) {
+  if (!L.nextIf(TK_NONE) && !L.nextIf(TK_NONE_TYPE)) {
     L.expect(TK_IDENT);
   }
   std::string text = tok.text();
