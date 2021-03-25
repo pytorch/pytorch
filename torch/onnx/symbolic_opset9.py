8e1fd82e27f5441e957bffe1baeb3b094155ef43
whiteking64 <whiteking64@gmail.com>
2021-03-25 21:13:03 +0900

    simplify onnx hardsigmoid function

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -1869,15 +1869,7 @@ def hardswish(g, self):
 
 @parse_args('v')
 def hardsigmoid(g, self):
-    input_ = g.op("Div", self, g.op('Constant', value_t=torch.tensor(3, dtype=torch.float)))
-    hardsigmoid_ = sym_help._hardtanh_helper(
-        g,
-        input_,
-        g.op('Constant', value_t=torch.tensor(-1, dtype=torch.float)),
-        g.op('Constant', value_t=torch.tensor(1, dtype=torch.float))
-    )
-    hardsigmoid_ = g.op("Add", hardsigmoid_, g.op('Constant', value_t=torch.tensor(1, dtype=torch.float)))
-    return g.op("Div", hardsigmoid_, g.op('Constant', value_t=torch.tensor(2, dtype=torch.float)))
+    return g.op('HardSigmoid', self, alpha_f=1 / 6)
 
 
 def alias(g, self):
