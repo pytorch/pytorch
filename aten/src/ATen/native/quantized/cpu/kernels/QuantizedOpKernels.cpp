f416aab8ba693fe3a42bae425de2c4c6a2d11c13
leslie-fang-intel <leslie.fang@intel.com>
2021-03-28 21:40:22 +0800

    enhence overflow issue

--- aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
+++ aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
@@ -279,18 +279,19 @@ int64_t hsum(const int32_t* A, int len) {
 int64_t hsum_sq(const uint8_t* A, int len) {
   int64_t row_sum = 0;
   int i = 0;
-  int j = 1;
 
 #ifdef CPU_CAPABILITY_AVX2
-  __m256i sum_v_epu32[2];
-  // = _mm256_setzero_si256();
-  alignas(64) int32_t temp_0[8];
-  alignas(64) int32_t temp_1[8];
-  
+
   int overflow_max_num = 524288;
   if( (len / 16) > overflow_max_num){
+      __m256i sum_v_epu32[2];
+    // = _mm256_setzero_si256();
+    alignas(64) int32_t temp_0[8];
+    alignas(64) int32_t temp_1[8];
     int loop = len/overflow_max_num;
     for(int j=1; j<=loop; j++){
+      sum_v_epu32[0] = _mm256_setzero_si256();
+      sum_v_epu32[1] = _mm256_setzero_si256();
       for (; i < overflow_max_num * j; i += 16) {
         // (i15, ..., i0)
         __m128i src_epu8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
@@ -308,36 +309,38 @@ int64_t hsum_sq(const uint8_t* A, int len) {
         sum_v_epu32[0] = _mm256_add_epi32(sum_v_epu32[0], sq_lo_epu32);
         sum_v_epu32[1] = _mm256_add_epi32(sum_v_epu32[1], sq_hi_epu32);
       }
-    }
-  }else{
-    // vectorized
-    for (; j<=16; j +=1){
-      sum_v_epu32[0] = _mm256_setzero_si256();
-      sum_v_epu32[1] = _mm256_setzero_si256();
-      for (; i < len / 16 * j; i += 16) {
-        // (i15, ..., i0)
-        __m128i src_epu8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
-        __m256i src_epu16 = _mm256_cvtepu8_epi16(src_epu8);
-        // (i15 ^ 2, ..., i0 ^ 2)
-        __m256i sq_epu16 = _mm256_mullo_epi16(src_epu16, src_epu16);
-        // (i7 ^ 2, ..., i0 ^ 2)
-        __m128i sq_lo_epu16 = _mm256_castsi256_si128(sq_epu16);
-        // (i15 ^ 2, ..., i8 ^ 2)
-        __m128i sq_hi_epu16 = _mm256_extractf128_si256(sq_epu16, 1);
-        // widen to epu32
-        __m256i sq_lo_epu32 = _mm256_cvtepu16_epi32(sq_lo_epu16);
-        __m256i sq_hi_epu32 = _mm256_cvtepu16_epi32(sq_hi_epu16);
-        // add to running sum
-        sum_v_epu32[0] = _mm256_add_epi32(sum_v_epu32[0], sq_lo_epu32);
-        sum_v_epu32[1] = _mm256_add_epi32(sum_v_epu32[1], sq_hi_epu32);
-      }
-
       _mm256_store_si256(reinterpret_cast<__m256i*>(temp_0), sum_v_epu32[0]);
       _mm256_store_si256(reinterpret_cast<__m256i*>(temp_1), sum_v_epu32[1]);
       for (int k = 0; k < 8; ++k) {
         row_sum += temp_0[k];
         row_sum += temp_1[k];
       }
+    }
+  }else{  
+    __m256i sum_v_epu32 = _mm256_setzero_si256();
+    // vectorized
+    for (; i < len / 16 * 16; i += 16) {
+      // (i15, ..., i0)
+      __m128i src_epu8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
+      __m256i src_epu16 = _mm256_cvtepu8_epi16(src_epu8);
+      // (i15 ^ 2, ..., i0 ^ 2)
+      __m256i sq_epu16 = _mm256_mullo_epi16(src_epu16, src_epu16);
+      // (i7 ^ 2, ..., i0 ^ 2)
+      __m128i sq_lo_epu16 = _mm256_castsi256_si128(sq_epu16);
+      // (i15 ^ 2, ..., i8 ^ 2)
+      __m128i sq_hi_epu16 = _mm256_extractf128_si256(sq_epu16, 1);
+      // widen to epu32
+      __m256i sq_lo_epu32 = _mm256_cvtepu16_epi32(sq_lo_epu16);
+      __m256i sq_hi_epu32 = _mm256_cvtepu16_epi32(sq_hi_epu16);
+      // add to running sum
+      sum_v_epu32 = _mm256_add_epi32(sum_v_epu32, sq_lo_epu32);
+      sum_v_epu32 = _mm256_add_epi32(sum_v_epu32, sq_hi_epu32);
+    }
+
+    alignas(64) int32_t temp[8];
+    _mm256_store_si256(reinterpret_cast<__m256i*>(temp), sum_v_epu32);
+    for (int k = 0; k < 8; ++k) {
+      row_sum += temp[k];
     }
   }
 
@@ -354,17 +357,20 @@ int64_t hsum_sq(const uint8_t* A, int len) {
 int64_t hsum_sq(const int8_t* A, int len) {
   int64_t row_sum = 0;
   int i = 0;
-  int j = 1;
 
 #ifdef CPU_CAPABILITY_AVX2
-  __m256i sum_v_epi32[2]; // = _mm256_setzero_si256();
-  alignas(64) int32_t temp_0[8];
-  alignas(64) int32_t temp_1[8];
 
   int overflow_max_num = 2097152;
+
   if( (len / 16) > overflow_max_num){
+    __m256i sum_v_epi32[2]; // = _mm256_setzero_si256();
+    alignas(64) int32_t temp_0[8];
+    alignas(64) int32_t temp_1[8];
+
     int loop = len/overflow_max_num;
     for(int j=1; j<=loop; j++){
+      sum_v_epi32[0] = _mm256_setzero_si256();
+      sum_v_epi32[1] = _mm256_setzero_si256();
       for (; i < overflow_max_num * j; i += 16) {
         // (i15, ..., i0)
         __m128i src_epu8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
@@ -379,39 +385,41 @@ int64_t hsum_sq(const int8_t* A, int len) {
         __m256i sq_lo_epu32 = _mm256_cvtepu16_epi32(sq_lo_epu16);
         __m256i sq_hi_epu32 = _mm256_cvtepu16_epi32(sq_hi_epu16);
         // add to running sum
-        sum_v_epu32[0] = _mm256_add_epi32(sum_v_epu32[0], sq_lo_epu32);
-        sum_v_epu32[1] = _mm256_add_epi32(sum_v_epu32[1], sq_hi_epu32);
+        sum_v_epi32[0] = _mm256_add_epi32(sum_v_epi32[0], sq_lo_epu32);
+        sum_v_epi32[1] = _mm256_add_epi32(sum_v_epi32[1], sq_hi_epu32);
       }
-    }
-  }else{
-    // vectorized
-    for (; j<=16; j +=1){
-      sum_v_epi32[0] = _mm256_setzero_si256();
-      sum_v_epi32[1] = _mm256_setzero_si256();
-      for (; i < len / 16 * j; i += 16) {
-        // (i15, ..., i0)
-        __m128i src_epi8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
-        __m256i src_epi16 = _mm256_cvtepi8_epi16(src_epi8);
-        // (i15 ^ 2, ..., i0 ^ 2)
-        __m256i sq_epi16 = _mm256_mullo_epi16(src_epi16, src_epi16);
-        // (i7 ^ 2, ..., i0 ^ 2)
-        __m128i sq_lo_epi16 = _mm256_castsi256_si128(sq_epi16);
-        // (i15 ^ 2, ..., i8 ^ 2)
-        __m128i sq_hi_epi16 = _mm256_extractf128_si256(sq_epi16, 1);
-        // widen to epi32
-        __m256i sq_lo_epi32 = _mm256_cvtepi16_epi32(sq_lo_epi16);
-        __m256i sq_hi_epi32 = _mm256_cvtepi16_epi32(sq_hi_epi16);
-        // add to running sum
-        sum_v_epi32[0] = _mm256_add_epi32(sum_v_epi32[0], sq_lo_epi32);
-        sum_v_epi32[1] = _mm256_add_epi32(sum_v_epi32[1], sq_hi_epi32);
-      }
-
       _mm256_store_si256(reinterpret_cast<__m256i*>(temp_0), sum_v_epi32[0]);
       _mm256_store_si256(reinterpret_cast<__m256i*>(temp_1), sum_v_epi32[1]);
       for (int k = 0; k < 8; ++k) {
         row_sum += temp_0[k];
         row_sum += temp_1[k];
       }
+    }
+  }else{
+    __m256i sum_v_epi32 = _mm256_setzero_si256();
+    // vectorized
+    for (; i < len / 16 * 16; i += 16) {
+      // (i15, ..., i0)
+      __m128i src_epi8 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(A + i));
+      __m256i src_epi16 = _mm256_cvtepi8_epi16(src_epi8);
+      // (i15 ^ 2, ..., i0 ^ 2)
+      __m256i sq_epi16 = _mm256_mullo_epi16(src_epi16, src_epi16);
+      // (i7 ^ 2, ..., i0 ^ 2)
+      __m128i sq_lo_epi16 = _mm256_castsi256_si128(sq_epi16);
+      // (i15 ^ 2, ..., i8 ^ 2)
+      __m128i sq_hi_epi16 = _mm256_extractf128_si256(sq_epi16, 1);
+      // widen to epi32
+      __m256i sq_lo_epi32 = _mm256_cvtepi16_epi32(sq_lo_epi16);
+      __m256i sq_hi_epi32 = _mm256_cvtepi16_epi32(sq_hi_epi16);
+      // add to running sum
+      sum_v_epi32 = _mm256_add_epi32(sum_v_epi32, sq_lo_epi32);
+      sum_v_epi32 = _mm256_add_epi32(sum_v_epi32, sq_hi_epi32);
+    }
+
+    alignas(64) int32_t temp[8];
+    _mm256_store_si256(reinterpret_cast<__m256i*>(temp), sum_v_epi32);
+    for (int k = 0; k < 8; ++k) {
+      row_sum += temp[k];
     }
   }
 #endif // CPU_CAPABILITY_AVX2
