cd7c84a24671ec605874d0eaa44d81d82c5d8a5f
Jane Xu <janeyx@fb.com>
2021-03-29 18:02:58 -0400

    use defaultdict

--- tools/export_slow_tests.py
+++ tools/export_slow_tests.py
@@ -3,40 +3,39 @@
 import argparse
 import json
 import os
+import statistics
+from collections import defaultdict
 from tools.stats_utils.s3_stat_parser import get_previous_reports_for_branch, Report, Version2Report
-from typing import cast, Dict, Tuple, List
+from typing import cast, DefaultDict, Dict, List
 
 SLOW_TESTS_FILE = '.pytorch-slow-tests'
-SLOW_TEST_CASE_THRESHOLD = 60.0
+SLOW_TEST_CASE_THRESHOLD_SEC = 60.0
 
 
 def get_test_case_times() -> Dict[str, float]:
-    # an entry will be like ("test_doc_examples (__main__.TestTypeHints)" -> (current_avg, # values))
     reports: List[Report] = get_previous_reports_for_branch('origin/viable/strict', "")
-    test_names_to_times: Dict[str, Tuple[float, int]] = dict()
+    # an entry will be like ("test_doc_examples (__main__.TestTypeHints)" -> [values]))
+    test_names_to_times: DefaultDict[str, List[float]] = defaultdict(list)
     for report in reports:
-        if 'format_version' not in report:  # version 1 implicitly
+        if report.get('format_version', 1) != 2:
             raise RuntimeError("S3 format currently handled is version 2 only")
-        else:
-            v2report = cast(Version2Report, report)
-            for _, test_file in v2report['files'].items():
-                for suitename, test_suite in test_file['suites'].items():
-                    for casename, test_case in test_suite['cases'].items():
-                        name = f'{casename} (__main__.{suitename})'
-                        succeeded: bool = test_case['status'] is None
-                        if succeeded:
-                            if name not in test_names_to_times:
-                                test_names_to_times[name] = (test_case['seconds'], 1)
-                            else:
-                                curr_avg, curr_count = test_names_to_times[name]
-                                new_count = curr_count + 1
-                                new_avg = (curr_avg * curr_count + test_case['seconds']) / new_count
-                                test_names_to_times[name] = (new_avg, new_count)
-    return {test_case: time for test_case, (time, _) in test_names_to_times.items()}
+        v2report = cast(Version2Report, report)
+        for test_file in v2report['files'].values():
+            for suitename, test_suite in test_file['suites'].items():
+                for casename, test_case in test_suite['cases'].items():
+                    # The below attaches a __main__ as that matches the format of test.__class__ in
+                    # common_utils.py (where this data will be used), and also matches what the output
+                    # of a running test would look like.
+                    name = f'{casename} (__main__.{suitename})'
+                    succeeded: bool = test_case['status'] is None
+                    if succeeded:
+                        if name not in test_names_to_times:
+                            test_names_to_times[name].append(test_case['seconds'])
+    return {test_case: statistics.mean(times) for test_case, times in test_names_to_times.items()}
 
 
 def filter_slow_tests(test_cases_dict: Dict[str, float]) -> Dict[str, float]:
-    return {test_case: time for test_case, time in test_cases_dict.items() if time >= SLOW_TEST_CASE_THRESHOLD}
+    return {test_case: time for test_case, time in test_cases_dict.items() if time >= SLOW_TEST_CASE_THRESHOLD_SEC}
 
 
 def export_slow_tests(filename: str) -> None:
