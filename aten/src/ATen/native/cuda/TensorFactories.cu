d74f0f72952a0c68cb3ed936a02deb02487011ed
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-30 15:01:57 -0700

    save

--- aten/src/ATen/native/cuda/TensorFactories.cu
+++ aten/src/ATen/native/cuda/TensorFactories.cu
@@ -131,8 +131,7 @@ Tensor& randperm_out_cuda(int64_t n, c10::optional<Generator> generator, Tensor&
   // See note [Algorithm of randperm]
   const double log_threshold_12 = std::log(0.9) * 12;
   double nd = static_cast<double>(n);
-  int bits = std::min(64,
-    static_cast<int>(std::ceil(std::log2(nd - (6 * nd * nd + 1) / log_threshold_12))));
+  int bits = 64;
 
   if (n == 0) {
     return result;
