8473936cf319bee68afd77966c76e39530fdf48a
Yi Wang <wayi@fb.com>
2021-03-30 09:25:19 -0700

    [Gradient Compression] Add cuda.syncrhonize back to batched powerSGD (#54838)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54838
    
    Realize that an explicit sync is somehow still needed for batched PowerSGD hook. I find that a job failure can be fixed by this change.
    
    The sync was once removed by #54482.
    
    Test Plan:
    f260900882
    f260899693
    
    Reviewed By: rohan-varma
    
    Differential Revision: D27384738
    
    fbshipit-source-id: 3efd738b9fd375e2ceb36ed3a6bf99cd8ce8ff95

--- torch/distributed/algorithms/ddp_comm_hooks/powerSGD_hook.py
+++ torch/distributed/algorithms/ddp_comm_hooks/powerSGD_hook.py
@@ -678,6 +678,10 @@ def batched_powerSGD_hook(
         if state.use_error_feedback:
             # Memorize the local errors.
             state.error_dict[bucket_index] = input_tensor_cp - input_tensor
+        # Removing this seemingly unnecessary sync somehow may cause faliures.
+        # See: https://github.com/pytorch/pytorch/pull/54838
+        if torch.cuda.is_available():
+            torch.cuda.synchronize(device)
         if not state.warm_start:
             state.p_memory_dict.clear()
             state.q_memory_dict.clear()
