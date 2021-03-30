e1cce33f47f6d140fc33c7417d8049be7d69bdeb
Michael Suo <suo@fb.com>
2021-03-29 18:40:21 -0700

    Update on "[package] fix mangling issues with TorchScript"
    TorchScript and torch.package have different mangling schemes. To avoid
    them interfering with each other, we should undo the torch.package
    mangling before processing anything with TorchScript (since TS
    independently makes sure that no names collide).
    
    Differential Revision: [D27410472](https://our.internmc.facebook.com/intern/diff/D27410472)
    
    [ghstack-poisoned]

--- test/package/package_a/script_class_fake.py
+++ test/package/package_a/script_class_fake.py
@@ -1,6 +1,5 @@
-import torch
-
 class MyScriptClass:
+    """Intended to be scripted."""
     def __init__(self, x):
         self.foo = x
 
@@ -8,5 +7,6 @@ class MyScriptClass:
         return self.foo
 
 def uses_script_class(x):
+    """Intended to be scripted."""
     foo = MyScriptClass(x)
     return foo.foo
