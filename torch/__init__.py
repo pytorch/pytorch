3b81a8f96bf506044c40e7735a40d62a8370847c
harish <harishsdev@gmail.com>
2021-03-31 16:15:22 +0530

    Example added in Documentation for is_tensor API

--- torch/__init__.py
+++ torch/__init__.py
@@ -278,6 +278,11 @@ def is_tensor(obj):
 
     Args:
         obj (Object): Object to test
+    Example::
+    >>>import torch
+    >>>x=torch.tensor([1, 2, 3])
+    >>>torch.is_tensor(x)
+    >>>True
     """
     return isinstance(obj, torch.Tensor)
 
