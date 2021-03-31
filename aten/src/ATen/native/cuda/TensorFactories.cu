4f48626a1a55d94a6eed5c34bcc36cdcf5ad25ef
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-03-31 13:13:03 -0700

    Update TensorFactories.cu

--- aten/src/ATen/native/cuda/TensorFactories.cu
+++ aten/src/ATen/native/cuda/TensorFactories.cu
@@ -184,8 +184,7 @@ Tensor& randperm_out_cuda(int64_t n, c10::optional<Generator> generator, Tensor&
       at::cuda::cub::sort_pairs<int64_t, scalar_t>(
         keys.data_ptr<int64_t>(), keys_out,
         range.data_ptr<scalar_t>(), shuffled_data_,
-        n, false, 0, bits);
-      randperm_handle_duplicate_keys(keys_out, shuffled_data_, bits, n, generator);
+        n);
     });
   }
 
