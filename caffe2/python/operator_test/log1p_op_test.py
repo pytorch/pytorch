ce7541dfa2c6e04599c5ea584b25ae9c35a6368a
Oleg Khabinov <khabinov@fb.com>
2021-03-30 22:41:30 -0700

    [caffe2] Implement gradient for Log1p (#55018)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55018
    
    We know that derivative of Log1p is easy to calculate:
    
    d/dx(log(x + 1)) = 1/(x + 1)
    
    {F560806794}
    
    Differential Revision: D27452603
    
    fbshipit-source-id: dfd18cd52cd8b1d18436a196aa5b79746f92ccc2

--- caffe2/python/operator_test/log1p_op_test.py
+++ caffe2/python/operator_test/log1p_op_test.py
@@ -3,14 +3,22 @@ from hypothesis import given
 import caffe2.python.hypothesis_test_util as hu
 import numpy as np
 
+def ref_log1p(input):
+    result = np.log1p(input)
+    return (result,)
+
+def ref_log1p_grad(g_out, outputs, fwd_inputs):
+    result = g_out / (fwd_inputs[0] + 1)
+    return (result,)
+
 class TestLog1pOp(hu.HypothesisTestCase):
 
-    @given(data=hu.tensor(dtype=np.float32), **hu.gcs)
-    def test_log1p(self, data, gc, dc):
+    @given(input=hu.tensor(dtype=np.float32), **hu.gcs)
+    def test_log1p(self, input, gc, dc):
         op = core.CreateOperator("Log1p", ["input"], ["output"])
 
-        def ref_log1p(input):
-            result = np.log1p(input)
-            return (result,)
-
-        self.assertReferenceChecks(gc, op, [data], ref_log1p)
+        self.assertReferenceChecks(gc, op, [input], ref_log1p,
+                                   output_to_grad="output",
+                                   grad_reference=ref_log1p_grad,
+                                   ensure_outputs_are_inferred=True)
+        self.assertDeviceChecks(dc, op, [input], [0])
