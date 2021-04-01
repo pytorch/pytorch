443c3378eaeff06e44aac0db3a75d8e9c2398e77
wxie <wxie@devvm2675.vll0.facebook.com>
2021-03-31 23:21:52 -0700

    Eliminate device guard in generic dispatch key kernel wrappers
    Differential Revision: [D27496584](https://our.internmc.facebook.com/intern/diff/D27496584/)
    
    [ghstack-poisoned]

--- tools/codegen/dest/register_dispatch_key.py
+++ tools/codegen/dest/register_dispatch_key.py
@@ -151,15 +151,14 @@ return {sig.name()}({', '.join(e.expr for e in translate(cpp_sig.arguments(), si
             init_cuda = ""
             device_guard = "// DeviceGuard omitted"  # default
 
-            if (is_generic_dispatch_key(self.dispatch_key) or is_cuda_dispatch_key(self.dispatch_key)) and f.device_guard:
+            if f.device_guard and is_cuda_dispatch_key(self.dispatch_key):
                 has_tensor_options = any(isinstance(a.argument, TensorOptionsArguments) for a in args)
                 if has_tensor_options:
                     # kernel is creating a tensor
                     device_guard = "const DeviceGuard device_guard(device_or_default(device));"
 
-                    if is_cuda_dispatch_key(self.dispatch_key):
-                        # initialize CUDA on construction of CUDA tensors
-                        init_cuda = "globalContext().lazyInitCUDA();"
+                    # initialize CUDA on construction of CUDA tensors
+                    init_cuda = "globalContext().lazyInitCUDA();"
                 else:
                     # kernel is operating on existing tensors
 
