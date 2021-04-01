b39591fb50be1a16405288812f0958d10cbbb6e7
Antonio Cuni <anto.cuni@gmail.com>
2021-04-01 09:45:44 +0000

    use the torch.linalg.det also here

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -5268,7 +5268,7 @@ class TestONNXRuntime(unittest.TestCase):
     def test_det(self):
         class Det(torch.nn.Module):
             def forward(self, x):
-                return torch.det(x)
+                return torch.linalg.det(x)
 
         x = torch.randn(2, 3, 5, 5)
         self.run_test(Det(), x)
