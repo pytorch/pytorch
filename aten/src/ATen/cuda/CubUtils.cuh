a3b8503d7e24ca84c4a559e50c2063e8da93f183
Natalia Gimelshein <ngimel@fb.com>
2021-04-04 16:27:10 -0700

    lint

--- aten/src/ATen/cuda/CubUtils.cuh
+++ aten/src/ATen/cuda/CubUtils.cuh
@@ -3,6 +3,6 @@
 // include cub in a safe manner
 #define CUB_NS_PREFIX namespace at{ namespace native{
 #define CUB_NS_POSTFIX }}
-#include "cub/cub.cuh"
+#include <cub/cub.cuh>
 #undef CUB_NS_POSTFIX
 #undef CUB_NS_PREFIX
