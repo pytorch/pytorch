984f91c36fa7f69bf7ca72422018d0268558fc0c
Natalia Gimelshein <ngimel@fb.com>
2021-04-04 20:21:49 -0700

    avoid redefinition warnings

--- aten/src/ATen/cuda/CubUtils.cuh
+++ aten/src/ATen/cuda/CubUtils.cuh
@@ -1,6 +1,8 @@
 #pragma once
 
 // include cub in a safe manner
+#undef CUB_NS_POSTFIX //undef to avoid redefinition warnings
+#undef CUB_NS_PREFIX
 #define CUB_NS_PREFIX namespace at{ namespace native{
 #define CUB_NS_POSTFIX }}
 #include <cub/cub.cuh>
