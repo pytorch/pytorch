30a398a93d7a79e48acde4184608eeb52861035a
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-04-06 06:16:32 -0500

    fix typo

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1977,13 +1977,13 @@ def sample_inputs_polar(op_info, device, dtype, requires_grad):
 
 
 def sample_inputs_polygamma(op_info, device, dtype, requires_grad):
-    make_arg = partial(make_tensor(shape, device=device, dtype=dtype, requires_grad=requires_grad))
+    make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=requires_grad)
     tensor_shapes = [(S, S), ()]
     ns = [1, 2, 3, 4, 5]
 
     def generator():
         for shape, n in product(tensor_shapes, ns):
-            yield SampleInput(make_arg(shape, args=(n,)))
+            yield SampleInput(make_arg(shape), args=(n,))
 
     return list(generator())
 
