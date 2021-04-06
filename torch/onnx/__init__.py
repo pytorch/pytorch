763aa6581a197ce357b5d5c73e4e8528baf0dc47
Nikita Shulga <nshulga@fb.com>
2021-04-06 13:55:44 -0700

    Delete some unicode whitespaces

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -82,15 +82,15 @@ def export(model, args, f, export_params=True, verbose=False, training=TrainingM
                         return x
 
                 m = Model()
-                k = torch.randn(2, 3)  
-                x = {torch.tensor(1.): torch.randn(2, 3)}
+                k = torch.randn(2, 3)
+                x = {torch.tensor(1.): torch.randn(2, 3)}
 
                 In the previous iteration, the call to export API would look like
 
                     torch.onnx.export(model, (k, x), 'test.onnx')
 
                 This would work as intended. However, the export function
-                would now assume that the ‘x’ input is intended to represent the optional
+                would now assume that the `x` input is intended to represent the optional
                 dictionary consisting of named arguments. In order to prevent this from being
                 an issue a constraint is placed to provide an empty dictionary as the last
                 input in the tuple args in such cases. The new call would look like this.
