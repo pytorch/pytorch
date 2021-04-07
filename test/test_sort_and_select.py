0fd6090a5d0e4396f0c852c646b428bc2538fbd7
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-06 22:17:59 -0700

    better test_sort coverage

--- test/test_sort_and_select.py
+++ test/test_sort_and_select.py
@@ -17,29 +17,33 @@ SIZE = 100
 class TestSortAndSelect(TestCase):
 
     def assertIsOrdered(self, order, x, mxx, ixx, task):
-        SIZE = 4
+        SIZE = x.size(1)
         if order == 'descending':
             def check_order(a, b):
                 # `a != a` because we put NaNs
                 # at the end of ascending sorted lists,
                 # and the beginning of descending ones.
-                return a != a or a >= b
+                return ((a != a) | (a >= b)).all().item()
         elif order == 'ascending':
             def check_order(a, b):
                 # see above
-                return b != b or a <= b
+                return ((b != b) | (a <= b)).all().item()
         else:
             error('unknown order "{}", must be "ascending" or "descending"'.format(order))
 
         are_ordered = True
-        for j, k in product(range(SIZE), range(1, SIZE)):
-            self.assertTrue(check_order(mxx[j][k - 1], mxx[j][k]),
+        for k in range(1, SIZE):
+            self.assertTrue(check_order(mxx[:, k - 1], mxx[:, k]),
                             'torch.sort ({}) values unordered for {}'.format(order, task))
 
         seen = set()
         indicesCorrect = True
+        size0 = x.size(0)
         size = x.size(x.dim() - 1)
-        for k in range(size):
+        x = x.tolist()
+        mxx = mxx.tolist()
+        ixx = ixx.tolist()
+        for k in range(size0):
             seen.clear()
             for j in range(size):
                 self.assertEqual(x[k][ixx[k][j]], mxx[k][j],
@@ -48,70 +52,70 @@ class TestSortAndSelect(TestCase):
             self.assertEqual(len(seen), size)
 
     def test_sort(self, device):
-        SIZE = 4
-        x = torch.rand(SIZE, SIZE, device=device)
-        res1val, res1ind = torch.sort(x)
+        for SIZE in (4, 2049):
+            x = torch.rand(4, SIZE, device=device)
+            res1val, res1ind = torch.sort(x)
 
-        # Test use of result tensor
-        res2val = torch.tensor((), device=device)
-        res2ind = torch.tensor((), device=device, dtype=torch.long)
-        torch.sort(x, out=(res2val, res2ind))
-        self.assertEqual(res1val, res2val, atol=0, rtol=0)
-        self.assertEqual(res1ind, res2ind, atol=0, rtol=0)
-        self.assertEqual(torch.argsort(x), res1ind)
-        self.assertEqual(x.argsort(), res1ind)
+            # Test use of result tensor
+            res2val = torch.tensor((), device=device)
+            res2ind = torch.tensor((), device=device, dtype=torch.long)
+            torch.sort(x, out=(res2val, res2ind))
+            self.assertEqual(res1val, res2val, atol=0, rtol=0)
+            self.assertEqual(res1ind, res2ind, atol=0, rtol=0)
+            self.assertEqual(torch.argsort(x), res1ind)
+            self.assertEqual(x.argsort(), res1ind)
 
-        # Test sorting of random numbers
-        self.assertIsOrdered('ascending', x, res2val, res2ind, 'random')
+            # Test sorting of random numbers
+            self.assertIsOrdered('ascending', x, res2val, res2ind, 'random')
 
-        # Test simple sort
-        self.assertEqual(
-            torch.sort(torch.tensor((50, 40, 30, 20, 10), device=device))[0],
-            torch.tensor((10, 20, 30, 40, 50), device=device),
-            atol=0, rtol=0
-        )
+            # Test simple sort
+            self.assertEqual(
+                torch.sort(torch.tensor((50, 40, 30, 20, 10), device=device))[0],
+                torch.tensor((10, 20, 30, 40, 50), device=device),
+                atol=0, rtol=0
+            )
 
-        # Test that we still have proper sorting with duplicate keys
-        x = torch.floor(torch.rand(SIZE, SIZE, device=device) * 10)
-        torch.sort(x, out=(res2val, res2ind))
-        self.assertIsOrdered('ascending', x, res2val, res2ind, 'random with duplicate keys')
+            # Test that we still have proper sorting with duplicate keys
+            x = torch.floor(torch.rand(4, SIZE, device=device) * 10)
+            torch.sort(x, out=(res2val, res2ind))
+            self.assertIsOrdered('ascending', x, res2val, res2ind, 'random with duplicate keys')
 
-        # DESCENDING SORT
-        x = torch.rand(SIZE, SIZE, device=device)
-        res1val, res1ind = torch.sort(x, x.dim() - 1, True)
+            # DESCENDING SORT
+            x = torch.rand(4, SIZE, device=device)
+            res1val, res1ind = torch.sort(x, x.dim() - 1, True)
 
-        # Test use of result tensor
-        res2val = torch.tensor((), device=device)
-        res2ind = torch.tensor((), device=device, dtype=torch.long)
-        torch.sort(x, x.dim() - 1, True, out=(res2val, res2ind))
-        self.assertEqual(res1val, res2val, atol=0, rtol=0)
-        self.assertEqual(res1ind, res2ind, atol=0, rtol=0)
-        self.assertEqual(torch.argsort(x, x.dim() - 1, True), res1ind)
-        self.assertEqual(x.argsort(x.dim() - 1, True), res1ind)
+            # Test use of result tensor
+            res2val = torch.tensor((), device=device)
+            res2ind = torch.tensor((), device=device, dtype=torch.long)
+            torch.sort(x, x.dim() - 1, True, out=(res2val, res2ind))
+            self.assertEqual(res1val, res2val, atol=0, rtol=0)
+            self.assertEqual(res1ind, res2ind, atol=0, rtol=0)
+            self.assertEqual(torch.argsort(x, x.dim() - 1, True), res1ind)
+            self.assertEqual(x.argsort(x.dim() - 1, True), res1ind)
 
-        # Test sorting of random numbers
-        self.assertIsOrdered('descending', x, res2val, res2ind, 'random')
+            # Test sorting of random numbers
+            self.assertIsOrdered('descending', x, res2val, res2ind, 'random')
 
-        # Test simple sort task
-        self.assertEqual(
-            torch.sort(torch.tensor((10, 20, 30, 40, 50), device=device), 0, True)[0],
-            torch.tensor((50, 40, 30, 20, 10), device=device),
-            atol=0, rtol=0
-        )
+            # Test simple sort task
+            self.assertEqual(
+                torch.sort(torch.tensor((10, 20, 30, 40, 50), device=device), 0, True)[0],
+                torch.tensor((50, 40, 30, 20, 10), device=device),
+                atol=0, rtol=0
+            )
 
-        # Test that we still have proper sorting with duplicate keys
-        self.assertIsOrdered('descending', x, res2val, res2ind, 'random with duplicate keys')
+            # Test that we still have proper sorting with duplicate keys
+            self.assertIsOrdered('descending', x, res2val, res2ind, 'random with duplicate keys')
 
-        # Test sorting with NaNs
-        x = torch.rand(SIZE, SIZE, device=device)
-        x[1][2] = float('NaN')
-        x[3][0] = float('NaN')
-        torch.sort(x, out=(res2val, res2ind))
-        self.assertIsOrdered('ascending', x, res2val, res2ind,
-                             'random with NaNs')
-        torch.sort(x, out=(res2val, res2ind), descending=True)
-        self.assertIsOrdered('descending', x, res2val, res2ind,
-                             'random with NaNs')
+            # Test sorting with NaNs
+            x = torch.rand(4, SIZE, device=device)
+            x[1][2] = float('NaN')
+            x[3][0] = float('NaN')
+            torch.sort(x, out=(res2val, res2ind))
+            self.assertIsOrdered('ascending', x, res2val, res2ind,
+                                'random with NaNs')
+            torch.sort(x, out=(res2val, res2ind), descending=True)
+            self.assertIsOrdered('descending', x, res2val, res2ind,
+                                'random with NaNs')
 
     @onlyCUDA
     @dtypes(*set(torch.testing.get_all_dtypes()) - {torch.bfloat16, torch.complex64, torch.complex128})
