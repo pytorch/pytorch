4568553682d190f664f79ae1da5188fb1bb52ebf
Sam Estep <sestep@fb.com>
2021-04-07 11:34:57 -0700

    Disallow GitHub Actions expressions in scripts

--- tools/extract_scripts.py
+++ tools/extract_scripts.py
@@ -33,28 +33,46 @@ def main() -> None:
     if out.exists():
         sys.exit(f'{out} already exists; aborting to avoid overwriting')
 
+    gha_expressions_found = False
+
     for p in Path('.github/workflows').iterdir():
         with open(p) as f:
             workflow = yaml.safe_load(f)
 
-        for name, job in workflow['jobs'].items():
-            job_dir = out / p / name
+        for job_name, job in workflow['jobs'].items():
+            job_dir = out / p / job_name
             default_shell = get_default_shell(job)
             steps = job['steps']
             index_chars = len(str(len(steps) - 1))
-            for i, step in enumerate(steps):
+            for i, step in enumerate(steps, start=1):
                 script = step.get('run')
-                if script and step.get('shell', default_shell) == 'bash':
-                    job_dir.mkdir(parents=True, exist_ok=True)
+                if script:
+                    step_name = step['name']
+                    if '${{' in script:
+                        gha_expressions_found = True
+                        print(
+                            f'{p} job `{job_name}` step {i}: {step_name}',
+                            file=sys.stderr
+                        )
 
-                    sanitized = re.sub(
-                        '[^a-zA-Z_]+', '_',
-                        f'_{step["name"]}',
-                    ).rstrip('_')
-                    filename = f'{i:0{index_chars}}{sanitized}.sh'
-                    (job_dir / filename).write_text(
-                        f'#!/usr/bin/env bash\nset -eo pipefail\n{script}'
-                    )
+                    if step.get('shell', default_shell) == 'bash':
+                        job_dir.mkdir(parents=True, exist_ok=True)
+
+                        sanitized = re.sub(
+                            '[^a-zA-Z_]+', '_',
+                            f'_{step_name}',
+                        ).rstrip('_')
+                        filename = f'{i:0{index_chars}}{sanitized}.sh'
+                        (job_dir / filename).write_text(
+                            f'#!/usr/bin/env bash\nset -eo pipefail\n{script}'
+                        )
+
+    if gha_expressions_found:
+        sys.exit(
+            'Each of the above scripts contains a GitHub Actions '
+            '${{ <expression> }} which must be replaced with an `env` variable'
+            ' for security reasons.'
+        )
 
 
 if __name__ == '__main__':
