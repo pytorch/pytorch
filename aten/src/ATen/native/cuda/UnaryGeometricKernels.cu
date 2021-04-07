00049c9d28299e9c90aad1121d4b3b580c591c9b
Meghan Lele <meghanl@fb.com>
2021-04-07 14:54:18 -0700

    Migrate cos to structured kernel
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/UnaryGeometricKernels.cu
+++ aten/src/ATen/native/cuda/UnaryGeometricKernels.cu
@@ -42,7 +42,7 @@ void sin_kernel_cuda(TensorIteratorBase& iter) {
   });
 }
 
-void cos_kernel_cuda(TensorIterator& iter) {
+void cos_kernel_cuda(TensorIteratorBase& iter) {
   AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(ScalarType::Half, ScalarType::BFloat16, iter.common_dtype(), "cos_cuda", [&]() {
     gpu_kernel(iter, []GPU_LAMBDA(scalar_t a) -> scalar_t {
       return ::cos(a);
