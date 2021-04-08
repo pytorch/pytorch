a597ae29122861361b63773adb1a8979128f79b2
Can Balioglu <cbalioglu@users.noreply.github.com>
2021-04-08 13:11:14 -0700

    [torch/elastic] Introduce `C10dRendezvousBackend`.
    This diff introduces:
    
    - The `C10dRendezvousBackend` type to support C10d stores as rendezvous backends.
    - A fix to the `TCPStore.compare_set()` function to support non-existent keys.
    - A placeholder `c10d-experimental` registry to instantiate C10d-baked rendezvous backends via `get_rendezvous_handler()`.
    
    Differential Revision: [D27654492](https://our.internmc.facebook.com/intern/diff/D27654492/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D27654492/)!
    
    [ghstack-poisoned]

--- torch/distributed/elastic/rendezvous/registry.py
+++ torch/distributed/elastic/rendezvous/registry.py
@@ -6,6 +6,7 @@
 
 from .api import RendezvousHandler, RendezvousParameters
 from .api import rendezvous_handler_registry as handler_registry
+from .default_rendezvous import create_handler
 
 
 def _create_etcd_handler(params: RendezvousParameters) -> RendezvousHandler:
@@ -14,8 +15,17 @@ def _create_etcd_handler(params: RendezvousParameters) -> RendezvousHandler:
     return etcd_rendezvous.create_rdzv_handler(params)
 
 
+def _create_expr_c10d_handler(params: RendezvousParameters) -> RendezvousHandler:
+    from .c10d_rendezvous_backend import create_backend
+
+    backend = create_backend(params)
+
+    return create_handler(backend.store, backend, params)
+
+
 def _register_default_handlers() -> None:
     handler_registry.register("etcd", _create_etcd_handler)
+    handler_registry.register("c10d-experimental", _create_expr_c10d_handler)
 
 
 # The legacy function kept for backwards compatibility.
