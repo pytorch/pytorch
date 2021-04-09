e3091698bc112c691b3ca2ec99d9bb5dff9e6bd1
BowenBao <bowbao@microsoft.com>
2021-04-09 11:45:15 -0700

    Support .item() export & NumberType to tensor conversion

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -4206,6 +4206,19 @@ class TestONNXRuntime(unittest.TestCase):
         ind = torch.tensor(-2, dtype=torch.long)
         self.run_test(GetItemModel(), (x, y, z, ind))
 
+    def test_item(self):
+        class M(torch.nn.Module):
+            def comp(self, a:int, b:int):
+                return a > b
+
+            def forward(self, x, y, i:int) -> bool:
+                return int(x[y[i]].item()) > i
+
+        x = torch.arange(6, dtype=torch.float)
+        y = torch.tensor([0, 1, 2, 3, 4], dtype=torch.long)
+        i = 3
+        self.run_test(torch.jit.script(M()), (x, y, i))
+
     @disableScriptTest()  # torch.nonzero(x, as_tuple=True) is not scriptable.
     @skipIfUnsupportedMinOpsetVersion(9)
     def test_nonzero(self):
