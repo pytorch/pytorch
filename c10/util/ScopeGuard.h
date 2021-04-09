60b65f080e8acbdb45db8a6d277d9d85fb4cfa74
Dhruv Matani <dhruvbird@fb.com>
2021-04-09 15:05:41 -0700

    Update on "[PyTorch Edge] Simplify error logging in mobile/import.cpp"
    Currently, there is some complex logic that tries to handle all exceptions but re-throws them as a `c10::Error` so that it can log the error message. I'm looking for context on why this was added. The current logic (after talking with @swolchok) seems equivalent, simpler, and also preserves the original stack trace from where the exception was originally thrown. This is useful when viewing the backtrace in logview. Re-throwing an exception using `TORCH_CHECK(false, message)` results in the original exception stack trace getting lost, so we want to avoid that.
    
    Differential Revision: [D27688352](https://our.internmc.facebook.com/intern/diff/D27688352/)
    
    [ghstack-poisoned]

--- c10/util/ScopeGuard.h
+++ c10/util/ScopeGuard.h
@@ -8,8 +8,9 @@ class ScopeGuard {
  public:
   explicit ScopeGuard(std::function<void()> cb) : callback(std::move(cb)) {}
 
-  // Disallow copy, move, assignment.
+  // Disallow copy, move, assignment, and move assignment.
   ScopeGuard(const ScopeGuard &) = delete;
+  ScopeGuard(ScopeGuard &&) = delete;
   ScopeGuard &operator=(ScopeGuard &&) = delete;
   ScopeGuard &operator=(const ScopeGuard &) = delete;
 
