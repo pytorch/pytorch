7ef0cac0653b7973cd4fa426c3d505751e721360
Kurt Mohler <kmohler@quansight.com>
2021-04-09 15:30:10 -0500

    Restore original signature for compatibility

--- aten/src/ATen/native/EmbeddingBag.cpp
+++ aten/src/ATen/native/EmbeddingBag.cpp
@@ -704,6 +704,15 @@ embedding_bag(const Tensor &weight, const Tensor &indices,
   return out;
 };
 
+std::tuple<Tensor, Tensor, Tensor, Tensor>
+embedding_bag(const Tensor &weight, const Tensor &indices,
+              const Tensor &offsets, const bool scale_grad_by_freq,
+              const int64_t mode, bool sparse, const c10::optional<Tensor>& per_sample_weights_opt,
+              bool include_last_offset) {
+  return at::native::embedding_bag(weight, indices, offsets, scale_grad_by_freq,
+      mode, sparse, per_sample_weights_opt, include_last_offset, c10::nullopt);
+}
+
 // Assumes all input tensors except for `weight` are contiguous.
 // See NOTE [ embedding_bag Native Functions ] in native_functions.yaml for details
 std::tuple<Tensor, Tensor, Tensor, Tensor>
