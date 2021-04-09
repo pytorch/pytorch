7e278ca6e469a61bf4cad12d99f24524852ab3d6
David Riazati <davidriazati@fb.com>
2021-04-09 10:04:11 -0700

    Update on "Add breakpad to Docker image"
    This adds [breakpad](https://github.com/google/breakpad) to the build in CI (just on one image for now). I attempted in #54739 to build it from source as a normal third_party submodule but it uses autotools and has some weird build steps that made it hacky to integrate. We really only need it for release builds anyways since its use is moot if built with anything but `RELEASE=1`.
    
    Differential Revision: [D27679766](https://our.internmc.facebook.com/intern/diff/D27679766)
    
    [ghstack-poisoned]

--- .circleci/docker/common/install_breakpad.sh
+++ .circleci/docker/common/install_breakpad.sh
@@ -2,24 +2,12 @@
 
 set -ex
 
-# Get code
 git clone https://github.com/google/breakpad.git
 cd breakpad
-# Checkout a commit from the head of the Chrome 90 branch
-git checkout b324760c7f53667af128a6b77b790323da04fcb9
 
-# Get dependency for Linux builds
 git clone https://chromium.googlesource.com/linux-syscall-support src/third_party/lss
-cd src/third_party/lss
-# Pin to a commit that was working when this was committed
-git checkout 29f7c7e018f4ce706a709f0b0afbf8bacf869480
-cd ../../..
-
-# Build
 ./configure
 make
 make install
-
-# Cleanup
 cd ..
 rm -rf breakpad
