b82b01592dc7f761088d387ae9483090978d2134
ErjiaGuan <erjia@fb.com>
2021-04-12 17:36:05 -0400

    Update on "[DataLoader] Modfity construct_time_validation to argument_validation"
    Change construct_time_validation to argument_validation as we should provide users the flexibility to use this decorator over all different functions, which are required with type validation.
    
    It can also work as a construct-time validation
    ```py
    class ExampleDataPipe(IterDataPipe):
        @argument_validation
        def __init__(self, dp: IterDataPipe[int]):
            self.dp = dp
    
        ...
    ```
    Notebook is also updated.
    
    [ghstack-poisoned]

--- torch/utils/data/_typing.py
+++ torch/utils/data/_typing.py
@@ -234,6 +234,7 @@ class _DataPipeMeta(GenericMeta):
     Metaclass for `DataPipe`. Add `type` attribute and `__init_subclass__` based
     on the type, and validate the return hint of `__iter__`.
     """
+    type: _DataPipeType
 
     def __new__(cls, name, bases, namespace, **kargs):
         # For Python > 3.6
