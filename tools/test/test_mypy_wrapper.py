540133bb347a46b05f1dc74d420d85c6f7d557ec
Sam Estep <sestep@fb.com>
2021-04-12 11:27:03 -0700

    Use mypy internals instead of fnmatch for mypy wrapper (#55702)
    Summary:
    I noticed that https://github.com/pytorch/pytorch/issues/53296 added these two lines to the `files` list in `mypy-strict.ini`:
    ```
        benchmarks/instruction_counts/*.py,
        benchmarks/instruction_counts/*/*.py,
    ```
    I opened https://github.com/pytorch/pytorch/issues/55700 to simplify them into one line, but I was also curious whether `tools/mypy_wrapper.py` correctly handles those patterns, so I added the `test_glob_wildcards_dont_expand_or_collapse` case shown in this PR. Turns out, it doesn't!
    
    I believe this is because [`mypy` uses `glob`](https://github.com/python/mypy/blob/v0.770/mypy/config_parser.py#L45-L63) to parse these patterns, and for some reason, [`fnmatch`](https://docs.python.org/3/library/fnmatch.html) and [`glob`](https://docs.python.org/3/library/glob.html) don't agree with each other on what `*` means:
    
    - according to `fnmatch`, `*` seems to mean `.*`
    - according to `glob`, `*` seems to mean `[^/]*`
    
    [This SO answer](https://stackoverflow.com/a/60174071) suggests using the [`glob.globmatch` function from the `wcmatch` library](https://facelessuser.github.io/wcmatch/glob/#globmatch) to solve the issue, but [we didn't want to add another external dependency](https://github.com/pytorch/pytorch/pull/55702#discussion_r610868623), so instead I simply modified our matching function to just directly call `mypy`'s own internal function that does the globbing (linked above).
    
    One possible downside of this approach is that now the tests in `tools/test/test_mypy_wrapper.py` could break if the directory structure of PyTorch is changed.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55702
    
    Test Plan:
    ```
    python tools/test/test_mypy_wrapper.py
    ```
    
    Reviewed By: malfet, seemethere
    
    Differential Revision: D27684499
    
    Pulled By: samestep
    
    fbshipit-source-id: d99387a579c21eee73d1714e3e815ab7155f9646

--- tools/test/test_mypy_wrapper.py
+++ tools/test/test_mypy_wrapper.py
@@ -1,67 +1,77 @@
 import unittest
-from pathlib import PurePosixPath
 
 from tools import mypy_wrapper
 
 
 class TestMypyWrapper(unittest.TestCase):
-    def test_config_files(self):
+    def test_config_files(self) -> None:
         self.assertEqual(mypy_wrapper.config_files(), {
             'mypy.ini',
             'mypy-strict.ini',
         })
 
-    def test_glob_can_match_individual_files(self):
-        self.assertTrue(mypy_wrapper.glob(
+    def test_is_match_can_match_individual_files(self) -> None:
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='test/test_torch.py',
-            filename=PurePosixPath('test/test_torch.py'),
+            filename='test/test_torch.py',
         ))
-        self.assertFalse(mypy_wrapper.glob(
+        self.assertFalse(mypy_wrapper.is_match(
             pattern='test/test_torch.py',
-            filename=PurePosixPath('test/test_testing.py'),
+            filename='test/test_testing.py',
         ))
 
-    def test_glob_dir_matters(self):
-        self.assertFalse(mypy_wrapper.glob(
+    def test_is_match_dir_matters(self) -> None:
+        self.assertFalse(mypy_wrapper.is_match(
             pattern='tools/codegen/utils.py',
-            filename=PurePosixPath('torch/nn/modules.py'),
+            filename='torch/nn/modules.py',
         ))
-        self.assertTrue(mypy_wrapper.glob(
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='setup.py',
-            filename=PurePosixPath('setup.py'),
+            filename='setup.py',
         ))
-        self.assertFalse(mypy_wrapper.glob(
+        self.assertFalse(mypy_wrapper.is_match(
             pattern='setup.py',
-            filename=PurePosixPath('foo/setup.py'),
+            filename='foo/setup.py',
         ))
-        self.assertTrue(mypy_wrapper.glob(
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='foo/setup.py',
-            filename=PurePosixPath('foo/setup.py'),
+            filename='foo/setup.py',
         ))
 
-    def test_glob_can_match_dirs(self):
-        self.assertTrue(mypy_wrapper.glob(
+    def test_is_match_can_match_dirs(self) -> None:
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='torch',
-            filename=PurePosixPath('torch/random.py'),
+            filename='torch/random.py',
         ))
-        self.assertTrue(mypy_wrapper.glob(
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='torch',
-            filename=PurePosixPath('torch/nn/cpp.py'),
+            filename='torch/nn/cpp.py',
         ))
-        self.assertFalse(mypy_wrapper.glob(
+        self.assertFalse(mypy_wrapper.is_match(
             pattern='torch',
-            filename=PurePosixPath('tools/fast_nvcc/fast_nvcc.py'),
+            filename='tools/fast_nvcc/fast_nvcc.py',
         ))
 
-    def test_glob_can_match_wildcards(self):
-        self.assertTrue(mypy_wrapper.glob(
+    def test_is_match_can_match_wildcards(self) -> None:
+        self.assertTrue(mypy_wrapper.is_match(
             pattern='tools/autograd/*.py',
-            filename=PurePosixPath('tools/autograd/gen_autograd.py'),
+            filename='tools/autograd/gen_autograd.py',
         ))
-        self.assertFalse(mypy_wrapper.glob(
+        self.assertFalse(mypy_wrapper.is_match(
             pattern='tools/autograd/*.py',
-            filename=PurePosixPath('tools/autograd/deprecated.yaml'),
+            filename='tools/autograd/deprecated.yaml',
         ))
+
+    def test_is_match_wildcards_dont_expand_or_collapse(self) -> None:
+        self.assertFalse(mypy_wrapper.is_match(
+            pattern='benchmarks/instruction_counts/*.py',
+            filename='benchmarks/instruction_counts/core/utils.py',
+        ))
+        self.assertFalse(mypy_wrapper.is_match(
+            pattern='benchmarks/instruction_counts/*/*.py',
+            filename='benchmarks/instruction_counts/main.py',
+        ))
+
 
 if __name__ == '__main__':
     unittest.main()
