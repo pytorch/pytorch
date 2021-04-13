e5b9cb74c43a5708a5ee69ef5d1c5c97a0cb4314
Nikita Shulga <nshulga@fb.com>
2021-04-13 08:55:29 -0700

    Update test_jit_fuser_te.py

--- test/test_jit_fuser_te.py
+++ test/test_jit_fuser_te.py
@@ -1314,7 +1314,8 @@ class TestTEFuser(JitTestCase):
         dtypes = [
             torch.bool,
             torch.int,
-            torch.float16,
+            # TODO: Add back when https://github.com/pytorch/pytorch/issues/55905 is closed
+            # torch.float16,
             torch.float32,
             torch.float64,
         ]
