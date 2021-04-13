753264756d03b323ff52436560d2a8b7e4296890
Sam Estep <sestep@fb.com>
2021-04-13 12:55:35 -0700

    Remove redundant noqa

--- benchmarks/operator_benchmark/benchmark_all_test.py
+++ benchmarks/operator_benchmark/benchmark_all_test.py
@@ -1,7 +1,7 @@
 import benchmark_all_other_test  # noqa
 import benchmark_all_quantized_test  # noqa
 import operator_benchmark as op_bench
-from pt import unary_test  # noqa; noqa
+from pt import unary_test  # noqa
 
 if __name__ == "__main__":
     op_bench.benchmark_runner.main()
