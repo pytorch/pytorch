cc9f03c8b5b317b01c022a9e60721a751977d7e0
BowenBao <bowbao@microsoft.com>
2021-04-13 13:35:31 -0700

    Special post process for onnx::Cast and onnx::ConstantOfShape shape type inference

--- test/onnx/test_pytorch_onnx_shape_inference.py
+++ test/onnx/test_pytorch_onnx_shape_inference.py
@@ -1,72 +1,68 @@
 import unittest
 import torch
+import numpy as np
 
 import copy
 
-import test_pytorch_onnx_onnxruntime
-from test_pytorch_onnx_onnxruntime import TestONNXRuntime
-from torch.onnx import utils, OperatorExportTypes, TrainingMode
-from torch.onnx.utils import _validate_dynamic_axes
-from torch.onnx.symbolic_helper import (_set_opset_version, _set_operator_export_type,
-                                        _set_onnx_shape_inference, _set_training_mode,
-                                        _is_tensor_list, _is_tensor, _is_none)
+def expect_tensor(scalar_type, shape=None, dynamic_shape=None):
+    def verify(actual_type):
+        np.testing.assert_equal(actual_type.scalarType(), scalar_type)
+        if shape is not None:
+            np.testing.assert_equal(actual_type.sizes(), shape)
+        if dynamic_shape is not None:
+            np.testing.assert_equal(actual_type.varyingSizes(), dynamic_shape)
+    return verify
 
+class TestONNXShapeInference(unittest.TestCase):
+    from torch.onnx.symbolic_helper import _onnx_main_opset
+    opset_version = _onnx_main_opset
 
-def verify_inferred_shape(graph):
-    # Check every node in graph has type properly assigned.
-    for n in graph.nodes():
-        for out in n.outputs():
-            if not _is_tensor_list(out) and not _is_tensor(out) and not _is_none(out):
-                raise RuntimeError("Output of node is neither type Tensor nor type list of Tensor: ", out)
-            if _is_tensor(out) and out.type().scalarType() is None:
-                raise RuntimeError("Output of node does not have type assigned", out)
-            if _is_tensor(out) and out.type().dim() is None:
-                raise RuntimeError("Output of node does not have shape assigned", out)
+    def run_test(self, g, n, expected_types):
+        if not isinstance(expected_types, list):
+            expected_types = [expected_types]
 
+        torch._C._jit_pass_onnx_graph_shape_type_inference(g, {}, self.opset_version)
+        for out, expected_type in zip(n.outputs(), expected_types):
+            expected_type(out.type())
 
-def run_model_test(self, model, batch_size=2, state_dict=None,
-                   input=None, use_gpu=True, rtol=0.001, atol=1e-7,
-                   example_outputs=None, do_constant_folding=True,
-                   dynamic_axes=None, test_with_inputs=None,
-                   input_names=None, output_names=None,
-                   fixed_batch_size=False):
-    model.eval()
+    def create_empty_graph(self):
+        return torch._C.Graph()
 
-    if input is None:
-        input = torch.randn(batch_size, 3, 224, 224, requires_grad=True)
+    def insert_tensor_constant(self, g, tensor):
+        return g.op("Constant", value_t=tensor)
 
-    with torch.no_grad():
-        if isinstance(input, torch.Tensor):
-            input = (input,)
-        # In-place operators will update input tensor data as well.
-        # Thus inputs are replicated before every forward call.
-        input_copy = copy.deepcopy(input)
-        output = model(*input_copy)
-        if isinstance(output, torch.Tensor):
-            output = (output,)
+    def test_cast(self):
+        # Test cast with input of unknown scalar type.
+        g = self.create_empty_graph()
+        input = g.addInput()
+        cast_out = g.op("Cast", input, to_i=1)
+        self.run_test(g, cast_out.node(), expect_tensor('Float'))
 
-        _set_opset_version(self.opset_version)
-        _set_operator_export_type(OperatorExportTypes.ONNX)
-        _set_onnx_shape_inference(True)
-        _set_training_mode(False)
-        if dynamic_axes is None:
-            dynamic_axes = {}
-        _validate_dynamic_axes(dynamic_axes, model, input_names, output_names)
+    def test_constant_of_shape(self):
+        # Test ConstantOfShape with input of onnx::Shape node.
+        g = self.create_empty_graph()
+        constant = self.insert_tensor_constant(g, torch.ones(1, 2, 3, 4))
+        shape = g.op("Shape", constant)
+        constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', (1, 2, 3, 4)))
 
-        input_copy = copy.deepcopy(input)
-        graph, _, _ = utils._model_to_graph(model, input_copy,
-                                            input_names=input_names,
-                                            output_names=output_names,
-                                            operator_export_type=OperatorExportTypes.ONNX,
-                                            example_outputs=output,
-                                            do_constant_folding=do_constant_folding,
-                                            training=TrainingMode.EVAL,
-                                            dynamic_axes=dynamic_axes)
-        verify_inferred_shape(graph)
+        # Test ConstantOfShape with input of prim::ListConstruct of static tensor
+        rank = 4
+        g = self.create_empty_graph()
+        constants = [self.insert_tensor_constant(g, torch.tensor(i+1)) for i in range(rank)]
+        shape = g.op("prim::ListConstruct", *constants)
+        shape.setType(torch._C.ListType.ofInts())
+        constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', (1, 2, 3, 4)))
 
+        # Test ConstantOfShape with input of prim::ListConstruct of dynamic tensor
+        rank = 4
+        g = self.create_empty_graph()
+        inputs = [g.addInput() for i in range(rank)]
+        shape = g.op("prim::ListConstruct", *inputs)
+        shape.setType(torch._C.ListType.ofInts())
+        constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', None, (None, None, None, None)))
 
 if __name__ == '__main__':
-    TestONNXRuntime.opset_version = 12
-    test_pytorch_onnx_onnxruntime.run_model_test = run_model_test
-
     unittest.main()
