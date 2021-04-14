14f308a53666dbbe6570815a49ea9779b61b19c4
Sam Estep <sestep@fb.com>
2021-04-14 12:53:40 -0700

    Test CUDA initialization time too

--- test/test_import_time.py
+++ test/test_import_time.py
@@ -1,15 +1,22 @@
 import subprocess
+import sys
 
 from torch.testing._internal.common_utils import TestCase, run_tests
 
 
+# these tests could eventually be changed to fail if the import/init
+# time is greater than a certain threshold, but for now we just use them
+# as a way to track the duration of `import torch` in our ossci-metrics
+# S3 bucket (see tools/print_test_stats.py)
 class TestImportTime(TestCase):
-    # this test could eventually be changed to fail if the import time
-    # is greater than a certain threshold, but for now we just use it as
-    # a way to track the duration of `import torch` in our ossci-metrics
-    # S3 bucket (see tools/print_test_stats.py)
     def test_time_import_torch(self):
         subprocess.run([sys.executable, '-c', 'import torch'])
+
+    def test_time_cuda_device_count(self):
+        subprocess.run([
+            sys.executable, '-c',
+            'import torch; torch.cuda.device_count()',
+        ])
 
 
 if __name__ == '__main__':
