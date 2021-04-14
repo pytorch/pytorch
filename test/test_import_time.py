ff5d0e2b5964ab7ee344b86b26c4fb2007b0f05f
Sam Estep <sestep@fb.com>
2021-04-14 15:00:31 -0700

    Use TestCase.runWithPytorchAPIUsageStderr

--- test/test_import_time.py
+++ test/test_import_time.py
@@ -1,6 +1,3 @@
-import subprocess
-import sys
-
 from torch.testing._internal.common_utils import TestCase, run_tests
 
 
@@ -10,13 +7,12 @@ from torch.testing._internal.common_utils import TestCase, run_tests
 # S3 bucket (see tools/print_test_stats.py)
 class TestImportTime(TestCase):
     def test_time_import_torch(self):
-        subprocess.run([sys.executable, '-c', 'import torch'])
+        TestCase.runWithPytorchAPIUsageStderr('import torch')
 
     def test_time_cuda_device_count(self):
-        subprocess.run([
-            sys.executable, '-c',
+        TestCase.runWithPytorchAPIUsageStderr(
             'import torch; torch.cuda.device_count()',
-        ])
+        )
 
 
 if __name__ == '__main__':
