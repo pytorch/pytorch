2ae07403a4695cabae30a224f9603e52eddf38d6
Vasiliy Kuznetsov <vasiliy@fb.com>
2021-04-14 08:59:38 -0700

    ns for fx: move more weight matching logic to weight_utils.py (#55288)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55288
    
    No logic change, just moving util-like code to the utils file.
    
    Test Plan:
    ```
    python test/test_quantization.py TestFXNumericSuiteCoreAPIs
    ```
    
    Imported from OSS
    
    Reviewed By: hx89
    
    Differential Revision: D27575423
    
    fbshipit-source-id: cd5188a0940bb664be7d0275faa7df8ea18401a8

--- torch/quantization/ns/ns_types.py
+++ torch/quantization/ns/ns_types.py
@@ -3,6 +3,8 @@ from typing import NamedTuple
 
 from torch.fx.graph import Node
 
+from typing import Dict, Any, List
+
 class NSSingleResultValuesType(str, enum.Enum):
     WEIGHT = 'weight'
     NODE_OUTPUT = 'node_output'
@@ -12,3 +14,36 @@ NSSubgraph = NamedTuple(
     'NSSubgraph',
     [('start_node', Node), ('end_node', Node), ('base_op_node', Node)]
 )
+
+# TODO(future PR): see if we can use typing_extensions's TypedDict instead
+# to properly type the various keys
+# {
+#   # one of NSSingleResultValuesType
+#   'type': 'weight',
+#   # the values of type specified above
+#   'values': [torch.tensor(...), ...],
+#   # name of the node directly before the logger
+#   'prev_node_name': 'linear1',
+#   # type of the underlying function or module
+#   'prev_node_target_type': torch.nn.functional.linear  # or torch.nn.Linear, etc
+#   # name of the node responsible for adding this logger
+#   # Note: this may differ from prev_node_name if we are logging inputs
+#   'ref_node_name': 'linear1',
+#   # index of this node within the arg of the input/output node
+#   # for example, in cat([x1, x2, x3], dim=0), x2 would have index_within_arg == 1
+#   'index_within_arg': 0,
+# }
+NSSingleResultType = Dict[str, Any]
+
+# {
+#   'layer_name_1': {  # subgraph name
+#     'node_output': {  # results type (node_output, node_input, weight)
+#       'model_name_a':  # model name
+#          [NSSingleResultType, ...],  # results, ordered by index_within_arg
+#       'model_name_b':
+#          [NSSingleResultType, ...],
+#     },
+#   },
+# }
+#
+NSResultsType = Dict[str, Dict[str, Dict[str, List[NSSingleResultType]]]]
