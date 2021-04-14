b4a49de2fd6e699f0e7be27fa8d9315c9b628e47
hwangdeyu <deyhuang@qq.com>
2021-04-14 23:49:28 +0800

    fix bad optional access error

--- torch/csrc/jit/passes/onnx/scalar_type_analysis.cpp
+++ torch/csrc/jit/passes/onnx/scalar_type_analysis.cpp
@@ -309,7 +309,8 @@ static void LowPrecisionCastNodeForStandardOps(Node* n, int opset_version) {
   auto output_scalar_type =
       n->output()->type()->cast<TensorType>()->scalarType().value();
   for (size_t i = 0; i < n->inputs().size(); ++i) {
-    if (n->input(i)->type()->cast<TensorType>() == nullptr){
+    bool is_tensor = !!n->input(i)->type()->cast<TensorType>();
+    if (n->input(i)->type()->cast<TensorType>() == nullptr or is_tensor){
       // skip LowPrecisionCast if any op input type node is null.
       return;
     }
