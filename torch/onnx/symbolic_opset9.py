c0a43f05a720626f03dae943b54edad365a064d8
Jay Zhang <36183870+fatcat-z@users.noreply.github.com>
2021-04-15 11:06:44 -0700

    [ONNX] Add support for hann_window operator. (#54587)
    * [ONNX] Improve index_put symbolic to handle singular Bool updates (#53690)
    
    Adds support for cases where the updates to the index_put node is a single Bool value, such as the case shown below
    
    ```
    mask[indices] = True
    ```
    
    Fixes #53507
    
    * [ONNX] Support primitive type input/outputs and attributes (#53550)
    
    Support primitive type attributes. Needed for Silero model.
    
    * [ONNX] Fix if output shape mismatch error & Fix graph input directly used as output (#53219)
    
    Fix if output shape mismatch error & Fix graph input directly used as output
    
    * Add support for hann_window operator.
    
    * [ONNX] Replace decomposeLinear pre process pass with a symbolic (#53077)
    
    Replace decomposeLinear pre process pass with a symbolic
    
    * Add a test case for dtype is None.
    
    * Resolve flake8 issue.
    
    * Remove one unused test case.
    
    * Add support for hann_window operator.
    
    * Add a test case for dtype is None.
    
    * Remove one unused test case.
    
    Co-authored-by: Shubham Bhokare <32080845+shubhambhokare1@users.noreply.github.com>
    Co-authored-by: Negin Raoof <neginmr@utexas.edu>
    Co-authored-by: Bowen Bao <bowbao@microsoft.com>
    Co-authored-by: Ksenija Stanojevic <KsenijaS@users.noreply.github.com>
    
    [ghstack-poisoned]

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -3022,3 +3022,22 @@ def linear(g, input, weight, bias):
             output = add(g, bias, output)
 
     return output
+
+@parse_args('v', 'b', 'i', 'v', 'v', 'v', 'v')
+def hann_window(g, window_length, periodic=True, dtype=None, layout=None, device=None, pin_memory=None, requires_grad=False):
+    if dtype is None:
+        dtype = torch.get_default_dtype()
+        if sym_help._dtype_is_fp(dtype) is False:
+            dtype = torch.float
+        dtype = sym_help.scalar_type_to_pytorch_type.index(dtype)
+
+    n_array = arange(g, window_length, 4, None, None, None)
+    output = g.op('Cast', n_array, to_i=sym_help.cast_pytorch_to_onnx['Float'])
+    output = mul(g, g.op('Constant', value_t=torch.tensor(math.pi, dtype=torch.float)), output)
+
+    if periodic is False:
+        window_length = sub(g, window_length, g.op("Constant", value_t=torch.tensor(1, dtype=torch.int)))
+    output = div(g, output, window_length)
+    output = g.op("Cast", square(g, sin(g, output)), to_i=sym_help.scalar_type_to_onnx[dtype])
+
+    return output
