cfc97162469d759231cf65efee17b0adaeb6c825
Edward Yang <ezyang@fb.com>
2021-04-15 15:55:49 -0700

    Change all unary functions stubs to use TensorIteratorBase& (#56078)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56078
    
    This is in preparation for making all unary functions structured.
    I don't actually have to make them structured yet as TensorIterator&
    casts to TensorIteratorBase&
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Test Plan: Imported from OSS
    
    Reviewed By: mruberry
    
    Differential Revision: D27777768
    
    Pulled By: ezyang
    
    fbshipit-source-id: 05a3a95f200698eef72c5c74fff85fe881e1c4a3

--- aten/src/ATen/native/UnaryOps.h
+++ aten/src/ATen/native/UnaryOps.h
@@ -5,13 +5,10 @@
 #include <ATen/Generator.h>
 #include <stdexcept>
 
-namespace at { struct TensorIterator; }
-
 namespace at { namespace native {
 
-using unary_fn = void(*)(TensorIterator&);
-using unary_fn_with_scalar = void(*)(TensorIterator&, const Scalar& a);
-using structured_unary_fn = void(*)(TensorIteratorBase&);
+using unary_fn = void(*)(TensorIteratorBase&);
+using unary_fn_with_scalar = void(*)(TensorIteratorBase&, const Scalar& a);
 
 DECLARE_DISPATCH(unary_fn, abs_stub);
 DECLARE_DISPATCH(unary_fn, angle_stub);
@@ -19,7 +16,7 @@ DECLARE_DISPATCH(unary_fn, real_stub);
 DECLARE_DISPATCH(unary_fn, imag_stub);
 DECLARE_DISPATCH(unary_fn, conj_stub);
 DECLARE_DISPATCH(unary_fn, acos_stub);
-DECLARE_DISPATCH(structured_unary_fn, acosh_stub);
+DECLARE_DISPATCH(unary_fn, acosh_stub);
 DECLARE_DISPATCH(unary_fn, asinh_stub);
 DECLARE_DISPATCH(unary_fn, atanh_stub);
 DECLARE_DISPATCH(unary_fn, asin_stub);
@@ -29,8 +26,8 @@ DECLARE_DISPATCH(unary_fn, logical_not_stub);
 DECLARE_DISPATCH(unary_fn, ceil_stub);
 DECLARE_DISPATCH(unary_fn_with_scalar, clamp_max_stub);
 DECLARE_DISPATCH(unary_fn_with_scalar, clamp_min_stub);
-DECLARE_DISPATCH(structured_unary_fn, cos_stub);
-DECLARE_DISPATCH(structured_unary_fn, cosh_stub);
+DECLARE_DISPATCH(unary_fn, cos_stub);
+DECLARE_DISPATCH(unary_fn, cosh_stub);
 DECLARE_DISPATCH(unary_fn, digamma_stub);
 DECLARE_DISPATCH(unary_fn, entr_stub);
 DECLARE_DISPATCH(unary_fn, erf_stub);
@@ -43,7 +40,7 @@ DECLARE_DISPATCH(unary_fn, floor_stub);
 DECLARE_DISPATCH(unary_fn, frac_stub);
 DECLARE_DISPATCH(unary_fn, frexp_stub);
 DECLARE_DISPATCH(unary_fn, i0_stub);
-DECLARE_DISPATCH(structured_unary_fn, i0e_stub);
+DECLARE_DISPATCH(unary_fn, i0e_stub);
 DECLARE_DISPATCH(unary_fn, log_stub);
 DECLARE_DISPATCH(unary_fn, log10_stub);
 DECLARE_DISPATCH(unary_fn, log1p_stub);
@@ -58,9 +55,9 @@ DECLARE_DISPATCH(unary_fn_with_scalar, logit_stub);
 DECLARE_DISPATCH(unary_fn, sign_stub);
 DECLARE_DISPATCH(unary_fn, signbit_stub);
 DECLARE_DISPATCH(unary_fn, sgn_stub);
-DECLARE_DISPATCH(structured_unary_fn, sin_stub);
-DECLARE_DISPATCH(structured_unary_fn, sinc_stub);
-DECLARE_DISPATCH(structured_unary_fn, sinh_stub);
+DECLARE_DISPATCH(unary_fn, sin_stub);
+DECLARE_DISPATCH(unary_fn, sinc_stub);
+DECLARE_DISPATCH(unary_fn, sinh_stub);
 DECLARE_DISPATCH(unary_fn, sqrt_stub);
 DECLARE_DISPATCH(unary_fn, tan_stub);
 DECLARE_DISPATCH(unary_fn, tanh_stub);
@@ -68,25 +65,28 @@ DECLARE_DISPATCH(unary_fn, trigamma_stub);
 DECLARE_DISPATCH(unary_fn, trunc_stub);
 DECLARE_DISPATCH(unary_fn, lgamma_stub);
 
+// NB: these are actually defined in Distribution
 DECLARE_DISPATCH(void(*)(Tensor&, const Tensor&, c10::optional<Generator>), bernoulli_tensor_stub);
 DECLARE_DISPATCH(void(*)(Tensor&, const double, c10::optional<Generator>), bernoulli_scalar_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const double, const double, c10::optional<Generator>), cauchy_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const double, c10::optional<Generator>), exponential_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const double, c10::optional<Generator>), geometric_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const double, const double, c10::optional<Generator>), log_normal_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const double, const double, c10::optional<Generator>), uniform_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const double, const double, c10::optional<Generator>), cauchy_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const double, c10::optional<Generator>), exponential_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const double, c10::optional<Generator>), geometric_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const double, const double, c10::optional<Generator>), log_normal_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const double, const double, c10::optional<Generator>), uniform_stub);
 DECLARE_DISPATCH(void(*)(Tensor&, const double, const double, c10::optional<Generator>), normal_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const uint64_t, const int64_t, c10::optional<Generator>), random_from_to_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, c10::optional<Generator>), random_full_64_bits_range_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, c10::optional<Generator>), random_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const int64_t), polygamma_stub);
-DECLARE_DISPATCH(void(*)(TensorIterator&, const Scalar& a, const Scalar& b), clamp_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const uint64_t, const int64_t, c10::optional<Generator>), random_from_to_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, c10::optional<Generator>), random_full_64_bits_range_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, c10::optional<Generator>), random_stub);
+
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const int64_t, const double), kaiser_window_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const int64_t), polygamma_stub);
+DECLARE_DISPATCH(void(*)(TensorIteratorBase&, const Scalar& a, const Scalar& b), clamp_stub);
 DECLARE_DISPATCH(
     void (*)(Tensor&, const Tensor&, int64_t, c10::optional<Generator>),
     multinomial_with_replacement_stub);
 DECLARE_DISPATCH(
     void (*)(
-        TensorIterator&,
+        TensorIteratorBase&,
         c10::optional<double>,
         c10::optional<double>,
         c10::optional<double>),
