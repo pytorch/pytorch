e08d731911fd8c75e3d769000b2918f94e102db8
Kurt Mohler <kmohler@quansight.com>
2021-04-15 15:05:55 -0700

    Add complex autograd support for `torch.cross` (#55854)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/53512
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55854
    
    Reviewed By: nikithamalgifb
    
    Differential Revision: D27737571
    
    Pulled By: anjali411
    
    fbshipit-source-id: 38165b952cc4c9213d61c7d98b549b984c154927

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -4727,6 +4727,22 @@ class TestLinalg(TestCase):
         torch.cross(x, y, out=res2)
         self.assertEqual(res1, res2)
 
+    # TODO: This test should be removed and OpInfo should enable complex
+    #       types after this PR is merged:
+    #       https://github.com/pytorch/pytorch/pull/55483
+    @dtypes(torch.cdouble)
+    def test_cross_autograd(self, device, dtype):
+        x = torch.rand(100, 3, dtype=dtype, device=device, requires_grad=True)
+        y = torch.rand(100, 3, dtype=dtype, device=device, requires_grad=True)
+
+        if torch.device(device).type == 'cuda' and dtype.is_complex:
+            # TODO: Remove this error when cross CUDA supports complex
+            with self.assertRaisesRegex(RuntimeError, r'_th_cross_kernel_out not supported on CUDAType for Complex'):
+                gradcheck(torch.cross, [x, y])
+        else:
+            gradcheck(torch.cross, [x, y])
+            gradgradcheck(torch.cross, [x, y], atol=1e-3, check_batched_grad=False)
+
     @onlyCPU
     @dtypes(torch.float)
     def test_cross_with_and_without_dim(self, device, dtype):
