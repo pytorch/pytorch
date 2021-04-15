afbd24990e1eb24a049f25905a9e79f4d14984e6
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-04-15 06:50:41 -0500

    update pow opinfo to use broadcasts_input

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1884,30 +1884,30 @@ def _sample_inputs_svd(op_info, device, dtype, requires_grad=False, is_linalg_sv
 
 # Based on erstwhile method_tests tests & some tensor_op_tests for pow
 def sample_inputs_pow(op_info, device, dtype, requires_grad, **kwargs):
-    for_inplace_variant = kwargs.get('for_inplace_variant', False)
     samples = []
 
     if dtype in [torch.float16, torch.bfloat16, torch.float32, torch.float64]:
         test_cases = (
-            ((2, 2), 0, 5, 1e-3, requires_grad, (2, 2), 0, 1, 0.1, requires_grad),
-            ((2, 2), 0, 5, 1e-3, requires_grad, (1,), 0, 1, 0.1, requires_grad),
-            ((), 1e-3, 1e-3 + 1, 0, True, (), 0.1, 1.1, 0, False),
-            ((2, 2), 0, 5, 1e-3, requires_grad, (), 0.1, 1.1, 1, False),
+            ((2, 2), 0, 5, 1e-3, requires_grad, (2, 2), 0, 1, 0.1, requires_grad, False),
+            ((2, 2), 0, 5, 1e-3, requires_grad, (1,), 0, 1, 0.1, requires_grad, False),
+            ((), 1e-3, 1e-3 + 1, 0, True, (), 0.1, 1.1, 0, False, False),
+            ((2, 2), 0, 5, 1e-3, requires_grad, (), 0.1, 1.1, 1, False, False),
         )
         tests_require_resizing = (
-            ((1,), 0, 5, 1e-3, requires_grad, (2, 2), 0, 1, 0.1, requires_grad),
-            ((2, 1, 2), 0, 5, 1e-3, requires_grad, (1, 2, 1), 0, 1, 0.1, requires_grad),
-            ((), 1e-3, 1e-3 + 1, 0, True, (1, S, 1), 0, 1, 0.1, requires_grad),
+            ((1,), 0, 5, 1e-3, requires_grad, (2, 2), 0, 1, 0.1, requires_grad, True),
+            ((2, 1, 2), 0, 5, 1e-3, requires_grad, (1, 2, 1), 0, 1, 0.1, requires_grad, True),
+            ((), 1e-3, 1e-3 + 1, 0, True, (1, S, 1), 0, 1, 0.1, requires_grad, True),
         )
-        cases = test_cases if for_inplace_variant else (test_cases + tests_require_resizing)
+        cases = test_cases + tests_require_resizing
         samples = list(SampleInput(make_tensor(shape_b, low=low_b, high=high_b,
                                                requires_grad=b_grad, device=device,
                                                dtype=dtype) + additive_b,
                                    args=(make_tensor(shape_e, low=low_e, high=high_e,
                                                      requires_grad=e_grad, device=device,
-                                                     dtype=dtype) + additive_e,))
+                                                     dtype=dtype) + additive_e,),
+                                    broadcasts_input=broadcasts_input)
                        for shape_b, low_b, high_b, additive_b, b_grad, shape_e, low_e,
-                       high_e, additive_e, e_grad in cases)
+                       high_e, additive_e, e_grad, broadcasts_input in cases)
         tensor_scalar_inputs = (
             ((2, 2), 0, 5, 1e-3, requires_grad, (3.14,)),
             ((), 1e-3, 1e-3 + 1, 0, True, (3.14,))
