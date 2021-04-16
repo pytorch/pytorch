0e9ed37718ab6ef8ae32d3ba9ca1462a498abd93
Jerry Zhang <jerryzh@fb.com>
2021-04-16 01:24:41 -0700

    [nn] Add allow_duplicate option for named_modules (#54812)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54812
    
    Needed for quantization since different attribute might refer to the same module instance
    
    Test Plan: Imported from OSS
    
    Reviewed By: vkuzo
    
    Differential Revision: D27408376
    
    fbshipit-source-id: cada85c4a1772d3dd9502c3f6f9a56d690d527e7

--- torch/distributed/nn/api/remote_module.py
+++ torch/distributed/nn/api/remote_module.py
@@ -310,7 +310,7 @@ class _RemoteModule(nn.Module):
     def modules(self) -> Iterator[Module]:  # type: ignore[return]
         _raise_not_supported(self.modules.__name__)
 
-    def named_modules(self, memo: Optional[Set[Module]] = None, prefix: str = ""):
+    def named_modules(self, memo: Optional[Set[Module]] = None, prefix: str = "", remove_duplicate: bool = True):
         _raise_not_supported(self.named_modules.__name__)
 
     def train(self: T, mode: bool = True) -> T:  # type: ignore[return]
