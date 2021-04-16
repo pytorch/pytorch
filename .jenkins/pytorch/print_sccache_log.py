951e017fef05cd84f2f51674ab490eedaa82ca7a
Sam Estep <sam@samestep.com>
2021-04-16 10:50:04 -0700

    Rename from `keywords` to `ignored_keywords`
    Co-authored-by: Rong Rong <walterddr.walterddr@gmail.com>

--- .jenkins/pytorch/print_sccache_log.py
+++ .jenkins/pytorch/print_sccache_log.py
@@ -8,10 +8,10 @@ with open(log_file_path) as f:
 for line in lines:
     # Ignore errors from CPU instruction set, symbol existing testing,
     # or compilation error formatting
-    keywords = [
+    ignored_keywords = [
         'src.c',
         'CheckSymbolExists.c',
         'test_compilation_error_formatting',
     ]
-    if all([keyword not in line for keyword in keywords]):
+    if all([keyword not in line for keyword in ignored_keywords]):
         print(line)
