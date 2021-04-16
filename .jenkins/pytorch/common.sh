b7aadb4aa4138ee7c0c702f509847d00ca43e1bd
Eli Uriegas <eliuriegas@fb.com>
2021-04-16 10:32:38 -0700

    don't move this
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .jenkins/pytorch/common.sh
+++ .jenkins/pytorch/common.sh
@@ -156,7 +156,3 @@ fi
 retry () {
   "$@"  || (sleep 1 && "$@") || (sleep 2 && "$@")
 }
-
-# Enable LLVM dependency for TensorExpr testing
-export USE_LLVM=/opt/llvm
-export LLVM_DIR=/opt/llvm/lib/cmake/llvm
