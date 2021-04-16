ee5a24a0191a11a5889cb5cfa9919fad886ae978
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-16 14:58:54 -0700

    save

--- aten/src/ATen/native/cuda/Randperm.cuh
+++ aten/src/ATen/native/cuda/Randperm.cuh
@@ -26,9 +26,9 @@ __global__ void randperm_handle_duplicate_keys_kernel(T *keys, scalar_t *data, T
 
   // do random permutation inside each island.
   data += tid;
-  // auto seeds = at::cuda::philox::unpack(philox_args);
-  // curandStatePhilox4_32_10_t state;
-  // curand_init(std::get<0>(seeds), tid, std::get<1>(seeds), &state);
+  auto seeds = at::cuda::philox::unpack(philox_args);
+  curandStatePhilox4_32_10_t state;
+  curand_init(0, tid, 0, &state);
   for (int i = island_size - 1; i > 0; i--) {
     unsigned int r = 0;
     if (i != r) {
