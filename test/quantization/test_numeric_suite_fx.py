2380cc7d653b9701e29365863f2d1791adf8b978
Vasiliy Kuznetsov <vasiliy@fb.com>
2021-04-16 10:28:31 -0700

    ns for fx: fill out coverage for node I/O types (#55918)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55918
    
    Adds coverage for determining I/O dtype for various ops. This will
    enable shadowing of these ops.
    
    Test Plan:
    ```
    python test/test_quantization.py TestFXNumericSuiteCoreAPIs.test_op_io_dtype_coverage
    ```
    
    Imported from OSS
    
    Reviewed By: jerryzh168
    
    Differential Revision: D27740661
    
    fbshipit-source-id: c5ce873ec56bffa50ca46d2fe134c70ed677e37e

--- test/quantization/test_numeric_suite_fx.py
+++ test/quantization/test_numeric_suite_fx.py
@@ -42,6 +42,14 @@ from torch.quantization.ns.graph_matcher import (
     get_matching_subgraph_pairs,
     GraphMatchingException,
 )
+from torch.quantization.ns.mappings import (
+    FUNS_IO_TYPE_FP32,
+    FUNS_IO_TYPE_INT8,
+    FUNS_IO_TYPE_FP32_OR_INT8,
+    MODS_IO_TYPE_FP32,
+    MODS_IO_TYPE_INT8,
+    MODS_IO_TYPE_FP32_OR_INT8,
+)
 from torch.quantization._numeric_suite_fx import (
     extract_weights,
     _extract_weights_impl,
@@ -1041,6 +1049,129 @@ class TestFXNumericSuiteCoreAPIs(FXNumericSuiteQuantizationTestCase):
         }
         self.checkGraphModuleNodes(
             mp_shadows_mq_ns, expected_node_occurrence=unshadowed_expected_occurrence)
+
+    def test_op_io_dtype_coverage(self):
+        """
+        Tests that all the ops quantization cares about have input and output
+        dtypes defined.
+        """
+        base_name_to_sets_of_related_ops = get_base_name_to_sets_of_related_ops()
+        type_a_related_to_b = \
+            get_type_a_related_to_b(base_name_to_sets_of_related_ops)
+
+        # 1. check static quant module mappings
+        static_quant_mod_mappings = get_default_static_quant_module_mappings()
+        for fp32_type, int8_type in static_quant_mod_mappings.items():
+            types_to_skip = (
+                torch.quantization.QuantStub,
+                torch.quantization.DeQuantStub,
+                nnq.FloatFunctional,
+                # TODO(future PR): look into whether shadowing embeddings
+                # makes sense
+                nn.Embedding,
+                nn.EmbeddingBag,
+            )
+            if fp32_type in types_to_skip:
+                continue
+            self.assertTrue(
+                fp32_type in MODS_IO_TYPE_FP32,
+                f"missing IO type handling for f{fp32_type}")
+            self.assertTrue(
+                int8_type in MODS_IO_TYPE_INT8,
+                f"missing IO type handling for f{int8_type}")
+
+        # 2. check static quant op mappings
+        static_quant_fun_mappings = get_default_float_to_quantized_operator_mappings()
+        for fp32_type, int8_type in static_quant_fun_mappings.items():
+            self.assertTrue(
+                fp32_type in FUNS_IO_TYPE_FP32,
+                f"missing IO type handling for f{fp32_type}")
+            self.assertTrue(
+                int8_type in FUNS_IO_TYPE_INT8,
+                f"missing IO type handling for f{int8_type}")
+
+        # 3. check dynamic quant mappings
+        dynamic_quant_mappings = get_default_dynamic_quant_module_mappings()
+        for fp32_type1, fp32_type2 in dynamic_quant_mappings.items():
+            # TODO(future PR): verify correct I/O for these and remove from
+            # this list.
+            types_to_skip = (
+                nn.GRUCell,
+                nn.GRU,
+                nn.LSTMCell,
+                nn.RNNCell,
+            )
+            if fp32_type1 in types_to_skip:
+                continue
+            self.assertTrue(
+                fp32_type1 in MODS_IO_TYPE_FP32,
+                f"missing IO type handling for f{fp32_type1}")
+            self.assertTrue(
+                fp32_type2 in MODS_IO_TYPE_FP32,
+                f"missing IO type handling for f{fp32_type2}")
+
+        # 4. go through the ops mapped to each QuantizeHandler type, and verify
+        # correctness.
+        default_quant_patterns = get_default_quant_patterns()
+        for pattern, qhandler_cls in default_quant_patterns.items():
+            base_op = None
+            if isinstance(pattern, tuple):
+                base_op = pattern[-1]
+            elif isinstance(pattern, str):
+                # TODO(future PR): add handling for these
+                continue
+            else:
+                base_op = pattern
+
+            if (
+                qhandler_cls in (
+                    qp.BinaryOpQuantizeHandler,
+                    qp.RNNDynamicQuantizeHandler,
+                )
+            ):
+                # TODO(future PR): implement shadowing for binary ops
+                # TODO(future PR): implement shadowing for RNN ops
+                continue
+            elif qhandler_cls == qp.CatQuantizeHandler:
+                self.assertTrue(
+                    base_op in FUNS_IO_TYPE_FP32,
+                    f"missing IO type handling for {base_op}")
+            elif (
+                qhandler_cls in (
+                    qp.ConvReluQuantizeHandler,
+                    qp.LinearReLUQuantizeHandler,
+                    qp.BatchNormQuantizeHandler,
+                    qp.DefaultNodeQuantizeHandler,
+                    qp.ELUQuantizeHandler,
+                )
+            ):
+                self.assertTrue(
+                    (base_op in FUNS_IO_TYPE_FP32) or (base_op in MODS_IO_TYPE_FP32),
+                    f"missing IO type handling for {base_op}")
+            elif (
+                qhandler_cls in (
+                    qp.FixedQParamsOpQuantizeHandler,
+                    qp.CopyNodeQuantizeHandler,
+                )
+            ):
+                # TODO(future PR): move the skip list to a mapping and stop
+                # matching these nodes.
+                to_skip = set([
+                    operator.getitem,
+                ])
+                if base_op in to_skip:
+                    continue
+
+                self.assertTrue(
+                    (base_op in FUNS_IO_TYPE_FP32_OR_INT8) or
+                    (base_op in MODS_IO_TYPE_FP32_OR_INT8),
+                    f"missing IO type handling for {base_op}")
+            elif qhandler_cls == qp.EmbeddingQuantizeHandler:
+                # embedding shadowing is not implemented, for now
+                continue
+            else:
+                raise AssertionError(
+                    f"handing for {qhandler_cls} not implemented")
 
 
 class TestFXNumericSuiteCoreAPIsModels(FXNumericSuiteQuantizationTestCase):
