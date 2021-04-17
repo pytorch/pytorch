c361543e256588ec8f63d323c5ac4f2560161c07
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-17 10:05:12 -0700

    save

--- aten/src/ATen/native/cuda/Randperm.cuh
+++ aten/src/ATen/native/cuda/Randperm.cuh
@@ -10,7 +10,7 @@ namespace {
 
 // See note [Algorithm of randperm]
 template<typename T, typename scalar_t>
-__global__ void randperm_handle_duplicate_keys_kernel(T *keys, scalar_t *data, T mask, int n/*, at::PhiloxCudaState philox_args*/) {
+__global__ void randperm_handle_duplicate_keys_kernel(T *keys, scalar_t *data, T mask, int n, at::PhiloxCudaState philox_args) {
   int tid = threadIdx.x + blockDim.x * blockIdx.x;
 
   // find the beginning of islands
@@ -26,9 +26,9 @@ __global__ void randperm_handle_duplicate_keys_kernel(T *keys, scalar_t *data, T
 
   // do random permutation inside each island.
   data += tid;
-  // auto seeds = at::cuda::philox::unpack(philox_args);
+  auto seeds = at::cuda::philox::unpack(philox_args);
   curandStatePhilox4_32_10_t state;
-  curand_init(0, tid, 0, &state);
+  curand_init(std::get<0>(seeds), tid, std::get<1>(seeds), &state);
   for (int i = island_size - 1; i > 0; i--) {
     unsigned int r = 0;
     if (i != r) {
@@ -42,16 +42,16 @@ __global__ void randperm_handle_duplicate_keys_kernel(T *keys, scalar_t *data, T
 // See note [Algorithm of randperm]
 template<typename T, typename scalar_t>
 void randperm_handle_duplicate_keys(T *keys, scalar_t *data, int bits, int64_t n, c10::optional<at::Generator> &gen_) {
-  // auto gen = at::get_generator_or_default<at::CUDAGeneratorImpl>(gen_, at::cuda::detail::getDefaultCUDAGenerator());
-  // int64_t counter_offset = n;
-  // at::PhiloxCudaState rng_engine_inputs;
-  // {
-  //   // See Note [Acquire lock when using random generators]
-  //   std::lock_guard<std::mutex> lock(gen->mutex_);
-  //   rng_engine_inputs = gen->philox_cuda_state(counter_offset);
-  // }
+  auto gen = at::get_generator_or_default<at::CUDAGeneratorImpl>(gen_, at::cuda::detail::getDefaultCUDAGenerator());
+  int64_t counter_offset = n;
+  at::PhiloxCudaState rng_engine_inputs;
+  {
+    // See Note [Acquire lock when using random generators]
+    std::lock_guard<std::mutex> lock(gen->mutex_);
+    rng_engine_inputs = gen->philox_cuda_state(counter_offset);
+  }
   T mask = static_cast<T>((1UL << bits) - 1);
-  std::cout << "bits: " << bits /*<< ", seeds: " << rng_engine_inputs.seed_ << ", " << rng_engine_inputs.offset_.val*/ << std::endl;
+  std::cout << "bits: " << bits << ", seeds: " << rng_engine_inputs.seed_ << ", " << rng_engine_inputs.offset_.val << std::endl;
   randperm_handle_duplicate_keys_kernel<<<(n + 511) / 512, 512, 0, at::cuda::getCurrentCUDAStream()>>>(
     keys, data, mask, n/*, rng_engine_inputs*/);
 }
