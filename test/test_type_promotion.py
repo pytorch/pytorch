786c7c224ccbf64a5933377a3cd08843be0b91a6
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-04-18 18:30:32 -0500

    [skip ci] Add steps argument to tests

--- test/test_type_promotion.py
+++ test/test_type_promotion.py
@@ -325,8 +325,8 @@ class TestTypePromotion(TestCase):
         expected = torch.ones(0, dtype=torch.int64, device=device)
         self.assertEqual(torch.arange(False, False, device=device), expected)
 
-        self.assertEqual(torch.linspace(False, True, device=device), torch.linspace(0, 1, device=device))
-        self.assertEqual(torch.logspace(False, True, device=device), torch.logspace(0, 1, device=device))
+        self.assertEqual(torch.linspace(False, True, 100, device=device), torch.linspace(0, 1, 100, device=device))
+        self.assertEqual(torch.logspace(False, True, 100, device=device), torch.logspace(0, 1, 100, device=device))
 
         # this seems like odd behavior but ints also create float tensors, numpy doesn't have this function.
         self.assertEqual(torch.scalar_tensor(False, device=device), torch.tensor(0., device=device))
