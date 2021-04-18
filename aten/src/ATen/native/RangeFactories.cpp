43c17acfc60b01df6836183cc1ecb589dea9de83
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-04-17 23:50:47 -0500

    [skip ci]

--- aten/src/ATen/native/RangeFactories.cpp
+++ aten/src/ATen/native/RangeFactories.cpp
@@ -12,21 +12,10 @@ namespace at { namespace native {
 DECLARE_DISPATCH(void(*)(TensorIterator&, const Scalar&, const Scalar&, const Scalar&), arange_stub);
 DECLARE_DISPATCH(void(*)(TensorIterator&, const Scalar&, const Scalar&, int64_t), linspace_stub);
 
-Tensor& linspace_cpu_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, Tensor& result) {
-  const auto steps = optional_steps.value_or(100);
-  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
-
-  if (!optional_steps.has_value()) {
-    TORCH_WARN_ONCE(
-      "Not providing a value for linspace's steps is deprecated and will "
-      "throw a runtime error in a future release. This warning will appear "
-      "only once per process.");
-  }
-
+Tensor& linspace_cpu_out(const Scalar& start, const Scalar& end, const int64_t steps, Tensor& result) {
   if (result.numel() != steps) {
     result.resize_({steps});
   }
-
   if (steps == 0) {
     // skip
   } else if (steps == 1) {
@@ -43,17 +32,7 @@ Tensor& linspace_cpu_out(const Scalar& start, const Scalar& end, c10::optional<i
   return result;
 }
 
-Tensor& logspace_cpu_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, double base, Tensor& result) {
-  const auto steps = optional_steps.value_or(100);
-  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
-
-  if (!optional_steps.has_value()) {
-    TORCH_WARN_ONCE(
-      "Not providing a value for logspace's steps is deprecated and will "
-      "throw a runtime error in a future release. This warning will appear "
-      "only once per process.");
-  }
-
+Tensor& logspace_cpu_out(const Scalar& start, const Scalar& end, const int64_t steps, double base, Tensor& result) {
   if (result.numel() != steps) {
     result.resize_({steps});
   }
