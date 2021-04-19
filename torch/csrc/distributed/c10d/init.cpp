030b65994757a3ff7a8571ca86301e6794a72406
Edward Z. Yang <ezyang@fb.com>
2021-04-19 16:03:34 -0400

    Update on "Release GIL before destructing ProcessGroup classes"
    Part of fix for https://github.com/pytorch/pytorch/issues/56297
    
    Differential Revision: [D27855337](https://our.internmc.facebook.com/intern/diff/D27855337/)
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -51,7 +51,9 @@ public:
   IntrusivePtrNoGilDestructor(c10::intrusive_ptr<T> impl) : impl_(std::move(impl)) {}
   // This ctor is very important; see
   // https://github.com/pybind/pybind11/issues/2957
-  IntrusivePtrNoGilDestructor(T* impl) : impl_(c10::intrusive_ptr<T>::reclaim(impl)) {}
+  IntrusivePtrNoGilDestructor(T* impl) : impl_(c10::intrusive_ptr<T>::reclaim(impl)) {
+    c10::raw::intrusive_ptr::incref(impl);
+  }
   ~IntrusivePtrNoGilDestructor() {
     if (impl_) {
       if (PyGILState_Check()) {
