e744bc599ff161447df643b22906527cb87bd9c9
beningodfrey4 <beningodfrey4@gmail.com>
2021-04-21 00:50:19 +0530

    move 0-dim assert from Linear to kaiming_*

--- torch/nn/modules/linear.py
+++ torch/nn/modules/linear.py
@@ -84,8 +84,7 @@ class Linear(Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        if 0 not in self.weight.shape:
-            init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         if self.bias is not None:
             fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in)
