2b7f8044ecf7b7f6b00b1b71a66f1c659488bfb3
beningodfrey4 <beningodfrey4@gmail.com>
2021-04-21 00:10:09 +0530

    prevent kaiming init of 0-dimensional tensors in Linear

--- torch/nn/modules/linear.py
+++ torch/nn/modules/linear.py
@@ -84,7 +84,8 @@ class Linear(Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if 0 not in self.weight.shape:
+            init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         if self.bias is not None:
             fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in)
