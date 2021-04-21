6654e380014d903d34399a0e4f125c6f0aa695b4
Mike Guo <myguo@microsoft.com>
2021-04-21 10:17:05 +0800

    add test case for gzip format

--- test/test_profiler.py
+++ test/test_profiler.py
@@ -514,6 +514,34 @@ class TestProfiler(TestCase):
                 file_num += 1
             self.assertEqual(file_num, 3)
 
+        # test case for gzip file format
+        with TemporaryDirectoryName() as dname:
+            with profile(
+                activities=[
+                    torch.profiler.ProfilerActivity.CPU
+                ] + ([
+                    torch.profiler.ProfilerActivity.CUDA
+                ] if use_cuda else []),
+                schedule=torch.profiler.schedule(
+                    wait=1,
+                    warmup=1,
+                    active=2,
+                    repeat=3),
+                on_trace_ready=torch.profiler.tensorboard_trace_handler(dname, use_gzip=True)
+            ) as p:
+                for _ in range(18):
+                    self.payload(use_cuda=use_cuda)
+                    p.step()
+
+            self.assertTrue(os.path.exists(dname))
+            file_num = 0
+            for file_name in os.listdir(dname):
+                parts = file_name.split('.')
+                self.assertTrue(len(parts) > 4)
+                self.assertTrue(parts[-5].isdigit() and int(parts[-5]) > 0, "Wrong tracing file name pattern")
+                self.assertEqual(parts[-4:], ['pt', 'trace', 'json', 'gz'])
+                file_num += 1
+            self.assertEqual(file_num, 3)
 
 if __name__ == '__main__':
     run_tests()
