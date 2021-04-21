a4e84e0cc81e5c26b66e93515feabb5e6fd9aae1
Alexander <aocsa.cs@gmail.com>
2021-04-21 11:12:43 -0500

    Update on "CUDA support in the CSR layout: constructors"
    Closes gh-56485
    
    
    
    [ghstack-poisoned]

--- test/test_sparse_csr.py
+++ test/test_sparse_csr.py
@@ -2,7 +2,7 @@ import torch
 import warnings
 from torch.testing._internal.common_utils import TestCase, run_tests, load_tests
 from torch.testing._internal.common_device_type import \
-    (instantiate_device_type_tests, dtypes, onlyCPU, onlyCUDA)
+    (instantiate_device_type_tests, dtypes, onlyCPU)
 
 # load_tests from torch.testing._internal.common_utils is used to automatically filter tests for
 # sharding on sandcastle. This line silences flake warnings
