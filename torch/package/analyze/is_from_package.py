331c66cf45ba9d262b14767bd91b13caf88117cd
Lillian Johnson <lillianjohnson@fb.com>
2021-04-22 13:46:15 -0700

    Update on "[torch.package] is_from_package check"
    Differential Revision: [D27949829](https://our.internmc.facebook.com/intern/diff/D27949829)
    
    [ghstack-poisoned]

--- torch/package/analyze/is_from_package.py
+++ torch/package/analyze/is_from_package.py
@@ -7,7 +7,7 @@ def is_from_package(obj: Any) -> bool:
     """
     Return whether an object was loaded from a package.
 
-    Note: packaged objects from externed modules will return False
+    Note: packaged objects from externed modules will return ``False``.
     """
     if type(obj) == ModuleType:
         return is_mangled(obj.__name__)
