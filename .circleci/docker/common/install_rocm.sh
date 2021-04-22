1a2b27a11d51d4269e0d7cad95f5006c57a322ec
Aswin John Mathews <aswin.mathews@amd.com>
2021-04-22 17:30:02 +0000

    Update to match pytorch/builder; and run on CENTOS

--- .circleci/docker/common/install_rocm.sh
+++ .circleci/docker/common/install_rocm.sh
@@ -11,6 +11,8 @@ install_magma() {
     echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
     echo 'LIB += -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib -Wl,--rpath,$(MKLROOT)/lib -Wl,--rpath,/opt/rocm/magma/lib' >> make.inc
     echo 'DEVCCFLAGS += --amdgpu-target=gfx803 --amdgpu-target=gfx900 --amdgpu-target=gfx906 --amdgpu-target=gfx908 --gpu-max-threads-per-block=256' >> make.inc
+    # hipcc with openmp flag may cause isnan() on __device__ not to be found; depending on context, compiler may attempt to match with host definition
+    sed -i 's/^FOPENMP/#FOPENMP/g' make.inc
     export PATH="${PATH}:/opt/rocm/bin"
     make -f make.gen.hipMAGMA -j $(nproc)
     LANG=C.UTF-8 make lib/libmagma.so -j $(nproc) MKLROOT=/opt/conda
