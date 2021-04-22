79239cc516993a5b2c458e29b1de8cbab1c6a865
Taylor Robie <taylorrobie@fb.com>
2021-04-22 10:53:01 -0700

    fix name

--- torch/utils/benchmark/utils/cpp_jit.py
+++ torch/utils/benchmark/utils/cpp_jit.py
@@ -11,7 +11,7 @@ from typing import Any, List, Optional
 
 import torch
 from torch.utils.benchmark.utils._stubs import CallgrindModuleType, TimeitModuleType
-from torch.utils.benchmark.utils.common import make_temp_dir
+from torch.utils.benchmark.utils.common import _make_temp_dir
 from torch.utils import cpp_extension
 
 
