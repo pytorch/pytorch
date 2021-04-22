7dd867cb10ef1904110f46ad92dc3541bf5a26d2
Tao Xu <taox@fb.com>
2021-04-21 21:10:11 -0700

    [iOS GPU][Design] Support multiple tensors as outputs (#56072)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56072
    
    Currently, we don't support outputting more than one tensors on GPU. For example, if you do
    
    ```
    auto x = at::rand(1,4,2,2).metal();
    auto y = at::chunk(x,2,1);  //y is a tuple
    auto output1 = y[0].cpu();
    auto output2 = y[1].cpu();
    ```
    In the example above, when it hits `y[0].cpu()`, the command buffer will be committed to move `y[0]` from GPU to CPU. By the time it hits `y[1].cpu()`, since the command buffer has already become invalid, the temporary image that lives in `output2` has been recycled. Thus, a runtime exception will be thrown.
    
    The way we address it is using the observer pattern
    1. Before we flush the command buffer, we'll notify its the observers(a.k.a MPSImageWrapper objects) who hold the temporary images.
    2. When observers receive the notification, they'll turn the current temporary images into a static images.
    3. Now, when `.cpu()` happens, the output tensor can just read the data directly from the static image generated in the above step.
    
    You may be wondering does that have a hidden cost where all the intermediate tensors have hold unused static images? The answers is no. All intermediate tensors will be released once their reference counts become zero. Since the MetalTensorImpl is subclassing from the TensorImpl, we're overriding the release_resource method  which gives us a chance to release the underlying storage (textures and buffers) and remove observers from the command buffer. Therefore, once the intermediate tensors go away, the temporary images will be recycled immediately.
    ghstack-source-id: 127079751
    
    Test Plan:
    - We'll be using `at::chunk` to test this in the following diffs, as it returns a tuple that contains multiple tensors.
    - Sandcastle CI
    - CircleCI
    
    Reviewed By: dreiss
    
    Differential Revision: D27165886
    
    fbshipit-source-id: 290b0d77b1dc74990b25cbd0abb775df1ab47ca0

--- aten/src/ATen/native/metal/mpscnn/MPSImageWrapper.mm
+++ aten/src/ATen/native/metal/mpscnn/MPSImageWrapper.mm
@@ -1,10 +1,47 @@
 #import <ATen/native/metal/MetalCommandBuffer.h>
 #import <ATen/native/metal/MetalUtils.h>
-#import <ATen/native/metal/mpscnn/MPSCNNUtils.h>
 #import <ATen/native/metal/mpscnn/MPSCNNContext.h>
+#import <ATen/native/metal/mpscnn/MPSCNNUtils.h>
 #import <ATen/native/metal/mpscnn/MPSImage+Tensor.h>
 #import <ATen/native/metal/mpscnn/MPSImageUtils.h>
 #import <ATen/native/metal/mpscnn/MPSImageWrapper.h>
+
+using namespace at::native::metal;
+@interface MPSImageWrapperTrampoline : NSObject<PTMetalCommandBuffer>
++ (instancetype)newWithMPSImageWrapper:(MPSImageWrapper*)wrapper;
+@end
+
+@implementation MPSImageWrapperTrampoline {
+  MPSImageWrapper* _imageWrapper;
+}
+
++ (instancetype)newWithMPSImageWrapper:(MPSImageWrapper*)wrapper {
+  MPSImageWrapperTrampoline* trampoline = [MPSImageWrapperTrampoline new];
+  trampoline->_imageWrapper = wrapper;
+  return trampoline;
+}
+
+- (void)dealloc {
+  _imageWrapper = nullptr;
+}
+
+- (void)beginSynchronization {
+  if (_imageWrapper) {
+    _imageWrapper->prepare();
+  }
+}
+
+- (void)endSynchronization:(NSError*)error {
+  if (error) {
+    if (_imageWrapper) {
+      _imageWrapper->release();
+    }
+    // throw exceptions if we failed to flush the command buffer
+    TORCH_CHECK(error);
+  }
+}
+
+@end
 
 namespace at {
 namespace native {
@@ -12,17 +49,24 @@ namespace metal {
 
 MPSImageWrapper::MPSImageWrapper(IntArrayRef sizes) {
   _textureSizes = computeTextureSize(sizes);
+  _delegate = [MPSImageWrapperTrampoline newWithMPSImageWrapper:this];
+}
+
+MPSImageWrapper::~MPSImageWrapper() {
+  release();
 }
 
 void MPSImageWrapper::copyDataFromHost(const float* inputData) {
   TORCH_CHECK(inputData);
   _commandBuffer = [MetalCommandBuffer currentBuffer];
+  [_commandBuffer addSubscriber:_delegate];
   _image = createTemporaryImage(_commandBuffer, _textureSizes, inputData);
 }
 
 void MPSImageWrapper::copyDataToHost(float* hostData) {
   TORCH_CHECK(_image);
   synchronize();
+  TORCH_CHECK(_image && !_image.isTemporaryImage);
   copyToHost(hostData, _image);
 }
 
@@ -30,15 +74,10 @@ MPSImage* MPSImageWrapper::image() const {
   return _image;
 }
 
-void MPSImageWrapper::recycleImage() {
-  if ([_image isTemporaryImage]) {
-    [_image recycle];
-    [_commandBuffer remove:(MPSTemporaryImage*)_image];
-  }
-}
-
-void MPSImageWrapper::setCommandBuffer(MetalCommandBuffer* cb) {
-  _commandBuffer = cb;
+void MPSImageWrapper::setCommandBuffer(MetalCommandBuffer* commandBuffer) {
+  TORCH_CHECK(commandBuffer && commandBuffer.valid);
+  _commandBuffer = commandBuffer;
+  [_commandBuffer addSubscriber:_delegate];
 }
 MetalCommandBuffer* MPSImageWrapper::commandBuffer() const {
   return _commandBuffer;
@@ -56,9 +95,8 @@ void MPSImageWrapper::allocateTextureStorage(IntArrayRef sizes) {
 void MPSImageWrapper::allocateTemporaryTextureStorage(
     IntArrayRef sizes,
     MetalCommandBuffer* commandBuffer) {
-  TORCH_CHECK(commandBuffer)
+  setCommandBuffer(commandBuffer);
   _textureSizes = computeTextureSize(sizes);
-  _commandBuffer = commandBuffer;
   _image = createTemporaryImage(commandBuffer, _textureSizes);
 }
 
@@ -71,17 +109,37 @@ void MPSImageWrapper::copyFromTexture(MPSImage* image) {
 }
 
 void MPSImageWrapper::setTexture(MPSImage* image) {
-    TORCH_CHECK(image);
-    _image = image;
+  TORCH_CHECK(image);
+  if(image.isTemporaryImage) {
+    TORCH_CHECK(_commandBuffer && _commandBuffer.valid);
+  }
+  _image = image;
 }
 
-void MPSImageWrapper::synchronize() {
-  if ([_image isTemporaryImage]) {
+void MPSImageWrapper::prepare() {
+  // If the temporary image is still alive in the current command buffer,
+  // make it a static image.
+  if (_image.isTemporaryImage && _image.readCount != 0) {
     _image =
         createStaticImage((MPSTemporaryImage*)_image, _commandBuffer, false);
   }
-  [_commandBuffer synchronize];
+}
+
+void MPSImageWrapper::synchronize() {
+  if(_commandBuffer && _commandBuffer.valid) {
+    [_commandBuffer commit];
+  }
+}
+
+void MPSImageWrapper::release() {
+  if ([_image isTemporaryImage]) {
+    [_image recycle];
+    [_commandBuffer remove:(MPSTemporaryImage*)_image];
+  }
+  [_commandBuffer removeSubscriber:_delegate];
+  _delegate = nil;
   _commandBuffer = nil;
+  _image = nil;
 }
 
 }
