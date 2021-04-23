711fd8508da7603462182cedac59592ff5778e2a
BowenBao <bowbao@microsoft.com>
2021-04-23 16:09:44 -0700

    Update more test case and comment

--- test/onnx/test_pytorch_onnx_shape_inference.py
+++ test/onnx/test_pytorch_onnx_shape_inference.py
@@ -3,26 +3,26 @@ import torch
 import numpy as np
 
 
-def expect_tensor(scalar_type, shape=None, dynamic_shape=None):
+def expect_tensor(scalar_type, shape=None):
     def verify(actual_type):
         np.testing.assert_equal(actual_type.scalarType(), scalar_type)
+        # if shape is not None:
+        #     np.testing.assert_equal(actual_type.sizes(), shape)
         if shape is not None:
-            np.testing.assert_equal(actual_type.sizes(), shape)
-        if dynamic_shape is not None:
-            np.testing.assert_equal(actual_type.varyingSizes(), dynamic_shape)
+            np.testing.assert_equal(actual_type.varyingSizes(), shape)
     return verify
 
 class TestONNXShapeInference(unittest.TestCase):
     from torch.onnx.symbolic_helper import _onnx_main_opset
     opset_version = _onnx_main_opset
 
-    def run_test(self, g, n, expected_types):
-        if not isinstance(expected_types, list):
-            expected_types = [expected_types]
+    def run_test(self, g, n, type_assertion_funcs):
+        if not isinstance(type_assertion_funcs, list):
+            type_assertion_funcs = [type_assertion_funcs]
 
         torch._C._jit_pass_onnx_graph_shape_type_inference(g, {}, self.opset_version)
-        for out, expected_type in zip(n.outputs(), expected_types):
-            expected_type(out.type())
+        for out, type_assertion_func in zip(n.outputs(), type_assertion_funcs):
+            type_assertion_func(out.type())
 
     def create_empty_graph(self):
         return torch._C.Graph()
@@ -43,7 +43,7 @@ class TestONNXShapeInference(unittest.TestCase):
         constant = self.insert_tensor_constant(g, torch.ones(1, 2, 3, 4))
         shape = g.op("Shape", constant)
         constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
-        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', (1, 2, 3, 4)))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', shape=(1, 2, 3, 4)))
 
     def test_constant_of_shape_static(self):
         # Test ConstantOfShape with input of prim::ListConstruct of static tensor
@@ -53,7 +53,7 @@ class TestONNXShapeInference(unittest.TestCase):
         shape = g.op("prim::ListConstruct", *constants)
         shape.setType(torch._C.ListType.ofInts())
         constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
-        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', (1, 2, 3, 4)))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', shape=(1, 2, 3, 4)))
 
     def test_constant_of_shape_dynamic(self):
         # Test ConstantOfShape with input of prim::ListConstruct of dynamic tensor
@@ -63,7 +63,26 @@ class TestONNXShapeInference(unittest.TestCase):
         shape = g.op("prim::ListConstruct", *inputs)
         shape.setType(torch._C.ListType.ofInts())
         constant_of_shape = g.op("ConstantOfShape", shape, value_t=torch.tensor([2.0]))
-        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', dynamic_shape=(None, None, None, None)))
+        self.run_test(g, constant_of_shape.node(), expect_tensor('Float', shape=(None, None, None, None)))
+
+    def test_reshape(self):
+        g = self.create_empty_graph()
+        constant = self.insert_tensor_constant(g, torch.ones(2, 16, 5, 5))
+        constant_2 = self.insert_tensor_constant(g, torch.tensor([2, 0, -1]))
+        shape = g.op("Reshape", constant, constant_2)
+        self.run_test(g, shape.node(), expect_tensor('Float', shape=(2, 16, 25)))
+
+        g = self.create_empty_graph()
+        constant = self.insert_tensor_constant(g, torch.ones(2, 16, 5, 4))
+        constant_2 = self.insert_tensor_constant(g, torch.tensor([-1, 0, 4]))
+        shape = g.op("Reshape", constant, constant_2)
+        self.run_test(g, shape.node(), expect_tensor('Float', shape=(10, 16, 4)))
+
+        g = self.create_empty_graph()
+        constant = self.insert_tensor_constant(g, torch.ones(2, 16, 5, 4))
+        constant_2 = self.insert_tensor_constant(g, torch.tensor([-1, 0, 0]))
+        shape = g.op("Reshape", constant, constant_2)
+        self.run_test(g, shape.node(), expect_tensor('Float', shape=(8, 16, 5)))
 
 if __name__ == '__main__':
     unittest.main()
