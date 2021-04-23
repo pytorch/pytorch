3056ea672b431da2e755de8b112dac27d47b0546
driazati <driazati@users.noreply.github.com>
2021-04-23 14:55:40 -0700

    verbose ninja

--- tools/setup_helpers/cmake.py
+++ tools/setup_helpers/cmake.py
@@ -348,5 +348,5 @@ class CMake:
         if IS_WINDOWS and not USE_NINJA:  # We are likely using msbuild here
             build_args += ['--', '/p:CL_MPCount={}'.format(max_jobs)]
         else:
-            build_args += ['--', '-j', max_jobs]
+            build_args += ['--', '-j', max_jobs, "-v"]
         self.run(build_args, my_env)
