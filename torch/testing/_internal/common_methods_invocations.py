78cfed5c81c149e6c60a2c7196ac6399f7438afe
Yukio Siraichi <yukio.siraichi@gmail.com>
2021-04-24 20:31:43 +0900

    Add `scatter` reduce variant for `OpInfo` tests.

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -3743,7 +3743,7 @@ def sample_inputs_scatter(op_info, device, dtype, requires_grad):
         return gather_variable(shape, index_dim, max_indices, device=device)
 
     zero = torch.tensor(0, dtype=torch.long, device=device)
-    test_cases = (
+    test_cases = [
         (_tensor((M, S)), (0, _gather((S, S), 1, M), _tensor((S, S)))),
         (_tensor((M, S)), (1, _gather((S, S), 0, S), _tensor((S, S)))),
         (_tensor((M, S)), (-1, _gather((S, S), 0, S), _tensor((S, S)))),
@@ -3772,9 +3772,16 @@ def sample_inputs_scatter_add(op_info, device, dtype, requires_grad):
         (_tensor((M, S)), (1, _gather((M, S // 2), 0, S), _tensor((M, S // 2)))),
         (_tensor((M, S)), (-1, _gather((M, S // 2), 0, S), _tensor((M, S // 2)))),
         (_tensor(()), (0, zero.clone().detach(), _tensor(()))),
-    )
+    ]
 
-    return [SampleInput(tensor, args=args) for tensor, args in test_cases]
+    samples = []
+    for tensor, args in test_cases:
+        for reduce in (None, 'add', 'multiply'):
+            samples.append(
+                SampleInput(tensor, args=args, kwargs={'reduce': reduce}),
+            )
+
+    return samples
 
 
 def sample_inputs_ravel(op_info, device, dtype, requires_grad, **kwargs):
