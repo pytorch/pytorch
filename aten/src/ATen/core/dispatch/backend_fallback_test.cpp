5d4616dd336c6f6e6c691ce86b3b2a0ce5a32e0b
Nikita Shulga <nshulga@fb.com>
2021-04-25 11:35:55 -0700

    NOLINT stubs for aten/src/ATen/core/dispatch/backend_fallback_test.cpp

--- aten/src/ATen/core/dispatch/backend_fallback_test.cpp
+++ aten/src/ATen/core/dispatch/backend_fallback_test.cpp
@@ -18,6 +18,7 @@ namespace {
 // but this could be used as a starting point to do more interesting things.
 
 // Global counter for ease of testing
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static int64_t override_call_count = 0;
 
 // Mode implementation
@@ -80,6 +81,7 @@ void generic_wrapper_fallback(const c10::OperatorHandle& op, torch::jit::Stack* 
   }
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(BackendFallbackTest, TestBackendFallbackWithMode) {
   auto m = MAKE_TORCH_LIBRARY_IMPL(_, TESTING_ONLY_GenericMode);
   m.fallback(torch::CppFunction::makeFromBoxedFunction<&generic_mode_fallback>());
@@ -87,21 +89,27 @@ TEST(BackendFallbackTest, TestBackendFallbackWithMode) {
   c10::impl::IncludeDispatchKeyGuard guard(DispatchKey::TESTING_ONLY_GenericMode);
 
   override_call_count = 0;
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   Tensor a = ones({5, 5}, kDouble);
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   Tensor b = batch_norm(a, {}, {}, {}, {}, true, 0.1, 1e-05, false);
   ASSERT_EQ(override_call_count, 2);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(BackendFallbackTest, TestBackendFallbackWithWrapper) {
   auto m = MAKE_TORCH_LIBRARY_IMPL(_, TESTING_ONLY_GenericWrapper);
   m.fallback(torch::CppFunction::makeFromBoxedFunction<&generic_wrapper_fallback>());
 
   override_call_count = 0;
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   Tensor a = at::detail::make_tensor<GenericWrapperTensorImpl>(ones({5, 5}, kDouble));
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   Tensor b = batch_norm(a, {}, {}, {}, {}, true, 0.1, 1e-05, false);
   ASSERT_EQ(override_call_count, 1);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(BackendFallbackTest, TestFallthroughBackendFallback) {
   auto m = MAKE_TORCH_LIBRARY_IMPL(aten, TESTING_ONLY_GenericMode);
   m.impl("mul.Tensor", torch::CppFunction::makeFromBoxedFunction<&generic_mode_fallback>());
@@ -113,6 +121,7 @@ TEST(BackendFallbackTest, TestFallthroughBackendFallback) {
 
   override_call_count = 0;
   // Doesn't trigger, as we fallthrough
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   Tensor a = zeros({5, 5}, kDouble);
   ASSERT_EQ(override_call_count, 0);
   // Does trigger, because we explicitly set it
