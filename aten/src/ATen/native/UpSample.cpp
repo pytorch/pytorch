851a894646a6a8d9cd5b14451bf423f56dfb49e7
Nikita Shulga <nshulga@fb.com>
2021-04-25 17:23:27 -0700

    NOLINT stubs for aten/src/ATen/native/UpSample.cpp

--- aten/src/ATen/native/UpSample.cpp
+++ aten/src/ATen/native/UpSample.cpp
@@ -10,14 +10,17 @@ TORCH_API c10::SmallVector<int64_t, 3> compute_output_size(
     c10::IntArrayRef input_size,  // Full input tensor size.
     c10::optional<c10::IntArrayRef> output_size,
     c10::optional<c10::ArrayRef<double>> scale_factors) {
+  // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-narrowing-conversions)
   int spatial_dimensions = input_size.size() - 2;
   if (output_size) {
     TORCH_CHECK(!scale_factors, "Must specify exactly one of output_size and scale_factors");
+    // NOLINTNEXTLINE(clang-diagnostic-sign-compare)
     TORCH_CHECK(output_size->size() == spatial_dimensions);
     return {output_size->data(), output_size->data() + output_size->size()};
   }
   if (scale_factors) {
     TORCH_CHECK(!output_size, "Must specify exactly one of output_size and scale_factors");
+    // NOLINTNEXTLINE(clang-diagnostic-sign-compare)
     TORCH_CHECK(scale_factors->size() == spatial_dimensions);
     c10::SmallVector<int64_t, 3> ret;
     for (int i = 0; i < spatial_dimensions; ++i) {
