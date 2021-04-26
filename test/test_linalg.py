f84f2063b43a32fbe88c7e4de7e1019a00adb302
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-04-26 09:47:13 -0700

    Port CUDA torch.geqrf to ATen (#56251)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56251
    
    This PR ports `torch.geqrf` from TH to ATen for CUDA path.
    
    Resolves https://github.com/pytorch/pytorch/issues/24569
    
    Test Plan: Imported from OSS
    
    Reviewed By: ngimel
    
    Differential Revision: D27960155
    
    Pulled By: mruberry
    
    fbshipit-source-id: a8b010c41d703a5de4bf40b045c89e6b95b5a5ca

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -7410,7 +7410,6 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
     @skipCUDAIfNoMagma
     @skipCPUIfNoLapack
     @dtypes(torch.float32, torch.float64, torch.complex64, torch.complex128)
-    @dtypesIfCUDA(torch.float32, torch.float64)
     def test_geqrf(self, device, dtype):
 
         def run_test(shape):
@@ -7438,11 +7437,6 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         batches = [(), (0, ), (2, ), (2, 1)]
         ns = [5, 2, 0]
         for batch, (m, n) in product(batches, product(ns, ns)):
-            # TODO: CUDA path doesn't work with batched or empty inputs
-            if self.device_type == 'cuda' and (batch != () or m == 0 or n == 0):
-                with self.assertRaisesRegex(RuntimeError, "A should be non-empty 2 dimensional"):
-                    run_test((*batch, m, n))
-                continue
             run_test((*batch, m, n))
 
     @skipCUDAIfNoMagma
