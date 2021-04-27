5e29b5da05cb01d044264916d714acf48ff97a1f
Vasiliy Kuznetsov <vasiliy@fb.com>
2021-04-27 16:26:26 -0700

    ns for fx: additional bugfix for user defined functions (#57028)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57028
    
    Adds a test case for wrapped sigmoid, and fixes the following issues
    to make it pass in NS:
    * allows comparing between x.sigmoid() and torch.sigmoid(x), if they are related
    * allows dtype cast from FP32_OR_INT8 to FP32, via dequantize (this will be improved later)
    
    Test Plan:
    ```
    python test/test_quantization.py TestFXNumericSuiteCoreAPIs.test_user_defined_function
    ```
    
    Reviewed By: jerryzh168
    
    Differential Revision: D28030089
    
    Pulled By: vkuzo
    
    fbshipit-source-id: b237353e2d564a4476f409df461746a259015a4b

--- torch/quantization/ns/graph_passes.py
+++ torch/quantization/ns/graph_passes.py
@@ -182,7 +182,12 @@ def _insert_dtype_cast_after_node(
         (node_input_type_a == NodeInputOrOutputType.FP32 and
          node_input_type_c == NodeInputOrOutputType.INT8) or
         (node_input_type_a == NodeInputOrOutputType.FP32 and
-         node_input_type_c == NodeInputOrOutputType.FP16)
+         node_input_type_c == NodeInputOrOutputType.FP16) or
+        # TODO(future PR): determine the actual dtype of node_c,
+        # the current code only works because dequantize works with
+        # multiple input dtypes.
+        (node_input_type_a == NodeInputOrOutputType.FP32 and
+         node_input_type_c == NodeInputOrOutputType.FP32_OR_INT8)
     ):
         dtype_cast_op = torch.dequantize
     elif (
