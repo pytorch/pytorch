ea0af6328864bbb18097e09191d4bceec7066293
Nikita Shulga <nshulga@fb.com>
2021-04-27 12:34:35 -0700

    NOLINT stubs for aten/src/TH/THTensorMoreMath.cpp

--- aten/src/TH/THTensorMoreMath.cpp
+++ aten/src/TH/THTensorMoreMath.cpp
@@ -3,14 +3,18 @@
 #include <TH/THBlas.h>
 #include <TH/THTensorDimApply.h>
 
+// NOLINTNEXTLINE(bugprone-suspicious-include)
 #include <TH/generic/THTensorMoreMath.cpp>
 #include <TH/THGenerateAllTypes.h>
 
+// NOLINTNEXTLINE(bugprone-suspicious-include)
 #include <TH/generic/THTensorMoreMath.cpp>
 #include <TH/THGenerateBoolType.h>
 
+// NOLINTNEXTLINE(bugprone-suspicious-include)
 #include <TH/generic/THTensorMoreMath.cpp>
 #include <TH/THGenerateBFloat16Type.h>
 
+// NOLINTNEXTLINE(bugprone-suspicious-include)
 #include <TH/generic/THTensorMoreMath.cpp>
 #include <TH/THGenerateHalfType.h>
