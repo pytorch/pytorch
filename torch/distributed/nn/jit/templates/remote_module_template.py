2177847e6a2e4e0946ed3242ad90c5aac1eb912e
Yi Wang <wayi@fb.com>
2021-04-26 20:00:57 -0700

    [RPC Framework] Supporting reading the input from the remote worker (#56943)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56943
    
    If the module is placed on a CUDA device, then all the CPU tensors in `args` and `kwargs` will also be implicitly moved to the same CUDA device to run forward.
    
    Currently still need to move the forward output from CUDA device back to CPU, until:
    1) Process group RPC backend is completely deprecated, and we always use TensorPipe RPC backend;
    2) A device map is explicitly provided to TensorPipe RPC backend.
    
    These steps will be done in a separate PR.
    
    #Original PR issue: https://github.com/pytorch/pytorch/issues/51670
    ghstack-source-id: 127457584
    
    Test Plan:
    buck test mode/dev-nosan caffe2/test/distributed/rpc:process_group_agent -- test_input_moved_to_cuda_device
    
    buck test mode/dev-nosan caffe2/test/distributed/rpc:process_group_agent -- test_input_moved_to_cuda_device_script
    
    buck test mode/dev-nosan caffe2/test/distributed/rpc:process_group_agent -- RemoteModule
    
    buck test mode/dev-nosan //caffe2/torch/fb/training_toolkit/applications/sparse_nn/batch_distributed_inference/tests:batch_distributed_inference_test -- --exact 'caffe2/torch/fb/training_toolkit/applications/sparse_nn/batch_distributed_inference/tests:batch_distributed_inference_test - test_load_di_parts (caffe2.torch.fb.training_toolkit.applications.sparse_nn.batch_distributed_inference.tests.batch_distributed_inference_test.BatchDistributedInferenceTest)'
    
    Reviewed By: wanchaol
    
    Differential Revision: D27934791
    
    fbshipit-source-id: de27e27b905db83cc52800e63684fc6c942e9dc7

--- torch/distributed/nn/jit/templates/remote_module_template.py
+++ torch/distributed/nn/jit/templates/remote_module_template.py
@@ -7,19 +7,57 @@ import torch.distributed.rpc as rpc
 from torch import Tensor
 from torch._jit_internal import Future
 from torch.distributed.rpc import RRef
+from typing import List, Tuple, Any
 
 
 {assign_module_interface_cls}
 
 
 {jit_script_decorator}
-def _remote_forward(module_rref: RRef[module_interface_cls], {arg_types}){arrow_and_return_type}:
+# WARNING: If the module is on a cuda device, any CPU tensor stored in a nested ``arg`` or `kwargs``,
+# will not be implicitly moved to the same cuda device.
+def _remote_forward(
+    module_rref: RRef[module_interface_cls], device: str, {arg_types}){arrow_and_return_type}:
     module = module_rref.local_value()
+    device = torch.device(device)
+
     return module.forward({args}, {kwargs})
+
+    if device.type != "cuda":
+        return module.forward({args}, {kwargs})
+
+    # If the module is on a cuda device,
+    # move any CPU tensor in args or kwargs to the same cuda device.
+    # Since torch script does not support generator expression,
+    # have to use concatenation instead of
+    # ``list(i.to(device) if isinstance(i, Tensor) else i for i in *args)``.
+    args = ({args},)
+    out_args: List[Any] = []
+    for arg in args:
+        out_args.append((arg.to(device),) if isinstance(arg, Tensor) else (arg,))
+
+    kwargs = {{{kwargs}}}
+    for k, v in kwargs.items():
+        if isinstance(v, Tensor):
+            kwargs[k] = kwargs[k].to(device)
+
+    # Since only CPU tensors are allowed to send over wire,
+    # need to move any GPU tensor to CPU in the output.
+    # Since torch script does not support generator expression,
+    # have to use concatenation instead of
+    # ``tuple(i.cpu() if isinstance(i, Tensor) else i for i in module.forward(*out_args, {kwargs}))``.
+    # TODO: Once process group RPC backend is deprecated,
+    # and a device map is explicitly provided to TensorPipe backend,
+    # we can leave the forward output on CUDA device and avoid the post-processing here.
+    ret: Tuple[Any] = ()
+    for i in module.forward(*out_args, {kwargs}):
+        i = (i.cpu(),) if isinstance(i, Tensor) else (i,)
+        ret = ret + i
+    return ret
 
 
 def forward_async(self, {arg_types}){arrow_and_future_return_type}:
-    args = (self.module_rref, {args})
+    args = (self.module_rref, self.device, {args})
     kwargs = {{{kwargs}}}
     return rpc.rpc_async(
         self.module_rref.owner(),
@@ -30,7 +68,7 @@ def forward_async(self, {arg_types}){arrow_and_future_return_type}:
 
 
 def forward(self, {arg_types}){arrow_and_return_type}:
-    args = (self.module_rref, {args})
+    args = (self.module_rref, self.device, {args})
     kwargs = {{{kwargs}}}
     ret_fut = rpc.rpc_async(
         self.module_rref.owner(),
