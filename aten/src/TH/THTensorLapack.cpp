cd7fc68d5263a59407309ac725b12800305c0ef6
Nikita Shulga <nshulga@fb.com>
2021-04-27 12:34:08 -0700

    NOLINT stubs for aten/src/TH/THTensorLapack.cpp

--- aten/src/TH/THTensorLapack.cpp
+++ aten/src/TH/THTensorLapack.cpp
@@ -1,5 +1,6 @@
 #include <TH/THTensor.hpp>
 #include <TH/THLapack.h>
 
+// NOLINTNEXTLINE(bugprone-suspicious-include)
 #include <TH/generic/THTensorLapack.cpp>
 #include <TH/THGenerateFloatTypes.h>
