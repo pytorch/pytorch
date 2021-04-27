fix normalization issues with lists of tensors

test/test_fx_experimental.py
@@ -3,6 +3,7 @@ import operator
 import unittest
 import sys
 import math
+import numbers
 from typing import Callable, Dict, Union, List
 from torch.fx.symbolic_trace import symbolic_trace
 from torch.fx.graph_module import GraphModule
@@ -29,7 +30,7 @@ from torch.fx.experimental import merge_matmul
 from torch.fx.experimental.normalize import NormalizeOperators, NormalizeArgs
 from torch.fx.experimental.schema_type_annotation import AnnotateTypesWithSchema
 from torch.testing._internal.common_nn import module_tests, new_module_tests
-from torch.fx.operator_schemas import _torchscript_type_to_python_type, normalize_function, normalize_module
+from torch.fx.operator_schemas import _torchscript_type_to_python_type, normalize_function, normalize_module, type_matches, create_type_hint
 from torch.fx.passes.shape_prop import extract_tensor_metadata, ShapeProp
 
 try:
@@ -1199,6 +1200,32 @@ class {test_classname}(torch.nn.Module):
         self.assertEqual(_count_matmuls(module), 2)
         self.assertEqual(_count_matmuls(opt_module), 2)
 
+    def test_type_matches(self):
+        should_be_equal = [
+            (int, type(5)),
+            (numbers.Number, type(5)),
+            (numbers.Number, type(5.0)),
+            (int, type(torch.float)),
+            (Union[int, float], type(5)),
+            (Union[int, float], type(5.0)),
+            (List[int], type(5)),
+            (List[int], create_type_hint([int, int])),
+            (List[int], create_type_hint((int, int))),
+            (List[torch.Tensor], create_type_hint([torch.Tensor, torch.Tensor])),
+            (List[torch.Tensor], create_type_hint([torch.nn.Parameter, torch.nn.Parameter])),
+            (torch.Tensor, torch.nn.Parameter),
+            (List[torch.Tensor], create_type_hint([torch.nn.Parameter, torch.Tensor])),
+            (List[torch.Tensor], create_type_hint([torch.Tensor, torch.nn.Parameter])),
+            (List[torch.Tensor], create_type_hint((torch.Tensor, torch.Tensor))),
+            (List[torch.Tensor], create_type_hint((torch.nn.Parameter, torch.nn.Parameter))),
+            (torch.Tensor, torch.nn.Parameter),
+            (List[torch.Tensor], create_type_hint((torch.nn.Parameter, torch.Tensor))),
+            (List[torch.Tensor], create_type_hint((torch.Tensor, torch.nn.Parameter))),
+        ]
+        for sig_type, arg_type in should_be_equal:
+            print(sig_type, arg_type)
+            self.assertTrue(type_matches(sig_type, arg_type))
+
     @skipIfNoMkldnn
     def test_prepare_for_inference_cpu(self):
         import torch.nn as nn
@@ -1264,12 +1291,11 @@ class TestNormalizeOperators(JitTestCase):
     @ops(op_db, allowed_dtypes=(torch.float,))
     def test_normalize_operator_exhaustive(self, device, dtype, op):
         # Unsupported input types
-        if op.name in {'index_put', '__getitem__', 'unfold', 'repeat', 'polygamma',
-                       'hsplit', 'vsplit', 'dsplit', 'einsum'}:
+        if op.name in {'index_put', '__getitem__', 'unfold', 'repeat', 'polygamma'}:
             return
         # These ops currently don't trace in FX for various reasons (i.e. they take a list of tensors)
-        fx_fail = {'stack', 'hstack', 'vstack', 'dstack', 'linalg.multi_dot'}
-        print(op.name)
+        fx_fail = {'stack', 'hstack', 'vstack', 'dstack',
+                   'linalg.multi_dot'}
         sample_inputs_itr = op.sample_inputs(device, dtype, requires_grad=False)
         for sample_input in sample_inputs_itr:
             unsupported_arg_type = False