e5fa8ab61e60b66676a5a696f66652561fee062f
Nikita Shulga <nshulga@fb.com>
2021-04-27 16:28:02 -0700

    NOLINT stubs for caffe2/core/init_test.cc

--- caffe2/core/init_test.cc
+++ caffe2/core/init_test.cc
@@ -7,7 +7,9 @@
 
 namespace caffe2 {
 namespace {
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 bool gTestInitFunctionHasBeenRun = false;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 bool gTestFailInitFunctionHasBeenRun = false;
 
 bool TestInitFunction(int*, char***) {
@@ -20,23 +22,29 @@ bool TestFailInitFunction(int*, char***) {
   return false;
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 REGISTER_CAFFE2_INIT_FUNCTION(
     TestInitFunction,
     &TestInitFunction,
     "Just a test to see if GlobalInit invokes "
     "registered functions correctly.");
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 int dummy_argc = 1;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 const char* dummy_name = "foo";
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-pro-type-const-cast)
 char** dummy_argv = const_cast<char**>(&dummy_name);
 } // namespace
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(InitTest, TestInitFunctionHasRun) {
   caffe2::GlobalInit(&dummy_argc, &dummy_argv);
   EXPECT_TRUE(gTestInitFunctionHasBeenRun);
   EXPECT_FALSE(gTestFailInitFunctionHasBeenRun);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(InitTest, CanRerunGlobalInit) {
   caffe2::GlobalInit(&dummy_argc, &dummy_argv);
   EXPECT_TRUE(caffe2::GlobalInit(&dummy_argc, &dummy_argv));
@@ -57,10 +65,13 @@ void LateRegisterFailInitFunction() {
       TestFailInitFunction, false, "This should fail for failed init");
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(InitTest, FailLateRegisterInitFunction) {
   caffe2::GlobalInit(&dummy_argc, &dummy_argv);
   LateRegisterInitFunction();
+  // NOLINTNEXTLINE(hicpp-avoid-goto,cppcoreguidelines-avoid-goto)
   EXPECT_THROW(LateRegisterEarlyInitFunction(), ::c10::Error);
+  // NOLINTNEXTLINE(hicpp-avoid-goto,cppcoreguidelines-avoid-goto)
   EXPECT_THROW(LateRegisterFailInitFunction(), ::c10::Error);
   EXPECT_TRUE(gTestInitFunctionHasBeenRun);
   EXPECT_TRUE(gTestFailInitFunctionHasBeenRun);
