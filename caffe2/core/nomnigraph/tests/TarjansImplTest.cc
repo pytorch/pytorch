17932091e97351ef89b1dc23d4cf0d8035897dac
Nikita Shulga <nshulga@fb.com>
2021-04-27 16:37:21 -0700

    NOLINT stubs for caffe2/core/nomnigraph/tests/TarjansImplTest.cc

--- caffe2/core/nomnigraph/tests/TarjansImplTest.cc
+++ caffe2/core/nomnigraph/tests/TarjansImplTest.cc
@@ -4,6 +4,7 @@
 
 #include "nomnigraph/Graph/Graph.h"
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(Tarjans, Simple) {
   TestClass t1;
   TestClass t2;
@@ -16,6 +17,7 @@ TEST(Tarjans, Simple) {
   EXPECT_EQ(sccs.size(), 1);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(Tarjans, WithEdgeStorage) {
   TestClass t1;
   TestClass t2;
@@ -28,27 +30,34 @@ TEST(Tarjans, WithEdgeStorage) {
   EXPECT_EQ(sccs.size(), 1);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(Tarjans, DAG) {
   auto graph = createGraph();
   auto sccs = nom::algorithm::tarjans(&graph);
   EXPECT_EQ(sccs.size(), 9);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(Tarjans, Cycle) {
   auto graph = createGraphWithCycle();
   auto sccs = nom::algorithm::tarjans(&graph);
   EXPECT_EQ(sccs.size(), 8);
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 TEST(Tarjans, Random) {
   nom::Graph<TestClass> g;
   std::vector<nom::Graph<TestClass>::NodeRef> nodes;
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   for (auto i = 0; i < 10; ++i) {
     TestClass t;
     nodes.emplace_back(g.createNode(std::move(t)));
   }
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   for (auto i = 0; i < 30; ++i) {
+    // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-narrowing-conversions,clang-analyzer-security.insecureAPI.rand)
     int ri1 = rand() % nodes.size();
+    // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-narrowing-conversions,clang-analyzer-security.insecureAPI.rand)
     int ri2 = rand() % nodes.size();
     g.createEdge(nodes[ri1], nodes[ri2]);
   }
