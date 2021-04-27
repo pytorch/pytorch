df78242623fe41ae600e7f2ba76b908f36a5bc1a
Eddie Yan <eddiey@nvidia.com>
2021-04-27 15:30:51 -0700

    fix #56822 (#56967)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/56822
    
    There was an off by one in CPU randperm when checking the limits of the requested range. Also shows up in the "CUDA" version as it will fallback to CPU for small input sizes.
    
    CC zasdfgbnm
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56967
    
    Reviewed By: mruberry
    
    Differential Revision: D28031819
    
    Pulled By: ngimel
    
    fbshipit-source-id: 4d25995628997f164aafe94e7eae6c54f018e4e5

--- test/test_tensor_creation_ops.py
+++ test/test_tensor_creation_ops.py
@@ -3280,7 +3280,8 @@ class TestRandomTensorCreation(TestCase):
         self.assertEqual(res2.numel(), 0)
 
         # Test exceptions when n is too large for a floating point type
-        for dtype, small_n, large_n in ((torch.half, 2**11 + 1, 2**11 + 2),
+        for dtype, small_n, large_n in ((torch.uint8, 2**8, 2**8 + 1),
+                                        (torch.half, 2**11 + 1, 2**11 + 2),
                                         (torch.float, 2**24 + 1, 2**24 + 2),
                                         (torch.double, 2**25,  # 2**53 + 1 is too large to run
                                          2**53 + 2)):
