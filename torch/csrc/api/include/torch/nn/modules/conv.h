f2bebf528c3773dbce5d0a3dd51a0586fc979512
Nikita Shulga <nshulga@fb.com>
2021-04-27 23:28:43 -0700

    NOLINT stubs for torch/csrc/api/src/nn/modules/conv.cpp

--- torch/csrc/api/include/torch/nn/modules/conv.h
+++ torch/csrc/api/include/torch/nn/modules/conv.h
@@ -25,6 +25,7 @@ template <size_t D, typename Derived>
 class ConvNdImpl : public torch::nn::Cloneable<Derived> {
  public:
   explicit ConvNdImpl(detail::ConvNdOptions<D> options_) : options(std::move(options_)) {
+    // NOLINTNEXTLINE(clang-analyzer-optin.cplusplus.VirtualCall)
     reset();
   }
 
