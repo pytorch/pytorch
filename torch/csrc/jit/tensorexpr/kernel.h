02b5835e6922463413db26586017f51092888426
Mikhail Zolotukhin <mvz@fb.com>
2021-04-27 17:25:32 -0700

    Update base for Update on "[JIT] Add a pass for annotating graph with input types derived from sample inputs."
    This pass is intended to be used in conjunction with shape propagation
    pass: first we use sample inputs to specify shape info for graph inputs
    and then we run shape-prop to infer shapes of intermediate values in the
    graph.
    
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/kernel.h
+++ torch/csrc/jit/tensorexpr/kernel.h
@@ -57,10 +57,6 @@ class TORCH_API TensorExprKernel {
     return graph_;
   }
 
-  std::vector<ConstantDescr> getConstantDescriptors() const {
-    return constants_;
-  };
-
  private:
   enum ElementType {
     kAllTypes = 0,
@@ -82,6 +78,7 @@ class TORCH_API TensorExprKernel {
 
   void compile();
   void genInputDebugNames();
+
   void runKernel(Stack& stack);
 
   std::vector<DimArg> dimsFromSizes(const std::vector<ExprHandle>& sizes);
