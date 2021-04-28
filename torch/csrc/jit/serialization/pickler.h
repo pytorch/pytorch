2fb27b8645e6786ed23f582560a3886c7c277e53
Nikita Shulga <nshulga@fb.com>
2021-04-28 01:28:23 -0700

    NOLINT stubs for torch/csrc/jit/serialization/pickler.cpp

--- torch/csrc/jit/serialization/pickler.h
+++ torch/csrc/jit/serialization/pickler.h
@@ -92,6 +92,7 @@ enum class PickleOpCode : char {
 
 using ::c10::IValue;
 
+// NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
 struct WriteableTensorData {
   const char* data() const {
     return static_cast<const char*>(tensor_.storage().data());
@@ -133,6 +134,7 @@ class TORCH_API Pickler {
         tensor_table_(tensor_table),
         type_renamer_(std::move(type_renamer)),
         memoized_class_types_(memoized_class_types) {}
+  // NOLINTNEXTLINE(bugprone-exception-escape)
   ~Pickler();
 
   // Push protocol onto the stack
