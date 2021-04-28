1c7526b1f5d3bf0c43cad97ff352070775d51e42
Nikita Shulga <nshulga@fb.com>
2021-04-27 19:42:40 -0700

    NOLINT stubs for caffe2/operators/quantized/int8_conv_op_relu.cc

--- caffe2/operators/quantized/int8_conv_op_relu.cc
+++ caffe2/operators/quantized/int8_conv_op_relu.cc
@@ -2,6 +2,7 @@
 
 namespace caffe2 {
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 REGISTER_CPU_OPERATOR(Int8ConvRelu, int8::Int8ConvOp<int8::Activation::RELU>);
 
 } // namespace caffe2
