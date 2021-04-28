7da28b5203ff85f53b4c585a1cfbe455a77b4b97
Nikita Shulga <nshulga@fb.com>
2021-04-27 23:55:44 -0700

    NOLINT stubs for torch/csrc/autograd/python_anomaly_mode.cpp

--- torch/csrc/autograd/python_anomaly_mode.h
+++ torch/csrc/autograd/python_anomaly_mode.h
@@ -8,7 +8,9 @@
 namespace torch { namespace autograd {
 
 struct PyAnomalyMetadata : public AnomalyMetadata {
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables,clang-diagnostic-writable-strings)
   static constexpr char* ANOMALY_TRACE_KEY = "traceback_";
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables,clang-diagnostic-writable-strings)
   static constexpr char* ANOMALY_PARENT_KEY = "parent_";
 
   PyAnomalyMetadata() {
