19e974622026a25737cd9b87a7463c0ae1aba4f8
chenlai <chenlai@devvm2090.vll0.facebook.com>
2021-04-29 06:48:48 -0700

    [PyTorch][Edge] Fix mypy error (#56999)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56999
    
    ## Summary
    Currently
    
    ## Test
    ![image](https://user-images.githubusercontent.com/16430979/116294682-19acaf80-a74d-11eb-9596-3a1d697ae835.png)
    Note: there are still some other mypy failure for other functions in other repo
    
    Differential Revision: D28023671
    
    Test Plan:
    See the test image above
    Also CI
    
    Reviewed By: dhruvbird
    
    Pulled By: cccclai
    
    fbshipit-source-id: d59da32b8b5a12c3f13bc5f4e02794db01132be3

--- torch/jit/mobile/__init__.py
+++ torch/jit/mobile/__init__.py
@@ -73,5 +73,4 @@ def _export_operator_list(module: LiteScriptModule):
         return a set of root operator names (with overload name) that are used by any method
         in this mobile module.
     """
-    # TODO fix mypy here
-    return torch._C._export_operator_list(module._c)  # type: ignore[attr-defined]
+    return torch._C._export_operator_list(module._c)
