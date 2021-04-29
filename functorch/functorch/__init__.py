57fc8d0aa461a16a03a92f3d1521052d6643f96b
Richard Zou <zou3519@gmail.com>
2021-04-29 11:56:21 -0700

    [functorch] Quick temporary fix for python key

--- functorch/functorch/__init__.py
+++ functorch/functorch/__init__.py
@@ -4,7 +4,12 @@ from . import _C
 from ._src.vmap import vmap
 from ._src.eager_transforms import grad, grad_with_value, vjp, jacrev
 from ._src.make_functional import make_functional, make_functional_with_buffers
-from ._src.python_key import key_wrap, ModuleWrap
+
+# Python key stuff is not in pytorch/pytorch core
+try:
+    from ._src.python_key import key_wrap, ModuleWrap
+except:
+    pass
 
 
 # Monkeypatching lol
