df495f156cd657fdbc7816cfe6b3b63ba88dcafd
Arindam Roy <rarindam@gmail.com>
2021-04-29 11:05:09 -0700

    ROCM: Re-enable test_eig_with_eigvec_cuda_float64
    After PR #56279 was merged, disabling gradgradcheck,
    the test is not flaky now.
    Hence re-enabling the test.
    
    Signed-off-by: Arindam Roy <rarindam@gmail.com>

--- test/test_torch.py
+++ test/test_torch.py
@@ -7877,7 +7877,7 @@ tensor_op_tests = [
     ('geqrf', '', _new_t((20, 20)), lambda t, d: [],
         1e-5, 1e-5, 3e-4, _float_types_no_half, _cpu_types, False, [skipCUDAIfNoMagma, skipCPUIfNoLapack]),
     ('eig', 'with_eigvec', _new_t((10, 10)), lambda t, d: [True],
-        1e-5, 1e-5, 1e-5, _float_types_no_half, _cpu_types, False, [skipCUDAIfNoMagma, onlyOnCPUAndCUDA, skipIfRocm]),
+        1e-5, 1e-5, 1e-5, _float_types_no_half, _cpu_types, False, [skipCUDAIfNoMagma, onlyOnCPUAndCUDA]),
 ]
 
 # Creates and decorates a generic test and adds it to the class.
