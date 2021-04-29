4f85a731cba36618dc434fe01653e5032a06eaac
Serhat Yilmaz <serhaty@fb.com>
2021-04-29 15:40:36 -0700

    [torch] Add backward support for segment reduce (CPU only)
    Summary:
    This is to setup boiler plate code for backward and CPU implementation.
    
    Next Steps in order:
    - Add backward support for CUDA
    - Add support for more aggregation types
    - Benchmarking (for cuda mainly)/more testing/documentation
    - Support for multi dimension
    
    Test Plan:
    Updated unit test to also check correctness of backward.
    
    Wait for CI signal
    
    Reviewed By: ngimel
    
    Differential Revision: D27970340
    
    fbshipit-source-id: 3e608c7fe3628b0a761dd8affc6aad8f65a6ef7f

--- aten/src/ATen/native/SegmentReduce.cpp
+++ aten/src/ATen/native/SegmentReduce.cpp
@@ -9,6 +9,7 @@ namespace native {
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 DEFINE_DISPATCH(_segment_reduce_stub);
+DEFINE_DISPATCH(_segment_reduce_backward_stub);
 
 namespace {
 
@@ -61,6 +62,54 @@ Tensor _segment_reduce_cpu_kernel(
   return output;
 }
 
+Tensor _segment_reduce_cpu_backward_kernel(
+    const Tensor& grad_contig,
+    const Tensor& output_contig,
+    const Tensor& data_contig,
+    const Tensor& lengths_contig) {
+  auto grad_input = at::zeros({data_contig.sizes()}, grad_contig.options());
+
+  int64_t batch_size = lengths_contig.numel();
+  const auto* lengths_data = lengths_contig.data_ptr<int64_t>();
+
+  AT_DISPATCH_ALL_TYPES_AND2(
+      kBFloat16,
+      kHalf,
+      data_contig.scalar_type(),
+      "_segment_reduce_cpu",
+      ([&]() {
+        auto* output_data = output_contig.data_ptr<scalar_t>();
+        auto* grad_data = grad_contig.data_ptr<scalar_t>();
+        auto* grad_input_data = grad_input.data_ptr<scalar_t>();
+        const auto* values_data = data_contig.data_ptr<scalar_t>();
+        int64_t k = 0;
+        for (int64_t i = 0; i < batch_size; ++i) {
+          int64_t counter = 0;
+          for (int64_t j = 0; j < lengths_data[i]; ++j) {
+            if (at::_isnan(values_data[k]) ||
+                values_data[k] == output_data[i]) {
+              grad_input_data[k] = grad_data[i];
+              counter++;
+            }
+            k++;
+          }
+          // Average gradient output based on number of maximum in the segment
+          TORCH_INTERNAL_ASSERT(counter > 0);
+          if (counter < 2) {
+            continue;
+          }
+          for (int64_t j = 0; j < lengths_data[i]; ++j) {
+            int64_t index = k - j - 1;
+            if (grad_input_data[index] > 0) {
+              grad_input_data[index] = grad_input_data[index] / counter;
+            }
+          }
+        }
+      }));
+
+  return grad_input;
+}
+
 } // namespace
 
 enum SegmentReductionType { MAX };
@@ -107,6 +156,45 @@ REGISTER_AVX_DISPATCH(_segment_reduce_stub, &_segment_reduce_cpu_kernel);
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 REGISTER_AVX2_DISPATCH(_segment_reduce_stub, &_segment_reduce_cpu_kernel);
 REGISTER_VSX_DISPATCH(_segment_reduce_stub, &_segment_reduce_cpu_kernel);
+
+// Currently some computation is beind duplicated across forward and backward.
+// TODO: Cache indices in forward pass to re-use in backward
+Tensor segment_reduce_backward_kernel(
+    const Tensor& grad,
+    const Tensor& output,
+    const Tensor& data,
+    const c10::optional<Tensor>& lengths) {
+  TORCH_CHECK(
+      lengths.has_value(),
+      "Currently only lengths based reduction is supported!")
+  const auto& lengths_value = lengths.value();
+
+  const auto grad_contig = grad.contiguous();
+  const auto output_contig = output.contiguous();
+  const auto data_contig = data.contiguous();
+  const auto lengths_contig = lengths_value.contiguous();
+
+  return _segment_reduce_backward_stub(
+      grad_contig.device().type(),
+      grad_contig,
+      output_contig,
+      data_contig,
+      lengths_contig);
+}
+
+REGISTER_ARCH_DISPATCH(
+    _segment_reduce_backward_stub,
+    DEFAULT,
+    &_segment_reduce_cpu_backward_kernel);
+REGISTER_AVX_DISPATCH(
+    _segment_reduce_backward_stub,
+    &_segment_reduce_cpu_backward_kernel);
+REGISTER_AVX2_DISPATCH(
+    _segment_reduce_backward_stub,
+    &_segment_reduce_cpu_backward_kernel);
+REGISTER_VSX_DISPATCH(
+    _segment_reduce_backward_stub,
+    &_segment_reduce_cpu_backward_kernel);
 
 } // namespace native
 } // namespace at
