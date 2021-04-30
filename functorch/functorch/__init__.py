c6773c67d6f5a773e1c73949912701941572b2e8
Richard Zou <zou3519@gmail.com>
2021-04-30 07:51:02 -0700

    [functorch] Add pytree support for grad and grad_and_value

--- functorch/functorch/__init__.py
+++ functorch/functorch/__init__.py
@@ -2,7 +2,7 @@ import torch
 from . import _C
 
 from ._src.vmap import vmap
-from ._src.eager_transforms import grad, grad_with_value, vjp, jacrev
+from ._src.eager_transforms import grad, grad_and_value, vjp, jacrev
 from ._src.make_functional import make_functional, make_functional_with_buffers
 
 # Monkeypatching lol
