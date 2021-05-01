2801ea1529a56f5b3326626863b7b4e0acd6dc32
Alex Suhan <asuhan@fb.com>
2021-04-30 20:14:17 -0700

    Implement rank 1 literals

--- lazy_tensor_core/lazy_tensors/literal_util.h
+++ lazy_tensor_core/lazy_tensors/literal_util.h
@@ -9,17 +9,28 @@ namespace lazy_tensors {
 class LiteralUtil {
  public:
   template <typename NativeT>
-  static Literal CreateR0(NativeT value) {
-    Literal literal(ShapeUtil::MakeShape(
-        primitive_util::NativeToPrimitiveType<NativeT>(), {}));
-    literal.Set<NativeT>({}, value);
-    return literal;
-  }
+  static Literal CreateR0(NativeT value);
 
   template <typename NativeT>
-  static Literal CreateR1(lazy_tensors::Span<const NativeT> values) {
-    LTC_LOG(FATAL) << "Not implemented yet.";
-  }
+  static Literal CreateR1(lazy_tensors::Span<const NativeT> values);
 };
+
+template <typename NativeT>
+/* static */ Literal LiteralUtil::CreateR0(NativeT value) {
+  Literal literal(ShapeUtil::MakeShape(
+      primitive_util::NativeToPrimitiveType<NativeT>(), {}));
+  literal.Set<NativeT>({}, value);
+  return literal;
+}
+
+template <typename NativeT>
+/* static */ Literal LiteralUtil::CreateR1(
+    lazy_tensors::Span<const NativeT> values) {
+  Literal literal(
+      ShapeUtil::MakeShape(primitive_util::NativeToPrimitiveType<NativeT>(),
+                           {static_cast<int64>(values.size())}));
+  literal.PopulateR1(values);
+  return literal;
+}
 
 }  // namespace lazy_tensors
