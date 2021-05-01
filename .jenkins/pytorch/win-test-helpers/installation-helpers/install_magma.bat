65fbdeddc6f8d700c95ad9716205cbc208c5a52e
peterjc123 <peterghost86@gmail.com>
2021-05-01 12:49:30 +0800

    Remove useless logic in install_magma.bat

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.bat
@@ -7,11 +7,6 @@ rem remove dot in cuda_version, fox example 11.1 to 111
 set VERSION_SUFFIX=%CUDA_VERSION:.=%
 set CUDA_SUFFIX=cuda%VERSION_SUFFIX%
 
-if "%CUDA_SUFFIX%" == "" (
-  echo unknown CUDA version, please set `CUDA_VERSION` higher than 9.2
-  exit /b 1
-)
-
 if "%REBUILD%"=="" (
   if "%BUILD_ENVIRONMENT%"=="" (
     curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/magma_2.5.4_%CUDA_SUFFIX%_%BUILD_TYPE%.7z --output %TMP_DIR_WIN%\magma_2.5.4_%CUDA_SUFFIX%_%BUILD_TYPE%.7z
