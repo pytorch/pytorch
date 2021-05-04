c7bddb045eff7dd767f5db88bee73d5d90b99e6b
Mike Ruberry <mruberry@fb.com>
2021-05-04 11:28:04 -0700

    Revert D28048289: [TensorExpr] Add methods for inspecting generated code in `TensorExprKernel`.
    Test Plan: revert-hammer
    
    Differential Revision:
    D28048289 (https://github.com/pytorch/pytorch/commit/6b2cb939c54cc828ff5547fc212ab05513a7c57d)
    
    Original commit changeset: 3867e862a0ec
    
    fbshipit-source-id: bdd45dcc4b229673efeb06da411bbf0c58d44026

--- torch/csrc/jit/tensorexpr/kernel.h
+++ torch/csrc/jit/tensorexpr/kernel.h
@@ -85,14 +85,6 @@ class TORCH_API TensorExprKernel {
     return graph_;
   }
 
-  const std::vector<ConstantDescr>& getConstantDescriptors() const {
-    return constants_;
-  }
-
-  const std::vector<CodeGen::BufferArg>& getBufferArgs() const {
-    return bufferArgs_;
-  }
-
  private:
   enum BackendType {
     kUninitialized,
@@ -104,6 +96,7 @@ class TORCH_API TensorExprKernel {
 
   void compile();
   void genInputDebugNames();
+
   void runKernel(Stack& stack);
 
   std::vector<DimArg> dimsFromSizes(const std::vector<ExprHandle>& sizes);
