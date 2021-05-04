48f3cfd5cc8e6b3ebb716bdc181be235a929a5f2
Yi Zhang <zhanyi@microsoft.com>
2021-05-03 21:03:13 -0700

    update cudnn as well

--- .circleci/scripts/windows_cudnn_install.sh
+++ .circleci/scripts/windows_cudnn_install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -eux -o pipefail
 
-cuda_major_version=${CUDA_VERSION%.*}
+# cuda_installer_name
+declare -a installers=(
+    "10.1 cudnn-10.1-windows10-x64-v7.6.4.38"
+    "11.1 cudnn-11.1-windows-x64-v8.0.5.39"
+    "11.2 cudnn-11.2-windows-x64-v8.1.0.77"
+    "11.3 cudnn-11.3-windows-x64-v8.2.0.53"
+)
 
-if [[ "$cuda_major_version" == "10" ]]; then
-    cudnn_installer_name="cudnn-${CUDA_VERSION}-windows10-x64-v7.6.4.38"
-elif [[ "$cuda_major_version" == "11" ]]; then
-    if [[ "${CUDA_VERSION}" == "11.1" ]]; then
-        cudnn_installer_name="cudnn-${CUDA_VERSION}-windows-x64-v8.0.5.39"
-    elif [[ "${CUDA_VERSION}" == "11.2" ]]; then
-        cudnn_installer_name="cudnn-${CUDA_VERSION}-windows-x64-v8.1.0.77"
-    else
-        echo "This should not happen! ABORT."
-        exit 1
+for elem in "${installers[@]}"; do
+    read -a pair <<< "$elem"  # uses default whitespace IFS
+    if [[ "$CUDA_VERSION" == "${pair[0]}" ]]; then
+        cudnn_installer_name=${pair[1]}
+        break
     fi
-else
+done
+
+if [ -z $cudnn_installer_name ]; then
     echo "CUDNN for CUDA_VERSION $CUDA_VERSION is not supported yet"
     exit 1
 fi
