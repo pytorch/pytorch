babae61f2fe9d8b01bc485cd5aeee41d581876ed
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-05-05 13:10:03 -0700

    Make torch.linalg.svdvals differentiable (#57188)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57188
    
    `torch.linalg.svdvals` now supports autograd. This is achieved by
    computing the singular vectors internally if input requires grad,
    otherwise the vectors are not computed and the operation is faster.
    
    Test Plan: Imported from OSS
    
    Reviewed By: mrshenli
    
    Differential Revision: D28199709
    
    Pulled By: albanD
    
    fbshipit-source-id: cf39cf40965c606927db5331ce16743178fa711f

--- aten/src/ATen/native/BatchLinearAlgebra.cpp
+++ aten/src/ATen/native/BatchLinearAlgebra.cpp
@@ -2803,10 +2803,14 @@ Tensor linalg_svdvals(const Tensor& input) {
       "torch.linalg.svdvals: input should have at least 2 dimensions, but has ",
       input.dim(),
       " dimensions instead");
+
   Tensor singular_values;
+
+  // if input requires grad we must compute the singular vectors to make this function differentiable
+  // the singular vectors are not exposed to the user
+  const bool input_requires_grad = (at::GradMode::is_enabled() && input.requires_grad());
   std::tie(std::ignore, singular_values, std::ignore) =
-      // NOLINTNEXTLINE(bugprone-argument-comment)
-      at::_svd_helper(input, /*full_matrices=*/false, /*compute_uv=*/false);
+      at::_svd_helper(input, /*some=*/input_requires_grad, /*compute_uv=*/input_requires_grad);
   return singular_values;
 }
 
