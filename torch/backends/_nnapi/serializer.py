b67cde700ce5c95cc91aaf521d4fd34ee800d2fb
Akshit Khurana <axit@fb.com>
2021-05-05 13:51:59 -0700

    NNAPI: Add qadd flexible size support (#57562)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57562
    
    Add flexible size support for qadd op in nnapi model conversion
    
    Test Plan:
    pytest test/test_nnapi.py
    
    Imported from OSS
    
    Reviewed By: dreiss
    
    Differential Revision: D28200849
    
    fbshipit-source-id: d5b2ea8e9eb8ae405ff2c960f7549cef60bc0991

--- torch/backends/_nnapi/serializer.py
+++ torch/backends/_nnapi/serializer.py
@@ -1070,8 +1070,8 @@ class _NnapiSerializer(object):
         assert node.inputsAt(1).type().kind() == "TensorType"
 
         # TODO: Should support constant as either operand.
-        in0_id, in0_oper = self.get_tensor_operand_by_jitval_fixed_size(node.inputsAt(0))
-        in1_id, in1_oper = self.get_tensor_operand_by_jitval_fixed_size(node.inputsAt(1))
+        in0_id, in0_oper = self.get_tensor_operand_by_jitval(node.inputsAt(0))
+        in1_id, in1_oper = self.get_tensor_operand_by_jitval(node.inputsAt(1))
 
         assert in0_oper.op_type == in1_oper.op_type
         in0_id, in0_oper, in1_id, in1_oper = self.transpose_for_broadcast(
@@ -1083,13 +1083,25 @@ class _NnapiSerializer(object):
             scale, zp = qparams
             out_oper = out_oper._replace(scale=scale, zero_point=zp)
 
+        out_id = self.add_tensor_operand(node.outputsAt(0), out_oper)
+        for idx, (d0, d1) in enumerate(zip(in0_oper.shape, in1_oper.shape)):
+            if d0 == 1 and d1 == 0:
+                self.forward_operand_shape(out_id, idx, in1_id, idx)
+            elif d0 == 0 and d1 == 1:
+                self.forward_operand_shape(out_id, idx, in0_id, idx)
+            elif d0 == 0 and d1 == 0:
+                self.flexible_shape_computation_lines.append(
+                    f"assert {flex_name(in0_id, idx)} == {flex_name(in1_id, idx)}"
+                )
+                self.forward_operand_shape(out_id, idx, in0_id, idx)
+
         inputs = [None] * 3
         inputs[0] = in0_id
         inputs[1] = in1_id
         inputs[2] = self.add_immediate_int_scalar(fuse_code)
 
         outputs = [None] * 1
-        outputs[0] = self.add_tensor_operand(node.outputsAt(0), out_oper)
+        outputs[0] = out_id
 
         self.add_operation(opcode, inputs, outputs)
 
