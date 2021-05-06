b0dda20c495cbbd46123b19e023acbddbe571d94
Luca Wehrstedt <lcw@fb.com>
2021-05-06 01:10:10 -0700

    Make RRefContext get devices from RPC agent when creating OwnerRRef (#57443)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57443
    
    Based on the comments in https://github.com/pytorch/pytorch/pull/57355, I started looking at the callsites of `getOrCreateOwnerRRef` and `createOwnerRRef`, and noticed that many of them didn't specify the `devices` argument, which was optional and thus defaulted to `{}`, which created a CPU-only Future inside the OwnerRRef. (Such callsites were, for example, in `processPythonRemoteCall` and `processBaseScriptRemoteCall`, or `PyRRef::unpickle`, ...).
    
    Some (or all?) of these callsites might still have worked thanks to the RRef's own handling of CUDA streams and events, however we intend to remove that in https://github.com/pytorch/pytorch/pull/57355. I think it would be a safer and more generic solution to always create OwnerRRefs with the full set of devices supported by the RPC agent, and this is in fact easy to do since the RRefContext has access to the RPC agent. This means that all OwnerRRefs, no matter how they're created, will support CUDA if the agent does. This also allows us to stop requiring to specify devices when creating a OwnerRRef by hand in Python.
    ghstack-source-id: 128184665
    
    Test Plan: CI
    
    Reviewed By: mrshenli
    
    Differential Revision: D28144365
    
    fbshipit-source-id: 1f2d446873f31ee297415c46b94126b6502b12d3

--- torch/csrc/distributed/rpc/rref_context.cpp
+++ torch/csrc/distributed/rpc/rref_context.cpp
@@ -312,8 +312,7 @@ c10::intrusive_ptr<RRef> RRefContext::getOrCreateRRef(
 
 c10::intrusive_ptr<OwnerRRef> RRefContext::getOrCreateOwnerRRef(
     const RRefId& rrefId,
-    const TypePtr& type,
-    std::vector<c10::Device> devices) {
+    const TypePtr& type) {
   std::lock_guard<std::mutex> lock(mutex_);
   const auto iter = owners_.find(rrefId);
   if (iter == owners_.end()) {
@@ -322,7 +321,7 @@ c10::intrusive_ptr<OwnerRRef> RRefContext::getOrCreateOwnerRRef(
     // NB: cannot use make_shared here as the constructor of OwnerRRef is
     // private.
     auto rref = c10::make_intrusive<OwnerRRef>(
-        getWorkerId(), rrefId, type, std::move(devices));
+        getWorkerId(), rrefId, type, agent_->getDevices());
     owners_[rref->rrefId()] = rref;
     const auto pendingOwnerIter = pendingOwners_.find(rrefId);
     if (pendingOwnerIter != pendingOwners_.end()) {
@@ -368,14 +367,13 @@ c10::intrusive_ptr<OwnerRRef> RRefContext::getOrCreateOwnerRRef(
 }
 
 c10::intrusive_ptr<OwnerRRef> RRefContext::createOwnerRRef(
-    const TypePtr& type,
-    std::vector<c10::Device> devices) {
+    const TypePtr& type) {
   // Don't add this OnwerRRef to the owners_ map yet, otherwise
   // it will never be removed from there. Instead, only add it to the
   // map in prepareChildFork, in case this local RRef is being passed
   // to another worker.
   return c10::make_intrusive<OwnerRRef>(
-      getWorkerId(), genGloballyUniqueId(), type, std::move(devices));
+      getWorkerId(), genGloballyUniqueId(), type, agent_->getDevices());
 }
 
 c10::intrusive_ptr<JitFuture> RRefContext::getOwnerRRef(
