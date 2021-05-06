de9d59aad9da9428e708b270c7a8da64d483f61f
Horace He <chilli@fb.com>
2021-05-05 21:09:11 -0700

    Revert D28052211: [paramcomms] support for in and out split sizes
    Test Plan: revert-hammer
    
    Differential Revision:
    D28052211 (https://github.com/pytorch/pytorch/commit/866b19e95d1b28fee07161e92692d2cccc788dcd)
    
    Original commit changeset: 4ab7d425fc72
    
    fbshipit-source-id: 80c001ddcb3730f0487adddf66d9166f53c45a8c

--- torch/lib/c10d/ParamCommsUtils.cpp
+++ torch/lib/c10d/ParamCommsUtils.cpp
@@ -12,14 +12,14 @@ ParamCommsDebugInfo::ParamCommsDebugInfo(
     int inSize,
     int outSize,
     at::ScalarType dType,
-    std::vector<int64_t> inSplitSizes,
-    std::vector<int64_t> outSplitSizes) :
+    std::vector<int64_t>&& inSplitSizes,
+    std::vector<int64_t>&& outSplitSizes) :
       rank_(rank),
       columnName_(colName),
       inMessageSize_(inSize),
       outMessageSize_(outSize),
       dType_(dType),
-      inputSplitSizes_(std::move(inSplitSizes)),
-      outputSplitSizes_(std::move(outSplitSizes)) {}
+      inputSplitSizes_(inSplitSizes),
+      outputSplitSizes_(outSplitSizes) {}
 
 } // namespace torch
