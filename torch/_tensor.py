61407c15c862844668d336c3338cc0f984953e11
Akifumi Imanishi <imanishi@preferred.jp>
2021-05-06 09:00:12 +0000

    Revert "Fix some operators"
    This reverts commit 1d209db1cc624fbbe99eee5c82b7b68273c1666e.

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -532,13 +532,11 @@ class Tensor(torch._C._TensorBase):
             )
         return torch.unique_consecutive(self, return_inverse=return_inverse, return_counts=return_counts, dim=dim)
 
-    @_wrap_type_error_to_not_implemented
     def __rsub__(self, other):
         if has_torch_function_variadic(self, other):
             return handle_torch_function(Tensor.__rsub__, (self, other), self, other)
         return _C._VariableFunctions.rsub(self, other)
 
-    @_wrap_type_error_to_not_implemented
     def __rdiv__(self, other):
         if has_torch_function_variadic(self, other):
             return handle_torch_function(Tensor.__rdiv__, (self, other), self, other)
@@ -547,7 +545,7 @@ class Tensor(torch._C._TensorBase):
     __rtruediv__ = __rdiv__
     __itruediv__ = _C._TensorBase.__idiv__
 
-    __pow__ = _wrap_type_error_to_not_implemented(_C._TensorBase.pow)
+    __pow__ = _C._TensorBase.pow
 
     def __format__(self, format_spec):
         if has_torch_function_unary(self):
