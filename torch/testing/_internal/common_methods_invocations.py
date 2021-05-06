5cb47fc1c77835af935a3df9f4a9ece77e972c9b
Nikolay Korovaiko <korovaikon@gmail.com>
2021-05-06 18:13:55 +0000

    Update on "init hardshrink"
    add to a fx test
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -4758,7 +4758,7 @@ op_db: List[OpInfo] = [
            supports_autograd=True,
            assert_autodiffed=True,
            sample_inputs_func=sample_inputs_hardshrink,
-           supports_gradgrad=False,
+           supports_gradgrad=True,
            supports_out=False,
            autodiff_fusible_nodes=["aten::hardshrink"]),
     OpInfo('mm',
