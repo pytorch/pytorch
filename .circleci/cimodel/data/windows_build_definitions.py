67ab21cac58d5753b8bc87e437fe05913830b4b6
Yi Zhang <zhanyi@microsoft.com>
2021-05-06 07:46:39 -0700

    Revert "enable cuda test temporaily"
    This reverts commit 6fc57cb5e393346d4ed7604de0fe6244d9219209.

--- .circleci/cimodel/data/windows_build_definitions.py
+++ .circleci/cimodel/data/windows_build_definitions.py
@@ -144,8 +144,8 @@ WORKFLOW_DATA = [
     WindowsJob('_azure_multi_gpu', _VC2019, CudaVersion(10, 1), multi_gpu=True, nightly_only=True),
     # VS2019 CUDA-11.1
     WindowsJob(None, _VC2019, CudaVersion(11, 1)),
-    WindowsJob(1, _VC2019, CudaVersion(11, 1)),
-    WindowsJob(2, _VC2019, CudaVersion(11, 1)),
+    WindowsJob(1, _VC2019, CudaVersion(11, 1), master_only=True),
+    WindowsJob(2, _VC2019, CudaVersion(11, 1), master_only=True),
     # VS2019 CPU-only
     WindowsJob(None, _VC2019, None),
     WindowsJob(1, _VC2019, None),
