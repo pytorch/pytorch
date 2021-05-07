f1a6abee4ab1aa0a7675d7435e77056de41e4ff1
lezcano <lezcano-93@hotmail.com>
2021-05-07 09:29:24 +0000

    minor

--- torch/distributions/constraints.py
+++ torch/distributions/constraints.py
@@ -463,7 +463,7 @@ class _PositiveDefinite(Constraint):
     event_dim = 2
 
     def check(self, value):
-        # Assumes that the matrix in value are symmetric
+        # Assumes that the matrix or batch of matrices in value are symmetric
         # info == 0 means no error, that is, it's SPD
         return torch.linalg.cholesky_ex(value).info.eq(0).unsqueeze(0)
 
