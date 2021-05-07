054318d98db4dfa18cee66c4c86af02044db6294
Venkata Sreeram <62790398+venkata-sreeram@users.noreply.github.com>
2021-05-07 15:49:15 +0530

    Updated TechDocs

--- torch/__init__.py
+++ torch/__init__.py
@@ -1,9 +1,13 @@
 
 r"""
+An open source machine learning framework that accelerates the path from research prototyping to production deployment.
+
 The torch package contains data structures for multi-dimensional
 tensors and defines mathematical operations over these tensors.
 Additionally, it provides many utilities for efficient serializing of
 Tensors and arbitrary types, and other useful utilities.
+
+
 
 It has a CUDA counterpart, that enables you to run your tensor computations
 on an NVIDIA GPU with compute capability >= 3.0.
