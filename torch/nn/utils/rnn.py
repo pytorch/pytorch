46895cc5eee26ca55650157d20fc27994fd1803e
Eddie Yan <eddiey@nvidia.com>
2021-05-07 22:47:32 +0000

    check in

--- torch/nn/utils/rnn.py
+++ torch/nn/utils/rnn.py
@@ -356,24 +356,7 @@ def pad_sequence(sequences, batch_first=False, padding_value=0.0):
 
     # assuming trailing dimensions and type of all the Tensors
     # in sequences are same and fetching those from sequences[0]
-    max_size = sequences[0].size()
-    trailing_dims = max_size[1:]
-    max_len = max([s.size(0) for s in sequences])
-    if batch_first:
-        out_dims = (len(sequences), max_len) + trailing_dims
-    else:
-        out_dims = (max_len, len(sequences)) + trailing_dims
-
-    out_tensor = sequences[0].new_full(out_dims, padding_value)
-    for i, tensor in enumerate(sequences):
-        length = tensor.size(0)
-        # use index notation to prevent duplicate references to the tensor
-        if batch_first:
-            out_tensor[i, :length, ...] = tensor
-        else:
-            out_tensor[:length, i, ...] = tensor
-
-    return out_tensor
+    return torch._C._nn.pad_sequence(sequences, batch_first, padding_value)
 
 
 def pack_sequence(sequences, enforce_sorted=True):
