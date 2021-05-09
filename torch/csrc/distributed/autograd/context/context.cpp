854b7b17d13d0da314d4227dfa121be15299cf16
Shen Li <cs.shenli@gmail.com>
2021-05-08 22:39:00 -0700

    Fix distributed autograd gradient accumulation
    In distributed autograd, the same gradient could be updated by
    multiple RPC sessions within the same backward pass. Hence, those
    updates need to be synchronized.
    
    [ghstack-poisoned]

--- torch/csrc/distributed/autograd/context/context.cpp
+++ torch/csrc/distributed/autograd/context/context.cpp
@@ -99,6 +99,7 @@ void DistAutogradContext::accumulateGrad(
 
   at::Tensor new_grad = AccumulateGrad::callHooks(variable, grad);
 
+  waitGradEvent(new_grad.device());
   // TODO: Need to bump 'num_expected_refs' here when we support post_hooks for
   // distributed autograd as part of
   // https://github.com/pytorch/pytorch/issues/33482
@@ -176,7 +177,18 @@ void DistAutogradContext::recordGradEvent(c10::Device device) {
           std::forward_as_tuple(device),
           std::forward_as_tuple(std::move(event)));
     } else {
-      iter->second.record(impl_.getStream(device));
+      auto stream = impl_.getStream(device);
+      iter->second.block(stream);
+      iter->second.record(stream);
+    }
+  }
+}
+
+void DistAutogradContext::waitGradEvent(c10::Device device) {
+  if (device.is_cuda()) {
+    auto iter = gradReadyEvents_.find(device);
+    if (iter != gradReadyEvents_.end()) {
+      iter->second.block(impl_.getStream(device));
     }
   }
 }
