c86a4dd545cdcb99749491efe3808a240ed7e5ac
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-05-08 20:36:49 -0500

    Enable `logaddexp` and `logaddexp2` for BFloat16 on CUDA

--- aten/src/ATen/native/cuda/LogAddExpKernel.cu
+++ aten/src/ATen/native/cuda/LogAddExpKernel.cu
@@ -10,31 +10,37 @@
 namespace at { namespace native {
 
 void logaddexp_kernel_cuda(TensorIteratorBase& iter) {
-  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "logaddexp_cuda", [&]() {
-    gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
-      if (::isinf(a) && a == b) {
-        return a;
-      }
-      else {
-        scalar_t m = ::max(a, b);
-        return m + ::log((scalar_t)(1.0) + ::exp(-::abs(a - b)));
-      }
-    });
-  });
+  AT_DISPATCH_FLOATING_TYPES_AND(
+      ScalarType::BFloat16,
+      iter.dtype(), "logaddexp_cuda",
+      [&]() {
+        gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
+          if (::isinf(a) && a == b) {
+            return a;
+          }
+          else {
+            scalar_t m = ::max(a, b);
+            return m + ::log((scalar_t)(1.0) + ::exp(-::abs(a - b)));
+          }
+        });
+      });
 }
 
 void logaddexp2_kernel_cuda(TensorIteratorBase& iter) {
-  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "logaddexp2_cuda", [&]() {
-    gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
-      if (::isinf(a) && a == b) {
-        return a;
-      }
-      else {
-        scalar_t m = ::max(a, b);
-        return m + ::log2((scalar_t)(1.0) + ::pow((scalar_t)(2.0), -::abs(a - b)));
-      }
-    });
-  });
+  AT_DISPATCH_FLOATING_TYPES_AND(
+      ScalarType::BFloat16,
+      iter.dtype(), "logaddexp2_cuda",
+      [&]() {
+        gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
+          if (::isinf(a) && a == b) {
+            return a;
+          }
+          else {
+            scalar_t m = ::max(a, b);
+            return m + ::log2((scalar_t)(1.0) + ::pow((scalar_t)(2.0), -::abs(a - b)));
+          }
+        });
+      });
 }
 
 REGISTER_DISPATCH(logaddexp_stub, &logaddexp_kernel_cuda);
