6116d1a116093fd78b8afbd4ef33265396662c1e
John Clow <jclow@fb.com>
2021-05-10 13:36:25 -0700

    Added isnot op support as well as `checkScript` testing
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/jit/test_peephole.py
+++ test/jit/test_peephole.py
@@ -219,10 +219,21 @@ class TestPeephole(JitTestCase):
             FileCheck().check("conv").check("dim").run(conv_dim.graph)
 
     def test_normalized_is_op(self):
-        print("test_start")
-        @torch.jit.script
         def convertible_is_op(x:bool, y:bool):
             return x is True, False is x, x is y
 
-        FileCheck().check_count("aten::eq", 3, exactly=True).run(convertible_is_op.graph)
-        FileCheck().check_count("aten::__is__", 0, exactly=True).run(convertible_is_op.graph)
+        self.checkScript(convertible_is_op, (True, False))
+
+        op_graph = torch.jit.script(convertible_is_op).graph
+        FileCheck().check_count("aten::eq", 3, exactly=True).run(op_graph)
+        FileCheck().check_count("aten::__is__", 0, exactly=True).run(op_graph)
+
+    def test_normalized_isnot_op(self):
+        def convertible_isnot_op(x:bool, y:bool):
+            return x is not True, False is not x, x is not y
+
+        self.checkScript(convertible_isnot_op, (True, False))
+
+        op_graph = torch.jit.script(convertible_isnot_op).graph
+        FileCheck().check_count("aten::ne", 3, exactly=True).run(op_graph)
+        FileCheck().check_count("aten::__isnot__", 0, exactly=True).run(op_graph)
--- torch/csrc/jit/passes/normalize_ops.cpp
+++ torch/csrc/jit/passes/normalize_ops.cpp
@@ -1,9 +1,9 @@
 #include <torch/csrc/jit/passes/normalize_ops.h>
 
 #include <c10/util/Exception.h>
-#include "ATen/core/interned_strings.h"
-#include "jit/ir/graph_node_list.h"
-#include "jit/ir/ir.h"
+#include <ATen/core/interned_strings.h>
+#include <jit/ir/graph_node_list.h>
+#include <jit/ir/ir.h>
 
 namespace torch {
 namespace jit {
@@ -23,14 +23,19 @@ bool normalizeOpAliases(graph_node_list_iterator& iter) {
   return false;
 }
 
-// Normalizes a __is__ comparison with a bool to `eq`
+// Normalizes a `__is__` comparison with a bool to `eq` (and same with `__isnot__`)
 bool normalizeIsBool(graph_node_list_iterator& iter) {
-  if(iter->kind() == aten::__is__){
-    ArrayRef<Value*> args = iter->inputs();
-    if(args[0]->type() == BoolType::get() && args[1]->type() == BoolType::get()){
-      iter->replaceWithNewSymbol(aten::eq);
-      iter.destroyCurrent();
-      return true;
+  ArrayRef<Value*> args = iter->inputs();
+  if(args.size() == 2 && args[0]->type() == BoolType::get() && args[1]->type() == BoolType::get()){
+    if(iter->kind() == aten::__is__){
+        iter->replaceWithNewSymbol(aten::eq);
+        iter.destroyCurrent();
+        return true;
+      }
+    if(iter->kind() == aten::__isnot__){
+        iter->replaceWithNewSymbol(aten::ne);
+        iter.destroyCurrent();
+        return true;
     }
   }
   return false;
