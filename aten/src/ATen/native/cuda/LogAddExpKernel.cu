401edc35d17a864a2b4bcafe6a7cad6737d4ed95
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-05-10 16:00:42 -0500

    Add workaround suggested by @zasdfgbnm

--- aten/src/ATen/native/cuda/LogAddExpKernel.cu
+++ aten/src/ATen/native/cuda/LogAddExpKernel.cu
@@ -3,6 +3,8 @@
 #include <ATen/native/cuda/Loops.cuh>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/BinaryOps.h>
+#include <ATen/AccumulateType.h>
+
 
 // NOTE: CUDA on Windows requires that the enclosing function
 // of a __device__ lambda not have internal linkage.
@@ -14,8 +16,9 @@ void logaddexp_kernel_cuda(TensorIteratorBase& iter) {
       ScalarType::BFloat16,
       iter.dtype(), "logaddexp_cuda",
       [&]() {
+        using accscalar_t = at::acc_type<scalar_t, /*is_cuda=*/true>;
         gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
-          if (::isinf(a) && a == b) {
+          if (::isinf(static_cast<accscalar_t>(a)) && a == b) {
             return a;
           }
           else {
@@ -31,8 +34,9 @@ void logaddexp2_kernel_cuda(TensorIteratorBase& iter) {
       ScalarType::BFloat16,
       iter.dtype(), "logaddexp2_cuda",
       [&]() {
+        using accscalar_t = at::acc_type<scalar_t, /*is_cuda=*/true>;
         gpu_kernel(iter, [] GPU_LAMBDA (scalar_t a, scalar_t b) -> scalar_t {
-          if (::isinf(a) && a == b) {
+          if (::isinf(static_cast<accscalar_t>(a)) && a == b) {
             return a;
           }
           else {
