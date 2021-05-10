ebc10eba0fa53142f7b7f8de42cd9405a4baf05e
Nikita Shulga <nshulga@fb.com>
2021-05-10 06:18:59 -0700

    Fix issubinstance

--- torch/utils/data/_typing.py
+++ torch/utils/data/_typing.py
@@ -170,21 +170,23 @@ def issubinstance(data, data_type):
     if not issubtype(type(data), data_type, recursive=False):
         return False
 
+    # In Python-3.9 typing library __args__ attribute is not defined for untyped generics
+    dt_args = getattr(data_type, "__args__", None)
     if isinstance(data, tuple):
-        if data_type.__args__ is None or len(data_type.__args__) == 0:
+        if dt_args is None or len(dt_args) == 0:
             return True
-        if len(data_type.__args__) != len(data):
+        if len(dt_args) != len(data):
             return False
-        return all(issubinstance(d, t) for d, t in zip(data, data_type.__args__))
+        return all(issubinstance(d, t) for d, t in zip(data, dt_args))
     elif isinstance(data, (list, set)):
-        if data_type.__args__ is None or len(data_type.__args__) == 0:
+        if dt_args is None or len(dt_args) == 0:
             return True
-        t = data_type.__args__[0]
+        t = dt_args[0]
         return all(issubinstance(d, t) for d in data)
     elif isinstance(data, dict):
-        if data_type.__args__ is None or len(data_type.__args__) == 0:
+        if dt_args is None or len(dt_args) == 0:
             return True
-        kt, vt = data_type.__args__
+        kt, vt = dt_args
         return all(issubinstance(k, kt) and issubinstance(v, vt) for k, v in data.items())
 
     return True
