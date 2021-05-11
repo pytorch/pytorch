1f6408bf7e58eb226ecf61d4bfcbc74a4c6201dc
ilia-cher <iliacher@fb.com>
2021-05-11 01:18:28 -0700

    [profiler][small] Add skip_first parameter to the default schedule
    Summary:
    Add `skip_first` parameter to allow for arbitrary profiler step ranges
    
    Test Plan:
    python test/test_profiler.py
    
    [ghstack-poisoned]

--- torch/profiler/profiler.py
+++ torch/profiler/profiler.py
@@ -21,16 +21,20 @@ class ProfilerAction(Enum):
     RECORD_AND_SAVE = 3
 
 
-def schedule(*, wait: int, warmup: int, active: int, repeat: int = 0) -> Callable:
+def schedule(*, wait: int, warmup: int, active: int, repeat: int = 0, skip_first: int = 0) -> Callable:
     """
-    Returns a callable that can be used as profiler ``schedule`` argument. The profiler will wait for ``wait`` steps, then
-    do the warmup for the next ``warmup`` steps, then
-    do the active recording for the next ``active`` steps and then
-    repeat the cycle starting with the next step. The number of cycles is specified by the ``repeat`` parameter.
-    When the parameter's value is zero, the cycles will continue until the profiling is finished.
+    Returns a callable that can be used as profiler ``schedule`` argument. The profiler will skip
+    the first ``skip_first`` steps, then wait for ``wait`` steps, then do the warmup for the next ``warmup`` steps,
+    then do the active recording for the next ``active`` steps and then repeat the cycle starting with ``wait`` steps.
+    The optional number of cycles is specified with the ``repeat`` parameter, the zero value means that
+    the cycles will continue until the profiling is finished.
     """
     def schedule_fn(step: int) -> ProfilerAction:
         assert step >= 0
+        if step < skip_first:
+            return ProfilerAction.NONE
+        else:
+            step -= skip_first
         num_steps = wait + warmup + active
         if repeat > 0 and step / num_steps >= repeat:
             return ProfilerAction.NONE
@@ -42,7 +46,8 @@ def schedule(*, wait: int, warmup: int, active: int, repeat: int = 0) -> Callabl
         else:
             return ProfilerAction.RECORD if mod_step < num_steps - 1 \
                 else ProfilerAction.RECORD_AND_SAVE
-    assert wait >= 0 and warmup >= 0 and active > 0, \
+    assert wait >= 0 and warmup >= 0 and active > 0 and \
+           repeat >= 0 and skip_first >= 0, \
         "Invalid profiler schedule arguments"
     if warmup == 0:
         warn("Profiler won't be using warmup, this can skew profiler results")
