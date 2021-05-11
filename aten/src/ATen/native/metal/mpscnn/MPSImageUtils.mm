0d564904b51c67a1ce8b8d0b3734ba3df87f602f
Tao Xu <taox@fb.com>
2021-05-11 04:46:52 -0700

    [iOS GPU][Perf][4/n] Reuse the same command buffer when copying results to CPU (#57667)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57667
    
    Context - https://fb.workplace.com/groups/pytorch.edge.team/permalink/855194118368662/
    
    Got 5% win for mobilenetv2 and unet
    ghstack-source-id: 128338532
    
    Test Plan: - CI
    
    Reviewed By: kimishpatel
    
    Differential Revision: D28116806
    
    fbshipit-source-id: b9c766c58ae41f3408724ec962695f38985ace05

--- aten/src/ATen/native/metal/mpscnn/MPSImageUtils.mm
+++ aten/src/ATen/native/metal/mpscnn/MPSImageUtils.mm
@@ -1,6 +1,6 @@
 #import <ATen/native/metal/MetalUtils.h>
-#import <ATen/native/metal/mpscnn/MPSCNNUtils.h>
 #import <ATen/native/metal/mpscnn/MPSCNNContext.h>
+#import <ATen/native/metal/mpscnn/MPSCNNUtils.h>
 #import <ATen/native/metal/mpscnn/MPSImage+Tensor.h>
 #import <ATen/native/metal/mpscnn/MPSImageUtils.h>
 
@@ -175,9 +175,9 @@ MPSTemporaryImage* createTemporaryImage(
   TORCH_CHECK(buffer);
   int64_t size_bytes = c10::multiply_integers(sizes) * sizeof(float);
   id<MTLBuffer> buff = [[MPSCNNContext sharedInstance].device
-      newBufferWithLength:size_bytes
-                  options:MTLResourceOptionCPUCacheModeWriteCombined];
-  memcpy(buff.contents, src, size_bytes);
+      newBufferWithBytes:src
+                  length:size_bytes
+                 options:MTLResourceStorageModeShared];
   MPSTemporaryImage* output = createTemporaryImage(buffer, sizes);
   id<MTLComputePipelineState> state = [[MPSCNNContext sharedInstance]
       specializedPipelineState:metal::mpscnn::kernelFor(
@@ -254,6 +254,35 @@ void copyToHost(float* dst, MPSImage* image) {
   [cb commit];
   [cb waitUntilCompleted];
   memcpy(dst, buffer.contents, buffer.length);
+}
+
+void copyToMetalBuffer(
+    MetalCommandBuffer* cmdBuffer,
+    id<MTLBuffer> dst,
+    MPSImage* image) {
+  TORCH_CHECK(cmdBuffer.buffer);
+  id<MTLComputeCommandEncoder> encoder =
+      [cmdBuffer.buffer computeCommandEncoder];
+  id<MTLComputePipelineState> state = [[MPSCNNContext sharedInstance]
+      specializedPipelineState:metal::mpscnn::kernelFor(
+                                   image,
+                                   "copy_metal_to_nchw",
+                                   "copy_metal_to_nchw_nonarray")
+                     Constants:@[
+                       @(image.featureChannels),
+                       @(image.height),
+                       @(image.width)
+                     ]];
+
+  [encoder setComputePipelineState:state];
+  [encoder setBuffer:dst offset:0 atIndex:0];
+  [encoder setTexture:[image texture] atIndex:0];
+
+  const auto& launchParams =
+      metal::mpscnn::spatialPointwiseKernelLaunchParams(state, image);
+  [encoder dispatchThreadgroups:launchParams.threadgroupsPerGrid
+          threadsPerThreadgroup:launchParams.threadsPerThreadgroup];
+  [encoder endEncoding];
 }
 
 std::vector<fp16_t> staticImageToFp16Array(MPSImage* image) {
