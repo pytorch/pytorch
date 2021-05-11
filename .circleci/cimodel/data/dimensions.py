b7d4a596c5c04540e475965f12f4c1047e6d25f8
kedejesu <dkeren@seas.upenn.edu>
2021-05-11 15:46:46 -0700

    adding back the py3.6 binaries

--- .circleci/cimodel/data/dimensions.py
+++ .circleci/cimodel/data/dimensions.py
@@ -15,6 +15,7 @@ ROCM_VERSION_LABELS = ["rocm" + v for v in ROCM_VERSIONS]
 GPU_VERSIONS = ["cuda" + v for v in CUDA_VERSIONS] 
 
 STANDARD_PYTHON_VERSIONS = [
+    "3.6",
     "3.7",
     "3.8",
     "3.9"
