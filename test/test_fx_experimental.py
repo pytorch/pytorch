35521a26299b288d4c077540e93d9bc0649fe2e5
Akifumi Imanishi <imanishi@preferred.jp>
2021-05-12 11:01:58 -0700

    Fix some tensor operators to return `NotImplemented` for invalid inputs (#57934)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/57719.
    
    This PR fixes `torch.Tensor{__rsub__, __rdiv__, __rtruediv__, __pow__, __rmatmul__}` to return `NotImplemented` instead of raising a `TypeError`.
    
    cc/ mruberry: The first commit of this PR is the same as https://github.com/pytorch/pytorch/pull/56997/commits/1d209db1cc624fbbe99eee5c82b7b68273c1666e excepts the commit message.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57934
    
    Reviewed By: mruberry
    
    Differential Revision: D28351931
    
    Pulled By: albanD
    
    fbshipit-source-id: 985457a44dba24d2496794dfb8c1661cbcd4ff8f

--- test/test_fx_experimental.py
+++ test/test_fx_experimental.py
@@ -1323,7 +1323,8 @@ class TestNormalizeOperators(JitTestCase):
                    '__rsub__',
                    '__rmul__',
                    '__rdiv__',
-                   '__rpow__'}
+                   '__rpow__',
+                   '__rmatmul__'}
 
         # Unsupported input types
         if op.name in op_skip:
