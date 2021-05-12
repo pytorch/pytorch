3a278446e40fbee162d38c85731cf9c4f8864158
lezcano <lezcano-93@hotmail.com>
2021-05-12 02:20:31 -0700

    Deprecate symeig (#57732)
    Summary:
    This one had a tricky usage of `torch.symeig` that had to be replaced. I tested the replacement locally though.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57732
    
    Reviewed By: bdhirsh
    
    Differential Revision: D28328189
    
    Pulled By: mruberry
    
    fbshipit-source-id: 7f000fcbf2b029beabc76e5a89ff158b47977474

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -7463,8 +7463,6 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         with warnings.catch_warnings(record=True) as w:
             # Trigger warning
             torch.symeig(a, out=(out_w, out_v))
-            # Check warning occurs
-            self.assertEqual(len(w), 2)
             self.assertTrue("An output with one or more elements was resized" in str(w[-2].message))
             self.assertTrue("An output with one or more elements was resized" in str(w[-1].message))
 
