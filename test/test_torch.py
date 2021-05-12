040be0ef3c0e26d57b1d5e9119f1127ebd39a7f1
Yu Guo <yuguo@fb.com>
2021-05-12 00:29:58 -0700

    enable deterministic path for index_put with accumulate=False on CPU and CUDA (#57839)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57839
    
    we reuse the `index_put_accum_kernel`, rename it to  `index_put_deterministic_kernel` and add a bool `accumulate` in `index_backward_kernel`
    
    Test Plan:
    buck test mode/opt //caffe2/test:torch -- test_index_put_non_accumulate_deterministic
    
        ✓ Pass: caffe2/test:torch - test_index_put_non_accumulate_deterministic_cpu (test_torch.TestTorchDeviceTypeCPU) (5.120)
    Summary
      Pass: 1
      Skip: 1
        ↻ caffe2/test:torch - test_index_put_non_accumulate_deterministic_meta (test_torch.TestTorchDeviceTypeMETA)
      ListingSuccess: 1
    
    buck test mode/opt //caffe2/test:torch_cuda -- test_index_put_non_accumulate_deterministic
    
        ✓ ListingSuccess: caffe2/test:torch_cuda - main (6.397)
        ✓ Pass: caffe2/test:torch_cuda - test_index_put_non_accumulate_deterministic_cuda (test_torch.TestTorchDeviceTypeCUDA) (26.030)
        ✓ Pass: caffe2/test:torch_cuda - main (26.030)
    Summary
      Pass: 2
      ListingSuccess: 1
    
    Reviewed By: ngimel
    
    Differential Revision: D28290699
    
    fbshipit-source-id: df8bbe7af2e72017566161b05b85737fda4ceb3f

--- test/test_torch.py
+++ test/test_torch.py
@@ -3966,25 +3966,6 @@ else:
         test_func(torch.Tensor.scatter_add)
         test_func(torch.scatter_add)
 
-    # Ensures that index_put throws nondeterministic alerts in the correct cases
-    @onlyOnCPUAndCUDA
-    def test_nondeterministic_alert_index_put(self, device):
-        def test_func(op_call):
-            a = torch.randn(10, device=device)
-            indices = (torch.tensor([0, 0], device=device), )
-            values = torch.tensor([0, 1], device=device)
-
-            @expectedAlertNondeterministic('index_put_ with accumulate=False')
-            def forward_func(slf, device):
-                op_call(a, indices, values, accumulate=False)
-
-            forward_func(self, device)
-
-        test_func(torch.index_put)
-        test_func(torch.Tensor.index_put)
-        test_func(torch.index_put_)
-        test_func(torch.Tensor.index_put_)
-
     @onlyOnCPUAndCUDA
     def test_nondeterministic_alert_put(self, device):
         def test_func(op_call):
@@ -5305,6 +5286,25 @@ else:
                 for _ in range(3):
                     y_nd = torch.index_add(x, dim, index, src, alpha=alpha)
                     self.assertEqual(y_nd, y0, atol=1e-3, rtol=1e-5)
+
+    @onlyOnCPUAndCUDA
+    def test_index_put_non_accumulate_deterministic(self, device) -> None:
+        with DeterministicGuard(True):
+            for i in range(3):
+                m = random.randint(10, 20)
+                elems = random.randint(20000, 30000)
+                values = torch.rand(elems, device=device)
+                indices = torch.randint(m, (elems,), device=device)
+                input = torch.rand(m, device=device)
+                output = input.index_put((indices,), values, accumulate=False)
+
+                input_list = input.tolist()
+                indices_list = indices.tolist()
+                values_list = values.tolist()
+                for i, v in zip(indices_list, values_list):
+                    input_list[i] = v
+
+                self.assertEqual(output, input_list)
 
     @dtypes(*torch.testing.get_all_dtypes())
     def test_index_fill(self, device, dtype):
