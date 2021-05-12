47c52107e79e47f1fb23a1b62ba1535b1c75336c
garrett <gcramer@fb.com>
2021-05-12 02:33:29 +0000

    DdpSparseDenseRpcTrainer
    [ghstack-poisoned]

---
+++ benchmarks/distributed/rpc/parameter_server/trainers/DdpSparseDenseRpcTrainer.py
@@ -0,0 +1,15 @@
+from .DdpSparseRpcTrainer import DdpSparseRpcTrainer
+
+
+class DdpSparseDenseRpcTrainer(DdpSparseRpcTrainer):
+
+    def __init__(self, rank, trainer_count, ps_rref, ps_name, backend, use_cuda_rpc, epochs):
+        super().__init__(rank, trainer_count, ps_rref, ps_name, backend, use_cuda_rpc, epochs)
+
+    @staticmethod
+    def hook(state, bucket):
+        cref = state.cref
+        cref.process_bucket(state, bucket)
+
+    def get_hook(self):
+        return DdpSparseDenseRpcTrainer.hook
