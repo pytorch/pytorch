4400cd803a573936a5115cfb3506ed1d249440c9
Sam Estep <sestep@fb.com>
2021-05-13 15:01:53 -0700

    Print stderrs in tools/mypy_wrapper.py

--- tools/mypy_wrapper.py
+++ tools/mypy_wrapper.py
@@ -126,7 +126,11 @@ def make_plan(
     return plan
 
 
-def run(*, args: List[str], files: List[str]) -> Tuple[int, List[str]]:
+def run(
+    *,
+    args: List[str],
+    files: List[str],
+) -> Tuple[int, List[str], List[str]]:
     """
     Return the exit code and list of output lines from running `mypy`.
 
@@ -168,6 +172,7 @@ def run(*, args: List[str], files: List[str]) -> Tuple[int, List[str]]:
             for stdout, _, _ in mypy_results
             for item in stdout.splitlines()
         )),
+        [stderr for _, stderr, _ in mypy_results],
     )
 
 
@@ -202,12 +207,14 @@ def main(args: List[str]) -> None:
     and allows you to set the path to the mypy executable.
     """
     repo_root = str(Path.cwd())
-    exit_code, mypy_issues = run(
+    exit_code, mypy_issues, stderrs = run(
         args=[arg for arg in args if not arg.startswith(repo_root)],
         files=[arg for arg in args if arg.startswith(repo_root)],
     )
     for issue in mypy_issues:
         print(issue)
+    for stderr in stderrs:
+        print(stderr, end='', file=sys.stderr)
     sys.exit(exit_code)
 
 
