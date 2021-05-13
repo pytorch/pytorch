8806babd035bbf52ea4b31cfddd060125de9593e
Heitor Schueroff <heitorschueroff@fb.com>
2021-05-13 14:17:22 +0000

    Implement torch.cov

--- aten/src/ATen/native/Correlation.cpp
+++ aten/src/ATen/native/Correlation.cpp
@@ -6,14 +6,59 @@ namespace native {
 
 Tensor cov(
     const Tensor& self,
-    int64_t correlation,
+    int64_t correction,
     const c10::optional<Tensor>& fweights,
     const c10::optional<Tensor>& aweights) {
+  TORCH_CHECK(self.ndimension() < 2, "cov(): input has more than 2 dimensions");
 
-  // Check Preconditions
-  checkDimRange("cov", TensorArg{self, "input", 1}, 0, 2);
+  // TODO: type promotion to float64 like NumPy
+  auto input = self.ndimension() < 2 ? self.view({1, -1}) : self;
+  const auto num_observations = self.size(1);
 
-  return at::empty(0);
+  // TODO: should we check that weights are non-negative? what about cross-device sync
+  Tensor w;
+  if (fweights.has_value()) {
+    const auto& fw = fweights.value();
+    TORCH_CHECK(
+        fw.ndimension() > 1, "cov(): fweights has more than 1 dimension");
+    TORCH_CHECK(
+        at::isIntegralType(fw.scalar_type(), false),
+        "cov(): fweights must be integral dtype");
+    TORCH_CHECK(
+        fw.numel() == num_observations,
+        "cov(): incompatible number of observations and fweights");
+    w = fw;
+  }
+  if (aweights.has_value()) {
+    const auto& aw = aweights.value();
+    TORCH_CHECK(
+        aw.ndimension() > 1, "cov(): aweights has more than 1 dimension");
+    TORCH_CHECK(
+        aw.numel() == num_observations,
+        "cov(): incompatible number of observations and aweights");
+    w = w.defined() ? w * aw : aw;
+  }
+  // TODO: is there a perf impact to creating a weight tensor when none is given?
+  w = w.defined() ? w.to(self.dtype()) : at::ones(num_observations, self.options());
+
+  const auto w_sum = w.sum();
+  const auto avg = (input * w).sum(1) / w_sum;
+
+  Tensor fact;
+  if (correction == 0) {
+    fact = w_sum;
+  } else if (!aweights.has_value()) {
+    fact = w_sum - correction;
+  } else {
+    fact = w_sum - correction * (w * aweights.value()).sum() / w_sum;
+  }
+
+  // TODO: should we check fact >= 0?
+  fact = at::where(fact < 0, at::zeros_like(fact), fact);
+
+  input -= avg.unsqueeze(1);
+  const auto c = at::mm(input, (input * w).t().conj());
+  return at::true_divide(c, fact).squeeze();
 }
 
 } // namespace native
