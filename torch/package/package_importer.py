f78e785ce85eb9a7c796839346fa598618e4cad3
Lillian Johnson <lillianjohnson@fb.com>
2021-05-17 16:59:05 -0700

    Update on "[package] fix persistent_load error"
    Differential Revision: [D28494250](https://our.internmc.facebook.com/intern/diff/D28494250)
    
    [ghstack-poisoned]

--- torch/package/package_importer.py
+++ torch/package/package_importer.py
@@ -209,7 +209,7 @@ class PackageImporter(Importer):
                 return storage
             elif typename == "reduce_package":
                 # to fix BC breaking change, objects on this load path
-                # will be loaded multiple times erroneously 
+                # will be loaded multiple times erroneously
                 if len(data) == 2:
                     func, args = data
                     return func(self, *args)
