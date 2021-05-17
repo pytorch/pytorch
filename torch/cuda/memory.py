4db36bb8bc48262e7f1401b6977c06bfbfcdd3bc
Corey Lammie <coreylammie@gmail.com>
2021-05-17 11:51:54 +1000

    Integrated cudaDetDeviceFreeMemory

--- torch/cuda/memory.py
+++ torch/cuda/memory.py
@@ -41,18 +41,9 @@ def caching_allocator_det_malloc(size, device: Union[Device, int] = None, stream
             raise Exception("Invalid size. size must not exceed {}".format(size))
         memory_to_be_allocated = torch._C._cuda_cudaDetMalloc(size, stream)
         if warn_if_exceeds_free_memory_capacity:
-            memory_stats = torch.cuda.memory_stats(device=current_device)
-            # torch._C._cuda_
-            # free_memory_capacity = memory_stats.get("allocated_bytes.all.reserved", 0) - memory_stats.get("allocated_bytes.all.current", 0)
-            # free_memory_capacity = torch.cuda.get_device_properties(current_device).total_memory - memory_stats.get("active.all.freed", 0)
-            free_memory_capacity = memory_cached(current_device)
-            print(free_memory_capacity)
-            # print(memory_to_be_allocated)
-            # print(free_memory_capacity)
-            # # print(torch.cuda.get_device_properties(current_device).total_memory)
-            # # print(torch.cuda.memory_stats(device=current_device).get("allocated_bytes.all.current", 0))
-            # if memory_to_be_allocated > free_memory_capacity:
-            #     raise Warning("Memory to be allocated exceeds the free memory capacity of the current device ({})".format(free_memory_capacity))
+            free_memory_capacity = torch._C._cuda_cudaDetDeviceFreeMemory(device)
+            if memory_to_be_allocated > free_memory_capacity:
+                warnings.warn("Memory to be allocated exceeds the free memory capacity of the current device ({})".format(free_memory_capacity))
         return memory_to_be_allocated
 
 
