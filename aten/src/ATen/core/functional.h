43249058a3096ffba789ee0c5cb782c6eb3e0324
Ansley Ussery <ansley@fb.com>
2021-05-17 16:44:48 -0700

    Improve `c10::fmap` capabilities
    [ghstack-poisoned]

--- aten/src/ATen/core/functional.h
+++ aten/src/ATen/core/functional.h
@@ -13,14 +13,23 @@ namespace c10 {
 //
 // No explicit template parameters are required.
 
+
+// A traditional C++ approach with iterators
+template<class F, class InputIt>
+inline auto fmap(InputIt first, InputIt last, const F& fn)
+            -> std::vector<decltype(fn(*first))> {
+  std::vector<decltype(fn(*first))> r;
+  while (first != last) {
+    r.push_back(fn(*first));
+  }
+  return r;
+}
+
 // Overload for explicit function and ArrayRef
 template<class F, class T>
-inline auto fmap(const T& inputs, const F& fn) -> std::vector<decltype(fn(*inputs.begin()))> {
-  std::vector<decltype(fn(*inputs.begin()))> r;
-  r.reserve(inputs.size());
-  for(const auto & input : inputs)
-    r.push_back(fn(input));
-  return r;
+inline auto fmap(const T& inputs, const F& fn)
+            -> std::vector<decltype(fn(*inputs.begin()))> {
+  return fmap(inputs.begin(), inputs.end(), fn);
 }
 
 // C++ forbids taking an address of a constructor, so here's a workaround...
@@ -32,6 +41,18 @@ inline std::vector<R> fmap(const T& inputs) {
   for(auto & input : inputs)
     r.push_back(R(input));
   return r;
+}
+
+template<class F, class InputIt, class OutputIt>
+inline void fmap_to(InputIt first, InputIt last, OutputIt d_first, const F& fn) {
+  while (first != last) {
+    *d_first++ = *first++;
+  }
+}
+
+template<class F, class From, class To>
+inline void fmap_to(const From& inputs, To& outputs, const F& fn) {
+  return fmap_to(inputs.begin(), inputs.end(), std::back_inserter(outputs), fn);
 }
 
 template<typename F, typename T>
