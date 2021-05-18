5b6306a11f9afd48874ee0e772382c2da18a945b
Jerry Zhang <jerryzh168@gmail.com>
2021-05-17 18:28:53 -0700

    [quant][fx][graphmode][refactor] Factor out generate_qconfig_map to qconfig_utils.py
    Summary:
    Move the class method generate_qconfig_map to qconfig_utils, will add more PRs
    to remove functions out of Quantizer and eventually remove the Quantizer object
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/quantization/fx/qconfig_utils.py
+++ torch/quantization/fx/qconfig_utils.py
@@ -1,7 +1,11 @@
 import torch
 from collections import OrderedDict
-from typing import Union, Callable, Any, Dict
+from typing import Union, Callable, Any, Dict, Tuple
 import re
+
+from torch.fx.graph import (
+    Graph,
+)
 
 from .utils import _parent_name
 
@@ -97,3 +101,41 @@ def get_qconfig(qconfig_dict, module_type, module_name, global_qconfig):
     module_name_qconfig = get_module_name_qconfig(
         qconfig_dict, module_name, module_name_regex_qconfig)
     return module_name_qconfig
+
+def generate_qconfig_map(
+        root: torch.nn.Module,
+        modules: Dict[str, torch.nn.Module],
+        input_graph: Graph,
+        qconfig_dict: Any,
+        node_name_to_scope: Dict[str, Tuple[str, type]]) -> Dict[str, QConfigAny]:
+    global_qconfig = qconfig_dict.get("", None)
+    qconfig_map = dict()
+    for node in input_graph.nodes:
+        qconfig = None
+        if node.op == "get_attr":
+            module_name, _ = _parent_name(node.target)
+            qconfig = get_qconfig(
+                qconfig_dict, type(modules[module_name]), module_name, global_qconfig)
+        elif node.op == "call_function":
+            # precedence: module_name_qconfig
+            # > function_qconfig > global_qconfig
+            # module_name takes precedence over function qconfig
+            function_qconfig = get_object_type_qconfig(
+                qconfig_dict, node.target, global_qconfig)
+            module_path, module_type = node_name_to_scope[node.name]
+            qconfig = get_qconfig(
+                qconfig_dict, module_type, module_path, function_qconfig)
+        elif node.op == "call_method":
+            module_path, module_type = node_name_to_scope[node.name]
+            # use the qconfig of the module that the node belongs to
+            qconfig = get_qconfig(
+                qconfig_dict, module_type, module_path, global_qconfig)
+        elif node.op == 'call_module':
+            qconfig = get_qconfig(
+                qconfig_dict, type(modules[node.target]), node.target, global_qconfig)
+            # regex is not supported eager mode propagate_qconfig_, we'll
+            # need to set the qconfig explicitly here in case regex
+            # is used
+            modules[node.target].qconfig = qconfig
+        qconfig_map[node.name] = qconfig
+    return qconfig_map
