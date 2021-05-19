9e9425b375181c86e717ba38e3f4c080ef1baa95
Richard Zou <zou3519@gmail.com>
2021-05-19 11:57:56 -0700

    [1.10] Remove torch.vmap
    torch.vmap is a prototype feature and should not be in the stable
    binary. This PR:
    - Removes the torch.vmap API
    - Removes the documentation entry for torch.vmap
    - Changes the vmap tests to use an internal API instead of torch.vmap.
    
    Test Plan:
    - Tested locally (test_torch, test_autograd, test_type_hints, test_vmap),
    but also wait for CI.

--- torch/__init__.py
+++ torch/__init__.py
@@ -756,8 +756,6 @@ del register_after_fork
 # torch.jit.script as a decorator, for instance):
 from ._lobpcg import lobpcg as lobpcg
 
-from ._vmap_internals import vmap as vmap
-
 # These were previously defined in native_functions.yaml and appeared on the
 # `torch` namespace, but we moved them to c10 dispatch to facilitate custom
 # class usage. We add these lines here to preserve backward compatibility.
