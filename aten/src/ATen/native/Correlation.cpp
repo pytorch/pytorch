c779ec121cc2ed6362e4b6775b9917468517cda4
Heitor Schueroff <heitorschueroff@fb.com>
2021-05-20 14:43:33 +0000

    Improve error messages

--- aten/src/ATen/native/Correlation.cpp
+++ aten/src/ATen/native/Correlation.cpp
@@ -13,12 +13,12 @@ Tensor cov(
 
   TORCH_CHECK(
       self.ndimension() <= 2,
-      "cov(): expected input to have two or fewer dimensions but got an input with ,",
+      "cov(): expected input to have two or fewer dimensions but got an input with ",
       self.ndimension(),
       " dimensions");
 
   TORCH_CHECK(
-      self.scalar_type() != kBool, "cov(): bool dtype is not supported");
+      self.scalar_type() != kBool, "cov(): bool dtype is not supported for input");
 
   // View input tensor as 2D (variables, observations)
   auto in = self.ndimension() < 2 ? self.view({1, -1}) : self;
@@ -31,8 +31,9 @@ Tensor cov(
     w = fweights.value();
     TORCH_CHECK(
         w.ndimension() <= 1,
-        "cov(): expected fweights to have one or fewer dimensions but got fweights with ,",
-        w.ndimension());
+        "cov(): expected fweights to have one or fewer dimensions but got fweights with ",
+        w.ndimension(),
+        " dimensions");
     TORCH_CHECK(
         at::isIntegralType(w.scalar_type(), false),
         "cov(): expected fweights to have integral dtype but got fweights with ",
@@ -40,7 +41,7 @@ Tensor cov(
         " dtype");
     TORCH_CHECK(
         w.numel() == num_observations,
-        "cov(): expected fweights to have the same numel as there are observations in the input but got, ",
+        "cov(): expected fweights to have the same numel as there are observations in the input but got ",
         w.numel(),
         " != ",
         num_observations);
@@ -53,8 +54,9 @@ Tensor cov(
     const auto& aw = aweights.value();
     TORCH_CHECK(
         aw.ndimension() <= 1,
-        "cov(): expected aweights to have one or fewer dimensions but got aweights with ,",
-        aw.ndimension());
+        "cov(): expected aweights to have one or fewer dimensions but got aweights with ",
+        aw.ndimension(),
+        " dimensions");
     TORCH_CHECK(
         at::isFloatingType(aw.scalar_type()),
         "cov(): expected aweights to have floating point dtype but got aweights with ",
@@ -62,7 +64,7 @@ Tensor cov(
         " dtype");
     TORCH_CHECK(
         aw.numel() == num_observations,
-        "cov(): expected aweights to have the same numel as there are observations in the input but got, ",
+        "cov(): expected aweights to have the same numel as there are observations in the input but got ",
         aw.numel(),
         " != ",
         num_observations);
@@ -76,19 +78,22 @@ Tensor cov(
   const auto w_sum = w.defined()
       ? w.sum()
       : at::scalar_tensor(num_observations, in.options().dtype(kLong));
+
   const auto avg = (w.defined() ? in * w : in).sum(OBSERVATIONS_DIM) / w_sum;
 
   // Compute the normalization factor
   Tensor norm_factor;
-  if (!w.defined() || !aweights.has_value() || correction == 0) {
-    norm_factor = w_sum - correction;
-  } else {
+
+  if (w.defined() && aweights.has_value() && correction != 0) {
     norm_factor = w_sum - correction * (w * aweights.value()).sum() / w_sum;
+  } else {
+    norm_factor = w_sum - correction;
   }
 
   if (norm_factor.is_cpu() && norm_factor.le(0).item<bool>()) {
     TORCH_WARN("cov(): degrees of freedom is <= 0");
   }
+
   norm_factor =
       at::where(norm_factor < 0, at::zeros_like(norm_factor), norm_factor);
 
