b218539c80f9e45885ffe8e4b3e0f5c9a2e92340
driazati <driazati@users.noreply.github.com>
2021-05-20 11:29:26 -0700

    [skip ci] Use testing builder branch
    [ghstack-poisoned]

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -64,5 +64,6 @@ popd
 retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
+git checkout driazati/debug_whls
 git --no-pager log --max-count 1
 popd
