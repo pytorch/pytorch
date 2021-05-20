1bda1f215c5da8de1d51bbeeb8da09f4f04d6cf2
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-20 07:37:24 -0500

    change empty to zeros

--- aten/src/ATen/native/BatchLinearAlgebra.cpp
+++ aten/src/ATen/native/BatchLinearAlgebra.cpp
@@ -1561,7 +1561,7 @@ std::tuple<Tensor, Tensor, Tensor> _lu_with_info(const Tensor& self, bool comput
   auto req_size = self.sizes().vec();
   req_size.pop_back();
   req_size.back() = std::min(m, n);
-  auto pivots_tensor = at::empty(req_size, self.options().dtype(kInt));
+  auto pivots_tensor = at::zeros(req_size, self.options().dtype(kInt));
   req_size.pop_back();
   auto infos_tensor = at::zeros(req_size, self.options().dtype(kInt));
 
