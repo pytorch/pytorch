1602c422a97027bf835e6f2cfdd1e79f8f81ef03
Natalia Gimelshein <ngimel@fb.com>
2021-05-23 16:22:43 -0700

    simplify cpu_kernel to not have contiguous special case

--- aten/src/ATen/native/cpu/Loops.h
+++ aten/src/ATen/native/cpu/Loops.h
@@ -258,14 +258,9 @@ void cpu_kernel(TensorIteratorBase& iter, func_t&& op) {
   TORCH_INTERNAL_ASSERT(!needs_dynamic_casting<func_t>::check(iter));
 
   iter.for_each([&](char** data, const int64_t* strides, int64_t n) {
-    if (is_contiguous<traits>(strides)) {
+    // basic loop can handle 1d slices with arbitrary strides, and 1d slices is all that
+    // iter.for_each is ever sending to the loop lambda
       basic_loop(data, strides, 0, n, std::forward<func_t>(op));
-    } else {
-      using Indices = std::make_index_sequence<traits::arity>;
-      unroll_contiguous_scalar_checks<traits>(strides, Indices{}, [&](size_t _idx) {
-        basic_loop(data, strides, 0, n, std::forward<func_t>(op));
-      });
-    }
   });
   iter.cast_outputs();
 }
