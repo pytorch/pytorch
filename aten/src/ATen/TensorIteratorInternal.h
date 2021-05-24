32735be220846dbc76fa7a4ae7dea7b8f91d9523
Peter Bell <peterbell10@live.co.uk>
2021-05-24 16:59:48 +0100

    Update on "TensorIteratorReduce: Avoid tensor operations in parallel_for"
    Ref gh-56794
    
    The two pass reduction calls `copy_` and `select` inside a parallel region. The
    `copy_` can just be moved outside of the parallel region, but avoiding the
    `select` call is more complicated because it's needed to construct the
    `TensorIterator`. Instead, I factor out a `serial_for_each` free-function that
    just takes pointers and strides. Then manually advance the pointer to the
    thread-specific slice of data.
    
    [ghstack-poisoned]

--- aten/src/ATen/TensorIteratorInternal.h
+++ aten/src/ATen/TensorIteratorInternal.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <ATen/native/TensorIterator.h>
+#include <c10/util/SmallBuffer.h>
 
 namespace at {
 
@@ -17,35 +19,40 @@ struct DimCounter {
 
 namespace internal {
 
-inline SmallVector<char*, 4> get_data_ptrs(
-    ArrayRef<char*> base, IntArrayRef strides, IntArrayRef counter) {
-  int64_t ntensors = base.size();
-  int64_t ndim = strides.size() / ntensors;
-  auto ptrs = SmallVector<char*, 4>(base);
-  for (int dim = 0; dim < ndim; dim++) {
+inline void get_data_ptrs(
+    char** ptrs, ArrayRef<char*> base, IntArrayRef strides, IntArrayRef counter) {
+  const int64_t ntensors = base.size();
+  const int64_t ndim = counter.size();
+  std::copy(base.begin(), base.end(), ptrs);
+  for (int64_t dim = 0; dim < ndim; ++dim) {
     int64_t value = counter[dim];
-    for (int arg = 0; arg < ntensors; arg++) {
+    for (int64_t arg = 0; arg < ntensors; ++arg) {
       ptrs[arg] += value * strides[dim * ntensors + arg];
     }
   }
-  return ptrs;
 }
 
 inline void serial_for_each(
     IntArrayRef shape, IntArrayRef strides, ArrayRef<char*> base_ptrs,
     typename TensorIteratorBase::loop2d_t loop, Range range) {
   auto ntensors = base_ptrs.size();
+  c10::SmallBuffer<char*, 4> ptrs(ntensors);
 
   if (strides.size() <= ntensors) {  // ndim <= 1
-    auto ptrs = get_data_ptrs(base_ptrs, strides, {range.begin});
+    if (range.begin == 0) {
+      std::copy(base_ptrs.begin(), base_ptrs.end(), ptrs.begin());
+    } else {
+      get_data_ptrs(ptrs.data(), base_ptrs, strides, {range.begin});
+    }
     // Pad strides to 2d
-    DimVector padded_strides(2 * ntensors, 0);
+    c10::SmallBuffer<int64_t, 8> padded_strides(2 * ntensors);
     std::copy(strides.begin(), strides.end(), padded_strides.begin());
+    std::fill(padded_strides.data() + strides.size(), padded_strides.end(), 0);
     loop(ptrs.data(), padded_strides.data(), range.size(), 1);
   } else {
     auto counter = DimCounter(shape, range);
     while (!counter.is_done()) {
-      auto ptrs = get_data_ptrs(base_ptrs, strides, counter.values);
+      get_data_ptrs(ptrs.data(), base_ptrs, strides, counter.values);
       auto step = counter.max_2d_step();
       loop(ptrs.data(), strides.data(), step[0], step[1]);
       counter.increment(step);
