953e1bdc0f4cde6b09c1f230d5dbcb594cd435c6
Brian Hirsh <hirsheybar@fb.com>
2021-05-24 15:46:06 -0700

    test changes against my pytorch branch

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -200,7 +200,7 @@ fi
 
 # Patch required to build xla
 if [[ "${BUILD_ENVIRONMENT}" == *xla* ]]; then
-  git clone --recursive https://github.com/pytorch/xla.git
+  git clone --recursive https://github.com/pytorch/xla.git -b make_codegen_backend_agnostic_minus_fallbacks
   ./xla/scripts/apply_patches.sh
 fi
 
