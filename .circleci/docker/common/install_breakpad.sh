e8943290c4c75fac81cad40be7e31d318517bacc
driazati <driazati@users.noreply.github.com>
2021-05-25 14:20:37 -0700

    Enable breakpad on more images
    This includes the breakpad library in all Docker images except mobile + the slim ones. It gets picked up by the build from the system libraries automatically so this is all that's necessary to enable it on more platforms.
    
    The install breakpad script was also missing some version pinning, so this adds that as well.

--- .circleci/docker/common/install_breakpad.sh
+++ .circleci/docker/common/install_breakpad.sh
@@ -3,11 +3,21 @@
 set -ex
 
 git clone https://github.com/google/breakpad.git
-cd breakpad
+pushd breakpad
+
+# breakpad has no actual releases, so this is pinned to the top commit from
+# master (circa when this was added)
+git checkout f7428bc397fd1e5bd6162a592ebc988d6d585eb3
 
 git clone https://chromium.googlesource.com/linux-syscall-support src/third_party/lss
+pushd src/third_party/lss
+# same as with breakpad, there are no real releases for this repo so use a
+# commit as the pin
+git checkout e1e7b0ad8ee99a875b272c8e33e308472e897660
+popd
+
 ./configure
 make
 make install
-cd ..
+popd
 rm -rf breakpad
