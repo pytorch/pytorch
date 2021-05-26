f3683560529c32995ea939fe7b084cca5c150c35
Richard Zou <zou3519@gmail.com>
2021-05-26 12:07:19 -0700

    [functorch] Add LICENSE headers to code files

--- functorch/test/test_vmap.py
+++ functorch/test/test_vmap.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from torch.testing._internal.common_utils import TestCase, run_tests
 import torch
 import torch.nn.functional as F
