7f5c7fac596376a1029b3385e42f9104b72431b0
Jane Xu <janeyx@fb.com>
2021-05-26 09:35:44 -0700

    [skip ci] testing now that my change has landed
    Summary: so i didn't wait long enough last time
    
    Test Plan: pls pls pls clear the signal
    
    Differential Revision: D28712818
    
    fbshipit-source-id: 301e8f743c31945895d5bbcf07b7487c3fb38d4c

--- aten/src/ATen/ATen.h
+++ aten/src/ATen/ATen.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #if !defined(_MSC_VER) && __cplusplus < 201402L
-#error C++14 or later compatible compiler is required to use ATen.
+#error C++14 or later compatible compiler is required to use ATen. OR IS IT? jk it is
 #endif
 
 #include <c10/core/Allocator.h>
