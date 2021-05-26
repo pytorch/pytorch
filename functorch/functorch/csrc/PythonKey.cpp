0d3d7f99519927b12801ea5204f1ac7c94f50ce8
Richard Zou <zou3519@gmail.com>
2021-05-26 12:07:19 -0700

    [functorch] Add LICENSE headers to code files

--- functorch/functorch/csrc/PythonKey.cpp
+++ functorch/functorch/csrc/PythonKey.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include <functorch/csrc/PythonKey.h>
 #include <torch/csrc/utils/pybind.h>
 #include <torch/csrc/jit/python/pybind_utils.h>
