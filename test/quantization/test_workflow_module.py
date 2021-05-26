946a4fc6fe44165865291f406fc26da6db87c2a9
Supriya Rao <supriyar@fb.com>
2021-05-25 18:11:57 -0700

    Update on "[quant][refactor tests] split test_workflow_module into test_workflow_ops and test_workflow_module"
    Summary:
    some tests are used to check the op level numerics of the fake quantize operations
    
    Test Plan:
    python test/test_quantization.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D28696599](https://our.internmc.facebook.com/intern/diff/D28696599)
    
    [ghstack-poisoned]

--- test/quantization/test_workflow_module.py
+++ test/quantization/test_workflow_module.py
@@ -524,7 +524,7 @@ class TestRecordHistogramObserver(QuantizationTestCase):
                 observer_dict = {}
                 get_observer_dict(model, observer_dict)
 
-                self.assertTrue('fc1.(module).activation_post_process' in observer_dict.keys(),
+                self.assertTrue('fc1.module.activation_post_process' in observer_dict.keys(),
                                 'observer is not recorded in the dict')
                 self.assertEqual(len(observer_dict['fc1.module.activation_post_process'].get_tensor_value()),
                                  2 * len(self.calib_data))
