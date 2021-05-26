519911174898c1b34ae2f1fa2fd3a754e083d429
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-05-26 17:43:08 -0500

    [skip ci] Revert change in comment

--- aten/src/ATen/native/cuda/Dropout.cu
+++ aten/src/ATen/native/cuda/Dropout.cu
@@ -62,7 +62,7 @@ __global__ void fused_dropout_kernel_vec(
 
   float4 rand;
 
-  // Note: Vectorizedd loads means we'll stride each thread by an additional VEC factor, as we'll load VEC elements at a time
+  // Note: Vectorized loads means we'll stride each thread by an additional VEC factor, as we'll load VEC elements at a time
   for (IndexType linearIndex = idx * VEC;
       linearIndex < totalElements;
       linearIndex += gridDim.x * blockDim.x * VEC) {
@@ -105,7 +105,7 @@ __global__ void fused_dropout_kernel_vec(
       r[ii] = src[ii]*(&rand.x)[ii]*pinv;
       mask[ii] = (uint8_t)(&rand.x)[ii];
     }
-    // Vectorizedd writes for both mask & result
+    // Vectorized writes for both mask & result
     *(reinterpret_cast<LoadT*>(&b.data[linearIndex])) = *reinterpret_cast<LoadT*>(&r[0]);
     *(reinterpret_cast<MaskLoadT*>(&c.data[linearIndex])) = *reinterpret_cast<MaskLoadT*>(&mask[0]);
 
