30998ee170b6d8bc03e34dca7170accad4fe8c95
Richard Zou <zou3519@gmail.com>
2021-05-26 12:07:19 -0700

    [functorch] Add LICENSE headers to code files

--- functorch/functorch/csrc/BatchingMetaprogramming.h
+++ functorch/functorch/csrc/BatchingMetaprogramming.h
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #pragma once
 #include <ATen/Tensor.h>
 #include <functorch/csrc/OutOfPlacePlumbing.h>
--- functorch/functorch/csrc/VmapModeRegistrations.cpp
+++ functorch/functorch/csrc/VmapModeRegistrations.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include <torch/library.h>
 #include <ATen/ATen.h>
 #include <functorch/csrc/VmapTransforms.h>
