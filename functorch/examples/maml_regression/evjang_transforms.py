09095946501858b91bf566bc45f9f464b81e8597
Richard Zou <zou3519@gmail.com>
2021-05-27 10:19:18 -0700

    [functorch] Add some citations

--- functorch/examples/maml_regression/evjang_transforms.py
+++ functorch/examples/maml_regression/evjang_transforms.py
@@ -1,3 +1,15 @@
+# This example code was modified from https://github.com/ericjang/maml-jax .
+#
+# The original code comes with the following license:
+# https://github.com/ericjang/maml-jax/blob/master/LICENSE
+# Copyright Eric Jang
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+
 import math
 import random
 import torch
