b3985d2518f963e4764ed41688730cd61ab41de1
ilqar ramazanli <iramazanli@fb.com>
2021-06-01 05:13:29 +0000

    To add functional API for multi Tensor Adadelta

--- torch/optim/_multi_tensor/_functional.py
+++ torch/optim/_multi_tensor/_functional.py
@@ -193,3 +193,36 @@ def sgd(params: List[Tensor],
         # foreach APIs dont support sparse
         for i in range(len(params)):
             params[i].add_(grads[i], alpha=-lr)
+
+
+def adadelta(params: List[Tensor],
+             grads: List[Tensor],
+             square_avgs: List[Tensor],
+             acc_deltas: List[Tensor],
+             *
+             lr: float,
+             weight_decay: float,
+             rho: float):
+    r"""Functional API that performs Adadelta algorithm computation.
+    
+    See :class:`~torch.optim.Adadelta` for details.
+    """
+
+    if weight_decay != 0:
+        torch._foreach_add_(grads, params, alpha=weight_decay)
+
+    torch._foreach_mul_(square_avgs, rho)
+    torch._foreach_addcmul_(square_avgs, grads, grads, value=1 - rho)
+
+    std = torch._foreach_add(square_avgs, eps)
+    torch._foreach_sqrt_(std)
+
+    deltas = torch._foreach_add(acc_deltas, eps)
+    torch._foreach_sqrt_(deltas)
+    torch._foreach_div_(deltas, std)
+    torch._foreach_mul_(deltas, grads)
+
+    torch._foreach_add_(params, deltas, alpha=-lr)
+
+    torch._foreach_mul_(acc_deltas, rho)
+    torch._foreach_addcmul_(acc_deltas, deltas, deltas, value=1 - rho)
