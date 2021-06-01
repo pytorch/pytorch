a1d4899586910af9210a3ae62a0101bfde7306f1
ilqar ramazanli <iramazanli@fb.com>
2021-06-01 05:57:11 +0000

    To add functional API for multi-tensor ASGD optimizer

--- torch/optim/_multi_tensor/_functional.py
+++ torch/optim/_multi_tensor/_functional.py
@@ -269,7 +269,7 @@ def rprop(params: List[Tensor],
 
 def rmsprop(params: List[Tensor],
             grads: List[Tensor],
-            states: List[Tensor],
+            states: List[Dict],
             square_avg: List[Tensor],
             *,
             weight_decay: float,
@@ -308,3 +308,39 @@ def rmsprop(params: List[Tensor],
     else:
         torch._foreach_addcdiv_(params_with_grad, grads, avg, value=-lr)
 
+
+def asgd(params: List[Tensor],
+         grads: List[Tensor],
+         states: List[Dict],
+         lambd: float,
+         lr: float,
+         t0: float,
+         alpha: float,
+         weight_decay: float):
+    r"""Functional API that performs ASGD algorithm computation.
+
+    See :class:`~torch.optim.ASGD` for details.
+    """
+
+    if weight_decay != 0:
+        torch._foreach_add_(grads, params, alpha=weight_decay)
+
+    # decay term
+    eta = states[0]['eta'] 
+    torch._foreach_mul_(params, 1 - lambd * eta)
+
+    # update parameter
+    torch._foreach_add_(params, grads, alpha=-eta)
+
+    # averaging
+    for i in range(len(states)):
+        if states[i]['mu'] != 1:
+            states[i]['ax'].add_(params[i].sub(states[i]['ax']).mul(states[i]['mu']))
+        else:
+            states[i]['ax'].copy_(params[i])
+
+    # update eta and mu
+    for state in states:
+        state['eta'] = (lr /
+                        math.pow((1 + lambd * lr * state['step']), alpha))
+        state['mu'] = 1 / max(1, state['step'] - t0)
