b36ca8769c4451c82f0fc504820f8c8b956aea62
Eli Uriegas <eliuriegas@fb.com>
2021-06-01 22:03:06 -0700

    Update on ".github: Remove windows dependency installs"
    We were observing 403s when attempting to install dependencies from
    chocolatey leading us to believe that we were getting rate limited from
    chocolatey.
    
    We've instead opted to install our dependencies in our base AMIs instead
    considering we would install them on every workflow anyway. This also
    comes with the moving of the windows 10 sdk installation to the base sdk
    as well since we were observing failures there as well due to failed
    dependency installations.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .circleci/scripts/vs_install.ps1
+++ .circleci/scripts/vs_install.ps1
@@ -12,8 +12,11 @@ $VS_INSTALL_ARGS = @("--nocache","--quiet","--wait", "--add Microsoft.VisualStud
                                                      "--add Microsoft.VisualStudio.Component.VC.Redist.14.Latest",
                                                      "--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core",
                                                      "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
-                                                     "--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Win81"
-                                                     "--add Microsoft.VisualStudio.Component.Windows10SDK")
+                                                     "--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Win81")
+
+if (${env:INSTALL_WINDOWS_SDK} -eq "1") {
+    $VS_INSTALL_ARGS.Add("--add Microsoft.VisualStudio.Component.Windows10SDK.19041")
+}
 
 curl.exe --retry 3 -kL $VS_DOWNLOAD_LINK --output vs_installer.exe
 if ($LASTEXITCODE -ne 0) {
