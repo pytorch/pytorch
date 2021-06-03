57f2d95caa7527ccdcad4aa218834a1ad6b57acb
Howard Huang <howardhuang@fb.com>
2021-06-03 12:59:02 -0700

    Revert D28538996: Enable torch::deploy GPU tests in sandcastle
    Test Plan: revert-hammer
    
    Differential Revision:
    D28538996 (https://github.com/pytorch/pytorch/commit/4b74c848aa5a4b05f36a21c36d469d5cc60c116e)
    
    Original commit changeset: 1a6ccea07cfe
    
    fbshipit-source-id: 6e01a96d3746d3ca3e4e792a7b623ef960c9d2d6

--- torch/csrc/deploy/test_deploy_gpu.cpp
+++ torch/csrc/deploy/test_deploy_gpu.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 #include <torch/csrc/deploy/deploy.h>
-#include <torch/cuda.h>
 #include <torch/script.h>
 #include <torch/torch.h>
 #include <future>
@@ -22,9 +21,6 @@ const char* path(const char* envname, const char* path) {
 }
 
 TEST(TorchDeployGPUTest, SimpleModel) {
-  if (!torch::cuda::is_available()) {
-    GTEST_SKIP();
-  }
   const char* model_filename = path("SIMPLE", simple);
   const char* jit_filename = path("SIMPLE_JIT", simple_jit);
 
