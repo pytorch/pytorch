20f9e562c7263b6b24e7db87163416348438bac5
Alexander Golynski <agolynski@fb.com>
2021-06-03 11:13:17 -0700

    Switch PG::Work to Future in default_comm_hooks.cpp
    [ghstack-poisoned]

--- torch/lib/c10d/default_comm_hooks.cpp
+++ torch/lib/c10d/default_comm_hooks.cpp
@@ -9,18 +9,19 @@ namespace c10d {
 c10::intrusive_ptr<c10::ivalue::Future> AllReduceCommHook::runHook(
     GradBucket& bucket) {
   std::vector<at::Tensor> tensors = {bucket.getTensorRef()};
-  auto allreduce_work = state_->allreduce(tensors);
+  auto allreduce_fut = state_->allreduce(tensors)->getFuture();
 
-  // FIXME Access the result through the Future passed as argument, instead of
-  // capturing the Work.
-  auto div_by_process_group_size = [allreduce_work,
+  auto div_by_process_group_size = [allreduce_fut,
                                     this](c10::ivalue::Future& /* unused */) {
-    auto tensor = allreduce_work->result()[0] / state_->getSize();
+
+    auto result = allreduce_fut->value();
+    TORCH_INTERNAL_ASSERT(result.isTensorList(),
+        "ProcessGroup::allreduce should return TensorList");
+    auto tensor = result.toTensorVector()[0] / state_->getSize();
     return c10::IValue(tensor);
   };
 
-  auto fut = allreduce_work->getFuture();
-  return fut->then(div_by_process_group_size, fut->elementType());
+  return allreduce_fut->then(div_by_process_group_size, allreduce_fut->elementType());
 }
 
 c10::intrusive_ptr<c10::ivalue::Future> FP16CompressCommHook::runHook(
@@ -28,20 +29,22 @@ c10::intrusive_ptr<c10::ivalue::Future> FP16CompressCommHook::runHook(
   auto& tensor = bucket.getTensorRef();
   tensor.copy_(tensor.to(torch::kFloat16));
   std::vector<at::Tensor> tensors = {tensor};
-  auto allreduce_work = state_->allreduce(tensors);
+  auto allreduce_fut = state_->allreduce(tensors)->getFuture();
 
   // FIXME Access the result through the Future passed as argument, instead of
   // capturing the Work.
   auto decompress_and_div_by_process_group_size =
-      [allreduce_work, this](c10::ivalue::Future& /* unused */) {
-        auto tensor = allreduce_work->result()[0];
+      [allreduce_fut, this](c10::ivalue::Future& /* unused */) {
+        auto result = allreduce_fut->value();
+        TORCH_INTERNAL_ASSERT(result.isTensorList(),
+            "ProcessGroup::allreduce should return TensorList");
+        auto tensor = result.toTensorVector()[0];
         tensor.copy_(tensor.to(torch::kFloat) / state_->getSize());
         return c10::IValue(tensor);
       };
 
-  auto fut = allreduce_work->getFuture();
-  return fut->then(
-      decompress_and_div_by_process_group_size, fut->elementType());
+  return allreduce_fut->then(
+      decompress_and_div_by_process_group_size, allreduce_fut->elementType());
 }
 
 } // namespace c10d
