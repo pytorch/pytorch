f6e5b2ffec3569ba858f09e87e2398843ffc277c
Peng Wu <pengwu@fb.com>
2021-06-03 10:21:49 -0700

    Add schema-based transfer function dispatch
    Summary:
    
    + Added all the ops needed for mobilenetv3
    + Use schemas to dispatch to different transfer functions
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/jit/test_tensor_property_propagation.py
+++ test/jit/test_tensor_property_propagation.py
@@ -3,7 +3,6 @@ from torch.testing._internal.jit_utils import JitTestCase
 
 from torch.testing import FileCheck
 
-
 if __name__ == '__main__':
     raise RuntimeError("This test file is not meant to be run directly, use:\n\n"
                        "\tpython test/test_jit.py TESTNAME\n\n"
@@ -19,7 +18,7 @@ class TestTensorPropertyPropagation(JitTestCase):
 
         @torch.jit.script
         def foo(x, y):
-            return x + y
+            return x // y
 
         inputs = list(foo.graph.inputs())
 
@@ -30,6 +29,15 @@ class TestTensorPropertyPropagation(JitTestCase):
             ("Float", "Int", "Float"),
         ]
 
+        # binary_op_list = ['add']
+        # for op in binary_op_list:
+        # code = '''
+        #         graph(%3 : Tensor, %4 : Tensor):
+        #             %2 : Tensor(*, *) = aten::{op}(%3, %4)
+        #             %1 : {dtype}(*, *) = aten::relu(%2)
+        #             return (%1)
+        #     '''.format(op=op)
+
         for inp0, inp1, result in test_cases:
             # set scalar type of input tensors
             inputs[0].setType(inputs[0].type().with_scalarType(inp0))
@@ -37,4 +45,4 @@ class TestTensorPropertyPropagation(JitTestCase):
             print(f"After setType graph: {foo.graph}")
             torch._C._jit_pass_propagate_tensor_property_on_graph(foo.graph)
             print(f"After propagation graph: {foo.graph}")
-            FileCheck().check(f"{tensor_name_dict[result]} = aten::add").run(foo.graph)
+            FileCheck().check(f"{tensor_name_dict[result]} = aten::floor_divide").run(foo.graph)
--- torch/csrc/jit/passes/tensor_property_propagation.cpp
+++ torch/csrc/jit/passes/tensor_property_propagation.cpp
@@ -1,4 +1,5 @@
 #include <ATen/core/function_schema.h>
+#include <ATen/core/interned_strings.h>
 #include <ATen/core/jit_type.h>
 #include <c10/core/ScalarType.h>
 #include <c10/util/Optional.h>
@@ -16,6 +17,10 @@ namespace {
 #define TRACE_EXEC(msg, ...) \
   std::cout << "TRACE_EXEC:" << msg << c10::str(__VA_ARGS__) << std::endl;
 
+using dtype_func_t = std::function<c10::optional<ScalarType>(Node*)>;
+static std::vector<std::pair<OperatorSet, dtype_func_t>>
+    dtype_transfer_functions;
+
 // TensorPropertyPropagationPass is an analysis pass that walks through a graph
 // in topological order and forward propagate TensorProperties from graph inputs
 // (expressed in input_descriptors) to all output tensor nodes in the graph.
@@ -27,7 +32,13 @@ namespace {
 //  the output tensor will be updated
 struct TensorPropertyPropagationPass {
   TensorPropertyPropagationPass(std::shared_ptr<Graph> graph)
-      : graph_(std::move(graph)) {}
+      : graph_(std::move(graph)) {
+    // FIXME (penguin): multi-threading? how do I initialize things once for
+    // all?
+    if (dtype_transfer_functions.empty()) {
+      initializeTransferFunctions();
+    }
+  }
 
   // returns true if at least one node has its scalar type set on a tensor node
   bool run() {
@@ -35,7 +46,7 @@ struct TensorPropertyPropagationPass {
   }
 
  private:
-  c10::optional<at::ScalarType> getScalarType(const Value* value) const {
+  static c10::optional<at::ScalarType> getScalarType(const Value* value) {
     c10::optional<at::ScalarType> stype;
     auto type = value->type();
     if (type->kind() == TypeKind::TensorType) {
@@ -90,15 +101,15 @@ struct TensorPropertyPropagationPass {
 
   bool processNode(Node* n) {
     TRACE_EXEC("processNode");
-    // process block nodes
     switch (n->kind()) {
       case prim::If:
         return processIf(n);
+      case prim::RaiseException:
+        return processRaiseException(n);
       case prim::Loop:
-        return processLoop(n);
       case prim::CallMethod:
       case prim::CallFunction:
-        return processCall(n);
+        TORCH_INTERNAL_ASSERT(false, "Loop/Call not handled now");
       default:
         break;
     }
@@ -126,18 +137,18 @@ struct TensorPropertyPropagationPass {
       case prim::Constant:
         changed = processConstant(n);
         break;
-      case aten::add:
-      case aten::add_:
-      case aten::conv2d:
-      case aten::append:
-      case aten::view:
-        changed = processAtenOps(n);
-        break;
       case prim::ListConstruct:
         changed = processListConstruct(n);
         break;
+      case prim::ListUnpack:
+        changed = processListUnpack(n);
+        break;
       default:
-        TORCH_INTERNAL_ASSERT(false, "not supported IR");
+        if (n->kind().is_aten()) {
+          changed = processAtenOps(n);
+        } else {
+          TORCH_INTERNAL_ASSERT(false, "not supported IR");
+        }
     }
     return changed;
   }
@@ -170,19 +181,99 @@ struct TensorPropertyPropagationPass {
     return changed;
   }
 
-  bool processLoop(Node* n) {
-    TORCH_INTERNAL_ASSERT(false, "Loop not supported yet");
-    return false;
-  }
-
-  bool processCall(Node* n) {
-    TORCH_INTERNAL_ASSERT(false, "Call not supported yet");
+  bool processRaiseException(Node* n) {
+    TORCH_INTERNAL_ASSERT(false, "RaiseException not supported yet");
     return false;
   }
 
   bool processListConstruct(Node* n) {
     TORCH_INTERNAL_ASSERT(false, "ListConstruct not supported yet");
     return false;
+  }
+
+  bool processListUnpack(Node* n) {
+    TORCH_INTERNAL_ASSERT(false, "ListUnpack not supported yet");
+    return false;
+  }
+
+  bool checkSchemaReturnsTensors(const c10::FunctionSchema* schema) {
+    const std::vector<Argument>& return_args = schema->returns();
+    bool has_tensor_output = false;
+    for (size_t i = 0; i < return_args.size(); i++) {
+      auto arg = return_args[i];
+      if (auto tt = arg.type()->castRaw<TensorType>()) {
+        has_tensor_output = true;
+        break;
+      }
+    }
+    return has_tensor_output;
+  }
+
+  // TODO: keep a cache of valid schema to avoid repeated schema checking
+  // for efficiency
+  bool processAtenOps(Node* n) {
+    TRACE_EXEC("processAtenOps");
+
+    auto schema_opt = n->maybeSchema();
+    if (!schema_opt || !checkSchemaReturnsTensors(schema_opt)) {
+      TRACE_EXEC("schema not found or op does not return tensors");
+      return false;
+    }
+
+    TRACE_EXEC("case = ", n->kind(), " ", *n);
+
+    bool changed = false;
+    bool found = false;
+    c10::optional<ScalarType> scalarType;
+    switch (n->kind()) {
+      case aten::eq:
+      case aten::lt:
+      case aten::gt:
+      case aten::ne:
+        scalarType = kBool;
+        found = true;
+        break;
+      case aten::dim:
+        scalarType = kInt64;
+        found = true;
+        break;
+      case aten::size:
+      case aten::len:
+        scalarType = kInt;
+        found = true;
+        break;
+      case aten::format: // note: returns string but ScalarType does not include
+                         // string
+        break;
+      case aten::append:
+        // TODO: add
+        break;
+      default:
+        for (auto& entry : dtype_transfer_functions) {
+          if (n->isMemberOf(entry.first)) {
+            scalarType = entry.second(n);
+            found = true;
+            break;
+          }
+        }
+    }
+    if (!found) {
+      TORCH_INTERNAL_ASSERT(false, "schema not supported yet: ", schema_opt);
+    } else {
+      if (scalarType.has_value()) {
+        TORCH_INTERNAL_ASSERT(
+            n->outputs().size() == 1, "Only handle a single output");
+        changed = setTensorScalarType(n->output(0), scalarType.value());
+      }
+    }
+    return changed;
+  }
+
+  AliasDb* getOrCreateAliasDb() {
+    if (!aliasDb_) {
+      aliasDb_ = std::make_unique<AliasDb>(graph_);
+    }
+    return aliasDb_.get();
   }
 
   // simpleTypeTransferFunction returns the type that is common among all input
@@ -193,7 +284,7 @@ struct TensorPropertyPropagationPass {
   //    input types = (float, int, int)
   //    when promoteToCommonType == true: output type = float
   //    when promoteToCommonType == false: output type = nullopt
-  c10::optional<ScalarType> simpleTypeTransferFunction(
+  static c10::optional<ScalarType> simpleTypeTransferFunction(
       Node* n,
       bool promoteType = false) {
     auto scalarType = getScalarType(n->inputs().at(0));
@@ -224,6 +315,15 @@ struct TensorPropertyPropagationPass {
     return stype;
   }
 
+  // This transfer function returns the dtype of the <idx>th operand
+  static c10::optional<ScalarType> typeOfNthOperand(Node* n, int idx) {
+    auto stype = getScalarType(n->inputs().at(idx));
+    if (stype.has_value()) {
+      TRACE_EXEC("typeOfNthOperand: result = ", stype.value());
+    }
+    return stype;
+  }
+
   // This transfer function uses the scalar type of one input as that of the
   // output slected_input: the index of the input Value* whose scalar type will
   // be used for the output
@@ -233,75 +333,83 @@ struct TensorPropertyPropagationPass {
     TORCH_INTERNAL_ASSERT(false, "not implemented");
   }
 
-  bool checkSchemaReturnsTensors(const c10::FunctionSchema* schema) {
-    const std::vector<Argument>& return_args = schema->returns();
-    bool has_tensor_output = false;
-    for (size_t i = 0; i < return_args.size(); i++) {
-      auto arg = return_args[i];
-      if (auto tt = arg.type()->castRaw<TensorType>()) {
-        has_tensor_output = true;
-        break;
+  // Initialize all transfer functions for OperatorSet
+  static void initializeTransferFunctions() {
+    // transfer functions take a node as input and propagate properties to the
+    // outputs of the node, return true if output node properties are updated.
+    struct register_transfer_func_for {
+      register_transfer_func_for(OperatorSet operators, dtype_func_t tfunc) {
+        dtype_transfer_functions.emplace_back(
+            std::move(operators), std::move(tfunc));
       }
-    }
-    return has_tensor_output;
+    };
+
+    static const register_transfer_func_for simple_ops_with_common_type_promotion{
+        {
+            "aten::add(Tensor self, Tensor other, *, Scalar alpha) -> Tensor",
+            // TODO: use mutation variant rules
+            "aten::add_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha) -> Tensor(a!)",
+            "aten::sub(Tensor self, Tensor other, *, Scalar alpha) -> Tensor",
+            "aten::div(Tensor self, Tensor other) -> Tensor",
+            "aten::mul(Tensor self, Tensor other) -> Tensor",
+            "aten::floor_divide(Tensor self, Tensor other) -> Tensor",
+            // TODO: use meta-tensor rules
+            "aten::addmm(Tensor self, Tensor mat1, Tensor mat2, *, Scalar beta, Scalar alpha) -> Tensor",
+        },
+        [](Node* node) -> c10::optional<ScalarType> {
+          return simpleTypeTransferFunction(node, true);
+        }};
+
+    // simply return the dtype of the 1st operand
+    static const register_transfer_func_for ops_return_with_first_operand_type{
+        {
+            "aten::hardsigmoid(Tensor self) -> Tensor",
+            "aten::hardswish(Tensor self) -> Tensor",
+            "aten::hardtanh(Tensor self, Scalar min_val, Scalar max_val) -> Tensor",
+            "aten::relu(Tensor self) -> Tensor",
+            "aten::transpose.int(Tensor(a) self, int dim0, int dim1) -> Tensor(a)",
+            "aten::transpose.Dimname(Tensor(a) self, Dimname dim0, Dimname dim1) -> Tensor(a)",
+            "aten::view(Tensor(a) self, int[] size) -> Tensor(a)",
+            "aten::flatten.using_ints(Tensor(a) self, int start_dim, int end_dim) -> Tensor(a)",
+            "aten::flatten.named_out_dim(Tensor(a) self, int start_dim, int end_dim, Dimname out_dim) -> Tensor(a)",
+            "aten::flatten.using_names(Tensor(a) self, Dimname start_dim, Dimname end_dim, Dimname out_dim) -> Tensor(a)",
+            "aten::flatten.DimnameList(Tensor(a) self, Dimname[] dims, Dimname out_dim) -> Tensor(a)",
+            "aten::max_pool2d(Tensor self, int[2] kernel_size, int[2] stride, int[2] padding, int[2] dilation, bool ceil_mode) -> Tensor",
+            "aten::chunk(Tensor(a) self, int chunks, int dim) -> Tensor(a)[]",
+            "aten::contiguous(Tensor(a) self, *, MemoryFormat memory_format) -> Tensor(a)",
+            "aten::adaptive_avg_pool2d(Tensor self, int[2] output_size) -> Tensor",
+            "aten::avg_pool2d(Tensor self, int[2] kernel_size, int[2] stride, int[2] padding, bool ceil_mode, bool count_include_pad, int? divisor_override) -> Tensor",
+            "aten::cat(Tensor[] tensors, int dim) -> Tensor",
+            "aten::expand_as(Tensor(a) self, Tensor other) -> Tensor(a)",
+            // TODO: need validation
+            "aten::batch_norm(Tensor input, Tensor? weight, Tensor? bias, Tensor? running_mean, Tensor? running_var, bool training, float momentum, float eps, bool cudnn_enabled) -> Tensor",
+            "aten::linear(Tensor input, Tensor weight, Tensor? bias) -> Tensor",
+            "aten::matmul(Tensor self, Tensor other) -> Tensor",
+            "aten::conv2d(Tensor input, Tensor weight, Tensor? bias, int[2] stride, int[2] padding, int[2] dilation, int groups) -> Tensor",
+            "aten::conv2d.padding(Tensor input, Tensor weight, Tensor? bias, int[2] stride, str padding, int[2] dilation, int groups) -> Tensor",
+            // TODO: use out variant rules
+            "aten::adaptive_avg_pool2d.out(Tensor self, int[2] output_size, *, Tensor(a!) out) -> Tensor(a!)",
+            "aten::avg_pool2d.out(Tensor self, int[2] kernel_size, int[2] stride, int[2] padding, bool ceil_mode, bool count_include_pad, int? divisor_override, *, Tensor(a!) out) -> Tensor(a!)",
+            "aten::addmm.out(Tensor self, Tensor mat1, Tensor mat2, *, Scalar beta, Scalar alpha, Tensor(a!) out) -> Tensor(a!)",
+            "aten::matmul.out(Tensor self, Tensor other, *, Tensor(a!) out) -> Tensor(a!)",
+            "aten::cat.out(Tensor[] tensors, int dim, *, Tensor(a!) out) -> Tensor(a!)",
+        },
+        [](Node* node) -> c10::optional<ScalarType> {
+          return typeOfNthOperand(node, 0);
+        }};
   }
 
-  // TODO: keep a cache of valid schema to avoid repeated schema checking
-  // for efficiency
-  bool processAtenOps(Node* n) {
-    TRACE_EXEC("processAtenOps");
-
-    auto schema_opt = n->maybeSchema();
-    if (!schema_opt || !checkSchemaReturnsTensors(schema_opt)) {
-      TRACE_EXEC("schema not found or op does not return tensors");
-      return false;
-    }
-
-    bool changed = false;
-    c10::optional<ScalarType> scalarType;
-    TRACE_EXEC("case = ", n->kind(), " ", *n);
-    switch (n->kind()) {
-      case aten::add: {
-        if (n->matches(
-                "aten::add(Tensor self, Tensor other, *, Scalar alpha) -> Tensor")) {
-          scalarType = simpleTypeTransferFunction(n, true);
-        } else {
-          TORCH_INTERNAL_ASSERT(false, "add other schema not supported yet");
-        }
-        break;
-      }
-      case aten::add_:
-        TORCH_INTERNAL_ASSERT(false, "add_ not supported yet");
-        break;
-      case aten::conv2d:
-        TORCH_INTERNAL_ASSERT(false, "conv2d not supported yet");
-        break;
-      default:
-        TORCH_INTERNAL_ASSERT(false, "AtenOps not supported yet");
-    }
-
-    if (scalarType.has_value()) {
-      TORCH_INTERNAL_ASSERT(
-          n->outputs().size() == 1, "Only handle a single output");
-      changed = setTensorScalarType(n->output(0), scalarType.value());
-    }
-    return changed;
-  }
-
-  AliasDb* getOrCreateAliasDb() {
-    if (!aliasDb_) {
-      aliasDb_ = std::make_unique<AliasDb>(graph_);
-    }
-    return aliasDb_.get();
-  }
+  /*
+  // This one is a special rule -- mean take the ScalarType if specified,
+  otherwise Tensor type
+  // This is an example where the dtype rule has to be extracted from the
+  implementation
+  "aten::mean(Tensor self, *, ScalarType? dtype) -> Tensor",
+  */
 
   std::shared_ptr<Graph> graph_;
   // lazily initialized if using aliasing_types, otherwise not initialized
   std::unique_ptr<AliasDb> aliasDb_ = nullptr;
-
-  // stores inferred tensor properties
-  // SymbolicTensorPropertiesMap value_to_tensor_properties_map_;
-  // const SymbolicTensorPropertiesMap & input_value_to_tensor_properties_map_;
 };
 
 } // anonymous namespace
