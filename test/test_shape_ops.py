b640730f09fe0d779d24a11db20257689f059bca
Lezcano <Lezcano@users.noreply.github.com>
2021-06-03 10:58:10 +0000

    Update test/test_shape_ops.py
    Co-authored-by: Gao, Xiang <qasdfgtyuiop@gmail.com>

--- test/test_shape_ops.py
+++ test/test_shape_ops.py
@@ -463,7 +463,7 @@ class TestShapeOps(TestCase):
     @onlyCUDA  # CPU is too slow
     @largeTensorTest('17GB') # 4 tensors of 4GB (in, out) x (torch, numpy) + 1GB
     def test_flip_large_tensor(self, device):
-        t_in = torch.empty(2**32 + 1, dtype=toch.uint8).random_()
+        t_in = torch.empty(2**32 + 1, dtype=torch.uint8).random_()
         torch_fn = partial(torch.flip, dims=(0,))
         np_fn = partial(np.flip, axis=0)
         self.compare_with_numpy(torch_fn, np_fn, t_in)
