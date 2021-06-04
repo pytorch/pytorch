69ec5be5966d8b7342933184df3b840270c00e71
Bert Maher <bertrand@fb.com>
2021-06-03 23:09:09 -0700

    [nnc] Add hardsigmoid (#59069)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/59069
    
    Test Plan: Imported from OSS
    
    Reviewed By: mrshenli
    
    Differential Revision: D28738166
    
    Pulled By: bertmaher
    
    fbshipit-source-id: d9f5b87ef1f2323a3631add79c2670ce794f911e

--- test/test_jit_fuser_te.py
+++ test/test_jit_fuser_te.py
@@ -1262,6 +1262,7 @@ class TestTEFuser(JitTestCase):
             torch.atan,
             torch.tanh,
             F.hardtanh,
+            F.hardsigmoid,
             F.hardswish,
             torch.sqrt,
             torch.rsqrt,
@@ -1810,6 +1811,35 @@ class TestTEFuser(JitTestCase):
         z = 2
         script = self.checkScript(eager, (x, y, z))
 
+    def _test_fwd_bwd(self, fn):
+        x = torch.arange(-10, 10, dtype=torch.float32, requires_grad=True)
+        xs = torch.arange(-10, 10, dtype=torch.float32, requires_grad=True)
+        script = torch.jit.script(fn)
+        for i in range(11):
+            y = fn(x)
+            g0 = torch.rand_like(y)
+            y.backward(g0)
+
+            ys = script(xs)
+            ys.backward(g0)
+
+            with torch.no_grad():
+                x -= 0.1 * x.grad
+                xs -= 0.1 * xs.grad
+                x.grad = None
+                xs.grad = None
+        torch.testing.assert_allclose(y, ys)
+
+    def test_relu_fwd_bwd(self):
+        def eager(x):
+            return torch.relu(x * 1.01)
+        self._test_fwd_bwd(eager)
+
+    def test_hardsigmoid_fwd_bwd(self):
+        def eager(x):
+            return F.hardsigmoid(x) * 1.01
+        self._test_fwd_bwd(eager)
+
     def test_dynamic_cat(self):
         with inline_fusion_groups():
             @torch.jit.script
@@ -1875,6 +1905,7 @@ works_list = [
     'neg',
     'nn.functional.gelu',
     'nn.functional.hardshrink',
+    'nn.functional.hardsigmoid',
     'nn.functional.hardswish',
     'nn.functional.hardtanh',
     'nn.functional.leaky_relu',
