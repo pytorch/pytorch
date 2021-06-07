f8ad0f76d95536358f209032c2b7c8de1156e4ad
Rong Rong <rongr@fb.com>
2021-06-07 09:45:00 -0700

    enalbe upload test stats on PR

--- tools/print_test_stats.py
+++ tools/print_test_stats.py
@@ -766,12 +766,12 @@ def send_report_to_s3(head_report: Version2Report) -> None:
     job = os.environ.get('CIRCLE_JOB')
     sha1 = os.environ.get('CIRCLE_SHA1')
     branch = os.environ.get('CIRCLE_BRANCH', '')
-    if branch not in ['master', 'nightly'] and not branch.startswith("release/"):
-        print("S3 upload only enabled on master, nightly and release branches.")
-        print(f"skipping test report on branch: {branch}")
-        return
     now = datetime.datetime.utcnow().isoformat()
-    key = f'test_time/{sha1}/{job}/{now}Z.json.bz2'  # Z meaning UTC
+    if branch not in ['master', 'nightly'] and not branch.startswith("release/"):
+        pr = os.environ.get('CIRCLE_PR_NUMBER', 'unknown')
+        key = f'test_time/{pr}/{sha1}/{job}/{now}Z.json.bz2'  # Z meaning UTC
+    else:
+        key = f'test_time/{sha1}/{job}/{now}Z.json.bz2'  # Z meaning UTC
     obj = get_S3_object_from_bucket('ossci-metrics', key)
     # use bz2 because the results are smaller than gzip, and the
     # compression time penalty we pay is only about half a second for
