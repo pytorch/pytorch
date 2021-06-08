197d5ac360900f6fff62b34ea1de91263183860d
Taylor Robie <taylorrobie@fb.com>
2021-06-08 10:40:26 -0700

    Update base for Update on "proof of concept for redispatch inline optimization"
    Right now the `::redispatch` functions are in a cpp file, which costs about 30 instructions relative to inlining them. (Or defining extern TypedOperatorDefs) This PR isn't really for submission, but rather just to start a discussion about how we can collect this perf. (Since it will be really easy to run afoul of subtle linker / binary size / SOIF issues if we just start codegen-ing massive numbers of static defs.)
    
    Perf: (instructions in C++)
    ```
    function unary             504 -> 395*  (22%)
    function binary            634 -> 473*	(25%)
    function (manual) unary    489 -> 344	(30%)
    function (manual) binary   613 -> 395	(35%)
    method unary               504 -> 395*	(22%)
    method binary              634 -> 473*	(25%)
    method (manual) unary      489 -> 344	(30%)
    method (manual) binary     613 -> 395	(35%)
    
    *Does not inline redispatch, but could.
    ```
    
    
    
    [ghstack-poisoned]

--- aten/src/ATen/Overhead.cpp
+++ aten/src/ATen/Overhead.cpp
@@ -1,22 +1,36 @@
 #include <ATen/ATen.h>
 
 namespace at {
+
+namespace {
+Tensor trivial_empty() {
+  return detail::empty_generic(
+    /*size=*/{1},
+    /*allocator=*/getCPUAllocator(),
+    /*dispatch_key=*/DispatchKey::CPU,
+    /*scalar_type=*/ScalarType::Float,
+    /*device=*/c10::Device(c10::kCPU),
+    /*memory_format_opt=*/c10::MemoryFormat::Contiguous
+  );
+}
+}  // namespace
+
 namespace native {
 
 Tensor _noop_unary(const Tensor& self) {
-  return self;
+  return std::move(trivial_empty());
 }
 
 Tensor _noop_unary_manual(const Tensor& self) {
-  return self;
+  return std::move(trivial_empty());
 }
 
 Tensor _noop_binary(const Tensor& self, const Tensor& other) {
-  return self;
+  return std::move(trivial_empty());
 }
 
 Tensor _noop_binary_manual(const Tensor& self, const Tensor& other) {
-  return self;
+  return std::move(trivial_empty());
 }
 
 }  // namespace native
