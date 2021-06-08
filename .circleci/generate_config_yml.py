3b73f225d8c97a9e58603fd9f2dd343b8b5023a4
Tao Xu <taox@fb.com>
2021-06-08 12:13:29 -0700

    Update on "[DO NOT MERGE][iOS] Upload libotorch-lite to S3"
    [ghstack-poisoned]

--- .circleci/generate_config_yml.py
+++ .circleci/generate_config_yml.py
@@ -129,6 +129,7 @@ def filter_master_only_jobs(items):
 
 def gen_build_workflows_tree():
     build_workflows_functions = [
+        cimodel.data.simple.ios_definitions.get_workflow_jobs,
         cimodel.data.simple.nightly_ios.get_workflow_jobs,
     ]
 
