a1280c9307a3235676c7a1b6ddbd13b1fbd03842
Yi Zhang <zhanyi@microsoft.com>
2021-06-08 02:43:05 -0700

    fix incorrect grammar

--- .jenkins/pytorch/win-test-helpers/build_pytorch.bat
+++ .jenkins/pytorch/win-test-helpers/build_pytorch.bat
@@ -39,9 +39,10 @@ if not "%USE_CUDA%"=="1" goto cuda_build_end
 set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_VERSION%
 
 rem version transformer, for example 10.1 to 10_1.
-if x%CUDA_VERSION:.=%==x%CUDA_VERSION%
+if x%CUDA_VERSION:.=%==x%CUDA_VERSION% (
     echo CUDA version %CUDA_VERSION% format isn't correct, which doesn't contain '.'
     exit /b 1
+)
 set VERSION_SUFFIX=%CUDA_VERSION:.=_%
 set CUDA_PATH_V%VERSION_SUFFIX%=%CUDA_PATH%
 
