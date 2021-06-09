fa030d1213600fa8626540f1b8857cbcbfeeb67c
Kevin Tse <ktse@fb.com>
2021-06-09 16:51:58 -0700

    [DataPipes] Add simple unbatch to DataPipe (#59610)
    Summary:
    Implements the simple unbatch feature for DataPipe https://github.com/pytorch/pytorch/issues/58148
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59610
    
    Reviewed By: VitalyFedyunin
    
    Differential Revision: D28994180
    
    Pulled By: NivekT
    
    fbshipit-source-id: 4bafe6e26c4f95a808c489b147369413a196fa1c

--- test/test_datapipe.py
+++ test/test_datapipe.py
@@ -415,34 +415,6 @@ class TestFunctionalIterDataPipe(TestCase):
         with self.assertRaises(ValueError):
             input_dp.map(fn, nesting_level=-2)
 
-        def addition(item, amount=1.0):
-            return item + amount
-
-        map_dp = input_dp.map(addition, nesting_level=-1)
-        self.assertEqual(len(input_dp), len(map_dp))
-        for x, y in zip(map_dp, input_dp):
-            self.assertEqual(len(x), len(y))
-            for a, b in zip(x, y):
-                self.assertEqual(a, b + 1.0)
-
-        map_dp = input_dp.map(addition, fn_kwargs={'amount': 2.0}, nesting_level=-1)
-        self.assertEqual(len(input_dp), len(map_dp))
-        for x, y in zip(map_dp, input_dp):
-            self.assertEqual(len(x), len(y))
-            for a, b in zip(x, y):
-                self.assertEqual(a, b + 2.0)
-
-        def subtraction(item, amount):
-            return item - amount
-
-        map_dp = input_dp.map(subtraction, fn_args=(1.0, ), nesting_level=-1)
-        self.assertEqual(len(input_dp), len(map_dp))
-        for x, y in zip(map_dp, input_dp):
-            self.assertEqual(len(x), len(y))
-            for a, b in zip(x, y):
-                self.assertEqual(a, b - 1.0)
-
-
     def test_collate_datapipe(self):
         arrs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         input_dp = IDP(arrs)
@@ -488,6 +460,54 @@ class TestFunctionalIterDataPipe(TestCase):
         batch_dp_nl = input_dp_nl.batch(batch_size=2)
         with self.assertRaisesRegex(TypeError, r"instance doesn't have valid length$"):
             len(batch_dp_nl)
+
+    def test_unbatch_datapipe(self):
+
+        target_length = 6
+        prebatch_dp = IDP(range(target_length))
+
+        input_dp = prebatch_dp.batch(3)
+        unbatch_dp = input_dp.unbatch()
+        self.assertEqual(len(list(unbatch_dp)), target_length)
+        for i, res in zip(prebatch_dp, unbatch_dp):
+            self.assertEqual(i, res)
+
+        input_dp = IDP([[0, 1, 2], [3, 4, 5]])
+        unbatch_dp = input_dp.unbatch()
+        self.assertEqual(len(list(unbatch_dp)), target_length)
+        for i, res in zip(prebatch_dp, unbatch_dp):
+            self.assertEqual(i, res)
+
+        input_dp = IDP([[[0, 1], [2, 3]], [[4, 5], [6, 7]]])
+
+        unbatch_dp = input_dp.unbatch()
+        expected_dp = [[0, 1], [2, 3], [4, 5], [6, 7]]
+        self.assertEqual(len(list(unbatch_dp)), 4)
+        for i, res in zip(expected_dp, unbatch_dp):
+            self.assertEqual(i, res)
+
+        unbatch_dp = input_dp.unbatch(unbatch_level=2)
+        expected_dp2 = [0, 1, 2, 3, 4, 5, 6, 7]
+        self.assertEqual(len(list(unbatch_dp)), 8)
+        for i, res in zip(expected_dp2, unbatch_dp):
+            self.assertEqual(i, res)
+
+        unbatch_dp = input_dp.unbatch(unbatch_level=-1)
+        self.assertEqual(len(list(unbatch_dp)), 8)
+        for i, res in zip(expected_dp2, unbatch_dp):
+            self.assertEqual(i, res)
+
+        input_dp = IDP([[0, 1, 2], [3, 4, 5]])
+        with self.assertRaises(ValueError):
+            unbatch_dp = input_dp.unbatch(unbatch_level=-2)
+            for i in unbatch_dp:
+                print(i)
+
+        with self.assertRaises(IndexError):
+            unbatch_dp = input_dp.unbatch(unbatch_level=5)
+            for i in unbatch_dp:
+                print(i)
+
 
     def test_bucket_batch_datapipe(self):
         input_dp = IDP(range(20))
