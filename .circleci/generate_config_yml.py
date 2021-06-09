dd334a9ea377a73d5357f1f98a06ab0fcc9d5afa
Tao Xu <taox@fb.com>
2021-06-09 16:36:53 -0700

    [DO NOT MERGE][iOS] upload binary for lite interpreter
    [ghstack-poisoned]

--- .circleci/generate_config_yml.py
+++ .circleci/generate_config_yml.py
@@ -129,21 +129,21 @@ def filter_master_only_jobs(items):
 
 def gen_build_workflows_tree():
     build_workflows_functions = [
-        cimodel.data.simple.docker_definitions.get_workflow_jobs,
-        pytorch_build_definitions.get_workflow_jobs,
-        cimodel.data.simple.macos_definitions.get_workflow_jobs,
-        cimodel.data.simple.android_definitions.get_workflow_jobs,
-        cimodel.data.simple.ios_definitions.get_workflow_jobs,
-        cimodel.data.simple.mobile_definitions.get_workflow_jobs,
-        cimodel.data.simple.ge_config_tests.get_workflow_jobs,
-        cimodel.data.simple.bazel_definitions.get_workflow_jobs,
-        cimodel.data.simple.binary_smoketest.get_workflow_jobs,
+        # cimodel.data.simple.docker_definitions.get_workflow_jobs,
+        # pytorch_build_definitions.get_workflow_jobs,
+        # cimodel.data.simple.macos_definitions.get_workflow_jobs,
+        # cimodel.data.simple.android_definitions.get_workflow_jobs,
+        # cimodel.data.simple.ios_definitions.get_workflow_jobs,
+        # cimodel.data.simple.mobile_definitions.get_workflow_jobs,
+        # cimodel.data.simple.ge_config_tests.get_workflow_jobs,
+        # cimodel.data.simple.bazel_definitions.get_workflow_jobs,
+        # cimodel.data.simple.binary_smoketest.get_workflow_jobs,
         cimodel.data.simple.nightly_ios.get_workflow_jobs,
-        cimodel.data.simple.nightly_android.get_workflow_jobs,
-        cimodel.data.simple.anaconda_prune_defintions.get_workflow_jobs,
-        windows_build_definitions.get_windows_workflows,
-        binary_build_definitions.get_post_upload_jobs,
-        binary_build_definitions.get_binary_smoke_test_jobs,
+        # cimodel.data.simple.nightly_android.get_workflow_jobs,
+        # cimodel.data.simple.anaconda_prune_defintions.get_workflow_jobs,
+        # windows_build_definitions.get_windows_workflows,
+        # binary_build_definitions.get_post_upload_jobs,
+        # binary_build_definitions.get_binary_smoke_test_jobs,
     ]
 
     binary_build_functions = [
@@ -155,17 +155,9 @@ def gen_build_workflows_tree():
     master_build_jobs = filter_master_only_jobs(build_jobs)
     return {
         "workflows": {
-            "binary_builds": {
-                "when": r"<< pipeline.parameters.run_binary_tests >>",
-                "jobs": [f() for f in binary_build_functions],
-            },
             "build": {
                 "when": r"<< pipeline.parameters.run_build >>",
                 "jobs": build_jobs,
-            },
-            "master_build": {
-                "when": r"<< pipeline.parameters.run_master_build >>",
-                "jobs": master_build_jobs,
             },
         }
     }
