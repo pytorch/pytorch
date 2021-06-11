cbafb1c950c9fc1eeb40ed09e502cefbab4d4b32
Luca Wehrstedt <lcw@fb.com>
2021-06-11 05:04:04 -0700

    Fix Windows guards in c10d (#59696)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59696
    
    Some files in c10d refer to dist autograd. However, on Windows, dist autograd isn't built. Hence we need to "mask out" those references under Windows. This was already partly done, but when moving c10d to libtorch some issues came up, possibly due to the different way in which linking happens. Hence I masked out the remaining references.
    ghstack-source-id: 131169541
    
    Test Plan: CI
    
    Reviewed By: agolynski
    
    Differential Revision: D28987579
    
    fbshipit-source-id: c29c5330f8429d699554972d30f99a89b2e3971d

--- torch/lib/c10d/reducer.hpp
+++ torch/lib/c10d/reducer.hpp
@@ -7,6 +7,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include <ATen/core/ivalue_inl.h>
+#include <ATen/ThreadLocalState.h>
 #include <c10/util/intrusive_ptr.h>
 #include <c10d/ProcessGroup.hpp>
 #include <c10d/Utils.hpp>
@@ -14,7 +16,9 @@
 #include <c10d/default_comm_hooks.hpp>
 #include <torch/csrc/autograd/function.h>
 #include <torch/csrc/autograd/variable.h>
+#ifndef _WIN32
 #include <torch/csrc/distributed/autograd/context/context.h>
+#endif
 
 namespace c10d {
 
@@ -224,8 +228,19 @@ class Reducer {
   // the buckets
   void sync_bucket_indices(std::vector<std::vector<size_t>>& bucket_indices);
 
-  using GradCallback =
-      torch::distributed::autograd::DistAutogradContext::GradCallback;
+  // We'd like to use DistAutogradContext::GradCallback here but dist autograd
+  // doesn't exist under Windows. So we just directly use the concrete type but
+  // to preserve and enforce our original intent we do a static assert when dist
+  // autograd is available.
+  using GradCallback = std::function<bool(at::Tensor&)>;
+#ifndef _WIN32
+  static_assert(
+      std::is_same<
+          GradCallback,
+          torch::distributed::autograd::DistAutogradContext::GradCallback>::
+          value,
+      "");
+#endif
   void runGradCallbackForVariable(at::Tensor& variable, GradCallback&& cb);
 
   // A bucket replica represents [1..N] gradients to be reduced,
@@ -318,7 +333,7 @@ class Reducer {
     // Keep future work handle around DDP comm hook.
     // If no hook is registered, a temporary vanilla allreduce hook will be
     // used.
-    c10::intrusive_ptr<torch::jit::Future> future_work;
+    c10::intrusive_ptr<at::ivalue::Future> future_work;
 
     // If this bucket should expect a single sparse gradient.
     // Implies: replicas[i].variables.size() == 1.
@@ -381,6 +396,7 @@ class Reducer {
   std::vector<int64_t> rebuilt_param_indices_;
   const int64_t bucket_bytes_cap_;
 
+#ifndef _WIN32
   struct RpcContext {
     using ContextPtr = torch::distributed::autograd::ContextPtr;
     // The shared_ptr is to hold the context instance.
@@ -390,6 +406,7 @@ class Reducer {
     void set(ContextPtr&& new_context_ptr);
   };
   RpcContext rpc_context_;
+#endif
 
   // A struct containing work handle and tensor for allreduce scheduled in
   // forward pass, if applicable.
