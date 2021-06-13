9371a09e6cb25bcd5f619853ddb1da98fb54302c
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2021-06-13 15:16:37 +0530

    Remove print statements

--- test/test_ops.py
+++ test/test_ops.py
@@ -351,7 +351,6 @@ class TestCommon(JitCommonTestCase):
                                                       **sample.kwargs)
                         continue
 
-                    print(cloned, *sample.args, **sample.kwargs)
                     variant_forward = variant(cloned,
                                               *sample.args,
                                               **sample.kwargs)
@@ -552,7 +551,6 @@ class TestCommon(JitCommonTestCase):
                         args_annot_kw=", ".join(args_annot_kw),
                         args_kw=", ".join(args_kw),
                     )
-                print(script, variant.__name__)
                 scripted = torch.jit.CompilationUnit(script)._fn
 
                 if (variant is inplace and not torch.can_cast(expected_dtype, dtype)):
