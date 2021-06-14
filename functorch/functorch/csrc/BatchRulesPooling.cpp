23013e425586f1c2f8f4ba29a8a2c40b5564a3b2
Richard Zou <zou3519@gmail.com>
2021-06-14 12:32:37 -0700

    [functorch] maxpool_2d_with_indices_backward batch rule for specific case
    This gives us coverage on all the batch rules for the cifar10 dp
    example. We're slightly slower than opacus though, the numbers on my
    machine are:
    - 4 it/s for functorch vmap+grad
    - 4.2 it/s for opacus
    
    The differential should be investigated and I am also not sure if the
    benchmarks are comparing the right things.

--- functorch/functorch/csrc/BatchRulesPooling.cpp
+++ functorch/functorch/csrc/BatchRulesPooling.cpp
@@ -22,8 +22,61 @@ std::tuple<Tensor,optional<int64_t>> adaptive_avg_pool2d_batch_rule(
   return std::make_tuple( reshape_dim_outof(0, batch_size, result), 0 );
 }
 
+std::tuple<Tensor,int64_t> max_pool2d_with_indices_backward_batch_rule(
+    const Tensor & grad_output, optional<int64_t> grad_output_bdim,
+    const Tensor & self, optional<int64_t> self_bdim,
+    IntArrayRef kernel_size, IntArrayRef stride, IntArrayRef padding,
+    IntArrayRef dilation, bool ceil_mode,
+    const Tensor & indices, optional<int64_t> indices_bdim) {
+  TORCH_INTERNAL_ASSERT(grad_output_bdim && self_bdim && indices_bdim);
+
+  auto bdim_size = self.size(*self_bdim);
+  auto grad_output_ = reshape_dim_into(*grad_output_bdim, 0, grad_output);
+  auto self_ = reshape_dim_into(*self_bdim, 0, self);
+  auto indices_ = reshape_dim_into(*indices_bdim, 0, indices);
+
+  auto result = at::max_pool2d_with_indices_backward(
+      grad_output_, self_, kernel_size, stride, padding, dilation, ceil_mode,
+      indices_);
+
+  result = reshape_dim_outof(0, bdim_size, result);
+  return {result, 0};
+}
+
+Tensor max_pool2d_with_indices_backward_plumbing(const Tensor & grad_output, const Tensor & self, IntArrayRef kernel_size, IntArrayRef stride, IntArrayRef padding, IntArrayRef dilation, bool ceil_mode, const Tensor & indices) {
+  auto maybe_layer = maybeCurrentDynamicLayer();
+  TORCH_INTERNAL_ASSERT(maybe_layer.has_value());
+  int64_t cur_level = maybe_layer->layerId();
+
+  Tensor grad_output_value;
+  optional<int64_t> grad_output_bdim;
+  std::tie(grad_output_value, grad_output_bdim) = unwrapTensorAtLevel(grad_output, cur_level);
+  Tensor self_value;
+  optional<int64_t> self_bdim;
+  std::tie(self_value, self_bdim) = unwrapTensorAtLevel(self, cur_level);
+  Tensor indices_value;
+  optional<int64_t> indices_bdim;
+  std::tie(indices_value, indices_bdim) = unwrapTensorAtLevel(indices, cur_level);
+
+  if (self_bdim && grad_output_bdim && indices_bdim) {
+    c10::impl::ExcludeDispatchKeyGuard guard(kBatchedKey);
+    auto result = max_pool2d_with_indices_backward_batch_rule(
+        grad_output_value, grad_output_bdim,
+        self_value, self_bdim,
+        kernel_size, stride, padding, dilation, ceil_mode,
+        indices_value, indices_bdim);
+    return makeBatched(std::get<0>(result), std::get<1>(result), cur_level);
+  }
+
+  static auto op = c10::Dispatcher::singleton()
+    .findSchemaOrThrow("aten::max_pool2d_with_indices_backward", "");
+  return slow_fallback<Tensor>(op, { grad_output, self, kernel_size, stride, padding, dilation, ceil_mode, indices });
+}
+
+
 TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   VMAP_SUPPORT("adaptive_avg_pool2d", adaptive_avg_pool2d_batch_rule);
+  m.impl("max_pool2d_with_indices_backward", max_pool2d_with_indices_backward_plumbing);
 }
 
 }}
