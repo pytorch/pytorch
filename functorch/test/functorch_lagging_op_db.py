7dae4c89dd2f95bdb2034f3a61be15e436d5b3db
Richard Zou <zou3519@gmail.com>
2021-06-15 10:47:53 -0700

    [functorch] Fix some headers

--- functorch/test/functorch_lagging_op_db.py
+++ functorch/test/functorch_lagging_op_db.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 from torch.testing._internal.common_methods_invocations import op_db
 
 # Generated from codegen/gen_functorch_op_db.py via
