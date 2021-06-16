4a3174cd182c4afdafdeee68fc4af5dfdb9da0dc
Eli Uriegas <eliuriegas@fb.com>
2021-06-16 11:05:45 -0700

    Update on "ci: Add note about file_diff_from_base for GHA"
    file_diff_from_base is currently bugged for ghstack PRs since it fails
    to find a merge base
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    Differential Revision: [D29168767](https://our.internmc.facebook.com/intern/diff/D29168767)
    
    [ghstack-poisoned]

--- .jenkins/pytorch/macos-test.sh
+++ .jenkins/pytorch/macos-test.sh
@@ -54,6 +54,7 @@ test_python_all() {
   # Try to pull value from CIRCLE_PULL_REQUEST first then GITHUB_HEAD_REF second
   # CIRCLE_PULL_REQUEST comes from CircleCI
   # NOTE: file_diff_from_base is currently bugged for GHA due to an issue finding a merge base for ghstack PRs
+  #       see https://github.com/pytorch/pytorch/issues/60111
   IN_PULL_REQUEST=${CIRCLE_PULL_REQUEST:-${GITHUB_HEAD_REF:-}}
   if [ -n "$IN_PULL_REQUEST" ]; then
     DETERMINE_FROM=$(mktemp)
