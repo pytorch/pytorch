71984f96176441453a782b25e7f87866e7e34ec1
Masaki Kozuki <mkozuki@nvidia.com>
2021-06-18 06:48:52 +0900

    [add/sub] Cast 16-bit inputs

--- aten/src/ATen/native/cuda/BinaryAddSubKernel.cu
+++ aten/src/ATen/native/cuda/BinaryAddSubKernel.cu
@@ -1,3 +1,4 @@
+#include <ATen/AccumulateType.h>
 #include <ATen/Dispatch.h>
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/cuda/Loops.cuh>
@@ -8,19 +9,20 @@
 
 namespace at { namespace native {
 
-template<typename scalar_t>
+template<typename scalar_t, typename accscalar_t>
 struct AddFunctor {
-  AddFunctor(scalar_t a): alpha(a) {}
+  AddFunctor(accscalar_t a): alpha(a) {}
   __device__ __forceinline__ scalar_t operator() (const scalar_t a, const scalar_t b) const {
     return a + alpha * b;
   }
   private:
-    scalar_t alpha;
+    accscalar_t alpha;
 };
 
 void add_kernel_cuda(TensorIteratorBase& iter, const Scalar& alpha_scalar) {
   AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(kHalf, kBool, kBFloat16, iter.common_dtype(), "add_cuda/sub_cuda", [&]() {
-    AddFunctor<scalar_t> f(alpha_scalar.to<scalar_t>());
+    using accscalar_t = at::acc_type<scalar_t, true>;
+    AddFunctor<scalar_t, accscalar_t> f(alpha_scalar.to<accscalar_t>());
     gpu_kernel_with_scalars(iter, f);
   });
 }
