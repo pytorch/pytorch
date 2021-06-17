0de4422d4a1638a703fffa663b929fc7a46f2073
Luca Wehrstedt <lcw@fb.com>
2021-06-17 11:27:26 -0700

    [reland] Add some TORCH_API annotations to RPC
    Summary: Reland of #59939.
    
    Test Plan: CI
    
    Differential Revision: D29193233
    
    fbshipit-source-id: fdee5578280cdd253d46dacd5f0e351ca4211ceb

--- torch/csrc/distributed/rpc/tensorpipe_utils.h
+++ torch/csrc/distributed/rpc/tensorpipe_utils.h
@@ -15,7 +15,7 @@ namespace torch {
 namespace distributed {
 namespace rpc {
 
-const c10::Stream& getStreamForDevice(
+TORCH_API const c10::Stream& getStreamForDevice(
     const std::vector<c10::Stream>& streams,
     const c10::Device& device);
 
@@ -44,12 +44,12 @@ class TensorpipeDeviceTypeConverter {
   virtual ~TensorpipeDeviceTypeConverter() = default;
 };
 
-extern C10_API std::array<
+extern TORCH_API std::array<
     std::atomic<const TensorpipeDeviceTypeConverter*>,
     static_cast<size_t>(DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES)>
     device_type_converter_registry;
 
-class C10_API TensorpipeDeviceTypeConverterRegistrar {
+class TORCH_API TensorpipeDeviceTypeConverterRegistrar {
  public:
   TensorpipeDeviceTypeConverterRegistrar(
       DeviceType,
