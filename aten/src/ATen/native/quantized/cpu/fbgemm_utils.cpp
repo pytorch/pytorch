f4630f92133ba3d5f0fd0df276114f10f4294898
David Reiss <dreiss@fb.com>
2021-06-17 22:39:29 -0700

    Use more type inference in fbgemm_utils.cpp
    Summary:
    This just makes it possible to change the serialization type in a future
    diff without changing this file.
    
    Test Plan:
    CI
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cpu/fbgemm_utils.cpp
+++ aten/src/ATen/native/quantized/cpu/fbgemm_utils.cpp
@@ -368,14 +368,14 @@ Tensor ConvertConvWeightsToChannelLastTensor<3>(
         "quantized", "Conv" + c10::to_string(kSpatialDim) + "dPackedParamsBase")
     .def_pickle(
         [](const c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>>& params)
-        -> ConvParamsSerializationType { // __getstate__
+        -> typeof(serialize_conv<kSpatialDim>(params)) { // __getstate__
           return serialize_conv<kSpatialDim>(params);
         },
         // __setstate__ takes c10::IValue because we support parsing historical
         // serialization versions.
         [](c10::IValue v)
         -> c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> { // __setstate__
-          ConvParamsSerializationType state = parse_conv_serialized_state<kSpatialDim>(v);
+          auto state = parse_conv_serialized_state<kSpatialDim>(v);
           return deserialize_conv<kSpatialDim>(state);
         })
     .def("weight", [](const c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>>& self) {
