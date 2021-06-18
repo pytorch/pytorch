1a88d236e39e40772fe17b4807d987225a367b6b
Masaki Kozuki <mkozuki@nvidia.com>
2021-06-18 11:15:12 +0900

    check device type, not cuda availability

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -587,7 +587,7 @@ class TestForeach(TestCase):
             foreach_op_(tensors1, tensors2)
 
         # different devices
-        if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        if self.device_type == "cuda" and torch.cuda.device_count() > 1:
             tensor1 = torch.zeros(10, 10, device="cuda:0", dtype=dtype)
             tensor2 = torch.ones(10, 10, device="cuda:1", dtype=dtype)
             if dtype == torch.bool and foreach_op == torch._foreach_sub:
