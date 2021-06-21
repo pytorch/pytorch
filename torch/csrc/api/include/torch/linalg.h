b2000cfee29ee517eb65ab5028c7d8cf5e5fbcc5
Heitor Schueroff <heitorschueroff@fb.com>
2021-06-21 16:09:23 +0000

    Replace deprecated comment with C10_DEPRECATED in linalg.h

--- torch/csrc/api/include/torch/linalg.h
+++ torch/csrc/api/include/torch/linalg.h
@@ -214,7 +214,7 @@ inline Tensor cholesky_out(Tensor& result, const Tensor& self) {
   return detail::cholesky_out(result, self);
 }
 
-/// DEPRECATED
+C10_DEPRECATED("torch::linalg::linalg_det is deprecated, use torch::linalg::det instead.")
 inline Tensor linalg_det(const Tensor& self) {
   return detail::det(self);
 }
@@ -294,22 +294,22 @@ inline std::tuple<Tensor, Tensor, Tensor, Tensor> lstsq(const Tensor& self, cons
   return detail::lstsq(self, b, cond, driver);
 }
 
-/// DEPRECATED
+C10_DEPRECATED("torch::linalg::linalg_norm is deprecated, use torch::linalg::norm instead.")
 inline Tensor linalg_norm(const Tensor& self, const optional<Scalar>& opt_ord, optional<IntArrayRef> opt_dim, bool keepdim, optional<ScalarType> opt_dtype) {
   return detail::norm(self, opt_ord, opt_dim, keepdim, opt_dtype);
 }
 
-/// DEPRECATED
+C10_DEPRECATED("torch::linalg::linalg_norm is deprecated, use torch::linalg::norm instead.")
 inline Tensor linalg_norm(const Tensor& self, c10::string_view ord, optional<IntArrayRef> opt_dim, bool keepdim, optional<ScalarType> opt_dtype) {
   return detail::norm(self, ord, opt_dim, keepdim, opt_dtype);
 }
 
-/// DEPRECATED
+C10_DEPRECATED("torch::linalg::linalg_norm is deprecated, use torch::linalg::norm instead.")
 inline Tensor& linalg_norm_out(Tensor& result, const Tensor& self, const optional<Scalar>& opt_ord, optional<IntArrayRef> opt_dim, bool keepdim, optional<ScalarType> opt_dtype) {
   return detail::norm_out(result, self, opt_ord, opt_dim, keepdim, opt_dtype);
 }
 
-/// DEPRECATED
+C10_DEPRECATED("torch::linalg::linalg_norm is deprecated, use torch::linalg::norm instead.")
 inline Tensor& linalg_norm_out(Tensor& result, const Tensor& self, c10::string_view ord, optional<IntArrayRef> opt_dim, bool keepdim, optional<ScalarType> opt_dtype) {
   return detail::norm_out(result, self, ord, opt_dim, keepdim, opt_dtype);
 }
