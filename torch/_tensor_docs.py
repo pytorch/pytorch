737a6aca53cbce512e520f51aedd2f15420edd9a
Martin Pašen <pasen.mm@gmail.com>
2021-06-21 19:00:13 +0200

    documentation

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -2841,11 +2841,26 @@ See :func:`torch.numel`
 
 add_docstr_all('numpy',
                r"""
-numpy() -> numpy.ndarray
+numpy(force=False) -> numpy.ndarray
 
-Returns :attr:`self` tensor as a NumPy :class:`ndarray`. This tensor and the
+Returns :attr:`self` tensor as a NumPy :class:`ndarray`.
+
+If :attr:`force` is ``False``, tensor :attr:`self` and the
 returned :class:`ndarray` share the same underlying storage. Changes to
 :attr:`self` tensor will be reflected in the :class:`ndarray` and vice versa.
+If tensors storage is not CPU, or if :attr:`requires_grad` is ``True`` and 
+:attr:`GradMode` is ``True``, or tensor is sprase, or :attr:`is_conj()` 
+is ``True``, it throws exception.
+
+If :attr:`force` is ``True`` it tries to resolve issues, that would throw exception.
+It calls :func:`torch.Tensor.detach`, :func:`torch.Tensor.cpu` and if :attr:`is_conj()` 
+is ``True`` it calls :func:`torch.Tensor.resolve_conj`.
+Therefore tensor :attr:`self` and the returned :class:`ndarray` can have separate
+underlying storage and changes to :attr:`self` may not be reflected 
+in the :class:`ndarray` and vice versa.
+
+Args:
+    force (bool): whether to copy/change :attr:`self` or throw exception
 """)
 
 add_docstr_all('orgqr',
