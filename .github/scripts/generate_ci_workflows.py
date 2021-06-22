ac8e28187e9d06eadedef6a293635e7191df7a5b
imaginary-person <sanchit@cs.wisc.edu>
2021-06-22 02:09:16 -0400

    pytorch-linux-bionic-py3.8-gcc9-coverage.yml should be run in PRs as well

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -171,6 +171,7 @@ LINUX_WORKFLOWS = [
         build_environment="pytorch-linux-bionic-py3.8-gcc9-coverage",
         docker_image_base=f"{DOCKER_REGISTRY}/pytorch/pytorch-linux-bionic-py3.8-gcc9",
         test_runner_type=LINUX_CPU_TEST_RUNNER,
+        on_pull_request=True,
     ),
     # PyTorchLinuxWorkflow(
     #     build_environment="pytorch-linux-bionic-rocm3.9-py3.6",
