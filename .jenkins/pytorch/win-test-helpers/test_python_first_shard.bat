7c01963685adc1a8cafb6378c06c3ebc18b8af84
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-06-22 17:47:37 -0500

    [skip ci] [skip-ci]

--- .jenkins/pytorch/win-test-helpers/test_python_first_shard.bat
+++ .jenkins/pytorch/win-test-helpers/test_python_first_shard.bat
@@ -4,7 +4,7 @@ echo Copying over test times file
 copy /Y "%PYTORCH_FINAL_PACKAGE_DIR_WIN%\.pytorch-test-times.json" "%TEST_DIR_WIN%"
 
 pushd test
-python run_test.py --exclude-jit-executor --shard 1 2 --verbose --determine-from="%1"
+python run_test.py --continue-through-error --exclude-jit-executor --shard 1 2 --verbose --determine-from="%1"
 if ERRORLEVEL 1 exit /b 1
 
 popd
