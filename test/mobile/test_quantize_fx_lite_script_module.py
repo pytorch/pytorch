a22c25245a45e54449e24bebc5544c7865607a2b
pavithran <pavithran@devvm1803.vll0.facebook.com>
2021-06-22 11:04:23 -0700

    [PyTorch][Edge] Tests for QuantizationFx API on lite modules
    # Context
    Add tests for Lite modules that are quantized using fx API
    
    Differential Revision: [D29306402](https://our.internmc.facebook.com/intern/diff/D29306402/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D29306402/)!
    
    [ghstack-poisoned]

---
+++ test/mobile/test_quantize_fx_lite_script_module.py
@@ -0,0 +1,102 @@
+import io
+
+import torch
+import torch.nn as nn
+import torch.nn.quantized as nnq
+import torch.utils.bundled_inputs
+
+# graph mode quantization based on fx
+from torch.quantization.quantize_fx import (
+    prepare_fx,
+    convert_fx,
+)
+from torch.testing._internal.common_quantization import NodeSpec as ns
+from torch.testing._internal.common_quantization import (
+    QuantizationTestCase,
+)
+from torch.testing._internal.common_quantized import (
+    override_quantized_engine,
+)
+from torch.quantization import (
+    default_qconfig,
+    float_qparams_weight_only_qconfig,
+)
+from torch.jit.mobile import _load_for_lite_interpreter
+
+
+class TestFuseFx(QuantizationTestCase):
+    def _compare_script_and_mobile(self, model: torch.nn.Module, input: torch.Tensor):
+        qengine = "qnnpack"
+        with override_quantized_engine(qengine):
+            script_module = torch.jit.script(model)
+            script_module_result = script_module(input)
+
+            max_retry = 5
+            for retry in range(1, max_retry + 1):
+                # retires `max_retry` times; breaks iff succeeds else throws exception
+                try:
+                    buffer = io.BytesIO(
+                        script_module._save_to_buffer_for_lite_interpreter()
+                    )
+                    buffer.seek(0)
+                    mobile_module = _load_for_lite_interpreter(buffer)
+
+                    mobile_module_result = mobile_module(input)
+
+                    torch.testing.assert_allclose(
+                        script_module_result, mobile_module_result
+                    )
+                    mobile_module_forward_result = mobile_module.forward(input)
+                    torch.testing.assert_allclose(
+                        script_module_result, mobile_module_forward_result
+                    )
+
+                    mobile_module_run_method_result = mobile_module.run_method(
+                        "forward", input
+                    )
+                    torch.testing.assert_allclose(
+                        script_module_result, mobile_module_run_method_result
+                    )
+                except AssertionError as e:
+                    if retry == max_retry:
+                        raise e
+                    else:
+                        continue
+                break
+
+    def test_embedding(self):
+        class M(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.emb = torch.nn.Embedding(num_embeddings=10, embedding_dim=12)
+
+            def forward(self, indices):
+                return self.emb(indices)
+
+        model = M().eval()
+        indices = torch.tensor([9, 6, 5, 7, 8, 8, 9, 2, 8, 6, 6, 9, 1, 6, 8, 8, 3, 2, 3, 6, 3, 6, 5, 7, 0, 8, 4, 6, 5, 8, 2, 3])
+
+        quantized_node = ns.call_module(nnq.Embedding)
+        configs = [
+            (float_qparams_weight_only_qconfig, ns.call_module(nnq.Embedding)),
+            (None, ns.call_module(nn.Embedding)),
+            (default_qconfig, ns.call_module(nn.Embedding)),
+        ]
+
+        for qconfig, node in configs:
+            qconfig_dict = {"": qconfig}
+            m = prepare_fx(model, qconfig_dict)
+            self.checkGraphModuleNodes(
+                m,
+                expected_node_occurrence={
+                    ns.call_module(torch.quantization.MinMaxObserver): 0
+                },
+            )
+            m = convert_fx(m)
+            self.checkGraphModuleNodes(m, expected_node=node)
+            # make sure it runs
+            self._compare_script_and_mobile(m, input=indices)
+
+
+if __name__ == "__main__":
+    run_tests()
