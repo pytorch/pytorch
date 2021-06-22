4c7f64eeba24ab060d41272ea34e690b41e8244e
Luca Wehrstedt <lcw@fb.com>
2021-06-22 02:42:05 -0700

    Support RRefs that contain threading.Locks
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57943
    
    This is a common scenario (our own tutorials propose it), hence we should ensure it works.
    
    A more generic solution is desirable, but this should fix the immediate concern.
    ghstack-source-id: 132050805
    
    Differential Revision: [D28316076](https://our.internmc.facebook.com/intern/diff/D28316076/)

--- torch/testing/_internal/distributed/rpc/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -524,10 +524,12 @@ def async_cuda_nested_add(to, x, y, z):
 # A custom Python class that contains a tensor, needed to see if we correctly
 # use the Python pickler to extract tensors from non-IValue-convertible types.
 class TensorWrapper:
-    __slots__ = ("tensor",)
+    __slots__ = ("tensor", "lock")
 
     def __init__(self, t):
         self.tensor = t
+        # Add one non-picklable field, to ensure it's ignored/skipped.
+        self.lock = Lock()
 
 
 # Copied from test/test_cuda.py.
