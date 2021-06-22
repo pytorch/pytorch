abd24f0c0a7064d20a514bbae6895eb2ebe98663
Winston Smith <76181208+imaginary-person@users.noreply.github.com>
2021-06-22 17:46:54 -0500

    [skip-ci] [skip ci]

--- .jenkins/pytorch/win-test-helpers/test_python.bat
+++ .jenkins/pytorch/win-test-helpers/test_python.bat
@@ -1,3 +1,3 @@
 call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
-cd test && python run_test.py --exclude-jit-executor --verbose --determine-from="%1" && cd ..
+cd test && python run_test.py --continue-through-error --exclude-jit-executor --verbose --determine-from="%1" && cd ..
 if ERRORLEVEL 1 exit /b 1
