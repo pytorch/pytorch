0d2a936176ab8c4ecf3651f3360212f088039ff1
Ilqar Ramazanli <iramazanli@fb.com>
2021-06-22 10:37:31 -0700

    To add Rectified Adam Algorithm to Optimizers (#58968)
    Summary:
    Fixes : https://github.com/pytorch/pytorch/issues/24892
    
    In the paper : https://arxiv.org/pdf/1908.03265.pdf  Liyuan Liu et al. suggested a new optimization algorithm with an essence of similar to Adam Algorithm.
    
    It has been discussed in the paper that, without warmup heuristic, in the early stage of adaptive optimization / learning algorithms sometimes we can get undesirable large variance which can slow overall convergence process.
    
    Authors proposed the idea of rectification of variance of adaptive learning rate when it is expected to be high.
    
    Differing from the paper, we selected variance tractability cut-off as 5 instead of 4. This adjustment is common practice, and could be found in the code-repository and also tensorflow swift optim library as well :
    
    https://github.com/LiyuanLucasLiu/RAdam/blob/2f03dd197022da442c6a15c47321f4335d113a3f/radam/radam.py#L156
    
    https://github.com/tensorflow/swift-apis/blob/f51ee4618d652a2419e998bf9418ad80bda67454/Sources/TensorFlow/Optimizers/MomentumBased.swift#L638
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58968
    
    Reviewed By: gchanan
    
    Differential Revision: D29241736
    
    Pulled By: iramazanli
    
    fbshipit-source-id: 288b9b1f3125fdc6c7a7bb23fde1ea5c201c0448

--- torch/optim/__init__.pyi
+++ torch/optim/__init__.pyi
@@ -8,6 +8,7 @@ from .adamw import AdamW as AdamW
 from .asgd import ASGD as ASGD
 from .lbfgs import LBFGS as LBFGS
 from .optimizer import Optimizer as Optimizer
+from .radam import RAdam as RAdam
 from .rmsprop import RMSprop as RMSprop
 from .rprop import Rprop as Rprop
 from .sgd import SGD as SGD
