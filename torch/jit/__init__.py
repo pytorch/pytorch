156119269d1e2548650aefac0fe481b6c052a719
lezcano <lezcano-93@hotmail.com>
2021-06-23 14:58:53 +0000

    Fix plurals

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -83,7 +83,7 @@ def annotate(the_type, the_value):
     Though TorchScript can infer correct type for most Python expressions, there are some cases where
     type inference can be wrong, including:
 
-    - Empty containers like `[]` and `{}`, which TorchScript assumes to be container of `Tensor`\ s
+    - Empty containers like `[]` and `{}`, which TorchScript assumes to be container of `Tensor`
     - Optional types like `Optional[T]` but assigned a valid value of type `T`, TorchScript would assume
       it is type `T` rather than `Optional[T]`
 
