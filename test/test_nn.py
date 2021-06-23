e7877ec6bdfccd68287d6217ce0a4ae5eb14bd93
Thomas J. Fan <thomasjpfan@gmail.com>
2021-06-22 23:27:46 -0400

    TST Removes unneeded tests

--- test/test_nn.py
+++ test/test_nn.py
@@ -15979,20 +15979,6 @@ class TestNNDeviceType(NNTestCase):
             with self.assertRaisesRegex(RuntimeError, msg):
                 F.nll_loss(x, t, weight=weight)
 
-    def test_nll_loss_zero_weights(self, device):
-        x = torch.randn((10, 3), device=device)
-        t = torch.empty(10, dtype=torch.int64, device=device).random_(0, 3)
-        weight = torch.zeros(3, device=device)
-        for reduction in ['mean', 'sum']:
-            result = F.nll_loss(x, t, weight=weight, reduction=reduction)
-            self.assertAlmostEqual(result, 0.0)
-
-    def test_nll_loss_zero_weights_empty_with_mean(self, device):
-        x = torch.randn((0, 3), device=device)
-        t = torch.empty(0, dtype=torch.int64, device=device).random_(0, 3)
-        result = F.nll_loss(x, t, reduction='mean')
-        self.assertTrue(torch.isnan(result))
-
     def _nll_loss_helper(self, input_size, reduction, expected, device):
         input = torch.rand(input_size, requires_grad=True, device=device)
         num_channels = input_size[1]
