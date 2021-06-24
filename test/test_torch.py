bb7409c0439133136f7c52fb2ef37200751e6967
Victor Bittorf <vsb@fb.com>
2021-06-24 10:32:52 -0700

    Add CUDA Vital (#58059)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58059
    
    Add CUDA.used vital sign which is true only if CUDA was "used" which technically means the context was created.
    
    Also adds the following features:
    - Force vitals to be written even if vitals are disabled, to enable testing when the env variable is not set from the start of execution
    - Add a read_vitals call for python to read existing vital signs.
    
    Test Plan: buck test mode/dbg caffe2/test:torch -- --regex basic_vitals
    
    Reviewed By: xuzhao9
    
    Differential Revision: D28357615
    
    fbshipit-source-id: 78a8755801b11ccc7e33a9eac669ebb7a83ddb87

--- test/test_torch.py
+++ test/test_torch.py
@@ -2919,21 +2919,27 @@ def torch_vital_set(value):
 
 
 # Tests Vital Signs for Torch
-class TestVitalSigns(TestCase):
+class TestBasicVitalSigns(TestCase):
     def test_basic_vitals(self):
         with torch_vital_set(''):
             self.assertFalse(torch.vitals_enabled())
         with torch_vital_set('ON'):
             self.assertTrue(torch.vitals_enabled())
 
-    def test_write_vital(self):
+    def test_basic_vitals_read_write(self):
         with torch_vital_set('ON'):
             self.assertTrue(torch.vitals_enabled())
             # This tests the code path of setting a vital
-            self.assertTrue(torch.set_vital('Dataloader', 'basic_unit_test', 'TEST'))
-            # Ideally we would have a read test for vitals, though because the the C++ design
-            # pattern of loggers we use, we can't know the whole list of vitals until the
-            # global C++ namespace is destructed.
+            self.assertTrue(torch.set_vital('Dataloader', 'basic_unit_test', 'TEST_VALUE_STRING'))
+            self.assertIn('TEST_VALUE_STRING', torch.read_vitals())
+            self.assertIn('CUDA.used', torch.read_vitals())
+
+
+class TestVitalSignsCuda(TestCase):
+    @onlyCUDA
+    def test_cuda_vitals_gpu_only(self, device):
+        with torch_vital_set('ON'):
+            self.assertIn('CUDA.used\t\t true', torch.read_vitals())
 
 
 # Device-generic tests. Instantiated below and not run directly.
@@ -8135,6 +8141,7 @@ class TestTensorDeviceOps(TestCase):
 # pytest will fail.
 add_neg_dim_tests()
 instantiate_device_type_tests(TestViewOps, globals())
+instantiate_device_type_tests(TestVitalSignsCuda, globals())
 instantiate_device_type_tests(TestTensorDeviceOps, globals())
 instantiate_device_type_tests(TestTorchDeviceType, globals())
 instantiate_device_type_tests(TestDevicePrecision, globals(), except_for='cpu')
