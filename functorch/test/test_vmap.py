ec25616e0c6e0a67474f51aecf97fa7e8a0fa99e
Horace He <horacehe2007@yahoo.com>
2021-06-24 23:50:27 -0700

    [functorch] Added maximum/minimum/clamp batching rules

--- functorch/test/test_vmap.py
+++ functorch/test/test_vmap.py
@@ -540,7 +540,7 @@ class TestVmapAPI(TestCase):
         # NB: One day we will implement a batching rule for torch.atan2.
         # If/when we do, this test should be replaced to test the fallback
         # path on another operator to avoid bitrot.
-        op = torch.maximum
+        op = torch.copysign
         x = torch.randn(11)
         y = torch.randn(11)
         with warnings.catch_warnings(record=True) as wa:
@@ -575,7 +575,7 @@ class TestVmapAPI(TestCase):
         # NB: One day we will implement a batching rule for torch.atan2.
         # If/when we do, this test should be replaced to test the fallback
         # path on another operator to avoid bitrot.
-        op = torch.maximum
+        op = torch.copysign
         x = torch.randn(11)
         y = torch.randn(11)
         self._assert_uses_vmap_fallback((op,), (x, y))
@@ -606,7 +606,7 @@ class TestVmapAPI(TestCase):
         # NB: One day we will implement a batching rule for torch.atan2.
         # If/when we do, this test should be replaced to test the fallback
         # path on another operator to avoid bitrot.
-        op = torch.maximum
+        op = torch.copysign
 
         x = torch.randn(5, 7, 11)
         y = torch.randn(5, 7, 11)
