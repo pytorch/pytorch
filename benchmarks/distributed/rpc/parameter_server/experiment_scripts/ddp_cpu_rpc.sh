1c57b99ff8e370431c6a61df00fabab650f6f4e8
garrett <gcramer@fb.com>
2021-06-25 19:55:58 +0000

    add_experiment_scripts
    [ghstack-poisoned]

---
+++ benchmarks/distributed/rpc/parameter_server/experiment_scripts/ddp_cpu_rpc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+cd ..
+
+python -u launcher.py \
+    --master_addr="localhost" \
+    --master_port="29500" \
+    --trainer="DdpTrainer" \
+    --ntrainer=2 \
+    --ncudatrainer=0 \
+    --filestore="/tmp/tmpn_k_8so02" \
+    --server="AverageParameterServer" \
+    --nserver=1 \
+    --ncudaserver=0 \
+    --rpc_timeout=120 \
+    --backend="gloo" \
+    --epochs=1 \
+    --batch_size=32 \
+    --data="DummyData" \
+    --model="DummyModelSparse" \
+    --data_config_path="configurations/data_configurations.json" \
+    --model_config_path="configurations/model_configurations.json" \
+    --preprocess_data="preprocess_dummy_data" \
+    --create_criterion="cel" \
+    --create_ddp_model="basic_ddp_model" \
+    --create_optimizer="sgd_optimizer" \
+    --iteration_step="basic_iteration_step" \
+    --hook_state="BasicHookState" \
+    --ddp_hook="rpc_hook" \
+    --lr=1e-4
