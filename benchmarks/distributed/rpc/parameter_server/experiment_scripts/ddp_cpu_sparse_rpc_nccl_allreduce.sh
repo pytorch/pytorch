0079fa34db494c08a16011bff1b44372c53871db
Garrett Cramer <gcramer@fb.com>
2021-06-24 17:20:33 -0700

    ps sparse rpc (#58003)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58003
    
    adds trainer class DdpTrainer
    adds trainer class DdpSparseRpcTrainer
    adds server class ParameterServerBase
    adds server class AverageParameterServer
    adds experiment ddp_cpu_sparse_rpc_nccl_allreduce
    adds experiment ddp_cuda_sparse_rpc_nccl_allreduce
    
    quip document https://fb.quip.com/iQUtAeKIxWpF
    
    Test Plan: Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D29379696
    
    Pulled By: gcramer23
    
    fbshipit-source-id: 9cf5fb7398ba2fa3eb694afbddc4ed00d97f205f

--- benchmarks/distributed/rpc/parameter_server/experiment_scripts/ddp_cpu_sparse_rpc_nccl_allreduce.sh
+++ benchmarks/distributed/rpc/parameter_server/experiment_scripts/ddp_cpu_sparse_rpc_nccl_allreduce.sh
@@ -6,17 +6,18 @@ cd ..
 python -u launcher.py \
     --master_addr="localhost" \
     --master_port="29500" \
-    --trainer="DdpTrainer" \
+    --trainer="DdpSparseRpcTrainer" \
     --ntrainer=2 \
     --ncudatrainer=0 \
     --filestore="/tmp/tmpn_k_8so02" \
-    --nserver=0 \
+    --server="AverageParameterServer" \
+    --nserver=1 \
     --ncudaserver=0 \
     --rpc_timeout=30 \
     --backend="nccl" \
     --epochs=10 \
     --batch_size=10 \
     --data="DummyData" \
-    --model="DummyModel" \
+    --model="DummyModelSparse" \
     --data_config_path="configurations/data_configurations.json" \
     --model_config_path="configurations/model_configurations.json"
