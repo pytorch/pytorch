664bcfbd212695f69523a5663ded6db040b045d4
Jay Zhang <36183870+fatcat-z@users.noreply.github.com>
2021-06-25 09:33:27 +0800

    [ONNX] Fix the issue of converting empty list to sequence. (#58651)
    When we construct an empty list by python list comprehension, we need to avoid converting the node without inputs to onnx::Concat in shape_type_inference.cpp and peephole.cpp because it will create an invalid Concat node which doesn't have inputs.
    
    In addition, update the code to avoid passing a Sequence input to an onnx::Cast node which doesn't accept Sequence data type as an input.
    
    Add tests for the validation as well.
    
    Co-authored-by: fatcat-z <jiz@microsoft.com>

--- torch/onnx/symbolic_helper.py
+++ torch/onnx/symbolic_helper.py
@@ -212,8 +212,23 @@ def _is_constant(value):
 def _is_tensor(x):
     return x.type().isSubtypeOf(torch._C.TensorType.get())
 
+def _is_list(x):
+    return isinstance(x.type(), torch._C.ListType)
+
 def _is_tensor_list(x):
-    return isinstance(x.type(), torch._C.ListType) and isinstance(x.type().getElementType(), torch._C.TensorType)
+    return _is_list(x) and isinstance(x.type().getElementType(), torch._C.TensorType)
+
+def _is_scalar_list(x):
+    """
+    Check if x is a scalar list, for example: List[float], List[int].
+
+    Besides checking the type is ListType, we also check if the data type is
+    a valid ONNX data type.
+    """
+    element_type = str(x.type().getElementType())
+    return _is_list(x) and \
+        element_type in scalar_name_to_pytorch.keys() and \
+        (scalar_name_to_pytorch[element_type] in cast_pytorch_to_onnx.keys())
 
 def _get_tensor_rank(x):
     if not _is_tensor(x) or x.type() is None:
@@ -745,7 +760,6 @@ def _optional_input_placeholder_tensor(g):
     return n
 
 def _handle_reduce_dim_none(g, self, op_name):
-    dim_size = _get_tensor_dim_size(self, 0)
     rank = _get_tensor_rank(self)
     if rank is not None and any([_get_tensor_dim_size(self, i) == 0 for i in range(rank)]):
         # If input tensor is empty, according to ONNX ReduceSum definition,
