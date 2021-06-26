6916cc5d5b5da3c357ef44f8bc4c26b685861125
Horace He <horacehe2007@yahoo.com>
2021-06-26 16:25:03 -0700

    [functorch] Added full_like and refactored factory macros a bit

--- functorch/functorch/csrc/BatchRulesFactory.cpp
+++ functorch/functorch/csrc/BatchRulesFactory.cpp
@@ -8,18 +8,18 @@
 
 namespace at { namespace functorch {
 
-using FactoryType = Tensor (*)(const Tensor&, optional<ScalarType>, optional<Layout>, optional<Device>, optional<bool>, optional<MemoryFormat>);
-using RandIntType = Tensor (*)(const Tensor&, optional<ScalarType>, optional<Layout>, optional<Device>, optional<bool>, optional<MemoryFormat>);
 
 #define SINGLE_ARG(...) __VA_ARGS__
-#define FACTORY_BATCH_RULE(op) SINGLE_ARG(basic_unary_batch_rule<FactoryType, &op, optional<ScalarType>, optional<Layout>, optional<Device>, optional<bool>, optional<MemoryFormat>>)
+#define FACTORY_TYPES optional<ScalarType>, optional<Layout>, optional<Device>, optional<bool>, optional<MemoryFormat>
+#define FACTORY_BATCH_RULE(op) SINGLE_ARG(basic_unary_batch_rule<decltype(&ATEN_FN(op)), &at::op, FACTORY_TYPES>)
 
 TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
-  VMAP_SUPPORT("ones_like", FACTORY_BATCH_RULE(at::ones_like));
-  VMAP_SUPPORT("zeros_like", FACTORY_BATCH_RULE(at::zeros_like));
-  VMAP_SUPPORT("empty_like", FACTORY_BATCH_RULE(at::empty_like));
-  VMAP_SUPPORT("randn_like", FACTORY_BATCH_RULE(at::randn_like));
-  VMAP_SUPPORT("rand_like", FACTORY_BATCH_RULE(at::rand_like));
+  VMAP_SUPPORT("ones_like", FACTORY_BATCH_RULE(ones_like));
+  VMAP_SUPPORT("zeros_like", FACTORY_BATCH_RULE(zeros_like));
+  VMAP_SUPPORT("empty_like", FACTORY_BATCH_RULE(empty_like));
+  VMAP_SUPPORT("randn_like", FACTORY_BATCH_RULE(randn_like));
+  VMAP_SUPPORT("rand_like", FACTORY_BATCH_RULE(rand_like));
+  VMAP_SUPPORT("full_like", SINGLE_ARG(basic_unary_batch_rule<decltype(&ATEN_FN(full_like)), &at::full_like, const Scalar&, FACTORY_TYPES>));
   // Not sure how to add the ones with irregular args to the mix cleanly (i.e. randint takes an extra int parameter)
 }
 }}
