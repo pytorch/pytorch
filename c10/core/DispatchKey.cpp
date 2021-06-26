f82186a4bb8012cb7de7c2906beae066bb456265
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared14930.06.frc2.facebook.com>
2021-06-26 00:02:35 -0700

    2021-06-26 nightly release (ad69e2fd1120ac37f826c09ee2f1654f1d5c80b2)

--- c10/core/DispatchKey.cpp
+++ c10/core/DispatchKey.cpp
@@ -61,6 +61,9 @@ const char* toString(DispatchKey t) {
     case DispatchKey::NestedTensor:
       return "NestedTensor";
 
+    case DispatchKey::Python:
+      return "Python";
+
     case DispatchKey::PrivateUse1:
       return "PrivateUse1";
     case DispatchKey::PrivateUse2:
