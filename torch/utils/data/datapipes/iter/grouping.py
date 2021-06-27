203d0de6d2c2f1f59110e684bfe82762f18568b3
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-06-27 14:36:39 -0400

    Update on "[DataLoader] Make `batch` DataPipe sensitive to unbatch_level argument"
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/grouping.py
+++ torch/utils/data/datapipes/iter/grouping.py
@@ -19,7 +19,7 @@ class BatchIterDataPipe(IterDataPipe[List[T_co]]):
         datapipe: Iterable DataPipe being batched
         batch_size: The size of each batch
         drop_last: Option to drop the last batch if it's not full
-        unbatch_level: Specifies if it necessary to unbatch source data before 
+        unbatch_level: Specifies if it necessary to unbatch source data before
             applying new batching rule
     """
     datapipe: IterDataPipe[T_co]
