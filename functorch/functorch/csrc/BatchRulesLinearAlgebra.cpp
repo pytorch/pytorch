d236bee3722874272e4ab27f334beeaec64f4ca1
Richard Zou <zou3519@users.noreply.github.com>
2021-06-28 16:08:36 -0400

    [functorch] Selectively enable dispatch on kBatchedKey (pytorch/functorch#63)
    This PR makes it so that dispatch on kBatchedKey only can happen if there are
    tensors batched at the current level. Otherwise, kBatchedKey is excluded
    (even if there are BatchedTensors!).
    
    To find tensors batched at the current level, we check:
    - all tensor arguments
    - we peek into all TensorLists
    - we peek into all Tensor?[].
    the above bullet points should be sufficient.
    
    Dispatch for kVmapModeKey is not affected.
    
    Test Plan:
    - run all tests
    - removed the special case in dot_batch_rule and added a test

--- functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
+++ functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
@@ -40,8 +40,6 @@ std::tuple<Tensor, optional<int64_t>> dot_batch_rule(const Tensor& A, optional<i
   auto B_ = moveBatchDimToFront(B, B_bdim);
   if (A_bdim && B_bdim) {
     return std::make_tuple(at::matmul(A_.unsqueeze(-2), B_.unsqueeze(-1)).squeeze(-1).squeeze(-1), 0);
-  } else if (!A_bdim && !B_bdim) {
-    return std::make_tuple(at::dot(A_, B_), nullopt);
   } else {
     return std::make_tuple(at::matmul(A_, B_.t()), 0);
   }
