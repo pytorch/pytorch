81b8bf3a11ffbcd8167ba8805c9b7d452369a36f
Zafar <cc.rafaz@zafar.cc>
2021-06-28 02:51:44 -0700

    Update base for Update on "[sparsity] WeightNormSparsifier"
    Differential Revision: [D28970960](https://our.internmc.facebook.com/intern/diff/D28970960)
    
    [ghstack-poisoned]

--- torch/ao/nn/sparse/quantized/dynamic/linear.py
+++ torch/ao/nn/sparse/quantized/dynamic/linear.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from torch.ao.nn.sparse.quantized import linear
-from torch.ao.nn.sparse.quantized.utils import QNNPACKLinearBlockSparsePattern
+from torch.ao.nn.sparse.quantized.utils import LinearBlockSparsePattern
 
 import torch
 import torch.nn.intrinsic as nni
@@ -126,9 +126,7 @@ class Linear(torch.nn.Module):
             assert w_zp == 0, 'Weight zero point must map to 0'
         qweight = _quantize_weight(weight.float(), weight_observer)
 
-        # Use these default values until we figure out how to augment
-        # `mod` to contain sparse config
-        row_block_size, col_block_size = QNNPACKLinearBlockSparsePattern.block_size()
+        row_block_size, col_block_size = LinearBlockSparsePattern.block_size()
         qlinear = cls(mod.in_features,
                       mod.out_features,
                       row_block_size,
