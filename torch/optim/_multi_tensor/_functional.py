a6a2c11f4198c5ee6999d66c4197527bd5bffd9a
ilqar ramazanli <iramazanli@fb.com>
2021-06-29 14:05:06 +0000

    To refactor Multi Tensor AdamW to Functional API
    [ghstack-poisoned]

--- torch/optim/_multi_tensor/_functional.py
+++ torch/optim/_multi_tensor/_functional.py
@@ -252,3 +252,50 @@ def adam(params: List[Tensor],
 
     step_size = [(lr / bc) * -1 for bc in bias_correction1]
     torch._foreach_addcdiv_(params, exp_avg, denom, step_size)
+
+
+def adamw(params: List[Tensor],
+          grads: List[Tensor],
+          exp_avg: List[Tensor],
+          exp_avg_sq: List[Tensor],
+          max_exp_avg_sq: List[Tensor],
+          states: List[Dict],
+          *,
+          amsgrad: bool,
+          beta1: float,
+          beta2: float,
+          lr: float,
+          eps: float):
+    r"""Functional API that performs Adamw algorithm computation.
+    See :class:`~torch.optim.Adamw` for details.
+    """
+
+    bias_correction1 = [1 - beta1 ** state['step'] for state in states]
+    bias_correction2 = [1 - beta2 ** state['step'] for state in states]
+
+    #
+    # Decay the first and second moment running average coefficient
+    #
+    torch._foreach_mul_(exp_avg, beta1)
+    torch._foreach_add_(exp_avg, grads, alpha=1 - beta1)
+
+    torch._foreach_mul_(exp_avg_sq, beta2)
+    torch._foreach_addcmul_(exp_avg_sq, grads, grads, 1 - beta2)
+
+    if amsgrad:
+        # Maintains the maximum of all 2nd moment running avg. till now
+        max_exp_avg_sq = torch._foreach_maximum(max_exp_avg_sq, exp_avg_sq)  # type: ignore[assignment]
+
+        # Use the max. for normalizing running avg. of gradient
+        max_exp_avg_sq_sqrt = torch._foreach_sqrt(max_exp_avg_sq)
+        bias_correction_sqrt = [math.sqrt(bc) for bc in bias_correction2]
+        torch._foreach_div_(max_exp_avg_sq_sqrt, bias_correction_sqrt)
+        denom = torch._foreach_add(max_exp_avg_sq_sqrt, eps)
+    else:
+        exp_avg_sq_sqrt = torch._foreach_sqrt(exp_avg_sq)
+        bias_correction_sqrt = [math.sqrt(bc) for bc in bias_correction2]
+        torch._foreach_div_(exp_avg_sq_sqrt, bias_correction_sqrt)
+        denom = torch._foreach_add(exp_avg_sq_sqrt, eps)
+
+    step_size = [-1 * (lr / bc) for bc in bias_correction1]
+    torch._foreach_addcdiv_(params, exp_avg, denom, step_size)
