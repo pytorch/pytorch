1c7c258544c5c6cc54c74581a10448cfbaeec624
Sam Estep <sestep@fb.com>
2021-06-29 11:56:51 -0700

    Run multigpu on linux.16xlarge.nvidia.gpu

--- .github/scripts/generate_pytorch_test_matrix.py
+++ .github/scripts/generate_pytorch_test_matrix.py
@@ -9,29 +9,41 @@ dictated by just sharding.
 
 import json
 import os
+from typing import Dict
+
+from typing_extensions import TypedDict
+
+
+class Config(TypedDict):
+    num_shards: int
+    runner: str
 
 
 def main() -> None:
+    TEST_RUNNER_TYPE = os.getenv('TEST_RUNNER_TYPE')
     NUM_TEST_SHARDS = int(os.getenv('NUM_TEST_SHARDS', '1'))
-    shards_per_config = {}
-    if os.getenv('ENABLE_MULTIGPU_TEST'):
-        shards_per_config['multigpu'] = 1
+    MULTIGPU_RUNNER_TYPE = os.getenv('MULTIGPU_RUNNER_TYPE')
+    configs: Dict[str, Config] = {}
+    if MULTIGPU_RUNNER_TYPE is not None and os.getenv('ENABLE_MULTIGPU_TEST'):
+        configs['multigpu'] = {'num_shards': 1, 'runner': MULTIGPU_RUNNER_TYPE}
     matrix = {
         'include': [
             {
                 'config': 'default',
                 'shard': shard,
                 'num_shards': NUM_TEST_SHARDS,
+                'runner': TEST_RUNNER_TYPE,
             }
             for shard in range(1, NUM_TEST_SHARDS + 1)
         ] + [
             {
-                'config': config,
+                'config': name,
                 'shard': shard,
-                'num_shards': num_shards,
+                'num_shards': config['num_shards'],
+                'runner': config['runner'],
             }
-            for config, num_shards in shards_per_config.items()
-            for shard in range(1, num_shards + 1)
+            for name, config in configs.items()
+            for shard in range(1, config['num_shards'] + 1)
         ]
     }
     render_matrix = {'config': list(dict.fromkeys(x['config'] for x in matrix['include']))}
