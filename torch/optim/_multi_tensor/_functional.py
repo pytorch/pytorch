e84c666a2a42c541aab8e89f0177ebe3bb303aba
ilqar ramazanli <iramazanli@fb.com>
2021-06-30 13:26:28 +0000

    Update on "To refactor multi tensor SGD to functional API"
    [ghstack-poisoned]

--- torch/optim/_multi_tensor/_functional.py
+++ torch/optim/_multi_tensor/_functional.py
@@ -305,12 +305,12 @@ def sgd(params: List[Tensor],
         grads: List[Tensor],
         states: List[Dict],
         *,
-        momentum : float,
         has_sparse_grad: bool,
-        dampening: float,
-        nesterov: bool,
         weight_decay: float,
-        lr: float):
+        momentum : float,
+        lr: float,
+        dampening: float,
+        nesterov: bool):
     r"""Functional API that performs SGD algorithm computation.
     See :class:`~torch.optim.SGD` for details.
     """
