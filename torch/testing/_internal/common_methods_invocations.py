cc1e4fd4c5b0ce5df179d70fb34d6bb8fe4aca91
Saketh Are <saketh@fb.com>
2021-06-30 20:04:06 +0000

    use supports_out flag

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -7674,9 +7674,8 @@ op_db: List[OpInfo] = [
     OpInfo('attn',
            dtypes=floating_types_and(torch.bfloat16),
            sample_inputs_func=sample_inputs_attn,
-           skips=(
-               SkipInfo('TestCommon', 'test_out'),
-           )),
+           supports_out=False,
+           ),
 ]
 
 # Common operator groupings
