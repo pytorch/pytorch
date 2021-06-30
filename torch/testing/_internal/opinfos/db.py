f88d82f15695e42032fbbcbbbd8f5b41b89bfa34
Antonio Cuni <anto.cuni@gmail.com>
2021-06-30 14:16:00 +0000

    this got lost in the merge

--- torch/testing/_internal/opinfos/db.py
+++ torch/testing/_internal/opinfos/db.py
@@ -2128,6 +2128,22 @@ def sample_inputs_std_var(op_info, device, dtype, requires_grad, **kwargs):
     ]
 
 
+def sample_inputs_cov(op_info, device, dtype, requires_grad, **kwargs):
+    shapes = [(2,), (1, 2), (3, 2), (2, 3)]
+
+    inputs = []
+    for shape in shapes:
+        t = make_tensor(shape, device, dtype, requires_grad=requires_grad)
+        inputs.append(SampleInput(t))
+        num_observations = t.numel() if t.ndimension() < 2 else t.size(1)
+        fweights = make_tensor((num_observations,), device, torch.int, low=0, high=10, requires_grad=requires_grad)
+        aweights = make_tensor((num_observations,), device, torch.float, low=0, high=1, requires_grad=requires_grad)
+        for correction, fw, aw in product(range(num_observations), [None, fweights], [None, aweights]):
+            inputs.append(SampleInput(t, kwargs={'correction': correction, 'fweights': fw, 'aweights': aw}))
+
+    return inputs
+
+
 def _sample_inputs_svd(op_info, device, dtype, requires_grad=False, is_linalg_svd=False):
     """
     This function generates input for torch.svd with distinct singular values so that autograd is always stable.
