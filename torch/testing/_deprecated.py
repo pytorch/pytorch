4939d120ffe4ad08d2de64bd7963d5baee9fec55
Philip Meier <github.pmeier@posteo.de>
2021-06-30 11:49:03 +0200

    deprecate rather than remove randn?

---
+++ torch/testing/_deprecated.py
@@ -0,0 +1,31 @@
+"""This module exists since the `torch.testing` exposed a lot of stuff that shouldn't have been public. Although this
+was never documented anywhere, some other internal FB projects as well as downstream OSS projects might use this. Thus,
+they sill go through a deprecation cycle before we internalize them.
+"""
+
+import functools
+import warnings
+from typing import Any, Callable, Optional
+
+import torch
+
+
+__all__ = ["rand", "randn"]
+
+
+def warn_deprecated(fn: Callable, instructions: str, name: Optional[str] = None) -> Callable:
+    if name is None:
+        name = fn.__name__
+
+    msg = f"torch.testing.{name} is deprecated and will be removed in the future. {instructions.strip()}"
+
+    @functools.wraps(fn)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        warnings.warn(msg, FutureWarning)
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+
+rand = warn_deprecated(torch.rand, "Use torch.rand instead.")
+randn = warn_deprecated(torch.randn, "Use torch.randn instead.")
