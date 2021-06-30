977ae7bd88239775a88eb7e8898b713c0ca7a388
imaginary-person <76181208+imaginary-person@users.noreply.github.com>
2021-06-30 00:42:11 -0500

    Merge clauses in intrinsics.h
    [skip-ci] [skip ci]
    
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>

--- aten/src/ATen/cpu/vec/vec512/intrinsics.h
+++ aten/src/ATen/cpu/vec/vec512/intrinsics.h
@@ -1,11 +1,8 @@
 #pragma once
-#if defined(__clang__) && (defined(__x86_64__) || defined(__i386__))
-/* Clang-compatible compiler, targeting x86/x86-64 */
+#if (defined(__clang__) || defined(__GNUC__)) && (defined(__x86_64__) || defined(__i386__))
+/* GCC or Clang, targeting x86/x86-64 */
 #include <x86intrin.h>
 #elif defined(_MSC_VER)
 /* Microsoft C/C++-compatible compiler */
 #include <intrin.h>
-#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
-/* GCC-compatible compiler, targeting x86/x86-64 */
-#include <x86intrin.h>
 #endif
