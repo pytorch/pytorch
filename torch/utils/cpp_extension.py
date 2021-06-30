a3ff4090a6361df20a06fb4a85dbda29d7e0730f
Rong Rong <rongr@fb.com>
2021-06-30 11:24:19 -0700

    let's see if it passes mypy

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -772,7 +772,7 @@ class BuildExtension(build_ext, object):
             if cuda_version is not None:
                 cuda_str_version = cuda_version.group(1)
                 cuda_ver = packaging.version.parse(cuda_str_version)
-                torch_cuda_version = packaging.version.parse(torch.version.cuda)  # type: ignore[arg-type]
+                torch_cuda_version = packaging.version.parse(torch.version.cuda)
                 if cuda_ver.major != torch_cuda_version.major:  # type: ignore[attr-defined]
                     raise RuntimeError(CUDA_MISMATCH_MESSAGE.format(
                         cuda_str_version, torch.version.cuda))
