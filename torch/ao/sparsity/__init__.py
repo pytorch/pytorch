ca2702a776541e81945c5410984ba07b2a71ca96
Karen Zhou <kazhou@fb.com>
2021-07-01 14:48:36 -0700

    [pruner] Make bias hook stateless (#61077)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61077
    
    Removing `BiasHook` class, using function instead.
    ghstack-source-id: 132899223
    
    Test Plan:
    ` buck test mode/dev-nosan //caffe2/test:ao -- TestBasePruner`
    
    https://pxl.cl/1L7Tg
    
    Reviewed By: z-a-f
    
    Differential Revision: D29504119
    
    fbshipit-source-id: 6dd9689d18b17ac64e8a461f466e2c9018bc530b

--- torch/ao/sparsity/__init__.py
+++ torch/ao/sparsity/__init__.py
@@ -1,7 +1,6 @@
 # Parametrizations
 from .experimental.pruner.parametrization import PruningParametrization
 from .experimental.pruner.parametrization import ActivationReconstruction
-from .experimental.pruner.parametrization import BiasHook
 
 # Pruner
 from .experimental.pruner.base_pruner import BasePruner
