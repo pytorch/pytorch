52d683bba6beb7b99fa03ee19d8b4c8a419c92ab
ilqar ramazanli <iramazanli@fb.com>
2021-07-03 00:17:06 +0000

    Update on "First one"
    [ghstack-poisoned]

--- test/test_optim.py
+++ test/test_optim.py
@@ -483,7 +483,7 @@ class TestOptim(TestCase):
             )
             with self.assertRaisesRegex(ValueError, "Invalid rho value: 1.1"):
                 optimizer(None, lr=1e-2, rho=1.1)
-
+55555555
 absa
 
     def test_nadam(self):
