cd881df6deacfb0b981f2445ae6c6117643d9ba1
Ansley Ussery <ansley@fb.com>
2021-07-06 08:05:31 -0700

    Update base for Update on "Add type refinement for Union"
    Differential Revision: [D28572811](https://our.internmc.facebook.com/intern/diff/D28572811)
    
    [ghstack-poisoned]

--- torch/csrc/jit/frontend/ir_emitter.cpp
+++ torch/csrc/jit/frontend/ir_emitter.cpp
@@ -1015,23 +1015,7 @@ struct to_ir {
   void emitReturn(const Return& stmt) {
     TypePtr declared_return_type =
         def_stack_.back().declared_return_type_; // nullptr if not annotated
-    TypePtr type_hint = nullptr;
-
-    // If 1) we have a return statement in one or more branches, and 2)
-    // the type of the expression returned is a subtype of the declared
-    // return type for all branches, then we should use the declared
-    // return type as the `type_hint` when we emit each return
-    // statement. If we don't do this, we'll get an error like "x is set
-    // to type T1 in the true branch and type T2 in the false branch"--
-    // even if `unifyTypes(T1, T2)->isSubtypeOf(declared_return_type)`
-    if (declared_return_type &&
-        (declared_return_type == AnyType::get() ||
-         declared_return_type->kind() == UnionType::Kind ||
-         declared_return_type->kind() == OptionalType::Kind)) {
-      type_hint = declared_return_type;
-    }
-
-    Value* actual_return = emitExpr(stmt.expr(), type_hint);
+    Value* actual_return = emitExpr(stmt.expr(), declared_return_type);
 
     // result type is annotated, every return must convert to that type
     if (declared_return_type) {
