15bfa2cc4552f6647bf11c0343507191ed51f882
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-07-06 19:44:48 -0400

    Update on "Add DataPipes Graph Functions"
    [ghstack-poisoned]

--- test/test_datapipe.py
+++ test/test_datapipe.py
@@ -40,6 +40,17 @@ except ImportError:
     HAS_TORCHVISION = False
 skipIfNoTorchVision = skipIf(not HAS_TORCHVISION, "no torchvision")
 
+try:
+    import dill
+    # XXX: By default, dill writes the Pickler dispatch table to inject its
+    # own logic there. This globally affects the behavior of the standard library
+    # pickler for any user who transitively depends on this module!
+    # Undo this extension to avoid altering the behavior of the pickler globally.
+    dill.extend(use_dill=False)
+    HAS_DILL = True
+except ImportError:
+    HAS_DILL = False
+skipIfNoDill = skipIf(not HAS_DILL, "no dill")
 
 T_co = TypeVar('T_co', covariant=True)
 
@@ -1266,6 +1277,7 @@ class NumbersDataset(IterDataPipe):
 
 
 class TestGraph(TestCase):
+    @skipIfNoDill
     def test_simple_traverse(self):
         numbers_dp = NumbersDataset(size=50)
         mapped_dp = numbers_dp.map(lambda x: x * 10)
@@ -1275,6 +1287,7 @@ class TestGraph(TestCase):
 
     # TODO(VitalyFedyunin): This test is incorrect because of 'buffer' nature
     # of the fork fake implementation, update fork first and fix this test too
+    @skipIfNoDill
     def test_traverse_forked(self):
         numbers_dp = NumbersDataset(size=50)
         dp0, dp1, dp2 = numbers_dp.fork(3)
