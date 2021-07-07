f890ca8f3d1c8be3596fa29b700a0b6894c6c731
guyang3532 <yangu@microsoft.com>
2021-07-07 18:44:26 +0800

    Exclude time of waiting in queue from gloo communication profiling

--- torch/csrc/distributed/c10d/ProcessGroupGloo.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupGloo.cpp
@@ -33,6 +33,7 @@
 #include <gloo/scatter.h>
 
 #include <ATen/SparseTensorUtils.h>
+#include <ATen/ThreadLocalState.h>
 
 #include <c10/util/StringUtil.h>
 #include <c10/util/intrusive_ptr.h>
@@ -405,6 +406,9 @@ const auto kLoopbackAddress = "127.0.0.1";
 
 // static
 void ProcessGroupGloo::AsyncWork::execute(c10::intrusive_ptr<AsyncWork> work) {
+  if(work->recordFunctionBeforeCallback_){
+    work->recordFunctionBeforeCallback_();
+  }
   try {
     work->run();
   } catch (...) {
@@ -465,9 +469,37 @@ ProcessGroupGloo::AsyncWork::AsyncWork(
     std::vector<std::vector<at::Tensor>> outputTensors,
     const char* profilingTitle,
     const c10::optional<std::vector<at::Tensor>>& inputTensors)
-    : ProcessGroup::Work(-1, OpType::UNKNOWN, profilingTitle, inputTensors),
+    // Specify nullptr to profilingTitle of Parent Class is to disable the profiling in Parent Class.
+    // It's because general ProcessGroup profiling is kicked off upon construction of Work and it's not
+    // reasonable for async work. We will add a new record function in this AsyncWork and delay the profiling
+    // start time to when the the work really executed, not constructed.
+    : ProcessGroup::Work(-1, OpType::UNKNOWN, nullptr, inputTensors),
       outputTensors_(std::move(outputTensors)),
-      future_(createFutureAsOutput(outputTensors)) {}
+      future_(createFutureAsOutput(outputTensors)) {
+  if (profilingTitle != nullptr) {
+    auto recordingFunction =
+        std::make_shared<at::RecordFunction>(at::RecordScope::USER_SCOPE);
+    if (recordingFunction->isActive()) {
+      std::function<void()> before_handler = [inputTensors, profilingTitle, recordingFunction]() {
+        // This Work will be start and completed by another thread.
+        recordingFunction->_setAsync();
+        std::vector<c10::IValue> inputs;
+        if (inputTensors) {
+          inputs.reserve(inputTensors->size());
+          for (const auto& tensor : *inputTensors) {
+            inputs.emplace_back(tensor);
+          }
+        }
+        recordingFunction->before(profilingTitle, inputs);
+      };
+      recordFunctionBeforeCallback_ = at::wrapPropagateTLSState(before_handler);
+      std::function<void()> end_handler = [recordingFunction]() {
+        recordingFunction->end();
+      };
+      recordFunctionEndCallback_ = at::wrapPropagateTLSState(end_handler);
+    }
+  }
+}
 
 void ProcessGroupGloo::AsyncWork::finishWorkGlooError(std::exception_ptr eptr) {
   future_->setError(eptr);
