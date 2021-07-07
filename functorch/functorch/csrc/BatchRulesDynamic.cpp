0c9f28d8d1e9ae951ab74038361638b5e6155b31
Horace He <horacehe2007@yahoo.com>
2021-07-07 13:01:53 -0700

    [functorch] Added errors for dynamic ops

---
+++ functorch/functorch/csrc/BatchRulesDynamic.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <ATen/ATen.h>
+#include <functorch/csrc/BatchRulesHelper.h>
+#include <functorch/csrc/BatchedFallback.h>
+#include <c10/util/Metaprogramming.h>
+
+
+namespace at { namespace functorch {
+
+
+void unsupportedDynamicOp(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+    TORCH_CHECK(false, "vmap: We do not support batching operators that can output dynamic shape.");
+}
+#define UNSUPPORTED_DYNAMIC(op) \
+    m.impl(#op, torch::CppFunction::makeFromBoxedFunction<&unsupportedDynamicOp>());
+
+TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
+    UNSUPPORTED_DYNAMIC(nonzero);
+    UNSUPPORTED_DYNAMIC(unique);
+}
+
+}}
