1680d034943ad6fc3e6c45bbc741643ddc7b58c8
Elton Pinto <eltonpinto@fb.com>
2021-07-07 13:47:20 -0700

    Write unittests

--- tools/linter/clang_tidy/max_tokens_pragma.py
+++ tools/linter/clang_tidy/max_tokens_pragma.py
@@ -8,45 +8,55 @@ MAX_TOKENS_CHECK_DIAG_NAME = "misc-max-tokens"
 MAX_TOKENS_PRAGMA_PATERN = r"#pragma\s+clang\s+max_tokens_total\s+(\d+)"
 
 
-def add_max_tokens_pragma(files: List[str], num_max_tokens: int) -> None:
+def add_max_tokens_pragma(code: str, num_max_tokens: int) -> str:
+    lines = code.split("\n")
+
+    found_pragma = False
+    pragma = f"#pragma clang max_tokens_total {num_max_tokens}"
+
+    for idx, line in enumerate(lines):
+        match = re.match(MAX_TOKENS_PRAGMA_PATERN, line.strip())
+        if match:
+            found_pragma = True
+            token_count = match.group(1)
+            if int(token_count) != num_max_tokens:
+                lines[idx] = pragma
+
+    if not found_pragma:
+        lines = [pragma] + lines
+
+    return "\n".join(lines)
+
+
+def strip_max_tokens_pragmas(code: str) -> str:
+    lines = code.split("\n")
+    lines = [
+        line
+        for line in lines
+        if re.match(MAX_TOKENS_PRAGMA_PATERN, line.strip()) is None
+    ]
+    return "\n".join(lines)
+
+
+def add_max_tokens_pragma_to_files(files: List[str], num_max_tokens: int) -> None:
     for filename in files:
         with open(filename, "r+") as f:
-            lines = f.readlines()
+            data = f.read()
+            data = add_max_tokens_pragma(data, num_max_tokens)
 
             f.seek(0)
-
-            found_pragma = False
-            pragma = f"#pragma clang max_tokens_total {num_max_tokens}\n"
-
-            for idx, line in enumerate(lines):
-                match = re.match(MAX_TOKENS_PRAGMA_PATERN, line)
-                if match:
-                    found_pragma = True
-                    token_count = match.group(1)
-                    if token_count != num_max_tokens:
-                        lines[idx] = pragma
-
-            if not found_pragma:
-                f.write(pragma)
-
-            for line in lines:
-                f.write(line)
-
+            f.write(data)
             f.truncate()
 
 
-def strip_max_tokens_pragma(files: List[str]) -> None:
+def strip_max_tokens_pragma_from_files(files: List[str]) -> None:
     for filename in files:
         with open(filename, "r+") as f:
-            lines = f.readlines()
-            lines = [
-                line
-                for line in lines
-                if re.match(MAX_TOKENS_PRAGMA_PATERN, line) is None
-            ]
+            data = f.read()
+            data = strip_max_tokens_pragmas(data)
+
             f.seek(0)
-            for line in lines:
-                f.write(line)
+            f.write(data)
             f.truncate()
 
 
@@ -64,7 +74,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "files", nargs="+", help="Add max_tokens_total pragmas to the specified files"
     )
-    parser.add_argument("-i", "--ignores", nargs="+", help="Ignore the specified files")
+    parser.add_argument(
+        "-i", "--ignores", nargs="+", default=[], help="Ignore the specified files"
+    )
     parser.add_argument(
         "-s",
         "--strip",
@@ -80,9 +92,9 @@ def main() -> None:
     ignores = set(options.ignores)
     files = [filename for filename in options.files if filename not in ignores]
     if options.strip:
-        strip_max_tokens_pragma(files)
+        strip_max_tokens_pragma_from_files(files)
     else:
-        add_max_tokens_pragma(files, options.num_max_tokens)
+        add_max_tokens_pragma_to_files(files, options.num_max_tokens)
 
 
 if __name__ == "__main__":
