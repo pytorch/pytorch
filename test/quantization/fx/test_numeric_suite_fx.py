794ea3687d89e534f01c4593d646c0bf35e8fd8f
vasiliy <vasiliy@fb.com>
2021-07-07 15:26:11 -0700

    Update on "ns for fx: add utils for l2 error and cosine similarity"
    Summary:
    
    Adds convenience wrappers for l2 error and cosine similarity
    to NS utils.
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestFXNumericSuiteCoreAPIs.test_extend_logger_results_with_comparison
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- test/quantization/fx/test_numeric_suite_fx.py
+++ test/quantization/fx/test_numeric_suite_fx.py
@@ -779,6 +779,13 @@ class FXNumericSuiteQuantizationTestCase(QuantizationTestCase):
                     len(results) == results_len,
                     f"expected len {results_len}, got len {len(results)}")
                 self.assert_ns_compare_dict_valid(results)
+                extend_logger_results_with_comparison(
+                    results, 'a', 'b', compute_sqnr, 'sqnr')
+                extend_logger_results_with_comparison(
+                    results, 'a', 'b', compute_l2_error, 'l2_error')
+                extend_logger_results_with_comparison(
+                    results, 'a', 'b', compute_cosine_similarity,
+                    'cosine_similarity')
 
     def _test_match_activations(
         self, m, data, prepared_expected_node_occurrence=None, results_len=0,
@@ -835,6 +842,13 @@ class FXNumericSuiteQuantizationTestCase(QuantizationTestCase):
                 len(act_compare_dict) == results_len,
                 f"expected len {results_len}, got len {len(act_compare_dict)}")
             self.assert_ns_compare_dict_valid(act_compare_dict)
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_l2_error, 'l2_error')
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_cosine_similarity,
+                'cosine_similarity')
             results.append(act_compare_dict)
         return results
 
@@ -891,6 +905,13 @@ class FXNumericSuiteQuantizationTestCase(QuantizationTestCase):
                     len(act_compare_dict) == results_len,
                     f"expected len {results_len}, got len {len(act_compare_dict)}")
             self.assert_ns_compare_dict_valid(act_compare_dict)
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_l2_error, 'l2_error')
+            extend_logger_results_with_comparison(
+                act_compare_dict, 'a', 'b', compute_cosine_similarity,
+                'cosine_similarity')
             results.append(act_compare_dict)
         return results
 
