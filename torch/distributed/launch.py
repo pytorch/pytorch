8c1be47a55b2b5b260aa8868afa8bdf06266b870
Aliaksandr Ivanou <aivanou@fb.com>
2021-07-08 15:56:08 -0700

    [torch] Various improvements to `torch.distributed.launch` and `torch.distributed.run` (#61294)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61294
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60925
    
    * Make `torch.distributed.launch` restarts to 0
    * Remove unnecessary `-use_env` warning, move `-use_env` warnings
    * Move `-use_env` warnings to `torch.distributed.launch`
    * Make default log level WARNING
    * Add new doc section around transitioning to `torch.distributed.run`
    * Make `torch.distributed.launch` not use error-propagation
    * Set default events handler to `null` that does not print events to console
    * Add reference from `torch.distributed.launch` to `torch.distributed.run`
    * Set correct preexec function that sends SIGTERM to child processes when parent dies
    
    Issues resolved:
    
    https://github.com/pytorch/pytorch/issues/60716
    https://github.com/pytorch/pytorch/issues/60754
    
    Test Plan:
    sandcastle
    
        python -m torch.distributed.launch --nproc_per_node 2 main.py -> uses 0 restarts
        python -m torch.distributed.run --nproc_per_node 2 main.py -> uses default for torchelastic, 0 restarts
    
        python -m torch.distributed.launch --nproc_per_node=4  --use_env --no_python  main.py -> produces error
        python -m torch.distributed.launch --nproc_per_node=4  --use_env main.py -> no warning
        python -m torch.distributed.launch --nproc_per_node=4  --no_python  main.py ->warning
    
    Output of running torch.distributed.launch without --use_env:
    
        $path/torch/distributed/launch.py:173: FutureWarning: The module torch.distributed.launch is deprecated
        and will be removed in future. Use torch.distributed.run.
        Note that --use_env is set by default in torch.distributed.run.
        If your script expects `--local_rank` argument to be set, please
        change it to read from `os.environ('LOCAL_RANK')` instead.
    
    New section:
    
    {F628923078}
    
    {F628974089}
    
    Reviewed By: cbalioglu
    
    Differential Revision: D29559553
    
    fbshipit-source-id: 03ed9ba638bf154354e1530ffc964688431edf6b

--- torch/distributed/launch.py
+++ torch/distributed/launch.py
@@ -1,8 +1,10 @@
 r"""
-`torch.distributed.launch` is a module that spawns up multiple distributed
+``torch.distributed.launch`` is a module that spawns up multiple distributed
 training processes on each of the training nodes.
 
-NOTE: This module is deprecated, use torch.distributed.run.
+.. warning::
+
+    This module is going to be deprecated in favor of :ref:`torch.distributed.run <launcher-api>`.
 
 The utility can be used for single-node distributed training, in which one or
 more processes per node will be spawned. The utility can be used for either
@@ -136,9 +138,12 @@ will not pass ``--local_rank`` when you specify this flag.
     https://github.com/pytorch/pytorch/issues/12042 for an example of
     how things can go wrong if you don't do this correctly.
 
+
+
 """
 
 import logging
+import warnings
 
 from torch.distributed.run import get_args_parser, run
 
@@ -159,14 +164,27 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
+def launch(args):
+    if args.no_python and not args.use_env:
+        raise ValueError(
+            "When using the '--no_python' flag,"
+            " you must also set the '--use_env' flag."
+        )
+    run(args)
+
+
 def main(args=None):
-    logger.warning(
-        "The module torch.distributed.launch is deprecated "
-        "and going to be removed in future."
-        "Migrate to torch.distributed.run"
+    warnings.warn(
+        "The module torch.distributed.launch is deprecated\n"
+        "and will be removed in future. Use torch.distributed.run.\n"
+        "Note that --use_env is set by default in torch.distributed.run.\n"
+        "If your script expects `--local_rank` argument to be set, please\n"
+        "change it to read from `os.environ['LOCAL_RANK']` instead. See \n"
+        "https://pytorch.org/docs/stable/distributed.html#launch-utility for \n"
+        "further instructions\n", FutureWarning
     )
     args = parse_args(args)
-    run(args)
+    launch(args)
 
 
 if __name__ == "__main__":
