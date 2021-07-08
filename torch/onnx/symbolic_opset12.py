3a2929d6f42e7c3ee2a34399f05aa86d592d84d0
Negin Raoof <neginmr@utexas.edu>
2021-07-08 10:55:07 -0700

    Update symbolic_opset12.py

--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -26,9 +26,9 @@ def outer(g, input, other):
 
 @parse_args("v", "f", "i")
 def dropout(g, input, p, train):
-    sym_help.assert_training_mode(train, "dropout")
+    sym_help.check_training_mode(train, "dropout")
     # in eval mode, dropout is non-op - if the node's train param is set to False, dropout is non-op
-    if not sym_help._training_mode:
+    if not train:
         return input
     warnings.warn("Dropout is a training op and should not be exported in inference mode. "
                   "For inference, make sure to call eval() on the model and to export it with param training=False.")
