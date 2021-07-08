5777cdc448f428fae1cb79d96a37b1c689d5ce16
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2021-07-08 04:05:40 +0000

    fix, use range_floats

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1987,8 +1987,8 @@ def make_tensor(size, device: torch.device, dtype: torch.dtype, *, low=None, hig
         result = torch.randint(low, high, size, device=device, dtype=dtype)
     elif dtype in floating_types_and(torch.half, torch.bfloat16):
         ranges_floats = (torch.finfo(dtype).min, torch.finfo(dtype).max)
-        low = ranges_floats[0] if low is float('-inf') else -9 if low is None or low < ranges[0] else low
-        high = ranges_floats[1] if high is float('inf') else 9 if high is None or high >= ranges[1] else high
+        low = ranges_floats[0] if low is float('-inf') else -9 if low is None or low < ranges_floats[0] else low
+        high = ranges_floats[1] if high is float('inf') else 9 if high is None or high >= ranges_floats[1] else high
         span = high - low
         # Windows doesn't support torch.rand(bfloat16) on CUDA
         if IS_WINDOWS and torch.device(device).type == 'cuda' and dtype is torch.bfloat16:
@@ -1999,8 +1999,8 @@ def make_tensor(size, device: torch.device, dtype: torch.dtype, *, low=None, hig
         assert dtype in complex_types()
         float_dtype = torch.float if dtype is torch.cfloat else torch.double
         ranges_floats = (torch.finfo(dtype).min, torch.finfo(dtype).max)
-        low = ranges_floats[0] if low is float('-inf') else -9 if low is None or low < ranges[0] else low
-        high = ranges_floats[1] if high is float('inf') else 9 if high is None  or high >= ranges[1] else high
+        low = ranges_floats[0] if low is float('-inf') else -9 if low is None or low < ranges_floats[0] else low
+        high = ranges_floats[1] if high is float('inf') else 9 if high is None  or high >= ranges_floats[1] else high
         span = high - low
         real = torch.rand(size, device=device, dtype=float_dtype) * span + low
         imag = torch.rand(size, device=device, dtype=float_dtype) * span + low
