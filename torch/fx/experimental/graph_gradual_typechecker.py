b8f087d69110461f98f756121852744a76aa5849
Zeina Migeed <migeedz@fb.com>
2021-07-08 20:06:51 -0700

    Update base for Update on "add linear operation and modify one of the tests"
    Differential Revision: [D29626333](https://our.internmc.facebook.com/intern/diff/D29626333)
    
    [ghstack-poisoned]

--- torch/fx/experimental/graph_gradual_typechecker.py
+++ torch/fx/experimental/graph_gradual_typechecker.py
@@ -326,7 +326,8 @@ def maxpool2d_inference_rule(n: Node, module_instance):
     if n.args[0].type == Dyn and isinstance(n.type, TensorType):
         n.args[0].type = expand_to_tensor_dim(n.args[0].type, len(n.type.__args__))
     if isinstance(n.args[0].type, TensorType):
-        n.type = get_greatest_upper_bound(maxpool2d_check(n.args[0].type, module_instance), n.type)
+        output = maxpool2d_check(n.args[0].type, module_instance)
+        n.type = get_greatest_upper_bound(output, n.type)
     return n.type
 
 
