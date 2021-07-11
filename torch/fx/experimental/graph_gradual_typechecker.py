93ef40bd833b7539c0d9ed89f769be17eaef22bc
Zeina Migeed <migeedz@fb.com>
2021-07-10 20:04:51 -0700

    add linear operation and modify one of the tests (#61238)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/61238
    
    Test Plan: Imported from OSS
    
    Reviewed By: iramazanli
    
    Differential Revision: D29626333
    
    Pulled By: migeed-z
    
    fbshipit-source-id: d4303918e380d64ba8ab678f249db6674e89357a

--- torch/fx/experimental/graph_gradual_typechecker.py
+++ torch/fx/experimental/graph_gradual_typechecker.py
@@ -331,6 +331,35 @@ def maxpool2d_inference_rule(n: Node, module_instance):
     return n.type
 
 
+
+def linear_check(tensor_type, module_instance):
+    """
+    Checks that an input tensor type satisfies the conditions for linear operation
+    and returns the output type based on in and out features given by module_instance
+    """
+    if len(tensor_type.__args__) >= 2:
+        if is_consistent(module_instance.in_features, tensor_type.__args__[-1]):
+            # Todo backwards propagation
+            new_type_args = list(tensor_type.__args__)
+            new_type_args[-1] = module_instance.out_features
+            return TensorType(tuple(new_type_args))
+        else:
+            raise TypeError(f'Inconsistent {module_instance.in_features} and {tensor_type.__args__[-1]} in {module_instance}')
+    else:
+        raise TypeError(f'Type {tensor_type} must have rank 2 or more.')
+
+
+@register_inference_rule(torch.nn.Linear)
+def linear_inference_rule(n: Node, module_instance):
+    assert isinstance(n.args[0], Node)
+    if n.args[0].type == Dyn and isinstance(n.type, TensorType):
+        n.args[0].type = expand_to_tensor_dim(n.args[0].type, len(n.type.__args__))
+    if isinstance(n.args[0].type, TensorType):
+        output_type = linear_check(n.args[0].type, module_instance)
+        n.type = get_greatest_upper_bound(output_type, n.type)
+    return n.type
+
+
 class GraphTypeChecker:
     def __init__(self, env, traced):
         self.env = env
