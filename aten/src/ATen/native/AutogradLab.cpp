37a5682012ce3e296019efe07e936799c8d2cbde
Saketh Are <saketh@fb.com>
2021-07-11 23:42:32 +0000

    don't include torch/torch.h

--- aten/src/ATen/native/AutogradLab.cpp
+++ aten/src/ATen/native/AutogradLab.cpp
@@ -1,7 +1,6 @@
 #include <ATen/ATen.h>
 #include <ATen/core/DimVector.h>
 #include <c10/util/SmallBuffer.h>
-#include <torch/torch.h>
 
 #include <vector>
 
@@ -9,7 +8,7 @@ namespace at {
 namespace native {
 
 std::tuple<Tensor, Tensor> attn(const Tensor& q, const Tensor& k, const Tensor& v) {
-    TORCH_CHECK(!q.is_complex() && q.dtype() != kBFloat16 && q.dtype() != torch::kFloat16,
+    TORCH_CHECK(!q.is_complex() && q.dtype() != kBFloat16 && q.dtype() != at::kHalf,
             "torch.attn: not implemented for ", q.dtype());
 
     Tensor attn = tanh(q.matmul(k.swapaxes(0, 1)));
