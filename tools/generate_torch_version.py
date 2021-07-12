978cb436093ed5f3e0a5d5d4c43c5a957e77308b
Eli Uriegas <eliuriegas@fb.com>
2021-07-12 16:15:28 -0700

    torch: Make __version__ better with comparisons
    Prior to 1.10.0 torch.__version__ was stored as a str and so many did
    comparisons against torch.__version__ as if it were a str. In order to not
    break them we have TorchVersion which masquerades as a str while also
    having the ability to compare against both packaging.version.Version as
    well as tuples of values, eg. (1, 2, 1)
    
    Examples:
      Comparing a TorchVersion object to a Version object
      >>> TorchVersion('1.10.0a') > Version('1.10.0a')
    
      Comparing a TorchVersion object to a Tuple object
      >>> TorchVersion('1.10.0a') > (1, 2)    # 1.2
      >>> TorchVersion('1.10.0a') > (1, 2, 1) # 1.2.1
    
      Comparing a TorchVersion object against a string
      >>> TorchVersion('1.10.0a') > '1.2'
      >>> TorchVersion('1.10.0a') > '1.2.1'
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- tools/generate_torch_version.py
+++ tools/generate_torch_version.py
@@ -29,9 +29,22 @@ def get_torch_version(sha: Optional[str] = None) -> str:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate torch/version.py from build and environment metadata.")
-    parser.add_argument("--is_debug", type=distutils.util.strtobool, help="Whether this build is debug mode or not.")
-    parser.add_argument("--cuda_version", type=str)
-    parser.add_argument("--hip_version", type=str)
+    parser.add_argument(
+        "--is_debug",
+        type=distutils.util.strtobool,
+        help="Whether this build is debug mode or not.",
+        default=False
+    )
+    parser.add_argument(
+        "--cuda_version",
+        type=str,
+        default=""
+    )
+    parser.add_argument(
+        "--hip_version",
+        type=str,
+        default=""
+    )
 
     args = parser.parse_args()
 
@@ -40,7 +53,7 @@ if __name__ == "__main__":
     args.hip_version = None if args.hip_version == '' else args.hip_version
 
     pytorch_root = Path(__file__).parent.parent
-    version_path = pytorch_root / "torch" / "version.py"
+    version_path = pytorch_root / "torch" / "_version.py"
     sha = get_sha(pytorch_root)
     version = get_torch_version(sha)
 
--- torch/__init__.py
+++ torch/__init__.py
@@ -26,7 +26,10 @@ from ._utils_internal import get_file_path, prepare_multiprocessing_environment,
 if sys.executable == 'torch_deploy':
     __version__ = "torch-deploy-1.8"
 else:
-    from .version import __version__ as __version__
+    from .torch_version import __version__ as __version__
+    # for backwards compat of people using torch.version directly
+    from . import _version as version
+
 from ._six import string_classes as _string_classes
 
 from typing import Set, Type, TYPE_CHECKING
