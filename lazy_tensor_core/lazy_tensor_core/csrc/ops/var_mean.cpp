ed0f7f20d3a5555a1243cce27ff0691546ae93dd
Alex Suhan <asuhan@fb.com>
2021-07-12 15:22:47 -0700

    Pull latest changes from PT/XLA

--- lazy_tensor_core/lazy_tensor_core/csrc/ops/var_mean.cpp
+++ lazy_tensor_core/lazy_tensor_core/csrc/ops/var_mean.cpp
@@ -10,7 +10,7 @@ namespace ops {
 VarMean::VarMean(const Value& input,
                  std::vector<lazy_tensors::int64> dimensions,
                  lazy_tensors::int64 correction, bool keep_reduced_dimensions)
-    : Node(ir::OpKind(at::aten::var), {input},
+    : Node(ir::OpKind(at::aten::var_mean), {input},
            /*num_outputs=*/2,
            lazy_tensors::util::MHash(dimensions, correction,
                                      keep_reduced_dimensions)),
