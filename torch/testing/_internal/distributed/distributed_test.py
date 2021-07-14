182270e6c8b32ba44486089662c00fc0c1aa041e
Rohan Varma <rvarm1@fb.com>
2021-07-14 11:34:23 -0700

    [DDP] fix test_ddp_inference
    Closes https://github.com/pytorch/pytorch/issues/61481. Fixes this
    test by removing section that uses only torch.no_grad() and doesn't call
    model.eval(). For SyncBN, need to call model.eval() otherwise SyncBN will
    assume it is in training mode, which does collective calls in the forward pass
    and does not work for inference.
    
    Differential Revision: [D29699444](https://our.internmc.facebook.com/intern/diff/D29699444/)
    
    [ghstack-poisoned]

--- torch/testing/_internal/distributed/distributed_test.py
+++ torch/testing/_internal/distributed/distributed_test.py
@@ -7080,7 +7080,6 @@ class DistributedTest:
             # certain parameters.
             self._test_ddp_multiple_nested_unused_params_error(ignore_sparse=True)
 
-        @unittest.skip("See: https://github.com/pytorch/pytorch/issues/61481")
         @unittest.skipIf(BACKEND != 'nccl' and BACKEND != 'gloo',
                          "Only Nccl & Gloo backend support DistributedDataParallel")
         @skip_if_lt_x_gpu(2)
@@ -7112,14 +7111,8 @@ class DistributedTest:
             for test in tests:
                 test_model, test_local_model, test_inp = test
                 if self.rank == 0:
-                    with torch.no_grad():
-                        for _ in range(6):
-                            self.assertEqual(
-                                test_model(test_inp),
-                                test_local_model(test_inp)
-                            )
-
                     test_model.eval()
+                    test_local_model.eval()
                     for _ in range(6):
                         self.assertEqual(
                             test_model(test_inp),
