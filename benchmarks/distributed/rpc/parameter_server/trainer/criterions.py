68ccc32ef1d4e896721aca359568cedb12bb117e
garrett <gcramer@fb.com>
2021-07-14 14:26:20 +0000

    Update base for Update on "add trainer hook functions"
    This pr adds hook functions for the trainers.
    
    [ghstack-poisoned]

--- benchmarks/distributed/rpc/parameter_server/trainer/criterions.py
+++ benchmarks/distributed/rpc/parameter_server/trainer/criterions.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
 
 
-def cel(rank, model):
+def cel(rank):
     r"""A function that creates a CrossEntropyLoss
     criterion for training.
     Args:
         rank (int): worker rank
-        model (nn.Module): neural network model
     """
     return nn.CrossEntropyLoss().cuda(rank)
