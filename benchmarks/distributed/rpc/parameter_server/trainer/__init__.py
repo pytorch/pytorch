3134d0bb2f2fafabcb6653c6cac4a30b364129f4
Garrett Cramer <gcramer@fb.com>
2021-07-14 13:14:08 -0700

    add trainer hook functions (#60785)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60785
    
    This pr adds hook functions for the trainers.
    
    Test Plan: Imported from OSS
    
    Reviewed By: zou3519
    
    Differential Revision: D29697299
    
    Pulled By: gcramer23
    
    fbshipit-source-id: cc3b991aad0d32503fbfc5acd4fca8b404e74c0f

--- benchmarks/distributed/rpc/parameter_server/trainer/__init__.py
+++ benchmarks/distributed/rpc/parameter_server/trainer/__init__.py
@@ -1,12 +1,20 @@
 from .criterions import cel
 from .ddp_models import basic_ddp_model
 from .hook_states import BasicHookState
+from .hooks import allreduce_hook, hybrid_hook, rpc_hook, sparse_rpc_hook
 from .iteration_steps import basic_iteration_step
 from .preprocess_data import preprocess_dummy_data
 from .trainer import DdpTrainer
 
 criterion_map = {
     "cel": cel
+}
+
+ddp_hook_map = {
+    "allreduce_hook": allreduce_hook,
+    "hybrid_hook": hybrid_hook,
+    "rpc_hook": rpc_hook,
+    "sparse_rpc_hook": sparse_rpc_hook
 }
 
 ddp_model_map = {
