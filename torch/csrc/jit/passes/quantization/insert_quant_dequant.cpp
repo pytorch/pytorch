ae15375c36a4549b8a013e6a7059507f6eba0916
Peter Bell <peterbell10@live.co.uk>
2021-07-14 17:40:55 +0100

    Update on "[JIT] FuseLinear pass now handles CallFunction("linear", ...)"
    There are several passes which are written to handle both
    `CallFunction("linear", ...)` and `aten::linear(...)` despite the two being
    functionally identical.
    
    This changes `FuseLinear` to alse normalize the `CallFunction` variant to
    `aten::linear`. That way each subsequent transformation only has to handle one
    form instead of both.
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
+++ torch/csrc/jit/passes/quantization/insert_quant_dequant.cpp
@@ -4,11 +4,11 @@
 #include <torch/csrc/jit/ir/subgraph_matcher.h>
 #include <torch/csrc/jit/jit_log.h>
 #include <torch/csrc/jit/passes/constant_propagation.h>
+#include <torch/csrc/jit/passes/fuse_linear.h>
 #include <torch/csrc/jit/passes/graph_rewrite_helper.h>
 #include <torch/csrc/jit/passes/inliner.h>
 #include <torch/csrc/jit/passes/quantization/helper.h>
 #include <torch/csrc/jit/passes/subgraph_rewrite.h>
-#include <torch/csrc/jit/passes/fuse_linear.h>
 
 #include <stack>
 
