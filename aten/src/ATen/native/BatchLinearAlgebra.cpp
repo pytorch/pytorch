054f733664e21503d3e7d9fc4442861a39d3aa7e
Nikita Vedeneev <nik@quansight.com>
2021-07-15 11:00:52 -0700

    det_backward: more robust and with complex support (#58195)
    Summary:
    As per title.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58195
    
    Reviewed By: albanD
    
    Differential Revision: D29361872
    
    Pulled By: anjali411
    
    fbshipit-source-id: b1f0fec7e3ac52acd1481bcc878cc0c1d07c1852

--- aten/src/ATen/native/BatchLinearAlgebra.cpp
+++ aten/src/ATen/native/BatchLinearAlgebra.cpp
@@ -3560,9 +3560,20 @@ std::tuple<Tensor, Tensor, Tensor, Tensor> linalg_lstsq(
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ lu_solve ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 DEFINE_DISPATCH(lu_solve_stub);
+DEFINE_DISPATCH(lu_solve_trans_stub);
 
 // Supports arbitrary batch dimensions for self and LU_data (implicitly LU_pivots also)
-Tensor lu_solve(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivots) {
+Tensor _lu_solve_trans(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivots, const c10::string_view trans_str) {
+  auto trans = std::toupper(trans_str[0]);
+  switch (trans) {
+    case 'N':
+    case 'T':
+    case 'C':
+      break;
+    default:
+      TORCH_CHECK(false,
+                  "lu_solve: wrong `trans` parameter, it must be one of 'N', 'T' or 'C'");
+  }
   TORCH_CHECK(self.dim() >= 2,
               "b should have at least 2 dimensions, but has ", self.dim(), " dimensions instead");
   TORCH_CHECK(LU_data.dim() >= 2,
@@ -3591,7 +3602,7 @@ Tensor lu_solve(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivo
   IntArrayRef new_pivots_sizes(LU_data_broadcasted.sizes().data(), LU_data_broadcasted.dim() - 1);
   Tensor LU_pivots_broadcasted = LU_pivots.expand(new_pivots_sizes);
 
-  // lu_solve_stub (apply_lu_solve) requires batched column major (Fortran-contiguous) tensors
+  // lu_solve_trans_stub (apply_lu_solve) requires batched column major (Fortran-contiguous) tensors
   // 'result' tensor is modified in-place and must be a copy of 'self_broadcasted'
   Tensor result = cloneBatchedColumnMajor(self_broadcasted);
 
@@ -3600,8 +3611,12 @@ Tensor lu_solve(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivo
   Tensor LU_data_working_copy = is_LU_data_batched_column_major ? LU_data_broadcasted : cloneBatchedColumnMajor(LU_data_broadcasted);
   Tensor LU_pivots_working_copy = LU_pivots_broadcasted.is_contiguous() ? LU_pivots_broadcasted : LU_pivots_broadcasted.contiguous();
 
-  lu_solve_stub(self.device().type(), result, LU_data_working_copy, LU_pivots_working_copy);
+  lu_solve_trans_stub(self.device().type(), result, LU_data_working_copy, LU_pivots_working_copy, trans);
   return result;
+}
+
+Tensor lu_solve(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivots) {
+  return at::native::_lu_solve_trans(self, LU_data, LU_pivots, "N");
 }
 
 Tensor& lu_solve_out(const Tensor& self, const Tensor& LU_data, const Tensor& LU_pivots, Tensor& result) {
@@ -3711,6 +3726,98 @@ std::tuple<Tensor&,Tensor&> legacy_lstsq_out(
   resize_output(B_out, B_tmp.sizes());
   B_out.copy_(B_tmp);
   return std::tuple<Tensor&, Tensor&>(B_out, A_out);
+}
+
+// The backward for this function is just a specialized version of
+// lu.backward, which is implemented in /torch/_autograd_functions.py
+Tensor _det_lu_based_helper_backward_helper(
+  const Tensor& det_grad,
+  const Tensor& det,
+  const Tensor& self,
+  const Tensor& lu,
+  const Tensor& pivs
+) {
+  auto eps = at::native::_get_epsilon(c10::toValueType(self.scalar_type()));
+  auto n = self.size(-1);
+  auto eps_tensor = at::tensor(eps, self.options());
+  auto condition_diagonal = [&](const Tensor& x) {
+    auto x_diag = x.diagonal(0, -2, -1);
+    auto x_diag_conditioned = at::where(
+      x_diag == 0.0,
+      eps_tensor,
+      x_diag
+    );
+    x_diag.copy_(x_diag_conditioned);
+  };
+
+  // create a matrix d := (det_grad * det.conj()) I
+  // NOTE: we do not use the shorter version
+  // auto d = at::zeros_like(self);
+  // d.diagonal(0, -2, -1).copy_((det_grad * det.conj()).unsqueeze(-1));
+  // to avoid in-place operations to eliminate potential issues with Vmap
+  auto det_expanded_sizes = det.sizes().vec();
+  det_expanded_sizes.push_back(n);
+  auto d_diag = det_grad * det.conj();
+  auto d = at::diag_embed(d_diag.unsqueeze(-1).expand(det_expanded_sizes));
+  // make sure that d is Fortran-contiguous. The transposition is sufficient as d is a diagonal square matrix
+  d = d.transpose(-2, -1);
+
+  if (self.device().type() == at::kCPU) {
+    // we want to condition the diagonal of the lu Tensor, but it is not allowed
+    // to modify arguments of backward functions in-place, hence the cloning.
+    auto lu_clone = lu.clone();
+    condition_diagonal(lu_clone);
+
+    auto trans = self.is_complex() ? 'C' : 'T';
+
+    // d is modified in-place and will contain the result
+    lu_solve_trans_stub(self.device().type(), d, lu_clone, pivs, trans);
+    return d;
+  }
+  // lu_solve is less stable than two triangular_solve for CUDA tensors.
+  else {
+    Tensor p, l, u;
+    std::tie(p, l, u) = at::lu_unpack(lu, pivs, /*unpack_data=*/true, /*unpack_pivots=*/true);
+
+    if (self.is_complex()) {
+      // Tensors u_h and l_h should be physically conjugated prior to applying kernel stubs,
+      // as .conj() is lazy and will not materialize conjugated output.
+      l.conj_physical_();
+      u.conj_physical_();
+    }
+
+    auto infos = at::zeros({std::max<int64_t>(1, batchCount(self))}, self.options().dtype(kInt));
+
+    // triangular_solve_stub performs operations in-place.
+    // Tensor d will contain the result
+    condition_diagonal(u);
+
+    // Solve u^h x = d
+    // note that d = c I for some scalar c, hence
+    // d u_h^{-1} = c I u_h^{-1} = u_h^{-1} c I = u_h^{-1} d.
+    // NOTE: u is contigious and upper-triangular,
+    // but from the Fortran respective it is lower-triangular and already transposed.
+    // Since u is conjugated in-place in the code above, it is sufficient
+    // to just run triangular_solve with upper=false.
+    triangular_solve_stub(
+      self.device().type(), u, d, infos,
+      /*upper=*/false,
+      /*transpose=*/false,
+      /*conjugate_transpose=*/false,
+      /*unitriangular=*/false);
+
+    // After this operation d will contain a row-wise permuted grad wrt to self
+    // The same notes as for the system involving u apply here.
+    triangular_solve_stub(
+      self.device().type(), l, d, infos,
+      /*upper=*/true,
+      /*transpose=*/false,
+      /*conjugate_transpose=*/false,
+      /*unitriangular=*/true);
+
+    // multiply by p to restore the row order
+    return at::matmul(p, d);
+  }
 }
 
 }}  // namespace at::native
