905deac3650bf406d89ea2c22cc6daf1d1232bf4
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2021-07-15 13:25:45 -0500

    Update install_rocm.sh

--- .circleci/docker/common/install_rocm.sh
+++ .circleci/docker/common/install_rocm.sh
@@ -11,6 +11,7 @@ install_magma() {
     echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
     echo 'LIB += -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib -Wl,--rpath,$(MKLROOT)/lib -Wl,--rpath,/opt/rocm/magma/lib' >> make.inc
     echo 'DEVCCFLAGS += --gpu-max-threads-per-block=256' >> make.inc
+    export PATH="${PATH}:/opt/rocm/bin"
     if [[ -n "$PYTORCH_ROCM_ARCH" ]]; then
       amdgpu_targets=`echo $PYTORCH_ROCM_ARCH | sed 's/;/ /g'`
     else
@@ -22,7 +23,6 @@ install_magma() {
     done
     # hipcc with openmp flag may cause isnan() on __device__ not to be found; depending on context, compiler may attempt to match with host definition
     sed -i 's/^FOPENMP/#FOPENMP/g' make.inc
-    export PATH="${PATH}:/opt/rocm/bin"
     make -f make.gen.hipMAGMA -j $(nproc)
     LANG=C.UTF-8 make lib/libmagma.so -j $(nproc) MKLROOT=/opt/conda
     make testing/testing_dgemm -j $(nproc) MKLROOT=/opt/conda
