526a8100fd526eae1c2f4bd0bc6d439ba34be904
Jay Zhang <jiz@microsoft.com>
2021-07-17 00:30:41 +0800

    Restore the public interface for backward support.

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -81,6 +81,10 @@ def export(model, args, f, export_params=True, verbose=False, training=None,
             operator_export_type = OperatorExportTypes.ONNX_ATEN_FALLBACK
         else:
             operator_export_type = OperatorExportTypes.ONNX
+    if _retain_param_name is not None:
+        warnings.warn("`_retain_param_name' is DEPRECATED. Please stop using it."
+                      "The code now will work as it is True to keep the original "
+                      "parameter names in the final ONNX graph.")
     _export(model, args, f, export_params, verbose, training, input_names, output_names,
             operator_export_type=operator_export_type, opset_version=opset_version,
             do_constant_folding=do_constant_folding, example_outputs=example_outputs,
