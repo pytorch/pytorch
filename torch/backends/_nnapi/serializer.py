d0fcdcdf6673e94feb1136cacc8bb1b6bff72380
Akshit Khurana <axit@fb.com>
2021-07-16 16:22:31 -0700

    Handle simple NNAPI flatten NHWC case
    Summary:
    
    We can easily handle nnapi conversion for
    the times we flatten all dimensions
    
    Test Plan:
    
    pytest test/test_nnapi.py::TestNNAPI::test_flatten
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/backends/_nnapi/serializer.py
+++ torch/backends/_nnapi/serializer.py
@@ -940,7 +940,8 @@ class _NnapiSerializer(object):
         start_ctype, start_dim = self.get_constant_value(node.inputsAt(1), "IntType")
         end_ctype, end_dim = self.get_constant_value(node.inputsAt(2), "IntType")
 
-        if in_oper.dim_order != DimOrder.PRESUMED_CONTIGUOUS:
+        is_trivial_flatten = start_dim <= 1 and end_dim == -1
+        if in_oper.dim_order != DimOrder.PRESUMED_CONTIGUOUS and not is_trivial_flatten:
             raise Exception(
                 "Currently, reshape is not supported on NHWC tensors")
 
@@ -962,7 +963,7 @@ class _NnapiSerializer(object):
         if non_flattened_dims.count(0) > 1:
             raise Exception("Only 1 dim can be flexible")
 
-        out_oper = in_oper._replace(shape=out_shape)
+        out_oper = in_oper._replace(shape=out_shape, dim_order=DimOrder.PRESUMED_CONTIGUOUS)
         out_id = self.add_tensor_operand(node.outputsAt(0), out_oper)
 
         for idx, dim in enumerate(out_shape):
