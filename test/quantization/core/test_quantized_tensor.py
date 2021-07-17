63f74900277478f159824f84daa2c172a1bf34ee
Philip Meier <github.pmeier@posteo.de>
2021-07-17 11:50:44 +0200

    fix quantization test clone

--- test/quantization/core/test_quantized_tensor.py
+++ test/quantization/core/test_quantized_tensor.py
@@ -745,7 +745,10 @@ class TestQuantizedTensor(TestCase):
                 [numel], scale=scale, zero_point=zero_point,
                 device=device, dtype=dtype)
             per_channel_quantized = torch._empty_per_channel_affine_quantized(
-                [numel], scales=torch.tensor([scale] * numel), zero_points=torch.tensor([zero_point] * numel), axis=0,
+                [numel],
+                scales=torch.tensor([scale] * numel, device=device),
+                zero_points=torch.tensor([zero_point] * numel, device=device),
+                axis=0,
                 device=device, dtype=dtype)
             qtensors = [per_tensor_quantized, per_channel_quantized]
 
