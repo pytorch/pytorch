945fa46b540b5e16d5420c456b1800ea83566d5c
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-07-19 11:32:40 -0500

    try to make compiler gods happy :)

--- c10/util/BFloat16-math.h
+++ c10/util/BFloat16-math.h
@@ -91,7 +91,9 @@ inline c10::BFloat16 fmod(c10::BFloat16 a, c10::BFloat16 b) {
   return std::fmod(float(a), float(b));
 }
 
-C10_HOST_DEVICE inline c10::BFloat16 nextafter(c10::BFloat16 from, c10::BFloat16 to) {
+C10_HOST_DEVICE inline c10::BFloat16 nextafter(
+    c10::BFloat16 from,
+    c10::BFloat16 to) {
   using int_repr_t = uint16_t;
   using float_t = c10::BFloat16;
   constexpr uint8_t bits = 16;
@@ -103,7 +105,18 @@ C10_HOST_DEVICE inline c10::BFloat16 nextafter(c10::BFloat16 from, c10::BFloat16
   // get a mask to get the sign bit i.e. MSB
   int_repr_t sign_mask = int_repr_t{1} << (bits - 1);
 
-  if (::isnan(from) || ::isnan(to)) {
+#if defined(__HIP_PLATFORM_HCC__)
+  bool from_is_nan = (from != from);
+  bool to_is_nan = (to != to);
+#elif defined(_MSC_VER)
+  bool from_is_nan = isnan(from);
+  bool to_is_nan = isnan(to);
+#else
+  bool from_is_nan = std::isnan(from);
+  bool to_is_nan = std::isnan(to);
+#endif
+
+  if (from_is_nan || to_is_nan) {
     return from;
   }
 
