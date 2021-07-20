a7e4496eb87758b6206421c7458f96dd34267a75
Andrew Gu <andgu@fb.com>
2021-07-20 15:28:44 +0000

    Update on "Add generic join unit tests"
    This adds unit tests for the generic join context manager.
    
    ```
    gpurun python test/distributed/algorithms/test_join.py
    ```
    
    Differential Revision: [D29746646](https://our.internmc.facebook.com/intern/diff/D29746646)
    
    [ghstack-poisoned]

--- test/distributed/algorithms/test_join.py
+++ test/distributed/algorithms/test_join.py
@@ -15,7 +15,7 @@ from torch.testing._internal.common_distributed import (
     MultiProcessTestCase,
     require_n_gpus_for_nccl_backend,
 )
-from torch.testing._internal.common_utils import run_tests, TEST_WITH_ASAN
+from torch.testing._internal.common_utils import TEST_WITH_ASAN, run_tests
 
 BACKEND = dist.Backend.NCCL if torch.cuda.is_available() else dist.Backend.GLOO
 WORLD_SIZE = min(4, max(2, torch.cuda.device_count()))
