cde002ca8f863e3a8a0b4062fb118da6fedc1b5d
Richard Zou <zou3519@gmail.com>
2021-07-20 17:30:59 -0700

    [functorch] Some more batch rules for pointwise ops

--- functorch/functorch/csrc/BatchRulesUnaryOps.cpp
+++ functorch/functorch/csrc/BatchRulesUnaryOps.cpp
@@ -83,16 +83,33 @@ TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   UNARY_POINTWISE_ALL(tan);
   UNARY_POINTWISE_ALL(trunc);
 
+  // special-related
+  UNARY_POINTWISE_ALL(i0);
+  UNARY_POINTWISE_ALL(erfc);
+  UNARY_POINTWISE_ALL(erfinv);
+  UNARY_POINTWISE_ALL(exp2);
+
   // torch.special.* functions
   UNARY_POINTWISE(special_entr);
   UNARY_POINTWISE(special_erf);
   UNARY_POINTWISE(special_erfc);
+  UNARY_POINTWISE(special_erfcx);
   UNARY_POINTWISE(special_erfinv);
   UNARY_POINTWISE(special_expit);
   UNARY_POINTWISE(special_expm1);
+  UNARY_POINTWISE(special_digamma);
+  UNARY_POINTWISE(special_psi);
   UNARY_POINTWISE(special_exp2);
   UNARY_POINTWISE(special_gammaln);
+  UNARY_POINTWISE(special_i0);
   UNARY_POINTWISE(special_i0e);
+  UNARY_POINTWISE(special_i1);
+  UNARY_POINTWISE(special_i1e);
+  UNARY_POINTWISE(special_log1p);
+  UNARY_POINTWISE(special_ndtr);
+  UNARY_POINTWISE(special_ndtri);
+  UNARY_POINTWISE(special_round);
+  UNARY_POINTWISE(special_sinc);
 
   // Activation functions (from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity)
   UNARY_POINTWISE_SCALAR_SCALAR_SCALAR(elu);
