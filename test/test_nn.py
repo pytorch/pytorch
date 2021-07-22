7ba1b2582b1aa906d3afb540cef40632b0f1984b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-07-22 23:55:38 +0900

    update tests for fractional pool 3d

--- test/test_nn.py
+++ test/test_nn.py
@@ -13262,8 +13262,21 @@ class TestNNDeviceType(NNTestCase):
     def test_FractionalMaxPool2d_zero_batch(self, device):
         mod = nn.FractionalMaxPool2d(3, output_ratio=(0.5, 0.5)).to(device)
         inp = torch.ones(0, 16, 50, 32).to(device)
-
         self._test_module_empty_input(mod, inp)
+
+        with self.assertRaisesRegex(RuntimeError):
+            inp = torch.randn(1, 0, 50, 32).to(device)
+            mod(inp)
+
+    @onlyOnCPUAndCUDA
+    def test_FractionalMaxPool3d_zero_batch(self, device):
+        mod = nn.FractionalMaxPool3d(3, output_ratio=(0.5, 0.5, 0.5)).to(device)
+        inp = torch.ones(0, 16, 50, 32, 32).to(device)
+        self._test_module_empty_input(mod, inp)
+
+        with self.assertRaisesRegex(RuntimeError):
+            inp = torch.randn(1, 0, 50, 32, 32).to(device)
+            mod(inp)
 
     @onlyOnCPUAndCUDA
     def test_Unfold_empty(self, device):
