17c6dd7bfbe68953b8bade735d1eb5f3c2a321fd
Saketh Are <saketh@fb.com>
2021-07-22 23:20:32 +0000

    fix functional test

--- test/cpp/api/functional.cpp
+++ test/cpp/api/functional.cpp
@@ -1305,7 +1305,7 @@ TEST_F(FunctionalTest, LocalResponseNorm) {
 TEST_F(FunctionalTest, Bias) {
   {
     const auto x = torch::arange(100., 118).resize_({3, 3, 2});
-    const auto b = torch::arange(300., 303);
+    const auto b = torch::arange(300., 302);
     const auto y = F::bias(x, b);
     ASSERT_EQ(y.ndimension(), 3);
     ASSERT_EQ(y.sizes(), torch::IntArrayRef({3, 3, 2}));
