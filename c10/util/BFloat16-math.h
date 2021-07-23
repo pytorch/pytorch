55a7bcd58d926c2ebe6da9ab5e857265c307fa04
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-07-22 23:49:13 -0500

    add license info and update reference

--- c10/util/BFloat16-math.h
+++ c10/util/BFloat16-math.h
@@ -91,11 +91,37 @@ inline c10::BFloat16 fmod(c10::BFloat16 a, c10::BFloat16 b) {
   return std::fmod(float(a), float(b));
 }
 
+/*
+  The following function is inspired from the implementation in `musl`
+  Link to License: https://git.musl-libc.org/cgit/musl/tree/COPYRIGHT
+  ----------------------------------------------------------------------
+  Copyright © 2005-2020 Rich Felker, et al.
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ----------------------------------------------------------------------
+ */
 C10_HOST_DEVICE inline c10::BFloat16 nextafter(
     c10::BFloat16 from,
     c10::BFloat16 to) {
-  // inspired from :
-  // https://github.com/ifduyue/musl/blob/master/src/math/nextafter.c
+  // Reference:
+  // https://git.musl-libc.org/cgit/musl/tree/src/math/nextafter.c
   using int_repr_t = uint16_t;
   using float_t = c10::BFloat16;
   constexpr uint8_t bits = 16;
