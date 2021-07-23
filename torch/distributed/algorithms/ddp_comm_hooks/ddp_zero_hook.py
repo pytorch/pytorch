2c56ae37f934a80fddcbfd71aeb91bb1d1b4b3df
Andrew Gu <andgu@fb.com>
2021-07-23 00:13:05 +0000

    Add second stream support
    [ghstack-poisoned]

--- torch/distributed/algorithms/ddp_comm_hooks/ddp_zero_hook.py
+++ torch/distributed/algorithms/ddp_comm_hooks/ddp_zero_hook.py
@@ -1,3 +1,4 @@
+import contextlib
 from typing import Any, Callable, List, Optional
 
 import torch
@@ -46,64 +47,67 @@ def hook_then_zero_step(
                 gradient bucket.
         """
         fut = hook(state, bucket)
+        if not zero._use_extra_stream:
+            fut.wait()
 
-        def zero_step(_) -> List[torch.Tensor]:
-            r"""
-            Performs a partial :class:`ZeroRedundancyOptimizer` :meth:`step`
-            using the gradients in the given :class:`DistributedDataParallel`
-            gradient bucket.
-            """
-            # Proceed as normal until the DDP buckets have been rebuilt
-            if not ddp._has_rebuilt_buckets:
-                return [bucket.get_tensor()]
+        with torch.cuda.stream(zero._optim_stream) if zero._use_extra_stream else contextlib.suppress():
+            def zero_step(_) -> List[torch.Tensor]:
+                r"""
+                Performs a partial :class:`ZeroRedundancyOptimizer` :meth:`step`
+                using the gradients in the given :class:`DistributedDataParallel`
+                gradient bucket.
+                """
+                # Proceed as normal until the DDP buckets have been rebuilt
+                if not ddp._has_rebuilt_buckets:
+                    return [bucket.get_tensor()]
 
-            bucket_index = bucket.get_index()
-            rank = zero.global_rank
-            overlap_info = zero._overlap_info
-            if overlap_info.status == _OverlapStatus.UNINITIALIZED:
-                overlap_info.status = _OverlapStatus.DDP_HAS_REBUILT_BUCKETS
+                bucket_index = bucket.get_index()
+                rank = zero.global_rank
+                overlap_info = zero._overlap_info
+                if overlap_info.status == _OverlapStatus.UNINITIALIZED:
+                    overlap_info.status = _OverlapStatus.DDP_HAS_REBUILT_BUCKETS
 
-            bucket_params = bucket.get_model_params_for_bucket()
-            assert len(bucket_params) > 0, "Empty bucket"
-            rank_to_update = zero._ddp_bucket_index_to_rank(bucket_index)
+                bucket_params = bucket.get_model_params_for_bucket()
+                assert len(bucket_params) > 0, "Empty bucket"
+                rank_to_update = zero._ddp_bucket_index_to_rank(bucket_index)
 
-            # Once DDP buckets have been rebuilt but ZeRO has not been
-            # properly initialized yet, collect the information needed
-            if overlap_info.status == _OverlapStatus.DDP_HAS_REBUILT_BUCKETS:
-                params_per_rank = overlap_info.params_per_rank
-                params_per_bucket = overlap_info.params_per_bucket
+                # Once DDP buckets have been rebuilt but ZeRO has not been
+                # properly initialized yet, collect the information needed
+                if overlap_info.status == _OverlapStatus.DDP_HAS_REBUILT_BUCKETS:
+                    params_per_rank = overlap_info.params_per_rank
+                    params_per_bucket = overlap_info.params_per_bucket
+                    if rank_to_update == rank:
+                        overlap_info.offsets[bucket_index] = len(params_per_rank[rank_to_update])
+                    params_per_rank[rank_to_update].extend(bucket_params)
+                    params_per_bucket.append(bucket_params)
+
+                    return [bucket.get_tensor()]
+
                 if rank_to_update == rank:
-                    overlap_info.offsets[bucket_index] = len(params_per_rank[rank_to_update])
-                params_per_rank[rank_to_update].extend(bucket_params)
-                params_per_bucket.append(bucket_params)
+                    assert len(zero.optim.param_groups) == 1, \
+                        "Overlapping DDP with ZeRO only supports a single " \
+                        "parameter group"
+                    # Construct the `gradients` input for the local optimizer step,
+                    # which expects `None` in a list position to indicate that the
+                    # corresponding parameter should not be updated
+                    num_local_optim_params = len(zero.optim.param_groups[0]["params"])
+                    gradients: List[Optional[torch.Tensor]] = \
+                        [None for _ in range(num_local_optim_params)]
+                    assert bucket_index in overlap_info.offsets, \
+                        f"Bucket index {bucket_index} was not assigned to rank " \
+                        f"{rank}"
+                    offset = overlap_info.offsets[bucket_index]
+                    bucket_gradients = bucket.get_per_parameter_tensors()
+                    for i, grad in enumerate(bucket_gradients):
+                        gradients[offset + i] = grad
+                    zero._local_step(gradients)
+
+                device = bucket_params[0].device
+                device_index = zero._device_to_device_index[device]
+                assert bucket_index in zero._buckets[device_index][rank_to_update]
+                dist.broadcast(zero._buckets[device_index][rank_to_update][bucket_index], src=rank_to_update, async_op=True)
 
                 return [bucket.get_tensor()]
-
-            if rank_to_update == rank:
-                assert len(zero.optim.param_groups) == 1, \
-                    "Overlapping DDP with ZeRO only supports a single " \
-                    "parameter group"
-                # Construct the `gradients` input for the local optimizer step,
-                # which expects `None` in a list position to indicate that the
-                # corresponding parameter should not be updated
-                num_local_optim_params = len(zero.optim.param_groups[0]["params"])
-                gradients: List[Optional[torch.Tensor]] = \
-                    [None for _ in range(num_local_optim_params)]
-                assert bucket_index in overlap_info.offsets, \
-                    f"Bucket index {bucket_index} was not assigned to rank " \
-                    f"{rank}"
-                offset = overlap_info.offsets[bucket_index]
-                bucket_gradients = bucket.get_per_parameter_tensors()
-                for i, grad in enumerate(bucket_gradients):
-                    gradients[offset + i] = grad
-                zero._local_step(gradients)
-
-            device = bucket_params[0].device
-            device_index = zero._device_to_device_index[device]
-            assert bucket_index in zero._buckets[device_index][rank_to_update]
-            dist.broadcast(zero._buckets[device_index][rank_to_update][bucket_index], src=rank_to_update, async_op=True)
-
-            return [bucket.get_tensor()]
 
         return fut.then(zero_step)
 
--- torch/distributed/optim/zero_redundancy_optimizer.py
+++ torch/distributed/optim/zero_redundancy_optimizer.py
@@ -229,6 +229,9 @@ class ZeroRedundancyOptimizer(Optimizer, _Joinable):
             If ``False``, :meth:`step` runs disjointly after the backward pass
             (per normal).
             (default: ``False``)
+        use_extra_stream (bool, optional): if ``True``, use a second CUDA
+            stream for the optimizer computation; if ``False``, only use the
+            single default stream (default: ``False``).
         **defaults: any trailing arguments, which are forwarded to the local
             optimizer.
 
@@ -277,6 +280,7 @@ class ZeroRedundancyOptimizer(Optimizer, _Joinable):
         process_group: Optional[Any] = None,
         parameters_as_bucket_view: bool = False,
         overlap_with_ddp: bool = False,
+        use_extra_stream: bool = False,
         **defaults: Any,
     ):
         # Perform type and assumption checks on the input parameters
@@ -327,6 +331,14 @@ class ZeroRedundancyOptimizer(Optimizer, _Joinable):
                 )
             raise ValueError(error_msg)
         self._overlap_with_ddp = overlap_with_ddp
+        self._use_extra_stream = use_extra_stream
+        if use_extra_stream:
+            assert overlap_with_ddp, \
+                "`use_extra_stream` should only be set to `True` if " \
+                "`overlap_with_ddp=True`"
+        if use_extra_stream:
+            self._bwd_stream = torch.cuda.current_stream(self._default_device)
+            self._optim_stream = torch.cuda.Stream(self._default_device)
 
         # If `overlap_with_ddp=True`, local optimizer initialization is delayed
         # to run time after the necessary information has been collected
@@ -832,6 +844,11 @@ class ZeroRedundancyOptimizer(Optimizer, _Joinable):
                 # Since all information has been collected, perform the delayed
                 # initialization of the local optimizer and supporting state
                 self._init_zero_for_overlap()
+
+            if self._use_extra_stream:
+                # Ensure that all parameter updates are finished before the
+                # next forward pass
+                self._bwd_stream.wait_stream(self._optim_stream)
             # `step()` does not actually perform any parameter updates and is
             # only used for bookkeeping when `overlap_with_ddp=True`
             return None
