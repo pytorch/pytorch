6c82bdf57bf75e7ea6816314187c475ade6d53fe
Richard Barnes <rbarnes@fb.com>
2021-07-23 23:03:21 -0700

    Modernize override (#61744)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/61744
    
    Test Plan: Sandcastle
    
    Reviewed By: malfet
    
    Differential Revision: D29717320
    
    fbshipit-source-id: 6eea4295ee2e5572ab337620be412376fcc2f3cc

--- torch/csrc/jit/python/python_ivalue.h
+++ torch/csrc/jit/python/python_ivalue.h
@@ -77,8 +77,7 @@ struct C10_EXPORT ConcretePyObjectHolder final : PyObjectHolder {
   // nullptr, on destruction, effectively does nothing because of it calls
   // Py_XDECREF(NULL) underlying.
   // https://docs.python.org/3/c-api/refcounting.html#c.Py_XDECREF
-  // NOLINTNEXTLINE(modernize-use-override)
-  ~ConcretePyObjectHolder() {
+  ~ConcretePyObjectHolder() override {
     pybind11::gil_scoped_acquire ag;
     py_obj_.dec_ref();
     // explicitly setting PyObject* to nullptr to prevent py::object's dtor to
