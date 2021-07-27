d4ef09be62d9c6748961d9a9ec83e9614b458ec4
ahe <ahe@devvm4633.ftw0.facebook.com>
2021-07-27 10:09:36 -0700

    Draft: Nnapi backend execute and compile
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/jit/backends/nnapi/nnapi_backend_lib.cpp
+++ torch/csrc/jit/backends/nnapi/nnapi_backend_lib.cpp
@@ -1,5 +1,10 @@
+#include <memory>
+
+#include <ATen/nnapi/nnapi_bind.h>
 #include <torch/csrc/jit/backends/backend.h>
 #include <torch/csrc/jit/backends/backend_exception.h>
+#include <torch/csrc/jit/mobile/import.h>
+#include <torch/csrc/jit/mobile/module.h>
 
 namespace torch {
 namespace jit {
@@ -17,23 +22,101 @@ class NnapiBackend : public PyTorchBackendInterface {
     return true;
   }
 
-  // Function stub
-  // TODO: implement compile
   c10::impl::GenericDict compile(
       c10::IValue processed,
       c10::impl::GenericDict method_compile_spec) override {
-    auto handles =
-        c10::Dict<std::string, std::vector<std::tuple<std::string, int64_t>>>();
+    auto dict = processed.toGenericDict();
+
+    // make weights contiguous
+    auto weights = dict.at("weights").toTensorList();
+    for (int i = 0; i < weights.size(); i++) {
+      weights[i] = weights.get(i).contiguous();
+    }
+    dict.insert("weights", weights);
+
+    // save ser_model to member variable
+    ser_model_ = dict.at("ser_model").toTensor();
+
+    // wrap procesed in dictionary: {"forward": processed}
+    c10::Dict<c10::IValue, c10::IValue> handles(
+      c10::StringType::get(), c10::AnyType::get());
+    handles.insert("forward", dict);
     return c10::impl::toGenericDict(handles);
   }
 
-  // Function stub
-  // TODO: implement execute
   c10::impl::GenericList execute(
       c10::IValue handle,
       c10::impl::GenericList inputs) override {
-    c10::List<at::Tensor> output_list;
-    return c10::impl::toList(output_list);
+    if (comp_ == nullptr) {
+      init(handle, inputs);
+    }
+    TORCH_CHECK(comp_ != nullptr)
+
+    c10::List<at::Tensor> outputs;
+    for(at::Tensor out: out_templates_) {
+      outputs.push_back(at::empty_like(out));
+    }
+
+    // Format inputs
+    auto dict = handle.toGenericDict();
+    auto inp_mem_fmts = dict.at("inp_mem_fmts").toIntList();
+    TORCH_CHECK(inputs.size() == inp_mem_fmts.size());
+    std::vector<at::Tensor> fixed_inputs;
+    for (int i = 0; i < inputs.size(); i++) {
+      int fmt = inp_mem_fmts[i];
+      c10::IValue curInp = inputs[i];
+      // These constants match the values in DimOrder in serializer.py
+      // TODO: See if it's possible to use those directly.
+      if (fmt == 0) {
+        fixed_inputs.push_back(curInp.toTensor().contiguous());
+      } else if (fmt == 1) {
+        c10::IntArrayRef order = {0, 2, 3, 1};
+        fixed_inputs.push_back(curInp.toTensor().permute(order).contiguous());
+      } else {
+        throw std::exception();
+        std::cerr << "Invalid mem_fmt" << std::endl;
+      }
+    }
+
+    comp_->run(fixed_inputs, outputs.vec());
+
+    // Format outputs
+    auto out_mem_fmts = dict.at("out_mem_fmts").toIntList();
+    TORCH_CHECK(outputs.size() == out_mem_fmts.size());
+    for (int i = 0; i < outputs.size(); i++) {
+      int fmt = out_mem_fmts[i];
+      // These constants match the values in DimOrder in serializer.py
+      if (fmt == 1) {
+        c10::IntArrayRef order = {0, 3, 1, 2};
+        at::Tensor curOut = outputs[i];
+         outputs[i] = curOut.permute(order);
+      } else if (fmt != 0) {
+        throw std::exception();
+        std::cerr << "Invalid mem_fmt" << std::endl;
+      }
+    }
+
+    return c10::impl::toList(outputs);
+  }
+
+ private:
+  std::unique_ptr<torch::nnapi::bind::NnapiCompilation> comp_;
+  c10::List<at::Tensor> out_templates_;
+  at::Tensor ser_model_;
+  mobile::Module shape_compute_module_;
+
+  void init(c10::IValue handle, c10::impl::GenericList inputs) {
+    auto dict = handle.toGenericDict();
+    std::stringstream ss;
+    auto shape_ptr = dict.at("shape_compute_module").toString();
+    ss.str(*shape_ptr);
+    shape_compute_module_ = _load_for_mobile(ss);
+
+    TORCH_CHECK(comp_ == nullptr);
+    auto weights = dict.at("weights").toTensorList();
+    out_templates_ = shape_compute_module_.run_method("prepare", ser_model_, weights).toTensorList();
+    comp_.reset(new torch::nnapi::bind::NnapiCompilation());
+    comp_->init(ser_model_, weights.vec());
   }
 };
 
