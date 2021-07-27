8f3a3da187547043db7c25e5c4b8b7d3fd25ceba
Zeina Migeed <migeedz@fb.com>
2021-07-27 14:25:24 -0700

    Update on "use unification source code instead of library"
    [ghstack-poisoned]

--- torch/fx/experimental/unification/__init__.py
+++ torch/fx/experimental/unification/__init__.py
@@ -1,7 +1,5 @@
 # type: ignore
 
-from .core import unify, reify
-from .more import unifiable
-from .variable import var, isvar, vars, variables, Var
-
-__version__ = '0.2.2'
+from .core import unify, reify  # # noqa: F401
+from .more import unifiable  # noqa: F401
+from .variable import var, isvar, vars, variables, Var  # noqa: F401
