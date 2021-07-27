8de012adf9c26b89b5e42f9881d63ea68fb50d69
wanchaol <wanchaol@devvm3348.frc0.facebook.com>
2021-07-27 10:33:14 -0700

    Update on "[dist_optim] fix the bug of none grads on sgd"
    parameter and grads passed to torch.optim.functional should always match, we should skip the parameters that have none gradients to avoid the size mismatch
    
    Differential Revision: [D29929653](https://our.internmc.facebook.com/intern/diff/D29929653/)
    
    [ghstack-poisoned]

--- torch/testing/_internal/distributed/rpc/dist_optimizer_test.py
+++ torch/testing/_internal/distributed/rpc/dist_optimizer_test.py
@@ -210,13 +210,12 @@ class DistOptimizerTest(RpcAgentTestFixture):
         self._test_dist_optim_base(optim.Adamax, lr=0.05)
         self._test_dist_optim_base(optim.Rprop, lr=0.05)
 
-    @dist_init()
-    def test_dist_optim_none_grads(self):
+    def _test_dist_optim_none_grads(self, optim_cls, *args, **kwargs):
         # local version
         module1 = MyModule()
         module2 = MyModule(requires_grad=False)
         params = [module1.get_w(), module2.get_w()]
-        local_optim = optim.SGD(params, lr=0.05)
+        local_optim = optim_cls(params, *args, **kwargs)
 
         old_w1 = module1.w.clone().detach()
         old_w2 = module2.w.clone().detach()
@@ -238,32 +237,30 @@ class DistOptimizerTest(RpcAgentTestFixture):
 
         remote_module1 = rpc.remote(owner1, MyModule)
         remote_module2 = rpc.remote(owner2, MyModule, args=(False,))
-        remote_param1 = remote_method(MyModule.get_w, remote_module1)
-        remote_param2 = remote_method(MyModule.get_w, remote_module2)
-
-        old_w1_remote = remote_param1.to_here()
+        remote_param1 = remote_module1.remote().get_w()
+        remote_param2 = remote_module2.remote().get_w()
 
         # sanity check: local and remote initial weights should match
         self.assertEqual(old_w1, remote_param1.to_here())
         self.assertEqual(old_w2, remote_param2.to_here())
 
         dist_optim = DistributedOptimizer(
-            optim.SGD, [remote_param1, remote_param2], lr=0.05
+            optim_cls, [remote_param1, remote_param2], *args, **kwargs
         )
 
         with dist_autograd.context() as context_id:
             g_cpu.manual_seed(0)
             t1 = torch.rand((3, 3), requires_grad=True, generator=g_cpu)
             t2 = torch.rand((3, 3), requires_grad=True, generator=g_cpu)
-            output1 = rpc_async_method(MyModule.forward, remote_module1, t2)
-            output2 = rpc_async_method(MyModule.forward, remote_module2, output1.wait())
+            output1 = remote_module1.rpc_async().forward(t2)
+            output2 = remote_module2.rpc_async().forward(output1.wait())
             loss = torch.add(output2.wait(), t1)
 
             dist_autograd.backward(context_id, [loss.sum()])
             dist_optim.step(context_id)
 
-            new_w1 = rpc_async_method(MyModule.get_w, remote_module1).wait()
-            new_w2 = rpc_async_method(MyModule.get_w, remote_module2).wait()
+            new_w1 = remote_module1.rpc_async().get_w().wait()
+            new_w2 = remote_module2.rpc_async().get_w().wait()
 
             # ensure optimizer changed weights for w1
             self.assertNotEqual(old_w1, new_w1)
@@ -273,3 +270,10 @@ class DistOptimizerTest(RpcAgentTestFixture):
             # ensure local equals remote
             self.assertEqual(new_w1, module1.get_w())
             self.assertEqual(new_w2, module2.get_w())
+
+    @dist_init()
+    def test_dist_optim_none_grads(self):
+        self._test_dist_optim_none_grads(optim.SGD, lr=0.05)
+        self._test_dist_optim_none_grads(optim.RMSprop, lr=0.05)
+        self._test_dist_optim_none_grads(optim.Rprop, lr=0.05)
+        self._test_dist_optim_none_grads(optim.Adadelta, rho=0.95)
