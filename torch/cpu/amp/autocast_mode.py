b713acfbf3cb187c6eaafb27ea68f10608eba7f1
puririshi98 <puririshi98@berkeley.edu>
2021-07-28 10:52:54 -0700

    Update autocast_mode.py

--- torch/cpu/amp/autocast_mode.py
+++ torch/cpu/amp/autocast_mode.py
@@ -2,4 +2,4 @@ import torch
 
 class autocast(torch.autocast):
     def __init__(self, enabled=True, fast_dtype=torch.float16):
-          super().__init__("cpu", enabled=enabled, fast_dtype=fast_dtype)
+        super().__init__("cpu", enabled=enabled, fast_dtype=fast_dtype)
