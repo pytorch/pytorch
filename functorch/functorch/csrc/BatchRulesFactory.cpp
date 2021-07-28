2d5a0c1bf85a603c0b9a4b3ac9c6cd4713d77fb1
Richard Zou <zou3519@gmail.com>
2021-07-28 15:58:13 -0700

    [functorch] replace basic_unary_batch_rule with BASIC_UNARY_BATCH_RULE
    For the new macro there is no need to pass in the additional non-tensor
    arguments. This is dome by some mix of tensor metaprogramming.

--- functorch/functorch/csrc/BatchRulesFactory.cpp
+++ functorch/functorch/csrc/BatchRulesFactory.cpp
@@ -8,18 +8,13 @@
 
 namespace at { namespace functorch {
 
-
-#define SINGLE_ARG(...) __VA_ARGS__
-#define FACTORY_TYPES optional<ScalarType>, optional<Layout>, optional<Device>, optional<bool>, optional<MemoryFormat>
-#define FACTORY_BATCH_RULE(op) SINGLE_ARG(basic_unary_batch_rule<decltype(&ATEN_FN(op)), &at::op, FACTORY_TYPES>)
-
 TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
-  VMAP_SUPPORT("ones_like", FACTORY_BATCH_RULE(ones_like));
-  VMAP_SUPPORT("zeros_like", FACTORY_BATCH_RULE(zeros_like));
-  VMAP_SUPPORT("empty_like", FACTORY_BATCH_RULE(empty_like));
-  VMAP_SUPPORT("randn_like", FACTORY_BATCH_RULE(randn_like));
-  VMAP_SUPPORT("rand_like", FACTORY_BATCH_RULE(rand_like));
-  VMAP_SUPPORT("full_like", SINGLE_ARG(basic_unary_batch_rule<decltype(&ATEN_FN(full_like)), &at::full_like, const Scalar&, FACTORY_TYPES>));
+  VMAP_SUPPORT("ones_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(ones_like)));
+  VMAP_SUPPORT("zeros_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(zeros_like)));
+  VMAP_SUPPORT("empty_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(empty_like)));
+  VMAP_SUPPORT("randn_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(randn_like)));
+  VMAP_SUPPORT("rand_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(rand_like)));
+  VMAP_SUPPORT("full_like", BASIC_UNARY_BATCH_RULE(ATEN_FN(full_like)));
   // Not sure how to add the ones with irregular args to the mix cleanly (i.e. randint takes an extra int parameter)
 }
 }}
