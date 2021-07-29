4f4cad0872d5b2fec9c4a391a0995be69f622c2d
anjali411 <chourdiaanjali123@gmail.com>
2021-07-29 08:02:44 -0700

    Draft for LinearOperator classes + the new solver API
    [ghstack-poisoned]

--- test/test_LO.py
+++ test/test_LO.py
@@ -15,132 +15,117 @@ def no_dispatch() -> Iterator[None]:
     finally:
         del guard
 
-# HANDLED_FUNCTIONS_DIAGONAL is a dispatch table that
-# DiagonalTensor.__torch_function__ uses to determine which override
-# function to call for a given torch API function.  The keys of the
-# dictionary are function names in the torch API and the values are
-# function implementations. Implementations are added to
-# HANDLED_FUNCTION_DIAGONAL by decorating a python function with
-# implements_diagonal. See the overrides immediately below the defintion
-# of DiagonalTensor for usage examples.
-HANDLED_FUNCTIONS_DIAGONAL = {}
+HANDLED_FUNCTIONS_RECTANGULAR = {}
 
-def implements_diagonal(torch_function):
-    """Register a torch function override for DiagonalTensor.
+def implements(torch_aten_function, HANDLES_DICT):
+    """Register a torch function override for RectangularTensor.
 
     This decorator takes a function in the torch API as a
     parameter. Applying this decorator to a function adds that function
     as the registered override for the torch function passed as a
-    parameter to the decorator. See DiagonalTensor.__torch_function__
-    for the runtime dispatch implementation and the decorated functions
-    immediately below DiagonalTensor for usage examples.
+    parameter to the decorator.
     """
-    @functools.wraps(torch_function)
+    @functools.wraps(torch_aten_function)
     def decorator(func):
-        HANDLED_FUNCTIONS_DIAGONAL[torch_function] = func
+        HANDLES_DICT[torch_aten_function] = func
         return func
     return decorator
 
-class DiagonalTensor(torch.Tensor):
-    """A class with __torch_function__ and a specific diagonal representation
-
-    This class has limited utility and is mostly useful for verifying that the
-    dispatch mechanism works as expected. It is based on the `DiagonalArray
-    example`_ in the NumPy documentation.
-
-    ``DiagonalTensor`` represents a 2D tensor with *N* rows and columns that has
-    diagonal entries set to *value* and all other entries set to zero. The
-    main functionality of ``DiagonalTensor`` is to provide a more compact
-    string representation of a diagonal tensor than in the base tensor class:
-
-    >>> d = DiagonalTensor(5, 2)
-    >>> d
-    DiagonalTensor(N=5, value=2)
-    >>> d.tensor()
-    tensor([[2., 0., 0., 0., 0.],
-            [0., 2., 0., 0., 0.],
-            [0., 0., 2., 0., 0.],
-            [0., 0., 0., 2., 0.],
-            [0., 0., 0., 0., 2.]])
-
-    Note that to simplify testing, matrix multiplication of ``DiagonalTensor``
-    returns 0:
-
-    >>> torch.mm(d, d)
-    0
-
-    .. _DiagonalArray example:
-        https://numpy.org/devdocs/user/basics.dispatch.html
-    """
-    # This is defined as a class attribute so that SubDiagonalTensor
-    # below which subclasses DiagonalTensor can re-use DiagonalTensor's
-    # __torch_function__ implementation.
-    handled_functions = HANDLED_FUNCTIONS_DIAGONAL
-    elem: torch.Tensor
-    N: int
-
-    __slots__ = ['elem', 'N']
+class RectangularTensor(torch.Tensor):
+    handled_functions = HANDLED_FUNCTIONS_RECTANGULAR
+    __slots__ = "full_rank", "well_cond"
 
     @staticmethod
     def __new__(cls, elem, *args, **kwargs):
-        # The wrapping tensor (DiagonalTensor) is just a meta tensor, so it
+        # The wrapping tensor (RectangularTensor) is just a meta tensor, so it
         # doesn't hold any memory (meta tensor is generally the preferred type
         # of tensor you want to make a subclass from)...
         N = elem.size(0)
-        r = torch.Tensor._make_subclass(cls, torch.empty(N, N, dtype=elem.dtype, device='meta'), elem.requires_grad)
+        r = torch.Tensor._make_subclass(cls, elem.to('meta'), elem.requires_grad)
         # ...the real tensor is held as an element on the tensor.
         r.elem = elem
-        r.N = N
+        r.full_rank = kwargs['full_rank'] if 'full_rank' in kwargs else True
+        r.well_cond = kwargs['well_cond'] if 'well_cond' in kwargs else True
+        r.rcond = kwargs['rcond'] if 'rcond' in kwargs else None
         return r
 
     def __repr__(self):
-        return "DiagonalTensor(N={}, diagonal elements={})".format(self.N, self.elem)
-
-    def __array__(self):
-        return torch.diag(self.elem).cpu().numpy()
+        return f"RectangularTensor({self.elem})"
 
     def tensor(self):
-        return torch.diag(self.elem)
+        return self.elem
 
     @classmethod
     def __torch_dispatch__(self, func, types, args=(), kwargs=None):
         if kwargs is None:
             kwargs = {}
-        # if func not in self.handled_functions:
-        #     return NotImplemented
-        def unwrap(e):
-            return e.elem if isinstance(e, DiagonalTensor) else e
-
-        def wrap(e):
-            return DiagonalTensor(e) if isinstance(e, torch.Tensor) else e
-
-        if func == torch.ops.aten.add:
-            with no_dispatch():
-                print(*tree_map(unwrap, args))
-                rs = tree_map(wrap, torch.add(*tree_map(unwrap, args)))
-                return rs
+        if func not in self.handled_functions:
+            return NotImplemented
         return self.handled_functions[func](*args, **kwargs)
 
-    def __eq__(self, other):
-        if type(other) is type(self):
-            if self.N == other.N and self.elem == other.elem:
-                return True
+@implements(torch.ops.aten.solve, HANDLED_FUNCTIONS_RECTANGULAR)
+def solve(A, b):
+    A_tensor = A.elem
+    print(A_tensor)
+    # As per https://github.com/pytorch/pytorch/issues/54151
+    rcond = A.rcond if A.rcond is not None else \
+            torch.finfo(A_tensor.dtype).eps * max(A_tensor.size(-2), A_tensorA.size(-1))
+    if A.full_rank:  # QR
+        return torch.linalg.lstsq(A_tensor, B, driver="gels").solution
+    else:
+        if self.well_cond:  # PQR
+            return torch.linalg.lstsq(A_tensor, B, rcond=rcond, driver="gelsy").solution
+        else:  # SVD
+            return torch.linalg.lstsq(A_tensor, B, rcond=rcond, driver="gelsd").solution
 
-        return False
+HANDLED_FUNCTIONS_SQUARE={}
+class Square(RectangularTensor):
+    handled_functions = HANDLED_FUNCTIONS_SQUARE
 
-@implements_diagonal(torch.div)
-def diagonal_div(input, other, out=None):
-    return DiagonalTensor(torch.div(input.elem, other.elem, out=out))
+    def __new__(cls, elem, *args, **kwargs):
+        invertible = kwargs['invertible'] if 'invertible' in kwargs else True
+        well_cond = kwargs['well_cond'] if 'well_cond' in kwargs else True
+        r = super().__new__(cls, elem, invertible=invertible, well_cond=well_cond)
+        return r
 
-@implements_diagonal(torch.add)
-def add(input, other):
-    return DiagonalTensor(torch.add(input.elem, other.elem))
+@implements(torch.ops.aten.solve, HANDLED_FUNCTIONS_SQUARE)
+def solve(A, b):
+    if A.full_rank:  # PLU
+        return torch.linalg.solve(A.elem, B)
+    else:
+        return super().solve(A.elem, B)
 
-@implements_diagonal(torch.sub)
-def sub(input, other):
-    return DiagonalTensor(torch.sub(input.elem, other.elem))
+if __name__ == "__main__":
+    # A = torch.randn(3, 4)
+    B = torch.randn(3, 2)
 
-if __name__ == '__main__':
-    a = DiagonalTensor(torch.randn(3))
-    b = DiagonalTensor(torch.randn(3))
-    torch.add(a, b)
+    # # Rectangular. Dispatches to lstsq, as it should
+    # X = torch.solve(A, B)
+    # assert torch.allclose(A @ X, B, atol=1e-6)
+
+    # # Dispatches to PLU (faster than lstsq), because it is square. Backwards compatible.
+    # A = torch.randn(3, 3)
+    # X = solve(A, B)
+    # assert torch.allclose(A @ X, B, atol=1e-6)
+
+    # # Make A symmetric positive definite
+    # A = A @ A.transpose(-2, -1) + torch.eye(3)
+    # # Dispatches to cholesky, 2x faster than LU solve
+    # X = solve(A, B, HPD())
+    # assert torch.allclose(A @ X, B, atol=1e-6)
+
+    A = torch.randn(3, 3)
+    # Make A not invertible
+    A[2] = 2.4*A[0] + 3*A[1]
+    A_sqr = Square(A, invertible=False)
+    X = solve(A_sqr, B)
+    # B is not in the image of A, so there is no solution
+    # solve returns the vectors X that minimize ||AX - B|| (least squares solution)
+    assert not torch.allclose(A @ X, B, atol=1e-6)
+
+    # "Project" B onto the image of A
+    B = A @ A.T @ B
+    A_sqr = Square(A, invertible=False)
+    X = solve(A_sqr, B)
+    # B is now on the image of A, so there is a solution and solve finds it
+    assert torch.allclose(A @ X, B, atol=1e-6)
