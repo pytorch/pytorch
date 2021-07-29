d0abe4e4dc6b0dca1145b172e2e5b380cf33b5e2
Richard Zou <zou3519@gmail.com>
2021-07-28 19:17:52 -0700

    [functorch] Quick grab bag of batching rules

--- functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
+++ functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
@@ -140,6 +140,8 @@ TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   OP_DECOMPOSE(outer);
   m.impl("linear", linear_decomp);
   VMAP_SUPPORT("cholesky_inverse", VARIADIC_BDIMS_BATCH_RULE(at::cholesky_inverse));
+  VMAP_SUPPORT("pinverse", VARIADIC_BDIMS_BATCH_RULE(at::pinverse));
+  VMAP_SUPPORT("linalg_pinv", VARIADIC_BDIMS_BATCH_RULE(ATEN_FN(linalg_pinv)));
 }
 }}
 
