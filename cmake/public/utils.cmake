007f6073d1507739f333ef564a33490ed6f00bf2
Peter Bell <peterbell10@live.co.uk>
2021-07-30 21:53:47 +0100

    Update on "WIP: Update CMake and use native CUDA language support"
    PyTorch currently uses the old style of compiling CUDA in CMake which is just a
    bunch of scripts in `FindCUDA.cmake`. Newer versions support CUDA natively as
    a language just like C++ or C.
    
    [ghstack-poisoned]

--- cmake/public/utils.cmake
+++ cmake/public/utils.cmake
@@ -469,13 +469,13 @@ function(torch_compile_options libname)
     # Unfortunately, hidden visibility messes up some ubsan warnings because
     # templated classes crossing library boundary get duplicated (but identical)
     # definitions. It's easier to just disable it.
-    target_compile_options(${libname} PRIVATE "-fvisibility=hidden")
+    target_compile_options(${libname} PRIVATE
+	    $<$<COMPILE_LANGUAGE:CXX,C>: -fvisibility=hidden>)
   endif()
 
   # Use -O2 for release builds (-O3 doesn't improve perf, and -Os results in perf regression)
   target_compile_options(${libname} PRIVATE
-      $<$<COMPILE_LANGUAGE:C++>:
-        $<OR:$<CONFIG:Release>,$<CONFIG:RelWithDebInfo>>:-O2>)
+      $<$<AND:$<COMPILE_LANGUAGE:CXX,C>,$<OR:$<CONFIG:Release>,$<CONFIG:RelWithDebInfo>>>:-O2>)
 
 endfunction()
 
