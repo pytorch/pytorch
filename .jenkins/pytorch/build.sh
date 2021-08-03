ab8af155455bc9148234b4a595a6e3280478478d
Raghavan Raman <raghavanr@fb.com>
2021-08-03 08:51:12 -0700

    [Static Runtime] Enabled building Static Runtime tests and benchmarks in OSS CI (#62226)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/62226
    
    Test Plan: Imported from OSS
    
    Reviewed By: malfet
    
    Differential Revision: D29923800
    
    Pulled By: navahgar
    
    fbshipit-source-id: 33cfe0e92a34c7140ea762e5715301cfbf401434

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -210,6 +210,12 @@ if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-py3.6-gcc7-build* || "${BUILD_ENVI
   export USE_GLOO_WITH_OPENSSL=ON
 fi
 
+if [[ "${BUILD_ENVIRONMENT}" == pytorch-linux-xenial-py3* ]]; then
+  if [[ "${BUILD_ENVIRONMENT}" != *android* && "${BUILD_ENVIRONMENT}" != *cuda* ]]; then
+    export BUILD_STATIC_RUNTIME_BENCHMARK=ON
+  fi
+fi
+
 if [[ "$BUILD_ENVIRONMENT" == *-bazel-* ]]; then
   set -e
 
