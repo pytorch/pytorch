01e3d2a81aa6b54adf6507c31dc4ce063fe1cf93
Ivan Kobzarev <ivankobzarev@fb.com>
2021-08-03 16:32:41 -0700

    Revert "Remove CUDA10.2 + gcc 9 in CI (#62609)"
    This reverts commit 76c447a730c77df9c571605d6b589db9876b4c3f.
    
    [ghstack-poisoned]

--- .circleci/cimodel/data/simple/docker_definitions.py
+++ .circleci/cimodel/data/simple/docker_definitions.py
@@ -6,6 +6,7 @@ from cimodel.data.simple.util.branch_filters import gen_filter_dict, RC_PATTERN
 
 # TODO: make this generated from a matrix rather than just a static list
 IMAGE_NAMES = [
+    "pytorch-linux-bionic-cuda10.2-cudnn7-py3.8-gcc9",
     "pytorch-linux-bionic-cuda10.2-cudnn7-py3.9-gcc7",
     "pytorch-linux-bionic-py3.6-clang9",
     "pytorch-linux-bionic-cuda10.2-cudnn7-py3.6-clang9",
--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -200,6 +200,16 @@ case "$image" in
     VISION=yes
     BREAKPAD=yes
     ;;
+  pytorch-linux-bionic-cuda10.2-cudnn7-py3.8-gcc9)
+    CUDA_VERSION=10.2
+    CUDNN_VERSION=7
+    ANACONDA_PYTHON_VERSION=3.8
+    GCC_VERSION=9
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    BREAKPAD=yes
+    ;;
   pytorch-linux-bionic-cuda10.2-cudnn7-py3.9-gcc7)
     CUDA_VERSION=10.2
     CUDNN_VERSION=7
