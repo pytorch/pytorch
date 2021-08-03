9727a22218003749c2ea1545033bbe394d5460ff
Kevin Tse <ktse@fb.com>
2021-08-03 13:01:09 -0700

    Update on "Codegen + Structured Kernels Lab Exercise"
    This PR is an onboarding exercise and will not be merged to master.
    
    This is an exercise to update the `add` operator to bypass the dispatcher to observe the differences in behavior and performance. As a result of bypassing the dispatcher, many functionalities (e.g. autograd) will be lost and many of the CI tests will fail as expected for the PR.
    
    [ghstack-poisoned]

--- test/test_timing.py
+++ test/test_timing.py
@@ -1,12 +1,24 @@
-from torch.utils.benchmark import Timer
+from torch.utils.benchmark import Language, Timer
 
 timer = Timer(
     stmt="x + y",
     setup="""
-        x = torch.ones((16,))
-        y = torch.ones((16,))
+        x = torch.ones(0)
+        y = torch.ones(0)
     """
+)
+
+cpp_timer = Timer(
+    "x + y;",
+    """
+        auto x = torch::ones({0});
+        auto y = torch::ones({0});
+    """,
+    language=Language.CPP,
 )
 
 stats = timer.collect_callgrind()
 print(stats)
+
+cpp_stats = cpp_timer.collect_callgrind()
+print(cpp_stats)
