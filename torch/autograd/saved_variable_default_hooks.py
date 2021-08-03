9beb279d848e9b141027a0f3e395b8329bf1733e
Victor Quach <quach@fb.com>
2021-08-03 13:07:01 -0700

    Add context manager to save tensors on CPU (#61928)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61928
    
    Fix #57100.
    Creates a function `torch.autograd.graph.set_save_on_cpu_hooks()` which can be used to register default hooks under which all tensors saved during the forward pass are actually copied* to cpu, then copied back to the appropriate device for the backward pass.
    
    *If the tensor was already on cpu, the entire operation is a no op.
    
    If the tensor is on GPU, we copy the tensor to `pin_memory` during packing so that the unpacking can be done asynchronously.
    
    See [benchmark](https://github.com/pytorch/pytorch/pull/61928#issuecomment-885089279) and [note about training large models](https://github.com/pytorch/pytorch/pull/61928#issuecomment-887009448)
    
    Test Plan: Imported from OSS
    
    Reviewed By: soulitzer
    
    Differential Revision: D29848526
    
    Pulled By: Varal7
    
    fbshipit-source-id: 3d289cddd4fa377bd4884ba0d569fa47c777d9e5

--- torch/autograd/saved_variable_default_hooks.py
+++ torch/autograd/saved_variable_default_hooks.py
@@ -1,7 +1,35 @@
 import torch
+from typing import Any
 
 def set_saved_tensors_default_hooks(pack_hook, unpack_hook):
     torch._C._autograd._register_default_hooks(pack_hook, unpack_hook)
 
 def reset_saved_tensors_default_hooks():
     torch._C._autograd._reset_default_hooks()
+
+class save_on_cpu(object):
+    def __init__(self, pin_memory=False):
+        def pack_to_cpu(tensor):
+            if not pin_memory:
+                return (tensor.device, tensor.cpu())
+
+            storage = torch.empty(
+                tensor.size(),
+                dtype=tensor.dtype,
+                layout=tensor.layout,
+                pin_memory=(torch.cuda.is_available() and not tensor.is_sparse))
+            storage.copy_(tensor)
+            return (tensor.device, storage)
+
+        def unpack_from_cpu(packed):
+            device, tensor = packed
+            return tensor.to(device, non_blocking=pin_memory)
+
+        self.pack_hook = pack_to_cpu
+        self.unpack_hook = unpack_from_cpu
+
+    def __enter__(self):
+        torch._C._autograd._register_default_hooks(self.pack_hook, self.unpack_hook)
+
+    def __exit__(self, *args: Any):
+        torch._C._autograd._reset_default_hooks()
