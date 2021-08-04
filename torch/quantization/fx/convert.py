d5882f5e7e7a897b6bdc47f53fb588aa0f657965
Jerry Zhang <jerryzh168@gmail.com>
2021-08-03 18:38:46 -0700

    [quant][graphmode][fx] Add reference option support for binary ops
    Summary:
    We also removed the special handling in match_utils for binary ops
    
    Test Plan:
    python test/test_quantize.py TestQuantizeFx
    python test/test_quantize.py TestQuantizeFxOps
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/quantization/fx/convert.py
+++ torch/quantization/fx/convert.py
@@ -335,11 +335,11 @@ def convert(model: GraphModule, is_reference: bool = False,
         else:
             return False
 
-    def is_output_quantized(node: Node, obj: QuantizeHandler, qconfig: QConfigAny, modules: Dict[str, torch.nn.Module]) -> bool:
+    def is_output_quantized(node: Node, obj: QuantizeHandler, qconfig: QConfigAny, modules: Dict[str, torch.nn.Module], is_reference=False) -> bool:
         """ Check if output node is quantized or not """
         assert modules is not None
         # by default the output for a quantizable node is expected to be quantized
-        quantized = True
+        quantized = obj.is_output_quantized(qconfig, is_reference)
 
         # Need to get correct quantized/non-quantized state forn the output
         # of FixedQParamsQuantizeHandler
@@ -454,7 +454,7 @@ def convert(model: GraphModule, is_reference: bool = False,
                     node, qconfig, modules, quantized_graph, node_name_to_scope, load_arg, is_reference=is_reference,
                     convert_custom_config_dict=convert_custom_config_dict)
                 if not is_observed_standalone_module_node:
-                    quantized = is_output_quantized(node, obj, qconfig, modules)
+                    quantized = is_output_quantized(node, obj, qconfig, modules, is_reference)
 
             if quantized:
                 env[node.name][activation_dtype(qconfig)] = result
