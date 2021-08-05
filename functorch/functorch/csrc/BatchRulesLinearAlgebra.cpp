7b41b4d3d7dc3f7bd2e602760488f4fcbb98d8ae
Richard Zou <zou3519@gmail.com>
2021-08-05 15:08:04 -0700

    [functorch] Some easy backward batching rules

--- functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
+++ functorch/functorch/csrc/BatchRulesLinearAlgebra.cpp
@@ -141,6 +141,7 @@ TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   VMAP_SUPPORT("slogdet", slogdet_batch_rule);
   OP_DECOMPOSE(einsum);
   OP_DECOMPOSE(outer);
+  OP_DECOMPOSE(ger);
   m.impl("linear", linear_decomp);
   VMAP_SUPPORT("cholesky_inverse", VARIADIC_BDIMS_BATCH_RULE(at::cholesky_inverse));
   VMAP_SUPPORT("pinverse", VARIADIC_BDIMS_BATCH_RULE(at::pinverse));
