fa29183d2c243898cc3170e48ff0a3ab5b0341d5
Thomas J. Fan <thomasjpfan@gmail.com>
2021-08-05 12:28:23 -0400

    ENH Migrate nll_loss2d from THC to ATen

--- aten/src/THCUNN/generic/THCUNN.h
+++ aten/src/THCUNN/generic/THCUNN.h
@@ -26,27 +26,6 @@ TORCH_CUDA_CU_API void THNN_(MultiMarginCriterion_updateGradInput)(
     THCTensor* weights, // [OPTIONAL]
     accreal margin);
 
-TORCH_CUDA_CU_API void THNN_(SpatialClassNLLCriterion_updateOutput)(
-    THCState* state,
-    THCTensor* input,
-    THCIndexTensor* target,
-    THCTensor* output,
-    int64_t reduction,
-    THCTensor* weights, // [OPTIONAL]
-    THCTensor* total_weight,
-    int64_t ignore_index);
-
-TORCH_CUDA_CU_API void THNN_(SpatialClassNLLCriterion_updateGradInput)(
-    THCState* state,
-    THCTensor* input,
-    THCIndexTensor* target,
-    THCTensor* gradOutput,
-    THCTensor* gradInput,
-    int64_t reduction,
-    THCTensor* weights, // [OPTIONAL]
-    THCTensor* total_weight,
-    int64_t ignore_index);
-
 TORCH_CUDA_CU_API void THNN_(SpatialConvolutionMM_updateOutput)(
     THCState* state,
     THCTensor* input,
