78fbe22be5a9b4cdbbdd02f69ea4ad4df2c6d721
Eddie Yan <eddiey@nvidia.com>
2021-08-06 19:27:30 +0000

    change nccl version reporting

--- torch/csrc/cuda/nccl.cpp
+++ torch/csrc/cuda/nccl.cpp
@@ -329,7 +329,10 @@ bool is_available(TensorList tensors) {
 
 std::uint64_t version() {
 #if defined(NCCL_MAJOR)
-  return NCCL_MAJOR * 1000 + NCCL_MINOR * 100 + NCCL_PATCH;
+  constexpr std::uint64_t ver = (((uint64_t) NCCL_MAJOR) << 32) |
+				(((uint64_t) NCCL_MINOR) << 16) |
+				 ((uint64_t) NCCL_PATCH);
+  return ver;
 #elif defined(USE_NCCL)
   return 1000;
 #else
