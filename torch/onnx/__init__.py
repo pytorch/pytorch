04b115ac32a4cb817a681da7227b7d0f885a2721
Jay Zhang <36183870+fatcat-z@users.noreply.github.com>
2021-08-06 08:24:37 +0800

    [ONNX] Remove strip_doc_string param from torch.onnx.export() function. (#61712)
    As of now, the "strip_doc_string" parameter was described as below:
    
    strip_doc_string (bool, default True): do not include the field
    doc_string``` from the exported model. Otherwise the field will mention the source code locations for model``.
    
    This is usually useless to users who want to transform a PyTorch model to ONNX one. Only when the user wants to debug the export process, these source code locations could provide benefits.
    
    To make the export() function more friendly by providing less parameters, we combined "strip_doc_string" into "verbose" parameter. If a user set verbose to True, it means the users need some log information for debugging the export process and this is similar with the purpose of strip_doc_string parameter.
    
    But the usage of these 2 arguments are opposite: setting verbose to True means we want to print log information to help debug, which means strip_doc_string should be False. And this is how we replace strip_doc_string with verbose argument in this PR.
    
    This PR will still keep it in torch.onnx.export() function for backward support while the usage of it has been combined with verbose argument.
    
    Co-authored-by: fatcat-z <zhang-ji@outlook.com>

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -108,7 +108,9 @@ def export(model, args, f, export_params=True, verbose=False, training=TrainingM
             Besides, if this is False, any optimization that may adjust graph inputs will
             be skipped - for example, Conv and BatchNorm fusion.
         verbose (bool, default False): if True, prints a description of the
-            model being exported to stdout.
+            model being exported to stdout. In addition, the final ONNX graph will include the
+            field ``doc_string``` from the exported model which mentions the source code locations
+            for ``model``.
         training (enum, default TrainingMode.EVAL):
             * ``TrainingMode.EVAL``: export the model in inference mode. In this case, optimizations
               (e.g., fusing Conv and BatchNorm ops) may adjust graph inputs by modifying model params
@@ -198,9 +200,7 @@ def export(model, args, f, export_params=True, verbose=False, training=TrainingM
             Must be provided when exporting a ScriptModule or ScriptFunction, ignored otherwise.
             Used to determine the type and shape of the outputs without tracing the execution of
             the model. A single object is treated as equivalent to a tuple of one element.
-        strip_doc_string (bool, default True): do not include the field
-            ``doc_string``` from the exported model. Otherwise the field will mention the source
-            code locations for ``model``.
+        strip_doc_string (bool, default True): [Deprecated and ignored. Will be removed in next PyTorch release]
         dynamic_axes (dict<string, dict<int, string>> or dict<string, list(int)>, default empty dict):
 
             By default the exported model will have the shapes of all input and output tensors
