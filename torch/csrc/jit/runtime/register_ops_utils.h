d67963a21439a5cc098b3d740fb92c9fdbfd451f
Nikita Shulga <nshulga@fb.com>
2021-08-09 12:57:05 -0700

    Mark unused functions with `C10_UNUSED` (#62929)
    Summary:
    Which fixes number of warnings
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/62929
    
    Reviewed By: walterddr, albanD
    
    Differential Revision: D30171953
    
    Pulled By: malfet
    
    fbshipit-source-id: f82475289ff4aebb0c97794114e94a24d00d2ff4

--- torch/csrc/jit/runtime/register_ops_utils.h
+++ torch/csrc/jit/runtime/register_ops_utils.h
@@ -89,8 +89,7 @@ IValue tensorToListRecursive(
     at::IntArrayRef strides,
     size_t element_size);
 
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-static int64_t floordiv(int64_t a, int64_t b) {
+static C10_UNUSED int64_t floordiv(int64_t a, int64_t b) {
   if (b == 0) {
     throw std::runtime_error("division by 0");
   }
@@ -104,19 +103,16 @@ static int64_t floordiv(int64_t a, int64_t b) {
   }
 }
 TORCH_API void checkDoubleInRange(double a);
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-static int64_t floor(double a) {
+static C10_UNUSED int64_t floor(double a) {
   checkDoubleInRange(a);
   return std::floor(a);
 }
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-static int64_t ceil(double a) {
+static C10_UNUSED int64_t ceil(double a) {
   checkDoubleInRange(a);
   return std::ceil(a);
 }
 
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-static int64_t gcd(int64_t a, int64_t b) {
+static C10_UNUSED int64_t gcd(int64_t a, int64_t b) {
   while (b != 0) {
     int64_t r = a % b;
     a = b;
@@ -139,8 +135,7 @@ double degrees(double x);
 double radians(double x);
 
 // reference function THPVariable_to in python_variable_methods.cpp
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-static at::Tensor to_dispatch(
+static C10_UNUSED at::Tensor to_dispatch(
     at::Tensor self,
     c10::optional<at::Device> device,
     c10::optional<at::ScalarType> scalarType,
