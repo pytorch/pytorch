89124ce3315603f120b5847910e70528069d9d1c
Joel Benjamin Schlosser <jbschlosser@fb.com>
2021-08-09 12:37:52 -0400

    Operate correctly with strict=True

--- test/test_nn.py
+++ test/test_nn.py
@@ -5462,6 +5462,71 @@ class TestNN(NNTestCase):
         self.assertEqual(mm[0].param[0].item(), 10)
         self.assertEqual(mm[0].sub.weight[0, 0].item(), 555)
 
+    def test_extra_state(self):
+
+        class SubModule(torch.nn.Module):
+            def __init__(self, foo):
+                super().__init__()
+                self.foo = foo
+
+            def get_extra_state(self):
+                return {
+                    'foo': self.foo
+                }
+
+            def set_extra_state(self, state):
+                self.foo = state['foo']
+
+        class MyModule(torch.nn.Module):
+            def __init__(self, foo, bar):
+                super().__init__()
+                self.sub = SubModule(foo)
+                self.bar = bar
+
+            def get_extra_state(self):
+                return {
+                    'bar': self.bar
+                }
+
+            def set_extra_state(self, state):
+                self.bar = state['bar']
+
+        # Ensure state_dict contains the extra state by loading it into another module.
+        m = MyModule(3, 'something')
+        m2 = MyModule(5, 'something else')
+        m2.load_state_dict(m.state_dict())
+        self.assertEqual(m.state_dict(), m2.state_dict())
+        self.assertEqual(m2.bar, m.bar)
+        self.assertEqual(m2.sub.foo, m.sub.foo)
+
+    def test_extra_state_missing_set_extra_state(self):
+
+        class MyModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def get_extra_state(self):
+                return {
+                    'foo': 5
+                }
+
+        m = MyModule()
+        with self.assertRaisesRegex(RuntimeError, 'Unexpected key'):
+            m.load_state_dict(m.state_dict())
+
+    def test_extra_state_missing_get_extra_state(self):
+
+        class MyModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def set_extra_state(self):
+                pass
+
+        m = MyModule()
+        with self.assertRaisesRegex(RuntimeError, 'Missing key'):
+            m.load_state_dict(m.state_dict())
+
     def test_parameter_assignment(self):
         l = nn.Linear(5, 5)
 
--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -1228,11 +1228,10 @@ class Module:
         for name, buf in self._buffers.items():
             if buf is not None and name not in self._non_persistent_buffers_set:
                 destination[prefix + name] = buf if keep_vars else buf.detach()
-        extra_state = self.get_extra_state()
-        if extra_state:
+        if hasattr(self, 'get_extra_state'):
             EXTRA_STATE_KEY = prefix + '_extra_state'
             destination[EXTRA_STATE_KEY] = OrderedDict()
-            for name, obj in extra_state.items():
+            for name, obj in self.get_extra_state().items():
                 destination[EXTRA_STATE_KEY][name] = obj
 
     # The user can pass an optional arbitrary mappable object to `state_dict`, in which case `state_dict` returns
@@ -1372,8 +1371,13 @@ class Module:
                 missing_keys.append(key)
 
         EXTRA_STATE_KEY = prefix + '_extra_state'
-        if EXTRA_STATE_KEY in state_dict:
-            self.set_extra_state(state_dict[EXTRA_STATE_KEY])
+        if hasattr(self, 'set_extra_state'):
+            if EXTRA_STATE_KEY in state_dict:
+                self.set_extra_state(state_dict[EXTRA_STATE_KEY])
+            elif strict:
+                missing_keys.append(EXTRA_STATE_KEY)
+        elif strict and (EXTRA_STATE_KEY in state_dict):
+            unexpected_keys.append(EXTRA_STATE_KEY)
 
         if strict:
             for key in state_dict.keys():
@@ -1761,12 +1765,6 @@ class Module:
         strings are acceptable.
         """
         return ''
-
-    def get_extra_state(self):
-        return {}
-
-    def set_extra_state(self, state):
-        pass
 
     def __repr__(self):
         # We treat the extra repr like the sub-module, one item per line
