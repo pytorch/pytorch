5b5b80974168e9be188102f334f5f1eb4f9658b2
Elias Ellison <eellison@devfair044.h1.fair>
2021-08-09 21:24:16 +0000

    Update base for Update on "Only optimize after change (redo)"
    Some how this didnt get landed previously in ghstack mixup
    
    Differential Revision: [D30200060](https://our.internmc.facebook.com/intern/diff/D30200060)
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/register_prim_ops_fulljit.cpp
+++ torch/csrc/jit/runtime/register_prim_ops_fulljit.cpp
@@ -372,8 +372,7 @@ RegisterOperators logging_operators(
          },
          aliasAnalysisFromSchema())});
 
-// NOLINTNEXTLINE(clang-diagnostic-unused-function)
-void hashValue(Stack* stack) {
+C10_UNUSED void hashValue(Stack* stack) {
   auto value = pop(stack);
   push(stack, value.hash());
 }
