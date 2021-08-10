f307120df49d00ffac63dbdd4f84726a1d2c0c19
tktrungna <tktrungna@gmail.com>
2021-08-10 16:24:57 -0700

    Update test_torch_deploy (#62838)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/62838
    
    Fixes #62380
    
    * update test functions to call wheel install folder {sitepackages}/torch instead of build/ folder
    * add symbolic link for shared libraries which are called by the tests (this is a bit hacky and should be fixed the rpath before compiling -- similar to https://github.com/pytorch/pytorch/blob/master/.jenkins/pytorch/test.sh#L204-L208).
    
    ### Test plan
    check if all ci workflows pass
    
    Test Plan: Imported from OSS
    
    Reviewed By: walterddr
    
    Differential Revision: D30193141
    
    Pulled By: tktrungna
    
    fbshipit-source-id: 72c2bd3a740fca0f72e4803df505240193692c44

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -474,7 +474,10 @@ test_vec256() {
 
 test_torch_deploy() {
   python torch/csrc/deploy/example/generate_examples.py
-  build/bin/test_deploy
+  ln -sf "$TORCH_LIB_DIR"/libtorch* "$TORCH_BIN_DIR"
+  ln -sf "$TORCH_LIB_DIR"/libshm* "$TORCH_BIN_DIR"
+  ln -sf "$TORCH_LIB_DIR"/libc10* "$TORCH_BIN_DIR"
+  "$TORCH_BIN_DIR"/test_deploy
   assert_git_not_dirty
 }
 
