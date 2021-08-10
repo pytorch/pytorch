fe0e6d27f9a34836f7d92093706f3e9ec63af6eb
tktrungna <tktrungna@gmail.com>
2021-08-10 16:24:57 -0700

    update test_vulkan (#62795)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/62795
    
    Test Plan: Imported from OSS
    
    Reviewed By: driazati
    
    Differential Revision: D30124421
    
    Pulled By: tktrungna
    
    fbshipit-source-id: 235ba166b02f7334e89cb2493024067851bf5b9b

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -278,12 +278,14 @@ test_libtorch() {
 
 test_vulkan() {
   if [[ "$BUILD_ENVIRONMENT" == *vulkan-linux* ]]; then
+    ln -sf "$TORCH_LIB_DIR"/libtorch* "$TORCH_TEST_DIR"
+    ln -sf "$TORCH_LIB_DIR"/libc10* "$TORCH_TEST_DIR"
     export VK_ICD_FILENAMES=/var/lib/jenkins/swiftshader/build/Linux/vk_swiftshader_icd.json
     # NB: the ending test_vulkan must match the current function name for the current
     # test reporting process (in print_test_stats.py) to function as expected.
     TEST_REPORTS_DIR=test/test-reports/cpp-vulkan/test_vulkan
     mkdir -p $TEST_REPORTS_DIR
-    build/bin/vulkan_test --gtest_output=xml:$TEST_REPORTS_DIR/vulkan_test.xml
+    "$TORCH_TEST_DIR"/vulkan_test --gtest_output=xml:$TEST_REPORTS_DIR/vulkan_test.xml
   fi
 }
 
