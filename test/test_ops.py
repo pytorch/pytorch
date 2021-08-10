04ee88287c9f670e9591479c92b242b98488f614
Elias Ellison <eellison@devfair044.h1.fair>
2021-08-10 09:40:41 -0700

    Test shape analysis with opinfos (#59814)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59814
    
    Using opinfos to test shape analysis. By default, we just check that we don't give incorrect answers, and then if `assert_jit_shape_analysis` is true, tests that we correctly propagates the full shape. and it found a couple bugs {emoji:1f603}
    
    Test Plan: Imported from OSS
    
    Reviewed By: Krovatkin
    
    Differential Revision: D30200058
    
    Pulled By: eellison
    
    fbshipit-source-id: 6226be87f5390277cfa5a1fffaa1b072d4bc8803

--- test/test_ops.py
+++ test/test_ops.py
@@ -751,7 +751,8 @@ class TestJit(JitCommonTestCase):
 
                     # Check traced forward, grad, and grad grad
                     # TODO: fix tracing here
-                    if not has_fake_function:
+                    supports_tracing = not has_fake_function
+                    if supports_tracing:
                         traced_fn = create_traced_fn(self, variant)
                         check_against_reference(self,
                                                 traced_fn,
@@ -763,11 +764,23 @@ class TestJit(JitCommonTestCase):
 
                     # Check alias annotation schema for correctness (make
                     #   sure inputs that aren't supposed to be modified aren't)
-                    # Note: only runs in float32 and int64 because schema isn't affected by dtype,
+                    # Note: only runs in float32 because schema isn't affected by dtype,
                     #   so running it on all dtypes is would be excessive
-                    if dtype in [torch.float32, torch.int32]:
+                    if dtype == torch.float32:
                         check_alias_annotation(name, (get_sample(),) + sample.args, sample.kwargs,
                                                func_type=func_type, aten_name=op.aten_name)
+
+                        # TODO: use script graph as well
+                        checked_shape_analysis = False
+                        if supports_tracing:
+                            out = variant(get_sample(), *sample.args, **sample.kwargs)
+
+                            # TODO: handle multiple outputs
+                            if isinstance(out, torch.Tensor):
+                                self.checkShapeAnalysis(out.size(), traced_fn.graph, op.assert_jit_shape_analysis)
+                                checked_shape_analysis = True
+                        if op.assert_jit_shape_analysis:
+                            self.assertTrue(checked_shape_analysis)
 
                     # Check autodifferentiation of nodes for traced and scripted graphs, only need to check once per sample
                     if dtype is torch.float32:
@@ -780,7 +793,7 @@ class TestJit(JitCommonTestCase):
                             nonfusible_nodes = op.autodiff_nonfusible_nodes
                             fusible_nodes = op.autodiff_fusible_nodes
 
-                        if not has_fake_function:
+                        if supports_tracing:
                             self.assertAutodiffNode(traced_fn.last_graph, op.assert_autodiffed, nonfusible_nodes, fusible_nodes)
                         self.assertAutodiffNode(script_fn.last_graph, op.assert_autodiffed, nonfusible_nodes, fusible_nodes)
         assert tested, "JIT Test does not execute any logic"
