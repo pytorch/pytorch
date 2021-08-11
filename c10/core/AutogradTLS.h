f1c8e90d488bb4d682f5e8271404f1340fc52a54
albanD <desmaison.alban@gmail.com>
2021-08-11 19:19:59 -0400

    Update base for Update on "remove special grad_mode tls handling"
    This PR removes the special flag to disable grad mode tracking on the ThreadLocalState and replaces it with an explicit setter that users can use.
    This allows to reduce complexity of ThreadLocalState.
    
    [ghstack-poisoned]

--- c10/core/AutogradTLS.h
+++ c10/core/AutogradTLS.h
@@ -12,16 +12,13 @@ struct TORCH_API AutogradTLS {
   static uint8_t get_mode();
   static void set_mode(uint8_t flag);
   static void set_grad_mode(bool enabled);
-  static void set_fw_grad_mode(bool enabled);
   static void set_inference_mode(bool enabled);
   static bool get_grad_mode();
-  static bool get_fw_grad_mode();
   static bool get_inference_mode();
 
 
   static const uint8_t GRAD_MODE_MASK = 1;
   static const uint8_t INFERENCE_MODE_MASK = 2;
-  static const uint8_t FW_GRAD_MODE_MASK = 4;
 };
 
 } // namespace c10
