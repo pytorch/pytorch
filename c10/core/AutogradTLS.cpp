7348602543c26b813730294ed49d703e5c8a1b31
albanD <desmaison.alban@gmail.com>
2021-08-11 19:19:59 -0400

    Update on "Add a common autograd TLS state"
    This PR collapses the GradMode and InferenceMode thread local booleans into a single thread local uint8.
    This helps reducing the number of thread local variable accesses done when we propagate ThreadLocalStates.
    
    Note that this is even more beneficial as we will add a forward mode AD TLS (similar to GradMode) higher in this stack and this new structure should reduce the perf impact of adding this new TLS.
    
    Benchmark is coming.
    
    [ghstack-poisoned]

--- c10/core/AutogradTLS.cpp
+++ c10/core/AutogradTLS.cpp
@@ -2,8 +2,8 @@
 
 namespace c10 {
 
-// By default, both grad modes are enabled and inference mode is disabled
-thread_local uint8_t autograd_mode_bitfield = AutogradTLS::GRAD_MODE_MASK | AutogradTLS::FW_GRAD_MODE_MASK;
+// By default, grad mode is enabled and inference mode is disabled
+thread_local uint8_t autograd_mode_bitfield = AutogradTLS::GRAD_MODE_MASK;
 
 uint8_t AutogradTLS::get_mode() {
   return autograd_mode_bitfield;
@@ -21,14 +21,6 @@ void AutogradTLS::set_grad_mode(bool enabled) {
   }
 }
 
-void AutogradTLS::set_fw_grad_mode(bool enabled) {
-  if (enabled) {
-    autograd_mode_bitfield |= AutogradTLS::FW_GRAD_MODE_MASK;
-  } else {
-    autograd_mode_bitfield &= ~AutogradTLS::FW_GRAD_MODE_MASK;
-  }
-}
-
 void AutogradTLS::set_inference_mode(bool enabled) {
   if (enabled) {
     autograd_mode_bitfield |= AutogradTLS::INFERENCE_MODE_MASK;
@@ -39,10 +31,6 @@ void AutogradTLS::set_inference_mode(bool enabled) {
 
 bool AutogradTLS::get_grad_mode() {
   return AutogradTLS::get_mode() & AutogradTLS::GRAD_MODE_MASK;
-}
-
-bool AutogradTLS::get_fw_grad_mode() {
-  return AutogradTLS::get_mode() & AutogradTLS::FW_GRAD_MODE_MASK;
 }
 
 bool AutogradTLS::get_inference_mode() {
