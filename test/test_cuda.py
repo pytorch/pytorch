a800387d362a4e56889fc6395d02a63607b38d93
Arindam Roy <rarindam@gmail.com>
2021-08-10 19:21:41 -0700

    ROCM: Skip mem leak test as its flaky in ROCM
    https://github.com/pytorch/pytorch/issues/62533
    In very rare cases in CI, sometimes memory leak
    is detected leading to CI failures.
    
    Internally we have not been able to reproduce the
    same, and so prevent CI faulures, we are skipping
    the test.
    
    Signed-off-by: Arindam Roy <rarindam@gmail.com>

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1636,6 +1636,10 @@ except RuntimeError as e:
         self.assertEqual(a.norm(p=0, dtype=torch.float32), 65536)
 
     # Test that wrap_with_cuda_memory_check successfully detects leak
+
+    # test is flaky in ROCM
+    # https://github.com/pytorch/pytorch/issues/62533
+    @skipIfRocm
     def test_cuda_memory_leak_detection(self):
         l = []
 
