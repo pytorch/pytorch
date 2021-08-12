8cb6ae915703dda096cf1f0eb31a959f1b58195b
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-08-12 13:23:13 -0700

    [DOC] fix torch.randint doc
    [ghstack-poisoned]

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -8840,8 +8840,8 @@ Keyword args:
 
 add_docstr(
     torch.randint,
-    """
-randint(low=0, high, size, \\*, generator=None, out=None, \
+    r"""
+randint(low=0, high, size, *, generator=None, out=None, \
 dtype=None, layout=torch.strided, device=None, requires_grad=False) -> Tensor
 
 Returns a tensor filled with random integers generated uniformly
