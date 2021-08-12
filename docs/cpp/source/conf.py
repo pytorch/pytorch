ec7dc4f62a5df15f953608f92b06bc51e6c79d7e
anjali411 <chourdiaanjali123@gmail.com>
2021-08-12 11:51:08 -0700

    Update on "Add copy button to code snippets in docs"
    [ghstack-poisoned]

--- docs/cpp/source/conf.py
+++ docs/cpp/source/conf.py
@@ -25,7 +25,7 @@ import textwrap
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8'
+needs_sphinx = '3.5.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
