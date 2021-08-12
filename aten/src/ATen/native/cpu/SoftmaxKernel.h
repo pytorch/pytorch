1204fc026ca4de77273247a52acde0a5f28cd6b5
Meghan Lele <meghanl@fb.com>
2021-08-11 21:01:28 -0700

    Port `log_softmax_backward_data` to structured kernel (#62372)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/62372
    
    Test Plan: Imported from OSS
    
    Reviewed By: saketh-are
    
    Differential Revision: D30240242
    
    Pulled By: SplitInfinity
    
    fbshipit-source-id: 67d5e4b1543c2e43675e905ce18ca49c11e33748

--- aten/src/ATen/native/cpu/SoftmaxKernel.h
+++ aten/src/ATen/native/cpu/SoftmaxKernel.h
@@ -7,7 +7,7 @@ namespace at {
 namespace native {
 
 using forward_fn = void (*)(const Tensor&, const Tensor&);
-using backward_fn = void(*)(Tensor &, const Tensor &, const Tensor&);
+using backward_fn = void(*)(const Tensor &, const Tensor &, const Tensor&);
 
 DECLARE_DISPATCH(forward_fn, softmax_lastdim_kernel);
 DECLARE_DISPATCH(forward_fn, log_softmax_lastdim_kernel);
