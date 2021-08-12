c9ac43dbc2c223b8626366c94e3a5e43fad40dba
David Riazati <driazati@users.noreply.github.com>
2021-08-12 14:28:42 -0700

    [skip ci] Use CMake for breakpad

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1881,6 +1881,15 @@ set_target_properties(fmt-header-only PROPERTIES INTERFACE_COMPILE_FEATURES "")
 list(APPEND Caffe2_DEPENDENCY_LIBS fmt::fmt-header-only)
 set(BUILD_SHARED_LIBS ${TEMP_BUILD_SHARED_LIBS} CACHE BOOL "Build shared libs" FORCE)
 
+if(USE_BREAKPAD)
+  message(STATUS "using")
+  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/breakpad)
+
+else()
+  message(STATUS "not using")
+endif()
+message(FATAL "stopped")
+
 # ---[ Kineto
 # edge profiler depends on KinetoProfiler but it only does cpu
 # profiling. Thus we dont need USE_CUDA/USE_ROCM
