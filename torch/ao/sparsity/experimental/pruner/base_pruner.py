22d6c3c521a5ac55f6a47e025ad9d67dfb6d5637
Karen Zhou <kazhou@fb.com>
2021-08-12 13:23:02 -0700

    [pruner] amend base pruner API to match base sparsifier
    Update base pruner API to match base sparsifier API as defined in D28970960 / PR58955
    
    Changes include:
    - `enable_mask_update = True` in `__init__`
    - `prepare` takes model and config instead of constructor
    - convert functionality renamed to `squash_mask`, `convert` method call now raises Error
    - `activation_handles` ad `bias_handles` initialized in `_prepare` instead of constructor
    
    Differential Revision: [D30287179](https://our.internmc.facebook.com/intern/diff/D30287179/)
    
    [ghstack-poisoned]

--- torch/ao/sparsity/experimental/pruner/base_pruner.py
+++ torch/ao/sparsity/experimental/pruner/base_pruner.py
@@ -15,17 +15,17 @@ SUPPORTED_MODULES = {
     nn.Conv2d
 }
 
-def _module_to_path(model, layer, prefix=''):
+def _module_to_fqn(model, layer, prefix=''):
     for name, child in model.named_children():
         new_name = prefix + '.' + name
         if child is layer:
             return new_name
-        child_path = _module_to_path(child, layer, prefix=new_name)
+        child_path = _module_to_fqn(child, layer, prefix=new_name)
         if child_path is not None:
             return child_path
     return None
 
-def _path_to_module(model, path):
+def _fqn_to_module(model, path):
     path = path.split('.')
     for name in path:
         model = getattr(model, name, None)
@@ -53,43 +53,14 @@ class BasePruner(abc.ABC):
             be updated.
 
     """
-    def __init__(self, model, config, defaults):
+    def __init__(self, defaults):
         super().__init__()
-        self.config = config
         self.defaults = defaults
         if self.defaults is None:
             self.defaults = dict()
 
         self.module_groups = []
-        self.enable_mask_update = False
-        self.activation_handles = []
-        self.bias_handles = []
-
-        self.model = model
-        # If no config -- try getting all the supported layers
-        if self.config is None:
-            # Add all models to the config
-            self.config = []
-            stack = [model]
-            while stack:
-                module = stack.pop()
-                for name, child in module.named_children():
-                    if type(child) in SUPPORTED_MODULES:
-                        self.config.append(child)
-                    else:
-                        stack.append(child)
-
-        for module_config in self.config:
-            if isinstance(module_config, nn.Module):
-                module_config = {'module': module_config}
-            local_args = copy.deepcopy(self.defaults)
-            local_args.update(module_config)
-            module = local_args['module']
-            module_path = _module_to_path(self.model, module)
-            if module_path and module_path[0] == '.':
-                module_path = module_path[1:]
-            local_args['path'] = module_path
-            self.module_groups.append(local_args)
+        self.enable_mask_update = True
 
     def __getstate__(self):
         return {
@@ -122,12 +93,15 @@ class BasePruner(abc.ABC):
             output += bias
         return output
 
-    def prepare(self, use_path=False, *args, **kwargs):
+    def _prepare(self, use_path=False, *args, **kwargs):
         r"""Adds mask parametrization to the layer weight
         """
+        self.activation_handles = []  # store removable hook handles
+        self.bias_handles = []
+
         for config in self.module_groups:
             if use_path:
-                module = _path_to_module(self.model, config['path'])
+                module = _fqn_to_module(self.model, config['fqn'])
             else:
                 module = config['module']
 
@@ -152,10 +126,46 @@ class BasePruner(abc.ABC):
                 module.bias = None
             self.bias_handles.append(module.register_forward_hook(self.bias_hook))
 
-    def convert(self, use_path=False, *args, **kwargs):
+    def prepare(self, model, config):
+        r"""Prepares a model, by adding the parametrizations.
+        Note::
+            The model is modified inplace. If you need to preserve the original
+            model, use copy.deepcopy.
+        """
+        self.model = model  # TODO: Need to figure out how to load without this.
+        self.config = config
+
+        # If no config -- try getting all the supported layers
+        if self.config is None:
+            # Add all models to the config
+            self.config = []
+            stack = [model]
+            while stack:
+                module = stack.pop()
+                for name, child in module.named_children():
+                    if type(child) in SUPPORTED_MODULES:
+                        self.config.append(child)
+                    else:
+                        stack.append(child)
+
+        for module_config in self.config:
+            if isinstance(module_config, nn.Module):
+                module_config = {'module': module_config}
+            local_args = copy.deepcopy(self.defaults)
+            local_args.update(module_config)
+            module = local_args['module']
+            module_fqn = _module_to_fqn(model, module)
+            if module_fqn and module_fqn[0] == '.':
+                module_fqn = module_fqn[1:]
+            local_args['fqn'] = module_fqn
+            self.module_groups.append(local_args)
+
+        self._prepare()
+
+    def squash_mask(self, use_path=False, *args, **kwargs):
         for config in self.module_groups:
             if use_path:
-                module = _path_to_module(self.model, config['path'])
+                module = _fqn_to_module(self.model, config['fqn'])
             else:
                 module = config['module']
             parametrize.remove_parametrizations(module, 'weight',
@@ -166,13 +176,18 @@ class BasePruner(abc.ABC):
                 del module._buffers['mask']
             delattr(module, 'mask')
 
+    def convert(self):
+        # TODO: Call the torch.ao.utils.convert in here
+        raise NotImplementedError('`convert` is not implemented. Please, use '
+                                  '`torch.ao.utils.convert` instead.')
+
     def step(self, use_path=True):
         if not self.enable_mask_update:
             return
         with torch.no_grad():
             for config in self.module_groups:
                 if use_path:
-                    module = _path_to_module(self.model, config['path'])
+                    module = _fqn_to_module(self.model, config['fqn'])
                 else:
                     module = config['module']
                 self.update_mask(module, **config)
