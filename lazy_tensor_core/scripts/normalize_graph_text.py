39f1f0f267a2ce5fbe9dc5e5662afb506d27fde1
Jiewen Tan <jwtan@fb.com>
2021-08-11 22:20:54 -0700

    [LTC] Fix linter issues for lazy_tensor_core subdirectory
    Summary:
    Applies more autopep8.

--- lazy_tensor_core/scripts/normalize_graph_text.py
+++ lazy_tensor_core/scripts/normalize_graph_text.py
@@ -9,19 +9,19 @@ import sys
 
 
 def normalize(args):
-  fd = sys.stdin
-  if args.input:
-    fd = open(args.input)
-  for line in fd:
-    line.rstrip('\n')
-    m = re.match(r'(\s*)%\d+\s*=\s*(.*::[^(]+\()[^)]*(.*)', line)
-    if m:
-      line = m.group(1) + m.group(2) + m.group(3)
-    print(line)
+    fd = sys.stdin
+    if args.input:
+        fd = open(args.input)
+    for line in fd:
+        line.rstrip('\n')
+        m = re.match(r'(\s*)%\d+\s*=\s*(.*::[^(]+\()[^)]*(.*)', line)
+        if m:
+            line = m.group(1) + m.group(2) + m.group(3)
+        print(line)
 
 
 if __name__ == '__main__':
-  arg_parser = argparse.ArgumentParser()
-  arg_parser.add_argument('--input', type=str)
-  args = arg_parser.parse_args()
-  normalize(args)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--input', type=str)
+    args = arg_parser.parse_args()
+    normalize(args)
