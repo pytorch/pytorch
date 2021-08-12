3efea793b9f63c68e11b8ec7cad9641a967e831d
Jiewen Tan <jwtan@fb.com>
2021-08-11 22:10:20 -0700

    [LTC] Fix linter issues for lazy_tensor_core subdirectory
    Summary:
    This is the second commit that applies autopep8 to the entire subdirectory.

--- lazy_tensor_core/lazy_tensor_core/debug/graph_saver.py
+++ lazy_tensor_core/lazy_tensor_core/debug/graph_saver.py
@@ -10,20 +10,20 @@ _SAVE_GRAPH_IDS = collections.defaultdict(dict)
 
 
 def save_tensors_graph(save_dir, name, tensors):
-  fmt = os.environ.get('SAVE_GRAPH_FMT', 'text')
-  if fmt == 'text':
-    graph = lazy_tensor_core._LAZYC._get_ltc_tensors_text(tensors)
-  elif fmt == 'dot':
-    graph = lazy_tensor_core._LAZYC._get_ltc_tensors_dot(tensors)
-  elif fmt == 'backend':
-    graph = lazy_tensor_core._LAZYC._get_ltc_tensors_backend(tensors)
-  else:
-    raise RuntimeError('Invalid save graph format: {}'.format(fmt))
-  tid = threading.current_thread().ident
-  with _SAVE_GRAPH_LOCK:
-    tdict = _SAVE_GRAPH_IDS[tid]
-    id = tdict.get(name, 0)
-    tdict[name] = id + 1
-  fname = '{}-{}-{}.{}'.format(name, tid, id, fmt)
-  with open(os.path.join(save_dir, fname), 'w') as fd:
-    fd.write(graph)
+    fmt = os.environ.get('SAVE_GRAPH_FMT', 'text')
+    if fmt == 'text':
+        graph = lazy_tensor_core._LAZYC._get_ltc_tensors_text(tensors)
+    elif fmt == 'dot':
+        graph = lazy_tensor_core._LAZYC._get_ltc_tensors_dot(tensors)
+    elif fmt == 'backend':
+        graph = lazy_tensor_core._LAZYC._get_ltc_tensors_backend(tensors)
+    else:
+        raise RuntimeError('Invalid save graph format: {}'.format(fmt))
+    tid = threading.current_thread().ident
+    with _SAVE_GRAPH_LOCK:
+        tdict = _SAVE_GRAPH_IDS[tid]
+        id = tdict.get(name, 0)
+        tdict[name] = id + 1
+    fname = '{}-{}-{}.{}'.format(name, tid, id, fmt)
+    with open(os.path.join(save_dir, fname), 'w') as fd:
+        fd.write(graph)
--- lazy_tensor_core/lazy_tensor_core/utils/checkpoint_tagger.py
+++ lazy_tensor_core/lazy_tensor_core/utils/checkpoint_tagger.py
@@ -7,34 +7,34 @@ import json
 
 class CheckpointTagger(object):
 
-  def __init__(self, remover=None):
-    self._tags = dict()
-    self._refcount = collections.defaultdict(int)
-    remover = (lambda x: None) if remover is None else remover
-    assert callable(remover)
-    self._remover = remover
+    def __init__(self, remover=None):
+        self._tags = dict()
+        self._refcount = collections.defaultdict(int)
+        remover = (lambda x: None) if remover is None else remover
+        assert callable(remover)
+        self._remover = remover
 
-  def tag(self, name, path):
-    self._refcount[path] += 1
-    old_path = self._tags.get(name)
-    if old_path is not None:
-      self._refcount[old_path] -= 1
-      if self._refcount[old_path] == 0:
-        self._refcount.pop(old_path)
-        self._remover(old_path)
-    self._tags[name] = path
+    def tag(self, name, path):
+        self._refcount[path] += 1
+        old_path = self._tags.get(name)
+        if old_path is not None:
+            self._refcount[old_path] -= 1
+            if self._refcount[old_path] == 0:
+                self._refcount.pop(old_path)
+                self._remover(old_path)
+        self._tags[name] = path
 
-  @property
-  def tags(self):
-    return self._tags
+    @property
+    def tags(self):
+        return self._tags
 
-  def save_to_json(self):
-    return json.dumps(self._tags)
+    def save_to_json(self):
+        return json.dumps(self._tags)
 
-  @classmethod
-  def load_from_json(cls, str_json, remover=None):
-    instance = cls(remover=remover)
-    dat = json.loads(str_json)
-    for name, path in dat.items():
-      instance.tag(name, path)
-    return instance
+    @classmethod
+    def load_from_json(cls, str_json, remover=None):
+        instance = cls(remover=remover)
+        dat = json.loads(str_json)
+        for name, path in dat.items():
+            instance.tag(name, path)
+        return instance
