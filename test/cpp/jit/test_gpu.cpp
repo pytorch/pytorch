fd1700fdd2a8c521e0f0f9d5696487262df9c6f5
Christian Sarofeen <csarofeen@nvidia.com>
2021-08-14 13:34:44 -0400

    Fix fusion parser test. (#1057)

--- test/cpp/jit/test_gpu.cpp
+++ test/cpp/jit/test_gpu.cpp
@@ -1167,20 +1167,20 @@ TEST(NVFuserTest, FusionParser_CUDA) {
   // 2. use a fuzzy compare (ignore non-significant whitespaces for example)
   const std::string expected_kernel = R"(
 __global__ void CUDAGeneratedKernel(Tensor<float, 1> T0, Tensor<float, 1> T1, Tensor<float, 1> T3) {
-  if ((((((((((nvfuser_index_t)blockIdx.x) * 1) + (1 - 1)) * 1) + (1 - 1)) * 128) + ((nvfuser_index_t)threadIdx.x)) < T0.size[0])) {
+  if ((((((((((nvfuser_index_t)blockIdx.x) * 1) + (1 - 1)) * 1) + (1 - 1)) * ((nvfuser_index_t)blockDim.x)) + ((nvfuser_index_t)threadIdx.x)) < T0.size[0])) {
     constexpr nvfuser_index_t ki167 = 0;
     float T5[1];
     constexpr nvfuser_index_t ki201 = 0;
     T5[ki201] = 0;
     constexpr nvfuser_index_t ki192 = 0;
     T5[ki192]
-       = T1[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki192) * 128) + ((nvfuser_index_t)threadIdx.x)) * 1)];
+       = T1[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki192) * ((nvfuser_index_t)blockDim.x)) + ((nvfuser_index_t)threadIdx.x)) * 1)];
     float T4[1];
     constexpr nvfuser_index_t ki207 = 0;
     T4[ki207] = 0;
     constexpr nvfuser_index_t ki187 = 0;
     T4[ki187]
-       = T0[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki187) * 128) + ((nvfuser_index_t)threadIdx.x)) * 1)];
+       = T0[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki187) * ((nvfuser_index_t)blockDim.x)) + ((nvfuser_index_t)threadIdx.x)) * 1)];
     float T6[1];
     constexpr nvfuser_index_t ki176 = 0;
     float T2[1];
@@ -1191,7 +1191,7 @@ __global__ void CUDAGeneratedKernel(Tensor<float, 1> T0, Tensor<float, 1> T1, Te
       = T2[0]
       * T4[ki176];
     constexpr nvfuser_index_t ki169 = 0;
-    T3[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki169) * 128) + ((nvfuser_index_t)threadIdx.x)) * 1)]
+    T3[(((((((((nvfuser_index_t)blockIdx.x) * 1) + ki167) * 1) + ki169) * ((nvfuser_index_t)blockDim.x)) + ((nvfuser_index_t)threadIdx.x)) * 1)]
        = T6[ki169];
   }
 }
