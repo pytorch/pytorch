1b310bbe44867d20398c342c0180838f39b39e76
peter <peterghost86@gmail.com>
2021-08-15 11:32:57 +0800

    follow redirect

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
@@ -1,6 +1,6 @@
 mkdir %TMP_DIR_WIN%\bin
 
 if "%REBUILD%"=="" (
-  curl --retry 3 -k "https://www.dropbox.com/s/ydxmvn03ij2ynam/llvm-14.0.0.7z?dl=1" --output %TMP_DIR_WIN%\llvm-14.0.0.7z
+  curl --retry 3 -kL "https://www.dropbox.com/s/ydxmvn03ij2ynam/llvm-14.0.0.7z?dl=1" --output %TMP_DIR_WIN%\llvm-14.0.0.7z
   7z x -aoa %TMP_DIR_WIN%\llvm-14.0.0.7z -o"C:\Program Files\LLVM"
 )
