49004b83517bc8705ceb3f8deed6c3f6ba17b80e
peter <peterghost86@gmail.com>
2021-08-15 12:07:16 +0800

    debug

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
@@ -4,3 +4,7 @@ if "%REBUILD%"=="" (
   curl --retry 3 -kL "https://www.dropbox.com/s/dmrqgqt4fqjpjtv/llvm-14.0.0.7z?dl=1" --output %TMP_DIR_WIN%\llvm-14.0.0.7z
   7z x -aoa %TMP_DIR_WIN%\llvm-14.0.0.7z -o"C:\Program Files\LLVM"
 )
+
+dir "C:\Program Files\LLVM"
+dir "C:\Program Files\LLVM\bin"
+where clang-cl
