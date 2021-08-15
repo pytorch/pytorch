c1fb65526ed455eec27a27e23eb086e194eafb38
peter <peterghost86@gmail.com>
2021-08-15 12:16:49 +0800

    repackage

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_llvm.bat
@@ -1,10 +1,6 @@
 mkdir %TMP_DIR_WIN%\bin
 
 if "%REBUILD%"=="" (
-  curl --retry 3 -kL "https://www.dropbox.com/s/dmrqgqt4fqjpjtv/llvm-14.0.0.7z?dl=1" --output %TMP_DIR_WIN%\llvm-14.0.0.7z
+  curl --retry 3 -kL "https://www.dropbox.com/s/b2m742ckj6j0u6k/llvm-14.0.0.7z?dl=1" --output %TMP_DIR_WIN%\llvm-14.0.0.7z
   7z x -aoa %TMP_DIR_WIN%\llvm-14.0.0.7z -o"C:\Program Files\LLVM"
 )
-
-dir "C:\Program Files\LLVM"
-dir "C:\Program Files\LLVM\bin"
-where clang-cl
