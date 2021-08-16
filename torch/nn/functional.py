e7a438c32be0d1b00b694b5c21c7c41aa7389f2c
Jeff Daily <jeff.daily@amd.com>
2021-08-16 13:36:09 -0700

    Revert "Allow TransformerEncoder and TransformerDecoder to accept 0-dim batch sized tensors. (#62800)"
    This reverts commit 809e1e7457b011f3e6528304d9f8461b1cb3475b.

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -5037,7 +5037,7 @@ def multi_head_attention_forward(
     #
     q = q.contiguous().view(tgt_len, bsz * num_heads, head_dim).transpose(0, 1)
     if static_k is None:
-        k = k.contiguous().view(k.shape[0], bsz * num_heads, head_dim).transpose(0, 1)
+        k = k.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
     else:
         # TODO finish disentangling control flow so we don't do in-projections when statics are passed
         assert static_k.size(0) == bsz * num_heads, \
@@ -5046,7 +5046,7 @@ def multi_head_attention_forward(
             f"expecting static_k.size(2) of {head_dim}, but got {static_k.size(2)}"
         k = static_k
     if static_v is None:
-        v = v.contiguous().view(v.shape[0], bsz * num_heads, head_dim).transpose(0, 1)
+        v = v.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
     else:
         # TODO finish disentangling control flow so we don't do in-projections when statics are passed
         assert static_v.size(0) == bsz * num_heads, \
