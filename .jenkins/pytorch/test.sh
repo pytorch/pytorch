78071626c5042a2dc938c23fccf1af874ffc4b2c
Jiewen Tan <jwtan@fb.com>
2021-08-16 07:00:57 +0000

    Enable test_api IMethodTest in OSS Summary: Tries running `python torch/csrc/deploy/example/generate_examples.py` before test_api.
    [ghstack-poisoned]

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -263,6 +263,7 @@ test_libtorch() {
     python test/cpp/jit/tests_setup.py shutdown
     # Wait for background download to finish
     wait
+    python torch/csrc/deploy/example/generate_examples.py # test_api now includes IMethodTest that relies on it.
     OMP_NUM_THREADS=2 TORCH_CPP_TEST_MNIST_PATH="test/cpp/api/mnist" build/bin/test_api --gtest_output=xml:$TEST_REPORTS_DIR/test_api.xml
     build/bin/test_tensorexpr --gtest_output=xml:$TEST_REPORTS_DIR/test_tensorexpr.xml
     build/bin/test_mobile_nnc --gtest_output=xml:$TEST_REPORTS_DIR/test_mobile_nnc.xml
