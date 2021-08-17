6c235cfb4944d11e0f67d691338d4afbf7294bcd
Shubham Bhokare <shubhambhokare@gmail.com>
2021-08-17 19:02:01 +0000

    Remove duplicate comments

--- torch/csrc/jit/passes/remove_inplace_ops.cpp
+++ torch/csrc/jit/passes/remove_inplace_ops.cpp
@@ -1,4 +1,5 @@
 #include <torch/csrc/jit/passes/remove_inplace_ops.h>
+#include <torch/csrc/jit/jit_log.h>
 
 namespace torch {
 namespace jit {
@@ -113,18 +114,14 @@ void ImplicitCastForBinaryInplaceOps(Block* b) {
         continue;
       }
 
-      // Introduce warning for special case where shape values are obtained from
-      // tensor.shape In eager mode these values are python int, so there are
-      // treated as different values. However in tracing both are traced as
-      // tensors, and then they share same memory. This causes a mismatch in
-      // final outputs
       auto shape_node = originalInputs.at(0)->node();
       if ((shape_node->kind() == prim::NumToTensor) &&
           (shape_node->inputs().at(0)->node()->kind() == aten::size)) {
+        
         std::cerr
             << "In tracing mode, shape values obtained from tensor.shape are traced as tensors, "
-            << "and share the same memory. This might causes a mismatch in final outputs. "
-            << "As a workaround, avoid use of inplace operations in these scenarios. "
+            << "and share the same memory. This might cause a mismatch in values of the final outputs. "
+            << "As a workaround, avoid use of inplace operations in these scenarios."
             << std::endl;
       }
 
