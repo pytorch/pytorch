8d256908331b383eb499e4efeadf02dbb08a67b6
BowenBao <bowbao@microsoft.com>
2021-08-18 13:25:19 -0700

    [ONNX] Fix for batchnorm training op mode (#52758) (#62760)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/62760
    
    * Rebase
    
    # Conflicts:
    #	torch/csrc/jit/passes/onnx/eval_peephole.cpp
    
    # Conflicts:
    #	test/onnx/test_utility_funs.py
    #	torch/onnx/symbolic_opset9.py
    
    * Update symbolic_opset12.py
    
    * Update test.sh
    # Conflicts:
    #	.jenkins/caffe2/test.sh
    
    * Merge
    
    * Fix utility tests
    
    # Conflicts:
    #	test/onnx/test_pytorch_onnx_onnxruntime.py
    #	test/onnx/test_utility_funs.py
    
    * Fix for comment
    
    * Enable BN tests
    
    * Fix for test
    
    * Update test_pytorch_onnx_onnxruntime.py
    
    * Update test_pytorch_onnx_onnxruntime.py
    
    * Update test_utility_funs.py
    
    * Update test_pytorch_onnx_onnxruntime.py
    
    Test Plan: Imported from OSS
    
    Reviewed By: SplitInfinity
    
    Differential Revision: D30349060
    
    Pulled By: msaroufim
    
    fbshipit-source-id: 93312c17607974731c17099ae181acb6e4c1c409

--- torch/onnx/symbolic_opset12.py
+++ torch/onnx/symbolic_opset12.py
@@ -4,6 +4,7 @@ from torch.onnx.symbolic_helper import parse_args, _parse_arg, _unimplemented
 from torch.onnx.utils import _add_block, _add_input_to_block, _add_output_to_block
 from sys import maxsize
 from torch.onnx.symbolic_opset9 import permute, _reshape_from_tensor
+import warnings
 
 
 # EDITING THIS FILE? READ THIS FIRST!
@@ -25,11 +26,12 @@ def outer(g, input, other):
 
 @parse_args("v", "f", "i")
 def dropout(g, input, p, train):
-    sym_help.assert_training_mode(train, "dropout")
+    sym_help.check_training_mode(train, "dropout")
     # in eval mode, dropout is non-op - if the node's train param is set to False, dropout is non-op
-    if not sym_help._training_mode:
+    if not train:
         return input
-
+    warnings.warn("Dropout is a training op and should not be exported in inference mode. "
+                  "For inference, make sure to call eval() on the model and to export it with param training=False.")
     p = g.op("Constant", value_t=torch.tensor(p))
     t = g.op("Constant", value_t=torch.tensor(True))
     r, _ = g.op("Dropout", input, p, t, outputs=2)
