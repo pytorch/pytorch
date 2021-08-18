b01c397c529d7fda16d61865df94264622a596a8
JouleYuan <yuanhaoran0318@gmail.com>
2021-08-18 12:18:17 +0800

    add profiling start args

--- torch/profiler/_listen.py
+++ torch/profiler/_listen.py
@@ -1,28 +1,43 @@
 from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers.json import JSONMixin
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException
 from multiprocessing import Process
 import signal
 import os
+from .profiler import profile, tensorboard_trace_handler
+
+
+class JSONRequest(Request, JSONMixin):
+
+    pass
+
 
 class HTTPServer(object):
 
-    def __init__(self, main_pid: int):
+    def __init__(self, main_pid: int, shared_config: dict):
         self.url_map = Map([
             Rule('/', endpoint='index'),
             Rule('/cmd/start', endpoint='start'),
-            Rule('/cmd/exit', endpoint='exit')
+            Rule('/cmd/stop', endpoint='stop')
         ])
         self.main_pid = main_pid
+        self.shared_config = shared_config
 
     def dispatch_request(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
             endpoint, values = adapter.match()
             if request.method == 'PUT' and endpoint == "start":
+                config = request.get_json()
+                self.shared_config['log_dir'] = config['log_dir']
+                self.shared_config['record_shapes'] = config['record_shapes']
+                self.shared_config['profile_memory'] = config['profile_memory']
+                self.shared_config['with_stack'] = config['with_stack']
+                self.shared_config['with_flops'] = config['with_flops']
                 os.kill(self.main_pid, signal.SIGUSR1)
                 return Response("sucess\n")
-            elif request.method == 'PUT' and endpoint == "exit":
+            elif request.method == 'PUT' and endpoint == "stop":
                 os.kill(self.main_pid, signal.SIGUSR2)
                 return Response("sucess\n")
             else:
@@ -31,19 +46,22 @@ class HTTPServer(object):
             return e
 
     def wsgi_app(self, environ, start_response):
-        request = Request(environ)
+        request = JSONRequest(environ)
         response = self.dispatch_request(request)
         return response(environ, start_response)
 
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
+
 class Listener(object):
 
-    def __init__(self, main_pid: int, port: int):
+    def __init__(self, main_pid: int, host: str, port: int, shared_config: dict):
         self.proc = None
+        self.host = host
         self.port = port
         self.main_pid = main_pid
+        self.shared_config = shared_config
     
     def open(self):
         self.proc = Process(target=self.__open, args=(), daemon=True)
@@ -51,4 +69,4 @@ class Listener(object):
 
     def __open(self):
         from werkzeug.serving import run_simple
-        run_simple('localhost', self.port, HTTPServer(self.main_pid))
+        run_simple(self.host, self.port, HTTPServer(self.main_pid, self.shared_config))
--- torch/profiler/_service.py
+++ torch/profiler/_service.py
@@ -2,28 +2,41 @@ from .profiler import profile, tensorboard_trace_handler
 from . import _listen
 import signal
 import os
+import multiprocessing
 
-logdir = './log/default' if os.environ.get('PYTORCH_PROFILER_SERVICE_LOGDIR') is None else os.environ.get('PYTORCH_PROFILER_SERVICE_LOGDIR')
-port = 5000 if os.environ.get('PYTORCH_PROFILER_SERVICE_PORT') is None else int(os.environ.get('PYTORCH_PROFILER_SERVICE_PORT'))
+#logdir = './log/default' if os.environ.get('PYTORCH_PROFILER_SERVICE_LOGDIR') is None else os.environ.get('PYTORCH_PROFILER_SERVICE_LOGDIR')
+host = '0.0.0.0' if os.environ.get('PYTORCH_PROFILER_SERVICE_HOST') is None else int(os.environ.get('PYTORCH_PROFILER_SERVICE_HOST'))
+port = 3180 if os.environ.get('PYTORCH_PROFILER_SERVICE_PORT') is None else int(os.environ.get('PYTORCH_PROFILER_SERVICE_PORT'))
 
-prof = profile(
-    on_trace_ready=tensorboard_trace_handler(logdir),
-    record_shapes=True,
-    profile_memory=True,
-    with_stack=True,
-    with_flops=True
-)
+prof: profile = None
+
+proc_manager = multiprocessing.Manager()
+shared_config = proc_manager.dict({
+    'log_dir': './log/default',
+    'record_shapes': True,
+    'profile_memory': True,
+    'with_stack': True,
+    'with_flops': False
+})
 
 def enter(signum, frame):
     print("start profiling")
+    global prof
+    prof = profile(
+        on_trace_ready=tensorboard_trace_handler(shared_config['log_dir']),
+        record_shapes=shared_config['record_shapes'],
+        profile_memory=shared_config['profile_memory'],
+        with_stack=shared_config['with_stack'],
+        with_flops=shared_config['with_flops']
+    )
     prof.start()
 
-def exit(signum, frame):
+def stop(signum, frame):
     print("exit profiling")
     prof.stop()
 
 signal.signal(signal.SIGUSR1, enter)
-signal.signal(signal.SIGUSR2, exit)
+signal.signal(signal.SIGUSR2, stop)
 
-listener = _listen.Listener(os.getpid(), port)
+listener = _listen.Listener(os.getpid(), host, port, shared_config)
 listener.open()
