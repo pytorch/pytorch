6ef172cf2019581a8b32a2e54be0e89e2deed2e8
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-08-18 18:55:59 -0400

    Update on "Adding DataLoader2 class as future replacement of DataLoader"
    Supports sharding and batching on loader level**
    * #63522 Adding IterableAsDataPipe IterDataPipe
    usefull for tests and simple cases
    
    Supports sharding and batching on loader level
    
    [ghstack-poisoned]

--- torch/utils/data/__init__.py
+++ torch/utils/data/__init__.py
@@ -1,19 +1,13 @@
 # TODO(VitalyFedyunin): Rearranging this imports leads to crash,
 # need to cleanup dependencies and fix it
-from torch.utils.data._decorator import (
-    argument_validation,
-    functional_datapipe,
-    guaranteed_datapipes_determinism,
-    non_deterministic,
-    runtime_validation,
-    runtime_validation_disabled,
+from torch.utils.data.sampler import (
+    BatchSampler,
+    RandomSampler,
+    Sampler,
+    SequentialSampler,
+    SubsetRandomSampler,
+    WeightedRandomSampler,
 )
-from torch.utils.data.dataloader import (
-    DataLoader,
-    _DatasetKind,
-    get_worker_info,
-)
-from torch.utils.data.dataloader_experimental import DataLoader2
 from torch.utils.data.dataset import (
     ChainDataset,
     ConcatDataset,
@@ -26,15 +20,22 @@ from torch.utils.data.dataset import (
     TensorDataset,
     random_split,
 )
-from torch.utils.data.distributed import DistributedSampler
-from torch.utils.data.sampler import (
-    BatchSampler,
-    RandomSampler,
-    Sampler,
-    SequentialSampler,
-    SubsetRandomSampler,
-    WeightedRandomSampler,
+from torch.utils.data.dataloader import (
+    DataLoader,
+    _DatasetKind,
+    get_worker_info,
 )
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data._decorator import (
+    argument_validation,
+    functional_datapipe,
+    guaranteed_datapipes_determinism,
+    non_deterministic,
+    runtime_validation,
+    runtime_validation_disabled,
+)
+from torch.utils.data.dataloader_experimental import DataLoader2
+
 
 __all__ = ['BatchSampler',
            'ChainDataset',
@@ -70,4 +71,3 @@ assert __all__ == sorted(__all__)
 ################################################################################
 # import subpackage
 ################################################################################
-from torch.utils.data import datapipes
