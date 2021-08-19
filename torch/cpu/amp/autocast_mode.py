c81e81f8edc1d83004e3d9402c041fcf20aa5ad3
leslie-fang-intel <leslie.fang@intel.com>
2021-08-19 17:17:11 +0800

    Update on "add operation list for AutocastCPU"
    [ghstack-poisoned]

--- torch/cpu/amp/autocast_mode.py
+++ torch/cpu/amp/autocast_mode.py
@@ -5,5 +5,5 @@ class autocast(torch.autocast_mode.autocast):
     See :class:`torch.autocast`.
     ``torch.cpu.amp.autocast(args...)`` is equivalent to ``torch.autocast("cpu", args...)``
     """
-    def __init__(self, enabled=True, fast_dtype=torch.float16):
+    def __init__(self, enabled=True, fast_dtype=torch.bfloat16):
         super().__init__("cpu", enabled=enabled, fast_dtype=fast_dtype)
