b07703e20ec6dbce405ba2b40fa35db89312ea45
driazati <driazati@users.noreply.github.com>
2021-08-23 10:41:37 -0700

    Update on "[wip] Use more shards"
    [ghstack-poisoned]

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -497,6 +497,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *libtorch* ]]; then
   # TODO: run some C++ tests
   echo "no-op at the moment"
 elif [[ "${BUILD_ENVIRONMENT}" == *-test1 || "${JOB_BASE_NAME}" == *-test1 || "${SHARD_NUMBER}" == 1 ]]; then
+  echo test1
+  exit 0
   if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.1*-test1* ]]; then
     test_torch_deploy
   fi
@@ -505,6 +507,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *-test1 || "${JOB_BASE_NAME}" == *-test1 || "$
   test_python_shard1
   test_aten
 elif [[ "${BUILD_ENVIRONMENT}" == *-test2 || "${JOB_BASE_NAME}" == *-test2 || "${SHARD_NUMBER}" == 2 ]]; then
+  echo test2
+  exit 0
   install_torchvision
   test_python_shard2
   test_libtorch
@@ -524,6 +528,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *vulkan-linux* ]]; then
 elif [[ "${BUILD_ENVIRONMENT}" == *-bazel-* ]]; then
   test_bazel
 else
+  echo other
+  exit 0
   install_torchvision
   install_monkeytype
   test_python
