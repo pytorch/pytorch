23966083a17d6810002cf5379e993dd921f94bdd
peterjc123 <peterghost86@gmail.com>
2021-08-23 23:49:24 +0800

    minor fix

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -37,7 +37,7 @@ TORCH_LIB_PATH = os.path.join(_TORCH_PATH, 'lib')
 BUILD_SPLIT_CUDA = os.getenv('BUILD_SPLIT_CUDA') or (os.path.exists(os.path.join(
     TORCH_LIB_PATH, f'{CLIB_PREFIX}torch_cuda_cu{CLIB_EXT}')) and os.path.exists(os.path.join(TORCH_LIB_PATH, f'{CLIB_PREFIX}torch_cuda_cpp{CLIB_EXT}')))
 
-SUBPROCESS_DECODE_ARGS = ('oem') if IS_WINDOWS else ()
+SUBPROCESS_DECODE_ARGS = ('oem',) if IS_WINDOWS else ()
 
 # Taken directly from python stdlib < 3.9
 # See https://github.com/pytorch/pytorch/issues/48617
