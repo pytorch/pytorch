16ba20507a7a8fcb62d88c719eceab578e09e210
Karen Zhou <kazhou@fb.com>
2021-08-24 10:17:28 -0700

    [pruner] amend base pruner API to match base sparsifier (#63178)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63178
    
    Update base pruner API to match base sparsifier API as defined in D28970960 / PR58955
    
    Changes include:
    - `enable_mask_update = True` in `__init__`
    - `prepare` takes model and config instead of constructor
    - convert functionality renamed to `squash_mask`, `convert` method call now raises Error
    - `activation_handles` ad `bias_handles` initialized in `_prepare` instead of constructor
    ghstack-source-id: 136467595
    
    Test Plan:
    Function names updates according to changes
    
    `buck test mode/dev-nosan //caffe2/test:ao -- TestBasePruner`
    
    https://pxl.cl/1MTgH
    
    TODO will need to modify `fbcode/scripts/kazhou/fusion_tests.py` to use new API
    
    Reviewed By: z-a-f
    
    Differential Revision: D30287179
    
    fbshipit-source-id: d4727bea1873b500f2d4bb784db26d532bf26cce

--- torch/ao/sparsity/experimental/pruner/base_pruner.py
+++ torch/ao/sparsity/experimental/pruner/base_pruner.py
@@ -1,6 +1,5 @@
 
 import abc
-import copy
 
 import torch
 from torch import nn
@@ -10,31 +9,15 @@ from torch.nn.modules.container import ModuleDict, ModuleList
 
 from .parametrization import PruningParametrization, ActivationReconstruction
 
+from torch.ao.sparsity import BaseSparsifier, fqn_to_module
+
 SUPPORTED_MODULES = {
     nn.Linear,
     nn.Conv2d
 }
 
-def _module_to_path(model, layer, prefix=''):
-    for name, child in model.named_children():
-        new_name = prefix + '.' + name
-        if child is layer:
-            return new_name
-        child_path = _module_to_path(child, layer, prefix=new_name)
-        if child_path is not None:
-            return child_path
-    return None
 
-def _path_to_module(model, path):
-    path = path.split('.')
-    for name in path:
-        model = getattr(model, name, None)
-        if model is None:
-            return None
-    return model
-
-
-class BasePruner(abc.ABC):
+class BasePruner(BaseSparsifier):
     r"""Base class for all pruners.
 
     Abstract methods that need to be implemented:
@@ -53,66 +36,8 @@ class BasePruner(abc.ABC):
             be updated.
 
     """
-    def __init__(self, model, config, defaults):
-        super().__init__()
-        self.config = config
-        self.defaults = defaults
-        if self.defaults is None:
-            self.defaults = dict()
-
-        self.module_groups = []
-        self.enable_mask_update = False
-        self.activation_handles = []
-        self.bias_handles = []
-
-        self.model = model
-        # If no config -- try getting all the supported layers
-        if self.config is None:
-            # Add all models to the config
-            self.config = []
-            stack = [model]
-            while stack:
-                module = stack.pop()
-                for name, child in module.named_children():
-                    if type(child) in SUPPORTED_MODULES:
-                        self.config.append(child)
-                    else:
-                        stack.append(child)
-
-        for module_config in self.config:
-            if isinstance(module_config, nn.Module):
-                module_config = {'module': module_config}
-            local_args = copy.deepcopy(self.defaults)
-            local_args.update(module_config)
-            module = local_args['module']
-            module_path = _module_to_path(self.model, module)
-            if module_path and module_path[0] == '.':
-                module_path = module_path[1:]
-            local_args['path'] = module_path
-            self.module_groups.append(local_args)
-
-    def __getstate__(self):
-        return {
-            'defaults': self.defaults,
-            'module_groups': self.module_groups,
-        }
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-
-    def __repr__(self):
-        format_string = self.__class__.__name__ + ' ('
-        for i, sparse_args in enumerate(self.module_groups):
-            module = sparse_args['module']
-            format_string += '\n'
-            format_string += f'\tModule Group {i}\n'
-            format_string += f'\t    module: {module}\n'
-            for key in sorted(sparse_args.keys()):
-                if key == 'module':
-                    continue
-                format_string += f'\t    {key}: {sparse_args[key]}\n'
-        format_string += ')'
-        return format_string
+    def __init__(self, defaults):
+        super().__init__(defaults)
 
     def bias_hook(self, module, input, output):
         if getattr(module, '_bias', None) is not None:
@@ -122,12 +47,15 @@ class BasePruner(abc.ABC):
             output += bias
         return output
 
-    def prepare(self, use_path=False, *args, **kwargs):
+    def _prepare(self, use_path=False, *args, **kwargs):
         r"""Adds mask parametrization to the layer weight
         """
+        self.activation_handles = []  # store removable hook handles
+        self.bias_handles = []
+
         for config in self.module_groups:
             if use_path:
-                module = _path_to_module(self.model, config['path'])
+                module = fqn_to_module(self.model, config['fqn'])
             else:
                 module = config['module']
 
@@ -152,10 +80,10 @@ class BasePruner(abc.ABC):
                 module.bias = None
             self.bias_handles.append(module.register_forward_hook(self.bias_hook))
 
-    def convert(self, use_path=False, *args, **kwargs):
+    def squash_mask(self, use_path=False, *args, **kwargs):
         for config in self.module_groups:
             if use_path:
-                module = _path_to_module(self.model, config['path'])
+                module = fqn_to_module(self.model, config['fqn'])
             else:
                 module = config['module']
             parametrize.remove_parametrizations(module, 'weight',
@@ -165,17 +93,6 @@ class BasePruner(abc.ABC):
             elif getattr(module._buffers, 'mask', None):
                 del module._buffers['mask']
             delattr(module, 'mask')
-
-    def step(self, use_path=True):
-        if not self.enable_mask_update:
-            return
-        with torch.no_grad():
-            for config in self.module_groups:
-                if use_path:
-                    module = _path_to_module(self.model, config['path'])
-                else:
-                    module = config['module']
-                self.update_mask(module, **config)
 
     @abc.abstractmethod
     def update_mask(self, layer, **kwargs):
