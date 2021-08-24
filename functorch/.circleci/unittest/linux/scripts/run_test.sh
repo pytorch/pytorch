865d98902ec2b75ff56b23b22bf4c54c0bee05dd
Richard Zou <zou3519@users.noreply.github.com>
2021-08-23 22:13:09 -0400

    [functorch] Run python key tests in CI (pytorch/functorch#98)
    Fixes pytorch/functorch#97

--- functorch/.circleci/unittest/linux/scripts/run_test.sh
+++ functorch/.circleci/unittest/linux/scripts/run_test.sh
@@ -9,8 +9,4 @@ python -m torch.utils.collect_env
 
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
-#
-# test_pythonkey.py: No CI for this yet because I need to figure out
-# how to actually build PyTorch with LLVM and set up a configuration
-# for it.
-find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py ! -name test_pythonkey.py \) | xargs -I {} -n 1 bash -c "python {} || exit 255"
+find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py \) | xargs -I {} -n 1 bash -c "python {} || exit 255"
