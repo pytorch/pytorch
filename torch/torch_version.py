810abc76f85f9b2d3c71930df34849b26806c78d
Nived P A <62166124+nivedwho@users.noreply.github.com>
2021-08-24 15:49:57 +0530

    correct spacing

--- torch/torch_version.py
+++ torch/torch_version.py
@@ -55,7 +55,7 @@ class TorchVersion(str):
             # Fall back to regular string comparison if dealing with an invalid
             # version like 'parrot'
             return super().__gt__(cmp)
-    
+
     def __lt__(self, cmp):
         try:
             return Version(self).__lt__(self._convert_to_version(cmp))
@@ -63,7 +63,7 @@ class TorchVersion(str):
             # Fall back to regular string comparison if dealing with an invalid
             # version like 'parrot'
             return super().__lt__(cmp)
-        
+
     def __eq__(self, cmp):
         try:
             return Version(self).__eq__(self._convert_to_version(cmp))
@@ -71,7 +71,7 @@ class TorchVersion(str):
             # Fall back to regular string comparison if dealing with an invalid
             # version like 'parrot'
             return super().__eq__(cmp)
-    
+
     def __ge__(self, cmp):
         try:
             return Version(self).__ge__(self._convert_to_version(cmp))
