3f68ad329ccd5827d06bcb8782ec76b72fb3382d
Edward Z. Yang <ezyang@fb.com>
2021-08-25 10:12:04 -0700

    Update on "Add acc_gpu_kernel_with_scalars and port add to use it"
    See https://dev-discuss.pytorch.org/t/cuda-loops-case-study-code-generation-vs-templates/302
    for explanation of what's going on here.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Differential Revision: [D30545296](https://our.internmc.facebook.com/intern/diff/D30545296)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/Loops.cuh
+++ aten/src/ATen/native/cuda/Loops.cuh
@@ -111,38 +111,38 @@ void gpu_kernel(TensorIteratorBase& iter, const func_t& f) {
   gpu_kernel_impl(iter, f);
 }
 
-template<typename arg1_t, typename arg2_t, typename func_t>
+template<typename arg1_t, typename arg2_t, typename return_t, typename func_t>
 struct AUnaryFunctor {
   using traits = function_traits<func_t>;
-  using return_t = typename traits::result_type;
+  using accarg1_t = typename traits::template arg<0>::type;
   __device__ return_t operator()(arg2_t b) const {
     return f(a, b);
   }
-  AUnaryFunctor(func_t f_, arg1_t a_): f(f_), a(a_) {}
+  // NB: scalar is stored in higher precision!
+  AUnaryFunctor(func_t f_, accarg1_t a_): f(f_), a(a_) {}
   private:
     func_t f;
-    arg1_t a;
+    accarg1_t a;
 };
 
-template<typename arg1_t, typename arg2_t, typename func_t>
+template<typename arg1_t, typename arg2_t, typename return_t, typename func_t>
 struct BUnaryFunctor {
   using traits = function_traits<func_t>;
-  using return_t = typename traits::result_type;
+  using accarg2_t = typename traits::template arg<1>::type;
   __device__ return_t operator()(arg1_t a) const {
     return f(a, b);
   }
-  BUnaryFunctor(func_t f_, arg2_t b_): f(f_), b(b_) {}
+  // NB: scalar is stored in higher precision!
+  BUnaryFunctor(func_t f_, accarg2_t b_): f(f_), b(b_) {}
   private:
     func_t f;
-    arg2_t b;
+    accarg2_t b;
 };
 
 // Though seemingly noop, this inserts casts from arg1_t to func_t's type
-// (which may be higher precision)!
-template <typename arg1_t, typename arg2_t, typename func_t>
+// (which may be higher precision), as well as casts to return_t
+template <typename arg1_t, typename arg2_t, typename return_t, typename func_t>
 struct BinaryFunctor {
-  using traits = function_traits<func_t>;
-  using return_t = typename traits::result_type;
   __device__ return_t operator()(arg1_t a, arg2_t b) const {
     return f(a, b);
   }
@@ -156,17 +156,19 @@ struct BinaryFunctor {
 // ensure that we load from memory at the correct precision (scalar_t)
 // to avoid expensive loads.  For the whole sordid story see
 // https://dev-discuss.pytorch.org/t/cuda-loops-case-study-code-generation-vs-templates/302
-template <typename arg1_t, typename arg2_t = arg1_t, typename func_t>
+template <typename arg1_t, typename arg2_t = arg1_t, typename return_t = arg1_t, typename func_t>
 void acc_gpu_kernel_with_scalars(TensorIteratorBase& iter, const func_t& f) {
   TORCH_INTERNAL_ASSERT(iter.ntensors() == 3);
 
   using traits = function_traits<func_t>;
+  using accarg1_t = typename traits::template arg<0>::type;
+  using accarg2_t = typename traits::template arg<1>::type;
   static_assert(
       traits::arity == 2,
       "gpu_kernel_with_scalars only supports two input arguments");
 
   if (iter.is_cpu_scalar(1)) {
-    AUnaryFunctor<arg1_t, arg2_t, func_t> af(f, iter.scalar_value<arg1_t>(1));
+    AUnaryFunctor<arg1_t, arg2_t, return_t, func_t> af(f, iter.scalar_value<accarg1_t>(1));
     iter.remove_operand(1);
     // TODO: When all kernels that use gpu_kernel_with_scalars are
     // ported to structured, this device guard can be deleted.  This
@@ -176,11 +178,11 @@ void acc_gpu_kernel_with_scalars(TensorIteratorBase& iter, const func_t& f) {
     const OptionalDeviceGuard device_guard(device_of(iter.tensor(1)));
     gpu_kernel(iter, af);
   } else if (iter.is_cpu_scalar(2)) {
-    BUnaryFunctor<arg1_t, arg2_t, func_t> bf(f, iter.scalar_value<arg2_t>(2));
+    BUnaryFunctor<arg1_t, arg2_t, return_t, func_t> bf(f, iter.scalar_value<accarg2_t>(2));
     iter.remove_operand(2);
     gpu_kernel(iter, bf);
   } else {
-    gpu_kernel(iter, BinaryFunctor<arg1_t, arg2_t, func_t>(f));
+    gpu_kernel(iter, BinaryFunctor<arg1_t, arg2_t, return_t, func_t>(f));
   }
 }
 
