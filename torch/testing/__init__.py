86d2a069e2bff6c7ebaa621a013f26452c0711c9
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2021-08-25 08:43:23 +0000

    Shift make_tensor to _creation.py, all args except size are now keyword-only

--- torch/testing/__init__.py
+++ torch/testing/__init__.py
@@ -1,4 +1,5 @@
 from ._core import *  # noqa: F403
 from ._asserts import *  # noqa: F403
+from ._creation import *  # noqa: F403
 from ._check_kernel_launches import *  # noqa: F403
 from ._deprecated import *  # noqa: F403
