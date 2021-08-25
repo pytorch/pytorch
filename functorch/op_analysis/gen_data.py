8b64876832fc3abd9973fcd133ca878f3439a9db
Horace He <horacehe2007@yahoo.com>
2021-08-24 17:21:49 -0700

    [functorch] Updated gen_data script to use new APIs

--- functorch/op_analysis/gen_data.py
+++ functorch/op_analysis/gen_data.py
@@ -1,10 +1,49 @@
 import yaml
 import csv
+import torch
+import functorch
+from torch.testing._internal.common_utils import CapturedOutput
+import re
+
+def get_ops_for_key(key):
+    all_out = CapturedOutput()
+    with all_out:
+        if key is None:
+            torch._C._dispatch_print_registrations_for_dispatch_key()
+        else:
+            torch._C._dispatch_print_registrations_for_dispatch_key(key)
+
+    ops = all_out.capturedtext.split('\n')
+    cleaned_ops = []
+    for i in ops:
+        if 'aten::' not in i:
+            continue
+        cleaned_ops.append(i[6:].strip())
+    return set(cleaned_ops)
+
+batched_registrations = get_ops_for_key('FuncTorchBatched')
+all_ops = get_ops_for_key(None)
+
+# Find all occurrences of things inside of STOP_DECOMPOSE(...) using regex
+# Look in ../functorch/csrc/BatchRulesStopDecomposition.cpp
+# Example:
+# STOP_DECOMPOSE(sin); => sin
+with open('../functorch/csrc/BatchRulesStopDecomposition.cpp') as f:
+    content = f.read()
+    stop_decomposition_regex = re.compile(r'STOP_DECOMPOSE\((.*)\);')
+    stop_decomposition_matches = stop_decomposition_regex.findall(content)
+    stop_decomposition_matches = [m.strip() for m in stop_decomposition_matches]
+    stop_decomposition_ops = set(stop_decomposition_matches)
+
+composite_ops = get_ops_for_key('CompositeImplicitAutograd')
+decomposed_ops = composite_ops - stop_decomposition_ops
+
+
+vmap_ops = (batched_registrations - stop_decomposition_ops) | (composite_ops - stop_decomposition_ops)
+noncomposite_ops = all_ops - composite_ops
+
 ops = yaml.load(open('/home/chilli/fb/pytorch/aten/src/ATen/native/native_functions.yaml', 'r').read())
-# A list of all non-composite ops starting with aten::
-noncomposite_ops = [i[6:].strip() for i in open('noncomposite.txt').readlines()]
-# A list of all batching rules for ops starting with aten::
-vmap_ops = [i[6:].strip() for i in open('vmap.txt').readlines()]
+
 annotated_ops = {a.strip(): b.strip() for a,b in list(csv.reader(open('annotated_ops.txt')))}
 from collections import defaultdict
 
@@ -35,13 +74,17 @@ def annotate_ops(ops, is_unique):
     categorization = defaultdict(int)
     for i in ops:
         old_tcnt = sum(categorization.values())
+        if i['name'][-1] == '_':
+            categorization['inplace'] += 1
+            i['meta'] = 'inplace'
+            continue
+        if not is_unique and 'a!' in i['func'].lower():
+            categorization['out'] += 1
+            i['meta'] = 'out'
+            continue
         if 'conv' in i['name']:
             categorization['conv'] += 1
             i['meta'] = 'conv'
-            continue
-        if not is_unique and 'out' in i['full_name'].lower():
-            categorization['out'] += 1
-            i['meta'] = 'out'
             continue
         if 'pool' in i['name']:
             categorization['pool'] += 1
@@ -54,10 +97,6 @@ def annotate_ops(ops, is_unique):
         if i['name'][0] == '_' and i['name'][1] != '_':
             categorization['private'] += 1
             i['meta'] = 'private'
-            continue
-        if i['name'][-1] == '_':
-            categorization['inplace'] += 1
-            i['meta'] = 'inplace'
             continue
         if 'batch_norm' in i['name']:
             categorization['batch_norm'] += 1
@@ -82,6 +121,6 @@ def annotate_ops(ops, is_unique):
 categorization = annotate_ops(uniq_ops, True)
 categorization = annotate_ops(ops, False)
 
-for op in sorted(ops):
+for op in ops:
     info = [op['full_name'], op['meta'], not (op['full_name'] in noncomposite_ops), op['full_name'] in vmap_ops]
     print(','.join([str(i) for i in info]))
