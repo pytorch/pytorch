2caa3f3f1ed4c29f5c7217dc6e30e1ee3123917b
Peter Bell <peterbell10@live.co.uk>
2021-08-27 20:29:38 +0100

    Add comment that input is channels last

--- aten/src/ATen/native/cuda/Normalization.cuh
+++ aten/src/ATen/native/cuda/Normalization.cuh
@@ -1649,6 +1649,7 @@ at::Tensor batch_norm_backward_elemt_channels_last_cuda_template(
   const auto stride = input.sizes()[1];
   const auto reduction_size = input.numel() / stride;
 
+  // Input is guarunteed to be channels-last compatible
   at::Tensor grad_input = at::empty_like(input);
 
   dim3 block;
@@ -1716,6 +1717,7 @@ at::Tensor batch_norm_backward_elemt_channels_last_cuda_template(
   const auto reduction_size = input.numel() / stride;
   auto norm_fct = 1.0 / reduction_size;
 
+  // Input is guarunteed to be channels-last compatible
   at::Tensor grad_input = at::empty_like(input);
 
   dim3 block;
