7d6e6a548f74e24ee8f1bfb3e2fd9ff3b54d7790
Supriya Rao <supriyar@fb.com>
2021-08-26 21:05:56 -0700

    [quant] Add support for linear_relu fusion for FP16 dynamic quant (#63826)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63826
    
    Support the conversion of the intrinsic linearRelu module to the quantized dynamic LinearReLU module
    Verify the support works for both linear module and functional linear fusion
    
    Test Plan:
    python test/test_quantization.py test_dynamic_with_fusion
    
    Imported from OSS
    
    Reviewed By: iramazanli
    
    Differential Revision: D30503513
    
    fbshipit-source-id: 70446797e9670dfef7341cba2047183d6f88b70f

--- test/quantization/fx/test_quantize_fx.py
+++ test/quantization/fx/test_quantize_fx.py
@@ -2922,18 +2922,24 @@ class TestQuantizeFx(QuantizationTestCase):
                 return x
 
         model = M().eval()
-        qconfig = {
-            "": default_dynamic_qconfig,
+
+        dynamic_quantized_ops = {
+            float16_dynamic_qconfig: torch.ops.quantized.linear_relu_dynamic_fp16,
+            default_dynamic_qconfig: torch.ops.quantized.linear_relu_dynamic
         }
-        m = prepare_fx(model, qconfig)
-        m = convert_fx(m)
-        m(torch.rand(5, 5))
-        node_list = [
-            ns.call_module(nniqd.LinearReLU),
-            ns.call_module(nniqd.LinearReLU),
-            ns.call_function(torch.ops.quantized.linear_relu_dynamic),
-        ]
-        self.checkGraphModuleNodes(m, expected_node_list=node_list)
+        for config in [float16_dynamic_qconfig, default_dynamic_qconfig]:
+            qconfig = {
+                "": config
+            }
+            m = prepare_fx(model, qconfig)
+            m = convert_fx(m)
+            m(torch.rand(5, 5))
+            node_list = [
+                ns.call_module(nniqd.LinearReLU),
+                ns.call_module(nniqd.LinearReLU),
+                ns.call_function(dynamic_quantized_ops[config]),
+            ]
+            self.checkGraphModuleNodes(m, expected_node_list=node_list)
 
 @skipIfNoFBGEMM
 class TestQuantizeFxOps(QuantizationTestCase):
@@ -3089,7 +3095,10 @@ class TestQuantizeFxOps(QuantizationTestCase):
             if is_reference:
                 qlinear_fun = ns.call_function(torch.nn.functional.linear)
             else:
-                qlinear_fun = ns.call_function(torch.ops.quantized.linear_dynamic_fp16)
+                if has_relu:
+                    qlinear_fun = ns.call_function(torch.ops.quantized.linear_relu_dynamic_fp16)
+                else:
+                    qlinear_fun = ns.call_function(torch.ops.quantized.linear_dynamic_fp16)
             prepare_node_occurrence = {
                 # weight
                 ns.call_module(torch.quantization.PlaceholderObserver): 1
