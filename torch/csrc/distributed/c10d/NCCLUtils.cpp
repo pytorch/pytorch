c84aa3f5916b3e2f1d8f03abc7e36f74fd1b5e31
kelvinzhang <kelvinzhang@devgpu088.ash6.facebook.com>
2021-08-27 09:25:04 -0700

    Fix NCCL version parsing for 2.10.3 #64028
    Summary:
    replace url in multiline comment with a shorter url
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torch/csrc/distributed/c10d/NCCLUtils.cpp
+++ torch/csrc/distributed/c10d/NCCLUtils.cpp
@@ -18,8 +18,7 @@ std::string getNcclVersion() {
     if (status != ncclSuccess || version < 100) {
       versionString = "Unknown NCCL version";
     } else {
-      // logic reference: https://github.com/NVIDIA/nccl/blob\
-       /7e515921295adaab72adf56ea71a0fafb0ecb5f3/src/nccl.h.in#L22
+      // Ref: https://github.com/NVIDIA/nccl/blob/v2.10.3-1/src/nccl.h.in#L22
       int majorDiv = (version >= 10000)? 10000 : 1000;
       auto ncclMajor = version / majorDiv;
       auto ncclMinor = (version % majorDiv) / 100;
