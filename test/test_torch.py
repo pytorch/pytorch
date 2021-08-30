b32fa976a0ef72939e48bdb6d328d697de0b24cf
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-08-30 06:30:51 -0500

    update map

--- test/test_torch.py
+++ test/test_torch.py
@@ -7664,11 +7664,11 @@ else:
 
         def checkResult(scalar_type, dtype, condition, x, scalar_1):
             def x_like(scalar):
-                python_torch_dtype_map = {
-                    complex: torch.complex64,
-                    float: torch.get_default_dtype(),
-                    bool: torch.bool,
-                    int: torch.long}
+                complex_dtype = torch.complex64 if torch.float == torch.get_default_dtype() else torch.complex128
+                python_torch_dtype_map = {int: torch.long,
+                                        float: torch.get_default_dtype(),
+                                        complex: complex_dtype,
+                                        bool : torch.bool}
 
                 dtype = python_torch_dtype_map[scalar_type]
                 return torch.tensor(scalar, dtype=dtype, device=device).expand_as(x)
