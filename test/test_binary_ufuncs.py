c7a38807db2f609bf141fa37cd75bf3b03101272
Freey0 <freey7955@gmail.com>
2021-08-31 22:10:04 +0800

    restore test_inplace_comparison_ops_require_inputs_have_same_dtype Expected behavior
    This test expects every operation to throw a runtime error.
    
    See also: #64018
    
    [ghstack-poisoned]

--- test/test_binary_ufuncs.py
+++ test/test_binary_ufuncs.py
@@ -224,12 +224,12 @@ class TestBinaryUfuncs(TestCase):
     # TODO: update to work on CUDA, too
     @onlyCPU
     def test_inplace_comparison_ops_require_inputs_have_same_dtype(self, device):
-        with self.assertRaisesRegex(RuntimeError, 'Expected object of scalar type'):
             for op in ['lt_', 'le_', 'gt_', 'ge_', 'eq_', 'ne_', 'logical_xor_', 'logical_and_', 'logical_or_']:
-                x = torch.tensor([1], dtype=torch.int)
-                y = torch.tensor([2], dtype=torch.long)
-                in_place_method = getattr(x, op)
-                in_place_method(y)
+                with self.assertRaisesRegex(RuntimeError, 'Expected object of scalar type'):
+                    x = torch.tensor([1], dtype=torch.int)
+                    y = torch.tensor([2], dtype=torch.long)
+                    in_place_method = getattr(x, op)
+                    in_place_method(y)
 
     # TODO: update to work on CUDA, too
     @onlyCPU
