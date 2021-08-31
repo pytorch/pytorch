efda71e101242cbf19937c14d09325ca7c134e09
John Clow <jclow@fb.com>
2021-08-30 23:18:10 -0700

    Moving the constant parameter check to a more common file
    [ghstack-poisoned]

--- torch/csrc/jit/passes/frozen_graph_optimizations.cpp
+++ torch/csrc/jit/passes/frozen_graph_optimizations.cpp
@@ -1,14 +1,30 @@
 #include <c10/util/irange.h>
 #include <torch/csrc/jit/ir/alias_analysis.h>
+#include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/ir/ir_views.h>
+#include <torch/csrc/jit/passes/frozen_concat_linear.h>
 #include <torch/csrc/jit/passes/frozen_conv_folding.h>
 #include <torch/csrc/jit/passes/frozen_graph_optimizations.h>
 #include <torch/csrc/jit/passes/remove_dropout.h>
 #include <torch/csrc/jit/runtime/graph_executor.h>
 #include <torch/csrc/utils/memory.h>
+#include <vector>
+#include "ATen/core/functional.h"
+#include "ATen/core/interned_strings.h"
 
 namespace torch {
 namespace jit {
+
+bool nonConstantParameters(Node* n) {
+  // Checks if the parameters, not including the
+  // first param are all constants.
+  for (size_t i = 1; i < n->inputs().size(); i++) {
+    if (n->inputs().at(i)->node()->kind() != prim::Constant) {
+      return true;
+    }
+  }
+  return false;
+}
 
 void OptimizeFrozenGraph(
     std::shared_ptr<Graph>& graph,
@@ -22,6 +38,7 @@ void OptimizeFrozenGraph(
       FoldFrozenConvAddOrSub(graph);
       FoldFrozenConvMulOrDiv(graph);
     }
+    FrozenConcatLinear(graph);
   }
 }
 
