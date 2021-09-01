5d80a48cef373e22393af1b1f4f4e3f2ad948a76
anjali411 <chourdiaanjali123@gmail.com>
2021-09-01 16:11:38 -0700

    Add fast path for addmm when the inputs are conjugate (#59380)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/59380
    
    Test Plan: Imported from OSS
    
    Reviewed By: ngimel
    
    Differential Revision: D28898374
    
    Pulled By: anjali411
    
    fbshipit-source-id: eab0e64d37bb57c18b54cabb8e5c00666338ba04

--- test/test_torch.py
+++ test/test_torch.py
@@ -5328,6 +5328,13 @@ else:
         y = x.as_strided([2, 1, 5], [1, 0, 2])
         self.assertEqual(y, y.clone())
 
+    def test_clone_not_memory_dense(self):
+        # github issue: https://github.com/pytorch/pytorch/issues/64176
+        x = torch.randn(10, 8).t()[::2, ::2]
+        y = x.clone()
+        # should retain permutation after densification
+        self.assertTrue(y.stride() == (1, 4))
+
     @dtypesIfCUDA(*set(torch.testing.get_all_math_dtypes('cuda')))
     @dtypes(*set(torch.testing.get_all_math_dtypes('cpu')))
     def test_addcmul(self, device, dtype):
@@ -6013,9 +6020,9 @@ else:
             out_dc = torch.empty(size * size, device=device)[::2]
             for v, m in product(vals_list, mask_list):
                 if m.is_contiguous():
-                    expected = v[:, ::2].clone().view(-1)
+                    expected = v[:, ::2].clone().reshape((-1, ))
                 else:
-                    expected = v[::2].clone().view(-1)
+                    expected = v[::2].clone().reshape((-1, ))
                 out = torch.masked_select(v, m)
                 self.assertEqual(out, expected, atol=0, rtol=0)
                 torch.masked_select(v, m, out=out_dc)
