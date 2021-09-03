96780a1804aecf3f85bce2dda0cc8c55016b4136
Antonio Cuni <anto.cuni@gmail.com>
2021-09-03 13:42:34 +0000

    reduce the diff w.r.t. master

--- torch/__init__.py
+++ torch/__init__.py
@@ -637,6 +637,7 @@ for name in dir(_C._VariableFunctions):
 # needs to be after the above ATen bindings so we can overwrite from Python side
 from .functional import *  # noqa: F403
 
+
 ################################################################################
 # Remove unnecessary members
 ################################################################################
