e578de6ac22ae8c3ca031709c3a77036c9a30b6d
Aswin John Mathews <Aswin.Mathews@amd.com>
2021-09-08 13:33:21 +0000

    Fixing warning cleanup

--- aten/src/ATen/native/ConvUtils.h
+++ aten/src/ATen/native/ConvUtils.h
@@ -109,10 +109,6 @@ static inline bool cudnn_conv_use_channels_last(const at::Tensor& input, const a
 
 static inline bool miopen_conv_use_channels_last(const at::Tensor& input, const at::Tensor& weight) {
 
-  // TODO: Remove PYTORCH_MIOPEN_SUGGEST_NHWC once ROCm officially supports NHWC in MIOpen
-  // See #64427
-  static c10::optional<bool> PYTORCH_MIOPEN_SUGGEST_NHWC = c10::utils::check_env("PYTORCH_MIOPEN_SUGGEST_NHWC");
-
   // disable NHWC for float64 input.
   if (!at::detail::getCUDAHooks().compiledWithMIOpen() ||
       input.scalar_type() == at::kDouble ||
@@ -125,6 +121,10 @@ static inline bool miopen_conv_use_channels_last(const at::Tensor& input, const 
 
   bool can_use_miopen_channels_last_2d = false;
 #if defined(USE_ROCM) && (ROCM_VERSION >= 40300)
+  // TODO: Remove PYTORCH_MIOPEN_SUGGEST_NHWC once ROCm officially supports NHWC in MIOpen
+  // See #64427
+  static c10::optional<bool> PYTORCH_MIOPEN_SUGGEST_NHWC = c10::utils::check_env("PYTORCH_MIOPEN_SUGGEST_NHWC");
+
   can_use_miopen_channels_last_2d = PYTORCH_MIOPEN_SUGGEST_NHWC &&  *PYTORCH_MIOPEN_SUGGEST_NHWC && (
             ( (input_memory_format  == at::MemoryFormat::ChannelsLast) ||
             (weight_memory_format == at::MemoryFormat::ChannelsLast) )
