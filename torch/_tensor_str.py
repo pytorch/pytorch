5689091b7166eaaf1d1369059458a437fa98586b
Mainak <mainak.pal08@gmail.com>
2021-09-09 03:23:52 +0530

    fix flake errors

--- torch/_tensor_str.py
+++ torch/_tensor_str.py
@@ -297,8 +297,15 @@ def _str_intern(inp):
     # within this function.
     # TODO(albanD) This needs to be updated when more than one level is supported
     self, tangent = torch.autograd.forward_ad.unpack_dual(inp)
-    # print shape of the tensor 
-    if PRINT_OPTS.show_shape and not self.is_sparse and not self.is_sparse_csr and not self.is_quantized and not self.is_meta and self.numel()!=0:
+    # print shape of the tensor
+    if (
+        PRINT_OPTS.show_shape and
+        not self.is_sparse and
+        not self.is_sparse_csr and
+        not self.is_quantized and
+        not self.is_meta and
+        self.numel()!=0
+        ):
         suffixes.append('shape=' + str(tuple(self.shape)))
 
     # Note [Print tensor device]:
