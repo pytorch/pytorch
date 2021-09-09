97d8ea94cebf3e9469f2e40a7a09e2342f8afd05
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-09-08 21:18:50 -0700

    Expose wrap_cpp_module
    [ghstack-poisoned]

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -46,6 +46,10 @@ from torch.jit._trace import (
     _script_if_tracing,
     _get_trace_graph,
 )
+from torch.jit._recursive import (
+    wrap_cpp_module,
+)
+
 from torch.jit._async import fork, wait
 from torch.jit._serialization import save, load
 from torch.jit._fuser import optimized_execution, fuser, last_executed_optimized_graph
