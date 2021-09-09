9a7ae115c4ad34085857b1ff5852f194fcbbf502
soulitzer <soulitzer@gmail.com>
2021-09-08 20:21:13 -0400

    Update base for Update on "Replacing codegen with boxed for testing"
    [ghstack-poisoned]

--- torch/csrc/autograd/autograd_not_implemented_fallback.h
+++ torch/csrc/autograd/autograd_not_implemented_fallback.h
@@ -8,15 +8,15 @@ namespace autograd {
 
 TORCH_API torch::CppFunction autogradNotImplementedFallback();
 
-TORCH_API torch::CppFunction ADInplaceOrViewFallback();
+TORCH_API torch::CppFunction autogradNotImplementedInplaceOrViewFallback();
 
 
 #define REGISTER_AUTOGRAD_NOT_IMPLEMENTED_FALLBACK(ns, op)      \
   TORCH_LIBRARY_IMPL(ns, Autograd, m) {                         \
-    m.def(op, AutogradNotImplementedFallback());                \
+    m.def(op, autogradNotImplementedFallback());                \
   }                                                             \
   TORCH_LIBRARY_IMPL(ns, ADInplaceOrView, m) {                  \
-    m.def(op, ADInplaceOrViewFallback());                       \
+    m.def(op, autogradNotImplementedInplaceOrViewFallback());   \
   }
 
 }} // namespace torch::autograd
