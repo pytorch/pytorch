9cc96ad811404957480eeea7a4f83226a23ad510
Nikita Shulga <nshulga@fb.com>
2021-09-09 19:33:26 -0700

    [torch] Set `nproc_per_node` to 1 (#61552) (#64791)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61552
    
    Set `nproc_per_node` to 1
    
    Test Plan: unittests
    
    Reviewed By: cbalioglu
    
    Differential Revision: D29667056
    
    fbshipit-source-id: 6601f66fec5e018c7737d909f8c71642451abb29
    
    Co-authored-by: Aliaksandr Ivanou <aivanou@fb.com>

--- torch/distributed/run.py
+++ torch/distributed/run.py
@@ -240,6 +240,7 @@ from typing import Callable, List, Tuple, Union
 import torch
 from torch.distributed.argparse_util import check_env, env
 from torch.distributed.elastic.multiprocessing import Std
+from torch.distributed.elastic.multiprocessing.errors import record
 from torch.distributed.elastic.rendezvous.utils import _parse_rendezvous_config
 from torch.distributed.elastic.utils import macros
 from torch.distributed.elastic.utils.logging import get_logger
@@ -269,7 +270,7 @@ def get_args_parser() -> ArgumentParser:
         "--nproc_per_node",
         action=env,
         type=str,
-        default="auto",
+        default="1",
         help="Number of workers per node; supported values: [auto, cpu, gpu, int].",
     )
 
