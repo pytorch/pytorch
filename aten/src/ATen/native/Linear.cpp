ece88bd0d9b987b937cd429385e0e3accb632e2e
Antonio Cuni <anto.cuni@gmail.com>
2021-09-10 13:21:57 +0000

    WIP

--- aten/src/ATen/native/Linear.cpp
+++ aten/src/ATen/native/Linear.cpp
@@ -637,7 +637,65 @@ Tensor &tensordot_out(const Tensor& input1, const Tensor& input2, IntArrayRef di
 
 // implements tensordot, a matrix-multiplication-like contraction, but the dimensions given
 // in the two dimension lists
+
+static Tensor linalg_tensordot_allocate_out(const Tensor& input1, const Tensor& input2,
+                                            IntArrayRef dims1, IntArrayRef dims2)
+{
+  TORCH_CHECK(dims1.size() == dims2.size(), "both dimension lists should have same length");
+  int64_t csize = 1;  // total size of the contracted dimensions
+  Tensor t1 = input1;
+  Tensor t2 = input2;
+  for (const auto i : c10::irange(dims1.size())) {
+    int s1 = input1.size(dims1[i]);
+    int s2 = input2.size(dims2[i]);
+    if (s2 == 1) { // broadcasted dimensions can be summed right away
+      t1 = t1.sum(dims1[i], true);
+    } else if (s1 == 1) {
+      t2 = t2.sum(dims2[i], true);
+    } else {
+      TORCH_CHECK(s1 == s2, "contracted dimensions need to match, but first has size ", s1, " in dim ", dims1[i],
+               " and second has size ", s2, " in dim ", dims2[i]);
+      csize *= s1;
+    }
+  }
+
+  auto cdims1 = at::dim_list_to_bitset(dims1, input1.dim());
+  auto cdims2 = at::dim_list_to_bitset(dims2, input2.dim());
+  std::vector<int64_t> p1, p2, rsizes;  // p1, p2: input permutations, rsizes: sizes of the result
+  p1.reserve(input1.dim());
+  p2.reserve(input2.dim());
+  rsizes.reserve(input1.dim() + input2.dim() - (int64_t) dims1.size());
+  int64_t size1 = 1; // number of non-contracted elements in input1
+  int64_t size2 = 1; // number of non-contracted elements in input2
+
+  // fill the permutations and compute sizes
+  for (const auto i : c10::irange(input1.dim())) {
+    if (! cdims1[i]) {
+      p1.emplace_back(i);
+      size1 *= t1.size(i);
+      rsizes.emplace_back(t1.size(i));
+    }
+  }
+  for (const auto x : dims1) {
+    p1.emplace_back(x);
+  }
+  for (const auto x : dims2) {
+    p2.emplace_back(x);
+  }
+  for (const auto i : c10::irange(input2.dim())) {
+    if (! cdims2[i]) {
+      p2.emplace_back(i);
+      size2 *= t2.size(i);
+      rsizes.emplace_back(t2.size(i));
+    }
+  }
+
+  return at::empty(rsizes, input1.options());
+}
+
 Tensor linalg_tensordot(const Tensor& input1, const Tensor& input2, IntArrayRef dims1, IntArrayRef dims2) {
+  Tensor out = linalg_tensordot_allocate_out(input1, input2, dims1, dims2);
+
   TORCH_CHECK(dims1.size() == dims2.size(), "both dimension lists should have same length");
   int64_t csize = 1;  // total size of the contracted dimensions
   Tensor t1 = input1;
@@ -690,7 +748,10 @@ Tensor linalg_tensordot(const Tensor& input1, const Tensor& input2, IntArrayRef 
   t1 = t1.permute(p1).reshape({size1, csize});
   t2 = t2.permute(p2).reshape({csize, size2});
   // multiply and reshape to target size
-  return at::mm(t1, t2).reshape(rsizes);
+  auto tmp = at::mm(t1, t2).reshape(rsizes);
+  assert(tmp.sizes() == out.sizes());
+  out.copy_(tmp);
+  return out;
 }
 
 Tensor &linalg_tensordot_out(const Tensor& input1, const Tensor& input2, IntArrayRef dims1, IntArrayRef dims2, Tensor& result) {
