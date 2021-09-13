5d24d95efa139da480a93f58b50c9cc6e72a0f41
zaf <zaf@devgpu109.ash6.facebook.com>
2021-09-13 02:34:02 -0700

    [quant] Add imports to the torch/ao/quantization/__init__.py
    The import statements that involve the `quantize.py` were not added to the module level __init__ file. Those imports are necessary to mimic the behavior of the old import locations. Otherwise, the user would need to change their import statements to `from torch.ao.quantization.quantize import quantize` (instead of `from torch.ao.quantization import quantize`.
    
    Differential Revision: [D30897663](https://our.internmc.facebook.com/intern/diff/D30897663/)
    
    [ghstack-poisoned]

---
+++ torch/ao/quantization/__init__.py
@@ -0,0 +1,14 @@
+from .quantize import (
+    add_observer_,
+    add_quant_dequant,
+    convert,
+    get_observer_dict,
+    prepare,
+    prepare_qat,
+    propagate_qconfig_,
+    quantize,
+    quantize_dynamic,
+    quantize_qat,
+    register_activation_post_process_hook,
+    swap_module,
+)  # noqa: F401
