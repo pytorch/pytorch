pytorch/pytorch

commit 0a53b8f02d4dad6a681146d16b98ca6442fafbd6

[Reland] Replacing the p.data acccess in utils with tensor.set_ . Passes both test_post_localSGD_optimizer_pari and test_periodic_model_averager tests (#63895)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/63895

When updating the model parameter, updating `parameter.data` is no longer recommended, because this `data` field will be deprecated in the future.

The replacement is `tensor.set_`.
ghstack-source-id: 136593433

Test Plan:
buck test mode/dev-nosan //caffe2/test/distributed:distributed_nccl_spawn -- test_periodic_model_averager
buck test mode/dev-nosan //caffe2/test/distributed:distributed_nccl_spawn -- test_post_localSGD_optimizer_parity

Reviewed By: SciPioneer

Differential Revision: D30526178

fbshipit-source-id: a1ac0ec3665d8623edd5bf94f01c1132daff5c00

torch/distributed/algorithms/model_averaging/utils.py
@@ -29,5 +29,6 @@ def average_parameters(
 
     offset = 0
     for p in params_it2:
-        p.data = flat_params[offset : offset + p.numel()].view_as(p)
+        with torch.no_grad():
+            p.set_(flat_params[offset : offset + p.numel()].view_as(p).type_as(p))  # type: ignore[call-overload]
         offset += p.numel()

commit a86370c50c7403eaba5d671f373ac48048caeecc

[Model Averaging] Revert #63895
Pull Request resolved: https://github.com/pytorch/pytorch/pull/64903

Fix the accuracy regression caused by https://github.com/pytorch/pytorch/pull/63895.

Differential Revision: [D30894688](https://our.internmc.facebook.com/intern/diff/D30894688/)

**NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D30894688/)!
ghstack-source-id: 137996763

torch/distributed/algorithms/model_averaging/utils.py
@@ -32,6 +32,5 @@ def average_parameters(
 
     offset = 0
     for p in params_it2:
-        with torch.no_grad():
-            p.set_(flat_params[offset : offset + p.numel()].view_as(p).type_as(p))  # type: ignore[call-overload]
+        p.data = flat_params[offset : offset + p.numel()].view_as(p)
         offset += p.numel()