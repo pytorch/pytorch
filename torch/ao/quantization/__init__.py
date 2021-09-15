3132ca2e1bc69057b2ba47e9a5826688fcd58eef
zaf <zaf@devgpu109.ash6.facebook.com>
2021-09-15 11:50:53 -0700

    Update base for Update on "[quant] [quant] AO migration of the `quant_types.py` (phase 1)"
    AO Team is migrating the existing torch.quantization into torch.ao.quantization. We are doing it one file at a time to make sure that the internal callsites are updated properly.
    This migrates the quant_type.py from torch.quantization to torch.ao.quantization.
    At this point both locations will be supported. Eventually the torch.quantization will be deprecated.
    
    Differential Revision: [D30898422](https://our.internmc.facebook.com/intern/diff/D30898422/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D30898422/)!
    
    [ghstack-poisoned]

--- torch/ao/quantization/__init__.py
+++ torch/ao/quantization/__init__.py
@@ -1,36 +1,3 @@
-from .fake_quantize import (
-    # FakeQuantize (for qat)
-    default_fake_quant,
-    default_weight_fake_quant,
-    default_symmetric_fixed_qparams_fake_quant,
-    default_affine_fixed_qparams_fake_quant,
-    default_per_channel_weight_fake_quant,
-    default_histogram_fake_quant,
-)
-from .quantize import (
-    add_observer_,
-    add_quant_dequant,
-    convert,
-    get_observer_dict,
-    prepare,
-    prepare_qat,
-    propagate_qconfig_,
-    quantize,
-    quantize_dynamic,
-    quantize_qat,
-    register_activation_post_process_hook,
-    swap_module,
-)  # noqa: F401
-
-# TODO: Only a few of these functions are used outside the `fuse_modules.py`
-#       Keeping here for compatibility, need to remove them later.
-from .fuse_modules import (
-    _fuse_modules,
-    _get_module,
-    _set_module,
-    fuse_conv_bn,
-    fuse_conv_bn_relu,
-    fuse_known_modules,
-    fuse_modules,
-    get_fuser_method,
-)  # noqa: F401
+from .fake_quantize import *  # noqa: F403
+from .fuse_modules import *  # noqa: F403
+from .quantize import *  # noqa: F403
