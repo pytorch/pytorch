79c8acf113957cffdfff60364d23c5d70943905d
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-09-16 15:36:34 +0000

    address review : update samples

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -2631,7 +2631,7 @@ def sample_inputs_conv2d(op_info, device, dtype, requires_grad, jit_fail_sample=
         ((1, 3, 4, 4), (3, 3, 3, 3), (3,),
             {'stride': (2, 2), 'padding': 2, 'groups': 1}),
         ((2, 4, 8, 8), (2, 2, 3, 3), (2,),
-            {'stride': (2, 2), 'padding': (2, 1), 'groups': 2, 'dilation': (4, 4)}),
+            {'stride': (3, 2), 'padding': (2, 1), 'groups': 2, 'dilation': (4, 4)}),
         ((1, 4, 5, 5), (1, 4, 2, 3), (1,),
             {'stride': 2, 'padding': 1, 'groups': 1, 'dilation': (2, 3)}),
         ((1, 4, 5, 5), (1, 4, 2, 3), (1,),
@@ -2645,7 +2645,8 @@ def sample_inputs_conv2d(op_info, device, dtype, requires_grad, jit_fail_sample=
         # Below are the group related samples from common_nn.py
         ((2, 4, 6, 6), (4, 1, 3, 3), (4,), {'groups': 4}),
         ((2, 4, 6, 6), (8, 1, 3, 3), (8,), {'groups': 4}),
-        ((2, 4, 6, 6), (4, 1, 3, 3), (4,), {'groups': 4, 'stride': (2, 2)}),
+        ((2, 4, 6, 6), (8, 1, 3, 3), None, {'groups': 4}),
+        ((2, 4, 6, 6), (4, 1, 3, 3), (4,), {'groups': 4, 'stride': (3, 2)}),
         ((2, 4, 6, 6), (4, 1, 3, 3), (4,), {'groups': 4, 'padding': (1, 1)}),
         ((2, 4, 5, 5), (4, 1, 2, 2), (4,), {'groups': 4, 'dilation': (2, 2)}),
         ((2, 4, 6, 5), (6, 2, 3, 2), (6,), {'groups': 2}),
