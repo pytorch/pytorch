2af7c6bdb0f07c94a9f77bb068345468eb486b12
Eli Uriegas <eliuriegas@fb.com>
2021-09-15 17:37:10 -0700

    Revert D30752939: [pytorch][PR] nvfuser update
    Test Plan: revert-hammer
    
    Differential Revision:
    D30752939 (https://github.com/pytorch/pytorch/commit/cfaecaf40bd6cabd3f4e0ef0d8c7252655349b61)
    
    Original commit changeset: ce122e80f01b
    
    fbshipit-source-id: 57685df8f9946032a06eff1de8a3d1498500d2d2

--- torch/csrc/jit/codegen/cuda/ops/all_ops.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <torch/csrc/jit/codegen/cuda/arith.h>
-#include <torch/csrc/jit/codegen/cuda/ops/composite.h>
-#include <torch/csrc/jit/codegen/cuda/ops/normalization.h>
--- torch/csrc/jit/codegen/cuda/transform_rfactor.cpp
+++ torch/csrc/jit/codegen/cuda/transform_rfactor.cpp
@@ -48,13 +48,13 @@ class ReplayRFactor : public ReplayTransformations {
       return ReplayTransformations::handle(s);
 
     // outer loop size
-    Val* remainder = ceilDiv(mapped->extent(), s->factor());
+    Val* oe = ceilDiv(mapped->extent(), s->factor());
 
     // Manually replay the split, making reduction = false and rfactor = true
     // outer IterDomain
     IterDomain* ido = new IterDomain(
         new Int(0),
-        s->innerSplit() ? remainder->as<Int>() : s->factor(),
+        oe->as<Int>(),
         mapped->getParallelType(),
         rfactor_outer ? IterType::Reduction : IterType::Iteration,
         true); // broadcast
@@ -62,13 +62,13 @@ class ReplayRFactor : public ReplayTransformations {
     // inner IterDomain
     IterDomain* idi = new IterDomain(
         new Int(0),
-        s->innerSplit() ? s->factor() : remainder->as<Int>(),
+        s->factor(),
         mapped->getParallelType(),
         rfactor_inner ? IterType::Reduction : IterType::Iteration,
         true);
 
     // Generate the split node
-    new Split(ido, idi, mapped, s->factor(), s->innerSplit());
+    new Split(ido, idi, mapped, s->factor());
 
     // Remove mapped id from leaf IDs
     leaf_ids_.erase(mapped);
@@ -153,7 +153,7 @@ class ReplayRFactor : public ReplayTransformations {
 TensorDomain* TransformRFactor::runReplay(
     TensorDomain* orig_td,
     std::vector<int> axes) {
-  FUSER_PERF_SCOPE("TransformRFactor::runReplay");
+  FUSER_PERF_SCOPE("runReplay");
 
   TORCH_CHECK(!axes.empty(), "No axes provided to rfactor replay.");
 
@@ -304,7 +304,7 @@ TensorDomain* TransformRFactor::runReplay(
 TensorDomain* TransformRFactor::runReplay2(
     TensorDomain* orig_td,
     std::vector<int> axes) {
-  FUSER_PERF_SCOPE("TransformRFactor::runReplay2");
+  FUSER_PERF_SCOPE("runReplay2");
 
   int ndims = (int)orig_td->nDims();
 
