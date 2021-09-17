fc7abb62b46d435efdcb425a3cbd35c5211be31c
erjia <erjia@fb.com>
2021-09-17 16:12:27 +0000

    Update on "[DataPipe] Fix deepcopy filehandle for Mapper and in-place modification for IterableWrapper"
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/utils.py
+++ torch/utils/data/datapipes/iter/utils.py
@@ -1,3 +1,5 @@
+import copy
+import warnings
 from torch.utils.data import IterDataPipe
 
 
@@ -13,7 +15,19 @@ class IterableWrapperIterDataPipe(IterDataPipe):
         self.iterable = iterable
 
     def __iter__(self):
-        for data in self.iterable:
+        try:
+            source_data = copy.deepcopy(self.iterable)
+        # For the case that data cannot be deep-copied,
+        # all in-place operations will affect iterable variable.
+        # When this DataPipe is iterated second time, it will
+        # yield modified items.
+        except TypeError:
+            warnings.warn(
+                "The input iterable can not be deepcopied, "
+                "please be aware of in-place modification would affect source data"
+            )
+            source_data = self.iterable
+        for data in source_data:
             yield data
 
     def __len__(self):
