ba3e4fc17e118c9599312538e5f9170b47f01a7b
jiej <jiej@nvidia.com>
2021-09-19 03:20:17 -0700

    fixing build

--- torch/csrc/autograd/FunctionsManual.h
+++ torch/csrc/autograd/FunctionsManual.h
@@ -91,7 +91,8 @@ at::Tensor _sparse_addmm_sparse_backward(const at::Tensor& grad, const at::Tenso
 at::Tensor sparse_sparse_matmul_backward(const at::Tensor& grad, const at::Tensor& mat1, const at::Tensor& mat2,int64_t grad_order);
 at::Tensor renorm_backward(const at::Tensor & grad, const at::Tensor & self, const at::Scalar& p, int64_t dim, const at::Scalar& maxnorm);
 at::Tensor repeat_backward(at::Tensor grad, at::IntArrayRef repeats, at::IntArrayRef input_shape);
-at::Tensor infinitely_differentiable_native_dropout_backward(at::Tensor grad, at::Tensor mask, double scale);
+at::Tensor infinitely_differentiable_native_dropout_backward(const at::Tensor& grad, const at::Tensor& mask, double scale);
+at::Tensor native_dropout_double_backward(const at::Tensor& ggI, const at::Tensor& grad, const at::Tensor& mask, double scale);
 at::Tensor evenly_distribute_backward(at::Tensor grad, const at::Tensor & input, const at::Tensor & value);
 at::Tensor sgn_backward(Tensor result, Tensor grad, Tensor self);
 at::Tensor var_backward(at::Tensor grad, const at::Tensor& self, c10::optional<IntArrayRef> dim, c10::optional<int64_t> correction, bool keepdim);
