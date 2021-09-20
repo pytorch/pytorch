20db9eb416b9461b827e1ef40e84e6f9adc960e5
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-09-20 13:39:38 +0000

    correct the docs

--- torch/nn/modules/distance.py
+++ torch/nn/modules/distance.py
@@ -20,7 +20,7 @@ class PairwiseDistance(Module):
     Shape:
         - Input1: :math:`(N, D)` or :math:`(D)` where `N = batch dimension` and `D = vector dimension`
         - Input2: :math:`(N, D)` or :math:`(D)`, same shape as the Input1
-        - Output: :math:`(N)` or :math:`(D)` based on input dimension.
+        - Output: :math:`(N)` or :math:`()` based on input dimension.
                 If :attr:`keepdim` is ``True``, then :math:`(N, 1)` or :math:`(1)` based on input dimension.
     Examples::
         >>> pdist = nn.PairwiseDistance(p=2)
