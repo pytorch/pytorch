c9b974709c1bbf69160a5e90d40505124e9e77ca
Keyrat06 <raoulemil@gmail.com>
2021-09-21 15:43:46 -0700

    [ONNX] Support torch.isfinite export (#64759)
    Summary:
    Pull Request resolved:  https://github.com/pytorch/pytorch/issues/64754
    
    1. onnx::IsInf is introduced in opset 10, onnx:isnan is introduced in opset 9 -> isfinite = not(or(isinf,isnan)) -> opset 10
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64759
    
    Test Plan: Imported from OSS
    
    Reviewed By: seemethere, bdhirsh
    
    Differential Revision: D31060760
    
    Pulled By: malfet
    
    fbshipit-source-id: 499ecd6cc55ea881b8a57e6a9a4fb38eaaee5242

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -7013,6 +7013,15 @@ class TestONNXRuntime(unittest.TestCase):
         x = torch.tensor([[1, 2, float("inf")], [2, float("nan"), float("inf")]])
         self.run_test(M(), (x, ))
 
+    @skipIfUnsupportedMinOpsetVersion(10)
+    def test_isfinite(self):
+        class M(torch.nn.Module):
+            def forward(self, x):
+                return x.isfinite()
+
+        x = torch.tensor([[1, 2, float("inf")], [2, float("nan"), -float("inf")]])
+        self.run_test(M(), (x, ))
+
     @skipIfUnsupportedMinOpsetVersion(9)  # ONNX IsNaN op is added in opset 9.
     def test_isnan(self):
         class M(torch.nn.Module):
