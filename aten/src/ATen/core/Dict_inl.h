19a10638881c56b022353e8c0c65ccf7b0c98de6
Pavel Belevich <pbelevich@fb.com>
2021-09-20 21:52:34 -0700

    [JIT] Support device as Dict key (#65079)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/65079
    
    This is required to use RPC DeviceMap aka Dict[torch.device, torch.device] in torchscript
    
    Test Plan: Imported from OSS
    
    Reviewed By: malfet
    
    Differential Revision: D31072626
    
    Pulled By: pbelevich
    
    fbshipit-source-id: 51cfa5653db86de73b624e9157d68d1b319bfc64

--- aten/src/ATen/core/Dict_inl.h
+++ aten/src/ATen/core/Dict_inl.h
@@ -50,6 +50,8 @@ inline size_t DictKeyHash::operator()(const IValue& ivalue) const {
     return std::hash<bool>()(ivalue.toBool());
   } else if (ivalue.isTensor()) {
     return std::hash<TensorImpl*>()(ivalue.toTensor().unsafeGetTensorImpl());
+  } else if (ivalue.isDevice()) {
+    return std::hash<Device>()(ivalue.toDevice());
   } else {
     throw std::runtime_error(
         "Can't hash IValues with tag '" + ivalue.tagKind() + "'");
