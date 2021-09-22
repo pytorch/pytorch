f363e52ed9460d9ffebcfb10e27096e15a520a84
jjsjann123 <jiej@nvidia.com>
2021-09-22 03:30:22 -0700

    clang_format

--- torch/csrc/jit/passes/autocast.cpp
+++ torch/csrc/jit/passes/autocast.cpp
@@ -7,8 +7,8 @@
 #include <c10/util/Optional.h>
 #include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/jit_log.h>
-#include <torch/csrc/jit/passes/quantization/helper.h>
 #include <torch/csrc/jit/passes/cuda_graph_fuser.h>
+#include <torch/csrc/jit/passes/quantization/helper.h>
 
 #include <stack>
 #include <unordered_set>
--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -264,9 +264,7 @@ void initJITBindings(PyObject* module) {
           })
       .def("_jit_pass_onnx_set_dynamic_input_shape", ONNXSetDynamicInputShape)
       .def("_jit_pass_autocast", Autocast)
-      .def(
-          "_jit_set_autocast_mode",
-          &setAutocastMode)
+      .def("_jit_set_autocast_mode", &setAutocastMode)
       .def("_jit_pass_fuse", FuseGraph)
       .def(
           "_jit_pass_dce",
