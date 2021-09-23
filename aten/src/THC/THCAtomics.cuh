a0fc0ab15fa412e3bd7739911758dcc74372fa4e
Peter Bell <peterbell10@live.co.uk>
2021-09-23 23:18:53 +0100

    Update base for Remove manual hipifying include path on "Migrate THCThrustAllocator to ATen"
    Differential Revision: [D31148180](https://our.internmc.facebook.com/intern/diff/D31148180)
    
    [ghstack-poisoned]

--- aten/src/THC/THCAtomics.cuh
+++ aten/src/THC/THCAtomics.cuh
@@ -1,7 +1,3 @@
 #pragma once
 // TODO: Remove once torchvision has been updated to use the ATen header
-#ifdef __CUDACC__
 #include <ATen/cuda/Atomic.cuh>
-#else
-#include <ATen/hip/Atomic.cuh>
-#endif
