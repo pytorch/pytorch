1260d978e08171ae1e418b128d5eefb195fc8083
Thomas J. Fan <thomasjpfan@gmail.com>
2021-09-23 15:58:19 -0400

    ENH Do not change type for list input

--- test/test_modules.py
+++ test/test_modules.py
@@ -207,7 +207,9 @@ class TestModule(TestCase):
             self.assertEqual(input_args[0].grad, input_arg_copy[0].grad)
 
     def _traverse_obj(self, obj, func):
-        if isinstance(obj, (tuple, list)) or isgenerator(obj):
+        if isinstance(obj, (tuple, list)):
+            return type(obj)(self._traverse_obj(o, func) for o in obj)
+        elif isgenerator(obj):
             return tuple(self._traverse_obj(o, func) for o in obj)
         elif isinstance(obj, dict):
             return {name: self._traverse_obj(o, func) for name, o in obj.items()}
