86a408220e20f5b8d45accbbbd5b55a74ac30ae2
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2021-09-23 05:06:51 +0000

    Use IntArrayRef's equality feature, improve error messages

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -70,17 +70,19 @@ void common_checks_baddbmm_bmm(Meta& meta, const Tensor& batch1, const Tensor& b
   int64_t contraction_size = batch1_sizes[2];
   int64_t res_rows = batch1_sizes[1];
   int64_t res_cols = batch2_sizes[2];
+  const IntArrayRef output_size = {bs, res_rows, res_cols};
 
-  TORCH_CHECK(batch2_sizes[0] == bs && batch2_sizes[1] == contraction_size);
+  TORCH_CHECK(batch2_sizes[0] == bs && batch2_sizes[1] == contraction_size,
+              "Expected size for first two dimensions of batch2 tensor to be: [",
+              bs, ", ", contraction_size, "] but got: [", batch2_sizes[0], ", ", batch2_sizes[1], "].");
 
   auto& result = meta.maybe_get_output(0);
   // 'set_output' does not resize for in-place calls
   meta.set_output({bs, res_rows, res_cols}, batch1.options());
   const auto result_sizes = result.sizes();
   // Error is raised if called from in-place overload with incorrect shape
-  TORCH_CHECK(result_sizes[0] == bs && result_sizes[1] == res_rows && result_sizes[2] == res_cols,
-              "Expected an output tensor with shape [",
-              bs, ", ", res_rows, ", ", res_cols, "] but got shape ", result_sizes);
+  TORCH_CHECK(result_sizes == output_size,
+              "Expected an output tensor with shape ", output_size, " but got shape ", result_sizes);
 
   std::vector<Dimname> outnames = {};
   if (!is_bmm) {
@@ -89,9 +91,8 @@ void common_checks_baddbmm_bmm(Meta& meta, const Tensor& batch1, const Tensor& b
       if (beta.toComplexDouble() != 0.0) result.copy_(self);
       TORCH_CHECK(self.dim() == 3, "self must be a 3D tensor");
       const auto self_sizes = self.sizes();
-      TORCH_CHECK(self_sizes[0] == bs && self_sizes[1] == res_rows && self_sizes[2] == res_cols,
-                  "Expected an input tensor shape with shape [",
-                  bs, ", ", res_rows, ", ", res_cols, "] but got shape ", self_sizes);
+      TORCH_CHECK(self_sizes == output_size,
+                  "Expected an input tensor shape with shape ", output_size, " but got shape: ", self_sizes);
       outnames = namedinference::compute_baddbmm_outnames(result, batch1, batch2, self);
     }
   } else {
