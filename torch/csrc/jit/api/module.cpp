c292b601884c9fcd19564858a63f9b325785cd18
Scott Wolchok <swolchok@fb.com>
2021-09-23 13:04:47 -0700

    Update base for Update on "[PyTorch] Adopt faster Tuple::create"
    The previous diff adds a way to make Tuples of size 3 or less
    more efficiently. This diff makes it easier to hit that path and
    updates a bunch of callsites to hit it.
    
    Differential Revision: [D31069538](https://our.internmc.facebook.com/intern/diff/D31069538/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D31069538/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/api/module.cpp
+++ torch/csrc/jit/api/module.cpp
@@ -260,7 +260,7 @@ IValue Module::operator()(std::vector<IValue> inputs) {
     IValue result = Method(_ivalue(), pre_hook)({tuple_input});
     if (!result.isNone()) {
       if (result.isTuple()) {
-        inputs = std::move(*std::move(result).toTuple()).elements().vec();
+        inputs = result.toTuple()->elements().vec();
       } else {
         inputs = {result};
       }
