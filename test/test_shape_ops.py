734b50ac4b81abcefcb2a2ecffd4ffda1787a21c
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-09-23 10:12:04 +0000

    remove unused import

--- test/test_shape_ops.py
+++ test/test_shape_ops.py
@@ -12,7 +12,7 @@ from torch.testing._internal.common_utils import (
     TestCase, run_tests, torch_to_numpy_dtype_dict)
 from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests, onlyCPU, onlyCUDA, dtypes, onlyNativeDeviceTypes,
-    dtypesIfCPU, dtypesIfCUDA, largeTensorTest)
+    dtypesIfCUDA, largeTensorTest)
 from torch.testing._internal.common_dtype import get_all_dtypes
 
 # TODO: replace with make_tensor
