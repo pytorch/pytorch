610afac107fddc3b8d2114646c6f157e4bdb3874
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-09-24 13:01:41 +0000

    Update base for Update on "Sparse CSR CPU: add `torch.addmm`"
    This PR adds `torch.addmm(c, a, b, alpha=1.0, beta=0.0, out=out)` variant with `a, b, c, out` all being sparse CSR tensors on CPU.
    
    cc nikitaved pearu cpuhrsch @IvanYashchuk
    
    [ghstack-poisoned]

--- aten/src/ATen/native/mkl/SparseBlasImpl.cpp
+++ aten/src/ATen/native/mkl/SparseBlasImpl.cpp
@@ -2,7 +2,6 @@
 #include <ATen/SparseCsrTensorImpl.h>
 #include <ATen/Tensor.h>
 #include <ATen/mkl/Sparse.h>
-#include <ATen/native/LinearAlgebraUtils.h>
 #include <ATen/native/mkl/SparseBlasImpl.h>
 
 #include <c10/core/ScalarType.h>
@@ -20,18 +19,6 @@ namespace impl {
 namespace mkl {
 
 namespace {
-
-c10::MaybeOwned<Tensor> inline prepare_dense_matrix_for_mkl(
-    const Tensor& tensor) {
-  if (tensor.is_non_overlapping_and_dense() ||
-      is_blas_compatible_row_major_order(tensor) ||
-      is_blas_compatible_column_major_order(tensor)) {
-    return c10::MaybeOwned<Tensor>::borrowed(tensor);
-  } else {
-    return c10::MaybeOwned<Tensor>::owned(
-        tensor.clone(at::MemoryFormat::Contiguous));
-  }
-}
 
 c10::MaybeOwned<Tensor> inline prepare_dense_vector_for_mkl(
     const Tensor& tensor) {
@@ -217,79 +204,6 @@ void add_out_sparse_csr(
             rows_C * sizeof(MKL_INT));
         result_crow_indices.data_ptr<MKL_INT>()[rows_C] = nnz_C;
       });
-#endif
-}
-
-void triangular_solve_out_sparse_csr(
-    const Tensor& A,
-    const Tensor& B,
-    const Tensor& X,
-    bool upper,
-    bool transpose,
-    bool unitriangular) {
-#if !AT_USE_MKL_SPARSE()
-  TORCH_CHECK(
-      false,
-      "Calling triangular_solve on a sparse CPU tensor requires Linux platform. ",
-      "Please use PyTorch built with MKL on Linux.");
-#else
-  if (B.numel() == 0 || X.numel() == 0 || A._nnz() == 0) {
-    return;
-  }
-
-  c10::MaybeOwned<Tensor> B_ = prepare_dense_matrix_for_mkl(B);
-  c10::MaybeOwned<Tensor> X_ = prepare_dense_matrix_for_mkl(X);
-
-  sparse_operation_t opA = transpose ? SPARSE_OPERATION_TRANSPOSE : SPARSE_OPERATION_NON_TRANSPOSE;
-  matrix_descr descrA;
-  descrA.type = SPARSE_MATRIX_TYPE_TRIANGULAR;
-  descrA.mode = upper ? SPARSE_FILL_MODE_UPPER : SPARSE_FILL_MODE_LOWER;
-  descrA.diag = unitriangular ? SPARSE_DIAG_UNIT : SPARSE_DIAG_NON_UNIT;
-
-  AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(
-      X.scalar_type(), "triangular_solve_out_sparse_csr_impl_mkl", [&] {
-        auto mkl_sparse_mat =
-            at::mkl::sparse::MklSparseCsrDescriptor<scalar_t>(A);
-        scalar_t alpha = 1;
-
-        if (B.size(-1) == 1) {
-          at::mkl::sparse::trsv<scalar_t>(
-              opA,
-              alpha,
-              mkl_sparse_mat.descriptor(),
-              descrA,
-              B_->data_ptr<scalar_t>(),
-              X_->data_ptr<scalar_t>());
-        } else {
-          // TODO: support mixed memory format
-          IntArrayRef X_strides = X_->strides();
-          IntArrayRef B_strides = B_->strides();
-          auto ndim = X_->dim();
-          bool is_X_row_major = (X_strides[ndim - 1] == 1);
-          bool is_B_row_major = (B_strides[ndim - 1] == 1);
-          TORCH_INTERNAL_ASSERT(is_X_row_major && is_B_row_major);
-
-          auto order = is_X_row_major ? SPARSE_LAYOUT_ROW_MAJOR : SPARSE_LAYOUT_COLUMN_MAJOR;
-          auto nrhs = mkl_int_cast(B.size(-1), "nrhs");
-          auto ldx = is_X_row_major ? X_strides[ndim - 2] : X_strides[ndim - 1];
-          auto ldb = is_B_row_major ? B_strides[ndim - 2] : B_strides[ndim - 1];
-          at::mkl::sparse::trsm<scalar_t>(
-              opA,
-              alpha,
-              mkl_sparse_mat.descriptor(),
-              descrA,
-              order,
-              B_->data_ptr<scalar_t>(),
-              nrhs,
-              ldb,
-              X_->data_ptr<scalar_t>(),
-              ldx);
-        }
-      });
-
-  if (!X.is_same(*X_)) {
-    X.copy_(*X_);
-  }
 #endif
 }
 
--- aten/src/ATen/native/sparse/SparseBlasImpl.h
+++ aten/src/ATen/native/sparse/SparseBlasImpl.h
@@ -22,14 +22,6 @@ void add_out_sparse_csr(
     const Scalar& alpha,
     const Tensor& result);
 
-void triangular_solve_out_sparse_csr(
-    const Tensor& A,
-    const Tensor& B,
-    const Tensor& X,
-    bool upper,
-    bool transpose,
-    bool unitriangular);
-
 } // namespace cpu
 } // namespace impl
 } // namespace sparse
