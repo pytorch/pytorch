31c223a718aef865c22193cc250c007bade8e664
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-09-24 11:44:24 -0700

    Update on "Add all_paths utility function"
    fixes: https://github.com/pytorch/pytorch/issues/65261
    Differential Revision: [D31163681](https://our.internmc.facebook.com/intern/diff/D31163681)
    
    [ghstack-poisoned]

--- test/package/test_digraph.py
+++ test/package/test_digraph.py
@@ -116,10 +116,10 @@ class TestDiGraph(PackageTestCase):
 
         result = g.all_paths("1", "3")
         # to get rid of indeterminism
-        actual_edges = set([i.strip("\n") for i in result.split(";")[2:-1]])
-        expected_edges = set(['"2" -> "3"', '"1" -> "7"', '"7" -> "8"',
-                          '"1" -> "2"', '"8" -> "3"'])
-        self.assertEqual(actual_edges, expected_edges)
+        actual = set([i.strip("\n") for i in result.split(";")[2:-1]])
+        expected = {'"2" -> "3"', '"1" -> "7"', '"7" -> "8"',
+                    '"1" -> "2"', '"8" -> "3"'}
+        self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
     run_tests()
