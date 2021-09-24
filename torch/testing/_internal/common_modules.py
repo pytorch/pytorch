d11d817e12639c2a8c587c02140084d4a9bc6fc6
Thomas J. Fan <thomasjpfan@gmail.com>
2021-09-24 15:03:45 -0700

    TST Adds test for non-contiguous tensors (#64954)
    Summary:
    Follow up to https://github.com/pytorch/pytorch/issues/61935
    
    This PR:
    
    1. Adds test for non-contiguous tensors
    2. Fixes bug in `NLLLoss` that was catch by the test.
    
    The reason this was not catch in `common_nn` is because `CriterionTest` overrides `test_cuda` but does not call `test_nonconfig`.
    
    cc albanD mruberry jbschlosser walterddr
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64954
    
    Reviewed By: zou3519
    
    Differential Revision: D31174149
    
    Pulled By: jbschlosser
    
    fbshipit-source-id: a16073e59b40ccc01c82ede016b63a8db2e810f5

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -195,6 +195,8 @@ def module_inputs_torch_nn_NLLLoss(module_info, device, dtype, requires_grad, **
 
     cases: List[Tuple[str, dict]] = [
         ('', {}),
+        ('reduction_sum', {'reduction': 'sum'}),
+        ('reduction_none', {'reduction': 'none'}),
         ('ignore_index', {'ignore_index': 2}),
         ('weights', {'weight': make_weight(10).abs()}),
         ('weights_ignore_index', {'weight': make_weight(10).abs(), 'ignore_index': 2}),
