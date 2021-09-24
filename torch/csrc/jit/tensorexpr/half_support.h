ea28743e8df27a70ca909b8b2150ca297d41200e
Mikhail Zolotukhin <mvz@fb.com>
2021-09-24 14:39:13 -0700

    [TensorExpr] Delete dtype_ field from Let - it should use its var's dtype.
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/half_support.h
+++ torch/csrc/jit/tensorexpr/half_support.h
@@ -122,10 +122,10 @@ class HalfRewriter : public IRMutator {
   }
 
   StmtPtr mutate(LetPtr v) override {
-    if (isHalf(v->dtype().scalar_type())) {
+    if (isHalf(v->value()->dtype().scalar_type())) {
       VarPtr load_new_var = alloc<Var>(v->var()->name_hint(), kFloat);
       ExprPtr new_value = alloc<Cast>(
-          v->dtype().cloneWithScalarType(ScalarType::Float),
+          v->value()->dtype().cloneWithScalarType(ScalarType::Float),
           v->value()->accept_mutator(this));
       var_map[v->var()] = load_new_var;
 
