2c5b22b1d4540c7a8f14909217a4786c6363ed38
vfdev <vfdev.5@gmail.com>
2021-09-26 00:28:50 +0200

    [functorch] Fixed unexpected failures with log_softmax (pytorch/functorch#142)

--- functorch/test/test_ops.py
+++ functorch/test/test_ops.py
@@ -346,7 +346,6 @@ class TestOperators(TestCase):
         xfail('block_diag'),
         xfail('nn.functional.dropout'),
         xfail('nn.functional.max_pool2d'),
-        xfail('log_softmax'),
     }))
     def test_vmapvjp(self, device, dtype, op):
         # These are too annoying to put into the list above
@@ -476,7 +475,6 @@ class TestOperators(TestCase):
         xfail('nn.functional.dropout'),
         xfail('nn.functional.max_pool2d'),
         xfail('nn.functional.batch_norm'),
-        xfail('log_softmax'),
     })
     def test_vmapvjp_has_batch_rule(self, device, dtype, op):
         # These are too annoying to put into the list above
