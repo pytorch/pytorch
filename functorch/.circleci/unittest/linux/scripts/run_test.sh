321ebb585d0b67d39ed7821ce1285c70220be7ee
vfdev <vfdev.5@gmail.com>
2021-09-26 06:04:00 +0200

    [functorch] Update run_test.sh (pytorch/functorch#143)
    Fixed `-v` position

--- functorch/.circleci/unittest/linux/scripts/run_test.sh
+++ functorch/.circleci/unittest/linux/scripts/run_test.sh
@@ -9,4 +9,4 @@ python -m torch.utils.collect_env
 
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
-find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py \) | xargs -I {} -n 1 bash -c "python -v {} || exit 255"
+find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py \) | xargs -I {} -n 1 bash -c "python {} -v || exit 255"
