609384c056756fec17d9ae2ab8e0cc5756b2dd80
Zafar <cc.rafaz@zafar.cc>
2021-09-28 14:11:12 -0700

    [sparsity][doc] Docstring for WeightNormSparsifier (#65294)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/65294
    
    This adds the docstring documentation to the WeightNormSparsifier and adds the typehints for the constructor args.
    Note, this does not require testing as only the doc is changed.
    
    Test Plan: Imported from OSS
    
    Reviewed By: gchanan
    
    Differential Revision: D31186827
    
    Pulled By: z-a-f
    
    fbshipit-source-id: c5010c9bba25b074c4cc6c88f251474b758f950d

--- torch/ao/sparsity/sparsifier/weight_norm_sparsifier.py
+++ torch/ao/sparsity/sparsifier/weight_norm_sparsifier.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from typing import Tuple
 
 import torch
 import torch.nn.functional as F
@@ -11,9 +12,37 @@ def _flat_idx_to_2d(idx, shape):
     return rows, cols
 
 class WeightNormSparsifier(BaseSparsifier):
+    r"""Weight-Norm Sparsifier
+
+    This sparsifier computes the norm of every sparse block and "zeroes-out" the
+    ones with the lowest norm. The level of sparsity defines how many of the
+    blocks is removed.
+
+    This sparsifier is controlled by three variables:
+    1. `sparsity_level` defines the number of *sparse blocks* that are zeroed-out
+    2. `sparse_block_shape` defines the shape of the sparse blocks. Note that
+        the sparse blocks originate at the zero-index of the tensor.
+    3. `zeros_per_block` is the number of zeros that we are expecting in each
+        sparse block. By default we assume that all elements within a block are
+        zeroed-out. However, setting this variable sets the target number of
+        zeros per block. The zeros within each block are chosen as the *smallest
+        absolute values*.
+
+    Args:
+
+        sparsity_level: The target level of sparsity
+        sparse_block_shape: The shape of a sparse block
+        zeros_per_block: Number of zeros in a sparse block
+
+    Note::
+        All arguments to the WeightNormSparsifier constructor are "default"
+        arguments and could be overriden by the configuration provided in the
+        `prepare` step.
+    """
     def __init__(self,
-                 sparsity_level=0.5, sparse_block_shape=(1, 4),
-                 zeros_per_block=None):
+                 sparsity_level: float = 0.5,
+                 sparse_block_shape: Tuple[int, int] = (1, 4),
+                 zeros_per_block: int = None):
         if zeros_per_block is None:
             zeros_per_block = reduce((lambda x, y: x * y), sparse_block_shape)
         defaults = {
