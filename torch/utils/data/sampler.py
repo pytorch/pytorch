1eb111c3f4714342345af54ae8887150e4379902
erjia <erjia@fb.com>
2021-09-29 22:26:23 +0000

    Update on "Expose iterator-local generator to RandomSampler instance"
    Make sure the iter-local generator is attached to Sampler. Then, the state of iter-local generator is able to be serialized and resumed.
    
    Differential Revision: [D31288686](https://our.internmc.facebook.com/intern/diff/D31288686)
    
    [ghstack-poisoned]

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -129,10 +129,10 @@ class RandomSampler(Sampler[int]):
 
         if self.replacement:
             for _ in range(self.num_samples // 32):
-                yield from torch.randint(high=n, size=(32,), dtype=torch.int64, generator=generator).tolist()
-            yield from torch.randint(high=n, size=(self.num_samples % 32,), dtype=torch.int64, generator=generator).tolist()
+                yield from torch.randint(high=n, size=(32,), dtype=torch.int64, generator=rng).tolist()
+            yield from torch.randint(high=n, size=(self.num_samples % 32,), dtype=torch.int64, generator=rng).tolist()
         else:
-            yield from torch.randperm(n, generator=generator).tolist()
+            yield from torch.randperm(n, generator=rng).tolist()
 
     def __len__(self) -> int:
         return self.num_samples
