546e8a060e4d68282232b77ea1232435dc9d0861
moderato <zhongyi.lin.926@gmail.com>
2021-09-30 13:21:18 -0700

    Removed unnecessary logic that causes failure in capture op call stack.

--- torch/csrc/autograd/python_function.cpp
+++ torch/csrc/autograd/python_function.cpp
@@ -664,19 +664,16 @@ PyObject *THPFunction_apply(PyObject *cls, PyObject *inputs)
 {
   HANDLE_TH_ERRORS
   auto info_pair = unpack_input<false>(inputs);
-  bool record = false;
-  if (C10_UNLIKELY(at::shouldRunRecordFunction(&record))) {
-    at::RecordFunction guard(at::RecordScope::FUNCTION);
-    if (guard.isActive()) {
-      if (guard.needsInputs()) {
-        auto iv_inputs = std::vector<c10::IValue>();
-        for (auto const &var : info_pair.first.input_vars) {
-           iv_inputs.emplace_back(c10::IValue(var));
-        }
-        guard.before(((PyTypeObject*)cls)->tp_name, iv_inputs, at::sequence_number::peek());
-      } else {
-        guard.before(((PyTypeObject*)cls)->tp_name, at::sequence_number::peek());
+  at::RecordFunction guard(at::RecordScope::FUNCTION);
+  if (guard.isActive()) {
+    if (guard.needsInputs()) {
+      auto iv_inputs = std::vector<c10::IValue>();
+      for (auto const &var : info_pair.first.input_vars) {
+         iv_inputs.emplace_back(c10::IValue(var));
       }
+      guard.before(((PyTypeObject*)cls)->tp_name, iv_inputs, at::sequence_number::peek());
+    } else {
+      guard.before(((PyTypeObject*)cls)->tp_name, at::sequence_number::peek());
     }
   }
 
