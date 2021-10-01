2e594746846d87c77101f5569b4819eaa30a9ba4
Tao Xu <taox@fb.com>
2021-09-30 20:48:16 -0700

    [iOS][CI] Update dev certs
    [ghstack-poisoned]

--- .circleci/scripts/binary_ios_test.sh
+++ .circleci/scripts/binary_ios_test.sh
@@ -8,7 +8,7 @@ cd ${PROJ_ROOT}/ios/TestApp
 # install fastlane
 sudo gem install bundler && bundle install
 # install certificates
-echo "${IOS_CERT_KEY}" >> cert.txt
+echo "${IOS_CERT_KEY_2022}" >> cert.txt
 base64 --decode cert.txt -o Certificates.p12
 rm cert.txt
 bundle exec fastlane install_cert
@@ -17,7 +17,7 @@ PROFILE=PyTorch_CI_2021.mobileprovision
 PROVISIONING_PROFILES=~/Library/MobileDevice/Provisioning\ Profiles
 mkdir -pv "${PROVISIONING_PROFILES}"
 cd "${PROVISIONING_PROFILES}"
-echo "${IOS_SIGN_KEY}" >> cert.txt
+echo "${IOS_SIGN_KEY_2022}" >> cert.txt
 base64 --decode cert.txt -o ${PROFILE}
 rm cert.txt
 # run the ruby build script
