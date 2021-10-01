940cd880ece457e11fd8b80fd95d101740b8e9cc
chenlai <chenlai@devvm4827.prn0.facebook.com>
2021-09-30 17:36:28 -0700

    [PyTorch Edge][type] Move TypeParser class definition to header file
    More TypeParser class to header file so it can be called from somewhere else. For example, the getContainedTypes() api in this stack can be moved to other files.
    
    Differential Revision: [D31294254](https://our.internmc.facebook.com/intern/diff/D31294254/)
    
    [ghstack-poisoned]

--- torch/csrc/jit/mobile/type_parser.h
+++ torch/csrc/jit/mobile/type_parser.h
@@ -1,5 +1,30 @@
 #include <ATen/core/jit_type.h>
 
 namespace c10 {
+class TypeParser {
+ public:
+  explicit TypeParser(std::string pythonStr);
+
+  TypePtr parse();
+
+ private:
+  TypePtr parseClassType();
+
+  void expect(const std::string& s);
+
+  template <class T>
+  TypePtr CreateSingleElementType();
+
+  void lex();
+
+  std::string next();
+
+  std::string& cur();
+
+  std::string pythonStr_;
+  size_t start_;
+  std::string next_token_;
+};
+
 TORCH_API TypePtr parseType(const std::string& pythonStr);
 } // namespace c10
