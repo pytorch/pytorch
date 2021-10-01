f8e6c7155d97abc6683b95c4df79bfffa218549f
zaf <zaf@devgpu109.ash6.facebook.com>
2021-10-01 15:22:19 -0700

    Update base for Update on "[ao_migration] torch/ao/ns: torch.quantization -> torch.ao.quantization"
    This changes the imports in the `caffe2/torch/ao/ns` to include the new import locations.
    
    Differential Revision: [D31299692](https://our.internmc.facebook.com/intern/diff/D31299692/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D31299692/)!
    
    [ghstack-poisoned]

--- torch/ao/__init__.py
+++ torch/ao/__init__.py
@@ -1,2 +1,3 @@
-from torch.ao import nn
-from torch.ao import sparsity
+from torch.ao import nn  # noqa: F401
+from torch.ao import quantization  # noqa: F401
+from torch.ao import sparsity  # noqa: F401
