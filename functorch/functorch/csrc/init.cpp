8d6984b56650872c6bdd406471271342bf8325a3
Bert Maher <bertrand@fb.com>
2021-10-01 02:25:10 +0000

    [functorch] Python bindings for compilation cache

--- functorch/functorch/csrc/init.cpp
+++ functorch/functorch/csrc/init.cpp
@@ -13,6 +13,7 @@
 #include <functorch/csrc/VmapTransforms.h>
 #include <functorch/csrc/BatchedFallback.h>
 #include <functorch/csrc/BatchRulesHelper.h>
+#include <functorch/csrc/CompileCache.h>
 
 
 namespace at {
@@ -250,4 +251,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("get_unwrapped", &at::functorch::get_unwrapped);
   m.def("maybe_get_level", &at::functorch::maybe_get_level);
   m.def("maybe_get_bdim", &at::functorch::maybe_get_bdim);
+  at::functorch::initCompileCacheBindings(m.ptr());
 }
