f21956e3296d900a12c6ccfd588f54653f2a4967
Bert Maher <bertmaher@users.noreply.github.com>
2021-10-01 23:12:17 -0400

    [functorch] Revert the compile cache (pytorch/functorch#168)

--- functorch/functorch/__init__.py
+++ functorch/functorch/__init__.py
@@ -22,8 +22,6 @@ from ._src.make_functional import functional_init, functional_init_with_buffers
 from ._src.python_key import wrap_key, PythonTensor, pythonkey_trace, make_fx, nnc_jit, make_nnc
 from ._src.nnc_compile import nnc_compile, get_ops
 from ._src.eager_compilation import compiled_function, compiled_module, tvm_compile, draw_joint_graph, default_partition
-from ._src.operator_authoring import pointwise_operator
-
 
 # Monkeypatching lol
 _old_cross_entropy = torch.nn.functional.cross_entropy
