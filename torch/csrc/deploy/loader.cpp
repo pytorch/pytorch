pytorch/pytorch

commit a6280ab653efbf3c22e9f2dcc0dde7d22edc9a92

Compile without -Wno-unused-variable (#65954)
Summary:
Delete `-Wno-unused-variable` from top level `CMakeLists.txt`
Still suppress those warnings for tests and `torch_python`

Delete number of unused variables from caffe2 code
Use `(void)var;` to suppress unused variable in range loops
Use `C10_UNUSED` for global constructors and use `constexpr` instead of `static` for global constants

Pull Request resolved: https://github.com/pytorch/pytorch/pull/65954

Reviewed By: ngimel

Differential Revision: D31326599

Pulled By: malfet

fbshipit-source-id: 924155f1257a2ba1896c50512f615e45ca1f61f3

CMakeLists.txt
@@ -744,7 +744,6 @@ if(NOT MSVC)
   string(APPEND CMAKE_CXX_FLAGS " -Wno-unknown-pragmas")
   string(APPEND CMAKE_CXX_FLAGS " -Wno-sign-compare")
   string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-parameter")
-  string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-variable")
   string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-function")
   string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-result")
   string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-local-typedefs")

benchmarks/cpp/nvfuser/CMakeLists.txt
@@ -15,4 +15,8 @@ if(USE_CUDA)
     main.cpp)
 
   target_link_libraries(nvfuser_bench PRIVATE torch_library benchmark)
+  if(NOT MSVC)
+    target_compile_options(nvfuser_bench PRIVATE -Wno-unused-variable)
+  endif()
+
 endif()

c10/test/CMakeLists.txt
@@ -6,6 +6,9 @@ if(BUILD_TEST)
     get_filename_component(test_file_name ${test_src} NAME_WE)
     set(test_name "c10_${test_file_name}")
     add_executable(${test_name} "${test_src}")
+    if(NOT MSVC)
+      target_compile_options(${test_name} PRIVATE -Wno-unused-variable)
+    endif()
     target_link_libraries(${test_name} c10 gmock gtest gtest_main)
     add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
     if(INSTALL_TEST)

caffe2/CMakeLists.txt
@@ -1762,6 +1762,9 @@ if(BUILD_TEST)
     target_include_directories(${test_name} PRIVATE $<INSTALL_INTERFACE:include>)
     target_include_directories(${test_name} PRIVATE $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>)
     target_include_directories(${test_name} PRIVATE ${Caffe2_CPU_INCLUDE})
+    if(NOT MSVC)
+      target_compile_options(${test_name} PRIVATE -Wno-unused-variable)
+    endif()
     add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
     if(INSTALL_TEST)
       install(TARGETS ${test_name} DESTINATION test)

test/cpp/api/CMakeLists.txt
@@ -53,6 +53,9 @@ endif()
 add_executable(test_api ${TORCH_API_TEST_SOURCES})
 target_include_directories(test_api PRIVATE ${ATen_CPU_INCLUDE})
 target_link_libraries(test_api PRIVATE torch gtest)
+if(NOT MSVC)
+  target_compile_options(test_api PRIVATE -Wno-unused-variable)
+endif()
 
 if(USE_CUDA)
   target_link_libraries(test_api PRIVATE

test/cpp/jit/CMakeLists.txt
@@ -97,6 +97,9 @@ endif(MSVC)
 
 target_link_libraries(test_jit PRIVATE ${JIT_TEST_DEPENDENCIES})
 target_include_directories(test_jit PRIVATE ${ATen_CPU_INCLUDE})
+if(NOT MSVC)
+  target_compile_options(test_jit PRIVATE -Wno-unused-variable)
+endif()
 
 if(LINUX)
   #Update to target_link_options when CMake version can be upgraded

test/cpp/tensorexpr/CMakeLists.txt
@@ -38,6 +38,9 @@ add_executable(test_tensorexpr
 target_link_libraries(test_tensorexpr PRIVATE torch gtest)
 target_include_directories(test_tensorexpr PRIVATE ${ATen_CPU_INCLUDE})
 target_compile_definitions(test_tensorexpr PRIVATE USE_GTEST)
+if(NOT MSVC)
+  target_compile_options(test_tensorexpr PRIVATE -Wno-unused-variable)
+endif()
 
 add_executable(tutorial_tensorexpr ${TENSOREXPR_TEST_ROOT}/tutorial.cpp)
 target_link_libraries(tutorial_tensorexpr PRIVATE torch)

torch/CMakeLists.txt
@@ -354,6 +354,10 @@ if(USE_PRECOMPILED_HEADERS)
       "$<$<COMPILE_LANGUAGE:CXX>:ATen/ATen.h>")
 endif()
 
+if(NOT MSVC)
+  target_compile_options(torch_python PRIVATE -Wno-unused-variable)
+endif()
+
 # Required workaround for generated sources
 # See https://samthursfield.wordpress.com/2015/11/21/cmake-dependencies-between-targets-and-files-and-custom-commands/#custom-commands-in-different-directories
 add_dependencies(torch_python generate-torch-sources)

commit b57bd27a3805f13372b897d4e2f5b74a93b24f89

Fix cang-tidy regressions caused by #65954
Also, build `libtorch` with `-Wno-unused-variable` (as it includes JNI
library, that violates that rules)

CMakeLists.txt
@@ -1067,6 +1067,9 @@ endif()
 
 # ---[ JNI
 if(BUILD_JNI)
+  if(NOT MSVC)
+    string(APPEND CMAKE_CXX_FLAGS " -Wno-unused-variable")
+  endif()
   set(BUILD_LIBTORCH_WITH_JNI 1)
   set(FBJNI_SKIP_TESTS 1)
   add_subdirectory(android/pytorch_android)

torch/csrc/deploy/loader.cpp
@@ -1046,7 +1046,6 @@ struct __attribute__((visibility("hidden"))) CustomLibraryImpl
   }
 
   at::optional<TLSIndex> tls_lookup_symbol(Elf64_Xword r_info) {
-    const uint32_t r_type = ELF64_R_TYPE(r_info);
     const uint32_t r_sym = ELF64_R_SYM(r_info);
 
     if (r_sym == 0) {

torch/utils/benchmark/utils/timeit_template.cpp
@@ -31,6 +31,7 @@ double timeit(int n) {
     // Main loop
     auto start_time = std::chrono::high_resolution_clock::now();
     for(const auto loop_idx : c10::irange(n)) {
+        (void)loop_idx;
         // STMT_TEMPLATE_LOCATION
     }
     auto end_time = std::chrono::high_resolution_clock::now();

torch/utils/benchmark/utils/valgrind_wrapper/timer_callgrind_template.cpp
@@ -44,14 +44,17 @@ int main(int argc, char* argv[]) {
 
     // Warmup
     for(const auto i : c10::irange(number_warmup)) {
+        (void)i;
         // STMT_TEMPLATE_LOCATION
     }
 
     // Main loop
     for(const auto repeat : c10::irange(repeats)) {
+        (void)repeat;
         CALLGRIND_TOGGLE_COLLECT;
 
         for(const auto i : c10::irange(number)) {
+        (void)i;
         // STMT_TEMPLATE_LOCATION
         }
 

test/benchmark_utils/test_benchmark_utils.py
@@ -170,7 +170,7 @@ class TestBenchmarkUtils(TestCase):
     @unittest.skipIf(IS_SANDCASTLE, "C++ timing is OSS only.")
     def test_timer_tiny_fast_snippet(self):
         timer = benchmark_utils.Timer(
-            'auto x = 1;',
+            'auto x = 1;(void)x;',
             timer=timeit.default_timer,
             language=benchmark_utils.Language.CPP,
         )

torch/csrc/deploy/test_deploy.cpp
@@ -107,8 +107,8 @@ TEST(TorchpyTest, MultiSerialSimpleModel) {
   size_t ninterp = 3;
   std::vector<at::Tensor> outputs;
 
-  // NOLINTNEXTLINE(clang-analyzer-deadcode.DeadStores)
   for (const auto i : c10::irange(ninterp)) {
+    (void)i;
     outputs.push_back(model({input.alias()}).toTensor());
   }
 
@@ -151,11 +151,12 @@ TEST(TorchpyTest, ThreadedSimpleModel) {
   std::vector<at::Tensor> outputs;
 
   std::vector<std::future<at::Tensor>> futures;
-  // NOLINTNEXTLINE(clang-analyzer-deadcode.DeadStores)
   for (const auto i : c10::irange(nthreads)) {
+    (void)i;
     futures.push_back(std::async(std::launch::async, [&model]() {
       auto input = torch::ones({10, 20});
-      for (const auto i : c10::irange(100)) {
+      for (const auto j : c10::irange(100)) {
+        (void)j;
         model({input.alias()}).toTensor();
       }
       auto result = model({input.alias()}).toTensor();
@@ -230,8 +231,8 @@ TEST(TorchpyTest, TaggingRace) {
   constexpr int64_t trials = 4;
   constexpr int64_t nthreads = 16;
   torch::deploy::InterpreterManager m(nthreads);
-  // NOLINTNEXTLINE(clang-analyzer-deadcode.DeadStores)
   for (const auto n : c10::irange(trials)) {
+    (void)n;
     at::Tensor t = torch::empty(2);
     std::atomic<int64_t> success(0);
     std::atomic<int64_t> failed(0);
@@ -283,6 +284,7 @@ TEST(TorchpyTest, FxModule) {
   std::vector<at::Tensor> outputs;
   auto input = torch::ones({5, 10});
   for (const auto i : c10::irange(nthreads)) {
+    (void)i;
     outputs.push_back(model({input.alias()}).toTensor());
   }
 