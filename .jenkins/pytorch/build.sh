ad6b84fec1372c09b945656a8f019c3a0dbb4097
Eli Uriegas <eliuriegas@fb.com>
2021-10-05 16:06:11 -0700

    ci: Migrate vulkan builds to GHA (#66044)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66044
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    Test Plan: Imported from OSS
    
    Reviewed By: malfet
    
    Differential Revision: D31370889
    
    Pulled By: seemethere
    
    fbshipit-source-id: 399f5f0c184f7856dcddb138c357f1374706e676

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -131,7 +131,7 @@ if [[ "${BUILD_ENVIRONMENT}" == *-android* ]]; then
   exec ./scripts/build_android.sh "${build_args[@]}" "$@"
 fi
 
-if [[ "$BUILD_ENVIRONMENT" != *android* && "$BUILD_ENVIRONMENT" == *vulkan-linux* ]]; then
+if [[ "$BUILD_ENVIRONMENT" != *android* && "$BUILD_ENVIRONMENT" == *vulkan* ]]; then
   export USE_VULKAN=1
   # shellcheck disable=SC1091
   source /var/lib/jenkins/vulkansdk/setup-env.sh
