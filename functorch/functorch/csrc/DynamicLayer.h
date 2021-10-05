e2e0e4a10a6c3a788c67e555bb7f2a4fd296ad73
Richard Zou <zou3519@users.noreply.github.com>
2021-10-05 12:59:54 -0400

    [functorch] no_grad fix (#179)
    Fixes https://github.com/facebookresearch/functorch/issues/13
    
    Case 1: grad gets called inside torch.no_grad.
    
    - grad should ignore torch.no_grad because it's "creating a new level of
    autograd above the current level"
    - Another way to think about this is that grad(f) is a "function
    transform": its result should not be affected by context managers that
    are outside of the function f
    
    Case 2: torch.no_grad gets called inside `grad`
    - grad should respect torch.no_grad
    
    See NOTE [grad and vjp interaction with no_grad] for implementation
    strategy. It unfortunately involves a mode.
    
    Test Plan:
    - Many tests

--- functorch/functorch/csrc/DynamicLayer.h
+++ functorch/functorch/csrc/DynamicLayer.h
@@ -17,21 +17,43 @@ namespace at {
 namespace functorch {
 
 struct TORCH_API DynamicLayer {
-  DynamicLayer(DispatchKey key, int64_t layerId, optional<int64_t> batchSize = nullopt): key_(key), layerId_(layerId), batchSize_(batchSize) {}
+  DynamicLayer(
+      DispatchKey key,
+      int64_t layerId,
+      optional<int64_t> batchSize = nullopt,
+      optional<bool> prev_grad_mode = nullopt):
+    key_(key), layerId_(layerId), batchSize_(batchSize), prevGradMode_(prev_grad_mode)
+  {
+    if (key_ == DispatchKey::Autograd) {
+      TORCH_INTERNAL_ASSERT(prev_grad_mode.has_value());
+    }
+  }
 
   DispatchKey key() const { return key_; }
   int64_t layerId() const { return layerId_; }
+  // Only valid for vmap
   int64_t batchSize() const {
     TORCH_INTERNAL_ASSERT(batchSize_);
     return *batchSize_;
   }
+  // only valid for grad-based transforms
+  optional<bool> prevGradMode() const {
+    return prevGradMode_;
+  }
  private:
   DispatchKey key_;
   int64_t layerId_;
+
+  // Honestly these should be a union or some extendable metadata class.
+  // Not doing that for now because I don't think we'll use this mechanism for very long.
   optional<int64_t> batchSize_;
+  optional<bool> prevGradMode_;
 };
 
-TORCH_API int64_t initAndPushDynamicLayer(DispatchKey key, optional<int64_t> batch_size = nullopt);
+TORCH_API int64_t initAndPushDynamicLayer(
+    DispatchKey key,
+    optional<int64_t> batch_size = nullopt,
+    optional<bool> prev_grad_mode = nullopt);
 TORCH_API DynamicLayer popDynamicLayerAndDeleteMetadata();
 TORCH_API c10::optional<DynamicLayer> maybeCurrentDynamicLayer();
 TORCH_API const std::vector<DynamicLayer>& getDynamicLayerStack();
