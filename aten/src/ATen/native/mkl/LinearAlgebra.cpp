d2ab268050ce87c07dc186211e3c6250b767ba1a
Peter Bell <peterbell10@live.co.uk>
2021-10-06 12:09:03 -0700

    slow_conv3d: Avoid dispatch in parallel region (#65737)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/65737
    
    See gh-56794
    
    Avoid dispatch inside of parallel_for by:
    - Replacing Tensor slicing with TensorAccessor
    - Copy bias into output only once, outside of the parallel region
    - Replaces `addmm_` and `baddbmm_` with direct calls to gemm.
    
    Test Plan: Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D31257874
    
    Pulled By: ngimel
    
    fbshipit-source-id: 20b94daa13082fb1e39eaa8144bfa4c611b61bab

--- aten/src/ATen/native/mkl/LinearAlgebra.cpp
+++ aten/src/ATen/native/mkl/LinearAlgebra.cpp
@@ -1,3 +1,4 @@
+#include <ATen/native/mkl/LinearAlgebra.h>
 #include <ATen/ATen.h>
 #include <ATen/NativeFunctions.h>
 #include <ATen/Config.h>
@@ -5,6 +6,38 @@
 #if !AT_MKL_ENABLED()
 
 namespace at { namespace native {
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const float alpha,
+    const float** A, const int lda, const float** B, const int ldb, const float beta,
+    float** C, const int ldc) {
+  TORCH_INTERNAL_ASSERT(false, "mkl_gemm_batched: ATen not compiled with MKL support");
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const double alpha,
+    const double** A, const int lda, const double** B, const int ldb, const double beta,
+    double** C, const int ldc) {
+  TORCH_INTERNAL_ASSERT(false, "mkl_gemm_batched: ATen not compiled with MKL support");
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const c10::complex<float> alpha,
+    const c10::complex<float>** A, const int lda, const c10::complex<float>** B, const int ldb,
+    const c10::complex<float> beta, c10::complex<float>** C, const int ldc) {
+  TORCH_INTERNAL_ASSERT(false, "mkl_gemm_batched: ATen not compiled with MKL support");
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const c10::complex<double> alpha,
+    const c10::complex<double>** A, const int lda, const c10::complex<double>** B, const int ldb,
+    const c10::complex<double> beta, c10::complex<double>** C, const int ldc) {
+  TORCH_INTERNAL_ASSERT(false, "mkl_gemm_batched: ATen not compiled with MKL support");
+}
 
 Tensor& _baddbmm_mkl_(Tensor& self, const Tensor& batch1, const Tensor& batch2, const Scalar& beta, const Scalar& alpha) {
   AT_ERROR("bmm: ATen not compiled with MKL support");
@@ -98,6 +131,63 @@ static inline void gemm_batched(const CBLAS_TRANSPOSE trans_A, const CBLAS_TRANS
   cblas_zgemm_batch(CblasRowMajor, &trans_A, &trans_B, &M, &N, &K, reinterpret_cast<const void*>(&alpha),
     reinterpret_cast<const void**>(A), &lda, reinterpret_cast<const void**>(B), &ldb,
     reinterpret_cast<const void*>(&beta), reinterpret_cast<void**>(C), &ldc, 1, &batch_size);
+}
+
+static CBLAS_TRANSPOSE to_cblas(TransposeType x) {
+  switch (x) {
+    case TransposeType::NoTranspose: return CblasNoTrans;
+    case TransposeType::Transpose: return CblasTrans;
+    case TransposeType::ConjTranspose: return CblasConjTrans;
+  }
+  TORCH_INTERNAL_ASSERT(false, "Unknown TransposeType");
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const float alpha,
+    const float** A, const int lda, const float** B, const int ldb, const float beta,
+    float** C, const int ldc) {
+  auto transa_cblas = to_cblas(trans_A);
+  auto transb_cblas = to_cblas(trans_B);
+  cblas_sgemm_batch(CblasColMajor, &transa_cblas, &transb_cblas, &M, &N, &K, &alpha,
+                    A, &lda, B, &ldb, &beta, C, &ldc, 1, &batch_size);
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const double alpha,
+    const double** A, const int lda, const double** B, const int ldb, const double beta,
+    double** C, const int ldc) {
+  auto transa_cblas = to_cblas(trans_A);
+  auto transb_cblas = to_cblas(trans_B);
+  cblas_dgemm_batch(CblasColMajor, &transa_cblas, &transb_cblas, &M, &N, &K, &alpha,
+                    A, &lda, B, &ldb, &beta, C, &ldc, 1, &batch_size);
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const c10::complex<float> alpha,
+    const c10::complex<float>** A, const int lda, const c10::complex<float>** B, const int ldb,
+    const c10::complex<float> beta, c10::complex<float>** C, const int ldc) {
+  auto transa_cblas = to_cblas(trans_A);
+  auto transb_cblas = to_cblas(trans_B);
+  cblas_cgemm_batch(CblasColMajor, &transa_cblas, &transb_cblas, &M, &N, &K,
+                    reinterpret_cast<const void*>(&alpha),
+                    reinterpret_cast<const void**>(A), &lda, reinterpret_cast<const void**>(B), &ldb,
+                    reinterpret_cast<const void*>(&beta), reinterpret_cast<void**>(C), &ldc, 1, &batch_size);
+}
+
+void mkl_gemm_batched(
+    const TransposeType trans_A, const TransposeType trans_B,
+    const int batch_size, const int M, const int N, const int K, const c10::complex<double> alpha,
+    const c10::complex<double>** A, const int lda, const c10::complex<double>** B, const int ldb,
+    const c10::complex<double> beta, c10::complex<double>** C, const int ldc) {
+  auto transa_cblas = to_cblas(trans_A);
+  auto transb_cblas = to_cblas(trans_B);
+  cblas_zgemm_batch(CblasColMajor, &transa_cblas, &transb_cblas, &M, &N, &K,
+                    reinterpret_cast<const void*>(&alpha),
+                    reinterpret_cast<const void**>(A), &lda, reinterpret_cast<const void**>(B), &ldb,
+                    reinterpret_cast<const void*>(&beta), reinterpret_cast<void**>(C), &ldc, 1, &batch_size);
 }
 
 template <typename scalar_t>
