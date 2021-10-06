e26a33a7035663ef4edebb204a6d945b8d4b52bc
Richard Zou <zou3519@users.noreply.github.com>
2021-10-06 11:36:38 -0400

    [functorch] Fix CI by unpinning the binary version (pytorch/functorch#187)

--- functorch/.circleci/unittest/linux/scripts/install.sh
+++ functorch/.circleci/unittest/linux/scripts/install.sh
@@ -55,15 +55,9 @@ pip install expecttest
 if [ "${CU_VERSION:-}" == cpu ] ; then
     # conda install -y pytorch torchvision cpuonly -c pytorch-nightly
     # use pip to install pytorch as conda can frequently pick older release
-    # TEMPORARY FIX PYTORCH VERSION DUE TO REVERTED log_softmax PR
-    # https://github.com/facebookresearch/functorch/pull/160#issuecomment-932389442
-    # pip install torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html --pre
-    pip install torch==1.11.0.dev20211001+cpu torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html --pre
+    pip install torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html --pre
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 else
-    # conda install -y pytorch== torchvision cudatoolkit=10.2 -c pytorch-nightly
-    # TEMPORARY FIX PYTORCH VERSION DUE TO REVERTED log_softmax PR
-    # https://github.com/facebookresearch/functorch/pull/160#issuecomment-932389442
-    pip install torch==1.11.0.dev20211001+cu102 torchvision -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html --pre
+    conda install -y pytorch== torchvision cudatoolkit=10.2 -c pytorch-nightly
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 fi
