0354206ba4c7d782e435e89981363e81bb98c1ee
Peter Bell <peterbell10@live.co.uk>
2021-10-06 07:23:16 -0700

    [quant][fx] Don't assume bias is a keyword argument (#61647)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61647
    
    `prepare_fx` currently assumes that bias is always a positional argument to
    convolutions, and only a keyword argument to other functions. This happens to work
    today due to a quirk in how `__torch_function__` is handled for python
    functions but shouldn't be considered stable.
    
    Instead, we should support `bias` for both positional and keyword forms.
    
    cc jerryzh168 jianyuh raghuramank100 jamesr66a vkuzo
    
    Test Plan: Imported from OSS
    
    Reviewed By: ngimel
    
    Differential Revision: D31401360
    
    Pulled By: albanD
    
    fbshipit-source-id: 1e2f53d80e2176b870f326dc498e251e2386136e

--- torch/ao/quantization/fx/utils.py
+++ torch/ao/quantization/fx/utils.py
@@ -27,11 +27,14 @@ WEIGHT_INDEX_DICT = {
 
 NON_QUANTIZABLE_WEIGHT_OPS = {torch.nn.functional.layer_norm, torch.nn.functional.group_norm, torch.nn.functional.instance_norm}
 
-FUNCTIONAL_OPS_WITH_BIAS = {
-    torch.nn.functional.linear,
-    torch.nn.functional.layer_norm,
-    torch.nn.functional.group_norm,
-    torch.nn.functional.instance_norm
+BIAS_INDEX_DICT = {
+    torch.nn.functional.conv1d : 2,
+    torch.nn.functional.conv2d : 2,
+    torch.nn.functional.conv3d : 2,
+    torch.nn.functional.linear : 2,
+    torch.nn.functional.layer_norm : 3,
+    torch.nn.functional.group_norm : 3,
+    torch.nn.functional.instance_norm : 4,
 }
 
 # turn foo.bar -> ['foo', 'bar']
