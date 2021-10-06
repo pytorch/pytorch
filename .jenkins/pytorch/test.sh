8fdfbb94dfab42e23773df391426066f019fc44c
Nikita Shulga <nshulga@fb.com>
2021-10-05 20:12:13 -0700

    Fix backward compatibility tests (#66186)
    Compare operator list against RC1 build rather than against nightly

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -422,7 +422,7 @@ test_backward_compatibility() {
   python -m venv venv
   # shellcheck disable=SC1091
   . venv/bin/activate
-  pip_install --pre torch -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+  pip_install --pre torch -f https://download.pytorch.org/whl/test/cpu/torch_nightly.html
   pip show torch
   python dump_all_function_schemas.py --filename nightly_schemas.txt
   deactivate
