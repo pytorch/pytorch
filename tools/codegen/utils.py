33dd905fb484a851b07e354c360aea14304dbd78
Brian Hirsh <hirsheybar@fb.com>
2021-10-06 20:16:03 -0700

    Update on "assert no duplicate yaml keys in codegen"
    The codegen should error if it sees two yaml entries with the same key. The default behavior of python's yaml loader is to overwrite duplicate keys with the new value (see https://github.com/yaml/pyyaml/issues/165).
    
    This would have caught a nasty bug that showed up in https://github.com/pytorch/pytorch/pull/66225/files#r723796194.
    
    I tested it on that linked PR, to confirm that it errors correctly (and gives the line number containing the duplicate).
    
    
    Differential Revision: [D31464585](https://our.internmc.facebook.com/intern/diff/D31464585)
    
    [ghstack-poisoned]

--- tools/codegen/utils.py
+++ tools/codegen/utils.py
@@ -22,7 +22,7 @@ class YamlLoader(Loader):
     def construct_mapping(self, node, deep=False):  # type: ignore[no-untyped-def]
         mapping = []
         for key_node, value_node in node.value:
-            key = self.construct_object(key_node, deep=deep)
+            key = self.construct_object(key_node, deep=deep) # type: ignore[no-untyped-call]
             assert key not in mapping, f"Found a duplicate key in the yaml. key={key}, line={node.start_mark.line}"
             mapping.append(key)
         mapping = super().construct_mapping(node, deep=deep)  # type: ignore[no-untyped-call]
