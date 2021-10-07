pytorch/pytorch

commit 3e5acde4736f495a844a6cd5bb979eed771dac98

pytorch quantization ao migration phase 2: torch/jit (#65829)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/65829

Renames `torch.quantization` to `torch.ao.quantization` in `torch/jit` folder.

```
find caffe2/torch/jit/ -type f -name "*.py" -print0 | xargs -0 sed -i "s/torch\.quantization/torch.ao.quantization/g"
```

Test Plan: CI

Differential Revision: D31273365

fbshipit-source-id: cc4f4115a04c308a8b9745a14df54a9e3fe75fef

torch/jit/_recursive.py
@@ -131,7 +131,7 @@ def infer_concrete_type_builder(nn_module, share_types=True):
         concrete_type_builder.set_module_list()
 
     class_annotations = getattr(nn_module, '__annotations__', {})
-    if isinstance(nn_module, (torch.quantization.QuantWrapper)):
+    if isinstance(nn_module, (torch.ao.quantization.QuantWrapper)):
         class_annotations = {}
 
     # Get user-annotated ignored attributes.

torch/jit/quantized.py
@@ -475,7 +475,7 @@ class QuantizedGRU(QuantizedRNNBase):
 
 def quantize_rnn_cell_modules(module):
     warnings.warn("quantize_rnn_cell_modules function has been deprecated. "
-                  "Please use torch.quantization.quantize_dynamic API instead.")
+                  "Please use torch.ao.quantization.quantize_dynamic API instead.")
     reassign = {}
     for name, mod in module.named_modules():
         if mod is module:
@@ -496,7 +496,7 @@ def quantize_rnn_cell_modules(module):
 
 def quantize_linear_modules(module, dtype=torch.int8):
     warnings.warn("quantize_linear_modules function has been deprecated. "
-                  "Please use torch.quantization.quantize_dynamic API instead.")
+                  "Please use torch.ao.quantization.quantize_dynamic API instead.")
 
     reassign = {}
     for name, mod in module.named_modules():
@@ -521,7 +521,7 @@ def quantize_linear_modules(module, dtype=torch.int8):
 
 def quantize_rnn_modules(module, dtype=torch.int8):
     warnings.warn("quantize_rnn_modules function has been deprecated. "
-                  "Please use torch.quantization.quantize_dynamic API instead.")
+                  "Please use torch.ao.quantization.quantize_dynamic API instead.")
     reassign = {}
     for name, mod in module.named_modules():
         if mod is module:

commit 6814e21cd0255e8e720cba60f126a150f3707da1

clean up mypy nit in torch/jit/_recursive.py
Summary:

This was initially broken in #65829 and unbroken in #66003, this PR cleans
it up by removing the mypy ignore line.

Test Plan:

```
mypy torch/jit/_recursive.py --no-incremental
```

Reviewers:

Subscribers:

Tasks:

Tags:

[ghstack-poisoned]

torch/jit/_recursive.py
@@ -131,7 +131,7 @@ def infer_concrete_type_builder(nn_module, share_types=True):
         concrete_type_builder.set_module_list()
 
     class_annotations = getattr(nn_module, '__annotations__', {})
-    if isinstance(nn_module, (torch.ao.quantization.QuantWrapper)):  # type: ignore[attr-defined]
+    if isinstance(nn_module, (torch.ao.quantization.QuantWrapper)):
         class_annotations = {}
 
     # Get user-annotated ignored attributes.