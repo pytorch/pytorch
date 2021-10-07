beeadb6eda26bdc808c6229de8bdd66223ac6254
Eli Uriegas <eliuriegas@fb.com>
2021-10-07 12:13:41 -0500

    Update on "docker: Ensure libgnutls30 for all docker builds"
    Installing libgnutls30 has shown to be good when confronted with the
    CERT issue related to deb.nodesource.com
    
    Signed-off-by: Eli Uriegas <eliuriegasfb.com>
    
    [ghstack-poisoned]

--- .circleci/docker/common/install_base.sh
+++ .circleci/docker/common/install_base.sh
@@ -113,10 +113,7 @@ esac
 # Install Valgrind separately since the apt-get version is too old.
 mkdir valgrind_build && cd valgrind_build
 VALGRIND_VERSION=3.16.1
-if ! wget http://valgrind.org/downloads/valgrind-${VALGRIND_VERSION}.tar.bz2
-then
-  wget https://sourceware.org/ftp/valgrind/valgrind-${VALGRIND_VERSION}.tar.bz2
-fi
+wget https://ossci-linux.s3.amazonaws.com/valgrind-${VALGRIND_VERSION}.tar.bz2
 tar -xjf valgrind-${VALGRIND_VERSION}.tar.bz2
 cd valgrind-${VALGRIND_VERSION}
 ./configure --prefix=/usr/local
