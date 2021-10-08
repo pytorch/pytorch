515a64f4b7eebb565ebf850e2ed4ac8c60c7dec3
Kevin Tse <ktse@fb.com>
2021-10-08 08:29:25 -0700

    [DataPipe] adding SequenceWrapperMapDataPipe (#66275)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66275
    
    Once this is added to Core, TorchData's PR will not need a custom class and can use this wrapper instead.
    
    cc VitalyFedyunin ejguan NivekT
    
    Test Plan: Imported from OSS
    
    Reviewed By: ejguan
    
    Differential Revision: D31485822
    
    Pulled By: NivekT
    
    fbshipit-source-id: 790de27629c89c0ca7163a8ee5a09ee8b8233340

---
+++ torch/utils/data/datapipes/map/utils.py
@@ -0,0 +1,38 @@
+import copy
+import warnings
+from torch.utils.data import MapDataPipe
+
+
+class SequenceWrapperMapDataPipe(MapDataPipe):
+    r""":class:`SequenceWrapperMapDataPipe`.
+
+    Map DataPipe that wraps a sequence object.
+
+    Args:
+        sequence: Sequence object to be wrapped into an IterDataPipe
+        deepcopy: Option to deepcopy input sequence object
+
+    .. note::
+      If `deepcopy` is set to False explicitly, users should ensure
+      that data pipeline doesn't contain any in-place operations over
+      the iterable instance, in order to prevent data inconsistency
+      across iterations.
+    """
+    def __init__(self, sequence, deepcopy=True):
+        if deepcopy:
+            try:
+                self.sequence = copy.deepcopy(sequence)
+            except TypeError:
+                warnings.warn(
+                    "The input sequence can not be deepcopied, "
+                    "please be aware of in-place modification would affect source data"
+                )
+                self.sequence = sequence
+        else:
+            self.sequence = sequence
+
+    def __getitem__(self, index):
+        return self.sequence[index]
+
+    def __len__(self):
+        return len(self.sequence)
