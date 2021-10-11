7363f0ec3b783128f7cb14aa4b8fc1205339e919
Nick Korovaiko <korovaikon@gmail.com>
2021-10-11 10:30:13 -0700

    fix mean out shape formula (#66397)

--- lazy_tensor_core/lazy_tensor_core/csrc/ts_backend/LazyShapeDtype.cpp
+++ lazy_tensor_core/lazy_tensor_core/csrc/ts_backend/LazyShapeDtype.cpp
@@ -25,7 +25,7 @@ c10::ScalarType compute_dtype_dropout(const at::Tensor& input, double p, bool tr
 }
 
 std::vector<int64_t> compute_shape_mean(const at::Tensor& self, c10::optional<at::ScalarType> dtype) {
-  return std::vector<int64_t>({1});
+  return std::vector<int64_t>({});
 }
 
 c10::ScalarType compute_dtype_mean(const at::Tensor& self, c10::optional<at::ScalarType> dtype) {
