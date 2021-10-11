53aee05b4c5f762b8c32ea5096934a5786504618
Saketh Are <saketh@fb.com>
2021-10-11 19:30:19 +0000

    fix doc examples formatting

--- torch/functional.py
+++ torch/functional.py
@@ -408,7 +408,7 @@ else:
 
         Example::
             >>> torch.histogramdd(torch.tensor([[0., 1.], [1., 0.], [2., 0.], [2., 2.]]), bins=(3, 3),
-                                  weight=torch.tensor([1., 2., 4., 8.]))
+            ...                   weight=torch.tensor([1., 2., 4., 8.]))
                 histogramdd_return_type(hist=tensor([[0., 1., 0.],
                                                      [2., 0., 0.],
                                                      [4., 0., 8.]]),
@@ -416,7 +416,7 @@ else:
                                                    tensor([0.0000, 0.6667, 1.3333, 2.0000])))
 
             >>> torch.histogramdd(torch.tensor([[0., 0.], [1., 1.], [2., 2.]]), bins=(2, 2),
-                                  range=(0., 1., 0., 1.), density=True)
+            ...                   range=(0., 1., 0., 1.), density=True)
                 histogramdd_return_type(hist=tensor([[2., 0.],
                                                      [0., 2.]]),
                                         bin_edges=(tensor([0.0000, 0.5000, 1.0000]),
