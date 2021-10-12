9cd4acb295eb6d8d98a7fd374894c8df21f44d94
christopher <chris@polarity.io>
2021-10-12 12:05:21 -0500

    No longer need to changee param names as we now pas params as kwargs

--- torch/onnx/symbolic_opset8.py
+++ torch/onnx/symbolic_opset8.py
@@ -280,8 +280,8 @@ def repeat(g, self, repeats):
             self = sym_opset9.view(g, self, g.op("Constant", value_t=torch.tensor([1] * diff_dims + sizes)))
     return g.op("Tile", self, repeats)
 
-@parse_args("v", "f", "f", "v")
-def uniform(g, self, from_, to_, generator, out=None):
-    from_ = g.op("Constant", value_t=torch.tensor(from_, dtype=torch.float))
-    to_ = g.op("Constant", value_t=torch.tensor(to_, dtype=torch.float))
+def uniform(g, self, **kwargs):
+    out = kwargs["out"] if "out" in kwargs else None
+    from_ = g.op("Constant", value_t=torch.tensor(kwargs["from"], dtype=torch.float))
+    to_ = g.op("Constant", value_t=torch.tensor(kwargs["to"], dtype=torch.float))
     return g.op("Uniform", self, from_, to_, generator, upper_i=1)
