75eb75f65c7660ae2314367e80b69f8ebe98704b
christopher <chris@polarity.io>
2021-10-12 12:11:42 -0500

    get generator from kwargs

--- torch/onnx/symbolic_opset8.py
+++ torch/onnx/symbolic_opset8.py
@@ -284,4 +284,4 @@ def uniform(g, self, **kwargs):
     out = kwargs["out"] if "out" in kwargs else None
     from_ = g.op("Constant", value_t=torch.tensor(kwargs["from"], dtype=torch.float))
     to_ = g.op("Constant", value_t=torch.tensor(kwargs["to"], dtype=torch.float))
-    return g.op("Uniform", self, from_, to_, generator, upper_i=1)
+    return g.op("Uniform", self, from_, to_, kwargs["generator"], upper_i=1)
