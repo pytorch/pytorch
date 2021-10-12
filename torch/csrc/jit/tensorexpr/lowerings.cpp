6864146f2b26bbb2ab4fe868634d9c098c00d60a
Mikhail Zolotukhin <mvz@fb.com>
2021-10-12 01:23:38 -0700

    [TensorExpr] Fix lowerings for aten::view and aten::reshape. (#65852)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/65852
    
    Test Plan: Imported from OSS
    
    Reviewed By: navahgar
    
    Differential Revision: D31286024
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: eb5b5f2ed86b6f325f09904e841815b8183b4e1d

--- torch/csrc/jit/tensorexpr/lowerings.cpp
+++ torch/csrc/jit/tensorexpr/lowerings.cpp
@@ -1399,10 +1399,12 @@ RegisterNNCLoweringsFunction aten_expand(
 
 // TODO: convert to schema, add a test
 // RegisterNNCLoweringsFunction aten_flatten({"aten::flatten"}, computeFlatten);
-// TODO: convert to schema, add a test
-// RegisterNNCLoweringsFunction aten_view(
-//     {"aten::view", "aten::reshape"},
-//     computeReshape);
+RegisterNNCLoweringsFunction aten_view(
+    {"aten::reshape(Tensor(a) self, int[] shape) -> (Tensor(a))",
+     "aten::reshape_as(Tensor(a) self, Tensor other) -> (Tensor(a))",
+     "aten::view(Tensor(a) self, int[] size) -> (Tensor(a))",
+     "aten::view_as(Tensor(a) self, Tensor other) -> (Tensor(a))"},
+    computeReshape);
 
 // aten::mm is a subset of aten::matmul where both inputs are rank 2
 RegisterNNCLoweringsFunction aten_matmul(
