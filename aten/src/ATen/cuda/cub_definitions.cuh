21abede594471e4f225def12796c1d7c73a96b20
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-10-12 12:46:32 -0700

    save

--- aten/src/ATen/cuda/cub_definitions.cuh
+++ aten/src/ATen/cuda/cub_definitions.cuh
@@ -15,10 +15,10 @@
 #define CUB_SUPPORTS_NV_BFLOAT16() false
 #endif
 
-// cub sort support for CUB_WRAPPED_NAMESPACE is added to cub 1.14 in:
+// cub sort support for CUB_WRAPPED_NAMESPACE is added to cub 1.13.1 in:
 // https://github.com/NVIDIA/cub/pull/326
 // CUB_WRAPPED_NAMESPACE is defined globally in cmake/Dependencies.cmake
-// starting from CUDA 11.6
+// starting from CUDA 11.5
 #if defined(CUB_WRAPPED_NAMESPACE) || defined(THRUST_CUB_WRAPPED_NAMESPACE)
 #define USE_GLOBAL_CUB_WRAPPED_NAMESPACE() true
 #else
