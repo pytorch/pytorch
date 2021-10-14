545815ab92ff4ccfc1075f66d3b54c08d0f3739a
Jay Zhang <jiz@microsoft.com>
2021-10-14 15:56:15 +0800

    Deprecate the argument _retain_param_name of export() method entirely.

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -35,9 +35,9 @@ def _export(*args, **kwargs):
 
 def export(model, args, f, export_params=True, verbose=False, training=TrainingMode.EVAL,
            input_names=None, output_names=None, operator_export_type=None,
-           opset_version=None, _retain_param_name=None, do_constant_folding=True,
-           example_outputs=None, strip_doc_string=None, dynamic_axes=None,
-           keep_initializers_as_inputs=None, custom_opsets=None, enable_onnx_checker=None,
+           opset_version=None, do_constant_folding=True, example_outputs=None,
+           strip_doc_string=None, dynamic_axes=None, keep_initializers_as_inputs=None,
+           custom_opsets=None, enable_onnx_checker=None,
            use_external_data_format=None, export_modules_as_functions=False):
     r"""
     Exports a model into ONNX format. If ``model`` is not a
@@ -187,8 +187,6 @@ def export(model, args, f, export_params=True, verbose=False, training=TrainingM
         opset_version (int, default 9):
             Must be ``== _onnx_main_opset or in _onnx_stable_opsets``,
             defined in torch/onnx/symbolic_helper.py.
-        _retain_param_name (bool, default True): Deprecated and ignored. Will be removed in next PyTorch
-            release.
         do_constant_folding (bool, default False): Apply the constant-folding optimization.
             Constant-folding will replace some of the ops that have all constant inputs
             with pre-computed constant nodes.
@@ -315,9 +313,9 @@ def export(model, args, f, export_params=True, verbose=False, training=TrainingM
     from torch.onnx import utils
     return utils.export(model, args, f, export_params, verbose, training,
                         input_names, output_names, operator_export_type, opset_version,
-                        _retain_param_name, do_constant_folding, example_outputs,
-                        strip_doc_string, dynamic_axes, keep_initializers_as_inputs,
-                        custom_opsets, enable_onnx_checker, use_external_data_format,
+                        do_constant_folding, example_outputs, strip_doc_string,
+                        dynamic_axes, keep_initializers_as_inputs, custom_opsets,
+                        enable_onnx_checker, use_external_data_format,
                         export_modules_as_functions)
 
 
