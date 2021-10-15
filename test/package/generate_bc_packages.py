54c519c3855f02cefc68bf77b0835c87f30d5dee
sahanp <sahanp@devvm2367.prn0.facebook.com>
2021-10-15 15:26:18 -0700

    saved bc files
    [ghstack-poisoned]

---
+++ test/package/generate_bc_packages.py
@@ -0,0 +1,26 @@
+import torch
+from torch.package import PackageExporter
+from torch.fx import symbolic_trace
+from pathlib import Path
+from package_a.test_nn_module import TestNnModule
+
+packaging_directory = f'{Path(__file__).parent}/package_bc'
+torch.package.package_exporter._gate_torchscript_serialization = False
+
+def generate_bc_packages():
+
+    test_nn_module = TestNnModule()
+    test_torchscript_module = torch.jit.script(TestNnModule())
+    test_fx_module : torch.fx.GraphModule = symbolic_trace(TestNnModule())
+    with PackageExporter(f"{packaging_directory}/test_nn_module.pt") as pe1:
+        pe1.intern("**")
+        pe1.save_pickle("nn_module", "nn_module.pkl", test_nn_module)
+    with PackageExporter(f"{packaging_directory}/test_torchscript_module.pt") as pe2:
+        pe2.intern("**")
+        pe2.save_pickle("torchscript_module", "torchscript_module.pkl", test_torchscript_module)
+    with PackageExporter(f"{packaging_directory}/test_fx_module.pt") as pe3:
+        pe3.intern("**")
+        pe3.save_pickle("fx_module", "fx_module.pkl", test_fx_module)
+
+if __name__ == "__main__":
+    generate_bc_packages()
