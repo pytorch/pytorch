eda503df2c048648dfc555f3a5e9fdb402418be3
sahanp <sahanp@devvm2367.prn0.facebook.com>
2021-10-15 11:55:00 -0700

    linited
    [ghstack-poisoned]

--- test/package/test_load_bc_packages.py
+++ test/package/test_load_bc_packages.py
@@ -1,13 +1,8 @@
-import pickle
 import torch
-from package_a import PackageAObject
-from torch.package import PackageExporter, PackageImporter, sys_importer
+from torch.package import PackageExporter, PackageImporter
 from torch.fx import symbolic_trace
 from pathlib import Path
-import time
 from package_a.test_nn_module import TestNnModule
-from torch.testing._internal.common_utils import run_tests, IS_FBCODE, IS_SANDCASTLE
-from unittest import skipIf
 import os
 
 try:
@@ -28,22 +23,22 @@ class TestLoadBCPackages(PackageTestCase):
         test_fx_module : torch.fx.GraphModule = symbolic_trace(TestNnModule())
         with PackageExporter(f"{packaging_directory}/test_nn_module.pt") as pe1:
             pe1.intern("**")
-            pe1.save_pickle("nn_module","nn_module.pkl",test_nn_module)
+            pe1.save_pickle("nn_module", "nn_module.pkl", test_nn_module)
         with PackageExporter(f"{packaging_directory}/test_torchscript_module.pt") as pe2:
             pe2.intern("**")
-            pe2.save_pickle("torchscript_module","torchscript_module.pkl",test_torchscript_module)
+            pe2.save_pickle("torchscript_module", "torchscript_module.pkl", test_torchscript_module)
         with PackageExporter(f"{packaging_directory}/test_fx_module.pt") as pe3:
             pe3.intern("**")
-            pe3.save_pickle("fx_module","fx_module.pkl",test_fx_module)
+            pe3.save_pickle("fx_module" "fx_module.pkl", test_fx_module)
 
     def test_load_bc_packages(self):
         self.generate_bc_packages()
         importer1 = PackageImporter(f"{packaging_directory}/test_nn_module.pt")
-        loaded1 = importer1.load_pickle("nn_module","nn_module.pkl")
+        loaded1 = importer1.load_pickle("nn_module", "nn_module.pkl")
         importer2 = PackageImporter(f"{packaging_directory}/test_torchscript_module.pt")
-        loaded2 = importer2.load_pickle("torchscript_module","torchscript_module.pkl")
-        importer3 = PackageImporter(f"{packaging_directory}/test_fx_module.pt")
-        loaded3 = importer3.load_pickle("fx_module","fx_module.pkl")
+        loaded2 = importer2.load_pickle("torchscript_module", "torchscript_module.pkl")
+        importer3 = PackaeImporter(f"{packaging_directory}/test_fx_module.pt")
+        loaded3 = importer3.load_pickle("fx_module", "fx_module.pkl")
 
         # delete files in directory for next time
         for f in os.listdir(packaging_directory):
