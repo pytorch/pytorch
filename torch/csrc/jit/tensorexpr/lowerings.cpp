3ea627fae2c4ff38ae4d7863dbbe010279348cf3
Mikhail Zolotukhin <mvz@fb.com>
2021-10-15 09:12:45 -0700

    Update on "[TensorExpr] Add missing schema for aten::where and aten::pow lowerings."
    Differential Revision: [D31689431](https://our.internmc.facebook.com/intern/diff/D31689431)
    
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/lowerings.cpp
+++ torch/csrc/jit/tensorexpr/lowerings.cpp
@@ -712,7 +712,8 @@ RegisterNNCLoweringsFunction aten_type_as(
 
 RegisterNNCLoweringsFunction aten_pow(
     {"aten::pow.Tensor_Scalar(Tensor self, Scalar exponent) -> (Tensor)",
-     "aten::pow.Tensor_Tensor(Tensor self, Tensor exponent) -> (Tensor)"},
+     "aten::pow.Tensor_Tensor(Tensor self, Tensor exponent) -> (Tensor)",
+     "aten::pow.Scalar(Scalar self, Tensor exponent) -> Tensor"},
     [](const std::vector<ArgValue>& inputs,
        const std::vector<ExprHandle>& outputShape,
        const c10::optional<ScalarType>& outputType,
