c2c425607bf374d7c0511f4cd883bd6680e48d40
Nikita Shulga <nshulga@fb.com>
2021-10-15 14:01:17 -0700

    Revert D31656999: Add meta support to tensor range factories
    Test Plan: revert-hammer
    
    Differential Revision:
    D31656999 (https://github.com/pytorch/pytorch/commit/7400f34b8e3692384ab19de0914271e01cb3fd88)
    
    Original commit changeset: 06e7f3655b94
    
    fbshipit-source-id: 2f9d8d1acbb01c5105ece73472e5c1f5f90886ee

--- aten/src/ATen/native/RangeFactories.cpp
+++ aten/src/ATen/native/RangeFactories.cpp
@@ -13,7 +13,7 @@ namespace at { namespace native {
 DECLARE_DISPATCH(void(*)(TensorIterator&, const Scalar&, const Scalar&, const Scalar&), arange_stub);
 DECLARE_DISPATCH(void(*)(TensorIterator&, const Scalar&, const Scalar&, int64_t), linspace_stub);
 
-Tensor& linspace_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, Tensor& result) {
+Tensor& linspace_cpu_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, Tensor& result) {
   const auto steps = optional_steps.value_or(100);
   TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
 
@@ -26,10 +26,6 @@ Tensor& linspace_out(const Scalar& start, const Scalar& end, c10::optional<int64
 
   if (result.numel() != steps) {
     result.resize_({steps});
-  }
-
-  if (result.device() == kMeta) {
-    return result;
   }
 
   if (steps == 0) {
@@ -48,7 +44,7 @@ Tensor& linspace_out(const Scalar& start, const Scalar& end, c10::optional<int64
   return result;
 }
 
-Tensor& logspace_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, double base, Tensor& result) {
+Tensor& logspace_cpu_out(const Scalar& start, const Scalar& end, c10::optional<int64_t> optional_steps, double base, Tensor& result) {
   const auto steps = optional_steps.value_or(100);
   TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
 
@@ -62,11 +58,6 @@ Tensor& logspace_out(const Scalar& start, const Scalar& end, c10::optional<int64
   if (result.numel() != steps) {
     result.resize_({steps});
   }
-
-  if (result.device() == kMeta) {
-    return result;
-  }
-
   Tensor r = result.is_contiguous() ? result : result.contiguous();
 
   if (steps == 0) {
@@ -122,7 +113,7 @@ Tensor& logspace_out(const Scalar& start, const Scalar& end, c10::optional<int64
   return result;
 }
 
-Tensor& range_out(const Scalar& start, const Scalar& end, const Scalar& step, Tensor& result) {
+Tensor& range_cpu_out(const Scalar& start, const Scalar& end, const Scalar& step, Tensor& result) {
   AT_DISPATCH_ALL_TYPES_AND(kBFloat16, result.scalar_type(), "range_cpu", [&]() {
     using accscalar_t = at::acc_type<scalar_t, false>;
     auto xstart = start.to<accscalar_t>();
@@ -139,11 +130,6 @@ Tensor& range_out(const Scalar& start, const Scalar& end, const Scalar& step, Te
     if (result.numel() != size) {
       result.resize_({size});
     }
-
-    if (result.device() == kMeta) {
-      return;
-    }
-
     Tensor r = result.is_contiguous() ? result : result.contiguous();
     scalar_t *data_ptr = r.data_ptr<scalar_t>();
 
@@ -161,7 +147,7 @@ Tensor& range_out(const Scalar& start, const Scalar& end, const Scalar& step, Te
   return result;
 }
 
-Tensor& arange_out(const Scalar& start, const Scalar& end, const Scalar& step, Tensor& result) {
+Tensor& arange_cpu_out(const Scalar& start, const Scalar& end, const Scalar& step, Tensor& result) {
   AT_DISPATCH_ALL_TYPES_AND(kBFloat16, result.scalar_type(), "arange_cpu", [&]() {
     using accscalar_t = at::acc_type<scalar_t, false>;
     auto xstart = start.to<accscalar_t>();
@@ -206,10 +192,6 @@ Tensor& arange_out(const Scalar& start, const Scalar& end, const Scalar& step, T
                     "The out tensor will be resized to a tensor of shape (", size, ",).");
       }
       result.resize_({size});
-    }
-
-    if (result.device() == kMeta) {
-      return;
     }
 
     Tensor r = result.is_contiguous() ? result : result.contiguous();
