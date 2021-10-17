dca3f619c29eff97520e705bbfc324f98cc42af0
Natalia Gimelshein <ngimel@fb.com>
2021-10-16 21:48:40 -0700

    Revert D31547709: Remove THCGeneral.cpp
    Test Plan: revert-hammer
    
    Differential Revision:
    D31547709 (https://github.com/pytorch/pytorch/commit/aa0c31876bbd0f9813ec6f000154a24c91beece9)
    
    Original commit changeset: 059c47621863
    
    fbshipit-source-id: e8c3597f2badbc5ecf356b381edea06a07331f24

--- torch/csrc/cuda/Stream.cpp
+++ torch/csrc/cuda/Stream.cpp
@@ -16,7 +16,9 @@ static PyObject * THCPStream_pynew(
   PyTypeObject *type, PyObject *args, PyObject *kwargs) {
   HANDLE_TH_ERRORS
 
-  const auto current_device = c10::cuda::current_device();
+  // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
+  int current_device;
+  THCudaCheck(cudaGetDevice(&current_device));
 
   int priority = 0;
   uint64_t cdata = 0;
