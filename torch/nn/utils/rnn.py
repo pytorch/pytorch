e6b13a181c468d8ac1f4874862d4cfe05893e181
Dani El-Ayyass <dayyass@yandex.ru>
2021-10-19 23:08:46 +0300

    add unpad_sequence function docstring

--- torch/nn/utils/rnn.py
+++ torch/nn/utils/rnn.py
@@ -364,7 +364,35 @@ def pad_sequence(sequences, batch_first=False, padding_value=0.0):
 
 
 def unpad_sequence(padded_sequences, lengths, batch_first=False):
-    """TODO"""
+    # type: (Tensor, Tensor, bool) -> List[Tensor]
+    r"""Unpad padded Tensor into a list of variable length Tensors
+
+    ``unpad_sequence`` unstacks padded Tensor into a list of variable length Tensors.
+
+    Example:
+        >>> from torch.nn.utils.rnn import pad_sequence, unpad_sequence
+        >>> a = torch.ones(25, 300)
+        >>> b = torch.ones(22, 300)
+        >>> c = torch.ones(15, 300)
+        >>> sequences = [a, b, c]
+        >>> padded_sequences = pad_sequence(sequences)
+        >>> lengths = torch.as_tensor([v.size(0) for v in sequences])
+        >>> unpadded_sequences = unpad_sequence(padded_sequences, lengths)
+        >>> torch.allclose(sequences[0], unpadded_sequences[0])
+        True
+        >>> torch.allclose(sequences[1], unpadded_sequences[1])
+        True
+        >>> torch.allclose(sequences[2], unpadded_sequences[2])
+        True
+
+    Args:
+        padded_sequences (Tensor): padded sequences.
+        lengths (Tensor): length of original (unpadded) sequences.
+        batch_first (bool, optional): whether batch dimension first or not. Default: False.
+
+    Returns:
+        a list of :class:`Tensor` objects
+    """
 
     unpadded_sequences = []
 
