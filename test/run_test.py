5d015e4a6ae1d2f00a6c71f8803f03cc40a70b1b
Junjie Wang <fduwjj@fb.com>
2021-10-20 15:15:08 -0700

    [PyTorch][1/N] Basic implementation of ShardedEmbedding using ShardedTensor. (#66604)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66604
    
    This diff/PR is trying to implement the ShardedEmbedding and ShardedEmbedding using the ShardedTensor.
    
    Several caveats:
    1. We support limited input params for the op. To support more params are on the way.
    2. We only support chuck sharding for now.
    3. We only support a single local shard per rank for now.
    
    ghstack-source-id: 141056130
    
    Test Plan: Unit test and CI
    
    Reviewed By: pritamdamania87
    
    Differential Revision: D31544556
    
    fbshipit-source-id: cc867dcba8c11e6f4c7c3722488908f5108cc67f

--- test/run_test.py
+++ test/run_test.py
@@ -198,6 +198,7 @@ WINDOWS_BLOCKLIST = [
     "distributed/elastic/agent/server/test/api_test",
     "distributed/elastic/multiprocessing/api_test",
     "distributed/_sharded_tensor/test_sharded_tensor",
+    "distributed/_sharded_tensor/ops/test_embedding",
     "distributed/_sharded_tensor/ops/test_linear",
 ] + FSDP_TEST
 
@@ -207,6 +208,7 @@ ROCM_BLOCKLIST = [
     "distributed/rpc/test_tensorpipe_agent",
     "distributed/rpc/cuda/test_tensorpipe_agent",
     "distributed/_sharded_tensor/test_sharded_tensor",
+    "distributed/_sharded_tensor/ops/test_embedding",
     "distributed/_sharded_tensor/ops/test_linear",
     "test_determination",
     "test_multiprocessing",
@@ -342,6 +344,7 @@ DISTRIBUTED_TESTS = [
     "distributed/elastic/multiprocessing/api_test",
     "distributed/_sharding_spec/test_sharding_spec",
     "distributed/_sharded_tensor/test_sharded_tensor",
+    "distributed/_sharded_tensor/ops/test_embedding",
     "distributed/_sharded_tensor/ops/test_linear",
 ] + [test for test in TESTS if test.startswith("distributed/fsdp")]
 
