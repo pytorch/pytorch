3dd2d1347818e3645c92012f0aa739aa5a09cc23
Howard Huang <howardhuang@fb.com>
2021-10-20 18:35:31 +0000

    Update on "Fix test_create_store_multi flaky test"
    Fixes #66641, #66319
    
    The issue is a race condition where free port is determined by `get_free_port`, but before that port is actually used by the server store it could have been used by another process leading to a `port already in use` error. The fix is to switch the order of the server store creation and have the server store specify port 0 (choose an unallocated port during its creation), then have the worker processes use the port that was allocated to the server store by accessing `store.port`.
    
    cc @pietern @mrshenli @pritamdamania87 @zhaojuanmao @satgera @rohan-varma @gqchen @aazzolini @osalpekar @jiayisuse @SciPioneer @H-Huang
    
    Differential Revision: [D31802767](https://our.internmc.facebook.com/intern/diff/D31802767)
    
    [ghstack-poisoned]

--- test/distributed/elastic/utils/distributed_test.py
+++ test/distributed/elastic/utils/distributed_test.py
@@ -14,7 +14,6 @@ from contextlib import closing
 
 from torch.distributed.elastic.utils.distributed import (
     create_c10d_store,
-    get_free_port,
     get_socket_with_port,
 )
 from torch.testing._internal.common_utils import IS_MACOS, IS_WINDOWS, run_tests
