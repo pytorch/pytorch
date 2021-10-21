1dc8466509c20ab3cf93d237536cdcc8046caf1a
Eddie Yan <eddiey@nvidia.com>
2021-10-21 19:15:57 +0000

    fix

--- test/test_nn.py
+++ test/test_nn.py
@@ -16348,7 +16348,7 @@ class TestNNDeviceType(NNTestCase):
     @onlyCUDA
     def test_layernorm_half_precision(self):
        width = 128
-       input = torch.rand((1,5,width, device="cuda", dtype=torch.half))*0.1
+       input = torch.rand(1, 5, width, device="cuda", dtype=torch.half)*0.1
        normalized_shape = (width,)
        weight = torch.ones(width, device="cuda", dtype=torch.half)
        bias = torch.zeros(width, device="cuda", dtype=torch.half)
@@ -16356,7 +16356,7 @@ class TestNNDeviceType(NNTestCase):
 
        output_fp16 = torch.layer_norm(input, normalized_shape, weight, bias, eps)
        output_fp32 = torch.layer_norm(input.float(), normalized_shape, weight.float(), bias.float(), eps).half()
-       torch.testing.assert_allclose(output_fp16, output_fp32, atol=0, rtol=0)
+       self.assertEqual(output_fp16, output_fp32, atol=0, rtol=0)
 
     def test_hardsigmoid_grad(self, device):
         inputs = (torch.randn(4, 16, 16, device=device) - 0.5) * 10
