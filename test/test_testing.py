67d0364a9ae9131eb90dba60518ff92ebe02366d
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-10-21 07:29:48 +0000

    move test to test_torch as it is run by XLA

--- test/test_testing.py
+++ test/test_testing.py
@@ -15,9 +15,9 @@ from torch.testing._internal.common_utils import \
     (IS_FBCODE, IS_SANDCASTLE, IS_WINDOWS, TestCase, run_tests, skipIfRocm, slowTest,
      parametrize, subtest, instantiate_parametrized_tests, dtype_name)
 from torch.testing._internal.common_device_type import \
-    (PYTORCH_TESTING_DEVICE_EXCEPT_FOR_KEY, PYTORCH_TESTING_DEVICE_ONLY_FOR_KEY, dtypes, expectedFailureXLA,
+    (PYTORCH_TESTING_DEVICE_EXCEPT_FOR_KEY, PYTORCH_TESTING_DEVICE_ONLY_FOR_KEY, dtypes,
      get_device_type_test_bases, instantiate_device_type_tests, onlyCUDA, onlyOnCPUAndCUDA,
-     deviceCountAtLeast, ops, skipXLA)
+     deviceCountAtLeast, ops)
 from torch.testing._internal.common_methods_invocations import op_db
 import torch.testing._internal.opinfo_helper as opinfo_helper
 from torch.testing._internal.common_dtype import get_all_dtypes
@@ -602,18 +602,6 @@ if __name__ == '__main__':
 
             self.assertTrue(set(dtypes) == set(dynamic_dtypes))
             self.assertTrue(set(dtypes) == set(dynamic_dispatch.dispatch_fn()))
-
-    @skipXLA
-    def test_skip_xla(self, device):
-        if self.device_type == 'xla':
-            # Should not reach here!
-            self.assertTrue(False)
-
-    @expectedFailureXLA
-    def test_expected_failure_xla(self, device):
-        if self.device_type == 'xla':
-            # Should not reach here!
-            self.assertTrue(False)
 
 
 instantiate_device_type_tests(TestTesting, globals())
