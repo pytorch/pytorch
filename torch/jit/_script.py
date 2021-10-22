55125b0be7b0abcb3c5cce203065928365e7de62
BashkirovN <63573283+BashkirovN@users.noreply.github.com>
2021-10-21 23:44:32 -0700

    Update _script.py
    Please mypy, let me pass!!

--- torch/jit/_script.py
+++ torch/jit/_script.py
@@ -61,7 +61,7 @@ set_module(ScriptFunction, "torch.jit")
 def _reduce(cls):
     raise pickle.PickleError("ScriptFunction cannot be pickled")
 
-ScriptFunction.__reduce__ = _reduce  # type: ignore
+ScriptFunction.__reduce__ = _reduce  # type: ignore[attr-defined]
 
 
 if _enabled:
