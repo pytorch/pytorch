5ddca43936334b8c6f27be7afe0da77a7aba503d
Hui Guo <huiguo@fb.com>
2021-10-23 05:09:07 -0700

    [tensorexpr] Move the allocation of intermediate buffers from TEK to CodeGen
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/llvm_codegen.cpp
+++ torch/csrc/jit/tensorexpr/llvm_codegen.cpp
@@ -338,9 +338,9 @@ LLVMCodeGen::LLVMCodeGen(
     c10::optional<std::string> triple,
     c10::optional<std::string> cpu,
     c10::optional<std::string> attrs)
-    : CodeGen(stmt, args, device, kernel_func_name),
-      impl_(std::make_unique<
-            LLVMCodeGenImpl>(stmt, args, device, dtype, triple, cpu, attrs)) {
+    : CodeGen(stmt, args, device, kernel_func_name) {
+  impl_ = std::make_unique<LLVMCodeGenImpl>(
+      this->stmt(), args, device, dtype, triple, cpu, attrs);
   callee_ = std::make_unique<LLVMCodeGenCallee>(
       impl_->releaseJIT(), (void*)impl_->getKernelAddress());
 }
