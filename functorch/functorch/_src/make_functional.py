1890e48ae9c88e5b3d98f39dcbd1a3fc826b5764
Richard Zou <zou3519@gmail.com>
2021-10-25 13:52:48 -0700

    [functorch] Kill make_functional deprecation warning

--- functorch/functorch/_src/make_functional.py
+++ functorch/functorch/_src/make_functional.py
@@ -273,11 +273,6 @@ def make_functional(model: nn.Module):
     grad_weights = grad(compute_loss)(params, x, t)
     ```
     """
-    warnings.warn('If this is your first time using make_functional, please '
-                  'ignore this warning. Otherwise, we recently made a '
-                  'backwards incompatible change to make_functional: '
-                  'please try make_functional_deprecated_v1 if you want the '
-                  'previous behavior.', stacklevel=2)
     buffers = list(model.buffers())
     if len(buffers) > 0:
         raise RuntimeError('make_functional(model): `model` has buffers. Please use '
@@ -322,11 +317,6 @@ def make_functional_with_buffers(model: nn.Module):
     grad_weights = grad(compute_loss)(params, buffers, x, t)
     ```
     """
-    warnings.warn('If this is your first time using make_functional_with_buffers, please '
-                  'ignore this warning. Otherwise, we recently made a '
-                  'backwards incompatible change to make_functional_with_buffers: '
-                  'please try make_functional_with_buffers_deprecated_v1 if you want the '
-                  'previous behavior.', stacklevel=2)
     return FunctionalModuleWithBuffers._create_from(model)
 
 
