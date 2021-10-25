f4b7f6fa3fc5779f5580e6742c2105e92686010b
Eli Uriegas <eliuriegas@fb.com>
2021-10-25 10:06:37 -0700

    ci: Build periodic jobs with DEBUG=1
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    [ghstack-poisoned]

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -24,6 +24,11 @@ if [[ "$BUILD_ENVIRONMENT" == *-mobile-code-analysis* ]]; then
   exec "$(dirname "${BASH_SOURCE[0]}")/build-mobile-code-analysis.sh" "$@"
 fi
 
+# Always build periodic jobs with debug
+if [[ "$BUILD_ENVIRONMENT" == periodic-* ]]; then
+  export DEBUG=1
+fi
+
 if [[ "$BUILD_ENVIRONMENT" == *linux-xenial-cuda11.3* ]]; then
   # Enabling DEPLOY build (embedded torch python interpreter, experimental)
   # only on one config for now, can expand later
