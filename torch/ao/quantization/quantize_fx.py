194a3ae93a9b1357950b906e620004d48d9fe6de
Jerry Zhang <jerryzh@fb.com>
2021-10-25 08:00:54 -0700

    [quant] Fix docs build (#67169)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67169
    
    Looks like the doc error only appears after it's landed
    
    Test Plan: Imported from OSS
    
    Reviewed By: seemethere
    
    Differential Revision: D31890431
    
    fbshipit-source-id: d40cba082712c4b35704ea15d82fbc4749f85aec

--- torch/ao/quantization/quantize_fx.py
+++ torch/ao/quantization/quantize_fx.py
@@ -639,11 +639,11 @@ def convert_fx(
 
         * `_remove_qconfig`: Option to remove the qconfig attributes in the model after convert.
 
-        * `qconfig_dict`: qconfig_dict with the either
-            a) same keys as what is passed to the qconfig_dict in prepare_fx API, with same values or `None`.
-            b) additional keys with values set to `None`
-            For each entry whose value is set to None, we skip quantizing that entry in the model.
-          Example::
+        * `qconfig_dict`: qconfig_dict with either same keys as what is passed to
+          the qconfig_dict in `prepare_fx` API, with same values or `None`, or
+          additional keys with values set to `None`
+
+          For each entry whose value is set to None, we skip quantizing that entry in the model::
 
             qconfig_dict = {
               # used for object_type, skip quantizing torch.nn.functional.add
