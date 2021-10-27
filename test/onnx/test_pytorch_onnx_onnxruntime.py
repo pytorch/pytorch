d9a5668983c05a01c8785837394c42081b19d0a5
Shubham Bhokare <32080845+shubhambhokare1@users.noreply.github.com>
2021-10-27 13:43:41 -0700

    [ONNX] Add dim argument to all symbolic (#66093) (#67270)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67270
    
    * Add dim argument to all symbolic
    
    * All symbolic depends on any symbolic
    
    Test Plan: Imported from OSS
    
    Reviewed By: msaroufim
    
    Differential Revision: D31962518
    
    Pulled By: malfet
    
    fbshipit-source-id: f7ee05cf4eff5880fc508154267e060952b5b42d

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -7414,6 +7414,20 @@ class TestONNXRuntime(unittest.TestCase):
         x = torch.tensor([[True, False], [False, False]])
         self.run_test(M(), (x, ))
 
+        class MDim(torch.nn.Module):
+            def forward(self, x):
+                return x.any(dim=1)
+
+        x = torch.rand(3, 4).bool()
+        self.run_test(MDim(), (x, ))
+
+        class MKeepdim(torch.nn.Module):
+            def forward(self, x):
+                return x.any(dim=1, keepdim=True)
+
+        x = torch.rand(3, 4).bool()
+        self.run_test(MKeepdim(), (x, ))
+
     @skipIfUnsupportedMinOpsetVersion(9)
     def test_all(self):
         class M(torch.nn.Module):
@@ -7422,6 +7436,20 @@ class TestONNXRuntime(unittest.TestCase):
 
         x = torch.tensor([[True, False], [False, False]])
         self.run_test(M(), (x, ))
+
+        class MDim(torch.nn.Module):
+            def forward(self, x):
+                return x.all(dim=1)
+
+        x = torch.rand(3, 4).bool()
+        self.run_test(MDim(), (x, ))
+
+        class MKeepdim(torch.nn.Module):
+            def forward(self, x):
+                return x.all(dim=1, keepdim=True)
+
+        x = torch.rand(3, 4).bool()
+        self.run_test(MKeepdim(), (x, ))
 
     def test_dropout(self):
         class M(torch.nn.Module):
