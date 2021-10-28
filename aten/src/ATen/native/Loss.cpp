8cff7383fd2e672816ceec8963f1d7bde4d75ea1
George Qi <georgeqi94@gmail.com>
2021-10-28 22:09:07 +0000

    add mse loss structured
    [ghstack-poisoned]

--- aten/src/ATen/native/Loss.cpp
+++ aten/src/ATen/native/Loss.cpp
@@ -20,7 +20,25 @@ namespace {
   }
 }
 
-namespace at { namespace native {
+namespace at {
+namespace meta {
+
+TORCH_META_FUNC(mse_loss)
+(const Tensor& input,
+ const Tensor& target,
+ int64_t reduction) {
+  build_borrowing_binary_op(maybe_get_output(), input, target);
+
+  if (reduction != Reduction::None) {
+    MetaBase::set_output(input.sizes(), input.options());
+  }
+
+  MetaBase::set_output({}, input.options());
+}
+
+} // namespace meta
+
+namespace native {
 
 DEFINE_DISPATCH(smooth_l1_stub);
 DEFINE_DISPATCH(smooth_l1_backward_stub);
@@ -445,14 +463,11 @@ Tensor& huber_loss_backward_out(const Tensor& grad_output, const Tensor& input, 
   return grad_input;
 }
 
-Tensor mse_loss(const Tensor& input, const Tensor& target, int64_t reduction) {
-  Tensor loss;
-  auto iter = TensorIterator::borrowing_binary_op(loss, input, target);
-  mse_stub(iter.device_type(), iter);
-  return apply_loss_reduction(iter.output(), reduction);
-}
-
-Tensor& mse_loss_out(const Tensor& input, const Tensor& target, int64_t reduction, Tensor&result) {
+TORCH_IMPL_FUNC(mse_loss_out)
+(const Tensor& input,
+ const Tensor& target,
+ int64_t reduction,
+ const Tensor& result) {
   if (reduction != Reduction::None) {
     Tensor loss;
     auto iter = TensorIterator::borrowing_binary_op(loss, input, target);
@@ -466,7 +481,6 @@ Tensor& mse_loss_out(const Tensor& input, const Tensor& target, int64_t reductio
     auto iter = TensorIterator::borrowing_binary_op(result, input, target);
     mse_stub(iter.device_type(), iter);
   }
-  return result;
 }
 
 Tensor mse_loss_backward(const Tensor& grad_output, const Tensor& input, const Tensor& target, int64_t reduction) {
