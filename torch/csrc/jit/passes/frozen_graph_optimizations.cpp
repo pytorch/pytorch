995644e72eb821de0b9e62508d2f6b504cc506ba
John Clow <jclow@fb.com>
2021-10-27 17:45:48 -0700

    Move Concat Linear out of Optimize Numerics and add LinearTranspose
    ghstack-source-id: e1c0700b17dcce1dda7a040a8b41416a6f820431
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67196

--- torch/csrc/jit/passes/frozen_graph_optimizations.cpp
+++ torch/csrc/jit/passes/frozen_graph_optimizations.cpp
@@ -2,6 +2,7 @@
 #include <torch/csrc/jit/ir/alias_analysis.h>
 #include <torch/csrc/jit/ir/ir_views.h>
 #include <torch/csrc/jit/passes/frozen_concat_linear.h>
+#include <torch/csrc/jit/passes/frozen_linear_transpose.h>
 #include <torch/csrc/jit/passes/frozen_conv_folding.h>
 #include <torch/csrc/jit/passes/frozen_graph_optimizations.h>
 #include <torch/csrc/jit/passes/remove_dropout.h>
@@ -15,15 +16,16 @@ void OptimizeFrozenGraph(
     std::shared_ptr<Graph>& graph,
     bool optimize_numerics) {
   removeDropout(graph);
+  FrozenConcatLinear(graph);
   // run a couple times to capture Conv -> Mul -> Add etc
   if (optimize_numerics) {
+    FrozenLinearTranspose(graph);
     for (const auto i : c10::irange(2)) {
       (void)i; // Suppress unused variable warning
       FoldFrozenConvBatchnorm(graph);
       FoldFrozenConvAddOrSub(graph);
       FoldFrozenConvMulOrDiv(graph);
     }
-    FrozenConcatLinear(graph);
   }
 }
 
