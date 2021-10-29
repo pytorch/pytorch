8c6ec6196243b186df77f71dd40e087fba24a92d
Mike Ruberry <mruberry@fb.com>
2021-10-29 03:43:16 -0700

    removes noqa

--- test/test_ops.py
+++ test/test_ops.py
@@ -210,7 +210,7 @@ class TestCommon(TestCase):
     @onlyOnCPUAndCUDA
     @suppress_warnings
     @ops(op_db, allowed_dtypes=(torch.float32, torch.long, torch.complex64))
-    def test_noncontiguous_samples(self, device, dtype, op):  # noqa
+    def test_noncontiguous_samples(self, device, dtype, op):
         test_grad = dtype in op.supported_backward_dtypes(torch.device(device).type)
         sample_inputs = op.sample_inputs(device, dtype, requires_grad=test_grad)
         for sample_input in sample_inputs:
