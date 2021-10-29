3c68679b18e6e34e161d7c843c50db747b7f4ecb
Michael Andreas Dagitses <mikeyd@fb.com>
2021-10-29 18:43:10 +0000

    remove unused target_arg parameter from multi_margin_loss_shape_check
    [ghstack-poisoned]

--- aten/src/ATen/native/LossMultiMargin.cpp
+++ aten/src/ATen/native/LossMultiMargin.cpp
@@ -13,7 +13,6 @@ void multi_margin_loss_shape_check(
   int64_t& nframe,
   int64_t& dim,
   const int64_t& ndims,
-  TensorArg& target_arg,
   const Tensor& input,
   const Tensor& target) {
   bool valid_inputs = (ndims == 2 && input.size(1) != 0) || (ndims == 1 && input.size(0) != 0) || ndims == 0;
@@ -124,11 +123,10 @@ void multi_margin_loss_out_cpu_template(
   // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
   int64_t nframe, dim;
   const auto ndims = input.dim();
-  auto target_arg = TensorArg(target, "target", 2);
 
   TORCH_CHECK(p == 1 || p == 2, "only p == 1 and p == 2 supported");
 
-  multi_margin_loss_shape_check(nframe, dim, ndims, target_arg, input, target);
+  multi_margin_loss_shape_check(nframe, dim, ndims, input, target);
 
   // produce a scalar output for 1d input
   if (reduction == Reduction::None && target.dim() > 0) {
@@ -232,12 +230,11 @@ void multi_margin_loss_backward_out_cpu_template(
     int64_t reduction) {
   // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
   int64_t nframe, dim;
-  auto target_arg = TensorArg(target, "target", 2);
   const auto ndims = input.dim();
 
   TORCH_CHECK(p == 1 || p == 2, "only p == 1 and p == 2 supported");
 
-  multi_margin_loss_shape_check(nframe, dim, ndims, target_arg, input, target);
+  multi_margin_loss_shape_check(nframe, dim, ndims, input, target);
   grad_input.resize_as_(input);
   TORCH_CHECK(grad_input.is_contiguous(), "grad_input must be contiguous");
 
