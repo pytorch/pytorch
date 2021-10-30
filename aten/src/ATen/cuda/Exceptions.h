aa16de517df967ac7a9e2025a1361a54349e404c
Mike Ruberry <mruberry@fb.com>
2021-10-30 03:49:44 -0700

    Revert D31984694: [pytorch][PR] make `TORCH_(CUDABLAS|CUSOLVER)_CHECK` usable in custom extensions
    Test Plan: revert-hammer
    
    Differential Revision:
    D31984694 (https://github.com/pytorch/pytorch/commit/d4493b27ee513100aab94381a2fc86ba6188f1a1)
    
    Original commit changeset: 0035ecd13980
    
    fbshipit-source-id: c85689007719c9e4a930b0a8a32d481a501d3c14

--- aten/src/ATen/cuda/Exceptions.h
+++ aten/src/ATen/cuda/Exceptions.h
@@ -2,7 +2,6 @@
 
 #include <cublas_v2.h>
 #include <cusparse.h>
-#include <c10/macros/Export.h>
 
 #ifdef CUDART_VERSION
 #include <cusolver_common.h>
@@ -40,7 +39,7 @@ class CuDNNError : public c10::Error {
   } while (0)
 
 namespace at { namespace cuda { namespace blas {
-C10_EXPORT const char* _cublasGetErrorEnum(cublasStatus_t error);
+const char* _cublasGetErrorEnum(cublasStatus_t error);
 }}} // namespace at::cuda::blas
 
 #define TORCH_CUDABLAS_CHECK(EXPR)                              \
@@ -67,7 +66,7 @@ const char *cusparseGetErrorString(cusparseStatus_t status);
 #ifdef CUDART_VERSION
 
 namespace at { namespace cuda { namespace solver {
-C10_EXPORT const char* cusolverGetErrorMessage(cusolverStatus_t status);
+const char* cusolverGetErrorMessage(cusolverStatus_t status);
 }}} // namespace at::cuda::solver
 
 #define TORCH_CUSOLVER_CHECK(EXPR)                                              \
