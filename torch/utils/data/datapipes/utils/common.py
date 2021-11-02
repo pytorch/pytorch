a41ac6f33fef8448dc50bcc511173fd840c9f7b3
Kevin Tse <ktse@fb.com>
2021-11-02 18:36:28 -0400

    [DataPipe] Changing StreamWrapper API to handle more subclass behaviors
    [ghstack-poisoned]

--- torch/utils/data/datapipes/utils/common.py
+++ torch/utils/data/datapipes/utils/common.py
@@ -52,7 +52,7 @@ def get_file_binaries_from_pathnames(pathnames: Iterable, mode: str):
         if not isinstance(pathname, str):
             raise TypeError("Expected string type for pathname, but got {}"
                             .format(type(pathname)))
-        yield pathname, StreamWrapper(open(pathname, mode))
+        yield pathname, StreamWrapper(open(pathname, mode))  #open(pathname, mode)  #StreamWrapper(open(pathname, mode))
 
 def validate_pathname_binary_tuple(data: Tuple[str, IOBase]):
     if not isinstance(data, tuple):
@@ -80,13 +80,16 @@ class StreamWrapper:
     def __getattr__(self, name):
         return getattr(self.file_obj, name)
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.file_obj.close()
+
+    def __dir__(self) -> Iterable[str]:
+        return ['file_obj'] + dir(self.file_obj)
 
     def __getstate__(self):
         return self.file_obj.__getstate__()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "StreamWrapper<" + repr(self.file_obj) + ">"
 
     def __iter__(self):
