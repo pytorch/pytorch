9888ebff36de4573f91118a9eebe66953db40943
Peter Bell <peterbell10@live.co.uk>
2021-11-03 19:49:26 +0000

    Update base for Update on "Remove native_functions.yaml dependency from Sorting.cpp"
    Differential Revision: [D31856101](https://our.internmc.facebook.com/intern/diff/D31856101)
    
    [ghstack-poisoned]

--- c10/core/DeviceArray.h
+++ c10/core/DeviceArray.h
@@ -4,10 +4,9 @@ namespace c10 {
 
 template <typename T>
 class C10_API DeviceArray {
-public:
-
-  DeviceArray(c10::Allocator &allocator, size_t size):
-    data_ptr_(allocator.allocate(size * sizeof(T))) {
+ public:
+  DeviceArray(c10::Allocator& allocator, size_t size)
+      : data_ptr_(allocator.allocate(size * sizeof(T))) {
     static_assert(std::is_pod<T>::value, "T must be 'plain old data'");
   }
 
@@ -15,8 +14,8 @@ public:
     return static_cast<T*>(data_ptr_.get());
   }
 
-private:
+ private:
   c10::DataPtr data_ptr_;
 };
 
-}  // namespace c10
+} // namespace c10
