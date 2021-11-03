510336499b4f13f10034893333fbd924e271631b
Scott Wolchok <swolchok@fb.com>
2021-11-02 23:57:19 -0700

    [PyTorch][Static Runtime] Separate overlap checks for easier debugging (#66637)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66637
    
    We can give more information when verify_no_memory_overlap would fail by separating the DCHECK.
    ghstack-source-id: 142226105
    
    Test Plan: fitsships
    
    Reviewed By: d1jang
    
    Differential Revision: D31517151
    
    fbshipit-source-id: 8cbc324c27f6b4db4489d1bd469d37b1d8ae6ce1

--- torch/csrc/jit/runtime/static/impl.cpp
+++ torch/csrc/jit/runtime/static/impl.cpp
@@ -1575,6 +1575,11 @@ static bool checkNoMemoryOverlap(const at::Tensor& a, const at::Tensor& b) {
 }
 
 bool ProcessedNode::verify_no_memory_overlap() const {
+  return verify_outputs_dont_overlap_each_other() &&
+      verify_inputs_dont_overlap_outputs();
+}
+
+bool ProcessedNode::verify_outputs_dont_overlap_each_other() const {
   for (const auto i : c10::irange(outputs_size_)) {
     if (!outputs_[i].isTensor()) {
       continue;
@@ -1592,7 +1597,10 @@ bool ProcessedNode::verify_no_memory_overlap() const {
       }
     }
   }
+  return true;
+}
 
+bool ProcessedNode::verify_inputs_dont_overlap_outputs() const {
   auto schema = node()->maybeSchema();
   // skip memory overlap check for mutable ops with only one output
   if (!schema || (schema->is_mutable() && outputs_size_ == 1)) {
