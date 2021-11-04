2486061c72c12ac7eb53946448f0a29525bf8cd1
Elias Ellison <eellison@devfair044.h1.fair>
2021-11-03 20:09:52 -0700

    [JIT] make x (+ or -) 0 and x (* or /) 1 peepholes type promotion aware (#67688)
    Summary:
    Some of the "no-ops" are not actually no-ops because they can change the dtype
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67688
    
    Reviewed By: davidberard98
    
    Differential Revision: D32104601
    
    Pulled By: eellison
    
    fbshipit-source-id: ccb99179a4b30fd20b5a9228374584f2cdc8ec21

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -458,11 +458,11 @@ void initJITBindings(PyObject* module) {
           [](std::shared_ptr<Graph>& g) { return InlineFunctionalGraphs(g); })
       .def(
           "_jit_pass_peephole",
-          [](const std::shared_ptr<Graph>& g, bool addmm_fusion_enabled) {
-            return PeepholeOptimize(g, addmm_fusion_enabled);
+          [](const std::shared_ptr<Graph>& g, bool disable_shape_peepholes) {
+            return PeepholeOptimize(g, disable_shape_peepholes);
           },
           py::arg("graph"),
-          py::arg("addmm_fusion_enabled") = false)
+          py::arg("disable_shape_peepholes") = false)
       .def(
           "_jit_pass_peephole_list_idioms",
           [](const std::shared_ptr<Graph>& g, bool refine_list_len) {
