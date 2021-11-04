6742d63d3f90d3238ee00e171f2b4adc0554ac53
Jiewen Tan <jwtan@fb.com>
2021-11-03 17:08:59 -0700

    [LTC] A little bit more clean up on class Shape
    Summary:
    This commit improves class Shape a bit.
    
    Test Plan:
    lazy_tensor_core/test/cpp/build/test_ptltc

--- lazy_tensor_core/lazy_tensors/shape.cc
+++ lazy_tensor_core/lazy_tensors/shape.cc
@@ -10,4 +10,12 @@ std::string Shape::ToString() const {
   return c10::str(toString(scalar_type_), "[", c10::Join(",", sizes_), "]");
 }
 
+bool Shape::operator==(const Shape& other) const {
+  return scalar_type_ == other.scalar_type_ && sizes_ == other.sizes_;
+}
+
+std::ostream& operator<<(std::ostream& out, const Shape& shape) {
+  return out << shape.ToString();
+}
+
 }  // namespace lazy_tensors
