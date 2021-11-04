fb89e88ac1fa9f07b619d66a1e732ad1df5d59da
fduwjj <fduwjj@fb.com>
2021-11-03 20:10:23 -0700

    Update on "[PyTorch][3/N] Enable the rest forward spec options for ShardedEmbedding and ShardedEmbeddingBag."
    We have enabled the sharding embedding and embedding bag in https://github.com/pytorch/pytorch/pull/67188 and https://github.com/pytorch/pytorch/pull/66604. We now want to enable as many parameters as defined in doc as possible: https://pytorch.org/docs/stable/generated/torch.nn.functional.embedding_bag.html, https://pytorch.org/docs/stable/generated/torch.nn.functional.embedding.html.
    
    For the ones that we don't support we just throw exception.
    
    Last but not least, we use get to get params instead of directly using the key.
    
    Differential Revision: [D31985333](https://our.internmc.facebook.com/intern/diff/D31985333/)
    
    [ghstack-poisoned]

--- torch/distributed/_sharded_tensor/ops/__init__.py
+++ torch/distributed/_sharded_tensor/ops/__init__.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-
 from .init import kaiming_uniform_, normal_, uniform_
 from .linear import sharded_linear
 from .embedding import sharded_embedding
--- torch/distributed/_sharded_tensor/ops/embedding.py
+++ torch/distributed/_sharded_tensor/ops/embedding.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from typing import cast
 
 import torch
