7aef6e30ddc726f9acdfa6d443921fcd1cde2234
Jane Xu <janeyx@fb.com>
2021-11-08 13:13:16 -0500

    Add recursion to make the lint more stable

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -554,6 +554,24 @@ def sanitize_test_filename(filename):
     strip_py = re.sub(r'.py$', '', filename)
     return re.sub('/', r'.', strip_py)
 
+def lint_test_case_extension(suite):
+    succeed = True
+    for test_case_or_suite in suite:
+        test_case = test_case_or_suite
+        if isinstance(test_case_or_suite, unittest.TestSuite):
+            first_test = test_case_or_suite._tests[0] if len(test_case_or_suite._tests) > 0 else None
+            if first_test is not None and isinstance(first_test, unittest.TestSuite):
+                return succeed and lint_test_case_extension(test_case_or_suite)
+            test_case = first_test
+
+        if test_case is not None:
+            test_class = test_case.id().split('.', 1)[1].split('.')[0]
+            if not isinstance(test_case, TestCase):
+                err = "This test class should extend from torch.testing._internal.common_utils.TestCase but it doesn't."
+                print(f"{test_class} - failed. {err}")
+                succeed = False
+    return succeed
+
 def run_tests(argv=UNITTEST_ARGS):
     # import test files.
     if IMPORT_SLOW_TESTS:
@@ -577,18 +595,7 @@ def run_tests(argv=UNITTEST_ARGS):
 
     # Before running the tests, lint to check that every test class extends from TestCase
     suite = unittest.TestLoader().loadTestsFromModule(__main__)
-    failed = False
-    for tc in suite:
-        test_case = tc
-        if isinstance(tc, unittest.TestSuite):
-            test_case = tc._tests[0] if len(tc._tests) > 0 else None
-        if test_case is not None:
-            test_class = test_case.id().split('.', 1)[1].split('.')[0]
-            if not isinstance(test_case, TestCase):
-                err = "This test class should extend from torch.testing._internal.common_utils.TestCase but it doesn't."
-                print(f"{test_class} - failed. {err}")
-                failed = True
-    if failed:
+    if not lint_test_case_extension(suite):
         sys.exit(1)
 
     if TEST_IN_SUBPROCESS:
