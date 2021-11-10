4c363e919b5563a79bcb0ef156cbf5ed3909c530
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared16405.03.ash7.facebook.com>
2021-11-10 04:01:46 -0800

    2021-11-10 postnightly release (a6c0edff1a60e299d3f9794407f0a1c9a269fd19)

--- torch/csrc/jit/runtime/static/impl.h
+++ torch/csrc/jit/runtime/static/impl.h
@@ -182,6 +182,7 @@ class TORCH_API StaticModule {
   }
 
   const Module& module() const {
+    DCHECK(module_.has_value());
     return *module_;
   }
 
@@ -229,14 +230,13 @@ class TORCH_API StaticModule {
   }
 
   bool first_input_is_self() const {
-    return first_input_is_self_;
+    return module_.has_value();
   }
 
   StaticRuntime& runtime();
 
  private:
   StaticModuleOptions opts_;
-  bool first_input_is_self_{false};
   std::shared_ptr<torch::jit::Graph> graph_;
   c10::optional<torch::jit::Module> module_;
   c10::optional<c10::FunctionSchema> schema_;
@@ -336,6 +336,10 @@ class TORCH_API StaticRuntime {
 
   const Graph& graph() const {
     return static_module_.graph();
+  }
+
+  const MemoryPlanner* get_memory_planner() const {
+    return planner_.get();
   }
 
   void check_for_memory_leak(bool output_returned = true);
