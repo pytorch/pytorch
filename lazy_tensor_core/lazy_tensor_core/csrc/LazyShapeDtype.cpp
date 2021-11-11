be45db727a9514985b85e861b4749c9fced54dd0
Will Constable <whc@fb.com>
2021-11-10 16:28:28 -0800

    fix masked_fill logic

--- lazy_tensor_core/lazy_tensor_core/csrc/LazyShapeDtype.cpp
+++ lazy_tensor_core/lazy_tensor_core/csrc/LazyShapeDtype.cpp
@@ -55,13 +55,13 @@ std::vector<std::vector<int64_t>> compute_shape_masked_fill(at::Tensor & self, c
   return {self.sizes().vec()};
 }
 std::vector<c10::ScalarType> compute_dtype_masked_fill(at::Tensor & self, const at::Tensor & mask, const at::Scalar & value) {
-  return {value.type()};
+  return {self.scalar_type()};
 }
 std::vector<std::vector<int64_t>> compute_shape_masked_fill(at::Tensor & self, const at::Tensor & mask, const at::Tensor & value) {
   return {self.sizes().vec()};
 }
 std::vector<c10::ScalarType> compute_dtype_masked_fill(at::Tensor & self, const at::Tensor & mask, const at::Tensor & value) {
-   return {value.scalar_type()};
+   return {self.scalar_type()};
 }
 std::vector<std::vector<int64_t>> compute_shape_embedding_dense_backward(const at::Tensor& grad_output, const at::Tensor& indices, int64_t num_weights, int64_t padding_idx, bool scale_grad_by_freq) {
   // Based on aten/src/ATen/native/Embedding.cpp::embedding_dense_backward_cpu.
