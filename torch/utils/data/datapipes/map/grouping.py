17c7e5bef431d0a84703ae9085ddef9d15d6244c
Kevin Tse <ktse@fb.com>
2021-11-11 15:16:26 -0500

    [DataPipe] Adding BatcherMapDataPipe
    [ghstack-poisoned]

--- torch/utils/data/datapipes/map/grouping.py
+++ torch/utils/data/datapipes/map/grouping.py
@@ -1,8 +1,71 @@
 from torch.utils.data import MapDataPipe, functional_datapipe, DataChunk
-from typing import TypeVar
+from typing import List, Optional, Sized, TypeVar
 
 
 T = TypeVar('T')
+
+@functional_datapipe('batch')
+class BatcherMapDataPipe(MapDataPipe[DataChunk]):
+    r""" :class:`BatcherMapDataPipe`.
+
+    Map DataPipe to create mini-batches of data. An outer dimension will be added as
+    `batch_size` if `drop_last` is set to `True`, or `length % batch_size` for the
+    last batch if `drop_last` is set to `False`.
+
+    Args:
+        datapipe: Iterable DataPipe being batched
+        batch_size: The size of each batch
+        drop_last: Option to drop the last batch if it's not full
+        unbatch_level: Specifies if it necessary to unbatch source data before
+            applying new batching rule
+    """
+    datapipe: MapDataPipe
+    batch_size: int
+    drop_last: bool
+    length: Optional[int]
+
+    def __init__(self,
+                 datapipe: MapDataPipe[T],
+                 batch_size: int,
+                 drop_last: bool = False,
+                 unbatch_level: int = 0,
+                 wrapper_class=DataChunk,
+                 ) -> None:
+        assert batch_size > 0, "Batch size is required to be larger than 0!"
+        super().__init__()
+        if unbatch_level == 0:
+            self.datapipe = datapipe
+        else:
+            self.datapipe = datapipe.unbatch(unbatch_level=unbatch_level)
+        self.unbatch_level = unbatch_level
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.length = None
+        self.wrapper_class = wrapper_class
+
+    def __getitem__(self, index) -> DataChunk:
+        batch: List[T] = []
+        indices = range(index * self.batch_size, (index + 1) * self.batch_size)
+        try:
+            for i in indices:
+                batch.append(self.datapipe[i])
+            return self.wrapper_class(batch)
+        except IndexError:
+            if not self.drop_last and len(batch) > 0:
+                return self.wrapper_class(batch)
+            else:
+                raise IndexError(f"Index {index} is out of bound.")
+
+    def __len__(self) -> int:
+        if self.length is not None:
+            return self.length
+        if isinstance(self.datapipe, Sized) and self.unbatch_level == 0:
+            if self.drop_last:
+                self.length = len(self.datapipe) // self.batch_size
+            else:
+                self.length = (len(self.datapipe) + self.batch_size - 1) // self.batch_size
+            return self.length
+        raise TypeError("{} instance doesn't have valid length".format(type(self).__name__))
 
 
 @functional_datapipe('unbatch')
