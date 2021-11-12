1543930501f18464edb4f234954565018d111cd5
Richard Zou <zou3519@gmail.com>
2021-11-12 15:04:53 -0800

    [functorch] Better error on item() and data dependent control flow

--- functorch/functorch/csrc/BatchRulesDynamic.cpp
+++ functorch/functorch/csrc/BatchRulesDynamic.cpp
@@ -20,9 +20,41 @@ void unsupportedDynamicOp(const c10::OperatorHandle& op, torch::jit::Stack* stac
 #define UNSUPPORTED_DYNAMIC(op) \
     m.impl(#op, torch::CppFunction::makeFromBoxedFunction<&unsupportedDynamicOp>());
 
+// NB: item and is_nonzero can decompose to this...
+void unsupportedLocalScalarDense(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+    TORCH_CHECK(false,
+        "vmap: It looks like you're either (1) calling .item() on a Tensor or ",
+        "(2) attempting to use a Tensor in some data-dependent control flow or ",
+        "(3) encountering this error in PyTorch internals. ",
+        "For (1): we don't support vmap over calling .item() on a Tensor, please try to ",
+        "rewrite what you're doing with other operations. ",
+        "For (2): If you're doing some ",
+        "control flow instead, we don't support that yet, please shout over at ",
+        "https://github.com/pytorch/functorch/issues/257 .",
+        "For (3): please file an issue.");
+}
+
+void unsupportedItem(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+    TORCH_CHECK(false,
+        "vmap: It looks like you're calling .item() on a Tensor. ",
+        "We don't support vmap over calling .item() on a Tensor, please try to ",
+        "rewrite what you're doing with other operations.");
+}
+
+void unsupportedIsNonzero(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+    TORCH_CHECK(false,
+        "vmap: It looks like you're attempting to use a Tensor in some ",
+        "data-dependent control flow. ",
+        "We don't support that yet, please shout over at ",
+        "https://github.com/pytorch/functorch/issues/257 .");
+}
+
 TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
     UNSUPPORTED_DYNAMIC(nonzero);
     UNSUPPORTED_DYNAMIC(unique);
+    m.impl("_local_scalar_dense", torch::CppFunction::makeFromBoxedFunction<&unsupportedLocalScalarDense>());
+    m.impl("item", torch::CppFunction::makeFromBoxedFunction<&unsupportedItem>());
+    m.impl("is_nonzero", torch::CppFunction::makeFromBoxedFunction<&unsupportedIsNonzero>());
 }
 
 }}
