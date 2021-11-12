e7906d57d7d7db0486b41a01de13fc2fc0baf5cf
Rohan Varma <rvarm1@fb.com>
2021-11-12 09:19:20 -0800

    Update on "[c10d] Fix object-based collectives for debug mode"
    DETAIL debug mode didn't work with object-based collectives for NCCL backend, because we'd only check if backend is NCCL and then move tensors to CUDA.
    
    Instead, check if it is a wrapped PG, and then check the pg that is wrapped to see if its nccl.
    
    Differential Revision: [D32366840](https://our.internmc.facebook.com/intern/diff/D32366840/)
    
    cc pietern mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse @SciPioneer @H-Huang
    
    [ghstack-poisoned]

--- torch/_C/_distributed_c10d.pyi
+++ torch/_C/_distributed_c10d.pyi
@@ -351,6 +351,8 @@ class _ProcessGroupWrapper(ProcessGroup):
         pg: ProcessGroup,
         gloo_pg: ProcessGroupGloo
     ): ...
+    wrapped_pg: ProcessGroup
+
 
 class ProcessGroupNCCL(ProcessGroup):
     class Options: ...
