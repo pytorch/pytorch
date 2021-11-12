1a8b803ab4711842fdfa1abacc50526ec71177c4
Maksim Levental <mlevental@fb.com>
2021-11-12 07:12:50 -0600

    Update on "[JIT][Symbolic Shape Analysis] expose op shape functions"
    fixes https://github.com/pytorch/pytorch/issues/68204
    
    cc @silvasean 
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/symbolic_shape_registry.cpp
+++ torch/csrc/jit/runtime/symbolic_shape_registry.cpp
@@ -12,7 +12,7 @@ namespace {
 std::mutex lock;
 
 const std::string _shape_compute_functions =
-#include "torch/jit/shapes.py"
+#include "torch/jit/_shapes.py"
     ;
 
 const std::string _xnnpack_shape_compute_functions =
