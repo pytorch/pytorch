d72b4d9048ecdacf5a57d7f256213334d027f950
Mikhail Zolotukhin <mvz@fb.com>
2021-11-12 13:01:29 -0800

    Update base for Update on "[TensorExpr] Add a way to define target triple/cpu/attrs for llvm codegen and turn on the AOT workflow."
    Differential Revision: [D31593869](https://our.internmc.facebook.com/intern/diff/D31593869)
    
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/tensorexpr_init.cpp
+++ torch/csrc/jit/tensorexpr/tensorexpr_init.cpp
@@ -453,6 +453,7 @@ void initTensorExprBindings(PyObject* module) {
 
   py::class_<LoopNest>(te, "LoopNest")
       .def(py::init<const std::vector<Tensor>&>())
+      .def(py::init<const std::vector<Tensor>&, const std::vector<Tensor>&>())
       .def(py::init([](StmtPtr s, const std::vector<BufHandle>& bufs) {
         std::unordered_set<BufPtr> buf_nodes;
         for (auto& buf : bufs) {
@@ -623,7 +624,7 @@ void initTensorExprBindings(PyObject* module) {
             return LoopNest::compressBuffer(buf.node(), stmt);
           },
           py::return_value_policy::reference)
-      .def(
+      .def_static(
           "cache_accesses",
           [](const BufHandle& producer,
              const std::string& name,
@@ -633,7 +634,7 @@ void initTensorExprBindings(PyObject* module) {
             return std::make_pair(BufHandle(ret.first), ret.second);
           },
           py::return_value_policy::reference)
-      .def(
+      .def_static(
           "compute_at",
           [](StmtPtr s, ForPtr at) { LoopNest::computeAt(s, at); })
       .def(
@@ -885,12 +886,6 @@ void initTensorExprBindings(PyObject* module) {
       });
   te.def("annotate_input_shapes", &tensorexpr::annotateInputShapes);
   te.def("remove_unused_self_argument", &tensorexpr::removeUnusedSelfArgument);
-  te.def("is_graph_compilable", &tensorexpr::isGraphCompilable);
-  te.def("fixup_missing_shape_info", &tensorexpr::fixupMissingShapeInfo);
-  te.def("remove_graph_output", &tensorexpr::removeGraphOutput);
-  te.def(
-      "replace_list_output_with_tuple",
-      &tensorexpr::replaceListOutputWithTuple);
 }
 } // namespace jit
 } // namespace torch
