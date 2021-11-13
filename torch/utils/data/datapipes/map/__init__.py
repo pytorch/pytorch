39fee7044be67d5719b79bfd1e4aec277775a01f
Kevin Tse <ktse@fb.com>
2021-11-12 19:40:32 -0500

    Update on "[DataPipe] Adding UnBatcherMapDataPipe"
    cc @VitalyFedyunin ejguan @NivekT
    
    [ghstack-poisoned]

--- torch/utils/data/datapipes/map/__init__.py
+++ torch/utils/data/datapipes/map/__init__.py
@@ -4,10 +4,13 @@ from torch.utils.data.datapipes.map.combining import (
     ConcaterMapDataPipe as Concater,
     ZipperMapDataPipe as Zipper
 )
+from torch.utils.data.datapipes.map.grouping import (
+    UnBatcherMapDataPipe as UnBatcher
+)
 from torch.utils.data.datapipes.map.utils import SequenceWrapperMapDataPipe as SequenceWrapper
 
 
-__all__ = ['Concater', 'Mapper', 'SequenceWrapper', 'Zipper']
+__all__ = ['Concater', 'Mapper', 'SequenceWrapper', 'UnBatcher', 'Zipper']
 
 # Please keep this list sorted
 assert __all__ == sorted(__all__)
--- torch/utils/data/datapipes/map/grouping.py
+++ torch/utils/data/datapipes/map/grouping.py
@@ -1,5 +1,7 @@
+import warnings
+
 from torch.utils.data import MapDataPipe, functional_datapipe, DataChunk
-from typing import TypeVar
+from typing import List, TypeVar
 
 
 T = TypeVar('T')
@@ -21,41 +23,88 @@ class UnBatcherMapDataPipe(MapDataPipe[T]):
     def __init__(self,
                  datapipe: MapDataPipe[DataChunk],
                  unbatch_level: int = 1):
+        if (not isinstance(unbatch_level, int)) or unbatch_level < -1:
+            raise RuntimeError(f"unbatch_level must be an integer > -1, got {unbatch_level} instead.")
         self.datapipe = datapipe
-        self.unbatch_level = unbatch_level
+        self.deepest_level = self._get_deepest_level()
+        self.unbatch_level = unbatch_level if unbatch_level != -1 else self.deepest_level
+        self.length = None
+
+    def _get_batch_sizes(self) -> List[int]:
+        sizes = [len(self.datapipe)]
+        curr = self.datapipe.__getitem__(0)
+        while isinstance(curr, list) or isinstance(curr, DataChunk):
+            sizes.append(len(curr))
+            curr = curr[0]
+        return sizes
+
+    def _get_deepest_level(self) -> int:
+        level = 0
+        curr = self.datapipe
+        try:
+            while isinstance(curr[0], list) or isinstance(curr[0], DataChunk):
+                curr = curr[0]
+                level += 1
+        except IndexError:
+            pass
+        return level
+
+    def _unbatch_up_to_level(self, target, unbatch_level: int) -> T:
+        # print("UNBATCH UP TO LEVEL")
+        # print(f"target: {target}")
+        # print(f"unbatch_level: {unbatch_level}")
+        if unbatch_level == 0 or not (isinstance(target[0], list) or isinstance(target[0], DataChunk)):
+            return target
+        else:
+            res = []
+            for ele in target:
+                ele_res = self._unbatch_up_to_level(ele, unbatch_level - 1)
+                for child in ele_res:
+                    res.append(child)
+            # print(res)
+            return res
+
+    # This only unbatch a specific level
+    # def _unbatch_deeper_level(self, target, unbatch_level: int) -> T:
+    #     if unbatch_level == 1:
+    #         if isinstance(target[0], list) or isinstance(target[0], DataChunk):  # Unbatching is possible
+    #             res = []
+    #             for ele in target:
+    #                 for child in ele:
+    #                     res.append(child)
+    #             return res
+    #         else:  # Unbatching is impossible
+    #             warnings.warn("Unbatching is not possible because the elements are not batches.")
+    #             return target
+    #     else:
+    #         return [self._unbatch_deeper_level(ls, unbatch_level - 1) for ls in target]
 
     def __getitem__(self, index) -> T:
-        # TODO: This is not handling unbatch_level, maybe I should do it once up front?
-        try:
-            batch_size = len(self.datapipe[0])
-            if batch_size == 0:
-                raise RuntimeError("Batch 0 is empty")
-            batch_idx, sub_idx = index // batch_size, index % batch_size
-            return self.datapipe[batch_idx][sub_idx]
-        except IndexError:
-            raise IndexError(f"Index {index} is invalid")
+        batch_sizes: List[int] = self._get_batch_sizes()
+        # print(f"__getitem__({index})")
+        if self.unbatch_level == 0:
+            return self.datapipe[index]
+        if self.unbatch_level == 1:
+            if len(batch_sizes) == 1:  # No unbatching is possible
+                warnings.warn("Unbatching is not possible because the elements are not batches.")
+                return self.datapipe[index]
+            else:  # Unbatching is possible
+                curr_batch_size = batch_sizes[1]
+                first_index, second_index = index // curr_batch_size, index % curr_batch_size
+                return self.datapipe[first_index][second_index]
+        else:  # We should go to a deeper level
+            # return self._unbatch_deeper_level(self.datapipe[index], self.unbatch_level - 1)
+            # print("Unbatch Level >= 2")
+            return self._unbatch_up_to_level(self.datapipe, self.unbatch_level)[index]
 
-    # def __iter__(self):
-    #     for element in self.datapipe:
-    #         for i in self._dive(element, unbatch_level=self.unbatch_level):
-    #             yield i
-
-    # def _dive(self, element, unbatch_level):
-    #     if unbatch_level < -1:
-    #         raise ValueError("unbatch_level must be -1 or >= 0")
-    #     if unbatch_level == -1:
-    #         if isinstance(element, list) or isinstance(element, DataChunk):
-    #             for item in element:
-    #                 for i in self._dive(item, unbatch_level=-1):
-    #                     yield i
-    #         else:
-    #             yield element
-    #     elif unbatch_level == 0:
-    #         yield element
-    #     else:
-    #         if isinstance(element, list) or isinstance(element, DataChunk):
-    #             for item in element:
-    #                 for i in self._dive(item, unbatch_level=unbatch_level - 1):
-    #                     yield i
-    #         else:
-    #             raise IndexError(f"unbatch_level {self.unbatch_level} exceeds the depth of the DataPipe")
+    def __len__(self) -> int:
+        if self.length is not None:
+            return self.length
+        self.length = 0
+        while True:
+            try:
+                self.__getitem__(self.length)
+                self.length += 1
+            except IndexError:
+                break
+        return self.length
