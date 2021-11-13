1545838f45d3256c4d470bbf2306feab3163182a
Mikhail Zolotukhin <mvz@fb.com>
2021-11-13 00:49:11 -0800

    [TensorExpr] Add a way to define target triple/cpu/attrs for llvm codegen and turn on the AOT workflow. (#66527)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/66527
    
    Differential Revision:
    D31593869
    D31593869
    
    Test Plan: Imported from OSS
    
    Reviewed By: navahgar
    
    Pulled By: ZolotukhinM
    
    fbshipit-source-id: e7534c11fbcf0dab5f49d01d6053caf77b833ef0

--- torch/csrc/jit/tensorexpr/kernel.h
+++ torch/csrc/jit/tensorexpr/kernel.h
@@ -137,6 +137,7 @@ class TORCH_API TensorExprKernel {
   void fallback(Stack& stack) {
     InterpreterState(code_).run(stack);
   }
+  void recompile();
 
   StmtPtr getCodeGenStmt();
 
@@ -285,6 +286,7 @@ class TORCH_API TensorExprKernel {
   std::vector<ConstantDescr> constants_;
 
   std::unordered_map<c10::Symbol, NNCLoweringFunction> custom_lowerings_;
+  StmtPtr stmt_ = nullptr;
   bool pre_alloc_{false};
   const std::string& kernel_func_name_;
 };
