770d4d5f135732c9cbcada15109f0e86daf99037
Scott Wolchok <swolchok@fb.com>
2021-11-15 13:15:59 -0800

    Update on "[PyTorch] Remove enabled checks in RecordFunction constructor"
    All callers should be checking `shouldRunRecordFunction` before creating a RecordFunction, so we don't need to check if we're enabled again.
    
    Differential Revision: [D32440630](https://our.internmc.facebook.com/intern/diff/D32440630/)
    
    [ghstack-poisoned]

--- aten/src/ATen/record_function.cpp
+++ aten/src/ATen/record_function.cpp
@@ -495,11 +495,17 @@ void enableRecordFunction(bool enable) {
 }
 
 RecordFunction::RecordFunction(RecordScope scope, bool pre_sampled) {
-  // Don't bother checking if we're enabled; caller should be using
-  // `shouldRunRecordFunction`. Can't debug-check that because there's
-  // the possibility that another thread turned it off after
-  // shouldRunRecordFunction returned.
-  manager().init(*this, scope, pre_sampled);
+  if (pre_sampled) {
+    manager().init(*this, scope, pre_sampled);
+  } else {
+    auto* rf_tls_ptr = &rf_tls();
+    if (rf_tls_ptr->tls_record_function_enabled_) {
+      auto& m = manager();
+      if (!m.sorted_global_callbacks_.empty() || !rf_tls_ptr->sorted_tls_callbacks_.empty()) {
+        m.init(*this, scope, pre_sampled);
+      }
+    }
+  }
 }
 
 /* static */
