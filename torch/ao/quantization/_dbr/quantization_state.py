340286880bde738f1db312ba866524f206fa3820
Vasiliy Kuznetsov <vasiliy@fb.com>
2021-11-15 09:21:06 -0500

    dbr quant overhead[2/x]: precalculate op_convert_info
    Summary:
    
    Moves `op_convert_info` to be precalculated in the convert step
    instead of calculated dynamically.  This should help with framework
    overhead.
    
    Test plan:
    
    Noisy benchmark:
    
    ```
    // before
    
    fp32: 0.016103 seconds avg
    fx_prepared: 0.019841 seconds avg, 0.811601 speedup vs fp32
    fx_quantized: 0.011907 seconds avg, 1.352346 speedup vs fp32
    dt_prepared: 0.035055 seconds avg, 0.459357 speedup vs fp32
    dt_quantized: 0.018891 seconds avg, 0.852417 speedup vs fp32
    
    // after
    
    fp32: 0.020535 seconds avg
    fx_prepared: 0.023071 seconds avg, 0.890070 speedup vs fp32
    fx_quantized: 0.011693 seconds avg, 1.756206 speedup vs fp32
    dt_prepared: 0.038691 seconds avg, 0.530734 speedup vs fp32
    dt_quantized: 0.021109 seconds avg, 0.972793 speedup vs fp32
    ```
    
    The benchmark is too noisy to rely on, but according to `cProfiler`
    this removes about 5% of overhead.
    
    [ghstack-poisoned]

--- torch/ao/quantization/_dbr/quantization_state.py
+++ torch/ao/quantization/_dbr/quantization_state.py
@@ -33,6 +33,14 @@ from .utils import (
     get_input_args_quant_dequant_info,
 )
 
+OpConvertInfo = Tuple[
+    Optional[Callable],
+    List[Optional[Tuple[float, int]]],
+    List[bool],
+    Optional[str],
+    Dict[str, Any],
+]
+
 # TODO(future PR): maybe better name
 # TODO(future PR): add serialization support
 class AutoQuantizationState(torch.nn.Module):
@@ -89,6 +97,8 @@ class AutoQuantizationState(torch.nn.Module):
         ]]] = []
         # model name to use in logging results
         self.logging_model_name: Optional[str]
+
+        self.idx_to_op_convert_info: Dict[int, OpConvertInfo] = {}
 
     def has_at_least_one_seen_op_info(self) -> bool:
         return len(self.idx_to_seen_op_infos) > 0
@@ -424,13 +434,7 @@ class AutoQuantizationState(torch.nn.Module):
     def get_op_convert_info(
         self,
         op: Callable,
-    ) -> Tuple[
-        Optional[Callable],
-        List[Optional[Tuple[float, int]]],
-        List[bool],
-        Optional[str],
-        Dict[str, Any]
-    ]:
+    ) -> OpConvertInfo:
         """
         Returns the information needed for convert time modifications to `op`.
         Has no side effects.  Return types:
@@ -446,8 +450,16 @@ class AutoQuantizationState(torch.nn.Module):
         applicable.
         """
         assert self.cur_op_needs_hooks(op)
-        seen_op_info = self._get_cur_seen_op_info()
+        return self.idx_to_op_convert_info[self.idx]
 
+    def calculate_op_convert_info(
+        self,
+        seen_op_info: SeenOpInfo,
+    ) -> OpConvertInfo:
+        """
+        This precalculates the information which will be returned by
+        `get_op_convert_info`.
+        """
         # calculate new op
         maybe_new_op = get_quantized_op(seen_op_info)
 
@@ -465,12 +477,10 @@ class AutoQuantizationState(torch.nn.Module):
         additional_kwargs = {}
         needs_scale_zp = converted_func_needs_scale_zp(seen_op_info)
         if needs_scale_zp:
-            seen_op_info = self._get_cur_seen_op_info()
             output_tensor_infos = seen_op_info.output_tensor_infos
             tensor_id = output_tensor_infos[0].id
             scale, zp = self.tensor_id_to_scale_zp[str(tensor_id)]
             additional_kwargs.update({'scale': scale, 'zero_point': zp})
-
         return maybe_new_op, arg_quant_infos, arg_dequant_infos, \
             packed_param_name, additional_kwargs
 
