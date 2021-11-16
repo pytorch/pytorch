b25edc6aedfe42e8cdbfd1e4fb2f9faa18660389
johncalab <johncalab@fb.com>
2021-11-16 12:05:55 -0600

    fix lint but still need to generate github workflows

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -543,7 +543,7 @@ ANDROID_WORKFLOWS = [
         test_runner_type=LINUX_CPU_TEST_RUNNER,
         exclude_test=True,
         ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_LINUX,LABEL_CIFLOW_CPU, LABEL_CIFLOW_ANDROID, LABEL_CIFLOW_DEFAULT}
+            labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CPU, LABEL_CIFLOW_ANDROID, LABEL_CIFLOW_DEFAULT}
         ),
     ),
 ]
