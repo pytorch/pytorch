bbc24222d25d4e86d5f8c2f94a45f2ad0f95e88c
Scott Wolchok <swolchok@fb.com>
2021-11-16 12:25:38 -0800

    [PyTorch][Static Runtime] Refcount bump pass in native_ops (#68159)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68159
    
    These all look like they'll cause unnecessary refcount bumps to me.
    ghstack-source-id: 143424917
    
    Test Plan:
    CI
    
    TODO profile local_ro?
    
    Reviewed By: hlu1
    
    Differential Revision: D32347392
    
    fbshipit-source-id: d8ed91b5855b86765db00c61ad3650273302c7b6

--- torch/csrc/jit/runtime/static/native_ops.cpp
+++ torch/csrc/jit/runtime/static/native_ops.cpp
@@ -102,7 +102,7 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
         DCHECK(p_node->num_inputs() - 1 == p_node->outputs().size());
         auto dict = p_node->Input(0).toGenericDict();
         for (size_t i = 1; i < p_node->num_inputs(); ++i) {
-          auto key = p_node->Input(i);
+          const auto& key = p_node->Input(i);
           auto value = dict.find(key);
           TORCH_CHECK(value != dict.end(), "Key not in dict: ", key);
           p_node->Output(i - 1) = value->value();
@@ -121,14 +121,14 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
       if (n->input(0)->type()->castRaw<DictType>()) {
         return [](ProcessedNode* p_node) {
           auto dict = p_node->Input(0).toGenericDict();
-          auto key = p_node->Input(1);
+          const auto& key = p_node->Input(1);
           auto value = dict.find(key);
           TORCH_CHECK(value != dict.end(), "Key not in dict: ", key);
           p_node->Output(0) = value->value();
         };
       } else if (n->input(0)->type()->castRaw<ListType>()) {
         return [](ProcessedNode* p_node) {
-          auto list = p_node->Input(0).toList();
+          const auto& list = p_node->Input(0).toList();
           auto idx = p_node->Input(1).toInt();
           p_node->Output(0) = getItem(list, idx);
         };
@@ -200,9 +200,9 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
       return [](ProcessedNode* p_node) {
         auto module = p_node->Input(0).toObject();
         Node* node = p_node->node();
-        const auto type = node->input()->type()->expect<ClassType>();
+        const auto& type = node->input()->type()->expectRef<ClassType>();
         const auto& field = node->s(attr::name);
-        const auto slot = type->getAttributeSlot(field);
+        const auto slot = type.getAttributeSlot(field);
         p_node->Output(0) = module->getSlot(slot);
       };
     });
@@ -214,9 +214,9 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
       return [](ProcessedNode* p_node) {
         auto module = p_node->Input(0).toObject();
         Node* node = p_node->node();
-        const auto type = node->inputs()[0]->type()->expect<ClassType>();
+        const auto& type = node->inputs()[0]->type()->expectRef<ClassType>();
         const auto& field = node->s(attr::name);
-        const auto slot = type->getAttributeSlot(field);
+        const auto slot = type.getAttributeSlot(field);
         module->setSlot(slot, p_node->Input(1));
       };
     });
@@ -430,7 +430,7 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
         auto input_type = p_node->Input(0).type();
 
         auto* node = p_node->node();
-        std::vector<TypePtr> candidates = node->tys(attr::types);
+        const std::vector<TypePtr>& candidates = node->tys(attr::types);
         for (const auto& candidate_type : candidates) {
           if (input_type->isSubtypeOf(*candidate_type)) {
             p_node->Output(0) = true;
@@ -452,7 +452,7 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
         TORCH_INTERNAL_ASSERT(
             num_inputs && num_inputs + 1 == node->outputs().size());
 
-        auto expected_types = node->tys(attr::types);
+        const auto& expected_types = node->tys(attr::types);
 
         for (size_t i = 0; i < num_inputs; i++) {
           p_node->Output(i) = p_node->Input(i);
