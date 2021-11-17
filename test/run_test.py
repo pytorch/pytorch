1aae557e424087a25a3bfa6b99bfa52da1c2ce06
Rohan Varma <rvarm1@fb.com>
2021-11-16 19:42:58 -0800

    [Dist CI][BE] c10d gloo tests run in subprocess
    Per title
    
    Differential Revision: [D32485100](https://our.internmc.facebook.com/intern/diff/D32485100/)
    
    [ghstack-poisoned]

--- test/run_test.py
+++ test/run_test.py
@@ -465,6 +465,10 @@ def test_cuda_primary_ctx(test_module, test_directory, options):
 run_test_with_subprocess = functools.partial(run_test, extra_unittest_args=["--subprocess"]
 
 
+def get_run_test_with_subprocess_fn():
+    return lambda test_module, test_directory, options: run_test_with_subprocess(test_module, test_directory, options)
+
+
 
 def _test_cpp_extensions_aot(test_directory, options, use_ninja):
     if use_ninja:
@@ -614,7 +618,8 @@ CUSTOM_HANDLERS = {
     "test_cpp_extensions_aot_no_ninja": test_cpp_extensions_aot_no_ninja,
     "test_cpp_extensions_aot_ninja": test_cpp_extensions_aot_ninja,
     "distributed/test_distributed_spawn": test_distributed,
-    "distributed/test_c10d_nccl": lambda test_module, test_directory, options: run_test_with_subprocess(test_module, test_directory, options)
+    "distributed/test_c10d_nccl": get_run_test_with_subprocess_fn(),
+    "distributed/test_c10d_gloo": get_run_test_with_subprocess_fn(),
 }
 
 
