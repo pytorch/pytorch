39a43ffe598cadd8e0c671b27c1b52da91cee95e
Peter Bell <peterbell10@live.co.uk>
2021-11-17 18:59:52 +0000

    Update base for Update on "Remove native_functions.yaml dependency from CUDA distributions"
    Differential Revision: [D32500425](https://our.internmc.facebook.com/intern/diff/D32500425)
    
    [ghstack-poisoned]

--- c10/core/DeviceArray.h
+++ c10/core/DeviceArray.h
@@ -8,6 +8,9 @@ class DeviceArray {
   DeviceArray(c10::Allocator& allocator, size_t size)
       : data_ptr_(allocator.allocate(size * sizeof(T))) {
     static_assert(std::is_trivial<T>::value, "T must be a trivial type");
+    TORCH_INTERNAL_ASSERT(
+        0 == (reinterpret_cast<intptr_t>(data_ptr_.get()) % alignof(T)),
+        "c10::DeviceArray: Allocated memory is not aligned for this data type");
   }
 
   T* get() {
