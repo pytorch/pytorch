c70f630779b703307944aa80a5765978b1a075d7
wanchaol <wanchaol@devvm3348.frc0.facebook.com>
2021-11-18 12:23:44 -0800

    [WIP] add state_dict_support for ShardedOptimizer
    This PR added state_dict and load_state_dict support for ShardedOptimizer
    
    Differential Revision: [D32540202](https://our.internmc.facebook.com/intern/diff/D32540202/)
    
    [ghstack-poisoned]

--- torch/distributed/_sharded_optim/api.py
+++ torch/distributed/_sharded_optim/api.py
@@ -46,8 +46,29 @@ class ShardedOptimizer(object):
         instead of parameter indices in torch.Optimizer.
         This allows for advanced functionality like optimizer re-sharding to be implemented.
         """
-        # TODO: implement state_dict
-        raise NotImplementedError("ShardedOptimizer state_dict not implemented yet!")
+        state = self.state
+        param_groups = self.param_groups
+        params = self.params
+
+        # actual param to key mapping, to be used for states mapping
+        param_to_key = {param: key for key, param in params.item()}
+
+        ret_state = {
+            param_to_key[param]: state_val
+            for param, state_val in state.items()
+        }
+
+        ret_groups = []
+        for param_group in param_groups:
+            ret_group = {k: v for k, v in param_group.items() if k != "params"}
+            keys = [param_to_key[param] for param in param_group["params"]]
+            ret_group['params'] = sorted(keys)
+            ret_groups.append(ret_group)
+
+        return {
+            'state': ret_state,
+            'param_groups': ret_groups
+        }
 
 
     def load_state_dict(self, state_dict: Mapping[str, Any]):
@@ -57,5 +78,23 @@ class ShardedOptimizer(object):
             state_dict (dict): ShardedOptimizer state. Should be an object returned
                 from a call to :meth:`state_dict`.
         """
-        # TODO: implement load_state_dict
-        raise NotImplementedError("ShardedOptimizer load_state_dict not implemented yet!")
+        # Validate the state_dict
+        groups = self.param_groups
+        state = self.state
+        saved_groups = state_dict['param_groups']
+        saved_state = state_dict['state']
+
+        # load state values
+        if len(state) != len(saved_state):
+            raise ValueError("loaded state_dict has a different number of parameters")
+
+        for param_key, param in self.params.items():
+            if param not in state:
+                continue
+
+            if param_key not in state:
+                raise ValueError(f"Parameter {param_key} not found!")
+
+            for state_key, state_val in state[param].items():
+                if state_key not in saved_state[param_key]:
+                    pass
