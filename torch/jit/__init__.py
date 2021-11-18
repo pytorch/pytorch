13aa456655a4ebe8175b687b20046a569b17cf7e
Maksim <maksim.levental@gmail.com>
2021-11-17 20:57:07 -0600

    Update on "[JIT][Symbolic Shape Analysis] expose op shape functions"
    let's try this again...
    
    fixes https://github.com/pytorch/pytorch/issues/68204
    
    cc silvasean eellison
    
    [ghstack-poisoned]

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -50,6 +50,7 @@ from torch.jit._async import fork, wait
 from torch.jit._serialization import save, load
 from torch.jit._fuser import optimized_execution, fuser, last_executed_optimized_graph
 from torch.jit._freeze import freeze, optimize_for_inference, run_frozen_optimizations
+from torch.jit import _shapes as _shapes
 
 # For backwards compatibility
 _fork = fork
