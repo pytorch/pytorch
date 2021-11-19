e097c3ce4b868998d3280b5305205dd81df50aa6
Sahan Paliskara <sahanp@fb.com>
2021-11-19 10:23:43 -0800

    passes unit tests
    [ghstack-poisoned]

--- test/package/package_d/test_extern.py
+++ test/package/package_d/test_extern.py
@@ -1,6 +1,6 @@
-import package_d.test_selective_intern as test_selective_intern
+import package_d.test_selective_intern as test_selective_intern # noqa
 
 result = "package_d_file"
 test_number = [
-    456
+    431
     ]
