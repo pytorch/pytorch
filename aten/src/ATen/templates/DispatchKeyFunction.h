81a3defabfd01cad5a169b8e6f5c0c0a8ae77f02
Peter Bell <peterbell10@live.co.uk>
2021-11-20 16:59:55 +0000

    Update base for Update on "Remove CUDA Foreach... files dependency on function operators"
    [ghstack-poisoned]

--- aten/src/ATen/templates/DispatchKeyFunction.h
+++ aten/src/ATen/templates/DispatchKeyFunction.h
@@ -8,7 +8,35 @@
 #include <c10/core/Scalar.h>
 #include <ATen/core/Reduction.h>
 
+// Forward declarations of any types needed in the operator signatures.
+// We can't directly include these classes because it will cause circular include dependencies.
+// This file is included by TensorBody.h, which defines the Tensor class.
+namespace c10 {
+
+template<typename T>
+class optional;
+template<typename T>
+class List;
+class Stream;
+class Scalar;
+struct Storage;
+struct TensorOptions;
+
+}
+
 namespace at {
+
+class Tensor;
+struct Dimname;
+struct Generator;
+using TensorList = c10::ArrayRef<Tensor>;
+using DimnameList = c10::ArrayRef<Dimname>;
+using c10::Stream;
+using c10::Storage;
+using c10::QScheme;
+using c10::Scalar;
+using c10::TensorOptions;
+
 namespace ${dispatch_namespace} {
 
 ${dispatch_namespaced_declarations}
