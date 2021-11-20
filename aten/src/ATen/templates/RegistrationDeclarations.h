dc93d71bc8fabbdad4b0875e9e91d25b256a1672
Peter Bell <peterbell10@live.co.uk>
2021-11-20 16:10:07 +0000

    Functionalization: Only include headers for required ops
    RegisterFunctionalization.cpp is a shared file, so only including the
    required operators means a single operator change only requires 1
    shard to be rebuilt instead of all of them.
    
    [ghstack-poisoned]

--- aten/src/ATen/templates/RegistrationDeclarations.h
+++ aten/src/ATen/templates/RegistrationDeclarations.h
@@ -1,4 +1,5 @@
 // This file contains all native_functions that can be registered to
 // and the schema string that they should be registered with
+#include <ATen/Operators.h>
 
 ${registration_declarations}
