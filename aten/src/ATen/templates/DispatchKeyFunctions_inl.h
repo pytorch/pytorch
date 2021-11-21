c105485f596e0bdce33c1fd0d86a85c04b3e1d3e
Peter Bell <peterbell10@live.co.uk>
2021-11-21 17:17:56 +0000

    Update base for Update on "Codegen: TraceType only includes operators being registered"
    TraceType is a sharded file, so by only including specific operator
    headers, we ensure that changing one (non-method) operator only needs
    one shard to be re-compiled.
    
    This also changes all the included autograd and jit headers from
    including `ATen/ATen.h` to just including `ATen/core/Tensor.h`.
    
    [ghstack-poisoned]

--- aten/src/ATen/templates/DispatchKeyFunctions_inl.h
+++ aten/src/ATen/templates/DispatchKeyFunctions_inl.h
@@ -12,8 +12,8 @@
 #ifdef TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #error This change adds a dependency on all pytorch operators, meaning the     \
   file will need to be re-compiled every time an operator is changed or added. \
-  Consider if your change would be better placed in another file, or if a more \
-  specific header might achieve the same goal.
+  Consider including a specific operator from \
+  <ATen/ops/{my_operator}_${dispatch_namespace}_dispatch.h>
 #endif
 
 $DispatchKeyFunctions_inl_includes
