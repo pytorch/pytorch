8a0cef2b9b9b28b58b62c19b546fe0fc42bcd8c8
Kim Juhyeong <nonconvexopt@gmail.com>
2021-11-21 22:31:50 +0900

    Fix error in positive definite constraint

--- torch/distributions/constraints.py
+++ torch/distributions/constraints.py
@@ -463,9 +463,7 @@ class _PositiveDefinite(Constraint):
     event_dim = 2
 
     def check(self, value):
-        # Assumes that the matrix or batch of matrices in value are symmetric
-        # info == 0 means no error, that is, it's SPD
-        return torch.linalg.cholesky_ex(value).info.eq(0).unsqueeze(0)
+        return (torch.linalg.eig(value)[0].float() > 0).all()
 
 
 class _Cat(Constraint):
