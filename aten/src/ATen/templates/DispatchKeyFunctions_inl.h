d0aac3462419a58f2dd61167fe26848d58bdca04
Peter Bell <peterbell10@live.co.uk>
2021-11-21 17:17:55 +0000

    Update base for Update on "Codegen: Registration now only includes the functions used"
    Currently Register{DispatchKey}.cpp includes all of
    `NativeFunctions.h`, so any operator signature change requires all
    backend registration to be recompiled. However, most backends only
    have registrations for a small fraction of operators so it makes sense
    to only include the specific functions required.
    
    [ghstack-poisoned]

--- aten/src/ATen/templates/DispatchKeyFunctions_inl.h
+++ aten/src/ATen/templates/DispatchKeyFunctions_inl.h
@@ -1,3 +1,4 @@
+#pragma once
 // ${generated_comment}
 
 // NB: The implementing C++ file is RegisterDispatchKey.cpp
@@ -11,14 +12,8 @@
 #ifdef TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #error This change adds a dependency on all pytorch operators, meaning the     \
   file will need to be re-compiled every time an operator is changed or added. \
-  Consider if your change would be better placed in another file, or if a more \
-  specific header might achieve the same goal.
+  Consider including a specific operator from \
+  <ATen/ops/{my_operator}_${dispatch_namespace}_dispatch.h>
 #endif
 
-namespace at {
-namespace ${dispatch_namespace} {
-
-${dispatch_namespaced_declarations}
-
-} // namespace ${dispatch_namespace}
-} // namespace at
+$DispatchKeyFunctions_inl_includes
