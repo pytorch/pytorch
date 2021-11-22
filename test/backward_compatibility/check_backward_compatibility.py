7c6a8a47db112e6efe24d2d55d69fe634b732de1
Wanchao Liang <wanchaol@fb.com>
2021-11-21 23:28:40 -0800

    [BE] minor improvement to dist quantization (#67401)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67401
    
    some minor changes to dist quantization, mainly change the namespace and add some notes for future code dedup
    ghstack-source-id: 143910067
    ghstack-source-id: 143910067
    
    Test Plan: wait for ci
    
    Reviewed By: mrshenli
    
    Differential Revision: D31979269
    
    fbshipit-source-id: 85a2f395e6a3487dd0b9d1fde886eccab106e289

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -72,6 +72,8 @@ ALLOW_LIST = [
     ("caffe2::", datetime.date(2021, 10, 23)),
     ("prepacked::unpack_prepacked_sizes_conv2d", datetime.date(9999, 1, 1)),
     ("prepacked::unpack_prepacked_sizes_linear", datetime.date(9999, 1, 1)),
+    ("q::_FloatToBfloat16Quantized", datetime.date(2021, 12, 21)),
+    ("q::_Bfloat16QuantizedToFloat", datetime.date(2021, 12, 21)),
 ]
 
 ALLOW_LIST_COMPILED = [
