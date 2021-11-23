4b7f91597a991a15ac4f9e1ae548ba817f370ec0
Peter Bell <peterbell10@live.co.uk>
2021-11-23 16:56:11 +0000

    Update base for Update on "Remove native_functions.yaml dependency from IndexKernel.{cpp,cu}"
    Differential Revision: [D31856105](https://our.internmc.facebook.com/intern/diff/D31856105)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/NonEmptyUtils.h
+++ aten/src/ATen/native/NonEmptyUtils.h
@@ -4,20 +4,20 @@
 
 namespace at { namespace native {
 
-static inline int64_t ensure_nonempty_dim(int64_t dim) {
+inline int64_t ensure_nonempty_dim(int64_t dim) {
   return std::max<int64_t>(dim, 1);
 }
 
-static inline int64_t ensure_nonempty_size(const TensorBase &t, int64_t dim) {
+inline int64_t ensure_nonempty_size(const TensorBase &t, int64_t dim) {
   return t.dim() == 0 ? 1 : t.size(dim);
 }
 
-static inline int64_t ensure_nonempty_stride(const TensorBase &t, int64_t dim) {
+inline int64_t ensure_nonempty_stride(const TensorBase &t, int64_t dim) {
   return t.dim() == 0 ? 1 : t.stride(dim);
 }
 
 using IdxVec = std::vector<int64_t>;
-static inline IdxVec ensure_nonempty_vec(IdxVec vec) {
+inline IdxVec ensure_nonempty_vec(IdxVec vec) {
   if (vec.size() == 0) {
     vec.push_back(1);
   }
