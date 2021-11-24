96929ea9954e7485b0206323b92595bd0ed4625f
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2021-11-24 13:59:03 -0800

    Update empty and empty_like examples in docs (#68874)
    Summary:
    For some reason, the example for `torch.empty` showed the usage of `torch.empty_like` and the other way around. These are now swapped.
    
    Fixes https://github.com/pytorch/pytorch/issues/68799
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68874
    
    Reviewed By: wenleix
    
    Differential Revision: D32646645
    
    Pulled By: ejguan
    
    fbshipit-source-id: c8298bcaca450aaa4abeef2239af2b14cadc05b3

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -10521,10 +10521,9 @@ Keyword args:
 
 Example::
 
-    >>> a=torch.empty((2,3), dtype=torch.int32, device = 'cuda')
-    >>> torch.empty_like(a)
-    tensor([[0, 0, 0],
-            [0, 0, 0]], device='cuda:0', dtype=torch.int32)
+    >>> torch.empty((2,3), dtype=torch.int64)
+    tensor([[ 9.4064e+13,  2.8000e+01,  9.3493e+13],
+            [ 7.5751e+18,  7.1428e+18,  7.5955e+18]])
 """.format(**factory_common_args))
 
 add_docstr(torch.empty_like,
@@ -10547,9 +10546,10 @@ Keyword args:
 
 Example::
 
-    >>> torch.empty((2,3), dtype=torch.int64)
-    tensor([[ 9.4064e+13,  2.8000e+01,  9.3493e+13],
-            [ 7.5751e+18,  7.1428e+18,  7.5955e+18]])
+    >>> a=torch.empty((2,3), dtype=torch.int32, device = 'cuda')
+    >>> torch.empty_like(a)
+    tensor([[0, 0, 0],
+            [0, 0, 0]], device='cuda:0', dtype=torch.int32)
 """.format(**factory_like_common_args))
 
 add_docstr(torch.empty_strided,
