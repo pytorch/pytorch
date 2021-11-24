995a91091f5780156d028282ccd80521fd926527
Santiago Castro <bryant1410@gmail.com>
2021-11-24 18:07:06 -0500

    Fix flake

--- torch/utils/data/_utils/collate.py
+++ torch/utils/data/_utils/collate.py
@@ -95,7 +95,7 @@ def default_collate(batch):
         if not all(len(elem) == elem_size for elem in it):
             raise RuntimeError('each element in list of batch should be of equal size')
         transposed = list(zip(*batch))  # It may be accessed twice, so we use a list.
-        
+
         if elem is tuple:
             return [default_collate(samples) for samples in transposed]  # Backwards compatibility.
         else:
