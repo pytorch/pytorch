70c985cf3cd6236311d7231c91ef5d461e8536dd
Kim Juhyeong <nonconvexopt@gmail.com>
2021-11-25 12:44:25 +0900

    Use abstract Constraint method for 'df' argument constraint.

--- torch/distributions/wishart.py
+++ torch/distributions/wishart.py
@@ -46,7 +46,7 @@ class Wishart(Distribution):
         'covariance_matrix': constraints.positive_definite,
         'precision_matrix': constraints.positive_definite,
         'scale_tril': constraints.positive_definite,
-        'df': constraints.greater_than,
+        'df': constraints.Constraint,
     }
     support = constraints.positive_definite
     has_rsample = True
