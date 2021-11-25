07f8e7d3052a259fc090f4b025bfd55a028f8f31
Paul Streli <paul@streli.eu>
2021-11-25 10:36:59 +0100

    Simplify t1.

--- torch/distributions/kl.py
+++ torch/distributions/kl.py
@@ -671,10 +671,10 @@ def _kl_normal_laplace(p, q):
     loc_diff = p.loc - q.loc
     scale_ratio = p.scale / q.scale
     loc_diff_scale_ratio = loc_diff / p.scale
-    t1 = 0.5 * torch.log(0.5 * math.pi * scale_ratio.pow(2))
+    t1 = torch.log(scale_ratio)
     t2 = math.sqrt(2 / math.pi) * p.scale * torch.exp(-0.5 * loc_diff_scale_ratio.pow(2))
     t3 = loc_diff * torch.erf(math.sqrt(0.5) * loc_diff_scale_ratio)
-    return -0.5 - t1 + (t2 + t3) / q.scale
+    return -t1 + (t2 + t3) / q.scale - (0.5 * (1 + math.log(0.5 * math.pi)))
 
 
 @register_kl(Pareto, Beta)
