74a8d4283ccc4bfb00b51ca31c3b56fe48610dc1
Richard Zou <zou3519@users.noreply.github.com>
2021-11-28 18:43:18 -0500

    [functorch] layer_norm batch rule (pytorch/functorch#293)
    Test Plan:
    - run tests

--- functorch/functorch/csrc/BatchRulesNorm.cpp
+++ functorch/functorch/csrc/BatchRulesNorm.cpp
@@ -159,10 +159,123 @@ std::tuple<Tensor,Tensor,Tensor> native_group_norm_plumbing(
   return slow_fallback<Tensor,Tensor,Tensor>(op, { input, weight, bias, N, C, HxW, group, eps });
 }
 
+C10_ALWAYS_INLINE bool has_same_shape(
+    const Tensor& tensor, optional<int64_t> tensor_bdim,
+    IntArrayRef normalized_shape) {
+  if (!tensor.defined()) {
+    return true;
+  }
+  if (rankWithoutBatchDim(tensor, tensor_bdim) != normalized_shape.size()) {
+    return false;
+  }
+  const auto tensor_shape = tensor.sizes();
+  for (const auto i : c10::irange(normalized_shape.size())) {
+    auto j = i;
+    // (0, 1, 2), 1 -> (0, 2, 3)
+    if (tensor_bdim.has_value() && (int64_t)i >= tensor_bdim.value()) {
+      j = j + 1;
+    }
+    if (normalized_shape[i] != tensor_shape[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+C10_ALWAYS_INLINE void check_same_shape(
+    const Tensor& tensor, optional<int64_t> tensor_bdim,
+    IntArrayRef normalized_shape, const std::string& name) {
+  TORCH_CHECK(has_same_shape(tensor, tensor_bdim, normalized_shape),
+      "Expected ", name, " to be of same shape as normalized_shape, but got ",
+      name, " of shape ",
+      tensor.sizes(),
+      " and normalized_shape = ",
+      normalized_shape);
+}
+
+// Ugh, hard to deduplicate
+C10_ALWAYS_INLINE void _check_layer_norm_inputs(
+    IntArrayRef normalized_shape,
+    const Tensor& weight, optional<int64_t> weight_bdim,
+    const Tensor& bias, optional<int64_t> bias_bdim) {
+
+  const int normalized_ndim = normalized_shape.size();
+  TORCH_CHECK(
+      normalized_ndim >= 1,
+      "Expected normalized_shape to be at least 1-dimensional, i.e., ",
+      "containing at least one element, but got normalized_shape = ",
+      normalized_shape);
+  check_same_shape(weight, weight_bdim, normalized_shape, "weight");
+  check_same_shape(bias, bias_bdim, normalized_shape, "weight");
+}
+
+static bool is_empty_tensor(const Tensor& tensor) {
+  const auto shape = tensor.sizes();
+  return shape.size() == 1 && shape[0] == 0;
+}
+
+static optional<int64_t> compute_stat_bdim(
+    optional<int64_t> input_bdim,
+    const Tensor& stat) {
+  // There's a weird case where mean, rstd can both have shape (0,).
+  // It's possible that this is a bug on the PyTorch side.
+  // When that happens we don't want to return a BatchedTensor.
+  if (input_bdim.has_value() && !is_empty_tensor(stat)) {
+    return 0;
+  }
+  return nullopt;
+}
+
+std::tuple<Tensor,optional<int64_t>,Tensor,optional<int64_t>,Tensor,optional<int64_t>>
+native_layer_norm_batch_rule(
+    const Tensor& input, optional<int64_t> input_bdim,
+    IntArrayRef normalized_shape,
+    const c10::optional<Tensor>& weight_opt, optional<int64_t> weight_bdim,
+    const c10::optional<Tensor>& bias_opt, optional<int64_t> bias_bdim,
+    double eps) {
+  auto input_ = moveBatchDimToFront(input, input_bdim);
+  if (!weight_bdim && !bias_bdim) {
+    const auto result = at::native_layer_norm(input_, normalized_shape, weight_opt, bias_opt, eps);
+    const auto mean = std::move(std::get<1>(result));
+    const auto rstd = std::move(std::get<2>(result));
+    const auto stats_bdim = compute_stat_bdim(input_bdim, mean);
+    return std::make_tuple(std::move(std::get<0>(result)), 0, mean, stats_bdim, rstd, stats_bdim);
+  }
+
+  // See [Note: hacky wrapper removal for optional tensor]
+  c10::MaybeOwned<Tensor> weight_maybe_owned = at::borrow_from_optional_tensor(weight_opt);
+  const Tensor& weight = *weight_maybe_owned;
+  c10::MaybeOwned<Tensor> bias_maybe_owned = at::borrow_from_optional_tensor(bias_opt);
+  const Tensor& bias = *bias_maybe_owned;
+  _check_layer_norm_inputs(normalized_shape, weight, weight_bdim, bias, bias_bdim);
+
+  const auto input_logical_rank = rankWithoutBatchDim(input, input_bdim);
+  const auto result = at::native_layer_norm(input_, normalized_shape, nullopt, nullopt, eps);
+  auto result0 = std::move(std::get<0>(result));
+  const auto mean = std::move(std::get<1>(result));
+  const auto rstd = std::move(std::get<2>(result));
+  const auto stats_bdim = compute_stat_bdim(input_bdim, mean);
+
+  if (weight.defined()) {
+    auto weight_ = moveBatchDimToFront(weight, weight_bdim);
+    weight_ = maybePadToLogicalRank(weight_, /*has_bdim*/weight_bdim, input_logical_rank);
+    result0 = result0 * weight_;
+  }
+  if (bias.defined()) {
+    const auto result_logical_rank = rankWithoutBatchDim(
+        result0,
+        input_bdim.has_value() || weight_bdim.has_value() ? optional<int64_t>(0) : optional<int64_t>(nullopt));
+    auto bias_ = moveBatchDimToFront(bias, bias_bdim);
+    bias_ = maybePadToLogicalRank(bias_, /*has_bdim*/bias_bdim, result_logical_rank);
+    result0 = result0 + bias_;
+  }
+  return std::make_tuple(result0, 0, mean, stats_bdim, rstd, stats_bdim);
+}
 
 TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   m.impl("batch_norm", batch_norm_plumbing);
   m.impl("native_group_norm", native_group_norm_plumbing);
+  VMAP_SUPPORT("native_layer_norm", native_layer_norm_batch_rule);
 }
 
 }}
