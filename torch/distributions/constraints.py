b28994a744499ba08e969c103eddce3c5319c927
Kim Juhyeong <nonconvexopt@gmail.com>
2021-11-28 23:33:11 +0900

    Fixing CI errors

--- torch/distributions/constraints.py
+++ torch/distributions/constraints.py
@@ -484,7 +484,7 @@ class _Symmetric(_Square):
         # square_check = super().check(value)
         # if not square_check.all():
         #    return square_check
-        return value.eq(value.mT).all(dim=-1).all(dim=-1)
+        return value.eq(value.mT).all(-2).all(-1)
 
 
 class _PositiveDefinite(_Symmetric):
@@ -496,7 +496,8 @@ class _PositiveDefinite(_Symmetric):
         sym_check = super().check(value)
         if not sym_check.all():
             return sym_check
-        return torch.linalg.cholesky_ex(value).info.eq(0)
+        # return torch.linalg.cholesky_ex(value).info.eq(0)
+        return torch.linalg.eigvalsh(value).gt(0).all(-1)
 
 
 class _Cat(Constraint):
