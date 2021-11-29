e4ac3e79c826eea042c7e65dc39f11eb0229ff82
Sahan Paliskara <sahanp@fb.com>
2021-11-29 14:35:51 -0800

    till the lint
    ghstack-source-id: c04766e646a71ff383e842076cf3d0232e762bcf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69036

--- test/package/package_b/__init__.py
+++ test/package/package_b/__init__.py
@@ -3,10 +3,11 @@ __import__("subpackage_0.subsubpackage_0", globals(), fromlist=[""], level=1)
 __import__("subpackage_2", globals=globals(), locals=locals(), fromlist=["*"], level=1)
 
 result = "package_b"
-
+package_b_li = [123]
 
 class PackageBObject:
     __slots__ = ["obj"]
+    package_b_li = [789]
 
     def __init__(self, obj):
         self.obj = obj
