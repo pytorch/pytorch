eb14ff1fd114b6e69a9e75b7d40d927293178c27
Michael Andreas Dagitses <mikeyd@fb.com>
2021-11-29 14:46:50 +0000

    move multilabel_margin_loss_shape_check into the only file that uses it
    [ghstack-poisoned]

--- aten/src/ATen/native/LossMultiLabelMargin.cpp
+++ aten/src/ATen/native/LossMultiLabelMargin.cpp
@@ -2,13 +2,47 @@
 #include <ATen/AccumulateType.h>
 #include <ATen/Dispatch.h>
 #include <ATen/TensorUtils.h>
-#include <ATen/native/LossMulti.h>
 #include <c10/util/irange.h>
 
 namespace at {
 namespace native {
 
 namespace {
+
+void multilabel_margin_loss_shape_check(
+  int64_t& nframe,
+  int64_t& dim,
+  const int64_t& ndims,
+  TensorArg& target_arg,
+  const Tensor& input,
+  const Tensor& target) {
+  bool valid_inputs = (ndims == 2 && input.size(1) != 0) || (ndims == 1 && input.size(0) != 0) || ndims == 0;
+  TORCH_CHECK(
+              valid_inputs,
+              "Expected non-empty vector or matrix with optional 0-dim batch size, but got: ",
+              input.sizes());
+
+  if (ndims <= 1) {
+    nframe = 1;
+    dim = ndims == 0 ? 1 : input.size(0);
+    TORCH_CHECK(
+                valid_inputs && target.dim() <= 1 && target.numel() == dim,
+                "inconsistent size ",
+                target.sizes(),
+                " for ",
+                target_arg);
+  } else {
+    nframe = input.size(0);
+    dim = input.size(1);
+    TORCH_CHECK(
+                valid_inputs && target.dim() == 2 && target.size(0) == nframe &&
+                target.size(1) == dim,
+                "inconsistent size ",
+                target.sizes(),
+                " for ",
+                target_arg);
+  }
+}
 
 template <typename scalar_t>
 inline scalar_t multilabel_margin_loss_forward_inner_sum_cpu(
