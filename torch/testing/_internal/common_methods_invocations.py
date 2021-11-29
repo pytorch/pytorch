9e2dc2c9d7958288a17f6c786378ccb619467ae2
Peter Bell <peterbell10@live.co.uk>
2021-11-29 08:46:58 -0800

    neg_sparse: Fix output dtype (#68885)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68885
    
    `torch.neg` should preserve the input dtype but for sparse tensors it
    was promoting integers to floating point. This would have been picked
    up by the OpInfo-based test, but `neg` wasn't marked with
    `supports_sparse=True` so it was never run.
    
    cc nikitaved pearu cpuhrsch IvanYashchuk
    
    Test Plan: Imported from OSS
    
    Reviewed By: mrshenli
    
    Differential Revision: D32680008
    
    Pulled By: cpuhrsch
    
    fbshipit-source-id: 502f8743c1c33ab802e3d9d097792887352cd220

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -10702,6 +10702,7 @@ op_db: List[OpInfo] = [
                    dtypes=all_types_and_complex_and(torch.half, torch.bfloat16),
                    error_inputs_func=error_inputs_neg,
                    supports_forward_ad=True,
+                   supports_sparse=True,
                    assert_autodiffed=True,),
     OpInfo('dist',
            op=torch.dist,
