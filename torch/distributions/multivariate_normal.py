7adfb508d7978198c578be8a7889611a8afa4b34
Kim Juhyeong <nonconvexopt@gmail.com>
2021-11-29 16:40:07 +0900

    Revert modification in '_precision_to_scale_tril' in multivariate_normal

--- torch/distributions/multivariate_normal.py
+++ torch/distributions/multivariate_normal.py
@@ -68,12 +68,11 @@ def _batch_mahalanobis(bL, bx):
 
 def _precision_to_scale_tril(P):
     # Ref: https://nbviewer.jupyter.org/gist/fehiepsi/5ef8e09e61604f10607380467eb82006#Precision-to-scale_tril
-    # Lf = torch.linalg.cholesky(torch.flip(P, (-2, -1)))
-    # L_inv = torch.transpose(torch.flip(Lf, (-2, -1)), -2, -1)
-    # L = torch.triangular_solve(torch.eye(P.shape[-1], dtype=P.dtype, device=P.device),
-    #                           L_inv, upper=False)[0]
-    # return L
-    return torch.linalg.cholesky(P.inverse())
+    Lf = torch.linalg.cholesky(torch.flip(P, (-2, -1)))
+    L_inv = torch.transpose(torch.flip(Lf, (-2, -1)), -2, -1)
+    L = torch.triangular_solve(torch.eye(P.shape[-1], dtype=P.dtype, device=P.device),
+                               L_inv, upper=False)[0]
+    return L
 
 
 class MultivariateNormal(Distribution):
