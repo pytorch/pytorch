f05399e358ab7e0b0eb1ff052ef89f4a95395fac
Emilio Castillo <ecastill@preferred.jp>
2021-11-29 01:59:31 +0000

    Add a lightweight reparametrization mechanism for functional calls

--- torch/nn/utils/_stateless.py
+++ torch/nn/utils/_stateless.py
@@ -1,6 +1,43 @@
 import contextlib
 
-import torch
+
+def _change_class(module) -> None:
+    cls = module.__class__
+
+    def _getattr(self, name):
+        if name in module._functional_parameters:
+            return module._functional_parameters[name]
+        return cls.__getattr__(self, name)
+
+    param_cls = type(
+        f"Parametrized{cls.__name__}",
+        (cls,),
+        {
+            "__getattr__": _getattr,
+        },
+    )
+
+    module.__class__ = param_cls
+    module._orig_class = cls
+
+
+def _swap_parameters(module, tensor_name, tensor):
+    # Changes the module class to get a new __getattr__ dunder method
+    # that looks for the reparametrized tensor
+    if hasattr(module, '_functional_parameters'):
+        module._functional_parameters[tensor_name] = tensor
+    else:
+        # change module class to set a new __getattr__ function
+        # register tensor
+        _change_class(module)
+        module._functional_parameters = {}
+        module._functional_parameters[tensor_name] = tensor
+
+
+def _remove_swap(module, name):
+    if hasattr(module, '_orig_class'):
+        module.__class__ = module._orig_class
+        delattr(module, '_orig_class')
 
 
 @contextlib.contextmanager
@@ -8,22 +45,13 @@ def reparametrize_module(module, parameters_and_buffers):
     # Parametrization does not support to change submodules directly
     for name, tensor in parameters_and_buffers.items():
         _apply_func_submodules(
-            torch.nn.utils.parametrize.register_parametrization,
-            module, name.split("."), (_ReparametrizedTensor(tensor),))
+            _swap_parameters,
+            module, name.split("."), (tensor,))
     yield
     for name in parameters_and_buffers:
         _apply_func_submodules(
-            torch.nn.utils.parametrize.remove_parametrizations,
-            module, name.split("."), (False,))
-
-
-class _ReparametrizedTensor(torch.nn.Module):
-    def __init__(self, tensor):
-        super().__init__()
-        self._tensor = tensor
-
-    def forward(self, original):
-        return self._tensor
+            _remove_swap,
+            module, name.split("."), ())
 
 
 def _apply_func_submodules(func, module, path, args):
