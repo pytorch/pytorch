23a0bab89a7c3342a09328e3020cb0f553fcb650
Sahan Paliskara <sahanp@fb.com>
2021-11-30 13:11:23 -0800

    selective intern
    [ghstack-poisoned]

--- test/package/package_b/subpackage_0/subsubpackage_0/__init__.py
+++ test/package/package_b/subpackage_0/subsubpackage_0/__init__.py
@@ -1,7 +1,7 @@
 __import__("subpackage_1", globals(), locals(), ["PackageBSubpackage1Object_0"], 3)
 
 result = "subsubpackage_0"
-
+subsubpackage_0_li = [123]
 
 class PackageBSubsubpackage0Object_0:
     pass
