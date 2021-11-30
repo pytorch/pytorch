7194faed7f362d40f19a4a3f429b38bb81588712
Scott Wolchok <swolchok@fb.com>
2021-11-29 16:31:38 -0800

    [PyTorch] Optimzize mergeRunCallbacks for RecordFunction (#68387)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68387
    
    Function call overhead on tryRunCallback was notable.
    ghstack-source-id: 144235788
    
    Test Plan:
    Run //caffe2/caffe2/fb/high_perf_models/pytorch/benchmark_framework_overheads:cpp_benchmark before/after this diff with arguemnts `--stressTestRecordFunction --op empty`.
    
    Before: P467891339
    After: P467891381
    
    Reviewed By: chaekit
    
    Differential Revision: D32443863
    
    fbshipit-source-id: c0b3dd40bbd5bca976c2ebb0f21aa62e097b302e

--- aten/src/ATen/record_function.cpp
+++ aten/src/ATen/record_function.cpp
@@ -369,7 +369,11 @@ class CallbackManager {
   std::atomic<uint_fast32_t> num_enabled_global_callbacks_;
 
  private:
-  bool tryRunCallback(
+  static void logTryRunCallbackError(const char* what, const RecordFunction& rf) {
+    LOG(WARNING) << "Exception in RecordFunction callback: " << what << " , for the range " << rf.name();
+  }
+
+  C10_ALWAYS_INLINE static bool tryRunCallback(
       const RecordFunctionCallback& rfcb,
       RecordFunction& rf,
       std::unique_ptr<ObserverContext>& ctx,
@@ -385,18 +389,16 @@ class CallbackManager {
       }
       return true;
     } catch (const std::exception &e) {
-      LOG(WARNING) << "Exception in RecordFunction callback: "
-          << e.what() << " , for the range " << rf.name();
+      logTryRunCallbackError(e.what(), rf);
       return false;
     } catch (...) {
-      LOG(WARNING) << "Exception in RecordFunction callback: unknown"
-          << " , for the range " << rf.name();
+      logTryRunCallbackError("unknown", rf);
       return false;
     }
   }
 
   template <typename RecordFunctionCallbacks>
-  void mergeRunCallbacks(
+  static void mergeRunCallbacks(
       const RecordFunctionCallbacks& sorted_callbacks,
       const CallbackHandles& sorted_handles,
       ObserverContextList& ctx_list,
@@ -404,12 +406,15 @@ class CallbackManager {
       RecordFunction& rf) {
     size_t num_executed = 0;
     size_t idx_c = 0;
-    for (size_t idx_h = 0; idx_h < sorted_handles.size() && idx_h < ctx_list.size(); ++idx_h) {
-      while (idx_c < sorted_callbacks.size() &&
+    const auto sorted_handles_size = sorted_handles.size();
+    const auto ctx_list_size = ctx_list.size();
+    const auto sorted_callbacks_size = sorted_callbacks.size();
+    for (size_t idx_h = 0; idx_h < sorted_handles_size && idx_h < ctx_list_size; ++idx_h) {
+      while (idx_c < sorted_callbacks_size &&
             sorted_callbacks[idx_c].handle < sorted_handles[idx_h]) {
         ++idx_c;
       }
-      if (idx_c >= sorted_callbacks.size()) {
+      if (idx_c >= sorted_callbacks_size) {
         break;
       }
       if (sorted_callbacks[idx_c].handle == sorted_handles[idx_h]) {
