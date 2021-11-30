e4ac3b237dcbfa3727870d8adaf252b7704c9f70
Bin Bao <binbao@fb.com>
2021-11-30 07:07:34 -0800

    [LT] Sync LTC branch changes on torch/csrc/lazy/core (#69012)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69012
    
    Some changes to torch/csrc/lazy/core were done on the
    lazy_tensor_staging branch (https://github.com/pytorch/pytorch/pull/68427).
    Merge those back into the trunk.
    
    Test Plan: Imported from OSS
    
    Reviewed By: wconstab
    
    Differential Revision: D32708696
    
    Pulled By: desertfire
    
    fbshipit-source-id: e54b978f2bdb9c7db27880f60246fdf1e8b41019

--- torch/csrc/lazy/backend/backend_interface.h
+++ torch/csrc/lazy/backend/backend_interface.h
@@ -119,9 +119,9 @@ class TORCH_API BackendRegistrar {
   BackendRegistrar(const BackendImplInterface* backend_impl_interface);
 };
 
-inline const BackendImplInterface* TORCH_API getBackend() {
+static inline const BackendImplInterface* getBackend() {
   auto p = backend_impl_registry.load();
-  CHECK(p) << "Lazy tensor backend not registered.";
+  TORCH_CHECK(p, "Lazy tensor backend not registered.");
   return p;
 }
 
