8f8a2682b2253f19535046e10211eb37de226e3f
Bella Laybourn <ilaybour@andrew.cmu.edu>
2021-12-01 09:20:26 -0500

    remove optional from parameter

--- torch/nn/modules/container.py
+++ torch/nn/modules/container.py
@@ -621,7 +621,7 @@ class ParameterDict(Module):
         del self[key]
         return v
 
-    def get(self, key: str, default: Optional['Parameter'] = None) -> 'Parameter':
+    def get(self, key: str, default: 'Parameter' = None) -> 'Parameter':
         r"""Return the parameter associated with key if present.
             Otherwise return default if provided, None if not.
 
