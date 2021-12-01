7c5f0f7ddeaecce3a176c225e042dc7cd36790d9
Yi Zhang <zhanyi@microsoft.com>
2021-12-01 18:24:03 +0800

    Update install_sccache.bat

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -6,13 +6,8 @@ if "%REBUILD%"=="" (
     taskkill /im sccache.exe /f /t || ver > nul
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
     del %TMP_DIR_WIN%\bin\sccache-cl.exe || ver > nul
-    if "%BUILD_ENVIRONMENT%"=="" (
-      curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20211130.exe --output %TMP_DIR_WIN%\bin\sccache.exe
-      curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %TMP_DIR_WIN%\bin\sccache-cl.exe
-    ) else (
-      aws s3 cp s3://ossci-windows/sccache-20211130.exe %TMP_DIR_WIN%\bin\sccache.exe
-      aws s3 cp s3://ossci-windows/sccache-cl.exe %TMP_DIR_WIN%\bin\sccache-cl.exe
-    )
+    curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20211130.exe --output %TMP_DIR_WIN%\bin\sccache.exe
+    curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %TMP_DIR_WIN%\bin\sccache-cl.exe
     goto :check_sccache
   )
 )
