bf156c9611245b37b780b373f51559f009b3edef
Jiewen Tan <jwtan@fb.com>
2021-12-01 14:49:01 -0800

    [LTC] Move BackendRegistrar to backend_interface.cpp
    Summary:
    This commit moves BackendRegistrar to backend_interface.cpp such that
    it can write to the current backend_impl_registry.
    
    Test Plan:
    ./lazy_tensor_core/test/cpp/build/test_ptltc

--- torch/csrc/lazy/backend/backend_interface.cpp
+++ torch/csrc/lazy/backend/backend_interface.cpp
@@ -7,6 +7,11 @@ namespace {
 std::atomic<const BackendImplInterface*> backend_impl_registry;
 } // namespace
 
+BackendRegistrar::BackendRegistrar(
+    const BackendImplInterface* backend_impl_interface) {
+  backend_impl_registry.store(backend_impl_interface);
+}
+
 const BackendImplInterface* getBackend() {
   auto* interface = backend_impl_registry.load();
   TORCH_CHECK(interface, "Lazy tensor backend not registered.");
