8a6d67723110c9eae9a420464dda212e685d8187
Andrey Talman <atalman@fb.com>
2021-12-02 13:02:42 -0800

    Enabling CUDA 11.5 for binary builds, Adding windows workflows for CUDA 11.5 (#69262)
    Summary:
    Fixes https://github.com/pytorch/pytorch/issues/68259
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69262
    
    Reviewed By: malfet
    
    Differential Revision: D32804850
    
    Pulled By: atalman
    
    fbshipit-source-id: abac45ad1d49ec7e0e7df6cb9a22a46fbcd905a2

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -294,6 +294,19 @@ WINDOWS_WORKFLOWS = [
     ),
     CIWorkflow(
         arch="windows",
+        build_environment="win-vs2019-cuda11.5-py3",
+        cuda_version="11.5",
+        test_runner_type=WINDOWS_CUDA_TEST_RUNNER,
+        num_test_shards=2,
+        only_run_smoke_tests_on_pull_request=True,
+        enable_force_on_cpu_test=1,
+        ciflow_config=CIFlowConfig(
+            run_on_canary=True,
+            labels={LABEL_CIFLOW_DEFAULT, LABEL_CIFLOW_CUDA, LABEL_CIFLOW_WIN}
+        ),
+    ),
+    CIWorkflow(
+        arch="windows",
         build_environment="periodic-win-vs2019-cuda11.1-py3",
         cuda_version="11.1",
         test_runner_type=WINDOWS_CUDA_TEST_RUNNER,
