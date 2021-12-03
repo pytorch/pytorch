f66f6597bcea3357a2c8292e7301240ab331b207
Raghavan Raman <raghavanr@fb.com>
2021-12-02 22:57:53 -0800

    [jit] Add support for dynamic shape fusion in JIT.
    Summary:
    This diff adds support for dynamic shape fusion in JIT. This is done
    by performing fusion with the static shapes observed on the first run,
    generalizing the fused subgraphs and generating code for the generalized fused
    subgraphs with dynamic shapes.
    
    Test Plan: buck test mode/dev-nosan //caffe2/test/cpp/jit:jit
    
    Differential Revision: D32781307
    
    fbshipit-source-id: 6fce84b47557cbaae5ae52e960973b945548e1cc

--- torch/csrc/jit/passes/tensorexpr_fuser.h
+++ torch/csrc/jit/passes/tensorexpr_fuser.h
@@ -22,6 +22,8 @@ TORCH_API void FuseTensorExprs(
 
 TORCH_API void setTensorExprFuserEnabled(bool val);
 TORCH_API bool tensorExprFuserEnabled();
+TORCH_API void setTensorExprDynamicShapeFusionEnabled(bool val);
+TORCH_API bool tensorExprDynamicShapeFusionEnabled();
 TORCH_API bool setTexprReductionsEnabled(bool value);
 TORCH_API bool texprReductionsEnabled();
 
