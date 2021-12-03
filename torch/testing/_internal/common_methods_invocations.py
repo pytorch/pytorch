fix for python 3.10 for gradient opinfo (#68113)
Summary:
This PR fixes https://github.com/pytorch/pytorch/issues/67612 by creating a tensor first and then converting the dtype explicitly using `.to(dtype)` call.

Looking forward to your feedback and suggestions on this.

cc: kshitij12345 mruberry

Pull Request resolved: https://github.com/pytorch/pytorch/pull/68113

Reviewed By: zou3519

Differential Revision: D32797329

Pulled By: saketh-are

fbshipit-source-id: 5c34709ab277c82cda316a3ea1cf01e853e4c38b

torch/testing/_internal/common_methods_invocations.py
@@ -2983,8 +2983,11 @@ def sample_inputs_gradient(op_info, device, dtype, requires_grad):
         t = make_tensor(size, device, dtype, low=None, high=None, requires_grad=requires_grad)
         coordinates_tensor_list = []
         for coords in coordinates:
-            a = torch.tensor(coords, dtype=dtype, device=device)
-            coordinates_tensor_list.append(a)
+            # `coords` will always contain floating point values and Python 3.10 does not support this
+            # implicit conversion to an integer using `__int__`
+            # TODO: this can be simplified after https://github.com/pytorch/pytorch/issues/69316 is fixed
+            a = torch.tensor(coords, device=device)
+            coordinates_tensor_list.append(a.to(dtype))
         sample_inputs.append(SampleInput(t, kwargs=dict(dim=dim, spacing=coordinates_tensor_list, edge_order=edge_order)))
 
     return tuple(sample_inputs)