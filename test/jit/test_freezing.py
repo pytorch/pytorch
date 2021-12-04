d3e4e45fc7e40e02b0bb18079c6d079ad53bcd6c
David Berard <dberard@fb.com>
2021-12-03 16:16:29 -0800

    Update on "[JIT] optimize_for_inference on methods other than forward"
    Differential Revision: [D32835529](https://our.internmc.facebook.com/intern/diff/D32835529)
    
    [ghstack-poisoned]

--- test/jit/test_freezing.py
+++ test/jit/test_freezing.py
@@ -2191,8 +2191,6 @@ class TestFrozenOptimizations(JitTestCase):
         mod_eager = Net(3, 6, kernel_size=3, stride=2).eval().cuda()
 
         inps = [5, 3, 4, 4]
-        if conv == nn.Conv3d:
-            inps.append(inps[-1])
         inp = torch.rand(inps).cuda()
 
         scripted_mod = torch.jit.script(mod_eager)
