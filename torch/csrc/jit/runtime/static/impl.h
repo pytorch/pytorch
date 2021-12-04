1082207f3422562434345bac6207192ad219453c
Don Jang <djang@fb.com>
2021-12-04 14:46:49 -0800

    [Static Runtime] Move implementation details from impl.h into internal.h (#69274)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69274
    
    `impl.h` is the main header file that defines the interface of Static Runtime to its clients.
    
    However, it is currently filled with implementation details that should not be leaked to our clients. 1) this can unnecessarily leak our internals to our clients which can make it hard to change them later 2) cause unnecessary merge conflicts when multiple people are touching this enormous impl.cpp file.
    
    To alleviate the situation, this change moves the implementation details from impl.h into a new file, internal.h, that's internally kept without leaking the details to our clients.
    
    This change will be followed by another change to rename `impl.h` into `runtime.h` or anything better since `impl.h` is currently not about implementation but SR's interface.
    
    Note that this change is NOT complete since the remaining declarations in impl.h still contain a lot of implementation details. Therefore, we should keep working on minimizing the interface to prevent our API from being bloated unnecessarily. Also we need to work on modularizing our implementations into separate pieces organized by separate files in the near future.
    
    Test Plan: Existing unittests
    
    Reviewed By: donaldong
    
    Differential Revision: D32780415
    
    fbshipit-source-id: 119b7aedbf563b195641c5674572a9348732145f

--- torch/csrc/jit/runtime/static/impl.h
+++ torch/csrc/jit/runtime/static/impl.h
@@ -36,112 +36,6 @@ using FastSet = std::unordered_set<Key>;
 TORCH_API bool canEnableStaticRuntime(
     const std::shared_ptr<torch::jit::Graph>& graph);
 
-TORCH_API std::string dumpValueSet(
-    const FastSet<const Value*>& value_set,
-    const char* set_name = "");
-
-TORCH_API inline bool doesNotHeapAllocateWhenStoredInIValue(const Type& type) {
-  switch (type.kind()) {
-    // NOTE: NumberType may allocate because it includes complex.
-    case TypeKind::NoneType:
-    case TypeKind::IntType:
-    case TypeKind::FloatType:
-    case TypeKind::BoolType:
-    case TypeKind::DeviceObjType:
-    case TypeKind::StreamObjType:
-      return true;
-    default:
-      return false;
-  }
-}
-
-// Group values used by `graph` into three categories:
-//
-// - output_aliases:
-//     values that are either outputs or contain aliases of outputs
-// - external_aliases:
-//     values that are inputs, constants, or their aliases.
-//     The output aliases that end up here are as a result of aliasDb failing to
-//     recognize them as outputs due to collection object (e.g., Tuple) aliasing
-//     inputs.
-// Values that dont't show up in output_aliases or external_aliases are created
-// and consumed within the graph.
-class ValueGroup {
- public:
-  explicit ValueGroup() = default;
-  void init(const std::shared_ptr<torch::jit::Graph>& graph, AliasDb& db);
-
-  bool isExternalAlias(const Value* value) const {
-    return external_aliases_.find(value) != external_aliases_.end();
-  }
-
-  bool isOutputAlias(const Value* value) const {
-    return output_aliases_.find(value) != output_aliases_.end();
-  }
-
-  bool isAlwaysAlive(const Value* value) const {
-    return isExternalAlias(value) || isOutputAlias(value);
-  }
-
-  std::string toString() const {
-    return c10::str(
-        dumpValueSet(output_aliases_, "ValueGroup::output_aliases_"),
-        "\n",
-        dumpValueSet(external_aliases_, "ValueGroup::external_aliases_"));
-  }
-
- private:
-  FastSet<const Value*> output_aliases_;
-  FastSet<const Value*> external_aliases_;
-};
-
-class TORCH_API ManagedTensorRanges {
- public:
-  ManagedTensorRanges() = default;
-  ManagedTensorRanges(
-      const std::shared_ptr<Graph>& graph,
-      const FastSet<const Value*>& managed_tensor_values);
-
-  // If true, then this node is the last use of at least one
-  // managed tensor. availableTensorsAfterNode(node) will return a vector
-  // of the managed tensors that are available for re-use
-  // in the nodes following this one.
-  bool nodeFreesManagedTensors(Node* node) const;
-  const std::vector<const Value*>& availableTensorsAfterNode(Node* node) const;
-
-  // True if the value has a tracked lifetime and lifetime.start ==
-  // lifetime.end. "Unused" does not imply "unmanaged" -
-  // managed tensors can be unused if they're not passed to any ops!
-  bool isUnusedValue(const Value* value) const;
-
-  // For testing. True if v1 and v2 are both mutable types and have lifetimes
-  // that overlap.
-  bool lifetimesOverlap(const Value* v1, const Value* v2) const;
-
- private:
-  struct Lifetime {
-    Lifetime(size_t start_, size_t end_) : start(start_), end(end_) {}
-    size_t start;
-    size_t end;
-  };
-
-  // Returns nullptr if we are not tracking the lifetime of value
-  Lifetime* getLifetime(const Value* value);
-  const Lifetime* getLifetime(const Value* value) const;
-  // Collect all values in the input that have tracked lifetimes.
-  // A value's lifetime may not be tracked if it is a graph input
-  // or immutable type (containers with at least one mutable
-  // type are mutable)
-  std::vector<const Value*> collectValuesWithTrackedLifetimes(
-      at::ArrayRef<const Value*> values);
-
-  // Maps Node* to the set of managed tensors that are now available
-  // for re-use after this node.
-  FastMap<Node*, std::vector<const Value*>> node_to_newly_free_tensors_{};
-  // Maps each Value* to its lifetime (start node index, end node index)
-  FastMap<const Value*, Lifetime> value_lifetimes_{};
-};
-
 struct TORCH_API StaticModuleOptions {
   // to batch allocate (deallocate) tensor storage for all non-escaping
   // temporary tensors
@@ -206,6 +100,7 @@ class MemoryPlanner;
 class ProcessedFunction;
 class ProcessedNode;
 class StaticRuntime;
+class ValueGroup;
 class TORCH_API StaticModule {
  public:
   explicit StaticModule(
@@ -216,6 +111,11 @@ class TORCH_API StaticModule {
       const torch::jit::Module& m,
       bool is_frozen = false,
       const StaticModuleOptions& opts = StaticModuleOptions());
+
+  StaticModule(const torch::jit::StaticModule&) = delete;
+  StaticModule(torch::jit::StaticModule&&) noexcept;
+  StaticModule& operator=(StaticModule&&) = delete;
+  ~StaticModule();
 
   typedef enum {
     CONSTANT_VALUE = -2, // VALUE nodes defined by prim::Constant
@@ -255,7 +155,7 @@ class TORCH_API StaticModule {
   const StaticModuleOptions& opts() const;
 
   const ValueGroup& valueGroup() const {
-    return value_group_;
+    return *value_group_;
   }
 
   size_t num_inputs() const;
@@ -274,14 +174,10 @@ class TORCH_API StaticModule {
 
   // Our nodes don't have their inputs & outputs initialized; don't
   // let anybody but StaticRuntime and tests get them.
-  const std::vector<ProcessedNode>& nodes() const {
-    return nodes_;
-  }
+  const std::vector<ProcessedNode>& nodes() const;
 
  public:
-  auto num_nodes() const {
-    return nodes_.size();
-  }
+  size_t num_nodes() const;
 
   C10_NODISCARD Node* findNodeWithKindForTesting(const std::string& kind) const;
 
@@ -300,7 +196,7 @@ class TORCH_API StaticModule {
   }
 
   const ValueGroup& value_group() const {
-    return value_group_;
+    return *value_group_;
   }
 
   const FastSet<const Value*>& managed_tensor_values() const {
@@ -336,13 +232,13 @@ class TORCH_API StaticModule {
   // IValue table (defined by prim::Constant nodes)
   std::vector<IValue> constants_;
   // The functions to be called by corresponding ProcessedNode.
-  std::vector<ProcessedFunction> functions_{};
+  std::vector<ProcessedFunction> functions_;
   // The nodes we need to run
   std::vector<ProcessedNode> nodes_;
   // Indices of graph outputs in the single values array.
   std::vector<uint16_t> output_indices_;
 
-  ValueGroup value_group_;
+  std::unique_ptr<ValueGroup> value_group_;
 
   // map a value to the set of values that may share the same storage with it
   FastMap<const Value*, std::vector<const Value*>>
@@ -420,13 +316,9 @@ class TORCH_API StaticRuntime {
     return outputs_;
   }
 
-  const std::vector<ProcessedNode>& nodes() const {
-    return nodes_;
-  }
+  const std::vector<ProcessedNode>& nodes() const;
 
-  std::vector<ProcessedNode>& nodes() {
-    return nodes_;
-  }
+  std::vector<ProcessedNode>& nodes();
 
   const Graph& graph() const {
     return static_module_.graph();
@@ -507,152 +399,6 @@ class TORCH_API StaticRuntime {
   std::vector<IValue> values_;
   std::vector<IValue*> outputs_;
   std::vector<ProcessedNode> nodes_;
-};
-
-class TORCH_API ProcessedFunction {
- public:
-  ProcessedFunction(
-      Node* node,
-      bool enable_out_variant,
-      bool check_memory_overlap);
-
-  enum class Kind : uint8_t {
-    kOutVariant,
-    kNativeFunction,
-    kInterpreterFallback,
-  };
-
-  const std::function<void(ProcessedNode*)>& f() const {
-    return f_;
-  }
-
-  Kind kind() const {
-    return kind_;
-  }
-
-  bool checkMemoryOverlap() const {
-    return check_memory_overlap_;
-  }
-
- private:
-  std::function<void(ProcessedNode*)> f_;
-  Kind kind_{ProcessedFunction::Kind::kOutVariant};
-  bool check_memory_overlap_{false};
-};
-
-// NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
-class TORCH_API ProcessedNode {
- public:
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
-  ProcessedNode() = default;
-  // ProcessedNodes are created within StaticModule and then
-  // associated with a shared values array using set_values() when
-  // they are copied into a StaticRuntime.
-  ProcessedNode(
-      Node* n,
-      ProcessedFunction* fn,
-      ProcessedNodeInputs inputs,
-      uint16_t outputs_offset);
-
-  ProcessedNode(const ProcessedNode&) = default;
-  ProcessedNode& operator=(const ProcessedNode&) = default;
-
-  // These should be noexcept, but some Android build is failing
-  // saying the noexcept specification doesn't match the calculated
-  // one. Maybe c10::variant is throwing it off?
-  ProcessedNode(ProcessedNode&&) = default;
-  ProcessedNode& operator=(ProcessedNode&&) = default;
-
-  void run();
-
-  Node* node() const {
-    return node_;
-  }
-
-  // Input is readonly
-  C10_NODISCARD const IValue& Input(uint32_t i) const {
-    return values_[inputs_[i]];
-  }
-
-  // Output is readwrite
-  IValue& Output(uint32_t i) {
-    DCHECK(i < num_outputs_);
-    return values_[outputs_offset_ + i];
-  }
-
-  C10_NODISCARD const IValue& Output(uint32_t i) const {
-    DCHECK(i < num_outputs_);
-    return values_[outputs_offset_ + i];
-  }
-
-  C10_NODISCARD c10::ArrayRef<const IValue> outputs() const {
-    return c10::ArrayRef<const IValue>(values_ + outputs_offset_, num_outputs_);
-  }
-
-  C10_NODISCARD auto num_outputs() const {
-    return num_outputs_;
-  }
-
-  C10_NODISCARD uint16_t num_inputs() const {
-    return inputs_.size();
-  }
-
-  std::vector<IValue> inputs_ivalue_vec() const;
-
-  bool has_out_variant() const {
-    return fn_->kind() == ProcessedFunction::Kind::kOutVariant;
-  }
-
-  bool has_native() const {
-    return fn_->kind() == ProcessedFunction::Kind::kNativeFunction;
-  }
-
-#ifndef PYTORCH_DISABLE_PER_OP_PROFILING
-  const char* get_op_name() const {
-    return op_name_;
-  }
-#endif
-
-  bool check_outputs_for_memory_overlap() const {
-    return fn_->checkMemoryOverlap();
-  }
-
-  void set_outputs_memory_overlap_detected() {
-    overlap_detected_ = true;
-  }
-
-  bool outputs_memory_overlap_detected() {
-    return overlap_detected_;
-  }
-
-  void verify_and_correct_memory_overlap();
-
-  void set_values(IValue* values) {
-    DCHECK(values_ == nullptr);
-    values_ = values;
-  }
-
-  C10_NODISCARD uint16_t output_ivalue_index(uint16_t i) const {
-    return outputs_offset_ + i;
-  }
-  // used in debug mode
-  bool verify_no_memory_overlap(bool force_check = false) const;
-
- private:
-  C10_NODISCARD bool verify_outputs_dont_overlap_each_other() const;
-
-  C10_NODISCARD bool verify_inputs_dont_overlap_outputs(bool force_check) const;
-
-  Node* node_;
-  const ProcessedFunction* fn_;
-  bool overlap_detected_{false};
-  ProcessedNodeInputs inputs_;
-  uint16_t outputs_offset_;
-  uint16_t num_outputs_;
-  IValue* values_ = nullptr; // unowned
-#ifndef PYTORCH_DISABLE_PER_OP_PROFILING
-  const char* op_name_;
-#endif
 };
 
 } // namespace jit
