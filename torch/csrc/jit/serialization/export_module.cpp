782658ada1629d9c0d53e05b7f49f2a2a1d416a4
Scott Wolchok <swolchok@fb.com>
2021-12-03 20:34:38 -0800

    [PyTorch] Make TypePrinter take const Type&
    TypePrinter does not need to take ownership of the Type.
    
    This helps unblock the following diff to stop refcounting Type singletons.
    
    Differential Revision: [D32858525](https://our.internmc.facebook.com/intern/diff/D32858525/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D32858525/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/serialization/export_module.cpp
+++ torch/csrc/jit/serialization/export_module.cpp
@@ -188,9 +188,8 @@ std::pair<IValue, IValue> getFunctionTuple(
 
   // schema
   const auto& schema = func.getSchema();
-  auto type_printer =
-      [&](const c10::ConstTypePtr& t) -> c10::optional<std::string> {
-    auto namedType = t->cast<c10::NamedType>();
+  auto type_printer = [&](const c10::Type& t) -> c10::optional<std::string> {
+    auto namedType = t.cast<c10::NamedType>();
     if (namedType && namedType->name()) {
       return type_name_uniquer_.getUniqueName(namedType).qualifiedName();
     }
@@ -709,9 +708,8 @@ void ScriptModuleSerializer::convertNamedType(
   std::string qualifier = qualname.prefix();
   PythonPrint* pp = file_streams_.find(qualifier);
 
-  auto type_printer =
-      [&](const c10::ConstTypePtr& t) -> c10::optional<std::string> {
-    auto namedType = t->cast<c10::NamedType>();
+  auto type_printer = [&](const c10::Type& t) -> c10::optional<std::string> {
+    auto namedType = t.cast<c10::NamedType>();
     if (namedType && namedType->name()) {
       return type_name_uniquer_.getUniqueName(namedType).qualifiedName();
     }
