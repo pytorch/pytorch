a97d82c17b58696d21c99267b3021734b7f780c8
Jerry Zhang <jerryzh@fb.com>
2021-12-06 22:54:48 -0800

    [quant][graphmode][fx] Add default_replay_qconfig for ops like reshape (#69249)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69249
    
    This PR added default_replay_qconfig and default_replay_observer which is used
    when we want to configure an operator to reuse the observer from input, if the input
    Tensor for the operator is not observed, we will not observe the output of this operator either,
    if the input Tensor is observed, we will observe the output of the operator with the same observer.
    
    e.g.
    
    ```
    x1 = x0.reshape()
    ```
    if reshape is configured with default_replay_qconfig:
    1. if x0 is observed with observer_0, we'll observe x1 with the same observer instance
    2. if x0 is not observed, we won't observe x1 either
    
    Test Plan:
    ```
    python test/test_quantization.py TestQuantizeFx.test_replay_qconfig
    ```
    
    Imported from OSS
    
    Reviewed By: vkuzo
    
    Differential Revision: D32774723
    
    fbshipit-source-id: 26862b2bc181d0433e2243daeb3b8f7ec3dd33b2

--- test/quantization/fx/test_quantize_fx.py
+++ test/quantization/fx/test_quantize_fx.py
@@ -46,6 +46,7 @@ from torch.ao.quantization import (
     float_qparams_weight_only_qconfig,
     get_default_qconfig,
     get_default_qat_qconfig,
+    get_default_qconfig_dict,
     fuse_modules,
     prepare,
     prepare_qat,
@@ -3348,6 +3349,54 @@ class TestQuantizeFx(QuantizationTestCase):
         self.assertTrue(get_default_output_activation_post_process_map(is_training=False) is
                         DEFAULT_OUTPUT_OBSERVER_MAP)
 
+    def test_reuse_input_qconfig(self):
+        class M1(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.conv = torch.nn.Conv2d(3, 3, 3)
+
+            def forward(self, x):
+                x = self.conv(x)
+                x = x.reshape()
+                return x
+
+        class M2(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x):
+                x = x.reshape()
+                return x
+
+        options = itertools.product([M1, M2], [True, False])
+        for M, is_qat in options:
+            m = M1().eval()
+            m = prepare_fx(m, get_default_qconfig_dict())
+            m = convert_fx(m)
+            node_list = [
+                ns.call_function(torch.quantize_per_tensor),
+                ns.call_module(nnq.Conv2d),
+                ns.call_method("reshape"),
+                ns.call_method("dequantize"),
+            ]
+            self.checkGraphModuleNodes(
+                m,
+                expected_node_list=node_list)
+
+            m = M2().eval()
+            m = prepare_fx(m, get_default_qconfig_dict())
+            m = convert_fx(m)
+            node_occurrence = {
+                ns.call_function(torch.quantize_per_tensor): 0,
+                ns.call_method("dequnatize"): 0,
+            }
+            node_list = [
+                ns.call_method("reshape"),
+            ]
+            self.checkGraphModuleNodes(
+                m,
+                expected_node_occurrence=node_occurrence,
+                expected_node_list=node_list)
 
 @skipIfNoFBGEMM
 class TestQuantizeFxOps(QuantizationTestCase):
