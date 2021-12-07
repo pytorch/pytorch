18247b8c2b77c93e9fb2e79f17b2bbe46ec00ae8
BowenBao <bowbao@microsoft.com>
2021-12-06 20:08:14 -0800

    Allow caffe2-specific graph transformations for OperatorExportTypes.ONNX_ATEN_FALLBACK when BUILD_CAFFE2 is ON (#67460)
    The use of ATEN as a fallback operator during ONNX conversion is important for increasing operator coverage or even provide more efficient implementations over some ONNX ops.
    
    Currently this feature is available through `OperatorExportTypes.ONNX_ATEN_FALLBACK`,
    but it also performs changes to the graph that are runnable by Caffe2, only.
    
    This PR introduces restricts caffe2-specific graph transformations for `ONNX_ATEN_FALLBACK`
    operator export type for when pytorch is built with caffe2 support (aka BUILD_CAFFE2=1 during build)
    
    The first version of this PR introduced a new operator export type `ONNX_ATEN__STRICT_FALLBACK`,
    which essentially is the same as `ONNX_ATEN_FALLBACK` but without caffe2 transformations.
    It was preferred to not introduce a new operator export type, but to refine the existing aten fallback one
    
    ## BC-breaking note
    ### The global constant `torch.onnx.PYTORCH_ONNX_CAFFE2_BUNDLE` is removed in favor of
    a less visible `torch.onnx._CAFFE2_ATEN_FALLBACK`.
    `PYTORCH_ONNX_CAFFE2_BUNDLE` is really a dead code flag always set to False.
    One alternative would be fixing it, but #66658 disables Caffe2 build by default.
    Making a Caffe2 feature a private one seems to make more sense for future deprecation.
    
    ### The method `torch.onnx.export` now defaults to ONNX when `operator_export_type` is not specified.
    Previously `torch.onnx.export's operator_export_type` intended to default to `ONNX_ATEN_FALLBACK` when `PYTORCH_ONNX_CAFFE2_BUNDLE` was set, but it would never happen as `PYTORCH_ONNX_CAFFE2_BUNDLE` is always undefined
    
    ghstack-source-id: 8ae8ac0cf67bbc6b6d50f0d0632436b169f627fc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68490

--- torch/onnx/symbolic_opset11.py
+++ torch/onnx/symbolic_opset11.py
@@ -550,7 +550,10 @@ def arange(g, *args):
 def _dim_arange(g, like, dim):
     like_shape = g.op("Shape", like)
     stop = g.op("Gather", like_shape, g.op("Constant", value_t=torch.tensor(dim)), axis_i=0)
-    if sym_help._operator_export_type == torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK:
+    # Caffe2-specific op
+    is_caffe2_aten_fallback = (sym_help._operator_export_type == torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK and
+                               torch.onnx._CAFFE2_ATEN_FALLBACK)
+    if is_caffe2_aten_fallback:
         return g.op("_caffe2::Range", stop)
     return arange(g, stop, 4, None, None, None)
 
