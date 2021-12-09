0d17797615c73e813dbbcfe53f1567fd106a3672
Bin Bao <binbao@fb.com>
2021-12-09 21:06:23 +0000

    [LTC] Upstream several internal ops
    Summary: To prepare for the landing of LazyTensor and LazyGraphExecutor,
    - arithmetic_ir_ops.h
    - cast.h
    - device_data.h
    - expand.h
    - generic.h
    - scalar.h
    
    [ghstack-poisoned]

---
+++ torch/csrc/lazy/core/internal_ops/generic.cpp
@@ -0,0 +1,42 @@
+#include <torch/csrc/lazy/core/internal_ops/generic.h>
+
+namespace torch {
+namespace lazy {
+
+Generic::Generic(
+    OpKind op,
+    OpList operands,
+    Shape shape,
+    size_t num_outputs,
+    hash_t hash_seed)
+    : TsNode(
+          std::move(op),
+          operands,
+          {std::move(shape)},
+          num_outputs,
+          hash_seed),
+      hash_seed_(hash_seed) {}
+
+Generic::Generic(
+    OpKind op,
+    OpList operands,
+    const std::function<Shape()>& shape_fn,
+    size_t num_outputs,
+    hash_t hash_seed)
+    : TsNode(std::move(op), operands, shape_fn, num_outputs, hash_seed),
+      hash_seed_(hash_seed) {}
+
+Generic::Generic(
+    OpKind op,
+    OpList operands,
+    size_t num_outputs,
+    hash_t hash_seed)
+    : TsNode(std::move(op), operands, num_outputs, hash_seed),
+      hash_seed_(hash_seed) {}
+
+Generic::Generic(OpKind op, Shape shape, size_t num_outputs, hash_t hash_seed)
+    : TsNode(std::move(op), std::move(shape), num_outputs, hash_seed),
+      hash_seed_(hash_seed) {}
+
+} // namespace lazy
+} // namespace torch
