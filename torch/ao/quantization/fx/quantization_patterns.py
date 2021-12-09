5c0f478972bf46470bacd1d4e53bcc876e0f2226
Terry Chen <terrychen@fb.com>
2021-12-09 11:15:29 -0800

    remove func with params

--- torch/ao/quantization/fx/quantization_patterns.py
+++ torch/ao/quantization/fx/quantization_patterns.py
@@ -1488,27 +1488,14 @@ class CopyNodeQuantizeHandler(QuantizeHandler):
                 convert_custom_config_dict: Dict[str, Any] = None) -> Node:
         # always produce reference pattern for relu
         func_list = [
-            torch.adaptive_avg_pool1d,
-            torch.nn.functional.adaptive_avg_pool2d,
-            torch.nn.functional.adaptive_avg_pool3d,
-            torch.nn.functional.dropout,
             torch.nn.functional.hardtanh,
             torch.nn.functional.hardtanh_,
-            torch.nn.functional.interpolate,
-            torch.nn.functional.max_pool1d,
-            torch.nn.functional.max_pool2d,
-            torch.nn.functional.max_pool3d,
             torch.nn.functional.relu,
             torch.nn.functional.relu6,
-            torch.avg_pool1d,
-            torch._C._nn.avg_pool2d,
-            torch._C._nn.avg_pool3d,
-            torch.clamp,
             torch.flatten,
             torch.max,
             torch.mean,
             torch.min,
-            operator.floordiv,
         ]
         is_relu = node.op == "call_function" and node.target in func_list
         if is_reference or is_relu:
