7ec7a37f9dda7ccaf248bc9e50beb7670e21bef6
Will Constable <whc@fb.com>
2021-12-10 11:52:36 -0800

    Ensure different output filenames for test/train results (#69784)
    * Ensure different output filenames for test/train results
    
    * Allow specifying output_dir

--- lazy_tensor_core/lazy_bench.py
+++ lazy_tensor_core/lazy_bench.py
@@ -339,7 +339,7 @@ def lazy_overhead_experiment(args, results, benchmark, lazy_benchmark):
     overhead = median[1] / median[0]
     results.append(overhead)
     output_csv(
-        "lazy_overheads.csv",
+        os.path.join(args.output_dir, f"lazy_overheads_{args.test}.csv"),
         ("dev", "name", "overhead", "pvalue"),
     ).writerow([current_device, current_name, f"{overhead:.4f}", f"{pvalue:.4e}"])
     print(f"{short_name(name, limit=30):<30} {current_device:<4} {args.test:<5} {'trace overheads':<20} overhead: {overhead:.3f} pvalue: {pvalue:.2e}")
@@ -379,7 +379,7 @@ def lazy_compute_experiment(args, experiment, results, benchmark, lazy_benchmark
     speedup = median[0] / median[1]
     results.append(speedup)
     output_csv(
-        "lazy_compute.csv",
+        os.path.join(args.output_dir, f"lazy_compute_{args.test}.csv"),
         ("name", "dev", "experiment", "speedup", "pvalue"),
     ).writerow([current_name, current_device, experiment, f"{speedup:.4f}", f"{pvalue:.2e}"])
     print(f"{short_name(current_name, limit=30):<30} {current_device:<4} {args.test:<5} {experiment:<20} speedup:  {speedup:.3f} pvalue: {pvalue:.2e}")
@@ -431,6 +431,7 @@ if __name__ == "__main__" :
     parser.add_argument("--fuser", type=str, choices=['noopt', 'fuser0', 'fuser1', 'fuser2'], help="0=legacy, 1=nnc, 2=nvfuser")
     parser.add_argument("--test", type=str, choices=['eval', 'train'], default='eval')
     parser.add_argument("--torchbench_dir", type=str, help="path to torchbenchmark repo")
+    parser.add_argument("--output_dir", type=str, default=".", help="path to write output files")
     parser.add_argument("--dump_lazy_counters", action='store_true', help="dump lazy counter values after each timing run")
     parser.add_argument("--run_tracing_execute_noops", action='store_true', help="Run the tracing portion only, with noop backend, useful for running under a profiler.")
     args = parser.parse_args()
