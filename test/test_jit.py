8ab4a95021c322fdb0c6ae835487059e20b5ce8f
John Clow <jclow@fb.com>
2021-12-10 11:36:51 -0800

    Update base for Update on "small change to the implementation of Optional == to make it more readable."
    [ghstack-poisoned]

--- test/test_jit.py
+++ test/test_jit.py
@@ -69,7 +69,6 @@ from jit.test_union import TestUnion  # noqa: F401
 from jit.test_models import MnistNet
 from jit.test_batch_mm import TestBatchMM  # noqa: F401
 from jit.test_dtype_analysis import TestDtypeAnalysis, TestDtypeCustomRulesCPU  # noqa: F401
-from jit.test_device_analysis import TestDeviceAnalysis  # noqa: F401
 from jit.test_dce import TestDCE  # noqa: F401
 
 # Torch
