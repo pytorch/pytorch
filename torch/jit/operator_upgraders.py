605e716a324b7451020ce7cfeef43985eac83f25
Tugsbayasgalan (Tugsuu) Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-11 13:42:29 -0800

    Populate runtime with upgrader graph (#68773)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/68773
    
    Test Plan: Imported from OSS
    
    Reviewed By: qihqi, gmagogsfm
    
    Differential Revision: D32603258
    
    Pulled By: tugsbayasgalan
    
    fbshipit-source-id: 6fa0b7ee4ebe46c9aa148923c6ef3e1de106ad13

--- torch/jit/operator_upgraders.py
+++ torch/jit/operator_upgraders.py
@@ -12,53 +12,58 @@ import torch
 import yaml
 from typing import List, no_type_check, Optional, Union
 
-@torch.jit.script
-def div_Tensor_0_3(self: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
-    if (self.is_floating_point() or other.is_floating_point()):
-        return self.true_divide(other)
-    return self.divide(other, rounding_mode='trunc')
+# TODO (tugsuu) This context manager
+# forbids the tests to override torch.jit.script
+# Without it, torch.jit.load will fail due to
+# circular dependency
+with torch._jit_internal._disable_emit_hooks():
+    @torch.jit.script
+    def div_Tensor_0_3(self: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
+        if (self.is_floating_point() or other.is_floating_point()):
+            return self.true_divide(other)
+        return self.divide(other, rounding_mode='trunc')
 
-@torch.jit.script
-def div_Scalar_0_3(self: torch.Tensor, other: Union[int, float]) -> torch.Tensor:
-    if (self.is_floating_point() or isinstance(other, float)):
-        return self.true_divide(other)
-    return self.divide(other, rounding_mode='trunc')
+    @torch.jit.script
+    def div_Scalar_0_3(self: torch.Tensor, other: Union[int, float]) -> torch.Tensor:
+        if (self.is_floating_point() or isinstance(other, float)):
+            return self.true_divide(other)
+        return self.divide(other, rounding_mode='trunc')
 
-@torch.jit.script
-def div_out_0_3(self: torch.Tensor, other: torch.Tensor, *, out: torch.Tensor) -> torch.Tensor:
-    if (self.is_floating_point() or other.is_floating_point() or out.is_floating_point()):
-        return self.true_divide(other, out=out)
-    return self.divide(other, rounding_mode='trunc', out=out)  # type: ignore[call-overload]
+    @torch.jit.script
+    def div_out_0_3(self: torch.Tensor, other: torch.Tensor, *, out: torch.Tensor) -> torch.Tensor:
+        if (self.is_floating_point() or other.is_floating_point() or out.is_floating_point()):
+            return self.true_divide(other, out=out)
+        return self.divide(other, rounding_mode='trunc', out=out)  # type: ignore[call-overload]
 
-@torch.jit.script
-def div__Tensor_0_3(self: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
-    if (self.is_floating_point() or other.is_floating_point()):
-        return self.true_divide_(other)
-    return self.divide_(other, rounding_mode='trunc')
+    @torch.jit.script
+    def div__Tensor_0_3(self: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
+        if (self.is_floating_point() or other.is_floating_point()):
+            return self.true_divide_(other)
+        return self.divide_(other, rounding_mode='trunc')
 
-@torch.jit.script
-def div__Scalar_0_3(self: torch.Tensor, other: Union[int, float]) -> torch.Tensor:
-    if (self.is_floating_point() or isinstance(other, float)):
-        return self.true_divide_(other)
-    return self.divide_(other, rounding_mode='trunc')
+    @torch.jit.script
+    def div__Scalar_0_3(self: torch.Tensor, other: Union[int, float]) -> torch.Tensor:
+        if (self.is_floating_point() or isinstance(other, float)):
+            return self.true_divide_(other)
+        return self.divide_(other, rounding_mode='trunc')
 
-# TODO: since TS relies on typecheck comment of mypy
-# adding type: ignore at specific lines in this function
-# messes up our type refinement. For now, let's not check
-# type here.
-@no_type_check
-@torch.jit.script
-def full_names_0_4(size: List[int], fill_value: Union[int, float], *,
-                   dtype: Optional[int], layout: Optional[int], device: Optional[torch.device],
-                   pin_memory: Optional[bool]) -> torch.Tensor:
-    if dtype is None:
-        fill_value = float(fill_value)
-    return torch.full(size, fill_value, dtype=dtype, layout=layout,
-                      device=device, pin_memory=pin_memory)
+    # TODO: since TS relies on typecheck comment of mypy
+    # adding type: ignore at specific lines in this function
+    # messes up our type refinement. For now, let's not check
+    # type here.
+    @no_type_check
+    @torch.jit.script
+    def full_names_0_4(size: List[int], fill_value: Union[int, float], *,
+                       dtype: Optional[int], layout: Optional[int], device: Optional[torch.device],
+                       pin_memory: Optional[bool]) -> torch.Tensor:
+        if dtype is None:
+            fill_value = float(fill_value)
+        return torch.full(size, fill_value, dtype=dtype, layout=layout,
+                          device=device, pin_memory=pin_memory)
 
-@torch.jit.script
-def full_out_0_4(size: List[int], fill_value: Union[int, float], *, out: torch.Tensor) -> torch.Tensor:
-    return torch.full(size, fill_value, out=out)
+    @torch.jit.script
+    def full_out_0_4(size: List[int], fill_value: Union[int, float], *, out: torch.Tensor) -> torch.Tensor:
+        return torch.full(size, fill_value, out=out)
 
 def format_bytecode(table):
     # given a nested tuples, convert them to nested list
@@ -109,8 +114,15 @@ def generate_bytecode(file_name):
         entry = {upgrader_name: format_bytecode(upgrader_bytecode)}
         yaml_content.append(entry)
 
-    stream = open(file_name, 'w')
-    yaml.dump(yaml_content, stream)
+    with open(file_name, 'w') as stream:
+        yaml.dump(yaml_content, stream)
+
+def populate_upgraders_map():
+    upgrader_set = collect_available_upgraders()
+    content = {}
+    for upgrader_name in upgrader_set:
+        content[upgrader_name] = str(globals()[upgrader_name].graph)
+    torch._C._populate_upgraders_map(content)
 
 if __name__ == "__main__":
     raise RuntimeError("This file is not meant to be run directly")
