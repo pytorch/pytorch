fbfbc279ca0432e8dd43511ea639f20e15380a08
rohitgr7 <rohitgr1998@gmail.com>
2021-12-13 01:50:10 +0530

    rev

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -448,7 +448,7 @@ class KLDivLoss(_Loss):
         >>> # input should be a distribution in the log space
         >>> input = F.log_softmax(torch.randn(3, 5, requires_grad=True))
         >>> # Sample a batch of distributions. Usually this would come from the dataset
-        >>> target = torch.softmax(torch.rand(3, 5))
+        >>> target = F.softmax(torch.rand(3, 5))
         >>> output = kl_loss(input, target)
 
         >>> log_target = F.log_softmax(torch.rand(3, 5))
