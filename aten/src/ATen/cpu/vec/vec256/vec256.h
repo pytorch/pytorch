3fedf7f66d3bccd29c69008d0a3c5867739232f8
linuxone <quickwritereader@gmail.com>
2021-12-13 13:24:43 +0000

    formatting

--- aten/src/ATen/cpu/vec/vec256/vec256.h
+++ aten/src/ATen/cpu/vec/vec256/vec256.h
@@ -15,7 +15,7 @@
 #include <ATen/cpu/vec/vec256/vec256_qint.h>
 #include <ATen/cpu/vec/vec256/vec256_complex_float.h>
 #include <ATen/cpu/vec/vec256/vec256_complex_double.h>
-#elif defined(__VSX__)  || defined(CPU_CAPABILITY_VSX) 
+#elif defined(__VSX__)  || defined(CPU_CAPABILITY_VSX)
 #include <ATen/cpu/vec/vec256/vsx/vec256_common_vsx.h>
 #else
 #include <ATen/cpu/vec/vec256/zarch/vec256_zarch.h>
