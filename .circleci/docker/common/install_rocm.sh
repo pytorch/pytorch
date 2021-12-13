5e7a5b552ed4c47d747d4c5a446f2137f2272569
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2021-12-13 15:38:06 -0800

    [ROCm] Refactor how to specify AMD gpu targets using PYTORCH_ROCM_ARCH (#61706)
    Summary:
    Remove all hardcoded AMD gfx targets
    
    PyTorch build and Magma build will use rocm_agent_enumerator as
    backup if PYTORCH_ROCM_ARCH env var is not defined
    
    PyTorch extensions will use same gfx targets as the PyTorch build,
    unless PYTORCH_ROCM_ARCH env var is defined
    
    torch.cuda.get_arch_list() now works for ROCm builds
    
    PyTorch CI dockers will continue to be built for gfx900 and gfx906 for now.
    
    PYTORCH_ROCM_ARCH env var can be a space or semicolon separated list of gfx archs eg. "gfx900 gfx906" or "gfx900;gfx906"
    cc jeffdaily sunway513 jithunnair-amd ROCmSupport KyleCZH
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61706
    
    Reviewed By: seemethere
    
    Differential Revision: D32735862
    
    Pulled By: malfet
    
    fbshipit-source-id: 3170e445e738e3ce373203e1e4ae99c84e645d7d

--- .circleci/docker/common/install_rocm.sh
+++ .circleci/docker/common/install_rocm.sh
@@ -11,10 +11,18 @@ install_magma() {
     cp make.inc-examples/make.inc.hip-gcc-mkl make.inc
     echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
     echo 'LIB += -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib -Wl,--rpath,$(MKLROOT)/lib -Wl,--rpath,/opt/rocm/magma/lib' >> make.inc
-    echo 'DEVCCFLAGS += --amdgpu-target=gfx803 --amdgpu-target=gfx900 --amdgpu-target=gfx906 --amdgpu-target=gfx908 --gpu-max-threads-per-block=256' >> make.inc
+    echo 'DEVCCFLAGS += --gpu-max-threads-per-block=256' >> make.inc
+    export PATH="${PATH}:/opt/rocm/bin"
+    if [[ -n "$PYTORCH_ROCM_ARCH" ]]; then
+      amdgpu_targets=`echo $PYTORCH_ROCM_ARCH | sed 's/;/ /g'`
+    else
+      amdgpu_targets=`rocm_agent_enumerator | grep -v gfx000 | sort -u | xargs`
+    fi
+    for arch in $amdgpu_targets; do
+      echo "DEVCCFLAGS += --amdgpu-target=$arch" >> make.inc
+    done
     # hipcc with openmp flag may cause isnan() on __device__ not to be found; depending on context, compiler may attempt to match with host definition
     sed -i 's/^FOPENMP/#FOPENMP/g' make.inc
-    export PATH="${PATH}:/opt/rocm/bin"
     make -f make.gen.hipMAGMA -j $(nproc)
     LANG=C.UTF-8 make lib/libmagma.so -j $(nproc) MKLROOT=/opt/conda
     make testing/testing_dgemm -j $(nproc) MKLROOT=/opt/conda
