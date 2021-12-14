fd0207ad6a3a2821fd6a25f0bf1e3a691055384b
Tero Keski-Valkama <tero.keski-valkama@here.com>
2021-12-14 13:30:02 +0000

    #69894: setuptools LooseVersion missing from the latest setuptools, so migrated to using packaging.version.Version.

--- torch/utils/tensorboard/__init__.py
+++ torch/utils/tensorboard/__init__.py
@@ -1,12 +1,9 @@
 import tensorboard
-from setuptools import distutils
-
-LooseVersion = distutils.version.LooseVersion
+from packaging.version import Version as LooseVersion
 
 if not hasattr(tensorboard, '__version__') or LooseVersion(tensorboard.__version__) < LooseVersion('1.15'):
     raise ImportError('TensorBoard logging requires TensorBoard version 1.15 or above')
 
-del distutils
 del LooseVersion
 del tensorboard
 
