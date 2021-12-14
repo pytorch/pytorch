48da801ec648887be47c65d6a1066a3666a31b0d
Jiewen Tan <jwtan@fb.com>
2021-12-14 19:35:12 +0000

    Set maxsize=None

--- lazy_tensor_core/lazy_bench.py
+++ lazy_tensor_core/lazy_bench.py
@@ -56,7 +56,7 @@ SKIP_TRAIN_ONLY = {
 current_name = ""
 current_device = ""
 
-@functools.lru_cache
+@functools.lru_cache(maxsize=None)
 def output_csv(name, headers):
     output = csv.writer(
         io.TextIOWrapper(
