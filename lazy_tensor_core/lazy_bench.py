62c3f537de9d134541401cac05745fb5e0976823
Will Constable <whc@fb.com>
2021-12-14 10:37:30 -0800

    Add --just_run_once to lazy_bench

--- lazy_tensor_core/lazy_bench.py
+++ lazy_tensor_core/lazy_bench.py
@@ -443,10 +443,21 @@ def lazy_compute_experiment(args, experiment, results, benchmark, lazy_benchmark
         print(f"CIDEBUGOUTPUT,lazy_compute_experiment,{current_name},{current_device},{experiment},{args.test},{speedup:.4f},{pvalue:.2e},{args.warmup},{args.repeat},{warmup_time:.2f},{bench_time:.2f}")
     return (speedup, pvalue)
 
+def just_run_once(args, lazy_benchmark):
+    torch.manual_seed(1337)
+    if args.test == 'eval':
+        model, example_inputs = lazy_benchmark.get_module()
+        results.append(call_model_with(model, example_inputs))
+    elif args.test == 'train':
+        lazy_benchmark.train(niter=1)
+    ltm.mark_step()
+    ltm.wait_device_ops()
+    if current_device == 'cuda':
+        torch.cuda.synchronize()
 
 def check_results(correct_result, lazy_result, device):
     # to_device has recursive logic and special handling for
-    # extracting relevant tensors from huggingface data structures
+    # extracting relevant tensors from huggingface data structures:
     correct_result = to_device(correct_result, device)
     lazy_result = to_device(lazy_result, device)
 
@@ -575,6 +586,7 @@ if __name__ == "__main__" :
     parser.add_argument("--torchbench_dir", type=str, help="path to torchbenchmark repo")
     parser.add_argument("--output_dir", type=str, default=".", help="path to write output files")
     parser.add_argument("--dump_lazy_counters", action='store_true', help="dump lazy counter values after each timing run")
+    parser.add_argument("--just_run_once", action="store_true")
     parser.add_argument("--run_tracing_execute_noops", action='store_true', help="Run the tracing portion only, with noop backend, useful for running under a profiler.")
     parser.add_argument("--run_in_subprocess", "-s", type=str, help="which model run in subprocess.This will ignore filter and exclude")
     args = parser.parse_args()
@@ -615,7 +627,10 @@ if __name__ == "__main__" :
                     run_tracing_execute_noops(args.test, lazy_benchmark)
                     # when profiling, we really don't want to do anything else
                     exit(0)
-
+                if args.just_run_once:
+                    just_run_once(args, lazy_benchmark)
+                    exit(0)
+                
                 with pick_grad(args, name):
                     with fuser(args.fuser) if args.fuser != 'noopt' else optimized_execution(False):
                         if args.fuser == 'noopt':
