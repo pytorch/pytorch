c3bc141ec566ec3839aa852ce916393d47155f23
Tugsbayasgalan (Tugsuu) Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-15 00:38:18 -0800

    Add test operator in upgrader entry (#69427)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/69427
    
    Test Plan: Imported from OSS
    
    Reviewed By: gmagogsfm
    
    Differential Revision: D32867984
    
    Pulled By: tugsbayasgalan
    
    fbshipit-source-id: 25810fc2fd4b943911f950618968af067c04da5c

--- torch/csrc/jit/operator_upgraders/version_map.h
+++ torch/csrc/jit/operator_upgraders/version_map.h
@@ -12,7 +12,7 @@ struct UpgraderEntry {
   std::string old_schema;
 };
 
-const static std::unordered_map<std::string, std::vector<UpgraderEntry>> kOperatorVersionMap(
+static std::unordered_map<std::string, std::vector<UpgraderEntry>> operatorVersionMap(
     {{"aten::div.Tensor",
       {{4,
         "div_Tensor_0_3",
@@ -42,9 +42,17 @@ const static std::unordered_map<std::string, std::vector<UpgraderEntry>> kOperat
         "full_out_0_4",
         "aten::full.out(int[] size, Scalar fill_value, *, Tensor(a!) out) -> Tensor(a!)"}}}});
 
-std::unordered_map<std::string, std::vector<UpgraderEntry>>
+const std::unordered_map<std::string, std::vector<UpgraderEntry>>&
 get_operator_version_map() {
-  return kOperatorVersionMap;
+  return operatorVersionMap;
+}
+
+void test_only_add_entry(std::string op_name, UpgraderEntry entry) {
+  operatorVersionMap[op_name].push_back(entry);
+}
+
+void test_only_remove_entry(std::string op_name) {
+  operatorVersionMap.erase(op_name);
 }
 
 } // namespace jit
