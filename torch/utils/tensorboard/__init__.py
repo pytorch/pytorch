77bd0e0c752bfb75d6bd2df08d284a33b78c463e
Nikita Shulga <nshulga@fb.com>
2021-12-15 07:31:28 -0800

    Update __init__.py

--- torch/utils/tensorboard/__init__.py
+++ torch/utils/tensorboard/__init__.py
@@ -4,7 +4,6 @@ from distutils.version import LooseVersion
 if not hasattr(tensorboard, '__version__') or LooseVersion(tensorboard.__version__) < LooseVersion('1.15'):
     raise ImportError('TensorBoard logging requires TensorBoard version 1.15 or above')
 
-del distutils
 del LooseVersion
 del tensorboard
 
