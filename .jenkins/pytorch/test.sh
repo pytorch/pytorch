dadc3fbedf9b2d3c17aa7c54a44413e7edf52cae
Michael Andreas Dagitses <mikeyd@fb.com>
2021-12-15 11:37:00 -0800

    test
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -459,6 +459,7 @@ test_bazel() {
 
   get_bazel
 
+  tools/bazel test --test_timeout=480 --test_output=all --test_tag_filters=-gpu-required --test_filter=-*CUDA //c10/...
   tools/bazel test --test_timeout=480 --test_output=all --test_tag_filters=-gpu-required --test_filter=-*CUDA :all_tests
 }
 
