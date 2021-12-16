04ae345b411e28737e26ddf649740e87efba407b
Horace He <chilli@fb.com>
2021-12-16 00:32:18 -0800

    [functorch] add option for meta tensor tracing (pytorch/functorch#349)
    * add option for meta tensor tracing
    
    * Added meta tensor flag

--- functorch/functorch/compile/__init__.py
+++ functorch/functorch/compile/__init__.py
@@ -1,6 +1,6 @@
 from .._src.operator_authoring import pointwise_operator
 from .._src.memory_efficient_op_authoring import memory_efficient_pointwise_fusion, torchscript_nvfuser_compile
-from .._src.python_key import nnc_jit, make_nnc, pythonkey_decompose
+from .._src.python_key import nnc_jit, make_nnc, pythonkey_decompose, pythonkey_meta
 from .._src.decompositions import register_decomposition, decomposition_table
 from .._src.nnc_compile import nnc_compile, get_ops
 from .._src.fx_minifier import minimizer
@@ -9,7 +9,6 @@ from .._src.aot_autograd import (
     aot_module,
     compiled_function,
     compiled_module,
-    tvm_compile,
     draw_joint_graph,
     default_partition,
     partition_with_recompute_fwd_in_bwd,
@@ -17,3 +16,4 @@ from .._src.aot_autograd import (
     clear_compile_cache,
     draw_graph,
 )
+from .._src.compilers import ts_compile, tvm_compile, draw_graph_compile, nop
