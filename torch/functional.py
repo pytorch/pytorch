a9638760cdf1f6cc93417578dd6f5a6a4326fd87
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-12-16 08:05:35 -0600

    Remove chain_matmul from functional.py

--- torch/functional.py
+++ torch/functional.py
@@ -1596,49 +1596,6 @@ def norm(input, p="fro", dim=None, keepdim=False, out=None, dtype=None):  # noqa
             else:
                 return _VF.norm(input, p, _dim, keepdim=keepdim, dtype=dtype, out=out)  # type: ignore[attr-defined]
 
-def chain_matmul(*matrices, out=None):
-    r"""Returns the matrix product of the :math:`N` 2-D tensors. This product is efficiently computed
-    using the matrix chain order algorithm which selects the order in which incurs the lowest cost in terms
-    of arithmetic operations (`[CLRS]`_). Note that since this is a function to compute the product, :math:`N`
-    needs to be greater than or equal to 2; if equal to 2 then a trivial matrix-matrix product is returned.
-    If :math:`N` is 1, then this is a no-op - the original matrix is returned as is.
-
-    .. warning::
-
-        :func:`torch.chain_matmul` is deprecated and will be removed in a future PyTorch release.
-        Use :func:`torch.linalg.multi_dot` instead, which accepts a list of two or more tensors
-        rather than multiple arguments.
-
-    Args:
-        matrices (Tensors...): a sequence of 2 or more 2-D tensors whose product is to be determined.
-        out (Tensor, optional): the output tensor. Ignored if :attr:`out` = ``None``.
-
-    Returns:
-        Tensor: if the :math:`i^{th}` tensor was of dimensions :math:`p_{i} \times p_{i + 1}`, then the product
-        would be of dimensions :math:`p_{1} \times p_{N + 1}`.
-
-    Example::
-
-        >>> a = torch.randn(3, 4)
-        >>> b = torch.randn(4, 5)
-        >>> c = torch.randn(5, 6)
-        >>> d = torch.randn(6, 7)
-        >>> torch.chain_matmul(a, b, c, d)
-        tensor([[ -2.3375,  -3.9790,  -4.1119,  -6.6577,   9.5609, -11.5095,  -3.2614],
-                [ 21.4038,   3.3378,  -8.4982,  -5.2457, -10.2561,  -2.4684,   2.7163],
-                [ -0.9647,  -5.8917,  -2.3213,  -5.2284,  12.8615, -12.2816,  -2.5095]])
-
-    .. _`[CLRS]`: https://mitpress.mit.edu/books/introduction-algorithms-third-edition
-    """
-    # This wrapper exists to support variadic args.
-    if has_torch_function(matrices):
-        return handle_torch_function(chain_matmul, matrices, *matrices)
-
-    if out is None:
-        return _VF.chain_matmul(matrices)  # type: ignore[attr-defined]
-    else:
-        return _VF.chain_matmul(matrices, out=out)  # type: ignore[attr-defined]
-
 
 def _lu_impl(A, pivot=True, get_infos=False, out=None):
     # type: (Tensor, bool, bool, Any) -> Tuple[Tensor, Tensor, Tensor]
