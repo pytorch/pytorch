82df192320746be07037d27fe7b80e4026623a36
Joel Benjamin Schlosser <jbschlosser@fb.com>
2021-12-16 17:17:19 -0500

    Remove backward op for slow dilated 3d convolution
    [ghstack-poisoned]

--- aten/src/ATen/core/aten_interned_strings.h
+++ aten/src/ATen/core/aten_interned_strings.h
@@ -704,7 +704,6 @@ _(aten, thnn_conv_depthwise2d_backward) \
 _(aten, thnn_conv_depthwise2d_forward) \
 _(aten, slow_conv_dilated2d) \
 _(aten, slow_conv_dilated3d) \
-_(aten, slow_conv_dilated3d_backward) \
 _(aten, slow_conv_transpose2d) \
 _(aten, slow_conv_transpose2d_backward) \
 _(aten, slow_conv_transpose3d) \
