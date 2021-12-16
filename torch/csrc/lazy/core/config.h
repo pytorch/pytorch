e6a4988b2db8ad89463465c92f1a046596e8b332
Bin Bao <binbao@fb.com>
2021-12-16 05:41:42 -0800

    [LTC] Upstream utils in computation_client (#69621)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69621
    
    Upstream the following utils
    - metrics.h
    - multi_wait.h
    - thread_pool.h
    - unique.h
    
    Test Plan: Imported from OSS
    
    Reviewed By: wconstab, VitalyFedyunin
    
    Differential Revision: D32957629
    
    Pulled By: desertfire
    
    fbshipit-source-id: 5f2fb57493856556099b7cda7560a568d1f9ed97

--- torch/csrc/lazy/core/config.h
+++ torch/csrc/lazy/core/config.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <c10/util/Flags.h>
 
+C10_DECLARE_int(torch_lazy_io_thread_pool_size);
 C10_DECLARE_bool(torch_lazy_ir_debug);
+C10_DECLARE_string(torch_lazy_metrics_percentiles);
+C10_DECLARE_int(torch_lazy_metrics_samples);
