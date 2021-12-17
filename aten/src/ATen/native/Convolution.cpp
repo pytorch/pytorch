962a96cfff5801cb2b1b5ed81bdf0e6d478fac62
Joel Schlosser <jbschlosser@fb.com>
2021-12-17 09:57:40 -0800

    Remove backward ops for miopen transposed convolution (#70063)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70063
    
    Test Plan: Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D33171170
    
    Pulled By: jbschlosser
    
    fbshipit-source-id: 4fd6c1cd027f714354644c4ac7694d0f9092c762

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -26,9 +26,11 @@ DEFINE_DISPATCH(cudnn_convolution_backward_stub);
 DEFINE_DISPATCH(cudnn_convolution_transpose_backward_stub);
 DEFINE_DISPATCH(convolution_depthwise3x3_winograd_stub);
 DEFINE_DISPATCH(miopen_convolution_backward_stub);
+DEFINE_DISPATCH(miopen_convolution_transpose_backward_stub);
 REGISTER_NO_CPU_DISPATCH(cudnn_convolution_backward_stub, cudnn_convolution_backward_fn);
 REGISTER_NO_CPU_DISPATCH(cudnn_convolution_transpose_backward_stub, cudnn_convolution_transpose_backward_fn);
 REGISTER_NO_CPU_DISPATCH(miopen_convolution_backward_stub, miopen_convolution_backward_fn);
+REGISTER_NO_CPU_DISPATCH(miopen_convolution_transpose_backward_stub, miopen_convolution_transpose_backward_fn);
 
 std::ostream& operator<<(std::ostream & out, const ConvParams& params) {
   out << "ConvParams {"
@@ -1630,7 +1632,8 @@ std::tuple<Tensor, Tensor, Tensor> convolution_backward(
     case ConvBackend::MiopenTranspose:
       check_input_same_type_as_parameters(input, weight);
       std::tie(backend_grad_input, backend_grad_weight, backend_grad_bias) =
-        at::miopen_convolution_transpose_backward(
+        miopen_convolution_transpose_backward_stub(
+          input.device().type(),
           input.contiguous(backend_memory_format), grad_output, weight, params.padding, params.output_padding,
           params.stride, params.dilation, params.groups, params.benchmark, params.deterministic, output_mask);
       break;
