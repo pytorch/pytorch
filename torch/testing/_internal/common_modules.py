b0bcda53c32fc0f0efe990aa47039fc61d2287e8
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-12-17 07:54:58 +0000

    add comments

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -232,6 +232,16 @@ def no_batch_dim_reference_fn(m, p, *args, **kwargs):
 
     The module is passed the input and target in batched form with a single item.
     The output is squeezed to compare with the no-batch input.
+
+    Currently it only supports modules which return a single Tensor as output.
+    You can bind the following kwargs.
+    Kwargs:
+        batch_first[bool] : If True, all the Tensors in `args` while be unsqueezed at dim `0` .
+                        and output will be squeezed at dim `0` else dim `1` for both.
+        kwargs_to_batchify[dict] : Dictionary specifying the name of the argument and dimension to unsqueeze.
+                               Useful if there are few arguments whose batch dimension are different
+                               from the ones selected by `batch_first`.
+        is_criterion[bool] : Specify if the module is a criterion and handle the reduction for output accordingly.
     """
     def get_and_pop(key, default):
         v = kwargs.get(key, default)
@@ -240,13 +250,15 @@ def no_batch_dim_reference_fn(m, p, *args, **kwargs):
         return v
 
     batch_dim = 0 if get_and_pop('batch_first', True) else 1
-    kwargs_to_batchify = get_and_pop('kwargs_to_batchify', {})
+    kwargs_to_batchify = get_and_pop('kwargs_to_batchify', None)
     is_criterion = get_and_pop('is_criterion', False)
 
-    for k, v in kwargs.items():
-        if k in kwargs_to_batchify and v is not None:
-            bdim = kwargs_to_batchify[k]
-            kwargs[k] = v.unsqueeze(bdim)
+    if kwargs_to_batchify is not None:
+        assert isinstance(kwargs_to_batchify, dict)
+        for k, v in kwargs.items():
+            if k in kwargs_to_batchify and v is not None:
+                bdim = kwargs_to_batchify[k]
+                kwargs[k] = v.unsqueeze(bdim)
 
     single_batch_input_args = [input.unsqueeze(batch_dim) for input in args]
     with freeze_rng_state():
