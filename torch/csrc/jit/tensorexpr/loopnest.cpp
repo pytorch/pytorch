ac92f7cc75d743294133703e406fe412502f35bd
Hui Guo <huiguo@fb.com>
2021-12-17 01:34:48 -0800

    [tensorexpr] Remove the optional argument in LoopNest::prepareForCodeGen (#67144)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/67144
    
    Test Plan: Imported from OSS
    
    Reviewed By: ZolotukhinM
    
    Differential Revision: D31881150
    
    Pulled By: huiguoo
    
    fbshipit-source-id: af99087722ec71d6deb9049b63b573ae7720c9ec

--- torch/csrc/jit/tensorexpr/loopnest.cpp
+++ torch/csrc/jit/tensorexpr/loopnest.cpp
@@ -1189,8 +1189,7 @@ void LoopNest::eliminateDeadStores() {
   root_stmt_ = root_stmt_->accept_mutator(&deleter);
 }
 
-void LoopNest::prepareForCodegen(
-    const c10::optional<std::vector<BufPtr>>& interm_bufs /*= c10::nullopt*/) {
+void LoopNest::prepareForCodegen() {
   // Expand reduction ops.
   ReductionExpander reduceExpander;
   root_stmt_ = reduceExpander.expand(root_stmt_);
