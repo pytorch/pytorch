854eb842d855891255cc4d26c355669c1dc45d80
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-20 14:38:34 +0900

    Fix CI error.

--- torch/distributions/wishart.py
+++ torch/distributions/wishart.py
@@ -179,7 +179,7 @@ class Wishart(ExponentialFamily):
         noise = self._dist_chi2.rsample(sample_shape).sqrt().diag_embed(dim1=-2, dim2=-1)
         i, j = torch.tril_indices(p, p, offset=-1)
         noise[..., i, j] = torch.randn(
-            torch.Size(sample_shape) + self._batch_shape + (p * (p - 1) / 2,),
+            torch.Size(sample_shape) + self._batch_shape + (int(p * (p - 1) / 2),),
             dtype=noise.dtype,
             device=noise.device,
         )
