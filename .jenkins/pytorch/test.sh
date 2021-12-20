d17488651944bed5a770951caec44cba54102672
Shirong Wu <shirong@fb.com>
2021-12-20 12:36:32 -0800

    Install TensorRT lib on oss docker and enable fx2trt unit test
    Summary: CI
    
    Differential Revision: D33241684
    
    fbshipit-source-id: af1dbac1ad2857833f0ff8fc56fe0b5364f689af

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -517,6 +517,12 @@ test_docs_test() {
   .jenkins/pytorch/docs-test.sh
 }
 
+test_torch_fx2trt() {
+  pip install parameterized
+  time python test/run_test.py --fx2trt-tests --verbose
+  assert_git_not_dirty
+}
+
 if ! [[ "${BUILD_ENVIRONMENT}" == *libtorch* || "${BUILD_ENVIRONMENT}" == *-bazel-* ]]; then
   (cd test && python -c "import torch; print(torch.__config__.show())")
   (cd test && python -c "import torch; print(torch.__config__.parallel_info())")
@@ -562,6 +568,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *distributed* || "${JOB_BASE_NAME}" == *distri
   test_rpc
 elif [[ "${TEST_CONFIG}" = docs_test ]]; then
   test_docs_test
+elif [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.3-py3.6-gcc7* ]]; then
+  test_torch_fx2trt
 else
   install_torchvision
   install_monkeytype
--- test/run_test.py
+++ test/run_test.py
@@ -86,7 +86,8 @@ TESTS = discover_tests(
         'bottleneck_test',
         'custom_backend',
         'custom_operator',
-        'fx',        # executed by test_fx.py
+        'fx/',        # executed by test_fx.py
+        'fx_acc/',
         'jit',      # executed by test_jit.py
         'mobile',
         'onnx',
@@ -137,6 +138,8 @@ TESTS = discover_tests(
 )
 
 FSDP_TEST = [test for test in TESTS if test.startswith("distributed/fsdp")]
+
+FX2TRT_TESTS = [test for test in TESTS if test.startswith("fx2trt/")]
 
 # Tests need to be run with pytest.
 USE_PYTEST_LIST = [
@@ -205,7 +208,7 @@ WINDOWS_BLOCKLIST = [
     "distributed/_sharded_tensor/ops/test_init",
     "distributed/_sharded_tensor/ops/test_linear",
     "distributed/_sharded_optim/test_sharded_optim",
-] + FSDP_TEST
+] + FSDP_TEST + FX2TRT_TESTS
 
 ROCM_BLOCKLIST = [
     "distributed/nn/jit/test_instantiator",
@@ -665,6 +668,12 @@ def parse_args():
         help="run all distributed tests",
     )
     parser.add_argument(
+        "--fx2trt-tests",
+        "--fx2trt-tests",
+        action="store_true",
+        help="run all fx2trt tests",
+    )
+    parser.add_argument(
         "-core",
         "--core",
         action="store_true",
@@ -872,6 +881,14 @@ def get_selected_tests(options):
         selected_tests = list(
             filter(lambda test_name: test_name in DISTRIBUTED_TESTS, selected_tests)
         )
+
+    # Only run fx2trt test with specified option argument
+    if options.fx2trt_tests:
+        selected_tests = list(
+            filter(lambda test_name: "fx2trt" in test_name, selected_tests)
+        )
+    else:
+        options.exclude.extend(FX2TRT_TESTS)
 
     # Filter to only run core tests when --core option is specified
     if options.core:
