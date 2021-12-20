859e6344c6bdfd872a815848f8806352519d5475
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-20 23:00:37 +0900

    Refactor '__init__' to pass mypy test.

--- torch/distributions/wishart.py
+++ torch/distributions/wishart.py
@@ -67,34 +67,36 @@ class Wishart(ExponentialFamily):
             assert scale_tril.dim() > 1, \
                 "scale_tril must be at least two-dimensional, with optional leading batch dimensions"
             if isinstance(df, Number):
-                df = torch.tensor(df, dtype=scale_tril.dtype, device=scale_tril.device)
                 batch_shape = torch.Size()
+                self.df = torch.tensor(df, dtype=scale_tril.dtype, device=scale_tril.device)
             else:
                 batch_shape = torch.broadcast_shapes(scale_tril.shape[:-2], df.shape)
+                self.df = df.expand(batch_shape)
             event_shape = scale_tril.shape[-2:]
             self.scale_tril = scale_tril.expand(batch_shape + (-1, -1))
         elif covariance_matrix is not None:
             assert covariance_matrix.dim() > 1, \
                 "covariance_matrix must be at least two-dimensional, with optional leading batch dimensions"
             if isinstance(df, Number):
-                df = torch.tensor(df, dtype=covariance_matrix.dtype, device=covariance_matrix.device)
                 batch_shape = torch.Size()
+                self.df = torch.tensor(df, dtype=covariance_matrix.dtype, device=covariance_matrix.device)
             else:
                 batch_shape = torch.broadcast_shapes(covariance_matrix.shape[:-2], df.shape)
+                self.df = df.expand(batch_shape)
             event_shape = covariance_matrix.shape[-2:]
             self.covariance_matrix = covariance_matrix.expand(batch_shape + (-1, -1))
         elif precision_matrix is not None:
             assert precision_matrix.dim() > 1, \
                 "precision_matrix must be at least two-dimensional, with optional leading batch dimensions"
             if isinstance(df, Number):
-                df = torch.tensor(df, dtype=precision_matrix.dtype, device=precision_matrix.device)
                 batch_shape = torch.Size()
+                self.df = torch.tensor(df, dtype=precision_matrix.dtype, device=precision_matrix.device)
             else:
                 batch_shape = torch.broadcast_shapes(precision_matrix.shape[:-2], df.shape)
+                self.df = df.expand(batch_shape)
             event_shape = precision_matrix.shape[-2:]
             self.precision_matrix = precision_matrix.expand(batch_shape + (-1, -1))
 
-        self.df = df.expand(batch_shape)
         self.arg_constraints['df'] = constraints.greater_than(event_shape[-1] - 1)
         if self.df.lt(event_shape[-1]).any():
             warnings.warn("Low df values detected. Singular samples are highly likely to occur for ndim - 1 < df < ndim.")
