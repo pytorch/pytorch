9b3d046b462a7213c00415388df7a88e7cfc941c
Yi Zhang <zhanyi@microsoft.com>
2021-12-22 15:34:54 +0800

    For builder PR911

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -62,7 +62,7 @@ popd
 
 # Clone the Builder master repo
 ## retry git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
-retry git clone -q https://github.com/mszhanyi/builder.git -b zhanyi/retry "$BUILDER_ROOT"
+retry git clone -q https://github.com/mszhanyi/builder.git -b zhanyi/vslink "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
