787ada6e387a042bcc59d26308d5dca07078cb2a
George Qi <georgeqi94@gmail.com>
2021-12-22 14:24:23 -0800

    FractionalMaxPool3d with no_batch_dim support (#69732)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/69732
    
    Test Plan: Imported from OSS
    
    Reviewed By: jbschlosser
    
    Differential Revision: D33280090
    
    Pulled By: george-qi
    
    fbshipit-source-id: aaf90a372b6d80da0554bad28d56436676f9cb89

--- torch/testing/_internal/common_nn.py
+++ torch/testing/_internal/common_nn.py
@@ -1312,6 +1312,56 @@ def fractional_max_pool3d_test(test_case, return_indices=False):
     return out
 
 
+def fractional_max_pool3d_no_batch_dim_test(test_case, use_random_samples):
+    if use_random_samples:
+        # random_samples enables CPU and GPU checks to be consistent
+        random_samples = torch.empty((2, 4, 3), dtype=torch.double).uniform_()
+        if test_case == 'ratio':
+            return dict(
+                constructor=lambda: nn.FractionalMaxPool3d(
+                    2, output_ratio=0.5, _random_samples=random_samples),
+                cpp_constructor_args='''torch::nn::FractionalMaxPool3dOptions(2)
+                                        .output_ratio(0.5)
+                                        ._random_samples(random_samples)''',
+                input_size=(4, 5, 5, 5),
+                cpp_var_map={'random_samples': random_samples},
+                reference_fn=single_batch_reference_fn,
+                fullname='FractionalMaxPool3d_ratio_no_batch_dim')
+        elif test_case == 'size':
+            return dict(
+                constructor=lambda: nn.FractionalMaxPool3d((2, 2, 2), output_size=(
+                    4, 4, 4), _random_samples=random_samples),
+                cpp_constructor_args='''torch::nn::FractionalMaxPool3dOptions({2, 2, 2})
+                                        .output_size(std::vector<int64_t>({4, 4, 4}))
+                                        ._random_samples(random_samples)''',
+                input_size=(4, 7, 7, 7),
+                cpp_var_map={'random_samples': random_samples},
+                reference_fn=single_batch_reference_fn,
+                fullname='FractionalMaxPool3d_size_no_batch_dim')
+    else:
+        # can not check cuda because there RNG is different between cpu and cuda
+        if test_case == 'ratio':
+            return dict(
+                constructor=lambda: nn.FractionalMaxPool3d(
+                    2, output_ratio=0.5),
+                cpp_constructor_args='''torch::nn::FractionalMaxPool3dOptions(2)
+                                        .output_ratio(0.5)''',
+                input_size=(4, 5, 5, 5),
+                reference_fn=single_batch_reference_fn,
+                test_cuda=False,
+                fullname='FractionalMaxPool3d_ratio_no_batch_dim_no_random_samples')
+        elif test_case == 'size':
+            return dict(
+                constructor=lambda: nn.FractionalMaxPool3d((2, 2, 2), output_size=(
+                    4, 4, 4)),
+                cpp_constructor_args='''torch::nn::FractionalMaxPool3dOptions({2, 2, 2})
+                                        .output_size(std::vector<int64_t>({4, 4, 4}))''',
+                input_size=(4, 7, 7, 7),
+                reference_fn=single_batch_reference_fn,
+                test_cuda=False,
+                fullname='FractionalMaxPool3d_size_no_batch_dim_no_random_samples')
+
+
 def single_batch_reference_fn(input, parameters, module):
     """Reference function for modules supporting no batch dimensions.
 
@@ -1391,6 +1441,10 @@ new_module_tests = [
     fractional_max_pool3d_test('size'),
     fractional_max_pool3d_test('asymsize'),
     fractional_max_pool3d_test('ratio', return_indices=True),
+    fractional_max_pool3d_no_batch_dim_test('ratio', True),
+    fractional_max_pool3d_no_batch_dim_test('ratio', False),
+    fractional_max_pool3d_no_batch_dim_test('size', True),
+    fractional_max_pool3d_no_batch_dim_test('size', False),
     dict(
         module_name='BatchNorm1d',
         constructor_args=(10,),
