68d887c5c4e417cd46c065aa525b45bd72d37a98
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-22 23:24:08 -0800

    Update base for Update on "Add graph op replacement pass"
    Differential Revision: [D33198158](https://our.internmc.facebook.com/intern/diff/D33198158)
    
    [ghstack-poisoned]

--- torch/csrc/jit/operator_upgraders/version_map.h
+++ torch/csrc/jit/operator_upgraders/version_map.h
@@ -16,9 +16,13 @@ struct UpgraderEntry {
 TORCH_API const std::unordered_map<std::string, std::vector<UpgraderEntry>>&
 get_operator_version_map();
 
-TORCH_API void test_only_add_entry(std::string op_name, UpgraderEntry entry);
+TORCH_API void test_only_add_entry(
+    const std::string& op_name,
+    UpgraderEntry entry);
 
-TORCH_API void test_only_remove_entry(std::string op_name);
+TORCH_API void test_only_remove_entry(const std::string& op_name);
+
+TORCH_API void test_only_reset_flag();
 
 } // namespace jit
 } // namespace torch
