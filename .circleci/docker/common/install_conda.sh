72eb2b716b5aab9a670fe7436e3e23ff7be691de
Nikita Shulga <nshulga@fb.com>
2021-12-23 14:26:20 -0800

    Install dependencies in py_${ANACONDA_VERSION} env

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -59,13 +59,13 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   as_jenkins conda update -y -n base conda
 
   # Install correct Python version
-  as_jenkins conda install -y python="$ANACONDA_PYTHON_VERSION"
+  as_jenkins conda create -n py_$ANACONDA_PYTHON_VERSION -y python="$ANACONDA_PYTHON_VERSION"
 
   conda_install() {
     # Ensure that the install command don't upgrade/downgrade Python
     # This should be called as
     #   conda_install pkg1 pkg2 ... [-c channel]
-    as_jenkins conda install -q -y python="$ANACONDA_PYTHON_VERSION" $*
+    as_jenkins conda install -q -n py_$ANACONDA_PYTHON_VERSION -y python="$ANACONDA_PYTHON_VERSION" $*
   }
 
   # Install PyTorch conda deps, as per https://github.com/pytorch/pytorch README
