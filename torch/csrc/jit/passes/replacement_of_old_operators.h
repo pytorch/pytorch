bbdd7ecb8d54a1efce3c2f336b817f4bcdb61223
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-22 23:24:09 -0800

    Update base for Update on "Bump version number to 7 and compile old operators with old schema"
    Differential Revision: [D33198155](https://our.internmc.facebook.com/intern/diff/D33198155)
    
    [ghstack-poisoned]

--- torch/csrc/jit/passes/replacement_of_old_operators.h
+++ torch/csrc/jit/passes/replacement_of_old_operators.h
@@ -5,7 +5,12 @@
 namespace torch {
 namespace jit {
 
-TORCH_API void ApplyOpsUpgraders(std::shared_ptr<Graph> graph);
+// Find the valid upgrader graph for the upgrader and cache the result
+// for later lookups. Will error out if there is no valid upgrader graph
+// provided for the upgrader name.
+std::shared_ptr<Graph> getUpgraderGraph(const std::string& upgrader_name);
+
+TORCH_API void ReplaceOldOperatorsWithUpgraders(std::shared_ptr<Graph> graph);
 
 } // namespace jit
 } // namespace torch
