67a03c6337fccce553a65c0b16af6cba0d12ce0b
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-27 12:54:51 +0900

    Trivial Revision of '.entropy()'.

--- torch/distributions/wishart.py
+++ torch/distributions/wishart.py
@@ -262,7 +262,7 @@ class Wishart(ExponentialFamily):
             (p + 1) * self._unbroadcasted_scale_tril.diagonal(dim1=-2, dim2=-1).log().sum(-1)
             + p * (p + 1) * _log_2 / 2
             + torch.mvlgamma(nu / 2, p=p)
-            - _mvdigamma(nu / 2, p=p) * (nu - p - 1) / 2
+            - (nu - p - 1) / 2 * _mvdigamma(nu / 2, p=p)
             + nu * p / 2
         )
 
