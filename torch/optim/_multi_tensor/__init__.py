9cb4d29c64cef3eedbdfc810e4a2c2961b722cae
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2021-12-31 03:43:48 +0000

    Update base for Update on "Optim foreach cleanup for Rprop"
    [ghstack-poisoned]

--- torch/optim/_multi_tensor/__init__.py
+++ torch/optim/_multi_tensor/__init__.py
@@ -4,27 +4,20 @@ Most commonly used methods are already supported, and the interface is general
 enough, so that more sophisticated ones can be also easily integrated in the
 future.
 """
+from functools import partial
+from torch import optim
 
-from .adam import Adam
+Adam = partial(optim.Adam, foreach=True)
 from .adamw import AdamW
-from .nadam import NAdam
-from .sgd import SGD
-from .radam import RAdam as RAdam
-from .rmsprop import RMSprop
+NAdam = partial(optim.NAdam, foreach=True)
+SGD = partial(optim.SGD, foreach=True)
+RAdam = partial(optim.RAdam, foreach=True)
+RMSprop = partial(optim.RMSprop, foreach=True)
 from .rprop import Rprop
-from .asgd import ASGD
-from .adamax import Adamax
-from .adadelta import Adadelta
-from .adagrad import Adagrad
+ASGD = partial(optim.ASGD, foreach=True)
+Adamax = partial(optim.Adamax, foreach=True)
+Adadelta = partial(optim.Adadelta, foreach=True)
+Adagrad = partial(optim.Adagrad, foreach=True)
 
-del adam
 del adamw
-del sgd
-del radam
-del nadam
-del rmsprop
 del rprop
-del asgd
-del adamax
-del adadelta
-del adagrad
