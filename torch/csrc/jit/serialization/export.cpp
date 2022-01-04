e5c7c0c6b33379d5e38ab08576f504293abe00f7
BowenBao <bowbao@microsoft.com>
2022-01-04 15:18:32 -0800

    Relax sequence tensor dim_param serialization

--- torch/csrc/jit/serialization/export.cpp
+++ torch/csrc/jit/serialization/export.cpp
@@ -543,7 +543,8 @@ void GraphEncoder::EncodeValueInfoType(
         std::unordered_map<int64_t, std::string>>& dynamic_axes) {
   auto tensorTypeToONNXType = [&dynamic_axes, n, this](
                                   const TensorTypePtr& t,
-                                  onnx::TypeProto_Tensor* onnx_tensor_type) {
+                                  onnx::TypeProto_Tensor* onnx_tensor_type,
+                                  bool is_sequence_tensor) {
     std::string name = n->debugName();
     if (t->dim()) {
       onnx::TensorShapeProto* shape = onnx_tensor_type->mutable_shape();
@@ -558,7 +559,12 @@ void GraphEncoder::EncodeValueInfoType(
           }
         } else if (sizes[i].is_static()) {
           shape->mutable_dim(i)->set_dim_value(sizes[i].static_size());
-        } else {
+        } else if (!is_sequence_tensor) {
+          // Do not assign dim_param for sequence tensor type.
+          // Sequence of tensors could differ in dimension size.
+          // Use a dimension with neither dim_value nor dim_param set
+          // to denote an unknown dimension.
+          // Create and assign dim_param for normal tensor type.
           if (symbol_dim_map_.find(sizes[i]) == symbol_dim_map_.end()) {
             if (n->node()->kind() == prim::Param) {
               symbol_dim_map_[sizes[i]] = name + "_dim_" + std::to_string(i);
@@ -583,7 +589,7 @@ void GraphEncoder::EncodeValueInfoType(
       // Encode type if either shape or dtype exists.
       onnx::TypeProto_Tensor* onnx_tensor_type =
           onnx_type->mutable_tensor_type();
-      tensorTypeToONNXType(tensor_type, onnx_tensor_type);
+      tensorTypeToONNXType(tensor_type, onnx_tensor_type, tensor_type != n->type());
     }
   } else if (BoolTypePtr bool_type = node_type->cast<BoolType>()) {
     onnx::TypeProto_Tensor* onnx_tensor_type = onnx_type->mutable_tensor_type();
