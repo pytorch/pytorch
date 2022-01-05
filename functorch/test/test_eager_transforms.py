dba6940900269928515fd1087ac439c0323c1cf9
Sam Andow <samdow@fb.com>
2022-01-05 22:03:25 +0000

    [functorch] switch per sample grad test to use group norm

--- functorch/test/test_eager_transforms.py
+++ functorch/test/test_eager_transforms.py
@@ -1862,15 +1862,15 @@ class TestExamplesCorrectness(TestCase):
 
         net = nn.Sequential(
             nn.Conv2d(1, 64, 3),
-            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.GroupNorm(64, 64, affine=True),
             nn.ReLU(inplace=inplace_relu),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(64, 64, 3),
-            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.GroupNorm(64, 64, affine=True),
             nn.ReLU(inplace=inplace_relu),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(64, 64, 3),
-            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.GroupNorm(64, 64, affine=True),
             nn.ReLU(inplace=inplace_relu),
             nn.MaxPool2d(2, 2),
             Flatten(),
