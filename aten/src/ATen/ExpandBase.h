68b4587d3ff4a90c39fc6e913f7b527f365eabdf
Peter Bell <peterbell10@live.co.uk>
2022-01-06 19:48:43 +0000

    Update base for Update on "Remove native_functions.yaml dependency from DistributionNormal.cu"
    Differential Revision: [D32500424](https://our.internmc.facebook.com/intern/diff/D32500424)
    
    [ghstack-poisoned]

--- aten/src/ATen/ExpandBase.h
+++ aten/src/ATen/ExpandBase.h
@@ -4,18 +4,20 @@
 namespace at {
 namespace internal {
 TensorBase expand_slow_path(const TensorBase &self, IntArrayRef size);
-}
+} // namespace internal
 
-inline c10::MaybeOwned<TensorBase> expand_to_size(const TensorBase &self, IntArrayRef size) {
+inline c10::MaybeOwned<TensorBase> expand_size(const TensorBase &self, IntArrayRef size) {
   if (size.equals(self.sizes())) {
     return c10::MaybeOwned<TensorBase>::borrowed(self);
   }
   return c10::MaybeOwned<TensorBase>::owned(
       at::internal::expand_slow_path(self, size));
 }
+c10::MaybeOwned<Tensor> expand_size(TensorBase &&to_expand, IntArrayRef sizes) = delete;
 
 inline c10::MaybeOwned<TensorBase> expand_inplace(const TensorBase &tensor, const TensorBase &to_expand) {
-  return expand_to_size(to_expand, tensor.sizes());
+  return expand_size(to_expand, tensor.sizes());
 }
+c10::MaybeOwned<TensorBase> expand_size(const TensorBase &tensor, const TensorBase &to_expand) = delete;
 
-}
+} // namespace at
