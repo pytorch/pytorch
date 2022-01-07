1cecf78900dafcbb8c2dec0f5fb03f28f33518a4
Emilio Castillo <ecastill@preferred.jp>
2022-01-07 03:14:19 +0000

    Support parametrizations

--- torch/nn/utils/_stateless.py
+++ torch/nn/utils/_stateless.py
@@ -1,16 +1,31 @@
 import contextlib
+from typing import Any, Callable, Optional, Dict, Tuple
 
-import torch.jit
+import torch
+from torch import Tensor
+from torch.nn.utils.parametrize import ParametrizationList
 
 
-def _change_class(module) -> None:
+def _create_new_parametrization(parametrization: ParametrizationList, tensor: Tensor) -> None:
+    modules = [module for module in parametrization]
+    # TODO: Find a way to cache it? but if tensor value changes each time
+    # this will be hard
+    return ParametrizationList(modules, tensor, parametrization.unsafe)
+
+
+def _change_class(module: torch.nn.Module, apply_parametrizations: bool) -> None:
     cls = module.__class__
     func_params = module._functional_parameters
+    parametrizations = None
+    if apply_parametrizations:
+        parametrizations = getattr(module, 'parametrizations', None)
 
     def _getattribute(self, name):
         if name in func_params:
+            if parametrizations is not None and name in parametrizations:
+                return _create_new_parametrization(parametrizations[name], func_params[name])()
             return func_params[name]
-        return object.__getattribute__(self, name)
+        return cls.__getattribute__(self, name)
 
     param_cls = type(
         f"StatelessReplacer{cls.__name__}",
@@ -24,34 +39,30 @@ def _change_class(module) -> None:
     module._orig_class = cls
 
 
-def _swap_parameters(module, tensor_name, tensor):
+def _swap_parameters(module: torch.nn.Module, tensor_name: str, tensor: Tensor, apply_parametrizations: bool):
     # Changes the module class to get a new __getattr__ dunder method
     # that looks for the reparametrized tensor
     if hasattr(module, "_functional_parameters"):
-        # Check if the module has an active reparametrization
-        # for this parameter
         module._functional_parameters[tensor_name] = tensor
     else:
-        # change module class to set a new __getattr__ function
-        # register tensor
         module._functional_parameters = {}
         module._functional_parameters[tensor_name] = tensor
-        _change_class(module)
+        _change_class(module, apply_parametrizations)
 
 
-def _remove_swap(module, name):
+def _remove_swap(module: torch.nn.Module, name: str):
     if hasattr(module, "_orig_class"):
         module.__class__ = module._orig_class
         delattr(module, "_orig_class")
+        delattr(module, "_functional_parameters")
 
 
 @contextlib.contextmanager
-def reparametrize_module(module, parameters_and_buffers):
-    # Parametrization does not support to change submodules directly
+def reparametrize_module(module: torch.nn.Module, parameters_and_buffers: Dict[str, Tensor], apply_parametrizations: bool):
     for name, tensor in parameters_and_buffers.items():
         _apply_func_submodules(
             _swap_parameters,
-            module, name.split("."), (tensor,))
+            module, name.split("."), (tensor, apply_parametrizations))
     yield
     for name in parameters_and_buffers:
         _apply_func_submodules(
@@ -59,14 +70,41 @@ def reparametrize_module(module, parameters_and_buffers):
             module, name.split("."), ())
 
 
-def _apply_func_submodules(func, module, path, args):
+def _apply_func_submodules(
+    func: Callable[[torch.nn.Module, str, Tuple[Any]], None],
+    module: torch.nn.Module,
+    path: str, args: Tuple[Any],
+):
     if len(path) == 1:
         func(module, path[0], *args)
     else:
         _apply_func_submodules(func, getattr(module, path[0]), path[1:], args)
 
 
-def functional_call(module, parameters_and_buffers, args, kwargs=None):
+def functional_call(
+    module: torch.nn.Module,
+    parameters_and_buffers: Dict[str, Tensor],
+    args: Tuple[Any],
+    kwargs : Dict[str, Any] = None,
+    *, apply_parametrizations : Optional[bool] = False,
+):
+    r"""Performs a functional call on the module by replacing the module parameters with
+    the provideed ones.
+
+    Args:
+        module (torch.nn.Module): the module to call
+        parameters_and_buffers (dict of str and Tensor): the parameters that will be used in
+            the module call.
+        args (tuple): arguments to be passed to the module call
+        kwargs (dict): keyword arguments to be passed to the module call
+        apply_parametrizations (bool): if ``True`` and ``module`` has parametrizations registered
+            the parametrizations will be applied on the parameter provided in ``parameters_and_buffers``.
+            If false, parametrizations will be ignored and the value in ``parameters_and_buffers`` will
+            be used directly. Default: ``False``.
+
+    Returns:
+        Any: the result of calling ``module``.
+    """
     # TODO allow kwargs such as unsafe and others for parametrization
     if (
             torch.jit.is_tracing()
@@ -80,7 +118,7 @@ def functional_call(module, parameters_and_buffers, args, kwargs=None):
         raise RuntimeError("The stateless API can't be used with Jitted modules")
     if kwargs is None:
         kwargs = {}
-    with reparametrize_module(module, parameters_and_buffers):
+    with reparametrize_module(module, parameters_and_buffers, apply_parametrizations):
         if isinstance(args, tuple):
             out = module(*args, **kwargs)
         else:
