7b8f73dd32a8a893dfb794433ce501e76c53bc89
Joel Schlosser <jbschlosser@fb.com>
2022-01-06 16:52:12 -0800

    No-batch-dim support for ConvNd (#70506)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70506
    
    Test Plan: Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D33355034
    
    Pulled By: jbschlosser
    
    fbshipit-source-id: 5a42645299b1d82cee7d461826acca1c5b35a71c

--- test/test_jit.py
+++ test/test_jit.py
@@ -13005,7 +13005,7 @@ dedent """
                 return self.conv(x)
         foo = Foo()
         # testing that the correct error message propagates
-        with self.assertRaisesRegex(RuntimeError, "Expected 4-dimensional input for 4-dimensional weight"):
+        with self.assertRaisesRegex(RuntimeError, r"Expected 3D \(unbatched\) or 4D \(batched\) input to conv2d"):
             foo(torch.ones([123]))  # wrong size
 
     def test_builtin_error_messsage(self):
