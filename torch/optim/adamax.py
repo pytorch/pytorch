7e276dc26e942b8315eb39665f4e59533c1f4a7e
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-07 23:10:28 +0000

    Update base for Update on "Optim foreach cleanup for RAdam"
    [ghstack-poisoned]

--- torch/optim/adamax.py
+++ torch/optim/adamax.py
@@ -64,9 +64,9 @@ class Adamax(Optimizer):
         super(Adamax, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super().__setstate__(state)
+        super(Adamax, self).__setstate__(state)
         for group in self.param_groups:
-            group.setdefault('foreach', False)
+            group.setdefault('foreach', None)
 
     @torch.no_grad()
     def step(self, closure=None):
