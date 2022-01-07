f828f691d377635915cce8b36e3a5481d0fd5689
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-07 23:10:33 +0000

    Update base for Update on "Optim foreach cleanup for Rmsprop"
    [ghstack-poisoned]

--- torch/optim/adadelta.py
+++ torch/optim/adadelta.py
@@ -64,9 +64,9 @@ class Adadelta(Optimizer):
         super(Adadelta, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super().__setstate__(state)
+        super(Adadelta, self).__setstate__(state)
         for group in self.param_groups:
-            group.setdefault('foreach', False)
+            group.setdefault('foreach', None)
 
     @torch.no_grad()
     def step(self, closure=None):
