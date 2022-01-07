e64b2f7e4322cd8d106996c3dd8bd6ef04f580ba
Emilio Castillo <ecastill@preferred.jp>
2022-01-07 05:15:55 +0000

    Fix typing

--- torch/nn/utils/_stateless.py
+++ torch/nn/utils/_stateless.py
@@ -1,26 +1,27 @@
 import contextlib
-from typing import Any, Callable, Optional, Dict, Tuple
+from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple
 
 import torch
 from torch import Tensor
 from torch.nn.utils.parametrize import ParametrizationList
 
 
-def _create_new_parametrization(parametrization: ParametrizationList, tensor: Tensor) -> None:
-    modules = [module for module in parametrization]
-    # TODO: Find a way to cache it? but if tensor value changes each time
-    # this will be hard
+def _create_new_parametrization(parametrization: ParametrizationList, tensor: Tensor) -> ParametrizationList:
+    # TODO: Find a way to cache it? but if tensor value changes each time this will be hard
+    modules = list(parametrization)
     return ParametrizationList(modules, tensor, parametrization.unsafe)
 
 
-def _change_class(module: torch.nn.Module, apply_parametrizations: bool) -> None:
+# We avoid typing module here because module attributes are declared as Union[Parameter, Tensor] by default
+# and using other types causes mypy errors
+def _change_class(module, apply_parametrizations: bool) -> None:
     cls = module.__class__
-    func_params = module._functional_parameters
-    parametrizations = None
+    func_params : Dict[str, Tensor] = module._functional_parameters
+    parametrizations : Optional[ParametrizationList] = None
     if apply_parametrizations:
         parametrizations = getattr(module, 'parametrizations', None)
 
-    def _getattribute(self, name):
+    def _getattribute(self, name: str) -> Any:
         if name in func_params:
             if parametrizations is not None and name in parametrizations:
                 return _create_new_parametrization(parametrizations[name], func_params[name])()
@@ -39,7 +40,7 @@ def _change_class(module: torch.nn.Module, apply_parametrizations: bool) -> None
     module._orig_class = cls
 
 
-def _swap_parameters(module: torch.nn.Module, tensor_name: str, tensor: Tensor, apply_parametrizations: bool):
+def _swap_parameters(module, tensor_name: str, tensor: Tensor, apply_parametrizations: bool) -> None:
     # Changes the module class to get a new __getattr__ dunder method
     # that looks for the reparametrized tensor
     if hasattr(module, "_functional_parameters"):
@@ -50,7 +51,7 @@ def _swap_parameters(module: torch.nn.Module, tensor_name: str, tensor: Tensor, 
         _change_class(module, apply_parametrizations)
 
 
-def _remove_swap(module: torch.nn.Module, name: str):
+def _remove_swap(module, name: str) -> None:
     if hasattr(module, "_orig_class"):
         module.__class__ = module._orig_class
         delattr(module, "_orig_class")
@@ -58,7 +59,7 @@ def _remove_swap(module: torch.nn.Module, name: str):
 
 
 @contextlib.contextmanager
-def reparametrize_module(module: torch.nn.Module, parameters_and_buffers: Dict[str, Tensor], apply_parametrizations: bool):
+def reparametrize_module(module: torch.nn.Module, parameters_and_buffers: Dict[str, Tensor], apply_parametrizations: bool) -> Iterator[None]:
     for name, tensor in parameters_and_buffers.items():
         _apply_func_submodules(
             _swap_parameters,
@@ -71,9 +72,10 @@ def reparametrize_module(module: torch.nn.Module, parameters_and_buffers: Dict[s
 
 
 def _apply_func_submodules(
-    func: Callable[[torch.nn.Module, str, Tuple[Any]], None],
+    func: Callable[..., None],
     module: torch.nn.Module,
-    path: str, args: Tuple[Any],
+    path: List[str],
+    args: Tuple,
 ):
     if len(path) == 1:
         func(module, path[0], *args)
@@ -84,9 +86,9 @@ def _apply_func_submodules(
 def functional_call(
     module: torch.nn.Module,
     parameters_and_buffers: Dict[str, Tensor],
-    args: Tuple[Any],
+    args: Tuple,
     kwargs : Dict[str, Any] = None,
-    *, apply_parametrizations : Optional[bool] = False,
+    *, apply_parametrizations : bool = False,
 ):
     r"""Performs a functional call on the module by replacing the module parameters with
     the provideed ones.
