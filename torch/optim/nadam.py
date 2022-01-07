1678f1bfaf7d8e4b8a33ae84e194f49ac9ef84ad
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-07 23:10:29 +0000

    Update base for Update on "Optim foreach cleanup for ASGD"
    [ghstack-poisoned]

--- torch/optim/nadam.py
+++ torch/optim/nadam.py
@@ -74,9 +74,9 @@ class NAdam(Optimizer):
         super(NAdam, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super().__setstate__(state)
+        super(NAdam, self).__setstate__(state)
         for group in self.param_groups:
-            group.setdefault('foreach', False)
+            group.setdefault('foreach', None)
 
     @torch.no_grad()
     def step(self, closure=None):
