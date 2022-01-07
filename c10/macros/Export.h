b80d59aec78928ea99b4e5294b16a305b94eb161
Michael Andreas Dagitses <mikeyd@fb.com>
2022-01-06 20:06:41 -0800

    Update base for Update on "define //c10/core:CPUAllocator target"
    Differential Revision: [D33330151](https://our.internmc.facebook.com/intern/diff/D33330151/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D33330151/)!
    
    [ghstack-poisoned]

--- c10/macros/Export.h
+++ c10/macros/Export.h
@@ -41,7 +41,11 @@
 // file.
 
 #ifndef C10_USING_CUSTOM_GENERATED_MACROS
-#include <c10/macros/cmake_macros.h>
+// Bazel does not support using system includes for libraries. For
+// non-generated headers we avoid this problem because we add
+// "-I.". Including this private header with quotes allows Bazel to
+// locate this as designed.
+#include "c10/macros/cmake_macros.h"
 #endif // C10_USING_CUSTOM_GENERATED_MACROS
 
 #ifdef _WIN32
