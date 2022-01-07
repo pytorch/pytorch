83bb9033bb1f22044bc29fcb67c4d564b8ee2d61
Animesh Jain <anijain@umich.edu>
2022-01-07 13:30:34 -0800

    [functorch] [Compile Cache] Handle non tensor args (pytorch/functorch#383)
    * [Compile Cache] Handle non tensor args
    
    * Minor test fixes
    
    * Added more tests
    
    * Combining helper functions to reduce cache overhead
    
    * Adding static_numargs
    
    * Handle pytree stuff
    
    * Adding few more tests
    
    * Linter

--- functorch/functorch/csrc/CompileCache.cpp
+++ functorch/functorch/csrc/CompileCache.cpp
@@ -48,7 +48,6 @@ using hash_key_t = std::vector<int64_t>;
 /// dtype, dispatch options, aliasing, and per-dim contiguity/broadcasting
 /// information.
 
-
 enum DimFlags {
   /// A leading dimension implicitly added by broadcasting.
   SIZE_MISSING = 1 << 0,
@@ -84,16 +83,16 @@ std::vector<int> genDimFlags(c10::IntArrayRef sizes, c10::IntArrayRef strides) {
   for (int64_t dim = 0; dim < nDims; ++dim) {
     uint8_t flag =
         (sizes[dim] == 0 ? SIZE_MISSING
-                          : (sizes[dim] == 1 ? SIZE_ONE : SIZE_OTHER));
+                         : (sizes[dim] == 1 ? SIZE_ONE : SIZE_OTHER));
     if (strides[dim] == 0) {
       flag |= STRIDE_ZERO;
     } else if (strides[dim] == 1) {
       flag |= STRIDE_ONE;
     } else if (dim + 1 < (int64_t)sizes.size() &&
-                strides[dim] == strides[dim + 1] * sizes[dim + 1]) {
+               strides[dim] == strides[dim + 1] * sizes[dim + 1]) {
       flag |= STRIDE_CONTIGUOUS;
     } else if (dim > 0 && strides[dim] == strides[dim - 1] * sizes[dim - 1] &&
-                (dimflags[dim - 1] & STRIDE_CONTIGUOUS) == 0) {
+               (dimflags[dim - 1] & STRIDE_CONTIGUOUS) == 0) {
       flag |= STRIDE_TRANSPOSED_CONTIGUOUS;
     } else {
       flag |= STRIDE_AS_ARG;
@@ -104,27 +103,23 @@ std::vector<int> genDimFlags(c10::IntArrayRef sizes, c10::IntArrayRef strides) {
 }
 
 hash_key_t dynamic_hasher(const LocalState &state, const at::Tensor &v) {
-    hash_key_t hash = {
-      0,
-      static_cast<int>(packFlags(state, v)),
-      static_cast<int>(state.apply(v.key_set()).raw_repr()),
-      static_cast<int>(v.ndimension())};
-    auto dimFlags = genDimFlags(v.sizes(), v.strides());
-    hash.insert(hash.end(), dimFlags.begin(), dimFlags.end());
-    return hash;
+  hash_key_t hash = {0, static_cast<int>(packFlags(state, v)),
+                     static_cast<int>(state.apply(v.key_set()).raw_repr()),
+                     static_cast<int>(v.ndimension())};
+  auto dimFlags = genDimFlags(v.sizes(), v.strides());
+  hash.insert(hash.end(), dimFlags.begin(), dimFlags.end());
+  return hash;
 }
 
 /// Per-tensor cache specialization key targetting static shapes. Recordsdtype,
 /// dispatch options, aliasing, and full shapes and strides.
 hash_key_t static_hasher(const LocalState &state, const at::Tensor &v) {
-    hash_key_t hash = {
-      1,
-      static_cast<int>(packFlags(state, v)),
-      static_cast<int>(state.apply(v.key_set()).raw_repr()),
-      static_cast<int>(v.ndimension())};
-    hash.insert(hash.end(), v.sizes().begin(), v.sizes().end());
-    hash.insert(hash.end(), v.strides().begin(), v.strides().end());
-    return hash;
+  hash_key_t hash = {1, static_cast<int>(packFlags(state, v)),
+                     static_cast<int>(state.apply(v.key_set()).raw_repr()),
+                     static_cast<int>(v.ndimension())};
+  hash.insert(hash.end(), v.sizes().begin(), v.sizes().end());
+  hash.insert(hash.end(), v.strides().begin(), v.strides().end());
+  return hash;
 }
 
 /// ArgCompileCache is a templated class allowing plugging of different types of
@@ -139,9 +134,9 @@ public:
   /// Cache type mapping specialization keys to compiled kernels.
   class vector_hasher {
   public:
-    std::size_t operator()(hash_key_t const& vec) const {
+    std::size_t operator()(hash_key_t const &vec) const {
       std::size_t seed = vec.size();
-      for(auto& i : vec) {
+      for (auto &i : vec) {
         seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
       }
       return seed;
@@ -151,39 +146,55 @@ public:
 
   /// Compute the set of specialization keys based on the inputs to
   /// the kernel.
-  hash_key_t computeCacheKey(const std::vector<at::Tensor>& args, int numArgs,
-                              const std::string& hasherType, int64_t id) {
+  hash_key_t computeCacheKey(PyObject *args,
+                             const std::vector<at::Tensor> &tensorArgs,
+                             int numTensorArgs, const std::string &hasherType,
+                             int64_t id) {
     LocalState state;
     hash_key_t cacheKey;
-    for (int i = 0; i < numArgs; ++i) {
+    for (int i = 0; i < numTensorArgs; ++i) {
       if (hasherType == "StaticShapeHasher") {
-        auto res = static_hasher(state, args[i]);
+        auto res = static_hasher(state, tensorArgs[i]);
         cacheKey.insert(cacheKey.end(), res.begin(), res.end());
       } else if (hasherType == "DynamicShapeHasher") {
-        auto res = dynamic_hasher(state, args[i]);
+        auto res = dynamic_hasher(state, tensorArgs[i]);
         cacheKey.insert(cacheKey.end(), res.begin(), res.end());
       }
     }
     cacheKey.push_back(id);
-    cacheKey.push_back(numArgs);
+    cacheKey.push_back(numTensorArgs);
+
+    // Cache the non-tensor args. Currently, all the non-tensor args are cached.
+    for (int i = numTensorArgs; i < PyTuple_Size(args); i++) {
+      PyObject *arg = PyTuple_GET_ITEM(args, i);
+      assert(PyLong_Check(arg));
+      cacheKey.push_back(PyLong_AsLong(arg));
+    }
     return cacheKey;
   }
 
-  std::vector<at::Tensor> parsePythonArgs(int numArgs, PyObject *args) {
+  std::vector<at::Tensor> parsePythonArgs(int numTensorArgs, PyObject *args) {
     // Convert to Tensor Args
-    std::vector<at::Tensor> tensorArgs(numArgs);
-    for (int i = 0; i < numArgs; ++i) {
-      tensorArgs[i] = THPVariable_Unpack(PyTuple_GET_ITEM(args, i));
+    std::vector<at::Tensor> tensorArgs(numTensorArgs);
+    for (int i = 0; i < numTensorArgs; ++i) {
+      PyObject *arg = PyTuple_GET_ITEM(args, i);
+      if (!THPVariable_Check(arg)) {
+        throw std::runtime_error("Encountered a non-tensor argument. Set up "
+                                 "static_argnums correctly.");
+      }
+      tensorArgs[i] = THPVariable_Unpack(arg);
     }
     return tensorArgs;
   }
 
   /// Check if the function has already been compiled.
-  // py::object at(int64_t id, int numArgs, PyObject *args, PyObject *kwargs) {
-  py::object at(int64_t id, int numArgs, const std::string &hasherType,
+  // py::object at(int64_t id, int numTensorArgs, PyObject *args, PyObject
+  // *kwargs) {
+  py::object at(int64_t id, int numTensorArgs, const std::string &hasherType,
                 PyObject *args) {
-    std::vector<at::Tensor> tensorArgs = parsePythonArgs(numArgs, args);
-    hash_key_t cacheKey = computeCacheKey(tensorArgs, numArgs, hasherType, id);
+    std::vector<at::Tensor> tensorArgs = parsePythonArgs(numTensorArgs, args);
+    hash_key_t cacheKey =
+        computeCacheKey(args, tensorArgs, numTensorArgs, hasherType, id);
 
     auto item = cache_.find(cacheKey); // protected by GIL
 
@@ -194,11 +205,12 @@ public:
   }
 
   /// Insert a new compiled functions for new tensor properties.
-  void insert(int64_t id, int numArgs, const std::string &hasherType,
+  void insert(int64_t id, int numTensorArgs, const std::string &hasherType,
               const py::object &compileFn, PyObject *args) {
-    std::vector<at::Tensor> tensorArgs = parsePythonArgs(numArgs, args);
+    std::vector<at::Tensor> tensorArgs = parsePythonArgs(numTensorArgs, args);
     LocalState state;
-    hash_key_t cacheKey= computeCacheKey(tensorArgs, numArgs, hasherType, id);
+    hash_key_t cacheKey =
+        computeCacheKey(args, tensorArgs, numTensorArgs, hasherType, id);
     cache_.emplace(cacheKey, compileFn);
   }
 
@@ -225,15 +237,15 @@ void initCompileCacheBindings(PyObject *module) {
   py::class_<CompileCache>(te, "CompileCache")
       .def(py::init(&createCompileCache))
       .def("at",
-           [](CompileCache &self, int64_t id, int numArgs,
+           [](CompileCache &self, int64_t id, int numTensorArgs,
               const std::string hasherType, py::args args) {
-             return self.at(id, numArgs, hasherType, args.ptr());
+             return self.at(id, numTensorArgs, hasherType, args.ptr());
            })
       .def("insert",
-           [](CompileCache &self, int64_t id, int numArgs,
+           [](CompileCache &self, int64_t id, int numTensorArgs,
               const std::string hasherType, const py::object &compileFn,
               py::args args, py::kwargs kwargs) {
-             self.insert(id, numArgs, hasherType, compileFn, args.ptr());
+             self.insert(id, numTensorArgs, hasherType, compileFn, args.ptr());
            })
       .def("clear", [](CompileCache &self) { self.clear(); })
       .def("size", [](CompileCache &self) { return self.size(); });
