dee577eae8c77e8dec9d08532f78274ffced9166
Jambay Kinley <jambaykinley@microsoft.com>
2022-01-07 19:59:02 -0800

    builder repo corrected

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -61,7 +61,7 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-retry git clone -q https://github.com/pytorch/builder.git -b jambayk/conda-3.6-cpuonly "$BUILDER_ROOT"
+retry git clone -q https://github.com/jambayk/builder.git -b jambayk/conda-3.6-cpuonly "$BUILDER_ROOT"
 pushd "$BUILDER_ROOT"
 echo "Using builder from "
 git --no-pager log --max-count 1
