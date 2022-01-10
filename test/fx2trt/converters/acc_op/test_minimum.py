e2a163541b03281b935f2ceca5ee7f826cbe3520
Shirong Wu <shirong@fb.com>
2022-01-10 10:30:41 -0800

    Fix fx2trt CI test trigger condition (#71014)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71014
    
    Replace test trigger with test_config matching.
    
    Test Plan:
    CI
    https://github.com/pytorch/pytorch/runs/4746717568?check_suite_focus=true
    
    Differential Revision: D33480971
    
    fbshipit-source-id: 2b06d53da4bfcc3bce25af6a5c32318f496b6212

--- test/fx2trt/converters/acc_op/test_minimum.py
+++ test/fx2trt/converters/acc_op/test_minimum.py
@@ -3,6 +3,7 @@
 import torch
 import torch.fx.experimental.fx_acc.acc_ops as acc_ops
 from torch.testing._internal.common_fx2trt import AccTestCase
+from torch.testing._internal.common_utils import run_tests
 
 
 class TestMinimumConverter(AccTestCase):
@@ -29,3 +30,6 @@ class TestMinimumMethodConverter(AccTestCase):
             torch.randn(3, 4),
         ]
         self.run_test(Minimum(), inputs, expected_ops={acc_ops.minimum})
+
+if __name__ == '__main__':
+    run_tests()
