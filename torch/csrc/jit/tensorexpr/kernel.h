b5e40ec30a064d81bca17546f29dbb3c52f2eddc
Mikhail Zolotukhin <mvz@fb.com>
2022-01-11 14:53:02 -0800

    [TensorExpr] Support scalar intermediate and output values.
    So far we've only supported scalar inputs, but couldn't handle scalar outputs
    or intermediates. This PR adds it.
    
    Scalar outputs are returned as 0-dim tensors. If the kernel is invoked on a
    stack of IValues, we correctly convert the results to scalar IValues when
    needed. If the kernel is invoked with a vector of void* pointers, everything
    works out of the box without any conversions.
    
    Lowerings for scalar operators are a bit tricky. Usual lowerings return a pair
    <Buf, Stmt> (aka Tensor), but for scalar operators we also want to have the
    corresponding Var that the lowering function supposedly creates (in theory we
    could just use Loads and Stores, but I'm worried it can affect performance as
    there is no guarantee this will be optimized by LLVM). So, what we do here to
    work around this is we return a fake buf + stmt that sets the corresponding
    var. Then outside of the lowering we create a real buffer and generate a Store
    to it with the value from the variable we passed as the base handle of the fake
    buf. This real buffer is then treated as usual by the rest of the system and we
    can use it if we need to return this scalar value as a kernel output. If we do
    not need to return it, then the Store will be deleted by the DCE pass.
    
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/kernel.h
+++ torch/csrc/jit/tensorexpr/kernel.h
@@ -251,6 +251,7 @@ class TORCH_API TensorExprKernel {
   std::vector<CodeGen::BufferArg> bufferArgs_;
   std::vector<std::vector<int64_t>> tensorOutputSizes_;
   std::vector<std::vector<int64_t>> tensorOutputStrides_;
+  std::vector<bool> isOutputScalar_;
   std::vector<UnpackedTensorOptions> tensorOutputTensorOptions_;
   std::unordered_set<BufPtr> bufOutputs_;
   std::unordered_map<const torch::jit::Value*, BufPtr> bufs_;
