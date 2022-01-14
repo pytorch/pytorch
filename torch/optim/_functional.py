583dd81453c110b213f79a890379e9ae28ded39c
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-14 22:17:33 +0000

    [optim] NAdam fold state updates into functional and pass list of vars rather than states
    [ghstack-poisoned]

--- torch/optim/_functional.py
+++ torch/optim/_functional.py
@@ -373,13 +373,16 @@ def adamax(params: List[Tensor],
 def asgd(params: List[Tensor],
          grads: List[Tensor],
          axs: List[Tensor],
-         mus: List[float],
-         etas: List[float],
+         mus: List[Tensor],
+         etas: List[Tensor],
+         state_steps: List[Tensor],
          *,
-         weight_decay: float,
-         lambd: float):
+         lambd: float,
+         lr: float,
+         t0: float,
+         alpha: float,
+         weight_decay: float):
     r"""Functional API that performs asgd algorithm computation.
-
     See :class:`~torch.optim.ASGD` for details.
     """
 
@@ -388,21 +391,31 @@ def asgd(params: List[Tensor],
         mu = mus[i]
         ax = axs[i]
         eta = etas[i]
+        step_t = state_steps[i]
+
+        # update step
+        step_t += 1
+        step = step_t.item()
 
         if weight_decay != 0:
             grad = grad.add(param, alpha=weight_decay)
 
         # decay term
-        param.mul_(1 - lambd * eta)
+        param.mul_(1 - lambd * eta.item())
 
         # update parameter
-        param.add_(grad, alpha=-eta)
+        param.add_(grad, alpha=-eta.item())
 
         # averaging
-        if mu != 1:
+        if mu.item() != 1:
             ax.add_(param.sub(ax).mul(mu))
         else:
             ax.copy_(param)
+
+        new_eta = torch.tensor(lr / math.pow((1 + lambd * lr * step), alpha))
+        eta.copy_(new_eta)
+        new_mu = torch.tensor(1 / max(1, step - t0))
+        mu.copy_(new_mu)
 
 
 def nadam(params: List[Tensor],
