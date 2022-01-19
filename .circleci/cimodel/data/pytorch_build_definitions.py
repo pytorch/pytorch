7332778f76e50458b2a5d1096129493c8b811943
Jambay Kinley <jambaykinley@microsoft.com>
2022-01-19 12:04:10 -0800

    return to large

--- .circleci/cimodel/data/pytorch_build_definitions.py
+++ .circleci/cimodel/data/pytorch_build_definitions.py
@@ -112,7 +112,6 @@ class Conf:
             parameters["resource_class"] = "xlarge"
         if hasattr(self, 'filters'):
             parameters['filters'] = self.filters
-        parameters["resource_class"] = "xlarge"
         return parameters
 
     def gen_workflow_job(self, phase):
