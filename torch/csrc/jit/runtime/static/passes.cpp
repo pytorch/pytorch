923e92049af49a3935dc5ee5fa44311174de260a
mikeiovine <mikeiovine@devvm4121.frc0.facebook.com>
2022-01-19 14:21:05 -0800

    [SR] Make fused_gather_ranges_to_dense work on graph outputs
    We previously disabled `FuseListUnpack` if the fused outputs of the op would alias the graph outputs. The concern was that some ops were assuming that `p_node->Output(0).isTensor()` implies `p_node->Output(i).isTensor()` for all `i > 0`. This condition can be violated if there exists both managed and unmanaged tensors in the output list.
    
    Instead of adding this special case and missing out on some fusions, we should implement fused ops correctly.
    
    Differential Revision: [D33663969](https://our.internmc.facebook.com/intern/diff/D33663969/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D33663969/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/static/passes.cpp
+++ torch/csrc/jit/runtime/static/passes.cpp
@@ -770,13 +770,15 @@ void FuseListUnpack(std::shared_ptr<torch::jit::Graph>& graph) {
       continue;
     }
 
-    const bool is_equally_split =
-        node->kind() == fromQualString("fb::equally_split");
-    if (!is_equally_split) {
+    const bool checks_all_outputs =
+        node->kind() == fromQualString("fb::equally_split") ||
+        node->kind() == fromQualString("fb::gather_ranges_to_dense") ||
+        node->kind() == fromQualString("fb::gather_ranges_to_dense_v2");
+
+    if (!checks_all_outputs) {
       // If any output of the ListUnpack node is unmanaged, disable fusion
       // since the fused op assumes all outputs are either managed or not.
-      // "fb::equally_split" is excluded here since it does doublecheck
-      // individual outputs without having this assumption.
+      // Ops excluded here check all outputs.
       const std::vector<Value*> list_unpack_outputs_vec(
           list_unpack_outputs.begin(), list_unpack_outputs.end());
       if (alias_db.mayContainAlias(list_unpack_outputs_vec, graph_outputs)) {
