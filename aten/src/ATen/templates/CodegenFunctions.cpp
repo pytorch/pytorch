ece6f0491b007f195b8fac9d940f1ba6d596ff0d
Mengwei Liu <larryliu@fb.com>
2022-01-20 19:45:14 -0800

    [PyTorch] Add codegen unboxing ability (#69881)
    Summary:
    This PR (re)introduces python codegen for unboxing wrappers. Given an entry of `native_functions.yaml` the codegen should be able to generate the corresponding C++ code to convert ivalues from the stack to their proper types. To trigger the codegen, run
    ```
    tools/jit/gen_unboxing.py -d cg/torch/share/ATen --enable_codegen_unboxing
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69881
    
    Test Plan:
    See code is generated using
    ```
    tools/jit/gen_unboxing.py -d cg/torch/share/ATen --enable_codegen_unboxing
    ```
    Will add unit test and CI in later diffs.
    
    Differential Revision: D33692299
    
    Pulled By: larryliu0820
    
    fbshipit-source-id: 731a50befa34a1fd1533ed9b6f518adbc41f71cd

---
+++ aten/src/ATen/templates/CodegenFunctions.cpp
@@ -0,0 +1,40 @@
+#include <ATen/CodegenFunctions.h>
+#include <ATen/Functions.h>
+
+#include <ATen/Tensor.h>
+#include <ATen/core/functional.h>
+#include <ATen/core/interned_strings.h>
+#include <ATen/core/ivalue.h>
+#include <ATen/core/stack.h>
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstring>
+#include <sstream>
+#include <stdexcept>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+namespace at {
+namespace unboxing {
+using at::Scalar;
+using at::ScalarType;
+using at::Tensor;
+using at::TensorOptions;
+using at::DeviceGuard;
+
+using ::c10::fmap;
+using ::c10::filter;
+using torch::jit::peek;
+using torch::jit::drop;
+using torch::jit::pack;
+using torch::jit::pop;
+
+// Generated function declaration
+${definitions}
+
+} // namespace unboxing
+} // namespace at
