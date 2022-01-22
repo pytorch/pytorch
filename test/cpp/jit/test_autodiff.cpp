3d77e5f165eeed0ae22a832264ffc4d36c33565e
David Berard <dberard@fb.com>
2022-01-22 09:15:03 -0800

    Update on "[JIT] Propagate requires_grad to autodiff subgraphs"
    When JIT autodiff is constructing a gradient computation graph, it will only add gradients for tensors that require_grad. Previously, require_grad information was **not** propagated to the subgraph that autodiff used; as a result, autodiff would calculate *all* gradients, even if requires_grad had never been set during profiling runs. In certain cases, this can lead to performance issues. For example, during training, the gradient of the input data is not needed, but is still computed.
    
    This propagates requires_grad to the subgraph passed into autodiff, so that autodiff will not compute unnecessary gradients.
    
    Test: `./bin/test_jit --gtest_filter="AutodiffRemoveUnusedGradientsTest.Linear"`
    
    Differential Revision: [D33725304](https://our.internmc.facebook.com/intern/diff/D33725304)
    
    [ghstack-poisoned]

--- test/cpp/jit/test_autodiff.cpp
+++ test/cpp/jit/test_autodiff.cpp
@@ -13,6 +13,9 @@
 #include "torch/csrc/jit/passes/utils/subgraph_utils.h"
 #include "torch/csrc/jit/runtime/argument_spec.h"
 #include "torch/csrc/jit/runtime/autodiff.h"
+#include "torch/csrc/jit/runtime/graph_iterator.h"
+#include "torch/csrc/jit/runtime/profiling_graph_executor_impl.h"
+#include "torch/torch.h"
 
 #include <ATen/ATen.h>
 #include "torch/csrc/autograd/engine.h"
@@ -279,6 +282,74 @@ TEST(AutodiffTest, DifferentiateWithRequiresGrad) {
   testing::FileCheck()
       .check_count("prim::GradOf[name=\"aten::mul\"]", 1, /*exactly*/ true)
       ->run(*grad_spec.df);
+}
+
+class AutodiffRemoveUnusedGradientsTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    prev_exec = getExecutorMode();
+    getExecutorMode() = true;
+    prev_profiling = getProfilingMode();
+    getProfilingMode() = true;
+    prev_inline_autodiff = getAutodiffSubgraphInlining();
+    debugSetAutodiffSubgraphInlining(false);
+  }
+  void TearDown() override {
+    getExecutorMode() = prev_exec;
+    getProfilingMode() = prev_profiling;
+    debugSetAutodiffSubgraphInlining(prev_inline_autodiff);
+  }
+
+  bool prev_exec;
+  bool prev_profiling;
+  bool prev_inline_autodiff;
+};
+
+TEST_F(AutodiffRemoveUnusedGradientsTest, Linear) {
+  auto graph = std::make_shared<Graph>();
+  const std::string input =
+      R"IR(
+graph(%inp.1 : Tensor,
+      %weight.1 : Tensor,
+      %bias.1 : Tensor):
+  %6 : Tensor = aten::linear(%inp.1, %weight.1, %bias.1)
+  return (%6))IR";
+  parseIR(input, graph.get());
+
+  auto inp = torch::randn({10, 10}).requires_grad_(false);
+  auto weight = torch::randn({10, 10}).requires_grad_(true);
+  auto bias = torch::randn({1, 10}).requires_grad_(true);
+  auto stack = createStack({inp, weight, bias});
+
+  ProfilingGraphExecutorImpl executor(graph, "linear");
+
+  // initial run to profile requires_grad information
+  auto plan = executor.getPlanFor(stack, 20);
+  InterpreterState is{plan.code};
+  is.run(stack);
+
+  auto optimized_plan = executor.getPlanFor(stack, 20);
+  DepthFirstGraphNodeIterator it(optimized_plan.graph);
+  Node* diff_graph_node = nullptr;
+
+  while ((diff_graph_node = it.next()) != nullptr) {
+    if (diff_graph_node->kind() == prim::DifferentiableGraph) {
+      break;
+    }
+  }
+  ASSERT_NE(nullptr, diff_graph_node);
+
+  auto backward_graph = diff_graph_node->g(attr::ReverseSubgraph);
+
+  // "input" does not need gradient, so it shouldn't be in the backwards graph
+  testing::FileCheck()
+      .check_not("%grad_input")
+      ->check_not("%grad_inp")
+      ->run(*backward_graph);
+
+  // "bias" and "weight" need gradients
+  testing::FileCheck().check("%grad_bias")->run(*backward_graph);
+  testing::FileCheck().check("%grad_weight")->run(*backward_graph);
 }
 
 } // namespace jit
