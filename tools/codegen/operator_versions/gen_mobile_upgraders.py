71a2657b1deb916e0b6172169e47ab398c808b92
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2022-01-23 12:34:54 -0800

    Update on "Move bytecode generation to python"
    This PR moves the bytecode generation logic for mobile from C++ to python. Previously, the upgraders_entry.cpp file would need a reference to methods in `export_module.cpp `and `export_bytecode.cpp`. Therefore, they needed to be linked in the same library to work correctly. This required changing how we use `build_variables.bzl` and broke internal tests due to many different targets using different subset of files. Therefore, it is safer if we just move everything into python.
    Differential Revision: [D33730791](https://our.internmc.facebook.com/intern/diff/D33730791)
    
    [ghstack-poisoned]

--- tools/codegen/operator_versions/gen_mobile_upgraders.py
+++ tools/codegen/operator_versions/gen_mobile_upgraders.py
@@ -341,7 +341,7 @@ def sort_upgrader(upgrader_list: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
 
 def main() -> None:
 
-    upgrader_list = generate_bytecode()
+    upgrader_list = generate_upgraders_bytecode()
     sorted_upgrader_list = sort_upgrader(upgrader_list)
     for up in sorted_upgrader_list:
         print("after sort upgrader : ", next(iter(up)))
--- torch/jit/generate_bytecode.py
+++ torch/jit/generate_bytecode.py
@@ -19,7 +19,7 @@ def format_bytecode(table):
 def generate_upgraders_bytecode() -> List:
     yaml_content = []
     upgraders_graph_map = _generate_upgraders_graph()
-    for (upgrader_name, upgrader_graph) in upgraders_graph_map:
+    for upgrader_name, upgrader_graph in upgraders_graph_map.items():
         bytecode_table = _compile_graph_to_code_table(upgrader_name, upgrader_graph)
         entry = {upgrader_name: format_bytecode(bytecode_table)}
         yaml_content.append(entry)
