7d58b852e5109e076939b450b15004a68adf7372
Kim Juhyeong <nonconvexopt@gmail.com>
2022-01-24 23:50:23 +0900

    Focus on full rank case for now
    This reverts commit f35a85f3a37e6cf401470ac50ec246a5bb7e3a3b.

--- torch/distributions/inverse_wishart.py
+++ torch/distributions/inverse_wishart.py
@@ -48,12 +48,12 @@ class InverseWishart(ExponentialFamily):
         covariance matrix :math:`\Psi^{-1}`
     """
     arg_constraints = {
-        'covariance_matrix': constraints.positive_semidefinite,
-        'precision_matrix': constraints.positive_semidefinite,
+        'covariance_matrix': constraints.positive_definite,
+        'precision_matrix': constraints.positive_definite,
         'scale_tril': constraints.lower_cholesky,
         'df': constraints.greater_than(0),
     }
-    support = constraints.positive_semidefinite
+    support = constraints.positive_definite
     has_rsample = True
     _mean_carrier_measure = 0
 
@@ -221,11 +221,47 @@ class InverseWishart(ExponentialFamily):
             In some cases, sampling algorithn based on Bartlett decomposition may return singular matrix samples.
             Several tries to correct singular samples are performed by default, but it may end up returning
             singular matrix samples. Sigular samples may return `-inf` values in `.log_prob()`.
-            In those cases, the user should validate the samples and either fix the value of `df` accordingly.
+            In those cases, the user should validate the samples and either fix the value of `df`
+            or adjust `max_try_correction` value for argument in `.rsample` accordingly.
         """
+
+        if max_try_correction is None:
+            max_try_correction = 3 if torch._C._get_tracing_state() else 10
 
         sample_shape = torch.Size(sample_shape)
         sample = self._bartlett_sampling(sample_shape)
+
+        # Below part is to improve numerical stability temporally and should be removed in the future
+        is_singular = self.support.check(sample)
+        if self._batch_shape:
+            is_singular = is_singular.amax(self._batch_dims)
+
+        if torch._C._get_tracing_state():
+            # Less optimized version for JIT
+            for _ in range(max_try_correction):
+                sample_new = self._bartlett_sampling(sample_shape)
+                sample = torch.where(is_singular, sample_new, sample)
+
+                is_singular = ~self.support.check(sample)
+                if self._batch_shape:
+                    is_singular = is_singular.amax(self._batch_dims)
+
+        else:
+            # More optimized version with data-dependent control flow.
+            if is_singular.any():
+                warnings.warn("Singular sample detected.")
+
+                for _ in range(max_try_correction):
+                    sample_new = self._bartlett_sampling(is_singular[is_singular].shape)
+                    sample[is_singular] = sample_new
+
+                    is_singular_new = ~self.support.check(sample_new)
+                    if self._batch_shape:
+                        is_singular_new = is_singular_new.amax(self._batch_dims)
+                    is_singular[is_singular.clone()] = is_singular_new
+
+                    if not is_singular.any():
+                        break
 
         return sample
 
