2ffb5a971902b1d47ef5cd1a4c0ca2e30b9652a6
vfdev <vfdev.5@gmail.com>
2022-01-24 17:47:12 +0100

    [functorch] Fixes printing issue with TensorWrapper (pytorch/functorch#397)
    * Fixes printing issue with TensorWrapper
    Description:
    - Hacky fix to fix printing tensors inside `grad`
    
    Fixes pytorch/functorch#382
    
    * Applied comments from the review and updated test
    
    * Added printing test from the issue
    
    * Updated tests
    
    * Added dtypes and bdim

--- functorch/functorch/__init__.py
+++ functorch/functorch/__init__.py
@@ -91,7 +91,17 @@ def _functorch_str(tensor):
         return _old_str(tensor)
 
     value = _C.get_unwrapped(tensor)
-    value_repr = repr(value)
+    dl_enabled = _C.tls_set_is_included()
+    try:
+        # Disable temporarily kDynamicLayerFrontModeKey/kDynamicLayerBackModeKey as included dispatch keys
+        if (dl_enabled):
+            _C._set_dynamic_layer_keys_included(False)
+        value_repr = repr(value)
+    finally:
+        # Reenable kDynamicLayerFrontModeKey/kDynamicLayerBackModeKey as included dispatch keys
+        if (dl_enabled):
+            _C._set_dynamic_layer_keys_included(True)
+
     if _C.is_batchedtensor(tensor):
         bdim = _C.maybe_get_bdim(tensor)
         assert bdim != -1
