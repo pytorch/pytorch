782649a48cbb58d0333b92a2a305175e657c3579
pritam <pritam.damania@fb.com>
2022-01-24 16:41:53 -0800

    Create torch.distributed.shard package.
    We have many sharding components currently:
    torch.distributed._sharded_tensor, torch.distributed._sharding_spec,
    torch.distributed._sharded_optimizer and more coming.
    
    As a result, organizing all of this under the `torch.distributed.shard`
    package. For BC reasons, I'm still keeping the old packages and have them just
    reference the new package.
    
    Differential Revision: [D33755913](https://our.internmc.facebook.com/intern/diff/D33755913/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D33755913/)!
    
    [ghstack-poisoned]

--- torch/distributed/_sharding_spec/__init__.py
+++ torch/distributed/_sharding_spec/__init__.py
@@ -1,8 +1,10 @@
-from .api import (
-    ChunkShardingSpec,
-    DevicePlacementSpec,
-    EnumerableShardingSpec,
-    PlacementSpec,
-    ShardMetadata,
-    ShardingSpec,
+# Keep old package for BC purposes, this file should be removed once
+# everything moves to the `torch.distributed.shard` package.
+import sys
+import torch.distributed.shard.sharding_spec
+import warnings
+warnings.warn(
+    "torch.distributed._sharding_spec will be deprecated, use torch.distributed.shard.sharding_spec instead",
+    DeprecationWarning
 )
+sys.modules['torch.distributed._sharding_spec'] = torch.distributed.shard.sharding_spec
