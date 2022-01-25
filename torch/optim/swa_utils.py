1eacc624020c856b6f1380b67297087a19dbb1fe
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2022-01-25 15:46:14 +0000

    Update the documentation

--- torch/optim/swa_utils.py
+++ torch/optim/swa_utils.py
@@ -28,11 +28,8 @@ class AveragedModel(Module):
             :class:`AveragedModel` parameter, the current value of :attr:`model`
             parameter and the number of models already averaged; if None,
             equally weighted average is used (default: None)
-        use_buffers (bool, optional): If the model has layers that contain
-            buffers(like BN), passing `use_buffers=True` would use buffers
-            along with parameters to perform the update. This has shown to
-            increase accuracy in models with layers that contain buffers.
-            (default: ``False``)
+        use_buffers (bool): if ``True``, it will compute running averages for
+            both the parameters and the buffers of the model. (default: ``False``)
 
     Example:
         >>> loader, optimizer, model, loss_fn = ...
@@ -60,15 +57,21 @@ class AveragedModel(Module):
     equally-weighted average of the weights.
 
     Example:
-        >>> # Compute exponential moving averages of the weights
+        >>> # Compute exponential moving averages of the weights and buffers
         >>> ema_avg = lambda averaged_model_parameter, model_parameter, num_averaged:\
                             0.1 * averaged_model_parameter + 0.9 * model_parameter
-        >>> swa_model = torch.optim.swa_utils.AveragedModel(model, avg_fn=ema_avg)
+        >>> swa_model = torch.optim.swa_utils.AveragedModel(model, avg_fn=ema_avg, use_buffers=True)
 
     .. note::
         When using SWA with models containing Batch Normalization you may
         need to update the activation statistics for Batch Normalization.
-        You can do so by using :meth:`torch.optim.swa_utils.update_bn` utility.
+        This can be done either by using the :meth:`torch.optim.swa_utils.update_bn`
+        or by setting :attr:`use_buffers` to `True`. The first approach updates the 
+        statistics in a post-training step by passing data through the model. The 
+        second does it during the parameter update phase by averaging all buffers.
+        Empirical evidence has shown that updating the statistics in normalization 
+        layers increases accuracy, but you may wish to empirically test which
+        approach yields the best results in your problem.
 
     .. note::
         :attr:`avg_fn` is not saved in the :meth:`state_dict` of the model.
@@ -103,7 +106,7 @@ class AveragedModel(Module):
                 return averaged_model_parameter + \
                     (model_parameter - averaged_model_parameter) / (num_averaged + 1)
         self.avg_fn = avg_fn
-        self._use_buffers = use_buffers
+        self.use_buffers = use_buffers
 
     def forward(self, *args, **kwargs):
         return self.module(*args, **kwargs)
@@ -111,11 +114,11 @@ class AveragedModel(Module):
     def update_parameters(self, model):
         self_param = (
             itertools.chain(self.module.parameters(), self.module.buffers())
-            if self._use_buffers else self.parameters()
+            if self.use_buffers else self.parameters()
         )
         model_param = (
             itertools.chain(model.parameters(), model.buffers())
-            if self._use_buffers else model.parameters()
+            if self.use_buffers else model.parameters()
         )
         for p_swa, p_model in zip(self_param, model_param):
             device = p_swa.device
