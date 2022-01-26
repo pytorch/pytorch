eec4c63877a26924ce53c4b82fdb85629166e514
Jeff Daily <jeff.daily@amd.com>
2022-01-26 08:16:19 -0800

    add div_zerotensor to div.Tensor(Tensor self, Tensor other) -> Tensor

--- aten/src/ATen/native/BinaryOps.cpp
+++ aten/src/ATen/native/BinaryOps.cpp
@@ -642,6 +642,36 @@ Tensor mul_zerotensor(const Tensor& self, const Tensor& other) {
   return at::_efficientzerotensor(meta_out.sizes(), meta_out.options().device(out_device));
 }
 
+Tensor div_zerotensor(const Tensor& self, const Tensor& other) {
+  TORCH_INTERNAL_ASSERT(self._is_zerotensor() || other._is_zerotensor());
+
+  auto out_device = correct_out_device(self, other);
+  // hack to use the TensorIterator to get the correct broadcasting and type promotion logic
+  auto device_ = Device(DeviceType::Meta);
+  auto meta_out = at::redispatch::div(c10::DispatchKeySet(at::DispatchKey::Meta), self.to(device_), other.to(device_));
+
+  if (self._is_zerotensor()) {
+    if (other._is_zerotensor()) {
+      // 0/0, return full NAN
+      return at::full(meta_out.sizes(), std::numeric_limits<float>::quiet_NaN(), meta_out.options().device(out_device));
+    }
+    else {
+      // 0/x, return zero tensor
+      return at::_efficientzerotensor(meta_out.sizes(), meta_out.options().device(out_device));
+    }
+  }
+  else {
+    if (other._is_zerotensor()) {
+      // x/0, return full INF
+      return at::full(meta_out.sizes(), std::numeric_limits<float>::infinity(), meta_out.options().device(out_device));
+    }
+    else {
+      // x/y -- unreachable, see TORCH_INTERNAL_ASSERT above
+      return at::_efficientzerotensor(meta_out.sizes(), meta_out.options().device(out_device));
+    }
+  }
+}
+
 Tensor add_zerotensor(const Tensor& self, const Tensor& other, const Scalar& alpha) {
   auto out_device = correct_out_device(self, other);
   // hack to use the TensorIterator to get the correct broadcasting and type promotion logic
