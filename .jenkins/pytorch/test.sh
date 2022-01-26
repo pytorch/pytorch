3d7fcf683308a08bf3fa7a94625d399cd0d6f190
Wonjoo Lee <wonjoo@google.com>
2022-01-26 11:02:21 -0800

    Update jenkins tesh.sh script

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -430,11 +430,15 @@ build_xla() {
 test_xla() {
   clone_pytorch_xla
   # shellcheck disable=SC1091
-  source "./xla/.circleci/common.sh"
-  SITE_PACKAGES="$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
-  CMAKE_PREFIX_PATH="${SITE_PACKAGES}/torch:${CMAKE_PREFIX_PATH}" run_torch_xla_tests "$(pwd)" "$(pwd)/xla"
-  XLA_ARTIFACT_PATH="${CUSTOM_TEST_ARTIFACT_BUILD_DIR}"
-  cp -a "$XLA_ARTIFACT_PATH" "$(pwd)/xla/tmp/pytorch_py_test.log"
+  source "./xla/.circleci/common.sh" || true
+  SITE_PACKAGES="$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')" || true
+  CMAKE_PREFIX_PATH="${SITE_PACKAGES}/torch:${CMAKE_PREFIX_PATH}" run_torch_xla_tests "$(pwd)" "$(pwd)/xla" || true
+  XLA_ARTIFACT_PATH="${CUSTOM_TEST_ARTIFACT_BUILD_DIR}" || true
+  cp -a "$(pwd)/xla/tmp/pytorch_py_test.log" "$XLA_ARTIFACT_PATH" || true
+  cp -a "$(pwd)/xla/tmp/pytorch_py_test.log" "$XLA_ATRIFACT_PATH/custom-backend-build" || true
+  cp -a "$(pwd)/xla/tmp/pytorch_py_test.log" "$XLA_ATRIFACT_PATH/custom-op-build" || true
+  cp -a "$(pwd)/xla/tmp/pytorch_py_test.log" "$XLA_ATRIFACT_PATH/jit-hook-build" || true
+  cat "$(pwd)/xla/tmp/pytorch_py_test.log" || true
   assert_git_not_dirty
 }
 
