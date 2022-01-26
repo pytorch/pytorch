7b02a5e65495cf9bcf74bfe0edee6fff3ce338a7
Wonjoo Lee <wonjoo@google.com>
2022-01-26 01:05:08 -0800

    Fix linter issue

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -434,7 +434,7 @@ test_xla() {
   SITE_PACKAGES="$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
   CMAKE_PREFIX_PATH="${SITE_PACKAGES}/torch:${CMAKE_PREFIX_PATH}" run_torch_xla_tests "$(pwd)" "$(pwd)/xla"
   XLA_ARTIFACT_PATH="${CUSTOM_TEST_ARTIFACT_BUILD_DIR}"
-  cp -a "$XLA_ARTIFACT_PATH" $(pwd)/xla/tmp/pytorch_py_test.log
+  cp -a "$XLA_ARTIFACT_PATH" "$(pwd)/xla/tmp/pytorch_py_test.log"
   assert_git_not_dirty
 }
 
