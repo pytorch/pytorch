ceb86639ccb98487ffa2c4a12a58f6b6cbfc07fc
Alex Hagiopol <hagiopol@fb.com>
2022-01-26 13:52:31 -0800

    refactor test_vulkan.py for modularity, move vulkan tests to test_vulkan.py

--- test/jit/test_graph_rewrite_passes.py
+++ test/jit/test_graph_rewrite_passes.py
@@ -5,8 +5,6 @@ import torch
 import torch._C
 from torch.testing import FileCheck
 from typing import Callable, Dict
-from os import environ
-import unittest
 
 class FunctionalConv2d(torch.nn.Module):
     def __init__(self, weight: torch.Tensor, bias: torch.Tensor) -> None:
@@ -92,61 +90,3 @@ class TestGraphRewritePasses(JitTestCase):
         self.check_single_replacement("aten::matmul", "aten::matmul", torch._C._jit_pass_fuse_linear, model_3)
         self.check_op_presence(check_pattern_count_map_3, torch._C._jit_pass_fuse_linear, model_3)
         model_3(x_3)  # make sure it runs
-
-    @unittest.skipUnless(environ.get("USE_VULKAN") == "1", "Test depends on Vulkan installation.")
-    def test_vulkan_insert_pre_packed_ops(self) -> None:
-        x_1 = torch.rand(3)
-        w_1 = torch.rand(5, 3)
-        b_1 = torch.rand(5)
-        model_1 = torch.jit.trace(FunctionalLinear(w_1, b_1), [x_1])
-        check_pattern_count_map_1 = {"aten::matmul": 0, "aten::add_": 0, "aten::t": 0}
-        self.check_single_replacement(
-            "aten::matmul",
-            "vulkan_prepack::linear_run",
-            torch._C._jit_pass_vulkan_insert_prepacked_ops,
-            model_1
-        )
-        self.check_op_presence(check_pattern_count_map_1, torch._C._jit_pass_vulkan_insert_prepacked_ops, model_1)
-        model_1(x_1)  # make sure it runs
-
-        conv2d_in_channels = 3
-        conv2d_out_channels = 4
-        conv2d_kernel = 2
-        conv2d_weight = torch.rand(
-            conv2d_out_channels,
-            conv2d_in_channels,
-            conv2d_kernel,
-            conv2d_kernel,
-        )
-        conv2d_bias = torch.rand(conv2d_out_channels)
-        x_2_shape = (3, 2, 5)
-        x_2 = torch.rand(x_2_shape)
-
-        model_2 = torch.jit.trace(FunctionalConv2d(conv2d_weight, conv2d_bias), [x_2])
-        check_pattern_count_map_2 = {
-            "aten::_convolution": 0,
-            "vulkan_prepack::conv2d_clamp_run": 1,
-            "vulkan_prepack::conv2d_clamp_prepack": 1
-        }
-        self.check_single_replacement(
-            "aten::_convolution",
-            "prim::Constant",
-            torch._C._jit_pass_vulkan_insert_prepacked_ops,
-            model_2
-        )
-        self.check_op_presence(
-            check_pattern_count_map_2,
-            torch._C._jit_pass_vulkan_insert_prepacked_ops,
-            model_2
-        )
-        model_2(x_2)  # make sure it runs
-
-        # this test fails!
-        model_3 = torch.jit.script(FunctionalConv2d(conv2d_weight, conv2d_bias), [x_2])
-        self.check_single_replacement(
-            "aten::conv2d",
-            "vulkan_prepack::conv2d_clamp_run",
-            torch._C._jit_pass_vulkan_insert_prepacked_ops,
-            model_3
-        )
-        model_3(x_2)  # make sure it runs
--- test/test_vulkan.py
+++ test/test_vulkan.py
@@ -7,23 +7,91 @@ from torch.nn import functional as F
 from torch.testing._internal.common_utils import TestCase, run_tests
 from torch.testing import FileCheck
 import io
+from typing import Dict, List, Tuple
 
-@unittest.skipUnless(torch.is_vulkan_available(),
-                     "Vulkan backend must be available for these tests.")
+class ExampleConv2D(torch.nn.Module):
+    def __init__(self):
+        # convolutiopn parameters for unit tests
+        self.batch_size = 2
+        self.input_channels_per_group = 6
+        self.height = 16
+        self.width = 16
+        self.output_channels_per_group = 6
+        self.groups = 4
+        self.kernel_h = 3
+        self.kernel_w = self.kernel_h
+        self.stride_h = 1
+        self.stride_w = self.stride_h
+        self.pad_h = 1
+        self.pad_w = self.pad_h
+        self.dilation = 1
+        self.input_channels = self.input_channels_per_group * self.groups
+        self.output_channels = self.output_channels_per_group * self.groups
+        self.kernels = (self.kernel_h, self.kernel_w)
+        self.strides = (self.stride_h, self.stride_w)
+        self.paddings = (self.pad_h, self.pad_w)
+        self.dilations = (self.dilation, self.dilation)
+        self.conv_weight_shape = (self.output_channels, self.input_channels_per_group, self.kernel_h, self.kernel_w)
+        self.conv_bias_shape = (self.output_channels)
+        super(ExampleConv2D, self).__init__()
+        self.weight = torch.nn.Parameter(torch.rand(self.conv_weight_shape), requires_grad=False)
+        self.bias = torch.nn.Parameter(torch.rand(self.conv_bias_shape), requires_grad=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return F.conv2d(x, self.weight, self.bias, self.strides, self.paddings, self.dilations, self.groups)
+
+class ExampleConv2DRelu(ExampleConv2D):
+    def __init__(self):
+        super(ExampleConv2DRelu, self).__init__()
+        self.weight = torch.nn.Parameter(torch.rand(self.conv_weight_shape), requires_grad=False)
+        self.bias = torch.nn.Parameter(torch.rand(self.conv_bias_shape), requires_grad=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        o = F.conv2d(x, self.weight, self.bias, self.strides, self.paddings, self.dilations, self.groups)
+        o = F.relu(o)
+        return o
+
+class ExampleConv2DHardtanh(ExampleConv2D):
+    def __init__(self):
+        super(ExampleConv2DHardtanh, self).__init__()
+        self.weight = torch.nn.Parameter(torch.rand(self.conv_weight_shape), requires_grad=False)
+        self.bias = torch.nn.Parameter(torch.rand(self.conv_bias_shape), requires_grad=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        o = F.conv2d(x, self.weight, self.bias, self.strides, self.paddings, self.dilations, self.groups)
+        o = F.hardtanh(o)
+        return o
+
+@unittest.skipUnless(torch.is_vulkan_available(), "Vulkan backend must be available for these tests.")
 class TestVulkanRewritePass(TestCase):
-    @staticmethod
+    def get_source_range(self, scripted_model, kind) -> str:
+        for node in scripted_model.graph.nodes():
+            if node.kind() == kind:
+                source_range = node.sourceRange()
+        return source_range
+
     def validate_transformed_module(
-            # To please flake
             self,
-            pattern_count_map,
-            data_shape,
-            prepack_removal=False,
-            fuse_clamping_ops=False):
-        module_instance = self
-        scripted_model = torch.jit.script(module_instance)
+            model: torch.nn.Module,
+            pattern_count_map: Dict[str, int],
+            data_shape: Tuple[int, int, int, int],
+            old_node_kinds: List[str] = None,
+            new_node_kinds: List[str] = None,
+            prepack_removal: bool = False,
+            fuse_clamping_ops: bool = False) -> None:
+        scripted_model = torch.jit.script(model)
+        # store old source ranges
+        if old_node_kinds:
+            old_source_ranges = len(old_node_kinds) * [None]
+            new_source_ranges = len(new_node_kinds) * [None]
+            self.assertTrue(len(old_source_ranges) == len(new_source_ranges) == len(old_node_kinds) == len(new_node_kinds))
+            for i in range(len(old_node_kinds)):
+                old_source_ranges[i] = self.get_source_range(scripted_model, old_node_kinds[i])
+        # evaluate non-rewritten model
         scripted_model.eval()
         input_data = torch.normal(1, 20, size=data_shape)
         ref_result = scripted_model(input_data)
+        # rewrite model
         torch._C._jit_pass_vulkan_insert_prepacked_ops(scripted_model._c)
         if fuse_clamping_ops or prepack_removal:
             scripted_model._c = torch._C._freeze_module(scripted_model._c)
@@ -31,130 +99,71 @@ class TestVulkanRewritePass(TestCase):
             torch._C._jit_pass_vulkan_fuse_clamp_w_prepacked_conv(scripted_model._c)
         if prepack_removal:
             torch._C._jit_pass_vulkan_fold_prepacking_ops(scripted_model._c)
-
+        # validate source ranges
+        if old_node_kinds:
+            for i in range(len(old_node_kinds)):
+                new_source_ranges[i] = self.get_source_range(scripted_model, new_node_kinds[i])
+            for i in range(len(old_node_kinds)):
+                self.assertTrue(old_source_ranges[i] == new_source_ranges[i])
+        # validate rewrite
         buffer = io.BytesIO()
         torch.jit.save(scripted_model, buffer)
         buffer.seek(0)
         deserialized_scripted_model = torch.jit.load(buffer)
         for pattern, v in pattern_count_map.items():
-            if (v == 0):
-                FileCheck().check(pattern).run(deserialized_scripted_model.graph)
-            elif (v == -1):
-                FileCheck().check_not(pattern).run(deserialized_scripted_model.graph)
-            else:
-                FileCheck().check_count(pattern, v, exactly=True).run(deserialized_scripted_model.graph)
+            FileCheck().check_count(pattern, v, exactly=True).run(deserialized_scripted_model.graph)
 
-    def test_conv(self):
-        # Conv params
-        batch_size = 2
-        input_channels_per_group = 6
-        height = 16
-        width = 16
-        output_channels_per_group = 6
-        groups = 4
-        kernel_h = kernel_w = 3
-        stride_h = stride_w = 1
-        pad_h = pad_w = 1
-        dilation = 1
-        input_channels = input_channels_per_group * groups
-        output_channels = output_channels_per_group * groups
-        kernels = (kernel_h, kernel_w)
-        strides = (stride_h, stride_w)
-        paddings = (pad_h, pad_w)
-        dilations = (dilation, dilation)
-        conv_weight_shape = (output_channels, input_channels_per_group, kernel_h, kernel_w)
-        conv_bias_shape = (output_channels)
+        # TODO: evaluate rewritten model
 
-        class Conv2D(torch.nn.Module):
-            def __init__(self):
-                super(Conv2D, self).__init__()
-                self.weight = torch.nn.Parameter(torch.rand(conv_weight_shape), requires_grad=False)
-                self.bias = torch.nn.Parameter(torch.rand(conv_bias_shape), requires_grad=False)
-                self.strides = strides
-                self.paddings = paddings
-                self.dilations = dilations
-                self.groups = groups
+    def test_conv2d_rewrite(self) -> None:
+        model = ExampleConv2D()
+        old_node_kinds = ["aten::conv2d"]
+        new_node_kinds = ["vulkan_prepack::conv2d_clamp_run"]
 
-            def forward(self, x):
-                return F.conv2d(x, self.weight, self.bias,
-                                self.strides, self.paddings, self.dilations, self.groups)
-
-        data_shape = (batch_size, input_channels, height, width)
-        pattern_count_map = {"Tensor = aten::conv2d": -1,
+        data_shape = (model.batch_size, model.input_channels, model.height, model.width)
+        pattern_count_map = {"Tensor = aten::conv2d": 0,
                              "vulkan_prepack::conv2d_clamp_prepack": 1,
                              "vulkan_prepack::conv2d_clamp_run": 1}
-        TestVulkanRewritePass.validate_transformed_module(Conv2D(), pattern_count_map, data_shape)
+        self.validate_transformed_module(model, pattern_count_map, data_shape, old_node_kinds, new_node_kinds)
 
-        class Conv2DRelu(torch.nn.Module):
-            def __init__(self):
-                super(Conv2DRelu, self).__init__()
-                self.weight = torch.nn.Parameter(torch.rand(conv_weight_shape), requires_grad=False)
-                self.bias = torch.nn.Parameter(torch.rand(conv_bias_shape), requires_grad=False)
-                self.strides = strides
-                self.paddings = paddings
-                self.dilations = dilations
-                self.groups = groups
-
-            def forward(self, x):
-                o = F.conv2d(x, self.weight, self.bias,
-                             self.strides, self.paddings, self.dilations, self.groups)
-                o = F.relu(o)
-                return o
-
-        data_shape = (batch_size, input_channels, height, width)
-        pattern_count_map = {"Tensor = aten::conv2d": -1,
+    def test_conv2d_relu_rewrite(self) -> None:
+        model = ExampleConv2DRelu()
+        old_node_kinds = ["aten::conv2d"]
+        new_node_kinds = ["vulkan_prepack::conv2d_clamp_run"]
+        data_shape = (model.batch_size, model.input_channels, model.height, model.width)
+        pattern_count_map = {"Tensor = aten::conv2d": 0,
                              "vulkan_prepack::conv2d_clamp_prepack": 1,
                              "vulkan_prepack::conv2d_clamp_run": 1}
-        TestVulkanRewritePass.validate_transformed_module(
-            Conv2DRelu(), pattern_count_map, data_shape)
+        self.validate_transformed_module(model, pattern_count_map, data_shape, old_node_kinds, new_node_kinds)
 
         pattern_count_map["aten::relu"] = 1
-        pattern_count_map["vulkan_prepack::conv2d_clamp_prepack"] = -1
-        TestVulkanRewritePass.validate_transformed_module(
-            Conv2DRelu(),
-            pattern_count_map,
-            data_shape,
-            prepack_removal=True)
-        pattern_count_map["aten::relu"] = -1
-        TestVulkanRewritePass.validate_transformed_module(
-            Conv2DRelu(),
-            pattern_count_map,
-            data_shape,
-            prepack_removal=True,
-            fuse_clamping_ops=True)
+        pattern_count_map["vulkan_prepack::conv2d_clamp_prepack"] = 0
+        self.validate_transformed_module(model, pattern_count_map, data_shape, prepack_removal=True)
 
+        pattern_count_map["aten::relu"] = 0
+        self.validate_transformed_module(model, pattern_count_map, data_shape, prepack_removal=True, fuse_clamping_ops=True)
 
-        class Conv2DHardtanh(torch.nn.Module):
-            def __init__(self):
-                super(Conv2DHardtanh, self).__init__()
-                self.weight = torch.nn.Parameter(torch.rand(conv_weight_shape), requires_grad=False)
-                self.bias = torch.nn.Parameter(torch.rand(conv_bias_shape), requires_grad=False)
-                self.strides = strides
-                self.paddings = paddings
-                self.dilations = dilations
-                self.groups = groups
-
-            def forward(self, x):
-                o = F.conv2d(x, self.weight, self.bias,
-                             self.strides, self.paddings, self.dilations, self.groups)
-                o = F.hardtanh(o)
-                return o
-
-        data_shape = (batch_size, input_channels, height, width)
-        pattern_count_map = {"Tensor = aten::conv2d": -1,
+    def test_conv2d_hardtanh_rewrite(self) -> None:
+        model = ExampleConv2DHardtanh()
+        old_node_kinds = ["aten::conv2d"]
+        new_node_kinds = ["vulkan_prepack::conv2d_clamp_run"]
+        data_shape = (model.batch_size, model.input_channels, model.height, model.width)
+        pattern_count_map = {"Tensor = aten::conv2d": 0,
                              "vulkan_prepack::conv2d_clamp_prepack": 1,
                              "vulkan_prepack::conv2d_clamp_run": 1}
-        TestVulkanRewritePass.validate_transformed_module(Conv2DHardtanh(), pattern_count_map, data_shape)
+        self.validate_transformed_module(model, pattern_count_map, data_shape, old_node_kinds, new_node_kinds)
+
         pattern_count_map["aten::hardtanh"] = 1
-        pattern_count_map["vulkan_prepack::conv2d_clamp_prepack"] = -1
-        TestVulkanRewritePass.validate_transformed_module(
-            Conv2DHardtanh(),
+        pattern_count_map["vulkan_prepack::conv2d_clamp_prepack"] = 0
+        self.validate_transformed_module(
+            model,
             pattern_count_map,
             data_shape,
             prepack_removal=True)
-        pattern_count_map["aten::hardtanh"] = -1
-        TestVulkanRewritePass.validate_transformed_module(
-            Conv2DRelu(),
+
+        pattern_count_map["aten::hardtanh"] = 0
+        self.validate_transformed_module(
+            model,
             pattern_count_map,
             data_shape,
             prepack_removal=True,
