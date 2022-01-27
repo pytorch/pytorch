85e3e0a2e72364415f9f20fe4763581733136ac5
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-27 09:32:33 -0800

    Add new reduce options and autograd support for scatter_reduce (#71788)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/71788
    
    Test Plan: Imported from OSS
    
    Reviewed By: mikaylagawarecki
    
    Differential Revision: D33778525
    
    Pulled By: cpuhrsch
    
    fbshipit-source-id: 47b8544e29df3075bc6ede894c59499a7ffec876
    (cherry picked from commit ddcddac7262dd78a4002aaaea08daa3c50526028)

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -1285,6 +1285,7 @@ Tensor scatter_reduce_two_cpu(const Tensor& self,
 
   // TODO: Add documentation.
 
+
   TORCH_CHECK(dim >= -self.dim() && dim < self.dim(),
       "Expected `dim` to be in range ", -self.dim(), " to ", self.dim() - 1, " (got ", dim, ")");
 
@@ -1305,19 +1306,36 @@ Tensor scatter_reduce_two_cpu(const Tensor& self,
         "Shape mismatch between `self` (got ", self.sizes(), ") and `index` (got ", index.sizes(), ")");
   }
 
-  TORCH_CHECK(reduce == "sum", "`reduce` argument must be 'sum'");
+  TORCH_CHECK(reduce == "sum" || reduce == "prod" || reduce == "mean" || reduce == "amax" || reduce =="amin",
+              "`reduce` argument must be one of ('sum', 'prod', 'mean', 'amax', 'amin'");
 
-  // TODO: We currently expect contiguous memory layout.
-  TORCH_CHECK(self.is_contiguous(), "`self` needs to be contiguous");
-  TORCH_CHECK(index.is_contiguous(), "`index` needs to be contiguous");
-  TORCH_CHECK(out.is_contiguous(), "`out` needs to be contiguous");
-
+  if (self.numel() == 0) {
+    return out.zero_();
+  }
 
   AT_DISPATCH_ALL_TYPES_AND2(kHalf, kBFloat16, self.scalar_type(), "scatter_reduce", [&] {
-    out.fill_((scalar_t)0);
-    auto self_data = self.data_ptr<scalar_t>();
-    auto index_data = index.data_ptr<int64_t>();
-    auto out_data = out.data_ptr<scalar_t>();
+    if (reduce == "prod") {
+      out.fill_((scalar_t)1);
+    } else if (reduce == "amax") {
+      out.fill_(std::numeric_limits<scalar_t>::lowest());
+    } else if (reduce == "amin") {
+      out.fill_(std::numeric_limits<scalar_t>::max());
+    } else {
+      out.fill_((scalar_t)0);
+    }
+
+
+    auto self_cont = self.contiguous();
+    auto index_cont = index.contiguous();
+    auto self_data = self_cont.data_ptr<scalar_t>();
+    auto index_data = index_cont.data_ptr<int64_t>();
+    bool out_is_contiguous = out.is_contiguous();
+    auto out_cont = out.contiguous();
+    auto out_cont_data = out_cont.data_ptr<scalar_t>();
+
+    auto counts = at::zeros_like(out_cont);
+    auto counts_data = counts.data_ptr<scalar_t>();
+
 
     int64_t offset1 = 1, offset2 = 1;
     for (int64_t d = 0; d < dim; d++)
@@ -1330,14 +1348,37 @@ Tensor scatter_reduce_two_cpu(const Tensor& self,
     for (int64_t i = 0; i < offset1; i++) {
       for (int64_t j = 0; j < self.size(dim); j++) {
         for (int64_t k = 0; k < offset2; k++) {
-          value = self_data[i * self.stride(dim) * self.size(dim) + j * self.stride(dim) + k];
-          dim_index = index_data[i * index.stride(dim) * index.size(dim) + j * index.stride(dim) + k];
+          value = self_data[i * self_cont.stride(dim) * self_cont.size(dim) + j * self_cont.stride(dim) + k];
+          dim_index = index_data[i * index_cont.stride(dim) * index_cont.size(dim) + j * index_cont.stride(dim) + k];
           TORCH_CHECK(dim_index >= 0 && dim_index < out.size(dim),
               "Expected `index` values to be in range ", 0, " to ", out.size(dim), " (got ", dim_index, ")");
-          out_data[i * out.stride(dim) * out.size(dim) + dim_index * out.stride(dim) + k] += value;
+          int64_t ind = i * out_cont.stride(dim) * out_cont.size(dim) + dim_index * out_cont.stride(dim) + k;
+          if (reduce == "sum") {
+            out_cont_data[ind] += value;
+          } else if (reduce == "prod") {
+            out_cont_data[ind] *= value;
+          } else if (reduce == "mean") {
+            auto n = counts_data[ind];
+            out_cont_data[ind] = (out_cont_data[ind] * n + value) / (n + 1);
+            counts_data[ind] += 1;
+          } else if (reduce == "amax") {
+            out_cont_data[ind] = std::max(out_cont_data[ind], value);
+          } else {
+            out_cont_data[ind] = std::min(out_cont_data[ind], value);
+          }
         }
       }
     }
+
+    if (reduce == "amin" || reduce == "amax") {
+      auto val = (reduce == "amin") ? std::numeric_limits<scalar_t>::max() : std::numeric_limits<scalar_t>::lowest();
+      out_cont.masked_fill_(out_cont == val, (scalar_t)0);
+    }
+
+    if (!out_is_contiguous) {
+      out.copy_(out_cont);
+    }
+
   });
 
   return out;
