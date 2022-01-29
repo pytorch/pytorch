a9f8ccadae73a4f70ff8a1391fe5bc0bb3cbbef1
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared46390.04.pnb3.facebook.com>
2022-01-29 04:01:34 -0800

    2022-01-29 postnightly release (815532d40c25e81d8c09b3c36403016bea394aee)

--- test/jit/fixtures_srcs/fixtures_src.py
+++ test/jit/fixtures_srcs/fixtures_src.py
@@ -9,3 +9,20 @@ class TestVersionedDivTensorExampleV7(torch.nn.Module):
         result_1 = torch.div(a, b)
         result_2 = a.div(b)
         return result_0, result_1, result_2
+
+class TestVersionedLinspaceV7(torch.nn.Module):
+    def __init__(self):
+        super(TestVersionedLinspaceV7, self).__init__()
+
+    def forward(self, a, b):
+        c = torch.linspace(a, b, steps=5)
+        d = torch.linspace(a, b)
+        return c, d
+
+class TestVersionedLinspaceOutV7(torch.nn.Module):
+    def __init__(self):
+        super(TestVersionedLinspaceOutV7, self).__init__()
+
+    def forward(self, a, b):
+        out = torch.empty(100, )
+        return torch.linspace(a, b, out=out)
