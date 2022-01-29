ab36033777f6aedfd6d3b0d14e135fd1ac02deb5
Elias Ellison <eellison@devfair044.h1.fair>
2022-01-28 18:29:34 -0800

    Add docs for fusion strategy
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/profiling_graph_executor_impl.h
+++ torch/csrc/jit/runtime/profiling_graph_executor_impl.h
@@ -7,9 +7,51 @@ namespace jit {
 enum class FusionBehavior { STATIC, DYNAMIC };
 
 using FusionStrategy = std::vector<std::pair<FusionBehavior, size_t>>;
-// E.g. {(Static, 2), (Dynamic, 10)}
-// Fuse with static shapes twice then fallback to 10 dynamic fusions, then stop
-// // compiling new fusion groups
+// FusionStrategy is used to control the type and number of specializations that
+//   can occur during fusion
+//
+// Usage: provide a list of pairs (type, depth) where type is one of "STATIC" or
+//   "DYNAMIC" and depth is an integer.
+//
+// Behavior - static vs dynamic:
+// - in STATIC fusion, fused ops are compiled to have fixed input shapes. The
+//   input shapes are determined based on a number of initial profiling runs.
+//   The shape is determined based on some initial profiling runs. For example,
+//   if on the first run an input of shape [2, 4] is observed, then the compiled
+//   op will only work on shapes of size [2, 4].
+// - in DYNAMIC fusion, fused ops are compiled to have variable input shapes, so
+//   that multiple shapes are possible. Dynamic fusion uses "symbolic shapes",
+//   where any dimensions of the same value that are observed in profiling runs
+//   are assumed to have the same value. For example, if inputs of [2,3,4] and
+//   [3,4,5] are observed, then it is assumed that future inputs will have
+//   shapes [a,b,c] and [b,c,d] for some values of a,b,c,d.
+//
+//   In both cases, we also recompile on new striding behavior, device, or dtype.
+//
+// Behavior - fallback functions & depth:
+//   When an input doesn't match the format required by the specialized compiled
+//   op, it will run a fallback function.
+//   Fallback functions can also recursively be compiled and specialized based
+//   on the input shape. Since compilation can be slow, the "depth" parameter is
+//   provided to limit the number of specializations that can be compiled,
+//   before JIT gives up on recompiling and falls back to a completely un-fused,
+//   un-specialized implementation.
+//
+// The list of (type, depth) pairs controls the type of specializations and the
+//   number of specializations. For example: [("STATIC", 2), ("DYNAMIC", 2)]
+//   indicates that the first two specializations will use static fusions, the
+//   following two specializations will use dynamic fusion, and any inputs that
+//   satisfy none of the 4 options will run an unfused implementation.
+// Below an example of the fallback function structure is shown, if given a
+//   strategy of [("STATIC", 2), ("DYNAMIC", 2)] and if consecutive runs had
+//   these input shapes:
+//     [2, 2], [3, 3], [4, 4], [3, 5], ...
+//
+//   + specialized: statically fused, shape [2, 2]
+//    \-> + fallback 1; statically fused, shape [3, 3]
+//         \-> + fallback 2; dynamically fused, shape [A, A]
+//              \-> + fallback 3: dynamically fused, shape [A, B]
+//                   \-> final fallback: unspecialized, unfused
 TORCH_API FusionStrategy getFusionStrategy();
 // returns previous strategy
 TORCH_API FusionStrategy setFusionStrategy(FusionStrategy& fusion_strategy);
