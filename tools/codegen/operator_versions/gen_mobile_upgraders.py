f108b1b8ddc18cc4083847501552d2e15c93a305
Chen Lai <chenlai@fb.com>
2022-01-29 19:07:09 -0800

    Use upgrader_mobile.cpp as the reference for codegen unittest (#71930)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71930
    
    Previously `fbcode/caffe2/test/mobile/test_upgrader_bytecode_table_example.cpp` was checked in as intermediate step to make sure upgrader codegen works properly, before upgrader codegen is actually being used.
    
    this change use `buck run mode/opt //caffe2/torch/fb/mobile/upgrader_codegen:upgrader_codegen` to codegen `upgrader_mobile.cpp` and we no longer need to use the checkin file `test_upgrader_bytecode_table_example.cpp` for the codegen unit test.
    ghstack-source-id: 147957826
    
    Test Plan:
    ```
    buck test mode/opt //caffe2/test:upgrader_codegen
    ```
    
    Reviewed By: tugsbayasgalan
    
    Differential Revision: D33746264
    
    fbshipit-source-id: 18de3cae53aed966e67f8dc42976a2d10d3788b3
    (cherry picked from commit 661ffa786063d3e47cd7bcbe16b3baf2fff74808)

--- tools/codegen/operator_versions/gen_mobile_upgraders.py
+++ tools/codegen/operator_versions/gen_mobile_upgraders.py
@@ -92,11 +92,8 @@ getOperatorVersionMapForMobile() {
 
 
 UPGRADER_CPP_SRC = CodeTemplate(MOBILE_UPGRADERS_HEADER_DESCRIPTION + """
-#include <torch/csrc/jit/mobile/upgrader_mobile.h>
-
-#include <ATen/core/ivalue.h>
 #include <caffe2/serialize/versions.h>
-#include <torch/csrc/jit/mobile/type_parser.h>
+#include <torch/csrc/jit/mobile/upgrader_mobile.h>
 
 namespace c10 {
 TypePtr parseType(const std::string& pythonStr);
