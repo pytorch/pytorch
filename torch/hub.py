2bd2f5e5821fca3d5c6732a40a52457221b013cf
Vincent Moens <vincentmoens@gmail.com>
2022-01-31 16:59:52 +0000

    Update hub.py

--- torch/hub.py
+++ torch/hub.py
@@ -254,7 +254,7 @@ def _check_repo(repo, trust_repo=None, calling_fn="load"):
                 file.write(_repo + "\n")
 
     # load list
-    trusted_repos = [trusted_repo for trusted_repo in _PREDEFINED_TRUSTED]
+    trusted_repos = list(_PREDEFINED_TRUSTED)
     with open(filepath, 'r') as file:
         trusted_repos += file.readlines()
     is_trusted = any(trusted_repo == repo for trusted_repo in trusted_repos)
