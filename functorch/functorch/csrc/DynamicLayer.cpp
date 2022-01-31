c8075da9c152b4509cbd8b934595b9fe7c91211c
Richard Zou <zou3519@users.noreply.github.com>
2022-01-31 09:44:59 -0500

    [functorch] Fix CI (pytorch/functorch#425)
    - Update to new FuncTorchTLS interface from pytorch/pytorch

--- functorch/functorch/csrc/DynamicLayer.cpp
+++ functorch/functorch/csrc/DynamicLayer.cpp
@@ -96,6 +96,11 @@ class FuncTorchTLS : public FuncTorchTLSBase {
     return result;
   }
 
+  int64_t checkSupportsAutogradFunction() const override {
+    // Does nothing
+    return 0;
+  }
+
   // Initial autograd layer, because autograd is always "on"
   // TODO: Get rid of this, it is bad for composability
   std::vector<DynamicLayer> dynamicLayerStack = { DynamicLayer(DispatchKey::Autograd, 1, nullopt, true) };
