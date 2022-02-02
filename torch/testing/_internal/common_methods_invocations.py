910ff39e61a0215b29a61a7907ba6a0e50239ecd
Rishi Puri <puririshi98@berkeley.edu>
2022-02-02 11:22:01 -0800

    fix

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -40,7 +40,9 @@ from torch.testing._internal.common_utils import \
 import torch.testing._internal.opinfo_helper as opinfo_helper
 
 from distutils.version import LooseVersion
-from test_binary_ufuncs import IS_JETSON
+
+dev_name = torch.cuda.get_device_name(torch.cuda.current_device()).lower()
+IS_JETSON = 'xavier' in dev_name or 'nano' in dev_name or 'jetson' in dev_name or 'tegra' in dev_name
 
 has_scipy_fft = False
 if TEST_SCIPY:
