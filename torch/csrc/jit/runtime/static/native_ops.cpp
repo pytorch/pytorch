2aa699505d7595ec081f94389dfa42901cea148b
Mike Iovine <mikeiovine@fb.com>
2022-02-02 11:24:23 -0800

    [SR] Implement prim::If (#69837)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69837
    
    Implement `prim::If` with the new `StaticRuntimeBlockRunner` abstraction.
    ghstack-source-id: 148186475
    
    Test Plan:
    New unit tests: `buck test caffe2/benchmarks/static_runtime/...`
    
    Accuracy test at top of stack
    
    Reviewed By: d1jang
    
    Differential Revision: D33045908
    
    fbshipit-source-id: 281fb4a73528249fa60f65ac26f8ae6737771f55
    (cherry picked from commit de3b12dc0871e8ca09891c257e1dfd7cd352aa7c)

--- torch/csrc/jit/runtime/static/native_ops.cpp
+++ torch/csrc/jit/runtime/static/native_ops.cpp
@@ -701,5 +701,26 @@ REGISTER_NATIVE_OPERATOR_FUNCTOR(
           [](ProcessedNode* p_node) { p_node->Output(0) = p_node->Input(0); };
     });
 
+REGISTER_NATIVE_OPERATOR_FUNCTOR(prim::If, prim_If, [](Node*) -> SROperator {
+  return [](ProcessedNode* p_node) {
+    auto condition = p_node->Input(0).toBool();
+    auto* block_runners = p_node->block_runners();
+    DCHECK(block_runners);
+    DCHECK_EQ(block_runners->size(), 2);
+    auto& runner = (*block_runners)[!condition];
+
+    auto output = runner({});
+    if (!output.isTuple()) {
+      p_node->Output(0) = std::move(output);
+      return;
+    }
+    auto& elems = output.toTupleRef().elements();
+    DCHECK_EQ(elems.size(), p_node->num_outputs());
+    for (const auto i : c10::irange(elems.size())) {
+      p_node->Output(i) = elems[i];
+    }
+  };
+});
+
 } // namespace jit
 } // namespace torch
