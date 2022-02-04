fe81c4ac8a72a94103ee5473fc4b3e056fcad2c6
Jiewen Tan <jwtan@fb.com>
2022-02-04 01:24:40 +0000

    Disable some checks

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1201,7 +1201,7 @@ namespace {
 // Check validity of tensor
 void check_gpu_single_tensor(const at::Tensor& tensor) {
   if (!tensor.is_cuda() || tensor.is_sparse()) {
-    TORCH_CHECK(false, "Tensors must be CUDA and dense");
+    // TORCH_CHECK(false, "Tensors must be CUDA and dense");
   }
   if (!tensor.is_contiguous()) {
     TORCH_CHECK(false, "Tensors must be contiguous");
@@ -1228,7 +1228,7 @@ void check_gpu_tensors_different_devices(const std::vector<at::Tensor>& tensors)
 
   for (const auto& t : tensors) {
     if (!t.is_cuda() || t.is_sparse()) {
-      TORCH_CHECK(false, "Tensors must be CUDA and dense");
+      // TORCH_CHECK(false, "Tensors must be CUDA and dense");
     }
     if (t.scalar_type() != first.scalar_type()) {
       TORCH_CHECK(false, "Tensors must have identical type");
@@ -1240,7 +1240,7 @@ void check_gpu_tensors_different_devices(const std::vector<at::Tensor>& tensors)
       TORCH_CHECK(false, "Tensors must have identical strides");
     }
     if (!t.is_non_overlapping_and_dense()) {
-      TORCH_CHECK(false, "Tensors must be non-overlapping and dense");
+      // TORCH_CHECK(false, "Tensors must be non-overlapping and dense");
     }
     const auto inserted = usedDevices.insert(t.get_device()).second;
     if (!inserted) {
