cca6d117bd097df0bb915371f37c72e27f402bbf
Richard Zou <zou3519@users.noreply.github.com>
2022-02-04 11:24:42 -0500

    [functorch] Fix a bunch of tests (pytorch/functorch#452)

--- functorch/test/test_vmap.py
+++ functorch/test/test_vmap.py
@@ -2882,7 +2882,6 @@ class TestVmapBatchedGradient(Namespace.TestVmapBase):
             return x.expand(5, 5, 2, 3)
         self._batched_grad_test(op, (x,))
 
-    @unittest.expectedFailure
     @allowVmapFallbackUsage
     def test_index(self, device):
         x = torch.randn(2, 3, requires_grad=True, device=device)
