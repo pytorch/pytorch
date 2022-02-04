f9c1a7de6a13b1205bed625c0de6458b1f04b0df
soulitzer <soulitzer@gmail.com>
2022-02-04 11:55:15 -0500

    Update on "Gradcheck forward AD respects requires grad but run with requires_grad=False"
    Fixes: https://github.com/pytorch/pytorch/issues/72113
    
    Hmm, currently failing for forward over reverse grad according to CI, will investigate more.
    
    Differential Revision: [D33991570](https://our.internmc.facebook.com/intern/diff/D33991570)
    
    [ghstack-poisoned]

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -1541,7 +1541,7 @@ def gradgradcheck(
         input_args = tuple(x.requires_grad_() if i in diff_input_args_indices else x for i, x in enumerate(args[:-num_outputs]))
         outputs = _differentiable_outputs(func(*input_args))
         grad_outputs = tuple(x.requires_grad_() for x in args[-num_outputs:])
-        diff_input_args = tuple(x.requires_grad_() for i, x in enumerate(input_args) if i in diff_input_args_indices)
+        diff_input_args = tuple(x for i, x in enumerate(input_args) if i in diff_input_args_indices)
         grad_inputs = torch.autograd.grad(outputs, diff_input_args, grad_outputs, create_graph=True,
                                           allow_unused=True)
         grad_inputs = tuple(g for g in grad_inputs if g is not None)
