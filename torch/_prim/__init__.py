2dd45bba95013e4eab61ed9c860abaa3f5b93c36
Mike Ruberry <mruberry@devfair044.h1.fair>
2022-02-04 08:15:35 -0800

    fix

--- torch/_prim/__init__.py
+++ torch/_prim/__init__.py
@@ -1,6 +1,6 @@
 
 # See the [primTorch Build] note for how these imports work
 # Note: they must be ignored by linters
-from torch.prim.aliases import alias_infos
+from torch._prim.aliases import alias_infos
 
 __all__ = ['alias_infos']
