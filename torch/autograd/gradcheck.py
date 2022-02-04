e587a8537ebd5b6cf97f0ed474fa0d57584d5383
soulitzer <soulitzer@gmail.com>
2022-02-04 11:53:43 -0500

    Update on "Gradcheck forward AD respects requires grad but run with requires_grad=False"
    Fixes: https://github.com/pytorch/pytorch/issues/72113
    
    Hmm, currently failing for forward over reverse grad according to CI, will investigate more.
    
    Differential Revision: [D33991570](https://our.internmc.facebook.com/intern/diff/D33991570)
    
    [ghstack-poisoned]

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -1534,14 +1534,15 @@ def gradgradcheck(
         tupled_grad_outputs = _as_tuple(grad_outputs)
 
     num_outputs = len(tupled_grad_outputs)
+    diff_input_args_indices = set(i for i, x in enumerate(tupled_inputs) if is_tensor_like(x) and x.requires_grad)
 
     def new_func(*args):
-        input_args = args[:-num_outputs]
-        grad_outputs = args[-num_outputs:]
+        # Make sure inputs requires_grad=True here because forward AD detaches
+        input_args = tuple(x.requires_grad_() if i in diff_input_args_indices else x for i, x in enumerate(args[:-num_outputs]))
         outputs = _differentiable_outputs(func(*input_args))
-        input_args = tuple(x for x in input_args
-                           if is_tensor_like(x) and x.requires_grad)
-        grad_inputs = torch.autograd.grad(outputs, input_args, grad_outputs, create_graph=True,
+        grad_outputs = tuple(x.requires_grad_() for x in args[-num_outputs:])
+        diff_input_args = tuple(x.requires_grad_() for i, x in enumerate(input_args) if i in diff_input_args_indices)
+        grad_inputs = torch.autograd.grad(outputs, diff_input_args, grad_outputs, create_graph=True,
                                           allow_unused=True)
         grad_inputs = tuple(g for g in grad_inputs if g is not None)
         return grad_inputs
