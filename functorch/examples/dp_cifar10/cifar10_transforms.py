692c4c157f8934217530ef2f3e1cf10735501aff
Sam Andow <samdow@fb.com>
2022-02-04 19:22:53 +0000

    [functorch] update differential privacy example

--- functorch/examples/dp_cifar10/cifar10_transforms.py
+++ functorch/examples/dp_cifar10/cifar10_transforms.py
@@ -6,21 +6,18 @@ Runs CIFAR10 training with differential privacy.
 """
 
 import argparse
-import os
+import logging
 import shutil
+import sys
+from datetime import datetime, timedelta
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
-import torch.utils.data.distributed
-import torch.utils.tensorboard as tensorboard
-import torchvision.models as models
 import torchvision.transforms as transforms
-from opacus import PrivacyEngine
-from opacus.utils import stats
-from opacus.utils.module_modification import convert_batchnorm_modules
+from torchvision import models
 from torchvision.datasets import CIFAR10
 from tqdm import tqdm
 
@@ -30,6 +27,14 @@ from functorch import make_functional
 
 # disable warning spam
 functorch._C._set_vmap_fallback_warning_enabled(False)
+
+logging.basicConfig(
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    stream=sys.stdout,
+)
+logger = logging.getLogger("ddp")
+logger.setLevel(level=logging.INFO)
 
 
 def save_checkpoint(state, is_best, filename="checkpoint.tar"):
@@ -76,7 +81,8 @@ def clip_and_accumulate_and_add_noise(model, max_per_sample_grad_norm=1.0, noise
 
 
 def train(args, model, train_loader, optimizer, epoch, device):
-    model.train()
+    start_time = datetime.now()
+
     criterion = nn.CrossEntropyLoss()
 
     losses = []
@@ -137,15 +143,11 @@ def train(args, model, train_loader, optimizer, epoch, device):
         acc1 = accuracy(preds, labels)
 
         top1_acc.append(acc1)
-        stats.update(stats.StatType.TRAIN, acc1=acc1)
 
         # make sure we take a step after processing the last mini-batch in the
         # epoch to ensure we start the next epoch with a clean state
-        if ((i + 1) % args.n_accumulation_steps == 0) or ((i + 1) == len(train_loader)):
-            optimizer.step()
-            optimizer.zero_grad()
-        else:
-            optimizer.virtual_step()
+        optimizer.step()
+        optimizer.zero_grad()
 
         if i % args.print_freq == 0:
             print(
@@ -153,6 +155,8 @@ def train(args, model, train_loader, optimizer, epoch, device):
                 f"Loss: {np.mean(losses):.6f} "
                 f"Acc@1: {np.mean(top1_acc):.6f} "
             )
+    train_duration = datetime.now() - start_time
+    return train_duration
 
 
 def test(args, model, test_loader, device):
@@ -176,13 +180,139 @@ def test(args, model, test_loader, device):
             top1_acc.append(acc1)
 
     top1_avg = np.mean(top1_acc)
-    stats.update(stats.StatType.TEST, acc1=top1_avg)
 
     print(f"\tTest set:" f"Loss: {np.mean(losses):.6f} " f"Acc@1: {top1_avg :.6f} ")
     return np.mean(top1_acc)
 
 
+# flake8: noqa: C901
 def main():
+    args = parse_args()
+
+    if args.debug >= 1:
+        logger.setLevel(level=logging.DEBUG)
+
+    device = args.device
+
+    if args.secure_rng:
+        try:
+            import torchcsprng as prng
+        except ImportError as e:
+            msg = (
+                "To use secure RNG, you must install the torchcsprng package! "
+                "Check out the instructions here: https://github.com/pytorch/csprng#installation"
+            )
+            raise ImportError(msg) from e
+
+        generator = prng.create_random_device_generator("/dev/urandom")
+
+    else:
+        generator = None
+
+    augmentations = [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+    ]
+    normalize = [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+    train_transform = transforms.Compose(normalize)
+
+    test_transform = transforms.Compose(normalize)
+
+    train_dataset = CIFAR10(
+        root=args.data_root, train=True, download=True, transform=train_transform
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=int(args.sample_rate * len(train_dataset)),
+        generator=generator,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+
+    test_dataset = CIFAR10(
+        root=args.data_root, train=False, download=True, transform=test_transform
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args.batch_size_test,
+        shuffle=False,
+        num_workers=args.workers,
+    )
+
+    best_acc1 = 0
+
+    model = models.__dict__[args.architecture](
+        pretrained=False, norm_layer=(lambda c: nn.GroupNorm(args.gn_groups, c))
+    )
+    model = model.to(device)
+
+    if args.optim == "SGD":
+        optimizer = optim.SGD(
+            model.parameters(),
+            lr=args.lr,
+            momentum=args.momentum,
+            weight_decay=args.weight_decay,
+        )
+    elif args.optim == "RMSprop":
+        optimizer = optim.RMSprop(model.parameters(), lr=args.lr)
+    elif args.optim == "Adam":
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    else:
+        raise NotImplementedError("Optimizer not recognized. Please check spelling")
+
+    # Store some logs
+    accuracy_per_epoch = []
+    time_per_epoch = []
+
+    for epoch in range(args.start_epoch, args.epochs + 1):
+        if args.lr_schedule == "cos":
+            lr = args.lr * 0.5 * (1 + np.cos(np.pi * epoch / (args.epochs + 1)))
+            for param_group in optimizer.param_groups:
+                param_group["lr"] = lr
+
+        train_duration = train(
+            args, model, train_loader, optimizer, epoch, device
+        )
+        top1_acc = test(args, model, test_loader, device)
+
+        # remember best acc@1 and save checkpoint
+        is_best = top1_acc > best_acc1
+        best_acc1 = max(top1_acc, best_acc1)
+
+        time_per_epoch.append(train_duration)
+        accuracy_per_epoch.append(float(top1_acc))
+
+        save_checkpoint(
+            {
+                "epoch": epoch + 1,
+                "arch": "Convnet",
+                "state_dict": model.state_dict(),
+                "best_acc1": best_acc1,
+                "optimizer": optimizer.state_dict(),
+            },
+            is_best,
+            filename=args.checkpoint_file + ".tar",
+        )
+
+    time_per_epoch_seconds = [t.total_seconds() for t in time_per_epoch]
+    avg_time_per_epoch = sum(time_per_epoch_seconds) / len(time_per_epoch_seconds)
+    metrics = {
+        "accuracy": best_acc1,
+        "accuracy_per_epoch": accuracy_per_epoch,
+        "avg_time_per_epoch_str": str(timedelta(seconds=int(avg_time_per_epoch))),
+        "time_per_epoch": time_per_epoch_seconds,
+    }
+
+    logger.info(
+        "\nNote:\n- 'total_time' includes the data loading time, training time and testing time.\n- 'time_per_epoch' measures the training time only.\n"
+    )
+    logger.info(metrics)
+
+def parse_args():
     parser = argparse.ArgumentParser(description="PyTorch CIFAR10 DP Training")
     parser.add_argument(
         "-j",
@@ -208,27 +338,23 @@ def main():
     )
     parser.add_argument(
         "-b",
-        "--batch-size",
-        # This should be 256, but that OOMs using the prototype.
-        default=64,
+        "--batch-size-test",
+        default=256,
         type=int,
         metavar="N",
-        help="mini-batch size (default: 64), this is the total "
-        "batch size of all GPUs on the current node when "
-        "using Data Parallel or Distributed Data Parallel",
+        help="mini-batch size for test dataset (default: 256)"
     )
     parser.add_argument(
-        "-na",
-        "--n_accumulation_steps",
-        default=1,
-        type=int,
-        metavar="N",
-        help="number of mini-batches to accumulate into an effective batch",
+        "--sample-rate",
+        default=0.005,
+        type=float,
+        metavar="SR",
+        help="sample rate used for batch construction (default: 0.005)",
     )
     parser.add_argument(
         "--lr",
         "--learning-rate",
-        default=0.001,
+        default=0.1,
         type=float,
         metavar="LR",
         help="initial learning rate",
@@ -240,10 +366,10 @@ def main():
     parser.add_argument(
         "--wd",
         "--weight-decay",
-        default=5e-4,
+        default=0,
         type=float,
         metavar="W",
-        help="SGD weight decay (default: 1e-4)",
+        help="SGD weight decay",
         dest="weight_decay",
     )
     parser.add_argument(
@@ -271,16 +397,11 @@ def main():
     parser.add_argument(
         "--seed", default=None, type=int, help="seed for initializing training. "
     )
-    parser.add_argument(
-        "--device",
-        type=str,
-        default="cuda",
-        help="GPU ID for this process (default: 'cuda')",
-    )
+
     parser.add_argument(
         "--sigma",
         type=float,
-        default=1.0,
+        default=1.5,
         metavar="S",
         help="Noise multiplier (default 1.0)",
     )
@@ -288,21 +409,17 @@ def main():
         "-c",
         "--max-per-sample-grad_norm",
         type=float,
-        default=1.0,
+        default=10.0,
         metavar="C",
         help="Clip per-sample gradients to this norm (default 1.0)",
-    )
-    parser.add_argument(
-        "--disable-dp",
-        action="store_true",
-        default=False,
-        help="Disable privacy training and just train with vanilla SGD",
     )
     parser.add_argument(
         "--secure-rng",
         action="store_true",
         default=False,
-        help="Enable Secure RNG to have trustworthy privacy guarantees. Comes at a performance cost",
+        help="Enable Secure RNG to have trustworthy privacy guarantees."
+        "Comes at a performance cost. Opacus will emit a warning if secure rng is off,"
+        "indicating that for production use it's recommender to turn it on.",
     )
     parser.add_argument(
         "--delta",
@@ -325,149 +442,53 @@ def main():
         help="Where CIFAR10 is/will be stored",
     )
     parser.add_argument(
-        "--log-dir", type=str, default="", help="Where Tensorboard log will be stored"
+        "--log-dir",
+        type=str,
+        default="/tmp/stat/tensorboard",
+        help="Where Tensorboard log will be stored",
     )
     parser.add_argument(
         "--optim",
         type=str,
-        default="Adam",
+        default="SGD",
         help="Optimizer to use (Adam, RMSprop, SGD)",
     )
-
-    args = parser.parse_args()
-    args.disable_dp = True
-
-    if args.disable_dp and args.n_accumulation_steps > 1:
-        raise ValueError("Virtual steps only works with enabled DP")
-
-    # The following few lines, enable stats gathering about the run
-    # 1. where the stats should be logged
-    stats.set_global_summary_writer(
-        tensorboard.SummaryWriter(os.path.join("/tmp/stat", args.log_dir))
-    )
-    # 2. enable stats
-    stats.add(
-        # stats about gradient norms aggregated for all layers
-        stats.Stat(stats.StatType.GRAD, "AllLayers", frequency=0.1),
-        # stats about gradient norms per layer
-        stats.Stat(stats.StatType.GRAD, "PerLayer", frequency=0.1),
-        # stats about clipping
-        stats.Stat(stats.StatType.GRAD, "ClippingStats", frequency=0.1),
-        # stats on training accuracy
-        stats.Stat(stats.StatType.TRAIN, "accuracy", frequency=0.01),
-        # stats on validation accuracy
-        stats.Stat(stats.StatType.TEST, "accuracy"),
+    parser.add_argument(
+        "--lr-schedule", type=str, choices=["constant", "cos"], default="cos"
     )
 
-    # The following lines enable stat gathering for the clipping process
-    # and set a default of per layer clipping for the Privacy Engine
-    clipping = {"clip_per_layer": False, "enable_stat": True}
-
-    if args.secure_rng:
-        assert False
-        try:
-            import torchcsprng as prng
-        except ImportError as e:
-            msg = (
-                "To use secure RNG, you must install the torchcsprng package! "
-                "Check out the instructions here: https://github.com/pytorch/csprng#installation"
-            )
-            raise ImportError(msg) from e
-
-        generator = prng.create_random_device_generator("/dev/urandom")
-
-    else:
-        generator = None
-
-    augmentations = [
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-    ]
-    normalize = [
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ]
-    train_transform = transforms.Compose(
-        augmentations + normalize if args.disable_dp else normalize
+    parser.add_argument(
+        "--device", type=str, default="cuda", help="Device on which to run the code."
     )
 
-    test_transform = transforms.Compose(normalize)
-
-    train_dataset = CIFAR10(
-        root=args.data_root, train=True, download=True, transform=train_transform
+    parser.add_argument(
+        "--architecture",
+        type=str,
+        default="resnet18",
+        help="model from torchvision to run",
     )
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=args.workers,
-        drop_last=True,
-        generator=generator,
+    parser.add_argument(
+        "--gn-groups",
+        type=int,
+        default=8,
+        help="Number of groups in GroupNorm",
     )
 
-    test_dataset = CIFAR10(
-        root=args.data_root, train=False, download=True, transform=test_transform
+    parser.add_argument(
+        "--clip_per_layer",
+        action="store_true",
+        default=False,
+        help="Use static per-layer clipping with the same clipping threshold for each layer. Necessary for DDP. If `False` (default), uses flat clipping.",
     )
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=args.batch_size,
-        shuffle=False,
-        num_workers=args.workers,
+    parser.add_argument(
+        "--debug",
+        type=int,
+        default=0,
+        help="debug level (default: 0)",
     )
 
-    best_acc1 = 0
-    device = torch.device(args.device)
-    model = convert_batchnorm_modules(models.resnet18(num_classes=10))
-    # model = CIFAR10Model()
-    model = model.to(device)
-
-    if args.optim == "SGD":
-        optimizer = optim.SGD(
-            model.parameters(),
-            lr=args.lr,
-            momentum=args.momentum,
-            weight_decay=args.weight_decay,
-        )
-    elif args.optim == "RMSprop":
-        optimizer = optim.RMSprop(model.parameters(), lr=args.lr)
-    elif args.optim == "Adam":
-        optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    else:
-        raise NotImplementedError("Optimizer not recognized. Please check spelling")
-
-    if not args.disable_dp:
-        privacy_engine = PrivacyEngine(
-            model,
-            batch_size=args.batch_size * args.n_accumulation_steps,
-            sample_size=len(train_dataset),
-            alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
-            noise_multiplier=args.sigma,
-            max_grad_norm=args.max_per_sample_grad_norm,
-            secure_rng=args.secure_rng,
-            **clipping,
-        )
-        privacy_engine.attach(optimizer)
-
-    for epoch in range(args.start_epoch, args.epochs + 1):
-        train(args, model, train_loader, optimizer, epoch, device)
-        top1_acc = test(args, model, test_loader, device)
-
-        # remember best acc@1 and save checkpoint
-        is_best = top1_acc > best_acc1
-        best_acc1 = max(top1_acc, best_acc1)
-
-        save_checkpoint(
-            {
-                "epoch": epoch + 1,
-                "arch": "ResNet18",
-                "state_dict": model.state_dict(),
-                "best_acc1": best_acc1,
-                "optimizer": optimizer.state_dict(),
-            },
-            is_best,
-            filename=args.checkpoint_file + ".tar",
-        )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
