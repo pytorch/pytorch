37e65e262c8bc4a8ffa968dc33008066c2abd943
vasiliy <vasiliy@fb.com>
2022-02-07 08:07:35 -0800

    quant: add QAT fused Linear-Bn1d [1/x]: prepared module
    Summary:
    
    Adds support for a fused QAT observed module for `Linear` followed by
    `BatchNorm1d`. In this PR, only the support for prepared module with
    fake_quants in the right places is added.
    
    A future PR will add support for `convert`, and tests for eager and FX
    graph mode workflows.
    
    Similar to conv-bn, we rescale the weight before applying the fake
    quant, and undo the rescaling after the linear operation.
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestQuantizeEagerQATNumerics.test_linear_bn
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/quantization/fuser_method_mappings.py
+++ torch/ao/quantization/fuser_method_mappings.py
@@ -114,7 +114,12 @@ def fuse_linear_bn(is_qat, linear, bn):
     if is_qat:
         # TODO: remove the assert later
         assert linear.training, "qat is only supported when linear.training is True currently"
-        raise Exception("Fusing Linear+BatchNorm not yet supported in training.")
+        assert bn.num_features == linear.out_features,\
+            "Output features of Linear must match num_features of BatchNorm1d"
+        assert bn.affine, "Only support fusing BatchNorm1d with affine set to True"
+        assert bn.track_running_stats,\
+            "Only support fusing BatchNorm1d with tracking_running_stats set to True"
+        return nni.LinearBn1d(linear, bn)
     else:
         return nn.utils.fusion.fuse_linear_bn_eval(linear, bn)
 
