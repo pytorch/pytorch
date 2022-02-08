480cda066a094e76b9c4b53dc0c15f4360c41dc8
Yeounoh Chung <yeounoh@google.com>
2022-02-08 04:19:39 +0000

    pin xla installation to tf-update branch

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -100,6 +100,6 @@ function checkout_install_torchvision() {
 
 function clone_pytorch_xla() {
   if [[ ! -d ./xla ]]; then
-    git clone --recursive https://github.com/pytorch/xla.git
+    git clone --recursive -b tf-update --single-branch https://github.com/pytorch/xla.git
   fi
 }
