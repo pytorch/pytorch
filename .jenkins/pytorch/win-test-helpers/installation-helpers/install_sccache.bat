f150897008777971ce71f8406e57c5157c21a19f
Yi Zhang <zhanyi@microsoft.com>
2022-02-08 03:18:15 -0800

    update to 0208 version

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -6,7 +6,7 @@ if "%REBUILD%"=="" (
     taskkill /im sccache.exe /f /t || ver > nul
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
     del %TMP_DIR_WIN%\bin\sccache-cl.exe || ver > nul
-    curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20220125.exe --output %TMP_DIR_WIN%\bin\sccache.exe
+    curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-20220208.exe --output %TMP_DIR_WIN%\bin\sccache.exe
     curl --retry 3 -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %TMP_DIR_WIN%\bin\sccache-cl.exe
     goto :check_sccache
   )
