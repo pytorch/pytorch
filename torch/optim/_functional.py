a008227d227749d79367d7d592bcefcf51c22df5
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-02-09 08:46:31 -0800

    Optim foreach cleanup for Adadelta (#69980)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69980
    
    - Merged `torch/optim/adadelta.py` and `torch/optim/_multitensor/adadelta.py` into `torch/optim/adadelta.py`
    - Moved adadelta functional forms from `torch/optim/_functional.py` and `torch/optim/_multi_tensor/_functional.py` to `torch/optim/adadelta.py`
    - `torch/optim/_functional.py` just imports from `torch/optim/adadelta.py`
    - Added a test `test_optimizers_foreach_flag` which replicates `test_multi_tensor_optimizers` in `test/test_optim.py`
    - Add a test `test_adadelta_new` that replicates the behavior of `test_adadelta` but with `foreach` flag instead of using the multitensor adadleta class. If we delete `_multitensor/` we could replace `test_adadelta` with this
    
    Remaining TODO:
    
    - [ ] single_tensor adadelta supports complex but multitensor does not, need to integrate the singletensor logic in multitensor and switch the `test_adadelta_complex` to test for foreach in [True, False]
    
    Test Plan: Imported from OSS
    
    Reviewed By: VitalyFedyunin, albanD
    
    Differential Revision: D33413059
    
    Pulled By: mikaylagawarecki
    
    fbshipit-source-id: 92a9fa98705762bb6bd464261671e49aef40070e

--- torch/optim/_functional.py
+++ torch/optim/_functional.py
@@ -4,6 +4,8 @@ import torch
 from torch import Tensor
 from typing import List, Optional
 
+from .adadelta import adadelta  # type: ignore[attr-defined] # noqa: F401
+
 # TODO: use foreach API in optim._functional to do all the computation
 
 def _make_sparse(grad, grad_indices, values):
@@ -213,37 +215,6 @@ def sgd(params: List[Tensor],
         alpha = lr if maximize else -lr
         param.add_(d_p, alpha=alpha)
 
-
-def adadelta(params: List[Tensor],
-             grads: List[Tensor],
-             square_avgs: List[Tensor],
-             acc_deltas: List[Tensor],
-             *,
-             lr: float,
-             rho: float,
-             eps: float,
-             weight_decay: float):
-    r"""Functional API that performs Adadelta algorithm computation.
-
-    See :class:`~torch.optim.Adadelta` for details.
-    """
-
-    for (param, grad, square_avg, acc_delta) in zip(params, grads, square_avgs, acc_deltas):
-        if weight_decay != 0:
-            grad = grad.add(param, alpha=weight_decay)
-
-        if torch.is_complex(param):
-            square_avg = torch.view_as_real(square_avg)
-            acc_delta = torch.view_as_real(acc_delta)
-            grad = torch.view_as_real(grad)
-
-        square_avg.mul_(rho).addcmul_(grad, grad, value=1 - rho)
-        std = square_avg.add(eps).sqrt_()
-        delta = acc_delta.add(eps).sqrt_().div_(std).mul_(grad)
-        acc_delta.mul_(rho).addcmul_(delta, delta, value=1 - rho)
-        if torch.is_complex(param):
-            delta = torch.view_as_complex(delta)
-        param.add_(delta, alpha=-lr)
 
 def rmsprop(params: List[Tensor],
             grads: List[Tensor],
