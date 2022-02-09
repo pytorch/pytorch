d9bdecd5a8ec2db7ef3a1c557b4a93d24777001a
Rui Zhu <zrphercule@fb.com>
2022-02-08 16:53:39 -0800

    Add naive native FFN in aten
    Summary:
    This is an alias of feed forward network in transformer. We would like to use this API in the prototype of the new transformer encoder. Notice this diff might not bring any perf improvement since it is just a combination of aten ops, but we can improve its kernels later.
    
    fp16 is not supported in this version, due to the poor support of Half type in current aten::baddbmm. We should improve this in our future implemented fast FFN kernel.
    
    Test Plan: buck build mode/opt -c fbcode.enable_gpu_sections=true caffe2/test:nn && buck-out/gen/caffe2/test/nn\#binary.par -r test_ffn
    
    Differential Revision: D33800859
    
    fbshipit-source-id: d6163a39361421f4ef0602a53e239872d3522b0c

---
+++ aten/src/ATen/native/cuda/Transformer.cu
@@ -0,0 +1,21 @@
+#include <ATen/ATen.h>
+#include <ATen/AccumulateType.h>
+#include <ATen/Dispatch.h>
+#include <ATen/NativeFunctions.h>
+
+namespace at {
+
+namespace native {
+
+Tensor ffn_cuda(const Tensor& input, const Tensor& w1, const Tensor& b1, const Tensor& w2, const Tensor& b2, bool use_gelu, bool add_norm){
+  TORCH_CHECK(add_norm == false, "TODO add_norm to be supported in FFN");
+  TORCH_CHECK(use_gelu == false, "TODO gelu to be supported in FFN");
+  Tensor res = at::baddbmm(b1, input, w1);
+  if (!use_gelu) {
+    res = at::relu(res);
+  }
+  res = at::baddbmm(b2, res, w2);
+  return res;
+}
+} // namespace native
+} // namespace at
