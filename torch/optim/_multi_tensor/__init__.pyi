5948522e9cc520cebe177a6cd534951ec626964f
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-02-09 08:46:31 -0800

    Optim foreach cleanup for RAdam (#70230)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70230
    
    Test Plan: Imported from OSS
    
    Reviewed By: anjali411
    
    Differential Revision: D33767874
    
    Pulled By: mikaylagawarecki
    
    fbshipit-source-id: 9379db24266a7bbcc2c23849f87ae0af2e6729c0
    (cherry picked from commit ecf7b31fc39ccfeeef36bb763ca8c96960be3577)

--- torch/optim/_multi_tensor/__init__.pyi
+++ torch/optim/_multi_tensor/__init__.pyi
@@ -5,7 +5,7 @@ from .adam import Adam as Adam
 from .adamw import AdamW as AdamW
 NAdam = partial(optim.NAdam, foreach=True)
 from .sgd import SGD as SGD
-from .radam import RAdam as RAdam
+RAdam = partial(optim.RAdam, foreach=True)
 from .rmsprop import RMSprop as RMSprop
 from .rprop import Rprop as Rprop
 from .asgd import ASGD as ASGD
