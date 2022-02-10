9324a14e1912516e08bf6f889431614f77c5b7bf
Linbin Yu <linbin@fb.com>
2022-02-10 13:39:40 -0800

    try to add more models to iOS test (#72666)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/72666
    
    Differential Revision: D34149284
    
    fbshipit-source-id: 7455e0f4646ceada834b09ffb22caf6e95342531

--- ios/TestApp/TestAppTests/TestLiteInterpreter.mm
+++ ios/TestApp/TestAppTests/TestLiteInterpreter.mm
@@ -11,14 +11,36 @@
 @implementation TestAppTests {
 }
 
-- (void)testLiteInterpreter {
-  NSString* modelPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"model_lite"
+- (void)testMobileNetV2 {
+  NSString* modelPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"mobilenet_v2"
                                                                          ofType:@"ptl"];
   auto module = torch::jit::_load_for_mobile(modelPath.UTF8String);
   c10::InferenceMode mode;
   auto input = torch::ones({1, 3, 224, 224}, at::kFloat);
   auto outputTensor = module.forward({input}).toTensor();
   XCTAssertTrue(outputTensor.numel() == 1000);
+}
+
+- (void)testMobileNetV3 {
+  NSString* modelPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"mobilenet_v3_small"
+                                                                         ofType:@"ptl"];
+  auto module = torch::jit::_load_for_mobile(modelPath.UTF8String);
+  c10::InferenceMode mode;
+  auto input = torch::ones({1, 3, 224, 224}, at::kFloat);
+  auto outputTensor = module.forward({input}).toTensor();
+  XCTAssertTrue(outputTensor.numel() == 1000);
+}
+
+- (void)testKeypointrcnn {
+  NSString* modelPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"keypointrcnn_resnet50_fpn"
+                                                                         ofType:@"ptl"];
+  auto module = torch::jit::_load_for_mobile(modelPath.UTF8String);
+  c10::InferenceMode mode;
+  std::vector<torch::jit::IValue> inputs;
+  inputs.emplace_back(torch::rand(3, 300, 400));
+  inputs.emplace_back(torch::rand(3, 400, 500));
+  auto outputTensor = module.forward({inputs}).toTensor();
+  XCTAssertTrue(true);
 }
 
 - (void)testCoreML {
@@ -30,5 +52,6 @@
   auto outputTensor = module.forward({input}).toTensor();
   XCTAssertTrue(outputTensor.numel() == 1000);
 }
+
 
 @end
--- ios/TestApp/benchmark/trace_model.py
+++ ios/TestApp/benchmark/trace_model.py
@@ -3,9 +3,18 @@ import torchvision
 from torch.utils.mobile_optimizer import optimize_for_mobile
 
 model = torchvision.models.mobilenet_v2(pretrained=True)
-model.eval()
-example = torch.rand(1, 3, 224, 224)
-traced_script_module = torch.jit.trace(model, example)
-optimized_scripted_module = optimize_for_mobile(traced_script_module)
-torch.jit.save(optimized_scripted_module, '../models/model.pt')
-exported_optimized_scripted_module = optimized_scripted_module._save_for_lite_interpreter("../models/model_lite.ptl")
+trace_and_export_model(model, torch.rand(1, 3, 224, 224), 'mobilenet_v2')
+
+model = torchvision.models.mobilenet_v3_small(pretrained=True)
+trace_and_export_model(model, torch.rand(1, 3, 224, 224), 'mobilenet_v3_small')
+
+model = torchvision.models.detection.keypointrcnn_resnet50_fpn(pretrained=True)
+trace_and_export_model(model, [torch.rand(3, 300, 400), torch.rand(3, 500, 400)], 'keypointrcnn_resnet50_fpn')
+
+
+def trace_and_export_model(model, input, name):
+    model.eval()
+    traced_script_module = torch.jit.trace(model, input)
+    optimized_scripted_module = optimize_for_mobile(traced_script_module)
+    torch.jit.save(optimized_scripted_module, f'../models/{name}.pt')
+    optimized_scripted_module._save_for_lite_interpreter(f'../models/{name}.ptl')
