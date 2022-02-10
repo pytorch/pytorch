c573c3ae43aefdf69786bd94dbf6ce46286c6b92
Terry Chen <terrychen@fb.com>
2022-02-09 16:14:35 -0800

    Update on "[Qunat] Add ConvTranspose reference module"
    Summary:
    Add ConvTranspose reference module
    
    Test Plan:
    python3 test/test_quantization.py TestQuantizeEagerOps.test_conv_transpose_op
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D34126542](https://our.internmc.facebook.com/intern/diff/D34126542)
    
    [ghstack-poisoned]

--- test/quantization/eager/test_quantize_eager_ptq.py
+++ test/quantization/eager/test_quantize_eager_ptq.py
@@ -76,7 +76,6 @@ import numpy as np
 
 class TestQuantizeEagerOps(QuantizationTestCase):
     def _test_reference_module_impl(self,
-                                    ref_module_class,
                                     float_module_class,
                                     quantized_module_class,
                                     extra_module_kwargs,
@@ -154,27 +153,27 @@ class TestQuantizeEagerOps(QuantizationTestCase):
         self.assertEqual(res, ref_res)
 
     def test_conv_1d(self):
-        self._test_reference_module_impl(nnqr.Conv1d, nn.Conv1d, nnq.Conv1d, \
+        self._test_reference_module_impl(nn.Conv1d, nnq.Conv1d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 1))
 
     def test_conv_2d(self):
-        self._test_reference_module_impl(nnqr.Conv2d, nn.Conv2d, nnq.Conv2d, \
+        self._test_reference_module_impl(nn.Conv2d, nnq.Conv2d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 10, 10))
 
     def test_conv_3d(self):
-        self._test_reference_module_impl(nnqr.Conv3d, nn.Conv3d, nnq.Conv3d, \
+        self._test_reference_module_impl(nn.Conv3d, nnq.Conv3d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 10, 10, 10))
 
     def test_conv_transpose_1d(self):
-        self._test_reference_module_impl(nnqr.ConvTranspose1d, nn.ConvTranspose1d, nnq.ConvTranspose1d, \
+        self._test_reference_module_impl(nn.ConvTranspose1d, nnq.ConvTranspose1d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 1))
 
     def test_conv_transpose_2d(self):
-        self._test_reference_module_impl(nnqr.ConvTranspose2d, nn.ConvTranspose2d, nnq.ConvTranspose2d, \
+        self._test_reference_module_impl(nn.ConvTranspose2d, nnq.ConvTranspose2d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 10, 10))
 
     def test_conv_transpose_3d(self):
-        self._test_reference_module_impl(nnqr.ConvTranspose3d, nn.ConvTranspose3d, nnq.ConvTranspose3d, \
+        self._test_reference_module_impl(nn.ConvTranspose3d, nnq.ConvTranspose3d, \
                                         {'in_channels': 1, 'out_channels': 1, 'kernel_size': 1}, (16, 1, 10, 10, 10))
 
     def _test_activation_op_impl(
