6af440243acfc3ddd04590a31320360561c6168d
BowenBao <bowbao@microsoft.com>
2022-02-11 12:13:26 -0800

    address comments

--- torch/onnx/symbolic_opset11.py
+++ torch/onnx/symbolic_opset11.py
@@ -894,12 +894,21 @@ def embedding_bag(g,
     return loop.node().output(), None, None, None
 
 
-@parse_args('v', 'v', 'f', 'f')
+@parse_args("v", "v", "f", "f")
 def embedding_renorm(g, weight, indices, max_norm, norm_type):
     unique_indices = g.op("Unique", indices)
     partial_weight = g.op("Gather", weight, unique_indices)
-    norm_type = "ReduceL2" if int(norm_type) == 2 else "ReduceL1"
+    norm_type = int(norm_type)
+    if norm_type == 1:
+        norm_type = "ReduceL1"
+    elif norm_type == 2:
+        norm_type = "ReduceL2"
+    else:
+        raise RuntimeError(f"Unsupported: ONNX export of embedding_renorm with norm: {norm_type}. "
+                           "Only 1. and 2. are supported.")
     partial_weight_norm = g.op(norm_type, partial_weight, axes_i=[1], keepdims_i=1)
+    # https://github.com/pytorch/pytorch/blob/0a07488ed2c47765e337e290bd138c0e6e459cbd/aten/src/ATen/native/Embedding.cpp#L177
+    # Add 1e-7 to prevent division by zero.
     partial_weight_norm_ = g.op("Add", partial_weight_norm, g.op("Constant", value_t=torch.tensor(1e-7)))
     max_norm = torch.tensor(max_norm)
     scales = g.op("Div", max_norm, partial_weight_norm_)
