2910c70b5f3c07d766c3b453ce43dff9754e977c
fduwjj <fduwjj@fb.com>
2022-02-11 11:38:29 -0800

    [PT-D][Sharded Tensor] new init api for local tensor and sharding spec auto inference
    To improve the perf cost due to communication in the process of init the sharded tensor. There are two changes in this PR/diff:
    
    1. We create a new API named `_init_from_local_tensor` so that if we have only one local tensor, we can initiate a sharded tensor directly from it. (GH issue: https://github.com/pytorch/pytorch/issues/72092)
    2. We create a new API to infer the sharding spec from global meta data, so we don't have to manually set the sharding spec when it's not `EnumerableShardingSpec`. (GH issue: https://github.com/pytorch/pytorch/issues/67244)
    
    Differential Revision: [D34132739](https://our.internmc.facebook.com/intern/diff/D34132739/)
    
    [ghstack-poisoned]

--- torch/distributed/_shard/api.py
+++ torch/distributed/_shard/api.py
@@ -115,12 +115,7 @@ def _shard_tensor(
         )
     ]
 
-    st = ShardedTensor._init_from_local_shards(local_shards, tensor.size(), process_group=pg)
-
-    # Manually set sharding_spec
-    st._sharding_spec = sharding_spec
-
-    return st
+    return ShardedTensor._init_from_local_shards(local_shards, tensor.size(), process_group=pg)
 
 def shard_parameter(
         module: torch.nn.Module,
