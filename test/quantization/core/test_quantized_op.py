9ae1750a5779b4b8fedeb1eaf33e3f2e10840640
dzdang <dzdang@umich.edu>
2022-02-12 18:31:01 -0800

    test
    [ghstack-poisoned]

--- test/quantization/core/test_quantized_op.py
+++ test/quantization/core/test_quantized_op.py
@@ -4145,10 +4145,10 @@ class TestQuantizedConv(TestCase):
            # TODO: enable channelwise
            use_channelwise=st.sampled_from([False]))
     @skipIfNoFBGEMM
-    @unittest.skipIf(not TEST_CUDNN, "cudnn is not enabled.")
-    @unittest.skip("Local only - currently the qconv2d_cudnn op is bulid "
-                   "with USE_EXPERIMENTAL_CUDNN_V8_API, we can enable the test "
-                   "after it is built by default")
+    # @unittest.skipIf(not TEST_CUDNN, "cudnn is not enabled.")
+    # @unittest.skip("Local only - currently the qconv2d_cudnn op is bulid "
+    #                "with USE_EXPERIMENTAL_CUDNN_V8_API, we can enable the test "
+    #                "after it is built by default")
     def test_qconv2d_cudnn(
             self,
             batch_size,
