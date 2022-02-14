88590ef51a0e7ce3a8dfd63867ad16de97ae2e89
Alban Desmaison <albandes@fb.com>
2022-02-14 12:05:45 -0500

    Add new tls snapshot feature

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -1476,9 +1476,9 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
 
   void set_python_dispatch(bool k) {
     if (k) {
-      key_set_ = key_set_.add(DispatchKey::Python);
+      key_set_ = key_set_.add(DispatchKey::Python).add(DispatchKey::PythonTLSSnapshot);
     } else {
-      key_set_ = key_set_.remove(DispatchKey::Python);
+      key_set_ = key_set_.remove(DispatchKey::Python).remove(DispatchKey::PythonTLSSnapshot);
     }
   }
 
