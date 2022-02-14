81bf50c3f1c9adf95ad21ce4d67ea955f9608b0b
patel-zeel <patel_zeel@iitgn.ac.in>
2022-02-14 23:02:52 +0530

    Increase verbosity of NotImplementedError for KL divergence

--- torch/distributions/kl.py
+++ torch/distributions/kl.py
@@ -166,7 +166,7 @@ def kl_divergence(p, q):
         fun = _dispatch_kl(type(p), type(q))
         _KL_MEMOIZE[type(p), type(q)] = fun
     if fun is NotImplemented:
-        raise NotImplementedError
+        raise NotImplementedError("No KL(p || q) is implemented for p type {} and q type {}".format(p.__class__, q.__class__))
     return fun(p, q)
 
 
