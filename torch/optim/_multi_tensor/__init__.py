2dd9b77917d67223012cfe1719d0919a422c5428
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-02-15 09:52:30 -0800

    Optim foreach cleanup for AdamW (#70484)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70484
    
    Test Plan: Imported from OSS
    
    Reviewed By: anjali411
    
    Differential Revision: D33767869
    
    Pulled By: mikaylagawarecki
    
    fbshipit-source-id: 2f5273bbfeea3ed502c5d77da4bebe1674243e86

--- torch/optim/_multi_tensor/__init__.py
+++ torch/optim/_multi_tensor/__init__.py
@@ -8,7 +8,7 @@ from functools import partial
 from torch import optim
 
 Adam = partial(optim.Adam, foreach=True)
-from .adamw import AdamW
+AdamW = partial(optim.AdamW, foreach=True)
 NAdam = partial(optim.NAdam, foreach=True)
 SGD = partial(optim.SGD, foreach=True)
 RAdam = partial(optim.RAdam, foreach=True)
@@ -18,5 +18,3 @@ ASGD = partial(optim.ASGD, foreach=True)
 Adamax = partial(optim.Adamax, foreach=True)
 Adadelta = partial(optim.Adadelta, foreach=True)
 Adagrad = partial(optim.Adagrad, foreach=True)
-
-del adamw
