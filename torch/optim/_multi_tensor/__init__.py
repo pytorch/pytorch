0c6c2a04bd48def1d64b8cccd2f2feb39afb9929
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-02-15 09:52:30 -0800

    Optim foreach cleanup for Rprop (#70483)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70483
    
    Test Plan: Imported from OSS
    
    Reviewed By: anjali411
    
    Differential Revision: D33767866
    
    Pulled By: mikaylagawarecki
    
    fbshipit-source-id: ffc5ae68eeea8fa09385862b853b731554b77bcb
    (cherry picked from commit 3a0fe295807bb4519884a1838edeea1a9d222e41)

--- torch/optim/_multi_tensor/__init__.py
+++ torch/optim/_multi_tensor/__init__.py
@@ -13,11 +13,10 @@ NAdam = partial(optim.NAdam, foreach=True)
 SGD = partial(optim.SGD, foreach=True)
 RAdam = partial(optim.RAdam, foreach=True)
 RMSprop = partial(optim.RMSprop, foreach=True)
-from .rprop import Rprop
+Rprop = partial(optim.Rprop, foreach=True)
 ASGD = partial(optim.ASGD, foreach=True)
 Adamax = partial(optim.Adamax, foreach=True)
 Adadelta = partial(optim.Adadelta, foreach=True)
 Adagrad = partial(optim.Adagrad, foreach=True)
 
 del adamw
-del rprop
