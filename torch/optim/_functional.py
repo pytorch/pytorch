66233a8a3eaa073acdaeaa16ca83413da8a2d969
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-02-15 09:52:30 -0800

    Optim foreach cleanup for Adam (#70295)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70295
    
    Test Plan: Imported from OSS
    
    Reviewed By: anjali411
    
    Differential Revision: D33767870
    
    Pulled By: mikaylagawarecki
    
    fbshipit-source-id: f922f15ecb0307458c8ecee737325c42c4f3ce8b

--- torch/optim/_functional.py
+++ torch/optim/_functional.py
@@ -6,6 +6,7 @@ from typing import List, Optional
 
 from .adadelta import adadelta  # type: ignore[attr-defined] # noqa: F401
 from .adagrad import adagrad, _make_sparse  # type: ignore[attr-defined] # noqa: F401
+from .adam import adam  # type: ignore[attr-defined] # noqa: F401
 from .adamax import adamax  # type: ignore[attr-defined] # noqa: F401
 from .asgd import asgd  # type: ignore[attr-defined] # noqa: F401
 from .nadam import nadam  # type: ignore[attr-defined] # noqa: F401
@@ -13,59 +14,6 @@ from .radam import radam  # type: ignore[attr-defined] # noqa: F401
 
 
 # TODO: use foreach API in optim._functional to do all the computation
-
-def adam(params: List[Tensor],
-         grads: List[Tensor],
-         exp_avgs: List[Tensor],
-         exp_avg_sqs: List[Tensor],
-         max_exp_avg_sqs: List[Tensor],
-         state_steps: List[Tensor],
-         *,
-         amsgrad: bool,
-         beta1: float,
-         beta2: float,
-         lr: float,
-         weight_decay: float,
-         eps: float,
-         maximize: bool):
-    r"""Functional API that performs Adam algorithm computation.
-    See :class:`~torch.optim.Adam` for details.
-    """
-
-    if not all([isinstance(t, torch.Tensor) for t in state_steps]):
-        raise RuntimeError("API has changed, `state_steps` argument must contain a list of singleton tensors")
-
-    for i, param in enumerate(params):
-
-        grad = grads[i] if not maximize else -grads[i]
-        exp_avg = exp_avgs[i]
-        exp_avg_sq = exp_avg_sqs[i]
-        step_t = state_steps[i]
-        # update step
-        step_t += 1
-        step = step_t.item()
-
-        bias_correction1 = 1 - beta1 ** step
-        bias_correction2 = 1 - beta2 ** step
-
-        if weight_decay != 0:
-            grad = grad.add(param, alpha=weight_decay)
-
-        # Decay the first and second moment running average coefficient
-        exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-        exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
-        if amsgrad:
-            # Maintains the maximum of all 2nd moment running avg. till now
-            torch.maximum(max_exp_avg_sqs[i], exp_avg_sq, out=max_exp_avg_sqs[i])
-            # Use the max. for normalizing running avg. of gradient
-            denom = (max_exp_avg_sqs[i].sqrt() / math.sqrt(bias_correction2)).add_(eps)
-        else:
-            denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
-
-
-
-        step_size = lr / bias_correction1
-        param.addcdiv_(exp_avg, denom, value=-step_size)
 
 def adamw(params: List[Tensor],
           grads: List[Tensor],
