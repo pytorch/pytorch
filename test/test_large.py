b1775e2b72f7623e65fbae63c80062e5e2691ded
Jambay Kinley <jambaykinley@microsoft.com>
2022-02-14 18:15:11 -0800

    test files added

---
+++ test/test_large.py
@@ -0,0 +1,38 @@
+import torch
+
+def _compare_large_tensors(expected, actual, num_splits):
+    length = expected.shape[0]
+    for i in range(num_splits):
+        start = (i * length) // num_splits
+        end = ((i + 1) * length) // num_splits
+        print("  " + str(i))
+        torch.allclose(expected[start:end], actual[start:end], rtol=1.3e-06, atol=1e-05, equal_nan=True)
+
+def _test_large_cum_fn_helper(x, fn, num_splits):
+    x_cpu = x.cpu().float()
+    print(" Point 2")
+    expected = fn(x_cpu)
+    print(" Point 3")
+    actual = fn(x).cpu().float()
+    print(" Point 4")
+    _compare_large_tensors(expected, actual, num_splits)
+    print(" Pass")
+
+def test_large_cumprod(p, num_splits):
+    device = "cuda:0"
+    dtype = torch.float16
+    print("----------------------")
+    print("test_large")
+    print(p)
+    print("----------------------")
+
+    x = torch.empty(2**p + 200, device=device, dtype=dtype)
+    x[::3] = 8
+    x[1::3] = .25
+    x[2::3] = .5
+    print(" Point 1")
+    _test_large_cum_fn_helper(x, lambda x: torch.cumprod(x, 0), num_splits)
+
+import sys
+
+test_large_cumprod(int(sys.argv[1]), int(sys.argv[2]))
---
+++ test/test_large_pdist.py
@@ -0,0 +1,31 @@
+import torch
+
+def _compare_large_tensors(expected, actual, num_splits):
+    length = expected.shape[0]
+    for i in range(num_splits):
+        start = (i * length) // num_splits
+        end = ((i + 1) * length) // num_splits
+        print("  " + str(i))
+        torch.allclose(expected[start:end], actual[start:end], rtol=1.3e-06, atol=1e-05, equal_nan=True)
+
+def test_pdist_norm_large(size, num_splits):
+    device = "cuda:0"
+
+    print("----------------------")
+    print("test_pdist")
+    print(size)
+    print("----------------------")
+
+    x = torch.randn(size, 1, dtype=torch.float32)
+    print(" Point 1")
+    expected_cpu = torch.pdist(x, p=2)
+    print(" Point 2")
+    actual_gpu = torch.pdist(x.to(device), p=2)
+    print(" Point 3")
+    _compare_large_tensors(expected_cpu, actual_gpu.cpu(), num_splits)
+    print(" Pass")
+            
+
+import sys
+
+test_pdist_norm_large(int(sys.argv[1]), int(sys.argv[2]))
