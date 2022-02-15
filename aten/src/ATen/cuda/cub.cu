fb65be0da6f9af08f95f189cbd62dd9c0bdff30e
Pruthvi Madugundu <pmagundu@amd.com>
2022-02-15 10:54:12 -0800

    Updates for review comments

--- aten/src/ATen/cuda/cub.cu
+++ aten/src/ATen/cuda/cub.cu
@@ -57,7 +57,10 @@ AT_INSTANTIATE_SORT_PAIRS(int64_t, 4)
 
 AT_FORALL_SCALAR_TYPES_AND2(Bool, Half, AT_INSTANTIATE_SORT_PAIRS_8)
 
+// BFloat16 Radix sort is supported from ROCm 4.5 onwards
+#if !AT_ROCM_ENABLED() || (AT_ROCM_ENABLED() && ROCM_VERSION >= 40500)
 AT_INSTANTIATE_SORT_PAIRS(c10::BFloat16, 8)
+#endif
 
 }  // namespace detail
 
