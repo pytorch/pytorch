80f23469dd5cbb4b4d52cfdd628974697aa98e99
Nikita Shulga <nshulga@fb.com>
2022-02-15 07:15:04 -0800

    Revert D34152115: [pytorch][PR] [ROCm] Enable sort operator BF16 support
    Test Plan: revert-hammer
    
    Differential Revision:
    D34152115 (https://github.com/pytorch/pytorch/commit/aa44480b40075d4f696605f3ab7a03d87372b4e2)
    
    Original commit changeset: 53841c91976b
    
    Original Phabricator Diff: D34152115 (https://github.com/pytorch/pytorch/commit/aa44480b40075d4f696605f3ab7a03d87372b4e2)
    
    fbshipit-source-id: c9b5cc06198032af73cd6390466de2c62576a1e1
    (cherry picked from commit eb72533ae9723610cc3974f5c45a827c438460f1)

--- test/test_sort_and_select.py
+++ test/test_sort_and_select.py
@@ -135,6 +135,8 @@ class TestSortAndSelect(TestCase):
     # FIXME: remove torch.bool from unsupported types once support is added for cub sort
     @dtypes(*set(get_all_dtypes()) - {torch.bool, torch.complex64, torch.complex128})
     def test_stable_sort(self, device, dtype):
+        if TEST_WITH_ROCM and dtype == torch.bfloat16:
+            return
         sizes = (100, 1000, 10000)
         for ncopies in sizes:
             x = torch.tensor([0, 1] * ncopies, dtype=dtype, device=device)
@@ -228,6 +230,8 @@ class TestSortAndSelect(TestCase):
     # FIXME: remove torch.bool from unsupported types once support is added for cub sort
     @dtypes(*set(get_all_dtypes()) - {torch.bool, torch.complex64, torch.complex128})
     def test_stable_sort_against_numpy(self, device, dtype):
+        if TEST_WITH_ROCM and dtype == torch.bfloat16:
+            return
         if dtype in floating_types_and(torch.float16, torch.bfloat16):
             inf = float('inf')
             neg_inf = -float('inf')
@@ -291,6 +295,9 @@ class TestSortAndSelect(TestCase):
 
     @dtypes(*(get_all_int_dtypes() + get_all_fp_dtypes()))
     def test_msort(self, device, dtype):
+        if TEST_WITH_ROCM and dtype == torch.bfloat16:
+            return
+
         def test(shape):
             tensor = make_tensor(shape, device, dtype, low=-9, high=9)
             if tensor.size() != torch.Size([]):
