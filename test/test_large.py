0b0349ae5419ce2e76023671af293b8e71358086
Jambay Kinley <jambaykinley@microsoft.com>
2022-02-14 21:21:11 -0800

    use sys.stdout.write

--- test/test_large.py
+++ test/test_large.py
@@ -1,38 +1,37 @@
 import torch
+import sys
 
 def _compare_large_tensors(expected, actual, num_splits):
     length = expected.shape[0]
     for i in range(num_splits):
         start = (i * length) // num_splits
         end = ((i + 1) * length) // num_splits
-        print("  " + str(i))
+        sys.stdout.write("  " + str(i))
         torch.allclose(expected[start:end], actual[start:end], rtol=1.3e-06, atol=1e-05, equal_nan=True)
 
 def _test_large_cum_fn_helper(x, fn, num_splits):
     x_cpu = x.cpu().float()
-    print(" Point 2", flush=True)
+    sys.stdout.write(" Point 2" + "\n")
     expected = fn(x_cpu)
-    print(" Point 3", flush=True)
+    sys.stdout.write(" Point 3" + "\n")
     actual = fn(x).cpu().float()
-    print(" Point 4", flush=True)
+    sys.stdout.write(" Point 4" + "\n")
     _compare_large_tensors(expected, actual, num_splits)
-    print(" Pass", flush=True)
+    sys.stdout.write(" Pass" + "\n")
 
 def test_large_cumprod(p, num_splits):
     device = "cuda:0"
     dtype = torch.float16
-    print("----------------------", flush=True)
-    print("test_large", flush=True)
-    print(p, flush=True)
-    print("----------------------", flush=True)
+    sys.stdout.write("----------------------" + "\n")
+    sys.stdout.write("test_large" + "\n")
+    sys.stdout.write(str(p) + "\n")
+    sys.stdout.write("----------------------" + "\n")
 
     x = torch.empty(2**p + 200, device=device, dtype=dtype)
     x[::3] = 8
     x[1::3] = .25
     x[2::3] = .5
-    print(" Point 1", flush=True)
+    sys.stdout.write(" Point 1" + "\n")
     _test_large_cum_fn_helper(x, lambda x: torch.cumprod(x, 0), num_splits)
-
-import sys
 
 test_large_cumprod(int(sys.argv[1]), int(sys.argv[2]))
