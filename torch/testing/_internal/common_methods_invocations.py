fbd2189d69bbc6202aef03024e75383c3a6877a3
Peter Bell <peterbell10@live.co.uk>
2022-02-16 02:40:11 +0000

    Update on "stft: move towards always returning complex"
    For `stft` this makes all cases where `return_complex` default to
    `False` into an error and adds a warning when `return_complex=False`
    is passed explicitly.
    
    For `istft` this raises an error if the input is not a complex tensor.
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -4906,11 +4906,17 @@ def sample_inputs_stft(op_info, device, dtype, requires_grad, **kwargs):
     def mt(shape, **kwargs):
         return make_tensor(shape, device=device, dtype=dtype,
                            requires_grad=requires_grad, **kwargs)
-    yield SampleInput(mt(100), kwargs=dict(n_fft=10))
+
+    yield SampleInput(mt(100), kwargs=dict(n_fft=10, return_complex=True))
+    yield SampleInput(mt(100), kwargs=dict(n_fft=10, return_complex=False))
+    if dtype.is_complex:
+        yield SampleInput(mt(100), kwargs=dict(n_fft=10))
 
     for center in [False, True]:
-        yield SampleInput(mt(10), kwargs=dict(n_fft=7, center=center))
-        yield SampleInput(mt((10, 100)), kwargs=dict(n_fft=16, hop_length=4, center=center))
+        yield SampleInput(mt(10), kwargs=dict(n_fft=7, center=center,
+                                              return_complex=True))
+        yield SampleInput(mt((10, 100)), kwargs=dict(n_fft=16, hop_length=4,
+                                                     center=center, return_complex=True))
 
     window = make_tensor(16, low=.5, high=2.0, dtype=dtype, device=device, requires_grad=requires_grad)
     yield SampleInput(
@@ -4919,7 +4925,8 @@ def sample_inputs_stft(op_info, device, dtype, requires_grad, **kwargs):
         mt((3, 100)), kwargs=dict(n_fft=16, window=window, return_complex=True, center=center))
     if not dtype.is_complex:
         yield SampleInput(
-            mt((10, 100)), kwargs=dict(n_fft=16, window=window, onesided=False))
+            mt((10, 100)), kwargs=dict(n_fft=16, window=window, onesided=False,
+                                       return_complex=True))
 
 
 def sample_inputs_istft(op_info, device, dtype, requires_grad, **kwargs):
@@ -9512,7 +9519,7 @@ op_db: List[OpInfo] = [
                # gradcheck fails on ROCm (gh-68429)
                DecorateInfo(skipCUDAIfRocm, 'TestGradients', 'test_fn_grad'),
            ],
-           dtypes=floating_and_complex_types(),
+           dtypes=complex_types(),
            sample_inputs_func=sample_inputs_istft,
            check_batched_grad=False,
            check_batched_gradgrad=False,
