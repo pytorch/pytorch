889f3f48b2ef0fb27cff5a1a474d7c316fd7b5d4
Will Constable <whc@fb.com>
2022-02-16 11:23:00 -0800

    Revert D34178476: Update lazy_ir.py from lazy_tensor_staging
    Test Plan: revert-hammer
    
    Differential Revision:
    D34178476 (https://github.com/pytorch/pytorch/commit/3842140fd5eb6dcb7eaab5628a79a9093aac5a24)
    
    Original commit changeset: 7190b2e0d82b
    
    Original Phabricator Diff: D34178476 (https://github.com/pytorch/pytorch/commit/3842140fd5eb6dcb7eaab5628a79a9093aac5a24)
    
    fbshipit-source-id: 4c969a355f01244c6f5acc52bc31679f2182aa55
    (cherry picked from commit 17082075ddc6bae93981d95eb4512856c3f91bb1)

--- torch/csrc/lazy/ts_backend/ts_node.h
+++ torch/csrc/lazy/ts_backend/ts_node.h
@@ -55,7 +55,7 @@ class TORCH_API TsNode : public lazy::Node {
 
   std::string ToString() const override;
 
-  static hash_t GetOpHash(OpKind op, const Shape& shape, hash_t hash_seed, bool bakeInSizes);
+  static hash_t GetOpHash(OpKind op, const Shape& shape, hash_t hash_seed);
 
   const std::vector<Output>& operands() const override {
     return operands_as_outputs_;
