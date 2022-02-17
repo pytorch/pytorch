fix visual indent. silent mypy.

torch/distributions/kl.py
@@ -818,8 +818,8 @@ def add_kl_info():
     """Appends a list of implemented KL functions to the doc for kl_divergence."""
     rows = ["KL divergence is currently implemented for the following distribution pairs:"]
     for p, q in sorted(_KL_REGISTRY,
-                        key=lambda p_q: (p_q[0].__name__, p_q[1].__name__)):
+                       key=lambda p_q: (p_q[0].__name__, p_q[1].__name__)):
         rows.append("* :class:`~torch.distributions.{}` and :class:`~torch.distributions.{}`"
                     .format(p.__name__, q.__name__))
     kl_info = '\n\t'.join(rows)
-    kl_divergence.__doc__ += kl_info
+    kl_divergence.__doc__ += kl_info # type: ignore[operator]