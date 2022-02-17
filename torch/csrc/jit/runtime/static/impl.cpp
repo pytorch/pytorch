d1c5f9e43993157f1c66e835c98624e4daef8564
Mike Iovine <mikeiovine@fb.com>
2022-02-17 10:18:33 -0800

    [JIT][SR] Introduce prim::IfThenElse (#72587)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/72587
    
    This pattern frequently appears in a few graphs:
    
    ```
    %result = prim::If(%condition)
      block0():
        -> (%a)
      block1():
        -> (%b)
    ```
    
    This is slow, particularly in static runtime. Static runtime creates memory planners/block runners for each sub-block, which eats up a lot of memory and introduces a lot of extra overhead for this relatively simple operation.
    
    This diff introduces a new op that replaces nodes like the above with a single op meant to act like a ternary operator:
    
    ```
    %result = prim::IfThenElse(%condition, %a, %b)
    ```
    
    Test Plan: New unit tests
    
    Reviewed By: eellison
    
    Differential Revision: D34091789
    
    fbshipit-source-id: eb6a8c460c39b4c019a1f4ab1f3f1e5b6edc400c
    (cherry picked from commit 0f1b335e5b83f402bda2dcdd9ecb411e0b67c651)

--- torch/csrc/jit/runtime/static/impl.cpp
+++ torch/csrc/jit/runtime/static/impl.cpp
@@ -12,6 +12,7 @@
 #include <caffe2/core/timer.h>
 #include <torch/csrc/jit/ir/alias_analysis.h>
 #include <torch/csrc/jit/jit_log.h>
+#include <torch/csrc/jit/passes/add_if_then_else.h>
 #include <torch/csrc/jit/passes/canonicalize.h>
 #include <torch/csrc/jit/passes/dead_code_elimination.h>
 #include <torch/csrc/jit/passes/eliminate_no_ops.h>
@@ -173,6 +174,7 @@ void OptimizeGraph(
   UseVariadicGroupedAccessor(graph);
   EliminateNoOps(
       graph, /* custom_ops */ {fromQualString("fb::scale_gradient")});
+  AddIfThenElseOp(graph);
   GRAPH_DUMP("Final graph after optimizations: ", graph);
 }
 
@@ -1846,8 +1848,9 @@ static bool checkNoMemoryOverlap(const at::Tensor& a, const at::Tensor& b) {
 }
 
 bool ProcessedNode::verify_no_memory_overlap(bool force_check) const {
-  const static std::array<c10::Symbol, 5> special_case_ops = {
+  const static std::array<c10::Symbol, 6> special_case_ops = {
       fromQualString("prim::TypeCheck"),
+      fromQualString("prim::IfThenElse"),
       fromQualString("static_runtime::select_tensor"),
       fromQualString("static_runtime::VarTupleUnpack"),
       fromQualString("static_runtime::dict_unpack"),
