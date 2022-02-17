dbfb2f95838684752b4ab075d2a90ac3bb85be4b
Michael Suo <suo@fb.com>
2022-02-16 20:28:18 -0800

    Update on "[ci] switch arm mac jobs to periodic, delete fulljit jobs"
    Also switch the custom-ops and metal build/test to x86 instead of ARM.
    
    As discussed in #72933
    
    [ghstack-poisoned]

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -747,24 +747,26 @@ IOS_WORKFLOWS = [
     ),
     CIWorkflow(
         arch="macos",
-        build_environment="ios-12-5-1-x86-64-custom-ops",
-        ios_arch="x86_64",
+        build_environment="ios-12-5-1-arm64-custom-ops",
+        ios_arch="arm64",
         ios_platform="OS",
         test_runner_type=MACOS_TEST_RUNNER_10_15,
+        is_scheduled="45 4,10,16,22 * * *",
         exclude_test=True,
         ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_IOS, LABEL_CIFLOW_MACOS},
+            labels={LABEL_CIFLOW_SCHEDULED, LABEL_CIFLOW_IOS, LABEL_CIFLOW_MACOS},
         ),
     ),
     CIWorkflow(
         arch="macos",
-        build_environment="ios-12-5-1-x86-64-metal",
-        ios_arch="x86_64",
+        build_environment="ios-12-5-1-arm64-metal",
+        ios_arch="arm64",
         ios_platform="OS",
         test_runner_type=MACOS_TEST_RUNNER_10_15,
+        is_scheduled="45 4,10,16,22 * * *",
         exclude_test=True,
         ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_IOS, LABEL_CIFLOW_MACOS},
+            labels={LABEL_CIFLOW_SCHEDULED, LABEL_CIFLOW_IOS, LABEL_CIFLOW_MACOS},
         ),
     ),
     CIWorkflow(
