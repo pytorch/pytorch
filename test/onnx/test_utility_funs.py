61aa6607b222f1b9a8e692f3292a8466326c910f
BowenBao <bowbao@microsoft.com>
2022-02-17 14:49:27 -0800

    Add new test for predefined attributes

--- test/onnx/test_utility_funs.py
+++ test/onnx/test_utility_funs.py
@@ -802,6 +802,46 @@ class TestUtilityFuns_opset9(_BaseTestCase):
         funcs = onnx_model.functions
         self.assertIn("M", [f.name for f in funcs])
 
+    @skipIfUnsupportedMinOpsetVersion(15)
+    def test_local_function_predefined_attributes(self):
+        class M(torch.nn.Module):
+            num_layers: int
+
+            def __init__(self, num_layers):
+                super().__init__()
+                self.num_layers = num_layers
+                self.lns = torch.nn.ModuleList([torch.nn.LayerNorm(3, eps=1e-4) for _ in range(num_layers)])
+
+            def forward(self, x):
+                for ln in self.lns:
+                    x = ln(x)
+                return x
+
+        x = torch.randn(2, 3)
+        f = io.BytesIO()
+        torch.onnx.export(M(3), (x, ), f, export_modules_as_functions=True,
+                          opset_version=self.opset_version)
+
+        onnx_model = onnx.load(io.BytesIO(f.getvalue()))
+        funcs = onnx_model.functions
+        m_funcs = [fn for fn in funcs if fn.name == "M"]
+        self.assertEqual(m_funcs[0].attribute, ["num_layers"])
+        ln_funcs = [fn for fn in funcs if fn.name == "LayerNorm"]
+        self.assertEqual(ln_funcs[0].attribute, ["eps", "elementwise_affine"])
+
+        from onnx import helper
+        m_node = [n for n in onnx_model.graph.node if n.op_type == "M"]
+        self.assertEqual(m_node[0].attribute[0], helper.make_attribute("num_layers", 3))
+
+        ln_nodes = [n for n in m_funcs[0].node if n.op_type == "LayerNorm"]
+        expected_ln_attrs = [
+            helper.make_attribute("elementwise_affine", 1),
+            helper.make_attribute("eps", 1e-4)
+        ]
+        for ln_node in ln_nodes:
+            self.assertIn(ln_node.attribute[0], expected_ln_attrs)
+            self.assertIn(ln_node.attribute[1], expected_ln_attrs)
+
     def test_aten_fallthrough(self):
         # Test aten export of op with no symbolic
         class Module(torch.nn.Module):
