32f6a1e2a2e0afb0f94c7f2cfc84fdaae5c90f85
BowenBao <bowbao@microsoft.com>
2022-02-17 10:45:23 -0800

    [ONNX] First version of quantized model export: Support quantized.Linear (#69232)
    Co-authored-by: David Fan <jiafamicrosoft.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/72986

--- torch/csrc/onnx/init.cpp
+++ torch/csrc/onnx/init.cpp
@@ -119,8 +119,9 @@ void initONNXBindings(PyObject* module) {
       .def(
           "_jit_pass_onnx_unpack_quantized_weights",
           [](std::shared_ptr<Graph>& graph,
-             std::map<std::string, IValue>& paramsDict) {
-            UnpackQuantizedWeights(graph, paramsDict);
+             std::map<std::string, IValue>& paramsDict,
+             bool caffe2) {
+            UnpackQuantizedWeights(graph, paramsDict, caffe2);
             return paramsDict;
           },
           pybind11::return_value_policy::move)
