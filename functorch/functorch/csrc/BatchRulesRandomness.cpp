fba8c2699bd86f6f773b4adccc9fbcba44d6d2d8
Samantha Andow <samdow@fb.com>
2022-02-18 13:56:54 -0500

    [functorch] randperm support (pytorch/functorch#411)
    [ghstack-poisoned]

--- functorch/functorch/csrc/BatchRulesRandomness.cpp
+++ functorch/functorch/csrc/BatchRulesRandomness.cpp
@@ -35,6 +35,26 @@ Tensor random_batching_rule(IntArrayRef shape, ExtraArgs... extra_args) {
   }
 }
 
+template <typename F, F Func, typename... ExtraArgs>
+Tensor randperm_batching_rule(int64_t n, ExtraArgs... extra_args) {
+  c10::impl::ExcludeDispatchKeyGuard guard(kVmapModeKey);
+  auto maybe_layer = maybeCurrentDynamicLayer();
+  auto const batch_size = maybe_layer->batchSize();
+  RandomnessType randomness = maybe_layer->randomness();
+  check_randomness(randomness);
+  if (randomness == RandomnessType::Different) {
+    std::vector<at::Tensor> stackedList(batch_size);
+    stackedList.reserve(batch_size);
+    for (int64_t idx = 0; idx < batch_size; ++idx) {
+      // since this is done in a loop, need to pass by reference for generator to update
+      stackedList[idx] = Func(n, extra_args...);
+    }
+    return makeBatched(at::stack(stackedList), 0, maybe_layer->layerId());
+  } else {
+    return Func(n, std::forward<ExtraArgs>(extra_args)...);
+  }
+}
+
 template <typename A, A a, typename C>
 struct RandomBatchRuleHelper;
 
@@ -79,6 +99,16 @@ struct RandIntLowBatchRuleHelper<F, Func, typelist<T1, T2, T3, T...>> {
   }
 };
 
+template <typename A, A a, typename C>
+struct RandpermBatchRuleHelper;
+
+template <typename F, F Func, typename T1, typename... T>
+struct RandpermBatchRuleHelper<F, Func, typelist<T1, T...>> {
+  static Tensor apply(int64_t n, T... extra_args) {
+    return randperm_batching_rule<F, Func, T...>(n, std::forward<T>(extra_args)...);
+  }
+};
+
 TORCH_LIBRARY_IMPL(aten, FuncTorchVmapMode, m) {
   #define RANDOM_BATCH_RULE(op) \
     m.impl(#op, SINGLE_ARG(\
@@ -104,6 +134,15 @@ TORCH_LIBRARY_IMPL(aten, FuncTorchVmapMode, m) {
     m.impl(#op"."#overload, SINGLE_ARG(\
       RandIntLowBatchRuleHelper<decltype(&ATEN_FN2(op, overload)), &ATEN_FN2(op, overload), \
                                 c10::guts::function_traits<decltype(ATEN_FN2(op, overload))>::parameter_types>::apply))
+  #define RANDPERM_BATCH_RULE(op) \
+    m.impl(#op, SINGLE_ARG(\
+      RandpermBatchRuleHelper<decltype(&ATEN_FN(op)), &ATEN_FN(op), \
+                            c10::guts::function_traits<decltype(ATEN_FN(op))>::parameter_types>::apply))
+
+  #define RANDPERM_BATCH_RULE2(op, overload) \
+    m.impl(#op"."#overload, SINGLE_ARG(\
+      RandpermBatchRuleHelper<decltype(&ATEN_FN2(op, overload)), &ATEN_FN2(op, overload), \
+                            c10::guts::function_traits<decltype(ATEN_FN2(op, overload))>::parameter_types>::apply))
 
   RANDOM_BATCH_RULE(randn);
   RANDOM_BATCH_RULE2(randn, generator);
@@ -120,10 +159,15 @@ TORCH_LIBRARY_IMPL(aten, FuncTorchVmapMode, m) {
   RANDINT_LOW_BATCH_RULE(randint, low);
   RANDINT_LOW_BATCH_RULE(randint, low_generator);
 
+  RANDPERM_BATCH_RULE(randperm);
+  RANDPERM_BATCH_RULE2(randperm, generator);
+
   #undef RANDOM_BATCH_RULE
   #undef RANDOM_BATCH_RULE2
   #undef RANDINT_BATCH_RULE
   #undef RANDINT_BATCH_RULE2
   #undef RANDINT_LOW_BATCH_RULE
+  #undef RANDPERM_BATCH_RULE
+  #undef RANDPERM_BATCH_RULE2
 }
 }} // namespace at::functorch
