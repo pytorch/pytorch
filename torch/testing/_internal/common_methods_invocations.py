54748cdb4d1525e880062fd3d673eeec50626ccb
Nikita Shulga <nshulga@fb.com>
2022-02-22 11:19:49 -0800

    `scatter_reduce` documentation (#73125)
    Summary:
    Reland of https://github.com/pytorch/pytorch/issues/68580 (which were milestoned for 1.11) plus partial revert of https://github.com/pytorch/pytorch/pull/72543
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73125
    
    Reviewed By: bdhirsh
    
    Differential Revision: D34355217
    
    Pulled By: malfet
    
    fbshipit-source-id: 325ecdeaf53183d653b44ee5e6e8839ceefd9200
    (cherry picked from commit 71db31748a8adcd8f95d5faf04aaa454e9c4c760)
    (cherry picked from commit cfb6c942fed64dbb81ccc4f14b2a6650123af2e1)

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -15472,7 +15472,7 @@ op_db: List[OpInfo] = [
         supports_fwgrad_bwgrad=True,
     ),
     OpInfo(
-        '_scatter_reduce',
+        'scatter_reduce',
         dtypes=all_types_and(torch.float16, torch.bfloat16),
         sample_inputs_func=sample_inputs_scatter_reduce,
         supports_out=False,
