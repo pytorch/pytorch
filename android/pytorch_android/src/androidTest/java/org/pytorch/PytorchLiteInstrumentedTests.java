1a08e79b492b1137cab505133366bd87b986ab3f
Linbin Yu <linbin@fb.com>
2022-02-23 16:59:02 -0800

    merge

--- android/pytorch_android/src/androidTest/java/org/pytorch/PytorchLiteInstrumentedTests.java
+++ android/pytorch_android/src/androidTest/java/org/pytorch/PytorchLiteInstrumentedTests.java
@@ -1,26 +1,14 @@
 package org.pytorch;
 
 import android.content.Context;
-<<<<<<< HEAD
-
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
-
-import org.junit.runner.RunWith;
-
-=======
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
->>>>>>> 5eb5b61221759012909112a6a8de6b3306aa8dea
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-<<<<<<< HEAD
-=======
 import org.junit.runner.RunWith;
->>>>>>> 5eb5b61221759012909112a6a8de6b3306aa8dea
 
 @RunWith(AndroidJUnit4.class)
 public class PytorchLiteInstrumentedTests extends PytorchTestBase {
@@ -51,8 +39,4 @@ public class PytorchLiteInstrumentedTests extends PytorchTestBase {
       throw e;
     }
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> 5eb5b61221759012909112a6a8de6b3306aa8dea
 }
