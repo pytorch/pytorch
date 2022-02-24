29f15ebea4afe14bd6a87b67991f9155e5a3c4ed
Mengwei Liu <larryliu@fb.com>
2022-02-23 17:22:09 -0800

    [PyTorch] Enable test

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -474,10 +474,9 @@ LINUX_WORKFLOWS = [
         build_environment="linux-xenial-py3-clang5-mobile-lightweight-dispatch-build",
         docker_image_base=f"{DOCKER_REGISTRY}/pytorch/pytorch-linux-xenial-py3-clang5-android-ndk-r19c",
         test_runner_type=LINUX_CPU_TEST_RUNNER,
-        build_generates_artifacts=False,
-        exclude_test=True,
+        distributed_test=False,
         ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_MOBILE, LABEL_CIFLOW_DEFAULT},
+            labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_MOBILE, LABEL_CIFLOW_LIBTORCH, LABEL_CIFLOW_CPU},
         ),
     ),
     CIWorkflow(
