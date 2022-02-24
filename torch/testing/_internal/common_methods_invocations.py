6dee11d2ff2997403514dc72a4ca722cc31be81d
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-02-25 00:22:05 +0530

    remove commits from other branch, draft here

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -2206,7 +2206,7 @@ def sample_inputs_addcmul_addcdiv(op_info, device, dtype, requires_grad, **kwarg
 
     sample_inputs = []
     for input_args, broadcasts_input in test_cases:
-        args = tuple(make_tensor(arg, device, dtype, requires_grad=requires_grad, exclude_zero=True) if isinstance(arg, tuple) else arg
+        args = tuple(make_tensor(arg, device, dtype, requires_grad=requires_grad) if isinstance(arg, tuple) else arg
                      for arg in input_args)
         sample_inputs.append(SampleInput(
             args[0],
