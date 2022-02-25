4399b23261f807249abd18a39b0f28e436da6ba8
Kelly Stanton <kpstanton@fb.com>
2022-02-25 15:13:06 -0600

    moved requirements-ci.txt to /opt/conda

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -110,7 +110,7 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
     tb-nightly \
     librosa>=0.6.2
 
-  as_jenkins pip install --progress-bar off -r ~/requirements-ci.txt
+  as_jenkins pip install --progress-bar off -r /opt/conda/requirements-ci.txt
 
   # Install numba only on python-3.8 or below
   # For numba issue see https://github.com/pytorch/pytorch/issues/51511
