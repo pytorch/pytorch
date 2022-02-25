2a43cc1223adb4f9dc2ac403264326a53eba80c4
Dhruv Matani <dhruvbird@fb.com>
2022-02-24 16:18:50 -0800

    [RFC] Implement c10::Synchronized<T>, a basic error-avoiding synchronization wrapper for data
    After adding mutex/lock in a previous change, I realized that `folly::Synchronized<T>` exists for this reason. However, we can't use it, so I have implemented a basic version for use in `c10` in this change.
    
    Reviewers, please check if this looks reasonable as a minimal first step to enforce that data that is supposed to be used in a multi-threaded context ends up being locked before use. i.e. don't rely on the developer to do the right thing, instead enforce it using this abstraction.
    
    Differential Revision: [D34465420](https://our.internmc.facebook.com/intern/diff/D34465420/)
    
    [ghstack-poisoned]

---
+++ c10/util/Synchronized.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <mutex>
+
+namespace c10 {
+
+/**
+ * A very simple Synchronization class for error-free use of data
+ * in a multi-threaded context. See folly/docs/Synchronized.md for
+ * the inspiration of this class.
+ *
+ * This class implements a small subset of the generic functionality
+ * implemented by folly:Synchronized<T>.
+ */
+template <typename T>
+class Synchronized final {
+  mutable std::mutex mutex_;
+  T data_;
+
+public:
+  Synchronized() = default;
+  Synchronized(T const& data): data_(data) {}
+  Synchronized(T&& data): data_(data) {}
+
+  Synchronized(Synchronized const&) = delete;
+  Synchronized(Synchronized&&) = delete;
+  Synchronized operator=(Synchronized const&) = delete;
+  Synchronized operator=(Synchronized&&) = delete;
+
+  template <typename CB>
+  void withLock(CB cb) {
+    std::lock_guard<std::mutex> guard(this->mutex_);
+    cb(this->data_);
+  }
+
+  template <typename CB>
+  void withLock(CB cb) const {
+    std::lock_guard<std::mutex> guard(this->mutex_);
+    cb(this->data_);
+  }
+};
+} // end namespace c10
