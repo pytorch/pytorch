235ce217af2dd8736731dbd7761039f242b9e53a
Jerry Zhang <jerryzh168@gmail.com>
2022-02-25 09:47:25 -0800

    [quant][core] Add Embedding and EmbeddingBag reference module
    Summary:
    This PR adds support reference module support for Embedding and EmbeddingBag, following https://github.com/pytorch/rfcs/blob/master/RFC-0019-Extending-PyTorch-Quantization-to-Custom-Backends.md
    
    * the reference module inherits from the corresponding float module (e.g. nn.Embedding), and the ReferenceQuantizedModule (which defines some utility functions to store qparms for a single weight)
    * in forward, we first quantize and then dequantize weight (to generate the pattern) and then feed the weight to the original fp32 op
    
    We'll connect this with fx grpah mode quantization later, in the final PR that deprecates the current convert implementation. Since current convert doesn't
    support emitting quantize_per_tensor_dynamic ops, we don't want to implement it and immediately throw away the code, so might be better to just implement this
    in the final flow.
    
    Test Plan:
    Will be tested later, in the final PR that deprecates the current convert implementation
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/nn/quantized/_reference/modules/__init__.py
+++ torch/nn/quantized/_reference/modules/__init__.py
@@ -1,6 +1,7 @@
 from .linear import Linear
 from .conv import Conv1d, Conv2d, Conv3d, ConvTranspose1d, ConvTranspose2d, ConvTranspose3d
 from .rnn import RNNCell, LSTMCell, GRUCell, LSTM
+from .sparse import Embedding, EmbeddingBag
 
 __all__ = [
     'Linear',
@@ -14,4 +15,6 @@ __all__ = [
     'LSTMCell',
     'GRUCell',
     'LSTM',
+    'Embedding',
+    'EmbeddingBag',
 ]
