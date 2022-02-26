9d08c2bb02aa26c62aaed9cf90684f14f3351ccc
Kelly Stanton <kpstanton@fb.com>
2022-02-25 23:07:50 -0600

    replaced manual package install in install_conda

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -97,20 +97,20 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   # Pin SciPy because of failing distribution tests (see #60347)
   # Pin MyPy version because new errors are likely to appear with each release
   # Pin hypothesis to avoid flakiness: https://github.com/pytorch/pytorch/issues/31136
-  as_jenkins pip install --progress-bar off pytest \
-    scipy==1.6.3 \
-    scikit-image \
-    psutil \
-    unittest-xml-reporting \
-    boto3==1.16.34 \
-    hypothesis==4.53.2 \
-    expecttest==0.1.3 \
-    mypy==0.812 \
-    tb-nightly \
-    librosa>=0.6.2
+  # as_jenkins pip install --progress-bar off pytest \
+  #   scipy==1.6.3 \
+  #   scikit-image \
+  #   psutil \
+  #   unittest-xml-reporting \
+  #   boto3==1.16.34 \
+  #   hypothesis==4.53.2 \
+  #   expecttest==0.1.3 \
+  #   mypy==0.812 \
+  #   tb-nightly \
+  #   librosa>=0.6.2
 
   as_jenkins pip install --progress-bar off -r /opt/conda/requirements-ci.txt
-  as_jenkins pip freeze
+
   # Install numba only on python-3.8 or below
   # For numba issue see https://github.com/pytorch/pytorch/issues/51511
   if [[ $(python -c "import sys; print(int(sys.version_info < (3, 9)))") == "1" ]]; then
