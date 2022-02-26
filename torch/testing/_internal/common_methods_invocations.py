ee104253fdda29e3e46ad2b7e74c8cfeabd525d6
Natalia Gimelshein <ngimel@fb.com>
2022-02-26 12:02:39 -0800

    product of devices

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -7033,12 +7033,12 @@ def sample_inputs_where(op_info, device, dtype, requires_grad, **kwargs):
 def error_inputs_where(op_info, device, **kwargs):
     shape = (S,)
     err_msg = "Expected all tensors to be on the same device"
-    for devices in [('cpu', device, device), (device, 'cpu', 'cpu'),
-                        (device, 'cpu', device), ('cpu', device, 'cpu')]:
-        si = SampleInput(make_tensor(shape, device=devices[0], dtype=torch.float32),
-                         args=(make_tensor(shape, dtype=torch.bool, device=devices[1]),
-                         make_tensor(shape, device=devices[2], dtype=torch.float32)))
-        yield ErrorInput(si, error_type=RuntimeError, error_regex=err_msg)
+    for devices in product(('cpu', device), repeat=3):
+        if len(set(devices)) == 2:
+            si = SampleInput(make_tensor(shape, device=devices[0], dtype=torch.float32),
+                            args=(make_tensor(shape, dtype=torch.bool, device=devices[1]),
+                            make_tensor(shape, device=devices[2], dtype=torch.float32)))
+            yield ErrorInput(si, error_type=RuntimeError, error_regex=err_msg)
 
 def sample_inputs_nonzero(op_info, device, dtype, requires_grad, **kwargs):
     make_arg = partial(make_tensor, dtype=dtype, device=device, requires_grad=requires_grad)
