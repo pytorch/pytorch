ebfa7056baf60d47f38bd290d4c2aa1289edb1dd
Yanli Zhao <yanlizhao@fb.com>
2022-02-28 19:40:47 -0800

    Back out "[DDP][BE] Remove bucket replicas" (#73523)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73523
    
    Original commit changeset: de8124e1628d
    
    Original Phabricator Diff: D34399176 (https://github.com/pytorch/pytorch/commit/7fcf6c5c022268a8685e3aea9caf6648b8112549)
    ghstack-source-id: 150128616
    
    Test Plan: unit tests
    
    Reviewed By: rohan-varma
    
    Differential Revision: D34527919
    
    fbshipit-source-id: 00914eeb6c84188b89d8fa8b32f8a4bf7c58774f
    (cherry picked from commit e29aa43e36aa096e4a36de1fb7c477190a479cdf)

--- torch/csrc/distributed/c10d/logger.cpp
+++ torch/csrc/distributed/c10d/logger.cpp
@@ -128,7 +128,7 @@ std::vector<std::vector<size_t>> Logger::get_per_bucket_variable_indices() {
 std::vector<int> Logger::get_bucket_sizes() {
   std::vector<int> bucket_sizes;
   for (const auto& bucket : reducer_->buckets_) {
-    const auto& variables = bucket.variables;
+    const auto& variables = bucket.replicas[0].variables;
     int bucket_size = 0;
     for (const auto& v : variables) {
       bucket_size += v.numel() * v.element_size();
