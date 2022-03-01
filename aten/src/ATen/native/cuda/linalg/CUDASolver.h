d3ddbb021b227e3638f6f7c22c6eadfa73695e31
Nikita Shulga <nshulga@fb.com>
2022-03-01 09:49:43 -0800

    Revert D33767740: [pytorch][PR] Sparse CSR CPU: cuSolverSP backend for `linalg.solve`
    Test Plan: revert-hammer
    
    Differential Revision:
    D33767740 (https://github.com/pytorch/pytorch/commit/199d9a992c0062de7bd7fe0a9f3414a3c1f08a74)
    
    Original commit changeset: a945f065210c
    
    Original Phabricator Diff: D33767740 (https://github.com/pytorch/pytorch/commit/199d9a992c0062de7bd7fe0a9f3414a3c1f08a74)
    
    fbshipit-source-id: b7934df18118f8d6d5f165deb5aae9887953ae43

--- aten/src/ATen/native/cuda/linalg/CUDASolver.h
+++ aten/src/ATen/native/cuda/linalg/CUDASolver.h
@@ -13,45 +13,6 @@ namespace at {
 namespace cuda {
 namespace solver {
 
-#define CUSOLVER_LINEAR_SOLVER_ARGTYPES(scalar_t, value_t)                          \
-    cusolverSpHandle_t handle, int n, int nnzA, const cusparseMatDescr_t descrA,    \
-    const scalar_t *csrValA, const int *csrRowPtrA, const int *csrColIndA,          \
-    const scalar_t *b, value_t tol, int reorder, scalar_t *x, int *singularity
-
-template <typename scalar_t, typename value_t>
-inline void lsvlu(CUSOLVER_LINEAR_SOLVER_ARGTYPES(scalar_t, value_t)) {
-    TORCH_INTERNAL_ASSERT(
-        false,
-        "at::cuda::sparse::lsvlu: not implemented for ",
-        typeid(scalar_t).name());
-}
-
-template <>
-void lsvlu<float, float>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(float, float));
-template <>
-void lsvlu<double, double>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(double, double));
-template <>
-void lsvlu<c10::complex<float>, float>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(c10::complex<float>, float));
-template <>
-void lsvlu<c10::complex<double>, double>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(c10::complex<double>, double));
-
-template <typename scalar_t, typename value_t>
-inline void lsvqr(CUSOLVER_LINEAR_SOLVER_ARGTYPES(scalar_t, value_t)) {
-    TORCH_INTERNAL_ASSERT(
-        false,
-        "at::cuda::sparse::lsvqr: not implemented for ",
-        typeid(scalar_t).name());
-}
-
-template <>
-void lsvqr<float, float>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(float, float));
-template <>
-void lsvqr<double, double>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(double, double));
-template <>
-void lsvqr<c10::complex<float>, float>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(c10::complex<float>, float));
-template <>
-void lsvqr<c10::complex<double>, double>(CUSOLVER_LINEAR_SOLVER_ARGTYPES(c10::complex<double>, double));
-
 #define CUDASOLVER_GETRF_ARGTYPES(Dtype)  \
     cusolverDnHandle_t handle, int m, int n, Dtype* dA, int ldda, int* ipiv, int* info
 
