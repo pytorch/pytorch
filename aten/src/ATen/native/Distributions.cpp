6cd1e9f53135a36bb46089dc8db6cd40d11eb80f
Nikita Karetnikov <nikita@karetnikov.org>
2022-03-01 15:07:13 -0800

    Unify checks for `normal` (#70087)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/70087
    
    Test Plan: Imported from OSS
    
    Reviewed By: davidberard98
    
    Differential Revision: D34089965
    
    Pulled By: bdhirsh
    
    fbshipit-source-id: 17d7eab3d8d60d03ca8ee63875ff2813bb2992c8

--- aten/src/ATen/native/Distributions.cpp
+++ aten/src/ATen/native/Distributions.cpp
@@ -262,7 +262,7 @@ Tensor& normal_(Tensor& self, double mean, double std, c10::optional<Generator> 
 }
 
 Tensor& normal_meta_(Tensor& self, double mean, double std, c10::optional<Generator> gen) {
-  TORCH_CHECK(std >= 0.0, "normal_ expects std >= 0.0, but found std=", std);  // TODO: dedupe
+  CHECK_NORMAL_STD(std);
   return self;
 }
 
