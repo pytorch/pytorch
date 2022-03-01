cf475e39786d26172d099921a34e63e6e16ccc2e
Jane Xu <janeyx@fb.com>
2022-03-01 11:57:44 -0500

    testing flakiness override. DO NOT MERGE.

--- test/jit/test_async.py
+++ test/jit/test_async.py
@@ -26,6 +26,11 @@ class TestAsync(JitTestCase):
         fut = torch.jit.fork(foo, x)
         y_hat = foo(x)
         y = torch.jit.wait(fut)
+
+        # testing flakiness
+        import random
+        if random.randint(0, 1) == 1:
+            assert(False)
         # assert nothing; only to make sure the fake python path works
 
     def test_async_future_type_python(self):
