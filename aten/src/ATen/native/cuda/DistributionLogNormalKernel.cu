27622d70ba543a84bf3cd3401df42f3a8f58e2db
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared25803.06.pnb2.facebook.com>
2022-03-01 04:01:41 -0800

    2022-03-01 postnightly release (7506042e69049b3261337a84ae2c11faea588519)

--- aten/src/ATen/native/cuda/DistributionLogNormalKernel.cu
+++ aten/src/ATen/native/cuda/DistributionLogNormalKernel.cu
@@ -1,26 +1,7 @@
-#include <ATen/Dispatch.h>
-#include <ATen/ExpandUtils.h>
-#include <ATen/NativeFunctions.h>
-#include <ATen/cuda/CUDAApplyUtils.cuh>
-#include <ATen/AccumulateType.h>
+#define TORCH_ASSERT_NO_OPERATORS
 #include <ATen/cuda/CUDAGeneratorImpl.h>
 #include <ATen/native/UnaryOps.h>
 #include <ATen/native/cuda/DistributionTemplates.h>
-
-#include <curand.h>
-#include <curand_kernel.h>
-#include <curand_philox4x32_x.h>
-#include <utility>
-#include <functional>
-
-#include <ATen/native/Distributions.h>
-#include <ATen/native/cuda/Loops.cuh>
-#include <ATen/native/TensorIterator.h>
-
-#include <cstdint>
-#include <limits>
-#include <utility>
-#include <type_traits>
 
 namespace at { namespace native {
 
