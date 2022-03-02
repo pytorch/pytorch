1c082fdf0a95e6dbed8421afc3a5d2f323fafaf8
Yi Zhang <zhanyi@microsoft.com>
2022-03-02 00:35:58 -0800

    print more

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -192,9 +192,15 @@ if [[ "${BUILD_ENVIRONMENT}" == pytorch-linux-xenial-py3* ]]; then
 fi
 
 echo $PATH
+ll /opt/cache/bin
 echo "env ---------------------------"
 env
 which g++
+which c++
+sccache nvcc --version
+which nvcc
+ll /opt/cache/lib
+
 
 if [[ "$BUILD_ENVIRONMENT" == *-bazel-* ]]; then
   set -e
