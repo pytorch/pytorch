df6e55e2940e22c17ad7925dad7f40d9a4386021
Andrew Or <andrewor@fb.com>
2022-03-02 09:48:21 -0800

    [Quant][fx] Add lowering for functional linear (#72855)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/72855
    
    This adds functionality to lower reference models
    involving functional linear in FX.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFxOps.test_functional_linear
    
    Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D34514127
    
    fbshipit-source-id: 7af4f37bdeda710dc7197ede9d46f66227d7932c
    (cherry picked from commit a14cbc04dea4e578643c4183f0c8ea43fbdaf5c7)

--- torch/ao/quantization/fx/quantization_patterns.py
+++ torch/ao/quantization/fx/quantization_patterns.py
@@ -936,7 +936,8 @@ class LinearReLUQuantizeHandler(QuantizeHandler):
                     self.linear_node)
         else:  # call_function
             assert self.linear_node.op == 'call_function'
-            if is_reference:
+            if is_reference or self.linear_node.target == torch.nn.functional.linear and\
+                    dtypes in [(torch.quint8, torch.qint8, None)]:
                 quantized_input_dtypes = [torch.float, torch.float]
                 if activation_int8_quantized:
                     quantized_input_dtypes[0] = torch.quint8
@@ -973,7 +974,8 @@ class LinearReLUQuantizeHandler(QuantizeHandler):
                         modules,
                         quantized_graph,
                         node_name_to_scope,
-                        is_input=False)
+                        is_input=False,
+                        output_prefix="")
                 else:
                     # output for dynamically quantized linear op is not quantized
                     return op_out
