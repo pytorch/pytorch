cfdd8754b988f2c289a712d2469634812d762951
soulitzer <soulitzer@gmail.com>
2022-03-02 13:42:00 -0500

    Update on "batch_norm_jvp: improve error message when running_{mean,var} have forward grad defined"
    Fixes: https://github.com/pytorch/pytorch/issues/73541
    
    [ghstack-poisoned]

--- test/test_nn.py
+++ test/test_nn.py
@@ -9587,13 +9587,11 @@ class TestNN(NNTestCase):
         fn = partial(F.batch_norm, **kwargs)
 
         for dual_indices in ((0,), (1,), (1, 2), (0, 1), (0, 1, 2),):
-            print(dual_indices)
             tangents = tuple(torch.rand_like(x) for x in args)
 
             with fwAD.dual_level():
                 duals = [fwAD.make_dual(primal, tangent) if i in dual_indices else primal
                         for i, (primal, tangent) in enumerate(zip(args, tangents))]
-                print(duals)
                 msg = "batch_norm is not differentiable wrt running_mean and running_var"
                 # 0 needs to have forward grad because otherwise we won't even run batch_norm_jvp
                 if (1 in dual_indices or 2 in dual_indices) and 0 in dual_indices:
