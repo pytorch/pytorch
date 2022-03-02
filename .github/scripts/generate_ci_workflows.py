pytorch/pytorch

commit ba14f0ee6cfa2fe248784d2dc5d54e427aef6bf7

Create a CI workflow for XLA tests using the XLA test image (#72496)
Summary:
This PR resolves https://github.com/pytorch/pytorch/issues/72693

Pull Request resolved: https://github.com/pytorch/pytorch/pull/72496

Reviewed By: H-Huang

Differential Revision: D34255441

Pulled By: seemethere

fbshipit-source-id: fdfd54fbd59ef7266a78c9f729c1d5b6ed25e9d6

.github/scripts/generate_ci_workflows.py
@@ -625,9 +625,8 @@ LINUX_WORKFLOWS = [
         num_test_shards=2,
         distributed_test=False,
         enable_noarch_test=1,
-        enable_xla_test=1,
         ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_DEFAULT, LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CPU, LABEL_CIFLOW_XLA, LABEL_CIFLOW_NOARCH},
+            labels={LABEL_CIFLOW_DEFAULT, LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CPU, LABEL_CIFLOW_NOARCH},
         ),
     ),
     CIWorkflow(
@@ -655,6 +654,22 @@ LINUX_WORKFLOWS = [
             labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CUDA, LABEL_CIFLOW_SLOW_GRADCHECK, LABEL_CIFLOW_SLOW, LABEL_CIFLOW_SCHEDULED},
         ),
     ),
+]
+
+XLA_WORKFLOWS = [
+    CIWorkflow(
+        arch="linux",
+        build_environment="pytorch-xla-linux-bionic-py3.7-clang8",
+        docker_image_base=f"{DOCKER_REGISTRY}/pytorch/xla_base",
+        test_runner_type=LINUX_CPU_TEST_RUNNER,
+        num_test_shards=2,
+        distributed_test=False,
+        enable_xla_test=1,
+        ciflow_config=CIFlowConfig(
+            labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CPU, LABEL_CIFLOW_XLA},
+        ),
+    ),
+
 ]
 
 ANDROID_SHORT_WORKFLOWS = [
@@ -832,7 +847,6 @@ MACOS_WORKFLOWS = [
 ]
 
 DOCKER_IMAGES = {
-    f"{DOCKER_REGISTRY}/pytorch/pytorch-linux-bionic-cuda10.2-cudnn7-py3.7-clang9",  # for pytorch/xla
     f"{DOCKER_REGISTRY}/pytorch/pytorch-linux-bionic-rocm4.3.1-py3.7",               # for rocm
     f"{DOCKER_REGISTRY}/pytorch/pytorch-linux-bionic-rocm4.5-py3.7",                 # for rocm
 }
@@ -1023,6 +1037,7 @@ def main() -> None:
     )
     template_and_workflows = [
         (jinja_env.get_template("linux_ci_workflow.yml.j2"), LINUX_WORKFLOWS),
+        (jinja_env.get_template("linux_ci_workflow.yml.j2"), XLA_WORKFLOWS),
         (jinja_env.get_template("windows_ci_workflow.yml.j2"), WINDOWS_WORKFLOWS),
         (jinja_env.get_template("bazel_ci_workflow.yml.j2"), BAZEL_WORKFLOWS),
         (jinja_env.get_template("ios_ci_workflow.yml.j2"), IOS_WORKFLOWS),

.github/templates/linux_ci_workflow.yml.j2
@@ -55,6 +55,11 @@ env:
   PR_NUMBER: ${{ github.event.pull_request.number }}
   SHA1: ${{ github.event.pull_request.head.sha || github.sha }}
   PYTORCH_RETRY_TEST_CASES: 1
+{%- if enable_xla_test == 1 %}
+  # This is used for XLA tests only
+  XLA_CUDA: 0
+  XLA_IMAGE_TAG: v0.2
+{%- endif %}
 {%- if build_with_debug %}
   DEBUG: 1
 {%- endif %}
@@ -74,7 +79,19 @@ jobs:
         run: echo "${PR_LABELS}"
       !{{ common.setup_ec2_linux() }}
       !{{ common.checkout() }}
+    {%- if enable_xla_test == 1 %}
+      - name: Calculate docker image tag
+        id: calculate-tag
+        run: |
+          echo "XLA workflow uses pre-built test image at ${XLA_IMAGE_TAG}"
+          DOCKER_TAG=$(git rev-parse HEAD:.circleci/docker)
+          echo "DOCKER_TAG=${DOCKER_TAG}" >> "${GITHUB_ENV}"
+          echo "DOCKER_IMAGE=${DOCKER_IMAGE_BASE}:${XLA_IMAGE_TAG}" >> "${GITHUB_ENV}"
+          echo "::set-output name=docker_tag::${DOCKER_TAG}"
+          echo "::set-output name=docker_image::${DOCKER_IMAGE_BASE}:${XLA_IMAGE_TAG}"
+    {%- else %}
       !{{ common.calculate_docker_image(false) }}
+    {%- endif %}
       - name: Pull Docker image
         run: |
           !{{ common.add_retry_to_env() }}
@@ -96,6 +113,9 @@ jobs:
             -e BRANCH \
             -e GITHUB_RUN_ID \
             -e SCCACHE_BUCKET \
+          {%- if enable_xla_test == 1 %}
+            -e XLA_CUDA \
+          {%- endif %}
             -e XLA_CLANG_CACHE_S3_BUCKET_NAME \
             -e CUSTOM_TEST_ARTIFACT_BUILD_DIR \
             -e SKIP_SCCACHE_INITIALIZATION=1 \
@@ -307,6 +327,9 @@ jobs:
             -e PR_LABELS \
             -e MAX_JOBS="$(nproc --ignore=2)" \
             -e SCCACHE_BUCKET \
+          {%- if enable_xla_test == 1 %}
+            -e XLA_CUDA \
+          {%- endif %}
             -e XLA_CLANG_CACHE_S3_BUCKET_NAME \
 {%- if 'rocm' not in test_runner_type %}
             ${PROXY_ENV} \

.github/workflows/generated-pytorch-xla-linux-bionic-py3.7-clang8.yml
@@ -1,25 +1,24 @@
 # @generated DO NOT EDIT MANUALLY
 # Template is at:    .github/templates/linux_ci_workflow.yml.j2
 # Generation script: .github/scripts/generate_ci_workflows.py
-name: linux-bionic-py3.7-clang9
+name: pytorch-xla-linux-bionic-py3.7-clang8
 
 on:
-  pull_request:
   push:
     tags:
       - 'ciflow/all/*'
       - 'ciflow/cpu/*'
       - 'ciflow/linux/*'
-      - 'ciflow/noarch/*'
       - 'ciflow/trunk/*'
+      - 'ciflow/xla/*'
     branches:
       - master
       - release/*
   workflow_dispatch:
 
 env:
-  BUILD_ENVIRONMENT: linux-bionic-py3.7-clang9
-  DOCKER_IMAGE_BASE: 308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/pytorch-linux-bionic-py3.7-clang9
+  BUILD_ENVIRONMENT: pytorch-xla-linux-bionic-py3.7-clang8
+  DOCKER_IMAGE_BASE: 308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/xla_base
   SCCACHE_BUCKET: ossci-compiler-cache-circleci-v2
   XLA_CLANG_CACHE_S3_BUCKET_NAME: ossci-compiler-clang-cache-circleci-xla
   TORCH_CUDA_ARCH_LIST: 5.2
@@ -36,8 +35,11 @@ env:
   PR_NUMBER: ${{ github.event.pull_request.number }}
   SHA1: ${{ github.event.pull_request.head.sha || github.sha }}
   PYTORCH_RETRY_TEST_CASES: 1
+  # This is used for XLA tests only
+  XLA_CUDA: 0
+  XLA_IMAGE_TAG: v0.2
 concurrency:
-  group: linux-bionic-py3.7-clang9-${{ github.event.pull_request.number || github.sha }}-${{ github.event_name == 'workflow_dispatch' }}
+  group: pytorch-xla-linux-bionic-py3.7-clang8-${{ github.event.pull_request.number || github.sha }}-${{ github.event_name == 'workflow_dispatch' }}
   cancel-in-progress: true
 
 jobs:
@@ -46,7 +48,7 @@ jobs:
     runs-on: linux.2xlarge
     timeout-minutes: 240
     env:
-      JOB_BASE_NAME: linux-bionic-py3.7-clang9-build
+      JOB_BASE_NAME: pytorch-xla-linux-bionic-py3.7-clang8-build
     outputs:
       docker_image: ${{ steps.calculate-tag.outputs.docker_image }}
     steps:
@@ -108,50 +110,12 @@ jobs:
       - name: Calculate docker image tag
         id: calculate-tag
         run: |
+          echo "XLA workflow uses pre-built test image at ${XLA_IMAGE_TAG}"
           DOCKER_TAG=$(git rev-parse HEAD:.circleci/docker)
           echo "DOCKER_TAG=${DOCKER_TAG}" >> "${GITHUB_ENV}"
-          echo "DOCKER_IMAGE=${DOCKER_IMAGE_BASE}:${DOCKER_TAG}" >> "${GITHUB_ENV}"
+          echo "DOCKER_IMAGE=${DOCKER_IMAGE_BASE}:${XLA_IMAGE_TAG}" >> "${GITHUB_ENV}"
           echo "::set-output name=docker_tag::${DOCKER_TAG}"
-          echo "::set-output name=docker_image::${DOCKER_IMAGE_BASE}:${DOCKER_TAG}"
-      - name: Check if image should be built
-        id: check
-        env:
-          BASE_REVISION: ${{ github.event.pull_request.base.sha || github.sha }}
-        run: |
-          set -x
-          # Check if image already exists, if it does then skip building it
-          if docker manifest inspect "${DOCKER_IMAGE_BASE}:${DOCKER_TAG}"; then
-            exit 0
-          fi
-          if [[ "$BASE_REVISION" = "$(git rev-parse HEAD)" ]]; then
-            # if we're on the base branch then use the parent commit
-            MERGE_BASE=$(git rev-parse HEAD~)
-          else
-            # otherwise we're on a PR, so use the most recent base commit
-            MERGE_BASE=$(git merge-base HEAD "$BASE_REVISION")
-          fi
-          # Covers the case where a previous tag doesn't exist for the tree
-          # this is only really applicable on trees that don't have `.circleci/docker` at its merge base, i.e. nightly
-          if ! git rev-parse "$MERGE_BASE:.circleci/docker"; then
-            echo "Directory '.circleci/docker' not found in commit $MERGE_BASE, you should probably rebase onto a more recent commit"
-            exit 1
-          fi
-          PREVIOUS_DOCKER_TAG=$(git rev-parse "$MERGE_BASE:.circleci/docker")
-          # If no image exists but the hash is the same as the previous hash then we should error out here
-          if [[ "${PREVIOUS_DOCKER_TAG}" = "${DOCKER_TAG}" ]]; then
-            echo "ERROR: Something has gone wrong and the previous image isn't available for the merge-base of your branch"
-            echo "       contact the PyTorch team to restore the original images"
-            exit 1
-          fi
-          echo ::set-output name=rebuild::yes
-      - name: Build and push docker image
-        if: ${{ steps.check.outputs.rebuild }}
-        env:
-          DOCKER_SKIP_S3_UPLOAD: 1
-        working-directory: .circleci/docker
-        run: |
-          export IMAGE_NAME=${DOCKER_IMAGE_BASE#308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/}
-          ./build_docker.sh
+          echo "::set-output name=docker_image::${DOCKER_IMAGE_BASE}:${XLA_IMAGE_TAG}"
       - name: Pull Docker image
         run: |
           retry () {
@@ -177,6 +141,7 @@ jobs:
             -e BRANCH \
             -e GITHUB_RUN_ID \
             -e SCCACHE_BUCKET \
+            -e XLA_CUDA \
             -e XLA_CLANG_CACHE_S3_BUCKET_NAME \
             -e CUSTOM_TEST_ARTIFACT_BUILD_DIR \
             -e SKIP_SCCACHE_INITIALIZATION=1 \
@@ -263,8 +228,8 @@ jobs:
       ENABLE_SLOW_TEST: ''
       ENABLE_DOCS_TEST: ''
       ENABLE_BACKWARDS_COMPAT_TEST: ''
-      ENABLE_XLA_TEST: ''
-      ENABLE_NOARCH_TEST: 1
+      ENABLE_XLA_TEST: 1
+      ENABLE_NOARCH_TEST: ''
       NUM_TEST_SHARDS: 2
       MULTIGPU_RUNNER_TYPE: linux.16xlarge.nvidia.gpu
       DISTRIBUTED_GPU_RUNNER_TYPE: linux.8xlarge.nvidia.gpu
@@ -294,7 +259,7 @@ jobs:
     timeout-minutes: 240
     env:
       DOCKER_IMAGE: ${{ needs.build.outputs.docker_image }}
-      JOB_BASE_NAME: linux-bionic-py3.7-clang9-test
+      JOB_BASE_NAME: pytorch-xla-linux-bionic-py3.7-clang8-test
       TEST_CONFIG: ${{ matrix.config }}
       SHARD_NUMBER: ${{ matrix.shard }}
       NUM_TEST_SHARDS: ${{ matrix.num_shards }}
@@ -438,6 +403,7 @@ jobs:
             -e PR_LABELS \
             -e MAX_JOBS="$(nproc --ignore=2)" \
             -e SCCACHE_BUCKET \
+            -e XLA_CUDA \
             -e XLA_CLANG_CACHE_S3_BUCKET_NAME \
             ${PROXY_ENV} \
             --env-file="/tmp/github_env_${GITHUB_RUN_ID}" \
@@ -512,7 +478,7 @@ jobs:
         env:
           AWS_DEFAULT_REGION: us-east-1
           BRANCH: ${{ steps.parse-ref.outputs.branch }}
-          JOB_BASE_NAME: linux-bionic-py3.7-clang9-test
+          JOB_BASE_NAME: pytorch-xla-linux-bionic-py3.7-clang8-test
           PR_NUMBER: ${{ github.event.pull_request.number }}
           SHA1: ${{ github.event.pull_request.head.sha || github.sha }}
           TAG: ${{ steps.parse-ref.outputs.tag }}

.circleci/docker/build.sh
@@ -40,6 +40,12 @@ function extract_all_from_image_name() {
   done
 }
 
+# Use the same pre-built XLA test image from PyTorch/XLA
+if [[ "$image" == *xla* ]]; then
+  echo "Using pre-built XLA test image..."
+  exit 0
+fi
+
 if [[ "$image" == *-xenial* ]]; then
   UBUNTU_VERSION=16.04
 elif [[ "$image" == *-artful* ]]; then

.github/scripts/generate_pytorch_test_matrix.py
@@ -61,6 +61,7 @@ def run_as_if_on_trunk() -> bool:
     return current_workflow_triggered_by_label
 
 def main() -> None:
+    INCLUDE_DEFAULT_TEST = True
     TEST_RUNNER_TYPE = os.getenv('TEST_RUNNER_TYPE')
     assert TEST_RUNNER_TYPE is not None
     RUN_SMOKE_TESTS_ONLY_ON_PR = os.getenv('RUN_SMOKE_TESTS_ONLY_ON_PR')
@@ -97,6 +98,7 @@ def main() -> None:
         configs['backwards_compat'] = {'num_shards': 1, 'runner': TEST_RUNNER_TYPE}
     if os.getenv('ENABLE_XLA_TEST'):
         configs['xla'] = {'num_shards': 1, 'runner': TEST_RUNNER_TYPE}
+        INCLUDE_DEFAULT_TEST = False
     if os.getenv('ENABLE_NOARCH_TEST'):
         configs['noarch'] = {'num_shards': 1, 'runner': TEST_RUNNER_TYPE}
     if RUN_SMOKE_TESTS:
@@ -110,6 +112,7 @@ def main() -> None:
                 'runner': TEST_RUNNER_TYPE,
             }
             for shard in range(1, NUM_TEST_SHARDS + 1)
+            if INCLUDE_DEFAULT_TEST
         ] + [
             {
                 'config': name,

.github/workflows/generated-docker-builds.yml
@@ -28,8 +28,6 @@ jobs:
     strategy:
       matrix:
         include:
-            - docker_image_base: '308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/pytorch-linux-bionic-cuda10.2-cudnn7-py3.7-clang9'
-              docker_image_short_name: 'pytorch-linux-bionic-cuda10.2-cudnn7-py3.7-clang9'
             - docker_image_base: '308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/pytorch-linux-bionic-cuda10.2-cudnn7-py3.9-gcc7'
               docker_image_short_name: 'pytorch-linux-bionic-cuda10.2-cudnn7-py3.9-gcc7'
             - docker_image_base: '308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/pytorch-linux-bionic-cuda11.5-cudnn8-py3-gcc7'

.jenkins/pytorch/build.sh
@@ -209,10 +209,13 @@ else
 
   if [[ "$BUILD_ENVIRONMENT" != *libtorch* ]]; then
 
-    # ppc64le build fails when WERROR=1
+    # ppc64le, rocm builds fail when WERROR=1
+    # XLA test build fails when WERROR=1
     # set only when building other architectures
-    # only use for "python setup.py install" line
-    if [[ "$BUILD_ENVIRONMENT" != *ppc64le* && "$BUILD_ENVIRONMENT" != *rocm* ]]; then
+    # or building non-XLA tests.
+    if [[ "$BUILD_ENVIRONMENT" != *ppc64le* &&
+          "$BUILD_ENVIRONMENT" != *rocm*  &&
+          "$BUILD_ENVIRONMENT" != *xla* ]]; then
       WERROR=1 python setup.py bdist_wheel
     else
       python setup.py bdist_wheel

commit a7a9e534ef21b1c2da95ccebc802c7bdfd2e83fe

[GHA] Enable XLA tests on PR
This fixes accidental regression introduced by https://github.com/pytorch/pytorch/pull/72496

Add `on_pull_request` template parameter and set it to true during
workflow generation

.github/scripts/generate_ci_workflows.py
@@ -165,6 +165,7 @@ class CIWorkflow:
     build_with_debug: bool = False
     is_scheduled: str = ''
     is_default: bool = False
+    on_pull_request: bool = False
     num_test_shards: int = 1
     timeout_after: int = 240
     xcode_version: str = ''
@@ -195,6 +196,9 @@ class CIWorkflow:
 
         if LABEL_CIFLOW_DEFAULT in self.ciflow_config.labels:
             self.is_default = True
+
+        if self.is_default:
+            self.on_pull_request = True
 
         self.test_jobs = self._gen_test_jobs()
         self.assert_valid()
@@ -719,10 +723,10 @@ XLA_WORKFLOWS = [
         build_environment="pytorch-xla-linux-bionic-py3.7-clang8",
         docker_image_base=f"{DOCKER_REGISTRY}/pytorch/xla_base",
         test_runner_type=LINUX_CPU_TEST_RUNNER,
-        num_test_shards=2,
         enable_distributed_test=False,
         enable_xla_test=True,
         enable_default_test=False,
+        on_pull_request=True,
         ciflow_config=CIFlowConfig(
             labels={LABEL_CIFLOW_LINUX, LABEL_CIFLOW_CPU, LABEL_CIFLOW_XLA},
         ),

.github/templates/linux_ci_workflow.yml.j2
@@ -7,7 +7,7 @@ name: !{{ build_environment }}
 {%- endblock %}
 
 on:
-{%- if is_default %}
+{%- if on_pull_request %}
   pull_request:
 {%- endif %}
   push:

.github/templates/windows_ci_workflow.yml.j2
@@ -19,7 +19,7 @@
 name: !{{ build_environment }}
 
 on:
-{%- if is_default %}
+{%- if on_pull_request %}
   pull_request:
 {%- endif %}
   push:

.github/workflows/generated-pytorch-xla-linux-bionic-py3.7-clang8.yml
@@ -4,6 +4,7 @@
 name: pytorch-xla-linux-bionic-py3.7-clang8
 
 on:
+  pull_request:
   push:
     tags:
       - 'ciflow/all/*'