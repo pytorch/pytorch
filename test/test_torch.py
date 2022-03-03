c93fd80c68da1f3bd420049a12424f86cc2a3056
Yukio Siraichi <yukio.siraichi@gmail.com>
2022-03-03 18:39:27 +0900

    Update base for Update on "Remove structured kernels special casing in the codegen."
    This PR removes the special casing introduced in #73350.
    
    **What was this special casing for?**
    It was introduced so that structured kernels could take advantage of `IList` from the
    dispatcher. Basically, this special casing associated, for example, `Tensor[]` with
    `const ITensorList &`, instead of the previous `TensorList`. The other kernels remained
    using `TensorList`.
    
    **Why is it being removed?**
    After #72414 and #72414, this special casing is no longer needed. Those 2 PRs modifies the
    signatures of every kernel that has `Tensor[]` or `Tensor?[]` as arguments. Replacing them
    by `ITensorList` and `IOptTensorRefList`, respectively. From that PR onwards, every kernel
    is using the new API, so there's no need to special case it only for structured kernels.
    
    [ghstack-poisoned]

--- test/test_torch.py
+++ test/test_torch.py
@@ -5451,19 +5451,10 @@ class TestDevicePrecision(TestCase):
     def test_advancedindex_mixed_devices_error(self, devices) -> None:
         def test(x: torch.Tensor, ia: torch.Tensor, ib: torch.Tensor) -> None:
             # test getitem
-            with self.assertRaisesRegex(RuntimeError, f"indices should be either .* ({ia.device})"):
+            with self.assertRaisesRegex(RuntimeError, fr"indices should be either .* \({x.device}\)"):
                 value = x[:, ia, None, ib, 0]
-            with self.assertRaisesRegex(RuntimeError, f"indices should be either .* ({ia.device})"):
-                value = x[ia]
-            # test setitem
-            x_clone1 = x.clone()
-            x_clone2 = x.clone()
-            first_shape = x[:, ia, None, ib, 0].shape
-            second_shape = x[ia].shape
-            with self.assertRaisesRegex(RuntimeError, f"indices should be either .* ({ia.device})"):
-                x_clone1[:, ia, None, ib, 0] = torch.randn(first_shape).to(x_clone1)
-            with self.assertRaisesRegex(RuntimeError, f"indices should be either .* ({ia.device})"):
-                x_clone2[ia] = torch.randn(second_shape).to(x_clone2)
+            with self.assertRaisesRegex(RuntimeError, fr"indices should be either .* \({x.device}\)"):
+                value = x[ib]
 
         cpu = torch.device('cpu')
         for device in devices:
@@ -5480,7 +5471,7 @@ class TestDevicePrecision(TestCase):
             test(x, ia, ib)
 
             if len(devices) > 1:
-                other_device = devices[0] if device == devices[0] else devices[1]
+                other_device = devices[0] if device == devices[1] else devices[1]
 
                 # Index device tensor with mixed cpu, device tensors on different devices
                 x = x.to(device)
