b866a2b5dafb8d8af061190080c367099c12b178
Peter Bell <peterbell10@live.co.uk>
2022-03-04 16:39:20 -0800

    CUDA Kernels: Use per-operator headers (1/4) (#71212)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71212
    
    Splitting this into multiple PRs to keep the diffs more managable.
    
    Test Plan: Imported from OSS
    
    Reviewed By: ngimel
    
    Differential Revision: D33949896
    
    Pulled By: malfet
    
    fbshipit-source-id: b11e5effa44d660932b8c21ccab6ece3e48e848c

--- aten/src/ATen/native/BatchLinearAlgebra.h
+++ aten/src/ATen/native/BatchLinearAlgebra.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <ATen/ATen.h>
+#include <ATen/core/Tensor.h>
 #include <ATen/Config.h>
 #include <ATen/native/DispatchStub.h>
-#include <ATen/native/LinearAlgebraUtils.h>
 #include <ATen/native/TransposeType.h>
-#include <ATen/native/cpu/zmath.h>
 
 
 namespace at { namespace native {
