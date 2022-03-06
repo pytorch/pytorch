296b3eca3649a4d0d30d72d57947c26e1a07e077
Michael <35029869+mikeb127@users.noreply.github.com>
2022-03-06 22:55:43 +1100

    Update LossNLL.cpp

--- aten/src/ATen/native/LossNLL.cpp
+++ aten/src/ATen/native/LossNLL.cpp
@@ -491,7 +491,7 @@ Tensor cross_entropy_loss_prob_target(
 
     switch (reduction) {
       case Reduction::Mean:
-        return -(input * target * weight_).sum() / ( (float) input.numel() / (float) input.size(1));
+        return -(input * target * weight_).sum() / (input.numel() / input.size(1));
       case Reduction::Sum:
         return -(input * target * weight_).sum();
       case Reduction::None:
@@ -502,7 +502,7 @@ Tensor cross_entropy_loss_prob_target(
   } else {
     switch (reduction) {
       case Reduction::Mean:
-        return -(input * target).sum() / ( (float) input.numel() / (float) input.size(1));
+        return -(input * target).sum() / (input.numel() / input.size(1));
       case Reduction::Sum:
         return -(input * target).sum();
       case Reduction::None:
