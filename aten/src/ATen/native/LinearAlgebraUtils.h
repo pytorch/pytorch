df69a9d17e542373487c6cfd2fdb6a3e7c092b23
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared38052.02.vll1.facebook.com>
2022-03-08 00:02:26 -0800

    2022-03-08 nightly release (07410207c487369d87a03e4602f72ec870abe4b7)

--- aten/src/ATen/native/LinearAlgebraUtils.h
+++ aten/src/ATen/native/LinearAlgebraUtils.h
@@ -25,23 +25,6 @@
 
 namespace at { namespace native {
 
-// Used as an interface between the different BLAS-like libraries
-enum class TransposeType {
-  NoTranspose,
-  Transpose,
-  ConjTranspose,
-};
-
-// Transforms TransposeType into the BLAS / LAPACK format
-static char to_blas(TransposeType trans) {
-  switch (trans) {
-    case TransposeType::Transpose: return 'T';
-    case TransposeType::NoTranspose: return 'N';
-    case TransposeType::ConjTranspose: return 'C';
-  }
-  TORCH_INTERNAL_ASSERT(false, "Invalid transpose type");
-}
-
 static inline c10::MaybeOwned<Tensor> expect_resolved_conj(const Tensor& tensor) {
   if (tensor.is_conj()) {
     return c10::MaybeOwned<Tensor>::owned(tensor.resolve_conj());
