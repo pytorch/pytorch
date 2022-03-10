81962efdea333dcc6186c384802b35157cd205f0
Nick Korovaiko <korovaikon@gmail.com>
2022-03-09 16:30:22 -0800

    opinfo tests (#73979)

--- lazy_tensor_core/test/test_lazy.py
+++ lazy_tensor_core/test/test_lazy.py
@@ -1,13 +1,38 @@
 import torch
 
-from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.common_utils import run_tests, TestCase
 from torch.testing._internal.jit_utils import JitTestCase
+from torch.testing._internal.common_methods_invocations import op_db
+from torch.testing._internal.common_device_type import ops, instantiate_device_type_tests
 
 import lazy_tensor_core
-
 lazy_tensor_core._LAZYC._ltc_init_ts_backend()
-torch.manual_seed(42)
+import lazy_tensor_core.core.lazy_model as ltm
+import lazy_tensor_core.debug.metrics as metrics
+import itertools
+import yaml
+import os
+import pathlib
 
+def remove_suffixes(l):
+    return [x.split(".")[0] for x in l]
+
+def init_lists():
+    path_to_script = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+    TS_NATIVE_FUNCTIONS_PATH = os.path.join(*path_to_script.parts[:-1], "ts_native_functions.yaml")
+    yaml_ts = yaml.load(open(TS_NATIVE_FUNCTIONS_PATH), yaml.Loader)
+    LAZY_OPS_LIST = set(remove_suffixes(itertools.chain(yaml_ts["full_codegen"], yaml_ts["supported"], yaml_ts["autograd"])))
+    FALLBACK_LIST = set(["clamp"])
+    BAD_OPS_LIST = set([
+        'index_select',  # Empty output_sizes is not supported
+        'clone',  # is clone decomposed?
+    ])
+
+    return (LAZY_OPS_LIST, FALLBACK_LIST, BAD_OPS_LIST)
+
+(LAZY_OPS_LIST, FALLBACK_LIST, BAD_OPS_LIST) = init_lists()
+
+torch.manual_seed(42)
 
 class TestLazyTensor(JitTestCase):
     def testConvolutionBackward(self):
@@ -40,6 +65,43 @@ class TestLazyTensor(JitTestCase):
         torch.testing.assert_allclose(bias_copy_grad.cpu(), bias_grad.cpu())
         torch.testing.assert_allclose(weight_copy_grad.cpu(), weight_grad.cpu())
         torch.testing.assert_allclose(inp_copy_grad.cpu(), inp_grad.cpu())
+
+class TestLazyOpInfo(TestCase):
+    @ops([op for op in op_db if op.name in LAZY_OPS_LIST and op.name not in BAD_OPS_LIST], allowed_dtypes=(torch.float,))
+    def test_dispatched_to_lazy(self, device, dtype, op):
+
+        def get_name(op):
+            l = [op.name]
+            if op.variant_test_name != '':
+                l.append(op.variant_test_name)
+            return '.'.join(l)
+
+        global FALLBACK_LIST
+        samples = op.sample_inputs("lazy", dtype, requires_grad=False)
+        sample = list(samples)[0]
+        args = [sample.input] + list(sample.args)
+        kwargs = sample.kwargs
+        ltm.mark_step()
+        ltm.wait_device_ops()
+        metrics.reset_metrics()
+
+        r = op(*args, **kwargs)
+        ltm.mark_step()
+        ltm.wait_device_ops()
+        prefix = "aten" if op.name in FALLBACK_LIST else "lazy"
+        found = f"{prefix}::{op.name}" in remove_suffixes(metrics.counter_names())
+        # check aliases
+        if not found:
+            for alias in op.aliases:
+                alias_found = f"{prefix}::{alias.name}" in remove_suffixes(metrics.counter_names())
+                found = found or alias_found
+                if found:
+                    break
+        self.assertTrue(found)
+
+# TODO: after we move to master, add Lazy as a new Device here:
+# https://github.com/pytorch/pytorch/blob/master/torch/testing/_internal/common_device_type.py#L532
+instantiate_device_type_tests(TestLazyOpInfo, globals(), only_for="cpu")
 
 
 if __name__ == '__main__':
