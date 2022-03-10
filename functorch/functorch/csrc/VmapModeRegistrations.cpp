d63a025cb77527029963d27837762ae4aeb393a7
Richard Zou <zou3519@gmail.com>
2022-03-10 08:25:34 -0800

    [functorch] unsupported random errors

--- functorch/functorch/csrc/VmapModeRegistrations.cpp
+++ functorch/functorch/csrc/VmapModeRegistrations.cpp
@@ -11,7 +11,7 @@
 #include <functorch/csrc/PlumbingHelper.h>
 #include <functorch/csrc/Constants.h>
 #include <functorch/csrc/DynamicLayer.h>
-
+#include <ATen/core/dispatch/Dispatcher.h>
 
 namespace at {
 namespace functorch {
@@ -25,6 +25,10 @@ TORCH_LIBRARY_IMPL(_, FuncTorchVmapMode, m) {
   m.fallback(torch::CppFunction::makeFallthrough());
 }
 
+void nyiRandomOp(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+  TORCH_CHECK(false, "vmap: we do not yet support ", op.schema().operator_name(),
+              ". Please file an issue");
+}
 
 #define UNSUPPORTED_RANDOM(op) \
   m.impl(#op, torch::CppFunction::makeFromBoxedFunction<&unsupportedRandomOp>());
@@ -32,9 +36,32 @@ TORCH_LIBRARY_IMPL(_, FuncTorchVmapMode, m) {
 #define UNSUPPORTED_RANDOM2(op, overload) \
   m.impl(#op"."#overload, torch::CppFunction::makeFromBoxedFunction<&unsupportedRandomOp>());
 
+#define NYI_RANDOM(op) \
+  m.impl(#op, torch::CppFunction::makeFromBoxedFunction<&nyiRandomOp>());
+
+#define NYI_RANDOM2(op, overload) \
+  m.impl(#op"."#overload, torch::CppFunction::makeFromBoxedFunction<&nyiRandomOp>());
 
 TORCH_LIBRARY_IMPL(aten, FuncTorchVmapMode, m) {
   UNSUPPORTED_RANDOM2(bernoulli, out);
+  UNSUPPORTED_RANDOM2(rand, generator_out);
+  UNSUPPORTED_RANDOM2(rand, out);
+  UNSUPPORTED_RANDOM2(randint, generator_out);
+  UNSUPPORTED_RANDOM2(randint, out);
+  UNSUPPORTED_RANDOM2(randn, generator_out);
+  UNSUPPORTED_RANDOM2(randn, out);
+  UNSUPPORTED_RANDOM2(randperm, generator_out);
+  UNSUPPORTED_RANDOM2(randperm, out);
+  UNSUPPORTED_RANDOM2(multinomial, out);
+  UNSUPPORTED_RANDOM2(normal, float_Tensor_out);
+  UNSUPPORTED_RANDOM2(normal, Tensor_Tensor_out);
+  UNSUPPORTED_RANDOM2(normal, float_float_out);
+  UNSUPPORTED_RANDOM2(rrelu_with_noise, out);
+
+  NYI_RANDOM(rrelu_with_noise);
+  NYI_RANDOM(rrelu_with_noise_);
+  NYI_RANDOM(rrelu_);
+  NYI_RANDOM(rrelu);
 }
 
 
