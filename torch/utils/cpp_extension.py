42cc7ed61fd0ab5fef0b58ccf8369cdb62924c39
Jithun Nair <jithun.nair@amd.com>
2022-03-10 01:25:56 +0000

    Remove gfx1030 from list of default targets for PyTorch since Navi21 support is not in PT1.9

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1649,7 +1649,7 @@ def _get_rocm_arch_flags(cflags: Optional[List[str]] = None) -> List[str]:
     # Use same defaults from file cmake/public/LoadHIP.cmake.
     # Must keep in sync if defaults change.
     # Allow env var to override, just like during initial cmake build.
-    archs = os.environ.get('PYTORCH_ROCM_ARCH', 'gfx900;gfx906;gfx908;gfx90a;gfx1030')
+    archs = os.environ.get('PYTORCH_ROCM_ARCH', 'gfx900;gfx906;gfx908;gfx90a')
     flags = ['--amdgpu-target=%s' % arch for arch in archs.split(';')]
     flags += ['-fno-gpu-rdc']
     return flags
