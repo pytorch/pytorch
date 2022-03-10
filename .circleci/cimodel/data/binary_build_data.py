d82bb369cf005a9d2ad93f38d40d516a9a35e8ab
Nikita Shulga <nshulga@fb.com>
2022-03-09 22:01:17 -0800

    Add M1 Py3.10 build definitions
    This change is specific to release-1.11 as on master those builds are
    already unified

--- .circleci/cimodel/data/binary_build_data.py
+++ .circleci/cimodel/data/binary_build_data.py
@@ -42,10 +42,12 @@ CONFIG_TREE_DATA = OrderedDict(
         wheel=[
             "3.8",
             "3.9",
+            "3.10",
         ],
         conda=[
             "3.8",
             "3.9",
+            "3.10",
         ],
     )),
     windows=(
