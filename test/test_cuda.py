46be3406112544cccfc002052852a6b2491782f5
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-03-10 03:51:36 -0600

    Add explicit coalesce call to fix the test

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -3941,7 +3941,8 @@ class TestCudaComm(TestCase):
         r_tensors = [comm.reduce_add(t) for t in zip(*dup_tensors)]
         for r, t in zip(r_tensors, tensors):
             self.assertEqualTypeString(r, t)
-            self.assertEqual(r, t * 2)
+            maybe_coalesce = lambda x: x.coalesce() if x.is_sparse else x
+            self.assertEqual(maybe_coalesce(r), t * 2)
 
         rc_tensors = comm.reduce_add_coalesced(dup_tensors, buffer_size=buffer_size)
         self.assertEqual(r_tensors, rc_tensors)
