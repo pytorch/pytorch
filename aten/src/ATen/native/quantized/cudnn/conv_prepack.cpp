4a9d06c7cd7aba6f22b66fa38f35bdb7dcc1bccb
David Dang <dzdang@umich.edu>
2022-03-12 09:51:33 -0800

    [Quant][core][gpu][improvement] Refactored implementation for conv2d_cudnn to use packed parameters (#73510)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73510
    
    The previous implementation introduced in https://github.com/pytorch/pytorch/pull/70622
    and expanded on in https://github.com/pytorch/pytorch/pull/72770,
    https://github.com/pytorch/pytorch/pull/73035, https://github.com/pytorch/pytorch/pull/73337
    did not make use of packed parameters. This PR refactors the existing
    implementation to use packed parameters for cudnn conv2d in the same manner
    as was done for qnnpack and fbgemm in the following files:
    aten/src/ATen/native/quantized/cpu/fbgemm_utils.h.
    aten/src/ATen/native/quantized/cpu/qnnpack_utils.h.
    aten/src/ATen/native/quantized/cpu/qconv_prepack.cpp.
    aten/src/ATen/native/quantized/cpu/qconv_unpack.cpp (note this file will be
    refactored into two files (one located in /quantized/ and the other in
    /quantized/cpu/) in a subsequent PR, as we are currently using the dispatch introduced
    in this file for the cudnn operator as well)
    
    This allows for all cudnn operators to be registered as quantized::conv2d,
    quantized::conv2d_relu, quantized::conv2d_prepack, and to allow the dispatcher
    to determine which backend to use (e.g., cuda/cudnn, fbgemm, or qnnpack).
    
    Test cases were also modified to adhere to the methodology of using
    prepacking the weight & bias prior to passing it into the conv2d operator.
    
    We also ensured that the refactorization did not result in a reduction in speed
    by verifying that the computation times in the benchmark test case (see test plan below)
    are consistent with the results pre-refactorization.
    
    Note the following:
    apply_impl is now what was formerly raw_cudnn_convolution_forward
    apply_impl_helper is now what was formerly raw_cudnn_convolution_forward_out
    
    Test Plan:
    In pytorch main directory, execute
    ```
    python test/test_quantization.py TestQuantizedConv.test_qconv2d_cudnn
    ```
    for accuracy testing and
    ```
    python test/test_quantization.py TestQuantizedConv.test_benchmark
    ```
    for benchmark testing.
    
    In pytorch main directory, execute
    ```
    python test/test_quantization.py TestQuantizedConv.test_qconv2d_cudnn
    ```
    for accuracy testing and
    ```
    python test/test_quantization.py TestQuantizedConv.test_benchmark
    ```
    for benchmark testing.
    
    Differential Revision:
    D34803275
    D34803275
    
    Reviewed By: jerryzh168
    
    Pulled By: dzdang
    
    fbshipit-source-id: 299479c0315f41d758ab62125c9e5e7074e372e8
    (cherry picked from commit 03d9e68712f0ed5f39aedd9c29589f7f19b3a082)

---
+++ aten/src/ATen/native/quantized/cudnn/conv_prepack.cpp
@@ -0,0 +1,151 @@
+#ifdef USE_CUDA
+#include <ATen/cuda/CUDAConfig.h>  // for the definition of AT_CUDNN_ENABLED
+
+#if AT_CUDNN_ENABLED()
+
+#include <ATen/native/cudnn/Macros.h>
+
+#if HAS_CUDNN_V8()
+
+#include <ATen/ATen.h>
+#include <torch/library.h>
+#include <ATen/native/quantized/cudnn/cudnnpack_utils.h>
+#include <ATen/native/quantized/packed_params.h>
+#include <ATen/quantized/Quantizer.h>
+#include <c10/core/QScheme.h>
+#include <c10/util/irange.h>
+#include <torch/library.h>
+
+#include <array>
+#include <vector>
+
+template <int kSpatialDim>
+c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> PackedConvWeightCudnn<
+    kSpatialDim>::
+    prepack(
+        at::Tensor weight,
+        c10::optional<at::Tensor> bias,
+        torch::List<int64_t> stride,
+        torch::List<int64_t> padding,
+        torch::List<int64_t> output_padding,
+        torch::List<int64_t> dilation,
+        int64_t groups,
+        bool transpose) {
+  TORCH_CHECK(weight.qscheme() == c10::kPerTensorAffine, "Unsupported qscheme: ", toString(weight.qscheme()));
+  TORCH_CHECK(
+      weight.ndimension() == kSpatialDim + 2,
+      "Weights are expected to have ",
+      kSpatialDim + 2,
+      " dimensions");
+  TORCH_CHECK(
+      stride.size() == kSpatialDim,
+      "stride should contain ",
+      kSpatialDim,
+      " elements for ",
+      kSpatialDim,
+      "D convolution.");
+  TORCH_CHECK(
+      padding.size() == kSpatialDim,
+      "quantized::conv_prepack (cudnn): Specify front/top/left padding only. "
+      "end/bottom/right padding assumed to be equal to front/top/left");
+  TORCH_CHECK(
+      !transpose || output_padding.size() == kSpatialDim,
+      "quantized::conv_prepack: Specify top/left output padding "
+      "only. bottom/right padding assumed to be equal to top/left");
+  TORCH_CHECK(
+      dilation.size() == kSpatialDim,
+      "quantized::conv_prepack (cudnn): dilation should contain ",
+      kSpatialDim,
+      " elements for ",
+      kSpatialDim,
+      "D convolution.");
+  const int output_channels = transpose ? weight.size(1) * groups
+                                        : weight.size(0);
+  const auto qtype = weight.qscheme();
+  if (bias.has_value()) {
+    TORCH_CHECK(bias.value().dim() == 1, "bias should be a vector (1D Tensor)");
+    TORCH_CHECK(
+        bias.value().size(0) == output_channels,
+        "bias should have K elements: " + std::to_string(output_channels));
+    // TODO: we create a broadcasted_bias tensor later so I think we don't need to make this contiguous here.
+    // we will revisit this when nvidia adds proper support for broadcasting
+    // bias_contig = bias->contiguous();
+  }
+
+  auto ret_ptr = c10::make_intrusive<PackedConvWeightCudnn<kSpatialDim>>(
+          weight.contiguous(c10::MemoryFormat::ChannelsLast), // TODO: this assumes 2D I think. make it more general?
+          bias,
+          stride,
+          padding,
+          output_padding,
+          dilation,
+          groups,
+          transpose,
+          qtype);
+  return ret_ptr;
+}
+
+template
+c10::intrusive_ptr<ConvPackedParamsBase<2>> PackedConvWeightCudnn<
+    2>::
+    prepack(
+        at::Tensor weight,
+        c10::optional<at::Tensor> bias_in,
+        torch::List<int64_t> stride,
+        torch::List<int64_t> padding,
+        torch::List<int64_t> output_padding,
+        torch::List<int64_t> dilation,
+        int64_t groups,
+        bool transpose);
+
+namespace at {
+namespace native {
+namespace {
+
+template <int kSpatialDim = 2>
+class QConvPackWeightInt8Cudnn final {
+ public:
+  static c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> run_conv(
+      Tensor weight,
+      c10::optional<Tensor> bias,
+      torch::List<int64_t> stride,
+      torch::List<int64_t> padding,
+      torch::List<int64_t> dilation,
+      int64_t groups) {
+    torch::List<int64_t> output_padding;
+    output_padding.reserve(kSpatialDim);
+    for (const auto idx : c10::irange(kSpatialDim)) {
+      (void)idx; //Suppress unused variable warning
+      output_padding.push_back((int64_t)0);
+    }
+    return _run(weight, bias, stride, padding, output_padding, dilation, groups,
+                /*transpose=*/false);
+  }
+
+ private:
+  static c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> _run(
+      Tensor weight,
+      c10::optional<Tensor> bias,
+      torch::List<int64_t> stride,
+      torch::List<int64_t> padding,
+      torch::List<int64_t> output_padding,
+      torch::List<int64_t> dilation,
+      int64_t groups,
+      bool transpose) {
+    return PackedConvWeightCudnn<kSpatialDim>::prepack(
+        weight, bias, stride, padding, output_padding, dilation, groups,
+        transpose);
+  }
+};
+
+TORCH_LIBRARY_IMPL(quantized, QuantizedCUDA, m) {
+  m.impl(TORCH_SELECTIVE_NAME("quantized::conv2d_prepack"), TORCH_FN(QConvPackWeightInt8Cudnn<2>::run_conv));
+}
+
+} // namespace
+} // namespace native
+} // namespace at
+
+#endif  // HAS_CUDNN_V8
+#endif  // AT_CUDNN_ENABLED
+#endif  // USE_CUDA
