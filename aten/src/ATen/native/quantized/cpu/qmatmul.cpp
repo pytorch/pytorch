2634593b9f55c4cba18a94a1b0571de28d206637
Salil Desai <salilsdesai@fb.com>
2022-03-14 08:22:59 -0700

    [PyTorch Edge] Use Parallelization in Internal Quantized Matmul (#73247)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73247
    
    Split up multiplication over outer dimensions
    ghstack-source-id: 151250864
    
    Test Plan:
    From fbcode:
    ```buck test caffe2/test:quantization -- test_qmatmul```
    
    Performance Improvement Summary:
    For matmuls used by Transformer Model
    - This diff makes qmatmul ~53% faster than the preceding diff (Ruy without parallelization)
    - This entire diff stack makes qmatmul ~75% faster than the naive implementation
    (see below for details)
    
    **Detailed Benchmarking Results:**
    *Benchmarking done by on a model which performs matmuls of the same shapes and counts as Transformer Model, as determined in D30901505*
    
    *Notebook in which Benchmarking was performed: https://www.internalfb.com/intern/anp/view/?id=1582075&revision_id=537916317667891*
    
    - Ruy QMatMul, Parallelization within PyTorch (this diff, v5): [7.5257ms](https://www.internalfb.com/intern/aibench/details/621856970876663)
    - Ruy QMatMul, No Parallelization (D33735479, v18): [16.0261ms](https://www.internalfb.com/intern/aibench/details/867786467365069)
    - Naive QMatMul (on master branch (base of D33332098), v22): [30.9919ms](https://www.internalfb.com/intern/aibench/details/418359955621359)
    
    Experiments using Ruy Threadpool (which ended up being bad; abandoning):
    -  Ruy QMatMul, with Ruy Threadpool 4 threads (D34110676, v1): [59.8889ms](https://www.internalfb.com/intern/aibench/details/487293857402229)
    - Ruy QMatMul, Parallelization within PyTorch and with Ruy Threadpool 4 threads (D34111050, v1): [624.8932 ms (?!)](https://www.internalfb.com/intern/aibench/details/330231112631355)
    
    Reviewed By: kimishpatel
    
    Differential Revision: D34012771
    
    fbshipit-source-id: 79d137f295b05812968ab53fdf9798606f3f4e63

--- aten/src/ATen/native/quantized/cpu/qmatmul.cpp
+++ aten/src/ATen/native/quantized/cpu/qmatmul.cpp
@@ -2,6 +2,7 @@
 #include <torch/library.h>
 
 #ifdef USE_RUY_QMATMUL
+#include <ATen/Parallel.h>
 #include <ruy/ruy.h>
 #endif
 
@@ -104,8 +105,6 @@ Tensor qmatmul(
   const Tensor& qa_contig = qa.contiguous();
   const Tensor& qb_contig = qb.contiguous();
 
-  ruy::Context context;
-
   AT_DISPATCH_QINT_BYTE_TYPES(qa.scalar_type(), "qmatmul", [&] {
     using underlying_t = typename scalar_t::underlying;
 
@@ -116,50 +115,60 @@ Tensor qmatmul(
     underlying_t* out_data =
         reinterpret_cast<underlying_t*>(out.data_ptr<scalar_t>());
 
-    ruy::Matrix<underlying_t> qa_matrix;
-    ruy::MakeSimpleLayout(
-        m, k, ruy::Order::kRowMajor, qa_matrix.mutable_layout());
-    qa_matrix.set_zero_point(qa.q_zero_point());
-
-    ruy::Matrix<underlying_t> qb_matrix;
-    ruy::MakeSimpleLayout(
-        k, n, ruy::Order::kRowMajor, qb_matrix.mutable_layout());
-    qb_matrix.set_zero_point(qb.q_zero_point());
-
-    ruy::Matrix<underlying_t> out_matrix;
-    ruy::MakeSimpleLayout(
-        m, n, ruy::Order::kRowMajor, out_matrix.mutable_layout());
-    out_matrix.set_zero_point(output_zero_point);
-
-    // Requantization explanation:
-    // https://github.com/google/gemmlowp/blob/e844ffd17118c1e17d94e1ba4354c075a4577b88/doc/quantization.md
-    const double requantization_scale_inv =
-        (qa.q_scale() * qb.q_scale()) / output_scale;
-
-    ruy::MulParams<int32_t, underlying_t> mul_params;
-
-    int multiplier_fixedpoint;
-    int multiplier_exponent;
-    QuantizeMultiplier(requantization_scale_inv,
-                       &multiplier_fixedpoint,
-                       &multiplier_exponent);
-    mul_params.set_multiplier_fixedpoint(multiplier_fixedpoint);
-    mul_params.set_multiplier_exponent(multiplier_exponent);
-
     const size_t qa_stride = m * k;
     const size_t qb_stride = k * n;
     const size_t out_stride = m * n;
 
-    for (size_t i = 0; i < num_matmuls; i++) {
-      qa_matrix.set_data(qa_data);
-      qb_matrix.set_data(qb_data);
-      out_matrix.set_data(out_data);
-      ruy::Mul(qa_matrix, qb_matrix, mul_params, &context, &out_matrix);
+    auto matmuls = [&](int64_t begin, int64_t end) {
+      ruy::Context context;
 
-      qa_data += qa_stride;
-      qb_data += qb_stride;
-      out_data += out_stride;
-    }
+      ruy::Matrix<underlying_t> qa_matrix;
+      ruy::MakeSimpleLayout(
+          m, k, ruy::Order::kRowMajor, qa_matrix.mutable_layout());
+      qa_matrix.set_zero_point(qa.q_zero_point());
+
+      ruy::Matrix<underlying_t> qb_matrix;
+      ruy::MakeSimpleLayout(
+          k, n, ruy::Order::kRowMajor, qb_matrix.mutable_layout());
+      qb_matrix.set_zero_point(qb.q_zero_point());
+
+      ruy::Matrix<underlying_t> out_matrix;
+      ruy::MakeSimpleLayout(
+          m, n, ruy::Order::kRowMajor, out_matrix.mutable_layout());
+      out_matrix.set_zero_point(output_zero_point);
+
+      // Requantization explanation:
+      // https://github.com/google/gemmlowp/blob/e844ffd17118c1e17d94e1ba4354c075a4577b88/doc/quantization.md
+      const double requantization_scale_inv =
+          (qa.q_scale() * qb.q_scale()) / output_scale;
+
+      ruy::MulParams<int32_t, underlying_t> mul_params;
+
+      int multiplier_fixedpoint;
+      int multiplier_exponent;
+      QuantizeMultiplier(requantization_scale_inv,
+                         &multiplier_fixedpoint,
+                         &multiplier_exponent);
+      mul_params.set_multiplier_fixedpoint(multiplier_fixedpoint);
+      mul_params.set_multiplier_exponent(multiplier_exponent);
+
+      const underlying_t* qa_subtensor = qa_data + begin * qa_stride;
+      const underlying_t* qb_subtensor = qb_data + begin * qb_stride;
+      underlying_t* out_subtensor = out_data + begin * out_stride;
+
+      for (int64_t i = begin; i < end; i++) {
+        qa_matrix.set_data(qa_subtensor);
+        qb_matrix.set_data(qb_subtensor);
+        out_matrix.set_data(out_subtensor);
+        ruy::Mul(qa_matrix, qb_matrix, mul_params, &context, &out_matrix);
+
+        qa_subtensor += qa_stride;
+        qb_subtensor += qb_stride;
+        out_subtensor += out_stride;
+      }
+    };
+
+    at::parallel_for(0, num_matmuls, 1, matmuls);
   });
 
   return out;
