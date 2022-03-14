c6f991e7f1da6153e668ee5ed158c3d551023cc8
Salil Desai <salilsdesai@fb.com>
2022-03-14 08:22:59 -0700

    [PyTorch Edge] Internal Optimized Quantized Matmul (#73244)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73244
    
    Use Ruy for Optimization, internally
    ghstack-source-id: 151250891
    
    Test Plan:
    From fbcode:
    ```buck test caffe2/test:quantization -- test_qmatmul```
    
    Performance Improvement/Benchmarking: See testplan of D34012771
    
    Reviewed By: kimishpatel
    
    Differential Revision: D33460961
    
    fbshipit-source-id: c4b40147b27384f64f7fd0ecd6154d6bcd3fceab
    (cherry picked from commit df7f63bef236dba0e3b7cee9b0793b13c420847b)

--- aten/src/ATen/native/quantized/cpu/qmatmul.cpp
+++ aten/src/ATen/native/quantized/cpu/qmatmul.cpp
@@ -1,6 +1,10 @@
 #include <ATen/ATen.h>
 #include <torch/library.h>
 
+#ifdef USE_RUY_QMATMUL
+#include <ruy/ruy.h>
+#endif
+
 namespace at {
 namespace native {
 
@@ -21,6 +25,92 @@ inline void check_inputs(const Tensor& qa, const Tensor& qb) {
       "Both inputs to Matmul must have the same quantization scheme.");
 }
 
+#ifdef USE_RUY_QMATMUL
+
+Tensor qmatmul(
+    const Tensor& qa,
+    const Tensor& qb,
+    const double output_scale,
+    const int64_t output_zero_point) {
+  check_inputs(qa, qb);
+
+  const size_t num_dims = qa.dim();
+  const size_t b_num_dims = qb.dim();
+
+  TORCH_CHECK(
+      num_dims == b_num_dims,
+      "MatMul operands should have the same dimensionality. (", num_dims,
+      " and ", b_num_dims, " provided)");
+  TORCH_CHECK(
+      num_dims == 2,
+      "Quantized Matmul currently only suports operands which are 2-dimensional. (",
+      num_dims, " provided)");
+
+  const int64_t m = qa.size(0);
+  const int64_t k = qa.size(1);
+  const int64_t b_k = qb.size(0);
+  const int64_t n = qb.size(1);
+
+  TORCH_CHECK(
+      b_k == k,
+      "For Quantized Matmul, the size of tensor a (", k,
+      ") at dimension ", 1, " must match the size of tensor b (",
+      b_k, ") at dimension ", 0, ".");
+
+  Tensor out = at::empty(
+      {m, n},
+      at::device(kCPU).dtype(c10::kInt).memory_format(
+          qa.suggest_memory_format()));
+
+  const Tensor& qa_contig = qa.contiguous();
+  const Tensor& qb_contig = qb.contiguous();
+
+  ruy::Context context;
+
+  AT_DISPATCH_QINT_BYTE_TYPES(qa.scalar_type(), "qmatmul", [&] {
+    using underlying_t = typename scalar_t::underlying;
+
+    const underlying_t* qa_data = reinterpret_cast<const underlying_t*>(
+        qa_contig.data_ptr<scalar_t>());
+    const underlying_t* qb_data = reinterpret_cast<const underlying_t*>(
+        qb_contig.data_ptr<scalar_t>());
+    int32_t* out_data = out.data_ptr<int32_t>();
+
+    ruy::Matrix<underlying_t> qa_matrix;
+    ruy::MakeSimpleLayout(
+        m, k, ruy::Order::kRowMajor, qa_matrix.mutable_layout());
+    qa_matrix.set_data(qa_data);
+    qa_matrix.set_zero_point(qa.q_zero_point());
+
+    ruy::Matrix<underlying_t> qb_matrix;
+    ruy::MakeSimpleLayout(
+        k, n, ruy::Order::kRowMajor, qb_matrix.mutable_layout());
+    qb_matrix.set_data(qb_data);
+    qb_matrix.set_zero_point(qb.q_zero_point());
+
+    ruy::Matrix<int32_t> out_matrix;
+    ruy::MakeSimpleLayout(
+        m, n, ruy::Order::kRowMajor, out_matrix.mutable_layout());
+    out_matrix.set_data(out_data);
+
+    ruy::MulParams<int32_t, int32_t> mul_params;
+    ruy::Mul(qa_matrix, qb_matrix, mul_params, &context, &out_matrix);
+  });
+
+  // Requantization explanation:
+  // https://github.com/google/gemmlowp/blob/e844ffd17118c1e17d94e1ba4354c075a4577b88/doc/quantization.md
+  const double requantization_scale =
+      output_scale / (qa.q_scale() * qb.q_scale());
+
+  return at::quantize_per_tensor(
+      out.toType(c10::kFloat),
+      requantization_scale,
+      output_zero_point,
+      qa.scalar_type());
+}
+
+#else // ifdef USE_RUY_QMATMUL
+
 Tensor qmatmul(
     const Tensor& qa,
     const Tensor& qb,
@@ -33,6 +123,8 @@ Tensor qmatmul(
   return at::quantize_per_tensor(
       rc, output_scale, output_zero_point, qa.scalar_type());
 }
+
+#endif // ifdef USE_RUY_QMATMUL
 
 TORCH_LIBRARY_IMPL(quantized, QuantizedCPU, m) {
   m.impl(TORCH_SELECTIVE_NAME("quantized::matmul"), TORCH_FN(qmatmul));
