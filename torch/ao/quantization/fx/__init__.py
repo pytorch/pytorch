e70a91e53deed43a5617ccc5edfabc5758ac7332
Jerry Zhang <jerryzh168@gmail.com>
2022-03-16 12:18:13 -0700

    [quant] Rename _convert_do_not_use.py to convert.py
    Summary:
    att, also change all references to _convert_do_not_use
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestAOMigrationQuantizationFx
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/quantization/fx/__init__.py
+++ torch/ao/quantization/fx/__init__.py
@@ -1,4 +1,4 @@
 from .prepare import prepare
-from ._convert_do_not_use import _convert_do_not_use as convert
+from .convert import convert
 from .fuse import fuse
 from .backend_config import get_tensorrt_backend_config_dict
