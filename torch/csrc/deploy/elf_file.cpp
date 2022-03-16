fa18714b91b7b25428cfb24d2e600fec564cbb18
Sahan Chanuka Paliskara <sahanp@fb.com>
2022-03-16 13:20:23 -0700

    [torch::deploy] remove c10::irange from torch deploy (#74285)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74285
    
    This diff effectively renames `c10::irange` to `multipy::irange` in `torch::deploy` in by creating a copy of `c10::irange` in order to help torch::deploy function without a dependency on `torch`
    
    Revision History:  https://github.com/pytorch/pytorch/pull/73526
    
    Test Plan: buck test //caffe2/torch/csrc/deploy:test_deploy
    
    Reviewed By: d4l3k
    
    Differential Revision: D34911008
    
    fbshipit-source-id: 99a7934a6c90b6e6b456409319104405f660f186

--- torch/csrc/deploy/elf_file.cpp
+++ torch/csrc/deploy/elf_file.cpp
@@ -1,7 +1,8 @@
-#include <c10/util/irange.h>
+#include <string.h>
 #include <torch/csrc/deploy/Exception.h>
 #include <torch/csrc/deploy/elf_file.h>
 #include <torch/csrc/deploy/interpreter/Optional.hpp>
+#include <torch/csrc/deploy/irange.h>
 
 namespace torch {
 namespace deploy {
@@ -22,7 +23,7 @@ ElfFile::ElfFile(const char* filename) : memFile_(filename) {
   strtabSection_ = toSection(&shdrList_[strtabSecNo]);
 
   sections_.reserve(numSections_);
-  for (const auto i : c10::irange(numSections_)) {
+  for (const auto i : multipy::irange(numSections_)) {
     sections_.emplace_back(toSection(&shdrList_[i]));
   }
 }
