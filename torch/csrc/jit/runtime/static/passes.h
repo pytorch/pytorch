201c46eed6e1557aeecfd47c391e571831af5b5c
mikeiovine <mikeiovine@devvm4121.frc0.facebook.com>
2022-03-15 18:15:05 -0700

    [SR] Force split_and_squeeze usage via graph transformation
    Differential Revision: [D34913889](https://our.internmc.facebook.com/intern/diff/D34913889/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D34913889/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/static/passes.h
+++ torch/csrc/jit/runtime/static/passes.h
@@ -64,5 +64,7 @@ TORCH_API void ForceNonEmptyOutputs(Graph& graph);
 
 TORCH_API void UseVariadicGroupedAccessor(const std::shared_ptr<Graph>& graph);
 
+TORCH_API void UseSplitAndSqueeze(std::shared_ptr<Graph>& graph);
+
 } // namespace jit
 } // namespace torch
