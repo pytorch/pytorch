23c59a9295c298a10ef3b4641fa7f753872b5e68
Kiryl Kliushkin <kiryl@fb.com>
2022-03-17 09:11:27 -0700

    [pytorch][PR] Added missing c10 namespace for C++ API
    Summary: This PR adds c10:: to avoid possible `error: reference to 'optional' is ambiguous` error.
    
    Test Plan: Ensure successfull test run
    
    Reviewed By: VitalyFedyunin
    
    Differential Revision: D34940852
    
    fbshipit-source-id: b0d367c780c5986ec62140fb7feed93c727d2879

--- aten/src/ATen/DeviceGuard.h
+++ aten/src/ATen/DeviceGuard.h
@@ -22,8 +22,8 @@ inline c10::optional<Device> device_of(const Tensor& t) {
   }
 }
 
-inline optional<Device> device_of(const optional<Tensor>& t) {
-  return t.has_value() ? device_of(t.value()) : nullopt;
+inline c10::optional<Device> device_of(const c10::optional<Tensor>& t) {
+  return t.has_value() ? device_of(t.value()) : c10::nullopt;
 }
 
 /// Return the Device of a TensorList, if the list is non-empty and
--- aten/src/ATen/ops/from_blob.h
+++ aten/src/ATen/ops/from_blob.h
@@ -26,13 +26,13 @@ class TORCH_API TensorMaker {
  public:
   using ContextDeleter = DeleterFnPtr;
 
-  TensorMaker& strides(optional<IntArrayRef> value) noexcept {
+  TensorMaker& strides(c10::optional<IntArrayRef> value) noexcept {
     strides_ = value;
 
     return *this;
   }
 
-  TensorMaker& storage_offset(optional<int64_t> value) noexcept {
+  TensorMaker& storage_offset(c10::optional<int64_t> value) noexcept {
     storage_offset_ = value;
 
     return *this;
@@ -51,7 +51,7 @@ class TORCH_API TensorMaker {
     return *this;
   }
 
-  TensorMaker& target_device(optional<Device> value) noexcept {
+  TensorMaker& target_device(c10::optional<Device> value) noexcept {
     device_ = value;
 
     return *this;
@@ -79,11 +79,11 @@ class TORCH_API TensorMaker {
 
   void* data_;
   IntArrayRef sizes_;
-  optional<IntArrayRef> strides_{};
-  optional<int64_t> storage_offset_{};
+  c10::optional<IntArrayRef> strides_{};
+  c10::optional<int64_t> storage_offset_{};
   std::function<void(void*)> deleter_{};
   std::unique_ptr<void, ContextDeleter> ctx_{nullptr, detail::noopDelete};
-  optional<Device> device_{};
+  c10::optional<Device> device_{};
   TensorOptions opts_{};
 };
 
