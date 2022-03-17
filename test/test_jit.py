93f389031591d977108474d590c189737ecf5f94
jiej <jiej@nvidia.com>
2022-03-17 10:41:00 -0700

    fixing tests; verified failure and fix

--- test/test_jit.py
+++ test/test_jit.py
@@ -1094,22 +1094,6 @@ class TestJit(JitTestCase):
         checkBackwardTwiceScript(test_script_backward_twice_with_saved_values, (inp1, inp2), False)
         checkBackwardTwiceScript(test_script_backward_twice_with_saved_values, (inp1, inp2), True)
 
-    def test_autodiff_complex(self):
-        def foo(x: torch.Tensor, y: torch.Tensor, W: torch.Tensor):
-            return torch.exp(torch.mm(torch.complex(x, y), W.cfloat()))
-        
-        @torch.jit.script
-        def jitted_foo(x: torch.Tensor, y: torch.Tensor, W: torch.Tensor):
-            return torch.exp(torch.mm(torch.complex(x, y), W.cfloat()))
-        
-        x = torch.randn(128, 16, dtype=torch.float32, device='cuda:0')
-        y = torch.randn(128, 16, dtype=torch.float32, device='cuda:0')
-        W = torch.randn(16, 1, dtype=torch.float32, device='cuda:0', requires_grad=True)
-        W.data /= 4
-
-        for i in range(4):
-          self.assertTrue((foo(x, y, W).grad_fn is None) and (jitted_foo(x, y, W).grad_fn is None))
-
     def test_diff_subgraph_clones_constants(self):
         @torch.jit.script
         def f(x, y):
@@ -11063,6 +11047,25 @@ dedent """
         if GRAPH_EXECUTOR != ProfilingMode.SIMPLE:
             FileCheck().check("Double(*, *, requires_grad=0, device=cpu)") \
                        .check_not("Float(*, *, requires_grad=0, device=cpu)").run(randint.graph_for())
+
+    @unittest.skipIf(GRAPH_EXECUTOR != ProfilingMode.PROFILING, "skip if profiling isn't enabled")
+    def test_autodiff_complex(self):
+        def foo(x: torch.Tensor, y: torch.Tensor, W: torch.Tensor):
+            return torch.exp(torch.mm(torch.complex(x, y), W.cfloat()))
+
+        @torch.jit.script
+        def jitted_foo(x: torch.Tensor, y: torch.Tensor, W: torch.Tensor):
+            return torch.exp(torch.mm(torch.complex(x, y), W.cfloat()))
+
+        x = torch.randn(128, 16, dtype=torch.float32, device='cuda:0')
+        y = torch.randn(128, 16, dtype=torch.float32, device='cuda:0')
+        W = torch.randn(16, 1, dtype=torch.float32, device='cuda:0', requires_grad=True)
+        W.data /= 4
+
+        with enable_profiling_mode_for_profiling_tests():
+            for i in range(4):
+                self.assertTrue((foo(x, y, W).grad_fn is None) == (jitted_foo(x, y, W).grad_fn is None))
+
 
     def test_linear_grad(self):
         with enable_profiling_mode_for_profiling_tests():
