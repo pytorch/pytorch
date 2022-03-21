9f6106538642a9b5dfa9cbd3147a4e2962606975
David Dang <dzdang@umich.edu>
2022-03-21 06:48:59 -0700

    [quant][gpu][core] Implemented quantized added operator in cudnn
    Summary:
    This PR implements the quantized add operator using cudnn operations.
    A test case was added ....
    
    Test Plan:
    TBA
    
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/quantized/cudnn/BinaryOps.cpp
@@ -0,0 +1,176 @@
+#ifdef USE_CUDA
+#include <ATen/cuda/CUDAConfig.h>  // for the definition of AT_CUDNN_ENABLED
+
+#if AT_CUDNN_ENABLED()
+#include <ATen/native/cudnn/Macros.h>
+#if HAS_CUDNN_V8()
+
+#include <ATen/core/TensorBase.h>
+#include <ATen/native/quantized/cudnn/utils.h>
+#include <ATen/TensorUtils.h>
+
+#include <unordered_map>
+
+
+struct CacheKey {
+  uint8_t input_a_alignment;
+  uint8_t input_b_alignment;
+  uint8_t output_alignment;
+};
+
+// FIXME: make this thread-safe by reusing the benchmark cache in Conv_v7.cpp
+namespace {
+std::unordered_map<CacheKey, cudnn_frontend::ManagedOpaqueDescriptor, at::native::ParamsHash<CacheKey>, at::native::ParamsEqual<CacheKey>> execution_plan_cache;
+}
+
+namespace at {
+namespace native {
+
+// currently we only support int8 symmetric (zero_point = 0 for inputs and output) quantized add
+template <bool ReLUFused = false>
+Tensor add(Tensor qa, Tensor qb, double output_scale, int64_t output_zero_point) {
+  if (quantized_output.numel() == 0) {
+    return;
+  }
+  check_inputs(qa, qb);
+
+  at::Tensor add_output = at::empty(qa.sizes(), at::device(at::kCUDA).dtype(at::kFloat), at::MemoryFormat::ChannelsLast);
+  auto requantize_multiplier = qa.q_scale() * qb.qscale() / output_scale;
+  // TODO: When cudnn enables support for broadcasting, we can remove this tensor
+  at::Tensor requantize_multiplier_tensor = at::empty(quantized_output.sizes(), at::device(at::kCUDA).dtype(at::kFloat), at::MemoryFormat::ChannelsLast);
+  requantize_multiplier_tensor.fill_(requantize_multiplier);
+  at::Tensor quantized_output = at::_empty_affine_quantized(
+      qa.sizes(),
+      at::device(at::kCUDA).dtype(at::ScalarType::QInt8),
+      output_scale,
+      output_zero_point,
+      at::MemoryFormat::ChannelsLast);
+
+  cudnnHandle_t handle = at::native::getCudnnHandle();
+  CacheKey key;
+  bool deterministic{true};
+  bool allow_tf32{false};
+
+  key.input_a_alignment = cudnn_utils::getAlignment(qa);
+  key.input_b_alignment = cudnn_utils::getAlignment(qb);
+  key.output_alignment = cudnn_utils::getAlignment(add_output);
+
+  auto run = [&](cudnn_frontend::ManagedOpaqueDescriptor plan_desc) {
+    auto workspace_size = 0;
+    auto workspace = at::empty({workspace_size}, input.options().dtype(at::kByte));
+    std::vector<void *> data_ptrs;
+    std::vector<int64_t> uids;
+    data_ptrs.reserve(10);
+    uids.reserve(10);
+    data_ptrs = {reinterpret_cast<int8_t*>(qa.data_ptr()), reinterpret_cast<int8_t*>(qb.data_ptr()),
+                                           add_output.data_ptr(), requantize_multiplier_tensor.data_ptr(),
+                                           reinterpret_cast<int8_t*>(quantized_output.data_ptr())};
+    uids = {'a', 'b', 'c', 'r', 'q'};
+    if (kReluFused) {
+        data_ptrs.emplace_back(add_output.data_ptr()),
+        uids.emplace_back('d');
+    }
+
+    auto variantPack = cudnn_frontend::VariantPackBuilder()
+      .setWorkspacePointer(workspace.data_ptr())
+      .setDataPointers(uids.size(), data_ptrs.data())
+      .setUids(uids.size(), uids.data())
+      .build();
+    auto variant_pack_desc = variantPack.get_raw_desc();
+    AT_CUDNN_CHECK(cudnnBackendExecute(handle, plan_desc->get_backend_descriptor(), variant_pack_desc));
+  };
+
+  auto search = execution_plan_cache.find(key);
+  if (search != execution_plan_cache.end()) {
+    cudnn_frontend::ManagedOpaqueDescriptor plan_desc = search->second;
+    run(plan_desc);
+    return;
+  }
+
+  // add_op computes qa + qb  (both inputs are int8)
+  // add_output is a fp32 tensor for accumulation purposes
+  auto add_op = cudnn_frontend::OperationBuilder(CUDNN_BACKEND_OPERATION_POINTWISE_DESCRIPTOR)
+      .setxDesc(cudnn_utils::getTensorDescriptor(qa.sizes(), qa.strides(), CUDNN_DATA_INT8, 'a', key.input_alignment))
+      .setbDesc(cudnn_utils::getTensorDescriptor(qb.sizes(), qb.strides(), CUDNN_DATA_INT8, 'b', key.input_alignment))
+      .setyDesc(cudnn_utils::getTensorDescriptor(add_output, 'c', key.output_alignment))
+      .setpwDesc(cudnn_utils::getPointWiseAddDescriptor(at::native::getCudnnDataType(add_output)
+      .build());
+
+  // relu_op computes
+  // relu( (qa + qb) * (qa_scale * qb_scale) / out_scale )
+  // output is a fp32 tensor
+  c10::optional<cudnn_frontend::Operation> relu_op;
+  if (kReluFused) {
+    // we use inplace operation here where the output is assigned to the input
+    relu_op.emplace(cudnn_frontend::OperationBuilder(CUDNN_BACKEND_OPERATION_POINTWISE_DESCRIPTOR)
+      .setxDesc(add_op.getOutputTensor())
+      .setyDesc(cudnn_utils::getTensorDescriptor(add_output, 'f', key.output_alignment))
+      .setpwDesc(cudnn_utils::getPointWiseReluDescriptor(at::native::getCudnnDataType(add_output)))
+      .build());
+  }
+
+  // requant_op computes
+  // (qa_int8 + qb_int8) * (qa_scale * qb_scale) / out_scale
+  // where requantize_multiplier = (qa_scale * qb_scale) / out_scale
+  auto requant_op = cudnn_frontend::OperationBuilder(CUDNN_BACKEND_OPERATION_POINTWISE_DESCRIPTOR)
+    .setxDesc(kReluFused ? relu_op.value().getOutputTensor() : add_op.getOutputTensor())
+    .setbDesc(cudnn_utils::getTensorDescriptor(requantize_multiplier_tensor, 's', cudnn_utils::getAlignment(requantize_multiplier_tensor)))
+    .setyDesc(cudnn_utils::getTensorDescriptor(quantized_output.sizes(), quantized_output.strides(), CUDNN_DATA_INT8, 'r', cudnn_utils::getAlignment(quantized_output)))
+    .setpwDesc(cudnn_utils::getPointWiseMulDescriptor(at::native::getCudnnDataType(requantize_multiplier_tensor)))
+    .build();
+
+  std::vector<cudnn_frontend::Operation const *> ops{&add_op};
+  if (kReluFused) {
+    ops.emplace_back(&(relu_op.value()));
+  }
+  ops.emplace_back(&requant_op);
+
+  auto opGraph = cudnn_frontend::OperationGraphBuilder()
+      .setHandle(handle)
+      .setOperationGraph(ops.size(), ops.data())
+      .build();
+  // std::cout << "opGraph: " << opGraph.describe() << std::endl;
+
+  auto heuristics = cudnn_frontend::EngineHeuristicsBuilder()
+      .setOperationGraph(opGraph)
+      .setHeurMode(CUDNN_HEUR_MODE_INSTANT)
+      .build();
+  auto fallback = cudnn_frontend::EngineFallbackListBuilder()
+                    .setOperationGraph(opGraph)
+                    .setOperation(CUDNN_BACKEND_OPERATION_CONVOLUTION_FORWARD_DESCRIPTOR)
+                    .build();
+
+  auto& engine_configs = heuristics.getEngineConfig(heuristics.getEngineConfigCount());
+  auto& fallback_list = fallback.getFallbackList();
+
+  cudnn_frontend::EngineConfigList filtered_configs;
+  cudnn_utils::filterEngineConfigs(engine_configs, filtered_configs, deterministic, allow_tf32, input.scalar_type());
+  cudnn_utils::filterEngineConfigs(fallback_list, filtered_configs, deterministic, allow_tf32, input.scalar_type());
+
+  for (auto &cfg : engine_configs) {
+    try {
+      auto plan = cudnn_frontend::ExecutionPlanBuilder()
+        .setHandle(handle)
+        .setEngineConfig(cfg)
+        .build();
+      auto plan_desc = plan.get_desc();
+      run(plan_desc);
+      execution_plan_cache[key] = plan_desc;
+      return quantized_output;
+    } catch (cudnn_frontend::cudnnException &e) {std::cout << "cudnn error:" << e.what() << std::endl;} catch(c10::CuDNNError &e) { std::cout << "other error" << e.what() << std::endl;}
+  }
+
+  TORCH_CHECK(false, "Unable to find an engine to execute this computation");
+}
+
+TORCH_LIBRARY_IMPL(quantized, QuantizedCUDA, m) {
+  m.impl(TORCH_SELECTIVE_NAME("quantized::add"), TORCH_FN(add</*ReLUFused=*/false>));
+  m.impl(TORCH_SELECTIVE_NAME("quantized::add_relu"), TORCH_FN(add</*ReLUFused=*/true>));
+}
+
+} // namespace native
+} // namespace at
+
+#endif  // HAS_CUDNN_V8
+#endif  // AT_CUDNN_ENABLED
+#endif  // USE_CUDA
