bd67bb0172807f9197e1139a58647f94bdf6e0ff
Jerry Zhang <jerryzh168@gmail.com>
2022-03-21 16:13:38 -0700

    [quant][core] Refactor qat conv implementation to use the same _ConvNd as base class
    Summary:
    att, this is to make supporting conv1d easier in future PR
    
    Test Plan:
    regression tests
    
    python test/test_quantization.py TestQuantizeFx
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/nn/qat/modules/conv.py
+++ torch/nn/qat/modules/conv.py
@@ -1,9 +1,90 @@
 import torch
 import torch.nn as nn
+from torch.nn.modules.utils import _pair, _triple
 from torch.nn.intrinsic import ConvReLU2d, ConvReLU3d, _FusedModule
+from typing import Tuple, Any
 
-# TODO: factor out the common parts to _ConvNd
-class Conv2d(nn.Conv2d):
+class _ConvNd(nn.modules.conv._ConvNd):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: Tuple[int, ...],
+                 stride: Tuple[int, ...],
+                 padding: Tuple[int, ...],
+                 dilation: Tuple[int, ...],
+                 transposed: bool,
+                 output_padding: Tuple[int, ...],
+                 groups: int,
+                 bias: bool,
+                 padding_mode: str,
+                 qconfig=None,
+                 device=None,
+                 dtype=None) -> None:
+        nn.modules.conv._ConvNd.__init__(self, in_channels, out_channels, kernel_size,
+                                         stride, padding, dilation, transposed,
+                                         output_padding, groups, bias, padding_mode, device, dtype)
+        assert qconfig, 'qconfig must be provided for QAT module'
+        self.qconfig = qconfig
+        self.weight_fake_quant = qconfig.weight(factory_kwargs=factory_kwargs)
+
+    def forward(self, input):
+        return self._conv_forward(input, self.weight_fake_quant(self.weight), self.bias)
+
+    @classmethod
+    def from_float(cls, mod):
+        r"""Create a qat module from a float module
+
+            Args:
+               `mod`: a float module, either produced by torch.ao.quantization utilities
+               or directly from user
+        """
+        assert type(mod) == cls._FLOAT_MODULE, (
+            "qat."
+            + cls.__name__
+            + ".from_float only works for "
+            + cls._FLOAT_MODULE.__name__
+        )
+        assert hasattr(mod, 'qconfig'), 'Input float module must have qconfig defined'
+        assert mod.qconfig, 'Input float module must have a valid qconfig'
+        if issubclass(type(mod), _FusedModule):
+            mod = mod[0]
+        qconfig = mod.qconfig
+        qat_conv = cls(mod.in_channels, mod.out_channels, mod.kernel_size,
+                       stride=mod.stride, padding=mod.padding, dilation=mod.dilation,
+                       groups=mod.groups, bias=mod.bias is not None,
+                       padding_mode=mod.padding_mode, qconfig=qconfig)
+        qat_conv.weight = mod.weight
+        qat_conv.bias = mod.bias
+        return qat_conv
+
+    def to_float(self):
+        conv = self._FLOAT_MODULE(
+            self.in_channels,
+            self.out_channels,
+            self.kernel_size,  # type: ignore[arg-type]
+            self.stride,  # type: ignore[arg-type]
+            self.padding,  # type: ignore[arg-type]
+            self.dilation,  # type: ignore[arg-type]
+            self.groups,
+            self.bias is not None,
+            self.padding_mode)
+        conv.weight = torch.nn.Parameter(self.weight.detach())
+        if self.bias is not None:
+            conv.bias = torch.nn.Parameter(self.bias.detach())
+        cls = type(self)
+        # conv relu
+        if issubclass(cls, _FusedModule):
+            modules = [conv]
+            assert hasattr(cls, "_FLOAT_RELU_MODULE")
+            relu = cls._FLOAT_RELU_MODULE()  # type: ignore[attr-defined]
+            modules.append(relu)
+            fused = cls._FLOAT_MODULE(*modules)  # type: ignore[arg-type]
+            fused.train(self.training)
+            return fused
+        else:
+            return conv
+
+class Conv2d(_ConvNd):
     r"""
     A Conv2d module attached with FakeQuantize modules for weight,
     used for quantization aware training.
@@ -20,72 +101,40 @@ class Conv2d(nn.Conv2d):
     """
     _FLOAT_MODULE = nn.Conv2d
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1,
-                 bias=True, padding_mode='zeros', qconfig=None,
-                 device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super().__init__(in_channels, out_channels, kernel_size,
-                         stride=stride, padding=padding, dilation=dilation,
-                         groups=groups, bias=bias, padding_mode=padding_mode,
-                         **factory_kwargs)
-        assert qconfig, 'qconfig must be provided for QAT module'
-        self.qconfig = qconfig
-        self.weight_fake_quant = qconfig.weight(factory_kwargs=factory_kwargs)
-
-    def forward(self, input):
-        return self._conv_forward(input, self.weight_fake_quant(self.weight), self.bias)
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: Tuple[int, ...],
+                 stride: Tuple[int, ...]=1,
+                 padding: Tuple[int, ...]=0,
+                 dilation: Tuple[int, ...]=1,
+                 groups: int=1,
+                 bias: bool=True,
+                 padding_mode: str='zeros',
+                 qconfig=None,
+                 device=None,
+                 dtype=None) -> None:
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            transposed=False,
+            output_padding=_pair(0),
+            groups=groups,
+            bias=bias,
+            padding_mode=padding_mode,
+            qconfig=qconfig,
+            device=device,
+            dtype=dtype)
 
     @classmethod
     def from_float(cls, mod):
-        r"""Create a qat module from a float module or qparams_dict
+        return super(Conv3d, cls).from_float(mod)
 
-            Args: `mod` a float module, either produced by torch.ao.quantization utilities
-            or directly from user
-        """
-        assert type(mod) == cls._FLOAT_MODULE, 'qat.' + cls.__name__ + '.from_float only works for ' + \
-            cls._FLOAT_MODULE.__name__
-        assert hasattr(mod, 'qconfig'), 'Input float module must have qconfig defined'
-        assert mod.qconfig, 'Input float module must have a valid qconfig'
-        if type(mod) == ConvReLU2d:
-            mod = mod[0]
-        qconfig = mod.qconfig
-        qat_conv = cls(mod.in_channels, mod.out_channels, mod.kernel_size,
-                       stride=mod.stride, padding=mod.padding, dilation=mod.dilation,
-                       groups=mod.groups, bias=mod.bias is not None,
-                       padding_mode=mod.padding_mode, qconfig=qconfig)
-        qat_conv.weight = mod.weight
-        qat_conv.bias = mod.bias
-        return qat_conv
-
-    def to_float(self):
-        conv = torch.nn.Conv2d(
-            self.in_channels,
-            self.out_channels,
-            self.kernel_size,  # type: ignore[arg-type]
-            self.stride,  # type: ignore[arg-type]
-            self.padding,  # type: ignore[arg-type]
-            self.dilation,  # type: ignore[arg-type]
-            self.groups,
-            self.bias is not None,
-            self.padding_mode)
-        conv.weight = torch.nn.Parameter(self.weight.detach())
-        if self.bias is not None:
-            conv.bias = torch.nn.Parameter(self.bias.detach())
-        cls = type(self)
-        # conv relu
-        if issubclass(cls, _FusedModule):
-            modules = [conv]
-            assert hasattr(cls, "_FLOAT_RELU_MODULE")
-            relu = cls._FLOAT_RELU_MODULE()  # type: ignore[attr-defined]
-            modules.append(relu)
-            fused = cls._FLOAT_MODULE(*modules)  # type: ignore[arg-type]
-            fused.train(self.training)
-            return fused
-        else:
-            return conv
-
-class Conv3d(nn.Conv3d):
+class Conv3d(_ConvNd):
     r"""
     A Conv3d module attached with FakeQuantize modules for weight,
     used for quantization aware training.
@@ -102,22 +151,19 @@ class Conv3d(nn.Conv3d):
     """
     _FLOAT_MODULE = nn.Conv3d
 
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        stride=1,
-        padding=0,
-        dilation=1,
-        groups=1,
-        bias=True,
-        padding_mode="zeros",
-        qconfig=None,
-        device=None,
-        dtype=None
-    ) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: Tuple[int, ...],
+                 stride: Tuple[int, ...]=1,
+                 padding: Tuple[int, ...]=0,
+                 dilation: Tuple[int, ...]=1,
+                 groups: int=1,
+                 bias: bool=True,
+                 padding_mode: str='zeros',
+                 qconfig=None,
+                 device=None,
+                 dtype=None) -> None:
         super().__init__(
             in_channels,
             out_channels,
@@ -125,75 +171,15 @@ class Conv3d(nn.Conv3d):
             stride=stride,
             padding=padding,
             dilation=dilation,
+            transposed=False,
+            output_padding=_triple(0),
             groups=groups,
             bias=bias,
             padding_mode=padding_mode,
-            **factory_kwargs
-        )
-        assert qconfig, "qconfig must be provided for QAT module"
-        self.qconfig = qconfig
-        self.weight_fake_quant = qconfig.weight(factory_kwargs=factory_kwargs)
-
-    def forward(self, input):
-        return self._conv_forward(input, self.weight_fake_quant(self.weight), self.bias)
+            qconfig=qconfig,
+            device=device,
+            dtype=dtype)
 
     @classmethod
     def from_float(cls, mod):
-        r"""Create a qat module from a float module or qparams_dict
-
-        Args: `mod` a float module, either produced by torch.ao.quantization utilities
-        or directly from user
-        """
-        assert type(mod) == cls._FLOAT_MODULE, (
-            "qat."
-            + cls.__name__
-            + ".from_float only works for "
-            + cls._FLOAT_MODULE.__name__
-        )
-        assert hasattr(mod, "qconfig"), "Input float module must have qconfig defined"
-        assert mod.qconfig, "Input float module must have a valid qconfig"
-        if type(mod) == ConvReLU3d:
-            mod = mod[0]
-        qconfig = mod.qconfig
-        qat_conv = cls(
-            mod.in_channels,
-            mod.out_channels,
-            mod.kernel_size,
-            stride=mod.stride,
-            padding=mod.padding,
-            dilation=mod.dilation,
-            groups=mod.groups,
-            bias=mod.bias is not None,
-            padding_mode=mod.padding_mode,
-            qconfig=qconfig,
-        )
-        qat_conv.weight = mod.weight
-        qat_conv.bias = mod.bias
-        return qat_conv
-
-    def to_float(self):
-        conv = torch.nn.Conv3d(
-            self.in_channels,
-            self.out_channels,
-            self.kernel_size,  # type: ignore[arg-type]
-            self.stride,  # type: ignore[arg-type]
-            self.padding,  # type: ignore[arg-type]
-            self.dilation,  # type: ignore[arg-type]
-            self.groups,
-            self.bias is not None,
-            self.padding_mode)
-        conv.weight = torch.nn.Parameter(self.weight.detach())
-        if self.bias is not None:
-            conv.bias = torch.nn.Parameter(self.bias.detach())
-        cls = type(self)
-        # conv relu
-        if issubclass(cls, _FusedModule):
-            modules = [conv]
-            assert hasattr(cls, "_FLOAT_RELU_MODULE")
-            relu = cls._FLOAT_RELU_MODULE()  # type: ignore[attr-defined]
-            modules.append(relu)
-            fused = cls._FLOAT_MODULE(*modules)  # type: ignore[arg-type]
-            fused.train(self.training)
-            return fused
-        else:
-            return conv
+        return super(Conv3d, cls).from_float(mod)
