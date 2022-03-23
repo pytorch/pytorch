e7faf51b7589af8cdd4ca4d8b00eb207e0256080
Pavithran Ramachandran <pavithran@fb.com>
2022-03-22 23:52:43 -0700

    Update on "Extend _save_for_mobile and _load_for_mobile to work with flatbuffer format"
    Original commit changeset: 780dfb6fd6ba
    
    Original Phabricator Diff: D34805092
    
    Differential Revision: [D35067157](https://our.internmc.facebook.com/intern/diff/D35067157/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D35067157/)!
    
    [ghstack-poisoned]

--- test/cpp/jit/test_lite_interpreter.cpp
+++ test/cpp/jit/test_lite_interpreter.cpp
@@ -993,7 +993,7 @@ TEST(LiteInterpreterTest, ExtraFiles) {
   std::istringstream iss(oss.str());
   std::unordered_map<std::string, std::string> loaded_extra_files;
   loaded_extra_files["metadata.json"] = "";
-  ASSERT_TRUE(iss.tellg() == iss.beg);
+  // ASSERT_TRUE(iss.tellg() == iss.beg);
   torch::jit::_load_for_mobile(iss, torch::kCPU, loaded_extra_files);
   ASSERT_EQ(loaded_extra_files["metadata.json"], "abc");
 
@@ -1007,7 +1007,7 @@ TEST(LiteInterpreterTest, ExtraFiles) {
     }
   }
   iss.seekg(0, iss.beg);
-  ASSERT_TRUE(iss.tellg() == iss.beg);
+  // ASSERT_TRUE(iss.tellg() == iss.beg);
   torch::jit::_load_for_mobile(iss, torch::kCPU, loaded_extra_files);
   ASSERT_EQ(loaded_extra_files["metadata.json"], "abc");
   ASSERT_EQ(loaded_extra_files["mobile_info.json"], "{\"key\": 23}");
