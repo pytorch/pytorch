8a9f7d32abc3737e840bab00dfb8c8c9c5a04cad
Jerry Zhang <jerryzh@fb.com>
2022-03-23 11:33:46 -0700

    [quant][fx] Support conv1d and its fusion variants in QAT (#74506)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74506
    
    This PR supports qat Conv1d, ConvBn1d, ConvBnReLU1d, ConvReLU1d in qat in FX Graph Mode Quantization
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx.test_conv_bn_relu
    
    Imported from OSS
    
    Reviewed By: vkuzo
    
    Differential Revision: D35032995
    
    fbshipit-source-id: 645da33f0d893aa44f35ee1384fd1539a9c788e7
    (cherry picked from commit 6b583baa74c5a4fd2f50270d633f277e2fc94716)

--- test/quantization/fx/test_quantize_fx.py
+++ test/quantization/fx/test_quantize_fx.py
@@ -1030,6 +1030,8 @@ class TestQuantizeFx(QuantizationTestCase):
         self.assertEqual(qparams, ref_qparams)
 
     def test_conv_bn_relu(self):
+        """ Tests fusion and quantization for "Conv - Bn" and "Conv - Bn - ReLU"
+        """
         convs = {
             1: nn.Conv1d,
             2: nn.Conv2d,
@@ -1070,8 +1072,7 @@ class TestQuantizeFx(QuantizationTestCase):
                 x = self.dequant(x)
                 return x
 
-        # TODO: add 1d support
-        options = itertools.product([2, 3], [True, False], self.static_quant_types)
+        options = itertools.product([1, 2, 3], [True, False], self.static_quant_types)
         for dim, has_relu, quant_type in options:
             expected_node = ns.call_module(
                 quantized_conv_relus[dim] if has_relu
