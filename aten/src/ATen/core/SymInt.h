f708b98c9bd67a3ab99cc6bda7317b8bee6edc17
Nikolay Korovaiko <korovaikon@gmail.com>
2022-03-23 10:42:11 -0700

    add a comment to SymInt

--- aten/src/ATen/core/SymInt.h
+++ aten/src/ATen/core/SymInt.h
@@ -3,6 +3,21 @@
 
 namespace c10 {
 
+// SymInt is a C++ wrapper class used by LTC and AOTAutograd for tracing
+// arithmetic operations on symbolic integers (e.g. sizes).
+// 
+// `SymInt` is also a data type in Pytorch that can be used in function schemas
+// to enable tracing.
+//
+// To trace the operations, SymInt overloads arithmetic operators (e.g. +, -, *)
+// and provides overloads taking SymInt for commonly used math functions.
+//
+// SymInt is semantically a union structure Union[int64_t, SymbolicIntNode*]
+// implemented as a single packed int64_t field named data_.
+//
+// data_ can be either a plain int64_t or (1 << 63 | `index`). `index` points to
+// SymbolicIntNode* that is responsible for constructing an IR node for
+// a traced operation to represent it in LTC or Fx graphs.
 class TORCH_API SymInt {
     public:
 
