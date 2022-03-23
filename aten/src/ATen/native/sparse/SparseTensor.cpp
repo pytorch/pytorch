6a9d73c33a226b0be5b6bcd4964b2af67f54171b
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared14901.04.pnb5.facebook.com>
2022-03-23 00:02:35 -0700

    2022-03-23 nightly release (65329f4fac8fb22318b7a3eb115e9da207d8d41a)

--- aten/src/ATen/native/sparse/SparseTensor.cpp
+++ aten/src/ATen/native/sparse/SparseTensor.cpp
@@ -664,8 +664,8 @@ SparseTensor _coalesce_sparse_cpu(const SparseTensor& self) {
   auto indicesBufferAccessor = indicesBuffer.accessor<int64_t, 1>();
 
   int64_t i = -1;
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND2(at::ScalarType::BFloat16, at::ScalarType::Half, values.scalar_type(),
-                                        "coalesce", [&] {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(at::ScalarType::BFloat16, at::ScalarType::Half, at::ScalarType::Bool, values.scalar_type(),
+                                         "coalesce", [&] {
     int64_t prev = -1;
     int64_t blockSize = values.stride(0);
     scalar_t* values_ptr = values.data_ptr<scalar_t>();
