61d466ded5f2a78816be9fac28c3ac3492b30e6e
atalman <atalman@fb.com>
2022-03-24 10:23:27 -0700

    Install cudnn into required path

--- .circleci/docker/common/install_cudnn.sh
+++ .circleci/docker/common/install_cudnn.sh
@@ -6,8 +6,10 @@ if [[ ${CUDNN_VERSION} == 8 ]]; then
     CUDNN_NAME="cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive"
     curl -OLs  https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/${CUDNN_NAME}.tar.xz
     tar xf ${CUDNN_NAME}.tar.xz
+    cp -a ${CUDNN_NAME}/include/* /usr/include/
     cp -a ${CUDNN_NAME}/include/* /usr/local/cuda/include/
     cp -a ${CUDNN_NAME}/include/* /usr/include/x86_64-linux-gnu/
+
     cp -a ${CUDNN_NAME}/lib/* /usr/local/cuda/lib64/
     cp -a ${CUDNN_NAME}/lib/* /usr/lib/x86_64-linux-gnu/
     cd ..
