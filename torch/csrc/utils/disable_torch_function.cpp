8b532a86cc3ba4dcf466e74c93da9f375205132a
Sherlockk Huang <bahuang@fb.com>
2022-03-24 23:21:21 +0000

    fix typo

--- torch/csrc/utils/disable_torch_function.cpp
+++ torch/csrc/utils/disable_torch_function.cpp
@@ -124,7 +124,7 @@ PyObject* THPModule_disable_torch_function(PyObject *self, PyObject *a) {
   } else if (PyTuple_Check(args)) {
     py_args = py::reinterpret_borrow<py::tuple>(args);
   } else {
-    throw torch::TypeError("expected List of Tuple (got %s)", Py_TYPE(args)->tp_name);
+    throw torch::TypeError("expected List or Tuple (got %s)", Py_TYPE(args)->tp_name);
   }
 
   // These are all C-API calls so no exceptions will be raised
@@ -153,7 +153,7 @@ PyObject* THPModule_disable_torch_dispatch(PyObject *self, PyObject *a) {
   } else if (PyTuple_Check(args)) {
     py_args = py::reinterpret_borrow<py::tuple>(args);
   } else {
-    throw torch::TypeError("expected List of Tuple (got %s)", Py_TYPE(args)->tp_name);
+    throw torch::TypeError("expected List or Tuple (got %s)", Py_TYPE(args)->tp_name);
   }
 
   // This implementation is not completely correct.  The moral
