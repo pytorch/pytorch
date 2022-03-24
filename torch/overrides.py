54b6b20b48e5b7d81e308ec48e6cd7fcffe2478b
Scott Wolchok <swolchok@fb.com>
2022-03-24 14:18:16 -0700

    Update on "[PyTorch] Delete NestedTensor Python wrapper"
    The wrapper just called through to methods on the underlying Tensor.
    
    Differential Revision: [D34689789](https://our.internmc.facebook.com/intern/diff/D34689789/)
    
    [ghstack-poisoned]

--- torch/overrides.py
+++ torch/overrides.py
@@ -151,6 +151,7 @@ def get_ignored_functions() -> Set[Callable]:
         torch.mkldnn_max_pool2d,
         torch.mkldnn_max_pool3d,
         torch.mkldnn_linear_backward_weights,
+        torch.nested_tensor,
         torch.normal,
         torch.ones,
         torch.promote_types,
