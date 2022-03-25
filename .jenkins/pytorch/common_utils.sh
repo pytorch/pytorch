192db444077454e17b2be996bf32f5aa755cf02c
Yeounoh Chung <yeounoh@google.com>
2022-03-25 17:01:30 +0000

    Add missing headers from torch/csrc/lazy/backend

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -92,6 +92,6 @@ function checkout_install_torchvision() {
 
 function clone_pytorch_xla() {
   if [[ ! -d ./xla ]]; then
-    git clone --recursive https://github.com/pytorch/xla.git
+    git clone --recursive -b test-lazy-tensor-device https://github.com/pytorch/xla.git
   fi
 }
