4bfb0d8680a21824c042318755134d79ca535d77
wayi1 <whuwangyi@gmail.com>
2022-03-24 23:01:35 -0700

    Fix post_localSGD_optimizer

--- torch/distributed/optim/post_localSGD_optimizer.py
+++ torch/distributed/optim/post_localSGD_optimizer.py
@@ -75,11 +75,12 @@ class PostLocalSGDOptimizer(torch.optim.Optimizer):
         Performs a single optimization step (parameter update).
         """
         self.optim.step()
+        params = []
         for param_group in self.param_groups:
-            for params in param_group["params"]:
-                if params.grad is None:
-                    continue
-                self.averager.average_parameters(iter(params))
+            for param in param_group["params"]:
+                if param.grad is not None:
+                    params.append(param)
+        self.averager.average_parameters(iter(params))
 
     def zero_grad(self):
         self.optim.zero_grad()
