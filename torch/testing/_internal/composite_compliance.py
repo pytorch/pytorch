72ae0355abe9826a2da9a11f9677e706b1f4d93a
Richard Zou <zou3519@gmail.com>
2022-03-28 15:05:58 -0700

    Test case where some inputs are Tensor Subclasses in CompositeCompiance (#74645)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74645
    
    This PR adds tests for when only some inputs are Tensor Subclasses.
    
    Why is this important to test?
    ==============================
    
    Consider the following hypothetical out-of-place operation:
    ```
    def my_add(x, y):
      result = x.clone()
      result.add_(y)
      return result
    ```
    
    You may expect this to work the same as torch.add. If x is not a Tensor
    Subclass, but y is a Tensor subclass, then this returns us a regular
    Tensor, NOT a Tensor subclass!
    
    This is exactly the type of in-place operations that causes `vmap` to
    fail and will be problematic for certain Tensor Subclasses in the future
    so we're adding tests to make sure Composite pytorch operations don't do
    this.
    
    What exactly does this PR do?
    =============================
    Composite compliance now takes a sample input and produces a test case
    where some of the sample inputs are Tensor Subclasses. It then sends
    this through the original operation, once with Python Mode and one
    without.
    
    (Why once with Python Mode? Because we want to use it to detect the
    pattern of "create a Tensor and call resize_ on it")
    
    Finally, it repeats this process for all possiblities where the inputs
    are Tensor subclasses. For example, if the sample input is (x, y), then
    we test all four of the following cases:
    - Subclass(x), y
    - x, Subclass(y)
    - Subclass(x), Subclass(y)
    - x, y
    
    Test Plan
    =========
    - run tests
    
    Test Plan: Imported from OSS
    
    Reviewed By: albanD
    
    Differential Revision: D35186862
    
    Pulled By: zou3519
    
    fbshipit-source-id: 102477507b56583463668db7523a6586d92b357d
    (cherry picked from commit bfcb087244b0598abb270f7c26d472482f00b5e2)

--- torch/testing/_internal/composite_compliance.py
+++ torch/testing/_internal/composite_compliance.py
@@ -1,10 +1,12 @@
 import torch
 from torch import Tensor
 import contextlib
+import itertools
 from typing import Iterator
-from torch.utils._pytree import tree_map
+from torch.utils._pytree import tree_map, tree_flatten, tree_unflatten
 from functools import partial
 from torch.utils._python_dispatch import enable_python_mode
+import re
 
 # TODO: move this into library proper
 @contextlib.contextmanager
@@ -92,6 +94,17 @@ def is_inplace_view_fn(func):
         'unsqueeze_',
     }
 
+
+# Introspection please save us
+def is_inplace(func):
+    name = func.overloadpacket.__name__
+    if re.match('__i.+__', name):
+        return True
+    if re.match('__.+__', name):
+        return False
+    return name[-1] == '_'
+
+
 class CompositeCompliantTensor(torch.Tensor):
     elem: torch.Tensor
 
@@ -126,6 +139,18 @@ class CompositeCompliantTensor(torch.Tensor):
             raise RuntimeError(
                 f"{func.__name__} is not allowed to be called inside of "
                 f"CompositeImplicitAutograd operators.")
+
+        if is_inplace(func):
+            # NB: We are making an assumption that if the function is in-place,
+            # then the first argument is being written to. Introspection please save us!
+            mutated_argument = args[0]
+            if not isinstance(mutated_argument, CompositeCompliantTensor) and \
+                    any([isinstance(a, CompositeCompliantTensor) for a in args[1:]]):
+                raise RuntimeError(
+                    'Not composite compliant: performing in-place operation '
+                    f'{func.__name__} where the Tensor being written to is '
+                    'regular Tensor but the other tensors are Tensor Subclasses. '
+                    'Please try to avoid this in-place operation.')
 
         with no_dispatch():
             unwrapped_args = tree_map(unwrap, args)
@@ -173,6 +198,127 @@ class CompositeCompliantTensor(torch.Tensor):
         tree_map(check, rs)
         return rs
 
+
+def is_tensorlist(lst):
+    if not isinstance(lst, list) and not isinstance(lst, tuple):
+        return False
+    if len(lst) == 0:
+        return False
+    all_tensors = all([isinstance(elt, torch.Tensor) for elt in lst])
+    if all_tensors:
+        return True
+    exists_one_tensor = all([isinstance(elt, torch.Tensor) for elt in lst])
+    if exists_one_tensor:
+        raise RuntimeError('This test assumes that PyTorch APIs cannot take '
+                           'mixed lists of Tensor and other things')
+    return False
+
+
+def maybe_map(fn, should_map, arg):
+    return fn(arg) if should_map else arg
+
+
+def wrap(arg):
+    if isinstance(arg, torch.Tensor):
+        return CompositeCompliantTensor(arg)
+    if is_tensorlist(arg):
+        return [CompositeCompliantTensor(a) for a in arg]
+    raise RuntimeError("wrap assumes that the input can be wrapped")
+
+
+# Given a list of flat arguments, some of which may be Tensors, return all
+# possible ways some of the arguments could be CompositeCompliantTensors (CCT).
+# For example, given Tensors A, B, C and flat_args = [A, 1, B],
+# We would return the following 4 options:
+# [CCT(A), 1, CCT(B)]
+# [CCT(A), 1, B]
+# [A, 1, CCT(B)]
+# [A, 1, B]
+# NB: Yes, this is exponential. No, we don't care too much because PyTorch ops
+# don't accept that many input Tensors.
+def generate_subclass_choices(flat_args):
+    is_tensor_likes = [isinstance(arg, torch.Tensor) or is_tensorlist(arg) for arg in flat_args]
+    subclass_options = [[False, True] if is_tensor_like else [False] for is_tensor_like in is_tensor_likes]
+
+    for which_args_are_wrapped in itertools.product(*subclass_options):
+        result = [maybe_map(wrap, should_wrap_arg, arg)
+                  for should_wrap_arg, arg in zip(which_args_are_wrapped, flat_args)]
+        yield result, which_args_are_wrapped
+
+
+# For an operation f(*args, **kwargs), each Tensor argument may either be
+# a regular Tensor or a Tensor Subclass. This iterator iterates through
+# all of those options.
+def generate_subclass_choices_args_kwargs(args, kwargs):
+    flat_kwargs, spec = tree_flatten(kwargs)
+    flat_args_kwargs = list(args) + list(flat_kwargs)
+    for choice, debug_metadata in generate_subclass_choices(flat_args_kwargs):
+        new_args = choice[:len(args)]
+        new_kwargs = tree_unflatten(choice[len(args):], spec)
+        which_args_are_wrapped = debug_metadata[:len(args)]
+        which_kwargs_are_wrapped = tree_unflatten(debug_metadata[len(args):], spec)
+        yield new_args, new_kwargs, which_args_are_wrapped, which_kwargs_are_wrapped
+
+
+def raise_composite_compliance_error(err, additional_info=''):
+    raise RuntimeError(
+        "CompositeImplicitAutograd compilance check failed with "
+        "the above error.\n"
+        f"{additional_info}"
+        "If you are adding an OpInfo of an "
+        "existing operator, please feel free to skip this test "
+        "because the problem was pre-existing and file an issue. "
+        "Otherwise, if you added a new operator, please read "
+        "through the CompositeImplicitAutograd Compliance section in "
+        "aten/src/ATen/native/README.md for how to resolve this. "
+    ) from err
+
+
+# This test checks ALL possible permutations of calling `op` with arguments
+# that are individually either a regular Tensor or a Tensor subclass.
+#
+# The general strategy is to wrap some Tensor args and kwargs in
+# CompositeCompliantTensor wrappers and call the operation.
+
+# If some composite operation does any non-compliant behavior,
+# CompositeCompliantTensor will raise an error.
+def check_all_permutations(op, args, kwargs):
+    def wrap(e):
+        return CompositeCompliantTensor(e) if isinstance(e, torch.Tensor) else e
+
+    for choice in generate_subclass_choices_args_kwargs(args, kwargs):
+        new_args, new_kwargs, which_args_are_wrapped, which_kwargs_are_wrapped = choice
+
+        try:
+            op(*new_args, **new_kwargs)
+        # NOTE: [What errors are Composite Compiance trying to catch?]
+        #
+        # There's two things we want to catch:
+        # - errors that would raise within the torch_dispatch impl
+        # - data_ptr accesses
+        # The first is easy to filter for (we could make the error a different
+        # error class), the second is always going to be a RuntimeError due to
+        # how it is implemented (if you try to access the data_ptr of thex
+        # wrapper Tensor, it raises you some internal RuntimeError).
+        #
+        # So the most general thing to catch here was RuntimeError. If you
+        # are here and debugging why your test failed, it's plausible that
+        # the operator itself is broken and that there are other tests failing.
+        except RuntimeError as err:
+            raise_composite_compliance_error(
+                err,
+                f"- wrapped_args: {which_args_are_wrapped}\n"
+                f"- wrapped_kwargs: {which_kwargs_are_wrapped}\n"
+            )
+
+# Checks via the usage of Python mode certain anti-patterns that
+# are not composite compliant.
+#
+# In particular, the anti-pattern we are trying to prevent is a user
+# creating an empty tensor and then resize_-ing it. Python Mode helps
+# here because all factory functions will create tensors that are
+# CompositeCompliantTensor.
+#
 # The general strategy is to wrap all Tensor args and kwargs in
 # CompositeCompliantTensor wrappers. If an operator that is
 # CompositeImplicitAutograd does any non-compliant behavior,
@@ -186,12 +332,6 @@ def check_with_mode(op, args, kwargs):
     try:
         with enable_python_mode(CompositeCompliantTensor):
             op(*args, **kwargs)
+    # see NOTE: [What errors are Composite Compiance trying to catch?]
     except RuntimeError as err:
-        raise RuntimeError("CompositeImplicitAutograd compilance check failed with "
-                           "the above error. If you are adding an OpInfo of an "
-                           "existing operator, please feel free to skip this test "
-                           "because the problem was pre-existing and file an issue. "
-                           "Otherwise, if you added a new operator, please read "
-                           "through the CompositeImplicitAutograd Compliance section in "
-                           "aten/src/ATen/native/README.md for how to resolve this. "
-                           ) from err
+        raise_composite_compliance_error(err)
