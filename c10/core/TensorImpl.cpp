49af97d7ae73c2554fe1c08e353fd3e96154172b
Brian Hirsh <hirsheybar@fb.com>
2022-03-28 12:58:00 -0700

    Back out "DispatchKeySet perf improvements"
    Original commit changeset: c7695e16dba3
    
    Original Phabricator Diff: D34227615
    
    The Milan Assistant is crashing, see P489979944
    
    After back out the change D34227615 and D34227616, it works fine now
    
    Differential Revision: [D35192317](https://our.internmc.facebook.com/intern/diff/D35192317/)
    
    [ghstack-poisoned]

--- c10/core/TensorImpl.cpp
+++ c10/core/TensorImpl.cpp
@@ -148,7 +148,10 @@ TensorImpl::TensorImpl(
       numel_(0),
       data_type_(data_type),
       device_opt_(storage_.device()),
-      key_set_(key_set - c10::python_ks) { // See [Note: Python key removal]
+      key_set_(
+          key_set.remove(DispatchKey::Python)
+              .remove(DispatchKey::PythonTLSSnapshot)) { // See [Note: Python
+                                                         // key removal]
   init_bitfields();
   // Inference tensor doesn't have version counter.
   if (!is_inference()) {
@@ -193,8 +196,10 @@ TensorImpl::TensorImpl(
 
   key_set = key_set | getAutocastRelatedKeySetFromBackend(k);
 
-  // See [Note: Python key removal]
-  key_set = key_set - c10::python_ks;
+  key_set =
+      key_set.remove(DispatchKey::Python)
+          .remove(
+              DispatchKey::PythonTLSSnapshot); // See [Note: Python key removal]
 
   // Inference tensor doesn't have autograd related keys.
   if (inference_mode) {
