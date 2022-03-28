85553785ec789d9ce7d08cddb96e4d4e70ac9b64
HDCharles <charlesdavidhernandez@gmail.com>
2022-03-28 11:33:11 -0700

    Composability of sparsity and fusion
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/nn/intrinsic/modules/fused.py
+++ torch/nn/intrinsic/modules/fused.py
@@ -1,6 +1,18 @@
 import torch
 from torch.nn import Conv1d, Conv2d, Conv3d, ReLU, Linear, BatchNorm1d, BatchNorm2d, BatchNorm3d
 
+# from torch.ao.quantization.utils import nonparam_type
+from torch.nn.utils.parametrize import is_parametrized
+def nonparam_type(module):
+    """
+    Returns type(module) or the original
+    type if module is currently parametrized
+    """
+    if is_parametrized(module):
+        return module.__class__.__bases__[0]
+    else:
+        return type(module)
+
 # Used for identifying intrinsic modules used in quantization
 class _FusedModule(torch.nn.Sequential):
     pass
@@ -9,90 +21,90 @@ class ConvReLU1d(_FusedModule):
     r"""This is a sequential container which calls the Conv1d and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, relu):
-        assert type(conv) == Conv1d and type(relu) == ReLU, \
+        assert nonparam_type(conv) == Conv1d and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(relu))
+                nonparam_type(conv), nonparam_type(relu))
         super().__init__(conv, relu)
 
 class ConvReLU2d(_FusedModule):
     r"""This is a sequential container which calls the Conv2d and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, relu):
-        assert type(conv) == Conv2d and type(relu) == ReLU, \
+        assert nonparam_type(conv) == Conv2d and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(relu))
+                nonparam_type(conv), nonparam_type(relu))
         super().__init__(conv, relu)
 
 class ConvReLU3d(_FusedModule):
     r"""This is a sequential container which calls the Conv3d and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, relu):
-        assert type(conv) == Conv3d and type(relu) == ReLU, \
+        assert nonparam_type(conv) == Conv3d and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(relu))
+                nonparam_type(conv), nonparam_type(relu))
         super().__init__(conv, relu)
 
 class LinearReLU(_FusedModule):
     r"""This is a sequential container which calls the Linear and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, linear, relu):
-        assert type(linear) == Linear and type(relu) == ReLU, \
+        assert nonparam_type(linear) == Linear and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(linear), type(relu))
+                nonparam_type(linear), nonparam_type(relu))
         super().__init__(linear, relu)
 
 class ConvBn1d(_FusedModule):
     r"""This is a sequential container which calls the Conv 1d and Batch Norm 1d modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn):
-        assert type(conv) == Conv1d and type(bn) == BatchNorm1d, \
+        assert nonparam_type(conv) == Conv1d and nonparam_type(bn) == BatchNorm1d, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(bn))
+                nonparam_type(conv), nonparam_type(bn))
         super().__init__(conv, bn)
 
 class ConvBn2d(_FusedModule):
     r"""This is a sequential container which calls the Conv 2d and Batch Norm 2d modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn):
-        assert type(conv) == Conv2d and type(bn) == BatchNorm2d, \
+        assert nonparam_type(conv) == Conv2d and nonparam_type(bn) == BatchNorm2d, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(bn))
+                nonparam_type(conv), nonparam_type(bn))
         super(ConvBn2d, self).__init__(conv, bn)
 
 class ConvBnReLU1d(_FusedModule):
     r"""This is a sequential container which calls the Conv 1d, Batch Norm 1d, and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn, relu):
-        assert type(conv) == Conv1d and type(bn) == BatchNorm1d and \
-            type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
-            .format(type(conv), type(bn), type(relu))
+        assert nonparam_type(conv) == Conv1d and nonparam_type(bn) == BatchNorm1d and \
+            nonparam_type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
+            .format(nonparam_type(conv), nonparam_type(bn), nonparam_type(relu))
         super().__init__(conv, bn, relu)
 
 class ConvBnReLU2d(_FusedModule):
     r"""This is a sequential container which calls the Conv 2d, Batch Norm 2d, and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn, relu):
-        assert type(conv) == Conv2d and type(bn) == BatchNorm2d and \
-            type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
-            .format(type(conv), type(bn), type(relu))
+        assert nonparam_type(conv) == Conv2d and nonparam_type(bn) == BatchNorm2d and \
+            nonparam_type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
+            .format(nonparam_type(conv), nonparam_type(bn), nonparam_type(relu))
         super().__init__(conv, bn, relu)
 
 class ConvBn3d(_FusedModule):
     r"""This is a sequential container which calls the Conv 3d and Batch Norm 3d modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn):
-        assert type(conv) == Conv3d and type(bn) == BatchNorm3d, \
+        assert nonparam_type(conv) == Conv3d and nonparam_type(bn) == BatchNorm3d, \
             'Incorrect types for input modules{}{}'.format(
-                type(conv), type(bn))
+                nonparam_type(conv), nonparam_type(bn))
         super().__init__(conv, bn)
 
 class ConvBnReLU3d(_FusedModule):
     r"""This is a sequential container which calls the Conv 3d, Batch Norm 3d, and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, conv, bn, relu):
-        assert type(conv) == Conv3d and type(bn) == BatchNorm3d and \
-            type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
-            .format(type(conv), type(bn), type(relu))
+        assert nonparam_type(conv) == Conv3d and nonparam_type(bn) == BatchNorm3d and \
+            nonparam_type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
+            .format(nonparam_type(conv), nonparam_type(bn), nonparam_type(relu))
         super().__init__(conv, bn, relu)
 
 
@@ -100,18 +112,18 @@ class BNReLU2d(_FusedModule):
     r"""This is a sequential container which calls the BatchNorm 2d and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, batch_norm, relu):
-        assert type(batch_norm) == BatchNorm2d and type(relu) == ReLU, \
+        assert nonparam_type(batch_norm) == BatchNorm2d and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(batch_norm), type(relu))
+                nonparam_type(batch_norm), nonparam_type(relu))
         super().__init__(batch_norm, relu)
 
 class BNReLU3d(_FusedModule):
     r"""This is a sequential container which calls the BatchNorm 3d and ReLU modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, batch_norm, relu):
-        assert type(batch_norm) == BatchNorm3d and type(relu) == ReLU, \
+        assert nonparam_type(batch_norm) == BatchNorm3d and nonparam_type(relu) == ReLU, \
             'Incorrect types for input modules{}{}'.format(
-                type(batch_norm), type(relu))
+                nonparam_type(batch_norm), nonparam_type(relu))
         super().__init__(batch_norm, relu)
 
 
@@ -119,6 +131,6 @@ class LinearBn1d(_FusedModule):
     r"""This is a sequential container which calls the Linear and BatchNorm1d modules.
     During quantization this will be replaced with the corresponding fused module."""
     def __init__(self, linear, bn):
-        assert type(linear) == Linear and type(bn) == BatchNorm1d, \
-            'Incorrect types for input modules{}{}'.format(type(linear), type(bn))
+        assert nonparam_type(linear) == Linear and nonparam_type(bn) == BatchNorm1d, \
+            'Incorrect types for input modules{}{}'.format(nonparam_type(linear), nonparam_type(bn))
         super().__init__(linear, bn)
