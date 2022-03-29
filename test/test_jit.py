aacdf291e00faa1c31c93ef0dd92a9a678898404
Elias Ellison <eellison@fb.com>
2022-03-29 11:32:31 -0700

    [JIT] Make aot autograd decompositions usable in JIT, add script for serializing the decompositions (#73938)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/73938
    
    This is a first step in porting and making usable all of the decompositions defined in [functorch](https://github.com/pytorch/functorch/blob/main/functorch/_src/decompositions.py#L349) in core and in JIT as well as C++.
    
    The decompositions are defined in python, scripted and inlined, and then serialized as C++ code which TorchScript can parse. The workflow is edit python decomposition file then run [tools/codegen/decompositions/gen_jit_decompositions.py](https://github.com/pytorch/pytorch/pull/73938/files#diff-6adef2116be233c3524e3b583e373ab0ffc9169beb6c1f6d96b5d0385e75afa1).
    
    Decompositions are mapped to their corresponding aten schemas via the schema in their python def. This allows multiple decompositions for an overloaded op like `aten.var` (shown here in the example).
    
    This is just a first PR, i'm sure there will be many follows ups such as:
    - making these runnable in C++ with simple executor
    - porting over more decompositions from AOT Autograd
    - Using opinfos / more robust testing
    - Categorizing decompositions
    - Hooking in decompositions at various points of JIT execution
    
    Test Plan: Imported from OSS
    
    Reviewed By: gchanan
    
    Differential Revision: D34938126
    
    Pulled By: eellison
    
    fbshipit-source-id: 9559a7cb731982e3a726f2f95af498b84fb09c13
    (cherry picked from commit a4e0e748791e378e7e12a9dd0b63fb3c62dc1890)

--- test/test_jit.py
+++ test/test_jit.py
@@ -25,6 +25,7 @@ from jit.test_class_type import TestClassType  # noqa: F401
 from jit.test_builtins import TestBuiltins, TestTensorBuiltins  # noqa: F401
 from jit.test_ignore_context_manager import TestIgnoreContextManager  # noqa: F401
 from jit.test_symbolic_shape_analysis import TestSymbolicShapeAnalysis  # noqa: F401
+from jit.test_op_decompositions import TestOpDecompositions  # noqa: F401
 from jit.test_if_hoisting import TestIfHoisting  # noqa: F401
 from jit.test_unsupported_ops import TestUnsupportedOps  # noqa: F401
 from jit.test_freezing import TestFreezing, TestFrozenOptimizations, TestMKLDNNReinplacing  # noqa: F401
