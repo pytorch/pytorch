fix PostLocalSGDOptimizer and ModelAverager average bug

torch/distributed/algorithms/model_averaging/averagers.py
@@ -1,6 +1,6 @@
 import warnings
 from abc import ABC, abstractmethod
-
+import types
 import torch.distributed as dist
 import torch.distributed.algorithms.model_averaging.utils as utils
 
@@ -83,6 +83,7 @@ class PeriodicModelAverager(ModelAverager):
         period,
         warmup_steps=0,
         process_group=None,
+        comm_memory_efficient=False
     ):
         super().__init__(process_group)
         if warmup_steps < 0:
@@ -98,6 +99,9 @@ class PeriodicModelAverager(ModelAverager):
                 "DistributedDataParallel should be used for this case."
             )
         self.period = period
+        # comm_memory_efficient is False, use a faster communication but more memory average strategy
+        # comm_memory_efficient is True, use a slower communication but less memory average strategy
+        self.comm_memory_efficient = comm_memory_efficient
 
     def average_parameters(self, params):
         r"""
@@ -109,5 +113,13 @@ class PeriodicModelAverager(ModelAverager):
             self.step >= self.warmup_steps
             and (self.step - self.warmup_steps) % self.period == 0
         ):
-            utils.average_parameters(iter(params), self.process_group)
+            if isinstance(params, types.GeneratorType):
+                # compatible with model.parameters() input
+                utils.average_parameters(params, self.process_group)
+            else:
+                # support optim.param_group input
+                if not self.comm_memory_efficient:
+                    utils.comm_average_param_fast(params, self.process_group)
+                else:
+                    utils.comm_average_param_memory_efficient(params, self.process_group)
         self.step += 1

torch/distributed/algorithms/model_averaging/hierarchical_model_averager.py
@@ -2,7 +2,7 @@
 import warnings
 from collections import OrderedDict
 import logging
-
+import types
 import torch.distributed as dist
 import torch.distributed.algorithms.model_averaging.averagers as averagers
 import torch.distributed.algorithms.model_averaging.utils as utils
@@ -92,7 +92,7 @@ class HierarchicalModelAverager(averagers.ModelAverager):
         `HierarchicalModelAverager` is experimental and subject to change.
     """
 
-    def __init__(self, period_group_size_dict=None, warmup_steps=0, process_group=None):
+    def __init__(self, period_group_size_dict=None, warmup_steps=0, process_group=None, comm_memory_efficient=False):
         super().__init__(process_group)
         if not period_group_size_dict:
             raise ValueError("Arg ``period_group_size_dict`` must not be empty.")
@@ -128,6 +128,10 @@ class HierarchicalModelAverager(averagers.ModelAverager):
         if warmup_steps < 0:
             raise ValueError("Arg ``warmup_steps`` must be a non-negative number.")
         self.warmup_steps = warmup_steps
+        # comm_memory_efficient is False, use a faster communication but more memory average strategy
+        # comm_memory_efficient is True, use a slower communication but less memory average strategy
+        self.comm_memory_efficient = comm_memory_efficient
+        logger.info("HierarchicalModelAverager comm_memory_efficient:{}".format(comm_memory_efficient))
 
     def _find_process_group(self):
         """
@@ -153,5 +157,13 @@ class HierarchicalModelAverager(averagers.ModelAverager):
         if self.step >= self.warmup_steps:
             found, group = self._find_process_group()
             if found:
-                utils.average_parameters(iter(params), group)
+                if isinstance(params, types.GeneratorType):
+                    # compatible with model.parameters() input
+                    utils.average_parameters(params, group)
+                else:
+                    # support optim.param_group input
+                    if not self.comm_memory_efficient:
+                        utils.comm_average_param_fast(params, group)
+                    else:
+                        utils.comm_average_param_memory_efficient(params, group)
         self.step += 1

torch/distributed/algorithms/model_averaging/utils.py
@@ -1,6 +1,6 @@
 # flake8: noqa C101
 import itertools
-from typing import Iterator
+from typing import Iterator, List
 
 import torch
 import torch.distributed as dist
@@ -32,5 +32,137 @@ def average_parameters(
 
     offset = 0
     for p in params_it2:
-        p.data = flat_params[offset : offset + p.numel()].view_as(p).type_as(p)
+        p.data = flat_params[offset: offset + p.numel()].view_as(p).type_as(p)
         offset += p.numel()
+
+
+class TensorBuffer:
+    """
+    Packs multiple tensors into one flat buffer for efficient
+    intra-worker communication.
+    refer to https://github.com/epfml/LocalSGD-Code/blob/3d4811d01673af205a00176f5389ed008a1ddb37/distributed_code/pcode/utils/tensor_buffer.py#L5
+    """
+
+    def __init__(self, tensors: List[torch.Tensor], use_cuda=True):
+        indices = [0]
+        for tensor in tensors:
+            new_end = indices[-1] + tensor.nelement()
+            indices.append(new_end)
+
+        self._start_idx = indices[:-1]
+        self._end_idx = indices[1:]
+        self._tensors_len = len(tensors)
+        self._tensors_sizes = [x.size() for x in tensors]
+
+        self.buffer = flatten(tensors, use_cuda=use_cuda)  # copies
+
+    def __getitem__(self, index):
+        return self.buffer[self._start_idx[index]: self._end_idx[index]].view(
+            self._tensors_sizes[index]
+        )
+
+    def __len__(self):
+        return self._tensors_len
+
+    def is_cuda(self):
+        return self.buffer.is_cuda
+
+    def nelement(self):
+        return self.buffer.nelement()
+
+    def unpack(self, tensors):
+        for tensor, entry in zip(tensors, self):
+            tensor.data[:] = entry
+
+
+def flatten(tensors, shapes=None, use_cuda=True):
+    """
+    refer to https://github.com/epfml/LocalSGD-Code/blob/3d4811d01673af205a00176f5389ed008a1ddb37/distributed_code/pcode/utils/tensor_buffer.py#L5
+    Args:
+        tensors:
+        shapes:
+        use_cuda:
+
+    Returns:
+
+    """
+    # init and recover the shapes vec.
+    pointers = [0]
+    if shapes is not None:
+        for shape in shapes:
+            pointers.append(pointers[-1] + shape[1])
+    else:
+        for tensor in tensors:
+            pointers.append(pointers[-1] + tensor.nelement())
+
+    # flattening.
+    vec = torch.empty(
+        pointers[-1],
+        device=tensors[0].device if tensors[0].is_cuda and use_cuda else "cpu",
+    )
+
+    for tensor, start_idx, end_idx in zip(tensors, pointers[:-1], pointers[1:]):
+        vec[start_idx:end_idx] = tensor.data.reshape(-1)
+    return vec
+
+
+def get_param_list(param_groups: List[dict]):
+    params_list = []
+    for param_group in param_groups:
+        for params in param_group["params"]:
+            if params.grad is None:
+                continue
+            params_list.append(params)
+    return params_list
+
+
+def average_parameters_v1(
+        params: torch.Tensor, process_group: dist.ProcessGroup
+):
+    """
+    Averages all the given parameters.
+    """
+    group_to_use = process_group if process_group is not None else dist.group.WORLD
+    # Do not update any parameter if not in the process group.
+    if dist._rank_not_in_group(group_to_use):
+        return
+
+    params /= dist.get_world_size(group_to_use)
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    dist.all_reduce(params, group=group_to_use)
+    return params
+
+
+def comm_average_param_fast(param_groups, process_group: dist.ProcessGroup = None):
+    """
+    use a faster communication but more memory average strategy
+    Args:
+        param_groups:
+
+    Returns:
+
+    """
+    params_list = get_param_list(param_groups)
+    params_tb = TensorBuffer(params_list)
+    params_tb.buffer = average_parameters_v1(params_tb.buffer, process_group)
+    # consistent the local models by assigning the consensus params.
+    params_tb.unpack(params_list)
+
+
+def comm_average_param_memory_efficient(param_groups, process_group: dist.ProcessGroup = None):
+    """
+    use a slower communication but less memory average strategy
+    Args:
+        param_groups:
+
+    Returns:
+
+    """
+    for param_group in param_groups:
+        for params in param_group["params"]:
+            if params.grad is None:
+                continue
+            buffer = flatten([params])
+            buffer = average_parameters_v1(buffer, process_group)
+            params.data[:] = buffer.reshape(params.size())

torch/distributed/optim/post_localSGD_optimizer.py
@@ -75,11 +75,7 @@ class PostLocalSGDOptimizer(torch.optim.Optimizer):
         Performs a single optimization step (parameter update).
         """
         self.optim.step()
-        for param_group in self.param_groups:
-            for params in param_group["params"]:
-                if params.grad is None:
-                    continue
-                self.averager.average_parameters(iter(params))
+        self.averager.average_parameters(params=self.param_groups)
 
     def zero_grad(self):
         self.optim.zero_grad()

torch/testing/_internal/distributed/distributed_test.py
@@ -1036,6 +1036,81 @@ class DistributedTest:
                         # No model averaging, so the parameters are not updated.
                         self.assertEqual(param.data, tensor)
 
+        @skip_if_lt_x_gpu(2)
+        def test_periodic_model_averager_param_group_fast(self):
+            rank = dist.get_rank()
+            world_size = dist.get_world_size()
+            rank_to_GPU = init_multigpu_helper(world_size, BACKEND)
+            device_id = rank_to_GPU[rank][0]
+    
+            model = nn.Linear(1, 5, bias=False).cuda(device_id)
+            param = next(model.parameters())
+            opt = torch.optim.SGD(model.parameters(), lr=0.1)
+    
+            period = 4
+            for warmup_steps in [12, 13, 14, 15]:
+                averager = averagers.PeriodicModelAverager(period=period, warmup_steps=warmup_steps)
+                for step in range(0, 20):
+                    # Reset the parameters at every step.
+                    for param_group in opt.param_groups:
+                        for params in param_group["params"]:
+                            if params.grad is None:
+                                continue
+                            params.data = torch.ones_like(param.data) * rank
+                    
+                    averager.average_parameters(opt.param_groups)
+                    if step >= warmup_steps and (step - warmup_steps) % period == 0:
+                        for param_group in opt.param_groups:
+                            for params in param_group["params"]:
+                                if params.grad is None:
+                                    continue
+                                self.assertEqual(param.data, torch.ones_like(param.data) * sum(range(world_size)) / world_size)
+                    else:
+                        # No model averaging, so the parameters are not updated.
+                        for param_group in opt.param_groups:
+                            for params in param_group["params"]:
+                                if params.grad is None:
+                                    continue
+                                self.assertEqual(param.data, torch.ones_like(param.data) * rank)
+
+        @skip_if_lt_x_gpu(2)
+        def test_periodic_model_averager_param_group_memory_efficient(self):
+            rank = dist.get_rank()
+            world_size = dist.get_world_size()
+            rank_to_GPU = init_multigpu_helper(world_size, BACKEND)
+            device_id = rank_to_GPU[rank][0]
+    
+            model = nn.Linear(1, 5, bias=False).cuda(device_id)
+            param = next(model.parameters())
+            opt = torch.optim.SGD(model.parameters(), lr=0.1)
+    
+            period = 4
+            for warmup_steps in [12, 13, 14, 15]:
+                averager = averagers.PeriodicModelAverager(period=period, warmup_steps=warmup_steps, comm_memory_efficient=True)
+                for step in range(0, 20):
+                    # Reset the parameters at every step.
+                    for param_group in opt.param_groups:
+                        for params in param_group["params"]:
+                            if params.grad is None:
+                                continue
+                            params.data = torch.ones_like(param.data) * rank
+            
+                    averager.average_parameters(opt.param_groups)
+                    if step >= warmup_steps and (step - warmup_steps) % period == 0:
+                        for param_group in opt.param_groups:
+                            for params in param_group["params"]:
+                                if params.grad is None:
+                                    continue
+                                self.assertEqual(param.data,
+                                                 torch.ones_like(param.data) * sum(range(world_size)) / world_size)
+                    else:
+                        # No model averaging, so the parameters are not updated.
+                        for param_group in opt.param_groups:
+                            for params in param_group["params"]:
+                                if params.grad is None:
+                                    continue
+                                self.assertEqual(param.data, torch.ones_like(param.data) * rank)
+
         @sandcastle_skip_if(
             BACKEND not in DistTestCases.backend_feature["subgroup"],
             f"The {BACKEND} backend does not support creating subgroups on CUDA devices"
@@ -1079,6 +1154,7 @@ class DistributedTest:
         @require_world_size(4)
         @skip_if_lt_x_gpu(4)
         def test_3_level_hierarchical_model_averager(self):
+            from torch.distributed.distributed_c10d import _pg_group_ranks
             rank = dist.get_rank()
             world_size = dist.get_world_size()
             rank_to_GPU = init_multigpu_helper(world_size, BACKEND)
@@ -1106,11 +1182,14 @@ class DistributedTest:
             averager = hierarchicalSGD.HierarchicalModelAverager(
                 period_group_size_dict=period_group_size_dict, warmup_steps=warmup_steps
             )
+            subgroup1 = averager.period_process_group_dict[subgroup_avg_period1]
+            subgroup2 = averager.period_process_group_dict[subgroup_avg_period2]
+            
             expected_avg_tensor_within_subgroup1 = (
-                torch.ones_like(param.data) * sum(range(subgroup_size1)) / subgroup_size1
+                torch.ones_like(param.data) * sum(list(_pg_group_ranks[subgroup1].keys())) / subgroup_size1
             )
             expected_avg_tensor_within_subgroup2 = (
-                torch.ones_like(param.data) * sum(range(subgroup_size2)) / subgroup_size2
+                torch.ones_like(param.data) * sum(list(_pg_group_ranks[subgroup2].keys())) / subgroup_size2
             )
             expected_global_avg_tensor = (
                 torch.ones_like(param.data) * sum(range(world_size)) / world_size
@@ -1124,7 +1203,7 @@ class DistributedTest:
                     self.assertEqual(param.data, expected_global_avg_tensor)
                 elif step == 12 or step == 20:
                     # Run model averaging within subgroup when `step` can be divided by 4 but not by 8.
-                    self.assertEqual(param.data, expected_avg_tensor_within_subgroup1)
+                    self.assertEqual(param.data, expected_avg_tensor_within_subgroup2)
                 elif step == 10 or step == 14 or step == 18 or step == 22:
                     # Run model averaging within subgroup when `step` can be divided by 2 but not by 4 or 8.
                     self.assertEqual(param.data, expected_avg_tensor_within_subgroup1)
@@ -4830,16 +4909,20 @@ class DistributedTest:
                 loss = loss_fn(output, target)
                 loss.backward()
                 opt.step()
-                averager.average_parameters(net.parameters())
+                averager.average_parameters(opt.param_groups)
 
                 post_localSGD_opt.zero_grad()
                 post_localSGD_output = post_localSGD_net(input)
                 post_localSGD_loss = loss_fn(post_localSGD_output, target)
                 post_localSGD_loss.backward()
                 post_localSGD_opt.step()
+                
+                for param_group1, param_group2 in zip(opt.param_groups, post_localSGD_opt.param_groups):
+                    for params1, params2 in zip(param_group1["params"], param_group2["params"]):
+                        if params1.grad is None:
+                            continue
+                        self.assertEqual(params1.data, params2.data)
 
-                for p1, p2 in zip(net.parameters(), post_localSGD_net.parameters()):
-                    self.assertEqual(p1.data, p2.data)
 
         @sandcastle_skip_if(
             BACKEND not in DistTestCases.backend_feature["ddp"],