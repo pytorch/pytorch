1d156c90aa41433fbfdd36fb9e4d1f335d2aaa5c
Eddie Yan <eddiey@nvidia.com>
2022-03-29 02:07:43 +0000

    fix no cudnn build

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -580,12 +580,46 @@ PyObject* THPModule_benchmarkCuDNN(PyObject* _unused, PyObject* noargs) {
   Py_RETURN_FALSE;
 }
 
+<<<<<<< HEAD
 PyObject* THPModule_setAllowTF32CuBLAS(PyObject* _unused, PyObject* arg) {
   THPUtils_assert(
       PyBool_Check(arg),
       "set_allow_tf32_cublas expects a bool, "
       "but got %s",
       THPUtils_typename(arg));
+=======
+PyObject *THPModule_setBenchmarkLimitCuDNN(PyObject *_unused, PyObject *arg)
+{
+  THPUtils_assert(THPUtils_checkLong(arg), "set_benchmark_limit_cudnn expects an int, "
+          "but got %s", THPUtils_typename(arg));
+  auto benchmark_limit = static_cast<int>(THPUtils_unpackLong(arg));
+#if defined(USE_ROCM)
+  if (benchmark_limit != 0 && benchmark_limit != 10) {
+    TORCH_WARN_ONCE("cuDNN Benchmark limit is not supported in MIOpen and will have no effect.");
+  }
+#endif
+#if AT_CUDNN_ENABLED()
+#if HAS_CUDNN_V8()
+  at::globalContext().setBenchmarkLimitCuDNN(benchmark_limit);
+#else
+  if (benchmark_limit != 0 && benchmark_limit != 10) {
+    TORCH_WARN_ONCE("cuDNN Benchmark limit is not supported with cuDNN v7 API and will have no effect.");
+  }
+#endif
+#endif
+  Py_RETURN_NONE;
+}
+
+PyObject *THPModule_benchmarkLimitCuDNN(PyObject *_unused, PyObject *noargs)
+{
+  return THPUtils_packInt32(at::globalContext().benchmarkLimitCuDNN());
+}
+
+PyObject *THPModule_setAllowTF32CuBLAS(PyObject *_unused, PyObject *arg)
+{
+  THPUtils_assert(PyBool_Check(arg), "set_allow_tf32_cublas expects a bool, "
+          "but got %s", THPUtils_typename(arg));
+>>>>>>> fix no cudnn build
   at::globalContext().setAllowTF32CuBLAS(arg == Py_True);
   Py_RETURN_NONE;
 }
