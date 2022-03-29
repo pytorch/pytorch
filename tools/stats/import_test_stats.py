475a4f6d1cfcd0bd0c2438088f381a9f84aa7d9d
Catherine Lee <csl@fb.com>
2022-03-29 14:43:56 -0700

    fixes #74233 actually modified a python file

--- tools/stats/import_test_stats.py
+++ tools/stats/import_test_stats.py
@@ -10,13 +10,14 @@ from urllib.request import urlopen
 
 def get_disabled_issues() -> List[str]:
     pr_body = os.getenv('PR_BODY', '')
+    commit_messages = os.getenv('COMMIT_MESSAGES', '')
     # The below regex is meant to match all *case-insensitive* keywords that
     # GitHub has delineated would link PRs to issues, more details here:
     # https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue.
     # E.g., "Close #62851", "fixES #62851" and "RESOLVED #62851" would all match, but not
     # "closes  #62851" --> extra space, "fixing #62851" --> not a keyword, nor "fix 62851" --> no #
     regex = '(?i)(Close(d|s)?|Resolve(d|s)?|Fix(ed|es)?) #([0-9]+)'
-    issue_numbers = [x[4] for x in re.findall(regex, pr_body)]
+    issue_numbers = [x[4] for x in re.findall(regex, f'{pr_body} {commit_messages}')]
     print("Ignoring disabled issues: ", issue_numbers)
     return issue_numbers
 
