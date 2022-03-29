568e02dcd73c7a0a6dadc98b1719b675b9d519ee
Christian Puhrsch <cpuhrsch@fb.com>
2022-03-29 18:44:02 +0000

    Support sum(sparse_csr)
    Basic support for summation of CSR. ~~Generalizes structured torch.sum to also support CSR.~~
    
    Follow up work:
    - Autograd support
    - OpInfo integration
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74766
    Approved by: https://github.com/ezyang

--- test/test_sparse_csr.py
+++ test/test_sparse_csr.py
@@ -1632,6 +1632,26 @@ class TestSparseCSR(TestCase):
 
     @skipMeta
     @dtypes(*get_all_dtypes())
+    def test_sum(self, device, dtype):
+        def run_test(shape, nnz, index_type):
+            a = self.genSparseCSRTensor(shape, nnz, dtype=dtype, device=device, index_dtype=index_dtype)
+            self.assertEqual(a.sum(), a.values().sum())
+            if dtype in get_all_fp_dtypes():
+                a.requires_grad_(True)
+                with self.assertRaisesRegex(RuntimeError,
+                                            ("Function SumBackward0 returned an invalid gradient at " +
+                                             "index 0 - expected layout SparseCsr but got Strided")):
+                    a.sum().backward()
+        for shape, index_dtype in itertools.product(
+                [(10, 5), (10, 10)],
+                [torch.int32, torch.int64]):
+            run_test(shape, 0, index_dtype)
+            run_test(shape, max(shape), index_dtype)
+            run_test(shape, shape[0] * shape[1], index_dtype)
+
+
+    @skipMeta
+    @dtypes(*get_all_dtypes())
     def test_transpose(self, device, dtype):
 
         def run_test(shape, nnz, index_type, dim0, dim1):
