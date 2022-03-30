fb94884804ea5296df6c27370061bda508cdad28
Brian Hirsh <hirsheybar@fb.com>
2022-03-30 11:57:41 -0700

    Update on "Back out "Back out "free up dispatch key space (in C++)"""
    Original commit changeset: b962de5d5eff
    
    Original Phabricator Diff: D35192346
    
    
    Back out "Back out "DispatchKeySet perf improvements""
    
    Original commit changeset: e38081810a56
    
    Original Phabricator Diff: D35192317
    
    Differential Revision: [D35222806](https://our.internmc.facebook.com/intern/diff/D35222806/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D35222806/)!
    
    [ghstack-poisoned]

--- aten/src/ATen/core/dispatch/OperatorEntry.cpp
+++ aten/src/ATen/core/dispatch/OperatorEntry.cpp
@@ -284,7 +284,7 @@ std::pair<const AnnotatedKernel&, const char*> OperatorEntry::computeDispatchTab
 
   // 3. Backend fallback
   auto dispatch_ix = getDispatchTableIndexForDispatchKey(dispatch_key);
-  if (dispatch_ix != -1) {
+  if (dispatch_ix < 0) {
     return {missingKernel(), "backend fallback not registered on mobile"};
   }
   if (dispatcher.backendFallbackKernels_[dispatch_ix].kernel.isValid()) {
