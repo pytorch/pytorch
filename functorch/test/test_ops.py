77b21a42e02878ca360fad665ccf543e649da57a
Horace He <chilli@fb.com>
2022-03-30 03:07:05 +0000

    [functorch] fixed some minor nits with decomposition

--- functorch/test/test_ops.py
+++ functorch/test/test_ops.py
@@ -1109,8 +1109,8 @@ class TestDecompositionOpInfo(TestCase):
             # Before adding an entry to this table, make sure your decomposition is right :)
             tol_table = {
                 # Due to strange epsilon behaviors, see https://github.com/pytorch/pytorch/issues/73161
-                (torch.float32, aten.native_layer_norm): (1e-3, 1e-3),
-                (torch.float32, aten.native_layer_norm_backward): (1e-3, 1e-3),
+                (torch.float32, aten.native_layer_norm.default): (1e-3, 1e-3),
+                (torch.float32, aten.native_layer_norm_backward.default): (1e-3, 1e-3),
             }
             if (b.dtype, op) in tol_table:
                 rtol, atol = tol_table[(b.dtype, op)]
@@ -1313,7 +1313,7 @@ class TestDecompositionOpInfo(TestCase):
                 f.write(f'{op}\n')
 
     def test_decompositions_torchscriptable(self, device):
-        skip_list = [torch.ops.aten.native_layer_norm_backward]
+        skip_list = []
         for op, decomposition in decomposition_table.items():
             if op in skip_list:
                 continue
