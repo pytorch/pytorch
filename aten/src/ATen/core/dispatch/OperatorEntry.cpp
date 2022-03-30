0b7247bd340fe355117632b612cea26ebdc07efb
Brian Hirsh <hirsheybar@fb.com>
2022-03-30 10:54:29 -0700

    Back out "Back out "free up dispatch key space (in C++)""
    Original commit changeset: b962de5d5eff
    
    Original Phabricator Diff: D35192346
    
    
    Back out "Back out "DispatchKeySet perf improvements""
    
    Original commit changeset: e38081810a56
    
    Original Phabricator Diff: D35192317
    
    Differential Revision: [D35222806](https://our.internmc.facebook.com/intern/diff/D35222806/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D35222806/)!
    
    [ghstack-poisoned]

--- aten/src/ATen/core/dispatch/OperatorEntry.cpp
+++ aten/src/ATen/core/dispatch/OperatorEntry.cpp
@@ -283,7 +283,10 @@ std::pair<const AnnotatedKernel&, const char*> OperatorEntry::computeDispatchTab
   }
 
   // 3. Backend fallback
-  auto dispatch_ix = static_cast<uint8_t>(dispatch_key);
+  auto dispatch_ix = getDispatchTableIndexForDispatchKey(dispatch_key);
+  if (dispatch_ix != -1) {
+    return {missingKernel(), "backend fallback not registered on mobile"};
+  }
   if (dispatcher.backendFallbackKernels_[dispatch_ix].kernel.isValid()) {
     return {dispatcher.backendFallbackKernels_[dispatch_ix], "backend fallback"};
   }
@@ -299,7 +302,7 @@ std::pair<const AnnotatedKernel&, const char*> OperatorEntry::computeDispatchTab
 // or alias keys and their associated keysets).
 // This function should be considered a private helper for updateDispatchTable_()
 void OperatorEntry::updateDispatchTableEntry_(const c10::Dispatcher& dispatcher, DispatchKey dispatch_key) {
-  const auto dispatch_ix = c10::getDispatchTableIndexForDispatchKey(dispatch_key);
+  const auto dispatch_ix = getDispatchTableIndexForDispatchKey(dispatch_key);
   if (C10_UNLIKELY(dispatch_ix == -1)) {
     return;
   }
@@ -329,8 +332,12 @@ void OperatorEntry::updateDispatchTable_(const c10::Dispatcher& dispatcher, Disp
   }
   // Note [Refresh Runtime Autograd entries in dispatchTable_]
   // Registering to backend key might affect computed entry at its Autograd backend key due to (2.1) & (2.3).
+  // In theory, we should only have to check if the given runtime key has "dense" functionality,
+  // e.g. DispatchKey::CPU (which is composed of DispatchKey::Dense and BackendComponent::CPUBit).
+  // However, there are some backends that should be included in this set that don't have the dense key set.
+  // E.g. DispatchKey::Meta, DispatchKey::ORT.
   if (c10::isBackendDispatchKey(dispatch_key)) {
-    DispatchKey autograd_key = getAutogradKeyFromBackend(dispatch_key);
+    DispatchKey autograd_key = getAutogradKeyFromBackend(toBackendComponent(dispatch_key));
     updateDispatchTableEntry_(dispatcher, autograd_key);
   }
 }
@@ -357,8 +364,9 @@ void OperatorEntry::updateDispatchTableFull_(const c10::Dispatcher& dispatcher) 
   // catchAll. After catchAllKernel_ is removed, Undefined now can get a kernel from either CompositeExplicitAutograd
   // or CompositeImplicitAutograd alias key so that we don't break the support. Ideally isIncludedInAlias(Undefined, CompositeImplicitAutograd)
   // should return true, it returns false because Undefined cannot be represented in a DispatchKeySet.
-  for (uint8_t iter = 0; iter != static_cast<uint8_t>(DispatchKey::NumDispatchKeys); ++iter) {
-    updateDispatchTable_(dispatcher, static_cast<DispatchKey>(iter));
+  updateDispatchTable_(dispatcher, DispatchKey::Undefined);
+  for (auto k : DispatchKeySet(DispatchKeySet::FULL)) {
+    updateDispatchTable_(dispatcher, k);
   }
 }
 
@@ -371,9 +379,13 @@ void OperatorEntry::checkInvariants() const {
   for (const auto& kv : kernels_) {
     TORCH_INTERNAL_ASSERT(kv.second.size() > 0, dumpState());
   }
-  for (uint8_t iter = 0; iter != static_cast<uint8_t>(DispatchKey::NumDispatchKeys); ++iter) {
-    auto expected_k = computeDispatchTableEntry(c10::Dispatcher::singleton(), static_cast<DispatchKey>(iter));
-    TORCH_INTERNAL_ASSERT(expected_k._equalsBoxedAndUnboxed(dispatchTable_[iter]),
+  for (auto k : DispatchKeySet(DispatchKeySet::FULL)) {
+    auto expected_k = computeDispatchTableEntry(c10::Dispatcher::singleton(), k);
+    auto idx = getDispatchTableIndexForDispatchKey(k);
+    if (C10_UNLIKELY(idx == -1)) {
+      continue;
+    }
+    TORCH_INTERNAL_ASSERT(expected_k._equalsBoxedAndUnboxed(dispatchTable_[idx]),
       "Canonical state\n~~~~~~~~~~~\n", dumpState(), "\n\n"
       "Computed table:\n~~~~~~~~~~~\n", dumpComputedTable());
   }
@@ -384,8 +396,9 @@ std::string OperatorEntry::listAllDispatchKeys() const {
   str << "[";
 
   bool has_kernels = false;
-  for (uint8_t iter = 0; iter != static_cast<uint8_t>(DispatchKey::NumDispatchKeys); ++iter) {
-    if (!dispatchTable_[iter].isValid()) {
+  for (auto k : DispatchKeySet(DispatchKeySet::FULL)) {
+    auto iter = getDispatchTableIndexForDispatchKey(k);
+    if (iter == -1 || !dispatchTable_[iter].isValid()) {
       continue;
     }
     if (has_kernels) {
@@ -423,6 +436,152 @@ void OperatorEntry::reportError(DispatchKey dispatchKey) const {
           "or that you (the operator writer) forgot to register a fallback function.  "
           "Available functions are ", listAllDispatchKeys(), ".\n\n", dumpComputedTable())
   }
+  switch (dispatchKey) {
+    case DispatchKey::Undefined:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::CPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::CUDA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::HIP:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::VE:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FPGA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::XPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::ORT:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::XLA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Lazy:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::MLC:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::HPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Vulkan:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Metal:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::QuantizedCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::QuantizedCUDA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::QuantizedXPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::CustomRNGKeyId:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::MkldnnCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseCUDA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseCsrCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseCsrCUDA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseHIP:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseVE:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::SparseXPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::NestedTensor:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Python:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::PythonTLSSnapshot:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::PrivateUse1:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::PrivateUse2:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::PrivateUse3:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Negative:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Conjugate:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Meta:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::ADInplaceOrView:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Autograd:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradXPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradCUDA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradXLA:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradLazy:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradMLC:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradHPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradPrivateUse1:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradPrivateUse2:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradPrivateUse3:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradOther:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradNestedTensor:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::ZeroTensor:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::BackendSelect:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Named:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Functionalize:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Tracer:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Autocast:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutocastCPU:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Batched:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::VmapMode:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::CompositeImplicitAutograd:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::CompositeExplicitAutograd:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::TESTING_ONLY_GenericWrapper:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::TESTING_ONLY_GenericMode:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FuncTorchDynamicLayerBackMode:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FuncTorchDynamicLayerFrontMode:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FuncTorchGradWrapper:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FuncTorchVmapMode:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::FuncTorchBatched:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Dense:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Quantized:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::Sparse:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    case DispatchKey::AutogradFunctionality:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+    default:
+      TORCH_CHECK_NOT_IMPLEMENTED(false, "");
+  }
 
   TORCH_CHECK_NOT_IMPLEMENTED(false, "Could not run '", name_, "' with arguments",
           " from the '", toString(dispatchKey), "' backend. This could be because "
@@ -443,8 +602,12 @@ void OperatorEntry::reportError(DispatchKey dispatchKey) const {
 // updateDispatchTableFull_ would update the dispatch table to be)
 std::string OperatorEntry::dumpComputedTable() const {
   std::ostringstream oss;
-  for (uint8_t i = 0; i < static_cast<uint8_t>(DispatchKey::NumDispatchKeys); i++) {
-    auto k = static_cast<DispatchKey>(i);
+  // Need to handle Undefined separately, because its a runtime key that can't be represented
+  // in a DispatchKeySet.
+  std::vector<DispatchKey> runtime_keys = {DispatchKey::Undefined};
+  for (auto k : DispatchKeySet(DispatchKeySet::FULL)) runtime_keys.push_back(k);
+
+  for (auto k : runtime_keys) {
     auto kernel_prov = computeDispatchTableEntryWithDebug(c10::Dispatcher::singleton(), k);
     if (kernel_prov.first.kernel.isValid()) {
       oss << toString(k) << ": "
