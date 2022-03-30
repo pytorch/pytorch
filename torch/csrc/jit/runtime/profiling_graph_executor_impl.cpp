9060872a9915491ee1e44d48adb04ec6010ceb2f
David Berard (Meta Employee) <dberard@fb.com>
2022-03-29 20:46:47 -0700

    [JIT][easy] comment about where nvfuser is called in profiling_graph_executor_impl.cpp (#74719)
    Summary:
    Will make it easier to figure out where NVFuser is getting called from.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74719
    
    Approved by: https://github.com/eellison
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/7cd78ea30784f4501d291827fe486492996560ed
    
    Reviewed By: seemethere
    
    Differential Revision: D35220875
    
    Pulled By: davidberard98
    
    fbshipit-source-id: f2857bae27b880707f38d42392fdb1982454c97f

--- torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
+++ torch/csrc/jit/runtime/profiling_graph_executor_impl.cpp
@@ -459,6 +459,7 @@ void ProfilingGraphExecutorImpl::runNoGradOptimizations(
     }
 
     // Run custom post-fusion passes
+    // e.g. NVFuser
     for (const auto& passPair : getCustomPostPasses()) {
       passPair.first(graph);
     }
