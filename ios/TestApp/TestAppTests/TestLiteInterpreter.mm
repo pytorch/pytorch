379a87835e3f8c83ada4b8c97845d78f9df26f50
Nikita Shulga <nshulga@fb.com>
2022-03-29 18:03:13 -0700

    Do not land - intro lint failures
    [ghstack-poisoned]

--- ios/TestApp/TestAppTests/TestLiteInterpreter.mm
+++ ios/TestApp/TestAppTests/TestLiteInterpreter.mm
@@ -11,7 +11,7 @@
 @implementation TestAppTests {
 }
 
-- (void)runModel:(NSString*)filename {
+- (void)runModel:(NSString*) filename {
   NSString* modelPath = [[NSBundle bundleForClass:[self class]] pathForResource:filename
                                                                          ofType:@"ptl"];
   XCTAssertNotNil(modelPath);
@@ -41,134 +41,134 @@
 }
 
 - (void)testPointwiseOps {
-  [self runModel:@"pointwise_ops"];
+    [self runModel:@"pointwise_ops"];
 }
 
 - (void)testReductionOps {
-  [self runModel:@"reduction_ops"];
+    [self runModel:@"reduction_ops"];
 }
 
 - (void)testComparisonOps {
-  [self runModel:@"comparison_ops"];
+    [self runModel:@"comparison_ops"];
 }
 
 - (void)testOtherMathOps {
-  [self runModel:@"other_math_ops"];
+    [self runModel:@"other_math_ops"];
 }
 
 - (void)testSpectralOps {
-  [self runModel:@"spectral_ops"];
+    [self runModel:@"spectral_ops"];
 }
 
 - (void)testBlasLapackOps {
-  [self runModel:@"blas_lapack_ops"];
+    [self runModel:@"blas_lapack_ops"];
 }
 
 - (void)testSamplingOps {
-  [self runModel:@"sampling_ops"];
+    [self runModel:@"sampling_ops"];
 }
 
 - (void)testTensorOps {
-  [self runModel:@"tensor_general_ops"];
+    [self runModel:@"tensor_general_ops"];
 }
 
 - (void)testTensorCreationOps {
-  [self runModel:@"tensor_creation_ops"];
+    [self runModel:@"tensor_creation_ops"];
 }
 
 - (void)testTensorIndexingOps {
-  [self runModel:@"tensor_indexing_ops"];
+    [self runModel:@"tensor_indexing_ops"];
 }
 
 - (void)testTensorTypingOps {
-  [self runModel:@"tensor_typing_ops"];
+    [self runModel:@"tensor_typing_ops"];
 }
 
 - (void)testTensorViewOps {
-  [self runModel:@"tensor_view_ops"];
+    [self runModel:@"tensor_view_ops"];
 }
 
 - (void)testConvolutionOps {
-  [self runModel:@"convolution_ops"];
+    [self runModel:@"convolution_ops"];
 }
 
 - (void)testPoolingOps {
-  [self runModel:@"pooling_ops"];
+    [self runModel:@"pooling_ops"];
 }
 
 - (void)testPaddingOps {
-  [self runModel:@"padding_ops"];
+    [self runModel:@"padding_ops"];
 }
 
 - (void)testActivationOps {
-  [self runModel:@"activation_ops"];
+    [self runModel:@"activation_ops"];
 }
 
 - (void)testNormalizationOps {
-  [self runModel:@"normalization_ops"];
+    [self runModel:@"normalization_ops"];
 }
 
 - (void)testRecurrentOps {
-  [self runModel:@"recurrent_ops"];
+    [self runModel:@"recurrent_ops"];
 }
 
 - (void)testTransformerOps {
-  [self runModel:@"transformer_ops"];
+    [self runModel:@"transformer_ops"];
 }
 
 - (void)testLinearOps {
-  [self runModel:@"linear_ops"];
+    [self runModel:@"linear_ops"];
 }
 
 - (void)testDropoutOps {
-  [self runModel:@"dropout_ops"];
+    [self runModel:@"dropout_ops"];
 }
 
 - (void)testSparseOps {
-  [self runModel:@"sparse_ops"];
+    [self runModel:@"sparse_ops"];
 }
 
 - (void)testDistanceFunctionOps {
-  [self runModel:@"distance_function_ops"];
+    [self runModel:@"distance_function_ops"];
 }
 
 - (void)testLossFunctionOps {
-  [self runModel:@"loss_function_ops"];
+    [self runModel:@"loss_function_ops"];
 }
 
 - (void)testVisionFunctionOps {
-  [self runModel:@"vision_function_ops"];
+    [self runModel:@"vision_function_ops"];
 }
 
 - (void)testShuffleOps {
-  [self runModel:@"shuffle_ops"];
+    [self runModel:@"shuffle_ops"];
 }
 
 - (void)testNNUtilsOps {
-  [self runModel:@"nn_utils_ops"];
+    [self runModel:@"nn_utils_ops"];
 }
 
 - (void)testQuantOps {
-  [self runModel:@"general_quant_ops"];
+    [self runModel:@"general_quant_ops"];
 }
 
 - (void)testDynamicQuantOps {
-  [self runModel:@"dynamic_quant_ops"];
+    [self runModel:@"dynamic_quant_ops"];
 }
 
 - (void)testStaticQuantOps {
-  [self runModel:@"static_quant_ops"];
+    [self runModel:@"static_quant_ops"];
 }
 
 - (void)testFusedQuantOps {
-  [self runModel:@"fused_quant_ops"];
+    [self runModel:@"fused_quant_ops"];
 }
 
 - (void)testTorchScriptBuiltinQuantOps {
-  [self runModel:@"torchscript_builtin_ops"];
+    [self runModel:@"torchscript_builtin_ops"];
 }
 
 - (void)testTorchScriptCollectionQuantOps {
-  [self runModel:@"torchscript_collection_ops"];
+    [self runModel:@"torchscript_collection_ops"];
 }
 @end
