d76904cb7eac24b13ae3e920aff981b83bb4beb3
Horace He <chilli@fb.com>
2022-03-30 04:52:51 +0000

    [functorch] fix None consistency

--- functorch/test/test_ops.py
+++ functorch/test/test_ops.py
@@ -1313,7 +1313,7 @@ class TestDecompositionOpInfo(TestCase):
                 f.write(f'{op}\n')
 
     def test_decompositions_torchscriptable(self, device):
-        skip_list = []
+        skip_list = [torch.ops.aten.native_layer_norm_backward.default]
         for op, decomposition in decomposition_table.items():
             if op in skip_list:
                 continue
