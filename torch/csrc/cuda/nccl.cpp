79e5b053b690852b21d881357904bc5a4438d95b
Richard Barnes <rbarnes@fb.com>
2022-03-30 22:02:52 -0700

    Check all CUDA API calls for errors in torch/ (#74923)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/74923
    
    Test Plan: Sandcastle
    
    Reviewed By: malfet
    
    Differential Revision: D35194935
    
    fbshipit-source-id: f5ec5be87cdf775eb9c99f8c3baed6b0366dda49
    (cherry picked from commit 7284c4ed7d57261d4936055e0c1a3f8f911fb1f0)

--- torch/csrc/cuda/nccl.cpp
+++ torch/csrc/cuda/nccl.cpp
@@ -154,7 +154,7 @@ struct NcclCommList {
     if (comms) {
       for(const auto i : c10::irange(ndevices)) {
         int dummy_var;
-        if (cudaGetDevice(&dummy_var) != cudaSuccess) {
+        if (C10_CUDA_ERROR_HANDLED(cudaGetDevice(&dummy_var)) != cudaSuccess) {
           /* there are cases when this destructor is called after the
            CUDA driver is already unloaded from the process.
            In these cases, skip ncclCommDestroy */
