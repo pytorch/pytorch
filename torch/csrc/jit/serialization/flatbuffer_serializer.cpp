1d2c377cb465ba27ef5e688716b634877d6cabf7
Pavithran Ramachandran <pavithran@fb.com>
2022-03-31 18:01:06 -0700

    reorder flatbuffer jit code
    Differential Revision: [D35301020](https://our.internmc.facebook.com/intern/diff/D35301020/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D35301020/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/serialization/flatbuffer_serializer.cpp
+++ torch/csrc/jit/serialization/flatbuffer_serializer.cpp
@@ -8,8 +8,6 @@
 #include <torch/csrc/jit/passes/inliner.h>
 #include <torch/csrc/jit/runtime/instruction.h>
 #include <torch/csrc/jit/serialization/export.h>
-#include <torch/csrc/jit/serialization/export_bytecode.h>
-#include <torch/csrc/jit/serialization/import.h>
 #include <string>
 
 namespace torch {
@@ -757,51 +755,6 @@ flatbuffers::DetachedBuffer save_mobile_module_to_bytes(
       extra_files,
       jit_sources,
       jit_constants);
-}
-
-Module parse_and_initialize_jit_module(
-    std::shared_ptr<char> data,
-    size_t,
-    c10::optional<at::Device>) {
-  auto* flatbuffer_module = mobile::serialization::GetMutableModule(data.get());
-  FlatbufferLoader loader;
-  mobile::Module mobilem = loader.parseModule(flatbuffer_module);
-  ExtraFilesMap files;
-  std::vector<IValue> constants;
-  loader.extractJitSourceAndConstants(&files, &constants);
-  Module m = jitModuleFromSourceAndConstants(
-      mobilem._ivalue(), files, constants, flatbuffer_module->version());
-  m.set_delete_memory(data);
-  return m;
-}
-
-Module load_jit_module_from_file(
-    const std::string& filename,
-    c10::optional<at::Device> device) {
-  auto data = get_file_content(filename.c_str());
-  return parse_and_initialize_jit_module(
-      std::move(std::get<0>(data)), std::get<1>(data), device);
-}
-
-void save_jit_module(
-    const Module& module,
-    const std::string& filename,
-    const ExtraFilesMap& extra_files) {
-  auto buffer = save_jit_module_to_bytes(module, extra_files);
-  std::fstream ofile(filename, std::ios::binary | std::ios::out);
-  ofile.write(reinterpret_cast<char*>(buffer.data()), buffer.size()); // NOLINT
-  ofile.close();
-}
-
-flatbuffers::DetachedBuffer save_jit_module_to_bytes(
-    const Module& module,
-    const ExtraFilesMap& extra_files) {
-  ExtraFilesMap jitfiles;
-  std::vector<IValue> constants;
-  jitModuleToPythonCodeAndConstants(module, &jitfiles, &constants);
-  CompilationOptions options;
-  mobile::Module mobilem = jitModuleToMobile(module, options);
-  return save_mobile_module_to_bytes(mobilem, extra_files, jitfiles, constants);
 }
 
 } // namespace jit
