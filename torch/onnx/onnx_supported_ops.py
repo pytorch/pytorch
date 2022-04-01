0a853e67111530ceb888c3dd76406a1c5a8a2d1f
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-04-01 10:29:08 -0700

    Address comments
    Signed-off-by: Thiago Crepaldi <thiago.crepaldi@microsoft.com>

--- torch/onnx/onnx_supported_ops.py
+++ torch/onnx/onnx_supported_ops.py
@@ -5,7 +5,10 @@ from torch.onnx.symbolic_helper import _onnx_main_opset, _onnx_stable_opsets
 from typing import Dict, List, Union
 
 
-class TorchSchema:
+for v in _onnx_stable_opsets + [_onnx_main_opset]:
+    register_version("", v)
+
+class _TorchSchema:
     def __init__(self, schema: Union[FunctionSchema, str]) -> None:
         if isinstance(schema, FunctionSchema):
             self.name: str = schema.name
@@ -32,10 +35,14 @@ class TorchSchema:
         s += ", ".join(str(opset) for opset in self.opsets)
         return s
 
+    def __hash__(self):
+        # TODO: handle overload_name?
+        return hash((self.name))
+
     def __eq__(self, other) -> bool:
-        if not isinstance(other, TorchSchema):
+        if not isinstance(other, _TorchSchema):
             return False
-        # TODO: handle overloads?
+        # TODO: handle overload_name?
         return self.name == other.name
 
     def is_aten(self) -> bool:
@@ -45,23 +52,15 @@ class TorchSchema:
         return "backward" in self.name
 
 
-def all_aten_forward_schemas():
-    """ Creates a list of TorchSchema for all aten schemas"""
-
-    torch_schemas = [TorchSchema(s) for s in _jit_get_all_schemas()]
+def _all_aten_forward_schemas():
+    """ Creates a list of _TorchSchema for all aten schemas"""
+    torch_schemas = [_TorchSchema(s) for s in _jit_get_all_schemas()]
     torch_schemas = sorted(torch_schemas, key=lambda x: x.name)
     aten_schemas = [s for s in torch_schemas if s.is_aten() and not s.is_backward()]
     return aten_schemas
 
 
-# TODO: Do not hard code opset here
-# Create TorchSchema object directory of all registered symbolics
-# get_registered_op(opname, domain, version):
-for v in _onnx_stable_opsets + [_onnx_main_opset]:
-    register_version("", v)
-
-
-def symbolic_argument_count(func):
+def _symbolic_argument_count(func):
     params = []
     sig = signature(func)
     optional_params = []
@@ -78,14 +77,14 @@ def symbolic_argument_count(func):
     return params
 
 
-def all_symbolics_schemas():
-    symbolics_schemas: Dict[str, TorchSchema] = dict()
+def _all_symbolics_schemas():
+    symbolics_schemas: Dict[str, _TorchSchema] = dict()
 
     for domain, version in _registry:
         for opname, sym_func in _registry[(domain, version)].items():
-            symbolics_schema = TorchSchema("aten::" + opname)
-            symbolics_schema.arguments = symbolic_argument_count(sym_func)
-            if opname in symbolics_schemas.keys():
+            symbolics_schema = _TorchSchema("aten::" + opname)
+            symbolics_schema.arguments = _symbolic_argument_count(sym_func)
+            if opname in symbolics_schemas:
                 symbolics_schemas[opname].opsets.append(version)
             else:
                 symbolics_schema.opsets = [version]
@@ -94,17 +93,18 @@ def all_symbolics_schemas():
 
 
 def onnx_supported_ops():
-    aten_schemas = all_aten_forward_schemas()
-    symbolic_schemas = all_symbolics_schemas()
+    aten_schemas = _all_aten_forward_schemas()
+    symbolic_schemas = _all_symbolics_schemas()
+    torch_schemas = set(symbolic_schemas.values())
     supported_ops, unsupported_ops = list(), list()
     onnx_supported_ops = list()
     for schema in aten_schemas:
-        if schema in symbolic_schemas.values():
+        if schema in torch_schemas:
             opname = schema.name[6:]  # without "aten::" prefix
             opsets = symbolic_schemas[opname].opsets
             if schema not in supported_ops:
                 supported_ops.append(symbolic_schemas[opname])
-                onnx_supported_ops.append((opname, " ".join([str(o) for o in opsets])))
+                onnx_supported_ops.append((opname, " ".join(str(o) for o in opsets)))
         else:
             unsupported_ops.append(schema)
     return sorted(onnx_supported_ops, key=lambda x: x[0])
