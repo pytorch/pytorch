914b75c0d7a01618169d9a515393f1c63aa014c5
Nikolay Korovaiko <korovaikon@gmail.com>
2022-03-31 20:53:01 -0700

    add missing bindings for lazy_bench.py

--- torch/_lazy/metrics.py
+++ torch/_lazy/metrics.py
@@ -4,6 +4,9 @@ def reset():
     """Resets all metric counters."""
     torch._C._lazy._reset_metrics()
 
+def counter_value(name: str) -> int:
+    return torch._C._lazy._counter_value(name)
+
 def counter_names():
     """Retrieves all the currently active counter names."""
     return torch._C._lazy._counter_names()
