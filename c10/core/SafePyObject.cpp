fe399fb7988b64933581efcee6b7426597f6dda7
Edward Z. Yang <ezyang@fb.com>
2022-04-01 20:40:54 -0700

    Update on "Introduce SafePyObject, make TorchDispatchTypeObject use it"
    The pattern of a PyObject* bundled with a PyInterpreter* is pretty
    useful in many contexts (e.g., TorchDispatchTypeObject) so I have turned
    it into a dedicated class SafePyObject.  In the process I fixed a
    bug with the old TorchDispatchTypeObject (copy constructor/assignment
    was not deleted), made the API more safe (retrieving the PyObject*
    pointer requires verification that the PyInterpreter* matches) and
    fixed some minor inefficiencies in C++ code.
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- c10/core/SafePyObject.cpp
+++ c10/core/SafePyObject.cpp
@@ -2,15 +2,6 @@
 
 namespace c10 {
 
-TorchDispatchTypeObject::TorchDispatchTypeObject(
-    PyObject* type_object,
-    c10::impl::PyInterpreter* pyinterpreter)
-    : data_(type_object), pyinterpreter_(pyinterpreter) {}
-
-TorchDispatchTypeObject::~TorchDispatchTypeObject() {
-  pyinterpreter_->decref(data_, /*is_tensor*/ false);
-}
-
 PyObject* SafePyObject::ptr(const c10::impl::PyInterpreter* interpreter) const {
   TORCH_INTERNAL_ASSERT(interpreter == pyinterpreter_);
   return data_;
--- c10/core/SafePyObject.h
+++ c10/core/SafePyObject.h
@@ -21,7 +21,7 @@ namespace c10 {
 struct C10_API SafePyObject {
   // Steals a reference to type_object
   SafePyObject(PyObject* data, c10::impl::PyInterpreter* pyinterpreter)
-    : data_(type_object), pyinterpreter_(pyinterpreter)  {}
+    : data_(data), pyinterpreter_(pyinterpreter)  {}
 
   // In principle this could be copyable if we add an incref to PyInterpreter
   // but for now it's easier to just disallow it.
@@ -30,7 +30,7 @@ struct C10_API SafePyObject {
 
   ~SafePyObject() { pyinterpreter_->decref(data_, /*is_tensor*/ false); }
 
-  c10::impl::PyInterpreter* pyinterpreter() const { return pyinterpreter_ }
+  c10::impl::PyInterpreter* pyinterpreter() const { return pyinterpreter_; }
   PyObject* ptr(const c10::impl::PyInterpreter*) const;
 
  private:
