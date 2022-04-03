bf708d5e49e7b9976c79f9603dfc456eef486667
lvhaijun01 <lvhaijun01@baidu.com>
2022-04-03 15:33:58 +0800

    fix argument name

--- torch/distributed/algorithms/model_averaging/utils.py
+++ torch/distributed/algorithms/model_averaging/utils.py
@@ -36,17 +36,17 @@ def average_parameters(
                     flat_params_all_reduce([param], group_to_use)
 
 
-def flat_params_all_reduce(params: Union[types.GeneratorType, List], process_group: dist.ProcessGroup):
+def flat_params_all_reduce(params: Union[types.GeneratorType, List], group_to_use: dist.ProcessGroup):
     params_it1, params_it2 = itertools.tee(params)
     # If the input parameters have different data types,
     # packing these parameters will trigger an implicit type up-casting.
     # The original parameter data types will be restored during the subsequent unpacking.
     flat_params = torch.cat([p.data.reshape(-1) for p in params_it1])
-    flat_params /= dist.get_world_size(process_group)
+    flat_params /= dist.get_world_size(group_to_use)
     # Make sure the allreduce will not conflict with any other ongoing process group.
     if torch.cuda.is_available():
         torch.cuda.synchronize()
-    dist.all_reduce(flat_params, group=process_group)
+    dist.all_reduce(flat_params, group=group_to_use)
     offset = 0
     for p in params_it2:
         p.data = flat_params[offset: offset + p.numel()].view_as(p).type_as(p)
