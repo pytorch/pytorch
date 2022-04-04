a8fd9e672db7e32dc8676ffa75da613d1223ebfa
Brian Hirsh <hirsheybar@fb.com>
2022-04-04 15:33:20 -0700

    add DispatchKeySet function to get highest backend key
    [ghstack-poisoned]

--- c10/core/DispatchKeySet.h
+++ c10/core/DispatchKeySet.h
@@ -731,6 +731,16 @@ constexpr auto autograd_privateuse3_ks =
     DispatchKeySet(DispatchKey::AutogradPrivateUse3);
 constexpr auto autograd_other_ks = DispatchKeySet(DispatchKey::AutogradOther);
 
+// This keyset has:
+// (1) the functionality bits corresponding to backends (dense, sparse, quantized)
+// (2) all of the backend bits set
+constexpr DispatchKeySet backend_functionality_keys = DispatchKeySet({
+    DispatchKey::Dense,
+    DispatchKey::Quantized,
+    DispatchKey::Sparse,
+}) | DispatchKeySet(DispatchKeySet::RAW, full_backend_mask);
+
+
 struct OpTableOffsetAndMask {
   uint16_t offset;
   uint16_t backend_mask;
@@ -802,6 +812,14 @@ inline DispatchKeySet getAutocastRelatedKeySetFromBackend(BackendComponent t) {
     default:
       return DispatchKeySet();
   }
+}
+
+// returns the "backend" DispatchKey of highest priority in the set.
+// This is basically like highestBackendKey(), except that we have some
+// "functionality" bits that correspond to backends (Sparse, Quantized)
+inline DispatchKey highestPriorityBackendTypeId(DispatchKeySet ks) {
+  auto backend_key = (ks & backend_functionality_keys).highestPriorityTypeId();
+  return backend_key;
 }
 
 // This API exists because we have a use case for checking
