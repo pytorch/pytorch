780018dec0ae82f75e8982ab1183b3be282e35ff
Pavithran Ramachandran <pavithran@fb.com>
2022-04-04 14:18:43 -0700

    Update on "[PyTorchEdge] Backport from v9 flatbuffer to v8 pickle"
    In this diff:
    1. Bump supported version to 9, which will serve as a placeholder for upcoming version bump to v9 for flatbuffer format migration.
    2. Implements backport from v9 flatbuffer file to v8 pickle file.
    
    Differential Revision: [D34702597](https://our.internmc.facebook.com/intern/diff/D34702597/)
    
    [ghstack-poisoned]

--- caffe2/serialize/versions.h
+++ caffe2/serialize/versions.h
@@ -118,14 +118,15 @@ constexpr uint64_t kMinProducedFileFormatVersion = 0x3L;
 //  `140245072983168.storage` Forward-compatibility change.
 //  0x6L: Implicit opereator versioning using number of specified argument.
 //  Refer to the summary of https://github.com/pytorch/pytorch/pull/56845 for
-//  details. 0x7L: Enable support for operators with default arguments plus out
+//  details.
+//  0x7L: Enable support for operators with default arguments plus out
 //  arguments. Refer. See https://github.com/pytorch/pytorch/pull/63651 for
-//  details 0x8L: Emit promoted operators as instructions. See
-//  https://github.com/pytorch/pytorch/pull/71662 for details 0x9L: Change
-//  serialization format from pickle to format This version is to serve
-//  migration. v8 pickle and v9 flatbuffer are the same.
-//  Refer to the summary of https://github.com/pytorch/pytorch/pull/75201
-//  for more details.
+//  details.
+//  0x8L: Emit promoted operators as instructions. See
+//  https://github.com/pytorch/pytorch/pull/71662 for details.
+//  0x9L: Change serialization format from pickle to format This version is to
+//  serve migration. v8 pickle and v9 flatbuffer are the same. Refer to the
+//  summary of https://github.com/pytorch/pytorch/pull/75201 for more details.
 constexpr uint64_t kProducedBytecodeVersion = 0x8L;
 
 // static_assert(
