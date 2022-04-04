d955e16034c8e77ba15c45769e686f0ef0bde454
Jerry Zhang <jerryzh168@gmail.com>
2022-04-04 10:28:12 -0700

    [quant][fx] Remove Standalone and CustomModule QuantizeHandler type checks in prepare
    Summary:
    Instead of checking the type we use a method in the QuantizeHandler to check if a module
    is a standalone or custom module, not user facing
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/quantization/fx/match_utils.py
+++ torch/ao/quantization/fx/match_utils.py
@@ -198,7 +198,7 @@ def find_matches(
            type(modules[node.target]) in custom_module_classes:
             custom_module_qconfig = qconfig_map[node.name]
             match_map[node.name] = (
-                node, node, None, CustomModuleQuantizeHandler(node, modules),
+                node, node, None, QuantizeHandler(node, modules, is_custom_module=True),
                 custom_module_qconfig)
 
     def is_standalone_module(node_target: str, modules: Dict[str, torch.nn.Module]):
@@ -214,10 +214,10 @@ def find_matches(
            (is_standalone_module(node.target, modules) or
                 is_observed_standalone_module(modules[node.target])):
             # add node to matched nodes
-            custom_module_qconfig = qconfig_map[node.name]
+            standalone_module_qconfig = qconfig_map[node.name]
             match_map[node.name] = (
                 node, node, None,
-                StandaloneModuleQuantizeHandler(node, modules),
-                custom_module_qconfig)
+                QuantizeHandler(node, modules, is_standalone_module=True),
+                standalone_module_qconfig)
 
     return match_map
--- torch/ao/quantization/fx/prepare.py
+++ torch/ao/quantization/fx/prepare.py
@@ -30,8 +30,6 @@ from .qconfig_utils import (
 
 from .quantization_patterns import (
     QuantizeHandler,
-    CustomModuleQuantizeHandler,
-    StandaloneModuleQuantizeHandler,
 )
 
 from .quantization_types import (
@@ -490,8 +488,7 @@ def maybe_insert_input_observer_for_arg_or_kwarg(
     # default (no observer)
     new_arg = arg
 
-    is_standalone_module = qhandler is not None and \
-        isinstance(qhandler, StandaloneModuleQuantizeHandler)
+    is_standalone_module = qhandler is not None and qhandler.is_standalone_module()
     assert qconfig is not None
     if not is_standalone_module:
         # regular flow for most nodes, except standalone modules
@@ -715,8 +712,7 @@ def maybe_insert_output_observer_for_node(
     assert qconfig is not None
     assert node.op != 'output', 'observer insertion for outputs is handled elsewhere'
 
-    is_standalone_module = qhandler is not None and \
-        isinstance(qhandler, StandaloneModuleQuantizeHandler)
+    is_standalone_module = qhandler is not None and qhandler.is_standalone_module()
 
     dtype = node_name_to_target_dtype[node.name]["output_activation_dtype"]
     should_insert_observer = dtype not in DO_NOT_OBS_DTYPE_LIST + [torch.float]
@@ -1253,7 +1249,7 @@ def insert_observers_for_model(
                                 if not maybe_make_input_output_share_observers(node, model, modules):
                                     remove_output_observer(node, model, modules)
 
-                            if isinstance(qhandler, CustomModuleQuantizeHandler):
+                            if qhandler.is_custom_module():
                                 swap_custom_module_to_observed(node, qconfig, modules, prepare_custom_config_dict)
 
                 else:  # output
@@ -1296,7 +1292,7 @@ def run_prepare_fx_on_standalone_modules(
     ) in matches.items():
         if qhandler is None:
             continue
-        elif not isinstance(qhandler, StandaloneModuleQuantizeHandler):
+        elif not qhandler.is_standalone_module():
             continue
 
         sm_qconfig_dict, sm_prepare_config_dict, sm_backend_config_dict = \
