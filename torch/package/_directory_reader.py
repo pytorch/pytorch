023b0d1445e0b1e1bb7a03c660cd62eb9d26d2a6
Sahan Paliskara <sahancpal@gmail.com>
2022-04-06 11:55:37 -0700

    [pkg] add generic ZipFile Reader/Writer (#72237)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/72237
    
    add a generic zip file reader/writer to torch.package in order to get rid of dependency on torch for non torchscript / tensor related usages of package. This also enables users to create a derived class from the zip file reader/writer classes to have their own serialization/deserialization if it's desired for performance needs.
    
    https://www.internalfb.com/intern/diff/D35423079/ was reverted due to this refactor changing the name of where most of the implementation components of PackageExporter/PackageImporter come from like ModuleActionType_ etc.
    This diff also changes the import paths where these components come from to point to the correct file compared to D35423079
    
    Test Plan: Imported from OSS
    
    Reviewed By: malfet
    
    Differential Revision: D35423079
    
    Pulled By: PaliC
    
    fbshipit-source-id: 31abc4364d5fd007911cfb67cf36ebfac5d786f4

--- torch/package/_directory_reader.py
+++ torch/package/_directory_reader.py
@@ -1,17 +1,5 @@
 import os.path
 from glob import glob
-from typing import cast
-
-import torch
-from torch.types import Storage
-
-# because get_storage_from_record returns a tensor!?
-class _HasStorage(object):
-    def __init__(self, storage):
-        self._storage = storage
-
-    def storage(self):
-        return self._storage
 
 
 class DirectoryReader(object):
@@ -19,9 +7,6 @@ class DirectoryReader(object):
     Class to allow PackageImporter to operate on unzipped packages. Methods
     copy the behavior of the internal PyTorchFileReader class (which is used for
     accessing packages in all other cases).
-
-    N.B.: ScriptObjects are not depickleable or accessible via this DirectoryReader
-    class due to ScriptObjects requiring an actual PyTorchFileReader instance.
     """
 
     def __init__(self, directory):
@@ -31,12 +16,6 @@ class DirectoryReader(object):
         filename = f"{self.directory}/{name}"
         with open(filename, "rb") as f:
             return f.read()
-
-    def get_storage_from_record(self, name, numel, dtype):
-        filename = f"{self.directory}/{name}"
-        nbytes = torch._utils._element_size(dtype) * numel
-        storage = cast(Storage, torch._UntypedStorage)
-        return _HasStorage(storage.from_file(filename=filename, nbytes=nbytes))
 
     def has_record(self, path):
         full_path = os.path.join(self.directory, path)
@@ -50,3 +29,6 @@ class DirectoryReader(object):
             if not os.path.isdir(filename):
                 files.append(filename[len(self.directory) + 1 :])
         return files
+
+    def close(self):
+        pass
