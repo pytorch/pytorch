ea3beb7cfc8b12730c57a3f25bd3fc7082688357
Scott Wolchok <swolchok@fb.com>
2022-04-07 20:36:56 -0700

    [PyTorch] Add NestedTensor support functions for transformers
    Here are the NestedTensor kernels we'll need for the improved transformer implementation.
    
    Differential Revision: [D35409275](https://our.internmc.facebook.com/intern/diff/D35409275/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D35409275/)!
    
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/nested/cuda/NestedTensorTransformerFunctions.cu
@@ -0,0 +1,327 @@
+#include <type_traits>
+
+#include <ATen/ATen.h>
+#include <ATen/AccumulateType.h>
+#include <ATen/Dispatch.h>
+#include <ATen/NestedTensorImpl.h>
+#include <ATen/TensorAccessor.h>
+
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/detail/KernelUtils.h>
+#include <ATen/cuda/detail/IndexUtils.cuh>
+#include <ATen/native/cuda/Loops.cuh>
+#include <ATen/native/cuda/MemoryAccess.cuh>
+#include <ATen/native/cuda/PersistentSoftmax.cuh>
+#include <ATen/native/cuda/block_reduce.cuh>
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/_nested_from_padded.h>
+#endif
+
+#include <c10/cuda/CUDAMathCompat.h>
+#include <c10/cuda/CUDAStream.h>
+
+#include <ATen/native/nested/NestedTensorTransformerFunctions.h>
+namespace at {
+namespace native {
+namespace {
+int64_t padded_tensor_numel(const Tensor& sizes) {
+  const auto sizes_num_rows = sizes.sizes()[0];
+  const auto sizes_row_length = sizes.sizes()[1];
+  const auto* sizes_data = sizes.data_ptr<int64_t>();
+  int64_t numel = 0;
+  for (const auto row_num : c10::irange(sizes_num_rows)) {
+    const auto* row_ptr = sizes_data + row_num * sizes_row_length;
+    int64_t prod = 1;
+    for (const auto idx : c10::irange(sizes_row_length)) {
+      prod *= row_ptr[idx];
+    }
+    numel += prod;
+  }
+  return numel;
+}
+} // namespace
+Tensor nested_from_padded_cuda(
+    const Tensor& padded,
+    const Tensor& sizes,
+    bool do_transform_0213) {
+  if (padded.dim() > 1 && padded.dim() < 5) {
+    TORCH_CHECK(
+        (padded.dim() == 4 && do_transform_0213) ||
+            (padded.dim() == 3 && !do_transform_0213),
+        "padded tensor size error");
+    Tensor target_offsets =
+        NestedTensor_batch_offsets_from_size_tensor(sizes, 0);
+    Tensor padded_sizes_tensor = at::tensor(padded.sizes());
+    Tensor output = at::empty({padded_tensor_numel(sizes)}, padded.options());
+    Tensor target_size_sizes = sizes.reshape(-1);
+
+    Tensor metadata =
+        at::cat({target_size_sizes, padded_sizes_tensor, target_offsets});
+    metadata = metadata.to(at::Device(kCUDA), kInt, true, true);
+
+    auto output_size_ptr = metadata.data_ptr<int>();
+    auto input_size_ptr = output_size_ptr + target_size_sizes.numel();
+    auto offsets_ptr = input_size_ptr + padded_sizes_tensor.numel();
+
+    if (padded.dtype() == kFloat) {
+      if (do_transform_0213) {
+        remove_padding_transform0213_kernelLauncher(
+            padded.data_ptr<float>(),
+            output.data_ptr<float>(),
+            offsets_ptr,
+            input_size_ptr,
+            output_size_ptr,
+            padded.dim() - 2,
+            padded.sizes()[0]);
+      } else {
+        remove_padding_kernelLauncher(
+            padded.data_ptr<float>(),
+            output.data_ptr<float>(),
+            offsets_ptr,
+            input_size_ptr,
+            output_size_ptr,
+            padded.dim() - 1,
+            padded.sizes()[0]);
+      }
+    } else if (padded.dtype() == kHalf) {
+      if (do_transform_0213) {
+        remove_padding_transform0213_kernelLauncher(
+            padded.data_ptr<c10::Half>(),
+            output.data_ptr<c10::Half>(),
+            offsets_ptr,
+            input_size_ptr,
+            output_size_ptr,
+            padded.dim() - 2,
+            padded.sizes()[0]);
+      } else {
+        remove_padding_kernelLauncher(
+            padded.data_ptr<c10::Half>(),
+            output.data_ptr<c10::Half>(),
+            offsets_ptr,
+            input_size_ptr,
+            output_size_ptr,
+            padded.dim() - 1,
+            padded.sizes()[0]);
+      }
+    } else {
+      AT_ERROR("Only support fp32/fp16 for padded input");
+    }
+    return at::detail::make_tensor<NestedTensorImpl>(std::move(output), sizes);
+  } else {
+    return at::native::nested_from_padded_generic(padded, sizes);
+  }
+}
+
+template <typename T>
+__global__ void remove_padding_transform0213_2(
+    const T* input,
+    T* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size) {
+  const int batch_id = blockIdx.x;
+  const int grid_id = blockIdx.y;
+  const int tid = threadIdx.x + grid_id * 256;
+  const int grainsize = 16 * 256;
+  const int offset = offsets[batch_id];
+  const int* sizes_i = output_sizes + batch_id * output_dim;
+  const int numel_i = sizes_i[0] * sizes_i[1];
+  int input_offset =
+      batch_id * input_sizes[1] * input_sizes[2] * input_sizes[3];
+  for (int ii = 0; ii < (numel_i / grainsize); ii++) {
+    const int i = ii * grainsize + tid;
+    const int i2 = i / sizes_i[1];
+    const int i13 = i % sizes_i[1];
+    const int i1 = i13 / (sizes_i[1] / input_sizes[1]);
+    const int i3 = i13 % (sizes_i[1] / input_sizes[1]);
+
+    output[offset + i] = input
+        [input_offset + i1 * input_sizes[2] * input_sizes[3] +
+         i2 * input_sizes[3] + i3];
+  }
+  const int i = (numel_i / grainsize) * grainsize + tid;
+  if (i < numel_i) {
+    const int i2 = i / sizes_i[1];
+    const int i13 = i % sizes_i[1];
+    const int i1 = i13 / (sizes_i[1] / input_sizes[1]);
+    const int i3 = i13 % (sizes_i[1] / input_sizes[1]);
+    output[offset + i] = input
+        [input_offset + i1 * input_sizes[2] * input_sizes[3] +
+         i2 * input_sizes[3] + i3];
+  }
+}
+
+template <typename T>
+__global__ void remove_padding_2(
+    const T* input,
+    T* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size) {
+  const int batch_id = blockIdx.x;
+  const int grid_id = blockIdx.y;
+  const int tid = threadIdx.x + grid_id * 256;
+  const int grainsize = 16 * 256;
+  const int offset = offsets[batch_id];
+  const int* sizes_i = output_sizes + batch_id * output_dim;
+  const int numel_i = sizes_i[0] * sizes_i[1];
+  int input_offset = batch_id * input_sizes[1] * input_sizes[2];
+  for (int ii = 0; ii < (numel_i / grainsize); ii++) {
+    const int i = ii * grainsize + tid;
+    const int i0 = i / sizes_i[1];
+    const int i1 = i % sizes_i[1];
+    const int i0_offset = i0 * input_sizes[2];
+    output[offset + i] = input[input_offset + i0_offset + i1];
+  }
+  const int i = (numel_i / grainsize) * grainsize + tid;
+  if (i < numel_i) {
+    const int i0 = i / sizes_i[1];
+    const int i1 = i % sizes_i[1];
+    const int i0_offset = i0 * input_sizes[2];
+    output[offset + i] = input[input_offset + i0_offset + i1];
+  }
+}
+
+template <typename T>
+__global__ void remove_padding(
+    const T* input,
+    T* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size) {
+  const int batch_id = blockIdx.x;
+  const int grid_id = blockIdx.y;
+  const int tid = threadIdx.x + grid_id * 256;
+  const int grainsize = 16 * 256;
+  const int offset = offsets[batch_id];
+  const int* sizes_i = output_sizes + batch_id * output_dim;
+  const int numel_i = sizes_i[0] * sizes_i[1] * sizes_i[2];
+  int input_offset =
+      batch_id * input_sizes[1] * input_sizes[2] * input_sizes[3];
+  for (int ii = 0; ii < (numel_i / grainsize); ii++) {
+    const int i = ii * grainsize + tid;
+    const int i0 = i / (sizes_i[1] * sizes_i[2]);
+    const int i1 = (i % (sizes_i[1] * sizes_i[2])) / sizes_i[2];
+    const int i2 = i % sizes_i[2];
+    const int i0_offset = i0 * input_sizes[2] * input_sizes[3];
+    const int i1_offset = i1 * input_sizes[3];
+    output[offset + i] = input[input_offset + i0_offset + i1_offset + i2];
+  }
+  const int i = (numel_i / grainsize) * grainsize + tid;
+  if (i < numel_i) {
+    const int i0 = i / (sizes_i[1] * sizes_i[2]);
+    const int i1 = (i % (sizes_i[1] * sizes_i[2])) / sizes_i[2];
+    const int i2 = i % sizes_i[2];
+    const int i0_offset = i0 * input_sizes[2] * input_sizes[3];
+    const int i1_offset = i1 * input_sizes[3];
+    output[offset + i] = input[input_offset + i0_offset + i1_offset + i2];
+  }
+}
+
+template <typename T>
+void remove_padding_kernelLauncher(
+    const T* input,
+    T* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size) {
+  dim3 grid;
+  grid.x = batch_size;
+  grid.y = 16;
+  at::cuda::CUDAStream stream = at::cuda::getDefaultCUDAStream();
+  if (output_dim == 2) {
+    remove_padding_2<T><<<grid, 256, 0, stream>>>(
+        input,
+        output,
+        offsets,
+        input_sizes,
+        output_sizes,
+        output_dim,
+        batch_size);
+  } else {
+    remove_padding<T><<<grid, 256, 0, stream>>>(
+        input,
+        output,
+        offsets,
+        input_sizes,
+        output_sizes,
+        output_dim,
+        batch_size);
+  }
+}
+
+template <typename T>
+void remove_padding_transform0213_kernelLauncher(
+    const T* input,
+    T* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size) {
+  dim3 grid;
+  grid.x = batch_size;
+  grid.y = 16;
+  at::cuda::CUDAStream stream = at::cuda::getDefaultCUDAStream();
+  TORCH_CHECK(
+      output_dim == 2,
+      "remove padding transform0213 only support output dim == 2");
+
+  remove_padding_transform0213_2<T><<<grid, 256, 0, stream>>>(
+      input,
+      output,
+      offsets,
+      input_sizes,
+      output_sizes,
+      output_dim,
+      batch_size);
+}
+
+template void remove_padding_kernelLauncher<float>(
+    const float* input,
+    float* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size);
+
+template void remove_padding_kernelLauncher<c10::Half>(
+    const c10::Half* input,
+    c10::Half* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size);
+
+template void remove_padding_transform0213_kernelLauncher<float>(
+    const float* input,
+    float* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size);
+
+template void remove_padding_transform0213_kernelLauncher<c10::Half>(
+    const c10::Half* input,
+    c10::Half* output,
+    const int* offsets,
+    const int* input_sizes,
+    const int* output_sizes,
+    int output_dim,
+    const int batch_size);
+} // namespace native
+} // namespace at
