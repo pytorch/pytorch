156d2e44c4db1afd2497088121ad7947e194110a
Jerry Zhang <jerryzh@fb.com>
2022-04-09 09:37:28 -0700

    [qunat][fx][bc-breaking] Remove "additional_quant_pattern" key from prepare_custom_config_dict (#75386)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75386
    
    This is now replaced with backend_config_dict, we don't want to expose the implementation detail to
    users. We'll have docs for backend_config_dict later
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Imported from OSS
    
    Reviewed By: ezyang
    
    Differential Revision: D35451957
    
    fbshipit-source-id: 52ebb5fb20cd96c1f21410b07c3d0c448c58cdba
    (cherry picked from commit ccb38026f14644f9eb43335b7a7de5568c556394)

--- torch/ao/quantization/quantize_fx.py
+++ torch/ao/quantization/quantize_fx.py
@@ -449,12 +449,6 @@ def prepare_fx(
                torch.nn.intrinsic.ConvBn2d: torch.nn.qat.ConvBn2d
             },
 
-            # Additional quantization patterns
-            "additional_quant_pattern": {
-               torch.nn.Conv2d: ConvReluQuantizeHandler,
-               (torch.nn.ReLU, torch.nn.Conv2d): ConvReluQuantizeHandler,
-            }
-
             # By default, inputs and outputs of the graph are assumed to be in
             # fp32. Providing `input_quantized_idxs` will set the inputs with the
             # corresponding indices to be quantized. Providing
