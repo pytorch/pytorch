88be793ac453b8c895d4b097c6282f6244ecf77e
lvhaijun01 <lvhaijun01@baidu.com>
2022-04-11 14:16:40 +0800

    refactoring model average code

--- torch/distributed/algorithms/model_averaging/utils.py
+++ torch/distributed/algorithms/model_averaging/utils.py
@@ -37,30 +37,29 @@ def average_parameters(
 
 def get_params_to_average(params: Union[Iterable[torch.nn.Parameter], Iterable[Dict[str, torch.nn.Parameter]]]):
     """
-    get params to average
+    Returns a list of parameters that need to average.
     Args:
         params: The parameters of a model or parameter groups of an optimizer.
     """
-    filter_params = []
+    filtered_params = []
     for param in params:
         if isinstance(param, torch.nn.Parameter):
             # model.parameters() input
             param_data = param
             if param_data.grad is not None:
-                filter_params.append(param_data)
+                filtered_params.append(param_data)
         elif isinstance(param, dict):
             # optimizer.param_groups input
             for param_data in param["params"]:
                 if param_data.grad is not None:
-                    filter_params.append(param_data)
+                    filtered_params.append(param_data)
         else:
             raise NotImplementedError(f"Parameter input of type {type(param)} is not supported")
-    return filter_params
+    return filtered_params
 
 
 def average_parameters_or_parameter_groups(params: Union[Iterable[torch.nn.Parameter], Iterable[Dict[str, torch.nn.Parameter]]], process_group: dist.ProcessGroup):
     """
-    average parameters or parameter_groups
+    Averages parameters of a model or parameter groups of an optimizer.
     """
-    filter_params = get_params_to_average(params)
-    average_parameters(filter_params, process_group)
+    average_parameters(get_params_to_average(params), process_group)
