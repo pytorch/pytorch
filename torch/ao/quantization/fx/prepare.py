55d479aca5e959c5f2fe3089e162db710bb7632d
Jerry Zhang <jerryzh@fb.com>
2022-04-10 21:57:12 -0700

    [qunat][fx][bc-breaking] Remove "additional_qat_mapping" key from prepare_custom_config_dict (#75387)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75387
    
    This is now replaced with backend_config_dict, we don't want to expose the implementation detail to
    users. We'll have docs for backend_config_dict later
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Imported from OSS
    
    Reviewed By: vkuzo
    
    Differential Revision: D35451955
    
    fbshipit-source-id: 77ede61f1d8f169dc1e1e6d847244ba99a97ab76
    (cherry picked from commit 953576259fdc8827437acb6f5d04e584e37a7d64)

--- torch/ao/quantization/fx/prepare.py
+++ torch/ao/quantization/fx/prepare.py
@@ -78,7 +78,6 @@ from torch.ao.quantization.quantize import (
 )
 
 from ..utils import (
-    get_combined_dict,
     get_qconfig_dtypes,
     get_swapped_custom_module_class,
     activation_is_statically_quantized,
@@ -1421,18 +1420,15 @@ def prepare(
     propagate_qconfig_(model, flattened_qconfig_dict, prepare_custom_config_dict)
 
     if is_qat:
-        additional_qat_module_mapping = prepare_custom_config_dict.get(
-            "additional_qat_module_mapping", {})
         # this path will be deprecated after we fully migrate the convert path
         # of fbgemm/qnnpack to use the reference path, it will stay
         # here for a few months
         if backend_config_dict is None:
-            module_to_qat_module = get_combined_dict(
-                get_default_qat_module_mappings(), additional_qat_module_mapping)
+            module_to_qat_module = get_default_qat_module_mappings()
         else:
             module_to_qat_module = get_module_to_qat_module(backend_config_dict)
         qat_swap_modules(model, module_to_qat_module)
-        qconfig_dict = update_qconfig_for_qat(qconfig_dict, additional_qat_module_mapping)
+        qconfig_dict = update_qconfig_for_qat(qconfig_dict, {})
 
     # mapping from fully qualified module name to module instance
     # for example,
