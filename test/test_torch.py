9c2638e3a3fef3606d6a192f736262bbda97c103
Beilei Zheng <beilei.zheng@intel.com>
2022-04-13 13:57:41 -0700

    Add BFloat16 support for multinomial and poisson on CPU (#63215)
    Summary:
    Add BFloat16 support for multinomial and poisson on CPU
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63215
    
    Reviewed By: zou3519
    
    Differential Revision: D35604536
    
    Pulled By: frank-wei
    
    fbshipit-source-id: 817869cb7b9b741e86ceb181094c395c3a5ee106

--- test/test_torch.py
+++ test/test_torch.py
@@ -5091,6 +5091,31 @@ else:
 
         self._test_where_scalar_template(device, dtype, checkRaises)
 
+    @dtypesIfCUDA(torch.float, torch.double, torch.half)
+    @dtypesIfCPU(torch.float, torch.double, torch.bfloat16)
+    @dtypes(torch.float, torch.double)
+    def test_multinomial_cpu(self, device, dtype):
+        def make_prob_dist(shape, is_contiguous):
+            if is_contiguous:
+                if dtype == torch.half or dtype == torch.bfloat16:
+                    return torch.zeros(shape, device=device).uniform_().to(dtype=dtype)
+                return torch.zeros(shape, device=device, dtype=dtype).uniform_()
+            elif len(shape) == 1:
+                if dtype == torch.half or dtype == torch.bfloat16:
+                    return torch.zeros((shape + [5]), device=device).uniform_().to(dtype=dtype)[:, 2]
+                return torch.zeros((shape + [5]), device=device, dtype=dtype).uniform_()[:, 2]
+            else:
+                # num dim = 2
+                new_shape = [2, shape[1], 7, 1, shape[0], 1, 10]
+                if dtype == torch.half or dtype == torch.bfloat16:
+                    prob_dist = torch.zeros(new_shape, device=device).uniform_().to(dtype=dtype)
+                else:
+                    prob_dist = torch.zeros(new_shape, device=device, dtype=dtype).uniform_()
+                prob_dist = prob_dist.transpose(1, 4)
+                prob_dist = prob_dist[1, :, 5, 0, :, 0, 4]
+                assert not prob_dist.is_contiguous()  # sanity check
+                return prob_dist
+
     # FIXME: move to elementwise ternary test suite
     @skipCUDAVersionIn([(11, 2)])  # test fails for 11.2, see https://github.com/pytorch/pytorch/issues/51980
     @dtypes(*all_types_and_complex_and(torch.half, torch.bfloat16))
