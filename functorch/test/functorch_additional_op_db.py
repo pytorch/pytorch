45e8381c9bebdd7f71e6a9e153bec80016e5e4fe
Richard Zou <zou3519@gmail.com>
2022-04-15 08:22:20 -0700

    [functorch] fix ci

--- functorch/test/functorch_additional_op_db.py
+++ functorch/test/functorch_additional_op_db.py
@@ -16,55 +16,6 @@ from torch.testing._internal.common_methods_invocations import OpInfo, SampleInp
 
 additional_op_db = []
 
-# https://github.com/pytorch/pytorch/pull/61971
-
-
-def sample_inputs_linear(has_bias, self, device, dtype, requires_grad):
-    features_options = [[3, 4], [128, 128]]
-    batch_options = [
-        [],  # no batch
-        [64],
-        [5, 7],
-    ]
-
-    sample_inputs = []
-    for (in_feat, out_feat), batch_shape in itertools.product(features_options, batch_options):
-        input_tensor = make_tensor(batch_shape + [in_feat], device=device,
-                                   dtype=dtype, requires_grad=requires_grad,
-                                   low=-2, high=2)
-        weight = make_tensor([out_feat, in_feat], device=device,
-                             dtype=dtype, requires_grad=requires_grad,
-                             low=-2, high=2)
-        if not has_bias:
-            sample_inputs.append(SampleInput(input_tensor, args=(weight,)))
-            continue
-
-        bias = make_tensor([out_feat], device=device,
-                           dtype=dtype, requires_grad=requires_grad,
-                           low=-2, high=2)
-        sample_inputs.append(SampleInput(input_tensor, args=(weight, bias)))
-    return sample_inputs
-
-
-additional_op_db.extend([
-    OpInfo('nn.functional.linear',
-           aten_name='linear',
-           variant_test_name='with_bias',
-           supports_autograd=True,
-           dtypes=all_types_and_complex_and(torch.half, torch.bfloat16),
-           dtypesIfCUDA=floating_and_complex_types_and(torch.half, torch.bfloat16),
-           sample_inputs_func=partial(sample_inputs_linear, False),
-           supports_out=False),
-    OpInfo('nn.functional.linear',
-           aten_name='linear',
-           variant_test_name='no_bias',
-           supports_autograd=True,
-           dtypes=all_types_and_complex_and(torch.half, torch.bfloat16),
-           dtypesIfCUDA=floating_and_complex_types_and(torch.half, torch.bfloat16),
-           sample_inputs_func=partial(sample_inputs_linear, True),
-           supports_out=False),
-])
-
 # https://github.com/pytorch/pytorch/pull/61068
 
 
@@ -86,6 +37,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='no_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, False),
            dtypes=floating_types(),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
@@ -94,6 +46,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -102,6 +55,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 2))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -110,6 +64,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_no_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, False, extra_args=((2, 2))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -118,6 +73,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_padding_with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 2), (1, 1))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -126,6 +82,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_padding_no_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, False, extra_args=((2, 2), (1, 1))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -134,6 +91,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='strided_padding_dilation_with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 2), (1, 1), (2, 2))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -142,6 +100,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='strided_padding_dilation_no_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 2), (1, 1), (2, 2))),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -150,6 +109,7 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_groups_with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 3), 0, 1, 2), groups=2),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
@@ -158,47 +118,12 @@ additional_op_db.extend([
            aten_name="conv2d",
            variant_test_name='stride_depthwise_with_bias',
            supports_autograd=True,
+           supports_forward_ad=True,
            sample_inputs_func=partial(sample_inputs_conv2d, True, extra_args=((2, 3), 0, 1, 6), groups=6),
            dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
            dtypes=floating_types(),
            supports_out=False),
 ])
-
-
-def sample_inputs_cross_entropy(self, device, dtype, requires_grad, reduction):
-    N = 2
-    C = 10
-    inp = make_tensor((2, C), device=device, dtype=dtype,
-                      requires_grad=requires_grad, low=-1, high=1)
-    target = torch.randint(0, C, (N,), device=device)
-    inp4d = make_tensor((2, C, 4, 5), device=device, dtype=dtype,
-                        requires_grad=requires_grad, low=-1, high=1)
-    target4d = torch.randint(0, C, (N, 4, 5), device=device)
-    weight = make_tensor((C,), device=device, dtype=dtype,
-                         low=0.5, high=1)
-    sample_inputs = [
-        SampleInput(inp, args=(target,), kwargs={'reduction': reduction}),
-        SampleInput(inp, args=(target,), kwargs={'ignore_index': 1, 'reduction': reduction}),
-        SampleInput(inp, args=(target, weight), kwargs={'ignore_index': 1, 'reduction': reduction}),
-    ]
-    sample_inputs.extend([
-        SampleInput(inp4d, args=(target4d,), kwargs={'reduction': reduction}),
-        SampleInput(inp4d, args=(target4d,), kwargs={'ignore_index': 1, 'reduction': reduction}),
-        SampleInput(inp4d, args=(target4d, weight), kwargs={'ignore_index': 1, 'reduction': reduction}),
-    ])
-    return sample_inputs
-
-
-for reduction in ['mean', 'sum', 'none']:
-    additional_op_db.append(
-        OpInfo('nn.functional.cross_entropy',
-               aten_name="cross_entropy",
-               variant_test_name=reduction,
-               supports_autograd=True,
-               sample_inputs_func=partial(sample_inputs_cross_entropy, reduction=reduction),
-               dtypes=floating_types(),
-               dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
-               supports_out=True))
 
 
 # TODO: PyTorch core has a check for if requires_grad=True or not.
