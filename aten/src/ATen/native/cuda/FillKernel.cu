f3a0b68cf5f940ac6dcb47efbdb8f56f63a0bb30
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-04-16 12:50:43 +0530

    jiterate `fill`

--- aten/src/ATen/native/cuda/FillKernel.cu
+++ aten/src/ATen/native/cuda/FillKernel.cu
@@ -1,6 +1,7 @@
 #define TORCH_ASSERT_NO_OPERATORS
 #include <ATen/Dispatch.h>
 #include <ATen/native/cuda/Loops.cuh>
+#include <ATen/native/cuda/JitLoops.cuh>
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/Fill.h>
@@ -18,10 +19,39 @@ struct FillFunctor {
     scalar_t value;
 };
 
+const char fill_name[] = "fill_kernel";
 void fill_kernel_cuda(TensorIterator& iter, const Scalar& value) {
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(at::ScalarType::Bool, at::ScalarType::Half, at::ScalarType::BFloat16, iter.dtype(), "fill_cuda", [&]() {
+  auto dtype = iter.dtype();
+  if(at::isComplexType(dtype)) {
+#if AT_USE_JITERATOR()
+  static const auto fill_string = jiterator_stringify(
+      template<typename scalar_t>
+      struct FillFunctor {
+        FillFunctor(scalar_t v): value(v) {}
+        __device__ __foceinline__ scalar_t operator() () const {
+          return value;
+        }
+        private:
+          scalar_t value;
+      };
+  ); // fill_string
+  AT_DISPATCH_COMPLEX_TYPES(dtype, "fill_cuda", [&]() {
+      jitted_gpu_kernel<
+        /*name=*/ fill_name,
+        /*return_dtype=*/ scalar_t,
+        /*common_dtype=*/ scalar_t,
+        /*arity=*/ 1>(iter, fill_string);
+  });
+#else
+  AT_DISPATCH_COMPLEX_TYPES(dtype, "fill_cuda", [&]() {
+      gpu_kernel(iter, FillFunctor<scalar_t>(value.to<scalar_t>()));
+  });
+#endif
+  } else {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(at::ScalarType::Bool, at::ScalarType::Half, at::ScalarType::BFloat16, dtype, "fill_cuda", [&]() {
     gpu_kernel(iter, FillFunctor<scalar_t>(value.to<scalar_t>()));
   });
+  }
 }
 
 REGISTER_DISPATCH(fill_stub, &fill_kernel_cuda);
