03931faa79d41ba8316fe3adfbf2043614cfc2ef
Pruthvi Madugundu <pmagundu@amd.com>
2022-04-18 22:16:32 -0700

    Updated handling of PYTORCH_ROCM_ARCH while building base docker

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -329,6 +329,7 @@ docker build \
        --build-arg "NINJA_VERSION=${NINJA_VERSION:-}" \
        --build-arg "KATEX=${KATEX:-}" \
        --build-arg "ROCM_VERSION=${ROCM_VERSION:-}" \
+       --build-arg "PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH:-gfx900;gfx906;gfx908}" \
        -f $(dirname ${DOCKERFILE})/Dockerfile \
        -t "$tmp_tag" \
        "$@" \
