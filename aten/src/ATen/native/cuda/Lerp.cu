cb9dcf9334be257cd9808951bc5c71db6f2d9486
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-04-19 17:22:48 +0530

    try to fix failures

--- aten/src/ATen/native/cuda/Lerp.cu
+++ aten/src/ATen/native/cuda/Lerp.cu
@@ -15,8 +15,8 @@ void lerp_tensor_kernel(at::TensorIteratorBase& iter) {
   if(at::isComplexType(dtype)) {
 #if AT_USE_JITERATOR()
   static const auto lerp_tensor_string = jiterator_stringify(
+      template <typename T>
       T lerp_tensor_kernel(scalar_t self_val, scalar_t end_val, scalar_t weight_val) {
-        template <typename T>
         return (std::abs(weight_val) < 0.5)
             ? self_val + weight_val * (end_val - self_val)
             : end_val -
@@ -79,8 +79,8 @@ void lerp_scalar_kernel(at::TensorIteratorBase& iter, const c10::Scalar& weight)
             ? self_val + weight_val * (end_val - self_val)
             : end_val -
                 (end_val - self_val) * (static_cast<scalar_t>(1) - weight_val);
-      } // lerp_scalar_string
-  );
+      }
+  ); // lerp_scalar_string
   AT_DISPATCH_COMPLEX_TYPES(dtype, "lerp_cuda", [&] {
       jitted_gpu_kernel<
         /*name=*/ lerp_scalar_name,
