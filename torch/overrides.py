ee955b8bb9c1e352180a74351148bf1b015acd3a
Edward Z. Yang <ezyang@fb.com>
2022-04-19 19:56:43 -0700

    Cannibalize noarch CI job into crossref CI job
    crossref is a new strategy for performing tests when you want
    to run a normal PyTorch API call, separately run some variation of
    the API call (e.g., same thing but all the arguments are meta tensors)
    and then cross-reference the results to see that they are consistent.
    Any logic you add to CrossRefMode will get run on *every* PyTorch API
    call that is called in the course of PyTorch's test suite.  This can
    be a good choice for correctness testing if OpInfo testing is not
    exhaustive enough.
    
    For now, the crossref test doesn't do anything except verify that
    we can validly push a mode onto the torch function mode stack for all
    functions.
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75988
    
    Approved by: https://github.com/seemethere

--- torch/overrides.py
+++ torch/overrides.py
@@ -1330,7 +1330,7 @@ def wrap_torch_function(dispatcher: Callable):
         def wrapped(*args, **kwargs):
             relevant_args = dispatcher(*args, **kwargs)
             if has_torch_function(relevant_args):
-                return handle_torch_function(func, relevant_args, *args, **kwargs)
+                return handle_torch_function(wrapped, relevant_args, *args, **kwargs)
 
             return func(*args, **kwargs)
 
