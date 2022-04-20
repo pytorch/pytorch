021b0352068708769899a6915d93385d9fe187e9
Yukio Siraichi <yukio.siraichi@gmail.com>
2022-04-20 17:41:10 +0900

    Add include.

--- aten/src/ATen/native/cuda/TensorTopK.cpp
+++ aten/src/ATen/native/cuda/TensorTopK.cpp
@@ -10,6 +10,7 @@
 #ifndef AT_PER_OPERATOR_HEADERS
 #include <ATen/Functions.h>
 #include <ATen/NativeFunctions.h>
+#include <ATen/CUDAFunctions.h>
 #else
 #include <ATen/ops/empty_like.h>
 #include <ATen/ops/sort_cuda_dispatch.h>
