87e556c91616df87cafc56d0eac29775f1a410bf
Animesh Jain <anijain@umich.edu>
2022-04-20 00:48:01 +0000

    Monkey patch Variable module to fix FX codegen

--- torch/autograd/__init__.py
+++ torch/autograd/__init__.py
@@ -319,3 +319,6 @@ def _register_py_tensor_class_for_device(device, cls):
     if not isinstance(cls, type):
         raise RuntimeError("cls isn't a typeinfo object")
     torch._C._register_py_class_for_device(device, cls)
+
+# Fix for FX codgen
+Variable.__module__ = "torch.autograd"
