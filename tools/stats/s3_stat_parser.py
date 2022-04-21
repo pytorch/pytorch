496a2f08f998fd88d96c3e85751021608bcfcb77
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared46383.04.pnb3.facebook.com>
2022-04-21 04:01:39 -0700

    2022-04-21 postnightly release (bf730e5039e8bf1614de6ad08ac6a24a1622dcbe)

--- tools/stats/s3_stat_parser.py
+++ tools/stats/s3_stat_parser.py
@@ -10,6 +10,7 @@ from typing_extensions import Literal, TypedDict
 try:
     import boto3  # type: ignore[import]
     import botocore  # type: ignore[import]
+
     HAVE_BOTO3 = True
 except ImportError:
     HAVE_BOTO3 = False
@@ -18,10 +19,10 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
-OSSCI_METRICS_BUCKET = 'ossci-metrics'
+OSSCI_METRICS_BUCKET = "ossci-metrics"
 
 Commit = str  # 40-digit SHA-1 hex string
-Status = Optional[Literal['errored', 'failed', 'skipped']]
+Status = Optional[Literal["errored", "failed", "skipped"]]
 
 
 class CaseMeta(TypedDict):
@@ -85,8 +86,10 @@ class Version2Report(VersionedReport):
 Report = Union[Version1Report, VersionedReport]
 
 if HAVE_BOTO3:
-    S3_RESOURCE_READ_ONLY = boto3.resource("s3", config=botocore.config.Config(signature_version=botocore.UNSIGNED))
-    S3_RESOURCE = boto3.resource('s3')
+    S3_RESOURCE_READ_ONLY = boto3.resource(
+        "s3", config=botocore.config.Config(signature_version=botocore.UNSIGNED)
+    )
+    S3_RESOURCE = boto3.resource("s3")
 
 
 def get_S3_bucket_readonly(bucket_name: str) -> Any:
@@ -98,7 +101,7 @@ def get_S3_object_from_bucket(bucket_name: str, object: str) -> Any:
 
 
 def case_status(case: Version1Case) -> Status:
-    for k in {'errored', 'failed', 'skipped'}:
+    for k in {"errored", "failed", "skipped"}:
         if case[k]:  # type: ignore[misc]
             return cast(Status, k)
     return None
@@ -106,8 +109,8 @@ def case_status(case: Version1Case) -> Status:
 
 def newify_case(case: Version1Case) -> Version2Case:
     return {
-        'seconds': case['seconds'],
-        'status': case_status(case),
+        "seconds": case["seconds"],
+        "status": case_status(case),
     }
 
 
@@ -119,28 +122,28 @@ def get_cases(
     test_name: Optional[str],
 ) -> List[Version2Case]:
     cases: List[Version2Case] = []
-    if 'format_version' not in data:  # version 1 implicitly
+    if "format_version" not in data:  # version 1 implicitly
         v1report = cast(Version1Report, data)
-        suites = v1report['suites']
+        suites = v1report["suites"]
         for sname, v1suite in suites.items():
             if not suite_name or sname == suite_name:
-                for v1case in v1suite['cases']:
-                    if not test_name or v1case['name'] == test_name:
+                for v1case in v1suite["cases"]:
+                    if not test_name or v1case["name"] == test_name:
                         cases.append(newify_case(v1case))
     else:
         v_report = cast(VersionedReport, data)
-        version = v_report['format_version']
+        version = v_report["format_version"]
         if version == 2:
             v2report = cast(Version2Report, v_report)
-            for fname, v2file in v2report['files'].items():
+            for fname, v2file in v2report["files"].items():
                 if fname == filename or not filename:
-                    for sname, v2suite in v2file['suites'].items():
+                    for sname, v2suite in v2file["suites"].items():
                         if sname == suite_name or not suite_name:
-                            for cname, v2case in v2suite['cases'].items():
+                            for cname, v2case in v2suite["cases"].items():
                                 if not test_name or cname == test_name:
                                     cases.append(v2case)
         else:
-            raise RuntimeError(f'Unknown format version: {version}')
+            raise RuntimeError(f"Unknown format version: {version}")
     return cases
 
 
@@ -148,19 +151,22 @@ def _parse_master_summaries(summaries: Any, jobs: List[str]) -> Dict[str, List[R
     summary_dict = defaultdict(list)
     for summary in summaries:
         # master summary format: "test_time/{sha}/{job}/file"
-        summary_job = summary.key.split('/')[2]
+        summary_job = summary.key.split("/")[2]
         if summary_job in jobs or len(jobs) == 0:
             binary = summary.get()["Body"].read()
             string = bz2.decompress(binary).decode("utf-8")
             summary_dict[summary_job].append(json.loads(string))
     return summary_dict
 
-def _parse_pr_summaries(summaries: Any, job_prefix: str) -> Dict[str, List[Tuple[Report, str]]]:
+
+def _parse_pr_summaries(
+    summaries: Any, job_prefix: str
+) -> Dict[str, List[Tuple[Report, str]]]:
     summary_dict = defaultdict(list)
     for summary in summaries:
         # PR summary format: "pr_test_time/{pr}/{sha}/{job}/file"
-        summary_job = summary.key.split('/')[3]
-        summary_timestamp = summary.key.split('/')[4][:len("YYYY-MM-ddTHH:mm:ss")]
+        summary_job = summary.key.split("/")[3]
+        summary_timestamp = summary.key.split("/")[4][: len("YYYY-MM-ddTHH:mm:ss")]
         if not job_prefix or len(job_prefix) == 0 or summary_job.startswith(job_prefix):
             binary = summary.get()["Body"].read()
             string = bz2.decompress(binary).decode("utf-8")
@@ -171,18 +177,25 @@ def _parse_pr_summaries(summaries: Any, job_prefix: str) -> Dict[str, List[Tuple
 # Collect and decompress S3 test stats summaries into JSON.
 # data stored on S3 buckets are pathed by {sha}/{job} so we also allow
 # optional jobs filter
-def get_test_stats_summaries(*, sha: str, jobs: Optional[List[str]] = None) -> Dict[str, List[Report]]:
+def get_test_stats_summaries(
+    *, sha: str, jobs: Optional[List[str]] = None
+) -> Dict[str, List[Report]]:
     bucket = get_S3_bucket_readonly(OSSCI_METRICS_BUCKET)
     summaries = bucket.objects.filter(Prefix=f"test_time/{sha}")
     return _parse_master_summaries(summaries, jobs=list(jobs or []))
 
 
-def get_test_stats_summaries_for_job(*, sha: str, job_prefix: str) -> Dict[str, List[Report]]:
+def get_test_stats_summaries_for_job(
+    *, sha: str, job_prefix: str
+) -> Dict[str, List[Report]]:
     bucket = get_S3_bucket_readonly(OSSCI_METRICS_BUCKET)
     summaries = bucket.objects.filter(Prefix=f"test_time/{sha}/{job_prefix}")
     return _parse_master_summaries(summaries, jobs=list())
 
-def get_test_stats_summaries_for_pr(*, pr: str, job_prefix: str) -> Dict[str, List[Tuple[Report, str]]]:
+
+def get_test_stats_summaries_for_pr(
+    *, pr: str, job_prefix: str
+) -> Dict[str, List[Tuple[Report, str]]]:
     bucket = get_S3_bucket_readonly(OSSCI_METRICS_BUCKET)
     summaries = bucket.objects.filter(Prefix=f"pr_test_time/{pr}/")
     return _parse_pr_summaries(summaries, job_prefix=job_prefix)
@@ -191,35 +204,50 @@ def get_test_stats_summaries_for_pr(*, pr: str, job_prefix: str) -> Dict[str, Li
 # This function returns a list of S3 test time reports. This function can run into errors if HAVE_BOTO3 = False
 # or the S3 bucket is somehow unavailable. Even though this function goes through ten commits' reports to find a
 # non-empty report, it is still conceivable (though highly unlikely) for this function to return no reports.
-def get_previous_reports_for_branch(branch: str, ci_job_prefix: str = "") -> List[Report]:
+def get_previous_reports_for_branch(
+    branch: str, ci_job_prefix: str = ""
+) -> List[Report]:
     commit_date_ts = subprocess.check_output(
-        ['git', 'show', '-s', '--format=%ct', 'HEAD'],
-        encoding="ascii").strip()
+        ["git", "show", "-s", "--format=%ct", "HEAD"], encoding="ascii"
+    ).strip()
     commit_date = datetime.fromtimestamp(int(commit_date_ts))
     # We go a day before this current commit to avoiding pulling incomplete reports
-    day_before_commit = str(commit_date - timedelta(days=1)).split(' ')[0]
+    day_before_commit = str(commit_date - timedelta(days=1)).split(" ")[0]
     # something like git rev-list --before="2021-03-04" --max-count=10 --remotes="*origin/nightly"
     commits = subprocess.check_output(
-        ["git", "rev-list", f"--before={day_before_commit}", "--max-count=10", f"--remotes=*{branch}"],
-        encoding="ascii").splitlines()
+        [
+            "git",
+            "rev-list",
+            f"--before={day_before_commit}",
+            "--max-count=10",
+            f"--remotes=*{branch}",
+        ],
+        encoding="ascii",
+    ).splitlines()
 
     reports: List[Report] = []
     commit_index = 0
     while len(reports) == 0 and commit_index < len(commits):
         commit = commits[commit_index]
-        logger.info(f'Grabbing reports from commit: {commit}')
-        summaries = get_test_stats_summaries_for_job(sha=commit, job_prefix=ci_job_prefix)
+        logger.info(f"Grabbing reports from commit: {commit}")
+        summaries = get_test_stats_summaries_for_job(
+            sha=commit, job_prefix=ci_job_prefix
+        )
         for job_name, summary in summaries.items():
             reports.append(summary[0])
             if len(summary) > 1:
-                logger.warning(f'WARNING: Multiple summary objects found for {commit}/{job_name}')
+                logger.warning(
+                    f"WARNING: Multiple summary objects found for {commit}/{job_name}"
+                )
         commit_index += 1
     return reports
 
 
-def get_previous_reports_for_pr(pr: str, ci_job_prefix: str = "") -> List[Tuple[Report, str]]:
+def get_previous_reports_for_pr(
+    pr: str, ci_job_prefix: str = ""
+) -> List[Tuple[Report, str]]:
     reports: List[Tuple[Report, str]] = []
-    logger.info(f'Grabbing reports from PR: {[pr]}')
+    logger.info(f"Grabbing reports from PR: {[pr]}")
     summaries = get_test_stats_summaries_for_pr(pr=pr, job_prefix=ci_job_prefix)
     for _, summary in summaries.items():
         reports.extend(summary)
