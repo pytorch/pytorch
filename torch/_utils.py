03e26a10eec55ade281720a5b1b18f42f647fe17
Feng Yuan <feng1.yuan@intel.com>
2022-04-21 17:17:37 +0800

    hook XPU device in _get_available_device_type
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>

--- torch/_utils.py
+++ torch/_utils.py
@@ -478,6 +478,8 @@ class ExceptionWrapper(object):
 def _get_available_device_type():
     if torch.cuda.is_available():
         return "cuda"
+    if hasattr(torch, "xpu") and torch.xpu.is_available():
+        return "xpu"
     # add more available device types here
     return None
 
@@ -486,6 +488,8 @@ def _get_device_attr(get_member):
     device_type = _get_available_device_type()
     if device_type and device_type.lower() == "cuda":
         return get_member(torch.cuda)
+    if device_type and device_type.lower() == "xpu":
+        return get_member(torch.xpu)
     # add more available device types here
     return None
 
