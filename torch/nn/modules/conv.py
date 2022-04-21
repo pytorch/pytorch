43b9da237e73547604de5cd9b97418809738b64e
Joel Benjamin Schlosser <jbschlosser@fb.com>
2022-04-21 17:42:57 -0400

    Update on "Fix to support no-batch-dim inputs in ConvTransposeNd._output_padding"
    Fixes #75889
    
    [ghstack-poisoned]

--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -616,17 +616,17 @@ class _ConvTransposeNd(_ConvNd):
         else:
             has_batch_dim = input.dim() == num_spatial_dims + 2
             num_non_spatial_dims = 2 if has_batch_dim else 1
-            k = num_spatial_dims
-            if len(output_size) == k + num_non_spatial_dims:
+            if len(output_size) == num_non_spatial_dims + num_spatial_dims:
                 output_size = output_size[num_non_spatial_dims:]
-            if len(output_size) != k:
+            if len(output_size) != num_spatial_dims:
                 raise ValueError(
                     "ConvTranspose{}D: for {}D input, output_size must have {} or {} elements (got {})"
-                    .format(num_spatial_dims, input.dim(), k, k + num_non_spatial_dims, len(output_size)))
+                    .format(num_spatial_dims, input.dim(), num_spatial_dims,
+                            num_non_spatial_dims + num_spatial_dims, len(output_size)))
 
             min_sizes = torch.jit.annotate(List[int], [])
             max_sizes = torch.jit.annotate(List[int], [])
-            for d in range(k):
+            for d in range(num_spatial_dims):
                 dim_size = ((input.size(d + num_non_spatial_dims) - 1) * stride[d] -
                             2 * padding[d] +
                             (dilation[d] if dilation is not None else 1) * (kernel_size[d] - 1) + 1)
@@ -644,7 +644,7 @@ class _ConvTransposeNd(_ConvNd):
                             output_size, min_sizes, max_sizes, input.size()[2:]))
 
             res = torch.jit.annotate(List[int], [])
-            for d in range(k):
+            for d in range(num_spatial_dims):
                 res.append(output_size[d] - min_sizes[d])
 
             ret = res
