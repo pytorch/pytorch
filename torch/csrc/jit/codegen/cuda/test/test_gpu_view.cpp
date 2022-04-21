faaff11d00edbf96744a0e662e34117e418e620e
Ryan Spring <rdspring1@gmail.com>
2022-04-21 12:02:00 -0700

    Add FusionViewConcreteDomain tests (#1609)

--- torch/csrc/jit/codegen/cuda/test/test_gpu_view.cpp
+++ torch/csrc/jit/codegen/cuda/test/test_gpu_view.cpp
@@ -292,9 +292,8 @@ TEST_F(NVFuserTest, FusionViewReductionShmoo_CUDA) {
   std::vector<view_example> view_before_examples = {
       {{19, 12, 7, 99}, {19, 3, 2772}},
       {{1, 19, 1, 12, 7, 1, 99}, {1, 19, 1, 3, 2772}},
-      // Incorrect Result - Broadcast Issue - Pointwise
-      // {{3, 17, 80, 1}, {51, 2, 4, 1, 10}},
-      // {{3, 17, 80, 1, 9}, {51, 2, 4, 1, 10, 9}},
+      {{3, 17, 80, 1}, {51, 2, 4, 1, 10}},
+      {{3, 17, 80, 1, 9}, {51, 2, 4, 1, 10, 9}},
       {{2, 3, 4, 5}, {1, 6, 1, 2, 2, 5, 1}},
       {{22, 22, 2}, {22, 11, 1, 1, 4}},
       {{37, 9, 7, 6, 10}, {333, 2, 2, 3, 35}},
@@ -388,19 +387,19 @@ TEST_F(NVFuserTest, FusionViewPersistentShmoo_CUDA) {
   std::vector<view_example> view_examples = {
       {{19, 12, 7, 99}, {19, 3, 2772}},
       {{1, 19, 1, 12, 7, 1, 99}, {1, 19, 1, 3, 2772}},
-      // Incorrect Result - Broadcast Issue - Pointwise
-      // {{3, 17, 80, 1}, {51, 2, 4, 1, 10}},
-      // {{3, 17, 80, 1, 9}, {51, 2, 4, 1, 10, 9}},
+      {{3, 17, 80, 1}, {51, 2, 4, 1, 10}},
+      {{3, 17, 80, 1, 9}, {51, 2, 4, 1, 10, 9}},
       {{2, 3, 4, 5}, {1, 6, 1, 2, 2, 5, 1}},
       {{22, 22, 2}, {22, 11, 1, 1, 4}},
       {{37, 9, 7, 6, 10}, {333, 2, 2, 3, 35}},
       {{1, 1, 333, 1}, {1, 1, 333, 1}},
       {{8, 1, 1, 8, 1, 8}, {8, 2, 4, 1, 8}},
       {{1, 333, 1}, {1, 37, 9, 1}},
-      {{1, 333}, {1, 1, 1, 111, 1, 3}},
+      // TODO Validation Error - Absolute Tolerance
+      // {{1, 333}, {1, 1, 1, 111, 1, 3}},
       {{22, 1, 22, 1}, {484}},
       {{1, 333, 1}, {333}},
-      // Incorrect Result - Broadcast Issue - Reduction
+      // TODO Incorrect Result - Broadcast Issue - Reduction
       {{1, 27454, 1, 2}, {1, 7844, 1, 7}},
       {{1, 7844, 1, 7}, {1, 27454, 2}}};
 
@@ -409,11 +408,10 @@ TEST_F(NVFuserTest, FusionViewPersistentShmoo_CUDA) {
         e.first, e.second, true /* view_before_persistent */);
   }
 
-  // Disabled: How to select post-view concrete ID?
-  // for (auto e : view_examples) {
-  // persistentViewAddFusion(e.first, e.second, false /* view_before_persistent
-  // */);
-  // }
+  for (auto e : view_examples) {
+    persistentViewAddFusion(
+      e.first, e.second, false /* view_before_persistent */);
+  }
 }
 
 void addViewGeluFusion(
@@ -623,6 +621,230 @@ void geluViewBinaryAddFusion(
 
 TEST_F(NVFuserTest, FusionViewBinary_CUDA) {
   geluViewBinaryAddFusion({27454, 2}, {54908}, {7844, 7});
+}
+
+// Repro of issue #1493
+TEST_F(NVFuserTest, FusionViewConcreteDomain_CUDA) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+
+  auto tv0 = makeSymbolicTensor(2);
+  fusion.addInput(tv0);
+  auto tv1 = makeContigTensor(2);
+  fusion.addInput(tv1);
+
+  auto tv2 = view(tv0, {2, 3}, {6});
+  auto tv3 = add(tv2, IrBuilder::create<Double>(1));
+  auto tv4 = broadcast(tv3, {true, false});
+  auto tv5 = add(tv4, tv1);
+
+  fusion.addOutput(tv5);
+
+  tv5->merge(0);
+  tv0->computeAt(tv5, -1);
+  tv1->computeAt(tv5, -1);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  at::manual_seed(0);
+  auto t0 = at::randn({2, 3}, options);
+  auto t1 = at::randn({1, 6}, options);
+
+  FusionExecutor fe;
+  fe.compileFusion(&fusion, {t0, t1});
+  auto cg_outputs = fe.runFusion({t0, t1});
+
+  auto ref = (at::native::view(t0, {6}) + 1).unsqueeze(0) + t1;
+
+  testValidate(&fusion, cg_outputs, {t0, t1}, {ref}, __LINE__, __FILE__);
+}
+
+TEST_F(NVFuserTest, FusionViewConcreteDomain2_CUDA) {
+  constexpr int kAxis = -1;
+  std::vector<int64_t> input_shape = {19, 12, 7, 99};
+  std::vector<int64_t> output_shape = {19, 3, 2772};
+
+  std::unique_ptr<Fusion> fusion_ptr = std::make_unique<Fusion>();
+  Fusion& fusion = *fusion_ptr.get();
+  FusionGuard fg(&fusion);
+
+  TensorView* x = makeSymbolicTensor(input_shape.size());
+  TensorView* bias = makeSymbolicTensor(output_shape.size());
+  fusion.addInput(x);
+  fusion.addInput(bias);
+
+  auto tv1 = softmax(x, kAxis);
+  auto x_view = view(tv1, input_shape, output_shape);
+  auto y = add(x_view, bias);
+  fusion.addOutput(y);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  at::Tensor at_x = at::randn(input_shape, options);
+  at::Tensor at_bias = at::randn(output_shape, options);
+  std::vector<IValue> aten_inputs = {at_x, at_bias};
+
+  FusionExecutorCache fusion_executor_cache(std::move(fusion_ptr));
+  auto outputs = fusion_executor_cache.runFusionWithInputs(aten_inputs);
+
+  auto at_tv1 = at::_softmax(at_x, kAxis, false /* half_to_float */);
+  auto at_x_view = at::native::view(at_tv1, output_shape);
+  auto at_y = at::add(at_x_view, at_bias);
+
+  testValidate(&fusion, outputs, aten_inputs, {at_y}, __LINE__, __FILE__);
+}
+
+// TODO Repro of issue #1608
+/*
+TEST_F(NVFuserTest, FusionViewConcreteDomain3_CUDA) {
+  std::vector<int64_t> input_shape = {14, 12, 8, 100};
+  std::vector<int64_t> bcast_shape = {14, 12, 8, 1};
+  std::vector<int64_t> other_shape = {14, 100, 96};
+  std::vector<int64_t> output_shape = {14, 3, 3200};
+
+  std::unique_ptr<Fusion> fusion_ptr = std::make_unique<Fusion>();
+  Fusion& fusion = *fusion_ptr.get();
+  FusionGuard fg(&fusion);
+
+  TensorView* x = makeSymbolicTensor(input_shape.size());
+  TensorView* y = makeConcreteTensor(bcast_shape);
+  TensorView* z = makeSymbolicTensor(other_shape.size());
+  fusion.addInput(x);
+  fusion.addInput(y);
+  fusion.addInput(z);
+
+  auto tv1 = add(x, y);
+  auto tv2 = view(tv1, input_shape, output_shape);
+  auto tv3 = view(z, other_shape, output_shape);
+  auto output = add(tv2, tv3);
+  fusion.addOutput(output);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  at::Tensor at_x = at::randn(input_shape, options);
+  at::Tensor at_y = at::randn(bcast_shape, options);
+  at::Tensor at_z = at::randn(other_shape, options);
+  std::vector<IValue> aten_inputs = {at_x, at_y, at_z};
+
+  FusionExecutorCache fusion_executor_cache(std::move(fusion_ptr));
+  auto outputs = fusion_executor_cache.runFusionWithInputs(aten_inputs);
+
+  auto at_tv1 = at::add(at_x, at_y);
+  auto at_tv2 = at::native::view(at_tv1, output_shape);
+  auto at_tv3 = at::native::view(at_z, output_shape);
+  auto at_output = at::add(at_tv2, at_tv3);
+
+  testValidate(&fusion, outputs, aten_inputs, {at_output}, __LINE__, __FILE__);
+}
+*/
+
+TEST_F(NVFuserTest, FusionViewConcreteDomain4_CUDA) {
+  std::vector<int64_t> shape1 = {3, 4, 5};
+  std::vector<int64_t> shape2 = {3 * 4 * 5};
+
+  std::unique_ptr<Fusion> fusion_ptr = std::make_unique<Fusion>();
+  Fusion& fusion = *fusion_ptr.get();
+  FusionGuard fg(&fusion);
+
+  auto tv0 = makeSymbolicTensor(shape1.size() - 1);
+  fusion.addInput(tv0);
+
+  auto tv1 = makeSymbolicTensor(shape1.size());
+  fusion.addInput(tv1);
+
+  auto tv2 = broadcast(tv0, {true, false, false});
+  auto tv3 = add(tv1, tv2);
+  auto tv4 = view(tv3, shape1, shape2);
+  auto tv5 = set(tv4);
+  fusion.addOutput(tv5);
+
+  tv0->computeAt(tv5, -1);
+  tv1->computeAt(tv5, -1);
+
+  TORCH_CHECK(tv5->nDims() == 1);
+
+  // The concrete domain of tv5, which is 1D, with permissive or loop mapping
+  // needs to be either the domain of tv4 or tv5, both of which have the three
+  // concrete root domains of tv1. In other words, it must map with tv4 and tv5
+  // with the exact mapping.
+  ComputeAtMap map(&fusion);
+  auto concrete_id = map.getConcreteMappedID(tv5->axis(0), IdMappingMode::PERMISSIVE);
+  TORCH_CHECK(
+      map.areMapped(concrete_id, tv5->axis(0), IdMappingMode::EXACT),
+      "Invalid concrete ID: ",
+      concrete_id->toString());
+  TORCH_CHECK(
+      map.areMapped(concrete_id, tv4->axis(0), IdMappingMode::EXACT),
+      "Invalid concrete ID: ",
+      concrete_id->toString());
+}
+
+TEST_F(NVFuserTest, FusionViewConcreteDomain5_CUDA) {
+  const std::vector<int64_t> shape1 = {12};
+  const std::vector<int64_t> shape2 = {4, 3};
+  const std::vector<int64_t> shape3 = {12, 5};
+  const std::vector<int64_t> shape4 = {4, 3, 5};
+
+  for (auto order : {true, false}) {
+    std::unique_ptr<Fusion> fusion_ptr = std::make_unique<Fusion>();
+    Fusion& fusion = *fusion_ptr.get();
+    FusionGuard fg(&fusion);
+
+    auto tv0 = makeSymbolicTensor(1);
+    fusion.addInput(tv0);
+
+    auto tv1 = makeSymbolicTensor(2);
+    fusion.addInput(tv1);
+
+    auto tv0_cache = set(tv0);
+
+    auto path1 = [&]() {
+      auto view_2d = view(tv0_cache, shape1, shape2);
+      auto view_2d_copy = set(view_2d);
+      fusion.addOutput(view_2d_copy);
+      return view_2d_copy;
+    };
+
+    auto path2 = [&]() {
+      auto tv0_bc = broadcast(tv0_cache, {false, true});
+      auto tv0_bc_plus_tv1 = add(tv0_bc, tv1);
+      auto view_3d = view(tv0_bc_plus_tv1, shape3, shape4);
+      auto view_3d_copy = set(view_3d);
+      fusion.addOutput(view_3d_copy);
+      return view_3d_copy;
+    };
+
+    TensorView* path1_out = nullptr;
+    TensorView* path2_out = nullptr;
+
+    if (order) {
+      // Fails before #1544. Concrete ID is picked from path1_out, which
+      // doesn't have the second root domain of tv1
+      path2_out = path2();
+      path1_out = path1();
+    } else {
+      // Works fine
+      path1_out = path1();
+      path2_out = path2();
+    }
+
+    path2_out->merge(-2, -1);
+    path2_out->merge(-2, -1);
+
+    tv0->computeAt(path2_out, -1);
+    tv1->computeAt(path2_out, -1);
+
+    TORCH_CHECK(path1_out->nDims() == 1);
+    TORCH_CHECK(path2_out->nDims() == 1);
+
+    ComputeAtMap map(&fusion);
+
+    // Make sure the two output tensors are mapped. Note both are 1D.
+    TORCH_CHECK(map.areMapped(path1_out->axis(0), path2_out->axis(0), IdMappingMode::LOOP));
+
+    auto concrete_id = map.getConcreteMappedID(path2_out->axis(0), IdMappingMode::LOOP);
+    TORCH_CHECK(
+        path2_out->axis(0) == concrete_id,
+        "Incorrect concrete ID: ",
+        concrete_id->toString());
+  }
 }
 
 } // namespace jit
