ebb997c2e656953f5991950443e047fc892caf7f
Felipe Petroski Such <felipe@openai.com>
2022-04-22 10:54:10 -0700

    [CUDA Graphs] Fix OOM inside graph capture_begin

--- c10/cuda/CUDACachingAllocator.cpp
+++ c10/cuda/CUDACachingAllocator.cpp
@@ -536,7 +536,7 @@ class DeviceCachingAllocator {
           || (release_available_cached_blocks(params) &&
               alloc_block(params, false))
           // Free all non-split cached blocks and retry alloc.
-          || (release_cached_blocks() && alloc_block(params, true));
+          || (C10_LIKELY(captures_underway == 0) && release_cached_blocks() && alloc_block(params, true));
     }
 
     if (!block_found) {
