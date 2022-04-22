55ddb291cbc4a6f7831e359a52a2aeff30ea77f3
Edward Z. Yang <ezyang@fb.com>
2022-04-22 07:00:58 -0700

    Update base for Update on "Make torch function plain method warning once"
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- torch/nn/__init__.py
+++ torch/nn/__init__.py
@@ -2,7 +2,6 @@ from .modules import *  # noqa: F403
 from .parameter import Parameter, UninitializedParameter, UninitializedBuffer
 from .parallel import DataParallel
 from . import init
-from . import functional
 from . import utils
 
 
