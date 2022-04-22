pytorch/pytorch

commit c179fba21cfa2a0093fad50ccad5a22dd7cff52c

Remove `-Wno-unused-variable` from `utils.cmake` (take 2) (#75538)
Summary:
[Comment](https://github.com/pytorch/pytorch/pull/62445/files#r680132022) claims, it got added for consistency with  top level CMakeLists.txt, but `-Wno-unused-variable` is not mentioned there.

Modify violations in 50+ files that were added in the interim by either removing unused variables, or decorating the code with `C10_UNUSED` if local variable is likely used to extend object lifetime until the end of the block.

Caused preventable revert in https://github.com/pytorch/pytorch/pull/72633#issuecomment-1092300787

Pull Request resolved: https://github.com/pytorch/pytorch/pull/75538

Reviewed By: anjali411

Differential Revision: D35747333

Pulled By: malfet

fbshipit-source-id: 3fc5828e44a4c05ba0e89e92613e6ebbdb260626

caffe2/operators/deform_conv_op_impl.h
@@ -155,7 +155,7 @@ bool DeformConvOp<T, Context>::RunOnDeviceWithOrderNCHW() {
     col_buffer->Resize(buffer_shape);
     T* col_buffer_data = col_buffer->template mutable_data<T>();
     // Im2col, followed by gemm.
-    for (const auto image_id : c10::irange(N)) {
+    for (C10_UNUSED const auto image_id : c10::irange(N)) {
       for (const auto group_id : c10::irange(group_)) {
         DeformableIm2col(
             Xdata + group_id * input_offset,
@@ -342,7 +342,7 @@ bool DeformConvGradientOp<T, Context>::RunOnDeviceWithOrderNCHW() {
     math::Set<T, Context>(dX->numel(), 0, dXdata, &context_);
   }
 
-  for (const auto image_id : c10::irange(N)) {
+  for (C10_UNUSED const auto image_id : c10::irange(N)) {
     for (const auto group_id : c10::irange(group_)) {
       math::Gemm<T, Context>(
           CblasTrans,

caffe2/operators/piecewise_linear_transform_op.h
@@ -62,7 +62,7 @@ class PiecewiseLinearTransformOp final : public Operator<Context> {
       const int64_t num_bounds_per_group,
       const int64_t num_group) {
     const T* start = bounds;
-    for (const auto i : c10::irange(num_group)) {
+    for (C10_UNUSED const auto i : c10::irange(num_group)) {
       if (!std::is_sorted(start, start + num_bounds_per_group)) {
         return false;
       }

aten/src/ATen/core/dynamic_type.cpp
@@ -372,7 +372,7 @@ ivalue::TupleTypeFactory<TupleType>::fallback(const Type& type) {
   for (const auto& elem : dyn.arguments().elems) {
     types.emplace_back(elem.ty);
     if (const auto& name = elem.label) {
-      fields.emplace_back(*elem.label);
+      fields.emplace_back(*name);
     }
   }
   if (const auto& name = dyn.name()) {

aten/src/ATen/native/RNN.cpp
@@ -1833,7 +1833,7 @@ DEFINE_QUANTIZED_RNN_CELL_DYNAMIC(quantized_rnn_tanh_cell_dynamic, simple_hx_typ
 
 namespace {
 
-static auto ensure_linear_params_registered = register_linear_params();
+static C10_UNUSED auto ensure_linear_params_registered = register_linear_params();
 
 static auto cell_params_base_registry =
     torch::selective_class_<CellParamsBase>("rnn", TORCH_SELECTIVE_CLASS("CellParamsBase"))

aten/src/ATen/native/ao_sparse/quantized/cpu/fbgemm_utils.cpp
@@ -71,7 +71,7 @@ int register_linear_params() {
 }
 
 namespace {
-static auto linear_params = register_linear_params();
+static C10_UNUSED auto linear_params = register_linear_params();
 }  // namespace
 
 }}  // namespace ao::sparse

aten/src/ATen/native/quantized/cpu/fbgemm_utils.cpp
@@ -554,9 +554,9 @@ int register_embedding_params() {
 
 namespace {
 
-static auto conv2d_params = register_conv_params<2>();
-static auto conv3d_params = register_conv_params<3>();
-static auto linear_params = register_linear_params();
-static auto embedding_params = register_embedding_params();
+static C10_UNUSED auto conv2d_params = register_conv_params<2>();
+static C10_UNUSED auto conv3d_params = register_conv_params<3>();
+static C10_UNUSED auto linear_params = register_linear_params();
+static C10_UNUSED auto embedding_params = register_embedding_params();
 
 } // namespace

aten/src/ATen/native/vulkan/api/Context.cpp
@@ -187,7 +187,6 @@ Descriptor::Set dispatch_prologue(
     const Shader::Descriptor& shader_descriptor,
     const Shader::WorkGroup& local_work_group_size) {
   Context* const context = api::context();
-  const GPU gpu = context->gpu();
   Descriptor& descriptor = context->descriptor();
   Pipeline& pipeline = context->pipeline();
   Shader& shader = context->shader();

aten/src/ATen/native/vulkan/ops/Concat.cpp
@@ -199,7 +199,6 @@ Tensor cat_height(const TensorList tensors, vTensor& v_output) {
 Tensor cat(
   const at::TensorList tensors,
   const int64_t dim) {
-  const auto norm_dim = normalize_dim(dim, 4);
   TORCH_CHECK(
     tensors.size() > 0,
     "Vulkan cat expects at least one tensor");

commit fd64655aa4b6890d205273ec76b416ee3b524783

Tentative fix for CUDA-10.2 windows build failures (#76204)
Summary:
`C10_UNUSED` somehow triggers segfault in some versions of NVCC that looks like something as follows:
```
caffe2\caffe2\operators\piecewise_linear_transform_op.h(65): internal error: assertion failed: gen_variable_decl: declared_type is NULL (cp_gen_be.c, line 22209 in gen_variable_decl)

1 catastrophic error detected in the compilation of "caffe2/caffe2/operators/piecewise_linear_transform_op.cu".
Compilation aborted.
nvcc error   : 'cudafe++' died with status 0xC0000409

```
Fixes regression introduced by  https://github.com/pytorch/pytorch/pull/75538 / D35747333 (https://github.com/pytorch/pytorch/commit/f6c275f55ddfd22f8c0558efad3068b0e91f1560)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/76204

Test Plan: CI

Reviewed By: EscapeZero, atalman

Differential Revision: D35831451

fbshipit-source-id: f744d4688c9fd324f8f54b27781a3def97778d1e

caffe2/operators/deform_conv_op_impl.h
@@ -155,7 +155,8 @@ bool DeformConvOp<T, Context>::RunOnDeviceWithOrderNCHW() {
     col_buffer->Resize(buffer_shape);
     T* col_buffer_data = col_buffer->template mutable_data<T>();
     // Im2col, followed by gemm.
-    for (C10_UNUSED const auto image_id : c10::irange(N)) {
+    for (const auto image_id : c10::irange(N)) {
+      (void)image_id; // CUDA-10.2 on Windows crashes when C10_UNUSED macro is used
       for (const auto group_id : c10::irange(group_)) {
         DeformableIm2col(
             Xdata + group_id * input_offset,
@@ -342,7 +343,8 @@ bool DeformConvGradientOp<T, Context>::RunOnDeviceWithOrderNCHW() {
     math::Set<T, Context>(dX->numel(), 0, dXdata, &context_);
   }
 
-  for (C10_UNUSED const auto image_id : c10::irange(N)) {
+  for (const auto image_id : c10::irange(N)) {
+    (void)image_id; // CUDA-10.2 on Windows crashes when C10_UNUSED macro is used
     for (const auto group_id : c10::irange(group_)) {
       math::Gemm<T, Context>(
           CblasTrans,

caffe2/operators/piecewise_linear_transform_op.h
@@ -62,7 +62,8 @@ class PiecewiseLinearTransformOp final : public Operator<Context> {
       const int64_t num_bounds_per_group,
       const int64_t num_group) {
     const T* start = bounds;
-    for (C10_UNUSED const auto i : c10::irange(num_group)) {
+    for (const auto i : c10::irange(num_group)) {
+      (void)i; // CUDA-10.2 on Windows crashes when C10_UNUSED macro is used
       if (!std::is_sorted(start, start + num_bounds_per_group)) {
         return false;
       }