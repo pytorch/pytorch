3c33ed4e749e4e1aa914318151dbb54295c44fa1
Edward Yang <ezyang@fb.com>
2022-04-24 16:45:01 -0700

    Rename tools/codegen to torchgen (#76275)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76275
    
    In preparation for addressing
    https://github.com/pytorch/pytorch/issues/73212
    
    Diff was generated with:
    
    ```
    git mv tools/codegen torchgen
    git grep -l 'tools.codegen' | xargs sed -i 's/tools.codegen/torchgen/g'
    sed -i "s/\${TOOLS_PATH}\/codegen/\${TORCH_ROOT}\/torchgen/g" caffe2/CMakeLists.txt
    ```
    
    and a manual edits to:
    
    * tools/test/test_gen_backend_stubs.py
    * torchgen/build.bzl
    * torchgen/gen_backend_stubs.py
    
    aka this diff:
    
    ```
     diff --git a/tools/test/test_gen_backend_stubs.py b/tools/test/test_gen_backend_stubs.py
    index 3dc26c6d2d..104054575e 100644
     --- a/tools/test/test_gen_backend_stubs.py
    +++ b/tools/test/test_gen_backend_stubs.py
    @@ -9,7 +9,7 @@ from torchgen.gen_backend_stubs import run
     from torchgen.gen import _GLOBAL_PARSE_NATIVE_YAML_CACHE  # noqa: F401
    
     path = os.path.dirname(os.path.realpath(__file__))
    -gen_backend_stubs_path = os.path.join(path, '../torchgen/gen_backend_stubs.py')
    +gen_backend_stubs_path = os.path.join(path, '../../torchgen/gen_backend_stubs.py')
    
     # gen_backend_stubs.py is an integration point that is called directly by external backends.
     # The tests here are to confirm that badly formed inputs result in reasonable error messages.
     diff --git a/torchgen/build.bzl b/torchgen/build.bzl
    index ed04e35a43..d00078a3cf 100644
     --- a/torchgen/build.bzl
    +++ b/torchgen/build.bzl
    @@ -1,6 +1,6 @@
     def define_targets(rules):
         rules.py_library(
    -        name = "codegen",
    +        name = "torchgen",
             srcs = rules.glob(["**/*.py"]),
             deps = [
                 rules.requirement("PyYAML"),
    @@ -11,6 +11,6 @@ def define_targets(rules):
    
         rules.py_binary(
             name = "gen",
    -        srcs = [":codegen"],
    +        srcs = [":torchgen"],
             visibility = ["//visibility:public"],
         )
     diff --git a/torchgen/gen_backend_stubs.py b/torchgen/gen_backend_stubs.py
    index c1a672a655..beee7a15e0 100644
     --- a/torchgen/gen_backend_stubs.py
    +++ b/torchgen/gen_backend_stubs.py
    @@ -474,7 +474,7 @@ def run(
     ) -> None:
    
         # Assumes that this file lives at PYTORCH_ROOT/torchgen/gen_backend_stubs.py
    -    pytorch_root = pathlib.Path(__file__).parent.parent.parent.absolute()
    +    pytorch_root = pathlib.Path(__file__).parent.parent.absolute()
         template_dir = os.path.join(pytorch_root, "aten/src/ATen/templates")
    
         def make_file_manager(install_dir: str) -> FileManager:
    ```
    
    run_all_fbandroid_tests
    
    Test Plan: sandcastle
    
    Reviewed By: albanD, ngimel
    
    Differential Revision: D35770317
    
    fbshipit-source-id: b77d0c2af80765289a6294f8aa4481951a1e6fb0

--- tools/linter/clang_tidy/generate_build_files.py
+++ tools/linter/clang_tidy/generate_build_files.py
@@ -43,7 +43,7 @@ def run_autogen() -> None:
         [
             sys.executable,
             "-m",
-            "tools.codegen.gen",
+            "torchgen.gen",
             "-s",
             "aten/src/ATen",
             "-d",
