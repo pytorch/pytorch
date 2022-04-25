f5009c9a270cb161ec65a8ee1718300e0dc197b1
Allen Goodman <allen.goodman@icloud.com>
2022-04-25 18:57:11 -0400

    Create Polynomial.h

---
+++ aten/src/ATen/native/Polynomial.h
@@ -0,0 +1,105 @@
+#pragma once
+
+namespace at {
+  namespace native {
+    Tensor polyadd(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polycompanion(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyder(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polydiv(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polydomain(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyfit(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyfromroots(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polygrid2d(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polygrid3d(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyint(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyline(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polymul(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polymulx(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyone(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polypow(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyroots(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polysub(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polytrim(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyval(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyval2d(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyval3d(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyvalfromroots(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyvander(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyvander2d(const Tensor& self) {
+      return self;
+    }
+
+    Tensor polyvander3d(const Tensor& self) {
+      return self;
+    }
+  }
+}
