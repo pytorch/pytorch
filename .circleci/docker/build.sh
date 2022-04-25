9291f38e7f25b62f413e9a088e09aae157cc427f
Peter Bell <peterbell10@live.co.uk>
2022-04-25 16:07:01 +0100

    Bump minimum CMake version to 3.13
    [ghstack-poisoned]

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -84,13 +84,11 @@ TRAVIS_DL_URL_PREFIX="https://s3.amazonaws.com/travis-python-archives/binaries/u
 case "$image" in
   pytorch-linux-xenial-py3.8)
     ANACONDA_PYTHON_VERSION=3.8
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     # Do not install PROTOBUF, DB, and VISION as a test
     ;;
   pytorch-linux-xenial-py3.7-gcc5.4)
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.12.4  # To make sure XNNPACK is enabled for the BACKWARDS_COMPAT_TEST used with this image
     GCC_VERSION=5
     PROTOBUF=yes
     DB=yes
@@ -99,13 +97,11 @@ case "$image" in
     ;;
   pytorch-linux-xenial-py3.7-gcc7.2)
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     # Do not install PROTOBUF, DB, and VISION as a test
     ;;
   pytorch-linux-xenial-py3.7-gcc7)
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -115,7 +111,6 @@ case "$image" in
     CUDA_VERSION=10.2
     CUDNN_VERSION=7
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -127,7 +122,6 @@ case "$image" in
     CUDNN_VERSION=8
     TENSORRT_VERSION=8.0.1.6
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -139,7 +133,6 @@ case "$image" in
     CUDNN_VERSION=8
     TENSORRT_VERSION=8.0.1.6
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     CLANG_VERSION=9
     PROTOBUF=yes
     DB=yes
@@ -150,7 +143,6 @@ case "$image" in
     CUDA_VERSION=11.5.0
     CUDNN_VERSION=8
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -161,7 +153,6 @@ case "$image" in
     CUDA_VERSION=11.6.0
     CUDNN_VERSION=8
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -171,7 +162,6 @@ case "$image" in
   pytorch-linux-xenial-py3-clang5-asan)
     ANACONDA_PYTHON_VERSION=3.7
     CLANG_VERSION=5.0
-    CMAKE_VERSION=3.13.5
     PROTOBUF=yes
     DB=yes
     VISION=yes
@@ -179,7 +169,6 @@ case "$image" in
   pytorch-linux-xenial-py3-clang7-asan)
     ANACONDA_PYTHON_VERSION=3.7
     CLANG_VERSION=7
-    CMAKE_VERSION=3.10.3
     PROTOBUF=yes
     DB=yes
     VISION=yes
@@ -187,7 +176,6 @@ case "$image" in
   pytorch-linux-xenial-py3-clang7-onnx)
     ANACONDA_PYTHON_VERSION=3.7
     CLANG_VERSION=7
-    CMAKE_VERSION=3.10.3
     PROTOBUF=yes
     DB=yes
     VISION=yes
@@ -195,7 +183,6 @@ case "$image" in
   pytorch-linux-xenial-py3-clang5-android-ndk-r19c)
     ANACONDA_PYTHON_VERSION=3.7
     CLANG_VERSION=5.0
-    CMAKE_VERSION=3.13.5
     LLVMDEV=yes
     PROTOBUF=yes
     ANDROID=yes
@@ -205,7 +192,6 @@ case "$image" in
     ;;
   pytorch-linux-xenial-py3.7-clang7)
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.10.3
     CLANG_VERSION=7
     PROTOBUF=yes
     DB=yes
@@ -268,7 +254,10 @@ case "$image" in
     VISION=yes
     echo "image '$image' did not match an existing build configuration"
     if [[ "$image" == *xenial* ]]; then
-      CMAKE_VERSION=3.10.3
+      CMAKE_VERSION=3.13.5
+    fi
+    if [[ "$image" == *bionic* ]]; then
+        CMAKE_VERSION=3.13.5
     fi
     if [[ "$image" == *py* ]]; then
       extract_version_from_image_name py ANACONDA_PYTHON_VERSION
