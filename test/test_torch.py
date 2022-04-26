23916e8967607f49f46bb1491c5b2d98be33ab44
Christian Puhrsch <cpuhrsch@fb.com>
2022-04-26 20:57:04 +0000

    Add missing brackets

--- test/test_torch.py
+++ test/test_torch.py
@@ -7553,7 +7553,7 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
         self.assertRaises(RuntimeError, lambda: torch.zeros(1, 6).expand(5, 6).copy_(torch.zeros(5, 6)))
 
     # FIXME: Port to a more appropriate test suite
-    @parametrize("layout", torch.strided, torch.sparse_csr)
+    @parametrize("layout", [torch.strided, torch.sparse_csr])
     def test_to(self, layout):
         def test_copy_behavior(t, non_blocking=False):
             self.assertIs(t, t.to(t, non_blocking=non_blocking))
