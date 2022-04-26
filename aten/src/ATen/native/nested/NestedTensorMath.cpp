cc8b799807158f42539dc9a803b1a4a6a764062c
Christian Puhrsch <cpuhrsch@fb.com>
2022-04-26 01:19:14 +0000

    Trying TORCH_API

--- aten/src/ATen/native/nested/NestedTensorMath.cpp
+++ aten/src/ATen/native/nested/NestedTensorMath.cpp
@@ -51,6 +51,26 @@ int64_t num_bytes(IntArrayRef sizes) {
   return result;
 }
 
+std::vector<int64_t> NestedTensor_get_max_size_from_size_tensor(const Tensor& sizes) {
+  if (sizes.dim() == 0) {
+    return {};
+  }
+  const auto sizes_ptr = sizes.data_ptr<int64_t>();
+  const auto sizes_size_0 = sizes.sizes()[0];
+  const auto sizes_size_1 = sizes.sizes()[1];
+  TORCH_INTERNAL_ASSERT(sizes_size_1 > 0);
+  std::vector<int64_t> results(sizes_size_1, 0);
+  for (const auto ii : c10::irange(sizes_size_0)) {
+    for (const auto jj : c10::irange(sizes_size_1)) {
+      auto val = sizes_ptr[ii * sizes_size_1 + jj];
+      if (results[jj] < val) {
+        results[jj] = val;
+      }
+    }
+  }
+  return results;
+}
+
 Tensor pad_tensor_to_shape(
     const Tensor& t,
     IntArrayRef goal_shape,
@@ -192,6 +212,11 @@ int64_t get_consistent_last_dim_of_nested_tensor(const NestedTensorImpl& nt) {
       "all tensors in NestedTensor must have the same trailing dim for Matmul but got ",
       nt.get_nested_size_tensor().select(1, -1));
   return *result;
+}
+
+inline std::vector<int64_t> NestedTensor_get_max_size(const NestedTensorImpl& nt) {
+  const auto& sizes = nt.get_nested_size_tensor();
+  return NestedTensor_get_max_size_from_size_tensor(sizes);
 }
 
 Tensor NestedTensor_layer_norm(
--- aten/src/ATen/native/nested/NestedTensorMath.h
+++ aten/src/ATen/native/nested/NestedTensorMath.h
@@ -7,30 +7,9 @@ struct NestedTensorImpl;
 // TODO: cache this and only do it once per NestedTensor
 int64_t get_consistent_last_dim_of_nested_tensor(const NestedTensorImpl& nt);
 
-inline std::vector<int64_t> NestedTensor_get_max_size_from_size_tensor(const Tensor& sizes) {
-  if (sizes.dim() == 0) {
-    return {};
-  }
-  const auto sizes_ptr = sizes.data_ptr<int64_t>();
-  const auto sizes_size_0 = sizes.sizes()[0];
-  const auto sizes_size_1 = sizes.sizes()[1];
-  TORCH_INTERNAL_ASSERT(sizes_size_1 > 0);
-  std::vector<int64_t> results(sizes_size_1, 0);
-  for (const auto ii : c10::irange(sizes_size_0)) {
-    for (const auto jj : c10::irange(sizes_size_1)) {
-      auto val = sizes_ptr[ii * sizes_size_1 + jj];
-      if (results[jj] < val) {
-        results[jj] = val;
-      }
-    }
-  }
-  return results;
-}
+TORCH_API std::vector<int64_t> NestedTensor_get_max_size_from_size_tensor(const Tensor& sizes);
 
-inline std::vector<int64_t> NestedTensor_get_max_size(const NestedTensorImpl& nt) {
-  const auto& sizes = nt.get_nested_size_tensor();
-  return NestedTensor_get_max_size_from_size_tensor(sizes);
-}
+TORCH_API std::vector<int64_t> NestedTensor_get_max_size(const NestedTensorImpl& nt);
 
 Tensor NestedTensor_to_padded_tensor_generic(const Tensor& t, double padding);
 
