af32cd4bfc3dd222d1d0f3a70e2675b1ea483880
Brian Hirsh (Meta Employee) <hirsheybar@fb.com>
2022-04-26 15:37:40 -0700

    functionalization: add native fill() op (#76084)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76084
    
    Approved by: https://github.com/ezyang
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/ea5209c9fd78a34849600875d0f3f5b994201833
    
    Reviewed By: osalpekar
    
    Differential Revision: D35938183
    
    Pulled By: bdhirsh
    
    fbshipit-source-id: 73c318c03e49671cdaee997807e2dba2a0c57198

--- tools/autograd/gen_python_functions.py
+++ tools/autograd/gen_python_functions.py
@@ -153,6 +153,8 @@ _SKIP_PYTHON_BINDINGS = [
     "replace_",  # only used by the functionalization pass, doesn't need to be exposed to python
     "zero",  # only used by the functionalization pass, doesn't need to be exposed to python
     "copy",  # only used by the functionalization pass
+    "fill.Tensor",  # only used by the functionalization pass
+    "fill.Scalar",  # only used by the functionalization pass
 ]
 
 SKIP_PYTHON_BINDINGS = list(
