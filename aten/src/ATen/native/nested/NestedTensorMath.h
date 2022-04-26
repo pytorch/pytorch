a017d8df76113af7b2f11fa06604c86194364e7f
Christian Puhrsch <cpuhrsch@fb.com>
2022-04-26 01:49:31 +0000

    Remove superfluous declaration

--- aten/src/ATen/native/nested/NestedTensorMath.h
+++ aten/src/ATen/native/nested/NestedTensorMath.h
@@ -7,8 +7,6 @@ struct NestedTensorImpl;
 // TODO: cache this and only do it once per NestedTensor
 int64_t get_consistent_last_dim_of_nested_tensor(const NestedTensorImpl& nt);
 
-TORCH_API std::vector<int64_t> NestedTensor_get_max_size_from_size_tensor(const Tensor& sizes);
-
 TORCH_API std::vector<int64_t> NestedTensor_get_max_size(const NestedTensorImpl& nt);
 
 Tensor NestedTensor_to_padded_tensor_generic(const Tensor& t, double padding);
