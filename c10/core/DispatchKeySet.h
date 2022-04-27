56c9c43e984508a8d1db162691d475090ffd87c2
Brian Hirsh <hirsheybar@fb.com>
2022-04-26 17:09:45 -0700

    Update on "free up 3 dispatch keys: make autocast + SparseCSR per-backend keys"
    Cleans up `DispatchKey.h` so that the remaining dispatch keys that currently have different implementations for different backends (`Autocast` and `SparseCsr`) are properly marked as "per-backend functionality keys".
    
    This also frees up 3 keys worth of space (SparseCsr saves us 1 key, Autocast saves us 2).
    
    Also fixes https://github.com/pytorch/pytorch/issues/76175
    
    
    
    
    [ghstack-poisoned]

--- c10/core/DispatchKeySet.h
+++ c10/core/DispatchKeySet.h
@@ -817,6 +817,9 @@ inline DispatchKeySet getAutocastRelatedKeySetFromBackend(BackendComponent t) {
   constexpr auto autocast_xla_ks = DispatchKeySet(DispatchKey::AutocastXLA);
   constexpr auto autocast_mlc_ks = DispatchKeySet(DispatchKey::AutocastMLC);
   constexpr auto autocast_ipu_ks = DispatchKeySet(DispatchKey::AutocastIPU);
+  constexpr auto autocast_ipu_ks = DispatchKeySet(DispatchKey::AutocastHPU);
+  constexpr auto autocast_ipu_ks = DispatchKeySet(DispatchKey::AutocastVE);
+  constexpr auto autocast_ipu_ks = DispatchKeySet(DispatchKey::AutocastLazy);
   constexpr auto autocast_xpu_ks = DispatchKeySet(DispatchKey::AutocastXPU);
 
   switch (t) {
