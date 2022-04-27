c37e920011c2e37027e436eed5fa29d3376940b6
Peter Bell <peterbell10@live.co.uk>
2022-04-28 00:32:10 +0100

    Update base for Update on "Bump minimum CMake version to 3.13"
    [ghstack-poisoned]

--- aten/src/ATen/native/vulkan/ops/Gru.cpp
+++ aten/src/ATen/native/vulkan/ops/Gru.cpp
@@ -99,7 +99,8 @@ TORCH_LIBRARY_IMPL(aten, Vulkan, m) {
 std::vector<LinearOpContext> pack_linear_op_contexts(
     const std::vector<Tensor>& params_cpu,
     int64_t num_layers) {
-  TORCH_CHECK(params_cpu.size() == 4 * num_layers, "Vulkan gru expects 'params_cpu' size to be 4 * 'num_layers'.");
+  TORCH_CHECK(static_cast<int64_t>(params_cpu.size()) == 4 * num_layers,
+              "Vulkan gru expects 'params_cpu' size to be 4 * 'num_layers'.");
   std::vector<LinearOpContext> linear_op_contexts;
   for (int64_t i = 0; i < num_layers; ++i) {
     const auto& w_ih = params_cpu.at(i * 4);
