bad23f4b7ef73ffc2ef4a893364512907e9c4555
Will Constable <whc@fb.com>
2022-04-27 20:16:46 -0700

    Make lazy tensor creation and value strings customizable (#76472)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76472
    
    - lets XLA backend customize codegenned functions during
      migration to LTC
    
    Test Plan: Imported from OSS
    
    Reviewed By: Krovatkin, bdhirsh
    
    Differential Revision: D35980435
    
    Pulled By: wconstab
    
    fbshipit-source-id: 6138aef20862fccec40d715ffbb5a40a0a7d0401

--- torchgen/gen_lazy_tensor.py
+++ torchgen/gen_lazy_tensor.py
@@ -276,6 +276,9 @@ def run_gen_lazy_tensor(
     metrics_counter: str = 'TORCH_LAZY_FN_COUNTER("lazy::")',
     create_tensor: str = "LazyTensor::Create",
     create_from_first_tensor: bool = False,
+    create_aten_from_ltc_tensor: str = "torch::lazy::CreateAtenFromLtcTensor",
+    tuple_aten_from_ltc_tensors: str = "torch::lazy::TupleAtenFromLtcTensors",
+    lazy_value_class: str = "torch::lazy::Value",
 ) -> None:
 
     template_dir = os.path.join(aten_path, "templates")
@@ -475,6 +478,9 @@ def run_gen_lazy_tensor(
                         metrics_counter,
                         create_tensor,
                         create_from_first_tensor,
+                        create_aten_from_ltc_tensor,
+                        tuple_aten_from_ltc_tensors,
+                        lazy_value_class,
                     ),
                     grouped_native_functions,
                     codegenInplaceVariant=True,
