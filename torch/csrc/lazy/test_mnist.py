258e0aaacc887a3f92c5696430dc74e62c2dca50
Nikolay Korovaiko <korovaikon@gmail.com>
2022-04-27 20:59:17 -0700

    fix lints2

--- torch/csrc/lazy/test_mnist.py
+++ torch/csrc/lazy/test_mnist.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
 import os
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
