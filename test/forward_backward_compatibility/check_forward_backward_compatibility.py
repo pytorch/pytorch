f30d8121ae45cae8a433738ab58acb32b989849d
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-04-29 13:34:30 +0000

    Sparse CSR: enable autograd for torch.sparse.addmm and torch.sparse.mm
    This PR updates the derivative rule for `torch.sparse.addmm` to be
    working with CSR sparse matrix. Notably `torch.sparse.sampled_addmm` is
    used in the backward function.
    
    [ghstack-poisoned]

--- test/forward_backward_compatibility/check_forward_backward_compatibility.py
+++ test/forward_backward_compatibility/check_forward_backward_compatibility.py
@@ -38,6 +38,7 @@ ALLOW_LIST = [
     # Internal, profiler-specific ops
     ("profiler::_call_end_callbacks_on_jit_fut*", datetime.date(9999, 1, 1)),
     ("profiler::_record_function_enter", datetime.date(9999, 1, 1)),
+    ("aten::_sparse_addmm", datetime.date(2022, 6, 30)),
     ("aten::linalg_matrix_rank", datetime.date(2021, 10, 30)),
     ("aten::linalg_pinv", datetime.date(2021, 10, 30)),
     ("aten::_cholesky_helper", datetime.date(9999, 1, 1)),
