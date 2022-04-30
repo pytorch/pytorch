fb7e625e75834199fd40ab52cb61d26433c3acbe
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2022-04-29 17:26:02 -0700

    fix comment

--- aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.cpp
+++ aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.cpp
@@ -821,7 +821,7 @@ inline static void svd_cusolver_gesvdaStridedBatched(
     const Tensor& A, const Tensor& U, const Tensor& S, const Tensor& V,
     const Tensor& infos, bool full_matrices, bool compute_uv) {
   // We need to pass a copy of A, as it will be overwritten
-  // gesvd just knows how to handle m >= n, so in the other case we need to transpose A
+  // gesvdaStridedBatched just knows how to handle m >= n, so in the other case we need to transpose A
   const auto not_A_H = A.size(-2) >= A.size(-1);
   AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(A.scalar_type(), "svd_cuda_gesvdaStridedBatched", [&] {
     apply_svd_cusolver_gesvdaStridedBatched<scalar_t>(
