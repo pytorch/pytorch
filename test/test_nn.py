773bd1a0e0b1e91c2c62019e39e6eff92675c379
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2022-05-02 15:18:19 -0700

    fix unit test 5

--- test/test_nn.py
+++ test/test_nn.py
@@ -16209,13 +16209,13 @@ class TestNNDeviceType(NNTestCase):
             input.requires_grad_(True)
             pool = torch.nn.MaxPool2d(
                 kernel_size, stride, padding, dilation, return_indices=True, ceil_mode=False
-                )
+            )
 
             ref_input = input.detach().clone().contiguous().requires_grad_(True)
             ref_grad = grad.detach().clone().contiguous()
             ref_pool = torch.nn.MaxPool2d(
                 kernel_size, stride, padding, dilation, return_indices=True, ceil_mode=False
-                ).to(device)
+            ).to(device)
 
             out, ind = pool(input)
             out.backward(grad)
