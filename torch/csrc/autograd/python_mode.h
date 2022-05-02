395a620a4fba8566d552c52732fa1c6d1c74379d
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-05-02 14:42:11 +0000

    Revert "Change 'python mode' to 'torch dispatch mode'"
    This reverts commit 7203a73986943c50b8d2f0fdc9d09821705a1602.
    
    Reverted https://github.com/pytorch/pytorch/pull/76562 on behalf of https://github.com/janeyx99

--- torch/csrc/autograd/python_mode.h
+++ torch/csrc/autograd/python_mode.h
@@ -5,7 +5,7 @@
 
 namespace torch { namespace autograd {
 
-struct TORCH_API TorchDispatchMode {
+struct TORCH_API PythonMode {
   // Enter python mode, causing all operators to dispatch to the type's __torch_dispatch__.
   // `type` is the type of a Tensor subclass that has __torch_dispatch__.
   static void enter(PyObject* type);
