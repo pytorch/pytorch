e5f79da9b4f4375b92e7ac9b34063dc082165f56
Peter Bell <peterbell10@live.co.uk>
2022-05-02 20:59:49 +0100

    Update base for Update on "Skip only hop of __torch_function__ at a time"
    Closes #55093
    
    This changes `Tensor.__torch_function__` to skip only one level of
    `__torch_function__` calls. This is achieved by adding a new state variable
    `skip_next_torch_function` which is reset to false every time
    `has_torch_function` is checked, which works both in C++ and Python.
    
    In the issue we discussed making `DisableTorchFunction` public, but with this
    you can just write `super().__torch_function__(func, types, args, kwargs)`
    to achieve the intended effect whilst being clear that this is
    intended for use in `__torch_function__` implementations only.
    
    [ghstack-poisoned]

--- torch/csrc/autograd/python_variable.cpp
+++ torch/csrc/autograd/python_variable.cpp
@@ -649,25 +649,22 @@ int THPVariable_set_data(THPVariable *self, PyObject *data, void *unused)
   END_HANDLE_TH_ERRORS_RET(-1)
 }
 
-PyObject *THPVariable_get__grad(THPVariable *self, void *unused)
+PyObject *THPVariable_get_grad(THPVariable *self, void *unused)
 {
-  HANDLE_TH_ERRORS
-  return THPVariable_Wrap(THPVariable_Unpack(self).grad());
-  END_HANDLE_TH_ERRORS
-}
-
-PyObject *THPVariable_get_grad(THPVariable *self, void *unused) {
   HANDLE_TH_ERRORS
   if (check_has_torch_function((PyObject *)self)) {
     return handle_torch_function_getter(self, "grad");
   }
-  return THPVariable_get__grad(self, unused);
+  return THPVariable_Wrap(THPVariable_Unpack(self).grad());
   END_HANDLE_TH_ERRORS
 }
 
-int THPVariable_set__grad(THPVariable *self, PyObject *py_grad, void *unused)
+int THPVariable_set_grad(THPVariable *self, PyObject *py_grad, void *unused)
 {
   HANDLE_TH_ERRORS
+  if (check_has_torch_function((PyObject *)self)) {
+    return handle_torch_function_setter(self, "grad", py_grad);
+  }
   const auto& var = THPVariable_Unpack(self);
   if (!py_grad || py_grad == Py_None) {
     var.mutable_grad().reset();
@@ -694,15 +691,6 @@ int THPVariable_set__grad(THPVariable *self, PyObject *py_grad, void *unused)
 
   var.mutable_grad() = grad;
   return 0;
-  END_HANDLE_TH_ERRORS_RET(-1)
-}
-
-int THPVariable_set_grad(THPVariable *self, PyObject *py_grad, void *unused) {
-  HANDLE_TH_ERRORS
-  if (check_has_torch_function((PyObject *)self)) {
-    return handle_torch_function_setter(self, "grad", py_grad);
-  }
-  return THPVariable_set__grad(self, py_grad, unused);
   END_HANDLE_TH_ERRORS_RET(-1)
 }
 
@@ -1196,7 +1184,7 @@ static struct PyGetSetDef THPVariable_properties[] = {
   {"is_leaf", (getter)THPVariable_is_leaf, nullptr, nullptr, nullptr},
   {"retains_grad", (getter)THPVariable_retains_grad, nullptr, nullptr, nullptr},
   {"data", (getter)THPVariable_get_data, (setter)THPVariable_set_data, nullptr, nullptr},
-  {"_grad", (getter)THPVariable_get__grad, (setter)THPVariable_set__grad, nullptr, nullptr}, // Allows the python class to override .grad
+  {"_grad", (getter)THPVariable_get_grad, (setter)THPVariable_set_grad, nullptr, nullptr}, // Allows the python class to override .grad
   {"grad", (getter)THPVariable_get_grad, (setter)THPVariable_set_grad, nullptr, nullptr},
   {"_base", (getter)THPVariable_get_base, nullptr, nullptr, nullptr},
   {"volatile", (getter)THPVariable_get_volatile, (setter)THPVariable_set_volatile, nullptr, nullptr},
