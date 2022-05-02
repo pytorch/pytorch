dcc37b4e8a746f0611d2e62be6aca447b5a77a97
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-05-02 14:42:11 +0000

    Revert "Change 'python mode' to 'torch dispatch mode'"
    This reverts commit 7203a73986943c50b8d2f0fdc9d09821705a1602.
    
    Reverted https://github.com/pytorch/pytorch/pull/76562 on behalf of https://github.com/janeyx99

--- aten/src/ATen/core/PythonModeTLS.cpp
+++ aten/src/ATen/core/PythonModeTLS.cpp
@@ -1,26 +1,26 @@
-#include <ATen/core/TorchDispatchModeTLS.h>
+#include <ATen/core/PythonModeTLS.h>
 #include <c10/core/SafePyObject.h>
 
 namespace at { namespace impl {
 
-thread_local std::shared_ptr<SafePyObject> torchDispatchModeState;
+thread_local std::shared_ptr<SafePyObject> pythonModeState;
 
-void TorchDispatchModeTLS::set_state(std::shared_ptr<SafePyObject> state) {
+void PythonModeTLS::set_state(std::shared_ptr<SafePyObject> state) {
   if (state) {
     c10::impl::tls_set_dispatch_key_included(DispatchKey::Python, true);
     c10::impl::tls_set_dispatch_key_included(DispatchKey::PythonTLSSnapshot, true);
   } else {
-    TorchDispatchModeTLS::reset_state();
+    PythonModeTLS::reset_state();
   }
-  torchDispatchModeState = std::move(state);
+  pythonModeState = std::move(state);
 }
 
-const std::shared_ptr<SafePyObject>& TorchDispatchModeTLS::get_state() {
-  return torchDispatchModeState;
+const std::shared_ptr<SafePyObject>& PythonModeTLS::get_state() {
+  return pythonModeState;
 }
 
-void TorchDispatchModeTLS::reset_state() {
-  torchDispatchModeState.reset();
+void PythonModeTLS::reset_state() {
+  pythonModeState.reset();
   c10::impl::tls_set_dispatch_key_included(DispatchKey::Python, false);
   c10::impl::tls_set_dispatch_key_included(DispatchKey::PythonTLSSnapshot, false);
 }
