0a40fe4c540084fc499b7746ac0c8f77ebc0dce5
Brian Hirsh <hirsheybar@fb.com>
2022-05-06 18:16:32 -0400

    [functorch] fix linear numeric accuracy for functorch, add test (#781)

--- functorch/functorch/csrc/PyTorchOperatorHacks.cpp
+++ functorch/functorch/csrc/PyTorchOperatorHacks.cpp
@@ -118,6 +118,12 @@ Tensor linear_hack(const Tensor& input, const Tensor& weight, const c10::optiona
     // Fused op is marginally faster.
     return at::addmm(*bias, input, weight.t());
   }
+  if (input.dim() == 3 && bias->defined() && input.is_contiguous()) {
+    // Also hit the fused path for contiguous 3D input.
+    const auto input_sizes = input.sizes();
+    const auto result = at::addmm(*bias, input.view({input_sizes[0] * input_sizes[1], input_sizes[2]}), weight.t());
+    return result.view({input_sizes[0], input_sizes[1], result.size(1)});
+  }
   auto output = at::matmul(input, weight.t());
   if (bias->defined()) {
     const auto& stack = getDynamicLayerStack();
