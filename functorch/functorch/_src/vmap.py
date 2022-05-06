5581bf4118a2ad038e8c3df08e03880999c38ff5
Horace He <chilli@fb.com>
2022-05-06 07:03:19 +0000

    [functorch] fix flake8 issues

--- functorch/functorch/_src/vmap.py
+++ functorch/functorch/_src/vmap.py
@@ -43,7 +43,7 @@ def _validate_and_get_batch_size(
         flat_args: List) -> int:
     batch_sizes = [arg.size(in_dim) for in_dim, arg in zip(flat_in_dims, flat_args)
                    if in_dim is not None]
-    if batch_sizes and any([size != batch_sizes[0] for size in batch_sizes]):
+    if batch_sizes and any(size != batch_sizes[0] for size in batch_sizes):
         raise ValueError(
             f'vmap: Expected all tensors to have the same size in the mapped '
             f'dimension, got sizes {batch_sizes} for the mapped dimension')
