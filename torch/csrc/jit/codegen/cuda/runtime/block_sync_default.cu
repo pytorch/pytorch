8d7102865ca9de50a01938702050fca03718313a
Jeff Daily <jeff.daily@amd.com>
2022-05-06 16:38:07 +0000

    use __syncthreads() for rocm

--- torch/csrc/jit/codegen/cuda/runtime/block_sync_default.cu
+++ torch/csrc/jit/codegen/cuda/runtime/block_sync_default.cu
@@ -6,7 +6,11 @@ __forceinline__ __device__ void init() {}
 
 // Thread-block synchronization
 __forceinline__ __device__ void sync() {
+#ifdef __HIP_PLATFORM_HCC__
+  __syncthreads();
+#else
   __barrier_sync(0);
+#endif
 }
 
 } // namespace block_sync
