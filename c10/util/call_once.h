dc3ae55aa0826a56352e52f2ee5c54af67612cb0
David Berard <dberard@fb.com>
2022-05-06 18:32:13 -0700

    Update on "[WIP] replace std::call_once"
    std::call_once has a bug in exception handling. This replaces it with a
    simple implementation using a mutex and a boolean.
    
    [ghstack-poisoned]

---
+++ c10/util/call_once.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <mutex>
+
+namespace c10 {
+
+struct once_flag {
+  std::mutex mutex_;
+  bool has_run_ = false;
+};
+
+// std::call_once has a bug in exception handling
+template<class Callable, class... Args>
+void call_once(once_flag& flag, Callable&& f, Args&&... args) {
+  std::lock_guard<flag.mutex_> lock;
+  if (flag.has_run_) {
+    return;
+  }
+  f(std::forward<Args>(args)...);
+  flag.has_run_ = true;
+}
+
+} // namespace c10
