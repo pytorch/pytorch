d6fd77a65cf0d711a7860a0a3dcfb7ba8fd40b01
David Berard <dberard@fb.com>
2022-05-09 16:03:32 -0700

    [JIT] fix opinfo utils to handle tensor kwargs
    Previously, the traced function would only take tensor args (and not tensor kwargs). Then the tensor kwargs would be inlined as constants into the graph during tracing, and changes to the kwarg inputs wouldn't be reflected in new calls to the traced function.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76768
    
    Approved by: https://github.com/mruberry

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -12396,9 +12396,6 @@ op_db: List[OpInfo] = [
             ),
             DecorateInfo(unittest.skip("Skipped!"), 'TestGradients', "test_fn_gradgrad", dtypes=(torch.float64,)),
             DecorateInfo(unittest.skip("Skipped!"), 'TestGradients', "test_fn_fwgrad_bwgrad", dtypes=(torch.float64,)),
-            # 76354
-            DecorateInfo(unittest.expectedFailure, 'TestCudaFuserOpInfo', 'test_nvfuser_extremal_values',
-                         dtypes=(torch.bfloat16, torch.float16, torch.float32, torch.float64)),
         ),
     ),
     OpInfo('nn.functional.relu',
@@ -13189,10 +13186,7 @@ op_db: List[OpInfo] = [
             # RuntimeError: Cannot insert a Tensor that requires grad as a constant.
             # Consider making it a parameter or input, or detaching the gradient
             # https://github.com/pytorch/pytorch/issues/68752
-            DecorateInfo(unittest.expectedFailure, 'TestJit', 'test_variant_consistency_jit'),
-            # 76354
-            DecorateInfo(unittest.expectedFailure, 'TestCudaFuserOpInfo', 'test_nvfuser_extremal_values',
-                         dtypes=(torch.float16, torch.float32, torch.float64)), ],
+            DecorateInfo(unittest.expectedFailure, 'TestJit', 'test_variant_consistency_jit'), ],
     ),
     UnaryUfuncInfo(
         'nn.functional.celu',
@@ -17652,9 +17646,6 @@ op_db: List[OpInfo] = [
             #     return torch.nn.functional.nll_loss(i0, i1, weight=tensor([8.4784, 1.7658, 4.3228], dtype=torch.float32))
             #                                                        ~~~~~~ <--- HERE
             DecorateInfo(unittest.skip("Skipped!"), "TestJit", "test_variant_consistency_jit", dtypes=(torch.float32,),),
-            # 76354
-            DecorateInfo(unittest.expectedFailure, 'TestCudaFuserOpInfo', 'test_nvfuser_extremal_values',
-                         dtypes=(torch.bfloat16, torch.float16, torch.float32, torch.float64)),
         ),
     ),
     OpInfo(
