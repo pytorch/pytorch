12a4b1b9bbf45149919b0a4d92bfa4f3adc35067
Rohit Goswami <rog32@hi.is>
2022-05-09 15:05:45 +0000

    TST: Move to more appropriate TestNN class
    Co-authored-by: kshitij12345 <kshitijkalambarkar@gmail.com>

--- test/test_nn.py
+++ test/test_nn.py
@@ -11090,13 +11090,6 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""")
         y = y.contiguous(memory_format=torch.contiguous_format)
         self.assertEqual(y, y_ref)
 
-    def test_channel_shuffle_runtime_errors(self):
-        # gh-76616: nn.ChannelShuffle will crash with empty input tensor
-        groups = 3
-        input_tensor = torch.rand([0, 9, 4, 4])
-        with self.assertRaisesRegex(RuntimeError, "nn.ChannelShuffle: Input tensor must not be empty"):
-            torch.nn.ChannelShuffle(groups)(input_tensor)
-
     def test_upsamplingLinear1d(self):
         for align_corners in [True, False]:
             for recompute_scale_factor in [True, False]:
@@ -14362,6 +14355,14 @@ class TestNNDeviceType(NNTestCase):
         if bias is not None:
             bias.requires_grad_(False)
         self.assertTrue(gradgradcheck(convolution, inputs, nondet_tol=gradcheck_nondet_tol))
+
+
+    def test_channel_shuffle_runtime_errors(self, device):
+        # gh-76616: nn.ChannelShuffle will crash with empty input tensor
+        groups = 3
+        input_tensor = torch.rand([0, 9, 4, 4], device=device)
+        with self.assertRaisesRegex(RuntimeError, "nn.ChannelShuffle: Input tensor must not be empty"):
+            torch.nn.ChannelShuffle(groups)(input_tensor)
 
     def test_Dropout(self, device):
         input = torch.empty(1000)
