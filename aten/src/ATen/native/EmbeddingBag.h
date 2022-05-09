6c81807af68aa3b2b1265fb7ab2a60f1796456a3
Max Podkorytov <maxdp@fb.com>
2022-05-09 16:41:26 -0700

    [static-runtime] add placeholder for kernel cache (#76209)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/76209
    
    Reviewed By: mikeiovine
    
    Differential Revision: D35771139
    
    fbshipit-source-id: e2d99e2bde7572ecfeb913addede49726513bc38

--- aten/src/ATen/native/EmbeddingBag.h
+++ aten/src/ATen/native/EmbeddingBag.h
@@ -38,13 +38,17 @@ void make_offset2bag_out(
     const c10::optional<Tensor>& per_sample_weights,
     const int64_t padding_idx = -1);
 
+struct _EmbeddingBagKernelCache {
+};
+
 void _embedding_bag_cpu_impl_out(Tensor& output, Tensor& offset2bag,
     Tensor& bag_size, Tensor* max_indices,
     const Tensor &weight, const Tensor &indices,
     const Tensor &offsets, const int64_t mode = 0,
     const c10::optional<Tensor>& per_sample_weights = c10::nullopt,
     bool include_last_offset = false,
-    int64_t padding_idx = -1);
+    int64_t padding_idx = -1,
+    _EmbeddingBagKernelCache* fbgemm_kernel_cache = nullptr);
 
 void _embedding_bag_cpu_out(
     at::Tensor& output,
@@ -59,6 +63,8 @@ void _embedding_bag_cpu_out(
     const bool sparse,
     const c10::optional<at::Tensor>& per_sample_weights,
     const bool include_last_offset,
-    const c10::optional<int64_t>& padding_idx);
+    const c10::optional<int64_t>& padding_idx,
+    _EmbeddingBagKernelCache* fbgemm_kernel_cache = nullptr);
+
 } // native
 } // at
