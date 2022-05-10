c33d79e0eacbd029276eb4059642e4225be09dba
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-05-10 18:20:32 +0000

    remove unused function

--- test/test_tensor_creation_ops.py
+++ test/test_tensor_creation_ops.py
@@ -20,7 +20,7 @@ from torch.testing._internal.common_device_type import (
     onlyCPU, largeTensorTest, precisionOverride, dtypes,
     onlyCUDA, skipCPUIf, dtypesIfCUDA, skipMeta, get_all_device_types)
 from torch.testing._internal.common_dtype import (
-    all_types_and_complex_and, get_all_math_dtypes, all_types_and, floating_and_complex_types,
+    all_types_and_complex_and, all_types_and, floating_and_complex_types,
     floating_types, floating_and_complex_types_and, integral_types_and, get_all_dtypes
 )
 from torch.testing._creation import float_to_corresponding_complex_type_map
