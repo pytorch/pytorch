2aaeb932923eb40bb55ef17b9abf0797f6dc5089
Yukio Siraichi <yukio.siraichi@gmail.com>
2022-05-10 19:04:30 +0900

    Update base for Restored ATen/core/DimVector.h on "Drop `set_output` function from `MetaBase` API."
    Fix #69813.
    
    This PR removes the definitions of the old `set_output` function, towards the newer
    `set_output_*` variations.
    
    [ghstack-poisoned]

--- aten/src/ATen/DimVector.h
+++ aten/src/ATen/DimVector.h
@@ -1,13 +1,2 @@
 #pragma once
-#include <c10/util/DimVector.h>
-
-namespace at {
-
-// Re-declaring 'DimVector' type and size inside 'at' namespace.
-// This is done to avoid modifying every use into their 'c10'
-// equivalent.
-
-constexpr size_t kDimVectorStaticSize = c10::kDimVectorStaticSize;
-using DimVector = c10::DimVector;
-
-} // namespace at
+#include <ATen/core/DimVector.h>
