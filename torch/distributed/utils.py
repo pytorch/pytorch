737483a6ee32da60bae40ec8b2b91a97b14691ad
Rohan Varma <rvarm1@fb.com>
2022-05-11 02:33:13 +0000

    Checkpoint wrapper state dict

--- torch/distributed/utils.py
+++ torch/distributed/utils.py
@@ -2,6 +2,7 @@ import collections
 
 import torch
 import torch.distributed as dist
+<<<<<<< HEAD
 from torch.nn.parallel._functions import _get_stream
 from torch.nn.parallel.scatter_gather import (  # type: ignore[attr-defined]
     is_namedtuple as _is_namedtuple
@@ -81,6 +82,9 @@ def _to_kwargs(inputs, kwargs, device_id, use_side_stream_for_tensor_copies):
     inputs = tuple(inputs)
     kwargs = tuple(kwargs)
     return inputs, kwargs
+=======
+from typing import Dict, Any
+>>>>>>> Checkpoint wrapper state dict
 
 def _verify_param_shape_across_processes(process_group, tensors, logger=None):
     return dist._verify_params_across_processes(process_group, tensors, logger)
@@ -111,3 +115,26 @@ def _sync_params_and_buffers(
         dist._broadcast_coalesced(
             process_group, module_states, broadcast_bucket_size, src
         )
+
+def _replace_by_prefix(
+    state_dict: Dict[str, Any],
+    old_prefix: str,
+    new_prefix: str,
+) -> None:
+    """
+    Replace all keys that match a given old_prefix with a new_prefix (in-place).
+
+    Usage::
+
+        state_dict = {"layer.xyz": torch.tensor(1)}
+        replace_by_prefix_(state_dict, "layer.", "module.layer.")
+        assert state_dict == {"module.layer.xyz": torch.tensor(1)}
+    """
+    if old_prefix == new_prefix:
+        raise ValueError("old_prefix and new_prefix must be distinct")
+    for key in list(state_dict.keys()):
+        if not key.startswith(old_prefix):
+            continue
+        new_key = new_prefix + key[len(old_prefix) :]
+        state_dict[new_key] = state_dict[key]
+        del state_dict[key]
