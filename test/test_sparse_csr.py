264fd6bc9907cebd10ef572a266771497037fdcb
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-05-11 12:09:07 +0000

    Sparse CSR: enable autograd for torch.sparse.addmm and torch.sparse.mm
    This PR updates the derivative rule for `torch.sparse.addmm` to be
    working with CSR sparse matrix. Notably `torch.sparse.sampled_addmm` is
    used in the backward function.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76591
    
    Approved by: https://github.com/cpuhrsch

--- test/test_sparse_csr.py
+++ test/test_sparse_csr.py
@@ -1748,6 +1748,11 @@ class TestSparseCSR(TestCase):
             dense_output.backward(dense_covector)
             self.assertEqual(sparse_input.grad, dense_input.grad)
 
+    @skipCUDAIfRocm
+    @skipCUDAIf(
+        not _check_cusparse_sddmm_available(),
+        "cuSparse Generic API SDDMM is not available"
+    )
     @dtypes(torch.float64)
     def test_autograd_dense_output_addmm(self, device, dtype):
         from torch.testing._internal.common_methods_invocations import sample_inputs_addmm
@@ -1760,8 +1765,7 @@ class TestSparseCSR(TestCase):
             raise ValueError("Expected at least one 2D tensor in samples to convert to sparse.")
 
         for sample in samples:
-            # TODO: Remove detach once we have autograd support for CSR input
-            a = sample.args[0].to_sparse_csr().detach()
+            a = sample.args[0].relu().to_sparse_csr()
 
             # This path tests the autograd path wrt dense inputs
             for addmm in [torch.addmm, torch.sparse.addmm]:
@@ -1779,35 +1783,42 @@ class TestSparseCSR(TestCase):
                 b = make_tensor(sample.args[1].shape, device=device, dtype=dtype, noncontiguous=True, requires_grad=True)
                 self.assertTrue(torch.autograd.gradcheck(fn, [c, b], fast_mode=True))
 
-            # Now test the autograd path wrt sparse inputs
-            # TODO: torch.sparse.addmm backward is not implemented for CSR
-            for reverse in [True, False]:
-                c, b = sample.input, sample.args[1]
-                if reverse and a.shape != b.shape:
-                    continue
+                # Now test the autograd path wrt sparse inputs
+                for reverse in [True, False]:
+                    c, b = sample.input, sample.args[1]
+                    if reverse and a.shape != b.shape:
+                        continue
 
-                def fn(a):
-                    inputs = (c, b, a) if reverse else (c, a, b)
-                    output = torch.addmm(*inputs, **sample.kwargs)
-                    if sample.output_process_fn_grad is not None:
-                        return sample.output_process_fn_grad(output)
-                    return output
+                    def fn(a):
+                        inputs = (c, b, a) if reverse else (c, a, b)
+                        output = addmm(*inputs, **sample.kwargs)
+                        if sample.output_process_fn_grad is not None:
+                            return sample.output_process_fn_grad(output)
+                        return output
 
-                # gradcheck doesn't work for sparse CSR yet, compare against dense path
-                # Compute sparse result
-                a.requires_grad_(True)
-                output = fn(a)
-                covector = torch.randn_like(output)
-                output.backward(covector)
-                self.assertTrue(torch.is_tensor(a.grad))
-                self.assertTrue(a.grad.layout == torch.strided)
+                    # gradcheck doesn't work for sparse CSR yet, compare against dense path
+                    # Compute sparse result
+                    a = a.detach().requires_grad_(True)
+                    output = fn(a)
+                    covector = torch.randn_like(output)
+                    output.backward(covector)
+                    self.assertTrue(torch.is_tensor(a.grad))
+                    if addmm == torch.sparse.addmm:
+                        self.assertTrue(a.grad.is_sparse_csr)
+                    else:
+                        self.assertTrue(a.grad.layout == torch.strided)
 
-                # Compute dense result and compare with sparse result
-                dense_a = a.detach().to_dense().requires_grad_(True)
-                dense_output = fn(dense_a)
-                dense_covector = covector.to_dense()
-                dense_output.backward(dense_covector)
-                self.assertEqual(a.grad, dense_a.grad)
+                    # Compute dense result and compare with sparse result
+                    dense_a = a.detach().to_dense().requires_grad_(True)
+                    dense_output = fn(dense_a)
+                    self.assertEqual(output, dense_output)
+                    dense_covector = covector.to_dense()
+                    dense_output.backward(dense_covector)
+
+                    if addmm == torch.sparse.addmm:
+                        self.assertEqual(a.grad, dense_a.grad.sparse_mask(a))
+                    else:
+                        self.assertEqual(a.grad, dense_a.grad)
 
     @skipCUDAIfRocm
     @skipCPUIfNoMklSparse
