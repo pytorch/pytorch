bad5467bb10828a59705bfe5fd06f07764403ad0
dzdang <dzdang@umich.edu>
2022-05-11 09:38:45 -0700

    Update on "[quant][core][gpu][feature] Implemented quantized cuda gelu"
    Summary:
    Support for quantized cuda gelu has been provided by using
    `dequantize -> fp32 cuda gelu kernel -> quantize`. Mathematically, this
    is not equivalent to doing int8 gelu, so we have opted for this approach
    for now. It might be possible to write a variant of the int8 gelu that's
    equivalent to `dequantize -> fp32 cuda gelu kernel -> quantize`, which
    can be a topic for future work.
    
    Test function `test_qgelu` was amended to test gelu for quantized cuda
    backends.
    
    Test Plan:
    ```
    python test/test_quantization.py -k test_qgelu
    ```
    
    Differential Revision: [D36302475](https://our.internmc.facebook.com/intern/diff/D36302475)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cudnn/Activation.cpp
+++ aten/src/ATen/native/quantized/cudnn/Activation.cpp
@@ -4,6 +4,10 @@
 namespace at {
 namespace native {
 
+// this kernel is currently implemented with dequantize -> fp32 gelu -> quantize, which is not equivalen to int8 gelu
+// It might be possible to write a variant of the int8 gelu that's equivalent to dequantize -> fp32 cuda gelu kernel -> quantize,
+// which can be a topic for future work. This is currently defined in the cuDNN directory because we may wish to change the
+// implementation to use cudNN's gelu pointwise op
 Tensor gelu_quantized_cuda(const Tensor& qx, c10::string_view approximate) {
   if (qx.numel() == 0) {
     return Tensor{};
