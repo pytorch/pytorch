25580cc0ce42e5cecc4ff4f945a15113126bafd4
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-05-11 12:40:57 +0000

    Make _csr_to_block_csr return sparse_bsr layout

--- test/test_sparse_csr.py
+++ test/test_sparse_csr.py
@@ -736,6 +736,7 @@ class TestSparseCSR(TestCase):
         st = sp.csr_matrix((t.values().cpu(), t.col_indices().cpu(), t.crow_indices().cpu()), shape=tuple(t.size()))
         block_t = torch.sparse._csr_to_block_csr(t, (blocksize, blocksize))
         self.assertEqual(block_t.values().dim(), 3)
+        self.assertTrue(block_t.layout == torch.sparse_bsr)
         block_st = st.tobsr(blocksize=(blocksize, blocksize))
         self.assertEqual(block_t.values().cpu(), block_st.data)
         self.assertEqual(block_t.col_indices().cpu(), torch.tensor(block_st.indices).to(index_dtype))
