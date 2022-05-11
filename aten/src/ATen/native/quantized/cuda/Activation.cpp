8c3a3ecc4e851311f9a96dd27fd69642136cd4d7
dzdang <dzdang@umich.edu>
2022-05-11 11:40:38 -0700

    Update on "[quant][core][gpu][feature] Implemented quantized cuda gelu"
    Summary:
    Support for quantized cuda gelu has been provided by using
    `dequantize -> fp32 cuda gelu kernel -> quantize`. Mathematically, this
    is not equivalent to doing int8 gelu, so we have opted for this approach
    for now. It might be possible to write a variant of the int8 gelu that's
    equivalent to `dequantize -> fp32 cuda gelu kernel -> quantize`, which
    can be a topic for future work.
    
    Test function `test_qgelu` was amended to test gelu for quantized cuda
    backends.
    
    Test Plan:
    ```
    python test/test_quantization.py -k test_qgelu
    ```
    
    Differential Revision: [D36302475](https://our.internmc.facebook.com/intern/diff/D36302475)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cuda/Activation.cpp
+++ aten/src/ATen/native/quantized/cuda/Activation.cpp
@@ -4,15 +4,13 @@
 namespace at {
 namespace native {
 
-// this kernel is currently implemented with dequantize -> fp32 gelu -> quantize, which is not equivalen to int8 gelu
+// this kernel is currently implemented with dequantize -> fp32 gelu -> quantize, which is not equivalent to int8 gelu
 // It might be possible to write a variant of the int8 gelu that's equivalent to dequantize -> fp32 cuda gelu kernel -> quantize,
-// which can be a topic for future work. This is currently defined in the cuDNN directory because we may wish to change the
-// implementation to use cudNN's gelu pointwise op
+// which can be a topic for future work.
 Tensor gelu_quantized_cuda(const Tensor& qx, c10::string_view approximate) {
   if (qx.numel() == 0) {
     return Tensor{};
   }
-  TORCH_CHECK(qx.qscheme() == at::kPerTensorAffine, "gelu_quantized_cuda only supports per tensor quantized tensors");
   auto x_fp32 = at::dequantize(qx);
   auto result_fp32 = at::gelu(x_fp32);
   return at::quantize_per_tensor(result_fp32, qx.q_scale(), qx.q_zero_point(), qx.scalar_type());
