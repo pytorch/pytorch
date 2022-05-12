Bugfix gradient formula for index_reduce('prod') + separate out sample_inputs for index_reduce
[ghstack-poisoned]

torch/testing/_internal/common_methods_invocations.py
@@ -5356,8 +5356,6 @@ def sample_inputs_index(op_info, device, dtype, requires_grad, **kwargs):
     copy = op_info.name == "index_copy"
     # target.index_fill(dim, idx, value)
     fill = op_info.name == "index_fill"
-    # target._index_reduce(dim, idx, source, reduce, *, include_self=True)
-    reduce_op = op_info.name == "index_reduce"
 
 
     make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=requires_grad)
@@ -5369,11 +5367,8 @@ def sample_inputs_index(op_info, device, dtype, requires_grad, **kwargs):
     shapes = [(), (1,), (S, S)]
     # extra parameter for add
     alphas = (-1, 0, 2) if add else (None,)
-    # extra parameters for reduce
-    include_selfs = (True, False) if reduce_op else (None,)
-    reduces = ('prod', 'mean', 'amin', 'amax') if reduce_op else (None,)
 
-    for shape, alpha, include_self, reduce in product(shapes, alphas, include_selfs, reduces):
+    for shape, alpha in product(shapes, alphas):
         t = make_arg(shape)
         args = []
 
@@ -5387,22 +5382,54 @@ def sample_inputs_index(op_info, device, dtype, requires_grad, **kwargs):
         args.append(idx)
 
         # source
-        if copy or add or reduce_op:
+        if copy or add:
             args.append(make_arg(shape))
         elif fill:
             # A weird number to catch errors
             args.append(make_arg((1,)).item())
 
-        # reduce
-        if reduce_op:
-            args.append(reduce)
-
         args = tuple(args)
         kwargs = {} if alpha is None else {"alpha": alpha}
-        if include_self is not None:
-            kwargs['include_self'] = include_self
 
         yield SampleInput(t, args=args, kwargs=kwargs)
+
+def sample_inputs_index_reduce(op_info, device, dtype, requires_grad, **kwargs):
+    make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=requires_grad)
+
+    def make_idx(n, m):
+        return make_tensor((n,), device=device, dtype=torch.int64, low=0, high=m)
+
+    shapes = [((), ()), ((1,), (1,)), ((S, S), (S, M)), ((S, S, S), (S, M, S))]
+    include_selfs = (True, False)
+    reduces = ('prod', 'mean', 'amin', 'amax')
+
+    for shape, include_self, reduce in product(shapes, include_selfs, reduces):
+        self_shape, src_shape = shape
+        # dim. We handle the scalar case
+        dim = 1 if len(self_shape) >= 2 else 0
+        idx = make_idx(src_shape[dim] if len(src_shape) != 0 else 1,
+                       self_shape[dim] if len(self_shape) != 0 else 1)
+        args = (dim, idx, make_arg(src_shape), reduce)
+        yield SampleInput(make_arg(self_shape),
+                          args=args,
+                          kwargs={'include_self' : include_self})
+
+    # Sample inputs to test edge cases for backward
+    if requires_grad:
+        # Check that gradients are propagated correctly for prod when zeros in self/src are reduced
+        # This sample tests gradients for the following cases
+        # (a) 1 zero reduced (from source (self[0, 1]), from self (self[0, 0]))
+        # (b) 2 zeros reduced (1 from src and 1 from self (self[1, 0], self[1, 1])
+        # (c) no zeros reduced (self[2, 1], self[2, 2])
+        # (d) 2 zeros reduced (both from src) is tested in test/test_autograd.py
+        #     test_scatter_index_reduce_prod_gradgrad_error as this case is not supported for gradgrad
+        input = torch.tensor([[0, 13], [0, 0], [15, 19]], dtype=dtype, device=device, requires_grad=requires_grad)
+        src = torch.tensor([[2, 0], [0, 0], [2, 3], [2, 2]], dtype=dtype, device=device, requires_grad=requires_grad)
+        idx = torch.tensor([0, 1, 2, 0], dtype=torch.long, device=device)
+
+        yield SampleInput(input,
+                          args=(0, idx, src, 'prod'),
+                          kwargs={'include_self': True})
 
 def sample_inputs_mode(op_info, device, dtype, requires_grad, **kwargs):
     inputs = []
@@ -7670,23 +7697,12 @@ def sample_inputs_scatter_reduce(op_info, device, dtype, requires_grad, **kwargs
         # (b) 2 zeros reduced (1 from src and 1 from self (self[1, 0])
         # (c) no zeros reduced (self([2, 1]))
         # (d) 2 zeros reduced (both from src) is tested in test/test_autograd.py
-        #     test_scatter_reduce_prod_gradgrad_error as this case is not supported for gradgrad
+        #     test_scatter_index_reduce_prod_gradgrad_error as this case is not supported for gradgrad
         input = torch.tensor([[0, 13], [0, 17], [0, 19]], dtype=dtype, device=device, requires_grad=requires_grad)
         src = torch.tensor([[0, 1, 2, 3], [0, 4, 0, 1], [2, 3, 5, 6]], dtype=dtype, device=device, requires_grad=requires_grad)
         idx = torch.tensor([[1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]], dtype=torch.long, device=device)
 
         yield SampleInput(input,
-                          args=(1, idx, src, reduce),
-                          kwargs={'include_self': True})
-
-
-    # Check that gradients are evenly distributed for amax/amin
-    if requires_grad and (reduce in ['amax', 'amin']):
-        input = _tensor((M, S))
-        src = input.clone().detach_().requires_grad_(requires_grad)
-        idx = torch.arange(S).expand((M, S)).to(dtype=torch.long, device=device)
-
-        yield SampleInput(_tensor((M, S)),
                           args=(1, idx, src, reduce),
                           kwargs={'include_self': True})
 
@@ -15320,7 +15336,7 @@ op_db: List[OpInfo] = [
     OpInfo('index_reduce',
            dtypes=floating_types_and(torch.float16, torch.bfloat16),
            supports_out=True,
-           sample_inputs_func=sample_inputs_index),
+           sample_inputs_func=sample_inputs_index_reduce),
     OpInfo('__getitem__',
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
            supports_out=False,
@@ -17916,7 +17932,6 @@ op_db: List[OpInfo] = [
         # and scatter_reduce hasn't been added to the whitelist in gen_variable_type yet
         dtypes=all_types_and(torch.float16, torch.bfloat16, torch.bool),
         dtypesIfCUDA=floating_types_and(torch.float16, torch.bfloat16),
-        # FIXME: vmap segfaults in _remove_batch_dim after node logic was added to scatter_reduce_backward(prod)
         check_batched_grad=False,
         sample_inputs_func=sample_inputs_scatter_reduce,
     ),