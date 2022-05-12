7143f06b1ecb30e0eb2e2db3b104c421194c8ed5
Brian Hirsh <hirsheybar@fb.com>
2022-05-12 10:15:30 -0700

    [functorch] Revert "fix functioanlization tests"
    This reverts commit e2cc167bab268ab43d9bc9b204735d963c4c6b52.

--- functorch/test/test_eager_transforms.py
+++ functorch/test/test_eager_transforms.py
@@ -2828,6 +2828,8 @@ class TestFunctionalize(TestCase):
         self.assertEqual(inpt1, inpt2)
         self.assertEqual(inpt1, inpt3)
 
+    # BUG: RuntimeError: Tensors of type FunctionalTensorWrapper do not have strides
+    @unittest.expectedFailure
     def test_simple_view(self, device):
 
         def f(x: torch.Tensor) -> torch.Tensor:
@@ -2837,6 +2839,8 @@ class TestFunctionalize(TestCase):
             return x
         self._check_functionalize_correctness(f, torch.zeros(4, 2, device=device))
 
+    # BUG: RuntimeError: Tensors of type FunctionalTensorWrapper do not have strides
+    @unittest.expectedFailure
     def test_multioutput_view(self, device):
 
         def f(x: torch.Tensor) -> torch.Tensor:
@@ -2848,6 +2852,8 @@ class TestFunctionalize(TestCase):
         self._check_functionalize_correctness(f, torch.zeros(4, 2, device=device))
 
 
+    # BUG: RuntimeError: Tensors of type FunctionalTensorWrapper do not have strides
+    @unittest.expectedFailure
     def test_inplace_view(self, device):
 
         def f(x: torch.Tensor) -> torch.Tensor:
@@ -2873,6 +2879,8 @@ class TestFunctionalize(TestCase):
         out_actual = functionalize(f)(x, y, z)
         self.assertEqual(out_expected, out_actual)
 
+    # BUG: RuntimeError: Tensors of type FunctionalTensorWrapper do not have strides
+    @unittest.expectedFailure
     def test_multioutput_inplace_slice_view(self, device):
 
         def f(x: torch.Tensor) -> torch.Tensor:
