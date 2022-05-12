795565987a98f39c1d58734087c97e2d2511ebd6
John Clow <jclow@fb.com>
2022-05-12 15:56:41 -0700

    Adding support for upper and lower bound functions in SSA
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/symbolic_shape_registry.cpp
+++ torch/csrc/jit/runtime/symbolic_shape_registry.cpp
@@ -72,6 +72,11 @@ static const OperatorMap<std::string>& conditionally_defined_ops() {
 std::unordered_map<const FunctionSchema*, std::shared_ptr<Graph>>
     cached_schema_to_graph;
 
+std::unordered_map<
+    const FunctionSchema*,
+    std::pair<std::shared_ptr<Graph>, std::shared_ptr<Graph>>>
+    cached_bounded_schema_to_graph;
+
 // CompilationUnit that holds all these Functions and keeps them alive.
 auto compilation_unit = std::make_shared<CompilationUnit>();
 
@@ -237,34 +242,58 @@ void transformShapeFunction(
   }
 }
 
+std::shared_ptr<Graph> addFunctionToCache(
+    const FunctionSchema* schema_string,
+    const std::string& shape_compute_function_name,
+    std::unordered_map<std::string, std::shared_ptr<Graph>>& reused_functions,
+    const CompilationUnit& module) {
+  // TODO: When elias edits transformShapeFunction, remove the unnecessary
+  // schema_string arg.
+  std::shared_ptr<Graph> graph;
+  if (reused_functions.count(shape_compute_function_name)) {
+    graph = reused_functions[shape_compute_function_name];
+  } else
+
+  {
+    Function& shape_compute_function =
+        module.get_function(shape_compute_function_name);
+    graph = toGraphFunction(shape_compute_function).graph();
+
+    transformShapeFunction(schema_string, graph);
+    // NB: we lint the shape functions registered in source
+    // in a test file
+    // LintShapeComputeGraph(schema_string, graph);
+
+    reused_functions[shape_compute_function_name] = graph;
+  }
+  // allow extra unused arguments to map multiple functions to e.g. unary
+  TORCH_INTERNAL_ASSERT(
+      graph->inputs().size() <= schema_string->arguments().size());
+  return graph;
+}
+
 void registerSchema(
     const FunctionSchema* schema_string,
     const std::string& shape_compute_function_name,
     std::unordered_map<std::string, std::shared_ptr<Graph>>& reused_functions,
     const CompilationUnit& module) {
-  if (reused_functions.count(shape_compute_function_name)) {
-    auto graph = reused_functions[shape_compute_function_name];
-
-    // allow extra unused arguments to map multiple functions to e.g. unary
-    TORCH_INTERNAL_ASSERT(
-        graph->inputs().size() <= schema_string->arguments().size());
-
-    cached_schema_to_graph[schema_string] = graph;
-    return;
-  }
-
-  Function& shape_compute_function =
-      module.get_function(shape_compute_function_name);
-  std::shared_ptr<Graph> graph =
-      toGraphFunction(shape_compute_function).graph();
-
-  transformShapeFunction(schema_string, graph);
-  // NB: we lint the shape functions registered in source
-  // in a test file
-  // LintShapeComputeGraph(schema_string, graph);
+  auto graph = addFunctionToCache(
+      schema_string, shape_compute_function_name, reused_functions, module);
 
   cached_schema_to_graph[schema_string] = graph;
-  reused_functions[shape_compute_function_name] = graph;
+}
+
+void registerBoundedSchema(
+    const FunctionSchema* schema_string,
+    const std::string& lower_bound_function_name,
+    const std::string& upper_bound_function_name,
+    std::unordered_map<std::string, std::shared_ptr<Graph>>& reused_functions,
+    const CompilationUnit& module) {
+  auto lower_graph = addFunctionToCache(
+      schema_string, lower_bound_function_name, reused_functions, module);
+  auto upper_graph = addFunctionToCache(
+      schema_string, upper_bound_function_name, reused_functions, module);
+  cached_bounded_schema_to_graph[schema_string] = {lower_graph, upper_graph};
 }
 
 void loadModule(const CompilationUnit& module) {
@@ -303,6 +332,20 @@ void loadModule(const CompilationUnit& module) {
             module);
       }
     }
+  }
+
+  // Now register the bounded schemas
+  for (const auto& pair : GetBoundedShapeMappings().getAllKeysAndValues()) {
+    const FunctionSchema* schema_string = &pair.first->schema();
+    const std::string& lower_bound_function_name = pair.second.first;
+    const std::string& upper_bound_function_name = pair.second.second;
+
+    registerBoundedSchema(
+        schema_string,
+        lower_bound_function_name,
+        upper_bound_function_name,
+        reused_functions,
+        module);
   }
 }
 
