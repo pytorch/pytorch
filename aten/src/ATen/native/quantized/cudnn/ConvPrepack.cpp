88a19f2d53e8a1a1790c0bda9fe89dd18b899f55
dzdang <dzdang@umich.edu>
2022-05-13 11:37:31 -0700

    Update on "[quant][core][better-engineering] Rename files in quantized/cudnn directory to conform with non-quantized countertpart filenames"
    Summary:
    Names of analogous files in quantized directory (previously snake case) were inconsistent with
    their non-quantized filename counterparts (pascal case). This is the third of a series of PRs that changes
    all files in quantized (and sub-directories) dir to have pascal case.
    
    Some files have not been renamed as it is causing issues related to
    custom class with import torch at runtime.
    
    Test Plan:
    ```
    python test/test_quantization.py
    ```
    
    Differential Revision: [D36381761](https://our.internmc.facebook.com/intern/diff/D36381761)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/cudnn/ConvPrepack.cpp
+++ aten/src/ATen/native/quantized/cudnn/ConvPrepack.cpp
@@ -9,7 +9,7 @@
 
 #include <ATen/ATen.h>
 #include <torch/library.h>
-#include <ATen/native/quantized/cpu/quant_Utils.h>
+#include <ATen/native/quantized/cpu/quant_utils.h>
 #include <ATen/native/quantized/cudnn/Utils.h>
 #include <ATen/native/quantized/packed_params.h>
 #include <ATen/quantized/Quantizer.h>
