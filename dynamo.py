2624a862c6aab6627524439487fa698fb9c5ff30
Jiewen Tan <jwtan@fb.com>
2022-05-13 19:30:41 +0000

    [c10d] Add a dynamo example to capture broadcast
    Summary:
    This commit adds an example to demonstrate that dynamo can capture
    c10d::broadcast.
    
    Test Plan:
    python dynamo.py
    
    [ghstack-poisoned]

---
+++ dynamo.py
@@ -0,0 +1,58 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+import torchdynamo
+
+from typing import List
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def broadcast(x):
+    dist.broadcast(x, 0)
+    print(f"{os.getpid()} broadcast: {x}")
+    return x
+
+def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
+    print("my_compiler() called with FX graph:")
+    gm.graph.print_tabular()
+    return gm.forward  # return a python callable
+
+def demo_basic(rank, world_size):
+    # without flushing, mp won't print this message if crash.
+    print(f"Running basic DDP example on rank {rank}, and process id: {os.getpid()}", flush=True)
+    setup(rank, world_size)
+
+    with torchdynamo.optimize(my_compiler):
+      device = torch.device("cuda", dist.get_rank())
+      x = torch.zeros(2, 3).to(device)
+      if device.index == 0:
+          x = torch.ones(2, 3).to(device)
+      ref = broadcast(x)
+
+    cleanup()
+
+
+def run_demo(demo_fn, world_size):
+    # it won't print exception messages in the child process.
+    mp.spawn(demo_fn,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
+
+if __name__ == "__main__":
+    print(f"main process id: {os.getpid()}")
+    n_gpus = torch.cuda.device_count()
+    assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
+    world_size = n_gpus
+    run_demo(demo_basic, world_size)
+    # demo_basic(0,1)
