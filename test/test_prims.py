d55c2816e359ab2ce7e37cc1e5cedefaf2d86926
jjsjann123 <jiej@nvidia.com>
2022-05-13 17:15:32 -0700

    adding import

--- test/test_prims.py
+++ test/test_prims.py
@@ -8,6 +8,7 @@ from torch.testing._internal.common_utils import run_tests, TestCase
 from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
     onlyCUDA,
+    skipCUDAIfRocm,
     dtypes,
 )
 from torch.testing._internal.logging_tensor import LoggingTensor, capture_logs, log_input
