a8811cf177ec1499b332455c7409216bb985cc19
akg91 <developerakash425@gmail.com>
2022-05-15 19:45:34 +0530

    To reduce no of lines from code

--- torch/__init__.py
+++ torch/__init__.py
@@ -1,12 +1,8 @@
 
 r"""
-The torch package contains data structures for multi-dimensional
-tensors and defines mathematical operations over these tensors.
-Additionally, it provides many utilities for efficient serializing of
-Tensors and arbitrary types, and other useful utilities.
-
-It has a CUDA counterpart, that enables you to run your tensor computations
-on an NVIDIA GPU with compute capability >= 3.0.
+The torch package contains data structures for multi-dimensional,tensors and defines mathematical operations over these tensors.
+Additionally, it provides many utilities for efficient serializing of tensors and arbitrary types, and other useful utilities.
+It has a CUDA counterpart, that enables you to run your tensor computations on an NVIDIA GPU with compute capability >= 3.0.
 """
 
 import os
