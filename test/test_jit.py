17816172c409ae660f726f5f2127e3f32326fd9b
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2022-05-16 10:54:34 -0700

    Update on "Get rid of ENABLE_UPGRADERS macro"
    Since it's been a while after we merged the upgrader design and we haven't encountered any issues, let's get rid of the macro for safe rollout
    
    [ghstack-poisoned]

--- test/test_jit.py
+++ test/test_jit.py
@@ -70,7 +70,6 @@ from jit.test_attr import TestGetDefaultAttr  # noqa: F401
 from jit.test_aten_pow import TestAtenPow  # noqa: F401
 from jit.test_optimize_for_mobile_preserve_debug_info import TestOptimizeForMobilePreserveDebugInfo  # noqa: F401
 from jit.test_union import TestUnion  # noqa: F401
-from jit.test_legacy_upgraders import TestLegacyUpgraders  # noqa: F401
 from jit.test_models import MnistNet
 from jit.test_batch_mm import TestBatchMM  # noqa: F401
 from jit.test_dtype_analysis import TestDtypeAnalysis, TestDtypeCustomRulesCPU  # noqa: F401
