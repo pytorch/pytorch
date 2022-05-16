9cb5d033c1c03e5287df725df50329aa7f00670c
Linbin Yu <linbin@fb.com>
2022-05-16 16:54:01 -0700

    fix glog

--- scripts/buck_setup.sh
+++ scripts/buck_setup.sh
@@ -19,10 +19,11 @@ tar -xf /tmp/bazel-skylib-1.0.2.tar.gz -C bazel-skylib/
 # glog
 printf "\n[Downloading glog-0.4.0]\n"
 curl -L -o /tmp/glog-0.4.0.tar.gz https://github.com/google/glog/archive/v0.4.0.tar.gz
-tar -xf /tmp/glog-0.4.0.tar.gz -C glog/
+tar -xf /tmp/glog-0.4.0.tar.gz -C /tmp/
+mv /tmp/glog-0.4.0/ glog/
 
 # ruy
 printf "\n[Downloading ruy]\n"
 curl -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
-unzip /tmp/ruy.zip -d /tmp/
+unzip -q /tmp/ruy.zip -d /tmp/
 mv /tmp/ruy-a09683b8da7164b9c5704f88aef2dc65aa583e5d ruy/
