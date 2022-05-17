da58581770dc73e22b0994b5fb2e823f16a90259
Brian Hirsh <hirsheybar@fb.com>
2022-05-17 14:13:46 +0000

    fix torch.jit.tracing for at::lift (#77588)
    After adding the `at::lift` op, it started getting traced during `torch.jit.trace`. We don't want that to happen for BC reasons
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77588
    Approved by: https://github.com/ezyang

--- torch/csrc/utils/tensor_new.cpp
+++ torch/csrc/utils/tensor_new.cpp
@@ -330,6 +330,9 @@ Tensor internal_new_from_data(
     tensor = tensor.to(device, inferred_scalar_type, /*non_blocking=*/false, /*copy=*/false);
   }
 
+  // torch.jit.trace will continue to trace out `.to()` instead of `.lift()`, since
+  // changing it is BC-breaking.
+  at::tracer::impl::NoTracerDispatchMode tracer_guard;
   return tensor.lift();
 }
 
