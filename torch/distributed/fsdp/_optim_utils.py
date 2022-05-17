7caa33aae20d7a0c2e5e3a17a4df7b756b72962f
Chien-Chin Huang <chienchin@fb.com>
2022-05-17 00:05:46 -0700

    Update on "[FSDP] Implement sharded_optim_state_dict and flatten_sharded_optim_state_dict."
    As title
    
    Differential Revision: [D36436496](https://our.internmc.facebook.com/intern/diff/D36436496/)
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/_optim_utils.py
+++ torch/distributed/fsdp/_optim_utils.py
@@ -228,7 +228,7 @@ def _unflatten_communicated_optim_state(
             optim_state: Union[torch.Tensor, ShardedTensor] = next(param_views)
             if shard_state:
                 optim_state = _distributed_chunk_tensor(
-                    optiom_state,
+                    optim_state,
                     fsdp_module.rank,
                     fsdp_module.world_size,
                     fsdp_module.process_group
