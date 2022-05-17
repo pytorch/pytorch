1ad55f1857b4def40a42fbaf653d6e8de9060dcd
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2022-05-17 14:43:04 -0700

    jit dispatch?

--- torch/csrc/jit/runtime/static/generated_ops.cpp
+++ torch/csrc/jit/runtime/static/generated_ops.cpp
@@ -4878,7 +4878,7 @@ REGISTER_OPERATOR_FUNCTOR(
     aten_linalg_svdvals,
     [](Node* n) -> SROperator {
       if (n->matches(
-              torch::schema("aten::linalg_svdvals(Tensor A) -> Tensor"))) {
+              torch::schema("aten::linalg_svdvals(Tensor A, *, str? driver=None) -> Tensor"))) {
         return [](ProcessedNode* p_node) {
           const auto& A = p_node->Input(0).toTensor();
           if (p_node->Output(0).isNone()) {
@@ -4887,7 +4887,7 @@ REGISTER_OPERATOR_FUNCTOR(
           }
           auto& out = p_node->Output(0).toTensor();
           fastResizeToZero(out);
-          at::native::linalg_svdvals_out(A, out);
+          at::native::linalg_svdvals_out(A, c10::nullopt, out);
         };
       }
       LogAndDumpSchema(n);
