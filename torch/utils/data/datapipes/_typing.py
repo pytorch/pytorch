520b021e78f235e425bebb3362350131d294bb11
Kevin Tse <ktse@fb.com>
2022-05-17 16:07:02 -0400

    [DataPipe] Enforcing single valid iterator for IterDataPipes multiple DataPipes as outputs
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75995
    
    Approved by: https://github.com/VitalyFedyunin

--- torch/utils/data/datapipes/_typing.py
+++ torch/utils/data/datapipes/_typing.py
@@ -380,6 +380,7 @@ def _generate_iterdatapipe_msg(datapipe):
 def _check_iterator_valid(datapipe, iterator_id, next_method_exists=False) -> None:
     r"""
     Given an instance of a DataPipe and an iterator ID, check if the IDs match, and if not, raises an exception.
+    In the case of ChildDataPipe, the ID gets compared to the one stored in `main_datapipe` as well.
     """
     msg = ("This iterator has been invalidated because another iterator has been created"
            f"from the same IterDataPipe: {_generate_iterdatapipe_msg(datapipe)}\n"
@@ -395,7 +396,13 @@ def _check_iterator_valid(datapipe, iterator_id, next_method_exists=False) -> No
             extra_msg = "\nNote that this exception is raised inside your IterDataPipe's a `__next__` method"
             raise RuntimeError(msg + extra_msg + feedback_msg)
     elif hasattr(datapipe, "_is_child_datapipe") and datapipe._is_child_datapipe is True:
-        pass  # TODO: Add logic for multiple ChildDataPipe
+        if hasattr(datapipe, "_check_valid_iterator_id"):
+            if not datapipe._check_valid_iterator_id(iterator_id):
+                raise RuntimeError("This iterator has been invalidated, because a new iterator has been created "
+                                   f"from one of the ChildDataPipes of "
+                                   f"{_generate_iterdatapipe_msg(datapipe.main_datapipe)}." + feedback_msg)
+        else:
+            raise RuntimeError("ChildDataPipe must have method `_check_valid_iterator_id`.")
     elif datapipe._valid_iterator_id != iterator_id:
         raise RuntimeError(msg + feedback_msg)
 
@@ -404,10 +411,16 @@ def _set_datapipe_valid_iterator_id(datapipe):
     r"""
     Given a DataPipe, updates its valid iterator ID.
     """
-    if datapipe._valid_iterator_id is None:
-        datapipe._valid_iterator_id = 0
+    if hasattr(datapipe, "_is_child_datapipe") and datapipe._is_child_datapipe is True:
+        if hasattr(datapipe, "_set_main_datapipe_valid_iterator_id"):
+            datapipe._set_main_datapipe_valid_iterator_id()  # reset() is called within this method when appropriate
+        else:
+            raise RuntimeError("ChildDataPipe must have method `_set_main_datapipe_valid_iterator_id`.")
     else:
-        datapipe._valid_iterator_id += 1
+        if datapipe._valid_iterator_id is None:
+            datapipe._valid_iterator_id = 0
+        else:
+            datapipe._valid_iterator_id += 1
     return datapipe._valid_iterator_id
 
 
