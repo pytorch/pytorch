fe1bf8a136286eaebde9c75cfe933d6b7ef6db3b
Zafar <cc.rafaz@zafar.cc>
2022-05-16 23:35:51 -0700

    Update base for Update on "[quant][ao_migration] `torch.nn.quantized.dynamic` → `torch.ao.nn.quantized.dynamic`."
    Context: In order to avoid the cluttering of the `torch.nn` namespace
    the quantized modules namespace is moved to `torch.ao.nn`.
    
    The list of the `nn.quantized` files that are being migrated:
    
    - [ ] `torch.nn.quantized` → `torch.ao.nn.quantized`
        - [X] `torch.nn.quantized.functional` → `torch.ao.nn.quantized.functional`
        - [X] `torch.nn.quantized.modules` → `torch.ao.nn.quantized.modules`
        - [X] [Current PR] `torch.nn.quantized.dynamic` → `torch.ao.nn.quantized.dynamic`
        - [ ] `torch.nn.quantized._reference` → `torch.ao.nn.quantized._reference`
    - [ ] `torch.nn.quantizable` → `torch.ao.nn.quantizable`
    - [ ] `torch.nn.qat` → `torch.ao.nn.qat`
        - [ ] `torch.nn.qat.modules` → `torch.ao.nn.qat.modules`
        - [ ] `torch.nn.qat.dynamic` → `torch.ao.nn.qat.dynamic`
    - [ ] `torch.nn.intrinsic` → `torch.ao.nn.intrinsic`
        - [ ] `torch.nn.intrinsic.modules` → `torch.ao.nn.intrinsic.modules`
        - [ ] `torch.nn.intrinsic.qat` → `torch.ao.nn.intrinsic.qat`
        - [ ] `torch.nn.intrinsic.quantized` → `torch.ao.nn.intrinsic.quantized`
        - [ ] `torch.nn.intrinsic.quantized` → `torch.ao.nn.intrinsic.quantized`
            - [ ] `torch.nn.intrinsic.quantized.modules` → `torch.ao.nn.intrinsic.quantized.modules`
            - [ ] `torch.nn.intrinsic.quantized.dynamic` → `torch.ao.nn.intrinsic.quantized.dynamic`
    
    Majority of the files are just moved to the new location.
    However, specific files need to be double checked:
    
    - [Documentation](docs/source/quantization-support.rst) vkuzo
    - [Public API test list](test/allowlist_for_publicAPI.json) peterbell10
    - [BC test](test/quantization/bc/test_backward_compatibility.py) vkuzo
    - [IR emitter](torch/csrc/jit/frontend/ir_emitter.cpp) jamesr66a
    - [JIT serialization](torch/csrc/jit/serialization/import_source.cpp) @IvanKobzarev jamesr66a
    
    [ghstack-poisoned]

--- torch/nn/quantized/functional.py
+++ torch/nn/quantized/functional.py
@@ -7,4 +7,4 @@ Note::
     Please, use the `torch.ao.nn.quantized.functional` instead.
 """
 
-from torch.ao.nn.quantized.functional import * # noqa: F401,F403
+from torch.ao.nn.quantized.functional import *  # noqa: F401,F403
