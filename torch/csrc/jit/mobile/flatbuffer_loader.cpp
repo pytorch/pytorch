78b82537f9ec4ac1cef720dbc7d662b11a225028
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-05-17 01:35:05 +0000

    Revert "Python function to extract information on mobile::Module from flatbuffer (#77328)"
    This reverts commit 69fa49f1230f80d1a0667e0a6ac8aca2746431b6.
    
    Reverted https://github.com/pytorch/pytorch/pull/77328 on behalf of https://github.com/atalman

--- torch/csrc/jit/mobile/flatbuffer_loader.cpp
+++ torch/csrc/jit/mobile/flatbuffer_loader.cpp
@@ -240,6 +240,8 @@ std::unique_ptr<mobile::Function> FlatbufferLoader::parseFunction(
     function->append_constant(getIValue(i));
   }
 
+  std::unordered_set<std::string> unsupported_op_names;
+
   appendUpgraderFunctions(function.get());
   // 2. Decides if upgrader is needed
   const uint32_t operator_version = module_->operator_version();
@@ -252,13 +254,19 @@ std::unique_ptr<mobile::Function> FlatbufferLoader::parseFunction(
       num_args = op->num_args_serialized();
     }
 
-    function->append_operator(
+    auto op_found = function->append_operator(
         op->name()->str(), op->overload_name()->str(), num_args);
+
+    if (!op_found) {
+      unsupported_op_names.emplace(
+          op->name()->str() + "/" + op->overload_name()->str());
+    }
   }
 
-  if (should_load_operators_) {
-    function->initialize_operators(true);
-  }
+  TORCH_CHECK(
+      unsupported_op_names.empty(),
+      "Unsupported ops: ",
+      c10::Join(", ", unsupported_op_names));
 
   for (const auto i : *method->type_annotations()) {
     function->append_type(getOrCreateTypeAnnotations(i));
@@ -715,14 +723,6 @@ uint64_t get_bytecode_version(const std::string& filename) {
       "Format error");
   auto* flatbuffer_module = mobile::serialization::GetMutableModule(data.get());
   return flatbuffer_module->bytecode_version();
-}
-
-mobile::ModuleInfo get_module_info_from_flatbuffer(char* flatbuffer_content) {
-  auto* ff_module = mobile::serialization::GetMutableModule(flatbuffer_content);
-  FlatbufferLoader loader;
-  loader.setShouldLoadOperators(false);
-  mobile::Module m = loader.parseModule(ff_module);
-  return mobile::get_module_info(m);
 }
 
 } // namespace jit
