f162cb066f2938cf9cd2d702c24b9bd9ad98f3ce
George Qi <georgeqi94@gmail.com>
2022-05-18 21:08:56 +0000

    MHA forward pass bug fix
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77761
    
    Approved by: https://github.com/jbschlosser

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -1066,9 +1066,9 @@ class MultiheadAttention(Module):
             # enforce that the dtypes all match or test cases where
             # they don't!
             why_not_fast_path = "non-self attention was used (query, key, and value are not the same Tensor)"
-        elif query.dtype != self.in_proj_bias.dtype:
+        elif self.in_proj_bias is not None and query.dtype != self.in_proj_bias.dtype:
             why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_bias ({self.in_proj_bias.dtype}) don't match"
-        elif query.dtype != self.in_proj_weight.dtype:
+        elif self.in_proj_weight is not None and query.dtype != self.in_proj_weight.dtype:
             # this case will fail anyway, but at least they'll get a useful error message.
             why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_weight ({self.in_proj_weight.dtype}) don't match"
         elif self.training:
