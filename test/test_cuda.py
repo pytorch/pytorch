e6470e1d2387ffc680f44fce39234a21e2e0db0d
Michael Carilli <mcarilli@gmail.com>
2022-05-18 14:42:00 -0600

    Explicitly set stream for new test

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -3131,10 +3131,13 @@ torch.cuda.synchronize()
         self.assertFalse(torch.cuda.is_current_stream_capturing())
 
         if (TEST_CUDA and (not TEST_WITH_ROCM) and int(torch.version.cuda.split(".")[0]) >= 11):
-            g = torch.cuda.CUDAGraph()
-            g.capture_begin()
-            self.assertTrue(torch.cuda.is_current_stream_capturing())
-            g.capture_end()
+            s = torch.cuda.Stream()
+            with torch.cuda.stream(s):
+                g = torch.cuda.CUDAGraph()
+                self.assertFalse(torch.cuda.is_current_stream_capturing())
+                g.capture_begin()
+                self.assertTrue(torch.cuda.is_current_stream_capturing())
+                g.capture_end()
 
     @unittest.skipIf((not TEST_CUDA) or
                      TEST_WITH_ROCM or
