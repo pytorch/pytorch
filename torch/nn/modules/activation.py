94039cb3e6d5be3c7a01c5cb19e295c249657e61
George Qi <georgeqi94@gmail.com>
2022-05-18 18:04:43 +0000

    Update on "MHA forward pass bug fix"
    [ghstack-poisoned]

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -1068,7 +1068,7 @@ class MultiheadAttention(Module):
             why_not_fast_path = "non-self attention was used (query, key, and value are not the same Tensor)"
         elif self.in_proj_bias is not None and query.dtype != self.in_proj_bias.dtype:
             why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_bias ({self.in_proj_bias.dtype}) don't match"
-        elif self.in_proj_bias is not None and query.dtype != self.in_proj_weight.dtype:
+        elif self.in_proj_weight is not None and query.dtype != self.in_proj_weight.dtype:
             # this case will fail anyway, but at least they'll get a useful error message.
             why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_weight ({self.in_proj_weight.dtype}) don't match"
         elif self.training:
