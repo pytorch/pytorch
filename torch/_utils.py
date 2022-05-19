a76edc26f98830309e472b50e47e730c85399ea3
Justin W. Lin <software@justinwlin.com>
2022-05-18 19:54:24 -0700

    [DOCS] Add docstring to _get_async_or_non_blocking() in _utils.py

--- torch/_utils.py
+++ torch/_utils.py
@@ -80,6 +80,13 @@ def _cuda(self, device=None, non_blocking=False, **kwargs):
 
 
 def _get_async_or_non_blocking(function_name, non_blocking, kwargs):
+    """ Return the non-blocking flag given the function name and kwargs.
+    
+    Args:
+        function_name (str): the name of the function being used.
+        non_blocking (bool): the default value.
+        **kwargs (dict): the kwargs passed to the function.
+    """
     if not kwargs:
         return non_blocking
     if len(kwargs) != 1 or 'async' not in kwargs:
