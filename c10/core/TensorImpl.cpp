8a9760c2d347f333966e319ccc69a68276176575
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-05-19 17:07:54 +0000

    Revert "add slow path for is_contiguous"
    This reverts commit f6beda89c6acbb92ff7f82699b9ea4c5c7428a19.
    
    Reverted https://github.com/pytorch/pytorch/pull/77396 on behalf of https://github.com/malfet

--- c10/core/TensorImpl.cpp
+++ c10/core/TensorImpl.cpp
@@ -350,16 +350,6 @@ void TensorImpl::throw_storage_access_error() const {
 }
 
 bool TensorImpl::is_contiguous_custom(at::MemoryFormat memory_format) const {
-  if (is_python_dispatch()) {
-    auto interpreter = pyobj_interpreter_.load(std::memory_order_acquire);
-    if (interpreter) {
-      return interpreter->is_contiguous(this);
-    }
-    TORCH_CHECK(
-        false,
-        "cannot access PyObject for Tensor on interpreter ",
-        pyobj_interpreter_.load()->name());
-  }
   TORCH_CHECK(
       false,
       "Tensors of type ",
