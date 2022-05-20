68e22aa9fc69290128ab7bf9f2d53652709a4c19
Michael Suo <suo@fb.com>
2022-05-19 17:10:29 -0700

    [symint] add support for negative integers
    The bit packing scheme is described in the comments.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77913
    
    Approved by: https://github.com/Krovatkin

--- c10/core/SymInt.cpp
+++ c10/core/SymInt.cpp
@@ -7,12 +7,14 @@ namespace c10 {
 std::shared_ptr<SymbolicIntNode> SymInt::toSymbolicIntNode() {
   auto& st = getSymIntTable();
   TORCH_CHECK(is_symbolic());
-  return st.getNode(SymInt::SYM_TAG_MASK ^ static_cast<uint64_t>(data_));
+  return st.getNode(static_cast<uint64_t>(data_) & ~MASK);
 }
 
 c10::SymInt SymInt::toSymInt(std::shared_ptr<SymbolicIntNode> sin_sp) {
   auto& sit = getSymIntTable();
-  auto data = sit.addNode(sin_sp) | SYM_TAG_MASK;
-  return c10::SymInt(data);
+  uint64_t idx = sit.addNode(sin_sp);
+  TORCH_CHECK(idx < MAX_SYM_IDX, "SymbolicIntNode index overflow: ", idx);
+  uint64_t data = idx | IS_SYM;
+  return c10::SymInt(static_cast<int64_t>(data));
 }
 } // namespace c10
