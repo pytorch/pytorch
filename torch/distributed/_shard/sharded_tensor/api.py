0f74b44f1a231e0050fee58baaf4a86fcbe631a9
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-05-20 17:59:03 +0000

    Revert "Add sharding tests to multigpu-test.sh and fix custom operator decorator (#77825)"
    This reverts commit 8d4c8df33a58dc5f905dfdcee1cd124a79aaf2d8.
    
    Reverted https://github.com/pytorch/pytorch/pull/77825 on behalf of https://github.com/janeyx99 due to as it will break multigpu test reporting

--- torch/distributed/_shard/sharded_tensor/api.py
+++ torch/distributed/_shard/sharded_tensor/api.py
@@ -11,8 +11,8 @@ from typing import (
     cast,
 )
 import copy
-from functools import reduce
 import weakref
+import math
 
 import threading
 import torch
@@ -49,11 +49,8 @@ _sharded_tensor_lock = threading.Lock()
 _sharded_tensor_current_id = 0
 _sharded_tensor_map: Dict[int, 'weakref.ReferenceType[ShardedTensor]'] = {}
 
-# Default sharded ops
+# Custom sharded ops
 _SHARDED_OPS: Dict[Callable, Callable] = {}
-
-# Customized user ops
-_CUSTOM_SHARDED_OPS: Dict[Callable, Callable] = {}
 
 def _register_remote_shards(sharded_tensor_id: int, rrefs: List[rpc.RRef[Shard]], rpc_rank: int):
     with _sharded_tensor_lock:
@@ -287,7 +284,7 @@ class ShardedTensor(object):
                 Default: ``None``
         """
         def shard_size(shard_md):
-            return reduce((lambda x, y: x * y), shard_md.shard_sizes)  # type: ignore[attr-defined]
+            return math.prod(shard_md.shard_sizes)  # type: ignore[attr-defined]
 
         rank = dist.get_rank(self._process_group)
         full_size = self.metadata().size
@@ -785,10 +782,6 @@ class ShardedTensor(object):
     @classmethod
     def __torch_function__(cls, func, types, args=(), kwargs=None):
         def dispatch(st: ShardedTensor, func: Callable):
-            # Dispatch to custom user provided op first if it exists.
-            if func in _CUSTOM_SHARDED_OPS:
-                return _CUSTOM_SHARDED_OPS[func](types, args, kwargs, st._process_group)
-
             # Dispatch to custom sharding spec op if it has one.
             if _has_custom_op(st._sharding_spec, func):
                 return _dispatch_custom_op(
