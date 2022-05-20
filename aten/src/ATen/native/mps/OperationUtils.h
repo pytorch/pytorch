bc7354d5fe82cee5d9c5ab5dc40c85a647849011
Kulin Seth <kulinseth@gmail.com>
2022-05-20 13:44:53 -0700

    Fix the BERT_pytorch network.

--- aten/src/ATen/native/mps/OperationUtils.h
+++ aten/src/ATen/native/mps/OperationUtils.h
@@ -63,7 +63,7 @@ class Placeholder {
   Placeholder() : _placeholder(nullptr), _value(nullptr) {}
   Placeholder(MPSGraphTensor* mpsGraphTensor) : _placeholder(mpsGraphTensor), _value(nullptr) {}
   Placeholder(MPSGraphTensor* mpsGraphTensor, const Tensor& self, MPSShape *mpsShape = nullptr,
-              bool check_view = true);
+              bool check_view = false);
   MPSGraphTensor* getMPSGraphTensor() {
     return _placeholder;
   }
