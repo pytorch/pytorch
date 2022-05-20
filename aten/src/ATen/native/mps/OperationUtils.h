3d83321b44b3f0c19315c3f646d5601f2a22e2fd
Kulin Seth <kulin_seth@apple.com>
2022-05-20 03:18:09 +0000

    MPS Fixes: copy operations, addmm and baddmm (#77791)
    Fixes for the copy operations and GEMM operations on MPS backend.
    
    Fixes https://github.com/pytorch/pytorch/issues/77819
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77791
    Approved by: https://github.com/albanD

--- aten/src/ATen/native/mps/OperationUtils.h
+++ aten/src/ATen/native/mps/OperationUtils.h
@@ -62,7 +62,8 @@ class Placeholder {
  public:
   Placeholder() : _placeholder(nullptr), _value(nullptr) {}
   Placeholder(MPSGraphTensor* mpsGraphTensor) : _placeholder(mpsGraphTensor), _value(nullptr) {}
-  Placeholder(MPSGraphTensor* mpsGraphTensor, const Tensor& self, MPSShape *mpsShape = nullptr);
+  Placeholder(MPSGraphTensor* mpsGraphTensor, const Tensor& self, MPSShape *mpsShape = nullptr,
+              bool check_view = true);
   MPSGraphTensor* getMPSGraphTensor() {
     return _placeholder;
   }
