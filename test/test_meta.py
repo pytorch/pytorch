5a7dcb4a17d73abb15f35bf7a7172e8ce32de416
Edward Z. Yang <ezyang@fb.com>
2022-05-20 14:26:25 -0700

    Update on "addr ref"
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- test/test_meta.py
+++ test/test_meta.py
@@ -913,8 +913,6 @@ meta_dispatch_expected_failures = {
 # these sometimes pass and sometimes fail
 meta_dispatch_skips = {
     aten._to_copy.default: {i64, bf16, f16, u8, b8, f32, i8, f64, i16, i32},
-    aten.addr.default: {b8},
-    aten.addr.out: {b8},
     aten.aminmax.default: {i64, u8, b8, f32, i8, f64, i16, i32},
     aten.cummax.default: {i64, bf16, u8, b8, f32, i8, f64, i16, i32},
     aten.cummin.default: {i64, bf16, u8, b8, f32, i8, f64, i16, i32},
