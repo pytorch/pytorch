dc454ed03045a0f65f66055674986964aefe941c
Milad Mohammadi <milad.mo@gmail.com>
2022-05-20 01:31:13 +0000

    Added support for `inverse` in LazyTensor shape inference (#77888)
    Fixes https://github.com/pytorch/xla/pull/3575
    
    Added support for `inverse` in LazyTensor shape inference
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77888
    Approved by: https://github.com/wconstab

--- torch/csrc/lazy/core/shape_inference.h
+++ torch/csrc/lazy/core/shape_inference.h
@@ -32,6 +32,7 @@ TORCH_API std::vector<torch::lazy::Shape> compute_shape_glu_jvp(const at::Tensor
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_grid_sampler_2d(const at::Tensor & input, const at::Tensor & grid, int64_t interpolation_mode, int64_t padding_mode, bool align_corners);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_grid_sampler_2d_backward(const at::Tensor & grad_output, const at::Tensor & input, const at::Tensor & grid, int64_t interpolation_mode, int64_t padding_mode, bool align_corners, ::std::array<bool,2> output_mask);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_index_select(const at::Tensor & self, int64_t dim, const at::Tensor & index);
+TORCH_API std::vector<torch::lazy::Shape> compute_shape_inverse(const at::Tensor & self);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_kl_div_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, int64_t reduction, bool log_target);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_l1_loss_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, int64_t reduction);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_log_sigmoid_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & buffer);
