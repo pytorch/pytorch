3a08365a507bddcd9e3b161cc2517617bd3561b8
jjsjann123 <jiej@nvidia.com>
2022-05-21 15:14:28 -0700

    c10 implementation added for call_once

---
+++ c10/util/CallOnce.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <utility>
+
+#include <c10/macros/Macros.h>
+
+namespace c10 {
+
+// custom c10 call_once implementation to avoid the deadlock in std::call_once. The implementation here is a simplified version from folly and likely much much higher memory footprint.
+template <typename Flag, typename F, typename... Args>
+C10_API inline void call_once(Flag& flag, F&& f, Args&&... args) {
+  if (C10_LIKELY(flag.test_once())) {
+    return;
+  }
+  flag.call_once_slow(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+class C10_API once_flag {
+ public:
+  constexpr once_flag() noexcept = default;
+  once_flag(const once_flag&) = delete;
+  once_flag& operator=(const once_flag&) = delete;
+
+ private:
+  template <typename Flag, typename F, typename... Args>
+  friend void call_once(Flag& flag, F&& f, Args&&... args);
+
+  template <typename F, typename... Args>
+  void call_once_slow(F&& f, Args&&... args) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (init_.load(std::memory_order_relaxed)) {
+      return;
+    }
+    f(std::forward<Args>(args)...);
+    init_.store(true, std::memory_order_release);
+  }
+
+  bool test_once() {
+    return init_.load(std::memory_order_acquire);
+  }
+
+  void reset_once() {
+    init_.store(false, std::memory_order_release);
+  }
+
+ private:
+  std::mutex mutex_;
+  std::atomic<bool> init_{false};
+};
+
+}
