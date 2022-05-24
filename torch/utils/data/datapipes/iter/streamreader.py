6f70034afef2aed41fa9430304b6bd528195043a
Kevin Tse <ktse@fb.com>
2022-05-24 15:06:15 -0400

    [DataPipe] Update StreamReader to allow continuation with empty chunk
    [ghstack-poisoned]

--- torch/utils/data/datapipes/iter/streamreader.py
+++ torch/utils/data/datapipes/iter/streamreader.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Optional, Tuple
 from torch.utils.data.datapipes._decorator import functional_datapipe
 from torch.utils.data.datapipes.datapipe import IterDataPipe
 
@@ -8,13 +8,15 @@ __all__ = ["StreamReaderIterDataPipe", ]
 @functional_datapipe('read_from_stream')
 class StreamReaderIterDataPipe(IterDataPipe[Tuple[str, bytes]]):
     r"""
-    Given IO streams and their label names, yields bytes with label
-    name in a tuple (functional name: ``read_from_stream``).
+    Given label names and IO streams, yields label
+    name and bytes in a tuple (functional name: ``read_from_stream``).
 
     Args:
-        datapipe: Iterable DataPipe provides label/URL and byte stream
+        datapipe: Iterable DataPipe provides tuples of label/URL and byte stream
         chunk: Number of bytes to be read from stream per iteration.
             If ``None``, all bytes will be read util the EOF.
+        break_if_empty: By default, this is set to ``True``, and the iteration ends as soon as a chunk read is empty.
+            Setting to ``False``, is useful for streams that send emply chunks as "keep alive" chunks.
 
     Example:
         >>> from torchdata.datapipes.iter import IterableWrapper, StreamReader
@@ -23,14 +25,18 @@ class StreamReaderIterDataPipe(IterDataPipe[Tuple[str, bytes]]):
         >>> list(StreamReader(dp, chunk=1))
         [('alphabet', 'a'), ('alphabet', 'b'), ('alphabet', 'c'), ('alphabet', 'd'), ('alphabet', 'e')]
     """
-    def __init__(self, datapipe, chunk=None):
-        self.datapipe = datapipe
-        self.chunk = chunk
+    def __init__(self, datapipe: IterDataPipe, chunk: Optional[int] = None, break_if_empty: bool = True):
+        self.datapipe: IterDataPipe = datapipe
+        self.chunk_size: Optional[int] = chunk
+        self.break_if_empty = break_if_empty
 
     def __iter__(self):
         for furl, stream in self.datapipe:
             while True:
-                d = stream.read(self.chunk)
-                if not d:
-                    break
-                yield (furl, d)
+                data = stream.read(self.chunk_size)
+                if not data:
+                    if self.break_if_empty:
+                        break
+                    else:
+                        continue
+                yield furl, data
