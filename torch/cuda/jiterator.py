d4a9002088991c346b4820db2e64bd489731f086
Sherlock Huang <bahuang@fb.com>
2022-05-24 11:49:22 -0700

    Fix doc build

--- torch/cuda/jiterator.py
+++ torch/cuda/jiterator.py
@@ -90,35 +90,35 @@ def _create_jit_fn(code_string: str, **kwargs) -> Callable:
         kwargs (Dict, optional): Keyword arguments for generated function
 
     Example:
-        >>> code_string = "template <typename T> T my_kernel(T x, T y, T alpha) { return  -x + alpha * y; }"
-        >>> jitted_fn = create_jit_fn(code_string, alpha=1.0)
-        >>> a = torch.rand(3, device='cuda')
-        >>> b = torch.rand(3, device='cuda')
-        >>> # invoke jitted function like a regular python function
-        >>> result = jitted_fn(a, b, alpha=3.14)
+        code_string = "template <typename T> T my_kernel(T x, T y, T alpha) { return  -x + alpha * y; }"
+        jitted_fn = create_jit_fn(code_string, alpha=1.0)
+        a = torch.rand(3, device='cuda')
+        b = torch.rand(3, device='cuda')
+        # invoke jitted function like a regular python function
+        result = jitted_fn(a, b, alpha=3.14)
 
     code_string also allows mulitple function definitions, and the last function will be treated as the entry function.
 
     Example:
-        >>> code_string = "template <typename T> T util_fn(T x, T y) { return ::sin(x) + ::cos(y); }\n"
-        >>> code_string += "template <typename T> T my_kernel(T x, T y, T val) { return ::min(val, util_fn(x, y)); }"
-        >>> jitted_fn = create_jit_fn(code_string, val=0.0)
-        >>> a = torch.rand(3, device='cuda')
-        >>> b = torch.rand(3, device='cuda')
-        >>> # invoke jitted function like a regular python function
-        >>> result = jitted_fn(a, b)  # using default val=0.0
+        code_string = "template <typename T> T util_fn(T x, T y) { return ::sin(x) + ::cos(y); }"
+        code_string += "template <typename T> T my_kernel(T x, T y, T val) { return ::min(val, util_fn(x, y)); }"
+        jitted_fn = create_jit_fn(code_string, val=0.0)
+        a = torch.rand(3, device='cuda')
+        b = torch.rand(3, device='cuda')
+        # invoke jitted function like a regular python function
+        result = jitted_fn(a, b)  # using default val=0.0
 
-    Jiterator can be used together with python registration to override an operator's cuda kernel
+    Jiterator can be used together with python registration to override an operator's cuda kernel.
+    Following example is overriding gelu's cuda kernel with relu.
 
-    Following example is overriding gelu's cuda kernel with relu:
-        >>> code_string = "template <typename T> T my_gelu(T a) { return a > 0 ? a : 0; }"
-        >>> my_gelu = create_jit_fn(code_string)
-        >>> my_lib = torch.library.Library("aten", "IMPL")
-        >>> my_lib.impl('aten::gelu', my_gelu, "CUDA")
-        >>> # torch.nn.GELU and torch.nn.function.gelu are now overridden
-        >>> a = torch.rand(3, device='cuda')
-        >>> torch.allclose(torch.nn.functional.gelu(a), torch.nn.functional.relu(a))
-
+    Example:
+        code_string = "template <typename T> T my_gelu(T a) { return a > 0 ? a : 0; }"
+        my_gelu = create_jit_fn(code_string)
+        my_lib = torch.library.Library("aten", "IMPL")
+        my_lib.impl('aten::gelu', my_gelu, "CUDA")
+        # torch.nn.GELU and torch.nn.function.gelu are now overridden
+        a = torch.rand(3, device='cuda')
+        torch.allclose(torch.nn.functional.gelu(a), torch.nn.functional.relu(a))
 
     .. warning::
         This API is in beta and may change in future releases.
@@ -128,14 +128,13 @@ def _create_jit_fn(code_string: str, **kwargs) -> Callable:
 
     .. warning::
         All input tensors must live in CUDA device
-
     """
 
     return _JittedFunction(code_string, return_by_ref=False, num_outputs=1, **kwargs)
 
 def _create_multi_output_jit_fn(code_string: str, num_outputs: int, **kwargs) -> Callable:
     """
-    Create a jiterator-generated cuda kernel for an elementwise op that supports returning >= 1 outputs.
+    Create a jiterator-generated cuda kernel for an elementwise op that supports returning one or more outputs.
 
     Args:
         code_string (string): CUDA code string to be compiled by jiterator. The entry functor must return value by reference.
@@ -143,19 +142,18 @@ def _create_multi_output_jit_fn(code_string: str, num_outputs: int, **kwargs) ->
         kwargs (Dict, optional): Keyword arguments for generated function
 
     Example:
-        >>> code_string = "template <typename T> void my_kernel(T x, T y, T alpha, T& out) { out = -x + alpha * y; }"
-        >>> jitted_fn = create_jit_fn(code_string, alpha=1.0)
-        >>> a = torch.rand(3, device='cuda')
-        >>> b = torch.rand(3, device='cuda')
-        >>> # invoke jitted function like a regular python function
-        >>> result = jitted_fn(a, b, alpha=3.14)
+        code_string = "template <typename T> void my_kernel(T x, T y, T alpha, T& out) { out = -x + alpha * y; }"
+        jitted_fn = create_jit_fn(code_string, alpha=1.0)
+        a = torch.rand(3, device='cuda')
+        b = torch.rand(3, device='cuda')
+        # invoke jitted function like a regular python function
+        result = jitted_fn(a, b, alpha=3.14)
 
     .. warning::
         This API is in beta and may change in future releases.
 
     .. warning::
         This API only supports up to 8 inputs and 8 outputs
-
     """
 
     return _JittedFunction(code_string, return_by_ref=True, num_outputs=num_outputs, **kwargs)
