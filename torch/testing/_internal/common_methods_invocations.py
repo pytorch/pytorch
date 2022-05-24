fc8f124df7b4784e39b7bff5e93ac01bc7505391
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-05-24 08:48:11 +0530

    add chalf in backward dtypes cuda

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -15210,9 +15210,7 @@ op_db: List[OpInfo] = [
                    domain=(0, None),
                    dtypes=all_types_and_complex_and(torch.bool, torch.bfloat16),
                    dtypesIfCUDA=all_types_and_complex_and(torch.chalf, torch.bool, torch.half, torch.bfloat16),
-                   # TODO: Add complex32 backward dtype support. Currently, we get:
-                   # AssertionError: The supported dtypes for sqrt on device type cuda are incorrect!
-                   backward_dtypesIfCUDA=floating_and_complex_types_and(torch.half, torch.bfloat16),
+                   backward_dtypesIfCUDA=floating_and_complex_types_and(torch.chalf, torch.half, torch.bfloat16),
                    assert_autodiffed=True,
                    supports_forward_ad=True,
                    supports_sparse_csr=True,
