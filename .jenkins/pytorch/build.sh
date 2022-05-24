85f1d7e94f9e8ac0f649177e13eb8ae06daeedd0
David Reiss <dreiss@fb.com>
2022-05-24 16:19:10 -0700

    Update on "Build Python module and tests CPU mode with Bazel in CI"
    This can potentially catch issues like #77002 before they hit
    Meta-internal CI, because Bazel is more strict about header visibility
    than (our current setup of) CMake.
    
    [ghstack-poisoned]

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -198,8 +198,8 @@ if [[ "$BUILD_ENVIRONMENT" == *-bazel-* ]]; then
 
   # first build torch for CPU-only
   tools/bazel build --config=no-tty :torch
-  # then build everything for CPU-only
-  tools/bazel build --config=no-tty :all
+  # then build Python module and tests for CPU-only
+  tools/bazel build --config=no-tty :_C :all_test
   # then build everything with CUDA
   tools/bazel build --config=no-tty --config=gpu :all
 else
