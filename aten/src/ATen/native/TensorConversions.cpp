34b781a62dce8bce273c3cf83accd1eeb278885a
Christian Puhrsch <cpuhrsch@fb.com>
2022-05-24 00:02:00 +0000

    Revert dense_to_sparse_csr

--- aten/src/ATen/native/TensorConversions.cpp
+++ aten/src/ATen/native/TensorConversions.cpp
@@ -524,23 +524,7 @@ std::pair<Tensor, Tensor> _not_zero_mask_to_col_row_indices(
 }
 
 Tensor dense_to_sparse_csr(const Tensor& self) {
-  TORCH_CHECK(self.dim() == 2, "Can only convert 2D Tensor to CSR.");
-  auto not_zero_mask = (self != 0);
-  auto values = self.masked_select(not_zero_mask);
-  Tensor col_indices;
-  Tensor row_indices;
-  std::tie(col_indices, row_indices) =
-      _not_zero_mask_to_col_row_indices(not_zero_mask);
-  Tensor crow_indices = at::_convert_indices_from_coo_to_csr(
-      row_indices.view({-1}), self.size(-2), false /* out_int32 */);
-  return at::native::_sparse_csr_tensor_unsafe(
-      crow_indices,
-      col_indices,
-      values,
-      self.sizes(),
-      self.scalar_type(),
-      c10::kSparseCsr,
-      self.device());
+  return self.to_sparse().to_sparse_csr();
 }
 
 Tensor dense_to_sparse_csc(const Tensor& self) {
