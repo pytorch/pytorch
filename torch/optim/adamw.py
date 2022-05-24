fa6c3b3c187d92883cd0a0e256c37755fda51598
Sergii Dymchenko <sdym@fb.com>
2022-05-23 17:43:11 -0700

    Use generators with all/any in torch/optim

--- torch/optim/adamw.py
+++ torch/optim/adamw.py
@@ -200,7 +200,7 @@ def adamw(params: List[Tensor],
     See :class:`~torch.optim.AdamW` for details.
     """
 
-    if not all([isinstance(t, torch.Tensor) for t in state_steps]):
+    if not all(isinstance(t, torch.Tensor) for t in state_steps):
         raise RuntimeError("API has changed, `state_steps` argument must contain a list of singleton tensors")
 
     if foreach is None:
