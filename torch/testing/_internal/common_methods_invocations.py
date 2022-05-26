37addcf432033393e545d41c0e0ace1015c0cc72
Allen Goodman <allen.goodman@icloud.com>
2022-05-26 12:30:39 -0400

    Update common_methods_invocations.py

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -18876,8 +18876,8 @@ op_db: List[OpInfo] = [
         dtypes=all_types_and(torch.bool),
         promotes_int_to_float=True,
         skips=(
-          DecorateInfo(unittest.skip("Skipped!"), 'TestCudaFuserOpInfo'),
-          DecorateInfo(unittest.skip("Skipped!"), 'TestNNCOpInfo'),
+            DecorateInfo(unittest.skip("Skipped!"), 'TestCudaFuserOpInfo'),
+            DecorateInfo(unittest.skip("Skipped!"), 'TestNNCOpInfo'),
         ),
         supports_one_python_scalar=True,
         supports_autograd=False,
