bab2560ba7bfe15aeea09cfdfbbf285c024bf294
BowenBao <bowbao@microsoft.com>
2022-05-26 14:40:39 -0700

    [ONNX][WIP] Refactor verification.py
    * Rename arguments, code clean up.
    * Refactor functions to smaller reusable functions.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77289
    
    Approved by: https://github.com/justinchuby, https://github.com/garymm

--- test/onnx/test_models_onnxruntime.py
+++ test/onnx/test_models_onnxruntime.py
@@ -15,11 +15,11 @@ def exportTest(self, model, inputs, rtol=1e-2, atol=1e-7, opset_versions=None):
     for opset_version in opset_versions:
         self.opset_version = opset_version
         self.onnx_shape_inference = True
-        run_model_test(self, model, input=inputs, rtol=rtol, atol=atol)
+        run_model_test(self, model, input_args=inputs, rtol=rtol, atol=atol)
 
         if self.is_script_test_enabled and opset_version > 11:
             script_model = torch.jit.script(model)
-            run_model_test(self, script_model, input=inputs, rtol=rtol, atol=atol)
+            run_model_test(self, script_model, input_args=inputs, rtol=rtol, atol=atol)
 
 
 TestModels = type(
