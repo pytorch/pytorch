877cd482ffa8e71efb9d3658bc07151d6f72dfa7
Allen Goodman <allen.goodman@icloud.com>
2022-05-26 12:58:34 -0400

    Update common_methods_invocations.py

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -18875,6 +18875,10 @@ op_db: List[OpInfo] = [
         'special.hermite_polynomial_he',
         dtypes=all_types_and(torch.bool),
         promotes_int_to_float=True,
+        skips=(
+            DecorateInfo(unittest.skip("Skipped!"), 'TestCudaFuserOpInfo'),
+            DecorateInfo(unittest.skip("Skipped!"), 'TestNNCOpInfo'),
+        ),
         supports_one_python_scalar=True,
         supports_autograd=False,
     ),
