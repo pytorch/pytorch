1d5ecdb3b913b03273ff2477822719b36679eb2b
Nikita Shulga <nshulga@fb.com>
2022-05-26 13:58:36 +0000

    Update PeachPy submodule (#78326)
    Forked the repo, merged latest changes into pre-generated branch and
    update pregenerared opcodes
    
    Re-enabled NNPACK builds on MacOS
    
    Picking https://github.com/Maratyszcza/PeachPy/commit/f8ef1a3c0a51a7691eb2b2a1bdd06335a03ff26f  fixes https://github.com/pytorch/pytorch/issues/76094
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78326
    Approved by: https://github.com/atalman, https://github.com/albanD
    
    (cherry picked from commit fa7117c64a9cc740e71728701adb2cb2ccc143c4)

--- .jenkins/pytorch/macos-build.sh
+++ .jenkins/pytorch/macos-build.sh
@@ -33,11 +33,11 @@ fi
 
 cross_compile_arm64() {
   # Cross compilation for arm64
-  USE_DISTRIBUTED=1 CMAKE_OSX_ARCHITECTURES=arm64 MACOSX_DEPLOYMENT_TARGET=11.0 USE_MKLDNN=OFF USE_NNPACK=OFF USE_QNNPACK=OFF BUILD_TEST=OFF python setup.py bdist_wheel
+  USE_DISTRIBUTED=1 CMAKE_OSX_ARCHITECTURES=arm64 MACOSX_DEPLOYMENT_TARGET=11.0 USE_MKLDNN=OFF USE_QNNPACK=OFF BUILD_TEST=OFF python setup.py bdist_wheel
 }
 
 compile_x86_64() {
-  USE_DISTRIBUTED=1 USE_NNPACK=OFF python setup.py bdist_wheel
+  USE_DISTRIBUTED=1 python setup.py bdist_wheel
 }
 
 build_lite_interpreter() {
