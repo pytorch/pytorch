f3c75df9bfcb17fffe1c298901dc4fd76aeea4b7
dzdang <dzdang@umich.edu>
2022-05-26 12:55:13 -0700

    [quant][core][feature] Implemented index_put for QuantizedCPU tensors
    Summary:
    Previously, there was no support for index_put for quantized tensors.
    This PR introduces the feature for quantized CPU tensors, but is limited
    to per tensor quantized tensors and accumulate=False. A
    corresponding test case was added to test the impl.
    
    Most of the implementation is copied over from the fp kernel and adapted
    for quantized tensors.
    
    TODO: implement index_put for QuantizedCUDA tensors.
    
    Test Plan:
    ```
    python test/test_quantization.py -k test_qtensor_index_put
    ```
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/IndexKernel.h
+++ aten/src/ATen/native/quantized/IndexKernel.h
@@ -4,8 +4,10 @@
 namespace at {
 namespace native {
 
+void index_put_kernel_quantized_cpu(TensorIterator& iter, IntArrayRef index_size, IntArrayRef index_stride, bool accumulate, double scale, int zero_point);
 void masked_fill_kernel_quantized_cpu(TensorIterator& iter, const Scalar& value, double scale, int zero_point);
 
+// TODO: implement index_put_kernel_quantized_cuda in cuda/IndexKernel.cu and put CPU & CUDA kernels in a stub
 // TODO: implement masked_fill_kernel_quantized_cuda in cuda/IndexKernel.cu and put CPU & CUDA kernels in a stub
 
 } // native
--- aten/src/ATen/native/quantized/cpu/IndexKernel.cpp
+++ aten/src/ATen/native/quantized/cpu/IndexKernel.cpp
@@ -1,5 +1,6 @@
 #include <ATen/Context.h>
 #include <ATen/Dispatch.h>
+#include <ATen/native/cpu/IndexKernelUtils.h>
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/quantized/IndexKernel.h>
 #include <ATen/native/TensorIterator.h>
@@ -39,6 +40,32 @@ void masked_fill_kernel_quantized_cpu(TensorIterator& iter, const Scalar& value,
       cpu_masked_fill_kernel_quantized_cpu<scalar_t, bool>(iter, scalar_val, scale, zero_point);
     } else {
       cpu_masked_fill_kernel_quantized_cpu<scalar_t, unsigned char>(iter, scalar_val, scale, zero_point);
+    }
+  });
+}
+
+// currently, we do not support accumulate=True for quantized tensors. We throw an exception in _index_put_impl_quantized_cpu_
+// However, we currently do have an implementation for accumulate=True in the below code, but I think accumulation for quantized tensors can be defined
+// different ways since quantize(x + y) = quantize(x) + quantize(y) does not always hold. The code in this function implements the RHS, i.e., it quantizes the
+// fp values and then accumluates the quantized values. The LHS first accumulates the fp values and then quantizes the accumulated fp value.
+// TODO: decide on which one we should be supporting
+void index_put_kernel_quantized_cpu(TensorIterator& iter, IntArrayRef index_size, IntArrayRef index_stride, bool accumulate, double scale, int zero_point) {
+  // NOTE: duplicate indices are only supported if accumulate is true.
+  AT_DISPATCH_QINT_TYPES(iter.dtype(), "index_put", [&] {
+    // See Note [Enabling Deterministic Operations]
+    // Parallel cpu_index_kernel with accumulation is nondeterministic, so we
+    // must enable serial execution if deterministic algorithms are enabled.
+    const bool is_deterministic = at::globalContext().deterministicAlgorithms();
+    if (accumulate) {
+      // TODO: can we do this atomically on ints like the fp variant did with cpu_atomic_add_float?
+      at::native::cpu_index_kernel<scalar_t>(iter, index_size, index_stride, [scale, zero_point](char* dst, char* src, int64_t offset) {
+        // TODO: define operator+= that can add qdtypes
+        *(underlying_t*)(dst + offset) += quantize_val<scalar_t>(scale, zero_point, *(float*)src).val_;
+      }, /*serial_execution=*/true);
+    } else {
+      at::native::cpu_index_kernel<scalar_t>(iter, index_size, index_stride, [scale, zero_point](char* dst, char* src, int64_t offset) {
+        *(scalar_t*)(dst + offset) = quantize_val<scalar_t>(scale, zero_point, *(float*)src);
+      }, /*serial_execution=*/is_deterministic);
     }
   });
 }
