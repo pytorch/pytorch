66068e247c74abbaff6725ef5f1543868b756368
dzdang <dzdang@umich.edu>
2022-05-26 09:57:17 -0700

    Update on "[quant][core][feature] Implemented masked_fill for QuantizedCPU tensors"
    Summary:
    Previously, there was no suport for masked_fill for quantized tensors.
    This PR introduces the feature for quantized CPU tensors. A
    corresponding test case was added to test the impl.
    
    TODO: implement masked_fill for QuantizedCUDA tensors.
    
    Test Plan:
    ```
    python test/test_quantization.py -k test_qtensor_masked_fill
    ```
    
    Differential Revision: [D36706178](https://our.internmc.facebook.com/intern/diff/D36706178)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/quantized/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/quantized/TensorAdvancedIndexing.cpp
@@ -59,43 +59,5 @@ Tensor & masked_fill__quantized_cpu(Tensor& self, const Tensor & mask, const Ten
   return self;
 }
 
-Tensor& _index_put_impl_quantized_cpu_(Tensor & self, const torch::List<c10::optional<Tensor>>& indices, const Tensor & value, const bool accumulate, const bool unsafe) {
-  TORCH_CHECK_INDEX(indices.size() <= (size_t)self.dim(), "too many indices for tensor of dimension ", self.dim(), " (got ", indices.size(), ")");
-  TORCH_CHECK(!value.is_quantized(), "Value argument for quantized input_put should not be quantized");
-  TORCH_CHECK(self.qscheme() == c10::kPerTensorAffine, "index_put for quantized tensors is currently only supported for per tensor quantized tensors");
-
-  if (at::has_internal_overlap(self) == MemOverlap::YES) {
-    TORCH_WARN(
-      "Use of index_put_ on expanded tensors is deprecated. "
-      "Please clone() the tensor before performing this operation. "
-      "This also applies to advanced indexing e.g. tensor[indices] = tensor");
-  }
-
-  // need to implement this
-  if (!accumulate) {
-    auto masked_fill_dispatch = canDispatchToMaskedFill(self, indices, value);
-    if (std::get<0>(masked_fill_dispatch)) {
-      return self.masked_fill_(std::get<1>(masked_fill_dispatch), value.item());
-    }
-  }
-
-  auto value_ = value;
-  if (value.device() != self.device() && value.numel() == 1 && value.dim() == 0) {
-    value_ = value.to(self.device());
-  }
-  at::assert_no_overlap(self, value);
-  // NOLINTNEXTLINE(performance-implicit-conversion-in-loop)
-  for (const c10::optional<Tensor>& index: indices) {
-    if (index.has_value()) {
-      at::assert_no_overlap(self, *index);
-    }
-  }
-
-  auto info = make_info(self, indices);
-  auto iter = make_index_put_iterator(info, value_);
-  at::native::index_put_kernel_quantized_cpu(iter, info.indexed_sizes, info.indexed_strides, accumulate, self.q_scale(), self.q_zero_point());
-  return self;
-}
-
 }
 }
