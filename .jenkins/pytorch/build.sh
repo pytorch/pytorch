e840b82b35ad3f273fe85e56d3139597ee49440e
Sergii Dymchenko <sdym@fb.com>
2022-05-26 20:06:46 +0000

    Migrate off xenial gcc5.4 (#78137)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78137
    Approved by: https://github.com/malfet

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -181,11 +181,23 @@ if [[ "${BUILD_ENVIRONMENT}" == *no-ops* ]]; then
   export USE_PER_OPERATOR_HEADERS=0
 fi
 
+# TODO: Remove after xenial->focal migration
 if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-py3.7-gcc7-build* || "${BUILD_ENVIRONMENT}" == *linux-xenial-py3.7-gcc5.4-build* ]]; then
   export USE_GLOO_WITH_OPENSSL=ON
 fi
 
+if [[ "${BUILD_ENVIRONMENT}" == *linux-focal-py3.7-gcc7-build*  ]]; then
+  export USE_GLOO_WITH_OPENSSL=ON
+fi
+
+# TODO: Remove after xenial->focal migration
 if [[ "${BUILD_ENVIRONMENT}" == pytorch-linux-xenial-py3* ]]; then
+  if [[ "${BUILD_ENVIRONMENT}" != *android* && "${BUILD_ENVIRONMENT}" != *cuda* ]]; then
+    export BUILD_STATIC_RUNTIME_BENCHMARK=ON
+  fi
+fi
+
+if [[ "${BUILD_ENVIRONMENT}" == pytorch-linux-focal-py3* ]]; then
   if [[ "${BUILD_ENVIRONMENT}" != *android* && "${BUILD_ENVIRONMENT}" != *cuda* ]]; then
     export BUILD_STATIC_RUNTIME_BENCHMARK=ON
   fi
