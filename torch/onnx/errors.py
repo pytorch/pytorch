9fc29bed96929e96a752e8a7419b6d4efc17a1f3
Justin Chu <justinchuby@users.noreply.github.com>
2022-05-31 12:55:12 -0700

    Move errors

--- torch/onnx/errors.py
+++ torch/onnx/errors.py
@@ -1,6 +1,11 @@
 """ONNX exporter exceptions."""
 
 
+from typing import Optional
+
+__all__ = ["OnnxExporterError", "CheckerError", "UnsupportedOperatorError"]
+
+
 class OnnxExporterError(RuntimeError):
     """Errors raised by the ONNX exporter."""
 
@@ -11,3 +16,25 @@ class CheckerError(OnnxExporterError):
     r"""Raised when ONNX checker detects an invalid model."""
 
     pass
+
+
+class UnsupportedOperatorError(OnnxExporterError):
+    def __init__(
+        self, domain: str, opname: str, version: int, supported_version: Optional[int]
+    ):
+        if domain in {"", "aten", "prim", "quantized"}:
+            msg = f"Exporting the operator {domain}::{opname} to ONNX opset version {version} is not supported. "
+            if supported_version is not None:
+                msg += (
+                    f"Support for this operator was added in version {supported_version}, "
+                    "try exporting with this version."
+                )
+            else:
+                msg += "Please feel free to request support or submit a pull request on PyTorch GitHub."
+        else:
+            msg = (
+                f"ONNX export failed on an operator with unrecognized namespace {domain}::{opname}. "
+                "If you are trying to export a custom operator, make sure you registered "
+                "it with the right domain and version."
+            )
+        super().__init__(msg)
