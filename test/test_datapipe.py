fddeebf06a14cde4c553966553fcfaddf8f756f2
Robert Xiu <xiurobert@gmail.com>
2022-05-31 23:29:36 +0800

    refactor: reverts "rename to list file for API consistency"
    revert: 10608574b60ae61152966c4e76b3d73867b536d2

--- test/test_datapipe.py
+++ test/test_datapipe.py
@@ -301,7 +301,7 @@ class TestIterableDataPipeBasic(TestCase):
         self.assertEqual(count, 2 * len(self.temp_files))
 
         # test functional API
-        datapipe = datapipe.list_file()
+        datapipe = datapipe.list_files()
         count = 0
         for pathname in datapipe:
             count += 1
