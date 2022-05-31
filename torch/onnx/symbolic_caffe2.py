96acd6c908a679c626dd8fd130cb07e3444161be
Justin Chu <justinchuby@users.noreply.github.com>
2022-05-31 12:58:51 -0700

    format

--- torch/onnx/symbolic_caffe2.py
+++ torch/onnx/symbolic_caffe2.py
@@ -12,7 +12,9 @@ def register_quantized_ops(domain: str, version: int):
     # Register all quantized ops
     module = importlib.import_module("torch.onnx.symbolic_caffe2")
     symbolic_registry._symbolic_versions["caffe2"] = module
-    quant_version_ops = inspect.getmembers(symbolic_registry._symbolic_versions["caffe2"])
+    quant_version_ops = inspect.getmembers(
+        symbolic_registry._symbolic_versions["caffe2"]
+    )
     for op in quant_version_ops:
         if inspect.isfunction(op[1]) and not symbolic_registry.is_registered_op(
             op[0], domain, version
