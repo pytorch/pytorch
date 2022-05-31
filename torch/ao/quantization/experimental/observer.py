7d69150cfadc23cdbb622de2a33e3f46ba45a6f2
asl3 <amandaliu@fb.com>
2022-05-31 14:15:32 -0700

    Add nonuniform observer class and tests
    [ghstack-poisoned]

---
+++ torch/ao/quantization/experimental/observer.py
@@ -0,0 +1,44 @@
+"""
+This module implements nonuniform observers used to collect statistics about
+the values observed during calibration (PTQ) or training (QAT).
+"""
+
+import torch
+from torch.ao.quantization.observer import ObserverBase
+from typing import Tuple
+
+class NonUniformQuantizationObserverBase(ObserverBase):
+    alpha = 0
+    gamma = 0
+    level_indices = torch.Tensor()
+
+    def __init__(
+        self,
+        min_val: torch.Tensor,
+        max_val: torch.Tensor,
+        level_indices: torch.Tensor,
+            b: int,
+            k: int) -> None:
+        super().__init__
+
+    def _calculate_qparams(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+class APoTObserver(NonUniformQuantizationObserverBase):
+    def __init__(
+        self,
+        min_val: torch.Tensor,
+        max_val: torch.Tensor,
+        level_indices: torch.Tensor,
+            b: int,
+            k: int) -> None:
+        super(APoTObserver, self).__init__(min_val, max_val, level_indices, b, k)
+
+    def calculate_qparams(self):
+        return self._calculate_qparams()
+
+    # def _calculate_qparams(self):
+    #     return NonUniformQuantizationObserverBase._calculate_qparams()
+
+    def forward(self, x_orig):
+        pass
