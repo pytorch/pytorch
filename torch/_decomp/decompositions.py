cd3937d4fa1d90bc6c22a2339641f33300453369
Shangdi Yu <yushangdi123@gmail.com>
2022-05-31 22:41:10 +0000

    remove dtype from decorator, special case for p=2

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -1303,8 +1303,7 @@ def log_sigmoid_forward(self: Tensor) -> Tuple[Tensor, Tensor]:
     return min - torch.log1p(z), buffer
 
 # torch.ops.aten.norm only supports numeric p, does not support Frobenius norm or nuclear norm
-@register_decomposition([aten.norm.ScalarOpt_dtype, aten.norm.Scalar, aten.norm.ScalarOpt_dim_dtype,
-        aten.norm.ScalarOpt_dim, aten.norm.names_ScalarOpt_dim_dtype, aten.norm.names_ScalarOpt_dim])
+@register_decomposition([aten.norm.Scalar, aten.norm.ScalarOpt_dim])
 @reduction_complex_to_real
 def norm(self: Tensor, p: float, dim: List[int] = [], keepdim: bool = False):
     self_ = self.abs()
@@ -1314,6 +1313,8 @@ def norm(self: Tensor, p: float, dim: List[int] = [], keepdim: bool = False):
         result = self_.amax(dim, keepdim = keepdim)
     elif p==-float('inf'):
         result = self_.amin(dim, keepdim = keepdim)
+    elif p==2:
+        result = self_.square().sum(dim, keepdim = keepdim).sqrt()
     else:
         result = self_.pow(p).sum(dim, keepdim = keepdim).pow(1/p)
     return result.to(dtype=self.dtype)
