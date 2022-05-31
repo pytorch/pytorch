0d64ecc730e9a3067b8310792af56182c3d6a970
Elias Ellison <eellison@devfair044.h1.fair>
2022-05-31 07:02:17 -0700

    Fake Tensor Part 1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77969
    
    Approved by: https://github.com/ezyang

---
+++ torch/_subclasses/base_tensor.py
@@ -0,0 +1,19 @@
+import torch
+
+# Ideally, tensor subclasses would would inherit directly from Tensor.
+# This is just our staging ground for applying behavior that hasn't yet made it
+# into the core Tensor class but that we would like to apply by default.
+class BaseTensor(torch.Tensor):
+    # See https://github.com/pytorch/pytorch/pull/73727 ; this is necessary
+    # to ensure that super().__new__ can cooperate with each other
+    @staticmethod
+    def __new__(cls, elem, *, requires_grad=None):
+        if requires_grad is None:
+            return super().__new__(cls, elem)  # type: ignore[call-arg]
+        else:
+            return cls._make_subclass(cls, elem, requires_grad)
+
+    # If __torch_dispatch__ is defined (which it will be for all our examples)
+    # the default torch function implementation (which preserves subclasses)
+    # typically must be disabled
+    __torch_function__ = torch._C._disabled_torch_function_impl
