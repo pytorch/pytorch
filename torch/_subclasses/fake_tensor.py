2dbbd5ab5a6667d614545083035ed43537cb5407
Elias Ellison <elias.ellison@gmail.com>
2022-06-01 06:30:09 -0700

    Update base for Update on "add non-kwarg device and _like constructors"
    Adds a few more special cases. 
    
    [ghstack-poisoned]

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -89,7 +89,7 @@ def torch_dispatch_impl(cls_or_mode_instance, func, types, args, kwargs, run_fun
     converter = cls_or_mode_instance.fake_tensor_converter if in_fake_mode else FakeTensorConverter()
 
     # This classes virtualizes .device() calls, need to short-circuit
-    # it insteead of calling device again or we would keep on recurring
+    # it instead of calling device again or we would keep on recurring
     if func == torch.ops.prim.device.default:
         assert len(args) == 1 and isinstance(args[0], FakeTensor)
         return args[0].fake_device
