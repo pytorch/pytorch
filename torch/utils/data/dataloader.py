a8127e3913bfc15ea07832db178327251c7b11b1
Sergii Dymchenko <sdym@fb.com>
2022-06-01 05:25:23 +0000

    Remove Python 2-related code from dataloader (#78594)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78594
    Approved by: https://github.com/seemethere

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -590,8 +590,6 @@ class _BaseDataLoaderIter(object):
                 warnings.warn(warn_msg)
             return data
 
-    next = __next__  # Python 2 compatibility
-
     def __len__(self) -> int:
         return len(self._index_sampler)
 
