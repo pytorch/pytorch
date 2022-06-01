fb6e3ed4e0ffd043255a87fbf13978d4c0cf4b71
jjsjann123 <alex.jann2012@gmail.com>
2022-06-01 13:51:37 -0700

    disabling jit

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -10692,6 +10692,7 @@ op_db: List[OpInfo] = [
            supports_gradgrad=False,
            assert_autodiffed=False,
            supports_autograd=False,
+           supports_scripting=False,
            sample_inputs_func=sample_inputs_broadcast_shapes,
            skips=(
                # skip these tests since we have non tensor input
