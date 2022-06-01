54a0278aaed39fb35620b02acd93f8cb781903a1
Fiona Victoria <37321147+fionavictoria@users.noreply.github.com>
2022-05-31 22:53:03 -0700

    Update view_model.py

--- torch/modelviewer/view_model.py
+++ torch/modelviewer/view_model.py
@@ -4,6 +4,5 @@ from modelviewer import ModelViewer
 model_path = 'cifar_net.pth'
 model = ModelViewer.get_model(model_path)
 layer_list, ref = ModelViewer.get_layers(model)
-print(layer_list)
 fig = ModelViewer.display_model(layer_list, ref)
 ModelViewer.save_model(fig, model_path)
