c962fbd407cffcd1c80efa5979d5d332e7151f4a
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2022-06-01 15:12:08 -0400

    Update on "[DataLoader] DataLoader now automatically apply sharding to DataPipes"
    This code assumes that every rank in distributed setup would have an equal amount of workers. I will drop this assumption in follow-up PR.
    
    [ghstack-poisoned]

--- torch/utils/data/backward_compatibility.py
+++ torch/utils/data/backward_compatibility.py
@@ -2,4 +2,4 @@ import warnings
 
 def worker_init_fn(worker_id):
     warnings.warn("Usage of backward_compatibility.worker_init_fn is deprecated"
-                  " as DataLoader automatically calls it in every worker")
+                  " as DataLoader automatically applies sharding in every worker")
