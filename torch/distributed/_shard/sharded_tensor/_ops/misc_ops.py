878bfa03e50abf7cf20f985d1fc8ac284f90b3af
Wanchao Liang <wanchaol@users.noreply.github.com>
2022-06-01 21:41:42 +0000

    Update on "[shard] Enable ShardedTensor as a nn.Parameter"
    Do whatever it takes to make ShardedTensor be able to be wrapped by
    nn.Parameter, so that it appears to the module.parameters() and
    module.named_parameters() list. We could in the future remove the
    named_params_with_sharded_tensor API.
    
    [ghstack-poisoned]

--- torch/distributed/_shard/sharded_tensor/_ops/misc_ops.py
+++ torch/distributed/_shard/sharded_tensor/_ops/misc_ops.py
@@ -10,5 +10,3 @@ from torch.distributed._shard.sharded_tensor import (
 @_sharded_op_impl(torch._has_compatible_shallow_copy_type)
 def tensor_has_compatible_shallow_copy_type(types, args=(), kwargs=None, pg=None):
     return False
-
-    
