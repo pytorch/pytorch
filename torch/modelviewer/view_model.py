8e77a83748c6a0103011913fdea485cf43486a96
Fiona Victoria <37321147+fionavictoria@users.noreply.github.com>
2022-05-31 22:07:17 -0700

    Update view_model.py
    Remove unused imports

--- torch/modelviewer/view_model.py
+++ torch/modelviewer/view_model.py
@@ -1,4 +1,3 @@
-import netron
 from modelviewer import ModelViewer
 
 # Provide path of trained model
@@ -8,5 +7,3 @@ layer_list, ref = ModelViewer.get_layers(model)
 print(layer_list)
 fig = ModelViewer.display_model(layer_list, ref)
 ModelViewer.save_model(fig, model_path)
-# compare against state-of-the-art third part library - "Netron" that is also used to visualize neural network models
-netron.start(model_path)
