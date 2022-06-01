2206874170ba94ef8e997379ebc8a19f9f20f60d
Red Magnos <redmagnos@gmail.com>
2022-06-01 16:51:37 -0300

    Update documentation
    Updates the documentation to reflect the removal of the constraint.

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -3362,7 +3362,6 @@ This is the reverse operation of the manner described in :meth:`~Tensor.gather`.
 
 :attr:`self`, :attr:`index` and :attr:`src` (if it is a Tensor) should all have
 the same number of dimensions. It is also required that
-``index.size(d) <= src.size(d)`` for all dimensions ``d``, and that
 ``index.size(d) <= self.size(d)`` for all dimensions ``d != dim``.
 Note that ``index`` and ``src`` do not broadcast.
 
@@ -3453,8 +3452,7 @@ For a 3-D tensor, :attr:`self` is updated as::
     self[i][j][index[i][j][k]] += src[i][j][k]  # if dim == 2
 
 :attr:`self`, :attr:`index` and :attr:`src` should have same number of
-dimensions. It is also required that ``index.size(d) <= src.size(d)`` for all
-dimensions ``d``, and that ``index.size(d) <= self.size(d)`` for all dimensions
+dimensions. It is also required that ``index.size(d) <= self.size(d)`` for all dimensions
 ``d != dim``. Note that ``index`` and ``src`` do not broadcast.
 
 Note:
@@ -3501,7 +3499,6 @@ tensor are included in the reduction.
 
 :attr:`self`, :attr:`index` and :attr:`src` should all have
 the same number of dimensions. It is also required that
-``index.size(d) <= src.size(d)`` for all dimensions ``d``, and that
 ``index.size(d) <= self.size(d)`` for all dimensions ``d != dim``.
 Note that ``index`` and ``src`` do not broadcast.
 
