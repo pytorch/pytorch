82c80253ca482b05c39f5cd3b21e645ebe44bb38
Fiona Victoria <37321147+fionavictoria@users.noreply.github.com>
2022-05-31 22:06:41 -0700

    Update modelviewer.py
    Modification on trailing spaces

--- torch/modelviewer/modelviewer.py
+++ torch/modelviewer/modelviewer.py
@@ -1,7 +1,8 @@
 # Author: Fiona Victoria Stanley Jothiraj
 
-# The experimental implementation consists of adding components to the existing Pytorch’s architecture that help users to view the trained/inference models. 
-# Currently, no such support exists in PyTorch, and the users are forced to use other third-party tools from Tensorflow. 
+# The experimental implementation consists of adding components to the existing Pytorch’s architecture.
+# The new implementation can help users to view the trained/inference models.
+# Currently, no such support exists in PyTorch, and the users are forced to use other third-party tools from Tensorflow.
 # However, integrating monitoring and visualization features like these would be of great use to the Pytorch developer community.
 
 import torch
@@ -13,6 +14,7 @@ class ModelViewer:
         # load the pytorch model
         model = torch.load(path)
         return model
+    
     @staticmethod
     def get_layers(model):
         # get the list of layers from model
@@ -36,14 +38,15 @@ class ModelViewer:
             layer_list.append(layers)
         # check to remove duplicate entries
         layer_list = [i for n, i in enumerate(layer_list) if i not in layer_list[n + 1:]]
-        return layer_list, d    
+        return layer_list, d
+    
     @staticmethod
     def display_model(layers, ref):
         # generate rough sketch of architecture
         # plot size ranges from 0.0 to 1.0 in x and y axis
         fig, ax = plt.subplots(figsize=(5, 13))
         ax.axis("off")
-        # initializing position of weights and bias components   
+        # initializing position of weights and bias components
         # initializers - Experimental version
         x, y = 0.1, 0.9
         step_size = 0.25
@@ -57,15 +60,15 @@ class ModelViewer:
                 name += str(i)
                 name += "\n"
             ax.text(x, y, name, color='black', fontsize= 13, fontfamily="monospace",
-                            bbox = dict(boxstyle = "round", facecolor = 'none', edgecolor = 'black', ec = (0,0,0),
-                            fc = (0.949, 0.964, 0.917)))
+                            bbox = dict(boxstyle="round", facecolor='none', edgecolor='black', ec=(0,0,0), fc=(0.949, 0.964, 0.917)))
             y = y - step_size
         plt.show()
         return fig
-    @staticmethod                
+    
+    @staticmethod     
     def save_model(fig, img):
         # path to save the architecture diagram
-        img = img.rsplit('.',1)[0]
+        img = img.rsplit('.', 1)[0]
         # save the figure to file
         fig.savefig(img + ".png", bbox_inches='tight')
         # close the figure window
