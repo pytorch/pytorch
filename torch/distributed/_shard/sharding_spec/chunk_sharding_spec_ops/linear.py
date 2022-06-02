bf3e74d4d5e4bf03d208fd2bb3d2003e05791c72
pritam (Meta Employee) <9958665+pritamdamania87@users.noreply.github.com>
2022-06-02 09:16:14 -0700

    Fuse row-wise sharded linear matmul to increase perf. (#78449)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78449
    
    Instead of looping through and performing a matmul separately, we can
    just perform a single matmul to ensure we launch a single cuda kernel for this
    operation.
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/effd270986c119859f188e8391882ae70a46bbed
    
    Reviewed By: aazzolini, wanchaol
    
    Differential Revision: D36743354
    
    fbshipit-source-id: 2397c5caaa2a162d1af92086a24922fc503021a9

--- torch/distributed/_shard/sharding_spec/chunk_sharding_spec_ops/linear.py
+++ torch/distributed/_shard/sharding_spec/chunk_sharding_spec_ops/linear.py
@@ -323,21 +323,17 @@ def _handle_row_wise_sharding_sharded_tensor(
     Returns:
         A :class:`_PartialTensor` object which stores the partial local result.
     """
-    results = []
-    local_shard = input.local_shards()[0].tensor
+    local_input = input.local_tensor()
     if input.sharding_spec().dim not in (-1, len(input.size()) - 1):
         raise NotImplementedError(
             "The case when the input does not come from col-wise sharded "
             "linear is not supported for row-wise sharded linear."
         )
 
-    for tensor in torch.tensor_split(local_shard, world_size):
-        results.append(
-            tensor.matmul(local_shard_t) + _BiasTensorPartial.apply(world_size, bias)
-        )
+    result = torch.addmm(_BiasTensorPartial.apply(world_size, bias), local_input, local_shard_t)
 
     # Return the partial local result.
-    return _PartialTensor(torch.cat(results), pg)
+    return _PartialTensor(result, pg)
 
 
 class _BiasTensorNarrow(Function):
@@ -356,9 +352,7 @@ class _BiasTensorNarrow(Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        results = []
-        for idx in range(ctx.world_size):
-            results.append(grad_output.clone())
+        results = [grad_output.clone()] * ctx.world_size
         return (None, None, None, None, None) + (
             _result_distribute_with_col_rearrange(
                 results, grad_output, ctx.world_size, ctx.weight, ctx.pg
