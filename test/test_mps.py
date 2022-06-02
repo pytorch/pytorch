717aaaaf055f03615103c55c2829b2f417c3e543
Kulin Seth <kulin_seth@apple.com>
2022-06-02 06:17:19 +0000

    MPS: Fix issues with view tensors and linspace. (#78690)
    Fixes: #https://github.com/pytorch/pytorch/issues/78642, https://github.com/pytorch/pytorch/issues/78511
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78690
    Approved by: https://github.com/razarmehr, https://github.com/DenisVieriu97

--- test/test_mps.py
+++ test/test_mps.py
@@ -1195,6 +1195,14 @@ class TestMPS(TestCase):
         mps_x.transpose_(0, 1)
         self.assertEqual(cpu_x, mps_x.to('cpu'))
 
+    def test_expand_cpu_to_mps_copy(self):
+        # https://github.com/pytorch/pytorch/issues/78642
+
+        x = torch.tensor(1).expand([10]).to("mps")
+        x_cpu = torch.tensor(1).expand([10])
+
+        self.assertEqual(x_cpu, x.cpu())
+
     def test_slice(self):
         values = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
         cpu_x = torch.tensor(values, device='cpu')
@@ -4137,6 +4145,32 @@ class TestNNMPS(NNTestCase):
         expect = F.conv2d(x, y)
         actual = F.conv2d(x, y, padding='valid')
         self.assertEqual(expect.to('cpu'), actual.to('cpu'))
+
+    def test_gemm_permute_transpose(self):
+        batch_size = 32
+        n = 20
+        hidden = 768
+        num_attention_heads = 12
+        attention_head_size = hidden // num_attention_heads
+
+        def transpose_for_scores(x: torch.Tensor) -> torch.Tensor:
+            new_x_shape = x.size()[:-1] + (num_attention_heads, attention_head_size)
+            x = x.view(new_x_shape)
+            return x.permute(0, 2, 1, 3)
+
+        def attention2(key, *, workaround=False, device):
+            key = transpose_for_scores(key)
+            res = key.transpose(-1, -2)
+            return res
+
+        A = torch.randn(batch_size, n, hidden)
+        A_mps = A.detach().clone().to("mps")
+
+        r1 = attention2(A, device="cpu")
+        r2 = attention2(A_mps, device="mps")
+
+        r2_cpu = r2.to("cpu")
+        self.assertEqual(r1, r2_cpu)
 
     # def test_conv2d_same_padding(self, device='mps'):
         # x = torch.rand(1, 1, 10, 11, device=device)
