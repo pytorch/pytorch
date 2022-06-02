881619f6f4b35e9f4ab2b2250691180e7ec51e29
jjsjann123 <alex.jann2012@gmail.com>
2022-06-01 19:59:35 -0700

    fixing exception condition for division by 0

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2066,7 +2066,7 @@ def tensor_split(
 def dsplit(a: TensorLikeType, sections: DimsType) -> TensorSequenceType:
     if a.ndim < 3:
         raise RuntimeError(f"torch.dsplit requires a tensor with at least 3 dimension, but got a tensor with {a.ndim} dimensions!")
-    if isinstance(sections, int) and a.shape[2] % sections != 0:
+    if isinstance(sections, int) and (sections == 0 or a.shape[2] % sections != 0):
         raise RuntimeError(f"torch._refs.dsplit attempted to split along dimension 2, but the size of the dimension {a.shape[2]} is not divisible by the split_size {sections}!")
     return tensor_split(a, sections, 2)
 
