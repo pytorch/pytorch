7c82bde00122d9b043b29ae7f21a65b93b70c82e
David Chen <davidchencsl@fb.com>
2022-06-02 23:08:31 +0000

    add some stuff to compute self time

--- torch/profiler/utils.py
+++ torch/profiler/utils.py
@@ -2,29 +2,38 @@ from collections import deque
 from dataclasses import dataclass
 from torch.profiler import DeviceType
 
+
 class EventKey:
     def __init__(self, event):
         self.event = event
 
     def __hash__(self):
         return hash(self.event.id)
-    
+
     def __eq__(self, other):
         return self.event.id == other.event.id
 
     def __repr__(self):
-        return f"<{self.event.name()} id={self.event.correlation_id} start={self.event.start_time_ns}>"
-        
+        return f"<{self.event.name()} id={self.event.correlation_id}>"
+
+
 @dataclass
 class EventMetrics:
+    duration_time_ns: int = 0
     self_time_ns: int = 0
     idle_time_ns: int = 0
+    def percent_idle_time(self):
+        if self.duration_time_ns == 0:
+            return 0
+        return self.idle_time_ns / self.duration_time_ns
+
 
 def compute_event_metrics(prof) -> dict[EventKey, EventMetrics]:
     metrics = dict()
     compute_self_time(prof, metrics)
     compute_idle_time(prof, metrics)
     return metrics
+
 
 def compute_self_time(prof, metrics):
     '''
@@ -50,17 +59,19 @@ def compute_self_time(prof, metrics):
             metrics[EventKey(curr_event)].self_time_us = self_time
         else:
             metrics[EventKey(curr_event)] = EventMetrics(self_time_ns=self_time)
+        metrics[EventKey(curr_event)].duration_time_ns = curr_event.duration_time_ns
+
 
 def compute_idle_time(prof, metrics):
     event_tree = prof.profiler.kineto_results.experimental_event_tree()
     event_list = prof.profiler.kineto_results.events()
-    
+
     def is_cuda_launch_kernel(e):
         return e.name() == "cudaLaunchKernel"
-    # TODO: find a better way to identify cudaLaunchKernel
+    
     def is_cuda_kernel(e):
+        # TODO: find a better way to identify cudaLaunchKernel
         return e.device_type() == DeviceType.CUDA and e.name() != "[memory]" and e.name() != "Memset (Device)"
-
 
     # Record All the idle intervals
     queue_depth = 0
@@ -71,7 +82,7 @@ def compute_idle_time(prof, metrics):
     if len(cuda_kernel_events) == 1:
         return
     for i in range(1, len(cuda_kernel_events)):
-        prev_event = cuda_kernel_events[i-1]
+        prev_event = cuda_kernel_events[i - 1]
         curr_event = cuda_kernel_events[i]
         if (is_cuda_launch_kernel(prev_event)):
             queue_depth += 1
@@ -79,18 +90,24 @@ def compute_idle_time(prof, metrics):
             queue_depth -= 1
         if (prev_event.start_us() + prev_event.duration_us()) < curr_event.start_us() and queue_depth == 0:
             idle_interval.append((prev_event.start_us() + prev_event.duration_us(), curr_event.start_us()))
-        #print(f"{prev_event.name()} {prev_event.start_us()} {prev_event.start_us() + prev_event.duration_us()} {queue_depth}") 
-    
+
     # For every event, compute the absolute idle time and the percentage idle time
     # idle_interval Seems correct
+    event_list = [e.event for e in metrics.keys()]
     for event in event_list:
+        idle_time = 0
         for interval in idle_interval:
-            pass
+            overlap_start = max(event.start_time_ns, interval[0] * 1000)
+            overlap_end = min(event.end_time_ns, interval[1] * 1000)
 
-        
+            if overlap_start < overlap_end:
+                idle_time += overlap_end - overlap_start
+        metrics[EventKey(event)].idle_time_ns = idle_time
+
+def get_optimizable_events(prof):
+    metrics = compute_event_metrics(prof)
     
+    # Compute a list of events that that long idle time
 
-            
+    # Print the list of events in human-friendly format
 
-        
-    
