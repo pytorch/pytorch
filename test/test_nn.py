aa725c8716ae37423065bab823b7a8ae491c7f0b
zaf <zaf@devvm10206.prn0.facebook.com>
2022-06-02 03:32:51 -0700

    [quant][ao_migration] `torch.nn.qat` → `torch.ao.nn.qat`
    Context: In order to avoid the cluttering of the `torch.nn` namespace
    the quantized modules namespace is moved to `torch.ao.nn`.
    
    The list of the `nn.quantized` files that are being migrated:
    
    - [X] `torch.nn.quantized` → `torch.ao.nn.quantized`
        - [X] `torch.nn.quantized.functional` → `torch.ao.nn.quantized.functional`
        - [X] `torch.nn.quantized.modules` → `torch.ao.nn.quantized.modules`
        - [X] `torch.nn.quantized.dynamic` → `torch.ao.nn.quantized.dynamic`
        - [X] `torch.nn.quantized._reference` → `torch.ao.nn.quantized._reference`
    - [X] `torch.nn.quantizable` → `torch.ao.nn.quantizable`
    - [X] [Current PR] `torch.nn.qat` → `torch.ao.nn.qat`
        - [X] `torch.nn.qat.modules` → `torch.ao.nn.qat.modules`
        - [X] `torch.nn.qat.dynamic` → `torch.ao.nn.qat.dynamic`
    - [ ] `torch.nn.intrinsic` → `torch.ao.nn.intrinsic`
        - [ ] `torch.nn.intrinsic.modules` → `torch.ao.nn.intrinsic.modules`
        - [ ] `torch.nn.intrinsic.qat` → `torch.ao.nn.intrinsic.qat`
        - [ ] `torch.nn.intrinsic.quantized` → `torch.ao.nn.intrinsic.quantized`
            - [ ] `torch.nn.intrinsic.quantized.modules` → `torch.ao.nn.intrinsic.quantized.modules`
            - [ ] `torch.nn.intrinsic.quantized.dynamic` → `torch.ao.nn.intrinsic.quantized.dynamic`
    
    Majority of the files are just moved to the new location.
    However, specific files need to be double checked:
    
    - None
    
    Differential Revision: [D36861197](https://our.internmc.facebook.com/intern/diff/D36861197/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D36861197/)!
    
    [ghstack-poisoned]

--- test/test_nn.py
+++ test/test_nn.py
@@ -3291,7 +3291,7 @@ class TestNN(NNTestCase):
         parametrize.register_parametrization(model, "weight", MinusOne())
         hold_weight = model.weight
 
-        to_model = nn.qat.Linear(
+        to_model = torch.ao.nn.qat.Linear(
             5, 5, qconfig=torch.ao.quantization.get_default_qconfig()
         )
         parametrize.transfer_parametrizations_and_params(model, to_model)
@@ -3344,7 +3344,7 @@ class TestNN(NNTestCase):
         parametrize.register_parametrization(model, "weight", Double())
         hold_weight = model.weight
 
-        to_model = nn.qat.Linear(
+        to_model = torch.ao.nn.qat.Linear(
             5, 5, qconfig=torch.ao.quantization.get_default_qconfig()
         )
         parametrize.transfer_parametrizations_and_params(model, to_model)
@@ -3382,7 +3382,7 @@ class TestNN(NNTestCase):
         parametrize.register_parametrization(model, "bias", Double())
         parametrize.register_parametrization(model, "bias", MinusOne())
 
-        to_model = nn.qat.Linear(
+        to_model = torch.ao.nn.qat.Linear(
             5, 5, bias=True, qconfig=torch.ao.quantization.get_default_qconfig()
         )
         parametrize.transfer_parametrizations_and_params(model, to_model, "weight")
@@ -3421,7 +3421,7 @@ class TestNN(NNTestCase):
         parametrize.register_parametrization(model, "weight", Double())
         hold_weight = model.weight
 
-        to_model = nn.qat.Linear(
+        to_model = torch.ao.nn.qat.Linear(
             3, 3, qconfig=torch.ao.quantization.get_default_qconfig()
         )
 
