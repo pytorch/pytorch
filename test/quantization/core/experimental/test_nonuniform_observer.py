ffc4930fcb11cc117de2485ae80fbce8118705a5
asl3 <amandaliu@fb.com>
2022-06-02 04:58:29 -0700

    Add param calcs, update tests
    [ghstack-poisoned]

--- test/quantization/core/experimental/test_nonuniform_observer.py
+++ test/quantization/core/experimental/test_nonuniform_observer.py
@@ -6,18 +6,32 @@ import torch
 
 class TestNonUniformObserver(unittest.TestCase):
     def test_calculate_qparams(self):
+        # t = torch.Tensor()
+        # obs = APoTObserver(t, t, t, 0, 0)
+
+        # with self.assertRaises(NotImplementedError):
+        #     obs.calculate_qparams()
         t = torch.Tensor()
         obs = APoTObserver(t, t, t, 0, 0)
 
-        with self.assertRaises(NotImplementedError):
+        raised = False
+        try:
             obs.calculate_qparams()
+        except Exception as e:
+            raised = True
+            print(e)
+        self.assertFalse(raised, 'Exception raised')
 
     def test_override_calculate_qparams(self):
         t = torch.Tensor()
         obs = APoTObserver(t, t, t, 0, 0)
 
-        with self.assertRaises(NotImplementedError):
-            obs._calculate_qparams()
+        raised = False
+        try:
+            obs._calculate_qparams(t, t)
+        except Exception:
+            raised = True
+        self.assertFalse(raised, 'Exception raised')
 
 if __name__ == '__main__':
     unittest.main()
