91723c5b193987e262cfe59e02fd1f12ef1ac8df
swang392 <sarah.wang2009@gmail.com>
2022-06-02 15:09:48 -0400

    modified branch filtering (bc testing locally requires different path)

--- .github/scripts/print_latest_commits.py
+++ .github/scripts/print_latest_commits.py
@@ -26,7 +26,7 @@ def print_latest_commits(minutes: int = 30) -> None:
             "git",
             "rev-list",
             f"--max-age={timestamp_since}",
-            "--remotes=*master",
+            "--remotes=*origin/master",
         ],
         encoding="ascii",
     ).splitlines()
