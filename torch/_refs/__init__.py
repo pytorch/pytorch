f9e27c1b6dcf9b151f2e3e2f73ce0fff0d471f8e
jjsjann123 <alex.jann2012@gmail.com>
2022-06-01 19:48:45 -0700

    fixing exception condition

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2064,7 +2064,7 @@ def tensor_split(
 
 
 def dsplit(a: TensorLikeType, sections: DimsType) -> TensorSequenceType:
-    if a.ndim >= 3 and isinstance(sections, int) and a.shape[2] % sections == 0:
+    if a.ndim >= 3 and isinstance(sections, int) and a.shape[2] % sections != 0:
         raise RuntimeError(f"torch._refs.dsplit attempted to split along dimension 2, but the size of the dimension {a.shape[2]} is not divisible by the split_size {sections}!")
     return tensor_split(a, sections, 2)
 
