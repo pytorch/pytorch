655629a0e1e1f27d3e4c4f174c2c3a6c791937ee
Rohan Varma <rvarm1@fb.com>
2022-06-02 07:46:45 +0000

    Update on "Add apply_activation_checkpointing helper function"
    Adds `apply_activation_checkpointing` helper function, which will allow users to easily checkpoint multiple submodules within a module depending on some condition. 
    
    The function takes in `model`, `checkpoint_wrap_fn` which indicates the checkpoint_wrapper config, and a lambda indicating whether a module should be wrapped or not. It does this wrapper and updates the model in place, i.e.
    
    `Sequential(Linear, Linear)` would become `Sequential(wrapped(Linear), wrapped(Linear))` in place. 
    
    This is useful for several uses cases we've been seeing that have been trying to apply activation checkpointing, where we've had to hack around the module hierarchy via setattr/getattr which can easily lead to issues if not done properly.
    
    [ghstack-poisoned]

--- test/distributed/fsdp/test_checkpoint_wrapper.py
+++ test/distributed/fsdp/test_checkpoint_wrapper.py
@@ -46,9 +46,10 @@ class CheckpointWrapperTest(TestCase):
                 super().__init__()
                 self.lin = nn.Linear(10, 10)
                 self.bn = nn.BatchNorm1d(10)
+                self.nested_linear = nn.Sequential(nn.Linear(10, 10))
 
             def forward(self, x):
-                return self.bn(self.lin(x))
+                return self.bn(self.nested_linear(self.lin(x)))
 
         class MyModel(nn.Module):
             def __init__(self):
@@ -68,19 +69,20 @@ class CheckpointWrapperTest(TestCase):
         print(model)
         for j in range(3):
             self.assertTrue(isinstance(model.seq[j].lin, CheckpointWrapper))
+            self.assertTrue(isinstance(model.seq[j].nested_linear[0], CheckpointWrapper))
 
         inp = torch.randn(4, 10, requires_grad=True)
         for i in range(6):
             loss = model(inp).sum().backward()
             # ensure checkpointed part of model has gradients
             for j in range(3):
-                weight = model.seq[j].lin.mod.weight
-                bias = model.seq[j].lin.mod.bias
-                self.assertTrue(weight.requires_grad)
-                self.assertTrue(bias.requires_grad)
-                self.assertFalse(weight.grad is None)
-                self.assertFalse(bias.grad is None)
-
+                weight_lin = model.seq[j].lin.mod.weight
+                bias_lin = model.seq[j].lin.mod.bias
+                weight_nested_lin = model.seq[j].nested_linear[0].mod.weight
+                bias_nested_lin = model.seq[j].nested_linear[0].mod.bias
+                for param in [weight_lin, bias_lin, weight_nested_lin, bias_nested_lin]:
+                    self.assertTrue(param.requires_grad)
+                    self.assertFalse(param.grad is None)
 
 
 if __name__ == "__main__":
