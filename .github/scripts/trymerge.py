273e6bd602b876390a7b26ab0b7bdc6f1907a2dc
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-06-03 09:54:38 -0500

    fixed naming

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -889,7 +889,7 @@ def prefix_with_github_url(suffix_str: str) -> str:
     return f"https://github.com/{suffix_str}"
 
 
-def merge_on_green(pr_num: int, repo: GitRepo, dry_run: bool = False, timeout_minutes: int = 400) -> None:
+def merge(pr_num: int, repo: GitRepo, dry_run: bool = False, timeout_minutes: int = 400) -> None:
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
     start_time = time.time()
@@ -945,7 +945,7 @@ def main() -> None:
         return
 
     try:
-        merge_on_green(args.pr_num, repo, dry_run=args.dry_run)
+        merge(args.pr_num, repo, dry_run=args.dry_run)
     except Exception as e:
         handle_exception(e)
 
