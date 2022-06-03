697ab59c75c5aa26c3b7fedf3b3fe3e3efde1e2a
David Berard <dberard@fb.com>
2022-06-02 19:17:34 -0700

    fix cpp tests

--- test/cpp/jit/test_create_autodiff_subgraphs.cpp
+++ test/cpp/jit/test_create_autodiff_subgraphs.cpp
@@ -3,12 +3,30 @@
 #include "test/cpp/jit/test_utils.h"
 
 #include "torch/csrc/jit/passes/create_autodiff_subgraphs.h"
+#include "torch/csrc/jit/runtime/graph_iterator.h"
 
 namespace torch {
 namespace jit {
 
 TEST(CreateAutodiffSubgraphsTest, Basic) {
   auto graph = build_lstm();
+  auto add_requires_grad_to_value = [](Value* v) {
+    if (v->type()->kind() == TypeKind::TensorType) {
+      v->setType(v->type()->expectRef<TensorType>().withRequiresGrad(true));
+    }
+  };
+
+  DepthFirstGraphNodeIterator it(graph);
+  Node* n = nullptr;
+  while ((n = it.next()) != nullptr) {
+    for (Value* v : n->outputs()) {
+      add_requires_grad_to_value(v);
+    }
+  }
+  for (Value* v : graph->inputs()) {
+    add_requires_grad_to_value(v);
+  }
+
   CreateAutodiffSubgraphs(graph, /*threshold=*/2);
   // all of the ops are within the DifferentiableGraph
   testing::FileCheck()
