94b9851f8cad2e7375877f9817db7c291c199c04
David Chen <davidchencsl@fb.com>
2022-06-03 17:45:52 +0000

    Computed self time for events

--- torch/profiler/utils.py
+++ torch/profiler/utils.py
@@ -1,14 +1,49 @@
+from hypothesis import event
 import torch
 import torchvision.models as models
 from torch.profiler import profile, record_function, ProfilerActivity
-from objprint import op
+from collections import deque
 
-def compute_self_time(profiler : torch.autograd.profiler.profile):
-    call_tree = profiler.kineto_results.experimental_event_tree()
-    events = profiler.kineto_results.events()
-    op(call_tree)
-    for e in events:
-        print(e)
+
+class EventKey:
+    def __init__(self, event):
+        self.event = event
+
+    def __hash__(self):
+        return hash(self.event.id)
+
+    def __repr__(self):
+        return self.event.name()
+
+
+def compute_self_time(event_tree):
+    '''
+    return a dictionary of EventKey to event's self time (total time - time in child ops).
+
+        Parameters:
+            event_tree: Profiler's kineto_results.experimental_event_tree
+        
+        Returns:
+            result_dict: dictionary of EventKey to event's self time
+    '''
+    stack = deque()
+    for event in event_tree:
+        stack.append(event)
+
+    result_dict = dict()
+    # standard iterating dfs
+    while stack:
+        curr_event = stack.pop()
+        self_time = curr_event.duration_time_ns
+        if curr_event.children:
+            for child_event in curr_event.children:
+                self_time - child_event.duration_time_ns
+                stack.append(child_event)
+        result_dict[EventKey(curr_event)] = self_time
+
+    return result_dict
+
+
 
 
 if __name__ == '__main__':
@@ -18,4 +53,4 @@ if __name__ == '__main__':
     with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
         with record_function("model_inference"):
             model(inputs)
-    compute_self_time(prof.profiler)
+    print(compute_self_time(prof.profiler.kineto_results.experimental_event_tree()))
