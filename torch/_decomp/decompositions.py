ea3c4d0c75c99855d6f6aae2e67860a627052104
Horace He <chilli@fb.com>
2022-06-06 19:55:59 +0000

    Added glu_backward decomp (#78919)
    Requested in https://github.com/pytorch/torchdynamo/issues/327
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78919
    Approved by: https://github.com/mruberry

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -442,6 +442,23 @@ def _nll_loss_backward(
 
     return grad_input * grad_output
 
+
+@register_decomposition(aten.glu_backward)
+@pw_cast_for_opmath
+def glu_backward(grad_output: Tensor, self: Tensor, dim: int) -> Tensor:
+    assert self.dim() > 0, "glu does not support 0-dimensional tensors"
+    wrap_dim = utils.canonicalize_dim(self.dim(), dim)
+    nIn = self.size(wrap_dim)
+    assert nIn % 2 == 0, f"Halving dimension must be even, but dimension {wrap_dim} is size {nIn}"
+    inputSize = nIn // 2
+    firstHalf = self.narrow(wrap_dim, 0, inputSize)
+    secondHalf = self.narrow(wrap_dim, inputSize, inputSize)
+    gradInputFirstHalf = torch.sigmoid(secondHalf)
+    gradInputSecondHalf = (1.0 - gradInputFirstHalf) * gradInputFirstHalf * firstHalf * grad_output
+    gradInputFirstHalf = gradInputFirstHalf * grad_output
+    return torch.cat([gradInputFirstHalf, gradInputSecondHalf], dim=wrap_dim)
+
+
 @register_decomposition(aten.nll_loss_backward)
 def nll_loss_backward(
     grad_output: Tensor,
