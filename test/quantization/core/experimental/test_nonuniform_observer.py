fc743037b219916232fbafd05e6d8b3bbb86383a
asl3 <amandaliu@fb.com>
2022-06-05 17:47:44 -0700

    Add tests for APoT observer param calcs
    [ghstack-poisoned]

--- test/quantization/core/experimental/test_nonuniform_observer.py
+++ test/quantization/core/experimental/test_nonuniform_observer.py
@@ -6,18 +6,57 @@ import torch
 
 class TestNonUniformObserver(unittest.TestCase):
     def test_calculate_qparams(self):
-        t = torch.Tensor()
-        obs = APoTObserver(t, t, t, 0, 0)
+        """
+        cases to test:
+        1. works for empty observer
+        2. works for normal observer (vasiliy playground example)
+        3. alpha is gotten correctly -> test after checking w charlie about computation implementation
+        4. works for normal observer of larger size
+        """
 
-        with self.assertRaises(NotImplementedError):
-            obs.calculate_qparams()
+        obs1 = APoTObserver()
 
-    def test_override_calculate_qparams(self):
-        t = torch.Tensor()
-        obs = APoTObserver(t, t, t, 0, 0)
+        """
+        vasiliy playground example
+        Assume hardcoded parameters:
+        * b = 4 (total number of bits across all terms)
+        * k = 2 (base bitwidth, i.e. bitwidth of every term)
+        * n = 2 (number of additive terms)
+        * note: b = k * n
+        """
+        obs2 = APoTObserver(b = 2, k = 2)
 
-        with self.assertRaises(NotImplementedError):
-            obs._calculate_qparams()
+        ObserverList = [obs1, obs2]
+
+        print(obs2.calculate_qparams(signed=False))
+
+        # t = torch.Tensor()
+        # obs = APoTObserver(t, t, t, 0, 0)
+
+        # with self.assertRaises(NotImplementedError):
+        #     obs.calculate_qparams()
+
+        # t = torch.Tensor()
+        # obs = APoTObserver(t, t, t, 0, 0)
+
+        # raised = False
+        # try:
+        #     obs.calculate_qparams()
+        # except Exception as e:
+        #     raised = True
+        #     print(e)
+        # self.assertFalse(raised, 'Exception raised')
+
+    # def test_override_calculate_qparams(self):
+    #     t = torch.Tensor()
+    #     obs = APoTObserver(t, t, t, 0, 0)
+
+    #     raised = False
+    #     try:
+    #         obs._calculate_qparams(t, t)
+    #     except Exception:
+    #         raised = True
+    #     self.assertFalse(raised, 'Exception raised')
 
 if __name__ == '__main__':
     unittest.main()
