c461d8a9777ad54ab5a5587d8c67bf2d19bca894
Kshiteej K <kshitijkalambarkar@gmail.com>
2022-06-06 19:54:05 +0000

    [primTorch] refs: hsplit, vsplit (#78418)
    As per title
    
    TODO:
    * [x] Add error inputs (already exist)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78418
    Approved by: https://github.com/mruberry

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -1731,7 +1731,7 @@ This function is based on NumPy's :func:`numpy.hsplit`.
 
 Args:
     input (Tensor): tensor to split.
-    indices_or_sections (Tensor, int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
+    indices_or_sections (int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
 
 Example::
     >>> t = torch.arange(16.0).reshape(4,4)
@@ -1778,7 +1778,7 @@ This function is based on NumPy's :func:`numpy.vsplit`.
 
 Args:
     input (Tensor): tensor to split.
-    indices_or_sections (Tensor, int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
+    indices_or_sections (int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
 
 Example::
     >>> t = torch.arange(16.0).reshape(4,4)
@@ -1817,7 +1817,7 @@ This function is based on NumPy's :func:`numpy.dsplit`.
 
 Args:
     input (Tensor): tensor to split.
-    indices_or_sections (Tensor, int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
+    indices_or_sections (int or list or tuple of ints): See argument in :func:`torch.tensor_split`.
 
 Example::
     >>> t = torch.arange(16.0).reshape(2, 2, 4)
