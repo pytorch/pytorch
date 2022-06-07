1e27d38165b9014705fb5bfd51de16d39caaaaab
Yifan Shen <yifanshensz@fb.com>
2022-06-07 15:50:10 -0400

    use maybe_wrap_dim for canonical error message; use opt_size(0) for number of orignal tensors; follow NestedTensorTransformerFunctions.cpp to use pointer access for prod

--- aten/src/ATen/native/nested/NestedTensorMath.cpp
+++ aten/src/ATen/native/nested/NestedTensorMath.cpp
@@ -563,31 +563,49 @@ Tensor select_nested(const Tensor& self, int64_t dim, int64_t index) {
     "NestedTensor can only be selected along dimension 0 ",
     "got dimension ", dim, " instead."
   );
-  // size_matrix.dim() can only have 2 possible values:
-  //     * 2, as expected
-  //     * 0, when the nested tensor is empty
-  Tensor sizemat = get_nested_size_tensor(self);
+  auto self_ptr = get_nested_tensor_impl(self);
+  // buffer contains the underlying data in a contiguous vector
+  const at::Tensor & buffer = self_ptr->get_buffer();
+  int64_t numel = buffer.numel();
   TORCH_CHECK(
-    sizemat.dim() == 2,
+    numel > 0,
     "cannot index an empty nested tensor."
   );
   // nested_tensor[i] = i-th original tensor
-  int64_t ntensors = sizemat.size(0);
-  TORCH_CHECK_INDEX(
-    index >= -ntensors && index < ntensors,
-    "index ", index, " is out of bounds for nested tensor dimension 0 with size ", ntensors);
-  int64_t positive_index = index < 0 ? index + ntensors : index;
-  // buffer contains the underlying data in a contiguous vector
-  Tensor buffer = get_buffer(self);
-  // determine the memory section of the i-th original tensor
-  Tensor numels = at::prod(sizemat, 1);
-  const int64_t * pt_numels = numels.data_ptr<int64_t>();
-  int64_t start = 0;
-  for (int64_t row = 0; row < positive_index; row++) start += pt_numels[row];
-  int64_t stop  = start + pt_numels[positive_index];
-  // determine the shape of the i-th original tensor
-  int64_t * pt_sizematrow = sizemat[positive_index].data_ptr<int64_t>();
-  IntArrayRef shape(pt_sizematrow, pt_sizematrow + sizemat.size(1));
+  int64_t ntensors = *(self_ptr->opt_size(0));
+  int64_t positive_index = at::maybe_wrap_dim(index, ntensors);
+  // determine the memory segment of the i-th original tensor
+  Tensor sizemat = get_nested_size_tensor(self);
+  int64_t original_dim = sizemat.size(1);
+  const int64_t * sizemat_ptr = sizemat.data_ptr<int64_t>();
+  // start of the segment
+  int64_t start = 0, sizemat_offset = 0;
+  for (int64_t i = 0; i < positive_index; i++) {
+    int64_t row_product = sizemat_ptr[sizemat_offset];
+    sizemat_offset++;
+    for (int64_t j = 1; j < original_dim; j++) {
+      row_product *= sizemat_ptr[sizemat_offset];
+      sizemat_offset++;
+    }
+    start += row_product;
+  }
+  // btw determine the shape of the i-th original tensor
+  IntArrayRef shape(sizemat_ptr + sizemat_offset, sizemat_ptr + sizemat_offset + original_dim);
+  // stop of the segment
+  int64_t stop;
+  if (positive_index == ntensors - 1) {
+    stop = numel;
+  }
+  else {
+    int64_t row_product = sizemat_ptr[sizemat_offset];
+    sizemat_offset++;
+    for (int64_t j = 1; j < original_dim; j++) {
+      row_product *= sizemat_ptr[sizemat_offset];
+      sizemat_offset++;
+    }
+    stop = start + row_product;
+  }
+  // extract the memory segment then reshape to the original shape
   return buffer.slice(0, start, stop).view(shape);
 }
 
