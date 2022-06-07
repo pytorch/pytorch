bb8a40f0ceaad7b2ad05e3436b79c2697041486b
Edward Z. Yang <ezyang@fb.com>
2022-06-07 10:22:38 -0700

    Black torch._meta_registrations
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -8,13 +8,15 @@ from typing import List, Optional
 
 meta_lib = torch.library.Library("aten", "IMPL", "Meta")
 
+
 def toRealValueType(dtype):
     from_complex = {
         torch.complex32: torch.half,
         torch.cfloat: torch.float,
-        torch.cdouble: torch.double
+        torch.cdouble: torch.double,
     }
     return from_complex.get(dtype, dtype)
+
 
 # Implementations below are taken from https://github.com/albanD/subclass_zoo/blob/main/python_meta_tensor.py
 @torch.library.impl(meta_lib, "index_select")
@@ -24,30 +26,40 @@ def meta_index_select(self, dim, index):
         result_size[dim] = index.numel()
     return self.new_empty(result_size)
 
+
 @torch.library.impl(meta_lib, "index_select.out")
 def meta_index_select_out(self, dim, index, out):
     torch._resize_output_(out, self.size(), self.device)
     return out.copy_(torch.index_select(self, dim, index))
 
+
 @torch.library.impl(meta_lib, "max")
 def meta_max(self):
     return self.new_empty(())
+
 
 @torch.library.impl(meta_lib, "min")
 def meta_min(self):
     return self.new_empty(())
 
+
 def squareCheckInputs(self, f_name):
-    assert self.dim() >= 2, f"{f_name}: The input tensor must have at least 2 dimensions."
+    assert (
+        self.dim() >= 2
+    ), f"{f_name}: The input tensor must have at least 2 dimensions."
     # TODO: I think the error message has the -2 and -1 swapped.  If you fix
     # it fix the C++ squareCheckInputs too
-    assert self.size(-1) == self.size(-2), \
-        f"{f_name}: A must be batches of square matrices, but they are {self.size(-1)} by {self.size(-2)} matrices"
+    assert self.size(-1) == self.size(
+        -2
+    ), f"{f_name}: A must be batches of square matrices, but they are {self.size(-1)} by {self.size(-2)} matrices"
+
 
 def checkUplo(uplo: str):
     uplo_uppercase = uplo.upper()
-    assert len(uplo) == 1 and uplo_uppercase == 'U' or uplo_uppercase == 'L', \
-        f"Expected UPLO argument to be 'L' or 'U', but got {uplo}"
+    assert (
+        len(uplo) == 1 and uplo_uppercase == "U" or uplo_uppercase == "L"
+    ), f"Expected UPLO argument to be 'L' or 'U', but got {uplo}"
+
 
 @torch.library.impl(meta_lib, "linalg_eigh")
 def meta_linalg_eigh(self, uplo="L"):
@@ -60,13 +72,14 @@ def meta_linalg_eigh(self, uplo="L"):
     vectors = self.new_empty(self.shape[:-1])
     return (values, vectors)
 
+
 @torch.library.impl(meta_lib, "reflection_pad2d")
 def meta_pad2d(self, padding):
     valid_dims = self.size(1) != 0 and self.size(2) != 0
     check(
         (self.ndim == 3 and valid_dims)
         or (self.ndim == 4 and valid_dims and self.size(3) != 0),
-        lambda: f"3D or 4D (batch mode) tensor expected for input, but got: {self}"
+        lambda: f"3D or 4D (batch mode) tensor expected for input, but got: {self}",
     )
     if self.ndim == 4:
         nbatch, nplane, input_h, input_w = self.shape
@@ -84,24 +97,29 @@ def meta_pad2d(self, padding):
     else:
         return self.new_empty((nbatch, nplane, output_h, output_w))
 
+
 @torch.library.impl(meta_lib, "dot")
 def meta_dot(self, tensor):
     check(
         self.dim() == 1 and tensor.dim() == 1,
-        lambda: f"1D tensors expected, but got {self.dim()}D and {tensor.dim()}D tensors"
+        lambda: f"1D tensors expected, but got {self.dim()}D and {tensor.dim()}D tensors",
     )
     return self.new_empty(())
+
 
 @torch.library.impl(meta_lib, "var_mean.correction")
 def meta_var_mean_correction(self, dim, *, correction, keepdim=False):
     dim = utils.reduction_dims(self.shape, dim)
     if keepdim:
-        output_shape = tuple(self.shape[i] if i not in dim else 1 for i in range(self.ndim))
+        output_shape = tuple(
+            self.shape[i] if i not in dim else 1 for i in range(self.ndim)
+        )
     else:
         output_shape = utils.compute_reduction_output_shape(self.shape, dim)
     result1 = self.new_empty(output_shape, dtype=toRealValueType(self.dtype))
     result2 = self.new_empty(output_shape)
     return result1, result2
+
 
 @torch.library.impl(meta_lib, "inverse")
 def meta_inverse(self):
@@ -112,28 +130,37 @@ def meta_inverse(self):
     r.transpose_(-2, -1)
     return r
 
+
 @torch.library.impl(meta_lib, "bernoulli.out")
 def meta_bernoulli(self, *, generator=None, out):
     torch._resize_output_(out, self.size(), self.device)
     return out
 
+
 @torch.library.impl(meta_lib, "_adaptive_avg_pool2d")
 def meta_adaptive_avg_pool2d(self, output_size):
-    check(self.ndim == 3 or self.ndim == 4, lambda: f"Expected 3D or 4D tensor, but got {self.shape}")
+    check(
+        self.ndim == 3 or self.ndim == 4,
+        lambda: f"Expected 3D or 4D tensor, but got {self.shape}",
+    )
     return self.new_empty(self.shape[:-2] + tuple(output_size))
+
 
 @torch.library.impl(meta_lib, "_adaptive_avg_pool3d")
 def meta_adaptive_avg_pool3d(self, output_size):
-    check(self.ndim == 4 or self.ndim == 5, lambda: f"Expected 4D or 5D tensor, but got {self.shape}")
+    check(
+        self.ndim == 4 or self.ndim == 5,
+        lambda: f"Expected 4D or 5D tensor, but got {self.shape}",
+    )
     return self.new_empty(self.shape[:-3] + tuple(output_size))
+
 
 @torch.library.impl(meta_lib, "repeat_interleave.Tensor")
 def meta_repeat_interleave_Tensor(repeats, output_size=None):
     if output_size is None:
-        raise RuntimeError(
-            "cannot repeat_interleave a meta tensor without output_size"
-        )
+        raise RuntimeError("cannot repeat_interleave a meta tensor without output_size")
     return repeats.new_empty(output_size)
+
 
 @torch.library.impl(meta_lib, "_linalg_qr_helper")
 def meta_linalg_qr_helper(input, mode):
@@ -148,10 +175,13 @@ def meta_linalg_qr_helper(input, mode):
         reduced_mode = True
     else:
         raise RuntimeError(f"qr received unrecognized mode {mode}")
-    check(input.ndim >= 2, lambda: f"expected matrix or batch of matrices, but got {input.ndim}-D tensor")
+    check(
+        input.ndim >= 2,
+        lambda: f"expected matrix or batch of matrices, but got {input.ndim}-D tensor",
+    )
     check(
         utils.is_float_dtype(input.dtype) or utils.is_complex_dtype(input.dtype),
-        lambda: f"expected float or complex tensor, but got {input.dtype}"
+        lambda: f"expected float or complex tensor, but got {input.dtype}",
     )
     m = input.size(-2)
     n = input.size(-1)
@@ -171,6 +201,7 @@ def meta_linalg_qr_helper(input, mode):
     R.transpose_(-2, -1)
     return (Q, R)
 
+
 @torch.library.impl(meta_lib, "index.Tensor")
 def meta_index_Tensor(self, indices):
     check(indices, lambda: "at least one index must be provided")
@@ -181,7 +212,7 @@ def meta_index_Tensor(self, indices):
         if index is not None:
             check(
                 index.dtype in [torch.long, torch.int8, torch.bool],
-                lambda: "tensors used as indices must be long, byte or bool tensors"
+                lambda: "tensors used as indices must be long, byte or bool tensors",
             )
             if index.dtype in [torch.int8, torch.bool]:
                 nonzero = index.nonzero()
@@ -189,14 +220,14 @@ def meta_index_Tensor(self, indices):
                 check(
                     k + index.ndim <= self.ndim,
                     lambda: f"too many indices for tensor of dimension {self.ndim}",
-                    IndexError
+                    IndexError,
                 )
                 for j in range(index.ndim):
                     check(
                         index.shape[j] == self.shape[k + j],
                         lambda: f"The shape of the mask {index.shape} at index {i} "
-                                f"does not match the shape of the indexed tensor {self.shape} at index {k + j}",
-                        IndexError
+                        f"does not match the shape of the indexed tensor {self.shape} at index {k + j}",
+                        IndexError,
                     )
                     result.append(nonzero.select(1, j))
             else:
@@ -204,9 +235,13 @@ def meta_index_Tensor(self, indices):
         else:
             result.append(index)
     indices = result
-    check(len(indices) <= self.ndim, lambda: f"too many indices for tensor of dimension {self.ndim} (got {len(indices)})")
+    check(
+        len(indices) <= self.ndim,
+        lambda: f"too many indices for tensor of dimension {self.ndim} (got {len(indices)})",
+    )
     # expand_outplace
     import torch._refs as refs  # avoid import cycle in mypy
+
     indices = list(refs._maybe_broadcast(*indices))
     # add missing null tensors
     while len(indices) < self.ndim:
@@ -268,22 +303,31 @@ def meta_index_Tensor(self, indices):
             replacement_shape = list(index.shape)
     return self.new_empty(before_shape + replacement_shape + after_shape)
 
+
 @out_wrapper_multi("L", "info")
 def meta_linalg_cholesky_ex(input, upper=False, check_errors=False):
-    check(input.ndim >= 2, lambda: f"expected matrix or batch of matrices, but got {input.ndim}-D tensor")
+    check(
+        input.ndim >= 2,
+        lambda: f"expected matrix or batch of matrices, but got {input.ndim}-D tensor",
+    )
     check(
         utils.is_float_dtype(input.dtype) or utils.is_complex_dtype(input.dtype),
-        lambda: f"expected float or complex tensor, but got {input.dtype}"
+        lambda: f"expected float or complex tensor, but got {input.dtype}",
     )
-    check(input.size(-1) == input.size(-2), lambda: f"expected square matrix but got {input.shape}")
+    check(
+        input.size(-1) == input.size(-2),
+        lambda: f"expected square matrix but got {input.shape}",
+    )
     L = input.new_empty(input.size())
     L.transpose_(-2, -1)
     info_sizes = input.size()[:-2]
     info = input.new_empty(info_sizes, dtype=torch.int)
     return L, info
 
+
 torch.library.impl(meta_lib, "linalg_cholesky_ex")(meta_linalg_cholesky_ex)
 torch.library.impl(meta_lib, "linalg_cholesky_ex.L")(meta_linalg_cholesky_ex)
+
 
 @out_wrapper
 def meta_addbmm(self, batch1, batch2, *, beta=1, alpha=1):
@@ -294,36 +338,53 @@ def meta_addbmm(self, batch1, batch2, *, beta=1, alpha=1):
     check(batch2.dim() == 3, lambda: "batch2 must be a 3D tensor")
     check(
         batch1.size(0) == batch2.size(0),
-        lambda: f"batch1 and batch2 must have same number of batches, got {batch1.size(0)} and {batch2.size(0)}"
+        lambda: f"batch1 and batch2 must have same number of batches, got {batch1.size(0)} and {batch2.size(0)}",
     )
     check(
         batch1.size(2) == batch2.size(1),
         lambda: (
             f"Incompatible matrix sizes for bmm ({batch1.size(1)}x{batch1.size(2)} "
             f"and {batch2.size(1)}x{batch2.size(2)})"
-        )
+        ),
     )
     check(
         self.size(0) == dim1 and self.size(1) == dim2,
-        lambda: "self tensor does not match matmul output shape"
+        lambda: "self tensor does not match matmul output shape",
     )
     return self.new_empty(self.size())
+
 
 torch.library.impl(meta_lib, "addbmm")(meta_addbmm)
 torch.library.impl(meta_lib, "addbmm.out")(meta_addbmm)
 
+
 @torch.library.impl(meta_lib, "_cdist_forward")
 def meta_cdist_forward(x1, x2, p, compute_mode):
-    check(x1.dim() >= 2, lambda: f"cdist only supports at least 2D tensors, X1 got: {x1.dim()}D")
-    check(x2.dim() >= 2, lambda: f"cdist only supports at least 2D tensors, X2 got: {x2.dim()}D")
+    check(
+        x1.dim() >= 2,
+        lambda: f"cdist only supports at least 2D tensors, X1 got: {x1.dim()}D",
+    )
+    check(
+        x2.dim() >= 2,
+        lambda: f"cdist only supports at least 2D tensors, X2 got: {x2.dim()}D",
+    )
     check(
         x1.size(-1) == x2.size(-1),
-        lambda: f"X1 and X2 must have the same number of columns. X1: {x1.size(-1)} X2: {x2.size(-1)}"
+        lambda: f"X1 and X2 must have the same number of columns. X1: {x1.size(-1)} X2: {x2.size(-1)}",
     )
-    check(utils.is_float_dtype(x1.dtype), lambda: "cdist only supports floating-point dtypes, X1 got: {x1.dtype}")
-    check(utils.is_float_dtype(x2.dtype), lambda: "cdist only supports floating-point dtypes, X2 got: {x2.dtype}")
+    check(
+        utils.is_float_dtype(x1.dtype),
+        lambda: "cdist only supports floating-point dtypes, X1 got: {x1.dtype}",
+    )
+    check(
+        utils.is_float_dtype(x2.dtype),
+        lambda: "cdist only supports floating-point dtypes, X2 got: {x2.dtype}",
+    )
     check(p >= 0, lambda: "cdist only supports non-negative p values")
-    check(compute_mode >= 0 and compute_mode <= 2, lambda: f"possible modes: 0, 1, 2, but was: {compute_mode}")
+    check(
+        compute_mode >= 0 and compute_mode <= 2,
+        lambda: f"possible modes: 0, 1, 2, but was: {compute_mode}",
+    )
     r1 = x1.size(-2)
     r2 = x2.size(-2)
     batch_tensor1 = x1.shape[:-2]
@@ -332,28 +393,46 @@ def meta_cdist_forward(x1, x2, p, compute_mode):
     output_shape.extend([r1, r2])
     return x1.new_empty(output_shape)
 
+
 @torch.library.impl(meta_lib, "_embedding_bag")
 def meta_embedding_bag(
-    weight, indices, offsets, scale_grad_by_freq=False, mode=0,
-    sparse=False, per_sample_weights=None, include_last_offset=False, padding_idx=-1
+    weight,
+    indices,
+    offsets,
+    scale_grad_by_freq=False,
+    mode=0,
+    sparse=False,
+    per_sample_weights=None,
+    include_last_offset=False,
+    padding_idx=-1,
 ):
-    check(indices.dtype in (torch.long, torch.int), lambda: f"expected indices to be long or int, got {indices.dtype}")
-    check(offsets.dtype in (torch.long, torch.int), lambda: f"expected offsets to be long or int, got {offsets.dtype}")
+    check(
+        indices.dtype in (torch.long, torch.int),
+        lambda: f"expected indices to be long or int, got {indices.dtype}",
+    )
+    check(
+        offsets.dtype in (torch.long, torch.int),
+        lambda: f"expected offsets to be long or int, got {offsets.dtype}",
+    )
     check(
         utils.is_float_dtype(weight.dtype),
-        lambda: f"expected weight to be floating point type, got {weight.dtype}"
+        lambda: f"expected weight to be floating point type, got {weight.dtype}",
     )
 
     num_bags = offsets.size(0)
     if include_last_offset:
-        check(num_bags >= 1, lambda: "include_last_offset: numBags should be at least 1")
+        check(
+            num_bags >= 1, lambda: "include_last_offset: numBags should be at least 1"
+        )
         num_bags -= 1
 
     output = weight.new_empty(num_bags, weight.size(1))
     MODE_SUM, MODE_MEAN, MODE_MAX = range(3)
 
     def is_fast_path_index_select_scale(src, scale, output, padding_idx):
-        return is_fast_path_index_select(src, output, padding_idx) and scale.stride(0) == 1
+        return (
+            is_fast_path_index_select(src, output, padding_idx) and scale.stride(0) == 1
+        )
 
     def is_fast_path_index_select(src, output, padding_idx):
         return (
@@ -368,7 +447,6 @@ def meta_embedding_bag(
             return is_fast_path_index_select_scale(src, scale, output, padding_idx)
         else:
             return is_fast_path_index_select(src, output, padding_idx)
-
 
     if offsets.device.type != "cpu":
         offset2bag = indices.new_empty(indices.size(0))
@@ -387,9 +465,12 @@ def meta_embedding_bag(
         max_indices = offsets.new_empty(bag_size.size())
     return output, offset2bag, bag_size, max_indices
 
+
 @torch.library.impl(meta_lib, "_embedding_bag_forward_only")
 def meta_embedding_bag_forward_only(weight, indices, offsets, *args):
-    output, offset2bag, bag_size, max_indices = meta_embedding_bag(weight, indices, offsets, *args)
+    output, offset2bag, bag_size, max_indices = meta_embedding_bag(
+        weight, indices, offsets, *args
+    )
     if offsets.device.type == "cpu":
         bag_size = offsets.new_empty(offsets.size())
     return output, offset2bag, bag_size, max_indices
