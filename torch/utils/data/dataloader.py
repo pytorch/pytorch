621ed8aabd1bb928506d387f2dc7e692d56d21f5
erjia <erjia@fb.com>
2022-06-07 18:48:38 +0000

    Fix the place to retreive distributed settings

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -92,26 +92,26 @@ class _InfiniteConstantSampler(Sampler):
             yield None
 
 
-def _apply_distributed_sharding(datapipe):
+def _get_distributed_settings():
     if dist.is_available() and dist.is_initialized():
-        total_workers = dist.get_world_size()
-        global_worker_id = dist.get_rank()
-        torch.utils.data.graph_settings.apply_sharding(datapipe, total_workers, global_worker_id)
-    return datapipe
+        return dist.get_world_size(), dist.get_rank()
+    else:
+        return 1, 0
 
-def _sharding_worker_init_fn(worker_init_fn, worker_id):
+
+def _sharding_worker_init_fn(worker_init_fn, world_size, rank_id, worker_id):
     global_worker_id = worker_id
     info = torch.utils.data.get_worker_info()
     total_workers = info.num_workers
     datapipe = info.dataset
     # To distribute elements across distributed process evenly, we should shard data on distributed
     # processes first then shard on worker processes
-    if dist.is_available() and dist.is_initialized():
-        total_workers *= dist.get_world_size()
-        global_worker_id = global_worker_id * dist.get_world_size() + dist.get_rank()
+    total_workers *= world_size
+    global_worker_id = global_worker_id * world_size + rank_id
     torch.utils.data.graph_settings.apply_sharding(datapipe, total_workers, global_worker_id)
     if worker_init_fn is not None:
         worker_init_fn(worker_id)
+
 
 class DataLoader(Generic[T_co]):
     r"""
@@ -248,19 +248,19 @@ class DataLoader(Generic[T_co]):
         # 2. Additional worker init function will take care of sharding in MP and Distributed
         if isinstance(self.dataset, IterDataPipe):
             self.dataset = _IterDataPipeSerializationWrapper(self.dataset)
+            ws, rank = _get_distributed_settings()
             if num_workers > 0:
                 self.worker_init_fn = functools.partial(
-                    _sharding_worker_init_fn, self.worker_init_fn)
+                    _sharding_worker_init_fn, self.worker_init_fn, ws, rank)
             else:
-                self.dataset = _apply_distributed_sharding(self.dataset)
+                self.dataset = torch.utils.data.graph_settings.apply_sharding(self.dataset, ws, rank)
         elif isinstance(self.dataset, MapDataPipe):
             self.dataset = _MapDataPipeSerializationWrapper(self.dataset)
             if num_workers > 0:
                 self.worker_init_fn = functools.partial(
-                    _sharding_worker_init_fn, self.worker_init_fn)
+                    _sharding_worker_init_fn, self.worker_init_fn, ws, rank)
             else:
-                self.dataset = _apply_distributed_sharding(self.dataset)
-
+                self.dataset = torch.utils.data.graph_settings.apply_sharding(self.dataset, ws, rank)
 
 
         # Arg-check dataset related before checking samplers because we want to
