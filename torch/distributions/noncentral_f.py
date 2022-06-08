0a3c33287f8a74c7244e396086079837fe5ff74f
Allen Goodman <allen.goodman@icloud.com>
2022-06-08 15:52:29 -0400

    distributions

---
+++ torch/distributions/noncentral_f.py
@@ -0,0 +1,48 @@
+from typing import Any, Dict, Optional
+
+from torch import Size
+from torch.distributions.constraints import Constraint
+from torch.distributions.distribution import Distribution
+
+
+class NoncentralF(Distribution):
+    @property
+    def arg_constraints(self) -> Dict[str, Constraint]:
+        return {}
+
+    @property
+    def mean(self):
+        return 0.0
+
+    @property
+    def mode(self):
+        return 0.0
+
+    @property
+    def support(self) -> Optional[Any]:
+        return 0.0
+
+    @property
+    def variance(self):
+        return 0.0
+
+    def cdf(self, value):
+        pass
+
+    def entropy(self):
+        pass
+
+    def enumerate_support(self, expand=True):
+        pass
+
+    def expand(self, batch_shape, _instance=None):
+        pass
+
+    def icdf(self, value):
+        pass
+
+    def log_prob(self, value):
+        pass
+
+    def rsample(self, sample_shape=Size()):
+        pass
