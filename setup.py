826a26f4703400ef4ffa8985d768a28edcbafdb7
atalman <atalman@fb.com>
2022-06-08 09:57:58 -0700

    Testing delocate

--- setup.py
+++ setup.py
@@ -454,13 +454,15 @@ class build_ext(setuptools.command.build_ext.build_ext):
             return
 
         # Copy libiomp5 from rpath locations
+        report('--Copy libiomp5 from rpath locations')
         for rpath in rpaths:
             source_lib = os.path.join(rpath, omp_lib_name)
             if not os.path.exists(source_lib):
                 continue
             target_lib = os.path.join(self.build_lib, 'torch', 'lib', omp_lib_name)
             self.copy_file(source_lib, target_lib)
-            subprocess.check_call(['install_name_tool', '-id', '@rpath/libiomp5.dylib', target_lib])
+            report('--Copying libomp to: '+target_lib + ' from '+ source_lib)
+            report(subprocess.check_call(['install_name_tool', '-id', '@rpath/libiomp5.dylib', target_lib]))
             break
 
         # Delete rpath from those libs
