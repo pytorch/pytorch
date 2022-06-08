b21e24fdf8235981ee63e6cf3a85b27b7bb6ea13
Michael Andreas Dagitses <mikeyd@fb.com>
2022-06-08 13:09:46 -0700

    turn on -Werror=unused-function in our Bazel CPU build
    Summary:
    We also fix any existing issues. Note that we only do this for the CPU
    build because nvcc is considered a C++ toolchain but it does not have
    the same flag support. Adding flags to the GPU build will cause nvcc
    errors.
    
    Test Plan: Built locally, rely on CI to confirm.
    
    Reviewers: malfet
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- c10/test/util/exception_test.cpp
+++ c10/test/util/exception_test.cpp
@@ -5,9 +5,6 @@
 using c10::Error;
 
 namespace {
-bool throw_func() {
-  throw std::runtime_error("I'm throwing...");
-}
 
 template <class Functor>
 inline void expectThrowsEq(Functor&& functor, const char* expectedMessage) {
@@ -28,7 +25,7 @@ TEST(ExceptionTest, TORCH_INTERNAL_ASSERT_DEBUG_ONLY) {
   ASSERT_NO_THROW(TORCH_INTERNAL_ASSERT_DEBUG_ONLY(false));
   // Does nothing - `throw_func()` should not be evaluated
   // NOLINTNEXTLINE(cppcoreguidelines-avoid-goto,hicpp-avoid-goto)
-  ASSERT_NO_THROW(TORCH_INTERNAL_ASSERT_DEBUG_ONLY(throw_func()));
+  ASSERT_NO_THROW(TORCH_INTERNAL_ASSERT_DEBUG_ONLY(throw std::runtime_error("I'm throwing...")));
 #else
   ASSERT_THROW(TORCH_INTERNAL_ASSERT_DEBUG_ONLY(false), c10::Error);
   ASSERT_NO_THROW(TORCH_INTERNAL_ASSERT_DEBUG_ONLY(true));
