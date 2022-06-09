b6bba1c5afe7ba89b6b2b651a4b7e751e90ff5fc
Michael Andreas Dagitses <mikeyd@fb.com>
2022-06-09 08:46:43 -0700

    Update base for Update on "fix some unused-parameter warnings"
    Summary:
    We have a lot of these warnings, but this doesn't fix enough for us to
    turn it into an error.
    
    Test Plan: Rely on CI.
    
    Reviewers: malfet
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/csrc/StorageMethods.cpp
+++ torch/csrc/StorageMethods.cpp
@@ -477,7 +477,7 @@ static PyMethodDef THPStorage_methods[] = {
   {"from_file", castPyCFunctionWithKeywords(THPStorage_fromFile),
     METH_VARARGS | METH_KEYWORDS | METH_STATIC, nullptr},
   {"_set_cdata", THPStorage__setCdata, METH_O, nullptr},
-  {nullptr}
+  {nullptr, nullptr, 0, nullptr}
 };
 
 PyMethodDef* THPStorage_getMethods() {
