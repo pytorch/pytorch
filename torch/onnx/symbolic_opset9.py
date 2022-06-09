ec0a818cbf60a254572561c4ba9821d73092e1a0
qqaatw <qqaatw@gmail.com>
2022-06-09 19:41:09 +0000

    Add onnx support for `movedim` and `moveaxis` (#78931)
    Fixes #68918
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78931
    Approved by: https://github.com/BowenBao

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -4758,6 +4758,38 @@ def dot(g, self, other):
     return matmul(g, self, other)
 
 
+@symbolic_helper.parse_args("v", "t", "t")
+def movedim(g, self, source, destination):
+    # This is a pythonic implementation mostly taken from aten/src/ATen/native/TensorShape.cpp::movedim
+    source = source.view(-1)
+    destination = destination.view(-1)
+
+    assert source.size() == destination.size()
+
+    if (source == destination).all():
+        return self
+
+    self_rank = symbolic_helper._get_tensor_rank(self)
+
+    perm = list(range(self_rank))
+
+    src_dims = perm.copy()
+    dst_dims = perm.copy()
+
+    for src, dst in zip(source.tolist(), destination.tolist()):
+        perm[dst] = src
+        src_dims[src] = -1
+        dst_dims[dst] = -1
+
+    src_dims = [dim for dim in src_dims if dim != -1]
+    dst_dims = [dim for dim in dst_dims if dim != -1]
+
+    for src, dst in zip(src_dims, dst_dims):
+        perm[dst] = src
+
+    return g.op("Transpose", self, perm_i=perm)
+
+
 @symbolic_helper.parse_args("v", "v")
 def fill(g, self, value):
     dtype = self.type().scalarType()
