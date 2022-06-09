be8fe0408f347bb159edff4e6c6c5189466bf676
Michael Andreas Dagitses <mikeyd@fb.com>
2022-06-09 10:15:18 -0700

    Update base for Update on "fix some unused-parameter warnings"
    Summary:
    We have a lot of these warnings, but this doesn't fix enough for us to
    turn it into an error.
    
    Test Plan: Rely on CI.
    
    Reviewers: malfet
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/lib/libshm/manager.cpp
+++ torch/lib/libshm/manager.cpp
@@ -38,7 +38,7 @@ std::unordered_map<int, ClientSession> client_sessions;
 std::set<std::string> used_objects;
 
 void register_fd(int fd) {
-  pollfds.push_back({/*fd=*/fd, /*events=*/POLLIN, /*revents=*/0);
+  pollfds.push_back({/*fd=*/fd, /*events=*/POLLIN, /*revents=*/0});
 }
 
 void unregister_fd(int fd) {
