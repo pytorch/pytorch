614b231567e828e00abf5df58c799731eeabcffb
swang392 <sarah.wang2009@gmail.com>
2022-06-09 11:30:07 -0400

    isGreen returns error message instead of False if commit was not green

--- .github/scripts/print_latest_commits.py
+++ .github/scripts/print_latest_commits.py
@@ -36,7 +36,7 @@ def print_latest_commits(qlambda: Any, minutes: int = 30) -> None:
             "git",
             "rev-list",
             f"--max-age={timestamp_since}",
-            "--remotes=*origin/master",
+            "--remotes=*master",
         ],
         encoding="ascii",
     ).splitlines()
@@ -67,7 +67,7 @@ def get_commit_results(commit: str, results: Dict[str, Any]) -> List[Dict[str, A
             )._asdict())
     return workflow_checks
 
-def isGreen(commit: str, results: Dict[str, Any]) -> bool:
+def isGreen(commit: str, results: Dict[str, Any]) -> Any:
     workflow_checks = get_commit_results(commit, results)
 
     for check in workflow_checks:
@@ -78,9 +78,9 @@ def isGreen(commit: str, results: Dict[str, Any]) -> bool:
                 pass
                 # there are trunk checks that run the same tests, so this pull workflow check can be skipped
             else:
-                return False
+                return workflowName + " checks were not successful"
         elif workflowName in ["periodic", "docker-release-builds"] and conclusion not in ["success", "skipped"]:
-            return False
+            return workflowName + " checks were not successful"
     return True
 
 def main() -> None:
