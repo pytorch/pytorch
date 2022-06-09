3e73ce18c4bee9e5e7684d871fa6d872f5dcc6a6
Red Magnos <redmagnos@gmail.com>
2022-06-08 21:49:39 -0300

    Fetch number of dimensions from shape array

--- aten/src/ATen/native/cuda/ScatterGatherKernel.cu
+++ aten/src/ATen/native/cuda/ScatterGatherKernel.cu
@@ -183,7 +183,7 @@ struct _cuda_scatter_large_index_internal_kernel {
     char* src_ptr = (char*)iter.data_ptr(1);
     char* index_ptr = (char*)iter.data_ptr(2);
 
-    int ndim = iter.ndim();
+    int ndim = index_shape.size();
     int64_t index_shape_host[ndim], index_strides_host[ndim], src_shape_host[ndim], src_strides_host[ndim];
     int64_t *index_shape_device, *index_strides_device, *src_shape_device, *src_strides_device;
     cudaMalloc((void**)&index_shape_device, ndim * sizeof(int64_t));
