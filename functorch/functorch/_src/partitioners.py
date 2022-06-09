c9b3281ac8666ce06ed7639f297c7730adafac8e
Shangdi Yu <yushangdi123@gmail.com>
2022-06-08 20:08:50 -0700

    [functorch] Implement a Common Subexpression Elimination (CSE) pass in AOTAutograd (pytorch/functorch#852)
    Implement a Common Subexpression Elimination (CSE) pass in AOTAutograd
    
    The test is in test/test_memory_efficient_fusion.py.

--- functorch/functorch/_src/partitioners.py
+++ functorch/functorch/_src/partitioners.py
@@ -7,6 +7,7 @@ import copy
 import os
 from torch.fx.passes import graph_drawer
 from typing import Tuple
+from .cse import fx_graph_cse
 
 
 class InvalidNodeBase(object):
@@ -226,6 +227,10 @@ def min_cut_rematerialization_partition(
     except ImportError:
         raise RuntimeError("Need networkx installed to perform smart recomputation heuristics")
 
+    #  add the CSE pass
+    fx_g = joint_module.graph
+    cse_graph = fx_graph_cse(fx_g)
+    joint_module.graph = cse_graph
     full_bw_graph = joint_module.graph
 
     name_to_node = {}
