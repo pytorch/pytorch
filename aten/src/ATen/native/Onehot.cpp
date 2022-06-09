17a01e9659a9b4a2696c0a6420f5aa4e07393603
Elias Ellison <elias.ellison@gmail.com>
2022-06-09 11:52:09 -0700

    Add  Dynamic Output Shape Tagdfor ata-dependent ops, handle in FakeTensor
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79170
    
    Approved by: https://github.com/ezyang

--- aten/src/ATen/native/Onehot.cpp
+++ aten/src/ATen/native/Onehot.cpp
@@ -16,6 +16,9 @@ Tensor one_hot(const Tensor &self, int64_t num_classes) {
             return at::empty(shape, self.options());
         }
     }
+    if (self.is_meta()) {
+        AT_ERROR("Can not infer total number of classes from meta tensor.");
+    }
 
     // non-empty tensor
     if (self.device().type() != at::kCUDA) {
