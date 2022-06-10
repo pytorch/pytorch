3f4661cc0916b91411c37f01a47a2611702af15e
pritam <9958665+pritamdamania87@users.noreply.github.com>
2022-06-09 22:59:09 -0700

    Update on "Fix `shard_module` to appropriately deal with sub process groups."
    `shard_module` API didn't work correctly with a sub-pg since
    `dist.scatter` actually takes the global rank as input for `src`.
    
    Fixing this by passing in the appropriate rank to `dist.scatter`
    
    Differential Revision: [D37062766](https://our.internmc.facebook.com/intern/diff/D37062766/)
    
    [ghstack-poisoned]

--- torch/distributed/_shard/sharding_spec/chunk_sharding_spec.py
+++ torch/distributed/_shard/sharding_spec/chunk_sharding_spec.py
@@ -168,10 +168,14 @@ class ChunkShardingSpec(ShardingSpec):
 
         # Scatter the shards to all ranks in the pg
         # scatter takes the global rank as ``src``
+        src_for_scatter = src_rank
+        if process_group is not None and process_group is not distributed_c10d._get_default_group():
+            src_for_scatter = distributed_c10d._get_global_rank(process_group, src_for_scatter)
+
         dist.scatter(
             local_tensor,
             scatter_list=tensors_to_scatter if current_rank == src_rank else None,
-            src=distributed_c10d._get_global_rank(process_group, src_rank),
+            src=src_for_scatter,
             group=process_group
         )
 
