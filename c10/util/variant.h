566aa44a06e3c2a6abab0fcd4f0bbd2e41cc7928
Nathan Lanza <lanza@fb.com>
2022-06-10 16:54:46 -0700

    Explicitly default a copy constructor (#79257)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79257
    
    The explicitly defaulted copy assignment operator here makes the
    automatic generation of a default copy constructor invalid. clang-13 now warns
    against this and thus it's an error for us under -Werror. So simply do here what
    clang would do otherwise.
    
    Test Plan: mostly sandcastle. But the behavior can be observed with `-fsyntax-only -ast-dump`
    
    Reviewed By: smeenai
    
    Differential Revision: D37050081
    
    fbshipit-source-id: 45a81beb74596ed4c0dcc1decb635827c76f7da7

--- c10/util/variant.h
+++ c10/util/variant.h
@@ -2248,6 +2248,7 @@ class impl : public copy_assignment<traits<Ts...>> {
  public:
   C10_MPARK_INHERITING_CTOR(impl, super)
   impl& operator=(const impl& other) = default;
+  impl(const impl& other) = default;
 
   template <std::size_t I, typename Arg>
   inline void assign(Arg&& arg) {
