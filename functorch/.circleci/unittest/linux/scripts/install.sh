ae24147211e1ebd7b34e4c29d390fef3aca38982
Samantha Andow <samdow@fb.com>
2022-06-10 12:27:52 -0400

    [functorch] Fix cuda tests, skip mac unexpected successes (pytorch/functorch#864)
    * a terrible hack that makes cuda work again
    
    * add mac test skips
    
    * reinstall with cuda pytorch--idk why this is needed
    
    * add higher tolerance for conv_transpose3d in vmap exhaustive
    
    * try without pip
    
    * use only conda, xfail vjpvmap clamp

--- functorch/.circleci/unittest/linux/scripts/install.sh
+++ functorch/.circleci/unittest/linux/scripts/install.sh
@@ -53,11 +53,9 @@ gcc --version
 pip install expecttest
 
 if [ "${CU_VERSION:-}" == cpu ] ; then
-    # conda install -y pytorch torchvision cpuonly -c pytorch-nightly
-    # use pip to install pytorch as conda can frequently pick older release
-    pip install torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html --pre
+    conda install -y pytorch torchvision cpuonly -c pytorch-nightly
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 else
-    pip install torch torchvision -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html --pre
+    conda install -y pytorch cudatoolkit=10.2 -c pytorch-nightly
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 fi
