b65aacaf25e6c109fd3628a3ac6e87f53a0490b2
Justin Chu <justinchu@microsoft.com>
2022-06-10 18:05:28 +0000

    Fix verification

--- torch/onnx/verification.py
+++ torch/onnx/verification.py
@@ -3,6 +3,8 @@
 ONNX Runtime is required, and is used as the ONNX backend for export verification.
 """
 
+from __future__ import annotations
+
 import contextlib
 import copy
 import difflib
@@ -506,7 +508,8 @@ def verify(
     ] = None,
     input_names: Optional[Sequence[str]] = None,
     output_names: Optional[Sequence[str]] = None,
-    training: Optional[bool] = None,
+    # TODO(justinchuby): Default training = torch.onnx.TrainingMode.EVAL after #78446 is checked in
+    training: Optional[torch.onnx.TrainingMode] = None,
     opset_version: Optional[int] = None,
     keep_initializers_as_inputs: bool = True,
     verbose: bool = False,
@@ -530,7 +533,7 @@ def verify(
         dynamic_axes (dict, optional): See :func:`torch.onnx.export`.
         input_names (list, optional): See :func:`torch.onnx.export`.
         output_names (list, optional): See :func:`torch.onnx.export`.
-        training (bool, optional): See :func:`torch.onnx.export`.
+        training (torch.onnx.TrainingMode, optional): See :func:`torch.onnx.export`.
         opset_version (int, optional): See :func:`torch.onnx.export`.
         keep_initializers_as_inputs (bool, optional): See :func:`torch.onnx.export`.
         verbose (bool, optional): See :func:`torch.onnx.export`.
@@ -560,6 +563,7 @@ def verify(
         model.train()
     elif training is None or training == torch.onnx.TrainingMode.EVAL:
         model.eval()
+        training = torch.onnx.TrainingMode.EVAL
     with torch.no_grad(), contextlib.ExitStack() as stack:
         model_f: Union[str, io.BytesIO] = io.BytesIO()
         if use_external_data:
@@ -568,7 +572,7 @@ def verify(
 
         inputs_for_export = _prepare_input_for_export(input_args, input_kwargs)
 
-        # TODO: remove this and treat mutating model separately. See #77679
+        # TODO(#77679): remove this and treat mutating model separately.
         model_copy = _try_clone_model(model)
         torch.onnx._export(
             model,
