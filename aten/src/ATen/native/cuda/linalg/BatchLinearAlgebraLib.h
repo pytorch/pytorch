1b41a6085169123fec5a40e4d7a11b61e0a2f5a1
lezcano <lezcano-93@hotmail.com>
2022-06-10 15:22:05 -0700

    Port linalg_qr to structured (#79054)
    Summary:
    This PR simplifies the logic of `linalg.qr` using structured kernels. I
    also took this chance and merged a few `copy_` operations with other
    ops.
    
    This PR removes a the previous magma implementation as is never faster
    than that of cusolver and it's rather buggy. This has the side-effect
    that now `qr` is not supported in Rocm. Ivan confirmed that this is
    fine, given how incredibly slow was QR on Rocm anyway (we were marking
    some tests as slow because of this...).
    
    This PR also corrects the dispatch in geqrf. Before, if we called it
    with a matrix for which `input.size(-2) <= 256 && batchCount(input) >= std::max<int64_t>(2, input.size(-2) / 16)` is false, and we have cublas but not cusolver, we would end up calling magma rather than cublas. This is not what the heuristic suggested.
    Probaly we should benchmark these heuristics again, but that's beyond the scope of this PR.
    
    Note. It looks like `torch.geqrf` maybe broken in MAGMA as per the
    previous comment in `linalg_qr_helper_magma`. IvanYashchuk wdyt?
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79054
    
    Approved by: https://github.com/IvanYashchuk, https://github.com/ezyang
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/af6321f3d81d935744c67620551550447e6729b2
    
    Reviewed By: osalpekar
    
    Differential Revision: D37059097
    
    Pulled By: osalpekar
    
    fbshipit-source-id: 7337def3d8e20023c1c737d3cad9d336d4279768

--- aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.h
+++ aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.h
@@ -88,7 +88,6 @@ namespace cuda { namespace detail {
 // Please do not add any new entires to it
 struct LinalgDispatch {
    std::tuple<Tensor, Tensor> (*symeig_helper)(const Tensor& self, bool eigenvectors, bool upper);
-   std::tuple<Tensor, Tensor> (*qr_helper)(const Tensor& input, c10::string_view mode);
    Tensor (*cholesky_solve_helper)(const Tensor& self, const Tensor& A, bool upper);
    std::tuple<Tensor, Tensor> (*legacy_lstsq)(const Tensor &B, const Tensor &A);
    Tensor& (*inv_out_helper)(Tensor &result, Tensor& infos_lu, Tensor& infos_getri);
