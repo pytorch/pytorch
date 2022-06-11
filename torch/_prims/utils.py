Fixes maybe_broadcast to actually broadcast only when needed (#79298)
Adds a `same_shape` util and updates maybe_broadcast to use it; previously maybe_broadcast was always broadcasting because its equality check was always failing.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/79298
Approved by: https://github.com/ezyang

torch/_prims/utils.py
@@ -139,6 +139,17 @@ def TensorMeta(
         )
 
 
+def same_shape(a: ShapeType, b: ShapeType) -> bool:
+    if len(a) != len(b):
+        return False
+
+    for x, y in zip(a, b):
+        if x != y:
+            return False
+
+    return True
+
+
 # TODO: look at using torch.testing.assert_close instead with an option
 #   to just compare metadata
 def compare_tensor_meta(a: TensorLikeType, b: TensorLikeType):
@@ -152,10 +163,9 @@ def compare_tensor_meta(a: TensorLikeType, b: TensorLikeType):
     assert isinstance(a, TensorLike)
     assert isinstance(b, TensorLike)
 
-    for x, y in zip(a.shape, b.shape):
-        if x != y:
-            msg = "Shapes {0} and {1} are not equal!".format(a.shape, b.shape)
-            raise AssertionError(msg)
+    if not same_shape(a.shape, b.shape):
+        msg = "Shapes {0} and {1} are not equal!".format(a.shape, b.shape)
+        raise AssertionError(msg)
 
     if a.dtype != b.dtype:
         msg = "Dtypes {0} and {1} are not equal!".format(a.dtype, b.dtype)