c23bb206d5b36fceba689f5c4e76269fcb02c9e5
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared52358.06.ldc1.facebook.com>
2022-06-12 04:01:50 -0700

    2022-06-12 postnightly release (f23685f008a5d4f03ec1c999e4623b26d8aa7812)

--- torch/csrc/api/src/jit.cpp
+++ torch/csrc/api/src/jit.cpp
@@ -1,7 +1,7 @@
 #include <torch/jit.h>
 
-#include <torch/csrc/jit/frontend/ir_emitter.h>
 #include <ATen/core/stack.h>
+#include <torch/csrc/jit/frontend/ir_emitter.h>
 
 #include <memory>
 #include <string>
@@ -11,11 +11,7 @@ namespace jit {
 
 std::shared_ptr<CompilationUnit> compile(const std::string& source) {
   auto module = std::make_shared<CompilationUnit>();
-  module->define(
-      c10::nullopt,
-      source,
-      nativeResolver(),
-      nullptr);
+  module->define(c10::nullopt, source, nativeResolver(), nullptr);
   return module;
 }
 
