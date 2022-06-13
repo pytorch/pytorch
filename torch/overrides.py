50810d1ada6ad7a8dd1edda8910d503d7c5f2346
Nikolay Korovaiko <korovaikon@gmail.com>
2022-06-13 14:05:30 -0700

    make overrides test pass

--- torch/overrides.py
+++ torch/overrides.py
@@ -280,6 +280,7 @@ def get_ignored_functions() -> Set[Callable]:
         Tensor._addmm_activation,
         Tensor._nested_tensor_layer_norm,
         Tensor.to_padded_tensor,
+        Tensor.sym_size
     }
 
 
