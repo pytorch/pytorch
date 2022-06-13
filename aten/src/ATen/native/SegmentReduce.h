c8af6d9656a1d60a30200f2f22759fde8420e99f
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-06-13 22:37:25 +0000

    Revert "Add offsets-based reduction to segment_reduce (CPU, CUDA)"
    This reverts commit 1ec30a6647be35d123a741a39cab8b4253c1cbe0.
    
    Reverted https://github.com/pytorch/pytorch/pull/78907 on behalf of https://github.com/osalpekar due to Caused Typecasting errors in PT Distributed and fx2trt builds internally

--- aten/src/ATen/native/SegmentReduce.h
+++ aten/src/ATen/native/SegmentReduce.h
@@ -11,23 +11,15 @@ namespace native {
 
 enum SegmentReductionType { MAX, MEAN, MIN, SUM, PROD};
 
-using segment_reduce_lengths_fn = Tensor (*)(
+using segment_reduce_fn = Tensor (*)(
     SegmentReductionType,
     const Tensor&,
     const Tensor&,
     int64_t,
     const c10::optional<Scalar>&);
-DECLARE_DISPATCH(segment_reduce_lengths_fn, _segment_reduce_lengths_stub);
+DECLARE_DISPATCH(segment_reduce_fn, _segment_reduce_stub);
 
-using segment_reduce_offsets_fn = Tensor (*)(
-    SegmentReductionType,
-    const Tensor&,
-    const Tensor&,
-    int64_t,
-    const c10::optional<Scalar>&);
-DECLARE_DISPATCH(segment_reduce_offsets_fn, _segment_reduce_offsets_stub);
-
-using segment_reduce_lengths_backward_fn = Tensor (*)(
+using segment_reduce_backward_fn = Tensor (*)(
     const Tensor&,
     const Tensor&,
     const Tensor&,
@@ -35,17 +27,7 @@ using segment_reduce_lengths_backward_fn = Tensor (*)(
     const Tensor&,
     int64_t,
     const c10::optional<Scalar>&);
-DECLARE_DISPATCH(segment_reduce_lengths_backward_fn, _segment_reduce_lengths_backward_stub);
-
-using segment_reduce_offsets_backward_fn = Tensor (*)(
-    const Tensor&,
-    const Tensor&,
-    const Tensor&,
-    SegmentReductionType,
-    const Tensor&,
-    int64_t,
-    const c10::optional<Scalar>&);
-DECLARE_DISPATCH(segment_reduce_offsets_backward_fn, _segment_reduce_offsets_backward_stub);
+DECLARE_DISPATCH(segment_reduce_backward_fn, _segment_reduce_backward_stub);
 
 } // namespace native
 } // namespace at
