39b06b9ae9ac0568e88fb598689e14abdc50c87a
asl3 <amandaliu@fb.com>
2022-06-13 20:22:41 -0700

    [quant] Implement quantize APoT method
    [ghstack-poisoned]

--- test/quantization/core/experimental/test_quantized_tensor.py
+++ test/quantization/core/experimental/test_quantized_tensor.py
@@ -6,9 +6,14 @@ import unittest
 
 class TestQuantizedTensor(unittest.TestCase):
     def test_quantize_APoT(self):
-        t = torch.Tensor()
-        with self.assertRaises(NotImplementedError):
-            TensorAPoT.quantize_APoT(t)
+        t = TensorAPoT()
+
+        tensor2quantize = torch.tensor([-1.0, 0.0, 1.0, 2.0])
+
+        t.quantize_APoT(tensor2quantize, 4, 2)
+
+        # with self.assertRaises(NotImplementedError):
+        #     TensorAPoT.quantize_APoT(t)
 
     def test_dequantize(self):
         with self.assertRaises(NotImplementedError):
