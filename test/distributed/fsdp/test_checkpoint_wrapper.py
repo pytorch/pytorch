ad2e8ff554f3d7a87d31fdeef4d342c3741e50a2
Rohan Varma <rvarm1@fb.com>
2022-06-14 20:37:41 +0000

    Lint

--- test/distributed/fsdp/test_checkpoint_wrapper.py
+++ test/distributed/fsdp/test_checkpoint_wrapper.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     checkpoint_wrapper,
-    CheckpointWrapper,
     CheckpointImpl
 )
 
