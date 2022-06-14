507338849273f044ade588b35d3e77231722a1a5
Joel Benjamin Schlosser <jbschlosser@fb.com>
2022-06-14 17:22:18 -0400

    Add Dropout1d module
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79545
    
    Approved by: https://github.com/ngimel, https://github.com/albanD

--- test/test_nn.py
+++ test/test_nn.py
@@ -7334,6 +7334,8 @@ class TestNN(NNTestCase):
         v = torch.ones(1)
         self.assertRaises(ValueError, lambda: nn.Dropout(-0.1))
         self.assertRaises(ValueError, lambda: nn.Dropout(1.1))
+        self.assertRaises(ValueError, lambda: nn.Dropout1d(-0.1))
+        self.assertRaises(ValueError, lambda: nn.Dropout1d(1.1))
         self.assertRaises(ValueError, lambda: nn.Dropout2d(-0.1))
         self.assertRaises(ValueError, lambda: nn.Dropout2d(1.1))
         self.assertRaises(ValueError, lambda: nn.Dropout3d(-0.1))
@@ -14371,6 +14373,28 @@ class TestNNDeviceType(NNTestCase):
 
         for b, c in product(range(B), range(C)):
             self.assertTrue(result[b, c].count_nonzero() in (0, channel_numel))
+
+    @expectedFailureXLA  # seems like freeze_rng_state is not honoured by XLA
+    def test_Dropout1d(self, device):
+        N, C, L = random.randint(10, 15), random.randint(10, 15), random.randint(10, 15)
+        input = torch.empty(N, C, L)
+        self._test_dropout(nn.Dropout1d, device, input)
+
+        with self.assertRaisesRegex(RuntimeError, "Expected 2D or 3D input, but received a 4D input"):
+            nn.Dropout1d(p=0.5)(torch.rand(1, 2, 2, 2, device=device))
+
+        with self.assertRaisesRegex(RuntimeError, "Expected 2D or 3D input, but received a 1D input"):
+            nn.Dropout1d(p=0.5)(torch.rand(2, device=device))
+
+        # no batch dims
+        input = torch.rand(50, 2, device=device)
+        self._test_dropoutNd_no_batch(nn.Dropout1d(p=0.5), input)
+        self._test_dropoutNd_no_batch(nn.Dropout1d(p=0.5, inplace=True), input)
+
+        # check that complete channels are dropped
+        input = torch.ones(10, 4, 2, device=device)
+        self._test_dropoutNd_channel_zero(nn.Dropout1d(p=0.5), input)
+        self._test_dropoutNd_channel_zero(nn.Dropout1d(p=0.5, inplace=True), input)
 
     @expectedFailureXLA  # seems like freeze_rng_state is not honoured by XLA
     def test_Dropout2d(self, device):
