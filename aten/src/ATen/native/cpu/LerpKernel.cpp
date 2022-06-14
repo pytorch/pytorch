9dce7686c3be8d9933f9d3bb0b0b019f2220fe2b
Kshiteej K <kshitijkalambarkar@gmail.com>
2022-06-14 04:44:17 +0000

    [primTorch] refs: lerp (#78473)
    As per title
    
    TODO:
    * [x] Add error inputs
    * [x] Add reference inputs
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78473
    Approved by: https://github.com/ngimel, https://github.com/mruberry

--- aten/src/ATen/native/cpu/LerpKernel.cpp
+++ aten/src/ATen/native/cpu/LerpKernel.cpp
@@ -10,12 +10,11 @@ namespace {
 
 void lerp_scalar_kernel(at::TensorIteratorBase& iter, const Scalar& weight) {
   AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(iter.common_dtype(), "lerp_kernel_scalar", [&] {
-    using value_t = typename c10::scalar_value_type<scalar_t>::type;
     scalar_t weight_val = weight.to<scalar_t>();
     at::native::cpu_kernel(
         iter,
         [weight_val](scalar_t self_val, scalar_t end_val) {
-          return (zabs<scalar_t, value_t>(weight_val) < 0.5)
+          return (std::abs(weight_val) < 0.5)
               ? self_val + weight_val * (end_val - self_val)
               : end_val - (end_val - self_val) * (scalar_t(1) - weight_val);
         });
@@ -24,11 +23,10 @@ void lerp_scalar_kernel(at::TensorIteratorBase& iter, const Scalar& weight) {
 
 void lerp_tensor_kernel(at::TensorIteratorBase& iter) {
   AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(iter.common_dtype(), "lerp_kernel_tensor", [&] {
-    using value_t = typename c10::scalar_value_type<scalar_t>::type;
     at::native::cpu_kernel(
         iter,
         [](scalar_t self_val, scalar_t end_val, scalar_t weight_val) {
-          return (zabs<scalar_t, value_t>(weight_val) < 0.5)
+          return (std::abs(weight_val) < 0.5)
               ? self_val + weight_val * (end_val - self_val)
               : end_val - (end_val - self_val) * (scalar_t(1) - weight_val);
         });
