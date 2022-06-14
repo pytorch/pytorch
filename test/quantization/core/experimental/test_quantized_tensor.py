a6fce77fbe970ed46316b6cfc780440124f43a01
asl3 <amandaliu@fb.com>
2022-06-14 08:11:34 -0700

    Update on "[quant] Implement quantize APoT method"
    [ghstack-poisoned]

--- test/quantization/core/experimental/test_quantized_tensor.py
+++ test/quantization/core/experimental/test_quantized_tensor.py
@@ -2,18 +2,39 @@
 
 import torch
 from torch.ao.quantization.experimental.APoT_tensor import TensorAPoT
+from torch.ao.quantization.experimental.observer import APoTObserver
 import unittest
 
 class TestQuantizedTensor(unittest.TestCase):
     def test_quantize_APoT(self):
         t = TensorAPoT()
 
-        tensor2quantize = torch.tensor([-1.0, 0.0, 1.0, 2.0])
+        tensor2quantize = torch.tensor([0, 1])
 
-        t.quantize_APoT(tensor2quantize, 4, 2)
+        result = t.quantize_APoT(tensor2quantize, 4, 2)
 
-        # with self.assertRaises(NotImplementedError):
-        #     TensorAPoT.quantize_APoT(t)
+        max_val = torch.max(tensor2quantize)
+
+        # make observer
+        obs = APoTObserver(max_val=max_val, b=4, k=2)
+        obs_result = obs.calculate_qparams(signed=False)
+
+        quantized_levels = obs_result[1]
+        level_indices = obs_result[2]
+
+        tensor2quantize_arr = list(tensor2quantize)
+        result_arr = result.numpy()
+
+        zipped_result = zip(tensor2quantize_arr, result_arr)
+
+        expected_result = True
+
+        for ele, res in zipped_result:
+            idx = list(level_indices).index(ele)
+            if res != quantized_levels[idx]:
+                expected_result = False
+
+        self.assertTrue(expected_result)
 
     def test_dequantize(self):
         with self.assertRaises(NotImplementedError):
--- torch/ao/quantization/experimental/APoT_tensor.py
+++ torch/ao/quantization/experimental/APoT_tensor.py
@@ -1,7 +1,7 @@
 import torch
 import tensorflow as tf
 from torch import Tensor
-from torch.ao.quantization.experimental.observer import APoTObserver
+from torch.ao.quantization.experimental.observer import APoTObserver, apot_to_float
 
 # class to store APoT quantized tensor
 class TensorAPoT(torch.Tensor):
@@ -16,19 +16,10 @@ class TensorAPoT(torch.Tensor):
         quantized_levels = obs_result[1]
         level_indices = obs_result[2]
 
-        apot_result = torch.Tensor([])
+        # map apot_to_float over tensor2quantize elements
+        result = tf.map_fn(fn=lambda t: apot_to_float(t, quantized_levels, level_indices), elems=tensor2quantize, dtype=tf.float32)
 
-        num_rows, num_columns = tensor2quantize.get_shape()
-
-        # traverse tensor2quantize, quantize each element
-        for x in tensor2quantize:
-            x_apot = obs.float_to_apot(x, quantized_levels, level_indices)
-            apot_result = torch.cat(apot_result, x_apot)
-
-        torch.reshape(apot_result, (num_rows, num_columns))
-
-        print(apot_result)
-
+        return result
 
     def dequantize(self) -> Tensor:
         raise NotImplementedError
