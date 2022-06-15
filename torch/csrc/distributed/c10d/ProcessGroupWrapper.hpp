2cb20549317d713e68230fa4b15048215aae099d
Rodrigo Kumpera <kumpera@fb.com>
2022-06-15 18:43:59 +0000

    Add _reduce_scatter_base to ProcessGroupWrapper.

--- torch/csrc/distributed/c10d/ProcessGroupWrapper.hpp
+++ torch/csrc/distributed/c10d/ProcessGroupWrapper.hpp
@@ -111,6 +111,11 @@ class TORCH_API ProcessGroupWrapper : public ProcessGroup {
   c10::intrusive_ptr<ProcessGroup::Work> barrier(
       const BarrierOptions& opts = BarrierOptions()) override;
 
+    c10::intrusive_ptr<ProcessGroup::Work> _reduce_scatter_base(
+      at::Tensor& outputBuffer,
+      at::Tensor& inputBuffe,
+      const ReduceScatterOptions& opts) override;
+
   c10::intrusive_ptr<ProcessGroup> getWrappedPg() const;
 
  private:
