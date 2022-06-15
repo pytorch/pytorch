f1990e033820f5d8021b229a8166808813e43326
macandro96 <anishm@fb.com>
2022-06-15 21:30:00 +0000

    [ao][sparsity] Support for sparsifying data operations on raw torch tensors.
    The users can now pass in raw torch tensors and the base class handles all the parametrizations and masking
    
    Example -
        >>> data_list = [('tensor_1', torch.randn(3,3)), ('tensor_2', torch.randn(4,4))]
        >>> defaults = {'sparsity_level': 0.7}
        >>> sparsifier = DerivedDataSparsifier(data_list = data_list, **defaults) # Some sparsifier that inherits BaseDataSparsifier
        >>> new_tensor_to_add = {'name': 'tensor_3', 'data': torch.randn(5,5), 'sparsity_level': 0.3}
        >>> sparsifier.add_data(**new_tensor_to_add)
        >>> # tensor_1 and tensor_2 will have sparsity_level of 0.7 but tensor_3 will have sparsity_level=0.3
    
    Test Plan:
    ```python test/test_ao_sparsity.py TestBaseDataSparsifier```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79252
    
    Approved by: https://github.com/HDCharles, https://github.com/z-a-f

--- test/ao/sparsity/test_data_sparsifier.py
+++ test/ao/sparsity/test_data_sparsifier.py
@@ -2,12 +2,159 @@
 # Owner(s): ["module: unknown"]
 
 import logging
-
+import random
+import torch
+from torch.nn.utils.parametrize import is_parametrized
 from torch.testing._internal.common_utils import TestCase
+from torch.ao.sparsity import BaseDataSparsifier
+from typing import Tuple
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-class TestBaseDataSparsifier(TestCase):
+class ImplementedSparsifier(BaseDataSparsifier):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update_mask(self, name, data, **kwargs):
+        mask = self.get_mask(name)
+        mask[0] = 0
+        linear_state = self.state[name]
+        linear_state['step_count'] = linear_state.get('step_count', 0) + 1
+
+
+class TestBaseDataSparsiferRunner(TestCase):
+    r"""This helper test class takes in any supported type of and runs some tests.
+        The user is required to pass in the data that needs to sparsified and the
+        runner will run some tests that needs to be passed in order for the data
+        type to be supported.
+        TODO: Change the structure by creating a separate test case class for each
+              member function
+    """
+    def __init__(self, data_list, defaults, data_with_config):
+        self.data_list = data_list
+        self.defaults = defaults
+        self.data_with_config = data_with_config
+
+    def get_name_data_config(self, some_data):
+        if isinstance(some_data, Tuple):
+            # dealing with data_list
+            name, data = some_data
+            config = self.defaults
+        else:
+            # dealing with data_with_config
+            name, data, config = some_data['name'], some_data['data'], some_data['config']
+        return name, data, config
+
+    def get_sparsifier(self):
+        sparsifier = ImplementedSparsifier(data_list=self.data_list, **self.defaults)
+        assert len(sparsifier.data_groups) == len(self.data_list)
+        for data_config_dict in self.data_with_config:
+            name, data, config = data_config_dict['name'], data_config_dict['data'], data_config_dict['config']
+            sparsifier.add_data(name=name, data=data, **config)
+        return sparsifier
+
     def test_constructor(self):
-        pass  # Nothing to test so far
+        sparsifier = self.get_sparsifier()
+        assert len(sparsifier.data_groups) == len(self.data_list) + len(self.data_with_config)
+
+        all_data = self.data_list + self.data_with_config
+
+        for some_data in all_data:
+            name, _, config = self.get_name_data_config(some_data)
+            assert name in sparsifier.data_groups
+            assert sparsifier.data_groups[name] == config
+
+    def test_step(self):
+        sparsifier = self.get_sparsifier()
+        all_data = self.data_list + self.data_with_config
+
+        # Check data and mask before doing the step
+        for some_data in all_data:
+            name, data, _ = self.get_name_data_config(some_data)
+            data = sparsifier._extract_weight(data)
+            sparsified_data = sparsifier.get_data(name=name, return_original=False)
+            original_data = sparsifier.get_data(name=name, return_original=True)
+            mask = sparsifier.get_mask(name=name)
+            assert torch.all(sparsified_data == data)
+            assert torch.all(original_data == data)
+            assert torch.all(mask[0] == 1)
+
+        step_count = 3
+
+        for _ in range(0, step_count):
+            sparsifier.step()
+        for some_data in all_data:
+            name, data, _ = self.get_name_data_config(some_data)
+            data = sparsifier._extract_weight(data)
+            sparsified_data = sparsifier.get_data(name=name, return_original=False)
+            original_data = sparsifier.get_data(name=name, return_original=True)
+            mask = sparsifier.get_mask(name=name)
+            assert torch.all(sparsified_data[0] == 0)
+            assert torch.all(original_data == data)
+            assert torch.all(mask[0] == 0)
+            assert 'step_count' in sparsifier.state[name]
+            assert sparsifier.state[name]['step_count'] == 3
+
+    def test_squash_mask(self):
+        sparsifier = self.get_sparsifier()
+        all_data = self.data_list + self.data_with_config
+        for some_data in all_data:
+            name, _, _ = self.get_name_data_config(some_data)
+            assert hasattr(sparsifier._container, name)
+            assert is_parametrized(sparsifier._container, name)
+        sparsifier.step()
+        sparsifier.squash_mask()
+
+        for some_data in all_data:
+            name, _, _ = self.get_name_data_config(some_data)
+            assert not is_parametrized(sparsifier._container, name)  # not parametrized anymore
+            with self.assertRaises(ValueError):
+                sparsifier.get_data(name, return_original=True)
+
+    def test_add_data(self):
+        sparsifier = self.get_sparsifier()
+        all_data = self.data_list + self.data_with_config
+        for some_data in all_data:
+            name1, data1, _ = self.get_name_data_config(some_data)
+            data1 = sparsifier._extract_weight(data1)
+            assert torch.all(data1 == sparsifier.get_data(name=name1))
+            # get some other data at random and with the same name
+            rand_idx = random.randint(0, len(all_data) - 1)
+            _, data2, _ = self.get_name_data_config(all_data[rand_idx])
+            data2 = sparsifier._extract_weight(data2)
+            sparsifier.add_data(name=name1, data=data2)
+            assert torch.all(data2 == sparsifier.get_data(name=name1))
+
+    def run_tests(self):
+        self.test_constructor()
+        self.test_squash_mask()
+        self.test_add_data()
+        self.test_step()
+
+
+class TestBaseDataSparsifier(TestCase):
+    """To add unit tests to support new data types for the BaseDataSparsifier, create the following
+        data_list: List of tuples of name, data to be added to the constructor
+        defaults: default config for the above data in data_list
+        data_with_config: list of dictionaries defining name, data and config (look test_tensors())
+
+    Once the above is done, create an instance of TestBaseDataSparsifierType and call all it's functions
+    """
+    def test_tensors(self):
+        tensor1, tensor2, tensor3 = torch.randn(3, 3), torch.randn(4, 4), torch.randn(5, 5)
+        tensor4, tensor5 = torch.randn(1, 1), torch.randn(4, 4)
+        data_list = [('tensor1', tensor1), ('tensor2', tensor2), ('tensor3', tensor3)]
+        defaults = {'test': 3}
+
+        data_with_config = [
+            {
+                'name': 'tensor4', 'data': tensor4, 'config': {'test': 7}
+            },
+            {
+                'name': 'tensor5', 'data': tensor5, 'config': {'test': 8}
+            },
+        ]
+        tensor_test = TestBaseDataSparsiferRunner(data_list=data_list, defaults=defaults,
+                                                  data_with_config=data_with_config)
+        tensor_test.run_tests()
--- torch/ao/sparsity/experimental/data_sparsifier/base_data_sparsifier.py
+++ torch/ao/sparsity/experimental/data_sparsifier/base_data_sparsifier.py
@@ -1,9 +1,19 @@
 import abc
+import torch
 from typing import Optional, Tuple, List, Any, Dict
 from ...sparsifier import base_sparsifier
 from collections import defaultdict
 from torch import nn
+import warnings
+import copy
+from ...sparsifier import utils
+from torch.nn.utils import parametrize
+
 __all__ = ['BaseDataSparsifier']
+
+SUPPORTED_TYPES = {
+    torch.Tensor
+}
 
 
 class _Container(nn.Module):
@@ -28,6 +38,14 @@ class BaseDataSparsifier(base_sparsifier.BaseSparsifier):
             default configurations will be attached to the
             configuration. Only the keys that don't exist in the `config` will
             be updated.
+    Example::
+
+        >>> data_list = [('tensor_1', torch.randn(3,3)), ('tensor_2', torch.randn(4,4))]
+        >>> defaults = {'sparsity_level': 0.7}
+        >>> sparsifier = DerivedDataSparsifier(data_list = data_list, **defaults) # Some sparsifier that inherits BaseDataSparsifier
+        >>> new_tensor_to_add = {'name': 'tensor_3', 'data': torch.randn(5,5), 'sparsity_level': 0.3}
+        >>> sparsifier.add_data(**new_tensor_to_add)
+        >>> # tensor_1 and tensor_2 will have sparsity_level of 0.7 but tensor_3 will have sparsity_level=0.3
     """
     def __init__(self, data_list: Optional[List[Tuple[str, Any]]] = None, **defaults):
         super().__init__(defaults=defaults)
@@ -42,35 +60,98 @@ class BaseDataSparsifier(base_sparsifier.BaseSparsifier):
     def prepare(self):
         raise NotImplementedError("this function is undefined for this class")
 
+    def _extract_weight(self, data):
+        if isinstance(data, torch.Tensor):
+            return data
+
     def add_data(self, name: str, data, **config):
         r""" Configures and parametrizes the internal container model with name and data
         """
-        pass
+        assert type(data) in SUPPORTED_TYPES, \
+            "specified data type not supported at the moment"
+        local_args = copy.deepcopy(self.defaults)
+        local_args.update(config)
+        self.data_groups[name] = local_args
 
-    def get_data(self, name: str):
-        r"""Returns weight tensor (or data) based on the input name.
+        weight = self._extract_weight(data)
+
+        # Bookkeeping in the container class
+        mask = local_args.get('mask', torch.ones_like(weight))
+        param_class = local_args.get('parametrization', utils.FakeSparsity)  # change once public_api for utils is fixed!
+        param = nn.Parameter(weight, requires_grad=False)
+
+        if name in self.state:
+            # If the named data already exists - replace
+            warnings.warn("Replacing existing data of the same name. - Did you mean a different name?")
+            # check if parametrized
+            if parametrize.is_parametrized(self._container, name):
+                # If parametrized, squash mask
+                self.squash_mask(names=[name], leave_parametrized=False)
+            self._container.get_parameter(name).data = weight  # overwrite the data
+        else:
+            setattr(self._container, name, param)
+        parametrize.register_parametrization(self._container, name, param_class(mask))
+        self.state[name]['mask'] = mask
+        return getattr(self._container, name)
+
+    def get_data(self, name: str, return_original: bool = True):
+        r"""Returns weight tensor (or data)
+        Args:
+            - name: name of the data to be returned
+            - return_original returns weight tensor without applying parametrization if True
+                else - returns the sparsified version (parametrized)
         """
-        pass
+        if name not in self.data_groups:
+            raise ValueError("data with specified name does not exist")
+
+        if return_original:
+            if not parametrize.is_parametrized(self._container, name):
+                raise ValueError("mask squashed - original mask value does not exist")
+            data = getattr(self._container.parametrizations, name).original
+            return data
+        else:
+            return getattr(self._container, name)
 
     def __repr__(self):
-        r"""String representation of an object when printed
-        """
-        pass
+        format_string = self.__class__.__name__ + ' ('
+        for name, sparse_args in self.data_groups.items():
+            format_string += '\n'
+            format_string += '\tData Group\n'
+            format_string += f'\t    name: {name}\n'
+            for key in sorted(sparse_args.keys()):
+                if key == 'data':
+                    continue
+                format_string += f'\t    {key}: {sparse_args[key]}\n'
+        format_string += ')'
+        return format_string
 
     def get_mask(self, name: str):
-        r"""Returns the mask currently associated with the named tensor.
-        """
-        pass
+        if name not in self.state:
+            raise ValueError("data with specified name does not exist")
+        return self.state[name]['mask']
 
-    def squash_mask(self, *args, **kwargs):
-        r"""Squashes the sparse masks into the appropriate tensors.
+    def squash_mask(self, *args, leave_parametrized=True, names=None, **kwargs):
+        r"""Squashes the sparse masks into the appropriate tensors. Also, accepts list of strings
+        to squash mask for. If none, squashes mask for all the keys
+        kwargs:
+            * names: list of strings to squash mask for
+            * sparsified: if true - applies the mask before squashing
+                          if false - does not apply the mask before squashing
         """
-        pass
+        if names is None:
+            names = list(self.data_groups.keys())
+        for name in names:
+            parametrize.remove_parametrizations(self._container, name, leave_parametrized=leave_parametrized)
 
     def step(self):
-        r"""Updates the mask for all the named data.
-        """
-        pass
+        if not self.enable_mask_update:
+            return
+        with torch.no_grad():
+            for name, config in self.data_groups.items():
+                # get non-sparsified data
+                data = self.get_data(name)
+                # need name for the mask otherwise can directly pass mask?
+                self.update_mask(name, data, **config)
 
     @abc.abstractmethod
     def update_mask(self, name, data, **kwargs):
