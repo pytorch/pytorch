daa65155406d4327e34bd7543b2ed12e7e733fee
Michael Andreas Dagitses <mikeyd@fb.com>
2022-06-15 18:55:23 -0700

    Update base for Update on "canonicalize includes of form <aten/src/ATen/...>"
    This was never intended to be supported.
    
    Differential Revision: [D36567054](https://our.internmc.facebook.com/intern/diff/D36567054/)
    
    [ghstack-poisoned]

--- test/onnx/test_pytorch_helper.py
+++ test/onnx/test_pytorch_helper.py
@@ -5,7 +5,7 @@ import unittest
 
 import numpy as np
 from pytorch_helper import PyTorchModule
-from test_pytorch_common import skipIfNoLapack
+from test_pytorch_common import skipIfNoLapack, run_tests, TestCase
 
 import torch.nn.init as init
 import torch.onnx
@@ -14,7 +14,7 @@ from caffe2.python.model_helper import ModelHelper
 from torch import nn
 
 
-class TestCaffe2Backend(unittest.TestCase):
+class TestCaffe2Backend(TestCase):
     @skipIfNoLapack
     @unittest.skip("test broken because Lapack was always missing.")
     def test_helper(self):
@@ -67,4 +67,4 @@ class TestCaffe2Backend(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    run_tests()
