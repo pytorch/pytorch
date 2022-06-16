c1c13224ee0a958f260a91ca524ad1fbae6ef627
Michael Andreas Dagitses <mikeyd@fb.com>
2022-06-16 03:12:07 -0700

    canonicalize includes of form <aten/src/ATen/...>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78033
    
    This was never intended to be supported.
    
    @override-unit-failures
    (Note: this ignores all push blocking failures!)
    
    Differential Revision: [D36567054](https://our.internmc.facebook.com/intern/diff/D36567054/)
    
    Approved by: https://github.com/kit1980

--- torch/csrc/lazy/core/shape_inference.cpp
+++ torch/csrc/lazy/core/shape_inference.cpp
@@ -51,13 +51,13 @@
 
 #include <ATen/AccumulateType.h>
 #include <ATen/Dispatch.h>
+#include <ATen/ExpandUtils.h>
 #include <ATen/Functions.h>
 #include <ATen/InferSize.h>
 #include <ATen/WrapDimUtils.h>
 #include <ATen/native/ConvUtils.h>
+#include <ATen/native/ReduceOpsUtils.h>
 #include <ATen/native/TensorConversions.h>
-#include <aten/src/ATen/ExpandUtils.h>
-#include <aten/src/ATen/native/ReduceOpsUtils.h>
 #include <c10/core/ScalarType.h>
 #include <torch/csrc/api/include/torch/enum.h>
 #include <torch/csrc/lazy/core/ops/utils.h>
