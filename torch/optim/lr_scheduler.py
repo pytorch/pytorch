22723b1955208f1decafd271f09a70af50d124a5
Antonio Kim <antonio.kim@cerebras.net>
2022-06-17 12:21:49 -0700

    Move Changes to SequentialLR
    - This is primarily to prevent the changes from affecting
      ChainedScheduler

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -5,7 +5,6 @@ from functools import wraps
 import warnings
 import weakref
 from collections import Counter
-from contextlib import contextmanager
 from bisect import bisect_right
 
 from .optimizer import Optimizer
@@ -71,33 +70,15 @@ class _LRScheduler(object):
             return wrapper
 
         self.optimizer.step = with_counter(self.optimizer.step)
-        self.optimizer._step_count = 0
-        self._step_count = 0
         self.verbose = verbose
 
-        # Increment LR registration count
-        # Tracks the number of schedulers which this optimizer is registered to
-        self._scheduler_id = getattr(self.optimizer, "_lr_registration_count", 0)
-        self.optimizer._lr_registration_count = self._scheduler_id + 1
+        self._initial_step()
 
-        with self._init_optimizer_lr():
-            self.step()
-
-    @contextmanager
-    def _init_optimizer_lr(self):
-        # Save the current learning rate values for all schedulers except the first
-        if self._scheduler_id > 0:
-            for group in self.optimizer.param_groups:
-                group["prev_lr"] = group["lr"]
-
-        try:
-            yield
-        finally:
-            # Reset the learning rates to the previous value for all but the first scheduler
-            if self._scheduler_id > 0:
-                for group in self.optimizer.param_groups:
-                    group["lr"] = group.pop("prev_lr")
-
+    def _initial_step(self):
+        """Initialize step counts and performs a step"""
+        self.optimizer._step_count = 0
+        self._step_count = 0
+        self.step()
 
     def state_dict(self):
         """Returns the state of the scheduler as a :class:`dict`.
@@ -658,7 +639,16 @@ class SequentialLR(_LRScheduler):
         self._milestones = milestones
         self.last_epoch = last_epoch + 1
         self.optimizer = optimizer
+
+        # Reset learning rates back to initial values
+        for group in self.optimizer.param_groups:
+            group["lr"] = group["initial_lr"]
+
+        # Perform the initial step for only the first scheduler
+        self._schedulers[0]._initial_step()
+
         self._last_lr = schedulers[0].get_last_lr()
+
 
     def step(self):
         self.last_epoch += 1
