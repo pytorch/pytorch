8a7da5277be12bc638dee315d23eb2e0c2423ec5
Osama Habib <ohanna@ucla.edu>
2022-06-20 22:20:21 +0000

    separated faulty agent tests

--- torch/testing/_internal/distributed/rpc/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -18,7 +18,7 @@ import torch.distributed as dist
 import torch.distributed.rpc as rpc
 import torch.distributed.autograd as dist_autograd
 from torch.distributed.rpc import RRef, _get_debug_info, _rref_context_get_debug_info, WorkerInfo
-from torch.distributed.rpc.api import _delete_all_user_and_unforked_owner_rrefs, _use_rpc_pickler, _thread_local_var, _wait_all
+from torch.distributed.rpc.api import _use_rpc_pickler, _thread_local_var, _wait_all
 from torch.distributed.rpc.internal import (
     PythonUDF,
     RPCExecMode,
