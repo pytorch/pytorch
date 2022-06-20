35a4234955a3c15e700932b9b9d7af806405c0a8
vspenubarthi <vspenubarthi@gmail.com>
2022-06-20 12:52:49 -0700

    [ao] Adding model report detector base class and implemented detectors
    Summary: the goal is to add a base class to the model report detectors
    so that they can contain a lot more specific information compared to the
    primary model report class related to the observers and where they are
    inserted etc.
    
    Since this is just a base class, the testing will be with the
    implemenations of the classes that derive from the base class
    
    The two current detector methods were turned into Detector classes and
    the tests were modified to reflect this, but the same functionality was
    tested.
    
    As a result, _detector.py was changed to detector.py
    
    Test Plan: python test/test_quantization.py TestFxModelReportDetector
    python test/test_quantization.py TestFxModelReportDetectDynamicStatic
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79671
    
    Approved by: https://github.com/andrewor14

--- test/quantization/fx/test_model_report_fx.py
+++ test/quantization/fx/test_model_report_fx.py
@@ -5,7 +5,7 @@ import torch
 import torch.ao.quantization.quantize_fx as quantize_fx
 import torch.nn.functional as F
 from torch.ao.quantization import QConfig, QConfigMapping
-from torch.ao.quantization.fx._model_report._detector import _detect_dynamic_vs_static, _detect_per_channel
+from torch.ao.quantization.fx._model_report.detector import DynamicStaticDetector, PerChannelDetector
 from torch.ao.quantization.fx._model_report.model_report_observer import ModelReportObserver
 from torch.ao.quantization.observer import HistogramObserver, default_per_channel_weight_observer
 from torch.nn.intrinsic.modules.fused import ConvReLU2d, LinearReLU
@@ -97,7 +97,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         prepared_model = self._prepare_model_and_run_input(ConvModel(), q_config_mapping, input)
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # no optims possible and there should be nothing in per_channel_status
         self.assertEqual(
@@ -136,7 +137,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         )
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # there should be optims possible
         self.assertNotEqual(
@@ -202,7 +204,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         )
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # the only suggestions should be to linear layers
 
@@ -251,7 +254,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         )
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # there should be optims possible
         self.assertNotEqual(
@@ -292,7 +296,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         )
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # there should be optims possible
         self.assertNotEqual(
@@ -333,7 +338,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         )
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(prepared_model)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(prepared_model)
 
         # no optims possible and there should be nothing in per_channel_status
         self.assertEqual(
@@ -398,7 +404,8 @@ class TestFxModelReportDetector(QuantizationTestCase):
         model_fp32_prepared = torch.quantization.prepare_qat(model_fp32_fused)
 
         # run the detector
-        optims_str, per_channel_info = _detect_per_channel(model_fp32_prepared)
+        per_channel_detector = PerChannelDetector(torch.backends.quantized.engine)
+        optims_str, per_channel_info = per_channel_detector.generate_detector_report(model_fp32_prepared)
 
         # there should be optims possible
         self.assertNotEqual(
@@ -785,7 +792,8 @@ class TestFxModelReportDetectDynamicStatic(QuantizationTestCase):
             model_prep(example_input)
 
         # run it through the dynamic vs static detector
-        dynam_vs_stat_str, dynam_vs_stat_dict = _detect_dynamic_vs_static(model_prep, tolerance=0.5)
+        dynamic_vs_static_detector = DynamicStaticDetector()
+        dynam_vs_stat_str, dynam_vs_stat_dict = dynamic_vs_static_detector.generate_detector_report(model_prep)
 
         # one of the stats should be stationary, and the other non-stationary
         # as a result, dynamic should be recommended
