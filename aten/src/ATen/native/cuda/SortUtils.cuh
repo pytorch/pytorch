82bb702874c65e68eb89db97967c43e819ecbe14
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-06-21 21:46:10 +0000

    Improve small sort performance on CUDA
    Currently, `bitonicSortKVInPlace` is written to sort one array per
    block of threads. If that dimension happens to be very small
    (<128 elements), this results in low thread occupancy.
    
    Instead, this changes `bitonicSortKVInPlace` to operate with a 2d
    block. Sorting happens along the x dimension, and the y dimension
    is a fixed size batch.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79627
    
    Approved by: https://github.com/ngimel

--- aten/src/ATen/native/cuda/SortUtils.cuh
+++ aten/src/ATen/native/cuda/SortUtils.cuh
@@ -30,11 +30,11 @@ __device__ inline void bitonicSwap(K& kA, V& vA, bool& validA,
   }
 };
 
-template <typename Comparator, typename K, typename V,
-          typename IndexType, int Power2SortSize>
-__device__ inline void bitonicSort(K keys[Power2SortSize],
-                                   V values[Power2SortSize],
-                                   bool valid[Power2SortSize],
+template <int Power2SortSize, typename IndexType, typename Comparator,
+          typename K, typename V>
+__device__ inline void bitonicSort(K *keys,
+                                   V *values,
+                                   bool *valid,
                                    const Comparator& comp) {
 #if !defined(USE_ROCM)
 #pragma unroll
@@ -78,10 +78,9 @@ __device__ inline void bitonicSort(K keys[Power2SortSize],
 // at::cuda::detail::TensorInfo version
 // Sorts (key, value) pairs (in different tensors) in-place; i.e.,
 // modifies the input `keys` and `values`
-template <typename K, typename V,
-          int KeyDims, int ValueDims,
-          typename Comparator, typename IndexType, int Power2SortSize>
-C10_LAUNCH_BOUNDS_1(1024)
+template <int KeyDims, int ValueDims, int block_dim_x, int max_block_dim_y,
+          typename K, typename V, typename Comparator, typename IndexType>
+C10_LAUNCH_BOUNDS_1(block_dim_x * max_block_dim_y)
 __global__ void
 bitonicSortKVInPlace(at::cuda::detail::TensorInfo<K, IndexType> keys,
                      IndexType keySlices,
@@ -91,69 +90,63 @@ bitonicSortKVInPlace(at::cuda::detail::TensorInfo<K, IndexType> keys,
                      IndexType valueSliceStride,
                      Comparator comp) {
   // Find the slice of the tensor that we are sorting
-  const IndexType linearIndex = getLinearBlockId<IndexType>();
-  // Tiling the slices could have us be out of bounds, if there are a
-  // lot of slices to sort
-  if (linearIndex >= keySlices) {
+  // NOTE: blockDim.y may be less max_block_dim_y
+  const IndexType blockIndex = getLinearBlockId<IndexType>();
+  const IndexType linearIndex = blockIndex * blockDim.y + threadIdx.y;
+
+  // If the entire block is out of bounds exit early
+  if (blockIndex * blockDim.y >= keySlices) {
     return;
   }
+  // It's also possible for some rows of a block to be out of bounds
+  // but all thread need to run for __syncthreads to work.
+  const bool row_valid = linearIndex < keySlices;
 
-  __shared__ K sharedKeys[Power2SortSize];
-  __shared__ V sharedValues[Power2SortSize];
-  __shared__ bool sharedValid[Power2SortSize];
+  constexpr int items_per_thread = 2;
+  constexpr int Power2SortSize = block_dim_x * items_per_thread;
+
+  // Storage for max_block_dim_y sorts performed in parallel
+  __shared__ K blockSharedKeys[max_block_dim_y][Power2SortSize];
+  __shared__ V blockSharedValues[max_block_dim_y][Power2SortSize];
+  __shared__ bool blockSharedValid[max_block_dim_y][Power2SortSize];
+
+  auto sharedKeys = blockSharedKeys[threadIdx.y];
+  auto sharedValues = blockSharedValues[threadIdx.y];
+  auto sharedValid = blockSharedValid[threadIdx.y];
 
   const IndexType keyStartOffset =
     at::cuda::detail::IndexToOffset<K, IndexType, KeyDims>::get(linearIndex, keys);
   const IndexType valueStartOffset =
     at::cuda::detail::IndexToOffset<V, IndexType, ValueDims>::get(linearIndex, values);
 
-  // If the sort size is 1, the data is already sorted
-  if (Power2SortSize == 1) {
-    return;
-  } else {
-    // Otherwise, each thread is responsible for loading and storing 2
-    // elements. The sort size is guaranteed to be >= 2
-    const int elem1 = threadIdx.x;
-    const int elem2 = threadIdx.x + (Power2SortSize / 2);
+  // Load 2 values per thread into the shared workspace
+  #pragma unroll
+  for (int k = 0; k < items_per_thread; ++k) {
+    auto idx = threadIdx.x + k * blockDim.x;
+    bool valid = row_valid && idx < keySliceSize;
 
-    bool valid1 = (elem1 < keySliceSize);
-    K k1 = valid1 ?
-      keys.data[keyStartOffset + elem1 * keySliceStride] : static_cast<K>(0);
-    V v1 = valid1 ?
-      values.data[valueStartOffset + elem1 * valueSliceStride] : static_cast<V>(0);
+    sharedKeys[idx] = valid ?
+        keys.data[idx * keySliceStride + keyStartOffset] : K{};
+    sharedValues[idx] = valid ?
+        values.data[idx * valueSliceStride + valueStartOffset] : V{};
+    sharedValid[idx] = valid;
+  }
 
-    sharedKeys[elem1] = k1;
-    sharedValues[elem1] = v1;
-    sharedValid[elem1] = valid1;
-
-    bool valid2 = (elem2 < keySliceSize);
-    K k2 = valid2 ?
-      keys.data[keyStartOffset + elem2 * keySliceStride] : static_cast<K>(0);
-    V v2 = valid2 ?
-      values.data[valueStartOffset + elem2 * valueSliceStride] : static_cast<V>(0);
-
-    sharedKeys[elem2] = k2;
-    sharedValues[elem2] = v2;
-    sharedValid[elem2] = valid2;
-
-    // Sort!
-    bitonicSort<Comparator, K, V, IndexType, Power2SortSize>(
+  // Sort!
+  bitonicSort<Power2SortSize, IndexType>(
       sharedKeys, sharedValues, sharedValid, comp);
 
-    // elem1 and elem2 values might be out-of-range, if the data size we are
-    // sorting is smaller than half the power2 size
-    if (valid1) {
-      keys.data[keyStartOffset + elem1 * keySliceStride] =
-        sharedKeys[elem1];
-      values.data[valueStartOffset + elem1 * valueSliceStride] =
-        sharedValues[elem1];
-    }
+  if (!row_valid) {
+    return;
+  }
 
-    if (valid2) {
-      keys.data[keyStartOffset + elem2 * keySliceStride] =
-        sharedKeys[elem2];
-      values.data[valueStartOffset + elem2 * valueSliceStride] =
-        sharedValues[elem2];
+  // Store outputs
+  #pragma unroll
+  for (int k = 0; k < items_per_thread; ++k) {
+    auto idx = threadIdx.x + k * blockDim.x;
+    if (idx < keySliceSize) {
+      keys.data[idx * keySliceStride + keyStartOffset] = sharedKeys[idx];
+      values.data[idx * valueSliceStride + valueStartOffset] = sharedValues[idx];
     }
   }
 }
