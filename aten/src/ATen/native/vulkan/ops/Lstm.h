6fce671271b5bc16dfb60462804e05b1603dc3e7
Manuel Candales <mcandales@fb.com>
2022-06-21 13:59:32 -0700

    [Vulkan] Optimize LSTM operator with pre-packing (#79702)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79702
    
    Optimized LSTM operator by using pre-packing for weights and biases in the Vulkan GPU backend
    - The weights and biases are always on the CPU side by design.
    - The packed and unpacked data are stored in a VulkanOpContext
    - Ops:
      - `at::native::vulkan::ops::create_lstm_context`: Creates a VulkanOpContext object with the packed and unpacked data, and returns a pointer to it.
      - `at::native::vulkan::ops::run_lstm_context`: Takes in the three input vulkan tensors (input sequence, initial hidden state and initial cell state) and a pointer to the context, and runs the LSTM operation.
    - Registered the ops in [Register.cpp](https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/vulkan/ops/Register.cpp).
    - Rewrote the subgraph function of LSTM in [vulkan_rewrite.cpp](https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/passes/vulkan_rewrite.cpp) so that `create_lstm_context` and `run_lstm_context` can be executed instead in the Vulkan GPU backend.
    - Added new test for the LSTM pre-packing and run ops: `lstm_prepack_success`
    
    Test Plan: buck run //xplat/caffe2:pt_vulkan_api_test_binAppleMac
    
    Reviewed By: SS-JIA
    
    Differential Revision: D37052597
    
    fbshipit-source-id: 3ae6b7391cbc8a05d03169cf7ebe18cc2a134026

---
+++ aten/src/ATen/native/vulkan/ops/Lstm.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#ifdef USE_VULKAN_API
+
+#include <ATen/native/vulkan/ops/Common.h>
+#include <ATen/native/vulkan/ops/VulkanOpContext.h>
+#include <torch/library.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace ops {
+
+//   packed
+//     std::vector<c10::intrusive_ptr<VulkanOpContext>> linear_op_contexts;  // {{ op context for b_ii, w_ii, op context for b_hi, w_hi,
+//                                                                           //    op context for b_if, w_if, op context for b_hf, w_hf,
+//                                                                           //    op context for b_ig, w_ig, op context for b_hg, w_hg,
+//                                                                           //    op context for b_io, w_io, op context for b_ho, w_ho,}, ...}
+//     bool has_biases{};
+//     int64_t num_layers{};
+//     double dropout{};
+//     bool train{};
+//     bool bidirectional{};
+//     bool batch_first{};
+
+//   unpacked
+//     std::vector<Tensor> params_cpu      // weights/biases (cpu)
+//     bool has_biases
+//     int64_t num_layers
+//     double dropout
+//     bool train
+//     bool bidirectional
+//     bool batch_first
+
+c10::intrusive_ptr<VulkanOpContext> create_lstm_context(
+    std::vector<Tensor>&& params_cpu,   // weights/biases (cpu)
+    bool has_biases,
+    int64_t num_layers,
+    double dropout,
+    bool train,
+    bool bidirectional,
+    bool batch_first);
+
+std::tuple<Tensor, Tensor, Tensor> run_lstm_context(
+    const Tensor& input_vk,      // input sequence (vulkan)
+    const Tensor& hx_vk,         // initial hidden state (vulkan)
+    const Tensor& cx_vk,         // initial cell state (vulkan)
+    const c10::intrusive_ptr<VulkanOpContext>& vulkan_context);
+
+} // namespace ops
+} // namespace vulkan
+} // namespace native
+} // namespace at
+
+#endif /* USE_VULKAN_API */
