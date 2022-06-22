7c99a36dda403a5d6aec7e01586475b57ff8c6cf
qqaatw <qqaatw@gmail.com>
2022-06-22 02:20:24 +0000

    [ONNX] Fix `prelu` output's shape (#79846)
    Part of #79263
    
    Before: The output has `[1]` shape when the input is a scalar.
    After: The output has `[]` shape, matching PyTorch's behavior.
    
    The original comment along the code states `torch allows scalar self, and ONNX is ambiguous about whether this is allowed`. The fact seems to be that ONNX never clearly indicates whether scalar inputs are allowed for all the ONNX operators. At least in this case, a scalar input seems to be allowed.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79846
    Approved by: https://github.com/BowenBao

--- torch/onnx/symbolic_opset8.py
+++ torch/onnx/symbolic_opset8.py
@@ -183,8 +183,12 @@ def matmul(g, self, other):
 
 def prelu(g, self, weight):
     self_rank = symbolic_helper._get_tensor_rank(self)
+    weight_sizes = symbolic_helper._get_tensor_sizes(weight)
     if self_rank is not None and self_rank > 2:
         weight = g.op("Unsqueeze", weight, axes_i=list(range(1, self_rank - 1)))
+    elif self_rank == 0 and weight_sizes == [1]:
+        # self and weight are both scalar but weight has rank == 1, squeeze weight.
+        weight = symbolic_helper._squeeze_helper(g, weight, [0])
     if symbolic_helper._try_get_scalar_type(self):
         old_type, self, weight = _try_cast_integer_to_float(g, self, weight)
         return _cast_to_type(g, g.op("PRelu", self, weight), old_type)
