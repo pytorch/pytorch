d322a38b0157c9db8707a229ae4fc80e47b473c2
asl3 <amandaliu@fb.com>
2022-06-21 19:29:25 -0700

    [quant] Add quantizer class skeleton
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79936
    
    Approved by: https://github.com/jerryzh168

--- torch/ao/quantization/experimental/APoT_tensor.py
+++ torch/ao/quantization/experimental/APoT_tensor.py
@@ -1,14 +1,12 @@
 import torch
-from torch import Tensor
+from torch.ao.quantization.experimental.quantizer import APoTQuantizer
 
 # class to store APoT quantized tensor
 class TensorAPoT(torch.Tensor):
-    @staticmethod
-    def quantize_APoT(tensor2quantize: Tensor) -> Tensor:
+    quantizer: APoTQuantizer
+
+    def __init__(self, quantizer):
         raise NotImplementedError
 
-    def dequantize(self) -> Tensor:
-        raise NotImplementedError
-
-    def q_apot_alpha(self) -> float:
+    def int_repr(self):
         raise NotImplementedError
