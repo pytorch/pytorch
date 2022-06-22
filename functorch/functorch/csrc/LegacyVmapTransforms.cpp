3fe985f5cb473d67451fa9e51044e6374d66a385
Richard Zou <zou3519@users.noreply.github.com>
2022-06-22 10:36:02 -0400

    [functorch] Clearly mark the old batching rule API as "legacy" (pytorch/functorch#897)
    It's possible it is buggy and we should aim to delete the old batching
    rule API in the future.

--- functorch/functorch/csrc/LegacyVmapTransforms.cpp
+++ functorch/functorch/csrc/LegacyVmapTransforms.cpp
@@ -4,7 +4,7 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
-#include <functorch/csrc/VmapTransforms.h>
+#include <functorch/csrc/LegacyVmapTransforms.h>
 #include <functorch/csrc/DynamicLayer.h>
 
 #include <ATen/ATen.h>
