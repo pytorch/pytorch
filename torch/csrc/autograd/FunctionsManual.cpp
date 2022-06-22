ba3ad4adecad1e936809d8efe573ce900ea3c4fd
lezcano <lezcano-93@hotmail.com>
2022-06-22 20:29:22 +0000

    More forward AD formulas
    This PR:
    - Corrects the forward AD formula of `torch.sgn`.
      - The reason why we can't use `auto_element_wise` for this operations is rather subtle. I left a comment.
      - This, in turn, fixes a problem we had in forward-over-backward for `linalg.svd` and other spectral decompositions (and `norm`, `linalg.norm`, `linalg.matrix_norm`) that were using `torch.abs` (whose derivative is given by `torch.sgn`.
    - Implement the formula for a number of missing operations `nansum`, `amax`, `amin`...
    - Simplified a few formulas, most notably the forward AD for `div` and the derivative of `norm`, `linalg.norm` and `vector_norm` for `ord=+-inf`.
    - Correct the formula for `mean`, `std_mean`, `var_mean` when `dim` is provided and equal to `()` (or `None`)
    - A few minor improvements to `sum_backward`, `unsqueeze_multiple` and formulas depending on them
    - Fix the derivatives of `std_mean` and `std_var` (complex support,
    ASAN, forward AD...)
    
    Fixes: https://github.com/pytorch/pytorch/issues/67539
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80082
    
    Approved by: https://github.com/zou3519

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -187,6 +187,16 @@ Tensor scale_grad_by_count(
   return (grad / mask.sum(dims, true)) * mask;
 }
 
+Tensor amaxamin_jvp(
+    const Tensor& x,
+    const Tensor& dx,
+    const Tensor& result,
+    IntArrayRef dim,
+    bool keepdim) {
+  auto mask = x == restore_reduced_dims(result, dim, keepdim);
+  return at::where(mask, dx, 0.).sum(dim, keepdim) / mask.sum(dim, keepdim);
+}
+
 std::tuple<Tensor, Tensor> _euclidean_dist_backward(
     const Tensor& grad,
     const Tensor& x1,
@@ -235,11 +245,11 @@ Tensor norm_backward(
   }
 
   if (p == 0.0) {
-    return at::zeros_like(self, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
+    return {};
   } else if (p == 1.0) {
     return self.sgn() * grad;
   } else if (p == 2.0) {
-    return self * (grad / norm).masked_fill_(norm == 0, 0);
+    return grad * (self / norm).masked_fill_(norm == 0, 0);
   } else if (std::isinf(p)) {
     // Derivative of amax(abs(self), dim, keepdim) but respecting nans
     // We create a mask of `argmax`: it's argmax if self.abs() == norm or it's
@@ -475,17 +485,13 @@ Tensor mvlgamma_backward(Tensor grad, const Tensor& self, int64_t p) {
   return grad * args.digamma_().sum(-1);
 }
 
-Tensor sgn_backward(Tensor result, Tensor grad, Tensor self) {
-  if (self.is_complex()) {
-    auto abs = at::abs(self);
-    // C -> C
-    // https://arxiv.org/pdf/1701.00392.pdf Section 4.20
-    return at::where(
-        abs == 0.0,
-        at::zeros({}, grad.options()),
-        (grad / abs - (at::real(grad / self) * result)));
+Tensor sgn_backward(const Tensor& x, const Tensor& gx, const Tensor& sgn) {
+  if (x.is_complex()) {
+    auto abs = x.abs();
+    return ((gx - (sgn * sgn) * gx.conj()) / (2. * abs))
+        .masked_fill_(abs == 0., 0.);
   } else {
-    return at::zeros_like(self, at::MemoryFormat::Preserve);
+    return at::_efficientzerotensor(sgn.sizes(), sgn.options());
   }
 }
 
@@ -551,14 +557,22 @@ Tensor deg2rad_backward(const Tensor& grad) {
 }
 
 Tensor unsqueeze_multiple(const Tensor& t, IntArrayRef dim, size_t n_dims) {
-  auto dims_to_unsqueeze = at::dim_list_to_bitset(dim, n_dims);
-  Tensor res = t;
-  for (const auto i : c10::irange(n_dims)) {
-    if (dims_to_unsqueeze[i]) {
-      res = res.unsqueeze(i);
+  auto dim_size = dim.size();
+  // Optimisation for two common cases
+  if (dim_size == 0) {
+    return t;
+  } else if (dim_size == 0) {
+    return t.unsqueeze(dim[0]);
+  } else {
+    auto dims_to_unsqueeze = at::dim_list_to_bitset(dim, n_dims);
+    Tensor res = t;
+    for (const auto i : c10::irange(n_dims)) {
+      if (dims_to_unsqueeze[i]) {
+        res = res.unsqueeze(i);
+      }
     }
+    return res;
   }
-  return res;
 }
 
 Tensor sum_backward(
@@ -566,13 +580,8 @@ Tensor sum_backward(
     IntArrayRef sizes,
     IntArrayRef dims,
     bool keepdim) {
-  if (!keepdim && sizes.size() > 0) {
-    if (dims.size() == 1) {
-      return grad.unsqueeze(dims[0]).expand(sizes);
-    } else {
-      Tensor res = unsqueeze_multiple(grad, dims, sizes.size());
-      return res.expand(sizes);
-    }
+  if (!keepdim && sizes.size() > 0 && dims.size() > 0) {
+    return unsqueeze_multiple(grad, dims, sizes.size()).expand(sizes);
   } else {
     return grad.expand(sizes);
   }
@@ -583,17 +592,18 @@ Tensor nansum_backward(
     const Tensor& self,
     IntArrayRef dims,
     bool keepdim) {
-  auto sizes = self.sizes();
-  if (!keepdim && sizes.size() > 0) {
-    if (dims.size() == 1) {
-      return grad.unsqueeze(dims[0]).expand(sizes) * self.isnan().logical_not();
-    } else {
-      Tensor res = unsqueeze_multiple(grad, dims, sizes.size());
-      return res.expand(sizes) * self.isnan().logical_not();
-    }
-  } else {
-    return grad.expand(sizes) * self.isnan().logical_not();
-  }
+  return sum_backward(grad, self.sizes(), dims, keepdim) *
+      self.isnan().logical_not();
+}
+
+Tensor mean_backward(
+    const Tensor& grad,
+    IntArrayRef shape,
+    IntArrayRef dim,
+    int64_t numel,
+    bool keepdim) {
+  auto n = dim.size() == 0 ? numel : _safe_size(shape, dim);
+  return sum_backward(grad, shape, dim, keepdim) / n;
 }
 
 std::vector<int64_t> reverse_list(const IntArrayRef list) {
@@ -1460,14 +1470,6 @@ Tensor evenly_read_jvp(
   return at::sum(mask * grad_output);
 }
 
-static Tensor var_backward(
-    const Tensor& grad,
-    const Tensor& self,
-    int64_t correction) {
-  // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-narrowing-conversions)
-  return (2.0 / (self.numel() - correction)) * grad * (self - self.mean());
-}
-
 Tensor var_backward(
     Tensor grad,
     const Tensor& self,
@@ -1476,7 +1478,13 @@ Tensor var_backward(
     bool keepdim) {
   auto correction = correction_opt.value_or(1);
   if (self.dim() == 0 || !dim_opt.has_value()) {
-    return var_backward(grad, self, correction);
+    // To apease ASAN
+    auto n = self.numel();
+    if (n == correction) {
+      return INFINITY * grad;
+    } else {
+      return (2.0 / (self.numel() - correction)) * grad * (self - self.mean());
+    }
   }
   auto dim = dim_opt.value();
   if (!keepdim && self.dim() > 1) {
@@ -1485,28 +1493,6 @@ Tensor var_backward(
   const int64_t dof = _safe_size(self.sizes(), dim) - correction;
   // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-narrowing-conversions)
   return (2.0 / dof) * grad * (self - self.mean(dim, /*keepdim=*/true));
-}
-
-Tensor var_jvp(
-    const Tensor& self_t,
-    const Tensor& self_p,
-    const Tensor& result,
-    at::OptionalIntArrayRef dim_opt,
-    c10::optional<int64_t> correction_opt,
-    bool keepdim) {
-  auto correction = correction_opt.value_or(1);
-  if (self_p.dim() == 0 || !dim_opt.has_value()) {
-    return var_backward(self_t.conj(), self_p, correction)
-        .sum()
-        .expand_as(result)
-        .conj();
-  }
-  auto dim = dim_opt.value();
-  const int64_t dof = _safe_size(self_p.sizes(), dim) - correction;
-  return ((2.0 / dof) * self_t.conj() *
-          (self_p - self_p.mean(dim, /*keepdim=*/true)))
-      .sum(dim, keepdim)
-      .conj();
 }
 
 Tensor std_backward(
@@ -1520,51 +1506,53 @@ Tensor std_backward(
   return var_backward(grad_var, self, dim, correction, keepdim);
 }
 
-Tensor mean_backward(
-    Tensor grad,
-    const IntArrayRef sizes,
-    IntArrayRef dim,
-    bool keepdim) {
-  return sum_backward(grad, sizes, dim, keepdim) / _safe_size(sizes, dim);
-}
-
-Tensor mean_backward(Tensor grad, const IntArrayRef sizes, int64_t numel) {
-  return grad.expand(sizes) / numel;
-}
-
-static Tensor mean_backward(
-    const Tensor& grad,
-    const IntArrayRef sizes,
-    int64_t numel,
-    at::OptionalIntArrayRef dim,
-    bool keepdim) {
-  if (dim.has_value()) {
-    return mean_backward(grad, sizes, *dim, keepdim);
-  } else {
-    return mean_backward(grad, sizes, numel);
-  }
-}
-
-Tensor var_std_mean_backward(
-    const variable_list& grads,
+Tensor var_mean_backward(
+    const Tensor& gvar,
+    const Tensor& gmean,
     const Tensor& self,
-    const Tensor& r1,
-    const Tensor& r2,
-    at::OptionalIntArrayRef dim,
-    c10::optional<int64_t> correction,
-    bool keepdim,
-    bool is_std) {
-  Tensor grad;
-  if (grads[0].defined()) {
-    grad = is_std ? std_backward(r1, grads[0], self, dim, correction, keepdim)
-                  : var_backward(grads[0], self, dim, correction, keepdim);
+    at::OptionalIntArrayRef dim_opt,
+    c10::optional<int64_t> correction_opt,
+    bool keepdim) {
+  auto correction = correction_opt.value_or(1);
+  Tensor gself;
+  if (gvar.defined()) {
+    gself = var_backward(gvar, self, dim_opt, correction, keepdim);
   }
-  if (grads[1].defined()) {
-    Tensor mean_grad =
-        mean_backward(grads[1], self.sizes(), self.numel(), dim, keepdim);
-    grad = grad.defined() ? grad + mean_grad : mean_grad;
+  if (gmean.defined()) {
+    auto aux = mean_backward(
+        gmean,
+        self.sizes(),
+        dim_opt.value_or(IntArrayRef({})),
+        self.numel(),
+        keepdim);
+    gself = gself.defined() ? gself + aux : aux;
   }
-  return grad;
+  return gself;
+}
+
+Tensor std_mean_backward(
+    const Tensor& gstd,
+    const Tensor& gmean,
+    const Tensor& self,
+    const Tensor& std,
+    at::OptionalIntArrayRef dim_opt,
+    c10::optional<int64_t> correction_opt,
+    bool keepdim) {
+  auto correction = correction_opt.value_or(1);
+  Tensor gself;
+  if (gstd.defined()) {
+    gself = std_backward(std, gstd, self, dim_opt, correction, keepdim);
+  }
+  if (gmean.defined()) {
+    auto aux = mean_backward(
+        gmean,
+        self.sizes(),
+        dim_opt.value_or(IntArrayRef({})),
+        self.numel(),
+        keepdim);
+    gself = gself.defined() ? gself + aux : aux;
+  }
+  return gself;
 }
 
 Tensor masked_scatter_backward(
@@ -4908,27 +4896,23 @@ infinitely_differentiable_native_group_norm_backward(
       c = -b * mean_tensor - c * rstd_tensor * s;
       dX = a * dY_tensor + b * X_tensor + c;
       if (dmean.defined() && drstd.defined()) {
-        dX += var_std_mean_backward(
-            {dvar, dmean.view({N, G, 1, 1})},
+        dX += var_mean_backward(
+            dvar,
+            dmean.view({N, G, 1, 1}),
             X_tensor,
-            var,
-            mean_tensor,
             IntArrayRef{2, 3},
             0,
-            true,
-            false);
+            true);
       }
       dX = dX.reshape_as(X);
     } else if (dmean.defined() && drstd.defined()) {
-      dX = var_std_mean_backward(
-               {dvar, dmean.view({N, G, 1, 1})},
+      dX = var_mean_backward(
+               dvar,
+               dmean.view({N, G, 1, 1}),
                X_tensor,
-               var,
-               mean_tensor,
                IntArrayRef{2, 3},
                0,
-               true,
-               false)
+               true)
                .reshape_as(X);
     }
   }
