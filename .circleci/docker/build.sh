00e3229ba8d287bc03385bd32c66484cec2f1118
Sergii Dymchenko <sdym@fb.com>
2022-06-22 19:17:38 +0000

    Migrate pull off linux-xenial-py3_7-clang7-onnx (#79489)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79489
    Approved by: https://github.com/malfet

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -176,6 +176,13 @@ case "$image" in
     DB=yes
     VISION=yes
     ;;
+  pytorch-linux-focal-py3-clang10-onnx)
+    ANACONDA_PYTHON_VERSION=3.7
+    CLANG_VERSION=10
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    ;;
   pytorch-linux-xenial-py3-clang5-android-ndk-r19c)
     ANACONDA_PYTHON_VERSION=3.7
     CLANG_VERSION=5.0
