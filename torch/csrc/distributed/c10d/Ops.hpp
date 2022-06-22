79eb15514db5bb73f59be8a1e5fb354d6442ea50
Jiewen Tan <jwtan@fb.com>
2022-06-22 22:29:59 +0000

    [c10d] Make allgather as a custom op (#79669)
    Summary:
    This patch makes allgather as a custom op such that it's dispatcher
    passable. It's one part of the effort to route comm ops to the dispatcher
    such that tracing mechanisms that relies on the dispatcher can trace them,
    e.g., LazyTensor and AOTAutograd.
    
    Test Plan:
    python test/distributed/test_c10d_nccl.py -k test_allgather_ops
    python test/distributed/test_c10d_gloo.py -k test_allgather_basics
    ...and other existing distributed tests.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79669
    Approved by: https://github.com/mrshenli

--- torch/csrc/distributed/c10d/Ops.hpp
+++ torch/csrc/distributed/c10d/Ops.hpp
@@ -11,6 +11,9 @@ TORCH_API c10::intrusive_ptr<ProcessGroup::Work> broadcast(const c10::intrusive_
     at::TensorList tensors, const BroadcastOptions& opts = {});
 TORCH_API c10::intrusive_ptr<ProcessGroup::Work> allreduce(const c10::intrusive_ptr<ProcessGroup>& process_group,
     at::TensorList tensors, const AllreduceOptions& opts = {});
+TORCH_API c10::intrusive_ptr<ProcessGroup::Work> allgather(const c10::intrusive_ptr<ProcessGroup>& process_group,
+    const std::vector<std::vector<at::Tensor>>& output_tensors, const std::vector<at::Tensor>& input_tensors,
+    const AllgatherOptions& opts = {});
 
 } // namespace ops
 } // namespace c10d
