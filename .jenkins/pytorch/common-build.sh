68d5bed5ed3d51bdadb216e67943e644052730f5
Michael Suo <suo@fb.com>
2022-06-22 10:34:51 -0700

    Update on "[ci] delete JOB_BASE_NAME"
    `JOB_BASE_NAME` was a holdover from jenkins compatibility. Eventually,
    it morphed to be always set to the build enviroment + `-test` or
    `-build`, and we used it to detect whether we were in a build or test.
    
    That's sort of pointless, so removing and fixing up the few remaining
    use cases.
    
    [ghstack-poisoned]

--- .jenkins/pytorch/common-build.sh
+++ .jenkins/pytorch/common-build.sh
@@ -32,7 +32,7 @@ if [[ "$BUILD_ENVIRONMENT" != *win-* ]]; then
     function sccache_epilogue() {
         echo "::group::Sccache Compilation Log"
         echo '=================== sccache compilation log ==================='
-        python "$SCRIPT_DIR/print_sccache_log.py" ~/sccache_error.log 2>/dev/null
+        python "$script_dir/print_sccache_log.py" ~/sccache_error.log 2>/dev/null
         echo '=========== If your build fails, please take a look at the log above for possible reasons ==========='
         sccache --show-stats
         sccache --stop-server || true
