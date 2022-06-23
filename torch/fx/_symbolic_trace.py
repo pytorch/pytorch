c8a9da931297d471fbb2381340d7561adf789b81
Jeremy Malloch <jeremy.malloch@getcruise.com>
2022-06-23 13:04:09 -0700

    Added reporting of the name of modules
    - The name of the module that is not installed as a submodule is now
      reproted

--- torch/fx/_symbolic_trace.py
+++ torch/fx/_symbolic_trace.py
@@ -328,7 +328,7 @@ class Tracer(TracerBase):
         if self.submodule_paths:
             path = self.submodule_paths.get(mod)
             if path is None:
-                raise NameError('module is not installed as a submodule')
+                raise NameError(f'module {mod._get_name()} is not installed as a submodule')
             assert isinstance(path, str)
             return path
         # O(N^2) fallback in the case that we didn't store the submodule
@@ -337,7 +337,7 @@ class Tracer(TracerBase):
             for n, p in self.root.named_modules():
                 if mod is p:
                     return n
-            raise NameError('module is not installed as a submodule')
+            raise NameError(f'module {mod._get_name()} is not installed as a submodule')
 
     @compatibility(is_backward_compatible=True)
     def call_module(self, m: torch.nn.Module, forward: Callable[..., Any], args : Tuple[Any, ...], kwargs : Dict[str, Any]) -> Any:
