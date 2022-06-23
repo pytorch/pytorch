68617f998e33b197a9d878ed653b2cb89379fb34
Linbin Yu <linbin@fb.com>
2022-06-23 05:50:41 +0000

    Update buck_setup.sh (#80116)
    Remove destination folders if they already exist. Otherwise the copy step will fail. It happens if people tried to run this script several times.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80116
    Approved by: https://github.com/kit1980

--- scripts/buck_setup.sh
+++ scripts/buck_setup.sh
@@ -13,6 +13,7 @@ python3 generate-xnnpack-wrappers.py
 # bazel-skylib
 printf "\n[Downloading bazel-skylib-1.0.2]\n"
 curl -L -o /tmp/bazel-skylib-1.0.2.tar.gz https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz
+rm -rf bazel-skylib/
 mkdir bazel-skylib
 tar -xf /tmp/bazel-skylib-1.0.2.tar.gz -C bazel-skylib/
 
@@ -20,10 +21,12 @@ tar -xf /tmp/bazel-skylib-1.0.2.tar.gz -C bazel-skylib/
 printf "\n[Downloading glog-0.4.0]\n"
 curl -L -o /tmp/glog-0.4.0.tar.gz https://github.com/google/glog/archive/v0.4.0.tar.gz
 tar -xf /tmp/glog-0.4.0.tar.gz -C /tmp/
+rm -rf glog/
 mv /tmp/glog-0.4.0/ glog/
 
 # ruy
 printf "\n[Downloading ruy]\n"
 curl -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
 unzip -q /tmp/ruy.zip -d /tmp/
+rm -rf ruy/
 mv /tmp/ruy-a09683b8da7164b9c5704f88aef2dc65aa583e5d ruy/
