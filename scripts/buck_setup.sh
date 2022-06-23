4b1375fb353fa99ee672df978c9484e502c567e9
Linbin Yu <linbin@fb.com>
2022-06-22 20:01:39 -0700

    Update buck_setup.sh

--- scripts/buck_setup.sh
+++ scripts/buck_setup.sh
@@ -13,8 +13,8 @@ python3 generate-xnnpack-wrappers.py
 # bazel-skylib
 printf "\n[Downloading bazel-skylib-1.0.2]\n"
 curl -L -o /tmp/bazel-skylib-1.0.2.tar.gz https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz
-mkdir bazel-skylib
 rm -rf bazel-skylib/
+mkdir bazel-skylib
 tar -xf /tmp/bazel-skylib-1.0.2.tar.gz -C bazel-skylib/
 
 # glog
