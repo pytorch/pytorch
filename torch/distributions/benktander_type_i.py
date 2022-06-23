ea7ef9061c712dfeac5f3371d8d403af88e19c7e
Allen Goodman <allen.goodman@icloud.com>
2022-06-23 16:20:43 -0400

    distributions

--- torch/distributions/benktander_type_i.py
+++ torch/distributions/benktander_type_i.py
@@ -6,7 +6,6 @@ from torch.distributions.distribution import Distribution
 
 
 class BenktanderTypeI(Distribution):
-
     def __init__(self, validate_args=None):
         super().__init__(validate_args=validate_args)
 
