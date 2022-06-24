b6769daac29f0f54cea047392c996a6ffd7d5878
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-06-24 14:49:06 +0000

    Revert "Raise warning for unpickable local function (#80140)"
    This reverts commit 4b75b7d3c1504323d62c9f702de04b17aec0175c.
    
    Reverted https://github.com/pytorch/pytorch/pull/80140 on behalf of https://github.com/ejguan due to It will break the CI for TorchData

--- torch/utils/data/datapipes/iter/callable.py
+++ torch/utils/data/datapipes/iter/callable.py
@@ -4,9 +4,8 @@ from torch.utils.data.datapipes._decorator import functional_datapipe
 from torch.utils.data._utils.collate import default_collate
 from torch.utils.data.datapipes.datapipe import IterDataPipe
 from torch.utils.data.datapipes.utils.common import (
-    _check_unpickable_fn,
-    validate_input_col
-)
+    _check_lambda_fn,
+    validate_input_col)
 
 __all__ = [
     "CollatorIterDataPipe",
@@ -67,7 +66,7 @@ class MapperIterDataPipe(IterDataPipe[T_co]):
         super().__init__()
         self.datapipe = datapipe
 
-        _check_unpickable_fn(fn)
+        _check_lambda_fn(fn)
         self.fn = fn  # type: ignore[assignment]
 
         self.input_col = input_col
