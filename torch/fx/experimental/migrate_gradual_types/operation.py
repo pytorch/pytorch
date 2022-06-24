044f24eae7b04e571c0f8b938aff2e8f6550e0eb
migeedz <migeedz@fb.com>
2022-06-24 11:04:19 -0700

    Add gradual typing constraint definitions (#79912)
    Differential Revision: [D37315020](https://our.internmc.facebook.com/intern/diff/D37315020)
    
    Add high-level gradual type constraint definitions. Those definitions will be used by the constraint generator. The generator will generate high-level constraints for FX graphs which will then be transformed to constraints that are understood by Z3
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79912
    Approved by: https://github.com/anijain2305, https://github.com/jamesr66a

---
+++ torch/fx/experimental/migrate_gradual_types/operation.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+op_add = '+'
+op_sub = '-'
+op_mul = '*'
+op_div = '/'
+op_eq = '='
+op_neq = '!='
+op_imp = '=>'
+op_matching = '⊳'
+op_consistency = '~'
+op_precision = '⊑'
+op_leq = '≤'
+op_mod = '%'
