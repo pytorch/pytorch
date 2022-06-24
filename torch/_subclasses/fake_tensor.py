9d7b4a29a7b18464eb4b4197969cc8523dd7fd03
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-06-24 14:53:37 +0000

    Revert "[FakeTensor] Use the device of the meta tensor for fallback kernel (#80193)"
    This reverts commit 93e70c5973bd2ad9eeee1469dd6ac0076943d9aa.
    
    Reverted https://github.com/pytorch/pytorch/pull/80193 on behalf of https://github.com/b0noI due to broken test: https://github.com/pytorch/pytorch/runs/7035945243?check_suite_focus=true

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -393,8 +393,8 @@ class FakeTensor(torch.Tensor):
 
 
 class FakeTensorMode(TorchDispatchMode):
-    def __init__(self, allow_fallback_kernels=True):
-        self.allow_fallback_kernels = allow_fallback_kernels
+    def __init__(self, allow_cpu_fallback=True):
+        self.allow_cpu_fallback = allow_cpu_fallback
         self.fake_tensor_converter = FakeTensorConverter()
 
         # [in_kernel_invocation]
@@ -475,9 +475,9 @@ class FakeTensorMode(TorchDispatchMode):
                 try:
                     r = func(*args, **kwargs)
                 except NotImplementedError as not_implemented_error:
-                    if not self.allow_fallback_kernels:
+                    if not self.allow_cpu_fallback:
                         raise not_implemented_error
-                    r = run_fallback_kernel(func, args, kwargs, not_implemented_error)
+                    r = run_cpu_fallback(func, args, kwargs, not_implemented_error)
 
             # TODO: handle non-kwarg devices
             assert func not in _device_not_kwarg_ops, f"NYI: {func}"
@@ -493,16 +493,16 @@ class FakeTensorMode(TorchDispatchMode):
     def from_tensor(self, tensor):
         return self.fake_tensor_converter(self, tensor)
 
-def run_fallback_kernel(func, args, kwargs, orig_not_implemented_exception):
+def run_cpu_fallback(func, args, kwargs, orig_not_implemented_exception):
     with no_dispatch():
-        def to_real_tensor(e):
+        def to_cpu(e):
             if isinstance(e, FakeTensor):
-                return torch.zeros_like(e, device=e.fake_device)
+                return torch.zeros_like(e, device="cpu")
             return e
 
         try:
-            args = tree_map(to_real_tensor, args)
-            kwargs = tree_map(to_real_tensor, kwargs)
+            args = tree_map(to_cpu, args)
+            kwargs = tree_map(to_cpu, kwargs)
 
             r = func(*args, **kwargs)
         except Exception as new_exception:
