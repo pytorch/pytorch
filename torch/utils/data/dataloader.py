22261af6cb94a93c8d40422a52be36f9f342d289
erjia <erjia@fb.com>
2022-06-27 18:53:24 +0000

    Add constants

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -573,26 +573,26 @@ class DataLoader(Generic[T_co]):
                 store = dist.distributed_c10d._get_default_store()
                 if rank == 0:
                     _shared_seed_str = str(_shared_seed)
-                    store.set("_dl_shared_seed", _shared_seed_str)
+                    store.set(_utils.DATAPIPE_SHARED_SEED, _shared_seed_str)
                     logger.info(f"Shared seed ({_shared_seed_str}) sent to store on rank 0")
                     # Use 'add' instead of 'get' since for some store implementations 'add'
                     # doesn't work well with 'get'.
-                    _shared_seed_recv_cnt = store.add("_dl_shared_seed_recv_cnt", 1)
+                    _shared_seed_recv_cnt = store.add(_utils.DATAPIPE_SHARED_SEED_COUNTER, 1)
                     while _shared_seed_recv_cnt < ws:
-                        time.sleep(0.01)
-                        _shared_seed_recv_cnt = store.add("_dl_shared_seed_recv_cnt", 0)
+                        time.sleep(_utils.DATAPIPE_SHARED_SEED_CHECK_INTERVAL)
+                        _shared_seed_recv_cnt = store.add(_utils.DATAPIPE_SHARED_SEED_COUNTER, 0)
                     # Reset after all distributed processes have received the shared seed
-                    store.set("_dl_shared_seed", "")
-                    _shared_seed_recv_cnt = store.add("_dl_shared_seed_recv_cnt", -ws)
+                    store.set(_utils.DATAPIPE_SHARED_SEED, "")
+                    _shared_seed_recv_cnt = store.add(_utils.DATAPIPE_SHARED_SEED_COUNTER, -ws)
                     assert _shared_seed_recv_cnt == 0
                 else:
                     _shared_seed_str = ""
-                    store.wait(["_dl_shared_seed"], _utils.MP_STATUS_CHECK_INTERVAL)
+                    store.wait([_utils.DATAPIPE_SHARED_SEED], _utils.MP_STATUS_CHECK_INTERVAL)
                     while len(_shared_seed_str) == 0:
-                        time.sleep(0.01)
-                        _shared_seed_str = store.get("_dl_shared_seed")
+                        time.sleep(_utils.DATAPIPE_SHARED_SEED_CHECK_INTERVAL)
+                        _shared_seed_str = store.get(_utils.DATAPIPE_SHARED_SEED)
                     logger.info(f"Shared seed ({_shared_seed_str}) received from store on rank {rank}")
-                    store.add("_dl_shared_seed_recv_cnt", 1)
+                    store.add(_utils.DATAPIPE_SHARED_SEED_COUNTER, 1)
                     _shared_seed = int(_shared_seed_str)
             return _shared_seed
         else:
