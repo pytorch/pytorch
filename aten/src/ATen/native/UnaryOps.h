439cb695316ecf7592e0fd9611133f315e523526
Allen Goodman <allen.goodman@icloud.com>
2022-06-27 19:36:17 +0000

    torch.special.gamma (#78904)
    ```Python
    gamma(input, *, out=None) -> Tensor
    ```
    
    Gamma function $\Gamma\left(\text{input}\right)$.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78904
    Approved by: https://github.com/mruberry

--- aten/src/ATen/native/UnaryOps.h
+++ aten/src/ATen/native/UnaryOps.h
@@ -75,6 +75,7 @@ DECLARE_DISPATCH(unary_fn, special_bessel_j0_stub);
 DECLARE_DISPATCH(unary_fn, special_bessel_j1_stub);
 DECLARE_DISPATCH(unary_fn, special_bessel_y0_stub);
 DECLARE_DISPATCH(unary_fn, special_bessel_y1_stub);
+DECLARE_DISPATCH(unary_fn, special_gamma_stub);
 DECLARE_DISPATCH(unary_fn, special_modified_bessel_i0_stub);
 DECLARE_DISPATCH(unary_fn, special_modified_bessel_i1_stub);
 DECLARE_DISPATCH(unary_fn, special_modified_bessel_k0_stub);
