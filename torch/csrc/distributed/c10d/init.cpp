dcd17357a4aa7ed175dc6cfd46f502499dd4bdb0
Jiewen Tan <jwtan@fb.com>
2022-06-27 08:14:04 +0000

    [c10d] Make alltoall as a custom op (#79691)
    Summary:
    This patch makes alltoall as a custom op such that it's dispatcher
    passable. It's one part of the effort to route comm ops to the dispatcher
    such that tracing mechanisms that relies on the dispatcher can trace them,
    e.g., LazyTensor and AOTAutograd.
    
    Test Plan:
    BACKEND=nccl WORLD_SIZE=2 python test/distributed/test_distributed_spawn.py -v TestDistBackendWithSpawn.test_all_to_all_cuda
        BACKEND=nccl WORLD_SIZE=2 python test/distributed/test_distributed_spawn.py -v TestDistBackendWithSpawn.test_all_to_all_cuda_complex
        BACKEND=nccl WORLD_SIZE=2 python test/distributed/test_distributed_spawn.py -v TestDistBackendWithSpawn.test_all_to_all_full_group_cuda
        and other existing distributed tests.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79691
    Approved by: https://github.com/mrshenli, https://github.com/wanchaol

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -1263,7 +1263,13 @@ Arguments:
 
           .def(
               "alltoall",
-              &::c10d::ProcessGroup::alltoall,
+              [](const c10::intrusive_ptr<::c10d::ProcessGroup>& self,
+                 const std::vector<at::Tensor>& output_tensors,
+                 const std::vector<at::Tensor>& input_tensors,
+                 const ::c10d::AllToAllOptions& opts) {
+                return ::c10d::ops::alltoall(
+                    self, output_tensors, input_tensors, opts);
+              },
               py::arg("output_tensor"),
               py::arg("input_tensor"),
               py::arg("opts") = ::c10d::AllToAllOptions(),
@@ -1271,10 +1277,11 @@ Arguments:
 
           .def(
               "alltoall",
-              [](::c10d::ProcessGroup& pg,
+              [](const c10::intrusive_ptr<::c10d::ProcessGroup>& pg,
                  std::vector<at::Tensor>& output,
                  std::vector<at::Tensor>& input) {
-                return pg.alltoall(output, input, ::c10d::AllToAllOptions());
+                return ::c10d::ops::alltoall(
+                    pg, output, input, ::c10d::AllToAllOptions());
               },
               py::arg("output"),
               py::arg("input"),
