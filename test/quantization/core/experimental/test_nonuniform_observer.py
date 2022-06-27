777c12f2dfd1bddbf7f04245833e3a0320bab41b
asl3 <amandaliu@fb.com>
2022-06-27 03:51:00 -0700

    [quant] Modify APoT nonuniform quantization workflow (#80075)
    ### Summary:
    This PR updates the design of APoT Observer, Quantizer, and Tensor to be more consistent with their uniform counterparts in the PyTorch framework. APoT Observer now calculates alpha as the max between the absolute values of the max and min values in the input tensor. APoT Quantizer is modified so its instance methods quantize_APoT and dequantize_APoT are called by their global method counterparts. APoT Tensor is modified to account for the new method definition of the `quantize_APoT` from APoT Quantizer.
    
    ### Test Plan:
    Run APoT Observer class unit tests with: `python pytorch/test/quantization/core/experimental/test_nonuniform_observer.py`
    Run APoT Quantize class unit tests with: `python pytorch/test/quantization/core/experimental/test_quantizer.py`
    Run APoT Tensor class unit tests with: `python pytorch/test/quantization/core/experimental/test_quantized_tensor.py`
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80075
    Approved by: https://github.com/jerryzh168

--- test/quantization/core/experimental/test_nonuniform_observer.py
+++ test/quantization/core/experimental/test_nonuniform_observer.py
@@ -2,20 +2,23 @@
 
 from torch.ao.quantization.experimental.observer import APoTObserver
 import unittest
+import torch
 
 class TestNonUniformObserver(unittest.TestCase):
     """
-        Test case 1
+        Test case 1: calculate_qparams
         Test that error is thrown when k == 0
     """
     def test_calculate_qparams_invalid(self):
-        obs = APoTObserver(max_val=0.0, b=0, k=0)
+        obs = APoTObserver(b=0, k=0)
 
         with self.assertRaises(AssertionError):
-            obs_result = obs.calculate_qparams(signed=False)
+            alpha, gamma, quantization_levels, level_indices = obs.calculate_qparams(signed=False,
+                                                                                     min_val=torch.tensor([0]),
+                                                                                     max_val=torch.tensor([0]))
 
     """
-        Test case 2
+        Test case 2: calculate_qparams
         APoT paper example: https://arxiv.org/pdf/1909.13144.pdf
         Assume hardcoded parameters:
         * b = 4 (total number of bits across all terms)
@@ -24,8 +27,18 @@ class TestNonUniformObserver(unittest.TestCase):
         * note: b = k * n
     """
     def test_calculate_qparams_2terms(self):
-        obs = APoTObserver(max_val=1.0, b=4, k=2)
-        obs_result = obs.calculate_qparams(signed=False)
+        obs = APoTObserver(b=4, k=2)
+
+        min_val = torch.tensor([0])
+        max_val = torch.tensor([1])
+        alpha, gamma, quantization_levels, level_indices = obs.calculate_qparams(signed=False,
+                                                                                 min_val=min_val,
+                                                                                 max_val=max_val)
+
+        alpha_test = torch.max(-min_val, max_val)
+
+        # check alpha value
+        self.assertEqual(alpha, alpha_test)
 
         # calculate expected gamma value
         gamma_test = 0
@@ -35,32 +48,41 @@ class TestNonUniformObserver(unittest.TestCase):
         gamma_test = 1 / gamma_test
 
         # check gamma value
-        self.assertEqual(obs_result[0], gamma_test)
+        self.assertEqual(gamma, gamma_test)
 
         # check quantization levels size
-        quantlevels_size_test = int(len(obs_result[1]))
+        quantlevels_size_test = int(len(quantization_levels))
         quantlevels_size = 2**4
         self.assertEqual(quantlevels_size_test, quantlevels_size)
 
         # check level indices size
-        levelindices_size_test = int(len(obs_result[2]))
+        levelindices_size_test = int(len(level_indices))
         self.assertEqual(levelindices_size_test, 16)
 
         # check level indices unique values
-        level_indices_test_list = obs_result[2].tolist()
+        level_indices_test_list = level_indices.tolist()
         self.assertEqual(len(level_indices_test_list), len(set(level_indices_test_list)))
 
     """
-        Test case 3
+        Test case 3: calculate_qparams
         Assume hardcoded parameters:
         * b = 6 (total number of bits across all terms)
         * k = 2 (base bitwidth, i.e. bitwidth of every term)
         * n = 3 (number of additive terms)
     """
     def test_calculate_qparams_3terms(self):
-        obs = APoTObserver(max_val=1.0, b=6, k=2)
+        obs = APoTObserver(b=6, k=2)
 
-        obs_result = obs.calculate_qparams(signed=False)
+        min_val = torch.tensor([0])
+        max_val = torch.tensor([1])
+        alpha, gamma, quantization_levels, level_indices = obs.calculate_qparams(signed=False,
+                                                                                 min_val=min_val,
+                                                                                 max_val=max_val)
+
+        alpha_test = torch.max(-min_val, max_val)
+
+        # check alpha value
+        self.assertEqual(alpha, alpha_test)
 
         # calculate expected gamma value
         gamma_test = 0
@@ -70,23 +92,23 @@ class TestNonUniformObserver(unittest.TestCase):
         gamma_test = 1 / gamma_test
 
         # check gamma value
-        self.assertEqual(obs_result[0], gamma_test)
+        self.assertEqual(gamma, gamma_test)
 
         # check quantization levels size
-        quantlevels_size_test = int(len(obs_result[1]))
+        quantlevels_size_test = int(len(quantization_levels))
         quantlevels_size = 2**6
         self.assertEqual(quantlevels_size_test, quantlevels_size)
 
         # check level indices size
-        levelindices_size_test = int(len(obs_result[2]))
+        levelindices_size_test = int(len(level_indices))
         self.assertEqual(levelindices_size_test, 64)
 
         # check level indices unique values
-        level_indices_test_list = obs_result[2].tolist()
+        level_indices_test_list = level_indices.tolist()
         self.assertEqual(len(level_indices_test_list), len(set(level_indices_test_list)))
 
     """
-        Test case 4
+        Test case 4: calculate_qparams
         Same as test case 2 but with signed = True
         Assume hardcoded parameters:
         * b = 4 (total number of bits across all terms)
@@ -95,8 +117,17 @@ class TestNonUniformObserver(unittest.TestCase):
         * signed = True
     """
     def test_calculate_qparams_signed(self):
-        obs = APoTObserver(max_val=1.0, b=4, k=2)
-        obs_result = obs.calculate_qparams(signed=True)
+        obs = APoTObserver(b=4, k=2)
+
+        min_val = torch.tensor([0])
+        max_val = torch.tensor([1])
+        alpha, gamma, quantization_levels, level_indices = obs.calculate_qparams(signed=True,
+                                                                                 min_val=min_val,
+                                                                                 max_val=max_val)
+        alpha_test = torch.max(-min_val, max_val)
+
+        # check alpha value
+        self.assertEqual(alpha, alpha_test)
 
         # calculate expected gamma value
         gamma_test = 0
@@ -106,15 +137,15 @@ class TestNonUniformObserver(unittest.TestCase):
         gamma_test = 1 / gamma_test
 
         # check gamma value
-        self.assertEqual(obs_result[0], gamma_test)
+        self.assertEqual(gamma, gamma_test)
 
         # check quantization levels size
-        quantlevels_size_test = int(len(obs_result[1]))
+        quantlevels_size_test = int(len(quantization_levels))
         self.assertEqual(quantlevels_size_test, 49)
 
         # check negatives of each element contained
         # in quantization levels
-        quantlevels_test_list = obs_result[1].tolist()
+        quantlevels_test_list = quantization_levels.tolist()
         negatives_contained = True
         for ele in quantlevels_test_list:
             if not (-ele) in quantlevels_test_list:
@@ -122,15 +153,15 @@ class TestNonUniformObserver(unittest.TestCase):
         self.assertTrue(negatives_contained)
 
         # check level indices size
-        levelindices_size_test = int(len(obs_result[2]))
+        levelindices_size_test = int(len(level_indices))
         self.assertEqual(levelindices_size_test, 49)
 
         # check level indices unique elements
-        level_indices_test_list = obs_result[2].tolist()
+        level_indices_test_list = level_indices.tolist()
         self.assertEqual(len(level_indices_test_list), len(set(level_indices_test_list)))
 
     """
-        Test case 5
+    Test case 5: calculate_qparams
         Assume hardcoded parameters:
         * b = 6 (total number of bits across all terms)
         * k = 1 (base bitwidth, i.e. bitwidth of every term)
@@ -164,6 +195,27 @@ class TestNonUniformObserver(unittest.TestCase):
         # check level indices unique values
         level_indices_test_list = obs_result[2].tolist()
         self.assertEqual(len(level_indices_test_list), len(set(level_indices_test_list)))
+
+    """
+        Test forward method on hard-coded tensor with arbitrary values.
+        Checks that alpha is max of abs value of max and min values in tensor.
+    """
+    def test_forward(self):
+        obs = APoTObserver(b=4, k=2)
+
+        X = torch.tensor([0.0, -100.23, -37.18, 3.42, 8.93, 9.21, 87.92])
+
+        X = obs.forward(X)
+
+        alpha, gamma, quantization_levels, level_indices = obs.calculate_qparams(True)
+
+        min_val = torch.min(X)
+        max_val = torch.max(X)
+
+        expected_alpha = torch.max(-min_val, max_val)
+
+        self.assertEqual(alpha, expected_alpha)
+
 
 if __name__ == '__main__':
     unittest.main()
