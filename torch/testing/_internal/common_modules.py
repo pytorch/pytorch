3c799a17b42dd04308b2b33d38ca42276fff40f3
Joel Benjamin Schlosser <jbschlosser@fb.com>
2022-06-28 16:21:12 -0400

    Update on "Fix ModuleInfo skip logic"
    Fixes #80247
    
    This PR:
    * Refactors the skip logic as done for OpInfo in #62713, fixing the logic error
    * For tests that were wrongly skipped before and now fail:
        * Fix `TestModule.test_cpu_gpu_parity` to support Lazy modules - this was affecting `LazyConv*`
        * Adds `expectedFailure` decorators and a follow-up message to address `Conv*` failures on `TestModule.test_memory_format`
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -10,7 +10,7 @@ from torch.testing import make_tensor
 from torch.testing._internal.common_cuda import TEST_CUDNN
 from torch.testing._internal.common_dtype import floating_types
 from torch.testing._internal.common_device_type import (
-    _TestParametrizer, _update_param_kwargs, skipIf, toleranceOverride, tol,
+    _TestParametrizer, _update_param_kwargs, toleranceOverride, tol,
     skipCUDAIfCudnnVersionLessThan, skipCUDAIfRocm, precisionOverride, skipMeta)
 from torch.testing._internal.common_methods_invocations import DecorateInfo
 from torch.testing._internal.common_nn import nllloss_reference, get_reduction
