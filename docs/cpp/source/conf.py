6cd38de8f739c9b9370413959dcbac7d92fbf644
Joel Benjamin Schlosser <jbschlosser@fb.com>
2022-06-28 11:21:54 -0400

    Update on "Disable doxygen / breathe / exhale generation of C++ API docs"
    Fixes #79992
    
    This PR:
    * Removes doxygen / breathe / exhale configuration from the Sphinx config in `source/conf.py` so it no longer runs
    * Maintains the human-generated content describing API usage in the various .rst files
        * Exception: `library.rst` is removed, as its main purpose is linking to API docs
    * Removes all links to the generated API docs from the human-generated content
    
    [ghstack-poisoned]

--- docs/cpp/source/conf.py
+++ docs/cpp/source/conf.py
@@ -23,8 +23,6 @@
 import os
 # sys.path.insert(0, os.path.abspath('.'))
 
-import textwrap
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
