74ded6fc21b383b122a97270fac1c9d972a19d2f
Linbin Yu <linbin@fb.com>
2022-06-28 12:31:34 -0700

    Update buck_setup.sh
    Add a parameter for proxy setup when running this script in devserver

--- scripts/buck_setup.sh
+++ scripts/buck_setup.sh
@@ -13,16 +13,16 @@ python3 generate-xnnpack-wrappers.py
 # bazel-skylib
 printf "\n[Downloading bazel-skylib-1.0.2]\n"
 rm -rf bazel-skylib; mkdir bazel-skylib
-curl -L https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz|tar zx -C bazel-skylib
+curl $1 -L https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz|tar zx -C bazel-skylib
 
 # glog
 printf "\n[Downloading glog-0.4.0]\n"
 rm -rf glog; mkdir glog
-curl -L https://github.com/google/glog/archive/v0.4.0.tar.gz | tar zx -C glog --strip-components 1
+curl $1 -L https://github.com/google/glog/archive/v0.4.0.tar.gz | tar zx -C glog --strip-components 1
 
 # ruy
 printf "\n[Downloading ruy]\n"
-curl -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
+curl $1 -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
 unzip -q /tmp/ruy.zip -d /tmp/
 rm -rf ruy/
 mv /tmp/ruy-a09683b8da7164b9c5704f88aef2dc65aa583e5d ruy/
