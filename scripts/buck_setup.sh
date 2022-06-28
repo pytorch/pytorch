9fec238a5a93886df08e6ad67cc2229d5211b2d3
Linbin Yu <linbin@fb.com>
2022-06-28 12:50:55 -0700

    Update buck_setup.sh

--- scripts/buck_setup.sh
+++ scripts/buck_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$proxy = $1
+$PROXY_CONFIG = $1
 
 printf "\n[Creating .buckconfig]\n"
 cp .buckconfig.oss .buckconfig
@@ -15,16 +15,16 @@ python3 generate-xnnpack-wrappers.py
 # bazel-skylib
 printf "\n[Downloading bazel-skylib-1.0.2]\n"
 rm -rf bazel-skylib; mkdir bazel-skylib
-curl $proxy -L https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz|tar zx -C bazel-skylib
+curl $PROXY_CONFIG -L https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.2/bazel-skylib-1.0.2.tar.gz|tar zx -C bazel-skylib
 
 # glog
 printf "\n[Downloading glog-0.4.0]\n"
 rm -rf glog; mkdir glog
-curl $proxy -L https://github.com/google/glog/archive/v0.4.0.tar.gz | tar zx -C glog --strip-components 1
+curl $PROXY_CONFIG -L https://github.com/google/glog/archive/v0.4.0.tar.gz | tar zx -C glog --strip-components 1
 
 # ruy
 printf "\n[Downloading ruy]\n"
-curl $proxy -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
+curl $PROXY_CONFIG -L -o /tmp/ruy.zip https://github.com/google/ruy/archive/a09683b8da7164b9c5704f88aef2dc65aa583e5d.zip
 unzip -q /tmp/ruy.zip -d /tmp/
 rm -rf ruy/
 mv /tmp/ruy-a09683b8da7164b9c5704f88aef2dc65aa583e5d ruy/
