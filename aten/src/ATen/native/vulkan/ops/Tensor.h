19fe10a168f6722c1e42186a6186378064457408
Stephen Jia <ssjia@fb.com>
2022-06-30 08:08:39 -0700

    [vulkan] Integrate refactored memory mapping, remove Tensor Future pattern
    Differential Revision: D36863390
    
    fbshipit-source-id: 897e7e7324632f06aafb44509f9aeb6c06544c45

--- aten/src/ATen/native/vulkan/ops/Tensor.h
+++ aten/src/ATen/native/vulkan/ops/Tensor.h
@@ -95,74 +95,15 @@ class vTensor final {
   typedef api::Resource::Memory Memory;
 
   /*
-    Future
+    Host access
   */
 
-  template<typename Type, Access::Flags kAccess>
-  class Future final {
-    template<typename T, Access::Flags A>
-    using is_convertible = std::enable_if_t<
-        std::is_convertible<
-            Access::Pointer<T, A>,
-            Access::Pointer<Type, kAccess>>::value>;
 
-   public:
-    explicit Future(const vTensor* tensor);
-    Future(const Future&) = delete;
-    Future& operator=(const Future&) = delete;
-    Future(Future&&);
-    Future& operator=(Future&&) &;
-    Future& operator=(Future&&) && = delete;
-    template<typename T, Access::Flags A, typename = is_convertible<T, A>>
-    Future(Future<T, A>&&);
-    template<typename T, Access::Flags A, typename = is_convertible<T, A>>
-    Future& operator=(Future<T, A>&&) &;
-    template<typename T, Access::Flags A>
-    Future& operator=(Future<T, A>&&) && = delete;
-    ~Future();
+  inline void wait_for_fence() const {
+    view_->wait_for_fence();
+  }
 
-    typedef Memory::Handle<
-        Access::Pointer<
-            Type,
-            kAccess>> Payload;
-
-    // This is a blocking operation as the name suggests.  A call to host() will
-    // trigger an async copy if pending writes are detected.  Consequently, for
-    // optimal performance, put as much time and distance between the place
-    // where a vTensor::host() call occurs and the location where the returned
-    // future is explicitly waited on as a result of a call to this function.
-
-    Payload wait() const &;
-
-   private:
-    template<typename, Access::Flags>
-    friend class Future;
-
-    // Intentionally disabed to enforce a usage pattern wherein the Future's
-    // lifetime exceeds that of the Payload as we use the Future's destructor
-    // to eagerly (as opposed to lazily and upon first use) upload the
-    // modifications back onto the GPU in an effort to hide the upload latency.
-
-    Payload wait() const && = delete;
-
-   private:
-    const vTensor* tensor_;
-  };
-
-  /*
-    Host access - these functions will be expensive if they trigger a GPU -> CPU
-    sync due to pending writes.  A call to host() will trigger an async copy in
-    such scenarios, which is then explicitly waited on as part of Future::wait().
-    Consequently, for optimal performance, put as much time and distance between
-    the place where this function is called, and the location where the future is
-    waited on.
-  */
-
-  template<typename Type>
-  Future<Type, Access::Read> host(api::Command::Buffer&) const &;
-
-  template<typename Type, Access::Flags kAccess>
-  Future<Type, kAccess> host(api::Command::Buffer&) &;
+  api::VulkanBuffer& host_buffer(api::Command::Buffer&, const Access::Flags) &;
 
   /*
     Device access - these functions will be expensive if they trigger a buffer
@@ -198,25 +139,6 @@ class vTensor final {
   size_t nbytes() const;
 
  private:
-  // Some overloads below are intentionally disabled to enforce a usage pattern
-  // that ensures the Tensor's lifetime exceeds that of the scope in which the
-  // underlying data is accessed.  Allowing deleted overloads below to be
-  // invoked on a temporary would open the door to the possibility of accessing
-  // the underlying memory out of the expected scope.
-
-  /*
-    Host
-  */
-
-  const vTensor* host(api::Command::Buffer&) const;
-  vTensor* host(api::Command::Buffer&, Access::Flags);
-
-  template<typename Type>
-  Future<Type, Access::Read> host(api::Command::Buffer&) const && = delete;
-
-  template<typename Type, Access::Flags kAccess>
-  Future<Type, kAccess> host(api::Command::Buffer&) && = delete;
-
   /*
     Device
   */
@@ -262,7 +184,8 @@ class vTensor final {
     */
 
     api::VulkanBuffer& staging(api::Command::Buffer&, Stage::Flags, Access::Flags) const;
-    vTensor::Memory& wait() const;
+
+    void wait_for_fence() const;
 
     /*
       Metadata
@@ -355,7 +278,6 @@ class vTensor final {
     mutable api::VulkanBuffer buffer_;
     mutable api::VulkanImage image_;
     mutable api::VulkanBuffer staging_;
-    mutable Memory staging_memory_;
     mutable api::VulkanFence fence_;
 
     // Context
@@ -416,89 +338,6 @@ void verify(const TensorOptions& options);
 //
 // Impl
 //
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline vTensor::Future<Type, kAccess>::Future(
-    const vTensor* const tensor)
-  : tensor_(tensor) {
-  TORCH_INTERNAL_ASSERT_DEBUG_ONLY(
-      tensor_,
-      "Invalid Vulkan tensor!");
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline vTensor::Future<Type, kAccess>::Future(
-    Future&& future)
-  : tensor_(std::move(future.tensor_)) {
-  future.tensor_ = nullptr;
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline vTensor::Future<Type, kAccess>&
-vTensor::Future<Type, kAccess>::operator=(
-    Future&& future) & {
-  tensor_ = std::move(future.tensor_);
-  future.tensor_ = nullptr;
-  return *this;
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-template<typename Type_, vTensor::Access::Flags kAccess_, typename>
-inline vTensor::Future<Type, kAccess>::Future(
-    Future<Type_, kAccess_>&& future)
-  : tensor_(std::move(future.tensor_)) {
-  future.tensor_ = nullptr;
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-template<typename Type_, vTensor::Access::Flags kAccess_, typename>
-inline vTensor::Future<Type, kAccess>&
-vTensor::Future<Type, kAccess>::operator=(
-    Future<Type_, kAccess_>&& future) & {
-  tensor_ = std::move(future.tensor_);
-  future.tensor_ = nullptr;
-  return *this;
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline vTensor::Future<Type, kAccess>::~Future() {
-#if VULKAN_SYNC_TENSORS_EAGERLY
-  // Sync eagerly in an effort to hide latency.
-  // Upside: Kick off the async transfer early on to keep the GPU busy.
-  // Downside: An extra CPU command submission.
-  if (tensor_ && (Access::Write & kAccess)) {
-    if (tensor_->has_image()) {
-      tensor_->image();
-    }
-    else {
-      tensor_->buffer();
-    }
-  }
-#endif
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline typename vTensor::Future<Type, kAccess>::Payload
-vTensor::Future<Type, kAccess>::wait() const & {
-  TORCH_CHECK(
-      tensor_,
-      "vTensor::Future is in an invalid state!  "
-      "Potential reason: This future is moved from.");
-
-  return tensor_->view_->wait().template map<Type, kAccess>();
-}
-
-template<typename Type>
-inline vTensor::Future<Type, vTensor::Access::Read>
-vTensor::host(api::Command::Buffer& command_buffer) const & {
-  return Future<Type, vTensor::Access::Read>(host(command_buffer));
-}
-
-template<typename Type, vTensor::Access::Flags kAccess>
-inline vTensor::Future<Type, kAccess>
-vTensor::host(api::Command::Buffer& command_buffer) & {
-  return Future<Type, kAccess>(host(command_buffer, kAccess));
-}
 
 inline bool vTensor::has_image() const {
   return view_->has_image();
