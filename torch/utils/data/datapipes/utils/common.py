f1bf7340283c21d6560d7fba344e4cdde41ce186
Robert <xiurobert@gmail.com>
2022-06-30 13:48:58 +0000

    Use str(fn) for fn_name

--- torch/utils/data/datapipes/utils/common.py
+++ torch/utils/data/datapipes/utils/common.py
@@ -42,11 +42,7 @@ def validate_input_col(fn: Callable, input_col: Optional[Union[int, tuple, list]
     var_pos_args = [p for p in sig.parameters.values() if
                           p.kind == inspect.Parameter.VAR_POSITIONAL]
 
-    fn_name = "(unknown)"
-    if hasattr(fn, "__name__"):
-        fn_name = fn.__name__
-    elif inspect.isclass(type(fn)):
-        fn_name = type(fn).__name__
+    fn_name = str(fn)
 
     if len(sig.parameters) > input_col_size:
         non_default_params = [p for p in sig.parameters.values() if p.default is p.empty]
