c74e7ba7202519fe5ff6fb083a0ea106f4991904
ssjia <ssjia@fb.com>
2022-06-30 20:58:34 -0700

    [vulkan] Refactor Descriptor::Pool (#80727)
    Part of a refactor of the Vulkan codebase.
    
    Differential Revision: [D37125677](https://our.internmc.facebook.com/intern/diff/D37125677/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80727
    Approved by: https://github.com/kimishpatel

--- aten/src/ATen/native/vulkan/api/Descriptor.h
+++ aten/src/ATen/native/vulkan/api/Descriptor.h
@@ -53,90 +53,76 @@ class DescriptorSet final {
   void add_binding(const ResourceBinding& resource);
 };
 
-//
-// This struct defines caches of descriptor pools, and descriptor sets allocated
-// from those pools, intended to minimize redundant object reconstructions or
-// accelerate unavoidable memory allocations, both at the cost of extra memory
-// consumption.
-//
-// A descriptor set is logically an array of descriptors, each of which
-// references a resource (i.e. buffers and images), in turn telling the core
-// executing the shader, where in GPU, or GPU-accessible system, memory the said
-// resource resides.
-//
-// To accelerate creation of the descriptor sets, modern graphics APIs allocate
-// them from a pool, more elaborately referred to as descriptor pools, which do
-// need to be purged frequently _after_ none of the descriptors the pools contain
-// is in use by the GPU.  Care must be taken that descriptors are not freed while
-// they are in use by the pipeline, which considering the asynchronous nature of
-// CPU-GPU interactions, can be anytime after the command is issued until it is
-// fully executed by the GPU.
-//
-// As you can imagine, it is possible to have multiple descriptor pools, each of
-// which is configured to house different types of descriptor sets with different
-// allocation strategies. These descriptor pools themselves are fairly stable
-// objects in that they theymself should not be created and destroyed frequently.
-// That is the reason why we store them in a cache, which according to our usage
-// of the term 'cache' in this implementatoin, is reserved for objects that are
-// created infrequently and stabilize to a manageable number quickly over the
-// lifetime of the program.
-//
-// Descriptor sets though, on the other hand, are allocated from pools which
-// indeed does mean that the pools must be purged on a regular basis or else
-// they will run out of free items.  Again, this is in line with our usage of
-// the term 'pool' in this implementation which we use to refer to a container
-// of objects that is allocated out of and is required to be frequently purged.
-//
-// It is important to point out that for performance reasons, we intentionally
-// do not free the descriptor sets individually, and instead opt to purge the
-// pool in its totality, even though Vulkan supports the former usage pattern
-// as well.  This behavior is by design.
-//
+class DescriptorSetPile final {
+ public:
+  DescriptorSetPile(
+      const uint32_t,
+      const VkDescriptorSetLayout,
+      const VkDevice,
+      const VkDescriptorPool);
 
-struct Descriptor final {
-  //
-  // Pool
-  //
+  DescriptorSetPile(const DescriptorSetPile&) = delete;
+  DescriptorSetPile& operator=(const DescriptorSetPile&) = delete;
 
-  class Pool final {
-   public:
-    explicit Pool(const GPU& gpu);
-    Pool(const Pool&) = delete;
-    Pool& operator=(const Pool&) = delete;
-    Pool(Pool&&);
-    Pool& operator=(Pool&&);
-    ~Pool();
+  DescriptorSetPile(DescriptorSetPile&&) = default;
+  DescriptorSetPile& operator=(DescriptorSetPile&&) = default;
 
-    DescriptorSet allocate(
-        const VkDescriptorSetLayout handle,
-        const ShaderLayout::Signature& signature);
-    void purge();
+  ~DescriptorSetPile() = default;
 
-   private:
-    void invalidate();
+ private:
+  uint32_t pile_size_;
+  VkDescriptorSetLayout set_layout_;
+  VkDevice device_;
+  VkDescriptorPool pool_;
+  std::vector<VkDescriptorSet> descriptors_;
+  size_t in_use_;
 
-   private:
-    struct Configuration final {
-      static constexpr uint32_t kQuantum = 16u;
-      static constexpr uint32_t kReserve = 64u;
-    };
+ public:
+  VkDescriptorSet get_descriptor_set();
 
-    VkDevice device_;
-    Handle<VkDescriptorPool, VK_DELETER(DescriptorPool)> descriptor_pool_;
+ private:
+  void allocate_new_batch();
+};
 
-    struct {
-      struct Layout final {
-        std::vector<VkDescriptorSet> pool;
-        size_t in_use;
-      };
+struct DescriptorPoolConfig final {
+  // Overall Pool capacity
+  uint32_t descriptorPoolMaxSets;
+  // DescriptorCounts by type
+  uint32_t descriptorUniformBufferCount;
+  uint32_t descriptorStorageBufferCount;
+  uint32_t descriptorCombinedSamplerCount;
+  uint32_t descriptorStorageImageCount;
+  // Pile size for pre-allocating descriptor sets
+  uint32_t descriptorPileSizes;
+};
 
-      ska::flat_hash_map<VkDescriptorSetLayout, Layout> layouts;
-    } set_;
-  } pool /* [thread_count] */;
+class DescriptorPool final {
+ public:
+  explicit DescriptorPool(
+      const VkDevice, const DescriptorPoolConfig&);
 
-  explicit Descriptor(const GPU& gpu)
-    : pool(gpu) {
-  }
+  DescriptorPool(const DescriptorPool&) = delete;
+  DescriptorPool& operator=(const DescriptorPool&) = delete;
+
+  DescriptorPool(DescriptorPool&&) = delete;
+  DescriptorPool& operator=(DescriptorPool&&) = delete;
+
+  ~DescriptorPool();
+
+ private:
+  VkDevice device_;
+  VkDescriptorPool pool_;
+  DescriptorPoolConfig config_;
+  // New Descriptors
+  std::mutex mutex_;
+  ska::flat_hash_map<VkDescriptorSetLayout, DescriptorSetPile> piles_;
+
+ public:
+  DescriptorSet get_descriptor_set(
+      const VkDescriptorSetLayout handle,
+      const ShaderLayout::Signature& signature);
+
+  void flush();
 };
 
 } // namespace api
