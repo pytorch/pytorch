cf19d7a729a7e9d06710b8bf577834cc42512dfb
ssjia <ssjia@fb.com>
2022-06-30 20:58:20 -0700

    [vulkan] Refactor Command (#80720)
    Part of a refactor of the Vulkan codebase.
    
    This diff introduces the new `CommandBuffer` class as well as a revamped `CommandPool` class.
    
    ## `CommandPool`
    
    This class is a wrapper around a `VkCommandPool` that is responsible for allocating command buffers. It only has one function of interest, `get_new_cmd()`, which will fetch a command buffer from the pool. At the end of each inference, `flush()` will reset all command buffers in the pool, allowing them to be re-used.
    
    ## `CommandBuffer`
    
    This class will represent a command buffer and will provide function calls necessary to encode commands into a command buffer.
    
    This class has an internal state that tracks what stage of recording it's in. Note that this is separate from the State of the `VkCommandBuffer` that it owns, which is defined by the Vulkan API.
    
    ```
    enum State {
        INVALID,  // Used to indicate the command buffer is moved from
        NEW,  // Set during constructor
        RECORDING,  // Set during call to begin(), dispatch(), and copy_texture_to_texture()
        PIPELINE_BOUND,  // Set during call to  bind_pipeline()
        DESCRIPTORS_BOUND,  // Set during call to bind_descriptors()
        BARRIERS_INSERTED,  // Set during call to insert_barrier()
        READY,  //  Set during call to end()
        SUBMITTED,  // Set during call to get_submit_handle()
      };
    ```
    
    The usage of a `CommandBuffer` object can be described below.
    
    ```
    // Fetch a new command buffer from the command pool
    CommandBuffer cmd = command_pool.get_new_cmd()
    // The state is now NEW
    
    // This calls vkBeginCommandBuffer
    cmd.begin()
    // The state is now RECORDING
    
    // This calls vkCmdBindPipeline to bind a compute pipeline to the command buffer
    cmd.bind_pipeline(...)
    // The state is now PIPELINE_BOUND
    
    // This calls vkCmdBindDescriptors to bind a descriptor set to the command buffer
    cmd.bind_descriptors(...)
    // The state is now DESCRIPTORS_BOUND
    
    // This calls vkCmdPipelineBarrier and inserts a pipeline barrier in the command buffer
    cmd.insert_barrier(...)
    // The state is now BARRERS_INSERTED
    
    // This calls vkCmdDispatch and records a compute dispatch job into the command buffer
    cmd.dispatch(...)
    // The state is now RECORDING
    
    // From here, we can repeat the process from bind_pipeline() to record a new compute dispatch into the command buffer.
    
    ...
    
    // When we are ready to submit the command buffer, this calls vkEndCommandBuffer
    cmd.end()
    // The state is now READY
    
    // Relinquish the VkCommandBuffer cmd owns and returns it to be used in a vkQueueSubmit
    VkCommandBuffer handle = cmd.get_submit_handle()
    // The state is now SUBMITTED
    
    ```
    
    Differential Revision: [D36824003](https://our.internmc.facebook.com/intern/diff/D36824003/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80720
    Approved by: https://github.com/kimishpatel

--- aten/src/ATen/native/vulkan/api/Command.h
+++ aten/src/ATen/native/vulkan/api/Command.h
@@ -14,6 +14,118 @@ namespace native {
 namespace vulkan {
 namespace api {
 
+class CommandBuffer final {
+ public:
+  explicit CommandBuffer(
+      const VkCommandBuffer,
+      const VkCommandBufferUsageFlags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+
+  CommandBuffer(const CommandBuffer&) = delete;
+  CommandBuffer& operator=(const CommandBuffer&) = delete;
+
+  CommandBuffer(CommandBuffer&&) noexcept;
+  CommandBuffer& operator=(CommandBuffer&&) noexcept;
+
+  ~CommandBuffer() = default;
+
+  // The lifecycle of a command buffer is as follows:
+  enum State {
+    INVALID,  // Used to indicate the command buffer is moved from
+    NEW,  // Set during constructor
+    RECORDING,  // Set during call to begin(), dispatch(), and copy_texture_to_texture()
+    PIPELINE_BOUND,  // Set during call to  bind_pipeline()
+    DESCRIPTORS_BOUND,  // Set during call to bind_descriptors()
+    BARRIERS_INSERTED,  // Set during call to insert_barrier()
+    READY,  //  Set during call to end()
+    SUBMITTED,  // Set during call to get_submit_handle()
+  };
+
+  struct Bound {
+    VkPipeline pipeline;
+    VkPipelineLayout pipeline_layout;
+    utils::uvec3 local_workgroup_size;
+    VkDescriptorSet descriptors;
+
+    explicit Bound()
+      : pipeline{VK_NULL_HANDLE},
+        pipeline_layout{VK_NULL_HANDLE},
+        local_workgroup_size{0u, 0u, 0u},
+        descriptors{VK_NULL_HANDLE} {
+    }
+
+    inline void reset() {
+      pipeline = VK_NULL_HANDLE;
+      pipeline_layout = VK_NULL_HANDLE;
+      local_workgroup_size = {0u, 0u, 0u};
+      descriptors = VK_NULL_HANDLE;
+    }
+  };
+
+ private:
+  VkCommandBuffer handle_;
+  VkCommandBufferUsageFlags flags_;
+  State state_;
+  Bound bound_;
+
+ public:
+  void begin();
+  void end();
+
+  void bind_pipeline(
+      const VkPipeline,
+      const VkPipelineLayout,
+      const utils::uvec3);
+  void bind_descriptors(const VkDescriptorSet);
+
+  void insert_barrier(const PipelineBarrier& pipeline_barrier);
+  void dispatch(const utils::uvec3&);
+
+  void copy_texture_to_texture(
+      const api::VulkanImage&,
+      const api::VulkanImage&,
+      const api::utils::uvec3&,
+      const api::utils::uvec3&,
+      const api::utils::uvec3&);
+
+  VkCommandBuffer get_submit_handle();
+};
+
+struct CommandPoolConfig final {
+  uint32_t cmdPoolInitialSize;
+  uint32_t cmdPoolBatchSize;
+};
+
+class CommandPool final {
+ public:
+  explicit CommandPool(
+      const VkDevice, const uint32_t, const CommandPoolConfig&);
+
+  CommandPool(const CommandPool&) = delete;
+  CommandPool& operator=(const CommandPool&) = delete;
+
+  CommandPool(CommandPool&&) noexcept;
+  CommandPool& operator=(CommandPool&&) = delete;
+
+  ~CommandPool();
+
+ private:
+  VkDevice device_;
+  uint32_t queue_family_idx_;
+  VkCommandPool pool_;
+  CommandPoolConfig config_;
+  // New Buffers
+  std::vector<VkCommandBuffer> buffers_;
+  size_t in_use_;
+
+ public:
+  CommandBuffer get_new_cmd();
+
+  void flush();
+
+ private:
+  void allocate_new_batch(const uint32_t);
+};
+
 struct Command final {
   class Pool;
 
