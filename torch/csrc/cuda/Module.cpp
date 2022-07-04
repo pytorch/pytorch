4e718520c0af8081eef1381b3b4bb9ffee992171
Can Balioglu <cbalioglu@users.noreply.github.com>
2022-07-04 16:47:09 +0000

    Improve readability of cuda_lazy_init (#80788)
    This PR cleans up the implementation of `cuda_lazy_init.cpp` and improves its readability. No behavioral changes are introduced.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80788
    Approved by: https://github.com/ezyang

--- torch/csrc/cuda/Module.cpp
+++ torch/csrc/cuda/Module.cpp
@@ -42,7 +42,7 @@ static bool in_bad_fork = false; // True for children forked after cuda init
 // Called in the forked child if cuda has already been initialized
 static void forked_child() {
   in_bad_fork = true;
-  torch::utils::set_run_yet_variable_to_false();
+  torch::utils::set_requires_cuda_init(false);
 }
 #endif
 
