6c4044345dafa5494a3b5a95a690a5012f78911d
qqaatw <qqaatw@gmail.com>
2022-07-04 14:28:25 +0800

    Use tensor string key to prevent possible cache issue

--- aten/src/ATen/native/mps/operations/ReduceOps.mm
+++ aten/src/ATen/native/mps/operations/ReduceOps.mm
@@ -1506,7 +1506,7 @@ void argmin_argmax_out_mps
     auto stream = at::mps::getCurrentMPSStream();
 
     @autoreleasepool {
-        string key = func_name + to_string(dim_) + ":" + native_mps::getMPSTypeString(input_t.scalar_type());
+        string key = func_name + to_string(dim_) + ":" + native_mps::getTensorsStringKey(input_t);
         CachedGraph* cachedGraph = static_cast<CachedGraph *>(cache_->LookUp(key));
 
         if(!cachedGraph) {
