6ab1bc6b907fbf1da2a49ad678c4523e9da46558
goldenxuett <ttanx@fb.com>
2022-07-05 15:34:03 -0700

    Update on "[JIT] Add aliasing checks in FunctionSchema with associated tests"
    [ghstack-poisoned]

--- test/cpp/jit/test_schema_info.cpp
+++ test/cpp/jit/test_schema_info.cpp
@@ -43,20 +43,16 @@ TEST(FunctionSchemaAreAliasingTest, InvalidArgument) {
           "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))")
           ->schema();
   ASSERT_THROW(
-      schema.areAliasing(
-          {c10::input, -1}, {c10::output, 0}),
-      c10::Error);
+      schema.areAliasing({c10::input, -1}, {c10::output, 0}), c10::Error);
   ASSERT_THROW(
-      schema.areAliasing(
-          {c10::input, 0}, {c10::output, -1}),
-      c10::Error);
+      schema.areAliasing({c10::input, 0}, {c10::output, -1}), c10::Error);
 }
 
 TEST(FunctionSchemaAreAliasingTest, Wildcard) {
   c10::FunctionSchema schema =
       torch::jit::getOperatorForLiteral(
-        "aten::split.Tensor(Tensor(a -> *) self, int split_size, int dim=0) -> Tensor(a)[]")
-        ->schema();
+          "aten::split.Tensor(Tensor(a -> *) self, int split_size, int dim=0) -> Tensor(a)[]")
+          ->schema();
   ASSERT_TRUE(schema.areAliasing({c10::input, 0}, {c10::output, 0}, true));
   ASSERT_FALSE(schema.areAliasing({c10::input, 0}, {c10::output, 0}, false));
 }
