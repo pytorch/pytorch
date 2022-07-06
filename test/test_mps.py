61080c7f97f5978dfbda988469f42dd9b58b94b3
qqaatw <qqaatw@gmail.com>
2022-07-06 15:36:47 +0000

    [MPS] Add huber loss (#80163)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80163
    Approved by: https://github.com/kulinseth, https://github.com/malfet

--- test/test_mps.py
+++ test/test_mps.py
@@ -6105,6 +6105,7 @@ class TestConsistency(TestCase):
         'nn.functional.hinge_embedding_loss': ['torch.float32'],
         'nn.functional.kl_div': ['torch.float32'],
         'nn.functional.l1_loss': ['torch.float32'],
+        'nn.functional.huber_loss': ['torch.float32'],
         'nn.functional.leaky_relu': ['torch.float32'],
         'nn.functional.mse_loss': ['torch.float16', 'torch.float32'],
         'nn.functional.relu': ['torch.float32',
