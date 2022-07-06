c564897044a9b958673744fe50f751bb06add22d
lezcano <lezcano-93@hotmail.com>
2022-07-05 22:04:30 -0700

    Make slogdet, linalg.sloget and logdet support metatensors (#79742) (#79742)
    Summary:
    This PR also adds complex support for logdet, and makes all these
    functions support out= and be composite depending on one function. We
    also extend the support of `logdet` to complex numbers and improve the
    docs of all these functions.
    
    We also use `linalg_lu_factor_ex` in these functions, so we remove the
    synchronisation present before.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79742
    Approved by: https://github.com/IvanYashchuk, https://github.com/albanD
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/37a5819665bd37a2af148a42dfcc5255e3194f3e
    
    Reviewed By: mehtanirav
    
    Differential Revision: D37604723
    
    Pulled By: mehtanirav
    
    fbshipit-source-id: 208b405f7688efb32f30f95bc6d364b3ce8bef16

--- test/test_meta.py
+++ test/test_meta.py
@@ -468,9 +468,7 @@ meta_function_expected_failures = {
     torch.linalg.eigvals: {f32, f64},
     torch.linalg.householder_product: {f32, f64},  # aten::linalg_householder_product
     torch.linalg.lstsq: {f32, f64},  # aten::linalg_lstsq.out
-    torch.linalg.slogdet: {f32, f64},  # aten::linalg_slogdet
     torch.linalg.solve_triangular: {f32, f64},  # aten::linalg_solve_triangular
-    torch.logdet: {f32, f64},  # aten::_local_scalar_dense, aten::nonzero
 }
 
 """
@@ -700,10 +698,8 @@ meta_dispatch_expected_failures = {
     aten.linalg_householder_product.default: {f32, f64},  # aten::linalg_householder_product
     aten.linalg_householder_product.out: {f32, f64},  # aten::linalg_householder_product.out
     aten.linalg_lstsq.default: {f32, f64},  # aten::linalg_lstsq.out
-    aten.linalg_slogdet.default: {f32, f64},  # aten::linalg_slogdet
     aten.linalg_solve_triangular.default: {f32, f64},  # aten::linalg_solve_triangular
     aten.linalg_solve_triangular.out: {f32, f64},  # aten::linalg_solve_triangular.out
-    aten.logdet.default: {f32, f64},  # aten::_local_scalar_dense, aten::nonzero
     aten.ormqr.default: {f32, f64},  # aten::ormqr
     aten.ormqr.out: {f32, f64},  # aten::ormqr.out
     aten.symeig.default: {f32, f64},  # aten::_symeig_helper
