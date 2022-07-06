1bca1f42260e6e669e2f8c641f045acfd78ca778
Kurt Mohler <kmohler@quansight.com>
2022-07-05 22:04:30 -0700

    Fix `Module.share_memory` error (#80843) (#80843)
    Summary:
    Fixes #80733
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80843
    Approved by: https://github.com/malfet
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/4c279994fd56e3d0c34dcd39adedb73abccb0b65
    
    Reviewed By: mehtanirav
    
    Differential Revision: D37619124
    
    Pulled By: mehtanirav
    
    fbshipit-source-id: 2b0d71d5a420d4aab286eea0b3cccdc96d15afeb

--- test/test_torch.py
+++ test/test_torch.py
@@ -265,6 +265,14 @@ class TestTorchDeviceType(TestCase):
                 error_storage = a.to(error_dtype).storage()
                 b = torch.tensor([], device=device, dtype=dtype).set_(error_storage)
 
+    @onlyCUDA
+    def test_module_share_memory(self):
+        # Test fix for issue #80733
+        # See https://github.com/pytorch/pytorch/issues/80733
+        model = torch.nn.Linear(3, 1)
+        model_cuda = model.to('cuda')
+        model.share_memory()
+
     @dtypes(torch.float32, torch.complex64)
     def test_deepcopy(self, device, dtype):
         from copy import deepcopy
