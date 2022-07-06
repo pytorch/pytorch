e45b7ad4b148ca053561a523cf2efc9f1a5d9407
Nikolay Korovaiko <korovaikon@gmail.com>
2022-07-06 09:50:14 -0700

    python bindings for create_metric_report (#79679)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79679
    Approved by: https://github.com/JackCaoG, https://github.com/wconstab

--- torch/_lazy/metrics.py
+++ torch/_lazy/metrics.py
@@ -11,3 +11,7 @@ def counter_names():
 def counter_value(name: str):
     """Return the value of the counter with the speficied name"""
     return torch._C._lazy._counter_value(name)
+
+def metrics_report():
+    """Return the combined (lazy core and backend) metric report"""
+    return torch._C._lazy._metrics_report()
