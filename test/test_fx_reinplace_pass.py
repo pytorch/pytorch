a479354d9ac1f2c29620ac928e3e006a06711971
Brian Hirsh <hirsheybar@fb.com>
2022-07-06 10:56:55 -0700

    Update on "add a reinplacing FX pass"
    Adds a "reinplacing" FX transform, that goes through an FX graph and tries to convert out-of-place op calls into inplace calls whenever possible.
    
    
    Followups from this PR include:
    - Set up torch bench, and run the whole torchbench suite using AOTAutograd + functionalize + rein placing transforms to surface any issues (this is what I'm currently working on). Right now, I have some basic unit tests just to sanity check that the general logic makes sense.
    - Add any missing inplace ops. This is mostly the `*_scatter*` ops, e.g. `diagonal_scatter_`, because these ops will commonly show up an FX graph after running functionalization.
    
    The criteria for when you can swap an op `b = a.add(...)` with `a.add_(...)` is:
    (1) An inplace variant of the operator with the same schema needs to exist (`aten.add` -> `aten.add_`)
    (2) `a` (**or any of its aliases**) can't be used as an input to any other operators later on in the graph
    (3) `a` can't be one of the inputs to the entire graph. It also can't be an **alias** of any of the inputs ***
    
    *** One thing to note: (3) means that we can't technically guarantee that we'll get back **all** memory usage that we lost from functionalization. Functionalization converts input mutations into out-of-place calls, and then adds a `copy_()` to the end of the graph to preserve semantics.
    
    I added logic to handle `copy_()` in this PR because it it's a pretty important optimizations in the context of `functionalization()`: any program that performs input mutations will have a `copy_()` in it after running functionalization.
    ```
    // Before functionalization
    def foo(a):
        tmp1 = a.add_(1)
        tmp2 = a.add(2)
    
    // After functionalization
    def foo(a)
        tmp1 = a.add(1)
        tmp2 = a.add(2)
        ....
        a.copy_(tmp1)
    
    // After re-inplacing
    def foo(a)
        // first add() is safe to re-inplace even though a is a program input, because a's data is overwritten later by a copy_()
        tmp1 = a.add_(1)
       // second add() is NOT safe to re-inplace, because:
       // (1) a and tmp1 are aliased
       // (2) tmp1 is used as an input later in the program
        tmp2 = a.add(2)
        ....
        a.copy_(tmp1)
    ```
    
    
    
    
    
    [ghstack-poisoned]

--- test/test_fx_reinplace_pass.py
+++ test/test_fx_reinplace_pass.py
@@ -11,10 +11,8 @@ class TestReinplacePass(TestCase):
         # into add_()
         def f(x):
             a = x.clone()
-            b = a.view(-1)
-            b_updated = b.add(1)
-            a_updated = b_updated.view(a.shape)
-            return a_updated
+            b = a.add(1)
+            return b
 
         inpt = torch.ones(2, device='meta')
         f2 = reinplace(make_fx(f)(inpt), inpt)
@@ -24,10 +22,8 @@ class TestReinplacePass(TestCase):
 
 def forward(self, x_1):
     clone_default = torch.ops.aten.clone.default(x_1);  x_1 = None
-    view_default = torch.ops.aten.view.default(clone_default, [-1]);  clone_default = None
-    add_tensor = torch.ops.aten.add_.Tensor(view_default, 1)
-    view_default_1 = torch.ops.aten.view.default(view_default, [2]);  view_default = None
-    return view_default_1
+    add_tensor = torch.ops.aten.add_.Tensor(clone_default, 1)
+    return clone_default
     """)
 
     def test_reinplace_mutation_on_input(self):
@@ -51,12 +47,37 @@ def forward(self, x_1):
     return add_tensor
     """)
 
+    def test_reinplace_with_view(self):
+        def f(x):
+            a = x.clone()
+            a_view = a.view(-1)
+            # We shouldn't re-inplace the first add(), because an alias of a is re-used later in the program
+            b = a.add(1)
+            # Second add() is fine to re-inplace
+            c = a_view.add(1)
+            return c
+
+        inpt = torch.ones(2, device='meta')
+        f2 = reinplace(make_fx(f)(inpt), inpt)
+        self.assertExpectedInline(f2.code, """\
+
+
+
+def forward(self, x_1):
+    clone_default = torch.ops.aten.clone.default(x_1);  x_1 = None
+    view_default = torch.ops.aten.view.default(clone_default, [-1])
+    add_tensor = torch.ops.aten.add.Tensor(clone_default, 1);  clone_default = None
+    add_tensor_1 = torch.ops.aten.add_.Tensor(view_default, 1)
+    return view_default
+    """)
+
     def test_reinplace_mutation_on_input_alias(self):
         def f(x):
-            # The first add() should be reinplaced, but not the second.
-            # The first add() is performed on an alias of the input.
             a = x.view(-1)
+            # We can't reinplace the first add, since it was run on an alias
+            # of an input.
             b = a.add(1)
+            # Second add() is fine to re-inplace though.
             c = b.add(1)
             return c
 
@@ -71,6 +92,36 @@ def forward(self, x_1):
     add_tensor = torch.ops.aten.add.Tensor(view_default, 1);  view_default = None
     add_tensor_1 = torch.ops.aten.add_.Tensor(add_tensor, 1)
     return add_tensor
+    """)
+
+
+    def test_reinplace_mutation_on_input_alias_with_overwritten_data(self):
+        def f(x, y):
+            # The first add() is safe to reinplace even though x is an input, because x is overwritten later.
+            a = x.add(1)
+            # The second add() is NOT safe to reinplace, because a aliases a_view,
+            # and a is used as an input to the copy_() later.
+            a_view = a.view(-1)
+            b = a_view.add(1)
+            x.copy_(a)
+            # The second add() is NOT safe to reinplace, because y is an input that is not overwritten later.
+            c = y.add(1)
+            return c
+
+        x = torch.ones(2, device='meta')
+        y = torch.ones(2, device='meta')
+        f2 = reinplace(make_fx(f)(x, y), x, y)
+        self.assertExpectedInline(f2.code, """\
+
+
+
+def forward(self, x_1, y_1):
+    add_tensor = torch.ops.aten.add_.Tensor(x_1, 1)
+    view_default = torch.ops.aten.view.default(x_1, [-1])
+    add_tensor_1 = torch.ops.aten.add.Tensor(view_default, 1);  view_default = None
+    copy__default = torch.ops.aten.copy_.default(x_1, x_1);  x_1 = None
+    add_tensor_2 = torch.ops.aten.add.Tensor(y_1, 1);  y_1 = None
+    return add_tensor_2
     """)
 
 if __name__ == '__main__':
--- torch/fx/passes/reinplace.py
+++ torch/fx/passes/reinplace.py
@@ -8,6 +8,12 @@ import itertools
 
 __all__ = ['reinplace']
 
+def concatMap(func, xs):
+    for x in xs:
+        for r in func(x):
+            yield r
+
+
 # Another option would be to expose `Tensor::is_alias_of` to python.
 # This wouldn't be valid for non-standard backends like opaque tensor impls,
 # but works just fine for meta tensors.
@@ -32,8 +38,15 @@ class ComputeInputAliasNames:
     executes, we use the runtime aliasing information
     to determine which node inputs in the graph
     correspond to aliases of the input arguments.
-    It returns a set of all graph inputs and intermediate
-    variable names that alias with the inputs.
+
+    It returns a list[set[str]] "input_aliases",
+    where input_aliases[i] is a list of names of all arguments
+    in the GraphModule that alias with the i'th input to the program.
+
+    It also returns a dict[str, set[str]], "alias_map"
+    containing alias information of all variables in the graph.
+    For a given variable name "x", alias_map[x] gives you a set
+    of all variables in the graph that alias with x (including x itself).
     """
     def __init__(self, mod):
         self.mod = mod
@@ -43,8 +56,10 @@ class ComputeInputAliasNames:
     def __call__(self, *args):
         args_iter = iter(args)
         env : Dict[str, Node] = {}
-        tensor_inputs = [a for a in args if isinstance(a, torch.Tensor)]
-        input_aliases = set()
+        # For every argument, keep track of a list of all aliases of that argument.
+        input_aliases = [set() for _ in range(len(args))]
+        tensor_to_node_name_map = {}
+        base_tensor_to_aliases_map = defaultdict(set)
 
         def load_arg(a):
             return torch.fx.graph.map_arg(a, lambda n: env[n.name])
@@ -72,16 +87,29 @@ class ComputeInputAliasNames:
             elif node.op == 'call_module':
                 result = self.modules[node.target](*load_arg(node.args), **load_arg(node.kwargs))
 
-            # This is the only code specific to shape propagation.
-            # you can delete this `if` branch and this becomes
-            # a generic GraphModule interpreter.
+            # Maintain mapping from each tensor in the program to its name in the graph
+            tensor_to_node_name_map[result] = node.name
+
             for a in itertools.chain(node.args, node.kwargs.values()):
                 arg = load_arg(a)
-                if isinstance(arg, torch.Tensor) and any(is_view_of(arg, tensor_input) for tensor_input in tensor_inputs):
-                    input_aliases.add(a.name)
+                if isinstance(arg, torch.Tensor):
+                    for i, inpt in enumerate(args):
+                        if isinstance(inpt, torch.Tensor) and is_view_of(arg, inpt):
+                            input_aliases[i].add(a.name)
+
+                    # Keep track of, for every "base" tensor in the program, what are all of its aliases.
+                    curr_base_tensor_name = a.name if arg._base is None else tensor_to_node_name_map[arg._base]
+                    base_tensor_to_aliases_map[curr_base_tensor_name].add(a.name)
+
             env[node.name] = result
 
-        return input_aliases
+        # Post-process our base-tensor-to-aliases info into a more convenient lookup table
+        alias_map = defaultdict(set)
+        for base_tensor_name, alias_names in base_tensor_to_aliases_map.items():
+            for name in [base_tensor_name] + list(alias_names):
+                alias_map[name] = alias_names
+
+        return input_aliases, alias_map
 
 
 def schemas_match(functional_schema, inplace_schema):
@@ -95,13 +123,37 @@ def schemas_match(functional_schema, inplace_schema):
     return names_match and arg_types_match
 
 @compatibility(is_backward_compatible=True)
-def reinplace(gm: fx.GraphModule, *sample_args):
+def reinplace(gm: fx.GraphModule, *sample_args, **sample_kwargs):
+    """
+    Given an fx.GraphModule, modifies it to perform "reinplacing".
+    We look for out-of-place op call sites like `b = a.add(...)`,
+    and convert them to be inplace (`b = a.add_(...)`),
+    as long as the input to the current operator ("a") isn't re-used
+    anywhere later in the graph.
+
+    Sample inputs are needed to determine aliasing relationships of the inputs.
+    In general, we can't reinplace node `b = a.add(...)` if "a" aliases any of the
+    inputs to the program.
+
+    There is one exception though: if "a" is copied to at any point later in the program,
+    e.g. `a.copy_(...)`, then we are free to re-use a's buffer any time
+    before that node executes.
+
+    This is an important optimization to include, to ensure that after running
+    functionalization and then reinplacing, we don't regress the net memory usage
+    of the original model.
+    """
 
     # Maintain a mapping: for every variable in the graph,
-    # what are all of nodes in the graph that use it as an input?
+    # what are all of nodes in the graph that use it as an input, where the data is read from?
     variable_to_nodes = defaultdict(list)
     for idx, node in enumerate(gm.graph.nodes):
-        for arg in itertools.chain(node.args, node.kwargs.values()):
+        node_args = node.args
+        # copy_() doesn't read from its first argument; it writes to it, overwriting previous data.
+        # We don't want to treat it as "being used as an input".
+        if node.target is torch.ops.aten.copy_.default:
+            node_args = node_args[1:]
+        for arg in itertools.chain(node_args, node.kwargs.values()):
             if not isinstance(arg, Iterable):
                 arg = [arg]
             for a in arg:
@@ -112,8 +164,35 @@ def reinplace(gm: fx.GraphModule, *sample_args):
     # If the first argument to the an out-of-place op is a graph input,
     # we can't convert it to an inplace op.
     sample_args = [a.to(device='meta') if isinstance(a, torch.Tensor) else a for a in sample_args]
-    graph_input_alias_names = ComputeInputAliasNames(gm)(*sample_args)
+    # E.g. for a program with two tensor inputs x and y, graph_input_alias_names will look like:
+    # [
+    #   ['x', 'x_alias_1', ...]
+    #   ['y', 'y_alias_1', ...]
+    # ]
+    graph_input_alias_names, graph_alias_map = ComputeInputAliasNames(gm)(*sample_args, **sample_kwargs)
 
+    # In general, we can't inplace-ify ops that use program inputs (or their aliases) as arguments.
+    # However, if we know that later in the program the input will be overwritten anyway,
+    # then inplace-ifying is safe to do as long as it happens before the input is overwritten.
+    #
+    # This optimization is especially important to do because of functionalization:
+    # functionalization will remove input mutations from a program, but append a copy_()
+    # to the end of the program.
+    #
+    # Here, we're keeping a mapping of, for every input arg, the last place
+    # in the program (if any) that its data was overwritten using a different tensor's data..
+    # Mapping is from (input arg idx) -> (idx of the node that overwrites the data for that input)
+    last_node_where_input_alias_is_overwritten: Dict[int, int] = defaultdict(lambda: -1)
+    for node_idx, node in enumerate(gm.graph.nodes):
+        if node.op == 'call_function' and node.target is torch.ops.aten.copy_.default:
+            copied_to_arg_name = node.args[0].name
+            copied_from_arg_name = node.args[1].name
+            for input_idx, input_alias_names in enumerate(graph_input_alias_names):
+                # Check that the program input is written to, but not read from here.
+                if copied_to_arg_name in input_alias_names and copied_from_arg_name not in input_alias_names:
+                    last_node_where_input_alias_is_overwritten[input_idx] = node_idx
+
+    # Now do the real work: inplace-ify functional ops, subject to the constraints written below.
     for idx, node in enumerate(gm.graph.nodes):
         if node.op == 'call_function':
             # __module__ seems broken; it returns torch._ops.aten which doesn't exist
@@ -139,17 +218,32 @@ def reinplace(gm: fx.GraphModule, *sample_args):
                 inplace_op = inplace_overloads_with_matching_schemas[0]
 
                 # This is a proxy check for ensuring that the first argument is "tensor-like"
-                # (should be the case for all ops with inplace variants).
+                # (This should be the case for all ops with inplace variants in ATen,
+                # although we technically don't have guarantees for custom ops).
                 assert len(node.target._schema.arguments) > 0
                 assert 'Tensor' in str(node.target._schema.arguments[0].type)
 
                 # Step 2: Check to see if the input to the op is re-used later in the graph.
                 self_arg = node.args[0]
                 self_arg_name = self_arg.name
-                # We also can't re-inplace an op if input was an (alias to an) input to the graph.
-                if self_arg_name in graph_input_alias_names:
-                    continue
-                nodes_used = variable_to_nodes[self_arg_name]
+
+                input_aliases_of_current_tensor = [
+                    input_idx for (input_idx, alias_names) in enumerate(graph_input_alias_names)
+                    if self_arg_name in alias_names
+                ]
+                # In general, we can't re-inplace an op if input was
+                # an (alias to an) input to the graph.
+                if len(input_aliases_of_current_tensor) > 0:
+                    # However, if that input is overwritten by a copy_() later in the graph, then
+                    # it's fair game to re-inplace.
+                    if not any(last_node_where_input_alias_is_overwritten[inpt_alias_idx] > idx for inpt_alias_idx in input_aliases_of_current_tensor):
+                        continue
+
+
+                # For the current self_tensor, we want to know:
+                # What are all of the nodes in the graph that use self_tensor, or any of its aliases, as an input?
+                self_arg_aliases = graph_alias_map[self_arg_name]
+                nodes_used = concatMap(lambda alias_name: variable_to_nodes[alias_name], self_arg_aliases)
                 # for every node that uses the current variable, we've tracked what their index is
                 # in the graph. If there aren't any nodes with a higher index that use the current
                 # argument, that means it isn't re-used anywhere.
@@ -160,22 +254,50 @@ def reinplace(gm: fx.GraphModule, *sample_args):
                     # Step 4:
                     # Now that we've replaced b = a.foo() with a.foo_(),
                     # We need to replace any later usages of "b" with "a"
-                    for _, node_to_update in variable_to_nodes[node.name]:
+                    #
+                    # A lot of the extra below is because we have several data structures
+                    # That we need to update when we change the nodes' arguments:
+                    # - variable_to_nodes (some nodes now take in different variables as arguments)
+                    # - graph_alias_map (updating a view nodes' arguments will change with variables alias)
+                    for node_to_update_idx, node_to_update in variable_to_nodes[node.name]:
                         new_args = []
-                        for a in node_to_update.args:
+                        for arg_idx, a in enumerate(node_to_update.args):
                             if isinstance(a, fx.node.Node) and a.name == node.name:
                                 new_args.append(self_arg)
+                                if node_to_update.op == 'call_function':
+                                    schema_arg = node_to_update.target._schema.arguments[arg_idx]
+                                    # And if this argument is aliasing according to the schema,
+                                    # then we'll need to update our graph alias map.
+                                    if schema_arg.alias_info is not None and not schema_arg.alias_info.is_write:
+                                        graph_alias_map[node.name].remove(node_to_update.name)
+                                        graph_alias_map[self_arg_name].add(node_to_update.name)
+                                        graph_alias_map[node_to_update.name] = graph_alias_map[self_arg_name]
                             else:
                                 new_args.append(a)
                         new_kwargs = {}
-                        for k, v in node_to_update.kwargs:
+                        for kwarg_idx, (k, v) in enumerate(node_to_update.kwargs):
                             if isinstance(v, fx.node.Node) and v.name == node.name:
                                 new_kwargs[k] = self_arg
+                                if node_to_update.op == 'call_function':
+                                    schema_arg = node_to_update.target._schema.arguments[len(node_to_update.args) + kwarg_idx]
+                                    # And if this argument is aliasing according to the schema,
+                                    # then we'll need to update our graph alias map.
+                                    if schema_arg.alias_info is not None and not schema_arg.alias_info.is_write:
+                                        graph_alias_map[node.name].remove(node_to_update.name)
+                                        graph_alias_map[self_arg_name].add(node_to_update.name)
+                                        graph_alias_map[node_to_update.name] = graph_alias_map[self_arg_name]
                             else:
                                 new_kwargs[k] = v
-
                         node_to_update.args = new_args
                         node_to_update.kwargs = new_kwargs
+
+                    # We also need to update our variable-to-node mapping,
+                    # now that we changed some of our nodes' inputs.
+
+                    # {self_arg} now gets some extra node entries
+                    variable_to_nodes[self_arg_name] += [x for x in variable_to_nodes[node.name] if x[0] > idx]
+                    # And {node.name} loses some entries (since we replaced {node.name} with {self_arg}
+                    variable_to_nodes[node.name] = [x for x in variable_to_nodes[node.name] if x[0] <= idx]
 
     gm.recompile()
     return gm
