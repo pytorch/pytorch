1dfd9131cc0709eec9bea88d31f03ee9512a3077
Wonjoo Lee <wonjoo@google.com>
2022-07-07 01:47:06 +0000

    Add const modifier for lazy shape inference for boolean logic ops (#79999)
    Follow up to https://github.com/pytorch/pytorch/pull/78004.
    
    ---
    
    Trying to fully codegen these boolean logic ops on pt/xla's side, saw errors:
    ```
    Exception: Missing shape inference function.
    
    Please add declare this function in /workspace/pytorch/torch/csrc/lazy/core/shape_inference.h:
    
    and implement it in the the corresponding shape_inference.cpp file.
    
    TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_and(const at::Tensor & self, const at::Tensor & other);
    TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_not(const at::Tensor & self);
    TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_or(const at::Tensor & self, const at::Tensor & other);
    TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_xor(const at::Tensor & self, const at::Tensor & other);
    Failed to generate lazy files: ['python', '/workspace/pytorch/xla/scripts/gen_lazy_tensor.py']
    ```
    
    Add const modifier for lazy shape inference for boolean logic ops
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79999
    Approved by: https://github.com/wconstab

--- torch/csrc/lazy/core/shape_inference.h
+++ torch/csrc/lazy/core/shape_inference.h
@@ -47,10 +47,10 @@ TORCH_API std::vector<torch::lazy::Shape> compute_shape_kl_div_backward(const at
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_log_sigmoid_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & buffer);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_log_sigmoid_forward(const at::Tensor & self);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_logdet(const at::Tensor & self);
-TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_and(at::Tensor & self, const at::Tensor & other);
-TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_not(at::Tensor & self);
-TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_or(at::Tensor & self, const at::Tensor & other);
-TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_xor(at::Tensor & self, const at::Tensor & other);
+TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_and(const at::Tensor & self, const at::Tensor & other);
+TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_not(const at::Tensor & self);
+TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_or(const at::Tensor & self, const at::Tensor & other);
+TORCH_API std::vector<torch::lazy::Shape> compute_shape_logical_xor(const at::Tensor & self, const at::Tensor & other);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_masked_fill(const at::Tensor & self, const at::Tensor & mask, const at::Scalar & value);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_masked_fill(const at::Tensor & self, const at::Tensor & mask, const at::Tensor & value);
 TORCH_API std::vector<torch::lazy::Shape> compute_shape_max(const at::Tensor & self);
