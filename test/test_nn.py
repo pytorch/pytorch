2551fea3a9c50cde037d3edcf5ec3b2e51579c78
eqy <eddiey@nvidia.com>
2022-07-07 16:53:53 -0700

    Update test_nn.py

--- test/test_nn.py
+++ test/test_nn.py
@@ -15929,14 +15929,15 @@ torch.cuda.synchronize()
             if not n:
                 input = input.squeeze(0).detach().clone().requires_grad_()
             if isinstance(kernel_size, int):
-                kernel_size = [kernel_size]*3
+                kernel_size = [kernel_size] * 3
             if stride is None:
                 stride = kernel_size
             elif isinstance(stride, int):
-                stride = [stride]*3
-            grad = torch.randn(batch, c, (d - kernel_size[0]) // stride[0] + 1,
-                                     (h - kernel_size[1]) // stride[1] + 1,
-                                     (w - kernel_size[2]) // stride[2] + 1,
+                stride = [stride] * 3
+            grad = torch.randn(batch, c,
+                               (d - kernel_size[0]) // stride[0] + 1,
+                               (h - kernel_size[1]) // stride[1] + 1,
+                               (w - kernel_size[2]) // stride[2] + 1,
                                dtype=dtype, device=device)
             grad = grad.contiguous(memory_format=torch.channels_last_3d)
             if not n:
