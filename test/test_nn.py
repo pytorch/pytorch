05793e852db2d69c809fb6c42f90a285b68c37a0
Eddie Yan <eddiey@nvidia.com>
2022-07-07 23:07:06 +0000

    new tests

--- test/test_nn.py
+++ test/test_nn.py
@@ -15921,15 +15921,26 @@ torch.cuda.synchronize()
     @onlyCUDA
     def test_max_pool3d_ndhwc(self, device, dtype):
         def helper(n, c, h, w, d, kernel_size, stride=None):
+            batch = n
+            if not batch:
+                batch = 1
+            input = torch.randn(batch, c, d, h, w, dtype=dtype, device=device)
+            input = input.contiguous(memory_format=torch.channels_last_3d).requires_grad_()
+            if not n:
+                input = input.squeeze(0).detach().clone().requires_grad_()
+            if isinstance(kernel_size, int):
+                kernel_size = [kernel_size]*3
             if stride is None:
                 stride = kernel_size
-            input = torch.randn(n, c, d, h, w, dtype=dtype, device=device)
-            input = input.contiguous(memory_format=torch.channels_last_3d).requires_grad_()
-            grad = torch.randn(n, c, (d - kernel_size) // stride + 1,
-                                     (h - kernel_size) // stride + 1,
-                                     (w - kernel_size) // stride + 1,
+            elif isinstance(stride, int):
+                stride = [stride]*3
+            grad = torch.randn(batch, c, (d - kernel_size[0]) // stride[0] + 1,
+                                     (h - kernel_size[1]) // stride[1] + 1,
+                                     (w - kernel_size[2]) // stride[2] + 1,
                                dtype=dtype, device=device)
             grad = grad.contiguous(memory_format=torch.channels_last_3d)
+            if not n:
+                grad = grad.squeeze(0)
             pool = torch.nn.MaxPool3d(kernel_size, stride, return_indices=True).to(device)
 
             ref_input = input.detach().clone().contiguous().requires_grad_(True)
@@ -15940,9 +15951,15 @@ torch.cuda.synchronize()
             ref_out, ref_ind = ref_pool(ref_input)
             ref_out.backward(ref_grad)
 
-            self.assertTrue(out.is_contiguous(memory_format=torch.channels_last_3d))
+            if len(out.shape) == 4:
+                self.assertTrue(out.unsqueeze(0).is_contiguous(memory_format=torch.channels_last_3d))
+            else:
+                self.assertTrue(out.is_contiguous(memory_format=torch.channels_last_3d))
             self.assertTrue(ref_out.is_contiguous())
-            self.assertTrue(ind.is_contiguous(memory_format=torch.channels_last_3d))
+            if len(ind.shape) == 4:
+                self.assertTrue(ind.unsqueeze(0).is_contiguous(memory_format=torch.channels_last_3d))
+            else:
+                self.assertTrue(ind.is_contiguous(memory_format=torch.channels_last_3d))
             self.assertTrue(ref_ind.is_contiguous())
             self.assertEqual(out, ref_out)
             self.assertEqual(ind, ref_ind)
@@ -15952,9 +15969,17 @@ torch.cuda.synchronize()
                 self.assertEqual(input.grad, ref_input.grad)
 
         helper(4, 8, 8, 8, 8, 7)
+        helper(4, 8, 8, 8, 8, (5, 6, 7))
+        helper(1, 8, 8, 8, 8, (5, 6, 7))
+        helper(0, 6, 12, 13, 14, (5, 6, 7))
         helper(4, 8, 7, 7, 7, 3, stride=1)
         helper(10, 128, 19, 19, 19, 3, stride=2)
+        helper(10, 128, 19, 19, 19, (1, 2, 3), stride=2)
+        helper(1, 128, 19, 19, 19, (1, 2, 3), stride=2)
+        helper(0, 128, 19, 19, 19, (1, 2, 3), stride=2)
         helper(1, 79, 4, 4, 4, 3, stride=2)
+        helper(0, 79, 4, 4, 4, 3, stride=2)
+
 
     @onlyCPU
     def test_max_pool2d_bfloat16(self, device):
