e9a9b50f48841adba6450301e28bd43e9ac8ca87
lezcano <lezcano-93@hotmail.com>
2022-07-09 15:52:55 +0000

    Reference for linalg.vector_norm (#78350)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78350
    Approved by: https://github.com/mruberry

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -1352,7 +1352,7 @@ def sub(
     alpha: Optional[NumberType] = None,
 ):
     """
-    Reference implementation of torch.add
+    Reference implementation of torch.sub
     """
 
     if isinstance(a, Number) and isinstance(b, Number):
