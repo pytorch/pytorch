db14eb039dfa9981bc9852cc9d4dc8f341ac1cde
unknown <{sanathan6522@gmail.com}>
2022-07-08 17:59:43 -0800

    Grammatically updated the tech docs

--- torch/__init__.py
+++ torch/__init__.py
@@ -1,7 +1,8 @@
 
 r"""
-The torch package contains data structures for multi-dimensional
-tensors and defines mathematical operations over these tensors.
+The torch package contains data structures for multi-dimensional tensors. 
+It also  defines mathematical operations over these tensors.
+
 Additionally, it provides many utilities for efficient serializing of
 Tensors and arbitrary types, and other useful utilities.
 
