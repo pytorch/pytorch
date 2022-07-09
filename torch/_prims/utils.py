29382c35609feb5ad5381eac3b62d2db62916d53
lezcano <lezcano-93@hotmail.com>
2022-07-09 15:52:55 +0000

    Fix handling of device (#78615)
    Removes an unnecessary auxiliary function (we had already implemented
    it), uses DeviceLikeType to denote str or dtype, and adds `is_cpu` and
    `is_cuda` helper functions
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78615
    Approved by: https://github.com/mruberry

--- torch/_prims/utils.py
+++ torch/_prims/utils.py
@@ -477,7 +477,7 @@ def check_same_device(*args, allow_cpu_scalar_tensors):
             raise RuntimeError(msg)
 
 
-def canonicalize_device(device: Union[str, torch.device]) -> torch.device:
+def canonicalize_device(device: DeviceLikeType) -> torch.device:
     if isinstance(device, torch.device):
         return device
 
@@ -1097,20 +1097,6 @@ def reduction_dtypes(
     else:  # ALWAYS_BOOL
         result_dtype = torch.bool
     return computation_dtype, result_dtype
-
-
-def wrap_device(d: Union[str, torch.device]) -> torch.device:
-    """
-    Wraps strings into torch.device objects.
-
-    Given torch.device objects are returned unmodified.
-    """
-
-    assert isinstance(d, (str, torch.device))
-    if isinstance(d, str):
-        return torch.device(d)
-
-    return d
 
 
 def make_contiguous_strides_for(shape: ShapeType) -> Tuple[int, ...]:
