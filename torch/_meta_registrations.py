9d1783c2fbd9f3a15fcc3575a84025df9ceb2a99
Robert <xiurobert@gmail.com>
2022-07-10 15:05:34 +0800

    Fix breaking every other op
    Only raise RuntimeError in correct situations

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -305,9 +305,11 @@ def meta_conv(
         )
 
     else:
-        if weight.shape[1] != input_tensor.shape[1]:
-            raise RuntimeError("Invalid channel dimensions")
         out_channels = weight.shape[0]
+        if weight.shape[1] != input_tensor.shape[1] / groups:
+            raise RuntimeError(
+                "Invalid channel dimensions"
+            )
         shape_out = calc_conv_nd_return_shape(
             dims, kernel_size, stride, padding, dilation
         )
