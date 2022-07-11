1d14eb34e6ece58807c28b8e349ac63342428f0f
macandro96 <anishm@fb.com>
2022-07-11 17:51:47 +0000

    Update base for Update on "[ao][sparsity] Implementation of register_layer() and get_mask()"
    The register_layer() attaches a pre-forward hook to the layer to aggregate
    activations over time. The mask shape is also inferred here.
    
    The get_mask() returns the computed mask associated to the attached layer.
    The mask is
        - a torch tensor is features for that layer is None.
        - a list of torch tensors for each feature, otherwise
    
    Test Plan:
    ```python test/test_ao_sparsity.py TestActivationSparsifier```
    
    [ghstack-poisoned]

--- test/ao/sparsity/test_activation_sparsifier.py
+++ test/ao/sparsity/test_activation_sparsifier.py
@@ -33,37 +33,10 @@ class Model(nn.Module):
         return out
 
 
-class _ActivationSparsifierUtils:
-    """
-    Contains utility functions to build and test an activation sparsifier
-    """
-    @staticmethod
-    def _get_defaults():
-        """Returns the default config and sparse config dicts
-        that will be passed while creating the sparsifier object
-        """
-        sparse_config = {
-            'sparsity_level': 0.5
-        }
-
-        def agg_fn(x, y):
-            return x + y
-
-        def reduce_fn(x):
-            return torch.mean(x, dim=0)
-
-        return {
-            'aggregate_fn': agg_fn,
-            'reduce_fn': reduce_fn
-        }, sparse_config
-
-
-class _ActivationSparsiferTestCase(TestCase):
-    """Runner class for unit tests that comprises functions to test each functionality
-    """
-    def check_constructor(self, activation_sparsifier, model, defaults, sparse_config):
-        """Check if the model, defaults and sparse_config are loaded correctly in the activation
-        sparsifier
+class TestActivationSparsifier(TestCase):
+    def _check_constructor(self, activation_sparsifier, model, defaults, sparse_config):
+        """Helper function to check if the model, defaults and sparse_config are loaded correctly
+        in the activation sparsifier
         """
         sparsifier_defaults = activation_sparsifier.defaults
         combined_defaults = {**defaults, 'sparse_config': sparse_config}
@@ -78,19 +51,34 @@ class _ActivationSparsiferTestCase(TestCase):
             # all the keys in combined_defaults should be present in sparsifier defaults
             assert config == combined_defaults.get(key, None)
 
-
-class TestActivationSparsifier(_ActivationSparsiferTestCase):
     def test_activation_sparsifier(self):
         """Simulates the workflow of the activation sparsifier, starting from object creation
         till squash_mask().
         The idea is to check that everything works as expected while in the workflow.
         """
-        # simulate the workflow
+        # defining aggregate, reduce functions
+        def agg_fn(x, y):
+            return x + y
 
+        def reduce_fn(x):
+            return torch.mean(x, dim=0)
+
+        # Creating default function and sparse configs
+        # default sparse_config
+        sparse_config = {
+            'sparsity_level': 0.5
+        }
+
+        defaults = {
+            'aggregate_fn': agg_fn,
+            'reduce_fn': reduce_fn
+        }
+
+        # simulate the workflow
         # STEP 1: make data and activation sparsifier object
         defaults, sparse_config = _ActivationSparsifierUtils._get_defaults()
         model = Model()  # create model
         activation_sparsifier = ActivationSparsifier(model, **defaults, **sparse_config)
 
         # Test Constructor
-        self.check_constructor(activation_sparsifier, model, defaults, sparse_config)
+        self._check_constructor(activation_sparsifier, model, defaults, sparse_config)
