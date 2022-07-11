1f6a2501bba9862b04b3a07646a51290cd1a491b
Eric Han <erichan1@fb.com>
2022-07-11 21:31:22 +0000

    lint fixes

--- torch/nn/modules/transformer.py
+++ torch/nn/modules/transformer.py
@@ -413,8 +413,8 @@ class TransformerEncoderLayer(Module):
             self.self_attn.batch_first and
             self.self_attn._qkv_same_embed_dim and self.activation_relu_or_gelu and
             self.norm1.eps == self.norm2.eps and
-            src_mask is None and 
-            not (src.is_nested and src_key_padding_mask is not None):
+            src_mask is None and
+            not (src.is_nested and src_key_padding_mask is not None)):
             tensor_args = (
                 src,
                 self.self_attn.in_proj_weight,
