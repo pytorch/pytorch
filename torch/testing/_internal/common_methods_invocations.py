0d8e398f5ef07ae253e18908dbc691ea881eafc7
Fabio Rocha <frocha@quansight.com>
2022-07-11 12:59:49 +0000

    Added OpInfo for unflatten and ref for flatten OpInfo
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -1,6 +1,7 @@
 from functools import wraps, partial
 from itertools import product, chain, islice
 import itertools
+import functools
 import collections
 import copy
 from enum import Enum
@@ -8738,6 +8739,20 @@ def reference_inputs_flatten(op, device, dtype, requires_grad, **kwargs):
         yield SampleInput(make_arg(shape, noncontiguous=True).transpose(0, -1), args=(start, end,))
         yield SampleInput(make_arg(shape).transpose(0, -1), args=(start, end,))
 
+def sample_inputs_unflatten(op_info, device, dtype, requires_grad, **kwargs):
+    # in_shape, dim, sizes
+    args = (((8,), 0, (8,)),
+            ((8,), 0, (4, 2)),
+            ((8,), -1, (2, 2, 2)),
+            ((3, 6, 2), 1, (2, 3)),
+            ((3, 6, 2), -2, (2, 3)),
+            ((3, 2, 12), 2, (3, 2, 2))
+            )
+    make_tensor_partial = partial(make_tensor, dtype=dtype, device=device, requires_grad=requires_grad)
+    for in_shape, dim, sizes in args:
+        yield SampleInput(make_tensor_partial(in_shape), args=(dim, sizes))
+
+
 def sample_inputs_select(op_info, device, dtype, requires_grad, **kwargs):
     make_arg = partial(make_tensor, dtype=dtype, device=device, requires_grad=requires_grad)
 
@@ -10469,6 +10484,18 @@ def error_inputs_mean(op_info, device, **kwargs):
     return (ErrorInput(si1, error_regex=err_msg1),
             ErrorInput(si2, error_regex=err_msg2),
             ErrorInput(si3, error_regex=err_msg3))
+
+def reference_flatten(input, start_dim=0, end_dim=-1):
+    in_shape = input.shape
+    in_dim = len(in_shape)
+    end_dim = end_dim if end_dim >= 0 else in_dim + end_dim
+    if in_dim == 0:
+        end_dim = start_dim
+    if end_dim < start_dim:
+        raise RuntimeError
+    flatten_bit_dim = functools.reduce(operator.mul, in_shape[start_dim:end_dim + 1], 1)
+    out_shape = in_shape[:start_dim] + (flatten_bit_dim,) + in_shape[end_dim + 1:]
+    return np.reshape(input, out_shape)
 
 # Operator database (sorted alphabetically)
 op_db: List[OpInfo] = [
@@ -16583,6 +16610,7 @@ op_db: List[OpInfo] = [
            ),
     OpInfo('flatten',
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
+           ref=reference_flatten,
            supports_out=False,
            supports_forward_ad=True,
            supports_fwgrad_bwgrad=True,
@@ -16591,6 +16619,17 @@ op_db: List[OpInfo] = [
            sample_inputs_func=sample_inputs_flatten,
            reference_inputs_func=reference_inputs_flatten,
            ),
+    OpInfo('unflatten',
+           op=torch.Tensor.unflatten,
+           dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
+           supports_out=False,
+           supports_forward_ad=True,
+           supports_fwgrad_bwgrad=True,
+           sample_inputs_func=sample_inputs_unflatten,
+           skips=(
+               # RuntimeError: object has no attribute unflatten
+               DecorateInfo(unittest.expectedFailure, 'TestJit', 'test_variant_consistency_jit'),
+           )),
     OpInfo('column_stack',
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
            supports_forward_ad=True,
