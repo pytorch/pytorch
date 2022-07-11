b7770db553dd40269d3cd6a09479790805d67072
macandro96 <anishm@fb.com>
2022-07-11 17:51:47 +0000

    Update on "[ao][sparsity] Basic implementation of activation sparsifier"
    The Activation sparsifier class aims to sparsify/prune activations in a neural
    network. The idea is to attach the sparsifier to a layer (or layers) and it
    zeroes out the activations based on the mask_fn (or sparsification function)
    input by the user.
    The mask_fn is applied once all the inputs are aggregated and reduced i.e.
    mask = mask_fn(reduce_fn(aggregate_fn(activations)))
    
    Note::
        The sparsification mask is computed on the input **before it goes through the attached layer**.
    
    Test Plan:
    ```python test/test_ao_sparsity.py TestActivationSparsifier```
    
    [ghstack-poisoned]

--- torch/ao/sparsity/experimental/activation_sparsifier/activation_sparsifier.py
+++ torch/ao/sparsity/experimental/activation_sparsifier/activation_sparsifier.py
@@ -18,62 +18,63 @@ class ActivationSparsifier:
         The sparsification mask is computed on the input **before it goes through the attached layer**.
 
     Args:
-        model (nn.Module)
+        model (nn.Module):
             The model whose layers will be sparsified. The layers that needs to be
             sparsified should be added separately using the register_layer() function
-
-        aggregate_fn (Optional, function type)
+        aggregate_fn (Optional, Callable):
             default aggregate_fn that is used if not specified while registering the layer.
-            specifies how inputs should be aggreagated over time.
-
-        reduce_fn (Optional, function type)
+            specifies how inputs should be aggregated over time.
+            The aggregate_fn should usually take 2 torch tensors and return the aggregated tensor.
+            Example
+                >>> def add_agg_fn(tensor1, tensor2):  return tensor1 + tensor2
+        reduce_fn (Optional, Callable):
             default reduce_fn that is used if not specified while registering the layer.
-            reduce_fn will be applied on the aggregated input
-
-        mask_fn (Optional, function type)
-            default mask_fn that is used to create the sparsification mask (based on the
-            aggregated & reduced input). This is used if not specified while registering the layer
-
-        features (Optional, list)
+            reduce_fn will be called on the aggregated tensor i.e. the tensor obtained after
+            calling agg_fn() on all inputs.
+            Example
+                >>> def mean_reduce_fn(agg_tensor):    return agg_tensor.mean(dim=0)
+        mask_fn (Optional, Callable):
+            default mask_fn that is used to create the sparsification mask using the tensor obtained after
+            calling the reduce_fn(). This is used by default if a custom one is passed in the
+            register_layer().
+            Note that the mask_fn() definition should contain the sparse arguments that is passed in sparse_config
+            arguments.
+        features (Optional, list):
             default selected features to sparsify.
             If this is non-empty, then the mask_fn will be applied for each feature of the input.
             For example,
-            >>> mask = [mask_fn(reduce_fn(aggregated_fn(input[feature])) for feature in features]
-
-        feature_dim (Optional, int)
+                >>> mask = [mask_fn(reduce_fn(aggregated_fn(input[feature])) for feature in features]
+        feature_dim (Optional, int):
             default dimension of input features. Again, features along this dim will be chosen
             for sparsification.
-
-        sparse_config (Dict)
+        sparse_config (Dict):
             Default configuration for the mask_fn. This config will be passed
             with the mask_fn()
 
-    Expected Usage:
+    Example:
         >>> model = SomeModel()
         >>> act_sparsifier = ActivationSparsifier(...)  # init activation sparsifier
-
         >>> # Initialize aggregate_fn
         >>> def agg_fn(x, y):
         >>>     return x + y
-
+        >>>
         >>> # Initialize reduce_fn
         >>> def reduce_fn(x):
         >>>     return torch.mean(x, dim=0)
-
+        >>>
         >>> # Initialize mask_fn
         >>> def mask_fn(data):
         >>>     return torch.eye(data.shape).to(data.device)
-
-
+        >>>
+        >>>
         >>> act_sparsifier.register_layer(model.some_layer, aggregate_fn=agg_fn, reduce_fn=reduce_fn, mask_fn=mask_fn)
-
+        >>>
         >>> # start training process
-            >>> # epoch starts
-                >>> # model.forward(), compute_loss() and model.backwards()
-            >>> # epoch ends
-            >>> act_sparsifier.step()
+        >>>     # epoch starts
+        >>>         # model.forward(), compute_loss() and model.backwards()
+        >>>     # epoch ends
+        >>>     act_sparsifier.step()
         >>> # end training process
-
         >>> sparsifier.squash_mask()
     """
     def __init__(self, model: nn.Module, aggregate_fn=None, reduce_fn=None, mask_fn=None,
