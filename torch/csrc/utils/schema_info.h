14c28caed9b3b8109afb4622c8772e489887429a
goldenxuett <ttanx@fb.com>
2022-07-12 13:35:51 -0700

    [JIT] Add determinism checks for ops in SchemaInfo subclass (#81000)
    - Added is_non_deterministic which returns whether a given op is non-deterministic. Currently this is implemented with a hard-coded list of non-deterministic functions copied from ir.cpp in AliasDB, but this will eventually be implemented by returning with a given schema has the non_deterministic tag.
    - Tested is_non_deterministic method with a deterministic op and a non deterministic op in test_schema_info.cpp
    
    **Note that the case for op "aten::dropout(Tensor input, float p, bool train) -> Tensor" which is deterministic whenever "train=false" is not accounted for in this pr and will be fixed in a later pr. Currently "aten::dropout(Tensor input, float p, bool train) -> Tensor" is always considered nondeterministic.**
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81000
    Approved by: https://github.com/davidberard98

--- torch/csrc/utils/schema_info.h
+++ torch/csrc/utils/schema_info.h
@@ -27,6 +27,8 @@ struct TORCH_API SchemaInfo {
 
   bool is_mutable(c10::string_view name);
 
+  bool is_nondeterministic() const;
+
   bool may_alias(
       const c10::SchemaArgument& lhs,
       const c10::SchemaArgument& rhs);
@@ -41,6 +43,8 @@ struct TORCH_API SchemaInfo {
 
  private:
   void generateAliasMaps();
+
+  static std::vector<c10::FunctionSchema> getNonDeterministicOps();
 
   // Map of argument IValues
   std::unordered_map<std::string, at::IValue> value_map_;
