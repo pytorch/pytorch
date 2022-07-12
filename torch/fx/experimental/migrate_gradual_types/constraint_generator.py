4744ac83dd32128ed726a7071a362bc92c5a1ab4
migeedz <migeedz@fb.com>
2022-07-12 11:57:31 -0700

    expose gradual type inferface to HFtracer and generate constraints for gt nodes (#80744)
    Define the function `transform_all_constraints_trace_time` which takes a node which represents a conditional, a graph and a root which represent the trace so far. We then generate constraints for the current trace, and a separate constraint for the conditional. We then combine the constraints with the node constraint in one set and we combine the constraint with the negation of the node's constraint in another set. We then transform both sets to Z3 format and we return both sets.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80744
    Approved by: https://github.com/jansel

--- torch/fx/experimental/migrate_gradual_types/constraint_generator.py
+++ torch/fx/experimental/migrate_gradual_types/constraint_generator.py
@@ -5,9 +5,11 @@ from typing import Callable, Dict
 from torch.fx._symbolic_trace import _assert_is_none
 from torch.fx.experimental.migrate_gradual_types.constraint import ApplyBroadcasting, CalcProduct, \
     Disj, TGreatestUpperBound, CalcMaxPool, CalcConv, Conj, BinConstraintT, CanReshape, BinConstraintD, GetItem
-from torch.fx.experimental.migrate_gradual_types.operation import op_eq, op_matching, op_consistency, op_leq, op_precision
+from torch.fx.experimental.migrate_gradual_types.operation import \
+    op_eq, op_matching, op_consistency, op_leq, op_precision, op_gt
 from torch.fx.node import Target, Node
-from torch.fx.experimental.migrate_gradual_types.util import gen_tensor_dims, gen_nat_constraints, gen_dvar, gen_tvar
+from torch.fx.experimental.migrate_gradual_types.util import gen_tensor_dims, gen_nat_constraints, gen_dvar, gen_tvar, \
+    gen_bvar
 
 from torch.fx.tensor_type import Dyn, TensorType
 from torch.nn.modules.conv import Conv2d
@@ -252,10 +254,23 @@ def mul_inference_rule(n: Node, symbols, constraints, counter):
     else:
         raise NotImplementedError('Case not yet implemented')
 
-# TODO
+
 @register_inference_rule(operator.gt)
 def gt_inference_rule(n: Node, symbols, constraints, counter):
-    raise NotImplementedError('Not yet implemented')
+    assert isinstance(n.args[0], Node) or isinstance(n.args[0], int)
+    assert isinstance(n.args[1], Node) or isinstance(n.args[1], int)
+
+    # We make sure this node will not be used again. We do not
+    # generate a constraint about that node. Only about the operands.
+    assert len(n.users) == 1
+
+    e1 = symbols[n.args[0]] if isinstance(n.args[0], Node) else n.args[0]
+    e2 = symbols[n.args[1]] if isinstance(n.args[1], Node) else n.args[1]
+    gt_constraint = BinConstraintD(e1, e2, op_gt)
+
+    my_gt, counter = gen_bvar(counter)
+    equality_constraint = BinConstraintD(my_gt, gt_constraint, op_eq)
+    return [equality_constraint], counter
 
 # TODO
 @register_inference_rule(operator.lt)
