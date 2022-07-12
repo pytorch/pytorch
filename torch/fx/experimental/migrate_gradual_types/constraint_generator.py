80bdf26cb476cdecd1e202401d7a4a713ce52c11
migeedz <migeedz@fb.com>
2022-07-12 11:53:11 -0700

    IndexSelect constraints, add a missing constraint in constraint tranformations and modify tests accordingly
    [ghstack-poisoned]

--- torch/fx/experimental/migrate_gradual_types/constraint_generator.py
+++ torch/fx/experimental/migrate_gradual_types/constraint_generator.py
@@ -5,7 +5,7 @@ from typing import Callable, Dict, Iterable
 from torch.fx._symbolic_trace import _assert_is_none
 from torch.fx.experimental.migrate_gradual_types.constraint import ApplyBroadcasting, CalcProduct, \
     Disj, TGreatestUpperBound, CalcMaxPool, CalcConv, Conj, BinConstraintT, CanReshape, BinConstraintD, GetItem, T, F, \
-    TVar, DVar, GetItemTensor
+    TVar, DVar, GetItemTensor, IndexSelect
 from torch.fx.experimental.migrate_gradual_types.operation import \
     op_eq, op_matching, op_consistency, op_leq, op_precision, op_gt, op_div, op_sub, op_neq, op_lt, op_add
 from torch.fx.node import Target, Node
@@ -57,6 +57,40 @@ def get_attr_inference_rule(n: Node, symbols, constraints, counter):
         return [BinConstraintT(input, output, op_eq)], counter
     else:
         raise NotImplementedError('Not yet implemented')
+
+
+@register_inference_rule("index_select")
+def index_select_inference_rule(n: Node, symbols, constraints, counter):
+    """
+    We constrain the second argument to a vector
+    The output replaces the input with the shape of the vector
+    at the position given by the index (first argument)
+    """
+    # print(n.args)
+    assert isinstance(n.args[0], Node)
+    assert isinstance(n.args[1], int)
+    assert isinstance(n.args[2], Node)
+
+
+
+    index_select, counter = gen_tvar(counter)
+    symbols[n] = index_select
+
+    dims, counter = gen_tensor_dims(1, counter)
+
+    # matching constraint
+    is_size_1 = BinConstraintT(symbols[n.args[2]], TensorType(dims), op_eq)
+    is_dyn = BinConstraintT(symbols[n.args[2]], Dyn, op_eq)
+
+    c2 = Conj([is_size_1, Disj([IndexSelect(i + 1, symbols[n.args[0]], dims[0], n.args[1], index_select)
+                                for i in range(MAX_TENSOR_RANK)])])
+    c3 = Conj([is_dyn, Disj([IndexSelect(i + 1, symbols[n.args[0]], Dyn, n.args[1], index_select)
+                             for i in range(MAX_TENSOR_RANK)])])
+
+    return [c3], counter
+
+    # return [Disj([c2, c3])], counter
+
 
 @register_inference_rule("expand")
 def expand_inference_rule(n: Node, symbols, constraints, counter):
