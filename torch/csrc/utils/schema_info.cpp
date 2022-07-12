790567684895df354ad9092646884a91d40ab8dd
goldenxuett <ttanx@fb.com>
2022-07-12 13:35:49 -0700

    [JIT] Add argumentValue functions and is_mutable checks to SchemaInfo (#80972)
    - Created addArgumentValue/s methods in SchemaInfo to pass argument values into the subclass. These are used for more accurate mutation, aliasing and determinism checks which include special cases.
    - Added input_alias_map_ to keep track of which inputs alias each other. This is updated with the method generateAliasMap.
    - Implemented is_mutable methods in SchemaInfo which also give information based on argument values. For instance, if two inputs alias and one is mutable by the schema, then the other will also be mutable.
    - Tested Schema Info is_mutable implementation where inputs alias as mentioned above.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80972
    Approved by: https://github.com/davidberard98

--- torch/csrc/utils/schema_info.cpp
+++ torch/csrc/utils/schema_info.cpp
@@ -1,5 +1,87 @@
 #include <torch/csrc/utils/schema_info.h>
 
 namespace torch {
-namespace utils {} // namespace utils
+namespace utils {
+void SchemaInfo::addArgumentValue(
+    const std::string& name,
+    const at::IValue& value) {
+  c10::optional<int> index = schema_.argumentIndexWithName(name);
+  TORCH_INTERNAL_ASSERT(
+      index != c10::nullopt, "Schema has no argument named ", name);
+  value_map_[name] = value;
+  alias_maps_current_ = false;
+}
+
+void SchemaInfo::addArgumentValues(
+    const std::vector<c10::optional<at::IValue>>& value_list) {
+  TORCH_INTERNAL_ASSERT(
+      value_list.size() <= schema_.arguments().size(),
+      "Schema does not have enough arguments for value list");
+
+  for (size_t i = 0; i < value_list.size(); i++) {
+    if (value_list[i] != c10::nullopt) {
+      value_map_[schema_.arguments()[i].name()] = *(value_list[i]);
+      alias_maps_current_ = false;
+    }
+  }
+}
+
+void SchemaInfo::addArgumentValues(
+    const std::unordered_map<std::string, at::IValue>& values) {
+  for (const auto& key_pair : values) {
+    addArgumentValue(key_pair.first, key_pair.second);
+  }
+}
+
+bool SchemaInfo::is_mutable() {
+  for (size_t i = 0; i < schema_.arguments().size(); i++) {
+    if (is_mutable(i)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool SchemaInfo::is_mutable(size_t index) {
+  TORCH_INTERNAL_ASSERT(
+      index < schema_.arguments().size(), "Invalid index for schema.");
+  if (!alias_maps_current_) {
+    generateAliasMaps();
+  }
+  return std::any_of(
+      input_alias_map_[index].begin(),
+      input_alias_map_[index].end(),
+      [this](size_t index) { return this->schema_.is_mutable(index); });
+}
+
+bool SchemaInfo::is_mutable(c10::string_view name) {
+  c10::optional<int> index = schema_.argumentIndexWithName(name);
+  TORCH_INTERNAL_ASSERT(
+      index != c10::nullopt, "Schema has no argument named ", name);
+
+  return is_mutable(*index);
+}
+
+void SchemaInfo::generateAliasMaps() {
+  alias_maps_current_ = true;
+  input_alias_map_ = std::vector<std::unordered_set<size_t>>(
+      schema_.arguments().size(), std::unordered_set<size_t>());
+  for (size_t i = 0; i < schema_.arguments().size(); i++) {
+    for (size_t j = i; j < schema_.arguments().size(); j++) {
+      if (i == j) {
+        input_alias_map_[i].insert(i);
+      } else if (
+          value_map_.count(schema_.arguments()[i].name()) &&
+          value_map_.count(schema_.arguments()[j].name())) {
+        if (value_map_[schema_.arguments()[i].name()].isAliasOf(
+                value_map_[schema_.arguments()[j].name()])) {
+          input_alias_map_[i].insert(j);
+          input_alias_map_[j].insert(i);
+        }
+      }
+    }
+  }
+}
+
+} // namespace utils
 } // namespace torch
--- torch/csrc/utils/schema_info.h
+++ torch/csrc/utils/schema_info.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <torch/csrc/jit/frontend/function_schema_parser.h>
+#include <unordered_set>
 
 namespace torch {
 namespace utils {
@@ -15,12 +16,37 @@ namespace utils {
 struct TORCH_API SchemaInfo {
  public:
   explicit SchemaInfo(c10::FunctionSchema schema)
-      : schema_(std::move(schema)) {}
+      : schema_(std::move(schema)), alias_maps_current_(false) {}
   explicit SchemaInfo(const char* signature)
-      : schema_(torch::jit::parseSchema(signature)) {}
+      : schema_(torch::jit::parseSchema(signature)),
+        alias_maps_current_(false) {}
+
+  bool is_mutable();
+
+  bool is_mutable(size_t index);
+
+  bool is_mutable(c10::string_view name);
+
+  void addArgumentValue(const std::string& name, const at::IValue& value);
+
+  void addArgumentValues(
+      const std::vector<c10::optional<at::IValue>>& value_list);
+
+  void addArgumentValues(
+      const std::unordered_map<std::string, at::IValue>& values);
 
  private:
+  void generateAliasMaps();
+
+  // Map of argument IValues
+  std::unordered_map<std::string, at::IValue> value_map_;
+
+  // Alias map of inputs with each other
+  std::vector<std::unordered_set<size_t>> input_alias_map_;
+
   c10::FunctionSchema schema_;
+
+  bool alias_maps_current_;
 };
 } // namespace utils
 } // namespace torch
