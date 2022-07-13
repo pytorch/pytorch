7b66f524983f1059e3bb7ad0f2a8c46446737c75
migeedz <migeedz@fb.com>
2022-07-13 11:00:36 -0700

    IndexSelect constraints, add a missing constraint in constraint tranformations and modify tests accordingly (#81344)
    - Constraints for IndexSelect, which allow us to replace a dimension from the input
    - There was a bug, which is that for a constraint of the form "x \leq 4" where x is a tensor, we did not generate the constraint" x = Dyn in the transformation step. This is fixed and the tests are modified accordingly.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81344
    Approved by: https://github.com/jansel, https://github.com/jamesr66a

--- torch/fx/experimental/migrate_gradual_types/constraint.py
+++ torch/fx/experimental/migrate_gradual_types/constraint.py
@@ -211,6 +211,49 @@ class CanReshape(Constraint):
             return False
 
 
+
+class IndexSelect(Constraint):
+
+    def __init__(self, tensor_size, input_var, dim_replace, index, output):
+        """
+        Args:
+            input_var: input to index_select
+            tensor_size: tensor size we are considering
+            dim_replace: the dimension of the output at "index"
+            index: location of the dimensiont to replace in the input
+            outut: variable to store the result
+        """
+        assert isinstance(input_var, TVar)
+        assert isinstance(output, TVar)
+        assert isinstance(dim_replace, DVar) or dim_replace == Dyn
+        assert isinstance(index, int)
+
+        self.input_var = input_var
+        self.tensor_size = tensor_size
+        self.dim_replace = dim_replace
+        self.index = index
+        self.output = output
+
+    def __repr__(self):
+
+        return f' {self.output} = ' \
+               f'IndexSelect({self.input_var}, ' \
+               f'tensor_size: {self.tensor_size}, ' \
+               f'{self.dim_replace}, ' \
+               f'{self.index})'
+
+    def __eq__(self, other):
+        if isinstance(other, IndexSelect):
+            return self.tensor_size == other.tensor_size and\
+                self.dim_replace == other.dim_replace and\
+                self.index == other.index and\
+                self.output == other.output and\
+                self.input_var == other.input_var
+        else:
+            return False
+
+
+
 class GetItem(Constraint):
 
     def __init__(self, tensor_size, index, res, input_var):
