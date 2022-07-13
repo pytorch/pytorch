5c94792fe21629dfd3f0c3d415bda38538cc66a1
asl3 <amandaliu@fb.com>
2022-07-13 08:48:03 -0700

    Update on "[quant] Implement QAT for APoT FakeQuant"
    ### Summary:
    This PR implements FX Graph Mode QAT for APoT FakeQuant.
    
    ### Test Plan:
    Run models with: `python test/quantization/core/experimental/fx_graph_mode_apot.py`
    
    ### Accuracy Stats: 
    Uniform: int8
    APoT: 8-bit (b = 8, k = 2)
    
    **Model #1:** Uniform activation, uniform weight (FX Graph Mode quantized)
    Evaluation accuracy on test dataset: 69.54%, 88.99%
    
    **Model #2:** Uniform activation, APoT weight (FX Graph Mode quantized)
    Evaluation accuracy on test dataset: 69.76%, 89.08%
    
    **Model #3:** APoT activation and weight (FX Graph Mode quantized)
    Evaluation accuracy on test dataset: 69.54%, 89.04%
    
    **Model #4:** Eager mode quantized model Resnet18
    Evaluation accuracy on test dataset: 69.49%, 88.90%
    
    [ghstack-poisoned]

--- torch/ao/quantization/experimental/qconfig.py
+++ torch/ao/quantization/experimental/qconfig.py
@@ -6,8 +6,8 @@ import torch
 from torch.ao.quantization.experimental.fake_quantize import APoTFakeQuantize
 
 # uniform activation and weight
-uniform_qconfig = QConfig(activation=default_observer.with_args(dtype=torch.quint8),
-                          weight=default_observer.with_args(dtype=torch.qint8))
+uniform_qconfig = QConfig(activation=default_observer.with_args(quant_min=0, quant_max=255, dtype=torch.quint8),
+                          weight=default_observer.with_args(quant_min=0, quant_max=255, dtype=torch.qint8))
 
 # uniform activation, APoT weight
 apot_weight_qconfig = QConfig(activation=default_observer.with_args(dtype=torch.quint8),
