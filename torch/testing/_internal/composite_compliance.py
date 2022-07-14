a6b050f8033d441f7044a7ccb9c2d3dbb4b9382f
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-07-14 05:51:05 +0000

    [composite compliance] check output of backward with subclass args against regular tensor (#81400)
    Time Before
    ```
    = 919 passed, 12 skipped, 38374 deselected, 36 xfailed, 31 warnings in 699.56s (0:11:39) =
    ```
    
    Time After
    ```
    = 913 passed, 12 skipped, 38374 deselected, 42 xfailed, 31 warnings in 663.96s (0:11:03) =
    ```
    
    Will follow-up for operator and forward-ad
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81400
    Approved by: https://github.com/zou3519

--- torch/testing/_internal/composite_compliance.py
+++ torch/testing/_internal/composite_compliance.py
@@ -413,6 +413,29 @@ def check_backward_formula(op: Callable, args, kwargs,
                            output_process_fn_grad=None,
                            gradcheck_wrapper=None):
     CCT = generate_cct()
+
+    def compute_expected_grads(args, kwargs):
+        if gradcheck_wrapper is None:
+            results = op(*args, **kwargs)
+        else:
+            results = gradcheck_wrapper(op, *args, **kwargs)
+
+        if output_process_fn_grad is not None:
+            results = output_process_fn_grad(results)
+
+        flat_results, _ = tree_flatten(results)
+        flat_diff_results = [r for r in flat_results if r.requires_grad]
+        assert len(flat_diff_results) > 0
+
+        grads = [torch.ones(r.shape, device=r.device, dtype=r.dtype)
+                 for r in flat_diff_results]
+        leaf_tensors = gather_leaf_tensors(args, kwargs)
+        assert len(leaf_tensors) > 0
+        return torch.autograd.grad(flat_diff_results, leaf_tensors,
+                                   grads, allow_unused=True, retain_graph=True)
+
+    expected = compute_expected_grads(args, kwargs)
+
     for choice in generate_subclass_choices_args_kwargs(args, kwargs, CCT):
         new_args, new_kwargs, which_args_are_wrapped, which_kwargs_are_wrapped = choice
         leaf_tensors = gather_leaf_tensors(new_args, new_kwargs)
@@ -442,8 +465,8 @@ def check_backward_formula(op: Callable, args, kwargs,
                  for r in flat_diff_results]
         for flat_new_grads, which_grad_is_batched in generate_subclass_choices(grads, CCT):
             try:
-                torch.autograd.grad(flat_diff_results, leaf_tensors, flat_new_grads,
-                                    allow_unused=True, retain_graph=True)
+                actual = torch.autograd.grad(flat_diff_results, leaf_tensors, flat_new_grads,
+                                             allow_unused=True, retain_graph=True)
             # see NOTE: [What errors are Composite Compiance trying to catch?]
             except RuntimeError as err:
                 raise_composite_compliance_error(
@@ -452,6 +475,11 @@ def check_backward_formula(op: Callable, args, kwargs,
                     f"- wrapped_kwargs: {which_kwargs_are_wrapped}\n"
                     f"- wrapped_grads: {which_grad_is_batched}\n"
                 )
+
+            def unwrap(e):
+                return e.elem if isinstance(e, CCT) else e
+
+            torch.testing.assert_close(tuple(map(unwrap, actual)), expected, equal_nan=True)
 
 # Checks if the forward AD formula is composite compliant by testing
 # all possible permutations of {primals, tangents} being
