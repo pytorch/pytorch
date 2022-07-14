6107e811abd8949acec791ad96bda82daa7667c7
Justin Chu <justinchu@microsoft.com>
2022-07-14 18:50:43 +0000

    [ONNX] Add tests to quantized cat

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -11812,13 +11812,55 @@ class TestONNXRuntime(onnx_test_common._TestONNXRuntime):
     )
     @skipIfUnsupportedMinOpsetVersion(10)
     @skipScriptTest()  # torch.jit.frontend.FrontendError: Cannot instantiate class 'QFunctional' in a script function:
-    def test_quantized_cat(self):
+    def test_quantized_cat_when_concatinating_the_same_tensor(self):
         class QuantizedConcatenationModel(torch.nn.Module):
             def forward(self, x):
                 return torch.nn.quantized.QFunctional().cat((x, x), dim=1)
 
         q_input = torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 128, torch.quint8)
         self.run_test(QuantizedConcatenationModel(), q_input)
+
+    @common_utils.parametrize(
+        "input_1, input_2",
+        [
+            common_utils.subtest(
+                [
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 128, torch.quint8),
+                    torch.quantize_per_tensor(torch.zeros(1, 3), 0.26, 128, torch.quint8)
+                ],
+                name="when_shape_is_different",
+            ),
+            common_utils.subtest(
+                [
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 128, torch.quint8),
+                    torch.quantize_per_tensor(torch.ones(2, 3), 42, 1, torch.quint8)
+                ],
+                name="when_scale_is_different",
+            ),
+            common_utils.subtest(
+                [
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 128, torch.quint8),
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 63, torch.quint8)
+                ],
+                name="when_zero_point_is_different",
+            ),
+            common_utils.subtest(
+                [
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.26, 128, torch.quint8),
+                    torch.quantize_per_tensor(torch.ones(2, 3), 0.1, 63, torch.quint8)
+                ],
+                name="when_zero_point_and_scale_are_different",
+            ),
+        ],
+    )
+    @skipIfUnsupportedMinOpsetVersion(10)
+    @skipScriptTest()  # torch.jit.frontend.FrontendError: Cannot instantiate class 'QFunctional' in a script function:
+    def test_quantized_cat(self, input_1: torch.Tensor, input_2: torch.Tensor):
+        class QuantizedConcatenationModel(torch.nn.Module):
+            def forward(self, x, y):
+                return torch.nn.quantized.QFunctional().cat((x, y), dim=0)
+
+        self.run_test(QuantizedConcatenationModel(), input_1, input_2)
 
     @skipIfUnsupportedMinOpsetVersion(10)
     # torch.jit.frontend.FrontendError:
