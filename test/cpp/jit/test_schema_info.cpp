cabceb7f8d43368e5e9c29edd6f0665cf7cb1bed
goldenxuett <ttanx@fb.com>
2022-07-14 16:18:55 -0700

    [JIT] Add special cases batch_norm, instance_norm and dropout for SchemaInfo (#81007)
    - Added special cases for detach in is_non_deterministic() check and batch_norm and instance_norm in is_mutable() check in SchemaInfo().
    - Added tests for the above special cases for detach, batch_norm and instance_norm.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81007
    Approved by: https://github.com/davidberard98

--- test/cpp/jit/test_schema_info.cpp
+++ test/cpp/jit/test_schema_info.cpp
@@ -62,6 +62,27 @@ TEST(SchemaInfoIsMutableTest, AliasingInputs) {
   ASSERT_TRUE(schema.is_mutable(1));
   ASSERT_TRUE(schema.is_mutable("other"));
 }
+
+TEST(SchemaInfoIsMutableTest, InstanceNorm) {
+  SchemaInfo schema_info(
+      "aten::instance_norm(Tensor input, Tensor? weight, Tensor? bias, Tensor? running_mean, Tensor? running_var, bool use_input_stats, float momentum, float eps, bool cudnn_enabled) -> Tensor");
+  ASSERT_FALSE(schema_info.is_mutable("running_mean"));
+  ASSERT_FALSE(schema_info.is_mutable("running_var"));
+  schema_info.addArgumentValue("use_input_stats", true);
+  ASSERT_TRUE(schema_info.is_mutable("running_mean"));
+  ASSERT_TRUE(schema_info.is_mutable("running_var"));
+}
+
+TEST(SchemaInfoIsMutableTest, BatchNorm) {
+  SchemaInfo schema_info(
+      "aten::batch_norm(Tensor input, Tensor? weight, Tensor? bias, Tensor? running_mean, Tensor? running_var, bool training, float momentum, float eps, bool cudnn_enabled) -> Tensor");
+  ASSERT_FALSE(schema_info.is_mutable("running_mean"));
+  ASSERT_FALSE(schema_info.is_mutable("running_var"));
+  schema_info.addArgumentValue("training", true);
+  ASSERT_TRUE(schema_info.is_mutable("running_mean"));
+  ASSERT_TRUE(schema_info.is_mutable("running_var"));
+}
+
 TEST(SchemaInfoIsNonDeterministicTest, Basic) {
   SchemaInfo deterministic_schema_info(
       "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
@@ -69,6 +90,14 @@ TEST(SchemaInfoIsNonDeterministicTest, Basic) {
       "aten::bernoulli(Tensor self, *, Generator? generator) -> Tensor");
   ASSERT_FALSE(deterministic_schema_info.is_nondeterministic());
   ASSERT_TRUE(nondeterministic_schema_info.is_nondeterministic());
+}
+
+TEST(SchemaInfoIsNonDeterministicTest, Dropout) {
+  SchemaInfo droupout_schema_info(
+      "aten::dropout(Tensor input, float p, bool train) -> Tensor");
+  ASSERT_TRUE(droupout_schema_info.is_nondeterministic());
+  droupout_schema_info.addArgumentValue("train", false);
+  ASSERT_FALSE(droupout_schema_info.is_nondeterministic());
 }
 
 TEST(FunctionSchemaMayAliasTest, Basic) {
