4963adcc8d1ac7ac3be5fd72d946b2389c0d27bb
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-07-14 19:53:51 +0000

    Revert "[composite compliance] matrix_exp (#81225)"
    This reverts commit 367c6952373a8a10a3f7f25693fc16da99f90382.
    
    Reverted https://github.com/pytorch/pytorch/pull/81225 on behalf of https://github.com/clee2000 due to broke functorch https://github.com/pytorch/pytorch/runs/7345901504?check_suite_focus=true

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -3972,22 +3972,10 @@ Tensor differential_analytic_matrix_function(
   meta_grad_sizes[A.dim() - 1] *= 2;
 
   auto n = A.size(-1);
-  Tensor meta_grad;
-  // For Composite Compliance, we can't copy a Subclass into a Regular Tensor,
-  // so we use out-of-place ops with equivalent output.
-  // NOTE: We can't use `new_zeros` directly as both `A` and `grad` can
-  // be Tensor Subclass and we don't want to make assumption about which
-  // one to choose for creating output buffer.
-  // eg. if both are BatchedTensor at different level.
-  if (areAnyTensorSubclassLike({A, grad})) {
-    meta_grad = at::cat(
-        {at::cat({A, grad}, -1), at::cat({at::zeros_like(A), A}, -1)}, -2);
-  } else {
-    meta_grad = at::zeros(meta_grad_sizes, grad.options());
-    meta_grad.narrow(-2, 0, n).narrow(-1, 0, n).copy_(A);
-    meta_grad.narrow(-2, n, n).narrow(-1, n, n).copy_(A);
-    meta_grad.narrow(-2, 0, n).narrow(-1, n, n).copy_(grad);
-  }
+  auto meta_grad = at::zeros(meta_grad_sizes, grad.options());
+  meta_grad.narrow(-2, 0, n).narrow(-1, 0, n).copy_(A);
+  meta_grad.narrow(-2, n, n).narrow(-1, n, n).copy_(A);
+  meta_grad.narrow(-2, 0, n).narrow(-1, n, n).copy_(grad);
 
   return matrix_function(meta_grad).narrow(-2, 0, n).narrow(-1, n, n);
 }
