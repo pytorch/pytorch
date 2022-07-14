4e8aa2bd185aca8aa59134f68d7b3fcc1176b1e2
o-hanna <107527460+o-hanna@users.noreply.github.com>
2022-07-13 18:42:00 -0700

    Update test/distributed/test_pg_wrapper.py
    Co-authored-by: Andrew Gu <31054793+awgu@users.noreply.github.com>

--- test/distributed/test_pg_wrapper.py
+++ test/distributed/test_pg_wrapper.py
@@ -37,7 +37,7 @@ class AbstractProcessGroupWrapperTest(MultiProcessTestCase):
         seq_num_msg = "The sequence number is: "
         seq_num_msg_index = err.find(seq_num_msg)
         seq_num_msg_len = len(seq_num_msg)
-        self.assertTrue(seq_num_msg_index != -1, "Sequence number is missing.")
+        self.assertNotEqual(seq_num_msg_index, -1, "Sequence number is missing.")
         self.assertTrue(err[seq_num_msg_index + seq_num_msg_len].isdigit(), "Sequence number is invalid.")
 
         self.assertTrue(
