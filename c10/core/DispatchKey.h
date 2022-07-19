d06e168517fa8c68848a546f6613ac82a9ade7e5
Edward Z. Yang <ezyang@fb.com>
2022-07-19 11:25:05 -0700

    Fix off by one error with EndOfBackendKeys
    It looks like the alias was cargo culted from functionality keys,
    but you only need to actually do an alias if you need a break in the
    middle of the sequence while keeping the numbering contiguous, which
    doesn't apply here.  (It would also be necessary if we exactly filled
    the representation type, but uint8_t has plenty of space.)
    
    I audited all uses of this key and it turns out that it had committed
    (harmless) off by one errors.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- c10/core/DispatchKey.h
+++ c10/core/DispatchKey.h
@@ -66,11 +66,8 @@ enum class BackendComponent : uint8_t {
   PrivateUse1Bit,
   PrivateUse2Bit,
   PrivateUse3Bit,
-  // Define an alias to represent end of backend dispatch keys.
-  // If you add new backend keys after PrivateUse3, please also update it here.
-  // (But you shouldn't: private use keys should have higher precedence than
-  // all built-in keys)
-  EndOfBackendKeys = PrivateUse3Bit,
+  // Don't put any more keys after here
+  EndOfBackendKeys,
 };
 
 // Semantically, a dispatch key identifies a possible "level" in our
