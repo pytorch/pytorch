85eb881dbc357383c14bee5f095658fd71337d83
Fabio Rocha <frocha@quansight.com>
2022-07-20 13:59:36 +0000

    Update base for Update on "[primTorch] Added reference for unflatten"
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -10541,12 +10541,16 @@ def error_inputs_mean(op_info, device, **kwargs):
 
 def reference_flatten(input, start_dim=0, end_dim=-1):
     in_shape = input.shape
-    in_dim = len(in_shape)
-    end_dim = end_dim if end_dim >= 0 else in_dim + end_dim
-    if in_dim == 0:
+    in_rank = len(in_shape)
+    for d in start_dim, end_dim:
+        if not((in_rank == 0 and d in (-1, 0)) or -in_rank <= d < in_rank):
+            raise IndexError(f"Dimension out of range (expected to be in range of [{-in_rank}, {in_rank-1}], but got {d}")
+    end_dim = end_dim if end_dim >= 0 else in_rank + end_dim
+    start_dim = start_dim if start_dim >= 0 else in_rank + start_dim
+    if in_rank == 0:
         end_dim = start_dim
     if end_dim < start_dim:
-        raise RuntimeError
+        raise RuntimeError("flatten() has invalid args: start_dim cannot come after end_dim")
     flatten_bit_dim = functools.reduce(operator.mul, in_shape[start_dim:end_dim + 1], 1)
     out_shape = in_shape[:start_dim] + (flatten_bit_dim,) + in_shape[end_dim + 1:]
     return np.reshape(input, out_shape)
@@ -16758,16 +16762,13 @@ op_db: List[OpInfo] = [
            reference_inputs_func=reference_inputs_flatten,
            ),
     OpInfo('unflatten',
-           op=torch.Tensor.unflatten,
+           op=torch.unflatten,
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
            supports_out=False,
            supports_forward_ad=True,
            supports_fwgrad_bwgrad=True,
            sample_inputs_func=sample_inputs_unflatten,
-           skips=(
-               # RuntimeError: object has no attribute unflatten
-               DecorateInfo(unittest.expectedFailure, 'TestJit', 'test_variant_consistency_jit'),
-           )),
+           ),
     OpInfo('column_stack',
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
            supports_forward_ad=True,
