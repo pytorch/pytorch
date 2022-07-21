c7bc8aeb120eacc0d6df9787085e4ccd0cec07f7
David Chen (Meta Employee) <davidchencsl@fb.com>
2022-07-21 07:58:50 -0700

    Add pattern to detect for loop indexing into tensor (#81056) (#81056)
    Summary:
    This commit added a pattern in the pattern matcher to detect if for loop & indexing is used that can be vectorized.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81056
    Approved by: https://github.com/robieta
    
    Test Plan:
    contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/6e621d028766808f3c0bc8c45f776af3bf9cf22f
    
    Test plan from GitHub:
    Added a test in test_profiler.py that creates a bootstrap example.
    
    Original Phabricator Test Plan:
    Imported from OSS
    
    Reviewed By: jeanschmidt
    
    Differential Revision: D37725266
    
    Pulled By: davidchencsl
    
    fbshipit-source-id: 896b31d06bee7fa1daf3b32ee6b8340e921086bd

--- torch/csrc/autograd/init.cpp
+++ torch/csrc/autograd/init.cpp
@@ -271,18 +271,32 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
 
   {
     using torch::profiler::impl::Result;
+    py::enum_<EventType>(m, "_EventType")
+        .value("TorchOp", EventType::TorchOp)
+        .value("Backend", EventType::Backend)
+        .value("Allocation", EventType::Allocation)
+        .value("PyCall", EventType::PyCall)
+        .value("PyCCall", EventType::PyCCall);
     py::class_<ExtraFields<EventType::TorchOp>>(m, "_ExtraFields_TorchOp")
         .def_readonly("inputs", &ExtraFields<EventType::TorchOp>::inputs_);
-
     py::class_<Inputs>(m, "_Inputs")
         .def_readonly("shapes", &Inputs::shapes_)
         .def_readonly("dtypes", &Inputs::dtypes_);
-
     py::class_<ExtraFields<EventType::Backend>>(m, "_ExtraFields_Backend");
     py::class_<ExtraFields<EventType::Allocation>>(
         m, "_ExtraFields_Allocation");
-    py::class_<ExtraFields<EventType::PyCall>>(m, "_ExtraFields_PyCall");
-    py::class_<ExtraFields<EventType::PyCCall>>(m, "_ExtraFields_PyCCall");
+    py::class_<ExtraFields<EventType::PyCall>>(m, "_ExtraFields_PyCall")
+        .def_readonly("caller", &ExtraFields<EventType::PyCall>::caller_);
+    py::class_<ExtraFields<EventType::PyCCall>>(m, "_ExtraFields_PyCCall")
+        .def_readonly("caller", &ExtraFields<EventType::PyCall>::caller_);
+    py::class_<PyFrameState>(m, "_PyFrameState")
+        .def_readonly("line_number", &PyFrameState::line_no_)
+        .def_property_readonly(
+            "file_name",
+            [](const PyFrameState& s) { return s.filename_.str(); })
+        .def_property_readonly("function_name", [](const PyFrameState& s) {
+          return s.funcname_.str();
+        });
 
     py::class_<Result, std::shared_ptr<Result>>(m, "_ProfilerEvent")
         .def("name", &Result::name)
