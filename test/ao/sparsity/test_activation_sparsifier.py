5901ab4ae17cc89729299bec0c88e622e5aac6c8
macandro96 <anishm@fb.com>
2022-07-22 17:52:07 +0000

    [ao][sparsity] Serialization support (#80890)
    Implemented dumping and loading of state_dicts and __get_state__ and __set_state__ functions.
    hook and layer are removed from the data_groups dictionary before serializing.
    
    In the future, might have to treat functions differently before serializing. Currently, it is being
    treated similar to other types while serializing.
    
    Test Plan:
    ```python test/test_ao_sparsity.py TestActivationSparsifier```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80890
    Approved by: https://github.com/z-a-f

--- test/ao/sparsity/test_activation_sparsifier.py
+++ test/ao/sparsity/test_activation_sparsifier.py
@@ -197,6 +197,41 @@ class TestActivationSparsifier(TestCase):
 
         activation_sparsifier.model(data)
 
+
+    def _check_state_dict(self, sparsifier1):
+        """Checks if loading and restoring of state_dict() works as expected.
+        Basically, dumps the state of the sparsifier and loads it in the other sparsifier
+        and checks if all the configuration are in line.
+
+        This function is called at various times in the workflow to makes sure that the sparsifier
+        can be dumped and restored at any point in time.
+        """
+        state_dict = sparsifier1.state_dict()
+
+        new_model = Model()
+
+        # create an empty new sparsifier
+        sparsifier2 = ActivationSparsifier(new_model)
+
+        assert sparsifier2.defaults != sparsifier1.defaults
+        assert len(sparsifier2.data_groups) != len(sparsifier1.data_groups)
+
+        sparsifier2.load_state_dict(state_dict)
+
+        assert sparsifier2.defaults == sparsifier1.defaults
+        assert sparsifier2.state == sparsifier1.state
+
+        dg1, dg2 = sparsifier1.data_groups, sparsifier2.data_groups
+
+        for layer_name, config in dg1.items():
+            assert layer_name in dg2
+
+            # exclude hook and layer
+            config1 = {key: value for key, value in config.items() if key not in ['hook', 'layer']}
+            config2 = {key: value for key, value in dg2[layer_name].items() if key not in ['hook', 'layer']}
+
+            assert config1 == config2
+
     @skipIfTorchDynamo("TorchDynamo fails with unknown reason")
     def test_activation_sparsifier(self):
         """Simulates the workflow of the activation sparsifier, starting from object creation
@@ -284,8 +319,11 @@ class TestActivationSparsifier(TestCase):
         # check if things are registered correctly
         self._check_register_layer(activation_sparsifier, defaults, sparse_config, layer_args_list)
 
+        # check state_dict after registering and before model forward
+        self._check_state_dict(activation_sparsifier)
+
         # check if forward pre hooks actually work
-        # create some dummy data and run model forward
+        # some dummy data
         data_list = []
         num_data_points = 5
         for _ in range(0, num_data_points):
@@ -294,9 +332,14 @@ class TestActivationSparsifier(TestCase):
             data_list.append(rand_data)
 
         data_agg_actual = self._check_pre_forward_hook(activation_sparsifier, data_list)
+        # check state_dict() before step()
+        self._check_state_dict(activation_sparsifier)
 
         # STEP 3: sparsifier step
         activation_sparsifier.step()
+
+        # check state_dict() after step() and before squash_mask()
+        self._check_state_dict(activation_sparsifier)
 
         # self.check_step()
         self._check_step(activation_sparsifier, data_agg_actual)
@@ -305,3 +348,6 @@ class TestActivationSparsifier(TestCase):
         activation_sparsifier.squash_mask()
 
         self._check_squash_mask(activation_sparsifier, data_list[0])
+
+        # check state_dict() after squash_mask()
+        self._check_state_dict(activation_sparsifier)
