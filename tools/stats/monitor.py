e150485c87d24bdab912fa5c100057278a822cb9
Catherine Lee <csl@fb.com>
2022-07-22 19:41:40 -0700

    gpu utilization, sig handler, better pip

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -1,77 +1,54 @@
 #!/usr/bin/env python3
 import datetime
-import importlib.util
 import json
-import subprocess
-import sys
+import signal
 import time
 from typing import Any, Dict, List
 
-
-def pip_install(package_name: str) -> None:
-    if importlib.util.find_spec(package_name) is None:
-        output = subprocess.run(
-            [sys.executable, "-m", "pip", "install", package_name],
-            stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-        )
-        if output.returncode != 0:
-            print(output.stderr.decode("utf-8"))
-            print(output.stdout.decode("utf-8"))
-    # when running this script in the background, pip installs the packages to
-    # a location that is not in a path python uses to look for modules, so we
-    # manually add the location to the path
-    location = subprocess.run(
-        [sys.executable, "-m", "pip", "show", package_name],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
-    for s in location.stdout.decode("utf-8").splitlines():
-        if "Location:" in s:
-            sys.path.append(s.split(" ")[1])
+import psutil  # type: ignore[import]
+import pynvml  # type: ignore[import]
 
 
-def main() -> None:
-    pip_install("psutil")
-    pip_install("pynvml")
-    import psutil  # type: ignore[import]
-    import pynvml  # type: ignore[import]
+def get_processes_running_python_tests() -> List[Any]:
+    python_processes = []
+    for process in psutil.process_iter():
+        try:
+            if "python" in process.name() and process.cmdline():
+                python_processes.append(process)
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            # access denied or the process died
+            pass
+    return python_processes
 
-    def get_processes_running_python_tests() -> List[Any]:
-        python_processes = []
-        for process in psutil.process_iter():
-            try:
-                if "python" in process.name() and process.cmdline():
-                    python_processes.append(process)
-            except (psutil.NoSuchProcess, psutil.AccessDenied):
-                # access denied or the process died
-                pass
-        return python_processes
 
-    def get_per_process_cpu_info() -> List[Dict[str, Any]]:
-        processes = get_processes_running_python_tests()
-        per_process_info = []
-        for p in processes:
-            info = {
-                "pid": p.pid,
-                "cmd": " ".join(p.cmdline()),
-                "cpu_percent": p.cpu_percent(),
-                "rss_memory": p.memory_info().rss,
-                "uss_memory": p.memory_full_info().uss,
-            }
-            if "pss" in p.memory_full_info():
-                # only availiable in linux
-                info["pss_memory"] = p.memory_full_info().pss
-            per_process_info.append(info)
-        return per_process_info
+def get_per_process_cpu_info() -> List[Dict[str, Any]]:
+    processes = get_processes_running_python_tests()
+    per_process_info = []
+    for p in processes:
+        info = {
+            "pid": p.pid,
+            "cmd": " ".join(p.cmdline()),
+            "cpu_percent": p.cpu_percent(),
+            "rss_memory": p.memory_info().rss,
+            "uss_memory": p.memory_full_info().uss,
+        }
+        if "pss" in p.memory_full_info():
+            # only availiable in linux
+            info["pss_memory"] = p.memory_full_info().pss
+        per_process_info.append(info)
+    return per_process_info
 
-    def get_per_process_gpu_info(handle: Any) -> List[Dict[str, Any]]:
-        processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
-        per_process_info = []
-        for p in processes:
-            info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
-            per_process_info.append(info)
-        return per_process_info
+
+def get_per_process_gpu_info(handle: Any) -> List[Dict[str, Any]]:
+    processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+    per_process_info = []
+    for p in processes:
+        info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
+        per_process_info.append(info)
+    return per_process_info
+
+
+if __name__ == "__main__":
 
     handle = None
     try:
@@ -81,7 +58,15 @@ def main() -> None:
         # no pynvml avaliable, probably because not cuda
         pass
 
-    while True:
+    kill_now = False
+
+    def exit_gracefully(*args: Any) -> None:
+        global kill_now
+        kill_now = True
+
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+    while not kill_now:
         try:
             stats = {
                 "time": datetime.datetime.utcnow().isoformat("T") + "Z",
@@ -90,6 +75,9 @@ def main() -> None:
             }
             if handle is not None:
                 stats["per_process_gpu_info"] = get_per_process_gpu_info(handle)
+                stats["total_gpu_utilizaiton"] = pynvml.nvmlDeviceGetUtilizationRates(
+                    handle
+                ).gpu
         except Exception as e:
             stats = {
                 "time": datetime.datetime.utcnow().isoformat("T") + "Z",
@@ -98,7 +86,3 @@ def main() -> None:
         finally:
             print(json.dumps(stats))
             time.sleep(1)
-
-
-if __name__ == "__main__":
-    main()
