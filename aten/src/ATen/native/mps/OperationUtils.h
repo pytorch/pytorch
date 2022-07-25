951f62f877cb7878f6790e3c1df4e7934a76d16d
Kulin Seth <kulin_seth@apple.com>
2022-07-25 23:02:13 +0000

    [MPS]  Perf fixes. (#81951)
    Fixes https://github.com/pytorch/pytorch/issues/81610
    * Use fillBuffer() for zero_mps()
    Fix minor bug in add_sub_template() with value=0.0
    Change default value of use_scalar_value to false in getTensorsStringKey()
    
    * Fallback to fill_scalar_mps() if buffer isn't contiguous.
    
    * Fix high memory consumption in view ops
    
    * Change commitAndWait to Commit in View Ops
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81951
    Approved by: https://github.com/razarmehr, https://github.com/albanD

--- aten/src/ATen/native/mps/OperationUtils.h
+++ aten/src/ATen/native/mps/OperationUtils.h
@@ -47,7 +47,7 @@ MPSDataType getMPSDataType(ScalarType scalar_type);
 MPSDataType getMPSScalarType(ScalarType scalar_type);
 std::string getMPSTypeString(ScalarType scalar_type);
 std::string getMPSShapeString(MPSShape* shape);
-std::string getTensorsStringKey(const TensorList& tensors, bool use_scalar_value = true);
+std::string getTensorsStringKey(const TensorList& tensors, bool use_scalar_value = false);
 double getMPSScalarValue(const Tensor& t);
 std::string getArrayRefString(const IntArrayRef s);
 // use has_storage() on the returned tensor to determine if src actually is a view
