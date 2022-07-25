2da1eb3f0b446f8d6432950c545d0bc9b03f5b0a
Scott Wolchok <swolchok@fb.com>
2022-07-25 09:48:58 -0700

    [PyTorch] Support ExclusivelyOwned<caffe2::Tensor> (#81964)
    Since `caffe2::Tensor` also shares `TensorImpl`, we can apply `ExclusivelyOwned` to it as needed.
    
    Differential Revision: [D38066340](https://our.internmc.facebook.com/intern/diff/D38066340/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81964
    Approved by: https://github.com/ezyang

--- c10/util/intrusive_ptr.h
+++ c10/util/intrusive_ptr.h
@@ -24,9 +24,6 @@ namespace intrusive_ptr {
 inline void incref(intrusive_ptr_target* self);
 }
 
-template <typename TTarget>
-struct ExclusivelyOwnedTraits;
-
 // constructor tag used by intrusive_ptr constructors
 struct DontIncreaseRefcount {};
 } // namespace raw
@@ -92,7 +89,7 @@ class C10_API intrusive_ptr_target {
       intrusive_ptr_target* self);
 
   template <typename T>
-  friend struct ExclusivelyOwnedTraits;
+  friend struct ExclusivelyOwnedTensorTraits;
 
  protected:
   // protected destructor. We never want to destruct intrusive_ptr_target*
@@ -249,7 +246,7 @@ class intrusive_ptr final {
   TTarget* target_;
 
   template <typename T>
-  friend struct ExclusivelyOwnedTraits;
+  friend struct ExclusivelyOwnedTensorTraits;
   template <class TTarget2, class NullType2>
   friend class intrusive_ptr;
   friend class weak_intrusive_ptr<TTarget, NullType>;
