2397e6bd4e20a70a33560556835704262ca54ca1
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-07-25 14:50:23 +0300

    Add _nvfuser_impls to nvfuser_prims.py

--- torch/_prims/nvfuser_prims.py
+++ torch/_prims/nvfuser_prims.py
@@ -5,7 +5,16 @@
 # but some additional primitives can be added in the future for the corresponding
 # higher-level torch/aten functions.
 
+from typing import Any, Dict
+
 import torch
+
+from torch._prims_common import (
+    DimsSequenceType,
+    getnvFuserDtype,
+    ShapeType,
+    TensorLikeType,
+)
 
 from torch.utils._pytree import tree_flatten, tree_unflatten
 
@@ -99,6 +108,179 @@ def _autograd_fn(prim):
     return _autograd_impl
 
 
+_nvfuser_impls: Dict[str, Any] = {}
+
+_nvfuser_unary_ops = {
+    "abs",
+    "acos",
+    "asin",
+    "atan",
+    "atanh",
+    "cos",
+    "cosh",
+    "bitwise_not",
+    "ceil",
+    "erf",
+    "erfc",
+    "exp",
+    "expm1",
+    "floor",
+    "imag",
+    "isfinite",
+    "lgamma",
+    "log",
+    "log1p",
+    "log2",
+    "log10",
+    "reciprocal",
+    "neg",
+    "real",
+    "round",
+    "rsqrt",
+    "sin",
+    "sinh",
+    "sqrt",
+    "tan",
+    "tanh",
+    "trunc",
+}
+
+
+def _assert_nvfuser_op_exists(fname: str):
+    try:
+        from torch._C._nvfuser import FusionDefinition as fd  # type: ignore[import]
+
+        assert getattr(fd.Ops, fname)
+    except ImportError:
+        # Not all PyTorch builds have nvfuser
+        pass
+
+
+for fname in _nvfuser_unary_ops:
+    exec(
+        f"""
+# Ensure that the nvfuser implementation exists
+_assert_nvfuser_op_exists("{fname}")
+
+def _{fname}_nvfuser(fd, a):
+    return fd.Ops.{fname}(a)  # type: ignore[attr-defined]
+
+_nvfuser_impls["{fname}"] = _{fname}_nvfuser
+"""
+    )
+
+_nvfuser_binary_ops = {
+    "add",
+    "atan2",
+    "bitwise_and",
+    "bitwise_or",
+    "bitwise_xor",
+    "div",
+    "eq",
+    "fmod",
+    "ge",
+    "gt",
+    "le",
+    "lt",
+    "mul",
+    "ne",
+    "pow",
+    "remainder",
+    "sub",
+}
+
+for fname in _nvfuser_binary_ops:
+    exec(
+        f"""
+# Ensure that the nvfuser implementation exists
+_assert_nvfuser_op_exists("{fname}")
+
+def _{fname}_nvfuser(fd, a, b):
+    return fd.Ops.{fname}(a, b)  # type: ignore[attr-defined]
+
+_nvfuser_impls["{fname}"] = _{fname}_nvfuser
+"""
+    )
+
+_nvfuser_ternary_ops = {
+    "where",
+}
+
+for fname in _nvfuser_ternary_ops:
+    exec(
+        f"""
+# Ensure that the nvfuser implementation exists
+_assert_nvfuser_op_exists("{fname}")
+
+def _{fname}_nvfuser(fd, a, b, c):
+    return fd.Ops.{fname}(a, b, c)  # type: ignore[attr-defined]
+
+_nvfuser_impls["{fname}"] = _{fname}_nvfuser
+"""
+    )
+
+
+def _broadcast_in_dim_nvfuser(
+    fd: Any,
+    a: TensorLikeType,
+    shape: ShapeType,
+    broadcast_dimensions: ShapeType,
+):
+    return fd.Ops.broadcast_in_dim(a, shape, broadcast_dimensions)  # type: ignore[attr-defined]
+
+
+def _convert_element_type_nvfuser(fd: Any, a: TensorLikeType, dtype: torch.dtype):
+    nvfuser_dtype = getnvFuserDtype(dtype)
+    return fd.Ops.cast(nvfuser_dtype, a)  # type: ignore[attr-defined]
+
+
+def _sum_nvfuser(
+    fd: Any,
+    a: TensorLikeType,
+    dims: DimsSequenceType,
+):
+    keep_dims = False
+    output_dtype = torch._C._nvfuser.DataType.Null
+    return fd.Ops.sum(a, dims, keep_dims, output_dtype)
+
+
+def _var_nvfuser(
+    fd: Any,
+    a: TensorLikeType,
+    dims: DimsSequenceType,
+    *,
+    correction: int,
+):
+    keep_dims = False
+    return fd.Ops.var(a, dims, correction, keep_dims)
+
+
+def _amax_nvfuser(
+    fd: Any,
+    a: TensorLikeType,
+    dims: DimsSequenceType,
+):
+    keep_dims = False
+    return fd.Ops.max(a, dims, keep_dims)
+
+
+def _amin_nvfuser(
+    fd: Any,
+    a: TensorLikeType,
+    dims: DimsSequenceType,
+):
+    keep_dims = False
+    return fd.Ops.min(a, dims, keep_dims)
+
+
+_nvfuser_impls["broadcast_in_dim"] = _broadcast_in_dim_nvfuser
+_nvfuser_impls["convert_element_type"] = _convert_element_type_nvfuser
+_nvfuser_impls["sum"] = _sum_nvfuser
+_nvfuser_impls["var"] = _var_nvfuser
+_nvfuser_impls["amax"] = _amax_nvfuser
+_nvfuser_impls["amin"] = _amin_nvfuser
+
+
 def register_nvprims():
     """Registers all nvFuser primitives in the torch.ops.nvprims module."""
     for name in nvprim_names:
@@ -115,5 +297,5 @@ def register_nvprims():
 
         for p in (_prim_packet, _prim):
             p.__doc__ = main_prim.__doc__
-            p.impl_nvfuser = main_prim.impl_nvfuser  # type: ignore[attr-defined]
+            p.impl_nvfuser = _nvfuser_impls[name]
             p.return_type = main_prim.return_type  # type: ignore[attr-defined]
