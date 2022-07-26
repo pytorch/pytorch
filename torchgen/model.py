c1d22cb7545fe6e6159efec9c4ffb40a9a3f7ac1
Edward Z. Yang <ezyang@fb.com>
2022-07-26 14:00:21 -0700

    Make factory functions CompositeExplicitAutograd
    This also makes them not decompose when we switch Python key.
    Note that CompositeExplicitAutogradNonFunctional maybe be overly
    conservative for some implementations (which actually call into
    other functional ops), but for now I just uniformly apply this
    everywhere to avoid errors.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- torchgen/model.py
+++ torchgen/model.py
@@ -665,7 +665,12 @@ class NativeFunction:
             )
         elif not structured and structured_delegate is None:
             name = str(func.name.name)
-            assert not (name.startswith("new_") or name.endswith("_like")), (
+            assert not (
+                name.startswith("new_")
+                or name.endswith("_like")
+                # TODO: maybe it's better to test the return
+                or (func.arguments.tensor_options and not func.arguments.has_tensor_arg())
+            ), (
                 f"expected {name} to have a CompositeExplicitAutogradNonFunctional "
                 "dispatch entry, but there was no dispatch table.  Factory functions "
                 "should not have implicit dispatch as they should not be decomposed "
@@ -2029,6 +2034,9 @@ class Arguments:
         )
 
         return arguments
+
+    def has_tensor_arg(self) -> bool:
+        return any(a.type.is_tensor_like() for a in self.flat_non_out)
 
     def signature(self, *, strip_default: bool = False) -> "Arguments":
         # dataclasses.replace could be used here, but it is less
