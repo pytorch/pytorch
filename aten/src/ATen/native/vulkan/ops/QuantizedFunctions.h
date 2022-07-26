1aacdb9491680f3bf6d7e2b2d4bfd9ac431732f2
Parth Savla <psavla2@fb.com>
2022-07-25 23:04:25 -0700

    [vulkan] implement quantized mul (#81640)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81640
    implemented quantized mul operator
    
    ghstack-source-id: 162439166
    
    Test Plan: buck run //xplat/caffe2:pt_vulkan_quantized_api_test_binAppleMac
    
    Reviewed By: SS-JIA
    
    Differential Revision: D37927234
    
    fbshipit-source-id: c4afa0efec2f95dc14697a32248ae95edb6db5b0

--- aten/src/ATen/native/vulkan/ops/QuantizedFunctions.h
+++ aten/src/ATen/native/vulkan/ops/QuantizedFunctions.h
@@ -31,6 +31,12 @@ Tensor quantized_sub(
     const double scale,
     const int64_t zero_point);
 
+Tensor quantized_mul(
+    const Tensor& self_arg,
+    const Tensor& other_arg,
+    const double scale,
+    const int64_t zero_point);
+
 Tensor conv2d(
     const Tensor& input_,
     const Tensor& weight,
