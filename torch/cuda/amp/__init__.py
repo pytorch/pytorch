5beaf8f589a27fff60a128b0507e553b991716fc
liyidong.lyd <liyidong.lyd@alibaba-inc.com>
2022-07-27 10:02:50 +0800

    dev

--- torch/cuda/amp/__init__.py
+++ torch/cuda/amp/__init__.py
@@ -1,2 +1,3 @@
 from .autocast_mode import autocast, custom_fwd, custom_bwd  # noqa: F401
 from .grad_scaler import GradScaler  # noqa: F401
+from .overlapped_grad_scaler import OverlappedGradScaler   # noqa: F401
