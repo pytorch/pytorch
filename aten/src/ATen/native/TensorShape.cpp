51f45dfeee7fca250336cd34062e0dc8305a6000
Nikolay Korovaiko <korovaikon@gmail.com>
2022-07-27 02:28:00 +0000

    redispatch expand.SymInt to its int version properly (#82264)
    ### Description
    we need to make sure the int overload of expand gets redispatched to the same device. Otherwise at::native::expand just calls a bunch of lower-level ops.
    
    ### Issue
    <!-- Link to Issue ticket or RFP -->
    
    ### Testing
    <!-- How did you test your change? -->
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82264
    Approved by: https://github.com/bdhirsh

--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -845,7 +845,7 @@ Tensor diag_embed(const Tensor& self, int64_t offset, int64_t dim1_, int64_t dim
 
 Tensor expand_symint(const Tensor& self, c10::SymIntArrayRef packed_size, bool implicit) {
   auto size = asIntArrayRefSlow(packed_size);
-  return expand(self, size, implicit);
+  return self.expand(size, implicit);
 }
 
 Tensor expand(const Tensor& self, IntArrayRef size, bool /*unused*/) {
