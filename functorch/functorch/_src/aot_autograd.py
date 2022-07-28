0d8fd49710a1e8c92cf5b11c1b56ed32a43a1fa0
Shangdi Yu <yushangdi123@gmail.com>
2022-07-28 20:39:42 +0000

    add graph_name to functorch (#82368)
    ### Description
    Add utilities to get the graph being compiled for debugging purposes.
    
    used in this PR: https://github.com/pytorch/torchdynamo/pull/665
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82368
    Approved by: https://github.com/Chillee

--- functorch/functorch/_src/aot_autograd.py
+++ functorch/functorch/_src/aot_autograd.py
@@ -135,6 +135,35 @@ def new_full(inp, size, value, dtype=None, layout=None, device=None, pin_memory=
     return torch.full(size, value, dtype=inp.dtype, device=inp.device)
 
 
+graph_being_compiled: str = None
+nth_graph: int = 0
+model_name: str = "model"
+
+
+def set_model_name(name):
+    global model_name
+    model_name = name
+
+
+def get_graph_being_compiled() -> str:
+    """
+    Returns the name of the graph being compiled.
+    """
+    global model_name, graph_being_compiled, nth_graph
+    return f"{model_name}_{graph_being_compiled}_{nth_graph}"
+
+
+@contextmanager
+def track_graph_compiling(graph_name, increment_index=False):
+    global graph_being_compiled
+    graph_being_compiled = graph_name
+    yield
+    if increment_index:
+        global nth_graph
+        nth_graph += 1
+    graph_being_compiled = None
+
+
 def create_aot_autograd_function(
     flat_fn, fw_compiler, bw_compiler, partition_fn, decompositions, grad_state
 ):
@@ -202,14 +231,18 @@ def create_aot_autograd_function(
                             def fake_fn(primals, tangents):
                                 return fx_g(primals, tangents)
                             fx_g = make_fx(functionalize(fake_fn))(*joint_inputs)
-                fw_module, bw_module = partition_fn(fx_g, joint_inputs)
+
+                with track_graph_compiling("joint"):
+                    fw_module, bw_module = partition_fn(fx_g, joint_inputs)
                 # print(fw_module.code, bw_module.code)
 
-                compiled_fw = fw_compiler(fw_module, flat_tensor_args)
+                with track_graph_compiling("forward"):
+                    compiled_fw = fw_compiler(fw_module, flat_tensor_args)
                 fw_outs = normalize_as_list(compiled_fw(*flat_tensor_args))
 
                 bw_args = fw_outs[num_outs:] + fw_outs[0:num_outs]
-                compiled_bw = bw_compiler(bw_module, bw_args)
+                with track_graph_compiling("backward", True):
+                    compiled_bw = bw_compiler(bw_module, bw_args)
             else:
                 fw_outs = normalize_as_list(compiled_fw(*flat_tensor_args))
             torch._C._jit_set_autocast_mode(old_jit_autocast_flag)
