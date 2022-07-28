0344998d4720972c4db7c8eaf5356672ea22d539
Edward Z. Yang <ezyang@fb.com>
2022-07-28 14:09:53 -0700

    Change SymIntNode into an intrusive pointer
    This will make the pointer type a single word, which is important
    for packing it into an int64_t
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- torchgen/dest/lazy_ir.py
+++ torchgen/dest/lazy_ir.py
@@ -49,8 +49,7 @@ def node_ctor_arg_rvalue_string(arg: LazyArgument) -> str:
             elif arg.is_symint_or_list:
                 cpp_type = arg.lazy_type.cpp_type()
                 return (
-                    f"{cpp_type}(std::dynamic_pointer_cast<torch::lazy::SymIntNodeImpl>"
-                    f"({arg.name}.toSymIntNodeImpl())->node_, 0)"
+                    f"{cpp_type}(dynamic_cast<torch::lazy::SymIntNodeImpl*>({arg.name}.toSymIntNodeImpl().get())->node_, 0)"
                 )
             return f"lazy_{arg.name}->GetIrValue()"
         elif isinstance(arg.lazy_type, OptionalCType):
