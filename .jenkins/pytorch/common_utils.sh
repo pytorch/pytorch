fa539d0f1dfcefc4b7a42d7b539c5520e629a487
Milad Mohammadi <milad.mo@gmail.com>
2022-07-29 21:07:02 +0000

    adding submodule update to the build process

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -117,6 +117,7 @@ function clone_pytorch_xla() {
     pushd xla
     # pin the xla hash so that we don't get broken by changes to xla
     git checkout "$(cat ../.github/ci_commit_pins/xla.txt)"
+    git submodule update --init --recursive
     popd
   fi
 }
