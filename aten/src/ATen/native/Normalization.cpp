9296e24616b644f961d25b851e1e624578f7a819
eqy <eddiey@nvidia.com>
2022-07-29 03:14:12 +0000

    [cuDNN] Work-around 32-bit indexing failures in cuDNN batchnorm (#81486)
    The following workload fails on Ampere:
    
    ```
    import torch
    import torch.nn as nn
    bn = nn.BatchNorm2d(128).cuda()
    x = torch.randn([256, 128, 294, 294], device='cuda')
    x = bn(x)
    print(x.shape)
    ```
    
    This PR adds a magic number in the `use_cudnn` condition to avoid this failure.
    
    CC @ptrblck
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81486
    Approved by: https://github.com/ngimel

--- aten/src/ATen/native/Normalization.cpp
+++ aten/src/ATen/native/Normalization.cpp
@@ -489,7 +489,9 @@ std::tuple<Tensor, Tensor, Tensor, Tensor, int64_t> _batch_norm_impl_index(
           ||(input.size(0) <= 65535 && !training)) //spatial, eval
       && detail::getCUDAHooks().compiledWithCuDNN()
       && eps >= detail::getCUDAHooks().batchnormMinEpsilonCuDNN()
-      && cudnn_enabled && detail::getCUDAHooks().versionCuDNN() >= 5110L);
+      && cudnn_enabled && detail::getCUDAHooks().versionCuDNN() >= 5110L
+      && input.numel() < std::numeric_limits<std::int32_t>::max() // some cuDNN kernels have 32-bit indexing limitations
+      );
 
   if (use_cudnn) {
     auto input_c = input.contiguous(input.suggest_memory_format());
