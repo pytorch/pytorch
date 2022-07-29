1c523f75a9173854bc567765864544b80a42a81a
Zain Rizvi <zainr@fb.com>
2022-07-29 01:52:12 +0000

    Allow PR collaborators revert PRs (#82360)
    ### Description
    This enables COLLABORATORS for a repository to revert PRs if they discover a problem.
    
    The main difference between a COLLABORATOR and a MEMBER is that a MEMBER has access to the entire pytorch organization, while a COLLABORATOR's access is limited to a specific repository.  But within that repository, granting them both revert rights seems quite reasonable
    
    ### Issue
    This request originated from pytorchbot's refusal to revert this pr: https://github.com/pytorch/pytorch/pull/79694#issuecomment-1189460942
    
    ### Testing
    CI
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82360
    Approved by: https://github.com/malfet, https://github.com/janeyx99

--- .github/scripts/test_trymerge.py
+++ .github/scripts/test_trymerge.py
@@ -21,7 +21,6 @@ from trymerge import (find_matching_merge_rule,
                       GitHubPR,
                       MergeRule,
                       MandatoryChecksMissingError,
-                      PostCommentError,
                       main as trymerge_main)
 from gitutils import get_git_remote_name, get_git_repo_dir, GitRepo
 from typing import Any, List, Optional
@@ -323,12 +322,10 @@ class TestGitHubPR(TestCase):
 
     @mock.patch('trymerge.gh_graphql', side_effect=mocked_gh_graphql)
     def test_revert_rules(self, mock_gql: Any) -> None:
-        """ Tests that reverts from collaborators are not allowed, Zain to change it soon """
+        """ Tests that reverts from collaborators are allowed """
         pr = GitHubPR("pytorch", "pytorch", 79694)
         repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
-        self.assertRaisesRegex(PostCommentError,
-                               ".*is not a MEMBER, but COLLABORATOR.*",
-                               lambda: validate_revert(repo, pr, comment_id=1189459845))
+        self.assertIsNotNone(validate_revert(repo, pr, comment_id=1189459845))
 
 if __name__ == "__main__":
     main()
