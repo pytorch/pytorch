ef6dbfcc58c19b7fb3d70ecdd962ff069d77ea2a
Edward Z. Yang <ezyang@fb.com>
2022-08-01 16:47:35 -0400

    Update base for Update on "Revert "Revert "Add a lint rule for PYBIND11_DECLARE_HOLDER_TYPE (#82556)"""
    This reverts commit ab8e5e6fb5135689734c34a3aa5e64721a738969.
    
    [ghstack-poisoned]

--- torch/csrc/python_headers.h
+++ torch/csrc/python_headers.h
@@ -1,4 +1,6 @@
 #pragma once
+// workaround for https://github.com/python/cpython/pull/23326
+#include <complex>
 #include <cmath>
 // workaround for Python 2 issue: https://bugs.python.org/issue17120
 // NOTE: It looks like this affects Python 3 as well.
@@ -13,6 +15,10 @@
 
 #pragma pop_macro("_XOPEN_SOURCE")
 #pragma pop_macro("_POSIX_C_SOURCE")
+
+#ifdef copysign
+#undef copysign
+#endif
 
 #if PY_MAJOR_VERSION < 3
 #error "Python 2 has reached end-of-life and is no longer supported by PyTorch."
