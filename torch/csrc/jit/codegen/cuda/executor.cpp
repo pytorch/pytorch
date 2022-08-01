d7f83bcad0c8aa8c6f10d9af87272e3c3f8a3793
Jeff Daily <jeff.daily@amd.com>
2022-08-01 23:30:58 +0000

    add unsigned short

--- torch/csrc/jit/codegen/cuda/executor.cpp
+++ torch/csrc/jit/codegen/cuda/executor.cpp
@@ -47,9 +47,10 @@ static const char* defineIndexMode(KernelIndexMode index_mode) {
 
 static const char* defineIntegerTypes() {
   return R"(
-typedef unsigned char uint8_t;
 typedef signed char int8_t;
+typedef unsigned char uint8_t;
 typedef short int int16_t;
+typedef unsigned short int uint16_t;
 typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef long long int int64_t;
