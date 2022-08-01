a7177a2c70135daebe0335c256c96c5ba377e8a3
Zecheng He <zechengh@fb.com>
2022-08-01 13:16:11 -0700

    [Quantization] Allow fuse function to take custom config (#82210)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82210
    
    ``fuse_modules`` takes ``fuse_custom_config_dict`` as an argument but does not pass it to the lower level functions.
    
    Test Plan: Easy fix
    
    Reviewed By: newstzpz
    
    Differential Revision: D38144060
    
    fbshipit-source-id: ca4b46e543eb698d0e85cc761745e3388611cbdf

--- torch/ao/quantization/fuse_modules.py
+++ torch/ao/quantization/fuse_modules.py
@@ -154,7 +154,7 @@ def fuse_modules(model, modules_to_fuse, inplace=False, fuser_func=fuse_known_mo
         is_qat=False,
         inplace=inplace,
         fuser_func=fuse_known_modules,
-        fuse_custom_config_dict=None)
+        fuse_custom_config_dict=fuse_custom_config_dict)
 
 def fuse_modules_qat(model, modules_to_fuse, inplace=False, fuser_func=fuse_known_modules, fuse_custom_config_dict=None):
     """ QAT version for `fuse_modules`
