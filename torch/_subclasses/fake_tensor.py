0ec43e6572517e5b6e5013e6642d98f0d71aef57
Elias Ellison <elias.ellison@gmail.com>
2022-08-01 16:14:50 -0700

    Update on "Fake: copy over grad attribute"
    Copy over the grad attribute... let me any suggestions of other tests/opinfos/crossrefs I should be doing to make this more comprehensive. 
    
    [ghstack-poisoned]

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -9,7 +9,7 @@ from typing import Callable, Union
 import torch
 import torch.fx.experimental.symbolic_shapes as symbolic_shapes
 from torch._ops import OpOverload
-from torch._subclasses.meta_utils import MetaConverter, safe_is_leaf, WeakTensorRefKey
+from torch._subclasses.meta_utils import MetaConverter, WeakTensorRefKey
 from torch.fx.operator_schemas import normalize_function
 from torch.overrides import TorchFunctionMode
 from torch.utils._mode_utils import no_dispatch
