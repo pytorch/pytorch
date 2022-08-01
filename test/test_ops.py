3d0b6ba44c57dbfc70e6fe832b84d3a763bc22c7
Elias Ellison <elias.ellison@gmail.com>
2022-08-01 18:02:11 +0000

    Add tests for aliasing in fake tensor (#82337)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82337
    Approved by: https://github.com/ezyang, https://github.com/bdhirsh

--- test/test_ops.py
+++ test/test_ops.py
@@ -1706,6 +1706,12 @@ sometimes_dynamic_output_op_test = (
     "index_select",
 )
 
+aliasing_failures = (
+    "histogramdd",
+    "nn.functional.pixel_shuffle",
+    "nn.functional.pixel_unshuffle",
+)
+
 
 @skipIfSlowGradcheckEnv
 class TestFakeTensorNonErroring(TestCase):
@@ -1741,6 +1747,16 @@ class TestFakeTensorNonErroring(TestCase):
                     with enable_torch_dispatch_mode(mode):
                         res_fake = op(input, *args, **kwargs)
 
+                def outputs_alias_inputs(outputs, inputs):
+                    input_storages = set()
+                    for out in tree_flatten(outputs)[0]:
+                        if isinstance(out, torch.Tensor):
+                            input_storages.add(out.storage()._cdata)
+                    for inp in tree_flatten(inputs)[0]:
+                        if isinstance(inp, torch.Tensor) and inp.storage()._cdata in input_storages:
+                            return True
+                    return False
+
                 for fake_out, real_out in zip(
                     tree_flatten(res_fake)[0], tree_flatten(res)[0]
                 ):
@@ -1752,6 +1768,11 @@ class TestFakeTensorNonErroring(TestCase):
                     # if you see a shape exception here, you may need to add
                     # a `dynamic_output_shape` tag to an operator
                     prims.utils.compare_tensor_meta(fake_out, real_out)
+
+                    if name not in aliasing_failures:
+                        fake_aliasing = outputs_alias_inputs((input, args, kwargs), res_fake)
+                        real_aliasing = outputs_alias_inputs((sample.input, sample, args, sample.kwargs), res)
+                        self.assertEqual(fake_aliasing, real_aliasing)
 
                 self.assertTrue(name not in dynamic_output_op_tests)
 
