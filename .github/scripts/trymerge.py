dc49d53bb897797e4d3b73486cc171be40e572b6
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-02 10:44:11 -0500

    fix up message again

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -888,9 +888,9 @@ class GitHubPR:
         commit = repo.get_commit('HEAD').commit_hash
         gh_post_pr_comment(self.org, self.project, self.pr_num,
                            '@pytorchbot successfully started a merge and created land time checks.' +
-                          f' See merge status [here]({RUN_URL}) ' +
-                          f'and [land check]({BOT_COMMANDS_WIKI}) '
-                          f'progress [here](https://hud.pytorch.org/{org}/{project}/commit/{commit}).')
+                           f' See merge status [here]({os.getenv("GH_RUN_URL")}) ' +
+                           f'and [land check]({BOT_COMMANDS_WIKI}) '
+                           f'progress [here](https://hud.pytorch.org/{self.org}/{self.project}/commit/{commit})')
         return commit
 
 
