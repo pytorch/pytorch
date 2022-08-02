ecdfc62666bd256adb2a56ba8b3710671a8718b3
atalman <atalman@fb.com>
2022-08-02 16:00:19 +0000

    Bumping nvidia docker version and using python 3.10 for cuda11.7 (#82472)
    ### Description
    CUDA 11.7 Requires Driver update : https://docs.nvidia.com/cuda/cuda-toolkit-release-notes/index.html
    This is only Linux update, will require followup PR for Windows update.
    
    This should resolve this error:
    https://github.com/pytorch/pytorch/runs/7478454141?check_suite_focus=true
    
    On this PR: https://github.com/pytorch/pytorch/pull/81688
    ```
    nvidia-container-cli: requirement error: unsatisfied condition: cuda>=11.7, please update your driver to a newer version, or use an earlier cuda container: unknown.
    ```
    
    ### Testing
    IN CI
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82472
    Approved by: https://github.com/DanilBaibak, https://github.com/malfet

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -151,7 +151,7 @@ case "$image" in
   pytorch-linux-bionic-cuda11.7-cudnn8-py3-gcc7)
     CUDA_VERSION=11.7.0
     CUDNN_VERSION=8
-    ANACONDA_PYTHON_VERSION=3.7
+    ANACONDA_PYTHON_VERSION=3.10
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
