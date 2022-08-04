c14878ebf53f24eb8be2d9856493ba732e906f26
albanD <desmaison.alban@gmail.com>
2022-08-04 17:07:06 -0400

    CI passed! Doing cleanup now!

--- .jenkins/pytorch/macos-common.sh
+++ .jenkins/pytorch/macos-common.sh
@@ -7,19 +7,21 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 sysctl -a | grep machdep.cpu
 
-# NOTE: mkl 2021.3.0+ cmake requires sub-command PREPEND, may break the build
 if [[ ${BUILD_ENVIRONMENT} = *arm64* ]]; then
+  # We use different versions here as the arm build/tests runs on python 3.9
+  # while the x86 one runs on python 3.8
   retry conda install -y \
-    numpy=1.22 \
-    pyyaml \
-    setuptools \
-    cmake \
+    numpy=1.22.3 \
+    pyyaml=6.0 \
+    setuptools=61.2.0 \
+    cmake=3.22.1 \
     cffi \
     ninja \
     typing_extensions \
     dataclasses \
     pip
 else
+  # NOTE: mkl 2021.3.0+ cmake requires sub-command PREPEND, may break the build
   retry conda install -y \
     mkl=2021.2.0 \
     mkl-include=2021.2.0 \
--- .jenkins/pytorch/macos-test.sh
+++ .jenkins/pytorch/macos-test.sh
@@ -6,9 +6,9 @@ source "$(dirname "${BASH_SOURCE[0]}")/macos-common.sh"
 
 conda install -y six
 if [[ ${BUILD_ENVIRONMENT} = *arm64* ]]; then
-  pip install -q hypothesis "expecttest==0.1.3" "librosa>=0.6.2" numba psutil scipy
+  pip install hypothesis "expecttest==0.1.3" "librosa>=0.6.2" numba psutil scipy
 else
-  pip install -q hypothesis "expecttest==0.1.3" "librosa>=0.6.2" "numba<=0.49.1" psutil "scipy==1.6.3"
+  pip install hypothesis "expecttest==0.1.3" "librosa>=0.6.2" "numba<=0.49.1" psutil "scipy==1.6.3"
 fi
 
 # TODO move this to docker
