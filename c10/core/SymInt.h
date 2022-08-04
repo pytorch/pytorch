c5924f42bb262b88252c74f211251e9b1e57f205
Nikolay Korovaiko <korovaikon@gmail.com>
2022-08-04 20:00:47 +0000

    add integer divison for symints (#82791)
    ### Description
    This PR brings integer division (floor) to symints + tests.
    
    ### Issue
    
    https://github.com/orgs/pytorch/projects/17/views/2
    
    ### Testing
    added two tests to TestPySymInts
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82791
    Approved by: https://github.com/ezyang

--- c10/core/SymInt.h
+++ c10/core/SymInt.h
@@ -87,6 +87,7 @@ class C10_API SymInt {
 
   SymInt operator+(SymInt sci) const;
   SymInt operator*(SymInt sci) const;
+  SymInt operator/(SymInt sci) const;
   bool operator==(SymInt sci) const;
   bool operator!=(SymInt p2) const;
   bool operator<(SymInt sci) const;
