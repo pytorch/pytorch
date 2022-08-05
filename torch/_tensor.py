f7d399655cb32745daa3422b02068c62213d92aa
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-08-05 01:51:39 +0000

    Revert "Add DLPack support for XPU backend by mapping to kDLOneAPI in DLPack. (#81021)"
    This reverts commit 8be853025cb9fe7dd165924957b01984b46b9459.
    
    Reverted https://github.com/pytorch/pytorch/pull/81021 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -1330,8 +1330,6 @@ class Tensor(torch._C._TensorBase):
             device_type = DLDeviceType.kDLGPU
         elif self.device.type == "cpu":
             device_type = DLDeviceType.kDLCPU
-        elif self.device.type == "xpu":
-            device_type = DLDeviceType.kDLOneAPI
         else:
             raise ValueError(
                 "Unknown device type {} for Dlpack".format(self.device.type)
