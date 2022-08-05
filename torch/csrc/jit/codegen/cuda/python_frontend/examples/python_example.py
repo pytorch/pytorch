f78dc483053b16553e0711f0cdb632c10facfdd7
Kevin Stephano <kevin.stephano@gmail.com>
2022-08-05 08:18:48 +0000

    Change example to have 2 definitions.

--- torch/csrc/jit/codegen/cuda/python_frontend/examples/python_example.py
+++ torch/csrc/jit/codegen/cuda/python_frontend/examples/python_example.py
@@ -1,10 +1,10 @@
 import torch
-from torch._C._nvfuser import Fusion, FusionDefinition, DataType
+from torch._C._nvfuser import FusionManager, FusionDefinition, DataType
 
 # Construct and Define Fusion
-fusion = Fusion()
+fmanager = FusionManager()
 
-with FusionDefinition(fusion) as fd :
+with FusionDefinition(fmanager) as fd :
     t0 = fd.define_tensor(3)
     t1 = fd.define_tensor(3)
     s0 = fd.define_scalar()
@@ -13,15 +13,13 @@ with FusionDefinition(fusion) as fd :
 
     t2 = fd.ops.add(t0, t1)
     t3 = fd.ops.mul(t2, c0)
-    t4 = fd.ops.atan2(t3, s0)
-    t5 = fd.ops.relu(t4)
-    t6 = fd.ops.sum(t5, [-1], False, DataType.Float)
-    t7 = fd.ops.isfinite(t6)
+    t4 = fd.ops.sum(t3, [-1], False, DataType.Float)
+    t5 = fd.ops.isfinite(t4)
 
-    fd.add_output(t6)
-    fd.add_output(t7)
+    fd.add_output(t4)
+    fd.add_output(t5)
 
-fusion.print_ir()
+fmanager.print_ir()
 
 # Execute Fusion
 input1 = torch.ones(2, 4, 8, device='cuda')
@@ -30,7 +28,28 @@ input2 = torch.ones(2, 4, 8, device='cuda')
 # Kernel compilation should be cached for the 2nd iteration
 # with input tensors of the same shape
 for _ in range(5) :
-    outputs = fusion.execute([input1, input2, 2.0])
+    outputs = fmanager.execute([input1, input2, 2.0])
 
 print(outputs[0])
 print(outputs[1])
+
+with FusionDefinition(fmanager) as fd :
+    t0 = fd.define_tensor(3)
+    t1 = fd.define_tensor(3)
+    s0 = fd.define_scalar()
+
+    c0 = fd.define_constant(3.0)
+
+    t2 = fd.ops.add(t0, t1)
+    t3 = fd.ops.mul(t2, c0)
+    t4 = fd.ops.sum(t3, [-1], False, DataType.Float)
+    t5 = fd.ops.isfinite(t4)
+
+    fd.add_output(t4)
+    fd.add_output(t5)
+
+input3 = torch.ones(2, 4, 8, device='cuda')
+input4 = torch.ones(2, 4, 8, device='cuda')
+
+for _ in range(5) :
+    outputs = fmanager.execute([input3, input4, 2.0])
