b9f25d9942db522df7cb890725cb11c124b7bfe2
richard <yueyericardo@gmail.com>
2022-08-05 14:25:38 -0700

    revert header posistion

--- torch/csrc/autograd/autograd.cpp
+++ torch/csrc/autograd/autograd.cpp
@@ -5,11 +5,12 @@
 #endif
 
 #include <torch/csrc/autograd/autograd.h>
+#include <torch/csrc/autograd/variable.h>
+
 #include <torch/csrc/autograd/edge.h>
 #include <torch/csrc/autograd/engine.h>
 #include <torch/csrc/autograd/function.h>
 #include <torch/csrc/autograd/functions/basic_ops.h>
-#include <torch/csrc/autograd/variable.h>
 
 #include <c10/util/irange.h>
 
