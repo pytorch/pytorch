bdb0abb234e4914d68a99abf06184b3926b0abf0
Natalia Gimelshein <ngimel@fb.com>
2022-08-05 03:26:54 +0000

    Fix profiling with record_shapes=True and nested tensor (#82854)
    We probably want more systematic fix for other tensorImpls that don't support sizes (and also there are other places in profiler that call sizes unchecked, which should be fixed), but this is to unblock Transformer profiling.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82854
    Approved by: https://github.com/robieta

--- test/test_profiler.py
+++ test/test_profiler.py
@@ -1162,6 +1162,22 @@ class TestProfiler(TestCase):
                     id_uniqueness_set.add(corr_id)
                     self.assertTrue(corr_id < uint32_max)
 
+    def test_nested_tensor_with_shapes(self):
+        a = torch.randn(4, 4)
+        b = torch.randn(4, 4)
+        c = torch.randn(4, 4)
+        inp = torch.nested_tensor([a, b])
+        with torch.profiler.profile(record_shapes=True) as prof:
+            torch.nn.functional.linear(inp, c, None)
+        for e in prof.events():
+            if e.name in ("aten::mm", "aten::addmm"):
+                # intentionally vague tests to protect against possible future changes
+                # of mm to addmm or other impl, or changing internal order of args
+                self.assertTrue(len(e.input_shapes) > 0)
+                self.assertTrue(len(e.input_shapes[0]) > 0)
+
+
+
 def find_node_with_name(nodes, name):
     for node in nodes:
         if node.name() == name:
