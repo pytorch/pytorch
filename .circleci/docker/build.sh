f769b016478873335323b0139a2f79a16b9231ce
Peter Bell <peterbell10@live.co.uk>
2022-08-05 16:46:30 +0100

    Add pre-compiled headers to one of the CI runners (#77351)
    Pre-compiled headers currently aren't tested in CI.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77351
    Approved by: https://github.com/janeyx99

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -262,7 +262,7 @@ case "$image" in
     ;;
   pytorch-linux-focal-py3.7-gcc7)
     ANACONDA_PYTHON_VERSION=3.7
-    CMAKE_VERSION=3.12.4  # To make sure XNNPACK is enabled for the BACKWARDS_COMPAT_TEST used with this image
+    CMAKE_VERSION=3.16.9  # Required for precompiled header support
     GCC_VERSION=7
     PROTOBUF=yes
     DB=yes
