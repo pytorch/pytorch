667c836b90c33985bb32bd2001d6f73ed9ebacbb
Edward Z. Yang <ezyang@fb.com>
2022-08-05 08:29:00 -0700

    Add EnableTorchFunction (#82647)
    If you DisableTorchFunction, as is done in the default __torch_function__
    implementation, if you want to reentrantly use TorchFunction (e.g., to
    trace FX proxies), you have to be able to turn it back on.
    enable_reentrant_dispatch does not work in this case because by the time
    we snapshot TLS, torch function is already disabled.
    
    Differential Revision: [D38354504](https://our.internmc.facebook.com/intern/diff/D38354504/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D38354504/)!
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82647
    Approved by: https://github.com/zou3519, https://github.com/albanD

--- test/test_overrides.py
+++ test/test_overrides.py
@@ -1532,6 +1532,20 @@ class TestTorchFunctionMode(TestCase):
 
         self.assertTrue(called)
 
+    def test_disable_enable_subclass(self):
+        called = False
+
+        class A(torch.Tensor):
+            pass
+
+        x = A(torch.randn(5))
+        with torch._C.DisableTorchFunction():
+            g = torch._C._EnableTorchFunction()
+            try:
+                self.assertIsInstance(torch.sum(x), A)
+            finally:
+                del g
+
 
 if __name__ == '__main__':
     run_tests()
