54fda46ba4d4f17de4f6466a7ae4856dd77b2f4a
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-05 15:27:05 -0500

    some stuff

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -6,15 +6,36 @@ import os
 import re
 import time
 import urllib.parse
-from datetime import datetime
 from dataclasses import dataclass
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-from typing import Iterable, Pattern, cast, Any, Callable, Dict, List, Optional, Tuple, Union
-from gitutils import get_git_remote_name, get_git_repo_dir, patterns_to_regex, GitRepo
+from datetime import datetime
 from functools import lru_cache
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Pattern,
+    Tuple,
+    Union,
+    cast,
+)
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
 from warnings import warn
 
+from gitutils import (
+    GitRepo,
+    get_git_remote_name,
+    get_git_repo_dir,
+    patterns_to_regex,
+)
+from trymerge_explainer import (
+    TryMergeExplainer,
+    get_land_check_troubleshooting_message,
+    print_revert_message,
+)
 
 GH_PR_REVIEWS_FRAGMENT = """
 fragment PRReviews on PullRequestReviewConnection {
@@ -1174,15 +1195,21 @@ def merge(pr_num: int, repo: GitRepo,
     org, project = repo.gh_owner_and_name()
     pr = GitHubPR(org, project, pr_num)
     initial_commit_sha = pr.last_commit()['oid']
+
     check_for_sev(org, project, force)
+
+    if land_checks:
+        land_check_commit = pr.create_land_time_check_branch(repo, 'viable/strict', force=force, comment_id=comment_id)
+
+    explainer = TryMergeExplainer(force, on_green, land_checks, pr, org, project, land_check_commit)
+    on_green, land_checks = explainer.get_flags()
+    explainer.print_merge_message(dry_run)
+
     if force or can_skip_internal_checks(pr, comment_id):
         # do not wait for any pending signals if PR is closed as part of co-development process
         return pr.merge_into(repo, dry_run=dry_run, force=force, comment_id=comment_id)
     if (datetime.utcnow() - pr.last_pushed_at()).days > stale_pr_days:
         raise RuntimeError("This PR is too stale; the last push date was more than 3 days ago. Please rebase and try again.")
-
-    if land_checks:
-        land_check_commit = pr.create_land_time_check_branch(repo, 'viable/strict', force=force, comment_id=comment_id)
 
     start_time = time.time()
     last_exception = ''
@@ -1228,64 +1255,26 @@ def merge(pr_num: int, repo: GitRepo,
         gh_add_labels(org, project, pr_num, ["land-failed"])
     raise RuntimeError(msg)
 
-def get_msg_template(prefix, explaination):
-    return f"{prefix}. {explaination}"
-
-def get_flag_prefix(flag):
-    return f"The merge job was triggered with the {flag} flag."
-
-def get_flag_explaination_message(force, on_green, land_checks, has_trunk_label, has_ciflow_label):
-    if force:
-        return get_msg_template(get_flag_prefix("-f"),
-                                "This means your PR will be merged immediately, bypassing any checks")
-    elif on_green:
-        return get_msg_template(get_flag_prefix("-g"),
-                                "This means that your PR will be merged once all signals on your PR has passed.")
-    elif land_checks:
-        if has_trunk_label:
-            land_check_msg_suffix = f"have run since you have the {CIFLOW_TRUNK_LABEL}"
-        else:
-            land_check_msg_suffix = "and the land checks branch have run."
-        return get_msg_template(get_flag_prefix('-l'),
-                                "This means that your PR will be merged once all signals on your PR " +
-                                land_check_msg_suffix
-                                )
-    else:
-        if has_ciflow_label:
-            normal_msg_suffix = "Since your PR has a ciflow label, we will wait for all checks to be green on your PR."
-        else:
-            normal_msg_suffix = "Merge bot will wait for mandatory checks defined in the merge rules to be green before merging."
-        return get_msg_template("The merge job was triggered with no flags.", normal_msg_suffix)
-
-
 def main() -> None:
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
     pr = GitHubPR(org, project, args.pr_num)
-    land_checks = args.land_checks and not has_label(pr.get_labels(), CIFLOW_TRUNK_LABEL)
-    on_green = args.on_green or has_label(pr.get_labels(), CIFLOW_LABEL)
 
     def handle_exception(e: Exception, msg: str = "Merge failed") -> None:
         msg += f" due to {e}"
         run_url = os.getenv("GH_RUN_URL")
         if run_url is not None:
             msg += f"\nRaised by {run_url}"
-        if land_checks:
-            msg += (" If you believe this is an error, you can use the old behavior with `@pytorchbot merge -g`" +
-                    ' (optionally with the "ciflow/trunk" to get land signals)' +
-                    ' or use `@pytorchbot merge -f "some reason here"`.' +
-                    f" For more information, see the [bot wiki]({BOT_COMMANDS_WIKI}).")
+        if args.land_checks:
+            msg += get_land_check_troubleshooting_message()
         gh_post_pr_comment(org, project, args.pr_num, msg, dry_run=args.dry_run)
         import traceback
         traceback.print_exc()
-    if not land_checks:
-        msg = f"@pytorchbot successfully started a {'revert' if args.revert else 'merge'} job."
-        msg += f" Check the current status [here]({os.getenv('GH_RUN_URL')})."
-        gh_post_pr_comment(org, project, args.pr_num, msg, dry_run=args.dry_run)
 
     if args.revert:
         try:
+            print_revert_message(org, project, args.pr_num, args.dry_run)
             try_revert(repo, pr, dry_run=args.dry_run, comment_id=args.comment_id, reason=args.reason)
         except Exception as e:
             handle_exception(e, f"Reverting PR {args.pr_num} failed")
@@ -1304,9 +1293,9 @@ def main() -> None:
               dry_run=args.dry_run,
               force=args.force,
               comment_id=args.comment_id,
-              on_green=on_green,
+              on_green=args.on_green,
               mandatory_only=args.on_mandatory,
-              land_checks=land_checks)
+              land_checks=args.land_checks)
     except Exception as e:
         handle_exception(e)
 
---
+++ .github/scripts/trymerge_explainer.py
@@ -0,0 +1,127 @@
+import os
+import re
+from typing import List, Pattern, Tuple
+
+from trymerge import BOT_COMMANDS_WIKI, GitHubPR, gh_post_pr_comment
+
+CIFLOW_LABEL = re.compile(r"^ciflow/.+")
+CIFLOW_TRUNK_LABEL = re.compile(r"^ciflow/trunk")
+
+
+def has_label(labels: List[str], pattern: Pattern[str] = CIFLOW_LABEL) -> bool:
+    return len(list(filter(pattern.match, labels))) > 0
+
+
+class TryMergeExplainer(object):
+    force: bool
+    on_green: bool
+    land_checks: bool
+    pr: GitHubPR
+    org: str
+    project: str
+    commit: str
+
+    has_trunk_label: bool
+    has_ciflow_label: bool
+
+    def __init__(
+        self,
+        force: bool,
+        on_green: bool,
+        land_checks: bool,
+        pr: GitHubPR,
+        org: str,
+        project: str,
+        commit: str,
+    ):
+        self.force = force
+        self.on_green = on_green
+        self.land_checks = land_checks
+        self.pr = pr
+        self.org = org
+        self.project = project
+        self.commit = commit
+
+    def get_flags(self) -> Tuple[bool, bool]:
+        self.has_trunk_label = has_label(self.pr.get_labels(), CIFLOW_TRUNK_LABEL)
+        self.has_ciflow_label = has_label(self.pr.get_labels(), CIFLOW_LABEL)
+        should_check_land_branch = self.land_checks and not self.has_trunk_label
+        should_check_green = self.on_green or self.has_ciflow_label
+
+        return (should_check_green, should_check_land_branch)
+
+    def _get_flag_msg(self) -> str:
+        if self.force:
+            return " the force (-f)"
+        elif self.on_green:
+            return " the green (-g)"
+        elif self.land_checks:
+            return " the land check (-l)"
+        else:
+            return "out a "
+
+    def _get_land_check_progress(self) -> str:
+        if self.commit != None:
+            return (
+                f" and [land check]({BOT_COMMANDS_WIKI}) "
+                + f"progress [here](https://hud.pytorch.org/{self.org}/{self.project}/commit/{self.commit})"
+            )
+        else:
+            return ""
+
+    def _get_flag_explaination_message(self) -> str:
+        if self.force:
+            return (
+                "This means your PR will be merged immediately, bypassing any checks."
+            )
+        elif self.on_green:
+            return "This means that your PR will be merged once all signals on your PR has passed."
+        elif self.land_checks:
+            if self.has_trunk_label:
+                land_check_msg_suffix = (
+                    f"have run since you have the {CIFLOW_TRUNK_LABEL}."
+                )
+            else:
+                land_check_msg_suffix = (
+                    "and the land checks branch have run (ETA 4 Hours)."
+                )
+            return (
+                "This means that your PR will be merged once all signals on your PR "
+                + land_check_msg_suffix
+            )
+
+        else:
+            if self.has_ciflow_label:
+                normal_msg_suffix = "Since your PR has a ciflow label, we will wait for all checks to be green on your PR."
+            else:
+                normal_msg_suffix = "Merge bot will wait for mandatory checks defined in the merge rules to be green before merging."
+            return "The merge job was triggered with no flags." + normal_msg_suffix
+
+    def print_merge_message(self, dry_run: bool) -> None:
+        message_prefix = f"@pytorchbot successfully started a merge job."
+        progress_links = f"Check the current status [here]({os.getenv('GH_RUN_URL')}){self._get_land_check_progress()}."
+        flag_message = f"The merge job was triggered with{self._get_flag_msg()} flag."
+        explaination_message = self._get_flag_explaination_message()
+
+        msg = message_prefix + " "
+        msg += progress_links + "\n"
+        msg += flag_message + " "
+        msg += explaination_message
+        gh_post_pr_comment(self.org, self.project, self.pr.pr_num, msg, dry_run)
+
+
+def print_revert_message(org: str, project: str, pr_num: int, dry_run: bool) -> None:
+    msg = (
+        "@pytorchbot successfully started a revert job."
+        + f"Check the current status [here]({os.getenv('GH_RUN_URL')})"
+    )
+    gh_post_pr_comment(org, project, pr_num, msg, dry_run)
+
+
+def get_land_check_troubleshooting_message() -> str:
+    return (
+        " If you believe this is an error, you can use the old behavior with `@pytorchbot merge -g`"
+        + ' (optionally with the "ciflow/trunk" to get land signals)'
+        + ' or use `@pytorchbot merge -f "some reason here"`.'
+        + f" For more information, see the [bot wiki]({BOT_COMMANDS_WIKI})."
+    )
