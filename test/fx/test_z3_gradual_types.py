b26c9ac63ee00f8e7cd92759ce72e6192ae3cbc4
migeedz <migeedz@fb.com>
2022-08-06 13:30:28 -0700

    add the operations needed for electra model (#82856)
    - Add equality constraints and dimension constraints
    - Bypass an error for getitem(not relavant to control-flow but needs to be revisited later)
    - Add test-cases.
    - Full test-case for model can be seen here https://github.com/migeed-z/transformers/pull/3/files
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82856
    Approved by: https://github.com/jansel

--- test/fx/test_z3_gradual_types.py
+++ test/fx/test_z3_gradual_types.py
@@ -60,6 +60,29 @@ class TorchDynamoUseCases(unittest.TestCase):
 
 class HFOperations(unittest.TestCase):
 
+    def test_eq_dim(self):
+        """
+        test dimensions and equalities
+        """
+        class BasicBlock(torch.nn.Module):
+            def __init__(self):
+                super(BasicBlock, self).__init__()
+
+            def forward(self, x: TensorType([32, 4, 4])):
+                eq = x.dim() == 3
+                return eq
+
+        ast_rewriter = RewritingTracer()
+        graph = ast_rewriter.trace(BasicBlock())
+
+        # The node we are considering is the gt node
+        for n in graph.nodes:
+            if n.target == operator.eq:
+                node = n
+
+        positive, negative = evaluate_conditional_with_constraints(ast_rewriter.root, graph, node)
+        self.assertEqual(positive, z3.sat)
+        self.assertEqual(negative, z3.unsat)
 
     def test_conditional_ne_1(self):
         """
