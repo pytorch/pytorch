a999bfebf14736940c5850956bf4a9c49c355445
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-08-09 05:58:33 +0000

    Update base for Update on "[DO NOT MERGE] Testing add.Tensor kernel registered for AutogradNestedTensor in derivatives.yaml works"
    [ghstack-poisoned]

--- tools/autograd/gen_trace_type.py
+++ tools/autograd/gen_trace_type.py
@@ -465,9 +465,9 @@ def type_wrapper_name(f: NativeFunction, key: str = "Default") -> str:
         name = cpp.name(f.func)
 
     # The key argument is only used in gen_variable_type where we need fns per autograd dispatch key.
-    # In gen_trace_type and gen_inplace_type where only one fn per native_fn must be generated,
+    # In gen_trace_type and gen_inplace_view_type where only one fn per native_fn must be generated,
     # the key argument should not be passed.
-    # We do not append key if dispatch key is Default so that generated functions from
+    # We do not append key if it is Default so that generated functions from
     # before per-dispatch-key derivatives were added retain the same names.
     if key != "Default":
         name = name + f"_{key}"
