82c4d9457909f5ee6272cfcdd472872045fe9a89
Jane Xu <janeyx@fb.com>
2022-08-09 14:07:43 -0400

    Testing yet another time

--- .ci/pytorch/common-build.sh
+++ .ci/pytorch/common-build.sh
@@ -31,16 +31,16 @@ if [[ "$BUILD_ENVIRONMENT" != *win-* ]]; then
     sccache --zero-stats
     function sccache_epilogue() {
         echo "::group::Sccache Compilation Log"
-        echo '=================== sccache compilation log ==================='
+        # echo '=================== sccache compilation log ==================='
         # TODO: see test.sh line 503 to remove fallback in a few weeks for the bc test
         # if [[ -f "$script_dir/print_sccache_log.py" ]]; then
         #     python "$script_dir/print_sccache_log.py" ~/sccache_error.log
         # else
         #     python ".jenkins/pytorch/print_sccache_log.py" ~/sccache_error.log
         # fi
-        echo '=========== If your build fails, please take a look at the log above for possible reasons ==========='
-        sccache --show-stats
-        sccache --stop-server || true
+        # echo '=========== If your build fails, please take a look at the log above for possible reasons ==========='
+        # sccache --show-stats
+        # sccache --stop-server || true
         echo "::endgroup::"
     }
 
