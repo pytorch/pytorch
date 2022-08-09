42cec929d237f6f55eab3416c2318a7d9277463e
Nikita Shulga <nshulga@fb.com>
2022-08-09 12:08:39 -0700

    Remove unbalanced `pragma diagnostic pop`
    Detected by internal CI, we should have it in OSS as well:
    ```
    aten/src/ATen/native/cpu/Loops.h:398:24: error: pragma diagnostic pop could not pop, no matching push [-Werror,-Wunknown-pragmas]
    #pragma GCC diagnostic pop
    ```

--- aten/src/ATen/native/cpu/Loops.h
+++ aten/src/ATen/native/cpu/Loops.h
@@ -394,6 +394,3 @@ void cpu_serial_kernel_vec(TensorIteratorBase& iter, func_t&& op, vec_func_t&& v
 
 }}}  // namespace at::native::<anonymous>
 
-#ifndef _MSC_VER
-#pragma GCC diagnostic pop
-#endif
