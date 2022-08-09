42e847459990e4a2a1ee6ef8f71fc487963a0ce9
Yi Wang <yi.wang.2005@gmail.com>
2022-08-09 12:52:58 -0700

    Details the distributed.pipeline.sync.Pipe example

--- torch/distributed/pipeline/sync/pipe.py
+++ torch/distributed/pipeline/sync/pipe.py
@@ -269,18 +269,20 @@ class Pipe(Module):
     Example::
         Pipeline of two FC layers across GPUs 0 and 1.
 
-        >>> # Need to initialize RPC framework first.
-        >>> os.environ['MASTER_ADDR'] = 'localhost'
-        >>> os.environ['MASTER_PORT'] = '29500'
-        >>> torch.distributed.rpc.init_rpc('worker', rank=0, world_size=1)
-        >>>
-        >>> # Build pipe.
-        >>> fc1 = nn.Linear(16, 8).cuda(0)
-        >>> fc2 = nn.Linear(8, 4).cuda(1)
-        >>> model = nn.Sequential(fc1, fc2)
-        >>> model = Pipe(model, chunks=8)
-        >>> input = torch.rand(16, 16).cuda(0)
-        >>> output_rref = model(input)
+        # Need to initialize RPC framework first.
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '29500'
+        # Here we use one Python proccess to build a pipe over two GPUs. 
+        # The world_size refers to the one process.
+        torch.distributed.rpc.init_rpc('worker', rank=0, world_size=1)
+        
+        # Build pipe on two GPUs.
+        fc1 = nn.Linear(16, 8).cuda(0)
+        fc2 = nn.Linear(8, 4).cuda(1)
+        model = nn.Sequential(fc1, fc2)
+        model = Pipe(model, chunks=8)
+        input = torch.rand(16, 16).cuda(0)
+        output_rref = model(input)
 
     .. note::
         You can wrap a :class:`Pipe` model with
