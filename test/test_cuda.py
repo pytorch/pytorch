f8ebc5ad6ac1cd046faa46032fb093b5caa43bab
Masaki Kozuki <mkozuki@nvidia.com>
2022-08-09 15:04:30 -0700

    deliberately copy kwargs
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -4,6 +4,7 @@ from itertools import repeat, chain, product
 from typing import NamedTuple
 import collections
 import contextlib
+import copy
 import ctypes
 import gc
 import io
@@ -2361,7 +2362,7 @@ torch.cuda.synchronize()
         (
             mod_control, mod_scaling, opt_control, opt_scaling, data, loss_fn, _,
         ) = self._create_scaling_case(optimizer_ctor=optimizer_ctor, optimizer_kwargs=optimizer_kwargs)
-        kwargs = optimizer_kwargs
+        kwargs = copy.deepcopy(optimizer_kwargs)
         kwargs["fused"] = False
         opt_control = optimizer_ctor(mod_control.parameters(), lr=1.0, **kwargs)
 
