d3a1f17fc7e4ea7c77691b9a7df55cc3f292eee1
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-08-10 10:27:59 +0000

    Revert "[BE] Add `append_cxx_flag_if_supported` macro (#82883)"
    This reverts commit d7e6aaa59b4660876d3d9bbee50113fad2c0b115.
    
    Reverted https://github.com/pytorch/pytorch/pull/82883 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally

--- aten/src/ATen/native/cpu/Loops.h
+++ aten/src/ATen/native/cpu/Loops.h
@@ -36,6 +36,11 @@
 #include <ATen/native/TensorIteratorDynamicCasting.h>
 #include <ATen/cpu/vec/vec.h>
 
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+#endif
+
 namespace at { namespace native { inline namespace CPU_CAPABILITY {
 
 using namespace vec;
