ee6bb125a3144d630be733d02b2e80a9e1abde90
soulitzer (Meta Employee) <soulitzer@gmail.com>
2022-08-10 13:49:07 -0700

    Add forward AD support for elu_, celu_, selu_ (#83080) (#83080)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83080
    Approved by: https://github.com/albanD
    
    Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/b55f9047e14743c078c8458f71d657aabb286c62
    
    Reviewed By: seemethere
    
    Differential Revision: D38585618
    
    Pulled By: soulitzer
    
    fbshipit-source-id: 2b1a10d33bad3592f5adb91c722388ffb8430a69

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -12840,8 +12840,6 @@ op_db: List[OpInfo] = [
         inplace_variant=lambda x, alpha=1.0:
             torch.nn.functional.elu(x, alpha, inplace=True),
         decorators=[
-            # Not implemented yet
-            DecorateInfo(unittest.expectedFailure, 'TestGradients', 'test_inplace_forward_mode_AD'),
             DecorateInfo(
                 toleranceOverride({
                     torch.float16: tol(atol=1e-03, rtol=1.2e-03),
@@ -12895,8 +12893,6 @@ op_db: List[OpInfo] = [
         inplace_variant=lambda x, alpha=1.0:
             torch.nn.functional.celu(x, alpha, inplace=True),
         decorators=[
-            # Not implemented yet
-            DecorateInfo(unittest.expectedFailure, 'TestGradients', 'test_inplace_forward_mode_AD'),
             DecorateInfo(
                 toleranceOverride({
                     torch.float16: tol(atol=1e-03, rtol=1.2e-03),
@@ -12959,8 +12955,6 @@ op_db: List[OpInfo] = [
         supports_out=False,
         inplace_variant=lambda x: torch.nn.functional.selu(x, inplace=True),
         decorators=[
-            # Not implemented yet (depends on 'elu_')
-            DecorateInfo(unittest.expectedFailure, 'TestGradients', 'test_inplace_forward_mode_AD'),
             DecorateInfo(
                 toleranceOverride({
                     torch.float16: tol(atol=1e-2, rtol=1.8e-2),
