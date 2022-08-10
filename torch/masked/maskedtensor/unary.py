591e5474a5a99475c3aadc1dd2b8cd17a3d92597
George Qi <georgeqi94@gmail.com>
2022-08-10 02:01:05 +0000

    Update base for Update on "[maskedtensor] add basic tests and unary/binary/reduction tests from common_method_invocations"
    [ghstack-poisoned]

--- torch/masked/maskedtensor/unary.py
+++ torch/masked/maskedtensor/unary.py
@@ -101,72 +101,62 @@ UNARY_NAMES_UNSUPPORTED = [
 ]
 
 
-def torch_unary(fn_name):
-    fn = getattr(torch.ops.aten, fn_name)
+def _unary_helper(fn, args, kwargs, inplace):
     from .passthrough import _map_mt_args_kwargs, _wrap_result
 
-    def unary_fn(*args, **kwargs):
-        assert len(kwargs) == 0
-        if len(args) > 1:
-            for a in args[1:]:
-                assert not torch.is_tensor(a)
-        mask_args, mask_kwargs = _map_mt_args_kwargs(
-            args, kwargs, lambda x: x.masked_mask
-        )
-        data_args, data_kwargs = _map_mt_args_kwargs(
-            args, kwargs, lambda x: x.masked_data
-        )
-        if args[0].layout() == torch.sparse_coo:
-            data_args[0] = data_args[0].coalesce()
-            s = data_args[0].size()
-            i = data_args[0].indices()
-            data_args[0] = data_args[0].coalesce().values()
-            v = fn(*data_args)
-            result_data = torch.sparse_coo_tensor(i, v, size=s)
-        elif args[0].layout() == torch.sparse_csr:
-            crow = data_args[0].crow_indices()
-            col = data_args[0].col_indices()
-            data_args[0] = data_args[0].values()
-            v = fn(*data_args)
-            result_data = torch.sparse_csr_tensor(crow, col, v)
-        else:
-            result_data = fn(*data_args)
+    if len(kwargs) != 0:
+        raise ValueError("MaskedTensor unary ops require that len(kwargs) != 0. If you need support for this, please open an issue on Github.")
+    if len(args) > 1:
+        for a in args[1:]:
+            if torch.is_tensor(a):
+                raise TypeError("MaskedTensor unary ops do not support Tensor arguments")
+
+    mask_args, mask_kwargs = _map_mt_args_kwargs(
+        args, kwargs, lambda x: x._masked_mask
+    )
+    data_args, data_kwargs = _map_mt_args_kwargs(
+        args, kwargs, lambda x: x._masked_data
+    )
+
+    if args[0].layout() == torch.sparse_coo:
+        data_args[0] = data_args[0].coalesce()
+        s = data_args[0].size()
+        i = data_args[0].indices()
+        data_args[0] = data_args[0].coalesce().values()
+        v = fn(*data_args)
+        result_data = torch.sparse_coo_tensor(i, v, size=s)
+
+    elif args[0].layout() == torch.sparse_csr:
+        crow = data_args[0].crow_indices()
+        col = data_args[0].col_indices()
+        data_args[0] = data_args[0].values()
+        v = fn(*data_args)
+        result_data = torch.sparse_csr_tensor(crow, col, v)
+
+    else:
+        result_data = fn(*data_args)
+
+    if inplace:
+        args[0]._set_data_mask(result_data, mask_args[0])
+        return args[0]
+    else:
         return _wrap_result(result_data, mask_args[0])
+
+
+def torch_unary(fn_name):
+    fn = getattr(torch.ops.aten, fn_name)
+
+    def unary_fn(*args, **kwargs):
+        return _unary_helper(fn, args, kwargs, inplace=False)
 
     return unary_fn
 
 
 def torch_inplace_unary(fn_name):
     fn = getattr(torch.ops.aten, fn_name)
-    from .passthrough import _map_mt_args_kwargs
 
     def unary_fn(*args, **kwargs):
-        assert len(kwargs) == 0
-        if len(args) > 1:
-            for a in args[1:]:
-                assert not torch.is_tensor(a)
-        mask_args, mask_kwargs = _map_mt_args_kwargs(
-            args, kwargs, lambda x: x.masked_mask
-        )
-        data_args, data_kwargs = _map_mt_args_kwargs(
-            args, kwargs, lambda x: x.masked_data
-        )
-        if args[0].layout() == torch.sparse_coo:
-            s = data_args[0].size()
-            i = data_args[0].indices()
-            data_args[0] = data_args[0].values()
-            v = fn(*data_args)
-            result_data = torch.sparse_coo_tensor(i, v, size=s)
-        elif args[0].layout() == torch.sparse_csr:
-            crow = data_args[0].crow_indices()
-            col = data_args[0].col_indices()
-            data_args[0] = data_args[0].values()
-            v = fn(*data_args)
-            result_data = torch.sparse_csr_tensor(crow, col, v)
-        else:
-            result_data = fn(*data_args)
-        args[0]._set_data_mask(result_data, mask_args[0])
-        return args[0]
+        return _unary_helper(fn, args, kwargs, inplace=True)
 
     return unary_fn
 
