beb4d0889634c8a7539360db514896f7ddce4cd6
Justin Chu <justinchu@microsoft.com>
2022-08-11 18:46:51 +0000

    [ONNX] Update the default opset
    Update the default opset by running the `update_default_opset_version.py` script
    
    [ghstack-poisoned]

--- torch/onnx/_constants.py
+++ torch/onnx/_constants.py
@@ -1,7 +1,7 @@
 """Constant values used in ONNX."""
 
 ONNX_ARCHIVE_MODEL_PROTO_NAME = "__MODEL_PROTO"
-onnx_default_opset = 13
+onnx_default_opset = 14
 onnx_main_opset = 16
 onnx_stable_opsets = tuple(range(7, onnx_main_opset))
 onnx_constant_folding_opsets = tuple(range(9, onnx_main_opset + 1))
