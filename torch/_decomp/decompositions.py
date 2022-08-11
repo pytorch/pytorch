74bfda5de9739920c550f0437b36dd181abca375
Horace He <chilli@fb.com>
2022-08-11 07:27:09 +0000

    Added nll_loss_forward decomposition + some other minor decomps
    [ghstack-poisoned]

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -1376,3 +1376,88 @@ def upsample_bilinear2d_vec(
     q2 = torch.mul(v3, xscale1) + torch.mul(v4, xscale2)
     result = torch.mul(q1, yscale1) + torch.mul(q2, yscale2)
     return result
+
+
+# We should be applying decompositions after all transformations
+@register_decomposition(aten.is_same_size.default)
+def is_same_size(a: Tensor, b: Tensor) -> bool:
+    return a.shape == b.shape
+
+
+@register_decomposition(aten.nll_loss_forward)
+def nll_loss_forward(
+    self: Tensor,
+    target: Tensor,
+    weight: Optional[Tensor],
+    reduction: int,
+    ignore_index: int,
+) -> Tuple[Tensor, Tensor]:
+    assert self.dim() > 0 and self.dim() <= 2, "input tensor should be 1D or 2D"
+    assert (
+        target.dim() <= 1
+    ), "0D or 1D target tensor expected, multi-target not supported"
+
+    no_batch_dim = self.dim() == 1 and target.dim() == 0
+    assert no_batch_dim or (
+        self.shape[0] == target.shape[0]
+    ), f"size mismatch (got input: {self.shape}, target: {target.shape})"
+
+    n_classes = self.shape[-1]
+
+    assert weight is None or (
+        weight.dim() == 1 and weight.numel() == n_classes
+    ), f"weight tensor should be defined either for all {n_classes} classes or no classes but got weight tensor of shape: {weight.shape}"  # noqa: B950
+
+    # self can be [N, C] or [C]
+    # target can be [N] or []
+
+    n_dims = self.dim()
+    channel_dim = 1
+    if n_dims < 2:
+        channel_dim = 0
+
+    if weight is not None:
+        # Here is a specific case with reduction mean and non-batched tensors
+        # https://github.com/pytorch/pytorch/issues/61309
+        # In this case weight is cancelled: w * x[t] / w -> x[t]
+        if not (reduction == Reduction.MEAN.value and n_dims < 2):
+            w = weight.unsqueeze(0) if n_dims > 1 else weight
+            self = self * w
+
+    target_ = target.unsqueeze(channel_dim)
+    # target can be [N, 1] or [1]
+
+    result = -torch.gather(self, channel_dim, target_).squeeze(channel_dim)
+
+    ignore_index_mask = None
+    if ignore_index >= 0:
+        ignore_index_mask = target != ignore_index
+        result = result * ignore_index_mask
+
+    if reduction == Reduction.NONE.value and n_dims > 1:
+        total_weight = self.new_full((), 0.0)
+        return result, total_weight
+
+    if weight is not None:
+        w = weight.unsqueeze(0).expand(self.shape) if n_dims > 1 else weight
+        wsum = torch.gather(w, channel_dim, target_).squeeze(channel_dim)
+        if ignore_index_mask is not None:
+            wsum = wsum * ignore_index_mask
+        total_weight = wsum.sum()
+    elif ignore_index_mask is not None:
+        total_weight = ignore_index_mask.sum().to(self)
+    else:
+        total_weight = self.new_full((), 1.0 * result.numel())
+
+    if result.dim() > 0:
+        if reduction == Reduction.SUM.value:
+            result = result.sum()
+        elif reduction == Reduction.MEAN.value:
+            if weight is None:
+                result = (
+                    result.sum() / total_weight if ignore_index >= 0 else result.mean()
+                )
+            else:
+                result = result.sum() / total_weight
+
+    return result, total_weight
