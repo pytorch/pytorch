c1857d4a61e34685b781134f30e5295cac21ed04
soulitzer <soulitzer@gmail.com>
2022-08-10 23:09:02 -0400

    Rename PyFunctionPreHook to PyFunctionTensorPreHook (#83225)
    Now that there will be two types of Python function prehooks, I prefer have the PyFunction hook taking all grad_outputs and returning all grad_inputs as the more "canonical" one
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83225
    Approved by: https://github.com/albanD

--- torch/csrc/autograd/python_hook.h
+++ torch/csrc/autograd/python_hook.h
@@ -7,9 +7,9 @@
 namespace torch {
 namespace autograd {
 
-struct PyFunctionPreHook : public FunctionPreHook {
-  PyFunctionPreHook(PyObject* dict, int value_idx);
-  ~PyFunctionPreHook() override;
+struct PyFunctionTensorPreHook : public FunctionPreHook {
+  PyFunctionTensorPreHook(PyObject* dict, int value_idx);
+  ~PyFunctionTensorPreHook() override;
   variable_list operator()(const variable_list& values) override;
   PyObject* dict;
   int value_idx;
