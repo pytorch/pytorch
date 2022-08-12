c26dfeabc71b0b7ed32328fc7c0aa9dda48f84cc
Huy Do <huydhn@gmail.com>
2022-08-12 20:01:15 +0000

    Switch to the new Windows AMI with conda preinstalled (#83188)
    Miniconda3 has been installed in the Windows AMI (ami-04a25a5bcec5bc2bf) with all the build and test dependencies in https://github.com/fairinternal/pytorch-gha-infra/pull/41
    
    ### Testing
    All Windows build and test jobs pass on canary https://github.com/pytorch/pytorch-canary/pull/134
    
    ### Issue
    #83003
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83188
    Approved by: https://github.com/atalman

--- .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
@@ -4,24 +4,32 @@ if "%BUILD_ENVIRONMENT%"=="" (
   set CONDA_PARENT_DIR=C:\Jenkins
 )
 
-if "%REBUILD%"=="" set INSTALL_FRESH_CONDA=1
-if NOT "%BUILD_ENVIRONMENT%"==""  set INSTALL_FRESH_CONDA=1
+
+:: Be conservative here when rolling out the new AMI with conda. This will try
+:: to install conda as before if it couldn't find the conda installation. This
+:: can be removed eventually after we gain enough confidence in the AMI
+if not exist %CONDA_PARENT_DIR%\Miniconda3 (
+  set INSTALL_FRESH_CONDA=1
+)
 
 if "%INSTALL_FRESH_CONDA%"=="1" (
-  IF EXIST %CONDA_PARENT_DIR%\Miniconda3 ( rd /s /q %CONDA_PARENT_DIR%\Miniconda3 )
   curl --retry 3 -k https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
+
   %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
 )
 
+:: Activate conda so that we can use its commands, i.e. conda, python, pip
 call %CONDA_PARENT_DIR%\Miniconda3\Scripts\activate.bat %CONDA_PARENT_DIR%\Miniconda3
+
 if "%INSTALL_FRESH_CONDA%"=="1" (
-  call conda install -y -q python=%PYTHON_VERSION% numpy"<1.23" cffi pyyaml boto3 libuv
+  call conda install -y -q numpy"<1.23" cffi pyyaml boto3 libuv
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
+
   call conda install -y -q -c conda-forge cmake=3.22.3
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
