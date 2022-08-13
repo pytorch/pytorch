af7d1e1887eac0328c719cd91879024c54b54b0f
Edward Z. Yang <ezyang@fb.com>
2022-08-12 21:06:19 -0700

    Respect TorchDispatchMode for shallow_copy_and_detach
    I noticed I was missing tensor creations with modes when I tried
    to delete proxy tensor.  This was the cause.
    
    Hypothetically, all PyInterpreter calls could get this treatment.
    But I think it only matters for detach; the rest do not return
    Tensors and most modes will not be interested in them.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- torch/csrc/autograd/python_variable.cpp
+++ torch/csrc/autograd/python_variable.cpp
@@ -2140,7 +2140,8 @@ py::object torchDispatchFromTensorImpl(
       c10::intrusive_ptr<c10::TensorImpl, c10::UndefinedTensorImpl>::
           unsafe_reclaim_from_nonowning(const_cast<c10::TensorImpl*>(self)));
   auto self_p = py::reinterpret_steal<py::object>(THPVariable_Wrap(self_t));
-  TORCH_INTERNAL_ASSERT(isPythonTensor(self_t));
+  // NB: this may not be a python tensor if you got here from a mode!
+  // TORCH_INTERNAL_ASSERT(isPythonTensor(self_t));
   append_overloaded_tensor(&overloaded_args, self_p.ptr());
   auto args = py::reinterpret_steal<py::object>(PyTuple_New(1));
   PyTuple_SET_ITEM(args.ptr(), 0, self_p.release().ptr());
