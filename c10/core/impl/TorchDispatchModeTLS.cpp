a83b0512a6a05e8c2cb83ffd8599b7db778e2124
Edward Z. Yang <ezyang@fb.com>
2022-08-12 21:09:17 -0700

    Update on "Move TorchDispatchModeTLS to c10/core"
    I need to access it directly from TensorImpl to route directly
    TensorImpl induced operations to modes (upcoming PR).
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- c10/core/impl/TorchDispatchModeTLS.cpp
+++ c10/core/impl/TorchDispatchModeTLS.cpp
@@ -1,16 +1,18 @@
-#include <c10/core/impl/TorchDispatchModeTLS.h>
-#include <c10/core/SafePyObject.h>
 #include <c10/core/DispatchKeySet.h>
+#include <c10/core/SafePyObject.h>
 #include <c10/core/impl/LocalDispatchKeySet.h>
+#include <c10/core/impl/TorchDispatchModeTLS.h>
 
-namespace c10 { namespace impl {
+namespace c10 {
+namespace impl {
 
 thread_local std::shared_ptr<SafePyObject> torchDispatchModeState;
 
 void TorchDispatchModeTLS::set_state(std::shared_ptr<SafePyObject> state) {
   if (state) {
     c10::impl::tls_set_dispatch_key_included(DispatchKey::Python, true);
-    c10::impl::tls_set_dispatch_key_included(DispatchKey::PythonTLSSnapshot, true);
+    c10::impl::tls_set_dispatch_key_included(
+        DispatchKey::PythonTLSSnapshot, true);
   } else {
     TorchDispatchModeTLS::reset_state();
   }
@@ -24,7 +26,8 @@ const std::shared_ptr<SafePyObject>& TorchDispatchModeTLS::get_state() {
 void TorchDispatchModeTLS::reset_state() {
   torchDispatchModeState.reset();
   c10::impl::tls_set_dispatch_key_included(DispatchKey::Python, false);
-  c10::impl::tls_set_dispatch_key_included(DispatchKey::PythonTLSSnapshot, false);
+  c10::impl::tls_set_dispatch_key_included(
+      DispatchKey::PythonTLSSnapshot, false);
 }
 
 bool dispatch_mode_enabled() {
@@ -32,4 +35,4 @@ bool dispatch_mode_enabled() {
 }
 
 } // namespace impl
-} // namespace at
+} // namespace c10
