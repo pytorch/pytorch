1cec7c2a19fd80edfb319cbc32315eea382797ac
Xiang Gao <qasdfgtyuiop@gmail.com>
2022-08-15 16:18:28 -0700

    UCX_TLS=tcp,cuda

--- test/run_test.py
+++ test/run_test.py
@@ -299,6 +299,7 @@ if dist.is_available():
         DISTRIBUTED_TESTS_CONFIG["ucc"] = {
             "WORLD_SIZE": "2" if torch.cuda.device_count() == 2 else "3",
             "TEST_REPORT_SOURCE_OVERRIDE": "dist-ucc",
+            "UCX_TLS": "tcp,cuda"
         }
 
 # https://stackoverflow.com/questions/2549939/get-signal-names-from-numbers-in-python
--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -80,8 +80,8 @@ class DistTestCases:
 
     # Sets showing that something is implemented
     backend_feature = {}
-    backend_feature["gpu"] = {"nccl", "gloo", "ucc"}
-    backend_feature["cuda"] = {"nccl", "gloo", "ucc"}
+    backend_feature["gpu"] = {"nccl", "gloo"}
+    backend_feature["cuda"] = {"nccl", "gloo"}
     backend_feature["ddp"] = {"nccl", "gloo", "ucc"}
     backend_feature["subgroup"] = {"nccl", "gloo", "ucc"}
     backend_feature["plugin"] = set()
