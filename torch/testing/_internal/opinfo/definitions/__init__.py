a3dada230f676aa64f050a857cb4f58ce55b5227
Peter Bell <peterbell10@live.co.uk>
2022-08-16 23:05:48 +0100

    Move `torch.linalg` opinfos to opinfo.definitions
    [ghstack-poisoned]

--- torch/testing/_internal/opinfo/definitions/__init__.py
+++ torch/testing/_internal/opinfo/definitions/__init__.py
@@ -1,13 +1,15 @@
 from typing import List
 
 from torch.testing._internal.opinfo.core import OpInfo
-from torch.testing._internal.opinfo.definitions import fft
+from torch.testing._internal.opinfo.definitions import fft, linalg
 
 # Operator database
 op_db: List[OpInfo] = [
     *fft.op_db,
+    *linalg.op_db,
 ]
 
 python_ref_db: List[OpInfo] = [
     *fft.python_ref_db,
+    *linalg.python_ref_db,
 ]
