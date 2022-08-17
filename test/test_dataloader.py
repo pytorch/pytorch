493e42e82ce7a05a3bd1c16f76ab02575331ce64
erjia <erjia@fb.com>
2022-08-17 17:46:42 +0000

    Update on "[DataPipe] Reset iterator when NotStarted"
    This PR changes the behavior of `IterDataPipe` to always invoke `reset` for the state of `NotStarted`. The main reason is we normally put lazy initialization code into `reset` function. Even for the state of `NotStarted`, we should invoke `reset` to initialize those lazy variables. Otherwise, we have to manually determine if the state is `NotStarted` or `Iterating` in `__iter__` function and only manually invoke `reset` in the state of `NotStarted`.
    
    I am also add `_snapshot_state` into serialization state and during `__setstate__` only change the state to `Restored` if the original state is `Iterating`. Especially, for the case of deserializing/serializing `NotStarted` DataPipe (multiprocessing), we would invoke `set_seed` for `Shuffler`. We need the `DataPipe` remains as `NotStarted` to properly `reset`.
    
    I am listing all the expected behavior state transition below:
    - Initial state: `NotStarted`
      - `iter` -> Call `reset` and change the state to `Iterating` 
      - serialize/deserialize -> Keep the state as `NotStarted` (will `reset` if `iter` is called afterwards)
    - Initial state: `Iterating`
      - `iter` -> Call `reset` and keep the state to `Iterating` 
      - serialize/deserialize -> Change the state as `Restored`
    - Initial state: `Restored`
      - `iter` -> Only change the state to `Iterating`
      - serialize/deserialize -> Not allowed
    
    What do you think about it?
    
    [ghstack-poisoned]

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -2340,17 +2340,12 @@ class IntegrationTestDataLoaderDataPipe(TestCase):
             return input_ds.shuffle(buffer_size=buffer_size).sharding_filter()
 
         for bs in (5, 20, 33):
-            shuffle_dp = _create_dp(bs)
-            self.assertEqual(len(shuffle_dp), len(exp))
-
-            torch.manual_seed(123)
-            res = list(shuffle_dp)
-            self.assertEqual(sorted(res), exp)
-
             # Test Deterministic
             for num_workers, pw in itertools.product((0, 1, 2), (True, False)):
                 if num_workers == 0 and pw:
                     continue
+
+                shuffle_dp = _create_dp(bs)
 
                 mp_ctx = "spawn" if num_workers > 0 else None
                 dl = DataLoader(
@@ -2363,7 +2358,7 @@ class IntegrationTestDataLoaderDataPipe(TestCase):
 
                 # No seed
                 dl_res_ns = list(dl)
-                self.assertEqual(sorted(dl_res_ns), sorted(exp))
+                self.assertEqual(sorted(dl_res_ns), exp)
 
                 # Same seeds
                 dl_res = []
@@ -2371,6 +2366,7 @@ class IntegrationTestDataLoaderDataPipe(TestCase):
                     torch.manual_seed(123)
                     dl_res.append(list(dl))
                 self.assertEqual(dl_res[0], dl_res[1])
+                self.assertEqual(sorted(dl_res[0]), exp)
 
                 # Different seeds
                 torch.manual_seed(321)
@@ -2379,6 +2375,11 @@ class IntegrationTestDataLoaderDataPipe(TestCase):
                 self.assertEqual(len(dl_res[0]), len(dl_res[2]))
                 self.assertNotEqual(dl_res[0], dl_res[2])
                 self.assertEqual(sorted(dl_res[0]), sorted(dl_res[2]))
+
+                if dl._iterator is not None:
+                    dl._iterator._shutdown_workers()
+                    dl._iterator = None
+                del dl
 
 
 class StringDataset(Dataset):
