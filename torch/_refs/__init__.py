1a49eea30102b9d083367f8f088f60381576a54c
Nikita Karetnikov <nkaretnikov@quansight.com>
2022-08-17 14:26:09 +0200

    [primTorch] Add ref for diag_embed (#82322)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82322
    Approved by: https://github.com/Lezcano, https://github.com/ngimel

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -201,6 +201,7 @@ __all__ = [
     "conj",
     "constant_pad_nd",
     "contiguous",
+    "diag_embed",
     "diagonal",
     "dsplit",
     "dstack",
@@ -3031,6 +3032,63 @@ def diagonal(
     result = self.as_strided(size=sizes, stride=strides, storage_offset=storage_offset)
 
     return result
+
+
+@register_decomposition(torch.ops.aten.diag_embed)
+def diag_embed(
+    t: TensorLikeType,
+    offset: int = 0,
+    dim1: int = -2,
+    dim2: int = -1,
+) -> TensorLikeType:
+    """
+    Reference implementation of torch.diag_embed
+    """
+    # as per the docs, exchanging dims is equivalent to changing the sign of
+    # offset
+    if dim1 > dim2:
+        dim1, dim2 = dim2, dim1
+        offset = -offset
+
+    # convert from negative dims
+    rank = t.ndim + 1
+    dim1 = utils.canonicalize_dim(rank=rank, idx=dim1)
+    dim2 = utils.canonicalize_dim(rank=rank, idx=dim2)
+
+    check(
+        dim1 != dim2, lambda: f"diagonal dimensions cannot be identical {dim1}, {dim2}"
+    )
+
+    # as per the docs, the size of last dim is placed at dim1 and dim2
+    last_dim = t.size(-1)
+
+    if offset != 0:
+        # add padding to match the new size
+        t_shape = list(t.shape)
+        t_shape[-1] = builtins.abs(offset)
+        z = torch.zeros(t_shape, dtype=t.dtype, device=t.device, requires_grad=False)
+        pair = (z, t) if offset > 0 else (t, z)
+        t = torch.cat(pair, dim=-1)
+        # make sure the diagonal always has the same size
+        last_dim += builtins.abs(offset)
+
+    # preserve original data, but place 1 at dim1 and move last dim to dim2
+    t = t.unsqueeze(dim1).movedim(-1, dim2)
+
+    # generate ranges shifting indices based on offset
+    a_range = torch.arange(last_dim, device=t.device, dtype=torch.int64)
+    b_range = torch.arange(
+        offset, last_dim + offset, device=t.device, dtype=torch.int64
+    )
+
+    # broadcast
+    cond = a_range == b_range.unsqueeze(-1)
+    cond_shape = [last_dim if i in (dim1, dim2) else 1 for i in range(len(t.shape))]
+    cond = cond.reshape(cond_shape)
+    if t.dtype is torch.bool:
+        return cond.logical_and(t)
+    else:
+        return torch.where(cond, t, 0)
 
 
 # CompositeImplicitAutograd - don't register decomp
