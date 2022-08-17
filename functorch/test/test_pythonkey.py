a705ff3e6afdeb752208fbedd9ae79745d37c4fd
Horace He <chilli@fb.com>
2022-08-17 02:45:19 +0000

    Delayed compilation of backwards pass to when backwards runs (#83367)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83367
    Approved by: https://github.com/ngimel, https://github.com/zou3519

--- functorch/test/test_pythonkey.py
+++ functorch/test/test_pythonkey.py
@@ -267,9 +267,12 @@ class TestAOTAutograd(TestCase):
         with torch.set_grad_enabled(False):
             f(*inps)
         self.assertIsNone(graph_size)
+
         with torch.set_grad_enabled(True):
-            f(*inps)
-        self.assertTrue(graph_size > 2)
+            out = f(*inps)
+            self.assertIsNone(graph_size)
+            out.sum().backward()
+            self.assertTrue(graph_size > 2)
         self.assertEqual(num_of_recompilations() - start_recompilations, 2)
 
     def test_output_dict(self):
@@ -431,7 +434,7 @@ def get_fw_bw_graph(f, inps, partitioner=min_cut_rematerialization_partition):
                  fw_compiler=partial(extract_graph, graph_cell=fw_graph_cell),
                  bw_compiler=partial(extract_graph, graph_cell=bw_graph_cell),
                  partition_fn=partitioner,
-                 decompositions=default_decompositions)(*inps)
+                 decompositions=default_decompositions)(*inps).sum().backward()
     return (fw_graph_cell[0], bw_graph_cell[0])
 
 
