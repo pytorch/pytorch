4962fb29bd105d465e3697098892837699ed3897
Andrew Or <andrewor@fb.com>
2022-08-16 18:52:29 -0700

    [Quant] Separate FBGEMM/QNNPACK BackendConfigs
    Summary: Previously we use a single BackendConfig
    (get_native_backend_config) for both the FBGEMM and QNNPACK
    backends. However, these two backends have subtle differences
    in terms of their requirements that cannot be satisfied using
    a single BackendConfig. Therefore, this commit is the first step
    torwards decoupling the two backends. The real change in
    functionality will come in a future commit after DTypeConfig
    supports quant_min/quant_max and scale_min/scale_max. Existing
    uses of `get_native_backend_config` should not be affected.
    
    Public facing changes:
    ```
    from torch.ao.quantization.backend_config import (
        get_fbgemm_backend_config,
        get_qnnpack_backend_config,
    )
    fbgemm_backend_config = get_fbgemm_backend_config()
    qnnpack_backend_config = get_qnnpack_backend_config()
    ```
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers: jerryzh168
    
    Subscribers: jerryzh168
    
    [ghstack-poisoned]

---
+++ torch/ao/quantization/backend_config/fbgemm.py
@@ -0,0 +1,102 @@
+import torch
+from .native import _get_native_backend_config_helper
+from .backend_config import DTypeConfig
+
+
+# ===================
+# |  DTYPE CONFIGS  |
+# ===================
+
+# TODO: For now, these DTypeConfigs are identical to the ones defined in native.py
+# In the future, once we support specifying quant_min/quant_max and scale_min/scale_max,
+# these will diverge. In particular, for FBGEMM, we will restrict the activation quantized
+# values to within [0, 127].
+
+fbgemm_weighted_op_int8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.quint8,
+    weight_dtype=torch.qint8,
+    bias_dtype=torch.float,
+)
+
+fbgemm_default_op_quint8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.quint8,
+)
+
+fbgemm_default_op_fp16_dtype_config = DTypeConfig(
+    input_dtype=torch.float16,
+    output_dtype=torch.float16,
+    weight_dtype=torch.float16,
+    bias_dtype=torch.float16,
+)
+
+fbgemm_default_dynamic_int8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.float,
+    weight_dtype=torch.qint8,
+    bias_dtype=torch.float,
+    is_dynamic=True,
+)
+
+fbgemm_default_dynamic_float16_dtype_config = DTypeConfig(
+    input_dtype=torch.float16,
+    output_dtype=torch.float,
+    weight_dtype=torch.float16,
+    bias_dtype=torch.float,
+    is_dynamic=True,
+)
+
+fbgemm_weight_only_quint8_dtype_config = DTypeConfig(
+    input_dtype=torch.float,
+    output_dtype=torch.float,
+    weight_dtype=torch.quint8,
+)
+
+fbgemm_weight_only_quint4x2_dtype_config = DTypeConfig(
+    input_dtype=torch.float,
+    output_dtype=torch.float,
+    weight_dtype=torch.quint4x2,
+)
+
+
+# =====================
+# |  BACKEND CONFIGS  |
+# =====================
+
+def get_fbgemm_backend_config() -> BackendConfig:
+    """
+    Return the `BackendConfig` for PyTorch's native FBGEMM backend.
+    """
+    conv_dtype_configs = [fbgemm_weighted_op_int8_dtype_config]
+    linear_dtype_configs = [
+        fbgemm_weighted_op_int8_dtype_config,
+        fbgemm_default_dynamic_int8_dtype_config,
+        fbgemm_default_dynamic_float16_dtype_config,
+    ]
+    binary_op_dtype_configs = [fbgemm_weighted_op_int8_dtype_config]
+    default_op_dtype_configs = [fbgemm_default_op_quint8_dtype_config]
+    fixed_qparams_op_dtype_configs = [fbgemm_weighted_op_int8_dtype_config]
+    share_qparams_op_dtype_configs = [fbgemm_default_op_quint8_dtype_config]
+    rnn_op_dtype_configs = [
+        fbgemm_default_dynamic_int8_dtype_config,
+        fbgemm_default_dynamic_float16_dtype_config,
+    ]
+    embedding_op_dtype_configs = [
+        fbgemm_weight_only_quint8_dtype_config,
+        fbgemm_weight_only_quint4x2_dtype_config,
+    ]
+    return _get_native_backend_config_helper(
+        "fbgemm",
+        conv_dtype_configs,
+        linear_dtype_configs,
+        binary_op_dtype_configs,
+        default_op_dtype_configs,
+        fixed_qparams_op_dtype_configs,
+        share_qparams_op_dtype_configs,
+        rnn_op_dtype_configs,
+        embedding_op_dtype_configs)
+
+__all__ = [
+    "get_fbgemm_backend_config",
+]
---
+++ torch/ao/quantization/backend_config/qnnpack.py
@@ -0,0 +1,102 @@
+import torch
+from .native import _get_native_backend_config_helper
+from .backend_config import DTypeConfig
+
+
+# ===================
+# |  DTYPE CONFIGS  |
+# ===================
+
+# TODO: For now, these DTypeConfigs are identical to the ones defined in native.py
+# In the future, once we support specifying quant_min/quant_max and scale_min/scale_max,
+# these will diverge. In particular, for QNNPACK, we will restrict the weight quantized
+# values to within [-127, 127] and set the min scale value to 2 ** -12.
+
+qnnpack_weighted_op_int8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.quint8,
+    weight_dtype=torch.qint8,
+    bias_dtype=torch.float,
+)
+
+qnnpack_default_op_quint8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.quint8,
+)
+
+qnnpack_default_op_fp16_dtype_config = DTypeConfig(
+    input_dtype=torch.float16,
+    output_dtype=torch.float16,
+    weight_dtype=torch.float16,
+    bias_dtype=torch.float16,
+)
+
+qnnpack_default_dynamic_int8_dtype_config = DTypeConfig(
+    input_dtype=torch.quint8,
+    output_dtype=torch.float,
+    weight_dtype=torch.qint8,
+    bias_dtype=torch.float,
+    is_dynamic=True,
+)
+
+qnnpack_default_dynamic_float16_dtype_config = DTypeConfig(
+    input_dtype=torch.float16,
+    output_dtype=torch.float,
+    weight_dtype=torch.float16,
+    bias_dtype=torch.float,
+    is_dynamic=True,
+)
+
+qnnpack_weight_only_quint8_dtype_config = DTypeConfig(
+    input_dtype=torch.float,
+    output_dtype=torch.float,
+    weight_dtype=torch.quint8,
+)
+
+qnnpack_weight_only_quint4x2_dtype_config = DTypeConfig(
+    input_dtype=torch.float,
+    output_dtype=torch.float,
+    weight_dtype=torch.quint4x2,
+)
+
+
+# =====================
+# |  BACKEND CONFIGS  |
+# =====================
+
+def get_qnnpack_backend_config() -> BackendConfig:
+    """
+    Return the `BackendConfig` for PyTorch's native QNNPACK backend.
+    """
+    conv_dtype_configs = [qnnpack_weighted_op_int8_dtype_config]
+    linear_dtype_configs = [
+        qnnpack_weighted_op_int8_dtype_config,
+        qnnpack_default_dynamic_int8_dtype_config,
+        qnnpack_default_dynamic_float16_dtype_config,
+    ]
+    binary_op_dtype_configs = [qnnpack_weighted_op_int8_dtype_config]
+    default_op_dtype_configs = [qnnpack_default_op_quint8_dtype_config]
+    fixed_qparams_op_dtype_configs = [qnnpack_weighted_op_int8_dtype_config]
+    share_qparams_op_dtype_configs = [qnnpack_default_op_quint8_dtype_config]
+    rnn_op_dtype_configs = [
+        qnnpack_default_dynamic_int8_dtype_config,
+        qnnpack_default_dynamic_float16_dtype_config,
+    ]
+    embedding_op_dtype_configs = [
+        qnnpack_weight_only_quint8_dtype_config,
+        qnnpack_weight_only_quint4x2_dtype_config,
+    ]
+    return _get_native_backend_config_helper(
+        "qnnpack",
+        conv_dtype_configs,
+        linear_dtype_configs,
+        binary_op_dtype_configs,
+        default_op_dtype_configs,
+        fixed_qparams_op_dtype_configs,
+        share_qparams_op_dtype_configs,
+        rnn_op_dtype_configs,
+        embedding_op_dtype_configs)
+
+__all__ = [
+    "get_qnnpack_backend_config",
+]
