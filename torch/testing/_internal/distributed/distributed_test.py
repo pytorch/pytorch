a15e64a83c782048d07b6bfe0468b9a45f0f3359
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-08-17 14:42:33 +0000

    [ROCm] Enable test_ddp_profiling_torch_profiler (#82749)
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82749
    Approved by: https://github.com/ngimel, https://github.com/rohan-varma

--- torch/testing/_internal/distributed/distributed_test.py
+++ torch/testing/_internal/distributed/distributed_test.py
@@ -6152,7 +6152,6 @@ class DistributedTest:
             IS_MACOS or IS_WINDOWS,
             "torch.profiler not enabled for mac/windows: https://github.com/pytorch/pytorch/pull/56124",
         )
-        @skip_if_rocm
         def test_ddp_profiling_torch_profiler(self):
             cpu_act = torch.profiler.ProfilerActivity.CPU
             cuda_act = torch.profiler.ProfilerActivity.CUDA
