aec3b02f35803e04ec53e32fdfd94bdf013934d5
migeedz <migeedz@fb.com>
2022-08-18 10:13:37 -0700

    fix torch._C._nn.linear bug (#83682)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83682
    Approved by: https://github.com/jansel

--- torch/fx/experimental/migrate_gradual_types/constraint_generator.py
+++ torch/fx/experimental/migrate_gradual_types/constraint_generator.py
@@ -1011,7 +1011,7 @@ def torch_linear_inference_rule(n: Node, symbols, constraints, counter):
     assert isinstance(n.args[0], Node)
     weight_dims, counter = gen_tensor_dims(2, counter)
     equality_constraint = BinConstraintT(symbols[n.args[1]], TensorType(weight_dims), op_eq)
-    constraints, counter = linear_constraints(n, weight_dims[0], weight_dims[1], symbols, counter)
+    constraints, counter = linear_constraints(n, weight_dims[1], weight_dims[0], symbols, counter)
     return [equality_constraint] + constraints, counter
 
 
