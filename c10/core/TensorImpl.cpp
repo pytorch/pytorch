0587bb7ea41a955a71bea91e0edbc801cc00f8db
Brian Hirsh <hirsheybar@fb.com>
2022-08-18 14:42:39 -0700

    fix functionalization <> fake tensor mode
    [ghstack-poisoned]

--- c10/core/TensorImpl.cpp
+++ c10/core/TensorImpl.cpp
@@ -548,7 +548,8 @@ c10::intrusive_ptr<TensorImpl> TensorImpl::shallow_copy_and_detach_core(
   const auto& maybe_torch_dispatch_mode_state =
       c10::impl::TorchDispatchModeTLS::get_state();
   // TODO: do we have to exclude after Python dispatch key set?
-  if (maybe_torch_dispatch_mode_state) {
+  if (maybe_torch_dispatch_mode_state &&
+      !c10::impl::tls_is_dispatch_key_excluded(DispatchKey::Python)) {
     r = maybe_torch_dispatch_mode_state->pyinterpreter()->detach(this);
   } else if (
       key_set_.has(DispatchKey::Python) &&
