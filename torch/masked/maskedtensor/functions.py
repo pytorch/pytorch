18a9f10c2828db9c6722168bd225659079834df2
George Qi <georgeqi94@gmail.com>
2022-08-18 14:43:42 +0000

    Update base for Update on "[maskedtensor] add basic tests and unary/binary/reduction tests from common_method_invocations"
    [ghstack-poisoned]

--- torch/masked/maskedtensor/functions.py
+++ torch/masked/maskedtensor/functions.py
@@ -5,13 +5,14 @@ import math
 import torch
 from torch.nn.functional import dropout, linear
 
+from .matmul import masked_bmm
+
+__all__ = ['multi_head_attention_forward']
 
 def _in_projection_packed(q, k, v, w, b):
-    # if is_masked_tensor(k):
-    #     assert not is_masked_tensor(q)
-    #     assert not is_masked_tensor(v)
     w_q, w_k, w_v = w.chunk(3)
-    assert b is None
+    if b is not None:
+        raise ValueError("b must be None")
     b_q = b_k = b_v = None
     return linear(q, w_q, b_q), linear(k, w_k, b_k), linear(v, w_v, b_v)
 
@@ -20,8 +21,6 @@ def _scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=0.0):
     B, Nt, E = q.shape
     q = q / math.sqrt(E)
     # (B, Nt, E) x (B, E, Ns) -> (B, Nt, Ns)
-    from .matmul import masked_bmm
-
     attn = masked_bmm(q, k.transpose(-2, -1), attn_mask)
     attn = torch.nn.functional.softmax(attn, dim=-1)
     if dropout_p > 0.0:
@@ -60,38 +59,51 @@ def multi_head_attention_forward(
     # set up shape vars
     tgt_len, bsz, embed_dim = query.shape
     src_len, _, _ = key.shape
-    assert (
-        embed_dim == embed_dim_to_check
-    ), f"was expecting embedding dimension of {embed_dim_to_check}, but got {embed_dim}"
+
+    if embed_dim != embed_dim_to_check:
+        raise ValueError(f"Was expecting embedding dimension of {embed_dim_to_check}, but got {embed_dim}")
+
     if isinstance(embed_dim, torch.Tensor):
         # embed_dim can be a tensor when JIT tracing
         head_dim = embed_dim.div(num_heads, rounding_mode="trunc")
     else:
         head_dim = embed_dim // num_heads
-    assert (
-        head_dim * num_heads == embed_dim
-    ), f"embed_dim {embed_dim} not divisible by num_heads {num_heads}"
-    assert not use_separate_proj_weight
-    assert q_proj_weight is None
-    assert k_proj_weight is None
-    assert v_proj_weight is None
-    assert in_proj_bias is None
-    assert bias_k is None
-    assert bias_v is None
-    assert static_k is None
-    assert static_v is None
-    assert not add_zero_attn
-    assert key_padding_mask is None
+
+    if head_dim * num_heads != embed_dim:
+        raise ValueError(f"embed_dim {embed_dim} not divisible by num_heads {num_heads}")
+
+    if use_separate_proj_weight:
+        raise ValueError("`use_separate_proj_weight` must be False")
+    if q_proj_weight is not None:
+        raise ValueError("`q_proj_weight` must be None")
+    if k_proj_weight is not None:
+        raise ValueError("`k_proj_weight` must be None")
+    if v_proj_weight is not None:
+        raise ValueError("`v_proj_weight` must be None")
+    if in_proj_bias is not None:
+        raise ValueError("`in_proj_bias` must be None")
+    if bias_k is not None:
+        raise ValueError("`bias_k` must be None")
+    if bias_v is not None:
+        raise ValueError("`bias_v` must be None")
+    if static_k is not None:
+        raise ValueError("`static_k` must be None")
+    if static_v is not None:
+        raise ValueError("`static_v` must be None")
+    if add_zero_attn:
+        raise ValueError("`add_zero_attn` must be False")
+    if key_padding_mask is not None:
+        raise ValueError("`key_padding_mask` must be None")
 
     q, k, v = _in_projection_packed(query, key, value, in_proj_weight, in_proj_bias)
 
     # prep attention mask
     if attn_mask is not None:
-        assert (
-            attn_mask.is_floating_point() or attn_mask.dtype == torch.bool
-        ), f"Only float and bool types are supported for attn_mask, not {attn_mask.dtype}"
+        if not (attn_mask.is_floating_point() or attn_mask.dtype == torch.bool):
+            raise TypeError(f"Only float and bool types are supported for attn_mask, not {attn_mask.dtype}")
         # ensure attn_mask's dim is 3
-        assert attn_mask.dim() == 2
+        if attn_mask.dim() != 2:
+            raise ValueError("attn_mask must have dim 3")
         correct_2d_size = (tgt_len, src_len)
         if attn_mask.shape != correct_2d_size:
             raise RuntimeError(
--- torch/masked/maskedtensor/passthrough.py
+++ torch/masked/maskedtensor/passthrough.py
@@ -8,6 +8,8 @@ a new MaskedTensor as a result.
 
 import torch
 
+from .core import _map_mt_args_kwargs, _wrap_result
+
 __all__ = []  # type: ignore[var-annotated]
 
 
@@ -26,45 +28,6 @@ PASSTHROUGH_FNS = [
     torch.ops.aten._reshape_alias,
     torch.ops.aten.cat,
 ]
-
-
-# TODO: tree_map doesn't cut it due to kwargs support for torch_function
-def _map_mt_args_kwargs(args, kwargs, map_fn):
-    if kwargs is None:
-        kwargs = {}
-    impl_args = []
-    for a in args:
-        from .core import is_masked_tensor
-
-        if is_masked_tensor(a):
-            impl_args.append(map_fn(a))
-        elif torch.is_tensor(a):
-            impl_args.append(a)
-        elif isinstance(a, list):
-            a_impl, _ = _map_mt_args_kwargs(a, {}, map_fn)
-            impl_args.append(a_impl)
-        elif isinstance(a, tuple):
-            a_impl, _ = _map_mt_args_kwargs(a, {}, map_fn)
-            impl_args.append(tuple(a_impl))
-        else:
-            impl_args.append(a)
-    impl_kwargs = {
-        k: map_fn(v) if is_masked_tensor(v) else v for (k, v) in kwargs.items()
-    }
-    return impl_args, impl_kwargs
-
-
-def _wrap_result(result_data, result_mask):
-    if isinstance(result_data, list):
-        return list(_wrap_result(r, m) for (r, m) in zip(result_data, result_mask))
-    if isinstance(result_data, tuple):
-        return tuple(_wrap_result(r, m) for (r, m) in zip(result_data, result_mask))
-    if torch.is_tensor(result_data):
-        from .core import MaskedTensor
-
-        return MaskedTensor(result_data, result_mask)
-    # Expect result_data and result_mask to be Tensors only
-    return NotImplemented
 
 
 def _is_pass_through_fn(fn):
