d70c3a0a15cabf27924a075fff2312627efd7a18
Huy Do <huydhn@gmail.com>
2022-08-18 16:33:23 -0700

    WIP

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import requests
-from typing import Any, List
+from typing import Any, Dict, Set, List
 import warnings
+
+CIFLOW_PREFIX = "ciflow/"
 
 
 def parse_args() -> Any:
@@ -14,7 +17,7 @@ def parse_args() -> Any:
     return parser.parse_args()
 
 
-def get_labels(pr_number: int) -> List[str]:
+def get_labels(pr_number: int) -> Set[str]:
     """
     Dynamical get the latest list of labels from the pull request
     """
@@ -35,27 +38,47 @@ def get_labels(pr_number: int) -> List[str]:
 
     if response.status_code != requests.codes.ok:
         warnings.warn(f"Failed to get the labels for #{pr_number} with status code {response.status_code}")
-        return []
+        return set()
 
-    print(response.json())
-    return []
+    return {label.get("name", "") for label in response.json()}
+
+
+def filter(test_matrix: Dict[str, List[Any]], labels: Set[str]) -> Dict[str, List[Any]]:
+    filtered_test_matrix: Dict[str, List[Any]] = {
+        "include": []
+    }
+
+    for config in test_matrix.get("include", []):
+        config_name = config.get("name", "")
+        if not config_name:
+            continue
+
+        label = f"{CIFLOW_PREFIX}{config_name.strip().lower()}"
+        if label in labels:
+            print(f"Select {config_name} because label {label} is presented in the pull request by the time the test starts")
+            filtered_test_matrix["include"].append(config)
+
+    # If no matching label is found, the default is to run everything as normal
+    return filtered_test_matrix if filtered_test_matrix["include"] else test_matrix
 
 
 def main() -> None:
     args = parse_args()
     # The original test matrix set by the workflow
-    test_matrix = args.test_matrix
+    test_matrix = json.loads(args.test_matrix)
     pr_number = args.pr_number
 
     # First, query all the labels from the pull requests
     labels = get_labels(pr_number)
+    # Then filter the test matrix and keep only the selected ones
+    filtered_test_matrix = filter(test_matrix, labels)
 
-    print(test_matrix)
-    print("====")
+    print(json.dumps(filtered_test_matrix))
+    print("=======")
     print(labels)
 
-    # DEBUG
-    print(f"::set-output name=test-matrix::{test_matrix}")
+    # Set the filtered test matrix as the output
+    print(f"::set-output name=test-matrix::{json.dumps(filtered_test_matrix)}")
 
 
 if __name__ == "__main__":
