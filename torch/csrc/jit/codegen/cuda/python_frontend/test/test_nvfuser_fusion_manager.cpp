27d6a3375d7d676f6a817fdb41d2b0f1ad0b165a
Kevin Stephano <kevin.stephano@gmail.com>
2022-08-18 08:41:09 +0000

    Starting a test.

--- torch/csrc/jit/codegen/cuda/python_frontend/test/test_nvfuser_fusion_manager.cpp
+++ torch/csrc/jit/codegen/cuda/python_frontend/test/test_nvfuser_fusion_manager.cpp
@@ -4,6 +4,7 @@
 
 #include <torch/torch.h>
 
+#include <torch/csrc/jit/codegen/cuda/python_frontend/fusion_definition.h>
 #include <torch/csrc/jit/codegen/cuda/python_frontend/fusion_manager.h>
 #include <torch/csrc/jit/codegen/cuda/test/test_gpu_validator.h>
 
@@ -15,6 +16,19 @@ using namespace nvfuser;
 using namespace torch::jit::fuser::cuda;
 
 TEST_F(NVFuserTest, FusionManager_CUDA) {
+  //FusionManager* fm = FusionManager::get(1);
+
+  /*
+  {
+    FusionDefinition fd(fm, 10);
+
+    fd.enter();
+
+    nvfuser::Tensor* t0 = fd.defineTensor();
+    fd.defineRecord(new TensorRecord({*static_cast<State*>(t0)}, {3}, {true}, Nvf::DataType::Float));
+
+    TORCH_CHECK(fm->fusionCachePtr()->record.use_count() == 2, "Both the FusionDefintion and FusionManager Cache should share Record's pointer!");
+  }*/
 
   TORCH_CHECK(true, "Hello test world!");
 }
