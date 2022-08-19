7fd0cc640f61ac87eb6a1b42c85651c717e7e306
Howard Huang <howardhuang@fb.com>
2022-08-19 07:51:31 -0700

    Update on "[3/N] [Dispatchable Collectives] Update broadcast_ with CPU and CUDA implementations"
    ### Changes
    - Update the broadcast op to dispatch to cpu and cuda implementations. Right now they both perform the same logic so this is essentially a no-op.
    - Add test to validate that a separate device implementation is not supported.
    
    #### Motivation
    In the future PRs we will repurpose ProcessGroup to instead contain a list of Backends (ProcessGroupNCCL/Gloo/UCC) and perform dispatching to them based on tensor type. The CPU and CUDA implementations will be updated to have process group select its CPU and CUDA backends respectively.
    
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/OpsImpl.cpp
+++ torch/csrc/distributed/c10d/OpsImpl.cpp
@@ -1,11 +1,11 @@
-#include <torch/library.h>
 #include <torch/csrc/distributed/c10d/OpsImpl.hpp>
+#include <torch/library.h>
 
 namespace c10d {
 namespace ops {
 
-// Below are ProcessGroup's corresponding ops for each backend. Ops are but routed
-// through the dispatcher to be dispatched to the appropriate backend.
+// Below are ProcessGroup's corresponding ops for each backend. Ops are but
+// routed through the dispatcher to be dispatched to the appropriate backend.
 // Currently a no-op as the process group does not have a list of backends.
 c10::intrusive_ptr<Work> broadcast_cpu_(
     at::TensorList tensors,
@@ -15,9 +15,9 @@ c10::intrusive_ptr<Work> broadcast_cpu_(
     int64_t timeout) {
   auto tensor_vec = tensors.vec();
   return process_group->broadcast(
-          tensor_vec,
-          BroadcastOptions{
-              root_rank, root_tensor, std::chrono::milliseconds(timeout)});
+      tensor_vec,
+      BroadcastOptions{
+          root_rank, root_tensor, std::chrono::milliseconds(timeout)});
 }
 
 c10::intrusive_ptr<Work> broadcast_cuda_(
@@ -28,11 +28,10 @@ c10::intrusive_ptr<Work> broadcast_cuda_(
     int64_t timeout) {
   auto tensor_vec = tensors.vec();
   return process_group->broadcast(
-          tensor_vec,
-          BroadcastOptions{
-              root_rank, root_tensor, std::chrono::milliseconds(timeout)});
+      tensor_vec,
+      BroadcastOptions{
+          root_rank, root_tensor, std::chrono::milliseconds(timeout)});
 }
-
 
 // register functions to dispatcher
 namespace {
@@ -43,7 +42,7 @@ TORCH_LIBRARY_IMPL(c10d, CPU, m) {
 TORCH_LIBRARY_IMPL(c10d, CUDA, m) {
   m.impl("broadcast_", broadcast_cuda_);
 }
-}
+} // namespace
 
 } // namespace ops
 } // namespace c10d
