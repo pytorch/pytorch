5f950bd11dbb51c3f2fa5f0b16697f26ed4ac91d
chengscott <60510scott@gmail.com>
2022-08-21 06:18:39 +0800

    implement DistReduceOp and replace reduce_agg by BlockReduce

--- aten/src/ATen/native/cuda/DistanceKernel.cu
+++ aten/src/ATen/native/cuda/DistanceKernel.cu
@@ -6,6 +6,7 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <math.h>
 
+#include <ATen/native/cuda/block_reduce.cuh>
 #include <ATen/native/cuda/DeviceSqrt.cuh>
 #include <ATen/native/Distance.h>
 
@@ -22,7 +23,7 @@ namespace at { namespace native {
 
 namespace {
 
-static const int forward_threads = 256;
+constexpr int kCUDANumThreads = 256;
 
 template <typename scalar_t>
 struct dists {
@@ -80,27 +81,16 @@ struct dists {
 };
 
 template <typename scalar_t, typename F>
-__device__ static inline scalar_t reduce_agg(scalar_t agg) {
-  for (int offset = warpSize / 2; offset > 0; offset /= 2) {
-    F::agg(agg, WARP_SHFL_DOWN(agg, offset));
-  }
-
-  __shared__ scalar_t shared[forward_threads];
-  int lane = threadIdx.x % warpSize;
-  int warp_id = threadIdx.x / warpSize;
-  if (lane == 0) {
-    shared[warp_id] = agg;
-  }
-
-  __syncthreads();
-  agg = (threadIdx.x < blockDim.x / warpSize) ? shared[lane] : 0.0;
-  if (warp_id == 0) {
-    for (int offset = blockDim.x / warpSize / 2; offset > 0; offset /= 2) {
-      F::agg(agg, WARP_SHFL_DOWN(agg, offset));
+struct DistReduceOp {
+    __forceinline__ __device__ scalar_t combine(scalar_t a, scalar_t b) const {
+        F::agg(a, b);
+        return a;
     }
-  }
-  return agg;
-}
+
+    __forceinline__ __device__ scalar_t warp_shfl_down(scalar_t data, int offset) const {
+        return WARP_SHFL_DOWN(data, offset);
+    }
+};
 
 template <typename scalar_t, typename F>
 __global__ static void pdist_kernel_cuda_impl(scalar_t * result, const scalar_t * self, const int64_t n, const int64_t m, const scalar_t p,
@@ -121,7 +111,9 @@ __global__ static void pdist_kernel_cuda_impl(scalar_t * result, const scalar_t 
     F::inc(agg, std::abs(*a - *b), p);
   }
 
-  agg = reduce_agg<scalar_t, F>(agg);
+  __shared__ scalar_t agg_smem[kCUDANumThreads];
+  scalar_t agg_init{0.0};
+  agg = cuda_utils::BlockReduce(agg, DistReduceOp<scalar_t, F>{}, agg_init, agg_smem);
   if (threadIdx.x == 0) {
     result[k] = F::finish(agg, p);
   }
@@ -210,7 +202,9 @@ __global__ static void cdist_kernel_cuda_impl(scalar_t * result, const scalar_t 
   for (; a < end; a += stride, b += stride) {
     F::inc(agg, std::abs(*a - *b), p);
   }
-  agg = reduce_agg<scalar_t, F>(agg);
+  __shared__ scalar_t agg_smem[kCUDANumThreads];
+  scalar_t agg_init{0.0};
+  agg = cuda_utils::BlockReduce(agg, DistReduceOp<scalar_t, F>{}, agg_init, agg_smem);
   if (threadIdx.x == 0) {
     result[blockIdx.x] = F::finish(agg, p);
   }
@@ -224,7 +218,7 @@ void cdist_kernel_impl(Tensor& result, const Tensor& x1, const Tensor& x2, doubl
   const int64_t l1_size = r1 * m;
   const int64_t l2_size = r2 * m;
   const dim3 grid(result.numel());
-  const dim3 block(forward_threads);
+  const dim3 block(kCUDANumThreads);
 
   AT_DISPATCH_FLOATING_TYPES(x1.scalar_type(), "cdist_cuda", [&] {
     if (p == 0.0) {
@@ -248,7 +242,7 @@ void cdist_kernel_impl(Tensor& result, const Tensor& x1, const Tensor& x2, doubl
 
 void pdist_forward_kernel_impl(Tensor& result, const Tensor& self, double p) {
   const dim3 grid(result.numel());
-  const dim3 block(forward_threads);
+  const dim3 block(kCUDANumThreads);
   int64_t n = self.size(0);
   int64_t m = self.size(1);
   // https://github.com/pytorch/pytorch/issues/15511 demonstrated we need to do
