35ae798d601ad78e3754ddebd6f5b17fc0f639c8
Peter Bell <peterbell10@live.co.uk>
2022-08-22 14:23:55 +0100

    Move `torch.special` OpInfos into opinfo/definitions/special.py (#83762)
    Ref #82518
    
    As with `linalg` this doesn't include ops with an alias in special,
    only the ones where `special.foo` is the actual name of the opinfo.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83762
    Approved by: https://github.com/albanD

--- torch/testing/_internal/opinfo/definitions/__init__.py
+++ torch/testing/_internal/opinfo/definitions/__init__.py
@@ -1,15 +1,17 @@
 from typing import List
 
 from torch.testing._internal.opinfo.core import OpInfo
-from torch.testing._internal.opinfo.definitions import fft, linalg
+from torch.testing._internal.opinfo.definitions import fft, linalg, special
 
 # Operator database
 op_db: List[OpInfo] = [
     *fft.op_db,
     *linalg.op_db,
+    *special.op_db,
 ]
 
 python_ref_db: List[OpInfo] = [
     *fft.python_ref_db,
     *linalg.python_ref_db,
+    *special.python_ref_db,
 ]
