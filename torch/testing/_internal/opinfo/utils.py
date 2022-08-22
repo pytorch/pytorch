7656ef73f1ae73798ae965da6dedd260b7cb4f01
Peter Bell <peterbell10@live.co.uk>
2022-08-22 14:23:55 +0100

    Move `torch.special` OpInfos into opinfo/definitions/special.py (#83762)
    Ref #82518
    
    As with `linalg` this doesn't include ops with an alias in special,
    only the ones where `special.foo` is the actual name of the opinfo.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83762
    Approved by: https://github.com/albanD

--- torch/testing/_internal/opinfo/utils.py
+++ torch/testing/_internal/opinfo/utils.py
@@ -1,6 +1,8 @@
 import collections
 import warnings
-from functools import partial
+from functools import partial, wraps
+
+import numpy as np
 
 import torch
 from torch.testing._internal.common_cuda import TEST_CUDA
@@ -20,6 +22,8 @@ from torch.testing._internal.common_dtype import (
     integral_types,
     integral_types_and,
 )
+from torch.testing._internal.common_utils import torch_to_numpy_dtype_dict
+
 
 COMPLETE_DTYPES_DISPATCH = (
     all_types,
@@ -146,3 +150,39 @@ def str_format_dynamic_dtype(op):
     )
 
     return fmt_str
+
+
+def np_unary_ufunc_integer_promotion_wrapper(fn):
+    # Wrapper that passes PyTorch's default scalar
+    #   type as an argument to the wrapped NumPy
+    #   unary ufunc when given an integer input.
+    #   This mimicks PyTorch's integer->floating point
+    #   type promotion.
+    #
+    # This is necessary when NumPy promotes
+    #   integer types to double, since PyTorch promotes
+    #   integer types to the default scalar type.
+
+    # Helper to determine if promotion is needed
+    def is_integral(dtype):
+        return dtype in [
+            np.bool_,
+            bool,
+            np.uint8,
+            np.int8,
+            np.int16,
+            np.int32,
+            np.int64,
+        ]
+
+    @wraps(fn)
+    def wrapped_fn(x):
+        # As the default dtype can change, acquire it when function is called.
+        # NOTE: Promotion in PyTorch is from integer types to the default dtype
+        np_dtype = torch_to_numpy_dtype_dict[torch.get_default_dtype()]
+
+        if is_integral(x.dtype):
+            return fn(x.astype(np_dtype))
+        return fn(x)
+
+    return wrapped_fn
