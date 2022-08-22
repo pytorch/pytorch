007aa63ea4d150e4cc518e49976c0fe9b257c4b5
Edward Z. Yang <ezyang@fb.com>
2022-08-21 21:29:40 -0700

    Update on "Reset torchdynamo before running individual tests"
    This ensures that tests are more isolated against each other
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- test/test_optim.py
+++ test/test_optim.py
@@ -1146,6 +1146,7 @@ class TestLRScheduler(TestCase):
 
         self.assertWarnsRegex(UserWarning, r'how-to-adjust-learning-rate', old_pattern2)
 
+    @skipIfTorchDynamo("https://github.com/pytorch/torchdynamo/issues/948")
     def test_new_pattern_no_warning(self):
         epochs = 35
         with warnings.catch_warnings(record=True) as ws:
@@ -1160,6 +1161,7 @@ class TestLRScheduler(TestCase):
                 scheduler.step()
             self.assertTrue(len(ws) == 0, "No warning should be raised")
 
+    @skipIfTorchDynamo("https://github.com/pytorch/torchdynamo/issues/948")
     def test_new_pattern_no_warning_with_arg(self):
         epochs = 35
         with warnings.catch_warnings(record=True) as ws:
