50e97daea76a63dd58c197919203402c6480bfd5
Peter Bell <peterbell10@live.co.uk>
2022-08-22 14:23:55 +0100

    Move _masked opinfos to opinfo/definitions/_masked.py (#83763)
    Ref #82518
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83763
    Approved by: https://github.com/albanD

--- torch/testing/_internal/opinfo/definitions/__init__.py
+++ torch/testing/_internal/opinfo/definitions/__init__.py
@@ -1,13 +1,14 @@
 from typing import List
 
 from torch.testing._internal.opinfo.core import OpInfo
-from torch.testing._internal.opinfo.definitions import fft, linalg, special
+from torch.testing._internal.opinfo.definitions import _masked, fft, linalg, special
 
 # Operator database
 op_db: List[OpInfo] = [
     *fft.op_db,
     *linalg.op_db,
     *special.op_db,
+    *_masked.op_db,
 ]
 
 python_ref_db: List[OpInfo] = [
