432fae64147ad33f8301c3c35698c000a3db3178
chenlai <chenlai@devvm7453.prn0.facebook.com>
2022-08-22 20:35:11 -0700

    Update on "Fix load_extra_only api for flatbuffers and enable flatbuffers in mobile for OSS properly"
    `_load_extra_only_for_mobile` API hasn't handled flatbuffers logic yet. Update the api accordingly.
    
    Also find out mobile build in OSS doesn't build with flatbuffers. Filed task T129996445 to track
    
    Differential Revision: [D38890847](https://our.internmc.facebook.com/intern/diff/D38890847/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D38890847/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/mobile/import.cpp
+++ torch/csrc/jit/mobile/import.cpp
@@ -707,7 +707,7 @@ void _load_extra_only_for_mobile(
   switch (format) {
     case FileFormat::ZipFileFormat: {
       std::unique_ptr<FileAdapter> rai =
-        std::make_unique<FileAdapter>(filename);
+          std::make_unique<FileAdapter>(filename);
       auto reader = torch::make_unique<PyTorchStreamReader>(std::move(rai));
       BytecodeDeserializer deserializer(std::move(reader));
       deserializer.deserialize_only_extra(device, extra_files);
