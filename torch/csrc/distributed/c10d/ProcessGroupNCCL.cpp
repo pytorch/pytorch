aaf80145e8bb39ca5f294e7fefa4c3f2fc2698c6
Ke Wen <kw2501@fb.com>
2022-08-22 17:54:28 -0700

    Update torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
    Co-authored-by: Nikita Shulga <nshulga@fb.com>

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -584,8 +584,7 @@ ProcessGroupNCCL::ProcessGroupNCCL(
       "ProcessGroupNCCL is only supported with GPUs, no GPUs found!");
   blockingWait_ = parseEnvVarFlag(NCCL_BLOCKING_WAIT);
   asyncErrorHandling_ = parseEnvVarFlag(NCCL_ASYNC_ERROR_HANDLING);
-  desyncDebug_ = parseEnvVarFlag(NCCL_DESYNC_DEBUG) ||
-      dist_debug_level_ >= DebugLevel::Detail;
+  desyncDebug_ = parseEnvVarFlag(NCCL_DESYNC_DEBUG) || (dist_debug_level_ >= DebugLevel::Detail);
 
   if (blockingWait_) {
     if (asyncErrorHandling_ || desyncDebug_) {
