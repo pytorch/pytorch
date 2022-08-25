99592ef74b130e0fd46b9666fa5fc3b2d4badd10
Justin Chu <justinchu@microsoft.com>
2022-08-25 21:24:35 +0000

    [ONNX] Add runtime type checking to `export` (#83673)
    This PR adds an internal wrapper on the [beartype](https://github.com/beartype/beartype) library to perform runtime type checking in `torch.onnx`. It uses beartype when it is found in the environment and is reduced to a no-op when beartype is not found.
    
    Setting the env var `TORCH_ONNX_EXPERIMENTAL_RUNTIME_TYPE_CHECK=ERRORS` will turn on the feature. setting `TORCH_ONNX_EXPERIMENTAL_RUNTIME_TYPE_CHECK=DISABLED` will disable all checks. When not set and `beartype` is installed, a warning message is emitted.
    
    Now when users call an api with invalid arguments e.g.
    
    ```python
    torch.onnx.export(conv, y, path, export_params=True, training=False)
    
    # traning should take TrainingModel, not bool
    ```
    
    they get
    
    ```
    Traceback (most recent call last):
      File "bisect_m1_error.py", line 63, in <module>
        main()
      File "bisect_m1_error.py", line 59, in main
        reveal_error()
      File "bisect_m1_error.py", line 32, in reveal_error
        torch.onnx.export(conv, y, cpu_model_path, export_params=True, training=False)
      File "<@beartype(torch.onnx.utils.export) at 0x1281f5a60>", line 136, in export
      File "pytorch/venv/lib/python3.9/site-packages/beartype/_decor/_error/errormain.py", line 301, in raise_pep_call_exception
        raise exception_cls(  # type: ignore[misc]
    beartype.roar.BeartypeCallHintParamViolation: @beartyped export() parameter training=False violates type hint <class 'torch._C._onnx.TrainingMode'>, as False not instance of <protocol "torch._C._onnx.TrainingMode">.
    ```
    
    when `TORCH_ONNX_EXPERIMENTAL_RUNTIME_TYPE_CHECK` is not set and `beartype` is installed, a warning message is emitted.
    
    ```
    >>> torch.onnx.export("foo", "bar", "f")
    <stdin>:1: CallHintViolationWarning: Traceback (most recent call last):
      File "/home/justinchu/dev/pytorch/torch/onnx/_internal/_beartype.py", line 54, in _coerce_beartype_exceptions_to_warnings
        return beartyped(*args, **kwargs)
      File "<@beartype(torch.onnx.utils.export) at 0x7f1d4ab35280>", line 39, in export
      File "/home/justinchu/anaconda3/envs/pytorch/lib/python3.9/site-packages/beartype/_decor/_error/errormain.py", line 301, in raise_pep_call_exception
        raise exception_cls(  # type: ignore[misc]
    beartype.roar.BeartypeCallHintParamViolation: @beartyped export() parameter model='foo' violates type hint typing.Union[torch.nn.modules.module.Module, torch.jit._script.ScriptModule, torch.jit.ScriptFunction], as 'foo' not <protocol "torch.jit.ScriptFunction">, <protocol "torch.nn.modules.module.Module">, or <protocol "torch.jit._script.ScriptModule">.
    
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/home/justinchu/dev/pytorch/torch/onnx/_internal/_beartype.py", line 63, in _coerce_beartype_exceptions_to_warnings
        return func(*args, **kwargs)
      File "/home/justinchu/dev/pytorch/torch/onnx/utils.py", line 482, in export
        _export(
      File "/home/justinchu/dev/pytorch/torch/onnx/utils.py", line 1422, in _export
        with exporter_context(model, training, verbose):
      File "/home/justinchu/anaconda3/envs/pytorch/lib/python3.9/contextlib.py", line 119, in __enter__
        return next(self.gen)
      File "/home/justinchu/dev/pytorch/torch/onnx/utils.py", line 177, in exporter_context
        with select_model_mode_for_export(
      File "/home/justinchu/anaconda3/envs/pytorch/lib/python3.9/contextlib.py", line 119, in __enter__
        return next(self.gen)
      File "/home/justinchu/dev/pytorch/torch/onnx/utils.py", line 95, in select_model_mode_for_export
        originally_training = model.training
    AttributeError: 'str' object has no attribute 'training'
    ```
    
    We see the error is caught right when the type mismatch happens, improving from what otherwise would become `AttributeError: 'str' object has no attribute 'training'`
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83673
    Approved by: https://github.com/BowenBao

---
+++ test/onnx/internal/test_beartype.py
@@ -0,0 +1,86 @@
+# Owner(s): ["module: onnx"]
+"""Unit tests for the internal beartype wrapper module."""
+
+import unittest
+
+import torch
+from torch.onnx import _exporter_states
+from torch.onnx._internal import _beartype
+from torch.testing._internal import common_utils
+
+
+def beartype_installed():
+    try:
+        import beartype  # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
+def skip_if_beartype_not_installed(test_case):
+    return unittest.skipIf(not beartype_installed(), "beartype is not installed")(
+        test_case
+    )
+
+
+def func_with_type_hint(x: int) -> int:
+    return x
+
+
+def func_with_incorrect_type_hint(x: int) -> str:
+    return x  # type: ignore[return-value]
+
+
+@common_utils.instantiate_parametrized_tests
+class TestBeartype(common_utils.TestCase):
+    def test_create_beartype_decorator_returns_no_op_decorator_when_disabled(self):
+        decorator = _beartype._create_beartype_decorator(
+            _exporter_states.RuntimeTypeCheckState.DISABLED,
+        )
+        decorated = decorator(func_with_incorrect_type_hint)
+        decorated("string_input")  # type: ignore[arg-type]
+
+    @skip_if_beartype_not_installed
+    def test_create_beartype_decorator_warns_when_warnings(self):
+        decorator = _beartype._create_beartype_decorator(
+            _exporter_states.RuntimeTypeCheckState.WARNINGS,
+        )
+        decorated = decorator(func_with_incorrect_type_hint)
+        with self.assertWarns(torch.onnx.errors.CallHintViolationWarning):
+            decorated("string_input")  # type: ignore[arg-type]
+
+    @common_utils.parametrize("arg", [1, "string_input"])
+    @skip_if_beartype_not_installed
+    def test_create_beartype_decorator_errors_when_errors(self, arg):
+        import beartype
+
+        decorator = _beartype._create_beartype_decorator(
+            _exporter_states.RuntimeTypeCheckState.ERRORS,
+        )
+        decorated = decorator(func_with_incorrect_type_hint)
+        with self.assertRaises(beartype.roar.BeartypeCallHintViolation):
+            decorated(arg)
+
+    @skip_if_beartype_not_installed
+    def test_create_beartype_decorator_warning_calls_function_once(self):
+        call_count = 0
+
+        def func_with_incorrect_type_hint_and_side_effect(x: int) -> str:
+            nonlocal call_count
+            call_count += 1
+            return x  # type: ignore[return-value]
+
+        decorator = _beartype._create_beartype_decorator(
+            _exporter_states.RuntimeTypeCheckState.WARNINGS,
+        )
+        decorated = decorator(func_with_incorrect_type_hint_and_side_effect)
+        decorated("string_input")  # type: ignore[arg-type]
+        self.assertEqual(call_count, 1)
+        decorated(1)
+        # The return value violates the type hint, but the function is called
+        # only once.
+        self.assertEqual(call_count, 2)
+
+
+if __name__ == "__main__":
+    common_utils.run_tests()
