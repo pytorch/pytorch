9ba8a5a38496ce046028bf1649a89648ef704a4a
Catherine Lee <csl@fb.com>
2022-08-26 15:08:06 -0700

    another bad file

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -735,7 +735,8 @@ def run_tests(argv=UNITTEST_ARGS):
                               'test_cuda', 'test_indexing', 'test_fx_backends', 'test_linalg', 'test_cpp_extensions_jit',
                               'test_torch', 'test_tensor_creation_ops', 'test_sparse_csr', 'test_dispatch']
                 or test_filename in ["lazy.test_ts_opinfo", "test_foreach", "lazy.test_reuse_ir", "test_fx_backends",
-                                     "test_linalg", "distributions.test_distributions", 'nn.test_pooling', 'test_ao_sparsity']):
+                                     "test_linalg", "distributions.test_distributions", 'nn.test_pooling', 
+                                     'test_ao_sparsity', 'test_native_mha']):
             exit(0)
         # exclude linux cuda tests because we run into memory issues when running in parallel
         import pytest
