f1d7c9e1189d79874f104a296e66fffcc04fd7ef
Kevin Stephano <kevin.stephano@gmail.com>
2022-08-26 05:10:24 +0000

    Fix lint issue.

--- test/test_nvfuser_frontend.py
+++ test/test_nvfuser_frontend.py
@@ -27,11 +27,11 @@ class TestNVFuserFrontend(TestCase):
             t0 = fd.define_tensor(3)
             t1 = fd.define_tensor(3)
             c0 = fd.define_constant(3.0)
-  
+
             t2 = fd.ops.add(t0, t1)
             t3 = fd.ops.mul(t2, c0)
             t4 = fd.ops.sum(t3, [-1], False, DataType.Float)
-  
+
             fd.add_output(t4)
 
         input1 = torch.ones(2, 4, 8, device='cuda')
@@ -39,31 +39,31 @@ class TestNVFuserFrontend(TestCase):
 
         # Expected Output is a tensor of 48's
         nvf_out1 = fm.execute([input1, input2])[0]
-      
+
         # Run the same definition to check caching
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(3)
             t1 = fd.define_tensor(3)
             c0 = fd.define_constant(3.0)
-  
+
             t2 = fd.ops.add(t0, t1)
             t3 = fd.ops.mul(t2, c0)
             t4 = fd.ops.sum(t3, [-1], False, DataType.Float)
-  
+
             fd.add_output(t4)
-       
+
         nvf_out2 = fm.execute([input1, input2])[0]
         eager_out = torch.sum((input1 + input2) * 3.0, dim=-1)
         self.assertEqual(eager_out, nvf_out1)
         self.assertEqual(eager_out, nvf_out2)
-    
+
     def test_basic_fp16(self) :
         fm = FusionManager.get()
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(3, DataType.Half)
             t1 = fd.define_tensor(3, DataType.Half)
             c0 = fd.define_constant(3.0)
-  
+
             t2 = fd.ops.add(t0, t1)
             t3 = fd.ops.mul(t2, c0)
             t4 = fd.ops.sum(t3, [-1], False, DataType.Float)
@@ -78,38 +78,38 @@ class TestNVFuserFrontend(TestCase):
         nvf_out = fm.execute([input1, input2])[0]
         eager_out = torch.sum((input1 + input2) * 3.0, dim=-1)
         self.assertEqual(eager_out, nvf_out)
-    
+
     def test_cast_double_to_half(self) :
         fm = FusionManager.get()
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(2, DataType.Double)
             t1 = fd.define_tensor(2, DataType.Double)
-  
+
             t0h = fd.ops.cast(t0, DataType.Half)
             t1h = fd.ops.cast(t1, DataType.Half)
             t2 = fd.ops.add(t0h, t1h)
             t3 = fd.ops.relu(t2)
             t4 = fd.ops.cast(t3, DataType.Half)
-  
+
             fd.add_output(t4)
 
         input1 = torch.randn(2, 4, device='cuda', dtype=torch.float64)
         input2 = torch.randn(2, 4, device='cuda', dtype=torch.float64)
-    
+
         nvf_out = fm.execute([input1, input2])[0]
         eager_out = torch.relu(input1.to(torch.half) + input2.to(torch.half))
         self.assertEqual(eager_out, nvf_out)
-    
+
     def test_promote_to_double(self) :
         fm = FusionManager.get()
 
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(2, DataType.Half)
             t1 = fd.define_tensor(2, DataType.Double)
-  
+
             t2 = fd.ops.add(t0, t1)
             t5 = fd.ops.relu(t2)
-  
+
             fd.add_output(t5)
 
         input1 = torch.randn(2, 4, device='cuda', dtype=torch.float16)
@@ -118,21 +118,21 @@ class TestNVFuserFrontend(TestCase):
         nvf_out = fm.execute([input1, input2])[0]
         eager_out = torch.relu(input1 + input2)
         self.assertEqual(eager_out, nvf_out)
-    
+
     def test_implicit_broadcast_input(self) :
         fm = FusionManager.get()
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(1)
             t1 = fd.define_tensor(3)
-  
+
             t0_b = fd.ops.broadcast_in_dim(t0, [2, 3, 4], [1])
             t2 = fd.ops.add(t0_b, t1)
-  
+
             fd.add_output(t2)
 
         input1 = torch.randn(3, device='cuda')
         input2 = torch.randn(2, 3, 4, device='cuda')
-        
+
         nvf_out = fm.execute([input1, input2])[0]
         eager_out = refs.add(prims.broadcast_in_dim(input1, [2, 3, 4], [1]), input2)
         self.assertEqual(eager_out, nvf_out)
@@ -145,10 +145,10 @@ class TestNVFuserFrontend(TestCase):
         with FusionDefinition(fm) as fd :
             t0 = fd.define_tensor(sizes=input1.size(), strides=input1.stride())
             t1 = fd.define_tensor(sizes=input2.size(), strides=input2.stride())
-  
+
             t0_b = fd.ops.broadcast_in_dim(t0, [2, 3, 4], [0, 1, 2])
             t2 = fd.ops.add(t0_b, t1)
-  
+
             fd.add_output(t2)
 
         nvf_out = fm.execute([input1, input2])[0]
@@ -161,9 +161,9 @@ class TestNVFuserFrontend(TestCase):
             t0 = fd.define_tensor([3, 1], [1, 1])
             t1 = fd.define_tensor(1)
 
-            t1_b = fd.ops.broadcast_in_dim(t1, [3, 3], [0]) 
+            t1_b = fd.ops.broadcast_in_dim(t1, [3, 3], [0])
             t2 = fd.ops.add(t0, t1_b)
-  
+
             fd.add_output(t2)
 
         input1 = torch.randn(3, 1, device='cuda')
