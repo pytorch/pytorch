ae70565699f72a3a9c26c29054e25d7032343bc9
Kimish Patel <kimishpatel@fb.com>
2022-08-27 16:06:15 -0700

    [pytorch][on device quant] Finalize method for ondevice quant (#83571)
    Summary:
    After inserting quant dequant nodes in the graph, we need
    1. Insert packed param creation and quantized op
    2. Create packed_params attribute in the top module. For this we need
    graph that inlined except for calculate_qparams method calls. But they
    can be inlined too. So perhaps we need to make sure no other callmethods
    exist.
    3. Insert SetAttr for the packed param
    4. Insert GetAttr for the packed param
    5. Use GetAttr output for quantized op where applicable, e.g.
    linear_dynamic
    
    The above is added to quantize_<method-name> method created inprevious
    step. Once the above steps are done clone the method into
    quantized_<method-name>
    
    Modify quantize_<method-name>:
    1. Remove all outputs from the method.
    2. Run dce
    3. Remove all inputs from the method except self.
    
    Modify quantized_<method-name>:
    1. Remove all packed_param setAttr nodes.
    2. Run dce.
    
    This should result in removal of all nodes that generate packed param.
    
    Test Plan: To be written
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D38771416](https://our.internmc.facebook.com/intern/diff/D38771416)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83571
    Approved by: https://github.com/jerryzh168

--- torch/ao/quantization/quantize_jit.py
+++ torch/ao/quantization/quantize_jit.py
@@ -122,9 +122,11 @@ def _convert_ondevice_jit(model, method_name, inplace=False, debug=False, quant_
     assert quant_type == QuantType.DYNAMIC, "This API, while should work for static quant, is only tested for dynamic quant."
     assert not method_name.startswith("observe_"), "Pass in valid method to be quantized, e.g. forward"
     observe_method_name = "observe_" + method_name
+    quantize_method_name = "quantize_" + method_name
     model_c = model._c
     model_c = torch._C._jit_pass_insert_quant_dequant_for_ondevice_ptq(
         model._c, observe_method_name, inplace, debug, QuantType.DYNAMIC)
+    model_c = torch._C._jit_pass_quant_finalize_for_ondevice_ptq(model_c, QuantType.DYNAMIC, quantize_method_name)
     if inplace:
         model._reconstruct(model_c)
     else:
