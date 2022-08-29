89c6e9a2b7dd2a4d0598f9af154731839b379081
Vitaly Fedyunin <vitalyf@fb.com>
2022-08-29 13:20:09 -0400

    [DataLoader] Changing the way we apply sharding in dist plus mp enviroment
    [ghstack-poisoned]

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -106,15 +106,13 @@ def _get_distributed_settings():
 
 
 def _sharding_worker_init_fn(worker_init_fn, world_size, rank_id, worker_id):
-    global_worker_id = worker_id
     info = torch.utils.data.get_worker_info()
-    total_workers = info.num_workers
     datapipe = info.dataset
     # To distribute elements across distributed process evenly, we should shard data on distributed
     # processes first then shard on worker processes
-    total_workers *= world_size
-    global_worker_id = global_worker_id * world_size + rank_id
-    torch.utils.data.graph_settings.apply_sharding(datapipe, total_workers, global_worker_id)
+    torch.utils.data.graph_settings.apply_sharding(datapipe, world_size, rank_id, sharding_group = 100)
+    torch.utils.data.graph_settings.apply_sharding(datapipe, info.num_workers, worker_id, sharding_group = 200)
+
     if worker_init_fn is not None:
         worker_init_fn(worker_id)
 
