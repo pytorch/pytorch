0d58c3e507d8aaa4a6163d33c15b09ada8d21116
jataylo <jack.taylor@amd.com>
2022-08-31 12:18:36 +0000

    Pinned scipy version

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -78,11 +78,10 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   # Install PyTorch conda deps, as per https://github.com/pytorch/pytorch README
   # DO NOT install cmake here as it would install a version newer than 3.10, but
   # we want to pin to version 3.10.
-  SCIPY_VERSION=1.1.0
+  SCIPY_VERSION=1.6.3
   if [ "$ANACONDA_PYTHON_VERSION" = "3.9" ]; then
     # Install llvm-8 as it is required to compile llvmlite-0.30.0 from source
     conda_install numpy=1.19.2 astunparse pyyaml mkl mkl-include setuptools cffi future six llvmdev=8.0.0 -c conda-forge
-    SCIPY_VERSION=1.6.0
   elif [ "$ANACONDA_PYTHON_VERSION" = "3.8" ]; then
     # Install llvm-8 as it is required to compile llvmlite-0.30.0 from source
     conda_install numpy=1.18.5 astunparse pyyaml mkl mkl-include setuptools cffi future six llvmdev=8.0.0
