24682d53cc9ac3872c113984df71d8fc76c410ce
Nikita Shulga <nshulga@fb.com>
2022-08-31 19:59:31 +0000

    [BE] Delete torch._dl extension (#84361)
    And lots of complexity around the availability of RTLD_GLOBAL flags in `os` module
    As this flag is always present since Python-3.3, see https://docs.python.org/3/library/os.html#os.RTLD_GLOBAL
    
    Fixes https://github.com/pytorch/pytorch/issues/84351
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84361
    Approved by: https://github.com/kit1980

--- torch/__init__.py
+++ torch/__init__.py
@@ -171,20 +171,11 @@ if (USE_RTLD_GLOBAL_WITH_LIBTORCH or os.getenv('TORCH_USE_RTLD_GLOBAL')) and \
     # you load consistently use the same libstdc++, or you may have
     # mysterious segfaults.
     #
-    import os as _dl_flags
-    if not hasattr(_dl_flags, 'RTLD_GLOBAL') or not hasattr(_dl_flags, 'RTLD_LAZY'):
-        try:
-            # next try if DLFCN exists
-            import DLFCN as _dl_flags  # type: ignore[import, no-redef]
-        except ImportError:
-            # as a last attempt, use compile-time constants
-            import torch._dl as _dl_flags  # type: ignore[import, no-redef]
     old_flags = sys.getdlopenflags()
-    sys.setdlopenflags(_dl_flags.RTLD_GLOBAL | _dl_flags.RTLD_LAZY)
+    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
     from torch._C import *  # noqa: F403
     sys.setdlopenflags(old_flags)
     del old_flags
-    del _dl_flags
 
 else:
     # Easy way.  You want this most of the time, because it will prevent
