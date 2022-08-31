d418bdd5206033120daf708aca06e95155d60fe1
Pruthvi Madugundu <pmagundu@amd.com>
2022-08-30 17:10:58 -0700

    Updated to latest conda for CentOS stream 9

--- .circleci/docker/common/install_conda.sh
+++ .circleci/docker/common/install_conda.sh
@@ -57,8 +57,13 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
 
   # Prevent conda from updating to 4.14.0, which causes docker build failures
   # See https://hud.pytorch.org/pytorch/pytorch/commit/754d7f05b6841e555cea5a4b2c505dd9e0baec1d
-  # Uncomment the below when resolved to track the latest conda update
-  # as_jenkins conda update -y -n base conda
+  # Uncomment the below when resolved to track the latest conda update,
+  # but this is required for CentOS stream 9 builds
+  ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+  OS_VERSION=$(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"')
+  if [[ $ID == centos && $OS_VERSION == 9 ]]; then
+    as_jenkins conda update -y -n base conda
+  fi
 
   # Install correct Python version
   as_jenkins conda install -y python="$ANACONDA_PYTHON_VERSION"
