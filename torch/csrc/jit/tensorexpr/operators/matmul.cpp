d5086b840785aa6e9a386e6b468924048dad2ce5
Wu, Chunyuan <chunyuan.wu@intel.com>
2022-09-01 13:05:29 +0800

    Update on "nnc: support linear-eltwise fusion"
    [ghstack-poisoned]

--- torch/csrc/jit/tensorexpr/operators/matmul.cpp
+++ torch/csrc/jit/tensorexpr/operators/matmul.cpp
@@ -9,12 +9,13 @@ namespace tensorexpr {
 bool mkldnnPrepackedLinearIsSupported(
     const TensorInfo& input,
     const TensorInfo& weight) {
-  // TODO: only support BF16 to make sure there's no performance regression
-  // if (input.dtype != c10::ScalarType::BFloat16 ||
-  //     weight.dtype != c10::ScalarType::BFloat16) {
-  //   GRAPH_DEBUG("mkldnnPrepackedLinearIsSupported: only bfloat16 allowed");
-  //   return false;
-  // }
+  // only support BF16 since in general FP32 MKL matmul performs better than
+  // MKLDNN
+  if (input.dtype != c10::ScalarType::BFloat16 ||
+      weight.dtype != c10::ScalarType::BFloat16) {
+    GRAPH_DEBUG("mkldnnPrepackedLinearIsSupported: only bfloat16 allowed");
+    return false;
+  }
   return true;
 }
 
