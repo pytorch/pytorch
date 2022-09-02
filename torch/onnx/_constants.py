e3b7a9d40a35dc1f7bc2f5d06ef443c6f291046a
Justin Chu <justinchu@microsoft.com>
2022-09-02 17:50:21 +0000

    Update on "[ONNX] New symbolic function registry"
    ## Summary
    
    The change brings the new registry for symbolic functions in ONNX. The `SymbolicRegistry` class in `torch.onnx._internal.registration` replaces the dictionary and various functions defined in `torch.onnx.symbolic_registry`.
    
    The new registry
    
    - Has faster lookup by storing only functions in the opset version they are defined in
    - Is easier to manage and interact with due to its class design
    - Builds the foundation for the more flexible registration process detailed in #83787
    
    Implementation changes
    
    - **Breaking**: Remove `torch.onnx.symbolic_registry`
    - `register_custom_op_symbolic` and `unregister_custom_op_symbolic` in utils maintain their api for compatibility
    - Update _onnx_supported_ops.py for doc generation to include quantized ops.
    - Update code to register python ops in `torch/csrc/jit/passes/onnx.cpp`
    
    ## Profiling results
    
    -0.1 seconds in execution time. -34% time spent in `_run_symbolic_function`. Tested on the alexnet example in public doc.
    
    ### After
    ```
       └─ 1.641 export  <beartype(torch.onnx.utils.export) at 0x7f19be17f790>:1
          └─ 1.641 export  torch/onnx/utils.py:185
             └─ 1.640 _export  torch/onnx/utils.py:1331
                ├─ 0.889 _model_to_graph  torch/onnx/utils.py:1005
                │  ├─ 0.478 _optimize_graph  torch/onnx/utils.py:535
                │  │  ├─ 0.214 PyCapsule._jit_pass_onnx_graph_shape_type_inference  <built-in>:0
                │  │  │     [2 frames hidden]  <built-in>
                │  │  ├─ 0.190 _run_symbolic_function  torch/onnx/utils.py:1670
                │  │  │  └─ 0.145 Constant  torch/onnx/symbolic_opset9.py:5782
                │  │  │     └─ 0.139 _graph_op  torch/onnx/_patch_torch.py:18
                │  │  │        └─ 0.134 PyCapsule._jit_pass_onnx_node_shape_type_inference  <built-in>:0
                │  │  │              [2 frames hidden]  <built-in>
                │  │  └─ 0.033 [self]  
    ```
    
    ### Before
    ![image](https://user-images.githubusercontent.com/11205048/188032302-688d881e-860d-4046-bdba-90da54233576.png)
    
    ### Start up time
    
    The startup process takes 0.03 seconds. Calls to `inspect` will be eliminated when we switch to using decorators for registration in #84448
    
    ![image](https://user-images.githubusercontent.com/11205048/188208910-250f0434-475d-4872-9abc-781535519305.png)
    
    
    [ghstack-poisoned]

--- torch/onnx/_constants.py
+++ torch/onnx/_constants.py
@@ -1,9 +1,11 @@
 """Constant values used in ONNX."""
 
 ONNX_ARCHIVE_MODEL_PROTO_NAME = "__MODEL_PROTO"
+
+ONNX_BASE_OPSET = 9
 onnx_default_opset = 14
 onnx_main_opset = 17
 onnx_stable_opsets = tuple(range(7, onnx_main_opset))
-onnx_constant_folding_opsets = tuple(range(9, onnx_main_opset + 1))
+onnx_constant_folding_opsets = tuple(range(ONNX_BASE_OPSET, onnx_main_opset + 1))
 
 PYTORCH_GITHUB_ISSUES_URL = "https://github.com/pytorch/pytorch/issues"
