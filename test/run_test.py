f7c0ea5e1a54e21ac1d9378be989987a391912b1
joncrall <jon.crall@kitware.com>
2022-09-05 18:54:02 -0400

    only test nn doctests

--- test/run_test.py
+++ test/run_test.py
@@ -727,7 +727,8 @@ def run_doctests(test_module, test_directory, options):
     pkgpath = os.path.dirname(torch.__file__)
 
     # Hack: Only do a small subset of the doctests to try to avoid CI errors.
-    pkgpath = os.path.join(pkgpath, 'utils')
+    # pkgpath = os.path.join(pkgpath, 'utils')
+    pkgpath = os.path.join(pkgpath, 'nn')
 
     xdoctest_config = {
         'global_exec': r'\n'.join([
