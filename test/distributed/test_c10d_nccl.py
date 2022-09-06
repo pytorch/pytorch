2cdf4971dbc125f1dc7c472d71804915827b1e12
Shen Li <cs.shenli@gmail.com>
2022-09-06 21:35:12 +0000

    Add reduce_scatter_ to CommTensor (#84592)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84592
    Approved by: https://github.com/wanchaol

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -2829,6 +2829,12 @@ class CompilerTest(test_c10d_common.CompilerTest):
             torch.ones(2, 2, device=self.rank) * self.rank
         )
 
+    @skip_if_lt_x_gpu(2)
+    def test_reduce_scatter_work_wait_gpu(self):
+        self._test_reduce_scatter_work_wait(
+            torch.ones(2, 2, device=self.rank) * self.rank
+        )
+
 
 if __name__ == "__main__":
     assert (
