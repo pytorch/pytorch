38a34ffab24271ecf81106e1196ae704762097a2
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-06 20:51:09 +0000

    wip

---
+++ aten/src/ATen/native/ComparisonUtils.h
@@ -0,0 +1,16 @@
+#include <ATen/core/TensorBase.h>
+#include <algorithm>
+#include <vector>
+
+namespace at { namespace native {
+
+inline void _assert_true(bool value) {
+  AT_ASSERT(value);
+}
+
+inline void _assert_true(bool value) {
+  AT_ASSERT(value);
+}
+
+
+}}  // namespace at::native
