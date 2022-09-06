bdf520ae3b89f223547eece2d93b0ad6b3c0fd57
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-09-06 15:59:34 -0400

    Add ONNX opset version 18 support for Torch ONNX export API
    Signed-off-by: Thiago Crepaldi <thiago.crepaldi@microsoft.com>

---
+++ torch/onnx/symbolic_opset18.py
@@ -0,0 +1,20 @@
+"""This file exports ONNX ops for opset 18.
+
+Note [ONNX Operators that are added/updated in opset 18]
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+https://github.com/onnx/onnx/blob/main/docs/Changelog.md#version-18-of-the-default-onnx-operator-set
+New operators:
+    CenterCropPad
+    Col2Im
+    Mish
+    OptionalGetElement
+    OptionalHasElement
+    Pad
+    Resize
+    ScatterElements
+    ScatterND
+"""
+
+# EDITING THIS FILE? READ THIS FIRST!
+# see Note [Edit Symbolic Files] in symbolic_helper.py
