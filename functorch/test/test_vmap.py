9403b9a0e25aa9524a13a7cd5be4d149dd1470d4
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-09-06 22:31:14 +0000

    Revert "Dispatch torch.norm to linalg.vector_norm and linalg.matrix_norm (#81761)"
    This reverts commit 65beff5acb0d7c0c484bd0558bcaf8ddc9c96aab.
    
    Reverted https://github.com/pytorch/pytorch/pull/81761 on behalf of https://github.com/mehtanirav due to Breakages in pytorch/glow

--- functorch/test/test_vmap.py
+++ functorch/test/test_vmap.py
@@ -3836,11 +3836,6 @@ class TestVmapOperatorsOpInfo(TestCase):
         skip('linalg.multi_dot'),  # accepts list of tensor inputs, has its own special test
         xfail('linalg.vander'),
         xfail('linalg.vecdot'),
-        # throws in vmap on CUDA
-        # IndexError: Dimension out of range (expected to be in range of [-1, 0], but got -2)
-        # https://github.com/pytorch/pytorch/runs/8110653462?check_suite_focus=true
-        # but it passes locally
-        skip('linalg.matrix_norm', ''),
         skip('linalg.ldl_solve', ''),
     })
     def test_vmap_linalg_failure_1D_input(self, device, dtype, op):
