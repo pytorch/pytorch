bc16ea21195bf67a195094ddc0bb42889153085f
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-07 02:35:57 +0000

    Fix

---
+++ aten/src/ATen/native/ComparisonUtils.cpp
@@ -0,0 +1,36 @@
+#include <ATen/core/TensorBase.h>
+#include <algorithm>
+#include <vector>
+#include <ATen/core/TensorBody.h>
+
+namespace at { 
+
+class Tensor;
+
+namespace native {
+
+void _assert_true(bool value, c10::basic_string_view<char> message) {
+  AT_ASSERT(value, message); 
+}
+
+bool _tensor_equal(at::Tensor const& tensor, c10::OptionalArrayRef<long> sizes, c10::OptionalArrayRef<long> strides, c10::optional<c10::ScalarType> dtype) {
+  if (sizes) {
+    if (tensor.sizes() != sizes.value()) {
+      return false;
+    }
+  }
+  if (strides) {
+    if (tensor.strides() != strides.value()) {
+      return false;
+    }
+  }
+  if (dtype) {
+    if (tensor.dtype() != dtype.value()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+}
+}  // namespace at::native
