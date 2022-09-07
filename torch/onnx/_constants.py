ca44c529bc001b7b3d3e7546a6ba4bf5725bfb91
Justin Chu <justinchu@microsoft.com>
2022-09-07 09:07:40 -0700

    [ONNX] Rename constants for clarity

--- torch/onnx/_constants.py
+++ torch/onnx/_constants.py
@@ -1,9 +1,11 @@
 """Constant values used in ONNX."""
 
 ONNX_ARCHIVE_MODEL_PROTO_NAME = "__MODEL_PROTO"
-onnx_default_opset = 14
-onnx_main_opset = 17
-onnx_stable_opsets = tuple(range(7, onnx_main_opset))
-onnx_constant_folding_opsets = tuple(range(9, onnx_main_opset + 1))
+
+ONNX_MIN_OPSET = 7
+ONNX_MAX_OPSET = 17
+ONNX_DEFAULT_OPSET = 14
+ONNX_STABLE_OPSETS = tuple(range(ONNX_MIN_OPSET, ONNX_MAX_OPSET))
+ONNX_CONSTANT_FOLDING_MIN_OPSET = 9
 
 PYTORCH_GITHUB_ISSUES_URL = "https://github.com/pytorch/pytorch/issues"
