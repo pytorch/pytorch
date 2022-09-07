818261607c0001908f78b1812990c1e2967399e4
Zain Rizvi <zainr@fb.com>
2022-09-07 15:53:09 -0500

    dummy change

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -464,6 +464,7 @@ def gh_graphql(query: str, **kwargs: Any) -> Dict[str, Any]:
     rc = _fetch_url("https://api.github.com/graphql", data={"query": query, "variables": kwargs}, reader=json.load)
     if "errors" in rc:
         raise RuntimeError(f"GraphQL query {query}, args {kwargs} failed: {rc['errors']}")
+
     return cast(Dict[str, Any], rc)
 
 
