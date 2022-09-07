70535e0ba49d5e7e8ec674dd23ac033906f17d81
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-07 02:49:27 +0000

    Rm spaces

--- aten/src/ATen/native/ComparisonUtils.cpp
+++ aten/src/ATen/native/ComparisonUtils.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <ATen/core/TensorBody.h>
 
-namespace at { 
+namespace at {
 
 class Tensor;
 
 namespace native {
 
 void _assert_true(bool value, c10::basic_string_view<char> message) {
-  AT_ASSERT(value, message); 
+  AT_ASSERT(value, message);
 }
 
 bool _tensor_equal(at::Tensor const& tensor, c10::OptionalArrayRef<long> sizes, c10::OptionalArrayRef<long> strides, c10::optional<c10::ScalarType> dtype) {
