df2168b953dd97610bf567ae90ca7f6f97e6ab91
Elias Ellison <elias.ellison@gmail.com>
2022-09-07 07:55:51 -0700

    Turn on functionalization by default in functorch (#84435)
    I talked to @SherlockNoMad abt this PR and we agreed prior to brian coming back it was worth disabling this test for getting functionalization on (and that is already the state of torchdynamo)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84435
    Approved by: https://github.com/Chillee

--- functorch/test/test_pythonkey.py
+++ functorch/test/test_pythonkey.py
@@ -412,7 +412,6 @@ class TestEagerFusionOpInfo(AOTTestCase):
         xfail('cholesky'),
         xfail('cumulative_trapezoid'),
         xfail('diag_embed'),
-        xfail('linalg.householder_product'),
         xfail('logit'),
         xfail('trapezoid'),
         xfail('trapz'),
@@ -664,6 +663,7 @@ class TestAOTModuleSimplified(AOTTestCase):
         assert torch.allclose(inputs[0].grad, cloned_inputs[0].grad)
         assert torch.allclose(inputs[1].grad, cloned_inputs[1].grad)
 
+    @unittest.skip("Breaks with functionalization on by default")
     def test_aot_module_simplified_preserves_stack_trace(self):
         class MockModule(torch.nn.Module):
             def __init__(self):
