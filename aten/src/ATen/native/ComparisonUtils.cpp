e545e34ac79e35105003416e3287f3bb1163636c
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-08 23:12:36 +0000

    Feedback, tests

--- aten/src/ATen/native/ComparisonUtils.cpp
+++ aten/src/ATen/native/ComparisonUtils.cpp
@@ -9,27 +9,20 @@ class Tensor;
 
 namespace native {
 
-void _assert_true(bool value, c10::basic_string_view<char> message) {
-  AT_ASSERT(value, message);
-}
-
-bool _tensor_equal(at::Tensor const& tensor, c10::OptionalArrayRef<long> sizes, c10::OptionalArrayRef<long> strides, c10::optional<c10::ScalarType> dtype) {
+void _assert_tensor_metadata(at::Tensor const& tensor, c10::OptionalArrayRef<long> sizes, c10::OptionalArrayRef<long> strides, c10::optional<c10::ScalarType> dtype) {
+  std::stringstream msg;
   if (sizes) {
-    if (tensor.sizes() != sizes.value()) {
-      return false;
-    }
+    msg << "Tensor size mismatch! Got " << tensor.sizes().vec() << " expected" << sizes.value().vec() << std::endl;
+    AT_ASSERT(tensor.sizes() == sizes.value(), msg.str());
   }
   if (strides) {
-    if (tensor.strides() != strides.value()) {
-      return false;
-    }
+    msg << "Tensor stride mismatch! Got " << tensor.strides() << " expected" << strides.value() << std::endl;
+    AT_ASSERT(tensor.strides() == strides.value(), msg.str());
   }
   if (dtype) {
-    if (tensor.dtype() != dtype.value()) {
-      return false;
-    }
+    msg << "Tensor dtype mismatch! Got " << tensor.dtype() << " expected" << dtype.value() << std::endl;
+    AT_ASSERT(tensor.dtype() == dtype.value(), msg.str());
   }
-  return true;
 }
 
 }
