aae373d522e8086a653bdc4187260d1693736cee
Shen Li <cs.shenli@gmail.com>
2022-09-08 21:39:07 +0000

    Update on "Moving CommTensor from tests to private _spmd folder"
    [ghstack-poisoned]

--- torch/distributed/_spmd/__init__.py
+++ torch/distributed/_spmd/__init__.py
@@ -1,5 +1,6 @@
-from .comm_tensor import _wait_comm, CommTensor
+from .comm_tensor import CommTensor, _wait_comm
 
 __all__ = ["CommTensor", "_wait_comm"]
 
+# Please keep this list sorted
 assert __all__ == sorted(__all__)
