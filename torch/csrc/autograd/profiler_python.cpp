656b3ac140452ea4f2869898298efaf61c7e2224
Taylor Robie <taylorrobie@fb.com>
2022-09-08 11:03:20 -0700

    [Profiler][Trivial] Move `PythonTracerBase` to `torch/csrc/profiler/orchestration` (#83895)
    The ownership model between `RecordQueue` and `PythonTracer` is brittle; if a profiler is popped without proper shutdown it can dangle a reference in `PythonTracer` which will segfault when dereferenced. The next PR will address this; to start we simply move the code into `torch/csrc/profiler/orchestration` to limit the sloc delta when making actual changes.
    
    Differential Revision: [D38933962](https://our.internmc.facebook.com/intern/diff/D38933962/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D38933962/)!
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83895
    Approved by: https://github.com/slgong-fb

--- torch/csrc/autograd/profiler_python.cpp
+++ torch/csrc/autograd/profiler_python.cpp
@@ -24,6 +24,7 @@
 #include <torch/csrc/autograd/python_variable.h>
 #include <torch/csrc/profiler/collection.h>
 #include <torch/csrc/profiler/containers.h>
+#include <torch/csrc/profiler/orchestration/python_tracer.h>
 #include <torch/csrc/profiler/util.h>
 #include <torch/csrc/utils/pybind.h>
 #include <torch/csrc/utils/python_compat.h>
