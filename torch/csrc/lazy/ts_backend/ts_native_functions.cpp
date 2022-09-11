d550723fa01b3de706d8b55c20b51d2231cf080b
Horace He <chilli@fb.com>
2022-09-11 18:46:00 +0000

    Update base for Update on "Added support for symbolic is_contiguous"
    [ghstack-poisoned]

--- torch/csrc/lazy/ts_backend/ts_native_functions.cpp
+++ torch/csrc/lazy/ts_backend/ts_native_functions.cpp
@@ -307,13 +307,8 @@ at::Tensor LazyNativeFunctions::empty_strided_symint(
     c10::optional<at::Device> device,
     c10::optional<bool> pin_memory) {
   TORCH_LAZY_FN_COUNTER("lazy::");
-  at::Tensor t = empty_symint(
-      sym_size,
-      dtype,
-      layout,
-      device,
-      pin_memory,
-      c10::nullopt);
+  at::Tensor t =
+      empty_symint(sym_size, dtype, layout, device, pin_memory, c10::nullopt);
   auto size = c10::asIntArrayRefSlow(sym_size);
   auto stride = c10::asIntArrayRefSlow(sym_stride);
   return t.as_strided(size, stride, /*storage_offset=*/0);
