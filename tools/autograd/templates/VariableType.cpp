4ca594e2900c36654de9a9e292950c1284d709ce
soulitzer <soulitzer@gmail.com>
2022-09-12 11:47:12 -0400

    Revert "Revert "Call jit decomposition in VariableType to increase forward AD coverage (#84151)""
    This reverts commit acb4a09628284201281e262aaee58e3dc6be9c2b.
    
    ghstack-source-id: 387eda832cbd9d976acbbbb9cf5468f8e0a9b0a9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84675

--- tools/autograd/templates/VariableType.cpp
+++ tools/autograd/templates/VariableType.cpp
@@ -1,4 +1,5 @@
 #include "torch/csrc/autograd/VariableTypeUtils.h"
+#include "torch/csrc/autograd/VariableTypeUtilsDependOnOps.h"
 #include "torch/csrc/autograd/generated/VariableType.h"
 #include "torch/csrc/autograd/FunctionsManual.h"
 
