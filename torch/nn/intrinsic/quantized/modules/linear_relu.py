fa3790215113f52e26784469fb68f9ed2001bd3b
zaf <zaf@devvm10206.prn0.facebook.com>
2022-09-12 02:16:40 -0700

    [quant][ao_migration] nn.intrinsic.quantized migration to ao
    All quantization-related modules are being migrated to `torch.ao`. This migrates the `nn.intrinsic.quantized`. Please, see the [tracker](https://github.com/pytorch/pytorch/issues/81667) for the timeline.
    
    Differential Revision: [D39425515](https://our.internmc.facebook.com/intern/diff/D39425515/)
    
    [ghstack-poisoned]

--- torch/nn/intrinsic/quantized/modules/linear_relu.py
+++ torch/nn/intrinsic/quantized/modules/linear_relu.py
@@ -1,41 +1,15 @@
-import torch
-import torch.ao.nn.quantized as nnq
-import torch.ao.nn.intrinsic as nni
+# flake8: noqa: F401
+r"""Intrinsic Quantized Modules
 
-class LinearReLU(nnq.Linear):
-    r"""
-    A LinearReLU module fused from Linear and ReLU modules
+This file is in the process of migration to `torch/ao/nn/intrinsic/quantized`, and
+is kept here for compatibility while the migration process is ongoing.
+If you are adding a new entry/functionality, please, add it to the
+appropriate file under the `torch/ao/nn/intrinsic/quantized/modules`,
+while adding an import statement here.
+"""
 
-    We adopt the same interface as :class:`torch.ao.nn.quantized.Linear`.
+from torch.ao.nn.intrinsic.quantized.modules.linear_relu import LinearReLU
 
-    Attributes:
-        Same as torch.ao.nn.quantized.Linear
-
-    Examples::
-
-        >>> # xdoctest: +SKIP
-        >>> m = nn.intrinsic.LinearReLU(20, 30)
-        >>> input = torch.randn(128, 20)
-        >>> output = m(input)
-        >>> print(output.size())
-        torch.Size([128, 30])
-    """
-    _FLOAT_MODULE = nni.LinearReLU
-
-    def __init__(self, in_features, out_features, bias=True, dtype=torch.qint8):
-        super().__init__(in_features, out_features, bias, dtype)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return torch.ops.quantized.linear_relu(
-            x, self._packed_params._packed_params, self.scale, self.zero_point)
-
-    def _get_name(self):
-        return 'QuantizedLinearReLU'
-
-    @classmethod
-    def from_float(cls, mod):
-        return super(LinearReLU, cls).from_float(mod)
-
-    @classmethod
-    def from_reference(cls, ref_linear_relu, output_scale, output_zero_point):
-        return super().from_reference(ref_linear_relu[0], output_scale, output_zero_point)
+__all__ = [
+    'LinearReLU',
+]
