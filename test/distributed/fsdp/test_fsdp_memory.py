713d8b855223970dc98ec81bb722fba002ac1390
Eddie Yan <eddiey@nvidia.com>
2022-09-14 21:56:48 +0000

    [CUBLAS][CUDA GRAPHS] Explicitly set the workspace for cuBLAS handles (#83461)
    We're seeing an issue where repeatedly capturing graphs incurs increasing memory usage as cuBLAS internally allocates a new workspace for each graph even when the same handle is being used:
    https://gist.github.com/tomconerlyanth/a20c04a4a46a0f6e9ce18f5280729b36
    
    This PR works around the issue by intercepting the `CUBLAS_WORKSPACE_CONFIG` environment variable and allocating the workspace for the cuBLAS handle explicitly.
    
    CC @ptrblck
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83461
    Approved by: https://github.com/ngimel

--- test/distributed/fsdp/test_fsdp_memory.py
+++ test/distributed/fsdp/test_fsdp_memory.py
@@ -180,43 +180,45 @@ class TestFSDPMemory(FSDPTest):
         expected = {}
 
         for iteration in range(iterations):
+            # another 4 MiB per thread/stream/cuBLAS handle expected
+            # after initial cuBLAS workspace allocation change #83461
             if iteration == 0:
                 # sharded model size + 1MB temp memory
                 expected[f"iter {iteration}: start"] = sharded_model_size_mb + 1
                 # it is hard to calculate this memory size, get it from printed memory usage
                 if ckpt == "ckpt":
-                    expected[f"iter {iteration}: after fwd"] = 51
-                    expected[f"iter {iteration}: after loss"] = 51
+                    expected[f"iter {iteration}: after fwd"] = 51 + 4
+                    expected[f"iter {iteration}: after loss"] = 51 + 4
                 else:
-                    expected[f"iter {iteration}: after fwd"] = 340
-                    expected[f"iter {iteration}: after loss"] = 340
+                    expected[f"iter {iteration}: after fwd"] = 340 + 4
+                    expected[f"iter {iteration}: after loss"] = 340 + 4
                 # sharded model size + sharded grad size + 1M temp memory
-                expected[f"iter {iteration}: after bwd"] = 2 * sharded_model_size_mb + 1
+                expected[f"iter {iteration}: after bwd"] = 2 * sharded_model_size_mb + 1 + 4 + 4
             else:
                 # after optimizer step in the first iteraiton, memory usage increased by
                 # sharded_model_size_mb becasue of increased optimizer states memory usage
-                expected[f"iter {iteration}: start"] = 2 * sharded_model_size_mb + 1
+                expected[f"iter {iteration}: start"] = 2 * sharded_model_size_mb + 1 + 4 + 4
                 if ckpt == "ckpt":
                     expected[f"iter {iteration}: after fwd"] = (
-                        51 + sharded_model_size_mb
+                        51 + sharded_model_size_mb + 4 + 4
                     )
                     expected[f"iter {iteration}: after loss"] = (
-                        51 + sharded_model_size_mb
+                        51 + sharded_model_size_mb + 4 + 4
                     )
                 else:
                     expected[f"iter {iteration}: after fwd"] = (
-                        340 + sharded_model_size_mb
+                        340 + sharded_model_size_mb + 4 + 4
                     )
                     expected[f"iter {iteration}: after loss"] = (
-                        340 + sharded_model_size_mb
+                        340 + sharded_model_size_mb + 4 + 4
                     )
-                expected[f"iter {iteration}: after bwd"] = 3 * sharded_model_size_mb + 1
+                expected[f"iter {iteration}: after bwd"] = 3 * sharded_model_size_mb + 1 + 4 + 4
 
             # sharded model size + sharded grad size + optimizer states + 1M temp memory
-            expected[f"iter {iteration}: after step"] = 3 * sharded_model_size_mb + 1
+            expected[f"iter {iteration}: after step"] = 3 * sharded_model_size_mb + 1 + 4 + 4
             # grad memory is claimed after setting grad = None
             # sharded model size + optimizer states + 1M temp memory
-            expected[f"iter {iteration}: done"] = 2 * sharded_model_size_mb + 1
+            expected[f"iter {iteration}: done"] = 2 * sharded_model_size_mb + 1 + 4 + 4
 
         # Get the fsdp and checkpoint flags.
         with_ckpt = ckpt == "ckpt"
