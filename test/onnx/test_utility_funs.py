9bc50594caa2a2c360d17c0392c2064571dd8a7d
Ubuntu <titaiwang@titaiwanglinuxcpudev.y3zdd0j2xrqelnmezcgpqgmnte.jx.internal.cloudapp.net>
2022-09-14 16:48:26 +0000

    [ONNX] Enable test_custom_opsets_inverse (#85013)
    in [#87004](https://github.com/pytorch/pytorch/pull/80074), `aten::inverse` becomes alias of `aten::linalg.inv`.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85013
    Approved by: https://github.com/BowenBao

--- test/onnx/test_utility_funs.py
+++ test/onnx/test_utility_funs.py
@@ -2,7 +2,6 @@
 
 import copy
 import io
-import unittest
 
 import onnx
 
@@ -1154,16 +1153,15 @@ class TestUtilityFuns_opset9(_BaseTestCase):
         self.assertEqual(graph.opset_import[1].domain, "com.microsoft")
 
     @skipIfNoLapack
-    @unittest.skip("It started failing after #80074")
     def test_custom_opsets_inverse(self):
         class CustomInverse(torch.nn.Module):
             def forward(self, x):
                 return torch.inverse(x) + x
 
-        def inverse(g, self):
+        def linalg_inv(g, self):
             return g.op("com.microsoft::Inverse", self).setType(self.type())
 
-        register_custom_op_symbolic("::inverse", inverse, 1)
+        register_custom_op_symbolic("::linalg_inv", linalg_inv, 1)
         model = CustomInverse()
         x = torch.randn(2, 3, 3)
         f = io.BytesIO()
