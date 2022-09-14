4ec158433856415b543798fc4f34df53cda03a78
Rohan Varma <rvarm1@fb.com>
2022-09-14 14:59:34 -0700

    [CheckpointWrapper] Reentrant kwarg support (#84908)
    A temporary patch to support keyword args when reentrant checkpoint wrapper is used. This is need to unblock some crucial workloads, the ideal fix would be checking this directly into torch.utils.checkpoint.
    
    Differential Revision: [D39453453](https://our.internmc.facebook.com/intern/diff/D39453453/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84908
    Approved by: https://github.com/awgu

--- test/distributed/fsdp/test_checkpoint_wrapper.py
+++ test/distributed/fsdp/test_checkpoint_wrapper.py
@@ -54,6 +54,58 @@ class CheckpointWrapperTest(TestCase):
         for p1, p2 in zip(lin.parameters(), lin_new.parameters()):
             self.assertEqual(p1, p2)
 
+    def test_checkpoint_wrapper_kwarg_support(self):
+        class MyModel(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.lin = nn.Linear(10, 10)
+
+            def forward(self, a, b, c=None, d=None, **kwargs):
+                return (
+                    self.lin(a),
+                    self.lin(b),
+                    self.lin(c),
+                    self.lin(d)
+                )
+
+
+        for wrapper in [
+            partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.REENTRANT),
+            partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.NO_REENTRANT),
+            partial(checkpoint_wrapper, offload_to_cpu=True),
+        ]:
+            with self.subTest(wrapper=wrapper):
+                model = wrapper(MyModel())
+                self.assertTrue(isinstance(model, CheckpointWrapper))
+                # Verify kwargs can be passed in
+                inp = torch.ones(4, 10, requires_grad=True)
+                out = model(inp, inp, c=inp, d=inp, e=inp, f=inp)
+                self.assertTrue(isinstance(out, tuple))
+                self.assertEqual(4, len(out))
+                # Without kwargs should have equivalent gradient requirements.
+                out_no_kwarg = model(inp, inp, inp, inp)
+                for t1, t2 in zip(out_no_kwarg, out):
+                    self.assertEqual(t1, t2)
+                    self.assertEqual(t1.requires_grad, t2.requires_grad)
+
+        # Test model that enforces kwarg inputs
+        class ModelEnforceKwarg(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.lin = nn.Linear(10, 10)
+
+            def forward(self, *, a=None, b=None):
+                return (self.lin(a), self.lin(b))
+
+        model = checkpoint_wrapper(
+            ModelEnforceKwarg(), checkpoint_impl=CheckpointImpl.REENTRANT
+        )
+
+        inp = torch.ones(4, 10, requires_grad=True)
+        out = model(a=inp, b=inp)
+        self.assertEqual(2, len(out))
+
+
     @unittest.skipIf(not torch.cuda.is_available(), "Test requires CUDA")
     def test_checkpoint_wrapper_parity(self):
         """
@@ -150,37 +202,49 @@ class CheckpointWrapperTest(TestCase):
             def forward(self, x):
                 return self.seq(x)
 
-        model = MyModel()
-        n_linear = sum(1 if isinstance(x, nn.Linear) else 0 for x in model.modules())
 
         def check_fn(l):
             return isinstance(l, nn.Linear)
 
-        apply_activation_checkpointing_wrapper(
-            model, checkpoint_wrapper_fn=checkpoint_wrapper, check_fn=check_fn
-        )
-        n_linear_wrapped = sum(1 if isinstance(x, nn.Linear) else 0 for x in model.modules())
-        n_checkpointed = sum(1 if isinstance(x, CheckpointWrapper) else 0 for x in model.modules())
-        self.assertEqual(n_checkpointed, n_linear_wrapped)
-        self.assertEqual(n_linear, n_linear_wrapped)
-        for j in range(3):
-            self.assertTrue(isinstance(model.seq[j].lin, CheckpointWrapper))
-            self.assertTrue(isinstance(model.seq[j].nested_linear[0], CheckpointWrapper))
+        n_linear = None
 
-        inp = torch.randn(4, 10, requires_grad=True)
-        for i in range(6):
-            loss = model(inp).sum()
-            self.assertTrue(loss.requires_grad)
-            loss.backward()
-            # ensure checkpointed part of model has gradients
-            for j in range(3):
-                weight_lin = model.seq[j].lin._checkpoint_wrapped_module.weight
-                bias_lin = model.seq[j].lin._checkpoint_wrapped_module.bias
-                weight_nested_lin = model.seq[j].nested_linear[0]._checkpoint_wrapped_module.weight
-                bias_nested_lin = model.seq[j].nested_linear[0]._checkpoint_wrapped_module.bias
-                for param in [weight_lin, bias_lin, weight_nested_lin, bias_nested_lin]:
-                    self.assertTrue(param.requires_grad)
-                    self.assertFalse(param.grad is None)
+        for wrapper in [
+            partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.REENTRANT),
+            partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.NO_REENTRANT),
+        ]:
+            model = MyModel()
+            if n_linear is None:
+                n_linear = sum(
+                    1 if isinstance(x, nn.Linear) else 0 for x in model.modules()
+                )
+
+            with self.subTest(wrapper=wrapper):
+                apply_activation_checkpointing_wrapper(
+                    model, checkpoint_wrapper_fn=wrapper, check_fn=check_fn
+                )
+                n_linear_wrapped = sum(1 if isinstance(x, nn.Linear) else 0 for x in model.modules())
+                n_checkpointed = sum(1 if isinstance(x, CheckpointWrapper) else 0 for x in model.modules())
+                self.assertEqual(n_checkpointed, n_linear_wrapped)
+                self.assertEqual(n_linear, n_linear_wrapped)
+                for j in range(3):
+                    self.assertTrue(isinstance(model.seq[j].lin, CheckpointWrapper))
+                    self.assertTrue(isinstance(model.seq[j].nested_linear[0], CheckpointWrapper))
+
+                inp = torch.randn(4, 10, requires_grad=True)
+                for i in range(6):
+                    # Kwarg input
+                    loss = model(x=inp).sum()
+                    self.assertTrue(loss.requires_grad)
+                    loss.backward()
+                    # ensure checkpointed part of model has gradients
+                    for j in range(3):
+                        weight_lin = model.seq[j].lin._checkpoint_wrapped_module.weight
+                        bias_lin = model.seq[j].lin._checkpoint_wrapped_module.bias
+                        weight_nested_lin = model.seq[j].nested_linear[0]._checkpoint_wrapped_module.weight
+                        bias_nested_lin = model.seq[j].nested_linear[0]._checkpoint_wrapped_module.bias
+                        for param in [weight_lin, bias_lin, weight_nested_lin, bias_nested_lin]:
+                            self.assertTrue(param.requires_grad)
+                            self.assertFalse(param.grad is None)
 
     def test_fqn(self):
         lin = nn.Linear(10, 10, bias=False)
