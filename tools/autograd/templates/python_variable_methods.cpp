f75e669120c02430fbe6cff88704f1b581cda41e
Edward Z. Yang <ezyang@fb.com>
2022-09-14 21:09:17 -0700

    Update on "Performance optimizations to proxy tensor"
    - Lazily allocate FX nodes for size/stride accessors on proxy tensor
    - Properly track derived computations on strides/numel/etc
    - Remove unnecessary tree_map at end of proxy tensor trace checking
      invariants; we will just have to be smart (it's too expensive)
    - Avoid tree_map in sym proxy tracing
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- tools/autograd/templates/python_variable_methods.cpp
+++ tools/autograd/templates/python_variable_methods.cpp
@@ -213,7 +213,7 @@ static PyObject * THPVariable_storage_offset(PyObject* self_, PyObject* args)
     return handle_torch_function(self_, "storage_offset");
   }
   auto& self = THPVariable_Unpack(self_);
-  return wrap(self.storage_offset());
+  return py::cast(self.sym_storage_offset()).release().ptr();
   END_HANDLE_TH_ERRORS
 }
 
