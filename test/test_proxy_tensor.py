bebc27c93134a4859338270a7f581eb7cc665afc
Edward Z. Yang <ezyang@fb.com>
2022-09-14 22:00:50 -0700

    Performance optimizations to proxy tensor (#85049)
    - Lazily allocate FX nodes for size/stride accessors on proxy tensor
    - Properly track derived computations on strides/numel/etc
    - Remove unnecessary tree_map at end of proxy tensor trace checking
      invariants; we will just have to be smart (it's too expensive)
    - Avoid tree_map in sym proxy tracing
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85049
    Approved by: https://github.com/wconstab

--- test/test_proxy_tensor.py
+++ test/test_proxy_tensor.py
@@ -800,9 +800,7 @@ def forward(self, a_1):
     sym_size = torch.ops.aten.sym_size(a_1, 0);  a_1 = None
     mul = sym_size * 2;  sym_size = None
     empty = torch.ops.aten.empty.memory_format([mul], device = device(type='cpu'), pin_memory = False);  mul = None
-    sym_size_1 = torch.ops.aten.sym_size(empty, 0)
     detach = torch.ops.aten.detach.default(empty);  empty = None
-    sym_size_2 = torch.ops.aten.sym_size(detach, 0)
     return detach""")
 
     def test_symint_to_tensor(self):
@@ -814,7 +812,6 @@ def forward(self, a_1):
 def forward(self, a_1):
     sym_size = torch.ops.aten.sym_size(a_1, 0)
     div = torch.ops.aten.div.Tensor(a_1, sym_size);  a_1 = sym_size = None
-    sym_size_1 = torch.ops.aten.sym_size(div, 0)
     return div""")
 
         r = str(make_fx(f, tracing_mode="symbolic", decomposition_table=decomposition_table)(torch.empty(4)).code).strip()
@@ -825,7 +822,6 @@ def forward(self, a_1):
     lt = sym_size < 0
     eq = sym_size == sym_size;  sym_size = None
     div = torch.ops.prims.div.default(a_1, sym_float);  a_1 = sym_float = None
-    sym_size_1 = torch.ops.aten.sym_size(div, 0)
     return div""")
 
     def test_cat(self):
