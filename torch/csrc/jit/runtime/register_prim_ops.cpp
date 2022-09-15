b615cd639f1f76bced4718b2887d4f6d777107e0
Edward Z. Yang <ezyang@fb.com>
2022-09-14 22:00:50 -0700

    Performance optimizations to proxy tensor (#85049)
    - Lazily allocate FX nodes for size/stride accessors on proxy tensor
    - Properly track derived computations on strides/numel/etc
    - Remove unnecessary tree_map at end of proxy tensor trace checking
      invariants; we will just have to be smart (it's too expensive)
    - Avoid tree_map in sym proxy tracing
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85049
    Approved by: https://github.com/wconstab

--- torch/csrc/jit/runtime/register_prim_ops.cpp
+++ torch/csrc/jit/runtime/register_prim_ops.cpp
@@ -421,6 +421,11 @@ static const std::vector<OperatorGeneratorArgs> opGenArgs{
         sym_size_int,
         aliasAnalysisFromSchema()),
     OperatorGeneratorArgs(
+        TORCH_SELECTIVE_SCHEMA(
+            "aten::sym_stride.int(Tensor self, int dim) -> SymInt"),
+        sym_stride_int,
+        aliasAnalysisFromSchema()),
+    OperatorGeneratorArgs(
         TORCH_SELECTIVE_SCHEMA("aten::stride(Tensor self) -> int[]"),
         [](Stack& stack) {
           at::Tensor arg = pop(stack).toTensor();
@@ -430,6 +435,11 @@ static const std::vector<OperatorGeneratorArgs> opGenArgs{
     OperatorGeneratorArgs(
         TORCH_SELECTIVE_SCHEMA("aten::sym_numel(Tensor self) -> SymInt"),
         sym_numel,
+        aliasAnalysisFromSchema()),
+    OperatorGeneratorArgs(
+        TORCH_SELECTIVE_SCHEMA(
+            "aten::sym_storage_offset(Tensor self) -> SymInt"),
+        sym_storage_offset,
         aliasAnalysisFromSchema()),
     OperatorGeneratorArgs(
         TORCH_SELECTIVE_SCHEMA("aten::sym_stride(Tensor self) -> SymInt[]"),
