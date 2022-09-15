60e61bf9f5e97ca35219364e32a33b95c02133e8
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-15 00:43:36 +0000

    Python Dispatcher integration with C++ dispatcher (#85050)
    #84826 but without ghstack
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85050
    Approved by: https://github.com/malfet

--- test/test_python_dispatch.py
+++ test/test_python_dispatch.py
@@ -1833,5 +1833,11 @@ $1 = torch._ops.aten.add.Tensor($0, $0)""")
             e = LayoutDefaultReturn(torch.randn(4, 2), use_wrapper_subclass)
             self.assertEqual(e.layout, torch.strided)
 
+class TestPythonDispatcher(TestCase):
+    def test_basic(self):
+        x = torch.randn(2, requires_grad=True)
+        r = torch._C._EnablePythonDispatcher()
+        torch.add(x, x)
+
 if __name__ == '__main__':
     run_tests()
