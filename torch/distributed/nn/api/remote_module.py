f68ca7ed6ee74033360acbf11b481746bcd2d29b
anjali411 <chourdiaanjali123@gmail.com>
2022-09-15 10:05:11 +0000

    Add __all__ to fx, fistributed and cuda submodules
    [ghstack-poisoned]

--- torch/distributed/nn/api/remote_module.py
+++ torch/distributed/nn/api/remote_module.py
@@ -28,6 +28,7 @@ from torch.nn import Module
 from torch.nn.parameter import Parameter
 from torch.utils.hooks import RemovableHandle
 
+__all__ = ["RemoteModule"]
 
 _grad_t = Union[Tuple[Tensor, ...], Tensor]
 # See https://mypy.readthedocs.io/en/latest/generics.html#generic-methods-and-generic-self for the use
