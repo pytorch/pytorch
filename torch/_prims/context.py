32834819b776598de2e7855bd0fc0335059c3327
Kevin Stephano <kevin.stephano@gmail.com>
2022-09-15 08:37:40 +0000

    Add nvprims rand_like support for dropout.

--- torch/_prims/context.py
+++ torch/_prims/context.py
@@ -229,6 +229,15 @@ class TorchRefsNvfuserCapabilityMode(TorchRefsMode):
             )
             and "aten.var_mean" in str(func)
         )
+    def _is_rand_like(self, func):
+        result = "torch.rand_like" == torch.overrides.resolve_name(func) or (
+            (
+                isinstance(func, torch._ops.OpOverload)
+                or isinstance(func, torch._ops.OpOverloadPacket)
+            )
+            and "aten.rand_like" in str(func)
+        )
+        return result
 
     def __torch_function__(
         self,
@@ -242,5 +251,7 @@ class TorchRefsNvfuserCapabilityMode(TorchRefsMode):
         # First we intercept calls for nvfuser-specific prims bypassing generic torch._refs
         if self._is_var_mean(orig_func):
             return torch.ops.nvprims.var_mean(*args, **kwargs)
+        #if self._is_rand_like(orig_func):
+        #    return torch.ops.nvprims.rand_like(*args)#, **kwargs)
         # Then we use TorchRefsMode to interpret the rest
         return super().__torch_function__(orig_func, types, args, kwargs)
