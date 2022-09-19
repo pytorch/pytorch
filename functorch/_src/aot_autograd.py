f803fbbf7627be8ad26f04a5c455b4e40c726c2f
Edward Z. Yang <ezyang@fb.com>
2022-09-19 13:24:07 -0700

    Update base for Update on "Apply new symbolic shape strategy to make_fx symbolic mode"
    This results in some test wobbling, which looks legit.  I also
    added some debug helpers for stuff that I found useful while
    working on this.
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    [ghstack-poisoned]

--- functorch/_src/aot_autograd.py
+++ functorch/_src/aot_autograd.py
@@ -288,7 +288,58 @@ def aot_dispatch_base(flat_fn, flat_args: List[Tensor], aot_config: AOTConfig):
 
 
 def aot_dispatch_autograd(flat_fn, flat_args: List[Tensor], aot_config: AOTConfig):
-    joint_forward_backward = create_joint_forward_backward(flat_fn)
+    # Deduplicate inputs.  Suppose you have:
+    #
+    #   [a, b, a, c]
+    #
+    # We want:
+    #
+    #   remove_dupe_args([a, b, a, c]) == [a, b, c]
+    #   add_dupe_args([a, b, c]) == [a, b, a, c]
+    #
+    # This is done via (respectively):
+    #
+    #   seen_args = {2}  # what to drop
+    #   add_dupe_map = {  # how to get args from the deduped list
+    #       0: 0,
+    #       1: 1,
+    #       2: 0,
+    #       3: 2,
+    #   }
+    #
+    # Whether to use flat_args or deduped_flat_args?  flat_fn takes flat_args,
+    # and the autograd.Function must take deduped_flat_args; everything
+    # else is just getting the types right.
+
+    seen_args = {}
+    deduped_flat_args = []
+    drop_args = set()
+    add_dupe_map = {}
+    duped_arg_len = len(flat_args)
+    for i, t in enumerate(flat_args):
+        if t in seen_args:
+            drop_args.add(i)
+            add_dupe_map[i] = seen_args[t]
+            continue
+        seen_args[t] = len(deduped_flat_args)
+        add_dupe_map[i] = len(deduped_flat_args)
+        deduped_flat_args.append(t)
+
+    def remove_dupe_args(args):
+        r = []
+        for i, t in enumerate(args):
+            if i in drop_args:
+                continue
+            r.append(t)
+        return r
+
+    def add_dupe_args(args):
+        r = []
+        for i in range(duped_arg_len):
+            r.append(args[add_dupe_map[i]])
+        return r
+
+    joint_forward_backward = create_joint_forward_backward(lambda *args: flat_fn(*add_dupe_args(args)))
 
     out = flat_fn(*flat_args)
     out = pytree.tree_map(
@@ -301,7 +352,7 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Tensor], aot_config: AOTConfi
     else:
         _num_outs = 1
 
-    joint_inputs = (flat_args, out)
+    joint_inputs = (deduped_flat_args, out)
 
     if config.use_functionalize:
         # Trace once without decompositions, into a graph of ATen ops.
@@ -339,10 +390,10 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Tensor], aot_config: AOTConfi
             bw_module.print_readable()
 
         with track_graph_compiling("forward"):
-            compiled_fw_func = aot_config.fw_compiler(fw_module, flat_args)
+            compiled_fw_func = aot_config.fw_compiler(fw_module, deduped_flat_args)
 
         if config.debug_partitioner:
-            fw_outs = call_func_with_args(compiled_fw_func, flat_args)
+            fw_outs = call_func_with_args(compiled_fw_func, deduped_flat_args)
             activation_sizes = 0
             for out in fw_outs[_num_outs:]:
                 if isinstance(out, torch.Tensor):
@@ -356,9 +407,9 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Tensor], aot_config: AOTConfi
 
         @staticmethod
         @disable_torchdynamo
-        def forward(ctx, *flat_tensor_args):
+        def forward(ctx, *deduped_flat_tensor_args):
             fw_outs = call_func_with_args(
-                CompiledFunction.compiled_fw, flat_tensor_args
+                CompiledFunction.compiled_fw, deduped_flat_tensor_args
             )
             num_outs = CompiledFunction.num_outs
             ctx.save_for_backward(*fw_outs[num_outs:])
@@ -381,7 +432,7 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Tensor], aot_config: AOTConfi
 
             return tuple(out)
 
-    return CompiledFunction.apply
+    return lambda *args: CompiledFunction.apply(*remove_dupe_args(args))
 
 
 @dynamo_timed
@@ -426,28 +477,9 @@ def create_aot_dispatcher_function(
                 def convert(x):
                     return fake_mode.from_tensor(x, shape_env=shape_env)
 
-                fake_flat_tensor_args = pytree.tree_map_only(Tensor, convert, flat_args)
+                return pytree.tree_map_only(Tensor, convert, flat_args)
             else:
-                # The detach().requires_grad_() pattern can cause some subtle bugs.
-                # These will be fixed once FakeTensor is always-on for AOTAutograd.
-                #
-                # For models that might resize their inputs, the input tensors
-                # must have allow_tensor_metadata_change() set to true.
-                # detach() returns a view tensor, but with that field set to false.
-                #
-                # Specifically, this breaks quantized models
-                # (resnet50_quantized_qat and mobilenet_v2_quantized_qat)
-                # because they use a "running-mean" style op that requires
-                # resizing the running counter buffers stored on the module.
-                def make_input(x):
-                    return x.detach().requires_grad_(x.requires_grad)
-
-                fake_flat_tensor_args = pytree.tree_map_only(
-                    Tensor,
-                    make_input,
-                    flat_args,
-                )
-            return fake_flat_tensor_args
+                return flat_args
 
         fake_flat_tensor_args = process_inputs(flat_args)
 
