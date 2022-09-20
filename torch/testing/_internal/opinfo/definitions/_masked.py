c77dda4d1d5a824a6111647b78944effaaa09d14
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-09-20 11:56:06 +0000

    update xfail for decomp

--- torch/testing/_internal/opinfo/definitions/_masked.py
+++ torch/testing/_internal/opinfo/definitions/_masked.py
@@ -562,6 +562,9 @@ op_db: List[OpInfo] = [
             DecorateInfo(
                 unittest.skip("Skipped!"), "TestJit", "test_variant_consistency_jit"
             ),
+            # RuntimeError: "prod_cpu" not implemented for 'BFloat16'
+            DecorateInfo(unittest.expectedFailure, 'TestDecomp', 'test_comprehensive',
+                        dtypes=(torch.bfloat16,), device_type='cpu'),
         ),
         # Can reuse the same inputs; dim is required in both
         sample_inputs_func=sample_inputs_masked_cumops,
