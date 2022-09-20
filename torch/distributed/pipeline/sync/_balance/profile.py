273121fc6bc47820c6f99bd4c5a02036f3f78556
eqy <eddiey@nvidia.com>
2022-09-19 22:37:00 -0700

    Update profile.py

--- torch/distributed/pipeline/sync/_balance/profile.py
+++ torch/distributed/pipeline/sync/_balance/profile.py
@@ -99,6 +99,7 @@ def profile_sizes(
         detach(batch)
 
         # Detect memory usage at forward.
+        # avoid counting cuBLAS workspace allocations
         torch._C._cuda_clearCublasWorkspaces()
         memory_before = torch.cuda.memory_allocated(device)
         batch = batch.call(layer)
