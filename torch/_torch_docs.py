9c41e466baae71353acd3354d575d8939ddfd005
foram-chandra <96388449+foram-chandra@users.noreply.github.com>
2022-09-20 14:49:52 +0530

    fix sphinx warnings

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -9165,9 +9165,10 @@ Keyword args:
 
 add_docstr(
     torch.randn,
-    r"""
+    """
 randn(*size, *, out=None, dtype=None, layout=torch.strided, device=None, requires_grad=False, \
 pin_memory=False) -> Tensor
+""" + r"""
 
 Returns a tensor filled with random numbers from a normal distribution
 with mean `0` and variance `1` (also called the standard normal
