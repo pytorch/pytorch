7e3880202965728fd85e4a8164a2144126925e86
Benoit Steiner <benoitsteiner@fb.com>
2022-09-20 15:38:58 +0000

    Revert D39583438: Multisect successfully blamed D39583438 for test or build failures (#85277)
    Summary:
    This diff is reverting D39583438
    D39583438 has been identified to be causing the following test or build failures:
    Tests affected:
    - https://www.internalfb.com/intern/test/281475048999851/
    
    Here's the Multisect link:
    https://www.internalfb.com/intern/testinfra/multisect/1260522
    Here are the tasks that are relevant to this breakage:
    T124797105: 18 tests started failing for employee benoitsteiner in the last 2 weeks
    We're generating a revert to back out the changes in this diff, please note the backout may land if someone accepts it.
    
    Test Plan: NA
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D39599694
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85277
    Approved by: https://github.com/dagitses

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -684,18 +684,6 @@ Tensor prod_safe_zeros_backward(
   return grad * (exclusive_normal * exclusive_reverse).conj();
 }
 
-// checking the storage also encompasses FakeTensors, which report device and
-// dispatch keys as non-meta when not in an composite explicit kernel
-// invocation. because these backwards are implicit kernels fake tensors do not
-// appear as meta.
-bool is_meta_in_composite_kernels(const Tensor& t) {
-  if (t.is_meta()) {
-    return true;
-  }
-  return t.has_storage() &&
-      t.storage().data_ptr().device() == c10::DeviceType::Meta;
-}
-
 // note that the gradient for prod is equivalent to:
 // cumprod(exclusive, normal) * cumprod(exclusive, reverse), e.g.:
 // input:                        [    a,     b,     c]
@@ -710,7 +698,7 @@ Tensor prod_backward(
   if (input.dim() == 0) {
     return grad;
   }
-  if (is_meta_in_composite_kernels(input) || isTensorSubclassLike(input)) {
+  if (input.is_meta() || isTensorSubclassLike(input)) {
     // For Composite Compliance, always take the safer (and slower) path
     return prod_safe_zeros_backward(grad, input.contiguous().view(-1), 0)
         .view_as(input);
@@ -742,8 +730,7 @@ Tensor prod_backward(
     grad = grad.unsqueeze(dim);
     result = result.unsqueeze(dim);
   }
-
-  if (is_meta_in_composite_kernels(input) || isTensorSubclassLike(input)) {
+  if (input.is_meta() || isTensorSubclassLike(input)) {
     // For Composite Compliance, always take the safer (and slower) path
     return prod_safe_zeros_backward(grad, input, dim);
   }
