747801ae4b250f078221ac2df2e59ebce5ac29f8
Can Korkut <cannkorkut@gmail.com>
2022-09-21 21:10:26 +0300

    ensure that batch size lower than dataset lenght

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -479,6 +479,9 @@ class DataLoader(Generic[T_co]):
             # Cannot statically verify that dataset is Sized
             length = self._IterableDataset_len_called = len(self.dataset)  # type: ignore[assignment, arg-type]
             if self.batch_size is not None:  # IterableDataset doesn't allow custom sampler or batch_sampler
+                if self.batch_size > length:
+                    raise ValueError('Batch_size must be lower than dataset lenght.' 
+                                     'But got batch_size={} dataset length={}'.format(self.batch_size, length))
                 from math import ceil
                 if self.drop_last:
                     length = length // self.batch_size
