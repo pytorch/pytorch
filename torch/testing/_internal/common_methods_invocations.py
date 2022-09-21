65440075b027b42225e6cf6769b1f06b76e392fc
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-09-21 12:45:15 +0000

    Add nvFuser support for transpose (#84629)
    `torch._refs.t`, `torch._refs.transpose`, `torch._refs.permute` are all should be working now with nvFuser executor. It would also work with graphs processed by AOT Autograd as these functions are registered to the aten->ref mapping via the "register_decomposition" decorator:
    https://github.com/pytorch/pytorch/blob/07d398fb269eebe314ae898287494a2bfdc7f278/torch/_refs/__init__.py#L3125-L3126
    https://github.com/pytorch/pytorch/blob/07d398fb269eebe314ae898287494a2bfdc7f278/torch/_refs/__init__.py#L3143-L3144
    https://github.com/pytorch/pytorch/blob/07d398fb269eebe314ae898287494a2bfdc7f278/torch/_refs/__init__.py#L2548-L2549
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84629
    Approved by: https://github.com/ngimel

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -17327,7 +17327,6 @@ python_ref_db = [
     PythonRefInfo(
         "_refs.permute",
         torch_opinfo_name="permute",
-        supports_nvfuser=False,
     ),
     PythonRefInfo(
         "_refs.ravel",
@@ -17396,12 +17395,10 @@ python_ref_db = [
     PythonRefInfo(
         "_refs.transpose",
         torch_opinfo_name="transpose",
-        supports_nvfuser=False,
     ),
     PythonRefInfo(
         "_refs.t",
         torch_opinfo_name="t",
-        supports_nvfuser=False,
     ),
     PythonRefInfo(
         "_refs.unfold_copy",
