80b5d303ce83903e4ce2a4627266d2bdd8316dc2
Justin Chu <justinchu@microsoft.com>
2022-09-22 14:01:07 -0700

    Update base for Update on "[ONNX] Use `symbolic_function_group` to dispatch function calls"
    This PR implements makes SymbolicFunctionGroup callable so we can dispatch right from it and avoid a get call and a None check.
    
    This is possible because we (1) included the opset version in the graph context and (2) used a decorator to handel the legacy functions taking an additional SymbolicContext argument so all functions expect the same first argument (GraphContext) and we no need to inspect function annotations.
    
    The next PR will create a general dispatching mechanism to replace conditionals in symbolic helper
    
    Also changed
    
    - `min_supported()` now returns an optional int to handle cases when no functions exist in the function group. Updated _onnx_supported_ops to fix mypy errors because of this change.
    
    [ghstack-poisoned]

--- torch/onnx/_internal/torchscript.py
+++ torch/onnx/_internal/torchscript.py
@@ -3,12 +3,9 @@
 # TODO(justinchuby): Move more of the symbolic helper functions here and expose
 # them to the user.
 
-import dataclasses
 import numbers
 import re
-from typing import Any, Dict, Iterable, Iterator, List, Tuple, Union
-
-from typing_extensions import Protocol, runtime_checkable
+from typing import Any, Dict, Iterable, Tuple, Union
 
 import torch
 from torch import _C
@@ -20,85 +17,8 @@ from torch.onnx._internal import _beartype
 _ATTR_PATTERN = re.compile("^(.+)_(([ifstgz])|(ty))$")
 
 
-@runtime_checkable
-class GraphLike(Protocol):
-    """Protocol for all methods defined in torch.Graph, as well as `op()` and `at()`."""
-
-    def op(
-        self,
-        opname: str,
-        *raw_args: Union[torch.Tensor, _C.Value],
-        outputs: int = 1,
-        **kwargs,
-    ) -> Union[_C.Value, Tuple[_C.Value, ...]]:
-        ...
-
-    def at(
-        self, operator: str, *args, overload_name: str = "", **kwargs
-    ) -> Union[_C.Value, Tuple[_C.Value, ...]]:
-        ...
-
-    def inputs(self) -> List[_C.Value]:
-        ...
-
-    def outputs(self) -> List[_C.Value]:
-        ...
-
-    def nodes(self) -> Iterator[_C.Node]:
-        ...
-
-    def param_node(self) -> _C.Node:
-        ...
-
-    def return_node(self) -> _C.Node:
-        ...
-
-    def addInput(self, name: str) -> _C.Value:
-        ...
-
-    def eraseInput(self, i: int) -> None:
-        ...
-
-    def registerOutput(self, n: _C.Value) -> int:
-        ...
-
-    def eraseOutput(self, i: int) -> None:
-        ...
-
-    def create(self, name: str, args, num_outputs: int) -> _C.Node:
-        ...
-
-    def appendNode(self, n: _C.Node) -> _C.Node:
-        ...
-
-    def prependNode(self, n: _C.Node) -> _C.Node:
-        ...
-
-    def insertNode(self, n: _C.Node) -> _C.Node:
-        ...
-
-    def block(self) -> _C.Block:
-        ...
-
-    def setInsertPoint(self, n: Union[_C.Block, _C.Node]) -> None:
-        ...
-
-    def insert_point_guard(self, n: Union[_C.Block, _C.Node]):
-        ...
-
-    def insertPoint(self) -> _C.Node:
-        ...
-
-    def insertGraph(self, callee: _C.Graph, inputs: List[_C.Value]) -> List[_C.Value]:
-        ...
-
-    def makeMultiOutputIntoTuple(self) -> None:
-        ...
-
-
-@dataclasses.dataclass
-class GraphContext(GraphLike):
-    """Extra context for symbolic functions with all methods from torch.Graph.
+class GraphContext(_C.Graph):
+    """Extra context for symbolic functions with all methods from torch.Graph, as well as `op()` and `at()`.
 
     Attributes:
         graph: The graph being constructed.
@@ -108,13 +28,27 @@ class GraphContext(GraphLike):
         params_dict: Mapping from graph initializer name to IValue.
     """
 
-    graph: _C.Graph
-    opset: int
-    original_node: _C.Node
-    onnx_block: _C.Block
-    params_dict: Dict[str, "_C.IValue"]
-    env: Dict[_C.Value, _C.Value]
-    # TODO(justinchuby): What should we call env?
+    def __init__(
+        self,
+        graph: _C.Graph,
+        opset: int,
+        original_node: _C.Node,
+        onnx_block: _C.Block,
+        params_dict: Dict[str, "_C.IValue"],
+        # FIXME(justinchuby): What should we call env?
+        env: Dict[_C.Value, _C.Value],
+    ):
+        self.graph = graph
+        self.opset = opset
+        self.original_node = original_node
+        self.onnx_block = onnx_block
+        self.params_dict = params_dict
+        self.env = env
+
+    # Relay methods from _C.Graph for compatibility with symbolic functions that expect
+    # a _C.Graph
+    def __getattr__(self, name: str) -> Any:
+        return getattr(self.graph, name)
 
     def op(
         self,
@@ -161,11 +95,6 @@ class GraphContext(GraphLike):
         return aten_op(
             self.graph, operator, *args, overload_name=overload_name, **kwargs
         )
-
-    # Relay methods from _C.Graph for compatibility with symbolic functions that expect
-    # a _C.Graph
-    def __getattr__(self, name: str) -> Any:
-        return getattr(self.graph, name)
 
 
 @_beartype.beartype
