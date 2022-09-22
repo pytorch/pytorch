8786cbd19c91451f816ac79689dc21b31c009790
Xia, Weiwen <weiwen.xia@intel.com>
2022-09-22 09:21:43 +0800

    Add x86 backend in the same branch as adding fbgemm

--- aten/src/ATen/Context.cpp
+++ aten/src/ATen/Context.cpp
@@ -309,12 +309,7 @@ const std::vector<at::QEngine>& Context::supportedQEngines() {
 #ifdef USE_FBGEMM
     if (fbgemm::fbgemmSupportedCPU()) {
       engines.push_back(at::kFBGEMM);
-    }
-#endif
-
-// The X86 qengine is available if and only if FBGEMM is available
-#ifdef USE_FBGEMM
-    if (fbgemm::fbgemmSupportedCPU()) {
+      // The X86 qengine is available if and only if FBGEMM is available
       engines.push_back(at::kX86);
     }
 #endif
