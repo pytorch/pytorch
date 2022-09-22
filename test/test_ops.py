80355bcbf1c8572ec43f35dfd217ed32196e79bd
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-09-22 15:44:38 +0000

    Revert "Add FakeCrossRef tests for backwards, Fix Layer Norm Backward Decomp (#85417)"
    This reverts commit 9c77083965e1283763a83f72a3adf299281761e3.
    
    Reverted https://github.com/pytorch/pytorch/pull/85417 on behalf of https://github.com/clee2000 due to broke tests on trunk (and pull somehow) https://hud.pytorch.org/pytorch/pytorch/commit/9c77083965e1283763a83f72a3adf299281761e3

--- test/test_ops.py
+++ test/test_ops.py
@@ -16,7 +16,6 @@ from torch.testing._internal.common_dtype import (
     floating_and_complex_types_and,
     all_types_and_complex_and,
 )
-from test_proxy_tensor import xfail, skipOps
 
 from torch.testing._internal.common_utils import (
     TestCase,
@@ -60,8 +59,6 @@ from torch._subclasses.fake_tensor import (
     FakeTensor,
     FakeTensorMode,
 )
-from torch._subclasses.fake_utils import outputs_alias_inputs
-
 from torch.utils._python_dispatch import enable_torch_dispatch_mode
 import torch._prims as prims
 from torch._prims.context import TorchRefsMode
@@ -96,8 +93,6 @@ _ref_test_ops = tuple(
     )
 )
 _ops_and_refs = op_db + python_ref_db
-
-aten = torch.ops.aten
 
 # Tests that apply to all operators and aren't related to any particular
 #   system
@@ -1779,10 +1774,8 @@ aliasing_failures = (
     "nn.functional.pixel_unshuffle",
 )
 
-# tests which have inconsistent fake tensor stride propagation
-# XXX: no new tests should be added to this list as a result of a
-# decomp or prim, see https://github.com/pytorch/pytorch/issues/78050#issuecomment-1253950325
-fake_tensor_stride_failing_ops = [
+fake_striding_skips = (
+    "diag_embed",
     "fft.fft2",
     "fft.fft",
     "fft.fftn",
@@ -1803,35 +1796,11 @@ fake_tensor_stride_failing_ops = [
     "fft.rfftn",
     "svd",
     "linalg.svd",
-]
+)
 
-fake_backward_xfails = fake_tensor_stride_failing_ops + [
-    "linalg.cond",
-    "linalg.matrix_norm",
-    "linalg.norm",
-    "linalg.svd",
-    "linalg.svdvals",
-    "nn.functional.binary_cross_entropy_with_logits",
-    "nn.functional.huber_loss",
-    "nn.functional.logsigmoid",
-    "nn.functional.multilabel_soft_margin_loss",
-    "pca_lowrank",
-    "roll",
-    "svd_lowrank",
-    "sgn",
-    "cholesky",
-    "linalg.eigh",
-    "symeig",
-]
-
-fake_backward_xfails = [xfail(stride_skip) for stride_skip in fake_backward_xfails] + [
-    xfail("segment_reduce", "lengths"),
-    xfail("norm", "nuc"),
-    xfail('linalg.norm', 'subgradients_at_zero'),  # can accept vector inputs
-]
 
 @skipIfSlowGradcheckEnv
-class TestFakeTensor(TestCase):
+class TestFakeTensorNonErroring(TestCase):
     def _test_fake_helper(self, device, dtype, op, context):
         name = op.name
         if op.variant_test_name:
@@ -1864,6 +1833,15 @@ class TestFakeTensor(TestCase):
                     with enable_torch_dispatch_mode(mode):
                         res_fake = op(input, *args, **kwargs)
 
+                def outputs_alias_inputs(outputs, inputs):
+                    input_storages = set()
+                    for out in tree_flatten(outputs)[0]:
+                        if isinstance(out, torch.Tensor):
+                            input_storages.add(out.storage()._cdata)
+                    for inp in tree_flatten(inputs)[0]:
+                        if isinstance(inp, torch.Tensor) and inp.storage()._cdata in input_storages:
+                            return True
+                    return False
 
                 for fake_out, real_out in zip(
                     tree_flatten(res_fake)[0], tree_flatten(res)[0]
@@ -1876,10 +1854,12 @@ class TestFakeTensor(TestCase):
                     # if you see a shape exception here, you may need to add
                     # a `dynamic_output_shape` tag to an operator
 
-                    check_strides = name not in fake_tensor_stride_failing_ops
+                    check_strides = name not in fake_striding_skips
 
-                    # prims/decomps must correctly model strides,
-                    # see https://github.com/pytorch/pytorch/issues/78050#issuecomment-1253950325
+                    # if there is a striding failure here as a result of adding a primtorch ref,
+                    # feel free to add the op to `fake_striding_skips` but please tag
+                    # @eellison on the pr.
+                    # see: https://github.com/pytorch/pytorch/issues/78050
                     prims.utils.compare_tensor_meta(fake_out, real_out, check_strides)
 
                     if name not in aliasing_failures:
@@ -1907,40 +1887,12 @@ class TestFakeTensor(TestCase):
         context = torch.cuda.amp.autocast if device == "cuda" else torch.cpu.amp.autocast
         self._test_fake_helper(device, dtype, op, context)
 
-    @onlyCUDA
-    @ops([op for op in op_db if op.supports_autograd], allowed_dtypes=(torch.float,))
-    @skipOps('TestFakeTensor', 'test_fake_crossref_backward', fake_backward_xfails)
-    def test_fake_crossref_backward(self, device, dtype, op):
-        # tests fake tensor property propagation through a cross ref mode
-        # on ops which support backward
-        samples = op.sample_inputs(device, dtype, requires_grad=True)
-
-        for iter, sample in enumerate(samples):
-            args = [sample.input] + list(sample.args)
-            kwargs = sample.kwargs
-
-            # skip these to speed up tests
-            common_skip_ops = (
-                aten.detach.default,
-                aten.empty_strided.default,
-                aten.copy_.default,
-                aten.is_same_size.default,
-
-            )
-            # TODO: enable check_aliasing, too many failures :/
-            with torch._subclasses.CrossRefFakeMode(ignore_op_fn=lambda fn: fn in common_skip_ops, check_aliasing=False):
-                with warnings.catch_warnings():
-                    composite_compliance.compute_expected_grads(
-                        op.get_op(), args, kwargs,
-                        sample.output_process_fn_grad,
-                        op.gradcheck_wrapper)
-
 
 instantiate_device_type_tests(TestCommon, globals())
 instantiate_device_type_tests(TestCompositeCompliance, globals())
 instantiate_device_type_tests(TestMathBits, globals())
 instantiate_device_type_tests(TestRefsOpsInfo, globals(), only_for="cpu")
-instantiate_device_type_tests(TestFakeTensor, globals())
+instantiate_device_type_tests(TestFakeTensorNonErroring, globals())
 instantiate_device_type_tests(TestTags, globals())
 
 if __name__ == "__main__":
