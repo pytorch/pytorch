d5402ce5ec0b20634c7184d1a2bbf573447cd288
Justin Chu <justinchu@microsoft.com>
2022-09-22 11:02:59 -0700

    [ONNX] Use symbolic dispatcher
    [ghstack-poisoned]

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -45,7 +45,7 @@ from torch.onnx import (  # noqa: F401
     symbolic_helper,
 )
 from torch.onnx._globals import GLOBALS
-from torch.onnx._internal import _beartype, registration, torchscript
+from torch.onnx._internal import _beartype, dispatch, registration, torchscript
 
 __all__ = [
     "is_in_onnx_export",
@@ -1789,10 +1789,9 @@ def _run_symbolic_function(
             domain = namespace
         symbolic_function_name = f"{domain}::{op_name}"
 
-        symbolic_function_group = registration.registry.get_function_group(
-            symbolic_function_name
-        )
-        if symbolic_function_group is not None:
+        if registration.registry.is_registered_op(
+            symbolic_function_name, opset_version
+        ):
             attrs = {
                 k: symbolic_helper._node_get(node, k) for k in node.attributeNames()
             }
@@ -1800,7 +1799,9 @@ def _run_symbolic_function(
             # this is inconsistent with regular op symbolic.
             if op_name == "PythonOp":
                 inputs = (node, *inputs)
-            return symbolic_function_group(graph_context, *inputs, **attrs)
+            return dispatch.symbolics(symbolic_function_name)(
+                graph_context, inputs, attrs
+            )
 
         attrs = {
             k + "_" + node.kindOf(k)[0]: symbolic_helper._node_get(node, k)
