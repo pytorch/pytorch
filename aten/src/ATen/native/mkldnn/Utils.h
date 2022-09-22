0d0733ec9041d631c4d38901b92492b4622e55be
Sunita Nadampalli <nadampal@amazon.com>
2022-09-22 00:54:59 +0000

    [mkldnn_matmul] enable mkldnn matmul for aarch64 bf16 devices (#83671)
    this PR enables mkldnn matmul for aarch64 bf16 devices for both bf16 as well as fp32 input.
    
    This PR is dependent on
    cpuinfo commit update PR: https://github.com/pytorch/pytorch/pull/83620
    Issue: https://github.com/pytorch/pytorch/issues/83594
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83671
    Approved by: https://github.com/malfet

--- aten/src/ATen/native/mkldnn/Utils.h
+++ aten/src/ATen/native/mkldnn/Utils.h
@@ -25,8 +25,18 @@ std::vector<int64_t> pool_output_sizes(
 };
 
 inline bool mkldnn_bf16_device_check() {
-  return cpuinfo_initialize() && cpuinfo_has_x86_avx512bw()
-      && cpuinfo_has_x86_avx512vl() && cpuinfo_has_x86_avx512dq();
+  return cpuinfo_initialize() && ((cpuinfo_has_x86_avx512bw()
+     && cpuinfo_has_x86_avx512vl() && cpuinfo_has_x86_avx512dq()) || (cpuinfo_has_arm_bf16()));
 }
+
+#if defined(__aarch64__)
+inline bool mkldnn_bf16_device_check_arm() {
+  return (cpuinfo_initialize() && cpuinfo_has_arm_bf16());
+}
+#else
+constexpr bool mkldnn_bf16_device_check_arm() {
+  return false;
+}
+#endif
 
 }
