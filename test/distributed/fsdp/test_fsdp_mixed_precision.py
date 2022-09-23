b121b7045463e1ed1b428041ec878a4ecddb1884
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared47064.03.ldc1.facebook.com>
2022-09-23 04:02:10 -0700

    2022-09-23 postnightly release (71dddec6eac4e518d428d2fdc1324d421f5c8b56)

--- test/distributed/fsdp/test_fsdp_mixed_precision.py
+++ test/distributed/fsdp/test_fsdp_mixed_precision.py
@@ -305,7 +305,7 @@ class TestFSDPMixedPrecision(FSDPTest):
             param_dtype=torch.float16,
             reduce_dtype=torch.float16,
             buffer_dtype=torch.float16,
-            keep_casted_gradients=True,
+            keep_low_precision_grads=True,
         )
         m.lin1 = FSDP(m.lin1, mixed_precision=mp)
         m = FSDP(m, mixed_precision=mp)
