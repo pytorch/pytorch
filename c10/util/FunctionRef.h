bdb3ecae836c0708db16e9a2c66585ad00195ee2
Ginseng <bitnick10@gmail.com>
2022-09-24 08:16:52 +0800

    cpp20 invoke_result
    invoke_result is from c++17.h file!

--- c10/util/FunctionRef.h
+++ c10/util/FunctionRef.h
@@ -55,7 +55,7 @@ class function_ref<Ret(Params...)> {
           typename std::remove_reference<Callable>::type,
           function_ref>::value>::type* = nullptr,
       typename std::enable_if<std::is_convertible<
-          typename std::result_of<Callable && (Params && ...)>::type,
+          typename invoke_result<Callable && (Params && ...)>::type,
           Ret>::value>::type* = nullptr)
       : callback(callback_fn<typename std::remove_reference<Callable>::type>),
         callable(reinterpret_cast<intptr_t>(&callable)) {}
