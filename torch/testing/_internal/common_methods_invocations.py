127ac4af1f75255cdcb82d7360971782bd5ca593
Thytu <valentin.de-matos@epitech.eu>
2022-09-25 09:47:05 +0000

    Style change in pixel_shuffle op test
    - Rename shapes variable to shape
    - Specify shape param as kwarg
    
    Signed-off-by: Thytu <valentin.de-matos@epitech.eu>

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -7433,10 +7433,10 @@ def sample_inputs_pixel_shuffle(op_info, device, dtype, requires_grad, **kwargs)
         for upscale_factor in (1, 3)
     ] + [
         SampleInput(
-            make_tensor(shapes, device=device, dtype=dtype, requires_grad=requires_grad),
+            make_tensor(shape=shape, device=device, dtype=dtype, requires_grad=requires_grad),
             kwargs=dict(upscale_factor=1),
         )
-        for shapes in [
+        for shape in [
             (1, 1, 0, 1, 1)
             (1, 1, 1, 0, 1),
             (1, 1, 1, 1, 0),
