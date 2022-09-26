3534772961405a35eee89dbd3bbf5a4e52f67436
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-09-26 05:37:26 +0000

    Update on "Add python `nested_tensor` and `as_nested_tensor` constructors in `torch.nested`"
    [ghstack-poisoned]

--- torch/overrides.py
+++ torch/overrides.py
@@ -160,7 +160,6 @@ def get_ignored_functions() -> Set[Callable]:
         torch.mkldnn_max_pool2d,
         torch.mkldnn_max_pool3d,
         torch.mkldnn_linear_backward_weights,
-        torch.nested_tensor,
         torch.normal,
         torch.ones,
         torch.promote_types,
