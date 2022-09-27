4f6044ab5918368c75a73f6e1e3c626611372c75
Weiyi Zheng <weiyzheng@tesla.com>
2022-09-27 13:31:38 -0700

    add test

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -1638,7 +1638,7 @@ class Module:
                 if child is not None:
                     child_prefix = prefix + name + '.'
                     child_state_dict = {k: v for k, v in local_state_dict.items() if k.startswith(child_prefix)}
-                    load(child, child_state_dict, prefix + name + '.')
+                    load(child, child_state_dict, child_prefix)
 
             # Note that the hook can modify missing_keys and unexpected_keys.
             incompatible_keys = _IncompatibleKeys(missing_keys, unexpected_keys)
