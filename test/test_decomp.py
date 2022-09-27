ba110fe3e12203089b9fffccf4d35f0049eb3ea8
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-09-27 18:01:27 +0000

    Revert "Symintifying slice ops (#85196)"
    This reverts commit 4c01c51266afae57c6d6952c84fff2802d9b2bb9.
    
    Reverted https://github.com/pytorch/pytorch/pull/85196 on behalf of https://github.com/atalman due to Break internal build Exutorch

--- test/test_decomp.py
+++ test/test_decomp.py
@@ -514,6 +514,7 @@ class TestDecomp(TestCase):
                     "only backwards is decomposed, but dtype doesn't support AD"
                 )
 
+
 instantiate_device_type_tests(TestDecomp, globals())
 
 if __name__ == "__main__":
