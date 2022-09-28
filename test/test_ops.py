8a3f6090122577f560bfdf043a4c08325910842c
Elias Ellison <elias.ellison@gmail.com>
2022-09-28 20:20:56 +0000

    Turn on aliasing tests for fake backwards, Fix Batch norm running mean/var decomp aliasing (#85471)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85471
    Approved by: https://github.com/ezyang

--- test/test_ops.py
+++ test/test_ops.py
@@ -1932,7 +1932,7 @@ class TestFakeTensor(TestCase):
             )
 
             # TODO: enable check_aliasing, batch norm fails
-            with torch._subclasses.CrossRefFakeMode(ignore_op_fn=lambda fn: fn in common_skip_ops, check_aliasing=False):
+            with torch._subclasses.CrossRefFakeMode(ignore_op_fn=lambda fn: fn in common_skip_ops, check_aliasing=True):
                 with warnings.catch_warnings(), context():
                     composite_compliance.compute_expected_grads(
                         op.get_op(), args, kwargs,
