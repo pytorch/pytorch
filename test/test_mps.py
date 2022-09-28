6e49ea41be80c527386941ed949f9d1c279eeeb4
Abhishek Pathak <apathak2@apple.com>
2022-09-28 16:00:33 -0700

    Remove trailing spaces

--- test/test_mps.py
+++ test/test_mps.py
@@ -7189,8 +7189,8 @@ class TestConsistency(TestCase):
         'clamp': ['f32', 'i16', 'i32', 'i64', 'u8'],
         'clamp_max': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'clamp_min': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
-        'logical_and': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'], 
-        'logical_or': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'], 
+        'logical_and': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
+        'logical_or': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'logical_xor': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8']}
 
 
