2ad1eff711be148829a831d1c9b956b730e97d90
Peter Bell <peterbell10@live.co.uk>
2022-09-28 17:38:25 +0100

    Improve make_tensor performance for float and complex types (#85473)
    For floating types, `make_tensor` calls `rand` and then does a linear
    interpolation from `low` to `high`. This instead calls `uniform_(low,
    high)` to cut out the interpolation step.
    
    For complex types, `make_tensor` does the `rand` + interpolation step
    twice and calls `torch.complex(real, imag)` at the end. This instead
    uses `view_as_real` and `uniform_(low, high)` to fuse it all into one
    operation.
    
    My benchmarks show significant speedups in all cases for float32 and
    complex64.
    
    | Device | dtype     | Size  | Master (us) | This PR (us) | Speedup |
    |--------|-----------|-------|-------------|--------------|---------|
    | CPU    | float32   | 8     | 19.4        | 6.34         | 3.1     |
    |        |           | 4096  | 36.8        | 21.3         | 1.7     |
    |        |           | 2**24 | 167,000     | 80,500       | 2.1     |
    |        | complex32 | 8     | 37.0        | 7.57         | 4.9     |
    |        |           | 4096  | 73.1        | 37.6         | 1.9     |
    |        |           | 2**24 | 409,000     | 161,000      | 2.5     |
    | CUDA   | float32   | 8     | 40.4        | 11.7         | 3.5     |
    |        |           | 4096  | 38.7        | 11.7         | 3.3     |
    |        |           | 2**24 | 2,300       | 238          | 9.7     |
    |        | complex32 | 8     | 78.7        | 14           | 5.6     |
    |        |           | 4096  | 82.7        | 13.8         | 6.0     |
    |        |           | 2**24 | 5,520       | 489          | 11.3    |
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85473
    Approved by: https://github.com/mruberry

--- torch/testing/_internal/opinfo/definitions/_masked.py
+++ torch/testing/_internal/opinfo/definitions/_masked.py
@@ -438,12 +438,12 @@ op_db: List[OpInfo] = [
         ),
         decorators=[
             DecorateInfo(
-                toleranceOverride({torch.bfloat16: tol(atol=1e-03, rtol=1e-03)}),
-                "TestReductions",
-                "test_reference_masked",
-            ),
-            DecorateInfo(
-                toleranceOverride({torch.float16: tol(atol=1e-03, rtol=1e-03)}),
+                toleranceOverride(
+                    {
+                        torch.bfloat16: tol(atol=1e-03, rtol=5e-2),
+                        torch.float16: tol(atol=1e-03, rtol=5e-3),
+                    }
+                ),
                 "TestReductions",
                 "test_reference_masked",
             ),
@@ -451,6 +451,16 @@ op_db: List[OpInfo] = [
                 toleranceOverride({torch.float16: tol(atol=1e-02, rtol=1e-03)}),
                 "TestReductions",
                 "test_ref_small_input",
+            ),
+            DecorateInfo(
+                toleranceOverride(
+                    {
+                        torch.bfloat16: tol(atol=0.1, rtol=0.1),
+                        torch.float16: tol(atol=5e-3, rtol=5e-3),
+                    }
+                ),
+                "TestMasked",
+                "test_mask_layout",
             ),
         ],
         sample_inputs_func=sample_inputs_masked_reduction,
@@ -518,6 +528,11 @@ op_db: List[OpInfo] = [
                 "TestReductions",
                 "test_ref_duplicate_values",
             ),
+            DecorateInfo(
+                toleranceOverride({torch.float16: tol(atol=1e-03, rtol=1e-03)}),
+                "TestReductions",
+                "test_ref_small_input",
+            ),
         ],
         sample_inputs_func=sample_inputs_masked_reduction,
         sample_inputs_sparse_coo_func=sample_inputs_sparse_coo_masked_reduction,
@@ -576,6 +591,12 @@ op_db: List[OpInfo] = [
                 "test_comprehensive",
                 dtypes=(torch.bfloat16,),
                 device_type="cpu",
+            ),
+            DecorateInfo(
+                toleranceOverride({torch.float32: tol(atol=1e-5, rtol=1e-5)}),
+                "TestCompositeCompliance",
+                "test_backward",
+                device_type="cuda",
             ),
         ),
         # Can reuse the same inputs; dim is required in both
@@ -766,9 +787,25 @@ op_db: List[OpInfo] = [
         ),
         decorators=[
             DecorateInfo(
-                toleranceOverride({torch.float16: tol(atol=1e-03, rtol=1e-03)}),
+                toleranceOverride(
+                    {
+                        torch.bfloat16: tol(atol=1e-03, rtol=0.05),
+                        torch.float16: tol(atol=1e-03, rtol=1e-03),
+                    }
+                ),
                 "TestReductions",
                 "test_reference_masked",
+            ),
+            DecorateInfo(
+                toleranceOverride({torch.float16: tol(atol=1e-03, rtol=1e-03)}),
+                "TestReductions",
+                "test_ref_small_input",
+            ),
+            DecorateInfo(
+                toleranceOverride({torch.float16: tol(atol=1e-03, rtol=2e-03)}),
+                "TestSparseCompressed",
+                "test_consistency",
+                device_type="cuda",
             ),
         ],
         sample_inputs_func=sample_inputs_masked_reduction,
@@ -891,6 +928,16 @@ op_db: List[OpInfo] = [
                 "TestCudaFuserOpInfo",
                 "test_nvfuser_correctness",
             ),
+            DecorateInfo(
+                toleranceOverride(
+                    {
+                        torch.float16: tol(atol=1e-02, rtol=1e-02),
+                        torch.bfloat16: tol(atol=1e-03, rtol=1e-03),
+                    }
+                ),
+                "TestMasked",
+                "test_reference_masked",
+            ),
         ],
         sample_inputs_func=sample_inputs_masked_std_var,
         gradcheck_wrapper=gradcheck_wrapper_masked_operation,
@@ -944,7 +991,12 @@ op_db: List[OpInfo] = [
         ),
         decorators=[
             DecorateInfo(
-                toleranceOverride({torch.float16: tol(atol=1e-02, rtol=1e-02)}),
+                toleranceOverride(
+                    {
+                        torch.bfloat16: tol(atol=1e-02, rtol=1e-02),
+                        torch.float16: tol(atol=1e-02, rtol=1e-02),
+                    }
+                ),
                 "TestReductions",
                 "test_reference_masked",
             ),
@@ -954,7 +1006,12 @@ op_db: List[OpInfo] = [
                 "test_ref_small_input",
             ),
             DecorateInfo(
-                toleranceOverride({torch.float16: tol(atol=1e-02, rtol=1e-02)}),
+                toleranceOverride(
+                    {
+                        torch.float16: tol(atol=1e-02, rtol=1e-02),
+                        torch.bfloat16: tol(atol=5e-03, rtol=5e-04),
+                    }
+                ),
                 "TestMasked",
                 "test_reference_masked",
             ),
