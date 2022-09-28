34a86d0325f5ca801275b46d63994cb2887789a7
Richard Zou <zou3519@gmail.com>
2022-09-28 12:23:08 -0700

    Re-introduce the functorch docs build
    We deleted it when merging functorch into pytorch. This PR makes a new
    functorch docs build.
    
    The docs are relatively simple:
    - cd into `functorch/docs` and run `make html` to build the docs.
    - docs should get pushed to the pytorch/functorch repo's gh-pages
    branch.
    
    The long term plan is:
    - one day, the functorch APIs will just be torch.* APIs, at which point
    we can fold all of the functorch docs into the regular PyTorch docs
    - When that happens, the functorch examples and tutorials (that are on
    the functorch docs site) can be moved to the pytorch examples and
    pytorch tutorials.
    
    Test Plan:
    - check docs preview
    - watch this PR after it goes in
    
    [ghstack-poisoned]

---
+++ .circleci/scripts/functorch_doc_push_script.sh
@@ -0,0 +1,45 @@
+# =================== The following code **should** be executed inside Docker container ===================
+
+# Install dependencies
+sudo apt-get -y update
+sudo apt-get -y install expect-dev
+
+# This is where the local pytorch install in the docker image is located
+pt_checkout="/var/lib/jenkins/workspace"
+source "$pt_checkout/.jenkins/pytorch/common_utils.sh"
+echo "functorch_doc_push_script.sh: Invoked with $*"
+
+set -ex
+
+version=${DOCS_VERSION:-nightly}
+echo "version: $version"
+
+# Build functorch docs
+pushd $pt_checkout
+pushd functorch/docs
+pip -q install -r requirements.txt
+make html
+popd
+
+
+if [[ "${WITH_PUSH:-}" == false ]]; then
+	exit 0
+fi
+
+# Push functorch docs
+git clone https://github.com/pytorch/functorch -b gh-pages
+pushd functorch
+
+git rm -rf "$install_path" || true
+mv "$pt_checkout/functorch/docs/build/html" "$install_path"
+
+git add "$install_path" || true
+git status
+git config user.email "soumith+bot@pytorch.org"
+git config user.name "pytorchbot"
+# If there aren't changes, don't make a commit; push is no-op
+git commit -m "Generate Python docs from pytorch/pytorch@${GITHUB_SHA}" || true
+git status
+git push -u origin gh-pages
+popd
+# =================== The above code **should** be executed inside Docker container ===================
