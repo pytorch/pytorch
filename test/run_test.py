22dc1c2bb6707213a49f07ea3f6b8ee4ecc4ec2a
Catherine Lee <csl@fb.com>
2022-09-30 11:52:14 -0700

    print expected time

--- test/run_test.py
+++ test/run_test.py
@@ -1017,7 +1017,8 @@ def get_selected_tests(options, test_times: Dict[str, float]) -> List[TestJob]:
     assert num_shards <= len(selected_tests), f"Number of shards must be less than {len(selected_tests)}"
 
     shards = calculate_shards(num_shards, selected_tests, test_times)
-    _, tests_from_shard = shards[which_shard - 1]
+    time, tests_from_shard = shards[which_shard - 1]
+    print_to_stderr(f"expected time: {time / 60} m")
     selected_tests = tests_from_shard
 
     if options.distributed_tests:
