57ca4655d6e452e1b38f9b882b854f913de54088
Catherine Lee <csl@fb.com>
2022-09-29 18:56:20 -0700

    undo failure

--- test/test_ops.py
+++ test/test_ops.py
@@ -128,7 +128,6 @@ class TestCommon(TestCase):
     @deviceCountAtLeast(2)
     @ops(op_db, allowed_dtypes=(torch.float32, torch.long))
     def test_multiple_devices(self, devices, dtype, op):
-        assert 1 + 2 == 1
         for cuda_device_str in devices:
             cuda_device = torch.device(cuda_device_str)
             # NOTE: only tests on first sample
