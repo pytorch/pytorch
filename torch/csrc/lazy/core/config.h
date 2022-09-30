1b10b47d831bdf4e68fd9a27271436b14dfa471b
Antonio Kim <antonio.kim@cerebras.net>
2022-09-30 19:25:38 +0000

    Add torch_lazy_all_numbers_special_scalars flag (#85902)
    This is to allow even non zero and one scalars to appear as constants in the graph. The assumption being that none of them will change.
    
    The flag is set to `false` by default to preserve the original behaviour.
    
    CC: @wconstab @JackCaoG @ke1337 @vaibhavc-cerebras @glebk-cerebras
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85902
    Approved by: https://github.com/wconstab

--- torch/csrc/lazy/core/config.h
+++ torch/csrc/lazy/core/config.h
@@ -4,6 +4,7 @@
 
 C10_DECLARE_bool(torch_lazy_ir_debug);
 C10_DECLARE_bool(torch_lazy_handle_special_scalars);
+C10_DECLARE_bool(torch_lazy_all_numbers_special_scalars);
 C10_DECLARE_bool(torch_lazy_param_aliasing);
 C10_DECLARE_bool(torch_lazy_reuse_ir);
 C10_DECLARE_bool(torch_lazy_use_thread_pool);
