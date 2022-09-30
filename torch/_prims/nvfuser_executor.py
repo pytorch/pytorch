703125aacf427df82cc09d30fbf79760bafd41a9
jjsjann123 <alex.jann2012@gmail.com>
2022-09-30 03:59:56 -0700

    fixing test

--- torch/_prims/nvfuser_executor.py
+++ torch/_prims/nvfuser_executor.py
@@ -214,8 +214,17 @@ def nvfuser_execute(gm: GraphModule, *args, executor_parameters=None):
 class NvfuserPrimOperatorSupport(torch.fx.passes.operator_support.OperatorSupport):
     def is_node_supported(self, submodules, node: torch.fx.Node) -> bool:
         # special case to stop lowering to nvprim when converting to an unsupported type
-        if node.op == "call_function" and node.target == torch.ops.prims.convert_element_type.default:
-            return _torch_dtype_to_nvfuser_dtype_map.get(node.args[1]) is not None
+        if (
+            node.op == "call_function"
+            and node.target == torch.ops.nvprims.convert_element_type.default
+        ):
+            return (
+                _torch_dtype_to_nvfuser_dtype_map.get(node.args[1]) is not None
+                and _torch_dtype_to_nvfuser_dtype_map.get(
+                    node.args[0].meta["tensor_meta"].dtype
+                )
+                is not None
+            )
         return (
             node.op == "call_function"
             and getattr(node.target, "impl_nvfuser", None) is not None
