1ad0048b64d0e709482d387419947c9142b94b04
Min Si <msi@fb.com>
2022-09-30 05:13:48 +0000

    Refactor distribuetd to use absolute header path  (#85780)
    Headers under torch/csrc/distributed may be referened with relative path, e.g., "<c10d/...>". However, relative path cannot be gracefully handled by Meta internal build when the NCCL PG is hipified to support AMD/RCCL because the "hipified" header files are generated in other directories. Moreover, using absolute path for header inclusion is the state-of-the-art in most components in Pytorch. Thus, this patch refactors all header paths in torch/csrc/distributed to be absolute.
    
    See D39835774 for more details about Meta internal complication.
    
    **How to test**: commit 9e5d199 removes -I./torch/csrc/distributed in compile options. Thus use it to verify we don't miss any relative path use of torch/csrc/distributed headers.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85780
    Approved by: https://github.com/kumpera, https://github.com/huydhn

--- torch/csrc/distributed/c10d/sequence_num.cpp
+++ torch/csrc/distributed/c10d/sequence_num.cpp
@@ -1,6 +1,6 @@
 #include <ATen/ThreadLocalState.h>
 #include <c10/util/Optional.h>
-#include <c10d/sequence_num.hpp>
+#include <torch/csrc/distributed/c10d/sequence_num.hpp>
 
 #include <c10/util/Logging.h>
 
