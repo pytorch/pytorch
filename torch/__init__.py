8f0251920839726558b45792016fa4c187b761a8
soulitzer <soulitzer@gmail.com>
2022-09-30 15:40:25 -0400

    Lazily load decompositions for jvp (#85989)
    Reduces time it takes to run `python -c "import torch"` by ~10%
    
    See https://github.com/pytorch/pytorch/issues/85513
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85989
    Approved by: https://github.com/albanD, https://github.com/zou3519

--- torch/__init__.py
+++ torch/__init__.py
@@ -896,29 +896,6 @@ def compiled_with_cxx11_abi():
 from torch._ops import ops
 from torch._classes import classes
 
-# Import from torch._decomp import decompositions_for_jvp to register
-# decompositions for jvp to the jit registry
-# (decompositions_for_jvp depends on torch.ops, so we place it after)
-#
-# FIXME: We specify that __debug__ must be True because
-# if python is run with -OO or -O flags (i.e., __debug__ is False), we encounter the
-# following error:
-#
-# Return value was annotated as having type Tuple[NoneType, NoneType] but is actually of
-# type Tuple[Tensor, Tensor]:
-#   File ".../torch/_decomp/__init__.py", line 1585
-#     else:
-#         buffer = z
-#     return min - torch.log1p(z), buffer
-#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <--- HERE
-# Currently broken for 3.11, see https://github.com/pytorch/pytorch/issues/85506
-if (os.environ.get("PYTORCH_JIT", "1" if sys.version_info < (3, 11) else "0") == "1" and
-        __debug__ and
-        not torch._C._is_deploy_enabled() and
-        os.environ.get('PYTORCH_DISABLE_LIBRARY', "0") == "0"):
-    from torch._decomp import decompositions_for_jvp
-    del decompositions_for_jvp
-
 # quantization depends on torch.fx
 # Import quantization
 from torch import quantization as quantization
