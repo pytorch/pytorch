1d7016bceb15ae0f7a3515011fa399f2f8b89369
Yu, Guangye <guangye.yu@intel.com>
2022-09-30 15:24:47 +0000

    refine code

--- aten/src/ATen/detail/XPUHooksInterface.cpp
+++ aten/src/ATen/detail/XPUHooksInterface.cpp
@@ -1,8 +1,7 @@
 #include <ATen/detail/XPUHooksInterface.h>
 
-#include <c10/util/Exception.h>
+#include <c10/util/CallOnce.h>
 
-#include <cstddef>
 #include <memory>
 #include <mutex>
 
@@ -12,8 +11,8 @@ namespace detail {
 static XPUHooksInterface *xpu_hooks = nullptr;
 
 const XPUHooksInterface &getXPUHooks() {
-  static std::once_flag once;
-  std::call_once(once, [] {
+  static c10::once_flag once;
+  c10::call_once(once, [] {
     xpu_hooks =
         XPUHooksRegistry()->Create("XPUHooks", XPUHooksArgs{}).release();
     if (!xpu_hooks) {
--- build_variables.bzl
+++ build_variables.bzl
@@ -1090,6 +1090,7 @@ aten_cpu_source_non_codegen_list = [
     "aten/src/ATen/detail/CUDAHooksInterface.cpp",
     "aten/src/ATen/detail/HIPHooksInterface.cpp",
     "aten/src/ATen/detail/ORTHooksInterface.cpp",
+    "aten/src/ATen/detail/XPUHooksInterface.cpp",
     "aten/src/ATen/metal/Context.cpp",
     "aten/src/ATen/native/AutogradComposite.cpp",
     "aten/src/ATen/native/ComparisonUtils.cpp",
