197dd099a927d5a14390992135385b0a9f9d48ad
Catherine Lee <csl@fb.com>
2022-09-30 00:02:59 -0700

    options.shard

--- test/run_test.py
+++ test/run_test.py
@@ -1012,7 +1012,7 @@ def get_selected_tests(options, test_times: Dict[str, float]) -> List[TestJob]:
     # sharding
     which_shard, num_shards = options.shard or (1, 1)
 
-    assert min(options.shard) > 0, "Shards must be positive numbers"
+    assert min(which_shard, num_shards) > 0, "Shards must be positive numbers"
     assert which_shard <= num_shards, "Selected shard must be less than or equal to total number of shards"
     assert num_shards <= len(selected_tests), f"Number of shards must be less than {len(selected_tests)}"
 
