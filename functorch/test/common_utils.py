66a8c8dcb10a5429d0376b8da4912a7e5bceee8f
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-10-01 06:32:19 +0000

    [functorch] refactor: get_exhaustive_batched_inputs (#85965)
    `get_exhaustive_batched_inputs_batch_norm_is_training` and `get_exhaustive_batched_inputs` are same except for a couple of lines.
    
    We move the above functionality into `generate_vmap_inputs` (which is now only function to create batched inputs)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85965
    Approved by: https://github.com/zou3519

--- functorch/test/common_utils.py
+++ functorch/test/common_utils.py
@@ -173,27 +173,6 @@ def construct_in_dims(bdim_choice_for_tensors, is_tensors):
         result.append(next(bdim))
     return tuple(result)
 
-def get_exhaustive_batched_inputs(arg_values, kwarg_values, batch_size=2):
-    flat_args, arg_spec = pytree.tree_flatten(tuple(arg_values))
-    is_tensors = [isinstance(a, torch.Tensor) for a in flat_args]
-    bdim_choices = get_bdim_choices(sum(is_tensors))
-
-    @memoize
-    def get_batched_arg(arg, bdim):
-        assert isinstance(arg, torch.Tensor)
-        assert bdim is not None
-        result, _ = add_batch_dim(arg, bdim, batch_size)
-        return result
-
-    for bdim_choice in bdim_choices:
-        flat_in_dims = construct_in_dims(bdim_choice, is_tensors)
-
-        flat_batched_args = tuple(arg if in_dim is None else get_batched_arg(arg, in_dim)
-                                  for arg, in_dim in zip(flat_args, flat_in_dims))
-        batched_args = pytree.tree_unflatten(flat_batched_args, arg_spec)
-        in_dims = pytree.tree_unflatten(flat_in_dims, arg_spec)
-        yield batched_args, in_dims, kwarg_values
-
 
 def is_batch_norm_training(op_name, kwarg_values):
     batch_norm_fns = ("nn.functional.batch_norm", "nn.functional.instance_norm")  # instance norm calls batch norm
@@ -210,13 +189,16 @@ def is_batch_norm_training(op_name, kwarg_values):
         return is_training[0]
 
 
-def get_exhaustive_batched_inputs_batch_norm_is_training(arg_values, kwarg_values, batch_size=2):
+def generate_vmap_inputs(arg_values, kwarg_values, is_batch_norm_and_training=False, batch_size=2):
     flat_args, arg_spec = pytree.tree_flatten(tuple(arg_values))
     is_tensors = [isinstance(a, torch.Tensor) for a in flat_args]
     num_tensors = sum(is_tensors)
-    if num_tensors == 1:  # if there's only an input, can't batch it since running_mean/var will be seen as unbatched tensors
+    # For Batch Norm, if there's only an input, we can't
+    # batch it since running_mean/var will be seen as unbatched tensors
+    if num_tensors == 1 and is_batch_norm_and_training:
         return
-    bdim_choices = get_bdim_choices_batch_norm(num_tensors, *arg_values)
+    bdim_choices = get_bdim_choices_batch_norm(
+        num_tensors, *arg_values) if is_batch_norm_and_training else get_bdim_choices(num_tensors)
 
     @memoize
     def get_batched_arg(arg, bdim):
@@ -233,13 +215,6 @@ def get_exhaustive_batched_inputs_batch_norm_is_training(arg_values, kwarg_value
         batched_args = pytree.tree_unflatten(flat_batched_args, arg_spec)
         in_dims = pytree.tree_unflatten(flat_in_dims, arg_spec)
         yield batched_args, in_dims, kwarg_values
-
-
-def generate_vmap_inputs(args, kwargs, is_batch_norm_and_training=False, batch_size=2):
-    if is_batch_norm_and_training:
-        return get_exhaustive_batched_inputs_batch_norm_is_training(
-            args, kwargs, batch_size)
-    return get_exhaustive_batched_inputs(args, kwargs, batch_size)
 
 
 def clone_if_tensor(x):
