9faaf5fd231ff06da30a801937040dfeede72403
John Detloff <johndetloff@fb.com>
2022-10-03 06:43:06 +0000

    Re-add benchmarking files to ios TestApp (#85539)
    Fixes #76033
    
    The benchmarking code in the iOS TestApp was removed a while back as dead code:
    https://github.com/pytorch/pytorch/pull/64849
    
    I believe this was done in error - as this leaves our TestApp empty, nothing occurs when it runs. And we still have a tutorial up demonstrating how to use the benchmarking feature of the TestApp.
    
    This diff restores the files that were deleted, with some minor tweaks for compatibility with changes that have happened since they were deleted.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85539
    Approved by: https://github.com/kimishpatel

--- ios/TestApp/TestApp/ViewController.mm
+++ ios/TestApp/TestApp/ViewController.mm
@@ -1,12 +1,45 @@
 #import "ViewController.h"
 
+#import "Benchmark.h"
+
 @interface ViewController ()
+@property(nonatomic, strong) UITextView* textView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  self.textView = [[UITextView alloc] initWithFrame:self.view.bounds];
+  self.textView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  [self.view addSubview:self.textView];
+
+  NSData* configData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"config" ofType:@"json"]];
+  if (!configData) {
+    NSLog(@"Config.json not found!");
+    return;
+  }
+
+  NSError* err;
+  NSDictionary* config = [NSJSONSerialization JSONObjectWithData:configData options:NSJSONReadingAllowFragments error:&err];
+  if (err) {
+    NSLog(@"Parse config.json failed!");
+    return;
+  }
+
+  [Benchmark setup:config];
+  [self runBenchmark];
+}
+
+- (void)runBenchmark {
+  self.textView.text = @"Start benchmarking...\n";
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    NSString* text = [Benchmark run];
+    dispatch_async(dispatch_get_main_queue(), ^{
+      self.textView.text = [self.textView.text stringByAppendingString:text];
+    });
+  });
 }
 
 @end
--- ios/TestApp/benchmark/setup.rb
+++ ios/TestApp/benchmark/setup.rb
@@ -46,13 +46,20 @@ group = project.main_group.find_subpath(File.join('TestApp'),true)
 group.set_source_tree('SOURCE_ROOT')
 group.files.each do |file|
     if (file.name.to_s.end_with?(".pt") ||
-        file.name.to_s.end_with?(".ptl"))
+        file.name.to_s.end_with?(".ptl") ||
+        file.name == "config.json")
         group.remove_reference(file)
         targets.each do |target|
             target.resources_build_phase.remove_file_reference(file)
         end
     end
 end
+
+config_path = File.expand_path("./config.json")
+if not File.exist?(config_path)
+    raise "config.json can't be found!"
+end
+config_file_ref = group.new_reference(config_path)
 
 file_refs = []
 # collect models
@@ -66,6 +73,7 @@ Dir.foreach(models_dir) do |model|
 end
 
 targets.each do |target|
+    target.resources_build_phase.add_file_reference(config_file_ref, true)
     file_refs.each do |ref|
         target.resources_build_phase.add_file_reference(ref, true)
     end
