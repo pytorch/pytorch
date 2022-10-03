268680fd617fd653f363c7f98ef5c96d92380a6d
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-10-03 17:53:21 +0300

    Use concrete overload for batch_norm from decompositions.py
    `cudnn_batch_norm_backward`'s implementation calls
    `aten.native_batch_norm_backward` that is not handled by nvprims
    translation. This call should either be just
    `native_batch_norm_backward` (calling into local function) or
    `aten.native_batch_norm_backward.default` because keys to the
    `torch._decomp.decomposition_table` are specific overloads.

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -1653,7 +1653,7 @@ def cudnn_batch_norm_backward(
     epsilon: float,
     reserveSpace: Tensor,
 ):
-    return aten.native_batch_norm_backward(
+    return aten.native_batch_norm_backward.default(
         grad_output,
         input,
         weight,
