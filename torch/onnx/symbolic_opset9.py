419f111a47ea55002a65987d2be875c5a20e45f3
Bowen Bao <bowbao@microsoft.com>
2022-10-03 17:17:04 -0700

    merge 2 lines

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -6639,8 +6639,7 @@ def prim_constant(g: jit_utils.GraphContext, *inputs, **attrs):
 @_onnx_symbolic("prim::type")
 @_beartype.beartype
 def prim_type(g: jit_utils.GraphContext, device_value: _C.Value, *args, **kwargs):
-    device_node_kind = device_value.node().kind()
-    if device_node_kind == "prim::device":
+    if device_value.node().kind() == "prim::device":
         device = symbolic_helper._try_get_device_from_value(device_value.node().input())
         if device is not None:
             return g.op("Constant", value_s=str(device))
