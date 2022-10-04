95d9c01871750e0089bd515a9723e2f3d9126c24
Richard Zou <zou3519@gmail.com>
2022-10-04 10:48:59 -0700

    Remove unnecessary header (#86212)
    This appears to fix an internal build failure
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86212
    Approved by: https://github.com/samdow

--- aten/src/ATen/functorch/BatchRulesHelper.h
+++ aten/src/ATen/functorch/BatchRulesHelper.h
@@ -4,7 +4,6 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
-#include <ATen/native/ResizeCommon.h>
 #include <ATen/ATen.h>
 #include <ATen/Operators.h>
 
