0deda0039c2e795940c4cd67c2d8f571efadfd87
tangleintel <lei1.tang@intel.com>
2022-10-05 21:19:29 +0800

    revert the UT

--- test/jit/test_tracer.py
+++ test/jit/test_tracer.py
@@ -2417,10 +2417,7 @@ class TestMixTracingScripting(JitTestCase):
         checks_dict = torch.jit.script(ChecksDict())
         returns_dict = torch.jit.script(ReturnsDict())
         eager_module = TestModule(checks_dict, returns_dict)
-        example_input = list()
-        example_input.append(input1)
-        example_input = tuple(example_input)
-        traced_module = torch.jit.trace(eager_module, example_inputs=example_input)
+        traced_module = torch.jit.trace(eager_module, input1)
         self.assertEqual(traced_module(input1), eager_module(input1))
         self.assertEqual(traced_module(input2), eager_module(input2))
 
