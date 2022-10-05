50db4bd73d7741639f6038d1d4a6ec4341d0d157
Edward Z. Yang <ezyang@fb.com>
2022-10-05 11:32:48 -0700

    Revert "Revert "SymIntify cat and narrow (#86191)"" (#86289)
    This reverts commit fc94a2115b31dfe7a0d8f28eb4f5ed532c4f0792.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86289
    Approved by: https://github.com/wconstab

--- aten/src/ATen/BatchingRegistrations.cpp
+++ aten/src/ATen/BatchingRegistrations.cpp
@@ -1096,9 +1096,9 @@ TORCH_LIBRARY_IMPL(aten, Batched, m) {
   m.impl("expand_as", native::expand_as); // composite wrt autograd
   m.impl("movedim.intlist", movedim_batching_rule);
   m.impl("movedim.int", static_cast<Tensor(*)(const Tensor&,int64_t,int64_t)>(native::movedim)); // composite wrt autograd
-  // NB: static_cast because there's another variant of narrow. However, we don't
+  // There is another variant of narrow.  However, we don't
   // want to support the other variant yet bc it isn't documented...
-  m.impl("narrow", static_cast<Tensor(*)(const Tensor&,int64_t,int64_t,int64_t)>(native::narrow)); // composite wrt autograd
+  m.impl("narrow", native::narrow_symint); // composite wrt autograd
   m.impl("numpy_T", native::numpy_T);   // composite wrt autograd
   m.impl("matrix_H", native::matrix_H); // composite wrt autograd
   m.impl("mT", native::mT);             // composite wrt autograd
