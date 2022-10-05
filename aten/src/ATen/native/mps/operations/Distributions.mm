8f1c8d04e10d3a34b074cb4260468745b88a85ab
Nikita Shulga <nshulga@fb.com>
2022-10-05 01:02:31 +0000

    Remove `std::cout` from `multinomial_out_mps` (#86246)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86246
    Approved by: https://github.com/xuzhao9, https://github.com/seemethere

--- aten/src/ATen/native/mps/operations/Distributions.mm
+++ aten/src/ATen/native/mps/operations/Distributions.mm
@@ -486,8 +486,6 @@ Tensor& multinomial_out_mps(const Tensor& self,
     c10::optional<Generator> gen,
     Tensor& result) {
 
-  std::cout<<"Multinomial MPS\n";
-
   TORCH_CHECK(
       result.device() == self.device(),
       "multinomial arguments must have the same device");
