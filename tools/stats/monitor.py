e01f9be931e7a4ab7ab80619e0c1aa3fcd0d9083
Huy Do <huydhn@gmail.com>
2022-10-06 18:53:59 +0000

    Add total GPU memory utilization (#86250)
    Although we already have per process GPU memory usage, I'm curious to see what is the number for `gpu_utilization.memory` per https://docs.nvidia.com/deploy/nvml-api/structnvmlUtilization__t.html.  Also fixing a tiny typo issue that has been bugging me for a while `total_gpu_utilizaiton`
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86250
    Approved by: https://github.com/ZainRizvi

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -75,9 +75,10 @@ if __name__ == "__main__":
             }
             if handle is not None:
                 stats["per_process_gpu_info"] = get_per_process_gpu_info(handle)
-                stats["total_gpu_utilizaiton"] = pynvml.nvmlDeviceGetUtilizationRates(
-                    handle
-                ).gpu
+                # https://docs.nvidia.com/deploy/nvml-api/structnvmlUtilization__t.html
+                gpu_utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
+                stats["total_gpu_utilization"] = gpu_utilization.gpu
+                stats["total_gpu_mem_utilization"] = gpu_utilization.memory
         except Exception as e:
             stats = {
                 "time": datetime.datetime.utcnow().isoformat("T") + "Z",
