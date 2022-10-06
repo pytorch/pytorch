7a33117910ef6fdbe6c536838feac43e0857b1fb
Edward Z. Yang <ezyang@fb.com>
2022-10-05 21:34:36 -0700

    fix merge
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/core/WrapDimMinimal.h
+++ c10/core/WrapDimMinimal.h
@@ -41,18 +41,4 @@ inline c10::SymInt maybe_wrap_dim(
   return _maybe_wrap_dim(dim, dim_post_expr, wrap_scalar);
 }
 
-static inline SymInt maybe_wrap_dim_symint(
-    SymInt dim,
-    SymInt dim_post_expr,
-    bool wrap_scalar = true) {
-  // Inline the fast paths
-  if (C10_LIKELY(dim_post_expr * -1 <= dim && dim < dim_post_expr)) {
-    // Branch-less version of dim + (dim < 0 ? dim_post_expr : 0)
-    return dim + dim_post_expr * (dim < 0);
-  }
-  // Check edge-cases out-of-line (wrapping scalars and out-of-bounds errors)
-  return c10::detail::maybe_wrap_dim_slow_symint(
-      dim, dim_post_expr, wrap_scalar);
-}
-
 } // namespace c10
