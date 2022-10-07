717c4532b4c0873052b3139a467638087a0f8d7d
Sheil Kumar <smk2007@gmail.com>
2022-10-07 15:16:33 -0700

    Enable privateuse1 backend with tensors that dont have storage

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -116,6 +116,7 @@ class Tensor(torch._C._TensorBase):
             if (
                 self.is_sparse
                 or self.device.type in ["lazy", "xla", "mps", "ort", "meta", "hpu"]
+                or (self.storage is None and self.device.type == "privateuseone")
                 or (type(self) is not Tensor and self.data_ptr() == 0)
             ):
                 new_tensor = self.clone()
@@ -271,7 +272,7 @@ class Tensor(torch._C._TensorBase):
         # 2. Python list is not a good fit due to performance reason.
         #    `tolist()` converts every single element in the tensor into python objects
         #    and serialize them one by one.
-        if self.device.type in ["xla", "ort", "hpu"]:
+        if self.device.type in ["xla", "ort", "hpu"] or (self.storage is None and self.device.type == 'privateuseone'):
             # Convert BFloat16 tesors to Float32 before conversion to numpy, as numpy doesn't
             # support BFloat16. The rebuild tensor from numpy takes in the original self.dtype,
             # this would reconstruct the BFloat16 tensor from numpy.
