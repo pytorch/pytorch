69ae7dca2aadfdb9258fee92c710b174989ffb7e
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-10-07 05:20:36 +0000

    Revert "Revert "Revert "SymIntify cat and narrow (#86191)"" (#86289)"
    This reverts commit e778fbf5197638d6196c5d5acf6f9588a1e83368.
    
    Reverted https://github.com/pytorch/pytorch/pull/86289 on behalf of https://github.com/seemethere due to Fails internal tests see: https://www.internalfb.com/intern/sandcastle/job/27021598552487548/

--- tools/autograd/load_derivatives.py
+++ tools/autograd/load_derivatives.py
@@ -29,7 +29,6 @@ from torchgen.api.types import (
     SpecialArgName,
     stringT,
     symIntArrayRefT,
-    SymIntT,
     tensorGeometryT,
     tensorOptionsT,
     typeAndSizeT,
@@ -847,16 +846,6 @@ def saved_variables(
                 "suffix": "_args_sizes",
                 "nctype": lambda name: NamedCType(
                     name, VectorCType(VectorCType(BaseCType(longT)))
-                ),
-            },
-        ),
-        # replace to_args_sizes_symint(self) with self_args_sizes
-        (
-            r"to_args_sizes_symint\({}\)",
-            {
-                "suffix": "_args_sizes_symint",
-                "nctype": lambda name: NamedCType(
-                    name, VectorCType(VectorCType(BaseCType(SymIntT)))
                 ),
             },
         ),
