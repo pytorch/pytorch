0e2f7f6f9760057a0c13afbf71dd3460d212c8aa
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2022-10-07 16:03:30 +0000

    Revert "Merge more symbolic meta kernels and symint changes from branch (#86334)"
    This reverts commit 08e3999fa494238f8f62346a140da36bd43864e7.
    
    Reverted https://github.com/pytorch/pytorch/pull/86334 on behalf of https://github.com/seemethere due to Trying to revert https://github.com/pytorch/pytorch/pull/86207, this PR causes merge conflicts with the initial revert so will have to revert this as well

--- c10/core/SymFloatNodeImpl.h
+++ c10/core/SymFloatNodeImpl.h
@@ -60,7 +60,6 @@ class C10_API SymFloatNodeImpl : public c10::intrusive_ptr_target {
     TORCH_CHECK(false, "NYI");
   };
   virtual SymIntNode ceil();
-  virtual SymIntNode floor();
   virtual std::string str() {
     TORCH_CHECK(false, "NYI");
   };
