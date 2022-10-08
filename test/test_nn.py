50daff5961c895c7abc6e5eb93552614685ca657
ecao <e.cao@intel.com>
2022-10-08 10:38:32 -0700

    fix empty input issue of convolution for channels last memory format

--- test/test_nn.py
+++ test/test_nn.py
@@ -740,6 +740,19 @@ class TestNN(NNTestCase):
                 self.assertFalse(output2.requires_grad)
                 self.assertRaises(RuntimeError, lambda: output2.backward(torch.ones(1, 5, 10, 10)))
 
+    def test_conv2d_channels_last_empty_input(self):
+        input = torch.randn((0, 4, 20, 20))
+        conv = torch.nn.Conv2d(4, 4, 3, 1)
+        out = conv(input)
+        conv_cl = torch.nn.Conv2d(4, 4, 3, 1).to(memory_format=torch.channels_last)
+        out_cl = conv_cl(input)
+        self.assertEqual(out, out_cl)
+        input_cl = input.to(memory_format=torch.channels_last)
+        out_cl2 = conv(input_cl)
+        self.assertEqual(out_cl, out_cl2)
+        out_cl3 = conv_cl(input_cl)
+        self.assertEqual(out_cl, out_cl3)
+
     def test_parameters_and_named_parameters(self):
         def names(named_parameters):
             return [k for k, _ in named_parameters]
