c9ef13ed35e8be5437e6a39cdb51b4903dc8e540
Zain Rizvi <zainr@fb.com>
2022-10-10 16:33:38 -0500

    make it a notice

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1678,10 +1678,14 @@ def check_if_enable(test: unittest.TestCase):
                     if len(invalid_platforms) > 0:
                         invalid_plats_str = ", ".join(invalid_platforms)
                         valid_plats = ", ".join(platform_to_conditional.keys())
-                        raise RuntimeError(f"Test {disabled_test} is disabled for some unrecognized " + \
+                        
+                        print(f"Test {disabled_test} is disabled for some unrecognized " + \
                             f"platforms: [{invalid_plats_str}]. Please edit issue {issue_url} to fix the platforms " + \
                             f"assigned to this flaky test, changing \"Platforms: ...\" to a comma separated " + \
                             f"subset of the following (or leave it blank to match all platforms): {valid_plats}")
+
+                        # Sanitize the platforms list so that we continue to disable the test for any valid platforms given
+                        platforms = list(filter(lambda p: p in platform_to_conditional, platforms))
 
                     if platforms == [] or any([platform_to_conditional[platform] for platform in platforms]):
                         skip_msg = f"Test is disabled because an issue exists disabling it: {issue_url}" \
