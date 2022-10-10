310d4f908f91705da82bb37da62e2a3e14c2fecc
Zain Rizvi <zainr@fb.com>
2022-10-10 17:14:25 -0500

    lint fixes

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1678,11 +1678,11 @@ def check_if_enable(test: unittest.TestCase):
                     if len(invalid_platforms) > 0:
                         invalid_plats_str = ", ".join(invalid_platforms)
                         valid_plats = ", ".join(platform_to_conditional.keys())
-                        
-                        print(f"Test {disabled_test} is disabled for some unrecognized " + \
-                            f"platforms: [{invalid_plats_str}]. Please edit issue {issue_url} to fix the platforms " + \
-                            f"assigned to this flaky test, changing \"Platforms: ...\" to a comma separated " + \
-                            f"subset of the following (or leave it blank to match all platforms): {valid_plats}")
+
+                        print(f"Test {disabled_test} is disabled for some unrecognized ",
+                              f"platforms: [{invalid_plats_str}]. Please edit issue {issue_url} to fix the platforms ",
+                              "assigned to this flaky test, changing \"Platforms: ...\" to a comma separated ",
+                              f"subset of the following (or leave it blank to match all platforms): {valid_plats}")
 
                         # Sanitize the platforms list so that we continue to disable the test for any valid platforms given
                         platforms = list(filter(lambda p: p in platform_to_conditional, platforms))
