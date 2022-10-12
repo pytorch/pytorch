809648b697ece67dda8dd48e489f2fb0e7d94c8b
Pruthvi Madugundu <pmagundu@amd.com>
2022-10-11 23:35:56 -0700

    Add support for python 3.8 builds

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -266,6 +266,22 @@ case "$image" in
     VISION=yes
     ROCM_VERSION=5.2
     ;;
+  pytorch-linux-focal-rocm5.3-py3.8)
+    ANACONDA_PYTHON_VERSION=3.8
+    GCC_VERSION=9
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    ROCM_VERSION=5.3
+    ;;
+  pytorch-linux-focal-rocm5.4-py3.8)
+    ANACONDA_PYTHON_VERSION=3.8
+    GCC_VERSION=9
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    ROCM_VERSION=5.4
+    ;;
   pytorch-linux-focal-py3.7-gcc7)
     ANACONDA_PYTHON_VERSION=3.7
     CMAKE_VERSION=3.16.9  # Required for precompiled header support
