62a475b6679cd1ef0cb68cddaf0d9b5b04644e35
Brian Hirsh <hirsheybar@fb.com>
2022-10-12 13:55:14 -0700

    [test] add cross-ref tests for python meta kernels (#86228)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86228
    Approved by: https://github.com/albanD

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -950,6 +950,7 @@ def native_dropout(input: Tensor, p: float, train: Optional[bool]):
 
 
 @register_decomposition(aten._softmax)
+@out_wrapper()
 def _softmax(x: Tensor, dim: int, half_to_float: bool):
     # eager softmax returns a contiguous tensor. Ensure that decomp also returns
     # a contiguous tensor.
@@ -969,6 +970,7 @@ def _softmax(x: Tensor, dim: int, half_to_float: bool):
 
 
 @register_decomposition(aten._log_softmax)
+@out_wrapper()
 def _log_softmax(x: Tensor, dim: int, half_to_float: bool):
     # eager log_softmax returns a contiguous tensor. Ensure that decomp also
     # returns a contiguous tensor.
@@ -990,6 +992,7 @@ def _log_softmax(x: Tensor, dim: int, half_to_float: bool):
 
 # Remove special case when https://github.com/pytorch/pytorch/pull/72949 is landed.
 @register_decomposition(aten.addcmul)
+@out_wrapper()
 @pw_cast_for_opmath
 def addcmul(self: Tensor, tensor1: Tensor, tensor2: Tensor, value: float = 1):
     if self.is_floating_point() or self.is_complex():
@@ -1094,6 +1097,7 @@ def split(self: Tensor, split_size: int, dim: int = 0) -> List[Tensor]:
 
 # TODO: this doesn't appear to have enough precision in bfloat16
 @register_decomposition(aten.addmm)
+@out_wrapper()
 @pw_cast_for_opmath
 def addmm(self: Tensor, mat1: Tensor, mat2: Tensor, beta: int = 1, alpha: int = 1):
     if not self.is_floating_point() and not self.is_complex():
@@ -1779,7 +1783,8 @@ def index_add_(
     if alpha != 1:
         python_type = utils.dtype_to_type(x.dtype)
         utils.check(
-            utils.is_weakly_lesser_type(type(alpha), python_type),
+            python_type == bool
+            or utils.is_weakly_lesser_type(type(alpha), python_type),
             lambda: f"alpha argument of type {type(alpha)} cannot be safely cast to type {python_type}!",
         )
         tensor = tensor * alpha
@@ -2170,6 +2175,7 @@ def grid_sampler_2d(
 
 
 @register_decomposition(aten.mv)
+@out_wrapper()
 @pw_cast_for_opmath
 def mv(self, vec):
     utils.check(
@@ -2184,6 +2190,7 @@ def mv(self, vec):
 
 
 @register_decomposition(aten.dot, disable_meta=True)
+@out_wrapper()
 @pw_cast_for_opmath
 def dot(self, other):
     if self.is_complex():
