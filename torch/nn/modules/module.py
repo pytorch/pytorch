3a4ebe64ae98f3d2121cc588e92cc3211d77573b
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-10-12 18:07:50 +0000

    update docs

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -186,6 +186,35 @@ def register_module_backward_hook(
 def register_module_backward_pre_hook(
     hook: Callable[['Module', _grad_t], Union[None, Tensor]]
 ) -> RemovableHandle:
+    r"""Registers a backward pre-hook common to all the modules.
+
+    .. warning ::
+        This adds global state to the `nn.module` module
+        and it is only intended for debugging/profiling purposes.
+
+    The hook will be called every time before the gradients with respect
+    to module inputs are computed. The hook should have the following signature::
+
+        hook(module, grad_output) -> Tensor or None
+
+    The :attr:`grad_output` is a tuple. The hook should
+    not modify its arguments, but it can optionally return a new gradient with
+    respect to the output that will be used in place of :attr:`grad_output` in
+    subsequent computations. Entry in :attr:`grad_output` will be ``None`` for
+    all non-Tensor arguments.
+
+    For technical reasons, when this hook is applied to a Module, its forward function will
+    receive a view of each Tensor passed to the Module. Similarly the caller will receive a view
+    of each Tensor returned by the Module's forward function.
+
+    Global hooks are called before hooks registered with `register_backward_pre_hook`
+
+    Returns:
+        :class:`torch.utils.hooks.RemovableHandle`:
+            a handle that can be used to remove the added hook by calling
+            ``handle.remove()``
+
+    """
     handle = hooks.RemovableHandle(_global_backward_pre_hooks)
     _global_backward_pre_hooks[handle.id] = hook
     return handle
@@ -1002,6 +1031,25 @@ class Module:
         self, hook: Callable[['Module', _grad_t], Union[None, Tensor]]
     ) -> RemovableHandle:
         r"""Registers a backward pre hook on the module.
+
+        The hook will be called every time before the gradients with respect
+        to module inputs are computed. The hook should have the following signature::
+
+            hook(module, grad_output) -> Tensor or None
+
+        The :attr:`grad_output` is a tuple. The hook should
+        not modify its arguments, but it can optionally return a new gradient with
+        respect to the output that will be used in place of :attr:`grad_output` in
+        subsequent computations. Entry in :attr:`grad_output` will be ``None`` for
+        all non-Tensor arguments.
+
+        For technical reasons, when this hook is applied to a Module, its forward function will
+        receive a view of each Tensor passed to the Module. Similarly the caller will receive a view
+        of each Tensor returned by the Module's forward function.
+
+        .. warning ::
+            Modifying inputs inplace is not allowed when using backward hooks and
+            will raise an error.
 
         Returns:
             :class:`torch.utils.hooks.RemovableHandle`:
