a6164016d057829c1053fb8fb0fe591fe827e5f9
Taylor Robie <taylorrobie@fb.com>
2022-10-12 10:07:53 -0700

    [Profiler] Memory profiler part 1: Gradient identification
    There are multiple ways to indentify that a Tensor is a gradient. (A subset of which also give additional context.) So to start off I've made a utility to handle that determination.
    
    Differential Revision: [D39920730](https://our.internmc.facebook.com/intern/diff/D39920730/)
    
    [ghstack-poisoned]

---
+++ torch/profiler/_memory_profiler.py
@@ -0,0 +1,77 @@
+import dataclasses
+from typing import Iterator, Optional, Tuple
+
+import torch
+from torch._C._profiler import (
+    _EventType,
+    _ExtraFields_PyCall,
+    _ExtraFields_TorchOp,
+    _ProfilerEvent,
+    _TensorMetadata,
+    RecordScope,
+)
+
+
+@dataclasses.dataclass(eq=True, unsafe_hash=True, frozen=True)
+class Key:
+    id: int
+    storage_ptr: int
+    device: torch.device
+
+    def __repr__(self) -> str:
+        return f"id={self.id}: {hex(self.storage_ptr)} ({self.device})"
+
+    @staticmethod
+    def _make(
+        id: Optional[int], ptr: Optional[int], device: torch.device
+    ) -> Optional["Key"]:
+        return Key(id, ptr, device) if id is not None and ptr is not None else None
+
+    @classmethod
+    def from_tensor(cls, t: Optional[_TensorMetadata]) -> Optional["Key"]:
+        return cls._make(t.id, t.storage_data_ptr, t.device) if t else None
+
+
+def extract_gradients(node: _ProfilerEvent) -> Iterator[Tuple[Optional[Key], Key]]:
+    e = node.extra_fields
+    children = node.children
+
+    # AccumulateGrad is used in the Autograd engine to handle gradient updates.
+    # There are two possible cases:
+    # 1) This is a newly created gradient Tensor. In that case there is nothing
+    #    to accumulate, so autograd simply detaches the Tensor.
+    #
+    # 2) There is a preexisting gradient Tensor and we need to add the newly
+    #    computed update. This is done with an in-place add (aten::add_) op.
+    #    (The underscore suffix denotes "in-place".)
+    if (
+        isinstance(e, _ExtraFields_TorchOp)
+        and e.scope == RecordScope.BACKWARD_FUNCTION
+        # TODO: Move away from load bearing names
+        and node.name == "torch::autograd::AccumulateGrad"
+        and children
+        and children[0].tag == _EventType.TorchOp
+        and children[0].name in ("aten::detach", "aten::add_")
+        and children[0].extra_fields.inputs.tensor_metadata
+        and children[0].extra_fields.inputs.tensor_metadata[0].id is not None
+    ):
+        key = Key.from_tensor(children[0].extra_fields.inputs.tensor_metadata[0])
+        yield None, key
+
+    # We directly instrument `torch.nn.Module` and `torch.optim.Optimizer`
+    # NOTE: The values captured by the python tracer are cached; they can be
+    #       used to build up labels but do not imply that a Tensor was live at
+    #       a particular time.
+    elif isinstance(e, _ExtraFields_PyCall):
+        assert e.module is None or e.optimizer is None
+        if e.module is not None:
+            for _, p, p_grad in e.module.parameters:
+                p_grad_key = Key.from_tensor(p_grad)
+                if p_grad_key is not None:
+                    yield Key.from_tensor(p), p_grad_key
+
+        if e.optimizer is not None:
+            for p, p_grad, _ in e.optimizer.parameters:
+                p_grad_key = Key.from_tensor(p_grad)
+                if p_grad_key is not None:
+                    yield Key.from_tensor(p), p_grad_key
