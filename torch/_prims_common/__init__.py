105cd0927de2eb11f89c1fab021fa87313aaf6a3
Brian Hirsh <hirsheybar@fb.com>
2022-10-12 13:55:13 -0700

    fix aliasing for primtorch view meta kernels (#86285)
    Fixes https://github.com/pytorch/pytorch/issues/86284
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86285
    Approved by: https://github.com/albanD, https://github.com/mruberry

--- torch/_prims_common/__init__.py
+++ torch/_prims_common/__init__.py
@@ -133,6 +133,14 @@ def compare_tensor_meta(a: TensorLikeType, b: TensorLikeType, check_strides=Fals
             )
             raise RuntimeError(msg)
 
+        if a.storage_offset() != b.storage_offset():
+            msg = (
+                "Storage offset mismatch! Storage offsets are {0} and {1}!".format(
+                    a.storage_offset(), b.storage_offset()
+                )
+            )
+            raise RuntimeError(msg)
+
 
 def check_significant_strides(
     a: TensorLikeType, b: TensorLikeType
