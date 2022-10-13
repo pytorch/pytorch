c2cc00e70626d70ea8e9cb6f081a9f43ef3ba40d
Neel Patel <neelpatel@meta.com>
2022-10-13 11:23:47 -0700

    Refactor check labels script and print error message to console

--- .github/scripts/check_labels.py
+++ .github/scripts/check_labels.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""check_labels.py"""
 
 from typing import Any, List
 
@@ -14,14 +15,16 @@ from trymerge import (
 )
 
 
+def get_release_notes_labels() -> List[str]:
+    return [label for label in get_pytorch_labels() if label.lstrip().startswith("release notes:")]
+
+
 def check_labels(pr_labels: List[str]) -> bool:
     is_not_user_facing_pr = any(label.strip() == "topic: not user facing" for label in pr_labels)
     if is_not_user_facing_pr:
         return True
 
-    release_labels = [label for label in get_pytorch_labels() if label.lstrip().startswith("release notes:")]
-
-    return any(label.strip() in release_labels for label in pr_labels)
+    return any(label.strip() in get_release_notes_labels() for label in pr_labels)
 
 
 def parse_args() -> Any:
@@ -42,8 +45,9 @@ def main() -> None:
         if not check_labels(pr.get_labels()):
             msg = ("This PR needs a label. If your changes are user facing and intended to be a "
                    "part of release notes, please use a label starting with `release notes:`. If "
-                   "not, please add the `topic:  not user facing` label. For more information, see "
+                   "not, please add the `topic: not user facing` label. For more information, see "
                    "https://github.com/pytorch/pytorch/wiki/PyTorch-AutoLabel-Bot#why-categorize-for-release-notes-and-how-does-it-work.")
+            print(msg)
             gh_post_pr_comment(pr.org, pr.project, pr.pr_num, msg)
             exit(1)
     except Exception as e:
