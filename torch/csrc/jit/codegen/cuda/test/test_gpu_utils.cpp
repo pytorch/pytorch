04ea3848507ebddeff6fd66b19e80ac90b5b6280
jjsjann123 <jiej@nvidia.com>
2022-10-14 13:47:40 -0700

    dtype mismatch

--- torch/csrc/jit/codegen/cuda/test/test_gpu_utils.cpp
+++ torch/csrc/jit/codegen/cuda/test/test_gpu_utils.cpp
@@ -43,7 +43,7 @@ TEST_F(NVFuserTest, FusionMergeDims_CUDA) {
   std::vector<size_t> dims{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   auto merged = scheduler_utils::mergeDims(tv, {2, 3, 7, 8, 9}, dims);
   TORCH_CHECK(merged == 2);
-  std::vector<size_t> expect_shape{
+  std::vector<int64_t> expect_shape{
       p[0], p[1], p[2] * p[3] * p[7] * p[8] * p[9], p[4], p[5], p[6], p[10]};
   TORCH_CHECK(tv->nDims() == expect_shape.size());
   for (auto i : c10::irange(expect_shape.size())) {
