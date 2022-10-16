2b7ee9f2737415200a286ee2aaebf9d9b2eab841
Jason Ansel <jansel@fb.com>
2022-10-16 09:20:50 -0700

    Update on "Migrate dynamo CI test shards to torch._dynamo"
    [ghstack-poisoned]

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -688,11 +688,13 @@ elif [[ "$TEST_CONFIG" == distributed ]]; then
 elif [[ "${TEST_CONFIG}" == *dynamo* && "${SHARD_NUMBER}" == 1 && $NUM_TEST_SHARDS -gt 1 ]]; then
   test_without_numpy
   install_torchvision
+  install_triton
   test_dynamo_shard 1
   test_aten
 elif [[ "${TEST_CONFIG}" == *dynamo* && "${SHARD_NUMBER}" == 2 && $NUM_TEST_SHARDS -gt 1 ]]; then
   install_torchvision
   install_filelock
+  install_triton
   test_dynamo_shard 2
 elif [[ "${SHARD_NUMBER}" == 1 && $NUM_TEST_SHARDS -gt 1 ]]; then
   test_without_numpy
