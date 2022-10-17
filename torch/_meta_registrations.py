a57d95f9a34ba0b1a422500bb8b7260e229ab572
albanD <desmaison.alban@gmail.com>
2022-10-16 22:16:16 -0400

    conv backward impl (#87047)
    ~~Waiting for test run to see if this backward is actually exercised.
    If not, I will add test before merging.~~
    Test updated. Ready to go now.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87047
    Approved by: https://github.com/ezyang

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -808,6 +808,36 @@ def meta_index_Tensor(self, indices):
     return self.new_empty(before_shape + replacement_shape + after_shape)
 
 
+@register_meta([aten.convolution_backward.default])
+def meta_convolution_backward(
+    grad_output_,
+    input_,
+    weight_,
+    bias_sizes_opt,
+    stride,
+    padding,
+    dilation,
+    transposed,
+    output_padding,
+    groups,
+    output_mask,
+):
+    # High level logic taken from slow_conv3d_backward_cpu which should
+    # be representative of all convolution_backward impls
+    backend_grad_input = None
+    backend_grad_weight = None
+    backend_grad_bias = None
+
+    if output_mask[0]:
+        backend_grad_input = grad_output_.new_empty(input_.size())
+    if output_mask[1]:
+        backend_grad_weight = grad_output_.new_empty(weight_.size())
+    if output_mask[2]:
+        backend_grad_bias = grad_output_.new_empty(bias_sizes_opt)
+
+    return (backend_grad_input, backend_grad_weight, backend_grad_bias)
+
+
 @register_meta([aten.addbmm.default, aten.addbmm.out])
 @out_wrapper()
 def meta_addbmm(self, batch1, batch2, *, beta=1, alpha=1):
