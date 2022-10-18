acd3f447240254a8e632a7c0c102149979c9853b
Will Feng (DPER) <willfeng@fb.com>
2022-10-18 20:26:30 +0000

    Improve complex_memory_overlap check for Inductor CUDA graph (#87177)
    Point fix for https://github.com/pytorch/torchdynamo/issues/1620 to unblock internal models. Supersedes https://github.com/pytorch/pytorch/pull/87058.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87177
    Approved by: https://github.com/ezyang

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -64,8 +64,19 @@ def index_expanded_dims(t, expanded_dims):
 
 
 def complex_memory_overlap(t):
-    indexed_tensor = index_expanded_dims(t, get_expanded_dims(t))
-    return torch._debug_has_internal_overlap(indexed_tensor) != 0
+    # if torch._debug_has_internal_overlap thinks this tensor potentially has
+    # memory overlap internally, let's dig deeper to find out whether it's true.
+    if torch._debug_has_internal_overlap(t) != 0:
+        strides = t.stride()
+        sizes = t.shape
+        indices = list(range(len(strides)))
+        indices = [x for _, x in sorted(zip(strides, indices))]
+        for i in range(len(strides)):
+            prev_stride = 1 if i == 0 else strides[indices[i - 1]]
+            prev_size = 1 if i == 0 else sizes[indices[i - 1]]
+            if strides[indices[i]] < prev_stride * prev_size:
+                return True
+    return False
 
 
 def is_unspec_input(t):
