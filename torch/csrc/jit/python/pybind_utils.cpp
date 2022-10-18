3e09fb1d4f71183f4206c47c6dce78ec5aab7940
lezcano <lezcano-93@hotmail.com>
2022-10-18 09:36:29 +0000

    Fix Scalar(bool) handling in toIValue (#87179)
    At the moment, they were casted to `int64`, which breaks quite a few
    casting rules for example in `ops.aten`.
    
    Quite a vintage bug, circa 2020.
    
    With this fix, the following code prints `torch.bool`, rather than `torch.int64`.
    ```python
    import torch
    msk = torch.tensor([False])
    b = torch.tensor([False])
    print(torch.ops.aten.where.ScalarSelf(msk, True, b).dtype)
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87179
    Approved by: https://github.com/albanD

--- torch/csrc/jit/python/pybind_utils.cpp
+++ torch/csrc/jit/python/pybind_utils.cpp
@@ -433,7 +433,9 @@ IValue toIValue(py::handle obj, const TypePtr& type, c10::optional<int32_t> N) {
         auto layout = reinterpret_cast<THPLayout*>(obj.ptr());
         return static_cast<int8_t>(layout->layout);
       }
-      if (py::isinstance<py::int_>(obj)) {
+      if (py::isinstance<py::bool_>(obj)) {
+        return py::cast<bool>(obj);
+      } else if (py::isinstance<py::int_>(obj)) {
         return py::cast<int64_t>(obj);
       } else if (py::isinstance<py::float_>(obj)) {
         return py::cast<double>(obj);
