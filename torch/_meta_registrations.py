4460be679fae9a4ac11d303bb1b3a48a80346556
Edward Z. Yang <ezyang@fb.com>
2022-10-19 20:57:10 -0700

    argmax and argmin meta registration
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -1737,6 +1737,42 @@ def _thnn_fused_lstm_cell_meta(input_gates, hidden_gates, cx, input_bias=None, h
     return (workspace, hy, cy)
 
 
+def zero_numel_check_dims(self, dim, fn_name):
+    if self.ndim == 0:
+        check(
+            dim == 0 or dim == -1,
+            lambda: f"{fn_name}: Expected reduction dim -1 or 0 for scalar but got {dim}",
+            IndexError)
+    else:
+        check(
+            self.size(dim) != 0,
+            lambda: f"{fn_name}: Expected reduction dim {dim} to have non-zero size.",
+            IndexError)
+
+def check_argmax_argmin(name, self, dim):
+    if dim is not None:
+        dim = maybe_wrap_dim(dim, self.dim())
+        zero_numel_check_dims(self, dim, name)
+    else:
+        check(
+            self.numel() != 0,
+            lambda: f"{name}: Expected reduction dim to be specified for input.numel() == 0."
+        )
+
+@register_meta(aten.argmax.default, register_dispatcher=False)
+def argmax_meta(self, dim=None, keepdim=False):
+    check_argmax_argmin("argmax", self, dim)
+    dims = utils.reduction_dims(self.shape, (dim,) if dim is not None else None)
+    shape = _compute_reduction_shape(self, dims, keepdim)
+    return self.new_empty(shape, dtype=torch.int64)
+
+@register_meta(aten.argmin.default, register_dispatcher=False)
+def argmax_meta(self, dim=None, keepdim=False):
+    check_argmax_argmin("argmin", self, dim)
+    dims = utils.reduction_dims(self.shape, (dim,) if dim is not None else None)
+    shape = _compute_reduction_shape(self, dims, keepdim)
+    return self.new_empty(shape, dtype=torch.int64)
+
 # We must also trigger meta registrations from PrimTorch ref
 # decompositions
 import torch._refs
