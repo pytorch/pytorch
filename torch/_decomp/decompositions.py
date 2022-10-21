5e19bb986af9da0683fdb168ff22c429a05e215c
Michael Voznesensky <voznesenskym@gmail.com>
2022-10-21 06:31:17 +0000

    rm imports

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -13,11 +13,6 @@ from torch import Tensor
 from torch._decomp import register_decomposition
 from torch._prims_common import IntLike, NumberType, TensorLike, TensorSequenceType
 from torch._prims_common.wrappers import _maybe_resize_out, _safe_copy_out, out_wrapper
-from torch.fx.experimental.symbolic_shapes import (
-    guard_int_hack_please_dont_use,
-    sym_float,
-    sym_int,
-)
 from torch.utils._pytree import tree_flatten, tree_map
 
 DispatchKey = torch._C.DispatchKey  # type: ignore[attr-defined]
