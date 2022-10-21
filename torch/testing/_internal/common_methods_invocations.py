7f4ce354374927bd9572e91bdd266a796950f2f6
Jane Xu <janeyx@fb.com>
2022-10-20 22:05:51 -0400

    Make it even laxer for complex64

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -13283,7 +13283,7 @@ op_db: List[OpInfo] = [
            supports_forward_ad=True,
            supports_fwgrad_bwgrad=True,
            sample_inputs_func=sample_inputs_attn,
-           decorators=[DecorateInfo(toleranceOverride({torch.float32: tol(atol=5e-05, rtol=3e-06), torch.complex64: tol(atol=5e-04, rtol=3e-05)}),
+           decorators=[DecorateInfo(toleranceOverride({torch.float32: tol(atol=5e-05, rtol=3e-06), torch.complex64: tol(atol=1e-02, rtol=5e-05)}),
                                     'TestCommon', 'test_noncontiguous_samples')],
            ),
     OpInfo('svd',
