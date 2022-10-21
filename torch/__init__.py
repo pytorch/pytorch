32f0adecbba2a5bedda6aeca7acc52ad62eb609a
chuksmbaka <mbakaforever@yahoo.com>
2022-10-21 17:30:18 +0000

    Grammatical update of the tech docs. (#87357)
    Fixes #ISSUE_NUMBER
    A more appropriate and correct word.
    ![grammatical correction](https://user-images.githubusercontent.com/25278471/196927273-7e4c0c9b-96a6-43d1-9b10-17b40665feed.png)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87357
    Approved by: https://github.com/albanD

--- torch/__init__.py
+++ torch/__init__.py
@@ -2,7 +2,7 @@
 r"""
 The torch package contains data structures for multi-dimensional
 tensors and defines mathematical operations over these tensors.
-Additionally, it provides many utilities for efficient serializing of
+Additionally, it provides many utilities for efficient serialization of
 Tensors and arbitrary types, and other useful utilities.
 
 It has a CUDA counterpart, that enables you to run your tensor computations
