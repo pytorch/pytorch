5f8e1f17fe03b97140ced644d6d5eb8c8e7b95ec
Wang, Eikan <eikan.wang@intel.com>
2022-10-22 05:36:02 +0000

    Update base for Update on "Support reduction vectorization"
    cc jansel lezcano fdrocha
    
    [ghstack-poisoned]

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -140,7 +140,7 @@ def is_gcc():
 
 
 def cpp_compile_command(input, output, include_pytorch=False):
-    if include_pytorch or config.cpp.simdlen:
+    if include_pytorch or (config.cpp.simdlen and config.cpp.simdlen > 1):
         ipaths = cpp_extension.include_paths() + [sysconfig.get_path("include")]
         lpaths = cpp_extension.library_paths() + [sysconfig.get_config_var("LIBDIR")]
         libs = ["c10", "torch", "torch_cpu", "torch_python", "gomp"]
