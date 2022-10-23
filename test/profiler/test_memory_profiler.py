86127a60b2969e32f7c3d04ddfa62ebc9f48313c
Taylor Robie <taylorrobie@fb.com>
2022-10-22 19:59:16 -0700

    [Profiler] Memory profiler part 6: Mark gradients and temporary intermediates.
    Semantic assignment will be built up as a series of passes which gradually pin down the regions of a trace. For this reason it is important to be very meticulous in the assignment of categories.
    
    We begin with gradients as they are both straightforward to identify and foundational to subsequent analysis. There are two mechanisms that the profiler can use to tag gradients, each with their own advantages and limitations. The first is direct inspection of the op graph which is generic but predicated on certain features of the Autograd engine. (And therefore not necessarily exhaustive.) The second approach is direct instrumentation via the python tracer. This method relies requires that gradients be attached to an nn.Module parameter and can miss corner cases such as `set_to_none=True` due to the cache structure of the python tracer. Combined these two approaches provide very high coverage.
    
    Temporaries are more straightforward; we can easily add them by trivial local inspection of a data flow node.
    
    Because this is the first PR in the end-to-end section most of the code is building the scaffolding for category bookkeeping and unit testing. (The actual gradient extraction was covered in an earlier PR.)
    
    Differential Revision: [D40220389](https://our.internmc.facebook.com/intern/diff/D40220389/)
    
    [ghstack-poisoned]

--- test/profiler/test_memory_profiler.py
+++ test/profiler/test_memory_profiler.py
@@ -3,7 +3,7 @@ import functools
 import gc
 import itertools as it
 import textwrap
-from typing import Callable, Dict, Iterator, List, Optional, Tuple
+from typing import Callable, Dict, Iterator, List, Optional, Set, Tuple
 
 import torch
 from torch._C._profiler import _EventType
@@ -770,6 +770,96 @@ class TestDataFlow(TestCase):
             AccumulateGrad            T16(v0*), T17(v0)  ->  T17(v1)
             [memory]                  T8(v2*)          ->""",
         )
+
+
+class TestMemoryProfilerE2E(TestCase):
+    @staticmethod
+    def _lookup_tensor_categories(
+        t: torch.Tensor, memory_profile: _memory_profiler.MemoryProfile
+    ) -> Dict[_memory_profiler.TensorAndID, Optional[_memory_profiler.Category]]:
+        storage = t.storage()
+        if storage is None:
+            raise ValueError("Cannot look up uninitialized Tensor.")
+
+        return {
+            (key, version): category
+            for (key, version), category in memory_profile._category_snapshot().items()
+            if key.storage_ptr == storage.data_ptr() and key.device == storage.device
+        }
+
+    def _run_and_check_gradients(self, inner_fn, model):
+
+        with profile() as prof:
+            inner_fn()
+
+        memory_profile = prof._memory_profile()
+        for p in model.parameters():
+            self.assertIsNotNone(p.grad)
+
+            p_grad_categories = self._lookup_tensor_categories(p.grad, memory_profile)
+            self.assertGreater(len(p_grad_categories), 0)
+            self.assertTrue(
+                all(
+                    category == _memory_profiler.Category.GRADIENT
+                    for category in p_grad_categories.values()
+                )
+            )
+
+    def test_gradients(self):
+        model = torch.nn.Sequential(
+            torch.nn.Linear(2, 2), ScaleLayer(), torch.nn.Linear(2, 1), ScaleLayer()
+        )
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+        def fwd_only():
+            _ = model(torch.ones((2, 2)))
+
+        def fwd_bwd_step():
+            y = model(torch.ones((2, 2)))
+            torch.nn.functional.mse_loss(y, torch.rand((2, 1))).backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # If we profile the first step then gradients will not have been
+        # created when we call `model.forward`, so if we don't call `.backward`
+        # then gradients are never created.
+        with self.assertRaises(AssertionError):
+            self._run_and_check_gradients(inner_fn=fwd_only, model=model)
+
+        # On the first step we must rely on `AccumulateGrad`, since gradients
+        # did not exist when `model.forward` was called.
+        self.assertTrue(all(p.grad is None for p in model.parameters()))
+        self._run_and_check_gradients(inner_fn=fwd_bwd_step, model=model)
+
+        # After one step the python tracer will also flag gradients.
+        self.assertTrue(not any(p.grad is None for p in model.parameters()))
+        self._run_and_check_gradients(inner_fn=fwd_bwd_step, model=model)
+
+        # The parameter gradients are not used but we still detect them with
+        # the python tracer.
+        self._run_and_check_gradients(inner_fn=fwd_only, model=model)
+
+    def test_gradients_set_to_none(self):
+        model = torch.nn.Sequential(torch.nn.Linear(2, 2), torch.nn.Linear(2, 1))
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+        def fwd_bwd_step():
+            for _ in range(3):
+                # zero grads at the start so gradients are still live to be
+                # checked.
+                optimizer.zero_grad(set_to_none=True)
+
+                y = model(torch.ones((2, 2)))
+                torch.nn.functional.mse_loss(y, torch.rand((2, 1))).backward()
+                optimizer.step()
+
+        fwd_bwd_step()
+        self.assertTrue(not any(p.grad is None for p in model.parameters()))
+        self._run_and_check_gradients(inner_fn=fwd_bwd_step, model=model)
+
+        optimizer.zero_grad(set_to_none=True)
+        self.assertTrue(all(p.grad is None for p in model.parameters()))
+        self._run_and_check_gradients(inner_fn=fwd_bwd_step, model=model)
 
 
 if __name__ == "__main__":
