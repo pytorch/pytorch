ce7caa4297734256b68be486473ab0705dfb621d
AllenTiTaiWang <titaiwang@microsoft.com>
2022-10-24 21:07:11 +0000

    Update base for Update on "[ONNX] Support custom Op with onnx-script local function"
    Extend `register_custom_op` to support onnx-script local function. The FunctionProto from onnx-script is represented by custom op and inserted into ModelProto for op execution. 
    
    The test cases can be found in the next PR #86907 .
    
    
    [ghstack-poisoned]

--- torch/onnx/_internal/jit_utils.py
+++ torch/onnx/_internal/jit_utils.py
@@ -321,3 +321,10 @@ def get_device_from_value(value: _C.Value) -> Optional[torch.device]:
         return None
     tensor_type = typing.cast(_C.TensorType, value.type())
     return tensor_type.device()
+
+@_beartype.beartype
+def _parse_node_kind(kind: str) -> Tuple[str, str]:
+    if "::" not in kind:
+        raise torch.onnx.errors.OnnxExporterError("Node kind is invalid. '::' is not in node kind.")
+    domain, opname = kind.split("::")
+    return domain, opname
