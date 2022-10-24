986bbabe1a200a88a7593444741f5eeac237c6cd
atalman <atalman@fb.com>
2022-10-24 13:29:33 -0700

    TEST

--- .github/regenerate.sh
+++ .github/regenerate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Allows this script to be invoked from any directory:
+# Allows this script to be invoked from any directory - dummy:
 cd "$(dirname "$0")"
 
 python3 scripts/generate_ci_workflows.py
