01fa9ef94e9ba1dd887e5bdd0aadf657443862ea
Sherlock Huang <bahuang@fb.com>
2022-10-25 04:46:42 +0000

    Prefer python meta function over c++ meta function (#87426)
    This is a policy update for meta registration. **We now prefer python meta implementation over C++ meta function.**  This is a flip of the previous policy, where we prefer C++ meta function over python meta function if they both exist.
    
    Here's the meta registration process:
    1. register_meta and register_decomposition will place the python meta/decomp functions into the `global_decomp_table`.  However, they will NOT register them into dispatcher.
    2. After global_decomp_table is populated, we will compile an `active_meta_table`. For a given op, we pick the most specific decomp function from `global_decomp_table` in the preference order of Meta > PostAutograd > PreAutograd.
    3. We will unconditionally register all of them into python dispatcher. And register them into C++ dispatcher, unless it one of the following 3 cases
    - 1. the op is a CompositeImplicitAutograd, and should rely on decomposed op's meta
    - 2. the op is a view op, as the MetaTensor doesn't support aliased storage
    - 3. the op is in the blocklist (due to UT failures, and we will burn down this list op by op)
    
    Over the long run, we wish to implement all meta functions in python. With this PR, 321 op_overloads will have cpp meta overridden by python meta. There are still 400 op_overloads is using cpp meta. The exact list can be found here https://gist.github.com/SherlockNoMad/d20bb736178df8eebd3b054c8bb7cdc5
    
    cc @ngimel @jansel @lezcano @fdrocha @mlazos @soumith @voznesenskym @yanboliang
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87426
    Approved by: https://github.com/ezyang, https://github.com/jansel

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -747,7 +747,7 @@ class FakeTensorMode(TorchDispatchMode):
         # is written to must be invalidated
         self.invalidate_written_to_constants(func, flat_arg_fake_tensors, args, kwargs)
 
-        from torch._decomp import _disabled_meta_decomps, decomposition_table
+        from torch._decomp import decomposition_table
 
         with self:
             # Decomposes CompositeImplicitAutograd ops
@@ -781,7 +781,6 @@ class FakeTensorMode(TorchDispatchMode):
         if (
             func in decomposition_table
             and torch_decomp_decompositions(func)
-            and func not in _disabled_meta_decomps
             and all(not e.is_sparse for e in flat_arg_fake_tensors)
         ):
             with self:
