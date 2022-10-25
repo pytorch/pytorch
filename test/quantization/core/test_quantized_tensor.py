56f9502c19c3accd51c1996309251cc99490e7c2
Jerry Zhang <jerryzh168@gmail.com>
2022-10-25 17:39:24 +0000

    [quant][core] Add quantize/dequantize ops for decomposed quantized Tensor representation (#87093)
    Summary:
    Added q/dq implementation for out of core (decomposed) quantized Tensor representation, meaning that
    instead of storing quantization parameters (e.g. scale/zero_point) in a separate quantized Tensor object, we will store
    quantization parameters in the argument of operators.
    ```
    quantize(float32_tensor, scale, zero_point, dtype) -> int8_tensor
    dequantize(int8_tensor, scale, zero_point, dtype) -> float32_tensor
    ```
    
    Test Plan:
    python test/test_quantization.py TestQuantizedTensor.test_decomposed_quantize
    python test/test_quantization.py TestQuantizedTensor.test_decomposed_dequantize
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87093
    Approved by: https://github.com/dzdang, https://github.com/z-a-f

--- test/quantization/core/test_quantized_tensor.py
+++ test/quantization/core/test_quantized_tensor.py
@@ -1463,6 +1463,41 @@ class TestQuantizedTensor(TestCase):
         dedequantized_X = quantized_X.to(torch.float32)
         torch.testing.assert_allclose(X, dedequantized_X, rtol=1e-4, atol=5e-3)
 
+    def test_decomposed_quantize(self):
+        # register the ops
+        import torch.ao.quantization.fx._decomposed
+        X = torch.randn(5, 10)
+        qdtype = torch.quint8
+        dtype = torch.uint8
+        scale, zero_point = _calculate_dynamic_qparams(X, qdtype)
+        quant_min, quant_max = 0, 255
+
+        quantized_X = torch.quantize_per_tensor(X, scale, zero_point, qdtype)
+        quantized_decomposed_X = \
+            torch.ops.quantized_decomposed.quantize_per_tensor(
+                X, scale, zero_point, quant_min, quant_max, dtype)
+        self.assertEqual(quantized_decomposed_X.dtype, dtype)
+        self.assertEqual(quantized_X.int_repr(), quantized_decomposed_X)
+
+    def test_decomposed_dequantize(self):
+        import torch.ao.quantization.fx._decomposed
+        X = torch.randn(5, 10)
+        dtype = torch.uint8
+        qdtype = torch.quint8
+        scale, zero_point = _calculate_dynamic_qparams(X, qdtype)
+        quant_min, quant_max = 0, 255
+
+        quantized_X = torch.quantize_per_tensor(X, scale, zero_point, qdtype)
+        dequantized_X = torch.dequantize(quantized_X)
+
+        quantized_decomposed_X = torch.ops.quantized_decomposed.quantize_per_tensor(
+            X, scale, zero_point, quant_min, quant_max, dtype)
+        dequantized_decomposed_X = torch.ops.quantized_decomposed.dequantize_per_tensor(
+            quantized_decomposed_X, scale, zero_point, quant_min, quant_max, dtype
+        )
+        self.assertEqual(quantized_X.int_repr(), quantized_decomposed_X)
+        self.assertEqual(dequantized_X, dequantized_decomposed_X)
+
 if __name__ == '__main__':
     raise RuntimeError("This test file is not meant to be run directly, use:\n\n"
                        "\tpython test/test_quantization.py TESTNAME\n\n"
