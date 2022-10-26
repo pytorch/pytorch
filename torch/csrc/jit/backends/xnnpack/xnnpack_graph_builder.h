a9b3cbd456c75d6cf336dd178261cc20aded4b56
maxren <maxren@meta.com>
2022-10-26 16:40:32 -0700

    [xnnpack][lite-int][graph-build] torchscript -> xnnpack graph
    This point we perform conversion for Torchscript IR to XNNPack graph. Currently we only support converting Add Nodes and fp32 tensor values.
    
    As a caveat, we are not building this at runtime. So for testing we just run the xnn graph once ahead of time and with sample inputs and forward it to execute. This is only for testing, and will be changed in a later diff. This will allow us to check that graph creation is sound.
    
    Differential Revision: [D39838851](https://our.internmc.facebook.com/intern/diff/D39838851/)
    
    [ghstack-poisoned]

--- torch/csrc/jit/backends/xnnpack/xnnpack_graph_builder.h
+++ torch/csrc/jit/backends/xnnpack/xnnpack_graph_builder.h
@@ -13,8 +13,19 @@ namespace delegate {
 
 class XNNGraph {
  private:
+  const float output_min = -std::numeric_limits<float>::infinity();
+  const float output_max = std::numeric_limits<float>::infinity();
+
   // xnn_subgraph
   xnn_subgraph_t _subgraph_ptr;
+  // Set of all the tensor values throughout the jit graph
+  std::unordered_set<torch::jit::Value*> _intermediate_tensors;
+  // Set of all the tensor values mapped to the xnnpack ids
+  std::unordered_map<torch::jit::Value*, uint32_t> _val_to_ids;
+  // Vector containing the torch valued inputs/outputs,
+  // must be ordered to preserve the order of input/outputs
+  std::vector<torch::jit::Value*> _inputs;
+  std::vector<torch::jit::Value*> _outputs;
 
   // Graph passes for optimizing and tracing torchscript graph
   // Essentially massaging the graph into a digestiable format for
@@ -23,8 +34,30 @@ class XNNGraph {
       std::shared_ptr<torch::jit::Graph> graph,
       std::vector<c10::IValue>& example_inputs);
 
+  // Gather all the intermediate tensor values within a graph. This
+  // skips through all prim constants. The purpose of this is for defining
+  // the tensor values beforehand for the xnnpack subgraph.
+  void gatherTensorValues(std::shared_ptr<torch::jit::Graph>& graph);
+
+  // Gathers the tensor values in a give node
+  void gatherNodeInputs(torch::jit::Node& node);
+
+  // Helper function to determine if a jit value is a graph input
+  bool isGraphInput(torch::jit::Value* val);
+
+  // Helper function to determine if a jit value is a graph output
+  bool isGraphOutput(torch::jit::Value* val);
+
+  // Defines all xnnpack nodes for the nodes in the graph
+  void defineAllNodes(std::shared_ptr<torch::jit::Graph>& graph);
+
+  // Defines all xnn tensor values used throughout the graph
+  void defineAllTensorValues();
+
   // Makes a pass through the graph and throws if any ops are unsupported
   void checkOpsToDelegate(std::shared_ptr<torch::jit::Graph>& graph);
+
+
 
  public:
   XNNGraph() : _subgraph_ptr(nullptr) {
@@ -42,6 +75,10 @@ class XNNGraph {
   void buildXNNGraph(
       std::shared_ptr<torch::jit::Graph>& graph,
       std::vector<c10::IValue> example_inputs);
+
+    void runGraphOnInputs(
+      std::vector<at::Tensor> tensor_inputs,
+      std::vector<at::Tensor> tensor_outputs);
 };
 
 } // namespace delegate
