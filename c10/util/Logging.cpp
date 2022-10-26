94465d6287e8f54c99f4b0b891a6c618bb80d7ce
Arfrever Frehtes Taifersar Arahesis <Arfrever.FTA@GMail.Com>
2022-10-26 07:32:59 +0200

    Support only GLog >=0.6.0
    Fixes https://github.com/pytorch/pytorch/issues/58054

--- c10/util/Logging.cpp
+++ c10/util/Logging.cpp
@@ -192,23 +192,13 @@ C10_DEFINE_int(
     google::GLOG_WARNING,
     "The minimum log level that caffe2 will output.");
 
-// Google glog's api does not have an external function that allows one to check
-// if glog is initialized or not. It does have an internal function - so we are
-// declaring it here. This is a hack but has been used by a bunch of others too
-// (e.g. Torch).
-namespace google {
-namespace glog_internal_namespace_ {
-bool IsGoogleLoggingInitialized();
-} // namespace glog_internal_namespace_
-} // namespace google
-
 namespace c10 {
 namespace {
 
 void initGoogleLogging(char const* name) {
 #if !defined(_MSC_VER)
   // This trick can only be used on UNIX platforms
-  if (!::google::glog_internal_namespace_::IsGoogleLoggingInitialized())
+  if (!::google::IsGoogleLoggingInitialized())
 #endif
   {
     ::google::InitGoogleLogging(name);
