b3efa5b0d0ef5d0260bb34694957d05461a23972
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-10-26 13:17:43 -0400

    Remove scale and zero point verification

--- torch/onnx/verification.py
+++ torch/onnx/verification.py
@@ -200,24 +200,14 @@ def _compare_ort_pytorch_outputs(
                     )
                     continue
             else:
-                    if (
-                        ort_out.dtype == torch.uint8 or ort_out.dtype == torch.int8
-                        ) and pt_out.is_quantized:
-                        if ort_out.q_scale() != pt_out.q_scale():
-                            warnings.warn(
-                                "ONNX output and PyTorch output has scale difference"
-                                )
-                        if ort_out.q_zero_point() != pt_out.q_zero_point():
-                            warnings.warn(
-                                "ONNX output and PyTorch output has zero point difference"
-                                )
-                    else:
-                        if (
-                            ort_out.dtype == torch.uint8 or ort_out.dtype == torch.int8
-                            ) == True:
-                            warnings.warn("ONNX output is quantized")
-                        if pt_out.is_quantized == True:
-                            warnings.warn("PyTorch output is quantized")
+                if (
+                    ort_out.dtype == torch.uint8 or ort_out.dtype == torch.int8
+                   ) == True:
+                    warnings.warn("ONNX output is quantized")
+                if (
+                    pt_out.dtype == torch.uint8 or pt_out.dtype == torch.int8
+                   ) == True:
+                    warnings.warn("PyTorch output is quantized")
             raise
 
 
