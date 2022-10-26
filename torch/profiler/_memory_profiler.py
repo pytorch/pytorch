19e0d946bc115bf9bd0fb3fe9bb58db94f069f70
Taylor Robie <taylorrobie@fb.com>
2022-10-26 16:52:05 -0700

    Update base for Update on "[Profiler] Memory profiler part 6: Mark gradients and temporary intermediates."
    Semantic assignment will be built up as a series of passes which gradually pin down the regions of a trace. For this reason it is important to be very meticulous in the assignment of categories.
    
    We begin with gradients as they are both straightforward to identify and foundational to subsequent analysis. There are two mechanisms that the profiler can use to tag gradients, each with their own advantages and limitations. The first is direct inspection of the op graph which is generic but predicated on certain features of the Autograd engine. (And therefore not necessarily exhaustive.) The second approach is direct instrumentation via the python tracer. This method relies requires that gradients be attached to an nn.Module parameter and can miss corner cases such as `set_to_none=True` due to the cache structure of the python tracer. Combined these two approaches provide very high coverage.
    
    Temporaries are more straightforward; we can easily add them by trivial local inspection of a data flow node.
    
    Because this is the first PR in the end-to-end section most of the code is building the scaffolding for category bookkeeping and unit testing. (The actual gradient extraction was covered in an earlier PR.)
    
    Differential Revision: [D40220389](https://our.internmc.facebook.com/intern/diff/D40220389/)
    
    [ghstack-poisoned]

--- torch/profiler/_memory_profiler.py
+++ torch/profiler/_memory_profiler.py
@@ -120,9 +120,10 @@ def extract_gradients(
         and children
         and children[0].typed[0] == _EventType.TorchOp
         and children[0].name in ("aten::detach", "aten::add_")
-        and children[0].typed[1].inputs.tensor_metadata
+        and children[0].typed[1].inputs
+        and isinstance(children[0].typed[1].inputs[0], _TensorMetadata)
     ):
-        key = TensorKey.from_tensor(children[0].typed[1].inputs.tensor_metadata[0])
+        key = TensorKey.from_tensor(children[0].typed[1].inputs[0])
         if key:
             yield None, key
 
@@ -144,10 +145,6 @@ def extract_gradients(
                 p_grad_key = TensorKey.from_tensor(p_grad)
                 if p_grad_key is not None:
                     yield TensorKey.from_tensor(p), p_grad_key
-
-
-def tensor_inputs(t: _ExtraFields_TorchOp) -> Tuple[Optional[TensorKey], ...]:
-    return tuple(TensorKey.from_tensor(i) for i in t.inputs.tensor_metadata)
 
 
 class SchemaMatcher:
@@ -179,11 +176,21 @@ class SchemaMatcher:
             for i, arg in enumerate(schema.arguments):
                 mutable[i] |= getattr(arg.alias_info, "is_write", False)
 
-        return tuple(mutable or (True for _ in t.inputs.tensor_metadata))
+        return tuple(mutable or (True for _ in t.inputs))
 
     @classmethod
     def match_schemas(cls, t: _ExtraFields_TorchOp) -> Tuple[FunctionSchema, ...]:
-        signature = tuple(t or s for t, s in zip(tensor_inputs(t), t.inputs.ivalues))
+        signature = tuple(
+            # Tensor
+            TensorKey.from_tensor(i) if isinstance(i, _TensorMetadata)
+            #
+            # TensorList
+            else [TensorKey.from_tensor(j) for j in i] if isinstance(i, list)
+            #
+            # Scalar and uncaptured inputs.
+            else i
+            for i in t.inputs
+        )
 
         def matches(schema) -> bool:
             return len(schema.arguments) == len(signature) and all(
@@ -201,6 +208,11 @@ class SchemaMatcher:
 
         if isinstance(schema_type, torch._C.AnyType):
             return True
+
+        if schema_type.isSubtypeOf(torch._C.ListType.ofTensors()):
+            return isinstance(observed, list) and all(
+                isinstance(i, TensorKey) for i in observed
+            )
 
         type_map: Tuple[Tuple[Any, Union[type, Tuple[type, ...]]], ...] = (
             (torch._C.TensorType, TensorKey),
@@ -289,7 +301,12 @@ class DataFlowNode:
         def zip_mutable(
             t: _ExtraFields_TorchOp,
         ) -> Iterator[Tuple[Optional[TensorKey], bool]]:
-            return zip(tensor_inputs(t), SchemaMatcher.inputs_are_mutable(t))
+            for i, mutable in zip(t.inputs, SchemaMatcher.inputs_are_mutable(t)):
+                if isinstance(i, _TensorMetadata):
+                    yield TensorKey.from_tensor(i), mutable
+                elif isinstance(i, list):
+                    for j in i:
+                        yield TensorKey.from_tensor(j), mutable
 
         edges: DefaultDict[Optional[TensorKey], DataFlowEdge]
         edges = collections.defaultdict(DataFlowEdge)
