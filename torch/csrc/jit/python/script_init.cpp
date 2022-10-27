1f2f1cfd60639ab45f008449abcc81442717ddd0
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-27 22:36:21 +0200

    [skip ci] Refactor NotImplementedError to c10

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -1011,9 +1011,7 @@ void initJitScriptBindings(PyObject* module) {
           mm_name,
           [mm_name](const Object& self, py::args args, py::kwargs kwargs) {
             auto method = self.find_method(mm_name);
-            if (!method) {
-              throw NotImplementedError();
-            }
+            TORCH_CHECK_NOT_IMPLEMENTED(!method, c10::str( "object has no attribute '", mm_name, "'"));
             return invokeScriptMethodFromPython(
                 *method,
                 // NOLINTNEXTLINE(performance-move-const-arg)
