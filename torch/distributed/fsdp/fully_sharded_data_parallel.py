1ece315964f64fd8d9163e48c30878174f0d6b43
Andrew Gu <andgu@fb.com>
2022-10-27 02:23:12 +0000

    Update on "[FSDP] Fix wrapped module changing after ctor"
    [ghstack-poisoned]

--- torch/distributed/fsdp/fully_sharded_data_parallel.py
+++ torch/distributed/fsdp/fully_sharded_data_parallel.py
@@ -1884,40 +1884,39 @@ class FullyShardedDataParallel(nn.Module):
         # to non-root instances
         inconsistent_limit_all_gathers = False
         for fsdp_module in self.fsdp_modules(self):
-            if not fsdp_module._use_orig_params:
-                if fsdp_module._has_params:
-                    # Check if the wrapped module changed after construction
-                    # (e.g. applying the activation checkpointing wrapper) and
-                    # if so, de-register the `FlatParameter` from the old
-                    # wrapped module and register it to the new wrapped module
-                    flat_param = fsdp_module._handles[0].flat_param
-                    target_submodule = None
-                    target_name = None
-                    for submodule in fsdp_module.modules():
-                        for param_name, param in submodule._parameters.items():
-                            if flat_param is param:
-                                target_submodule = submodule
-                                target_name = param_name
-                                break
-                    if (
-                        target_submodule is not None
-                        and target_submodule is not fsdp_module.module
-                    ):
-                        assert target_name is not None
-                        if fsdp_module._debug_level == dist.DebugLevel.DETAIL:
-                            warnings.warn(
-                                "The FSDP wrapped module changed from "
-                                f"{target_submodule} to {fsdp_module.module}"
-                            )
-                        target_submodule._parameters.pop(target_name)
-                    elif target_submodule is None:
-                        raise RuntimeError(
-                            "Either the FSDP wrapped module was removed from "
-                            "the model or its `FlatParameter` was manually "
-                            "de-registered. Both of these are invalid behavior."
+            if not fsdp_module._use_orig_params and fsdp_module._has_params:
+                # Check if the wrapped module changed after construction
+                # (e.g. applying the activation checkpointing wrapper) and
+                # if so, de-register the `FlatParameter` from the old
+                # wrapped module and register it to the new wrapped module
+                flat_param = fsdp_module._handles[0].flat_param
+                target_submodule = None
+                target_name = None
+                for submodule in fsdp_module.modules():
+                    for param_name, param in submodule._parameters.items():
+                        if flat_param is param:
+                            target_submodule = submodule
+                            target_name = param_name
+                            break
+                if (
+                    target_submodule is not None
+                    and target_submodule is not fsdp_module.module
+                ):
+                    assert target_name is not None
+                    if fsdp_module._debug_level == dist.DebugLevel.DETAIL:
+                        warnings.warn(
+                            "The FSDP wrapped module changed from "
+                            f"{target_submodule} to {fsdp_module.module}"
                         )
-                    if target_submodule is not fsdp_module.module:
-                        fsdp_module._register_flat_param()
+                    target_submodule._parameters.pop(target_name)  # de-register
+                elif target_submodule is None:
+                    raise RuntimeError(
+                        "Either the FSDP wrapped module was removed from "
+                        "the model or its `FlatParameter` was manually "
+                        "de-registered. Both of these are invalid behavior."
+                    )
+                if target_submodule is not fsdp_module.module:
+                    fsdp_module._register_flat_param()
             if fsdp_module is not self:
                 # Relax the assert for non-root FSDP instances in case the
                 # nested initialized module is wrapped again in FSDP later (e.g.
