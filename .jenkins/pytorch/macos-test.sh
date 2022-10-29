2cb5d2f2caa03d10004097e0da9fb9e2a330f797
Huy Do <huydhn@gmail.com>
2022-10-29 08:43:45 +0000

    Cleanup macos common conda installation (#87816)
    The conda dependencies have all been installed for `_mac-test` in https://github.com/pytorch/pytorch/pull/87541.  I missed the same step for `_mac-build` and `_mac-test-mps` workflows, so both are also updated here. Note that arm64 is cross-compiled from x86, so the env file needs to be set explicitly in that case
    
    After this one, I have a WIP PR to consolidate macos pip dependencies next
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87816
    Approved by: https://github.com/ZainRizvi

--- .jenkins/pytorch/macos-test.sh
+++ .jenkins/pytorch/macos-test.sh
@@ -4,7 +4,6 @@
 # shellcheck source=./macos-common.sh
 source "$(dirname "${BASH_SOURCE[0]}")/macos-common.sh"
 
-conda install -y six
 if [[ ${BUILD_ENVIRONMENT} = *arm64* ]]; then
   pip install hypothesis "expecttest==0.1.3" "librosa>=0.6.2" "numba==0.56.0" psutil "scipy==1.9.0"
 else
