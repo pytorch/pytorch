865eee772f2f0ed7485af3c7410fe27d3db7d131
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-10-30 10:48:01 -0400

    Fix Verification.py

--- torch/onnx/verification.py
+++ torch/onnx/verification.py
@@ -199,25 +199,14 @@ def _compare_ort_pytorch_outputs(
                         f"within acceptable range {acceptable_error_percentage}."
                     )
                     continue
-            else:
-                    if (
-                        ort_out.dtype == torch.uint8 or ort_out.dtype == torch.int8
-                        ) and pt_out.is_quantized:
-                        if ort_out.q_scale() != pt_out.q_scale():
-                            warnings.warn(
-                                "ONNX output and PyTorch output has scale difference"
-                                )
-                        if ort_out.q_zero_point() != pt_out.q_zero_point():
-                            warnings.warn(
-                                "ONNX output and PyTorch output has zero point difference"
-                                )
-                    else:
-                        if (
-                            ort_out.dtype == torch.uint8 or ort_out.dtype == torch.int8
-                            ) == True:
-                            warnings.warn("ONNX output is quantized")
-                        if pt_out.is_quantized == True:
-                            warnings.warn("PyTorch output is quantized")
+            if (
+                ort_out.dtype == np.uint8 or ort_out.dtype == np.int8
+               ):
+                    warnings.warn("ONNX output is quantized")
+            if (
+                pt_out.dtype == np.uint8 or pt_out.dtype == np.int8
+               ):
+                    warnings.warn("PyTorch output is quantized")
             raise
 
 
