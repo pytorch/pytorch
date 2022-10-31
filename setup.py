e39b3c1a5064390eab60d2a7a3f5cc8c0b27b102
Radek Bartoň <radek.barton@microsoft.com>
2022-10-31 21:11:16 +0000

    Fix random "C1041: cannot open program database" errors when compiling on Windows (#88084)
    Adds `/FS` option to `CMAKE_CXX_FLAGS` and `CMAKE_CUDA_FLAGS`.
    
    So far I've encountered this kind of errors:
    
    ```
    C:\Users\MyUser\AppData\Local\Temp\tmpxft_00004728_00000000-7_cuda.cudafe1.cpp: fatal error C1041: cannot open program database 'C:\Projects\pytorch\build\third_party\gloo\gloo\CMakeFiles\gloo_cuda.dir\vc140.pdb'; if multiple CL.EXE write to the same .PDB file, please use /FS
    ```
    when building with VS 2022.
    
    cc @peterjc123 @mszhanyi @skyline75489 @nbcsm
    
    Related issues:
    - https://github.com/pytorch/pytorch/issues/87691
    - https://github.com/pytorch/pytorch/issues/39989
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88084
    Approved by: https://github.com/ezyang

--- setup.py
+++ setup.py
@@ -795,7 +795,7 @@ def configure_extension_build():
         # /EHsc is about standard C++ exception handling
         # /DNOMINMAX removes builtin min/max functions
         # /wdXXXX disables warning no. XXXX
-        extra_compile_args = ['/MD', '/EHsc', '/DNOMINMAX',
+        extra_compile_args = ['/MD', '/FS', '/EHsc', '/DNOMINMAX',
                               '/wd4267', '/wd4251', '/wd4522', '/wd4522', '/wd4838',
                               '/wd4305', '/wd4244', '/wd4190', '/wd4101', '/wd4996',
                               '/wd4275']
