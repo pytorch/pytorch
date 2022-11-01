39d9d2ed706dd9737ee49828b63c8975f31a87fe
lezcano <lezcano-93@hotmail.com>
2022-11-01 16:31:11 +0000

    Implement reference for lerp (#87424)
    We follow the vectorised CPU implementation for numerical accuracy
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87424
    Approved by: https://github.com/ezyang

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -16484,6 +16484,11 @@ python_ref_db = [
         supports_nvfuser=False,
     ),
     PythonRefInfo(
+        "_refs.lerp",
+        torch_opinfo_name="lerp",
+        supports_nvfuser=False,
+    ),
+    PythonRefInfo(
         "_refs.ones",
         torch_opinfo_name="ones",
         skips=(
