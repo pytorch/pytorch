5287215c55523e0632bda8369f0ef864b47d6156
Sean Ross-Ross <srossross@gmail.com>
2022-11-01 21:42:51 +0000

    feature: adding batch support for narrow_copy operator (#88130)
    Implement batch support https://github.com/pytorch/functorch/issues/825 for narrow copy
    
    narrow_copy was already added as an opinfo
    
    cc @zou3519 @Chillee @samdow @soumith
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88130
    Approved by: https://github.com/kshitij12345, https://github.com/zou3519

--- aten/src/ATen/functorch/BatchRulesViews.cpp
+++ aten/src/ATen/functorch/BatchRulesViews.cpp
@@ -490,6 +490,18 @@ std::tuple<Tensor, optional<int64_t>> unfold_batch_rule(
   return std::make_tuple(result, 0);
 }
 
+std::tuple<Tensor, optional<int64_t>> narrow_copy_batch_rule(
+    const Tensor &self, optional<int64_t> self_bdim, int64_t dim, c10::SymInt start, c10::SymInt length)
+{
+  TORCH_INTERNAL_ASSERT(self_bdim.has_value());
+  auto self_ = moveBatchDimToFront(self, self_bdim);
+  auto logical_rank = rankWithoutBatchDim(self, self_bdim);
+  dim = maybe_wrap_dim(dim, logical_rank) + 1;
+  auto result = self_.narrow_copy_symint(dim, start, length);
+
+  return std::make_tuple(result, 0);
+}
+
 std::tuple<Tensor, optional<int64_t>> movedim_batch_rule(const Tensor& self, optional<int64_t> self_bdim, IntArrayRef source, IntArrayRef destination) {
   auto self_ = moveBatchDimToFront(self, self_bdim);
   auto source_ = getPhysicalDims(self_, self_bdim.has_value(), source);
@@ -539,6 +551,7 @@ TORCH_LIBRARY_IMPL(aten, FuncTorchBatched, m) {
   VMAP_SUPPORT2(slice, Tensor, slice_batch_rule);
   VMAP_SUPPORT2(transpose, int, transpose_int_batch_rule);
   VMAP_SUPPORT(diag_embed, diag_embed_batch_rule);
+  VMAP_SUPPORT(narrow_copy, narrow_copy_batch_rule);
 }
 
 }}
