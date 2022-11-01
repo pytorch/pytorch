42456eb4b18482e45c18e62ada27fc7031e77b1a
Bryce Long <blong@nvidia.com>
2022-11-01 14:57:48 -0700

    Add NVTX markers that dump additional information for: graphs that NVFuser JIT compiles: - the markers show the list of ops, args, and inputs that make up the graph FX nodes that are not touched by NVFuser: - the markers show the op, name, and arg list of the node

--- torch/_prims/nvfuser_executor.py
+++ torch/_prims/nvfuser_executor.py
@@ -28,6 +28,8 @@ if torch.cuda.is_available():
 else:
     DataType = None
 
+import os
+
 DEFAULT_NVFUSER_PYTHON_CONFIG = MappingProxyType(
     {
         "use_python_fusion_cache": True,
@@ -247,10 +249,18 @@ def nvfuser_execute(gm: GraphModule, *args, executor_parameters=None):
             arg for arg in flat_args if isinstance(arg, (torch.Tensor, Number))
         )
 
-        return tree_unflatten(
+        if os.getenv('PYTORCH_NVFUSER_DUMP_NVTX'): torch.cuda.nvtx.range_push("fusion: {0}, graph: {1}".format(fusion.id(),
+                str([{
+                    'op': n.op,
+                    'name': n.name,
+                    'args': n.args
+                } for n in gm.graph.nodes])))
+        result = tree_unflatten(
             fusion.execute(concrete_fusion_inputs),  # type: ignore[has-type]
             unflatten_spec,  # type: ignore[has-type]
         )
+        if os.getenv('PYTORCH_NVFUSER_DUMP_NVTX'): torch.cuda.nvtx.range_pop()
+        return result
     else:
         warn(
             "nvfuser_executor is executed with non-cuda args, fallback to aten executor"
@@ -420,6 +430,12 @@ def maybe_partition_graph(
     else:
         return gm, any_unsupported
 
+class NVTXInterpreter(torch.fx.Interpreter):
+    def run_node(self, n):
+        torch.cuda.nvtx.range_push("name: {0}, args: {1}, op: {2}, meta: {3}".format(n.name, n.args, n.op, n.meta))
+        result = super().run_node(n)
+        torch.cuda.nvtx.range_pop()
+        return result
 
 def nvfuser_execute_partitioned(gm: GraphModule, *args, executor_parameters=None):
     executor_parameters = executor_parameters or DEFAULT_NVFUSER_PYTHON_CONFIG
@@ -440,6 +456,9 @@ def nvfuser_execute_partitioned(gm: GraphModule, *args, executor_parameters=None
         use_python_fusion_cache=use_python_fusion_cache,
     )
     if is_partitioned:
-        return gm(*args)
+        if os.getenv('PYTORCH_NVFUSER_DUMP_NVTX'):
+            return NVTXInterpreter(gm).run(*args)
+        else:
+            return gm(*args)
     else:
         return nvfuser_execute(gm, *args, executor_parameters=executor_parameters)
