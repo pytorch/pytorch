47ab30a104451a527c85e09f03a12322fd393f38
Kiarash Jamali <kjamali@mrc-lmb.cam.ac.uk>
2022-11-01 11:19:17 +0000

    @eqy suggestion for compilation issue

--- aten/src/ATen/cuda/Atomic.cuh
+++ aten/src/ATen/cuda/Atomic.cuh
@@ -6,6 +6,9 @@
 
 #include <ATen/NumericUtils.h>
 
+#if !(defined(USE_ROCM) || ((defined(CUDA_VERSION) && CUDA_VERSION < 11000) || (defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 800))))
+#include <cuda_bf16.h>
+
 template <typename T>
 struct AtomicFPOp;
 
