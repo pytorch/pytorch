69eb52b0ca041090ea32a66292c8e8e7829786f0
soulitzer <soulitzer@gmail.com>
2022-11-01 13:36:05 -0400

    Split out forward AD tests from test_ops_gradients
    [ghstack-poisoned]

--- test/run_test.py
+++ test/run_test.py
@@ -26,6 +26,7 @@ from torch.testing._internal.common_utils import (
     shell,
     set_cwd,
     parser as common_parser,
+    is_slow_gradcheck_env,
 )
 import torch.distributed as dist
 from torch.multiprocessing import get_context
@@ -302,6 +303,7 @@ CORE_TEST_LIST = [
     "test_nn",
     "test_ops",
     "test_ops_gradients",
+    "test_ops_fwd_gradients",
     "test_ops_jit",
     "test_torch"
 ]
@@ -628,6 +630,9 @@ def run_doctests(test_module, test_directory, options):
     Assumes the incoming test module is called doctest, and simply executes the
     xdoctest runner on the torch library itself.
     """
+    # Maybe there's a better place to do this skip?
+    if is_slow_gradcheck_env():
+        return 1
     import xdoctest
     import pathlib
     pkgpath = pathlib.Path(torch.__file__).parent
@@ -775,6 +780,7 @@ CUSTOM_HANDLERS = {
     "doctests": run_doctests,
     "test_ops": run_test_ops,
     "test_ops_gradients": run_test_ops,
+    "test_ops_fwd_gradients": run_test_ops,
     "test_ops_jit": run_test_ops,
     "functorch/test_ops": run_test_ops,
 }
@@ -1116,6 +1122,11 @@ def get_selected_tests(options):
         else:
             print("Found test time stats from artifacts")
             test_file_times_config = test_file_times[test_config]
+            if 'slow-gradcheck' in os.getenv("BUILD_ENVIRONMENT", ""):
+                # HACK: hardcode approx test times, so these two don't get put in the same shard
+                #       we can remove this when their actual runtimes are recorded
+                test_file_times_config["test_ops_fwd_gradients"] = 3600 * 2 + 600 # 2:10
+                test_file_times_config["test_ops_gradients"] = 3600 * 2 + 600 # 2:10
             shards = calculate_shards(num_shards, selected_tests, test_file_times_config,
                                       must_serial=must_serial)
             _, tests_from_shard = shards[which_shard - 1]
