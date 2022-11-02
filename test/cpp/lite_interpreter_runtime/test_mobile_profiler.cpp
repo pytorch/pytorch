2e1fe3e38124a35327579e0de3753ae98df5764e
Digant Desai <digantdesai@fb.com>
2022-11-01 21:39:09 -0700

    [edge profiler] Add e2e test for profiler event and chrometrace (#87877)
    * Runs an existing model and checks an aten op if it gets perf events generated in the chrometrace
    * Doesn't check for exact values since that's harder to do in a hardware independent way
    
    Differential Revision: [D40474957](https://our.internmc.facebook.com/intern/diff/D40474957/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87877
    Approved by: https://github.com/SS-JIA

--- test/cpp/lite_interpreter_runtime/test_mobile_profiler.cpp
+++ test/cpp/lite_interpreter_runtime/test_mobile_profiler.cpp
@@ -9,6 +9,8 @@
 
 #include <unordered_set>
 
+#include <torch/csrc/profiler/events.h>
+
 #ifdef EDGE_PROFILER_USE_KINETO
 namespace torch {
 namespace jit {
@@ -25,7 +27,10 @@ bool checkMetaData(
     if (line.find(op_name) != std::string::npos) {
       while (std::getline(trace_file, line)) {
         if (line.find(metadata_name) != std::string::npos) {
-          if (line.find(metadata_val) != std::string::npos) {
+          if (line.find(metadata_val) != std::string::npos ||
+              !metadata_val.size()) {
+            /* if found the right metadata_val OR if expected
+             * metadata value is an empty string then ignore the matadata_val */
             return true;
           }
         }
@@ -155,6 +160,56 @@ TEST(MobileProfiler, BackendMemoryEvents) {
   trace_file.seekg(0, std::ios_base::beg);
   metadata_name = "Total Reserved";
   ASSERT_TRUE(checkMetaData("[memory]", metadata_name, "49152", trace_file));
+}
+
+TEST(MobileProfiler, ProfilerEvent) {
+  /*
+   * TODO: Using __FILE__ is unreliable e.g. it fails to resolve correctly when
+   * using buck2, works ok with buck1
+   */
+  std::string filePath(__FILE__);
+  auto testModelFile = filePath.substr(0, filePath.find_last_of("/\\") + 1);
+  testModelFile.append("test_backend_for_profiling.ptl");
+
+  std::vector<IValue> inputs;
+  inputs.emplace_back(at::rand({64, 64}));
+  inputs.emplace_back(at::rand({64, 64}));
+  std::string trace_file_name("/tmp/test_trace_profiler_event.trace");
+
+  std::vector<std::string> events(
+      torch::profiler::ProfilerPerfEvents.begin(),
+      torch::profiler::ProfilerPerfEvents.end());
+
+  mobile::Module bc = _load_for_mobile(testModelFile);
+  {
+    // Bail if something goes wrong here
+    try {
+      KinetoEdgeCPUProfiler profiler(
+          bc,
+          trace_file_name,
+          false, // record input_shapes
+          false, // profile memory
+          true, // record callstack
+          false, // record flops
+          true, // record module hierarchy
+          events); // performance events
+      bc.forward(inputs);
+    } catch (...) {
+      return;
+    }
+  } // End of profiler
+  std::ifstream trace_file(trace_file_name);
+  std::string line;
+  ASSERT_TRUE(trace_file.is_open());
+
+  for (auto& event : events) {
+    trace_file.seekg(0, std::ios_base::beg);
+    /*
+     * Just checking if the event entry exists in the chrometrace.
+     * Checking the value in a hardware independent matter is tricky.
+     */
+    ASSERT_TRUE(checkMetaData("aten::__getitem__", event, "", trace_file));
+  }
 }
 
 } // namespace mobile
