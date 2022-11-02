dd940eb79851c7cca68de4ed662fc01a0fc8af9c
Mergen Nachin <mergennachin@gmail.com>
2022-11-02 13:58:40 -0700

    Update on "Disallow module attribute mutation"
    Summary:
    
    See https://github.com/pytorch/torchdynamo/issues/1475
    
    Not allowing any new mutations happen inside forward() function during
    export.
    
    Test Plan:
    
    Added new unit tests (3 positive tests and 3 negative tests)
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx
    
    [ghstack-poisoned]

--- test/dynamo/test_export_mutations.py
+++ test/dynamo/test_export_mutations.py
@@ -1,7 +1,4 @@
-import logging
-import shutil
-import unittest
-
+# Owner(s): ["module: dynamo"]
 import torch
 import torch._dynamo.test_case
 import torch._dynamo.testing
@@ -10,6 +7,12 @@ class MutationExportTests(torch._dynamo.test_case.TestCase):
     def check_failure_on_export(self, mod, *args):
         with self.assertRaises(AssertionError):
             torch._dynamo.export(mod, *args)
+
+    def check_same_with_export(self, mod, arg):
+        real_result = mod(arg)
+        graph, _ = torch._dynamo.export(mod, arg)
+        result = graph(arg)
+        self.assertTrue(torch._dynamo.utils.same(result, real_result))
 
     def test_module_attribute_mutation_violation_positive_1(self):
         # Mutating attribute with a Tensor type
@@ -61,8 +64,7 @@ class MutationExportTests(torch._dynamo.test_case.TestCase):
             def forward(self, x):
                 return x.sum() + self.a.to(torch.float64).sum()
 
-        gm, _ = torch._dynamo.export(Foo(), torch.Tensor(3, 2))
-        self.assertTrue(gm is not None)
+        self.check_same_with_export(Foo(), torch.Tensor(3, 2))
 
     def test_module_attribute_mutation_violation_negative_2(self):
         # Mutating attribute with a Tensor type inside __init__ twice
@@ -75,8 +77,7 @@ class MutationExportTests(torch._dynamo.test_case.TestCase):
             def forward(self, x):
                 return x.sum() + self.a.sum()
 
-        gm, _ = torch._dynamo.export(Foo(), torch.Tensor(3, 2))
-        self.assertTrue(gm is not None)
+        self.check_same_with_export(Foo(), torch.Tensor(3, 2))
 
     def test_module_attribute_mutation_violation_negative_3(self):
         # Mutating local variable inside forward()
@@ -90,9 +91,26 @@ class MutationExportTests(torch._dynamo.test_case.TestCase):
                 b = b * 5
                 return x.sum() + self.a.sum() + b
 
-        gm, _ = torch._dynamo.export(Foo(), torch.Tensor(3, 2))
-        self.assertTrue(gm is not None)
+        self.check_same_with_export(Foo(), torch.Tensor(3, 2))
 
+
+    def test_module_attribute_mutation_violation_negative_4(self):
+        # Mutating attribute with a Tensor type
+        # But not exporting but using eager mode as well as dynamo optimize mode
+        class Foo(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.a = torch.Tensor(3, 2)
+
+            def forward(self, x):
+                self.a = self.a.to(torch.float64)
+                return x.sum() + self.a.sum()
+
+        mod = Foo()
+        arg = torch.Tensor(3, 2)
+        real_result = mod(arg)
+        opt_mod = torch._dynamo.optimize("eager", nopython=True)(mod)
+        self.assertTrue(torch._dynamo.utils.same(opt_mod(arg), real_result))
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
