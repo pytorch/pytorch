c78d2095ed4c0f4b76dcec38dba768d3d7b56a45
Brian Hirsh <hirsheybar@fb.com>
2022-11-02 11:22:01 -0700

    reland "fix as_strided_scatter_backward (#87646)"
    This reverts commit 71fb763e5452881cb3be8fefa9419b785d0a61e2.
    
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -952,7 +952,6 @@ aot_autograd_failures = {
     xfail('linalg.eig'),
     xfail('scatter_reduce', 'prod'),
 
-    # non-deterministic
     skip('as_strided_scatter'),
 
     # Too annoying to generate random inputs
