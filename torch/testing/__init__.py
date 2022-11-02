05cda6584135f36c8f998c044eb9326e0dc39726
Philip Meier <github.pmeier@posteo.de>
2022-11-02 11:25:06 +0100

    remove assert_allclose from torch.testing (#87974)
    See #87969 or #86586 for the reasoning.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87974
    Approved by: https://github.com/mruberry

--- torch/testing/__init__.py
+++ torch/testing/__init__.py
@@ -1,4 +1,3 @@
 from ._comparison import assert_close as assert_close
 from torch._C import FileCheck as FileCheck
 from ._creation import make_tensor as make_tensor
-from ._deprecated import *  # noqa: F403
