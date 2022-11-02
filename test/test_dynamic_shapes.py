eb6c4eaa5cca299c4c19c9c491bb7cc9d03b26d0
albanD <desmaison.alban@gmail.com>
2022-11-02 17:41:22 -0400

    Fix categorization of sym_int method
    [ghstack-poisoned]

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -420,16 +420,6 @@ expected_failure_sym_magic_methods = {
     ('floordiv', 'int', 'SymFloat'),  # unsupported operand type(s) for //: 'int' and 'SymFloat'
     ('floordiv', 'SymInt', 'SymFloat'),  # Cannot convert complex to float
     ('mod', 'int', 'SymFloat'),  # unsupported operand type(s) for %: 'int' and 'SymFloat'
-    ('sym_int', 'int', 'float'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'SymInt', 'float'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'int', 'SymFloat'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'SymInt', 'SymFloat'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'int', 'int'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'SymInt', 'int'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'int', 'SymInt'),  # sym_int() takes 1 positional argument but 2 were given
-    ('sym_int', 'SymInt', 'SymInt'),  # sym_int() takes 1 positional argument but 2 were given
-
-
 }
 
 @skipIfTorchDynamo("Creating ShapeEnv fails for confusing reasons (also we never expect dynamo to see code like this)")
