465fc9d6453ce55fa225c6e23bcd27fa2dbe5714
Grigory Sizov <grigorysizov@fb.com>
2022-11-03 15:56:54 +0100

    Allow both masks on fast path in transformer and MHA

--- test/test_nn.py
+++ test/test_nn.py
@@ -13123,9 +13123,9 @@ class TestNNDeviceType(NNTestCase):
         return exp / s
 
     def test_masked_softmax_mask_types_0_1(self, device):
-        # Test that mask type 0 (LxL attention mask) and mask type 1 (BxL padding mask)
-        # are processed correctly on the fast path and the results match explicit slow
-        # calculation.
+        # Test that mask type 0 (LxL attention mask), mask type 1 (BxL padding mask),
+        # and mask type 1 (generic BxHxLxL mask) are processed correctly on the
+        # fast path and the results match explicit slow calculation.
         sizes = [(1, 1, 32), (3, 16, 310), (12, 4, 1024), (4, 2, 1200)]
 
         for (B, num_heads, L) in sizes:
@@ -13138,7 +13138,12 @@ class TestNNDeviceType(NNTestCase):
             src_key_padding_mask_orig = torch.randint(0, 2, (B, L)).bool()
             src_key_padding_mask = src_key_padding_mask_orig.reshape(B, 1, 1, L).expand(B, num_heads, L, L).bool()
 
-            masks = [(src_mask_orig, src_mask, 0), (src_key_padding_mask_orig, src_key_padding_mask, 1)]
+            # mask_type == 2 =>  shape BxHxLxL
+            generic_mask = torch.randint(0, 2, (B, num_heads, L, L)).bool()
+            masks = [(src_mask_orig, src_mask, 0),
+                     (src_key_padding_mask_orig, src_key_padding_mask, 1),
+                     (generic_mask, generic_mask, 2)
+                     ]
             for dim in [0, 3]:
                 for mask_orig, mask, mask_type in masks:
                     if (self.device_type == "cuda") and (num_heads % 2) and (mask_type == 1):
@@ -13182,7 +13187,9 @@ class TestNNDeviceType(NNTestCase):
             src_mask = torch.randint(0, 2, (L, L)).bool()
             # mask_type == 1 => padding mask of shape BxL
             src_key_padding_mask = torch.randint(0, 2, (B, L)).bool()
-            masks = [(src_mask, 0), (src_key_padding_mask, 1)]
+            # mask_type == 2 => generic mask of shape BxHxLxL
+            generic_mask = torch.randint(0, 2, (B, num_heads, L, L)).bool()
+            masks = [(src_mask, 0), (src_key_padding_mask, 1), (generic_mask, 2)]
             input = torch.randn((B, num_heads, L, L))
             for dim in [0, 3]:
                 for mask, mask_type in masks:
@@ -13197,8 +13204,10 @@ class TestNNDeviceType(NNTestCase):
                         softmax_res = torch._masked_softmax(input_device, mask_device, dim, mask_type)
                         if mask_type == 0:
                             mask_expanded = mask_device.reshape(1, 1, L, L).expand(B, num_heads, L, L).bool()
-                        else:
+                        elif mask_type == 1:
                             mask_expanded = mask_device.reshape(B, 1, 1, L).expand(B, num_heads, L, L).bool()
+                        else:
+                            mask_expanded = mask_device
                         # In result, should only fill the entirely masked out rows since those are non-deterministic (*may* be 0)
                         # Fill rows with all True's with 0
                         mask_out = mask_expanded.all(dim, keepdim=True).expand(mask_expanded.shape)
@@ -15567,21 +15576,31 @@ class TestNNDeviceType(NNTestCase):
         """
         Test transformer fast path on CPU with different valid mask types and shapes
         """
-        model = torch.nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True, device=device, dtype=dtype)
+        d_model = 512
+        nhead = 8
+        batch_size = 32
+        src_len = 10
+
+        model = torch.nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, batch_first=True,
+                                                 device=device, dtype=dtype, dropout=0)
         model.eval()
 
         # Batched inputs
-        src = torch.rand(32, 10, 512)
+        src = torch.rand(batch_size, src_len, 512)
 
         # Attention mask of shape (src_len, src_len)
-        src_mask = torch.zeros(10, 10).to(torch.bool)
+        src_mask = torch.zeros(src_len, src_len).to(torch.bool)
         with torch.no_grad():
             model(src, src_mask=src_mask)
 
         # Padding mask of shape (batch_size, src_len)
-        src_key_padding_mask = torch.zeros(32, 10).to(torch.bool)
+        src_key_padding_mask = torch.zeros(batch_size, src_len).to(torch.bool)
         with torch.no_grad():
             model(src, src_key_padding_mask=src_key_padding_mask)
+
+        # Provide both masks
+        with torch.no_grad():
+            model(src, src_mask=src_mask, src_key_padding_mask=src_key_padding_mask)
 
 
     @dtypes(torch.float)
