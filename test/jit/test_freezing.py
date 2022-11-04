879df37b4a13fe8a51085a66122c34c7995df3fa
min-jean-cho <min.jean.cho@intel.com>
2022-11-03 21:02:02 -0700

    remove redundant line

--- test/jit/test_freezing.py
+++ test/jit/test_freezing.py
@@ -2278,7 +2278,7 @@ class TestFrozenOptimizations(JitTestCase):
 
     @skipCUDAMemoryLeakCheckIf(True)
     @unittest.skipIf(not TEST_CUDA, "Optimization currently only run for GPU")
-    def test_lin_bn_folding_autocast_scenario_cuda(self):
+    def test_linear_bn_folding_autocast_scenario_cuda(self):
         module_pairs = [(nn.Linear, nn.BatchNorm1d), (nn.Linear, nn.BatchNorm2d), (nn.Linear, nn.BatchNorm3d)]
         use_tracing = [True, False]
         bn_running_stats = [True, False]
@@ -2320,8 +2320,6 @@ class TestFrozenOptimizations(JitTestCase):
             self.assertTrue(bias_input is not None)
             self.assertTrue(weight_input.type().dtype() == torch.half)
             self.assertTrue(bias_input.type().dtype() == torch.half)
-
-            x = torch.rand(inps, dtype=torch.half).cuda()
 
             self.assertEqual(mod_eager(x), scripted_mod(x), atol=1e-2, rtol=1e-2)
             self.assertEqual(mod_eager(x), scripted_mod(x), atol=1e-2, rtol=1e-2)
