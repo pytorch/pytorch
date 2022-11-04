28e0c926da866f8f99ea76bcb5ca955e43492598
mikey dagitses <mikeyd@meta.com>
2022-11-04 08:50:07 -0700

    extract out the clean workspace test to its own file
    Summary:
    This test relies on what the root workspace is before any other code
    is run. However, some of the test cases change it. If the order the
    tests are run is randomized, then the test can fail if run after one
    of them.
    
    Having it on its own ensures that it always sees a pristine state.
    
    Test Plan: Verified locally and confirmed in CI.
    
    Differential Revision: D41030735
    
    fbshipit-source-id: 670f8cdfcb7ab44be76f792f0d7f45fb567d9132

---
+++ caffe2/python/clean_workspace_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from caffe2.python import workspace
+
+
+# This test is extracted out because it relies on the pristine state of the initial
+# workspace. When tests are run in different orders, this test may become flaky because
+# of global state modifications impacting what the root folder is after a reset.
+class TestWorkspace(unittest.TestCase):
+    def testRootFolder(self):
+        self.assertEqual(workspace.ResetWorkspace(), True)
+        self.assertEqual(workspace.RootFolder(), ".")
+        self.assertEqual(workspace.ResetWorkspace("/tmp/caffe-workspace-test"), True)
+        self.assertEqual(workspace.RootFolder(), "/tmp/caffe-workspace-test")
+
+
+if __name__ == "__main__":
+    unittest.main()
