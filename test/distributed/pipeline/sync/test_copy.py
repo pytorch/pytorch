de9b80ab76bb6d23f6f4371e7da03fbdae999858
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2022-11-07 15:31:10 +0900

    Fix non tensor cp test

--- test/distributed/pipeline/sync/test_copy.py
+++ test/distributed/pipeline/sync/test_copy.py
@@ -14,24 +14,22 @@ from torch.distributed.pipeline.sync.stream import CPUStream, current_stream, ge
 
 skip_if_no_cuda = pytest.mark.skipif(not torch.cuda.is_available(), reason="cuda required")
 
-
 def _test_copy_wait(prev_stream, next_stream, cuda_sleep=None):
     device = get_device(prev_stream)
 
     with use_stream(prev_stream):
         if is_cuda(prev_stream):
             cuda_sleep(0.5)
-        x = (torch.ones(100, device=device, requires_grad=True), 0)
+        x = torch.ones(100, device=device, requires_grad=True)
 
-    y = Copy.apply(prev_stream, next_stream, x)
-    y = Wait.apply(prev_stream, next_stream, x)
-    z = sum(*y)
+    (y,) = Copy.apply(prev_stream, next_stream, x)
+    (y,) = Wait.apply(prev_stream, next_stream, x)
 
     with use_stream(next_stream):
-        assert torch.allclose(z.sum(), torch.tensor(100.0, device=device))
-        z.norm().backward()
+        assert torch.allclose(y.sum(), torch.tensor(100.0, device=device))
+        y.norm().backward()
     with use_stream(prev_stream):
-        assert torch.allclose(x[0].grad.sum(), torch.tensor(10.0, device=device))
+        assert torch.allclose(x.grad.sum(), torch.tensor(10.0, device=device))
 
 
 def test_copy_wait_cpu_cpu():
@@ -69,3 +67,19 @@ def test_wait_multiple_tensors():
 
     assert a.grad_fn is b.grad_fn
     assert a.grad_fn.__class__ is Wait._backward_cls
+
+def test_copy_non_tensor():
+    prev_stream = CPUStream
+    next_stream = CPUStream
+    device = get_device(prev_stream)
+
+    with use_stream(prev_stream):
+        x = (torch.ones(100, device=device, requires_grad=True), 0)
+    
+    y = Copy.apply(prev_stream, next_stream, x)
+    z = sum(*y)
+    with use_stream(next_stream):
+        assert torch.allclose(z.sum(), torch.tensor(100.0, device=device))
+        z.norm().backward()
+    with use_stream(prev_stream):
+        assert torch.allclose(x[0].grad.sum(), torch.tensor(10.0, device=device))
