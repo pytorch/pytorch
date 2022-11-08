f0bb08784e279dbf299d3cb31ac7bce032c43bcb
Natalia Gimelshein <ngimel@fb.com>
2022-11-08 17:06:28 +0000

    enable scalar reduction with dim=-1 (#88628)
    Tested with all samples for `sum`, but also fixes all samples errors on other reductions (amin, amax, any, all etc)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88628
    Approved by: https://github.com/desertfire

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -3002,7 +3002,7 @@ def _validate_reduction_axis(x, axis):
     axis = list(axis)
     for i in range(len(axis)):
         if axis[i] < 0:
-            axis[i] += len(size)
+            axis[i] += len(size) if len(size) else 1
         assert 0 <= axis[i] < len(size) or (len(size) == 0 and axis[i] == 0)
     assert len(set(axis)) == len(axis), "reduction axis not unique"
     return axis
