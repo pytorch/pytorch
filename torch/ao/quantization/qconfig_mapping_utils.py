aaaeb76d953fc9e50cdc910e7170a1f419cb8e11
HDCharles <charlesdavidhernandez@gmail.com>
2022-11-08 07:59:11 -0800

    [ao] qconfig_mapping_utils.py fixing public v private (#87517)
    Summary: made _get_object_type_qconfig, _get_module_name_regex_qconfig,
    _get_module_name_qconfig, _maybe_adjust_qconfig_for_module_type_or_name,
    _get_flattened_qconfig_dict _update_qconfig_for_qat private
    
    Test Plan: python test/test_public_bindings.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D40709279](https://our.internmc.facebook.com/intern/diff/D40709279)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87517
    Approved by: https://github.com/jcaip

--- torch/ao/quantization/qconfig_mapping_utils.py
+++ torch/ao/quantization/qconfig_mapping_utils.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict, Callable, Union
+from typing import Dict, Callable, Union, List
 
 from .utils import (
     get_combined_dict,
@@ -12,25 +12,18 @@ from .qconfig import QConfigAny
 from .qconfig_mapping import QConfigMapping
 
 
-# TODO: revisit this list. Many helper methods shouldn't be public
-__all__ = [
-    "get_flattened_qconfig_dict",
-    "get_object_type_qconfig",
-    "get_module_name_qconfig",
-    "get_module_name_regex_qconfig",
-    "maybe_adjust_qconfig_for_module_type_or_name",
-    "update_qconfig_for_qat",
+__all__: List[str] = [
 ]
 
 
-def get_object_type_qconfig(
+def _get_object_type_qconfig(
         qconfig_mapping: QConfigMapping,
         object_type: Union[Callable, str],
         fallback_qconfig: QConfigAny) -> QConfigAny:
     return qconfig_mapping.object_type_qconfigs.get(object_type, fallback_qconfig)
 
 
-def get_module_name_regex_qconfig(qconfig_mapping, module_name, fallback_qconfig):
+def _get_module_name_regex_qconfig(qconfig_mapping, module_name, fallback_qconfig):
     for regex_pattern, qconfig in qconfig_mapping.module_name_regex_qconfigs.items():
         if re.match(regex_pattern, module_name):
             # first match wins
@@ -38,7 +31,7 @@ def get_module_name_regex_qconfig(qconfig_mapping, module_name, fallback_qconfig
     return fallback_qconfig
 
 
-def get_module_name_qconfig(qconfig_mapping, module_name, fallback_qconfig):
+def _get_module_name_qconfig(qconfig_mapping, module_name, fallback_qconfig):
     if module_name == '':
         # module name qconfig not found
         return fallback_qconfig
@@ -46,23 +39,23 @@ def get_module_name_qconfig(qconfig_mapping, module_name, fallback_qconfig):
         return qconfig_mapping.module_name_qconfigs[module_name]
     else:
         parent, _ = _parent_name(module_name)
-        return get_module_name_qconfig(qconfig_mapping, parent, fallback_qconfig)
+        return _get_module_name_qconfig(qconfig_mapping, parent, fallback_qconfig)
 
 
-def maybe_adjust_qconfig_for_module_type_or_name(qconfig_mapping, module_type, module_name, global_qconfig):
+def _maybe_adjust_qconfig_for_module_type_or_name(qconfig_mapping, module_type, module_name, global_qconfig):
     # get qconfig for module_name,
     # fallback to module_name_regex_qconfig, module_type_qconfig,
     # global_qconfig if necessary
-    module_type_qconfig = get_object_type_qconfig(
+    module_type_qconfig = _get_object_type_qconfig(
         qconfig_mapping, module_type, global_qconfig)
-    module_name_regex_qconfig = get_module_name_regex_qconfig(
+    module_name_regex_qconfig = _get_module_name_regex_qconfig(
         qconfig_mapping, module_name, module_type_qconfig)
-    module_name_qconfig = get_module_name_qconfig(
+    module_name_qconfig = _get_module_name_qconfig(
         qconfig_mapping, module_name, module_name_regex_qconfig)
     return module_name_qconfig
 
 
-def get_flattened_qconfig_dict(qconfig_mapping: QConfigMapping) -> Dict[Union[Callable, str], QConfigAny]:
+def _get_flattened_qconfig_dict(qconfig_mapping: QConfigMapping) -> Dict[Union[Callable, str], QConfigAny]:
     """ flatten the global, object_type and module_name qconfig
     to the same qconfig_dict so that it can be used by
     propagate_qconfig_ function.
@@ -94,7 +87,7 @@ def get_flattened_qconfig_dict(qconfig_mapping: QConfigMapping) -> Dict[Union[Ca
     return flattened
 
 
-def update_qconfig_for_qat(
+def _update_qconfig_for_qat(
         qconfig_mapping: QConfigMapping,
         additional_qat_module_mapping: Dict[Callable, Callable]):
     """
