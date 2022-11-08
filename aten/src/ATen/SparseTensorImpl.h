eec20532c7ee85b8df9f9aa1adfb5ff9fdfdf9fa
anjali411 <chourdiaanjali123@gmail.com>
2022-11-08 15:45:16 +0000

    don't need this since we have generic_sizes

--- aten/src/ATen/SparseTensorImpl.h
+++ aten/src/ATen/SparseTensorImpl.h
@@ -96,27 +96,6 @@ struct TORCH_API SparseTensorImpl : public TensorImpl {
     refresh_numel();
   }
 
-  // TODO: move this to TensorImpl.h
-
-  // From https://stackoverflow.com/a/3057522/23845
-  // TODO: does C++14 have a stdlib template for this?
-  template <typename T>
-  struct identity {
-    typedef T type;
-  };
-
-  template <typename T>
-  ArrayRef<T> impl_sizes() {
-    return _impl_sizes(identity<T>());
-  }
-
-  ArrayRef<int64_t> _impl_sizes(identity<int64_t>) {
-    return sizes();
-  }
-  ArrayRef<c10::SymInt> _impl_sizes(identity<c10::SymInt>) {
-    return sym_sizes();
-  }
-
   // NOTE: This function preserves invariants of sparse_dim/dense_dim with
   // respect to indices and values.
   //
