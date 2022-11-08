32b80bd2f52a1e7589c82c28ff7d32829c23e8e2
Iris <wz337@cornell.edu>
2022-11-08 01:37:00 +0000

    [RFC] Move distributed checkpointing from torch.distributed._shard.checkpoint to torch.distributed.checkpoint

--- torch/distributed/_shard/checkpoint/__init__.py
+++ torch/distributed/_shard/checkpoint/__init__.py
@@ -1,21 +1,12 @@
-from .metadata import (
-    TensorStorageMetadata,
-    BytesStorageMetadata,
-    ChunkStorageMetadata,
-    Metadata,
-)
-from .state_dict_loader import load_state_dict
-from .state_dict_saver import save_state_dict
-from .storage import StorageReader, StorageWriter
-from .filesystem import FileSystemReader, FileSystemWriter
-from .api import CheckpointException
+# Keep old package for BC purposes, this file should be removed once
+# everything moves to the `torch.distributed.checkpoint` package.
+import sys
+import torch
+import warnings
 
-
-from .planner import (
-    SavePlanner,
-    LoadPlanner,
-    SavePlan,
-    LoadPlan,
-    ReadItem,
-    WriteItem,
+from torch.distributed.checkpoint import *  # noqa: F403
+warnings.warn(
+    "torch.distributed._shard.checkpoint will be deprecated, use torch.distributed.checkpoint instead",
+    DeprecationWarning
 )
+sys.modules['torch.distributed._sharded.checkpoint'] = torch.distributed.checkpoint
