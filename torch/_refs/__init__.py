c16ad6ef566707927fdc5507cb29b2a0c52296fe
Edward Z. Yang <ezyang@fb.com>
2022-11-08 10:23:53 -0800

    Support diag_embed.out decomposition (#88671)
    This is a little tricky: there is a diag_embed.out, but its not bound
    in Python because it's autogenerated, see https://github.com/pytorch/pytorch/issues/88598
    So I can't "just" add the out variant to the ref, as this makes it
    inconsistent with the torch API.  To workaround this, I mark the ref
    as supporting out, but not the original function.
    
    This is useful to do, because it means that diag_embed.out now supports
    symbolic shapes.  However, this cannot be easily tested because
    I can't mark the out variant as being supported in the normal OpInfo test.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88671
    Approved by: https://github.com/mruberry

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -3585,6 +3585,7 @@ diagonal_copy = _make_copy_from_view(diagonal)
 
 
 @register_decomposition(torch.ops.aten.diag_embed)
+@out_wrapper()
 def diag_embed(
     t: TensorLikeType,
     offset: int = 0,
