40f7ad2c5334b348352b4a13812a7e215c0766fa
AllenTiTaiWang <titaiwang@microsoft.com>
2022-11-09 17:27:22 +0000

    [ONNX] Add onnx::Max into standard Op for scalar type alignment (#88750)
    Easy fix for onnx::Max ScalarType
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88750
    Approved by: https://github.com/justinchuby, https://github.com/BowenBao

--- aten/src/ATen/core/interned_strings.h
+++ aten/src/ATen/core/interned_strings.h
@@ -239,6 +239,7 @@ namespace c10 {
   _(onnx, LSTM)                      \
   _(onnx, MatMul)                    \
   _(onnx, Min)                       \
+  _(onnx, Max)                       \
   _(onnx, Mul)                       \
   _(onnx, Pow)                       \
   _(onnx, RNN)                       \
