a7001c2a598c0425c60a898dfb46f63352d76062
Xiongfei Wei <isaacwxf23@gmail.com>
2022-11-09 22:17:18 +0000

    Add a default constructor to PythonSymNodeImpl

--- torch/csrc/utils/python_symnode.h
+++ torch/csrc/utils/python_symnode.h
@@ -27,6 +27,7 @@ namespace impl {
 // this is just an adapter so C++ calls can get to the object.
 class PythonSymNodeImpl : public c10::SymNodeImpl {
  public:
+  PythonSymNodeImpl() = default;
   PythonSymNodeImpl(py::object pyobj) : c10::SymNodeImpl() {
     pyobj_ = std::make_shared<c10::SafePyObject>(
         pyobj.release().ptr(), getPyInterpreter());
