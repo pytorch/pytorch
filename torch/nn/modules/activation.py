54de4807e23c9b958d7a94c9dd0007923c2043b9
Olga Gerasimova <olgagerasimova@meta.com>
2022-11-09 06:08:11 -0800

    [AI Accelerators] Update torch.nn.functional multi_head_attention_forward(). Add pastpath (#88241)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88241
    
    The fastpath from torch.nn replicated in the torch.nn.functional multi_head_attention_forward().
    
    Test Plan: buck2 test @//mode/dev-nosan //caffe2/test:jit -- 'jit_multihead_attn_forward \(test_jit\.TestJit\)' test_functional_multi_head_attn_fast_path --print-passing-details
    
    Differential Revision: D40821294
    
    fbshipit-source-id: a174a0f062c3743f1f5b088146afb9d51412eac1

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -1066,60 +1066,28 @@ class MultiheadAttention(Module):
             if _kpm_dtype != torch.bool and not torch.is_floating_point(key_padding_mask):
                 raise AssertionError(
                     "only bool and floating types of key_padding_mask are supported")
-        why_not_fast_path = ''
-        if not is_batched:
-            why_not_fast_path = f"input not batched; expected query.dim() of 3 but got {query.dim()}"
-        elif query is not key or key is not value:
-            # When lifting this restriction, don't forget to either
-            # enforce that the dtypes all match or test cases where
-            # they don't!
-            why_not_fast_path = "non-self attention was used (query, key, and value are not the same Tensor)"
-        elif self.in_proj_bias is not None and query.dtype != self.in_proj_bias.dtype:
-            why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_bias ({self.in_proj_bias.dtype}) don't match"
-        elif self.in_proj_weight is not None and query.dtype != self.in_proj_weight.dtype:
-            # this case will fail anyway, but at least they'll get a useful error message.
-            why_not_fast_path = f"dtypes of query ({query.dtype}) and self.in_proj_weight ({self.in_proj_weight.dtype}) don't match"
-        elif self.training:
-            why_not_fast_path = "training is enabled"
-        elif not self.batch_first:
-            why_not_fast_path = "batch_first was not True"
-        elif self.bias_k is not None:
-            why_not_fast_path = "self.bias_k was not None"
-        elif self.bias_v is not None:
-            why_not_fast_path = "self.bias_v was not None"
-        elif self.dropout:
-            why_not_fast_path = f"dropout was {self.dropout}, required zero"
-        elif self.add_zero_attn:
-            why_not_fast_path = "add_zero_attn was enabled"
-        elif not self._qkv_same_embed_dim:
-            why_not_fast_path = "_qkv_same_embed_dim was not True"
-        elif query.is_nested and (key_padding_mask is not None or attn_mask is not None):
-            why_not_fast_path = "key_padding_mask and attn_mask are not supported with NestedTensor input"
-        elif not query.is_nested and key_padding_mask is not None and attn_mask is not None:
-            why_not_fast_path = "key_padding_mask and attn_mask were both supplied"
-        elif torch.is_autocast_enabled():
-            why_not_fast_path = "autocast is enabled"
-
+        why_not_fast_path = F._can_use_fastpath(query,
+                                                key,
+                                                value,
+                                                self.embed_dim,
+                                                self.num_heads,
+                                                self.in_proj_weight,
+                                                self.in_proj_bias,
+                                                self.bias_k,
+                                                self.bias_v,
+                                                self.add_zero_attn,
+                                                self.dropout,
+                                                self.out_proj.weight,
+                                                self.out_proj.bias,
+                                                self.training,
+                                                key_padding_mask,
+                                                attn_mask)
         if not why_not_fast_path:
-            tensor_args = (
-                query,
-                key,
-                value,
-                self.in_proj_weight,
-                self.in_proj_bias,
-                self.out_proj.weight,
-                self.out_proj.bias,
-            )
-            # We have to use list comprehensions below because TorchScript does not support
-            # generator expressions.
-            if torch.overrides.has_torch_function(tensor_args):
-                why_not_fast_path = "some Tensor argument has_torch_function"
-            elif not all([(x.is_cuda or 'cpu' in str(x.device)) for x in tensor_args]):
-                why_not_fast_path = "some Tensor argument is neither CUDA nor CPU"
-            elif torch.is_grad_enabled() and any([x.requires_grad for x in tensor_args]):
-                why_not_fast_path = ("grad is enabled and at least one of query or the "
-                                     "input/output projection weights or biases requires_grad")
-            if not why_not_fast_path:
+            if not self.batch_first:
+                why_not_fast_path = "batch_first was not True"
+            elif not self._qkv_same_embed_dim:
+                why_not_fast_path = "_qkv_same_embed_dim was not True"
+            else:
                 return torch._native_multi_head_attention(
                     query,
                     key,
@@ -1151,7 +1119,7 @@ class MultiheadAttention(Module):
                 query, key, value = [x.transpose(1, 0) for x in (query, key, value)]
 
         if not self._qkv_same_embed_dim:
-            attn_output, attn_output_weights = F.multi_head_attention_forward(
+            attn_output, attn_output_weights = F._multi_head_attention_forward_impl(
                 query, key, value, self.embed_dim, self.num_heads,
                 self.in_proj_weight, self.in_proj_bias,
                 self.bias_k, self.bias_v, self.add_zero_attn,
@@ -1162,7 +1130,7 @@ class MultiheadAttention(Module):
                 q_proj_weight=self.q_proj_weight, k_proj_weight=self.k_proj_weight,
                 v_proj_weight=self.v_proj_weight, average_attn_weights=average_attn_weights)
         else:
-            attn_output, attn_output_weights = F.multi_head_attention_forward(
+            attn_output, attn_output_weights = F._multi_head_attention_forward_impl(
                 query, key, value, self.embed_dim, self.num_heads,
                 self.in_proj_weight, self.in_proj_bias,
                 self.bias_k, self.bias_v, self.add_zero_attn,
