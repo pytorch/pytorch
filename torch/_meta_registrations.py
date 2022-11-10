13b75e4d8110702bb05e5c2802ea8b4c39032fe0
anjali411 <chourdiaanjali123@gmail.com>
2022-11-10 21:10:54 +0000

    Update base for Update on "Add meta impl for topk"
    [ghstack-poisoned]

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -1762,17 +1762,9 @@ def check_argmax_argmin(name, self, dim):
         )
 
 
-@register_meta(aten.argmax.default)
-def argmax_meta(self, dim=None, keepdim=False):
+@register_meta([aten.argmax.default, aten.argmin.default])
+def argmax_argmin_meta(self, dim=None, keepdim=False):
     check_argmax_argmin("argmax", self, dim)
-    dims = utils.reduction_dims(self.shape, (dim,) if dim is not None else None)
-    shape = _compute_reduction_shape(self, dims, keepdim)
-    return self.new_empty(shape, dtype=torch.int64)
-
-
-@register_meta(aten.argmin.default)
-def argmin_min(self, dim=None, keepdim=False):
-    check_argmax_argmin("argmin", self, dim)
     dims = utils.reduction_dims(self.shape, (dim,) if dim is not None else None)
     shape = _compute_reduction_shape(self, dims, keepdim)
     return self.new_empty(shape, dtype=torch.int64)
