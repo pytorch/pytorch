e114f470167471be6af2601c0368fba91e3b06e0
Wei-Sheng Chin <wschin@outlook.com>
2022-11-10 18:15:52 +0000

    Bypass ORT bug by giving Gemm 3 inputs

--- test/onnx/test_fx_to_onnx_with_onnxruntime.py
+++ test/onnx/test_fx_to_onnx_with_onnxruntime.py
@@ -21,20 +21,20 @@ class TestFxToOnnxWithOnnxRuntime(onnx_test_common._TestONNXRuntime):
         class MNISTModel(nn.Module):
             def __init__(self):
                 super().__init__()
-                self.conv1 = nn.Conv2d(1, 32, 3, 1, bias=False)
-                self.conv2 = nn.Conv2d(32, 64, 3, 1, bias=False)
-                self.fc1 = nn.Linear(9216, 128, bias=False)
-                self.fc2 = nn.Linear(128, 10, bias=False)
+                self.conv1 = nn.Conv2d(1, 32, 3, 1, bias=True)
+                self.conv2 = nn.Conv2d(32, 64, 3, 1, bias=True)
+                self.fc1 = nn.Linear(9216, 128, bias=True)
+                self.fc2 = nn.Linear(128, 10, bias=True)
 
             def forward(self, tensor_x: torch.Tensor):
                 tensor_x = self.conv1(tensor_x)
-                tensor_x = F.sigmoid(tensor_x)
+                tensor_x = torch.sigmoid(tensor_x)
                 tensor_x = self.conv2(tensor_x)
-                tensor_x = F.sigmoid(tensor_x)
+                tensor_x = torch.sigmoid(tensor_x)
                 tensor_x = F.max_pool2d(tensor_x, 2)
                 tensor_x = torch.flatten(tensor_x, 1)
                 tensor_x = self.fc1(tensor_x)
-                tensor_x = F.sigmoid(tensor_x)
+                tensor_x = torch.sigmoid(tensor_x)
                 tensor_x = self.fc2(tensor_x)
                 output = F.log_softmax(tensor_x, dim=1)
                 return output
