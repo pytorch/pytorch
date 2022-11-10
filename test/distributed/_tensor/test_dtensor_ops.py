1986e49601480a736a055e46d2a6f8399a107555
Wanchao Liang <wanchaol@users.noreply.github.com>
2022-11-10 16:55:54 +0000

    Update base for Update on "[dtensor] PART 8: move tensor parallel api and tests to core distributed"
    This PR moves tensor/parallel folder and tests to torch.distributed.
    
    [ghstack-poisoned]

--- test/distributed/_tensor/test_dtensor_ops.py
+++ test/distributed/_tensor/test_dtensor_ops.py
@@ -183,6 +183,7 @@ dtensor_fails = {
     xfail("diag_embed"),
     xfail("diagflat"),
     xfail("diagonal"),
+    xfail("diagonal_copy"),
     xfail("diagonal_scatter"),
     xfail("diff"),
     xfail("dist"),
