e94112da15cabbdf9d7530252da37803e717e400
Will Constable <whc@fb.com>
2022-11-11 21:22:49 +0000

    [FSDP+dynamo]: forward treats parameter-views as params (#88781)
    Dynamo+AotAutograd needs a way to wrap all tensors (whether
    inputs or params/buffers) in FakeTensor wrappers, and
    FSDP's mangling of parameters hides them from this wrapping.
    
    This PR unblocks running hf_bert and hf_T5 with FSDP under dynamo, whether using recursive wrapping around transformer layers or only applying FSDP around the whole model.  Perf/memory validation and possibly optimization is the next step.
    `python benchmarks/dynamo/distributed.py --torchbench_model hf_Bert --fsdp --dynamo aot_eager`
    `python benchmarks/dynamo/distributed.py --torchbench_model hf_Bert --fsdp --dynamo aot_eager --fsdp_wrap`
    `python benchmarks/dynamo/distributed.py --torchbench_model hf_T5 --fsdp --dynamo aot_eager`
    `python benchmarks/dynamo/distributed.py --torchbench_model hf_T5 --fsdp --dynamo aot_eager --fsdp_wrap`
    
    The problem:
    Dynamo (Actually aot_autograd) trips up with FSDP becuase it must
    wrap all input tensors in FakeTensor wrappers, and it only knows
    to wrap graph inputs or named_(parameters, buffers).  FSDP's
    pre_forward hook sets views (which are not nn.param) into the flatparam
    as attrs on the module with the same name as the original param, but
    they will not show up in named_parameters.
    
    - in use_orig_params mode, FSDP still de-registers
      params during pre-forward hook, then re-registers them
      post-forward
    - during forward (between the hooks), the params are setattr'd
      on the module as regular view tensors, not nn.Parameters
    - note: use_orig_params is the recommended way to use FSDP,
      and use_orig_params=False is being deprecated.  So i only consider
      use_orig_params=True for this enablement
    
    The solution:
    - adding them to named_buffers is not possible because it interferes
      with how FSDP's `_apply` works
    - since they are not actual nn.parameters, register_parameter will
      complain about registering them
    - simply seting `module._parameters[name] = view` seems to be a viable
      workaround, despite being hacky, and FSDP code does modify _parameters
      directly already.
    
    Note: Manual checkpointing still isn't working with FSDP+dynamo,
    so that will have to be addressed in a follow up.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88781
    Approved by: https://github.com/ezyang, https://github.com/awgu

--- benchmarks/dynamo/dist_util.py
+++ benchmarks/dynamo/dist_util.py
@@ -20,6 +20,9 @@ try:
 except ImportError:
     from torchbench import setup_torchbench_cwd
 
+from transformers.models.bert.modeling_bert import BertLayer, BertLMPredictionHead
+from transformers.models.t5.modeling_t5 import T5Block
+
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -122,26 +125,25 @@ def fsdp_checkpointing_base(model, blocks):
     )
 
 
-# from transformers.models.t5.modeling_t5 import T5Block
-
 MODEL_FSDP_WRAP = {
-    ToyModel: (MyModule,)
-    # TODO T5: (T5Block,)
+    "toy_model": (MyModule,),
+    "hf_Bert": (BertLayer, BertLMPredictionHead),
+    "hf_T5": (T5Block,),
 }
 
 
-def apply_fsdp(model, use_checkpointing=False, use_wrap_policy=True):
-    blocks = MODEL_FSDP_WRAP[model.__class__]
-
+def apply_fsdp(args, model, use_checkpointing=False, use_wrap_policy=True):
     wrap_policy = None
+    blocks = MODEL_FSDP_WRAP[
+        "toy_model" if model.__class__ is ToyModel else args.torchbench_model
+    ]
     if use_wrap_policy:
         # transformer policy is really a generic policy that wraps modules of specified classes
         wrap_policy = functools.partial(
             transformer_auto_wrap_policy, transformer_layer_cls=blocks
         )
 
-    model = FSDP(model, auto_wrap_policy=wrap_policy)
+    model = FSDP(model, auto_wrap_policy=wrap_policy, use_orig_params=True)
     if use_checkpointing:
         fsdp_checkpointing_base(model, blocks)
-
     return model
