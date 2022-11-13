8cf65d468fe391c0c3e7be2b4a2170a46d04172a
Sherlock Huang <bahuang@fb.com>
2022-11-13 06:06:24 +0000

    Symintify decomps for split and upsample_bilinear; Fix decomp for _softmax_backward_data and native_dropout_backward (#88761)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88761
    Approved by: https://github.com/ezyang

--- test/functorch/test_vmap.py
+++ test/functorch/test_vmap.py
@@ -3238,6 +3238,7 @@ class TestVmapOperatorsOpInfo(TestCase):
         xfail('broadcast_shapes', ''),  # test runner can't handle non-Tensor ops
         xfail('sparse.sampled_addmm'),  # sparse
         xfail('cross'),  # The default value of dim in op is *very* weird. No wonder it doesn't work
+        skip('_softmax_backward_data'),
         skip('linalg.eigh', ''),  # not unique, see test_linalg_eigh for manual test
         skip('to'),  # RuntimeError: required rank 4 tensor to use channels_last format
         # ----------------------------------------------------------------------
@@ -3379,6 +3380,7 @@ class TestVmapOperatorsOpInfo(TestCase):
         xfail('bernoulli', ''),
         xfail('linalg.lu_factor', ''),
         xfail('nn.functional.feature_alpha_dropout', 'with_train'),
+        xfail('native_dropout_backward'),
         xfail('nn.functional.kl_div', ''),
         xfail('multinomial', ''),
         xfail('column_stack', ''),
@@ -3452,6 +3454,7 @@ class TestVmapOperatorsOpInfo(TestCase):
         xfail('equal', ''),
         xfail('linalg.lu', ''),
         skip('linalg.ldl_solve', ''),
+        skip('_softmax_backward_data'),
     }))
     def test_op_has_batch_rule(self, device, dtype, op):
         # needs to be fixed
