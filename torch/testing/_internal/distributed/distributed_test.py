a7283d1e7195f629e7cb527f6e7b1f3233523dee
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2022-11-14 13:14:12 -0800

    refactor tests

--- torch/testing/_internal/distributed/distributed_test.py
+++ torch/testing/_internal/distributed/distributed_test.py
@@ -5323,55 +5323,11 @@ class DistributedTest:
             f"The {BACKEND} backend does not support DistributedDataParallel"
         )
         @skip_if_no_gpu
-        def test_DistributedDataParallel_SyncBatchNorm_Channels_Last_3D(self):
-            group, group_id, rank = self._init_global_test()
-            num_processes = dist.get_world_size()
-            local_bs = 2
-            bs_offset = int(rank * 2)
-            global_bs = int(num_processes * 2)
-
-            model = ONLY_SBN_NET
-            model_gpu = copy.deepcopy(model).cuda(rank)
-            model_DDP = nn.parallel.DistributedDataParallel(
-                model_gpu, device_ids=[rank]
-            )
-
-            memory_format = torch.channels_last_3d
-            input_gpu = (
-                torch.randn(global_bs, 2, 4, 4, 4, dtype=torch.float)
-                .cuda(rank)
-                .to(memory_format=memory_format)
-            )
-            target_gpu = (
-                torch.randn(global_bs, 2, 4, 4, 4, dtype=torch.float)
-                .cuda(rank)
-                .to(memory_format=memory_format)
-            )
-            loss = nn.MSELoss()
-
-            # check two model parameters over 5 iterations
-            self._test_DDP_niter(
-                model_gpu,
-                model_DDP,
-                input_gpu,
-                target_gpu,
-                loss,
-                local_bs,
-                rank,
-                global_bs,
-                True,
-                bs_offset,
-                dist.get_world_size(),
-                memory_format=memory_format,
-            )
-            self._barrier()
-
-        @sandcastle_skip_if(
-            BACKEND not in DistTestCases.backend_feature["ddp"],
-            f"The {BACKEND} backend does not support DistributedDataParallel"
-        )
-        @skip_if_no_gpu
         def test_DistributedDataParallel_SyncBatchNorm_Channels_Last(self):
+            self._test_DistributedDataParallel_SyncBatchNorm_with_memory_format(torch.channels_last)
+            self._test_DistributedDataParallel_SyncBatchNorm_with_memory_format(torch.channels_last_3d)
+
+        def _test_DistributedDataParallel_SyncBatchNorm_with_memory_format(self, memory_format):
             group, group_id, rank = self._init_global_test()
             num_processes = dist.get_world_size()
             local_bs = 2
@@ -5384,14 +5340,15 @@ class DistributedTest:
                 model_gpu, device_ids=[rank]
             )
 
-            memory_format = torch.channels_last
+            shapes = [global_bs, 2, 4, 4] + ([] if memory_format is torch.channels_last else [4])
+
             input_gpu = (
-                torch.randn(global_bs, 2, 4, 4, dtype=torch.float)
+                torch.randn(*shapes, dtype=torch.float)
                 .cuda(rank)
                 .to(memory_format=memory_format)
             )
             target_gpu = (
-                torch.randn(global_bs, 2, 4, 4, dtype=torch.float)
+                torch.randn(*shapes, dtype=torch.float)
                 .cuda(rank)
                 .to(memory_format=memory_format)
             )
