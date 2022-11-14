fe1bca1f67a98129a5f1d68a70f8f8111e4aa00f
mikey dagitses <mikeyd@meta.com>
2022-11-14 22:11:29 +0000

    support running test_mobile_profiler with buck1/buck2 and OSS (#89001)
    Summary:
    Internally we are switching to a new version of buck, but we also must
    keep this working in OSS.
    
    Test Plan: Rely on CI.
    
    Differential Revision: D41270673
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89001
    Approved by: https://github.com/r-barnes, https://github.com/osalpekar, https://github.com/malfet

---
+++ test/cpp/lite_interpreter_runtime/resources.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <experimental/filesystem>
+#include <string>
+
+namespace torch {
+namespace testing {
+
+/// Gets the path to the resource identified by name.
+///
+/// @param name identifies a resource, relative path starting from the
+///             repo root
+inline auto getResourcePath(std::string name)
+    -> std::experimental::filesystem::path {
+  return std::move(name);
+}
+
+} // namespace testing
+} // namespace torch
