9eacfdea9cec84314f91ca771f4beb97067192e7
Yanbo Liang <ybliang8@gmail.com>
2022-11-15 05:43:11 +0000

    Update unit tests

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -1345,7 +1345,7 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         self.assertTrue(result[1] == fn.__code__.co_lnotab)
 
     def test_torch_profiler(self):
-        # wrap torch.profiler.* as ProfilerContextWrapperVariable and do nothing
+        # wrap torch.profiler.* as NullContextVariable and do nothing
         def fn(x):
             y = x**2
             with torch.profiler.profile():
@@ -1365,7 +1365,7 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         self.assertEqual(cnts.frame_count, 2)
 
     def test_autograd_profiler(self):
-        # wrap torch.autograd.profiler.* as ProfilerContextWrapperVariable and do nothing
+        # wrap torch.autograd.profiler.* as NullContextVariable and do nothing
         def fn(x):
             y = x**2
             with torch.autograd.profiler.profile():
