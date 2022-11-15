7321299c4d3eb20515dd8455922e6f1aa7ae19af
andrewor14 <andrewor14@gmail.com>
2022-11-15 13:27:57 -0800

    [Quant][bc-breaking] Remove overwrite_output_observer (#88620)
    Summary: When the BackendConfig was first introduced,
    `overwrite_output_observer` and `overwrite_output_fake_quantize`
    were added to ensure fixed qparams ops like `torch.nn.Sigmoid`
    and `torch.nn.Tanh` used the correct observers and fake quantizes.
    However, this is hacky because the BackendConfig should not set
    the observer constructors themselves, but should instead specify
    only requirements on the observers.
    
    Later, https://github.com/pytorch/pytorch/pull/80184 added the
    correct observers to `get_default_qconfig_mapping` along with
    validation logic that throws an error if incorrect observers
    were specified. With this change, we no longer need to overwrite
    the observers from the BackendConfig, since we expect the user to
    pass in the correct observers for these ops.
    
    This commit removes these overwrite observer settings in the
    BackendConfig. Instead, we represent the observer constraints for
    fixed qparams ops through the existing DTypeWithConstraints
    mechanism. Note that, however, to be consistent with other
    DTypeWithConstraints checks, we no longer throw an error if an
    incorrect observer is specified, but simply ignore the offending
    QConfig and log a warning instead. This is the BC-breaking part
    of the change.
    
    BC-breaking notes:
    
    ```
    from torch.ao.quantization.qconfig import default_qconfig
    from torch.ao.quantization.quantize_fx import prepare_fx
    
    model = ModelWithFixedQParamsOps()
    qconfig_mapping = QConfigMapping().set_global(default_qconfig)
    example_inputs = ...
    prepare_fx(model, qconfig_mapping, example_inputs)
    ```
    
    Before this commit, running the above leads to an exception
    because the wrong observers are used for fixed qparams ops.
    After this commit, the above will only encounter a warning,
    and the fixed qparams ops will not be quantized. In both cases,
    switching to `get_default_qconfig_mapping` will cause the
    fixed qparams ops to be quantized.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers: jerryzh168, vkuzo
    
    Subscribers: jerryzh168, vkuzo
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88620
    Approved by: https://github.com/jerryzh168

--- torch/ao/quantization/fx/quantization_patterns.py
+++ torch/ao/quantization/fx/quantization_patterns.py
@@ -6,13 +6,10 @@ from torch.fx.graph import (
 from .utils import (
     all_node_args_have_no_tensors,
 )
-from torch.ao.quantization.utils import (
-    Pattern,
-    NodePattern,
-)
+from torch.ao.quantization.utils import NodePattern
 
 from abc import ABC
-from typing import Any, Callable, Dict, Optional
+from typing import Callable, Dict
 
 __all__ = [
     "QuantizeHandler",
@@ -97,19 +94,6 @@ class QuantizeHandler(ABC):
         observer_0 - avgpool2d - observer_0 (same observer instance as input)
         """
         return False
-
-    def get_activation_ctr(
-        self,
-        qconfig: Any,
-        pattern: Pattern,
-        is_training: bool,
-    ) -> Optional[Callable]:
-        """
-        Returns the constructor for the activation observer which should be
-        used for the pattern matched to this handler. Some handlers override
-        this to a different value than what is specified in the qconfig.
-        """
-        return qconfig.activation
 
     def is_custom_module(self):
         return self.is_custom_module_
