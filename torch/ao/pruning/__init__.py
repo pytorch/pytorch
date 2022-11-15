f0027c25a916b8f978f692e63f8bcf09c38c3dbe
Jesse Cai <jcjessecai@gmail.com>
2022-11-15 10:46:53 -0800

    Update on "Align BaseStructuredPruner with existing pruning flow"
    Summary:
    
    This PR aligns the "eager" mode of the structured pruning flow with the existing unstructured pruning flow.
    
    The base pruner has been moved from and has been renamed from BasePruner to BaseStructuredPruner
    `torch/ao/pruning/_experimental/pruner/base_pruner.py -> /torch/ao/pruning/_experimental/pruner/base_structured_pruner.py`
    
    Support for pruning batchnorm modules in the config have been removed, so now the structured pruning code can use more of the BaseSparsifier logic and we don't need to override as many functions.
    
    Since we aim to only support a single flow, we have only updated ZeroesParametrizations (FakeStructuredSparsity) and BiasHook.
    The parameterizations have also been rewritten to use a bool mask tensor for keeping track of pruned rows, instead of using sets before.
    This better aligns structured and unstructured sparsity.
    
    The BaseStructuredSparsifier tests have also been updated to reflect the above changes.
    
    We will migrate the structured pruning code out of this folder in a later PR.
    
    Test Plan:
    ```
    python test/test_ao_sparsity -- TestBaseStructuredPruner
    ```
    
    Reviewers:
    z-a-f vkuzo
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/pruning/__init__.py
+++ torch/ao/pruning/__init__.py
@@ -4,7 +4,6 @@ from ._mappings import get_static_sparse_quantized_mapping
 
 # Sparsifier
 from .sparsifier.base_sparsifier import BaseSparsifier
-from .sparsifier.base_structured_sparsifier import BaseStructuredSparsifier
 from .sparsifier.weight_norm_sparsifier import WeightNormSparsifier
 from .sparsifier.nearly_diagonal_sparsifier import NearlyDiagonalSparsifier
 
@@ -15,8 +14,6 @@ from .scheduler.cubic_scheduler import CubicSL
 
 # Parametrizations
 from .sparsifier.utils import FakeSparsity
-from .sparsifier.utils import FakeStructuredSparsity
-from .sparsifier.utils import BiasHook
 from .sparsifier.utils import module_to_fqn
 from .sparsifier.utils import fqn_to_module
 from .sparsifier.utils import get_arg_info_from_tensor_fqn
