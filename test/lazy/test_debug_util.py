c4379f04d34a4d81bb485595da56b29d43559bc3
Jiewen Tan <jwtan@google.com>
2022-11-16 18:43:58 +0000

    Make the test more robust

--- test/lazy/test_debug_util.py
+++ test/lazy/test_debug_util.py
@@ -25,14 +25,14 @@ class DebugUtilTest(TestCase):
         # graph. However, we cannot save the whole stack for comparison given
         # it depends on a lot of things.
         partial_graph = (r"Python Stacktrace:.*"
-                         r"mark_step \(/home/jwtan/work/pytorch/torch/_lazy/__init__.py:[0-9]+\).*"
-                         r"_run_linear \(/home/jwtan/work/pytorch/test/lazy/test_debug_util.py:[0-9]+\).*"
-                         r"test_get_python_frames \(/home/jwtan/work/pytorch/test/lazy/test_debug_util.py:[0-9]+\)")
+                         r"mark_step \(.*/torch/_lazy/__init__.py:[0-9]+\).*"
+                         r"_run_linear \(.*/test/lazy/test_debug_util.py:[0-9]+\).*"
+                         r"test_get_python_frames \(.*/test/lazy/test_debug_util.py:[0-9]+\)")
 
-        with tempfile.NamedTemporaryFile() as graph_file:
+        with tempfile.NamedTemporaryFile(mode="r+", encoding="utf-8") as graph_file:
             os.environ["LTC_SAVE_TENSORS_FILE"] = graph_file.name
             self._run_linear()
-            file = str(graph_file.read())
+            file = graph_file.read()
             self.assertNotEqual(re.search(partial_graph, file, re.DOTALL), None)
 
 
