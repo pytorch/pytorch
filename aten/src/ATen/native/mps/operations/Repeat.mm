1a7feebfd7663ab2c36f5c60a7d031e0cdc73c1d
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-11-17 18:52:16 -0500

    Implement native Metal kernel

--- aten/src/ATen/native/mps/operations/Repeat.mm
+++ aten/src/ATen/native/mps/operations/Repeat.mm
@@ -13,9 +13,83 @@
 #include <torch/library.h>
 #include <c10/util/irange.h>
 
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/repeat_interleave_native.h>
+#endif
+
 #ifdef __OBJC__
 #include <MetalPerformanceShaders/MetalPerformanceShaders.h>
 #endif
+
+template <typename index_t>
+static void compute_mps_kernel(index_t *repeat_ptr,
+    	int64_t *cumsum_ptr,
+    	index_t *result_ptr,
+    	int64_t size,
+    	int64_t result_size)
+    {
+    	using namespace mps;
+
+    	id<MTLBuffer> inputBuffer = getMTLBufferStorage(inputTensor);
+    	id<MTLBuffer> outputBuffer = getMTLBufferStorage(outputTensor);
+    	MPSStream *mpsStream = getCurrentMPSStream();
+    	id<MTLDevice> device = MPSDevice::getInstance()->device();
+
+    	dispatch_sync(mpsStream->queue(), ^ ()
+    		{         
+        @autoreleasepool {
+
+    				NSError *error = nil;
+    				TORCH_CHECK(result_size == cumsum_ptr[size - 1], "allocated size does not match required size");
+    				int64_t idx = blockIdx.x *blockDim.x + threadIdx.x;
+    				int64_t stride = (blockDim.x *gridDim.x) / at::mps::C10_WARP_SIZE;
+    				int warp_id = idx / at::mps::C10_WARP_SIZE;
+    				int tid_in_warp = idx % at::mps::C10_WARP_SIZE;
+    				for (int64_t i = warp_id; i < size; i += stride)
+    				{
+    					int64_t end = cumsum_ptr[i];
+    					index_t repeat = repeat_ptr[i];
+    					TORCH_CHECK(repeat >= 0, "repeats can not be negative"; int64_t start = end - repeat);
+    						for (int64_t j = start + tid_in_warp; j < end; j += at::mps::C10_WARP_SIZE)
+    						{
+    							result_ptr[j] = i;
+    						}
+    					}
+
+    				int64_t block = 512;
+    				int64_t warps_per_block = block / at::mps::C10_WARP_SIZE();
+
+    				NSUInteger threadGroupSize = ((size + warps_per_block - 1) / warps_per_block, 2048);
+    				if (threadGroupSize > 2048)
+    				{
+    					threadGroupSize = 2048;
+    				}
+
+    				MTLSize grid = MTLSizeMake(threadGroupSize, 1, 1);
+
+    			[computeEncoder dispatchThreads: gridSize threadsPerThreadgroup: grid];
+
+    			[computeEncoder endEncoding];
+    				mpsStream->commit(true);
+
+        }        
+      });
+      
+    return true;
+  }
+
+template <typename index_t>
+void compute_mps(index_t* repeat_ptr,
+    int64_t* cumsum_ptr,
+    index_t* result_ptr,
+    int64_t size,
+    int64_t result_size) {
+
+  TORCH_CHECK(compute_mps_kernel(
+      repeat_ptr, cumsum_ptr, result_ptr, size, result_size), "Failed compute_mps");
+}
 
 
 namespace at {
@@ -132,73 +206,12 @@ Tensor repeat_mps(const Tensor& self, IntArrayRef repeats) {
 }
 
 Tensor repeat_interleave_mps(const Tensor& repeats,c10::optional<int64_t> output_size) {
-
   Tensor output;
-  using namespace mps;
-  MPSStream* stream = getCurrentMPSStream();
-
- struct CachedGraph : public MPSCachedGraph
-  {
-    CachedGraph(MPSGraph *graph) : MPSCachedGraph(graph) {}
-    MPSGraphTensor* inputTensor_ = nil;
-    MPSGraphTensor* outputTensor_ = nil;
-  };
-
-  MPSGraphCache* cache_ = MPSGraphCache::getInstance();
-
-  NSNumber *repeats_shape;
-
-  if (output_size.has_value()) {
-    repeats_shape = [NSNumber numberWithInteger:output_size.value()];
-  } else {
-    Tensor cumsum = repeats.cumsum(0);
-    repeats_shape = [NSNumber numberWithInteger:cumsum[-1].item<int64_t>()];
-    TORCH_CHECK(
-        (repeats >= 0).all().item<uint8_t>(), "repeats can not be negative");
-  }
-
-
-  @autoreleasepool {
-    // A key is used to identify the MPSGraph which was created once, and can be reused if the parameters, data types etc match the earlier created MPSGraph
-    string key = "repeat_interleave_mps:" + getTensorsStringKey({repeats})+ ":" + repeats_shape.stringValue;
-
-    CachedGraph* cachedGraph = static_cast<CachedGraph *>(cache_->LookUp(key));
-    if(!cachedGraph) {
-      MPSCachedGraph *tmpCachedGraph = cache_->CreateCachedGraph(key, ^ MPSCachedGraph * () {
-        CachedGraph *newCachedGraph = nil;
-
-        @autoreleasepool {
-          MPSGraph* mpsGraph = make_mps_graph();
-          newCachedGraph = new CachedGraph(mpsGraph);
-
-
-          MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(repeats.scalar_type()), getMPSShape(repeats));
-          MPSGraphTensor* outputTensor = [mpsGraph tileTensor:inputTensor
-                                               withMultiplier:repeats_shape
-                                                         name:nil];
-
-          newCachedGraph->inputTensor_ = inputTensor;
-          newCachedGraph->outputTensor_ = outputTensor;
-        }
-        return newCachedGraph;
+  AT_DISPATCH_INDEX_TYPES(
+      repeat.scalar_type(), "repeat_interleave_cuda", [&]() {
+        output = repeat_interleave_common<index_t, compute_mps<index_t>>(
+            repeat, output_size);
       });
-
-      cachedGraph = static_cast<CachedGraph *>(tmpCachedGraph);
-    }
-
-    Placeholder selfPlaceholder = Placeholder(cachedGraph->inputTensor_, repeats, getMPSShape(repeats));
-    Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, output);
-
-    NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = @{
-      selfPlaceholder.getMPSGraphTensor() : selfPlaceholder.getMPSGraphTensorData()
-    };
-    NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* output = @{
-      outputPlaceholder.getMPSGraphTensor() : outputPlaceholder.getMPSGraphTensorData()
-    };
-
-    runMPSGraph(stream, cachedGraph->graph(), feeds, output);
-  }
-
   return output;
 }
 
