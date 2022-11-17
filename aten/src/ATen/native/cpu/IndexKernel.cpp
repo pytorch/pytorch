931e187320f50da06c9aefbcd1e95425ab0e9335
vfdev-5 <vfdev.5@gmail.com>
2022-11-17 22:48:24 +0000

    Use const stride for stride0 and stride1

--- aten/src/ATen/native/cpu/IndexKernel.cpp
+++ aten/src/ATen/native/cpu/IndexKernel.cpp
@@ -470,8 +470,8 @@ void cpu_hflip_vec(at::TensorIterator& iter) {
 
     using Vec = Vectorized<scalar_t>;
 
-    TORCH_INTERNAL_ASSERT(sizeof(scalar_t) == -strides[0]);
-    auto stride0 = sizeof(scalar_t);
+    constexpr auto stride = sizeof(scalar_t);
+    TORCH_INTERNAL_ASSERT(stride == -strides[0] && stride == strides[1]);
 
     for (const auto j C10_UNUSED : c10::irange(size1)) {
 
@@ -490,25 +490,26 @@ void cpu_hflip_vec(at::TensorIterator& iter) {
       int64_t offset = (j * n + (n - i - Vec::size())) % 32;
       offset = (offset >= n) ? n : offset;
       for (; i < offset; i++) {
-        scalar_t* out_ptr = (scalar_t*)(data[0] - i * stride0);
-        *out_ptr = *(scalar_t *)(data[1] + i * strides[1]);
+        scalar_t* out_ptr = (scalar_t*)(data[0] - i * stride);
+        *out_ptr = *(scalar_t *)(data[1] + i * stride);
       }
+      // Empirically found that it is faster to process 3 data items together vs 2 or 4
       for (; i <= n - 3 * Vec::size(); i += 3 * Vec::size()) {
-        auto out1 = Vec::loadu(data[1] + i * strides[1]);
-        auto out2 = Vec::loadu(data[1] + (i + Vec::size()) * strides[1]);
-        auto out3 = Vec::loadu(data[1] + (i + 2 * Vec::size()) * strides[1]);
+        auto out1 = Vec::loadu(data[1] + i * stride);
+        auto out2 = Vec::loadu(data[1] + (i + Vec::size()) * stride);
+        auto out3 = Vec::loadu(data[1] + (i + 2 * Vec::size()) * stride);
         // flip the vector: 1234 -> 4321
         out1 = flip(out1);
         out2 = flip(out2);
         out3 = flip(out3);
-        out1.store(data[0] - (i + Vec::size() - 1) * stride0);
-        out2.store(data[0] - (i + 2 * Vec::size() - 1) * stride0);
-        out3.store(data[0] - (i + 3 * Vec::size() - 1) * stride0);
+        out1.store(data[0] - (i + Vec::size() - 1) * stride);
+        out2.store(data[0] - (i + 2 * Vec::size() - 1) * stride);
+        out3.store(data[0] - (i + 3 * Vec::size() - 1) * stride);
       }
       if (i < n) {
         for (; i < n; i++) {
-          scalar_t* out_ptr = (scalar_t*)(data[0] - i * stride0);
-          *out_ptr = *(scalar_t *)(data[1] + i * strides[1]);
+          scalar_t* out_ptr = (scalar_t*)(data[0] - i * stride);
+          *out_ptr = *(scalar_t *)(data[1] + i * stride);
         }
       }
 
