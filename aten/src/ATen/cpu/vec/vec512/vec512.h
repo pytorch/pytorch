708663fa31a33012f80c5872f126fd453b893eb1
vfdev-5 <vfdev.5@gmail.com>
2022-11-18 10:31:07 +0000

    Added vec flip for int16

--- aten/src/ATen/cpu/vec/vec512/vec512.h
+++ aten/src/ATen/cpu/vec/vec512/vec512.h
@@ -219,6 +219,15 @@ inline Vectorized<int32_t> flip(const Vectorized<int32_t> & v) {
 }
 
 template<>
+inline Vectorized<int16_t> flip(const Vectorized<int16_t> & v) {
+  const __m512i mask = _mm512_set_epi16(
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+      16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+  );
+  return _mm512_permutexvar_epi16(mask, v);
+}
+
+template<>
 inline Vectorized<int8_t> flip(const Vectorized<int8_t> & v) {
   const __m512i mask1 = _mm512_set_epi8(
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
