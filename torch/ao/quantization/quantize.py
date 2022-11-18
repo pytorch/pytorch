40a13cbb505c1241997df5673204d425dee99b2e
HDCharles <charlesdavidhernandez@gmail.com>
2022-11-17 19:20:22 -0800

    [ao] maintain BC for is_activation_post_process (#89260)
    Summary: tests are failing due to code packaged with trained models calling now defunct function names (is_activation_post_process).
    
    this diff maintains BC temporarily until the cached code can be refreshed
    
    Test Plan: no functional change
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89260
    Approved by: https://github.com/jerryzh168

--- torch/ao/quantization/quantize.py
+++ torch/ao/quantization/quantize.py
@@ -27,7 +27,12 @@ from torch.ao.quantization.qconfig import (
     float_qparams_weight_only_qconfig_4bit,
     _activation_is_memoryless)
 from torch.nn.utils.parametrize import type_before_parametrizations
-from torch.ao.quantization.observer import _is_activation_post_process
+
+from torch.ao.quantization.observer import (  # noqa: F401
+    _is_activation_post_process,
+    _is_activation_post_process as is_activation_post_process,
+    # TODO remove this once problems from name change are resolved
+)
 
 __all__ = [
     "get_default_custom_config_dict",
