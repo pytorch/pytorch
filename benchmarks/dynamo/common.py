c10925cd382843a6f1b04f9f4beb32016ebb62c3
Edward Z. Yang <ezyang@fb.com>
2022-11-19 03:51:54 +0000

    [QOL] make explain a little easier to parse
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -1336,14 +1336,13 @@ class BenchmarkRunner:
             status = self.check_accuracy(
                 name, model, example_inputs, optimize_ctx, experiment
             )
-            print(f"{prefix} {status}")
         elif self.args.performance:
             status = self.run_performance_test(
                 name, model, example_inputs, optimize_ctx, experiment
             )
-            print(f"{prefix} {status}")
         if explain:
             print(torch._dynamo.explain(model, *example_inputs)[0])
+        print(f"{prefix} {status}")
 
 
 def help(fn):
