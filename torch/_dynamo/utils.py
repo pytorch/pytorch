808bdbab89e875abbbe9652bde675b4402eed532
Michael Voznesensky <voznesenskym@gmail.com>
2022-11-19 07:16:29 +0000

    Fix try/except flow where DataDependentOutputException is getting wrapped in a RuntimeError (#89314)
    Repro fixed
    
    ```
    def fn(a):
        return a.repeat_interleave(14, dim=0).repeat_interleave(14, dim=1)
    
    x = torch.ones(14, 14).to(dtype=torch.int64)
    opt_fn = torch._dynamo.optimize("eager")(fn)
    opt_fn(x)
    ```
    
    Fixes [#1886](https://github.com/pytorch/torchdynamo/issues/1886)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89314
    Approved by: https://github.com/anijain2305, https://github.com/eellison

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1055,13 +1055,20 @@ def get_fake_value(node, tx):
     except Unsupported:
         raise
     except RuntimeError as e:
-        if isinstance(e, torch._subclasses.fake_tensor.DataDependentOutputException):
+        cause = e
+        if e.__cause__ is not None:
+            cause = e.__cause__
+        if isinstance(
+            cause, torch._subclasses.fake_tensor.DataDependentOutputException
+        ):
             if config.capture_scalar_outputs and node.target == "item":
                 return torch.zeros(size=(), dtype=args[0].dtype).item()
             else:
-                unimplemented(f"data dependent operator: {e.func}")
-        elif isinstance(e, torch._subclasses.fake_tensor.DynamicOutputShapeException):
-            unimplemented(f"dynamic shape operator: {e.func}")
+                unimplemented(f"data dependent operator: {cause.func}")
+        elif isinstance(
+            cause, torch._subclasses.fake_tensor.DynamicOutputShapeException
+        ):
+            unimplemented(f"dynamic shape operator: {cause.func}")
         raise TorchRuntimeError() from e
 
 
