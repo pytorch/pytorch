1f69ab88e4ad5b1838d12b46068fc9f41a8f5fc9
Michael Voznesensky <voznesenskym@gmail.com>
2022-11-19 17:49:39 +0000

    RM expectedFailure UnspecReproTests.test_batch_norm_act_unspec (#89340)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89340
    Approved by: https://github.com/bertmaher

--- test/dynamo/test_unspec.py
+++ test/dynamo/test_unspec.py
@@ -50,8 +50,6 @@ def make_unspec_cls(cls):
 UnspecReproTests = make_unspec_cls(test_repros.ReproTests)
 UnspecNNModuleTests = make_unspec_cls(test_modules.NNModuleTests)
 
-unittest.expectedFailure(UnspecReproTests.test_batch_norm_act_unspec)
-
 
 @patch.object(torch._dynamo.config, "specialize_int_float", False)
 class UnspecTests(torch._dynamo.test_case.TestCase):
