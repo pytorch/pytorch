24ce829addf03b59d3309a69351123fdda27c272
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2022-11-19 10:49:52 -0500

    Fix compile issues

--- aten/src/ATen/native/mps/operations/Repeat.mm
+++ aten/src/ATen/native/mps/operations/Repeat.mm
@@ -12,7 +12,7 @@
 #include <ATen/native/mps/OperationUtils.h>
 #include <torch/library.h>
 #include <c10/util/irange.h>
-#include <ATen/native/RepeatKernel.h>
+#include <ATen/native/ComputeKernels.h>
 
 #ifndef AT_PER_OPERATOR_HEADERS
 #include <ATen/NativeFunctions.h>
