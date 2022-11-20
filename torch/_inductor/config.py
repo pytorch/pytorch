a358fdd6f9f399cb78aab3a78b1f18d42979f428
Jiong Gong <jiong.gong@intel.com>
2022-11-20 17:49:21 +0800

    get len

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -59,7 +59,7 @@ unroll_reductions_threshold = 8
 
 comment_origin = False
 
-compile_threads = min(32, os.sched_getaffinity(0) if hasattr(os, "sched_getaffinity")
+compile_threads = min(32, len(os.sched_getaffinity(0)) if hasattr(os, "sched_getaffinity")
                       else os.cpu_count()) if sys.platform != "win32" else 1
 
 # If kernel is fused, the name is generated from the origin node op names
