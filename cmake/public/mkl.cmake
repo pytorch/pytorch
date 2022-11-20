d2796693d3a533e9992a996950f4abf567291c5c
Edward Z. Yang <ezyang@fb.com>
2022-11-20 00:41:51 -0500

    Directly link against MKL::MKL target, instead of doing it from scratch
    See also https://github.com/pytorch/pytorch/issues/73008
    
    Also fixes https://github.com/pytorch/audio/issues/2784
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

--- cmake/public/mkl.cmake
+++ cmake/public/mkl.cmake
@@ -4,9 +4,4 @@ if(NOT TARGET caffe2::mkl)
   add_library(caffe2::mkl INTERFACE IMPORTED)
 endif()
 
-set_property(
-  TARGET caffe2::mkl PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-  ${MKL_INCLUDE_DIR})
-set_property(
-  TARGET caffe2::mkl PROPERTY INTERFACE_LINK_LIBRARIES
-  ${MKL_LIBRARIES})
+target_link_libraries(caffe2::mkl INTERFACE MKL::MKL)
