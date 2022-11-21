821ba6b51beb1844f264fd57e1eccecb446e4870
Wanchao Liang <wanchaol@users.noreply.github.com>
2022-11-21 19:19:29 +0000

    [4/n] Thread PG: add reduce_scatter to threaded pg (#89442)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89442
    Approved by: https://github.com/yhcharles, https://github.com/fduwjj

--- test/distributed/test_multi_threaded_pg.py
+++ test/distributed/test_multi_threaded_pg.py
@@ -55,6 +55,14 @@ class TestCollectivesWithBaseClass(MultiThreadedTestCase):
         dist.scatter(output_tensor, scatter_list)
         self.assertEqual(output_tensor, torch.ones(3, 3) * dist.get_rank())
 
+    def test_reduce_scatter(self):
+        to_reduce_scatter = [torch.ones(3, 3) * rank for rank in range(self.world_size)]
+        output_tensor = torch.empty(3, 3)
+
+        dist.reduce_scatter(output_tensor, to_reduce_scatter)
+        expected_tensor = torch.ones(3, 3) * dist.get_rank() * self.world_size
+        self.assertEqual(output_tensor, expected_tensor)
+
     def test_broadcast_object_list(self):
         val = 99 if dist.get_rank() == 0 else None
         object_list = [val] * dist.get_world_size()
