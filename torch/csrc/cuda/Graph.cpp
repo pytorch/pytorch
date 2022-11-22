f08541299a08334a9e1a648b78ab6602a92cd105
Eddie Yan <eddiey@nvidia.com>
2022-11-22 19:33:08 +0000

    lint

--- torch/csrc/cuda/Graph.cpp
+++ torch/csrc/cuda/Graph.cpp
@@ -49,14 +49,14 @@ void THCPGraph_init(PyObject* module) {
           "pool",
           torch::wrap_pybind_function(&at::cuda::CUDAGraph::pool),
           py::call_guard<py::gil_scoped_release>());
-      .def(
-         "debug_dump",
-         torch::wrap_pybind_function(&::at::cuda::CUDAGraph::debug_dump),
-         py::call_guard<py::gil_scoped_release>());
-      .def(
-          "enable_debug_mode",
-          torch::wrap_pybind_function(&::at::cuda::CUDAGraph::enable_debug_mode),
-          py::call_guard<py::gil_scoped_release>())
+  .def(
+      "debug_dump",
+      torch::wrap_pybind_function(&::at::cuda::CUDAGraph::debug_dump),
+      py::call_guard<py::gil_scoped_release>());
+  .def(
+      "enable_debug_mode",
+      torch::wrap_pybind_function(&::at::cuda::CUDAGraph::enable_debug_mode),
+      py::call_guard<py::gil_scoped_release>())
       .def(
           "debug_dump",
           torch::wrap_pybind_function(&::at::cuda::CUDAGraph::debug_dump),
