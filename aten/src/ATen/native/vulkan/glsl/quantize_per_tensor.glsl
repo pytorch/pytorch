54a70b090e60ec334fac0724e7637875232a1b33
Manuel Candales <mcandales@meta.com>
2022-11-22 11:05:58 +0000

    [Vulkan][TCC] Fix quantized shaders (#89456)
    Summary: Fix rounding issue in quantized shaders
    
    Test Plan:
    On Mac
    ```
    cd ~/fbsource
    buck1 run -c pt.vulkan_full_precision=1 //xplat/caffe2:pt_vulkan_quantized_api_test_binAppleMac\#macosx-arm64
    ```
    
    On Android
    ```
    cd ~/fbsource
    buck1 build -c ndk.custom_libcxx=false -c pt.enable_qpl=0 -c pt.vulkan_full_precision=1 //xplat/caffe2:pt_vulkan_quantized_api_test_binAndroid\#android-arm64 --show-output
    adb push buck-out/gen/xplat/caffe2/pt_vulkan_quantized_api_test_binAndroid\#android-arm64 /data/local/tmp/vulkan_quantized_api_test
    adb shell "/data/local/tmp/vulkan_quantized_api_test"
    ```
    
    Reviewed By: salilsdesai
    
    Differential Revision: D41047095
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89456
    Approved by: https://github.com/kirklandsign, https://github.com/digantdesai

--- aten/src/ATen/native/vulkan/glsl/quantize_per_tensor.glsl
+++ aten/src/ATen/native/vulkan/glsl/quantize_per_tensor.glsl
@@ -19,11 +19,13 @@ layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
   if (all(lessThan(pos, uBlock.size.xyz))) {
-    vec4 ret = texelFetch(uInput, pos, 0) / uBlock.scale.x + uBlock.zero_point.x;
-    uvec4 texel = uvec4(int(ret.x), int(ret.y), int(ret.z), int(ret.w));
+    vec4 q_res = roundEven(texelFetch(uInput, pos, 0) / uBlock.scale.x) + uBlock.zero_point.x;
+
+    uvec4 ret = uvec4(q_res);
+
     imageStore(
         uOutput,
         pos,
-        texel);
+        ret);
   }
 }
