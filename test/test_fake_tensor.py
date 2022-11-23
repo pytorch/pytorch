ffc86413a7b5bf32cfe93f12b7ed5333a7fb538a
Elias Ellison <elias.ellison@gmail.com>
2022-11-23 00:41:20 +0000

    Update on "Fix Upsample Decomp Striding For Small Channels"
    Fix for https://github.com/pytorch/torchdynamo/issues/623. 
    
    
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- test/test_fake_tensor.py
+++ test/test_fake_tensor.py
@@ -208,9 +208,10 @@ class FakeTensorTest(TestCase):
         out = []
         mode = FakeTensorMode()
         for i, context in enumerate([contextlib.nullcontext, lambda: mode]):
-            arg0_1 = torch.empty_strided((3, 427, 640), (1, 1920, 3), dtype=torch.float16, device='cuda')
-            unsqueeze = torch.ops.aten.unsqueeze.default(arg0_1, 0)
-            out.append(torch.ops.aten.upsample_bilinear2d.default(unsqueeze, [800, 1199], False))
+            with context():
+                arg0_1 = torch.empty_strided((3, 427, 640), (1, 1920, 3), dtype=torch.float32, device='cuda')
+                unsqueeze = torch.ops.aten.unsqueeze.default(arg0_1, 0)
+                out.append(torch.ops.aten.upsample_bilinear2d.default(unsqueeze, [800, 1199], False))
 
         self.assertTrue(out[1].is_contiguous())
         self.checkMetaProps(out[0], out[1])
