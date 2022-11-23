f03e6672fb6a694d6f03980e3f34d8181c7cc663
Iris <wz337@cornell.edu>
2022-11-23 19:39:01 +0000

    [Checkpoint][2D] Minor update for dedup_tensors.py (#89542)
    Rename variables for better readability.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89542
    Approved by: https://github.com/H-Huang

--- torch/distributed/checkpoint/dedup_tensors.py
+++ torch/distributed/checkpoint/dedup_tensors.py
@@ -30,8 +30,8 @@ def dedup_tensors(all_plans: List[SavePlan]) -> List[SavePlan]:
     all_plans = list(all_plans)
     key_to_plan: Dict[MetadataIndex, List[int]] = {}
     for plan_idx, plan in enumerate(all_plans):
-        for wi in plan.items:
-            key_to_plan.setdefault(wi.index, []).append(plan_idx)
+        for write_item in plan.items:
+            key_to_plan.setdefault(write_item.index, []).append(plan_idx)
 
     replicated_items = {k: v for k, v in key_to_plan.items() if len(v) > 1}
 
@@ -47,7 +47,9 @@ def dedup_tensors(all_plans: List[SavePlan]) -> List[SavePlan]:
         key_set = set(keys)
         # rewrite items and remove elements
         new_items = [
-            wi for wi in all_plans[plan_idx].items if wi.index not in key_set
+            write_item
+            for write_item in all_plans[plan_idx].items
+            if write_item.index not in key_set
         ]
         all_plans[plan_idx] = dataclasses.replace(
             all_plans[plan_idx], items=new_items
