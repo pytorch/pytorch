c12b89fee371016177f69852ab3db8f5b149c54c
Anupam Bhatnagar <anupamb@meta.com>
2022-11-22 19:32:04 -0800

    clean the test code

--- test/nn/test_optimizer_hooks.py
+++ test/nn/test_optimizer_hooks.py
@@ -1,87 +1,48 @@
-from typing import Any, Tuple, List, Dict
-from collections import defaultdict
-
-from sklearn.datasets import make_blobs
 import torch
 import torch.nn as nn
-from torch.optim import SGD
+import torch.optim as optim
 
+class Xor(nn.Module):
+    def __init__(self):
+        super(Xor, self).__init__()
+        self.fc1 = nn.Linear(2, 3, True)
+        self.fc2 = nn.Linear(3, 1, True)
 
-# Test: feed forward network
-class FeedForward(torch.nn.Module):
-    def __init__(self, input_size: int, hidden_size: int):
-        torch.manual_seed(7)
-        super(FeedForward, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(self.hidden_size, 1)
-        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.sigmoid(x)
+        x = self.fc2(x)
+        return x
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:  # type: ignore
-        out = self.fc1(x)
-        out = self.relu1(out)
-        out = self.fc2(out)
-        out = self.relu2(out)
-        out = self.fc3(out)
-        out = self.sigmoid(out)
-        return out
+def test_xor():
+    xor = Xor()
 
+    data = []
+    def pre_hook():
+        data.append(1)
 
-def load_data() -> Tuple[Any, Any]:
-    torch.manual_seed(11)
-    x_train, y_train = make_blobs(n_samples=40, n_features=2, cluster_std=1.5, shuffle=True, random_state=10)
-    x_train = torch.Tensor.float(x_train)
-    y_train = torch.Tensor.float(blob_label(y_train, 0, [0]))
-    y_train = torch.Tensor.float(blob_label(y_train, 1, [1, 2, 3]))
+    def post_hook():
+        data.pop(0)
 
-    data = (x_train, y_train)
-    return data
+    inputs = torch.Tensor([[0,0],[0,1], [1,0], [1,1]])
+    targets = torch.Tensor([0,1,1,0]).view(-1,1)
 
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(xor.parameters(), lr=0.01)
+    optimizer.register_step_post_hook(decrement)
+    optimizer.register_step_pre_hook(increment)
 
-def train_linear_model(model: FeedForward) -> Dict:
-    x, y = 2, 16
-    
-    def increment():
-        global x
-        x += 4
+    xor.train()
 
-    def decrement():
-        global y
-        y -= 3
-    
-    criterion = nn.CrossEntropyLoss()
-    optimizer = SGD(model.parameters(), lr=0.001)
-    pre_hook_handle = optimizer.register_step_pre_hook(increment)
-    post_hook_handle = optimizer.register_step_post_hook(decrement)
+    EPOCHS_TO_TRAIN = 5
+    for _ in range(0, EPOCHS_TO_TRAIN):
+        for input, target in zip(inputs, targets):
+            optimizer.zero_grad()   # zero the gradient buffers
+            output = xor(input)
+            loss = criterion(output, target)
+            loss.backward()
+            # pre_hook()
+            optimizer.step()    # Does the update
+            # post_hook()
 
-    x_train, y_train = load_data()
-    num_epochs = 2
-    model.train()
-    
-    assert x == 2
-    assert y == 16 
-    
-    state = defaultdict(List)
-    state[-1] = [2, 16]
-    
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        y_pred = model(x_train)
-   
-        loss = criterion(y_pred.squeeze(), y_train)
-        loss.backward()
-        optimizer.step()
-
-        state[epoch] = [x,y]
-
-    return state
-
-def test_linear_model() -> None:
-    model1 = FeedForward(2, 10)
-    vanilla_model = train_linear_model(model1)
-
-    assert vanilla_model[1][0] == vanilla_model[1][1]
+    assert len(data) == 0
