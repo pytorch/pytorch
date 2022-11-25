942271be73fd2fcc999701d9dd18c30a191f3c86
Shogo Hida <shogo.hida@gmail.com>
2022-11-26 00:01:51 +0900

    Put c10::complex<double>
    Signed-off-by: Shogo Hida <shogo.hida@gmail.com>

--- aten/src/ATen/native/cuda/ActivationSiluKernel.cu
+++ aten/src/ATen/native/cuda/ActivationSiluKernel.cu
@@ -29,7 +29,7 @@ void silu_kernel(TensorIteratorBase& iter) {
         gpu_kernel(iter, [] GPU_LAMBDA(scalar_t x) -> scalar_t {
           using opmath_t = at::opmath_type<scalar_t>;
           const opmath_t x_acc = static_cast<opmath_t>(x);
-          return x_acc / (opmath_t(1) + c10::cuda::compat::exp(-x_acc));
+          return x_acc / (opmath_t(1) + c10::complex<double>(-x_acc));
         });
       });
 }
@@ -46,7 +46,7 @@ void silu_backward_kernel(TensorIteratorBase& iter) {
           const opmath_t dy_acc = static_cast<opmath_t>(dy);
           const opmath_t x_acc = static_cast<opmath_t>(x);
           const opmath_t s_acc =
-              opmath_t(1) / (opmath_t(1) + c10::cuda::compat::exp(-x_acc));
+              opmath_t(1) / (opmath_t(1) + c10::complex<double>(-x_acc));
           return dy_acc * s_acc * (opmath_t(1) + x_acc * (opmath_t(1) - s_acc));
         });
       });
