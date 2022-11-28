67f27afcc548aa3aabe56c1df92fb1c68d8ade40
Edward Z. Yang <ezyang@fb.com>
2022-11-27 20:48:40 -0800

    Update on "Add mypy checking for a few files in torch/_dynamo"
    It's kind of intractable to enable mypy everywhere at the moment,
    because there are a lot of errors, and also mypy is really slow
    for some reason.  I just want enough types to explain the public
    types for user compiler calls, going through typing the _C.dynamo
    bindings along the way.  This is a first step for this.
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/types.py
+++ torch/_dynamo/types.py
@@ -1,17 +1,27 @@
 import types
+from typing import Any, Callable, Dict, Optional, Union
+
 from typing_extensions import Protocol
+
 from torch._dynamo.guards import GuardedCode
-from typing import Callable, Optional, Union, Dict, Any
+
 
 class DynamoCallbackFn(Protocol):
-    def __call__(frame: types.FrameType, cache_size: int) -> Optional[GuardedCode]:
+    def __call__(
+        self, frame: types.FrameType, cache_size: int
+    ) -> Optional[GuardedCode]:
         ...
+
 
 DynamoCallback = Union[DynamoCallbackFn, None, bool]
 
+
 class DynamoGuardHook(Protocol):
     def __call__(
-        guard_fn: Callable, code: types.CodeType, f_locals: Dict[str, Any], last: bool
+        self,
+        guard_fn: Callable,
+        code: types.CodeType,
+        f_locals: Dict[str, Any],
+        last: bool,
     ) -> None:
         ...
-
