24bf4ac538add85eab1a53f9294d7f7451be851a
fduwjj <fduwjj@fb.com>
2022-11-28 19:40:29 +0000

    [PT-D][Tensor Parallel] Sync change to pytorch and Add more test cases when we use use_orig_params for FSDP wrapping
    [ghstack-poisoned]

--- torch/distributed/_tensor/ops/math_ops.py
+++ torch/distributed/_tensor/ops/math_ops.py
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 from typing import cast, Optional, Sequence
 
+import torch
 from torch.distributed._tensor.api import DTensor
 from torch.distributed._tensor.placement_types import DTensorSpec
 from torch.distributed._tensor.dispatch import OpSchema, OutputSharding
@@ -138,4 +139,36 @@ def var_correction_rule(op_schema: OpSchema) -> OutputSharding:
     keep_dim = cast(bool, op_schema.kwargs_schema.get("keepdim", False))
     return reduction_rule(
         op_schema, dims=dims, keep_dim=keep_dim, reduction_linear=False
+    )
+
+
+@register_prop_rule("aten.native_layer_norm.default")
+def _prop_native_layer_norm(op_schema: OpSchema) -> OutputSharding:
+    # TODO: Assert placement is Replicate() since we need to insert
+    # allreduce calls for _Partial results.
+    input, normalized_shape, weight, bias, eps = op_schema.args_schema
+    out, mean, rstd = input, weight, weight
+    dims_list = op_schema.args_schema[1]
+    assert isinstance(input, DTensorSpec)
+    return OutputSharding(
+        output_spec=(
+            DTensorSpec(
+                mesh=input.mesh,
+                placements=tuple(input.placements),
+                ndim=input.ndim,
+                shape=torch.Size(s for (i, s) in enumerate(input.shape)),
+            ),
+            DTensorSpec(
+                mesh=input.mesh,
+                placements=tuple(input.placements),
+                ndim=input.ndim,
+                shape=torch.Size(s for (i, s) in enumerate(input.shape)),
+            ),
+            DTensorSpec(
+                mesh=input.mesh,
+                placements=tuple(input.placements),
+                ndim=input.ndim,
+                shape=torch.Size(s for (i, s) in enumerate(input.shape)),
+            ),
+        )
     )
