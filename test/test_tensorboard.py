1be7499ecf89bdc9d8759c4142e79084577b98d3
Yuxin Wu <ppwwyyxxc@gmail.com>
2022-11-29 00:18:58 -0800

    unittest: Print the file path that's not found

--- test/test_tensorboard.py
+++ test/test_tensorboard.py
@@ -515,7 +515,7 @@ def get_expected_file(function_ptr):
 
 def read_expected_content(function_ptr):
     expected_file = get_expected_file(function_ptr)
-    assert os.path.exists(expected_file)
+    assert os.path.exists(expected_file), expected_file
     with open(expected_file, "r") as f:
         return f.read()
 
