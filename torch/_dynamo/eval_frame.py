3037b514f64d5e8a189e3f292cb33cc028b64357
Michael Lazos <mlazos@fb.com>
2022-11-29 21:17:04 +0000

    Retriggering ci

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -301,6 +301,7 @@ def catch_errors_wrapper(callback):
     @functools.wraps(callback)
     def catch_errors(frame, cache_size):
         if frame.f_lasti >= 0 or skipfiles.check(frame.f_code.co_filename):
+
             log.debug(f"skipping {frame.f_code.co_name} {frame.f_code.co_filename}")
             return None
         if frame.f_code.co_filename == "<string>" and frame.f_code.co_name == "__new__":
