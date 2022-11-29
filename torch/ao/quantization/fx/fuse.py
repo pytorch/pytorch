34359313c70ddfc5c6f46fb36863bba4d500446a
andrewor14 <andrewor14@gmail.com>
2022-11-29 14:08:50 -0800

    [Quant][fx][bc-breaking] Rename fx/*patterns.py
    Summary: This commit renames fx/quantization_patterns.py
    to fx/quantize_handler.py, and fx/fusion_patterns.py to
    fx/fuse_handler.py. This is because these files contain
    only QuantizeHandler and FuseHandler respectively, so the
    new names are more descriptive. A future commit will
    further break BC by removing all the empty *QuantizeHandler
    classes.
    
    BC-breaking notes:
    
    The following classes under the
    `torch.ao.quantization.fx.quantization_patterns` namespace
    are migrated to the `torch.ao.quantization.fx.quantize_handler`
    namespace:
    ```
    QuantizeHandler
    BinaryOpQuantizeHandler
    CatQuantizeHandler
    ConvReluQuantizeHandler
    LinearReLUQuantizeHandler
    BatchNormQuantizeHandler
    EmbeddingQuantizeHandler
    RNNDynamicQuantizeHandler
    DefaultNodeQuantizeHandler
    FixedQParamsOpQuantizeHandler
    CopyNodeQuantizeHandler
    GeneralTensorShapeOpQuantizeHandler
    CustomModuleQuantizeHandler
    StandaloneModuleQuantizeHandler
    ```
    
    The following classes under the
    `torch.ao.quantization.fx.fusion_patterns` namespace are
    migrated to the `torch.ao.quantization.fx.fuse_handler`
    namespace:
    ```
    DefaultFuseHandler
    FuseHandler
    ```
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers: jerryzh168, vkuzo
    
    Subscribers: jerryzh168, vkuzo
    
    [ghstack-poisoned]

--- torch/ao/quantization/fx/fuse.py
+++ torch/ao/quantization/fx/fuse.py
@@ -27,7 +27,7 @@ from ..backend_config.utils import (
 
 from .custom_config import FuseCustomConfig
 
-from .fusion_patterns import (
+from .fuse_handler import (
     _get_fusion_pattern_to_fuse_handler_cls,
     FuseHandler,
 )
