664537b0eb3d679c545b584da10f4cf1b4e80f75
Michael Lazos <mlazos@fb.com>
2022-11-29 07:19:02 +0000

    [dynamo] Don't copy the graph during checkpointing (copy_graphstate) (#89232)
    copy_graphstate is called a ton, this makes copy_graphstate a lot faster, helps with https://github.com/pytorch/torchdynamo/issues/1803
    
    tag each graph node with a timestamp, when checkpointing store the timestamp, when restoring remove nodes older than the timestamp stored in the state. This essentially has the same behavior as the original impl, just doesn't copy the whole graph.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89232
    Approved by: https://github.com/jansel

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -281,7 +281,7 @@ class GradModeVariable(ContextWrappingVariable):
     def _call_func(self, tx, values):
         assert len(values) == 1
         value = values[0]
-        tx.output.graph.create_node(
+        tx.output.create_node(
             "call_function", torch._C._set_grad_enabled, (value,), {}
         ),
         torch._C._set_grad_enabled(value)
@@ -337,12 +337,12 @@ class AutocastModeVariable(ContextWrappingVariable):
         self.mode = None
 
     def exit(self, tx, *args):
-        tx.output.graph.create_node(
+        tx.output.create_node(
             "call_function", exit_functional_autocast, (self.mode,), {}
         )
 
     def enter(self, tx):
-        self.mode = tx.output.graph.create_node(
+        self.mode = tx.output.create_node(
             "call_function", enter_functional_autocast, (*self.target_values,), {}
         )
 
