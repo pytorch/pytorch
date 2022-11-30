d4b8ddb47e4fda9c37d948e72cd4511ec27199d5
Michael Lazos <mlazos@fb.com>
2022-11-30 07:36:17 +0000

    Disable dynamo on _init_group if present

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -754,6 +754,10 @@ class TorchPatcher:
                 opt._cuda_graph_capture_health_check
             )
             opt.zero_grad = disable(opt.zero_grad)
+
+            if hasattr(opt, "_init_group"):
+                opt._init_group = disable(opt._init_group)
+
             # disable any currently set hooks
             # Note: we only want to disable the profiling hook
             # which is the *last* hook applied, we want to keep the no_grad hook
