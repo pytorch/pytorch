e263504b2746010a8055c77d57750f92aad9638d
titaiwang <titaiwang@microsoft.com>
2022-11-30 01:28:05 +0000

    Update on "[ONNX] Fix ignored small eps in layer normalization in fp16"
    [ghstack-poisoned]

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -2701,14 +2701,24 @@ def native_layer_norm(
 
     two_cst = symbolic_helper._generate_wrapped_number(g, 2.0)
     eps_cst = symbolic_helper._generate_wrapped_number(g, eps)
+    eps_dtype = _type_utils.JitScalarType.from_value(eps_cst)
 
     mean = g.op("ReduceMean", input, axes_i=axes)
     numerator = sub(g, input, mean)
+    # Cast it to eps dtype to avoid precision loss
+    cast_numerator = g.op(
+        "Cast", numerator, to_i=_type_utils.JitScalarType(eps_dtype).onnx_type()
+    )
+
     # variance = e((x - e(x))^2), and (x - e(x)) is the numerator in the layer_norm formula
     variance = g.op("ReduceMean", pow(g, numerator, two_cst), axes_i=axes)
-    denominator = sqrt(g, g.op("Add", variance, eps_cst).setType(eps_cst.type()))
+    # Cast it to eps dtype to avoid precision loss
+    cast_variance = g.op(
+        "Cast", variance, to_i=_type_utils.JitScalarType(eps_dtype).onnx_type()
+    )
 
-    normalized = g.op("Div", numerator, denominator).setType(eps_cst.type())
+    denominator = sqrt(g, g.op("Add", cast_variance, eps_cst))
+    normalized = g.op("Div", cast_numerator, denominator).setType(eps_cst.type())
 
     if not (weight is None or symbolic_helper._is_none(weight)):
         normalized = mul(g, normalized, weight)
@@ -2716,7 +2726,14 @@ def native_layer_norm(
         normalized = add(g, normalized, bias)
 
     # rdenominator := 1 / sqrt(variance + eps)
-    rdenominator = reciprocal(g, denominator)
+    # According to aten::native_layer_norm, rdenominator should have the same dtype as input,
+    # mean and normalized, so we need to Cast it back
+    mean_dtype = _type_utils.JitScalarType.from_value(mean)
+    cast_denominator = g.op(
+        "Cast", denominator, to_i=_type_utils.JitScalarType(mean_dtype).onnx_type()
+    )
+    rdenominator = g.op("Reciprocal", cast_denominator)
+
     return normalized, mean, rdenominator
 
 
