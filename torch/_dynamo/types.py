3624ff38153510fb7e4a207f19f32c0f0ee6c8e1
Edward Z. Yang <ezyang@fb.com>
2022-11-30 11:32:29 -0800

    Type torch._dynamo.guards (#89919)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89919
    Approved by: https://github.com/albanD

--- torch/_dynamo/types.py
+++ torch/_dynamo/types.py
@@ -1,9 +1,25 @@
+import dataclasses
 import types
-from typing import Any, Callable, Dict, Optional, Union
+from typing import Dict, List, Optional, OrderedDict, Union
 
 from typing_extensions import Protocol
 
-from torch._dynamo.guards import GuardedCode
+
+class GuardFn(Protocol):
+    closure_vars: OrderedDict[str, object]
+    code_parts: List[str]
+    verbose_code_parts: List[str]
+    global_scope: Dict[str, object]
+
+    # maps locals of user function to bool
+    def __call__(self, *maybe_dotzero: object, **f_locals: object) -> bool:
+        ...
+
+
+@dataclasses.dataclass
+class GuardedCode:
+    code: types.CodeType
+    check_fn: GuardFn
 
 
 class DynamoCallbackFn(Protocol):
@@ -19,9 +35,9 @@ DynamoCallback = Union[DynamoCallbackFn, None, bool]
 class DynamoGuardHook(Protocol):
     def __call__(
         self,
-        guard_fn: Callable,
+        guard_fn: GuardFn,
         code: types.CodeType,
-        f_locals: Dict[str, Any],
+        f_locals: Dict[str, object],
         last: bool,
     ) -> None:
         ...
