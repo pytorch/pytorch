067e8fada18c7ed16b6b7fd8fd9916dc3cd8bf7f
Aidyn-A <31858918+Aidyn-A@users.noreply.github.com>
2022-11-30 11:38:33 -0800

    check if _request is not nullptr

--- torch/csrc/distributed/c10d/UCCUtils.hpp
+++ torch/csrc/distributed/c10d/UCCUtils.hpp
@@ -9,33 +9,33 @@
 namespace c10d {
 
 // Macro to generate the error message on a non-successful UCC return value.
-#define TORCH_UCC_GET_ERROR_MSG(_err, _error_msg) \
-  do {                                            \
-      _err = c10::str(                            \
-          "[",                                    \
-          std::string(__FILE__),                  \
-          ":",                                    \
-          std::to_string(__LINE__),               \
-          "] ",                                   \
-          logger->getLogPrefix(),                 \
-          _error_msg,                             \
-          ", error code ",                        \
-          result,                                 \
-          ": ",                                   \
-          ucc_status_string(result),              \
-          ", system error code ",                 \
-          errno);                                 \
+#define TORCH_UCC_GET_ERROR_MSG(_err, _error_msg, _result) \
+  do {                                                     \
+      _err = c10::str(                                     \
+          "[",                                             \
+          std::string(__FILE__),                           \
+          ":",                                             \
+          std::to_string(__LINE__),                        \
+          "] ",                                            \
+          logger->getLogPrefix(),                          \
+          _error_msg,                                      \
+          ", error code ",                                 \
+          _result,                                         \
+          ": ",                                            \
+          ucc_status_string(_result),                      \
+          ", system error code ",                          \
+          errno);                                          \
   } while (0)
 
 // Macro to throw on a non-successful UCC return value.
-#define TORCH_UCC_CHECK(_cmd, _error_msg)       \
-  do {                                          \
-    ucc_status_t result = _cmd;                 \
-    if (result != UCC_OK) {                     \
-      std::string err;                          \
-      TORCH_UCC_GET_ERROR_MSG(err, _error_msg); \
-      TORCH_CHECK(false, err);                  \
-    }                                           \
+#define TORCH_UCC_CHECK(_cmd, _error_msg)               \
+  do {                                                  \
+    ucc_status_t result = _cmd;                         \
+    if (result != UCC_OK) {                             \
+      std::string err;                                  \
+      TORCH_UCC_GET_ERROR_MSG(err, _error_msg, result); \
+      TORCH_CHECK(false, err);                          \
+    }                                                   \
   } while (0)
 
 // Macro and throw on a non-successful UCC return value and free its request.
@@ -44,8 +44,10 @@ namespace c10d {
     ucc_status_t result = _cmd;                             \
     if (result != UCC_OK) {                                 \
       std::string err;                                      \
-      TORCH_UCC_GET_ERROR_MSG(err, _error_msg);             \
-      ucc_collective_finalize(_request);                    \
+      TORCH_UCC_GET_ERROR_MSG(err, _error_msg, result);     \
+      if (_request != nullptr) {                            \
+        ucc_collective_finalize(_request);                  \
+      }                                                     \
       TORCH_CHECK(false, err);                              \
     }                                                       \
   } while (0)
