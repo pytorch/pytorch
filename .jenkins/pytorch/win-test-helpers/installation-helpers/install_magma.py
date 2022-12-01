14d821c682100191665801c26ecaa8b2cda554d1
andysamfb <111015134+andysamfb@users.noreply.github.com>
2022-12-01 02:21:51 -0600

    Add files via upload

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.py
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_magma.py
@@ -24,20 +24,20 @@ if os.environ['USE_CUDA'] != "1":
 
 if not cuda_version.contains('.'):
 
-    subprocess.run(['echo', 'CUDA version ' + cuda_version +\
-     'format isn\'t correct, which doesn\'t contain \'.\''])
+    subprocess.run(['echo', 'CUDA version ' + cuda_version +
+        'format isn\'t correct, which doesn\'t contain \'.\''])
 
     sys.exit(1)
 
 
 os.environ['VERSION_SUFFIX'] = cuda_version.replace('.', '')
 cuda_suffix = 'cuda' + cuda_version.replace('.', '')
-os.environ['CUDA_SUFFIX']= cuda_suffix
+os.environ['CUDA_SUFFIX'] = cuda_suffix
 
 
 if cuda_suffix == '':
 
-    subprocess.run(['echo', 'unknown CUDA version, please set \`CUDA_VERSION\` higher than 10.2'])
+    subprocess.run(['echo', 'unknown CUDA version, please set \'CUDA_VERSION\' higher than 10.2'])
 
     sys.exit(1)
 
@@ -48,19 +48,19 @@ if 'REBUILD' not in os.environ:
 
         if 'BUILD_ENVIRONMENT' not in os.environ:
 
-            subprocess.run(['curl', '--retry', '3', '-k',\
-             'https://s3.amazonaws.com/ossci-windows/magma_2.5.4_' + cuda_suffix +\
-              '_' + build_type + '.7z', '--output', tmp_win_dir + '\\magma_2.5.4_'\
-               + cuda_suffix + '_' + build_type + '.7z'])
+            subprocess.run(['curl', '--retry', '3', '-k',
+                'https://s3.amazonaws.com/ossci-windows/magma_2.5.4_' + cuda_suffix +
+                    '_' + build_type + '.7z', '--output', tmp_win_dir + '\\magma_2.5.4_'
+                        + cuda_suffix + '_' + build_type + '.7z'])
 
         else:
 
-            subprocess.run(['aws', 's3', 'cp', 's3://ossci-windows/magma_2.5.4_'+\
-             cuda_suffix + '_' + build_type + '.7z', tmp_win_dir + '\\magma_2.5.4_'\
-              + cuda_suffix + '_' + build_type + '.7z', '--quiet'])
+            subprocess.run(['aws', 's3', 'cp', 's3://ossci-windows/magma_2.5.4_' +
+                cuda_suffix + '_' + build_type + '.7z', tmp_win_dir + '\\magma_2.5.4_'
+                    + cuda_suffix + '_' + build_type + '.7z', '--quiet'])
 
-        subprocess.run(['7z', 'x', '-aoa', tmp_win_dir + '\\magma_2.5.4_' +\
-         cuda_suffix + '_' + build_type + '.7z', '-o' + tmp_win_dir + '\\magma'])
+        subprocess.run(['7z', 'x', '-aoa', tmp_win_dir + '\\magma_2.5.4_' +
+            cuda_suffix + '_' + build_type + '.7z', '-o' + tmp_win_dir + '\\magma'])
 
     except Exception as e:
 
--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_mkl.py
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_mkl.py
@@ -11,17 +11,17 @@ if 'REBUILD' not in os.environ:
 
         if 'BUILD_ENVIRONMENT' not in os.environ:
 
-            subprocess.run(['curl', '--retry', '3', '-k',\
-             'https://s3.amazonaws.com/ossci-windows/mkl_2020.2.254.7z',\
-             '--output', tmp_win_dir + '\\mkl.7z'])
+            subprocess.run(['curl', '--retry', '3', '-k',
+                'https://s3.amazonaws.com/ossci-windows/mkl_2020.2.254.7z',
+                    '--output', tmp_win_dir + '\\mkl.7z'])
 
         else:
 
-            subprocess.run(['aws', 's3', 'cp', 's3://ossci-windows/mkl_2020.2.254.7z',\
-             tmp_win_dir + '\\mkl.7z', '--quiet'])
+            subprocess.run(['aws', 's3', 'cp', 's3://ossci-windows/mkl_2020.2.254.7z',
+                tmp_win_dir + '\\mkl.7z', '--quiet'])
 
-        subprocess.run(['7z', 'x', '-aoa', tmp_win_dir + '\\mkl.7z',\
-         '-o' + tmp_win_dir + '\\mkl'])
+        subprocess.run(['7z', 'x', '-aoa', tmp_win_dir + '\\mkl.7z',
+            '-o' + tmp_win_dir + '\\mkl'])
 
     except Exception as e:
 
