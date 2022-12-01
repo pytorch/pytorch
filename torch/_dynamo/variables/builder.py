a2e4fadbb44ca35e12178ea01cff8d5d1f0d89ab
Michael Lazos <mlazos@fb.com>
2022-12-01 21:16:57 +0000

    Revert "Add contained guards in builder for container types"
    This reverts commit f45c4abd739ee846ff62b278811f16dca84f78a7.

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -223,10 +223,6 @@ class VariableBuilder:
                 ).add_guards(guards)
                 for i, item in enumerate(value)
             ]
-
-            # include contained guards
-            guards.update(*[var.guards for var in output])
-
             result = self.list_type(value)(output, guards=guards)
             if istype(value, list):
                 return self.tx.output.side_effects.track_list(
@@ -241,10 +237,6 @@ class VariableBuilder:
                 )(tuple_iterator_getitem(value, i)).add_guards(guards)
                 for i in range(tuple_iterator_len(value))
             ]
-
-            # include contained guards
-            guards.update(*[var.guards for var in output])
-
             return ListIteratorVariable(
                 output, mutable_local=MutableLocal(), guards=guards
             )
@@ -255,7 +247,6 @@ class VariableBuilder:
                 )
                 for k in ("start", "stop", "step")
             ]
-
             if isinstance(value, slice):
                 return SliceVariable(items, guards=make_guards(GuardBuilder.TYPE_MATCH))
             else:
@@ -295,9 +286,6 @@ class VariableBuilder:
                     for k in value.keys()
                 ]
             )
-
-            # include contained guards
-            guards.update(*[var.guards for var in result.values()])
 
             if istype(value, collections.defaultdict):
                 result = DefaultDictVariable(
