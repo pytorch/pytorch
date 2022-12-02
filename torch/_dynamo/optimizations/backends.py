4bf0833f9c508469299f4ef5106b04ab653233f2
Michael Voznesensky <voznesenskym@gmail.com>
2022-12-02 14:06:56 -0800

    Update backends.py

--- torch/_dynamo/optimizations/backends.py
+++ torch/_dynamo/optimizations/backends.py
@@ -750,12 +750,10 @@ def torchxla_trace_once(subgraph):
     compiled_graph = None
 
     def fwd(*args):
-        nonlocal subgraph
         nonlocal compiled_graph
         model = subgraph.model
         if compiled_graph is None:
             compiled_graph = integration.extract_compiled_graph(model, args)
-            del subgraph
         return compiled_graph(*args)
 
     return fwd
