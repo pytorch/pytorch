c541a9d6344b4e07fd2971b8296555372e070e24
Michael Voznesensky <voznesenskym@gmail.com>
2022-12-02 18:27:07 +0000

    Use dynamo fake tensor mode in aot_autograd, move aot_autograd compilation to lowering time [Merger of 89672 and 89773] (#90039)
    After all of the preparatory commits, this is a subset of the
    changes in https://github.com/pytorch/pytorch/pull/89392 that actually
    change us to propagating fake tensors to backends.
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    This is the merger of Ed's PR #89672, which is a rewrite of an older PR of mine (#89392), with CI Fixes on top of it (#89773)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90039
    Approved by: https://github.com/ezyang

--- torch/_dynamo/optimizations/log_args.py
+++ torch/_dynamo/optimizations/log_args.py
@@ -34,7 +34,6 @@ class ConvArgsAnalysis(torch.fx.Interpreter):
 
     def run_node(self, n: torch.fx.Node):
         result = super().run_node(n)
-
         if n.op == "call_function":
             if n.target == aten.convolution.default:
                 args, kwargs = self.fetch_args_kwargs_from_env(n)
@@ -67,8 +66,8 @@ class ConvArgsAnalysis(torch.fx.Interpreter):
 
 
 def conv_args_analysis(gm: torch.fx.GraphModule, example_inputs):
-    # lowering graph
-    gm = make_fx(gm)(*example_inputs)
-    # use Interpreter to logs the args of conv
-    ConvArgsAnalysis(gm).run(*example_inputs)
-    return gm
+    def conv_arg_inner(*args):
+        fx_g = make_fx(gm)(*args)
+        return ConvArgsAnalysis(fx_g).run(*args)
+
+    return conv_arg_inner
