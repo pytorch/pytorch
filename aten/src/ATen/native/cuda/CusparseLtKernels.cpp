ff12e838a904dd71485ebb698ede3ba226dfba26
Christian Puhrsch <cpuhrsch@fb.com>
2022-12-03 20:15:00 +0000

    Basic harness

--- aten/src/ATen/native/cuda/CusparseLtKernels.cpp
+++ aten/src/ATen/native/cuda/CusparseLtKernels.cpp
@@ -5,17 +5,18 @@ The following source file implements a sparse linear operator using cusparseLt
 #include <c10/core/ScalarType.h>
 #include <c10/util/Half.h>
 #include <torch/custom_class.h>
-#include <cusparseLt.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <ATen/cuda/CUDADataType.h>
 #include <ATen/cuda/CUDAUtils.h>
+#include <iostream>
 
 
 namespace at {
 namespace native {
 
 Tensor _cusparselt_linear(const Tensor& sparse, const Tensor& dense) {
-  return at::mm(sparse, dense);
+  std::cout << "Calling _cusparselt_linear" << std::endl;
+  return sparse.mm(dense);
 }
 
 }
