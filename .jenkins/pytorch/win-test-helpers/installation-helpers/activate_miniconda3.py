a3476a9864c7b3a566faec3a6c5fe56b17e9431d
andysamfb <111015134+andysamfb@users.noreply.github.com>
2022-12-06 14:50:39 -0600

    Add files via upload

--- .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.py
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.py
@@ -59,9 +59,9 @@ if install_fresh_conda == '1':
 
     try:
 
-        subprocess.run([os.environ['CONDA_ENV_RUN'].split(), 'install', '-y', '-q', 'numpy\"<1.23\"', 'cffi', 'pyyaml', 'boto3', 'libuv'])
+        subprocess.run([*os.environ['CONDA_ENV_RUN'].split(), 'install', '-y', '-q', 'numpy\"<1.23\"', 'cffi', 'pyyaml', 'boto3', 'libuv'])
 
-        subprocess.run([os.environ['CONDA_ENV_RUN'].split(), 'install', '-y', '-q', '-c', 'conda-forge', 'cmake=3.22.3'])
+        subprocess.run([*os.environ['CONDA_ENV_RUN'].split(), 'install', '-y', '-q', '-c', 'conda-forge', 'cmake=3.22.3'])
 
     except Exception as e:
 
