Fixes for removing item precision loss

test/test_cuda.py
@@ -2450,7 +2450,12 @@ torch.cuda.synchronize()
                     actual = state_scaling[k]
                     if k == "step":
                         actual = actual.squeeze()
-                    self.assertEqual(state_control[k], actual, msg=k)
+
+                    expected = state_control[k]
+                    if isinstance(state_control[k], torch.Tensor) and state_control[k].dtype == torch.float64:
+                        expected = torch._cast_Float(state_control[k])
+
+                    self.assertEqual(expected, actual, msg=k)
 
     def test_grad_scaling_clipping(self):
         def run(data, model, optimizer, scaler, loss_fn, skip_iter, try_scaling_api):

torch/optim/adam.py
@@ -197,7 +197,7 @@ class Adam(Optimizer):
                     state['step'] = (
                         torch.zeros((1,), dtype=torch.float, device=p.device)
                         if self.defaults['capturable'] or self.defaults['fused']
-                        else torch.tensor(0.)
+                        else torch.tensor(0., dtype=torch.float64)
                     )
                     # Exponential moving average of gradient values
                     state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)