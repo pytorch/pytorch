11db99ae81e6ad6d28504237b5e1bb1be00014c3
blorange-amd <bo.li2@amd.com>
2022-12-07 21:31:09 +0000

    Keep miopen-hip-gfx part

--- .circleci/docker/common/install_rocm.sh
+++ .circleci/docker/common/install_rocm.sh
@@ -70,6 +70,17 @@ install_ubuntu() {
       DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated ${MIOPENKERNELS}
     fi
 
+    if [[ $(ver $ROCM_VERSION) -ge $(ver 5.4) ]]; then
+        MIOPENHIPGFX=$(apt-cache search --names-only miopen-hip-gfx | awk '{print $1}' | grep -F -v . || true)
+        if [[ "x${MIOPENHIPGFX}" = x ]]; then
+          echo "miopen-hip-gfx package not available"
+        else
+          DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated ${MIOPENHIPGFX}
+        fi
+    fi
+
+    install_magma
+
     # Cleanup
     apt-get autoclean && apt-get clean
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
@@ -136,14 +147,6 @@ install_centos() {
                    rccl \
                    rocprofiler-dev \
                    roctracer-dev
-  fi
-
-  # if search fails it will abort this script; use true to avoid case where search fails
-  MIOPENKERNELS=$(yum -q search miopenkernels | grep miopenkernels- | awk '{print $1}'| grep -F kdb. || true)
-  if [[ "x${MIOPENKERNELS}" = x ]]; then
-    echo "miopenkernels package not available"
-  else
-    yum install -y ${MIOPENKERNELS}
   fi
 
   # if search fails it will abort this script; use true to avoid case where search fails
