961f05241d66957153dca6b5b4f1468ead360942
Alex Settle <alexmsettle@gmail.com>
2022-12-08 09:22:53 -0800

    Attempt to fix xla test error

--- torch/_dynamo/optimizations/backends.py
+++ torch/_dynamo/optimizations/backends.py
@@ -754,7 +754,7 @@ def torchxla_trace_once(subgraph):
         model = subgraph.model
         if compiled_graph is None:
             compiled_graph = integration.extract_compiled_graph(model, args)
-            del subgraph
+            #del subgraph
         return compiled_graph(*args)
 
     return fwd
