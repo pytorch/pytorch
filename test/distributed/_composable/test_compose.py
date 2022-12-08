7065c8990584965fd198ecfdca8438fc0e5ed0c2
Andrew Gu <andgu@fb.com>
2022-12-08 15:54:08 +0000

    [Composable API][Easy] Fix some follow-ups (#90471)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90471
    Approved by: https://github.com/mrshenli

--- test/distributed/_composable/test_compose.py
+++ test/distributed/_composable/test_compose.py
@@ -99,7 +99,7 @@ class TestFSDPCheckpoint(FSDPTest):
 
         test_model = copy.deepcopy(model)
         test_model.u1 = fully_shard(test_model.u1, policy=None)
-        test_model.u2 = fully_shard(test_model.u2, policy=None)
+        test_model.u2 = fully_shard(test_model.u2)
 
         test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=use_reentrant)
         test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=use_reentrant)
