acbc091c5486ef6beecd794089644e1a580bb389
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2022-12-09 01:09:18 +0000

    [threaded PG] fix long hang issue in testing (#90515)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90515
    Approved by: https://github.com/wanchaol

--- test/distributed/test_multi_threaded_pg.py
+++ test/distributed/test_multi_threaded_pg.py
@@ -27,6 +27,51 @@ class TestCollectivesWithWrapper(TestCase):
         dist.broadcast_object_list(object_list=object_list)
         self.assertEqual(99, object_list[0])
 
+    def test_collective_error_on_rank_zero(self):
+        @spawn_threads_and_init_comms(world_size=4)
+        def _test_method(self):
+            input_tensor = torch.ones(3, 3) * dist.get_rank()  # perform 1st all gather
+            output_tensors = [torch.empty_like(input_tensor) for _ in range(dist.get_world_size())]
+            dist.all_gather(output_tensors, input_tensor)
+
+            if dist.get_rank() == 0:
+                raise AssertionError("Mimic real test failure.")  # fail on rank 0
+
+            dist.all_gather(output_tensors, input_tensor)  # perform 2nd all gather
+
+        with self.assertRaisesRegex(AssertionError, "Mimic real test failure."):
+            _test_method(self)
+
+    def test_collective_error_on_rank_non_zero(self):
+        @spawn_threads_and_init_comms(world_size=4)
+        def _test_method(self):
+            input_tensor = torch.ones(3, 3) * dist.get_rank()  # perform 1st all gather
+            output_tensors = [torch.empty_like(input_tensor) for _ in range(dist.get_world_size())]
+            dist.all_gather(output_tensors, input_tensor)
+
+            if dist.get_rank() == 1:
+                raise AssertionError("Mimic real test failure.")  # fail on rank 1
+
+            dist.all_gather(output_tensors, input_tensor)  # perform 2nd all gather
+
+        with self.assertRaisesRegex(AssertionError, "Mimic real test failure."):
+            _test_method(self)
+
+    def test_collective_error_on_rank_non_zero_all(self):
+        @spawn_threads_and_init_comms(world_size=4)
+        def _test_method(self):
+            input_tensor = torch.ones(3, 3) * dist.get_rank()  # perform 1st all gather
+            output_tensors = [torch.empty_like(input_tensor) for _ in range(dist.get_world_size())]
+            dist.all_gather(output_tensors, input_tensor)
+
+            if dist.get_rank() > 0:
+                raise AssertionError("Mimic real test failure.")  # fail on all non-zero rank
+
+            dist.all_gather(output_tensors, input_tensor)  # perform 2nd all gather
+
+        with self.assertRaisesRegex(AssertionError, "Mimic real test failure."):
+            _test_method(self)
+
 class TestCollectivesWithBaseClass(MultiThreadedTestCase):
     @property
     def world_size(self):
