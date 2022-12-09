393127587916ba502efd7047e0dcd56131cff3b2
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-12-09 18:02:48 +0000

    Disable BUILD_CAFFE2 from ONNX builds (#90475)
    Fixes https://github.com/microsoft/onnx-converters-private/issues/132
    
    @kit1980 and @malfet agreed in disabling ONNX tests for Caffe2 builds.
    With this change, exporting models with `operator+export_type=ONNX_ATEN_FALLBACK` will properly test non-caffe2 builds, which is the only scenario for aten fallback after caffe2 deprecation
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90475
    Approved by: https://github.com/kit1980, https://github.com/BowenBao

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -49,7 +49,8 @@ if [[ "$BUILD_ENVIRONMENT" == *cuda11* ]]; then
   fi
 fi
 
-if [[ ${BUILD_ENVIRONMENT} == *"caffe2"* || ${BUILD_ENVIRONMENT} == *"onnx"* ]]; then
+if [[ ${BUILD_ENVIRONMENT} == *"caffe2"* ]]; then
+  echo "Caffe2 build is ON"
   export BUILD_CAFFE2=ON
 fi
 
