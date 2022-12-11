63166ede89eb70c12f9efa35cd67a4eaf37a3d18
Michael Voznesensky <voznesenskym@gmail.com>
2022-12-11 03:25:31 +0000

    Make torch._guards, shuffle structures around for migration (#90636)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90636
    Approved by: https://github.com/ezyang

--- torch/_dynamo/source.py
+++ torch/_dynamo/source.py
@@ -2,9 +2,10 @@ import collections
 import dataclasses
 from typing import Any
 
+from torch._guards import Guard, GuardSource
+
 from . import utils
 from .bytecode_transformation import create_instruction
-from .guards import Guard, GuardSource
 from .utils import rename_implicit
 
 _GUARD_SOURCE_NN_MODULE = {
