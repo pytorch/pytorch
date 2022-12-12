3ba94fd72405e0751a8c244f03986a2221e17158
Charlie Yan <yanhao.charles@gmail.com>
2022-12-12 19:58:39 +0000

    [Composable API] Check for `fully_shard` in `replicate` API To make sure `fully_shard` and `replicate` can work together, we need to check for each other in the implementation. This change adds the check in `replicate()`.
    [ghstack-poisoned]

--- test/distributed/_composable/test_compose.py
+++ test/distributed/_composable/test_compose.py
@@ -6,7 +6,7 @@ import sys
 import torch
 import torch.distributed as dist
 import torch.nn as nn
-from torch.distributed._composable import checkpoint, fully_shard
+from torch.distributed._composable import checkpoint, fully_shard, replicate
 from torch.distributed.fsdp.api import ShardingStrategy
 from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 from torch.testing._internal.common_dist_composable import (
@@ -76,7 +76,9 @@ class TestFSDPCheckpoint(FSDPTest):
 
         test_model = copy.deepcopy(model)
         # compose checkpoint and fully_shard
-        test_model.seq = checkpoint(test_model.seq, use_reentrant=use_reentrant)
+        test_model.seq = checkpoint(
+            test_model.seq, use_reentrant=use_reentrant
+        )
         test_model.seq = fully_shard(
             test_model.seq,
             policy=ModuleWrapPolicy({nn.Linear}),
@@ -101,8 +103,12 @@ class TestFSDPCheckpoint(FSDPTest):
         test_model.u1 = fully_shard(test_model.u1, policy=None)
         test_model.u2 = fully_shard(test_model.u2)
 
-        test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=use_reentrant)
-        test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=use_reentrant)
+        test_model.u1.seq = checkpoint(
+            test_model.u1.seq, use_reentrant=use_reentrant
+        )
+        test_model.u2.seq = checkpoint(
+            test_model.u2.seq, use_reentrant=use_reentrant
+        )
 
         self.run_subtests(
             {
@@ -159,7 +165,9 @@ class TestFSDPCheckpoint(FSDPTest):
         test_model = copy.deepcopy(model)
         test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=False)
         test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=False)
-        test_model = fully_shard(test_model, strategy=ShardingStrategy.NO_SHARD)
+        test_model = fully_shard(
+            test_model, strategy=ShardingStrategy.NO_SHARD
+        )
 
         self.run_subtests(
             {
@@ -170,6 +178,37 @@ class TestFSDPCheckpoint(FSDPTest):
             },
             self._test_parity,
         )
+
+    def test_composable_fsdp_replicate(self):
+        model = CompositeModel(device=torch.device("cpu"))
+        fully_shard(model.l1)
+        with self.assertRaisesRegex(
+            AssertionError, "Cannot apply .*replicate"
+        ):
+            replicate(model.l1)
+        replicate(model.l2)  # should not raise
+
+    @skip_if_lt_x_gpu(2)
+    def test_fsdp_in_replicate(self):
+        model = CompositeModel(device=torch.device("cuda"))
+        base_model = copy.deepcopy(model)
+        test_model = copy.deepcopy(model)
+        fully_shard(test_model.l1)
+        replicate(test_model)
+        self.run_subtests(
+            {
+                "base_model": [base_model],
+                "test_model": [test_model],
+                "x": [torch.randn(2, 100, device="cuda")],
+                "grad_to_none": [True, False],
+            },
+            self._test_parity,
+        )
+
+    @skip_if_lt_x_gpu(2)
+    def test_replicate_in_fsdp(self):
+        # TODO: to be added
+        ...
 
 
 instantiate_parametrized_tests(TestFSDPCheckpoint)
