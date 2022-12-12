066e6479fe6e671ae73f3fbcb7fec15a1d8cea21
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-12-12 10:07:27 +0100

    revert previous changes

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -4470,52 +4470,54 @@ Example::
 )
 
 
-def one_hot(tensor: Tensor, num_classes: int = -1) -> Tensor:
-    r"""Takes LongTensor with index values of shape ``(*)`` and returns a tensor
-    of shape ``(*, num_classes)`` that have zeros everywhere except where the
-    index of last dimension matches the corresponding value of the input tensor,
-    in which case it will be 1.
+one_hot = _add_docstr(
+    torch._C._nn.one_hot,
+    r"""
+one_hot(tensor, num_classes=-1) -> LongTensor
 
-    See also `One-hot on Wikipedia`_ .
+Takes LongTensor with index values of shape ``(*)`` and returns a tensor
+of shape ``(*, num_classes)`` that have zeros everywhere except where the
+index of last dimension matches the corresponding value of the input tensor,
+in which case it will be 1.
 
-    .. _One-hot on Wikipedia:
-        https://en.wikipedia.org/wiki/One-hot
+See also `One-hot on Wikipedia`_ .
 
-    Arguments:
-        tensor (LongTensor): class values of any shape.
-        num_classes (int):  Total number of classes. If set to -1, the number
-            of classes will be inferred as one greater than the largest class
-            value in the input tensor.
+.. _One-hot on Wikipedia:
+    https://en.wikipedia.org/wiki/One-hot
 
-    Returns:
-        LongTensor that has one more dimension with 1 values at the
-        index of last dimension indicated by the input, and 0 everywhere
-        else.
+Arguments:
+    tensor (LongTensor): class values of any shape.
+    num_classes (int):  Total number of classes. If set to -1, the number
+        of classes will be inferred as one greater than the largest class
+        value in the input tensor.
 
-    Examples:
-        >>> F.one_hot(torch.arange(0, 5) % 3)
-        tensor([[1, 0, 0],
-                [0, 1, 0],
-                [0, 0, 1],
-                [1, 0, 0],
-                [0, 1, 0]])
-        >>> F.one_hot(torch.arange(0, 5) % 3, num_classes=5)
-        tensor([[1, 0, 0, 0, 0],
-                [0, 1, 0, 0, 0],
-                [0, 0, 1, 0, 0],
-                [1, 0, 0, 0, 0],
-                [0, 1, 0, 0, 0]])
-        >>> F.one_hot(torch.arange(0, 6).view(3,2) % 3)
-        tensor([[[1, 0, 0],
-                 [0, 1, 0]],
-                [[0, 0, 1],
-                 [1, 0, 0]],
-                [[0, 1, 0],
-                 [0, 0, 1]]])
-    """
-    if has_torch_function_unary(tensor):
-        return handle_torch_function(one_hot, (tensor,), tensor, num_classes=num_classes)
-    return torch._C._nn.one_hot(tensor, num_classes)
+Returns:
+    LongTensor that has one more dimension with 1 values at the
+    index of last dimension indicated by the input, and 0 everywhere
+    else.
+
+Examples:
+    >>> F.one_hot(torch.arange(0, 5) % 3)
+    tensor([[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [1, 0, 0],
+            [0, 1, 0]])
+    >>> F.one_hot(torch.arange(0, 5) % 3, num_classes=5)
+    tensor([[1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0]])
+    >>> F.one_hot(torch.arange(0, 6).view(3,2) % 3)
+    tensor([[[1, 0, 0],
+             [0, 1, 0]],
+            [[0, 0, 1],
+             [1, 0, 0]],
+            [[0, 1, 0],
+             [0, 0, 1]]])
+""",
+)
 
 
 def triplet_margin_loss(
