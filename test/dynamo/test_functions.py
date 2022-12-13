2807b65167881a9381890d78f44aebee59f46699
Yanbo Liang <ybliang8@gmail.com>
2022-12-13 19:06:38 +0000

    [Dynamo] Supports two torch.distributed.* functions (#90683)
    Fixes Meta internal user cases.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90683
    Approved by: https://github.com/jansel

--- test/dynamo/test_functions.py
+++ test/dynamo/test_functions.py
@@ -688,6 +688,23 @@ class FunctionTests(torch._dynamo.test_case.TestCase):
         m[1:] = [6] * (len(m) - 1)
         return x + 1
 
+    @make_test
+    def test_distributed_is_available(x):
+        if torch.distributed.is_available():
+            return x + 1
+        else:
+            return x - 1
+
+    @unittest.skipIf(
+        not torch.distributed.is_available(), "requires distributed package"
+    )
+    @make_test
+    def test_distributed_is_initialized(x):
+        if torch.distributed.is_initialized():
+            return x + 1
+        else:
+            return x - 1
+
     # # This is to test the new syntax for pattern matching
     # # ("match ... case ...") added on python 3.10.
     # # Uncomment these test cases if you run on 3.10+
