d1b0fe38a6a6026939fad75451544c91c3213475
HDCharles <charlesdavidhernandez@gmail.com>
2022-12-12 19:46:04 -0800

    [ao][fx] fixing public v private match_utils.py (#88396)
    Summary: made _is_match, _find_matches, _MatchResult private also added
    __all__ to lower_to_qnnpack.py
    
    Test Plan: python test/test_public_bindings.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D41015540](https://our.internmc.facebook.com/intern/diff/D41015540)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88396
    Approved by: https://github.com/jcaip

--- torch/ao/ns/_numeric_suite_fx.py
+++ torch/ao/ns/_numeric_suite_fx.py
@@ -121,7 +121,7 @@ from .fx.ns_types import (
 )
 from torch.ao.quantization.backend_config.utils import get_fusion_pattern_to_root_node_getter
 from torch.ao.quantization.backend_config import BackendConfig
-from torch.ao.quantization.fx.match_utils import find_matches
+from torch.ao.quantization.fx.match_utils import _find_matches
 from torch.ao.quantization.fx.qconfig_mapping_utils import generate_node_name_to_qconfig
 from torch.ao.quantization.fx.quantize_handler import _get_pattern_to_quantize_handlers
 from torch.ao.quantization.qconfig import QConfigAny
@@ -815,7 +815,7 @@ def prepare_n_shadows_model(
     standalone_module_names: List[str] = []
     standalone_module_classes: List[Type] = []
     custom_module_classes: List[Type] = []
-    matches = find_matches(
+    matches = _find_matches(
         mt.graph, modules, patterns, root_node_getter_mapping,
         standalone_module_names, standalone_module_classes, custom_module_classes)
     subgraphs_dedup: Dict[str, List[Node]] = \
