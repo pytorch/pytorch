d211a615b06e3a118bb2e686356d5004b06cca42
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-12-13 11:31:05 +0000

    update comments and doc string

--- torch/_functorch/eager_transforms.py
+++ torch/_functorch/eager_transforms.py
@@ -355,8 +355,8 @@ def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False
             the function to be differentiated and the second element is
             auxiliary objects that will not be differentiated.
             Default: False.
-        chunk_size (None or int): maximum size for chunked computation
-            of Jacobian. Default: None (means compute with 1 chunk).
+        chunk_size (None or int): If specified, controls the maximum size of chunk for computing
+            Jacobian. Default: None.
 
     Returns:
         Returns a function that takes in the same inputs as :attr:`func` and
@@ -605,15 +605,16 @@ def _chunked_standard_basis_for_(tensors, tensor_numels, chunk_size=None):
     total_numel = sum(tensor_numels)
     if chunk_size and chunk_size < total_numel:
         n_chunks = total_numel // chunk_size
-        numels = [chunk_size] * n_chunks
-        numels.append(total_numel % chunk_size)
+        chunk_numels = [chunk_size] * n_chunks
+        # remainder chunk
+        chunk_numels.append(total_numel % chunk_size)
     else:  # chunk_size is None or chunk_size >= total_numel
         chunk_size = total_numel
-        numels = [total_numel]
+        chunk_numels = [total_numel]
 
     diag_start_indices = (0, *torch.tensor(tensor_numels).cumsum(dim=0)[:-1].neg().unbind())
 
-    for chunk_idx, total_numel in enumerate(numels):
+    for chunk_idx, total_numel in enumerate(chunk_numels):
         chunks = tuple(tensor.new_zeros(total_numel, tensor_numel)
                        for tensor, tensor_numel in zip(tensors, tensor_numels))
 
