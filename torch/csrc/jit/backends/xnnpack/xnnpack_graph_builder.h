cd88a331ffeb36d88d94bece061a17681152f10a
Nikita Shulga <nshulga@meta.com>
2022-12-14 20:30:31 +0000

    [BE] Tweak Meta copyright headers (#90805)
    s/Facebook, Inc./Meta Platforms, Inc/
    s/Confidential and proprietary./This source code is licensed under the BSD-style license/
    
    Per https://www.internalfb.com/intern/wiki/Open_Source/Licenses/Straight_BSD/
    
    Also, add linter that prevents adding those in the future
    
    Fixes https://github.com/pytorch/pytorch/issues/90187
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90805
    Approved by: https://github.com/zpao

--- torch/csrc/jit/backends/xnnpack/xnnpack_graph_builder.h
+++ torch/csrc/jit/backends/xnnpack/xnnpack_graph_builder.h
@@ -1,4 +1,8 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include <ATen/Functions.h>
 #include <ATen/Utils.h>
 #include <torch/torch.h>
