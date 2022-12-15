Update on "[FSDP] Fix input grad propagation when using param mixed precision"
[ghstack-poisoned]

torch/distributed/fsdp/_runtime_utils.py
@@ -456,10 +456,13 @@ def _cast_fp_inputs_to_dtype(
     def cast_fn(x: torch.Tensor) -> torch.Tensor:
         if not torch.is_floating_point(x) or x.dtype == dtype:
             return x
-        # Cast this in-place to ensure that gradients propagate to `x` in the
-        # case that it requires gradients
-        x.data = x.to(dtype)
-        return x
+        # Cast this in-place to ensure that gradients propagate to `x` if it
+        # requires gradient
+        if x.requires_grad:
+            x.data = x.to(dtype)
+            return x
+        # Otherwise, cast this out-of-place
+        return x.to(dtype)
 
     with torch.no_grad():
         return (_apply_to_tensors(cast_fn, args), _apply_to_tensors(cast_fn, kwargs))