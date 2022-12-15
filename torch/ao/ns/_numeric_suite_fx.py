447de0dd931d3b2db5750d7ea5d36f317148d54f
HDCharles <charlesdavidhernandez@gmail.com>
2022-12-14 17:39:43 -0800

    [ao][fx] fixing public v private qconfig_mapping_utils.py (#88399)
    Summary: made _check_is_valid_config_dict,
    _compare_prepare_convert_qconfig_mappings,
    _generate_node_name_to_qconfig,
    _is_qconfig_supported_by_dtype_configs,
    _maybe_adjust_qconfig_for_module_name_object_type_order,
    _update_qconfig_for_fusion private
    
    Test Plan: python test/test_public_bindings.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D41015544](https://our.internmc.facebook.com/intern/diff/D41015544)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88399
    Approved by: https://github.com/jcaip

--- torch/ao/ns/_numeric_suite_fx.py
+++ torch/ao/ns/_numeric_suite_fx.py
@@ -122,7 +122,7 @@ from .fx.ns_types import (
 from torch.ao.quantization.backend_config.utils import get_fusion_pattern_to_root_node_getter
 from torch.ao.quantization.backend_config import BackendConfig
 from torch.ao.quantization.fx.match_utils import _find_matches
-from torch.ao.quantization.fx.qconfig_mapping_utils import generate_node_name_to_qconfig
+from torch.ao.quantization.fx.qconfig_mapping_utils import _generate_node_name_to_qconfig
 from torch.ao.quantization.fx.quantize_handler import _get_pattern_to_quantize_handlers
 from torch.ao.quantization.qconfig import QConfigAny
 from torch.ao.ns.fx.n_shadows_utils import (
@@ -825,7 +825,7 @@ def prepare_n_shadows_model(
     # TODO(future PR): deduplicate repeating entries
     list_of_node_name_to_qconfig: List[Dict[str, QConfigAny]] = []
     for qconfig_mapping in qconfig_multi_mapping.qconfig_mappings_list:
-        node_name_to_qconfig = generate_node_name_to_qconfig(
+        node_name_to_qconfig = _generate_node_name_to_qconfig(
             mt, modules, mt.graph, qconfig_mapping, tracer.node_name_to_scope)
         list_of_node_name_to_qconfig.append(node_name_to_qconfig)
 
