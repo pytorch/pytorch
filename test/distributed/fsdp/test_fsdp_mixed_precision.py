Update on "[FSDP] Fix input grad propagation when using param mixed precision"
For parameter mixed precision, we cast the inputs to the low precision parameter dtype. If the input has tensors that require gradient, then we must cast them in place in order for them to receive a gradient. The cast should be tracked by autograd (e.g. with `grad_fn` equal to `ToCopyBackward0`). This removes the `torch.no_grad` context when calling `_apply_to_tensors`.

[ghstack-poisoned]

test/distributed/fsdp/test_fsdp_mixed_precision.py
@@ -757,10 +757,17 @@ class TestFSDPMixedPrecisionSharded(TestFSDPMixedPrecision):
         )
         # Use an input with dtype not equal to the mixed precision
         # `param_dtype` so that it gets cast
-        x_float = torch.randn(32, 1024, device="cuda", dtype=torch.float32)
-        x_float.requires_grad_()
+        x_float = torch.randn(
+            (32, 1024),
+            device="cuda",
+            dtype=torch.float32,
+            requires_grad=True,
+        )
         fsdp_model(x_float).sum().backward()
         self.assertTrue(x_float.grad is not None)
+        # Check that `x_float` preserves its dtype, meaning that the gradient
+        # propagated via `ToCopyBackward0`
+        self.assertEqual(x_float.grad.dtype, torch.float32)
 
 
 class TestFSDPMixedPrecisionUnsharded(TestFSDPMixedPrecision):