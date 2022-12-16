e1d9b6ef6e5ad74b9f45cd4f7fef8c75b1e4da6c
Nikita Karetnikov <nikita@karetnikov.org>
2022-12-16 03:56:40 +0100

    Update on "[Inductor] actually check `replacements` in `AutogradMonkeypatch`"
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 @EikanWang jgong5 @Guobing-Chen chunyuan-w @XiaobingSuper zhuhaozhe blzheng @Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/overrides.py
+++ torch/_inductor/overrides.py
@@ -32,7 +32,10 @@ class AutogradMonkeypatch(TorchFunctionMode):
     def __torch_function__(self, func, types, args=(), kwargs=None):
         if not kwargs:
             kwargs = {}
-        if func in replacements:
+        if func in replacements and not (
+            config.fallback_random
+            and replacements[func] in replacements_using_triton_random
+        ):
             return replacements[func](*args, **kwargs)
         return func(*args, **kwargs)
 
