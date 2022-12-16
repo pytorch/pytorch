1b15c3d8ea0c6bd03939589c20bdce2ad4f7dd03
Nikita Vedeneev <nik@quansight.com>
2022-12-16 09:59:40 +0000

    guard a bit more

--- torch/sparse/triton_ops/__init__.py
+++ torch/sparse/triton_ops/__init__.py
@@ -1,5 +1,8 @@
-from .triton_bsr_dense_mm import bsr_dense_mm
+from torch._inductor.utils import has_triton
 
-__all__ = [
-    "bsr_dense_mm",
-]
+if has_triton():
+    from .triton_bsr_dense_mm import bsr_dense_mm
+
+    __all__ = [
+        "bsr_dense_mm",
+    ]
