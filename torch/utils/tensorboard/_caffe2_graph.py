97a0ada5866b921d4ac6d60fcf94bcec154da05e
Edward Z. Yang <ezyang@fb.com>
2022-12-16 08:56:17 +0800

    Lint rule to forbid direct use of logging.info/etc APIs (#90907)
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90907
    Approved by: https://github.com/jansel

--- torch/utils/tensorboard/_caffe2_graph.py
+++ torch/utils/tensorboard/_caffe2_graph.py
@@ -13,6 +13,7 @@ from caffe2.python import core, workspace
 
 from typing import Set, Dict, Tuple, List
 
+log = logging.getLogger(__name__)
 
 def _make_unique_name(seen: Set[str], name: str, min_version: int = 0):
     """
@@ -754,7 +755,7 @@ def _try_get_shapes(nets):
         shapes, _ = workspace.InferShapesAndTypes(nets)
         return shapes
     except Exception as e:
-        logging.warning("Failed to compute shapes: %s", e)
+        log.warning("Failed to compute shapes: %s", e)
         return {}
 
 
