0126138f3f1e108533e85e23c4711f9c61f9d1d0
Nikita Vedeneev <nik@quansight.com>
2022-12-16 10:32:05 +0000

    try using Library in the module context

--- torch/_sparse_triton_registrations.py
+++ torch/_sparse_triton_registrations.py
@@ -1,11 +1,12 @@
 import torch
 from torch._inductor.utils import has_triton
 
+_sparse_kernels_lib = torch.library.Library("aten", "IMPL")
+
 if has_triton():
     from torch.sparse.triton_ops import bsr_dense_mm
 
-    lib = torch.library.Library("aten", "IMPL")
-    lib.impl(
+    _sparse_kernels_lib.impl(
         "aten::_triton_bsr_dense_mm",
         lambda *args, **kwargs: bsr_dense_mm(*args, skip_checks=True, **kwargs),
         "SparseCsrCUDA",
