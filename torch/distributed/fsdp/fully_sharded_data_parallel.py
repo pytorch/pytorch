038d552bf7f2e39d78b95a14e948578c0553c580
Iris <wz337@cornell.edu>
2022-12-17 03:02:28 +0000

    [Checkpoint] Fix checkpoint test test_fsdp_optim_state.py (#91036)
    This PR:
    1. Fix the test/distributed/fsdp/test_fsdp_optim_state.py according to change in FSDP.flatten_sharded_optim_state_dict() API.
    2. Update docstring accordingly.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91036
    Approved by: https://github.com/fegin

--- torch/distributed/fsdp/fully_sharded_data_parallel.py
+++ torch/distributed/fsdp/fully_sharded_data_parallel.py
@@ -1351,6 +1351,8 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
                 sharded optimizer state.
             model (torch.nn.Module):
                 Refer to :meth:``shard_full_optim_state_dict``.
+            optim (torch.optim.Optimizer): Optimizer for ``model`` 's
+            parameters.
 
         Returns:
             Refer to :meth:`shard_full_optim_state_dict`.
