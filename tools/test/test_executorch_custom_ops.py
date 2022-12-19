8008dc50db137eb2ea04d700c85a9475866253bb
Larry Liu <8188269+larryliu0820@users.noreply.github.com>
2022-12-19 17:56:58 +0000

    [torchgen] Move Executorch custom ops logic into torchgen (#90099)
    ## Logic to handle custom ops
    We generate files for custom ops, so that they can be registered into PyTorch.
    
    Generated files:
    * `Register{dispatch_key}CustomOps.cpp` (dispatch_key = CPU), it's basically the same as vanilla PyTorch `RegisterCPU.cpp`. The only difference is that we bind to native functions directly.
    * `Register{dispatch_key}Stub.cpp` (dispatch_key = CPU), register placeholder kernels for custom ops. Only used when there's no custom op kernel available.
    
    As an example:
    ```cpp
    namespace {
    
    at::Tensor & wrapper_out_unsqueeze_out(const at::Tensor & self, int64_t dim, at::Tensor & out) {
        // No device check
    
      // DeviceGuard omitted
      return torch::executor::native::unsqueeze_out(self, dim, out);
    }
    } // anonymous namespace
    
    TORCH_LIBRARY_IMPL(aten, CPU, m) {
    
    m.impl("unsqueeze.out",
    TORCH_FN(wrapper_out_unsqueeze_out));
    }
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90099
    Approved by: https://github.com/ezyang

---
+++ tools/test/test_executorch_custom_ops.py
@@ -0,0 +1,74 @@
+from typing import Any, Dict
+
+import expecttest
+
+from torchgen.executorch.api.custom_ops import ComputeNativeFunctionStub
+from torchgen.model import Location, NativeFunction
+
+SPACES = "    "
+
+
+def _get_native_function_from_yaml(yaml_obj: Dict[str, object]) -> NativeFunction:
+    native_function, _ = NativeFunction.from_yaml(
+        yaml_obj,
+        loc=Location(__file__, 1),
+        valid_tags=set(),
+    )
+    return native_function
+
+
+class TestComputeNativeFunctionStub(expecttest.TestCase):
+    """
+    Could use torch.testing._internal.common_utils to reduce boilerplate.
+    GH CI job doesn't build torch before running tools unit tests, hence
+    manually adding these parametrized tests.
+    """
+
+    def _test_function_schema_generates_correct_kernel(
+        self, obj: Dict[str, Any], expected: str
+    ) -> None:
+        func = _get_native_function_from_yaml(obj)
+
+        gen = ComputeNativeFunctionStub()
+        res = gen(func)
+        self.assertIsNotNone(res)
+        self.assertExpectedInline(
+            str(res),
+            expected,
+        )
+
+    def test_function_schema_generates_correct_kernel_tensor_out(self) -> None:
+        obj = {"func": "custom::foo.out(Tensor self, *, Tensor(a!) out) -> Tensor(a!)"}
+        expected = """
+at::Tensor & wrapper_out_foo_out(const at::Tensor & self, at::Tensor & out) {
+    return out;
+}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_function_schema_generates_correct_kernel_no_out(self) -> None:
+        obj = {"func": "custom::foo.Tensor(Tensor self) -> Tensor"}
+        expected = """
+at::Tensor wrapper_Tensor_foo(const at::Tensor & self) {
+    return self;
+}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_function_schema_generates_correct_kernel_no_return(self) -> None:
+        obj = {"func": "custom::foo(Tensor self, *, Tensor(a!)[] out) -> ()"}
+        expected = f"""
+void wrapper__foo_out(const at::Tensor & self, at::TensorList out) {{
+{SPACES}
+}}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_schema_has_no_return_type_argument_throws(self) -> None:
+        func = _get_native_function_from_yaml(
+            {"func": "custom::foo.bool(Tensor self) -> bool"}
+        )
+
+        gen = ComputeNativeFunctionStub()
+        with self.assertRaisesRegex(Exception, "Can't handle this return type"):
+            gen(func)
