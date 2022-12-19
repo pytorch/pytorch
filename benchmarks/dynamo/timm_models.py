4aceafa413c9a863130a19bad397eef21d67e156
William Wen <williamwen@fb.com>
2022-12-19 22:37:31 +0000

    [dynamo] Better error message for bad timm model name (#91049)
    Fixes https://github.com/pytorch/torchdynamo/issues/1995
    
    Running `python benchmarks/dynamo/timm_models.py --performance --float32 -dcuda --output=out.csv --training --inductor --only bad_model_name` gives
    ```
    Traceback (most recent call last):
      File "benchmarks/dynamo/timm_models.py", line 338, in <module>
        main(TimmRunnner())
      File "/scratch/williamwen/work/pytorch/benchmarks/dynamo/common.py", line 1660, in main
        return maybe_fresh_cache(run, args.cold_start_latency and args.only)(
      File "/scratch/williamwen/work/pytorch/benchmarks/dynamo/common.py", line 833, in inner
        return fn(*args, **kwargs)
      File "/scratch/williamwen/work/pytorch/benchmarks/dynamo/common.py", line 2000, in run
        ) = runner.load_model(device, model_name, batch_size=batch_size)
      File "benchmarks/dynamo/timm_models.py", line 215, in load_model
        raise RuntimeError(f"Failed to load model '{model_name}'")
    RuntimeError: Failed to load model 'bad_model_name'
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91049
    Approved by: https://github.com/ezyang

--- benchmarks/dynamo/timm_models.py
+++ benchmarks/dynamo/timm_models.py
@@ -187,6 +187,7 @@ class TimmRunnner(BenchmarkRunner):
 
         retries = 1
         success = False
+        model = None
         while not success and retries < 4:
             try:
                 model = create_model(
@@ -209,6 +210,9 @@ class TimmRunnner(BenchmarkRunner):
                 wait = retries * 30
                 time.sleep(wait)
                 retries += 1
+
+        if model is None:
+            raise RuntimeError(f"Failed to load model '{model_name}'")
 
         model.to(
             device=device,
