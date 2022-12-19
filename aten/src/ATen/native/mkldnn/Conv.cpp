44bb1f7e3b88e1be623f0388fa0861fa2945107a
chunyuan <chunyuan.wu@intel.com>
2022-12-19 08:53:27 +0000

    Update base for Update on "[inductor] weight prepack for single conv_transpose2d"
    This PR enables weight prepack for single conv_transpose2d OP.
    
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- aten/src/ATen/native/mkldnn/Conv.cpp
+++ aten/src/ATen/native/mkldnn/Conv.cpp
@@ -677,7 +677,7 @@ Tensor _mkldnn_convolution_transpose(
 
   if (bias.defined()) {
     const ideep::tensor b = itensor_from_tensor(bias);
-    ideep::convolution_transpose_forward::compute(
+    ideep::convolution_transpose_forward::compute_v2(
         x,
         w,
         b,
@@ -688,9 +688,14 @@ Tensor _mkldnn_convolution_transpose(
         padding_r(padding, output_padding),
         dilation.vec(),
         groups,
+        ideep::scale_t(),
+        ideep::scale_t(),
+        ideep::scale_t(),
+        ideep::zero_point_t(),
+        ideep::zero_point_t(),
         op_attr);
   } else {
-    ideep::convolution_transpose_forward::compute(
+    ideep::convolution_transpose_forward::compute_v2(
         x,
         w,
         output_sizes,
@@ -700,6 +705,11 @@ Tensor _mkldnn_convolution_transpose(
         padding_r(padding, output_padding),
         dilation.vec(),
         groups,
+        ideep::scale_t(),
+        ideep::scale_t(),
+        ideep::scale_t(),
+        ideep::zero_point_t(),
+        ideep::zero_point_t(),
         op_attr);
   }
   if (input.is_mkldnn()) {
