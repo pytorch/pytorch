bb386660b65436f8ce857d6b4da001f279b0a0bc
Douwe den Blanken <V0XNIHILI@users.noreply.github.com>
2022-12-21 19:45:36 +0000

    Fix small typo in comment (#91247)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91247
    Approved by: https://github.com/albanD

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -1709,7 +1709,7 @@ class Module:
             destination[extra_state_key] = self.get_extra_state()
 
     # The user can pass an optional arbitrary mappable object to `state_dict`, in which case `state_dict` returns
-    # back that same object. But if they pass nothing, an `OrederedDict` is created and returned.
+    # back that same object. But if they pass nothing, an `OrderedDict` is created and returned.
     T_destination = TypeVar('T_destination', bound=Dict[str, Any])
 
     @overload
