be8c3cfb44e497e264cbb37cc7ebdb1d670d8054
Chien-Chin Huang <chienchin@fb.com>
2022-12-21 07:38:07 +0000

    Update on "[FSDP][optim_state_dict][8/N] Enable fully_shard optim state_dict save and load"
    **What does this PR do?**
    This PR refactor `_optim_utils.py` to use `_FSDPState` instead of `FullyShardedDataParallel` class. This change enables the support of optim state_dict for `fully_shard`.
    
    [ghstack-poisoned]

--- test/distributed/fsdp/test_fsdp_optim_state.py
+++ test/distributed/fsdp/test_fsdp_optim_state.py
@@ -783,7 +783,7 @@ class TestFSDPOptimState(FSDPTest):
         )
 
     @skip_if_lt_x_gpu(2)
-    def test_use_orig_params(self) -> None:
+    def _test_use_orig_params(self) -> None:
         """Tests :meth:`optim_state_dict` for an FSDP-root nested model."""
         self._test_load_optim_state(
             _ModelClass.NESTED,
