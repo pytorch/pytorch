412a4c8222b45892eb1d769a9990f279e02e4036
Ramin Azarmehr <razarmehr@apple.com>
2022-12-21 17:37:09 +0000

    [MPS] Add MPSHooks interface to enable accessing MPS functions globally (#91104)
    This PR is a prerequisite to the upcoming MPSGenerator changes required for Random Ops.
    
    Add `MPSHooksInterface.cpp` to `aten_cpu_source_non_codegen_list`
    
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91104
    Approved by: https://github.com/kulinseth, https://github.com/malfet

---
+++ aten/src/ATen/detail/MPSHooksInterface.h
@@ -0,0 +1,50 @@
+//  Copyright © 2022 Apple Inc.
+
+#pragma once
+
+#include <c10/core/Allocator.h>
+#include <ATen/core/Generator.h>
+#include <c10/util/Exception.h>
+#include <c10/util/Registry.h>
+
+#include <cstddef>
+#include <functional>
+
+namespace at {
+class Context;
+}
+
+namespace at {
+
+struct TORCH_API MPSHooksInterface {
+  virtual ~MPSHooksInterface() = default;
+
+  // Initialize the MPS library state
+  virtual void initMPS() const {
+    AT_ERROR("Cannot initialize MPS without MPS backend.");
+  }
+
+  virtual bool hasMPS() const {
+    return false;
+  }
+
+  virtual const Generator& getDefaultMPSGenerator(C10_UNUSED DeviceIndex device_index = -1) const {
+    AT_ERROR("Cannot get default MPS generator without MPS backend.");
+  }
+
+  virtual Allocator* getMPSDeviceAllocator() const {
+    AT_ERROR("MPSDeviceAllocator requires MPS.");
+  }
+};
+
+struct TORCH_API MPSHooksArgs {};
+
+C10_DECLARE_REGISTRY(MPSHooksRegistry, MPSHooksInterface, MPSHooksArgs);
+#define REGISTER_MPS_HOOKS(clsname) \
+  C10_REGISTER_CLASS(MPSHooksRegistry, clsname, clsname)
+
+namespace detail {
+TORCH_API const MPSHooksInterface& getMPSHooks();
+
+} // namespace detail
+} // namespace at
---
+++ aten/src/ATen/mps/MPSHooks.h
@@ -0,0 +1,19 @@
+//  Copyright © 2022 Apple Inc.
+
+#pragma once
+
+#include <ATen/detail/MPSHooksInterface.h>
+#include <ATen/Generator.h>
+#include <c10/util/Optional.h>
+
+namespace at { namespace mps {
+
+// The real implementation of MPSHooksInterface
+struct MPSHooks : public at::MPSHooksInterface {
+  MPSHooks(at::MPSHooksArgs) {}
+  void initMPS() const override;
+  bool hasMPS() const override;
+  Allocator* getMPSDeviceAllocator() const override;
+};
+
+}} // at::mps
