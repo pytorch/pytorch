2e15a921899f446617288b1ba1a252cef3518fe2
Wonjoo Lee <wonjoo@google.com>
2022-12-21 00:30:38 +0000

    Update gen_functionalization_type for XLA dynamic shape

--- torchgen/gen_functionalization_type.py
+++ torchgen/gen_functionalization_type.py
@@ -347,7 +347,6 @@ def emit_view_functionalization_body(
         }}
       );
       auto compute_reference_meta =
-        {view_tensor_name}.key_set().has_backend(c10::BackendComponent::XLABit) ||
         {view_tensor_name}.key_set().has_backend(c10::BackendComponent::LazyBit);
       {return_type} reference_tensor_output;
       if (compute_reference_meta) {{
@@ -383,7 +382,6 @@ def emit_view_functionalization_body(
       }}
       auto reapply_views = at::functionalization::impl::getFunctionalizationReapplyViewsTLS();
       auto compute_reference_meta =
-        {view_tensor_name}.key_set().has_backend(c10::BackendComponent::XLABit) ||
         {view_tensor_name}.key_set().has_backend(c10::BackendComponent::LazyBit);
       {return_type} reference_tensor_output;
       if (compute_reference_meta) {{
