8eb8e039568422a3979ab770936838a039900a7f
cnemri <chouaib.nemri@gmail.com>
2022-12-21 10:55:34 +0000

    Revert "implemented test and Changed assert to TORCH_CHECK #88808"
    This reverts commit d90a3842831c6e2783d5a4884c32cff2c4634179.

--- aten/src/ATen/cuda/detail/TensorInfo.cuh
+++ aten/src/ATen/cuda/detail/TensorInfo.cuh
@@ -50,7 +50,7 @@ TensorInfo<T, IndexType>::TensorInfo(T* p,
                                      IndexType st[MAX_TENSORINFO_DIMS]) {
   data = p;
   dims = dim;
-  TORCH_CHECK(dims < MAX_TENSORINFO_DIMS, "CUDA Tensors cannot have more than 25 dimensions");
+  TORCH_CHECK(dims < MAX_TENSORINFO_DIMS, "CUDA Tensors cannot have more than ", toString(MAX_TENSORINFO_DIMS), " dimensions");
   for (int i = 0; i < dim; ++i) {
     sizes[i] = sz[i];
     strides[i] = st[i];
