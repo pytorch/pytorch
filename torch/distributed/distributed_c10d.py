cb2cd71d0e828e4dab72708fa9926f8c0361c718
Howard Huang <howardhuang@fb.com>
2022-12-22 12:15:27 -0500

    Update torch/distributed/distributed_c10d.py
    Co-authored-by: Andrew Gu <31054793+awgu@users.noreply.github.com>

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -182,7 +182,7 @@ class Backend(object):
             raise ValueError("Backend name must be a string, but got: {}".format(name))
         value = getattr(Backend, name.upper(), Backend.UNDEFINED)
 
-        if value != Backend.GLOO and value != Backend.NCCL and value != Backend.UCC and value != Backend.MPI:
+        if value not in (Backend.GLOO, Backend.NCCL, Backend.UCC, Backend.MPI):
             value = name.lower()
         return value
 
