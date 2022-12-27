5933b191d1e416453676b857c191b068b9485782
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-12-27 14:37:34 -0500

    Reverse order of overloads for consistency

--- aten/src/ATen/core/ivalue_inl.h
+++ aten/src/ATen/core/ivalue_inl.h
@@ -216,15 +216,6 @@ inline at::Generator IValue::toGenerator() const& {
   AT_ASSERT(isGenerator(), "Expected Generator but got ", tagKind());
   return at::Generator(toIntrusivePtr<at::GeneratorImpl>());
 }
-inline c10::SymInt IValue::toSymInt() const& {
-  AT_ASSERT(isSymInt() || isInt(), "Expected SymInt or int but got ", tagKind());
-  if (isSymInt()) {
-    return c10::SymInt(toIntrusivePtr<c10::SymNodeImpl>());
-  } else {
-    return c10::SymInt(payload.u.as_int);
-  }
-}
-
 inline c10::SymInt IValue::toSymInt() && {
   AT_ASSERT(isSymInt() || isInt(), "Expected SymInt or int but got ", tagKind());
   if (isSymInt()) {
@@ -233,20 +224,26 @@ inline c10::SymInt IValue::toSymInt() && {
     return c10::SymInt(payload.u.as_int);
   }
 }
-
-inline c10::SymFloat IValue::toSymFloat() const& {
-  AT_ASSERT(isSymFloat() || isDouble(), "Expected SymFloat or double but got ", tagKind());
-  if (isSymFloat()) {
-    return c10::SymFloat(toIntrusivePtr<c10::SymNodeImpl>());
+inline c10::SymInt IValue::toSymInt() const& {
+  AT_ASSERT(isSymInt() || isInt(), "Expected SymInt or int but got ", tagKind());
+  if (isSymInt()) {
+    return c10::SymInt(toIntrusivePtr<c10::SymNodeImpl>());
   } else {
-    return c10::SymFloat(payload.u.as_double);
+    return c10::SymInt(payload.u.as_int);
   }
 }
-
 inline c10::SymFloat IValue::toSymFloat() && {
   AT_ASSERT(isSymFloat() || isDouble(), "Expected SymFloat or double but got ", tagKind());
   if (isSymFloat()) {
     return c10::SymFloat(moveToIntrusivePtr<c10::SymNodeImpl>());
+  } else {
+    return c10::SymFloat(payload.u.as_double);
+  }
+}
+inline c10::SymFloat IValue::toSymFloat() const& {
+  AT_ASSERT(isSymFloat() || isDouble(), "Expected SymFloat or double but got ", tagKind());
+  if (isSymFloat()) {
+    return c10::SymFloat(toIntrusivePtr<c10::SymNodeImpl>());
   } else {
     return c10::SymFloat(payload.u.as_double);
   }
