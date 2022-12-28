4279d9aad71b6058e578286c83a1c880117c04d9
XiaobingSuper <xiaobing.zhang@intel.com>
2022-12-28 02:06:28 -0500

    Update on "inductor: add conv+hardsigmoid fusion for cpu path"
    cc jgong5 mingfeima sanchitintel ashokei jingxu10 mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang Guobing-Chen chunyuan-w zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- test/inductor/torch_compile_debug/run_2022_12_15_02_36_05_594966/aot_torchinductor/model__0_forward_1.0/fx_graph_readable.py
+++
@@ -1,6 +0,0 @@
-class GraphModule(torch.nn.Module):
-    def forward(self, primals_1: f32[]):
-        # File: /home/xiaobing/pytorch-offical/torch/testing/_internal/common_methods_invocations.py:14444, code: op=lambda x, *args, **kwargs: x.half(*args, **kwargs),
-        convert_element_type: f16[] = torch.ops.prims.convert_element_type.default(primals_1, torch.float16);  primals_1 = None
-        return [convert_element_type]
-        
