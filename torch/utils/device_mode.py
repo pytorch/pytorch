e7dae397d8d7da78329e5a9e76360b117e8d5b25
Edward Z. Yang <ezyang@fb.com>
2022-12-30 08:59:02 +0800

    Add torch.utils.device_mode
    Fixes https://github.com/pytorch/pytorch/issues/82296
    Fixes https://github.com/pytorch/pytorch/issues/27878
    Fixes https://github.com/pytorch/pytorch/issues/260
    
    Open to bikeshedding the module name.  Open to bikeshedding if any of
    the identifiers should get reexporting from torch top level.  Open to
    bikeshedding the global setter API name / location.
    
    Tests coming later.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    [ghstack-poisoned]

---
+++ torch/utils/device_mode.py
@@ -0,0 +1,67 @@
+import torch
+from torch.overrides import TorchFunctionMode
+
+_DEVICE_CONSTRUCTOR = {
+    # standard ones
+    torch.empty,
+    torch.empty_strided,
+    torch.empty_quantized,
+    torch.ones,
+    torch.arange,
+    torch.bartlett_window,
+    torch.blackman_window,
+    torch.eye,
+    torch.fft.fftfreq,
+    torch.fft.rfftfreq,
+    torch.full,
+    torch.fill,
+    torch.hamming_window,
+    torch.hann_window,
+    torch.kaiser_window,
+    torch.linspace,
+    torch.logspace,
+    torch.nested.nested_tensor,
+    # torch.normal,
+    torch.ones,
+    torch.rand,
+    torch.randn,
+    torch.randint,
+    torch.randperm,
+    torch.range,
+    torch.sparse_coo_tensor,
+    torch.sparse_compressed_tensor,
+    torch.sparse_csr_tensor,
+    torch.sparse_csc_tensor,
+    torch.sparse_bsr_tensor,
+    torch.sparse_bsc_tensor,
+    torch.tril_indices,
+    torch.triu_indices,
+    torch.vander,
+    torch.zeros,
+    torch.asarray,
+    # weird ones
+    torch.tensor,
+    torch.as_tensor,
+}
+
+class DeviceMode(TorchFunctionMode):
+    def __init__(self, device):
+        self.device = torch.device(device)
+
+    def __torch_function__(self, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        if func in _DEVICE_CONSTRUCTOR:
+            if kwargs.get('device') is None:
+                kwargs['device'] = self.device
+            return func(*args, **kwargs)
+        return func(*args, **kwargs)
+
+GLOBAL_DEVICE_MODE = None
+
+def set_default_tensor_device(device):
+    global GLOBAL_DEVICE_MODE
+    if GLOBAL_DEVICE_MODE is not None:
+        GLOBAL_DEVICE_MODE.__exit__(None, None, None)
+    GLOBAL_DEVICE_MODE = DeviceMode(device)
+    GLOBAL_DEVICE_MODE.__enter__()
