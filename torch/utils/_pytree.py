7ae621483a62d0defb7485e0c792a5b146159bca
Sean Ross-Ross <srossross@gmail.com>
2023-01-02 14:05:25 -0600

    fix lint

--- torch/utils/_pytree.py
+++ torch/utils/_pytree.py
@@ -304,7 +304,7 @@ def _broadcast_to_and_flatten(pytree: PyTree, spec: TreeSpec) -> Optional[List[A
     if _is_leaf(pytree):
         return [pytree] * spec.num_leaves
     if isinstance(spec, LeafSpec):
-        if isinstance(pytree, (list,tuple)):
+        if isinstance(pytree, (list, tuple)):
             return list(pytree)
         return [pytree]
     node_type = _get_node_type(pytree)
