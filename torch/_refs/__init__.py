bad076e4cf08b2a3daf9427e1d68c9f4c0a77c9a
Peter Bell <peterbell10@live.co.uk>
2023-01-03 01:39:24 +0000

    [decomp] Use new squeeze.dims overload in decompositions
    [ghstack-poisoned]

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2905,12 +2905,6 @@ def _unsqueeze_multiple(x: TensorLikeType, dimensions: List[int]) -> TensorLikeT
     return x
 
 
-def _squeeze_multiple(x: TensorLikeType, dimensions: List[int]) -> TensorLikeType:
-    for dim in reversed(sorted(dimensions)):
-        x = torch.squeeze(x, dim)
-    return x
-
-
 @register_decomposition(aten.native_group_norm.default)
 def native_group_norm(
     input: Tensor,
@@ -2959,8 +2953,8 @@ def native_group_norm(
     rstd = _maybe_convert_to_dtype(rstd, input.dtype)  # type: ignore[assignment]
 
     # remove broadcast dimensions from mean and rstd
-    mean = _squeeze_multiple(mean, reduction_dims)
-    rstd = _squeeze_multiple(rstd, reduction_dims)
+    mean = torch.squeeze(mean, reduction_dims)
+    rstd = torch.squeeze(rstd, reduction_dims)
     return (out, mean, rstd)
 
 
