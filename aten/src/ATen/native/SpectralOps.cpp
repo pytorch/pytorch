ffdceed4288c9dad6357877623d5ddcc3e5c612c
Peter Bell <peterbell10@live.co.uk>
2023-01-04 20:36:23 +0000

    Update on "Fix Meta tests for FFT functions"
    [ghstack-poisoned]

--- aten/src/ATen/native/SpectralOps.cpp
+++ aten/src/ATen/native/SpectralOps.cpp
@@ -81,8 +81,8 @@ ScalarType promote_type_fft(ScalarType type, bool require_complex, Device device
   }
 
   const bool maybe_support_half = (
-    // Only CUDA supports half precision, and since meta devices don't have a tensor we
-    // err on the side of accepting it
+    // Only CUDA supports half precision, but since meta tensors don't have a
+    // device we err on the side of accepting it
     (device.is_cuda() || device.is_meta()) &&
     !at::detail::getCUDAHooks().hasROCM()
   );
