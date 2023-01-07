78e98f95147d67d749de94fbd72c6b0382c9c3ea
milesial <milesial@users.noreply.github.com>
2023-01-07 09:49:22 +0100

    Foreach gradient clipping

--- torch/nn/utils/clip_grad.py
+++ torch/nn/utils/clip_grad.py
@@ -1,4 +1,6 @@
 import warnings
+from collections import defaultdict
+
 import torch
 from torch._six import inf
 from typing import Union, Iterable
@@ -9,7 +11,7 @@ __all__ = ['clip_grad_norm_', 'clip_grad_norm', 'clip_grad_value_']
 
 def clip_grad_norm_(
         parameters: _tensor_or_tensors, max_norm: float, norm_type: float = 2.0,
-        error_if_nonfinite: bool = False) -> torch.Tensor:
+        error_if_nonfinite: bool = False, foreach: bool = False) -> torch.Tensor:
     r"""Clips gradient norm of an iterable of parameters.
 
     The norm is computed over all gradients together, as if they were
@@ -24,6 +26,7 @@ def clip_grad_norm_(
         error_if_nonfinite (bool): if True, an error is thrown if the total
             norm of the gradients from :attr:`parameters` is ``nan``,
             ``inf``, or ``-inf``. Default: False (will switch to True in the future)
+        foreach (bool): use the faster foreach-based implementation
 
     Returns:
         Total norm of the parameter gradients (viewed as a single vector).
@@ -36,9 +39,20 @@ def clip_grad_norm_(
     if len(grads) == 0:
         return torch.tensor(0.)
     device = grads[0].device
+
+    if foreach:
+        per_device_and_dtype_grads = defaultdict(list)
+        for g in grads:
+            per_device_and_dtype_grads[(g.device, g.dtype)].append(g.detach())
+
     if norm_type == inf:
         norms = [g.detach().abs().max().to(device) for g in grads]
         total_norm = norms[0] if len(norms) == 1 else torch.max(torch.stack(norms))
+    elif foreach:
+        norms = []
+        for grads in per_device_and_dtype_grads.values():
+            norms.extend(torch._foreach_norm(grads, norm_type))
+        total_norm = torch.norm(torch.stack([norm.to(device) for norm in norms]), norm_type)
     else:
         total_norm = torch.norm(torch.stack([torch.norm(g.detach(), norm_type).to(device) for g in grads]), norm_type)
     if error_if_nonfinite and torch.logical_or(total_norm.isnan(), total_norm.isinf()):
@@ -52,8 +66,13 @@ def clip_grad_norm_(
     # avoids a `if clip_coef < 1:` conditional which can require a CPU <=> device synchronization
     # when the gradients do not reside in CPU memory.
     clip_coef_clamped = torch.clamp(clip_coef, max=1.0)
-    for g in grads:
-        g.detach().mul_(clip_coef_clamped.to(g.device))
+    if foreach:
+        clip_coef_clamped_scalar = clip_coef_clamped.item()
+        for (device, _), grads in per_device_and_dtype_grads.items():
+            torch._foreach_mul_(grads, clip_coef_clamped_scalar)
+    else:
+        for g in grads:
+            g.detach().mul_(clip_coef_clamped.to(g.device))
     return total_norm
 
 
@@ -71,7 +90,7 @@ def clip_grad_norm(
     return clip_grad_norm_(parameters, max_norm, norm_type, error_if_nonfinite)
 
 
-def clip_grad_value_(parameters: _tensor_or_tensors, clip_value: float) -> None:
+def clip_grad_value_(parameters: _tensor_or_tensors, clip_value: float, foreach: bool = False) -> None:
     r"""Clips gradient of an iterable of parameters at specified value.
 
     Gradients are modified in-place.
@@ -82,9 +101,19 @@ def clip_grad_value_(parameters: _tensor_or_tensors, clip_value: float) -> None:
         clip_value (float): maximum allowed value of the gradients.
             The gradients are clipped in the range
             :math:`\left[\text{-clip\_value}, \text{clip\_value}\right]`
+        foreach (bool): use the faster foreach-based implementation
     """
     if isinstance(parameters, torch.Tensor):
         parameters = [parameters]
     clip_value = float(clip_value)
-    for p in filter(lambda p: p.grad is not None, parameters):
-        p.grad.data.clamp_(min=-clip_value, max=clip_value)
+    if foreach:
+        per_device_dtype_grads = defaultdict(list)
+        for p in filter(lambda p: p.grad is not None, parameters):
+            per_device_dtype_grads[(p.grad.device, p.grad.dtype)].append(p.grad)
+
+        for grads in per_device_dtype_grads.values():
+            torch._foreach_clamp_min_(grads, -clip_value)
+            torch._foreach_clamp_max_(grads, clip_value)
+    else:
+        for p in filter(lambda p: p.grad is not None, parameters):
+            p.grad.data.clamp_(min=-clip_value, max=clip_value)
