414ea80a8e1e051c413bebb7014527e43abebd46
Aaron Gokaslan <aaronGokaslan@gmail.com>
2023-01-07 21:15:43 +0000

    Apply clang-tidy perf fixes to aten (#91772)
    Mostly just automated fixes to get rid of implicit copies. I also fixed on clang-tidy NOLINT comment that was in the wrong spot. Split off from #91559
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91772
    Approved by: https://github.com/soumith

--- c10/core/TensorImpl.h
+++ c10/core/TensorImpl.h
@@ -29,6 +29,7 @@
 #include <limits>
 #include <memory>
 #include <numeric>
+#include <utility>
 
 // A global boolean variable to control whether we free memory when a Tensor
 // is shrunk to a smaller size. As a result, a Tensor is always going to
@@ -2336,7 +2337,7 @@ struct C10_API TensorImpl : public c10::intrusive_ptr_target {
   void set_storage_and_dtype(
       at::Storage storage,
       const caffe2::TypeMeta data_type) {
-    set_storage_keep_dtype(storage);
+    set_storage_keep_dtype(std::move(storage));
     data_type_ = data_type;
   }
 
