462f3bdebb4ee805a2e07a9df2ca337470544e19
eqy <eddiey@nvidia.com>
2023-01-06 16:44:38 -0800

    lint

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -450,7 +450,7 @@ struct ConvParams {
         bool type_cond = (input.scalar_type() == kHalf &&
                              weight.scalar_type() == kHalf) ||
                          (input.scalar_type() == kBFloat16 &&
-                             weight.scalar_type() == kBFloat16 && 
+                             weight.scalar_type() == kBFloat16 &&
                              at::cuda::getCurrentDeviceProperties()->major >= 8);
         bool kernel_cond =  (use_cudnn(input, weight) &&
                              type_cond &&
