98f4e645e306749eadd2b1bdee8fde9290c3ca97
BowenBao <bowbao@microsoft.com>
2023-01-09 17:11:51 +0000

    [ONNX] Fix 'prim::PackPadded' shape inference (#91829)
    In `peephole` pass, user nodes of output of `prim::PackPadded` are modified to consume
    the input of `prim::PackPadded` instead. Hence the logic in shape type inference. However
    only the first output requires this workaround.
    
    Fixes #91528
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91829
    Approved by: https://github.com/titaiwangms

--- torch/csrc/jit/passes/onnx/shape_type_inference.cpp
+++ torch/csrc/jit/passes/onnx/shape_type_inference.cpp
@@ -317,7 +317,14 @@ Node* CloneNodeToGraph(
           }
           case ::c10::prim::PackPadded: {
             auto input = n_graph->addInput();
-            input->copyMetadata(v_n->input(0));
+            if (v == v_n->output(0)) {
+              // Only the first output requires this workaround.
+              // In `peephole` pass, user nodes are modified to consume the
+              // input instead.
+              input->copyMetadata(v_n->input(0));
+            } else {
+              input->copyMetadata(v);
+            }
             return input;
           }
           default: {
