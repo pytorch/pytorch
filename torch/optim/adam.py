a633a177f06758aef8876d26e551378bae138dfe
Nouran Ali <nouranalimohammed@gmail.com>
2023-01-09 15:00:29 +0200

    add docstring for adam differentiable parameter

--- torch/optim/adam.py
+++ torch/optim/adam.py
@@ -105,6 +105,8 @@ class Adam(Optimizer):
         capturable (bool, optional): whether this instance is safe to capture in a CUDA graph.
             Passing True can impair ungraphed performance, so if you don't intend to
             graph capture this instance, leave it False (default: False)
+        differentiable (bool,optional): use adaptative calculation of the derivative of 
+            learning rate (default: False)
         fused (bool, optional): whether the fused implementation (CUDA only) is used.
             Currently, `torch.float64`, `torch.float32`, `torch.float16`, and `torch.bfloat16`
             are supported. Since the fused implementation is usually significantly faster than
