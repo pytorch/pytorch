9deabc81b5320832a6e791eeb730fedf826caab2
XiaobingSuper <xiaobing.zhang@intel.com>
2023-01-10 03:19:27 -0500

    inductor: fix .to(memort_format) issue which doesn't generate right stride
    [ghstack-poisoned]

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -1547,8 +1547,10 @@ def _to_copy(
         x = torch._prims.convert_element_type(x, dtype)
     if memory_format is not None:  # no ref/prim for memory format
         out = torch.empty_like(x, memory_format=memory_format)
-        out = aten.copy.default(out, x)
-        return out  # type: ignore[call-overload]
+        if out.stride() == x.stride():
+            return x
+        else:
+            return out.copy_(x)
     return x
 
 
