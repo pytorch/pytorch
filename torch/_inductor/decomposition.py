c55293d64099ac4380f5e3955a891d1d7924f327
min-jean-cho <min.jean.cho@intel.com>
2023-01-10 23:05:01 +0000

    [Inductor] Added aten.uniform_ decomp (#90869)
    Fixes #90815
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90869
    Approved by: https://github.com/jgong5, https://github.com/jansel, https://github.com/lezcano, https://github.com/ngimel, https://github.com/albanD

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -533,7 +533,7 @@ Some decomps result in differences from eager related to randomness.
 We put these decomps in a separate table `extra_random_decomps` to allow
 turning them on and off via `config.fallback_random`.
 """
-extra_random_decomps = get_decompositions([aten.native_dropout])
+extra_random_decomps = get_decompositions([aten.native_dropout, aten.uniform_])
 register_extra_random_decomp = functools.partial(
     decomp.register_decomposition, registry=extra_random_decomps
 )
