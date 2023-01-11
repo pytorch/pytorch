c233cb9ae9e75a3d81d73d6b5fa426dbd21b2d0e
cyy <cyyever@outlook.com>
2023-01-11 11:33:48 +0800

    avoid std::move

--- torch/csrc/api/src/serialize/input-archive.cpp
+++ torch/csrc/api/src/serialize/input-archive.cpp
@@ -129,8 +129,8 @@ void InputArchive::load_from(
     const char* data_;
     size_t size_;
   };
-  std::unique_ptr<OurAdapter> adapter(new OurAdapter(data, size));
-  module_ = torch::jit::load(std::move(adapter), std::move(device));
+  module_ = torch::jit::load(
+      std::make_unique<OurAdapter>(data, size), std::move(device));
 }
 
 void InputArchive::load_from(
@@ -157,8 +157,8 @@ void InputArchive::load_from(
     const std::function<size_t(uint64_t, void*, size_t)>& read_func_;
     const std::function<size_t(void)>& size_func_;
   };
-  std::unique_ptr<OurAdapter> adapter(new OurAdapter(read_func, size_func));
-  module_ = torch::jit::load(std::move(adapter), std::move(device));
+  module_ = torch::jit::load(
+      std::make_unique<OurAdapter>(read_func, size_func), std::move(device));
 }
 
 std::vector<std::string> InputArchive::keys() {
