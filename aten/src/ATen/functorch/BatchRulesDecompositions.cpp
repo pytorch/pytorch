abbe2f18abb319bf08ee8e1605b2a6a953b96898
Sean Ross-Ross <srossross@gmail.com>
2023-01-12 16:56:36 -0600

    adding missing ops

--- aten/src/ATen/functorch/BatchRulesDecompositions.cpp
+++ aten/src/ATen/functorch/BatchRulesDecompositions.cpp
@@ -160,8 +160,12 @@ TORCH_LIBRARY_IMPL(aten, FuncTorchBatchedDecomposition, m) {
   OP_DECOMPOSE(linalg_svd);
   OP_DECOMPOSE(linalg_svdvals);
   OP_DECOMPOSE(linalg_tensorinv);
-  OP_DECOMPOSE(linalg_vander);
   OP_DECOMPOSE(cumprod_backward);
+  OP_DECOMPOSE(linalg_matrix_power);
+  OP_DECOMPOSE(linalg_pinv);
+  OP_DECOMPOSE(linalg_eigh);
+  OP_DECOMPOSE(linalg_vander);
+  OP_DECOMPOSE(linalg_vecdot);
   OP_DECOMPOSE(_lu_with_info);
   OP_DECOMPOSE(matmul);
   OP_DECOMPOSE(matrix_H);
