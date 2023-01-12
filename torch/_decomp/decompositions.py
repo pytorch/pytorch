a0c46f5ab85792df142fc8a4454ad0fa4a50d2d6
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared4887.02.pnb3.facebook.com>
2023-01-12 00:03:08 -0800

    2023-01-12 nightly release (5766764d6cf9b14380c1b6ef5c2fe5392722e37b)

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -988,8 +988,11 @@ def _softmax(x: Tensor, dim: int, half_to_float: bool):
         x, type_promotion_kind=utils.ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT
     )
     x = x.to(computation_dtype)
-    x_max = torch.amax(x, dim, keepdim=True)
-    unnormalized = torch.exp(x - x_max)
+    if x.numel() == 0:
+        unnormalized = torch.exp(x)
+    else:
+        x_max = torch.amax(x, dim, keepdim=True)
+        unnormalized = torch.exp(x - x_max)
     result = unnormalized / torch.sum(unnormalized, dim, keepdim=True)
     if not half_to_float:
         result = result.to(result_dtype)
@@ -1008,8 +1011,11 @@ def _log_softmax(x: Tensor, dim: int, half_to_float: bool):
         x, type_promotion_kind=utils.ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT
     )
     x = x.to(computation_dtype)
-    x_max = torch.amax(x, dim, keepdim=True)
-    shifted = x - x_max
+    if x.numel() == 0:
+        shifted = x
+    else:
+        x_max = torch.amax(x, dim, keepdim=True)
+        shifted = x - x_max
     shifted_logsumexp = torch.log(torch.sum(torch.exp(shifted), dim, keepdim=True))
     result = shifted - shifted_logsumexp
     if not half_to_float:
@@ -1547,9 +1553,7 @@ def _to_copy(
     if dtype is not None and not dtype_converted:
         x = torch._prims.convert_element_type(x, dtype)
     if memory_format is not None:  # no ref/prim for memory format
-        out = torch.empty_like(x, memory_format=memory_format)
-        out = aten.copy.default(out, x)
-        return out  # type: ignore[call-overload]
+        return torch.clone(x, memory_format=memory_format)
     return x
 
 
@@ -1897,6 +1901,12 @@ def uniform(
         dtype=x.dtype,
         device=x.device,
     )
+
+
+@register_decomposition(aten.uniform_)
+def uniform_(self, low=0, high=1, generator=None):
+    assert generator is None
+    return self.copy_((high - low) * torch.rand_like(self) + low)
 
 
 # aten/src/ATen/native/UpSample.cpp compute_output_size
--- torch/csrc/utils/disable_torch_function.cpp
+++ torch/csrc/utils/disable_torch_function.cpp
@@ -11,7 +11,8 @@ PyObject* disabled_torch_function = nullptr;
 PyObject* disabled_torch_dispatch = nullptr;
 
 bool torch_function_enabled() {
-  return !at::impl::PythonTorchFunctionTLS::is_disabled();
+  return at::impl::PythonTorchFunctionTLS::get_disabled_state() ==
+      at::impl::TorchFunctionDisabledState::ENABLED;
 }
 
 PyObject* disabled_torch_function_impl() {
@@ -34,20 +35,23 @@ void set_disabled_torch_dispatch_impl(PyObject* value) {
 typedef struct {
   PyObject_HEAD
       /* Type-specific fields go here. */
-      bool old_state;
+      at::impl::TorchFunctionDisabledState old_state;
 } DisableTorchFunctionSubclass;
 
 PyObject* DisableTorchFunctionSubclass__enter(
     PyObject* self,
     PyObject* unused) {
-  ((DisableTorchFunctionSubclass*)self)->old_state =
-      at::impl::PythonTorchFunctionTLS::is_disabled();
-  at::impl::PythonTorchFunctionTLS::set_disabled(true);
+  const auto old_state = at::impl::PythonTorchFunctionTLS::get_disabled_state();
+  ((DisableTorchFunctionSubclass*)self)->old_state = old_state;
+  if (old_state == at::impl::TorchFunctionDisabledState::ENABLED) {
+    at::impl::PythonTorchFunctionTLS::set_disabled_state(
+        at::impl::TorchFunctionDisabledState::SUBCLASSES_DISABLED);
+  }
   Py_RETURN_NONE;
 }
 
 PyObject* DisableTorchFunctionSubclass__exit(PyObject* self, PyObject* unused) {
-  at::impl::PythonTorchFunctionTLS::set_disabled(
+  at::impl::PythonTorchFunctionTLS::set_disabled_state(
       ((DisableTorchFunctionSubclass*)self)->old_state);
   Py_RETURN_NONE;
 }
@@ -115,6 +119,81 @@ PyObject* THPModule_DisableTorchFunctionSubclassType() {
   return (PyObject*)(&DisableTorchFunctionSubclassType);
 }
 
+typedef struct {
+  PyObject_HEAD
+      /* Type-specific fields go here. */
+      at::impl::TorchFunctionDisabledState old_state;
+} DisableTorchFunction;
+
+PyObject* DisableTorchFunction__enter(PyObject* self, PyObject* unused) {
+  ((DisableTorchFunctionSubclass*)self)->old_state =
+      at::impl::PythonTorchFunctionTLS::get_disabled_state();
+  at::impl::PythonTorchFunctionTLS::set_disabled_state(
+      at::impl::TorchFunctionDisabledState::ALL_DISABLED);
+  Py_RETURN_NONE;
+}
+
+PyObject* DisableTorchFunction__exit(PyObject* self, PyObject* unused) {
+  at::impl::PythonTorchFunctionTLS::set_disabled_state(
+      ((DisableTorchFunctionSubclass*)self)->old_state);
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef DisableTorchFunction_methods[] = { // NOLINT
+    {"__enter__", DisableTorchFunction__enter, METH_NOARGS, nullptr},
+    {"__exit__", DisableTorchFunction__exit, METH_VARARGS, nullptr},
+    {nullptr, nullptr, 0, nullptr}};
+
+PyTypeObject DisableTorchFunctionType = {
+    PyVarObject_HEAD_INIT(
+        nullptr,
+        0) "torch._C.DisableTorchFunction", /* tp_name */
+    sizeof(DisableTorchFunction), /* tp_basicsize */
+    0, /* tp_itemsize */
+    nullptr, /* tp_dealloc */
+    0, /* tp_vectorcall_offset */
+    nullptr, /* tp_getattr */
+    nullptr, /* tp_setattr */
+    nullptr, /* tp_reserved */
+    nullptr, /* tp_repr */
+    nullptr, /* tp_as_number */
+    nullptr, /* tp_as_sequence */
+    nullptr, /* tp_as_mapping */
+    nullptr, /* tp_hash  */
+    nullptr, /* tp_call */
+    nullptr, /* tp_str */
+    nullptr, /* tp_getattro */
+    nullptr, /* tp_setattro */
+    nullptr, /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT, /* tp_flags */
+    nullptr, /* tp_doc */
+    nullptr, /* tp_traverse */
+    nullptr, /* tp_clear */
+    nullptr, /* tp_richcompare */
+    0, /* tp_weaklistoffset */
+    nullptr, /* tp_iter */
+    nullptr, /* tp_iternext */
+    DisableTorchFunction_methods, /* tp_methods */
+    nullptr, /* tp_members */
+    nullptr, /* tp_getset */
+    nullptr, /* tp_base */
+    nullptr, /* tp_dict */
+    nullptr, /* tp_descr_get */
+    nullptr, /* tp_descr_set */
+    0, /* tp_dictoffset */
+    nullptr, /* tp_init */
+    PyType_GenericAlloc, /* tp_alloc */
+    PyType_GenericNew, /* tp_new */
+};
+
+PyObject* THPModule_DisableTorchFunctionType() {
+  if (PyType_Ready(&DisableTorchFunctionType) < 0) {
+    return nullptr;
+  }
+
+  return (PyObject*)(&DisableTorchFunctionType);
+}
+
 PyObject* THPModule_disable_torch_function(PyObject* self, PyObject* a) {
   HANDLE_TH_ERRORS
   PyObject *func = nullptr, *types = nullptr, *args = nullptr,
@@ -137,11 +216,14 @@ PyObject* THPModule_disable_torch_function(PyObject* self, PyObject* a) {
   // These are all C-API calls so no exceptions will be raised
   // and therefore no need for RAII approach to storing
   // the old value.
-  bool old_value = at::impl::PythonTorchFunctionTLS::is_disabled();
-  at::impl::PythonTorchFunctionTLS::set_disabled(true);
+  auto old_value = at::impl::PythonTorchFunctionTLS::get_disabled_state();
+  if (old_value == at::impl::TorchFunctionDisabledState::ENABLED) {
+    at::impl::PythonTorchFunctionTLS::set_disabled_state(
+        at::impl::TorchFunctionDisabledState::SUBCLASSES_DISABLED);
+  }
   // kwargs can safely be nullptr here.
   PyObject* result = PyObject_Call(func, py_args.ptr(), kwargs);
-  at::impl::PythonTorchFunctionTLS::set_disabled(old_value);
+  at::impl::PythonTorchFunctionTLS::set_disabled_state(old_value);
   return result;
   END_HANDLE_TH_ERRORS
 }
