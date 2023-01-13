ab78fa19edae21e54309f1685e81da25666a0389
Peter Bell <peterbell10@live.co.uk>
2023-01-13 13:44:53 +0000

    Vectorize torch.exp2 on CPU and add complex support
    I see an 11x speedup in `exp2` on CPU from this.
    
    [ghstack-poisoned]

--- aten/src/ATen/cpu/vec/vec_base.h
+++ aten/src/ATen/cpu/vec/vec_base.h
@@ -379,6 +379,9 @@ public:
   Vectorized<T> exp() const {
     return map(std::exp);
   }
+  Vectorized<T> exp2() const {
+    return map([](T x) -> T { return exp2_impl(x); });
+  }
   Vectorized<T> expm1() const {
     return map(std::expm1);
   }
