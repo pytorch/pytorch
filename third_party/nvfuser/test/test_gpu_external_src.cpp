b84ae9cc5caf17189d4191d17d19a1a070d83531
Gao, Xiang <qasdfgtyuiop@gmail.com>
2023-01-16 11:42:44 -0800

    Add FusionExternalSrcMatmul_CUDA to test_gpu_external_src.cpp (#2287)

--- third_party/nvfuser/test/test_gpu_external_src.cpp
+++ third_party/nvfuser/test/test_gpu_external_src.cpp
@@ -80,8 +80,99 @@ TEST_F(NVFuserTest, FusionExternalSrc_CUDA) {
               << " GB/s" << std::endl;
 
     auto fusion_out = t7.to(at::kFloat);
-    std::cout << "Max diff: " << (ref - fusion_out).abs().max() << std::endl;
+    std::cout << "Max diff: " << (ref - fusion_out).abs().max().item<float>()
+              << std::endl;
     TORCH_CHECK(ref.allclose(fusion_out, /*rtol*/ 0.005, /*atol*/ 0.5));
+  }
+}
+
+namespace {
+
+std::pair<at::Tensor, at::Tensor> fp16MatmulAtInput(
+    int M,
+    int N,
+    int K,
+    MmaOptions::MmaInputLayout layout) {
+  auto options = at::TensorOptions().dtype(at::kHalf).device(at::kCUDA, 0);
+
+  switch (layout) {
+    case MmaOptions::MmaInputLayout::TT:
+      return std::make_pair(
+          at::randn({M, K}, options), at::randn({K, N}, options));
+    case MmaOptions::MmaInputLayout::TN:
+      return std::make_pair(
+          at::randn({M, K}, options), at::randn({N, K}, options));
+    case MmaOptions::MmaInputLayout::NT:
+      return std::make_pair(
+          at::randn({K, M}, options), at::randn({K, N}, options));
+    default:
+      TORCH_CHECK(false, "unsupported data layout.");
+  }
+  return std::make_pair(at::Tensor(), at::Tensor());
+}
+
+// Utility to generate matmul input tensors based on given layout
+at::Tensor atMatmul(
+    at::Tensor a,
+    at::Tensor b,
+    MmaOptions::MmaInputLayout layout) {
+  switch (layout) {
+    case MmaOptions::MmaInputLayout::TT:
+      return a.matmul(b);
+    case MmaOptions::MmaInputLayout::TN:
+      return a.matmul(b.t());
+    case MmaOptions::MmaInputLayout::NT:
+      return a.t().matmul(b);
+    default:
+      TORCH_CHECK(false, "unsupported data layout.");
+  }
+  return at::Tensor();
+}
+
+} // namespace
+
+// This is based on the following benchmark:
+// Nvfuser_Matmul_4warp3stage/no_quant_nvfuser_4warp_TN_Legacy/2048/3456/2048/manual_time
+TEST_F(NVFuserTest, FusionExternalSrcMatmul_CUDA) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+  FusionExecutor fe;
+
+  // By default, this env var should not be defined. To test using an
+  // external source file, set it to the path to the external source
+  // file.
+  auto path = std::getenv("PYTORCH_NVFUSER_EXTERNAL_SRC");
+  if (path == nullptr) {
+    return;
+  }
+
+  std::cout << "Compiling " << path << std::endl;
+  std::ifstream cuda_src(path);
+  std::stringstream buffer;
+  buffer << cuda_src.rdbuf();
+  std::string cuda_src_str = buffer.str();
+
+  fe.compileRtc(cuda_src_str, "CudaCodeGen::kernel1", true);
+
+  int M = 2048, N = 3456, K = 2048;
+  MmaOptions::MmaInputLayout layout = MmaOptions::MmaInputLayout::TN;
+  auto inputs = fp16MatmulAtInput(M, N, K, layout);
+  auto at_output = atMatmul(inputs.first, inputs.second, layout).to(kFloat);
+
+  LaunchParams lp(16, 27, 1, 32, 2, 2);
+  lp.setSmem(49152);
+
+  for (int i = 0; i < 5; ++i) {
+    auto output = at::zeros_like(at_output);
+    clearL2Cache();
+    std::cout << "Launching the kernel" << std::endl;
+    float elapsed_time_ms = fe.runRtc(
+        lp, {inputs.first, inputs.second, output}, KernelIndexMode::INT32);
+    std::cout << "kernel run in " << elapsed_time_ms << " ms." << std::endl;
+
+    std::cout << "Max diff: " << (at_output - output).abs().max().item<float>()
+              << std::endl;
+    TORCH_CHECK(at_output.allclose(output, /*rtol*/ 0.005, /*atol*/ 0.5));
   }
 }
 
