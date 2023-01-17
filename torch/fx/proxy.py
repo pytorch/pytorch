8891003ce6da80ae0332a3dffc22ee66f690d92d
Angela Yi <yiangela7@gmail.com>
2023-01-17 17:40:47 +0000

    [fx] Throw error when symbolically tracing control flow ops

--- torch/fx/proxy.py
+++ torch/fx/proxy.py
@@ -334,9 +334,9 @@ class Proxy:
         if torch.overrides.is_tensor_method_or_property(orig_method):
             return tracer.create_proxy('call_method', orig_method.__name__, args, kwargs)
         else:
-            if orig_method.__name__ in {"cond", "map"}:
-                # TODO: symbolically trace control flow ops
-                raise RuntimeError("Unable to symbolically trace control flow ops")
+            if isinstance(orig_method, torch._ops.PyOperator):
+                # TODO: Define how to symbolically trace PyOperators
+                raise RuntimeError("Unable to symbolically trace PyOperators")
             return tracer.create_proxy('call_function', orig_method, args, kwargs,
                                        name=tracer.graph._target_to_str(orig_method.__name__))
 
