027509ed9bd60a5966e6b2d995ecd5d5c25364b2
Jane Xu <janeyx@fb.com>
2023-01-18 20:03:08 +0000

    [optim][rprop] group tensors in foreach to maximize perf (#92372)
    this one had a few more for loops than i was expecting
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92372
    Approved by: https://github.com/albanD

--- torch/optim/rprop.py
+++ torch/optim/rprop.py
@@ -2,6 +2,7 @@ import torch
 from torch import Tensor
 from .optimizer import Optimizer, _use_grad_for_differentiable, _differentiable_doc, _maximize_doc
 from typing import List, Optional
+from torch.utils._foreach_utils import _group_tensors_by_device_and_dtype
 
 __all__ = ["Rprop", "rprop"]
 
@@ -281,42 +282,44 @@ def _multi_tensor_rprop(
 
     assert not differentiable, "_foreach ops don't support autograd"
 
-    # Handle complex params
-    def _view_complex_as_real(tensor_list):
-        return [
-            torch.view_as_real(t) if torch.is_complex(t) else t for t in tensor_list
-        ]
+    grouped_tensors = _group_tensors_by_device_and_dtype([params, grads, prevs, step_sizes])
+    for grouped_params, grouped_grads, grouped_prevs, grouped_step_sizes in grouped_tensors.values():
+        # Handle complex params
+        def _view_complex_as_real(tensor_list):
+            return [
+                torch.view_as_real(t) if torch.is_complex(t) else t for t in tensor_list
+            ]
 
-    grads = _view_complex_as_real(grads)
-    prevs = _view_complex_as_real(prevs)
-    params = _view_complex_as_real(params)
-    step_sizes = _view_complex_as_real(step_sizes)
+        grouped_grads = _view_complex_as_real(grouped_grads)
+        grouped_prevs = _view_complex_as_real(grouped_prevs)
+        grouped_params = _view_complex_as_real(grouped_params)
+        grouped_step_sizes = _view_complex_as_real(grouped_step_sizes)
 
-    if maximize:
-        grads = torch._foreach_neg(grads)
+        if maximize:
+            grouped_grads = torch._foreach_neg(grouped_grads)
 
-    signs = torch._foreach_mul(grads, prevs)
-    signs = [s.sign() for s in signs]
-    for sign in signs:
-        sign[sign.gt(0)] = etaplus
-        sign[sign.lt(0)] = etaminus
-        sign[sign.eq(0)] = 1
+        signs = torch._foreach_mul(grouped_grads, grouped_prevs)
+        signs = [s.sign() for s in signs]
+        for sign in signs:
+            sign[sign.gt(0)] = etaplus
+            sign[sign.lt(0)] = etaminus
+            sign[sign.eq(0)] = 1
 
-    # update stepsizes with step size updates
-    torch._foreach_mul_(step_sizes, signs)
-    for step_size in step_sizes:
-        step_size.clamp_(step_size_min, step_size_max)
+        # update stepsizes with step size updates
+        torch._foreach_mul_(grouped_step_sizes, signs)
+        for step_size in grouped_step_sizes:
+            step_size.clamp_(step_size_min, step_size_max)
 
-    # for dir<0, dfdx=0
-    # for dir>=0 dfdx=dfdx
-    grads = list(grads)
-    for i in range(len(grads)):
-        grads[i] = grads[i].clone(memory_format=torch.preserve_format)
-        grads[i][signs[i].eq(etaminus)] = 0
+        # for dir<0, dfdx=0
+        # for dir>=0 dfdx=dfdx
+        grouped_grads = list(grouped_grads)
+        for i in range(len(grouped_grads)):
+            grouped_grads[i] = grouped_grads[i].clone(memory_format=torch.preserve_format)
+            grouped_grads[i][signs[i].eq(etaminus)] = 0
 
-    # update parameters
-    grad_signs = [grad.sign() for grad in grads]
-    torch._foreach_addcmul_(params, grad_signs, step_sizes, value=-1)
+        # update parameters
+        grad_signs = [grad.sign() for grad in grouped_grads]
+        torch._foreach_addcmul_(grouped_params, grad_signs, grouped_step_sizes, value=-1)
 
-    for i in range(len(prevs)):
-        prevs[i].copy_(grads[i])
+        for i in range(len(grouped_prevs)):
+            grouped_prevs[i].copy_(grouped_grads[i])
