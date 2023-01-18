b92a7afed9bb86a7cf04ef4ef994c122c7a75103
Edward Z. Yang <ezyang@meta.com>
2023-01-17 21:57:29 -0500

    Reclassify some dynamic aot_eager failures as static failures (#92376)
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92376
    Approved by: https://github.com/Chillee

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -71,13 +71,18 @@ CI_SKIP_AOT_EAGER_TRAINING = [
     *CI_SKIP_AOT_EAGER_INFERENCE,
     # TorchBench
     "Background_Matting",  # fp64_OOM
+    "hf_T5_base",  # fp64_OOM
+    "mobilenet_v2_quantized_qat",  # fp64_OOM
+    "resnet50_quantized_qat",  # fp64_OOM
     "moco",
     "pytorch_struct",
+    "tacotron2",  # AssertionError: Deduped args out of bounds
     "vision_maskrcnn",
     # Huggingface
     "AlbertForMaskedLM",  # OOM
     "AlbertForQuestionAnswering",  # OOM
     "BigBird",
+    "DebertaV2ForQuestionAnswering",  # OOM
     "M2M100ForConditionalGeneration",  # OOM
     "PegasusForConditionalGeneration",  # OOM
     "XGLMForCausalLM",  # OOM
@@ -86,20 +91,15 @@ CI_SKIP_AOT_EAGER_TRAINING = [
     # TIMM
     "cait_m36_384",  # fp64_OOM
     "convit_base",  # fp64_OOM
+    "levit_128",  # Accuracy (patch_embed.0.c.weight.grad)
     "mobilevit_s",  # Accuracy
+    "sebotnet33ts_256",  # Accuracy (stem.conv1.conv.weight.grad)
     "xcit_large_24_p8_224",  # fp64_OOM
 ]
 
 CI_SKIP_AOT_EAGER_DYNAMIC_TRAINING = [
     *CI_SKIP_AOT_EAGER_TRAINING,
-    "hf_T5_base",  # fp64_OOM
-    "mobilenet_v2_quantized_qat",  # setStorage
-    "resnet50_quantized_qat",  # setStorage
-    "tacotron2",  # aten._thnn_fused_lstm_cell.default
-    "DebertaV2ForQuestionAnswering",  # OOMs (but on CI only; graph breaks?)
     "crossvit_9_240",  # torch._C._nn.upsample_bicubic2d
-    "levit_128",  # Cannot call sizes() on tensor with symbolic sizes/strides
-    "sebotnet33ts_256",  # Accuracy failed for key name stem.conv1.conv.weight.grad
     "twins_pcpvt_base",  # timeout
 ]
 
