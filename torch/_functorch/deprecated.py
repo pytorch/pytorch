25870770e2a9a4f14f364b7838914f0ec24eee7a
Richard Zou <zou3519@gmail.com>
2023-01-17 17:46:11 -0800

    Update base for Update on "Deprecate torch.nn.utils.stateless.functional_call"
    This PR:
    - Updates the docs to say it is deprecated
    - Raises a UserWarning
    - Changes most of the callsites inside PyTorch to use
    torch.func.functional_call, minus the test_stateless testing.
    
    The motivation behind this is that we can now align behind a single
    functional_call API in PyTorch.
    
    Test Plan:
    - existing tests
    
    [ghstack-poisoned]

--- torch/_functorch/deprecated.py
+++ torch/_functorch/deprecated.py
@@ -1,13 +1,20 @@
 import torch._functorch.vmap as _vmap_impl
 import torch._functorch.eager_transforms as _impl
 import torch._functorch.make_functional as _nn_impl
+from torch._functorch.vmap import in_dims_t, out_dims_t
+from torch._functorch.eager_transforms import argnums_t
+import torch.nn as nn
 import textwrap
+from typing import Any, Callable, Optional, Tuple, Union
 import warnings
 
 """
 The APIs in this file are exposed as `functorch.*`. They are thin wrappers
 around the torch.func.* APIs that have deprecation warnings -- we're trying
 to move people to the torch.func.* equivalents.
+
+NB: We don't use *args, **kwargs in the signatures because that changes the
+documentation.
 """
 
 def get_warning(api, new_api=None, replace_newlines=False):
@@ -30,78 +37,85 @@ def warn_deprecated(api, new_api=None):
     warnings.warn(warning, stacklevel=2)
 
 
-def setup_docs_annotations(functorch_api, torch_func_api=None, new_api_name=None):
+def setup_docs(functorch_api, torch_func_api=None, new_api_name=None):
     api_name = functorch_api.__name__
     if torch_func_api is None:
         torch_func_api = getattr(_impl, api_name)
 
-    functorch_api.__annotations__ = torch_func_api.__annotations__
-
-    warning = get_warning(api_name, new_api_name)
+    warning = get_warning(api_name, new_api_name) + "\n"
     warning_note = "\n.. warning::\n\n" + textwrap.indent(warning, "    ")
     warning_note = textwrap.indent(warning_note, "    ")
-    functorch_api.__doc__ = torch_func_api.__doc__ + warning_note
+    functorch_api.__doc__ = warning_note + torch_func_api.__doc__
 
-def vmap(*args, **kwargs):
+def vmap(
+        func: Callable,
+        in_dims: in_dims_t = 0,
+        out_dims: out_dims_t = 0,
+        randomness: str = 'error',
+        *,
+        chunk_size=None) -> Callable:
     warn_deprecated('vmap', 'torch.vmap')
-    return _vmap_impl.vmap(*args, **kwargs)
+    return _vmap_impl.vmap(func, in_dims, out_dims, randomness, chunk_size=chunk_size)
 
-def grad(*args, **kwargs):
+def grad(func: Callable, argnums: argnums_t = 0, has_aux: bool = False) -> Callable:
     warn_deprecated('grad')
-    return _impl.grad(*args, **kwargs)
+    return _impl.grad(func, argnums, has_aux)
 
-def grad_and_value(*args, **kwargs):
+def grad_and_value(func: Callable, argnums: argnums_t = 0, has_aux: bool = False) -> Callable:
     warn_deprecated('grad_and_value')
-    return _impl.grad_and_value(*args, **kwargs)
+    return _impl.grad_and_value(func, argnums, has_aux)
 
-def vjp(*args, **kwargs):
+def vjp(func: Callable, *primals, has_aux: bool = False):
     warn_deprecated('vjp')
-    return _impl.vjp(*args, **kwargs)
+    return _impl.vjp(func, *primals, has_aux=has_aux)
 
-def jvp(*args, **kwargs):
+def jvp(func: Callable, primals: Any, tangents: Any, *, strict: bool = False, has_aux: bool = False):
     warn_deprecated('jvp')
-    return _impl.jvp(*args, **kwargs)
+    return _impl.jvp(func, primals, tangents, strict=strict, has_aux=has_aux)
 
-def jacrev(*args, **kwargs):
+def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False,
+           chunk_size: Optional[int] = None,
+           _preallocate_and_copy=False):
     warn_deprecated('jacrev')
-    return _impl.jacrev(*args, **kwargs)
+    return _impl.jacrev(func, argnums, has_aux=has_aux, chunk_size=chunk_size,
+                        _preallocate_and_copy=_preallocate_and_copy)
 
-def jacfwd(*args, **kwargs):
+def jacfwd(func: Callable, argnums: argnums_t = 0, has_aux: bool = False, *, randomness: str = "error"):
     warn_deprecated('jacfwd')
-    return _impl.jacfwd(*args, **kwargs)
+    return _impl.jacfwd(func, argnums, has_aux, randomness=randomness)
 
-def hessian(*args, **kwargs):
+def hessian(func, argnums=0):
     warn_deprecated('hessian')
-    return _impl.hessian(*args, **kwargs)
+    return _impl.hessian(func, argnums=argnums)
 
-def functionalize(*args, **kwargs):
+def functionalize(func: Callable, *, remove: str = 'mutations') -> Callable:
     warn_deprecated('functionalize')
-    return _impl.functionalize(*args, **kwargs)
+    return _impl.functionalize(func, remove=remove)
 
-def make_functional(*args, **kwargs):
+def make_functional(model: nn.Module, disable_autograd_tracking: bool = False):
     warn_deprecated('make_functional', 'torch.func.functional_call')
-    return _nn_impl.make_functional(*args, **kwargs)
+    return _nn_impl.make_functional(model, disable_autograd_tracking)
 
-def make_functional_with_buffers(*args, **kwargs):
+def make_functional_with_buffers(model: nn.Module, disable_autograd_tracking: bool = False):
     warn_deprecated('make_functional_with_buffers', 'torch.func.functional_call')
-    return _nn_impl.make_functional_with_buffers(*args, **kwargs)
+    return _nn_impl.make_functional_with_buffers(model, disable_autograd_tracking)
 
-def combine_state_for_ensemble(*args, **kwargs):
+def combine_state_for_ensemble(models):
     warn_deprecated('combine_state_for_ensemble', 'torch.func.stack_module_state')
-    return _nn_impl.combine_state_for_ensemble(*args, **kwargs)
+    return _nn_impl.combine_state_for_ensemble(models)
 
-setup_docs_annotations(vmap, _vmap_impl.vmap, 'torch.vmap')
-setup_docs_annotations(grad)
-setup_docs_annotations(grad_and_value)
-setup_docs_annotations(vjp)
-setup_docs_annotations(jvp)
-setup_docs_annotations(jacrev)
-setup_docs_annotations(jacfwd)
-setup_docs_annotations(hessian)
-setup_docs_annotations(functionalize)
-setup_docs_annotations(make_functional, _nn_impl.make_functional,
-                       'torch.func.functional_call')
-setup_docs_annotations(make_functional_with_buffers, _nn_impl.make_functional,
-                       'torch.func.functional_call')
-setup_docs_annotations(combine_state_for_ensemble, _nn_impl.combine_state_for_ensemble,
-                       'torch.func.stack_module_state')
+setup_docs(vmap, _vmap_impl.vmap, 'torch.vmap')
+setup_docs(grad)
+setup_docs(grad_and_value)
+setup_docs(vjp)
+setup_docs(jvp)
+setup_docs(jacrev)
+setup_docs(jacfwd)
+setup_docs(hessian)
+setup_docs(functionalize)
+setup_docs(make_functional, _nn_impl.make_functional,
+           'torch.func.functional_call')
+setup_docs(make_functional_with_buffers, _nn_impl.make_functional,
+           'torch.func.functional_call')
+setup_docs(combine_state_for_ensemble, _nn_impl.combine_state_for_ensemble,
+           'torch.func.stack_module_state')
