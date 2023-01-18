664e89de6f6734d5f0834c169be49fa6383b8cda
Michael Voznesensky <voznesenskym@gmail.com>
2023-01-18 18:55:29 +0000

    missing parens

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2114,7 +2114,7 @@ class CommonTemplate:
 
         self.common(fn, (torch.randn(4), torch.randn(4)), check_lowp=False)
 
-    @requires_multigpu
+    @requires_multigpu()
     def test_recompile_on_index(self):
         torch.set_float32_matmul_precision("high")
 
