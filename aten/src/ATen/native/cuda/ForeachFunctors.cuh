71a8e66d1d9d0ac874e9af9d5c12772298364735
Masaki Kozuki <mkozuki@nvidia.com>
2023-01-19 14:23:22 -0800

    always bump version counter
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- aten/src/ATen/native/cuda/ForeachFunctors.cuh
+++ aten/src/ATen/native/cuda/ForeachFunctors.cuh
@@ -11,9 +11,7 @@ namespace {
 // rel: https://github.com/pytorch/pytorch/blob/9cf84347767c8abb8feba18a9a1baba321eeb8b9/tools/autograd/gen_inplace_or_view_type.py#L481-L482
 inline void maybe_increment_version(TensorList tensors) {
   for (const auto & t : tensors) {
-    if (at::GradMode::is_enabled()) {
-      t.unsafeGetTensorImpl()->bump_version();
-    }
+    t.unsafeGetTensorImpl()->bump_version();
   }
 }
 
