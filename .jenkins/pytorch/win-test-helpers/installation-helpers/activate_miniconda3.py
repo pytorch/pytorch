4ccc481bd59a73c6c833cf422a501cb421c82663
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-01-19 10:05:09 -0600

    Add files via upload

--- .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.py
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.py
@@ -41,6 +41,8 @@ if install_fresh_conda == '1':
                 '--output ' + tmp_dir_win + '\\Miniconda3-latest-Windows-x86_64.exe', shell=True)
         result.check_returncode()
 
+        subprocess.run('ls tmp_dir_win', shell=True)
+
         subprocess.Popen('Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe ' +
             '/RegisterPython=0 /S /AddToPath=0 /D=' + conda_parent_dir + '\\Miniconda3', cwd=tmp_dir_win)
 
