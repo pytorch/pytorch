61c0f02bd0834e8de340ef949efa4e4cb8a40f50
Edward Z. Yang <ezyang@fb.com>
2023-01-19 21:08:29 +0000

    Update on "Delete get_pyobj() entirely"
    Opt for the shorter and more direct node attribute access.
    
    I need to do this because I'm going to publicly document
    SymInt and SymFloat but I don't want to doc get_pyobj().
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -68,9 +68,7 @@ class GraphLowering(torch.fx.Interpreter):
             size, stride = self._shape_env.create_symbolic_sizes_strides(ex)
 
         size = [i.node.expr if isinstance(i, torch.SymInt) else i for i in size]
-        stride = [
-            i.node.expr if isinstance(i, torch.SymInt) else i for i in stride
-        ]
+        stride = [i.node.expr if isinstance(i, torch.SymInt) else i for i in stride]
         return size, stride
 
     def static_sizes_strides(self, ex: torch.Tensor):
