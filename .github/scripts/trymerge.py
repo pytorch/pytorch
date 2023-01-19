774f15f333bdd6b7729fe13736cd39bdaccc0d76
Nikita Shulga <nshulga@meta.com>
2023-01-19 15:33:10 -0800

    Revert "Move check_label ci to mergebot (#92309)"
    This reverts commit 190f7803f5d90d027f331eaf48ef5fa63f14737a.
    
    As it broke revert workflow, see https://github.com/pytorch/pytorch/actions/runs/3963235531/jobs/6790838677

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -14,7 +14,6 @@ from typing import (
     Any,
     Callable,
     Dict,
-    Iterable,
     List,
     NamedTuple,
     Optional,
@@ -34,7 +33,6 @@ from gitutils import (
     get_git_repo_dir,
     patterns_to_regex,
 )
-from export_pytorch_labels import get_pytorch_labels
 from trymerge_explainer import (
     TryMergeExplainer,
     get_revert_message,
@@ -423,16 +421,6 @@ RE_DIFF_REV = re.compile(r'^Differential Revision:.+?(D[0-9]+)', re.MULTILINE)
 CIFLOW_LABEL = re.compile(r"^ciflow/.+")
 CIFLOW_TRUNK_LABEL = re.compile(r"^ciflow/trunk")
 MERGE_RULE_PATH = Path(".github") / "merge_rules.yaml"
-
-BOT_AUTHORS = ["github-actions", "pytorchmergebot", "pytorch-bot"]
-
-LABEL_ERR_MSG_TITLE = "This PR needs a label"
-LABEL_ERR_MSG = (
-    f"# {LABEL_ERR_MSG_TITLE}\n"
-    "If your changes are user facing and intended to be a part of release notes, please use a label starting with `release notes:`.\n\n"  # noqa: E501  pylint: disable=line-too-long
-    "If not, please add the `topic: not user facing` label.\n\n"
-    "For more information, see https://github.com/pytorch/pytorch/wiki/PyTorch-AutoLabel-Bot#why-categorize-for-release-notes-and-how-does-it-work."  # noqa: E501  pylint: disable=line-too-long
-)
 
 
 def _fetch_url(url: str, *,
@@ -1409,34 +1397,10 @@ def validate_land_time_checks(org: str, project: str, commit: str) -> None:
 def has_label(labels: List[str], pattern: Pattern[str] = CIFLOW_LABEL) -> bool:
     return len(list(filter(pattern.match, labels))) > 0
 
-def get_release_notes_labels() -> List[str]:
-    return [label for label in get_pytorch_labels() if label.lstrip().startswith("release notes:")]
-
-def has_required_labels(pr: GitHubPR) -> bool:
-    pr_labels = pr.get_labels()
-    # Check if PR is not user facing
-    is_not_user_facing_pr = any(label.strip() == "topic: not user facing" for label in pr_labels)
-    return is_not_user_facing_pr or any(label.strip() in get_release_notes_labels() for label in pr_labels)
-
-def delete_comment(comment_id: int) -> None:
-    url = f"https://api.github.com/repos/pytorch/pytorch/issues/comments/{comment_id}"
-    _fetch_url(url, method="DELETE")
-
-def is_label_err_comment(comment: GitHubComment) -> bool:
-    return comment.body_text.lstrip(" #").startswith(LABEL_ERR_MSG_TITLE) and comment.author_login in BOT_AUTHORS
-
-def delete_all_label_err_comments(pr: GitHubPR) -> None:
-    for comment in pr.get_comments():
-        if is_label_err_comment(comment):
-            delete_comment(comment.database_id)
-
-def add_label_err_comment(pr: GitHubPR) -> None:
-    # Only make a comment if one doesn't exist already
-    if not any(is_label_err_comment(comment) for comment in pr.get_comments()):
-        gh_post_pr_comment(pr.org, pr.project, pr.pr_num, LABEL_ERR_MSG)
-
-def categorize_checks(check_runs: Dict[str, JobCheckState],
-                      required_checks: Iterable[str]) -> Tuple[List[Tuple[str, Optional[str]]], List[Tuple[str, Optional[str]]]]:
+def categorize_checks(
+    check_runs: JobNameToStateDict,
+    required_checks: List[str],
+) -> Tuple[List[Tuple[str, Optional[str]]], List[Tuple[str, Optional[str]]]]:
     pending_checks: List[Tuple[str, Optional[str]]] = []
     failed_checks: List[Tuple[str, Optional[str]]] = []
 
@@ -1491,11 +1455,6 @@ def merge(pr_num: int, repo: GitRepo,
     # jobs. If there's missing approval, a RuntimeError will be raised
     # here to stop the merge process right away
     find_matching_merge_rule(pr, repo, skip_mandatory_checks=True)
-
-    if not has_required_labels(pr):
-        raise RuntimeError(LABEL_ERR_MSG.lstrip(" #"))
-    else:
-        delete_all_label_err_comments(pr)
 
     if land_checks and not dry_run:
         land_check_commit = pr.create_land_time_check_branch(
