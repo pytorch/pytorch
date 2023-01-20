28d2646b9abc959ebf5065eeddc7cba6d262fcb7
Michael Voznesensky <voznesenskym@gmail.com>
2023-01-20 05:03:32 +0000

    Torch package support in dynamo (#91821)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91821
    Approved by: https://github.com/suo, https://github.com/malfet

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -1,3 +1,4 @@
+import builtins
 import collections
 import logging
 import math
@@ -97,6 +98,17 @@ class GuardBuilder(GuardBuilderBase):
         else:
             scope = dict()
         self.scope: Dict[str, object] = scope
+        self.scope["__builtins__"] = builtins.__dict__.copy()
+        for (
+            name,
+            package_module,
+        ) in torch.package.package_importer._package_imported_modules.items():
+            name = name.replace(">", "_").replace("<", "_").replace(".", "_dot_")
+            # Write the package module into the scope so that we can import it
+            self.scope["__builtins__"][name] = package_module  # type: ignore[index]
+            # Write the demangled name to the scope so that we can use it
+            self.scope[name] = package_module
+
         self.argnames: List[str] = []
         # Code is python expression strings generated for each guard
         self.code: List[str] = []
