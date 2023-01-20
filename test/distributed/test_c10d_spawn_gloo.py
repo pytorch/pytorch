56325feeb9d651dda78dd0d67f38794f7c9d8b9e
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-01-20 04:38:07 +0000

    [ROCm] Fix distributed tests failure
    Distributed tests fails due to  AttributeError: 'torch._C._distributed_c10d.ProcessGroup'
    object has no attribute '_set_backend' , when running distributed/test_c10d_spawn_gloo.py
    This leads to tests not progressing resulting in hang.
    Check if the attribute '_set_backend' exists before using it.
    
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- test/distributed/test_c10d_spawn_gloo.py
+++ test/distributed/test_c10d_spawn_gloo.py
@@ -35,8 +35,11 @@ if sys.version_info < (3, 9):
             # set process group backends manually
             c10d.init_process_group(backend="gloo", store=store, rank=rank, world_size=world_size)
             pg = c10d.distributed_c10d._get_default_group()
-            pg._set_backend(torch.device("cpu"), c10d.Backend.GLOO, backend)
-            pg._set_backend(torch.device("cuda"), c10d.Backend.GLOO, backend)
+            try:
+                pg._set_backend(torch.device("cpu"), c10d.Backend.GLOO, backend)
+                pg._set_backend(torch.device("cuda"), c10d.Backend.GLOO, backend)
+            except AttributeError:
+                print('Attribute _set_backend not found')
             return pg
 
         @sandcastle_skip_if(not TEST_MULTIGPU, "At least 2 CUDA GPUS needed")
