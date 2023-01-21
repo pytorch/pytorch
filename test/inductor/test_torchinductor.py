3f4b98c681d1b610fd5701dc179e7160469b91ca
Michael Voznesensky <voznesenskym@gmail.com>
2023-01-21 11:26:53 -0800

    Update test_torchinductor.py

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -6446,21 +6446,22 @@ class ExprPrinterTests(TestCase):
             self.assertEqual(texpr(expr), result)
 
 
-class RNNTest(TestCase):
-    class Model(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.gru = torch.nn.GRU(16, 16, batch_first=True)
+if HAS_CUDA:
+    class RNNTest(TestCase):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.gru = torch.nn.GRU(16, 16, batch_first=True)
 
-        def forward(self, x):
-            return self.gru(x)
+            def forward(self, x):
+                return self.gru(x)
 
-    def test_rnn_compile_safe(self):
-        device = torch.device("cuda")
-        model = RNNTest.Model().to(device)
-        model = torch._dynamo.optimize("inductor")(model)
-        x = torch.rand(1024, 20, 16).to(device)
-        model(x)
+        def test_rnn_compile_safe(self):
+            device = torch.device("cuda")
+            model = RNNTest.Model().to(device)
+            model = torch._dynamo.optimize("inductor")(model)
+            x = torch.rand(1024, 20, 16).to(device)
+            model(x)
 
 
 if __name__ == "__main__":
