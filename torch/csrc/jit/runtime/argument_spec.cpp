eb44067382d9212b235378692b678ec54f263143
Aaron Gokaslan <aaronGokaslan@gmail.com>
2023-01-21 15:51:30 -0500

    Revert false positives

--- torch/csrc/jit/runtime/argument_spec.cpp
+++ torch/csrc/jit/runtime/argument_spec.cpp
@@ -172,7 +172,7 @@ ArgumentSpec ArgumentSpecCreator::create(bool with_grad, const Stack& input)
         // NOLINTNEXTLINE(clang-analyzer-core.uninitialized.Assign)
         const IValue* iv = stack[stack_top]++;
         AT_ASSERT(iv->isObject(), "Expected Object but got ", iv->tagKind());
-        auto obj_ptr = iv->toObjectRef().slots().data();
+        auto obj_ptr = &iv->toObjectRef().slots()[0];
         // push list of object elements to the stack
         stack[++stack_top] = obj_ptr;
       } break;
