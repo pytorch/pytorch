cc9c60f0f5ff11b8c9238c1aea09f6bde8cbed5c
cyy <cyyever@outlook.com>
2023-01-22 14:50:15 +0800

    disable dangerous move

--- torch/csrc/autograd/functions/tensor.cpp
+++ torch/csrc/autograd/functions/tensor.cpp
@@ -142,8 +142,7 @@ auto CopySlices::apply(variable_list&& inputs) -> variable_list {
       AT_ASSERT(res[i].defined());
       if (i == 0) {
         grad_slice.copy_(res[i]);
-        // NOLINTNEXTLINE(clang-analyzer-cplusplus.Move)
-        grad_inputs[i] = std::move(result); // NOLINT(bugprone-use-after-move)
+        grad_inputs[i] = result;
       } else {
         grad_inputs[i] = std::move(res[i]);
       }
