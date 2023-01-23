ee9d8898b60ac91b0f26199cce7b94664cdfaa88
Horace He <chilli@fb.com>
2023-01-23 23:30:57 +0000

    Some more inductor fixes for symbolic shapes (#92867)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92867
    Approved by: https://github.com/ezyang

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -87,7 +87,7 @@ def _warn_tf32_disabled():
         and torch.cuda.get_device_capability() >= (8, 0)
     ):
         warnings.warn(
-            "TensorFloat32 tensor cores for float32 matrix multiplication available but not enabled."
+            "TensorFloat32 tensor cores for float32 matrix multiplication available but not enabled. "
             "Consider setting `torch.set_float32_matmul_precision('high')` for better performance."
         )
 
