[FSDP] Fix `no_sync()`, `use_orig_params=True`, mixed precision, sharded (#92874)
When there is an original parameter with 1D shape that is fully assigned to one rank, then its `param.shape == view.shape` in `_use_unsharded_grad_views()`. In that case, we still want to check whether `param.dtype == view.dtype` and bypass as necessary.

The previous PR had an additional `and not self.uses_sharded_strategy` because the unit test did not require the check for sharded strategies, and I was conservatively adding a minimal fix. This was happenstance and because there was no 1D parameter fully assigned to one rank. Including the bias in the linear layer achieves that case, and removing the `and not self.uses_sharded_strategy` is necessary.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/92874
Approved by: https://github.com/zhaojuanmao

test/distributed/fsdp/test_fsdp_use_orig_params.py
@@ -1073,7 +1073,7 @@ class TestFSDPUseOrigParamsNoSync(FSDPTest):
         )
 
     def _test_no_sync_correctness(self, sharding_strategy: ShardingStrategy):
-        model = nn.Linear(3, 3, bias=False, device="cuda")
+        model = nn.Linear(3, 3, device="cuda")
         fsdp_kwargs = {
             "sharding_strategy": sharding_strategy,
         }
@@ -1135,7 +1135,9 @@ class TestFSDPUseOrigParamsNoSync(FSDPTest):
             param.grad.detach().clone() for param in model_use_flat_params.parameters()
         ]
         ref_grads_use_orig_params = [
-            param.grad.detach().clone() for param in model_use_orig_params.parameters()
+            param.grad.detach().clone()
+            for param in model_use_orig_params.parameters()
+            if param.grad is not None
         ]
 
         # Run a forward/backward in `no_sync()`
@@ -1159,7 +1161,9 @@ class TestFSDPUseOrigParamsNoSync(FSDPTest):
             param.grad.detach().clone() for param in model_use_flat_params.parameters()
         ]
         grads_use_orig_params = [
-            param.grad.detach().clone() for param in model_use_orig_params.parameters()
+            param.grad.detach().clone()
+            for param in model_use_orig_params.parameters()
+            if param.grad is not None
         ]
         for grad, ref_grad in zip(grads_use_flat_params, ref_grads_use_flat_params):
             torch.testing.assert_close(grad, 2 * ref_grad)
@@ -1184,7 +1188,7 @@ class TestFSDPUseOrigParamsNoSync(FSDPTest):
         )
 
     def _test_no_sync_mixed_precision(self, sharding_strategy: ShardingStrategy):
-        model = nn.Linear(3, 3, bias=False, device="cuda")
+        model = nn.Linear(3, 3, device="cuda")
         mixed_precision = MixedPrecision(
             param_dtype=torch.float16,
             reduce_dtype=torch.float32,