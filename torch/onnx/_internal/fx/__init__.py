1248f22fc9589ea45553795aa2aa4e7485e1ad19
Wei-Sheng Chin <wschin@outlook.com>
2023-01-24 01:57:18 +0000

    Remove private functions

--- torch/onnx/_internal/fx/__init__.py
+++ torch/onnx/_internal/fx/__init__.py
@@ -1,15 +1,11 @@
 from .export import (
-    _export,
-    _ONNX_FRIENDLY_DECOMPOSITION_TABLE,
     export,
     export_without_kwargs,
     export_without_parameters_and_buffers,
 )
 
 __all__ = [
-    "export_without_kwargs",
-    "_export",
-    "_ONNX_FRIENDLY_DECOMPOSITION_TABLE",
     "export",
+    "export_without_kwargs",
     "export_without_parameters_and_buffers",
 ]
