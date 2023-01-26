db812233cb1c7df03cff4dc43e3efa597c578690
kshitij12345 <kshitijkalambarkar@gmail.com>
2023-01-26 16:45:08 +0000

    make linter happy

--- test/cpp/api/modulelist.cpp
+++ test/cpp/api/modulelist.cpp
@@ -302,6 +302,7 @@ TEST_F(ModuleListTest, RangeBasedForLoop) {
 }
 
 TEST_F(ModuleListTest, InvalidAt) {
-  torch::nn::ModuleList m( torch::nn::Linear(1,2) );
-  ASSERT_THROWS_WITH(m->at<torch::nn::Dropout2dImpl>(0), "Unable to cast module");
+  torch::nn::ModuleList m(torch::nn::Linear(1, 2));
+  ASSERT_THROWS_WITH(
+      m->at<torch::nn::Dropout2dImpl>(0), "Unable to cast module");
 }
