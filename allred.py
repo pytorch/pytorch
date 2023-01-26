087cb1c304545304e27d656f20b5ba9031a40f39
Will Constable <whc@fb.com>
2023-01-26 23:58:06 +0000

    allred.py demo script (not for land)
    [ghstack-poisoned]

---
+++ allred.py
@@ -0,0 +1,56 @@
+import os
+import torch
+import torch.distributed as dist
+from functools import partial
+from torch._dynamo.utils import same
+from torch._inductor.compile_fx import compile_fx as inductor_compile_fx
+from torch.fx.experimental.proxy_tensor import make_fx
+from torch.distributed.distributed_c10d import _get_default_group
+from torch._C._distributed_c10d import _register_process_group
+from torch._dispatch.python import enable_python_dispatcher
+
+# LOL if you don't remember to import this, then the op isn't registered and it hits
+# the no-op C++ kernel that i am forced to implement despite not using it
+import torch.distributed.traceable_collectives
+
+def matmul_cat_col(a, b, c, d, e, f, *, pg_id):
+    x = torch.matmul(a, b)
+    y = torch.matmul(c, d)
+    z = torch.cat((x, y))
+    ar = torch.ops.aten.all_reduce(z, group_id=pg_id, reduce_op="sum")
+    g = torch.matmul(e, f)
+    out = torch.add(ar, g.repeat(2, 1))
+    return (out, )
+
+def compile(func, example_inputs):
+    graph = make_fx(func)(*example_inputs)
+    return inductor_compile_fx(graph, example_inputs)
+
+if __name__ == '__main__':
+    os.environ["RANK"] = os.getenv("RANK", "0")
+    os.environ["WORLD_SIZE"] = os.getenv("WORLD_SIZE", "1")
+    os.environ["MASTER_ADDR"] = os.getenv("MASTER_ADDR", "localhost")
+    os.environ["MASTER_PORT"] = os.getenv("MASTER_PORT", "12345")
+    rank = int(os.getenv("RANK"))
+    world_size = int(os.getenv("WORLD_SIZE"))
+    torch.cuda.set_device(rank)
+    dist.init_process_group(backend='nccl')
+
+    torch._inductor.config.debug = True
+
+    # this is a useless thing to do for the simple case of using default pg.
+    # however, i did it to demonstrate the API proposed, whereby pg as int is passed
+    # to collective APIs and pg object is recovered in execution layer
+    pg = _get_default_group()
+    pg_id = _register_process_group(pg)
+    matmul_cat_col = partial(matmul_cat_col, pg_id=pg_id)
+    inputs = (torch.ones(4, 4, device="cuda") + rank,) * 6
+
+    # non-ideally, i seem to need to enable this at user level in order to construct a torchdispatch subclass
+    # inside py registered collective ops
+    with enable_python_dispatcher():
+        correct_out = matmul_cat_col(*inputs)
+        compiled_matmul_cat_col = compile(matmul_cat_col, inputs)
+        inductor_out = compiled_matmul_cat_col(*inputs)
+        print(f"rank {rank}: {correct_out}, {inductor_out}")
+        assert same(correct_out, inductor_out)
