pytorch/pytorch

commit 050c97798ffbbd99cdd76acd25c5d083fbe1b52b

unify reduction types from different operators: scatter, scatter_reduce, segment_reduce (#91499)
The target of this PR is to unify `ReductionType` for reduce operators so that we have the same set of reduce utils for `init`, or `update` for vectorization.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/91499
Approved by: https://github.com/ngimel

aten/src/ATen/native/ReductionType.h
@@ -1,0 +1,40 @@
+#pragma once
+
+#include <c10/core/Scalar.h>
+
+namespace at { namespace native {
+
+enum ReductionType {MAX, MEAN, MIN, SUM, PROD};
+
+static inline ReductionType get_reduction_enum(const c10::string_view& reduce) {
+  if (reduce == "amax") {
+    return ReductionType::MAX;
+  } else if (reduce == "mean") {
+    return ReductionType::MEAN;
+  } else if (reduce == "amin") {
+    return ReductionType::MIN;
+  } else if (reduce == "sum") {
+    return ReductionType::SUM;
+  } else if (reduce == "prod") {
+    return ReductionType::PROD;
+  } else {
+    TORCH_CHECK(false, "reduce argument must be either sum, prod, mean, amax or amin, got ", reduce);
+  }
+}
+
+// used for `scatter_reduce`, old options for BC.
+static inline ReductionType get_operator_enum(const c10::string_view reduce, bool use_new_options) {
+  if (use_new_options) {
+    return get_reduction_enum(reduce);
+  } else {
+    if (reduce == "add") {
+      return ReductionType::SUM;
+    } else if (reduce == "multiply") {
+      return ReductionType::PROD;
+    } else {
+      TORCH_CHECK(false, "reduce argument must be either add or multiply.")
+    }
+  }
+}
+
+}} // at::native

test/test_segment_reductions.py
@@ -18,17 +18,17 @@ from torch.testing._internal.common_utils import (
 )
 
 
-reductions = ["max", "mean", "min", "sum", "prod"]
+reductions = ["amax", "mean", "amin", "sum", "prod"]
 
 
 def get_default_value(initial_value, reduction):
     if initial_value is not None:
         return initial_value
-    if reduction == "max":
+    if reduction == "amax":
         return -float("Inf")
     elif reduction == "mean":
         return float("nan")
-    elif reduction == "min":
+    elif reduction == "amin":
         return float("Inf")
     elif reduction == "sum":
         return 0.0
@@ -133,13 +133,13 @@ class TestSegmentReductions(TestCase):
                 check_backward = True if initial is not None else False
                 initial_value = initial
                 default_value = get_default_value(initial_value, reduction)
-                if reduction == "max":
+                if reduction == "amax":
                     expected_result = [1, float("nan"), 5, default_value]
                     expected_grad = [1, 1, 0, 0, 0.5, 0.5]
                 elif reduction == "mean":
                     expected_result = [1, float("nan"), 4.666, default_value]
                     expected_grad = [1.0, 0.5, 0.5, 0.333, 0.333, 0.333]
-                elif reduction == "min":
+                elif reduction == "amin":
                     if initial is not None:
                         initial_value = 1000  # some high number
                         default_value = get_default_value(initial_value, reduction)
@@ -191,7 +191,7 @@ class TestSegmentReductions(TestCase):
                 check_backward = True if initial is not None else False
                 initial_value = initial
                 default_value = get_default_value(initial_value, reduction)
-                if reduction == "max":
+                if reduction == "amax":
                     expected_result = [
                         [1, 1],
                         [float("nan"), float("nan")],
@@ -221,7 +221,7 @@ class TestSegmentReductions(TestCase):
                         [0.333, 0.333],
                         [0.333, 0.333],
                     ]
-                elif reduction == "min":
+                elif reduction == "amin":
                     if initial is not None:
                         initial_value = 1000  # some high number
                         default_value = get_default_value(initial_value, reduction)
@@ -308,7 +308,7 @@ class TestSegmentReductions(TestCase):
             (torch.int, torch.int64),
         )
     )
-    @parametrize("reduce", ['sum', 'prod', 'min', 'max', 'mean'])
+    @parametrize("reduce", ['sum', 'prod', 'amin', 'amax', 'mean'])
     def test_pytorch_scatter_test_cases(self, device, dtypes, reduce):
         val_dtype, length_dtype = dtypes
         # zero-length segments are filled with reduction inits contrary to pytorch_scatter.
@@ -320,8 +320,8 @@ class TestSegmentReductions(TestCase):
                 'sum': [3, 12, 0, 6],
                 'prod': [2, 60, 1, 6],
                 'mean': [1.5, 4, float('nan'), 6],
-                'min': [1, 3, float('inf'), 6],
-                'max': [2, 5, -float('inf'), 6],
+                'amin': [1, 3, float('inf'), 6],
+                'amax': [2, 5, -float('inf'), 6],
             },
             {
                 'src': [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]],
@@ -330,8 +330,8 @@ class TestSegmentReductions(TestCase):
                 'sum': [[4, 6], [21, 24], [0, 0], [11, 12]],
                 'prod': [[3, 8], [315, 480], [1, 1], [11, 12]],
                 'mean': [[2, 3], [7, 8], [float('nan'), float('nan')], [11, 12]],
-                'min': [[1, 2], [5, 6], [float('inf'), float('inf')], [11, 12]],
-                'max': [[3, 4], [9, 10], [-float('inf'), -float('inf')], [11, 12]],
+                'amin': [[1, 2], [5, 6], [float('inf'), float('inf')], [11, 12]],
+                'amax': [[3, 4], [9, 10], [-float('inf'), -float('inf')], [11, 12]],
             },
             {
                 'src': [[1, 3, 5, 7, 9, 11], [2, 4, 6, 8, 10, 12]],
@@ -340,8 +340,8 @@ class TestSegmentReductions(TestCase):
                 'sum': [[4, 21, 0, 11], [12, 18, 12, 0]],
                 'prod': [[3, 315, 1, 11], [48, 80, 12, 1]],
                 'mean': [[2, 7, float('nan'), 11], [4, 9, 12, float('nan')]],
-                'min': [[1, 5, float('inf'), 11], [2, 8, 12, float('inf')]],
-                'max': [[3, 9, -float('inf'), 11], [6, 10, 12, -float('inf')]],
+                'amin': [[1, 5, float('inf'), 11], [2, 8, 12, float('inf')]],
+                'amax': [[3, 9, -float('inf'), 11], [6, 10, 12, -float('inf')]],
             },
             {
                 'src': [[[1, 2], [3, 4], [5, 6]], [[7, 9], [10, 11], [12, 13]]],
@@ -351,10 +351,10 @@ class TestSegmentReductions(TestCase):
                 'prod': [[[3, 8], [5, 6], [1, 1]], [[7, 9], [1, 1], [120, 143]]],
                 'mean': [[[2, 3], [5, 6], [float('nan'), float('nan')]],
                          [[7, 9], [float('nan'), float('nan')], [11, 12]]],
-                'min': [[[1, 2], [5, 6], [float('inf'), float('inf')]],
-                        [[7, 9], [float('inf'), float('inf')], [10, 11]]],
-                'max': [[[3, 4], [5, 6], [-float('inf'), -float('inf')]],
-                        [[7, 9], [-float('inf'), -float('inf')], [12, 13]]],
+                'amin': [[[1, 2], [5, 6], [float('inf'), float('inf')]],
+                         [[7, 9], [float('inf'), float('inf')], [10, 11]]],
+                'amax': [[[3, 4], [5, 6], [-float('inf'), -float('inf')]],
+                         [[7, 9], [-float('inf'), -float('inf')], [12, 13]]],
             },
             {
                 'src': [[1, 3], [2, 4]],
@@ -363,8 +363,8 @@ class TestSegmentReductions(TestCase):
                 'sum': [[4], [6]],
                 'prod': [[3], [8]],
                 'mean': [[2], [3]],
-                'min': [[1], [2]],
-                'max': [[3], [4]],
+                'amin': [[1], [2]],
+                'amax': [[3], [4]],
             },
             {
                 'src': [[[1, 1], [3, 3]], [[2, 2], [4, 4]]],
@@ -373,8 +373,8 @@ class TestSegmentReductions(TestCase):
                 'sum': [[[4, 4]], [[6, 6]]],
                 'prod': [[[3, 3]], [[8, 8]]],
                 'mean': [[[2, 2]], [[3, 3]]],
-                'min': [[[1, 1]], [[2, 2]]],
-                'max': [[[3, 3]], [[4, 4]]],
+                'amin': [[[1, 1]], [[2, 2]]],
+                'amax': [[[3, 3]], [[4, 4]]],
             },
         ]
         for test in tests:
@@ -409,9 +409,9 @@ class TestSegmentReductions(TestCase):
                     initial = 1
                     # supply initial values to prevent gradcheck from failing for 0 length segments
                     # where nan/inf are reduction identities that produce nans when calculating the numerical jacobian
-                    if reduce == 'min':
+                    if reduce == 'amin':
                         initial = 1000
-                    elif reduce == 'max':
+                    elif reduce == 'amax':
                         initial = -1000
                     segment_reduce_args = {x, reduce}
                     segment_reduce_kwargs = dict(axis=dim, unsafe=True, initial=initial)
@@ -442,7 +442,7 @@ class TestSegmentReductions(TestCase):
 
         for reduction in reductions:
             initial_value = 0
-            if reduction == "max":
+            if reduction == "amax":
                 expected_result = [
                     np.full((2, 5), initial_value).tolist(),
                     np.max(data[:2], axis=0).tolist(),
@@ -456,7 +456,7 @@ class TestSegmentReductions(TestCase):
                     np.mean(data[2:], axis=0).tolist(),
                     np.full((2, 5), initial_value).tolist(),
                 ]
-            elif reduction == "min":
+            elif reduction == "amin":
                 initial_value = 1000  # some high number
                 expected_result = [
                     np.full((2, 5), initial_value).tolist(),

torch/testing/_internal/common_methods_invocations.py
@@ -6188,7 +6188,7 @@ def sample_inputs_segment_reduce(op_info, device, dtype, requires_grad, *, mode=
         ((S, S, S), 1, [[2, 0, 3, 0], [0, 1, 2, 2], [3, 0, 2, 0], [1, 1, 1, 2], [0, 1, 2, 2]], False),
     )
 
-    reductions = ["max", "mean", "min", "sum", "prod"]
+    reductions = ["amax", "mean", "amin", "sum", "prod"]
     for args, reduce, initial in product(test_cases, reductions, [1, 2]):
         inp_shape, dim, lengths, unsafe = args
         lengths_t = torch.tensor(lengths, dtype=torch.long, device=device)

aten/src/ATen/native/SegmentReduce.cpp
@@ -28,25 +28,9 @@ DEFINE_DISPATCH(_segment_reduce_offsets_backward_stub);
 
 namespace {
 
-SegmentReductionType get_reduction_enum(const c10::string_view& reduce) {
-  if (reduce == "max") {
-    return SegmentReductionType::MAX;
-  } else if (reduce == "mean") {
-    return SegmentReductionType::MEAN;
-  } else if (reduce == "min") {
-    return SegmentReductionType::MIN;
-  } else if (reduce == "sum") {
-    return SegmentReductionType::SUM;
-  } else if (reduce == "prod") {
-    return SegmentReductionType::PROD;
-  } else {
-    TORCH_CHECK(false, "unsupported reduction given! ", reduce);
-  }
-}
-
 template <typename T, bool is_offsets_like=false>
 void _segment_reduce_lengths_cpu_kernel1(
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const Tensor& data,
     const T* lengths_data,
     int64_t axis,
@@ -90,15 +74,15 @@ void _segment_reduce_lengths_cpu_kernel1(
               scalar_t initial_value;
               if (initial.has_value()) {
                 initial_value = initial.value().to<scalar_t>();
-              } else if (reduction == SegmentReductionType::MAX) {
+              } else if (reduction == ReductionType::MAX) {
                 initial_value = -std::numeric_limits<scalar_t>::infinity();
               } else if (
-                  reduction == SegmentReductionType::MEAN ||
-                  reduction == SegmentReductionType::SUM) {
+                  reduction == ReductionType::MEAN ||
+                  reduction == ReductionType::SUM) {
                 initial_value = 0;
-              } else if (reduction == SegmentReductionType::MIN) {
+              } else if (reduction == ReductionType::MIN) {
                 initial_value = std::numeric_limits<scalar_t>::infinity();
-              } else if (reduction == SegmentReductionType::PROD) {
+              } else if (reduction == ReductionType::PROD) {
                 initial_value = 1;
               }
 
@@ -107,19 +91,19 @@ void _segment_reduce_lengths_cpu_kernel1(
                 int64_t data_index = outer_idx * data_stride_axis * data_size_axis
                                      + j * data_stride_axis + inner_idx;
                 const auto val = values_data[data_index];
-                if (reduction == SegmentReductionType::MAX) {
+                if (reduction == ReductionType::MAX) {
                   initial_value = at::_isnan(val)
                       ? val
                       : std::max<scalar_t>(initial_value, val);
                 } else if (
-                    reduction == SegmentReductionType::MEAN ||
-                    reduction == SegmentReductionType::SUM) {
+                    reduction == ReductionType::MEAN ||
+                    reduction == ReductionType::SUM) {
                   initial_value = initial_value + val;
-                } else if (reduction == SegmentReductionType::MIN) {
+                } else if (reduction == ReductionType::MIN) {
                   initial_value = at::_isnan(val)
                       ? val
                       : std::min<scalar_t>(initial_value, val);
-                } else if (reduction == SegmentReductionType::PROD) {
+                } else if (reduction == ReductionType::PROD) {
                   initial_value = initial_value * val;
                 }
               }
@@ -128,10 +112,10 @@ void _segment_reduce_lengths_cpu_kernel1(
               TORCH_CHECK(segment_length >= 0);
 
               if (segment_length == 0 && !initial.has_value() &&
-                  reduction == SegmentReductionType::MEAN) {
+                  reduction == ReductionType::MEAN) {
                 initial_value = static_cast<scalar_t>(NAN);
               } else if (
-                  reduction == SegmentReductionType::MEAN &&
+                  reduction == ReductionType::MEAN &&
                   segment_length > 0 && !at::_isnan(initial_value)) {
                 initial_value = initial_value / segment_length;
               }
@@ -145,7 +129,7 @@ void _segment_reduce_lengths_cpu_kernel1(
 }
 
 Tensor _segment_reduce_lengths_cpu_kernel(
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const Tensor& data,
     const Tensor& lengths,
     int64_t axis,
@@ -171,7 +155,7 @@ Tensor _segment_reduce_lengths_cpu_kernel(
 }
 
 Tensor _segment_reduce_offsets_cpu_kernel(
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const Tensor& data,
     const Tensor& offsets,
     int64_t axis,
@@ -201,7 +185,7 @@ void _segment_reduce_cpu_lengths_backward_kernel1(
     const Tensor& grad_contig,
     const Tensor& output_contig,
     const Tensor& data_contig,
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const T* lengths_data,
     int64_t axis,
     const c10::optional<Scalar>& initial,
@@ -234,7 +218,7 @@ void _segment_reduce_cpu_lengths_backward_kernel1(
         const auto* values_data = data_contig.data_ptr<scalar_t>();
         // Used to calculate exclusive prod
         scalar_t initial_prod_value;
-        if (reduction == SegmentReductionType::PROD) {
+        if (reduction == ReductionType::PROD) {
           if (initial.has_value()) {
             initial_prod_value = initial.value().to<scalar_t>();
           } else {
@@ -265,8 +249,8 @@ void _segment_reduce_cpu_lengths_backward_kernel1(
             for (const auto inner_idx : c10::irange(inner_offset)) {
               int64_t output_index = outer_idx * output_stride_axis * output_size_axis
                                      + dim_idx * output_stride_axis + inner_idx;
-              if (reduction == SegmentReductionType::MAX ||
-                  reduction == SegmentReductionType::MIN) {
+              if (reduction == ReductionType::MAX ||
+                  reduction == ReductionType::MIN) {
                 int64_t counter = 0;
                 for (const auto j : c10::irange(segment_start, segment_end)) {
                   int64_t data_index = outer_idx * data_stride_axis * data_size_axis
@@ -290,21 +274,21 @@ void _segment_reduce_cpu_lengths_backward_kernel1(
                         grad_input_data[data_index] / counter;
                   }
                 }
-              } else if (reduction == SegmentReductionType::MEAN) {
+              } else if (reduction == ReductionType::MEAN) {
                 auto grad_val = grad_data[output_index] / segment_length;
                 for (const auto j : c10::irange(segment_start, segment_end)) {
                   int64_t data_index = outer_idx * data_stride_axis * data_size_axis
                                        + j * data_stride_axis + inner_idx;
                   grad_input_data[data_index] = grad_val;
                 }
-              } else if (reduction == SegmentReductionType::SUM) {
+              } else if (reduction == ReductionType::SUM) {
                 const auto& grad_val = grad_data[output_index];
                 for (const auto j : c10::irange(segment_start, segment_end)) {
                   int64_t data_index = outer_idx * data_stride_axis * data_size_axis
                                        + j * data_stride_axis + inner_idx;
                   grad_input_data[data_index] = grad_val;
                 }
-              } else if (reduction == SegmentReductionType::PROD) {
+              } else if (reduction == ReductionType::PROD) {
                 const auto& grad_val = grad_data[output_index] * output_data[output_index];
                 for (const auto j : c10::irange(segment_start, segment_end)) {
                   int64_t data_index = outer_idx * data_stride_axis * data_size_axis
@@ -337,7 +321,7 @@ Tensor _segment_reduce_cpu_lengths_backward_kernel(
     const Tensor& grad_contig,
     const Tensor& output_contig,
     const Tensor& data_contig,
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const Tensor& lengths_contig,
     int64_t axis,
     const c10::optional<Scalar>& initial) {
@@ -370,7 +354,7 @@ Tensor _segment_reduce_cpu_offsets_backward_kernel(
     const Tensor& grad_contig,
     const Tensor& output_contig,
     const Tensor& data_contig,
-    SegmentReductionType reduction,
+    ReductionType reduction,
     const Tensor& offsets_contig,
     int64_t axis,
     const c10::optional<Scalar>& initial) {

aten/src/ATen/native/SegmentReduce.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ATen/native/DispatchStub.h>
+#include <ATen/native/ReductionType.h>
 #include <c10/core/Scalar.h>
 #include <c10/util/Optional.h>
 
@@ -9,10 +10,8 @@ class Tensor;
 
 namespace native {
 
-enum SegmentReductionType { MAX, MEAN, MIN, SUM, PROD};
-
 using segment_reduce_lengths_fn = Tensor (*)(
-    SegmentReductionType,
+    ReductionType,
     const Tensor&,
     const Tensor&,
     int64_t,
@@ -20,7 +19,7 @@ using segment_reduce_lengths_fn = Tensor (*)(
 DECLARE_DISPATCH(segment_reduce_lengths_fn, _segment_reduce_lengths_stub);
 
 using segment_reduce_offsets_fn = Tensor (*)(
-    SegmentReductionType,
+    ReductionType,
     const Tensor&,
     const Tensor&,
     int64_t,
@@ -31,7 +30,7 @@ using segment_reduce_lengths_backward_fn = Tensor (*)(
     const Tensor&,
     const Tensor&,
     const Tensor&,
-    SegmentReductionType,
+    ReductionType,
     const Tensor&,
     int64_t,
     const c10::optional<Scalar>&);
@@ -41,7 +40,7 @@ using segment_reduce_offsets_backward_fn = Tensor (*)(
     const Tensor&,
     const Tensor&,
     const Tensor&,
-    SegmentReductionType,
+    ReductionType,
     const Tensor&,
     int64_t,
     const c10::optional<Scalar>&);

commit 10012205a773eccf6b9994d9e79d28c0f3355393

Fix BC-breaking change introduced by https://github.com/pytorch/pytorch/pull/91499
Make enum accept both `min` and `amin` values

Reinstante testing

aten/src/ATen/native/ReductionType.h
@@ -7,11 +7,11 @@ namespace at { namespace native {
 enum ReductionType {MAX, MEAN, MIN, SUM, PROD};
 
 static inline ReductionType get_reduction_enum(const c10::string_view& reduce) {
-  if (reduce == "amax") {
+  if (reduce == "max" || reduce == "amax") {
     return ReductionType::MAX;
   } else if (reduce == "mean") {
     return ReductionType::MEAN;
-  } else if (reduce == "amin") {
+  } else if (reduce == "min" || reduce == "amin") {
     return ReductionType::MIN;
   } else if (reduce == "sum") {
     return ReductionType::SUM;

test/test_segment_reductions.py
@@ -18,17 +18,17 @@ from torch.testing._internal.common_utils import (
 )
 
 
-reductions = ["amax", "mean", "amin", "sum", "prod"]
+reductions = ["max", "mean", "min", "sum", "prod"]
 
 
 def get_default_value(initial_value, reduction):
     if initial_value is not None:
         return initial_value
-    if reduction == "amax":
+    if reduction == "max":
         return -float("Inf")
     elif reduction == "mean":
         return float("nan")
-    elif reduction == "amin":
+    elif reduction == "min":
         return float("Inf")
     elif reduction == "sum":
         return 0.0
@@ -133,13 +133,13 @@ class TestSegmentReductions(TestCase):
                 check_backward = True if initial is not None else False
                 initial_value = initial
                 default_value = get_default_value(initial_value, reduction)
-                if reduction == "amax":
+                if reduction == "max":
                     expected_result = [1, float("nan"), 5, default_value]
                     expected_grad = [1, 1, 0, 0, 0.5, 0.5]
                 elif reduction == "mean":
                     expected_result = [1, float("nan"), 4.666, default_value]
                     expected_grad = [1.0, 0.5, 0.5, 0.333, 0.333, 0.333]
-                elif reduction == "amin":
+                elif reduction == "min":
                     if initial is not None:
                         initial_value = 1000  # some high number
                         default_value = get_default_value(initial_value, reduction)

torch/testing/_internal/common_methods_invocations.py
@@ -6260,7 +6260,7 @@ def sample_inputs_segment_reduce(op_info, device, dtype, requires_grad, *, mode=
         ((S, S, S), 1, [[2, 0, 3, 0], [0, 1, 2, 2], [3, 0, 2, 0], [1, 1, 1, 2], [0, 1, 2, 2]], False),
     )
 
-    reductions = ["amax", "mean", "amin", "sum", "prod"]
+    reductions = ["max", "mean", "min", "sum", "prod"]
     for args, reduce, initial in product(test_cases, reductions, [1, 2]):
         inp_shape, dim, lengths, unsafe = args
         lengths_t = torch.tensor(lengths, dtype=torch.long, device=device)