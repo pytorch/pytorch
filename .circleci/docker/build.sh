586e977bf27990017d8d0ab0589bdf148e654209
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-01-27 17:57:33 +0000

    Upgrade CI to ROCm 5.4.2 (#92972)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92972
    Approved by: https://github.com/malfet

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -184,7 +184,7 @@ case "$image" in
     PROTOBUF=yes
     DB=yes
     VISION=yes
-    ROCM_VERSION=5.2
+    ROCM_VERSION=5.3
     NINJA_VERSION=1.9.0
     CONDA_CMAKE=yes
     ;;
@@ -194,7 +194,7 @@ case "$image" in
     PROTOBUF=yes
     DB=yes
     VISION=yes
-    ROCM_VERSION=5.3
+    ROCM_VERSION=5.4
     NINJA_VERSION=1.9.0
     CONDA_CMAKE=yes
     ;;
