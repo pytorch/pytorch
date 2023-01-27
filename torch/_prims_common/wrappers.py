f45baea7a738757daa3c8489f6889a39b57db5c3
Sherlock Huang <bahuang@fb.com>
2023-01-27 03:17:48 +0000

    Update on "Rewrite some decomps to allow producing aten ops"
    [ghstack-poisoned]

--- torch/_prims_common/wrappers.py
+++ torch/_prims_common/wrappers.py
@@ -34,7 +34,6 @@ def _maybe_convert_to_dtype(a: None, dtype: torch.dtype) -> None:
 
 # TODO: implement ref.cast with an option to enforce safe casting
 def _maybe_convert_to_dtype(a, dtype):
-    import torch._prims as prims
     if isinstance(a, TensorLike):
         if a.dtype != dtype:
             # NOTE: this is incorrect on the CPU
