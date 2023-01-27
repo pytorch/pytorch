c2eddfae761541e90f58d03dcd5faa239c8727d4
Edward Z. Yang <ezyang@meta.com>
2023-01-27 12:27:16 -0500

    Make CPU inductor work with dynamic shapes (#93077)
    These errors were found by looking at wav2vec2
    
    See https://github.com/pytorch/pytorch/issues/91719
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/93077
    Approved by: https://github.com/voznesenskym, https://github.com/ngimel

--- torch/_inductor/mkldnn.py
+++ torch/_inductor/mkldnn.py
@@ -351,7 +351,7 @@ class PackedLinear(nn.Linear):
 
     def _update_module_params(self, linear, input_size):
         self.__dict__ = copy.deepcopy(linear.__dict__)
-        self.batch_size = int(reduce(lambda x, y: x * y, input_size) / input_size[-1])
+        self.batch_size = reduce(lambda x, y: x * y, input_size[:-1])
         self.packed_weight = torch.nn.Parameter(
             torch.ops.mkl._mkl_reorder_linear_weight(
                 self.weight.to_mkldnn(), self.batch_size
