174495504d7a104535bebfeefdeb68f86d0db0d9
cyy <cyyever@outlook.com>
2023-01-29 08:16:56 +0000

    fix building errors on FreeBSD

--- c10/test/util/exception_test.cpp
+++ c10/test/util/exception_test.cpp
@@ -34,7 +34,7 @@ TEST(ExceptionTest, TORCH_INTERNAL_ASSERT_DEBUG_ONLY) {
 }
 
 // On these platforms there's no assert
-#if !defined(__ANDROID__) && !defined(__APPLE__) && \
+#if !defined(__ANDROID__) && !defined(__APPLE__) && !defined(__FreeBSD__) && \
     !(defined(USE_ROCM) && ROCM_VERSION < 40100)
 TEST(ExceptionTest, CUDA_KERNEL_ASSERT) {
   // This function always throws even in NDEBUG mode
