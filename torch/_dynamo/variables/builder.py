e3a7f95fe633e743edc8c32bdfd4ff0151245c14
min-jean-cho <min.jean.cho@intel.com>
2023-01-30 10:30:52 -0800

    minor fix

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -422,6 +422,7 @@ class VariableBuilder:
             )
         elif (
             not is_allowed(value)
+            and hasattr(value, "__name__")
             and value.__name__ in dir(torch.ops.aten)
             and isinstance(
                 getattr(torch.ops.aten, value.__name__), torch._ops.OpOverloadPacket
