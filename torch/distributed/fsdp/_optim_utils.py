6c94619e2694d67741fa85b870eb810d9d8e9915
Chien-Chin Huang <chienchin@fb.com>
2023-01-30 15:02:18 -0800

    [FSDP][optim_state_dict] Fix `_is_named_optimizer` when the state is empty (#93303)
    Optimizer state is not eager initializaion -- only NamedOptimizer and KeyedOptimizer are. This PR makes it `_is_named_optimizer` work with regular optimizers.
    
    Differential Revision: [D42858589](https://our.internmc.facebook.com/intern/diff/D42858589/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/93303
    Approved by: https://github.com/fduwjj

--- torch/distributed/fsdp/_optim_utils.py
+++ torch/distributed/fsdp/_optim_utils.py
@@ -1292,8 +1292,13 @@ def _unflatten_param_groups(
 
 
 def _is_named_optimizer(optim_state_dict: Dict[str, Any]) -> bool:
+    state = optim_state_dict.get("state", None)
+    if not state:
+        # If we cannot find a state, assume it is not NamedOptimizer as
+        # NamedOptimizer has eagerly initialization.
+        return False
     try:
-        key = next(iter(optim_state_dict["state"].keys()))
+        key = next(iter(state.keys()))
     except Exception as e:
         raise Exception(optim_state_dict) from e
     return isinstance(key, str)
