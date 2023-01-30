de82b99f9a9df3e202ffca909c36a6fea4a57823
Ashutosh Mishra <ashutosh.mishra@amd.com>
2023-01-31 02:05:39 +0530

    * [ROCM] making cuSOLVER public (#1170)
    * [ROCM] Solution for SWDEV-379692

--- torch/utils/hipify/hipify_python.py
+++ torch/utils/hipify/hipify_python.py
@@ -49,7 +49,7 @@ PYTORCH_TEMPLATE_MAP = {"Dtype": "scalar_t", "T": "scalar_t"}
 __all__ = ['InputError', 'openf', 'bcolors', 'GeneratedFileCleaner', 'match_extensions', 'matched_files_iter',
            'preprocess_file_and_save_result', 'compute_stats', 'add_dim3', 'processKernelLaunches', 'find_closure_group',
            'find_bracket_group', 'find_parentheses_group', 'replace_math_functions', 'hip_header_magic', 'replace_extern_shared',
-           'get_hip_file_path', 'is_out_of_place', 'is_pytorch_file', 'is_cusparse_file', 'is_caffe2_gpu_file',
+           'get_hip_file_path', 'is_out_of_place', 'is_pytorch_file', 'is_cusparse_file', 'is_cusolver_file', 'is_caffe2_gpu_file',
            'is_caffe2_gpu_file', 'Trie', 'preprocessor', 'file_specific_replacement', 'file_add_header',
            'fix_static_global_kernels', 'extract_arguments', 'str2bool', 'hipify']
 
