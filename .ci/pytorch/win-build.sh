dc0629a6af41fb09c8911d7e5a5449dc4f6e7b8a
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-01-30 11:20:03 -0600

    Add files via upload

--- .ci/pytorch/win-build.sh
+++ .ci/pytorch/win-build.sh
@@ -4,12 +4,10 @@
 # If you want to build with CUDA, run this with USE_CUDA=1
 # If you want to build without CUDA, run this with USE_CUDA=0
 
-<<com
 if [ ! -f setup.py ]; then
   echo "ERROR: Please run this build script from PyTorch root directory."
   exit 1
 fi
-com
 
 SCRIPT_PARENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # shellcheck source=./common.sh
@@ -32,7 +30,7 @@ if [[ -n "$PYTORCH_FINAL_PACKAGE_DIR" ]]; then
     mkdir -p "$PYTORCH_FINAL_PACKAGE_DIR" || true
 fi
 
-# This directory is used only to hold "pytorch_env_restore.bat", called via "setup_pytorch_env.py"
+# This directory is used only to hold "pytorch_env_restore.bat", called via "setup_pytorch_env.bat"
 CI_SCRIPTS_DIR=$TMP_DIR/ci_scripts
 mkdir -p "$CI_SCRIPTS_DIR"
 
@@ -43,12 +41,12 @@ fi
 export SCRIPT_HELPERS_DIR=$SCRIPT_PARENT_DIR/win-test-helpers
 
 set +ex
-grep -E -R 'PyLong_(From|As)(Unsigned|)Long\(' --exclude=python_numbers.h torch/
+grep -E -R 'PyLong_(From|As)(Unsigned|)Long\(' --exclude=python_numbers.h --exclude=eval_frame.c torch/
 PYLONG_API_CHECK=$?
 if [[ $PYLONG_API_CHECK == 0 ]]; then
   echo "Usage of PyLong_{From,As}{Unsigned}Long API may lead to overflow errors on Windows"
   echo "because \`sizeof(long) == 4\` and \`sizeof(unsigned long) == 4\`."
-  echo "Please include \"torch/csrc/python_numbers.h\" and use the correspoding APIs instead."
+  echo "Please include \"torch/csrc/utils/python_numbers.h\" and use the correspoding APIs instead."
   echo "PyLong_FromLong -> THPUtils_packInt32 / THPUtils_packInt64"
   echo "PyLong_AsLong -> THPUtils_unpackInt (32-bit) / THPUtils_unpackLong (64-bit)"
   echo "PyLong_FromUnsignedLong -> THPUtils_packUInt32 / THPUtils_packUInt64"
@@ -57,9 +55,7 @@ if [[ $PYLONG_API_CHECK == 0 ]]; then
 fi
 set -ex
 
-python "$SCRIPT_HELPERS_DIR"/build_pytorch.py
-
-env
+"$SCRIPT_HELPERS_DIR"/build_pytorch.bat
 
 assert_git_not_dirty
 
