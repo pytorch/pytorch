b5da087733f588676992ae022320679915c10391
Michael Voznesensky <voznesenskym@gmail.com>
2023-01-30 18:58:05 +0000

    Update base for Update on "dont graph break on list[SymInt] comparisons"
    Make sure that we don't graph break on code like:
    ```
    assert a.size() == b.size()
    ```
    
    When a and b have dynamic shapes.
    
    Two things to call out:
    
    (1) The SymBool PR hasn't landed yet, so I just added `operator.__and__` support to SymInt as a dumb workaround. We can either remove that when SymBool lands, or wait for SymBool to land before merging this
    
    (1) There was some fiddling I had to do for ListVariable, that I didn't have to do for e.g. SizeVariable. Comparing two lists is supposed to return a bool, but ListVariable is represented as a List of proxies - so I had to manually reduce across the list.
    
    
    
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/base.py
+++ torch/_dynamo/variables/base.py
@@ -163,6 +163,13 @@ class VariableTracker(object, metaclass=HasPostInit):
         """
         return self
 
+    def compare(self, tx, op, right, **options):
+        """
+        Used to implement comparison operators for different types.
+        For example, list1 < list2 is implemented differently from tensor1 < tensor2
+        """
+        unimplemented(f"COMPARE_OP {typestr(self)} {op} {typestr(right)}")
+
     def can_make_guard(self):
         try:
             self.make_guard(None)
