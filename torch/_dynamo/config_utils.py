470d8e19070b57d326866d4cdaa405a0fa43ac02
Jason Ansel <jansel@fb.com>
2023-01-30 09:33:40 -0800

    Update base for Update on "Remove torch/_dynamo/optimizations/normalize.py"
    This file was largely made obsolete by dispatcher level functionalization,
    and has been disabled by config.
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/config_utils.py
+++ torch/_dynamo/config_utils.py
@@ -191,6 +191,9 @@ class SubConfigProxy:
     def __getattr__(self, name):
         return self._config.__getattr__(self._prefix + name)
 
+    def __delattr__(self, name):
+        return self._config.__delattr__(self._prefix + name)
+
 
 def patch_object(obj, name, value):
     """
