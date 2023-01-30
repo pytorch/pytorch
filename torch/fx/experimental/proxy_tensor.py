0959660ad90fecce92beee1f782e1e44d13b7d89
Edward Z. Yang <ezyang@meta.com>
2023-01-30 07:58:09 -0500

    Remove proxy tensor's check for data dependent output
    We'll rely on the underlying fake tensor to raise (or not
    raise) an error in these cases.  This used to be useful
    when we did tracing with real tensors (in which case this
    could help you detect if you were improperly burning in
    some data dependent value), but my claim is that if you
    want to do this, you should fakeify your inputs first.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- torch/fx/experimental/proxy_tensor.py
+++ torch/fx/experimental/proxy_tensor.py
@@ -275,13 +275,6 @@ def proxy_call(proxy_mode, func, args, kwargs):
             )
             with maybe_disable_fake_tensor_mode():
                 return func(*const_args, **const_kwargs)
-        # For symbolic tracing, we return a SymInt/SymFloat and try to
-        # get further in the trace
-        if proxy_mode.tracing_mode != "symbolic":
-            raise RuntimeError(
-                f"It appears that you're trying to get value out of a tracing tensor with {func} - erroring out! "
-                "It's likely that this is caused by data-dependent control flow or similar."
-            )
     proxy_args, proxy_kwargs = pytree.tree_map_only(
         (SymInt, SymFloat, SymBool),
         fetch_sym_proxy(proxy_mode.tracer),
