5dc5fd3b3045640ca5044cf603d659745c71ce81
blzheng <beilei.zheng@intel.com>
2023-01-29 17:55:33 -0800

    inductor: support more conv+unary fusion (#92518)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92518
    Approved by: https://github.com/jgong5, https://github.com/jansel

--- torch/_inductor/mkldnn.py
+++ torch/_inductor/mkldnn.py
@@ -516,7 +516,9 @@ def mkldnn_fuse_fx(gm: torch.fx.GraphModule, example_inputs):
 
 
 def create_unary_module(node: torch.fx.node):
-    assert node.op == "call_function", "The current node should be a function node"
+    assert (
+        node.op == "call_function" or node.op == "call_method"
+    ), "The current node should be a function/method node"
     unary_map = {
         F.relu: nn.ReLU,
         F.sigmoid: nn.Sigmoid,
@@ -527,6 +529,12 @@ def create_unary_module(node: torch.fx.node):
         F.gelu: nn.GELU,
         F.relu6: nn.ReLU6,
         F.silu: nn.SiLU,
+        torch.relu: nn.ReLU,
+        torch.sigmoid: nn.Sigmoid,
+        torch.tanh: nn.Tanh,
+        "relu": nn.ReLU,
+        "sigmoid": nn.Sigmoid,
+        "tanh": nn.Tanh,
     }
     return unary_map[node.target](*(node.args[1:]), **(node.kwargs))
 
@@ -548,7 +556,7 @@ def fuse_unary(gm: torch.fx.GraphModule):
                 ):  # Output of computation_node is used by other nodes
                     continue
                 computation_node = modules[node.args[0].target]
-                if node.op == "call_function":
+                if node.op == "call_function" or node.op == "call_method":
                     # make sure unary function's inputs only one fx.node(others should be constant value).
                     if any(isinstance(v, torch.fx.Node) for v in node.args[1:]) or any(
                         isinstance(v, torch.fx.Node) for _, v in node.kwargs.items()
@@ -781,6 +789,13 @@ unary_ops = [
     F.gelu,
     F.relu6,
     F.silu,
+    torch.relu,
+    torch.sigmoid,
+    torch.tanh,
+    # methods (torch.Tensor.xxx)
+    "relu",
+    "sigmoid",
+    "tanh",
 ]
 
 
