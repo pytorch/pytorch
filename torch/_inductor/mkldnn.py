bd4a5b400aa32092b41a6c39004dc0e5bb62883d
chunyuan <chunyuan.wu@intel.com>
2023-01-31 06:18:35 +0000

    [Re-open 90266] [inductor] weight prepack for _convolution_transpose_pointwise (#91955)
    Re-open https://github.com/pytorch/pytorch/pull/90266 since earlier pr on that stack got reverted.
    Depend on internal ideep upgrade.
    [Update]: internal ideep upgrade issue is resolved in https://github.com/pytorch/pytorch/pull/92239.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91955
    Approved by: https://github.com/jgong5, https://github.com/desertfire

--- torch/_inductor/mkldnn.py
+++ torch/_inductor/mkldnn.py
@@ -422,6 +422,7 @@ class ConvTransposeUnary2d(nn.ConvTranspose2d):
         self,
         conv_transpose: nn.Module,
         unary: nn.Module,
+        input_size: list,
     ):
         super(ConvTransposeUnary2d, self).__init__(
             conv_transpose.in_channels,
@@ -437,12 +438,25 @@ class ConvTransposeUnary2d(nn.ConvTranspose2d):
             conv_transpose.weight.device,
             conv_transpose.weight.dtype,
         )
-        self._update_module_params(conv_transpose, unary)
+        self._update_module_params(conv_transpose, unary, input_size)
 
-    def _update_module_params(self, conv_transpose, unary):
+    def _update_module_params(self, conv_transpose, unary, input_size):
         self.__dict__ = copy.deepcopy(conv_transpose.__dict__)
         self.attr, self.scalars, self.algorithm = unary_modules_map[unary.__class__](
             unary
+        )
+        packed_weight = torch.ops.mkldnn._reorder_convolution_transpose_weight(
+            self.weight.to_mkldnn(),
+            self.padding,
+            self.output_padding,
+            self.stride,
+            self.dilation,
+            self.groups,
+            input_size,
+        )
+        self.weight = torch.nn.Parameter(
+            packed_weight,
+            requires_grad=self.weight.requires_grad,
         )
 
     def _conv_transpose_forward(self, input, weight, bias):
@@ -556,6 +570,7 @@ def fused_conv_transpose_unary_eval(
     return ConvTransposeUnary2d(
         conv_transpose,
         unary,
+        input_size,
     )
 
 
