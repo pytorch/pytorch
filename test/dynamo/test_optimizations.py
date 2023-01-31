358723b604c33530ffbe87856fb489d00356af47
Jason Ansel <jansel@fb.com>
2023-01-31 09:57:24 -0800

    Remove many untested dynamo backends
    [ghstack-poisoned]

--- test/dynamo/test_optimizations.py
+++ test/dynamo/test_optimizations.py
@@ -1,7 +1,5 @@
 # Owner(s): ["module: dynamo"]
 import importlib
-import json
-import os
 import unittest
 
 import torch
@@ -142,6 +140,19 @@ class TestOptimizations(torch._dynamo.test_case.TestCase):
             r2 = opt_model(input)
         self.assertTrue(same(r1, r2.float(), tol=0.1))
         self.assertEqual(r2.dtype, torch.bfloat16)
+
+    def _check_backend_works(self, backend):
+        model = Conv_Bn_Relu(3, 32, kernel_size=3, stride=1).eval()
+        input = torch.randn(8, 3, 64, 64)
+        r1 = model(input)
+        r2 = torch.compile(model, backend=backend)(input)
+        self.assertTrue(same(r1, r2.float(), tol=0.01))
+
+    def test_eager(self):
+        self._check_backend_works("eager")
+
+    def test_torchscript(self):
+        self._check_backend_works("ts")
 
 
 class NormalizeIRTests(torch._dynamo.test_case.TestCase):
