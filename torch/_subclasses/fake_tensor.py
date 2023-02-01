39000f9331874f427f70fd8fad98f795f1337d71
Michael Voznesensky <voznesenskym@gmail.com>
2023-02-01 02:46:52 +0000

    A little fixing

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -727,6 +727,17 @@ class FakeTensor(torch.Tensor):
     __torch_function__ = torch._C._disabled_torch_function_impl
 
 
+def _short_circuit_alloc_op(fake_mode, a, b, **kwargs):
+    copy_kwargs = kwargs
+    real_device = copy_kwargs["device"]
+    del copy_kwargs["device"]
+    return FakeTensor(
+        fake_mode,
+        torch.empty_strided(a, b, device="meta", **copy_kwargs),
+        device=real_device,
+    )
+
+
 def _short_circuit_binary_broadcasting_op(a, b):
     if isinstance(a, (int, float, SymInt, SymFloat)):
         return b
@@ -734,35 +745,37 @@ def _short_circuit_binary_broadcasting_op(a, b):
         return a
     import torch._refs as refs
 
-    in_shapes = [a.shape, b.shape] # Note(voz): Must be list as we cannot hash symint
+    in_shapes = [a.shape, b.shape]  # Note(voz): Must be list as we cannot hash symint
     a_broadcast, b_broadcast = refs._maybe_broadcast(a, b)
 
     if not a_broadcast.dtype == b_broadcast.dtype:
-        breakpoint()
         return None
-    
+
     if not a_broadcast.device == b_broadcast.device:
-        breakpoint()
         return None
 
     # Easy case - both match
-    safe_both_match = (a_broadcast.shape == b_broadcast.shape) or (a_broadcast.shape in in_shapes and b_broadcast.shape in in_shapes)
+    safe_both_match = (a_broadcast.shape == b_broadcast.shape) or (
+        a_broadcast.shape in in_shapes and b_broadcast.shape in in_shapes
+    )
 
     # Complex case, we allow no more than 1 non trivial dim (non-0, non-1)
     # TODO(Support this?) never hit it on bench yet...
     if not safe_both_match:
-        breakpoint()
         return None
 
     simple_safe_strides = a.stride() == b.stride()
     if not simple_safe_strides:
-        a_strides_non_trivial = len([x for x in a_broadcast.stride() if x not in (0, 1)])
-        b_strides_non_trivial = len([x for x in b_broadcast.stride() if x not in (0, 1)])
-        # If one side of strides is nontrivial in only a single dim, it should be fine. 
+        a_strides_non_trivial = len(
+            [x for x in a_broadcast.stride() if x not in (0, 1)]
+        )
+        b_strides_non_trivial = len(
+            [x for x in b_broadcast.stride() if x not in (0, 1)]
+        )
+        # If one side of strides is nontrivial in only a single dim, it should be fine.
         if a_strides_non_trivial > 1 and b_strides_non_trivial > 1:
-            breakpoint()
             return None
-    
+
     return FakeTensor(
         a_broadcast.fake_mode,
         torch.empty(a_broadcast.shape, device="meta"),
@@ -784,7 +797,6 @@ short_circuit_binary_ops = {
     aten.mul.Tensor,
     aten.sub.Tensor,
     aten.div.Tensor,
-    aten.empty_strided.default,
 }
 
 short_circuit_unary_ops = {aten.relu.default, aten.rsqrt.default}
@@ -836,9 +848,7 @@ class FakeTensorMode(TorchDispatchMode):
     def __torch_dispatch__(self, func, types, args=(), kwargs=None):
         # Gross circular reference hack
         # import torch._prims as prims
-        skipped_prims = {
-            torch._prims.mul
-        }
+        skipped_prims = {torch._prims.mul}
 
         kwargs = kwargs if kwargs else {}
 
@@ -953,16 +963,15 @@ class FakeTensorMode(TorchDispatchMode):
         self.invalidate_written_to_constants(func, flat_arg_fake_tensors, args, kwargs)
 
         r = None
-        if (func in short_circuit_binary_ops or func in skipped_prims) and len(kwargs) == 0:
+        if func == aten.empty_strided.default:
+            r = _short_circuit_alloc_op(self, *args, **kwargs)
+        elif (func in short_circuit_binary_ops or func in skipped_prims) and len(
+            kwargs
+        ) == 0:
             r = _short_circuit_binary_broadcasting_op(*args)
         elif func in short_circuit_unary_ops and len(kwargs) == 0:
             r = _short_circuit_unary_op(*args)
-        else:
-            print("RUNNING ", func)
-            if "prims.mul" in str(func):
-                breakpoint()
-            if len(args) == 2:
-                breakpoint()
+
         if r is not None:
             return r
 
