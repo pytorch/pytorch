963571d2d02bfd01b56b3f183f36010aab21ee2a
Edward Z. Yang <ezyang@meta.com>
2023-02-01 10:58:00 -0500

    Update on "Fix Upsample/EmbeddingBag module printing"
    The fix generalizes but I want someone else to holistically figure this out.
    
    Fixes https://github.com/pytorch/pytorch/issues/93233
    Fixes https://github.com/pytorch/pytorch/issues/93512
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    [ghstack-poisoned]

--- torch/nn/modules/sparse.py
+++ torch/nn/modules/sparse.py
@@ -393,17 +393,17 @@ class EmbeddingBag(Module):
                                self.padding_idx)
 
     def extra_repr(self) -> str:
-        s = '{repr(num_embeddings)}, {repr(embedding_dim)}'
+        s = '{num_embeddings}, {embedding_dim}'
         if self.max_norm is not None:
-            s += ', max_norm={repr(max_norm)}'
+            s += ', max_norm={max_norm}'
         if self.norm_type != 2:
-            s += ', norm_type={repr(norm_type)}'
+            s += ', norm_type={norm_type}'
         if self.scale_grad_by_freq is not False:
-            s += ', scale_grad_by_freq={repr(scale_grad_by_freq)}'
-        s += ', mode={repr(mode)}'
+            s += ', scale_grad_by_freq={scale_grad_by_freq}'
+        s += ', mode={mode}'
         if self.padding_idx is not None:
-            s += ', padding_idx={repr(padding_idx)}'
-        return s.format(**self.__dict__)
+            s += ', padding_idx={padding_idx}'
+        return s.format(**{k: repr(v) for k, v in self.__dict__.items()})
 
     @classmethod
     def from_pretrained(cls, embeddings: Tensor, freeze: bool = True, max_norm: Optional[float] = None,
