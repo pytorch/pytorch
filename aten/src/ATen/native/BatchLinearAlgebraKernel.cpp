093829bcb7a12e296268cbf69ba5e0e9ff07a5e3
haozhe.zhu <haozhe.zhu@intel.com>
2023-02-01 10:05:19 +0800

    avoid overflow

--- aten/src/ATen/native/BatchLinearAlgebraKernel.cpp
+++ aten/src/ATen/native/BatchLinearAlgebraKernel.cpp
@@ -928,12 +928,12 @@ void apply_lu_factor(const Tensor& input, const Tensor& pivots, const Tensor& in
           infos_working_ptr);
     }
   };
-  int64_t matrix_rank = std::min(m, n);
-  auto matrix_rank = std::min(m, n);
+  // avoid overflow
+  float matrix_rank = float(std::min(m, n));
   // A heuristic tested on a 32 core/socket ICX system
   // https://github.com/pytorch/pytorch/pull/93037#discussion_r1090112948
-  auto grain_size =
-      int64_t(std::min(1, 3200.0 / (matrix_rank * matrix_rank * matrix_rank)));
+  int64_t grain_size = int64_t(
+      std::min(1.0, 3200.0 / (matrix_rank * matrix_rank * matrix_rank)));
   at::parallel_for(0, batch_size, grain_size, loop);
 #endif
 }
