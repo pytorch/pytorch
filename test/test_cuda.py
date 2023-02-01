3f03fe553944240f592ba0f51a441718d4596733
Rishi Puri <puririshi98@berkeley.edu>
2023-01-31 17:28:01 -0800

    trailing

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1770,7 +1770,7 @@ except RuntimeError as e:
                 leak_gpu0()
             except RuntimeError as e:
                 import re
-                assert re.match(regex, str(e)), str(e) + "\n does not match: \n" + regex 
+                assert re.match(regex, str(e)), str(e) + "\n does not match: \n" + regex
         else:
             # assertRaisesRegex does not pass with Python for Jetson,
             # even though the RuntimeError matches regex using re.match
