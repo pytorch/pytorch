84187399fc2ad4767aa86eb0a86f25d47a52bd85
Nikita Vedeneev <nik@quansight.com>
2023-02-02 10:00:21 +0000

    retire sparse_mask_helper (#91714)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91714
    Approved by: https://github.com/albanD, https://github.com/amjames, https://github.com/cpuhrsch

--- aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cu
+++ aten/src/ATen/native/sparse/cuda/SparseCUDATensor.cu
@@ -18,7 +18,6 @@
 #else
 #include <ATen/ops/_coalesce_native.h>
 #include <ATen/ops/_sparse_coo_tensor_unsafe_native.h>
-#include <ATen/ops/_sparse_mask_helper_native.h>
 #include <ATen/ops/empty.h>
 #include <ATen/ops/zeros.h>
 #endif
@@ -40,43 +39,6 @@
 namespace at { namespace native {
 
 using namespace at::sparse;
-using at::cuda::detail::TensorInfo;
-using at::cuda::detail::getTensorInfo;
-
-namespace {
-
-template <typename scalar_t>
-C10_LAUNCH_BOUNDS_1(1024)
-__global__ void _sparse_mask_copy_kernel(
-    int64_t total_threads,
-    int64_t t_nnz,
-    const TensorInfo<int64_t, int64_t> t_indices_ti,
-    const TensorInfo<int64_t, int64_t> mask_indices_ti,
-    const TensorInfo<int64_t, int64_t> t_indices_pos_ti,
-    const TensorInfo<scalar_t, int64_t> t_values_ti,
-    TensorInfo<scalar_t, int64_t> r_values_ti) {
-  const int64_t i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i >= total_threads) return;
-  const int64_t j = t_indices_pos_ti.data[i];
-
-  bool has_match = false;
-  if (j >= 0 &&  j < t_nnz && t_indices_ti.data[j] == mask_indices_ti.data[i]) {
-    has_match = true;
-  }
-
-  int64_t values_stride0 = r_values_ti.strides[0];
-  int64_t out_start = i * values_stride0;
-  int64_t out_end = (i + 1) * values_stride0;
-  int64_t in_start = j * t_values_ti.strides[0];
-
-  if (has_match) {
-    for (int64_t out_i = out_start, in_i = in_start; out_i < out_end; out_i++, in_i++) {
-      r_values_ti.data[out_i] = t_values_ti.data[in_i];
-    }
-  }
-}
-
-} // end namespace
 
 SparseTensor _coalesce_sparse_cuda(const SparseTensor& self) {
   int64_t nnz = self._nnz();
@@ -204,98 +166,4 @@ SparseTensor _coalesce_sparse_cuda(const SparseTensor& self) {
   return dst;
 }
 
-Tensor sparse_mask_helper_cuda(
-    const SparseTensor& t,
-    const Tensor& mask_indices) {
-  /*
-    This is a helper function which filter values from `t._values()` using the
-    `mask_indices`. This CUDA implementation uses `thrust::lower_bound`
-    operation to find the intersection of the `mask_indices` and the
-    `t._indices()` to then filter the values.
-
-    Inputs:
-      `t`             - coalesced sparse tensor input
-      `mask_indices`  - mask indices tensor
-
-    Note: The nnz in the output tensor will be same as the `mask_indices`. So it will
-    works independently if the mask is coalesced or not.
-  */
-  TORCH_CHECK(t.is_sparse(), "t: input is not a sparse tensor");
-  TORCH_CHECK(t.is_coalesced(), "t:  input is uncoalesced");
-  TORCH_CHECK(mask_indices.dim() == t._indices().dim(), "mask_indices: operands have incompatible indices dim; self has dim ",
-      t._indices().dim(), " but mask has dim ", mask_indices.dim());
-  TORCH_CHECK(mask_indices.is_contiguous(), "mask_indices: mask is not contiguous");
-
-  int64_t r_nnz = mask_indices.size(1);
-  auto t_values = t._values().contiguous();
-  auto full_size = t.sizes();
-  auto vsize = t_values.sizes().vec();
-  vsize[0] = r_nnz;
-
-
-  if (t.sparse_dim() == 0) {
-    Tensor t_values_expand = t_values;
-    t_values_expand = t_values_expand.expand(vsize).contiguous();
-    return t_values_expand;
-  }
-  Tensor r_values = at::zeros({vsize}, t_values.options());
-  auto t_indices = t._indices().contiguous();
-  auto t_nnz = t._nnz();
-
-  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  at::cuda::ThrustAllocator allocator;
-  auto policy = thrust::cuda::par(allocator).on(stream);
-
-  // Step 1: flatten the sparse indices `t._indices()` tensor into a 1D indices
-  // tensor `t_flatten_indices`.
-  auto t_flatten_indices = at::sparse::flatten_indices(t_indices, full_size).contiguous();
-
-  // Step 2: flatten the sparse indices `mask_indices` tensor into a 1D indices
-  // tensor `mask_flatten_indices`. Note: This could be not sorted if the input
-  // indices in the constructor are not in a coalesced form
-  auto flattened_mask_indices =
-      at::sparse::flatten_indices(mask_indices, full_size);
-
-  Tensor t_indices_pos = at::empty({r_nnz}, mask_indices.options());
-
-  // Step 3: Match the flattened `mask_indices` with the flattened
-  // `t._indices()` using the `thrust::lower_bound`
-  thrust::lower_bound(
-      policy,
-      t_flatten_indices.data_ptr<int64_t>(),
-      t_flatten_indices.data_ptr<int64_t>() + t_nnz,
-      flattened_mask_indices.data_ptr<int64_t>(),
-      flattened_mask_indices.data_ptr<int64_t>() + r_nnz,
-      t_indices_pos.data_ptr<int64_t>());
-
-  // Step 4: Copy the Filtered `t._values()` using the matches at `t_indices_pos`
-  if (r_nnz > 0 && t_values.numel() > 0) {
-    int64_t block_size = std::min(at::cuda::getCurrentDeviceProperties()->maxThreadsPerBlock, 1024);
-    auto grid_size = ceil_div(r_nnz, block_size);
-
-    auto t_indices_ti = getTensorInfo<int64_t, int64_t>(t_flatten_indices);
-    auto mask_indices_ti =
-        getTensorInfo<int64_t, int64_t>(flattened_mask_indices);
-    auto t_indices_pos_ti =
-        getTensorInfo<int64_t, int64_t>(t_indices_pos);
-
-    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND1(kHalf,
-        r_values.scalar_type(), "sparse_mask_helper_cuda", [&] {
-          auto t_values_ti = getTensorInfo<scalar_t, int64_t>(t_values);
-          auto r_values_ti =
-              getTensorInfo<scalar_t, int64_t>(r_values);
-
-          _sparse_mask_copy_kernel<scalar_t><<<grid_size, block_size, 0, stream>>>(
-              r_nnz,
-              t_nnz,
-              t_indices_ti,
-              mask_indices_ti,
-              t_indices_pos_ti,
-              t_values_ti,
-              r_values_ti);
-          C10_CUDA_KERNEL_LAUNCH_CHECK();
-        });
-  }
-  return r_values;
-}
 }} // namespace at::native
