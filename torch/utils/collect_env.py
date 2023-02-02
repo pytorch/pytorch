065a62056a714b3d17222ab94434522e8393cc69
Jing Xu <jing.xu@intel.com>
2023-02-02 11:27:20 +0900

    Update torch/utils/collect_env.py
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>

--- torch/utils/collect_env.py
+++ torch/utils/collect_env.py
@@ -213,6 +213,8 @@ def get_cpu_info(run_lambda):
     elif get_platform() == 'win32':
         rc, out, err = run_lambda('wmic cpu get Name,Manufacturer,Family,Architecture,ProcessorType,DeviceID,\
         CurrentClockSpeed,MaxClockSpeed,L2CacheSize,L2CacheSpeed,Revision /VALUE')
+    elif get_platform() == 'darwin':
+        rc, out, err = run_lambda("sysctl -n machdep.cpu.brand_string")
     cpu_info = 'None'
     if rc == 0:
         cpu_info = out
