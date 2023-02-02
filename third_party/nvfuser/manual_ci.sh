03069a7dc5a6963a01bff2b63e6ca302dfe0bb35
jjsjann123 <jiej@nvidia.com>
2023-02-02 13:02:31 -0800

    updating scripts for ci test (#2389)

--- third_party/nvfuser/manual_ci.sh
+++ third_party/nvfuser/manual_ci.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+
+failed_tests=false
+
+run_test() {
+  eval "$1"
+  status=$?
+  if [ $status -ne 0 ];
+  then
+    failed_tests=true
+    echo "============================================================="
+    echo "= test_failed!"
+    echo "= $1"
+    echo "============================================================="
+  fi
+}
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
-./bin/nvfuser_tests
-python python_tests/test_dynamo.py
-python python_tests/test_python_frontend.py
-PYTORCH_TEST_WITH_SLOW=1 python python_tests/test_torchscript.py
+
+run_test './bin/nvfuser_tests'
+run_test 'python python_tests/test_dynamo.py'
+run_test 'python python_tests/test_python_frontend.py'
+run_test 'PYTORCH_TEST_WITH_SLOW=1 python python_tests/test_torchscript.py'
+
+if $failed_tests;
+then
+  echo "=== CI tests failed, do NOT merge your PR! ==="
+  exit 1
+else
+  echo "=== CI tests passed, ship it! ==="
+  exit 0
+fi
