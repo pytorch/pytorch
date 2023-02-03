6506172bef8fb35b0704dac8c5245c8f98430b24
cyy <cyyever@outlook.com>
2023-02-04 00:16:07 +0800

    Update cmake/Modules/FindGoogleSanitizer.cmake
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>

--- cmake/Modules/FindGoogleSanitizer.cmake
+++ cmake/Modules/FindGoogleSanitizer.cmake
@@ -11,9 +11,11 @@ get_property(languages GLOBAL PROPERTY ENABLED_LANGUAGES)
 
 set(_source_code
     [==[
-  #include <stdio.h>
-  int main() {
-  printf("hello world!");
+  #include <stdlib.h>
+  int main(int argc, char *argv[]) {
+  int idx = argc > 1 ? atoi(argv) : 1;
+  int return_codes[] = {0, 1, 2};
+  return return_codes[idx];
   return 0;
   }
   ]==])
