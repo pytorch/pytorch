36f57adac8fd0020eb78058f76f274aa2dcd5cb6
Michael Lazos <mlazos@fb.com>
2023-02-04 01:35:24 +0000

    Remove unused var

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -61,9 +61,6 @@ class BaseSchedulerNode:
         self.max_order: Optional[int] = None
         self.last_usage: Set[str] = None  # buffers that won't be used after this kernel
         self.written = False
-        self.prunable_dep_names: Set[
-            str
-        ] = set()  # these deps can be removed if after fusion they are redundant
 
     def __repr__(self):
         return f"{type(self).__name__}(name={self.get_name()!r})"
