3650bb722b2e66fe32e8fd70d601119d5cbaa5f8
Ramin Azarmehr <razarmehr@apple.com>
2023-02-04 14:45:28 -0500

    Fix wrong results in AvgPool2D when count_include_pad=True (#273)
    - Add divisor to cached string key to avoid conflicts
    - Fallback to CPU when divisor is passed for int64 input types
    - Remove MaxPool2D from UNIMPLEMENTED_OP list (was put there by mistake)
    - Add test case when both ceil_mode and count_include_pad are True (previously failed).

--- test/test_mps.py
+++ test/test_mps.py
@@ -4764,6 +4764,19 @@ class TestNLLLoss(TestCase):
         helper(torch.half, torch.long)
         helper(torch.float, torch.int)
 
+    def test_avg_pool2d_count_include_pad(self):
+        cpu_x = torch.randn((1, 3, 9, 9), device='cpu', dtype=torch.float, requires_grad=True)
+        x = cpu_x.detach().clone().to('mps').requires_grad_()
+        pool = torch.nn.AvgPool2d(kernel_size=(3, 3), padding=(1, 1), stride=(1, 1), ceil_mode=True, count_include_pad=True)
+        ref_y = pool(cpu_x)
+        y = pool(x)
+        self.assertEqual(y, ref_y)
+        cpu_grad = torch.randn(ref_y.shape)
+        grad = cpu_grad.to('mps')
+        ref_y.backward(gradient=cpu_grad)
+        y.backward(gradient=grad)
+        self.assertEqual(x.grad, cpu_x.grad)
+
     # Test adaptive avg pool2d - when the input size is a multiple of output size
     # Not testing for channels last right now
     def test_adaptive_avg_pool2d_simple(self):
@@ -8752,8 +8765,6 @@ class TestConsistency(TestCase):
         'square': [torch.bool, torch.int16, torch.int32, torch.int64, torch.uint8],  # moved from section below
 
         # failure in average pooling when both ceilMode and includeZeroPadToAverage are True
-        'nn.functional.avg_pool1d': [torch.float32, torch.int64],
-        'nn.functional.avg_pool2d': [torch.float32, torch.int64],
         'nn.functional.adaptive_avg_pool1d': [torch.float32],
         'nn.functional.adaptive_avg_pool2d': [torch.float32],
         # count_nonzero returns wrong results for these dtypes
