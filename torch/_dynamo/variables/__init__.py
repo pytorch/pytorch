2362b5fca332454b04097998596b3f4fce795d22
Yanbo Liang <ybliang8@gmail.com>
2023-02-05 04:52:43 +0000

    [Dynamo] Put torch.cuda.stream into Dynamo FX graph (#93808)
    Fixes #92804
    
    This PR only handles ```torch.cuda.stream```. If this is a right direction, I'll add support for several relevant functions, e.g, ```torch.cuda.current_stream().wait_stream(s)```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/93808
    Approved by: https://github.com/jansel

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -21,6 +21,8 @@ from .misc import (
     BlackHoleVariable,
     ClosureVariable,
     ContextWrappingVariable,
+    CUDAStreamContextVariable,
+    CUDAStreamVariable,
     GetAttrVariable,
     GradModeVariable,
     InspectSignatureVariable,
