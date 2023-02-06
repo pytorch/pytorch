9044ca5267a760fb1073cc9ba3dda4b9bfaf28c3
Iris <wz337@cornell.edu>
2023-02-06 13:56:07 +0000

    [DCP] Fix test_file_system_checkpoint.py and test_file_system_checkpoint_cpu.py (#94069)
    This fixes the typo in assert that would always return True and adds missing import.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94069
    Approved by: https://github.com/kumpera

--- test/distributed/checkpoint/test_file_system_checkpoint.py
+++ test/distributed/checkpoint/test_file_system_checkpoint.py
@@ -1,6 +1,7 @@
 # Owner(s): ["oncall: distributed"]
 
 import os
+import sys
 import shutil
 import tempfile
 from typing import Dict
@@ -74,7 +75,7 @@ def assert_state_dict_equal(
                 value_1.local_shards(), value_2.local_shards()
             ):
                 self.assertTrue(
-                    torch.equal(local_shard_1.tensor, local_shard_1.tensor),
+                    torch.equal(local_shard_1.tensor, local_shard_2.tensor),
                     f"Key {key}'s shard does not match",
                 )
         elif isinstance(value_1, torch.Tensor):
