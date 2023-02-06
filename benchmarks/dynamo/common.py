11a3b26afb18c000e485626743bad7a61d16461d
Jason Ansel <jansel@fb.com>
2023-02-06 09:04:45 -0800

    Re-enable cudagraphs for benchmark scripts
    Related to https://github.com/pytorch/pytorch/pull/93253
    
    [ghstack-poisoned]

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -1175,8 +1175,9 @@ class BenchmarkRunner:
                 model = DDP(model, find_unused_parameters=True)
             elif self.args.fsdp:
                 model = FSDP(model, use_orig_params=True)
-                torch._inductor.config.triton.cudagraphs = False
-                log.warn("Disabling cudagraphs for FSDP compatibility")
+                if torch._inductor.config.triton.cudagraphs:
+                    log.warning("Disabling cudagraphs for FSDP compatibility")
+                    torch._inductor.config.triton.cudagraphs = False
             return model
 
         # Collect the fp64 reference outputs to be used later for accuracy checking.
@@ -2067,8 +2068,7 @@ def run(runner, args, original_dir=None):
         output_filename = "coverage.csv"
 
     if args.inductor or args.backend == "inductor":
-        if args.disable_cudagraphs:
-            inductor_config.triton.cudagraphs = False
+        inductor_config.triton.cudagraphs = not args.disable_cudagraphs
 
     runner.setup_amp()
 
