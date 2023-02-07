676a964ce2e8ac711605ebcfa7950427eac3ad57
Huy Do <huydhn@gmail.com>
2023-02-06 21:15:30 -0800

    Also setup node

--- .ci/docker/common/install_linter.sh
+++ .ci/docker/common/install_linter.sh
@@ -3,9 +3,12 @@
 set -ex
 
 if [ -n "${UBUNTU_VERSION}" ]; then
-  sudo apt-get install -y doxygen
+  sudo apt-get install -y doxygen nodejs npm
 fi
 
 # Install all linter dependencies
 python3 -mpip install -r .github/requirements-gha-cache.txt --user
 lintrunner init
+
+# Node dependencies required by toc linter job
+npm install -g markdown-toc
