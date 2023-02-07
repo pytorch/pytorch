6e2635cd3edf97c059283cf8260d9485701df638
Nikita Karetnikov <nikita@karetnikov.org>
2023-02-07 04:42:46 +0100

    [inductor] bug fix: use `create_symbolic_sizes_strides_storage_offset` (#94031)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94031
    Approved by: https://github.com/ezyang

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -5487,7 +5487,6 @@ test_skips = {
     "test_cudnn_rnn_dynamic_shapes": ("cuda",),
     "test_grid_sampler_2d_dynamic_shapes": ("cpu", "cuda"),
     "test_kwargs_dynamic_shapes": ("cpu",),
-    "test_list_clearing_dynamic_shapes": ("cpu", "cuda"),
     "test_lowmem_dropout1_dynamic_shapes": ("cpu", "cuda"),
     "test_lowmem_dropout2_dynamic_shapes": ("cpu", "cuda"),
     "test_nll_loss_forward_dynamic_shapes": ("cpu", "cuda"),
