e0844b21d0c54169cb7cd46fb689b687cfb75845
Huy Do <huydhn@gmail.com>
2023-02-06 21:05:41 -0800

    Add more dependencies

--- .ci/docker/common/install_linter.sh
+++ .ci/docker/common/install_linter.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
+if [ -n "${UBUNTU_VERSION}" ]; then
+  sudo apt-get install -y doxygen
+fi
+
+# Install all linter dependencies
+python3 -mpip install -r .github/requirements-gha-cache.txt --user
 lintrunner init
