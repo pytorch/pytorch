89a4ce87ec5bedf0b59ad92cc6267cb0752e5da6
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-07 14:38:14 -0600

    Add files via upload

--- .ci/pytorch/win-build.sh
+++ .ci/pytorch/win-build.sh
@@ -50,6 +50,8 @@ fi
 
 echo $BUILD_TYPE
 
+"$SCRIPT_HELPERS_DIR"/list_path.bat
+
 export PATH="C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;"${PATH}
 
 echo $PATH
