daebcb41adb4757de063fa0f7ec870b552587a01
Emilio Castillo <ecastill@preferred.jp>
2023-02-07 04:14:24 +0000

    lint

--- torch/csrc/cuda/CUDAPluggableAllocator.cpp
+++ torch/csrc/cuda/CUDAPluggableAllocator.cpp
@@ -91,7 +91,7 @@ void* CUDAPluggableAllocator::malloc(
   void* r = nullptr;
   int error = alloc_fn_(&r, size, device, stream);
   TORCH_CHECK(
-      error,"Memory allocation failed with error " + std::to_string(error));
+      error, "Memory allocation failed with error " + std::to_string(error));
   {
     const std::lock_guard<std::mutex> lock(allocator_mutex_);
     allocation_metadata_.emplace(r, _AllocationMetadata(size, device, stream));
