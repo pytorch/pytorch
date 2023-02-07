f7d1d1558fbc04e0838c69ce9f2c029bbb356eb2
Kulin Seth <kulin_seth@apple.com>
2023-02-07 15:52:42 +0000

    [MPS] Don't reset the Graph state (#94283)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94283
    Approved by: https://github.com/razarmehr

--- aten/src/ATen/native/mps/OperationUtils.mm
+++ aten/src/ATen/native/mps/OperationUtils.mm
@@ -332,7 +332,6 @@ void resize_tensor(Tensor* output) {
 
 MPSGraph* make_mps_graph() {
   MPSGraph* mpsGraph = [[MPSGraph new] autorelease];
-  mpsGraph.options = MPSGraphOptionsNone;
   return mpsGraph;
 }
 
