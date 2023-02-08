5b4b7dd2e26045b5859cfcaa63c6ed12e9b08ff0
Huy Do <huydhn@gmail.com>
2023-02-07 22:29:20 -0800

    Retrofit requirement-flake8.txt

--- .ci/docker/common/install_linter.sh
+++ .ci/docker/common/install_linter.sh
@@ -9,9 +9,12 @@ if [ -n "${UBUNTU_VERSION}" ]; then
   apt-get install -y doxygen nodejs npm
 fi
 
-# Install all linter dependencies
+# Install all linter dependencies. The lintrunner init step would still need to be
+# run later in the CI because the initialization requires some custom scripts from
+# PyTorch. The good news is having these dependencies installed here would act as
+# a cache, so lintrunner init wouldn't need to download them all again
 pip_install -r /opt/conda/requirements.txt
-conda_run lintrunner init
+pip_install -r /opt/conda/requirements-linter.txt
 
 # Node dependencies required by toc linter job
 npm install -g markdown-toc
