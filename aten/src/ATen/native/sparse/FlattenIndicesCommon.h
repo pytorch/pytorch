65ca9fc6cf70ba0ad6a2904fc675e2b35a57c9e6
Nikita Vedeneev <nik@quansight.com>
2023-02-08 16:06:03 +0000

    flatten_indices: remove syncs
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/sparse/FlattenIndicesCommon.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <ATen/Tensor.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/sparse/Macros.h>
+#include <ATen/ExpandUtils.h>
+#include <ATen/SparseTensorUtils.h>
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/arange.h>
+#include <ATen/ops/tensor.h>
+#endif
+
+#ifdef GPUCC
+#define NAME "flatten_indices_cuda"
+#else
+#define NAME "flatten_indices_cpu"
+#endif
+
+namespace at::native {
+
+namespace {
+
+template <template <typename func_t> class kernel_t>
+struct KernelLauncher {
+  template <typename func_t>
+  static void launch(TensorIteratorBase& iter, const func_t& f) {
+    kernel_t<func_t>::launch(iter, f);
+  }
+};
+
+template <
+  template <typename func_t> class kernel_t,
+  int64_t max_static_len = 0>
+Tensor _flatten_indices_impl(const Tensor& indices, IntArrayRef size) {
+  TORCH_INTERNAL_ASSERT(indices.dim() > 1 && indices.size(0) == size.size());
+
+  // Need owning storage in case of the Tensor class.
+  const auto hash_coeffs_storage = [&]() -> auto {
+    auto strides = c10::contiguous_strides(size);
+
+    if constexpr (max_static_len > 0) {
+      std::array<int64_t, max_static_len> strides_as_array;
+      std::copy(strides.begin(), strides.end(), strides_as_array.begin());
+      return strides_as_array;
+    } else {
+      auto strides_as_tensor = at::tensor(strides, indices.options().device(kCPU).dtype(kLong));
+      strides_as_tensor = strides_as_tensor.to(indices.device());
+      return strides_as_tensor;
+    }
+  }();
+
+  const auto hash_coeffs = [&]() -> auto {
+    if constexpr (max_static_len > 0) {
+      return hash_coeffs_storage;
+    } else {
+      return hash_coeffs_storage.template data_ptr<int64_t>();
+    }
+  }();
+
+  const auto hash_indices = [&]() -> Tensor {
+    // non-const because of gcc-5/clang-5 issues
+    auto sparse_dim = indices.dim();
+    auto indices_dim_stride = indices.stride(0);
+    auto indices_nnz_stride = indices.stride(1);
+
+    auto hash = at::arange(indices.size(1), indices.options());
+
+    auto iter = TensorIteratorConfig()
+      .set_check_mem_overlap(false)
+      .add_output(hash)
+      .add_input(hash)
+      .build();
+
+    {
+      const auto* RESTRICT ptr_indices = indices.data_ptr<int64_t>();
+
+      KernelLauncher<kernel_t>::launch(iter,
+          // NOTE: capture by value required by CUDA
+          [=] FUNCAPI (int64_t nnz_idx) -> int64_t {
+          const auto* RESTRICT ptr_indices_dim = ptr_indices + nnz_idx * indices_nnz_stride;
+          auto hash = static_cast<int64_t>(0);
+          for (int64_t dim = 0; dim < sparse_dim; ++dim) {
+            const auto dim_hash_coeff = hash_coeffs[dim];
+            const auto dim_index = ptr_indices_dim[dim * indices_dim_stride];
+            hash += dim_index * dim_hash_coeff;
+          }
+          return hash;
+      });
+    }
+
+    return hash;
+  }();
+
+  return hash_indices;
+}
+
+template <template <typename func_t> class kernel_t>
+Tensor _flatten_indices(const Tensor& indices, IntArrayRef size) {
+  TORCH_CHECK(indices.dim() > 1 && indices.size(0) == size.size(),
+      NAME, "(): the dimensionality of sparse `indices` and the lenght of `size` must match. ",
+            "Got `indices.size(0) == ", indices.size(0), "` != `size.size() == ", size.size(), "`.");
+  constexpr int64_t max_sparse_dims = 8;
+  if (indices.dim() <= max_sparse_dims) {
+    return _flatten_indices_impl<kernel_t, max_sparse_dims>(indices, size);
+  } else {
+    return _flatten_indices_impl<kernel_t>(indices, size);
+  }
+}
+
+}
+
+} // at::native
