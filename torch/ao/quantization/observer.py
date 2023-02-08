8b070f8310951cb7a4d3c0303f720764aa9d098a
Jacob Szwejbka <jakeszwe@meta.com>
2023-02-08 10:54:46 -0800

    [Executorch][Quant][BE] Refactor Choose_Qparams (#94338)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94338
    
    Refactor so that it can be decomposed
    
    Test Plan: ci
    
    Reviewed By: jerryzh168
    
    Differential Revision: D42681268
    
    fbshipit-source-id: 072fd62ea40a8b7134733a994ccd9e6f04063b1e

--- torch/ao/quantization/observer.py
+++ torch/ao/quantization/observer.py
@@ -13,7 +13,7 @@ from typing import Any, List, Tuple, Optional, Dict
 import torch
 import torch.nn as nn
 from torch.ao.quantization.utils import (
-    check_min_max_valid, calculate_qmin_qmax, is_per_tensor, is_per_channel)
+    check_min_max_valid, calculate_qmin_qmax, is_per_tensor, is_per_channel, validate_qmin_qmax)
 
 __all__ = [
     "default_affine_fixed_qparams_observer",
@@ -236,7 +236,7 @@ class UniformQuantizationObserverBase(ObserverBase):
         ), "Default Observer only works for qint8, quint8 and quint4x2 data type"
         self.has_customized_qrange = (quant_min is not None) and (quant_max is not None)
         if self.has_customized_qrange:
-            self._validate_qmin_qmax(quant_min, quant_max)
+            validate_qmin_qmax(quant_min, quant_max)
         self.quant_min, self.quant_max = \
             calculate_qmin_qmax(quant_min, quant_max, self.has_customized_qrange, self.dtype, self.reduce_range)
 
@@ -307,6 +307,11 @@ class UniformQuantizationObserverBase(ObserverBase):
             scales: Scales tensor of shape (#channels,)
             zero_points: Zero points tensor of shape (#channels,)
         """
+        # Functionally equivalent to 'determine_qparams' in utils.py. Observers must be torchscriptable however and qscheme
+        # as far as I can tell is not allowed to passed as a parameter in torchscript functions. This makes refactoring observer
+        # to use this utility a massive pain and very gross. For now Im opting just to duplicate as this code
+        # seems unlikey to change (last update over 1 year ago) and when torchscript is fully deprecated we can refactor.
+        # TODO(jakeszwe, jerryzh168)
         if not check_min_max_valid(min_val, max_val):
             return torch.tensor([1.0], device=min_val.device.type), torch.tensor([0], device=min_val.device.type)
 
