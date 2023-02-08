fdd9b6ad6f70d9989839552aa7bd4acb9a186870
Will Constable <whc@fb.com>
2023-02-08 01:06:13 +0000

    Update base for Update on "Inductor support for aten::all_reduce"
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen chunyuan-w XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -33,6 +33,7 @@ from .ir import (
     Reduction,
     SqueezeView,
     TensorBox,
+    validate_ir,
     View,
 )
 from .utils import ceildiv, sympy_product
@@ -221,7 +222,10 @@ def _register_lowering(
                         args[i], list(args[indices[0]].get_size())
                     )
 
-        return decomp_fn(*args, **kwargs)
+        out = decomp_fn(*args, **kwargs)
+        validate_ir(out)
+
+        return out
 
     if not isinstance(aten_fn, (list, tuple)):
         aten_fn = [aten_fn]
@@ -1173,7 +1177,7 @@ def philox_rand_like(x, seed, offset):
 
 
 def require_dense(_, *args, **kwargs):
-    args, kwargs = pytree.tree_map_only(
+    args, kwargs = pytree._only(
         ir.IRNode, lambda t: ir.ExternKernel.require_stride1(t), (args, kwargs)
     )
     return args, kwargs
