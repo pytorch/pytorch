1df2f8dec2ea791bff836e433c444f852d30d637
blorange-amd <bo.li2@amd.com>
2023-02-09 06:12:28 +0000

    Removed CUDA version check flag

--- test/test_matmul_cuda.py
+++ test/test_matmul_cuda.py
@@ -42,7 +42,7 @@ class TestMatmulCuda(TestCase):
         super(self.__class__, self).tearDown()
 
     @onlyCUDA
-    @unittest.skipIf(TEST_WITH_ROCM, "Only CUDA 11+ is supported")
+    @skipIfRocmVersionLessThan((5, 2))
     # imported 'tol' as 'xtol' to avoid aliasing in code above
     @toleranceOverride({torch.float16: xtol(atol=1e-1, rtol=1e-1),
                         torch.bfloat16: xtol(atol=1e-1, rtol=1e-1),
