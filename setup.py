3289296f78ff7cf829c409bf555055024de18512
Jeff Daily <jeff.daily@amd.com>
2023-02-09 09:46:15 -0800

    do not duplicate code in setup.py

--- setup.py
+++ setup.py
@@ -1022,19 +1022,12 @@ def main():
         'opt-einsum': ['opt-einsum>=3.3']
     }
     if platform.system() == 'Linux':
-        if os.path.exists('/opt/rocm'):
-            # assume rocm build
-            triton_pin_file = os.path.join(cwd, ".github", "ci_commit_pins", "triton-rocm.txt")
-            if os.path.exists(triton_pin_file):
-                with open(triton_pin_file) as f:
-                    triton_pin = f.read().strip()
-                    extras_require['dynamo'] = ['pytorch-triton-rocm==2.0.0+' + triton_pin[:10], 'jinja2']
-        else:
-            triton_pin_file = os.path.join(cwd, ".github", "ci_commit_pins", "triton.txt")
-            if os.path.exists(triton_pin_file):
-                with open(triton_pin_file) as f:
-                    triton_pin = f.read().strip()
-                    extras_require['dynamo'] = ['pytorch-triton==2.0.0+' + triton_pin[:10], 'jinja2']
+        pin_name, pkg_name = ("pytorch-triton", "triton.txt") if not os.path.exists('/opt/rocm') else ("pytorch-triton-rocm", "triton-rocm.txt")
+        triton_pin_file = os.path.join(cwd, ".github", "ci_commit_pins", "triton.txt")
+        if os.path.exists(triton_pin_file):
+            with open(triton_pin_file) as f:
+                triton_pin = f.read().strip()
+                extras_require['dynamo'] = [f'{pkg_name}==2.0.0+' + triton_pin[:10], 'jinja2']
 
     # Parse the command line and check the arguments before we proceed with
     # building deps and setup. We need to set values so `--help` works.
