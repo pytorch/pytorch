3281e933024d54b3368263e371661cd3082219aa
Will Constable <whc@fb.com>
2023-02-09 23:29:21 +0000

    Update base for Update on "Basic dynamo support for traceable allreduce"
    Attempt to make traceable allreduce work with torchdynamo,
    bypassing problems with tracing the AsyncTensor subclass.
    
    Accept a suboptimal solution for now, and optimize it later.
    The long term solution should be "torchdispatch tensor subclass support", which is WIP
    
    Option 1: Dynamo inlines Traceable Collectives, and handles AsyncTensor subclass
    - AsyncTensor needs special handling since it is an unsupported subclass
    - Special-case Dynamo to perform the simple function of AsyncTensor on its behalf:
      insert a 'Wait' op either "right after" the op that returned AsyncTensor (least optimal)
      or right before first user of AsyncTensor (optimal but more complicated)
    
      (a) add 'torch.distributed.traceable_collectives'
    	  to `_dynamo.config.skipfiles_inline_module_allowlist`
      (b) implement AsyncTensor special behavior
    
    Option 2: Dynamo allows traceable collectives in graph, doesn't inline
    - we avoid Dynamo worrying about AsyncTensor at all. Just let it come up later,
      when doing dispatch tracing during AotAutograd
      (a) maybe the default already allows this op in graph!
      (b) still need AsyncTensor handling? Bc we seem to be faketensor tracing the op,
    	  which fails when wrapping a FakeTensor in an AsyncTensor
    
    cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -80,11 +80,7 @@ In these cases, the underlying StorageBox/Buffer will be shared with the pre-vie
 """
 
 
-def validate_lowering_output(node_or_nodes):
-    """
-    This is a lightweight IR validation.  It doesn't validate the whole graph,
-    Just makes sure a particular lowering returned a reasonable type.
-    """
+def validate_ir(node_or_nodes):
     def _check_tensorbox(node):
         # Could expand this to check deeper properties
         # (e.g. TensorBox points to View or StorageBox)
