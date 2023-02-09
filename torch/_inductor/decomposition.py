66ae3aa096f897dbbd0ae2011c63821b176dd3a1
min-jean-cho <min.jean.cho@intel.com>
2023-02-09 00:02:52 +0000

    [Inductor] added aten.cauchy_ decomp (#92047)
    Fixes #91675
    
    TODO: compare perf of decomposed tan --vs-- libdevice tan, aten tan for triton, cpp backeneds
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92047
    Approved by: https://github.com/jgong5, https://github.com/jansel, https://github.com/lezcano, https://github.com/ngimel

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -376,7 +376,14 @@ We put these decomps in a separate table `extra_random_decomps` to allow
 turning them on and off via `config.fallback_random`.
 """
 extra_random_decomps = get_decompositions(
-    [aten.native_dropout, aten.exponential, aten.exponential_, aten.uniform_]
+    [
+        aten.native_dropout,
+        aten.cauchy,
+        aten.cauchy_,
+        aten.exponential,
+        aten.exponential_,
+        aten.uniform_,
+    ]
 )
 register_extra_random_decomp = functools.partial(
     decomp.register_decomposition, registry=extra_random_decomps
