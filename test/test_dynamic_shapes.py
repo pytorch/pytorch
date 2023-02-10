bc1c2ddef913ed8f8f0bc4cc43834945cfd0c771
Edward Z. Yang <ezyang@meta.com>
2023-02-10 21:11:40 +0000

    Prevent sym_int from showing up in FX graph (#94595)
    Apply the optimization to floor instead of sym_int
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94595
    Approved by: https://github.com/ngimel, https://github.com/bdhirsh

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -17,10 +17,10 @@ from torch.utils._pytree import tree_map
 from torch.fx.experimental import symbolic_shapes
 from torch.fx.experimental.proxy_tensor import make_fx
 from torch.fx.experimental.symbolic_shapes import SymNode, \
-    FloorDiv, ShapeEnv, sym_sqrt, sym_int, sym_float, to_node, GuardOnDataDependentSymNode, \
+    FloorDiv, ShapeEnv, sym_sqrt, sym_float, to_node, GuardOnDataDependentSymNode, \
     guard_bool, guard_int, guard_float
 from torch.utils._python_dispatch import TorchDispatchMode
-from torch import SymBool, SymInt, SymFloat
+from torch import SymBool, SymInt, SymFloat, sym_int
 
 aten = torch.ops.aten
 
