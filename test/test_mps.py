70df78dc26254ad4b814d8c2bcc36346132fe6e6
Kulin Seth <kulin_seth@apple.com>
2023-02-09 23:08:38 -0800

    Fix lint issues.

--- test/test_mps.py
+++ test/test_mps.py
@@ -8876,8 +8876,6 @@ class TestConsistency(TestCase):
         'triangular_solve': ['f32'],
         '_native_batch_norm_legit': ['f32'],
         'native_batch_norm': ['f32'],
-        'nn.functional.nll_loss': ['f32'],
-        'masked.var': ['b8', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'minreduction_with_dim': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'maxreduction_with_dim': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
     }
@@ -8893,7 +8891,6 @@ class TestConsistency(TestCase):
         'masked.softmax': ['f32'],
         'masked.softmin': ['f32'],
         'masked.std': ['f32'],
-        'masked.var': ['f32'],
         'abs': ['f16', 'f32'],
         'acos': ['f32'],
         'acosh': ['f32'],
