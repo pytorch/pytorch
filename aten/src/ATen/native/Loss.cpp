47e67abdf76587483190c3e575493c8ac334c158
Driss Guessous <drisspg@fb.com>
2023-02-10 20:10:39 +0000

    make bacwkard type promote for mse loss

--- aten/src/ATen/native/Loss.cpp
+++ aten/src/ATen/native/Loss.cpp
@@ -505,6 +505,9 @@ Tensor& mse_loss_backward_out(const Tensor& grad_output,
     .add_input(input)
     .add_input(target)
     .add_input(grad_output)
+    .promote_inputs_to_common_dtype(true)
+    .cast_common_dtype_to_outputs(true)
+    .enforce_safe_casting_to_output(true)
     .build();
   mse_backward_stub(iter.device_type(), iter, norm);
   return grad_input;
