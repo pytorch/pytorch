2cd4cc9ed102298a7884e157ceb4557d74a6f116
Chien-Chin Huang <chienchin@fb.com>
2023-02-10 15:20:50 -0800

    [FSDP][state_dict] Return tensors instead of FlatParameters to avoid pickling errors
    After https://github.com/pytorch/pytorch/pull/88913, user-defined parameter states will be pickled. For a FlatParameter, this means `_local_shard` will also be pickled. Since state_dict and load_state_dict only require the tensor, returning the full FlatParameter does not give us any extra benefit. This PR changes the behavior to simply return a view of the FlatParameter.
    
    Differential Revision: [D43205127](https://our.internmc.facebook.com/intern/diff/D43205127/)
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/_state_dict_utils.py
+++ torch/distributed/fsdp/_state_dict_utils.py
@@ -393,8 +393,7 @@ def _local_post_state_dict_hook(
     shard_offset = flat_param.numel() * fsdp_state.rank
     valid_data_size = flat_param.numel() - flat_param._shard_numel_padded
     if valid_data_size > 0:
-        if flat_param._shard_numel_padded > 0:
-            flat_param = flat_param.narrow(0, 0, valid_data_size)
+        flat_param = flat_param.view(valid_data_size)
         local_shards = [
             Shard.from_tensor_and_offsets(flat_param, [shard_offset], fsdp_state.rank)
         ]
