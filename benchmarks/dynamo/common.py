a936f0c0296c3aa83f1f7697f78f23f30b905ab0
Edward Z. Yang <ezyang@meta.com>
2023-02-11 14:28:30 -0500

    [TEST] Dynamic CI updates
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -175,9 +175,6 @@ CI_SKIP[CI("inductor", training=True)] = [
 
 CI_SKIP[CI("aot_eager", training=False, dynamic=True)] = [
     *CI_SKIP[CI("aot_eager", training=False)],
-    # torchbench
-    "pyhpc_turbulent_kinetic_energy",  # 'SymInt' object has no attribute '__iadd__'
-    "vision_maskrcnn",  # 'SymInt' object has no attribute '__iadd__'
 ]
 
 CI_SKIP[CI("aot_eager", training=True, dynamic=True)] = [
@@ -188,16 +185,6 @@ CI_SKIP[CI("aot_eager", training=True, dynamic=True)] = [
 CI_SKIP[CI("inductor", training=False, dynamic=True)] = [
     *CI_SKIP[CI("aot_eager", training=False, dynamic=True)],
     *CI_SKIP[CI("inductor", training=False)],
-    # torchbench
-    "Background_Matting",  # accuracy
-    "LearningToPaint",  # accuracy
-    "functorch_dp_cifar10",  # timeout
-    "opacus_cifar10",  # timeout
-    "pytorch_unet",  # floor is not defined
-    # timm_models
-    "pnasnet5large",  # ceiling is not defined
-    "swin_base_patch4_window7_224",  # floor is not defined
-    "volo_d1_224",  # ceiling is not defined
 ]
 
 CI_SKIP[CI("inductor", training=True, dynamic=True)] = [
