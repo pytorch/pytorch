2acac8a83a8c4ee56f385bd892eb6ad7a87321b5
mfkasim1 <firman.kasim@gmail.com>
2023-02-13 16:00:52 +0000

    Logcumsumexp for CUDA (build-time optimized) (#94310)
    Hopefully fixes #89205.
    This is another version of #90847 where it was reverted because it increases the compile-time significantly.
    From my discussion with @ngimel in https://github.com/pytorch/pytorch/pull/93153#issuecomment-1409051528, it seems the option of jiterator would be very tricky if not impossible.
    So what I did was to optimize the compile-time in my computer.
    
    To optimize the build time, first I compile the pytorch as a whole, then only change the `LogcumsumexpKernel.cu` file to see how it changes the compile time.
    Here are my results for the compilation time of only the `LogcumsumexpKernel.cu` file in my computer:
    
    - Original version (without any complex implementations): 56s (about 1 minute)
    - The previous PR (#90847): 13m 57s (about 14 minutes)
    - This PR: 3m 35s (about 3.5 minutes)
    
    If the previous PR increases the build time by 30 mins in pytorch's computer, then this PR reduces the increment of build time to about 6 mins. Hopefully this is an acceptable level of build-time increase.
    
    What I did was (sorted by how significant it reduces the build time from the most significant one):
    
    - Substituting `log(x)` to `log1p(x - 1)`. This is applied in the infinite case, so we don't really care about precision.
    - Implementing complex exponential manually
    
    tag: @malfet, @albanD
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94310
    Approved by: https://github.com/Skylion007, https://github.com/malfet

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -16290,9 +16290,9 @@ op_db: List[OpInfo] = [
            ),
     OpInfo('logcumsumexp',
            dtypes=floating_and_complex_types_and(torch.bfloat16),
-           dtypesIfCUDA=floating_types_and(torch.half, torch.bfloat16),
+           dtypesIfCUDA=floating_and_complex_types_and(torch.half, torch.bfloat16),
            backward_dtypes=floating_and_complex_types_and(torch.bfloat16),
-           backward_dtypesIfCUDA=floating_types_and(torch.bfloat16),
+           backward_dtypesIfCUDA=floating_and_complex_types_and(torch.bfloat16),
            skips=(
                # AssertionError: UserWarning not triggered : Resized a non-empty tensor but did not warn about it.
                DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_out_warning', device_type='cuda'),
