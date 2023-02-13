6df7741184ca53f5b23d8ca361c38bbd559c62aa
Justin Chu <justinchu@microsoft.com>
2023-02-13 19:17:21 +0000

    Update on "[ONNX] Remove the deprecated monkey patches to torch.Graph"
    cc ezyang gchanan
    
    [ghstack-poisoned]

--- torch/onnx/symbolic_opset10.py
+++ torch/onnx/symbolic_opset10.py
@@ -9,7 +9,7 @@ import torch.onnx
 from torch import _C
 
 # Monkey-patch graph manipulation methods on Graph, used for the ONNX symbolics
-from torch.onnx import (  # noqa: F401
+from torch.onnx import (
     _constants,
     _type_utils,
     errors,
