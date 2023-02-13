8e4497ae3537b7b16406f05352bce0bd587a9abf
Aidyn-A <31858918+Aidyn-A@users.noreply.github.com>
2023-02-13 13:54:10 -0800

    fix windows build

--- c10/core/impl/InlineDeviceGuard.h
+++ c10/core/impl/InlineDeviceGuard.h
@@ -113,7 +113,7 @@ class InlineDeviceGuard {
   ~InlineDeviceGuard() {
 #ifdef USE_CUDA
     if (original_device_.is_cuda() && original_device_.index() >= 0) {
-      if (at::cuda::detail::hasPrimaryContext(original_device_.index())) {
+      if (at::detail::getCUDAHooks().hasPrimaryContext(original_device_.index())) {
         impl_.uncheckedSetDevice(original_device_);
       }
     } else {
