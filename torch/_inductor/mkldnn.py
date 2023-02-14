Update on "Inductor: fix incorrect result of inplace unsqueeze"
This pr aims to fix the incorrect result in the following test case.
```
torch._dynamo.optimize("inductor")
def fn(a):
    unsqueeze_ = torch.ops.aten.unsqueeze_.default(a, 0)
    return unsqueeze_

args = [
      ((1, 1, 1, 12, 11, 3), (396, 396, 396, 33, 3, 1), torch.int64, "cpu")
       ]
args = [rand_strided(sh, st, dt, dev) for (sh, st, dt, dev) in args]

with torch.no_grad():
    out = fn(*args)

# expected result: (396, 396, 396, 396, 33, 3, 1) torch.Size([1, 1, 1, 1, 12, 11, 3])
print(args[0].stride(), args[0].shape) # incorrect result: (396, 396, 396, 396, 396, 396, 33, 3, 1) torch.Size([1, 1, 1, 1, 1, 1, 12, 11, 3])
```
**Root cause**

1. [fake_tensor](https://github.com/pytorch/pytorch/blob/master/torch/_dynamo/variables/builder.py#L140) is changed during [tracer.run](https://github.com/pytorch/pytorch/blob/master/torch/_dynamo/convert_frame.py#L311), then it will [pass error inputs to inductor](https://github.com/pytorch/pytorch/blob/master/torch/_dynamo/output_graph.py#L670).
2. example_inputs are changed during [propagate](https://github.com/pytorch/pytorch/blob/master/torch/_inductor/mkldnn.py#L509)


cc mlazos soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire 


[ghstack-poisoned]

torch/_inductor/mkldnn.py
@@ -507,7 +507,14 @@ def mkldnn_fuse_fx(gm: torch.fx.GraphModule, example_inputs):
 
     fake_mode = fake_mode_from_tensors(example_inputs)
     # clone inputs to avoid side effects caused by inplace ops during propagate
-    tmp_example_inputs = list(map(lambda x: torch._prims_common.clone_preserve_strides(x) if isinstance(x, torch.Tensor) else copy.deepcopy(x), example_inputs))
+    tmp_example_inputs = list(
+        map(
+            lambda x: torch._prims_common.clone_preserve_strides(x)
+            if isinstance(x, torch.Tensor)
+            else copy.deepcopy(x),
+            example_inputs,
+        )
+    )
     ShapeProp(gm, fake_mode=fake_mode).propagate(*tmp_example_inputs)
     gm = fuse_unary(gm)
     gm = fuse_binary(gm)