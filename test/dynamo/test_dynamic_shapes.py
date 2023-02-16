72e3fbc065ccd06e4afeb72a5f350073006755ae
Michael Voznesensky <voznesenskym@gmail.com>
2023-02-16 22:16:55 +0000

    Better tests

--- test/dynamo/test_dynamic_shapes.py
+++ test/dynamo/test_dynamic_shapes.py
@@ -1,5 +1,4 @@
 # Owner(s): ["module: dynamo"]
-
 from torch._dynamo import config
 from torch._dynamo.testing import make_test_cls_with_patches
 
@@ -25,66 +24,48 @@ except ImportError:
 import unittest
 
 
+test_classes = {}
+
+
 def make_dynamic_cls(cls, assume_static_by_default):
     assume_static_by_default_suffix = (
         "_static_default" if assume_static_by_default else ""
     )
-    return make_test_cls_with_patches(
+    cls_prefix = "StaticDefault" if assume_static_by_default else ""
+    test_class = make_test_cls_with_patches(
         cls,
-        "DynamicShapes",
+        f"{cls_prefix}DynamicShapes",
         f"_dynamic_shapes{assume_static_by_default_suffix}",
         (config, "dynamic_shapes", True),
         (config, "assume_static_by_default", assume_static_by_default),
     )
+    test_classes[test_class.__name__] = test_class
+    # REMOVING THIS LINE WILL STOP TESTS FROM RUNNING
+    globals()[test_class.__name__] = test_class
+    return test_class
 
 
-# Note - this is a little redundant, but some of these tests have specific
-# exclusions.
-# Note #2 - Putting this in a for loop over [True, False] for
-# assume_static_by_default clobbers the name, which makes the tests not run.
-DynamicShapesFunctionTests = make_dynamic_cls(
-    test_functions.FunctionTests, assume_static_by_default=False
-)
-DynamicShapesFunctionTestsDefaultStatic = make_dynamic_cls(
-    test_functions.FunctionTests, assume_static_by_default=True
-)
-DynamicShapesMiscTests = make_dynamic_cls(
-    test_misc.MiscTests, assume_static_by_default=False
-)
-DynamicShapesMiscTestsDefaultStatic = make_dynamic_cls(
-    test_misc.MiscTests, assume_static_by_default=True
-)
-DynamicShapesReproTests = make_dynamic_cls(
-    test_repros.ReproTests, assume_static_by_default=False
-)
-DynamicShapesReproTestsDefaultStatic = make_dynamic_cls(
-    test_repros.ReproTests, assume_static_by_default=True
-)
-DynamicShapesNNModuleTests = make_dynamic_cls(
-    test_modules.NNModuleTests, assume_static_by_default=False
-)
-DynamicShapesNNModuleTestsDefaultStatic = make_dynamic_cls(
-    test_modules.NNModuleTests, assume_static_by_default=True
-)
-DynamicShapesUnspecTests = make_dynamic_cls(
-    test_unspec.UnspecTests, assume_static_by_default=False
-)
-DynamicShapesUnspecTestsDefaultStatic = make_dynamic_cls(
-    test_unspec.UnspecTests, assume_static_by_default=True
-)
-DynamicShapesExportTests = make_dynamic_cls(
-    test_export.ExportTests, assume_static_by_default=False
-)
-DynamicShapesExportTestsDefaultStatic = make_dynamic_cls(
-    test_export.ExportTests, assume_static_by_default=True
-)
-DynamicShapesSubGraphTests = make_dynamic_cls(
-    test_subgraphs.SubGraphTests, assume_static_by_default=False
-)
-DynamicShapesSubGraphTestsDefaultStatic = make_dynamic_cls(
-    test_subgraphs.SubGraphTests, assume_static_by_default=True
-)
+tests = [
+    test_functions.FunctionTests,
+    test_misc.MiscTests,
+    test_repros.ReproTests,
+    test_modules.NNModuleTests,
+    test_unspec.UnspecTests,
+    test_export.ExportTests,
+    test_subgraphs.SubGraphTests,
+]
+for test in tests:
+    for assume_static_by_default in [True, False]:
+        make_dynamic_cls(test, assume_static_by_default=assume_static_by_default)
 
+DynamicShapesReproTests = test_classes["DynamicShapesReproTests"]
+DynamicShapesReproTestsDefaultStatic = test_classes[
+    "StaticDefaultDynamicShapesReproTests"
+]
+DynamicShapesSubGraphTests = test_classes["DynamicShapesSubGraphTests"]
+DynamicShapesSubGraphTestsDefaultStatic = test_classes[
+    "StaticDefaultDynamicShapesSubGraphTests"
+]
 
 unittest.expectedFailure(
     DynamicShapesReproTestsDefaultStatic.test_convert_boxes_to_pooler_format_dynamic_shapes_static_default
