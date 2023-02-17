1a9dc71f5c1364181c179fa3b690c79db5fb40fb
Johnson <j3.soon@msa.hinet.net>
2023-02-17 21:11:00 +0000

    Fix expired deprecation of comparison dtype for NumPy 1.24+ (#91517)
    > The `dtype=` argument to comparison ufuncs is now applied correctly. That
    > means that only `bool` and `object` are valid values and `dtype=object` is
    > enforced.
    
    Source: https://numpy.org/doc/stable/release/1.24.0-notes.html#expired-deprecations
    
    Fixes #91516
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91517
    Approved by: https://github.com/zou3519, https://github.com/huydhn

--- torch/utils/tensorboard/summary.py
+++ torch/utils/tensorboard/summary.py
@@ -380,7 +380,7 @@ def make_histogram(values, bins, max_bins=None):
         limits = new_limits
 
     # Find the first and the last bin defining the support of the histogram:
-    cum_counts = np.cumsum(np.greater(counts, 0, dtype=np.int32))
+    cum_counts = np.cumsum(np.greater(counts, 0))
     start, end = np.searchsorted(cum_counts, [0, cum_counts[-1] - 1], side="right")
     start = int(start)
     end = int(end) + 1
