f43efc0b0aeb28c7de48ae109918c191c386960c
Svetlana Karslioglu <svekars@fb.com>
2023-02-21 10:47:55 -0800

    Update secrets

--- .github/scripts/get-tutorials-stats.py
+++ .github/scripts/get-tutorials-stats.py
@@ -5,9 +5,9 @@ import boto3
 from botocore.exceptions import ClientError
 from pprint import pprint
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+aws-acces-key = os.environ.get('AWS_ACCESS_KEY_ID')
+aws-secret-key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+dynamodb = boto3.resource('dynamodb', region_name="us-east-1", aws_access_key=aws-acces-key, aws_secret_access_key=aws-secret-key)
 
 def run_command(cmd: str, cwd: Optional[str] = None) -> str:
     """
