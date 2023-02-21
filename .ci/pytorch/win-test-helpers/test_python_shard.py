3b96531292a20ddcd65a592dfa2862e6b9452289
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-21 07:32:01 -0600

    Update test_python_shard.py

--- .ci/pytorch/win-test-helpers/test_python_shard.py
+++ .ci/pytorch/win-test-helpers/test_python_shard.py
@@ -8,16 +8,6 @@ import contextlib
 shard_number = os.environ['SHARD_NUMBER']
 
 
-@contextlib.contextmanager
-def pushd(new_dir):
-    previous_dir = os.getcwd()
-    os.chdir(new_dir)
-    try:
-        yield
-    finally:
-        os.chdir(previous_dir)
-
-
 try:
 
     subprocess.run('python ' + os.environ['SCRIPT_HELPERS_DIR'] + '\\setup_pytorch_env.py', shell=True, check=True)
@@ -29,44 +19,43 @@ except Exception as e:
     sys.exit()
 
 
-with pushd('test'):
 
-    gflags_exe = "C:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\gflags.exe"
-    os.environ['GFLAGS_EXE'] = gflags_exe
-
-
-    if shard_number == "1" and exists(gflags_exe):
-
-        subprocess.run('echo Some smoke tests', shell=True)
-
-        try:
-            subprocess.run(gflags_exe + ' /i python.exe +sls', shell=True, check=True)
-
-            subprocess.run('conda run -n test_env' + ' python ' + os.environ['SCRIPT_HELPERS_DIR'] + '\\run_python_nn_smoketests.py', shell=True, check=True)
-
-            subprocess.run(gflags_exe + ' /i python.exe -sls', shell=True, check=True)
-
-        except Exception as e:
-
-            subprocess.run('echo shard dmoke test failed', shell=True)
-            subprocess.run('echo ' + str(e), shell=True)
-            sys.exit()
+gflags_exe = "C:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\gflags.exe"
+os.environ['GFLAGS_EXE'] = gflags_exe
 
 
-    subprocess.run('echo Copying over test times file', shell=True)
-    subprocess.run('copy /Y ' + str(os.environ['PYTORCH_FINAL_PACKAGE_DIR_WIN']) +
-        '\\.pytorch-test-times.json ' + str(os.environ['PROJECT_DIR_WIN']), shell=True)
+if shard_number == "1" and exists(gflags_exe):
 
-
-    subprocess.run('echo Run nn tests', shell=True)
+    subprocess.run('echo Some smoke tests', shell=True, cwd='test')
 
     try:
-        subprocess.run('conda install -n test_env python run_test.py --exclude-jit-executor ' +
-            '--exclude-distributed-tests --shard ' + shard_number + ' ' + str(os.environ['NUM_TEST_SHARDS']) +
-                ' --verbose', shell=True, check=True)
+        subprocess.run(gflags_exe + ' /i python.exe +sls', shell=True, check=True, cwd='test')
+
+        subprocess.run('conda run -n test_env' + ' python ' + os.environ['SCRIPT_HELPERS_DIR'] + '\\run_python_nn_smoketests.py', shell=True, check=True, cwd='test')
+
+        subprocess.run(gflags_exe + ' /i python.exe -sls', shell=True, check=True, cwd='test')
 
     except Exception as e:
 
-        subprocess.run('echo shard nn tests failed', shell=True)
+        subprocess.run('echo shard dmoke test failed', shell=True)
         subprocess.run('echo ' + str(e), shell=True)
         sys.exit()
+
+
+subprocess.run('echo Copying over test times file', shell=True)
+subprocess.run('copy /Y ' + str(os.environ['PYTORCH_FINAL_PACKAGE_DIR_WIN']) +
+    '\\.pytorch-test-times.json ' + str(os.environ['PROJECT_DIR_WIN']), shell=True, cwd='test')
+
+
+subprocess.run('echo Run nn tests', shell=True)
+
+try:
+    subprocess.run('conda install -n test_env python run_test.py --exclude-jit-executor ' +
+        '--exclude-distributed-tests --shard ' + shard_number + ' ' + str(os.environ['NUM_TEST_SHARDS']) +
+            ' --verbose', shell=True, check=True, cwd='test')
+
+except Exception as e:
+
+    subprocess.run('echo shard nn tests failed', shell=True)
+    subprocess.run('echo ' + str(e), shell=True)
+    sys.exit()
