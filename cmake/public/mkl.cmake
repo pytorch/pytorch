84e89138189b0e7170e082b910300f9bb439f82a
cyy <cyyever@outlook.com>
2023-02-21 11:59:56 +0800

    improve mkl link directory detection

--- cmake/public/mkl.cmake
+++ cmake/public/mkl.cmake
@@ -6,6 +6,15 @@ endif()
 
 target_include_directories(caffe2::mkl INTERFACE ${MKL_INCLUDE_DIR})
 target_link_libraries(caffe2::mkl INTERFACE ${MKL_LIBRARIES})
+foreach(MKL_LIB IN LISTS MKL_LIBRARIES)
+  if(EXISTS "${MKL_LIB}")
+    get_filename_component(MKL_LINK_DIR "${MKL_LIB}" DIRECTORY)
+    if(IS_DIRECTORY "${MKL_LINK_DIR}")
+      target_link_directories(caffe2::mkl INTERFACE "${MKL_LINK_DIR}")
+    endif()
+  endif()
+endforeach()
+
 # TODO: This is a hack, it will not pick up architecture dependent
 # MKL libraries correctly; see https://github.com/pytorch/pytorch/issues/73008
 set_property(
