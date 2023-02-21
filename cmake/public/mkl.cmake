7a82e8d682bd4127d9e7f4cc9c6c167b9ae08610
cyy <cyyever@outlook.com>
2023-02-21 11:40:11 +0800

    use caffe2::mkl target and some cleanup

--- cmake/public/mkl.cmake
+++ cmake/public/mkl.cmake
@@ -4,12 +4,8 @@ if(NOT TARGET caffe2::mkl)
   add_library(caffe2::mkl INTERFACE IMPORTED)
 endif()
 
-set_property(
-  TARGET caffe2::mkl PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-  ${MKL_INCLUDE_DIR})
-set_property(
-  TARGET caffe2::mkl PROPERTY INTERFACE_LINK_LIBRARIES
-  ${MKL_LIBRARIES} ${MKL_THREAD_LIB})
+target_include_directories(caffe2::mkl INTERFACE ${MKL_INCLUDE_DIR})
+target_link_libraries(caffe2::mkl INTERFACE ${MKL_LIBRARIES})
 # TODO: This is a hack, it will not pick up architecture dependent
 # MKL libraries correctly; see https://github.com/pytorch/pytorch/issues/73008
 set_property(
