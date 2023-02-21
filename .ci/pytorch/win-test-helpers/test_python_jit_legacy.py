2f64ed13a8d2d27be3eaf69feef79ac7ca1549bf
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-21 07:29:16 -0600

    Update test_python_jit_legacy.py

--- .ci/pytorch/win-test-helpers/test_python_jit_legacy.py
+++ .ci/pytorch/win-test-helpers/test_python_jit_legacy.py
@@ -4,28 +4,17 @@ import shutil
 import contextlib
 
 
-@contextlib.contextmanager
-def pushd(new_dir):
-    previous_dir = os.getcwd()
-    os.chdir(new_dir)
-    try:
-        yield
-    finally:
-        os.chdir(previous_dir)
-
-
 subprocess.run('python ' + os.environ['SCRIPT_HELPERS_DIR'] + "\\setup_pytorch_env.py", shell=True)
 
 subprocess.run('echo Copying over test times file', shell=True)
 shutil.copy(str(os.environ['PYTORCH_FINAL_PACKAGE_DIR_WIN']) + "\\.pytorch-test-times.json",
     os.environ['PROJECT_DIR_WIN'])
 
-with pushd('test'):
-    try:
-        subprocess.run('echo Run jit_profiling tests', shell=True, check=True)
-        
-        subprocess.run('conda install -n test_env python run_test.py --include test_jit_legacy ' +
-            'test_jit_fuser_legacy --verbose', shell=True, check=True)
-        
-    except Exception as e:
-        pass
+try:
+    subprocess.run('echo Run jit_profiling tests', shell=True, check=True)
+
+    subprocess.run('conda install -n test_env python run_test.py --include test_jit_legacy ' +
+        'test_jit_fuser_legacy --verbose', shell=True, check=True, cwd='test')
+
+except Exception as e:
+    pass
