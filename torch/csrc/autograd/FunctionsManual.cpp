b6a1c238bd181ccba2b3e83ac2f8fa9813188c22
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2023-02-22 19:43:50 +0000

    [MPS] Remove mps specialized path in BCE backward (#95220)
    Remove mps specialized path in BCE backward as `logit` op has been implemented for mps.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95220
    Approved by: https://github.com/soulitzer

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -1997,12 +1997,8 @@ Tensor binary_cross_entropy_target_backward(
     const Tensor& target,
     const c10::optional<Tensor>& weight,
     int64_t reduction) {
-  auto grad_target = [&] {
-    if (self.is_mps()) {
-      return self.neg().log1p_().sub_(self.log());
-    }
-    return at::logit(self).neg_();
-  }();
+  auto grad_target = at::logit(self).neg_();
+
   if (!areAnyTensorSubclassLike({grad})) {
     grad_target.mul_(grad);
   } else {
