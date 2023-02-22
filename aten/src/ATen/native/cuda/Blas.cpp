ae337b614bbdd0b52c37d3acec8a06fec561d4a9
Christian Puhrsch <cpuhrsch@fb.com>
2023-02-22 17:07:05 +0000

    Bring back contiguous

--- aten/src/ATen/native/cuda/Blas.cpp
+++ aten/src/ATen/native/cuda/Blas.cpp
@@ -686,8 +686,7 @@ Tensor& _int_mm_out_cuda(const Tensor& self__, const Tensor& mat2__, Tensor& res
   TORCH_CHECK(mat2__.dim() == 2, "Expected mat2 to be of dimension 2 but got ", mat2__.dim());
   // NOTE: cuBLAS is currently broken for some transposed inputs.
   Tensor self = self__.contiguous();
-  // Create TN layout for cuBLAS by storing mat2 in transposed form.
-  Tensor mat2 = mat2__.transpose(0, 1).contiguous().transpose(0, 1);
+  Tensor mat2 = mat2__.contiguous();
   TORCH_CHECK(self.size(0) > 16, "self.size(0) needs to be greater than 16, but got ", self.size(0));
   TORCH_CHECK(self.size(1) > 0 && self.size(1) % 8 == 0, "self.size(1) needs to be greater than 0 and a multiple of 8, but got ", self.size(1));
   TORCH_CHECK(self.size(1) == mat2.size(0), "self.size(1) needs to match mat2.size(0) but got ", self.size(1), " and ", mat2.size(0));
