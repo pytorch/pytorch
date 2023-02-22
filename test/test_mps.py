66341377d6376e45e4804633f499462fadce85ff
Denis Vieriu <dvieriu@apple.com>
2023-02-22 17:23:25 +0000

    [MPS] Cast int64 to int32 for reduction ops (#95231)
    - give warnings of converting int64 for reduction ops
    - use cast tensor for reduction sum on trace
    - unblock trace from running
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95231
    Approved by: https://github.com/razarmehr

--- test/test_mps.py
+++ test/test_mps.py
@@ -9392,6 +9392,7 @@ class TestConsistency(TestCaseMPS):
         'nn.functional.bilinear': ['f32'],
         'linalg.solve_triangular': ['f32'],
         'triangular_solve': ['f32'],
+        'trace': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         '_native_batch_norm_legit': ['f32'],
         'native_batch_norm': ['f32'],
         'minreduction_with_dim': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
@@ -9607,7 +9608,6 @@ class TestConsistency(TestCaseMPS):
         'stft': [torch.float32], 'var': [torch.float16],
         # + forward when requires_grad=True or running backward
         'nn.functional.embedding': [torch.float32, torch.float16],
-        '__rpow__': [torch.int64],
 
         'as_strided_scatter': [torch.uint8],
         'atan2': [torch.int64],
