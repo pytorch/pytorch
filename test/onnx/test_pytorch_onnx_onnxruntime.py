05cb5eadc73d851c6acbe8ca8422e4cc77764dc9
AllenTiTaiWang <titaiwang@microsoft.com>
2023-02-22 00:38:21 +0000

    [ONNX] UPdate opset18 in runtime test cases
    [ghstack-poisoned]

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -44,10 +44,7 @@ from torch.testing._internal.common_utils import skipIfNoLapack
 # The min onnx opset version to test for
 MIN_ONNX_OPSET_VERSION = 9
 # The max onnx opset version to test for
-MAX_ONNX_OPSET_VERSION = (
-    _constants.ONNX_MAX_OPSET - 1
-)  # TODO: ORT does not support opset 18 yet
-
+MAX_ONNX_OPSET_VERSION = _constants.ONNX_MAX_OPSET
 
 def _init_test_generalized_rcnn_transform():
     min_size = 100
