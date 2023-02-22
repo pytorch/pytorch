636cf4192fd0a498b04ef86605dd52b3b9224860
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-22 08:09:14 -0600

    Update build_pytorch.py

--- .ci/pytorch/win-test-helpers/build_pytorch.py
+++ .ci/pytorch/win-test-helpers/build_pytorch.py
@@ -6,28 +6,6 @@ import contextlib
 import pathlib
 
 
-def append_multiple_lines(file_name, lines_to_append):
-    # Open the file in append & read mode ('a+')
-    with open(file_name, "a+") as file_object:
-        appendEOL = False
-        # Move read cursor to the start of file.
-        file_object.seek(0)
-        # Check if file is not empty
-        data = file_object.read(100)
-        if len(data) > 0:
-            appendEOL = True
-        # Iterate over each string in the list
-        for line in lines_to_append:
-            # If file is not empty then append '\n' before first line for
-            # other lines always append '\n' before appending line
-            if appendEOL == True:
-                file_object.write("\n")
-            else:
-                appendEOL = True
-            # Append element at the end of file
-            file_object.write(line)
-
-
 '''
 :: This inflates our log size slightly, but it is REALLY useful to be
 :: able to see what our cl.exe commands are (since you can actually
