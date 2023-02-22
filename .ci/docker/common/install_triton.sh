c3354a165fe1eb48a73db0dd763c4230d2996d0a
Huy Do <huydhn@gmail.com>
2023-02-22 12:01:29 -0800

    Skip triton in ONNX image

--- .ci/docker/common/install_triton.sh
+++ .ci/docker/common/install_triton.sh
@@ -14,12 +14,6 @@ pip_install_with_env() {
   as_jenkins_with_env conda run -n py_$ANACONDA_PYTHON_VERSION pip install --progress-bar off $*
 }
 
-if [ -n "${ANDROID}" ]; then
-  # TODO: Not yet supported, linker fails with an undefined reference error.
-  # This is also not yet supported in the CI anyway
-  exit 0
-fi
-
 # The logic here is copied from .ci/pytorch/common_utils.sh
 TRITON_PINNED_COMMIT=$(get_pinned_commit triton)
 
@@ -30,15 +24,13 @@ if [ -n "${GCC_VERSION}" ] && [[ "${GCC_VERSION}" == "7" ]]; then
   # Triton needs at least gcc-9 to build
   apt-get install -y g++-9
 
-  export CXX=g++-9
-  pip_install_with_env "git+https://github.com/openai/triton@${TRITON_PINNED_COMMIT}#subdirectory=python"
+  CXX=g++-9 pip_install_with_env "git+https://github.com/openai/triton@${TRITON_PINNED_COMMIT}#subdirectory=python"
 elif [ -n "${CLANG_VERSION}" ]; then
   # Triton needs <filesystem> which surprisingly is not available with clang-9 toolchain
   add-apt-repository -y ppa:ubuntu-toolchain-r/test
   apt-get install -y g++-9
 
-  export CXX=g++-9
-  pip_install_with_env "git+https://github.com/openai/triton@${TRITON_PINNED_COMMIT}#subdirectory=python"
+  CXX=g++-9 pip_install_with_env "git+https://github.com/openai/triton@${TRITON_PINNED_COMMIT}#subdirectory=python"
 else
   pip_install "git+https://github.com/openai/triton@${TRITON_PINNED_COMMIT}#subdirectory=python"
 fi
