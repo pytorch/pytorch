486b2baca2e8f7ffaf45644410a5c0ced356f7ae
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-23 10:17:17 -0600

    Update win-build.sh

--- .ci/pytorch/win-build.sh
+++ .ci/pytorch/win-build.sh
@@ -43,7 +43,7 @@ fi
 
 echo $BUILD_TYPE
 
-export PATH=C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;${PATH}
+export PATH="C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;"${PATH}
 
 echo $PATH
 
