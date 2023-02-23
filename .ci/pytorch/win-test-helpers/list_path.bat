07867c6792326b01921e84ece1e15d8fe6e880aa
andysamfb <111015134+andysamfb@users.noreply.github.com>
2023-02-23 08:51:25 -0600

    Delete list_path.bat

--- .ci/pytorch/win-test-helpers/list_path.bat
+++
@@ -1,2 +0,0 @@
-echo %PATH%
-set PATH=C:\Program Files\CMake\bin;C:\Program Files\7-Zip;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Program Files\Amazon\AWSCLI;C:\Program Files\Amazon\AWSCLI\bin;%PATH%
