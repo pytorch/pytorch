0a8dcf3ad4218f8ffb7353f4393024112b667d22
XiaobingSuper <xiaobing.zhang@intel.com>
2023-02-22 23:15:48 -0500

    Update on "inductor: fix comnplier error when trying to vectorize logit_and"
    cc soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -362,16 +362,16 @@ class CppVecOverrides(OpOverrides):
         return f"{x}.lgamma()"
 
     """
-    #TODO: support logical_and and logical_or vectorization
+    #TODO: support logical_and vectorization
     @staticmethod
     def logical_and(a, b):
         return f"{a} && {b}"
-
+    """
 
     @staticmethod
     def logical_or(a, b):
         return f"{a} || {b}"
-    """
+
 
     @staticmethod
     def tan(a):
