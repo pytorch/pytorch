ab1ab3ab192c925d1d22b4ef28cbe37d2005de8a
Bin Bao <binbao@fb.com>
2023-02-24 16:13:02 +0000

    [CI] Specify more torch.backends.cudnn options to reduce non-determinism (#95478)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95478
    Approved by: https://github.com/ezyang

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -1947,6 +1947,8 @@ def run(runner, args, original_dir=None):
             # TODO - Using train mode for timm_models. Move to train mode for HF and Torchbench as well.
             args.use_eval_mode = True
         inductor_config.fallback_random = True
+        torch.backends.cudnn.allow_tf32 = False
+        torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
         # Remove randomeness when torch manual seed is called
