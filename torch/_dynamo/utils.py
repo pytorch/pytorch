2b78c716f78edf076f7082d4e47424e9315ad7b1
Brian Hirsh <hirsheybar@meta.com>
2023-02-24 18:38:51 +0000

    increase tolerance a bit for sebotnet33ts_256
    [ghstack-poisoned]

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -934,7 +934,7 @@ def same(
                     # In the presence of noise, noise might dominate our error
                     # metric for smaller tensors.
                     # Similary, for 1x1 kenerls, there seems to be high noise with amp.
-                    multiplier = 3.0
+                    multiplier = 3.5
 
                 passes_test = res_error <= (multiplier * ref_error + tol / 10.0)
                 if not passes_test:
