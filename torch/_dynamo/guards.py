714dc5d8657d2f92304af79e5377cb3d330b90da
Michael Voznesensky <voznesenskym@gmail.com>
2023-02-27 19:38:27 +0000

    Replace source check with assert
    [ghstack-poisoned]

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -497,9 +497,8 @@ class GuardBuilder(GuardBuilderBase):
             # In the case of when there is no Y, that is, there are no dynamic indices marked at all, the frame is safe to reuse
             # as an empty set is a safe degeneration - that is, a strictly static tensor is always valid for a frame compiled with that same
             # tensor + more onerous user directives.
-            static, reason = tensor_always_has_static_shape(
-                value, guard.source, is_tensor=True
-            )
+            assert guard.source is not None
+            static, reason = tensor_always_has_static_shape(value, is_tensor=True)
             if not static:
                 if hasattr(value, "_dynamo_dynamic_indices"):
                     code.append(
