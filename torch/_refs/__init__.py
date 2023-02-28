Fix bugs on derivatives and expect failures in the test

torch/_refs/__init__.py
@@ -1447,11 +1447,21 @@ def le(a: TensorLikeType, b: TensorLikeType) -> TensorLikeType:
 )
 def logaddexp(a: TensorLikeType, b: TensorLikeType) -> TensorLikeType:
     # Nb. this implementation does nto distribute the gradients evenly when a == b
-    mask = a.real >= b.real
+    mask = torch.real(a) >= torch.real(b)
     max_ = torch.where(mask, a, b)
     min_ = torch.where(mask, b, a)
-    inf_mask = torch.logical_and(torch.isinf(a.real), a.real == b.real)
-    return torch.where(inf_mask, a, max_ + torch.log1p(torch.exp(min_ - max_)))
+    inf_mask = torch.logical_and(torch.isinf(torch.real(a)), torch.real(a) == torch.real(b))
+    if utils.is_complex_dtype(a.dtype) or utils.is_complex_dtype(b.dtype):
+        # are you wondering what this bunch of codes are for? edge cases!
+        complex_tens = a if utils.is_complex_dtype(a.dtype) else b
+        neg_min_mask = torch.real(min_) < 0
+        inf_vals = torch.where(neg_min_mask, min_, torch.log(torch.exp(min_) + torch.exp(max_)))
+        non_nan_vals = torch.where(inf_mask, inf_vals, max_ + torch.log1p(torch.exp(min_ - max_)))
+        nan_vals = torch.full_like(complex_tens, complex(float('nan'), float('nan')))
+        nan_mask = torch.isnan(min_)
+        return torch.where(nan_mask, nan_vals, non_nan_vals)
+    else:
+        return torch.where(inf_mask, a, max_ + torch.log1p(torch.exp(min_ - max_)))
 
 
 # TODO: add docstring