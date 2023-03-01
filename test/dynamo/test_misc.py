2bfb8490c24b71802349da09dc1807073e1888be
Michael Voznesensky <voznesenskym@gmail.com>
2023-03-01 08:58:03 +0000

    Update on "[RFC] Always get attr static out?"
    Discussion here https://github.com/pytorch/pytorch/issues/95630#issuecomment-1449596766
    
    cc soumith yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -4607,6 +4607,22 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         ):
             torch._dynamo.optimize("eager")(e)(x)
 
+    def test_torch_compile_ctx_on_forward_and_training_step(self):
+        class MyModel(torch.nn.Module):
+            def forward(self):
+                ...
+
+            def training_step(self):
+                self()
+
+        model = MyModel()
+        compiled_model = torch.compile(model)
+
+        model.forward = compiled_model.dynamo_ctx(model.forward)
+        model.training_step = compiled_model.dynamo_ctx(model.training_step)
+
+        model.training_step()
+
 
 class CustomFunc1(torch.autograd.Function):
     @staticmethod
