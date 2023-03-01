8093abce3ef683dec21cb786e3c6b0b152ce1f5e
Michael Voznesensky <voznesenskym@gmail.com>
2023-03-01 23:03:00 +0000

    Always get attr static out (#95771)
    Discussion here https://github.com/pytorch/pytorch/issues/95630#issuecomment-1449596766
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95771
    Approved by: https://github.com/jansel

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -4675,6 +4675,22 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         torch._dynamo.optimize(counter)(my_dyn_fn)(x)
         self.assertEqual(counter.frame_count, 3)
 
+    def test_torch_compile_ctx_on_forward_and_training_step(self):
+        class MyModel(torch.nn.Module):
+            def forward(self):
+                ...
+
+            def training_step(self):
+                self()
+
+        model = MyModel()
+        compiled_model = torch.compile(model)
+
+        model.forward = compiled_model.dynamo_ctx(model.forward)
+        model.training_step = compiled_model.dynamo_ctx(model.training_step)
+
+        model.training_step()
+
 
 class CustomFunc1(torch.autograd.Function):
     @staticmethod
