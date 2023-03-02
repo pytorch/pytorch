48a09e63146617a93c42735f008e624a0940d529
Rodrigo Kumpera <kumpera@fb.com>
2023-03-02 22:46:19 +0000

    add other ops to the fix.

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -552,6 +552,8 @@ DONT_ENFORCE_TENSOR_IMPL_USE_COUNT = {
     "_nested_tensor_strides",
     # Functional collectives keep an internal ref through the Work object
     "all_reduce",
+    "all_gather_into_tensor",
+    "wait_tensor",
 }
 
 DONT_ENFORCE_STORAGE_IMPL_USE_COUNT = {
