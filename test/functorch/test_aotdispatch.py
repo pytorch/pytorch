e23ac21810e6152c748e751a345a2586f9a58206
Edward Z. Yang <ezyang@meta.com>
2023-03-06 10:12:35 -0800

    Update on "SymIntify a few more relatively non-controversial schemas"
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -2737,7 +2737,6 @@ symbolic_aot_autograd_module_failures = {
     torch.nn.Transformer,  # DataDependentOutputException: aten.equal compares a mask input to a mask producing a bool
     torch.nn.TransformerEncoder,  # DataDependentOutputException: aten.equal compares a mask input to a mask producing a bool
     torch.nn.TransformerEncoderLayer,  # RuntimeError: tried to get Double out of SymFloat
-    torch.nn.TransformerDecoderLayer,  # RuntimeError: tried to get Double out of SymFloat
     torch.nn.GaussianNLLLoss,  # NotImplementedError: local_scalar_dense/item NYI for torch.bool
     torch.nn.CrossEntropyLoss,  # Cannot call sizes() on tensor with symbolic sizes/strides
     torch.nn.Bilinear,  # Cannot call sizes() on tensor with symbolic sizes/strides
