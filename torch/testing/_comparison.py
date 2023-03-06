1eea220c1e5a624cb9a7f230b63fa0f9227d0098
Philip Meier <github.pmeier@posteo.de>
2023-03-06 21:34:16 +0100

    remove obsolete MPS compat in torch.testing.assert_close
    [ghstack-poisoned]

--- torch/testing/_comparison.py
+++ torch/testing/_comparison.py
@@ -788,13 +788,6 @@ class TensorLikePair(Pair):
         Returns:
             (Tuple[Tensor, Tensor]): Equalized tensors.
         """
-        # The comparison logic uses operators currently not supported by the MPS backends.
-        #  See https://github.com/pytorch/pytorch/issues/77144 for details.
-        # TODO: Remove this conversion as soon as all operations are supported natively by the MPS backend
-        if actual.is_mps or expected.is_mps:  # type: ignore[attr-defined]
-            actual = actual.cpu()
-            expected = expected.cpu()
-
         if actual.device != expected.device:
             actual = actual.cpu()
             expected = expected.cpu()
