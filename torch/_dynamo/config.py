203890e1e0e7f6827d11e53b3121fc3b0a60f477
Michael Lazos <mlazos@fb.com>
2023-03-07 22:52:22 +0000

    Properly show buck target to run (#96089)
    Summary: Makes the debug dir location configurable with TORCH_COMPILE_DEBUG_DIR env var
    
    Test Plan: TORCH_COMPILE_DEBUG_DIR=”.” buck2 run mode/dev-nosan //caffe2/test/inductor:minifier_smoke
    
    Reviewed By: bertmaher
    
    Differential Revision: D43639955
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/96089
    Approved by: https://github.com/bertmaher

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -4,6 +4,7 @@ import tempfile
 from os.path import abspath, dirname
 
 import torch
+
 from . import external_utils
 
 from .logging import get_loggers_level, set_loggers_level
@@ -210,7 +211,11 @@ def is_fbcode():
     return not hasattr(torch.version, "git_version")
 
 
-if is_fbcode():
+DEBUG_DIR_VAR_NAME = "TORCH_COMPILE_DEBUG_DIR"
+
+if DEBUG_DIR_VAR_NAME in os.environ:
+    debug_dir_root = os.path.join(os.environ[DEBUG_DIR_VAR_NAME], "torch_compile_debug")
+elif is_fbcode():
     debug_dir_root = os.path.join(tempfile.gettempdir(), "torch_compile_debug")
 else:
     debug_dir_root = os.path.join(os.getcwd(), "torch_compile_debug")
