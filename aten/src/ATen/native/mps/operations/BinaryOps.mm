ca5c1b450c690e36a30b35738ef2008f0a3ef956
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2023-03-07 17:52:49 +0800

    No need to copy when in-place

--- aten/src/ATen/native/mps/operations/BinaryOps.mm
+++ aten/src/ATen/native/mps/operations/BinaryOps.mm
@@ -233,9 +233,7 @@ void div_mode_template(const Tensor& self, const Tensor& other,
 void add_sub_template(const Tensor& self, const Tensor& other, const Scalar& alpha, const Tensor& output, std::string op_name)
 {
   if (alpha.toDouble() == 0.0) {
-    if (self.is_alias_of(output)) { 
-      output.copy_(self);
-    } else {
+    if (!self.is_alias_of(output)) {  // if inplace, no-op
       const_cast<Tensor&>(output) = self.clone();
     }
     return;
