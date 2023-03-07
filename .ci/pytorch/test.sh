666efd8d5dfdf7edd6aa6e8d317ce2d703c46ee3
cyy <cyyever@outlook.com>
2023-03-07 14:10:13 +0000

    Improve ASAN and TSAN handling in cmake (#93147)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/93147
    Approved by: https://github.com/malfet

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -126,7 +126,7 @@ fi
 # if you're not careful.  Check this if you made some changes and the
 # ASAN test is not working
 if [[ "$BUILD_ENVIRONMENT" == *asan* ]]; then
-    export ASAN_OPTIONS=detect_leaks=0:symbolize=1:detect_stack_use_after_return=1:strict_init_order=true:detect_odr_violation=0
+    export ASAN_OPTIONS=detect_leaks=0:symbolize=1:detect_stack_use_after_return=1:strict_init_order=true:detect_odr_violation=1:detect_container_overflow=0
     export UBSAN_OPTIONS=print_stacktrace=1
     export PYTORCH_TEST_WITH_ASAN=1
     export PYTORCH_TEST_WITH_UBSAN=1
