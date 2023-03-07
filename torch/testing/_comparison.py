772150c0855f91f5fb5a2ec2e5f7fa081f780abe
Philip Meier <github.pmeier@posteo.de>
2023-03-07 09:31:26 +0100

    Update on "improve memory footprint of torch.testing.assert_close"
    Redo of #90172 out of stack.
    
    [ghstack-poisoned]

--- torch/testing/_comparison.py
+++ torch/testing/_comparison.py
@@ -954,14 +954,22 @@ class TensorLikePair(Pair):
                 ),
             )
 
+        # Compressed and plain indices in the CSR / CSC / BSR / BSC sparse formates can be `torch.int32` _or_
+        # `torch.int64`. While the same dtype is enforced for the compressed and plain indices of a single tensor, it
+        # can be different between two tensors. Thus, we need to convert them to the same dtype, or the comparison will
+        # fail.
+        actual_compressed_indices = compressed_indices_method(actual)
+        expected_compressed_indices = compressed_indices_method(expected)
+        indices_dtype = torch.promote_types(actual_compressed_indices.dtype, expected_compressed_indices.dtype)
+
         self._compare_regular_values_equal(
-            compressed_indices_method(actual).to(torch.int64),
-            compressed_indices_method(expected).to(torch.int64),
+            actual_compressed_indices.to(indices_dtype),
+            expected_compressed_indices.to(indices_dtype),
             identifier=f"Sparse {format_name} {compressed_indices_method.__name__}",
         )
         self._compare_regular_values_equal(
-            plain_indices_method(actual).to(torch.int64),
-            plain_indices_method(expected).to(torch.int64),
+            plain_indices_method(actual).to(indices_dtype),
+            plain_indices_method(expected).to(indices_dtype),
             identifier=f"Sparse {format_name} {plain_indices_method.__name__}",
         )
         self._compare_regular_values_close(
