1e60c87904ff13250353eacc2eb97331f6ac13c3
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2023-03-07 13:09:30 +0200

    Remove cholesky opinfo mentions

--- test/test_ops.py
+++ test/test_ops.py
@@ -1990,7 +1990,6 @@ fake_backward_xfails = fake_tensor_stride_failing_ops | {
     "roll",
     "svd_lowrank",
     "sgn",
-    "cholesky",
 }
 
 fake_backward_xfails = {xfail(stride_skip) for stride_skip in fake_backward_xfails} | {
