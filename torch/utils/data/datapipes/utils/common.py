9b325ef8d2fca72bde592e5286fda74035120686
erjia <erjia@fb.com>
2023-03-07 19:24:29 +0000

    Fix validate_input_col for nn.Module or Callable

--- torch/utils/data/datapipes/utils/common.py
+++ torch/utils/data/datapipes/utils/common.py
@@ -80,7 +80,7 @@ def validate_input_col(fn: Callable, input_col: Optional[Union[int, tuple, list]
     if isinstance(fn, functools.partial):
         fn_name = fn.func.__name__
     else:
-        fn_name = fn.__name__
+        fn_name = getattr(fn, "__name__", repr(fn))
 
     if len(non_default_kw_only) > 0:
         raise ValueError(
