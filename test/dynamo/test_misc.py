56cfe7a14d9ede5f15fc6ee1046888b335538047
Driss Guessous <drisspg@fb.com>
2023-03-08 18:07:37 +0000

    [SDPA] Add an optional scale kwarg (#95259)
    # Summary
    This PR adds an optional kwarg to torch torch.nn.functional.scaled_dot_product_attention()
    The new kwarg is a scaling factor that is applied after the q@k.T step of the computation. Made updates to the efficient kernel to support but flash and math were minimally updated to support as well.
    
    Will reduce the complexity of: #94729 and has been asked for by a couple of users.
    
    # Review Highlights
    - As far as I know I did this the correct way and this both BC and FC compliant. However I always seem to break internal workloads so I would love if someone can advice I did this right?
    - I named the optional arg 'scale'. This is probably dumb and I should name it 'scale_factor'. I will make this change but this is annoying and it will require someone thinking we should rename.
    - 'scale' is interpreted as `Q@K.T * (scale)`
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95259
    Approved by: https://github.com/cpuhrsch

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -3340,6 +3340,9 @@ class MiscTests(torch._dynamo.test_case.TestCase):
             def forward(self, query, key, value):
                 out = F.scaled_dot_product_attention(query, key, value, None, 0, True)
                 out = F.scaled_dot_product_attention(
+                    query, key, value, None, 0, True, scale=8
+                )
+                out = F.scaled_dot_product_attention(
                     query=query,
                     key=key,
                     value=value,
@@ -3358,6 +3361,7 @@ class MiscTests(torch._dynamo.test_case.TestCase):
                 out = F.scaled_dot_product_attention(
                     query, key, value, None, dropout_p=0, is_causal=True
                 )
+                out = F.scaled_dot_product_attention(query, key, value, None, scale=8)
                 return out
 
         device = "cuda"
