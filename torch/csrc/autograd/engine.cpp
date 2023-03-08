fd6cc8e6ba80893f323e302f3375694be56e81ba
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2023-03-08 13:42:25 -0800

    revert changes in start_device_threads

--- torch/csrc/autograd/engine.cpp
+++ torch/csrc/autograd/engine.cpp
@@ -1395,24 +1395,12 @@ auto Engine::start_device_threads() -> void {
   // Second, create special threads for each non-CPU device
   // See Note [Allocating GPUs to autograd threads]
   c10::DeviceIndex num_devices = 0;
-  int target_cuda_device = -1;
-  bool one_cuda_device_in_registry = false;
   for (const auto& impl_atomic : c10::impl::device_guard_impl_registry) {
     auto* impl = impl_atomic.load();
     // Only record the number of devices for device that don't run on the
     // cpu ready queue.
     if (impl && !should_run_in_cpu_ready_queue(impl->type())) {
       num_devices = std::max(num_devices, impl->deviceCount());
-      if (impl->getDevice().is_cuda()) {
-        int dev = impl->getDevice().index();
-        if (target_cuda_device == -1) {
-          target_cuda_device = dev;
-          one_cuda_device_in_registry = true;
-        }
-        one_cuda_device_in_registry =
-            one_cuda_device_in_registry && (dev == target_cuda_device);
-        target_cuda_device = dev;
-      }
     }
   }
 
@@ -1432,22 +1420,9 @@ auto Engine::start_device_threads() -> void {
     queue = std::make_shared<ReadyQueue>();
   }
 
-  if (one_cuda_device_in_registry) {
-    for (const auto i : c10::irange(num_devices)) {
-      std::thread t(
-          &Engine::thread_init,
-          this,
-          target_cuda_device,
-          device_ready_queues_[i],
-          true);
-      t.detach();
-    }
-  } else {
-    for (const auto i : c10::irange(num_devices)) {
-      std::thread t(
-          &Engine::thread_init, this, i, device_ready_queues_[i], true);
-      t.detach();
-    }
+  for (const auto i : c10::irange(num_devices)) {
+    std::thread t(&Engine::thread_init, this, i, device_ready_queues_[i], true);
+    t.detach();
   }
   // Wait for the threads to start
   {
