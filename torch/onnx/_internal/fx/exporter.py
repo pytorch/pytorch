08b3955294443f8f310c1d7082b428d522e4e76e
AllenTiTaiWang <titaiwang@microsoft.com>
2023-03-08 22:12:15 +0000

    [ONNX] Set shape/type into torchscript
    [ghstack-poisoned]

--- torch/onnx/_internal/fx/exporter.py
+++ torch/onnx/_internal/fx/exporter.py
@@ -181,13 +181,23 @@ def _fill_tensor_meta(
     expected_values: Union[torch.Tensor, Tuple[torch.Tensor, ...]],
 ):
     """Fill the meta information of onnxscript_values with that from the fx FakeTensor."""
+
+    if isinstance(expected_values, (list, tuple)) and not isinstance(onnxscript_values, (list, tuple)):
+        # TODO(titaiwang): Support List of Tensors: https://github.com/microsoft/onnx-script/issues/481
+        # This is the case that fx has [tensor, tensor, ...], but onnxscript_value wrapped it as a single tensor.
+        # graph_buiding not yet support list of tensors, so we don't need to handle this case for now.
+        # eg: aten_split
+        return
+
     flat_onnxscript_values, _ = _pytree.tree_flatten(onnxscript_values)
     flat_expected_values, _ = _pytree.tree_flatten(expected_values)
     for i, (onnxscript_value, expected_value) in enumerate(
         zip(flat_onnxscript_values, flat_expected_values)
     ):
         # Only set shape for now as we don't need type information.
+        # import pdb; pdb.set_trace()
         onnxscript_value.shape = tuple(expected_value.size())
+        onnxscript_value.type = expected_value.dtype
         if i > 0:
             onnxscript_value.name = f"{name}_{i}"
         else:
