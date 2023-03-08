6b5ac8a2d6c6246fd7833584d0e4440eb728b7cd
Edward Z. Yang <ezyang@meta.com>
2023-03-08 06:07:29 -0800

    Update on "Extra CR comments from #95621"
    Specifically:
    https://github.com/pytorch/pytorch/pull/95621/files/063e44147152f4dd7e51852cf8c679692bd9fd53#r1120306196
    https://github.com/pytorch/pytorch/pull/95621#discussion_r1125015510
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    cc soumith voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -957,7 +957,7 @@ def same(
             log.error(f"Accuracy failed (float): {ref} != {res} (within tol={tol})")
         return r
     elif is_numpy_int_type(ref) or is_numpy_float_type(ref):
-        if relax_numpy_equality:
+        if relax_numpy_equality and not (is_numpy_int_type(res) or is_numpy_float_type(res)):
             ref = ref.item()
         r = (type(ref) is type(res)) and (ref == res)
         if not r:
