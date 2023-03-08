8d066e9a0181326bcad45bface485d7c3a057796
Michael Voznesensky <voznesenskym@gmail.com>
2023-03-08 22:36:22 +0000

    Update base for Update on "Create nn_modules_sources structure, map params and placeholders"
    cc soumith yanboliang penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_ops.py
+++ torch/_ops.py
@@ -22,14 +22,11 @@ def dl_open_guard():
     Context manager to set the RTLD_GLOBAL dynamic linker flag while we open a
     shared library to load custom operators.
     """
-    if not _SET_GLOBAL_FLAGS:
-        yield
-        return
-    old_flags = sys.getdlopenflags()
-    sys.setdlopenflags(old_flags | ctypes.RTLD_GLOBAL)
-    try:
-        yield
-    finally:
+    if _SET_GLOBAL_FLAGS:
+        old_flags = sys.getdlopenflags()
+        sys.setdlopenflags(old_flags | ctypes.RTLD_GLOBAL)
+    yield
+    if _SET_GLOBAL_FLAGS:
         sys.setdlopenflags(old_flags)
 
 
