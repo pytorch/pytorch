d8e91850bb53b61d88e4b0be14e0e2fba10ebc7d
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2023-03-10 14:25:39 -0800

    Add hacky example inputs to dynamo produced graph
    [ghstack-poisoned]

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -832,6 +832,9 @@ def export(
 
     new_graph.recompile()
 
+    # TODO remove this once Executorch uses proper functionalization
+    new_graph._example_fake_inputs = example_fake_inputs
+
     return (new_graph, out_guards)
 
 
