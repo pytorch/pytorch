2276bac1623468155e5d6500a5023b7b9183c580
Huy Do <huydhn@gmail.com>
2023-03-13 17:26:52 +0000

    Dockerize torch deploy setup (#96593)
    The step `conda_install "libpython-static=${ANACONDA_PYTHON_VERSION}"` could fail flakily, for example https://hud.pytorch.org/pytorch/pytorch/commit/5f89d147a100064ae8a9743fa0e297bdedfa1b61, so let's put that into Docker.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/96593
    Approved by: https://github.com/ZainRizvi

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -155,21 +155,9 @@ function install_triton() {
   fi
 }
 
-function setup_torchdeploy_deps(){
-  conda install -y -n "py_${ANACONDA_PYTHON_VERSION}" "libpython-static=${ANACONDA_PYTHON_VERSION}"
-  local CC
-  local CXX
-  CC="$(which gcc)"
-  CXX="$(which g++)"
-  export CC
-  export CXX
-  pip install --upgrade pip
-}
-
 function checkout_install_torchdeploy() {
   local commit
   commit=$(get_pinned_commit multipy)
-  setup_torchdeploy_deps
   pushd ..
   git clone --recurse-submodules https://github.com/pytorch/multipy.git
   pushd multipy
