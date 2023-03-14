pytorch/pytorch

commit 32888b94ef08b902dd39a0164101a1daea4bb0f6

flatten_indices: remove syncs (#94401)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/94401
Approved by: https://github.com/cpuhrsch, https://github.com/pearu

aten/src/ATen/native/sparse/FlattenIndicesCommon.h
@@ -1,0 +1,106 @@
+#pragma once
+
+#include <ATen/Tensor.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/sparse/Macros.h>
+#include <ATen/ExpandUtils.h>
+#include <ATen/SparseTensorUtils.h>
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/arange.h>
+#include <ATen/ops/tensor.h>
+#endif
+
+#ifdef GPUCC
+#define NAME "flatten_indices_cuda"
+#else
+#define NAME "flatten_indices_cpu"
+#endif
+
+namespace at::native {
+
+namespace {
+
+template <template <typename func_t> class kernel_t>
+struct KernelLauncher {
+  template <typename func_t>
+  static void launch(TensorIteratorBase& iter, const func_t& f) {
+    kernel_t<func_t>::launch(iter, f);
+  }
+};
+
+template <
+  template <typename func_t> class kernel_t,
+  typename index_t,
+  int64_t max_static_len = 0>
+Tensor _flatten_indices_impl(const Tensor& indices, IntArrayRef size) {
+  TORCH_INTERNAL_ASSERT(indices.dim() > 1 && indices.size(0) == size.size());
+
+  // Need owning storage in case of the Tensor class.
+  const auto hash_coeffs_storage = [&]() -> auto {
+    auto strides = c10::contiguous_strides(size);
+    return at::sparse::TensorGeometryHolder<max_static_len>(strides, strides, indices.options());
+  }();
+  const auto hash_coeffs = std::get<0>(*hash_coeffs_storage);
+
+  const auto hash_indices = [&]() -> Tensor {
+    // non-const because of gcc-5/clang-5 issues
+    auto sparse_dim = indices.size(0);
+    auto indices_dim_stride = indices.stride(0);
+    auto indices_nnz_stride = indices.stride(1);
+
+    auto hash = at::arange(indices.size(1), indices.options().dtype(kLong));
+
+    auto iter = TensorIteratorConfig()
+      .set_check_mem_overlap(false)
+      .add_output(hash)
+      .add_input(hash)
+      .build();
+
+    {
+      const auto* RESTRICT ptr_indices = indices.data_ptr<index_t>();
+
+      KernelLauncher<kernel_t>::launch(iter,
+          // NOTE: capture by value required by CUDA
+          [=] FUNCAPI (int64_t nnz_idx) -> int64_t {
+          const auto* RESTRICT ptr_indices_dim = ptr_indices + nnz_idx * indices_nnz_stride;
+          auto hash = static_cast<int64_t>(0);
+          for (int64_t dim = 0; dim < sparse_dim; ++dim) {
+            const auto dim_hash_coeff = hash_coeffs[dim];
+            const auto dim_index = ptr_indices_dim[dim * indices_dim_stride];
+            hash += dim_index * dim_hash_coeff;
+          }
+          return hash;
+      });
+    }
+
+    return hash;
+  }();
+
+  return hash_indices;
+}
+
+template <template <typename func_t> class kernel_t>
+Tensor _flatten_indices(const Tensor& indices, IntArrayRef size) {
+  TORCH_CHECK(indices.dim() > 1 && indices.size(0) == size.size(),
+      NAME, "(): the dimensionality of sparse `indices` and the lenght of `size` must match. ",
+            "Got `indices.size(0) == ", indices.size(0), "` != `size.size() == ", size.size(), "`.");
+  Tensor flattened_indices;
+  AT_DISPATCH_INDEX_TYPES(indices.scalar_type(), NAME, [&] () {
+    constexpr int64_t max_sparse_dims = 8;
+    if (indices.size(0) <= max_sparse_dims) {
+      flattened_indices = _flatten_indices_impl<kernel_t, index_t, max_sparse_dims>(indices, size);
+    } else {
+      flattened_indices = _flatten_indices_impl<kernel_t, index_t>(indices, size);
+    }
+  });
+  return flattened_indices;
+}
+
+}
+
+} // at::native

aten/src/ATen/SparseTensorUtils.cpp
@@ -2,10 +2,19 @@
 
 #include <ATen/ATen.h>
 #include <ATen/SparseTensorImpl.h>
+#include <ATen/native/sparse/SparseStubs.h>
 #include <ATen/Parallel.h>
 #include <c10/util/irange.h>
 
-namespace at { namespace sparse {
+namespace at {
+
+namespace native {
+
+DEFINE_DISPATCH(flatten_indices_stub);
+
+}
+
+namespace sparse {
 
 // NOTE [ Flatten Sparse Indices ]
 // This helper function flattens a sparse indices tensor (a Tensor) into a 1D
@@ -29,25 +38,10 @@ Tensor flatten_indices(const Tensor& indices, IntArrayRef full_size, bool force_
       return indices.squeeze(0);
     }
   } else {
-    std::vector<int64_t> indices_mult_cpu_vec;
-    indices_mult_cpu_vec.resize(sparse_dim);
-    int64_t mult = 1;
-    for (int64_t i = sparse_dim - 1; i >= 0; i--) {
-      indices_mult_cpu_vec[i] = mult;
-      mult *= full_size[i];
+    if (!indices.numel()) {
+      return at::zeros({indices.size(1)}, indices.options().dtype(kLong));
     }
-    Tensor indices_mult_cpu = at::from_blob(
-      indices_mult_cpu_vec.data(),
-      // NOLINTNEXTLINE(bugprone-argument-comment)
-      /*size=*/{sparse_dim, 1},
-      indices.options().device(kCPU).dtype(kLong));
-    // NB: must be blocking because this blob may be freed after
-    //     this closure, and non_blocking copy will see
-    //     garbage.
-    Tensor indices_mult = indices_mult_cpu.to(indices.device(), /*non_blocking=*/false);
-    // Ideally we want matmul but matmul is slow on CPU Long and not implemented
-    // on CUDA Long. So mul is faster.
-    return indices.mul(indices_mult).sum(0);
+    return at::native::flatten_indices_stub(indices.device().type(), indices, full_size.slice(0, sparse_dim));
   }
 }
 

aten/src/ATen/native/cuda/FlattenIndicesKernel.cu
@@ -1,0 +1,28 @@
+#define TORCH_ASSERT_ONLY_METHOD_OPERATORS
+#include <ATen/native/sparse/SparseStubs.h>
+#include <ATen/native/sparse/FlattenIndicesCommon.h>
+#include <ATen/native/cuda/Loops.cuh>
+#include <ATen/native/cuda/KernelUtils.cuh>
+#include <ATen/cuda/detail/OffsetCalculator.cuh>
+#include <ATen/AccumulateType.h>
+
+namespace at::native {
+
+namespace {
+
+template <typename func_t>
+struct CUDAKernelLauncher {
+  static void launch(TensorIteratorBase& iter, const func_t& f) {
+    gpu_kernel(iter, f);
+  }
+};
+
+Tensor flatten_indices_cuda_kernel(const Tensor& indices, IntArrayRef size) {
+  return _flatten_indices<CUDAKernelLauncher>(indices, size);
+}
+
+}
+
+REGISTER_CUDA_DISPATCH(flatten_indices_stub, &flatten_indices_cuda_kernel);
+
+} // namespace at::native

aten/src/ATen/native/sparse/FlattenIndicesKernel.cpp
@@ -1,0 +1,31 @@
+#define TORCH_ASSERT_ONLY_METHOD_OPERATORS
+#include <ATen/native/sparse/SparseStubs.h>
+#include <ATen/native/sparse/FlattenIndicesCommon.h>
+#include <ATen/native/cpu/Loops.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/AccumulateType.h>
+
+namespace at::native {
+
+namespace {
+
+template <typename func_t>
+struct CPUKernelLauncher {
+  static void launch(TensorIteratorBase& iter, const func_t& f) {
+    cpu_kernel(iter, f);
+  }
+};
+
+Tensor flatten_indices_cpu_kernel(const Tensor& indices, IntArrayRef size) {
+  return _flatten_indices<CPUKernelLauncher>(indices, size);
+}
+
+}
+
+REGISTER_ARCH_DISPATCH(flatten_indices_stub, DEFAULT, &flatten_indices_cpu_kernel);
+REGISTER_AVX512_DISPATCH(flatten_indices_stub, &flatten_indices_cpu_kernel);
+REGISTER_AVX2_DISPATCH(flatten_indices_stub, &flatten_indices_cpu_kernel);
+REGISTER_VSX_DISPATCH(flatten_indices_stub, &flatten_indices_cpu_kernel);
+REGISTER_ZVECTOR_DISPATCH(flatten_indices_stub, &flatten_indices_cpu_kernel);
+
+} // namespace at::native

aten/src/ATen/native/sparse/SparseStubs.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ATen/native/DispatchStub.h>
+#include <c10/util/ArrayRef.h>
 
 namespace at {
 
@@ -13,6 +14,9 @@ DECLARE_DISPATCH(mul_sparse_sparse_out_fn, mul_sparse_sparse_out_stub);
 
 using sparse_mask_intersection_out_fn = void (*)(Tensor& res, const Tensor& x, const Tensor& y);
 DECLARE_DISPATCH(sparse_mask_intersection_out_fn, sparse_mask_intersection_out_stub);
+
+using flatten_indices_fn = Tensor (*)(const Tensor& indices, IntArrayRef size);
+DECLARE_DISPATCH(flatten_indices_fn, flatten_indices_stub);
 
 }
 

build_variables.bzl
@@ -1363,6 +1363,7 @@ aten_native_source_non_codegen_list = [
     "aten/src/ATen/native/sparse/SparseFactories.cpp",
     "aten/src/ATen/native/sparse/ValidateCompressedIndicesKernel.cpp",
     "aten/src/ATen/native/sparse/SparseBinaryOpIntersectionKernel.cpp",
+    "aten/src/ATen/native/sparse/FlattenIndicesKernel.cpp",
     "aten/src/ATen/native/transformers/attention.cpp",
     "aten/src/ATen/native/transformers/transformer.cpp",
     "aten/src/ATen/native/xnnpack/Activation.cpp",

commit 51be8cc35b7f85519f50ccc35895913051c4043c

Fix signed-unsigned compare in  FlattenIndicesCommon.h
One more regression from https://github.com/pytorch/pytorch/pull/94401

aten/src/ATen/native/sparse/FlattenIndicesCommon.h
@@ -86,7 +86,7 @@ Tensor _flatten_indices_impl(const Tensor& indices, IntArrayRef size) {
 
 template <template <typename func_t> class kernel_t>
 Tensor _flatten_indices(const Tensor& indices, IntArrayRef size) {
-  TORCH_CHECK(indices.dim() > 1 && indices.size(0) == size.size(),
+  TORCH_CHECK(indices.dim() > 1 && static_cast<size_t>(indices.size(0)) == size.size(),
       NAME, "(): the dimensionality of sparse `indices` and the lenght of `size` must match. ",
             "Got `indices.size(0) == ", indices.size(0), "` != `size.size() == ", size.size(), "`.");
   Tensor flattened_indices;