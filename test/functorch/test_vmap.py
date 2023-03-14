32931f09d81288dd584b0abc145cdee2b34b6e75
Kshiteej K <kshitijkalambarkar@gmail.com>
2023-03-14 21:25:15 +0530

    update xfail

--- test/functorch/test_vmap.py
+++ test/functorch/test_vmap.py
@@ -3745,7 +3745,6 @@ class TestVmapOperatorsOpInfo(TestCase):
         decorate('bitwise_right_shift', decorator=unittest.skipIf(TEST_WITH_UBSAN, "Fails with above error")),
         # One or more of the overload doesn't have a Batch rule.
         xfail('bincount'),
-        xfail('float_power'),
         # UBSAN: runtime error: 1.27043e+262 is outside the range of representable values of type 'float'
         decorate('special.zeta', decorator=unittest.skipIf(TEST_WITH_UBSAN, "Fails with above error")),
         # RuntimeError: Expected all tensors to be on the same device,
