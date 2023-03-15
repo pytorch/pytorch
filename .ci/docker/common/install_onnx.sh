307da9d81e529703a8396c0c842e712aa9863544
Huy Do <huydhn@gmail.com>
2023-03-14 21:08:25 -0700

    Fix quote

--- .ci/docker/common/install_onnx.sh
+++ .ci/docker/common/install_onnx.sh
@@ -26,5 +26,6 @@ pip_install "onnx-script@git+https://github.com/microsoft/onnx-script@29241e15f5
 
 # Cache the transfomers model to be used later by ONNX tests. By default, the model
 # is cached at ~/.cache/huggingface/hub/
-as_jenkins conda run -n py_$ANACONDA_PYTHON_VERSION --no-capture-output \
-  python -c 'import transformers; transformers.AutoModel.from_pretrained("sshleifer/tiny-gpt2");'
+#
+# Very weird quoting behavior here https://github.com/conda/conda/issues/10972
+conda_run python -c "'import transformers; transformers.AutoModel.from_pretrained(\"sshleifer/tiny-gpt2\");'"
