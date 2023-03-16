26ef7296a7ac86659b32ffc975aa96f727a75009
Liao, Xuan <xuan.liao@intel.com>
2023-03-16 14:06:26 +0000

    fix standalone compile for op with multiple outputs

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -572,6 +572,14 @@ def graph_returns_tuple(gm: torch.fx.GraphModule):
     (rv,) = output_node(gm).args
     if isinstance(rv, (list, tuple)):
         return True
+    if (
+        isinstance(rv, torch.fx.node.Node)
+        and hasattr(rv.target, "_schema")
+        and len(rv.target._schema.returns) > 1
+        and all(str(ret.type) == "Tensor" for ret in rv.target._schema.returns)
+    ):
+        # for graphs whose result is one node with multiple outputs
+        return True
     return False
 
 
