4ed206520b80d60ac42dcd79e1bd618356e2345e
fduwjj <fduwjj@fb.com>
2023-03-16 23:13:30 +0000

    Update base for Update on "[10/N] Remove ST init, binary and chunk ops"
    [ghstack-poisoned]

--- torch/distributed/_shard/sharded_tensor/_ops/__init__.py
+++ torch/distributed/_shard/sharded_tensor/_ops/__init__.py
@@ -1,5 +1,4 @@
 import torch.distributed._shard.sharded_tensor._ops.chunk
-import torch.distributed._shard.sharded_tensor._ops.math_ops
 import torch.distributed._shard.sharded_tensor._ops.tensor_ops
 
 from .binary_cmp import equal, allclose
