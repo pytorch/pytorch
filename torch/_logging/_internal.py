da85598ff492def1eeace53ecde9f41e9e10fc83
mlazos <mlazos@fb.com>
2023-03-17 17:17:40 -0700

    Update torch/_logging/_internal.py
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- torch/_logging/_internal.py
+++ torch/_logging/_internal.py
@@ -416,11 +416,11 @@ def _init_logs(log_file_name=None):
 
 
 @functools.lru_cache(None)
-def warning_once(self, *args, **kwargs):
+def warning_once(logger_obj, *args, **kwargs):
     """
-    This method is identical to `logger.warning()`, but will emit the warning with the same message only once
+    This function is similar to `logger.warning()`, but will emit the warning with the same message only once
     Note: The cache is for the function arguments, so 2 different callers using the same arguments will hit the cache.
     The assumption here is that all warning messages are unique across the code. If they aren't then need to switch to
     another type of cache that includes the caller frame information in the hashing function.
     """
-    self.warning(*args, **kwargs)
+    logger_obj.warning(*args, **kwargs)
