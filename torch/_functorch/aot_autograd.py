a1c46e5f8fe64530af2cc8ee4526855faf14347b
Michael Lazos <mlazos@fb.com>
2023-03-18 04:17:31 +0000

    component-level configurable logging for dynamo, inductor, aot (#94858)
    Summary:
    
    Adds NNC-like logging that is configured through an env var `TORCH_COMPILE_LOGS`
    Examples:
    `TORCH_LOGS="dynamo,guards" python script.py` - prints dynamo logs at level INFO with guards of all functions that are compiled
    
    `TORCH_LOGS="+dynamo,guards,graph" python script.py` - prints dynamo logs at level DEBUG with guards and graphs (in tabular) format of all graphs that are compiled
    
    [More examples with full output](https://gist.github.com/mlazos/b17f474457308ce15e88c91721ac1cce)
    
    Implementation:
    The implementation parses the log settings from the environment, finds any components (aot, dynamo, inductor) or other loggable objects (guards, graph, etc.) and generates a log_state object. This object contains all of the enabled artifacts, and a qualified log name -> level mapping. _init_logs then adds handlers to the highest level logs (the registered logs), and sets any artifact loggers to level DEBUG if the artifact is enabled.
    
    Note: set_logs is an alternative for manipulating the log_state, but if the environment contains TORCH_LOGS, the environment settings will be prioritized.
    
    Adding a new log:
    To add a new log, a dev should add their log name to torch._logging._registrations (there are examples there already).
    
    Adding a new artifact:
    To add a new artifact, a dev should add their artifact name to torch._logging._registrations as well.
    Additionally, wherever the artifact is logged, `torch._logging.getArtifactLogger(__name__, <artifact_name>)` should be used instead of the standard logging implementation.
    
    [design doc](https://docs.google.com/document/d/1ZRfTWKa8eaPq1AxaiHrq4ASTPouzzlPiuquSBEJYwS8/edit#)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94858
    Approved by: https://github.com/ezyang

--- torch/_functorch/aot_autograd.py
+++ torch/_functorch/aot_autograd.py
@@ -18,7 +18,8 @@ import torch.utils._pytree as pytree
 import torch.utils.dlpack
 from torch import Tensor
 from torch._dispatch.python import enable_python_dispatcher
-from torch._dynamo.utils import dynamo_timed
+from torch._dynamo.utils import dynamo_timed, format_graph_code
+from torch._logging import getArtifactLogger
 from torch._subclasses import CrossRefFakeMode, FakeTensor, FakeTensorMode
 from torch.fx import immutable_collections, Interpreter
 from torch.fx.experimental.proxy_tensor import is_sym_node, py_sym_types
@@ -30,6 +31,9 @@ from .partitioners import default_partition
 from torch._guards import TracingContext, DuplicateInputs
 
 log = logging.getLogger(__name__)
+aot_forward_log = getArtifactLogger(__name__, "aot_forward_graph")
+aot_joint_log = getArtifactLogger(__name__, "aot_joint_graph")
+aot_backward_log = getArtifactLogger(__name__, "aot_backward_graph")
 
 MutationType = Enum(
     "MutationType", ("none", "metadata_only", "data", "data_and_metadata")
@@ -1260,9 +1264,7 @@ def aot_dispatch_base(flat_fn, flat_args: List[Tensor], aot_config: AOTConfig, *
         fw_module.graph.eliminate_dead_code()
         fw_module.recompile()
 
-    if config.debug_graphs:
-        log.debug(f"====== Forward (only) graph {aot_config.aot_id} ======")
-        log.debug(fw_module.print_readable(print_output=False))
+    aot_forward_log.info(format_graph_code(f"====== Forward graph {aot_config.aot_id} ======\n", fw_module))
 
     disable_amp = torch._C._is_any_autocast_enabled()
     context = disable_autocast_manager if disable_amp else nullcontext
@@ -2269,9 +2271,7 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Any], aot_config: AOTConfig, 
             "Graph partitioning without functionalization is not sound, we may introduce errors"
         )
 
-    if config.debug_joint:
-        log.debug(f"====== Joint graph {aot_config.aot_id} ======")
-        log.debug(fx_g.print_readable(print_output=False))
+    aot_joint_log.info(format_graph_code(f"====== Joint graph {aot_config.aot_id} =====\n", fx_g))
 
     with torch.no_grad():
         with track_graph_compiling(aot_config, "joint"):
@@ -2288,11 +2288,8 @@ def aot_dispatch_autograd(flat_fn, flat_args: List[Any], aot_config: AOTConfig, 
             ]
             _num_symints_saved_for_bw = len(symint_outs_saved_for_bw)
 
-        if config.debug_graphs:
-            log.debug(f"====== Forward graph {aot_config.aot_id} ======")
-            log.debug(fw_module.print_readable(print_output=False))
-            log.debug(f"====== Backward graph {aot_config.aot_id} ======")
-            log.debug(bw_module.print_readable(print_output=False))
+        aot_forward_log.info(format_graph_code(f"====== Forward graph {aot_config.aot_id} ======\n", fw_module))
+        aot_backward_log.info(format_graph_code(f"====== Backward graph {aot_config.aot_id} ======\n", bw_module))
 
         with track_graph_compiling(aot_config, "forward"):
             compiled_fw_func = aot_config.fw_compiler(
@@ -2603,8 +2600,6 @@ def create_aot_dispatcher_function(
         **aot_autograd_decompositions,
         **aot_config.decompositions,
     }
-
-    log.setLevel(config.log_level)
 
     # NB: don't bother setting allow_fallback_kernels; this should not actually
     # be configurable in fake tensor, we should automatically do the right
