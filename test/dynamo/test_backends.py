14ffc74f2d7afd68e849cef48da65f6428048847
Mark Saroufim <marksaroufim@fb.com>
2023-03-20 15:12:33 -0700

    Update test_backends.py

--- test/dynamo/test_backends.py
+++ test/dynamo/test_backends.py
@@ -203,7 +203,7 @@ class NormalizeIRTests(torch._dynamo.test_case.TestCase):
 class MPSNotSupportedTest(torch._dynamo.test_case.TestCase):
     @unittest.skipIf(not torch.backends.mps.is_available(), "requires mps")
     def test_default_mps_to_aot_eager(self):
-        model = Seq.to(torch.device("mps"))
+        model = Seq.to("mps")
 
         # Not sure yet if there's a better way to test this
         assert torch.compile(model, backend="inductor") == torch._dynamo.optimize(
