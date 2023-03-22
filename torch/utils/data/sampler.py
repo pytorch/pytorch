b08c5b009ed8f8f33769c127efe93980a4066c5c
Ramil Nugmanov <rnugmano@its.jnj.com>
2023-03-22 11:58:52 +0100

    Sampler API described for customization.

--- torch/utils/data/sampler.py
+++ torch/utils/data/sampler.py
@@ -12,23 +12,45 @@ __all__ = [
     "WeightedRandomSampler",
 ]
 
-T_co = TypeVar('T_co', covariant=True)
+T_co = TypeVar('T_co', int, List[int], covariant=True)
 
 
 class Sampler(Generic[T_co]):
     r"""Base class for all Samplers.
 
     Every Sampler subclass has to provide an :meth:`__iter__` method, providing a
-    way to iterate over indices of dataset elements, and a :meth:`__len__` method
+    way to iterate over indices or lists of indices (batches) of dataset elements, and a :meth:`__len__` method
     that returns the length of the returned iterators.
+
+    Example:
+        class AccedingSequenceLengthSampler(Sampler[int]):
+            def __init__(self, data: List[str]):
+                self.data = data
+
+            def __len__(self):
+                return len(self.data)
+
+            def __iter__(self):
+                sizes = torch.tensor([len(x) for x in self.data])
+                yield from torch.argsort(sizes).tolist()
+
+        class AccedingSequenceLengthBatchSampler(Sampler[List[int]]):
+            def __init__(self, data: List[str], batch_size: int):
+                self.data = data
+                self.batch_size = batch_size
+
+            def __len__(self):
+                return (len(self.data) + self.batch_size - 1) // self.batch_size
+
+            def __iter__(self):
+                sizes = torch.tensor([len(x) for x in self.data])
+                for batch in torch.chunk(torch.argsort(sizes), len(self)):
+                    yield batch.tolist()
 
     .. note:: The :meth:`__len__` method isn't strictly required by
               :class:`~torch.utils.data.DataLoader`, but is expected in any
               calculation involving the length of a :class:`~torch.utils.data.DataLoader`.
     """
-
-    def __init__(self, data_source: Optional[Sized]) -> None:
-        pass
 
     def __iter__(self) -> Iterator[T_co]:
         raise NotImplementedError
