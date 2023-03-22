c10213f658019695cbca3f02c74ab53ff85bc900
Soumith Chintala <soumith@gmail.com>
2023-03-22 13:15:46 -0400

    Update parameter.py

--- torch/nn/parameter.py
+++ torch/nn/parameter.py
@@ -121,28 +121,7 @@ class UninitializedTensorMixin:
         self.data = torch.empty(shape, device=device, dtype=dtype)
         self.__class__ = self.cls_to_become
 
-    @property
-    def shape(self):
-        raise RuntimeError(
-            'Can\'t access the shape of an uninitialized parameter or buffer. '
-            'This error usually happens in `load_state_dict` when trying to load '
-            'an uninitialized parameter into an initialized one. '
-            'Call `forward` to initialize the parameters before accessing their attributes.')
-
-    def share_memory_(self):
-        raise RuntimeError(
-            'Can\'t share memory on an uninitialized parameter or buffer. '
-            'Call `forward` to initialize the parameters before calling '
-            '`module.share_memory()`.')
-
-    def __repr__(self):
-        return f'<{self.__class__.__name__}>'
-
-    def __reduce_ex__(self, proto):
-        # See Note [Don't serialize hooks]
-        return (
-            self.__class__,
-            (self.requires_grad,)
+   
         )
 
     @classmethod
