551e24986c5830fdb72576ca9ba0996555ea045d
milesial <milesial@users.noreply.github.com>
2023-03-22 09:21:17 -0700

    Revert "Optimized optimizer EMA"
    This reverts commit 3b1b82a7467db5722a2ae57a07eb8b49ce17363d.

--- torch/optim/ema_utils.pyi
+++
@@ -1,5 +0,0 @@
-import torch
-from .optimizer import Optimizer
-
-class EMAOptimizer(torch.optim.Optimizer):
-    def __init__(self, optimizer: Optimizer, device: torch.device, decay: float=...) -> None:...
