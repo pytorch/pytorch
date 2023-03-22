da96ae230b5abded796dee4f3c549557de816094
Bin Bao <binbao@fb.com>
2023-03-22 17:08:16 +0000

    [CI] Add a missing dtype flag in nightly perf run (#97357)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97357
    Approved by: https://github.com/huydhn, https://github.com/weiwangmeta

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -292,7 +292,7 @@ test_perf_for_dashboard() {
     # All the accuracy tests can be skipped once the CI accuracy checking is stable enough
     for backend in eager aot_eager; do
       python "benchmarks/dynamo/$suite.py" \
-          --accuracy --backend "$backend" "$@" \
+          --accuracy --"$dtype" --backend "$backend" "$@" \
           --output "$TEST_REPORTS_DIR/${backend}_${suite}_${dtype}_training_cuda_accuracy.csv"
     done
 
@@ -301,10 +301,10 @@ test_perf_for_dashboard() {
     # TODO: update here once cudagraphs is turned on as default
     backend=inductor
     python "benchmarks/dynamo/$suite.py" \
-        --accuracy --backend "$backend" --disable-cudagraphs "$@" \
+        --accuracy --"$dtype" --backend "$backend" --disable-cudagraphs "$@" \
         --output "$TEST_REPORTS_DIR/${backend}_no_cudagraphs_${suite}_${dtype}_training_cuda_accuracy.csv"
     python "benchmarks/dynamo/$suite.py" \
-        --accuracy --backend "$backend" "$@" \
+        --accuracy --"$dtype" --backend "$backend" "$@" \
         --output "$TEST_REPORTS_DIR/${backend}_with_cudagraphs_${suite}_${dtype}_training_cuda_accuracy.csv"
 
     # Run performance test
@@ -312,10 +312,10 @@ test_perf_for_dashboard() {
     # Run performance test for inductor with different configs
     # TODO: add more configs here, e.g. dynamic-shapes, max-autotune, etc.
     python "benchmarks/dynamo/$suite.py" \
-        --performance --backend "$backend" --disable-cudagraphs "$@" \
+        --performance --"$dtype" --backend "$backend" --disable-cudagraphs "$@" \
         --output "$TEST_REPORTS_DIR/${backend}_no_cudagraphs_${suite}_${dtype}_training_cuda_performance.csv"
     python "benchmarks/dynamo/$suite.py" \
-        --performance --backend "$backend" "$@" \
+        --performance --"$dtype" --backend "$backend" "$@" \
         --output "$TEST_REPORTS_DIR/${backend}_with_cudagraphs_${suite}_${dtype}_training_cuda_performance.csv"
   done
 }
