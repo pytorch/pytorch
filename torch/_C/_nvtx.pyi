e1caadb70dda5846dd2ca466dae8be926bba3cf7
milesial <milesial@users.noreply.github.com>
2023-03-23 19:29:04 +0100

    Lint

--- torch/_C/_nvtx.pyi
+++ torch/_C/_nvtx.pyi
@@ -1,7 +1,23 @@
 from typing import Optional
+
 # Defined in torch/csrc/cuda/shared/nvtx.cpp
-def rangePush(message: str, domain: Optional[str] = None, category: Optional[str] = None, color: Optional[int] = None) -> int:  ...
+def rangePush(
+    message: str,
+    domain: Optional[str] = None,
+    category: Optional[str] = None,
+    color: Optional[int] = None,
+) -> int: ...
 def rangePop(domain: Optional[str] = None) -> int: ...
-def rangeStart(message: Optional[str], domain: Optional[str] = None, category: Optional[str] = None, color: Optional[int] = None) -> int: ...
+def rangeStart(
+    message: Optional[str],
+    domain: Optional[str] = None,
+    category: Optional[str] = None,
+    color: Optional[int] = None,
+) -> int: ...
 def rangeEnd(id: int, domain: Optional[str] = None) -> None: ...
-def mark(message: str, domain: Optional[str] = None, category: Optional[str] = None, color: Optional[int] = None) -> None: ...
+def mark(
+    message: str,
+    domain: Optional[str] = None,
+    category: Optional[str] = None,
+    color: Optional[int] = None,
+) -> None: ...
