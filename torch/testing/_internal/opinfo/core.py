4610ce49f65aa899b5411ea0e000d66fc02cd460
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2023-03-23 01:46:17 +0000

    Fix typo under torch/testing directory  (#97254)
    This PR fixes typo in comments and messages under `torch/testing` directory.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97254
    Approved by: https://github.com/kit1980, https://github.com/malfet

--- torch/testing/_internal/opinfo/core.py
+++ torch/testing/_internal/opinfo/core.py
@@ -151,7 +151,7 @@ class SampleInput:
                 not var_args and not var_kwargs
             ), """
 A SampleInput can be constructed "naturally" with *args and **kwargs or by
-explicitly setting the "args" and "kwargs" paremeters, but the two
+explicitly setting the "args" and "kwargs" parameters, but the two
 methods of construction cannot be mixed!"""
         elif len(var_args) or len(var_kwargs):
             assert (
@@ -1829,7 +1829,7 @@ def generate_elementwise_binary_with_scalar_samples(
         yield SampleInput(lhs_scalar, args=(rhs_scalar,))
 
 
-# Returns a generator of pairs of contiguous tensors and 0d tensos and scalars and type promotion
+# Returns a generator of pairs of contiguous tensors and 0d tensors and scalars and type promotion
 def generate_elementwise_binary_with_scalar_and_type_promotion_samples(
     op, *, device, dtype, requires_grad=False
 ):
