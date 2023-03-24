5fa336526843fdf9942dd5d4a67a1d2e0718c83d
lcskrishna <lollachaitanya@gmail.com>
2023-03-24 12:10:46 +0000

    remove unused variables

--- test/distributed/fsdp/test_fsdp_comm_hooks.py
+++ test/distributed/fsdp/test_fsdp_comm_hooks.py
@@ -16,7 +16,6 @@ from torch.testing._internal.common_distributed import (
     requires_nccl_version,
     skip_but_pass_in_sandcastle_if,
     skip_if_lt_x_gpu,
-    skip_if_rocm,
 )
 from torch.testing._internal.common_fsdp import FSDPTest
 from torch.testing._internal.common_utils import (
