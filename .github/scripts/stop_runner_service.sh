66e7fc14aa729c2af2e519ca28e40a1c03caf76e
Huy Do <huydhn@gmail.com>
2023-03-24 21:22:24 -0700

    Provide some additional debug message

--- .github/scripts/stop_runner_service.sh
+++ .github/scripts/stop_runner_service.sh
@@ -8,6 +8,7 @@ RUNNER_SERVICE=$(cat "${RUNNER_WORKSPACE}/../../.service")
 echo "GitHub self-hosted runner service: ${RUNNER_SERVICE}"
 
 if [[ -n "${RUNNER_SERVICE}" ]]; then
+  echo "The self-hosted runner has encounter an irrecoverable error and will be shutdown"
   # and stop it to prevent the runner from receiving new jobs
   sudo systemctl stop "${RUNNER_SERVICE}"
 fi
