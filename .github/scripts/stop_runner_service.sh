3c538b0dd5b9ce93c6d4e6a94317fab3b97d0b48
Huy Do <huydhn@gmail.com>
2023-03-24 19:45:15 -0700

    Stop runner service when its GPU crashes

---
+++ .github/scripts/stop_runner_service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set +e
+
+# Get the service name
+RUNNER_SERVICE=$(cat "${RUNNER_WORKSPACE}/../../.service")
+echo "GitHub self-hosted runner service: ${RUNNER_SERVICE}"
+
+if [[ -n "${RUNNER_SERVICE}" ]]; then
+  # and stop it to prevent the runner from receiving new jobs
+  sudo systemctl stop "${RUNNER_SERVICE}"
+fi
