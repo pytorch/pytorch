cb970ea2e5696fd63126a700bc813964cbebf1cf
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared31754.05.ldc2.facebook.com>
2023-03-27 00:03:29 -0700

    2023-03-27 nightly release (542fb0b1fad6bf61929df16e2133e9a296820f08)

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -268,12 +268,16 @@ def clone_preserve_strides(x):
 def run_and_get_cpp_code(fn, args):
     torch._dynamo.reset()
     import io
-    from contextlib import redirect_stdout
+    import logging
 
-    f = io.StringIO()
-    with redirect_stdout(f):
-        fn(*args)
-    s = f.getvalue()
+    log_capture_string = io.StringIO()
+    ch = logging.StreamHandler(log_capture_string)
+    from torch._inductor.graph import output_code_log
+
+    output_code_log.addHandler(ch)
+    fn(*args)
+    s = log_capture_string.getvalue()
+    output_code_log.removeHandler(ch)
     return s
 
 
