2d7f482441ce5270caf7a7834f768489bbfb9a43
Mark Saroufim <marksaroufim@fb.com>
2023-03-27 17:16:26 -0700

    Update test_compile_benchmark_util.py

--- test/test_compile_benchmark_util.py
+++ test/test_compile_benchmark_util.py
@@ -2,9 +2,15 @@ import torch
 import torch._dynamo as torchdynamo
 from torch.testing._internal.common_utils import TestCase, run_tests
 from torch.utils.benchmark.utils.compile import bench_all
+import unittest
 
-torchdynamo.reset()
+# We cannot import TEST_CUDA from torch.testing._internal.common_cuda here, because if we do that,
+# the TEST_CUDNN line from torch.testing._internal.common_cuda will be executed multiple times
+# as well during the execution of this test suite, and it will cause
+# CUDA OOM error on Windows.
+TEST_CUDA = torch.cuda.is_available()
 
+@unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
 class TestCompileBenchmarkUtil(TestCase):
     def test_training_and_inference(self):
         class ToyModel(torch.nn.Module):
@@ -14,7 +20,8 @@ class TestCompileBenchmarkUtil(TestCase):
 
             def forward(self, x):
                 return x * self.weight
-
+                
+        torchdynamo.reset()
         model = ToyModel().cuda()
 
         print("===== Inference =====")
@@ -22,13 +29,10 @@ class TestCompileBenchmarkUtil(TestCase):
         assert(inference_table.rows[0][0] == "Inference")
         assert(inference_table.rows[0][1] == "Eager")
         assert(inference_table.rows[0][2] == "-")
-        print(inference_table)
-        print("\n===== Training =====")
         training_table = bench_all(model, torch.ones(1024, 2, 2).cuda(), 5, is_training=True, optimizer=torch.optim.SGD(model.parameters(), lr=0.01))
         assert(training_table.rows[0][0] == "Training")
         assert(training_table.rows[0][1] == "Eager")
         assert(training_table.rows[0][2] == "-")
-        print(training_table)
 
 if __name__ == '__main__':
     run_tests()
