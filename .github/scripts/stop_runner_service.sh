ddf9554b3610f5ea01ab663c9a780ab899134ef1
Huy Do <huydhn@gmail.com>
2023-03-28 16:42:49 -0700

    Change to runner home directory

--- .github/scripts/stop_runner_service.sh
+++ .github/scripts/stop_runner_service.sh
@@ -9,9 +9,11 @@ echo "GitHub self-hosted runner service: ${RUNNER_SERVICE}"
 
 if [[ -n "${RUNNER_SERVICE}" ]]; then
   echo "The self-hosted runner has encountered an unrecoverable error and will be shutdown"
+
+  pushd "${RUNNER_WORKSPACE}/../.."
   # Stop it to prevent the runner from receiving new jobs
-  sudo systemctl stop "${RUNNER_SERVICE}"
-  # then uninstall it
+  sudo ./svc.sh stop
+  # then uninstall the service
   sudo ./svc.sh uninstall
   # Finally, shutting down the runner completely
   sudo shutdown -P now
