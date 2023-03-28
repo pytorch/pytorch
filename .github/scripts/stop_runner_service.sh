bf30e5200bf0dc82f5176ca5b0e0a618156cea29
Huy Do <huydhn@gmail.com>
2023-03-28 16:06:59 -0700

    Clean up and shutdown

--- .github/scripts/stop_runner_service.sh
+++ .github/scripts/stop_runner_service.sh
@@ -9,6 +9,14 @@ echo "GitHub self-hosted runner service: ${RUNNER_SERVICE}"
 
 if [[ -n "${RUNNER_SERVICE}" ]]; then
   echo "The self-hosted runner has encountered an unrecoverable error and will be shutdown"
-  # and stop it to prevent the runner from receiving new jobs
+  # Stop it to prevent the runner from receiving new jobs
   sudo systemctl stop "${RUNNER_SERVICE}"
+  # then uninstall it
+  sudo ./svc.sh uninstall
+  # Finally, shutting down the runner completely
+  sudo shutdown -P now
+  # NB: In my test, cleaning up and shutting down the runner this way would already
+  # remove the runner from the list of registered runners. Calling config.sh remove
+  # seems redundant as it would require an org token to use, which I don't want to
+  # add as yet another secret to the CI if there is no need
 fi
