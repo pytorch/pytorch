7770bbd777ecb98fd170fdb813cf1183c85d069c
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-03-29 21:29:18 +0000

    Add dynamo test for ModuleDict with custom __getitem__

--- test/dynamo/test_modules.py
+++ test/dynamo/test_modules.py
@@ -305,10 +305,22 @@ class ModuleDict(torch.nn.Module):
             }
         )
 
+    def __getitem__(self, key: str) -> torch.nn.Module:
+        return self.layers[key]
+
     def forward(self, x):
         # TODO(future PR): handle more logic
         x = self.layers["0"](x)
         return x
+
+
+class ModuleWithCustomModuleDict(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.dict = ModuleDict()
+        
+    def forward(self, x):
+        return self.dict["0"](x)
 
 
 class TensorList(torch.nn.Module):
@@ -760,6 +772,7 @@ class NNModuleTests(torch._dynamo.test_case.TestCase):
     test_stringmember = make_test(StringMember())
     test_modulelist = make_test(ModuleList())
     test_moduledict = make_test(ModuleDict())
+    test_custom_moduledict = make_test(ModuleWithCustomModuleDict())
     test_super1 = make_test(SuperModule())
     test_super2 = make_test(SuperModule2())
     test_super_class_method = make_test(SuperChildCallsClassMethod())
