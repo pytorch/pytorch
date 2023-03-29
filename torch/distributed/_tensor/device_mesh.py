1fe49d9ff9d26eeab9ae297f9aa5c994805164f9
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-03-29 16:39:03 +0000

    Update on "[dtensor] guard type imports for devicemesh"
    This PR tries to guard the type import for older version numpy
    
    [ghstack-poisoned]

--- torch/distributed/_tensor/device_mesh.py
+++ torch/distributed/_tensor/device_mesh.py
@@ -1,6 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 import warnings
-from typing import List, Optional, Union
+from typing import List, Optional, Union, TYPE_CHECKING
 
 import torch
 from torch.distributed.distributed_c10d import (
@@ -27,11 +27,13 @@ from torch.distributed.distributed_c10d import (
 import torch.distributed.distributed_c10d as c10d
 import torch.distributed._functional_collectives as funcol
 
-try:
-    from numpy.typing import ArrayLike
-    NDArrayType = ArrayLike
-except ImportError:
-    NDArrayType = "ArrayLike"
+# only import numpy when type checking
+if TYPE_CHECKING:
+    try:
+        from numpy.typing import ArrayLike
+        NDArrayType = ArrayLike
+    except ImportError:
+        NDArrayType = "ArrayLike"
 
 
 _global_device_mesh: Optional["DeviceMesh"] = None
