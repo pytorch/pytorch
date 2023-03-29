e6909f6ccc20a17737e01a69a256663cd6331d7e
Michael Lazos <mlazos@fb.com>
2023-03-29 19:20:05 +0000

    [Dynamo] Fix for tuple construction from tuple iterators (#97862)
    Fixes #93405
    
    In short - when calling the builtin function `Tuple` on a list variable we added a list length guard. This paired with converting tuple iterators to a ListIteratorVariable resulted in this guard being improperly added.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97862
    Approved by: https://github.com/yanboliang, https://github.com/jansel

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -68,12 +68,12 @@ from .dicts import (
 )
 from .functions import UserFunctionVariable, UserMethodVariable
 from .lists import (
-    ListIteratorVariable,
     ListVariable,
     NamedTupleVariable,
     RangeVariable,
     SizeVariable,
     SliceVariable,
+    TupleIteratorVariable,
     TupleVariable,
 )
 from .misc import (
@@ -618,7 +618,9 @@ class VariableBuilder:
             ).add_guards(guards)
             for i in range(tuple_iterator_len(value))
         ]
-        return ListIteratorVariable(output, mutable_local=MutableLocal(), guards=guards)
+        return TupleIteratorVariable(
+            output, mutable_local=MutableLocal(), guards=guards
+        )
 
     def wrap_slice_range(self, value: Union[slice, range]):
         items = [
