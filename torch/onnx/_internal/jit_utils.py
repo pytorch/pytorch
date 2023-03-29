4edd82d5d822b9574944f94d52b45ebfe10464c1
Xuehai Pan <XuehaiPan@outlook.com>
2023-03-29 21:17:02 +0800

    Merge `isinstance` checks
    Co-authored-by: Aaron Gokaslan <skylion.aaron@gmail.com>

--- torch/onnx/_internal/jit_utils.py
+++ torch/onnx/_internal/jit_utils.py
@@ -310,8 +310,7 @@ def _create_node(
 @_beartype.beartype
 def _is_onnx_list(value):
     return (
-        not isinstance(value, (str, bytes))
-        and not isinstance(value, torch.Tensor)
+        not isinstance(value, (str, bytes, torch.Tensor))
         and isinstance(value, Iterable)
     )
 
