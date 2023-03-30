b165d950b36db6da413b5b0c747ca687af11e9a9
Shunting Zhang <shunting@fb.com>
2023-03-30 06:48:58 +0000

    Update base for Update on "[WIP][inductor] coordinate descent tuning upon max-autotune"
    Previous results tunes RBLOCK for persistent reduction, which should be invalid. Will rerun experiments and report new numbers.
    
    
    Still a draft. cc ngimel  Chillee jansel 
    
    
    
    [ghstack-poisoned]

--- torch/_inductor/triton_heuristics.py
+++ torch/_inductor/triton_heuristics.py
@@ -320,13 +320,8 @@ def cached_autotune(
     configs = unique_configs(configs)
     assert len(configs) == 1 or filename
 
-    # The autotune cache will simply replace the list of candidate configs with
-    # the best config cached. We don't want that when we benchmark triton kernels.
-    # We need the perf for each of the candidate config instead.
-    cache_autotune_result = not config.benchmark_kernel
-
     # on disk caching logic
-    if cache_autotune_result and filename is not None and len(configs) > 1:
+    if filename is not None and len(configs) > 1:
         cache_filename = os.path.splitext(filename)[0] + ".best_config"
         configs_hash = hash_configs(configs)
         best_config = load_cached_autotuning(cache_filename, configs_hash, configs)
@@ -519,6 +514,10 @@ def pointwise(size_hints, meta, tile_hint=None, filename=None):
             configs.extend([
                 # improve 1.832x for https://gist.github.com/shunting314/69b5055193148ade349ac7e58c85d2d9 
                 Config({"XBLOCK": 256}, num_warps=8, num_stages=1),
+                # improve 1.031x for https://gist.github.com/shunting314/339dd078cb9711536e4539dbb50b9032
+                Config({"XBLOCK": 512}, num_warps=2, num_stages=1),
+                # improve 1.016x for https://gist.github.com/shunting314/a5e6ee5cf8700ad3d43ec6853db4b236
+                Config({"XBLOCK": 512}, num_warps=4, num_stages=1),
             ])
         return cached_autotune(configs, meta=meta, filename=filename)
     if len(size_hints) == 2:
@@ -590,6 +589,12 @@ def reduction(size_hints, reduction_hint=False, meta=None, filename=None):
                 triton_config_reduction(size_hints, 8, 512),
                 # improve 1.121x for https://gist.github.com/shunting314/6267da87c6524dab29a3e33f14ff91db
                 Config({"XBLOCK": 1, "RBLOCK": 4096}, num_warps=8, num_stages=1),
+                # improve 1.074x for https://gist.github.com/shunting314/bb36b37a3d049a29ff6bada9dbe18fb8
+                Config({"XBLOCK": 2, "RBLOCK": 2048}, num_warps=8, num_stages=1),
+                # improve 1.143x for https://gist.github.com/shunting314/ae6a0d3c63409bed7759ee7bdcde01a8
+                Config({"XBLOCK": 1, "RBLOCK": 1024}, num_warps=16, num_stages=1),
+                # improve 1.033x for https://gist.github.com/shunting314/d1036afd5210e19da3144c24f3b475d8
+                Config({"XBLOCK": 1, "RBLOCK": 1024}, num_warps=8, num_stages=1),
             ],
             meta=meta,
             filename=filename,
