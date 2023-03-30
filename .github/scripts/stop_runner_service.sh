657f5e953127487a4189117f6794e4dd64446f6d
Huy Do <huydhn@gmail.com>
2023-03-29 21:48:57 -0700

    Stop and shutdown runner in one command

--- .github/scripts/stop_runner_service.sh
+++ .github/scripts/stop_runner_service.sh
@@ -11,12 +11,9 @@ if [[ -n "${RUNNER_SERVICE}" ]]; then
   echo "The self-hosted runner has encountered an unrecoverable error and will be shutdown"
 
   pushd "${RUNNER_WORKSPACE}/../../"
-  # Stop it to prevent the runner from receiving new jobs
-  sudo ./svc.sh stop
-  # then uninstall the service
-  sudo ./svc.sh uninstall
-  # Finally, shutting down the runner completely
-  sudo shutdown -P now
+  # Stop it to prevent the runner from receiving new jobs. Run this as one command as
+  # svc.sh stop would terminate the connection to the runner
+  sudo ./svc.sh stop && sudo ./svc.sh uninstall && sudo shutdown -P now
   # NB: In my test, cleaning up and shutting down the runner this way would already
   # remove the runner from the list of registered runners. Calling config.sh remove
   # seems redundant as it would require an org token to use, which I don't want to
