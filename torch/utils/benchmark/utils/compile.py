0afaa71e2924bf6b914dd1410c4fe811eb8c0254
Mark Saroufim <marksaroufim@gmail.com>
2023-03-30 23:32:02 +0000

    remove old test

--- torch/utils/benchmark/utils/compile.py
+++ torch/utils/benchmark/utils/compile.py
@@ -174,27 +174,3 @@ def bench_all(
 
 
     return tabulate(table, headers=field_names, tablefmt="github")
-
-if __name__ == "__main__":
-    torch._dynamo.reset()
-
-    class ToyModel(torch.nn.Module):
-        def __init__(self):
-            super(ToyModel, self).__init__()
-            self.weight = torch.nn.Parameter(torch.Tensor(2, 2))
-
-        def forward(self, x):
-            return x * self.weight
-
-    model = ToyModel().cuda()
-
-    print("===== Inference =====")
-    inference_table = bench_all(model, torch.ones(1024, 2, 2).cuda(), 5)
-    assert("Inference" in inference_table) and "Eager" in inference_table and "-" in inference_table
-
-    print(inference_table)
-    print("\n===== Training =====")
-    training_table = bench_all(model, torch.ones(1024, 2, 2).cuda(), 5,
-                               optimizer=torch.optim.SGD(model.parameters(), lr=0.01))
-    assert("Training" in training_table) and "Eager" in training_table and "-" in training_table
-    print(training_table)
