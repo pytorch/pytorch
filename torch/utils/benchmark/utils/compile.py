0c7008b2f1ad94a0e9afecdcc010da83ebe8e261
Mark Saroufim <marksaroufim@gmail.com>
2023-03-31 00:07:50 +0000

    i

--- torch/utils/benchmark/utils/compile.py
+++ torch/utils/benchmark/utils/compile.py
@@ -17,7 +17,7 @@ def _enable_tensor_cores():
     if torch.cuda.is_available():
         if torch.backends.cuda.matmul.allow_tf32 is False and torch.cuda.get_device_capability() >= (8, 0):
             torch.set_float32_matmul_precision("high")
-            if _warned_tensor_cores:
+            if not _warned_tensor_cores:
                 print("Your GPU supports tensor cores")
                 print("we will enable it automatically by setting `torch.set_float32_matmul_precision('high')`")
                 _warned_tensor_cores = True
