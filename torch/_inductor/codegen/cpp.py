fcac263a7e3915439ede283a84e45235eafc4da7
haozhe.zhu <haozhe.zhu@intel.com>
2023-03-31 15:41:35 +0800

    enable data type propogation
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -4,6 +4,7 @@ import functools
 import logging
 import math
 import sys
+import itertools
 from copy import copy, deepcopy
 from pathlib import Path
 from typing import ClassVar, Dict, List
@@ -229,6 +230,7 @@ class OptimizationContext:
     is_bf16_mem_copy: bool = False
 
     dtype: torch.dtype = torch.float
+    dtype_propogated = False
     ops_name: str = ""
     is_most_inner_loop_irrevelant: bool = False
 
@@ -2060,6 +2062,77 @@ class CppKernelProxy(CppKernel):
         self.call_ranges = None
         self.picked_vec_isa: codecache.VecISA = codecache.pick_vec_isa()
 
+    def data_type_propogation(self, nodes):
+
+        def propogate_node(node: torch.fx.Node):
+            _node: torch.fx.Node = node
+            if _node.target == 'ops':
+                return False
+            ops_to_bool = ['is_inf', 'is_nan','bitwise_xor', 'logical_not', 'signbit', 'le', 'lt', 'ge', 'gt', 'eq', 'ne',
+                       'bitwise_not', 'bitwise_or', 'bitwise_left_shift', 'bitwise_right_shift']
+            ops_with_dtype_arg = ['constant', 'to_dtype', 'rand', 'randn']
+            reduction_to_bool = ['any']
+            reduction_to_int64 = ['argmin', 'argmax']
+            if OptimizationContext.key in _node.meta:
+                opt_ctx = _node.meta[OptimizationContext.key]
+            else:
+                opt_ctx = OptimizationContext()
+            if opt_ctx.dtype_propogated:
+                return False
+            if _node.target in ops_to_bool:
+                opt_ctx.dtype = torch.bool
+            elif _node.target in ops_with_dtype_arg:
+                opt_ctx.dtype = _node.args[-1]
+            elif _node.target == 'reduction':
+                reduction_type = _node.args[4]
+                if reduction_type in reduction_to_bool:
+                    opt_ctx.dtype = torch.bool
+                elif reduction_type in reduction_to_int64:
+                    opt_ctx.dtype = torch.int64
+            elif _node.target == 'load':
+                opt_ctx.dtype = V.graph.get_dtype(_node.args[1])
+
+            # node.target not belong to any ops which can directly get the dtype
+            # need propogate dtype with it's input node
+            dtype = None
+            inputs = node.all_input_nodes
+            input_nodes = [n for n in inputs if isinstance(n, torch.fx.node.Node) and n.target != 'ops']
+            if len(input_nodes) == 0:
+                return False
+            all_input_nodes_propogated = all(OptimizationContext.key in n.meta and n.meta[OptimizationContext.key].dtype_propogated for n in input_nodes)
+            if not all_input_nodes_propogated:
+                return False
+            # all input nodes have propogated dtype, we will promot to dtype with highest precision
+            dtype = functools.reduce(
+                torch.promote_types, [n.meta[OptimizationContext.key].dtype for n in itertools.chain(*input_nodes)]
+            )
+            opt_ctx.dtype = dtype
+            _node.meta[OptimizationContext.key] = opt_ctx
+            return True
+
+        def propogation(sub_graph: torch.fx.Graph):
+            new_node_propogated = False
+            for node in sub_graph.nodes:
+                new_node_propogated = propogate_node(node) or new_node_propogated
+
+            return new_node_propogated
+
+        def _data_type_propogation(loop_body: ir.LoopBody):
+            sub_blocks = [loop_body.root_block] + list(loop_body.subblocks.values())
+            for sub_block in sub_blocks:
+                _sub_graph: torch.fx.Graph = sub_block.graph
+                _sub_graph.print_tabular()
+                changed = propogation(_sub_graph)
+                while changed:
+                    changed = propogation(_sub_graph)
+
+        for _node in nodes:    
+            assert isinstance(_node, SchedulerNode)
+            node: SchedulerNode = _node
+            if isinstance(node._body, ir.LoopBody):
+                body: ir.LoopBody = node._body
+                _data_type_propogation(body)
+
     def legalize_bf16(self, nodes):
         def add_to_dtype(sub_graph: torch.fx.Graph):
             def is_bf16_mem_copy(node: torch.fx.Node):
@@ -2204,6 +2277,7 @@ class CppKernelProxy(CppKernel):
                 _legalize_bf16(body)
 
     def codegen_nodes(self, nodes):
+        self.data_type_propogation(nodes)
         # Legalize BF16 node by adding to_dtype explicitly
         self.legalize_bf16(nodes)
 
