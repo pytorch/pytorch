a15d752d7888804dde0acd81053d68a2cb3d1b00
Mark Saroufim <marksaroufim@gmail.com>
2023-03-31 00:06:43 +0000

    minor bug

--- torch/utils/benchmark/utils/compile.py
+++ torch/utils/benchmark/utils/compile.py
@@ -17,7 +17,7 @@ def _enable_tensor_cores():
     if torch.cuda.is_available():
         if torch.backends.cuda.matmul.allow_tf32 is False and torch.cuda.get_device_capability() >= (8, 0):
             torch.set_float32_matmul_precision("high")
-            if not _warned_tensor_cores:
+            if _warned_tensor_cores:
                 print("Your GPU supports tensor cores")
                 print("we will enable it automatically by setting `torch.set_float32_matmul_precision('high')`")
                 _warned_tensor_cores = True
@@ -73,6 +73,7 @@ def benchmark_compile(
     """
     if backend:
         try:
+            torch._dynamo.reset()
             opt_model = torch.compile(model, backend=backend, mode=mode)
 
             # Compilation only happens after the first inference
