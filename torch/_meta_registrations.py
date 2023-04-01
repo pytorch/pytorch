e8d39606ebddb1eda35836309bb353dd7629ac2f
Shen Li <cs.shenli@gmail.com>
2023-04-01 06:11:09 +0000

    [SPMD] Enable fused Adam in full train step tracing (#98113)
    Differential Revision: [](https://our.internmc.facebook.com/intern/diff/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98113
    Approved by: https://github.com/yifuwang, https://github.com/fegin

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -1367,6 +1367,32 @@ def meta__foreach_pow_scalar_and_tensor(self, exponent):
     return [torch.empty_like(e) for e in exponent]
 
 
+@register_meta([aten._fused_adam_.default])
+def meta__fused_adam_(
+    self,
+    grads,
+    exp_avgs,
+    exp_avg_sqs,
+    max_exp_avg_sqs,
+    state_steps,
+    *,
+    lr,
+    beta1,
+    beta2,
+    weight_decay,
+    eps,
+    amsgrad,
+    maximize,
+    grad_scale=None,
+    found_inf=None,
+):
+    for l in [self, grads, exp_avgs, exp_avg_sqs, max_exp_avg_sqs, state_steps]:
+        check(
+            isinstance(self, List),
+            lambda: f"exponent must be a tensor list but got {type(self)}",
+        )
+
+
 @register_meta([aten._int_mm])
 @out_wrapper()
 def meta__int_mm(a, b):
