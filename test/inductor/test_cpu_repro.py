8e5f491623885294f819090752f3388717166590
Jiong Gong <jiong.gong@intel.com>
2023-04-02 10:06:44 +0000

    [Inductor] simplify CPP backend Tile2D code and support non-contiguous load/store (#97626)
    Remove `CppTile2DTailKernel` and `CppTile2DKernelChecker` and reuse `CppVecKernel` and `CppVecKernelChecker` for them. Add vectorization with fallback for load/store in CppVecKernel for the non-contiguous load/store needed by `CppTile2DTailKernel`.
    
    This PR also adds a functional support for transposed copy of bfloat16 data types. Better performance requires vectorized intrinsics implemented for at::vec::transpose_mxn. cc @soumith @voznesenskym @penguinwu @anijain2305 @EikanWang @Guobing-Chen @XiaobingSuper @zhuhaozhe @blzheng @Xia-Weiwen @wenzhe-nrv @jiayisunx @peterbell10 @desertfire
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97626
    Approved by: https://github.com/jansel

--- test/inductor/test_cpu_repro.py
+++ test/inductor/test_cpu_repro.py
@@ -1005,7 +1005,7 @@ class CPUReproTests(TestCase):
             traced = make_fx(fn)(x1, x2)
             compiled = compile_fx_inner(traced, [x1, x2])
             assert same(fn(x1, x2)[0], compiled([x1, x2])[0], equal_nan=True)
-            assert metrics.generated_cpp_vec_kernel_count == 1
+            assert metrics.generated_cpp_vec_kernel_count == 2
 
             torch._dynamo.reset()
             metrics.reset()
@@ -1059,9 +1059,9 @@ class CPUReproTests(TestCase):
                 metrics.reset()
                 x = torch.randn(64, 58, 28, 28)
                 opt_fn = torch._dynamo.optimize("inductor")(channel_shuffle)
-                same(channel_shuffle(x, 2), opt_fn(x, 2))
+                self.assertTrue(same(channel_shuffle(x, 2), opt_fn(x, 2)))
                 if simdlen != 1:
-                    assert metrics.generated_cpp_vec_kernel_count == 1
+                    assert metrics.generated_cpp_vec_kernel_count == 2
 
     @slow()
     @unittest.skipIf(
@@ -1111,22 +1111,23 @@ class CPUReproTests(TestCase):
 
         for simdlen in (None, 256, 1):
             with config.patch({"cpp.simdlen": simdlen}):
-                for shape in (
-                    (7, 7),
-                    (8, 8),
-                    (9, 9),
-                    (16, 16),
-                    (17, 17),
-                    (32, 32),
-                    (33, 33),
-                ):
-                    torch._dynamo.reset()
-                    metrics.reset()
-                    x = torch.randn(shape)
-                    opt_fn = torch._dynamo.optimize("inductor")(fn)
-                    same(fn(x), opt_fn(x))
-                    if simdlen != 1:
-                        assert metrics.generated_cpp_vec_kernel_count == 1
+                for dtype in (torch.float, torch.bfloat16):
+                    for shape in (
+                        (7, 7),
+                        (8, 8),
+                        (9, 9),
+                        (16, 16),
+                        (17, 17),
+                        (32, 32),
+                        (33, 33),
+                    ):
+                        torch._dynamo.reset()
+                        metrics.reset()
+                        x = torch.randn(shape, dtype=dtype)
+                        opt_fn = torch._dynamo.optimize("inductor")(fn)
+                        self.assertTrue(same(fn(x), opt_fn(x)))
+                        if simdlen != 1:
+                            assert metrics.generated_cpp_vec_kernel_count == 2
 
     def test_transpose_non_contiguous(self):
         def fn(a):
