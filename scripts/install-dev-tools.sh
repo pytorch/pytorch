4736c69f480cbc698443fced431ed97e7be3b774
drisspg <drisspg@fb.com>
2023-04-03 16:55:42 -0700

    was double copying

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Run this command from the PyTorch directory after cloning the source code using the “Get the PyTorch Source“ section below
 pip install -r requirements.txt
-git clone --recursive https://github.com/pytorch/pytorch
-cd pytorch
 git submodule sync
 git submodule update --init --recursive
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-python setup.py develop
+# python setup.py develop
