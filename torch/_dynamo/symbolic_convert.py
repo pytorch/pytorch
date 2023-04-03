403a786b18149c0b412601fa1a33b13c6488fb2e
Michael Voznesensky <voznesenskym@gmail.com>
2023-04-03 06:41:49 +0000

    Update on "Add support for list copy"
    cc soumith penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -672,8 +672,8 @@ class InstructionTranslatorBase(Checkpointable[InstructionTranslatorGraphState])
     def STORE_FAST(self, inst):
         val = self.pop()
         if hasattr(val, "mutable_local") and val.mutable_local:
-            assert not hasattr(val.mutable_local, "source")
-            val.mutable_local.source = LocalSource(inst.argval)
+            if not hasattr(val.mutable_local, "source"):
+                val.mutable_local.source = LocalSource(inst.argval)
         self.symbolic_locals[inst.argval] = val
 
     def DELETE_FAST(self, inst):
