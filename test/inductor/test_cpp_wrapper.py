0590fa61fec6e2b62d6bcf4d7230d3e297a6296c
chunyuan <chunyuan.wu@intel.com>
2023-04-03 02:57:38 +0000

    Update on "Inductor cpp wrapper: support dynamic shapes"
    cc soumith voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- test/inductor/test_cpp_wrapper.py
+++ test/inductor/test_cpp_wrapper.py
@@ -1,5 +1,4 @@
 # Owner(s): ["module: inductor"]
-import itertools
 import sys
 import unittest
 
@@ -19,15 +18,18 @@ except unittest.SkipTest:
     raise
 
 
+class CppWrapperTemplate:
+    pass
+
+
 class TestCppWrapper(TorchTestCase):
     pass
 
 
-def make_test_case(name, dynamic_shapes, device="cpu"):
+def make_test_case(name, device="cpu"):
     test_name = f"{name}_{device}"
 
     @config.patch(cpp_wrapper=True, search_autotune_cache=False)
-    @torch._dynamo.config.patch(dynamic_shapes=dynamic_shapes)
     def fn(self):
         tests = test_torchinductor.CpuTests()
         tests.setUpClass()
@@ -42,34 +44,34 @@ def make_test_case(name, dynamic_shapes, device="cpu"):
             tests.tearDownClass()
 
     fn.__name__ = test_name
-    setattr(TestCppWrapper, test_name, fn)
+    setattr(CppWrapperTemplate, test_name, fn)
 
 
-for name, dynamic_shapes in itertools.product(
-    [
-        "test_as_strided",  # buffer reuse
-        "test_bitwise",  # int32
-        "test_bmm1",
-        "test_bmm2",
-        "test_cat",  # alias
-        "test_int_div",
-        "test_linear1",
-        "test_linear2",
-        "test_lowmem_dropout1",  # None as output
-        "test_mm_views",
-        "test_profiler_mark_wrapper_call",
-        "test_reduction1",  # Reduction
-        "test_relu",  # multiple inputs
-        "test_scalar_input",
-        "test_silu",  # single input, single output
-        "test_sum_dtype",  # float64
-        "test_sum_int",  # bool, int64, int8, uint8
-        "test_transpose",  # multiple outputs, buffer clear
-    ],
-    [False, True],
-):
+for name in [
+    "test_as_strided",  # buffer reuse
+    "test_bitwise",  # int32
+    "test_bmm1",
+    "test_bmm2",
+    "test_cat",  # alias
+    "test_int_div",
+    "test_linear1",
+    "test_linear2",
+    "test_lowmem_dropout1",  # None as output
+    "test_mm_views",
+    "test_profiler_mark_wrapper_call",
+    "test_reduction1",  # Reduction
+    "test_relu",  # multiple inputs
+    "test_scalar_input",
+    "test_silu",  # single input, single output
+    "test_sum_dtype",  # float64
+    "test_sum_int",  # bool, int64, int8, uint8
+    "test_transpose",  # multiple outputs, buffer clear
+]:
     # TODO: leverage test_inductor_dynamic_shapes.py
-    make_test_case(name, dynamic_shapes=dynamic_shapes)
+    make_test_case(name)
+
+
+test_torchinductor.copy_tests(CppWrapperTemplate, TestCppWrapper, "cpp_wrapper")
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
