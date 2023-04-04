6fdf52a6fcd29efc17c11287006a35438f33c7d1
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-04-04 00:11:16 -0700

    Update magma commit to support header deprecation

--- .ci/docker/common/install_rocm_magma.sh
+++ .ci/docker/common/install_rocm_magma.sh
@@ -5,8 +5,7 @@ set -ex
 # "install" hipMAGMA into /opt/rocm/magma by copying after build
 git clone https://bitbucket.org/icl/magma.git
 pushd magma
-# Fixes memory leaks of magma found while executing linalg UTs
-git checkout 5959b8783e45f1809812ed96ae762f38ee701972
+git checkout 28592a7170e4b3707ed92644bf4a689ed600c27f
 cp make.inc-examples/make.inc.hip-gcc-mkl make.inc
 echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
 echo 'LIB += -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib -Wl,--rpath,$(MKLROOT)/lib -Wl,--rpath,/opt/rocm/magma/lib' >> make.inc
