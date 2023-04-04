pytorch/pytorch

commit 96f548a1ac6e54791d81ed17d5bf6660ceba701d

[inductor] Add an AOT mode for the Triton backend (#98214)
Summary:
This is a copy of https://github.com/pytorch/pytorch/pull/97152 to make
the landing easier.

This PR implements a two-pass wrapper codegen for the Triton
backend to achieve ahead-of-time compilation. In the first pass, the
regular python wrapper code will be generated, and then the generated
code will be executed to perform Triton compilation and autotuning.
After that, the second pass wrapper codegen will generate C++ wrapper
with proper CUDA API to load and launch Triton-generated CUDA kernels.

Like the AOT mode for the cpp backend, the next step would be to provide
a more complete API for AOT.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/98214
Approved by: https://github.com/eellison

torch/_inductor/triton_heuristics.py
@@ -16,7 +16,8 @@ import torch
 from torch._dynamo.utils import dynamo_timed
 
 from . import config
-from .codecache import cache_dir
+from .codecache import cache_dir, cubin_cache_dir
+
 from .ir import ReductionHint, TileHint
 from .utils import (
     ceildiv,
@@ -137,6 +138,10 @@ class CachingAutotuner(KernelInterface):
         launcher.n_regs = getattr(binary, "n_regs", None)
         launcher.n_spills = getattr(binary, "n_spills", None)
         launcher.shared = getattr(binary, "shared", None)
+        if config.triton.store_cubin:
+            launcher.kernel_name = self.fn.__name__
+            launcher.bin = binary
+
         return launcher
 
     def bench(self, launcher, *args, grid):
@@ -184,12 +189,49 @@ class CachingAutotuner(KernelInterface):
         if self.save_cache_hook:
             self.save_cache_hook(self.launchers[0].config)
 
+    def save_cuda_kernel(self, grid, stream, launcher):
+        from .codegen.wrapper import KernelParamCache
+
+        # Make sure kernel_name is enough for distiguishing kernels
+        assert config.triton.unique_kernel_names
+
+        if callable(grid):
+            grid_x, grid_y, grid_z = grid(launcher.config.kwargs)
+        else:
+            grid_x, grid_y, grid_z = grid
+
+        kernel_name = launcher.kernel_name
+        cubin_path = os.path.join(cubin_cache_dir(), f"{kernel_name}.cubin")
+        with open(cubin_path, "wb") as f:
+            f.write(launcher.bin.asm["cubin"])
+
+        params = {
+            "mangled_name": launcher.bin.metadata["name"],
+            "grid_x": grid_x,
+            "grid_y": grid_y,
+            "grid_z": grid_z,
+            "num_warps": launcher.bin.num_warps,
+            "shared_mem": launcher.bin.shared,
+            "stream": stream,
+        }
+        with self.lock:
+            if KernelParamCache.cache.get(kernel_name, None):
+                assert (
+                    KernelParamCache.cache[kernel_name].get("mangled_name", None)
+                    == launcher.bin.metadata["name"]
+                )
+            else:
+                KernelParamCache.cache[kernel_name] = params
+
     def run(self, *args, grid, stream):
         if len(self.launchers) != 1:
             if len(self.launchers) == 0:
                 self.precompile()
             if len(self.launchers) > 1:
                 self.autotune_to_one_config(*args, grid=grid)
+
+        if config.triton.store_cubin:
+            self.save_cuda_kernel(grid, stream, self.launchers[0])
 
         (launcher,) = self.launchers
         if launcher.config.pre_hook is not None:

test/inductor/aot/cuda/test.cpp
@@ -5,6 +5,7 @@
 #include <torch/torch.h>
 
 extern std::vector<at::Tensor> aot_inductor_entry(const std::vector<at::Tensor>& args);
+
 /*
 class Net(torch.nn.Module):
     def __init__(self):
@@ -17,7 +18,7 @@ class Net(torch.nn.Module):
 */
 struct Net : torch::nn::Module {
   Net() {
-    weight = register_parameter("weight", torch::ones({32, 64}));
+    weight = register_parameter("weight", torch::ones({32, 64}, at::TensorOptions(at::kCUDA).dtype(at::ScalarType::Float)));
   }
   torch::Tensor forward(torch::Tensor input) {
     return torch::relu(input + weight);
@@ -26,12 +27,13 @@ struct Net : torch::nn::Module {
 };
 
 int main() {
-    torch::Tensor x = at::randn({32, 64});
+    torch::Tensor x = at::randn({32, 64}, at::dtype(at::kFloat).device(at::kCUDA));
     Net net;
     torch::Tensor results_ref = net.forward(x);
 
     // TODO: we need to provide an API to concatenate args and weights
     std::vector<torch::Tensor> inputs;
+
     for (const auto& pair : net.named_parameters()) {
       inputs.push_back(pair.value());
     }

test/inductor/aot/cuda/test.py
@@ -8,14 +8,15 @@ import torch._inductor
 class Net(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.weight = torch.ones(32, 64)
+        self.weight = torch.ones((32, 64), device="cuda")
 
     def forward(self, x):
         x = torch.relu(x + self.weight)
         return x
 
 
-inp = torch.randn((32, 64), device="cpu")
-module, _ = torch._dynamo.export(Net(), inp)
+inp = torch.randn((32, 64), device="cuda")
+
+module, _ = torch._dynamo.export(Net().cuda(), inp)
 lib_path = torch._inductor.aot_compile(module, [inp])
 shutil.copy(lib_path, "aot_inductor_output.so")

torch/_inductor/__init__.py
@@ -43,9 +43,15 @@ def aot_compile(
     Returns:
         Path to the generated shared library
     """
-    from .compile_fx import compile_fx
+    from .compile_fx import compile_fx_aot
 
-    return compile_fx(gm, example_inputs, config_patches=options, aot_mode=True)()
+    compiled = compile_fx_aot(
+        gm,
+        example_inputs,
+        config_patches=options,
+    )
+    lib_path = compiled()
+    return lib_path
 
 
 def list_mode_options(mode: str = None) -> Dict[str, Any]:

torch/_inductor/codecache.py
@@ -74,6 +74,13 @@ def cache_dir():
     return cache_dir
 
 
+@functools.lru_cache(None)
+def cubin_cache_dir():
+    cubin_dir = os.path.join(cache_dir(), "cubin")
+    os.makedirs(cubin_dir, exist_ok=True)
+    return cubin_dir
+
+
 class PersistentCache:
     def __init__(self):
         self.local_cache_path = os.path.join(cache_dir(), "local_cache")
@@ -447,8 +454,11 @@ def cpp_flags():
     return "-std=c++17 -Wno-unused-variable"
 
 
-def optimization_flags():
+def optimization_flags(cuda=False):
     base_flags = "-O3 -ffast-math -fno-finite-math-only"
+    if cuda:
+        return base_flags
+
     if sys.platform == "darwin":
         # Per https://mac.r-project.org/openmp/ right way to pass `openmp` flags to MacOS is via `-Xclang`
         # Also, `-march=native` is unrecognized option on M1
@@ -463,30 +473,38 @@ def use_custom_generated_macros():
 
 
 def get_include_and_linking_paths(
-    include_pytorch=False, vec_isa: VecISA = invalid_vec_isa
+    include_pytorch=False, vec_isa: VecISA = invalid_vec_isa, cuda=False
 ):
+    macros = ""
     if sys.platform == "linux" and (
         include_pytorch
         or vec_isa != invalid_vec_isa
+        or cuda
         or config.cpp.enable_kernel_profile
     ):
         # Note - We include pytorch only on linux right now. There is more work
         # to do to enable OMP build on darwin where PyTorch is built with IOMP
         # and we need a way to link to what PyTorch links.
-        ipaths = cpp_extension.include_paths() + [sysconfig.get_path("include")]
-        lpaths = cpp_extension.library_paths() + [sysconfig.get_config_var("LIBDIR")]
-        libs = ["c10", "torch", "torch_cpu", "torch_python", "gomp"]
-        macros = vec_isa.build_macro()
-        if macros:
-            macros = f"-D{macros}"
+        ipaths = cpp_extension.include_paths(cuda) + [sysconfig.get_path("include")]
+        lpaths = cpp_extension.library_paths(cuda) + [
+            sysconfig.get_config_var("LIBDIR")
+        ]
+        libs = ["c10", "torch", "torch_cpu", "torch_python"]
+        if cuda:
+            libs += ["c10_cuda", "cuda", "torch_cuda"]
+            ipaths += [f"{cpp_extension._TORCH_PATH}/../aten/src/"]
+        else:
+            libs += ["gomp"]
+            macros = vec_isa.build_macro()
+            if macros:
+                macros = f"-D{macros}"
     else:
         # Note - this is effectively a header only inclusion. Usage of some header files may result in
         # symbol not found, if those header files require a library.
         # For those cases, include the lpath and libs command as we do for pytorch above.
         # This approach allows us to only pay for what we use.
-        ipaths = cpp_extension.include_paths() + [sysconfig.get_path("include")]
+        ipaths = cpp_extension.include_paths(cuda) + [sysconfig.get_path("include")]
         lpaths = []
-        macros = ""
         if sys.platform == "darwin":
             # GNU OpenMP generally is not available on MacOS
             # There is either Intel OpenMP(for x86) or LLVM OpenMP (for both x86 and arm64)
@@ -517,20 +535,22 @@ def cpp_compile_command(
     shared=True,
     include_pytorch=False,
     vec_isa: VecISA = invalid_vec_isa,
+    cuda=False,
 ):
     ipaths, lpaths, libs, macros = get_include_and_linking_paths(
-        include_pytorch, vec_isa
+        include_pytorch, vec_isa, cuda
     )
 
     return re.sub(
         r"[ \n]+",
         " ",
         f"""
-            {cpp_compiler()} {input} {get_shared(shared)} {get_warning_all_flag(warning_all)} {cpp_flags()}
+            {cpp_compiler()} {input} {get_shared(shared)}
+            {get_warning_all_flag(warning_all)} {cpp_flags()}
             {ipaths} {lpaths} {libs} {macros}
-            {optimization_flags()}
+            {optimization_flags(cuda)}
             {use_custom_generated_macros()}
-            -o{output}
+            -o {output}
         """,
     ).strip()
 
@@ -540,15 +560,15 @@ class AotCodeCache:
     clear = staticmethod(cache.clear)
 
     @classmethod
-    def compile(cls, source_code):
-        from .codegen.wrapper import CppWrapperCodeGen
-
+    def compile(cls, source_code, cuda):
         # TODO: update cpp_compile_command for different platforms
-        picked_vec_isa = pick_vec_isa()
+        picked_vec_isa = invalid_vec_isa if cuda else pick_vec_isa()
         key, input_path = write(
             source_code,
             "cpp",
-            code_hash(repr(cpp_compile_command("i", "o", vec_isa=picked_vec_isa))),
+            code_hash(
+                repr(cpp_compile_command("i", "o", vec_isa=picked_vec_isa, cuda=cuda))
+            ),
         )
         if key not in cls.cache:
             from filelock import FileLock
@@ -556,21 +576,13 @@ class AotCodeCache:
             lock_dir = get_lock_dir()
             lock = FileLock(os.path.join(lock_dir, key + ".lock"), timeout=LOCK_TIMEOUT)
             with lock:
-                output_so = (
-                    os.path.join(os.getcwd(), f"{config.aot_codegen_output_prefix}.so")
-                    if config.aot_codegen_output_prefix
-                    else f"{input_path[:-3]}.so"
-                )
-
-                output_header = f"{output_so[:-3]}.h"
-                with open(output_header, "w") as header_file:
-                    header_file.writelines("#include <torch/torch.h>\n\n")
-                    header_file.writelines(f"{CppWrapperCodeGen.decl_str};\n")
-
-                log.info(f"AOT-Inductor compiles code into: {output_so}")
+                output_so = f"{input_path[:-4]}.so"
                 if not os.path.exists(output_so):
                     cmd = cpp_compile_command(
-                        input=input_path, output=output_so, vec_isa=picked_vec_isa
+                        input=input_path,
+                        output=output_so,
+                        vec_isa=picked_vec_isa,
+                        cuda=cuda,
                     ).split(" ")
                     try:
                         subprocess.check_output(cmd, stderr=subprocess.STDOUT)
@@ -578,6 +590,7 @@ class AotCodeCache:
                         raise exc.CppCompileError(cmd, e.output) from e
 
                 cls.cache[key] = output_so
+
         return cls.cache[key]
 
 

torch/_inductor/codegen/cpp.py
@@ -64,6 +64,11 @@ DTYPE_TO_ATEN = {
     torch.bfloat16: "at::ScalarType::BFloat16",
 }
 
+DEVICE_TO_ATEN = {
+    "cpu": "at::kCPU",
+    "cuda": "at::kCUDA",
+}
+
 INDEX_TYPE = "long"
 
 RTYPE_TO_CPP = {

commit 5da4128842da8b8049805d3c4e11d53b1350d007

[inductor] Fix a perf regression caused by https://github.com/pytorch/pytorch/pull/98214
Summary: See the description in https://github.com/pytorch/pytorch/issues/98342

[ghstack-poisoned]

torch/_inductor/triton_heuristics.py
@@ -138,7 +138,9 @@ class CachingAutotuner(KernelInterface):
         launcher.n_regs = getattr(binary, "n_regs", None)
         launcher.n_spills = getattr(binary, "n_spills", None)
         launcher.shared = getattr(binary, "shared", None)
-        if config.triton.store_cubin:
+        launcher.store_cubin = config.triton.store_cubin
+        # store this global varible to avoid the high overhead of reading it when calling run
+        if launcher.store_cubin:
             launcher.kernel_name = self.fn.__name__
             launcher.bin = binary
 
@@ -230,10 +232,10 @@ class CachingAutotuner(KernelInterface):
             if len(self.launchers) > 1:
                 self.autotune_to_one_config(*args, grid=grid)
 
-        if config.triton.store_cubin:
+        (launcher,) = self.launchers
+        if launcher.store_cubin:
             self.save_cuda_kernel(grid, stream, self.launchers[0])
 
-        (launcher,) = self.launchers
         if launcher.config.pre_hook is not None:
             launcher.config.pre_hook(
                 {**zip(self.arg_names, args), **launcher.config.kwargs}