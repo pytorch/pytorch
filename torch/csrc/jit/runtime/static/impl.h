1ebb78528f7c38bed989a0e387b6bf404cb5867a
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2023-04-04 07:47:39 +0000

    fix the use of incomplete vector<T> for C++20 compatibilities (#93978)
    Avoid referring to std::vector<T> members and constructor/desctructors when T is incomplete.
    
    Referring to incomplete members is [not legal](https://timsong-cpp.github.io/cppwp/n4868/vector#overview-4) according to the C++ standard.
    
    Non-noexcept constructors need access to members' destructors. As of C++20, std::vector's destructor is constexpr and so forcefully requires a complete type for the vector's elements.
    
    These issues cause build errors in newer toolchains under c++20 mode.
    
    Fix them by moving code that needs complete types to a different place where the type is already defined.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/93978
    Approved by: https://github.com/Skylion007

--- torch/csrc/jit/runtime/static/impl.h
+++ torch/csrc/jit/runtime/static/impl.h
@@ -272,8 +272,7 @@ using SROperator = std::function<void(ProcessedNode*)>;
 //   planner.
 class BlockInfo {
  public:
-  BlockInfo(uint32_t input_idx, Block& block)
-      : input_idx_(input_idx), block_(block) {}
+  BlockInfo(uint32_t input_idx, Block& block);
 
   void set_nodes(
       std::vector<StaticNodeInfo> nodes,
@@ -283,9 +282,7 @@ class BlockInfo {
     return nodes_;
   }
 
-  size_t num_nodes() const {
-    return nodes_.size();
-  }
+  size_t num_nodes() const;
 
   size_t num_inputs() const {
     return block_.inputs().size();
@@ -851,6 +848,10 @@ class TORCH_API StaticNodeInfo {
   ProcessedNodeInputs inputs_;
   uint16_t outputs_offset_;
 };
+
+inline size_t BlockInfo::num_nodes() const {
+  return nodes_.size();
+}
 
 /*
   ProcessedNodeMetadata class wraps the possible metadata
