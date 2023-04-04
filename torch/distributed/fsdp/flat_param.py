0898c617c4ad0a0f9d01559bb4a35109cd28dba4
Andrew Gu <andgu@fb.com>
2023-04-04 13:44:13 +0000

    Update on "[FSDP] Add skip writeback check gated by env var"
    This adds the option to skip the `_writeback_orig_params()` function that checks for parameter and gradient writeback in case storages changed, gated by an env var `FSDP_SKIP_WRITEBACK_CHECK`.
    
    As described in the code comment, this writeback check is important for detecting a failure mode of FSDP `use_orig_params=True`. However, because the failure mode is an atypical case and performing the check incurs nontrivial CPU overhead, we add this option to skip the check altogether.
    
    <details>
    <summary>(Before) Pre-backward hook: 1.044 ms</summary>
    
    ![Screenshot 2023-04-04 at 9 05 53 AM](https://user-images.githubusercontent.com/31054793/229800917-9580ce6b-3721-469a-9212-f0cbfd8cbb52.png)
    
    </details>
    
    <details>
    <summary>(After) Pre-backward hook: 0.500 ms</summary>
    
    ![Screenshot 2023-04-04 at 9 34 57 AM](https://user-images.githubusercontent.com/31054793/229810916-b16295d5-7da7-42c4-9168-04edeebe045c.png)
    
    </details>
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/flat_param.py
+++ torch/distributed/fsdp/flat_param.py
@@ -76,12 +76,11 @@ or a submodule chosen by the provided wrapping policy.
 # checks in the overrides, we may use 'unsafe'.
 _FSDP_USE_UNSAFE_SETATTR = "FSDP_USE_UNSAFE_SETATTR"
 # Environment variable toggling whether to check for parameter/gradient
-# writeback (i.e. for the user changing any FSDP-managed module's parameters
-# after FSDP initialization, whether the storage or the variable itself)
+# writeback in case their storages change after FSDP initialization
 # We should check by default since it prevents silent correctness errors, but
 # since such changes are atypical, we may want to skip the check to save CPU
-# overhead, especially since the check happens pre-forward and pre-backward
-# each iteration.
+# overhead, especially since the check happens in the pre-forward and
+# pre-backward each iteration.
 _FSDP_SKIP_WRITEBACK_CHECK = "FSDP_SKIP_WRITEBACK_CHECK"
 
 
