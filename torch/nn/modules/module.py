bf5478accc5ed914da214c096d10995d26125fbe
Mark Saroufim <marksaroufim@gmail.com>
2023-04-05 18:25:20 +0000

    fix docstring

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -2441,19 +2441,11 @@ class Module:
 
     def compile(self, *args, **kwargs):
         """
-        Compiles the module's forward function with the given arguments and
-        keyword arguments in place.
-        The compiled function is stored in the ``_compiled_call_impl``
-        attribute. The compiled function can be called with the same arguments
-        and keyword arguments as the original module's forward function.
-        The primary benefit of this API is it allows you to easily
-        ``torch.save()`` and `torch.load()` an ``OptimizedModule``
-
-        Args:
-            *args: Arguments to ``torch.compile`` the module's forward function with.
-            **kwargs: Keyword arguments to ``torch.compile`` the module's forward function with.
-
-        Returns:
-            Module: self
+        Compile this Module's forward using :func:`torch.compile`.
+       
+        This Module's `__call__` method is compiled and all arguments are passed as-is
+        to :func:`torch.compile`.
+       
+        See :func:`torch.compile` for details on the arguments for this function.
         """
         self._compiled_call_impl = torch.compile(self._call_impl, *args, **kwargs)
