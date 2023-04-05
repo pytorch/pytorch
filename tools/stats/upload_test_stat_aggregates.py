27684c99549ed01c437dac6b310cf5cc44f83a52
PaliC <sahancpal@gmail.com>
2023-04-04 19:11:44 -0700

    Update on "Change daily aggregates upload job to use sum and occurence counter instead of averages"
    We used to keep track of the average of stats, however, when we munge the data to find interesting insights this makes things difficult (ie. finding total test time for an oncall). The pin is updated such that we keep track of the sum instead as well as an "occurrences" field such that the average can be rederived from sum/occurrences. 
    
    
    [ghstack-poisoned]

--- tools/stats/upload_test_stat_aggregates.py
+++ tools/stats/upload_test_stat_aggregates.py
@@ -52,7 +52,7 @@ def get_test_stat_aggregates(date: datetime.date) -> Any:
     ]
     api_response = rs.QueryLambdas.execute_query_lambda(
         query_lambda=lambda_function_name,
-        version="71062190c39485ac",
+        version="d1e2d7f3f3ef2e5e",
         parameters=query_parameters,
     )
     for i in range(len(api_response["results"])):
