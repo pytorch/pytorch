4842d90925980f235dd679fc8f698a2530234553
Will Constable <whc@fb.com>
2023-04-05 01:00:34 +0000

    Skip nnmodule hook guards by default
    - the usual case is to not use hooks, so avoid guard overhead
    - registering any hook before compile will trigger a warning about hook support
    - registering a hook later (or removing one) requires user knowledge and opting in,
      currently this isn't warnable (but maybe we can observe compiled nnmodules to make it
      warnable).
    
    Why skip hook guards by default instead of not tracing __call__/hooks by default?
    - avoid having a mode flag that alters dynamo tracing behavior (harder to test both codepaths
      in CI with full coverage)
    - the most basic hook usecase (registering a hook before compile, and never removing it)
      will work by default with this PR, while it would require enablement and incur overhead
      in the 'not tracing __call__' proposal.
    
    [ghstack-poisoned]

--- test/dynamo/test_modules.py
+++ test/dynamo/test_modules.py
@@ -1195,6 +1195,7 @@ class OptimizedModuleTest(torch._dynamo.test_case.TestCase):
             )
         )
 
+    @patch.object(torch._dynamo.config, "skip_nnmodule_hook_guards", False)
     def test_hooks_outer(self):
         class TestModule(torch.nn.Module):
             def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -1241,6 +1242,7 @@ class OptimizedModuleTest(torch._dynamo.test_case.TestCase):
             the eval_frame entrypoint to Module.__call__?
         """
 
+    @patch.object(torch._dynamo.config, "skip_nnmodule_hook_guards", False)
     def test_hooks_inner(self):
         class TestModule(torch.nn.Module):
             def forward(self, x: torch.Tensor) -> torch.Tensor:
