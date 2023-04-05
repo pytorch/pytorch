a891ec90d7cf51cd45371d11c3c38269d6a94d49
Angela Yi <yiangela7@gmail.com>
2023-04-05 22:42:51 +0000

    clown

--- torch/fx/passes/utils/matcher_utils.py
+++ torch/fx/passes/utils/matcher_utils.py
@@ -216,7 +216,6 @@ class SubgraphMatcher:
                 return False
 
             for a1, a2 in zip(args1, args2):
-                print(a1, a2)
                 if isinstance(a1, Node) and isinstance(a2, Node):
                     matched = self._match_nodes(a1, a2, match)
                 elif isinstance(a1, (list, tuple)) and isinstance(a2, (list, tuple)):
@@ -224,7 +223,6 @@ class SubgraphMatcher:
                 else:
                     matched = self.ignore_literals or self._match_literals(a1, a2, match)
 
-                breakpoint()
                 if not matched:
                     return False
 
