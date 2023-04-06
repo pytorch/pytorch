204df7048dbc16bd2eba86195ce04426a923bb20
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-04-06 11:21:52 -0700

    move has_magma smoke test to ROCM only

--- .ci/pytorch/win-test-helpers/run_python_nn_smoketests.py
+++ .ci/pytorch/win-test-helpers/run_python_nn_smoketests.py
@@ -20,12 +20,15 @@ GPU_TESTS = [
         "import torch; torch.randn([3,5]).cuda()",
     ),
     (
-        "Checking that magma is available",
-        "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)",
-    ),
-    (
         "Checking that CuDNN is available",
         "import torch; exit(0 if torch.backends.cudnn.is_available() else 1)",
+    ),
+]
+
+ROCM_GPU_MAGMA_TESTS = [
+    (
+        "Checking that magma is available",
+        "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)",
     ),
 ]
 
@@ -34,6 +37,8 @@ if __name__ == "__main__":
 
     if 'USE_CUDA' in os.environ and os.environ['USE_CUDA'] == '1':
         TESTS = COMMON_TESTS + GPU_TESTS
+        if 'USE_ROCM' in os.environ and os.environ['USE_ROCM'] == '1':
+            TESTS += ROCM_GPU_MAGMA_TESTS
     else:
         TESTS = COMMON_TESTS
     for description, python_commands in TESTS:
