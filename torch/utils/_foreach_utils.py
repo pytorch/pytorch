b2206071f0f7e848a13020a4ad3857cc1ab63447
Masaki Kozuki <mkozuki@nvidia.com>
2023-04-05 22:13:53 -0700

    fix type hint
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- torch/utils/_foreach_utils.py
+++ torch/utils/_foreach_utils.py
@@ -48,5 +48,5 @@ def _has_foreach_support(tensors: List[Tensor], device: torch.device) -> bool:
 def _group_tensors_by_device_and_dtype(
     tensorlistlist: List[List[Optional[Tensor]]],
     with_indices: bool = False,
-) -> Dict[Tuple[torch.device, torch.dtype], Tuple[List[Tensor], List[int]]]:
+) -> Dict[Tuple[torch.device, torch.dtype], Tuple[List[List[Optional[Tensor]]], List[int]]]:
     return torch._C._group_tensors_by_device_and_dtype(tensorlistlist, with_indices)
