506e9288b2f8406da2e2dde5a6b19a2a66d38e1e
Huy Do <huydhn@gmail.com>
2023-04-07 13:00:15 -0700

    Skip test_conv2d_binary_dynamic_shapes on CPU

--- test/inductor/test_torchinductor_codegen_dynamic_shapes.py
+++ test/inductor/test_torchinductor_codegen_dynamic_shapes.py
@@ -157,6 +157,7 @@ test_failures = {
     "test_views4_dynamic_shapes": TestFailure(("cpu",)),
     "test_zeros_dynamic_shapes": TestFailure(("cpu",)),
     "test_upsample_cat_conv_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
+    "test_conv2d_binary_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
     #
     # Failed to find for loop/triton kernel:
     #
