67f49b36bbbb96211baae2b7644e93eb18c64837
Will Constable <whc@fb.com>
2023-04-07 21:50:55 +0000

    Update on "Add dynamo train_step_compiler"
    Support compiling a user-provided train_step function, provided
    - it includes exactly one call to .backward()
    - it includes any relevant optimizer steps, followed by zero_grad() calls
    
    cc soumith voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- test/dynamo/test_train_step.py
+++ test/dynamo/test_train_step.py
@@ -12,25 +12,15 @@ from torch._dynamo.testing import same
 class Seq(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer = torch.nn.Linear(2, 3)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(10, 10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10, 10),
+            torch.nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        return self.layer(x)
-
-
-# class Seq(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.layers = torch.nn.Sequential(
-#             torch.nn.Linear(10, 10),
-#             torch.nn.ReLU(),
-#             torch.nn.Linear(10, 10),
-#             torch.nn.Sigmoid(),
-#         )
-
-
-#     def forward(self, x):
-#         return self.layers(x)
+        return self.layers(x)
 
 
 def init_weights(m):
@@ -39,7 +29,22 @@ def init_weights(m):
         m.bias.data.fill_(0.01)
 
 
+
 class TestCompileTrainStep(torch._dynamo.test_case.TestCase):
+    """
+    The Basic Idea
+    1) dynamo stashes optimizer on graphmodule in special key
+    2) optimizer.step() just sticks a call in graph without trying to arg-proxy anything
+    3) inside train_step_compiler, we reparameterize the optimizer
+
+    WIP/Issues
+    - handle an optimizer with actual states
+    - put zero-grad in the graph
+    - dynamo asserts empty backward tape before trace
+    - train_step backend asserts full_graph mode was used
+    - handle more than one optimizer (e.g. for different submodules)
+    """
+
     def test_no_optimizer(self):
         def train_step(model, inputs):
             out = model(*inputs)
@@ -61,36 +66,29 @@ class TestCompileTrainStep(torch._dynamo.test_case.TestCase):
     def test_sgd_optimizer(self):
         model = Seq()
         model.apply(init_weights)
-        print(f"orig_params {model.layer.weight}")
 
         optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
         def train_step(model, optimizer, inputs):
             out = model(*inputs)
             loss = out.sum()
+
+            # dynamo special case lets this pass through as a call in the FX graph
+            # it gets traced out in the train_step backend, before being functionalized
             loss.backward()
+
+            # dynamo tracks the optimizer and smuggles it on the graphmodule as an attr
+            # train_step backend can reparametrize it with the fake parameter tensors it already
+            # uses for module tracing
             optimizer.step()
+
             # model.zero_grad()
             return loss
 
-        """
-        The plan
-        1) dynamo stashes optimizer on graphmodule in special key
-        2) optimizer.step() just sticks a call in graph without trying to arg-proxy anything
-        3) inside train_step_compiler, we reparameterize the optimizer
-
-        What i tried first:
-        - make a local function on user side that wraps calling optimizer.step() also using no_grad(), trace it
-        Why it failed:
-        - the optimizer states need to be reparametrized to faketensors inside train_step_compiler since that's
-          where the fake mode exists but the optimizer object is not easily accessible as currently wrapped up
-          in that call_opt_step thing
-
-        """
         # copy the model/optimizer up front so we don't have to reset them between eager/compile runs
         opt_model = deepcopy(model)
         opt_optimizer = deepcopy(optimizer)
-        inputs = [torch.randn((128, 2))]
+        inputs = [torch.randn((128, 10))]
 
         correct_loss = train_step(model, optimizer, inputs)
         correct_params = {
@@ -108,8 +106,6 @@ class TestCompileTrainStep(torch._dynamo.test_case.TestCase):
         self.assertTrue(same(correct_loss, opt_loss))
         for name in correct_params:
             self.assertTrue(name in opt_params)
-            print(f"correct_params \n{correct_params[name]}")
-            print(f"opt_params \n{opt_params[name]}")
             self.assertTrue(same(correct_params[name], opt_params[name]))
 
     def test_smoke(self):
@@ -128,7 +124,7 @@ class TestCompileTrainStep(torch._dynamo.test_case.TestCase):
         opt_model.apply(init_weights)
         opt_optimizer = torch.optim.SGD(opt_model.parameters(), lr=0.01, momentum=0.9)
         inputs = [torch.randn((128, 10))]
-        opt_train_step = torch.compile(train_step, backend="train_step_eager")
+        opt_train_step = torch.compile(train_step, backend="train_step_eager", fullgraph=True)
         opt_loss = opt_train_step(opt_model, opt_optimizer, inputs)
 
 
