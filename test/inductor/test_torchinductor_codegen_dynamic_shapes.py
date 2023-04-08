f5063b47b71e2619bf1e2579583b526dcd3c2c36
Huy Do <huydhn@gmail.com>
2023-04-07 17:48:02 -0700

    Adding 2 more tests

--- test/inductor/test_torchinductor_codegen_dynamic_shapes.py
+++ test/inductor/test_torchinductor_codegen_dynamic_shapes.py
@@ -159,6 +159,8 @@ test_failures = {
     "test_upsample_cat_conv_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
     "test_conv2d_binary_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
     "test_conv2d_unary_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
+    "test_conv_bn_fuse_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
+    "test_conv_transpose2d_unary_dynamic_shapes": TestFailure(("cpu",), is_skip=True),
     #
     # Failed to find for loop/triton kernel:
     #
