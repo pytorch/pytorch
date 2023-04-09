53543e46d7ab649912c3a721835ce59feadffae7
mikey dagitses <mikeyd@meta.com>
2023-04-09 16:03:27 -0700

    use exec for sed command in cmake_configure_file
    This simplifies the implementation quite a bit by dropping unnecessary
    quoting and other things.
    
    Differential Revision: [D44823439](https://our.internmc.facebook.com/intern/diff/D44823439/)
    
    [ghstack-poisoned]

--- c10/macros/cmake_configure_file/tool.py
+++ c10/macros/cmake_configure_file/tool.py
@@ -5,38 +5,28 @@ import subprocess
 import sys
 
 def main(argv: list[str]) -> None:
-    template_path = sys.argv[1]
+    template_path = pathlib.Path(sys.argv[1])
     output_path = pathlib.Path(sys.argv[2])
     definitions = list(sys.argv[3:])
-    subprocess.run(" ".join(cmake_configure_file_cmd(template_path, definitions, output_path)),
-                   check=True, shell=True)
+    cmake_configure_file(template_path, definitions, output_path)
 
-def cmake_configure_file_cmd(template_path: str, definitions: list[str], output_path: pathlib.Path):
+def cmake_configure_file(template_path: pathlib.Path, definitions: list[str], output_path: pathlib.Path):
     command = ["sed", "--regexp-extended"]
     for definition in definitions:
         command.append(
-            # Note that because of the shell redirection at the end of
-            # the command, we must return arguments that are parseable
-            # by a shell, hence the quoting here.
-            "--expr='s@#cmakedefine {}@#define {}@'".format(
+            "--expr=s@#cmakedefine {}@#define {}@".format(
                 definition,
                 definition,
             ),
         )
 
     # Replace any that remain with /* #undef FOO */.
-    command.append("--expr='s@#cmakedefine ([A-Z0-9_]+)@/* #undef \\1 */@'")
+    command.append("--expr=s@#cmakedefine ([A-Z0-9_]+)@/* #undef \\1 */@")
 
     # The input is the final argument to sed.
     command.append(template_path)
 
-    # You can not specify the output to sed, so file redirection is
-    # our only option. This means that even though we're returning a
-    # list, we have to join it into a string and have a shell evaluate
-    # it.
-    command.append("> {}".format(output_path))
-
-    return command
+    subprocess.run(command, check=True, stdout=output_path.open("x"))
 
 
 if __name__ == "__main__":
