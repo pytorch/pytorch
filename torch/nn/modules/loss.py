8a426aaa7538ca784146993aa2d499d68bb0449f
Yashika Jain <yashikajain201@gmail.com>
2023-04-11 18:50:09 +0530

    Minor modifications

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -1175,7 +1175,7 @@ class CrossEntropyLoss(_WeightedLoss):
 
     def __init__(self, weight: Optional[Tensor] = None, size_average=None, ignore_index: int = -100,
                  reduce=None, reduction: str = 'mean', label_smoothing: float = 0.0, dim: Optional[int] = None) -> None:
-        super().__init__(weight, size_average, reduce, reduction)
+        super().__init__(weight, size_average, reduce, reduction, dim)
         self.ignore_index = ignore_index
         self.label_smoothing = label_smoothing
 
