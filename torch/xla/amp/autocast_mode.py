3b9c31570b5dbb3ff2c8de8d77e6b9f84b7cd92c
Meghan <cowanmeg@google.com>
2023-04-12 17:30:20 +0000

    small fix

--- torch/xla/amp/autocast_mode.py
+++ torch/xla/amp/autocast_mode.py
@@ -9,25 +9,4 @@ class autocast(torch.amp.autocast_mode.autocast):
     ``torch.xla.amp.autocast(args...)`` is equivalent to ``torch.autocast("xla", args...)``
     """
     def __init__(self, enabled : bool = True, dtype : torch.dtype = torch.bfloat16, cache_enabled : bool = True):
-        if torch._jit_internal.is_scripting():
-            self._enabled = enabled
-            self.device = "xla"
-            self.fast_dtype = dtype
-            return
         super().__init__("xla", enabled=enabled, dtype=dtype, cache_enabled=cache_enabled)
-
-    def __enter__(self):
-        if torch._jit_internal.is_scripting():
-            return self
-        return super().__enter__()
-
-    # TODO: discuss a unified TorchScript-friendly API for autocast
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any):  # type: ignore[override]
-        if torch._jit_internal.is_scripting():
-            return
-        return super().__exit__(exc_type, exc_val, exc_tb)
-
-    def __call__(self, func):
-        if torch._jit_internal.is_scripting():
-            return func
-        return super().__call__(func)
