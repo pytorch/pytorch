4f4a8c849df0605d5b7b0a48e72b4611d3a327bc
Chien-Chin Huang <chienchin@fb.com>
2023-04-11 23:06:12 -0700

    [FSDP] Include duplicate parameters and modules when calling named_parameters and named_modules
    The default option of `named_parameters` and `named_modules` is to remove the duplicated parameters and modules. However, in FSDP, we need to know what parameters are shared. As a result, setting `remove_duplicate` to False is required in FSDP. Without setting `remove_duplicate` to False, FSDP won't be able to discover shared weights in some cases (e.g., the shared weights are in the same module or there are shared modules).
    
    Differential Revision: [D44897935](https://our.internmc.facebook.com/intern/diff/D44897935/)
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/flat_param.py
+++ torch/distributed/fsdp/flat_param.py
@@ -522,8 +522,8 @@ class FlatParamHandle:
         param_extensions: List[Any] = []
         is_padding_mask: List[bool] = []
         total_numel = total_numel_without_padding = 0
-        for submodule_name, submodule in module.named_modules():
-            for param_name, param in submodule.named_parameters(recurse=False):
+        for submodule_name, submodule in module.named_modules(remove_duplicate=False):
+            for param_name, param in submodule.named_parameters(recurse=False, remove_duplicate=False):
                 if param not in params_set:
                     continue
                 if param in shared_param_memo:  # shared reference
