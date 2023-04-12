9262ac148428e3c7f938bb754ca581ade6ec609a
PaliC <sahancpal@gmail.com>
2023-04-12 14:56:16 -0700

    Update on "remove merging label when merge is cancelled"
    Adds a script to get rid of the "merging" label when a job is cancelled. 
    At the moment this can create a race condition is someone cancels a job and starts a new one, though these cases should be pretty rare especially in cases where its from a new merge command.
    
    [ghstack-poisoned]

--- .github/scripts/on_cancel_merge.py
+++ .github/scripts/on_cancel_merge.py
@@ -1,24 +1,31 @@
 import argparse
+from typing import Any
 
-from scripts.gitutils import GitRepo, get_git_remote_name, get_git_repo_dir
-from scripts.trymerge import MERGE_IN_PROGRESS_LABEL, GitHubPR
+from gitutils import get_git_remote_name, get_git_repo_dir, GitRepo
+from trymerge import GitHubPR, MERGE_IN_PROGRESS_LABEL
 
 
-def parse_args():
+def parse_args() -> Any:
     parser = argparse.ArgumentParser(
-        description="Perform actions when a merge workflow is cancelled")
+        description="Perform actions when a merge workflow is cancelled"
+    )
     parser.add_argument(
         "--pr-num",
         type=int,
         required=True,
-        help="The PR number to cancel the merge for")
+        help="The PR number to cancel the merge for",
+    )
     return parser.parse_args()
 
 
-def main():
+def main() -> None:
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name(), debug=True)
     org, project = repo.gh_owner_and_name()
     pr_num = args.pr_num
 
     GitHubPR(org, project, pr_num).remove_label(MERGE_IN_PROGRESS_LABEL)
+
+
+if __name__ == "__main__":
+    main()
