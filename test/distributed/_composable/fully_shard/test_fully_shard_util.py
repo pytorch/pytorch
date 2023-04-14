ed0c6794f12eb9025ba4e714033baf93ff47be42
Yanli Zhao <yanlizhao@fb.com>
2023-04-14 07:30:07 +0000

    Update on "[Composable API] Add two helper functions for fully_shard"
    Add two helper functions for 'fully_shard', it will help validating whether sharding is expected and validating parameters managed by fully_shard.
    
    
    
    
    [ghstack-poisoned]

--- test/distributed/_composable/fully_shard/test_fully_shard_util.py
+++ test/distributed/_composable/fully_shard/test_fully_shard_util.py
@@ -6,15 +6,12 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 from torch.distributed._composable import fully_shard
-from torch.distributed.fsdp.wrap import ModuleWrapPolicy
-from torch.testing._internal.common_dist_composable import (
-    CompositeModel,
-    UnitModule,
-)
 from torch.distributed.fsdp._common_utils import (
     _get_fully_sharded_submodule_names,
     _get_managed_param_names,
 )
+from torch.distributed.fsdp.wrap import ModuleWrapPolicy
+from torch.testing._internal.common_dist_composable import CompositeModel, UnitModule
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_fsdp import FSDPTest
 from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
@@ -29,7 +26,6 @@ if TEST_WITH_DEV_DBG_ASAN:
         file=sys.stderr,
     )
     sys.exit(0)
-
 
 
 class TestUtils(FSDPTest):
@@ -49,7 +45,9 @@ class TestUtils(FSDPTest):
             policy=ModuleWrapPolicy({UnitModule}),
         )
         sharded_submodule_names = _get_fully_sharded_submodule_names(model)
-        self.assertEqual(sharded_submodule_names, ['UnitModule', 'UnitModule', 'CompositeModel'])
+        self.assertEqual(
+            sharded_submodule_names, ["UnitModule", "UnitModule", "CompositeModel"]
+        )
 
     @skip_if_lt_x_gpu(2)
     def test_get_managed_param_names(self):
@@ -59,11 +57,29 @@ class TestUtils(FSDPTest):
             policy=ModuleWrapPolicy({UnitModule}),
         )
         managed_param_names = _get_managed_param_names(model)
-        self.assertEqual(managed_param_names, [
-            ['l1.weight', 'l1.bias', 'l2.weight', 'l2.bias'], 
-            ['u1.l1.weight', 'u1.l1.bias', 'u1.seq.1.weight', 'u1.seq.1.bias', 'u1.l2.weight', 'u1.l2.bias'], 
-            ['u2.l1.weight', 'u2.l1.bias', 'u2.seq.1.weight', 'u2.seq.1.bias', 'u2.l2.weight', 'u2.l2.bias']
-        ])
+        self.assertEqual(
+            managed_param_names,
+            [
+                ["l1.weight", "l1.bias", "l2.weight", "l2.bias"],
+                [
+                    "u1.l1.weight",
+                    "u1.l1.bias",
+                    "u1.seq.1.weight",
+                    "u1.seq.1.bias",
+                    "u1.l2.weight",
+                    "u1.l2.bias",
+                ],
+                [
+                    "u2.l1.weight",
+                    "u2.l1.bias",
+                    "u2.seq.1.weight",
+                    "u2.seq.1.bias",
+                    "u2.l2.weight",
+                    "u2.l2.bias",
+                ],
+            ],
+        )
+
 
 if __name__ == "__main__":
     run_tests()
