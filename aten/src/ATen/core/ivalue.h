804fd5594606a7bbfea09bc8ecced47f9f51e838
Kimish Patel <kimishpatel@fb.com>
2023-04-14 07:34:33 -0700

    Update base for Update on "[PT2E][Quant][BE] Refactor observer code"
    Combine per channel and per tensor observer code
    
    Differential Revision: [D44918494](https://our.internmc.facebook.com/intern/diff/D44918494/)
    
    [ghstack-poisoned]

--- aten/src/ATen/core/ivalue.h
+++ aten/src/ATen/core/ivalue.h
@@ -8,7 +8,6 @@
 #include <ATen/core/jit_type_base.h>
 #include <ATen/core/type_factory.h>
 #include <c10/core/SymFloat.h>
-#include <c10/core/SymBool.h>
 #include <c10/macros/Export.h>
 #include <c10/util/C++17.h>
 #include <c10/util/MaybeOwned.h>
@@ -163,7 +162,6 @@ struct Capsule {
   _(Int)                     \
   _(SymInt)                  \
   _(SymFloat)                \
-  _(SymBool)                 \
   _(Bool)                    \
   _(Tuple)                   \
   _(String)                  \
@@ -617,23 +615,6 @@ public:
   c10::SymFloat toSymFloat() &&;
   c10::SymFloat toSymFloat() const&;
 
-  IValue(c10::SymBool i) {
-    if (i.is_symbolic()) {
-      tag = Tag::SymBool;
-      payload.u.as_intrusive_ptr = i.toSymNodeImpl().release();
-    } else {
-      tag = Tag::Bool;
-      payload.u.as_bool = i.as_bool_unchecked();
-    }
-  }
-
-  bool isSymBool() const {
-    return Tag::SymBool == tag;
-  }
-
-  c10::SymBool toSymBool() &&;
-  c10::SymBool toSymBool() const&;
-
   // allow you to pass literals (3, 4) without ambiguity
   IValue(int32_t i) : IValue(static_cast<int64_t>(i)) {}
 
@@ -859,9 +840,6 @@ public:
     } else if (s.isSymFloat()) {
       tag = Tag::SymFloat;
       payload.u.as_intrusive_ptr = s.toSymFloat().toSymNodeImpl().release();
-    } else if (s.isSymBool()) {
-      tag = Tag::SymBool;
-      payload.u.as_intrusive_ptr = s.toSymBool().toSymNodeImpl().release();
     } else if (s.isFloatingPoint()) {
       tag = Tag::Double;
       payload.u.as_double = s.toDouble();
@@ -878,7 +856,7 @@ public:
   }
 
   bool isScalar() const {
-    return isDouble() || isInt() || isComplexDouble() || isBool() || isSymInt() || isSymFloat() || isSymBool();
+    return isDouble() || isInt() || isComplexDouble() || isBool() || isSymInt() || isSymFloat();
   }
 
   at::Scalar toScalar() const {
@@ -894,8 +872,6 @@ public:
       return toSymInt();
     else if (isSymFloat())
       return toSymFloat();
-    else if (isSymBool())
-      return toSymBool();
     throw std::runtime_error("IValue is not a Scalar");
   }
 
@@ -1194,8 +1170,6 @@ public:
       case Tag::SymInt:
         return true;
       case Tag::SymFloat:
-        return true;
-      case Tag::SymBool:
         return true;
       case Tag::Bool:
         return false;
