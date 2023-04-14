651c1be885286afcef636834d85dedb2a40280fe
andrewor14 <andrewor14@gmail.com>
2023-04-14 08:22:53 -0700

    Recompute flat_arg_fake_tensors after fakification (#98769)
    Summary: This fixes the case when some of the input tensors were
    real tensors and fakified in `validate_and_convert_non_fake_tensors`,
    but `flat_arg_fake_tensors` would not contain all the inputs
    because it was computed before the fakification. We fix this by
    recomputing `flat_arg_fake_tensors` after fakification as well.
    
    Test Plan:
    python test/dynamo/test_export.py ExportTests.test_mixed_real_and_fake_inputs
    
    Reviewers: Chillee, voznesenskym
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98769
    Approved by: https://github.com/voznesenskym

--- test/test_fake_tensor.py
+++ test/test_fake_tensor.py
@@ -499,6 +499,34 @@ class FakeTensorTest(TestCase):
         with patch.object(torch._functorch.config, "fake_tensor_allow_meta", False):
             self.assertRaises(Exception, run_meta)
 
+    def test_mixed_real_and_fake_inputs(self):
+        class _TestPattern(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.conv = torch.nn.Conv2d(1, 1, 1)
+                self.bn = torch.nn.BatchNorm2d(1)
+
+            def forward(self, input):
+                running_std = torch.sqrt(self.bn.running_var + self.bn.eps)
+                scale_factor = self.bn.weight / running_std
+                weight_shape = [1] * len(self.conv.weight.shape)
+                weight_shape[0] = -1
+                bias_shape = [1] * len(self.conv.weight.shape)
+                bias_shape[1] = -1
+                scaled_weight = self.conv.weight * scale_factor.reshape(weight_shape)
+                zero_bias = torch.zeros_like(self.conv.bias, dtype=input.dtype)
+                conv = self.conv._conv_forward(input, scaled_weight, zero_bias)
+                conv_orig = conv / scale_factor.reshape(bias_shape)
+                conv_orig = conv_orig + self.conv.bias.reshape(bias_shape)
+                conv = self.bn(conv_orig)
+                return conv
+
+        example_inputs = (torch.randn(1, 1, 3, 3),)
+        mod = _TestPattern()
+        with FakeTensorMode(allow_non_fake_inputs=True):
+            out = mod(torch.randn(1, 1, 3, 3))
+        self.checkType(out, "cpu", (1, 1, 3, 3))
+
 
 class FakeTensorConstHandling(TestCase):
     def assertConst(self, *args):
