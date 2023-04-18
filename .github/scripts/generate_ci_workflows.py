f828ef09bcf0b002f000d990f2842d54218c5a8f
Nikita Shulga <nshulga@meta.com>
2023-04-18 15:17:21 -0700

    Add CUDA-12.1 manywheels build to trunk
    As CUDA-11.7 is getting deprecated anyway

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -146,15 +146,8 @@ LINUX_BINARY_SMOKE_WORKFLOWS = [
         os=OperatingSystem.LINUX,
         package_type="manywheel",
         build_configs=generate_binary_build_matrix.generate_wheels_matrix(
-            OperatingSystem.LINUX, arches=["11.8"], python_versions=["3.8"]
-        ),
-        branches="main",
-    ),
-    BinaryBuildWorkflow(
-        os=OperatingSystem.LINUX,
-        package_type="manywheel",
-        build_configs=generate_binary_build_matrix.generate_wheels_matrix(
-            OperatingSystem.LINUX, arches=["11.7"], python_versions=["3.8"]
+            OperatingSystem.LINUX, arches=["11.7", "12.1"], python_versions=["3.8"],
+            gen_special_an_non_special_wheel = False,
         ),
         branches="main",
     ),
