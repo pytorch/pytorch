768b211e231e0ce1b034317398a4a95c78d40d81
janEbert <janpublicebert@posteo.net>
2023-04-18 13:40:41 +0200

    Add type annotations for TorchScript

--- torch/nn/modules/transformer.py
+++ torch/nn/modules/transformer.py
@@ -178,7 +178,11 @@ class Transformer(Module):
         return output
 
     @staticmethod
-    def generate_square_subsequent_mask(sz: int, device=None, dtype=None) -> Tensor:
+    def generate_square_subsequent_mask(
+            sz: int,
+            device: Optional[torch.device] = None,
+            dtype: Optional[torch.dtype] = None,
+    ) -> Tensor:
         r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
             Unmasked positions are filled with float(0.0).
         """
