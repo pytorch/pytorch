ffa6fefab620bdf678bf5ef543f3a366ccf0f885
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared43334.04.ncg2.facebook.com>
2023-04-18 00:03:24 -0700

    2023-04-18 nightly release (436edc5ac3de4c4e677ed136473bafe72002cc93)

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -24,6 +24,7 @@ import torch._dynamo
 import torch.nn as nn
 from torch._dispatch.python import enable_python_dispatcher
 from torch._dynamo.testing import rand_strided, same
+from torch._inductor.codegen.common import _data_type_propagation, OptimizationContext
 from torch._inductor.utils import run_and_get_triton_code
 from torch.fx.experimental.proxy_tensor import make_fx
 from torch.nn import functional as F
@@ -1500,6 +1501,13 @@ class CommonTemplate:
                 torch.randn(8, 8),
             ),
         )
+
+    # https://github.com/pytorch/pytorch/issues/98979
+    @unittest.skipIf(HAS_CUDA, "cuda failed for float64 linear")
+    def test_linear_float64(self):
+        mod = torch.nn.Sequential(torch.nn.Linear(8, 16).to(torch.float64)).eval()
+        with torch.no_grad():
+            self.common(mod, (torch.randn(2, 8).to(torch.float64),))
 
     def test_linear1(self):
         mod = torch.nn.Sequential(
@@ -5701,6 +5709,119 @@ class CommonTemplate:
                 x = torch.rand(48, 3, 512, 512)
                 opt_fn = torch._dynamo.optimize("inductor")(fn)
                 same(fn(x), opt_fn(x))
+
+    def test_data_type_propogation(self):
+        _graph: torch.fx.Graph = torch.fx.Graph()
+        ops: torch.fx.Node = _graph.create_node("placeholder", "ops")
+        get_index: torch.fx.Node = _graph.create_node(
+            "call_module", "get_index", args=("index0",)
+        )
+        c1: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "constant",
+            args=(
+                ops,
+                get_index,
+                torch.bfloat16,
+            ),
+        )
+        c2: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "constant",
+            args=(
+                ops,
+                get_index,
+                torch.float,
+            ),
+        )
+        add: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "add",
+            args=(
+                ops,
+                c1,
+                c2,
+            ),
+        )
+        eq: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "eq",
+            args=(
+                ops,
+                add,
+                add,
+            ),
+        )
+        argmin: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "reduction",
+            args=(
+                ops,
+                "buf",
+                torch.float,
+                torch.int64,
+                "argmin",
+                get_index,
+                add,
+            ),
+        )
+        any: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "reduction",
+            args=(
+                ops,
+                "buf",
+                torch.float,
+                torch.bool,
+                "any",
+                get_index,
+                add,
+            ),
+        )
+        bitwise_not: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "bitwise_not",
+            args=(
+                ops,
+                argmin,
+            ),
+        )
+        bitwise_or: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "bitwise_or",
+            args=(
+                ops,
+                eq,
+                any,
+            ),
+        )
+        bitwise_left_shift: torch.fx.Node = _graph.create_node(
+            "call_method",
+            "bitwise_left_shift",
+            args=(
+                ops,
+                argmin,
+                bitwise_not,
+            ),
+        )
+        _data_type_propagation(_graph)
+
+        def get_data_type(node: torch.fx.Node):
+            if OptimizationContext.key in node.meta:
+                return node.meta[OptimizationContext.key].dtype
+            else:
+                return None
+
+        self.assertEqual(get_data_type(ops), None)
+        self.assertEqual(get_data_type(c1), torch.bfloat16)
+        self.assertEqual(get_data_type(c2), torch.float)
+        self.assertEqual(get_data_type(add), torch.float)
+        self.assertEqual(get_data_type(eq), torch.bool)
+        self.assertEqual(get_data_type(argmin), torch.int64)
+        self.assertEqual(get_data_type(any), torch.bool)
+        self.assertEqual(get_data_type(bitwise_not), torch.int64)
+        self.assertEqual(get_data_type(bitwise_or), torch.bool)
+        self.assertEqual(get_data_type(bitwise_left_shift), torch.int64)
 
 
 @dataclasses.dataclass
