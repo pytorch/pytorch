c18d1526e60c69742b2f3034862c943036f06f58
Jerry Zhang <jerryzh168@gmail.com>
2023-04-17 17:10:02 -0700

    [quant][pt2e][refactor] Move the annotation for observer sharing ops into separate util
    Summary:
    In order to keep quantizer simple, we want to move the annotation code for operators like flatten, hardtanh etc. to
    a separate utility function that is called after the quantizer annotation is done, this makes these ops (operator list) not
    configurable by user, and also makes prepare_pt2e operator aware instead of operator agnostic, this design is not final,
    we may change it in the future if we find there are use cases that need these to be configurable or if we feel it is important for prepare_pt2e
    to stay agnostic to operator/operator patterns
    
    Test Plan:
    python test/test_quantization.py TestQuantizePT2E.test_qnnpack_quantizer_obs_sharing_ops
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/quantization/_pt2e/quantizer/qnnpack_quantizer.py
+++ torch/ao/quantization/_pt2e/quantizer/qnnpack_quantizer.py
@@ -263,10 +263,6 @@ class QNNPackQuantizer(Quantizer):
             self._annotate_maxpool2d(node, global_config)
             self._annotate_add_relu(node, global_config)
             self._annotate_add(node, global_config)
-            self._annotate_hardtanh(node, global_config)
-            self._annotate_mean(node, global_config)
-            self._annotate_adaptive_avg_pool2d(node, global_config)
-            self._annotate_flatten(node, global_config)
 
         return model
 
@@ -421,6 +417,8 @@ class QNNPackQuantizer(Quantizer):
             "_annotated": True,
         }
 
+    # TODO: move to `_pt2e/_propagate_annotation.py` after we have
+    # decided on the how we want to use pattern matching for annotation
     def _annotate_maxpool2d(
         self, node: Node, quantization_config: Optional[QuantizationConfig]
     ) -> None:
@@ -452,60 +450,6 @@ class QNNPackQuantizer(Quantizer):
             "input_output_share_observers": True,
             "_annotated": True,
         }
-
-    def _annotate_input_out_obs_sharing_op(
-        self,
-        op: Callable,
-        node: Node,
-        quantization_config: Optional[QuantizationConfig],
-    ) -> None:
-        io_obs_sharing_node = node
-        if (
-            io_obs_sharing_node.op != "call_function"
-            or io_obs_sharing_node.target != op
-        ):
-            return
-        if _is_annotated([io_obs_sharing_node]):
-            return
-
-        io_obs_sharing_node.meta["target_dtype_info"] = {
-            "input_act_obs_or_fq_ctr": _get_act_obs_or_fq_ctr(quantization_config),
-            "output_act_obs_or_fq_ctr": _get_act_obs_or_fq_ctr(quantization_config),
-            "input_output_share_observers": True,
-            "_annotated": True,
-        }
-
-    def _annotate_hardtanh(
-        self, node: Node, quantization_config: Optional[QuantizationConfig]
-    ) -> None:
-        self._annotate_input_out_obs_sharing_op(
-            torch.ops.aten.hardtanh.default, node, quantization_config
-        )
-
-    def _annotate_mean(
-        self, node: Node, quantization_config: Optional[QuantizationConfig]
-    ) -> None:
-        self._annotate_input_out_obs_sharing_op(
-            torch.ops.aten.mean.default, node, quantization_config
-        )
-        self._annotate_input_out_obs_sharing_op(
-            torch.ops.aten.mean.dim, node, quantization_config
-        )
-
-    def _annotate_adaptive_avg_pool2d(
-        self, node: Node, quantization_config: Optional[QuantizationConfig]
-    ) -> None:
-        self._annotate_input_out_obs_sharing_op(
-            torch.ops.aten.adaptive_avg_pool2d.default, node, quantization_config
-        )
-
-    def _annotate_flatten(
-        self, node: Node, quantization_config: Optional[QuantizationConfig]
-    ) -> None:
-        for flatten_aten_op in [torch.ops.aten.view_copy.default, torch.ops.aten.view.default]:
-            self._annotate_input_out_obs_sharing_op(
-                flatten_aten_op, node, quantization_config
-            )
 
     def _annotate_add_relu(
         self, node: Node, quantization_config: Optional[QuantizationConfig]
