a2c9bb941b91522f9390406cb790918d87790778
Michael Voznesensky <voznesenskym@gmail.com>
2023-04-19 02:39:53 +0000

    Update on "[Still testing] Torch.compile dynamic back/forward config change"
    [ghstack-poisoned]

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -163,7 +163,10 @@ def enable_dynamic(enable: bool = True, export: bool = False):
     if not enable:
         yield
         return
-    with config.patch(dynamic_shapes=True):
+    # dynamic=True used to mean fully dynamic. However, with automatic dynamic, the default flipped to
+    # deriving dynamism. For back compat, and forward compat for when dynamic=True is default, we take
+    # dynamic=True here to mean "fully dynamic from the start".
+    with config.patch(dynamic_shapes=True, assume_static_by_default=False):
         yield
 
 
