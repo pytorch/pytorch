62641af71caa737a55cd62bfb6e5a61615542793
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-04-24 18:54:05 +0200

    [lowrank] disable automatic mixed precision
    see #99710

--- torch/_lowrank.py
+++ torch/_lowrank.py
@@ -152,38 +152,40 @@ def _svd_lowrank(
         M_t = _utils.transpose(M)
     A_t = _utils.transpose(A)
 
-    # Algorithm 5.1 in Halko et al 2009, slightly modified to reduce
-    # the number conjugate and transpose operations
-    if m < n or n > q:
-        # computing the SVD approximation of a transpose in
-        # order to keep B shape minimal (the m < n case) or the V
-        # shape small (the n > q case)
-        Q = get_approximate_basis(A_t, q, niter=niter, M=M_t)
-        Q_c = _utils.conjugate(Q)
-        if M is None:
-            B_t = matmul(A, Q_c)
+    with torch.amp.autocast(A.device.type, enabled=False): # issue #99710
+
+        # Algorithm 5.1 in Halko et al 2009, slightly modified to reduce
+        # the number conjugate and transpose operations
+        if m < n or n > q:
+            # computing the SVD approximation of a transpose in
+            # order to keep B shape minimal (the m < n case) or the V
+            # shape small (the n > q case)
+            Q = get_approximate_basis(A_t, q, niter=niter, M=M_t)
+            Q_c = _utils.conjugate(Q)
+            if M is None:
+                B_t = matmul(A, Q_c)
+            else:
+                B_t = matmul(A, Q_c) - matmul(M, Q_c)
+            assert B_t.shape[-2] == m, (B_t.shape, m)
+            assert B_t.shape[-1] == q, (B_t.shape, q)
+            assert B_t.shape[-1] <= B_t.shape[-2], B_t.shape
+            U, S, Vh = torch.linalg.svd(B_t, full_matrices=False)
+            V = Vh.mH
+            V = Q.matmul(V)
         else:
-            B_t = matmul(A, Q_c) - matmul(M, Q_c)
-        assert B_t.shape[-2] == m, (B_t.shape, m)
-        assert B_t.shape[-1] == q, (B_t.shape, q)
-        assert B_t.shape[-1] <= B_t.shape[-2], B_t.shape
-        U, S, Vh = torch.linalg.svd(B_t, full_matrices=False)
-        V = Vh.mH
-        V = Q.matmul(V)
-    else:
-        Q = get_approximate_basis(A, q, niter=niter, M=M)
-        Q_c = _utils.conjugate(Q)
-        if M is None:
-            B = matmul(A_t, Q_c)
-        else:
-            B = matmul(A_t, Q_c) - matmul(M_t, Q_c)
-        B_t = _utils.transpose(B)
-        assert B_t.shape[-2] == q, (B_t.shape, q)
-        assert B_t.shape[-1] == n, (B_t.shape, n)
-        assert B_t.shape[-1] <= B_t.shape[-2], B_t.shape
-        U, S, Vh = torch.linalg.svd(B_t, full_matrices=False)
-        V = Vh.mH
-        U = Q.matmul(U)
+            Q = get_approximate_basis(A, q, niter=niter, M=M)
+            Q_c = _utils.conjugate(Q)
+            if M is None:
+                B = matmul(A_t, Q_c)
+            else:
+                B = matmul(A_t, Q_c) - matmul(M_t, Q_c)
+            B_t = _utils.transpose(B)
+            assert B_t.shape[-2] == q, (B_t.shape, q)
+            assert B_t.shape[-1] == n, (B_t.shape, n)
+            assert B_t.shape[-1] <= B_t.shape[-2], B_t.shape
+            U, S, Vh = torch.linalg.svd(B_t, full_matrices=False)
+            V = Vh.mH
+            U = Q.matmul(U)
 
     return U, S, V
 
