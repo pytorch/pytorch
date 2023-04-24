f91ffb4c6ec0ebfcf77e41ea37acfd96f66a07da
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-04-24 08:27:56 +0000

    Revert "Do not assume static by default when exporting (#99554)"
    This reverts commit d3bb762f1edc770879b7ba51019b02455109349b.
    
    Reverted https://github.com/pytorch/pytorch/pull/99554 on behalf of https://github.com/DanilBaibak due to Break internal build

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -793,9 +793,7 @@ def export(
 
     remove_from_cache(f)
     with patch(f"{__name__}.most_recent_backend", None), config.patch(
-        summarize_dim_constraints=True,
-        specialize_int=True,
-        assume_static_by_default=False,
+        summarize_dim_constraints=True, specialize_int=True
     ):
         opt_f = optimize_assert(
             dynamo_normalization_capturing_compiler,
