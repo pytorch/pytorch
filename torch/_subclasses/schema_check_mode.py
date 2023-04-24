ec13abbe3d9748178cab3eb3f849cf190ef147a0
Brian Hirsh <hirsheybar@meta.com>
2023-04-24 14:47:28 +0000

    Update on "Get SchemaCheckMode to error on ops that return inputs directly. Expose as a dynamo backend, eager_debug"
    Talked to zou3519 and ezyang on what the right UX is: tentatively, adding a new dynamo backend is cheap and simple, so it seems worth doing. And longer term, we agreed (?) that it's worth seeing if we can get custom ops sanity asserts to run more automatically, instead of needing a separate backend.
    
    Side comment: that actually seems tough: the mode detects secret mutations by cloning every input to every op, running the op, and checking that the data matches between the real input and the cloned input. So I doubt we'll be able to make that behavior always-on? It would need some config at least.
    
    
    
    
    cc soumith voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_subclasses/schema_check_mode.py
+++ torch/_subclasses/schema_check_mode.py
@@ -124,11 +124,10 @@ class SchemaCheckMode(TorchDispatchMode):
                             raise RuntimeError(f'Argument {name} is not defined to alias output but was aliasing')
                         else:
                             self.aliasing.append(Aliasing(func._schema.name, name, f"output_{j}"))
-                    for i, inp in enumerate(tree_flatten(after)[0]):
-                        if inp is tuple_out[j]:
-                            # Only mutable ops e.g. (add_, add.out) are allowed to directly return inputs.
-                            if not schema_info.is_mutable(SchemaArgument(SchemaArgType.input, i)):
-                                raise RuntimeError(f"""\
+                    if after is tuple_out[j]:
+                        # Only mutable ops e.g. (add_, add.out) are allowed to directly return inputs.
+                        if not schema_info.is_mutable(SchemaArgument(SchemaArgType.input, i)):
+                            raise RuntimeError(f"""\
 Dispatcher operators below autograd are not allowed to directly return inputs.
 However, we found that `outputs[{str(j)}] is {name}""")
                 if any(has_mutated(a, b, c) for a, b, c in zip(tree_flatten(before)[0], tree_flatten(after)[0], md)):
