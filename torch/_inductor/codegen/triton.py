7d53aaad92b9638e9d4b7b0aeff02cac8f9a48a2
Michael Voznesensky <voznesenskym@gmail.com>
2023-04-24 21:59:56 +0000

    div16 changes for dyn shapes (#99930)
    Lint, fix
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99930
    Approved by: https://github.com/ngimel

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -74,10 +74,13 @@ def config_of(args):
         if isinstance(x, TensorArg):
             return x.buffer not in V.graph.unaligned_buffers
         if isinstance(x, SizeArg):
-            return V.graph.sizevars.maybe_guard_multiple_of(x.expr, ALIGNMENT)
+            if isinstance(x.expr, (int, sympy.Integer)):
+                return V.graph.sizevars.maybe_guard_multiple_of(x.expr, ALIGNMENT)
+            else:
+                return False
         raise NotImplementedError(f"unhandled {type(x)}: {x}")
 
-    if config.triton.divisible_by_16 and not dynamo_config.dynamic_shapes:
+    if config.triton.divisible_by_16:
         divisible_by_16 = [i for i, arg in enumerate(args) if is_aligned(arg)]
     else:
         divisible_by_16 = []
