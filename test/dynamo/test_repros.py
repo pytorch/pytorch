32fab293ecc423ebead7649067db51fc5185895e
Animesh Jain <anijain@umich.edu>
2023-04-25 15:48:21 +0000

    [dynamo] graph break on load_attr (tensor.unsqueeze_)
    [ghstack-poisoned]

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -3083,6 +3083,33 @@ class ReproTests(torch._dynamo.test_case.TestCase):
         finally:
             torch.set_default_device(None)
 
+    def test_hf_bigbird_unsqueeze(self):
+        def torch_bmm_nd(inp_1, inp_2, ndim=None):
+            torch._dynamo.graph_break()
+            return torch.bmm(inp1, inp2)
+
+        def fn(inp1, inp2, inp3, inp4, c):
+            a = torch_bmm_nd(inp1, inp2, 4)
+            a.unsqueeze_(2)
+
+            b = torch_bmm_nd(inp3, inp4, 4)
+            b.unsqueeze_(2)
+            l = a + b
+
+            out = torch.cat([a, b, c], dim=2)
+            return out, l
+
+        inp1 = torch.rand(1, 64, 448)
+        inp2 = torch.rand(1, 448, 64)
+        inp3 = torch.rand(1, 64, 448)
+        inp4 = torch.rand(1, 448, 64)
+        c = torch.rand(1, 64, 1, 64)
+
+        cnt = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(cnt)(fn)
+        opt_fn(inp1, inp2, inp3, inp4, c)
+        self.assertEqual(cnt.frame_count, 2)
+
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -1133,6 +1133,7 @@ class InstructionTranslatorBase(Checkpointable[InstructionTranslatorGraphState])
         fn = self.pop()
         self.call_function(fn, args, {})
 
+    @break_graph_if_unsupported(push=0)
     def LOAD_ATTR(self, inst):
         obj = self.pop()
         result = BuiltinVariable(getattr).call_function(
