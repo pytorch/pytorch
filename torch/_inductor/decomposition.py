6357d9cd435ba7bd1672291f996589e11a0fdff0
Brian Hirsh <hirsheybar@meta.com>
2023-04-26 18:55:18 +0000

    [inductor] fix incorrect strides in copy() decomp, fix hf_LongFormer + hf_BigBird errors
    [ghstack-poisoned]

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -9,6 +9,7 @@ import torch.ao.quantization.fx._decomposed
 from torch import Tensor
 from torch._decomp import core_aten_decompositions, get_decompositions
 from torch._decomp.decompositions import pw_cast_for_opmath
+from torch._prims_common import compute_required_storage_length
 from torch.utils._mode_utils import no_dispatch
 
 from . import config, utils
@@ -357,10 +358,29 @@ def all_dim(input, dim, keepdim=False):
 @register_decomposition(aten.copy)
 def copy(self, src, non_blocking=False):
     intermediate = src.to(self, non_blocking)
-    if self.size() != intermediate.size():
-        return aten.expand_copy.default(intermediate, self.size())
-    else:
+    # cheapest case
+    if self.size() == intermediate.size() and self.stride() == intermediate.stride():
         return intermediate
+    # next cheapest case
+    if self.size() == intermediate.size() and self.is_contiguous():
+        return intermediate.contiguous()
+    # next cheapest case (expand_copy is guaranteed to return a contiguous tensor)
+    if self.is_contiguous():
+        return aten.expand_copy.default(intermediate, self.size())
+    # expensive case
+    # We need to return a tensor with the data of "src",
+    # But with the size/stride/storage_offset of "self" (... and any other metadata! neg, conj, etc).
+    # A problem for another day
+    required_size = compute_required_storage_length(
+        self.size(), self.stride(), self.storage_offset()
+    )
+    out_buffer = torch.empty(required_size, dtype=self.dtype, device=self.device)
+    out_buffer_updated = aten.as_strided_scatter(
+        out_buffer, src, self.size(), self.stride(), self.storage_offset()
+    )
+    return out_buffer_updated.as_strided(
+        self.size(), self.stride(), self.storage_offset()
+    )
 
 
 @register_decomposition([aten.baddbmm])
