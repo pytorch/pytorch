e97d24a719891ae290f16153dc0a2604b94a2b1f
Larry Liu <8188269+larryliu0820@users.noreply.github.com>
2023-04-26 22:45:21 -0700

    [dynamo][numpy] Handle return value being numpy ndarray (#99560)
    On top of #95849 this PR is trying to handle the special case when dealing with numpy.
    
    Consider the following example:
    
    ```
    def f(x: torch.Tensor) -> np.ndarray:
    	a = x.numpy()
    	return a.T
    ```
    In previous PR this will error out because we translate `a.T` to be a method call on `torch_np.ndarray.T` which is also a `torch_np.ndarray`.
    
    This PR handles this case, by conditionally converting a `torch_np.ndarray` to `np.ndarray` before returning, to match the original behavior.
    
    The compiled version will be:
    
    ```
    def f(x):
        ___tmp_0 = __compiled_fn_0(x)
        if isinstance(___tmp_0, torch_np.ndarray):
            return ___tmp_0.tensor.numpy()
        else:
            return ___tmp_0
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99560
    Approved by: https://github.com/jansel, https://github.com/yanboliang

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -668,6 +668,7 @@ class OutputGraph(fx.Tracer, Checkpointable[OutputGraphState]):
                 self.compile_and_call_fx_graph(tx, list(reversed(stack_values)), root)
                 + [create_instruction("UNPACK_SEQUENCE", arg=len(stack_values))]
             )
+            codegen = PyCodegen(tx)
         else:
             graph_output_var = self.new_var("graph_out")
             pass1 = PyCodegen(tx, root, graph_output_var)
@@ -698,7 +699,14 @@ class OutputGraph(fx.Tracer, Checkpointable[OutputGraphState]):
                     output.append(create_instruction("POP_TOP"))
             append_prefix_insts()
             self.add_output_instructions(output + pass2.get_instructions())
+            codegen = pass2
 
+        if config.numpy_ndarray_as_tensor:
+            from .variables.tensor import NumpyNdarrayVariable
+
+            self.add_output_instructions(
+                NumpyNdarrayVariable.reconstruct_ndarray_before_return(codegen, self)
+            )
         # restore all the live local vars
         self.add_output_instructions(
             [PyCodegen(tx).create_store(var) for var in reversed(restore_vars)]
