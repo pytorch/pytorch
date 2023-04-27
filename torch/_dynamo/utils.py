b2fb023b583ca65ae37f0ccdc6866fac30fc4b83
Michael Voznesensky <voznesenskym@gmail.com>
2023-04-27 06:50:15 +0000

    Update on "[WIP] [Needs more testing] Produce constant variables in cases where a SymNode is created with a constant"
    ` AOT_DYNAMIC_SHAPES=1 TORCHDYNAMO_DYNAMIC_SHAPES=1  benchmarks/dynamo/huggingface.py --performance  --training --amp --backend eager --disable-cudagraphs --device cuda --only AllenaiLongformerBase --explain` 
    
    Looks promising!
    
    Goes from:
    
    Dynamo produced 173 graphs covering 2760 ops with 160 graph breaks (14 unique)
    
    To:
    
    Dynamo produced 6 graphs covering 2298 ops with 15 graph breaks (7 unique)
    
    cc soumith penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1355,6 +1355,12 @@ def ifdyn(count1, count2):
     else:
         return count2
 
+def ifdynstaticdefault(count1, count2):
+    if torch._dynamo.config.assume_static_by_default:
+        return count1
+    else:
+        return count2
+
 
 def ifunspec(count1, count2):
     if torch._dynamo.config.dynamic_shapes and not torch._dynamo.config.specialize_int:
