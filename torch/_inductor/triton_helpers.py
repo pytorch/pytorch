633c3425d5a4ee2cd37ae9a7bae7e2436dc9fce7
Peter Bell <peterbell10@live.co.uk>
2023-04-27 13:30:54 +0100

    [inductor] Fix nan-handling of max and min reductions (#99881)
    This adds helpers that replace tritons `minimum`, `maximum`, `min` and
    `max` with the correct NaN prrpagation. I also removed
    `ops.int_minimum` in favor of `ops.minimum` because we can just omit
    the nan-checks by checking the dtype.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99881
    Approved by: https://github.com/ngimel

--- torch/_inductor/triton_helpers.py
+++ torch/_inductor/triton_helpers.py
@@ -3,6 +3,13 @@ import triton.language as tl
 
 
 @triton.jit
+def is_floating(x):
+    # Addition to promote scalars to tensor
+    x += tl.zeros((1,), tl.int1)
+    return x.dtype.is_floating()
+
+
+@triton.jit
 def _prod_accumulate(a, b):
     return a * b
 
@@ -10,3 +17,29 @@ def _prod_accumulate(a, b):
 @triton.jit
 def prod(input, axis):
     return tl.reduce(input, axis, _prod_accumulate)
+
+
+@triton.jit
+def minimum(a, b):
+    mask = a < b
+    if is_floating(a):
+        mask |= a != a
+    return tl.where(mask, a, b)
+
+
+@triton.jit
+def maximum(a, b):
+    mask = a > b
+    if is_floating(a):
+        mask |= a != a
+    return tl.where(mask, a, b)
+
+
+@triton.jit
+def min(a, dim):
+    return tl.reduce(a, dim, minimum)
+
+
+@triton.jit
+def max(a, dim):
+    return tl.reduce(a, dim, maximum)
