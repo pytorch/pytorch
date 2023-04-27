3c18cbb6671c9634e82a88b6774c116a87328333
Huy Do <huydhn@gmail.com>
2023-04-26 18:22:44 -0700

    Add aten test

--- aten/tools/run_tests.sh
+++ aten/tools/run_tests.sh
@@ -3,71 +3,76 @@ set -x
 set -e
 
 VALGRIND_SUP="${PWD}/`dirname $0`/valgrind.sup"
-pushd $1
+export CPP_TESTS_DIR=$1
 
 VALGRIND=${VALGRIND:=ON}
-./basic
-./atest
-./scalar_test
-./broadcast_test
-./wrapdim_test
-./apply_utils_test
-./dlconvertor_test
-./native_test
-./scalar_tensor_test
+python test/run_test.py --cpp --verbose \
+  -i cpp/basic \
+  -i cpp/atest \
+  -i cpp/scalar_test \
+  -i cpp/broadcast_test \
+  -i cpp/wrapdim_test \
+  -i cpp/apply_utils_test \
+  -i cpp/dlconvertor_test \
+  -i cpp/native_test \
+  -i cpp/scalar_tensor_test \
+  -i cpp/undefined_tensor_test \
+  -i cpp/extension_backend_test \
+  -i cpp/lazy_tensor_test \
+  -i cpp/tensor_iterator_test \
+  -i cpp/Dimname_test \
+  -i cpp/Dict_test \
+  -i cpp/NamedTensor_test \
+  -i cpp/cpu_generator_test \
+  -i cpp/legacy_vmap_test \
+  -i cpp/operators_test
+
 if [[ -x ./tensor_interop_test ]]; then
-  ./tensor_interop_test
+  python test/run_test.py --cpp --verbose -i cpp/tensor_interop_test
 fi
-./undefined_tensor_test
-./extension_backend_test
-./lazy_tensor_test
-./tensor_iterator_test
-./Dimname_test
-./Dict_test
-./NamedTensor_test
-./cpu_generator_test
-./legacy_vmap_test
-./operators_test
 if [[ -x ./cudnn_test ]]; then
-  ./cudnn_test
+  python test/run_test.py --cpp --verbose -i cpp/cudnn_test
 fi
 if [[ -x ./cuda_generator_test ]]; then
-  ./cuda_generator_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_generator_test
 fi
 if [[ -x ./apply_test ]]; then
-  ./apply_test
+  python test/run_test.py --cpp --verbose -i cpp/apply_test
 fi
 if [[ -x ./stream_test ]]; then
-  ./stream_test
+  python test/run_test.py --cpp --verbose -i cpp/stream_test
 fi
 if [[ -x ./cuda_half_test ]]; then
-  ./cuda_half_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_half_test
 fi
 if [[ -x ./cuda_vectorized_test ]]; then
-  ./cuda_vectorized_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_vectorized_test
 fi
 if [[ -x ./cuda_distributions_test ]]; then
-  ./cuda_distributions_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_distributions_test
 fi
 if [[ -x ./cuda_optional_test ]]; then
-  ./cuda_optional_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_optional_test
 fi
 if [[ -x ./cuda_tensor_interop_test ]]; then
-  ./cuda_tensor_interop_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_tensor_interop_test
 fi
 if [[ -x ./cuda_complex_test ]]; then
-  ./cuda_complex_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_complex_test
 fi
 if [[ -x ./cuda_complex_math_test ]]; then
-  ./cuda_complex_math_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_complex_math_test
 fi
 if [[ -x ./cuda_cub_test ]]; then
-  ./cuda_cub_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_cub_test
 fi
 if [[ -x ./cuda_atomic_ops_test ]]; then
-  ./cuda_atomic_ops_test
+  python test/run_test.py --cpp --verbose -i cpp/cuda_atomic_ops_test
 fi
+
 if [ "$VALGRIND" == "ON" ]; then
+  # NB: As these tests are invoked by valgrind, let's leave them for now as it's
+  # unclear if valgrind -> python -> gtest would work
   valgrind --suppressions="$VALGRIND_SUP" --error-exitcode=1 ./basic --gtest_filter='-*CUDA'
   if [[ -x ./tensor_interop_test ]]; then
     valgrind --suppressions="$VALGRIND_SUP" --error-exitcode=1 ./tensor_interop_test
