f158bfab1c319fca9ec14c8e5005680420e34e7c
ts <tejfsingh@gmail.com>
2023-04-27 12:57:08 -0700

    first review

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -830,15 +830,16 @@ TORCH_IMPL_FUNC(index_add_cpu_out)
 
   auto index_contig = index.contiguous();
 
-  if (source.size(dim) != index.size(0)) {
+  if (source.size(dim) != index.numel()) {
     AT_ERROR("source tensor shape along dim does not match index tensor length.");
   }
 
-  for (int i = 0; i < self.dim(); i++) {
-    if (i == dim) continue;
-    if (i >= source.dim() || source.size(i) != self.size(i)) {
-      AT_ERROR("source tensor shape does not match self tensor shape along non-indexed dimensions.");
-    }
+  auto self_sizes = self.sizes().vec();
+  auto source_sizes = source.sizes().vec();
+  self_sizes.erase(self_sizes.begin() + dim);
+  source_sizes.erase(source_sizes.begin() + dim);
+  if (self_sizes != source_sizes) {
+    AT_ERROR("source tensor sizes must be broadcastable to self tensor sizes (excluding the specified dimension)");
   }
 
   if (result.dim() > 1) {
