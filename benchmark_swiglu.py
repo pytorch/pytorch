6efa09aaedbbff880257bacd43e15de0d9fdb7c2
Aleksandar Samardžić <asamardzic@quansight.com>
2023-04-28 16:52:50 +0200

    Add benchmark for SwiGLU operator

--- benchmark_swiglu.py
+++ benchmark_swiglu.py
@@ -5,27 +5,102 @@ import torch.utils.benchmark as benchmark
 from torch import nn
 from torch.ao.pruning import WeightNormSparsifier
 from itertools import product
-from torch.ao.nn.sparse.cutlass_linear import CUTLASSLinear
-
 
 device = 'cuda'
 dtype = torch.float16
 torch.set_printoptions(precision=3, threshold=None, edgeitems=4, linewidth=460, profile=None, sci_mode=False)
 
-class Model(nn.Module):
+class SwiGLU(nn.Module):
 
-    def __init__(self, m, k):
+    def __init__(self, m, k, dtype):
         super().__init__()
-        # transposed so reversed
-        self.linear = nn.Linear(k, m)
+
+        # to properly initialize weights and biases
+        linear1 = nn.Linear(k, m, dtype=dtype)
+        linear2 = nn.Linear(k, m, dtype=dtype)
+        linear3 = nn.Linear(m, m, dtype=dtype)
+
+        self.m = m
+        self.k = k
+        self.dtype = dtype
+        self.w1 = linear1.weight
+        self.b1 = linear1.bias
+        self.w2 = linear2.weight
+        self.b2 = linear2.bias
+        self.w3 = linear3.weight
+        self.b3 = linear3.bias
 
     def forward(self, x):
-        return self.linear(x)
+        x1 = x @ self.w1.transpose(-2, -1) + self.b1
+        x2 = x @ self.w2.transpose(-2, -1) + self.b2
+        x3 = torch.nn.functional.silu(x1)
+        x4 = x3 * x2
+        x5 = x4 @ self.w3.transpose(-2, -1) + self.b3
+        return x5
+
+class CUTLASSSwiGLU(SwiGLU):
+
+    def forward(self, x):
+        b = x.view(-1, x.shape[-1]).T
+
+        #x1 = x @ self.w1.transpose(-2, -1) + self.b1
+        c1 = self.b1.view(torch.numel(self.b1), 1)
+        if self.mask1 is not None:
+            x3, self.meta1 = torch._cutlass_linear(self.w1, b, c1, self.mask1, activation='silu')
+            self.mask = None
+        else:
+            x3, _ = torch._cutlass_linear(self.w1, b, c1, self.meta1, activation='silu')
+        x3 = x3.T.view(*x.shape[:-1], -1)
+
+        #x2 = x @ self.w2.transpose(-2, -1) + self.b2
+        c2 = self.b2.view(torch.numel(self.b2), 1)
+        if self.mask2 is not None:
+            x2, self.meta2 = torch._cutlass_linear(self.w2, b, c2, self.mask2)
+            self.mask = None
+        else:
+            x2, _ = torch._cutlass_linear(self.w2, b, c2, self.meta2)
+        x2 = x2.T.view(*x.shape[:-1], -1)
+
+        x4 = x3 * x2
+        x5 = x4 @ self.w3.transpose(-2, -1) + self.b3
+        return x5
+
+    @classmethod
+    def from_dense(cls, mod):
+        cutlass_swiglu = cls(mod.m, mod.k, mod.dtype)
+
+        m, k = mod.m, mod.k
+        mask1 = mod.w1.data != 0
+        mask2 = mod.w2.data != 0
+
+        cutlass_swiglu.w1 = torch.nn.parameter.Parameter(mod.w1.data.masked_select(mask1).view(m, k // 2))
+        cutlass_swiglu.b1 = torch.nn.parameter.Parameter(mod.b1)
+        cutlass_swiglu.w2 = torch.nn.parameter.Parameter(mod.w2.data.masked_select(mask2).view(m, k // 2))
+        cutlass_swiglu.b2 = torch.nn.parameter.Parameter(mod.b2)
+        cutlass_swiglu.w3 = torch.nn.parameter.Parameter(mod.w3)
+        cutlass_swiglu.b3 = torch.nn.parameter.Parameter(mod.b3)
+
+        cutlass_swiglu.mask1 = mask1
+        cutlass_swiglu.meta1 = None
+        cutlass_swiglu.mask2 = mask2
+        cutlass_swiglu.meta2 = None
+
+        return cutlass_swiglu
+
+class Model(nn.Module):
+
+    def __init__(self, m, k, dtype):
+        super().__init__()
+        self.swiglu = SwiGLU(m, k, dtype)
+
+    def forward(self, x):
+        return self.swiglu(x)
 
 if __name__ == '__main__':
     results = []
 
     shapes = [
+        ###(32, 64, 8),
         # distilbert shapes
         (768, 3072, 768),
         (3072, 768, 3072),
@@ -65,18 +140,18 @@ if __name__ == '__main__':
         # label and sub_label are the rows
         # description is the column
         try:
-            label = 'CUTLASSLinear vs nn.Linear'
+            label = 'CUTLASSSwiGLU vs SwiGLU'
             sub_label = f'm:{m:5d} | k:{k:5d} | n:{n:5d} | batch_size: {batch_size:4d}'
 
-            model = Model(m, k)
+            model = Model(m, k, dtype)
             model.half()
             model.cuda()
             model.eval()
 
             pruner = WeightNormSparsifier(sparsity_level=1.0, sparse_block_shape=(1, 4), zeros_per_block=2)
-            pruner.prepare(model, [{'tensor_fqn': 'linear.weight'}])
+            pruner.prepare(model, [{'tensor_fqn': 'swiglu.w1'}, {'tensor_fqn': 'swiglu.w2'}])
             pruner.step()
-            sparse_linear = pruner.convert(model, mapping={nn.Linear: CUTLASSLinear})
+            sparse_linear = pruner.convert(model, mapping={SwiGLU: CUTLASSSwiGLU})
             print(sparse_linear)
 
             pruner.squash_mask()
@@ -87,6 +162,7 @@ if __name__ == '__main__':
                 input_tensor = torch.randn(batch_size, n, k, device=device, dtype=dtype)
                 dense_output = dense_linear(input_tensor)
                 sparse_output = sparse_linear(input_tensor)
+
                 assert torch.allclose(sparse_output, dense_output, rtol=1e-3, atol=1e-3)
 
 
