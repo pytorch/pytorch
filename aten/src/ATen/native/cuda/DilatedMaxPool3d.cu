483109652fb480cc040d6a3c3561b576c0e6d098
eqy <eddiey@nvidia.com>
2023-04-27 22:40:00 -0700

    Update DilatedMaxPool3d.cu

--- aten/src/ATen/native/cuda/DilatedMaxPool3d.cu
+++ aten/src/ATen/native/cuda/DilatedMaxPool3d.cu
@@ -635,7 +635,7 @@ Tensor max_pool3d_with_indices_backward_cuda(
   // See Note [Writing Nondeterministic Operations]
   // Nondeterministic because of atomicAdd usage
   globalContext().alertNotDeterministic("max_pool3d_with_indices_backward_cuda");
-  auto gradInput = at::empty(input, input.options());
+  auto gradInput = at::empty(input.sizes(), input.options());
   max_pool3d_with_indices_backward_out_cuda_template(
     gradInput,
     gradOutput,
