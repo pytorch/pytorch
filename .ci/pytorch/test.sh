fbee29c5d4ef4a3e0bc15b81a385cb536e1fa2a2
Huy Do <huydhn@gmail.com>
2023-05-01 17:16:01 -0700

    Move the mitigation to test.sh

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -9,6 +9,13 @@ set -ex
 echo "Environment variables:"
 env
 
+# Flaky access to download.pytorch.org https://github.com/pytorch/pytorch/issues/100400
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+# Print the configurations
+sysctl -a 2>/dev/null | grep disable_ipv6
+
 TORCH_INSTALL_DIR=$(python -c "import site; print(site.getsitepackages()[0])")/torch
 TORCH_BIN_DIR="$TORCH_INSTALL_DIR"/bin
 TORCH_LIB_DIR="$TORCH_INSTALL_DIR"/lib
