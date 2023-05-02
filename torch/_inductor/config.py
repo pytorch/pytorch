a52d256016d3bde7324ac68714ebe48cf1a13c1c
Michael Lazos <mlazos@fb.com>
2023-05-02 23:19:31 +0000

    Fix config changes

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -125,7 +125,6 @@ def decide_compile_threads():
     2. Set to 1 if it's win32 platform or it's a fbcode build
     3. decide by the number of CPU cores
     """
-    return 1
     if "TORCHINDUCTOR_COMPILE_THREADS" in os.environ:
         return int(os.environ["TORCHINDUCTOR_COMPILE_THREADS"])
     elif sys.platform == "win32" or is_fbcode():
@@ -315,7 +314,7 @@ class trace:
     output_code = True
 
     # SVG figure showing post-fusion graph
-    graph_diagram = True
+    graph_diagram = False
 
     # Store cProfile (see snakeviz to view)
     compile_profile = False
