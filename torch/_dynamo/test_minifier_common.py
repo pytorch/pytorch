698705c3c49d3a7c605b9ec2ff7523e5ed7c66c6
Edward Z. Yang <ezyang@meta.com>
2023-05-01 18:46:38 -0700

    Simplify minifier testing by incorporating fault injection in prod code (#100357)
    Previously, minifier testing injected faults by injecting extra code
    into the repro scripts, and then ensuring this code got propagated to
    all subsequent subprocess calls.  This was not only quite complicated,
    but also induced a big slowdown on the minifier, because to inject the
    faults, you had to import torch._inductor, which would cause the
    compilation threads to immediately get initialized before you even got
    to do anything else in the repro script.
    
    This new approach fixes this problem by incorporating the fault
    injection into "prod" code.  Essentially, for inductor fault injection
    we introduce some new config flags that let you "configure" Inductor to
    be buggy; for Dynamo fault injection we just permanently keep the buggy
    testing backends registered.  This is MUCH simpler: we only have to
    propagate the buggy config (which is something we're already doing),
    and it saves the minifier scripts from having to immediately initialize
    inductor on entry.
    
    Also, I enable the test for Triton runtime errors, now that tl.assert_device is here.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100357
    Approved by: https://github.com/voznesenskym

--- torch/_dynamo/test_minifier_common.py
+++ torch/_dynamo/test_minifier_common.py
@@ -8,7 +8,6 @@ import unittest
 import torch
 import torch._dynamo
 import torch._dynamo.test_case
-from torch._dynamo.debug_utils import TEST_REPLACEABLE_COMMENT
 
 
 class MinifierTestBase(torch._dynamo.test_case.TestCase):
@@ -33,13 +32,6 @@ class MinifierTestBase(torch._dynamo.test_case.TestCase):
             print("test_minifier_common tmpdir kept at: {cls.DEBUG_DIR}")
         cls._exit_stack.close()
 
-    # Search for the name of the first function defined in a code string.
-    def _get_fn_name(self, code):
-        fn_name_match = re.search(r"def (\w+)\(", code)
-        if fn_name_match is not None:
-            return fn_name_match.group(1)
-        return None
-
     # Run `code` in a separate python process.
     # Returns the completed process state and the directory containing the
     # minifier launcher script, if `code` outputted it.
@@ -56,25 +48,13 @@ class MinifierTestBase(torch._dynamo.test_case.TestCase):
             return proc, repro_dir_match.group(1)
         return proc, None
 
-    # Patch generated files with testing patches
-    def _inject_code(self, patch_code, filename):
-        patch_code = f"""\
-{patch_code}
-torch._dynamo.config.debug_dir_root = "{self.DEBUG_DIR}"
-"""
-        with open(filename, "r") as f:
-            code = f.read()
-        code = code.replace(TEST_REPLACEABLE_COMMENT, patch_code)
-        with open(filename, "w") as f:
-            f.write(code)
-        return code
-
-    # Runs the minifier launcher script in `repro_dir`, patched with `patch_code`.
-    def _run_minifier_launcher(self, patch_code, repro_dir):
+    # Runs the minifier launcher script in `repro_dir`
+    def _run_minifier_launcher(self, repro_dir):
         self.assertIsNotNone(repro_dir)
         launch_file = os.path.join(repro_dir, "minifier_launcher.py")
+        with open(launch_file, "r") as f:
+            launch_code = f.read()
         self.assertTrue(os.path.exists(launch_file))
-        launch_code = self._inject_code(patch_code, launch_file)
 
         launch_proc = subprocess.run(
             ["python3", launch_file],
@@ -86,12 +66,13 @@ torch._dynamo.config.debug_dir_root = "{self.DEBUG_DIR}"
 
         return launch_proc, launch_code
 
-    # Runs the repro script in `repro_dir`, patched with `patch_code`
-    def _run_repro(self, patch_code, repro_dir):
+    # Runs the repro script in `repro_dir`
+    def _run_repro(self, repro_dir):
         self.assertIsNotNone(repro_dir)
         repro_file = os.path.join(repro_dir, "repro.py")
+        with open(repro_file, "r") as f:
+            repro_code = f.read()
         self.assertTrue(os.path.exists(repro_file))
-        repro_code = self._inject_code(patch_code, repro_file)
 
         repro_proc = subprocess.run(
             ["python3", repro_file], capture_output=True, cwd=repro_dir
@@ -100,7 +81,8 @@ torch._dynamo.config.debug_dir_root = "{self.DEBUG_DIR}"
 
     # Template for testing code.
     # `run_code` is the code to run for the test case.
-    # `patch_code` is the code to be patched in every generated file.
+    # `patch_code` is the code to be patched in every generated file; usually
+    # just use this to turn on bugs via the config
     def _gen_test_code(self, run_code, repro_after, repro_level, patch_code):
         return f"""\
 import torch
@@ -122,7 +104,11 @@ torch._dynamo.config.debug_dir_root = "{self.DEBUG_DIR}"
         test_proc, repro_dir = self._run_test_code(test_code)
         self.assertIsNotNone(repro_dir)
         print("running minifier")
-        launch_proc, launch_code = self._run_minifier_launcher(patch_code, repro_dir)
+        launch_proc, launch_code = self._run_minifier_launcher(repro_dir)
         print("running repro")
-        repro_proc, repro_code = self._run_repro(patch_code, repro_dir)
-        return ((test_proc, launch_proc, repro_proc), (launch_code, repro_code))
+        repro_proc, repro_code = self._run_repro(repro_dir)
+        return (test_proc, launch_proc, repro_proc), (launch_code, repro_code)
+
+    def _run_full_test_nocode(self, run_code, repro_after, repro_level, patch_code):
+        tbs, _ = self._run_full_test(run_code, repro_after, repro_level, patch_code)
+        return tbs
