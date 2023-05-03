9b4a6211dcdfe1e2750dc518ec3914dce5385cb3
Edward Z. Yang <ezyang@meta.com>
2023-05-03 06:18:04 -0700

    Update on "Add load_storage"
    This adds a new operator debugprims::load_storage which does the unusual thing of loading a tensor from disk (via ContentStoreReader)
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    cc soumith voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_prims/debug_prims.py
+++ torch/_prims/debug_prims.py
@@ -1,9 +1,12 @@
+import contextlib
+from typing import Tuple
+
 import torch
 from torch._custom_op import custom_op
 from torch.utils._content_store import ContentStoreReader
-import contextlib
 
 LOAD_TENSOR_READER = None
+
 
 @contextlib.contextmanager
 def load_tensor_reader(loc):
@@ -15,8 +18,9 @@ def load_tensor_reader(loc):
     finally:
         LOAD_TENSOR_READER = None
 
+
 def register_debug_prims():
-    @custom_op('debugprims::load_tensor')
+    @custom_op("debugprims::load_tensor")
     def load_tensor(
         name: str,
         sizes: Tuple[int, ...],
@@ -31,9 +35,11 @@ def register_debug_prims():
     def load_tensor_factory(name, sizes, strides, dtype, device):
         if LOAD_TENSOR_READER is None:
             from torch._dynamo.testing import rand_strided
+
             return rand_strided(sizes, strides, dtype, device)
         else:
             from torch._dynamo.utils import clone_input
+
             r = LOAD_TENSOR_READER.read_tensor(name, device=device)
             if r.dtype != dtype:
                 r = clone_input(r, dtype=dtype)
