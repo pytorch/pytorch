314b8ff71797cdda30a25ac21fef6b505e6b3be2
Nicolas Macchioni <nmacchioni@meta.com>
2023-05-04 10:22:06 -0700

    [inductor] add test_max_autotune.py target (#100487)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/100487
    
    Test Plan: sandcastle + CI
    
    Differential Revision: D45457009
    
    fbshipit-source-id: 73094a1048729441319d24ffe0682c9db705cbf2

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -10,10 +10,7 @@ from torch._inductor.kernel.mm_plus_mm import aten_mm_plus_mm
 from torch._inductor.select_algorithm import AlgorithmSelectorCache, ChoiceCaller
 from torch._inductor.virtualized import V
 from torch.fx.experimental.proxy_tensor import make_fx
-from torch.testing._internal.common_utils import (
-    instantiate_parametrized_tests,
-    parametrize,
-)
+from torch.testing._internal.common_utils import instantiate_parametrized_tests
 from torch.testing._internal.inductor_utils import HAS_CUDA
 
 torch.set_float32_matmul_precision("high")
@@ -113,8 +110,7 @@ class TestDoBench(TestCase):
             child.join()
             self.assertNotEqual(0, child.exitcode)
 
-    @parametrize("autotune_in_subproc", (True, False))
-    def test_max_autotune_mm_plus_mm(self, autotune_in_subproc):
+    def test_max_autotune_mm_plus_mm(self):
         """
         This crash previously due to a triton issue: https://github.com/openai/triton/issues/1298 .
         With autotuning in subprocess, we don't crash anymore.
@@ -129,9 +125,7 @@ class TestDoBench(TestCase):
         c = torch.randn(m, k).cuda()
         d = torch.randn(k, n).cuda()
 
-        with config.patch(
-            {"max_autotune": True, "autotune_in_subproc": autotune_in_subproc}
-        ):
+        with config.patch({"max_autotune": True, "autotune_in_subproc": True}):
             torch.compile(mm_plus_mm)(a, b, c, d)
 
     def test_max_autotune_regular_mm(self):
