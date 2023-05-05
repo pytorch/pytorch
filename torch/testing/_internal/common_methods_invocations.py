68a254657d9442465fb83691b3f1e9ec64071c47
Khushi <khushiagrawal411@gmail.com>
2023-05-05 17:52:33 +0530

    try to fix failure

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -12329,7 +12329,7 @@ op_db: List[OpInfo] = [
                DecorateInfo(unittest.skip("Skipped!"), 'TestCommon', 'test_noncontiguous_samples',
                             dtypes=(torch.complex64,), device_type='cuda'),
                # AssertionError: Tensor-likes are not close!
-               DecorateInfo(unittest.expectedFailure, 'TestCompositeCompliance', 'test_backward', active_if=TEST_WITH_ASAN,
+               DecorateInfo(unittest.expectedFailure, 'TestCompositeCompliance', 'test_backward',
                             dtypes=(torch.float32,), device_type='cuda'),
            ),
            supports_expanded_weight=True,
