274c2a88b42a102922773f7d93e046d3d9f203df
Peter Pham <peterpham86@gmail.com>
2023-05-05 12:27:00 -0700

    use lintrunner instead of flake8 to match CI linter

--- torch/onnx/symbolic_opset13.py
+++ torch/onnx/symbolic_opset13.py
@@ -441,18 +441,16 @@ def _reduce_with_dtype(onnx_op, name):
             if dtype.node().kind() == "onnx::Constant":
                 dtype = symbolic_helper._get_const(dtype, "i", "dtype")
                 dtype_onnx = _type_utils.JitScalarType(dtype).onnx_type()
-                self = g.op(
-                    "Cast", self, to_i=dtype_onnx
-                )
+                self = g.op("Cast", self, to_i=dtype_onnx)
             elif dtype.node().kind() != "prim::Constant":
                 return symbolic_helper._unimplemented(name, "dtype", dtype)
             result = symbolic(g, self)
             if dtype_onnx is not None:
-                result_dtype_onnx = _type_utils.JitScalarType.from_value(result).onnx_type()
+                result_dtype_onnx = _type_utils.JitScalarType.from_value(
+                    result
+                ).onnx_type()
                 if result_dtype_onnx != dtype_onnx:
-                    result = g.op(
-                        "Cast", result, to_i=dtype_onnx
-                    )
+                    result = g.op("Cast", result, to_i=dtype_onnx)
             return result
 
         @symbolic_helper.parse_args("v", "v", "i", "none")
@@ -462,18 +460,16 @@ def _reduce_with_dtype(onnx_op, name):
             if dtype.node().kind() == "onnx::Constant":
                 dtype = symbolic_helper._get_const(dtype, "i", "dtype")
                 dtype_onnx = _type_utils.JitScalarType(dtype).onnx_type()
-                self = g.op(
-                    "Cast", self, to_i=dtype_onnx
-                )
+                self = g.op("Cast", self, to_i=dtype_onnx)
             elif dtype.node().kind() != "prim::Constant":
                 return symbolic_helper._unimplemented(name, "dtype", dtype)
             result = symbolic(g, self, dim, keepdim)
             if dtype_onnx is not None:
-                result_dtype_onnx = _type_utils.JitScalarType.from_value(result).onnx_type()
+                result_dtype_onnx = _type_utils.JitScalarType.from_value(
+                    result
+                ).onnx_type()
                 if result_dtype_onnx != dtype_onnx:
-                    result = g.op(
-                        "Cast", result, to_i=dtype_onnx
-                    )
+                    result = g.op("Cast", result, to_i=dtype_onnx)
             return result
 
         return reduce_nodim, reduce_dim
