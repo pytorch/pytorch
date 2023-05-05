2feceb2fc7c47540c6ad3532b054494b0d510b88
Khushi <khushiagrawal411@gmail.com>
2023-05-05 16:39:44 +0530

    add skip for mps

--- test/test_mps.py
+++ test/test_mps.py
@@ -600,6 +600,7 @@ def mps_ops_modifier(ops):
         'linalg.pinv': None,
         'linalg.pinvhermitian': None,
         'nonzero_static': None,
+        'nn.functional.conv3d': None,
 
         # MPS: input sizes must be divisible by output sizes
         'nn.functional.adaptive_avg_pool1d': None,
