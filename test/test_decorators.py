60691672c0e37f5322e43b3e0b28af220eabe39f
ulasonat <ulasonat11@gmail.com>
2023-05-04 21:51:09 -0400

    Black applied

--- test/test_decorators.py
+++ test/test_decorators.py
@@ -1,17 +1,30 @@
 import unittest
-from torch.testing._internal.common_device_type import instantiate_device_type_tests, onlyCPU, onlyCUDA, onlyOn, skipIfRocm, dtypes
-from torch.testing._internal.common_utils import TestCase, run_tests, expectedFailureMeta, skipIfNoLapack, TEST_NUMPY
+from torch.testing._internal.common_device_type import (
+    instantiate_device_type_tests,
+    onlyCPU,
+    onlyCUDA,
+    onlyOn,
+    skipIfRocm,
+    dtypes,
+)
+from torch.testing._internal.common_utils import (
+    TestCase,
+    run_tests,
+    expectedFailureMeta,
+    skipIfNoLapack,
+    TEST_NUMPY,
+)
+
 
 class TestDecorators(TestCase):
-
     @expectedFailureMeta
     def test_expected_to_fail(self):
         self.assertEqual(1, 2)
 
     def run_single_test(self, test_method):
-        test_suite = unittest.defaultTestLoader.loadTestsFromTestCase(type("TempTest", (TestDecorators,), {
-            'single_test': test_method
-        }))
+        test_suite = unittest.defaultTestLoader.loadTestsFromTestCase(
+            type("TempTest", (TestDecorators,), {"single_test": test_method})
+        )
 
         class TestExecutionChecker(unittest.TestResult):
             executed = False
@@ -32,15 +45,15 @@ class TestDecorators(TestCase):
 
     @onlyCPU
     def test_only_cpu(self):
-        self.assertEqual(str(self.device), 'cpu')
+        self.assertEqual(str(self.device), "cpu")
 
     @onlyCUDA
     def test_only_cuda(self):
         self.assertIn("cuda", str(self.device).lower())
 
-    @onlyOn('cpu', 'cuda')
+    @onlyOn("cpu", "cuda")
     def test_only_on_cpu_or_cuda(self):
-        self.assertIn(str(self.device), ['cpu', 'cuda'])
+        self.assertIn(str(self.device), ["cpu", "cuda"])
 
     @skipIfRocm
     def test_skip_if_rocm(self):
@@ -49,11 +62,13 @@ class TestDecorators(TestCase):
     @skipIfNoLapack
     def test_skip_if_no_lapack(self):
         import torch
+
         self.assertTrue(torch._C.has_lapack)
 
     @TEST_NUMPY
     def test_test_numpy(self):
         import numpy as np
+
         self.assertIsNotNone(np)
 
     @dtypes(torch.float32, torch.float64)
@@ -61,5 +76,6 @@ class TestDecorators(TestCase):
         expected_dtypes = (torch.float32, torch.float64)
         self.assertIn(dtype, expected_dtypes)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_tests()
