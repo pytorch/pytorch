00d48902182b42a7cd48bef911c1addcc9fe73c5
Rodrigo Kumpera <kumpera@fb.com>
2023-05-05 20:18:35 +0000

    [c10d] Apply EFA workaround to Store tests. (#100382)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100382
    Approved by: https://github.com/fduwjj

--- test/distributed/test_store.py
+++ test/distributed/test_store.py
@@ -20,7 +20,8 @@ if not dist.is_available():
 import torch.testing._internal.common_utils as common
 from torch.testing._internal.common_distributed import (
     skip_if_win32,
-    create_tcp_store
+    create_tcp_store,
+    tp_transports
 )
 from torch.testing._internal.common_utils import (
     TestCase,
@@ -145,6 +146,7 @@ class FileStoreTest(TestCase, StoreTestBase):
         # Init RPC using file
         rpc_backend_options = rpc.TensorPipeRpcBackendOptions()
         rpc_backend_options.init_method = f"file://{file.name}"
+        rpc_backend_options._transports = tp_transports()
         rpc.init_rpc("worker", rank=0, world_size=1, rpc_backend_options=rpc_backend_options)
 
         # Init PG using file
@@ -258,7 +260,8 @@ class TCPStoreTest(TestCase, StoreTestBase):
         )
 
         backend_opts = rpc.TensorPipeRpcBackendOptions(
-            init_method=f"tcp://{addr}:{port}"
+            init_method=f"tcp://{addr}:{port}",
+            _transports=tp_transports()
         )
         rpc.init_rpc(
             name="worker0",
