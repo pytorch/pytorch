d06bb704bd3d52cd27895e47e0a34a9126943f14
ts <tejfsingh@gmail.com>
2023-05-08 15:54:16 -0700

    add test

--- test/nn/test_init.py
+++ test/nn/test_init.py
@@ -121,6 +121,15 @@ class TestNNInit(TestCase):
 
             assert self._is_trunc_normal(input_tensor, mean, std, a, b)
 
+    def test_trunc_normal_generator(self):
+        gen = torch.Generator()
+        gen.manual_seed(42)
+        input_tensor = torch.empty(5)
+        init.trunc_normal_(input_tensor, generator=gen)
+        expected = torch.tensor([ 1.1025,  1.2598, -0.2840,  1.5415, -0.2652])
+
+        self.assertEqual(input_tensor, expected)
+
     def test_constant(self):
         for dims in [1, 2, 4]:
             input_tensor = self._create_random_nd_tensor(dims, size_min=1, size_max=5)
