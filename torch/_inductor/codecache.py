6b8e2d05c1b25e24e1c068ee2ca1794ca08beb09
Peter Bell <peterbell10@live.co.uk>
2023-05-08 15:56:09 +0100

    Update on "[inductor] Make codecache file permissions less restrictive"
    `tempfile.mkstemp` creates a file with no permissions given to the
    user's group or to other users on the machine. Whereas the default is
    usually read-write permissions for group members and read-only for
    other users.
    
    This creates an issue in GitHub codespaces for example, where the
    terminal runs under a different user from vs code.
    
    cc soumith voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -229,22 +229,23 @@ def write(source_code, ext, extra=""):
     return basename, path
 
 
-def write_atomic(path: str, source_code: str):
+def get_umask():
+    # os.umask sets-and-gets, so we need to restore the previous value
+    mask = os.umask(0o666)
+    os.umask(mask)
+    return mask
+
+
+def write_atomic(path: str, source_code: str, mode=0o666):
     # use a temp file for thread safety
     fd, tmp_path = tempfile.mkstemp(dir=os.path.dirname(path))
     with os.fdopen(fd, "w") as f:
         f.write(source_code)
 
-    # mkstemp only gives permission to the creating user, reset to default
-    os.chmod(
-        tmp_path,
-        # User
-        stat.S_IRUSR | stat.S_IWUSR |
-        # Group
-        stat.S_IRGRP | stat.S_IWGRP |
-        # Other
-        stat.S_IROTH,
-    )
+    # set file permissions
+    masked_mode = mode & ~get_umask()
+    os.chmod(tmp_path, masked_mode)
+
     # rename atomically
     os.rename(tmp_path, path)
 
