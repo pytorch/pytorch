0e9bf4bf7c2a892249dece65432e8ad1e1665de3
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-05-08 06:15:38 +0000

    [spmd] enhance batch dim analysis of data parallel
    This PR enhances batch dim analysis of data parallel to understand
    more on the cases where batch dim get flattened or split, using
    dtensor's view ops, we could be able to track the batch dim that got
    transformed in non-trival ways.
    
    [ghstack-poisoned]

--- torch/distributed/_spmd/data_parallel.py
+++ torch/distributed/_spmd/data_parallel.py
@@ -12,6 +12,8 @@ import torch.library
 import torch.nn as nn
 
 import torch.utils._pytree as pytree
+
+from torch.distributed._spmd.batch_dim_utils import BatchDimAnalyzer
 from torch.distributed._tensor import DeviceMesh, distribute_tensor, Replicate, Shard
 
 from torch.distributed._tensor._utils import compute_local_shape
@@ -27,7 +29,6 @@ from torch.fx import GraphModule
 from torch.fx.experimental.proxy_tensor import make_fx
 from torch.fx.passes.shape_prop import _extract_tensor_metadata
 from torch.nn.utils._named_member_accessor import NamedMemberAccessor
-
 
 aten = torch.ops.aten
 
@@ -129,131 +130,6 @@ def gradients_tagging(params: Dict[str, torch.Tensor]):
         # remove those hooks after tracing
         for h in tagging_hooks:
             h.remove()
-
-
-class BatchDimAnalyzer:
-    """
-    This class is used to analyze the batch dimension of each tensor/node in the
-    graph. We need to know the batch dimension of each tensor/node so that we know
-    exactly the sharding layout of intermediate tensors.
-
-    We possibly should evaluate using symbolic shapes to track the batch dimension.
-    We can experiment it later with dynamo integration (as dynamo have mark_dynamic
-    API which allows marking batch dimension only) or try to use FakeTensorMode to
-    mark the batch dimension. For now, let's just use the batch dimension of the first
-    input tensor as the hint to track the batch dimension of all tensors/nodes in
-    the graph.
-    """
-
-    def __init__(self, batch_dim: int = 0) -> None:
-        self.batch_dim = batch_dim
-
-        if batch_dim != 0:
-            # TODO: see if this make sense or not
-            raise RuntimeError("Data Parallel only supports batch dim on dimension 0!")
-
-        self.batch_dim_map: Dict[fx.Node, int] = {}
-        # batch dim size is used to track the batch dim size of the input tensor
-        self.batch_dim_size = -1
-
-        # reduction ops that is used to detect whether there's a reduction over batch
-        # dimension operation, if there is, we mark the output as sharded instead of
-        # partial placement
-        self.reduction_ops = [
-            aten.binary_cross_entropy.default,
-            aten.binary_cross_entropy_with_logits.default,
-            aten.mean.default,
-            aten.mse_loss.default,
-            aten.nll_loss_forward.default,
-            aten.soft_margin_loss.default,
-            aten.sum.default,
-        ]
-
-    def init_batch_dim_size(self, batch_dim_size: int) -> None:
-        """
-        initialize batch dim size base on the first input batch size
-        """
-        if self.batch_dim_size != -1 and self.batch_dim_size != batch_dim_size:
-            raise RuntimeError(
-                f"batch dim size is already initialized! "
-                f"Found new batch size: {batch_dim_size} not "
-                f"matching existing batch dim size: {self.batch_dim_size}!"
-            )
-        self.batch_dim_size = batch_dim_size
-
-    def set_batch_dim(self, node: fx.Node, batch_dim: int) -> None:
-        self.batch_dim_map[node] = batch_dim
-
-    def get_batch_dim(self, node: fx.Node) -> int:
-        if node not in self.batch_dim_map:
-            raise RuntimeError(f"batch dim analysis failed on node: {node}!")
-        return self.batch_dim_map[node]
-
-    def compute_batch_dim(self, node: fx.Node, full_reduction=False) -> int:
-        """
-        compute the batch dimension for the `node`
-        """
-        assert self.batch_dim_size != -1, "batch dim size is not initialized!"
-
-        if node in self.batch_dim_map:
-            # if batch dim already computed, simply return it
-            return self.batch_dim_map[node]
-
-        shape = node.meta["val"].shape
-
-        if full_reduction:
-            # if it's a full reduction then we use operand batch dim as
-            # the node's batch dim
-            operand = node.all_input_nodes[0]
-            assert (
-                operand in self.batch_dim_map
-            ), "input have full reduction but not in dim map!"
-            operand_batch_dim = self.get_batch_dim(operand)
-            self.set_batch_dim(node, operand_batch_dim)
-            return operand_batch_dim
-        else:
-            # loop through the dim size to find the batch dim
-            for i, dim_size in enumerate(shape):
-                if dim_size == self.batch_dim_size:
-                    self.set_batch_dim(node, i)
-                    return i
-
-        # if we reach here, it means we failed to find the batch dim
-        raise RuntimeError(f"batch dim analysis failed on node: {node}!")
-
-    def compute_batch_dim_shard_spec(
-        self, node: fx.Node, mesh: DeviceMesh, input_full_reduction: bool = False
-    ) -> Tuple[DTensorSpec, bool]:
-        """
-        This function first compute the batch dimension for the current node,
-        then generate the sharding spec that shards on the batch dimension.
-        """
-        # for reduction op that reduces over the sharded batch dim
-        # we don't generate partial, but rather, we generate shard
-        # This is because the intention of data parallel is to never
-        # do full reduction across batch dimension, it would still
-        # keep the reduction activation as sharded.
-        reduction_over_batch = False
-        shape = node.meta["val"].shape
-        if node.target in self.reduction_ops and len(shape) == 0:
-            operand = node.all_input_nodes[0]
-            if operand in self.batch_dim_map:
-                operand_batch_dim = self.get_batch_dim(operand)
-                if operand_batch_dim == self.batch_dim:
-                    reduction_over_batch = True
-            else:
-                raise RuntimeError(f"batch dim analysis failed on node: {node}!")
-
-        full_reduction = reduction_over_batch or input_full_reduction
-        node_batch_dim = self.compute_batch_dim(node, full_reduction)
-        batch_dim_shard_spec = DTensorSpec(
-            mesh=mesh, placements=[Shard(node_batch_dim)]
-        )
-
-        if full_reduction:
-            batch_dim_shard_spec.from_local = True  # type: ignore[attr-defined]
-
-        return batch_dim_shard_spec, reduction_over_batch
 
 
 def _gen_shard_strategy(
@@ -623,7 +499,6 @@ def build_data_parallel_strategies(
                     raise RuntimeError(
                         f"Unrecognized node: {node} with input node types: {input_node_types}."
                     )
-
         elif node.op == "output":
             dp_strategy_map[node] = DataParallelStrategy(NodeType.NON_TENSOR, [])
         else:
