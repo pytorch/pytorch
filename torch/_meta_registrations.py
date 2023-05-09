fa46045c3144f78961f4a9828149069018fbd7db
Nikita Karetnikov <nikita@karetnikov.org>
2023-05-09 11:45:20 +0200

    [pt2] remove unused `meta_linalg_eigh`
    [ghstack-poisoned]

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -433,18 +433,6 @@ def meta__linalg_eigh(A: Tensor, UPLO: str = "L", compute_v: bool = True):
     return eigenvalues, eigenvectors
 
 
-# @register_meta(aten.linalg_eigh.default)
-def meta_linalg_eigh(self, uplo="L"):
-    squareCheckInputs(self, "linalg_eigh")
-    checkUplo(uplo)
-    real_dtype = toRealValueType(self.dtype)
-    assert self.dim() >= 2
-    values = self.new_empty(self.shape, dtype=real_dtype)
-    values.transpose_(-2, -1)
-    vectors = self.new_empty(self.shape[:-1])
-    return (values, vectors)
-
-
 # From aten/src/ATen/native/BatchLinearAlgebra.cpp
 @register_meta(aten.linalg_cholesky_ex.default)
 def linalg_cholesky_ex(A: Tensor, upper: bool = False, check_errors: bool = False):
