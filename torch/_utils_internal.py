cfe4b21c239a7fa22a336524a4a68977b938f41b
Sergei Vorobev <sergei.vorobev@getcruise.com>
2023-05-09 19:39:29 +0000

    monkey-patch

--- torch/_utils_internal.py
+++ torch/_utils_internal.py
@@ -67,7 +67,7 @@ TEST_MASTER_ADDR = "127.0.0.1"
 TEST_MASTER_PORT = 29500
 # USE_GLOBAL_DEPS controls whether __init__.py tries to load
 # libtorch_global_deps, see Note [Global dependencies]
-USE_GLOBAL_DEPS = False
+USE_GLOBAL_DEPS = True
 # USE_RTLD_GLOBAL_WITH_LIBTORCH controls whether __init__.py tries to load
 # _C.so with RTLD_GLOBAL during the call to dlopen.
 USE_RTLD_GLOBAL_WITH_LIBTORCH = False
--- torch/version.py.tpl
+++ torch/version.py.tpl
@@ -3,3 +3,9 @@ debug = False
 cuda = '{{CUDA_VERSION}}'
 git_version = '{{GIT_VERSION}}'
 hip = None
+
+# This is a gross monkey-patch hack that depends on the order of imports
+# in torch/__init__.py
+# TODO: find a more elegant solution to set `USE_GLOBAL_DEPS` for the bazel build
+import torch
+torch.USE_GLOBAL_DEPS = False
