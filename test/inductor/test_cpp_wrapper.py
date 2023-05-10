75c192dc641e6ec9df8737794831aac6977bddd6
chunyuan <chunyuan.wu@intel.com>
2023-05-10 07:17:37 +0000

    Update base for Update on "Inductor cpp wrapper: support ConvUnary and ConvBinary"
    TODO:
    address https://github.com/pytorch/pytorch/pull/90755#discussion_r1153917178
    Move assert_size_and_stride to a common place
    Fix `test_convolution1`
    
    - Support ConvUnary and ConvBinary in cpp wrapper.
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10 soumith voznesenskym yanboliang penguinwu anijain2305 EikanWang Guobing-Chen zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 desertfire
    
    [ghstack-poisoned]

--- test/inductor/test_cpp_wrapper.py
+++ test/inductor/test_cpp_wrapper.py
@@ -57,7 +57,7 @@ def make_test_case(name, device, tests, condition=True):
             func = getattr(tests, test_name)
             assert callable(func), "not a callable"
             code = test_torchinductor.run_and_get_cpp_code(func)
-            self.assertEqual("load_inline" in code, True)
+            self.assertEqual("CppWrapperCodeCache" in code, True)
         finally:
             tests.tearDown()
             tests.tearDownClass()
