6f91553200e95fe864b77d376fb4f8c3f9fc4df4
Daniel Dale <danny.dale@gmail.com>
2023-05-10 12:49:48 -0700

    clarify subtest configuration with `NamedTuple` s and minimize test class attributes

--- test/distributed/_composable/fully_shard/test_fully_shard_mixed_precision.py
+++ test/distributed/_composable/fully_shard/test_fully_shard_mixed_precision.py
@@ -1,7 +1,7 @@
 # Owner(s): ["oncall: distributed"]
 
 import sys
-from typing import Dict
+from typing import Dict, NamedTuple, Optional
 
 import torch
 import torch.distributed as dist
@@ -31,57 +31,58 @@ if TEST_WITH_DEV_DBG_ASAN:
 class TestMixedPrecision(FSDPTest):
     """Tests ``fully_shard`` with mixed precision."""
 
+    class SubtestKey(NamedTuple):
+        cast_root_forward_inputs: bool
+        cast_forward_inputs_submodule: bool
+        use_root_no_params: bool
+
+    class SubtestResult(NamedTuple):
+        subtest_alias: str
+        model_dtype: torch.dtype = torch.float16
+        c1_dtype: torch.dtype = torch.float16
+        c2_dtype: Optional[torch.dtype] = torch.float16
+
     EXPECTED_CAST_DTYPES = {
-        # (cast_root_forward_inputs_submodule, cast_forward_inputs_submodule, use_root_no_params)
-        (True, True, True): (
-            "cast_root_cast_child_no_root_params",
-            torch.float16,
-            torch.float16,
-            torch.float16,
+        SubtestKey(True, True, True): SubtestResult(
+            "cast_root_cast_child_no_root_params"
         ),
-        (True, True, False): (
+        SubtestKey(True, True, False): SubtestResult(
             "cast_root_cast_child_root_params",
-            torch.float32,
-            torch.float32,
-            torch.float16,
+            model_dtype=torch.float32,
+            c1_dtype=torch.float32,
         ),
-        (True, False, True): (
-            "cast_root_no_cast_child_no_root_params",
-            torch.float16,
-            torch.float16,
-            torch.float16,
+        SubtestKey(True, False, True): SubtestResult(
+            "cast_root_no_cast_child_no_root_params"
         ),
-        (True, False, False): (
+        SubtestKey(True, False, False): SubtestResult(
             "cast_root_no_cast_child_root_params",
-            torch.float32,
-            torch.float32,
-            torch.float16,
+            model_dtype=torch.float32,
+            c1_dtype=torch.float32,
         ),
-        (False, True, True): (
-            "no_cast_root_cast_child_no_root_params",
-            torch.float32,
-            torch.float16,
-            torch.float16,
+        SubtestKey(False, True, True): SubtestResult(
+            "no_cast_root_cast_child_no_root_params", model_dtype=torch.float32
         ),
-        (False, True, False): (
+        SubtestKey(False, True, False): SubtestResult(
             "no_cast_root_cast_child_root_params",
-            torch.float32,
-            torch.float32,
-            torch.float16,
+            model_dtype=torch.float32,
+            c1_dtype=torch.float32,
         ),
-        (False, False, True): (
+        SubtestKey(False, False, True): SubtestResult(
             "no_cast_root_no_cast_child_no_root_params",
-            torch.float32,
-            torch.float32,
-            None,
+            model_dtype=torch.float32,
+            c1_dtype=torch.float32,
+            c2_dtype=None,
         ),
-        # (False, False, True): ('no_cast_root_no_cast_child_no_root_params',
-        #                        torch.float32, torch.float32, torch.float32),  # expected result after eval mode fix
-        (False, False, False): (
+        # SubtestKey(False, False, True): SubtestResult(
+        #     "no_cast_root_no_cast_child_no_root_params",
+        #     model_dtype=torch.float32,
+        #     c1_dtype=torch.float32,
+        #     c2_dtype=torch.float32), # expected result after eval mode fix
+        SubtestKey(False, False, False): SubtestResult(
             "no_cast_root_no_cast_child_root_params",
-            torch.float32,
-            torch.float32,
-            torch.float32,
+            model_dtype=torch.float32,
+            c1_dtype=torch.float32,
+            c2_dtype=torch.float32,
         ),
     }
 
@@ -106,54 +107,44 @@ class TestMixedPrecision(FSDPTest):
         cast_forward_inputs_submodule: bool,
         use_root_no_params: bool,
     ):
-        self.cast_root_forward_inputs_submodule = cast_root_forward_inputs_submodule
-        self.cast_forward_inputs_submodule = cast_forward_inputs_submodule
-        x, fsdp = self._input_and_model_init(use_root_no_params=use_root_no_params)
+        cast_forward_cfg = (
+            cast_root_forward_inputs_submodule,
+            cast_forward_inputs_submodule,
+        )
+        x, fsdp = self._input_and_model_init(*cast_forward_cfg, use_root_no_params)
         # self._validate_eval(x, fsdp)
-        self._backward_or_validate_error(x, fsdp)
-        self._assert_expected_dtypes(use_root_no_params)
+        self._backward_or_validate_error(x, fsdp, *cast_forward_cfg)
+        self._assert_expected_dtypes(fsdp, *cast_forward_cfg, use_root_no_params)
 
-    def _backward_or_validate_error(
-        self, input: Dict[nn.Module, torch.Tensor], fsdp_model: nn.Module
+    def _input_and_model_init(
+        self,
+        cast_root_forward_inputs_submodule: bool,
+        cast_forward_inputs_submodule: bool,
+        use_root_no_params: bool,
     ):
-        # cast_root_forward_inputs_submodule or cast_forward_inputs_submodule should be True
-        if (
-            not self.cast_root_forward_inputs_submodule
-            and not self.cast_forward_inputs_submodule
-        ):
-            with self.assertRaisesRegex(
-                RuntimeError,
-                "mat1 and mat2 must have the same dtype",
-            ):
-                fsdp_model(input).sum().backward()
-        else:
-            fsdp_model(input).sum().backward()
-
-    def _input_and_model_init(self, use_root_no_params: bool):
-        self.forward_inputs: Dict[nn.Module, torch.Tensor] = {}
+        forward_inputs: Dict[nn.Module, torch.Tensor] = {}
         float16 = MixedPrecision(
             param_dtype=torch.float16,
-            cast_root_forward_inputs=self.cast_root_forward_inputs_submodule,
-            cast_forward_inputs=self.cast_forward_inputs_submodule,
+            cast_root_forward_inputs=cast_root_forward_inputs_submodule,
+            cast_forward_inputs=cast_forward_inputs_submodule,
         )
 
-        self.model = SaveForwardInputsModel(
-            forward_inputs=self.forward_inputs,
+        model = SaveForwardInputsModel(
+            forward_inputs=forward_inputs,
             cast_forward_inputs=False,
         ).cuda()
-        self.c1, self.c2 = self.model.c1, self.model.c2
         x = torch.zeros(2, 100, device="cuda")
 
         # float16 on at least one submodule
-        self.model.c2 = fully_shard(self.model.c2, mixed_precision=float16)
+        model.c2 = fully_shard(model.c2, mixed_precision=float16)
 
         # if not `use_root_no_params`, one float32 module will be left for root
         if use_root_no_params:
             # use float16 and wrap all submodules, leaving root without direct parameters
-            self.model.c1 = fully_shard(self.model.c1, mixed_precision=float16)
-            fsdp = fully_shard(self.model, mixed_precision=float16)
+            model.c1 = fully_shard(model.c1, mixed_precision=float16)
+            fsdp = fully_shard(model, mixed_precision=float16)
         else:
-            fsdp = fully_shard(self.model)
+            fsdp = fully_shard(model)
         return x, fsdp
 
     def _validate_eval(
@@ -162,27 +153,50 @@ class TestMixedPrecision(FSDPTest):
         # validate eval mode always forces full precision
         fsdp_model.eval()
         _ = fsdp_model(input)
-        self.assertEqual(self.forward_inputs[self.c1].dtype, torch.float32)
-        self.assertEqual(self.forward_inputs[self.c2].dtype, torch.float32)
+        self.assertEqual(fsdp_model.forward_inputs[fsdp_model.c1].dtype, torch.float32)
+        self.assertEqual(fsdp_model.forward_inputs[fsdp_model.c2].dtype, torch.float32)
         fsdp_model.train()
 
-    def _assert_expected_dtypes(self, use_root_no_params):
-        result_key = (
-            self.cast_root_forward_inputs_submodule,
-            self.cast_forward_inputs_submodule,
+    def _backward_or_validate_error(
+        self,
+        input: Dict[nn.Module, torch.Tensor],
+        fsdp_model: nn.Module,
+        cast_root_forward_inputs_submodule: bool,
+        cast_forward_inputs_submodule: bool,
+    ):
+        # cast_root_forward_inputs_submodule or cast_forward_inputs_submodule should be True
+        if not cast_root_forward_inputs_submodule and not cast_forward_inputs_submodule:
+            with self.assertRaisesRegex(
+                RuntimeError,
+                "mat1 and mat2 must have the same dtype",
+            ):
+                fsdp_model(input).sum().backward()
+        else:
+            fsdp_model(input).sum().backward()
+
+    def _assert_expected_dtypes(
+        self,
+        fsdp_model: nn.Module,
+        cast_root_forward_inputs_submodule: bool,
+        cast_forward_inputs_submodule: bool,
+        use_root_no_params: bool,
+    ):
+        subtest_key = TestMixedPrecision.SubtestKey(
+            cast_root_forward_inputs_submodule,
+            cast_forward_inputs_submodule,
             use_root_no_params,
         )
-        subtest_fail_msg = f"Subtest `{TestMixedPrecision.EXPECTED_CAST_DTYPES[result_key][0]}` failed."
+        subtest_fail_msg = f"Subtest `{TestMixedPrecision.EXPECTED_CAST_DTYPES[subtest_key].subtest_alias}` failed."
         self.assertEqual(
-            self.forward_inputs[self.model].dtype,
-            TestMixedPrecision.EXPECTED_CAST_DTYPES[result_key][1],
+            fsdp_model.forward_inputs[fsdp_model].dtype,
+            TestMixedPrecision.EXPECTED_CAST_DTYPES[subtest_key].model_dtype,
             msg=subtest_fail_msg,
         )
-        for i, mod in enumerate((self.c1, self.c2), start=2):
-            if self.forward_inputs.get(mod, None) is not None:
+        for i, mod in enumerate((fsdp_model.c1, fsdp_model.c2), start=2):
+            if fsdp_model.forward_inputs.get(mod, None) is not None:
                 self.assertEqual(
-                    self.forward_inputs[mod].dtype,
-                    TestMixedPrecision.EXPECTED_CAST_DTYPES[result_key][i],
+                    fsdp_model.forward_inputs[mod].dtype,
+                    TestMixedPrecision.EXPECTED_CAST_DTYPES[subtest_key][i],
                     msg=subtest_fail_msg,
                 )
 
