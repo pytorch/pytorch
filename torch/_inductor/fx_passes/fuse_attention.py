a1874008ef22d9a2a5bdf17cfd6f4284d53fad20
Natalia Gimelshein <ngimel@fb.com>
2023-05-11 01:00:32 +0000

    pattern without dropout

--- torch/_inductor/fx_passes/fuse_attention.py
+++ torch/_inductor/fx_passes/fuse_attention.py
@@ -168,6 +168,32 @@ def _sfdp_replacement_7(query, key, value, dropout_p):
         is_causal=False,
     )
 
+def _sfdp_pattern_8(query, key, value):
+    q = query.permute(0, 2, 1, 3)
+    k = key.permute(0, 2, 1, 3)
+    v = value.permute(0, 2, 1, 3)
+    div = q @ k.transpose(-2, -1) / math.sqrt(q.size(-1))
+    div = div.to(torch.float32)
+    attn_weight = torch.softmax(div, dim=-1)
+    attn_weight = attn_weight.to(torch.float16)
+    return attn_weight @ v
+
+
+def _sfdp_replacement_8(query, key, value):
+    counters["inductor"]["fuse_attention"] += 1
+    q = query.permute(0, 2, 1, 3)
+    k = key.permute(0, 2, 1, 3)
+    v = value.permute(0, 2, 1, 3)
+    return aten.scaled_dot_product_attention(
+        q,
+        k,
+        v,
+        attn_mask=None,  # attn_mask,
+        dropout_p=0.,
+        is_causal=False,
+    )
+
+
 
 # TODO(jansel): add more patterns based on what we see in real models
 # TODO(jansel): make these pattern work with lowmem_dropout=True
@@ -279,7 +305,8 @@ def _sfdp_init():
             d,
             _sfdp_params_check,
         ),
-        (_sfdp_pattern_7, _sfdp_replacement_7, [gp(), gp(), gp()], d, _return_true),
+        (_sfdp_pattern_7, _sfdp_replacement_7, [gp(), gp(), gp()], d, _sfdp_params_check),
+        (_sfdp_pattern_8, _sfdp_replacement_8, [gp(), gp(), gp()], {},_sfdp_params_check),
     ]:
         args = [*args, *workaround.values()]
         register_replacement(
