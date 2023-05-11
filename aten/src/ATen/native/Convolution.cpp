a397815335c1abd11883044f2b6d82e6f2467b69
cyy <cyyever@outlook.com>
2023-05-11 09:02:05 +0800

    put function inside proper macros

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -769,6 +769,7 @@ static void check_input_same_type_as_parameters(
   check_input_same_type_as_parameters(input, weight, /*bias=*/ Tensor());
 }
 
+#if AT_MKLDNN_ENABLED()
 static void check_input_same_type_as_parameters(
     const Tensor& input,
     const Tensor& weight,
@@ -787,6 +788,7 @@ static void check_input_same_type_as_parameters(
     check_input_same_type_as_parameters(input, weight, bias);
   }
 }
+#endif
 
 static auto view4d(const at::Tensor& tensor) -> at::Tensor {
   TORCH_CHECK(tensor.ndimension() == 3,
