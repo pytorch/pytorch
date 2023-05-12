6906fbd67833538a71545eb8d79258bd6467a750
Michael Voznesensky <voznesenskym@gmail.com>
2023-05-12 03:03:12 +0000

    Update on "[WIP] Rewrite frame state to use a strict for shapes, splitting scalar and size, prep for stride"
    cc soumith penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx desertfire
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -899,7 +899,7 @@ class VariableBuilder:
                 # know if bare integers are actually going to be sizevars
                 # and it is inappropriate to eagerly duck size them with
                 # real sizevars
-                if frame_state_entry.size is None or not config.assume_static_by_default:
+                if frame_state_entry.scalar is None or not config.assume_static_by_default:
                     dynamic_dim = DimDynamic.DYNAMIC
                 else:  # assume_static_by_default
                     # TODO: dynamic_dim = DimDynamic.STATIC should work but
