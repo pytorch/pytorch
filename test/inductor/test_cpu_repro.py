6f7ebcdcd82afa7355e002c6e2b01f8da802db9b
Jiong Gong <jiong.gong@intel.com>
2023-05-13 06:25:37 +0000

    [inductor] enable descriptive name for cpp kernels (#101330)
    This PR enables the descriptive name for cpp kernels similar to the triton kernel name. A new configuration `config.cpp.descriptive_names` is added similar to that of triton. The kernel name follows the format: `cpp_<fused_name>_<id>`.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101330
    Approved by: https://github.com/XiaobingSuper, https://github.com/jansel

--- test/inductor/test_cpu_repro.py
+++ test/inductor/test_cpu_repro.py
@@ -1327,6 +1327,7 @@ class CPUReproTests(TestCase):
     )
     @patch("torch.cuda.is_available", lambda: False)
     @config.patch({"cpp.enable_kernel_profile": True})
+    @config.patch({"cpp.descriptive_names": "original_aten"})
     def test_cpp_kernel_profile(self):
         from torch.profiler import profile
 
@@ -1341,7 +1342,7 @@ class CPUReproTests(TestCase):
 
         kernel_profile_events = []
         for e in prof.profiler.function_events:
-            if "kernel_cpp_0" in e.name:
+            if "cpp_fused_add_0" in e.name:
                 kernel_profile_events.append(e.name)
         assert len(kernel_profile_events) > 0
 
