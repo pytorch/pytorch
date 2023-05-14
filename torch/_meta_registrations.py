f436ac99f71599dc0cd6d68f12e1c5e1ea30ae43
Nikita Karetnikov <nikita@karetnikov.org>
2023-05-14 19:49:34 +0200

    [pt2] add meta for `linalg_ldl_factor_ex` (#101362)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101362
    Approved by: https://github.com/lezcano

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -543,6 +543,27 @@ def linalg_inv_ex_meta(A: Tensor, check_errors: bool = False):
     return L, infos
 
 
+@register_meta([aten.linalg_ldl_factor_ex.default, aten.linalg_ldl_factor_ex.out])
+@out_wrapper("LD", "pivots", "info")
+def linalg_ldl_factor_ex_meta(
+    self: Tensor,
+    *,
+    hermitian: bool = False,
+    check_errors: bool = False,
+) -> Tuple[Tensor, Tensor, Tensor]:
+    squareCheckInputs(self, "torch.linalg.ldl_factor_ex")
+    checkFloatingOrComplex(self, "torch.linalg.ldl_factor_ex")
+    LD = torch.empty_strided(
+        size=self.shape,
+        stride=make_contiguous_strides_for(self.shape, row_major=False),
+        dtype=self.dtype,
+        device=self.device,
+    )
+    pivots = self.new_empty(self.shape[:-1], dtype=torch.int)
+    info = self.new_empty(self.shape[:-2], dtype=torch.int)
+    return LD, pivots, info
+
+
 # parse the "mode" param in linalg_qr: return a tuple of bools (compute_q, reduced)
 def _parse_qr_mode(mode: str) -> Tuple[bool, bool]:
     if mode == "reduced":
