62efc6708db3f154c43f492e673a92cf3a9bf08d
XiaobingSuper <xiaobing.zhang@intel.com>
2023-05-14 21:27:36 -0400

    inductor: add dtype check before doing cpu binary fusion (#101376)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101376
    Approved by: https://github.com/EikanWang, https://github.com/jgong5, https://github.com/jansel

--- test/inductor/test_mkldnn_pattern_matcher.py
+++ test/inductor/test_mkldnn_pattern_matcher.py
@@ -429,6 +429,21 @@ class TestPaternMatcher(TestCase):
                 out = torch.add(out, out)
                 return out
 
+        # https://github.com/pytorch/pytorch/issues/101374.
+        # we can't do the fusion when add's inputs are mixed dtype.
+        class Model3(torch.nn.Module):
+            def __init__(self):
+                super(Model3, self).__init__()
+                self.conv = torch.nn.Conv2d(
+                    in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1
+                )
+
+            def forward(self, x):
+                temp = self.conv(x)
+                other = torch.ones(temp.shape, dtype=torch.double)
+                out = torch.add(temp, other)
+                return out
+
         input = torch.randn(1, 3, 28, 28).to(memory_format=torch.channels_last)
         others = [
             torch.randn(1, 32, 28, 28).to(memory_format=torch.channels_last),
@@ -446,6 +461,9 @@ class TestPaternMatcher(TestCase):
             self._test_code_common(mod, (input, other, alpha), include_ops, exclude_ops)
         # case2:
         mod = Model2().to(memory_format=torch.channels_last).eval()
+        self._test_code_common(mod, (input,), include_ops, exclude_ops)
+        # case3:
+        mod = Model3().to(memory_format=torch.channels_last).eval()
         self._test_code_common(mod, (input,), include_ops, exclude_ops)
 
     def test_reproduce_99842_issue(self):
