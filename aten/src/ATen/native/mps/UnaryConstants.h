cf855d5a97e6f177bd687a06a8b3e8941e5ae8c0
Peter Pham <peterpham86@gmail.com>
2023-05-17 08:31:18 -0700

    update kernel to use erfinv constants directly as part of kernel bypassing object-c variable to device mem copy

--- aten/src/ATen/native/mps/UnaryConstants.h
+++ aten/src/ATen/native/mps/UnaryConstants.h
@@ -1,25 +1,5 @@
 #pragma once
 
-struct ErfinvConstant {
-  float a[4];
-  float b[4];
-  float c[4];
-  float d[2];
-  constexpr ErfinvConstant(
-    float a0, float a1, float a2, float a3,
-    float b0, float b1, float b2, float b3,
-    float c0, float c1, float c2, float c3,
-    float d0, float d1)
-    :a{a0, a1, a2, a3}, b{b0, b1, b2, b3}, c{c0, c1, c2, c3}, d{d0, d1} {}
-};
-
-constexpr ErfinvConstant erfinv_constant(
-  0.886226899, -1.645349621, 0.914624893, -0.140543331,
-  -2.118377725, 1.442710462, -0.329097515, 0.012229801,
-  -1.970840454, -1.624906493, 3.429567803, 1.641345311,
-  3.543889200, 1.637067800
-);
-
 const char* UNARY_KERNEL_TEMPLATE = R"METAL(
 #include <metal_stdlib>
 using namespace metal;
@@ -29,18 +9,24 @@ struct ErfinvConstant {{
   float b[4];
   float c[4];
   float d[2];
-
 }};
+
+constant ErfinvConstant erfinv_constant = {{
+  {{0.886226899, -1.645349621, 0.914624893, -0.140543331}},
+  {{-2.118377725, 1.442710462, -0.329097515, 0.012229801}},
+  {{-1.970840454, -1.624906493, 3.429567803, 1.641345311}},
+  {{3.543889200, 1.637067800}}
+}};
+
 
 kernel void erfinv_mps_kernel( device {0} *output [[buffer(0)]],
                             device {1} *input [[buffer(1)]],
-                            constant ErfinvConstant& uniforms [[buffer(2)]],
                             uint index [[thread_position_in_grid]]) {{
 
-  constant const float *a = uniforms.a;
-  constant const float *b = uniforms.b;
-  constant const float *c = uniforms.c;
-  constant const float *d = uniforms.d;
+  constant const float *a = erfinv_constant.a;
+  constant const float *b = erfinv_constant.b;
+  constant const float *c = erfinv_constant.c;
+  constant const float *d = erfinv_constant.d;
   float y = input[index];
   float x, z, num, dem; /*working variables */
   /* coefficients in rational expansion */
--- aten/src/ATen/native/mps/operations/UnaryKernel.mm
+++ aten/src/ATen/native/mps/operations/UnaryKernel.mm
@@ -121,11 +121,6 @@ TORCH_IMPL_FUNC(erfinv_out_mps)(const Tensor& self, const Tensor& output_) {
       [computeEncoder setBuffer:outBuf offset:0 atIndex:0];
       [computeEncoder setBuffer:inputBuf offset:0 atIndex:1];
 
-      id<MTLBuffer> erfinvConstant = [device newBufferWithLength:sizeof(ErfinvConstant)
-                                                         options:MTLResourceStorageModePrivate];
-      memcpy([erfinvConstant contents], &erfinv_constant, sizeof(ErfinvConstant));
-      [computeEncoder setBuffer:erfinvConstant offset:0 atIndex:2];
-
       MTLSize gridSize = MTLSizeMake(length, 1, 1);
       uint32_t maxThreadsPerGroup = [cplState maxTotalThreadsPerThreadgroup];
 
