0cb1b689de56ed1da5de5cb0d958c1a1dbbe4d02
Nikita Shulga <nshulga@meta.com>
2023-05-18 09:50:36 -0700

    Fix `test_guard_function_builder` on Python-3.8
    If `astunparse` is not installed, following guard will be generated in `test_guard_function_builder_with_cse`:
    ```python
    def ___make_guard_fn():
        def guard(L):
            if not (x[0].a < x[1].a * (3 - x[2].a)):
                return False
            if not (a.b.c[0].d.e + a.b.c[1].d.e * a.b.c[2].d.e > 0):
                return False
            if not (f(m.n[0], '0').x.y.z * f(m.n[0], '1').x.y.z * f(m.n[0], '2').x.y.z < 512):
                return False
            if not (self.g(a, b).k + (1 - self.g(a, b).k) <= m[0].a + self.g(a, b).k):
                return False
            return True
        return guard
    ```
    
    Though, I have to say, hardcoding string comparison is pretty weird

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -5366,8 +5366,27 @@ def ___make_guard_fn():
         return True
     return guard
 """
+        expected_38_no_astunparse = """\
+def ___make_guard_fn():
+    def guard(L):
+        if not (x[0].a < x[1].a * (3 - x[2].a)):
+            return False
+        if not (a.b.c[0].d.e + a.b.c[1].d.e * a.b.c[2].d.e > 0):
+            return False
+        if not (f(m.n[0], '0').x.y.z * f(m.n[0], '1').x.y.z * f(m.n[0], '2').x.y.z < 512):
+            return False
+        if not (self.g(a, b).k + (1 - self.g(a, b).k) <= m[0].a + self.g(a, b).k):
+            return False
+        return True
+    return guard
+"""
 
-        expected = expected_38 if self._is_py38() else expected
+        if self._is_py38():
+            expected = (
+                expected_38
+                if "astunparse" in sys.modules
+                else expected_38_no_astunparse
+            )
         self.assertEqual(expected, pycode)
 
 
