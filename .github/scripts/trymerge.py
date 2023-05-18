7af998788ba53ef826cccc423fa0e93f09895bf0
Santiago Castro <bryant1410@gmail.com>
2023-05-18 15:48:36 -0400

    Fix trymerge.py

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -1261,9 +1261,11 @@ def find_matching_merge_rule(
             if num_matching_files > reject_reason_score:
                 reject_reason_score = num_matching_files
                 reject_reason = "\n".join(
-                    f"Not all files match rule `{rule_name}`.",
-                    f"{num_matching_files} files matched, but there are still non-matching files:",
-                    f"{','.join(non_matching_files[:5])}{', ...' if len(non_matching_files) > 5 else ''}",
+                    (
+                        f"Not all files match rule `{rule_name}`.",
+                        f"{num_matching_files} files matched, but there are still non-matching files:",
+                        f"{','.join(non_matching_files[:5])}{', ...' if len(non_matching_files) > 5 else ''}",
+                    )
                 )
             continue
 
