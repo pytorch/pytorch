dc64e8502f34c1073b6fa51e5bb9bb5941b189e8
Rohan Varma <rvarm1@fb.com>
2023-05-22 14:30:12 -0700

    [FSDP] Start to generalize modules to ignore for mixed precision
    The main use case here is that folks would like to ignore layer norm for mixed precision. This can now be enabled with:
    
    ```
    mp_config = MixedPrecision(
                param_dtype=torch.float16,
                reduce_dtype=torch.float16,
                buffer_dtype=torch.float16,
                _mixed_precision_module_classes_to_ignore=[_BatchNorm, nn.LayerNorm],
            )
    ```
    
    for LayerNorm, we also add module pre and post hooks to cast inputs to the appropriate full precision. This is not super generalizable at the moment, for example, if users want to skip module X, we won't install the same hooks for X, but we perhaps should. This can be done as follow up work for now as the main priority is to unblock layer norm skipping.
    
    Differential Revision: [D46079957](https://our.internmc.facebook.com/intern/diff/D46079957/)
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/_utils.py
+++ torch/distributed/fsdp/_utils.py
@@ -2,12 +2,46 @@ from typing import cast
 
 import torch
 from torch.nn.modules.batchnorm import _BatchNorm
+from torch.nn.modules.normalization import LayerNorm
 from torch.utils._mode_utils import no_dispatch
+
+from torch.distributed.utils import _apply_to_tensors
+from functools import partial
 
 
 def _contains_batchnorm(module):
     return any(isinstance(mod, _BatchNorm) for mod in module.modules())
 
+
+def _contains_module(root, module_to_check):
+    return any(isinstance(mod, module_to_check) for mod in root.modules())
+
+def _override_module_mixed_precision(root, module_to_override, wrap_override_dict={"mixed_precision": None}):
+    for mod in root.modules():
+        if isinstance(mod, module_to_override):
+            mod._wrap_overrides = wrap_override_dict  # type: ignore[assignment]
+
+        if isinstance(mod, LayerNorm): # TODO: generalize this logic if additional types need to be supported
+
+            old_dtype = None
+
+            def cast_fn(dtype, x: torch.Tensor) -> torch.Tensor:
+                if not torch.is_floating_point(x) or x.dtype == dtype:
+                    return x
+                nonlocal old_dtype
+                old_dtype = x.dtype
+                return x.to(dtype)
+
+            def ln_forward_pre_hook(module, args):
+                return _apply_to_tensors(partial(cast_fn, torch.float32), args)
+
+            def ln_forward_post_hook(module, args, output):
+                nonlocal old_dtype
+                if old_dtype is not None:
+                    return _apply_to_tensors(partial(cast_fn, old_dtype), output)
+
+            mod.register_forward_pre_hook(ln_forward_pre_hook)
+            mod.register_forward_hook(ln_forward_post_hook)
 
 def _override_batchnorm_mixed_precision(module):
     for mod in module.modules():
--- torch/distributed/fsdp/api.py
+++ torch/distributed/fsdp/api.py
@@ -5,8 +5,9 @@ constructor arguments.
 
 from dataclasses import dataclass
 from enum import auto, Enum
+from torch.nn.modules.batchnorm import _BatchNorm
 
-from typing import Optional
+from typing import Optional, Sequence
 
 import torch
 
@@ -133,6 +134,11 @@ class MixedPrecision:
             arguments and keyword arguments to ``param_dtype`` for the root FSDP instance.
             It takes precedence over ``cast_forward_inputs`` for the root FSDP instance.
             (Default: ``True``)
+        _mixed_precision_module_classes_to_ignore: (Sequence[type]): Module classes to ignore
+            for mixed precision. This will make the specified nn.Module types ignore mixed precision,
+            by wrapping them in their own FSDP unit and setting mixed_precision=None. Note that this
+            implies the ultimate wrapping of your FSDP module will be different than what the policy
+            specifies. Note that this API is experimental and subject to change.
 
     .. note:: This API is experimental and subject to change.
 
@@ -207,6 +213,8 @@ class MixedPrecision:
     keep_low_precision_grads: bool = False
     cast_forward_inputs: bool = False
     cast_root_forward_inputs: bool = True
+    _mixed_precision_module_classes_to_ignore: Optional[Sequence[type]] = (_BatchNorm,)
+
 
 
 @dataclass
