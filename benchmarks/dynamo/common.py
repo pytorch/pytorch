d7d8e56a155dd246dac0b9c97f50455783d7dd55
Bin Bao <binbao@fb.com>
2023-05-22 20:04:11 +0000

    Update on "[CI] Remove mobilenet_v3_large from NONDETERMINISTIC"
    Summary: The nondeterministic behavior should be gone after
    https://github.com/pytorch/pytorch/pull/99851.
    
    cc voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx
    
    [ghstack-poisoned]

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -230,7 +230,6 @@ CI_SKIP[CI("inductor", training=True)] = [
     "Background_Matting",  # fp64_OOM
     "dlrm",  # Fails on CI - unable to repro locally
     "hf_T5_base",  # accuracy
-    "mobilenet_v3_large",  # accuracy
     "resnet50_quantized_qat",  # Eager model failed to run
     "crossvit_9_240",  # fails to run on timm 0.8.22 with cudagraphs, mempools
     "deit_base_distilled_patch16_224",  # fails to run in timm 0.8.22, cudagraphs
