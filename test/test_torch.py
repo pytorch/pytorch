2b031ec94a5e99e01c67660ac10ab072c2180c27
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2023-05-22 01:12:04 +0000

    fix lint error

--- test/test_torch.py
+++ test/test_torch.py
@@ -6868,7 +6868,7 @@ class TestTorch(TestCase):
         swapped_8bytes = [7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8]
         swapped_4bytes = [3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12]
         swapped_2bytes = [1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14]
-        swapped_1byte  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        swapped_1byte = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
         storage = torch.storage.TypedStorage(input, dtype=torch.uint8)._untyped_storage
 
