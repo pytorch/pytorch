bad414ef193aef32f46829b4906c53c88912bec5
Han Qi <hanq@google.com>
2023-05-22 22:01:00 -0700

    Replace _dynamo.config with an dataclass object
    Summary:
    
    Currently _dynamo.config (and friends) is written as module but
    is an object with its data backed by a dictionary. This change
    makes it a dataclass instead.
    
    Nested namespace via class (`class cpp:` etc in _inductor/config.py)
    are also becoming dataclass.
    
    For now, the top level dataclass generated dynamically from the module
    attr declarations. Next step is to also remove that and just use a
    normal dataclass.
    
    The goal of having dataclasses for config is that there is a name for
    the config class, and therefore we can write functions that take those
    configs as input.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    reset test files

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -1,149 +1,157 @@
+import dataclasses
 import os
 import sys
+from typing import Any, Dict, Optional, Tuple
 
 import torch
+from torch._config_utils import ConfigMixin
 
 # add some debug printouts
-debug = False
+debug: bool = False
 
 # Whether to disable a progress bar for autotuning
-disable_progress = True
+disable_progress: bool = True
 
 # Whether to enable printing the source code for each future
-verbose_progress = False
+verbose_progress: bool = False
 
 # use cpp wrapper instead of python wrapper
-cpp_wrapper = False
+cpp_wrapper: bool = False
 
 # dead code elimination
-dce = False
+dce: bool = False
 
 # assume weight tensors are fixed size
-static_weight_shapes = True
+static_weight_shapes: bool = True
 
 # put correctness assertions in generated code
-size_asserts = os.environ.get("TORCHINDUCTOR_SIZE_ASSERTS", "1") == "1"
+size_asserts: bool = os.environ.get("TORCHINDUCTOR_SIZE_ASSERTS", "1") == "1"
 
 # enable loop reordering based on input orders
-pick_loop_orders = True
+pick_loop_orders: bool = True
 
 # generate inplace computations
-inplace_buffers = True
+inplace_buffers: bool = True
 
 # allow reusing buffers for more efficient memory use
-allow_buffer_reuse = True
+allow_buffer_reuse: bool = True
 
 # codegen benchmark harness
-benchmark_harness = True
+benchmark_harness: bool = True
 
 # fuse pointwise into templates
-epilogue_fusion = True
+epilogue_fusion: bool = True
 
 # do epilogue fusions before other fusions
-epilogue_fusion_first = False
+epilogue_fusion_first: bool = False
 
 # enable pattern match+replace optimizations
-pattern_matcher = True
+pattern_matcher: bool = True
 
 # Optimize away split cat patterns (Experimental)
-split_cat_fx_passes = True
+split_cat_fx_passes: bool = True
 
 # enable reordering pass
-reordering = True
+reordering: bool = True
 
 # enable slow autotuning passes to select algorithms
-max_autotune = os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE") == "1"
+max_autotune: bool = os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE") == "1"
 
 # enable slow autotuning passes to select pointwise/reductions algorithms
-max_autotune_pointwise = os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE_POINTWISE") == "1"
+max_autotune_pointwise: bool = (
+    os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE_POINTWISE") == "1"
+)
 
 # enable slow autotuning passes to select gemm algorithms
-max_autotune_gemm = os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE_GEMM") == "1"
+max_autotune_gemm: bool = os.environ.get("TORCHINDUCTOR_MAX_AUTOTUNE_GEMM") == "1"
 
 # enable searching global and local cache regardless of `max_autotune`
-search_autotune_cache = os.environ.get("TORCHINDUCTOR_SEARCH_AUTOTUNE_CACHE") == "1"
+search_autotune_cache: bool = (
+    os.environ.get("TORCHINDUCTOR_SEARCH_AUTOTUNE_CACHE") == "1"
+)
 
 # We will disable creating subprocess for autotuning if this is False
-autotune_in_subproc = os.environ.get("TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC") == "1"
+autotune_in_subproc: bool = os.environ.get("TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC") == "1"
 
-coordinate_descent_tuning = (
+coordinate_descent_tuning: bool = (
     os.environ.get("TORCHINDUCTOR_COORDINATE_DESCENT_TUNING") == "1"
 )
 
-layout_optimization = os.environ.get("TORCHINDUCTOR_LAYOUT_OPTIMIZATION", "1") == "1"
+layout_optimization: bool = (
+    os.environ.get("TORCHINDUCTOR_LAYOUT_OPTIMIZATION", "1") == "1"
+)
 
 # Whether to keep the output strides the same as eager after layout optimization.
-keep_output_stride = os.environ.get("TORCHINDUCTOR_KEEP_OUTPUT_STRIDE", "1") == "1"
+keep_output_stride: bool = (
+    os.environ.get("TORCHINDUCTOR_KEEP_OUTPUT_STRIDE", "1") == "1"
+)
 
 # Enabling this will let compiler print warning messages if a generated triton
 # kernel has inputs with mixed layouts.  This is helpful for perf debugging
 # since kernel with mixed layout inputs may run much slower then one whose inputs
 # have uniform layouts.
-warn_mix_layout = os.environ.get("TORCHINDUCTOR_WARN_MIX_LAYOUT") == "1"
+warn_mix_layout: bool = os.environ.get("TORCHINDUCTOR_WARN_MIX_LAYOUT") == "1"
 
 # control store vs recompute heuristic
 # For fanouts, rematerialization can lead to exponential blowup. So, have
 # smaller threshold
-realize_reads_threshold = 4
-realize_bytes_threshold = 2000
+realize_reads_threshold: int = 4
+realize_bytes_threshold: int = 2000
 
 # Threshold to prevent excessive accumulation of ops in one buffer during lowering
-realize_acc_reads_threshold = 8
+realize_acc_reads_threshold: int = 8
 
 # fallback to eager for random/dropout, this is slow but useful for debugging
-fallback_random = False
+fallback_random: bool = False
 
 # automatically create fallbacks when encountering an unhandled op
-implicit_fallbacks = True
+implicit_fallbacks: bool = True
 
 # fuse even in cases without common reads
-aggressive_fusion = False
+aggressive_fusion: bool = False
 
 # how many nodes to allow into a single fusion
-max_fusion_size = 64
+max_fusion_size: int = 64
 
 # replace small reductions with pointwise, disable with `= 1`
-unroll_reductions_threshold = 8
+unroll_reductions_threshold: int = 8
 
 # Add extra comments to output code (causes compile cache misses)
-comment_origin = False
+comment_origin: bool = False
 
 # Convert 1x1 convs into matmuls
-conv_1x1_as_mm = False
+conv_1x1_as_mm: bool = False
 
 # Enable split reductions for better utilization when the dimension
 # being reduced over is large (by splitting it)
-split_reductions = True
+split_reductions: bool = True
 
-benchmark_kernel = os.environ.get("TORCHINDUCTOR_BENCHMARK_KERNEL", "0") == "1"
+benchmark_kernel: bool = os.environ.get("TORCHINDUCTOR_BENCHMARK_KERNEL", "0") == "1"
 
 # Enable constant and index_expr folding
-constant_and_index_propagation = True
+constant_and_index_propagation: bool = True
 
 # Enable indirect_indexing asserts for decompositions and lowerings
-debug_index_asserts = False
-
-
-def is_fbcode():
-    return not hasattr(torch.version, "git_version")
+debug_index_asserts: bool = False
 
 
 # warnings intended for PyTorch developers, disable for point releases
-is_nightly_or_source = "dev" in torch.__version__ or "git" in torch.__version__
-developer_warnings = is_fbcode() or is_nightly_or_source
+is_nightly_or_source: bool = "dev" in torch.__version__ or "git" in torch.__version__
+developer_warnings: bool = ConfigMixin.is_fbcode() or is_nightly_or_source
 
 
 def decide_compile_threads():
     """
     Here are the precedence to decide compile_threads
-    1. User can override it by TORCHINDUCTOR_COMPILE_THREADS.  One may want to disable async compiling by
+    1. User can override it by TORCHINDUCTOR_COMPILE_THREADS.
+       One may want to disable async compiling by
        setting this to 1 to make pdb happy.
     2. Set to 1 if it's win32 platform or it's a fbcode build
     3. decide by the number of CPU cores
     """
     if "TORCHINDUCTOR_COMPILE_THREADS" in os.environ:
         return int(os.environ["TORCHINDUCTOR_COMPILE_THREADS"])
-    elif sys.platform == "win32" or is_fbcode():
+    elif sys.platform == "win32" or ConfigMixin.is_fbcode():
         return 1
     else:
         return min(
@@ -154,64 +162,63 @@ def decide_compile_threads():
         )
 
 
-compile_threads = decide_compile_threads()
+compile_threads: int = decide_compile_threads()
 
 # gemm autotuning global cache dir
-if is_fbcode():
-    global_cache_dir = "fb/cache"
-else:
-    global_cache_dir = None
+
+global_cache_dir: Optional[str] = "fb/cache" if ConfigMixin.is_fbcode() else None
 
 # If kernel is fused, the name is generated from the origin node op names
 # for larger kernels limit this
-kernel_name_max_ops = 10
+kernel_name_max_ops: int = 10
 
 # Pad input tensors of matmul/bmm/addmm to leverage Tensor Cores in NVIDIA GPUs
-shape_padding = os.environ.get("TORCHINDUCTOR_SHAPE_PADDING", "1") == "1"
+shape_padding: bool = os.environ.get("TORCHINDUCTOR_SHAPE_PADDING", "1") == "1"
 
 # Fx-based linear/matmul/bmm + permute/transpose vertical fusion
-permute_fusion = os.environ.get("TORCHINDUCTOR_PERMUTE_FUSION", "0") == "1"
+permute_fusion: bool = os.environ.get("TORCHINDUCTOR_PERMUTE_FUSION", "0") == "1"
 
 # Mark the wrapper call in PyTorch profiler
-profiler_mark_wrapper_call = False
+profiler_mark_wrapper_call: bool = False
 
 # Generate hook calls to torch._inductor.hooks.run_intermediate_hooks for
 # every intermediate for which we can correlate it with an intermediate
 # from the original FX graph
-generate_intermediate_hooks = False
+generate_intermediate_hooks: bool = False
 
 # Populate traceback field on IRNode; good for debugging why origin_node is
 # not populated, or finding out where an IRNode was constructed
-debug_ir_traceback = False
+debug_ir_traceback: bool = False
 
 # used for debugging to make sure config is properly set
-_raise_error_for_testing = False
+_raise_error_for_testing: bool = False
 
-_profile_var = os.environ.get("TORCHINDUCTOR_PROFILE", "")
-profile_bandwidth = _profile_var != ""
-profile_bandwidth_regex = "" if _profile_var == "1" else _profile_var
+_profile_var: str = os.environ.get("TORCHINDUCTOR_PROFILE", "")
+profile_bandwidth: bool = _profile_var != ""
+profile_bandwidth_regex: str = "" if _profile_var == "1" else _profile_var
 
-disable_cpp_codegen = is_fbcode()
+disable_cpp_codegen: bool = ConfigMixin.is_fbcode()
 
 
-# config specific to codegen/cpp.py
-class cpp:
+# config specific to codegen/cpp.pp
+@dataclasses.dataclass
+class CppConfig(ConfigMixin):
     # set to torch.get_num_threads()
-    threads = -1
+    threads: int = -1
 
     # Do not generate loops when the condition doesn't hold, like:
     # for(long i0=4096; i0<4096; i0+=1)
-    no_redundant_loops = True
+    no_redundant_loops: bool = True
 
     # Assume number of threads is dynamic, don't specialize thread number.
     # Kernels don't recompile on thread number changes with this flag on.
     # For single-threaded workload, turning it on would incur a slight
     # performance degradation.
-    dynamic_threads = False
+    dynamic_threads: bool = False
 
-    simdlen = None
-    min_chunk_size = 4096
-    cxx = (
+    simdlen: Optional[int] = None
+    min_chunk_size: int = 4096
+    cxx: Tuple[Optional[str]] = (
         None,  # download gcc12 from conda-forge if conda is installed
         # "g++-12",
         # "g++-11",
@@ -221,156 +228,160 @@ class cpp:
         # "g++.par",
     )
     # Allow kernel performance profiling via PyTorch profiler
-    enable_kernel_profile = False
+    enable_kernel_profile: bool = False
 
     # enable weight prepacking to get a better performance; may lead to large memory footprint
-    weight_prepack = True
+    weight_prepack: bool = True
 
     # Inject a bug into our relu implementation; useful for testing our repro
     # extraction and minification functionality.
     # Valid values: "compile_error", "runtime_error", "accuracy"
-    inject_relu_bug_TESTING_ONLY = None
-    inject_log1p_bug_TESTING_ONLY = None
+    inject_relu_bug_TESTING_ONLY: Optional[str] = None
+    inject_log1p_bug_TESTING_ONLY: Optional[str] = None
 
     # If None, autodetect whether or not AVX512/AVX2 can be used.  Otherwise,
     # force usage as specified, without testing.
-    vec_isa_ok = None
+    vec_isa_ok: Optional[str] = None
 
     # similar to config.triton.descriptive_names
-    descriptive_names = "original_aten"
+    descriptive_names: str = "original_aten"
 
     # how many nodes to allow into a single horizontal fusion
-    max_horizontal_fusion_size = 16
+    max_horizontal_fusion_size: int = 16
 
 
 # config specific to codegen/triton.py
-class triton:
+@dataclasses.dataclass
+class TritonConfig(ConfigMixin):
     # Use cudagraphs on output code
-    cudagraphs = False
+    cudagraphs: bool = False
 
     # Use cudagraph trees for memory pooling if `cudagraphs` is True
-    cudagraph_trees = not is_fbcode()
+    cudagraph_trees: bool = not ConfigMixin.is_fbcode()
 
     # assertions not on the fast path, steady state
-    slow_path_cudagraph_asserts = True
+    slow_path_cudagraph_asserts: bool = True
 
     # TODO - need to debug why this prevents cleanup
-    cudagraph_trees_history_recording = False
+    cudagraph_trees_history_recording: bool = False
 
     # assertions on the fast path
-    fast_path_cudagraph_asserts = False
+    fast_path_cudagraph_asserts: bool = False
 
     # skip warmup for cudagraph trees
-    skip_cudagraph_warmup = False
+    skip_cudagraph_warmup: bool = False
 
     # Synchronize before and after every compiled graph.
-    debug_sync_graph = False
+    debug_sync_graph: bool = False
 
     # Synchronize after every kernel launch, to help pinpoint bugs
-    debug_sync_kernel = False
+    debug_sync_kernel: bool = False
 
     # Always load full blocks (rather than broadcasting inside the block)
-    dense_indexing = False
+    dense_indexing: bool = False
 
     # limit tiling dimensions
-    max_tiles = 2
+    max_tiles: int = 2
 
     # use triton.autotune for pointwise ops with complex layouts
     # this should only be disabled for debugging/testing
-    autotune_pointwise = True
+    autotune_pointwise: bool = True
 
     # should we stop a fusion to allow better tiling?
-    tiling_prevents_pointwise_fusion = True
-    tiling_prevents_reduction_fusion = True
+    tiling_prevents_pointwise_fusion: bool = True
+    tiling_prevents_reduction_fusion: bool = True
 
     # assert that indirect indexing does not read / write out of bounds
-    assert_indirect_indexing = True
+    assert_indirect_indexing: bool = True
 
     # should we give different names to kernels
     # Note: This is orthogonal to descriptive_names - this is deciding whether
     # our triton kernel names should all be `triton_` (to maximize caching) or
     # whether they should be unique.
-    unique_kernel_names = os.environ.get("TORCHINDUCTOR_UNIQUE_KERNEL_NAMES") == "1"
+    unique_kernel_names: bool = (
+        os.environ.get("TORCHINDUCTOR_UNIQUE_KERNEL_NAMES") == "1"
+    )
 
     # should we put op names in kernel names
     # False: No special names (just triton__1, triton__2, etc.)
     # "torch": Maps to the fx op in the Dynamo graph (module name, method name, etc.)
     # "original_aten": Maps to the highest-level aten op (i.e. pre-decompositions)
     # "inductor_node": Maps to the node name in the FX graph passed to Inductor
-    descriptive_names = "original_aten"
+    descriptive_names: str = "original_aten"
 
     # use alternate codegen for smaller reductions
-    persistent_reductions = (
+    persistent_reductions: bool = (
         os.environ.get("TORCHINDUCTOR_PERSISTENT_REDUCTIONS", "1") == "1"
     )
 
     # hint to Triton when arguments are divisible by 16
-    divisible_by_16 = True
+    divisible_by_16: bool = True
 
     # theses are not enforced, but they are used by asserts in triton_heuristics.py
     # NOTE: mobilevit_s in timm_models required X to be set to the higher value 2048
-    max_block = {"X": 2048, "Y": 1024, "Z": 1024}
+    max_block: Dict[str, int] = dataclasses.field(
+        default_factory=lambda: {"X": 2048, "Y": 1024, "Z": 1024}
+    )
 
     # Store the generated cubin files for cpp wrapper code to load
-    store_cubin = False
+    store_cubin: bool = False
 
     # the max number of spills we allow for the configs we benchmark.
     # Setting this to 0 means we skip a config if it spills even a single
     # register.
     # Settting it to a larger value allows a config spilling a small amount
     # of registers being benchmarked.
-    spill_threshold = 0
+    spill_threshold: int = 0
 
     # Inject a bug into our relu implementation; useful for testing our repro
     # extraction and minification functionality.
     # Valid values: "compile_error", "runtime_error", "accuracy"
-    inject_relu_bug_TESTING_ONLY = None
+    inject_relu_bug_TESTING_ONLY: Optional[str] = None
 
 
 # create a directory containing lots of debug information
-class trace:
+@dataclasses.dataclass
+class TraceConfig(ConfigMixin):
     # master switch for all debugging flags below
-    enabled = os.environ.get("TORCH_COMPILE_DEBUG", "0") == "1"
+    enabled: bool = os.environ.get("TORCH_COMPILE_DEBUG", "0") == "1"
 
     # Save python logger call >=logging.DEBUG
-    debug_log = False
+    debug_log: bool = False
 
     # Save python logger call >=logging.INFO
-    info_log = False
+    info_log: bool = False
 
     # Save input FX graph (post decomps, pre optimization)
-    fx_graph = True
+    fx_graph: bool = True
 
     # Save FX graph after transformations
-    fx_graph_transformed = True
+    fx_graph_transformed: bool = True
 
     # Save TorchInductor IR before fusion pass
-    ir_pre_fusion = True
+    ir_pre_fusion: bool = True
 
     # Save TorchInductor IR after fusion pass
-    ir_post_fusion = True
+    ir_post_fusion: bool = True
 
     # Copy generated code to trace dir
-    output_code = True
+    output_code: bool = True
 
     # SVG figure showing post-fusion graph
-    graph_diagram = False
+    graph_diagram: bool = False
 
     # Store cProfile (see snakeviz to view)
-    compile_profile = False
+    compile_profile: bool = False
 
     # Upload the .tar.gz file
     # Needs to be overriden based on specific environment needs
-    upload_tar = None
+    # skip_pickle will make this field skipped by pickle
+    upload_tar: Any = dataclasses.field(default=None, metadata={"skip_pickle": True})
 
 
-_save_config_ignore = {
-    # workaround: "Can't pickle <function ...>"
-    "trace.upload_tar",
-}
+cpp: CppConfig = CppConfig()
+triton: TritonConfig = TritonConfig()
+trace: TraceConfig = TraceConfig()
 
+from torch._config_utils import install_config_module
 
-from .._dynamo.config_utils import install_config_module
-
-# adds patch, save_config, etc
-install_config_module(sys.modules[__name__])
+install_config_module("InductorConfig", sys.modules[__name__])
