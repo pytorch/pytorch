aaaf36f2832c5425e6ab25d5f218ef182bd90eef
Richard Zou <zou3519@gmail.com>
2023-05-23 11:26:52 -0700

    Add mechanism to turn any RAII guard into a Python Context Manager (#102037)
    This PR:
    - adds a mechanism to turn any RAII guard into a Python Context Manager
    - turns ExcludeDispatchKeyGuard into a context manager, and purges usages
    of the older torch._C.ExcludeDispatchKeyGuard from the codebase.
    
    The mechanism is that given a RAII guard, we construct a context
    manager object that holds an optional guard. When we enter the context
    manager we populate the guard, when we exit we reset it.
    
    We don't delete torch._C.ExcludeDispatchKeyGuard for BC reasons (people
    are using it in fbcode). If this code actually sticks
    (it is using C++17 and that worries me a bit), then I'll apply the
    change to other RAII guards we have, otherwise, we can write our own
    std::apply.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/102037
    Approved by: https://github.com/ezyang, https://github.com/bdhirsh

--- test/functorch/test_eager_transforms.py
+++ test/functorch/test_eager_transforms.py
@@ -27,7 +27,7 @@ from torch.testing import make_tensor
 from torch._subclasses.fake_tensor import FakeTensorMode
 from functools import partial
 from functorch.experimental import replace_all_batch_norm_modules_
-from torch._C import ExcludeDispatchKeyGuard, DispatchKeySet, DispatchKey
+from torch._C import _ExcludeDispatchKeyGuard, DispatchKeySet, DispatchKey
 
 import functorch
 from functorch import (
@@ -3373,16 +3373,13 @@ class TestComposability(TestCase):
         x = torch.randn([], device=device)
         expected = f(x)
 
-        guard = ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.Functionalize))
-        try:
+        with _ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.Functionalize)):
             gm = make_fx(functorch.functionalize(f))(x)
             self.assertTrue('sin_' not in gm.code)
             self.assertEqual(gm(x), expected)
 
             local_exclude_set = torch._C._dispatch_tls_local_exclude_set()
             self.assertTrue(local_exclude_set.has(DispatchKey.Functionalize))
-        finally:
-            del guard
 
     def test_can_use_vmap_when_key_is_excluded(self, device):
         def f(x):
@@ -3391,14 +3388,11 @@ class TestComposability(TestCase):
         x = torch.randn(3, device=device)
         expected = vmap(f)(x)
 
-        guard = ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.FuncTorchBatched))
-        try:
+        with _ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.FuncTorchBatched)):
             result = vmap(f)(x)
             self.assertEqual(result, expected)
             local_exclude_set = torch._C._dispatch_tls_local_exclude_set()
             self.assertTrue(local_exclude_set.has(DispatchKey.FuncTorchBatched))
-        finally:
-            del guard
 
     def test_can_use_grad_when_key_is_excluded(self, device):
         def f(x):
@@ -3407,14 +3401,11 @@ class TestComposability(TestCase):
         x = torch.randn([], device=device)
         expected = grad(f)(x)
 
-        guard = ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.Autograd))
-        try:
+        with _ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.Autograd)):
             result = grad(f)(x)
             self.assertEqual(result, expected)
             local_exclude_set = torch._C._dispatch_tls_local_exclude_set()
             self.assertTrue(local_exclude_set.has(DispatchKey.Autograd))
-        finally:
-            del guard
 
 
 class TestMakeFunctional(TestCase):
