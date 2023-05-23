942cd12d55fc4124eddc25d05c3df193eab10efe
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-05-23 18:44:09 +0000

    [spmd] add option to preserve node types (#100072)
    This PR adds a option to preserve node types for the entire graph,
    this could allow some exploration about using those node types to do
    things like act checkpoint, etc.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100072
    Approved by: https://github.com/mrshenli

--- torch/distributed/_spmd/data_parallel.py
+++ torch/distributed/_spmd/data_parallel.py
@@ -877,6 +877,22 @@ def partition_data_parallel(
     # 3. Partition the single machine graph to the distribute graph
     partitioned_graph = partitioner(graph)
 
+    # preserve node types for the expanded graph
+    for node in partitioned_graph.graph.nodes:
+        if node in strategy_map:
+            node_strategy = strategy_map[node]
+            if isinstance(node_strategy, DataParallelStrategy):
+                node.meta["node_type"] = node_strategy.node_type
+            elif isinstance(node_strategy, TupleStrategy):
+                node.meta["node_type"] = NodeType.NON_TENSOR
+            else:
+                raise RuntimeError(f"Unknown node strategy {node_strategy}")
+        else:
+            # if the nodes are expanded nodes (collectives), we mark them
+            # the same type as the input node.
+            input_node = node.all_input_nodes[0]
+            node.meta["node_type"] = input_node.meta["node_type"]
+
     # 4. Last, inplace partition the weights and optim states to
     #    DTensors base on the parallel style
     accessor = NamedMemberAccessor(model)
