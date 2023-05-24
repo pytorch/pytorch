6d6979a03e80ae55be5af7711d58eea2bb9edb84
Edward Z. Yang <ezyang@meta.com>
2023-05-24 10:40:08 -0700

    Add fake process group
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

---
+++ test/distributed/test_fake_pg.py
@@ -0,0 +1,32 @@
+# Owner(s): ["oncall: distributed"]
+
+import sys
+import torch
+import torch.distributed as dist
+from unittest import skip, SkipTest
+import operator
+from functools import reduce
+import threading
+
+if not dist.is_available():
+    print("Distributed not available, skipping tests", file=sys.stderr)
+    sys.exit(0)
+
+from torch.testing._internal.common_utils import (
+    TestCase,
+    run_tests,
+)
+
+HAS_CUDA = torch.cuda.is_available()
+
+class Test(TestCase):
+    @unittest.skipIf(not HAS_CUDA, 'No CUDA')
+    def test_construct_fsdp(self):
+        store = FakeStore()
+        dist.init_process_group(
+            backend="fake", rank=0, world_size=2, store=store
+        )
+        sharded_module = FSDP(nn.Linear(2, 3, device='cuda'))
+
+if __name__ == "__main__":
+    run_tests()
