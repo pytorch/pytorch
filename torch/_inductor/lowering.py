87b96ffd77c7cdcc5772399a064e6a191bac6bcf
Jason Ansel <jansel@meta.com>
2023-05-30 14:40:44 -0700

    Update on "[inductor] Fix issue with 0D reductions"
    Fixes https://github.com/pytorch/pytorch/issues/102546
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10
    
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -3620,12 +3620,13 @@ def avg_pool2d_backward(
 
 def _validate_reduction_axis(x, axis):
     size = x.get_size()
-    if len(size) == 0:
-        return []
     if isinstance(axis, int):
         axis = [axis]
     elif not axis:
         axis = range(len(size))
+    if len(size) == 0:
+        assert tuple(axis) in [(), (0,), (-1,)], f"invalid axis: {axis}"
+        return []
     axis = list(axis)
     for i in range(len(axis)):
         if axis[i] < 0:
