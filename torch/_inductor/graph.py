9ab37ed00fe7d0448ee107eb50cc17b3651210e5
Shunting Zhang <shunting@fb.com>
2023-06-01 22:50:10 +0000

    Update on "[temp] only force strides for user visible outputs"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10
    
    [ghstack-poisoned]

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -190,9 +190,6 @@ class GraphLowering(torch.fx.Interpreter):
         self.aot_mode = aot_mode
         self.graph_id = graph_id
         self.scheduler = None
-        self.nodes_prefer_channels_last = (
-            self.find_nodes_prefer_channels_last() if self.layout_opt else set()
-        )
         self._warned_fallback = {"aten.convolution_backward"}
         self.user_visible_outputs = user_visible_outputs
 
@@ -300,57 +297,6 @@ class GraphLowering(torch.fx.Interpreter):
                 return False
 
         return True
-
-    def find_nodes_prefer_channels_last(self):
-        """
-        The rule to decide if an node prefer channels last is simple.
-        1. if it's input/output of a convolution
-        2. if one of its user prefers channels last
-
-        We have rule 1 because cudnn runs a faster convolution kernel for channels last inputs;
-        Rule 2 is also important. It makes sure that indirect inputs to convolution also prefers
-        channels last.
-
-        Consider the scenario: conv -> batch-norm -> relu -> conv
-        Without rule 2, batch-norm output may use a contiguous layout. That will cause 2 extra copies:
-        1. the output of batch-norm should be channels last initially since its input is a conv's output.
-           Forcing the batch-norm's output to be contiguous results in the first copy
-        2. The second conv's input is initially contiguous. This layout is propagated from the batch-norm's output.
-           We need convert it to channels last layout which results in the second copy.
-        With rule 2, we makes sure all the tensors in the chain uses channels last layout. So both copies
-        can be saved.
-        """
-        output_set = set()
-        for n in reversed(self.module.graph.nodes):
-            if n.target == torch.ops.aten.convolution.default:
-                output_set.add(n)
-                continue
-
-            for user in n.users:
-                if user in output_set:
-                    output_set.add(n)
-                    break
-
-        # need a second pass to add downstream nodes of those channel last nodes to the sets.
-        # This pass is especially needed to avoid mix-layout kernel inputs in backward pass.
-        #
-        # Let's say a conv-batchnorm 's output is passed to relu whose output is in turn returned
-        # from the fwd graph. Without this second pass, we will force relu's output to be contiguous.
-        # Then in the kernel in backward pass, the contiguous output of relu may be mix with other channels last
-        # tensors and passed to a kernel.
-        #
-        # This pass improve yolov3 training speedup from 1.116x (worse than disabling layout optimization speedup 1.196x) to 1.457x.
-        # It also improves dla102 training speedup from 1.240x (worse than disabling layout optimization speedup 1.523x) to 1.835x .
-        # This also helps the following models:
-        # - res2net101_26w_4s
-        # - res2net50_14w_8s
-        # - sebotnet33ts_256
-        for n in self.module.graph.nodes:
-            if n in output_set:
-                for child in n.users:
-                    output_set.add(child)
-
-        return output_set
 
     def warn_fallback(self, name):
         if name not in self._warned_fallback:
