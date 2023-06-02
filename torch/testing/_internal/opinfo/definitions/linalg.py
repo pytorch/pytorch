0e35ecd2ca97dfa5c325336b86729830a0acc5ba
Arindam Roy <rarindam@gmail.com>
2023-06-02 10:39:56 -0700

    Remove sample_inputs_linalg_det_singular function
    After removing the det singular test, removing
    the sampling function which was being used only
    by that test.
    
    Signed-off-by: Arindam Roy <rarindam@gmail.com>

--- torch/testing/_internal/opinfo/definitions/linalg.py
+++ torch/testing/_internal/opinfo/definitions/linalg.py
@@ -186,37 +186,6 @@ def sample_inputs_householder_product(op_info, device, dtype, requires_grad, **k
     yield SampleInput(make_arg((S, S - 1)), make_arg((S - 2,), low=None, high=None))
 
 
-def sample_inputs_linalg_det_singular(op_info, device, dtype, requires_grad, **kwargs):
-    make_arg = partial(make_tensor, device=device, dtype=dtype)
-
-    def make_singular_matrix_batch_base(size, rank):
-        assert size[-1] == size[-2]
-        assert rank > 0 and rank < size[-1]
-
-        n = size[-1]
-        a = make_arg(size[:-2] + (n, rank)) / 10
-        b = make_arg(size[:-2] + (rank, n)) / 10
-        x = a @ b
-        lu, pivs, _ = torch.linalg.lu_factor_ex(x)
-        p, l, u = torch.lu_unpack(lu, pivs)
-        u_diag_abs = u.diagonal(0, -2, -1).abs()
-        u_diag_abs_largest = u_diag_abs.max(dim=-1, keepdim=True).values
-        u_diag_abs_smallest_idxs = torch.topk(
-            u_diag_abs, k=(n - rank), largest=False
-        ).indices
-        u.diagonal(0, -2, -1).div_(u_diag_abs_largest)
-        u.diagonal(0, -2, -1)[..., u_diag_abs_smallest_idxs] = torch.finfo(dtype).eps
-        matrix = p @ l @ u
-
-        matrix.requires_grad_(requires_grad)
-        return matrix
-
-    for batch, size in product(((), (2,), (2, 2)), range(6)):
-        shape = batch + (size, size)
-        for rank in range(1, size):
-            yield SampleInput(make_singular_matrix_batch_base(shape, rank))
-
-
 def sample_inputs_linalg_matrix_power(op_info, device, dtype, requires_grad, **kwargs):
     make_fullrank = make_fullrank_matrices_with_distinct_singular_values
     make_arg = partial(
