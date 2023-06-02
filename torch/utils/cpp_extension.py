ea92589e63c78c9c3a138336b27cd4b6c9c77de6
lcskrishna <lollachaitanya@gmail.com>
2023-06-02 08:03:33 +0000

    add spaces for lint

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -17,9 +17,9 @@ import torch._appdirs
 from .file_baton import FileBaton
 from ._cpp_extension_versioner import ExtensionVersioner
 try:
-    from .hipify import hipify_python # type: ignore[import]
+    from .hipify import hipify_python  # type: ignore[import]
 except ModuleNotFoundError:
-    hipify_python = None # type: ignore[assignment]
+    hipify_python = None  # type: ignore[assignment]
 from typing import Dict, List, Optional, Union, Tuple
 from torch.torch_version import TorchVersion
 
