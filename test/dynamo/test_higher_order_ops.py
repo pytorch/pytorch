eede857f26e9c0dfcfc9d23042d4037f1606f16f
Animesh Jain <anijain@umich.edu>
2023-06-04 16:08:58 +0000

    tagging based activation checkpointing
    [ghstack-poisoned]

--- test/dynamo/test_higher_order_ops.py
+++ test/dynamo/test_higher_order_ops.py
@@ -1,6 +1,7 @@
 # Owner(s): ["module: dynamo"]
 import functools
 import unittest
+from importlib import import_module
 
 import functorch.experimental.control_flow as control_flow
 
@@ -661,10 +662,13 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
         for arg in args:
             cloned_args.append(arg.clone().detach().requires_grad_(arg.requires_grad))
 
+        torch.manual_seed(0)
         expected = fn(*args)
         expected.sum().backward()
 
-        result = torch.compile(fn, fullgraph=fullgraph, backend=backend)(*cloned_args)
+        opt_fn = torch.compile(fn, fullgraph=fullgraph, backend=backend)
+        torch.manual_seed(0)
+        result = opt_fn(*cloned_args)
         result.sum().backward()
 
         if not skip_check:
@@ -779,25 +783,6 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
         self.assertEqual(cnt.op_count, 2)
         self.assertEqual(len(backend.graphs), 2)
 
-    def test_without_functionalization_turned_on(self):
-        def gn(x, y):
-            return torch.sigmoid(torch.matmul(x, y))
-
-        def fn(x, y):
-            return torch.cos(torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y))
-
-        x = torch.randn(4, 4, requires_grad=True)
-        y = torch.randn(4, 4, requires_grad=True)
-        args = (x, y)
-
-        backend = EagerAndRecordGraphs()
-        cnt = CompileCounterWithBackend(backend)
-
-        expected = fn(*args)
-        result = torch.compile(fn, backend=cnt)(*args)
-
-        self.assertEqual(result, expected)
-
     @requires_cuda()
     @torch._functorch.config.patch(functionalize_rng_ops=True)
     def test_module(self):
@@ -824,6 +809,221 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
         )
         backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
         self._validate(fn, backend, x)
+
+
+class ActivationCheckpointingViaTagsTests(torch._dynamo.test_case.TestCase):
+    def _validate(self, fn, backend, *args, skip_check=False, fullgraph=True):
+        cloned_args = []
+        for arg in args:
+            cloned_args.append(arg.clone().detach().requires_grad_(arg.requires_grad))
+
+        torch.manual_seed(0)
+        expected = fn(*args)
+        expected.sum().backward()
+
+        torch.manual_seed(0)
+        result = torch.compile(fn, fullgraph=fullgraph, backend=backend)(*cloned_args)
+        result.sum().backward()
+
+        if not skip_check:
+            self.assertEqual(result, expected)
+            for arg, cloned_arg in zip(args, cloned_args):
+                self.assertEqual(arg.grad, cloned_arg.grad)
+
+    @requires_cuda()
+    def test_tags_function(self):
+        def gn(x, y):
+            return torch.sigmoid(torch.matmul(x, y))
+
+        def fn(x, y):
+            return torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y)
+
+        x = torch.randn(4, 4, device="cuda", requires_grad=True)
+        y = torch.randn(4, 4, device="cuda", requires_grad=True)
+
+        fw_compiler = functools.partial(count_ops, freq=1, op=torch.ops.aten.mm.default)
+        bw_compiler = functools.partial(
+            count_ops, freq=3, op=torch.ops.aten.mm.default
+        )  # mm recomputed in the bwd
+        backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        self._validate(fn, backend, x, y)
+
+    @requires_cuda()
+    def test_tags_function_with_kwargs(self):
+        def gn(x, y):
+            return torch.sigmoid(torch.matmul(x, y))
+
+        def fn(x, y):
+            return torch.utils.checkpoint.checkpoint(
+                gn, torch.sin(x), y, use_reentrant=True, preserve_rng_state=False
+            )
+
+        x = torch.randn(4, 4, device="cuda", requires_grad=True)
+        y = torch.randn(4, 4, device="cuda", requires_grad=True)
+
+        fw_compiler = functools.partial(count_ops, freq=1, op=torch.ops.aten.mm.default)
+        bw_compiler = functools.partial(
+            count_ops, freq=3, op=torch.ops.aten.mm.default
+        )  # mm recomputed in the bwd
+        backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        self._validate(fn, backend, x, y)
+
+    @requires_cuda()
+    def test_tags_multiple_checkpoints(self):
+        def gn(x, y):
+            return torch.sigmoid(torch.matmul(x, y))
+
+        def fn(x, y):
+            x = torch.sin(x)
+            z = torch.utils.checkpoint.checkpoint(gn, x, y)
+            x = torch.sin(z)
+            z = torch.utils.checkpoint.checkpoint(gn, x, y)
+            return z
+
+        x = torch.randn(4, 4, device="cuda", requires_grad=True)
+        y = torch.randn(4, 4, device="cuda", requires_grad=True)
+
+        fw_compiler = functools.partial(count_ops, freq=2, op=torch.ops.aten.mm.default)
+        bw_compiler = functools.partial(
+            count_ops, freq=6, op=torch.ops.aten.mm.default
+        )  # mm recomputed in the bwd
+        backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        self._validate(fn, backend, x, y)
+
+    @requires_cuda()
+    def test_tags_module(self):
+        class MockModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.linear = torch.nn.Linear(10, 10)
+
+            def forward(self, x):
+                return torch.sigmoid(self.linear(x))
+
+        mod = MockModule().cuda()
+
+        def fn(x):
+            return torch.utils.checkpoint.checkpoint(mod, torch.sin(x))
+
+        x = torch.randn(10, 10, device="cuda", requires_grad=True)
+
+        fw_compiler = functools.partial(
+            count_ops, freq=1, op=torch.ops.aten.sigmoid.default
+        )
+        bw_compiler = functools.partial(
+            count_ops, freq=1, op=torch.ops.aten.sigmoid.default
+        )
+        backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        self._validate(fn, backend, x)
+
+    @requires_cuda()
+    def test_tags_decomps(self):
+        # Ensures that tags are passed on through decompositions as well
+        class MockModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.linear = torch.nn.Linear(10, 10)
+
+            def forward(self, x):
+                return torch.nn.functional.gelu(self.linear(x))
+
+        mod = MockModule().cuda()
+
+        def fn(x):
+            return torch.utils.checkpoint.checkpoint(mod, torch.sin(x))
+
+        x = torch.randn(10, 10, device="cuda", requires_grad=True)
+
+        fw_compiler = functools.partial(
+            count_ops, freq=1, op=torch.ops.aten.erf.default
+        )
+        bw_compiler = functools.partial(
+            count_ops, freq=1, op=torch.ops.aten.erf.default
+        )
+        backend = aot_autograd(
+            fw_compiler=fw_compiler,
+            bw_compiler=bw_compiler,
+            decompositions=lambda: import_module(
+                "torch._inductor.compile_fx"
+            ).select_decomp_table(),
+        )
+        self._validate(fn, backend, x)
+
+    @requires_cuda()
+    @torch._inductor.config.patch(fallback_random=True)
+    def test_tags_rand_recomputed(self):
+        def gn(x, y):
+            return torch.sigmoid(torch.rand_like(x) * y) * x
+
+        def fn(x, y):
+            x = torch.sin(x)
+            z = torch.utils.checkpoint.checkpoint(gn, x, y)
+            x = torch.sin(z)
+            z = torch.utils.checkpoint.checkpoint(gn, x, y)
+            return z
+
+        x = torch.randn(4, 4, device="cuda", requires_grad=True)
+        y = torch.randn(4, 4, device="cuda", requires_grad=True)
+
+        # fw_compiler = functools.partial(count_ops, freq=2, op=torch.ops.aten.mm.default)
+        # bw_compiler = functools.partial(
+        #     count_ops, freq=6, op=torch.ops.aten.mm.default
+        # )  # mm recomputed in the bwd
+        # backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        backend = "inductor"
+        self._validate(fn, backend, x, y)
+
+    @requires_cuda()
+    @torch._inductor.config.patch(fallback_random=True)
+    def test_tags_rand(self):
+        def gn(x, y):
+            x = torch.mm(x, y)
+            x = torch.mm(x, y)
+            # x = torch.mm(x, y)
+            # x = torch.mm(x, y)
+            return x
+
+        def fn(x, y):
+            x = torch.sin(x)
+            z = torch.utils.checkpoint.checkpoint(gn, x, y)
+            # x = toch.sin(z)
+            z = torch.utils.checkpoint.checkpoint(gn, z, y)
+            return z
+
+        x = torch.randn(4, 4, device="cuda", requires_grad=True)
+        y = torch.randn(4, 4, device="cuda", requires_grad=True)
+
+        # fw_compiler = functools.partial(count_ops, freq=2, op=torch.ops.aten.mm.default)
+        # bw_compiler = functools.partial(
+        #     count_ops, freq=6, op=torch.ops.aten.mm.default
+        # )  # mm recomputed in the bwd
+        # backend = aot_autograd(fw_compiler=fw_compiler, bw_compiler=bw_compiler)
+        # backend = "aot_eager"
+        backend = "inductor"
+        self._validate(fn, backend, x, y)
+
+    @requires_cuda()
+    @torch._inductor.config.patch(fallback_random=True)
+    def test_tags_dropout(self):
+        # Figure out a way to test the number of inductor_random calls
+        class MockModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.linear = torch.nn.Linear(10, 10)
+                self.dropout = torch.nn.Dropout(0.2)
+
+            def forward(self, x):
+                return self.dropout(self.linear(x))
+
+        mod = MockModule().cuda()
+
+        def fn(x):
+            return torch.utils.checkpoint.checkpoint(mod, x)
+
+        x = torch.randn(10, 10, device="cuda", requires_grad=True)
+        backend = "inductor"
+        # rand decomps do not have have numerical results as eager
+        self._validate(fn, backend, x, skip_check=True)
 
 
 if __name__ == "__main__":
--- torch/_higher_order_ops/wrap.py
+++ torch/_higher_order_ops/wrap.py
@@ -1,5 +1,7 @@
 from torch._ops import HigherOrderOperator
 from torch.utils.checkpoint import checkpoint
+from itertools import count
+counter = count()
 
 # Used for testing the HigherOrderOperator mechanism
 class Wrap(HigherOrderOperator):
@@ -26,7 +28,41 @@ class WrapActivationCheckpoint(HigherOrderOperator):
         # And we ensure that AOT Autograd traces through the non reentrant
         # version of checkpointing.
         kwargs["use_reentrant"] = False
-        result = checkpoint(function, *args, **kwargs)
-        return result
+        return checkpoint(function, *args, **kwargs)
 
 wrap_activation_checkpoint = WrapActivationCheckpoint()
+
+
+class TagActivationCheckpoint(HigherOrderOperator):
+    """
+    This operator is supposed to be used only with torch.compile stack. This
+    accepts a Fx graph module which needs to be checkpointed. This operator adds
+    "recomputable" tag to the nodes of the Fx graph that should be recomputed.
+    This goal is to pass this information to AOT Autograd's partitioner which
+    will actually perform the cut.
+    """
+
+    def __init__(self):
+        super().__init__("wrap_activation_checkpoint")
+
+    def tag_nodes(self, gmod):
+        # TODO - This needs major investigation. Currently, we are tagging all
+        # the forward nodes as recomputable. However, torch.utils.checkpoint
+        # provides a custom function to selectively recompute. We will have to
+        # figure out how to tag seletively.
+        unique_graph_id = next(counter)
+        for node in gmod.graph.nodes:
+            if node.op == "call_function":
+                node.meta["recompute"] = unique_graph_id
+        return gmod
+
+    def __call__(self, gmod, *args, **kwargs):
+        if "context_fn" in kwargs:
+            raise RuntimeError("Tagged Activation checkpointing does not support selective checkpointing yet.")
+        import torch.fx.traceback as fx_traceback
+        from torch.fx import Interpreter
+        gmod = self.tag_nodes(gmod)
+        with fx_traceback.preserve_node_meta():
+            return Interpreter(gmod).run(*args)
+
+tag_activation_checkpoint = TagActivationCheckpoint()
