aa74ae967d8abdc8e7a38759709ef9f5f24dfca9
Bin Bao <binbao@fb.com>
2023-06-05 02:24:08 +0000

    [inductor] Expriment cpp wrapper with dynamic shapes
    [ghstack-poisoned]

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -342,7 +342,7 @@ test_perf_for_dashboard() {
       if [[ "$DASHBOARD_TAG" == *dynamic-true* ]]; then
         python "benchmarks/dynamo/$suite.py" \
             "${target_flag[@]}" --"$mode" --"$dtype" --backend "$backend" --dynamic-shapes \
-            --dynamic-batch-only --disable-cudagraphs "$@" \
+            --dynamic-batch-only --disable-cudagraphs --cpp-wrapper "$@" \
             --output "$TEST_REPORTS_DIR/${backend}_dynamic_${suite}_${dtype}_${mode}_cuda_${target}.csv"
       fi
       if [[ "$DASHBOARD_TAG" == *cppwrapper-true* ]] && [[ "$mode" == "inference" ]]; then
