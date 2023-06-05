1a3cc8ac4872f2fb493cd1b9c6194033e1b8d78b
PaliC <sahancpal@gmail.com>
2023-06-05 13:47:00 -0400

    Update on "add timm and huggingface to inductor docker images"
    [ghstack-poisoned]

--- .ci/docker/common/install_triton.sh
+++ .ci/docker/common/install_triton.sh
@@ -46,17 +46,3 @@ elif [ -n "${CLANG_VERSION}" ]; then
 else
   pip_install "git+${TRITON_REPO}@${TRITON_PINNED_COMMIT}#subdirectory=python"
 fi
-
-if [ -n "${CONDA_CMAKE}" ]; then
-  # TODO: This is to make sure that the same cmake and numpy version from install conda
-  # script is used. Without this step, the newer cmake version (3.25.2) downloaded by
-  # triton build step via pip will fail to detect conda MKL. Once that issue is fixed,
-  # this can be removed.
-  #
-  # The correct numpy version also needs to be set here because conda claims that it
-  # causes inconsistent environment.  Without this, conda will attempt to install the
-  # latest numpy version, which fails ASAN tests with the following import error: Numba
-  # needs NumPy 1.20 or less.
-  conda_reinstall cmake="${CMAKE_VERSION}"
-  conda_reinstall numpy="${NUMPY_VERSION}"
-fi
