26b4263fdcf76dc1f376015f10f5b4c9fe46f7d6
Yanbo Liang <ybliang8@gmail.com>
2023-06-06 05:28:53 +0000

    Rename

--- torch/_inductor/fx_passes/split_cat.py
+++ torch/_inductor/fx_passes/split_cat.py
@@ -27,7 +27,7 @@ from ..pattern_matcher import (
 )
 from .pre_grad import (
     merge_splits_pass,
-    normalize_split_pass,
+    normalization_pass,
     split_cat_pass,
     unbind_stack_pass,
 )
@@ -85,12 +85,12 @@ def normalize_split_base(match: Match, _get_split_args: Callable):
 
 @register_graph_pattern(
     CallFunctionVarArgs(torch.split, users=MULTIPLE),
-    pass_dict=normalize_split_pass,
+    pass_dict=normalization_pass,
     extra_check=config_flag("split_cat_fx_passes"),
 )
 @register_graph_pattern(
     CallMethodVarArgs("split", users=MULTIPLE),
-    pass_dict=normalize_split_pass,
+    pass_dict=normalization_pass,
     extra_check=config_flag("split_cat_fx_passes"),
 )
 def normalize_split_default(match: Match, *args, **kwargs):
@@ -99,7 +99,7 @@ def normalize_split_default(match: Match, *args, **kwargs):
 
 @register_graph_pattern(
     CallFunctionVarArgs(torch.cat, users=MULTIPLE),
-    pass_dict=normalize_split_pass,
+    pass_dict=normalization_pass,
     extra_check=config_flag("split_cat_fx_passes"),
 )
 def normalize_cat_default(match: Match, *args, **kwargs):
@@ -143,7 +143,7 @@ def find_next_users(split_node):
 
 @register_graph_pattern(
     CallMethodVarArgs("squeeze", users=MULTIPLE),
-    pass_dict=normalize_split_pass,
+    pass_dict=normalization_pass,
     extra_check=config_flag("split_cat_fx_passes"),
 )
 def normalize_squeeze_default(match: Match, *args, **kwargs):
