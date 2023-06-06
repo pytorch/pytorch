fe889ecb475033fe6298a1cb8057324dff60f2ac
Masaki Kozuki <mkozuki@nvidia.com>
2023-06-06 12:02:34 -0700

    [skip ci] update test
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -5,6 +5,7 @@ from numbers import Number
 import re
 import torch
 import unittest
+import itertools
 
 from torch.testing import make_tensor
 from torch.testing._comparison import default_tolerances
@@ -1053,6 +1054,32 @@ class TestForeach(TestCase):
         nested_tensorlists[1][0] = nested_tensorlists[1][0].to(alt_dev)
         with self.assertRaisesRegex(RuntimeError, "Tensors of the same index"):
             torch.utils._foreach_utils._group_tensors_by_device_and_dtype(nested_tensorlists)
+
+        num_tensors_per_list = 10
+        num_devices = torch.cuda.device_count()
+        dtypes = (torch.float16, torch.float32, torch.float64)
+        list1 = [
+            torch.tensor(
+                i,
+                device=torch.device("cuda", random.randint(0, num_devices - 1)),
+                dtype=dtypes[random.randint(0, 2)],
+            ) for i in range(num_tensors_per_list)
+        ]
+        list2 = [None for _ in list1]
+        list3 = [torch.rand_like(t) for t in list1]
+        nested_tensorlists = [list1, list2, list3]
+        grouped_tensors = torch.utils._foreach_utils._group_tensors_by_device_and_dtype(nested_tensorlists, with_indices=True)
+        num_tensors_seen = 0
+        for (device, dtype), ([l1, l2, l3], indices) in grouped_tensors:
+            for t in itertools.chain(l1, l2, l3):
+                self.assertEquals(t.device, device)
+                self.assertEquals(t.dtype, dtype)
+                num_tensors_seen += 1
+                self.assertEquals(len(l2), 0)
+                for i, index in enumerate(indices):
+                    self.assertEquals(l1[i], list1[index])
+                    self.assertEquals(l3[i], list3[index])
+        self.assertEquals(num_tensors_seen, 2 * num_tensors_per_list)
 
 
 instantiate_device_type_tests(TestForeach, globals())
