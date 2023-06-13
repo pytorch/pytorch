bcc15f070ac76d23f32b3637a24241a5848810da
Nikita Shulga <nshulga@meta.com>
2023-06-13 13:35:38 -0700

    Use linear search

--- torch/_dynamo/resume_execution.py
+++ torch/_dynamo/resume_execution.py
@@ -436,8 +436,10 @@ class ContinueExecutionCache:
             # this will ensure the instructions after the break have the correct line numbers
             if is_py311_plus:
                 # In 3.11+ bytecode instruction size vary, so do bisect to find the index
-                target_ind = bisect.bisect_left(
-                    instructions, target.offset, key=lambda i: i.offset
+                target_ind = next(
+                    idx
+                    for (idx, inst) in enumerate(instructions)
+                    if inst.offset is not None and inst.offset >= target.offset
                 )
             else:
                 target_ind = target.offset // 2
