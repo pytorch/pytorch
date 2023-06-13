984583ca7e93dbe88f0782c308d90c3a5695c1da
Richard Zou <zou3519@gmail.com>
2023-06-13 07:18:49 -0700

    Refactor and improve make_fx testing (#103196)
    This is in preparation for the custom_op_compile_check utility, which
    will call the newly refactored function.
    
    This PR:
    - splits off code into helper functions
    - adds clearer error messages
    - stops updating the inputs destructively (leading to slightly slower
    tests)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103196
    Approved by: https://github.com/bdhirsh, https://github.com/soulitzer

--- test/test_proxy_tensor.py
+++ test/test_proxy_tensor.py
@@ -7,7 +7,7 @@ import warnings
 import operator
 from collections.abc import Iterable
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
-from torch.testing._internal.common_methods_invocations import op_db, wrapper_set_seed, skip, xfail, skipOps
+from torch.testing._internal.common_methods_invocations import op_db, skip, xfail, skipOps
 from torch._subclasses.fake_tensor import DynamicOutputShapeException, DataDependentOutputException, FakeTensorMode
 from torch._decomp import decomposition_table
 from torch.fx.experimental.symbolic_shapes import (
@@ -17,6 +17,7 @@ from torch.fx.experimental.symbolic_shapes import (
 from torch.testing._internal.custom_op_db import custom_op_db
 from torch.testing._internal.control_flow_opinfo_db import control_flow_opinfo_db
 from torch.testing._internal.common_device_type import ops
+import torch.testing._internal.optests as optests
 from torch._C import _disabled_torch_function_impl
 from torch.fx.experimental.proxy_tensor import make_fx, DecompositionInterpreter, get_isolated_graphmodule
 from torch.utils._pytree import tree_map
@@ -1562,8 +1563,6 @@ inplace_symbolic_tensor_failures = {
     xfail('float_power', ''),  # base given to float_power_ has dtype Float but the operation's result requires dtype Double
     # decomp not implemented
     xfail('unique', ''),
-    # in-place has a different signature than out-of-place
-    xfail('uniform', ''),
 }
 
 # Copies inputs to inplace operations to avoid inplace modifications
@@ -1576,18 +1575,8 @@ def _get_safe_inplace(inplace_variant):
     return _fn
 
 def _test_make_fx_helper(self, device, dtype, op, tracing_mode, inplace=False):
-    def f(args, kwargs, extra_args, extra_kwargs):
-        if extra_args:
-            for i, t in extra_args:
-                args[i] = t.size()
-        if extra_kwargs:
-            for k, t in extra_kwargs.items():
-                kwargs[k] = t.size()
-
-        fn = _get_safe_inplace(op.get_inplace()) if inplace else op.op
-        return fn(*args, **kwargs)
+    fn = _get_safe_inplace(op.get_inplace()) if inplace else op.op
     sample_inputs_itr = op.sample_inputs(device, dtype, requires_grad=False)
-    new_f = None
 
     # Limit ourselves to first 100 inputs so symbolic tracing tests don't take too long
     for sample_input in itertools.islice(sample_inputs_itr, 100):
@@ -1596,34 +1585,11 @@ def _test_make_fx_helper(self, device, dtype, op, tracing_mode, inplace=False):
         args = [sample_input.input] + list(sample_input.args)
         kwargs = sample_input.kwargs
 
-        # If any argument is a torch.Size(), maybe get dynamic shapes for it by:
-        # - Create a temporary Tensor whose size is the torch.Size() we want. Note that
-        #   we use an expanded Tensor as we cannot pass "meta" Tensors to make_fx.
-        # - Pass it to make_fx such that it is is converted to a proxy Tensor
-        # - Unpack the size in the wrapper to get a torch.Size with dynamic shapes (in
-        #   symbolic mode, a no-op otherwise)
-        extra_args = []
-        extra_kwargs = {}
-        for i, arg in enumerate(args):
-            if isinstance(arg, torch.Size):
-                extra_args.append((i, torch.empty(arg, device="cpu")))
-        for key, value in kwargs.items():
-            if isinstance(value, torch.Size):
-                extra_kwargs[key] = torch.empty(value, device="cpu")
-
         try:
-            new_f = make_fx(f, tracing_mode=tracing_mode)(args, kwargs, extra_args, extra_kwargs)
-        except DynamicOutputShapeException as e:
+            optests.make_fx_check(fn, args, kwargs, tracing_mode, self.assertEqual,
+                                  randomize_data=True)
+        except DynamicOutputShapeException:
             self.skipTest("Dynamic output shape operation in trace")
-        for arg in args:
-            if isinstance(arg, torch.Tensor) and arg.dtype == torch.float:
-                arg.uniform_(0, 1)
-        try:
-            old_out = f(args, kwargs, extra_args, extra_kwargs)
-        except Exception:
-            continue
-        new_out = wrapper_set_seed(new_f, args, kwargs, extra_args, extra_kwargs)
-        self.assertEqual(new_out, old_out)
 
 class TestProxyTensorOpInfo(TestCase):
     @ops(op_db + custom_op_db + control_flow_opinfo_db, allowed_dtypes=(torch.float,))
