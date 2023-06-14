99464992283447220532142ef71fcef3c10dc4e3
Edward Z. Yang <ezyang@meta.com>
2023-06-14 08:21:03 -0700

    Continue simplifying dynamic shapes tests (#103592)
    Remove the static by default / no automatic dynamic configuration as this is about to become the default.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103592
    Approved by: https://github.com/voznesenskym, https://github.com/Skylion007

--- test/dynamo/test_dynamic_shapes.py
+++ test/dynamo/test_dynamic_shapes.py
@@ -25,47 +25,24 @@ except ImportError:
     import test_repros
     import test_subgraphs
 
-import unittest
-
 
 test_classes = {}
 
-ALL_DYNAMIC_XFAILS = {
-    "MiscTests": [],
-    "ReproTests": [
-        # Could not infer dtype of torch._C.SymIntNode
-        "test_convert_boxes_to_pooler_format",
-    ],
-}
 
-XFAIL_HITS = 0
-
-
-def make_dynamic_cls(cls, *, static_default=False):
+def make_dynamic_cls(cls):
     suffix = "_dynamic_shapes"
-    if static_default:
-        suffix += "_static_default"
 
     cls_prefix = "DynamicShapes"
-    if static_default:
-        cls_prefix = f"StaticDefault{cls_prefix}"
 
     test_class = make_test_cls_with_patches(
         cls,
         cls_prefix,
         suffix,
         (config, "dynamic_shapes", True),
-        (config, "assume_static_by_default", static_default),
-        (config, "specialize_int", static_default),
-        xfail_prop="_expected_failure_dynamic" if not static_default else None,
+        (config, "assume_static_by_default", False),
+        (config, "specialize_int", False),
+        xfail_prop="_expected_failure_dynamic",
     )
-
-    xfail_tests = ALL_DYNAMIC_XFAILS.get(cls.__name__)
-    if xfail_tests is not None:
-        global XFAIL_HITS
-        XFAIL_HITS += 1
-        for t in xfail_tests:
-            unittest.expectedFailure(getattr(test_class, f"{t}{suffix}"))
 
     test_classes[test_class.__name__] = test_class
     # REMOVING THIS LINE WILL STOP TESTS FROM RUNNING
@@ -86,55 +63,6 @@ tests = [
 ]
 for test in tests:
     make_dynamic_cls(test)
-    make_dynamic_cls(test, static_default=True)
-
-assert XFAIL_HITS == len(ALL_DYNAMIC_XFAILS) * 2
-
-# Single config failures
-
-unittest.expectedFailure(
-    DynamicShapesMiscTests.test_change_backends_dynamic_shapes
-    # '__torch__.torch.SymInt (of Python compilation unit at: 0x4c9c0e0)'
-    # object has no attribute or method '__ne__'
-    # NB: I don't think this ever can actually work, cuz TorchScript
-    # can't deal with SymInt inputs
-)
-
-
-unittest.expectedFailure(
-    DynamicShapesMiscTests.test_slice_input_dynamic_shapes
-    # NotImplementedError: SymNodeVariable() is not a constant
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module1_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module2_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module3_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module4_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module5_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
-
-unittest.expectedFailure(
-    DynamicShapesNNModuleTests.test_lazy_module6_dynamic_shapes
-    # RuntimeError: SymIntArrayRef expected to contain only concrete integers
-)
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
