1c3e7b835ac302ca333a07526d1074f29d929839
Andres Lugo-Reyes <Andy.LugoReyes@amd.com>
2023-06-14 17:51:29 +0000

    Unskip pca_lowrank as it is also fixed by this

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -7213,7 +7213,6 @@ scipy_lobpcg  | {:10.2e}  | {:10.2e}  | {:6} | N/A
         run_test((1, 1), (1, 1, 1025))
 
     @skipCUDAIfNoCusolver
-    @skipCUDAIfRocm
     @skipCPUIfNoLapack
     def test_pca_lowrank(self, device):
         from torch.testing._internal.common_utils import random_lowrank_matrix, random_sparse_matrix
