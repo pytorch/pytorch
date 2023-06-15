9bd4d61da069225273def6555f331844f64d95ee
Jesse Cai <jcjessecai@gmail.com>
2023-06-15 12:53:53 -0700

    Update base for Update on "[core][pruning][feature] cuSPARSELt build integration"
    This PR modifies the pytorch build process to support CUSPARSELT.
    
    Currently, cuSPARSELt is built as part of libtorch_cuda.so
    
    [ghstack-poisoned]

--- torch/sparse/semi_structured.py
+++ torch/sparse/semi_structured.py
@@ -9,35 +9,34 @@ __all__ = [
     "SparseSemiStructuredTensor",
 ]
 
-CUSPARSELT_CONFIG = namedtuple("CUSPARELT_CONFIG", "compression_factor n min_size")
-DTYPE_TO_CUSPARSELT_CONFIG = {
-    torch.float32 : CUSPARSELT_CONFIG(9, 1, 8),
-    torch.float16 : CUSPARSELT_CONFIG(9, 2, 16),
-    torch.bfloat16 : CUSPARSELT_CONFIG(9, 2, 16),
-    torch.int8: CUSPARSELT_CONFIG(10, 4, 32),
+_CUSPARSELT_CONFIG = namedtuple("_CUSPARSELT_CONFIG", "compression_factor n min_size")
+_DTYPE_TO_CUSPARSELT_CONFIG = {
+    torch.float32 : _CUSPARSELT_CONFIG(9, 1, 8),
+    torch.float16 : _CUSPARSELT_CONFIG(9, 2, 16),
+    torch.bfloat16 : _CUSPARSELT_CONFIG(9, 2, 16),
+    torch.int8: _CUSPARSELT_CONFIG(10, 4, 32),
 }
 
 class SparseSemiStructuredTensor(torch.Tensor):
     """
-    This class implementes semi-structured (2:4) sparsity as a Tensor subclass.
+    This class implementes semi-structured sparsity as a Tensor subclass.
 
     Semi-structured sparsity describes a sparsity pattern where n in every 2n elements are sparse,
-    depending on the datatype. It is most commonly referred to as 2:4 sparsity or fine-grained
+    depending on the datatype. It is also referred to as 2:4 sparsity or fine-grained
     structured sparsity.
 
-    compressed_tensor is a tensor that stores both the kept elemenets and the metadata mask
+    For torch.float32, this tensor subclass implements 1:2 semi-structured sparsity and 2:4 semi-structured
+    sparsity for torch.int8, torch.float16, and torch.bfloat16 datatypes.
+
+    This subclass stores the dense tensor in a compressed form by only storing the specified elemenets and a metadata mask.
     These two are stored next to each other in one contiguous tensor.
 
-    compressed tensor = [ kept elemetns of original tensor |   mask_metadata     ]
+    compressed tensor = [ specified elements of original tensor |   mask_metadata     ]
 
-    For an original tensor of size (m, k) we expect the first m * k // 3 elements to be the kept elements
+    For an original tensor of size (m, k) we expect the first m * k // 2 elements to be the kept elements
     The rest of the tensor is metadata.
 
-    This tensor subclass also has a cslt object, which is set if pytorch is built with cuSPARSELt support.
-    In this case, we store some additional metadata containg the sparse matrix descriptor in order for
-    faster matmul performance.
-
-    In the future, when this cslt object is not set, we can use _structured_sparse_linear.
+    This subclass also overrides __torch_dispatch__ to use cuSPARSELt for faster matrix multiplications.
     """
 
     # When _fuse_transpose is set to True, we fuse a .T into the cuSPASRELt matmul operation.
@@ -67,18 +66,31 @@ class SparseSemiStructuredTensor(torch.Tensor):
 
     def __init__(
         self,
-        custom_shape,
+        custom_shape: torch.Size,
         compressed_tensor: torch.Tensor,
         cslt,
-        transposed,
-    ):
+        transposed: bool,
+    ) -> None:
+        """This function is the constuctor for SparseSemiStructured tensor.
+
+        Args:
+            custom_shape: The shape of the original dense tensor
+            compressed_tensor: A flattened tensor to store the specified elements and mask metadata.
+            cslt: The second parameter.
+            transposed: Whether to transpose the compressed_tensor before passing it to the cuSPARSELT.
+
+        Returns:
+            None
+        """
         self.compressed_tensor = compressed_tensor
         self.cslt = cslt
         self.transposed = transposed
 
 
     def __repr__(self):
-        return f"SparseSemiStructuredTensor(shape={self.shape} \n metadata={self.indices()} \n values={self.values()})"
+        return (f"SparseSemiStructuredTensor(shape={self.shape} "
+                "metadata={self.indices()}"
+                "values={self.values()})")
 
     __torch_function__ = torch._C._disabled_torch_function_impl
 
@@ -133,8 +145,8 @@ class SparseSemiStructuredTensor(torch.Tensor):
             raise NotImplementedError(
                 (
                     f"func: {func} is currently not supported for ",
-                    f"opA: {input_A.transposed} A: {input_A}",
-                    f"opB: {input_B.transposed} B {input_B}",
+                    f"opA transposed: {input_A.transposed} A: {input_A}",
+                    f"opB transposed: {input_B.transposed} B: {input_B}",
                 )
             )
 
@@ -154,8 +166,8 @@ class SparseSemiStructuredTensor(torch.Tensor):
             raise NotImplementedError(
                 (
                     f"func: {func} is currently not supported for ",
-                    f"opA: {input_A.transposed} A: {input_A}",
-                    f"opB: {input_B.transposed} B {input_B}",
+                    f"opA transposed: {input_A.transposed} A: {input_A}",
+                    f"opB transposed: {input_B.transposed} B: {input_B}",
                 )
             )
 
@@ -189,12 +201,11 @@ def to_sparse_semi_structured(
 ) -> SparseSemiStructuredTensor:
     """
     This function converts a dense tensor into a sparse semi-structured tensor.
-    It will return a SparseSemiStructuredTensor, a subclass of torch.Tensor. This subclass is
-    responsible for overriding __torch_dispatch__ for accelerated sparse matmul.
+    It will return a SparseSemiStructuredTensor, a subclass of torch.Tensor.
 
     This function will check to ensure the dense tensor has the right dtype, size, dims, and device.
     We currently only support semi-structured sparse tensors for 2d CUDA tensors.
-    Additionally, your tensor must be a multiple of a block size given the dtype
+    Additionally, your tensor must be a positive multiple of a block size given the dtype
 
     - torch.float32  (r, c) must be >= and a multiple of 8
     - torch.float16  (r, c) must be >= and a multiple of 16
@@ -282,13 +293,13 @@ def to_sparse_semi_structured(
                             "Only 2d tensors are currently supported."))
 
     # check dtype
-    if original_tensor.dtype not in DTYPE_TO_CUSPARSELT_CONFIG:
+    if original_tensor.dtype not in _DTYPE_TO_CUSPARSELT_CONFIG:
         raise RuntimeError((f"Error original_tensor.dtype {original_tensor.dtype} is not a supported dtype! "
-                            "dtype must be one of: {DTYPE_TO_CUSPARSELT_CONFIG}"))
+                            "dtype must be one of: {_DTYPE_TO_CUSPARSELT_CONFIG}"))
 
     # check shape
     m, n = original_tensor.shape
-    min_size = DTYPE_TO_CUSPARSELT_CONFIG[original_tensor.dtype].min_size
+    min_size = _DTYPE_TO_CUSPARSELT_CONFIG[original_tensor.dtype].min_size
     if m < min_size or m % min_size or n < min_size or n % min_size:
         # TODO add padding here
         raise RuntimeError((f"Error original_tensor.shape {original_tensor.shape} is not supported! "
@@ -298,7 +309,7 @@ def to_sparse_semi_structured(
     # This code calculates the size of the compressed tensor.
     # compression factor is different based on dtype
     num_bytes = original_tensor.nelement() * original_tensor.element_size()
-    compression_factor = DTYPE_TO_CUSPARSELT_CONFIG[original_tensor.dtype].compression_factor
+    compression_factor = _DTYPE_TO_CUSPARSELT_CONFIG[original_tensor.dtype].compression_factor
     compressed_size_bytes = num_bytes * compression_factor // 16
     compressed_size = compressed_size_bytes // original_tensor.element_size()
 
@@ -310,7 +321,7 @@ def to_sparse_semi_structured(
 
     # try to use cuSPARSELt
     # TODO default to CUTLASS is cuSPARELt is not avaliable
-    cslt = torch.classes.cusparselt.CusparseLt(compressed_tensor)
+    cslt = torch._C.sparse.cusparselt(compressed_tensor)
     # TODO Add option to prune tensor within cuSPARSELt
     cslt.compress(original_tensor, transposed)
     return SparseSemiStructuredTensor(
