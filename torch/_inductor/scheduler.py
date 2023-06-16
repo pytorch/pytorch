b6fc05df09b551e199f30ee1162e490562c55c1a
FindHao <find@findhao.net>
2023-06-16 11:55:13 -0700

    remove the redundant check

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -267,11 +267,7 @@ class BaseSchedulerNode:
                 input_node: BaseSchedulerNode = self.scheduler.name_to_node.get(
                     read.name
                 )
-                if (
-                    input_node
-                    and config.inplace_buffers
-                    and V.graph.wrapper_code.can_reuse(input_node)
-                ):
+                if input_node and V.graph.wrapper_code.can_reuse(input_node):
                     remaining_uses = [
                         x
                         for x in input_node.users
