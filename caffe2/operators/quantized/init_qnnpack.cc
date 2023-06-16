8e6aa71235711c082f34bf5a21af8a65aa6d19e9
cyy <cyyever@outlook.com>
2023-06-16 14:41:56 +0800

    add missing inclusions

--- caffe2/operators/quantized/init_qnnpack.cc
+++ caffe2/operators/quantized/init_qnnpack.cc
@@ -3,10 +3,11 @@
 #include <qnnpack.h>
 
 #include "caffe2/core/logging.h"
+#include "caffe2/operators/quantized/int8_utils.h"
 
 namespace caffe2 {
 
-static void initQNNPACK() {
+void initQNNPACK() {
   static std::once_flag once;
   static enum qnnp_status qnnpackStatus = qnnp_status_uninitialized;
   std::call_once(once, []() { qnnpackStatus = qnnp_initialize(); });
