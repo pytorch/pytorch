deafcdda01abbff66823648494c9d9f4b39ec83f
Catherine Lee <csl@fb.com>
2023-06-16 17:29:33 -0700

    try catch for getting commit messages

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -457,9 +457,13 @@ def parse_reenabled_issues(s: str) -> List[str]:
 
 def get_reenabled_issues(pr_body: str = "") -> List[str]:
     default_branch = os.getenv("GIT_DEFAULT_BRANCH", "main")
-    commit_messages = subprocess.check_output(
-        f"git cherry -v {default_branch}".split(" ")
-    ).decode("utf-8")
+    try:
+        commit_messages = subprocess.check_output(
+            f"git cherry -v {default_branch}".split(" ")
+        ).decode("utf-8")
+    except Exception as e:
+        warnings.warn(f"failed to get commit messages: {e}")
+        commit_messages = ""
     return parse_reenabled_issues(pr_body) + parse_reenabled_issues(commit_messages)
 
 
