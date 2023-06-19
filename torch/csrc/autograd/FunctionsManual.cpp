eeeb0b59f75764b3ce6665feeb1a5cc8d3fbbde8
Nikita Vedeneev <nik@quansight.com>
2023-06-19 13:50:43 +0000

    Update base for Update on "sparse gradcheck: reparametrize some tests to remove masked=True"
    Most of the sparse functions that work with sparse tensors assume that sparse is an optimization, so a green check with `masked=False` will imply success with `masked=True`. Functions that assume the sparse semantics and do not explicitly ignore grads outside of the sparse pattern can be re-parametrized with `torch.sparse_mask` so that the gradcheck succeeds when `masked=False`. Hence, we can remove `masked=True` altogether. 
    
    
    
    
    cc alexsamardzic pearu cpuhrsch amjames bhosmer ezyang albanD zou3519 gqchen soulitzer Lezcano Varal7
    
    [ghstack-poisoned]

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -1484,7 +1484,9 @@ Tensor sparse_mask_backward(
     const c10::Layout self_layout) {
   // NOTE: sparse_mask accumulates matches, so the backward step has to
   // accumulate as well.
-  return sparse_mask_like_grad(mask, grad, /*accumulate_matches=*/true);
+  const auto self_grad =
+      sparse_mask_like_grad(mask, grad, /*accumulate_matches=*/true);
+  return self_layout == at::kStrided ? self_grad.to_dense() : self_grad;
 }
 
 Tensor sparse_sparse_matmul_backward(
