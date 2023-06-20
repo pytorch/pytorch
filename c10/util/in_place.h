c1c83ef12f2c6fa3b91c1b882c6968bf0a09a97a
Richard Barnes <rbarnes@meta.com>
2023-06-20 09:35:35 -0700

    Eliminate c10/util/variant from PyTorch
    Test Plan: Sandcastle
    
    Differential Revision: D46772743
    
    fbshipit-source-id: 229df7bee23cbc9fdce6c22514665913433cdd1e

--- c10/util/in_place.h
+++ c10/util/in_place.h
@@ -1,22 +1,16 @@
 #pragma once
 
-#include <cstddef>
+#include <utility>
 
 namespace c10 {
 
-struct in_place_t {
-  explicit in_place_t() = default;
-};
+using in_place_t = std::in_place_t;
 
-template <std::size_t I>
-struct in_place_index_t {
-  explicit in_place_index_t() = default;
-};
+template<std::size_t I>
+using in_place_index_t = std::in_place_index_t<I>;
 
 template <typename T>
-struct in_place_type_t {
-  explicit in_place_type_t() = default;
-};
+using in_place_type_t = std::in_place_type_t<T>;
 
 constexpr in_place_t in_place{};
 
