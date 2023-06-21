f01ddb8a353cc9dcf1c0615392e18853cdbc8075
fduwjj <fduwjj@fb.com>
2023-06-21 05:31:04 +0000

    Update on "[2D][TP] Add DDP TP integration and unit test"
    Differential Revision: [D46731700](https://our.internmc.facebook.com/intern/diff/D46731700)
    
    [ghstack-poisoned]

--- test/distributed/tensor/parallel/test_tp_ddp_2d.py
+++ test/distributed/tensor/parallel/test_tp_ddp_2d.py
@@ -8,6 +8,8 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed.tensor.parallel import PairwiseParallel, parallelize_module
 from torch.distributed._tensor import DeviceMesh, DTensor, Replicate
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
+from torch.distributed.algorithms.ddp_comm_hooks.default_hooks import allreduce_hook
+from torch.distributed.tensor.parallel.fsdp import _STShardingInfo
 
 from torch.testing._internal.common_utils import run_tests
 
@@ -57,16 +59,68 @@ def init_model(model_parallel_size=TP_DEGREE):
 
     # Create Input
     twod_model = parallelize_module(twod_model, twod_mesh, PairwiseParallel(), tp_mesh_dim=1)
+    module_list = []
+
+    # We need to find a way for DDP to replicate local tensor of DTensor.
+    for name, param in twod_model.named_parameters():
+        if isinstance(param, DTensor):
+            t = param._local_tensor.requires_grad_()
+            parent_module = twod_model
+            module_path = name
+            if "." in module_path:
+                parent_module_path = ".".join(name.split(".")[:-1])
+                parent_module = twod_model.get_submodule(parent_module_path)
+                module_path = module_path.split(".")[-1]
+            t = torch.nn.Parameter(t)
+            t._st_info = _STShardingInfo(
+                None,
+                None,
+                None,
+                param.device_mesh,
+                list(param.placements),
+            )
+            module_list.append((parent_module, module_path, t))
+    for item in module_list:
+        parent_module, module_path, t = item
+        if hasattr(parent_module, module_path):
+            delattr(parent_module, module_path)
+        setattr(parent_module, module_path, t)
+
+
     twod_model = DDP(twod_model, process_group=dp_pg, find_unused_parameters=True)
-    def backward_hook(module, grad_input, grad_output):
-        return (grad_output[0] / model_parallel_size, )
-    # twod_model.register_backward_hook(backward_hook)
+
+    # We also need a pre-forward hook to ensure gradients get propagated back.
+    def forward_hook(module, input):
+        module_list = []
+        for name, t in module.named_parameters():
+            if hasattr(t, "_st_info"):
+                dtensor = DTensor.from_local(
+                    t,
+                    device_mesh=t._st_info.device_mesh,
+                    placements=t._st_info.placements,
+                    run_check=False,
+                )
+                parent_module = module
+                module_path = name
+                if "." in module_path:
+                    parent_module_path = ".".join(name.split(".")[:-1])
+                    parent_module = twod_model.get_submodule(parent_module_path)
+                    module_path = module_path.split(".")[-1]
+                module_list.append((parent_module, module_path, dtensor))
+        for item in module_list:
+            parent_module, module_path, t = item
+            if hasattr(parent_module, module_path):
+                delattr(parent_module, module_path)
+            # Using nn.parameter(t) will lose grad_fn.
+            setattr(parent_module, module_path, t)
+        return input
+    twod_model.register_forward_pre_hook(forward_hook)
 
     return model, twod_model, dp_pg
 
 
 class Test2dParallelIntegration(DTensorTestBase):
-    def _check_module(self, m1, m2, rank, check_grad=False):
+    def _check_module(self, m1, m2, check_grad=False):
         named_parameters = dict(m1.named_parameters())
         for name, param_m2 in m2.named_parameters():
             self.assertTrue(name in named_parameters)
@@ -79,9 +133,7 @@ class Test2dParallelIntegration(DTensorTestBase):
                 param_m2 = param_m2.redistribute(
                     device_mesh=param_m2.device_mesh, placements=replicate
                 ).to_local()
-            # if rank in [0, 2]:
-            #     print(param_m2, param_m1, param_m2 - param_m1)
-            # self.assertEqual(param_m2, param_m1)
+            self.assertEqual(param_m2, param_m1)
 
     @with_comms
     @skip_if_lt_x_gpu(4)
@@ -94,7 +146,6 @@ class Test2dParallelIntegration(DTensorTestBase):
         input_seed = dist.get_rank(dp_pg)
         torch.manual_seed(input_seed + 1)
         input = torch.rand(4, 5).cuda(self.rank)
-        # self._check_module(model, twod_model)
 
         output = model(input)
         twod_output = twod_model(input)
@@ -102,19 +153,20 @@ class Test2dParallelIntegration(DTensorTestBase):
 
         output.sum().backward()
         twod_output.sum().backward()
-        print("_check_reducer_finalized", twod_model._check_reducer_finalized())
-        self._check_module(model, twod_model, self.rank, check_grad=True)
-        # return
+        self._check_module(model, twod_model, check_grad=True)
+
         optim.step()
         twod_optim.step()
 
         torch.manual_seed(input_seed + 1004)
         input = torch.rand(16, 5).cuda(self.rank)
-        # self._check_module(model, twod_model)
+        self._check_module(model, twod_model)
 
         output = model(input)
         twod_output = twod_model(input)
-        # self.assertEqual(output, twod_output)
+        self.assertEqual(output, twod_output)
+
+        # TODO: Add save/load of 2D verification.
 
 
 if __name__ == "__main__":
