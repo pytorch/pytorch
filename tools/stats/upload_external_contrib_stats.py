442a961fc0dda6b37cfad0a308d03a2510045052
PaliC <sahancpal@gmail.com>
2023-06-21 10:53:53 -0700

    refactor upload_external_contrib_stat to use github_utils
    [ghstack-poisoned]

--- tools/stats/upload_external_contrib_stats.py
+++ tools/stats/upload_external_contrib_stats.py
@@ -1,13 +1,10 @@
 import argparse
 import datetime
-import json
-import os
 
 import time
-import urllib.parse
-from typing import Any, Callable, cast, Dict, List, Optional, Set
-from urllib.error import HTTPError
-from urllib.request import Request, urlopen
+from typing import Any, Dict, List, Set
+
+from tools.github.github_utils import gh_fetch_json_dict
 
 from tools.stats.upload_stats_lib import upload_to_s3
 
@@ -19,48 +16,6 @@ FILTER_OUT_USERS = {
     "pytorchupdatebot",
     "dependabot[bot]",
 }
-
-
-def _fetch_url(
-    url: str,
-    headers: Dict[str, str],
-    data: Optional[Dict[str, Any]] = None,
-    method: Optional[str] = None,
-    reader: Callable[[Any], Any] = lambda x: x.read(),
-) -> Any:
-    token = os.environ.get("GITHUB_TOKEN")
-    if token is not None and url.startswith("https://api.github.com/"):
-        headers["Authorization"] = f"token {token}"
-    data_ = json.dumps(data).encode() if data is not None else None
-    try:
-        with urlopen(Request(url, headers=headers, data=data_, method=method)) as conn:
-            return reader(conn)
-    except HTTPError as err:
-        print(err.reason)
-        print(err.headers)
-        if err.code == 403 and all(
-            key in err.headers for key in ["X-RateLimit-Limit", "X-RateLimit-Used"]
-        ):
-            print(
-                f"Rate limit exceeded: {err.headers['X-RateLimit-Used']}/{err.headers['X-RateLimit-Limit']}"
-            )
-        raise
-
-
-def fetch_json(
-    url: str,
-    params: Optional[Dict[str, Any]] = None,
-    data: Optional[Dict[str, Any]] = None,
-) -> List[Dict[str, Any]]:
-    headers = {"Accept": "application/vnd.github.v3+json"}
-    if params is not None and len(params) > 0:
-        url += "?" + "&".join(
-            f"{name}={urllib.parse.quote(str(val))}" for name, val in params.items()
-        )
-    return cast(
-        List[Dict[str, Any]],
-        _fetch_url(url, headers=headers, data=data, reader=json.load),
-    )
 
 
 def get_external_pr_data(
@@ -76,17 +31,14 @@ def get_external_pr_data(
         page = 1
         responses: List[Dict[str, Any]] = []
         while len(responses) > 0 or page == 1:
-            response = cast(
-                Dict[str, Any],
-                fetch_json(
-                    "https://api.github.com/search/issues",
-                    params={
-                        "q": f'repo:pytorch/pytorch is:pr is:closed \
+            response = gh_fetch_json_dict(
+                url="https://api.github.com/search/issues",
+                params={
+                    "q": f'repo:pytorch/pytorch is:pr is:closed \
                             label:"open source" label:Merged -label:Reverted closed:{period_begin_date}..{period_end_date}',
-                        "per_page": "100",
-                        "page": str(page),
-                    },
-                ),
+                    "per_page": "100",
+                    "page": str(page),
+                },
             )
             items = response["items"]
             for item in items:
