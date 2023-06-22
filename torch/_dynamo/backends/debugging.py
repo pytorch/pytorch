d2c9f2965ab3860f986f886b4a679f1fe9f6503b
Animesh Jain <anijain@umich.edu>
2023-06-22 06:44:36 +0000

    Update base for Update on "[debugging] aot_eager backend to use the min-cut partitioner"
    default_partitioner is kind of broken when it comes to memory footprint. Moving aot_eager to use min-cut partitioner is better debugging experience.
    
    One bad thing though would be that we will much lower speedup numbers, because min cut partitioner will try to recompute ops.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx ipiszy chenyang78 msaroufim
    
    [ghstack-poisoned]

--- torch/_dynamo/backends/debugging.py
+++ torch/_dynamo/backends/debugging.py
@@ -1,11 +1,13 @@
 import dataclasses
+import functools
 from importlib import import_module
 from typing import Any, List, Optional
+
+from functorch.compile import min_cut_rematerialization_partition
 
 import torch
 from torch import _guards
 from torch._functorch.compilers import ts_compile
-from torch._inductor.compile_fx import inductor_partition_fn
 from .common import aot_autograd
 from .registry import register_debug_backend as register_backend
 
@@ -65,7 +67,9 @@ aot_eager_decomp_partition = aot_autograd(
     decompositions=lambda: import_module(
         "torch._inductor.compile_fx"
     ).select_decomp_table(),
-    partition_fn=inductor_partition_fn,
+    partition_fn=functools.partial(
+        min_cut_rematerialization_partition, compiler="inductor"
+    ),
 )
 register_backend(
     name="aot_eager_decomp_partition", compiler_fn=aot_eager_decomp_partition
