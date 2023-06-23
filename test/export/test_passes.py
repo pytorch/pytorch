344bab2669fb75477f5d49a1a7c00b609376302c
xuanqi <xuanqi@meta.com>
2023-06-23 13:59:48 -0700

    [RFC]: Functionalize assertions (#103757)
    The idea here is to create do a graph mutation to:
    * Create an initial dependency token at the beginning of the program.
    * Replace non-functional version of assertion statements to functional version.
    * The functional version of assertion statement will:
      * Accept a dependency token from output of previous functional assertion statement (or the initial dependency token if there isn't any).
      * Generate a dependency token as the output of assertion statement.
      * Augment the output to include the dependency token generated by last assertion statement.
    
    The goal here is to:
    * Form an explicit dependency chain and avoid potential reordering during other passes of compiling.
    * Make the assertions a part of overall execution graph will affect the final output (or it could potentially be DCEed).
    
    **NOTE:**
    * Currently only cover `contrain_range` and WIP to support other assertions. Send out this PR to collect feedback first.
    * Here it only focus on implementation itself. Will integrate it with current export in future PR.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103757
    Approved by: https://github.com/avikchaudhuri

--- test/export/test_passes.py
+++ test/export/test_passes.py
@@ -3,15 +3,19 @@ import unittest
 
 import torch
 from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.testing import FileCheck
 from torch._dynamo.eval_frame import is_dynamo_supported
 from torch._export import export, dynamic_dim
-from torch._export.constraints import constrain_as_value
+from torch._export.constraints import constrain_as_value, constrain_as_size
 from torch._export.passes import (
     ReplaceViewOpsWithViewCopyOpsPass,
 )
 from torch._export.passes.replace_view_ops_with_view_copy_ops_pass import (
     is_view_op,
     get_view_copy_of_view_op,
+)
+from torch._export.passes.functionalize_side_effectful_ops_pass import (
+    _FunctionalizeSideEffectfulOpsPass,
 )
 from functorch.experimental.control_flow import cond
 
@@ -334,6 +338,81 @@ class TestPasses(TestCase):
         dynamo_result = exported(x, y)
         real_result = m(x, y)
         self.assertTrue(torch._dynamo.utils.same(real_result, dynamo_result))
+
+    def test_functionalize_inline_contraints(self) -> None:
+        def f(x):
+            a = x.item()
+            constrain_as_size(a, 4, 7)
+            return torch.empty((a, 4))
+
+        ep = torch._export.export(f, (torch.tensor([7]),))
+        gm = ep.graph_module
+        FileCheck().check_count(
+            "torch.ops.aten.sym_constrain_range.default",
+            1,
+            exactly=True,
+        ).run(gm.code)
+
+        gm = ep.transform(_FunctionalizeSideEffectfulOpsPass()).graph_module
+
+        with self.assertRaisesRegex(
+            RuntimeError,
+            r"_local_scalar_dense_default is outside of inline constraint \[4, 7\]",
+        ) as cm:
+            gm(torch.tensor([20]))
+
+        inp = torch.tensor([5])
+        res, dep_token = gm(inp)
+        self.assertEqual(res.shape, torch.Size([5, 4]))
+        self.assertEqual(dep_token.shape, torch.Size([]))
+
+        FileCheck().check_count(
+            "torch.ops.aten._functional_sym_constrain_range", 1, exactly=True
+        ).run(gm.code)
+        FileCheck().check_count(
+            "torch.ops.aten.sym_constrain_range.default", 0, exactly=True
+        ).run(gm.code)
+
+        dep_token_node = next(n for n in gm.graph.nodes if n.name == "dep_token3")
+        constrain_node = next(
+            n
+            for n in gm.graph.nodes
+            if n.target == torch.ops.aten._functional_sym_constrain_range
+        )
+        self.assertEqual(constrain_node.kwargs["dep_token"], dep_token_node)
+
+    def test_functionalize_input_constraints(self) -> None:
+        def f(x):
+            return x * 2
+
+        inp = torch.zeros(4, 8)
+        ep = torch._export.export(
+            f,
+            (inp,),
+            constraints=[
+                dynamic_dim(inp, 0) < 10,
+                dynamic_dim(inp, 0) >= 3,
+            ],
+        )
+        FileCheck().check_count(
+            "torch.ops.aten._assert_async.msg", 3, exactly=True
+        ).run(ep.graph_module.code)
+
+        gm = ep.transform(_FunctionalizeSideEffectfulOpsPass()).graph_module
+        with self.assertRaisesRegex(
+            RuntimeError,
+            r"Input arg0_1.shape\[0\] is outside of specified dynamic range \[3, 9\]",
+        ):
+            gm(torch.ones(11, 8))
+
+        inp = torch.ones(6, 8)
+        self.assertEqual(gm(inp)[0], f(inp))
+        FileCheck().check_count(
+            "torch.ops.aten._functional_assert_async.msg", 3, exactly=True
+        ).run(gm.code)
+        FileCheck().check_count(
+            "torch.ops.aten._assert_async.msg", 0, exactly=True
+        ).run(gm.code)
 
 
 if __name__ == '__main__':
