3b26649b7f476bf745f9425ee96681b8ec291032
Eddie Yan <eddiey@nvidia.com>
2023-06-23 20:23:50 +0000

    check in

--- aten/src/ATen/native/utils/ParamsHash.h
+++ aten/src/ATen/native/utils/ParamsHash.h
@@ -38,5 +38,56 @@ struct ParamsEqual {
   }
 };
 
+// Provide explicit byte-for-byte constructors to avoid uwittingly leaving
+// padding bytes unitialized (e.g., when passing Params by value)
+template <typename T>
+struct ParamsWrapper {
+  T pod;
+
+  ParamsWrapper() {
+    memset(&(this->pod), 0, sizeof(T));
+  }
+
+  ParamsWrapper(const ParamsWrapper<T> &other) {
+    memcpy(&(this->pod), &(other.pod), sizeof(T));
+  }
+
+  ParamsWrapper(ParamsWrapper<T> &&other) {
+    memcpy(&(this->pod), &(other.pod), sizeof(T));
+  }
+};
+
+// Wrapped version: this allows the outer struct to have custom copy and move
+// constructors for additional safety
+template <typename ParamsWrapper>
+struct ParamsWrapperHash {
+  // Params must be a POD because we read out its memory
+  // contents as char* when hashing
+  static_assert(std::is_standard_layout_v<decltype(ParamsWrapper::pod)>, "ParamsWrapper wraps non-POD data");
+
+  size_t operator()(const ParamsWrapper& params_wrapper) const {
+    auto ptr = reinterpret_cast<const uint8_t*>(&(params_wrapper.pod));
+    uint32_t value = 0x811C9DC5;
+    for (const auto i : c10::irange(sizeof(params_wrapper.pod))) {
+      value ^= ptr[i];
+      value *= 0x01000193;
+    }
+    return (size_t)value;
+  }
+};
+
+template <typename ParamsWrapper>
+struct ParamsWrapperEqual {
+  // Params must be a POD because we read out its memory
+  // contents as char* when comparing
+  static_assert(std::is_standard_layout_v<decltype(ParamsWrapper::pod)>, "ParamsWrapper wraps non-POD data");
+
+  bool operator()(const ParamsWrapper& a, const ParamsWrapper& b) const {
+    auto ptr1 = reinterpret_cast<const uint8_t*>(&(a.pod));
+    auto ptr2 = reinterpret_cast<const uint8_t*>(&(b.pod));
+    return memcmp(ptr1, ptr2, sizeof(a.pod)) == 0;
+  }
+};
+
 
 }  // at::native
