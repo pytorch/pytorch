4b6fc254cc20cfda8ebc1f712bec5a8973c13494
xuanqi <xuanqi@meta.com>
2023-06-26 11:48:20 -0700

    Update on "[RFC]: Eager mode functionalization"
    [ghstack-poisoned]

--- test/export/test_funtionalized_assertions.py
+++ test/export/test_funtionalized_assertions.py
@@ -1,6 +1,19 @@
+"""
+PYTEST_DONT_REWRITE (prevents pytest from rewriting assertions, which interferes
+with test_eager_mode_functionalization)
+"""
+
 # Owner(s): ["module: dynamo"]
+
+from typing import List
+
 import torch
 from torch.testing._internal.common_utils import run_tests, TestCase
+from functorch.compile import make_boxed_func
+import torch._functorch.config as config
+from contextlib import contextmanager
+from torch._dynamo.backends.common import aot_autograd
+from torch.testing import FileCheck
 
 
 class TestFuntionalAssertions(TestCase):
@@ -25,6 +38,36 @@ class TestFuntionalAssertions(TestCase):
             ),
             dep_token,
         )
+
+    def test_eager_mode_functionalization(self) -> None:
+        def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
+            FileCheck().check_count(
+                "aten._functional_assert_async.msg", 1, exactly=True,
+            ).run(gm.code)
+            return make_boxed_func(gm.forward)
+
+        my_compiler = aot_autograd(fw_compiler=my_compiler)
+
+        def f(x):
+            b = x.sin()
+            assert x[0] == 3
+            return x.cos() + b
+
+        with _enable_functionalization():
+            compiled = torch.compile(f, backend=my_compiler)
+            inp = torch.Tensor([3, 4, 5])
+            compiled(inp)
+            # self.assertTrue(torch._dynamo.utils.same(compiled(inp), f(inp)))
+
+
+@contextmanager
+def _enable_functionalization():
+    functionalize = config.functionalize_assertion_ops
+    config.functionalize_assertion_ops = True
+    try:
+        yield
+    finally:
+        config.functionalize_assertion_ops = functionalize
 
 
 if __name__ == "__main__":
