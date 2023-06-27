2d14395f176b38b8416c2713d285e5ae55695a5f
Elias Ellison <elias.ellison@gmail.com>
2023-06-27 23:25:57 +0000

    Re-enable low memory dropout (#103330)
    On attention_is_all_you_need_pytorch:
    
    Perf: 1.526x -> 1.544x
    Memory: 1.00 -> 1.05x
    
    Fix for https://github.com/pytorch/pytorch/issues/102319, although I'm not sure all the perf is recovered.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103330
    Approved by: https://github.com/jansel

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -1518,7 +1518,6 @@ def inductor_random(size: List[int], seed: TensorBox, mode: str, *, offset: int 
         inner_fn=inner_fn,
         ranges=[*size],
     )
-    result.realize()
     return result
 
 
