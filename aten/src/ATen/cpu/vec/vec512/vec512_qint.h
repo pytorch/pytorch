dfdbef64cf7e1c7304d9145443d29a6085ecf09e
leslie-fang-intel <leslie.fang@intel.com>
2023-06-27 17:37:07 +0800

    Update base for Update on "Add UT to combine dynamo export and inductor constant folding"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy ngimel yf225 chenyang78
    
    [ghstack-poisoned]

--- aten/src/ATen/cpu/vec/vec512/vec512_qint.h
+++ aten/src/ATen/cpu/vec/vec512/vec512_qint.h
@@ -98,18 +98,19 @@ inline __m512i pack_saturate_and_clamp<uint8_t>(
       _mm512_min_epu8(packed_and_sat, _mm512_set1_epi8(max_val)));
 }
 
-inline Vectorized<float> load_uint8_as_float(const uint8_t* src_data) {
-  // Load 16*uint8
-  __m128i input_128 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(src_data));
+inline Vectorized<float> convert_uint8_to_float(at::vec::Vectorized<uint8_t> src) {
+  // Note: this function only convert inputs number of elements equal to at::vec::Vectorized<float>.size()
+  // Only handle first 128 bits
+  __m128i input_128 = _mm512_castsi512_si128(src);
   // Convert from 16*u8 to 16*int32
   __m512i input_512_extended = _mm512_cvtepu8_epi32(input_128);
   // Convert from 16*int32 to 16*float32
   return _mm512_cvtepi32_ps(input_512_extended);
 }
 
-inline void store_float_as_uint8(at::vec::Vectorized<float> values, uint8_t* dst_data) {
+inline Vectorized<uint8_t> convert_float_to_uint8(at::vec::Vectorized<float> src) {
   // Convert from float32 to int32
-  __m512i x_values_int32 = _mm512_cvtps_epi32(values);
+  __m512i x_values_int32 = _mm512_cvtps_epi32(src);
 
   // Convert from int32 to int16 using signed saturation
   __m512i xy_packed_v = _mm512_packs_epi32(x_values_int32, x_values_int32);
@@ -123,12 +124,7 @@ inline void store_float_as_uint8(at::vec::Vectorized<float> values, uint8_t* dst
   __m512i permute_mask_v =
       _mm512_set_epi32(0x0f, 0x0b, 0x07, 0x03, 0x0e, 0x0a, 0x06, 0x02,
                       0x0d, 0x09, 0x05, 0x01, 0x0c, 0x08, 0x04, 0x00);
-  xyzw_clamped_v = _mm512_permutexvar_epi32(permute_mask_v, xyzw_clamped_v);
-
-  // Store to dst
-  _mm_storeu_si128(
-    reinterpret_cast<__m128i*>(dst_data),
-    _mm512_castsi512_si128(xyzw_clamped_v));
+  return _mm512_permutexvar_epi32(permute_mask_v, xyzw_clamped_v);
 }
 
 template <typename T>
