Update base for Update on "sparse.sum backward: fix gradient projection"
Fixes https://github.com/pytorch/pytorch/issues/99147 temporarily. I believe we can reuse our intersection kernels to refactor the backward function and make it more performant.




cc alexsamardzic pearu cpuhrsch amjames bhosmer ezyang albanD zou3519 gqchen soulitzer Lezcano Varal7

[ghstack-poisoned]

test/test_sparse.py
@@ -2163,9 +2163,8 @@ class TestSparse(TestSparseBase):
                     lhs_data.shape
                 )._coalesced_(rhs_is_coalesced)
 
-                # setting masked = True is required because of the broken backward of to_dense().
-                # See https://github.com/pytorch/pytorch/issues/95550.
-                gradcheck(lambda x: x.sparse_mask(rhs).to_dense(), (lhs,), masked=True, check_sparse_nnz=True)
+                gradcheck(lambda x: x.sparse_mask(rhs).to_dense(masked_grad=True), (lhs,), masked=True)
+                gradcheck(lambda x: x.sparse_mask(rhs).to_dense(masked_grad=False), (lhs,), masked=False)
 
     @coalescedonoff
     @dtypes(torch.double, torch.cdouble)