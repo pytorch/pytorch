b2d4fb52357bf5c6dfe6e6b409f8382c506e6793
Richard Zou <zou3519@gmail.com>
2023-06-30 15:01:31 -0700

    Deprecate registering autograd kernels at not an autograd key
    Context
    -------
    This PR adds a new fallback to the Autograd dispatch keys.
    
    If you would prefer the old behavior:
    - A quick (unsupported) way to get the previous behavior is to call
    `torch._C._set_autograd_fallback("nothing")`
    - Register "torch::CppFunction::makeFallthrough()" to your Autograd key,
    like in https://gist.github.com/zou3519/d09a5f4b1afe2430af09fea67c6ff2c8
    ```
    
    It is possible that this PR regresses performance of overhead-bound
    models. If this is the case, please reach out (and apply one of the
    temporary fixes in the previous section).
    
    Description for reviewers
    -------------------------
    In order to deprecate registering autograd kernels at not an autograd
    key, we add a fallback to the Autograd dispatch keys. This fallback
    raises a warning if the user attempts to backprop through the operator.
    
    The goal of this PR is to
    - preserve as much BC as possible
    - raise a warning that whatever the user is doing is potentially wrong.
    - be as performant as possible
    
    There are roughly two cases:
    - if the post-autograd kernels return a Tensor that requires grad, then
    we install an autograd hook that raises a warning. We are preserving BC
    in that it is possible that the user has a torch::autograd::Function
    registered to their CPU key.
    - if the post-autograd kernels return Tensors that do not require grad,
    then we make them require_grad and install a WarnNotImplemented grad fn
    that warns in the backward pass. This is mildy BC-breaking (see next
    section).
    
    Test Plan:
    - bunch of new tests
    
    BC-Breaking Note
    ----------------
    TODO(rzou)
    
    [ghstack-poisoned]

--- torch/csrc/autograd/autograd_not_implemented_fallback.h
+++ torch/csrc/autograd/autograd_not_implemented_fallback.h
@@ -5,9 +5,25 @@
 namespace torch {
 namespace autograd {
 
+// DispatchKey::Autograd fallback for built-in operators
 TORCH_API torch::CppFunction autogradNotImplementedFallback();
 
+// DispatchKey::AdInplaceOrView fallback for built-in operators
 TORCH_API torch::CppFunction autogradNotImplementedInplaceOrViewFallback();
+
+// DispatchKey::Autograd fallback for all other operators (i.e. custom
+// operators)
+TORCH_API torch::CppFunction basicAutogradNotImplementedFallback();
+
+enum class AutogradFallbackMode {
+  Nothing, // Fallback is a redispatch
+  Warn, // Fallback raises a warning if backward is called
+  Error, // Fallback raises an error if backward is called
+};
+
+// Change the behavior of "basicAutogradNotImplementedFallback"
+TORCH_API void setAutogradFallbackMode(AutogradFallbackMode mode);
+TORCH_API AutogradFallbackMode getAutogradFallbackMode();
 
 } // namespace autograd
 } // namespace torch
