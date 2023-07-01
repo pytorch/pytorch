bb3df0bb7c6bce70941199401f6b3550e10cba50
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared29967.03.ncg5.facebook.com>
2023-07-01 00:03:41 -0700

    2023-07-01 nightly release (1de1bea60d96c75403beaa13b1ce80d0ea6ccdbd)

--- test/test_proxy_tensor.py
+++ test/test_proxy_tensor.py
@@ -1361,28 +1361,6 @@ def forward(self, a_1):
         tensor = make_fx(f, tracing_mode="symbolic")(torch.randn(15))
         self.assertExpectedInline(show_guards(tensor), """L['a'].size()[0] < 20""")
 
-    def test_guard_upperbound_range_refinement_multivariate(self):
-        def f(a):
-            assert a.shape[0] > 5 and a.shape[0] > 12
-            assert a.shape[1] > 5 and a.shape[1] > a.shape[0]
-            return a.cos()
-        tensor = make_fx(f, tracing_mode="symbolic")(torch.randn((15, 20)))
-        self.assertExpectedInline(show_guards(tensor), """\
-L['a'].size()[1] > L['a'].size()[0]
-L['a'].size()[0] > 12""")
-
-    def test_guard_lowerbound_range_refinement_multivariate(self):
-        def f(a):
-            assert a.shape[0] < 20 and a.shape[0] < 30
-            assert a.shape[1] < 30 and a.shape[1] < a.shape[0]
-            return a.cos()
-        tensor = make_fx(f, tracing_mode="symbolic")(torch.randn((15, 5)))
-        self.assertExpectedInline(
-            show_guards(tensor),
-            """\
-L['a'].size()[1] < L['a'].size()[0]
-L['a'].size()[0] < 20""")
-
     def test_sym_storage_offset(self):
         def f(x, y):
             return x + y
@@ -1456,14 +1434,8 @@ L['a'].size()[0] < 20""")
         fx_g = _trace(f, 2, 4, 8, 16, 32)
         self.assertExpectedInline(show_guards(fx_g), """""")
 
-    @torch._dynamo.config.patch(translation_validation=True)
-    def test_constant_specialization(self):
-        def f(t):
-            assert t.shape[0] == 10
-            return t
 
-        tensor = make_fx(f, tracing_mode="symbolic")(torch.randn(10))
-        self.assertExpectedInline(show_guards(tensor), """""")
+
 
 
 make_fx_failures = {
