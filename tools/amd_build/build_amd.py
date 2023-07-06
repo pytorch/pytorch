7bc7421533ed8f440a71567a27457c2c10a43d0f
Joseph Groenenboom <joseph.groenenboom@amd.com>
2023-07-06 16:15:30 +0000

    Add support for hipifying AT kernels math fallback

--- tools/amd_build/build_amd.py
+++ tools/amd_build/build_amd.py
@@ -90,10 +90,9 @@ includes = [
     "aten/src/ATen/native/nested/cuda/*",
     "aten/src/ATen/native/sparse/cuda/*",
     "aten/src/ATen/native/quantized/cuda/*",
-    "aten/src/ATen/native/transformers/cuda/*",
-    "aten/src/ATen/native/transformers/cuda/mem_eff_attention/debug_utils.h",
-    "aten/src/ATen/native/transformers/cuda/mem_eff_attention/gemm_kernel_utils.h",
-    "aten/src/ATen/native/transformers/cuda/mem_eff_attention/pytorch_utils.h",
+    # "aten/src/ATen/native/transformers/cuda/*",
+    "aten/src/ATen/native/transformers/cuda/flash_attn/*",
+    "aten/src/ATen/native/transformers/cuda/mem_eff_attention/*",
     "aten/src/THC/*",
     "aten/src/ATen/test/*",
     # CMakeLists.txt isn't processed by default, but there are a few
