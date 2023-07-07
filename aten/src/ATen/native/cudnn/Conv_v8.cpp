d4a62b6db3b0f4633da5827ee429710ac16183dc
eqy <eddiey@nvidia.com>
2023-07-07 01:06:28 -0700

    Update Conv_v8.cpp

--- aten/src/ATen/native/cudnn/Conv_v8.cpp
+++ aten/src/ATen/native/cudnn/Conv_v8.cpp
@@ -230,6 +230,7 @@ cudnn_frontend::ExecutionPlan* find(const KeyType& key) {
     engine_cache_order.push_back(key);
     engine_cache.erase(key);
     engine_cache.emplace(key, std::make_pair(plan, --engine_cache_order.end()));
+    // iterator was invalidated by the erase, so we grab it again
     it = engine_cache.find(key);
     TORCH_INTERNAL_ASSERT(it->first == *(it->second.second), "CUDNN V8 LRU Cache Corrupted (refresh list vs. map key mismatch). Please report a bug to PyTorch.");
     TORCH_INTERNAL_ASSERT((long) engine_cache_order.size() <= lru_cache_limit, "CUDNN V8 LRU Cache Corrupted (refresh size exceeds limit: ", lru_cache_limit, " please report a bug to PyTorch.");
