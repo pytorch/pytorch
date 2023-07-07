pytorch/pytorch

commit ed24cf52ef45572139d42d813cf89eb81d0b1c9c

more numerically stable cosine_similarity (#31378)
Summary:
**Previous behavior**: compute inner product, then normalize.
**This patch**: first normalize, then compute inner product. This should be more numerically stable because it avoids losing precision in inner product for inputs with large norms.
By design ensures that cosine similarity is within `[-1.0, +1.0]`, so it should fix [#29442](https://github.com/pytorch/pytorch/issues/29442).

P.S. I had to change tests because this implementation handles division by 0 differently.
This PR computes cosine similarity as follows: <x/max(eps, ||x||), y/max(eps, ||y||)>.
Let f(x,y) = <x,y>/(||x|| * ||y||), then
df/dx = y/(||x|| * ||y||) - (||y||/||x|| * <x,y> * x)/(||x|| * ||y||)^2.
The changed test checks division by zero in backward when x=0 and y != 0.
For this case the non-zero part of the gradient is just y / (||x|| * ||y||).
The previous test evaluates y/(||x|| * ||y||) to y / eps, and this PR to 1/eps * y/||y||.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/31378
Approved by: https://github.com/ezyang, https://github.com/albanD

Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/9e137ee583c4fdb2dd3aa0c425dc9c289454cbf2

Reviewed By: seemethere

Differential Revision: D35850003

fbshipit-source-id: 5de5563377ad3fa8acc62d0d974b241ee5bc0af1

aten/src/ATen/native/Distance.cpp
@@ -239,19 +239,72 @@ Tensor _pdist_backward(const Tensor& grad, const Tensor& self, const double p, c
   return result;
 }
 
-Tensor cosine_similarity(const Tensor& x1, const Tensor& x2, int64_t dim, double eps) {
-  auto common_size = at::infer_size_dimvector(x1.sizes(), x2.sizes());
-  auto commonDtype = at::result_type(x1, x2);
+Tensor cosine_similarity(const Tensor& x1_, const Tensor& x2_, int64_t dim, double eps) {
+  /*
+   * cosine_similarity(x1, x2) = <x1, x2> / (||x1|| * ||x2||)
+   *
+   * The current implementation is an improvement over the previous version.
+   *
+   * Previous implementation:
+   * 1. Compute num = <x1, x2>,
+   * 2. Compute denom = ||x1|| * ||x2||,
+   * 3. Compute denom = max(denom, eps) to avoid division by zero,
+   * 4. Return num / denom.
+   *
+   * Previous implementation has the following issues:
+   * 1. Chance of losing precision in <x1, x2> when ||x1|| and ||x2|| are large.
+   * 2. Chance of losing precision in ||x1|| * ||x2|| when ||x1|| and ||x2|| are large.
+   * 3. Losing precision may cause |cosing_similarity(x1, x2)| > 1.0.
+   *
+   * Current implementation:
+   * 1. Compute x1_normalized = x1 / max(||x1||, eps),
+   *            x2_normalized = x2 / max(||x2||, eps),
+   * 2. Return <x1_normalized, x2_normalized>.
+   *
+   * The current implementation improves over the previous one by:
+   * 1. Making sure that <x1, x2> and ||x1|| * ||x2|| are not computed explicitly,
+   *    hence avoiding floating point overflows.
+   * 2. Both methods might have issues with computing ||x1|| and ||x2||, but for
+   *    the current method this is the only source of the floating point imprecision.
+   * 3. Makes sure |cosing_similarity(x1, x2)| <= 1.0.
+   *
+   */
+  auto commonDtype = at::result_type(x1_, x2_);
   TORCH_CHECK(at::isFloatingType(commonDtype), "expected common dtype to be floating point, yet common dtype is ", commonDtype);
-  Tensor x1_ = x1.to(commonDtype).expand(common_size);
-  Tensor x2_ = x2.to(commonDtype).expand(common_size);
-  // Follow scipy impl to improve numerical precision
-  // Use x / sqrt(x * x) instead of x / (sqrt(x) * sqrt(x))
-  Tensor w12 = at::sum(x1_ * x2_, dim);
-  Tensor w1 = at::sum(x1_ * x1_, dim);
-  Tensor w2 = at::sum(x2_ * x2_, dim);
-  Tensor n12 = (w1 * w2).clamp_min_(eps * eps).sqrt_();
-  return w12.div_(n12);
+
+  auto common_size = at::infer_size_dimvector(x1_.sizes(), x2_.sizes());
+  auto x1 = x1_.to(commonDtype).expand(common_size);
+  auto x2 = x2_.to(commonDtype).expand(common_size);
+
+  auto x1_squared_norm = at::pow(x1, 2).sum(dim, /*keepdim=*/true);
+  auto x2_squared_norm = at::pow(x2, 2).sum(dim, /*keepdim=*/true);
+
+  {
+    at::NoGradGuard guard;
+    x1_squared_norm.clamp_min_(eps * eps);
+    x2_squared_norm.clamp_min_(eps * eps);
+  }
+
+  auto x1_norm = x1_squared_norm.sqrt_();
+  auto x2_norm = x2_squared_norm.sqrt_();
+
+  auto x1_normalized = x1.div(x1_norm);
+  auto x2_normalized = x2.div(x2_norm);
+
+  Tensor cos_sim_value = at::sum(x1_normalized * x2_normalized, dim);
+
+  // The code above is resistant to over +/-1 overshoots.
+  // However, if this happens and if it is critical, uncommenting
+  // the lines below will solve the issue.
+  // We keep these lines commented as to reduce the number of kernel
+  // launches for better runtime performance.
+  //{
+  //  at::NoGradGuard guard;
+  //  cos_sim_value.clamp_min_(-1.0);
+  //  cos_sim_value.clamp_max_(1.0);
+  //}
+
+  return cos_sim_value;
 }
 
 }}  // namespace at::native

test/test_nn.py
@@ -10025,11 +10025,20 @@ class TestNN(NNTestCase):
         self.assertLessEqual(out, 1.0)
 
         # Check dividing by 0.
+        # previous behavior: <x,y>/max(eps, ||x|| * ||y||)
+        # current: <x/max(eps, ||x||), y/max(eps,||y||)>
+        # if f(x,y) is the cosine similarity, then
+        # df/dx = y/(||x|| * ||y||) - (x * <x,y> * ||y||/||x||)/(||x|| * ||y||)^2
+        # the tests below check division by zero in the backward formula when
+        # x := input2 = 0, y := input1 != 0.
+        # For these inputs the gradient wrt x simplifies to g(x,y) := y/(||x|| * ||y||)
+        # Previous test checks g(x,y) == y/eps,
+        # Current test checks g(x,y) == (y/||y||)/eps.
         input1 = torch.randn(10).requires_grad_()
         input2 = torch.zeros_like(input1).requires_grad_()
         torch.cosine_similarity(input1, input2, 0).sum().backward()
         self.assertEqual(input1.grad, torch.zeros_like(input1))
-        self.assertEqual(input2.grad, input1 * 1e8)
+        self.assertEqual(input2.grad, input1 / input1.norm() * 1e8)
 
         # Check type promotion, issue #61454
         input = torch.tensor(12.)

commit 96cf5e705520ad414d83d0f8f7f2b141045abec1

Fuse ops in eager cosine_similarity while keeping the stability and the gradients
There was a regression in https://github.com/pytorch/pytorch/pull/31378
which was reported in https://github.com/pytorch/pytorch/issues/104564.
This PR should keep the efficiency and memory usage from the original
implementation, while keeping the stability of the latter.

This solution was already discussed in https://github.com/pytorch/pytorch/pull/31378,
but it was discarded because it modified the vector_norm in-place. The
only magic ingredient that was missing for that solution to work was to
add a `clone()` after calling the `vector_norm`.

I hope this PR takes shorter to land than https://github.com/pytorch/pytorch/issues/104564.

Fixes https://github.com/pytorch/pytorch/issues/104564

[ghstack-poisoned]

aten/src/ATen/native/Distance.cpp
@@ -24,6 +24,7 @@
 #include <ATen/ops/cosine_similarity_native.h>
 #include <ATen/ops/empty.h>
 #include <ATen/ops/empty_like.h>
+#include <ATen/ops/linalg_vector_norm.h>
 #include <ATen/ops/norm.h>
 #include <ATen/ops/ones_like.h>
 #include <ATen/ops/pairwise_distance_native.h>
@@ -271,71 +272,31 @@ Tensor _pdist_backward(const Tensor& grad, const Tensor& self, const double p, c
 }
 
 Tensor cosine_similarity(const Tensor& x1_, const Tensor& x2_, int64_t dim, double eps) {
-  /*
-   * cosine_similarity(x1, x2) = <x1, x2> / (||x1|| * ||x2||)
-   *
-   * The current implementation is an improvement over the previous version.
-   *
-   * Previous implementation:
-   * 1. Compute num = <x1, x2>,
-   * 2. Compute denom = ||x1|| * ||x2||,
-   * 3. Compute denom = max(denom, eps) to avoid division by zero,
-   * 4. Return num / denom.
-   *
-   * Previous implementation has the following issues:
-   * 1. Chance of losing precision in <x1, x2> when ||x1|| and ||x2|| are large.
-   * 2. Chance of losing precision in ||x1|| * ||x2|| when ||x1|| and ||x2|| are large.
-   * 3. Losing precision may cause |cosing_similarity(x1, x2)| > 1.0.
-   *
-   * Current implementation:
-   * 1. Compute x1_normalized = x1 / max(||x1||, eps),
-   *            x2_normalized = x2 / max(||x2||, eps),
-   * 2. Return <x1_normalized, x2_normalized>.
-   *
-   * The current implementation improves over the previous one by:
-   * 1. Making sure that <x1, x2> and ||x1|| * ||x2|| are not computed explicitly,
-   *    hence avoiding floating point overflows.
-   * 2. Both methods might have issues with computing ||x1|| and ||x2||, but for
-   *    the current method this is the only source of the floating point imprecision.
-   * 3. Makes sure |cosing_similarity(x1, x2)| <= 1.0.
-   *
-   */
+
   auto commonDtype = at::result_type(x1_, x2_);
   TORCH_CHECK(at::isFloatingType(commonDtype), "expected common dtype to be floating point, yet common dtype is ", commonDtype);
 
-  auto common_size = at::infer_size_symdimvector(x1_.sym_sizes(), x2_.sym_sizes());
-  auto x1 = x1_.to(commonDtype).expand_symint(common_size);
-  auto x2 = x2_.to(commonDtype).expand_symint(common_size);
+  // We accept integral types but vector_norm does not
+  auto x1_int = isIntegralType(x1_.scalar_type());
+  auto x2_int = isIntegralType(x2_.scalar_type());
+  auto x1 = x1_int ? x1_.to(commonDtype) : x1_;
+  auto x2 = x2_int ? x2_.to(commonDtype) : x2_;
 
-  auto x1_squared_norm = at::pow(x1, 2).sum(dim, /*keepdim=*/true);
-  auto x2_squared_norm = at::pow(x2, 2).sum(dim, /*keepdim=*/true);
+
+  // We want to divide each tensor by its norm first, as it's more numerically stable.
+  // This keeps the result between -1.0 and 1.0
+  // We clone them, as we're going to modify them in-place
+  // This allows the gradients to propagate propertly all the way to x1 and x2
+  auto x1_norm = at::linalg_vector_norm(x1, 2, /*dim=*/dim, /*keepdim=*/true).clone();
+  auto x2_norm = at::linalg_vector_norm(x2, 2, /*dim=*/dim, /*keepdim=*/true).clone();
 
   {
     at::NoGradGuard guard;
-    x1_squared_norm.clamp_min_(eps * eps);
-    x2_squared_norm.clamp_min_(eps * eps);
+    x1_norm.clamp_min_(eps);
+    x2_norm.clamp_min_(eps);
   }
 
-  auto x1_norm = x1_squared_norm.sqrt_();
-  auto x2_norm = x2_squared_norm.sqrt_();
-
-  auto x1_normalized = x1.div(x1_norm);
-  auto x2_normalized = x2.div(x2_norm);
-
-  Tensor cos_sim_value = at::sum(x1_normalized * x2_normalized, dim);
-
-  // The code above is resistant to over +/-1 overshoots.
-  // However, if this happens and if it is critical, uncommenting
-  // the lines below will solve the issue.
-  // We keep these lines commented as to reduce the number of kernel
-  // launches for better runtime performance.
-  //{
-  //  at::NoGradGuard guard;
-  //  cos_sim_value.clamp_min_(-1.0);
-  //  cos_sim_value.clamp_max_(1.0);
-  //}
-
-  return cos_sim_value;
+  return ((x1 / x1_norm) * (x2 / x2_norm)).sum(dim);
 }
 
 }}  // namespace at::native