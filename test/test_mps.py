58bb64170a72fc2f42867267c81aa37bc561b93b
Peter Stefek <peterlikesemail@gmail.com>
2023-07-08 23:39:46 -0400

    flake8

--- test/test_mps.py
+++ test/test_mps.py
@@ -3661,7 +3661,8 @@ class TestMPS(TestCaseMPS):
             helper(torch.int64)
         except Exception as e:
             e_string = str(e)
-            self.assertEqual(e_string, "MPS does not support cumsum_out_mps op with int64 input. Support has been added in macOS 13.3")
+            self.assertEqual(e_string, "MPS does not support cumsum_out_mps op with int64 input." +
+                             "Support has been added in macOS 13.3")
 
     def test_cumsum_minus_one_axis(self):
         def helper(dtype):
@@ -3695,7 +3696,8 @@ class TestMPS(TestCaseMPS):
             helper(torch.int64)
         except Exception as e:
             e_string = str(e)
-            self.assertEqual(e_string, "MPS does not support cumprod_out_mps op with int64 input. Support has been added in macOS 13.3")
+            self.assertEqual(e_string, "MPS does not support cumprod_out_mps op with int64 input."
+                             + "Support has been added in macOS 13.3")
 
     def test_cumprod_minus_one_axis(self):
         def helper(dtype):
