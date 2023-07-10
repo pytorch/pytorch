8c01087634233323a0db56f9d52486e4f43769b7
Sangsu Lee <sangsulee@meta.com>
2023-07-10 16:06:57 -0700

    Update on "[PyTorch TB] Write raw tensor as tensor_proto"
    This is the first diff to support logging of raw tensors for [TensorBoard Intermediate Logging](https://www.internalfb.com/intern/wiki/TensorBoard/Intermediate_Logging/)
    
    Ultimately, we aim to support the feature where store full tensor is stored as a tensor protobuf to TB. Protobuf contains shape, dtype, and elements of the given tensor.
    
    1. add `tensor_proto()` to `summary.py` which takes a tensor and convert to protobuf
    2. add `add_tensor()` to `writer.py`
    3. formatting changes introduced by `arc lint`
    -------------
    
    
    Differential Revision: [D47302017](https://our.internmc.facebook.com/intern/diff/D47302017/)
    
    [ghstack-poisoned]

--- torch/utils/tensorboard/summary.py
+++ torch/utils/tensorboard/summary.py
@@ -380,7 +380,9 @@ def tensor_proto(tag, tensor):
             ],
             proto_val_field: torch.view_as_real(tensor).tolist()
             if proto_val_field == "scomplex_val" or proto_val_field == "dcomplex_val"
-            else tensor.flatten().tolist(),
+            else tensor.flatten().tolist(force=True)
+            if tensor.numel() > 0
+            else [],
         }
         tensor_proto = TensorProto(**tensor_proto_args)
     else:
