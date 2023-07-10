ccdc642255d8bc2640529439ba97859d6048f218
Michael Lazos <mlazos@fb.com>
2023-07-10 18:13:04 +0000

    Cleanup lerp meta reg

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -2810,19 +2810,8 @@ def meta__foreach_addcop__scalar(self, tensor1, tensor2, scalar=1):
         aten._foreach_lerp_.Scalar,
     ]
 )
-def meta__foreach_lerp__scalar(self, tensor1, scalar=1):
-    torch._check(
-        all(isinstance(l, List) for l in [self, tensor1]),
-        lambda: (
-            "All arguments of _foreach_lerp_ must be List[Tensor], "
-            f"but got {type(self)} and {type(tensor1)}"
-        ),
-    )
-    torch._check(len(self) > 0, lambda: "input tensor list must not be empty.")
-    torch._check(
-        len(self) == len(tensor1),
-        lambda: "All input tensor lists must have the same length",
-    )
+def meta__foreach_lerp__scalar(self, other, scalar=1):
+    _check_foreach_binop_tensor_lists(self, other)
 
 
 @register_meta(
