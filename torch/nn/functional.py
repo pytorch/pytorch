506c111ca8cb8f2c60bab1fe8e3126e8f2dcff63
Yashika <yashikajain201@gmail.com>
2023-07-10 11:19:53 +0000

    Modifications

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3057,6 +3057,8 @@ def cross_entropy(
         )
     if size_average is not None or reduce is not None:
         reduction = _Reduction.legacy_get_string(size_average, reduce)
+        if input.dim() == 1:
+            dim = None
         if dim is not None:
             input = input.swapaxes(dim, 1)
     output = torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
