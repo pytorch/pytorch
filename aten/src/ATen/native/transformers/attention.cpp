4f5766c6eeea3d66df59d41dc9a798c001419f94
Liao, Xuan <xuan.liao@intel.com>
2023-07-11 15:26:30 +0000

    Update base for Update on "[cpu] enable bfloat16 and refactor for flash attention"
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- aten/src/ATen/native/transformers/attention.cpp
+++ aten/src/ATen/native/transformers/attention.cpp
@@ -723,8 +723,12 @@ _scaled_dot_product_flash_attention_cpu(
   int64_t num_head = query.size(1);
   int64_t headSize = query.size(3);
 
+  bool is_training =
+      (query.requires_grad() || key.requires_grad() ||
+      value.requires_grad());
+
   at::Tensor output = at::empty({batchSize, qSize, num_head, headSize}, query.options());
-  at::Tensor logsumexp = at::empty({batchSize, qSize, num_head}, at::kFloat);
+  at::Tensor logsumexp = at::empty({batchSize, is_training ? qSize : 0, num_head}, at::kFloat);
   at::Tensor cum_seq_q = Tensor();
   at::Tensor cum_seq_k = Tensor();
   int64_t max_q = 0;
