ed5b378f65f460a65dedd629e0c91c61f9064b79
FFFrog <ljw1101.vip@gmail.com>
2023-07-12 03:55:39 +0000

    refactor GeneratorForPrivateuseone

--- aten/src/ATen/core/GeneratorForPrivateuseone.cpp
+++ aten/src/ATen/core/GeneratorForPrivateuseone.cpp
@@ -3,26 +3,30 @@
 
 namespace at {
 
+static std::mutex _generator_mutex_lock;
+
 c10::optional<GeneratorFuncType>& GetGeneratorPrivate() {
   static c10::optional<GeneratorFuncType> generator_privateuse1 = c10::nullopt;
   return generator_privateuse1;
 }
 
-std::mutex _generator_mutex_lock;
 _GeneratorRegister::_GeneratorRegister(GeneratorFuncType func) {
-  _generator_mutex_lock.lock();
-  TORCH_CHECK(!GetGeneratorPrivate().has_value(),
-    "Only can register a generator to the PrivateUse1 dispatch key once!");
+  std::lock_guard<std::mutex> lock(_generator_mutex_lock);
+  TORCH_CHECK(
+      !GetGeneratorPrivate().has_value(),
+      "Only can register a generator to the PrivateUse1 dispatch key once!");
+
   auto& m_generator = GetGeneratorPrivate();
   m_generator = func;
-  _generator_mutex_lock.unlock();
 }
 
 at::Generator GetGeneratorForPrivateuse1(c10::DeviceIndex device_index) {
-  TORCH_CHECK(GetGeneratorPrivate().has_value(),
-    "Please register a generator to the PrivateUse1 dispatch key, \
-     using the REGISTER_GENERATOR_PRIVATEUSE1 macro.");
+  TORCH_CHECK(
+      GetGeneratorPrivate().has_value(),
+      "Please register a generator to the PrivateUse1 dispatch key, \
+      using the REGISTER_GENERATOR_PRIVATEUSE1 macro.");
+
   return GetGeneratorPrivate().value()(device_index);
 }
 
-}
+} // namespace at
