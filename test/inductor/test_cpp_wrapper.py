c36dca7bc54ac2c8d772adc5e549cbcae9390fff
Catherine Lee <csl@fb.com>
2023-07-13 17:13:45 +0000

    Revert "[inductor] Register an op for mm_plus_mm (#104835)" (#105150)
    This reverts commit 9c46a1620c99626ee9db01985a569ba79888508b.
    
    Actual revert referenced in https://github.com/pytorch/pytorch/pull/105149
    
    #104835 is causing internal builds to fail
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105150
    Approved by: https://github.com/atalman

--- test/inductor/test_cpp_wrapper.py
+++ test/inductor/test_cpp_wrapper.py
@@ -85,9 +85,6 @@ test_failures_cuda_wrapper = {
     "test_custom_op_cuda_dynamic_shapes": test_torchinductor.TestFailure(
         ("cuda_wrapper",), is_skip=True
     ),
-    "test_mm_plus_mm2_dynamic_shapes": test_torchinductor.TestFailure(
-        ("cuda_wrapper",), is_skip=True
-    ),
 }
 
 
@@ -286,11 +283,6 @@ if RUN_CUDA:
         ),
         BaseTest(
             "test_convolution1",
-            device=None,
-            tests=test_select_algorithm.TestSelectAlgorithm(),
-        ),
-        BaseTest(
-            "test_mm_plus_mm2",
             device=None,
             tests=test_select_algorithm.TestSelectAlgorithm(),
         ),
