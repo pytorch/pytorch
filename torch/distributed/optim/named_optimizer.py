1646d6f93964accad7f477801b77c65c4af5ca04
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-07-14 16:04:54 +0000

    Revert "Merge and improve torch optim optimizer type stubs (#102593)"
    This reverts commit 3279f06410032e9798e380cedf552f5b706ac6c1.
    
    Reverted https://github.com/pytorch/pytorch/pull/102593 on behalf of https://github.com/malfet due to There is nothing wrong with this PR, but it fails some internal builds that depend on outdated typing_extensions, will reland when update is done ([comment](https://github.com/pytorch/pytorch/pull/102593#issuecomment-1636062515))

--- torch/distributed/optim/named_optimizer.py
+++ torch/distributed/optim/named_optimizer.py
@@ -2,7 +2,7 @@ import logging
 import warnings
 
 from copy import deepcopy
-from typing import Any, Callable, Collection, Dict, List, Mapping, Optional, Union, overload
+from typing import Any, Collection, Dict, List, Mapping, Union
 
 import torch
 import torch.nn as nn
@@ -144,22 +144,14 @@ class _NamedOptimizer(optim.Optimizer):
 
         return self._post_state_dict({"state": ret_state, "param_groups": ret_groups})
 
-    @overload
-    def step(self, closure: None = ...) -> None:
-        ...
-
-    @overload
-    def step(self, closure: Callable[[], float]) -> float:
-        ...
-
-    def step(self, closure: Optional[Callable[[], float]] = None) -> Optional[float]:
+    def step(self, closure: Any = None) -> None:
         """
         Performs a single optimization step.
 
         This will call :meth:`torch.optim.Optimizer.step` on the wrapped
         optimizer.
         """
-        return self._optimizer.step(closure=closure)
+        self._optimizer.step(closure=closure)
 
     @property
     def state(self) -> Mapping[torch.Tensor, Any]:
