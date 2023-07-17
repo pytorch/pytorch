307851aeedf1b1a8256bf6d1803ac89ea35a8d5b
eqy <eddiey@nvidia.com>
2023-07-17 12:48:42 -0700

    Update ProcessGroupNCCL.cpp

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -453,7 +453,7 @@ bool ProcessGroupNCCL::WorkNCCL::checkTimeout(
       currentTimepoint - workStartTime_);
   auto workTimeout = timeout ? *timeout : opTimeout_;
 
-  if (timeElapsed < workTimeout + _timeout_skew)
+  if (timeElapsed.count() < workTimeout.count() + _timeout_skew)
     return false;
 
   // Timed out
