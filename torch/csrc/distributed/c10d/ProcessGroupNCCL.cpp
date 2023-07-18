ac5c63cdd40631d0f610080d4ba321c3dcbad1e3
eqy <eddiey@nvidia.com>
2023-07-18 09:13:34 -0700

    Update ProcessGroupNCCL.cpp

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -907,8 +907,7 @@ void ProcessGroupNCCL::ncclCommWatchdog() {
     VLOG(2) << "[Rank " << rank_ << "] NCCL watchdog thread started!";
     // test if runtime API call works...
     auto skew_start = std::chrono::steady_clock::now();
-    auto mode = cudaStreamCaptureModeGlobal;
-    cudaThreadExchangeStreamCaptureMode(&mode);
+    auto mode = cudaStreamCaptureModeRelaxed;
     cudaThreadExchangeStreamCaptureMode(&mode);
     auto skew_end = std::chrono::steady_clock::now();
     auto skew = std::chrono::duration_cast<std::chrono::milliseconds>(
