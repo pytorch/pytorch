2e81cdc1ddbb07dead5b32a6fbf1514a118ac208
Edward Z. Yang <ezyang@meta.com>
2023-07-19 14:26:23 -0400

    Remove dead sizevars.__getitem__ (#105579)
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105579
    Approved by: https://github.com/albanD

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -342,9 +342,6 @@ class SizeVarAllocator:
     def guard_static_shapes(self, left: List[Expr]) -> List[int]:
         return [self.guard_static_shape(x) for x in left]
 
-    def __getitem__(self, val: int) -> Expr:
-        return self.shape_env.duck_int(val)
-
     def size_hint(self, expr: Expr) -> int:
         if not isinstance(expr, Expr):
             assert isinstance(expr, int)
