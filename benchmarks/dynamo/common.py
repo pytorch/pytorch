3b745d890c23adacffb26f2c83f4915919cae38c
Mark Saroufim <marksaroufim@fb.com>
2023-07-19 16:03:13 -0700

    Skip deterministic mode for SAM
    SAM uses cumsum which doesnt have a deterministic mode enabled so this the onl way I can work around this https://github.com/pytorch/pytorch/issues/89492

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -2980,6 +2980,7 @@ def run(runner, args, original_dir=None):
             # https://github.com/pytorch/pytorch/issues/96724
             "Wav2Vec2ForCTC",
             "Wav2Vec2ForPreTraining",
+            "sam",
         }:
             # some of the models do not support use_deterministic_algorithms
             torch.use_deterministic_algorithms(True)
