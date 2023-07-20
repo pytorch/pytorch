53e97d67b38be3efa260417fa06f47fd7ae3265c
Az0s <b924258@outlook.com>
2023-07-20 22:27:12 +0800

    perf optimize

--- aten/src/ATen/native/mps/operations/Sort.mm
+++ aten/src/ATen/native/mps/operations/Sort.mm
@@ -29,15 +29,21 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
   bool macOS13_3_plus = is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_3_PLUS);
   MPS_CHECK_INT64_OP_SUPPORTED(self, macOS13_3_plus, "sort_stable_out");
 
-
   // issue #101878: MPS might give a wrong sorted tensor when dealing with a strided view tensor.
-  // create a proxy tensor to temporarily address the issue
-  Tensor input_contiguous = self.contiguous();
-  Tensor values_contiguous = self.contiguous();
+  Tensor values_contiguous = values;
+  bool needsCopyToOutput = false;
+  if (!values.is_contiguous()) {
+    values_contiguous = values.contiguous();
+    needsCopyToOutput = true;
+  // else, determine if this is an in-place operation on a view output
+  } else if (values.is_view() && (self.is_alias_of(values) )) {
+    values_contiguous = at::empty(values.sizes(), values.scalar_type(), c10::nullopt, kMPS, c10::nullopt, c10::nullopt);
+    needsCopyToOutput = true;
+  }
 
-  // check if input_contiguous is scalar
-  dim = maybe_wrap_dim(dim, input_contiguous.dim(), true);
-  if (input_contiguous.dim() == 0 && input_contiguous.numel() == 1) {
+  // check if self is scalar
+  dim = maybe_wrap_dim(dim, self.dim(), true);
+  if (self.dim() == 0 && self.numel() == 1) {
     indices.zero_();
     return;
   }
@@ -46,7 +52,7 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
     TORCH_WARN_ONCE("torch.sort is supported by MPS on MacOS 13+, please upgrade. Falling back to CPU");
     Tensor cpu_indices = indices.clone().to("cpu");
     Tensor cpu_values = values.clone().to("cpu");
-    at::sort_out(cpu_values, cpu_indices, input_contiguous.to(at::Device(kCPU)), false, dim, descending);
+    at::sort_out(cpu_values, cpu_indices, self.to(at::Device(kCPU)), false, dim, descending);
     values.copy_(cpu_values);
     indices.copy_(cpu_indices);
     return;
@@ -59,15 +65,15 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
   };
   @autoreleasepool {
     // Input as placeholders
-    MPSShape* input_shape = getMPSShape(input_contiguous);
+    MPSShape* input_shape = getMPSShape(self);
     NSString* ns_shape_key = [[input_shape valueForKey:@"description"] componentsJoinedByString:@","];
-    string key = string("sort:") + [ns_shape_key UTF8String] + ":" + getMPSTypeString(input_contiguous) + ":dim" + to_string(dim) +
+    string key = string("sort:") + [ns_shape_key UTF8String] + ":" + getMPSTypeString(self) + ":dim" + to_string(dim) +
         ":descending" + to_string(descending);
     auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-      newCachedGraph->selfTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(input_contiguous), input_shape);
+      newCachedGraph->selfTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), input_shape);
 
       MPSGraphTensor* castInputTensor =
-          castToIHFTypes(mpsGraph, newCachedGraph->selfTensor, input_contiguous, /*includesInt64=*/macOS13_3_plus);
+          castToIHFTypes(mpsGraph, newCachedGraph->selfTensor, self, /*includesInt64=*/macOS13_3_plus);
       MPSGraphTensor* sortedTensor = [mpsGraph sortWithTensor:castInputTensor
                                                          axis:(NSInteger)dim
                                                    descending:(BOOL)descending
@@ -85,7 +91,7 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
       newCachedGraph->valuesTensor = sortedTensor;
       newCachedGraph->indicesTensor = argSortedTensor;
     });
-    Placeholder inputPlaceholder = Placeholder(cachedGraph->selfTensor, input_contiguous);
+    Placeholder inputPlaceholder = Placeholder(cachedGraph->selfTensor, self);
     // Outputs as placeholders
     Placeholder valuesPlaceholder = Placeholder(cachedGraph->valuesTensor, values_contiguous);
     Placeholder indicesPlaceholder = Placeholder(cachedGraph->indicesTensor, indices);
@@ -99,8 +105,10 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
 
     runMPSGraph(stream, cachedGraph->graph(), feeds, results);
   }
-
   // Copy the sorted values back to values
-  values.copy_(values_contiguous);
+  if (needsCopyToOutput) {
+    values.copy_(values_contiguous);
+  }
+  
 }
 }
