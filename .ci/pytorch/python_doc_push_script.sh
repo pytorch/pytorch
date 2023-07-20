b3d4acf4f95ebbdc39dc8dd419b5e2aab2811915
Catherine Lee <csl@fb.com>
2023-07-20 10:59:48 -0700

    fix docs, remove circleci scripts

--- .ci/pytorch/python_doc_push_script.sh
+++ .ci/pytorch/python_doc_push_script.sh
@@ -85,9 +85,9 @@ pushd docs
 
 # Build the docs
 if [ "$is_main_doc" = true ]; then
-  if ! build_docs html; then
-    exit $?
-  fi
+  build_docs html
+  [ $? -eq 0 ] || exit $?
+
   make coverage
   # Now we have the coverage report, we need to make sure it is empty.
   # Count the number of lines in the file and turn that number into a variable
@@ -110,9 +110,8 @@ if [ "$is_main_doc" = true ]; then
   fi
 else
   # skip coverage, format for stable or tags
-  if ! build_docs html-stable; then
-    exit $?
-  fi
+  build_docs html-stable
+  [ $? -eq 0 ] || exit $?
 fi
 
 # Move them into the docs repo
