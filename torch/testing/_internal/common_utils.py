d014067e1ee4f5d43bdad6cd53f8c402c043ed70
eqy <eddiey@nvidia.com>
2023-07-19 23:46:13 -0700

    Update common_utils.py

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -686,6 +686,7 @@ def wait_for_process(p, timeout=None):
     except subprocess.TimeoutExpired:
         # send SIGINT to give pytest a chance to make xml
         p.send_signal(signal.SIGINT)
+        exit_status = None
         try:
             exit_status = p.wait(timeout=5)
         # try to handle the case where p.wait(timeout=5) times out as well as
