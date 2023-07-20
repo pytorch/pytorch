b73980c3f1d977810e98d4d80ba098c22c1d03a8
Thomas Findlay <findlaytel@gmail.com>
2023-07-20 19:51:07 +0100

    Formatting changed

--- torch/nn/modules/rnn.py
+++ torch/nn/modules/rnn.py
@@ -73,8 +73,8 @@ class RNNBase(Module):
                           "num_layers greater than 1, but got dropout={} and "
                           "num_layers={}".format(dropout, num_layers))
 
-        if type(hidden_size) is not int:
-            raise TypeError("hidden_size should be of type int, got: {}".format(type(hidden_size).__name__))
+        if isinstance(hidden_size, int):
+            raise TypeError(f"hidden_size should be of type int, got: {type(hidden_size).__name__}")
         if hidden_size <= 0:
             raise ValueError("hidden_size must be greater than zero")
         if proj_size < 0:
