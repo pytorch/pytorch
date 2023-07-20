6f4456ca0c9a503f124db9755796e62aa5cd17fd
leslie-fang-intel <leslie.fang@intel.com>
2023-07-20 12:29:11 +0800

    [Quant][PT2E]Re-enable test case of conv add/add_relu recipe for x86inductorquantizer
    [ghstack-poisoned]

--- test/quantization/pt2e/test_x86inductor_quantizer.py
+++ test/quantization/pt2e/test_x86inductor_quantizer.py
@@ -20,7 +20,6 @@ from torch.testing._internal.common_quantized import override_quantized_engine
 from enum import Enum
 import itertools
 import torch.ao.quantization.pt2e.quantizer.x86_inductor_quantizer as xiq
-from torch.testing._internal.common_utils import skip_but_pass_in_sandcastle
 
 
 class Conv2DType(Enum):
@@ -261,7 +260,6 @@ class TestQuantizePT2EX86Inductor(X86InductorQuantTestCase):
                 )
 
     @skipIfNoX86
-    @skip_but_pass_in_sandcastle("times out in sandcastle, maybe related to torchdynamo export, but need to follow up and debug")
     def test_conv2d_binary_with_quantizer_api(self):
         """
         Test pattern of conv2d with binary post ops (such as add) with X86InductorQuantizer.
@@ -314,7 +312,6 @@ class TestQuantizePT2EX86Inductor(X86InductorQuantTestCase):
                 )
 
     @skipIfNoX86
-    @skip_but_pass_in_sandcastle("times out in sandcastle, maybe related to torchdynamo export, but need to follow up and debug")
     def test_conv2d_binary_unary_with_quantizer_api(self):
         """
         Test pattern of conv2d with binary + unary post ops (such as add + relu) with X86InductorQuantizer.
