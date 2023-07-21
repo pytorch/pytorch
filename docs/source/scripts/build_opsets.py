052ad856be7dc821ee492ac738b4414d5e64d8c2
Sherlock Huang <bahuang@fb.com>
2023-07-21 05:05:00 +0000

    Update Core Aten Doc
    [ghstack-poisoned]

--- docs/source/scripts/build_opsets.py
+++ docs/source/scripts/build_opsets.py
@@ -5,6 +5,7 @@ import torch
 from collections import OrderedDict
 from torchgen.gen import parse_native_yaml
 import torch._prims as prims
+from torch._decomp import decomposition_table
 
 ROOT = Path(__file__).absolute().parent.parent.parent.parent
 NATIVE_FUNCTION_YAML_PATH = ROOT / Path("aten/src/ATen/native/native_functions.yaml")
@@ -27,10 +28,22 @@ def get_aten():
 
     op_schema_pairs = []
     for key, op in sorted(aten_ops.items()):
-        op_name = f"aten.{key}"
+        aten_full_name = f"aten.{key}"
         schema = str(op.func).replace("*", r"\*")
 
-        op_schema_pairs.append((op_name, schema))
+        op_name_overload = key.split(".")
+        op_name = op_name_overload[0]
+        if len(op_name_overload) == 2:
+            overload = op_name_overload[1]
+        else:
+            overload = "default"
+
+        op_overloadpacket = getattr(torch.ops.aten, op_name, None)
+        op_overload = getattr(op_overloadpacket, overload, None)
+        decomp_fn = decomposition_table.get(op_overload, None)
+        has_decomp = decomp_fn is not None and decomp_fn.__module__ in {"torch._decomp.decompositions"}
+
+        op_schema_pairs.append((aten_full_name, schema, has_decomp))
 
     return op_schema_pairs
 
@@ -60,9 +73,9 @@ def main():
     os.makedirs(BUILD_DIR, exist_ok=True)
 
     with open(os.path.join(BUILD_DIR, ATEN_OPS_CSV_FILE), "w") as f:
-        f.write("Operator,Schema\n")
-        for name, schema in aten_ops_list:
-            f.write(f'"``{name}``","{schema}"\n')
+        f.write("Operator,Schema,Has Decomp\n")
+        for name, schema, has_decomp in aten_ops_list:
+            f.write(f'"``{name}``","{schema}","{has_decomp}"\n')
 
     with open(os.path.join(BUILD_DIR, PRIMS_OPS_CSV_FILE), "w") as f:
         f.write("Operator,Schema\n")
