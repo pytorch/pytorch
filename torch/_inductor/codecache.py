f990e1f60623321978511444dd9759db12da6010
Bert Maher <bertrand@fb.com>
2023-07-21 11:51:18 -0700

    [inductor] Enable vectorization in fbcode
    The method of running a test binary doesn't really work well in fbcode
    for various reasons, but there's no reason to do it;
    `torch.backends.cpu.get_cpu_capability()` will tell us AVX2 or AVX512 (or
    DEFAULT).
    
    I think this might actually be a better method even outside of fbcode, but I
    don't have a great way to test AVX512 behavior.
    
    Differential Revision: [D47678649](https://our.internmc.facebook.com/intern/diff/D47678649/)
    
    [ghstack-poisoned]

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -469,6 +469,7 @@ class VecISA:
     _macro: str
     _arch_flags: str
     _dtype_nelements: Dict[torch.dtype, int]
+    _capability: str
 
     # Note [Checking for Vectorized Support in Inductor]
     # TorchInductor CPU vectorization reuses PyTorch vectorization utility functions
@@ -518,6 +519,9 @@ cdll.LoadLibrary("__lib_path__")
     def build_arch_flags(self):
         return self._arch_flags
 
+    def capability(self):
+        return self._capability
+
     def __hash__(self) -> int:
         return hash(str(self))
 
@@ -525,6 +529,9 @@ cdll.LoadLibrary("__lib_path__")
     def __bool__(self):
         if config.cpp.vec_isa_ok is not None:
             return config.cpp.vec_isa_ok
+
+        if config.is_fbcode():
+            return torch.backends.cpu.get_cpu_capability() == self._capability
 
         key, input_path = write(VecISA._avx_code, "cpp")
         from filelock import FileLock
@@ -561,6 +568,7 @@ class VecAVX512(VecISA):
     _macro = "CPU_CAPABILITY_AVX512"
     _arch_flags = "-mavx512f -mavx512dq -mavx512vl -mavx512bw -mfma"
     _dtype_nelements = {torch.float: 16, torch.bfloat16: 32}
+    _capability = "AVX512"
 
     def __str__(self) -> str:
         return "avx512"
@@ -574,6 +582,7 @@ class VecAVX2(VecISA):
     _macro = "CPU_CAPABILITY_AVX2"
     _arch_flags = "-mavx2 -mfma"
     _dtype_nelements = {torch.float: 8, torch.bfloat16: 16}
+    _capability = "AVX2"
 
     def __str__(self) -> str:
         return "avx2"
@@ -586,6 +595,7 @@ class InvalidVecISA(VecISA):
     _macro = ""
     _arch_flags = ""
     _dtype_nelements = {}
+    _capability = "INVALID"
 
     def __str__(self) -> str:
         return "INVALID_VEC_ISA"
@@ -712,7 +722,18 @@ def get_include_and_linking_paths(
             libs += ["omp"]
         macros = vec_isa.build_macro()
         if macros:
-            macros = f"-D{macros}"
+            if config.is_fbcode():
+                cap = vec_isa.capability()
+                macros = " ".join(
+                    [
+                        vec_isa.build_arch_flags(),
+                        f"-D CPU_CAPABILITY={cap}",
+                        f"-D CPU_CAPABILITY_{cap}",
+                        f"-D HAVE_{cap}_CPU_DEFINITION",
+                    ]
+                )
+            else:
+                macros = f"-D{macros}"
         if cuda:
             if config.is_fbcode():
                 libs += ["cuda"]
