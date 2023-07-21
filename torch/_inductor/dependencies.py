e44f4e803a0802694c356da13ec679db19de99a9
Michael Lazos <mlazos@fb.com>
2023-07-21 03:07:06 +0000

    Add op counts and move set minus

--- torch/_inductor/dependencies.py
+++ torch/_inductor/dependencies.py
@@ -157,9 +157,15 @@ class ReadWrites:
     @staticmethod
     def merge_list(read_writes: List["ReadWrites"]):
         all_writes = set.union(*[rw.writes for rw in read_writes])
-        all_reads = set.union(*[rw.reads - all_writes for rw in read_writes])
+        all_reads = set.union(*[rw.reads for rw in read_writes]) - all_writes
         all_index_exprs = set.union(*[rw.index_exprs for rw in read_writes])
-        return ReadWrites(all_reads, all_writes, all_index_exprs)
+
+        op_counts = collections.Counter()
+        for rw in read_writes:
+            if rw.op_counts is not None:
+                op_counts.update(rw.op_counts)
+
+        return ReadWrites(all_reads, all_writes, all_index_exprs, op_counts=op_counts)
 
     def remove_reads(self, rem_reads):
         return ReadWrites(
