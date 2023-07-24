f3f306d1ecdf4e56c0bcd660e2a5ecfcfc2e9a7f
haozhe.zhu <haozhe.zhu@intel.com>
2023-07-24 16:43:07 +0800

    inductor: legalize fp16 (#100857)
    This PR aims to vectorize FP16 for CPU with what BF16 has done.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100857
    Approved by: https://github.com/jgong5, https://github.com/jansel

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -76,7 +76,7 @@ requires_multigpu = functools.partial(
 skip_if_x86_mac = functools.partial(
     unittest.skipIf, IS_MACOS and IS_X86, "Does not work on x86 Mac"
 )
-vec_dtypes = [torch.float, torch.bfloat16]
+vec_dtypes = [torch.float, torch.bfloat16, torch.float16]
 
 libfoo = None
 
