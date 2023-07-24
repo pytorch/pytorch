310b2b32c92e29472cd1ff0762f411f476f29440
Jason Ansel <jansel@meta.com>
2023-07-23 17:17:29 -0700

    Compiled autograd (#103822)
    This branch:
    1) converts the autograd tape into an FX graph
    2) caches that conversion using a "shadow" graph
    3) compiles and runs the generated FX graph instead of the normal autograd
    
    What works currently:
    1) Caching, capture, and initial integration
    2) Backwards hooks
    3) Inlining AotAutograd generated subgraphs
    4) torch.compiling the generated FX graph
    5) Auto-detecting dynamic shapes based on changes
    
    Future work
    1) Larger scale testing
    1) Boxed calling convention, so memory can be freed incrementally
    1) Support hooks on SavedTensor
    1) Additional testing by running eager autograd tests under compiled_autograd.enable()
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103822
    Approved by: https://github.com/ezyang, https://github.com/albanD

--- torch/csrc/autograd/function.h
+++ torch/csrc/autograd/function.h
@@ -29,12 +29,19 @@ C10_CLANG_DIAGNOSTIC_PUSH()
 C10_CLANG_DIAGNOSTIC_IGNORE("-Wshorten-64-to-32")
 #endif
 
+#ifndef _WIN32
+// Windows build fails due to 65536 symbol limit
+#define TORCH_COMPILED_AUTOGRAD
+#endif
+
 namespace torch {
 namespace autograd {
 
 struct Edge;
 struct FunctionPostHook;
 struct FunctionPreHook;
+class CompiledNodeArgs;
+class SwapSavedVariables;
 
 using tensor_list = std::vector<at::Tensor>;
 using variable_list = std::vector<Variable>;
@@ -556,6 +563,38 @@ struct TORCH_API Node : std::enable_shared_from_this<Node> {
   virtual bool passes_state_transparently() {
     return false;
   }
+
+#ifdef TORCH_COMPILED_AUTOGRAD
+  // see [Note: Compiled Autograd]
+  // Used by compiled autograd to
+  //   1) Extract tensors/symint args
+  //   2) Collect node information for specialization and caching
+  // Implementations in subclasses should call args.collect() with all node
+  // attrs. These functions are only called durring backward.
+  virtual void compiled_args(CompiledNodeArgs& args) {
+    throw std::runtime_error(
+        std::string("compiled_args not implemented: ") + name());
+  }
+
+  // Used by compiled autograd to call apply() with different saved tensors
+  // Implementations should call saved.before() on all attrs, then apply(), then
+  // saved.after() on all attrs.
+  virtual variable_list apply_with_saved(
+      const variable_list& inputs,
+      SwapSavedVariables& saved) {
+    throw std::runtime_error(
+        std::string("apply_with_saved not implemented: ") + name());
+  }
+#else
+  void compiled_args(CompiledNodeArgs& args) {
+    TORCH_CHECK(false, "Windows is not supported for compiled autograd");
+  }
+  variable_list apply_with_saved(
+      const variable_list& inputs,
+      SwapSavedVariables& saved) {
+    TORCH_CHECK(false, "Windows is not supported for compiled autograd");
+  }
+#endif
 
  protected:
   /// Performs the `Node`'s actual operation.
