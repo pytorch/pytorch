8119fda80b1c00e510449f42edaec440b4411304
Masaki Kozuki <mkozuki@nvidia.com>
2023-07-24 14:55:45 -0700

    merge make_saved_variable_list
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- tools/autograd/gen_variable_type.py
+++ tools/autograd/gen_variable_type.py
@@ -1460,10 +1460,7 @@ def emit_body(
                 or type == ListCType(OptionalCType(BaseCType(tensorT)))
                 or type == BaseCType(iTensorListRefT)
             ):
-                if is_foreach and is_output:
-                    expr = f"make_saved_variable_list_foreach_output({name})"
-                else:
-                    expr = f"make_saved_variable_list({name})"
+                expr = f"make_saved_variable_list({name}, {str(is_foreach and is_output).lower()})"
                 name += "_"
             elif type == BaseCType(intArrayRefT):
                 expr = expr + ".vec()"
