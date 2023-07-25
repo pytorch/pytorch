da5cde7601e9212f0622165a59aa07fda4eba25c
Rohan Varma <rvarm1@fb.com>
2023-07-25 10:50:30 -0700

    Update base for Update on "composable fsdp, checkpoint, + compile test"
    Test to ensure that composable FSDP, checkpoint, and compile all work
    together. Includes a change from https://github.com/pytorch/pytorch/pull/105090
    which we can land in that PR first.
    
    Differential Revision: [D47452973](https://our.internmc.facebook.com/intern/diff/D47452973/)
    
    [ghstack-poisoned]

--- test/distributed/_composable/test_compose.py
+++ test/distributed/_composable/test_compose.py
@@ -2,6 +2,7 @@
 
 import copy
 import sys
+from typing import Dict
 
 import torch
 import torch.distributed as dist
@@ -9,18 +10,20 @@ import torch.nn as nn
 from torch.distributed._composable import checkpoint, fully_shard, replicate
 from torch.distributed._shard.sharded_tensor import ShardedTensor
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, StateDictType
-from torch.distributed.fsdp.api import ShardingStrategy
+from torch.distributed.fsdp.api import MixedPrecision, ShardingStrategy
 from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 from torch.testing._internal.common_dist_composable import (
     CompositeModel,
     CompositeParamModel,
     UnitModule,
 )
-from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
+from torch.testing._internal.common_distributed import (
+    SaveForwardInputsModel,
+    skip_if_lt_x_gpu,
+)
 from torch.testing._internal.common_fsdp import FSDPTest
 from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
-    parametrize,
     run_tests,
     TEST_WITH_DEV_DBG_ASAN,
 )
@@ -80,15 +83,14 @@ class TestFSDPCheckpoint(FSDPTest):
             base_optim.zero_grad(set_to_none=grad_to_none)
 
     @skip_if_lt_x_gpu(2)
-    @parametrize("use_reentrant", [True, False])
-    def test_wrap_same_submodule(self, use_reentrant: bool):
+    def test_wrap_same_submodule(self):
         model = UnitModule(device=torch.device("cuda"))
 
         base_model = copy.deepcopy(model)
 
         test_model = copy.deepcopy(model)
         # compose checkpoint and fully_shard
-        test_model.seq = checkpoint(test_model.seq, use_reentrant=use_reentrant)
+        test_model.seq = checkpoint(test_model.seq)
         test_model.seq = fully_shard(
             test_model.seq,
             policy=ModuleWrapPolicy({nn.Linear}),
@@ -106,7 +108,7 @@ class TestFSDPCheckpoint(FSDPTest):
             self._test_parity,
         )
 
-    def _test_checkpoint_fsdp_submodules(self, use_reentrant):
+    def _test_checkpoint_fsdp_submodules(self):
         model = CompositeModel(device=torch.device("cuda"))
 
         base_model = copy.deepcopy(model)
@@ -115,8 +117,8 @@ class TestFSDPCheckpoint(FSDPTest):
         test_model.u1 = fully_shard(test_model.u1, policy=None)
         test_model.u2 = fully_shard(test_model.u2)
 
-        test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=use_reentrant)
-        test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=use_reentrant)
+        test_model.u1.seq = checkpoint(test_model.u1.seq)
+        test_model.u2.seq = checkpoint(test_model.u2.seq)
 
         self.run_subtests(
             {
@@ -129,21 +131,6 @@ class TestFSDPCheckpoint(FSDPTest):
             },
             self._test_parity,
         )
-
-    @skip_if_lt_x_gpu(2)
-    def test_checkpoint_fsdp_submodules_use_reentrant(self):
-        # Escape the brackets like `\[` since `[` has special meaning in regex
-        with self.assertRaisesRegex(
-            RuntimeError,
-            r"setStorage: sizes \[100, 100\], strides \[100, 1\], storage "
-            "offset 0, and itemsize 4 requiring a storage size of 40000 are "
-            "out of bounds for storage of size 0",
-        ):
-            self._test_checkpoint_fsdp_submodules(True)
-
-    @skip_if_lt_x_gpu(2)
-    def test_checkpoint_fsdp_submodules_non_reentrant(self):
-        self._test_checkpoint_fsdp_submodules(False)
 
     @skip_if_lt_x_gpu(2)
     def test_fully_shard_replicate_correct_replicate_params(self):
@@ -182,8 +169,8 @@ class TestFSDPCheckpoint(FSDPTest):
         base_model = copy.deepcopy(model)
 
         test_model = copy.deepcopy(model)
-        test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=False)
-        test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=False)
+        test_model.u1.seq = checkpoint(test_model.u1.seq)
+        test_model.u2.seq = checkpoint(test_model.u2.seq)
         test_model = fully_shard(test_model)
 
         self.run_subtests(
@@ -205,8 +192,8 @@ class TestFSDPCheckpoint(FSDPTest):
         base_model = copy.deepcopy(model)
 
         test_model = copy.deepcopy(model)
-        test_model.u1.seq = checkpoint(test_model.u1.seq, use_reentrant=False)
-        test_model.u2.seq = checkpoint(test_model.u2.seq, use_reentrant=False)
+        test_model.u1.seq = checkpoint(test_model.u1.seq)
+        test_model.u2.seq = checkpoint(test_model.u2.seq)
         test_model = fully_shard(test_model, strategy=ShardingStrategy.NO_SHARD)
 
         self.run_subtests(
