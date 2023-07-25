1a316c4a6ef158fc3f3c01d9d771b522486376d1
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2023-07-25 10:33:45 +0100

    Remove miopen_batch_norm_backward lowering

--- torch/_decomp/__init__.py
+++ torch/_decomp/__init__.py
@@ -267,7 +267,6 @@ def core_aten_decompositions() -> Dict[OpOverload, Callable]:
             aten.mish,
             aten.mish_,
             aten.miopen_batch_norm,
-            aten.miopen_batch_norm_backward,
             aten.mse_loss,
             aten.mse_loss_backward,
             aten.multi_margin_loss,
