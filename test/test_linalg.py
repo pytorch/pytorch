3349d3427b07f97ac163691e428f56607f881e89
nikitaved <nikitavedeneev@gmail.com>
2023-07-25 15:44:06 +0200

    Update base for Update on "test_linalg: triangular_solve - set higher precision"
    This is a follow-up on https://github.com/pytorch/pytorch/pull/104425.
    
    
    
    
    [ghstack-poisoned]

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -3992,8 +3992,8 @@ class TestLinalg(TestCase):
 
             if well_conditioned:
                 make_fullrank = make_fullrank_matrices_with_distinct_singular_values
-                make_A = partial(make_fullrank, device=device, dtype=dtype)
-                PLU = torch.linalg.lu(make_A(make_randn(*size_a)))
+                make_well_conditioned_arg = partial(make_fullrank, device=device, dtype=dtype)
+                PLU = torch.linalg.lu(make_well_conditioned_arg(make_randn(*size_a)))
                 if uni:
                     # A = L from PLU
                     A = PLU[1].transpose(-2, -1).contiguous()
