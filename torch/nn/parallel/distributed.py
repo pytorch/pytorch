64f4c27f6f4d9f5bf692a4efb0a1c1abe0ae9fb1
Rohan Varma <rvarm1@fb.com>
2023-07-25 19:08:36 -0700

    [DDP] Support optim in backward after DDP init
    This allows in backward optimizers to be configured after DDP init, in
    addition to before as was previously supported.
    
    Differential Revision: [D47783347](https://our.internmc.facebook.com/intern/diff/D47783347/)
    
    [ghstack-poisoned]

--- torch/nn/parallel/distributed.py
+++ torch/nn/parallel/distributed.py
@@ -861,7 +861,7 @@ class DistributedDataParallel(Module, Joinable):
         if static_graph:
             self._set_static_graph()
 
-        self._setup_in_backward_optimizers()
+        self._lazy_init_ran = False
 
     def _register_delay_all_reduce_hook(
         self,
@@ -1377,7 +1377,15 @@ class DistributedDataParallel(Module, Joinable):
             if all_param_grad_none:
                 self._delay_grad_buffer.zero_()
 
+    def _lazy_init(self):
+        # Initialization for DDP that occurs after construction, but lazily
+        # before the first forward pass.
+        self._setup_in_backward_optimizers()
+        self._lazy_init_ran = True
+
     def _pre_forward(self, *inputs, **kwargs):
+        if not self._lazy_init_ran:
+            self._lazy_init()
         if self._delay_all_reduce_all_params:
             return inputs, kwargs
 
