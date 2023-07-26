9d4c4031cca3f20dc4490a1bdeb1ddfc6793f40c
Frost Mitchell <frost.mitchell@intel.com>
2023-07-26 14:50:54 -0600

    Linting, comments, rename llga to onednn in some cases
    * Linting, comments, rename llga to onednn in some cases
    * Add env variable to enable config.cpp.onednn_graph

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -221,7 +221,7 @@ class GraphLowering(torch.fx.Interpreter):
         )
         self._warned_fallback = {"aten.convolution_backward"}
         self.user_visible_outputs = user_visible_outputs
-        self.opaque_ops = {}
+        self.opaque_ops: Dict[str, Callable] = {}
         self.cache_key: str = ""  # This is the cache key for the compiled artifact
         self.cache_path: str = ""  # This is the path in the filesystem where the compiled artifact is stored
         self.cache_linemap: List[
