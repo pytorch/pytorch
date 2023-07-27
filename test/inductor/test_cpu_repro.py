8bd373a423b1d03e4b52713a9b719048e56b4c62
XiaobingSuper <xiaobing.zhang@intel.com>
2023-07-26 20:06:10 -0400

    inductor: always convert weight to channels_last for cpu conv (#105517)
    For the CPU backend, we always use channels_last to get good performance by avoiding format reorder(block to plain or plain to black), and they also assume that the weight is channels_last when doing the weight packing, so there always convert weight format and doing layout optimization.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105517
    Approved by: https://github.com/jgong5, https://github.com/shunting314

--- test/inductor/test_cpu_repro.py
+++ test/inductor/test_cpu_repro.py
@@ -59,7 +59,7 @@ class CPUReproTests(TestCase):
     common = check_model
 
     def test_conv_stride_constraints(self):
-        for fmt in [torch.channels_last, torch.contiguous_format]:
+        for fmt in [torch.contiguous_format, torch.channels_last]:
             # TorchDispatch doesn't work in our cuda invocation for some reason
             m = torch.nn.Conv2d(5, 6, [3, 3])
 
@@ -81,6 +81,13 @@ class CPUReproTests(TestCase):
                 def __torch_dispatch__(self, func, types, args=(), kwargs=None):
                     kwargs = kwargs if kwargs else {}
                     if func == torch.ops.aten.convolution.default:
+                        # For CPU and mkldnn enable, we always using channles last
+                        nonlocal fmt
+                        if (
+                            torch.backends.mkldnn.enabled
+                            and torch.backends.mkldnn.is_available()
+                        ):
+                            fmt = torch.channels_last
                         test_self.assertTrue(args[0].is_contiguous(memory_format=fmt))
                         test_self.assertTrue(args[1].is_contiguous(memory_format=fmt))
                         nonlocal conv_seen
