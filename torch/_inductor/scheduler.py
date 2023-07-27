36369cc152fcc78738568caf1ec3fad77d381c52
Michael Lazos <mlazos@fb.com>
2023-07-27 00:11:45 +0000

    Remove window change (separate it out)

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -1140,7 +1140,7 @@ class Scheduler:
 
         def check_all_pairs(nodes):
             for node1_index, node1 in enumerate(nodes):
-                for node2 in nodes[node1_index + 1 : (node1_index + 64)]:
+                for node2 in nodes[node1_index + 1 :]:
                     key = (node1, node2)
                     if key in seen:
                         continue
