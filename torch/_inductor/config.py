552ddecdad2ce04ba2e9ce1249f3432c62bfccad
Elias Ellison <elias.ellison@gmail.com>
2023-07-27 10:32:41 -0700

    More descriptive graph diagram names
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -410,7 +410,7 @@ class trace:
     output_code = True
 
     # SVG figure showing post-fusion graph
-    graph_diagram = False
+    graph_diagram = os.environ.get("INDUCTOR_GRAPH", "0") == "1"
 
     # Store cProfile (see snakeviz to view)
     compile_profile = False
