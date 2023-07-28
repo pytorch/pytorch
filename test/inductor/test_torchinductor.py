0010a8f75363262a2befe3b0d1ffeff9711e3d18
Elias Ellison <elias.ellison@gmail.com>
2023-07-28 16:09:20 -0700

    Deallocate constant when it is no longer needed in constant folding (#106216)
    Differential Revision: [D47881214](https://our.internmc.facebook.com/intern/diff/D47881214)
    
    tested locally with :
    ```
    @torch.compile()
    def foo():
        size_gb = 1
        size_bytes = size_gb * 1024 * 1024 * 1024 * 20
    
        # Allocate the tensor on the GPU
        tensor = torch.empty(size_bytes // 4, device='cuda')  # Divide by 4 to allocate float32 elements
    
        for _ in range(10):
            tensor = tensor + 1
    
        return tensor
    
    foo()
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106216
    Approved by: https://github.com/Skylion007

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -46,6 +46,7 @@ from torch.testing._internal.common_utils import (
 )
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils._pytree import tree_flatten, tree_unflatten
+from torch.utils.weak import WeakTensorKeyDictionary
 
 if IS_WINDOWS and IS_CI:
     sys.stderr.write(
@@ -6947,6 +6948,46 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertFalse("to(tl.int64)" in code)
 
             self.assertEqual(fn_opt(*inps), fn(*inps))
+
+        def test_constant_folding_deallocation(self):
+            import torch._inductor
+
+            def fn():
+                x = torch.empty([100])
+                for _ in range(10):
+                    x = x + 1
+
+                return x
+
+            mod = make_fx(fn)()
+
+            live_tensors = WeakTensorKeyDictionary()
+            max_live_tensors = 0
+
+            class LiveTensors(TorchDispatchMode):
+                def __torch_dispatch__(self, func, types, args=(), kwargs=None):
+                    nonlocal live_tensors
+                    nonlocal max_live_tensors
+
+                    kwargs = kwargs if kwargs else {}
+                    for arg in tree_flatten((args, kwargs))[0]:
+                        if isinstance(arg, torch.Tensor):
+                            live_tensors[arg] = True
+
+                    out = func(*args, **kwargs)
+
+                    live_tensors[out] = True
+                    max_live_tensors = max(max_live_tensors, len(live_tensors))
+
+                    return out
+
+            mode = LiveTensors()
+            from torch._inductor.freezing import ConstantFolder
+
+            with mode:
+                ConstantFolder(mod).run()
+
+            self.assertTrue(max_live_tensors == 2)
 
         # See https://github.com/pytorch/pytorch/issues/100348
         def test_inductor_detach_view(self):
