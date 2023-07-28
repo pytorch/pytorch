340c6b65c2febd336542f47c7972051355b2d49e
Jason Ansel <jansel@meta.com>
2023-07-28 10:54:23 -0700

    [Compiled Autograd] Support CopySlices and CopyBackwards (#105809)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105809
    Approved by: https://github.com/albanD

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -328,12 +328,9 @@ skip_re = re.compile(r"^test_(sparse|profiler|gradcheck|checkpoint|named_tensor)
 # Bugs needing investigation:
 skips = {
     "test_accumulate_grad_tensor_reference",  # torch._dynamo.exc.BackendCompilerFailed: backend='inner_compiler' rai
-    "test_autograd_inplace_views_cross_dtype",  # RuntimeError: compiled_args not implemented: torch::autograd::CopyS
     "test_calculate_shape_util",  # AssertionError: NYI: aten._nested_tensor_from_tensor_list.default
-    "test_copy_slices_graph_task_updates",  # AssertionError: "Boom!" does not match "compiled_args not implemented:
     "test_current_graph_task_execution_order",  # torch._dynamo.exc.TorchRuntimeError: Failed running call_function <
     "test_current_node",  # RuntimeError: aten::detach() Expected a value of type 'Tensor' for argument 'self' but in
-    "test_dont_materialize_grads",  # RuntimeError: compiled_args not implemented: torch::autograd::UndefinedGradBack
     "test_duplicate_backward_root",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch/c
     "test_grad_fn_attr_bindings",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch/csr
     "test_grad_unreachable_discovery",  # RuntimeError: tensor does not have a device
@@ -342,13 +339,9 @@ skips = {
     "test_graph_save_on_cpu",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch/csrc/dy
     "test_hooks_cpp",  # torch._dynamo.exc.BackendCompilerFailed: backend='inner_compiler' raised:
     "test_index_backward_does_not_save_tensor",  # RuntimeError: expected int but got i0
-    "test_inplace_on_view_weak_grad_fn",  # RuntimeError: compiled_args not implemented: torch::autograd::CopySlices
     "test_input_buffer_accum",  # RuntimeError: Cannot access data pointer of Tensor that doesn't have storage
     "test_integer_outputs",  # TypeError: unsupported operand type(s) for +: 'OpOverload' and 'str'
-    "test_leaf_assignment",  # RuntimeError: compiled_args not implemented: torch::autograd::CopySlices
     "test_lobpcg",  # RuntimeError: tried to get Double out of SymFloat
-    "test_materialize_grads",  # RuntimeError: compiled_args not implemented: torch::autograd::UndefinedGradBackward
-    "test_no_unnecessary_save",  # RuntimeError: compiled_args not implemented: torch::autograd::CopyBackwards
     "test_no_unnecessary_unwrapping",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch
     "test_numpy_requires_grad",  # AssertionError: "Can't call numpy\(\) on Tensor that requires grad. Use tensor.det
     "test_pickle",  # TypeError: cannot pickle 'StorageWeakRef' object: a class that defines __slots__ without defini
@@ -359,10 +352,9 @@ skips = {
     "test_saved_variable_saved_original_inplace_detach",  # AssertionError: RuntimeError not raised
     "test_saving_variable_to_disk",  # AttributeError: Can't pickle local object 'WeakValueDictionary.__init__.<local
     "test_setitem_mask",  # torch.fx.experimental.symbolic_shapes.GuardOnDataDependentSymNode: It appears that you're
-    "test_setitem",  # RuntimeError: compiled_args not implemented: torch::autograd::CopySlices
     "test_setting_default_saved_variable_hooks_twice_should_use_inner",  # RuntimeError: inserted INTERNAL ASSERT FAI
     "test_sharded_grad",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch/csrc/dynamo/
-    "test_tensor_hooks_inplace_over_view",  # RuntimeError: compiled_args not implemented: torch::autograd::CopySlic
+    "test_tensor_hooks_inplace_over_view",  # torch._dynamo.exc.Unsupported: call_function UserDefinedClassVariable() [] {}
     "test_tensor_hooks_inplace",  # torch._dynamo.exc.Unsupported: call_function UserDefinedClassVariable() [] {}
     "test_to_sparse_backward",  # torch._dynamo.exc.BackendCompilerFailed: backend='inner_compiler' raised:
     "test_var_mean_differentiable",  # RuntimeError: inserted INTERNAL ASSERT FAILED at "/home/jansel/pytorch/torch/c
