e051f7e45d7925317697fcc5c61d2845cffb16d8
Edward Z. Yang <ezyang@meta.com>
2023-07-28 16:04:39 -0400

    Apply UFMT to all non test/torch files (#106205)
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106205
    Approved by: https://github.com/albanD

--- functorch/experimental/__init__.py
+++ functorch/experimental/__init__.py
@@ -1,5 +1,6 @@
 # PyTorch forward-mode is not mature yet
+from torch._functorch.batch_norm_replacement import replace_all_batch_norm_modules_
 from torch._functorch.eager_transforms import hessian, jacfwd, jvp
 from torch._functorch.vmap import chunk_vmap
-from torch._functorch.batch_norm_replacement import replace_all_batch_norm_modules_
+
 from functorch import functionalize
