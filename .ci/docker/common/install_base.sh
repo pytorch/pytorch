6e39adeb189a736130e974ef07ea0b9385bc6624
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-07-28 13:24:03 -0700

    Updates to build on Jammy
    - Fortran package installation moved after gcc
    - Update libtinfo search code in cmake1
    - Install libstdc++.so

--- .ci/docker/common/install_base.sh
+++ .ci/docker/common/install_base.sh
@@ -87,10 +87,14 @@ install_ubuntu() {
 
     # https://www.spinics.net/lists/libreoffice/msg07549.html
     sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/11
-    wget https://github.com/gcc-mirror/gcc/commit/2b2d97fc545635a0f6aa9c9ee3b017394bc494bf.patch -O noexecpt.patch
-    sudo patch  /usr/include/c++/10/bits/range_access.h noexecpt.patch
   fi
 
+  # Required to install the fortran after gcc update
+  if [[ "$UBUNTU_VERSION" == "22.04"* ]]; then
+    apt autoremove -y gfortran
+    apt-get update -y
+    apt-get install -y gfortran
+  fi
   # Cleanup package manager
   apt-get autoclean && apt-get clean
   rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
