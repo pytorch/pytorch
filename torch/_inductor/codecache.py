67b34180a7855da7f0bb25c153b911e536abe1ee
David Berard <dberard@fb.com>
2023-07-31 10:24:54 -0700

    make CppCompileError accept strings or bytes

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -951,15 +951,12 @@ def compile_file(input_path, output_path, cmd) -> None:
         else:
             subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        output = e.output
-        if (
-            "'omp.h' file not found" in output.decode("utf-8")
-            and sys.platform == "darwin"
-        ):
+        output = e.output.decode("utf-8")
+        if "'omp.h' file not found" in output and sys.platform == "darwin":
             output = (
-                output.decode("utf-8")
+                output
                 + "\n\nTry setting OMP_PREFIX; see https://github.com/pytorch/pytorch/issues/95708"
-            ).encode("utf-8")
+            )
         raise exc.CppCompileError(cmd, output) from e
 
 
