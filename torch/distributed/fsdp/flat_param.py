0ee4822c985a89f96c96a60496c589a6f90cd835
Michael Voznesensky <voznesenskym@gmail.com>
2023-08-01 02:25:08 +0000

    Revert "Revert "Simplify handle indexing (#105006)" (#105984)"
    This reverts commit a9a3c456495ccddff55e088ebf395c599db62d12.
    
    fixes
    
    [ghstack-poisoned]

--- torch/distributed/fsdp/flat_param.py
+++ torch/distributed/fsdp/flat_param.py
@@ -511,6 +511,19 @@ class FlatParamHandle:
         # it points to parameterizes behavior. We use the following attribute
         # to track which tensor data the parameters are unsharded views into.
         self._unsharded_flat_param_for_skipped_views: Optional[Tensor] = None
+        # The index in the state's `all_handles`, which must be the
+        # same across ranks for the execution order validation to work
+        self._handle_index: Optional[int] = None
+        # Index in handles_to_pre_forward_order
+        self._pre_forward_order_index: Optional[int] = None
+        # Index in `handles_post_forward_order`
+        self._post_forward_index: Optional[int] = None
+        # Used for guarding against mistargeted forward prefetches
+        self._needs_pre_forward_unshard = False
+        # Used for guarding against mistargeted backward prefetches
+        self._needs_pre_backward_unshard = False
+        # Was the handle prefetched? Set on successful _prefetch_handle and unshard
+        self._prefetched = False
         # Optimistically assume a valid input `params` and set dtype attributes
         # before `_init_flat_param()`, which performs the actual validation
         self._orig_param_dtype = params[0].dtype
