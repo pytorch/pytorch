32613c69ae0d44e9fe3e5e1896252838b8334ff5
igm503 <igm503@gmail.com>
2023-08-03 14:35:37 -0500

    digamma working; slight innaccuracy near negative integers

--- aten/src/ATen/native/mps/operations/Gamma.mm
+++ aten/src/ATen/native/mps/operations/Gamma.mm
@@ -12,6 +12,7 @@
 // #include <ATen/NativeFunctions.h>
 // #else
 #include <ATen/ops/lgamma_native.h>
+#include <ATen/ops/digamma_native.h>
 // #endif
 
 
@@ -30,74 +31,17 @@
 namespace at::native {
 namespace mps {
 
-    const float PSI_10 = 2.25175258906672110764;
-
-    kernel void digamma (device {0} *input [[buffer(0)]],
-                        device {1} *output [[buffer(1)]],
-                        uint id [[thread_position_in_grid]]) {
-        if (x < 0) {
-            if (x == trunc(x)) {
-                // As per C++ standard for gamma related functions and SciPy,
-                // If the argument is a negative integer, NaN is returned
-                output[id] = NAN;
-                }
-            else {
-                // Extracts the fractional part of x as r, since tan(pi * r) is more numerically
-                // accurate than tan(pi * x). While these operations are mathematically equivalent
-                // since both x and r are in radians and tan() has a periodicity of pi, in practice
-                // the computation of pi * x is a source of error (when |x| > 1).
-                float q, r;
-                r = std::modf(x, &q);
-                output[id] = calc_digamma_positive_domain(1 - x) - M_PI_BF / tan(M_PI_BF * r);
-            }
-
-  }
-
-    }
-
-    /*
- * This function is derived from the implementation of the digamma function in the Cephes Math Library.
- * See note [3-Clause BSD License for the Cephes Math Library].
- */
-float calc_digamma_positive_domain(git x) {
-  // [C++ Standard Reference: Gamma Function] https://en.cppreference.com/w/cpp/numeric/math/tgamma
-  if (x == 0) {
-    // As per C++ standard for gamma related functions and SciPy,
-    // If the argument is ±0, ±∞ is returned
-    return std::copysign(INFINITY, -x);
-  }
-
-  // Push x to be >= 10
-  double result = 0;
-  while (x < 10) {
-    result -= 1 / x;
-    x += 1;
-  }
-  if (x == 10) {
-    return result + PSI_10;
-  }
-
-  // Compute asymptotic digamma
-  static const double A[] = {
-      8.33333333333333333333E-2,
-      -2.10927960927960927961E-2,
-      7.57575757575757575758E-3,
-      -4.16666666666666666667E-3,
-      3.96825396825396825397E-3,
-      -8.33333333333333333333E-3,
-      8.33333333333333333333E-2,
-  };
-
-  double y = 0;
-  if (x < 1.0e17) {
-    double z = 1.0 / (x * x);
-    y = z * polevl(z, A, 6);
-  }
-  return result + log(x) - (0.5 / x) - y;
-}
-
-// The gamma function approximations come from John D Cook's
-// c++ implementation:  https://www.johndcook.com/Gamma.cpp.
+/*
+* The gamma function approximations follow John D Cook's
+* c++ implementation:  https://www.johndcook.com/Gamma.cpp.
+* (BSD License)
+*
+*
+* The digamma kernel and helper function is derived from the pytorch cpu
+* of this function, which is itself derived from the implementation
+* of the digamma function in the Cephes Math Library.
+* See note [3-Clause BSD License for the Cephes Math Library].
+*/
 
 static const char* GAMMA_OPS_TEMPLATE = R"METAL(
 #include <metal_stdlib>
@@ -109,10 +53,8 @@ constant float HALF_LOG_TWO_PI = 0.91893853320467274178032973640562;
 
 constant float LOG_PI = 1.14472988584940017414342735135305;
 
-constant float PI = 3.141592653589793238462643383279502;
 
-
-// numerator coefficients for approximation over the interval (1,2)
+// numerator coefficients for gamma approximation over the interval (1,2)
 constant float GAMMA_NUMERATOR_COEF[8] =
     {{
         -1.71618513886549492533811E+0,
@@ -125,7 +67,7 @@ constant float GAMMA_NUMERATOR_COEF[8] =
         6.64561438202405440627855E+4
     }};
 
-// denominator coefficients for approximation over the interval (1,2)
+// denominator coefficients for gamma approximation over the interval (1,2)
 constant float GAMMA_DENOMINATOR_COEF[8] =
     {{
         -3.08402300119738975254353E+1,
@@ -254,7 +196,7 @@ float LogGamma(float x) {{
 
     if (is_negative)
     {{
-        return LOG_PI - logGamma - log(fabs(x * sin(x * PI))); // Reflection Formula
+        return LOG_PI - logGamma - log(fabs(x * sinpi(x))); // Reflection Formula
     }}
 
     return logGamma;
@@ -264,11 +206,93 @@ float LogGamma(float x) {{
 
 kernel void lgamma(device {0} *input [[buffer(0)]],
                    device {1} *output [[buffer(1)]],
-                   uint id [[thread_position_in_grid]]) {{
+                   uint id [[thread_position_in_grid]])
+{{
     output[id] = LogGamma(static_cast<float>(input[id]));
 }}
 
+
+
+constant float PSI_10 = 2.25175258906672110764;
+
+constant float DIGAMMA_COEF[7] =
+    {{
+        8.33333333333333333333E-2,
+        -2.10927960927960927961E-2,
+        7.57575757575757575758E-3,
+        -4.16666666666666666667E-3,
+        3.96825396825396825397E-3,
+        -8.33333333333333333333E-3,
+        8.33333333333333333333E-2,
+    }};
+
+float calc_digamma_positive_domain(float x) {{
+
+    // Push x to be >= 10
+    float result = 0;
+    while (x < 10) {{
+        result -= 1 / x;
+        x += 1;
+    }}
+    if (x == 10) {{
+        return result + PSI_10;
+    }}
+
+    // Compute asymptotic digamma
+    float y = 0;
+    if (x < 1.0E+17) {{
+        float z = 1.0 / (x * x);
+        for (int i = 0; i <= 6; i++) {{
+            y += pow(z, i) * DIGAMMA_COEF[i];
+        }}
+        y *= z;
+        // for (int i = 6; i >= 0; i--) {{
+        //     y += DIGAMMA_COEF[i]
+        //     y *= z
+        // }}
+        //y = z * polevl(z, DIGAMMA_COEF, 6);
+    }}
+    return result + log(x) - (0.5 / x) - y;
+}}
+
+kernel void digamma (device {0} *input [[buffer(0)]],
+                    device {1} *output [[buffer(1)]],
+                    uint id [[thread_position_in_grid]])
+{{
+    float x = input[id];
+    if (x < 0) {{
+        if (x == trunc(x)) {{
+            // As per C++ standard for gamma related functions and SciPy,
+            // If the argument is a negative integer, NaN is returned
+            output[id] = NAN;
+        }}
+        else {{
+            // Extracts the fractional part of x as r, since tan(pi * r) is more numerically
+            // accurate than tan(pi * x). While these operations are mathematically equivalent
+            // since both x and r are in radians and tan() has a periodicity of pi, in practice
+            // the computation of pi * x is a source of error (when |x| > 1).
+            float q, r;
+            r = modf(x, q);
+            output[id] = calc_digamma_positive_domain(1 - x) - M_PI_F / tanpi(r);
+        }}
+    }}
+    else if (x == 0) {{
+        // As per C++ standard for gamma related functions and SciPy,
+        // If the argument is ±0, ±∞ is returned
+        output[id] = copysign(INFINITY, -x);
+    }}
+    else {{
+        output[id] = calc_digamma_positive_domain(x);
+    }}
+}}
+
+
+
 )METAL";
+
+
+
+
 
 void dispatch1DJob(id<MTLComputeCommandEncoder> commandEncoder, id<MTLComputePipelineState> cplState, uint32_t length);
 
@@ -352,6 +376,58 @@ TORCH_IMPL_FUNC(lgamma_out_mps)(const Tensor& self, const Tensor& output_) {
       id<MTLBuffer> selfBuf = getMTLBufferStorage(self);
 
       getMPSProfiler().beginProfileKernel(cplState, "lgamma_out", {self});
+
+      [computeEncoder setComputePipelineState:cplState];
+      [computeEncoder setBuffer:selfBuf offset:self.storage_offset() * self.element_size() atIndex:0];
+      [computeEncoder setBuffer:outBuf offset:output.storage_offset() * output.element_size() atIndex:1];
+
+
+      mps::dispatch1DJob(computeEncoder, cplState, static_cast<uint32_t>(length));
+
+      getMPSProfiler().endProfileKernel(cplState);
+    });
+  }
+  if (needs_output_copy) {
+    output_.copy_(output);
+  }
+}
+
+TORCH_IMPL_FUNC(digamma_out_mps)(const Tensor& self, const Tensor& output_) {
+
+  TORCH_CHECK(self.scalar_type() != ScalarType::Double, "MPS does not support digamma_out op with scalar type: Double");
+
+  Tensor output = output_;
+  bool needs_output_copy = false;
+  uint32_t length = output.numel();
+  if (length == 0) {
+    return;
+  }
+
+  if (!self.is_contiguous()) {
+      output = output.contiguous();
+      needs_output_copy = true;
+    }
+
+  using namespace mps;
+
+  std::string input_type = scalarToMetalTypeString(self.scalar_type());
+  std::string output_type = scalarToMetalTypeString(output.scalar_type());
+
+  @autoreleasepool {
+
+    id<MTLDevice> device = MPSDevice::getInstance()->device();
+    id<MTLComputePipelineState> cplState = getCPLState(device,
+                                                        input_type,
+                                                        output_type,
+                                                        "digamma");
+
+    MPSStream* mpsStream = getCurrentMPSStream();
+    dispatch_sync(mpsStream->queue(), ^() {
+      id<MTLComputeCommandEncoder> computeEncoder = mpsStream->commandEncoder();
+      id<MTLBuffer> outBuf = getMTLBufferStorage(output);
+      id<MTLBuffer> selfBuf = getMTLBufferStorage(self);
+
+      getMPSProfiler().beginProfileKernel(cplState, "digamma_out", {self});
 
       [computeEncoder setComputePipelineState:cplState];
       [computeEncoder setBuffer:selfBuf offset:self.storage_offset() * self.element_size() atIndex:0];
