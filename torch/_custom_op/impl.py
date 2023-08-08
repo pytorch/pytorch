7d458b42c62543eccacbc56e1e8c97b3dcccd5e2
Richard Zou <zou3519@gmail.com>
2023-08-08 13:50:43 -0700

    Update base for Update on "Extend impl_backward to handle non-Tensor outputs"
    Recall that the user must give us a backward function that accepts
    `(ctx, saved, *grads)`, with one grad per output. Previously,
    impl_backward only worked for functions that return one or more Tensors.
    
    The new semantics are that if the output has:
    - a TensorList, the backward function provided by the user will receive
    a List[Tensor] of grads for that output.
    - a number, the backward function provided by the user will receive
    None as the grad.
    
    Also recall that impl_backward is implemented by registering an
    autograd.Function to the autograd dispatch key.
    We needed to make the following changes:
    - If an output is a TensorList, autograd.Function will ignore it. So we
    need to tree-flatten it before returning it from the autograd.Function
    - This means that the autograd.Function receives a flat list of grad
    during the backwards pass. We need to tree-unflatten it into the correct
    shape before passing it to the user-defined backward
    - We modify the logic of output_differentiability. Only
    Tensor/TensorList outputs can be marked as differentiable. If a
    TensorList is marked as non-differentiable, then this is equivalent to
    all Tensors in the list being non-differentiable. There is no
    finer-grain control over this (to match derivatives.yaml).
    
    Test Plan:
    - There are new `numpy_split_copy` (returns TensorList) and
    `numpy_split_copy_with_int` (returns (TensorList, int)) operators in
    custom_op_db
    - Added tests for output_differentiability into test/test_custom_ops.py
    
    [ghstack-poisoned]

--- torch/_custom_op/impl.py
+++ torch/_custom_op/impl.py
@@ -580,6 +580,7 @@ def validate_namespace(ns: str) -> None:
             f"please choose something else. "
         )
 
+
 def validate_schema(schema: FunctionSchema) -> None:
     # Coming in the future. Requires us to have correct logic for
     # the ADInplaceOrView key
