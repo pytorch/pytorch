b408aa03df868ceb5e5570d4ed88700764ea4672
Edward Z. Yang <ezyang@meta.com>
2023-08-08 13:01:39 -0400

    Add gcc11 cuda11.8 docker image
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -160,6 +160,35 @@ case "$image" in
     TRITON=yes
     INDUCTOR_BENCHMARKS=yes
     ;;
+  pytorch-linux-bionic-cuda11.8-cudnn8-py3-gcc11)
+    CUDA_VERSION=11.8.0
+    CUDNN_VERSION=8
+    ANACONDA_PYTHON_VERSION=3.10
+    GCC_VERSION=11
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    KATEX=yes
+    UCX_COMMIT=${_UCX_COMMIT}
+    UCC_COMMIT=${_UCC_COMMIT}
+    CONDA_CMAKE=yes
+    TRITON=yes
+    ;;
+    pytorch-linux-bionic-cuda11.8-cudnn8-py3-gcc11-inductor-benchmarks)
+    CUDA_VERSION=11.8.0
+    CUDNN_VERSION=8
+    ANACONDA_PYTHON_VERSION=3.10
+    GCC_VERSION=11
+    PROTOBUF=yes
+    DB=yes
+    VISION=yes
+    KATEX=yes
+    UCX_COMMIT=${_UCX_COMMIT}
+    UCC_COMMIT=${_UCC_COMMIT}
+    CONDA_CMAKE=yes
+    TRITON=yes
+    INDUCTOR_BENCHMARKS=yes
+    ;;
   pytorch-linux-focal-cuda12.1-cudnn8-py3-gcc9)
     CUDA_VERSION=12.1.0
     CUDNN_VERSION=8
