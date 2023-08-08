537dd719c9b5cbd999ad88e42ac7bce731a95c2d
eqy <eddiey@nvidia.com>
2023-08-07 17:18:28 -0700

    Update CUDAGraph.cpp

--- aten/src/ATen/cuda/CUDAGraph.cpp
+++ aten/src/ATen/cuda/CUDAGraph.cpp
@@ -121,7 +121,6 @@ void CUDAGraph::capture_begin(MempoolId_t pool/*=0*/) {
   // https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__STREAM.html#group__CUDART__STREAM_1g9d0535d93a214cbf126835257b16ba85
   AT_CUDA_CHECK(cudaStreamBeginCapture(capture_stream_, cudaStreamCaptureModeGlobal));
 
-  // Stashes the current capture's uuid.
   cudaStreamCaptureStatus status;
   AT_CUDA_CHECK(cudaStreamGetCaptureInfo(stream, &status, nullptr));
   TORCH_INTERNAL_ASSERT(status == cudaStreamCaptureStatus::cudaStreamCaptureStatusActive);
