b930c048f4f48f802860d02a9cf5c10c4cbabef0
David Berard <dberard@fb.com>
2023-08-09 08:43:42 -0700

    cleanup

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -5665,7 +5665,6 @@ def bucketize(
 )
 def cauchy(self, median=0, sigma=1, generator=None):
     assert generator is None
-    # breakpoint()
     torch._check(
         not utils.is_complex_dtype(self.dtype)
         and not utils.is_integer_dtype(self.dtype)
