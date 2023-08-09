7b0165224247cc0b3b3619ca83d41ea58364aaa3
Bernie Gray <bfgray3@gmail.com>
2023-08-09 17:48:29 -0400

    test_out()

--- test/test_ops.py
+++ test/test_ops.py
@@ -823,7 +823,7 @@ class TestCommon(TestCase):
 
             # Validates the op doesn't support out if it claims not to
             if not op.supports_out:
-                with self.assertRaises(NameError):
+                with self.assertRaises(AssertionError):
                     assert op_out(out=expected) != NotImplemented
                 return
 
