df4805b521b4e9ced58805a7b8fe7e5c8b38f76a
Jerry Zhang <jerryzh168@gmail.com>
2023-08-10 16:10:08 -0700

    Update on "[quant][pt2e] Add choose_qparams ops in quant namespace, to be used in representing dynamically quantized linear"
    Summary:
    att
    
    Test Plan:
    will be tested later
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/ao/quantization/pt2e/representation/_ops.py
+++ torch/ao/quantization/pt2e/representation/_ops.py
@@ -1,21 +1,22 @@
-from torch.library import Library, impl
+from typing import Tuple
+
+import torch
 from torch.ao.quantization.utils import determine_qparams, validate_qmin_qmax
+from torch.library import impl, Library
 
 quant_lib = Library("quant", "DEF")
 
-quan_lib.define(
+quant_lib.define(
     "choose_qparams.tensor(Tensor input, int quant_min, int quant_max, "
-    "float eps, ScalarType dtype) -> (Tensor, Tensor)")
+    "float eps, ScalarType dtype) -> (Tensor, Tensor)"
+)
+
 
 @impl(quant_lib, "choose_qparams", "CompositeExplicitAutograd")
 def choose_qparams(
-    input: torch.Tensor,
-    qmin: int,
-    qmax: int,
-    eps: float,
-    dtype: torch.dtype
+    input: torch.Tensor, qmin: int, qmax: int, eps: float, dtype: torch.dtype
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    """ Given an input Tensor, derive the per tensor affine quantization parameter
+    """Given an input Tensor, derive the per tensor affine quantization parameter
     (scale and zero_point) for target quantized Tensor from the Tensor
 
     Args:
@@ -28,29 +29,38 @@ def choose_qparams(
        scale (float): quantization parameter for the target quantized Tensor
        zero_point (int): quantization parameter for the target quantized Tensor
     """
-    assert input.dtype == torch.float32, f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
-    assert dtype == torch.int8 or dtype == torch.uint8 or dtype == torch.int32, \
-        f"Expecting target dtype to be int8 uint8 or int32, but got: {dtype}"
+    assert (
+        input.dtype == torch.float32
+    ), f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
+    assert (
+        dtype == torch.int8 or dtype == torch.uint8 or dtype == torch.int32
+    ), f"Expecting target dtype to be int8 uint8 or int32, but got: {dtype}"
     validate_qmin_qmax(qmin, qmax)
 
     min_val, max_val = torch.aminmax(input)
 
     return determine_qparams(
-        min_val, max_val, qmin, qmax, dtype, torch.Tensor([eps]), has_customized_qrange=False)
+        min_val,
+        max_val,
+        qmin,
+        qmax,
+        dtype,
+        torch.Tensor([eps]),
+        has_customized_qrange=False,
+    )
+
 
 quant_lib.define(
     "choose_qparams_symmetric(Tensor input, int quant_min, int quant_max, "
-    "float eps, ScalarType dtype) -> (Tensor, Tensor)")
+    "float eps, ScalarType dtype) -> (Tensor, Tensor)"
+)
+
 
 @impl(quant_lib, "choose_qparams_symmetric", "CompositeExplicitAutograd")
 def choose_qparams_symmetric(
-    input: torch.Tensor,
-    qmin: int,
-    qmax: int,
-    eps: float,
-    dtype: torch.dtype
+    input: torch.Tensor, qmin: int, qmax: int, eps: float, dtype: torch.dtype
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    """ Given an input Tensor, derive the per tensor affine quantization parameter
+    """Given an input Tensor, derive the per tensor affine quantization parameter
     (scale and zero_point) for target quantized Tensor from the Tensor
 
     Args:
@@ -63,9 +73,12 @@ def choose_qparams_symmetric(
        scale (float): quantization parameter for the target quantized Tensor
        zero_point (int): quantization parameter for the target quantized Tensor
     """
-    assert input.dtype == torch.float32, f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
-    assert dtype == torch.int8 or dtype == torch.uint8 or dtype == torch.int32, \
-        f"Expecting target dtype to be int8 uint8 or int32, but got: {dtype}"
+    assert (
+        input.dtype == torch.float32
+    ), f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
+    assert (
+        dtype == torch.int8 or dtype == torch.uint8 or dtype == torch.int32
+    ), f"Expecting target dtype to be int8 uint8 or int32, but got: {dtype}"
     validate_qmin_qmax(qmin, qmax)
 
     min_val, max_val = torch.aminmax(input)
@@ -77,28 +90,30 @@ def choose_qparams_symmetric(
         dtype,
         torch.Tensor([eps]),
         has_customized_qrange=False,
-        qscheme=torch.per_tensor_symmetric
+        qscheme=torch.per_tensor_symmetric,
     )
+
 
 @impl(quant_lib, "choose_qparams", "Meta")
 def choose_qparams_meta(
-    input: torch.Tensor,
-    quant_min: int,
-    quant_max: int,
-    eps: float,
-    dtype: torch.dtype
+    input: torch.Tensor, quant_min: int, quant_max: int, eps: float, dtype: torch.dtype
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    assert input.dtype == torch.float32, f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
-    assert quant_min < quant_max, f"Expecting quant_min to be smaller than quant_max but received min: \
+    assert (
+        input.dtype == torch.float32
+    ), f"Expecting input to have dtype torch.float32, but got dtype: {input.dtype}"
+    assert (
+        quant_min < quant_max
+    ), f"Expecting quant_min to be smaller than quant_max but received min: \
         {quant_min} max: {quant_max}"
-    return torch.empty(1, dtype=torch.double, device=input.device), torch.empty(1, dtype=torch.int64, device=input.device)
+    return torch.empty(1, dtype=torch.double, device=input.device), torch.empty(
+        1, dtype=torch.int64, device=input.device
+    )
+
 
 @impl(quant_lib, "choose_qparams_symmetric", "Meta")
 def choose_qparams_symmetric_meta(
-    input: torch.Tensor,
-    quant_min: int,
-    quant_max: int,
-    eps: float,
-    dtype: torch.dtype
+    input: torch.Tensor, quant_min: int, quant_max: int, eps: float, dtype: torch.dtype
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    return torch.empty(1, dtype=torch.double, device=input.device), torch.empty(1, dtype=torch.int64, device=input.device)
+    return torch.empty(1, dtype=torch.double, device=input.device), torch.empty(
+        1, dtype=torch.int64, device=input.device
+    )
