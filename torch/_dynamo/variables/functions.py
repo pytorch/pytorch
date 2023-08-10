51afa1ab13d5ec0f069ad1b24328673437cb021a
Michael Voznesensky <voznesenskym@gmail.com>
2023-08-10 02:41:28 +0000

    [Dynamo x FSDP][5/x] Fix bug in __class__ sources
    ghstack-source-id: 828ec43504d249b4f51760d2245123c5a69cb7a6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106906

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -185,7 +185,7 @@ class UserFunctionVariable(BaseUserFunctionVariable):
             itertools.count(), self.fn.__code__.co_freevars, closure
         ):
             if name == "__class__":
-                source = AttrSource(self.source, "__class__") if self.source else None
+                source = AttrSource(self.source.base, "__class__") if self.source else None
                 result[name] = variables.UserDefinedClassVariable(
                     cell.cell_contents,
                     source=source,
