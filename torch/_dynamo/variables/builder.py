89488e27de1a667cf41bf1709a7fff33c1f63c11
Michael Voznesensky <voznesenskym@gmail.com>
2023-08-10 02:41:27 +0000

    [Dynamo x FSDP][1/x] Builder support for deque
    ghstack-source-id: 0d52cb324250e98c7d399300a938b4d43fc8f293
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106884
    
    Appendleft test

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -104,6 +104,7 @@ from .functions import (
 from .higher_order_ops import TorchHigherOrderOperatorVariable
 from .lists import (
     BaseListVariable,
+    DequeVariable,
     ListVariable,
     NamedTupleVariable,
     RangeVariable,
