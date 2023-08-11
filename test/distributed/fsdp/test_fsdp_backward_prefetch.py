c77817063f2e232ee962d1c26e869e6164676bf2
weifengpy <weif@meta.com>
2023-08-11 20:31:26 +0000

    [FSDP] verify backward_prefetch works correctly with unit test
    issue resolved: https://github.com/pytorch/pytorch/pull/105984
    
    context:
    * CI did not catch the commit that breaks backward_prefetch https://github.com/pytorch/pytorch/pull/105006
    * we had an action item to add unit test to prevent similar cases: https://github.com/pytorch/pytorch/pull/105984
    
    what's included in this unit test
    * monkey patch
    torch.distributed.fsdp._runtime_utils._get_handle_to_prefetch and get
    total function call and total non-None handle
    * BackwardPrefetch.BACKWARD_POST and BackwardPrefetch.BACKWARD_PRE:
    expect total non-None handle > 0
    * backward_prefetch = None: expect total function call = 0
    
    [ghstack-poisoned]

---
+++ test/distributed/fsdp/test_fsdp_backward_prefetch.py
@@ -0,0 +1,136 @@
+# Owner(s): ["oncall: distributed"]
+
+import sys
+import time
+from statistics import mean
+from unittest.mock import patch
+from typing import no_type_check
+
+import torch
+import torch.nn as nn
+from torch import distributed as dist
+from torch.cuda import Event
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
+from torch.testing._internal.common_fsdp import FSDPTest
+from torch.testing._internal.common_utils import (
+    run_tests,
+    TEST_WITH_DEV_DBG_ASAN,
+    
+)
+from torch.distributed.fsdp._runtime_utils import (
+    _get_handle_to_prefetch,
+    _get_training_state,
+)
+from torch.distributed.fsdp._common_utils import (
+    _FSDPState,
+)
+from torch.distributed.fsdp.wrap import ModuleWrapPolicy
+from torch.distributed.fsdp import (
+    BackwardPrefetch
+)
+from torch.distributed.fsdp.flat_param import (
+    HandleTrainingState,
+    FlatParamHandle,
+)
+
+NUM_ITERS = 1
+BACKWARD_PREFETCH_OPTIONS = [
+    None,
+    BackwardPrefetch.BACKWARD_PRE,
+    BackwardPrefetch.BACKWARD_POST,
+]
+
+
+if not dist.is_available():
+    print("Distributed not available, skipping tests", file=sys.stderr)
+    sys.exit(0)
+
+if TEST_WITH_DEV_DBG_ASAN:
+    print(
+        "Skip dev-asan as torch + multiprocessing spawn have known issues",
+        file=sys.stderr,
+    )
+    sys.exit(0)
+
+
+class TestBackwardPrefetch(FSDPTest):
+    @property
+    def world_size(self):
+        return 2
+
+    def _dist_train(self, backward_prefetch=BackwardPrefetch.BACKWARD_PRE):
+        rank = self.rank
+        orig_get_handle_to_prefetch = _get_handle_to_prefetch
+
+        torch.manual_seed(0)
+        policy = ModuleWrapPolicy({nn.TransformerEncoderLayer, nn.TransformerDecoderLayer})
+        model = FSDP(
+            nn.Transformer(d_model=1024, nhead=8, device="cuda"),
+            device_id=torch.cuda.current_device(),
+            auto_wrap_policy=policy,
+            use_orig_params=True,
+            backward_prefetch=backward_prefetch,
+        )
+        optim = torch.optim.SGD(model.parameters(), lr=1e-2)
+
+        # prepare input
+        torch.manual_seed(rank + 1)
+        src = torch.randn((10, 1, 1024), device="cuda")
+        tgt = torch.randn((20, 1, 1024), device="cuda")
+
+        # monkey patch 
+        none_handle_count = 0
+        func_call_count = 0
+        def patched_get_handle_to_prefetch(*args, **kwargs):
+            handle = orig_get_handle_to_prefetch(*args, **kwargs)
+
+            assert len(args) == 2, "expect _get_handle_to_prefetch(state, current_handle)"
+            state = args[0]
+            current_handle = args[1]
+            training_state = _get_training_state(current_handle)
+            if (
+                training_state == HandleTrainingState.BACKWARD_PRE
+                and state.backward_prefetch == BackwardPrefetch.BACKWARD_PRE
+            ) or (
+                training_state == HandleTrainingState.BACKWARD_POST
+                and state.backward_prefetch == BackwardPrefetch.BACKWARD_POST
+            ):
+                nonlocal none_handle_count
+                nonlocal func_call_count
+                if handle is not None:
+                    none_handle_count += 1
+                func_call_count += 1
+            return handle
+
+        # training Run one dummy iteration to trigger the execution order validation
+        # all-gathers
+        with patch(
+            "torch.distributed.fsdp._runtime_utils._get_handle_to_prefetch", patched_get_handle_to_prefetch
+        ):
+            for _ in range(NUM_ITERS):
+                optim.zero_grad()
+                loss = model(src, tgt).sum()
+                loss.backward()
+                optim.step()
+        if backward_prefetch is None:
+            self.assertTrue(func_call_count == 0, f"_get_handle_to_prefetch: {func_call_count}")
+        elif backward_prefetch in [BackwardPrefetch.BACKWARD_PRE, BackwardPrefetch.BACKWARD_POST]:
+            self.assertTrue(func_call_count > 0 and none_handle_count > 0, f"_get_handle_to_prefetch: {func_call_count} non-None handles: {none_handle_count}")
+        
+
+    @skip_if_lt_x_gpu(2)
+    def test_backward_prefetch_pre(self):
+        self._dist_train(BackwardPrefetch.BACKWARD_PRE)
+    
+    @skip_if_lt_x_gpu(2)
+    def test_backward_prefetch_post(self):
+        self._dist_train(BackwardPrefetch.BACKWARD_POST)
+    
+    @skip_if_lt_x_gpu(2)
+    def test_backward_prefetch_disabled(self):
+        self._dist_train(None)
+
+
+if __name__ == "__main__":
+    run_tests()
