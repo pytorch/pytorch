354484ea6d85ce2da90fd3e1e5fa9524987bcfe6
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-08-11 21:05:02 +0000

    Revert "Add `_foreach_clamp` (#106574)"
    This reverts commit 2b560d3c3a9b34cd11fc9ff9e3a0be6a81d47968.
    
    Reverted https://github.com/pytorch/pytorch/pull/106574 on behalf of https://github.com/kit1980 due to breaking internal windows builds ([comment](https://github.com/pytorch/pytorch/pull/106574#issuecomment-1675400335))

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -223,7 +223,7 @@ def _register_foreach_lowering(aten_fn, decomp_fn):
 
     @functools.wraps(decomp_fn)
     def wrapped(*args, **kwargs):
-        assert len(args) <= 3
+        assert len(args) <= 2
         out = decomp_fn(*args, **kwargs)
         validate_ir(out)
         return out
@@ -4428,8 +4428,8 @@ logical_xor = register_pointwise(
 )
 maximum = register_pointwise(aten.maximum)
 minimum = register_pointwise(aten.minimum)
-clamp_min = register_lowering(aten.clamp_min)(maximum)
-clamp_max = register_lowering(aten.clamp_max)(minimum)
+register_lowering(aten.clamp_min)(maximum)
+register_lowering(aten.clamp_max)(minimum)
 neg = register_pointwise(aten.neg)
 reciprocal = register_pointwise_numeric(aten.reciprocal)
 register_pointwise(aten.remainder)
@@ -4476,17 +4476,6 @@ register_foreach_pointwise(aten._foreach_maximum.List, maximum)
 register_foreach_pointwise(aten._foreach_maximum.Scalar, maximum)
 register_foreach_pointwise(aten._foreach_reciprocal, reciprocal)
 register_foreach_pointwise(aten._foreach_sign, sign)
-
-
-def _clamp_impl(v, min=None, max=None):
-    if min is not None:
-        v = clamp_min(v, min)
-    if max is not None:
-        v = clamp_max(v, max)
-    return v
-
-
-register_foreach_pointwise(aten._foreach_clamp, _clamp_impl)
 
 
 def register_inplace(aten_op, outplace_op):
