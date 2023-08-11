8d58bab62e58b6a9f8e67c0c3d6a751d74a7bc48
weifengpy <weif@meta.com>
2023-08-11 20:50:07 +0000

    Update on "[FSDP] verify backward_prefetch works correctly with unit test"
    issue resolved: https://github.com/pytorch/pytorch/pull/105984
    
    context:
    * CI did not catch the commit that breaks backward_prefetch https://github.com/pytorch/pytorch/pull/105006
    * we had an action item to add unit test to prevent similar cases: https://github.com/pytorch/pytorch/pull/105984
    
    what's included in this unit test
    * monkey patch
    torch.distributed.fsdp._runtime_utils._get_handle_to_prefetch and get
    total function call and total non-None handle
    * BackwardPrefetch.BACKWARD_POST and BackwardPrefetch.BACKWARD_PRE:
    expect total non-None handle > 0
    * backward_prefetch = None: expect total function call = 0
    
    [ghstack-poisoned]

--- test/distributed/fsdp/test_fsdp_backward_prefetch.py
+++ test/distributed/fsdp/test_fsdp_backward_prefetch.py
@@ -18,12 +18,6 @@ from torch.testing._internal.common_fsdp import FSDPTest
 from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
 
 NUM_ITERS = 1
-BACKWARD_PREFETCH_OPTIONS = [
-    None,
-    BackwardPrefetch.BACKWARD_PRE,
-    BackwardPrefetch.BACKWARD_POST,
-]
-
 
 if not dist.is_available():
     print("Distributed not available, skipping tests", file=sys.stderr)
