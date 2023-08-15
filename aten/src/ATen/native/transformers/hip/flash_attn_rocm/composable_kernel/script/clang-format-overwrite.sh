7ec674051b624e7776e272af60cb99dd344a23bc
Joseph Groenenboom <joseph.groenenboom@amd.com>
2023-08-15 17:40:26 +0000

    Header update and remove local CK.

--- aten/src/ATen/native/transformers/hip/flash_attn_rocm/composable_kernel/script/clang-format-overwrite.sh
+++
@@ -1,2 +0,0 @@
-#find . -name deps -prune -o -name build -prune -o -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' -o -iname '*.h.in' -o -iname '*.hpp.in' -o -iname '*.cpp.in' -o -iname '*.cl' -o -iname '*.cuh' -o -iname '*.cu' -o -iname '*.inc' | xargs -n 1 -P 16 -I{} -t sh -c 'clang-format-10 -i -style=file {}'
-git status --porcelain | awk '$1 != "D" && (match($2, "\\.cpp|hpp|inc")) {print $2}' | xargs -n 1 -P 16 -I{} -t sh -c 'clang-format-10 -i -style=file {}'
