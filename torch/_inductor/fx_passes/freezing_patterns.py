ef713f5302576a3216fab3164a4f58945830516c
XiaobingSuper <xiaobing.zhang@intel.com>
2023-08-14 20:48:26 -0400

    Update on "enable conv+bn folding for mixed-dtype when bn has post activation"
    For conv+bn+relu6, the joint-graph pass will remove one type of conversion and the graph will be like this:
    ```
     def forward(self, arg0_1: bf16[32, 3, 3, 3], arg1_1: bf16[32], arg2_1: bf16[32], ...)
         convolution: bf16[3, 32, 15, 15] = aten..convolution.default(arg6_1, arg0_1, None, [2, 2], [0, 0], [1, 1], False, [0, 0], 1);  arg6_1 = arg0_1 = None
         # weight upcasting
         convert_element_type: f32[32] = torch.ops.prims.convert_element_type.default(arg3_1, torch.float32);  arg3_1 = None
         convert_element_type_1: f32[32] = torch.ops.prims.convert_element_type.default(arg4_1, torch.float32);  arg4_1 = None
         ...
         # end of batch norm
         add_1: f32[3, 32, 15, 15] = aten..add.Tensor(mul_2, unsqueeze_7);  mul_2 = unsqueeze_7 = None
         # output downcast
         convert_element_type_2: bf16[3, 32, 15, 15] = torch.ops.prims.convert_element_type.default(add_1, torch.float32);  add_1 = None
         clamp_min: f32[3, 32, 15, 15] = torch.ops.aten.clamp_min.default(convert_element_type_2, 0.0); convert_element_type_2 = None
         clamp_max: f32[3, 32, 15, 15] = torch.ops.aten.clamp_max.default(clamp_min, 6.0);  clamp_min = None
         convert_element_type_3: bf16[3, 32, 15, 15] = torch.ops.prims.convert_element_type.default(clamp_max, torch.bfloat16); clamp_max = None
    ```
    
    the conv+bn folding will be failed, this PR will move the joint-graph pass's dtype conversion removing to after of conv_bn folding pass.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy ngimel yf225 chenyang78 kadeng muchulee8 aakhundov
    
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/freezing_patterns.py
+++ torch/_inductor/fx_passes/freezing_patterns.py
@@ -170,32 +170,6 @@ def addmm_patterns_init():
     )
 
 
-@register_graph_pattern(
-    CallFunction(
-        torch.ops.prims.convert_element_type.default,
-        CallFunction(
-            torch.ops.prims.convert_element_type.default,
-            KeywordArg("arg"),
-            KeywordArg("dtype1"),
-        ),
-        KeywordArg("dtype2"),
-    ),
-    pass_dict=pass_patterns[0],
-)
-def pointless_convert(match: Match, arg, dtype1, dtype2):
-    """Remove chain of dtype conversions often created by AMP"""
-    graph = match.graph
-    node = match.output_node()
-    allowed = {torch.float16, torch.bfloat16, torch.float32, torch.float64}
-    if dtype1 in allowed and dtype2 in allowed:
-        repl = graph.call_function(
-            torch.ops.prims.convert_element_type.default, (arg, dtype2)
-        )
-        repl.meta.update(node.meta)
-        node.replace_all_uses_with(repl)
-        match.erase_nodes(graph)
-
-
 def same_dtype(match):
     return match.output_node().args[0].meta["val"].dtype == match.kwargs["dtype"]
 
