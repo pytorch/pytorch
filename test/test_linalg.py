f08429207bb25b1e216adfcf1100409898c5c15d
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-08-15 16:22:36 -0700

    refactor test__int_mm

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -5638,15 +5638,14 @@ scipy_lobpcg  | {eq_err_scipy:10.2e}  | {eq_err_general_scipy:10.2e}  | {iters2:
 
     @unittest.skipIf(IS_WINDOWS, "Skipped on Windows!")
     @unittest.skipIf(IS_FBCODE and IS_REMOTE_GPU, "cublas runtime error")
+    @unittest.skipIf(_get_torch_cuda_version() < (11, 7), "_int_mm only compiled for CUDA 11.7")
+    @unittest.skipIf(TEST_WITH_ROCM, "_int_mm not compiled for ROCM")
     @onlyCUDA
     @parametrize("k", [16, 32])
     @parametrize("n", [16, 32])
     @parametrize("use_transpose_a", [True, False])
     @parametrize("use_transpose_b", [True, False])
     def test__int_mm(self, device, k, n, use_transpose_a, use_transpose_b):
-        if TEST_WITH_ROCM:
-            self.skipTest("_int_mm not compiled for ROCM")
-
         def genf_int_float(x, y, use_transpose):
             if use_transpose:
                 x, y = y, x
@@ -5674,50 +5673,15 @@ scipy_lobpcg  | {eq_err_scipy:10.2e}  | {eq_err_general_scipy:10.2e}  | {iters2:
             else:
                 self.assertNotEqual(c_int32_result.float(), torch.mm(a_float, b_float))
 
-        # NOTE: We're just exercising terrible failures here.
-        version = _get_torch_cuda_version()
-        SM80OrLater = torch.cuda.is_available() and torch.cuda.get_device_capability() >= (8, 0)
-        if version >= (11, 7):
-            if not use_transpose_a and use_transpose_b:
-                if SM80OrLater:
-                    _test(17, k, n, use_transpose_a, use_transpose_b, version > (11, 7))
-                else:
-                    with self.assertRaisesRegex(RuntimeError,
-                                                "CUDA error: CUBLAS_STATUS_NOT_SUPPORTED when calling cublasLtMatmul"):
-                        _test(17, k, n, use_transpose_a, use_transpose_b)
-
-            if use_transpose_a and not use_transpose_b:
-                with self.assertRaisesRegex(RuntimeError,
-                                            "CUDA error: CUBLAS_STATUS_NOT_SUPPORTED when calling cublasLtMatmul"):
-                    _test(17, k, n, use_transpose_a, use_transpose_b)
-
-            if use_transpose_a and use_transpose_b:
-                with self.assertRaisesRegex(RuntimeError,
-                                            "CUDA error: CUBLAS_STATUS_NOT_SUPPORTED when calling cublasLtMatmul"):
-                    _test(17, k, n, use_transpose_a, use_transpose_b)
-
-            if not use_transpose_a and not use_transpose_b:
-                if SM80OrLater:
-                    _test(17, k, n, use_transpose_a, use_transpose_b)
-                else:
-                    with self.assertRaisesRegex(RuntimeError,
-                                                "CUDA error: CUBLAS_STATUS_NOT_SUPPORTED when calling cublasLtMatmul"):
-                        _test(17, k, n, use_transpose_a, use_transpose_b)
-        else:
-            with self.assertRaisesRegex(RuntimeError, "_int_mm_out_cuda not compiled for CUDA"):
-                _test(17, k, n, use_transpose_a, use_transpose_b, False)
+        for use_transpose_a, use_transpose_b in itertools.product([True, False], [True, False]):
+            _test(17, k, n, use_transpose_a, use_transpose_b, version > (11, 7))
 
     @unittest.skipIf(IS_WINDOWS, "Skipped on Windows!")
     @unittest.skipIf(IS_FBCODE and IS_REMOTE_GPU, "cublas runtime error")
+    @unittest.skipIf(_get_torch_cuda_version() < (11, 7), "_int_mm only compiled for CUDA 11.7")
+    @unittest.skipIf(TEST_WITH_ROCM, "_int_mm not compiled for ROCM")
     @onlyCUDA
     def test__int_mm_errors(self, device):
-        if TEST_WITH_ROCM:
-            self.skipTest("_int_mm not compiled for ROCM")
-
-        version = _get_torch_cuda_version()
-        if version < (11, 7):
-            self.skipTest("_int_mm only compiled for CUDA 11.7")
-
         def genf_int(x, y):
             return torch.empty((x, y), dtype=torch.int8, device=device)
 
