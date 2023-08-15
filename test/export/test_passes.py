7b9f86d8978ce3045f315ede2e8786664e1b8546
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-08-15 20:47:35 +0000

    Revert "ExportedProgram.transform now updates graph_signature automatically (#107080)"
    This reverts commit 8c9b2fe8f097cd4b32000e5124232a0047d92234.
    
    Reverted https://github.com/pytorch/pytorch/pull/107080 on behalf of https://github.com/izaitsevfb due to Breaks executorch tests, see D48333170 ([comment](https://github.com/pytorch/pytorch/pull/107080#issuecomment-1679588292))

--- test/export/test_passes.py
+++ test/export/test_passes.py
@@ -357,10 +357,7 @@ class TestPasses(TestCase):
             exactly=True,
         ).run(gm.code)
 
-        # TODO(ycao): ExportedProgram.transform() forbids changes to number
-        # of inputs/outputs for now. When it supports that better, change this
-        # back to using ExportedProgram.transform()
-        gm = _FunctionalizeSideEffectfulOpsPass()(ep.graph_module).graph_module
+        gm = ep.transform(_FunctionalizeSideEffectfulOpsPass()).graph_module
 
         with self.assertRaisesRegex(
             RuntimeError,
