27eb49abd21cc33505105cb82f383dcf52fc5ac5
atalman <atalman@fb.com>
2023-08-16 19:05:23 -0700

    test

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -87,7 +87,7 @@ _UCC_COMMIT=7cb07a76ccedad7e56ceb136b865eb9319c258ea
 # from scratch
 case "$image" in
   pytorch-linux-focal-cuda12.1-cudnn8-py3-gcc9)
-    CUDA_VERSION=12.1.0
+    CUDA_VERSION=12.1.1
     CUDNN_VERSION=8
     ANACONDA_PYTHON_VERSION=3.10
     GCC_VERSION=9
