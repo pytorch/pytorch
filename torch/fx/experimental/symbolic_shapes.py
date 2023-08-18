7181018e2f98783636acdf7aa6036b936ca1d30f
Edward Z. Yang <ezyang@meta.com>
2023-08-18 11:07:30 -0700

    Update base for Update on "Use fast traceback for symbolic shapes"
    [ghstack-poisoned]

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -11,7 +11,6 @@ import sys
 import textwrap
 import threading
 import traceback
-import weakref
 from collections import defaultdict
 from contextlib import contextmanager
 from dataclasses import dataclass, field
@@ -706,27 +705,9 @@ class SymNode:
     This is a type erased SymInt/SymFloat which we use to do actual operations.
     End users don't touch this.  Magic methods are NOT defined on this object.
     """
-
-    @property
-    def shape_env(self):
-        r = self._shape_env()
-        assert r is not None, "you must keep ShapeEnv live to continue using SymNode"
-        return r
-
-    @property
-    def fx_node(self):
-        if self._fx_node is None:
-            return None
-        r = self._fx_node()
-        assert r is not None, "you must keep ShapeEnv live to continue using SymNode"
-        return r
-
     def __init__(self, expr, shape_env, pytype, hint: Optional[Union[int, float]], constant=None, fx_node=None):
-        # WARNING: SymNode never participates in garbage collection, so if you have
-        # a reference cycle that goes through SymNode it will NEVER get GC'ed.  This
-        # motivates some of the uses of weakref inside this class.
         self._expr = expr
-        self._shape_env = weakref.ref(shape_env)
+        self.shape_env = shape_env
         self.pytype = pytype
         # What's the difference between hint and constant?
         #
@@ -759,7 +740,7 @@ class SymNode:
         # Record the FX node of the current node if we are doing translation
         # validation. They will be used for building the input assertions for
         # the translation validation problem.
-        self._fx_node = weakref.ref(fx_node) if _translation_validation_enabled() else None
+        self.fx_node = fx_node if _translation_validation_enabled() else None
 
     @property
     def expr(self):
