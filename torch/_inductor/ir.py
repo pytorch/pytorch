c21e9de25d894e2a2ac055ba33fdd382beec5b69
chunyuan <chunyuan.wu@intel.com>
2023-08-18 01:12:26 +0000

    Inductor cpp wrapper: fix optional tensor input (#106847)
    Fix cpp wrapper failure on `clip` in Torchbench:
    ```
    RuntimeError: tensor does not have a device
    ```
    
    An `optional<at::Tensor>` variable with value equal to `at::Tensor()` will be considered as _contains value_. When it's converted to `bool`, it returns `true`. While for `None` in python, when converting it to `bool`, `false` is returned.
    Fix it to be an optional variable that _does not contain a value_.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106847
    Approved by: https://github.com/jgong5, https://github.com/jansel

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -236,6 +236,11 @@ class OptionalLayout(OptionalAttr):
         self.name = "optional_layout"
 
 
+class OptionalTensor(OptionalAttr):
+    def __init__(self):
+        self.name = "optional_tensor"
+
+
 default_value_map = {"Optional[Layout]": OptionalLayout}
 
 
@@ -3155,7 +3160,7 @@ class ExternKernel(InputsKernel):
         pass
 
     def codegen_const_args(self):
-        return map(V.graph.wrapper_code.val_to_str, self.constant_args)
+        return map(V.graph.wrapper_code.val_to_arg_str, self.constant_args)
 
     def codegen_args(self):
         args = []
@@ -3200,10 +3205,10 @@ class ExternKernel(InputsKernel):
                 ), "ordered_kwargs_for_cpp_kernel has to be provided"
                 for arg_name in self.ordered_kwargs_for_cpp_kernel:
                     v = self.get_kwargs_value(arg_name)
-                    kwargs.append(V.graph.wrapper_code.val_to_str(v))
+                    kwargs.append(V.graph.wrapper_code.val_to_arg_str(v))
             else:
                 kwargs = [
-                    f"{k}={V.graph.wrapper_code.val_to_str(v)}"
+                    f"{k}={V.graph.wrapper_code.val_to_arg_str(v)}"
                     for k, v in self.kwargs.items()
                 ]
         return kwargs
@@ -3661,7 +3666,7 @@ class FallbackKernel(ExternKernelAlloc):
 
         tensor_args = [Shim(x.codegen_reference()) for x in self.inputs]
         args, kwargs = self.unflatten_args(tensor_args, self.constant_args)
-        args = [V.graph.wrapper_code.val_to_str(x) for x in args]
+        args = [V.graph.wrapper_code.val_to_arg_str(x) for x in args]
         # let self.codegen_kwargs handle kwargs
         self.kwargs.update(kwargs)
         return args
