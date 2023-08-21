5c9b6e87207b9f08c489f75181de117c91ab3cb3
CaoE <e.cao@intel.com>
2023-08-21 06:29:18 -0700

    optimize scalar convertion for Half on CPU

--- c10/util/Half-inl.h
+++ c10/util/Half-inl.h
@@ -20,8 +20,7 @@
 #include <sycl/sycl.hpp> // for SYCL 2020
 #endif
 
-#if (defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)) && \
-    !defined(__APPLE__)
+#if defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)
 #include <ATen/cpu/vec/vec_half.h>
 #endif
 
@@ -41,8 +40,7 @@ inline C10_HOST_DEVICE Half::Half(float value)
 #elif defined(__SYCL_DEVICE_ONLY__)
       x(c10::bit_cast<uint16_t>(sycl::half(value)))
 #else
-#if (defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)) && \
-    !defined(__APPLE__)
+#if defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)
       x(at::vec::float2half_scalar(value))
 #else
       x(detail::fp16_ieee_from_fp32_value(value))
@@ -59,8 +57,7 @@ inline C10_HOST_DEVICE Half::operator float() const {
 #elif defined(__SYCL_DEVICE_ONLY__)
   return float(c10::bit_cast<sycl::half>(x));
 #else
-#if (defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)) && \
-    !defined(__APPLE__)
+#if defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_AVX512)
   return at::vec::half2float_scalar(x);
 #else
   return detail::fp16_ieee_to_fp32_value(x);
