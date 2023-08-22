62f793cf9c68015c45fa9946c762c5a216fb4541
Han Qi <hanq@google.com>
2023-08-22 16:21:56 +0000

    Store the arguments used to trace the exported program in itself.

--- torch/export/__init__.py
+++ torch/export/__init__.py
@@ -234,6 +234,7 @@ class ExportedProgram:
         range_constraints: Dict[sympy.Symbol, Any],
         equality_constraints: List[Tuple[Any, Any]],
         module_call_graph: List[ModuleCallEntry],
+        original_traced_arguments: Tuple[Any, ...] = None,
     ):
         from torch._export.exported_program import (
             _create_graph_module_for_export,
@@ -258,6 +259,7 @@ class ExportedProgram:
             Tuple[InputDim, InputDim]
         ] = equality_constraints
         self._module_call_graph: List[ModuleCallEntry] = module_call_graph
+        self._original_traced_arguments = original_traced_arguments
 
     @property
     @compatibility(is_backward_compatible=False)
@@ -298,6 +300,11 @@ class ExportedProgram:
     @compatibility(is_backward_compatible=False)
     def module_call_graph(self):
         return self._module_call_graph
+
+    @property
+    @compatibility(is_backward_compatible=False)
+    def original_traced_arguments(self):
+        return self._original_traced_arguments
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
         import torch._export.error as error
@@ -528,6 +535,7 @@ class ExportedProgram:
             _get_updated_range_constraints(transformed_gm),
             copy.deepcopy(self.equality_constraints),
             copy.deepcopy(self._module_call_graph),
+            self.original_traced_arguments,
         )
         transformed_ep.graph_module.meta.update(self.graph_module.meta)
         transformed_ep.graph_module.meta.update(res.graph_module.meta)
