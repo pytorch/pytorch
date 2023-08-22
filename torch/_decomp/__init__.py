37e047f02443ad20f8c1d8f79a03d76e4bb4c3d2
Salil Desai <salilsdesai@meta.com>
2023-08-22 12:13:17 -0700

    [Decomposition] Baddbmm
    Summary:
    The decomposition already exists, so we just need to add it to the core list
    
    https://www.internalfb.com/code/fbsource/[c901e3ccef06e5b5b2df3c77ac0af5932c91a00f]/xplat/caffe2/torch/_inductor/decomposition.py?lines=175
    
    Test Plan: contbuild and OSS CI
    
    Differential Revision: D48574057
    
    fbshipit-source-id: e089e7897160e8d4f72d269d43189510342140de

--- torch/_decomp/__init__.py
+++ torch/_decomp/__init__.py
@@ -191,6 +191,7 @@ def core_aten_decompositions() -> Dict[OpOverload, Callable]:
             aten.arange.default,
             aten.arange.start,
             aten.avg_pool2d_backward,
+            aten.baddbmm,
             aten.binary_cross_entropy,
             aten.binary_cross_entropy_backward,
             aten.binary_cross_entropy_with_logits,
