64ee535bd478c72b10a158e2bb70faccc25d2eca
Han, Xu <xu.han@intel.com>
2023-08-22 12:13:16 +0000

    update code.

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1344,6 +1344,9 @@ def _get_glibcxx_abi_build_flags():
     return glibcxx_abi_cflags
 
 def check_compiler_is_gcc(compiler):
+    if not IS_LINUX:
+        return False
+
     env = os.environ.copy()
     env['LC_ALL'] = 'C'  # Don't localize output
     version_string = subprocess.check_output([compiler, '-v'], stderr=subprocess.STDOUT, env=env).decode(*SUBPROCESS_DECODE_ARGS)
