ca985365239b66b79516d69f6e85ce59ded27d0f
Nikita Shulga <nikita.shulga@gmail.com>
2023-08-23 08:35:38 +0900

    [MPS] Fix `torch.std` for negative dimentions
    By simply comparing output dimentions to a properly wrapped dim
    Add regression test to opinfo
    
    copilot:poem
    
    Fixes https://github.com/pytorch/pytorch/issues/107116

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -5471,6 +5471,7 @@ def sample_inputs_std_var(op_info, device, dtype, requires_grad, **kwargs):
     yield SampleInput(tensor_nd(), dim=None, correction=0, keepdim=True)
     yield SampleInput(tensor_nd(), dim=None, correction=None)
     yield SampleInput(tensor_nd(), correction=0, keepdim=True)
+    yield SampleInput(make_tensor(3, 4, 5, device=device, dtype=dtype), dim=-3)
 
 
 def sample_inputs_std_var_unbiased(op_info, device, dtype, requires_grad, **kwargs):
