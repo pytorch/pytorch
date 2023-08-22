206bc2d45ee18b242bf20ab6a95e755bf26d8c42
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2023-08-22 11:01:39 +0100

    chmod setup_rocm_libs file

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -139,6 +139,9 @@ def build_triton(
 
         if build_rocm:
             print(f"rocm_version:\t{rocm_version}")
+            check_call(
+                [sys.executable, "chmod", "-x", "scripts/amd/setup_rocm_libs.sh"], cwd=triton_basedir, shell=True
+            )
             check_call("scripts/amd/setup_rocm_libs.sh", cwd=triton_basedir, shell=True)
 
         check_call(
