2d2df7b73b4755f9028724c7e28483ad7f0b3694
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2023-08-22 14:09:52 +0100

    Chmod without shell=True

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -139,9 +139,9 @@ def build_triton(
 
         if build_rocm:
             print(f"rocm_version:\t{rocm_version}")
-            #check_call(
-            #    [sys.executable, "chmod", "+x", "scripts/amd/setup_rocm_libs.sh"], cwd=triton_basedir, shell=True
-            #)
+            check_call(
+                [sys.executable, "chmod", "+x", "scripts/amd/setup_rocm_libs.sh"], cwd=triton_basedir
+            )
             check_call("scripts/amd/setup_rocm_libs.sh", cwd=triton_basedir, shell=True)
             print(f"ROCm libraries setup for triton installation...")
 
