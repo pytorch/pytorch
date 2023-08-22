db4ee2deae7b253c7a15e5ea26197f6bee793040
George White <georgew@graphcore.ai>
2023-08-22 11:27:17 +0100

    Add support to `fill_cuda`

--- aten/src/ATen/native/cuda/FillKernel.cu
+++ aten/src/ATen/native/cuda/FillKernel.cu
@@ -1,27 +1,37 @@
 #define TORCH_ASSERT_NO_OPERATORS
 #include <ATen/Dispatch.h>
-#include <ATen/native/cuda/Loops.cuh>
 #include <ATen/native/DispatchStub.h>
-#include <ATen/native/TensorIterator.h>
 #include <ATen/native/Fill.h>
+#include <ATen/native/TensorIterator.h>
 #include <c10/core/Scalar.h>
+#include <ATen/native/cuda/Loops.cuh>
 
 namespace at::native {
 
-template<typename scalar_t>
+template <typename scalar_t>
 struct FillFunctor {
-  FillFunctor(scalar_t v): value(v) {}
-  __device__ __forceinline__ scalar_t operator() () const {
+  FillFunctor(scalar_t v) : value(v) {}
+  __device__ __forceinline__ scalar_t operator()() const {
     return value;
   }
-  private:
-    scalar_t value;
+
+ private:
+  scalar_t value;
 };
 
 void fill_kernel_cuda(TensorIterator& iter, const Scalar& value) {
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND6(kComplexHalf, kBool, kHalf, kBFloat16, kFloat8_e4m3fn, kFloat8_e5m2, iter.dtype(), "fill_cuda", [&]() {
-    gpu_kernel(iter, FillFunctor<scalar_t>(value.to<scalar_t>()));
-  });
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND8(
+      kComplexHalf,
+      kBool,
+      kHalf,
+      kBFloat16,
+      kFloat8_e4m3fn,
+      kFloat8_e4m3fnuz,
+      kFloat8_e5m2,
+      kFloat8_e5m2fnuz,
+      iter.dtype(),
+      "fill_cuda",
+      [&]() { gpu_kernel(iter, FillFunctor<scalar_t>(value.to<scalar_t>())); });
 }
 
 REGISTER_DISPATCH(fill_stub, &fill_kernel_cuda);
