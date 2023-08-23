58b6aa6337352b2108f9c642e2c34c241be9240c
Oren Leung <oren.leung@gmail.com>
2023-08-23 14:28:26 -0400

    fix: add reflectionpad 3d cuda

--- aten/src/ATen/native/cuda/ReflectionPad.cu
+++ aten/src/ATen/native/cuda/ReflectionPad.cu
@@ -585,7 +585,7 @@ TORCH_IMPL_FUNC(reflection_pad3d_out_cuda) (
   auto input = input_.contiguous();
   bool batch_mode = (input.dim() == 5);
 
-  AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16,
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND2(kHalf, kBFloat16,
       input.scalar_type(), "reflection_pad3d_out_cuda", [&] {
         auto input_inner = input;
         auto output_inner = output;
