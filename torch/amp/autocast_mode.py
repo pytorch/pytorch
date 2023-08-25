4a8acbcd8e2c9d4227c4e1ce6459a8fa2941c64f
Hengwen Tong <kevint324@gmail.com>
2023-08-25 18:41:06 +0800

    add torch.float16 support for xla amp
    The "bfloat16 only" limitation is only applied to google TPU but not other backends like GPU and other accelerators

--- torch/amp/autocast_mode.py
+++ torch/amp/autocast_mode.py
@@ -306,11 +306,11 @@ class autocast:
                     "Current CUDA Device does not support bfloat16. Please switch dtype to float16."
                 )
         elif self.device == "xla":
-            supported_dtype = [torch.bfloat16]
+            supported_dtype = [torch.bfloat16, torch.float16]]
             if self.fast_dtype not in supported_dtype:
                 error_message = "In XLA autocast, but the target dtype is not supported. Disabling autocast.\n"
                 error_message += (
-                    "XLA Autocast only supports dtype of torch.bfloat16 currently."
+                    "XLA Autocast only supports dtype of torch.bfloat16 and torch.float16 currently."
                 )
                 warnings.warn(error_message)
                 enabled = False
