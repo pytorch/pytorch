42e8de58423e4724eaae393d56e5082c6315d186
leslie-fang-intel <leslie.fang@intel.com>
2023-08-25 20:54:15 +0800

    [Quant][Inductor] Enable quantization conv pattern fusion inside inductor (#104588)
    **Summary**
    Enable the `dequant-quantization-quant` pattern fusion and lowering inside inductor.
    
    **Test Plan**
    ```
    python -m pytest test_mkldnn_pattern_matcher.py -k test_qconv2d_unary
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104588
    Approved by: https://github.com/jgong5, https://github.com/eellison
    ghstack dependencies: #104580, #104581

--- test/inductor/test_mkldnn_pattern_matcher.py
+++ test/inductor/test_mkldnn_pattern_matcher.py
@@ -408,18 +408,21 @@ class TestPatternMatcher(TestPatternMatcherBase):
             v = torch.randn((1, 3, 8, 8), dtype=torch.float32, requires_grad=False).add(
                 1
             )
-            # Totally 3 pattern_matcher_count, 10 pattern_matcher_nodes
+
+            # Totally 4 pattern_matcher_count, 17 pattern_matcher_nodes
             # 1. pair of to_int8 and to_fp32 at conv input matched in pointless_convert pass
             #    at torch/_inductor/fx_passes/joint_graph.py: [convert_element_type, convert_element_type_1]
             # 2. dequant-conv pattern matched in quantization weight prepack
             #    [convert_element_type_1, sub, mul_1, dequantize_per_channel, clone, convolution]
             # 3. pair of to_int8 and to_fp32 at conv output matched in pointless_convert pass
             #    at torch/_inductor/fx_passes/joint_graph.py: [convert_element_type_2, convert_element_type_3]
+            # 4. Quantization fusion in post-grad fusion pass
+            #    [qconv2d_pointwise_default, div_1, round_2, add_1, clamp_min_1, clamp_max_1, convert_element_type_2]
             self._test_common(
                 mod,
                 (v,),
-                3,
-                10,
+                4,
+                17,
                 check_quantization=True,
             )
 
