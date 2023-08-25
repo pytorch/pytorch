22debd643c58d0a6ad173e495999471600dcf834
ydwu4 <ydwu2014@gmail.com>
2023-08-24 23:15:42 -0700

    Update on "Automatically run dynamo in torch.cond"
    The key modification is:
    1.  moving cond from functorch/experimental/ to torch/_higher_order
    2. always turn on dynamo in cond_compiled
    3. change test behaviors after turning on torch.compile  for some eager run tests.
    4. expected fail a few tests due to SymBool. Current plan is to wait for https://github.com/pytorch/pytorch/pull/107850 and forward fix.
    
    cc voznesenskym penguinwu anijain2305 EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx chenyang78 aakhundov 
    
    [ghstack-poisoned]

--- torch/_higher_order_ops/cond.py
+++ torch/_higher_order_ops/cond.py
@@ -36,12 +36,15 @@ def _set_compilation_env():
     _old_capture_dyn_ops = torch._dynamo.config.capture_dynamic_output_shape_ops
     _old_capture_scalar = torch._dynamo.config.capture_scalar_outputs
     _old_size_limit = torch._dynamo.config.cache_size_limit
+    _old_acc_size_limit = torch._dynamo.config.accumulated_cache_size_limit
 
     try:
+        # We need to turn off the is_fx_tracing_flag temporarily
+        # Remove it once we are confident fx tracing works with dynamo
         torch.fx._symbolic_trace._is_fx_tracing_flag = False
         torch._dynamo.config.capture_dynamic_output_shape_ops = True
         torch._dynamo.config.capture_scalar_outputs = True
-        # We need to enlarge the cache size limit because
+        # We need to enlarge the dynamo cache size limit because
         # cond's frame is expected to be hitted with different inputs frequently
         torch._dynamo.config.cache_size_limit = 256
         torch._dynamo.config.accumulated_cache_size_limit = 256
@@ -51,6 +54,7 @@ def _set_compilation_env():
         torch._dynamo.config.capture_dynamic_output_shape_ops = _old_capture_dyn_ops
         torch._dynamo.config.capture_scalar_outputs = _old_capture_scalar
         torch._dynamo.config.cache_size_limit = _old_size_limit
+        torch._dynamo.config.accumulated_cache_size_limit = _old_acc_size_limit
 
 
 @dataclass
