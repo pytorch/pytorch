bb9378d45fc1ccc15c12a09efa6c9baaba240d61
Andrew Beveridge <andrew.beveridge@transferwise.com>
2023-08-26 15:28:13 -0500

    Refactored FourierTransform.mm to remove dupe code and clean up significantly

--- aten/src/ATen/native/mps/operations/FourierTransform.mm
+++ aten/src/ATen/native/mps/operations/FourierTransform.mm
@@ -1,4 +1,3 @@
-// Copyright © 2023 Apple Inc.
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/native/mps/OperationUtils.h>
 #include <MetalPerformanceShadersGraph/MPSGraphFourierTransformOps.h>
@@ -7,38 +6,44 @@
 
 namespace at::native {
 
-// Real-to-Hermitean FFT
-Tensor _fft_r2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
+enum class FFTType {
+    R2C,
+    C2R,
+    C2C
+};
+
+MPSGraphTensor* createFFTGraph(MPSGraph* graph, const Tensor& tensor, int64_t signal_ndim, bool normalized, FFTType fftType, bool forward) {
+    MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(graph, getMPSDataType(tensor), getMPSShape(tensor));
+    
+    MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
+    descriptor.inverse = (fftType == FFTType::C2C) ? !forward : (fftType == FFTType::C2R);
+    descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
+    
+    switch (fftType) {
+        case FFTType::R2C:
+            return [graph realToHermiteanFFTWithTensor:inputTensor axes:@[@(signal_ndim)] descriptor:descriptor name:nil];
+        case FFTType::C2R:
+            return [graph HermiteanToRealFFTWithTensor:inputTensor axes:@[@(signal_ndim)] descriptor:descriptor name:nil];
+        case FFTType::C2C:
+            return [graph fastFourierTransformWithTensor:inputTensor axes:@[@(signal_ndim)] descriptor:descriptor name:nil];
+    }
+}
+
+Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, const std::string& key_prefix, FFTType fftType, int64_t signal_ndim, bool normalized, bool forward) {
     using namespace mps;
     
-    TORCH_CHECK(self.is_mps(), "_fft_r2c_mps: Expected MPS tensor");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
+    TORCH_CHECK(self.is_mps(), key_prefix + ": Expected MPS tensor");
+    if (out_provided) {
+        TORCH_CHECK(out.is_mps(), key_prefix + ": Expected MPS tensor for output");
+    }
     
     MPSStream* stream = getCurrentMPSStream();
     
     @autoreleasepool {
-        string key = "fft_r2c_" + getTensorsStringKey({self}) + getMPSTypeString(self);
+        string key = key_prefix + getTensorsStringKey({self}) + getMPSTypeString(self);
         
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = NO;
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the realToHermiteanFFTWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph realToHermiteanFFTWithTensor:inputTensor
-                                                                             axes:@[@(signal_ndim)]
-                                                                       descriptor:descriptor
-                                                                             name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
+        auto cachedGraph = LookUpOrCreateCachedGraph<MPSCachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
+            newCachedGraph->outputTensor_ = createFFTGraph(mpsGraph, self, signal_ndim, normalized, fftType, forward);
         });
         
         Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
@@ -46,267 +51,51 @@ Tensor _fft_r2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, In
         NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
         [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
         
-        Tensor output = at::empty(signal_sizes, self.options());
-        Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, output);
         NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        [results setObject:outputPlaceholder.getMPSGraphTensorData() forKey:outputPlaceholder.getMPSGraphTensor()];
         
         runMPSGraph(stream, cachedGraph->graph(), feeds, results);
         
+        MPSGraphTensorData* outputData = [results objectForKey:cachedGraph->outputTensor_];
+        Tensor output = out_provided ? out : createTensorFromMPSGraphTensorData(outputData);
+        
+        if (out_provided) {
+            out.copy_(output);
+        }
+        
         return output;
     }
+}
+
+// Real-to-Hermitean FFT
+Tensor _fft_r2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
+    return runFFTGraph(self, {}, false, "r2c", FFTType::R2C, signal_ndim, normalized, true);
 }
 
 // Real-to-Hermitean FFT, writing result to the provided output tensor
 Tensor& _fft_r2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    using namespace mps;
-    
-    TORCH_CHECK(self.is_mps(), "_fft_r2c_mps_out: Expected MPS tensor");
-    TORCH_CHECK(out.is_mps(), "_fft_r2c_mps_out: Expected MPS tensor for output");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
-    
-    MPSStream* stream = getCurrentMPSStream();
-    
-    @autoreleasepool {
-        string key = "fft_r2c_out_" + getTensorsStringKey({self, out}) + getMPSTypeString(self);
-        
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = NO;
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the realToHermiteanFFTWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph realToHermiteanFFTWithTensor:inputTensor
-                                                                             axes:@[@(signal_ndim)]
-                                                                       descriptor:descriptor
-                                                                             name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
-        });
-        
-        Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
-        [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
-        
-        Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, out);
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        [results setObject:outputPlaceholder.getMPSGraphTensorData() forKey:outputPlaceholder.getMPSGraphTensor()];
-        
-        runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-        
-        return out;
-    }
+    runFFTGraph(self, out, true, "r2c_out", FFTType::R2C, signal_ndim, normalized, true);
+    return out;
 }
 
 // Hermitean-to-Real FFT
 Tensor _fft_c2r_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    using namespace mps;
-    
-    TORCH_CHECK(self.is_mps(), "_fft_c2r_mps: Expected MPS tensor");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
-    
-    MPSStream* stream = getCurrentMPSStream();
-    
-    @autoreleasepool {
-        string key = "fft_c2r_" + getTensorsStringKey({self}) + getMPSTypeString(self);
-        
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = YES;
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the HermiteanToRealFFTWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph HermiteanToRealFFTWithTensor:inputTensor
-                                                                             axes:@[@(signal_ndim)]
-                                                                       descriptor:descriptor
-                                                                             name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
-        });
-        
-        Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
-        [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
-        
-        Tensor output = at::empty(signal_sizes, self.options());
-        Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, output);
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        [results setObject:outputPlaceholder.getMPSGraphTensorData() forKey:outputPlaceholder.getMPSGraphTensor()];
-        
-        runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-        
-        return output;
-    }
+    return runFFTGraph(self, {}, false, "c2r", FFTType::C2R, signal_ndim, normalized, false);
 }
 
 // Hermitean-to-Real FFT, writing result to the provided output tensor
 Tensor& _fft_c2r_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    using namespace mps;
-    
-    TORCH_CHECK(self.is_mps(), "_fft_c2r_mps_out: Expected MPS tensor");
-    TORCH_CHECK(out.is_mps(), "_fft_c2r_mps_out: Expected MPS tensor for output");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
-    
-    MPSStream* stream = getCurrentMPSStream();
-    
-    @autoreleasepool {
-        string key = "fft_c2r_out_" + getTensorsStringKey({self, out}) + getMPSTypeString(self);
-        
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = YES;
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the HermiteanToRealFFTWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph HermiteanToRealFFTWithTensor:inputTensor
-                                                                             axes:@[@(signal_ndim)]
-                                                                       descriptor:descriptor
-                                                                             name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
-        });
-        
-        Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
-        [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
-        
-        Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, out);
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        [results setObject:outputPlaceholder.getMPSGraphTensorData() forKey:outputPlaceholder.getMPSGraphTensor()];
-        
-        runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-        
-        return out;
-    }
+    runFFTGraph(self, out, true, "c2r_out", FFTType::C2R, signal_ndim, normalized, false);
+    return out;
 }
 
 // complex-to-complex FFT
 Tensor _fft_c2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes) {
-    using namespace mps;
-    
-    TORCH_CHECK(self.is_mps(), "_fft_c2c_mps: Expected MPS tensor");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
-    
-    MPSStream* stream = getCurrentMPSStream();
-    
-    @autoreleasepool {
-        string key = "fft_c2c_" + getTensorsStringKey({self}) + getMPSTypeString(self);
-        
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = !forward; // Set inverse based on the 'forward' flag
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the fastFourierTransformWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph fastFourierTransformWithTensor:inputTensor
-                                                                               axes:@[@(signal_ndim)]
-                                                                         descriptor:descriptor
-                                                                               name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
-        });
-        
-        Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
-        [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        
-        runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-        
-        MPSGraphTensorData* outputData = [results objectForKey:cachedGraph->outputTensor_];
-        Tensor output = createTensorFromMPSGraphTensorData(outputData);
-        
-        return output;
-    }
+    return runFFTGraph(self, {}, false, "c2c", FFTType::C2C, signal_ndim, normalized, forward);
 }
 
-// complex-to-complex FFT, writng result to the provided output tensor
+// complex-to-complex FFT, writing result to the provided output tensor
 Tensor& _fft_c2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes) {
-    using namespace mps;
-    
-    TORCH_CHECK(self.is_mps() && out.is_mps(), "_fft_c2c_mps_out: Expected MPS tensors for both input and output");
-    
-    struct CachedGraph : public MPSCachedGraph {
-        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
-        MPSGraphTensor* inputTensor_ = nil;
-        MPSGraphTensor* outputTensor_ = nil;
-    };
-    
-    MPSStream* stream = getCurrentMPSStream();
-    
-    @autoreleasepool {
-        string key = "fft_c2c_out_" + getTensorsStringKey({self, out}) + getMPSTypeString(self);
-        
-        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
-            newCachedGraph->inputTensor_ = inputTensor;
-            
-            // Create FFT descriptor
-            MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
-            descriptor.inverse = !forward; // Set inverse based on the 'forward' flag
-            descriptor.scalingMode = normalized ? MPSGraphFFTScalingModeUnitary : MPSGraphFFTScalingModeNone;
-            
-            // Use the fastFourierTransformWithTensor method from MPSGraphFourierTransformOps.h
-            MPSGraphTensor* outputTensor = [mpsGraph fastFourierTransformWithTensor:inputTensor
-                                                                               axes:@[@(signal_ndim)]
-                                                                         descriptor:descriptor
-                                                                               name:nil];
-            newCachedGraph->outputTensor_ = outputTensor;
-        });
-        
-        Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = [[[NSMutableDictionary alloc] init] autorelease];
-        [feeds setObject:inputPlaceholder.getMPSGraphTensorData() forKey:inputPlaceholder.getMPSGraphTensor()];
-        
-        NSMutableDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = [[[NSMutableDictionary alloc] init] autorelease];
-        
-        runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-        
-        MPSGraphTensorData* outputData = [results objectForKey:cachedGraph->outputTensor_];
-        Tensor output = createTensorFromMPSGraphTensorData(outputData);
-        
-        // Copy the result to the provided output tensor
-        out.copy_(output);
-    }
-    
+    runFFTGraph(self, out, true, "c2c_out", FFTType::C2C, signal_ndim, normalized, forward);
     return out;
 }
 
