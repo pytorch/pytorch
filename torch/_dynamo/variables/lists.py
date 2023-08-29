12ee86c800e31591662e36b13404f9ffd39da174
Yanbo Liang <ybliang8@gmail.com>
2023-08-29 15:01:05 -0700

    Update

--- torch/_dynamo/variables/lists.py
+++ torch/_dynamo/variables/lists.py
@@ -15,7 +15,7 @@ from ..bytecode_transformation import create_call_function, create_instruction
 from ..exc import unimplemented
 from ..guards import make_dupe_guard
 from ..source import GetItemSource
-from ..utils import check_constant_args, get_fake_value, guard_if_dyn, namedtuple_fields
+from ..utils import get_fake_value, guard_if_dyn, namedtuple_fields
 from .base import MutableLocal, VariableTracker
 from .constant import ConstantVariable
 from .functions import UserFunctionVariable, UserMethodVariable
@@ -24,8 +24,7 @@ from .functions import UserFunctionVariable, UserMethodVariable
 def _listlike_contains_helper(items, search, options):
     if search.is_python_constant():
         result = any(
-            x.as_python_constant() == search.as_python_constant()
-            for x in items
+            x.as_python_constant() == search.as_python_constant() for x in items
         )
         return variables.ConstantVariable(result, **options)
 
