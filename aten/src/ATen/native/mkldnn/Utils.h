79b060f6172e2159c83ed69ca9df7d69a32241ce
CaoE <e.cao@intel.com>
2023-08-28 23:34:10 -0700

    Update base for Update on "add fp16 support for gemm"
    Not complete yet. WIP.
    
    ### Testing
    
    Native matmul vs. mkldnn matmul
    56 cores:
    Input | Naïve impl   / ms | oneDNN /   ms | Speed up
    -- | -- | -- | --
    M: 128, N: 128, K: 128, trans_a: False, trans_b: False | 5.091 | 0.24 | 211.30
    M: 128, N: 128, K: 128, trans_a: False, trans_b: True | 5.224 | 0.23 | 220.09
    M: 128, N: 256, K: 128, trans_a: False, trans_b: False | 10.006 | 0.30 | 330.31
    M: 8192, N: 768, K: 768, trans_a: False, trans_b: False | 29435.372 | 1.770 | 1662.80
    M: 8192, N: 768, K: 768, trans_a: False, trans_b: True | 31464.961 | 1.728 |  18204.76
    M: 8192, N: 768, K: 3072, trans_a: False, trans_b: False | 115035.849  | 7.990 | 14396.90
    M: 8192, N: 768, K: 3072, trans_a: False, trans_b: True | 122981.023 |  7.725 | 15918.34
    Batch: 768, M: 128, N: 64, K: 128  | 2032.523 | 0.705 | 2882.23
    
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10 voznesenskym penguinwu EikanWang Guobing-Chen zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy ngimel yf225 chenyang78 kadeng muchulee8 aakhundov desertfire
    
    [ghstack-poisoned]

--- aten/src/ATen/native/mkldnn/Utils.h
+++ aten/src/ATen/native/mkldnn/Utils.h
@@ -90,12 +90,12 @@ inline void mkldnn_check_low_precision(ScalarType input_t, std::string name) {
     TORCH_CHECK(
         mkldnn_bf16_device_check(),
         name,
-        ": bf16 path needs the cpu support avx512bw, avx512vl and avx512dq");
+        ": bf16 path needs the cpu support avx_ne_convert or avx512bw, avx512vl and avx512dq");
   } else if (input_t == ScalarType::Half) {
     TORCH_CHECK(
         mkldnn_fp16_device_check(),
         name,
-        ": fp16 path needs the cpu support avx512_fp16, avx_vnni, avx512bw, avx512vl, avx512dq, avx512_vnni and avx512_bf16");
+        ": fp16 path needs the cpu support avx_ne_convert or avx512_fp16");
   }
 }
 
