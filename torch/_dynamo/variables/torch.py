6ad5568cbc7122356b58789a1d3bcd16d5faf775
Yukio Siraichi <yukio.siraichi@gmail.com>
2023-08-29 16:37:02 -0300

    Break graph on `manual_seed`. (#107594)
    Fix: #107187
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107594
    Approved by: https://github.com/eellison

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -452,6 +452,9 @@ class TorchVariable(VariableTracker):
         elif self.value is torch.nn.Parameter:
             # https://github.com/pytorch/pytorch/issues/99569
             unimplemented("torch.nn.Parameter not supported")
+        elif self.value is torch.manual_seed:
+            # https://github.com/pytorch/pytorch/issues/107187
+            unimplemented("torch.manual_seed not supported")
         if (
             self.value.__name__ == "get_state"
             and hasattr(self.value, "__self__")
