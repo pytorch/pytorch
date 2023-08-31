06d74e6b24a6f7dfd0b8d05e2171d74def166140
Bin Bao <binbao@meta.com>
2023-08-31 16:26:02 +0000

    Revert "[AOTInductor] Include constants in AOTInductor .so file. (#10… (#108349)
    This reverts commit c3239442a3dd1040b251ff33bef40589cba40e1c due to internal test failures.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108349
    Approved by: https://github.com/aakhundov, https://github.com/zhxchen17

--- torch/_export/__init__.py
+++ torch/_export/__init__.py
@@ -1,4 +1,3 @@
-import copy
 import dataclasses
 import io
 import re
@@ -627,6 +626,7 @@ def aot_compile(
     Returns:
         Path to the generated shared library, and the exported program
     """
+    from torch._inductor.compile_fx import compile_fx_aot
     from torch._inductor.decomposition import select_decomp_table
 
     global DECOMP_TABLE
@@ -635,46 +635,11 @@ def aot_compile(
     # Reset the global value
     DECOMP_TABLE = core_aten_decompositions()
 
+    param_buffer_values = list(ep.state_dict.values())
     flat_example_inputs = fx_pytree.tree_flatten_spec(
         combine_args_kwargs(args, kwargs), ep.call_spec.in_spec  # type: ignore[arg-type]
     )
+    all_args = (*param_buffer_values, *flat_example_inputs)
 
-    unlifted_module = ep.module()
-    unlifted_module.graph.set_codegen(torch.fx.CodeGen())  # type: ignore[attr-defined]
-    unlifted_module.recompile()
-    options = (
-        {"from_export": True}
-        if options is None
-        else {**options, "from_export": True}
-    )
-    so_path = torch._inductor.aot_compile(unlifted_module, flat_example_inputs, options)  # type: ignore[arg-type]
-
-    user_inputs = []
-    user_outputs = []
-    for node in unlifted_module.graph.nodes:
-        if node.op == "placeholder":
-            user_inputs.append(node.name)
-        elif node.op == "output":
-            user_outputs = [arg.name for arg in node.args[0]]
-
-    unlifted_ep = ExportedProgram(
-        unlifted_module,
-        unlifted_module.graph,
-        ExportGraphSignature(
-            [],
-            [],
-            user_inputs,
-            user_outputs,
-            {},
-            {},
-            {},
-            None,
-        ),
-        call_spec=copy.deepcopy(ep.call_spec),
-        state_dict={},
-        range_constraints=copy.deepcopy(ep.range_constraints),
-        equality_constraints=copy.deepcopy(ep.equality_constraints),
-        module_call_graph=ep.module_call_graph,
-    )
-
-    return so_path, unlifted_ep
+    so_path = torch._inductor.aot_compile(ep.graph_module, list(all_args), options)
+    return so_path, ep
