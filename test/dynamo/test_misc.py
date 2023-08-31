01dfa7620d715fb22e703a36db0930077252408c
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-08-31 16:21:09 +0000

    MAINT: np.unique works with f16 directly (#108228)
    (follow up on gh-107768)
    
    Remove a f16->f32 workaround from np.unique, since torch.unique and np.unique seem to just work with float16 tensors.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108228
    Approved by: https://github.com/lezcano

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -1544,6 +1544,18 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         self.assertEqual(r.dtype, torch.int64)
         self.assertEqual(cnts.frame_count, 1)
 
+    def test_numpy_unique_f16(self):
+        def fn():
+            x = np.asarray([1, 1, 2, 2, 3], dtype=np.float16)
+            return np.unique(x)
+
+        cnts = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(cnts)(fn)
+
+        r = opt_fn()
+        self.assertEqual(r.dtype, np.float16)
+        self.assertEqual(cnts.frame_count, 1)
+
     def test_inplace_view_on_graph_input(self):
         # graph break when calling methods with inplace_view tag on graph input
         func_args_map = {
