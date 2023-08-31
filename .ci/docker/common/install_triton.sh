c379b5e17f3d7e6780940f8d386a7aa7bcf0e332
Huy Do <huydhn@gmail.com>
2023-08-31 10:42:04 -0700

    No need to reinstall numpy after install triton

--- .ci/docker/common/install_triton.sh
+++ .ci/docker/common/install_triton.sh
@@ -27,9 +27,8 @@ apt update
 apt-get install -y gpg-agent
 
 if [ -n "${CONDA_CMAKE}" ]; then
-  # Keep the current cmake and numpy version here, so we can reinstall them later
+  # Keep the current cmake version here, so we can reinstall them later
   CMAKE_VERSION=$(get_conda_version cmake)
-  NUMPY_VERSION=$(get_conda_version numpy)
 fi
 
 if [ -z "${MAX_JOBS}" ]; then
@@ -56,11 +55,5 @@ if [ -n "${CONDA_CMAKE}" ]; then
   # script is used. Without this step, the newer cmake version (3.25.2) downloaded by
   # triton build step via pip will fail to detect conda MKL. Once that issue is fixed,
   # this can be removed.
-  #
-  # The correct numpy version also needs to be set here because conda claims that it
-  # causes inconsistent environment.  Without this, conda will attempt to install the
-  # latest numpy version, which fails ASAN tests with the following import error: Numba
-  # needs NumPy 1.20 or less.
   conda_reinstall cmake="${CMAKE_VERSION}"
-  conda_reinstall numpy="${NUMPY_VERSION}"
 fi
