aa73ba227e4f70b458d05eb12384c649e42b5e1a
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-09-01 00:22:55 +0000

    removing some redundant str splits (#106089)
    drop some redundant string splits, no factual changes, just cleaning the codebase
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106089
    Approved by: https://github.com/albanD, https://github.com/malfet

--- torch/distributed/fsdp/fully_sharded_data_parallel.py
+++ torch/distributed/fsdp/fully_sharded_data_parallel.py
@@ -2043,9 +2043,9 @@ def _get_param_to_fqn(
     """
     param_to_param_names = _get_param_to_fqns(model)
     for param_names in param_to_param_names.values():
-        assert len(param_names) > 0, (
-            "`_get_param_to_fqns()` " "should not construct empty lists"
-        )
+        assert (
+            len(param_names) > 0
+        ), "`_get_param_to_fqns()` should not construct empty lists"
         if len(param_names) > 1:
             raise RuntimeError(
                 "Each parameter should only map to one parameter name but got "
