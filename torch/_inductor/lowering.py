fa8edd93b77fcbd5326554ac86906209bd3f9465
Peter Bell <peterbell10@live.co.uk>
2023-09-01 21:34:27 +0100

    [inductor] Handle aten.full's dtype in the decomposition (#108443)
    In the lowering we don't have `SymFloat` and `SymInt`, we just have `sympy.Expr`
    so it is impossible to accurately determine the expected dtype of a `full` call.
    For example, `sym_float(int_expr)` has `is_integer=True` but should be treated
    as a float. In the decomposition though, we can get this right.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108443
    Approved by: https://github.com/lezcano

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -23,7 +23,6 @@ from torch._prims_common import (
     is_float_dtype,
     is_integer_dtype,
     Number,
-    type_to_dtype,
 )
 from torch.fx.experimental.symbolic_shapes import magic_methods, method_to_operator
 from torch.utils._pytree import tree_flatten
@@ -129,15 +128,6 @@ def decode_dtype(dtype: int):
     assert dtype in DTYPE_ID_LOOKUP, f"id {dtype} missing from DTYPE_ID_LOOKUP"
     dtype = DTYPE_ID_LOOKUP[dtype]
     return dtype
-
-
-def value_to_dtype(value: Any) -> torch.dtype:
-    if isinstance(value, sympy.Expr):
-        if value.is_integer:  # type: ignore[attr-defined]
-            return torch.long
-        if value.is_real:
-            return torch.get_default_dtype()
-    return type_to_dtype(type(value))
 
 
 def is_integer_type(x):
@@ -2467,8 +2457,7 @@ def copy_strided(x, stride):
 
 @register_lowering([torch.full, aten.full])
 def full(size, fill_value, **kwargs):
-    dtype = kwargs.get("dtype")
-    kwargs["dtype"] = dtype if dtype is not None else value_to_dtype(fill_value)
+    assert kwargs.get("dtype") is not None, "dtype should be handled by decomposition"
     return tensor_constructor(fill_value)(size, **kwargs)
 
 
