1ae74a030c644ad5cc81de5b1b9a3b016f2a8494
shibo19 <18207133434@163.com>
2023-09-01 11:29:59 +0800

    add check for torch_arg

--- test/cpp/api/modules.cpp
+++ test/cpp/api/modules.cpp
@@ -67,6 +67,12 @@ TEST_F(ModulesTest, Conv1dSameStrided) {
       "padding='same' is not supported for strided convolutions");
 }
 
+TEST_F(ModulesTest, Conv1dIvalidArg) {
+  auto options = Conv1dOptions(3, 2, 3);
+  ASSERT_THROWS_WITH(
+      options.groups(-1), "groups should be positive, and the dtype");
+}
+
 TEST_F(ModulesTest, Conv2dEven) {
   Conv2d model(Conv2dOptions(3, 2, 3).stride(1).bias(false));
   model->weight.set_data(
