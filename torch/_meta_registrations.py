cc3b9990edf03dc8335c1278ad15826b9b001164
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2023-09-02 21:51:03 +0800

    Fix meta registration for `aten.view.dtype`

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -2606,7 +2606,54 @@ def meta_complex(real, imag):
 
 @register_meta(aten.view.dtype)
 def view_dtype(self, dtype):
-    return utils.clone_preserve_strides(self).to(dtype)
+    if self.dtype == dtype:
+        return self
+    torch._check(not self.is_conj(),
+                 lambda: "torch.Tensor.view is not supported for conjugate view tensors when converting to a different dtype.")
+    torch._check(not self.is_neg(),
+                 lambda: "torch.Tensor.view is not supported for tensors with negative bit set when converting to a different dtype.")
+    torch._check(self.dim() == 0,
+                 lambda: f"self.dim() cannot be 0 to view {self.dtype} as {dtype} (different element sizes)")
+    self_elem_size = self.element_size()
+    new_elem_size = dtype.itemsize()
+
+    if self_elem_size == new_elem_size:
+        new_storage_offset = self.storage_offset()
+        new_size = self.size()
+        new_stride = self.stride()
+    elif self_elem_size > new_elem_size:
+        # Downsizing element size
+        size_ratio = self_elem_size // new_elem_size
+        new_stride = compute_strides_for_view_dtype_downsize(self.stride(), size_ratio, self.dtype, dtype)
+
+        new_size = list(self.size())
+        new_size[self.dim() - 1] *= size_ratio
+
+        new_storage_offset = size_ratio * self.storage_offset()
+    else:
+        # Upsizing element size
+        size_ratio = new_elem_size // self_elem_size
+
+        torch._check(
+        (self.size(-1) % size_ratio) == 0,
+        lambda: f"self.size(-1) must be divisible by {size_ratio} to view ",
+        f"{self.dtype} as {dtype} (different element sizes), ",
+        f"but got {self.size(-1)}")
+
+        torch._check(
+        (self.storage_offset() % size_ratio) == 0,
+        lambda: f"self.storage_offset() must be divisible by {size_ratio} to view ",
+        f"{self.dtype} as {dtype} (different element sizes), ",
+        f"but got {self.storage_offset()}")
+        new_stride = compute_strides_for_view_dtype_upsize(self.stride(), size_ratio, self.dtype, dtype)
+
+        new_size = list(self.size())
+        new_size[self.dim() - 1] /= size_ratio
+
+        new_storage_offset = size_ratio / self.storage_offset()
+    result = torch.empty((), dtype=dtype)
+    result.set_(self, storage_offset=new_storage_offset, size=new_size, stride=new_stride)
+    return result
 
 
 @register_meta(aten.vdot.default)
