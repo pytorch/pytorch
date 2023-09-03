27d9306b989ca2ac944605ddf4d37ddd0c24f550
igm503 <igm503@gmail.com>
2023-09-03 17:11:07 -0500

    mps_weight_norm now working for forward pass on dim=0 and dim=last

--- aten/src/ATen/native/mps/operations/WeightNorm.mm
+++ aten/src/ATen/native/mps/operations/WeightNorm.mm
@@ -70,38 +70,49 @@ std::tuple<Tensor,Tensor> weight_norm_mps
             gPlaceholder.getMPSGraphTensor() : gPlaceholder.getMPSGraphTensorData()};
 
       NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results =
-          @{normsPlaceholder.getMPSGraphTensor() : normsPlaceholder.getMPSGraphTensorData()};
-          @{wPlaceholder.getMPSGraphTensor() : wPlaceholder.getMPSGraphTensorData()};
+          @{normsPlaceholder.getMPSGraphTensor() : normsPlaceholder.getMPSGraphTensorData(),
+            wPlaceholder.getMPSGraphTensor() : wPlaceholder.getMPSGraphTensorData()};
 
       runMPSGraph(mpsStream, cachedGraph->graph(), feeds, results);
     }
   }
-  // else if(dim == ndims - 1)
-  // {
-  //   @autoreleasepool {
-  //     string key = "weight_norm_mps_second_dim" + getTensorsStringKey({self});
-  //     auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-  //       MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, self);
-  //       // passing selector of reLUWithTensor on the mpsGraph object
-  //       MPSGraphTensor* outputTensor = [mpsGraph reLUWithTensor:inputTensor name:nil];
+  else if(dim == ndims - 1)
+  {
+    @autoreleasepool {
+      string key = "weight_norm_mps_second_dim" + getTensorsStringKey({v, g});
+      auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
 
-  //       newCachedGraph->inputTensor_ = inputTensor;
-  //       newCachedGraph->outputTensor_ = outputTensor;
-  //     });
+        newCachedGraph->v_ = mpsGraphRankedPlaceHolder(mpsGraph, getMPSScalarType(v.scalar_type()), getMPSShape(v));
+        newCachedGraph->g_ = mpsGraphRankedPlaceHolder(mpsGraph, getMPSScalarType(g.scalar_type()), getMPSShape(g));
 
-  //     Placeholder selfPlaceholder = Placeholder(cachedGraph->inputTensor_, self, nil, executeGatherOp);
-  //     Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, output, nil, false);
+        // Compute the L2 norm for each column of v
+        MPSGraphTensor *squared = [mpsGraph squareWithTensor:newCachedGraph->v_ name:nil];
+        MPSGraphTensor *sum_squared = [mpsGraph reductionSumWithTensor:squared axes:@[@0] name:nil];
+        MPSGraphTensor *norms_ = [mpsGraph squareRootWithTensor:sum_squared name:nil];
+        newCachedGraph->norms_ = norms_;
+        // Divide each column of v by its L2 norm
+        MPSGraphTensor *unit_v = [mpsGraph divisionWithPrimaryTensor:newCachedGraph->v_ secondaryTensor:norms_ name:nil];
 
-  //     // Create dictionary of inputs and outputs
-  //     NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds =
-  //         @{selfPlaceholder.getMPSGraphTensor() : selfPlaceholder.getMPSGraphTensorData()};
+        // Multiply each columb of vNormalized by the corresponding element of g
+        newCachedGraph->w_ = [mpsGraph multiplicationWithPrimaryTensor:unit_v secondaryTensor:newCachedGraph->g_ name:nil];
+      });
 
-  //     NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results =
-  //         @{outputPlaceholder.getMPSGraphTensor() : outputPlaceholder.getMPSGraphTensorData()};
+      Placeholder vPlaceholder = Placeholder(cachedGraph->v_, v, nil, true);
+      Placeholder gPlaceholder = Placeholder(cachedGraph->g_, g, nil, true);
+      Placeholder normsPlaceholder = Placeholder(cachedGraph->norms_, norms);
+      Placeholder wPlaceholder = Placeholder(cachedGraph->w_, w);
 
-  //     runMPSGraph(stream, cachedGraph->graph(), feeds, results);
-  //   }
-  // }
+      NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds =
+          @{vPlaceholder.getMPSGraphTensor() : vPlaceholder.getMPSGraphTensorData(),
+            gPlaceholder.getMPSGraphTensor() : gPlaceholder.getMPSGraphTensorData()};
+
+      NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results =
+          @{normsPlaceholder.getMPSGraphTensor() : normsPlaceholder.getMPSGraphTensorData(),
+            wPlaceholder.getMPSGraphTensor() : wPlaceholder.getMPSGraphTensorData()};
+
+      runMPSGraph(mpsStream, cachedGraph->graph(), feeds, results);
+    }
+  }
 
   return std::tuple<Tensor, Tensor>{w, norms};
 }
