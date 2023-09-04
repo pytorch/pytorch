cec5603e27ec63b1df51aa58e0dd9d2eb7f85a81
David Radley <david_radley@uk.ibm.com>
2023-09-04 15:32:38 +0100

    105264 neaten test code

--- test/inductor/test_lowering.py
+++ test/inductor/test_lowering.py
@@ -44,6 +44,7 @@ assert(torchResult == dynamoResult )
 def mySum64(x):
     return (x+x).to(torch.int64)
 
+# failing test
 x = torch.tensor( (2147483647), dtype=torch.int32)
 torchResult = mySum64(x)
 dynamoResult = torch.compile(mySum64)(x)
