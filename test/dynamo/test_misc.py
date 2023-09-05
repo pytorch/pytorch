7dae7369d7b1d129a557a111938e6ab66b006bd8
Animesh Jain <anijain@umich.edu>
2023-09-05 17:56:23 +0000

    [dynamo] Reduce cache size limit to 8 (#108526)
    As title
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108526
    Approved by: https://github.com/ezyang

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -1470,13 +1470,14 @@ class MiscTests(torch._dynamo.test_case.TestCase):
 
         cnts = torch._dynamo.testing.CompileCounter()
         opt_fn = torch._dynamo.optimize(cnts, nopython=True)(mandelbrot_numpy)
-        for _ in range(10):
+        n_iter = torch._dynamo.config.cache_size_limit
+        for _ in range(n_iter):
             x = random.randint(2, 30)
             ref = mandelbrot_numpy(x)
             res = opt_fn(x)
             self.assertEqual(ref, res)
         # We need to specialise the number as it's in a forloop
-        self.assertEqual(cnts.frame_count, 10)
+        self.assertEqual(cnts.frame_count, n_iter)
 
     def test_numpy_as_global(self):
         global x
