e3b4562375687438ea4a7e7d7c6fa828a33d4572
pytorchbot <soumith+bot@pytorch.org>
2023-09-05 15:28:59 -0700

    debug

--- torch/_inductor/analysis.py
+++ torch/_inductor/analysis.py
@@ -8,7 +8,8 @@ def get_runtime_of_snode(snode: "BaseSchedulerNode"):
 
     TODO(yf225): potentially replace with a more sophisticated cost model (e.g. analytical)
     """
-    print(f"get_runtime_of_snode: snode: {snode}")
+    print(f"get_runtime_of_snode: snode.node: {snode.node}")
+    print(f"get_runtime_of_snode: snode.node.meta: {snode.node.meta}")
     if isinstance(snode.node, ir.AllReduce):
         return 5
     if isinstance(snode.node, ir.CollectiveKernel):
