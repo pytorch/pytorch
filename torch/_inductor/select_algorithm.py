90452f41e384e25ffbaf7bca15c13dc6f418fbd7
Jason Ansel <jansel@meta.com>
2023-09-05 11:34:47 -0700

    [dynamo] Graph break on pack_padded_sequence (#108120)
    Release branch cherrypick of #108096

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -340,6 +340,7 @@ class TritonTemplateKernel(TritonKernel):
     def call_kernel(self, name: str):
         wrapper = V.graph.wrapper_code
         _, call_args, _ = self.args.python_argdefs()
+        call_args = [str(a) for a in call_args]
 
         for i in range(len(call_args)):
             if V.graph.is_unspec_arg(call_args[i]):
@@ -354,7 +355,7 @@ class TritonTemplateKernel(TritonKernel):
                 device_index=V.graph.scheduler.current_device.index,
             )
         else:
-            call_args = ", ".join(call_args)
+            call_args = ", ".join(call_args)  # type: ignore[assignment]
             stream_name = wrapper.write_get_cuda_stream(
                 V.graph.scheduler.current_device.index
             )
