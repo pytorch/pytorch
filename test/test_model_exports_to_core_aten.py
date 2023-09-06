7ec5cd1a96a3612e2f1bc0e3aa6aeb32d7c72235
Guang Yang <guangyang@meta.com>
2023-09-05 18:38:10 -0700

    [pytorch] Test key ET models export to core aten ir (#108614)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/108614
    
    Test Plan: ci
    
    Differential Revision: D48992081

--- test/test_model_exports_to_core_aten.py
+++ test/test_model_exports_to_core_aten.py
@@ -3,22 +3,28 @@ import copy
 
 import torch
 import torch._export as export
+from torch._export.verifier import ATenDialectVerifier, SpecViolationError
 
 from torch.testing._internal.common_quantization import skip_if_no_torchvision
 from torch.testing._internal.common_utils import TestCase
 
 
-def _get_ops_list(m: torch.fx.GraphModule):
-    op_list = []
-    for n in m.graph.nodes:
-        if n.op == "call_function":
-            op_list.append(n.target)
-    return op_list
+class TestPT2ETModels(TestCase):
+    def _check_core_aten_ops(self, gm: torch.fx.GraphModule):
+        """
+        Check if all ops in the graph module are core aten ops.
+        """
+        verifier = ATenDialectVerifier()
+        for node in gm.graph.nodes:
+            if (
+                node.op == "call_function"
+                and node.target not in verifier.valid_builtin_funcs()
+            ):
+                with self.assertRaises(SpecViolationError):
+                    verifier.check_valid_op(node.target)
 
-
-class TestQuantizePT2EModels(TestCase):
     @skip_if_no_torchvision
-    def test_vit_aten_export(self):
+    def test_vit_export_to_core_aten(self):
         from torchvision.models import vit_b_16  # @manual
 
         m = vit_b_16(weights="IMAGENET1K_V1")
@@ -27,10 +33,98 @@ class TestQuantizePT2EModels(TestCase):
         example_inputs = (torch.randn(input_shape),)
         m = export.capture_pre_autograd_graph(m, copy.deepcopy(example_inputs))
         m(*example_inputs)
-        m = export.export(m, copy.deepcopy(example_inputs))
-        ops = _get_ops_list(m.graph_module)
-        non_core_aten_op_found = False
-        for op in ops:
-            if "scaled_dot_product" in str(op):
-                non_core_aten_op_found = True
-        self.assertFalse(non_core_aten_op_found)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    @skip_if_no_torchvision
+    def test_mv2_export_to_core_aten(self):
+        from torchvision.models import mobilenet_v2  # @manual
+
+        m = mobilenet_v2(pretrained=True)
+        m = m.eval()
+        input_shape = (1, 3, 224, 224)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    @skip_if_no_torchvision
+    def test_mv3_export_to_core_aten(self):
+        from torchvision.models import mobilenet_v3_small  # @manual
+
+        m = mobilenet_v3_small(pretrained=True)
+        m = m.eval()
+        input_shape = (1, 3, 224, 224)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    @skip_if_no_torchvision
+    def test_inception_v3_export_to_core_aten(self):
+        from torchvision.models import inception_v3  # @manual
+
+        m = inception_v3(weights="IMAGENET1K_V1")
+        m = m.eval()
+        input_shape = (1, 3, 224, 224)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    @skip_if_no_torchvision
+    def test_resnet18_export_to_core_aten(self):
+        from torchvision.models import resnet18, ResNet18_Weights  # @manual
+
+        m = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        m = m.eval()
+        input_shape = (1, 3, 224, 224)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    @skip_if_no_torchvision
+    def test_resnet50_export_to_core_aten(self):
+        from torchvision.models import resnet50, ResNet50_Weights  # @manual
+
+        m = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+        m = m.eval()
+        input_shape = (1, 3, 224, 224)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    def test_wav2letter_export_to_core_aten(self):
+        from torchaudio import models
+
+        batch_size = 10
+        input_frames = 700
+        vocab_size = 4096
+
+        m = models.Wav2Letter(num_classes=vocab_size)
+        m = m.eval()
+        input_shape = (batch_size, 1, input_frames)
+        example_inputs = (torch.randn(input_shape),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    def test_inception_v4_export_to_core_aten(self):
+        from timm.models import inception_v4
+
+        m = inception_v4(pretrained=True)
+        m = m.eval()
+        input_shape = (3, 299, 299)
+        example_inputs = (torch.randn(input_shape).unsqueeze(0),)
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
+
+    def test_mobilebert_export_to_core_aten(self):
+        from transformers import AutoTokenizer, MobileBertModel  # @manual
+
+        m = MobileBertModel.from_pretrained(
+            "google/mobilebert-uncased", return_dict=False
+        )
+        m = m.eval()
+        tokenizer = AutoTokenizer.from_pretrained("google/mobilebert-uncased")
+        example_inputs = (
+            tokenizer("Hello, my dog is cute", return_tensors="pt")["input_ids"],
+        )
+        gm = export.export(m, copy.deepcopy(example_inputs))
+        self._check_core_aten_ops(gm)
