418cf260a884653cb39992042bde73ec77b37367
pytorchbot <soumith+bot@pytorch.org>
2023-09-06 09:52:50 -0700

    clean up

---
+++ test/inductor/test_compute_comm_reordering.py
@@ -0,0 +1,11 @@
+"""
+TODO: unit tests to add:
+1. sink_waits()
+    TODO: design good # and variaty of compute ops
+2. raise_comms()
+    TODO: design good # and variaty of compute ops
+3. reorder_compute_for_overlap()
+    1) only 1 comm
+    2) 2+ comms
+    TODO: design good # and variaty of compute ops
+4. reorder_compute_and_comm_for_overlap() integration test
