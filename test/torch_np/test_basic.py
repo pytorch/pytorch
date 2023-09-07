bcc8f150be873e181fcb66857cb89dadfd4dd5e2
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-09-07 17:16:58 +0000

    TST: add if __name__ == "__main__" stanzas to numpy_tests/core

--- test/torch_np/test_basic.py
+++ test/torch_np/test_basic.py
@@ -16,17 +16,17 @@ from torch.testing._internal.common_utils import TestCase
 
 
 def test_fail_freefunc():
-    assert False
+    raise AssertionError()
 
 
 class TestFailInherit(TestCase):
     def test_fail_inherit(self):
-        assert False
+        raise AssertionError()
 
 
 class TestFailNoInhertit:
     def test_fail_noinherit(self):
-        assert False
+        raise AssertionError()
 
 
 from torch.testing._internal.common_cuda import TEST_CUDA
