44fd9b87e1ef928439e57d63bfbb68596f7af8b5
Peter Bell <peterbell10@live.co.uk>
2023-09-07 22:13:18 +0100

    Update on "[inductor] Lower masked_scatter on CUDA"
    This decomposes masked_scatter into `aten.cumsum` and a single pointwise kernel,
    which is similar to what is done in eager. I only do this for CUDA because on CPU
    it isn't split into two passes like this so would cause a slowdown.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx ipiszy ngimel yf225 chenyang78 kadeng muchulee8 aakhundov
    
    [ghstack-poisoned]

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -20,7 +20,7 @@ from torch._decomp.decompositions_for_rng import extra_random_decomps
 from torch._higher_order_ops.out_dtype import out_dtype
 from torch._prims_common import type_to_dtype
 
-from . import config
+from . import config, inductor_prims
 
 log = logging.getLogger(__name__)
 aten = torch.ops.aten
@@ -555,7 +555,7 @@ def select_decomp_table():
 @register_decomposition(aten.masked_scatter)
 def masked_scatter(self, mask, source):
     if self.device.type == "cuda":
-        self, mask = aten.broadcast_tensors(self, mask)
+        self, mask = aten.broadcast_tensors([self, mask])
         source_idx = mask.reshape(-1).cumsum(0) - 1
-        return prims.inductor_masked_scatter_with_index(self, mask, source_idx, source)
+        return inductor_prims.masked_scatter_with_index(self, mask, source_idx, source)
     return NotImplemented
