e8f71fef5aba956497b385edbd1fe1ec40a2107a
angelayi <yiangela7@gmail.com>
2023-09-08 16:16:57 -0700

    Modify docstring

--- torch/export/__init__.py
+++ torch/export/__init__.py
@@ -945,143 +945,6 @@ def export(
     this normalization and control flow elimination is sound for a future
     input.
 
-    Args:
-        f: The callable to trace.
-
-        args: Example positional inputs.
-
-        kwargs: Optional example keyword inputs.
-
-        constraints: An optional list of constraints on the dynamic arguments
-         that specify their possible range of shapes. By default, shapes of
-         input torch.Tensors are assumed to be static. If an input torch.Tensor
-         is expected to have dynamic shapes, please use :func:`dynamic_dim`
-         to define :class:`Constraint` objects that specify the dynamics and the possible
-         range of shapes. See :func:`dynamic_dim` docstring for examples on
-         how to use it.
-
-    Returns:
-        An :class:`ExportedProgram` containing the traced callable.
-
-    **How it works**
-    :func:`export` operates by using TorchDynamo to symbolically trace the
-    bytecode of the passed in Python callable, inlining functions and control
-    flow and recording the Tensor calls into an FX graph.  Export then
-    applies further lowering passes to normalize these calls into a
-    functionalized core ATen operator set.
-
-    Compared to :func:`torch.fx.symbolic_trace`, :func:`export` operates at
-    the bytecode level, giving it the ability to trace arbitrary Python constructs
-    not limited by what Python operator overloading supports.  We also keep
-    fine-grained track of Tensor metadata, so that conditionals on things like
-    ``tensor.size(i)`` do not fail tracing, and then normalize the graph to ATen operators.
-    In general, you should expect :func:`export` to work on more user programs, but to
-    produce lower-level graphs.  You can still use :func:`torch.fx.symbolic_trace` as
-    a pre-processing step before :func:`export`
-
-    Compared to :func:`torch.jit.script`, :func:`export` does not capture Python
-    control flow or data structures.  We support more Python language features than
-    TorchScript (as it is easier to have comprehensive coverage over Python
-    bytecodes).  The resulting graphs are simpler and only have straight line
-    control flow (except for explicit control flow operators).
-
-    Compared to :func:`torch.jit.trace`, :func:`export` is sound: it is able to
-    trace code that performs integer computation on sizes and records all of the
-    side-conditions necessary to show that a particular trace is valid for other inputs.
-
-    **Acceptable input/output types**
-    Acceptable types of inputs (for ``args`` and ``kwargs``) and outputs include:
-
-    - Primitive types, i.e. ``torch.Tensor``, ``int``, ``float``, ``bool`` and ``str``.
-    - (Nested) Data structures comprising of ``dict``, ``list``, ``tuple``, ``namedtuple`` and
-      ``OrderedDict`` containing all above types.
-
-
-    **What's specialized in the program?**
-
-    1. Non-tensor inputs
-
-    :func:`export` specializes the traced program based on the values of
-    inputs that are not torch.Tensors, ie. ``int``, ``float``, ``bool`` and ``str``.
-
-    For example::
-
-        from torch.export import export
-
-        def fn(x: torch.Tensor, i: int):
-            return x + i
-
-        example_inputs = (torch.rand(2, 2), 1)  # i is set to 1 in example inputs
-        ep = export(fn, example_inputs)
-
-    would yield an :class:`ExportedProgram` containing following graph::
-
-        %arg0_1 := placeholder[target=arg0_1]
-        %arg1_1 := placeholder[target=arg1_1]
-        %add := call_function[target=torch.ops.aten.add.Tensor](args = (%arg0_1, 1), kwargs = {})
-        return (add,)
-
-    Notice that ``%add`` is computed by adding ``%arg0_1`` and ``1``, which is a
-    constant rather than ``%arg1_1`` because integers are specialized.
-
-    2. Rank and static shapes (not values) of input Tensors
-
-    Rank of a tensor is always specialized and treated as constant. Sizes of
-    dimensions are also specialized as constant, i.e. static shapes unless
-    specified as dynamic via :func:`dynamic_dim` API, for example::
-
-        from torch.export import export
-
-        def fn(x):
-            if x.shape[0] > 5:
-                return x + 1
-            else:
-                return x
-
-        example_inputs = (torch.rand(10, 2))
-        ep = export(fn, example_inputs)
-
-    Would produce an :class:`ExportedProgram` containing following graph::
-
-        %arg0_1 := placeholder[target=arg0_1]
-        %add := call_function[target=torch.ops.aten.add.Tensor](args = (%arg0_1, 1), kwargs = {})
-        return (add,)
-
-    You can see that the conditional on ``x.shape[0]>5`` is removed because the
-    example inputs has the static shape of ``(10, 2)``. :func:`export` specializes
-    on the static shape, thus the ``else`` branch will never be reached, thus it
-    does not show up in the exported program.
-
-    Note:
-    If you want to preserve dynamic branching behavior based on value or
-    shape of torch.Tensor in the generated graph, you will need to use
-    :func:`dynamic_dim` to make a dimension of input tensor to be dynamic
-    and rewrite the source code using control flow operations like
-    :func:`torch.ops.higher_order.cond`.
-
-    3. Control flow
-
-    By default, control flow (like ``if``) branching decisions are spcialized
-    according to execution flow observed during tracing run. See following
-    section on how to preserve dynamic control flow
-
-    **How to express Dynamism**
-
-    1. Shape Dynamism
-
-    Because static shape use cases are more dominant, :func:`export` chooses to
-    assume shapes are all static by default unless there are explicit user
-    instructions that say otherwise. Specifically, users can use
-    :func:`dynamic_dim` to give a hint to :func:`export` about dynamism
-    and range of an input tensor dimension.
-
-    2. Dynamic Control Flow
-
-    To preserve dynamic branching behavior of control flows (like ``if``), users
-    need to rewrite source code of original program to use PyTorch's higher order
-    operators (like `torch.ops.higher_order.cond`).
-
-
     **Soundness Guarantee**
 
     While tracing, :func:`export()` takes note of shape-related assumptions
@@ -1120,27 +983,31 @@ def export(
     then copy this exact function into your code to generated needed
     constraints to be passed into ``constraints`` argument.
 
-    **ExportedProgram Invariants**
+    Args:
+        f: The callable to trace.
 
-    The returned :class:`ExportedProgram` maintains the following invariants:
+        args: Example positional inputs.
 
-    - It is guaranteed to be a sound representation of the original
-      program.
-    - It maintains the exact calling convention of the original program.
-    - It contains a ``state_dict`` that stores the :class:`torch.nn.Parameters`
-      and registered buffers involved in computation of the original program.
-    - It includes an :class:`fx.GraphModule` that represents the computation of
-      the original program. The :class:`fx.GraphModule`:
+        kwargs: Optional example keyword inputs.
 
-     - Contains only ``placeholder``, ``call_function``, ``get_attr`` and ``return`` nodes.
-     - Inlines all submodules from the original programs.
-     - Lifts all parameters and buffers of the original program as inputs to the graph.
-     - Does not mutate intermediate values, parameters, or buffers.
-     - Does not include operations with side effects.
-     - Contains only a curated subset of ATen operations and registered
-       custom operations (by default). See the list of Core ATen Ops
-       here: https://pytorch.org/docs/stable/ir.html
+        constraints: An optional list of constraints on the dynamic arguments
+         that specify their possible range of shapes. By default, shapes of
+         input torch.Tensors are assumed to be static. If an input torch.Tensor
+         is expected to have dynamic shapes, please use :func:`dynamic_dim`
+         to define :class:`Constraint` objects that specify the dynamics and the possible
+         range of shapes. See :func:`dynamic_dim` docstring for examples on
+         how to use it.
 
+    Returns:
+        An :class:`ExportedProgram` containing the traced callable.
+
+    **Acceptable input/output types**
+
+    Acceptable types of inputs (for ``args`` and ``kwargs``) and outputs include:
+
+    - Primitive types, i.e. ``torch.Tensor``, ``int``, ``float``, ``bool`` and ``str``.
+    - (Nested) Data structures comprising of ``dict``, ``list``, ``tuple``, ``namedtuple`` and
+      ``OrderedDict`` containing all above types.
 
     """
 
