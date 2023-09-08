f96fef20f05fc621f391386a0bd16200994a0914
XiaobingSuper <xiaobing.zhang@intel.com>
2023-09-08 02:10:54 -0400

    inductor: make onednn linear inputs are always real contiguous (#108560)
    For OneDNN linear, if packed linear inputs are not the default contiguous tensor, it always calls in ref pat and gets a worse performance, this PR will force its inputs to the actual default contiguous tensor.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108560
    Approved by: https://github.com/jgong5, https://github.com/desertfire, https://github.com/jansel

--- test/inductor/test_cpu_repro.py
+++ test/inductor/test_cpu_repro.py
@@ -2466,6 +2466,28 @@ class CPUReproTests(TestCase):
         y = torch.randn(8, 8, 3136, 8)
         self.common(fn, (x, y))
 
+    @unittest.skipIf(not torch.backends.mkldnn.is_available(), "MKLDNN is not enabled")
+    @patch("torch.cuda.is_available", lambda: False)
+    @config.patch(freezing=True)
+    def test_linear_with_no_default_contiguous_input(self):
+        mod = torch.nn.Sequential(torch.nn.Linear(16, 16)).eval()
+        temp = torch.randn(1, 16, 1, 1)
+        v = torch.as_strided(temp, [1, 16], [0, 1], 0)
+        self.assertTrue(v.is_contiguous())
+        with torch.no_grad():
+            self.common(
+                mod,
+                (v,),
+            )
+        if torch.ops.mkldnn._is_mkldnn_bf16_supported():
+            mod = mod.to(torch.bfloat16)
+            v = v.to(torch.bfloat16)
+            with torch.no_grad():
+                self.common(
+                    mod,
+                    (v,),
+                )
+
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
