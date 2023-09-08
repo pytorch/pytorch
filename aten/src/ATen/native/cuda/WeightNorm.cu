eea7d3b589c5a9100593769da0c116ac0f413231
jp <jp42maru@gmail.com>
2023-09-08 09:06:06 +0900

    Update aten/src/ATen/native/cuda/WeightNorm.cu
    Co-authored-by: mikaylagawarecki <35276741+mikaylagawarecki@users.noreply.github.com>

--- aten/src/ATen/native/cuda/WeightNorm.cu
+++ aten/src/ATen/native/cuda/WeightNorm.cu
@@ -454,7 +454,7 @@ std::tuple<Tensor, Tensor> weight_norm_backward_cuda
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
     AT_DISPATCH_FLOATING_TYPES_AND2
-      (kBFloat16, kHalf, v.scalar_type(),
+      (kBFloat16, kHalf, saved_v.scalar_type(),
        "weight_norm_bwd_first_dim_kernel",
        [&]
        {
