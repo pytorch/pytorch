0031485a195b6fb691c57d36cb11d350c98e116f
Elias Ellison <elias.ellison@gmail.com>
2023-09-08 20:40:38 +0000

    Update on "Add Python serialization to Pattern Matcher patterns"
    Adds a Python Pretty Printer to the pattern matcher that serializes patterns as python. Generating our fuse attention patterns was taking 4 seconds of compile time, which will only get worse as we add more variants (which I will do in the rest of this stack). To write out patterns, build pytorch, then run `gen_attention_patterns.py`.
    
    Since there is a line limit for PRs  i'm only including the _sdpa_pattern1 in this first diff. Then I will include all inference patterns, then all training patterns.
    
    
    Example Serialized Pattern:
    
    ```
    tmp_0 = CallFunction(
        aten.view.default,
        CallFunction(aten.expand.default, KeywordArg("query"), Ignored()),
        Ignored(),
        _users=2,
    )
    tmp_1 = CallFunction(
        aten.view.default,
        CallFunction(
            aten.expand.default,
            CallFunction(aten.permute.default, KeywordArg("key"), Ignored()),
            Ignored(),
        ),
        Ignored(),
        _users=2,
    )
    tmp_2 = CallFunction(
        aten.div.Tensor,
        CallFunction(
            aten.view.default, CallFunction(aten.bmm.default, tmp_0, tmp_1), Ignored()
        ),
        KeywordArg("inv_scale"),
        _users=2,
    )
    tmp_3 = CallFunction(
        aten.exp.default,
        CallFunction(
            aten.sub.Tensor, tmp_2, CallFunction(aten.amax.default, tmp_2, Ignored(), True)
        ),
        _users=2,
    )
    tmp_4 = CallFunction(
        aten.div.Tensor,
        tmp_3,
        CallFunction(aten.sum.dim_IntList, tmp_3, Ignored(), True),
        _users=3,
    )
    tmp_5 = CallFunction(
        aten.view.default,
        CallFunction(aten.expand.default, tmp_4, Ignored()),
        Ignored(),
        _users=2,
    )
    tmp_6 = CallFunction(
        aten.view.default,
        CallFunction(aten.expand.default, KeywordArg("value"), Ignored()),
        Ignored(),
        _users=2,
    )
    tmp_7 = CallFunction(aten.view.default, KeywordArg("tangents_1"), Ignored(), _users=2)
    tmp_8 = CallFunction(
        aten.mul.Tensor,
        CallFunction(
            aten.view.default,
            CallFunction(
                aten.bmm.default,
                tmp_7,
                CallFunction(aten.permute.default, tmp_6, Ignored()),
            ),
            Ignored(),
        ),
        tmp_4,
        _users=2,
    )
    tmp_9 = CallFunction(
        aten.view.default,
        CallFunction(
            aten.div.Tensor,
            CallFunction(
                aten.sub.Tensor,
                tmp_8,
                CallFunction(
                    aten.mul.Tensor,
                    tmp_4,
                    CallFunction(aten.sum.dim_IntList, tmp_8, Ignored(), True),
                ),
            ),
            KeywordArg("inv_scale"),
        ),
        Ignored(),
        _users=2,
    )
    ```
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy ngimel yf225 chenyang78 kadeng muchulee8 aakhundov
    
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/serialized_attention_patterns/central_index.py
+++ torch/_inductor/fx_passes/serialized_attention_patterns/central_index.py
@@ -2,60 +2,17 @@
 # To re-generate, run:
 # cd ~/pytorch && python
 # torchgen/fuse_attention_patterns/gen_attention_patterns.py
-from ._sfdp_pattern_1 import _sfdp_pattern_1_inference, _sfdp_pattern_1_training
-from ._sfdp_pattern_10 import _sfdp_pattern_10_inference, _sfdp_pattern_10_training
-from ._sfdp_pattern_11 import _sfdp_pattern_11_inference, _sfdp_pattern_11_training
-from ._sfdp_pattern_12 import _sfdp_pattern_12_inference, _sfdp_pattern_12_training
-from ._sfdp_pattern_13 import _sfdp_pattern_13_inference, _sfdp_pattern_13_training
-from ._sfdp_pattern_14 import _sfdp_pattern_14_inference, _sfdp_pattern_14_training
-from ._sfdp_pattern_15 import _sfdp_pattern_15_inference, _sfdp_pattern_15_training
-from ._sfdp_pattern_2 import _sfdp_pattern_2_inference, _sfdp_pattern_2_training
-from ._sfdp_pattern_3 import _sfdp_pattern_3_inference, _sfdp_pattern_3_training
-from ._sfdp_pattern_4 import _sfdp_pattern_4_inference, _sfdp_pattern_4_training
-from ._sfdp_pattern_5 import _sfdp_pattern_5_inference, _sfdp_pattern_5_training
-from ._sfdp_pattern_6 import _sfdp_pattern_6_inference, _sfdp_pattern_6_training
-from ._sfdp_pattern_7 import _sfdp_pattern_7_inference, _sfdp_pattern_7_training
-from ._sfdp_pattern_8 import _sfdp_pattern_8_inference, _sfdp_pattern_8_training
-from ._sfdp_pattern_9 import _sfdp_pattern_9_inference, _sfdp_pattern_9_training
+from ._sfdp_pattern_1 import (_sfdp_pattern_1_training, _sfdp_pattern_1_inference)
 
 central_index = {
-    "_sfdp_pattern_1_training": _sfdp_pattern_1_training,
-    "_sfdp_pattern_1_inference": _sfdp_pattern_1_inference,
-    "_sfdp_pattern_2_training": _sfdp_pattern_2_training,
-    "_sfdp_pattern_2_inference": _sfdp_pattern_2_inference,
-    "_sfdp_pattern_3_training": _sfdp_pattern_3_training,
-    "_sfdp_pattern_3_inference": _sfdp_pattern_3_inference,
-    "_sfdp_pattern_4_training": _sfdp_pattern_4_training,
-    "_sfdp_pattern_4_inference": _sfdp_pattern_4_inference,
-    "_sfdp_pattern_5_training": _sfdp_pattern_5_training,
-    "_sfdp_pattern_5_inference": _sfdp_pattern_5_inference,
-    "_sfdp_pattern_6_training": _sfdp_pattern_6_training,
-    "_sfdp_pattern_6_inference": _sfdp_pattern_6_inference,
-    "_sfdp_pattern_7_training": _sfdp_pattern_7_training,
-    "_sfdp_pattern_7_inference": _sfdp_pattern_7_inference,
-    "_sfdp_pattern_8_training": _sfdp_pattern_8_training,
-    "_sfdp_pattern_8_inference": _sfdp_pattern_8_inference,
-    "_sfdp_pattern_9_training": _sfdp_pattern_9_training,
-    "_sfdp_pattern_9_inference": _sfdp_pattern_9_inference,
-    "_sfdp_pattern_10_training": _sfdp_pattern_10_training,
-    "_sfdp_pattern_10_inference": _sfdp_pattern_10_inference,
-    "_sfdp_pattern_11_training": _sfdp_pattern_11_training,
-    "_sfdp_pattern_11_inference": _sfdp_pattern_11_inference,
-    "_sfdp_pattern_12_training": _sfdp_pattern_12_training,
-    "_sfdp_pattern_12_inference": _sfdp_pattern_12_inference,
-    "_sfdp_pattern_13_training": _sfdp_pattern_13_training,
-    "_sfdp_pattern_13_inference": _sfdp_pattern_13_inference,
-    "_sfdp_pattern_14_training": _sfdp_pattern_14_training,
-    "_sfdp_pattern_14_inference": _sfdp_pattern_14_inference,
-    "_sfdp_pattern_15_training": _sfdp_pattern_15_training,
-    "_sfdp_pattern_15_inference": _sfdp_pattern_15_inference,
+    '_sfdp_pattern_1_training': _sfdp_pattern_1_training,
+    '_sfdp_pattern_1_inference': _sfdp_pattern_1_inference,
 }
 
 
 def get_serialized_pattern(key):
     import torch._inductor  # noqa: F401
     from torch._inductor import config
-
     if config.fallback_random:
         return None
 
