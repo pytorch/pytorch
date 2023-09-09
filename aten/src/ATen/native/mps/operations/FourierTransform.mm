7ff3e2e4a4c6f38a3cf99e1e227ead2fdecaffd2
sriege <friege45@gmail.com>
2023-09-08 18:06:13 -0700

    added the CachedGraph structure broke out the inputTensor definition from createFFTGraph, so that it would be accessible for updating CachedGraph adjusted the naming convention of the string prefix to “fft_[type]_mps” other small bug fixes

--- aten/src/ATen/native/mps/operations/FourierTransform.mm
+++ aten/src/ATen/native/mps/operations/FourierTransform.mm
@@ -1,8 +1,12 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/native/mps/OperationUtils.h>
 #include <MetalPerformanceShadersGraph/MPSGraphFourierTransformOps.h>
+#ifndef AT_PER_OPERATOR_HEADERS
 #include <ATen/Functions.h>
 #include <ATen/NativeFunctions.h>
+#else
+
+#endif
 
 namespace at::native {
 
@@ -12,8 +16,22 @@ enum class FFTType {
     C2C
 };
 
-MPSGraphTensor* createFFTGraph(MPSGraph* graph, const Tensor& tensor, int64_t signal_ndim, bool normalized, FFTType fftType, bool forward) {
-    MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(graph, getMPSDataType(tensor), getMPSShape(tensor));
+// Prototypes
+
+
+Tensor _fft_r2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes);
+Tensor& _fft_r2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes);
+Tensor _fft_c2r_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes);
+Tensor& _fft_c2r_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes);
+Tensor _fft_c2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes);
+Tensor& _fft_c2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes);
+static Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, const std::string& key_prefix, FFTType fftType, int64_t signal_ndim, bool normalized, bool forward);
+
+
+using namespace mps;
+
+
+static MPSGraphTensor* createFFTGraph(MPSGraph* graph, MPSGraphTensor* inputTensor, int64_t signal_ndim, bool normalized, FFTType fftType, bool forward) {
     
     MPSGraphFFTDescriptor* descriptor = [MPSGraphFFTDescriptor descriptor];
     descriptor.inverse = (fftType == FFTType::C2C) ? !forward : (fftType == FFTType::C2R);
@@ -29,8 +47,7 @@ MPSGraphTensor* createFFTGraph(MPSGraph* graph, const Tensor& tensor, int64_t si
     }
 }
 
-Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, const std::string& key_prefix, FFTType fftType, int64_t signal_ndim, bool normalized, bool forward) {
-    using namespace mps;
+static Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, const std::string& key_prefix, FFTType fftType, int64_t signal_ndim, bool normalized, bool forward) {
     
     TORCH_CHECK(self.is_mps(), key_prefix + ": Expected MPS tensor");
     if (out_provided) {
@@ -38,12 +55,23 @@ Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, con
     }
     
     MPSStream* stream = getCurrentMPSStream();
+
+    struct CachedGraph : public MPSCachedGraph {
+        CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
+        MPSGraphTensor* inputTensor_ = nil;
+        MPSGraphTensor* outputTensor_ = nil;
+    };
     
     @autoreleasepool {
+
         string key = key_prefix + getTensorsStringKey({self}) + getMPSTypeString(self);
         
-        auto cachedGraph = LookUpOrCreateCachedGraph<MPSCachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-            newCachedGraph->outputTensor_ = createFFTGraph(mpsGraph, self, signal_ndim, normalized, fftType, forward);
+        auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
+
+            MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, getMPSDataType(self), getMPSShape(self));
+            MPSGraphTensor* outputTensor = createFFTGraph(mpsGraph, inputTensor, signal_ndim, normalized, fftType, forward);
+            newCachedGraph->inputTensor_ = inputTensor;
+            newCachedGraph->outputTensor_ = outputTensor;
         });
         
         Placeholder inputPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
@@ -68,34 +96,34 @@ Tensor runFFTGraph(const Tensor& self, const Tensor& out, bool out_provided, con
 
 // Real-to-Hermitean FFT
 Tensor _fft_r2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    return runFFTGraph(self, {}, false, "r2c", FFTType::R2C, signal_ndim, normalized, true);
+    return runFFTGraph(self, {}, false, "fft_r2c_mps", FFTType::R2C, signal_ndim, normalized, true);
 }
 
 // Real-to-Hermitean FFT, writing result to the provided output tensor
 Tensor& _fft_r2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    runFFTGraph(self, out, true, "r2c_out", FFTType::R2C, signal_ndim, normalized, true);
+    runFFTGraph(self, out, true, "fft_r2c_out_mps", FFTType::R2C, signal_ndim, normalized, true);
     return out;
 }
 
 // Hermitean-to-Real FFT
 Tensor _fft_c2r_mps(const Tensor& self, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    return runFFTGraph(self, {}, false, "c2r", FFTType::C2R, signal_ndim, normalized, false);
+    return runFFTGraph(self, {}, false, "fft_c2r_mps", FFTType::C2R, signal_ndim, normalized, false);
 }
 
 // Hermitean-to-Real FFT, writing result to the provided output tensor
 Tensor& _fft_c2r_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, IntArrayRef signal_sizes) {
-    runFFTGraph(self, out, true, "c2r_out", FFTType::C2R, signal_ndim, normalized, false);
+    runFFTGraph(self, out, true, "fft_c2r_out_mps", FFTType::C2R, signal_ndim, normalized, false);
     return out;
 }
 
 // complex-to-complex FFT
 Tensor _fft_c2c_mps(const Tensor& self, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes) {
-    return runFFTGraph(self, {}, false, "c2c", FFTType::C2C, signal_ndim, normalized, forward);
+    return runFFTGraph(self, {}, false, "fft_c2c_mps", FFTType::C2C, signal_ndim, normalized, forward);
 }
 
 // complex-to-complex FFT, writing result to the provided output tensor
 Tensor& _fft_c2c_mps_out(const Tensor& self, Tensor& out, int64_t signal_ndim, bool normalized, bool forward, IntArrayRef signal_sizes) {
-    runFFTGraph(self, out, true, "c2c_out", FFTType::C2C, signal_ndim, normalized, forward);
+    runFFTGraph(self, out, true, "fft_c2c_out_mps", FFTType::C2C, signal_ndim, normalized, forward);
     return out;
 }
 
