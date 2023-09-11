9d5ea011c990f9d382ed43cff1464d15b884625f
David Radley <david_radley@uk.ibm.com>
2023-09-11 22:09:45 +0100

    correct test names

--- test/inductor/test_torchinductor_codegen_dynamic_shapes.py
+++ test/inductor/test_torchinductor_codegen_dynamic_shapes.py
@@ -148,13 +148,13 @@ test_failures = {
     #
     # Failed to find for loop/cpu kernel:
     #
-    "test_add_uint8_tensor": TestFailure(("cpu", "cuda")),
-    "test_add_int8_tensor": TestFailure(("cpu", "cuda")),
-    "test_add_int16_tensor": TestFailure(("cpu", "cuda")),
-    "test_multiply_uint8_tensor": TestFailure(("cpu", "cuda")),
-    "test_square_uint8_tensor_overflow": TestFailure(("cpu", "cuda")),
-    "test_multiply_uint8_tensor_overflow": TestFailure(("cpu", "cuda")),
-    "test_square_uint8_tensor": TestFailure(("cpu", "cuda")),
+    "test_add_uint8_tensor_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_add_int8_tensor_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_add_int16_tensor_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_multiply_uint8_tensor_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_square_uint8_tensor_overflow_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_multiply_uint8_tensor_overflow_dynamic_shapes": TestFailure(("cpu", "cuda")),
+    "test_square_uint8_tensor_dynamic_shapes": TestFailure(("cpu", "cuda")),
 
     #
     # Failed to find for loop/triton kernel:
