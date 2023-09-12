0f952287e79ce44efffcf76cbbe87cb7224734f7
Ying Zhang <yingz@meta.com>
2023-09-12 00:10:12 -0700

    [Inductor CUTLASS backend] Step 5: Gemm CUTLASS templates (#108015)
    This is the step 5 to add cutlass as an alternative inductor backend.
    
    Feature request: https://github.com/pytorch/pytorch/issues/106991.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108015
    Approved by: https://github.com/kadeng, https://github.com/jansel, https://github.com/aakhundov
    ghstack dependencies: #107802, #107847, #107901, #107931

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -764,6 +764,25 @@ def use_triton_template(layout, *, enable_int32=False):
     )
 
 
+def use_cutlass_template(layout):
+    from .codegen.cuda.cutlass_utils import try_import_cutlass
+
+    layout_dtypes = [torch.float16, torch.bfloat16, torch.float32]
+    res = _use_template_for_cuda(layout, layout_dtypes) and _use_autotune_backend(
+        "CUTLASS"
+    )
+
+    if res:
+        if not try_import_cutlass():
+            log.warning(
+                "Failed to import CUTLASS lib. Please check whether "
+                "_inductor.config.cuda.cutlass_dir is set correctly. "
+                "Skipping CUTLASS backend for now."
+            )
+            return False
+    return res
+
+
 def use_aten_gemm_kernels():
     return not use_max_autotune() or _use_autotune_backend("ATEN")
 
