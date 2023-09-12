d1aa4165aa9a78500d3f5fc69c816d99fd837090
pytorchbot <soumith+bot@pytorch.org>
2023-09-12 13:07:07 -0700

    fix tests

--- test/distributed/test_inductor_collectives.py
+++ test/distributed/test_inductor_collectives.py
@@ -294,7 +294,7 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
             .check("buf1_work = dist.all_reduce(buf1") \
             .check("fun_col_impl._register_tensor_work(buf1, buf1_work)") \
             .check("_wait_tensor(buf0)") \
-            .check("return (buf2, )") \
+            .check("return (buf3, )") \
             .run(code)
         correct = func(inputs, **self.get_world_trs())
         self.assertTrue(same(out, correct))
@@ -328,7 +328,7 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
             .check("_wait_tensor(buf1)") \
             .check("buf3 = buf1") \
             .check("buf4 = empty_strided") \
-            .check("return (buf3, buf4") \
+            .check("return (buf4, buf5") \
             .run(code)
         out = compiled(inputs, **self.get_world_trs())
         correct = func(inputs, **self.get_world_trs())
@@ -357,8 +357,8 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
         code = run_and_get_triton_code(compiled, inputs, **self.get_world_trs())
         FileCheck() \
             .check("buf0 = empty_strided(") \
-            .check("buf4 = empty_strided") \
-            .check("triton_poi__0.run(arg0_1, buf0, buf4") \
+            .check("buf5 = empty_strided") \
+            .check("triton_poi__0.run(arg0_1, buf0, buf5") \
             .check_not("copy_(") \
             .check("buf1 = buf0; del buf0  # reuse") \
             .check("buf2 = buf1") \
@@ -366,7 +366,7 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
             .check("fun_col_impl._register_tensor_work(buf2, buf2_work)") \
             .check("_wait_tensor(buf1)") \
             .check("buf3 = buf1") \
-            .check("return (buf3, buf4, buf5") \
+            .check("return (buf4, buf5, buf6") \
             .run(code)
         out = compiled(inputs, **self.get_world_trs())
         correct = func(inputs, **self.get_world_trs())
@@ -598,8 +598,8 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
         code = run_and_get_triton_code(compiled, inputs, **self.get_world_trs())
         FileCheck() \
             .check("buf0 = empty_strided(") \
-            .check("buf5 = empty_strided(") \
-            .check("triton_poi__0.run(arg0_1, buf0, buf5") \
+            .check("buf6 = empty_strided(") \
+            .check("triton_poi__0.run(arg0_1, buf0, buf6") \
             .check("buf1 = empty_strided") \
             .check("buf2 = empty_strided") \
             .check_not("copy_(") \
@@ -610,10 +610,10 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
             .check("fun_col_impl._register_tensor_work(buf3, buf3_work)") \
             .check("_wait_tensor(buf1)") \
             .check("buf4 = buf1") \
-            .check("buf6 = buf0; del buf0  # reuse") \
+            .check("buf5 = buf0; del buf0  # reuse") \
             .check("_wait_tensor(buf2)") \
-            .check("buf7 = buf2") \
-            .check("return (buf4, buf5, buf6, buf7") \
+            .check("buf8 = buf2") \
+            .check("return (buf5, buf6, buf7, buf9") \
             .run(code)
         out = compiled(inputs, **self.get_world_trs())
         correct = func(inputs, **self.get_world_trs())
@@ -643,8 +643,8 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
         code = run_and_get_triton_code(compiled, inputs, **self.get_world_trs())
         FileCheck() \
             .check("buf0 = empty_strided(") \
-            .check("buf5 = empty_strided(") \
-            .check("triton_poi__0.run(arg0_1, buf0, buf5") \
+            .check("buf6 = empty_strided(") \
+            .check("triton_poi__0.run(arg0_1, buf0, buf6") \
             .check("buf1 = empty_strided") \
             .check("buf2 = empty_strided") \
             .check_not("copy_(") \
@@ -654,10 +654,10 @@ class TestCollectivesInductor(DynamoDistributedSingleProcTestCase):
             .check("fun_col_impl._register_tensor_work(buf3, buf3_work)") \
             .check("_wait_tensor(buf1)") \
             .check("buf4 = buf1") \
-            .check("buf6 = buf0; del buf0  # reuse") \
+            .check("buf5 = buf0; del buf0  # reuse") \
             .check("_wait_tensor(buf2)") \
-            .check("buf7 = buf2") \
-            .check("return (buf4, buf5, buf6, buf7") \
+            .check("buf8 = buf2") \
+            .check("return (buf5, buf6, buf7, buf9") \
             .run(code)
         out = compiled(inputs, **self.get_world_trs())
         correct = func(inputs, **self.get_world_trs())
