c8e577bf409591910f9667a51f2cf92b3c5455e0
Peter Bell <peterbell10@live.co.uk>
2023-09-12 02:08:36 +0100

    [inductor] Lower masked_scatter on CUDA (#108803)
    This decomposes masked_scatter into `aten.cumsum` and a single pointwise kernel,
    which is similar to what is done in eager. I only do this for CUDA because on CPU
    it isn't split into two passes like this so would cause a slowdown.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108803
    Approved by: https://github.com/lezcano
    ghstack dependencies: #108802

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -2750,6 +2750,42 @@ def index_put_impl_(self, indices, values, accumulate, check):
     return self
 
 
+@register_lowering(
+    inductor_prims.masked_scatter_with_index, type_promotion_kind=None, broadcast=False
+)
+def masked_scatter_with_index(self, mask, source_idx, source):
+    self_flat, mask_flat, source_flat = (view(x, (-1,)) for x in (self, mask, source))
+
+    assert self.get_size() == mask.get_size()
+    assert mask.get_dtype() in {torch.bool, torch.uint8}
+
+    self_loader = self_flat.make_loader()
+    mask_loader = mask_flat.make_loader()
+    source_idx_loader = source_idx.make_loader()
+    source_loader = source_flat.make_loader()
+    source_numel = source.get_numel()
+
+    def inner_fn(idx):
+        self_val = self_loader(idx)
+        mask_val = ops.to_dtype(mask_loader(idx), torch.bool)
+
+        def load_source_val():
+            source_idx_val = source_idx_loader(idx)
+            i = ops.indirect_indexing(source_idx_val, source_numel)
+            return source_loader([i])
+
+        source_val = ops.masked(mask_val, load_source_val, 0)
+        return ops.where(mask_val, source_val, self_val)
+
+    result_flat = Pointwise.create(
+        device=self.get_device(),
+        dtype=self.get_dtype(),
+        inner_fn=inner_fn,
+        ranges=self_flat.get_size(),
+    )
+    return view(result_flat, self.get_size())
+
+
 @register_lowering(aten.as_strided_scatter, type_promotion_kind=None)
 def as_strided_scatter(self, src, size, stride, storage_offset=None):
     output = clone(self)
