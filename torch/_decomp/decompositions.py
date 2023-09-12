0e853b887430aac3b7eb8a4a358b5f30f447f1be
Hansong Zhang <hsz@fb.com>
2023-09-12 11:18:58 -0700

    Decomp div.Tensor_mode and trunc
    Differential Revision: D49198219

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -4014,6 +4014,11 @@ def scaled_dot_product_flash_attention(
     )
 
 
+@register_decomposition([aten.trunc])
+def trunc(self: Tensor) -> Tensor:
+    return self
+
+
 def register_inplace(aten_op, outplace_op):
     @register_decomposition(aten_op)
     def inplace_op(*args, **kwargs):
