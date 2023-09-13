4a09ed54590f4fe2eecc9ea69d043fe6f090f3d5
Sam Larsen <slarsen@meta.com>
2023-09-13 09:13:31 -0700

    [inductor] Parallelize Max Autotune step 2: Use multiple GPUs (#109127)
    Test Plan:
    `python test/inductor/test_max_autotune.py`
    `TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC=1 TORCHINDUCTOR_MAX_AUTOTUNE=1 python benchmarks/dynamo/torchbench.py --device cuda --performance --backend inductor --inference --only hf_Bart`
    `TORCHINDUCTOR_AUTOTUNE_MULTI_DEVICE=1 TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC=1 TORCHINDUCTOR_MAX_AUTOTUNE=1 python benchmarks/dynamo/torchbench.py --device cuda --performance --backend inductor --inference --only hf_Bart`
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109127
    Approved by: https://github.com/shunting314, https://github.com/eellison
    ghstack dependencies: #109126

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -234,7 +234,7 @@ class PersistentCache(CacheBase):
         choices: List[ChoiceCaller],
         name: str,
         inputs: str,
-        benchmark: Callable[[Any], float],
+        benchmark: Callable[[Any], Dict[ChoiceCaller, float]],
     ) -> Dict[ChoiceCaller, float]:
         """
         Check to see if we have benchmarked the given choice callers. For each
@@ -278,11 +278,13 @@ class PersistentCache(CacheBase):
             ):
                 try:
                     # re-benchmark everything to try to get consistent numbers from the same machine
-                    for choice in choices:
-                        timings[choice] = benchmark(choice)
-                        local_cache.setdefault(name, {})
-                        local_cache[name].setdefault(inputs, {})
-                        local_cache[name][inputs][choice.hash_key()] = timings[choice]
+                    timings = benchmark(choices)
+                    assert all(choice in timings for choice in choices)
+
+                    local_cache.setdefault(name, {})
+                    local_cache[name].setdefault(inputs, {})
+                    for choice, timing in timings.items():
+                        local_cache[name][inputs][choice.hash_key()] = timing
                 except RuntimeError as e:
                     # catch and log autotuning failures
                     log_errors(e)
