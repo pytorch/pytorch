167c70847d6e68232fe077cbdf489dff553c0d62
ydwu4 <ydwu2014@gmail.com>
2023-09-13 16:20:58 -0700

    [dynamo] Add BACKEND_MATCH guard to detect and recompile when backend changes (#107337)
    **Motivation:**
    We try to make torch.cond use torch.compile automatically so that we could error out when there is side-effects in the branches and correctly handle the closures.
    
    Before this PR, we have a warning if we don't turn on a config raise_on_backend_change (turning it on gives us an error) for the following code:
    ```python
    def foo()
    
    # Inside torch.cond, we'd like to do something like
    torch.compile(foo, backend="eager", fullgraph=True)(...)
    ...
    # Users may then call torch.compile somewhere else.
    # Dynamo will use the cached code of foo for "eager" backend
    # but we expect dynamo to recompile with "inductor" backend.
    torch.compile(foo, backend="inductor")(...)
    ```
    
    This PR adds a BACKEND_MATCH guard. Effectively, it implements a per-backend cache. In the above example, the cached code for "eager" won't work for "inductor" due to guard check failures and the second torch.compile will do a re-compilation. In the future, it might be useful to have something like a configuration guard that guards against dynamo configuration changes across different compiles (e.g. compile a function with fullgraph=False then compile it again with fullgraph=True).
    
    **Implementation:**
    1. We add a guarded_backend_cache and check the most_recent_backend against the backend associated with cached code. We also remove the raise_on_backend_change flag.
    
    Note: More lines are printed for debug log due to newly added context manager and guard adds .
    
    **Test Plan:**
    Removed original tests that raise on different backend and add a new test to test whether the BACKEND_MATCH guard can guard against backend change.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107337
    Approved by: https://github.com/jansel

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -60,7 +60,7 @@ else:
         globals()[name] = getattr(torch._C._dynamo.eval_frame, name)
 
 from . import config, convert_frame, external_utils, skipfiles, utils
-from .exc import CondOpArgsMismatchError, ResetRequired, UserError, UserErrorType
+from .exc import CondOpArgsMismatchError, UserError, UserErrorType
 from .mutation_guard import install_generation_tagging_init
 from .types import DynamoCallback
 from .utils import compile_times
@@ -85,7 +85,62 @@ class Unset(Enum):
 unset = Unset.token
 
 compile_lock = threading.RLock()
-most_recent_backend: Optional[CompilerFn] = None
+guarded_backend_cache = threading.local()
+
+
+def _maybe_init_guarded_backend_cache():
+    if not hasattr(guarded_backend_cache, "skip_backend_check_for_run_only_mode"):
+        guarded_backend_cache.skip_backend_check_for_run_only_mode = False
+    if not hasattr(guarded_backend_cache, "current_backend"):
+        guarded_backend_cache.current_backend = None
+    if not hasattr(guarded_backend_cache, "cached_backends"):
+        guarded_backend_cache.cached_backends = {}
+
+
+def _reset_guarded_backend_cache():
+    _maybe_init_guarded_backend_cache()
+    guarded_backend_cache.skip_backend_check_for_run_only_mode = False
+    guarded_backend_cache.current_backend = None
+    cached_backends = guarded_backend_cache.cached_backends
+    for backend in cached_backends.values():
+        if hasattr(backend, "reset"):
+            backend.reset()
+    cached_backends.clear()
+    guarded_backend_cache.cached_backends = {}
+
+
+@contextlib.contextmanager
+def backend_cache_wrapper(callback: CompilerFn):
+    _maybe_init_guarded_backend_cache()
+
+    # callback is False for RunOnlyContext. RunOnlyContext is used
+    # as a way to re-use the previous compiled cache.
+    # We therefore skip the check and re-use whatever code that's already cached.
+    # Note: the cache that's actually used depends on the caching policy.
+    if callback is False:
+        try:
+            prev_skip = guarded_backend_cache.skip_backend_check_for_run_only_mode
+            guarded_backend_cache.skip_backend_check_for_run_only_mode = True
+            yield None
+        finally:
+            guarded_backend_cache.skip_backend_check_for_run_only_mode = prev_skip
+    else:
+        backend = innermost_fn(callback)
+
+        def _set_current_backend(backend: CompilerFn):
+            prev_backend = guarded_backend_cache.current_backend
+            guarded_backend_cache.current_backend = backend
+            # Mapping id of a CompilerFn to itself
+            guarded_backend_cache.cached_backends[id(backend)] = backend
+            return prev_backend
+
+        prev_backend = _set_current_backend(backend)
+        try:
+            yield backend
+        finally:
+            _set_current_backend(prev_backend)
+
+
 DONT_WRAP_FILES = {
     # For tracing into fx modules
     inspect.getsourcefile(GraphModule),
@@ -260,6 +315,8 @@ class _TorchDynamoContext:
             )
         self.on_enter()
         self.prior = set_eval_frame(self.callback)
+        self.backend_cache_manager = backend_cache_wrapper(self.callback)
+        self.backend_cache_manager.__enter__()
         self.backend_ctx = self.extra_ctx_ctor()
         self.backend_ctx.__enter__()
         self.dynamic_ctx = enable_dynamic(self.dynamic, self.export)
@@ -272,6 +329,7 @@ class _TorchDynamoContext:
         # TODO: This is totally not the right way to chain contexts manually
         self.dynamic_ctx.__exit__(exc_type, exc_val, exc_tb)
         self.backend_ctx.__exit__(exc_type, exc_val, exc_tb)
+        self.backend_cache_manager.__exit__(exc_type, exc_val, exc_tb)
 
     def __call__(self, fn):
         # public api for compiler config/options
@@ -329,6 +387,8 @@ class _TorchDynamoContext:
 
             on_enter()
             prior = set_eval_frame(callback)
+            backend_cache_manager = backend_cache_wrapper(self.callback)
+            backend_cache_manager.__enter__()
             backend_ctx = backend_ctx_ctor()
             backend_ctx.__enter__()
             dynamic_ctx = enable_dynamic(self.dynamic, self.export)
@@ -339,6 +399,7 @@ class _TorchDynamoContext:
                 set_eval_frame(prior)
                 dynamic_ctx.__exit__(None, None, None)
                 backend_ctx.__exit__(None, None, None)
+                backend_cache_manager.__exit__(None, None, None)
 
         # hooks to properly handle inlining
         if isinstance(self, DisableContext):
@@ -398,10 +459,6 @@ class _TorchDynamoContext:
 
 
 class OptimizeContext(_TorchDynamoContext):
-    @staticmethod
-    def _different_backend(old, new):
-        return not (old == new or old is None)
-
     def __init__(
         self,
         callback,
@@ -413,19 +470,8 @@ class OptimizeContext(_TorchDynamoContext):
         compiler_config=None,
     ):
         def on_enter():
-            global most_recent_backend
-            if OptimizeContext._different_backend(most_recent_backend, compiler_fn):
-                if config.raise_on_backend_change:
-                    raise ResetRequired()
-                else:
-                    warnings.warn(
-                        "changing options to `torch.compile()` may require "
-                        "calling `torch._dynamo.reset()` to take effect"
-                    )
-            most_recent_backend = compiler_fn
             install_generation_tagging_init()
 
-        compiler_fn = innermost_fn(callback)
         super().__init__(
             callback=callback,
             on_enter=on_enter,
@@ -657,14 +703,13 @@ def explain(f, *extra_args, **extra_kwargs):
             nonlocal out_guards
             out_guards.extend(guards)
 
-        with patch(f"{__name__}.most_recent_backend", None):
-            opt_f = optimize(
-                dynamo_graph_accumulating_compiler,
-                nopython=False,
-                guard_export_fn=guard_export_print,
-            )(f)
-            # TODO(voz): We may have instances of `f` that mutate inputs, we should track sideffects and reject.
-            opt_f(*args, **kwargs)
+        opt_f = optimize(
+            dynamo_graph_accumulating_compiler,
+            nopython=False,
+            guard_export_fn=guard_export_print,
+        )(f)
+        # TODO(voz): We may have instances of `f` that mutate inputs, we should track sideffects and reject.
+        opt_f(*args, **kwargs)
 
         graph_count = len(graphs)
 
@@ -1141,7 +1186,7 @@ def export(
         constraint_violation_error = None
         if tracing_mode != "symbolic":
             assume_static_by_default = True
-        with patch(f"{__name__}.most_recent_backend", None), config.patch(
+        with config.patch(
             specialize_int=True,
             assume_static_by_default=assume_static_by_default,
             automatic_dynamic_shapes=False,
