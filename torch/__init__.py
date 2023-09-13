c37412f9547b615ae5be23fef4695d60d64c85fe
soulitzer <soulitzer@gmail.com>
2023-09-13 00:03:43 -0400

    Make singleton SymInt hashable
    [ghstack-poisoned]

--- torch/__init__.py
+++ torch/__init__.py
@@ -289,6 +289,12 @@ class SymInt:
     def __repr__(self):
         return str(self.node)
 
+    def __hash__(self) -> builtins.int:
+        if self.node.is_singleton_int():
+            return hash(self.node)
+        else:
+            raise TypeError("unhashable type: non-singleton SymInt")
+
 class SymFloat:
     """
     Like an float (including magic methods), but redirects all operations on the
