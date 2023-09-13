100f3cfa0174ce36b722ec70d923b3d2b8c292c3
Aleksandar Samardžić <asamardzic@quansight.com>
2023-09-13 09:08:56 +0000

    Update on "Add CUTLASS-based support for mixed dtypes matrix multiplication"
    cc ptrblck
    
    [ghstack-poisoned]

--- test/test_matmul_cuda.py
+++ test/test_matmul_cuda.py
@@ -255,39 +255,54 @@ class TestMatmulMixedDtypesCuda(TestCase):
             assert ncols % 64 == 0
 
             # subbyte_transpose
-            tmp = inp.T.contiguous()
+            tmp = inp.T
 
             # permute_B_rows_for_mixed_gemm
             # (permute cols actually, as transpose is applied first here)
-            outp = torch.empty_like(tmp)
-            outp[:,0::16] = tmp[:,0::16]
-            outp[:,1::16] = tmp[:,1::16]
-            outp[:,2::16] = tmp[:,8::16]
-            outp[:,3::16] = tmp[:,9::16]
-            outp[:,4::16] = tmp[:,2::16]
-            outp[:,5::16] = tmp[:,3::16]
-            outp[:,6::16] = tmp[:,10::16]
-            outp[:,7::16] = tmp[:,11::16]
-            outp[:,8::16] = tmp[:,4::16]
-            outp[:,9::16] = tmp[:,5::16]
-            outp[:,10::16] = tmp[:,12::16]
-            outp[:,11::16] = tmp[:,13::16]
-            outp[:,12::16] = tmp[:,6::16]
-            outp[:,13::16] = tmp[:,7::16]
-            outp[:,14::16] = tmp[:,14::16]
-            outp[:,15::16] = tmp[:,15::16]
-
-            # make sure the output is contiguous, for reorderings that follow
-            outp = outp.contiguous()
+            cols_permuted = (
+                torch.tensor(
+                    [0, 1, 4, 5, 8, 9, 12, 13, 2, 3, 6, 7, 10, 11, 14, 15],
+                    device=device,
+                ).expand(nrows // 16, 16)
+                + (
+                    torch.arange(0, nrows // 16, device=device).reshape(-1, 1) * 16
+                ).expand(nrows // 16, 16)
+            ).view(-1)
+            outp = tmp.index_copy(1, cols_permuted, tmp)
 
             # interleave_column_major_tensor
             magic0 = 2
             magic1 = 16
 
-            tmp0 = (torch.arange(0, ncols // magic0, device=device) * (nrows // 4 * magic0)).view(-1, 1).repeat(1, nrows // 4 * magic0).view(-1)
-            tmp1 = (torch.arange(0, nrows // 4 // magic1, device=device) * (magic0 * magic1)).view(-1, 1).repeat(1, magic1).view(-1).repeat(ncols)
-            tmp2 = (torch.arange(0, magic0, device=device) * magic1).view(-1, 1).repeat(1, nrows // 4).view(-1).repeat(ncols // magic0)
-            tmp3 = torch.arange(0, magic1, device=device).repeat(nrows // 4 * ncols // magic1)
+            tmp0 = (
+                (
+                    torch.arange(0, ncols // magic0, device=device)
+                    * (nrows // 4 * magic0)
+                )
+                .view(-1, 1)
+                .repeat(1, nrows // 4 * magic0)
+                .view(-1)
+            )
+            tmp1 = (
+                (
+                    torch.arange(0, nrows // 4 // magic1, device=device)
+                    * (magic0 * magic1)
+                )
+                .view(-1, 1)
+                .repeat(1, magic1)
+                .view(-1)
+                .repeat(ncols)
+            )
+            tmp2 = (
+                (torch.arange(0, magic0, device=device) * magic1)
+                .view(-1, 1)
+                .repeat(1, nrows // 4)
+                .view(-1)
+                .repeat(ncols // magic0)
+            )
+            tmp3 = torch.arange(0, magic1, device=device).repeat(
+                nrows // 4 * ncols // magic1
+            )
 
             outp_offsets = tmp0 + tmp1 + tmp2 + tmp3
 
@@ -314,21 +329,29 @@ class TestMatmulMixedDtypesCuda(TestCase):
             input = make_tensor(
                 *batch_shape, m, k, low=val_lo, high=val_hi, dtype=dtype, device=device
             )
-            weight = make_tensor(k, n, low=valq_lo, high=valq_hi, dtype=torch.int8, device=device)
+            weight = make_tensor(
+                k, n, low=valq_lo, high=valq_hi, dtype=torch.int8, device=device
+            )
             scale = make_tensor(
                 (n,), low=val_lo, high=val_hi, dtype=input.dtype, device=device
             )
             bias = make_tensor(
-                (n,), low=val_lo, high=val_hi, dtype=
-                input.dtype, device=device
+                (n,), low=val_lo, high=val_hi, dtype=input.dtype, device=device
             )
 
             input_ref = input.reshape(-1, input.shape[-1])
             weight_ref = weight.to(input.dtype) * scale.expand(1, n)
             bias_ref = bias.expand(1, n)
-            output_ref = torch.addmm(bias_ref, input_ref, weight_ref).reshape(*input.shape[:-1], weight.shape[1])
+            output_ref = torch.addmm(bias_ref, input_ref, weight_ref).reshape(
+                *input.shape[:-1], weight.shape[1]
+            )
 
-            output = torch.ops.aten._fp16_uint8_mm(input, preprocess_weights_for_mixed_gemm(weight).view(torch.uint8), scale, bias)
+            output = torch.ops.aten._fp16_uint8_mm(
+                input,
+                preprocess_weights_for_mixed_gemm(weight).view(torch.uint8),
+                scale,
+                bias,
+            )
 
             torch.testing.assert_close(output, output_ref, rtol=1e-3, atol=0)
 
