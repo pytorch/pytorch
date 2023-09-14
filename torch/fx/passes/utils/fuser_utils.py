bf5622e9650c8dab77ccabc2fffe600bbdfc0b5b
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-09-14 22:43:03 +0000

    Revert "split by tag (#108892)"
    This reverts commit 89b6276be9f1b04491625cc0d05de01c15f75597.
    
    Reverted https://github.com/pytorch/pytorch/pull/108892 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/108892#issuecomment-1720249148))

--- torch/fx/passes/utils/fuser_utils.py
+++ torch/fx/passes/utils/fuser_utils.py
@@ -173,8 +173,8 @@ def fuse_as_graphmodule(gm: GraphModule,
 
     # lint to ensure correctness
     subgraph.lint()
-    fused_gm: GraphModule
-    fused_gm, _ = lift_subgraph_as_module(gm, subgraph, comp_name="", class_name=module_name)
+
+    fused_gm: GraphModule = lift_subgraph_as_module(gm, subgraph, class_name=module_name)
 
     # sub_gm's input nodes in the original module
     original_inputs: Tuple[Node, ...] = tuple(node_to_placeholder.keys())
