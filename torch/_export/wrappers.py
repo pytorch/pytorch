492bafbd98c639cc94a081349fecbdf9ff2c25fd
Brian Hirsh <hirsheybar@fb.com>
2023-09-15 09:58:23 -0700

    Update base for Update on "[not ready for review yet], AOTDispatch subclass"
    This is a PoC of AOTDispatch support. This PR actually works on basic examples, and I'm working on testing it out on `DTensor` (with wanchaol), `SemiStructuredSparsityTensor` (with jcaip), and `FP8Tensor`.
    
    There are some design decisions baked into the PR that I think we need consensus on though - so I'm planning on writing a larger design doc to go over the changes.
    
    
    
    
    [ghstack-poisoned]

--- torch/_export/wrappers.py
+++ torch/_export/wrappers.py
@@ -2,15 +2,9 @@ from contextlib import contextmanager
 
 import torch
 import torch._custom_ops
-from torch._C import _ExcludeDispatchKeyGuard, DispatchKey, DispatchKeySet
-from torch._functorch.eager_transforms import _unwrap_all_tensors_from_functional
+from torch._C import DispatchKey
 from torch._ops import HigherOrderOperator
 from torch._subclasses.fake_tensor import FakeTensorMode
-from torch._subclasses.functional_tensor import (
-    FunctionalTensor,
-    FunctionalTensorMode,
-    unset_functional_temporarily,
-)
 from torch.fx.experimental.proxy_tensor import ProxyTorchDispatchMode, track_tensor_tree
 from torch.utils import _pytree as pytree
 from torch.utils._python_dispatch import (
@@ -49,30 +43,14 @@ def export_tracepoint_fake_tensor_mode(*args, **kwargs):
     return args
 
 
-@_export_tracepoint.py_impl(FunctionalTensorMode)
-def export_tracepoint_functional_tensor_mode(*args, **kwargs):
-    from torch._functorch.aot_autograd import from_fun, to_fun
+@_export_tracepoint.py_functionalize_impl()
+def export_tracepoint_functional_(ctx, *args, **kwargs):
+    unwrapped_args = ctx.unwrap(args)
+    unwrapped_kwargs = ctx.unwrap(kwargs)
 
-    unwrapped_args = pytree.tree_map_only(FunctionalTensor, from_fun, args)
-    unwrapped_kwargs = pytree.tree_map_only(FunctionalTensor, from_fun, kwargs)
-
-
-    with unset_functional_temporarily():
+    with ctx.redispatch():
         out = _export_tracepoint(*unwrapped_args, **unwrapped_kwargs)
-        return pytree.tree_map_only(torch.Tensor, to_fun, out)
-
-
-@_export_tracepoint.py_impl(DispatchKey.Functionalize)
-def export_tracepoint_functionalize(*args, **kwargs):
-    reapply_views = torch._C._functionalization_reapply_views_tls()
-    unwrapped_args = _unwrap_all_tensors_from_functional(
-        args, reapply_views=reapply_views
-    )
-    unwrapped_kwargs = _unwrap_all_tensors_from_functional(
-        kwargs, reapply_views=reapply_views
-    )
-    with _ExcludeDispatchKeyGuard(DispatchKeySet(DispatchKey.Functionalize)):
-        return _export_tracepoint(*unwrapped_args, **unwrapped_kwargs)
+        return ctx.wrap(out)
 
 
 @_export_tracepoint.py_impl(DispatchKey.CPU)
