c8bb6d3cfc1355978ee71e0e5fb23ad21001afa4
Rockerz <64583161+rajveer43@users.noreply.github.com>
2023-09-16 18:49:23 +0530

    Update AdaptiveMaxPooling2d.cpp

--- aten/src/ATen/native/AdaptiveMaxPooling2d.cpp
+++ aten/src/ATen/native/AdaptiveMaxPooling2d.cpp
@@ -11,6 +11,53 @@
 #include <ATen/ops/adaptive_max_pool2d_native.h>
 #endif
 
+/**
+ * Adaptive Max Pooling 2D
+ *
+ * This function performs adaptive max pooling on a 2D input tensor. Adaptive max pooling allows you to specify the output size
+ * instead of a fixed kernel size. It divides the input into non-overlapping regions and computes the maximum value in each
+ * region to create the output.
+ *
+ * Input:
+ *   - `input`: The input tensor of shape (B, C, H, W), where B is the batch size, C is the number of channels, and H and W
+ *     are the input height and width.
+ *
+ * Output:
+ *   - The output tensor of shape (B, C, oH, oW), where oH and oW are the output height and width specified by `output_size`.
+ *
+ * Parameters:
+ *   - `output_size`: A tuple (oH, oW) specifying the output size. oH and oW must be integers greater than 0 and less than or
+ *     equal to H and W, respectively.
+ *
+ * Behavior:
+ *   - If the input shape is divisible by the output size (H % oH == 0 and W % oW == 0), the function divides the input into
+ *     non-overlapping regions of size (H/oH) x (W/oW) and computes the maximum value in each region to produce the output.
+ *   - If the input shape is not divisible by the output size, the function adjusts the region sizes to ensure that each output
+ *     element corresponds to the maximum value over one or more input elements. Specifically, it divides the input into regions
+ *     of size ceil(H/oH) x ceil(W/oW) and computes the maximum value in each region.
+ *
+ * Example:
+ *   - Input tensor: [B=1, C=1, H=4, W=4]
+ *   - Output size: (oH=2, oW=2)
+ *   - Output tensor: [B=1, C=1, oH=2, oW=2]
+ *   - Calculation:
+ *     - Region 1 (2x2): Max value is taken from [1, 2, 5, 6] => Output element [0, 0] = 6
+ *     - Region 2 (2x2): Max value is taken from [2, 3, 6, 7] => Output element [0, 1] = 7
+ *     - Region 3 (2x2): Max value is taken from [9, 10, 13, 14] => Output element [1, 0] = 14
+ *     - Region 4 (2x2): Max value is taken from [10, 11, 14, 15] => Output element [1, 1] = 15
+ *
+ * Notes:
+ *   - The function adapts to input and output sizes, ensuring that each output element represents the maximum value over one
+ *     or more input elements.
+ *   - This function works for both CPU and GPU (CUDA) backends.
+ *   - It supports batched inputs (multiple samples in the batch).
+ *
+ * @param input The input tensor.
+ * @param output_size The desired output size.
+ * @return The output tensor.
+ */
+
+
 namespace at {
 namespace meta {
 TORCH_META_FUNC(adaptive_max_pool2d) (const Tensor& input, IntArrayRef output_size) {
