ad7428633f3bf93310c06c2f0854518ea4d6a9f9
Edward Z. Yang <ezyang@meta.com>
2023-09-17 15:02:00 -0400

    Revert "[inductor] Fix inputs with existing offsets (#108168)"
    This reverts commit 2c87ef3dbfe1e5bd9ee4b6a9e4aa5030dc714284.
    
    [ghstack-poisoned]

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1873,10 +1873,12 @@ class ReinterpretView(BaseView):
         size = V.graph.wrapper_code.codegen_shape_tuple(self.layout.size)
         stride = V.graph.wrapper_code.codegen_shape_tuple(self.layout.stride)
         offset = V.graph.wrapper_code.codegen_sizevar(self.layout.offset)
-        # reinterpret_tensor is similar to as_strided except:
-        # - offset is added to the existing offset (rather than replacing it)
-        # - view tracking is disabled similar to unsafe_view
-        return f"reinterpret_tensor({self.get_name()}, {size}, {stride}, {offset})"
+        namespace = V.graph.wrapper_code.namespace
+        if offset != "0":
+            return (
+                f"{namespace}as_strided({self.get_name()}, {size}, {stride}, {offset})"
+            )
+        return f"{namespace}as_strided({self.get_name()}, {size}, {stride})"
 
 
 class SliceView(View):
