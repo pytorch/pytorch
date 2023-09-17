a8d5ece61ff24df0e366405261db5ab81e661595
arunppsg <arunppsg@gmail.com>
2023-09-17 18:51:18 +0530

    lint fix

--- aten/src/ATen/native/mps/operations/Lerp.mm
+++ aten/src/ATen/native/mps/operations/Lerp.mm
@@ -1,7 +1,7 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
-#include <ATen/native/Lerp.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/mps/MPSProfiler.h>
+#include <ATen/native/Lerp.h>
 #include <ATen/native/mps/OperationUtils.h>
 
 #ifndef AT_PER_OPERATOR_HEADERS
