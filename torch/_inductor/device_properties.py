0f77ea9816c048e2ecfed7dd44f345f50119af07
Yu, Guangye <guangye.yu@intel.com>
2023-09-18 17:24:29 +0000

    generalize inductor utils device agnostic

--- torch/_inductor/device_properties.py
+++ torch/_inductor/device_properties.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict
 
 import torch
 from torch._dynamo.runtime import get_registered_device_runtimes, get_runtime_for_device
