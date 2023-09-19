pytorch/pytorch

commit 25d5077ac736fa0947a94d2f1039ac512076816c

[inductor] Add a C shim layer for libtorch (#109391)
Summary:

This PR adds a limited C shim layer for libtorch. The ultimate goal is to ban any direct reference to aten/c10 data structures or functions, to avoid ABI breakage by providing stable C interfaces.

To make the review and landing easier, we broke the changes into several steps. In this PR (a combination of https://github.com/pytorch/pytorch/pull/109022 and https://github.com/pytorch/pytorch/pull/109351), we add C interfaces for certain libtorch functions and modify the wrapper codegen to generate calls to those interfaces. There are a few other items to be addressed in future PRs:

* The AOTInductor runtime interface still takes lists of aten tensors as input and output
* The interaction with ProxyExecutor (general fallback support) needs to move away from aten tensor
* Remove all references to aten/c10 headers in the AOTInductor-generated code

Reviewed By: chenyang78

Differential Revision: D49302669

torch/_inductor/ir.py
@@ -1879,12 +1879,16 @@ class ReinterpretView(BaseView):
     def freeze_layout(self):
         pass
 
-    def codegen_reference(self):
+    def codegen_reference(self, writer=None):
         # reinterpret_tensor is similar to as_strided except:
         # - offset is added to the existing offset (rather than replacing it)
         # - view tracking is disabled similar to unsafe_view
         return V.graph.wrapper_code.codegen_reinterpret_view(
-            self.get_name(), self.layout.size, self.layout.stride, self.layout.offset
+            self.get_name(),
+            self.layout.size,
+            self.layout.stride,
+            self.layout.offset,
+            writer,
         )
 
 
@@ -2406,8 +2410,12 @@ class Buffer(IRNode):
     def is_no_op(self):
         return False
 
-    def codegen_reference(self):
-        return self.get_name()
+    def codegen_reference(self, writer=None):
+        return (
+            self.get_name() + ".get()"
+            if V.graph.cpp_wrapper and config.aot_inductor.abi_compatible
+            else self.get_name()
+        )
 
     def decide_layout(self):
         pass
@@ -2464,7 +2472,7 @@ class ConstantBuffer(InputBuffer):
 
 
 class NoneAsConstantBuffer(IRNode):
-    def codegen_reference(self):
+    def codegen_reference(self, writer=None):
         return V.graph.wrapper_code.none_str
 
 
@@ -2473,7 +2481,7 @@ class ShapeAsConstantBuffer(IRNode):
         super().__init__()
         self.shape = shape
 
-    def codegen_reference(self):
+    def codegen_reference(self, writer=None):
         expr = V.graph.wrapper_code.expr_printer(V.graph.sizevars.simplify(self.shape))
         if V.graph.cpp_wrapper:
             # wrap scalar to 0-d tensor for cpp wrapper
@@ -3370,9 +3378,7 @@ class ExternKernelAlloc(ExternKernel):
     def codegen(self, wrapper):
         self.codegen_comment(wrapper)
         args = [*self.codegen_args(), *self.codegen_kwargs()]
-        V.graph.wrapper_code.generate_extern_kernel_alloc(
-            self.get_name(), self.kernel, args, self.get_origin_node()
-        )
+        V.graph.wrapper_code.generate_extern_kernel_alloc(self, args)
         if isinstance(self.layout, Layout):
             self.codegen_size_asserts(wrapper)
 

build_variables.bzl
@@ -500,6 +500,7 @@ lazy_tensor_core_python_sources = [
 ]
 
 inductor_core_resources = [
+    "torch/csrc/inductor/aoti_torch/shim_common.cpp",
     "torch/csrc/inductor/inductor_ops.cpp",
 ]
 
@@ -683,6 +684,7 @@ libtorch_cuda_core_sources = [
     "torch/csrc/CudaIPCTypes.cpp",
     "torch/csrc/cuda/comm.cpp",
     "torch/csrc/cuda/memory_snapshot.cpp",
+    "torch/csrc/inductor/aoti_torch/shim_cuda.cpp",
     "torch/csrc/jit/codegen/fuser/cuda/fused_kernel.cpp",
     "torch/csrc/profiler/stubs/cuda.cpp",
     "torch/csrc/autograd/functions/comm.cpp",

setup.py
@@ -1293,6 +1293,7 @@ def main():
         "include/torch/csrc/dynamo/*.h",
         "include/torch/csrc/inductor/*.h",
         "include/torch/csrc/inductor/aot_runtime/*.h",
+        "include/torch/csrc/inductor/aoti_torch/c/*.h",
         "include/torch/csrc/jit/*.h",
         "include/torch/csrc/jit/backends/*.h",
         "include/torch/csrc/jit/generated/*.h",

test/inductor/test_aot_inductor.py
@@ -9,6 +9,7 @@ import torch._inductor
 
 import torch.fx._pytree as fx_pytree
 from torch._dynamo.testing import same
+from torch._inductor import config
 from torch._inductor.utils import aot_inductor_launcher
 
 from torch.testing._internal.common_utils import IS_FBCODE, TEST_WITH_ROCM, TestCase
@@ -17,6 +18,16 @@ from torch.utils import _pytree as pytree
 
 aten = torch.ops.aten
 requires_cuda = functools.partial(unittest.skipIf, not HAS_CUDA, "requires cuda")
+
+try:
+    try:
+        from . import test_torchinductor
+    except ImportError:
+        import test_torchinductor
+except unittest.SkipTest:
+    if __name__ == "__main__":
+        sys.exit(0)
+    raise
 
 
 class AOTInductorModelRunner:
@@ -104,8 +115,40 @@ class AOTInductorModelRunner:
         return list_output_tensors
 
 
+def check_model(
+    self: TestCase,
+    model,
+    example_inputs,
+    options=None,
+    constraints=None,
+):
+    expected = model(*example_inputs)
+    with config.patch("aot_inductor.abi_compatible", self.abi_compatible):
+        actual = AOTInductorModelRunner.run(
+            model, example_inputs, expected, options, constraints
+        )
+    self.assertTrue(same(actual, expected))
+
+
+def check_model_with_multiple_inputs(
+    self: TestCase,
+    model,
+    list_example_inputs,
+    options=None,
+    constraints=None,
+):
+    list_expected = [
+        (model(*example_inputs),) for example_inputs in list_example_inputs
+    ]
+    with config.patch("aot_inductor.abi_compatible", self.abi_compatible):
+        list_actual = AOTInductorModelRunner.run_multiple(
+            model, list_example_inputs, list_expected, options, constraints
+        )
+    self.assertTrue(same(list_actual, list_expected))
+
+
 @unittest.skipIf(IS_FBCODE, "cpp extension doesn't work in fbcode CI")
-class AotInductorTests(TestCase):
+class AOTInductorTestsTemplate:
     def test_simple(self):
         class Repro(torch.nn.Module):
             def __init__(self):
@@ -115,14 +158,11 @@ class AotInductorTests(TestCase):
             def forward(self, x, y):
                 return x + torch.nn.functional.linear(y, self.weight)
 
-        model = Repro()
         example_inputs = (
             torch.randn(10, 10, device="cuda"),
             torch.randn(10, 10, device="cuda"),
         )
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(model, example_inputs, expected)
-        self.assertTrue(same(actual, expected))
+        self.check_model(Repro(), example_inputs)
 
     def test_large(self):
         class Repro(torch.nn.Module):
@@ -133,14 +173,11 @@ class AotInductorTests(TestCase):
             def forward(self, x, y):
                 return x + torch.nn.functional.linear(y, self.weight)
 
-        model = Repro()
         example_inputs = (
             torch.randn(1, 250112, device="cuda"),
             torch.randn(1, 512, device="cuda"),
         )
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(model, example_inputs, expected)
-        self.assertTrue(same(actual, expected))
+        self.check_model(Repro(), example_inputs)
 
     def test_with_offset(self):
         class Repro(torch.nn.Module):
@@ -156,14 +193,11 @@ class AotInductorTests(TestCase):
                     + self.tensor
                 )
 
-        model = Repro()
         example_inputs = (
             torch.randn(10, 10, device="cuda"),
             torch.randn(10, 10, device="cuda"),
         )
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(model, example_inputs, expected)
-        self.assertTrue(same(actual, expected))
+        self.check_model(Repro(), example_inputs)
 
     def test_missing_output(self):
         class Repro(torch.nn.Module):
@@ -176,14 +210,11 @@ class AotInductorTests(TestCase):
                 c = torch.cos(b)
                 return c
 
-        model = Repro()
         example_inputs = (
             torch.randn(10, 10, device="cuda"),
             torch.randn(10, 10, device="cuda"),
         )
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(model, example_inputs, expected)
-        self.assertTrue(same(actual, expected))
+        self.check_model(Repro(), example_inputs)
 
     def test_output_misaligned(self):
         class Repro(torch.nn.Module):
@@ -199,14 +230,11 @@ class AotInductorTests(TestCase):
                 x_sigmoid = torch.sigmoid(x_getitem)
                 return x_sigmoid, y_getitem
 
-        model = Repro()
         example_inputs = (
             torch.randn(10, 10, device="cuda"),
             torch.randn(10, 10, device="cuda"),
         )
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(model, example_inputs, expected)
-        self.assertTrue(same(actual, expected))
+        self.check_model(Repro(), example_inputs)
 
     def test_dynamic_smem_above_default_limit(self):
         class Repro(torch.nn.Module):
@@ -254,6 +282,46 @@ class AotInductorTests(TestCase):
         actual = AOTInductorModelRunner.run(model, example_inputs, expected)
         self.assertTrue(same(actual, expected))
 
+    def test_aliased_buffer_reuse(self):
+        class Repro(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x, y):
+                x = 2 * x
+                y = 2 * y
+                c = torch.cat([x, y], dim=-1)
+                d = 1 + c
+                m = torch.mm(d, d)
+                return m[:, :2] + x
+
+        example_inputs = (
+            torch.randn(4, 2, device="cuda"),
+            torch.randn(4, 2, device="cuda"),
+        )
+        self.check_model(Repro(), example_inputs)
+
+    def test_buffer_reuse(self):
+        class Repro(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x, y):
+                a = torch.sin(x)
+                b = torch.cos(y)
+                c = torch.mm(a, b)
+                d = torch.relu(c)
+                e = torch.sigmoid(d)
+                f = torch.mm(x, y)
+                g = e + f
+                return g
+
+        example_inputs = (
+            torch.randn(4, 4, device="cuda"),
+            torch.randn(4, 4, device="cuda"),
+        )
+        self.check_model(Repro(), example_inputs)
+
     def test_duplicated_params(self):
         class Model(torch.nn.Module):
             def __init__(self):
@@ -279,7 +347,6 @@ class AotInductorTests(TestCase):
                 add_0 = x + y
                 return torch.nn.functional.relu(input=add_0, inplace=False)
 
-        model = Model()
         a = torch.randn(128, 2048, device="cuda")
         b = torch.randn(128, 2048, device="cuda")
         constraints = [
@@ -288,11 +355,7 @@ class AotInductorTests(TestCase):
             torch._export.dynamic_dim(a, 0) == torch._export.dynamic_dim(b, 0),
         ]
         example_inputs = (a, b)
-        expected = model(*example_inputs)
-        actual = AOTInductorModelRunner.run(
-            model, example_inputs, expected, constraints=constraints
-        )
-        self.assertTrue(same(actual, expected))
+        self.check_model(Model(), example_inputs, constraints=constraints)
 
     def test_poi_multiple_dynamic(self):
         class Model(torch.nn.Module):
@@ -303,7 +366,6 @@ class AotInductorTests(TestCase):
                 add_0 = x + y
                 return torch.nn.functional.relu(input=add_0, inplace=False)
 
-        model = Model()
         a = torch.randn(128, 2048, device="cuda")
         b = torch.randn(128, 2048, device="cuda")
         constraints = [
@@ -324,13 +386,9 @@ class AotInductorTests(TestCase):
                 torch.randn(211, 2048, device="cuda"),
             ),
         )
-        list_expected = [
-            (model(*example_inputs),) for example_inputs in list_example_inputs
-        ]
-        list_actual = AOTInductorModelRunner.run_multiple(
-            model, list_example_inputs, list_expected, constraints=constraints
+        self.check_model_with_multiple_inputs(
+            Model(), list_example_inputs, constraints=constraints
         )
-        self.assertTrue(same(list_actual, list_expected))
 
     def test_addmm_multiple_dynamic(self):
         class Model(torch.nn.Module):
@@ -361,20 +419,15 @@ class AotInductorTests(TestCase):
         list_example_inputs.append(
             (torch.randn(batch, M, K, device="cuda"),),
         )
-        list_expected = [
-            (model(*example_inputs),) for example_inputs in list_example_inputs
-        ]
-        list_actual = AOTInductorModelRunner.run_multiple(
+        self.check_model_with_multiple_inputs(
             model,
             list_example_inputs,
-            list_expected,
             constraints=constraints,
             options={
                 "max_autotune": True,
                 "max_autotune_gemm_backends": "TRITON",
             },
         )
-        self.assertTrue(same(list_actual, list_expected))
 
     def test_bmm_multiple_dynamic(self):
         class Model(torch.nn.Module):
@@ -411,20 +464,15 @@ class AotInductorTests(TestCase):
                 torch.randn(batch, K, N, device="cuda"),
             ),
         )
-        list_expected = [
-            (model(*example_inputs),) for example_inputs in list_example_inputs
-        ]
-        list_actual = AOTInductorModelRunner.run_multiple(
+        self.check_model_with_multiple_inputs(
             model,
             list_example_inputs,
-            list_expected,
             options={
                 "max_autotune": True,
                 "max_autotune_gemm_backends": "TRITON",
             },
             constraints=constraints,
         )
-        self.assertTrue(same(list_actual, list_expected))
 
     def test_foreach_multiple_dynamic(self):
         class Model(torch.nn.Module):
@@ -458,13 +506,33 @@ class AotInductorTests(TestCase):
                 torch.randn(211, 2048, device="cuda"),
             ),
         )
-        list_expected = [
-            (model(*example_inputs),) for example_inputs in list_example_inputs
-        ]
-        list_actual = AOTInductorModelRunner.run_multiple(
-            model, list_example_inputs, list_expected, constraints=constraints
+        self.check_model_with_multiple_inputs(
+            model,
+            list_example_inputs,
+            constraints=constraints,
         )
-        self.assertTrue(same(list_actual, list_expected))
+
+
+class AOTInductorTestABICompatibile(TestCase):
+    abi_compatible = True
+    check_model = check_model
+    check_model_with_multiple_inputs = check_model_with_multiple_inputs
+
+
+test_torchinductor.copy_tests(
+    AOTInductorTestsTemplate, AOTInductorTestABICompatibile, "abi_compatible"
+)
+
+
+class AOTInductorTestNonABICompatible(TestCase):
+    abi_compatible = False
+    check_model = check_model
+    check_model_with_multiple_inputs = check_model_with_multiple_inputs
+
+
+test_torchinductor.copy_tests(
+    AOTInductorTestsTemplate, AOTInductorTestNonABICompatible, "non_abi_compatible"
+)
 
 
 if __name__ == "__main__":

torch/_inductor/codecache.py
@@ -834,6 +834,12 @@ def get_include_and_linking_paths(
                 )
             else:
                 macros = f"-D{macros}"
+
+        if aot_mode and cuda:
+            if macros is None:
+                macros = ""
+            macros += " -D USE_CUDA"
+
         if cuda:
             if config.is_fbcode():
                 libs += ["cuda"]

torch/_inductor/codegen/aot_runtime/interface.cpp
@@ -1,8 +1,9 @@
 #include <torch/csrc/inductor/aot_runtime/interface.h>
 #include <torch/csrc/inductor/aot_runtime/model_container.h>
-#include <ATen/core/dispatch/Dispatcher.h>
-#include <iostream>
 #include <torch/csrc/inductor/aot_runtime/proxy_executor.h>
+#include <ATen/core/dispatch/Dispatcher.h>
+
+#include <iostream>
 #include <stdexcept>
 #include <vector>
 
@@ -53,7 +54,7 @@ AOTInductorError AOTInductorModelContainerDelete(
 
 AOTInductorError AOTInductorModelContainerRun(
     AOTInductorModelContainerHandle container_handle,
-    const AOTInductorTensorHandle inputs_handle,
+    AOTInductorTensorHandle inputs_handle,
     size_t num_inputs,
     AOTInductorTensorHandle outputs_handle,
     size_t num_outputs,
@@ -64,7 +65,7 @@ AOTInductorError AOTInductorModelContainerRun(
       reinterpret_cast<torch::aot_inductor::AOTInductorModelContainer*>(
           container_handle);
 
-  const auto* inputs = reinterpret_cast<const at::Tensor*>(inputs_handle);
+  auto* inputs = reinterpret_cast<at::Tensor*>(inputs_handle);
   std::vector<at::Tensor> input_tensors;
   input_tensors.reserve(num_inputs);
   for (size_t i = 0; i < num_inputs; i++) {

torch/_inductor/config.py
@@ -433,6 +433,9 @@ class aot_inductor:
     # If not specified, a temp directory will be created under the default caching path
     output_path = ""
 
+    # Wether to codegen abi compatible model.so
+    abi_compatible = is_fbcode()
+
 
 class cuda:
     # CUDA arch to use for CUDA template kernel compilation.

commit 80b963040efd17c66bc0aa833c31b8b4bee89154

[AOTInductor] Fix aot_inductor/test:test_custom_ops
Summary: Fix aot_inductor/test:test_custom_ops, which was broken by https://github.com/pytorch/pytorch/pull/109391

Test Plan: buck2 run mode/dev-nosan //deeplearning/aot_inductor/test:test_custom_ops

Differential Revision: D49438928

torch/_export/serde/serialize.py
@@ -526,6 +526,7 @@ class GraphModuleSerializer:
 
     def serialize_input(self, arg) -> Argument:
         import torch._inductor.ir as inductor_ir
+        inductor_tensor_buffers = (inductor_ir.InputBuffer, inductor_ir.ComputedBuffer, inductor_ir.ConcatKernel)
 
         if isinstance(arg, torch.fx.Node):
             if arg.op == "get_attr":
@@ -547,7 +548,7 @@ class GraphModuleSerializer:
                 return Argument.create(as_sym_bool=SymBoolArgument.create(as_name=arg.name))
             else:
                 return Argument.create(as_tensor=TensorArgument(name=arg.name))
-        elif isinstance(arg, (inductor_ir.InputBuffer, inductor_ir.ComputedBuffer)):
+        elif isinstance(arg, inductor_tensor_buffers):
             # Other branches are for arguments in fx node.
             # This is a special branch for handling buffers (representing tensor arguments)
             # for inductor's ExternalFallbackNode
@@ -615,17 +616,17 @@ class GraphModuleSerializer:
                 return Argument.create(
                     as_optional_tensors=list(map(serialize_optional_tensor_args, arg))
                 )
-            elif all(isinstance(a, (inductor_ir.InputBuffer, inductor_ir.ComputedBuffer)) for a in arg):
+            elif all(isinstance(a, inductor_tensor_buffers) for a in arg):
                 # list of tensors
                 return Argument.create(
                     as_tensors=[TensorArgument(name=a.name) for a in arg],
                 )
-            elif all(isinstance(a, (inductor_ir.InputBuffer, inductor_ir.ComputedBuffer, type(None))) for a in arg):
+            elif all(isinstance(a, (*inductor_tensor_buffers, type(None))) for a in arg):
                 # list of optional tensors
                 def serialize_optional_tensor_args(a):
                     if a is None:
                         return OptionalTensorArgument.create(as_none=())
-                    elif isinstance(a, torch._inductor.ir.InputBuffer):
+                    elif isinstance(a, inductor_tensor_buffers):
                         return OptionalTensorArgument.create(as_tensor=a.name)
                     else:
                         raise SerializeError(f"Unsupported list/tuple argument: {a}")
@@ -633,7 +634,7 @@ class GraphModuleSerializer:
                     as_optional_tensors=list(map(serialize_optional_tensor_args, arg))
                 )
             else:
-                raise SerializeError(f"Unsupported list/tuple argument type: {type(arg[0])}")
+                raise SerializeError(f"Unsupported list/tuple argument type: {[type(a) for a in arg]}")
         elif isinstance(arg, torch.dtype):
             return Argument.create(as_scalar_type=_TORCH_TO_SERIALIZE_DTYPE[arg])
         elif isinstance(arg, torch.device):

torch/_inductor/ir.py
@@ -3815,10 +3815,12 @@ class FallbackKernel(ExternKernelAlloc):
     def codegen(self, wrapper):
         if self.use_cpp_op_schema:
             exported_args = None
+            args = None
             if config.is_fbcode():
                 exported_args = self.export_extern_kernel_node()
+            else:
+                args = [*self.codegen_args(), *self.codegen_kwargs()]
 
-            args = [*self.codegen_args(), *self.codegen_kwargs()]
             wrapper.generate_extern_kernel_alloc_and_find_schema_if_needed(
                 self.get_name(),
                 self.kernel,