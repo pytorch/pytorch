6d70453217971ec6c2919379b0fb8a5424f71dfe
zhxchen17 <zhxchen17@meta.com>
2023-09-19 19:55:38 -0700

    [export] Fix tree spec matching behavior.
    Summary:
    
    Test Plan:
    Internal test.
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torch/export/exported_program.py
+++ torch/export/exported_program.py
@@ -346,10 +346,12 @@ class ExportedProgram:
         if self.call_spec.in_spec is not None:
             try:
                 user_args = combine_args_kwargs(args, kwargs)
-                args = fx_pytree.tree_flatten_spec(user_args, self.call_spec.in_spec)  # type: ignore[assignment]
+                args = fx_pytree.tree_flatten_spec(
+                    user_args, self.call_spec.in_spec, exact_structural_match=True
+                )  # type: ignore[assignment]
             except Exception:
                 _, received_spec = pytree.tree_flatten(user_args)
-                raise error.InternalError(
+                raise TypeError(
                     "Trying to flatten user inputs with exported input tree spec: \n"
                     f"{self.call_spec.in_spec}\n"
                     "but actually got inputs with tree spec of: \n"
