b2df95c39752ecbf961ca5b6bee0074b42e30cdf
Simon Fan <xmfan@meta.com>
2023-09-20 15:33:10 -0700

    rename nanogpt_generate to nanogpt to also support train
    [ghstack-poisoned]

--- benchmarks/dynamo/torchbench.py
+++ benchmarks/dynamo/torchbench.py
@@ -89,6 +89,7 @@ SKIP_FOR_CPU = {
     "hf_T5_generate",  # OOMs
     "cm3leon_generate",  # model is CUDA only
     "nanogpt_generate",  # timeout
+    "nanogpt",  # timeout
     "sam",  # timeout
     "llama_v2_7b_16h",  # model is CUDA only
     "stable_diffusion",  # flaky
