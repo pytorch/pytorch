67ffbc5621a65536e9044dc26ff136ed50f30b97
wz337 <wz337@cornell.edu>
2023-09-20 15:25:45 -0700

    try state dict gap

--- test/distributed/fsdp/test_fsdp_dtensor_state_dict.py
+++ test/distributed/fsdp/test_fsdp_dtensor_state_dict.py
@@ -55,34 +55,34 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
 
         return model, optim
 
-    @with_comms
-    @skip_if_lt_x_gpu(2)
-    def test_fsdp_init_with_device_mesh(self):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+    # @with_comms
+    # @skip_if_lt_x_gpu(2)
+    # def test_fsdp_init_with_device_mesh(self):
+    #     device_mesh = init_device_mesh(self.device_type, (self.world_size,))
+    #     model, optim = self._create_model(device_mesh)
 
-        FSDP.set_state_dict_type(
-            model,
-            StateDictType.SHARDED_STATE_DICT,
-            state_dict_config=ShardedStateDictConfig(),
-            optim_state_dict_config=ShardedOptimStateDictConfig(),
-        )
-        state_dict_type = model.get_state_dict_type(model)
-        # If device_mesh is used when initializing FSDP, the field _use_dtensor will
-        # automatically be set to True.
-        self.assertEqual(state_dict_type.state_dict_config._use_dtensor, True)
-        self.assertEqual(state_dict_type.optim_state_dict_config._use_dtensor, True)
+    #     FSDP.set_state_dict_type(
+    #         model,
+    #         StateDictType.SHARDED_STATE_DICT,
+    #         state_dict_config=ShardedStateDictConfig(),
+    #         optim_state_dict_config=ShardedOptimStateDictConfig(),
+    #     )
+    #     state_dict_type = model.get_state_dict_type(model)
+    #     # If device_mesh is used when initializing FSDP, the field _use_dtensor will
+    #     # automatically be set to True.
+    #     self.assertEqual(state_dict_type.state_dict_config._use_dtensor, True)
+    #     self.assertEqual(state_dict_type.optim_state_dict_config._use_dtensor, True)
 
-        for v in model.state_dict().values():
-            self.assertEqual(len(v.placements), 1)
-            self.assertEqual(v.placements[0], (Shard(dim=0)))
-            self.assertEqual(v.device_mesh, device_mesh)
+    #     for v in model.state_dict().values():
+    #         self.assertEqual(len(v.placements), 1)
+    #         self.assertEqual(v.placements[0], (Shard(dim=0)))
+    #         self.assertEqual(v.device_mesh, device_mesh)
 
-        for v in optim.state_dict()["state"].values():
-            if isinstance(v, DTensor):
-                self.assertEqual(len(v.placements), 1)
-                self.assertEqual(v.placements[0], (Shard(dim=0)))
-                self.assertEqual(v.device_mesh, device_mesh)
+    #     for v in optim.state_dict()["state"].values():
+    #         if isinstance(v, DTensor):
+    #             self.assertEqual(len(v.placements), 1)
+    #             self.assertEqual(v.placements[0], (Shard(dim=0)))
+    #             self.assertEqual(v.device_mesh, device_mesh)
 
     @with_comms
     @skip_if_lt_x_gpu(2)
@@ -99,8 +99,11 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
                 offload_to_cpu=offload_to_cpu
             ),
         )
+        # state_dict_type = model.get_state_dict_type(model)
         dtensor_sd = model.state_dict()
+        # print(f"dtensor_sd:{dtensor_sd}")
         dtensor_osd = FSDP.optim_state_dict(model, optim)
+        # print(f"dtensor_osd:{dtensor_osd}")
 
         ref_model, ref_optim = self._create_model()
         FSDP.set_state_dict_type(
@@ -110,126 +113,133 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
                 offload_to_cpu=offload_to_cpu
             ),
         )
-        sharded_tensor_sd = model.state_dict()
+        sharded_tensor_sd = ref_model.state_dict()
+        # print(f"sharded_tensor_sd:{sharded_tensor_sd}")
         sharded_tensor_osd = FSDP.optim_state_dict(ref_model, ref_optim)
+        # print(f"sharded_tensor_osd:{sharded_tensor_osd}")
 
         # Check dtensor and sharded_tensor model state dict values are identical
         for dtensor_sd, sharded_tensor_sd in zip(
             dtensor_sd.items(), sharded_tensor_sd.items()
         ):
+            print("here")
             k1, v1 = dtensor_sd
             k2, v2 = sharded_tensor_sd
+            print(f"v1:{v1.to_local()}, v2:{v2.local_tensor()}")
             if isinstance(v1, DTensor) and isinstance(v2, ShardedTensor):
                 self.assertEqual(k1, k2)
                 # check whether local_tensor are the same
                 self.assertEqual(v1.to_local(), v2.local_tensor())
+                print(f"v1:{v1.to_local()}, v2:{v2.local_tensor()}")
                 # check whether device are the same
                 self.assertEqual(v1.to_local().device, v2.local_tensor().device)
 
-        # Check dtensor and sharde_tensor optim state dict values are identical
-        for dtensor_osd_state, sharded_tensor_osd_state in zip(
-            dtensor_osd["state"].items(), sharded_tensor_osd["state"].items()
-        ):
-            # check FQN are the same
-            self.assertEqual(dtensor_osd_state[0], sharded_tensor_osd_state[0])
-            for dtensor_hyper_param, sharded_tensor_hyper_param in zip(
-                dtensor_osd_state[1].items(),
-                sharded_tensor_osd_state[1].items(),
-            ):
-                k1, v1 = dtensor_hyper_param
-                k2, v2 = sharded_tensor_hyper_param
-                if isinstance(v1, DTensor) and isinstance(v2, ShardedTensor):
-                    self.assertEqual(k1, k2)
-                    # check whether local_tensor are the same
-                    self.assertEqual(v1.to_local(), v2.local_tensor())
-                    # check whether device are the same
-                    self.assertEqual(v1.to_local().device, v2.local_tensor().device)
+        # # Check dtensor and sharde_tensor optim state dict values are identical
+        # for dtensor_osd_state, sharded_tensor_osd_state in zip(
+        #     dtensor_osd["state"].items(), sharded_tensor_osd["state"].items()
+        # ):
+        #     # check FQN are the same
+        #     self.assertEqual(dtensor_osd_state[0], sharded_tensor_osd_state[0])
+        #     for dtensor_hyper_param, sharded_tensor_hyper_param in zip(
+        #         dtensor_osd_state[1].items(),
+        #         sharded_tensor_osd_state[1].items(),
+        #     ):
+        #         k1, v1 = dtensor_hyper_param
+        #         k2, v2 = sharded_tensor_hyper_param
+        #         if isinstance(v1, DTensor) and isinstance(v2, ShardedTensor):
+        #             self.assertEqual(k1, k2)
+        #             # check whether local_tensor are the same
+        #             self.assertEqual(v1.to_local(), v2.local_tensor())
+        #             # check whether device are the same
+        #             self.assertEqual(v1.to_local().device, v2.local_tensor().device)
 
-    @with_comms
-    @skip_if_lt_x_gpu(2)
-    @parametrize("offload_to_cpu", [True, False])
-    def test_dtensor_sharded_optim_load_state_dict(self, offload_to_cpu):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+    # @with_comms
+    # @skip_if_lt_x_gpu(2)
+    # @parametrize("offload_to_cpu", [True, False])
+    # def test_dtensor_sharded_optim_load_state_dict(self, offload_to_cpu):
+    #     device_mesh = init_device_mesh(self.device_type, (self.world_size,))
+    #     model, optim = self._create_model(device_mesh)
 
-        FSDP.set_state_dict_type(
-            model,
-            StateDictType.SHARDED_STATE_DICT,
-            optim_state_dict_config=ShardedOptimStateDictConfig(
-                offload_to_cpu=offload_to_cpu
-            ),
-        )
+    #     FSDP.set_state_dict_type(
+    #         model,
+    #         StateDictType.SHARDED_STATE_DICT,
+    #         optim_state_dict_config=ShardedOptimStateDictConfig(
+    #             offload_to_cpu=offload_to_cpu
+    #         ),
+    #     )
 
-        checkpoint = io.BytesIO()
-        torch.save(FSDP.optim_state_dict(model, optim), checkpoint)
-        # Deepcopy to save current optim_state_dict to compare with the optim_state_dict loaded back below.
-        ref_optim_state_dict = deepcopy(FSDP.optim_state_dict(model, optim))
+    #     checkpoint = io.BytesIO()
+    #     torch.save(FSDP.optim_state_dict(model, optim), checkpoint)
+    #     # Deepcopy to save current optim_state_dict to compare with the optim_state_dict loaded back below.
+    #     ref_optim_state_dict = deepcopy(FSDP.optim_state_dict(model, optim))
 
-        # Update the parameters so FSDP.optim_state_dict() will be different from ref_optim_state_dict.
-        model(model.get_input()).sum().backward()
-        optim.step()
+    #     # Update the parameters so FSDP.optim_state_dict() will be different from ref_optim_state_dict.
+    #     model(model.get_input()).sum().backward()
+    #     optim.step()
 
-        # Load ref_optim_state_dict back.
-        checkpoint.seek(0)
-        load_ref_optim_state_dict = torch.load(checkpoint)
-        optim.load_state_dict(
-            FSDP.optim_state_dict_to_load(model, optim, load_ref_optim_state_dict)
-        )
-        new_optim_state_dict = FSDP.optim_state_dict(model, optim)
+    #     # Load ref_optim_state_dict back.
+    #     checkpoint.seek(0)
+    #     load_ref_optim_state_dict = torch.load(checkpoint)
+    #     optim.load_state_dict(
+    #         FSDP.optim_state_dict_to_load(model, optim, load_ref_optim_state_dict)
+    #     )
+    #     new_optim_state_dict = FSDP.optim_state_dict(model, optim)
 
-        # Check whether new_optim_state_dict is the same as ref_optim_state_dict.
-        for new_optim_state_dict, ref_optim_state_dict in zip(
-            new_optim_state_dict["state"].items(),
-            ref_optim_state_dict["state"].items(),
-        ):
-            # check FQN are the same
-            self.assertEqual(new_optim_state_dict[0], ref_optim_state_dict[0])
-            for new_optim_hyper_param, ref_optim_hyper_param in zip(
-                new_optim_state_dict[1].items(),
-                ref_optim_state_dict[1].items(),
-            ):
-                k1, v1 = new_optim_hyper_param
-                k2, v2 = ref_optim_hyper_param
-                # check whether keys are the same
-                self.assertEqual(k1, k2)
-                # check whether DTensor are the same
-                self.assertEqual(v1, v2)
+    #     # Check whether new_optim_state_dict is the same as ref_optim_state_dict.
+    #     for new_optim_state_dict, ref_optim_state_dict in zip(
+    #         new_optim_state_dict["state"].items(),
+    #         ref_optim_state_dict["state"].items(),
+    #     ):
+    #         # check FQN are the same
+    #         self.assertEqual(new_optim_state_dict[0], ref_optim_state_dict[0])
+    #         for new_optim_hyper_param, ref_optim_hyper_param in zip(
+    #             new_optim_state_dict[1].items(),
+    #             ref_optim_state_dict[1].items(),
+    #         ):
+    #             k1, v1 = new_optim_hyper_param
+    #             k2, v2 = ref_optim_hyper_param
+    #             # check whether keys are the same
+    #             self.assertEqual(k1, k2)
+    #             # check whether DTensor are the same
+    #             self.assertEqual(v1, v2)
 
-    @with_comms
-    @skip_if_lt_x_gpu(2)
-    @parametrize("offload_to_cpu", [True, False])
-    def test_dtensor_sharded_model_load_state_dict(self, offload_to_cpu):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+    # @with_comms
+    # @skip_if_lt_x_gpu(2)
+    # @parametrize("offload_to_cpu", [True, False])
+    # def test_dtensor_sharded_model_load_state_dict(self, offload_to_cpu):
+    #     device_mesh = init_device_mesh(self.device_type, (self.world_size,))
+    #     model, optim = self._create_model(device_mesh)
 
-        FSDP.set_state_dict_type(
-            model,
-            StateDictType.SHARDED_STATE_DICT,
-            state_dict_config=ShardedStateDictConfig(offload_to_cpu=offload_to_cpu),
-        )
-        state_dict_type = model.get_state_dict_type(model)
+    #     FSDP.set_state_dict_type(
+    #         model,
+    #         StateDictType.SHARDED_STATE_DICT,
+    #         state_dict_config=ShardedStateDictConfig(offload_to_cpu=offload_to_cpu),
+    #     )
+    #     # state_dict_type = model.get_state_dict_type(model)
+    #     print(f"model:{model.state_dict()}")
 
-        checkpoint = io.BytesIO()
-        torch.save(model.state_dict(), checkpoint)
-        # Deepcopy to save current state_dict to compare with the state_dict loaded back below.
-        ref_state_dict = deepcopy(model.state_dict())
+    #     checkpoint = io.BytesIO()
+    #     torch.save(model.state_dict(), checkpoint)
+    #     # Deepcopy to save current state_dict to compare with the state_dict loaded back below.
+    #     ref_state_dict = deepcopy(model.state_dict())
 
-        # Update the parameters so model.state_dict() will be different from ref_dtensor_sd.
-        model(model.get_input()).sum().backward()
-        optim.step()
+    #     # Update the parameters so model.state_dict() will be different from ref_dtensor_sd.
+    #     model(model.get_input()).sum().backward()
+    #     optim.step()
 
-        # Load ref_state_dict back.
-        checkpoint.seek(0)
-        load_ref_state_dict = torch.load(checkpoint)
-        model.load_state_dict(load_ref_state_dict)
-        new_state_dict = model.state_dict()
+    #     # Load ref_state_dict back.
+    #     checkpoint.seek(0)
+    #     load_ref_state_dict = torch.load(checkpoint)
+    #     model.load_state_dict(load_ref_state_dict)
+    #     new_state_dict = model.state_dict()
+    #     print(f"new_state_dict:{new_state_dict}")
 
-        # Check whether new_state_dict is the same as ref_state_dict.
-        for (k1, v1), (k2, v2) in zip(ref_state_dict.items(), new_state_dict.items()):
-            # check whether fqn are the same
-            self.assertEqual(k1, k2)
-            # check whether DTensor are the same
-            self.assertEqual(v1, v2)
+    #     # Check whether new_state_dict is the same as ref_state_dict.
+    #     for (k1, v1), (k2, v2) in zip(ref_state_dict.items(), new_state_dict.items()):
+    #         # check whether fqn are the same
+    #         self.assertEqual(k1, k2)
+    #         # check whether DTensor are the same
+    #         self.assertEqual(v1, v2)
 
 
 instantiate_parametrized_tests(TestFSDPWithDeviceMeshAndDTensor)
