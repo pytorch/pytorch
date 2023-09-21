50189869c443e0dbb4d6c80511d759cd9604edc4
Jane Xu <janeyx@meta.com>
2023-09-20 23:30:06 -0700

    Update on "[foreach] check for empty tensors before dispatching to MTA"
    [ghstack-poisoned]

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -938,7 +938,7 @@ def check_autodiff_sample(op, sample, dtype, is_inplace):
         return (
             False,
             "Trying to set a forward gradient that has a different size than that of the original Tensor, "
-            "this is not supported. Tensor is of size [] while the given forward gradient is of size [1]."
+            "this is not supported. Tensor is of size [] while the given forward gradient is of size [1, 1]."
         )
     rhs_arg_has_complex_number = sample.args and ((
         isinstance(sample.args[0], list)
