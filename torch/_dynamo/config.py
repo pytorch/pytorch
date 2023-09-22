21884701a4c103cdc8855ff1dabf5238afd64bba
Yukio Siraichi <yukio.siraichi@gmail.com>
2023-09-22 16:09:19 -0300

    Don't record function call if `ShapeEnv` is not found.
    Fix: #109844
    
    - Redirecting execution to original function if `ShapeEnv` instance is not found in its arguments
    - Removed `dont_record_shape_env_events`, as it wasn't being used anywhere
    
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -275,9 +275,6 @@ translation_validation_timeout = int(
 translation_validation_no_bisect = (
     os.environ.get("TORCHDYNAMO_TRANSLATION_NO_BISECT", "0") == "1"
 )
-# Disables ShapeEnv event recording.
-# See: [Note: Recording ShapeEnv Events]
-dont_record_shape_env_events = False
 # Checks whether replaying ShapeEnv events on a freshly constructed one yields
 # the a ShapeEnv with the same state. This should be used only in testing.
 check_shape_env_recorded_events = False
