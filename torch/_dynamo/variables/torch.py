301862ba1c9bb7e04cb9e53fdda5f64d5e497be4
Michael Voznesensky <voznesenskym@gmail.com>
2023-09-22 10:49:00 -0700

    [Easy] ConstantVariable() -> .create (#109896)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109896
    Approved by: https://github.com/ezyang

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -185,7 +185,7 @@ class TorchVariable(VariableTracker):
 
     def call_hasattr(self, tx, name):
         result = hasattr(self.value, name)
-        return variables.ConstantVariable(result).add_options(self)
+        return variables.ConstantVariable.create(result).add_options(self)
 
     def unique_var_name(self):
         name = torch_get_name(self.value, f"allowed_fn_{id(self.value)}")
@@ -240,7 +240,9 @@ class TorchVariable(VariableTracker):
             ).call_function(tx, args, kwargs)
         elif self.value in config.constant_functions:
             assert not args and not kwargs
-            return ConstantVariable(config.constant_functions[self.value], **options)
+            return ConstantVariable.create(
+                config.constant_functions[self.value], **options
+            )
         elif self.value is torch._functorch.eager_transforms.grad_impl:
             op = TorchHigherOrderOperatorVariable.make(
                 self.value,
@@ -250,7 +252,7 @@ class TorchVariable(VariableTracker):
         elif self.can_constant_fold_through() and (constant_args or unspec_python_args):
             args, kwargs = specialize_args_kwargs(tx, args, kwargs)
             # constant fold
-            return ConstantVariable(
+            return ConstantVariable.create(
                 self.as_python_constant()(
                     *[x.as_python_constant() for x in args],
                     **{k: v.as_python_constant() for k, v in kwargs.items()},
@@ -271,9 +273,9 @@ class TorchVariable(VariableTracker):
                 and isinstance(args[0], UserDefinedObjectVariable)
                 and hasattr(args[0].value, "__torch_function__")
             ):
-                return ConstantVariable(True, **options)
+                return ConstantVariable.create(True, **options)
             else:
-                return ConstantVariable(False, **options)
+                return ConstantVariable.create(False, **options)
         elif self.value in (
             torch.is_floating_point,
             torch.is_complex,
@@ -286,11 +288,13 @@ class TorchVariable(VariableTracker):
                 input_arg = kwargs["input"]
             if isinstance(input_arg, TensorVariable) and input_arg.dtype is not None:
                 if self.value is torch.is_floating_point:
-                    return ConstantVariable(
+                    return ConstantVariable.create(
                         input_arg.dtype.is_floating_point, **options
                     )
                 elif self.value is torch.is_complex:
-                    return ConstantVariable(input_arg.dtype.is_complex, **options)
+                    return ConstantVariable.create(
+                        input_arg.dtype.is_complex, **options
+                    )
                 else:
                     raise AssertionError(f"calling {self.value}")
         elif (
@@ -298,7 +302,7 @@ class TorchVariable(VariableTracker):
             and isinstance(args[0], TensorVariable)
             and args[0].size is not None
         ):
-            return ConstantVariable(product(args[0].size), **options)
+            return ConstantVariable.create(product(args[0].size), **options)
         elif self.value in REWRITE_OPS_TO_TENSOR_SIZE_METHOD:
             assert len(args) == 1
             assert isinstance(args[0], TensorVariable)
@@ -319,16 +323,16 @@ class TorchVariable(VariableTracker):
             return GradModeVariable.create(tx, args[0].as_python_constant(), **options)
         elif self.value is torch.is_grad_enabled:
             assert not (args or kwargs)
-            return ConstantVariable(torch.is_grad_enabled(), **options).add_guards(
-                GradModeVariable._guards_singleton
-            )
+            return ConstantVariable.create(
+                torch.is_grad_enabled(), **options
+            ).add_guards(GradModeVariable._guards_singleton)
         elif self.value is torch.use_deterministic_algorithms and len(args) == 1:
             return DeterministicAlgorithmsVariable.create(
                 tx, args[0].as_python_constant(), **options
             )
         elif self.value is torch.are_deterministic_algorithms_enabled:
             assert not (args or kwargs)
-            return ConstantVariable(
+            return ConstantVariable.create(
                 torch.are_deterministic_algorithms_enabled(), **options
             ).add_guards(DeterministicAlgorithmsVariable._guards_singleton)
         elif self.value is torch.autograd.graph.disable_saved_tensors_hooks:
@@ -338,7 +342,7 @@ class TorchVariable(VariableTracker):
             )
         elif self.value is torch._C._is_torch_function_enabled:
             assert not (args or kwargs)
-            return ConstantVariable(
+            return ConstantVariable.create(
                 tx.output.torch_function_enabled, **options
             ).add_guards(TorchFunctionDisableVariable._guards_singleton)
         elif self.value is torch._C.DisableTorchFunctionSubclass:
@@ -453,7 +457,7 @@ class TorchVariable(VariableTracker):
             tensor_inp = torch.tensor(
                 0, dtype=tensor_variable.dtype, device=tensor_variable.device
             )
-            return ConstantVariable(
+            return ConstantVariable.create(
                 torch.backends.cudnn.is_acceptable(tensor_inp), **options
             )
         elif self.value is torch.nn.Parameter:
@@ -731,12 +735,12 @@ For now, dynamo will explicitly graph break when it encounters user code with th
         from . import ConstantVariable
 
         def normalize_args(
-            weight=ConstantVariable(None),
-            size_average=ConstantVariable(None),
-            ignore_index=ConstantVariable(-100),
-            reduce=ConstantVariable(None),
-            reduction=ConstantVariable("mean"),
-            label_smoothing=ConstantVariable(0.0),
+            weight=ConstantVariable.create(None),
+            size_average=ConstantVariable.create(None),
+            ignore_index=ConstantVariable.create(-100),
+            reduce=ConstantVariable.create(None),
+            reduction=ConstantVariable.create("mean"),
+            label_smoothing=ConstantVariable.create(0.0),
         ):
             return (
                 weight,
@@ -811,7 +815,7 @@ For now, dynamo will explicitly graph break when it encounters user code with th
                 )
             elif value.is_python_constant():
                 # constant prop through it
-                return variables.ConstantVariable(
+                return variables.ConstantVariable.create(
                     torch.nn.modules.utils._ntuple(count)(value.as_python_constant()),
                     **VariableTracker.propagate(self, value, args, kwargs.values()),
                 )
