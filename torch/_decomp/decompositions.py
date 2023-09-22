a38e2005f5b1d11a5f0ace53412f35733e041a14
andrewor14 <andrewor14@gmail.com>
2023-09-22 14:15:10 -0700

    Decompose to native_dropout in eval mode as well
    Summary: https://github.com/pytorch/pytorch/pull/106274 added a
    decomp for dropout such that for train mode we get
    `aten.native_dropout`, while for eval mode we get `aten.clone`.
    This commit makes it such that we get `aten.native_dropout` for
    both train and eval modes. For eval mode, we let the cloning
    happen in the aten op itself.
    
    The main motivation behind this change is QAT, which needs to
    swap between `aten.native_dropout(train=True)` and
    `aten.native_dropout(train=False)` in the graph. This was
    previously difficult to do since there was no dropout op to
    match and replace in eval mode.
    
    Test Plan: python test/test_ops.py
    
    Reviewers: SherlockNoMad, bdhirsh
    
    Subscribers: SherlockNoMad, bdhirsh, supriyar
    
    [ghstack-poisoned]

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -1048,10 +1048,7 @@ def logit_backward(
 @aten.dropout.default.py_impl(DispatchKey.CompositeImplicitAutograd)
 @aten.dropout.default.py_impl(DispatchKey.Autograd)
 def dropout(input: Tensor, p: float, train: Optional[bool]):
-    if train and p != 0:
-        return aten.native_dropout(input, p, train)[0]
-    else:
-        return input.clone()
+    return aten.native_dropout(input, p, train)[0]
 
 
 @register_decomposition(aten.native_dropout)
