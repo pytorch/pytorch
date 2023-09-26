3262c5358f7ffd315e599eed5984acb1b55a3fdd
Edward Z. Yang <ezyang@meta.com>
2023-09-26 19:38:57 +0000

    Use _check_is_size for validate_dim_length (#109849)
    _check_is_size has some extra juice for unbacked SymInts, use it.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109849
    Approved by: https://github.com/yanboliang

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -362,7 +362,12 @@ def _advise_is_size(a):
     # was responsible for expect_true'ing this
     assert a >= 0
 
-    if isinstance(a, SymInt) and isinstance(a.node.expr, sympy.Symbol):
+    # NB: it's important not to constrain range for size for *hinted* SymInts,
+    # because it is not only unsound, it will immediately trip our asserts
+    # that hints have to be consistent with static analysis!  If you somehow
+    # have an unbounded SymInt that later constrains to 1, this will be
+    # inconsistent with the range
+    if isinstance(a, SymInt) and isinstance(a.node, SymNode) and not a.node.has_hint() and isinstance(a.node.expr, sympy.Symbol):
         _constrain_range_for_size(a)
 
 @record_shapeenv_event()
