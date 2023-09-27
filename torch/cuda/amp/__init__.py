3c44936b0c72b075c5eb5b0a543890be4e94ab64
CaoE <e.cao@intel.com>
2023-09-27 00:52:56 -0700

    Update base for Update on "add test cases for gradscaler on CPU"
    [ghstack-poisoned]

--- torch/cuda/amp/__init__.py
+++ torch/cuda/amp/__init__.py
@@ -3,6 +3,7 @@ from .common import amp_definitely_not_available
 from .grad_scaler import GradScaler
 
 __all__ = [
+    "amp_definitely_not_available",
     "autocast",
     "custom_bwd",
     "custom_fwd",
