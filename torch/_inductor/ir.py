2ab893f20d45681d272dd3df20307068b5a7e4ab
Xu Zhao <xzhao9@fb.com>
2023-09-27 16:26:27 -0700

    [torchbench] Consistent accuracy results with dynamobench
    Summary:
    Use the upstream `torch._dynamo.same` function in accuracy checking and remove the self-hosted version in torchbench.
    
    Now cmf_10x and ads_dhen_5x can run in deterministic mode, enable deepcopy and deterministic mode.
    
    Test Plan:
    ```
    $ buck2 run mode/opt //pytorch/benchmark:run -- cmf_10x -d cuda -t train --accuracy
    Running train method from cmf_10x on cuda in eager mode with input batch size 4 and precision tf32.
    Accuracy:                            pass
    ```
    
    ```
    $ buck2 run mode/opt //pytorch/benchmark:run -- cmf_10x -d cuda -t train --torchdynamo inductor --torchinductor_enable_batch_fusion --torchinductor_enable_split_cat_fx_pass --accuracy
    Running train method from cmf_10x on cuda in dynamo inductor mode with input batch size 4 and precision tf32.
    Accuracy:                            pass
    ```
    
    ```
    $ buck2 run mode/opt //pytorch/benchmark:run -- ads_dhen_5x -d cuda -t train --accuracy
    Running train method from ads_dhen_5x on cuda in eager mode with input batch size 4 and precision tf32.
    Accuracy:                            pass
    ```
    
    Reviewed By: jackiexu1992, mengluy0125
    
    Differential Revision: D49639733

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -5331,6 +5331,10 @@ class StorageBox(MutableBox):
         return (
             (sum(read.index != 0 for read in self.data.get_reads()) > 1)
             if isinstance(self.data, Pointwise)
+            and all(
+                not isinstance(read, dependencies.StarDep)
+                for read in self.data.get_reads()
+            )
             else True
         )
 
