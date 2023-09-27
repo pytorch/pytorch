0bf8500422f53361840c33e1a7d3f0e9361cf0e2
Jez Ng <jezng@fb.com>
2023-09-26 18:31:25 -0700

    Update on "[dynamo] Add graph break on requires_grad_()"
    Fixes #107861.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -689,7 +689,11 @@ class TensorVariable(VariableTracker):
                 ), "Unreachable - See unimplemented for lambdas above"
             tx.output.side_effects.register_hook(self, fn_var, handle_variable)
             return handle_variable
-        elif name == "requires_grad_":
+        elif (
+            name == "requires_grad_"
+            and self.as_proxy().node.meta["example_value"].requires_grad
+            != args[0].value
+        ):
             unimplemented("Tensor.requires_grad_")
 
         else:
