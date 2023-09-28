1a8c29e460a812735dc68c3c047d208485a07f4e
Yanbo Liang <ybliang8@gmail.com>
2023-09-28 01:21:55 +0000

    [1/N] Dynamo skipfiles refactor (#109567)
    This is 1/N of the dynamo skipfiles/allowed_functions refactor, the major change in this PR includes:
    * Refactor & define the [skipfiles rules](https://github.com/pytorch/pytorch/pull/109567/files#diff-5aa3ce9db729bf0901ea97a5d3cc51924cc8575d9c516c1c8f572a35de92544aR56) and interface
    * For every ```skipfiles.check```, we return both the check result and the skip/inline reason and log them for debugging.
    * We found several latent issues/bugs and incorrect implementations in the codebase, but I'm planning to fix them in follow-up PRs to make the refactor decoupled with bug fixes.
    * More details in the inline comments.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109567
    Approved by: https://github.com/ezyang, https://github.com/jansel, https://github.com/anijain2305

--- torch/_dynamo/skipfiles.py
+++ torch/_dynamo/skipfiles.py
@@ -29,21 +29,22 @@ import types
 import typing
 import unittest
 import weakref
+from typing import Optional
 
 import torch
 import torch._inductor.test_operators
 import torch.distributed
 import torch.utils._content_store
 
-from . import comptime, config, external_utils
+from . import comptime, external_utils
 
 """
 A note on skipfiles:
 
-Dynamo consults this file to determine whether code should be compiled or skipped.
+Dynamo consults this file to determine whether function should be inlined or skipped.
 
 A skip applies at the frame boundary, meaning dynamo either triggers a graph break
-at the beginning of the frame or attempts to trace the whole frame.  When skipping
+at the beginning of the frame or attempts to trace/inline the whole frame. When skipping
 a frame, recursively called frames are still traced by dynamo unless also skipped.
 
 Skipfiles (skipped at the file level instead of function level) still apply on a
@@ -51,7 +52,89 @@ frame-by-frame boundary as dynamo traces, but apply to all functions in that fil
 
 @skip is a helper decorator that can be applied to your function to cause it to be
 included here.
+
+Dynamo skip/inline rules & priorities are defined as follows:
+* Inline is the default behavior and will be used unless explicitly skipped.
+* Dynamo has two SKIPLIST: BUILTIN_SKIPLIST and THIRDPARTY_SKIPLIST.
+    * BUILTIN_SKIPLIST contains builtin python modules, such as abc, collections, etc.
+    * THIRDPARTY_SKIPLIST contains common third party libraries, such as numpy, pandas, etc.
+* Functions in these two SKIPLISTs are always skipped, except when they are explicitly
+    put into the two INLINELIST: FILENAME_INLINELIST and SUBMODULE_INLINELIST.
+* PyTorch(torch) is in the BUILTIN_SKIPLIST by default, but there are many cases
+    where we want inline the functions under torch namespace. We should add them
+    into FILENAME_INLINELIST or SUBMODULE_INLINELIST to make dynamo inline those functions.
+* If you call functions under skipped modules/files, Dynamo will wrap these functions
+    as SkipFilesVariable. There are a few functions(e.g, collections.OrderedDict) that
+    we have special handling at SkipFilesVariable.call_function.
+
+Overall: *_INLINELIST has precedence over *_SKIPLIST has precedence over DEFAULT (inline)
+
+To figure out what the behavior is, check the following list in order:
+* FILENAME_INLINELIST (Inline if YES)
+* SUBMODULE_INLINELIST (Inline if YES)
+* BUILTIN_SKIPLIST & THIRDPARTY_SKIPLIST (Skip if YES)
+* Inline by default
+
 """
+
+
+BUILTIN_SKIPLIST = (
+    abc,
+    collections,
+    contextlib,
+    copy,
+    copyreg,
+    dataclasses,
+    enum,
+    functools,
+    importlib,
+    inspect,
+    linecache,
+    logging,
+    multiprocessing,
+    operator,
+    os,
+    posixpath,
+    random,
+    re,
+    selectors,
+    signal,
+    tempfile,
+    threading,
+    tokenize,
+    torch,  # torch/* is skipped by default unless specified in FILENAME_INLINELIST or SUBMODULE_INLINELIST
+    traceback,
+    types,
+    typing,
+    unittest,
+    weakref,
+    _collections_abc,
+    _weakrefset,
+)
+
+# third party libraries skiplist is defined by str, because users may not use these libraries.
+# we should use lazy import & skip in the future.
+THIRDPARTY_SKIPLIST = (
+    "functorch",
+    "fx2trt_oss",
+    "intel_extension_for_pytorch",
+    "networkx",
+    "numpy",
+    "omegaconf",
+    "onnx",
+    "onnxruntime",
+    "onnx_tf",
+    "pandas",
+    "sklearn",
+    "tabulate",
+    "tensorflow",
+    "tensorrt",
+    "torch2trt",
+    "tqdm",
+    "tree",
+    "tvm",
+    "xarray",
+)
 
 
 def _strip_init_py(s):
@@ -62,118 +145,89 @@ def _module_dir(m: types.ModuleType):
     return _strip_init_py(m.__file__)
 
 
-SKIP_DIRS = [
-    # torch.*
-    _module_dir(torch),
-    # torchdynamo.*
-    os.path.dirname(__file__) + "/",
-    "<frozen importlib",
-    "<__array_function__ internals>",
-] + [
-    # skip some standard libs
-    _module_dir(m)
-    for m in (
-        abc,
-        collections,
-        contextlib,
-        copy,
-        copyreg,
-        dataclasses,
-        enum,
-        functools,
-        importlib,
-        inspect,
-        linecache,
-        logging,
-        multiprocessing,
-        operator,
-        os,
-        posixpath,
-        random,
-        re,
-        selectors,
-        signal,
-        tempfile,
-        threading,
-        tokenize,
-        traceback,
-        types,
-        typing,
-        unittest,
-        weakref,
-        _collections_abc,
-        _weakrefset,
-    )
-]
-
-FILENAME_ALLOWLIST = {
+# Force inline functions in these files, even the files is in *_SKIPLIST.
+FILENAME_INLINELIST = {
     torch.nn.Sequential.__init__.__code__.co_filename,
     torch.set_rng_state.__code__.co_filename,
     torch._inductor.test_operators.__file__,
     torch.utils._content_store.__file__,
-    # These are dynamo files!
     external_utils.__file__,
-    comptime.__file__,  # Want to inline these helpers
+    comptime.__file__,
+    torch.optim._functional.__file__,
+    torch.utils._foreach_utils.__file__,
+    _module_dir(torch) + "ao/quantization/pt2e/qat_utils.py",
+    _module_dir(torch) + "ao/quantization/quantizer/xnnpack_quantizer.py",
+    _module_dir(torch) + "ao/quantization/pt2e/representation/rewrite.py",
+    _module_dir(torch) + "ao/quantization/pt2e/utils.py",
+    _module_dir(torch) + "ao/quantization/pt2e/eval_utils.py",
+    _module_dir(torch) + "_export/constraints.py",
+    _module_dir(torch) + "_higher_order_ops/cond.py",
+    _module_dir(torch) + "_functorch/apis.py",
+    _module_dir(torch) + "_functorch/deprecated.py",
+    _module_dir(torch) + "distributed/tensor/parallel/_utils.py",
+    _module_dir(torch) + "distributed/tensor/parallel/style.py",
+    _module_dir(torch) + "distributed/tensor/parallel/_data_parallel_utils.py",
+    _module_dir(torch) + "distributed/_tensor/api.py",
+    _module_dir(torch) + "distributed/_tensor/device_mesh.py",
+    _module_dir(torch) + "distributed/_tensor/placement_types.py",
+    _module_dir(torch) + "distributed/c10d_logger.py",
+    _module_dir(torch) + "distributed/_functional_collectives.py",
+    torch.distributions.normal.__file__,
+    torch.distributions.independent.__file__,
+    torch.distributions.utils.__file__,
+    torch.utils._contextlib.__file__,
+    torch.fx._pytree.__file__,
 }
 
 if torch.distributed.is_available():
     # Inline the checkpoint code from distributed
     import torch.distributed.algorithms._checkpoint.checkpoint_wrapper
 
-    FILENAME_ALLOWLIST |= {
+    FILENAME_INLINELIST |= {
         torch.distributed.algorithms._checkpoint.checkpoint_wrapper.__file__
     }
 
 # Include optimizer code for tracing
-FILENAME_ALLOWLIST |= {
+FILENAME_INLINELIST |= {
     inspect.getfile(obj)
     for obj in torch.optim.__dict__.values()
     if inspect.isclass(obj)
 }
-FILENAME_ALLOWLIST |= {torch.optim._functional.__file__}
-FILENAME_ALLOWLIST |= {torch.utils._foreach_utils.__file__}
-
-# Do trace through match and replace patterns used in PT2E QAT
-# Note: These patterns are comprised of torch ops and for internal use only.
-# They are exported to aten graphs before being passed to the FX subgraph rewriter.
-# TODO: find a better way to express this path without having to import
-# `torch.ao.quantization.pt2e`, which interferes with memory profiling
-FILENAME_ALLOWLIST |= {
-    _module_dir(torch) + "ao/quantization/pt2e/qat_utils.py",
-    _module_dir(torch) + "ao/quantization/quantizer/xnnpack_quantizer.py",
-    _module_dir(torch) + "ao/quantization/pt2e/representation/rewrite.py",
-    _module_dir(torch) + "ao/quantization/pt2e/utils.py",
-    _module_dir(torch) + "ao/quantization/pt2e/eval_utils.py",
-}
-
-FILENAME_ALLOWLIST |= {
-    _module_dir(torch) + "_export/constraints.py",
-}
-
-FILENAME_ALLOWLIST |= {
-    _module_dir(torch) + "_higher_order_ops/cond.py",
-}
 
 # TODO (zhxchen17) Make exportdb importable here.
-FILENAME_ALLOWLIST |= set(
+FILENAME_INLINELIST |= set(
     glob.glob(_module_dir(torch) + "_export/db/examples/*.py"),
 ) | {
     _module_dir(torch) + "_export/wrappers.py",
 }
 
-# torch.func: need to allow this file to be able to look at functorch transforms
-FILENAME_ALLOWLIST |= {
-    _module_dir(torch) + "_functorch/apis.py",
-    _module_dir(torch) + "_functorch/deprecated.py",
+
+# Force inline functions under these modules, even the modules is in *_SKIPLIST.
+SUBMODULE_INLINELIST = {
+    torch.nn,
+    torch.distributions,
+    torch.testing,
+    torch.ao.nn,
+    torch._refs,
+    torch._prims,
+    torch._decomp,
+    torch.utils._contextlib,
+    torch.utils._pytree,
+    torch.fx._pytree,
+    torch.sparse,
 }
 
-FILENAME_ALLOWLIST |= {
-    _module_dir(torch) + "distributed/tensor/parallel/_utils.py",
-    _module_dir(torch) + "distributed/tensor/parallel/style.py",
-    _module_dir(torch) + "distributed/tensor/parallel/_data_parallel_utils.py",
-    _module_dir(torch) + "distributed/_tensor/api.py",
-    _module_dir(torch) + "distributed/_tensor/device_mesh.py",
-}
+if torch.distributed.is_available():
+    from torch.distributed import _functional_collectives
+
+    SUBMODULE_INLINELIST.add(_functional_collectives)
+
+
+# skip some standard python builtin libs
+SKIP_DIRS = [
+    "<frozen importlib",
+    "<__array_function__ internals>",
+] + [_module_dir(m) for m in BUILTIN_SKIPLIST]
 
 FILENAME_ALLOWLIST |= {
     _module_dir(torch) + "_dynamo/_trace_wrapped_higher_order_op.py",
@@ -212,51 +266,50 @@ def add(import_name: str):
     _recompile_re()
 
 
-def check(filename, allow_torch=False):
+@dataclasses.dataclass
+class SkipResult:
+    skipped: bool
+    reason: Optional[str]
+
+
+def check_verbose(filename, allow_torch=False):
     """Should skip this file?"""
     if filename is None:
-        return True
-    if filename in FILENAME_ALLOWLIST:
-        return False
-    if allow_torch and is_torch(filename):
-        return False
+        return SkipResult(True, "filename is None")
+    if filename in FILENAME_INLINELIST:
+        return SkipResult(
+            False,
+            "inlined according skipfiles.FILENAME_INLINELIST",
+        )
+    if allow_torch and is_torch_inline_allowed(filename):
+        return SkipResult(
+            False,
+            "inlined according skipfiles.SUBMODULE_INLINELIST",
+        )
     if is_fbcode and bool(FBCODE_SKIP_DIRS_RE.match(filename)):
-        return True
-    return bool(SKIP_DIRS_RE.match(filename))
+        return SkipResult(
+            True,
+            "skipped according skipfiles.FBCODE_SKIP_DIRS",
+        )
+    if bool(SKIP_DIRS_RE.match(filename)):
+        return SkipResult(True, "skipped according skipfiles.SKIP_DIRS")
+    else:
+        return SkipResult(False, "inlined by default")
+
+
+def check(filename, allow_torch=False):
+    return check_verbose(filename, allow_torch).skipped
 
 
 # skip common third party libs
-for _name in (
-    "functorch",
-    "fx2trt_oss",
-    "intel_extension_for_pytorch",
-    "networkx",
-    "numpy",
-    "omegaconf",
-    "onnx",
-    "onnxruntime",
-    "onnx_tf",
-    "pandas",
-    "sklearn",
-    "tabulate",
-    "tensorflow",
-    "tensorrt",
-    "torch2trt",
-    "tqdm",
-    "tree",
-    "tvm",
-    "xarray",
-):
+for _name in THIRDPARTY_SKIPLIST:
     add(_name)
 
 _recompile_re()
 
 
 def is_torch_inline_allowed(filename):
-    return any(
-        filename.startswith(_module_dir(mod))
-        for mod in config.skipfiles_inline_module_allowlist
-    )
+    return any(filename.startswith(_module_dir(mod)) for mod in SUBMODULE_INLINELIST)
 
 
 @functools.lru_cache(None)
