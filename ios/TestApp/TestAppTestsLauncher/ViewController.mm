6e15c3ce275c328063e7928db806d2dfd7a79898
Huy Do <huydhn@gmail.com>
2023-09-29 13:11:39 -0700

    Add TestAppTestsLauncher

---
+++ ios/TestApp/TestAppTestsLauncher/ViewController.mm
@@ -0,0 +1,14 @@
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@property(nonatomic, strong) UITextView* textView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+@end
