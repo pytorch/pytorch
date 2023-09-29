6b1939b4877d55f2180924901561d1e1f3947e3d
Amadeusz Skrzypczak <askrzypczak@habana.ai>
2023-09-29 13:44:00 +0300

    Fix type promotion of float8_e5m2 and float8_e4m3fn

--- c10/core/ScalarType.h
+++ c10/core/ScalarType.h
@@ -46,15 +46,15 @@ namespace c10 {
   _(c10::quint8, QUInt8) /* 13 */                        \
   _(c10::qint32, QInt32) /* 14 */                        \
   _(at::BFloat16, BFloat16) /* 15 */                     \
-  _(c10::quint4x2, QUInt4x2) /* 16 */                    \
-  _(c10::quint2x4, QUInt2x4) /* 17 */                    \
-  _(c10::bits1x8, Bits1x8) /* 18 */                      \
-  _(c10::bits2x4, Bits2x4) /* 19 */                      \
-  _(c10::bits4x2, Bits4x2) /* 20 */                      \
-  _(c10::bits8, Bits8) /* 21 */                          \
-  _(c10::bits16, Bits16) /* 22 */                        \
-  _(c10::Float8_e5m2, Float8_e5m2) /* 23 */              \
-  _(c10::Float8_e4m3fn, Float8_e4m3fn) /* 24 */
+  _(c10::Float8_e5m2, Float8_e5m2) /* 16 */              \
+  _(c10::Float8_e4m3fn, Float8_e4m3fn) /* 17 */          \
+  _(c10::quint4x2, QUInt4x2) /* 18 */                    \
+  _(c10::quint2x4, QUInt2x4) /* 19 */                    \
+  _(c10::bits1x8, Bits1x8) /* 20 */                      \
+  _(c10::bits2x4, Bits2x4) /* 21 */                      \
+  _(c10::bits4x2, Bits4x2) /* 22 */                      \
+  _(c10::bits8, Bits8) /* 23 */                          \
+  _(c10::bits16, Bits16) /* 24 */
 
 // If you want to support ComplexHalf for real, add ComplexHalf
 // into this macro (and change the name).  But beware: convert()
