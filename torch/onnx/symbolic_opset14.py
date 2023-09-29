bacc05a69da513906f2c4156820741bebbf1ab3f
ruiren <ruiren1225@gmail.com>
2023-09-29 19:16:18 +0000

    support attn_mask fp16 type

--- torch/onnx/symbolic_opset14.py
+++ torch/onnx/symbolic_opset14.py
@@ -190,7 +190,7 @@ def scaled_dot_product_attention(
         mul_qk_add = g.op("Add", mul_qk, attn_mask)
     elif (
         _type_utils.JitScalarType.from_value(attn_mask)
-        == _type_utils.JitScalarType.FLOAT
+        in (_type_utils.JitScalarType.FLOAT, _type_utils.JitScalarType.HALF)
     ):
         mul_qk_add = g.op("Add", mul_qk, attn_mask)
     else:
