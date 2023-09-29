bbd49e106a179c1bb1e56706cb3a622cde1f4913
eellison <elias.ellison@gmail.com>
2023-09-29 12:13:37 -0700

    Update base for Update on "add full permuted"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -2547,6 +2547,37 @@ class MiscTests(torch._dynamo.test_case.TestCase):
         self.assertTrue(same(ref0, res0))
         self.assertTrue(same(ref1, res1))
 
+    def test_has_torch_function(self):
+        class MyTensor:
+            @classmethod
+            def __torch_function__(cls, func, types, args=(), kwargs=None):
+                if kwargs is None:
+                    kwargs = {}
+
+                if func is torch.max:
+                    return torch.tensor(123)
+                return func(*args, **kwargs)
+
+        class LocalSubclass(torch.Tensor):
+            @classmethod
+            def __torch_function__(cls, func, types, args=(), kwargs=None):
+                if kwargs is None:
+                    kwargs = {}
+                return func(*args, **kwargs)
+
+        def fn(x):
+            return torch.overrides.has_torch_function_unary(x), torch.overrides.has_torch_function_variadic(x)
+
+        for test_class in [MyTensor, LocalSubclass]:
+            x = test_class()
+            ref0 = fn(x)
+            ref1 = fn(4)
+            opt_fn = torch._dynamo.optimize("eager")(fn)
+            res0 = opt_fn(x)
+            res1 = opt_fn(4)
+            self.assertTrue(same(ref0, res0))
+            self.assertTrue(same(ref1, res1))
+
     def test_tensor_data(self):
         def fn(x, y):
             return x[y.data]
