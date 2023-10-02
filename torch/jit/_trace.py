bc22528e635dc2553eb78b1267241c9f87b9d931
jjsjann123 <jiej@nvidia.com>
2023-10-02 15:59:48 -0700

    move warning suppresion inside  ignroe_lib_warnings

--- torch/jit/_trace.py
+++ torch/jit/_trace.py
@@ -583,6 +583,7 @@ class TracerWarning(Warning):
         warnings.filterwarnings(
             "ignore", category=TracerWarning, module="torch.(?!jit)"
         )
+        warnings.filterwarnings("ignore", "torch::jit::fuser::cuda")
 
 
 # We ignore the tracer warnings coming form inside the library, because all our shape
