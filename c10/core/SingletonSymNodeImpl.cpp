9f4cf40d265a6dac79f7be38b977995615a7d7de
soulitzer <soulitzer@gmail.com>
2023-10-04 12:17:39 -0400

    Update singleton int to error when inequality relation is undefined (#110044)
    Previously, something like j0 >= 3, would return False. In sympy however, it is not possible to make it so that both j0 >= 3 and j0 < 3 return False. In sympy, you only get to dispatch on Ge, and the remaining are derived, e.g. defining Ge(j0 >= 3) to be False would force Lt(j0, 3) to be True, which is not what we want.
    
    In this PR, we make it so that both j0 >=3 and j0 < 3 error, so that in a future PR when we create the symbolic counterpart of this singleton, the behaviors can be the same.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110044
    Approved by: https://github.com/ezyang

--- c10/core/SingletonSymNodeImpl.cpp
+++ c10/core/SingletonSymNodeImpl.cpp
@@ -1,3 +1,65 @@
 #include <c10/core/SingletonSymNodeImpl.h>
+#include <c10/core/SymNodeImpl.h>
+#include <c10/util/Exception.h>
 
-namespace c10 {} // namespace c10
+namespace c10 {
+
+namespace {
+bool _eq(const char* op, c10::SymNodeImpl* lhs, c10::SymNodeImpl* rhs) {
+  TORCH_INTERNAL_ASSERT(lhs->singleton_int().has_value());
+  c10::optional<int64_t> c = rhs->singleton_int();
+  return c.has_value() && lhs->singleton_int() == *c;
+}
+bool _ge(const char* op, c10::SymNodeImpl* lhs, c10::SymNodeImpl* rhs) {
+  if (auto mb_si = lhs->singleton_int()) {
+    if (auto mb_si2 = rhs->singleton_int()) {
+      if (*mb_si == *mb_si2) {
+        return true;
+      }
+      TORCH_CHECK(false, "Singleton int ", op, ": Relation is indeterminate");
+    }
+    if (rhs->constant_int() && *rhs->constant_int() <= 2) {
+      return true;
+    }
+    TORCH_CHECK(false, "Singleton int ", op, ": Relation is indeterminate");
+  } else if (auto mb_si = rhs->singleton_int()) {
+    if (lhs->constant_int() && *lhs->constant_int() < 2) {
+      return false;
+    }
+    TORCH_CHECK(false, "Singleton int ", op, ": Relation is indeterminate");
+  }
+  TORCH_INTERNAL_ASSERT(false, "expect at least one singleton");
+}
+} // namespace
+
+c10::SymNode SingletonSymNodeImpl::eq(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      _eq("eq", this, other.get())));
+}
+
+c10::SymNode SingletonSymNodeImpl::ne(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      !_eq("ne", this, other.get())));
+}
+
+c10::SymNode SingletonSymNodeImpl::ge(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      _ge("ge", this, other.get())));
+}
+
+c10::SymNode SingletonSymNodeImpl::gt(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      !_ge("gt", other.get(), this)));
+}
+
+c10::SymNode SingletonSymNodeImpl::lt(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      !_ge("lt", this, other.get())));
+}
+
+c10::SymNode SingletonSymNodeImpl::le(const c10::SymNode& other) {
+  return SymNode(c10::make_intrusive<ConstantSymNodeImpl<bool>>(
+      _ge("le", other.get(), this)));
+}
+
+} // namespace c10
