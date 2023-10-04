771c49f6ab9419fd4c08bdbc6ca963e8cdc45bbf
Edward Z. Yang <ezyang@meta.com>
2023-10-04 15:34:33 -0400

    Print output type if assert fires (#110534)
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110534
    Approved by: https://github.com/albanD

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3985,7 +3985,9 @@ class FallbackKernel(ExternKernelAlloc):
             elif isinstance(output, int):
                 return output
             else:
-                assert output is None, "FallbackKernel output type is not supported"
+                assert (
+                    output is None
+                ), f"FallbackKernel output type {type(output)} is not supported"
                 return None
 
         outputs = generate_output(example_output, [])
