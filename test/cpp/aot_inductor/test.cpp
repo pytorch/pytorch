12f4a7e76b22a966adba5b12ba9f94a7198266a0
Bin Bao <binbao@meta.com>
2023-10-05 14:27:06 -0700

    [aotindutor] Update the cpp test example
    Summary: store inputs and outpus in python, and load them back to run the compiled model in c++ and compare the output
    
    [ghstack-poisoned]

--- test/cpp/aot_inductor/test.cpp
+++ test/cpp/aot_inductor/test.cpp
@@ -1,68 +1,67 @@
 #include <gtest/gtest.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include <c10/cuda/CUDAStream.h>
 #include <torch/csrc/inductor/aoti_runtime/interface.h>
 #include <torch/csrc/inductor/aoti_torch/tensor_converter.h>
+#include <torch/script.h>
 #include <torch/torch.h>
+
+#define STR_VALUE(x) #x
+#define STRINGIZE(x) STR_VALUE(x)
 
 namespace torch {
 namespace aot_inductor {
 
-struct Net : torch::nn::Module {
-  Net() : linear(register_module("linear", torch::nn::Linear(64, 10))) {}
-
-  torch::Tensor forward(torch::Tensor x, torch::Tensor y) {
-    return linear(torch::sin(x) + torch::cos(y));
+class RAIIModelContainer {
+ public:
+  RAIIModelContainer() {
+    AOTI_RUNTIME_ERROR_CODE_CHECK(AOTInductorModelContainerCreate(
+        &container_handle,
+        1 /*num_models*/,
+        false /*is_cpu*/,
+        nullptr /*cubin_dir*/));
   }
-  torch::nn::Linear linear;
+
+  ~RAIIModelContainer() {
+    AOTI_RUNTIME_ERROR_CODE_CHECK(
+        AOTInductorModelContainerDelete(container_handle));
+  }
+
+  AOTInductorModelContainerHandle get() const {
+    return container_handle;
+  }
+
+ private:
+  AOTInductorModelContainerHandle container_handle;
 };
 
 TEST(AotInductorTest, BasicTest) {
   torch::NoGradGuard no_grad;
-  Net net;
-  net.to(torch::kCUDA);
 
-  // We should fix the weight over here.
-  // This should match exactly with the one in test.py
-  torch::Tensor weights =
-      at::arange(640, at::dtype(at::kFloat).device(at::kCUDA));
-  weights = at::reshape(weights, {10, 64});
-  torch::Tensor bias = at::zeros({10}, at::dtype(at::kFloat).device(at::kCUDA));
+  std::filesystem::path io_tensors_path = std::filesystem::path(
+      STRINGIZE(CMAKE_CURRENT_BINARY_DIR)) / "io_tensors.pt";
+  torch::jit::script::Module tensor_loader =
+      torch::jit::load(io_tensors_path.string());
+  torch::Tensor x = tensor_loader.attr("x").toTensor();
+  torch::Tensor y = tensor_loader.attr("y").toTensor();
+  std::vector<torch::Tensor> input_tensors = {x, y};
+  torch::Tensor ref_output = tensor_loader.attr("output").toTensor();
 
-  for (const auto& pair : net.named_parameters()) {
-    if (pair.key().find("weight") != std::string::npos) {
-      pair.value().copy_(weights);
-    } else if (pair.key().find("bias") != std::string::npos) {
-      pair.value().copy_(bias);
-    }
-  }
-
-  torch::Tensor x =
-      at::randn({32, 64}, at::dtype(at::kFloat).device(at::kCUDA));
-  torch::Tensor y =
-      at::randn({32, 64}, at::dtype(at::kFloat).device(at::kCUDA));
-  torch::Tensor results_ref = net.forward(x, y);
-  std::vector<torch::Tensor> input_tensors;
-  input_tensors.push_back(x);
-  input_tensors.push_back(y);
-
-  AOTInductorModelContainerHandle container_handle;
-  AOTI_RUNTIME_ERROR_CODE_CHECK(AOTInductorModelContainerCreate(
-      &container_handle,
-      1 /*num_models*/,
-      false /*is_cpu*/,
-      nullptr /*cubin_dir*/));
-
+  RAIIModelContainer model_container;
+  // TODO: will add another API to avoid doing all these input preparations
   auto input_handles =
       torch::aot_inductor::unsafe_alloc_new_handles_from_tensors(input_tensors);
 
   // For outputs, we only allocate a vector to hold returned tensor handles,
   // not allocating the actual output tensor storage here
   size_t num_outputs;
-  AOTI_RUNTIME_ERROR_CODE_CHECK(
-      AOTInductorModelContainerGetNumOutputs(container_handle, &num_outputs));
+  AOTI_RUNTIME_ERROR_CODE_CHECK(AOTInductorModelContainerGetNumOutputs(
+      model_container.get(), &num_outputs));
   std::vector<AtenTensorHandle> output_handles(num_outputs);
 
   const auto& cuda_stream = at::cuda::getCurrentCUDAStream(0 /*device_index*/);
@@ -73,7 +72,7 @@ TEST(AotInductorTest, BasicTest) {
   AOTIProxyExecutorHandle proxy_executor_handle = nullptr;
 
   AOTI_RUNTIME_ERROR_CODE_CHECK(AOTInductorModelContainerRun(
-      container_handle,
+      model_container.get(),
       input_handles.data(),
       input_tensors.size(),
       output_handles.data(),
@@ -81,12 +80,11 @@ TEST(AotInductorTest, BasicTest) {
       stream_handle,
       proxy_executor_handle));
 
-  auto outputs = torch::aot_inductor::alloc_tensors_by_stealing_from_handles(
-      output_handles.data(), output_handles.size());
+  auto actual_output_tensors =
+      torch::aot_inductor::alloc_tensors_by_stealing_from_handles(
+          output_handles.data(), output_handles.size());
 
-  ASSERT_TRUE(torch::allclose(results_ref, outputs[0]));
-  AOTI_RUNTIME_ERROR_CODE_CHECK(
-      AOTInductorModelContainerDelete(container_handle));
+  ASSERT_TRUE(torch::allclose(ref_output, actual_output_tensors[0]));
 }
 
 } // namespace aot_inductor
--- test/cpp/aot_inductor/test.py
+++ test/cpp/aot_inductor/test.py
@@ -3,17 +3,10 @@ import shutil
 import torch
 from torch._export import aot_compile, dynamic_dim
 
-
 class Net(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.fc = torch.nn.Linear(64, 10)
-        weights = torch.arange(640)
-        weights = torch.reshape(weights, (10, 64))
-
-        with torch.no_grad():
-            self.fc.weight.copy_(weights)
-            self.fc.bias.copy_(torch.zeros(10))
 
     def forward(self, x, y):
         return self.fc(torch.sin(x) + torch.cos(y))
@@ -21,9 +14,10 @@ class Net(torch.nn.Module):
 
 x = torch.randn((32, 64), device="cuda")
 y = torch.randn((32, 64), device="cuda")
+with torch.no_grad():
+    ref_output = (Net().cuda())(x, y)
 
 torch._dynamo.reset()
-
 with torch.no_grad():
     constraints = [
         dynamic_dim(x, 0) >= 1,
@@ -32,4 +26,23 @@ with torch.no_grad():
     ]
     lib_path, module = aot_compile(Net().cuda(), (x, y), constraints=constraints)
 
-shutil.copy(lib_path, "libaot_inductor_output.so")
+shutil.copy(lib_path, "libmodel.so")
+
+
+# Use this to
+class IOTensors(torch.nn.Module):
+    def __init__(self, tensors):
+        super().__init__()
+        for key in tensors:
+            setattr(self, key, tensors[key])
+
+io_tensors = {
+    "x": x,
+    "y": y,
+    "output": [ref_output],
+}
+
+# Save arbitrary values supported by TorchScript
+# https://pytorch.org/docs/master/jit.html#supported-type
+tensor_saver = torch.jit.script(IOTensors(io_tensors))
+tensor_saver.save("io_tensors.pt")
