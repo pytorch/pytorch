cfb14323936f4a02429a39e8cee3bcf46fd5d11c
Avik Chaudhuri <avik@fb.com>
2023-10-04 17:33:51 -0700

    export db links for user errors (#110555)
    Ideally all `_dynamo.exc.UserError`s should have "case names", i.e., link to examples in `exportdb`.
    
    This PR adds case names to several instances of `_dynamo.exc.UserError`. In particular, looking at coverage based on `UserErrorType`:
    * `DYNAMIC_CONTROL_FLOW`, `ANTI_PATTERN`, and `STANDARD_LIBRARY` are fully covered.
    * `CONSTRAINT_VIOLATION` and `DYNAMIC_DIM` have no coverage. We don't seem to have any dedicated examples of specifying dynamic shapes in `exportdb` (although they are used in some other examples without explanation, to avoid some specialization that would make such examples moot).
    * `INVALID_INPUT` is only partly covered. Frankly this is tedious to cover via examples.
    
    Differential Revision: [D49928518](https://our.internmc.facebook.com/intern/diff/D49928518/)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110555
    Approved by: https://github.com/angelayi, https://github.com/ydwu4

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1410,7 +1410,7 @@ def get_fake_value(node, tx):
             cause, torch.fx.experimental.symbolic_shapes.GuardOnDataDependentSymNode
         ):
             raise UserError(
-                UserErrorType.CONSTRAIN_VIOLATION,
+                UserErrorType.CONSTRAINT_VIOLATION,
                 "Tried to use data-dependent value in the subsequent computation. "
                 "This can happen when we encounter unbounded dynamic value that is unknown during tracing time."
                 "You will need to explicitly give hint to the compiler. Please take a look at "
@@ -1418,7 +1418,7 @@ def get_fake_value(node, tx):
                 case_name="constrain_as_size_example",
             )
         elif isinstance(cause, torch.utils._sympy.value_ranges.ValueRangeError):
-            raise UserError(UserErrorType.CONSTRAIN_VIOLATION, e.args[0]) from e
+            raise UserError(UserErrorType.CONSTRAINT_VIOLATION, e.args[0]) from e
         raise TorchRuntimeError(str(e)).with_traceback(e.__traceback__) from None
 
 
