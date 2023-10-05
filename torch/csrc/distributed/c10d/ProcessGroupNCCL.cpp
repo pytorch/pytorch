e1bb0e3d5a8df57eaf3c8f0ad13a77b6c00bb748
Rodrigo Kumpera <kumpera@meta.com>
2023-10-05 10:42:00 -0700

    Update base for Update on "[C10D] Report detected failures when emitting collective end events."
    We leverage the fact that errors get tracked in Work to report them.
    
    The error message by itself is not crazy useful and we might need to
    include some form of error code in the mix (nccl failure, timeout, etc).
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1082,7 +1082,7 @@ void ProcessGroupNCCL::runHookLoop() {
             timeStarted, // timeStarted
             std::chrono::system_clock::now(), // timeFinished
             std::chrono::duration<float, std::milli>(
-                work.getDuration()) // activeDuration
+                work.getDuration().value()) // activeDuration
             ));
 
         lock.lock();
@@ -1585,19 +1585,19 @@ c10::intrusive_ptr<c10::ivalue::Future> ProcessGroupNCCL::WorkNCCL::
   return future_;
 }
 
-float ProcessGroupNCCL::WorkNCCL::getDuration() const {
-  TORCH_CHECK(timingEnabled_, "getDuration only works if timing was enabled")
+c10::optional<float> ProcessGroupNCCL::WorkNCCL::getDuration() const {
+  if (!timingEnabled_ || !((*ncclEndEvents_)[0].query())) {
+    return c10::optional<float>();
+  }
   TORCH_CHECK(
       ncclStartEvents_->size() == 1,
       "getDuration only works for single device per ProcessGroup.");
   TORCH_CHECK(
       ncclEndEvents_->size() == 1,
       "getDuration only works for single device per ProcessGroup.");
-  TORCH_CHECK(
-      (*ncclEndEvents_)[0].query(),
-      "getDuration can only be called after work is succeeded.")
   return (*ncclStartEvents_)[0].elapsed_time((*ncclEndEvents_)[0]);
 }
+
 uint64_t ProcessGroupNCCL::WorkNCCL::getSequencenumber() const {
   return seq_;
 }
