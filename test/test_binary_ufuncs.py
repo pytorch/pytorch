a266f936c4ccba3831de5eaf00d47453bd4b40c9
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-10-06 16:25:26 +0000

    lint

--- test/test_binary_ufuncs.py
+++ test/test_binary_ufuncs.py
@@ -3468,7 +3468,6 @@ class TestBinaryUfuncs(TestCase):
         _test_helper(a, b)
         _test_helper(a[:3], b[:3])
 
-
         # large value test for numerical stability
         a *= 10000
         b *= 10000
