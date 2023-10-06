81ce5d5725c5413db054c0972d7dc252538ca2ef
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-10-06 01:03:17 +0000

    Revert "pin_memory support for NT (#110404)"
    This reverts commit 3597325bc7f07d97ded1c94c47bb59c98e080a0f.
    
    Reverted https://github.com/pytorch/pytorch/pull/110404 on behalf of https://github.com/kit1980 due to Previous PR in the stack caused CUDA memory leaks ([comment](https://github.com/pytorch/pytorch/pull/110404#issuecomment-1749850211))

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -2282,22 +2282,16 @@ class TestDataLoaderDeviceType(TestCase):
 
         dataset = [torch.nested.nested_tensor([torch.randn(5)], device=device) for _ in range(10)]
 
-        pin_memory_settings = [False]
-        if device == 'cpu' and torch.cuda.is_available():
-            pin_memory_settings.append(True)
+        loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=1,
+            num_workers=4,
+            collate_fn=_clone_collate,
+            multiprocessing_context=context,
+        )
 
-        for pin_memory in pin_memory_settings:
-            loader = torch.utils.data.DataLoader(
-                dataset,
-                batch_size=1,
-                num_workers=4,
-                collate_fn=_clone_collate,
-                pin_memory=pin_memory,
-                multiprocessing_context=context,
-            )
-
-            for i, batch in enumerate(loader):
-                self.assertEqual(batch[0], dataset[i])
+        for i, batch in enumerate(loader):
+            self.assertEqual(batch[0], dataset[i])
 
         # Error case: default collate_fn doesn't currently support batches of nested tensors.
         # Following the current semantics, we'd need to stack them, which isn't possible atm.
