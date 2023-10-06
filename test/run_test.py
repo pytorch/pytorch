16e76dfcd5521214fd0cb8198e0dea78c44c09ff
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2023-10-06 08:47:15 +0000

    Add test_jit_cuda_fuser to ROCM_BLOCKLIST (#110440)
    Adds the nvfuser related unit test suite to ROCM_BLOCKLIST as should not be run on ROCm.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110440
    Approved by: https://github.com/jeffdaily, https://github.com/pruthvistony, https://github.com/lezcano

--- test/run_test.py
+++ test/run_test.py
@@ -292,6 +292,7 @@ ROCM_BLOCKLIST = [
     "test_determination",
     "test_jit_legacy",
     "test_cuda_nvml_based_avail",
+    "test_jit_cuda_fuser",
 ]
 
 # The tests inside these files should never be run in parallel with each other
