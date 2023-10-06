cf51014a5fa012f741c3a8a972c4189de4fd9b56
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-10-06 09:36:56 -0700

    device agnostic: torch.cpu.set_device
    to support device agnostic, add a dummpy placeholder in torch.cpu
    
    [ghstack-poisoned]

--- torch/distributed/_tensor/device_mesh.py
+++ torch/distributed/_tensor/device_mesh.py
@@ -93,10 +93,10 @@ def _get_device_handle(device_type: str = "cuda"):
     """
     Get the module corresponding to the device_type which is cuda or cuda-like device.
     For example, when the device_type is cuda, the module `torch.cuda` is returned.
-    Return None when device_type is cpu or there is no corresponding module,
-    otherwise return the corresponding module.
+    Return None when there is no corresponding module for device_type, otherwise
+    return the corresponding module.
     """
-    return getattr(torch, device_type, None) if device_type != "cpu" else None
+    return getattr(torch, device_type, None)
 
 
 class DeviceMesh:
