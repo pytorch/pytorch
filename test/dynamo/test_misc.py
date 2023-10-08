fa8e4ea2122f45d59b6a7e748ecd3c157e504024
cdzhan <zhancdi@163.com>
2023-10-08 05:33:56 +0000

    Add support for hasattr on ListVariable (#110438)
    Fixes #109502
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110438
    Approved by: https://github.com/jansel

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -6706,6 +6706,32 @@ def ___make_guard_fn():
         compiled_out = compiled_fn(x)
         self.assertTrue(same(fn_out, compiled_out))
 
+    def test_list_hasattr1(self):
+        def fn(x):
+            if hasattr(x, "foo"):
+                return x[0] + 1
+            return x[0] - 1
+
+        compiled_fn = torch.compile(backend="eager", fullgraph=True)(fn)
+
+        x = [torch.randn(3)]
+        fn_out = fn(x)
+        compiled_out = compiled_fn(x)
+        self.assertTrue(same(fn_out, compiled_out))
+
+    def test_list_hasattr2(self):
+        def fn():
+            x = [torch.zeros(3)]
+            if hasattr(x, "__len__"):
+                return x[0] + 1
+            return x[0] - 1
+
+        compiled_fn = torch.compile(backend="eager", fullgraph=True)(fn)
+
+        fn_out = fn()
+        compiled_out = compiled_fn()
+        self.assertTrue(same(fn_out, compiled_out))
+
     def test_torch_objects_as_keys(self):
         remap = {torch.float16: torch.float32}
 
