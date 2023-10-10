b17c247eb12c375ad2b29f6d830de6bcd209d526
Bin Bao <binbao@meta.com>
2023-10-09 20:33:13 -0700

    [aotindutor] Update the cpp test example (#110652)
    Summary: store inputs and outpus in python, and load them back to run the compiled model in c++ and compare the output
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110652
    Approved by: https://github.com/chenyang78

--- test/cpp/aot_inductor/test.py
+++ test/cpp/aot_inductor/test.py
@@ -3,33 +3,44 @@ import shutil
 import torch
 from torch._export import aot_compile, dynamic_dim
 
+torch.manual_seed(1337)
 
 class Net(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.fc = torch.nn.Linear(64, 10)
-        weights = torch.arange(640)
-        weights = torch.reshape(weights, (10, 64))
-
-        with torch.no_grad():
-            self.fc.weight.copy_(weights)
-            self.fc.bias.copy_(torch.zeros(10))
 
     def forward(self, x, y):
         return self.fc(torch.sin(x) + torch.cos(y))
 
-
+model = Net().to(device="cuda")
 x = torch.randn((32, 64), device="cuda")
 y = torch.randn((32, 64), device="cuda")
+with torch.no_grad():
+    ref_output = model(x, y)
 
 torch._dynamo.reset()
-
 with torch.no_grad():
     constraints = [
         dynamic_dim(x, 0) >= 1,
         dynamic_dim(x, 0) <= 1024,
         dynamic_dim(x, 0) == dynamic_dim(y, 0),
     ]
-    lib_path, module = aot_compile(Net().cuda(), (x, y), constraints=constraints)
+    lib_path, module = aot_compile(model, (x, y), constraints=constraints)
 
-shutil.copy(lib_path, "libaot_inductor_output.so")
+shutil.copy(lib_path, "libmodel.so")
+
+
+# Use this to communicate tensors to the cpp code
+class Serializer(torch.nn.Module):
+    def __init__(self, tensors):
+        super().__init__()
+        for key in tensors:
+            setattr(self, key, tensors[key])
+
+io_tensors = {
+    "inputs": [x, y],
+    "outputs": [ref_output],
+}
+
+torch.jit.script(Serializer(io_tensors)).save("io_tensors.pt")
