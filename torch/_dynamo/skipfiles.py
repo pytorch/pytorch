57b7b6382d4aaf72355d453aaaf2446760e10fda
Yanbo Liang <ybliang8@gmail.com>
2023-10-10 18:04:15 -0700

    Update

--- torch/_dynamo/skipfiles.py
+++ torch/_dynamo/skipfiles.py
@@ -399,11 +399,6 @@ _recompile_re()
 
 
 def is_torch_inline_allowed(filename):
-    if torch.distributed.is_available():
-        from torch.distributed import _functional_collectives
-
-        SUBMODULE_INLINELIST.add(_functional_collectives)
-
     return any(filename.startswith(_module_dir(mod)) for mod in SUBMODULE_INLINELIST)
 
 
