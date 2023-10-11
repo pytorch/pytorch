85d658c595680ccf4aa03acfec93acaea567e82d
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-10-11 07:31:51 +0000

    Revert "[export] Get export APIs ready for PTC (#110410)"
    This reverts commit b96ea9f361f2ed872c4a7d662427cadec345b702.
    
    Reverted https://github.com/pytorch/pytorch/pull/110410 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/110410#issuecomment-1757017249))

--- test/export/test_serialize.py
+++ test/export/test_serialize.py
@@ -100,7 +100,7 @@ class TestSerialize(TestCase):
 
         input = torch.arange(10.0).reshape(5, 2)
         input.requires_grad = True
-        exported_module = export(MyModule(), (input,)).run_decompositions()
+        exported_module = export(MyModule(), (input,))
 
         serialized, _ = ExportedProgramSerializer().serialize(exported_module)
         node = serialized.graph_module.graph.nodes[-1]
