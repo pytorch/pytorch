d4cff6cd99d3bf2c7fd760e8f910cd160b558c2f
Masaki Kozuki <mkozuki@nvidia.com>
2023-10-12 07:28:35 -0700

    format fused sgd
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- aten/src/ATen/native/cuda/FusedSgdKernel.cu
+++ aten/src/ATen/native/cuda/FusedSgdKernel.cu
@@ -2,10 +2,9 @@
 #include <ATen/OpMathType.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/native/ForeachUtils.h>
+#include <c10/util/Exception.h>
 #include <ATen/native/cuda/ForeachFunctors.cuh>
 #include <ATen/native/cuda/MultiTensorApply.cuh>
-#include <c10/util/Exception.h>
-
 
 namespace at::native {
 
@@ -21,8 +20,7 @@ C10_DEVICE __forceinline__ void sgd_math(
     const bool nesterov,
     const bool maximize,
     const bool is_first_step,
-    const float *grad_scale_ptr
-) {
+    const float* grad_scale_ptr) {
   using opmath_t = at::opmath_type<scalar_t>;
 #pragma unroll
   for (int ii = 0; ii < kILP; ii++) {
@@ -36,8 +34,10 @@ C10_DEVICE __forceinline__ void sgd_math(
       g += weight_decay * p;
     }
     if (depth > 2) {
-      const auto momentum_buffer = is_first_step ?
-        g : (momentum * static_cast<opmath_t>(r_args[2][ii]) + (1 - dampening) * g);
+      const auto momentum_buffer = is_first_step
+          ? g
+          : (momentum * static_cast<opmath_t>(r_args[2][ii]) +
+             (1 - dampening) * g);
       r_args[2][ii] = momentum_buffer;
 
       if (nesterov) {
@@ -57,7 +57,9 @@ C10_DEVICE __forceinline__ void sgd_math(
 
 template <typename scalar_t, int depth>
 struct FusedSgdMathFunctor {
-  static_assert(depth == 2 || depth == 3, "depth of 2 for SGD w/ momentum == 0, 3 for SGD w/ momentum != 0");
+  static_assert(
+      depth == 2 || depth == 3,
+      "depth of 2 for SGD w/ momentum == 0, 3 for SGD w/ momentum != 0");
   C10_DEVICE __forceinline__ void operator()(
       int chunk_size,
       TensorListMetadata<depth>& tl,
@@ -68,9 +70,8 @@ struct FusedSgdMathFunctor {
       const bool nesterov,
       const bool maximize,
       const bool is_first_step,
-      const float *grad_scale_ptr,
-      const float *found_inf_ptr
-  ) {
+      const float* grad_scale_ptr,
+      const float* found_inf_ptr) {
     if (found_inf_ptr && *found_inf_ptr == 1) {
       return;
     }
@@ -80,17 +81,30 @@ struct FusedSgdMathFunctor {
 
     scalar_t* args[depth];
     scalar_t r_args[depth][kILP];
-    const auto all_aligned{init_args<depth>(args, tl, chunk_idx, chunk_size, tensor_loc)};
+    const auto all_aligned{
+        init_args<depth>(args, tl, chunk_idx, chunk_size, tensor_loc)};
     n -= chunk_idx * chunk_size;
 
-    const auto use_faster_load_store = (n % kILP == 0) && (chunk_size % kILP == 0) && all_aligned;
+    const auto use_faster_load_store =
+        (n % kILP == 0) && (chunk_size % kILP == 0) && all_aligned;
     if (use_faster_load_store) {
-      for (auto i_start = threadIdx.x; i_start * kILP < n && i_start * kILP < chunk_size; i_start += blockDim.x) {
+      for (auto i_start = threadIdx.x;
+           i_start * kILP < n && i_start * kILP < chunk_size;
+           i_start += blockDim.x) {
 #pragma unroll
         for (auto i = 0; i < depth; i++) {
           load_store(r_args[i], args[i], 0, i_start);
         }
-        sgd_math<scalar_t, depth>(r_args, weight_decay, momentum, lr, dampening, nesterov, maximize, is_first_step, grad_scale_ptr);
+        sgd_math<scalar_t, depth>(
+            r_args,
+            weight_decay,
+            momentum,
+            lr,
+            dampening,
+            nesterov,
+            maximize,
+            is_first_step,
+            grad_scale_ptr);
         load_store(args[0], r_args[0], i_start, 0);
         if (grad_scale_ptr) {
           load_store(args[1], r_args[1], i_start, 0);
@@ -100,9 +114,19 @@ struct FusedSgdMathFunctor {
         }
       }
     } else {
-      for (auto i_start = 0; i_start < n && i_start < chunk_size; i_start += blockDim.x * kILP) {
+      for (auto i_start = 0; i_start < n && i_start < chunk_size;
+           i_start += blockDim.x * kILP) {
         load_args<depth>(r_args, args, i_start, chunk_size, n);
-        sgd_math<scalar_t, depth>(r_args, weight_decay, momentum, lr, dampening, nesterov, maximize, is_first_step, grad_scale_ptr);
+        sgd_math<scalar_t, depth>(
+            r_args,
+            weight_decay,
+            momentum,
+            lr,
+            dampening,
+            nesterov,
+            maximize,
+            is_first_step,
+            grad_scale_ptr);
         store_args(args[0], r_args[0], i_start, chunk_size, n);
         if (grad_scale_ptr) {
           store_args(args[1], r_args[1], i_start, chunk_size, n);
@@ -115,8 +139,7 @@ struct FusedSgdMathFunctor {
   }
 };
 
-
-} // namespace (anonymous)
+} // namespace
 
 void _fused_sgd_kernel_cuda_(
     at::TensorList params,
@@ -128,16 +151,21 @@ void _fused_sgd_kernel_cuda_(
     const bool nesterov,
     const bool maximize,
     const c10::optional<at::Tensor>& grad_scale,
-    const c10::optional<at::Tensor>& found_inf
-) {
+    const c10::optional<at::Tensor>& found_inf) {
   TORCH_CHECK_EQ(momentum, 0);
   TORCH_CHECK(at::native::check_fast_path_restrictions({params, grads}));
-  float *grad_scale_ptr = grad_scale.has_value() ? grad_scale->data_ptr<float>() : nullptr;
-  float *found_inf_ptr = found_inf.has_value() ? found_inf->data_ptr<float>() : nullptr;
+  float* grad_scale_ptr =
+      grad_scale.has_value() ? grad_scale->data_ptr<float>() : nullptr;
+  float* found_inf_ptr =
+      found_inf.has_value() ? found_inf->data_ptr<float>() : nullptr;
 
   std::vector<std::vector<at::Tensor>> tensor_lists{params.vec(), grads.vec()};
-  AT_DISPATCH_FLOATING_TYPES_AND2(kHalf, kBFloat16, params[0].scalar_type(),
-      "fused_sgd_kernel_cuda", [&]() {
+  AT_DISPATCH_FLOATING_TYPES_AND2(
+      kHalf,
+      kBFloat16,
+      params[0].scalar_type(),
+      "fused_sgd_kernel_cuda",
+      [&]() {
         multi_tensor_apply<2>(
             tensor_lists,
             FusedSgdMathFunctor<scalar_t, 2>(),
@@ -150,7 +178,7 @@ void _fused_sgd_kernel_cuda_(
             /* is_first_step */ false,
             grad_scale_ptr,
             found_inf_ptr);
-        });
+      });
 }
 
 void _fused_sgd_with_momentum_kernel_cuda_(
@@ -165,16 +193,23 @@ void _fused_sgd_with_momentum_kernel_cuda_(
     const bool maximize,
     const bool is_first_step,
     const c10::optional<at::Tensor>& grad_scale,
-    const c10::optional<at::Tensor>& found_inf
-) {
+    const c10::optional<at::Tensor>& found_inf) {
   TORCH_CHECK_GT(momentum, 0);
-  TORCH_CHECK(at::native::check_fast_path_restrictions({params, grads, momentum_buffer_list}));
-  float *grad_scale_ptr = grad_scale.has_value() ? grad_scale->data_ptr<float>() : nullptr;
-  float *found_inf_ptr = found_inf.has_value() ? found_inf->data_ptr<float>() : nullptr;
+  TORCH_CHECK(at::native::check_fast_path_restrictions(
+      {params, grads, momentum_buffer_list}));
+  float* grad_scale_ptr =
+      grad_scale.has_value() ? grad_scale->data_ptr<float>() : nullptr;
+  float* found_inf_ptr =
+      found_inf.has_value() ? found_inf->data_ptr<float>() : nullptr;
 
-  std::vector<std::vector<at::Tensor>> tensor_lists{params.vec(), grads.vec(), momentum_buffer_list.vec()};
-  AT_DISPATCH_FLOATING_TYPES_AND2(kHalf, kBFloat16, params[0].scalar_type(),
-      "fused_sgd_with_momentum_kernel_cuda", [&]() {
+  std::vector<std::vector<at::Tensor>> tensor_lists{
+      params.vec(), grads.vec(), momentum_buffer_list.vec()};
+  AT_DISPATCH_FLOATING_TYPES_AND2(
+      kHalf,
+      kBFloat16,
+      params[0].scalar_type(),
+      "fused_sgd_with_momentum_kernel_cuda",
+      [&]() {
         multi_tensor_apply<3>(
             tensor_lists,
             FusedSgdMathFunctor<scalar_t, 3>(),
@@ -187,7 +222,7 @@ void _fused_sgd_with_momentum_kernel_cuda_(
             is_first_step,
             grad_scale_ptr,
             found_inf_ptr);
-        });
+      });
 }
 
 } // namespace at::native
