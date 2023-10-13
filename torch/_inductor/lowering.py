f94e9b82f975aed05660b1abc5e8e4f5495ca36a
Edward Z. Yang <ezyang@meta.com>
2023-10-13 13:20:54 -0400

    Don't sympify reflection_pad2d ranges
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -3224,7 +3224,7 @@ def reflection_pad2d(x, padding):
         device=x.get_device(),
         dtype=x.get_dtype(),
         inner_fn=fn,
-        ranges=[*batch, sympy.sympify(h + top + bot), sympy.sympify(w + left + right)],
+        ranges=[*batch, h + top + bot, w + left + right],
     )
 
 
