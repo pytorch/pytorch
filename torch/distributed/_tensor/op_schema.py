91b00d4b51efa49d33bdb87ea3c761af48d94f14
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-10-13 21:05:27 -0700

    [dtensor][11/n] adds some __str__ for ease of read
    This add __Str__ to op schema and dtensor spec for ease of reading
    
    [ghstack-poisoned]

--- torch/distributed/_tensor/op_schema.py
+++ torch/distributed/_tensor/op_schema.py
@@ -207,6 +207,24 @@ class OpSchema:
             f" kwargs_schema={self.kwargs_schema})"
         )
 
+    def __str__(self) -> str:
+        args_sharding: List[str] = []
+        mesh = None
+        for spec in self.args_schema:
+            if isinstance(spec, DTensorSpec):
+                args_sharding.append(str(spec))
+                mesh = spec.mesh
+            elif isinstance(spec, OpStrategy):
+                assert len(spec.strategies) == 1
+                arg_spec = spec.strategies[0].output_spec
+                args_sharding.append(str(arg_spec))
+                mesh = arg_spec.mesh
+            else:
+                args_sharding.append(str(spec))
+        return (
+            f"Op(op={self.op}, args_sharding={','.join(args_sharding)}, @ mesh:{mesh})"
+        )
+
     def __post_init__(self) -> None:
         has_symints = False
         for a in self.args_schema:
