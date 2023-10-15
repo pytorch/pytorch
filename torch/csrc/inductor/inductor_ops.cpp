6afd29f223bf4b1d2daef8ab4725d48e555d8ca3
Jason Ansel <jansel@meta.com>
2023-10-15 14:31:24 -0700

    Jansel ops apply

--- torch/csrc/inductor/inductor_ops.cpp
+++ torch/csrc/inductor/inductor_ops.cpp
@@ -62,8 +62,7 @@ TORCH_LIBRARY_FRAGMENT(inductor, m) {
       dispatch(c10::DispatchKey::CompositeExplicitAutograd, _mm_plus_mm));
   m.def(
       "_reinterpret_tensor(Tensor self, int[] size, int[] stride, int offset_increment=0) -> Tensor",
-      dispatch(
-          c10::DispatchKey::CompositeExplicitAutograd, _reinterpret_tensor));
+      _reinterpret_tensor);
   m.def(
       "accumulate_grad_(Tensor variable, Tensor new_grad) -> ()",
       dispatch(c10::DispatchKey::CompositeExplicitAutograd, accumulate_grad_));
