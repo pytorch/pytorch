f7e74075eea40be960abf4a08e863e5447ba5a84
voz <voz@devgpu023.odn1.facebook.com>
2023-10-18 19:37:13 -0700

    rm name log

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -255,7 +255,6 @@ class GuardBuilder(GuardBuilderBase):
     # (like its type) which is what you permanently install into the
     # guard code.
     def get(self, name: str) -> Any:
-        print("NAME?", name)
         return eval(name, self.scope, CLOSURE_VARS)
 
     # Registers the usage of the source name referenced by the
