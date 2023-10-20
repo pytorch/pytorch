44d19a0c40e63661973707678dce3f081154d454
George White <georgew@graphcore.ai>
2023-10-20 11:19:08 +0100

    Remove even more changes

--- aten/src/ATen/NumericUtils.h
+++ aten/src/ATen/NumericUtils.h
@@ -7,9 +7,7 @@
 #include <c10/macros/Macros.h>
 #include <c10/util/BFloat16.h>
 #include <c10/util/Float8_e4m3fn.h>
-#include <c10/util/Float8_e4m3fnuz.h>
 #include <c10/util/Float8_e5m2.h>
-#include <c10/util/Float8_e5m2fnuz.h>
 #include <c10/util/Half.h>
 #include <c10/util/complex.h>
 
@@ -76,23 +74,7 @@ inline C10_HOST_DEVICE bool _isnan(T val) {
 
 template <
     typename T,
-    typename std::enable_if<std::is_same<T, at::Float8_e5m2fnuz>::value, int>::
-        type = 0>
-inline C10_HOST_DEVICE bool _isnan(T val) {
-  return val.isnan();
-}
-
-template <
-    typename T,
     typename std::enable_if<std::is_same<T, at::Float8_e4m3fn>::value, int>::
-        type = 0>
-inline C10_HOST_DEVICE bool _isnan(T val) {
-  return val.isnan();
-}
-
-template <
-    typename T,
-    typename std::enable_if<std::is_same<T, at::Float8_e4m3fnuz>::value, int>::
         type = 0>
 inline C10_HOST_DEVICE bool _isnan(T val) {
   return val.isnan();
@@ -132,15 +114,7 @@ inline C10_HOST_DEVICE bool _isinf(at::Float8_e5m2 val) {
   return val.isinf();
 }
 
-inline C10_HOST_DEVICE bool _isinf(at::Float8_e5m2fnuz val) {
-  return false;
-}
-
 inline C10_HOST_DEVICE bool _isinf(at::Float8_e4m3fn val) {
-  return false;
-}
-
-inline C10_HOST_DEVICE bool _isinf(at::Float8_e4m3fnuz val) {
   return false;
 }
 
