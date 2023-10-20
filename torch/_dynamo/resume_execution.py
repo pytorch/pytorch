ada04772c29569678b308da199fa4143b1c2153f
Kaichao You <youkaichao@gmail.com>
2023-10-20 18:54:47 +0000

    [Dynamo]make sure resume function have valid names (#111635)
    An ongoing effort for https://github.com/pytorch/pytorch/issues/111633 .
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/111635
    Approved by: https://github.com/ezyang, https://github.com/jansel

--- torch/_dynamo/resume_execution.py
+++ torch/_dynamo/resume_execution.py
@@ -365,11 +365,9 @@ class ContinueExecutionCache:
             freevars = tuple(code_options["co_cellvars"] or []) + tuple(
                 code_options["co_freevars"] or []
             )
-            code_options["co_name"] = f"<resume in {code_options['co_name']}>"
+            code_options["co_name"] = f"resume_in_{code_options['co_name']}"
             if is_py311_plus:
-                code_options[
-                    "co_qualname"
-                ] = f"<resume in {code_options['co_qualname']}>"
+                code_options["co_qualname"] = f"resume_in_{code_options['co_qualname']}"
             code_options["co_firstlineno"] = lineno
             code_options["co_cellvars"] = tuple()
             code_options["co_freevars"] = freevars
