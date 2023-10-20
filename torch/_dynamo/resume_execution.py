30d5a5e210366a94ce68b236729d24778e97d1c4
Kaichao You <youkaichao@gmail.com>
2023-10-20 06:25:25 +0000

    fix lint

--- torch/_dynamo/resume_execution.py
+++ torch/_dynamo/resume_execution.py
@@ -367,9 +367,7 @@ class ContinueExecutionCache:
             )
             code_options["co_name"] = f"resume_in_{code_options['co_name']}"
             if is_py311_plus:
-                code_options[
-                    "co_qualname"
-                ] = f"resume_in_{code_options['co_qualname']}"
+                code_options["co_qualname"] = f"resume_in_{code_options['co_qualname']}"
             code_options["co_firstlineno"] = lineno
             code_options["co_cellvars"] = tuple()
             code_options["co_freevars"] = freevars
