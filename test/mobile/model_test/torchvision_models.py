5eb0bb1c4ff3250cbca209ffe0ee12be8fb378df
Sergii Dymchenko <sdym@fb.com>
2023-10-20 14:45:57 -0700

    Simplify imports

--- test/mobile/model_test/torchvision_models.py
+++ test/mobile/model_test/torchvision_models.py
@@ -1,5 +1,4 @@
 import torch
-import torchvision
 from torch.utils.bundled_inputs import augment_model_with_bundled_inputs
 from torch.utils.mobile_optimizer import optimize_for_mobile
 from torchvision import models
@@ -7,7 +6,7 @@ from torchvision import models
 
 class MobileNetV2Module:
     def getModule(self):
-        model = torchvision.models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
         model.eval()
         example = torch.zeros(1, 3, 224, 224)
         traced_script_module = torch.jit.trace(model, example)
@@ -24,7 +23,7 @@ class MobileNetV2Module:
 
 class MobileNetV2VulkanModule:
     def getModule(self):
-        model = torchvision.models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
         model.eval()
         example = torch.zeros(1, 3, 224, 224)
         traced_script_module = torch.jit.trace(model, example)
@@ -41,7 +40,7 @@ class MobileNetV2VulkanModule:
 
 class Resnet18Module:
     def getModule(self):
-        model = torchvision.models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+        model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
         model.eval()
         example = torch.zeros(1, 3, 224, 224)
         traced_script_module = torch.jit.trace(model, example)
