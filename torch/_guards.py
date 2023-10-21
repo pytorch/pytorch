7435b5c20800c65251486ab967fdd1fa9822de73
voznesenskym <voznesenskym@gmail.com>
2023-10-20 18:00:56 -0700

    [easy][dynamo][wait for ci] Cleanup guard builder selection
    [ghstack-poisoned]

--- torch/_guards.py
+++ torch/_guards.py
@@ -77,29 +77,6 @@ class GuardSource(enum.Enum):
     LOCAL_FSDP_MODULE = 7
     GLOBAL_FSDP_MODULE = 8
 
-    def select(self, locals_, globals_):
-        # SHAPE_ENV counts as locals, because the guard expressions
-        # created by shape env can reference f_locals
-        #
-        # RANDOM_VALUE counts as locals, because what we do is we run
-        # Python RNG and assign it to a temporary, and then perform
-        # guard tests on that temporary
-        if self in (
-            GuardSource.LOCAL,
-            GuardSource.LOCAL_NN_MODULE,
-            GuardSource.LOCAL_FSDP_MODULE,
-            GuardSource.SHAPE_ENV,
-            GuardSource.RANDOM_VALUE,
-        ):
-            return locals_
-        if self in (
-            GuardSource.GLOBAL,
-            GuardSource.GLOBAL_NN_MODULE,
-            GuardSource.GLOBAL_FSDP_MODULE,
-        ):
-            return globals_
-        raise NotImplementedError(str(self))
-
     def is_fsdp_module(self) -> bool:
         return self in (GuardSource.GLOBAL_FSDP_MODULE, GuardSource.LOCAL_FSDP_MODULE)
 
@@ -255,8 +232,8 @@ class Guard:
         output += f"    Guarded Class Weakref: {self.guarded_class_weakref}\n"
         return output
 
-    def create(self, local_builder: GuardBuilderBase, global_builder: GuardBuilderBase):
-        return self.create_fn(self.source.select(local_builder, global_builder), self)
+    def create(self, builder: GuardBuilderBase):
+        return self.create_fn(builder, self)
 
     def is_nn_module(self):
         return self.source.is_nn_module()
