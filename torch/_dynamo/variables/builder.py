8625045ce0393969791687931702689914b759bf
drisspg <drisspguessous@gmail.com>
2023-10-23 13:03:17 -0700

    who knows

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -1850,6 +1850,8 @@ class SourcelessBuilder:
             return cls([self(tx, x) for x in value], mutable_local=MutableLocal())
         elif isinstance(value, types.MethodWrapperType):
             return MethodWrapperVariable(value)
+        elif isinstance(value, torch.dtype):
+            return ConstantVariable.create(value)
         unimplemented(f"Unexpected type in sourceless builder {type(value)}")
 
     @staticmethod
