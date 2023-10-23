4f92eb8c5b1f8575555150095981f376b3f39d40
Catherine Lee <csl@fb.com>
2023-10-23 14:29:39 -0700

    update

--- tools/testing/target_determination/heuristics/profiling.py
+++ tools/testing/target_determination/heuristics/profiling.py
@@ -1,26 +1,23 @@
 from typing import Any, Dict, List
 
-from tools.stats.import_test_stats import (
-    ADDITIONAL_CI_FILES_FOLDER,
-    TEST_FILE_RATINGS_FILE,
-)
+from tools.stats.import_test_stats import ADDITIONAL_CI_FILES_FOLDER, TD_HEURISTIC_PROFILING_FILE
 
 from tools.testing.target_determination.heuristics.interface import (
     HeuristicInterface,
     TestPrioritizations,
 )
 
-from tools.testing.target_determination.heuristics.utils import get_correlated_tests
+from tools.testing.target_determination.heuristics.utils import (
+    get_correlated_tests,
+)
 
 
-class CorrelatedWithHistoricalFailures(HeuristicInterface):
+class Profiling(HeuristicInterface):
     def __init__(self, **kwargs: Dict[str, Any]):
         super().__init__(**kwargs)
 
     def get_test_priorities(self, tests: List[str]) -> TestPrioritizations:
-        correlated_tests = get_correlated_tests(
-            ADDITIONAL_CI_FILES_FOLDER / TEST_FILE_RATINGS_FILE
-        )
+        correlated_tests = get_correlated_tests(ADDITIONAL_CI_FILES_FOLDER / TD_HEURISTIC_PROFILING_FILE)
         relevant_correlated_tests = [test for test in correlated_tests if test in tests]
         test_rankings = TestPrioritizations(
             tests_being_ranked=tests, probable_relevance=relevant_correlated_tests
