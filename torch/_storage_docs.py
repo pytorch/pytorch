7c3aeaaf9d56c3baaf4177683375e045d82363f6
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2023-10-23 18:42:18 -0700

    Update base for Update on "Clarify difference between share_memory and from_file"
    [ghstack-poisoned]

--- torch/_storage_docs.py
+++ torch/_storage_docs.py
@@ -21,23 +21,23 @@ def add_docstr_all(method, docstr):
 add_docstr_all(
     "from_file",
     """
-    from_file(filename, shared=False, size=0) -> Storage
+from_file(filename, shared=False, size=0) -> Storage
 
-    Creates a CPU storage backed by a memory-mapped file.
+Creates a CPU storage backed by a memory-mapped file.
 
-    If ``shared`` is ``True``, then memory is shared between all processes.
-    All changes are written to the file. If ``shared`` is ``False``, then the changes on
-    the storage do not affect the file.
+If ``shared`` is ``True``, then memory is shared between all processes.
+All changes are written to the file. If ``shared`` is ``False``, then the changes on
+the storage do not affect the file.
 
-    ``size`` is the number of elements in the storage. If ``shared`` is ``False``,
-    then the file must contain at least :math:`size * sizeof(Type)` bytes
-    (``Type`` is the type of storage, in the case of an ``UnTypedStorage`` the file must contain at
-    least ``size`` bytes). If ``shared`` is ``True`` the file will be created if needed.
+``size`` is the number of elements in the storage. If ``shared`` is ``False``,
+then the file must contain at least :math:`size * sizeof(Type)` bytes
+(``Type`` is the type of storage, in the case of an ``UnTypedStorage`` the file must contain at
+least ``size`` bytes). If ``shared`` is ``True`` the file will be created if needed.
 
-    Args:
-        filename (str): file name to map
-        shared (bool): whether to share memory (whether ``MAP_SHARED`` or ``MAP_PRIVATE`` is passed to the
-                       underlying `mmap(2) call <https://man7.org/linux/man-pages/man2/mmap.2.html>`_)
-        size (int): number of elements in the storage
+Args:
+    filename (str): file name to map
+    shared (bool): whether to share memory (whether ``MAP_SHARED`` or ``MAP_PRIVATE`` is passed to the
+                    underlying `mmap(2) call <https://man7.org/linux/man-pages/man2/mmap.2.html>`_)
+    size (int): number of elements in the storage
     """,
 )
