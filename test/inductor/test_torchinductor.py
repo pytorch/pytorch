f6230d00d094ad35e8f04668aad04641963f5946
Hongtao Yu <hoy@fb.com>
2023-10-24 10:58:10 -0700

    Tweaking the test.

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -621,6 +621,7 @@ class CommonTemplate:
         self.common(fn, (x, y, 2))
 
     def test_add_complex2(self):
+        @torch.compile
         def fn(a, b):
             c = a + b
             d = a + b
@@ -629,7 +630,8 @@ class CommonTemplate:
         x = torch.tensor([1 + 1j, -1 + 1j, -2 + 2j, 3 - 3j, 0, 1j, 1, -1])
         y = torch.tensor([1 + 1j, -1 + 1j, -2 + 2j, 3 - 3j, 0, 1j, 1, -1])
 
-        self.common(fn, (x, y))
+        _, code = run_and_get_code(fn, x, y)
+        self.assertEqual(code[0].count("aten.view"), 3)
 
     def test_concat_add_inplace(self):
         def fn(x, y, z):
