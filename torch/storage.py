5fb07cdb94bdb6a129954744993bd24ce6fc3a2b
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2023-10-25 09:35:09 -0700

    Update base for Update on "Clarify difference between share_memory and from_file"
    [ghstack-poisoned]

--- torch/storage.py
+++ torch/storage.py
@@ -1069,7 +1069,7 @@ class TypedStorage:
         the storage do not affect the file.
 
         ``size`` is the number of elements in the storage. If ``shared`` is ``False``,
-        then the file must contain at least :math:`size * sizeof(Type)` bytes
+        then the file must contain at least ``size * sizeof(Type)`` bytes
         (``Type`` is the type of storage). If ``shared`` is ``True`` the file will be created if needed.
 
         Args:
