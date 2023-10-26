a8fa6c2d81416b299f4111d445a92b585a35b2c9
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2023-10-26 16:41:36 -0700

    Update on "Add config to disable TransformerEncoder/MHA fastpath"
    [ghstack-poisoned]

--- torch/backends/mha/__init__.py
+++ torch/backends/mha/__init__.py
@@ -1,8 +1,8 @@
-_is_fastpath_enabled = True
+_is_fastpath_enabled: bool = True
 
-def get_fastpath_enabled():
+def get_fastpath_enabled() -> bool:
     return _is_fastpath_enabled
 
-def set_fastpath_enabled(value):
+def set_fastpath_enabled(value: bool) -> None:
     global _is_fastpath_enabled
     _is_fastpath_enabled = value
