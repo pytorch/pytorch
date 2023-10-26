1ebfab1c805b59ee9f07174a07d75cb5765455ce
Patrick <patwang@nvidia.com>
2023-10-25 20:03:34 -0700

    clean up comments, add missing

--- c10/cuda/driver_api.cpp
+++ c10/cuda/driver_api.cpp
@@ -17,27 +17,17 @@ DriverAPI create_driver_api() {
     nvml_is_available = false;
   }
 
-/*
-#define OPEN_LIBRARIES(name, n)               \
-  void* handle_##n = dlopen(name, RTLD_LAZY); \
-  TORCH_INTERNAL_ASSERT(handle_##n);
-
-  C10_FORALL_DRIVER_LIBRARIES(OPEN_LIBRARIES)
-#undef OPEN_LIBRARIES
-*/
   DriverAPI r{};
 
-// #define LOOKUP_ENTRY(name, n)                              \
-//  r.name##_ = ((decltype(&name))dlsym(handle_##n, #name)); 
 #define LOOKUP_LIBCUDA_ENTRY(name) \
-  r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); 
+  r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); \
   TORCH_INTERNAL_ASSERT(r.name##_)
   C10_LIBCUDA_DRIVER_API(LOOKUP_LIBCUDA_ENTRY)
 #undef LOOKUP_LIBCUDA_ENTRY
 
 if (nvml_is_available) {
 #define LOOKUP_NVML_ENTRY(name) \
-  r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); 
+  r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
   TORCH_INTERNAL_ASSERT(r.name##_)
   C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
 #undef LOOKUP_NVML_ENTRY
--- c10/cuda/driver_api.h
+++ c10/cuda/driver_api.h
@@ -18,27 +18,6 @@
     }                                                                      \
   } while (0)
 
-/*
-#define C10_FORALL_DRIVER_LIBRARIES(_) \
-  _("libcuda.so", 0)                   \
-  _("libnvidia-ml.so.1", 1)
-*/
-
-/*
-#define C10_FORALL_DRIVER_API(_)         \
-  _(cuMemAddressReserve, 0)              \
-  _(cuMemRelease, 0)                     \
-  _(cuMemMap, 0)                         \
-  _(cuMemAddressFree, 0)                 \
-  _(cuMemSetAccess, 0)                   \
-  _(cuMemUnmap, 0)                       \
-  _(cuMemCreate, 0)                      \
-  _(cuGetErrorString, 0)                 \
-  _(nvmlInit_v2, 1)                      \
-  _(nvmlDeviceGetHandleByPciBusId_v2, 1) \
-  _(nvmlDeviceGetComputeRunningProcesses, 1)
-*/
-
 #define C10_LIBCUDA_DRIVER_API(_) \
   _(cuMemAddressReserve)         \
   _(cuMemRelease)                \
@@ -58,9 +37,7 @@ namespace c10 {
 namespace cuda {
 
 struct DriverAPI {
-// #define CREATE_MEMBER(name, n) decltype(&name) name##_;
 #define CREATE_MEMBER(name) decltype(&name) name##_;
-  // C10_FORALL_DRIVER_API(CREATE_MEMBER)
   C10_LIBCUDA_DRIVER_API(CREATE_MEMBER)
   C10_NVML_DRIVER_API(CREATE_MEMBER)
 #undef CREATE_MEMBER
