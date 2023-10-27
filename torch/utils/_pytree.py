a4b73ecb542aec73f2eb11722af9567d40a425b4
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-10-28 00:37:25 +0800

    Update base for Update on "[pytree] register pytree node type in both C++ pytree and Python pytree"
    cc zou3519 avikchaudhuri gmagogsfm zhxchen17 tugsbayasgalan angelayi
    
    [ghstack-poisoned]

--- torch/utils/_pytree.py
+++ torch/utils/_pytree.py
@@ -48,9 +48,6 @@ Context = Any
 PyTree = Any
 FlattenFunc = Callable[[PyTree], Tuple[List, Context]]
 UnflattenFunc = Callable[[Iterable, Context], PyTree]
-DumpableContext = Any  # Any json dumpable text
-ToDumpableContextFn = Callable[[Context], DumpableContext]
-FromDumpableContextFn = Callable[[DumpableContext], Context]
 ToStrFunc = Callable[["TreeSpec", List[str]], str]
 MaybeFromStrFunc = Callable[[str], Optional[Tuple[Any, Context, str]]]
 
