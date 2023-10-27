c5ee220bd5472759ddb2d5da72c60fa7854a2546
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-10-27 01:37:31 +0000

    Revert "Readded device_assert skipping in index and index_put (and also added (#112093)"
    This reverts commit e38347f490ae14bf96913a19e7dab9b5e752c276.
    
    Reverted https://github.com/pytorch/pytorch/pull/112093 on behalf of https://github.com/izaitsevfb due to Sorry, trying to resolve a conflict with intern, and unblock the revert of #108690 ([comment](https://github.com/pytorch/pytorch/pull/112093#issuecomment-1782154814))

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -2275,7 +2275,7 @@ class CppVecKernelChecker(CppVecKernel):
                     return tmp_var
 
             @staticmethod
-            def indirect_indexing(index_var, size, add_asserts=True):
+            def indirect_indexing(index_var, size, check=True):
                 return sympy_symbol(str(index_var))
 
             @staticmethod
