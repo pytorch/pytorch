8d44999183e564e849519215f423a8ea5c4918ea
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-10-28 01:51:32 +0000

    Revert "[Inductor] Add triton.autotune support for user defined triton kernels with constant/simple grids (#112228)"
    This reverts commit dbb31a2984fa616b4bb6fac7abb2a06ec0533eb1.
    
    Reverted https://github.com/pytorch/pytorch/pull/112228 on behalf of https://github.com/huydhn due to Sorry for reverting your change but it is failing ROCm test in trunk https://hud.pytorch.org/pytorch/pytorch/commit/dbb31a2984fa616b4bb6fac7abb2a06ec0533eb1 ([comment](https://github.com/pytorch/pytorch/pull/112228#issuecomment-1783660326))

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -362,14 +362,10 @@ class VariableBuilder:
         from torch.utils._triton import has_triton
 
         if has_triton():
-            from triton.runtime.autotuner import Autotuner
             from triton.runtime.jit import JITFunction
         else:
 
             class JITFunction:
-                pass
-
-            class Autotuner:
                 pass
 
         make_guards = self.make_guards
@@ -720,7 +716,7 @@ class VariableBuilder:
                 sym_node_proxy,
                 new_symint == 1,
             )
-        elif isinstance(value, (JITFunction, Autotuner)):
+        elif isinstance(value, JITFunction):
             return TritonKernelVariable(
                 value,
                 None,  # No kernel idx provided
