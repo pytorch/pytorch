1e8f96950778e4a6f9b0254571e0afda2f762d99
Peter Bell <peterbell10@live.co.uk>
2023-10-28 22:12:12 +0100

    Update on "Use `pytree.tree_leaves` everywhere"
    This changes all the instances I could find of `tree_flatten(...)[0]` or
    `x, _ = tree_flatten` to use `tree_leaves`.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler avikchaudhuri gmagogsfm zhxchen17 tugsbayasgalan angelayi
    
    [ghstack-poisoned]

--- torch/_functorch/pytree_hacks.py
+++ torch/_functorch/pytree_hacks.py
@@ -4,11 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torch.utils._pytree import tree_unflatten
+from torch.utils import _pytree as pytree
 
 
-def tree_map_(fn_, pytree):
-    flat_args = pytree.tree_leaves(pytree)
+def tree_map_(fn_, tree):
+    flat_args = pytree.tree_leaves(tree)
     [fn_(arg) for arg in flat_args]
     return pytree
 
@@ -20,5 +20,5 @@ class PlaceHolder:
 
 def treespec_pprint(spec):
     leafs = [PlaceHolder() for _ in range(spec.num_leaves)]
-    result = tree_unflatten(leafs, spec)
+    result = pytree.tree_unflatten(leafs, spec)
     return repr(result)
