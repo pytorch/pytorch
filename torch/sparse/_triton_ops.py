efc0a1f28de1ddea22fea74b8d0bf810e6c2a717
Pearu Peterson <pearu.peterson@gmail.com>
2023-10-29 09:43:21 +0000

    Fix scatter_mm kernel failure on non-contiguous tensor arguments (#112337)
    This PR fixes
    ```
    RuntimeError: Triton Error [CUDA]: an illegal memory access was encountered
    ```
    that appears when using large non-contiguous tensor arguments in `scatter_mm` kernel launch.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112337
    Approved by: https://github.com/cpuhrsch
    ghstack dependencies: #112154, #112076

--- torch/sparse/_triton_ops.py
+++ torch/sparse/_triton_ops.py
@@ -1781,11 +1781,33 @@ if has_triton():
         assert p_offsets.stride(0) == 1
         assert q_offsets.stride(0) == 1
 
+        # Re non-contiguous tensor arguments. Sometimes triton kernel
+        # launches may fail with
+        #
+        #   RuntimeError: Triton Error [CUDA]: an illegal memory access was encountered
+        #
+        # that appears to be case when the size of a non-contiguous
+        # tensor argument is larger than a certain threshold. Could
+        # this be related to shared memory or L1 cache size of a GPU
+        # card? In anycase, ensuring that tensor arguments are
+        # contiguous seems to avoid the above exception. So, in the
+        # following we'll always convert tensor arguments to
+        # C-contiguous tensors.
+
+        blocks = blocks.contiguous()
+
+        others = others.contiguous()
+
+        if not accumulators.is_contiguous():
+            accumulators_ = accumulators.contiguous()
+        else:
+            accumulators_ = accumulators
+
         _scatter_mm6_kernel[grid](
             B, Ms, Ks, N,
             blocks, blocks.stride(0), blocks.stride(1), blocks.stride(2),
             others, others.stride(0), others.stride(1), others.stride(2),
-            accumulators, accumulators.stride(0), accumulators.stride(1), accumulators.stride(2),
+            accumulators_, accumulators_.stride(0), accumulators_.stride(1), accumulators_.stride(2),
             c_indices,
             r_offsets,
             p_offsets,
@@ -1793,6 +1815,9 @@ if has_triton():
             dot_out_dtype=dot_out_dtype,
             **meta
         )
+
+        if not accumulators.is_contiguous():
+            accumulators.copy_(accumulators_)
 
 else:
     bsr_softmax = None  # type: ignore[assignment]
