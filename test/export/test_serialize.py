e5cac10d2b205b8d3864d019652d283a1fd4d1c9
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2023-10-30 13:32:56 -0700

    Don't rewrite assert in pytest
    [ghstack-poisoned]

--- test/export/test_serialize.py
+++ test/export/test_serialize.py
@@ -1,3 +1,7 @@
+"""
+PYTEST_DONT_REWRITE (prevents pytest from rewriting assertions, which interferes
+with test_rewrite_assert_with_msg and test_rewrite_assert_without_msg)
+"""
 # Owner(s): ["module: dynamo"]
 import io
 import pathlib
