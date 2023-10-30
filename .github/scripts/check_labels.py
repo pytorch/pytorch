d1eba98e26a99da8a247141e40c362d966c5a9f2
Svetlana Karslioglu <svekars@meta.com>
2023-10-30 13:19:02 -0700

    Update check_labels.py

--- .github/scripts/check_labels.py
+++ .github/scripts/check_labels.py
@@ -5,9 +5,9 @@ import sys
 from typing import Any
 
 from github_utils import gh_delete_comment, gh_post_pr_comment
-
+from github import 
 from gitutils import get_git_remote_name, get_git_repo_dir, GitRepo
-from label_utils import has_required_labels, is_label_err_comment, LABEL_ERR_MSG
+from label_utils import has_required_labels, is_label_err_comment, LABEL_ERR_MSG, gh_add_labels
 from trymerge import GitHubPR
 
 
@@ -31,6 +31,43 @@ def parse_args() -> Any:
 
     return parser.parse_args()
 
+def check_docathon_label(pr: "GitHubPR") -> None:
+    """
+    A check used to propagate labels from the docathon issues to the
+    corresponding PRs.
+    Check if there is an issue mentioned in the body of the PR and if yes,
+    check that that issue has a docathon label. If it does, propagate all the
+    labels from the issue to the PR.
+    """
+    # Get the pull request body
+    pull_request_body = pr.get_body()
+    pull_request_labels = pr.get_labels()
+
+    # PR without description
+    if pull_request_body is None:
+        print("The pull request does not have a description.")
+        return
+
+    # Check for the docathon label
+    if not re.search(r'#\d{1,6}', pull_request_body): # search for six digit issue number
+        print("The pull request does not mention an issue.")
+        return
+    issue_number = int(re.findall(r'#(\d{1,6})', pull_request_body)[0])
+    issue_labels = pr.get_labels(issue_number)
+    docathon_label_present = any(label == 'docathon-h2-2023' for label in issue_labels) # the label updated for each docathon
+
+    # If the issue has a docathon label, add all labels from the issue to the PR.
+    if not docathon_label_present:
+        print("The 'docathon-h2-2023' label is not present in the issue.")
+        return
+    issue_label_names = [label.name for label in issue_labels]
+    labels_to_add = [label for label in issue_label_names if label not in pull_request_labels]
+    
+    if labels_to_add:
+        gh_add_labels(pr.org, pr.repo, pr.pr_num, labels_to_add)
+    else:
+        print("The pull request already has the same labels.")
+        return
 
 def main() -> None:
     args = parse_args()
@@ -44,6 +81,7 @@ def main() -> None:
             add_label_err_comment(pr)
         else:
             delete_all_label_err_comments(pr)
+        check_docathon_label(pr)
     except Exception as e:
         pass
 
