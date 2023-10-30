4388adea0c3812ab26bf27dd1868b539fbefbd01
drisspg <drisspguessous@gmail.com>
2023-10-30 13:03:46 -0700

    fix extra comment

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -2216,7 +2216,7 @@ def iota(
 
 @register_lowering(aten.select_scatter, type_promotion_kind=None)
 def select_scatter(x, src, dim: int, index: int):
-    # assert x.get_dtype() == src.get_dtype()
+    assert x.get_dtype() == src.get_dtype()
     x_loader = x.make_loader()
     dim = _validate_dim(x, dim, 0)
     if V.graph.sizevars.evaluate_expr(sympy.Lt(index, 0)):
