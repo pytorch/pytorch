47ad46242cfcb01c5cf0d91235e0c74b017f3948
Peter Bell <peterbell10@live.co.uk>
2023-10-30 00:05:28 +0000

    [pytree] Add optimized `tree_leaves` implementation (#112323)
    pytree is used in many hot paths for dynamo tracing and in many cases we don't
    care about the tree spec and just want the flattened list. This improves
    `pytree.tree_leaves` to not construct the spec which gives a noticeable
    performance improvement when multiplied by the many times it gets called
    during tracing.
    
    Concretely, I see a 2x speedup compared to `tree_flatten` in this benchmark:
    ```python
    import torch.utils._pytree as pytree
    %timeit pytree.tree_flatten([((100, 100), (100, 1)), dict(device="cuda")])[0]
    %timeit pytree.tree_leaves([((100, 100), (100, 1)), dict(device="cuda")])
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112323
    Approved by: https://github.com/lezcano, https://github.com/XuehaiPan
    ghstack dependencies: #112327

--- torch/utils/_pytree.py
+++ torch/utils/_pytree.py
@@ -321,7 +321,19 @@ def tree_flatten(pytree: PyTree) -> Tuple[List[Any], TreeSpec]:
 
 def tree_leaves(pytree: PyTree) -> List[Any]:
     """Get a list of leaves of a pytree."""
-    return tree_flatten(pytree)[0]
+    if _is_leaf(pytree):
+        return [pytree]
+
+    node_type = _get_node_type(pytree)
+    flatten_fn = SUPPORTED_NODES[node_type].flatten_fn
+    child_pytrees, context = flatten_fn(pytree)
+
+    # Recursively flatten the children
+    result: List[Any] = []
+    for child in child_pytrees:
+        result += tree_leaves(child)
+
+    return result
 
 
 def tree_structure(pytree: PyTree) -> TreeSpec:
@@ -369,7 +381,7 @@ def tree_map(fn: Any, pytree: PyTree) -> PyTree:
 
 
 def tree_map_(fn: Any, pytree: PyTree) -> PyTree:
-    flat_args, _ = tree_flatten(pytree)
+    flat_args = tree_leaves(pytree)
     deque(map(fn, flat_args), maxlen=0)  # consume and exhaust the iterable
     return pytree
 
@@ -475,12 +487,12 @@ def tree_map_only_(ty: TypeAny, fn: FnAny[Any], pytree: PyTree) -> PyTree:
 
 
 def tree_all(pred: Callable[[Any], bool], pytree: PyTree) -> bool:
-    flat_args, _ = tree_flatten(pytree)
+    flat_args = tree_leaves(pytree)
     return all(map(pred, flat_args))
 
 
 def tree_any(pred: Callable[[Any], bool], pytree: PyTree) -> bool:
-    flat_args, _ = tree_flatten(pytree)
+    flat_args = tree_leaves(pytree)
     return any(map(pred, flat_args))
 
 
@@ -500,7 +512,7 @@ def tree_all_only(ty: Type3[T, S, U], pred: Fn3[T, S, U, bool], pytree: PyTree) 
 
 
 def tree_all_only(ty: TypeAny, pred: FnAny[bool], pytree: PyTree) -> bool:
-    flat_args, _ = tree_flatten(pytree)
+    flat_args = tree_leaves(pytree)
     return all(pred(x) for x in flat_args if isinstance(x, ty))
 
 
@@ -515,7 +527,7 @@ def tree_any_only(ty: Type2[T, S], pred: Fn2[T, S, bool], pytree: PyTree) -> boo
 
 
 def tree_any_only(ty: TypeAny, pred: FnAny[bool], pytree: PyTree) -> bool:
-    flat_args, _ = tree_flatten(pytree)
+    flat_args = tree_leaves(pytree)
     return any(pred(x) for x in flat_args if isinstance(x, ty))
 
 
