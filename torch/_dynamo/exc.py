60539a1e697e72f09cfb74c773d88f5c53adfeee
Scott Wolchok <swolchok@fb.com>
2023-10-30 15:57:54 -0700

    Update base for make MiniArrayRef work more like std::span on "[PyTorch] AOTI: add ArrayRefTensor"
    This adds a shim for AOTI generated code to pretend a raw array works like an AtenTensorHandle. This allows parts of AOTI that generate uses of tensors to continue to be unaware of how those tensors are allocated. See the following diff/PR for usage.
    
    Differential Revision: [D50570252](https://our.internmc.facebook.com/intern/diff/D50570252/)
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_dynamo/exc.py
+++ torch/_dynamo/exc.py
@@ -2,7 +2,7 @@ import os
 import textwrap
 from enum import auto, Enum
 from traceback import extract_stack, format_exc, format_list, StackSummary
-from typing import cast, NoReturn, Optional
+from typing import cast, Optional
 
 import torch._guards
 
@@ -165,7 +165,7 @@ exceptions_allowed_to_be_fallback = (
 )
 
 
-def unimplemented_with_warning(e: Exception, code, msg: str) -> NoReturn:
+def unimplemented_with_warning(e, code, msg):
     # This function calls unimplemented internally and eventually graph breaks
     # or falls to eager. unimplemented itself does not print any user warnings,
     # i.e., its very silent. This helper function is intended when an error is
@@ -179,12 +179,12 @@ def unimplemented_with_warning(e: Exception, code, msg: str) -> NoReturn:
     raise unimplemented(msg) from e
 
 
-def unimplemented(msg: str) -> NoReturn:
+def unimplemented(msg: str):
     assert msg != os.environ.get("BREAK", False)
     raise Unsupported(msg)
 
 
-def warning(msg: str) -> None:
+def warning(msg: str):
     counters["warnings"][msg] += 1
     assert msg != os.environ.get("BREAK", False)
 
@@ -202,12 +202,14 @@ class KeyErrorMsg:
         return self.__str__()
 
 
-def augment_exc_message(exc: Exception, msg: str = "\n", export: bool = False) -> None:
+def augment_exc_message(exc, msg="\n", export=False):
     import traceback
 
     real_stack = get_real_stack(exc)
     if real_stack is not None:
-        msg += f"\nfrom user code:\n {''.join(traceback.format_list(real_stack))}"
+        msg += (
+            f"\nfrom user code:\n {''.join(traceback.format_list(get_real_stack(exc)))}"
+        )
 
     if config.replay_record_enabled and hasattr(exc, "record_filename"):
         msg += f"\nLast frame execution written to {exc.record_filename}. To run only this frame while debugging, run\
@@ -248,7 +250,7 @@ def augment_exc_message(exc: Exception, msg: str = "\n", export: bool = False) -
         exc.args = (new_msg,) + exc.args[1:]
 
 
-def get_real_stack(exc: Exception, frame=None) -> Optional[StackSummary]:
+def get_real_stack(exc, frame=None) -> Optional[StackSummary]:
     real_stack = getattr(exc, "real_stack", None)
     if real_stack is None:
         return None
@@ -290,9 +292,7 @@ def filter_stack(stack):
     return user_stack
 
 
-def format_error_msg_verbose(
-    exc: Exception, code, record_filename=None, frame=None
-) -> str:
+def format_error_msg_verbose(exc, code, record_filename=None, frame=None):
     msg = (
         f"WON'T CONVERT {code.co_name} {code.co_filename} line {code.co_firstlineno}\n"
     )
@@ -314,7 +314,7 @@ def format_error_msg_verbose(
     return msg
 
 
-def format_error_msg(exc: Exception, code, record_filename=None, frame=None) -> str:
+def format_error_msg(exc, code, record_filename=None, frame=None):
     msg = os.linesep * 2
 
     if config.verbose:
