82776e4b8a03dac6ed830d8ce062f322c5396ba4
Elias Ellison <elias.ellison@gmail.com>
2023-11-01 19:38:05 +0000

    [old][wip] benchmark
    [ghstack-poisoned]

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1275,7 +1275,7 @@ class TritonCodeCache:
 def _worker_compile(kernel_name, source_code, cc, device):
     cuda_properties.set_compiler_worker_current_device(device)
     kernel = TritonCodeCache.load(kernel_name, source_code)
-    kernel.precompile(warm_cache_only_with_cc=cc)
+    kernel.precompile()
 
 
 def _load_kernel(kernel_name, source_code):
@@ -1315,75 +1315,21 @@ class AsyncCompile:
     @staticmethod
     @functools.lru_cache(1)
     def pool():
-        assert config.compile_threads > 1
-        return ThreadPoolExecutor(config.compile_threads)
-
-    @staticmethod
-    @functools.lru_cache(1)
-    def process_pool():
-        # ensure properties have been calculated before processes
-        # are forked
         cuda_properties._properties()
+        import triton
+
+        triton.set_thread_pool()
+
         assert config.compile_threads > 1
-        orig_ppid = os.getpid()
-
-        # if this process dies abnormally (e.g. segfault)
-        # it will not shut down the workers. Instead
-        # the workers will have their parent reassigned to the
-        # init process. This launches a separate thread to
-        # watch for the worker getting reassigned,
-        # and cleans it up in this case.
-        def init():
-            def run():
-                while True:
-                    sleep(1)
-                    if orig_ppid != os.getppid():
-                        os.kill(os.getpid(), signal.SIGKILL)
-
-            global _watchdog_thread
-            _watchdog_thread = Thread(target=run, daemon=True)
-            _watchdog_thread.start()
-
-        # we rely on 'fork' because we cannot control whether users
-        # have an `if __name__ == '__main__'` in their main process.
-        fork_context = multiprocessing.get_context("fork")
-        pool = ProcessPoolExecutor(
-            config.compile_threads, mp_context=fork_context, initializer=init
-        )
-        # when this pool is created in a subprocess object, the normal exit handler
-        # doesn't run, and we need to register our own handler.
-        # exitpriority has to be high, because another one of the finalizers will
-        # kill the worker thread that sends the shutdown message to the workers...
-        multiprocessing.util.Finalize(None, pool.shutdown, exitpriority=sys.maxsize)
-        return pool
+        return ThreadPoolExecutor()
 
     @classmethod
     def warm_pool(cls):
         if config.compile_threads <= 1:
             return
         _compile_start()
-        pool = cls.process_pool()
+        pool = cls.pool()
 
-        # We have to fork processes for compiler workers, but the more memory and other resources that are loaded, the
-        # slower the os.fork time is, quite drastically. It also holds the GIL so we can't put it on another thread.
-
-        # Examples:
-        # A simple x + x + x script: 10ms seconds in the middle of the program, 2ms at startup
-        # tf_efficientnet_b0 benchmark: 50ms! in the middle of the program , 3ms at startup
-
-        # So we want to start the workers early when it is still cheap, and also to allow the workers to get
-        # ready before we have work for them.
-
-        # ProcessPoolExecutor also does not launch the workers until it finds a point when all the workers are idle.
-        # But if we waited until then fork time will be long and we will be waiting for the processes to initialize.
-
-        # We force them to start here with some YOLOing of the internal methods.
-        if hasattr(pool, "_start_queue_management_thread"):
-            pool._start_queue_management_thread()
-        else:
-            for _ in range(config.compile_threads):
-                pool._adjust_process_count()
-            pool._start_executor_manager_thread()
         _compile_end()
 
     @classmethod
@@ -1405,7 +1351,7 @@ class AsyncCompile:
             major, minor = torch.cuda.get_device_capability()
             device = torch.cuda.current_device()
             cc = major * 10 + minor
-            future = self.process_pool().submit(
+            future = self.pool().submit(
                 _worker_compile, kernel_name, source_code, cc, device
             )
             return TritonFuture(kernel_name, source_code, future)
@@ -1436,6 +1382,8 @@ class AsyncCompile:
             for key, result in scope.items():
                 if config.verbose_progress and not isinstance(pbar, _Faketqdm):
                     pbar.set_postfix_str(key)
+                # if key == "triton_poi_fused_unsqueeze_0":
+                #     breakpoint()
                 if isinstance(result, (Future, TritonFuture)):
                     scope[key] = result.result()
                     pbar.update(1)
