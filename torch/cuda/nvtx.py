32ccaede028bc0bc1457065a9c422c968d0bd7c9
Kazuaki Ishizaki <ishizaki@gmail.com>
2023-11-02 16:07:21 +0000

    fix docstring errors

--- torch/cuda/nvtx.py
+++ torch/cuda/nvtx.py
@@ -1,3 +1,5 @@
+r"""This package adds support for NVIDIA Tools Extension (NVTX) used in profiling."""
+
 from contextlib import contextmanager
 
 try:
@@ -22,8 +24,7 @@ __all__ = ["range_push", "range_pop", "range_start", "range_end", "mark", "range
 
 def range_push(msg):
     """
-    Pushes a range onto a stack of nested range span.  Returns zero-based
-    depth of the range that is started.
+    Push a range onto a stack of nested range span.  Returns zero-based depth of the range that is started.
 
     Args:
         msg (str): ASCII message to associate with range
@@ -32,17 +33,13 @@ def range_push(msg):
 
 
 def range_pop():
-    """
-    Pops a range off of a stack of nested range spans.  Returns the
-    zero-based depth of the range that is ended.
-    """
+    """Pop a range off of a stack of nested range spans.  Returns the  zero-based depth of the range that is ended."""
     return _nvtx.rangePop()
 
 
 def range_start(msg) -> int:
     """
-    Mark the start of a range with string message. It returns an unique handle
-    for this range to pass to the corresponding call to rangeEnd().
+    Mark the start of a range with string message. It returns an unique handle for this range to pass to the corresponding call to rangeEnd().
 
     A key difference between this and range_push/range_pop is that the
     range_start/range_end version supports range across threads (start on one
@@ -79,9 +76,7 @@ def mark(msg):
 @contextmanager
 def range(msg, *args, **kwargs):
     """
-    Context manager / decorator that pushes an NVTX range at the beginning
-    of its scope, and pops it at the end. If extra arguments are given,
-    they are passed as arguments to msg.format().
+    Context manager / decorator that pushes an NVTX range at the beginning of its scope, and pops it at the end. If extra arguments are given, they are passed as arguments to msg.format().
 
     Args:
         msg (str): message to associate with the range
