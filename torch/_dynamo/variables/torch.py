fd56aa0daeec624bcff822fef04fe9339770dc33
pytorchbot <pytorchbot@pytorch.com>
2023-11-03 07:34:11 +0000

    2023-11-03 nightly release (4a17693d193a8b23109ac3129e5cde92babfe191)

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -545,10 +545,8 @@ class TorchVariable(VariableTracker):
                 ),
                 **options,
             )
-        # TODO: These special cases shouldn't be necessary; we should
-        # generically support torch.ops that return int
         elif (
-            self.value in [torch.ops.aten.sym_size, torch.ops.aten.sym_size.int]
+            self.value is torch.ops.aten.sym_size
             and len(args) == 2
             and len(kwargs) == 0
             and isinstance(args[0], TensorVariable)
@@ -556,7 +554,7 @@ class TorchVariable(VariableTracker):
             # we see this when retracing already traced code
             return args[0].call_method(tx, "size", [args[1]], {})
         elif (
-            self.value is [torch.ops.aten.sym_stride, torch.ops.aten.sym_stride.int]
+            self.value is torch.ops.aten.sym_stride
             and len(args) == 2
             and len(kwargs) == 0
             and isinstance(args[0], TensorVariable)
