627913fcc2a11fbd572218291bca540a367fa637
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-11-05 00:29:50 +0530

    Update adamw.py - reverting changes

--- torch/optim/adamw.py
+++ torch/optim/adamw.py
@@ -150,7 +150,7 @@ class AdamW(Optimizer):
 
     @_use_grad_for_differentiable
     def step(self, closure=None):
-        """Perform a single optimization step.
+        """Performs a single optimization step.
 
         Args:
             closure (Callable, optional): A closure that reevaluates the model
@@ -305,6 +305,7 @@ def adamw(
 
     See :class:`~torch.optim.AdamW` for details.
     """
+
     if not torch._utils.is_compiling() and not all(isinstance(t, torch.Tensor) for t in state_steps):
         raise RuntimeError(
             "API has changed, `state_steps` argument must contain a list of singleton tensors"
