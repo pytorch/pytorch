3be02f364c98406fddbd3149df10eb8b09b61c8e
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-11-04 20:32:56 +0530

    Update sparse_adam.py

--- torch/optim/sparse_adam.py
+++ torch/optim/sparse_adam.py
@@ -37,7 +37,7 @@ class SparseAdam(Optimizer):
 
     @torch.no_grad()
     def step(self, closure=None):
-        """Performs a single optimization step.
+        """Perform a single optimization step.
 
         Args:
             closure (Callable, optional): A closure that reevaluates the model
