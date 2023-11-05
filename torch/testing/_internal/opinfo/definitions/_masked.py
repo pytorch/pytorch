4da85348da7bfe0844113915ebcf2de5e2c9178c
CaoE <e.cao@intel.com>
2023-11-05 12:31:38 +0000

    Add Half support for cummax, cummin, cumprod, logcumsumexp, and prod on CPU (#112132)
    Add Half support for cummax, cummin, cumprod, logcumsumexp, and prod on CPU.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112132
    Approved by: https://github.com/cpuhrsch

--- torch/testing/_internal/opinfo/definitions/_masked.py
+++ torch/testing/_internal/opinfo/definitions/_masked.py
@@ -504,11 +504,7 @@ op_db: List[OpInfo] = [
         supports_sparse=True,
         supports_sparse_csr=True,
         promotes_int_to_int64=True,
-        # FIXME: "prod_cpu" not implemented for 'Half'
-        dtypes=all_types_and_complex_and(torch.bool, torch.bfloat16),
-        dtypesIfCUDA=all_types_and_complex_and(
-            torch.bool, torch.float16, torch.bfloat16
-        ),
+        dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16),
         skips=(
             DecorateInfo(
                 unittest.expectedFailure,
@@ -554,6 +550,12 @@ op_db: List[OpInfo] = [
                 "TestReductions",
                 "test_ref_small_input",
             ),
+            DecorateInfo(
+                toleranceOverride({torch.float16: tol(atol=1e-02, rtol=1.5e-03)}),
+                "TestMasked",
+                "test_mask_layout",
+                device_type="cpu",
+            ),
         ],
         sample_inputs_func=sample_inputs_masked_reduction,
         sample_inputs_sparse_coo_func=sample_inputs_sparse_coo_masked_reduction,
@@ -585,8 +587,7 @@ op_db: List[OpInfo] = [
     ),
     OpInfo(
         "masked.cumprod",
-        dtypes=all_types_and_complex_and(torch.bfloat16),
-        dtypesIfCUDA=all_types_and_complex_and(torch.float16, torch.bfloat16),
+        dtypes=all_types_and_complex_and(torch.float16, torch.bfloat16),
         method_variant=None,
         # Runs very slowly on slow gradcheck - alternatively reduce input sizes
         gradcheck_fast_mode=True,
