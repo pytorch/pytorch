e0f4de369559df0c01b3d540cab7a076d7bdc193
Yanbo Liang <ybliang8@gmail.com>
2023-11-05 19:55:30 -0800

    Update

--- test/functorch/test_eager_transforms.py
+++ test/functorch/test_eager_transforms.py
@@ -1043,6 +1043,7 @@ class TestAutogradFunction(TestCase):
         grad(f)(y, x)
         grad(grad(f))(y, x)
 
+    @skipIfTorchDynamo("Debugging")
     @parametrize("inner_requires_grad", [True, False])
     @parametrize("save_for", ["jvp", "vjp"])
     @parametrize("save_tensors", ["input", "output", "neither"])
@@ -3090,6 +3091,7 @@ class TestComposability(TestCase):
             warnings.simplefilter("error")
             new_api(torch.sin)
 
+    @skipIfTorchDynamo("Debugging")
     def test_grad_grad(self, device):
         x = torch.randn([], device=device)
         y = grad(grad(torch.sin))(x)
