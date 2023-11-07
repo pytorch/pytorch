e571bcf4f41408ee7aec497ec17978b93c3d9f1f
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-11-08 00:10:49 +0530

    Update autocast_mode.py - removing trailing spaces

--- torch/cuda/amp/autocast_mode.py
+++ torch/cuda/amp/autocast_mode.py
@@ -87,8 +87,9 @@ def _cast(value, dtype):
 #     @custom_fwd(cast_inputs=torch.float)
 #     def forward(...):
 def custom_fwd(fwd=None, *, cast_inputs=None):
-    """Create a helper decorator for ``forward`` methods of custom autograd functions.
-    
+    """
+    Create a helper decorator for ``forward`` methods of custom autograd functions.
+
     Autograd functions are subclasses of :class:`torch.autograd.Function`.
     See the :ref:`example page<amp-custom-examples>` for more detail.
 
@@ -129,7 +130,7 @@ def custom_fwd(fwd=None, *, cast_inputs=None):
 # cast_inputs supplied to custom_fwd.
 def custom_bwd(bwd):
     """Create a helper decorator for backward methods of custom autograd functions.
-    
+
     Autograd functions are subclasses of :class:`torch.autograd.Function`.
     Ensures that ``backward`` executes with the same autocast state as ``forward``.
     See the :ref:`example page<amp-custom-examples>` for more detail.
