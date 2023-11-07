8200ada46ccf6cbcf386eee3583e3164d2f130e6
Richard Zou <rzou@fb.com>
2023-11-07 16:07:42 +0000

    Update impl_abstract_pystub to be less boilerplatey (#112851)
    Summary:
    We've made the following changes:
    - The new way to use the API is `m.impl_abstract_pystub(module, context)`.
      Every subsequent m.def of an op inside the TORCH_LIBRARY block gives
      the op the `impl_abstract_pystub`.
    - Added a mechanism to determine if an operator was defined in Python or C++.
      Library.define in Python appends the op to a global set, which is analogous
      to what we do for tracking Library.impl.
    - If someone does `torch.library.impl_abstract` in Python for an operator, then
      we require that it has an `impl_abstract_pystub` specified and we also check
      that the module in the `impl_abstract_pystub` is the same as the module where
      the call to `torch.library.impl_abstract` exists.
    - Unfortunately we can't check the "context" (which is the buck target on
      buck-based systems) because buck sits above us.
    
    Test Plan: - existing tests
    
    Differential Revision: D50972148
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112851
    Approved by: https://github.com/ezyang

--- torch/library.h
+++ torch/library.h
@@ -93,6 +93,8 @@ enum class _RegisterOrVerify { REGISTER, VERIFY };
 template <class CurClass>
 class class_;
 
+#define HAS_IMPL_ABSTRACT_PYSTUB
+
 /// Represents a C++ function that implements an operator.  Most users won't
 /// interact directly with this class, except via error messages: the
 /// constructors this function define the set of permissible "function"-like
@@ -606,19 +608,16 @@ class TORCH_API Library final {
     return _def(std::move(s), nullptr, tags, rv);
   }
 
-  /// Declares that an operator (given by name) has an abstract impl in a
-  /// Python module (pymodule). If the abstract impl was not yet imported,
-  /// we will warn about it.
+  /// Declares that for all operators that are subsequently def'ed, their
+  /// abstract impls may be found in the given Python module (pymodule).
+  /// This registers some help text that is used if the abstract impl
+  /// cannot be found.
   ///
   /// Args:
-  /// - name: the name of the operator
   /// - pymodule: the python module
   /// - context: We may include this in the error message.
-  Library& impl_abstract_pystub(const char* name, const char* pymodule, const char* context = "") {
-    at::OperatorName opname = _parseNameForLib(name);
-    registrars_.emplace_back(
-      c10::Dispatcher::singleton().registerAbstractImplPyStub(opname, pymodule, context)
-      );
+  Library& impl_abstract_pystub(const char* pymodule, const char* context = "") {
+    impl_abstract_pystub_ = {pymodule, context};
     return *this;
   }
 
@@ -840,6 +839,7 @@ class TORCH_API Library final {
   Kind kind_;
   c10::optional<std::string> ns_;
   c10::optional<c10::DispatchKey> dispatch_key_;
+  c10::optional<std::pair<const char*, const char*>> impl_abstract_pystub_;
   const char* file_;
   uint32_t line_;
 
