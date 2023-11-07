051e856f2a7aa011825ee8eb024b21335248dd79
Will Feng <yf225@cornell.edu>
2023-11-07 11:52:38 -0800

    lint

--- torch/_inductor/comms.py
+++ torch/_inductor/comms.py
@@ -292,16 +292,18 @@ def node_summary(snode):
     if isinstance(snode.node, ir.ExternKernelOut):
         detail = f" ({snode.node.kernel})"
     out_tensor_info = ""
-    if hasattr(snode.node, "layout") and hasattr(snode.node.layout, "size") and hasattr(snode.node.layout, "stride"):
+    if (
+        hasattr(snode.node, "layout")
+        and hasattr(snode.node.layout, "size")
+        and hasattr(snode.node.layout, "stride")
+    ):
         out_tensor_info = (
             f" (size={snode.node.layout.size}, stride={snode.node.layout.stride})"
         )
     node_name = ""
     if hasattr(snode.node, "name"):
         node_name = snode.node.name
-    return (
-        f"{snode.node.__class__.__name__}{detail}{out_tensor_info} ({node_name})"
-    )
+    return f"{snode.node.__class__.__name__}{detail}{out_tensor_info} ({node_name})"
 
 
 def visualize_overlap(order):
