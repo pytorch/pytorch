007a9f7017176fc1b624eaa052da586290256cd9
Hongtao Yu <hoy@fb.com>
2023-11-07 00:12:12 +0000

    [inductor] scale up num_warps for reductions to lower register pressure (#113039)
    Recent work (https://github.com/pytorch/pytorch/pull/108193 and https://github.com/pytorch/pytorch/pull/109275) unveiled that bigger Triton kernel can regress performance due to increased register pressure which in turn lowers thread occupancy. By taking a look at the Triton internal, I see an opportunity to reduce the register pressure by decreasing the amount of work each thread does. I'm bumping up the `num_warps` to achieve this. The change should only affect reduction cases.
    
    I'm seeing real compilation time reduction with this change which is likely due to smaller LLVM IR:
    https://hud.pytorch.org/benchmark/compilers?startTime=Mon%2C%2023%20Oct%202023%2017%3A57%3A40%20GMT&stopTime=Mon%2C%2006%20Nov%202023%2018%3A57%3A40%20GMT&granularity=hour&suite=torchbench&mode=training&dtype=amp&lBranch=hoy-reduction&lCommit=f2d31b83aa170914018407d88a76d5951153b316&rBranch=main&rCommit=64f326097be8ac66ff057365f3bed2d64c697563
    
    The slightly performance improvement can be noise, if not, the lower register pressure could explain.
    
    Ideally, we should improve Triton to automatically reroll large kernel to an inner loop, without hurting vectorization. That's something I'm considering on the LLVM side.
    
    I'm also seeing the fused kernel provided in https://github.com/pytorch/pytorch/pull/108193 gets a better performance by benefiting from a lower register pressure. PTXAS shows a usage of 32 registers compared to 55 previously.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113039
    Approved by: https://github.com/shunting314

--- torch/_inductor/triton_heuristics.py
+++ torch/_inductor/triton_heuristics.py
@@ -903,7 +903,7 @@ def triton_config_reduction(size_hints, x, r, num_stages=1, num_warps=None) -> C
     cfg = {"XBLOCK": x, "RBLOCK": r}
     if num_warps is None:
         num_warps = conditional_product(x, r) // 128
-    num_warps = next_power_of_2(min(max(num_warps, 2), 8))
+    num_warps = next_power_of_2(min(max(num_warps, 2), 16))
     check_config(cfg, xnumel=size_hints[0])
     return Config(cfg, num_warps=num_warps, num_stages=num_stages)
 
