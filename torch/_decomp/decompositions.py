b10797cb5d262b75e24061e497b4fb4dfc2135c9
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-11-07 15:53:32 +0000

    Revert "Add support for `torch.Generator` type in TorchScript (#110413)"
    This reverts commit 27e31ab6e86259b27d816d6fb6e7a69de526a0e4.
    
    Reverted https://github.com/pytorch/pytorch/pull/110413 on behalf of https://github.com/PaliC due to breaking internal builds ([comment](https://github.com/pytorch/pytorch/pull/110413#issuecomment-1799003164))

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -2331,7 +2331,6 @@ def uniform(
     x: Tensor,
     low: Union[bool, int, float] = 0.0,
     high: Union[bool, int, float] = 1.0,
-    generator: Optional[torch.Generator] = None,
 ):
     return prims._uniform_helper(
         x.shape,
@@ -2339,13 +2338,13 @@ def uniform(
         high=sym_float(high),
         dtype=x.dtype,
         device=x.device,
-        generator=generator,
     )
 
 
 @register_decomposition(aten.uniform_)
 def uniform_(self, low=0, high=1, generator=None):
-    return self.copy_(uniform(self, low, high, generator))
+    assert generator is None
+    return self.copy_(uniform(self, low, high))
 
 
 # aten/src/ATen/native/UpSample.cpp compute_output_size
