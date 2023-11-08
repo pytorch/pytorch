9833414097c86bb10695ed422ff932888e2d039e
zabboud <zeinab.abboud@polymtl.ca>
2023-11-07 22:38:11 -0500

    fix pydocstyle errors in /utils/_device.py

--- torch/utils/_device.py
+++ torch/utils/_device.py
@@ -82,9 +82,9 @@ def device_decorator(device, func):
 
 def set_device(device):
     """
-    Decorator which sets the default device inside of the wrapped
-    function.  If you would like to use this as a context manager,
-    use device as a context manager directly, e.g.,
-    ``with torch.device(device)``.
+    Set the default device inside of the wrapped function by decorating it with this function.
+    
+    If you would like to use this as a context manager, use device as a 
+    context manager directly, e.g., ``with torch.device(device)``.
     """
     return lambda func: device_decorator(torch.device(device), func)
