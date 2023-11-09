0b91fd4c16906b0379862b8ad74c55c71d03b051
Liao, Xuan <xuan.liao@intel.com>
2023-11-09 16:30:39 +0000

    [cpu] Modify inductor opt flag

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1182,7 +1182,7 @@ def cpp_wrapper_flags() -> str:
 
 def optimization_flags() -> str:
     base_flags = "-O0 -g" if config.aot_inductor.debug_compile else "-O3 -DNDEBUG"
-    base_flags += " -ffast-math -fno-finite-math-only"
+    base_flags += " -ffast-math -fno-unsafe-math-optimizations -fno-finite-math-only"
 
     if config.is_fbcode():
         # FIXME: passing `-fopenmp` adds libgomp.so to the generated shared library's dependencies.
