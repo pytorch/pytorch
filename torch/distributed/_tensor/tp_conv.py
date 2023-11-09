83412f10777508fe3dff58e597a5bd7583c3ebf4
KingsleyLiu-NV <kingsleyl@nvidia.com>
2023-11-09 00:47:36 -0800

    merge common communications as functions for convolution fwd/bwd

--- torch/distributed/_tensor/tp_conv.py
+++ torch/distributed/_tensor/tp_conv.py
@@ -34,6 +34,72 @@ def _is_supported(input_size, kernel_size, stride, padding, dilation):
     return True
 
 
+def _ring_send_recv_construct(in_tensor, d1, d2, left, right, rank, size):
+    # dist comms and reconstruct local input tensor
+    send_to_right = in_tensor[:, :, :, -d1:].contiguous()
+    send_to_left = in_tensor[:, :, :, :d2].contiguous()
+    recv_from_right = torch.zeros_like(send_to_left)
+    recv_from_left = torch.zeros_like(send_to_right)
+
+    send_op_right = dist.P2POp(dist.isend, send_to_right, right)
+    send_op_left = dist.P2POp(dist.isend, send_to_left, left)
+    recv_op_right = dist.P2POp(dist.irecv, recv_from_right, right)
+    recv_op_left = dist.P2POp(dist.irecv, recv_from_left, left)
+
+    reqs = dist.batch_isend_irecv(
+        [send_op_right, send_op_left, recv_op_left, recv_op_right]
+    )
+    for req in reqs:
+        req.wait()
+
+    if rank == 0:
+        in_tensor = torch.cat([in_tensor, recv_from_right], dim=-1)
+    elif rank == size - 1:
+        in_tensor = torch.cat([recv_from_left, in_tensor], dim=-1)
+    else:
+        in_tensor = torch.cat([recv_from_left, in_tensor, recv_from_right], dim=-1)
+
+    return in_tensor
+
+
+def _ring_send_recv_aggregate(grad_in_tensor, d1, d2, left, right, rank, size):
+    # dist comms and aggregate gradients for edge pixels
+    send_to_right = grad_in_tensor[:, :, :, -d2:].contiguous()
+    send_to_left = grad_in_tensor[:, :, :, :d1].contiguous()
+    recv_from_right = torch.zeros_like(send_to_left)
+    recv_from_left = torch.zeros_like(send_to_right)
+
+    send_op_right = dist.P2POp(dist.isend, send_to_right, right)
+    send_op_left = dist.P2POp(dist.isend, send_to_left, left)
+    recv_op_right = dist.P2POp(dist.irecv, recv_from_right, right)
+    recv_op_left = dist.P2POp(dist.irecv, recv_from_left, left)
+
+    reqs = dist.batch_isend_irecv(
+        [send_op_right, send_op_left, recv_op_left, recv_op_right]
+    )
+    for req in reqs:
+        req.wait()
+
+    if rank == 0:
+        grad_in_tensor = grad_in_tensor[:, :, :, :-d2]
+        grad_in_tensor[:, :, :, -d1:] = torch.add(
+            grad_in_tensor[:, :, :, -d1:], recv_from_right
+        )
+    elif rank == size - 1:
+        grad_in_tensor = grad_in_tensor[:, :, :, d1:]
+        grad_in_tensor[:, :, :, :d2] = torch.add(
+            grad_in_tensor[:, :, :, :d2], recv_from_left
+        )
+    else:
+        grad_in_tensor = grad_in_tensor[:, :, :, d1:-d2]
+        grad_in_tensor[:, :, :, -d1:] = torch.add(
+            grad_in_tensor[:, :, :, -d1:], recv_from_right
+        )
+        grad_in_tensor[:, :, :, :d2] = torch.add(
+            grad_in_tensor[:, :, :, :d2], recv_from_left
+        )
+
+
 def tp_convolution(
     op_call: torch._ops.OpOverload,
     *local_tensor_args: Tuple[object, ...],
@@ -63,38 +129,18 @@ def tp_convolution(
         right = (rank + 1) % size
         left = (rank - 1 + size) % size
 
-        # step1 dist comms
-        send_to_right = in_tensor[:, :, :, -d1:].contiguous()
-        send_to_left = in_tensor[:, :, :, :d2].contiguous()
-        recv_from_right = torch.zeros_like(send_to_left)
-        recv_from_left = torch.zeros_like(send_to_right)
-
-        send_op_right = dist.P2POp(dist.isend, send_to_right, right)
-        send_op_left = dist.P2POp(dist.isend, send_to_left, left)
-        recv_op_right = dist.P2POp(dist.irecv, recv_from_right, right)
-        recv_op_left = dist.P2POp(dist.irecv, recv_from_left, left)
-
-        reqs = dist.batch_isend_irecv(
-            [send_op_right, send_op_left, recv_op_left, recv_op_right]
+        # step1 reconstruct local input tensor
+        in_tensor = _ring_send_recv_construct(
+            in_tensor, d1, d2, left, right, rank, size
         )
-        for req in reqs:
-            req.wait()
 
-        # step2 reconstruct local input tensor
-        if rank == 0:
-            in_tensor = torch.cat([in_tensor, recv_from_right], dim=-1)
-        elif rank == size - 1:
-            in_tensor = torch.cat([recv_from_left, in_tensor], dim=-1)
-        else:
-            in_tensor = torch.cat([recv_from_left, in_tensor, recv_from_right], dim=-1)
-
-        # step3 feed local input tensor to op_call
+        # step2 feed local input tensor to op_call
         local_tensor_args = list(local_tensor_args)
         local_tensor_args[0] = in_tensor
         local_tensor_args = cast(Tuple[object, ...], local_tensor_args)
         local_results = op_call(*local_tensor_args, **local_tensor_kwargs)
 
-        # step4 remove extra ouputs from the results
+        # step3 remove extra ouputs from the results
         padding = local_tensor_args[4][1]
         w = local_results.size(3)
         if rank == 0:
@@ -137,32 +183,12 @@ def tp_convolution_backward(
         right = (rank + 1) % size
         left = (rank - 1 + size) % size
 
-        # step1 dist comms
-        send_to_right = in_tensor[:, :, :, -d1:].contiguous()
-        send_to_left = in_tensor[:, :, :, :d2].contiguous()
-        recv_from_right = torch.zeros_like(send_to_left)
-        recv_from_left = torch.zeros_like(send_to_right)
-
-        send_op_right = dist.P2POp(dist.isend, send_to_right, right)
-        send_op_left = dist.P2POp(dist.isend, send_to_left, left)
-        recv_op_right = dist.P2POp(dist.irecv, recv_from_right, right)
-        recv_op_left = dist.P2POp(dist.irecv, recv_from_left, left)
-
-        reqs = dist.batch_isend_irecv(
-            [send_op_right, send_op_left, recv_op_left, recv_op_right]
+        # step1 reconstruct local input tensor
+        in_tensor = _ring_send_recv_construct(
+            in_tensor, d1, d2, left, right, rank, size
         )
-        for req in reqs:
-            req.wait()
 
-        # step2 reconstruct local input tensor
-        if rank == 0:
-            in_tensor = torch.cat([in_tensor, recv_from_right], dim=-1)
-        elif rank == size - 1:
-            in_tensor = torch.cat([recv_from_left, in_tensor], dim=-1)
-        else:
-            in_tensor = torch.cat([recv_from_left, in_tensor, recv_from_right], dim=-1)
-
-        # step3 reconstruct local gradient output tensor
+        # step2 reconstruct local gradient output tensor
         N, C_out, H_out, _ = grad_out_tensor.shape
         padding = local_tensor_args[5][1]
         if rank == 0:
@@ -178,50 +204,18 @@ def tp_convolution_backward(
                 grad_out_tensor, (padding, padding), "constant", 0
             )
 
-        # step4 feed local input tensor to op_call
+        # step3 feed local input tensor to op_call
         local_tensor_args = list(local_tensor_args)
         local_tensor_args[0] = grad_out_tensor
         local_tensor_args[1] = in_tensor
         local_tensor_args = cast(Tuple[object, ...], local_tensor_args)
         local_results = op_call(*local_tensor_args, **local_tensor_kwargs)
 
-        # step5 aggregate gradients for edge pixels
+        # step4 aggregate gradients for edge pixels
         grad_in_tensor = local_results[0]
-
-        send_to_right = grad_in_tensor[:, :, :, -d2:].contiguous()
-        send_to_left = grad_in_tensor[:, :, :, :d1].contiguous()
-        recv_from_right = torch.zeros_like(send_to_left)
-        recv_from_left = torch.zeros_like(send_to_right)
-
-        send_op_right = dist.P2POp(dist.isend, send_to_right, right)
-        send_op_left = dist.P2POp(dist.isend, send_to_left, left)
-        recv_op_right = dist.P2POp(dist.irecv, recv_from_right, right)
-        recv_op_left = dist.P2POp(dist.irecv, recv_from_left, left)
-
-        reqs = dist.batch_isend_irecv(
-            [send_op_right, send_op_left, recv_op_left, recv_op_right]
+        grad_in_tensor = _ring_send_recv_aggregate(
+            grad_in_tensor, d1, d2, left, right, rank, size
         )
-        for req in reqs:
-            req.wait()
-
-        if rank == 0:
-            grad_in_tensor = grad_in_tensor[:, :, :, :-d2]
-            grad_in_tensor[:, :, :, -d1:] = torch.add(
-                grad_in_tensor[:, :, :, -d1:], recv_from_right
-            )
-        elif rank == size - 1:
-            grad_in_tensor = grad_in_tensor[:, :, :, d1:]
-            grad_in_tensor[:, :, :, :d2] = torch.add(
-                grad_in_tensor[:, :, :, :d2], recv_from_left
-            )
-        else:
-            grad_in_tensor = grad_in_tensor[:, :, :, d1:-d2]
-            grad_in_tensor[:, :, :, -d1:] = torch.add(
-                grad_in_tensor[:, :, :, -d1:], recv_from_right
-            )
-            grad_in_tensor[:, :, :, :d2] = torch.add(
-                grad_in_tensor[:, :, :, :d2], recv_from_left
-            )
 
         local_results = list(local_results)
         local_results[0] = grad_in_tensor
