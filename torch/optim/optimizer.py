6b645d7d1ada14376d7b23cfc034ce3b2539a8ea
Alperen ÜNLÜ <97191996+alperenunlu@users.noreply.github.com>
2023-11-09 23:15:23 +0300

    Update optimizer.py

--- torch/optim/optimizer.py
+++ torch/optim/optimizer.py
@@ -761,8 +761,7 @@ class Optimizer:
             elif isinstance(value, dict):
                 return {k: _cast(param, v, param_id=param_id, param_groups=param_groups, key=k) for k, v in value.items()}
             elif isinstance(value, Iterable):
-                # type: ignore[call-arg]
-                return type(value)(_cast(param, v, param_id=param_id, param_groups=param_groups) for v in value)
+                return type(value)(_cast(param, v, param_id=param_id, param_groups=param_groups) for v in value) # type: ignore[call-arg]
             else:
                 return value
 
