0af9b9c102c70f6dff8fea1ce0d402b01d7e0c95
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2023-11-10 18:28:33 +0000

    Update base for Update on "Extend _TestONNXRuntime to reuses all tests for new model format"
    _TestONNXRuntime has infra to test models which are either Callable or a
    torch.nn.Module.
    
    After #111497, we want to re-run all those tests for model input of type
    torch.export.ExportedProgram.
    
    This PR adds a terrible flag that must be specified by all
    self.run_test_with_fx_to_onnx_exporter_and_onnx_runtime calls to force
    the model conversion to torch.export.ExportedProgram before runnning
    tests. This is not ideal, just a quick hack, though
    
    Ideally, we should be able to _TestONNXRuntime, inheriting all the
    parameterized classes and be able to overload
    run_test_with_fx_to_onnx_exporter_and_onnx_runtime or similar.
    run_test_with_fx_to_onnx_exporter_and_onnx_runtime could also be moved
    out of the class
    
    A quick PoC shows the idea of writing on BaseTestCase and reusing
    through different test suits:
    
    ```python
    import unittest
    
    class _FXtoONNXRuntimeBasic:
        def __init__(self, input):
            self.input = input
        def get_result(self):
            return self.input+1
    
    class _FXtoONNXRuntimeExportedProgram(_FXtoONNXRuntimeBasic):
        def get_result(self):
            return self.input*10
    
    class _FXtoONNXRuntimeBaseTess(unittest.TestCase):
    
        def check(self, input, expected_output):
            obj = self.class_under_test(input)
            actual_output = obj.get_result()
            self.assertEqual(actual_output, expected_output)
    
        def check_all(self):
            for value in self.values:
                self.check(value[0], value[1])
    
    class TestFXtoONNXRuntimeBasic(_FXtoONNXRuntimeBaseTess):
        values = ((1, 2), (3, 4))
        class_under_test = _FXtoONNXRuntimeBasic
    
        def test_it(self):
            print("TestFXtoONNXRuntimeBasic.test_it")
            self.check_all()
    
    class TestFXtoONNXRuntimeExportedProgram(_FXtoONNXRuntimeBaseTess):
        values = ((1, 10), (2, 20))
        class_under_test = _FXtoONNXRuntimeExportedProgram
    
        def test_it(self):
            print("TestFXtoONNXRuntimeExportedProgram.test_it")
            self.check_all()
    ```
    
    [ghstack-poisoned]

--- torch/onnx/_internal/io_adapter.py
+++ torch/onnx/_internal/io_adapter.py
@@ -175,7 +175,7 @@ def _assert_identical_pytree_spec(
         raise ValueError(f"{error_message}\nExpect {spec1}.\nActual {spec2}.")
 
 
-class BindInputStep:
+class BindInputStep(InputAdaptStep):
     """Bind the input arguments to the model signature."""
 
     def __init__(self, model_signature: inspect.Signature):
@@ -213,7 +213,7 @@ class BindInputStep:
         return (), bound.arguments
 
 
-class MergeKwargsIntoArgsInputStep:
+class MergeKwargsIntoArgsInputStep(InputAdaptStep):
     """Merge the input kwargs into the input args."""
 
     def apply(
@@ -233,7 +233,7 @@ class MergeKwargsIntoArgsInputStep:
         return tuple(model_args) + tuple(model_kwargs.values()), {}
 
 
-class LiftParametersAndBuffersIntoArgsInputStep:
+class LiftParametersAndBuffersIntoArgsInputStep(InputAdaptStep):
     """Append parameters and buffers to model's positional argument list."""
 
     def __init__(self, inputs: Tuple["torch.Tensor", ...]) -> None:
@@ -256,7 +256,7 @@ class LiftParametersAndBuffersIntoArgsInputStep:
         return (*model_args, *self.inputs), model_kwargs
 
 
-class ConvertComplexToRealRepresentationInputStep:
+class ConvertComplexToRealRepresentationInputStep(InputAdaptStep):
     """Convert complex dtype tensors to real representation tensors.
 
     ONNX does not support complex dtype tensors. Thus, we convert complex dtype tensors
@@ -290,7 +290,7 @@ class ConvertComplexToRealRepresentationInputStep:
         )
 
 
-class RemoveNoneInputStep:
+class RemoveNoneInputStep(InputAdaptStep):
     """Remove `None` from arguments.
 
     This adapt step assumes ``model_kwargs`` is empty. It also assumes ``model_args``
@@ -318,7 +318,7 @@ class RemoveNoneInputStep:
         return tuple(arg for arg in model_args if arg is not None), {}
 
 
-class RemoveNonTensorInputStep:
+class RemoveNonTensorInputStep(InputAdaptStep):
     """Remove the non-tensor input arguments.
 
     Dynamo does not support non-tensor input arguments (https://github.com/pytorch/pytorch/issues/99534).
@@ -382,7 +382,7 @@ class RemoveNonTensorInputStep:
         )
 
 
-class FlattenInputWithTreeSpecValidationInputStep:
+class FlattenInputWithTreeSpecValidationInputStep(InputAdaptStep):
     """Flatten nested collection types and return a flat list of elements.
 
     ONNX can't represent collection types (e.g., dictionary, tuple of tuple of tensor,
@@ -426,7 +426,7 @@ class FlattenInputWithTreeSpecValidationInputStep:
         return flattened_args, {}
 
 
-class FlattenOutputStep:
+class FlattenOutputStep(OutputAdaptStep):
     """Flatten nested collection types and return a flat list of elements.
 
     ONNX can't represent collection types (e.g., dictionary, tuple of tuple of tensor,
@@ -446,7 +446,7 @@ class FlattenOutputStep:
         return flattened_outputs
 
 
-class ConvertComplexToRealRepresentationOutputStep:
+class ConvertComplexToRealRepresentationOutputStep(OutputAdaptStep):
     """Convert complex dtype tensors to real representation tensors.
 
     ONNX does not support complex dtype tensors. Thus, we convert complex dtype tensors
@@ -472,7 +472,7 @@ class ConvertComplexToRealRepresentationOutputStep:
         ]
 
 
-class FlattenOutputWithTreeSpecValidationOutputStep:
+class FlattenOutputWithTreeSpecValidationOutputStep(OutputAdaptStep):
     """Same as ``FlattenOutputStep``, with additional `TreeSpec` validation.
 
     This class stores the `SpecTree` output produced when `adapt` was called the first
@@ -507,7 +507,7 @@ class FlattenOutputWithTreeSpecValidationOutputStep:
         return flattened_outputs
 
 
-class PrependParamsAndBuffersAotAutogradInputStep:
+class PrependParamsAndBuffersAotAutogradInputStep(InputAdaptStep):
     """Prepend model parameters and buffers to the user input.
 
     :func:`torch.export.export` lifts model parameters and buffers as model input, thus, they
