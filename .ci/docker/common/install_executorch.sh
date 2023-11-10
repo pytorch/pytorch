6e9aed871b072d0b484da8e4a3d16bf3a29bbbb1
Huy Do <huydhn@gmail.com>
2023-11-09 16:39:41 -0800

    Init submodules

--- .ci/docker/common/install_executorch.sh
+++ .ci/docker/common/install_executorch.sh
@@ -13,6 +13,7 @@ clone_executorch() {
   # and fetch the target commit
   pushd executorch
   git checkout "${EXECUTORCH_PINNED_COMMIT}"
+  git submodule update --init
   popd
 
   chown -R jenkins executorch
