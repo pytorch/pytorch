9cfbeed662f73d37d8a30929a779ab4a1dd939ec
Siddharth Mishra <98332290+RustyGrackle@users.noreply.github.com>
2023-11-10 22:23:44 +0530

    Update gradcheck.py

--- torch/autograd/gradcheck.py
+++ torch/autograd/gradcheck.py
@@ -256,10 +256,9 @@ def _iter_tensor(x_tensor):
 def _get_numerical_jacobian(
     fn, inputs, outputs=None, target=None, eps=1e-3, is_forward_ad=False
 ) -> List[Tuple[torch.Tensor, ...]]:
-    """Calculate numerical Jacobian.
+    """Compute the numerical Jacobian of `fn(inputs)` with respect to `target`.
 
-    Computes the numerical Jacobian of `fn(inputs)` with respect to `target`. If
-    not specified, targets are the input. Returns M * N Jacobians where N is the
+    If not specified, targets are the input. Returns M * N Jacobians where N is the
     number of tensors in target that require grad and M is the number of non-integral
     outputs.
 
