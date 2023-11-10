af7d07cade59a728d1cdfc36bd38e56764c2707e
zabboud <zeinab.h.abboud@gmail.com>
2023-11-10 10:53:34 -0500

    linting branch

--- torch/backends/mps/__init__.py
+++ torch/backends/mps/__init__.py
@@ -8,9 +8,9 @@ __all__ = ["is_built", "is_available", "is_macos13_or_newer"]
 
 def is_built() -> bool:
     r"""Return whether PyTorch is built with MPS support.
-    
-    Note that this doesn't necessarily mean MPS is available; just that 
-    if this PyTorch binary were run a machine with working MPS drivers 
+
+    Note that this doesn't necessarily mean MPS is available; just that
+    if this PyTorch binary were run a machine with working MPS drivers
     and devices, we would be able to use it.
     """
     return torch._C._has_mps
