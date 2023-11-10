80a0f18d957c26d3e591f5e2cd61bbc0a1d49dcf
Siddharth Mishra <98332290+RustyGrackle@users.noreply.github.com>
2023-11-10 11:39:26 +0530

    Update _trace.py

--- torch/jit/_trace.py
+++ torch/jit/_trace.py
@@ -1117,7 +1117,9 @@ def trace_module(
 
 
 def is_tracing():
-    """Return ``True`` in tracing (if a function is called during the tracing of code with ``torch.jit.trace``) and ``False`` otherwise."""
+    """Return a boolean value.
+
+    Returns ``True`` in tracing (if a function is called during the tracing of code with ``torch.jit.trace``) and ``False`` otherwise."""
     if is_scripting():
         return False
     return torch._C._is_tracing()
