ef89ae7c746b943ca17e46bc0ef4b3f7e30f7a67
Siddharth Mishra <98332290+RustyGrackle@users.noreply.github.com>
2023-11-11 00:55:03 +0530

    Update torch/distributed/fsdp/_common_utils.py
    Co-authored-by: Svetlana Karslioglu <svekars@meta.com>

--- torch/distributed/fsdp/_common_utils.py
+++ torch/distributed/fsdp/_common_utils.py
@@ -78,8 +78,9 @@ class _FSDPDeviceHandle:
     @classmethod
     def from_device(cls, device: torch.device) -> "_FSDPDeviceHandle":
         """
-        Return an device handle corresponding to the device, and through this handle, operations with the same semantics as CUDA can be performed on the device.
-
+        Return a device handle corresponding to the device 
+        
+        Through this handle, operations with the same semantics as CUDA can be performed on the device.
         Just return torch.cuda if the device is cuda to make attribute-access faster.
         Custom backend must first register a module with the same name with {device.type} on torch.
         """
