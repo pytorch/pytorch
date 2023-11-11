c77d8488f6ac6d54a3a2b80cace61971bfba6dd8
Ashvanth.S <ashvanth016@gmail.com>
2023-11-11 12:29:26 +0530

    Fixed docstring errors in distributed module

--- torch/distributed/benchmarks/benchmark_ddp_rpc.py
+++ torch/distributed/benchmarks/benchmark_ddp_rpc.py
@@ -32,14 +32,15 @@ WARMUP_CYCLES = 5
 
 class HybridModel(torch.nn.Module):
     r"""
-   The model consists of a sparse part and a dense part. The dense part is an
-   nn.Linear module that is replicated across all trainers using
-   DistributedDataParallel. The sparse part has nn.EmbeddingBags stored on multiple
-   parameter servers.
+    The model consists of a sparse part and a dense part.
 
-   The model holds a Remote Reference to the embedding tables on the parameter
-   servers.
-   """
+    The dense part is an nn.Linear module that is replicated across all trainers using
+    DistributedDataParallel. The sparse part has nn.EmbeddingBags stored on multiple
+    parameter servers.
+
+    The model holds a Remote Reference to the embedding tables on the parameter
+    servers.
+    """
 
     def __init__(self, emb_rref_list, device):
         super().__init__()
@@ -117,13 +118,13 @@ def _run_printable(cmd):
 
 def _run_trainer(emb_rref_list, rank):
     r"""
-   Each trainer runs a forward pass which involves an embedding lookup on the
-   8 parameter servers and running nn.Linear locally. During the backward pass,
-   DDP is responsible for aggregating the gradients for the dense part
-   (nn.Linear) and distributed autograd ensures gradients updates are
-   propagated to the parameter servers.
-   """
-
+    Each trainer runs a forward pass which involves an embedding lookup on the 8 parameter servers,
+    and running nn.Linear locally. 
+    
+    During the backward pass, DDP is responsible for aggregating the gradients for the dense part
+    (nn.Linear) and distributed autograd ensures gradients updates are
+    propagated to the parameter servers.
+    """
     # Setup the model.
     model = HybridModel(emb_rref_list, rank)
 
@@ -199,10 +200,8 @@ def _run_trainer(emb_rref_list, rank):
 
 def run_worker(rank, world_size):
     r"""
-   A wrapper function that initializes RPC, calls the function, and shuts down
-   RPC.
-   """
-
+    Initialize RPC, calls the function, and shuts down RPC.
+    """
     # Using different port numbers in TCP init_method for init_rpc and
     # init_process_group to avoid port conflicts.
     rpc_backend_options = TensorPipeRpcBackendOptions()
