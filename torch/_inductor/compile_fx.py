404515020a792b9dd64fb0932df70fcf64de89df
Mu-Chu Lee <mlee8@meta.com>
2023-11-12 22:28:36 -0800

    Update on "[TEST]"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -1,4 +1,5 @@
 import contextlib
+import copy
 import dataclasses
 import functools
 import logging
@@ -51,7 +52,6 @@ from .fx_passes.post_grad import post_grad_passes, view_to_reshape
 from .fx_passes.pre_grad import pre_grad_passes
 from .graph import GraphLowering
 from .ir import ExternKernelNode
-from .pattern_matcher import clone_graph
 from .utils import get_dtype_size, has_incompatible_cudagraph_ops
 from .virtualized import V
 
@@ -302,12 +302,11 @@ def inner_compile_with_cpp_wrapper(inner_compile: Callable[..., Any]):
                     **kwargs,
                     "cpp_wrapper": False,
                 }
-                # clone_graph(gm) makes sure no graph modification from the first pass will
+                # clone graph makes sure no graph modification from the first pass will
                 # leak to the second pass. It does increase memory pressure, but the problem
                 # can be alleviated once we have parameters as FakeTensor.
-                from copy import deepcopy
                 compiled = inner_compile(
-                    deepcopy(gm), example_inputs, **kwargs_patched
+                    copy.deepcopy(gm), example_inputs, **kwargs_patched
                 )
 
                 def materialize(x):
