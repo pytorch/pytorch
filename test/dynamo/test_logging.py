eb0d76e942e4f56312bc54266846c71761493909
Edward Z. Yang <ezyang@meta.com>
2023-11-13 07:47:16 -0800

    Refactor loggers to use NOTSET when not set by user
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- test/dynamo/test_logging.py
+++ test/dynamo/test_logging.py
@@ -244,14 +244,14 @@ LoweringException: AssertionError:
     def test_all(self, _):
         registry = torch._logging._internal.log_registry
 
-        dynamo_qname = registry.log_alias_to_log_qname["dynamo"]
+        dynamo_qnames = registry.log_alias_to_log_qnames["dynamo"]
         for logger_qname in torch._logging._internal.log_registry.get_log_qnames():
             logger = logging.getLogger(logger_qname)
 
-            if logger_qname == dynamo_qname:
-                self.assertEqual(logger.level, logging.INFO)
+            if logger_qname in dynamo_qnames:
+                self.assertEqual(logger.getEffectiveLevel(), logging.INFO, msg=f"expected {logger_qname} is INFO, got {logger.level}")
             else:
-                self.assertEqual(logger.level, logging.DEBUG)
+                self.assertEqual(logger.getEffectiveLevel(), logging.DEBUG, msg=f"expected {logger_qname} is DEBUG, got {logger.level}")
 
     @make_logging_test(graph_breaks=True)
     def test_graph_breaks(self, records):
