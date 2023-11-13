a23440e006a8de387a15611c7a9d2581e5517355
albanD <desmaison.alban@gmail.com>
2023-11-13 11:44:43 -0500

    Fix compilation issues on macos and windows

--- functorch/csrc/dim/dim.cpp
+++ functorch/csrc/dim/dim.cpp
@@ -11,6 +11,10 @@
 #if IS_PYTHON_3_12_PLUS
 
 // Re-enable this some day
+int Dim_init() {
+    PyErr_SetString(PyExc_RuntimeError, "First class dim doesn't work with python 3.12");
+    return -1;
+}
 
 #else
 
