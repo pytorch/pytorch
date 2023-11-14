d1347a9266b5ea7140d86ce0b84f943cb1776fe2
drisspg <drisspguessous@gmail.com>
2023-11-14 15:18:40 -0800

    Update base for Update on "Add framework for adding new attn bias variants"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- aten/src/ATen/native/transformers/cuda/sdp_utils.h
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <ATen/Context.h>
+#include <c10/macros/Export.h>
 #include <ATen/native/transformers/sdp_utils_cpp.h>
 
 namespace sdp {
 
 bool check_for_seq_len_1_nested_tensor(sdp_params const& params, bool debug);
 SDPBackend select_sdp_backend(sdp_params const& kernel_params);
-bool can_use_flash_attention(sdp_params const& params, bool debug);
-bool can_use_mem_efficient_attention(sdp_params const& params, bool debug);
+TORCH_API bool can_use_flash_attention(sdp_params const& params, bool debug);
+TORCH_API bool can_use_mem_efficient_attention(sdp_params const& params, bool debug);
 
 } // namespace sdp
