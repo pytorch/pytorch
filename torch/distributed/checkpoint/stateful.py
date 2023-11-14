359dd8002dd2276757c0205e308f0b56e7997efd
Lucas Pasqualin <lpasqualin@meta.com>
2023-11-14 12:49:34 -0800

    adds stateful protocol

---
+++ torch/distributed/checkpoint/stateful.py
@@ -0,0 +1,16 @@
+from typing import Any, Dict, TypeVar
+
+from typing_extensions import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class Stateful(Protocol):
+    def state_dict(self) -> Dict[str, Any]:
+        ...
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        ...
+
+
+T = TypeVar("T", bound=Stateful)
+AppState = Dict[str, T]
