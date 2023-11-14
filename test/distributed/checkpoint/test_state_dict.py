031573a3c12f8668af611be309712e4c9b1821ee
Lucas Pasqualin <lpasqualin@meta.com>
2023-11-14 11:42:42 -0800

    fix typo

--- test/distributed/checkpoint/test_state_dict.py
+++ test/distributed/checkpoint/test_state_dict.py
@@ -27,7 +27,7 @@ from torch.testing._internal.common_dist_composable import (
     UnitModule,
 )
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
-from torch.testing._internal.distribtued.common_state_dict import VerifyStateDictMixin
+from torch.testing._internal.distributed.common_state_dict import VerifyStateDictMixin
 from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     DTensorTestBase,
