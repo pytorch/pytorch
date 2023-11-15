5bb765129d73ae5efbf170ac27fdc569853d9a85
Yanbo Liang <ybliang8@gmail.com>
2023-11-14 21:17:24 -0800

    [Inductor] Refactor group/batch fusion to support user defined execution order and configs

--- torch/_inductor/fx_passes/pre_grad.py
+++ torch/_inductor/fx_passes/pre_grad.py
@@ -22,7 +22,7 @@ from ..pattern_matcher import (
     stable_topological_sort,
 )
 from ..utils import is_cpu_device
-from .group_batch_fusion import group_batch_fusion_pre_grad_passes
+from .group_batch_fusion import group_batch_fusion_passes
 from .misc_patterns import numpy_compat_normalization
 
 log = logging.getLogger(__name__)
@@ -68,8 +68,12 @@ def pre_grad_passes(gm: torch.fx.GraphModule, example_inputs):
     if config.pattern_matcher:
         lazy_init()
         gm = fuse_fx(gm, example_inputs)
+<<<<<<< HEAD
         numpy_compat_normalization(gm.graph)
         group_batch_fusion_pre_grad_passes(gm.graph)
+=======
+        group_batch_fusion_passes(gm.graph, True)
+>>>>>>> 273280b1df ([Inductor] Refactor group/batch fusion to support user defined execution order and configs)
         for pattern_matcher_pass in pattern_matcher_passes:
             pattern_matcher_pass.apply(gm.graph)
 
