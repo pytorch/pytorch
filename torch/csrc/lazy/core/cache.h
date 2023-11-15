fdac8da2a8c23766cf1e0e69a8b141294c7a4d31
Behzad Abghari <behzad@cerebras.net>
2023-11-15 08:01:12 -0500

    Avoid acquiring lock when size is zero

--- torch/csrc/lazy/core/cache.h
+++ torch/csrc/lazy/core/cache.h
@@ -34,12 +34,10 @@ class Cache {
   // beyond the limit set during construction, the oldest used object will be
   // removed from the cache.
   TypePtr Add(K key, TypePtr object) {
-    std::lock_guard<std::mutex> slock(lock_);
-
     if (!max_size_) {
       return object;
     }
-
+    std::lock_guard<std::mutex> slock(lock_);
     element_list_.emplace_front(Element(std::move(key), std::move(object)));
     auto it = element_list_.begin();
     auto emplace_result = element_map_.emplace(&it->first, it);
@@ -59,6 +57,9 @@ class Cache {
   // Returns nullptr if no object with the specified key is found within the
   // cache.
   TypePtr Get(const K& key) {
+    if (!max_size_) {
+      return nullptr;
+    }
     std::lock_guard<std::mutex> slock(lock_);
     auto it = element_map_.find(&key);
     if (it == element_map_.end()) {
@@ -75,6 +76,9 @@ class Cache {
   }
 
   bool Erase(const K& key) {
+    if (!max_size_) {
+      return false;
+    }
     std::lock_guard<std::mutex> slock(lock_);
     auto it = element_map_.find(&key);
     if (it == element_map_.end()) {
@@ -87,12 +91,18 @@ class Cache {
   }
 
   void Clear() {
+    if (!max_size_) {
+      return;
+    }
     std::lock_guard<std::mutex> slock(lock_);
     element_map_.clear();
     element_list_.clear();
   }
 
   int Numel() const {
+    if (!max_size_) {
+      return 0;
+    }
     std::lock_guard<std::mutex> g(lock_);
     TORCH_CHECK(element_map_.size() == element_list_.size());
     return element_map_.size();
@@ -125,7 +135,7 @@ class Cache {
   }
 
   mutable std::mutex lock_;
-  size_t max_size_ = 0;
+  const size_t max_size_ = 0;
   ElementList element_list_;
   ElementMap element_map_;
 };
