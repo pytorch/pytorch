6456a48f9a5c12639abd593c840d2aad03d35078
Ting Lu <tingl@nvidia.com>
2023-11-16 13:52:04 -0800

    remove previous commit

--- test/distributed/fsdp/test_fsdp_fine_tune.py
+++ test/distributed/fsdp/test_fsdp_fine_tune.py
@@ -56,11 +56,6 @@ class TestFSDPFineTune(FSDPTest):
                 for param in seq[i * 2].parameters(recurse=True):
                     param.requires_grad = requires_grad
 
-    @classmethod
-    @skip_if_lt_x_gpu(2)
-    def _run(cls, rank, test_name, file_name, pipe):
-        super()._run(rank, test_name, file_name, pipe)
-
     @skip_if_lt_x_gpu(2)
     def test_backward_reshard_hooks(self):
         """
