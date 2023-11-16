968f98ebb2c68d538bdf276f74e525bf115dc892
lezcano <lezcano-93@hotmail.com>
2023-11-16 15:38:15 +0000

    [BE][easy] Simplify the registration of a few metafunctions (#113635)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113635
    Approved by: https://github.com/Skylion007
    ghstack dependencies: #113634, #113674

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -5947,41 +5947,11 @@ _create_unary_float_meta_func(aten.special_scaled_modified_bessel_k0)
 _create_unary_float_meta_func(aten.special_scaled_modified_bessel_k1)
 
 
-_create_binary_float_meta_func(
-    [
-        aten.special_chebyshev_polynomial_t.default,
-        aten.special_chebyshev_polynomial_t.out,
-        aten.special_chebyshev_polynomial_t.n_scalar_out,
-    ]
-)
-_create_binary_float_meta_func(
-    [
-        aten.special_chebyshev_polynomial_u.default,
-        aten.special_chebyshev_polynomial_u.out,
-        aten.special_chebyshev_polynomial_u.n_scalar_out,
-    ]
-)
-_create_binary_float_meta_func(
-    [
-        aten.special_hermite_polynomial_h.default,
-        aten.special_hermite_polynomial_h.out,
-        aten.special_hermite_polynomial_h.n_scalar_out,
-    ]
-)
-_create_binary_float_meta_func(
-    [
-        aten.special_hermite_polynomial_he.default,
-        aten.special_hermite_polynomial_he.out,
-        aten.special_hermite_polynomial_he.n_scalar_out,
-    ]
-)
-_create_binary_float_meta_func(
-    [
-        aten.special_laguerre_polynomial_l.default,
-        aten.special_laguerre_polynomial_l.out,
-        aten.special_laguerre_polynomial_l.n_scalar_out,
-    ]
-)
+_create_binary_float_meta_func(aten.special_chebyshev_polynomial_t)
+_create_binary_float_meta_func(aten.special_chebyshev_polynomial_u)
+_create_binary_float_meta_func(aten.special_hermite_polynomial_h)
+_create_binary_float_meta_func(aten.special_hermite_polynomial_he)
+_create_binary_float_meta_func(aten.special_laguerre_polynomial_l)
 
 
 # We must also trigger meta registrations from PrimTorch ref
