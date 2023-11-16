11591c28e226840c1c9fff6b9fbd719034ab053c
Jez Ng <jezng@fb.com>
2023-11-15 18:27:37 -0800

    [dynamo] Make {guards,eval_frame}.py pass follow_imports typechecking (#113721)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113721
    Approved by: https://github.com/Skylion007
    ghstack dependencies: #113722

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -1,3 +1,5 @@
+# mypy: disable-error-code="method-assign"
+
 from __future__ import annotations
 
 import contextlib
@@ -115,7 +117,7 @@ def _reset_guarded_backend_cache():
 
 
 @contextlib.contextmanager
-def backend_cache_wrapper(callback: CompilerFn):
+def backend_cache_wrapper(callback: DynamoCallback):
     _maybe_init_guarded_backend_cache()
 
     # callback is False for RunOnlyContext. RunOnlyContext is used
@@ -175,6 +177,9 @@ class OptimizedModule(torch.nn.Module):
     Wraps the original nn.Module object and later patches its
     forward method to optimized self.forward method.
     """
+
+    _torchdynamo_orig_callable: Callable[..., Any]
+    get_compiler_config: Callable[[], Any]
 
     def __init__(self, mod: torch.nn.Module, dynamo_ctx):
         super().__init__()
@@ -416,7 +421,7 @@ class _TorchDynamoContext:
             # when compiling torch.nn.Module,
             # provide public api OptimizedModule.get_compiler_config()
             assert not hasattr(new_mod, "get_compiler_config")
-            new_mod.get_compiler_config = get_compiler_config  # type: ignore[attr-defined]
+            new_mod.get_compiler_config = get_compiler_config
 
             return new_mod
         assert callable(fn)
@@ -929,7 +934,7 @@ class FlattenInputOutputSignature(torch.fx.interpreter.Transformer):
 
 class ExportResult(NamedTuple):
     graph_module: torch.fx.GraphModule
-    guards: Set[_guards.Guard]
+    guards: _guards.GuardsSet
     # NB: Do not add new fields without overriding __iter__; people are
     # destructuring so it is BC-breaking
 
@@ -1222,7 +1227,7 @@ def export(
         graph_captured_result: Optional[Tuple[torch.Tensor, ...]] = None
         fake_mode = None
 
-        def guard_export_print(guards: Set[_guards.Guard]):
+        def guard_export_print(guards: _guards.GuardsSet):
             nonlocal out_guards
             assert (
                 out_guards is None
@@ -1564,7 +1569,7 @@ class TorchPatcher:
                     opt.step = unwrapped_step
 
             # disable future hooking
-            opt.step.hooked = True
+            opt.step.hooked = True  # type: ignore[attr-defined]
 
     @staticmethod
     def suppress_torch_distributed_warnings(fn):
