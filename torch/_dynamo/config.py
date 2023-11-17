00b67193efc34702aaf70f2af158e0ce712c18b9
Jon Chuang <9093549+jon-chuang@users.noreply.github.com>
2023-11-17 01:07:05 -0500

    [utils] move `config_typing.pyi `to `torch.utils` (#113929)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113929
    Approved by: https://github.com/ezyang, https://github.com/jansel
    ghstack dependencies: #111299, #111300, #113901, #113916

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -349,7 +349,7 @@ _autograd_backward_strict_mode_banned_ops.extend(
 _experimental_support_context_fn_in_torch_utils_checkpoint = False
 
 if TYPE_CHECKING:
-    from .config_typing import *  # noqa: F401, F403
+    from torch.utils._config_typing import *  # noqa: F401, F403
 
 from torch.utils._config_module import install_config_module
 
