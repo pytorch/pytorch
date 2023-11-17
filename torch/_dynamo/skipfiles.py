0d906c5214bb5425f0c8bdde5698b92a1142159e
Yanbo Liang <ybliang8@gmail.com>
2023-11-16 16:15:58 -0800

    Update

--- torch/_dynamo/skipfiles.py
+++ torch/_dynamo/skipfiles.py
@@ -377,7 +377,9 @@ def check_verbose(obj, is_inlined_call=False):
     elif isinstance(obj, types.CodeType):
         fi = FunctionInfo(None, obj.co_name, obj.co_filename, obj)
     elif isinstance(obj, (types.FunctionType, types.MethodType)):
-        fi = FunctionInfo(obj, obj.__name__, getfile(obj), obj.__code__)  # type: ignore[union-attr] # FIXME Add MethodType.__code__ to typeshed
+        fi = FunctionInfo(
+            obj, obj.__name__, getfile(obj), obj.__code__  # type: ignore[union-attr] # FIXME Add MethodType.__code__ to typeshed
+        )
     else:
         fi = FunctionInfo(obj, None, getfile(obj), None)
     # Go through function based skip/inline rules.
