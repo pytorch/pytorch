b53d47a7196906891f22b4d154ce5053739fd1d8
Jiong Gong <jiong.gong@intel.com>
2023-11-17 22:39:31 +0800

    [inductor cpp] refactor: CppVecOverrides inherits CppOverrides (#113950)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113950
    Approved by: https://github.com/Skylion007

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -436,7 +436,362 @@ def get_current_node_opt_ctx() -> OptimizationContext:
     return get_opt_ctx(V.interpreter.current_node)
 
 
-class CppVecOverrides(OpOverrides):
+class CppOverrides(OpOverrides):
+    """Map element-wise ops to C++"""
+
+    @staticmethod
+    def add(a, b):
+        return f"decltype({a})({a} + {b})"
+
+    @staticmethod
+    def sub(a, b):
+        return f"decltype({a})({a} - {b})"
+
+    @staticmethod
+    def mul(a, b):
+        return f"decltype({a})({a} * {b})"
+
+    @staticmethod
+    def to_dtype(x, dtype, src_dtype=None):
+        assert dtype in DTYPE_TO_CPP, f"{dtype} missing from {__name__}.DTYPE_TO_CPP"
+        return f"c10::convert<{DTYPE_TO_CPP[dtype]}>({x})"
+
+    @staticmethod
+    def to_dtype_bitcast(x, dtype):
+        assert dtype in DTYPE_TO_CPP, f"{dtype} missing from {__name__}.DTYPE_TO_CPP"
+        return f"c10::bit_cast<{DTYPE_TO_CPP[dtype]}>({x})"
+
+    @staticmethod
+    def abs(x):
+        return f"std::abs({x})"
+
+    @staticmethod
+    def sin(x):
+        return f"std::sin({x})"
+
+    @staticmethod
+    def cos(x):
+        return f"std::cos({x})"
+
+    @staticmethod
+    def neg(x):
+        return f"decltype({x})(-{x})"
+
+    @staticmethod
+    def exp(x):
+        # return f"Sleef_expf_u10({x})"
+        return f"std::exp({x})"
+
+    @staticmethod
+    def exp2(x):
+        return f"std::exp2({x})"
+
+    @staticmethod
+    def expm1(x):
+        return f"std::expm1({x})"
+
+    @staticmethod
+    def erf(x):
+        return f"std::erf({x})"
+
+    @staticmethod
+    def erfc(x):
+        return f"std::erfc({x})"
+
+    @staticmethod
+    def erfinv(x):
+        return f"calc_erfinv({x})"
+
+    @staticmethod
+    def sqrt(x):
+        return f"std::sqrt({x})"
+
+    @staticmethod
+    def rsqrt(x):
+        return f"1 / std::sqrt({x})"
+
+    @staticmethod
+    def log1p(x):
+        bug = config.cpp.inject_log1p_bug_TESTING_ONLY
+        if bug == "accuracy":
+            return f"{x} + decltype({x})(1)"
+        elif bug is None:
+            return f"std::log1p({x})"
+        else:
+            raise AssertionError(
+                f"unrecognized config cpp.inject_log1p_bug_TESTING_ONLY = {bug!r}"
+            )
+
+    @staticmethod
+    def tan(x):
+        return f"std::tan({x})"
+
+    @staticmethod
+    def tanh(x):
+        return f"std::tanh({x})"
+
+    @staticmethod
+    def signbit(x):
+        return f"std::signbit({x})"
+
+    @staticmethod
+    def pow(a, b):
+        return f"std::pow({a}, {b})"
+
+    @staticmethod
+    def log(x):
+        return f"std::log({x})"
+
+    @staticmethod
+    def round(x):
+        return f"std::nearbyint({x})"
+
+    @staticmethod
+    def floor(x):
+        return f"std::floor({x})"
+
+    @staticmethod
+    def floordiv(a, b):
+        # a and b are integer type
+        quot = f"{a} / {b}"
+        rem = f"{a} % {b}"
+        return f"(({a} < 0) != ({b} < 0) ? ({rem} != 0 ? {quot} - 1 : {quot}) : {quot})"
+
+    @staticmethod
+    def ceil(x):
+        return f"std::ceil({x})"
+
+    @staticmethod
+    def trunc(x):
+        return f"std::trunc({x})"
+
+    @staticmethod
+    def truncdiv(a, b):
+        # a and b are integer type
+        return f"{a} / {b}"
+
+    @staticmethod
+    def fmod(a, b):
+        return f"std::fmod({a}, {b})"
+
+    @staticmethod
+    def isinf(x):
+        return f"std::isinf({x})"
+
+    @staticmethod
+    def isnan(x):
+        return f"std::isnan({x})"
+
+    @staticmethod
+    def lgamma(x):
+        return f"std::lgamma({x})"
+
+    @staticmethod
+    def acos(x):
+        return f"std::acos({x})"
+
+    @staticmethod
+    def acosh(x):
+        return f"std::acosh({x})"
+
+    @staticmethod
+    def cosh(x):
+        return f"std::cosh({x})"
+
+    @staticmethod
+    def sinh(x):
+        return f"std::sinh({x})"
+
+    @staticmethod
+    def asin(x):
+        return f"std::asin({x})"
+
+    @staticmethod
+    def asinh(x):
+        return f"std::asinh({x})"
+
+    @staticmethod
+    def atan2(x, y):
+        return f"std::atan2({x}, {y})"
+
+    @staticmethod
+    def atan(x):
+        return f"std::atan({x})"
+
+    @staticmethod
+    def atanh(x):
+        return f"std::atanh({x})"
+
+    @staticmethod
+    def copysign(x, y):
+        return f"std::copysign({x}, {y})"
+
+    @staticmethod
+    def hypot(x, y):
+        return f"std::hypot({x}, {y})"
+
+    @staticmethod
+    def log10(x):
+        return f"std::log10({x})"
+
+    @staticmethod
+    def nextafter(x, y):
+        return f"std::nextafter({x}, {y})"
+
+    @staticmethod
+    def relu(x):
+        bug = config.cpp.inject_relu_bug_TESTING_ONLY
+        if bug == "compile_error":
+            return "compile error!"
+        elif bug == "runtime_error":
+            return f"{x}; throw 1"
+        elif bug == "accuracy":
+            return f"{x} + decltype({x})(1)"
+        elif bug is None:
+            return f"{x} * ({x}>0)"
+        else:
+            raise AssertionError(
+                f"unrecognized config cpp.inject_relu_bug_TESTING_ONLY = {bug!r}"
+            )
+
+    @staticmethod
+    def minimum(a, b):
+        return f"min_propagate_nan({a}, {b})"
+
+    @staticmethod
+    def maximum(a, b):
+        return f"max_propagate_nan({a}, {b})"
+
+    @staticmethod
+    def where(a, b, c):
+        return f"{a} ? {b} : {c}"
+
+    @staticmethod
+    def mod(a, b):
+        return f"mod({a}, {b})"
+
+    @staticmethod
+    def constant(val, dtype):
+        if dtype in DTYPE_LOWP_FP:
+            # Since load promotes all half-precision inputs to float, constants
+            # must be promoted as well
+            dtype = torch.float32
+        if val == float("inf"):
+            return f"std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::infinity()"
+        elif val == float("-inf"):
+            return f"-std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::infinity()"
+        elif math.isnan(val):
+            return f"std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::quiet_NaN()"
+        elif val is True or val is False:
+            return ops.to_dtype(str(val).lower(), dtype)
+        return ops.to_dtype(repr(val), dtype)
+
+    @staticmethod
+    def index_expr(expr, dtype):
+        return ops.to_dtype(cexpr(V.kernel.rename_indexing(expr)), dtype)
+
+    @staticmethod
+    def masked(mask, body, other):
+        code = BracesBuffer()
+
+        # Write masked operation into a lambda
+        body_var = V.kernel.cse.newvar()
+        code.writeline(f"auto {body_var} = [&]")
+        with V.kernel.swap_buffers(code), code.indent():
+            result = body()
+            code.writeline(f"return {result};")
+        code.writeline(";")
+        V.kernel.compute.splice(code)
+
+        # Use the lambda's return type as the type of other
+        type = f"decltype({body_var}())"
+
+        if other == float("-inf"):
+            other_code = f"-std::numeric_limits<{type}>::infinity()"
+        elif other == float("inf"):
+            other_code = f"std::numeric_limits<{type}>::infinity()"
+        elif isinstance(other, bool):
+            other_code = f"static_cast<{type}>({str(other).lower()})"
+        elif math.isnan(other):
+            other_code = f"std::numeric_limits<{type}>::quiet_NaN()"
+        else:
+            other_code = f"static_cast<{type}>({repr(other)})"
+
+        return f"{mask} ? {body_var}() : {other_code}"
+
+    @staticmethod
+    def logical_and(a, b):
+        return f"{a} && {b}"
+
+    @staticmethod
+    def logical_not(a):
+        return f"!{a}"
+
+    @staticmethod
+    def logical_or(a, b):
+        return f"{a} || {b}"
+
+    @staticmethod
+    def logical_xor(a, b):
+        return f"{a} != {b}"
+
+    @staticmethod
+    def bitwise_and(a, b):
+        return f"decltype({a})({a} & {b})"
+
+    @staticmethod
+    def bitwise_not(a):
+        return f"decltype({a})(~{a})"
+
+    @staticmethod
+    def bitwise_or(a, b):
+        return f"decltype({a})({a} | {b})"
+
+    @staticmethod
+    def bitwise_xor(a, b):
+        return f"decltype({a})({a} ^ {b})"
+
+    @staticmethod
+    def bitwise_left_shift(a, b):
+        return f"decltype({a})({a} << {b})"
+
+    @staticmethod
+    def bitwise_right_shift(a, b):
+        return f"decltype({a})({a} >> {b})"
+
+    @staticmethod
+    def rand(seed: sympy.Expr, offset: sympy.Expr):
+        return f"normalized_rand_cpu({seed}, {offset})"
+
+    @staticmethod
+    def randn(seed: sympy.Expr, offset: sympy.Expr):
+        return f"randn_cpu({seed}, {offset})"
+
+    @staticmethod
+    def randint64(seed: sympy.Expr, offset: sympy.Expr, low, high):
+        return f"randint64_cpu({seed}, {offset}, {low}, {high})"
+
+    @staticmethod
+    def sigmoid(x):
+        return f"decltype({x})(1) / (decltype({x})(1) + std::exp(-{x}))"
+
+    @staticmethod
+    def sign(x):
+        code = BracesBuffer()
+        # auto tmp5 = tmp4 < 0 ? -1 : 1;
+        left = V.kernel.cse.newvar()
+        right = V.kernel.cse.newvar()
+        result = V.kernel.cse.newvar()
+        scalar_zero = f"decltype({x})(0)"
+        scalar_one = f"decltype({x})(1)"
+        code.writeline(f"auto {left} = {x} > 0 ? {scalar_one} : {scalar_zero};")
+        code.writeline(f"auto {right} = {x} < 0 ? {scalar_one} : {scalar_zero};")
+        code.writeline(f"auto {result} = {left} - {right};")
+        V.kernel.compute.splice(code)
+        return result
+
+
+class CppVecOverrides(CppOverrides):
     """Map element-wise ops to aten vectorization C++"""
 
     @staticmethod
@@ -830,361 +1185,6 @@ class CppVecOverrides(OpOverrides):
         assert opt_ctx.dtype == torch.int32
         assert opt_ctx.is_most_inner_loop_irrevelant
         return f"at::vec::Vectorized<int>(static_cast<int>({cexpr(V.kernel.rename_indexing(expr))}))"
-
-
-class CppOverrides(OpOverrides):
-    """Map element-wise ops to C++"""
-
-    @staticmethod
-    def add(a, b):
-        return f"decltype({a})({a} + {b})"
-
-    @staticmethod
-    def sub(a, b):
-        return f"decltype({a})({a} - {b})"
-
-    @staticmethod
-    def mul(a, b):
-        return f"decltype({a})({a} * {b})"
-
-    @staticmethod
-    def to_dtype(x, dtype, src_dtype=None):
-        assert dtype in DTYPE_TO_CPP, f"{dtype} missing from {__name__}.DTYPE_TO_CPP"
-        return f"c10::convert<{DTYPE_TO_CPP[dtype]}>({x})"
-
-    @staticmethod
-    def to_dtype_bitcast(x, dtype):
-        assert dtype in DTYPE_TO_CPP, f"{dtype} missing from {__name__}.DTYPE_TO_CPP"
-        return f"c10::bit_cast<{DTYPE_TO_CPP[dtype]}>({x})"
-
-    @staticmethod
-    def abs(x):
-        return f"std::abs({x})"
-
-    @staticmethod
-    def sin(x):
-        return f"std::sin({x})"
-
-    @staticmethod
-    def cos(x):
-        return f"std::cos({x})"
-
-    @staticmethod
-    def neg(x):
-        return f"decltype({x})(-{x})"
-
-    @staticmethod
-    def exp(x):
-        # return f"Sleef_expf_u10({x})"
-        return f"std::exp({x})"
-
-    @staticmethod
-    def exp2(x):
-        return f"std::exp2({x})"
-
-    @staticmethod
-    def expm1(x):
-        return f"std::expm1({x})"
-
-    @staticmethod
-    def erf(x):
-        return f"std::erf({x})"
-
-    @staticmethod
-    def erfc(x):
-        return f"std::erfc({x})"
-
-    @staticmethod
-    def erfinv(x):
-        return f"calc_erfinv({x})"
-
-    @staticmethod
-    def sqrt(x):
-        return f"std::sqrt({x})"
-
-    @staticmethod
-    def rsqrt(x):
-        return f"1 / std::sqrt({x})"
-
-    @staticmethod
-    def log1p(x):
-        bug = config.cpp.inject_log1p_bug_TESTING_ONLY
-        if bug == "accuracy":
-            return f"{x} + decltype({x})(1)"
-        elif bug is None:
-            return f"std::log1p({x})"
-        else:
-            raise AssertionError(
-                f"unrecognized config cpp.inject_log1p_bug_TESTING_ONLY = {bug!r}"
-            )
-
-    @staticmethod
-    def tan(x):
-        return f"std::tan({x})"
-
-    @staticmethod
-    def tanh(x):
-        return f"std::tanh({x})"
-
-    @staticmethod
-    def signbit(x):
-        return f"std::signbit({x})"
-
-    @staticmethod
-    def pow(a, b):
-        return f"std::pow({a}, {b})"
-
-    @staticmethod
-    def log(x):
-        return f"std::log({x})"
-
-    @staticmethod
-    def round(x):
-        return f"std::nearbyint({x})"
-
-    @staticmethod
-    def floor(x):
-        return f"std::floor({x})"
-
-    @staticmethod
-    def floordiv(a, b):
-        # a and b are integer type
-        quot = f"{a} / {b}"
-        rem = f"{a} % {b}"
-        return f"(({a} < 0) != ({b} < 0) ? ({rem} != 0 ? {quot} - 1 : {quot}) : {quot})"
-
-    @staticmethod
-    def ceil(x):
-        return f"std::ceil({x})"
-
-    @staticmethod
-    def trunc(x):
-        return f"std::trunc({x})"
-
-    @staticmethod
-    def truncdiv(a, b):
-        # a and b are integer type
-        return f"{a} / {b}"
-
-    @staticmethod
-    def fmod(a, b):
-        return f"std::fmod({a}, {b})"
-
-    @staticmethod
-    def isinf(x):
-        return f"std::isinf({x})"
-
-    @staticmethod
-    def isnan(x):
-        return f"std::isnan({x})"
-
-    @staticmethod
-    def lgamma(x):
-        return f"std::lgamma({x})"
-
-    @staticmethod
-    def acos(x):
-        return f"std::acos({x})"
-
-    @staticmethod
-    def acosh(x):
-        return f"std::acosh({x})"
-
-    @staticmethod
-    def cosh(x):
-        return f"std::cosh({x})"
-
-    @staticmethod
-    def sinh(x):
-        return f"std::sinh({x})"
-
-    @staticmethod
-    def asin(x):
-        return f"std::asin({x})"
-
-    @staticmethod
-    def asinh(x):
-        return f"std::asinh({x})"
-
-    @staticmethod
-    def atan2(x, y):
-        return f"std::atan2({x}, {y})"
-
-    @staticmethod
-    def atan(x):
-        return f"std::atan({x})"
-
-    @staticmethod
-    def atanh(x):
-        return f"std::atanh({x})"
-
-    @staticmethod
-    def copysign(x, y):
-        return f"std::copysign({x}, {y})"
-
-    @staticmethod
-    def hypot(x, y):
-        return f"std::hypot({x}, {y})"
-
-    @staticmethod
-    def log10(x):
-        return f"std::log10({x})"
-
-    @staticmethod
-    def nextafter(x, y):
-        return f"std::nextafter({x}, {y})"
-
-    @staticmethod
-    def relu(x):
-        bug = config.cpp.inject_relu_bug_TESTING_ONLY
-        if bug == "compile_error":
-            return "compile error!"
-        elif bug == "runtime_error":
-            return f"{x}; throw 1"
-        elif bug == "accuracy":
-            return f"{x} + decltype({x})(1)"
-        elif bug is None:
-            return f"{x} * ({x}>0)"
-        else:
-            raise AssertionError(
-                f"unrecognized config cpp.inject_relu_bug_TESTING_ONLY = {bug!r}"
-            )
-
-    @staticmethod
-    def minimum(a, b):
-        return f"min_propagate_nan({a}, {b})"
-
-    @staticmethod
-    def maximum(a, b):
-        return f"max_propagate_nan({a}, {b})"
-
-    @staticmethod
-    def where(a, b, c):
-        return f"{a} ? {b} : {c}"
-
-    @staticmethod
-    def mod(a, b):
-        return f"mod({a}, {b})"
-
-    @staticmethod
-    def constant(val, dtype):
-        if dtype in DTYPE_LOWP_FP:
-            # Since load promotes all half-precision inputs to float, constants
-            # must be promoted as well
-            dtype = torch.float32
-        if val == float("inf"):
-            return f"std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::infinity()"
-        elif val == float("-inf"):
-            return f"-std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::infinity()"
-        elif math.isnan(val):
-            return f"std::numeric_limits<{DTYPE_TO_CPP[dtype]}>::quiet_NaN()"
-        elif val is True or val is False:
-            return ops.to_dtype(str(val).lower(), dtype)
-        return ops.to_dtype(repr(val), dtype)
-
-    @staticmethod
-    def index_expr(expr, dtype):
-        return ops.to_dtype(cexpr(V.kernel.rename_indexing(expr)), dtype)
-
-    @staticmethod
-    def masked(mask, body, other):
-        code = BracesBuffer()
-
-        # Write masked operation into a lambda
-        body_var = V.kernel.cse.newvar()
-        code.writeline(f"auto {body_var} = [&]")
-        with V.kernel.swap_buffers(code), code.indent():
-            result = body()
-            code.writeline(f"return {result};")
-        code.writeline(";")
-        V.kernel.compute.splice(code)
-
-        # Use the lambda's return type as the type of other
-        type = f"decltype({body_var}())"
-
-        if other == float("-inf"):
-            other_code = f"-std::numeric_limits<{type}>::infinity()"
-        elif other == float("inf"):
-            other_code = f"std::numeric_limits<{type}>::infinity()"
-        elif isinstance(other, bool):
-            other_code = f"static_cast<{type}>({str(other).lower()})"
-        elif math.isnan(other):
-            other_code = f"std::numeric_limits<{type}>::quiet_NaN()"
-        else:
-            other_code = f"static_cast<{type}>({repr(other)})"
-
-        return f"{mask} ? {body_var}() : {other_code}"
-
-    @staticmethod
-    def logical_and(a, b):
-        return f"{a} && {b}"
-
-    @staticmethod
-    def logical_not(a):
-        return f"!{a}"
-
-    @staticmethod
-    def logical_or(a, b):
-        return f"{a} || {b}"
-
-    @staticmethod
-    def logical_xor(a, b):
-        return f"{a} != {b}"
-
-    @staticmethod
-    def bitwise_and(a, b):
-        return f"decltype({a})({a} & {b})"
-
-    @staticmethod
-    def bitwise_not(a):
-        return f"decltype({a})(~{a})"
-
-    @staticmethod
-    def bitwise_or(a, b):
-        return f"decltype({a})({a} | {b})"
-
-    @staticmethod
-    def bitwise_xor(a, b):
-        return f"decltype({a})({a} ^ {b})"
-
-    @staticmethod
-    def bitwise_left_shift(a, b):
-        return f"decltype({a})({a} << {b})"
-
-    @staticmethod
-    def bitwise_right_shift(a, b):
-        return f"decltype({a})({a} >> {b})"
-
-    @staticmethod
-    def rand(seed: sympy.Expr, offset: sympy.Expr):
-        return f"normalized_rand_cpu({seed}, {offset})"
-
-    @staticmethod
-    def randn(seed: sympy.Expr, offset: sympy.Expr):
-        return f"randn_cpu({seed}, {offset})"
-
-    @staticmethod
-    def randint64(seed: sympy.Expr, offset: sympy.Expr, low, high):
-        return f"randint64_cpu({seed}, {offset}, {low}, {high})"
-
-    @staticmethod
-    def sigmoid(x):
-        return f"decltype({x})(1) / (decltype({x})(1) + std::exp(-{x}))"
-
-    @staticmethod
-    def sign(x):
-        code = BracesBuffer()
-        # auto tmp5 = tmp4 < 0 ? -1 : 1;
-        left = V.kernel.cse.newvar()
-        right = V.kernel.cse.newvar()
-        result = V.kernel.cse.newvar()
-        scalar_zero = f"decltype({x})(0)"
-        scalar_one = f"decltype({x})(1)"
-        code.writeline(f"auto {left} = {x} > 0 ? {scalar_one} : {scalar_zero};")
-        code.writeline(f"auto {right} = {x} < 0 ? {scalar_one} : {scalar_zero};")
-        code.writeline(f"auto {result} = {left} - {right};")
-        V.kernel.compute.splice(code)
-        return result
 
 
 class CppKernel(Kernel):
