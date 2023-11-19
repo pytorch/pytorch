37a2498a4eda5fde2b89ab7b7ba4744ad6874d2d
Jon Chuang <9093549+jon-chuang@users.noreply.github.com>
2023-11-19 01:52:03 -0500

    revert

--- torch/jit/_trace.py
+++ torch/jit/_trace.py
@@ -1040,7 +1040,7 @@ def trace_module(
 
         if check_trace and check_inputs is None:
             # Copy the inputs, in-case there is any in-place mutation
-            check_inputs = [torch._dynamo.utils.clone_inputs(inputs)]
+            check_inputs = [copy.deepcopy(inputs)]
 
         for method_name, example_inputs in inputs.items():
             if method_name == "forward":
