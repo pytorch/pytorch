b13c359ca611cf24ce57746d34a052a86271e217
Han, Xu <xu.han@intel.com>
2023-11-19 20:30:30 +0000

    test new builder.

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1036,26 +1036,27 @@ cdll.LoadLibrary("__lib_path__")
         lock_dir = get_lock_dir()
         lock = FileLock(os.path.join(lock_dir, key + ".lock"), timeout=LOCK_TIMEOUT)
         with lock:
-            output_path = input_path[:-3] + "so"
+            # output_path = input_path[:-3] + "so"
             output_dir = get_dir_name_from_path(input_path)
-            print("!!!! dbg: ", output_dir)
             cxx_target = BuildTarget()
             cxx_target.target(
                 name=key, sources=[input_path], output_directory=output_dir, warning_all=False, vec_isa=self
             )
             print("!!! new: ", cxx_target.get_build_cmd())
-
+            
+            '''
             build_cmd = shlex.split(
                 cpp_compile_command(
                     input_path, output_path, warning_all=False, vec_isa=self
                 )
-            )            
+            )                
+            '''        
             
             try:
                 # Check build result
-                compile_file(input_path, output_path, build_cmd)
-                # cxx_target.build()
-                # output_path = cxx_target.get_target_file_path()
+                # compile_file(input_path, output_path, build_cmd)
+                cxx_target.build()
+                output_path = cxx_target.get_target_file_path()
                 subprocess.check_call(
                     [
                         sys.executable,
