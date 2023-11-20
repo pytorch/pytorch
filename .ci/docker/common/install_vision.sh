ae3870b2da2995a883185a4d6eca270a65607a5e
albanD <desmaison.alban@gmail.com>
2023-11-20 17:37:56 -0500

    Don't install vision since we don't have it yet

--- .ci/docker/common/install_vision.sh
+++ .ci/docker/common/install_vision.sh
@@ -29,6 +29,10 @@ install_centos() {
   rm -rf /var/lib/yum/history
 }
 
+if [[ $ANACONDA_PYTHON_VERSION == "3.12" ]]; then
+  exit
+fi
+
 # Install base packages depending on the base OS
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 case "$ID" in
