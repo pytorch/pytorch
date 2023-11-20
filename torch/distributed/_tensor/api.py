e2095a04ae755ae8724a5f052b62a0c8ea707c13
Andrew Gu <andgu@fb.com>
2023-11-20 08:47:22 -0800

    [DTensor] Ensured `grad_placements` was tuple (#113925)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113925
    Approved by: https://github.com/wanchaol
    ghstack dependencies: #113919, #113924, #114134

--- torch/distributed/_tensor/api.py
+++ torch/distributed/_tensor/api.py
@@ -390,6 +390,8 @@ class DTensor(torch.Tensor):  # pyre-ignore[13]: pyre is bad at __new__
         .. note:: `to_local` is differentiable, the `requires_grad` of the local tensor returned
             will depend on if the `DTensor` requires_grad or not.
         """
+        if grad_placements is not None and not isinstance(grad_placements, tuple):
+            grad_placements = tuple(grad_placements)
         return _ToTorchTensor.apply(
             self, grad_placements, True
         )  # pyre-ignore[16]: autograd func
