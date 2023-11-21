64a5372e6ce9b6ca0ee5c7482b27e24561725b28
Chip Turner <cturner@pattern.net>
2023-11-21 21:03:48 +0000

    Opportunistically use `ncclCommSplit` when creating new NCCL groups (#112889)
    Currently `ncclCommInitRankConfig` is always used when creating new
    communicator groups.  This is wasteful as it creates non-shared pairs
    of endpoint queues as well as costs time to re-establish
    communication.
    
    This change is transparent and opportunistic; when `dist.new_group` is
    called, it will use the existing, healthy world process group to
    select the right ranks to include in the process group.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112889
    Approved by: https://github.com/kwen2501

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -2290,6 +2290,9 @@ options :class:`~torch.distributed.ProcessGroupNCCL.Options`).
               py::call_guard<py::gil_scoped_release>())
           .def("_group_start", &::c10d::ProcessGroupNCCL::groupStart)
           .def("_group_end", &::c10d::ProcessGroupNCCL::groupEnd)
+          .def(
+              "comm_split_count",
+              &::c10d::ProcessGroupNCCL::getCommSplitCounter)
           .def_property_readonly(
               "options", &::c10d::ProcessGroupNCCL::getOptions)
           .def_property_readonly(
@@ -2354,15 +2357,18 @@ Example::
       )")
       .def(py::init<bool>(), py::arg("is_high_priority_stream") = false)
 #ifdef NCCL_HAS_COMM_CTA_CGA
+      .def_readwrite("config", &::c10d::ProcessGroupNCCL::Options::config)
+#endif
       .def_readwrite(
           "is_high_priority_stream",
           &::c10d::ProcessGroupNCCL::Options::is_high_priority_stream)
-      .def_readwrite("config", &::c10d::ProcessGroupNCCL::Options::config);
-#else
+#ifdef NCCL_HAS_COMM_SPLIT
       .def_readwrite(
-          "is_high_priority_stream",
-          &::c10d::ProcessGroupNCCL::Options::is_high_priority_stream);
+          "split_from", &::c10d::ProcessGroupNCCL::Options::split_from)
+      .def_readwrite(
+          "split_color", &::c10d::ProcessGroupNCCL::Options::split_color)
 #endif
+      ;
 
 #endif
 
