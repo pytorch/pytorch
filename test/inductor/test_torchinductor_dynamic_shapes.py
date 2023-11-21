7ea184d7e33369610492ff0936369ea00f2b3580
Edward Z. Yang <ezyang@meta.com>
2023-11-20 23:31:19 -0500

    Handle item() on boolean tensor (#114157)
    This needs some special handling because we don't actually allocate
    boolean symbols in sympy; we allocate an integer indicator variable.
    See comment for more details.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/114157
    Approved by: https://github.com/ydwu4

--- test/inductor/test_torchinductor_dynamic_shapes.py
+++ test/inductor/test_torchinductor_dynamic_shapes.py
@@ -207,6 +207,14 @@ class TestInductorDynamic(TestCase):
         f(torch.tensor([3], device=device))
 
     @torch._dynamo.config.patch(capture_scalar_outputs=True)
+    def test_item_bool_nobreak(self, device):
+        @torch.compile(fullgraph=True)
+        def f(x):
+            return x.item()
+
+        f(torch.tensor([True], device=device))
+
+    @torch._dynamo.config.patch(capture_scalar_outputs=True)
     def test_item_zeros_nobreak(self, device):
         @torch.compile(fullgraph=True)
         def f(x):
