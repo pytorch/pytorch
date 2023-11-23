4b8ee111223413e19ed24eaa811aeb1dc4cf4040
Andrew Gu <andgu@fb.com>
2023-11-22 17:21:51 -0800

    Update on "[FSDP] Passed `TORCH_NCCL_DESYNC_DEBUG` instead of `NCCL_DESYNC_DEBUG`"
    This is to silence some warnings like:
    ```
    [rank0]:[W Utils.hpp:164] Warning: Environment variable NCCL_DESYNC_DEBUG is deprecated; use TORCH_NCCL_DESYNC_DEBUG instead (function getCvarBool)
    [rank3]:[W Utils.hpp:164] Warning: Environment variable NCCL_DESYNC_DEBUG is deprecated; use TORCH_NCCL_DESYNC_DEBUG instead (function getCvarBool)
    [rank1]:[W Utils.hpp:164] Warning: Environment variable NCCL_DESYNC_DEBUG is deprecated; use TORCH_NCCL_DESYNC_DEBUG instead (function getCvarBool)
    [rank2]:[W Utils.hpp:164] Warning: Environment variable NCCL_DESYNC_DEBUG is deprecated; use TORCH_NCCL_DESYNC_DEBUG instead (function getCvarBool)
    ```
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_fsdp.py
+++ torch/testing/_internal/common_fsdp.py
@@ -865,7 +865,7 @@ class FSDPTestMultiThread(MultiThreadedTestCase):
 class FSDPTest(MultiProcessTestCase):
     def setUp(self):
         super().setUp()
-        # Set NCCL_DESYNC_DEBUG=0 to disable the NCCL `workCleanupLoop()`,
+        # Set TORCH_NCCL_DESYNC_DEBUG=0 to disable the NCCL `workCleanupLoop()`,
         # which can cause unit test flakiness:
         # https://github.com/pytorch/pytorch/issues/90848
         os.environ["TORCH_NCCL_DESYNC_DEBUG"] = "0"
