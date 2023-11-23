9ee17280848b1ca1d0ddfdfb67d3f31a40c23c0f
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-11-23 07:07:56 +0000

    Revert "Add support for models with mutated buffer on torch.onnx.dynamo_export (#112272)"
    This reverts commit c4a22d6918b7ca218f2712d7e7e147aca7127fa3.
    
    Reverted https://github.com/pytorch/pytorch/pull/112272 on behalf of https://github.com/huydhn due to Sorry for reverting you change but it is failing dynamo test in trunk https://hud.pytorch.org/pytorch/pytorch/commit/c4a22d6918b7ca218f2712d7e7e147aca7127fa3 ([comment](https://github.com/pytorch/pytorch/pull/112272#issuecomment-1823897964))

--- test/onnx/onnx_test_common.py
+++ test/onnx/onnx_test_common.py
@@ -436,18 +436,16 @@ def _compare_pytorch_onnx_with_ort(
         ref_input_args = input_args
         ref_input_kwargs = input_kwargs
 
-    # ONNXProgram holds a reference (not copy) to the original ref_model, including its state_dict.
-    # Thus, ONNXProgram() must run before ref_model() to prevent ref_model.forward() from changing the state_dict.
-    # Otherwise, the ref_model can change buffers on state_dict which would be used by ONNXProgram.__call__()
+    ref_outputs = onnx_program.adapt_torch_outputs_to_onnx(
+        ref_model(*ref_input_args, **ref_input_kwargs)
+    )
+
     ort_outputs = onnx_program(*input_args, **input_kwargs)
-    ref_outputs = ref_model(*ref_input_args, **ref_input_kwargs)
-    ref_outputs = onnx_program.adapt_torch_outputs_to_onnx(ref_outputs)
 
     if len(ref_outputs) != len(ort_outputs):
         raise AssertionError(
             f"Expected {len(ref_outputs)} outputs, got {len(ort_outputs)}"
         )
-
     for ref_output, ort_output in zip(ref_outputs, ort_outputs):
         torch.testing.assert_close(
             ref_output, torch.tensor(ort_output), rtol=rtol, atol=atol
