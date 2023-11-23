aa17b4165f2577e2b77f75c76a71d5a2a7c6959c
bruceSz <zsyuyizhang@gmail.com>
2023-11-23 12:31:43 +0800

    rewrite findSchemaOrThrow in Dispatcher.cpp
    Signed-off-by: bruceSz <zsyuyizhang@gmail.com>

--- aten/src/ATen/core/dispatch/Dispatcher.cpp
+++ aten/src/ATen/core/dispatch/Dispatcher.cpp
@@ -135,19 +135,17 @@ c10::optional<OperatorHandle> Dispatcher::findSchema(const OperatorName& overloa
 }
 
 OperatorHandle Dispatcher::findSchemaOrThrow(const char* name, const char* overload_name) {
-  auto it = findSchema({name, overload_name});
-  if (!it.has_value()) {
-    // Check if we have ANYTHING; if that's the case, that means you're
-    // missing schema
-    auto it2 = findOp({name, overload_name});
-    if (!it2.has_value()) {
-      TORCH_CHECK(false, "Could not find schema for ", name, ".", overload_name);
+  auto it = findOp({name, overload_name});
+  if (it.has_value()) {
+    if (it->hasSchema()) {
+      return it.value();
     } else {
       TORCH_CHECK(false, "Could not find schema for ", name, ".", overload_name,
         " but we found an implementation; did you forget to def() the operator?");
     }
+  } else {
+    TORCH_CHECK(false, "Could not find schema for ", name, ".", overload_name);
   }
-  return it.value();
 }
 
 const std::vector<OperatorName> Dispatcher::getAllOpNames() {
