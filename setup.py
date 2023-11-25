2c02422159d8d9aab22acf239926176bff70e011
chuboning <chuboning@foxmail.com>
2023-11-25 10:39:04 +0800

    Add torch/csrc/profiler/unwind/*.h to package

--- setup.py
+++ setup.py
@@ -1286,6 +1286,7 @@ def main():
         "include/torch/csrc/profiler/*.h",
         "include/torch/csrc/profiler/orchestration/*.h",
         "include/torch/csrc/profiler/stubs/*.h",
+        "include/torch/csrc/profiler/unwind/*.h",
         "include/torch/csrc/utils/*.h",
         "include/torch/csrc/tensor/*.h",
         "include/torch/csrc/lazy/backend/*.h",
