a032a9109e61ae9d519c6759b615fdc70d60e227
leslie-fang-intel <leslie.fang@intel.com>
2023-11-26 08:55:55 +0800

    Fix a issue in QConv Binary fusion
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -80,7 +80,7 @@ def post_grad_passes(gm: torch.fx.GraphModule, is_inference: bool):
 
         group_batch_fusion_passes(gm.graph, pre_grad=False)
         remove_noop_ops(gm.graph)
-
+        
         for patterns in pass_patterns:
             patterns.apply(gm.graph)
         if is_inference:
