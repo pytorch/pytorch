b6a30bbfb6c1bcb9c785e7a853c2622c8bc17093
Yanbo Liang <ybliang8@gmail.com>
2023-11-29 09:31:08 +0000

    [Dynamo] Forward fix dynamo trace rule test failure due to landing race (#114739)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/114739
    Approved by: https://github.com/janeyx99, https://github.com/huydhn

--- test/dynamo/test_trace_rules.py
+++ test/dynamo/test_trace_rules.py
@@ -66,7 +66,9 @@ ignored_ctx_manager_class_names = {
     "torch.sparse.check_sparse_tensor_invariants",
 }
 
-ignored_c_binding_in_graph_function_names = set()
+ignored_c_binding_in_graph_function_names = {
+    "torch._functionalize_are_all_mutations_under_no_grad_or_inference_mode"
+}
 if torch._C._llvm_enabled():
     ignored_c_binding_in_graph_function_names |= {
         "torch._C._te.set_llvm_aot_workflow",
