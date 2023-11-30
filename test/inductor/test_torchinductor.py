1eb9cb33f53f64b914314c406548aee5852851db
William Wen <williamwen@meta.com>
2023-11-30 15:49:53 -0800

    [inductor, test] remove cast for test_tmp_not_defined_issue2_cpu
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -6538,7 +6538,8 @@ class CommonTemplate:
             return (new_zeros_default_4, sum_default_7)
 
         # TODO: Remove once https://github.com/pytorch/pytorch/issues/94017 is resolved
-        dtype = torch.float64 if self.device == "cpu" else torch.float32
+        # dtype = torch.float64 if self.device == "cpu" else torch.float32
+        dtype = torch.float32
         args = [
             ((1, 88, 40, 40), (140800, 1600, 40, 1), dtype),
             ((), (), dtype),
