b0c75f0c78031334e303838677b2039f72a9b093
Yang Chen <yangche@fb.com>
2023-11-29 19:49:22 -0800

    [inductor][easy] print out exception message upon failing to write to a file
    To address Oleg's internal review feedback.
    
    [ghstack-poisoned]

--- torch/_inductor/triton_heuristics.py
+++ torch/_inductor/triton_heuristics.py
@@ -629,7 +629,9 @@ def end_graph():
                     file.write(bw_info_str + "\n")
                 file.write(f"{summary_str}\n\n")
         except Exception as e:
-            log.warning("failed to write profile bandwidth result into %s", output_file)
+            log.warning(
+                "failed to write profile bandwidth result into %s: %s", output_file, f"{e}"
+            )
 
 
 class DebugAutotuner(CachingAutotuner):
