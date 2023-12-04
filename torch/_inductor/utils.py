pytorch/pytorch

commit 8a90249bc2079f5ae7398ff87fc7c362852836e3

[inductor] Update triton pin (#114772)
Differential Revision: [D51761353](https://our.internmc.facebook.com/intern/diff/D51761353)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/114772
Approved by: https://github.com/shunting314, https://github.com/atalman

torch/_inductor/utils.py
@@ -1110,16 +1110,17 @@ def blue_text(msg):
 
 @functools.lru_cache(None)
 def get_device_tflops(dtype):
-    from triton.testing import get_max_simd_tflops, get_max_tensorcore_tflops
+    from triton.testing import get_max_simd_tflops, get_max_tensorcore_tflops, nvsmi
 
     assert dtype in (torch.float16, torch.bfloat16, torch.float32)
+    cur_sm_clock = nvsmi(["clocks.current.sm"])[0]
     if dtype in (torch.float16, torch.bfloat16):
-        return get_max_tensorcore_tflops(dtype)
+        return get_max_tensorcore_tflops(dtype, cur_sm_clock)
 
     if torch.backends.cuda.matmul.allow_tf32:
-        return get_max_tensorcore_tflops(torch.float32)
+        return get_max_tensorcore_tflops(torch.float32, cur_sm_clock)
     else:
-        return get_max_simd_tflops(torch.float32)
+        return get_max_simd_tflops(torch.float32, cur_sm_clock)
 
 
 @functools.lru_cache(None)

.github/scripts/build_triton_wheel.py
@@ -29,19 +29,6 @@ def check_and_replace(inp: str, src: str, dst: str) -> str:
     return inp.replace(src, dst)
 
 
-def patch_setup_py(path: Path, *, version: str, name: str = "triton") -> None:
-    with open(path) as f:
-        orig = f.read()
-    # Replace name
-    orig = check_and_replace(orig, 'name="triton",', f'name="{name}",')
-    # Replace version
-    orig = check_and_replace(
-        orig, f'version="{read_triton_version()}",', f'version="{version}",'
-    )
-    with open(path, "w") as f:
-        f.write(orig)
-
-
 def patch_init_py(path: Path, *, version: str) -> None:
     with open(path) as f:
         orig = f.read()
@@ -67,10 +54,12 @@ def build_triton(
         max_jobs = os.cpu_count() or 1
         env["MAX_JOBS"] = str(max_jobs)
 
+    version_suffix = ""
     if not release:
         # Nightly binaries include the triton commit hash, i.e. 2.1.0+e6216047b8
         # while release build should only include the version, i.e. 2.1.0
-        version = f"{version}+{commit_hash[:10]}"
+        version_suffix = f"+{commit_hash[:10]}"
+        version += version_suffix
 
     with TemporaryDirectory() as tmpdir:
         triton_basedir = Path(tmpdir) / "triton"
@@ -132,11 +121,9 @@ def build_triton(
             shutil.copy(conda_path, Path.cwd())
             return Path.cwd() / conda_path.name
 
-        patch_setup_py(
-            triton_pythondir / "setup.py",
-            name=triton_pkg_name,
-            version=f"{version}",
-        )
+        # change built wheel name and version
+        env["TRITON_WHEEL_NAME"] = triton_pkg_name
+        env["TRITON_WHEEL_VERSION_SUFFIX"] = version_suffix
         patch_init_py(
             triton_pythondir / "triton" / "__init__.py",
             version=f"{version}",

benchmarks/dynamo/timm_models.py
@@ -69,6 +69,7 @@ BATCH_SIZE_DIVISORS = {
 
 REQUIRE_HIGHER_TOLERANCE = {
     "fbnetv3_b",
+    "gmixer_24_224",
     "hrnet_w18",
     "inception_v3",
     "sebotnet33ts_256",

torch/_inductor/codegen/wrapper.py
@@ -879,10 +879,8 @@ class WrapperCodeGen(CodeGen):
         signature: List[Union[TensorArg, SizeArg]] = []
         constants = {}
         for key, arg in kwargs.items():
-            if (
-                key in kernel.__annotations__
-                and "constexpr" in kernel.__annotations__[key]
-            ):
+            idx = kernel.arg_names.index(key)
+            if idx in kernel.constexprs:
                 constants[key] = arg
                 continue
             if isinstance(arg, (ir.Buffer, ir.ReinterpretView)):

commit 0a2ccc75076b95b2892448ceabc3736a6b023ded

Will it fix ROCm
Attempts to fix numerous regressions that started after
https://github.com/pytorch/pytorch/pull/114772 change trition APIs a bit

torch/_inductor/utils.py
@@ -1110,9 +1110,20 @@ def blue_text(msg):
 
 @functools.lru_cache(None)
 def get_device_tflops(dtype):
-    from triton.testing import get_max_simd_tflops, get_max_tensorcore_tflops, nvsmi
-
+    from triton.testing import get_max_simd_tflops, get_max_tensorcore_tflops
     assert dtype in (torch.float16, torch.bfloat16, torch.float32)
+    if torch.version.hip:
+
+        if dtype in (torch.float16, torch.bfloat16):
+            return get_max_tensorcore_tflops(dtype)
+
+        if torch.backends.cuda.matmul.allow_tf32:
+            return get_max_tensorcore_tflops(torch.float32)
+        else:
+            return get_max_simd_tflops(torch.float32)
+
+    from triton.testing import nvsmi
+
     cur_sm_clock = nvsmi(["clocks.current.sm"])[0]
     if dtype in (torch.float16, torch.bfloat16):
         return get_max_tensorcore_tflops(dtype, cur_sm_clock)