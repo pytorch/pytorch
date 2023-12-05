563e13a30218920c7dccb78676c383470c95e3f7
Nikita Shulga <nshulga@meta.com>
2023-12-05 08:59:36 -0800

    Revert "[DeviceMesh] Rename _device_mesh.py to device_mesh.py to prepare for beta (#115099)"
    This reverts commit eaa64339d640ed1d36520ada379213f8361be5ff.

--- torch/distributed/_tensor/ops/random_ops.py
+++ torch/distributed/_tensor/ops/random_ops.py
@@ -1,5 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 import torch
+
+from torch.distributed._tensor.device_mesh import DeviceMesh
 from torch.distributed._tensor.op_schema import (
     OpSchema,
     OpStrategy,
@@ -7,8 +9,6 @@ from torch.distributed._tensor.op_schema import (
     StrategyType,
 )
 from torch.distributed._tensor.ops.utils import is_tensor_partial, register_op_strategy
-
-from torch.distributed.device_mesh import DeviceMesh
 
 aten = torch.ops.aten
 
