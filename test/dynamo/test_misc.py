de0599dd342f66c218014d69f727d52a39125695
Jon Chuang <9093549+jon-chuang@users.noreply.github.com>
2023-12-05 23:32:55 -0500

    improve test

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -8063,14 +8063,13 @@ def ___make_guard_fn():
             it = iter(["my_weight", "not_my_weight"])
             next(it)
             if "my_weight" in it:
-                return x
+                return x + 2
             return x + 1
 
-        x = torch.arange(1, 3)
+        x = torch.zeros(3)
         compiled_fn = torch._dynamo.optimize(backend="eager", nopython=True)(fn)
 
         self.assertEqual(fn(x), compiled_fn(x))
-
 
     def test_shape_env_no_recording(self):
         main = ShapeEnv(should_record_events=False)
