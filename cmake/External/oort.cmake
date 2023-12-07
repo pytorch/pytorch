73450da687a0af20d265a879cd90c9035371b70f
Xinya Zhang <Xinya.Zhang@amd.com>
2023-12-07 02:20:49 +0000

    Revert "Handle lib/ vs lib64/ difference between Debian and RHEL families."
    This reverts commit b9ef0ac775c2b3f206fc1215d1d66f7d451acaae.
    
    Forcing lib/ may be a better idea

--- cmake/External/oort.cmake
+++ cmake/External/oort.cmake
@@ -3,7 +3,6 @@ if(NOT __OORT_INCLUDED)
 
   set(__OORT_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/oort/build")
   set(__OORT_INSTALL_DIR "${PROJECT_SOURCE_DIR}/torch")
-  include(GNUInstallDirs)
   ExternalProject_Add(oort_external
     SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party/triton-mathaot/mathaot
     BINARY_DIR ${__OORT_BUILD_DIR}
@@ -11,12 +10,12 @@ if(NOT __OORT_INCLUDED)
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${__OORT_INSTALL_DIR}
     # CONFIGURE_COMMAND ""
     # BUILD_COMMAND ${MAKE_COMMAND}
-    BUILD_BYPRODUCTS "${__OORT_INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/liboort.a"
+    BUILD_BYPRODUCTS "${__OORT_INSTALL_DIR}/lib/liboort.a"
     # INSTALL_COMMAND ${MAKE_COMMAND} install
     )
   set(OORT_FOUND TRUE)
   add_library(__caffe2_oort INTERFACE)
   add_dependencies(__caffe2_oort oort_external)
-  target_link_libraries(__caffe2_oort INTERFACE ${__OORT_INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/liboort.a)
+  target_link_libraries(__caffe2_oort INTERFACE ${__OORT_INSTALL_DIR}/lib/liboort.a)
   target_include_directories(__caffe2_oort INTERFACE ${__OORT_INSTALL_DIR}/include)
 endif() # __OORT_INCLUDED
