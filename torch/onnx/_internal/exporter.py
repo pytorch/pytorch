a39d31800629c3cd73f09036c38efc4113f635bc
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2023-12-07 16:42:56 +0000

    Update base for Update on "Enable builtin tests for ONNX Export with ExportedProgram models"
    Fixed by https://github.com/pytorch/pytorch/pull/113982
    
    [ghstack-poisoned]

--- torch/onnx/_internal/exporter.py
+++ torch/onnx/_internal/exporter.py
@@ -874,10 +874,10 @@ class ONNXProgram:
             ...     return x + y1 + y2 + y3
             >>> x_dict = {"a": torch.tensor(1.)}
             >>> y_tuple = (torch.tensor(2.), (torch.tensor(3.), torch.tensor(4.)))
-            >>> onnx_program = torch.onnx.dynamo_export(x_dict, y_tuple, model_with_state_dict=func_with_nested_input_structure)
+            >>> onnx_program = torch.onnx.dynamo_export(x_dict, y_tuple, model=func_with_nested_input_structure)
             >>> print(x_dict, y_tuple)
             {'a': tensor(1.)} (tensor(2.), (tensor(3.), tensor(4.)))
-            >>> print(onnx_program.adapt_torch_inputs_to_onnx(x_dict, y_tuple, model=func_with_nested_input_structure))
+            >>> print(onnx_program.adapt_torch_inputs_to_onnx(x_dict, y_tuple, model_with_state_dict=func_with_nested_input_structure))
             (tensor(1.), tensor(2.), tensor(3.), tensor(4.))
 
         .. warning::
