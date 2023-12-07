63cff10a093d48b28bbb13f492217afdfa31ae04
Isuru Fernando <ifernando@quansight.com>
2023-12-07 14:40:34 +0000

    Add decompositions for reflection_pad{1, 2, 3}d (#115100)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115100
    Approved by: https://github.com/peterbell10

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -5,7 +5,7 @@ import sys
 from enum import Enum
 from functools import partial, reduce
 from itertools import chain, product
-from typing import Callable, cast, Iterable, List, Optional, Tuple, Union
+from typing import Any, Callable, cast, Iterable, List, Optional, Tuple, Union
 
 import torch
 import torch._prims as prims
@@ -4112,6 +4112,39 @@ def upsample_bicubic2d_vec(
         )
     scale_h, scale_w = scale_factors if scale_factors else (None, None)
     return upsample_bicubic2d_default(a, output_size, align_corners, scale_h, scale_w)
+
+
+@register_decomposition(aten.reflection_pad1d)
+@register_decomposition(aten.reflection_pad2d)
+@register_decomposition(aten.reflection_pad3d)
+@pw_cast_for_opmath
+@out_wrapper()
+def _reflection_pad(a: Tensor, padding: Tuple[int, ...]) -> Tensor:
+    dim = len(padding) // 2
+    torch._check(
+        a.dim() in (dim + 1, dim + 2),
+        lambda: f"reflection_pad{dim}d requires {dim + 1}D or {dim + 2}D input",
+    )
+    inp_shape = a.shape[-dim:]
+    nc_dim = a.dim() - dim
+
+    padding_left = [padding[2 * (dim - 1 - i)] for i in range(dim)]
+    padding_right = [padding[2 * (dim - 1 - i) + 1] for i in range(dim)]
+
+    result = a
+    for i in range(dim):
+        dim_idx = torch.arange(
+            -padding_left[i], inp_shape[i] + padding_right[i], device=a.device
+        )
+        dim_idx = inp_shape[i] - 1 - (inp_shape[i] - 1 - dim_idx.abs()).abs()
+        idx: List[Any] = [None] * result.dim()
+        idx[i + nc_dim] = dim_idx
+        result = aten._unsafe_index(result, idx)
+
+    # convert output to correct memory format, if necessary
+    memory_format = utils.suggest_memory_format(result)
+    result = result.contiguous(memory_format=memory_format)
+    return result
 
 
 @register_decomposition(aten.aminmax)
