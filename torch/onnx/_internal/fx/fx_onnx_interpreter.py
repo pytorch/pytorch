b6b5260b6057f49a06595a4bf6c7e55676ea54ed
BowenBao <bowbao@microsoft.com>
2023-12-08 10:11:01 -0800

    Update on "[ONNX][dynamo_export] Extend expected fx output types for int, float, bool"
    [ghstack-poisoned]

--- torch/onnx/_internal/fx/fx_onnx_interpreter.py
+++ torch/onnx/_internal/fx/fx_onnx_interpreter.py
@@ -249,8 +249,9 @@ def _fill_tensor_shape_type(
             )
             onnxscript_value.shape = torch.Size([1])
         elif isinstance(expected_value, (int, float, bool)):
-            # These are onnx constants, skip setting dtype/shape.
-            onnxscript_value.dtype = fx_type_utils.from_scalar_type_to_torch_dtype(type(expected_value))
+            onnxscript_value.dtype = fx_type_utils.from_scalar_type_to_torch_dtype(
+                type(expected_value)
+            )
             onnxscript_value.shape = torch.Size([])
         elif fx_type_utils.is_torch_complex_dtype(expected_value.dtype):
             # Like torch.view_as_real, we flatten complex tensors to real tensors with
