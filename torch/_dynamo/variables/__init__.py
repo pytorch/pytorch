f0a130a7008b7645d017053bb0997d27a6c9876b
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-12-08 20:15:39 +0000

    Revert "[Dynamo][6.1/N] Refactor out TorchInGraphFunctionVariable and improve heuristic (#113432)"
    This reverts commit e61d6b42f0f4e4fa5bb816e03fb81e5bbcc9fa06.
    
    Reverted https://github.com/pytorch/pytorch/pull/113432 on behalf of https://github.com/huydhn due to Sorry for reverting your change, but it is failing dynamo tests in trunk https://hud.pytorch.org/pytorch/pytorch/commit/e61d6b42f0f4e4fa5bb816e03fb81e5bbcc9fa06, landrace? ([comment](https://github.com/pytorch/pytorch/pull/113432#issuecomment-1847787981))

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -66,11 +66,7 @@ from .tensor import (
     TensorVariable,
     UnspecializedPythonVariable,
 )
-from .torch import (
-    TorchCtxManagerClassVariable,
-    TorchInGraphFunctionVariable,
-    TorchVariable,
-)
+from .torch import TorchCtxManagerClassVariable, TorchVariable
 from .user_defined import UserDefinedClassVariable, UserDefinedObjectVariable
 
 __all__ = [
@@ -116,7 +112,6 @@ __all__ = [
     "SuperVariable",
     "TensorVariable",
     "TorchCtxManagerClassVariable",
-    "TorchInGraphFunctionVariable",
     "TorchVariable",
     "TupleVariable",
     "UnknownVariable",
