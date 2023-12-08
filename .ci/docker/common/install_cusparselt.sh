5cfe38bb6bd327a24a2174966147efe0729cbb42
Jesse Cai <jcjessecai@gmail.com>
2023-12-07 18:28:57 -0800

    Update on "[sparse][ci] Add cuSPASRELt v0.4.0 to CI"
    Summary:
    
    This PR adds in cuSPARSELt into CI to run our cuSPARSELt specific tests.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- .ci/docker/common/install_cusparselt.sh
+++ .ci/docker/common/install_cusparselt.sh
@@ -5,10 +5,10 @@ if [[ ${CUSPARSELT_VERSION} == "0.4.0" ]]; then
     mkdir tmp_cusparselt && cd tmp_cusparselt
     # CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.5.0.1-archive.tar.xz"
     if [[ ${CUDA_VERSION:0:4} == "12.1" ]]; then
-        CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive.tar.xz"
+        CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive"
         curl --retry 3 -OLs https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-x86_64/${CUSPARSELT_NAME}.tar.xz
     elif [[ ${CUDA_VERSION:0:4} == "11.8" ]]; then
-        CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive.tar.xz"
+        CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive"
         curl --retry 3 -OLs https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-x86_64/${CUSPARSELT_NAME}.tar.xz
     fi
 
