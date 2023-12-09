10b0e397d5a33de00a40d0d55d41cbc9274c1909
fduwjj <fduwjj@gmail.com>
2023-12-08 19:46:44 -0800

    [c10d] Fix Store check condition in NCCL PG watchdog
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1359,11 +1359,16 @@ void ProcessGroupNCCL::workCleanupLoop() {
     // poll store to see if some ranks have flagged a timeout when
     // we haven't polled for `heartbeat_timeout` seconds and there haven't
     // any work added or removed for `watchdog_timeout` seconds.
-    if (dumpOnTimeout_) {
+    if (!terminateProcessGroup_.load() && dumpOnTimeout_) {
       auto currentTime = std::chrono::steady_clock::now();
       auto timeSinceLastWorkListUpdate =
-          (currentTime - lastWorkListUpdateTime_).count();
-      auto timeSinceLastPollStore = (currentTime - lastTimePollStore).count();
+          std::chrono::duration_cast<std::chrono::milliseconds>(
+              (currentTime - lastWorkListUpdateTime_))
+              .count();
+      auto timeSinceLastPollStore =
+          std::chrono::duration_cast<std::chrono::milliseconds>(
+              (currentTime - lastTimePollStore))
+              .count();
       if (timeSinceLastWorkListUpdate >= kWatchdogThreadSleepMillis &&
           timeSinceLastPollStore >= heartbeatTimeoutInSec_ * 1000) {
         lastTimePollStore = currentTime;
