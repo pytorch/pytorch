d7ec04f98e3b56f7aed84a6c457b27adf6ea1e3e
Yanbo Liang <ybliang8@gmail.com>
2023-12-08 17:28:54 -0800

    Resolve landing race

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -2281,11 +2281,14 @@ def get_static_address_type(t):
 
 def is_rng_state_getter_or_setter(value):
     getters = (
+        # The following two functions are not identical, so don't remove anyone!
+        torch._C.Generator.get_state,
         torch.default_generator.get_state,
         torch.get_rng_state,
         torch.cuda.get_rng_state,
     )
     setters = (
+        torch._C.Generator.set_state,
         torch.default_generator.set_state,
         torch.set_rng_state,
         torch.cuda.set_rng_state,
