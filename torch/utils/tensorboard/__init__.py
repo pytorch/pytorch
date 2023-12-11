bf392128f1e693f56012a2e079baad5d9ca9978c
Sacha <sachahu@hotmail.fr>
2023-12-11 23:22:14 +0000

    fix unnecessary import in torch.tensorboard. __init__.py

--- torch/utils/tensorboard/__init__.py
+++ torch/utils/tensorboard/__init__.py
@@ -1,12 +1,9 @@
 import tensorboard
-from packaging.version import Version
 
-if not hasattr(tensorboard, "__version__") or Version(
-    tensorboard.__version__
-) < Version("1.15"):
+
+if not hasattr(tensorboard, "__version__") or (tensorboard.__version__ < "1.15"):
     raise ImportError("TensorBoard logging requires TensorBoard version 1.15 or above")
 
-del Version
 del tensorboard
 
 from .writer import FileWriter, SummaryWriter  # noqa: F401
