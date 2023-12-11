95a24b9327ef6278448a4ed4de03eb5d7daffceb
Nikita Shulga <nikita.shulga@gmail.com>
2023-12-10 16:52:38 -0800

    [MPS] Enable torch.rand for complex types
    Test plan:
    ```
    % python -c "import torch;print(torch.rand(3, 3, dtype=torch.chalf, device='mps'))"
    tensor([[0.8599+0.j, 0.4790+0.j, 0.4880+0.j],
            [0.7979+0.j, 0.3518+0.j, 0.1967+0.j],
            [0.2603+0.j, 0.7568+0.j, 0.1967+0.j]], device='mps:0',
           dtype=torch.complex32)
    
    ```
    
    [ghstack-poisoned]

--- aten/src/ATen/native/mps/operations/Distributions.mm
+++ aten/src/ATen/native/mps/operations/Distributions.mm
@@ -216,21 +216,27 @@ static Tensor& bernoulli_mps_impl(Tensor& self,
 } // namespace mps
 
 Tensor& uniform_mps_(Tensor& self, double from, double to, c10::optional<Generator> gen) {
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(self.scalar_type(), "check_uniform_bounds", [&] {
-    const auto min = static_cast<double>(std::numeric_limits<scalar_t>::lowest());
-    const auto max = static_cast<double>(std::numeric_limits<scalar_t>::max());
-    TORCH_CHECK(from <= to, "uniform_ expects to return a [from, to) range, but found from=", from, " > to=", to);
-    TORCH_CHECK((to - from) <= std::numeric_limits<scalar_t>::max(),
-                "uniform_ expects to-from <= std::numeric_limits<",
-                toString(self.scalar_type()),
-                ">::max(), but found to=",
-                to,
-                " and from=",
-                from,
-                " which result in to-from to exceed the limit");
-    from = std::min(std::max(from, min), max);
-    to = std::max(std::min(to, max), min);
-  });
+  AT_DISPATCH_FLOATING_TYPES_AND3(
+      at::ScalarType::Half,
+      at::ScalarType::ComplexHalf,
+      at::ScalarType::ComplexFloat,
+      self.scalar_type(),
+      "check_uniform_bounds",
+      [&] {
+        const auto min = static_cast<double>(std::numeric_limits<scalar_t>::lowest());
+        const auto max = static_cast<double>(std::numeric_limits<scalar_t>::max());
+        TORCH_CHECK(from <= to, "uniform_ expects to return a [from, to) range, but found from=", from, " > to=", to);
+        TORCH_CHECK((to - from) <= std::numeric_limits<scalar_t>::max(),
+                    "uniform_ expects to-from <= std::numeric_limits<",
+                    toString(self.scalar_type()),
+                    ">::max(), but found to=",
+                    to,
+                    " and from=",
+                    from,
+                    " which result in to-from to exceed the limit");
+        from = std::min(std::max(from, min), max);
+        to = std::max(std::min(to, max), min);
+      });
 
   return mps::random_mps_impl<double>(
       self, from, to, c10::nullopt, c10::nullopt, MPSGraphRandomDistributionUniform, gen, __func__, nullptr);
