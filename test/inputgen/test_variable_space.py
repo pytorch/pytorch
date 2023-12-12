83f364f8d2893b3c98f448cdc6a000f24c40fdf7
Manuel Candales <manuelcandales@gmail.com>
2023-12-11 17:05:49 -0800

    [InputGen] introduce variable space
    This diff introduces the VariableSpace class.
    
    A VariableSpace needs to be initialized with a variable type. Its purpose is to describe a space of values for that variable type. This space can be discrete (like a Python set) or it can be a union of disjoint intervals of real numbers (only for suitable variable types).
    
    Differential Revision: [D51816908](https://our.internmc.facebook.com/intern/diff/D51816908/)
    
    [ghstack-poisoned]

---
+++ test/inputgen/test_variable_space.py
@@ -0,0 +1,216 @@
+from torch.testing._internal.common_utils import TestCase, run_tests
+from torch.testing._internal.inputgen.variable.space import (
+    Discrete,
+    Interval,
+    Intervals,
+)
+
+
+class TestDiscrete(TestCase):
+    def test_init(self):
+        values = Discrete()
+        self.assertEqual(values.values, set())
+        self.assertEqual(values.initialized, False)
+        values = Discrete([1, 2, 3])
+        self.assertEqual(values.values, {1, 2, 3})
+        self.assertEqual(values.initialized, True)
+
+    def test_empty(self):
+        with self.assertRaises(Exception):
+            Discrete().empty()
+        self.assertTrue(Discrete([]).empty())
+        self.assertFalse(Discrete([1]).empty())
+
+    def test_contains(self):
+        with self.assertRaises(Exception):
+            Discrete().contains(1)
+        values = Discrete([1, "a", False, 0.5])
+        self.assertTrue(values.contains(True))
+        self.assertTrue(values.contains(1))
+        self.assertTrue(values.contains(1.0))
+        self.assertTrue(values.contains(False))
+        self.assertTrue(values.contains(0))
+        self.assertTrue(values.contains(0.0))
+        self.assertTrue(values.contains("a"))
+        self.assertTrue(values.contains(0.5))
+        self.assertFalse(values.contains(2))
+        self.assertFalse(values.contains("b"))
+        self.assertFalse(values.contains(2.5))
+        self.assertFalse(values.contains(float("inf")))
+        self.assertFalse(values.contains(float("-inf")))
+
+        values = Discrete([1, float("inf")])
+        self.assertTrue(values.contains(1))
+        self.assertTrue(values.contains(float("inf")))
+        self.assertFalse(values.contains(float("-inf")))
+
+    def test_remove(self):
+        with self.assertRaises(Exception):
+            Discrete().remove(1)
+        values = Discrete([1, "a", False, 0.5])
+        self.assertTrue(values.contains("a"))
+        values.remove("a")
+        self.assertEqual(values.values, {1, False, 0.5})
+
+    def test_filter(self):
+        with self.assertRaises(Exception):
+            Discrete().filter(lambda v: True)
+        values = Discrete([1, "a", False, 0.5])
+        values.filter(lambda v: isinstance(v, int))
+        self.assertEqual(values.values, {1, False})
+
+    def test_nan_error(self):
+        with self.assertRaises(Exception):
+            values = Discrete([1, 2, 3, float("nan")])
+
+
+class TestInterval(TestCase):
+    def test_empty(self):
+        self.assertFalse(Interval().empty())
+        self.assertFalse(Interval(-3, 4).empty())
+        self.assertFalse(Interval(5.7, 5.7).empty())
+        self.assertTrue(Interval(5.7, 5.7, True, False).empty())
+        self.assertTrue(Interval(5.7, 5.7, False, True).empty())
+        self.assertTrue(Interval(5.8, 5.7).empty())
+
+    def test_contains(self):
+        self.assertTrue(Interval(-3, 4).contains(1))
+        self.assertTrue(Interval(-3, 4).contains(-3))
+        self.assertTrue(Interval(-3, 4).contains(4))
+        self.assertFalse(Interval(-3, 4).contains(5))
+        self.assertFalse(Interval(-3, 4).contains(-4))
+        self.assertFalse(Interval(-3, 4).contains(float("inf")))
+        self.assertFalse(Interval(-3, 4, True, False).contains(-3))
+        self.assertTrue(Interval(-3, 4, True, False).contains(-2.999))
+        self.assertTrue(Interval(-3, 4, True, False).contains(4))
+        self.assertFalse(Interval(-3, 4, False, True).contains(4))
+        self.assertTrue(Interval(-3, 4, False, True).contains(-3))
+        self.assertTrue(Interval(-3, 4, False, True).contains(3.999))
+
+    def test_contains_int(self):
+        self.assertTrue(Interval().contains_int())
+        self.assertTrue(Interval(5.3, float("inf")).contains_int())
+        self.assertTrue(Interval(float("-inf"), 5.3).contains_int())
+        self.assertTrue(Interval(3, 4).contains_int())
+        self.assertTrue(Interval(3, 4, True, False).contains_int())
+        self.assertTrue(Interval(3, 4, False, True).contains_int())
+        self.assertFalse(Interval(3, 4, True, True).contains_int())
+        self.assertFalse(Interval(3.001, 3.999).contains_int())
+        self.assertTrue(Interval(3.999, 4.001).contains_int())
+
+
+class TestIntervals(TestCase):
+    def test_empty(self):
+        self.assertTrue(Intervals([]).empty())
+        self.assertTrue(Intervals([Interval(3, 3, True, True)]).empty())
+        self.assertTrue(Intervals([Interval(3, 3, True, False)]).empty())
+        self.assertTrue(
+            Intervals(
+                [Interval(3, 3, True, False), Interval(4, 4, False, True)]
+            ).empty()
+        )
+        self.assertFalse(
+            Intervals(
+                [Interval(3, 3, True, False), Interval(4, 4.001, False, True)]
+            ).empty()
+        )
+
+    def test_contains(self):
+        self.assertFalse(Intervals([]).contains(1))
+        intervals = Intervals(
+            [Interval(-2, 3, True, False), Interval(4.3, 5.7, False, True)]
+        )
+        self.assertFalse(intervals.contains(-2.1))
+        self.assertFalse(intervals.contains(-2))
+        self.assertTrue(intervals.contains(False))
+        self.assertTrue(intervals.contains(True))
+        self.assertTrue(intervals.contains(3))
+        self.assertFalse(intervals.contains(3.5))
+        self.assertTrue(intervals.contains(4.3))
+        self.assertTrue(intervals.contains(5))
+        self.assertFalse(intervals.contains(5.7))
+        self.assertFalse(intervals.contains(6))
+
+    def test_remove(self):
+        # start with full range interval
+        intervals = Intervals()
+        self.assertEqual(str(intervals), "[-inf, inf]")
+
+        # remove 1
+        self.assertTrue(intervals.contains(1))
+        intervals.remove(1)
+        self.assertEqual(str(intervals), "[-inf, 1) (1, inf]")
+        self.assertFalse(intervals.contains(1))
+
+        # remove inf
+        self.assertTrue(intervals.contains(float("inf")))
+        intervals.remove(float("inf"))
+        self.assertEqual(str(intervals), "[-inf, 1) (1, inf)")
+        self.assertFalse(intervals.contains(float("inf")))
+
+        # remove 2
+        self.assertTrue(intervals.contains(2))
+        intervals.remove(2)
+        self.assertEqual(str(intervals), "[-inf, 1) (1, 2) (2, inf)")
+        self.assertFalse(intervals.contains(2))
+
+        # start with a separate collection of intervals
+        intervals = Intervals([Interval(3, 4), Interval(5, 6)])
+        self.assertEqual(str(intervals), "[3, 4] [5, 6]")
+        self.assertTrue(intervals.contains(4))
+        intervals.remove(4)
+        self.assertEqual(str(intervals), "[3, 4) [5, 6]")
+        self.assertFalse(intervals.contains(4))
+
+    def test_lower_and_upper(self):
+        # start with full range interval
+        intervals = Intervals()
+        self.assertEqual(str(intervals), "[-inf, inf]")
+
+        intervals.set_lower(3, True)
+        self.assertEqual(str(intervals), "(3, inf]")
+
+        intervals.set_upper(float("inf"), True)
+        self.assertEqual(str(intervals), "(3, inf)")
+
+        intervals.set_upper(1e300, False)
+        self.assertEqual(str(intervals), "(3, 1e+300]")
+
+        intervals.set_lower(2, False)
+        self.assertEqual(str(intervals), "(3, 1e+300]")
+
+        intervals.set_lower(3, True)
+        self.assertEqual(str(intervals), "(3, 1e+300]")
+
+        # start with a separate collection of intervals
+        intervals = Intervals([Interval(3, 4), Interval(5, 6)])
+        self.assertEqual(str(intervals), "[3, 4] [5, 6]")
+
+        intervals.set_lower(3, True)
+        self.assertEqual(str(intervals), "(3, 4] [5, 6]")
+
+        intervals.set_upper(5, False)
+        self.assertEqual(str(intervals), "(3, 4] [5, 5]")
+
+        intervals.set_upper(5, True)
+        self.assertEqual(str(intervals), "(3, 4]")
+
+        # start with a separate collection of intervals
+        intervals = Intervals([Interval(3, 4), Interval(5, 6, True, False)])
+        self.assertEqual(str(intervals), "[3, 4] (5, 6]")
+
+        intervals.set_upper(5, False)
+        self.assertEqual(str(intervals), "[3, 4]")
+
+        # start with a separate collection of intervals
+        intervals = Intervals(
+            [Interval(3, 4, False, True), Interval(5, 6, True, False)]
+        )
+        self.assertEqual(str(intervals), "[3, 4) (5, 6]")
+
+        intervals.set_lower(4, False)
+        self.assertEqual(str(intervals), "(5, 6]")
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ torch/testing/_internal/inputgen/variable/space.py
@@ -0,0 +1,233 @@
+import math
+from typing import Any, List, Optional, Union
+
+from torch.testing._internal.inputgen.variable.constants import INT64_MAX, INT64_MIN
+from torch.testing._internal.inputgen.variable.type import (
+    SUPPORTED_TENSOR_DTYPES,
+    ScalarDtype,
+    VariableType,
+    check_vtype,
+    is_integer,
+)
+
+
+class Discrete:
+    def __init__(self, values: Optional[List[Any]] = None):
+        if values is None:
+            self.initialized = False
+            self.values = set()
+        else:
+            if any(isinstance(v, float) and math.isnan(v) for v in values):
+                raise Exception("NaN values are not supported")
+            self.initialized = True
+            self.values = set(values)
+
+    def __str__(self) -> str:
+        if len(self.values) == 0:
+            return "{}"
+        return str(self.values)
+
+    def empty(self) -> bool:
+        if not self.initialized:
+            raise Exception("Discrete must be initialized before checking if empty")
+        return len(self.values) == 0
+
+    def contains(self, v) -> bool:
+        if not self.initialized:
+            raise Exception("Discrete must be initialized before checking membership")
+        return v in self.values
+
+    def remove(self, v) -> None:
+        if not self.initialized:
+            raise Exception("Discrete must be initialized before removing")
+        self.values.difference_update({v})
+
+    def filter(self, f) -> None:
+        if not self.initialized:
+            raise Exception("Discrete must be initialized before filtering")
+        new_values = set()
+        for v in list(self.values):
+            if f(v):
+                new_values.add(v)
+        self.values = new_values
+
+
+class Interval:
+    def __init__(
+        self,
+        lower: Union[int, float] = float("-inf"),
+        upper: Union[int, float] = float("inf"),
+        lower_open: bool = False,
+        upper_open: bool = False,
+    ):
+        self.lower = lower
+        self.upper = upper
+        self.lower_open = lower_open
+        self.upper_open = upper_open
+
+    def __str__(self) -> str:
+        lower_braket = "(" if self.lower_open else "["
+        upper_braket = ")" if self.upper_open else "]"
+        return f"{lower_braket}{self.lower}, {self.upper}{upper_braket}"
+
+    def empty(self) -> bool:
+        if self.lower < self.upper:
+            return False
+        elif self.lower == self.upper:
+            return self.lower_open or self.upper_open
+        else:
+            return True
+
+    def contains(self, v) -> bool:
+        if self.empty():
+            return False
+        if v < self.lower:
+            return False
+        if v == self.lower and self.lower_open:
+            return False
+        if v == self.upper and self.upper_open:
+            return False
+        if v > self.upper:
+            return False
+        return True
+
+    def contains_int(self) -> bool:
+        if self.empty():
+            return False
+        if self.lower > INT64_MAX or (self.lower == INT64_MAX and self.lower_open):
+            return False
+        if self.upper < INT64_MIN or (self.upper == INT64_MIN and self.upper_open):
+            return False
+        if self.upper - self.lower > 1:
+            return True
+        return self.contains(math.ceil(self.lower)) or self.contains(
+            math.floor(self.upper)
+        )
+
+
+class Intervals:
+    def __init__(self, intervals: Optional[List[Interval]] = None):
+        self.intervals = intervals
+        if self.intervals is None:
+            self.intervals = [Interval()]
+
+    def __str__(self) -> str:
+        if len(self.intervals) == 0:
+            return "{}"
+        return " ".join([str(r) for r in self.intervals])
+
+    def empty(self) -> bool:
+        return all(r.empty() for r in self.intervals)
+
+    def contains(self, v) -> bool:
+        return any(r.contains(v) for r in self.intervals)
+
+    def contains_int(self) -> bool:
+        return any(r.contains_int() for r in self.intervals)
+
+    def remove(self, v) -> None:
+        for ix, r in enumerate(self.intervals):
+            if r.lower <= v <= r.upper:
+                if r.lower == v and r.upper == v:
+                    self.intervals = self.intervals[:ix] + self.intervals[ix + 1 :]
+                elif r.lower == v:
+                    r.lower_open = True
+                elif r.upper == v:
+                    r.upper_open = True
+                else:
+                    new_intervals = [
+                        Interval(r.lower, v, r.lower_open, True),
+                        Interval(v, r.upper, True, r.upper_open),
+                    ]
+                    self.intervals = (
+                        self.intervals[:ix] + new_intervals + self.intervals[ix + 1 :]
+                    )
+                return
+
+    def set_lower(self, lower: float, lower_open: bool = False) -> None:
+        for ix, r in enumerate(self.intervals):
+            if r.upper < lower or r.upper == lower and (r.upper_open or lower_open):
+                continue
+            if r.lower < lower:
+                r.lower = lower
+                r.lower_open = lower_open
+            elif r.lower == lower and lower_open:
+                r.lower_open = True
+            self.intervals = self.intervals[ix:]
+            return
+        self.intervals = []
+
+    def set_upper(self, upper: float, upper_open: bool = False) -> None:
+        for ix, r in enumerate(self.intervals):
+            if r.upper < upper:
+                continue
+            if r.lower > upper or (r.lower == upper and (r.lower_open or upper_open)):
+                self.intervals = self.intervals[:ix]
+                return
+            if r.upper == upper and (r.upper_open or upper_open):
+                r.upper_open = True
+            else:
+                r.upper = upper
+                r.upper_open = upper_open
+            self.intervals = self.intervals[: ix + 1]
+            return
+
+
+class VariableSpace:
+    def __init__(self, vtype):
+        if not VariableType.contains(vtype):
+            raise ValueError(f"Unsupported variable type {vtype}")
+        self.vtype = vtype
+
+        self.discrete = Discrete()
+        self.intervals = Intervals()
+
+        if vtype == VariableType.Bool.value:
+            self.discrete = Discrete([False, True])
+        if vtype == VariableType.Int.value:
+            self.intervals = Intervals(
+                [Interval(float("-inf"), float("inf"), True, True)]
+            )
+        if vtype == VariableType.TensorDtype.value:
+            self.discrete = Discrete(SUPPORTED_TENSOR_DTYPES)
+        if vtype == VariableType.ScalarDtype.value:
+            self.discrete = Discrete(list(ScalarDtype))
+
+    def __str__(self) -> str:
+        if self.discrete.initialized:
+            return str(self.discrete)
+        else:
+            return str(self.intervals)
+
+    def empty(self) -> bool:
+        if self.discrete.initialized:
+            return self.discrete.empty()
+        elif self.vtype == int:
+            return not self.intervals.contains_int()
+        elif self.vtype == float:
+            return self.intervals.empty()
+        return False
+
+    def contains(self, v) -> bool:
+        check_vtype(self.vtype, v)
+        if self.discrete.initialized:
+            return self.discrete.contains(v)
+        elif self.vtype == int:
+            if is_integer(v):
+                return self.intervals.contains(v)
+            return False
+        elif self.vtype == float:
+            return self.intervals.contains(v)
+        return True
+
+    def remove(self, v) -> None:
+        check_vtype(self.vtype, v)
+        if self.discrete.initialized:
+            self.discrete.remove(v)
+        elif self.vtype == int:
+            if is_integer(v):
+                self.intervals.remove(int(v))
+        elif self.vtype == float:
+            self.intervals.remove(float(v))
+        else:
+            self.discrete = Discrete([])
