df6d528e6c1b6e4aecc4e18377bde288e3bd03d9
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-12-13 01:41:45 +0800

    Update docstrings for `tree_map`

--- torch/utils/_pytree.py
+++ torch/utils/_pytree.py
@@ -684,12 +684,57 @@ def tree_structure(tree: PyTree) -> TreeSpec:
 
 
 def tree_map(func: Callable[..., Any], tree: PyTree, *rests: PyTree) -> PyTree:
+    """Map a multi-input function over pytree args to produce a new pytree.
+
+    See also :func:`tree_map_`.
+
+    >>> tree_map(lambda x: x + 1, {'x': 7, 'y': (42, 64)})
+    {'x': 8, 'y': (43, 65)}
+    >>> tree_map(lambda x: x is None, {'x': 7, 'y': (42, 64), 'z': None})
+    {'x': False, 'y': (False, False), 'z': True}
+
+    If multiple inputs are given, the structure of the tree is taken from the first input;
+    subsequent inputs need only have ``tree`` as a prefix:
+
+    >>> tree_map(lambda x, y: [x] + y, [5, 6], [[7, 9], [1, 2]])
+    [[5, 7, 9], [6, 1, 2]]
+
+    Args:
+        func (callable): A function that takes ``1 + len(rests)`` arguments, to be applied at the
+            corresponding leaves of the pytrees.
+        tree (pytree): A pytree to be mapped over, with each leaf providing the first positional
+            argument to function ``func``.
+        rests (tuple of pytree): A tuple of pytrees, each of which has the same structure as
+            ``tree`` or has ``tree`` as a prefix.
+
+    Returns:
+        A new pytree with the same structure as ``tree`` but with the value at each leaf given by
+        ``func(x, *xs)`` where ``x`` is the value at the corresponding leaf in ``tree`` and ``xs``
+        is the tuple of values at corresponding nodes in ``rests``.
+    """
     leaves, treespec = tree_flatten(tree)
     flat_args = [leaves] + [treespec.flatten_up_to(r) for r in rests]
     return tree_unflatten(map(func, *flat_args), treespec)
 
 
 def tree_map_(func: Callable[..., Any], tree: PyTree, *rests: PyTree) -> PyTree:
+    """Like :func:`tree_map`, but do an inplace call on each leaf and return the original tree.
+
+    See also :func:`tree_map`.
+
+    Args:
+        func (callable): A function that takes ``1 + len(rests)`` arguments, to be applied at the
+            corresponding leaves of the pytrees.
+        tree (pytree): A pytree to be mapped over, with each leaf providing the first positional
+            argument to function ``func``.
+        rests (tuple of pytree): A tuple of pytrees, each of which has the same structure as
+            ``tree`` or has ``tree`` as a prefix.
+
+    Returns:
+        The original ``tree`` with the value at each leaf is given by the side-effect of function
+        ``func(x, *xs)`` (not the return value) where ``x`` is the value at the corresponding leaf
+        in ``tree`` and ``xs`` is the tuple of values at values at corresponding nodes in ``rests``.
+    """
     leaves, treespec = tree_flatten(tree)
     flat_args = [leaves] + [treespec.flatten_up_to(r) for r in rests]
     deque(map(func, *flat_args), maxlen=0)  # consume and exhaust the iterable
