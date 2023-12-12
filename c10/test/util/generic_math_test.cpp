pytorch/pytorch

commit 51ff7b327c67ac6722c16718808c4a2eb423dfc2

[inductor] Enable floor_div indexing to work under ABI-compat mode (#113276)
Previously, floor_div operations were defined in
ATen/native/BinaryOps.h. Since this header was not included under
ABI-compat mode, trying to use those indexing operations would result in
compilation errors.

Technically, it is safe to use aten::native::floor_div_* functions in
ABI-compat mode as they are header-only; we could simply include
BinaryOps.h. However, there are other declarations in BinaryOps.h that
are not binary-compatible, so this is not ideal. Thus, I have moved those
functions into a separate file, and put them under c10/util, since they
don't really have tensor-specific logic.

c10 functions are not all header-only, so this still isn't ideal, but
this still seems like an improvement. Moreover, cpp_prefix.h -- used
when compiling cpp kernels -- already includes c10 header files, so
ABI-compatibility already depends on maintaining some c10 functions as
header-only.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/113276
Approved by: https://github.com/chenyang78, https://github.com/desertfire

aten/src/ATen/native/BinaryOps.h
@@ -52,60 +52,6 @@ inline void sub_check(const TensorBase& self, const Scalar& scalar) {
               "If you are trying to invert a mask, use the `~` or `logical_not()` operator instead.");
 }
 
-#if defined(__CUDACC__) || defined(__HIPCC__)
-#define HOST_DEVICE __host__ __device__
-#else
-#define HOST_DEVICE
-#endif
-
-// NOTE: [Floor Division in Python]
-// Python's __floordiv__ operator is more complicated than just floor(a / b).
-// It aims to maintain the property: a == (a // b) * b + remainder(a, b)
-// which can otherwise fail due to rounding errors in the remainder.
-// So, instead it is calculated as: a // b = (a - remainder(a, b)) / b
-// With some additional fix-ups added to the result.
-//
-// For reference, see CPython's implementation:
-// https://github.com/python/cpython/blob/ace008c531dd685a30c1dd68f9b5ba35f20171cf/Objects/floatobject.c#L636
-
-template <typename scalar_t>
-inline HOST_DEVICE scalar_t div_floor_floating(scalar_t a, scalar_t b) __ubsan_ignore_float_divide_by_zero__ {
-  if (C10_UNLIKELY(b == 0)) {
-    // Divide by zero: return standard IEEE result
-    return a / b;
-  }
-
-  auto mod = std::fmod(a, b);
-  auto div = (a - mod) / b;
-  if ((mod != 0) && (b < 0) != (mod < 0)) {
-    div -= scalar_t(1);
-  }
-
-  scalar_t floordiv;
-  if (div != 0) {
-    floordiv = std::floor(div);
-    if (div - floordiv > scalar_t(0.5)) {
-      floordiv += scalar_t(1.0);
-    }
-  } else {
-    floordiv = compat_copysign(scalar_t(0), a / b);
-  }
-  return floordiv;
-}
-
-template <typename scalar_t>
-inline HOST_DEVICE scalar_t div_floor_integer(scalar_t a, scalar_t b) {
-  if (c10::signs_differ(a, b)) {
-    // Subtracts one from the results of truncation division if the
-    // divisor and dividend have different sign(bit)s and the remainder of
-    // the division is nonzero
-    const auto quot = a / b;
-    const auto rem = a % b;
-    return rem ? quot - 1 : quot;
-  }
-  return a / b;
-}
-
 using structured_binary_fn_alpha = void(*)(TensorIteratorBase&, const Scalar& alpha);
 using structured_binary_fn_double = void(*)(TensorIteratorBase&, double);
 using structured_binary_fn = void(*)(TensorIteratorBase&);

c10/util/generic_math.h
@@ -1,0 +1,61 @@
+#pragma once
+
+#include <c10/macros/Macros.h>
+#include <c10/util/TypeSafeSignMath.h>
+#include <cmath>
+
+// The functions in this file should be header-only as it is used under
+// ABI-compatibility mode.
+
+namespace c10 {
+
+// NOTE: [Floor Division in Python]
+// Python's __floordiv__ operator is more complicated than just floor(a / b).
+// It aims to maintain the property: a == (a // b) * b + remainder(a, b)
+// which can otherwise fail due to rounding errors in the remainder.
+// So, instead it is calculated as: a // b = (a - remainder(a, b)) / b
+// With some additional fix-ups added to the result.
+//
+// For reference, see CPython's implementation:
+// https://github.com/python/cpython/blob/ace008c531dd685a30c1dd68f9b5ba35f20171cf/Objects/floatobject.c#L636
+
+template <typename scalar_t>
+inline C10_HOST_DEVICE scalar_t div_floor_floating(scalar_t a, scalar_t b)
+    __ubsan_ignore_float_divide_by_zero__ {
+  if (C10_UNLIKELY(b == 0)) {
+    // Divide by zero: return standard IEEE result
+    return a / b;
+  }
+
+  auto mod = std::fmod(a, b);
+  auto div = (a - mod) / b;
+  if ((mod != 0) && (b < 0) != (mod < 0)) {
+    div -= scalar_t(1);
+  }
+
+  scalar_t floordiv;
+  if (div != 0) {
+    floordiv = std::floor(div);
+    if (div - floordiv > scalar_t(0.5)) {
+      floordiv += scalar_t(1.0);
+    }
+  } else {
+    floordiv = compat_copysign(scalar_t(0), a / b);
+  }
+  return floordiv;
+}
+
+template <typename scalar_t>
+inline C10_HOST_DEVICE scalar_t div_floor_integer(scalar_t a, scalar_t b) {
+  if (c10::signs_differ(a, b)) {
+    // Subtracts one from the results of truncation division if the
+    // divisor and dividend have different sign(bit)s and the remainder of
+    // the division is nonzero
+    const auto quot = a / b;
+    const auto rem = a % b;
+    return rem ? quot - 1 : quot;
+  }
+  return a / b;
+}
+
+} // namespace c10

aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
@@ -14,6 +14,7 @@
 #include <ATen/native/cpu/LogAddExp.h>
 #include <c10/macros/Macros.h>
 #include <c10/util/TypeSafeSignMath.h>
+#include <c10/util/generic_math.h>
 
 namespace at::native {
 
@@ -218,7 +219,7 @@ void div_floor_kernel(TensorIteratorBase& iter) {
     AT_DISPATCH_INTEGRAL_TYPES(dtype, "div_floor_cpu", [&]() {
       cpu_kernel(iter, [](scalar_t a, scalar_t b) -> scalar_t {
         TORCH_CHECK(b != 0, "ZeroDivisionError");
-        return div_floor_integer(a, b);
+        return c10::div_floor_integer(a, b);
       });
     });
   } else {
@@ -231,7 +232,7 @@ void div_floor_kernel(TensorIteratorBase& iter) {
         using vec_t = Vectorized<opmath_t>;
         cpu_kernel_vec(iter,
           [=](scalar_t a) -> scalar_t {
-            return div_floor_floating(static_cast<opmath_t>(a), b);
+            return c10::div_floor_floating(static_cast<opmath_t>(a), b);
           },
           [=](Vectorized<scalar_t> a) {
             return binary_op_scalar(a, b, [](const vec_t& x, const vec_t& y) { return div_floor_floating_vec(x, y); });
@@ -242,7 +243,7 @@ void div_floor_kernel(TensorIteratorBase& iter) {
         using vec_t = Vectorized<scalar_t>;
         cpu_kernel_vec(iter,
           [](scalar_t a, scalar_t b) -> scalar_t {
-            return div_floor_floating(a, b);
+            return c10::div_floor_floating(a, b);
           },
           [](vec_t a, vec_t b) -> vec_t {
             return div_floor_floating_vec(a, b);

aten/src/ATen/native/cuda/BinaryDivFloorKernel.cu
@@ -8,6 +8,7 @@
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/cuda/CUDAMathCompat.h>
 #include <c10/util/TypeSafeSignMath.h>
+#include <c10/util/generic_math.h>
 #include <ATen/native/cuda/BinaryInternal.h>
 #include <ATen/native/cuda/JitLoops.cuh>
 #include <ATen/native/cuda/Loops.cuh>
@@ -29,7 +30,7 @@ void div_floor_kernel_cuda(TensorIteratorBase& iter) {
     AT_DISPATCH_INTEGRAL_TYPES(dtype, "div_floor_cuda", [&]() {
       gpu_kernel_with_scalars(
           iter, [] GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-            return div_floor_integer(a, b);
+            return c10::div_floor_integer(a, b);
       });
     });
   } else if (iter.is_cpu_scalar(2)) {
@@ -70,7 +71,7 @@ void div_floor_kernel_cuda(TensorIteratorBase& iter) {
         kHalf, kBFloat16, dtype, "div_floor_cuda", [&]() {
           gpu_kernel_with_scalars(
               iter, [] GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-                return div_floor_floating(a, b);
+                return c10::div_floor_floating(a, b);
               });
         });
   }

test/inductor/test_indexing.py
@@ -230,11 +230,11 @@ class ExprPrinterTests(TorchTestCase):
             expr = FloorDiv(s1, s2)
             self.assertEqual(pexpr(expr), "(s1 // s2)")
             if integer:
-                self.assertEqual(cexpr(expr), "at::native::div_floor_integer(s1, s2)")
+                self.assertEqual(cexpr(expr), "c10::div_floor_integer(s1, s2)")
             else:
                 self.assertEqual(
                     cexpr(expr),
-                    "at::native::div_floor_floating(static_cast<double>(s1), static_cast<double>(s2))",
+                    "c10::div_floor_floating(static_cast<double>(s1), static_cast<double>(s2))",
                 )
 
         for integer in [True, False]:
@@ -248,7 +248,7 @@ class ExprPrinterTests(TorchTestCase):
                 self.assertEqual(pexpr(expr), "(s1 // (-1))")
                 self.assertEqual(
                     cexpr(expr),
-                    "at::native::div_floor_floating(static_cast<double>(s1), static_cast<double>((-1L)))",
+                    "c10::div_floor_floating(static_cast<double>(s1), static_cast<double>((-1L)))",
                 )
 
     def test_print_Min_Max(self):

torch/_inductor/codecache.py
@@ -1394,8 +1394,8 @@ def get_include_and_linking_paths(
         else:
             libs = ["omp"] if config.is_fbcode() else ["gomp"]
 
-    # Unconditionally import c10 for non-fbcode to use TORCH_CHECK - See PyTorch #108690
-    if not config.is_fbcode():
+    # Unconditionally import c10 for non-abi-compatible mode to use TORCH_CHECK - See PyTorch #108690
+    if not config.aot_inductor.abi_compatible:
         libs += ["c10"]
         lpaths += [cpp_extension.TORCH_LIB_PATH]
 

torch/_inductor/codegen/cpp.py
@@ -314,9 +314,9 @@ class CppPrinter(ExprPrinter):
         if div != 1:
             div = self.paren(self.doprint(div))
             if expr.is_integer:
-                x = f"at::native::div_floor_integer({x}, {div})"
+                x = f"c10::div_floor_integer({x}, {div})"
             else:
-                x = f"at::native::div_floor_floating(static_cast<double>({x}), static_cast<double>({div}))"
+                x = f"c10::div_floor_floating(static_cast<double>({x}), static_cast<double>({div}))"
         mod = self.paren(self.doprint(mod))
         return f"static_cast<{INDEX_TYPE}>({x}) % static_cast<{INDEX_TYPE}>({mod})"
 
@@ -325,8 +325,8 @@ class CppPrinter(ExprPrinter):
         x = self.paren(self.doprint(x))
         div = self.paren(self.doprint(div))
         if expr.is_integer:
-            return f"at::native::div_floor_integer({x}, {div})"
-        return f"at::native::div_floor_floating(static_cast<double>({x}), static_cast<double>({div}))"
+            return f"c10::div_floor_integer({x}, {div})"
+        return f"c10::div_floor_floating(static_cast<double>({x}), static_cast<double>({div}))"
 
     def _print_floor(self, expr):
         assert len(expr.args) == 1

torch/_inductor/codegen/cpp_prefix.h
@@ -9,11 +9,11 @@
 
 #include <ATen/NumericUtils.h>
 #include <ATen/core/PhiloxRNGEngine.h>
-#include <ATen/native/BinaryOps.h>
 #include <ATen/native/Math.h>
 
 #include <c10/util/BFloat16.h>
 #include <c10/util/BFloat16-math.h>
+#include <c10/util/generic_math.h>
 #include <c10/util/Half.h>
 #include <c10/util/TypeCast.h>
 

commit 7283432369e5d489c88958b5f8f09d502165a50e

Fix c10::div_floor_floating compile error
Introduced by #113276. I've added a test to catch future regressions.

[ghstack-poisoned]

aten/src/ATen/native/BinaryOps.h
@@ -4,16 +4,6 @@
 #include <ATen/native/DispatchStub.h>
 #include <c10/core/Scalar.h>
 #include <c10/util/TypeSafeSignMath.h>
-#if defined(__CUDA_ARCH__)
-#include <c10/cuda/CUDAMathCompat.h>
-#define compat_copysign c10::cuda::compat::copysign
-#elif defined(__HIPCC__)
-#include <c10/hip/HIPMathCompat.h>
-#define compat_copysign c10::hip::compat::copysign
-#else
-#include <c10/util/copysign.h>
-#define compat_copysign c10::copysign
-#endif
 
 
 namespace at {

c10/util/generic_math.h
@@ -4,6 +4,17 @@
 #include <c10/util/TypeSafeSignMath.h>
 #include <cmath>
 
+#if defined(__CUDA_ARCH__)
+#include <c10/cuda/CUDAMathCompat.h>
+#define compat_copysign c10::cuda::compat::copysign
+#elif defined(__HIPCC__)
+#include <c10/hip/HIPMathCompat.h>
+#define compat_copysign c10::hip::compat::copysign
+#else
+#include <c10/util/copysign.h>
+#define compat_copysign c10::copysign
+#endif
+
 // The functions in this file should be header-only as it is used under
 // ABI-compatibility mode.
 

c10/test/util/generic_math_test.cpp
@@ -1,0 +1,17 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include <c10/util/generic_math.h>
+
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+using namespace ::testing;
+
+TEST(GenericMathTest, div_floor_test) {
+  EXPECT_EQ(c10::div_floor_floating(5., 0.), INFINITY);
+  EXPECT_DOUBLE_EQ(c10::div_floor_floating(5., 2.), 2.);
+  EXPECT_DOUBLE_EQ(c10::div_floor_floating(5., -2.), -3.);
+  EXPECT_EQ(c10::div_floor_integer(5, 2), 2);
+  EXPECT_EQ(c10::div_floor_integer(5, -2), -3);
+}