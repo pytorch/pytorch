77cad4607ef0274898fbf9ae131f94159abc6df8
Manuel Candales <manuelcandales@gmail.com>
2023-12-11 17:05:58 -0800

    [InputGen] introduce solvable variable
    This diff introduces the SolvableVariable class, responsible for solving the space of values of a variable subjected to constraints.
    
    The supported constraints are:
    - Eq (equal to)
    - Ne (not equal to)
    - In (in list of values)
    - NotIn (not in list of values)
    - Le (less than or equal to)
    - Lt (less than)
    - Ge (greater than or equal to)
    - Gt (greater than)
    
    Note: A tentative future constraint not included here yet will be St (such that)
    
    A SolvableVariable needs to be initialized with the variable type. It maintains a space of values for that variable type (VariableSpace object).
    The result of applying multiple constraints to a SolvableVariable, is the conjunction of those constraints. Consequently, every constraint reduces or maintains its space. We currently don't support constraint disjunctions.
    
    Sample usage:
    ```
    v = SolvableVariable(float)
    v.Gt(0.5)
    v.Le(4.5)
    v.NotIn([1.5, 2.5])
    ```
    
    Differential Revision: [D51868719](https://our.internmc.facebook.com/intern/diff/D51868719/)
    
    [ghstack-poisoned]

---
+++ test/inputgen/test_variable_solving.py
@@ -0,0 +1,192 @@
+import torch
+from torch.testing._internal.common_utils import TestCase, run_tests
+from torch.testing._internal.inputgen.variable.constants import (
+    INT64_MAX,
+    INT64_MIN,
+)
+from torch.testing._internal.inputgen.variable.solve import SolvableVariable
+from torch.testing._internal.inputgen.variable.type import (
+    SUPPORTED_TENSOR_DTYPES,
+    ScalarDtype,
+)
+
+
+class TestSolvableVariable(TestCase):
+    def test_bool_solver(self):
+        s = SolvableVariable(bool)
+        self.assertTrue(s.space.discrete.initialized)
+        self.assertEqual(s.space.discrete.values, {False, True})
+
+        s.Ne(0.0)
+        self.assertEqual(str(s.space), "{True}")
+
+        s = SolvableVariable(bool)
+        s.Ne(1)
+        self.assertEqual(str(s.space), "{False}")
+
+        s = SolvableVariable(bool)
+        s.Eq(1)
+        self.assertEqual(str(s.space), "{True}")
+
+        s = SolvableVariable(bool)
+        s.NotIn([0.0, 1.0])
+        self.assertEqual(str(s.space), "{}")
+
+        s = SolvableVariable(bool)
+        s.Gt(2)
+        self.assertEqual(str(s.space), "{}")
+
+    def test_int_solver(self):
+        s = SolvableVariable(int)
+        self.assertFalse(s.space.empty())
+
+        s.Ne(0.0)
+        self.assertEqual(str(s.space), "(-inf, 0) (0, inf)")
+        self.assertFalse(s.space.contains(0))
+
+        s = SolvableVariable(int)
+        s.Eq(1.5)
+        self.assertEqual(str(s.space), "{}")
+
+        s = SolvableVariable(int)
+        s.Ge(3.5)
+        self.assertEqual(str(s.space), "[4, inf)")
+
+        s.Gt(5.0)
+        self.assertEqual(str(s.space), "(5, inf)")
+
+        s = SolvableVariable(int)
+        s.Gt(INT64_MAX)
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(int)
+        s.Lt(INT64_MIN)
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(int)
+        s.Gt(3)
+        s.Lt(4)
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(int)
+        s.Lt(float("inf"))
+        self.assertEqual(str(s.space), "(-inf, inf)")
+
+        s = SolvableVariable(int)
+        s.Lt(float("-inf"))
+        self.assertEqual(str(s.space), "{}")
+
+    def test_float_solver(self):
+        s = SolvableVariable(float)
+        s.Ge(1.7976931348623157e308)
+        self.assertEqual(str(s.space), "[1.7976931348623157e+308, inf]")
+
+        s = SolvableVariable(float)
+        s.Gt(float("inf"))
+        self.assertEqual(str(s.space), "{}")
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(float)
+        s.Eq(2)
+        self.assertEqual(str(s.space), "{2.0}")
+        s.Eq(1)
+        self.assertEqual(str(s.space), "{}")
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(float)
+        s.Eq(2)
+        s.Lt(2)
+        self.assertTrue(s.space.empty())
+
+        s = SolvableVariable(float)
+        s.Le(2)
+        self.assertEqual(str(s.space), "[-inf, 2.0]")
+        s.Eq(2)
+        self.assertEqual(str(s.space), "{2.0}")
+
+        s = SolvableVariable(float)
+        s.Ne(3)
+        self.assertEqual(str(s.space), "[-inf, 3.0) (3.0, inf]")
+        s.Le(3.5)
+        self.assertEqual(str(s.space), "[-inf, 3.0) (3.0, 3.5]")
+        s.Ge(3)
+        self.assertEqual(str(s.space), "(3.0, 3.5]")
+        s.In([3, 3.5])
+        self.assertEqual(str(s.space), "{3.5}")
+
+        s = SolvableVariable(float)
+        s.Lt(float("inf"))
+        self.assertEqual(str(s.space), "[-inf, inf)")
+
+        s = SolvableVariable(float)
+        s.Lt(float("-inf"))
+        self.assertEqual(str(s.space), "{}")
+
+        s = SolvableVariable(float)
+        s.Le(float("-inf"))
+        self.assertEqual(str(s.space), "[-inf, -inf]")
+
+        s = SolvableVariable(float)
+        s.Ge(float("inf"))
+        self.assertEqual(str(s.space), "[inf, inf]")
+
+    def test_str_solver(self):
+        s = SolvableVariable(str)
+
+        s.In(["a", "b", "c", "d"])
+        self.assertEqual(s.space.discrete.values, {"a", "b", "c", "d"})
+
+        s.Ne("a")
+        self.assertEqual(s.space.discrete.values, {"b", "c", "d"})
+
+        s.Eq("b")
+        self.assertEqual(s.space.discrete.values, {"b"})
+
+        s.NotIn(["b", "c"])
+        self.assertTrue(s.space.empty())
+
+        with self.assertRaises(Exception):
+            s.Le(3)
+
+    def test_tensor_dtype_solver(self):
+        s = SolvableVariable(torch.dtype)
+        self.assertTrue(s.space.discrete.initialized)
+        self.assertEqual(s.space.discrete.values, set(SUPPORTED_TENSOR_DTYPES))
+
+        s.In([torch.bool, torch.uint8, torch.int8, torch.int32, torch.float32])
+        self.assertEqual(
+            s.space.discrete.values,
+            {torch.bool, torch.uint8, torch.int8, torch.int32, torch.float32},
+        )
+
+        s.Ne(torch.float32)
+        self.assertEqual(
+            s.space.discrete.values, {torch.bool, torch.uint8, torch.int8, torch.int32}
+        )
+
+        s.NotIn([torch.bool, torch.uint8])
+        self.assertEqual(s.space.discrete.values, {torch.int8, torch.int32})
+
+        s.Eq(torch.int32)
+        self.assertEqual(s.space.discrete.values, {torch.int32})
+
+        with self.assertRaises(Exception):
+            s.Ge(3)
+
+    def test_scalar_dtype_solver(self):
+        s = SolvableVariable(ScalarDtype)
+        self.assertTrue(s.space.discrete.initialized)
+        self.assertEqual(s.space.discrete.values, set(ScalarDtype))
+
+        s.In([ScalarDtype.int, ScalarDtype.float])
+        self.assertEqual(s.space.discrete.values, {ScalarDtype.int, ScalarDtype.float})
+
+        s.Ne(ScalarDtype.float)
+        self.assertEqual(s.space.discrete.values, {ScalarDtype.int})
+
+        with self.assertRaises(Exception):
+            s.Gt(3)
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ torch/testing/_internal/inputgen/variable/solve.py
@@ -0,0 +1,118 @@
+import math
+from typing import Any, List, Union
+
+from torch.testing._internal.inputgen.variable.space import (
+    Discrete,
+    VariableSpace,
+)
+from torch.testing._internal.inputgen.variable.type import (
+    check_vtype,
+    convert_to_vtype,
+    is_integer,
+)
+
+
+class SolvableVariable:
+    def __init__(self, vtype):
+        self.vtype = vtype
+        self.space = VariableSpace(vtype)
+
+    def Eq(self, v: Any) -> None:
+        check_vtype(self.vtype, v)
+        if self.space.empty():
+            return
+        if self.space.contains(v):
+            self.space.discrete = Discrete([convert_to_vtype(self.vtype, v)])
+        else:
+            self.space.discrete = Discrete([])
+
+    def Ne(self, v: Any) -> None:
+        check_vtype(self.vtype, v)
+        if self.space.empty():
+            return
+        self.space.remove(v)
+
+    def In(self, values: List[Any]) -> None:
+        for v in values:
+            check_vtype(self.vtype, v)
+        if self.space.empty():
+            return
+        self.space.discrete = Discrete(
+            [convert_to_vtype(self.vtype, v) for v in values if self.space.contains(v)]
+        )
+
+    def NotIn(self, values: List[Any]) -> None:
+        for v in values:
+            check_vtype(self.vtype, v)
+        if self.space.empty():
+            return
+        for v in values:
+            self.space.remove(v)
+
+    def Le(self, upper: Union[bool, int, float]) -> None:
+        if self.vtype not in [bool, int, float]:
+            raise Exception(f"Le is not valid constraint on {self.vtype}")
+        check_vtype(self.vtype, upper)
+        if self.space.empty():
+            return
+        elif self.space.discrete.initialized:
+            self.space.discrete.filter(lambda v: v <= upper)
+        elif self.vtype == int:
+            if math.isfinite(upper):
+                self.space.intervals.set_upper(math.ceil(upper), upper_open=False)
+            else:
+                self.space.intervals.set_upper(upper, upper_open=False)
+        else:
+            self.space.intervals.set_upper(float(upper), upper_open=False)
+
+    def Lt(self, upper: Union[bool, int, float]) -> None:
+        if self.vtype not in [bool, int, float]:
+            raise Exception(f"Lt is not valid constraint on {self.vtype}")
+        check_vtype(self.vtype, upper)
+        if self.space.empty():
+            return
+        elif self.space.discrete.initialized:
+            self.space.discrete.filter(lambda v: v < upper)
+        elif self.vtype == int:
+            if math.isfinite(upper):
+                self.space.intervals.set_upper(
+                    math.floor(upper), upper_open=is_integer(upper)
+                )
+            else:
+                self.space.intervals.set_upper(upper, upper_open=True)
+        else:
+            self.space.intervals.set_upper(float(upper), upper_open=True)
+
+    def Ge(self, lower: Union[bool, int, float]) -> None:
+        if self.vtype not in [bool, int, float]:
+            raise Exception(f"Ge is not valid constraint on {self.vtype}")
+        check_vtype(self.vtype, lower)
+        if self.space.empty():
+            return
+        elif self.space.discrete.initialized:
+            self.space.discrete.filter(lambda v: v >= lower)
+        elif self.vtype == int:
+            if math.isfinite(lower):
+                self.space.intervals.set_lower(math.ceil(lower), lower_open=False)
+            else:
+                self.space.intervals.set_lower(lower, lower_open=False)
+        else:
+            self.space.intervals.set_lower(float(lower), lower_open=False)
+
+    def Gt(self, lower: Union[bool, int, float]) -> None:
+        if self.vtype not in [bool, int, float]:
+            raise Exception(f"Gt is not valid constraint on {self.vtype}")
+        check_vtype(self.vtype, lower)
+        if self.space.empty():
+            return
+        elif self.space.discrete.initialized:
+            self.space.discrete.filter(lambda v: v > lower)
+        elif self.vtype == int:
+            if math.isfinite(lower):
+                self.space.intervals.set_lower(
+                    math.ceil(lower), lower_open=is_integer(lower)
+                )
+            else:
+                self.space.intervals.set_lower(lower, lower_open=True)
+        else:
+            self.space.intervals.set_lower(float(lower), lower_open=True)
