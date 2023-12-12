924b182c152a3e74d0b432c7c0882b1c6dbaed1e
Manuel Candales <manuelcandales@gmail.com>
2023-12-12 14:40:15 -0800

    [InputGen] introduce attribute engine
    Differential Revision: [D52047186](https://our.internmc.facebook.com/intern/diff/D52047186/)
    
    [ghstack-poisoned]

---
+++ test/inputgen/test_attribute_engine.py
@@ -0,0 +1,46 @@
+# Owner(s): ["module: tests"]
+
+from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.testing._internal.inputgen.argument.type import ArgType
+from torch.testing._internal.inputgen.attribute.engine import AttributeEngine
+from torch.testing._internal.inputgen.attribute.model import Attribute
+from torch.testing._internal.inputgen.variable.type import ScalarDtype
+from torch.testing._internal.inputgen.specs.model import ConstraintProducer as cp
+
+
+class TestAttributeEngine(TestCase):
+    def test_engine(self):
+        constraints = [
+            cp.Value.Ge(lambda x, y: 1),
+            cp.Value.Le(lambda x, y: x + 3),
+            cp.Value.Ne(lambda x, y: y + 1),
+        ]
+        x = 2
+        y = 1
+
+        engine = AttributeEngine(
+            Attribute.VALUE, constraints, True, ArgType.Scalar, ScalarDtype.float
+        )
+        values = engine.gen(Attribute.VALUE, x, y)
+        self.assertEqual(len(values), 6)
+        self.assertTrue(all(v >= 1 for v in values))
+        self.assertTrue(all(v <= 5 for v in values))
+        self.assertTrue(all(v != 2 for v in values))
+
+        values = engine.gen(Attribute.DTYPE, x, y)
+        self.assertEqual(len(values), 1)
+
+        engine = AttributeEngine(
+            Attribute.VALUE, constraints, False, ArgType.Scalar, ScalarDtype.float
+        )
+        values = engine.gen(Attribute.VALUE, x, y)
+        self.assertEqual(len(values), 9)
+        self.assertTrue(float("-inf") in values)
+        self.assertTrue(0.9999999999999999 in values)
+        self.assertTrue(2.0 in values)
+        self.assertTrue(5.000000000000001 in values)
+        self.assertTrue(float("inf") in values)
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ torch/testing/_internal/inputgen/attribute/engine.py
@@ -0,0 +1,42 @@
+import copy
+from typing import List, Optional
+
+from torch.testing._internal.inputgen.argument.type import ArgType
+from torch.testing._internal.inputgen.attribute.model import Attribute
+from torch.testing._internal.inputgen.attribute.solve import AttributeSolver
+from torch.testing._internal.inputgen.specs.model import Constraint
+from torch.testing._internal.inputgen.variable.gen import VariableGenerator
+from torch.testing._internal.inputgen.variable.solve import SolvableVariable
+from torch.testing._internal.inputgen.variable.type import ScalarDtype
+
+
+class AttributeEngine(AttributeSolver):
+    def __init__(self, attribute: Attribute, constraints: List[Constraint], valid: bool, argtype: Optional[ArgType] = None, scalar_dtype: Optional[ScalarDtype] = None):
+        super().__init__(attribute, argtype, scalar_dtype)
+        self.constraints = constraints
+        self.valid = valid
+
+    def gen(self, focus: Attribute, *args):
+        if self.attribute == Attribute.OPTIONAL:
+            num = 2
+        elif self.attribute == focus:
+            if self.attribute == Attribute.DTYPE:
+                num = 8
+            else:
+                num = 6
+        else:
+            num = 1
+        gen_vals = set()
+        for variable in self.solve(self.constraints, focus, self.valid, *args):
+            vals = []
+            if variable.vtype in [bool, int, float]:
+                limits = self.attribute.get_custom_limits(self.argtype)
+                if limits is not None:
+                    v_copy = copy.deepcopy(variable)
+                    v_copy.Ge(limits[0])
+                    v_copy.Le(limits[1])
+                    vals = VariableGenerator(v_copy.space).gen(num)
+            if len(vals) == 0:
+                vals = VariableGenerator(variable.space).gen(num)
+            gen_vals.update(vals)
+        return gen_vals
