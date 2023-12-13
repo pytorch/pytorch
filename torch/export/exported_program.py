c7a8a943753899a22e7e62fff8909c29d22ec71f
angelayi <yiangela7@gmail.com>
2023-12-13 01:21:16 +0000

    [export] Do not copy state_dict in run_decomp (#115269)
    Fixes https://github.com/pytorch/pytorch/issues/114628
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115269
    Approved by: https://github.com/thiagocrepaldi, https://github.com/ydwu4

--- torch/export/exported_program.py
+++ torch/export/exported_program.py
@@ -466,14 +466,13 @@ class ExportedProgram:
             for inp_dim1, inp_dim2 in self.equality_constraints
         ]
 
-        state_dict = self.state_dict.copy()
         lift_constant_tensor_pass(gm, new_graph_signature)
         _replace_sym_size_ops_pass(gm)
         exported_program = ExportedProgram(
             gm,
             gm.graph,
             new_graph_signature,
-            state_dict,
+            self.state_dict,
             new_range_constraints,
             new_equality_constraints,
             copy.deepcopy(self.module_call_graph),
