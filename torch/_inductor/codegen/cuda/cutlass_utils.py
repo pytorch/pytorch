ee8084a86a8903943f8cd120afb3880fb8590f32
Kai Londenberg <klondenberg@meta.com>
2023-12-14 10:17:47 +0100

    [Inductor cutlass backend] Added (some) doc comments and type annotations
    Improved the coverage of doc comments and type annotations. No functional changes.
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/cuda/cutlass_utils.py
+++ torch/_inductor/codegen/cuda/cutlass_utils.py
@@ -141,8 +141,9 @@ def _gen_ops_cached(arch, version) -> List[Any]:
 
     # Import cutlass python scripts.
     assert try_import_cutlass()
-    import torch._inductor.codegen.cuda.cutlass_lib_extensions.generator_extended as cutlass_generator  # type: ignore[import]
     import cutlass_library.manifest as cutlass_manifest  # type: ignore[import]
+
+    import torch._inductor.codegen.cuda.cutlass_lib_extensions.generator_extended as cutlass_generator  # type: ignore[import]
 
     if arch is None or version is None:
         log.error(
