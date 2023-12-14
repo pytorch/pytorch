00bb0053fb5ebee3f8c49dcf55cd407385958f75
Kai Londenberg <klondenberg@meta.com>
2023-12-14 19:07:22 +0100

    [Inductor cutlass backend] Enable torch.bmm and torch.baddbmm support
    This adds support for torch.bmm and torch.baddbmm operations through Cutlass backend. A comparatively simple addition.
    
    ghstack-source-id: ae7355631ccf4e8e959ef1bffb0fd2dd7784e37d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113890

--- torch/_inductor/kernel/bmm.py
+++ torch/_inductor/kernel/bmm.py
@@ -6,7 +6,12 @@ from ..select_algorithm import (
     ExternKernelChoice,
     TritonTemplate,
 )
-from ..utils import ceildiv as cdiv, use_aten_gemm_kernels, use_triton_template
+from ..utils import (
+    ceildiv as cdiv,
+    use_aten_gemm_kernels,
+    use_cutlass_template,
+    use_triton_template,
+)
 
 from .mm_common import addmm_epilogue, mm_args, mm_configs, mm_options
 
@@ -99,6 +104,12 @@ def tuned_bmm(mat1, mat2, *, layout=None):
                 layout=layout,
                 **mm_options(config, m, n, k, layout),
             )
+    if m * n != 0 and use_cutlass_template(layout, m, n, k):
+        from ..codegen.cuda.gemm_template import CUTLASSGemmTemplate
+
+        CUTLASSGemmTemplate.add_cutlass_gemm_choices(
+            choices, layout, [mat1, mat2], fuseable=True, non_fuseable=True
+        )
 
     return autotune_select_algorithm("bmm", choices, [mat1, mat2], layout)
 
@@ -106,11 +117,11 @@ def tuned_bmm(mat1, mat2, *, layout=None):
 # Don't register this since it is slower than decomposing it
 # @register_lowering(aten.baddbmm)
 def tuned_baddbmm(inp, mat1, mat2, *, alpha=1, beta=1, layout=None):
-    m, n, k, layout, mat1, mat2, inp = mm_args(mat1, mat2, inp, layout=layout)
+    m, n, k, layout, mat1, mat2, inp_expanded = mm_args(mat1, mat2, inp, layout=layout)
 
     # options to tune from
     choices = (
-        [aten_baddbmm.bind((inp, mat1, mat2), layout, alpha=alpha, beta=beta)]
+        [aten_baddbmm.bind((inp_expanded, mat1, mat2), layout, alpha=alpha, beta=beta)]
         if use_aten_gemm_kernels()
         else []
     )
@@ -118,11 +129,25 @@ def tuned_baddbmm(inp, mat1, mat2, *, alpha=1, beta=1, layout=None):
         for config in mm_configs(m, n, k):
             bmm_template.maybe_append_choice(
                 choices,
-                input_nodes=(inp, mat1, mat2),
+                input_nodes=(inp_expanded, mat1, mat2),
                 layout=layout,
                 **mm_options(config, m, n, k, layout),
                 prefix_args=1,
                 epilogue_fn=addmm_epilogue(layout.dtype, alpha, beta),
             )
+
+    if use_cutlass_template(layout, m, n, k):
+        from ..codegen.cuda.gemm_template import CUTLASSGemmTemplate
+
+        CUTLASSGemmTemplate.add_cutlass_gemm_choices(
+            choices,
+            layout,
+            [mat1, mat2, inp_expanded],
+            alpha=alpha,
+            beta=beta,
+            input_reorder=[2, 0, 1],
+            fuseable=True,
+            non_fuseable=True,
+        )
 
     return autotune_select_algorithm("baddbmm", choices, [inp, mat1, mat2], layout)
