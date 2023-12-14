0fe014bd8a55ab9fef59db4e82a0136ab5da6c85
Will Constable <whc@meta.com>
2023-12-14 06:41:04 -0800

    [C10D] Change PGNCCL logs to prefix [PG {} Rank {}] (#115801)
    Adds a PG {process group uid} prefix component to logs.
    
    This is helpful in situations where there are multiple processgroups,
    and rank information by itself is confusing.  (For example rank0 on PG1
    may correspond to rank3 on PG0.  People may assume 'rank0' references
    the global (PG0) world, but it may reference a sub-pg.  Prefacing the PG
    helps clarify this.
    
    Does NOT change logs from inside WorkNCCL functions, since WorkNCCL
    doens't know what PG ID it corresponds to. Will address these logs
    separately.
    
    Example:
    
    ```
    [I ProcessGroupNCCL.cpp:787] [PG 0 Rank 0] ProcessGroupNCCL initialization ...
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115801
    Approved by: https://github.com/fduwjj
    ghstack dependencies: #115771, #115798, #115800

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1377,7 +1377,7 @@ struct DumpPipe {
 #endif
 
 const std::string& ProcessGroupNCCL::logPrefix() const {
-  static std::string prefix = c10::str("[Rank ", rank_, "] ");
+  static std::string prefix = c10::str("[PG ", uid_, " Rank ", rank_, "] ");
   return prefix;
 }
 
