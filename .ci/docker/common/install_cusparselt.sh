e863df30a599bd3e47869fbd9fae4c0e7bb03139
Jesse Cai <jcjessecai@gmail.com>
2023-12-14 15:28:38 -0800

    fix quotes

--- .ci/docker/common/install_cusparselt.sh
+++ .ci/docker/common/install_cusparselt.sh
@@ -7,7 +7,7 @@ pushd tmp_cusparselt
 CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive"
 curl --retry 3 -OLs "https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-x86_64/${CUSPARSELT_NAME}.tar.xz"
 tar xf "${CUSPARSELT_NAME}.tar.xz"
-cp -a "${CUSPARSELT_NAME}/include/*" /usr/local/cuda/include/
+cp -a ${CUSPARSELT_NAME}/include/* /usr/local/cuda/include/
 cp -a ${CUSPARSELT_NAME}/lib/* /usr/local/cuda/lib64/
 popd
 rm -rf tmp_cusparselt
