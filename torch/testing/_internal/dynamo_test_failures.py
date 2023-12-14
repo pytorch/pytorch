85262b0a9e88d56f11deea44f05f25c9f4b8eb30
rzou <zou3519@gmail.com>
2023-12-14 11:00:12 -0800

    markDynamoStrictTest some test_cpp_extensions.* (#115858)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115858
    Approved by: https://github.com/voznesenskym
    ghstack dependencies: #115845, #115855, #115856, #115857

--- torch/testing/_internal/dynamo_test_failures.py
+++ torch/testing/_internal/dynamo_test_failures.py
@@ -2,4 +2,9 @@
 # when run under PYTORCH_TEST_WITH_DYNAMO=1.
 #
 # This lists exists so we can more easily add large numbers of failing tests,
-dynamo_expected_failures = {}
+dynamo_expected_failures = {
+    "TestCppExtensionJIT.test_cpp_frontend_module_has_up_to_date_attribute",
+    "TestCppExtensionJIT.test_custom_compound_op_autograd",
+    "TestCppExtensionJIT.test_cpp_frontend_module_has_up_to_date_attributes",
+    "TestCppExtensionOpenRgistration.test_open_device_registration",
+}
