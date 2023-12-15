989d39d7acfa6e99302bb2d9188b675532f724f6
haozhe.zhu <haozhe.zhu@intel.com>
2023-12-15 16:06:12 +0800

    change pattern order to enable concat linear first
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/freezing_patterns.py
+++ torch/_inductor/fx_passes/freezing_patterns.py
@@ -83,14 +83,13 @@ def freezing_passes(gm: torch.fx.GraphModule, aot_example_inputs):
 
 @init_once_fakemode
 def lazy_init():
+    addmm_patterns_init()
     if torch._C._has_mkldnn and config.cpp.weight_prepack:
         from .mkldnn_fusion import _mkldnn_weight_pack_init
 
         _mkldnn_weight_pack_init()
 
     from .binary_folding import binary_folding_init
-
-    addmm_patterns_init()
     binary_folding_init()
 
 
