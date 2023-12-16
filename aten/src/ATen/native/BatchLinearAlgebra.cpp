622947afa86b1c9c3db37466f43984c73db77677
Nikita Shulga <nshulga@meta.com>
2023-12-16 06:07:40 +0000

    [BE] Use nested namespace in ATen/native (#115938)
    It's a C++17 feature that usually makes code a bit more compact, and should have no side-effects otherwise.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115938
    Approved by: https://github.com/Skylion007

--- aten/src/ATen/native/BatchLinearAlgebra.cpp
+++ aten/src/ATen/native/BatchLinearAlgebra.cpp
@@ -409,8 +409,7 @@ extern "C" void dtrsm_(char *side, char *uplo, char *trans, char *diag, int *n, 
 extern "C" void strsm_(char *side, char *uplo, char *trans, char *diag, int *n, int *nrhs, float *alpha, float *a, int *lda, float *b, int *ldb);
 #endif
 
-namespace at {
-namespace meta {
+namespace at::meta {
 
 TORCH_META_FUNC(linalg_ldl_factor_ex)
 (const Tensor& self, bool hermitian, bool check_errors) {
@@ -775,9 +774,9 @@ TORCH_META_FUNC(linalg_lu)(const Tensor& A, bool pivot) {
   set_output_raw_strided(2, sizes, {}, A.options(), {});
 }
 
-} // namespace meta
+} // namespace at::meta
 
-namespace native {
+namespace at::native {
 
 #if AT_BUILD_WITH_LAPACK()
 // Define the per-batch functions to be used in the main implementation of the batched
@@ -4036,4 +4035,4 @@ Tensor linalg_vander_symint(
   auto ones =  result.new_ones_symint(shape);
   return at::cat({std::move(ones), std::move(result)}, /*dim=*/ -1);
 }
-}}  // namespace at::native
+}  // namespace at::native
