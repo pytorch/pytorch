b9993e907656a319ca53b2f130fcd207a1461c73
FFFrog <ljw1101.vip@gmail.com>
2023-12-18 19:31:11 +0800

    torch.set_default_device & torch.get_default_device
    1. fix bug of torch.set_default_device in multi-threading
    2. add new interface named torch.get_default_device

--- test/test_utils.py
+++ test/test_utils.py
@@ -24,7 +24,6 @@ from torch.testing._internal.common_methods_invocations import op_db
 import torch.cuda
 from torch.utils._pytree import tree_any, tree_all_only
 from torch.utils.checkpoint import checkpoint, checkpoint_sequential
-from torch import set_default_device
 from torch.utils._device import set_device
 from torch.utils._traceback import report_compile_source_on_error, format_traceback_short, CapturedTraceback
 import torch.utils.cpp_extension
@@ -889,7 +888,6 @@ class TestDeviceUtils(TestCase):
         self.assertEqual(r1.device.type, 'meta')
         self.assertEqual(r2.device.type, 'meta')
 
-
     def test_nn_module(self):
         with torch.device('meta'):
             m = nn.Linear(40, 50)
@@ -897,12 +895,35 @@ class TestDeviceUtils(TestCase):
 
     def test_set_default_device(self):
         try:
-            set_default_device('meta')
+            torch.set_default_device('meta')
             r = torch.empty(2, 2)
         finally:
-            set_default_device(None)
+            torch.set_default_device(None)
 
         self.assertEqual(r.device.type, 'meta')
+
+    def test_get_default_device(self):
+        torch.set_default_device('meta')
+        self.assertEqual(torch.get_default_device().type, 'meta')
+        torch.set_default_device(None)
+
+    @unittest.skipIf(
+        not (torch.cuda.is_available() and torch.cuda.device_count() > 1),
+        "requires multiple GPUs",
+    )
+    def test_get_default_device_more(self):
+        torch.set_default_device("cuda")
+        self.assertEqual(torch.get_default_device(), torch.tensor([]).device)
+        torch.set_default_device(None)
+
+        torch.set_default_device("cuda")
+        torch.cuda.set_device("cuda:1")
+        self.assertEqual(torch.get_default_device(), torch.tensor([]).device)
+        torch.set_default_device(None)
+
+        torch.set_default_device("cuda:1")
+        self.assertEqual(torch.get_default_device(), torch.tensor([]).device)
+        torch.set_default_device(None)
 
     @onlyCPU
     @ops(op_db)
