a2ff5985f71c02c604fff3ca05e67d1c058ad2c7
zejun <zejun.chen@intel.com>
2023-12-19 13:29:49 +0800

    Update torch/_dynamo/variables/functions.py
    Use method to get value from ConstantVariable
    
    Co-authored-by: Jason Ansel <jansel@jansel.net>

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -596,7 +596,7 @@ class CollectiveFunctionRewriteVariable(UserFunctionVariable):
         # call_function must check any unsupported arguments and graph-break.
         # It's safe to assume args/kwargs from orig_fn map 1:1 to args/kwargs of remapped_fn,
         # since that's the contract for putting a mapping in `traceable_collective_remaps`
-        if "async_op" in kwargs and kwargs["async_op"].value:
+        if "async_op" in kwargs and kwargs["async_op"].as_python_constant():
             unimplemented(
                 f"CollectiveFunctionRewriteVariable can't support async_op=True for {self.fn}"
             )
