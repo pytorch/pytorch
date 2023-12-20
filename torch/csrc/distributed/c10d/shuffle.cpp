57373bca13f6f5105743f463ea07c5b0e603de78
Yifu Wang <yifu@fb.com>
2023-12-19 20:48:22 -0800

    Update on "fsdp_copy_out proof of concept"
    ~Benchmark script: P901599447~
    ~\~500GB/s throughput for fsdp copy_out, straight from all_gather output to individual parameters, considering all overhead.~
    
    ~The approach requires padding individual parameters to align with 128 bits. Storage-wise, this comes at no extra cost because the minimum allocation size of CUDACachingAllocator is 512 bytes. Transportation-wise, the upper-bound increase in send size for bf16 is NUM_PARAMS * 7 elements. To put this in perspective, consider the benchmark script that test against a 0.15GB all_gather output; in this case, each device would only need to send an additional 302 bytes.~
    
    No alignment requirement anymore. Can achieve 650-700GB/s on the tested sizes.
    
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 awgu penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/shuffle.cpp
+++ torch/csrc/distributed/c10d/shuffle.cpp
@@ -5,8 +5,7 @@
 void fsdpAllGatherCopyOut(
     std::vector<at::Tensor> params,
     at::Tensor allGatherRes,
-    int64_t worldSize,
-    int64_t maxBlocksPerShard);
+    int64_t worldSize);
 #endif
 
 namespace {
@@ -14,11 +13,9 @@ namespace {
 void fsdp_all_gather_copy_out(
     std::vector<at::Tensor> params,
     at::Tensor all_gather_res,
-    int64_t world_size,
-    int64_t max_blocks_per_shard) {
+    int64_t world_size) {
 #ifdef USE_CUDA
-  return fsdpAllGatherCopyOut(
-      params, all_gather_res, world_size, max_blocks_per_shard);
+  return fsdpAllGatherCopyOut(params, all_gather_res, world_size);
 #else
   C10_THROW_ERROR(NotImplementedError, "Not implemented for CPU");
 #endif
@@ -29,7 +26,7 @@ void fsdp_all_gather_copy_out(
 TORCH_LIBRARY_FRAGMENT(c10d, m) {
   m.def(
       "fsdp_all_gather_copy_out("
-      "Tensor[] params, Tensor all_gather_res, int world_size, int max_blocks_per_shard=4) -> ()",
+      "Tensor[] params, Tensor all_gather_res, int world_size) -> ()",
       torch::dispatch(
           c10::DispatchKey::CompositeExplicitAutograd,
           ::fsdp_all_gather_copy_out),
--- torch/csrc/distributed/c10d/shuffle.cu
+++ torch/csrc/distributed/c10d/shuffle.cu
@@ -43,11 +43,10 @@ static __device__ inline bool isAligned(const void* ptr, size_t alignment) {
   return addr % alignment == 0;
 }
 
-template <typename T>
 static __global__ void fsdpAllGatherCopyOutKernel(
-    T** paramPtrs,
-    T* allGatherResPtr,
-    int64_t numel,
+    void** paramPtrs,
+    void* allGatherResPtr,
+    int64_t totalSize,
     int64_t* blockOffsetToParamIdx,
     int64_t* blockCumSums,
     int64_t* shardDimCumSums,
@@ -56,8 +55,6 @@ static __global__ void fsdpAllGatherCopyOutKernel(
     int64_t blockDimSum,
     int64_t ranksPerBlock,
     int64_t worldSize) {
-  constexpr int64_t numelPerThread = BYTES_PER_THREAD / sizeof(T);
-
   const int64_t blockOffset = blockIdx.x % blockDimSum;
   const int64_t paramIdx = blockOffsetToParamIdx[blockOffset];
 
@@ -75,23 +72,23 @@ static __global__ void fsdpAllGatherCopyOutKernel(
     const int64_t srcOff = rank * shardDimSum + shardBegin;
     const int64_t dstOff = rank * shardLen;
 
-    const T* srcPtr = allGatherResPtr + srcOff;
-    T* dstPtr = &paramPtrs[paramIdx][dstOff];
+    const char* srcPtr = reinterpret_cast<char*>(allGatherResPtr) + srcOff;
+    char* dstPtr = &reinterpret_cast<char*>(paramPtrs[paramIdx])[dstOff];
 
     const int64_t alignOff =
-        divUp(dstOff, numelPerThread) * numelPerThread - dstOff;
-    const int64_t begin = alignOff + localTid * numelPerThread;
+        divUp(dstOff, BYTES_PER_THREAD) * BYTES_PER_THREAD - dstOff;
+    const int64_t begin = alignOff + localTid * BYTES_PER_THREAD;
     const int64_t end =
-        alignOff + (shardLen - alignOff) / numelPerThread * numelPerThread;
-    const int64_t stride = groupSize * numelPerThread;
+        alignOff + (shardLen - alignOff) / BYTES_PER_THREAD * BYTES_PER_THREAD;
+    const int64_t stride = groupSize * BYTES_PER_THREAD;
 
     for (size_t i = begin; i < end; i += stride) {
       uint4 val;
       if (isAligned(srcPtr + i, 128)) {
         streamLoad128(val, srcPtr + i);
       } else {
-        for (size_t j = 0; j < numelPerThread; ++j) {
-          reinterpret_cast<T*>(&val)[j] = srcPtr[i + j];
+        for (size_t j = 0; j < BYTES_PER_THREAD; ++j) {
+          reinterpret_cast<char*>(&val)[j] = srcPtr[i + j];
         }
       }
       streamStore128(&dstPtr[i], val);
@@ -105,8 +102,20 @@ static __global__ void fsdpAllGatherCopyOutKernel(
   }
 }
 
+static int64_t geometricMean(const std::vector<int64_t>& numbers) {
+  TORCH_CHECK(numbers.size() > 0);
+  double logSum = 0.0;
+  for (double num : numbers) {
+    TORCH_CHECK(num > 0);
+    logSum += log(num);
+  }
+  double avgLog = logSum / numbers.size();
+  return exp(avgLog);
+}
+
 std::pair<at::Tensor, std::vector<int64_t*>> pack(
-    std::vector<std::vector<int64_t>> vecs) {
+    std::vector<std::vector<int64_t>> vecs,
+    const at::Device& device) {
   int64_t numel = 0;
   for (const auto& vec : vecs) {
     numel += vec.size();
@@ -122,7 +131,7 @@ std::pair<at::Tensor, std::vector<int64_t*>> pack(
         sizeof(int64_t) * vec.size());
     offset += vec.size();
   }
-  packed = packed.cuda();
+  packed = packed.to(device, /*non_blocking=*/true);
 
   std::vector<int64_t*> ptrs;
   offset = 0;
@@ -136,63 +145,70 @@ std::pair<at::Tensor, std::vector<int64_t*>> pack(
 void fsdpAllGatherCopyOut(
     std::vector<at::Tensor> params,
     at::Tensor allGatherRes,
-    int64_t worldSize,
-    int64_t maxBlocksPerShard) {
+    int64_t worldSize) {
   const auto device = allGatherRes.device();
-  const auto scalarType = allGatherRes.scalar_type();
+  const auto totalSize = allGatherRes.numel() * allGatherRes.element_size();
 
   TORCH_CHECK(allGatherRes.is_cuda());
   TORCH_CHECK(allGatherRes.is_non_overlapping_and_dense());
-  TORCH_CHECK(allGatherRes.numel() % worldSize == 0);
 
   std::vector<int64_t> paramPtrs;
-  std::vector<int64_t> dimCumSums{0};
+  std::vector<int64_t> shardDims; // In bytes
+  std::vector<int64_t> dimCumSums{0}; // In bytes
   for (size_t i = 0; i < params.size(); ++i) {
     const auto& param = params[i];
     TORCH_CHECK(param.is_non_overlapping_and_dense());
     TORCH_CHECK(param.device() == device);
-    TORCH_CHECK(param.scalar_type() == scalarType);
+    TORCH_CHECK(param.numel() > 0);
     // All params are expected to be aligned at worldSize.
-    // But not neccessarily worldSize * numelPerThread.
     TORCH_CHECK(param.numel() % worldSize == 0);
+    const auto shardDim = param.numel() * param.element_size() / worldSize;
     paramPtrs.push_back(reinterpret_cast<int64_t>(param.data_ptr()));
-    dimCumSums.push_back(dimCumSums[i] + param.numel() / worldSize);
+    shardDims.push_back(shardDim);
+    dimCumSums.push_back(dimCumSums[i] + shardDim);
   }
 
   TORCH_CHECK(
-      dimCumSums.back() * worldSize == allGatherRes.numel(),
-      "allGatherRes and params must contain the same number of elements.");
+      dimCumSums.back() * worldSize == totalSize,
+      "The total byte size must be identical between params and allGatherRes");
 
-  // To balance the throughput larger shards and waste on smaller shards,
-  // determine the block size with the average shard length.
-  const int64_t numelPerThread = BYTES_PER_THREAD / params[0].element_size();
-  const int64_t avgShardLen = allGatherRes.numel() / worldSize / params.size();
-  int64_t blockSize = divUp(avgShardLen, numelPerThread);
+  // To balance the throughput larger shards and waste on smaller shards, we
+  // use the geometric mean of the shard dims to determine the block size.
+  int64_t meanShardDim = geometricMean(shardDims);
+  int64_t blockSize = divUp(meanShardDim, BYTES_PER_THREAD);
   blockSize = divUp(blockSize, WARP_SIZE) * WARP_SIZE;
   blockSize = std::min(std::max(blockSize, MIN_NUM_THREADS), MAX_NUM_THREADS);
 
-  // TODO: if the numBlocks produced at this stage far exceeds maxActiveBlocks,
-  // we should increase the iter factor here as well.
+  // TODO: this is only for A100
+  constexpr int64_t maxActiveBlocks = 32 * 108;
+  constexpr double smOverSubFactor = 1.75;
+
+  // Roughly estimate the amount of blocks needed for each rank, and calculate
+  // an iter factor to regularize SM over-subscription.
+  int64_t iterFactor = 1;
+  while (divUp(totalSize, blockSize * BYTES_PER_THREAD * iterFactor) >
+         (maxActiveBlocks * smOverSubFactor)) {
+    iterFactor += 1;
+  }
+
   std::vector<int64_t> blockOffsetToParamIdx;
   std::vector<int64_t> blockCumSums{0};
   for (int64_t paramIdx = 0; paramIdx < static_cast<int64_t>(params.size());
        ++paramIdx) {
-    const int64_t shardNumel = params[paramIdx].numel() / worldSize;
-    int64_t numBlocks = divUp(shardNumel, blockSize * numelPerThread);
-    numBlocks = std::min(numBlocks, maxBlocksPerShard);
+    int64_t numBlocks =
+        divUp(shardDims[paramIdx], blockSize * BYTES_PER_THREAD * iterFactor);
     blockOffsetToParamIdx.insert(
         blockOffsetToParamIdx.end(), numBlocks, paramIdx);
     blockCumSums.push_back(blockCumSums.back() + numBlocks);
   }
   const auto numBlocks = blockCumSums.back();
 
-  auto packed =
-      pack({paramPtrs, blockOffsetToParamIdx, blockCumSums, dimCumSums});
+  auto packed = pack(
+      {paramPtrs, blockOffsetToParamIdx, blockCumSums, dimCumSums}, device);
 
-  // TODO: this is only for A100
-  constexpr int64_t maxActiveBlocks = 32 * 108;
   int64_t ranksPerBlock = 1;
-  while (numBlocks * (worldSize / ranksPerBlock) < maxActiveBlocks &&
+  while (numBlocks * (worldSize / ranksPerBlock) >
+             maxActiveBlocks * smOverSubFactor &&
          ranksPerBlock < worldSize) {
     ++ranksPerBlock;
   }
@@ -200,25 +216,26 @@ void fsdpAllGatherCopyOut(
   dim3 blocks(numBlocks * (worldSize / ranksPerBlock), 1, 1);
   dim3 threads(blockSize, 1, 1);
 
-  LOG(INFO) << "blocks: " << blocks.x << ", threads: " << threads.x;
-  LOG(INFO) << "avgShardLen: " << avgShardLen
-            << ", ranksPerBlock: " << ranksPerBlock;
+  LOG(INFO) << "meanShardDim: " << meanShardDim
+            << ", iterFactor: " << iterFactor
+            << ", ranksPerBlock: " << ranksPerBlock << ", blocks: " << blocks.x
+            << ", threads: " << threads.x;
 
-  AT_DISPATCH_ALL_TYPES_AND(
-      at::ScalarType::BFloat16, scalarType, "fsdp_all_gather_copy_out", [&] {
-        fsdpAllGatherCopyOutKernel<scalar_t>
-            <<<blocks, threads, 0, at::cuda::getCurrentCUDAStream()>>>(
-                reinterpret_cast<scalar_t**>(packed.second[0]),
-                allGatherRes.data_ptr<scalar_t>(),
-                allGatherRes.numel(),
-                /*blockOffsetToParamIdx=*/packed.second[1],
-                /*blockCumSums=*/packed.second[2],
-                /*shardDimCumSums=*/packed.second[3],
-                params.size(),
-                dimCumSums.back(),
-                blockCumSums.back(),
-                ranksPerBlock,
-                worldSize);
-        C10_CUDA_KERNEL_LAUNCH_CHECK();
-      });
+  fsdpAllGatherCopyOutKernel<<<
+      blocks,
+      threads,
+      0,
+      at::cuda::getCurrentCUDAStream()>>>(
+      reinterpret_cast<void**>(packed.second[0]),
+      allGatherRes.data_ptr(),
+      totalSize,
+      /*blockOffsetToParamIdx=*/packed.second[1],
+      /*blockCumSums=*/packed.second[2],
+      /*shardDimCumSums=*/packed.second[3],
+      params.size(),
+      dimCumSums.back(),
+      blockCumSums.back(),
+      ranksPerBlock,
+      worldSize);
+  C10_CUDA_KERNEL_LAUNCH_CHECK();
 }
