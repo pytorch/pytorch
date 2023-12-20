05e5b505db44c6d0190ed0641e4394cdd12e04f1
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-12-20 12:33:50 +0000

    Revert "[inductor] Avoid bool being upcast to int (#109913)"
    This reverts commit 92998693a9455af6259cae468265f01cfff8810e.
    
    Reverted https://github.com/pytorch/pytorch/pull/109913 on behalf of https://github.com/jeanschmidt due to causing performance regression in relevant metrics, @malfet I believe you are the correct person to help identify and fix the issues. More details check internal OPS count for ads metricsnin the internal related diff ([comment](https://github.com/pytorch/pytorch/pull/109913#issuecomment-1864397407))

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1648,15 +1648,11 @@ class TritonKernel(Kernel):
 
         if self.persistent_reduction:
             default = ir.Reduction.default_value(reduction_type, src_dtype)
+            default = self._map_tuple_or_scalar(triton_constant, default)
 
             def _mask_value(value, default):
-                # TODO: int1 seems to be broken on triton-rocm
-                mask_dtype = torch.int8 if src_dtype == torch.bool else src_dtype
-                other = self.cse.generate(
-                    self.compute, TritonKernelOverrides.constant(default, mask_dtype)
-                )
                 return self.cse.generate(
-                    self.compute, TritonKernelOverrides.where(cond, value, other)
+                    self.compute, f"tl.where({cond}, {value}, {default})"
                 )
 
             if isinstance(value, tuple):
