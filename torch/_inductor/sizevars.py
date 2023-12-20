9b978394e78488fdddda3e56daea99d204c72f89
Jason Ansel <jansel@meta.com>
2023-12-20 11:41:51 -0800

    Update base for Update on "[inductor] Add support for tl.make_block_ptr"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -500,6 +500,8 @@ class SizeVarAllocator:
         return order
 
     def lookup_precomputed_size(self, expr: Expr) -> sympy.Symbol:
+        if expr.is_number or expr.is_symbol:
+            return expr
         expr = self.remove_precomputed_replacements(expr)
         if expr not in self.precomputed_replacements:
             sym = sympy_symbol(f"ps{len(self.precomputed_replacements)}")
