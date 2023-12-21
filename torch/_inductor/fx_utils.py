97f2fc0eff598d69f23072dc369210b1f6d2e5cc
Isuru Fernando <ifernando@quansight.com>
2023-12-21 04:28:41 +0000

    Update on "Fix FakeTensorUpdater logic for updating fake tensors"
    Fixes https://github.com/pytorch/pytorch/issues/114464
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_inductor/fx_utils.py
+++ torch/_inductor/fx_utils.py
@@ -109,9 +109,11 @@ class FakeTensorUpdater:
                 continue
 
             def is_aten_node(node):
-                return node.op == "call_function" and isinstance(
-                    node.target, torch._ops.OpOverload
-                ) and node.target.namespace == "aten"
+                return (
+                    node.op == "call_function"
+                    and isinstance(node.target, torch._ops.OpOverload)
+                    and node.target.namespace == "aten"
+                )
 
             if not is_aten_node(node):
                 continue
