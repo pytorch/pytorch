c5b2055cae2953653599ae065e0a80de928984d1
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-12-21 14:05:43 +0000

    Revert "Support nn_module_stack in torch.export(strict=False) (#115454)"
    This reverts commit 6730b5bcb41e0519572759d9ad9852a113d0a7e4.
    
    Reverted https://github.com/pytorch/pytorch/pull/115454 on behalf of https://github.com/jeanschmidt due to Breaking internal tests recycle_bin_citadel and executorch, check internal diff to see more details ([comment](https://github.com/pytorch/pytorch/pull/115454#issuecomment-1866315233))

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -521,13 +521,11 @@ def _export(
                 gm, sig = aot_export(Wrapper(mod), args, **kwargs)
 
                 def strip_root(x):
-                    if isinstance(x, str) and x.startswith("_export_root"):
-                        stripped = x[len("_export_root") :]
-                        return stripped[1:] if stripped.startswith(".") else stripped
-                    return x
-
-                def fixup_key(x):
-                    return "L__self__" + strip_root(x)
+                    return (
+                        x[len("_export_root.") :]
+                        if x.startswith("_export_root.")
+                        else x
+                    )
 
                 sig.parameters = pytree.tree_map(strip_root, sig.parameters)
                 sig.buffers = pytree.tree_map(strip_root, sig.buffers)
@@ -540,18 +538,6 @@ def _export(
                 sig.buffers_to_mutate = pytree.tree_map(
                     strip_root, sig.buffers_to_mutate
                 )
-                for node in gm.graph.nodes:
-                    if "nn_module_stack" in node.meta:
-                        nn_module_stack = node.meta["nn_module_stack"]
-                        # Delete the wrapper module reference
-                        del nn_module_stack[""]
-                        node.meta["nn_module_stack"] = {
-                            fixup_key(key): val
-                            for key, val in pytree.tree_map(
-                                strip_root, nn_module_stack
-                            ).items()
-                        }
-
                 return gm, sig
 
             return _aot_export_non_strict
