e0e90bc0d49928ddbe387d78d126d96be4fb4dd3
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2023-12-27 19:05:55 +0000

    Revert "[dynamo] fix `sum()` function with `start` argument (#116389)"
    This reverts commit 3c9076f070fab5b27eae3b7846755c98b7c97a1a.
    
    Reverted https://github.com/pytorch/pytorch/pull/116389 on behalf of https://github.com/kit1980 due to Breaks Meta-internal tests, but the issue could have been caught on GitHub ([comment](https://github.com/pytorch/pytorch/pull/116389#issuecomment-1870556927))

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -1073,33 +1073,31 @@ class BuiltinVariable(VariableTracker):
             items = [fn.call_function(tx, [x], {}) for x in seq.unpack_var_sequence(tx)]
             return variables.TupleVariable(items)
 
-    def call_sum(self, tx, seq, start=_SENTINEL):
+    def call_sum(self, tx, seq, **kwargs):
         # Special case for sum on tuple of floats and ints
-        if isinstance(seq, (variables.ListVariable, variables.TupleVariable)) and all(
-            isinstance(x, variables.ConstantVariable)
-            and isinstance(x.value, (int, float))
-            for x in seq.items
+        if (
+            isinstance(seq, (variables.ListVariable, variables.TupleVariable))
+            and all(
+                isinstance(x, variables.ConstantVariable)
+                and isinstance(x.value, (int, float))
+                for x in seq.items
+            )
+            and not kwargs
         ):
-            if start is self._SENTINEL:
-                return variables.ConstantVariable.create(
-                    sum(x.value for x in seq.items),
-                )
-            if isinstance(start, variables.ConstantVariable) and isinstance(
-                start.value, (int, float)
-            ):
-                return variables.ConstantVariable.create(
-                    sum((x.value for x in seq.items), start=start.value),
-                )
+            new_list = [x.value for x in seq.items]
+            return variables.ConstantVariable.create(sum(new_list))
         if seq.has_unpack_var_sequence(tx):
-            if start is self._SENTINEL:
-                start = variables.ConstantVariable.create(0)
-            items = seq.unpack_var_sequence(tx)
+            start = kwargs.pop(
+                "start", variables.ConstantVariable.create(0)
+            ).as_python_constant()
+            assert not kwargs
+            items = seq.unpack_var_sequence(tx)[start:]
             return BuiltinVariable(functools.reduce).call_function(
                 tx,
                 [
                     BuiltinVariable(operator.add),
                     variables.TupleVariable(items),
-                    start,
+                    variables.ConstantVariable.create(0),
                 ],
                 {},
             )
