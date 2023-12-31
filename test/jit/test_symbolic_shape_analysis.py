6d746820e3b4ebefb05e7d8022f5207c75b47415
Aaron Gokaslan <aaronGokaslan@gmail.com>
2023-12-31 14:11:09 -0500

    Fix another missing import

--- test/jit/test_symbolic_shape_analysis.py
+++ test/jit/test_symbolic_shape_analysis.py
@@ -5,7 +5,7 @@ import unittest
 from textwrap import dedent
 
 import torch
-from torch import nn
+from torch import nn, Tensor
 from torch.testing import FileCheck
 from torch.testing._internal.common_methods_invocations import sample_inputs_cat_concat
 from torch.testing._internal.common_utils import make_tensor
@@ -585,7 +585,7 @@ class TestSymbolicShapeAnalysis(JitTestCase):
 
         @torch.jit.script
         def wrong_output_types(x: List[int], y: List[int]):
-            x: List[Tensor] = []  # noqa: F821
+            x: List[Tensor] = []
             return x
 
         with self.assertRaisesRegex(RuntimeError, "but got graph_type"):
