675fde7f2391eba38f21a4bbb21129b80bcae03a
Aaron Gokaslan <aaronGokaslan@gmail.com>
2023-12-31 14:34:51 -0500

    Add missing typing import

--- test/quantization/fx/test_model_report_fx.py
+++ test/quantization/fx/test_model_report_fx.py
@@ -1,4 +1,5 @@
 # Owner(s): ["oncall: quantization"]
+from typing import Set
 
 import torch
 import torch.nn as nn
@@ -1875,9 +1876,9 @@ class TestFxModelReportVisualizer(QuantizationTestCase):
             # these two together should be the same as the generated report info in terms of keys
             tensor_info_modules = {row[1] for row in tensor_table}
             channel_info_modules = {row[1] for row in channel_table}
-            combined_modules: Set = tensor_info_modules.union(channel_info_modules)  # noqa: F821
+            combined_modules: Set = tensor_info_modules.union(channel_info_modules)
 
-            generated_report_keys: Set = set(mod_rep_visualizer.generated_reports.keys())  # noqa: F821
+            generated_report_keys: Set = set(mod_rep_visualizer.generated_reports.keys())
             self.assertEqual(combined_modules, generated_report_keys)
 
     @skipIfNoFBGEMM
@@ -1902,7 +1903,7 @@ class TestFxModelReportVisualizer(QuantizationTestCase):
 
             tensor_info_modules = {row[1] for row in tensor_table}
             channel_info_modules = {row[1] for row in channel_table}
-            combined_modules: Set = tensor_info_modules.union(channel_info_modules)  # noqa: F821
+            combined_modules: Set = tensor_info_modules.union(channel_info_modules)
             self.assertEqual(len(combined_modules), 0)  # should be no matching modules
 
     @skipIfNoFBGEMM
