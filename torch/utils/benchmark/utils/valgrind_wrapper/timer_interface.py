0a4ad5a97a1eabad83064d38501b1198a04d34ab
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-01-01 13:08:29 -0500

    Fix args to Iterator

--- torch/utils/benchmark/utils/valgrind_wrapper/timer_interface.py
+++ torch/utils/benchmark/utils/valgrind_wrapper/timer_interface.py
@@ -55,7 +55,7 @@ class FunctionCounts:
     # the print settings. This is simply to allow hermetic unit tests.
     _linewidth: Optional[int] = None
 
-    def __iter__(self) -> Iterator[FunctionCount, None, None]:
+    def __iter__(self) -> Iterator[FunctionCount]:
         yield from self._data
 
     def __len__(self) -> int:
