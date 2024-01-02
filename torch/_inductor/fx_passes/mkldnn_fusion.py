1822abe8147913aedc7d6a34f681198cc9f12fa1
leslie-fang-intel <leslie.fang@intel.com>
2024-01-02 09:46:38 +0800

    [Inductor] Fix Conv Binary Inplace Fusion issue (#115153)
    **Summary**
    Take this Pattern as example
    ```
      #      ReLU
      #     /   \
      #  Conv1
      #   /      \
      # Conv2
      #   \      /
      #      Add
    ```
    The current `ConvBinaryInplace` check will fail to perform Inplace fusion (using outplace fusion instead) due to `ReLU` having 2 users. However, if all users of `ReLU` are ancestor nodes of `Conv2`, we should be able to proceed with the `ConvBinaryInplace` fusion. This diff relaxes the `ConvBinaryInplace` check accordingly.
    
    **TestPlan**
    ```
    python -m pytest test_mkldnn_pattern_matcher.py -k test_conv2d_binary_inplace_fusion_pass_cpu
    python -m pytest test_mkldnn_pattern_matcher.py -k test_conv2d_binary_inplace_fusion_failed_cpu
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115153
    Approved by: https://github.com/CaoE, https://github.com/jgong5

--- torch/_inductor/fx_passes/mkldnn_fusion.py
+++ torch/_inductor/fx_passes/mkldnn_fusion.py
@@ -366,12 +366,69 @@ if torch._C._has_mkldnn:
 
         return fn
 
+    def _get_remaining_users(extra_input_node, compute_node):
+        # Think about this pattern:
+        #      ReLU
+        #     /   \
+        #  Conv1
+        #   /      \
+        # Conv2
+        #   \      /
+        #      Add
+        # Although, the extra input node (ReLU) has more than 1 users: Conv1 and Add.
+        # The Conv1 is the ancestor node of the current compute node (Conv2).
+        # This indicates that the buffer of ReLU has completed all its usage,
+        # So we can safely make changes to it now by doing Conv2->Add inplace fusion.
+        # Take above case as example:
+        # * extra_input_node: ReLU
+        # * compute_node: Conv2
+        # _get_remaining_users will return the users of extra_input_node which are not
+        # ancestor node of compute_node.
+        def _is_ancestor_node(_current_node, _ancestor_node):
+            # Check whether _ancestor_node is the ancestor node of _current_node
+            _node_list = [_current_node]
+            _visited_nodes = set()
+            while len(_node_list) != 0:
+                _current_node = _node_list.pop(0)
+                if _current_node not in _visited_nodes:
+                    _visited_nodes.add(_current_node)
+                    if _current_node == _ancestor_node:
+                        return True
+                    elif isinstance(
+                        _current_node, torch.fx.Node
+                    ) and _current_node.op not in ["placeholder", "output", "get_attr"]:
+                        for input in _current_node.all_input_nodes:
+                            _node_list.append(input)  # noqa: PERF402
+            return False
+
+        return [
+            user
+            for user in list(extra_input_node.users)
+            if not _is_ancestor_node(compute_node, user)
+        ]
+
     def _is_valid_computation_binary_inplace(computation_op, binary_op, other_index):
         def fn(match):
             if not _is_valid_computation_binary(computation_op, binary_op)(match):
                 return False
             binary_nodes = filter_nodes(match.nodes, binary_op)
-            if any(len(n.args[other_index].users) > 1 for n in binary_nodes):
+
+            def _get_compute_node(_binary_node, _other_index):
+                assert (
+                    len(_binary_node.all_input_nodes) == 2
+                ), "Binary node should have 2 input nodes."
+                _compute_index = 1 if (_other_index == 0) else 0
+                return _binary_node.args[_compute_index]
+
+            if any(
+                len(
+                    _get_remaining_users(
+                        n.args[other_index], _get_compute_node(n, other_index)
+                    )
+                )
+                > 1
+                for n in binary_nodes
+            ):
                 return False
             if any(
                 n.args[other_index].op in ["placeholder", "output"]
