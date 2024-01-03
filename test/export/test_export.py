8600f2d2b3c83040d9619efd249a6ee730c18888
Arun Ranganathan <arun.ranganathan111@gmail.com>
2024-01-03 05:04:18 +0000

    Fix user input mutations for run_decompositions (#116382)
    Fixes #115106
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116382
    Approved by: https://github.com/angelayi

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -2005,6 +2005,30 @@ def forward(self, l_x_):
 
         check_device_and_fake_mode()
 
+    def test_run_decomposition_supports_user_input_mutation(self):
+        class SingleOp(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.op = torch.ops.aten.native_batch_norm
+
+            def forward(self, input, weight, bias, running_mean, running_var, training, momentum, eps, **kwargs):
+                return self.op(input, weight, bias, running_mean, running_var, training, momentum, eps, **kwargs)
+
+        input = torch.randn(5, 5, 5)
+        weight = torch.randn(5)
+        bias = torch.randn(5)
+        running_mean = torch.randn(5)
+        running_var = torch.randn(5)
+        training = True
+        momentum = 0.5
+        eps = 0.6
+
+        model = SingleOp()
+        output = model(input, weight, bias, running_mean, running_var, training, momentum, eps)
+
+        ep = torch.export.export(model, args=(input, weight, bias, running_mean, running_var, training, momentum, eps))
+        ep.run_decompositions(decomp_table=torch._decomp.decomposition_table)
+        self.assertEqual(ep(input, weight, bias, running_mean, running_var, training, momentum, eps), output)
 
     def test_export_graph_with_no_inputs(self):
         # We saw this pattern when users want to export
