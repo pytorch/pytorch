5f7a1a2313facaed40b0e719c4586efffe4a2dec
Aaron Orenstein <aorenste@fb.com>
2024-01-04 11:09:58 -0800

    Replace recursive stable_topological_sort() with iterative. (#116761)
    Summary:
    A graph with a deep set of nodes caused stable_topological_sort() to recurse and
    pop the stack. Rewrite it to be iterative and avoid recursion.
    
    Fixes #115506
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116761
    Approved by: https://github.com/jansel, https://github.com/oulgen, https://github.com/Skylion007

--- torch/_inductor/pattern_matcher.py
+++ torch/_inductor/pattern_matcher.py
@@ -1290,25 +1290,38 @@ def _args(n: torch.fx.Node) -> List[torch.fx.node.Argument]:
 
 
 def stable_topological_sort(graph: torch.fx.Graph):
-    waiting = defaultdict(list)
-    ready = set()
-    cursor = None
+    # Nodes are in exactly one of these three collections:
 
-    def check(node):
+    # - Nodes in `pending` are waiting to be processed (in reverse order):
+    pending = list(reversed(graph.nodes))
+
+    # - Nodes in `ready` have been processed and are already in the correct
+    #   order.
+    ready = set()
+
+    # - `waiting` is a mapping from a dependency to nodes which depend on that
+    #   dependency.
+    waiting = defaultdict(list)
+
+    # The cursor indicates the last processed node so we can add new nodes
+    # after it.
+    cursor = None
+    while pending:
+        node = pending.pop()
         waiting_for = [x for x in _args(node) if x not in ready]
         if waiting_for:
-            # revisit this node when next input is ready
-            waiting[waiting_for[0]].append(node)
+            # We have unprocessed input nodes. Might as well wait for the last
+            # arg so an already sorted list will only recheck this node once.
+            waiting[waiting_for[-1]].append(node)
         else:
-            nonlocal cursor
-            cursor = node
             ready.add(node)
-            for other in waiting.pop(node, ()):
-                cursor.append(other)
-                check(other)
+            if cursor and cursor.next is not node:
+                cursor.append(node)
+            cursor = node
+            # Mark the nodes that have been waiting for this node to finish as
+            # ready to check again.
+            pending.extend(reversed(waiting.pop(node, ())))
 
-    for n in list(graph.nodes):
-        check(n)
     assert not waiting and len(ready) == len(graph.nodes)
 
 
