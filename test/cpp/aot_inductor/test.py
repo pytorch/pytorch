b887a2c67541cb4ed37a59970bcdf9b30cd71455
Mu-Chu Lee <mlee8@meta.com>
2024-01-04 22:04:50 -0800

    [TEST ONLY] NEW TEST
    [ghstack-poisoned]

--- test/cpp/aot_inductor/test.py
+++ test/cpp/aot_inductor/test.py
@@ -5,17 +5,20 @@ from torch._export import aot_compile, dynamic_dim
 torch.manual_seed(1337)
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, device):
         super().__init__()
         self.fc = torch.nn.Linear(64, 10)
+        self.const_1 = torch.randn((10), device=device)
+        self.const_2 = torch.randn((10), device=device)
 
     def forward(self, x, y):
-        return self.fc(torch.sin(x) + torch.cos(y))
+        const_3 = self.const_1 + self.const_2
+        return self.fc(torch.sin(x) + torch.cos(y)) + const_3
 
 data = {}
 
 for device in ["cpu", "cuda"]:
-    model = Net().to(device=device)
+    model = Net(device=device).to(device=device)
     x = torch.randn((32, 64), device=device)
     y = torch.randn((32, 64), device=device)
     with torch.no_grad():
@@ -31,12 +34,15 @@ for device in ["cpu", "cuda"]:
         model_so_path = aot_compile(model, (x, y), constraints=constraints)
 
     params = dict(model.named_parameters())
+    suffix = f"{device}"
     data.update({
-        f"model_so_path_{device}": model_so_path,
-        f"inputs_{device}": [x, y],
-        f"outputs_{device}": [ref_output],
-        f"fc_weight_{device}": params["fc.weight"],
-        f"fc_bias_{device}": params["fc.bias"],
+        f"model_so_path_{suffix}": model_so_path,
+        f"inputs_{suffix}": [x, y],
+        f"outputs_{suffix}": [ref_output],
+        f"fc_weight_{suffix}": params["fc.weight"],
+        f"fc_bias_{suffix}": params["fc.bias"],
+        f"const_1_{suffix}": model.const_1,
+        f"const_2_{suffix}": model.const_2,
     })
 
 # Use this to communicate tensors to the cpp code
