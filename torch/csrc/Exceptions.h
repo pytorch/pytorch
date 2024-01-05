775dd32b9e3c7e4dcff91dce3dcbaa0ee4421b90
cyy <cyyever@outlook.com>
2024-01-05 09:16:54 +0800

    Remove NotImplementedError

--- torch/csrc/Exceptions.h
+++ torch/csrc/Exceptions.h
@@ -308,15 +308,6 @@ struct ValueError : public PyTorchError {
   }
 };
 
-// Translates to Python NotImplementedError
-struct NotImplementedError : public PyTorchError {
-  NotImplementedError(const char* format, ...) TORCH_FORMAT_FUNC(2, 3);
-  NotImplementedError() = default;
-  PyObject* python_type() override {
-    return PyExc_NotImplementedError;
-  }
-};
-
 // Translates to Python AttributeError
 struct AttributeError : public PyTorchError {
   AttributeError(const char* format, ...) TORCH_FORMAT_FUNC(2, 3);
