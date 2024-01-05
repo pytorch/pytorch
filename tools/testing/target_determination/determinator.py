f70d7b44062d7e5c89a8f61ec12f8f01b64173f9
Catherine Lee <csl@fb.com>
2024-01-04 18:13:27 -0800

    update

--- tools/testing/target_determination/determinator.py
+++ tools/testing/target_determination/determinator.py
@@ -9,12 +9,12 @@ from tools.testing.target_determination.heuristics import (
 
 
 def get_test_prioritizations(
-    tests: List[str], f: Any = sys.stdout
+    tests: List[str], file: Any = sys.stdout
 ) -> AggregatedHeuristics:
     aggregated_results = AggregatedHeuristics(unranked_tests=tests)
-    print(f"Received {len(tests)} tests to prioritize", file=f)
+    print(f"Received {len(tests)} tests to prioritize", file=file)
     for test in tests:
-        print(f"  {test}", file=f)
+        print(f"  {test}", file=file)
 
     for heuristic in HEURISTICS:
         new_rankings: TestPrioritizations = heuristic.get_test_priorities(tests)
@@ -24,11 +24,11 @@ def get_test_prioritizations(
         print(
             f"Heuristic {heuristic} identified {num_tests_found} tests "
             + f"to prioritize ({(num_tests_found / len(tests)):.2%}%)",
-            file=f,
+            file=file,
         )
 
         if num_tests_found:
-            print(new_rankings.get_info_str(), file=f)
+            print(new_rankings.get_info_str(), file=file)
 
     return aggregated_results
 
