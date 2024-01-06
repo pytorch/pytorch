985e3e2d2844e506dbfc454cf91404c74374f97b
Yu, Guangye <guangye.yu@intel.com>
2024-01-06 01:03:12 +0000

    Update on "[WIP] [4/4] Intel GPU Runtime Upstreaming for Device"
    # Motivation
    According to [[1/4] Intel GPU Runtime Upstreaming for Device](https://github.com/pytorch/pytorch/pull/116019), this last PR  covers the changes under lazy initialization
    
    # Design
    This PR primarily offers the support of multi-processing via lazy initialization. We lazily initialize our runtime avoiding initializing XPU until the first time it is accessed.
    
    # Additional Context
    We adopt a similar design to CUDA. So we share some code with CUDA.
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- torch/xpu/__init__.py
+++ torch/xpu/__init__.py
@@ -2,10 +2,9 @@ r"""
 This package is lazily initialized, so you can always import it.
 """
 import threading
-import traceback
 from functools import lru_cache
 
-from typing import Any, cast, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Union
 
 import torch
 import torch._C
@@ -13,38 +12,9 @@ from .. import device as _device
 from ._utils import _dummy_type, _get_device_index
 
 _initialized = False
-_tls = threading.local()
 _initialization_lock = threading.Lock()
-_queued_calls = []  # don't invoke these until initialization occurs
 _is_in_bad_fork = getattr(torch._C, "_xpu_isInBadFork", lambda: False)
 _device_t = Union[_device, str, int, None]
-
-
-class _LazySeedTracker:
-    # Since seeding is memory-less, only track the latest seed.
-    # Note: `manual_seed_all` followed by `manual_seed` overwrites
-    # the seed on current device. We track the order of **latest**
-    # calls between these two API.
-    def __init__(self):
-        self.manual_seed_all_cb = None
-        self.manual_seed_cb = None
-        self.call_order = []
-
-    def queue_seed_all(self, cb, traceback):
-        self.manual_seed_all_cb = (cb, traceback)
-        # update seed_all to be latest
-        self.call_order = [self.manual_seed_cb, self.manual_seed_all_cb]
-
-    def queue_seed(self, cb, traceback):
-        self.manual_seed_cb = (cb, traceback)
-        # update seed to be latest
-        self.call_order = [self.manual_seed_all_cb, self.manual_seed_cb]
-
-    def get_calls(self) -> List:
-        return self.call_order
-
-
-_lazy_seed_tracker = _LazySeedTracker()
 has_half: bool = True
 
 
@@ -92,51 +62,25 @@ def is_initialized():
     return _initialized and not _is_in_bad_fork()
 
 
-def _lazy_call(callable, **kwargs):
-    if is_initialized():
-        callable()
-    else:
-        global _lazy_seed_tracker
-        if kwargs.get("seed_all", False):
-            _lazy_seed_tracker.queue_seed_all(callable, traceback.format_stack())
-        elif kwargs.get("seed", False):
-            _lazy_seed_tracker.queue_seed(callable, traceback.format_stack())
-        else:
-            # Don't store the actual traceback to avoid memory cycle
-            _queued_calls.append((callable, traceback.format_stack()))
-
-
-class DeferredXpuCallError(Exception):
-    pass
-
-
 def init():
     r"""Initialize PyTorch's XPU state.
     This is a Python API about lazy initialization that avoids initializing
-    XPU until the first time it is accessed. You may need to call this function
-    explicitly in very rare cases, since PyTorch's XPU methods could call
-    this initialization automatically when XPU functionality is on-demand.
-    If you are interacting with PyTorch via its C APIs, please call this
-    explicitly since Python bindings for XPU functionality are unavailable.
-    Does nothing if the XPU state is already initialized.
+    XPU until the first time it is accessed. Does nothing if the XPU state is
+    already initialized.
     """
     _lazy_init()
 
 
 def _lazy_init():
-    global _initialized, _queued_calls
-    if is_initialized() or hasattr(_tls, "is_initializing"):
+    global _initialized
+    if is_initialized():
         return
     with _initialization_lock:
         # This test was was protected via GIL. Double-check whether XPU has
-        # already been initialized. If a thread acquired the lock first,
-        # it will do an initialization. When the other threads get the lock,
-        # they will find XPU has been initialized.
+        # already been initialized.
         if is_initialized():
             return
-        # It is important to prevent other threads from entering _lazy_init
-        # immediately, while we are still guaranteed to have the GIL, because some
-        # of the C calls we make below will release the GIL
+        # Stop promptly upon encountering a bad fork error.
         if _is_in_bad_fork():
             raise RuntimeError(
                 "Cannot re-initialize XPU in forked subprocess. To use XPU with "
@@ -144,30 +88,8 @@ def _lazy_init():
             )
         if not _is_compiled():
             raise AssertionError("Torch not compiled with XPU enabled")
-        # This function detects bad fork processing and throws if there's a device
-        # initialization error, no XPUs are found or any other error occurs
+        # This function inits XPU backend and detects bad fork processing.
         torch._C._xpu_init()
-        # Some of the queued calls in _queued_calls[] may reentrantly call
-        # _lazy_init(). We must prevent multiple initializations. In that case
-        # just return early without initializeing to avoid a deadlock.
-        _tls.is_initializing = True
-
-        for calls in _lazy_seed_tracker.get_calls():
-            if calls:
-                _queued_calls.append(calls)
-
-        try:
-            for queued_call, orig_traceback in _queued_calls:
-                try:
-                    queued_call()
-                except Exception as e:
-                    msg = (
-                        f"XPU call failed lazily at initialization with error: {str(e)}\n\n"
-                        f"XPU call was originally invoked at:\n\n{''.join(orig_traceback)}"
-                    )
-                    raise DeferredXpuCallError(msg) from e
-        finally:
-            delattr(_tls, "is_initializing")
         _initialized = True
 
 
@@ -305,7 +227,6 @@ def _get_device(device: Union[int, str, torch.device]) -> torch.device:
 
 
 __all__ = [
-    "DeferredXpuCallError",
     "current_device",
     "device",
     "device_of",
