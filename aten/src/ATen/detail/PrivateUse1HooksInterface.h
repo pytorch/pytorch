294cbdf4352e69a8a700045a8e584b6db36326b7
feifan <37650440+huihoaan@users.noreply.github.com>
2024-01-09 09:37:29 +0800

    cio

--- aten/src/ATen/detail/PrivateUse1HooksInterface.h
+++ aten/src/ATen/detail/PrivateUse1HooksInterface.h
@@ -28,7 +28,6 @@ TORCH_API void RegisterPrivateUse1HooksInterface(at::PrivateUse1HooksInterface* 
 
 TORCH_API at::PrivateUse1HooksInterface* GetPrivateUse1HooksInterface();
 
-// check whether PrivateUse1HooksInterface has been registered
 TORCH_API bool isPrivateUse1HooksRegistered();
 
 }
