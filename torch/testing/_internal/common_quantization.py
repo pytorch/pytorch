01873472b4070531b36d66041603ae84ac86e41e
Max Ren <maxren@meta.com>
2024-01-09 11:11:07 -0800

    [quant][pt2e][xnnpack_quantizer] add support for linear_relu
    Add support for linear_relu annotation for XNNPACKQuantizer, this allows the input to linear and the output to relu to share the same quantization parameter.s
    
    Differential Revision: [D52574086](https://our.internmc.facebook.com/intern/diff/D52574086/)
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_quantization.py
+++ torch/testing/_internal/common_quantization.py
@@ -2678,3 +2678,13 @@ class TestHelperModules:
 
         def example_inputs(self):
             return (torch.randn(2, 4, 10, 10),)
+
+    class LinearReluModel(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.fc = torch.nn.Linear(5, 5).to(dtype=torch.float)
+            self.relu = torch.nn.ReLU()
+
+        def forward(self, x):
+            x = self.relu(self.fc(x))
+            return x
