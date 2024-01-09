5acf5ff4b474051cbef9ee096bb3b2a818f5b534
David Chiu <david20571015@gmail.com>
2024-01-09 22:52:15 +0800

    fix: add interfaces to swa_utils.pyi

--- torch/optim/swa_utils.pyi
+++ torch/optim/swa_utils.pyi
@@ -1,9 +1,19 @@
-from typing import Any, Callable, Iterable, Union
+from typing import Any, Callable, Iterable, List, Tuple, Union
 
 from torch import device, Tensor
 from torch.nn.modules import Module
 from .lr_scheduler import _LRScheduler
 from .optimizer import Optimizer
+
+PARAM_LIST = Tuple[Tensor, ...] | List[Tensor]
+
+def get_ema_multi_avg_fn(decay: float) -> Callable[[PARAM_LIST, PARAM_LIST, Any], None]: ...
+
+def get_swa_multi_avg_fn() -> Callable[[PARAM_LIST, PARAM_LIST, int], None]: ...
+
+def get_ema_avg_fn(decay: float) -> Callable[[Tensor, Tensor, Any], Tensor]: ...
+
+def get_swa_avg_fn() -> Callable[[Tensor, Tensor, int], Tensor]: ...
 
 class AveragedModel(Module):
     def __init__(
@@ -11,6 +21,7 @@ class AveragedModel(Module):
         model: Module,
         device: Union[int, device] = ...,
         avg_fn: Callable[[Tensor, Tensor, int], Tensor] = ...,
+        multi_avg_fn: Callable[[PARAM_LIST, PARAM_LIST, int], None] = ...,
         use_buffers: bool = ...,
     ) -> None: ...
     def update_parameters(self, model: Module) -> None: ...
@@ -30,3 +41,4 @@ class SWALR(_LRScheduler):
         anneal_strategy: str,
         last_epoch: int = ...,
     ) -> None: ...
+    def get_lr(self) -> List[float]: ...
