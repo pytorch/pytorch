f71af2b8f41ad0e3e6e4aaa875eba946f5950f94
Yanbo Liang <ybliang8@gmail.com>
2024-01-10 18:36:12 -0800

    delta change

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1064,7 +1064,7 @@ def tensor_or_module_to_id(value):
 
 
 def const_repr(x, *, local) -> str:
-    from .trace_rules import is_builtin_callable
+    from .allowed_functions import is_builtin_callable
 
     if isinstance(x, (list, tuple)):
         elems_repr = ",".join(const_repr(s, local=local) for s in x)
