a35cac5ad7537261a4b2903a5935d65f2b552d24
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-01-11 02:30:22 +0000

    Revert "Add _assert_scalar and teach Inductor to codegen it (#114148)"
    This reverts commit b6028acfa46363c1d3262a1522741a06c307843f.
    
    Reverted https://github.com/pytorch/pytorch/pull/114148 on behalf of https://github.com/osalpekar due to Going to revert this given the broken torchrec PT2 tests internally: [D52648865](https://www.internalfb.com/diff/D52648865). Logs aren't too clear but @dstaay-fb can help debug as well ([comment](https://github.com/pytorch/pytorch/pull/114148#issuecomment-1886100368))

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -1254,10 +1254,7 @@ def forward(self, arg_0):
 
         ep = export(M(), (torch.tensor(1), torch.ones(4, 5)))
 
-        with self.assertRaisesRegex(
-            RuntimeError,
-            r"(Deferred runtime assertion failed -i0 <= 0|_local_scalar_dense is outside of inline constraint \[0, inf\])"
-        ):
+        with self.assertRaisesRegex(RuntimeError, r"Deferred runtime assertion failed -i0 <= 0"):
             _ = ep(torch.tensor(-1), torch.randn(4, 5))
 
         self.assertTrue(
