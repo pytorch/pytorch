811f8fd5e4563dc5461b36ad76734a43bccc1ce5
Jiong Gong <jiong.gong@intel.com>
2024-01-11 13:14:08 +0800

    [inductor][cpp] improve vector contiguous checks for FloorDiv and ModularIndexing
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -16,7 +16,7 @@ import torch.fx
 from torch._inductor import dependencies
 from torch._inductor.ir import StorageBox, TensorBox
 from torch._prims_common import is_float_dtype
-from torch.utils._sympy.functions import FloorDiv
+from torch.utils._sympy.functions import FloorDiv, ModularIndexing
 from torch.utils._sympy.value_ranges import bound_sympy, ValueRanges
 
 from .. import codecache, config, ir, metrics
@@ -309,6 +309,48 @@ def stride_at(var: sympy.Symbol, index: sympy.Expr):
     replacement = {var: var + 1}
     new_index = sympy_subs(index, replacement)
     return sympy.simplify(new_index - index)
+
+
+@functools.lru_cache
+def simplify_index_in_vec_range(index: sympy.Expr, var: sympy.Expr, vec_length: int):
+    """
+    Simplify the index expression in the range of the vectorized loop.
+    """
+
+    def collect(expr, sub_expr, collected, filter=None):
+        m = expr.match(sub_expr)
+        if m is not None:
+            if filter is None or filter(m):
+                collected[sub_expr.xreplace(m)] = m
+            return
+        for arg in expr.args:
+            collect(arg, sub_expr, collected, filter)
+
+    div = sympy.Wild("divisor")
+    floor_divs: Dict[sympy.Expr, Dict[sympy.Expr, sympy.Expr]] = {}
+    collect(index, FloorDiv(var, div), floor_divs, lambda m: m[div] % vec_length == 0)
+    for i, floor_div in enumerate(floor_divs):
+        freevar = sympy.Symbol(f"{var}_div_c{i}")
+        index = index.xreplace({floor_div: freevar})
+
+    mod = sympy.Wild("modulus")
+    modulars: Dict[sympy.Expr, Dict[sympy.Expr, sympy.Expr]] = {}
+    collect(
+        index,
+        ModularIndexing(var, div, mod),
+        modulars,
+        lambda m: m[div] % vec_length == 0
+        or (m[div] == 1 and m[mod] % vec_length == 0),
+    )
+    for i, modular in enumerate(modulars):
+        freevar = sympy.Symbol(f"{var}_mod_c{i}")
+        m = modulars[modular]
+        if m[div] % vec_length == 0:
+            index = index.xreplace({modular: freevar})
+        else:
+            index = index.xreplace({modular: var + freevar})
+
+    return sympy.simplify(index)
 
 
 class CppPrinter(ExprPrinter):
@@ -1319,12 +1361,15 @@ class CppVecOverrides(CppOverrides):
         assert isinstance(V.kernel, CppVecKernel)
         index = V.kernel.rename_indexing(expr)
         tiling_var = V.kernel.itervars[V.kernel.tiling_idx]
-        if V.kernel.index_is_vector_invariant(index):
-            return CppOverrides.index_expr(expr, dtype)
-        if stride_at(
-            tiling_var, index
-        ).is_number and not V.kernel.index_indirect_depends_on(index, tiling_var):
-            stride = stride_at(tiling_var, index)
+        index_vec_simplified = simplify_index_in_vec_range(
+            index, tiling_var, V.kernel.tiling_factor
+        )
+        stride = stride_at(tiling_var, index_vec_simplified)
+        if stride.is_number and not V.kernel.index_indirect_depends_on(
+            index, tiling_var
+        ):
+            if stride == 0:
+                return CppOverrides.index_expr(expr, dtype)
             value = ops.to_dtype(cexpr(index), dtype)
             if isinstance(value, OpsValue):
                 value = value.value
@@ -1675,18 +1720,6 @@ class CppVecKernel(CppKernel):
         self.tiling_idx = tiling_idx
         metrics.generated_cpp_vec_kernel_count += 1
 
-    def index_is_vector_invariant(self, index: sympy.Expr):
-        """`index` is either independent from the tiling itervar or unchanged in the vector range"""
-        tiling_var = self.itervars[self.tiling_idx]
-        if not self.index_depends_on(index, tiling_var):
-            return True
-        if not self.index_indirect_depends_on(index, tiling_var):
-            vec_range = [
-                sympy_subs(index, {tiling_var: i}) for i in range(self.tiling_factor)
-            ]
-            return all(expr == vec_range[0] for expr in vec_range)
-        return False
-
     def _get_vec_load_line(
         self,
         var: str,
@@ -1847,12 +1880,16 @@ class CppVecKernel(CppKernel):
         index = self.rename_indexing(index)
         dtype = V.graph.get_dtype(name)
         tiling_var = self.itervars[self.tiling_idx]
-        if self.index_is_vector_invariant(index):
+        index_vec_simplified = simplify_index_in_vec_range(
+            index, tiling_var, self.tiling_factor
+        )
+        stride = stride_at(tiling_var, index_vec_simplified)
+        if stride == 0:
             # load scalar and lazily broadcast it on demand
             return super().load(name, index)
-        non_contiguous = stride_at(
-            tiling_var, index
-        ) != 1 or self.index_indirect_depends_on(index, tiling_var)
+        non_contiguous = stride != 1 or self.index_indirect_depends_on(
+            index, tiling_var
+        )
         if non_contiguous:
             csevar = self.load_non_contiguous(var, index, dtype)
         else:
