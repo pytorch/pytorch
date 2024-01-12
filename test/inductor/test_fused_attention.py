c8dd2a4b85c8806b151996967ef6737b0e84af35
Valentine233 <xuan.liao@intel.com>
2024-01-11 17:01:15 -0800

    Add UT for test_sdpa_rewriter_15_cuda

--- test/inductor/test_fused_attention.py
+++ test/inductor/test_fused_attention.py
@@ -690,7 +690,9 @@ if HAS_CUDA and PLATFORM_SUPPORTS_FUSED_ATTENTION:
         test_sdpa_rewriter_14_cuda = functools.partialmethod(
             TestSDPAPatternRewriterTemplate._test_sdpa_rewriter_14
         )
-        # TODO test_sdpa_rewriter_15 for cuda failed pattern match, need to be enabled
+        test_sdpa_rewriter_15_cuda = functools.partialmethod(
+            TestSDPAPatternRewriterTemplate._test_sdpa_rewriter_15
+        )
 
     class SDPAPatternRewriterCudaDynamicTests(SDPAPatternRewriterCudaTests):
         use_static_shapes = False
