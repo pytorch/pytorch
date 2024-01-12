2e3802f88ecd1a2a55be76373f9be812fc8c07db
BowenBao <bowbao@microsoft.com>
2024-01-11 16:55:54 -0800

    lint that was somehow missed on "[ONNX][dynamo_export] Decomposition skips using custom operator"
    A context manager that disables the decomposition of certain ops during dynamo tracing.
    
    The approach is to temporarily hijack the operator callable with PT2 custom operator.
    The custom operator will not be decomposed and will show up as a single node to be exported to ONNX.
    
    For the time being the decomposition of these ops is otherwise unavoidable.
    
    https://github.com/pytorch/pytorch/issues/116684
    https://github.com/pytorch/pytorch/issues/115883
    
    This solution will no longer be required once the issue is resolved.
    
    [ghstack-poisoned]

--- test/onnx/test_fx_to_onnx_decomp_skip.py
+++ test/onnx/test_fx_to_onnx_decomp_skip.py
@@ -1,11 +1,12 @@
 # Owner(s): ["module: onnx"]
 from __future__ import annotations
 
-import torch
-import pytorch_test_common
-
 import onnx
 import onnx.inliner
+import pytorch_test_common
+
+import torch
+
 
 def assert_op_in_onnx_model(model: onnx.ModelProto, op_type: str):
     inlined = onnx.inliner.inline_local_functions(model)
@@ -14,16 +15,17 @@ def assert_op_in_onnx_model(model: onnx.ModelProto, op_type: str):
             return
     raise AssertionError(f"Op {op_type} not found in model")
 
+
 class TestDynamoExportDecompSkip(pytorch_test_common.ExportTestCase):
     def test_upsample_bilinear2d(self):
         class TestModel(torch.nn.Module):
             def __init__(self):
                 super().__init__()
                 self.upsample = torch.nn.Upsample(scale_factor=2, mode="bilinear")
-            
+
             def forward(self, x):
                 return self.upsample(x)
-            
+
         onnx_program = torch.onnx.dynamo_export(TestModel(), torch.randn(1, 1, 2, 2))
         # If decomposition is skipped, the model will contain a Resize op instead of fine grained subgraph.
         assert_op_in_onnx_model(onnx_program.model_proto, "Resize")
--- torch/onnx/_internal/fx/decomposition_skip.py
+++ torch/onnx/_internal/fx/decomposition_skip.py
@@ -12,13 +12,14 @@ This solution will no longer be required once the issue is resolved.
 """
 from __future__ import annotations
 
-from typing import Callable, Sequence
-
 import abc
-import torch
 import contextlib
 
+from typing import Callable, Sequence
+
 from onnxscript.function_libs.torch_lib.ops import nn as torchlib_nn
+
+import torch
 
 
 class DecompSkip:
@@ -32,20 +33,20 @@ class DecompSkip:
     """The name for the custom operator."""
     new_op_schema: str
     """The schema for the custom operator. This should match with the signature of the original operator."""
-    
+
     @classmethod
     @abc.abstractmethod
     def register(cls, export_options: torch.onnx.ExportOptions):
         """Registers the custom operator and overrides the original operator.
-        
+
         It should do the following steps in order:
-        
+
         1. Register the custom operator.
         2. Override the original operator with the replacement callable.
         3. Register the ONNXScript function for exporting the custom operator.
         """
         ...
-    
+
     @classmethod
     @abc.abstractmethod
     def unregister(cls):
@@ -55,8 +56,7 @@ class DecompSkip:
     @classmethod
     @abc.abstractmethod
     def abstract(cls, *args, **kwargs):
-        """An abstract impl (meta kernel) for the operator.
-        """
+        """An abstract impl (meta kernel) for the operator."""
         ...
 
     @classmethod
@@ -70,7 +70,7 @@ class DecompSkip:
     @classmethod
     def replacement(cls, *args, **kwargs):
         """A replacement callable for the operator to be hijacked.
-        
+
         This has the same signature and eager behavior as the original operator.
         """
         return cls.op_callable(*args, **kwargs)
@@ -90,7 +90,11 @@ class UpsampleBilinear2DDecompSkip(DecompSkip):
         if export_options.onnx_registry is None:
             export_options.onnx_registry = torch.onnx.OnnxRegistry()
         registry = export_options.onnx_registry
-        registry.register_op(function=cls.onnxscript_function, namespace=cls.new_op_namespace, op_name=cls.new_op_name)
+        registry.register_op(
+            function=cls.onnxscript_function,
+            namespace=cls.new_op_namespace,
+            op_name=cls.new_op_name,
+        )
 
     @classmethod
     def unregister(cls):
@@ -103,17 +107,25 @@ class UpsampleBilinear2DDecompSkip(DecompSkip):
         else:
             h = int(input.size(2) * scale_factors[0])
             w = int(input.size(3) * scale_factors[1])
-            return torch.empty((input.size(0), input.size(1), h, w), dtype=input.dtype, device=input.device)
-        
+            return torch.empty(
+                (input.size(0), input.size(1), h, w),
+                dtype=input.dtype,
+                device=input.device,
+            )
+
 
 _DEFAULT_SKIP_LIST = [
     UpsampleBilinear2DDecompSkip,
 ]
 
+
 @contextlib.contextmanager
-def enable_decomposition_skips(export_options: torch.onnx.ExportOptions, skips: Sequence[DecompSkip] = _DEFAULT_SKIP_LIST):
-    """A context manager that enables the decomposition skips. 
-    
+def enable_decomposition_skips(
+    export_options: torch.onnx.ExportOptions,
+    skips: Sequence[DecompSkip] = _DEFAULT_SKIP_LIST,
+):
+    """A context manager that enables the decomposition skips.
+
     The original operator callables that are otherwise be decomposed are replaced with custom operators.
     The ONNXScript functions for exporting the custom operators are added to the ONNX registry inside export_options.
     """
