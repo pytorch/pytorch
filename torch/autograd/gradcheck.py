fix function name: vjp, not jacobian

torch/autograd/gradcheck.py
@@ -754,8 +754,8 @@ def _check_analytical_jacobian_attributes(
     inputs, output, nondet_tol, check_grad_dtypes, fast_mode=False, v=None
 ) -> Tuple[torch.Tensor, ...]:
     # This is used by both fast and slow mode:
-    #  - For slow mode, vjps[i][j] is the jth row the Jacobian wrt the ith
-    #    input.
+    #  - For slow mode, vjps[i][j] is the jth component of grad_output
+    #    multiplied by jth row of Jacobian wrt the ith input
     #  - For fast mode, vjps[i][0] is a linear combination of the rows
     #    of the Jacobian wrt the ith input
     diff_input_list = list(_iter_tensors(inputs, True))
@@ -770,8 +770,8 @@ def _check_analytical_jacobian_attributes(
         vjps1 = _get_analytical_vjps_wrt_specific_output(vjp_fn, output.clone(), v)
         vjps2 = _get_analytical_vjps_wrt_specific_output(vjp_fn, output.clone(), v)
     else:
-        vjps1 = _compute_analytical_jacobian_rows(vjp_fn, output.clone())
-        vjps2 = _compute_analytical_jacobian_rows(vjp_fn, output.clone())
+        vjps1 = _compute_analytical_vjp(vjp_fn, output.clone())
+        vjps2 = _compute_analytical_vjp(vjp_fn, output.clone())
 
     output_numel = output.numel() if not fast_mode else 1
     jacobians1, types_ok, sizes_ok = _stack_and_check_tensors(
@@ -870,10 +870,10 @@ def _get_analytical_jacobian(inputs, outputs, input_idx, output_idx):
     return jacobians[input_idx]
 
 
-def _compute_analytical_jacobian_rows(
+def _compute_analytical_vjp(
     vjp_fn, sample_output
 ) -> List[List[Optional[torch.Tensor]]]:
-    # Computes Jacobian row-by-row using backward function `vjp_fn` = v^T J
+    # Computes vector jacobian product components using backward function `vjp_fn` = v^T J
     # NB: this function does not assume vjp_fn(v) to return tensors with the same
     # number of elements for different v. This is checked when we later combine the
     # rows into a single tensor.
@@ -881,19 +881,20 @@ def _compute_analytical_jacobian_rows(
         sample_output, memory_format=torch.legacy_contiguous_format
     )
     flat_grad_out = grad_out_base.view(-1)
-    # jacobians_rows[i][j] represents the jth row of the ith input
-    jacobians_rows: List[List[Optional[torch.Tensor]]] = []
+    # vjp_rows[i][j] is jacobian for the jth row of the ith input multiplied by the jth component of grad_output.
+    # eg. summing vjp_rows[i][j] gives the ith component of the vector jacobian product.
+    vjp_rows: List[List[Optional[torch.Tensor]]] = []
     for j in range(flat_grad_out.numel()):
         flat_grad_out.zero_()
         flat_grad_out[j] = 1.0
         grad_inputs = vjp_fn(grad_out_base)
         for i, d_x in enumerate(grad_inputs):
             if j == 0:
-                jacobians_rows.append([])
-            jacobians_rows[i] += [
+                vjp_rows.append([])
+            vjp_rows[i] += [
                 d_x.clone() if isinstance(d_x, torch.Tensor) else None
             ]
-    return jacobians_rows
+    return vjp_rows
 
 
 def _get_analytical_vjps_wrt_specific_output(