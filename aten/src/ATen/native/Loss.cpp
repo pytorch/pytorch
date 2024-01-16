ef1375729c3704727bd1620e4d9bf24291fdfdb1
Qingpeng Li <43924785+qingpeng9802@users.noreply.github.com>
2024-01-17 02:42:00 +0800

    eliminate `neg_`

--- aten/src/ATen/native/Loss.cpp
+++ aten/src/ATen/native/Loss.cpp
@@ -363,9 +363,9 @@ Tensor binary_cross_entropy_with_logits(const Tensor& input, const Tensor& targe
   if (pos_weight.defined()) {
       // pos_weight need to be broadcasted, thus mul(target) is not inplace.
       auto log_weight = (pos_weight - 1).mul(target).add_(1);
-      loss = (1 - target).mul_(input).add_(log_weight.mul_(at::log_sigmoid(input).neg_()));
+      loss = (1 - target).mul_(input).sub_(log_weight.mul_(at::log_sigmoid(input)));
   } else {
-      loss = (1 - target).mul_(input).add_(at::log_sigmoid(input).neg_());
+      loss = (1 - target).mul_(input).sub_(at::log_sigmoid(input));
   }
 
   if (weight.defined()) {
