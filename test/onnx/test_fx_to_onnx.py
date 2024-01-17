28ebb66252e754b5b8bf0a3bcd05b64a31d5a88f
titaiwangms <titaiwang@microsoft.com>
2024-01-17 08:05:32 +0000

    [Reland][ONNX] Guard xfail tests with error messages (#117592)
    Reland #117425
    
    Previous to this PR, xfail tests didn't provide (1) guarantee of error message/reason (could be outdated), and (2) execution of the test (xfail_if_model_type_is_not_exportedprogram). Therefore, the tests are less robust with xfail labeled, as we can't be sure if it's still failing with the same reason, and if it's even still failing. This PR fixes the issue with try/except with error message matching to consolidate the xfail truth and reason.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117592
    Approved by: https://github.com/BowenBao

--- test/onnx/test_fx_to_onnx.py
+++ test/onnx/test_fx_to_onnx.py
@@ -580,8 +580,9 @@ class TestFxToOnnx(pytorch_test_common.ExportTestCase):
             onnx.shape_inference.infer_shapes(onnx_program.model_proto)
 
     @pytorch_test_common.xfail(
-        "This is addressed in main branch of transformers."
-        "https://github.com/huggingface/transformers/pull/24941"
+        error_message="aten._local_scalar_dense.default",
+        reason="This is addressed in main branch of transformers."
+        "https://github.com/huggingface/transformers/pull/24941",
     )
     def test_fake_tensor_mode_huggingface_databricks_dolly_v2_3b(self):
         config = transformers.GPTNeoXConfig(
@@ -608,9 +609,7 @@ class TestFxToOnnx(pytorch_test_common.ExportTestCase):
             onnx.shape_inference.infer_shapes(onnx_program.model_proto)
 
     @pytorch_test_common.xfail(
-        "Not decorated with xfail because CI doesn't have enough memory to run and then fail."
-        "AssertionError: Mutating module attribute seq_len_cached during export."
-        "self.seq_len_cached = seq_len"
+        error_message="Mutating module attribute seq_len_cached during export."
     )
     def test_fake_tensor_mode_huggingface_tiiuae_falcon(self):
         config = transformers.FalconConfig()
