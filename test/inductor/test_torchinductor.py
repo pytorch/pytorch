def4959662a23bc307fa961a912683beb9dd60f7
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-01-18 18:53:37 +0000

    Revert "[inductor] allow mm template to accumulate with float16 dtype (#117479)"
    This reverts commit a7fbbc2a4a05fa4863f9d0e2adabcdc5e276c675.
    
    Reverted https://github.com/pytorch/pytorch/pull/117479 on behalf of https://github.com/PaliC due to breaking tests internally ([comment](https://github.com/pytorch/pytorch/pull/117479#issuecomment-1899032973))

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2131,8 +2131,6 @@ class CommonTemplate:
                 torch.randint(-128, 127, (8, 8), dtype=torch.int8),
             ),
             check_lowp=True,
-            atol=5e-3,
-            rtol=5e-3,
         )
 
     @config.patch(force_mixed_mm=True)
@@ -2149,8 +2147,6 @@ class CommonTemplate:
                 torch.randn(8),
             ),
             check_lowp=True,
-            atol=5e-3,
-            rtol=5e-3,
         )
 
     @config.patch(use_mixed_mm=True)
