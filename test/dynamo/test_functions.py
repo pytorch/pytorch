3af1e0d65666bf49175d4720016dfabf3d18d9c8
laith sakka <lsakka@fb.com>
2024-01-18 08:27:48 -0800

    repo bug

--- test/dynamo/test_functions.py
+++ test/dynamo/test_functions.py
@@ -1693,6 +1693,28 @@ class FunctionTests(torch._dynamo.test_case.TestCase):
                 opt_fn = torch._dynamo.optimize(nopython=True)(fn)
                 self.assertEqual(opt_fn(), fn())
 
+    def test_unary_functions(self):
+        for op in (abs, operator.abs):
+            with self.subTest(op=op):
+                print("iteration %s" % op)
+                def fn(x, y):
+                    return x * op(y)
+
+                arg = torch.ones(4)*4
+                opt_fn = torch._dynamo.optimize(nopython=True)(fn)
+                print("tensor*abs(constant)")
+
+                # This fail on second iteration.
+                # tensor([-2., -2., -2., -2.]) VS tensor([2., 2., 2., 2.])
+                print(opt_fn(arg, -2), fn(arg, -2))
+
+                self.assertEqual(opt_fn(arg, -2), fn(arg, -2))
+
+                print("constant*abs(constant)")
+
+                # if we remove this the tests pass!
+                self.assertEqual(opt_fn(-2, -3), fn(-2, -3))
+
 
 def udf_mul(x, y):
     return x * y
