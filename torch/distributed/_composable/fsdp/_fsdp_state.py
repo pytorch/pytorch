213da21ede7ac72e2b40e3917482cc7285f1855f
Andrew Gu <andgu@fb.com>
2024-01-18 16:51:28 -0800

    Update on "[FSDP2] Added initial `device` argument and `FSDPState`"
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_state.py
+++ torch/distributed/_composable/fsdp/_fsdp_state.py
@@ -5,7 +5,7 @@ from torch.distributed._composable_state import _State
 
 
 class FSDPState(_State):
-    _module: nn.Module  # ref cycle
+    _module: nn.Module  # permit ref cycle since module and state lifetimes are 1:1
 
     def __init__(self):
         super().__init__()
