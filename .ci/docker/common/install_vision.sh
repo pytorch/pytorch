db693bd8fc86e3058c67560fcc68eefae8c177e6
atalman <atalman@fb.com>
2024-01-19 06:56:14 -0800

    Add vision install

--- .ci/docker/common/install_vision.sh
+++ .ci/docker/common/install_vision.sh
@@ -29,10 +29,6 @@ install_centos() {
   rm -rf /var/lib/yum/history
 }
 
-if [[ $ANACONDA_PYTHON_VERSION == "3.12" ]]; then
-  exit
-fi
-
 # Install base packages depending on the base OS
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 case "$ID" in
