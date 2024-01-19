9503319883fd2141df8109d154c4b9431dc369b6
Andrew Gu <andgu@fb.com>
2024-01-18 17:23:33 -0800

    Update base for Update on "[FSDP2] Added initial `mesh` arg and helper structures"
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_state.py
+++ torch/distributed/_composable/fsdp/_fsdp_state.py
@@ -6,7 +6,7 @@ from torch.distributed._composable_state import _State
 
 class FSDPState(_State):
     _module: nn.Module  # permit ref cycle since module and state lifetimes are 1:1
+    _device: torch.device
 
     def __init__(self):
         super().__init__()
-        self._device = torch.device("cpu")
