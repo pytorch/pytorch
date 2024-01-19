1e05b8fe3b329dbea1d771d271361d11adc2ceba
Andrew Gu <andgu@fb.com>
2024-01-19 13:40:08 -0800

    Update on "[FSDP2] Added initial `_lazy_init` and FQNs for debugging"
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    [ghstack-poisoned]

--- test/distributed/_composable/fsdp/test_fully_shard_init.py
+++ test/distributed/_composable/fsdp/test_fully_shard_init.py
@@ -378,5 +378,97 @@ class TestFullyShardShardedParameterDTensor(FSDPTestMultiThread):
             self.assertEqual(sharded_param._spec.placements, expected_placements)
 
 
+class TestFullyShardLazyInit(FSDPTestMultiThread):
+    @property
+    def world_size(self) -> int:
+        return 2
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_fully_shard_is_root(self):
+        """
+        Tests that ``is_root`` is set correctly after lazy initialization.
+
+        FSDP(model(
+            0: MLP(FSDP(in_proj), FSDP(out_proj)),
+            1: MLP(in_proj, out_proj),
+        ))
+        """
+        model = nn.Sequential(MLP(8), MLP(8))
+        fully_shard(model[0].in_proj)
+        fully_shard(model[0].out_proj)
+        fully_shard(model)  # root gets `model[1]`
+        root_state = fully_shard.state(model)
+        root_state._lazy_init()
+
+        model0_in_proj_state = fully_shard.state(model[0].in_proj)
+        model0_out_proj_state = fully_shard.state(model[0].out_proj)
+        self.assertTrue(root_state._is_root)
+        self.assertFalse(model0_in_proj_state._is_root)
+        self.assertFalse(model0_out_proj_state._is_root)
+
+        all_states = [ref() for ref in root_state._all_state_refs]
+        self.assertEqual(len(all_states), 3)
+        self.assertEqual(all_states, [root_state, model0_in_proj_state, model0_out_proj_state])
+
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_fully_shard_module_and_param_fqns(self):
+        """
+        Tests that the module and parameter FQNs are computed correctly after
+        lazy initialization.
+
+        FSDP(model(
+            0: MLP(FSDP(in_proj), FSDP(out_proj)),
+            1: MLP(in_proj, out_proj),
+        ))
+        """
+        model = nn.Sequential(MLP(8), MLP(8))
+        fully_shard(model[0].in_proj)
+        fully_shard(model[0].out_proj)
+        fully_shard(model)  # root gets `model[1]`
+        root_state = fully_shard.state(model)
+        root_state._lazy_init()
+
+        root_param_group = root_state._fsdp_param_group
+        self.assertIsNotNone(root_param_group)
+        self.assertEqual(root_param_group._module_fqn, "")
+        root_param_fqns = {
+            fsdp_param._param_fqn for fsdp_param in root_param_group.fsdp_params
+        }
+        self.assertEqual(
+            root_param_fqns,
+            {
+                "1.in_proj.weight",
+                "1.in_proj.bias",
+                "1.out_proj.weight",
+                "1.out_proj.bias",
+            },
+        )
+
+        model0_in_proj_state = fully_shard.state(model[0].in_proj)
+        model0_in_proj_param_group = model0_in_proj_state._fsdp_param_group
+        self.assertIsNotNone(model0_in_proj_param_group)
+        self.assertEqual(model0_in_proj_param_group._module_fqn, "0.in_proj")
+        model0_in_proj_param_fqns = {
+            fsdp_param._param_fqn
+            for fsdp_param in model0_in_proj_param_group.fsdp_params
+        }
+        self.assertEqual(
+            model0_in_proj_param_fqns, {"0.in_proj.weight", "0.in_proj.bias"}
+        )
+
+        model0_out_proj_state = fully_shard.state(model[0].out_proj)
+        model0_out_proj_param_group = model0_out_proj_state._fsdp_param_group
+        self.assertIsNotNone(model0_out_proj_param_group)
+        self.assertEqual(model0_out_proj_param_group._module_fqn, "0.out_proj")
+        model0_out_proj_param_fqns = {
+            fsdp_param._param_fqn
+            for fsdp_param in model0_out_proj_param_group.fsdp_params
+        }
+        self.assertEqual(
+            model0_out_proj_param_fqns, {"0.out_proj.weight", "0.out_proj.bias"}
+        )
+
+
 if __name__ == "__main__":
     run_tests()
