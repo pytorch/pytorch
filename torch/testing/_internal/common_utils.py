e402806bc9d230c8158c504cef90af912bd77bea
Catherine Lee <csl@fb.com>
2024-01-19 16:13:22 -0800

    update

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -834,8 +834,8 @@ def wait_for_process(p, timeout=None):
         p.send_signal(signal.SIGINT)
         exit_status = None
         try:
-            exit_status = p.wait(timeout=30)
-        # try to handle the case where p.wait(timeout=10) times out as well as
+            exit_status = p.wait(timeout=15)
+        # try to handle the case where p.wait(timeout=15) times out as well as
         # otherwise the wait() call in the finally block can potentially hang
         except subprocess.TimeoutExpired:
             pass
