58c5974d81af3b47290d9c61ab388c379dc97f4a
voznesenskym <voznesenskym@gmail.com>
2024-01-19 17:02:14 -0800

    Update base for Update on "Fixing up Jon's 111436 - fix dynamo subgraph shattering over graph breaks with better reconstruction"
    cc penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -117,12 +117,8 @@ class ConstDictVariable(VariableTracker):
 
         def __eq__(self, other: "ConstDictVariable._HashableTracker") -> bool:
             Hashable = ConstDictVariable._HashableTracker
-            assert isinstance(other, Hashable) or ConstantVariable.is_literal(other)
-            if isinstance(other, Hashable):
-                return Hashable._eq_impl(self.underlying_value, other.underlying_value)
-
-            # constant
-            return Hashable._eq_impl(self.underlying_value, other)
+            assert isinstance(other, Hashable)
+            return Hashable._eq_impl(self.underlying_value, other.underlying_value)
 
     def __init__(
         self, items: Dict[VariableTracker, VariableTracker], user_cls=dict, **kwargs
