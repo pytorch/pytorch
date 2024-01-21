0db53bb4f9283cc0750c0eb370d98c22200ee71b
suo <suo@fb.com>
2024-01-21 14:15:25 -0800

    [export] set proper fqn in lift constant tensor pass (#115222)
    See comments: previously we were populating the lifted constant in the buffer list without an FQN, which messed up unflattening.
    
    Differential Revision: [D50568062](https://our.internmc.facebook.com/intern/diff/D50568062/)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115222
    Approved by: https://github.com/tugsbayasgalan

--- torch/export/unflatten.py
+++ torch/export/unflatten.py
@@ -36,6 +36,10 @@ def _assign_attr(
             setattr(to_module, item, t)
         to_module = t
 
+    if isinstance(from_obj, torch.ScriptObject):
+        setattr(to_module, field, from_obj)
+        return
+
     # If it is a tensor and not a parameter attribute of a module, it should be a named buffer.
     # So, we register it as a named buffer in the target module.
     if not isinstance(from_obj, torch.Tensor):
@@ -167,10 +171,21 @@ class UnflattenedModule(torch.nn.Module):
                 name,
                 is_parameter=False,
             )
+        for fqn in self.graph_signature.inputs_to_lifted_tensor_constants.values():
+            constant = export_module.tensor_constants[fqn]
+            if isinstance(constant, torch.Tensor):
+                constant = constant.clone()
+            _assign_attr(
+                constant,
+                self,
+                fqn,
+                is_parameter=False,
+            )
 
         inputs_to_state: Dict[str, str] = {
             **self.graph_signature.inputs_to_parameters,
             **self.graph_signature.inputs_to_buffers,
+            **self.graph_signature.inputs_to_lifted_tensor_constants,
         }
 
         _sink_params(self, inputs_to_state, [])
@@ -737,7 +752,7 @@ def _sink_params(
     inputs_to_state: Dict[str, str],
     scope: List[str],
 ):
-    """Sink params and buffers from graph inputs into get_attr nodes.
+    """Sink params, buffers, and constants from graph inputs into get_attr nodes.
 
     Exported modules are purely functional, so they pass their parameters and
     buffers in as inputs to the graph.
@@ -785,7 +800,7 @@ def _sink_params(
                 continue
             attr_path = state_name[len(scope) :]
             state_attr = _recursive_getattr(module, attr_path)
-            assert isinstance(state_attr, torch.Tensor)
+            assert isinstance(state_attr, (torch.Tensor, torch.ScriptObject))
 
             # Make sure the newly created get_attr node is placed after the last placeholder node
             with graph.inserting_after(the_last_input):
