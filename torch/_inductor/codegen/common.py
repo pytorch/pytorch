adba41bfc6dd98c846782c36eebcb0cf1e9e3aef
Edward Z. Yang <ezyang@meta.com>
2024-01-22 09:09:46 -0800

    Rename unbacked SymInt prefix to u (#117859)
    Currently, it conflicts with Inductor's naming convention for index
    variables
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117859
    Approved by: https://github.com/lezcano, https://github.com/jansel, https://github.com/avikchaudhuri

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -1315,8 +1315,7 @@ class Kernel(CodeGen):
         replacements = {
             x: self.args.size(x)
             for x in sorted_symbols
-            if x.name.startswith("s")
-            or x.name.startswith("ps")
+            if x.name.startswith(("s", "u", "ps"))
             or (x.name.startswith("i") and not x.name.startswith("idx"))
         }
         return sympy_subs(index, replacements)
