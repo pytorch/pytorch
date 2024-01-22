6f9db0c2d6d8793e89982f6ff507e9592ea74857
Feng Yuan <feng1.yuan@intel.com>
2024-01-23 03:34:10 +0800

    SYCL building system The first step of #114835. See the RFC for detials.
    Main parts of the PR include,
    1. Find SYCL compiler tool kit in cmake. (`cmake/Module/FindSYCLToolkit.cmake`)
    2. Setup cmake variables for SYCL compilation flags, includes and runtime library. (`cmake/public/xpu.cmake`, `cmake/Dependencies.cmake`)
    3. Implement two cmake custom target building helpers, `sycl_add_library` and `sycl_add_executable`. (`cmake/Module/FindSYCL.cmake`, `cmake/Module/FindSYCL/run_sycl.cmake`)
    4. Provide unit test cases to verify SYCL building system, since there is no SYCL kernel added in PyTorch. (`test/cpp/sycl/CMakeLists.txt`)
    
    CUDA counterparts:
    `cmake/Modules/FindCUDAToolkit.cmake` - `cmake/Module/FindSYCLToolkit.cmake`
    `cmake/public/cuda.cmake` - `cmake/public/xpu.cmake`
    `cmake/Modules_CUDA_fix/upstream/FindCUDA.cmake` - `cmake/Module/FindSYCL.cmake`
    `cmake/Modules_CUDA_fix/upstream/FindCUDA/run_nvcc.cmake` - `cmake/Module/FindSYCL/run_sycl.cmake`
    
    SYCL program building is similar as CUDA and HIP. SYCL program requires custom compilation and additional device object linkage steps. We implement `sycl_add_library` and `sycl_add_excutable` to introduce SYCL compiler,
    1. Introduce SYCL compiler to compile `.cpp`, in which we have SYCL kernels. As to "host only" `.cpp`, we use CXX compiler for compilation. So far, we only enable GCC compiler in SYCL separate compilation. Next step we will enable CLANG compiler to align with PyTorch building requirement.
    2. Introduce SYCL compiler to link device object and produce a host relocatable object which could be linked by host linker in subsequence.
    
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>
    
    [ghstack-poisoned]

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -93,16 +93,6 @@ if(USE_CUDA)
   endif()
 endif()
 
-# ---[ XPU
-if(USE_XPU)
-  include(${CMAKE_CURRENT_LIST_DIR}/public/xpu.cmake)
-  if(NOT PYTORCH_FOUND_XPU)
-    # message(WARNING "Not compiling with XPU. Could NOT find SYCL."
-    # "Suppress this warning with -DUSE_XPU=OFF.")
-    caffe2_update_option(USE_XPU OFF)
-  endif()
-endif()
-
 # ---[ Custom Protobuf
 if(CAFFE2_CMAKE_BUILDING_WITH_MAIN_REPO AND NOT INTERN_BUILD_MOBILE)
   disable_ubsan()
@@ -1338,6 +1328,61 @@ if(USE_ROCM AND ROCM_VERSION_DEV VERSION_LESS "5.2.0")
   include_directories(SYSTEM ${HIPSPARSE_PATH}/include)
   include_directories(SYSTEM ${HIPRAND_PATH}/include)
   include_directories(SYSTEM ${THRUST_PATH})
+endif()
+
+# ---[ XPU
+if(USE_XPU)
+  include(${CMAKE_CURRENT_LIST_DIR}/public/xpu.cmake)
+  # Support GCC only in the moment.
+  if(PYTORCH_FOUND_XPU AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    # # -- Host flags (SYCL_CXX_FLAGS)
+    # Combine with torch_compile_options
+    list(APPEND SYCL_HOST_FLAGS -fPIC)
+    list(APPEND SYCL_HOST_FLAGS -std=c++17)
+    # SYCL headers warnings
+    list(APPEND SYCL_HOST_FLAGS -Wno-deprecated-declarations)
+    list(APPEND SYCL_HOST_FLAGS -Wno-attributes)
+
+    if(CMAKE_BUILD_TYPE MATCHES Debug)
+       list(APPEND SYCL_HOST_FLAGS -g)
+       list(APPEND SYCL_HOST_FLAGS -O0)
+    endif(CMAKE_BUILD_TYPE MATCHES Debug)
+
+    # -- Kernel flags (SYCL_KERNEL_OPTIONS)
+    # The fast-math will be enabled by default in SYCL compiler.
+    # Refer to [https://clang.llvm.org/docs/UsersManual.html#cmdoption-fno-fast-math]
+    # 1. We enable below flags here to be warn about NaN and Infinity,
+    # which will be hidden by fast-math by default.
+    # 2. The associative-math in fast-math allows floating point
+    # operations to be reassociated, which will lead to non-deterministic
+    # results compared with CUDA backend.
+    # 3. The approx-func allows certain math function calls (such as log, sqrt, pow, etc)
+    # to be replaced with an approximately equivalent set of instructions or
+    # alternative math function calls, which have great errors.
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fno-sycl-unnamed-lambda)
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -sycl-std=2020)
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fhonor-nans)
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fhonor-infinities)
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fno-associative-math)
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fno-approx-func)
+    # TODO: Align with PyTorch and switch to ABI=0 eventually, after
+    # resolving incompatible implementation in SYCL runtime.
+    set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -D_GLIBCXX_USE_CXX11_ABI=1)
+    set(SYCL_FLAGS ${SYCL_FLAGS} ${SYCL_KERNEL_OPTIONS})
+
+    if(BUILD_TEST)
+      add_subdirectory(${PROJECT_SOURCE_DIR}/test/cpp/sycl ${CMAKE_BINARY_DIR}/test_sycl)
+    endif()
+  else()
+    if(NOT PYTORCH_FOUND_XPU)
+      message(WARNING "Not compiling with XPU. Could NOT find SYCL."
+        "Suppress this warning with -DUSE_XPU=OFF.")
+    else()
+      message(WARNING "Not compiling with XPU. Only support GCC compiler as CXX compiler."
+        "Suppress this warning with -DUSE_XPU=OFF.")
+    endif()
+    caffe2_update_option(USE_XPU OFF)
+  endif()
 endif()
 
 # ---[ NCCL
--- cmake/Modules/FindSYCLToolkit.cmake
+++ cmake/Modules/FindSYCLToolkit.cmake
@@ -1,10 +1,38 @@
-# This will define the following variables:
-# SYCL_FOUND               : True if the system has the SYCL library.
-# SYCL_INCLUDE_DIR         : Include directories needed to use SYCL.
-# SYCL_LIBRARY_DIR         ：The path to the SYCL library.
-# SYCL_LIBRARY             : SYCL library fullname.
+#[=======================================================================[.rst:
+SYCLConfig
+-------
 
-include(FindPackageHandleStandardArgs)
+Library to verify SYCL compatability of CMAKE_CXX_COMPILER
+and passes relevant compiler flags.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``SYCLTOOLKIT_FOUND``
+  True if the system has the SYCL library.
+``SYCL_COMPILER``
+  SYCL compiler executable.
+``SYCL_INCLUDE_DIR``
+  Include directories needed to use SYCL.
+``SYCL_LIBRARY_DIR``
+  Libaray directories needed to use SYCL.
+``SYCL_FLAGS``
+  SYCL specific flags for the compiler.
+``SYCL_LANGUAGE_VERSION``
+  The SYCL language spec version by Compiler.
+
+``SYCL::SYCL_CXX``
+  Interface target for using SYCL compiler.  The following properties are
+  defined for the target: ``INTERFACE_COMPILE_OPTIONS``,
+  ``INTERFACE_LINK_OPTIONS``, ``INTERFACE_INCLUDE_DIRECTORIES``, and
+  ``INTERFACE_LINK_DIRECTORIES``
+
+#]=======================================================================]
+
+set(SYCLTOOLKIT_FOUND False)
+include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 
 set(SYCL_ROOT "")
 if(DEFINED ENV{SYCL_ROOT})
@@ -12,16 +40,35 @@ if(DEFINED ENV{SYCL_ROOT})
 elseif(DEFINED ENV{CMPLR_ROOT})
   set(SYCL_ROOT $ENV{CMPLR_ROOT})
 endif()
+if(NOT SYCL_ROOT)
+  execute_process(
+    COMMAND which icpx
+    OUTPUT_VARIABLE SYCL_CMPLR_FULL_PATH
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-string(COMPARE EQUAL "${SYCL_ROOT}" "" nosyclfound)
-if(nosyclfound)
-  set(SYCL_FOUND False)
-  set(SYCL_REASON_FAILURE "SYCL library not set!!")
-  set(SYCL_NOT_FOUND_MESSAGE "${SYCL_REASON_FAILURE}")
-  return()
+  if(NOT EXISTS "${SYCL_CMPLR_FULL_PATH}")
+    message("Cannot find ENV{CMPLR_ROOT} or icpx, please setup SYCL compiler Tool kit enviroment before building!!")
+    return()
+  endif()
+
+  get_filename_component(SYCL_BIN_DIR "${SYCL_CMPLR_FULL_PATH}" DIRECTORY)
+  set(SYCL_ROOT ${SYCL_BIN_DIR}/..)
 endif()
 
-# Find include path from binary.
+find_file(
+  SYCL_COMPILER
+  NAMES icpx
+  HINTS ${SYCL_ROOT}/bin
+  NO_DEFAULT_PATH
+  )
+
+string(COMPARE EQUAL "${SYCL_COMPILER}" "" nocmplr)
+if(nocmplr)
+  set(SYCLTOOLKIT_FOUND False)
+  set(SYCL_REASON_FAILURE "SYCL: CMAKE_CXX_COMPILER not set!!")
+  set(SYCL_NOT_FOUND_MESSAGE "${SYCL_REASON_FAILURE}")
+endif()
+
 find_file(
   SYCL_INCLUDE_DIR
   NAMES include
@@ -29,18 +76,15 @@ find_file(
   NO_DEFAULT_PATH
   )
 
-# Find include/sycl path from include path.
 find_file(
   SYCL_INCLUDE_SYCL_DIR
   NAMES sycl
-  HINTS ${SYCL_ROOT}/include/
+  HINTS ${SYCL_ROOT}/include
   NO_DEFAULT_PATH
   )
 
-# Due to the unrecognized compilation option `-fsycl` in other compiler.
 list(APPEND SYCL_INCLUDE_DIR ${SYCL_INCLUDE_SYCL_DIR})
 
-# Find library directory from binary.
 find_file(
   SYCL_LIBRARY_DIR
   NAMES lib lib64
@@ -48,7 +92,6 @@ find_file(
   NO_DEFAULT_PATH
   )
 
-# Find SYCL library fullname.
 find_library(
   SYCL_LIBRARY
   NAMES sycl
@@ -57,14 +100,163 @@ find_library(
 )
 
 if((NOT SYCL_INCLUDE_DIR) OR (NOT SYCL_LIBRARY_DIR) OR (NOT SYCL_LIBRARY))
-  set(SYCL_FOUND False)
-  set(SYCL_REASON_FAILURE "SYCL library is incomplete!!")
+  set(SYCLTOOLKIT_FOUND False)
+  set(SYCL_REASON_FAILURE "SYCL sdk is incomplete!!")
   set(SYCL_NOT_FOUND_MESSAGE "${SYCL_REASON_FAILURE}")
   return()
 endif()
 
+# Function to write a test case to verify SYCL features.
+
+function(SYCL_CMPLR_TEST_WRITE src)
+
+  set(cpp_macro_if "#if")
+  set(cpp_macro_endif "#endif")
+
+  set(SYCL_CMPLR_TEST_CONTENT "")
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "#include <iostream>\nusing namespace std;\n")
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "int main(){\n")
+
+  # Feature tests goes here
+
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "${cpp_macro_if} defined(SYCL_LANGUAGE_VERSION)\n")
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "cout << \"SYCL_LANGUAGE_VERSION=\"<<SYCL_LANGUAGE_VERSION<<endl;\n")
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "${cpp_macro_endif}\n")
+
+  string(APPEND SYCL_CMPLR_TEST_CONTENT "return 0;}\n")
+
+  file(WRITE ${src} "${SYCL_CMPLR_TEST_CONTENT}")
+
+endfunction()
+
+# Function to Build the feature check test case.
+
+function(SYCL_CMPLR_TEST_BUILD error TEST_SRC_FILE TEST_EXE)
+
+  set(SYCL_CXX_FLAGS_LIST "${SYCL_CXX_FLAGS}")
+  separate_arguments(SYCL_CXX_FLAGS_LIST)
+
+  execute_process(
+    COMMAND "${SYCL_COMPILER}"
+    ${SYCL_CXX_FLAGS_LIST}
+    ${TEST_SRC_FILE}
+    "-o"
+    ${TEST_EXE}
+    WORKING_DIRECTORY ${SYCL_CMPLR_TEST_DIR}
+    OUTPUT_VARIABLE output ERROR_VARIABLE output
+    OUTPUT_FILE ${SYCL_CMPLR_TEST_DIR}/Compile.log
+    RESULT_VARIABLE result
+    TIMEOUT 60
+    )
+
+  # Verify if test case build properly.
+  if(result)
+    message("SYCL: feature test compile failed!!")
+    message("compile output is: ${output}")
+  endif()
+
+  set(${error} ${result} PARENT_SCOPE)
+
+endfunction()
+
+function(SYCL_CMPLR_TEST_RUN error TEST_EXE)
+
+  execute_process(
+    COMMAND ${TEST_EXE}
+    WORKING_DIRECTORY ${SYCL_CMPLR_TEST_DIR}
+    OUTPUT_VARIABLE output ERROR_VARIABLE output
+    RESULT_VARIABLE result
+    TIMEOUT 60
+    )
+
+  if(test_result)
+    set(SYCLTOOLKIT_FOUND False)
+    set(SYCL_REASON_FAILURE "SYCL: feature test execution failed!!")
+  endif()
+
+  set(test_result "${result}" PARENT_SCOPE)
+  set(test_output "${output}" PARENT_SCOPE)
+
+  set(${error} ${result} PARENT_SCOPE)
+
+endfunction()
+
+function(SYCL_CMPLR_TEST_EXTRACT test_output)
+
+  string(REGEX REPLACE "\n" ";" test_output_list "${test_output}")
+
+  set(SYCL_LANGUAGE_VERSION "")
+  foreach(strl ${test_output_list})
+     if(${strl} MATCHES "^SYCL_LANGUAGE_VERSION=([A-Za-z0-9_]+)$")
+       string(REGEX REPLACE "^SYCL_LANGUAGE_VERSION=" "" extracted_sycl_lang "${strl}")
+       set(SYCL_LANGUAGE_VERSION ${extracted_sycl_lang})
+     endif()
+  endforeach()
+
+  set(SYCL_LANGUAGE_VERSION "${SYCL_LANGUAGE_VERSION}" PARENT_SCOPE)
+endfunction()
+
+set(SYCL_FLAGS "")
+set(SYCL_LINK_FLAGS "")
+list(APPEND SYCL_FLAGS "-fsycl")
+list(APPEND SYCL_LINK_FLAGS "-fsycl")
+
+set(SYCL_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SYCL_FLAGS}")
+
+# Create a clean working directory.
+set(SYCL_CMPLR_TEST_DIR "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/TESTSYCLCMPLR")
+file(REMOVE_RECURSE ${SYCL_CMPLR_TEST_DIR})
+file(MAKE_DIRECTORY ${SYCL_CMPLR_TEST_DIR})
+
+# Create the test source file
+set(TEST_SRC_FILE "${SYCL_CMPLR_TEST_DIR}/sycl_features.cpp")
+set(TEST_EXE "${TEST_SRC_FILE}.exe")
+SYCL_CMPLR_TEST_WRITE(${TEST_SRC_FILE})
+
+# Build the test and create test executable
+SYCL_CMPLR_TEST_BUILD(error ${TEST_SRC_FILE} ${TEST_EXE})
+if(error)
+  return()
+endif()
+
+# Execute the test to extract information
+SYCL_CMPLR_TEST_RUN(error ${TEST_EXE})
+if(error)
+  return()
+endif()
+
+# Extract test output for information
+SYCL_CMPLR_TEST_EXTRACT(${test_output})
+
+# As per specification, all the SYCL compatible compilers should
+# define macro  SYCL_LANGUAGE_VERSION
+string(COMPARE EQUAL "${SYCL_LANGUAGE_VERSION}" "" nosycllang)
+if(nosycllang)
+  set(SYCLTOOLKIT_FOUND False)
+  set(SYCL_REASON_FAILURE "SYCL: It appears that the ${SYCL_COMPILER} does not support SYCL")
+  set(SYCL_NOT_FOUND_MESSAGE "${SYCL_REASON_FAILURE}")
+endif()
+
+message(DEBUG "The SYCL compiler is ${SYCL_COMPILER}")
+message(DEBUG "The SYCL Flags are ${SYCL_FLAGS}")
+message(DEBUG "The SYCL Language Version is ${SYCL_LANGUAGE_VERSION}")
+
+add_library(SYCL::SYCL_CXX INTERFACE IMPORTED)
+set_property(TARGET SYCL::SYCL_CXX PROPERTY
+  INTERFACE_COMPILE_OPTIONS ${SYCL_FLAGS})
+set_property(TARGET SYCL::SYCL_CXX PROPERTY
+  INTERFACE_LINK_OPTIONS ${SYCL_LINK_FLAGS})
+set_property(TARGET SYCL::SYCL_CXX PROPERTY
+  INTERFACE_INCLUDE_DIRECTORIES ${SYCL_INCLUDE_DIR})
+set_property(TARGET SYCL::SYCL_CXX PROPERTY
+  INTERFACE_LINK_DIRECTORIES ${SYCL_LIBRARY_DIR})
+
 find_package_handle_standard_args(
-  SYCL
-  FOUND_VAR SYCL_FOUND
-  REQUIRED_VARS SYCL_INCLUDE_DIR SYCL_LIBRARY_DIR SYCL_LIBRARY
+  SYCLToolkit
+  FOUND_VAR SYCLTOOLKIT_FOUND
+  REQUIRED_VARS SYCL_INCLUDE_DIR SYCL_LIBRARY_DIR SYCL_LIBRARY SYCL_FLAGS
+  VERSION_VAR SYCL_LANGUAGE_VERSION
   REASON_FAILURE_MESSAGE "${SYCL_REASON_FAILURE}")
+
+# Include in Cache
+set(SYCL_LANGUAGE_VERSION "${SYCL_LANGUAGE_VERSION}" CACHE STRING "SYCL Language version")
