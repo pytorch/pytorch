07d0805cb88c362a8e5306a5388fa19b52d04c8e
Jane Xu <janeyx@meta.com>
2024-01-22 20:29:34 +0000

    Migrate param_group testing to OptimizerInfo (#117675)
    Today, our param_group testing does the equivalent of pitting weight and bias with different optimizer hyperparams and then check that the overall result is going the right direction based on maximize.
    
    This PR introduces two tests to encompass coverage:
    1. For every optimizer input (no differentiable), always force bias to have 0 weight_decay, and then check that the direction is expected. This is basically a replica to today's tests, but is more methodical as the test is a real use case.
    2. To ensure that the different groups have distinct behavior, I added another test where lr is basically 0 in default group, and ensure that the param in the default group doesn't move while loss does.
    
    Together, these tests do a better job of testing param groups than today's tests, **though we do lose some flavors**. For example, RMSProp also pits centered=True vs False across the param_groups, Adadelta has a variation on rho, and ASGD has a variation for t0. I don't think this is really a loss, as the previous test was just testing for direction and our new tests test stronger guarantees.
    
    The leftover param group configs are used in conjunction with LRSchedulers.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117675
    Approved by: https://github.com/albanD

--- torch/testing/_internal/common_optimizers.py
+++ torch/testing/_internal/common_optimizers.py
@@ -1191,6 +1191,16 @@ optim_db: List[OptimizerInfo] = [
                 "TestOptimRenewed",
                 "test_deepcopy_copies_all_public_attrs",
             ),
+            DecorateInfo(
+                unittest.skip("Does not support param groups"),
+                "TestOptimRenewed",
+                "test_param_groups_lr",
+            ),
+            DecorateInfo(
+                unittest.skip("Does not support param groups"),
+                "TestOptimRenewed",
+                "test_param_groups_weight_decay",
+            ),
         ),
     ),
     OptimizerInfo(
@@ -1441,6 +1451,22 @@ optim_db: List[OptimizerInfo] = [
                     "Errors with list out of range, see https://github.com/pytorch/pytorch/issues/116061"
                 ),
                 "TestOptimRenewed",
+                "test_param_groups_weight_decay",
+                device_type="cpu",
+            ),
+            DecorateInfo(
+                skipIfTorchDynamo(
+                    "Errors with list out of range, see https://github.com/pytorch/pytorch/issues/116061"
+                ),
+                "TestOptimRenewed",
+                "test_param_groups_lr",
+                device_type="cpu",
+            ),
+            DecorateInfo(
+                skipIfTorchDynamo(
+                    "Errors with list out of range, see https://github.com/pytorch/pytorch/issues/116061"
+                ),
+                "TestOptimRenewed",
                 "test_load_nontensor_step",
                 device_type="cpu",
             ),
@@ -1477,6 +1503,11 @@ optim_db: List[OptimizerInfo] = [
                 ),
                 "TestOptimRenewed",
                 "test_state_dict_deterministic",
+            ),
+            DecorateInfo(
+                skipIfTorchDynamo("cannot call to_sparse on p.grad, see #117184"),
+                "TestOptimRenewed",
+                "test_param_groups_lr",
             ),
             DecorateInfo(
                 unittest.skip(
