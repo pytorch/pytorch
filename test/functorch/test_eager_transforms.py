28001a1e843e81d8cc56e0aaade9563e5dabf235
Jeff Daily <jeff.daily@amd.com>
2024-01-22 21:00:25 +0000

    [ROCm] skip test_eager_transforms.py test_compile_vmap_hessian_cuda
    Memory leak detected on ROCm.  Skip until it can be addressed.
    
    PYTORCH_TEST_WITH_ROCM=1 PYTORCH_TEST_CUDA_MEM_LEAK_CHECK=1 python
    test_eager_transforms.py -k test_compile_vmap_hessian_cuda
    
    See #117642 for moving rocm CI to unstable due to this test.

--- test/functorch/test_eager_transforms.py
+++ test/functorch/test_eager_transforms.py
@@ -25,6 +25,7 @@ from functools import wraps
 from torch.testing._internal.common_device_type import instantiate_device_type_tests, onlyCPU, dtypes, onlyCUDA
 from torch.testing._internal.common_dtype import get_all_fp_dtypes
 from torch.testing._internal.common_cuda import with_tf32_off, SM70OrLater, TEST_CUDA
+from torch.testing._internal.common_utils import skipIfRocm
 from torch.testing import make_tensor
 from torch._dynamo import allow_in_graph
 from torch._subclasses.fake_tensor import FakeTensorMode
@@ -4832,6 +4833,7 @@ def traceable(f):
 
 @markDynamoStrictTest
 class TestCompileTransforms(TestCase):
+    @skipIfRocm(msg="test leaks memory on ROCm")
     @xfailIfTorchDynamo
     # torch.compile is not supported on Windows
     # Triton only supports GPU with SM70 or later.
