d745e8582283202431b98546ba73010a41578362
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2024-01-23 13:54:03 -0800

    [DTensor][fix] fix is_tensor_shardable to correctly handle Replicate placement (#117726)
    **Summary**
    Previously DTensor sharding plans filter (i.e. `is_tensor_shardable()`) cannot correctly handle the case where the input `DTensor` has 0 dimension. This filter should return `True` if the sharding placement on 0 dimension is `Replicate` even if `tensor dim < num of shards` on that dimension in which case `tensor dim == 0` and `num of shards == 1`.
    
    In this PR we also noticed a behavior discrepancy of `torch.addmm`. See #118131
    
    **Test Plan**
    ```
    pytest test/distributed/_tensor/test_dtensor_ops.py -s -k addmm
    pytest test/distributed/_tensor/test_dtensor_ops.py -s -k mm_cpu_float32
    CUDA_VISIBLE_DEVICES="" pytest test/distributed/_tensor/test_matrix_ops.py -s -k empty_operand
    pytest test/distributed/_tensor/test_matrix_ops.py -s -k empty_operand
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117726
    Approved by: https://github.com/wanchaol

--- test/distributed/_tensor/test_matrix_ops.py
+++ test/distributed/_tensor/test_matrix_ops.py
@@ -40,6 +40,29 @@ class DistMatrixOpsTest(DTensorTestBase):
         self.assertEqual(dist_res.full_tensor(), local_res)
 
     @with_comms
+    def test_addmm_empty_operand(self):
+        device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
+        shard_spec = [Shard(0)]
+        replica_spec = [Replicate()]
+
+        tensor_to_shard = torch.randn(12, 0)
+        mat1 = distribute_tensor(tensor_to_shard, device_mesh, shard_spec)
+        tensor_to_replicate = torch.randn(0, 4)
+        mat2 = distribute_tensor(tensor_to_replicate, device_mesh, replica_spec)
+        input_tensor = torch.randn(4)
+        inp = distribute_tensor(input_tensor, device_mesh, replica_spec)
+
+        dist_res = torch.addmm(inp, mat1, mat2)
+        local_res = torch.addmm(input_tensor, tensor_to_shard, tensor_to_replicate)
+        # The behavior of torch.ops.aten.addmm.default is different on CPU and GPU.
+        # For more info see: https://github.com/pytorch/pytorch/issues/118131
+        # TODO: these two tensors should be equal on GPU. fix this.
+        if self.device_type == "cuda":
+            self.assertNotEqual(dist_res.full_tensor(), local_res)
+        else:
+            self.assertEqual(dist_res.full_tensor(), local_res)
+
+    @with_comms
     def test_addmm_auto_redistribute(self):
         device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
         shard0_spec = [Shard(0)]
