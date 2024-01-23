bba213c151f2c8e7a29635d273a73a6c98d24393
Bin Bao <binbao@meta.com>
2024-01-22 19:02:05 -0800

    Update on "[AOTI] Support scalar to tensor in the ABI-compatible mode"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/csrc/inductor/aoti_torch/shim_common.cpp
+++ torch/csrc/inductor/aoti_torch/shim_common.cpp
@@ -7,7 +7,6 @@
 #include <torch/csrc/inductor/aoti_torch/tensor_converter.h>
 #include <torch/csrc/inductor/aoti_torch/utils.h>
 #include <torch/csrc/inductor/inductor_ops.h>
-#include <torch/torch.h>
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
@@ -31,6 +30,7 @@
 #include <ATen/ops/nonzero.h>
 #include <ATen/ops/scatter.h>
 #include <ATen/ops/scatter_reduce.h>
+#include <ATen/ops/tensor.h>
 
 #endif
 
@@ -141,30 +141,30 @@ AOTI_TORCH_ITEM_IMPL(int32, int32_t)
 AOTI_TORCH_ITEM_IMPL(int64, int64_t)
 AOTI_TORCH_ITEM_IMPL(bool, bool)
 
-#define AOTI_TORCH_VALUE_TO_TENSOR_IMPL(dtype, ctype, ttype)       \
-  AOTITorchError aoti_torch_value_to_tensor_##dtype(               \
-      ctype value, AtenTensorHandle* ret_new_tensor) {             \
-    AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({                   \
-      *ret_new_tensor =                                            \
-          new_tensor_handle(torch::tensor({value}, torch::ttype)); \
-    });                                                            \
+#define AOTI_TORCH_VALUE_TO_TENSOR_IMPL(dtype, ctype, ttype)              \
+  AOTITorchError aoti_torch_value_to_tensor_##dtype(                      \
+      ctype value, AtenTensorHandle* ret_new_tensor) {                    \
+    AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({                          \
+      *ret_new_tensor =                                                   \
+          new_tensor_handle(at::tensor({value}, c10::ScalarType::ttype)); \
+    });                                                                   \
   }
 
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(float32, float, kFloat32)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(float64, double, kFloat64)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint8, uint8_t, kUInt8)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint16, uint16_t, kUInt16)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(float32, float, Float)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(float64, double, Double)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint8, uint8_t, Byte)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint16, uint16_t, UInt16)
 // error: conversion from ‘uint32_t’ {aka ‘unsigned int’} to
 // ‘torch::detail::TensorDataContainer’ is ambiguous
-// AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint32, uint32_t, kUInt32)
+// AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint32, uint32_t, UInt32)
 // error: conversion from ‘uint64_t’ {aka ‘long unsigned int’} to
 // ‘torch::detail::TensorDataContainer’ is ambiguous
-// AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint64, uint64_t, kUInt64)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int8, int8_t, kInt8)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int16, int16_t, kInt16)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int32, int32_t, kInt32)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int64, int64_t, kInt64)
-AOTI_TORCH_VALUE_TO_TENSOR_IMPL(bool, bool, kBool)
+// AOTI_TORCH_VALUE_TO_TENSOR_IMPL(uint64, uint64_t, UInt64)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int8, int8_t, Char)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int16, int16_t, Short)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int32, int32_t, Int)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(int64, int64_t, Long)
+AOTI_TORCH_VALUE_TO_TENSOR_IMPL(bool, bool, Bool)
 
 bool aoti_torch_grad_mode_is_enabled() {
   return c10::GradMode::is_enabled();
