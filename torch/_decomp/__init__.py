7d2438ef66ba3785f22c1d2eaf555ad43419fd4a
Isuru Fernando <ifernando@quansight.com>
2024-01-23 19:14:38 +0000

    Update base for Update on "Add decompositions for avg_poolnd"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_decomp/__init__.py
+++ torch/_decomp/__init__.py
@@ -434,6 +434,7 @@ def core_aten_decompositions() -> Dict[torch._ops.OperatorBase, Callable]:
             aten.unbind,
             aten.unfold_backward,
             aten.unfold_copy,
+            aten._unsafe_masked_index,
             aten._unsafe_index,
             aten.unsafe_split.Tensor,
             aten.unsafe_split_with_sizes,
