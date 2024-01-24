3054b0fc769a6256df688138d8fabf75ac328470
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-01-24 21:26:35 +0000

    Revert "Check if enable inside run call (#118101)"
    This reverts commit 6fc015fedc96e532da756e9408fcedb9c81a423f.
    
    Reverted https://github.com/pytorch/pytorch/pull/118101 on behalf of https://github.com/clee2000 due to possibly causing failures on b025e5984ce30eed10df0cc89111e88983d823d3 ([comment](https://github.com/pytorch/pytorch/pull/118101#issuecomment-1908940940))

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -2820,17 +2820,6 @@ This message can be suppressed by setting PYTORCH_PRINT_REPRO_ON_FAILURE=0"""
 
 
     def run(self, result=None):
-        # Check if enable here as well so we don't have to worry about
-        # subclasses calling super().setUp().   Call it via a wrapper instead of
-        # directly because the skipTest call will raise an uncaught exception
-        def check_if_enable_wrapper(f):
-            @wraps(f)
-            def wrapper(*args, **kwargs):
-                check_if_enable(self)
-                f(*args, **kwargs)
-            return wrapper
-        setattr(self, self._testMethodName, check_if_enable_wrapper(getattr(self, self._testMethodName)))
-
         with contextlib.ExitStack() as stack:
             if TEST_WITH_CROSSREF:  # noqa: F821
                 stack.enter_context(CrossRefMode())
@@ -2839,6 +2828,7 @@ This message can be suppressed by setting PYTORCH_PRINT_REPRO_ON_FAILURE=0"""
             )
 
     def setUp(self):
+        check_if_enable(self)
         set_rng_seed(SEED)
 
         # Save global check sparse tensor invariants state that can be
