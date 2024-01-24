7457f45e3909e39493dbff58c18383279afb35fe
cyy <cyyever@outlook.com>
2024-01-25 03:30:01 +0800

    Remove unused inclusion of c10/util/C++17.h

--- aten/src/ATen/cpu/vec/vec_base.h
+++ aten/src/ATen/cpu/vec/vec_base.h
@@ -26,7 +26,6 @@
 #include <ATen/cpu/vec/intrinsics.h>
 #include <ATen/native/Math.h>
 #include <ATen/NumericUtils.h>
-#include <c10/util/C++17.h>
 #include <c10/util/Half.h>
 #include <c10/util/BFloat16.h>
 #include <c10/util/BFloat16-math.h>
