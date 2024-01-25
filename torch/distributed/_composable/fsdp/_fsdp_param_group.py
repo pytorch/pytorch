2b36804fcde2f58344f92bf63804b1ffd2fc1ac8
Andrew Gu <andgu@fb.com>
2024-01-25 15:45:12 -0800

    Update base for Update on "[FSDP2] Added `reshard_after_forward`"
    This PR adds the `reshard_after_forward: Union[bool, int]` arg and a `reshard()` method. The `reshard_after_forward` argument trades off communication and memory.
    - `reshard_after_forward=True`: reshard parameters after forward; unshard (all-gather) in backward
    - `reshard_after_forward=False`: no reshard of parameters after forward; no unshard (all-gather) in backward
    - `reshard_after_forwrad: int`: reshard parameters to a smaller world size; unshard (all-gather) over small world size in backward
    
    In comparison with DeepSpeed and existing FSDP:
    - `reshard_after_forward=True` == `FULL_SHARD` == ZeRO-3
    - `reshard_after_forward=False` == `SHARD_GRAD_OP` == ZeRO-2
    - `reshard_after_forward=8` == ZeRO++
    
    We prefer a simple state transition between `SHARDED` / `SHARDED_POST_FORWARD` and `UNSHARDED`, where the state directly defines what tensors are registered to the module. In particular, we _do not_ have a state where the sharded parameters are registered but the unsharded parameters are still in GPU memory. This greatly simplifies our state transitions, but it means that parameters may be non-intuitively registered to the module (e.g. if only the root does not reshard after forward, then the root will be the only without sharded parameters registered). To address this, we introduce a simple `reshard()` method that can force reshard the parameters. This makes sense to me because the typical case does not care about the registered parameters after forward (in fact, for existing FSDP with `use_orig_params=False`, the unsharded parameters are still registered and are dangling tensors without storage.)
    
    I plan to expose a complementary `unshard(async_op: bool = True)` method in the future.
    
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_param_group.py
+++ torch/distributed/_composable/fsdp/_fsdp_param_group.py
@@ -14,9 +14,8 @@ from ._fsdp_collectives import (
     foreach_all_gather_copy_out,
     foreach_reduce_scatter,
 )
-
-from ._fsdp_common import FSDPMeshInfo, HSDPMeshInfo, ParamModuleInfo, TrainingState
-from ._fsdp_param import FSDPParam, ShardedState
+from ._fsdp_common import FSDPMeshInfo, HSDPMeshInfo, TrainingState
+from ._fsdp_param import FSDPParam, ParamModuleInfo, ShardedState
 
 
 class FSDPParamGroup:
