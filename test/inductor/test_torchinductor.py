29e53aa4cf8175f596577907ee61ac732ecc6685
eqy <eddiey@nvidia.com>
2024-01-26 03:23:18 +0000

    [CUDA] Add missing TF32 annotation to `test_uint4x2_mixed_mm` (#118143)
    Addresses numerical mismatches seen on architectures with TF32.
    
    CC @nWEIdia
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118143
    Approved by: https://github.com/nWEIdia, https://github.com/jansel

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2162,6 +2162,7 @@ class CommonTemplate:
             check_lowp=True,
         )
 
+    @with_tf32_off
     @config.patch(use_mixed_mm=True)
     def test_uint4x2_mixed_mm(self):
         def fn(a, b):
