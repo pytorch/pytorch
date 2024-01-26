2154b07c44148666269be79fa598e6846ff501f1
haozhe.zhu <haozhe.zhu@intel.com>
2024-01-26 15:18:11 +0800

    Update on "remove rebundant to_dtype in Fused Schedular Nodes"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -1127,8 +1127,8 @@ class Kernel(CodeGen):
                         )
 
                     if name == "to_dtype":
-                        if hasattr(args[0], 'from') and getattr(args[0], 'from').dtype == args[1]:
-                            return getattr(args[0], 'from')
+                        if hasattr(args[0], 'store_from') and getattr(args[0], 'store_from').dtype == args[1]:
+                            return getattr(args[0], 'store_from')
 
                     csevar = self.cse.generate(
                         self.compute,
@@ -1226,6 +1226,8 @@ class Kernel(CodeGen):
                     if self.current_node:
                         for other_name in self.current_node.get_mutations():
                             self.cse.store_cache[other_name] = value
+                    if hasattr(value, 'from'):
+                        setattr(value, 'store_from', getattr(value, 'from'))
                 if name not in V.graph.removed_buffers:
                     return self.store(name, index, value, mode=mode)
                 else:
