c24f6548c12d016892bd9b087bd2de547d615789
Kai Londenberg <klondenberg@meta.com>
2024-01-26 14:53:48 +0100

    [inductor max autotune] Detailed autotuning result logs ( machine-readable )
    This diff introduces a new separate logging of autotuning results,
    with the intention of making the results analyzable, specifically
    those for the new experimental Cutlass backend.
    
    Results are logged as text files with one JSON document corresponding to a single benchmark result per line.

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -21,7 +21,12 @@ from torch._dynamo.utils import counters, identity, preserve_rng_state
 from . import config, ir
 from .autotune_process import TensorMeta, TritonBenchmarkRequest
 from .codecache import code_hash, PersistentCache, PyCodeCache
-from .codegen.common import ChoiceCaller, IndentedBuffer, KernelTemplate
+from .codegen.common import (
+    ChoiceCaller,
+    IndentedBuffer,
+    KernelTemplate,
+    PrimitiveInfoType,
+)
 from .codegen.triton import texpr, TritonKernel, TritonPrinter, TritonScheduling
 from .codegen.triton_utils import config_of, signature_to_meta
 from .exc import CUDACompileError
@@ -545,6 +550,19 @@ class TritonTemplate(KernelTemplate):
             make_kernel_render,
             extra.strip("-").replace("-", ", "),
             bmreq,
+            log_info={
+                "tile_shape": str(
+                    (
+                        kwargs.get("BLOCK_M", -1),
+                        kwargs.get("BLOCK_K", -1),
+                        kwargs.get("BLOCK_N", -1),
+                    )
+                ),
+                "num_stages": num_stages,
+                "num_warps": num_warps,
+                "allow_tf32": str(kwargs.get("ALLOW_TF32", None)),
+                "acc_type": str(kwargs.get("ACC_TYPE", None)),
+            },
         )
 
 
@@ -595,12 +613,32 @@ class ExternKernelChoice:
 
 class TritonTemplateCaller(ChoiceCaller):
     def __init__(
-        self, name, input_nodes, layout, make_kernel_render, debug_extra, bmreq
+        self,
+        name,
+        input_nodes,
+        layout,
+        make_kernel_render,
+        debug_extra,
+        bmreq,
+        log_info: Optional[
+            Dict[str, Union[PrimitiveInfoType, List[PrimitiveInfoType]]]
+        ] = None,
     ):
         super().__init__(name, input_nodes, layout)
         self.make_kernel_render = make_kernel_render
         self.debug_extra = debug_extra
-        self.bmreq = bmreq
+        self.bmreq: TritonBenchmarkRequest = bmreq
+        if log_info is None:
+            log_info = {}
+        self.log_info: Dict[str, Any] = log_info
+        self.log_info.update(
+            {
+                "backend": "Triton",
+                "grid": str(self.bmreq.grid),
+                "num_stages": self.bmreq.num_stages,
+                "num_warps": self.bmreq.num_warps,
+            }
+        )
 
     def benchmark(self, *args, out):
         assert self.bmreq is not None
@@ -628,6 +666,10 @@ class TritonTemplateCaller(ChoiceCaller):
                 make_kernel_render=self.make_kernel_render,
             )
         )
+
+    def info_dict(self) -> Dict[str, PrimitiveInfoType]:
+        """Information returned here is logged to the autotune log file when that is enabled."""
+        return self.log_info
 
 
 class ExternKernelCaller(ChoiceCaller):
@@ -695,6 +737,13 @@ class ExternKernelCaller(ChoiceCaller):
                 kwargs=self.kwargs,
             )
         )
+
+    def info_dict(self) -> Dict[str, PrimitiveInfoType]:
+        """Information returned here is logged to the autotune log file when that is enabled."""
+        return {
+            "backend": "extern",
+            "kernel_call_name": self.choice.call_name(),
+        }
 
 
 class ErrorFromChoice(RuntimeError):
@@ -810,6 +859,7 @@ class AlgorithmSelectorCache(PersistentCache):
         if (
             make_benchmark_fn.cache_info().currsize
             or log.getEffectiveLevel() == logging.DEBUG
+            or config.trace.log_autotuning_results
         ):
             self.log_results(name, input_nodes, timings, autotune_elapse)
         selected_choice = builtins.min(timings, key=timings.__getitem__).output_node()
@@ -941,7 +991,13 @@ class AlgorithmSelectorCache(PersistentCache):
         return benchmark
 
     @staticmethod
-    def log_results(name, input_nodes, timings, elapse):
+    def log_results(
+        name: str,
+        input_nodes: List[ir.IRNode],
+        timings: Dict[ChoiceCaller, float],
+        elapse: float,
+    ):
+        V.debug.log_autotuning_results(name, input_nodes, timings, elapse)
         if not (config.max_autotune or config.max_autotune_gemm) or not PRINT_AUTOTUNE:
             return
         sizes = ", ".join(
