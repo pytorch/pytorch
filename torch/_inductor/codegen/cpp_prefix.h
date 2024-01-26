0d49d0b1fe28b31726839a72829ede9b98f9b47e
leslie-fang-intel <leslie.fang@intel.com>
2024-01-26 11:50:20 +0800

    Update on "[Inductor] Fix Argmax codegen with Nan input"
    **Summary**
    Fix issue https://github.com/pytorch/pytorch/issues/118266, current `torch.argmax` and `torch.argmin` has different return values with eager and Inductor cpp backend when inputs has `Nan` value. Align cpp backend results to eager by reusing the compare function.
    
    
    **Test Plan**
    ```
    python -u -m pytest -s -v test_cpu_repro.py -k test_argmin_cpu_only
    python -u -m pytest -s -v test_cpu_repro.py -k test_argmax_argmin_with_nan_value
    ```
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_prefix.h
+++ torch/_inductor/codegen/cpp_prefix.h
@@ -10,7 +10,6 @@
 #include <ATen/NumericUtils.h>
 #include <ATen/core/PhiloxRNGEngine.h>
 #include <ATen/native/Math.h>
-#include <ATen/native/SharedReduceOps.h>
 
 #include <c10/util/Float8_e4m3fn.h>
 #include <c10/util/Float8_e5m2.h>
@@ -352,14 +351,25 @@ inline at::vec::Vectorized<float> flag_to_float_vec(const T* src) {
 // Refer to https://github.com/pytorch/pytorch/blob/b5b36cf0c4e1958f1ff25120f5d4beeef3288187/
 // aten/src/ATen/native/SharedReduceOps.h#L419-L445
 template <typename scalar_t>
-bool greater_or_nan(scalar_t a, scalar_t b, int64_t idx_a, int64_t idx_b) {
-  // TODO<Leslie> Should we include the header file of <ATen/native/SharedReduceOps.h> or copy the implementation
-  return at::native::detail::GreaterOrNan<scalar_t>()(a, b, idx_a, idx_b);
+inline bool greater_or_nan(scalar_t a, scalar_t b, int64_t idx_a, int64_t idx_b) {
+  if (at::_isnan(a)) {
+    if (at::_isnan(b)) {
+      return idx_a < idx_b;
+    }
+    return true;
+  }
+  return (a == b) ? idx_a < idx_b : (a > b);
 }
 
 template <typename scalar_t>
-bool less_or_nan(scalar_t a, scalar_t b, int64_t idx_a, int64_t idx_b) {
-  return at::native::detail::LessOrNan<scalar_t>()(a, b, idx_a, idx_b);
+inline bool less_or_nan(scalar_t a, scalar_t b, int64_t idx_a, int64_t idx_b) {
+  if (at::_isnan(a)) {
+    if (at::_isnan(b)) {
+      return idx_a < idx_b;
+    }
+    return true;
+  }
+  return (a == b) ? idx_a < idx_b : (a < b);
 }
 
 template <typename scalar_t>
