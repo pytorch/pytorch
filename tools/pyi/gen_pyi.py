406cafaca85d462ec6d83da285196af8bbedeb0a
Felix Zimmermann <fzimmermann89@gmail.com>
2024-01-29 00:08:15 +0100

    fix lint issues

--- tools/pyi/gen_pyi.py
+++ tools/pyi/gen_pyi.py
@@ -1106,12 +1106,12 @@ def gen_pyi(
                             "self",
                             "device: Optional[Union[_device, _int, str]] = None",
                             "non_blocking: _bool = False",
-                            "memory_format: torch.memory_format",
+                            "memory_format: torch.memory_format = torch.preserve_format",
                         ]
                     )
                 )
             ],
-            "cpu":  ["def cpu(self, memory_format: torch.memory_format) -> Tensor: ..."],
+            "cpu": ["def cpu(self, memory_format: torch.memory_format = torch.preserve_format) -> Tensor: ..."],
             "numpy": ["def numpy(self, *, force: _bool = False) -> Any: ..."],
             "apply_": ["def apply_(self, callable: Callable) -> Tensor: ..."],
             "map_": [
