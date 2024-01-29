76be462d78dd6edb2065796347435df022ac06bb
Will Constable <whc@meta.com>
2024-01-29 08:57:16 +0000

    Add justknobs env helper for pytorch distributed (#118451)
    Summary:
    Adds a JK killswitch check and configures the env for enabling pytorch
    nccl flight recorder.  Note- this only enables recording events in memory, not
    dumping them.
    
    Test Plan: CI test
    
    Reviewed By: zdevito
    
    Differential Revision: D52920092
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118451
    Approved by: https://github.com/malfet

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -36,6 +36,7 @@ from torch._C._distributed_c10d import (
     get_debug_level,
     Work
 )
+from torch._utils_internal import set_pytorch_distributed_envs_from_justknobs
 from .constants import default_pg_timeout, default_pg_nccl_timeout
 from .c10d_logger import _exception_logger, _time_logger
 from .rendezvous import register_rendezvous_handler, rendezvous  # noqa: F401
@@ -1182,6 +1183,8 @@ def init_process_group(
         "cpu:gloo,cuda:custom_backend".
 
     """
+    set_pytorch_distributed_envs_from_justknobs()
+
     global _world
 
     global _backend
