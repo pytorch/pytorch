82e5227fb6eb9c860876925a40b9e89f3825b715
Peter Bell <peterbell10@live.co.uk>
2024-01-29 14:40:54 +0000

    Remove remaining deprecated fbgemm operators
    These operators have been deprecated since #72690 (Feb 2022) but
    apparently still have a meta-internal user that relies on them
    https://github.com/pytorch/pytorch/pull/112153#issuecomment-1802889376
    
    BC-breaking message:
    
    The following internal operators have been removed, if your TorchScipt
    models relied on them please reexport using the newer
    `torch.ao.quantization` API.
    
    ```
    aten::fbgemm_linear_int8_weight
    aten::fbgemm_linear_int8_weight_fp32_activation
    aten::fbgemm_linear_quantize_weight
    aten::fbgemm_pack_quantized_matrix
    ```
    
    [ghstack-poisoned]

--- test/forward_backward_compatibility/check_forward_backward_compatibility.py
+++ test/forward_backward_compatibility/check_forward_backward_compatibility.py
@@ -151,6 +151,10 @@ ALLOW_LIST = [
     ("aten::quantized_rnn_tanh_cell", datetime.date(2024, 12, 31)),
     ("quantized::make_quantized_cell_params", datetime.date(2024, 12, 31)),
     ("profiler::_record_function_exit", datetime.date(2024, 12, 31)),
+    ("aten::fbgemm_pack_quantized_matrix", datetime.date(2024, 12, 31)),
+    ("aten::fbgemm_linear_int8_weight_fp32_activation", datetime.date(2024, 12, 31)),
+    ("aten::fbgemm_linear_int8_weight", datetime.date(2024, 12, 31)),
+    ("aten::fbgemm_linear_quantize_weight", datetime.date(2024, 12, 31)),
 ]
 
 ALLOW_LIST_COMPILED = [
