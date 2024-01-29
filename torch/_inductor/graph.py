ad43272cb5dd7c1d45d3e926b84e6f8f68dfc0c4
Kai Londenberg <klondenberg@meta.com>
2024-01-29 17:26:24 +0100

    [Inductor cutlass backend] Enable StreamK and nonzero workspace sizes

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -239,6 +239,7 @@ class GraphLowering(torch.fx.Interpreter):
         self.mutated_input_idxs: List[int] = []
         self.name_to_buffer: Dict[str, ir.Buffer] = {}
         self.name_to_users: DefaultDict[str, List[ir.IRNode]] = defaultdict(list)
+        self.name_to_workspace_buffer: Dict[str, ir.WorkspaceBuffer] = {}
         self.creation_time = time.time()
         self.name = "GraphLowering"
         self.cpp_wrapper = cpp_wrapper
@@ -542,6 +543,24 @@ class GraphLowering(torch.fx.Interpreter):
         if not isinstance(buffer, ir.ComputedBuffer) or not buffer.is_zero_elements():
             self.add_device_info(buffer.get_device())
         return name
+
+    def get_workspace_buffer_for(self, buffer: ir.Buffer):
+        if not hasattr(buffer, "get_workspace_size"):
+            return None
+        workspace_size = buffer.get_workspace_size()
+        if workspace_size <= 0:
+            return None
+        workspace_name = f"workspace_of_{buffer.get_name()}"
+        if workspace_name in self.name_to_buffer:
+            res = self.name_to_buffer[workspace_name]
+            assert isinstance(res, ir.WorkspaceBuffer)
+            res.resize(workspace_size)  # could have been changed via retuning
+            return res
+        workspace_buffer = ir.WorkspaceBuffer.create(workspace_size, buffer)
+        workspace_buffer.name = workspace_name
+        self.buffers.append(workspace_buffer)
+        self.name_to_buffer[workspace_name] = workspace_buffer
+        return workspace_buffer
 
     def register_list(self, buffer_names: List[str]):
         name = "list_" + "_".join(buffer_names)
