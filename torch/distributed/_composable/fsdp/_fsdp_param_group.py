ae33a0512348f9fc03546c0305867f7c6aae53bf
Andrew Gu <andgu@fb.com>
2024-01-29 06:42:08 -0800

    [FSDP2] Added initial `FSDPParamGroup`, `FSDPParam`, `ParamModuleInfo` (#117867)
    This PR adds the initial `FSDPParamGroup` and `FSDPParam` classes, and it focuses on the `ParamModuleInfo` data structure.
    
    - `ParamModuleInfo` has the info needed to `setattr` a managed parameter, where it must account for shared parameters and shared modules.
        ```
        # Shared parameter
        lin1.weight = lin2.weight
    
        # Shared module
        mlp.lin1 = mlp.lin2
        ```
    - In order for FSDP to find shared modules' parameters, we must use `remove_duplicate=False`. See https://github.com/pytorch/pytorch/pull/99448/ for the original context. Finding shared modules' parameters is not necessary for the `setattr` logic, but in case we need it in the future (like for existing FSDP's state dict), we include that info for now.
    
    With this PR, we see the general system architecture:
    - 1 `module` : 1 `fully_shard`
    - 1 `fully_shard` : 1 `FSDPParamGroup`
    - 1 `FSDPParamGroup` : k `FSDPParam`
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117867
    Approved by: https://github.com/weifengpy, https://github.com/wanchaol
    ghstack dependencies: #118525, #117814

---
+++ torch/distributed/_composable/fsdp/_fsdp_param_group.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+
+import torch
+import torch.nn as nn
+
+from torch.distributed.fsdp._common_utils import _named_parameters_with_duplicates
+
+from ._fsdp_common import FSDPMeshInfo
+from ._fsdp_param import FSDPParam, ParamModuleInfo
+
+
+class FSDPParamGroup:
+    """This class represents a parameter group to communicate together."""
+
+    def __init__(
+        self,
+        params: List[nn.Parameter],
+        module: nn.Module,
+        mesh_info: FSDPMeshInfo,
+        device: torch.device,
+    ):
+        self.module = module  # permit ref cycle because 1:1 lifetime
+        param_module_infos = _get_param_module_infos(params, module)
+        self.fsdp_params = [
+            FSDPParam(param, module_info, mesh_info, device)
+            for param, module_info in zip(params, param_module_infos)
+        ]
+        self.mesh_info = mesh_info
+        self.device = device
+
+
+def _get_param_module_infos(
+    params: List[nn.Parameter], module: nn.Module
+) -> List[ParamModuleInfo]:
+    """
+    Shared parameter:
+        lin1.weight = lin2.weight
+    Shared module:
+        mlp.lin1 = mlp.lin2
+    We do not remove duplicates when traversing both modules and parameters to
+    find shared modules' parameters and shared parameters within a module.
+    """
+    params_set = set(params)
+    param_to_module_info: Dict[nn.Parameter, ParamModuleInfo] = {}
+    for _, submodule in module.named_modules(remove_duplicate=False):
+        for param_name, param in _named_parameters_with_duplicates(
+            submodule, recurse=False
+        ):
+            if param in params_set:
+                if param not in param_to_module_info:
+                    param_to_module_info[param] = ParamModuleInfo(submodule, param_name)
+                else:
+                    param_to_module_info[param].shared_modules.append(submodule)
+                    param_to_module_info[param].shared_param_names.append(param_name)
+    if len(param_to_module_info) != len(params):
+        raise AssertionError(f"Some parameters are not in the module tree of {module}")
+    return [param_to_module_info[param] for param in params]
