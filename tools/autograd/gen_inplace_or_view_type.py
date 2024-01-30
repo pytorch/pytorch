a393f8dba34ee7784263a67ad536938a8f61a32e
Joel Schlosser <jbschlosser@meta.com>
2024-01-30 14:01:54 -0500

    Update base for Update on "Proper view support for jagged layout NestedTensor"
    This PR:
    * Introduces two ATen ops for creating true jagged views from a dense values buffer
        * `_nested_view_from_values_offsets(values, offsets, dummy)` and  `_nested_view_from_values_offsets_lengths(values, offsets, lengths, dummy)`
        * These ops are implemented on the Python side using torch.library so we can return a subclass instance
        * `jagged_from_list()` now uses these instead of the old autograd.Function `NestedViewFromBuffer`
        * The latter op is used for non-contiguous JTs returned via `torch.nested.narrow()`
        * `dummy` is an awful hack to ensure that `NestedTensor.__torch_dispatch__()` is invoked for our view
    * Introduces an ATen op for accessing the `values` component of an NT via a view
        * `_nested_get_values(nt)`
    * Changes test code to prefer `as_nested_tensor()` over `jagged_from_list()` directly
        * Similarly, avoid `buffer_from_jagged()`, preferring `values()`
    * Depends on [general subclass view fake-ification](https://docs.google.com/document/d/1C5taWiplmX7nKiURXDOAZG2W5VNJ2iV0fQFq92H0Cxw) on the PT2 side (handled solely in previous PRs in the stack)
    
    With these changes, the semantics of jagged layout NTs are such that they are considered a true view of the underlying `values` buffer. This means views of jagged NTs are views of the underlying buffer as well, simplifying some handling.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- tools/autograd/gen_inplace_or_view_type.py
+++ tools/autograd/gen_inplace_or_view_type.py
@@ -279,25 +279,6 @@ def inverse_view_name(f: NativeFunction) -> str:
     return f"{copy_variant}{overload}_inverse"
 
 
-# e.g. as_strided -> AsStridedViewFunc for camel case or
-# as_strided_view_func otherwise
-def view_func_name(
-    f: NativeFunction, include_namespace: bool = False, camel_case: bool = True
-) -> str:
-    name = f.func.name.unambiguous_name()
-    view_func_name = f"{name.replace('.', '_')}_view_func"
-    if camel_case:
-        is_private = view_func_name.startswith("_")
-        view_func_name = "".join(
-            [p.title() for p in view_func_name.replace(".", "_").split("_")]
-        )
-        if is_private:
-            # put the leading underscore back in
-            view_func_name = f"_{view_func_name}"
-    namespace = "torch::autograd::generated::" if include_namespace else ""
-    return f"{namespace}{view_func_name}"
-
-
 def extract_bindings(f: NativeFunction) -> List[Binding]:
     return [
         r
@@ -417,6 +398,8 @@ def emit_view_func(
             updated_args.append(arg)
         else:
             updated_args.append(arg)
+
+    from .gen_view_funcs import view_func_name
 
     view_func_args = [b.name for b in bindings if b.name != "self"]
     if view_idx is not None:
