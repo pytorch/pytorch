9492e1e62beaaa71f4f688b11ad41ed924147230
Edward Z. Yang <ezyang@meta.com>
2024-01-31 13:24:47 -0500

    Add TORCHDYNAMO_EXTENDED_DEBUG_GUARD_ADDED (#118750)
    This allows us to request extended (including C++ backtrace) information
    whenever a specific guard occurs.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118750
    Approved by: https://github.com/aakhundov

--- torch/fx/experimental/_config.py
+++ torch/fx/experimental/_config.py
@@ -25,6 +25,13 @@ translation_validation_no_bisect = (
 # the a ShapeEnv with the same state. This should be used only in testing.
 check_shape_env_recorded_events = False
 
+# Give extended debug information if the string representation of a guard
+# matches this.  For example, set this to "Ne(s0, 10)" and whenever we issue
+# this guard, we will generate full Python and C++ backtrace
+# [@compile_ignored: debug]
+extended_debug_guard_added = os.environ.get(
+    "TORCHDYNAMO_EXTENDED_DEBUG_GUARD_ADDED", None
+)
 
 # [@compile_ignored: debug] Show a warning for every specialization
 print_specializations = False
