84aa6cf2ff0b01559fab32513b76956d3786517a
Ant White <acwhite@opentable.com>
2024-01-31 02:25:13 +0000

    Only check and alter docstrings if python was compiled with docstrings

--- torch/distributed/rpc/api.py
+++ torch/distributed/rpc/api.py
@@ -6,6 +6,7 @@ import contextlib
 import functools
 import inspect
 import logging
+import sysconfig
 import threading
 from typing import Dict, Generic, TypeVar, Set, Any, TYPE_CHECKING
 
@@ -498,29 +499,34 @@ def method_factory(method_name, docstring):
     return method
 
 
-for method_name, method in inspect.getmembers(PyRRef):
-    # Ignore magic methods, except "__str__".
-    if method_name.startswith("_") and method_name != "__str__":
-        continue
+def _update_PyRRef_docstrings():
+    for method_name, method in inspect.getmembers(PyRRef):
+        # Ignore magic methods, except "__str__".
+        if method_name.startswith("_") and method_name != "__str__":
+            continue
 
-    # Get pybind11 generated docstring.
-    # It's like,
-    """
-    to_here(self: torch.distributed.rpc.PyRRef, timeout: float=-1.0) -> object
+        # Get pybind11 generated docstring.
+        # It's like,
+        """
+        to_here(self: torch.distributed.rpc.PyRRef, timeout: float=-1.0) -> object
 
-        Blocking call that copies the value of the RRef from the owner
-        to the local node and returns it. If the current node is the
-        owner, returns a reference to the local value.
-    """
-    docstring = getattr(method, "__doc__", None)
-    assert docstring is not None, "RRef user-facing methods should all have docstrings."
+            Blocking call that copies the value of the RRef from the owner
+            to the local node and returns it. If the current node is the
+            owner, returns a reference to the local value.
+        """
+        docstring = getattr(method, "__doc__", None)
+        assert docstring is not None, "RRef user-facing methods should all have docstrings."
 
-    # Do surgery on pybind11 generated docstrings.
-    docstring = docstring.replace("torch.distributed.rpc.PyRRef", "torch.distributed.rpc.RRef")
+        # Do surgery on pybind11 generated docstrings.
+        docstring = docstring.replace("torch.distributed.rpc.PyRRef", "torch.distributed.rpc.RRef")
 
-    # Attach user-facing RRef method with modified docstring.
-    new_method = method_factory(method_name, docstring)
-    setattr(RRef, method_name, new_method)
+        # Attach user-facing RRef method with modified docstring.
+        new_method = method_factory(method_name, docstring)
+        setattr(RRef, method_name, new_method)
+
+
+if sysconfig.get_config_var("WITH_DOC_STRINGS"):
+    _update_PyRRef_docstrings()
 
 
 @_require_initialized
