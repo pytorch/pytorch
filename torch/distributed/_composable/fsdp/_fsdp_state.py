1defeb08fd8e29f3b63a748d461746164e76a868
Andrew Gu <andgu@fb.com>
2024-01-31 06:58:36 -0800

    [FSDP2] Added `_to_kwargs` root forward input cast (#117955)
    This PR adds a `_to_kwargs()` call on the FSDP root module's forward inputs to move them to `device` similar to DDP.
    https://github.com/pytorch/pytorch/blob/39df084001c54cca5fe3174176f9b0206ddb7dcf/torch/nn/parallel/distributed.py#L1426-L1427
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117955
    Approved by: https://github.com/weifengpy
    ghstack dependencies: #117950

--- torch/distributed/_composable/fsdp/_fsdp_state.py
+++ torch/distributed/_composable/fsdp/_fsdp_state.py
@@ -8,6 +8,8 @@ from torch.distributed._composable_state import (
     _insert_module_state,
     _State,
 )
+from torch.distributed.utils import _to_kwargs
+from torch.utils.hooks import RemovableHandle
 from ._fsdp_common import TrainingState
 from ._fsdp_param import FSDPParam
 
@@ -23,6 +25,7 @@ class FSDPState(_State):
         self._fsdp_param_group: Optional[FSDPParamGroup] = None
         self._is_root: Optional[bool] = None
         self._training_state: TrainingState = TrainingState.IDLE
+        self._pre_forward_hook_handle: Optional[RemovableHandle] = None
 
         # Attributes only used on the root state:
         self._all_states: List[FSDPState] = []
@@ -32,6 +35,9 @@ class FSDPState(_State):
         _insert_module_state(module, self)
         self._module = module
         self._device = device
+        self._pre_forward_hook_handle = self._module.register_forward_pre_hook(
+            self._pre_forward, prepend=True, with_kwargs=True
+        )
 
     def _root_pre_forward(
         self, module: nn.Module, args: Tuple[Any, ...], kwargs: Dict[str, Any]
@@ -39,6 +45,13 @@ class FSDPState(_State):
         self._lazy_init()
         if not self._is_root:
             return args, kwargs
+        with torch.profiler.record_function("FSDP::root_pre_forward"):
+            if self._device.type == "cuda":
+                with torch.profiler.record_function("FSDP::inputs_to_device"):
+                    args_tuple, kwargs_tuple = _to_kwargs(
+                        args, kwargs, self._device, False
+                    )  # same as DDP
+                args, kwargs = args_tuple[0], kwargs_tuple[0]
         return args, kwargs
 
     def _lazy_init(self) -> None:
@@ -70,6 +83,13 @@ class FSDPState(_State):
         for module_name, module in root_module.named_modules():
             if module in module_to_fsdp_param_group:
                 module_to_fsdp_param_group[module]._module_fqn = module_name
+
+    def _pre_forward(
+        self, module: nn.Module, args: Tuple[Any, ...], kwargs: Dict[str, Any]
+    ) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
+        self._training_state = TrainingState.FORWARD
+        args, kwargs = self._root_pre_forward(module, args, kwargs)
+        return args, kwargs
 
 
 def _get_module_fsdp_state(module: nn.Module) -> Optional[FSDPState]:
