cd47cc132c0de7fb370c135ec8c771fee59b2345
laith sakka <lsakka@fb.com>
2024-01-31 11:27:10 -0800

    enable test_ellipsis_index_2 with Torch dynamo
    [ghstack-poisoned]

--- test/torch_np/numpy_tests/core/test_indexing.py
+++ test/torch_np/numpy_tests/core/test_indexing.py
@@ -20,7 +20,6 @@ from torch.testing._internal.common_utils import (
     skipIfTorchDynamo,
     TEST_WITH_TORCHDYNAMO,
     TestCase,
-    xfailIfTorchDynamo,
     xpassIfTorchDynamo,
 )
 
@@ -202,8 +201,11 @@ class TestIndexing(TestCase):
         b[(Ellipsis,)] = 2
         assert_equal(b, 2)
 
-    @xfailIfTorchDynamo  # numpy ndarrays do not have `.tensor` attribute
     def test_ellipsis_index_2(self):
+        # This test spefically testing torch._numpy.array()
+        import torch._numpy as np
+        from torch._numpy.testing import assert_equal
+
         a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         assert_(a[...] is not a)
         assert_equal(a[...], a)
