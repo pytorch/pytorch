0b96ccd3a08cca0bb6216d420f012a0163a33750
Michael Suo <suo@meta.com>
2024-01-31 09:06:43 -0800

    [export] support non-persistent buffers (#118722)
    Summary:
    X-link: https://github.com/pytorch/executorch/pull/1772
    
    
    Basic support for non-persistent buffers, which are buffers that do not show up in the state dict.
    
    One weird twist is that most of our other systems (FX, aot_export, dynamo) have completely buggy handling of non-persistent buffers. I tried to go on a wild goose chase to fix them all, but it got to be too much. So I introduced some sad rewrite passes in `_export` make the final state dict correctly align with the original module's state dict.
    
    This exposed some bugs/ambiguous handling of parameters/buffers in existing test code. For example, `TestSaveLoad.test_save_buffer` traced over a module that was not in the root module hierarchy and caused some weird behavior. I think we should error explicitly on use cases like this: https://github.com/pytorch/pytorch/issues/118410. For now I just rewrote the tests or skipped them.
    
    Test Plan: added a unit test
    
    Differential Revision: D53253905

--- torch/export/exported_program.py
+++ torch/export/exported_program.py
@@ -192,8 +192,12 @@ class ExportedProgram:
         Returns an iterator over original module buffers, yielding
         both the name of the buffer as well as the buffer itself.
         """
+        non_persistent_buffers = set(self.graph_signature.non_persistent_buffers)
         for buffer_name in self.graph_signature.buffers:
-            yield buffer_name, self.state_dict[buffer_name]
+            if buffer_name in non_persistent_buffers:
+                yield buffer_name, self.constants[buffer_name]
+            else:
+                yield buffer_name, self.state_dict[buffer_name]
 
     @property
     @compatibility(is_backward_compatible=False)
@@ -266,9 +270,20 @@ class ExportedProgram:
         for input_ in self.graph_signature.input_specs:
             if input_.kind == InputKind.USER_INPUT:
                 continue
-            elif input_.kind in (InputKind.PARAMETER, InputKind.BUFFER):
-                additional_inputs.append(self.state_dict[input_.target])
-            elif input_.kind in (InputKind.CONSTANT_TENSOR, InputKind.CUSTOM_OBJ):
+            elif input_.kind in (
+                InputKind.PARAMETER,
+                InputKind.BUFFER,
+            ):
+                if input_.persistent is False:
+                    # This is a non-persistent buffer, grab it from our
+                    # constants instead of the state dict.
+                    additional_inputs.append(self.constants[input_.target])
+                else:
+                    additional_inputs.append(self.state_dict[input_.target])
+            elif input_.kind in (
+                InputKind.CONSTANT_TENSOR,
+                InputKind.CUSTOM_OBJ,
+            ):
                 additional_inputs.append(self.constants[input_.target])
         additional_inputs = tuple(additional_inputs)
 
@@ -426,7 +441,12 @@ class ExportedProgram:
         }
 
         input_specs = [
-            InputSpec(spec.kind, update_arg(spec.arg, new_placeholders[i]), spec.target)
+            InputSpec(
+                spec.kind,
+                update_arg(spec.arg, new_placeholders[i]),
+                spec.target,
+                spec.persistent,
+            )
             for i, spec in enumerate(self.graph_signature.input_specs)
         ]
         output_specs = [
@@ -524,7 +544,12 @@ class ExportedProgram:
                     else type(old_input_spec.arg)(node.name)
                 )
                 new_input_specs.append(
-                    InputSpec(old_input_spec.kind, arg, old_input_spec.target)
+                    InputSpec(
+                        old_input_spec.kind,
+                        arg,
+                        old_input_spec.target,
+                        old_input_spec.persistent,
+                    )
                 )
 
             output_node = list(new_gm.graph.nodes)[-1]
