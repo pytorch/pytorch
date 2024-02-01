db72035641ce9203d01f08f4edc624a72a2aeb28
Andrew Gu <andgu@fb.com>
2024-01-31 16:29:05 -0800

    Update base for Update on "[FSDP2] Added gradient accumulation w/o reduction"
    This PR adds a way to do gradient accumulation without collectives (i.e. reduce-scatter for FSDP and reduce-scatter/all-reduce for HSDP, though HSDP is not yet implemented). Since the `no_sync()` context manager has received some feedback, we simply define a method on the module to set whether the module requires gradient synchronization or not, where this method can recurse or not.
    ```
    # Before with `no_sync()`:
    with fsdp_model.no_sync() if not is_last_microbatch else contextlib.nullcontext():
      # Forward/backward
    
    # After with a setter:
    fsdp_model.set_requires_gradient_sync(not is_last_microbatch)
    # Forward/backward
    ```
    Having the method be able to recurse or not also gives some flexibility. For example, some large modules can still reduce-scatter, while some smaller modules can avoid it to save communication bandwidth:
    ```
    fsdp_modules_to_reduce_scatter: Set[nn.Module] = ...
    for module in fsdp_model.modules():
      if isinstance(module, FSDP) and module not in fsdp_modules_to_reduce_scatter:
        module.set_requires_gradient_sync(not is_last_microbatch)
    # Forward/backward
    ```
    
    (Separately, we may expose a helper for `return [module for model.modules() if isinstance(module, FSDP)]`.)
    
    ---
    
    To show the spirit of this API choice, I also included `set_requires_all_reduce` that would give us the ability to only reduce-scatter but not all-reduce for HSDP (originally from the MiCS paper). If we want to flexibly support heterogeneous sharding where FSDP is applied to some modules and HSDP to others in the same model, then having a module-level method that has the option to not recurse makes sense to me.
    
    
    cc mrshenli pritamdamania87 zhaojuanmao satgera rohan-varma gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 penguinwu fegin XilunWu wanchaol fduwjj wz337 tianyu-l wconstab yf225
    
    
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_common.py
+++ torch/distributed/_composable/fsdp/_fsdp_common.py
@@ -1,3 +1,4 @@
+import math
 import traceback
 
 from dataclasses import dataclass
@@ -86,17 +87,8 @@ def _is_composable_with_fsdp(module: nn.Module) -> bool:
 
 
 def _get_dim0_padded_size(tensor_size: torch.Size, dim0_factor: int) -> torch.Size:
-    if tensor_size[0] % dim0_factor == 0:
-        padded_size = tensor_size
-    elif tensor_size[0] < dim0_factor:
-        padded_size = cast(torch.Size, torch.Size([dim0_factor]) + tensor_size[1:])
-    else:
-        size = (
-            torch.Size([tensor_size[0] + dim0_factor - (tensor_size[0] % dim0_factor)])
-            + tensor_size[1:]
-        )
-        padded_size = cast(torch.Size, size)
-    return padded_size
+    padded_dim0 = math.ceil(tensor_size[0] / dim0_factor) * dim0_factor
+    return cast(torch.Size, torch.Size([padded_dim0]) + tensor_size[1:])
 
 
 def _chunk_with_empty(
