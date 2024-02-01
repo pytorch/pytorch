150cdce9a046530d5cc9eac514f909c263f53545
anshgit101 <anshkhandelwal2001@gmail.com>
2024-02-01 16:28:41 -0500

    l1_loss updates

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3318,6 +3318,8 @@ def l1_loss(
 
     See :class:`~torch.nn.L1Loss` for details.
     """
+    if reduction != "sum":
+        raise Exception("Reduction is not sum")
     if has_torch_function_variadic(input, target):
         return handle_torch_function(
             l1_loss, (input, target), input, target, size_average=size_average, reduce=reduce, reduction=reduction
