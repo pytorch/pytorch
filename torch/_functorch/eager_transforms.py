8c56cbce63dbfd88f036c5d1822799ff52b9e2d4
Pierre Quinton <pierre.quinton@epfl.ch>
2024-02-01 16:49:46 +0100

    Run lint, fixes unused `Union` import in `eager_transforms.py`.

--- torch/_functorch/eager_transforms.py
+++ torch/_functorch/eager_transforms.py
@@ -6,7 +6,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Callable, Union, Tuple, List, Any, Optional
+from typing import Callable, Tuple, List, Any, Optional
 import torch
 from functools import partial, wraps
 import contextlib
