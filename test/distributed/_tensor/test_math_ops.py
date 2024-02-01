9cd8515f8ed35e8a40ed04896953ba5fc58086ad
Tianyu Liu <lty@fb.com>
2024-02-01 13:57:57 -0800

    [dtensor] add op support for nll_loss_forward
    [ghstack-poisoned]

--- test/distributed/_tensor/test_math_ops.py
+++ test/distributed/_tensor/test_math_ops.py
@@ -127,6 +127,43 @@ class DistMathOpsTest(DTensorTestBase):
             self.assertEqual(dist_x.grad.full_tensor(), x.grad)
 
     @with_comms
+    def test_nll_loss_fwd(self):
+        device_mesh = self.build_device_mesh()
+
+        channel_size, channel_dim = 16, 1
+        test_setup = [
+            (2, (8, channel_size), (8,)),  # calling aten.nll_loss_forward
+            (3, (8, channel_size, 12), (8, 12)),  # calling aten.nll_loss2d_forward
+        ]
+        for input_ndim, input_size, target_size in test_setup:
+            x = torch.rand(*input_size, device=self.device_type)
+            target = torch.randint(channel_size, target_size, device=self.device_type)
+            dist_target = distribute_tensor(target, device_mesh, [Replicate()])
+
+            shard_dims = list(range(input_ndim))
+            reductions = ["none", "mean", "sum"]
+            for shard_dim, reduction in itertools.product(shard_dims, reductions):
+                y = torch.nn.functional.nll_loss(x, target, reduction=reduction)
+                dist_x = distribute_tensor(x, device_mesh, [Shard(shard_dim)])
+                dist_y = torch.nn.functional.nll_loss(
+                    dist_x, dist_target, reduction=reduction
+                )
+                if shard_dim == channel_dim:
+                    self.assertTrue(dist_y.placements[0].is_replicate())
+                    self.assertEqual(dist_y.to_local(), y)
+                else:
+                    if reduction == "none":
+                        output_shard_dim = (
+                            shard_dim if shard_dim < channel_dim else shard_dim - 1
+                        )
+                        self.assertTrue(
+                            dist_y.placements[0].is_shard(dim=output_shard_dim)
+                        )
+                    else:
+                        self.assertTrue(dist_y.placements[0].is_partial())
+                    self.assertEqual(dist_y.full_tensor(), y)
+
+    @with_comms
     def test_full_shard_math_ops(self):
         mesh_shape = (2, self.world_size // 2)
         mesh = DeviceMesh(
