Change typehint of `argnums` in `jacrev` to `Optional[argnum)t]`. Specify the behaviour in case of `None`, i.e. differentiating w.r.t. all `inputs`. Fix the computation of basis in case of `None`.

torch/_functorch/eager_transforms.py
@@ -364,7 +364,7 @@ def error_if_complex(func_name, args, is_input):
             raise RuntimeError(err_msg)
 
 @exposed_in("torch.func")
-def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False,
+def jacrev(func: Callable, argnums: Optional[argnums_t] = 0, *, has_aux=False,
            chunk_size: Optional[int] = None,
            _preallocate_and_copy=False):
     """
@@ -379,8 +379,9 @@ def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False
     Args:
         func (function): A Python function that takes one or more arguments,
             one of which must be a Tensor, and returns one or more Tensors
-        argnums (int or Tuple[int]): Optional, integer or tuple of integers,
+        argnums (int or Tuple[int, ...]): Optional, integer or tuple of integers,
             saying which arguments to get the Jacobian with respect to.
+            If None, the Jacobian is computed with respect to all inputs.
             Default: 0.
         has_aux (bool): Flag indicating that ``func`` returns a
             ``(output, aux)`` tuple where the first element is the output of
@@ -514,7 +515,7 @@ def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False
         # Step 1: Construct grad_outputs by splitting the standard basis
         flat_output_numels = tuple(out.numel() for out in flat_output)
 
-        primals = _slice_argnums(args, argnums)
+        primals = args if argnums is None else _slice_argnums(args, argnums)
         flat_primals, primals_spec = tree_flatten(primals)
 
         def compute_jacobian_stacked():