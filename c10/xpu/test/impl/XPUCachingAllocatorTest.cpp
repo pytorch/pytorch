9c0e1389a39028f18d00cb77d21db9a0057de7a6
Yu, Guangye <guangye.yu@intel.com>
2024-02-01 15:51:09 +0000

    Update base for Update on "[1/2] Intel GPU Runtime Upstreaming for Generator"
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- c10/xpu/test/impl/XPUCachingAllocatorTest.cpp
+++ c10/xpu/test/impl/XPUCachingAllocatorTest.cpp
@@ -25,7 +25,13 @@ TEST(XPUCachingAllocatorTest, GetXPUAllocator) {
 }
 
 TEST(XPUCachingAllocatorTest, DeviceCachingAllocate) {
+  c10::xpu::XPUCachingAllocator::emptyCache();
   auto* allocator = c10::xpu::XPUCachingAllocator::get();
+  // 500M memory is reserved, can be reused later.
+  {
+    auto _500mb = 500 * 1024 * 1024;
+    auto cache = allocator->allocate(_500mb);
+  }
   auto _10mb = 10 * 1024 * 1024;
   auto buffer = allocator->allocate(_10mb);
   void* ptr0 = buffer.get();
@@ -33,13 +39,17 @@ TEST(XPUCachingAllocatorTest, DeviceCachingAllocate) {
   void* tmp = sycl::aligned_alloc_device(
       512, _10mb, c10::xpu::get_raw_device(0), c10::xpu::get_device_context());
   void* ptr1 = c10::xpu::XPUCachingAllocator::raw_alloc(_10mb);
-  // ptr0 and ptr1 should be on the same block, and ptr1 is the next block of
-  // ptr0, like [ptr0, ptr1]. So the offset of their pointers should equal to
-  // ptr0's size (10M).
+  // We have reserved 500M memory that can be reused. When we allocate ptr0
+  // and ptr1 via device caching allocator, they should be on the same block.
+  // And ptr1 is the next block of ptr0, like [ptr0, ptr1]. This is because tmp
+  // pointer is not allocated via device caching allocator so that it can NOT
+  // reuse our reserved memory. So the offset between ptr0 and ptr1 should equal
+  // to ptr0's size (10M).
   auto diff = static_cast<char*>(ptr1) - static_cast<char*>(ptr0);
   ASSERT_EQ_XPU(diff, _10mb);
   c10::xpu::XPUCachingAllocator::raw_delete(ptr1);
   sycl::free(tmp, c10::xpu::get_device_context());
+  c10::xpu::XPUCachingAllocator::emptyCache();
 }
 
 TEST(XPUCachingAllocatorTest, AllocateMemory) {
