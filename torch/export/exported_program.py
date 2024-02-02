3e49794616f69735e00cbc06fe84d97079375ff9
Angela Yi <angelayi@meta.com>
2024-02-02 16:40:01 +0000

    [export] Move _create_graph_module_for_export to torch/export (#118893)
    Summary: I have to keep the torch/_export one to not break executorch...
    
    Test Plan: CI
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D52842750
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118893
    Approved by: https://github.com/zhxchen17

--- torch/export/exported_program.py
+++ torch/export/exported_program.py
@@ -2,6 +2,7 @@ import copy
 import dataclasses
 import functools
 import types
+import warnings
 from typing import (
     Any,
     Callable,
@@ -141,8 +142,6 @@ class ExportedProgram:
             Dict[str, Union[torch.Tensor, torch._C.ScriptObject]]
         ] = None,
     ):
-        from torch._export.exported_program import _create_graph_module_for_export
-
         # Remove codegen related things from the graph. It should just be a flat graph.
         graph._codegen = torch.fx.graph.CodeGen()
         self._graph_module = _create_graph_module_for_export(root, graph)
@@ -664,3 +663,24 @@ def _get_updated_range_constraints(
         if k not in shape_env.replacements:
             range_constraints[k] = v
     return range_constraints
+
+
+def _create_graph_module_for_export(root, graph):
+    try:
+        gm = torch.fx.GraphModule(root, graph)
+    except SyntaxError:
+        # If custom objects stored in memory are being used in the graph,
+        # the generated python code will result in a syntax error on the custom
+        # object, since it is unable to parse the in-memory object. However
+        # we can still run the graph eagerly through torch.fx.Interpreter,
+        # so we will bypass this error.
+        warnings.warn(
+            "Unable to execute the generated python source code from "
+            "the graph. The graph module will no longer be directly callable, "
+            "but you can still run the ExportedProgram, and if needed, you can "
+            "run the graph module eagerly using torch.fx.Interpreter."
+        )
+        gm = torch.fx.GraphModule(root, torch.fx.Graph())
+        gm._graph = graph
+
+    return gm
