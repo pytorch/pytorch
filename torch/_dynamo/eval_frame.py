a448dfd5efda47898ad23478d4857d049e805f34
Taras Tsugrii <taras.tsugriy@gmail.com>
2024-02-02 15:36:08 -0800

    [dynamo] Simplify matched_input_elements_to_fake construction.
    It's more idiomatic and efficient.

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -840,10 +840,7 @@ class FlattenInputOutputSignature(torch.fx.interpreter.Transformer):
         super().__init__(m)
 
         assert len(flat_args_dynamic_dims) == len(flat_args)
-        matched_input_elements_to_fake = {
-            val: example_fake_inputs[ix]
-            for ix, val in enumerate(matched_input_elements_positions)
-        }
+        matched_input_elements_to_fake = dict(zip(example_fake_inputs, matched_input_elements_positions))
 
         self.new_args = []
         for i in range(0, len(flat_args)):
