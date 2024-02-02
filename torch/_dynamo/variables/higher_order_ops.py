4c93aa1e096e5e61849a807940f5507818fc078f
Animesh Jain <anijain@umich.edu>
2024-02-01 22:14:41 -0800

    Update base for Update on "[dynamo] Validate check_fn"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -395,9 +395,9 @@ def speculate_subgraph(
             with autograd_ctx:
                 output = f.call_function(tx, args, sub_kwargs)
 
-            new_side_effects = tx.output.side_effects.clone()
 
             if restore_side_effects:
+                new_side_effects = tx.output.side_effects.clone()
                 prev_side_effects.track_tensor_variables_from_runahead_side_effects(
                     new_side_effects
                 )
