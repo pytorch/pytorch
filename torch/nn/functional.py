f8e3309f690c314a44d965dc7c0e5741debcceb5
Madhukar Chavali <madhukar.chavali@gmail.com>
2024-02-01 20:24:28 -0500

    Add check for rediction mode of sum for l1_loss

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3318,6 +3318,10 @@ def l1_loss(
 
     See :class:`~torch.nn.L1Loss` for details.
     """
+    #Add check for reduction mode == sum
+    if reduction == "sum":
+         raise ValueError(reduction + " is not valid")
+
     if has_torch_function_variadic(input, target):
         return handle_torch_function(
             l1_loss, (input, target), input, target, size_average=size_average, reduce=reduce, reduction=reduction
