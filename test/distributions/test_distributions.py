f01b7f44a51f567483188da0a6d013545c13769a
CJMenart <cjmenart@gmail.com>
2024-02-02 06:34:04 -0500

    spaces after commas
    As caught by CI

--- test/distributions/test_distributions.py
+++ test/distributions/test_distributions.py
@@ -4049,8 +4049,8 @@ class TestDistributionShapes(DistributionsTestCase):
         self.assertEqual(continuous_bernoulli.log_prob(torch.ones(3, 1, 1)).size(), torch.Size((3, 3, 2)))
 
     def test_mixture_same_family_mean_shape(self):
-        mix_distribution = Categorical(torch.ones([3,1,3]))
-        component_distribution = Normal(torch.zeros([3,3,3]), torch.ones([3,3,3]))
+        mix_distribution = Categorical(torch.ones([3, 1, 3]))
+        component_distribution = Normal(torch.zeros([3, 3, 3]), torch.ones([3, 3, 3]))
         gmm = MixtureSameFamily(mix_distribution, component_distribution)
         self.assertEqual(len(gmm.mean.shape), 2)
 
