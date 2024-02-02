e1cc525e16b7c40666844ee6806f45a6a4f72ab6
rikkih <rikkih@andrew.cmu.edu>
2024-02-02 10:18:10 -0500

    Added nothing new

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3329,7 +3329,7 @@ def l1_loss(
         warnings.warn(
             f"Using a target size ({target.size()}) that is different to the input size ({input.size()}). "
             "This will likely lead to incorrect results due to broadcasting. "
-            "Please ensure they have the same size.",
+            "Please ensure they have the same size. Blah blah blah",
             stacklevel=2,
         )
     if size_average is not None or reduce is not None:
