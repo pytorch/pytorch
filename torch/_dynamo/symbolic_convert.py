72b3cef769015a0510993896157c6b329bdb2ebe
laith sakka <lsakka@fb.com>
2024-02-05 17:18:09 -0800

    Fix handling random() calls encountered inside inlined code. (#119218)
    Fix https://github.com/pytorch/pytorch/issues/118787
    
    In the compiled function, calls to random() are replaced with a single function call
    to a function that generates all the random variables .
    The random calls encountered during compilation used to be tracked inside a variable
    stored inside the instruction translator. And when there are nested translators, the tracked
    calls used to get lost when the inner instructions translator popped out.
    
    This diff fixes that by moving the tracked calla to the output graph which is shared across translators that are generating the same function.
    
    More details about the issue and why this solution is picked are in the github issue above.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119218
    Approved by: https://github.com/jansel, https://github.com/anijain2305

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -17,7 +17,7 @@ import traceback
 import types
 import typing
 import weakref
-from typing import Any, Callable, Dict, List, NamedTuple, Optional, Set, Tuple, Type
+from typing import Any, Dict, List, NamedTuple, Optional, Set, Tuple, Type
 from unittest.mock import patch
 
 import torch
@@ -588,9 +588,6 @@ class InstructionTranslatorBase(Checkpointable[InstructionTranslatorGraphState])
     inline_depth: int
     inconsistent_side_effects: bool
     current_speculation: Optional[SpeculationEntry]
-    random_calls: List[
-        Tuple[Callable[..., object], Tuple[object, ...], Dict[str, object]]
-    ]
 
     def mark_inconsistent_side_effects(self):
         """
@@ -1975,7 +1972,6 @@ class InstructionTranslatorBase(Checkpointable[InstructionTranslatorGraphState])
         self.export = export
 
         self.current_speculation = None
-        self.random_calls = []
 
         self.strict_checks_enabled = False
 
