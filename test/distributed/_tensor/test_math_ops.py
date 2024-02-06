d886d5efb8c588c3a58539aca697407e90bdbc88
Tianyu Liu <lty@fb.com>
2024-02-05 21:07:58 -0800

    [dtensor] add op support for nll_loss_backward
    [ghstack-poisoned]

--- test/distributed/_tensor/test_math_ops.py
+++ test/distributed/_tensor/test_math_ops.py
@@ -139,21 +139,25 @@ class DistMathOpsTest(DTensorTestBase):
         device_mesh = self.build_device_mesh()
         comm_mode = CommDebugMode()
 
-        channel_size, channel_dim = 16, 1
+        channel_size, channel_dim = 4, 1
         test_setup = [
-            (2, (8, channel_size), (8,)),  # calling aten.nll_loss_forward
+            (2, (4, channel_size), (4,)),  # calling aten.nll_loss_forward
             (3, (8, channel_size, 12), (8, 12)),  # calling aten.nll_loss2d_forward
         ]
         for input_ndim, input_size, target_size in test_setup:
-            x = torch.rand(*input_size, device=self.device_type)
+            x = torch.rand(*input_size, device=self.device_type, requires_grad=True)
             target = torch.randint(channel_size, target_size, device=self.device_type)
             dist_target = distribute_tensor(target, device_mesh, [Replicate()])
 
             shard_dims = list(range(input_ndim))
             reductions = ["none", "mean", "sum"]
             for shard_dim, reduction in itertools.product(shard_dims, reductions):
-                y = torch.nn.functional.nll_loss(x, target, reduction=reduction)
                 dist_x = distribute_tensor(x, device_mesh, [Shard(shard_dim)])
+                y = torch.nn.functional.nll_loss(x, target, reduction=reduction)
+                if reduction == "none":
+                    y.sum().backward()
+                else:
+                    y.backward()
                 with comm_mode:
                     dist_y = torch.nn.functional.nll_loss(
                         dist_x, dist_target, reduction=reduction
@@ -180,6 +184,12 @@ class DistMathOpsTest(DTensorTestBase):
                         else:
                             self.assertTrue(dist_y.placements[0].is_partial())
                         self.assertEqual(dist_y.full_tensor(), y)
+
+                    # if reduction == "none":
+                    #     dist_y.sum().backward()
+                    # else:
+                    #     dist_y.backward()
+                    # self.assertEqual(dist_x.grad.full_tensor(), x.grad)
 
     @with_comms
     def test_full_shard_math_ops(self):
