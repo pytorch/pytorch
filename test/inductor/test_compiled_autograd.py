b5d226740e247a662ce85e712ae353b90d1ae63f
Simon Fan <xmfan@meta.com>
2024-02-06 17:47:52 -0800

    Increase accumulate_grad_ gradient's expected refcount to account for pybind (#119068)
    Account for pybind of the op holding 1 ref when torch.ops.inductor.accumulate_grad_.default is called during run time
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119068
    Approved by: https://github.com/jansel
    ghstack dependencies: #118817, #119334

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -663,6 +663,24 @@ class TestCompiledAutograd(TestCase):
     def test_mismatch_fake_tensor_mode_dynamic_shape(self):
         self.test_mismatch_fake_tensor_mode(dynamic_shape=True)
 
+    def test_accumulate_grad_accuracy(self):
+        def fn():
+            model = torch.nn.Sequential(
+                torch.nn.Linear(2, 1, bias=False),
+                torch.nn.Linear(1, 2, bias=False),
+            )
+            x = torch.randn(2, 2)
+
+            out = model(x)
+            loss = out.sum()
+            torch.manual_seed(0)
+            loss.backward()
+
+            yield model[0].weight.grad
+            yield model[1].weight.grad
+
+        self.check_output_and_recompiles(fn, 1)
+
 
 def load_test_module(name):
     testdir = Path(__file__).absolute().parent.parent
