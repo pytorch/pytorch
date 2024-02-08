5ca8e7b41bc493ccfd8cb3bd401ef23745b0d34c
cyy <cyyever@outlook.com>
2024-02-08 12:00:00 +0800

    Add function for reading an environment variable

--- c10/util/env.h
+++ c10/util/env.h
@@ -2,18 +2,12 @@
 
 #include <c10/util/Exception.h>
 #include <cstdlib>
-#include <cstring>
 #include <optional>
 
 namespace c10::utils {
-// Reads an environment variable and returns
-// - optional<true>,              if set equal to "1"
-// - optional<false>,             if set equal to "0"
-// - nullopt,   otherwise
-//
-// NB:
-// Issues a warning if the value of the environment variable is not 0 or 1.
-inline std::optional<bool> check_env(const char* name) {
+
+// Reads an environment variable and returns the content if it is set
+inline std::optional<std::string> get_env(const char* name) {
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4996)
@@ -23,17 +17,32 @@ inline std::optional<bool> check_env(const char* name) {
 #pragma warning(pop)
 #endif
   if (envar) {
-    if (strcmp(envar, "0") == 0) {
+    return std::string(envar);
+  }
+  return std::nullopt;
+}
+
+// Reads an environment variable and returns
+// - optional<true>,              if set equal to "1"
+// - optional<false>,             if set equal to "0"
+// - nullopt,   otherwise
+//
+// NB:
+// Issues a warning if the value of the environment variable is not 0 or 1.
+inline std::optional<bool> check_env(const char* name) {
+  auto env_opt = get_env(name);
+  if (env_opt.has_value()) {
+    if (*env_opt == "0") {
       return false;
     }
-    if (strcmp(envar, "1") == 0) {
+    if (*env_opt == "1") {
       return true;
     }
     TORCH_WARN(
         "Ignoring invalid value for boolean flag ",
         name,
         ": ",
-        envar,
+        *env_opt,
         "valid values are 0 or 1.");
   }
   return std::nullopt;
