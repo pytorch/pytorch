64df7b13c286cb6df776d3befabafc086e10a354
laith sakka <lsakka@fb.com>
2024-02-09 08:55:59 -0800

    Update base for Update on "Support FunctoolsPartialVariable::get_function, fix NamedTupleVariable::as_proxy and handle call_function in get_fake_values_from_nodes"
    partially address https://github.com/pytorch/pytorch/issues/118785
    This diff fixes three things:
    1. add get_function to FunctoolsPartialVariable note that it will be available only if all args constant otherwise, 
    it would throw unimplemented in the call to asPythonConstant.  
    
    2. NamedTupleVariable takes args dispatched not as list ex: NamedTuple(a, b, c) vs NamedTuple([a, b, c]),
     hence fix that by specializing asProxy. 
    
    3. A call to create_arg from within create_proxy, changes a python NamedTuple to a function call node without 
    associating an example value! Updated get_fake_values_from_nodes to handle such case. 
     
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/testing/_internal/dynamo_test_failures.py
+++ torch/testing/_internal/dynamo_test_failures.py
@@ -438,9 +438,6 @@ dynamo_expected_failures = {
     "TestSortAndSelectCPU.test_isin_cpu_float32",  # test_sort_and_select
     "TestShapeOpsCUDA.test_flip_cuda_float32",  # test_shape_ops
     "TestShapeOpsCPU.test_flip_cpu_float32",  # test_shape_ops
-    "TestShapeOpsCPU.test_clamp_cpu_int64",  # test_shape_ops
-    "TestShapeOpsCPU.test_clamp_propagates_nans_cpu",  # test_shape_ops
-    "TestShapeOpsCPU.test_clamp_cpu_float32",  # test_shape_ops
     "TestSubclassSerialization.test_tensor_subclass_deepcopy",  # test_serialization
     "TestSubclassSerialization.test_tensor_subclass_getstate_overwrite",  # test_serialization
     "TestSubclassSerialization.test_tensor_subclass_wrapper_serialization",  # test_serialization
@@ -1176,6 +1173,7 @@ dynamo_expected_failures = {
     "TestVmapOperatorsLegacy.test_select",  # test_legacy_vmap
     "TestVmapOperatorsLegacy.test_binary_pointwise_ops",  # test_legacy_vmap
     "TestVmapAPILegacy.test_non_tensor_output_raises",  # test_legacy_vmap
+    "TestVmapBatchedGradientLegacyCPU.test_binary_cross_entropy_cpu",  # Cannot access storage of BatchedTensorImpl
     "TestVmapOperatorsLegacy.test_diagonal",  # test_legacy_vmap
     "TestVmapAPILegacy.test_nonzero_out_dims",  # test_legacy_vmap
     "TestVmapAPILegacy.test_unsupported_op_err_msg",  # test_legacy_vmap
