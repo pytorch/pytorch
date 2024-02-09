dc8e59effd11aa28f68e8f7d4e2a3b6aba4f4254
Ozan Aydin <148207261+ozanMSFT@users.noreply.github.com>
2024-02-09 16:07:16 +0100

    - sccache update

--- .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -5,13 +5,10 @@ if "%REBUILD%"=="" (
   %TMP_DIR_WIN%\bin\sccache.exe --show-stats || (
     taskkill /im sccache.exe /f /t || ver > nul
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
-    del %TMP_DIR_WIN%\bin\sccache-cl.exe || ver > nul
     if "%BUILD_ENVIRONMENT%"=="" (
-      curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache.exe --output %TMP_DIR_WIN%\bin\sccache.exe
-      curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %TMP_DIR_WIN%\bin\sccache-cl.exe
+      curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-v0.7.4.exe --output %TMP_DIR_WIN%\bin\sccache.exe
     ) else (
-      aws s3 cp s3://ossci-windows/sccache.exe %TMP_DIR_WIN%\bin\sccache.exe
-      aws s3 cp s3://ossci-windows/sccache-cl.exe %TMP_DIR_WIN%\bin\sccache-cl.exe
+      aws s3 cp s3://ossci-windows/sccache-v0.7.4.exe %TMP_DIR_WIN%\bin\sccache.exe
     )
     goto :check_sccache
   )
