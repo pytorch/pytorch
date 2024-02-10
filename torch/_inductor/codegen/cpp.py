d8e319a961bb872027f0abdc413d6beb7502ac9b
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-02-10 23:02:05 +0000

    Revert "[aot_inductor] move CppWrapperCodeGen into a separate file (#119491)"
    This reverts commit 760056bbdc552314e7e81adc45e11766ac0f333c.
    
    Reverted https://github.com/pytorch/pytorch/pull/119491 on behalf of https://github.com/DanilBaibak due to Reverted as a dependency for #119448 ([comment](https://github.com/pytorch/pytorch/pull/119491#issuecomment-1937344548))

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -3370,7 +3370,7 @@ class CppScheduling(BaseScheduling):
         return tuple(tuple(map(V.graph.sizevars.simplify, s)) for s in sizes)
 
     def get_kernel_group(self):
-        from .cpp_wrapper_cpu import CppWrapperCodeGen
+        from .wrapper import CppWrapperCodeGen
 
         self.kernel_group: Union[CppWrapperKernelGroup, KernelGroup]
         if isinstance(V.graph.wrapper_code, CppWrapperCodeGen):
