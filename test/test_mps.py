1eb5280dd118364b603e98e339fd7dfff6c125cc
Nikita Shulga <nikita.shulga@gmail.com>
2024-02-11 16:25:29 +0000

    [MPS] Enable `bfloat16` support on MacOS 14 (#119641)
    Per [MPSDataType](https://developer.apple.com/documentation/metalperformanceshaders/mpsdatatype/mpsdatatypebfloat16?changes=_11&language=objc) documentation bfloat16 are supported in MacOS Sonoma or later
    
    Added missing `MPSDataTypeBFloat16` and `MTLLanguageVersion3_1` enums to `MPSGraphSonomaOps.h`
    
    TODO: Enable more testing
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119641
    Approved by: https://github.com/Skylion007

--- test/test_mps.py
+++ test/test_mps.py
@@ -347,6 +347,7 @@ def mps_ops_modifier(ops):
         'asin',
         'atan',
         'atanh',
+        'bfloat16',
         'bool',
         'cartesian_prod',
         'cat',
@@ -809,7 +810,7 @@ def mps_ops_modifier(ops):
         'geometric_': None,
         'log_normal_': None,
         'log_normal': None,
-        'bfloat16': None,
+        'bfloat16': [] if product_version >= 14.0 else None,
         'cdouble': None,
         'double': None,
         'nn.functional.softminwith_dtype': None,
@@ -11657,7 +11658,7 @@ class TestCommon(TestCase):
     def test_tensor_creation(self, device, dtype):
         def ones(device):
             return torch.ones((2, 2), dtype=dtype, device=device)
-        if dtype not in MPS_DTYPES + [torch.complex64]:
+        if dtype not in MPS_DTYPES + ([torch.bfloat16, torch.complex64] if product_version > 14.0 else [torch.complex64]):
             with self.assertRaises(TypeError):
                 ones(device)
         else:
