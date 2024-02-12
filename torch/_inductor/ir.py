c4034ece23f1f1f2de5db7202e18e9cf7db87898
Yifu Wang <yifu@fb.com>
2024-02-11 21:11:33 -0800

    [reland] Fix estimate_nccl_collective_runtime (#118986)
    `estimate_nccl_collective_runtime` has been broken and the errors have been silently swallowed by inductor. This PR:
    - Fixes the issues described in https://github.com/pytorch/pytorch/issues/118497.
    - Adds white-box testing so future issues can be surfaced in tests.
    - Add support for native funcol IRs.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118986
    Approved by: https://github.com/yf225
    ghstack dependencies: #119102

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -7494,6 +7494,8 @@ class _CollectiveKernel(FallbackKernel):
     def create_inplace(
         cls, kernel, inputs: Union[TensorBox, List[TensorBox]], *args, **kwargs
     ) -> None:
+        cpp_kernel_name = kernel._name
+        python_kernel_name = cpp_kernel_name.replace("::", ".")
         with V.graph.fake_mode:
             (
                 example_output,
@@ -7511,6 +7513,8 @@ class _CollectiveKernel(FallbackKernel):
             non_tensor_args,
             unflatten_args,
         )
+        packed.cpp_kernel_name = cpp_kernel_name
+        packed.python_kernel_name = python_kernel_name
 
         def mark_mutation(x):
             if isinstance(x.data, BaseView):
@@ -7545,6 +7549,8 @@ class _CollectiveKernel(FallbackKernel):
     def create_out_of_place(
         cls, kernel, inputs: Union[TensorBox, List[TensorBox]], *args, **kwargs
     ):
+        cpp_kernel_name = kernel._name
+        python_kernel_name = cpp_kernel_name.replace("::", ".")
         with V.graph.fake_mode:
             (
                 example_output,
@@ -7564,6 +7570,8 @@ class _CollectiveKernel(FallbackKernel):
                 non_tensor_args,
                 unflatten_args,
             )
+            packed.cpp_kernel_name = cpp_kernel_name
+            packed.python_kernel_name = python_kernel_name
             packed.outputs = [
                 MultiOutput(
                     cls.tensor_to_layout(tensor),
@@ -7581,6 +7589,8 @@ class _CollectiveKernel(FallbackKernel):
                 non_tensor_args,
                 unflatten_args,
             )
+            packed.cpp_kernel_name = cpp_kernel_name
+            packed.python_kernel_name = python_kernel_name
             packed.outputs = [packed]
             return packed
 
