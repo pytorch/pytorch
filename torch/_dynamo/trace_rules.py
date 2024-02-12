7c090a2f8fd0267142287dadd7838a966e1dc2bf
andrewor14 <andrewor14@gmail.com>
2024-02-12 15:05:39 -0800

    [quant][pt2e][be] Rename eval_utils -> export_utils (#119725)
    It's not really eval_utils anymore, since we added some training
    related utils. Instead it should be util functions that are
    related to general export use cases.
    
    Differential Revision: [D53711494](https://our.internmc.facebook.com/intern/diff/D53711494)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119725
    Approved by: https://github.com/tugsbayasgalan

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -3127,7 +3127,7 @@ LEGACY_MOD_INLINELIST = {
     "torch._functorch.apis",
     "torch._functorch.deprecated",
     "torch._higher_order_ops.cond",
-    "torch.ao.quantization.pt2e.eval_utils",
+    "torch.ao.quantization.pt2e.export_utils",
     "torch.ao.quantization.pt2e.qat_utils",
     "torch.ao.quantization.pt2e.representation.rewrite",
     "torch.ao.quantization.pt2e.utils",
