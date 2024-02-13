096ebcca73d7e8397a0fe05e2fc981a7792b76c4
Andrew Gu <andgu@fb.com>
2024-02-13 11:42:48 -0800

    [FSDP2] Added gradient accumulation w/o reduction (#118298)
    This PR adds a way to do gradient accumulation without collectives (i.e. reduce-scatter for FSDP and reduce-scatter/all-reduce for HSDP, though HSDP is not yet implemented). Since the `no_sync()` context manager has received some feedback, we simply define a method on the module to set whether the module requires gradient synchronization or not, where this method can recurse or not.
    ```
    # Before with `no_sync()`:
    with fsdp_model.no_sync() if not is_last_microbatch else contextlib.nullcontext():
      # Forward/backward
    
    # After with a setter:
    fsdp_model.set_requires_gradient_sync(not is_last_microbatch)
    # Forward/backward
    ```
    Having the method be able to recurse or not also gives some flexibility. For example, some large modules can still reduce-scatter, while some smaller modules can avoid it to save communication bandwidth:
    ```
    fsdp_modules_to_reduce_scatter: Set[nn.Module] = ...
    for module in fsdp_model.modules():
      if isinstance(module, FSDP) and module not in fsdp_modules_to_reduce_scatter:
        module.set_requires_gradient_sync(not is_last_microbatch)
    # Forward/backward
    ```
    
    (Separately, we may expose a helper for `return [module for model.modules() if isinstance(module, FSDP)]`.)
    
    ---
    
    To show the spirit of this API choice, I also included `set_requires_all_reduce` that would give us the ability to only reduce-scatter but not all-reduce for HSDP (originally from the MiCS paper). If we want to flexibly support heterogeneous sharding where FSDP is applied to some modules and HSDP to others in the same model, then having a module-level method that has the option to not recurse makes sense to me.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118298
    Approved by: https://github.com/wconstab, https://github.com/wanchaol
    ghstack dependencies: #119550, #118136, #118223, #118755, #119825

--- test/distributed/_composable/fsdp/test_fully_shard_training.py
+++ test/distributed/_composable/fsdp/test_fully_shard_training.py
@@ -523,6 +523,105 @@ class TestFullyShardSharedParams(FSDPTest):
             self.assertEqual(losses[0], losses[1])
 
 
+class TestFullyShardGradientAccumulation(FSDPTest):
+    @property
+    def world_size(self) -> int:
+        return min(2, torch.cuda.device_count())
+
+    @skip_if_lt_x_gpu(2)
+    def test_set_requires_gradient_sync(self):
+        """
+        Tests the ``set_requires_gradient_sync`` API to exercise gradient
+        accumulation without gradient reduction. This test includes mixing with
+        gradient accumulation *with* gradient reduction.
+        """
+        self.run_subtests(
+            {
+                "reshard_after_forward": [True, False, 2],
+                # For `True`, disable reduce-scatter for all MLPs, and for
+                # `False`, only disable it for some MLPs
+                "recurse": [True, False],
+            },
+            self._test_set_requires_gradient_sync,
+        )
+
+    def _test_set_requires_gradient_sync(
+        self,
+        reshard_after_forward: Union[bool, int],
+        recurse: bool,
+    ):
+        torch.manual_seed(42)
+        local_batch_size, lin_dim, num_mlps, num_microbatches = (2, 32, 3, 3)
+        global_batch_size = local_batch_size * self.world_size
+        if not recurse:
+            num_mlps_to_disable_reduce_scatter = 2
+        model = nn.Sequential(
+            *[MLP(lin_dim, torch.device("cpu")) for _ in range(num_mlps)]
+        )
+        ref_model = copy.deepcopy(model).cuda()
+        fully_shard_fn = functools.partial(
+            fully_shard, reshard_after_forward=reshard_after_forward
+        )
+        for mlp in model:
+            fully_shard_fn(mlp)
+        fully_shard_fn(model)
+        ref_optim = torch.optim.Adam(ref_model.parameters(), lr=1e-2)
+        optim = torch.optim.Adam(model.parameters(), lr=1e-2)
+        orig_reduce_scatter = dist.reduce_scatter_tensor
+        reduce_scatter_count = 0
+
+        def reduce_scatter_with_count(*args, **kwargs):
+            nonlocal reduce_scatter_count
+            reduce_scatter_count += 1
+            return orig_reduce_scatter(*args, **kwargs)
+
+        torch.manual_seed(1)  # same on all ranks
+        for iter_idx in range(5):
+            with patch_reduce_scatter(reduce_scatter_with_count):
+                for microbatch_idx in range(num_microbatches):
+                    is_last_microbatch = microbatch_idx == num_microbatches - 1
+                    if recurse:
+                        model.set_requires_gradient_sync(is_last_microbatch)
+                    else:
+                        for mlp in model[:num_mlps_to_disable_reduce_scatter]:
+                            mlp.set_requires_gradient_sync(
+                                is_last_microbatch, recurse=False
+                            )
+                    global_inp = torch.rand((global_batch_size, lin_dim), device="cuda")
+                    local_inp = global_inp[
+                        self.rank
+                        * local_batch_size : (self.rank + 1)
+                        * local_batch_size
+                    ].detach()
+                    losses: List[torch.Tensor] = []
+                    for _model, _optim, inp in (
+                        (ref_model, ref_optim, global_inp),
+                        (model, optim, local_inp),
+                    ):
+                        losses.append(_model(inp).sum())
+                        losses[-1].backward()
+                    dist.all_reduce(losses[1])  # partial -> replicated
+                    self.assertEqual(losses[0], losses[1])
+            # Expect one reduce-scatter per MLP on the last microbatch
+            expected_reduce_scatter_count = num_mlps
+            if not recurse:
+                # Expect additional reduce-scatters for non-disabled MLPs
+                expected_reduce_scatter_count += (
+                    num_mlps - num_mlps_to_disable_reduce_scatter
+                ) * (num_microbatches - 1)
+            self.assertEqual(reduce_scatter_count, expected_reduce_scatter_count)
+            reduce_scatter_count = 0
+            for param in ref_model.parameters():
+                if param.grad is not None:
+                    param.grad.div_(self.world_size)
+            check_1d_sharded_parity(self, ref_model, model)
+            for _optim in (optim, ref_optim):
+                _optim.step()
+                # When `set_to_none=False`, we are exercising mixing
+                # gradient accumulation with and without communication
+                _optim.zero_grad(set_to_none=(iter_idx % 2))
+
+
 class TestFullyShard2DTraining(FSDPTest):
     @property
     def world_size(self) -> int:
