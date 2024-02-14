7cc4d53c44ca279597d9ee2293264c7062c77f60
lancerts <tangshao28@gmail.com>
2024-02-13 21:47:38 -0800

    amend the lint error

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -968,8 +968,8 @@ class ReduceLROnPlateau(LRScheduler):
         patience (int): The number of allowed epochs with no improvement after
             which the learning rate will be reduced. For example, if `patience = 2`,
             then we will tolerate 2 epochs with no improvement, and will only
-            decrease the LR after the 3rd epoch (starting from the 4th epoch) 
-            if the loss still hasn't improved. In other words, the learning rate reduction 
+            decrease the LR after the 3rd epoch (starting from the 4th epoch)
+            if the loss still hasn't improved. In other words, the learning rate reduction
             occurs after `patience + 1` epochs without improvement. Default: 10.
         threshold (float): Threshold for measuring the new optimum,
             to only focus on significant changes. Default: 1e-4.
