a9420aeecdff3a68eef6d49829edb585afb9dbf1
Oguz Ulgen <oulgen@meta.com>
2024-02-13 17:02:18 -0800

    Add support for while-loops in ttir analysis (#119838)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119838
    Approved by: https://github.com/aakhundov
    ghstack dependencies: #119834, #119835, #119836

--- torch/_higher_order_ops/triton_kernel_wrap.py
+++ torch/_higher_order_ops/triton_kernel_wrap.py
@@ -169,7 +169,6 @@ def parse_ttir(ttir, kwargs):
     parser which further makes parsing much simpler.
     """
     # TODO(oulgen):
-    # - Support parsing while loops
     # - Support closures (e.g. "tt.reduce")
 
     try:
@@ -195,12 +194,15 @@ def parse_ttir(ttir, kwargs):
 
         func_block: "tt.func" ("public"|"private") FN_NAME "(" /.+/ NEWLINE stmt* "}" LOC -> process_func
 
-        ?stmt: op | if | for | label_stmt | cf_stmt
+        ?stmt: op | if | for | while | condition_stmt | label_stmt | cf_stmt
 
         if: [assign_lhs "="] "scf.if" args rest stmt* "}" "else" "{" stmt* "}" LOC -> process_if
         for: [assign_lhs "="] "scf.for" args rest stmt* "}" LOC -> process_for
+        while: [assign_lhs "="] "scf.while" args rest stmt* "}" "do" "{" stmt* "}" LOC -> process_while
 
+        condition_stmt: "scf.condition" "(" arg ")" args rest
         label_stmt: LABEL ":" "// pred:" LABEL
+                  | LABEL "(" /.+/ NEWLINE
         cf_stmt: "cf" "." NAME /.+/ NEWLINE
 
         op: OP_NAME LOC
@@ -324,6 +326,9 @@ def parse_ttir(ttir, kwargs):
             return self._process_scf(ret, stmts)
 
         def process_for(self, ret, _args, _rest, *stmts):
+            return self._process_scf(ret, stmts)
+
+        def process_while(self, ret, _args, _rest, *stmts):
             return self._process_scf(ret, stmts)
 
     parser = Lark(
