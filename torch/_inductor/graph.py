5887e11c117df2fce343d14bdd10a1af3550a8f8
Yang Chen <yangche@fb.com>
2024-02-13 19:45:12 -0800

    Revert "Revert "[aot_inductor] move CppWrapperCodeGen into a separate file (#119491)""
    This reverts commit d8e319a961bb872027f0abdc413d6beb7502ac9b.
    
    [ghstack-poisoned]

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -30,8 +30,9 @@ from .codegen.common import (
     get_wrapper_codegen_for_device,
     register_backend_for_device,
 )
+from .codegen.cpp_wrapper_cpu import CppWrapperCodeGen
 from .codegen.cpp_wrapper_cuda import CudaWrapperCodeGen
-from .codegen.wrapper import CppWrapperCodeGen, WrapperCodeGen
+from .codegen.wrapper import WrapperCodeGen
 from .exc import (
     CppWrapperCodeGenError,
     LoweringException,
