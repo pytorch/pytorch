79548dbb75dd8e700bf67b93b328ea8b1bcf321d
Yang Chen <yangche@fb.com>
2024-02-14 00:37:46 -0800

    Update on "Revert "Revert "[aot_inductor] move CudaWrapperCodeGen into a separate file (#119448)"""
    This reverts commit 3ab08946d5052eaeda11d683d6a58e801a032755.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- tools/amd_build/build_amd.py
+++ tools/amd_build/build_amd.py
@@ -201,8 +201,8 @@ hipify_python.hipify(
     includes=includes,
     ignores=ignores,
     extra_files=[
-        "torch/_inductor/codegen/cpp_wrapper_cuda.py"
-        "torch/_inductor/codegen/wrapper.py"
+        "torch/_inductor/codegen/cpp_wrapper_cuda.py",
+        "torch/_inductor/codegen/wrapper.py",
     ],
     out_of_place_only=args.out_of_place_only,
     hip_clang_launch=is_hip_clang(),
