71a516abc14d2884ca0c47f1ac8c4794187f8ba7
Yifu Wang <yifu@fb.com>
2024-02-16 00:25:59 -0800

    Make tests using CommDebugMode work for both legacy and native funcol
    [ghstack-poisoned]

--- torch/distributed/_tensor/debug/comm_mode.py
+++ torch/distributed/_tensor/debug/comm_mode.py
@@ -5,7 +5,18 @@ import torch
 from torch.utils._python_dispatch import TorchDispatchMode
 
 
-funcol = torch.ops.c10d_functional
+native = torch.ops._c10d_functional
+legacy = torch.ops.c10d_functional
+
+NATIVE_TO_LEGACY_MAPPING = {
+    native.all_gather_into_tensor: legacy.all_gather_into_tensor,
+    native.all_gather_into_tensor_coalesced: legacy.all_gather_into_tensor_coalesced,
+    native.all_reduce: legacy.all_reduce,
+    native.all_to_all_single: legacy.all_to_all_single,
+    native.broadcast: legacy.broadcast,
+    native.reduce_scatter_tensor: legacy.reduce_scatter_tensor,
+    native.reduce_scatter_tensor_coalesced: legacy.reduce_scatter_tensor_coalesced,
+}
 
 
 class CommDebugMode(TorchDispatchMode):
@@ -30,15 +41,10 @@ class CommDebugMode(TorchDispatchMode):
 
     def __init__(self):
         self.comm_counts: Dict[Any, int] = defaultdict(int)
-        self.comm_registry = {
-            funcol.all_gather_into_tensor,
-            funcol.all_gather_into_tensor_coalesced,
-            funcol.all_reduce,
-            funcol.all_to_all_single,
-            funcol.broadcast,
-            funcol.reduce_scatter_tensor,
-            funcol.reduce_scatter_tensor_coalesced,
-        }
+        self.comm_registry = set()
+        for native_op, legacy_op in NATIVE_TO_LEGACY_MAPPING.items():
+            self.comm_registry.add(native_op)
+            self.comm_registry.add(legacy_op)
 
     def get_total_counts(self) -> int:
         return sum(self.comm_counts.values())
@@ -63,7 +69,16 @@ class CommDebugMode(TorchDispatchMode):
         kwargs = kwargs if kwargs else {}
         out = func(*args, **kwargs)
         func_packet = func._overloadpacket
+        # We have many tests that use CommDebugMode to verify the occurrence of
+        # collectives. These tests do so by querying comm_counts with legacy
+        # funcol ops as key. For the purpose of native funcol migration, we
+        # need these tests to work for both legacy and native funcol. To avoid
+        # the need to modify all tests to accommodate the two implementations,
+        # we make CommDebugMode translate native funcol ops into legacy funcol
+        # ops until the migration finishes.
         if func_packet in self.comm_registry:
+            if func_packet in NATIVE_TO_LEGACY_MAPPING:
+                func_packet = NATIVE_TO_LEGACY_MAPPING[func_packet]
             self.comm_counts[func_packet] += 1
 
         return out
