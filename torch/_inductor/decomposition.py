ba76aef3eb13da73548f3cda429cbc999726d823
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-02-16 17:44:59 +0000

    Revert "Add pixel_shuffle to core aten decomps (#119899)"
    This reverts commit 9201d7335a25d9a91e10c1914c399419af0bd7c3.
    
    Reverted https://github.com/pytorch/pytorch/pull/119899 on behalf of https://github.com/huydhn due to Sorry for reverting your change but keep the diff D53766709 around while investigating the failed tests is not a good practice and could lead to out of sync issue, so it is better to revert and reland this ([comment](https://github.com/pytorch/pytorch/pull/119899#issuecomment-1948970686))

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -57,6 +57,7 @@ inductor_decompositions = get_decompositions(
         aten.native_batch_norm,
         aten.native_group_norm,
         aten.native_layer_norm,
+        aten.pixel_shuffle,
         aten._softmax,
         aten.sin_,
         aten.sqrt_,
