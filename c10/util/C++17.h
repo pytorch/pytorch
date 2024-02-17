ce54527a1c65eacddd4ddb78734152481f062a5b
cyy <cyyever@outlook.com>
2024-02-17 14:28:17 +0000

    Remove unused c10/util/C++17.h inclusion and outdated checks (#120149)
    This is a continued work to clean up pre-C++17 code.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120149
    Approved by: https://github.com/ezyang

--- c10/util/C++17.h
+++ c10/util/C++17.h
@@ -8,17 +8,6 @@
 #include <type_traits>
 #include <utility>
 
-#if !defined(__clang__) && !defined(_MSC_VER) && defined(__GNUC__) && \
-    __GNUC__ < 5
-#error \
-    "You're trying to build PyTorch with a too old version of GCC. We need GCC 5 or later."
-#endif
-
-#if defined(__clang__) && __clang_major__ < 4
-#error \
-    "You're trying to build PyTorch with a too old version of Clang. We need Clang 4 or later."
-#endif
-
 #if (defined(_MSC_VER) && (!defined(_MSVC_LANG) || _MSVC_LANG < 201703L)) || \
     (!defined(_MSC_VER) && __cplusplus < 201703L)
 #error You need C++17 to compile PyTorch
