d3615ea1e641d3a2eff489616d986fd3a3598c0e
Animesh Jain <anijain@umich.edu>
2024-02-17 14:31:19 -0800

    Update base for Update on "[dynamo][guards-cpp-refactor] TypeGuardAccessor"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/csrc/dynamo/guards.cpp
+++ torch/csrc/dynamo/guards.cpp
@@ -955,28 +955,6 @@ class DATA_PTR_MATCH : public LeafGuard {
   py::object _data_ptr;
 };
 
-class DICT_VERSION : public LeafGuard {
- public:
-  DICT_VERSION(py::object value, py::object verbose_code_parts)
-      : LeafGuard(verbose_code_parts) {
-    if (!PyDict_Check(value.ptr())) {
-      throw py::type_error("DICT_VERSION expects a dict");
-    }
-    _tag = get_dict_version(value.ptr());
-  }
-  bool check_nopybind(PyObject* value) override { // borrowed ref
-    return PyDict_Check(value) && get_dict_version(value) == _tag;
-  }
-
- private:
-  int64_t get_dict_version(PyObject* dict) {
-    return ((PyDictObject*)dict)->ma_version_tag;
-  }
-
-  // Saved dict version.
-  int64_t _tag;
-};
-
 /**
  * Relational guards compare more than one value. We implement Relational
  * guards by capturing some state in the guard object. For example for tensor
@@ -1811,10 +1789,6 @@ PyObject* torch_c_dynamo_guards_init() {
       NO_TENSOR_ALIASING,
       LeafGuard,
       std::shared_ptr<NO_TENSOR_ALIASING>>(py_m, "NO_TENSOR_ALIASING");
-  py::class_<DICT_VERSION, LeafGuard, std::shared_ptr<DICT_VERSION>>(
-      py_m, "DICT_VERSION")
-      .def(py::init<py::object, py::list>())
-      .def("__call__", &DICT_VERSION::check);
 
   // Guard Accessors - These are present so that we can iterate over the
   // GuardManager hierarchy. We intentionally do not provide even an init
@@ -1906,14 +1880,6 @@ PyObject* torch_c_dynamo_guards_init() {
              py::object verbose_code_parts) -> void {
             self.add_leaf_guard(
                 std::make_shared<DATA_PTR_MATCH>(data_ptr, verbose_code_parts));
-          })
-      .def(
-          "add_dict_version_guard",
-          [](GuardManager& self,
-             py::object value,
-             py::object verbose_code_parts) -> void {
-            self.add_leaf_guard(
-                std::make_shared<DICT_VERSION>(value, verbose_code_parts));
           })
       // return by reference because GuardManager has the ownership of accessors
       // and guard managers
