b800ba7a5348ccb32a76990349a562aba2fde0a2
Anthony Alayo <anthony.alayo@applovin.com>
2024-02-19 00:56:24 +0000

    Updating sleef submodule to eb3d97785 to fix export errors (#119953)
    Fixes #119952 with submodule updates
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119953
    Approved by: https://github.com/ezyang

--- aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h
+++ aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h
@@ -265,7 +265,7 @@ public:
     }
     return b;
   }
-  Vectorized<T> map(const __m256 (*const vop)(__m256)) const {
+  Vectorized<T> map(__m256 (*const vop)(__m256)) const {
     __m256 lo, hi;
     cvt_to_fp32<T>(values, lo, hi);
     const auto o1 = vop(lo);
