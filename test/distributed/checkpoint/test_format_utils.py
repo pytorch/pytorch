1c1028ac49b07a9f029f8a14141618cc211edb04
Lucas Pasqualin <lpasqualin@meta.com>
2024-02-20 08:56:58 -0800

    [DCP] Adds utility for converting torch save to dcp (#119815)
    as title
    
    Differential Revision: [D53718040](https://our.internmc.facebook.com/intern/diff/D53718040/)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119815
    Approved by: https://github.com/fegin
    ghstack dependencies: #119813, #119814

--- test/distributed/checkpoint/test_format_utils.py
+++ test/distributed/checkpoint/test_format_utils.py
@@ -2,7 +2,10 @@
 
 import torch
 import torch.distributed.checkpoint as dcp
-from torch.distributed.checkpoint.format_utils import dcp_to_torch_save
+from torch.distributed.checkpoint.format_utils import (
+    dcp_to_torch_save,
+    torch_save_to_dcp,
+)
 from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     DTensorTestBase,
@@ -24,6 +27,20 @@ class TestFormatUtils(DTensorTestBase):
 
         loaded_sd = torch.load(torch_path)
         self.assertEqual(loaded_sd, {"model": model.state_dict()})
+
+    @with_temp_dir
+    def test_torch_save_to_dcp(self) -> None:
+        model = Transformer(ModelArgs())
+        sd = {"model": model.state_dict()}
+        torch_path = self.temp_dir + "/model.pt"
+        torch.save(sd, torch_path)
+
+        torch_save_to_dcp(torch_path, self.temp_dir)
+
+        model = Transformer(ModelArgs())
+        dcp.load({"model": model}, checkpoint_id=self.temp_dir)
+
+        self.assertEqual({"model": model.state_dict()}, sd)
 
 
 if __name__ == "__main__":
