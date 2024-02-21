5404978b1d34b67f64ba54364ebddf5b5761b4dc
soulitzer <soulitzer@gmail.com>
2024-02-20 22:43:57 -0500

    [do not review] test_pynode_destruction_deadlock 5
    [ghstack-poisoned]

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -8753,7 +8753,7 @@ get_out().sum().backward()
 # This used to deadlock when the PyNode is being destroyed after
 # the error is raised.
 """
-        # 4
+        # 5
         try:
             subprocess.check_output(
                 [sys.executable, '-c', script],
