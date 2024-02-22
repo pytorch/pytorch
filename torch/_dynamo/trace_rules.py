28d2c287312b0ae1a7774e34628db53b8771ed2a
Edward Z. Yang <ezyang@meta.com>
2024-02-21 20:29:43 -0800

    Handle guard_size_oblivious in user code
    Fixes https://github.com/pytorch/pytorch/issues/120083
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -187,6 +187,7 @@ manual_torch_name_rule_map = {
     "torch._C._functorch._add_batch_dim": TorchInGraphFunctionVariable,
     "torch._C._functorch._remove_batch_dim": TorchInGraphFunctionVariable,
     "torch._C._functorch.is_batchedtensor": TorchInGraphFunctionVariable,
+    "torch.fx.experimental.symbolic_shapes.guard_size_oblivious": TorchInGraphFunctionVariable,
 }
 
 
