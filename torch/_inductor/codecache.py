445a3ff9213e2e6c3eb4bc7a334233b88e7e66b9
Sergii Dymchenko <sdym@meta.com>
2024-02-22 17:42:03 -0800

    Fix lint after https://github.com/pytorch/pytorch/pull/105590

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -953,7 +953,8 @@ class VecISA:
     # In fbcode however, we are using the same compiler for pytorch and for inductor codegen,
     # making the runtime check unnecessary.
     _avx_code = """
-#if defined(CPU_CAPABILITY_AVX512) || defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_ZVECTOR) || defined(CPU_CAPABILITY_NEON)
+#if defined(CPU_CAPABILITY_AVX512) || defined(CPU_CAPABILITY_AVX2)\
+ || defined(CPU_CAPABILITY_ZVECTOR) || defined(CPU_CAPABILITY_NEON)
 #include <ATen/cpu/vec/functional.h>
 #include <ATen/cpu/vec/vec.h>
 #endif
