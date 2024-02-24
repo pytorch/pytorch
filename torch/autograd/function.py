fb88ee47dcc230fd1e7d1d428ba3b85fd849b5f1
Edward Z. Yang <ezyang@meta.com>
2024-02-24 15:14:01 -0800

    Update base for Update on "Don't use size on TensorVariable when doing out resize test"
    Fixes https://github.com/pytorch/pytorch/issues/120482
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 aakhundov kadeng
    
    [ghstack-poisoned]

--- torch/autograd/function.py
+++ torch/autograd/function.py
@@ -497,7 +497,6 @@ class Function(_SingleLevelFunction):
             "Instantiating an autograd function will raise an "
             "error in a future version of PyTorch.",
             DeprecationWarning,
-            stacklevel=2,
         )
 
     def __call__(self, *args, **kwargs):
