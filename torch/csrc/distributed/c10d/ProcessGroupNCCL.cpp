381a7ad3f1cd38bf8e814ae9d275f101a2136139
Will Constable <whc@meta.com>
2024-02-28 13:04:21 -0800

    [C10D] Add ProcessGroup op_id to track ops inside coalescing region (#120745)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120745
    Approved by: https://github.com/zdevito
    ghstack dependencies: #120724, #120270

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -2185,6 +2185,7 @@ c10::intrusive_ptr<ProcessGroupNCCL::WorkNCCL> ProcessGroupNCCL::initWork(
     r->trace_id_ = NCCLTraceBuffer::get()->record(
         uid_,
         seq_,
+        op_id_,
         profilingTitle ? profilingTitle : "",
         inputs,
         outputs,
@@ -2253,6 +2254,9 @@ void ProcessGroupNCCL::startCoalescing() {
   // start, which has one minor downside- we burn a seq_ if someone ever does a
   // 'start' and 'end' coalescing region without doing an operation inbetween.
   seq_++;
+
+  // Don't bump op_id_ here, becuase startCoalescing isn't a logical operation.
+  // Bump it for each logical op inside the coalescing group.
 }
 
 // `optype` is for specifying a composite optype, such as ALLGATHER and
@@ -2344,6 +2348,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collective(
 
   // Bump collective counter
   seq_++;
+  op_id_++;
 
   auto device = getDevice(input);
   const auto key = getKeyFromDevice(device);
@@ -2489,6 +2494,12 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collectiveCoalesced(
 
   // Bump collective counter
   seq_++;
+  // For coalescingManager collectives, there is no individual c++ call per
+  // collective so there is no flight record and we increment seq_ and op_id_
+  // together. Compare this to startCoalesing/endCoalescing flow where we
+  // increment seq_ once per group and increment op_id_ once per indvidual
+  // operation within the group
+  op_id_++;
 
   // Currently, the API permits one scenario where inputs.size() and
   // outputs.size() are > 0.
@@ -2676,6 +2687,10 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::pointToPoint(
     }
   }
 
+  // Bump the logical operation counter regardless of whether this op is
+  // coalesced or individual
+  op_id_++;
+
   auto ncclComm = getNCCLComm(key, device, opType, p2pRank, isSendRecvSelf);
 
   if (coalescing_state_ & CoalActive) {
@@ -2698,7 +2713,14 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::pointToPoint(
     // timing/state updates via watchdog thread, but lacks op metadata such as
     // input/output sizes and profilingTitle per-op in the group.
     auto trace_id = NCCLTraceBuffer::get()->record(
-        uid_, seq_, profilingTitle, {tensor}, {tensor}, nullptr, nullptr);
+        uid_,
+        seq_,
+        op_id_,
+        profilingTitle,
+        {tensor},
+        {tensor},
+        nullptr,
+        nullptr);
     // TODO(whc) if we want to make the per-p2p-op flightrecorder entries get
     // their timings/states updated by proxy when the Work obj representing the
     // coalesce group gets its update, we could accumulate these trace_ids
@@ -2723,6 +2745,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::pointToPoint(
     work->trace_id_ = NCCLTraceBuffer::get()->record(
         uid_,
         seq_,
+        op_id_,
         profilingTitle,
         {tensor},
         {tensor},
