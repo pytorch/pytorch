687c2c130cc1dac3824254ec03db1f6ccbed9602
lancerts <tangshao28@gmail.com>
2024-02-28 14:53:07 -0800

    include the error message in try_config

--- aten/src/ATen/native/cudnn/Conv_v8.cpp
+++ aten/src/ATen/native/cudnn/Conv_v8.cpp
@@ -916,9 +916,12 @@ bool try_configs(
       benchmark_cache.update(key, plan);
       return true;
     } catch (cudnn_frontend::cudnnException& e) {
+      TORCH_WARN("Plan failed with a cudnnException: ", e.what());
     } catch (CuDNNError& e) {
+      TORCH_WARN("Plan failed with a CuDNNError: ", e.what());
     } catch (c10::OutOfMemoryError& e) {
       (void)cudaGetLastError(); // clear CUDA error
+      TORCH_WARN("Plan failed with an OutOfMemoryError: ", e.what());
     }
   }
   return false;
