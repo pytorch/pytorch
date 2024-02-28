763021c48747af263c9ffd3dff8df3c10d8c366b
Adnan Akhundov <aakhundov@meta.com>
2024-02-27 21:30:03 -0800

    [inductor] Do not reuse buffers across  scopes in mem planning
    Summary: Previously, in the `memory_plan_reuse` we assumed that the generated code is flat: in the sense of it can't have nested scopes. However, with nested control flow codegen-ing, this is no longer the case. This causes bugs in buffers being reused across the visibility boundaries in different nested scopes.
    
    In this PR, we add nested planning states in `memory_plan_reuse` on entering and exiting scope in the codegen. This restricts the buffer reusability only to the currently active (peak) scope / planning state.
    
    Test Plan:
    
    ```
    python test/inductor/test_control_flow.py -k test_subgraphs_with_parameters
    ...
    ----------------------------------------------------------------------
    Ran 27 tests in 149.413s
    
    OK
    ```
    
    [ghstack-poisoned]

--- test/inductor/test_control_flow.py
+++ test/inductor/test_control_flow.py
@@ -267,6 +267,42 @@ class CondTests(TestCase):
         )
 
     @requires_cuda
+    def test_subgraphs_with_parameters(self):
+        # nested Modules with parameters
+        class InnerModel1(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.layer = torch.nn.Linear(20, 30, device="cuda")
+
+            def forward(self, x):
+                return self.layer(x + 1) * 3.14
+
+        class InnerModel2(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.layer1 = torch.nn.Linear(20, 10, device="cuda")
+                self.layer2 = torch.nn.Linear(10, 30, device="cuda")
+
+            def forward(self, x):
+                return self.layer2(self.layer1(x - 2)) * 3.14
+
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.true_fn = InnerModel1()
+                self.false_fn = InnerModel2()
+
+            def forward(self, p, a):
+                return torch.cond(p, self.true_fn, self.false_fn, [a])
+
+        self._run_test(
+            model=Model(),
+            inputs=(torch.randn(10, 20),),
+            device="cuda",
+            dynamic=True,
+        )
+
+    @requires_cuda
     def test_aliasing_outputs(self):
         # output aliasing in subgraphs: not supported
         class Model(torch.nn.Module):
