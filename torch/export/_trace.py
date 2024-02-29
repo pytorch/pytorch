574ad2898ec7f19f1d4f9d788bef594f039e2af9
pytorchbot <pytorchbot@pytorch.com>
2024-02-29 07:33:43 +0000

    2024-02-29 nightly release (f5b99976adcbb01fd71bd0a39ea15bdac6c9e48a)

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -762,7 +762,7 @@ def _export(
             fake_mode,
             fake_args,
             fake_kwargs,
-            src_equalities,
+            equalities_inputs,
             original_signature,
         ) = make_fake_inputs(f, args, kwargs, constraints)
 
@@ -778,9 +778,16 @@ def _export(
             pre_dispatch=pre_dispatch,
             transform=_tuplify_outputs,
         )
-        range_constraints, equality_constraints = make_constraints(
-            fake_mode, src_equalities, original_signature, ep_non_strict.gm
-        )
+        try:
+            range_constraints = make_constraints(
+                fake_mode,
+                equalities_inputs,
+                original_signature,
+                ep_non_strict.gm,
+            )
+        except (ConstraintViolationError, ValueRangeError) as e:
+            raise UserError(UserErrorType.CONSTRAINT_VIOLATION, str(e))  # noqa: TRY200
+
         assert out_spec is not None
 
         gm = ep_non_strict.gm
@@ -975,6 +982,7 @@ def _export(
         len(export_graph_signature.input_specs),
     )
     range_constraints = _process_constraints(
+        dynamo_fake_mode,
         gm,
         num_lifted,
         flat_args,
