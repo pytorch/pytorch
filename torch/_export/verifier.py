7b61a220c12ff93b4a5ad5790e349a37a31cb365
Pian Pawakapan <pianpwk@meta.com>
2024-02-29 13:16:52 -0800

    don't assume top-level module is index=0

--- torch/_export/verifier.py
+++ torch/_export/verifier.py
@@ -36,7 +36,7 @@ def check_nn_module_stack(graph_module: torch.fx.GraphModule) -> None:
     Perform nn_module_stack checks on the graph.
     Current constraints:
         For the top level graph:
-        - populated for 'call_method', 'call_function', 'get_attr', 'call_module'
+        - populated for 'call_function', 'get_attr'
         - None for 'placeholder', 'output'
         For submodule graphs:
         - None for 'placeholder', output'
@@ -44,16 +44,16 @@ def check_nn_module_stack(graph_module: torch.fx.GraphModule) -> None:
     TODO(pianpwk): extend this check to get_attr & call_module nodes.
     TODO(pianpwk): make this a consistent node-level check once nn_module_stack is populated for cond submodules.
     '''
-    # Check top-level graph for all nodes, submodule graphs for placeholder & output nodes
-    for i, mod in enumerate(graph_module.modules()):
+    # Check top-level graph for all nodes, all graphs for placeholder & output nodes
+    for i, mod in enumerate([graph_module] + list(graph_module.modules())):
         for node in mod.graph.nodes:
-            if node.op in ['call_method', 'call_function', 'get_attr', 'call_module']:
+            if node.op in ['call_function', 'get_attr']:
                 if i == 0:
                     if node.meta.get('nn_module_stack', None) is None:
                         raise SpecViolationError(
                             f"Node {node} of type {node.op} is missing nn_module_stack metadata"
                         )
-            else:
+            elif node.op in ['placeholder', 'output']:
                 if node.meta.get('nn_module_stack', None):
                     raise SpecViolationError(
                         f"Node {node} of type {node.op} contains nn_module_stack metadata, this should be None"
