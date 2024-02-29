2656c994b939f89e9a703f9574b5d9bb1c1f5633
Jane Xu <janeyx@meta.com>
2024-02-29 17:26:22 +0000

    Update (base update)
    [ghstack-poisoned]

--- test/inductor/test_triton_kernels.py
+++ test/inductor/test_triton_kernels.py
@@ -15,6 +15,7 @@ from torch._higher_order_ops.triton_kernel_wrap import (
     triton_kernel_wrapper_mutation,
 )
 from torch._inductor import metrics
+from torch._inductor.utils import run_and_get_code
 from torch.testing._internal import common_utils
 from torch.testing._internal.common_utils import skipIfRocm
 
@@ -956,6 +957,53 @@ def forward(self, x_1, output_1):
         x = torch.randn(4, device="cuda")
         eager_out = f(x)
         compiled_out = torch.compile(f, fullgraph=True, backend=backend)(x)
+        self.assertEqual(compiled_out, eager_out)
+
+    @requires_cuda
+    @skipIfRocm
+    @common_utils.parametrize("dynamic", [False, True])
+    def test_triton_kernel_equal_to_1_arg(self, dynamic):
+        @triton.jit
+        def add_kernel_half_n_elements(
+            in_ptr0,
+            in_ptr1,
+            out_ptr,
+            half_n_elements,
+            BLOCK_SIZE: "tl.constexpr",
+        ):
+            pid = tl.program_id(axis=0)
+            block_start = pid * BLOCK_SIZE
+            offsets = block_start + tl.arange(0, BLOCK_SIZE)
+            mask = offsets < half_n_elements * 2
+            x = tl.load(in_ptr0 + offsets, mask=mask)
+            y = tl.load(in_ptr1 + offsets, mask=mask)
+            output = x + y
+            tl.store(out_ptr + offsets, output, mask=mask)
+
+        def f(x, y):
+            out = torch.empty_like(x)
+            half_n_elements = x.numel() // 2
+            add_kernel_half_n_elements[(half_n_elements,)](
+                x, y, out, half_n_elements, BLOCK_SIZE=16
+            )
+            return out
+
+        x = torch.randn(2, device="cuda")
+        y = torch.randn(2, device="cuda")
+        eager_out = f(x, y)
+        compiled_out, sources = run_and_get_code(
+            torch.compile(f, dynamic=dynamic), x, y
+        )
+
+        if dynamic:
+            # when half_n_elements passed to the Triton kernel is
+            # dynamic, equal_to_1 specializaiton can't be enforced
+            self.assertTrue("equal_to_1=()" in sources[0])
+            self.assertTrue("ids_of_folded_args=()" in sources[0])
+        else:
+            self.assertTrue("equal_to_1=(3,)" in sources[0])
+            self.assertTrue("ids_of_folded_args=(3,)" in sources[0])
+
         self.assertEqual(compiled_out, eager_out)
 
 
