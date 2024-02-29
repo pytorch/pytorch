e7e415293ec8cafb5f6481a11a3a7d131c8cc3d5
Yan, Zhiwei <zhiwei.yan@intel.com>
2024-02-29 07:24:29 +0000

    Update base for Update on "Intel GPU oneDNN upstreaming: Linear pointwise fusion"
    # Motivation
    
    This PR is a part of RFC #114848,. This PR would depends on #117202 and its further predecessor PRs. i
    
    This PR is intended to add post-op fusion support fo Linear. The liner-pointwise fusion is expected to be used in graph mode like `torch.compile`. The `FusionUtils.cpp` file defines a utilization APIs for generating primitive attribute. This APIs would also be used for conv-pointwise fusion, which is in #118064.
    
    
    
    
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- test/test_xpu.py
+++ test/test_xpu.py
@@ -4,14 +4,19 @@ import sys
 import unittest
 
 import torch
+import numpy as np
+import math
+from math import inf, nan, isnan
+
 from torch.testing._internal.common_utils import NoTest, run_tests, TEST_XPU, TestCase
+from torch.testing._internal.common_device_type import instantiate_device_type_tests, dtypes, precisionOverride
+from torch.testing._internal.common_dtype import floating_and_complex_types_and
 
 if not TEST_XPU:
     print("XPU not available, skipping tests", file=sys.stderr)
     TestCase = NoTest  # noqa: F811
 
 TEST_MULTIXPU = torch.xpu.device_count() > 1
-
 
 class TestXpu(TestCase):
     def test_device_behavior(self):
@@ -108,6 +113,121 @@ if __name__ == "__main__":
         event.synchronize()
         self.assertTrue(event.query())
 
+class TestBasicGEMM(TestCase):
+    @precisionOverride({torch.double: 1e-8, torch.float: 1e-4,
+                        torch.half: 1e-1, torch.cfloat: 1e-4, torch.cdouble: 1e-8})
+    @dtypes(*floating_and_complex_types_and(torch.half))
+    def test_addmm(self, device, dtype):
+        m1 = torch.randn((10, 50), dtype=dtype, device=device)
+        m2 = torch.randn((50, 25), dtype=dtype, device=device)
+        M = torch.randn((10, 25), dtype=dtype, device=device)
+
+        res1 = torch.addmm(M, m1, m2)
+        res2 = torch.full_like(res1, math.nan)
+
+        torch.addmm(M, m1, m2, out=res2)
+
+        res3 = m1.cpu().numpy() @ m2.cpu().numpy()
+        res3 += M.cpu().numpy()
+        res3 = torch.from_numpy(res3).to(dtype)
+
+        self.assertEqual(res1.cpu(), res2.cpu())
+        self.assertEqual(res1.cpu(), res3.cpu())
+
+    @dtypes(torch.half, torch.float, torch.double, torch.cfloat, torch.cdouble)
+    def test_addmv(self, dtype):
+        m1 = torch.randn([2, 3], dtype=dtype)
+        m2 = torch.randn([3], dtype=dtype)
+        x = torch.randn([2], dtype=dtype)
+
+        res1 = torch.addmv(x, m1, m2)
+
+        res2 = torch.full_like(res1, math.nan)
+
+        torch.addmv(x, m1, m2, out=res2)
+
+        res3 = m1.cpu().numpy() @ m2.cpu().numpy()
+        res3 += x.cpu().numpy()
+        res3 = torch.from_numpy(res3).to(dtype)
+
+        self.assertEqual(res1.cpu(), res2.cpu())
+        self.assertEqual(res1.cpu(), res3.cpu())
+
+    @dtypes(torch.half, torch.float32, torch.float64, torch.int32, torch.int64, torch.cfloat, torch.cdouble)
+    def test_mm(self, dtype):
+        pass
+
+    @dtypes(*floating_and_complex_types_and(torch.half))
+    def test_bmm(self, device, dtype):
+        b1 = torch.randn((3, 3, 4), device=device, dtype=dtype)
+        b2 = torch.randn((3, 4, 5), device=device, dtype=dtype)
+
+        res1 = torch.bmm(b1, b2)
+        # res2 = torch.full((3, 3, 5), math.nan, dtype=dtype, devcie=device)
+        res2 = torch.full((3, 3, 5), math.nan, dtype=dtype, device=device)
+        torch.bmm(b1, b2, out=res2)
+        expect = torch.from_numpy(b1.to(dtype).cpu().numpy() @ b2.to(dtype).cpu().numpy()).to(device)
+        self.assertEqual(expect, res1.cpu())
+        self.assertEqual(expect, res2.cpu())
+
+
+    @dtypes(*floating_and_complex_types_and(torch.half))
+    def test_addbmm(self, device, dtype):
+        M = torch.zeros([3, 2], dtype=dtype, device=device)
+        b1 = torch.randn([10, 3, 4], dtype=dtype, device=device)
+        b2 = torch.randn([10, 4, 2], dtype=dtype, device=device)
+
+        res1 = torch.addbmm(M, b1, b2)
+        res2 = torch.full([3, 2], math.nan, dtype=dtype, device=device)
+        torch.addbmm(M, b1, b2, out=res2)
+        expect = torch.from_numpy(b1.to(dtype).cpu().numpy() @ b2.to(dtype).cpu().numpy()).to(device).sum(0)
+
+        self.assertEqual(res1, res2)
+        self.assertEqual(res1, expect)
+
+    @dtypes(*floating_and_complex_types_and(torch.half))
+    def test_baddbmm(self, device, dtype):
+        M, N, O = 12, 8, 50
+        num_batches = 10
+        b1 = torch.randn((num_batches, M, N), dtype=dtype, device=device)
+        b2 = torch.randn((num_batches, N, O), dtype=dtype, device=device)
+        x = torch.zeros((num_batches, M, O), dtype=dtype, device=device)
+
+        res1 = torch.baddbmm(x, b1, b2)
+        res2 = torch.full((num_batches, M, O), math.nan, dtype=dtype, device=device)
+        torch.baddbmm(x, b1, b2, out=res2)
+        expect = torch.from_numpy(b1.to(dtype).cpu().numpy() @ b2.to(dtype).cpu().numpy()).to(device)
+
+        self.assertEqual(res1, res2)
+        self.assertEqual(res1, expect)
+
+    def test_tensordot(self, device):
+        a = torch.randn((3, 4, 5), device=device)
+        b = torch.randn((4, 3, 2), device=device)
+        c = torch.tensordot(a, b, dims=([1, 0], [0, 1])).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy(), axes=([1, 0], [0, 1])))
+        self.assertEqual(c, cn)
+
+        cout = torch.zeros((5, 2), device=device)
+        torch.tensordot(a, b, dims=([1, 0], [0, 1]), out=cout).cpu()
+        self.assertEqual(c, cout)
+
+        a = torch.randn(2, 3, 4, 5, device=device)
+        b = torch.randn(4, 5, 6, 7, device=device)
+        c = torch.tensordot(a, b, dims=2).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy(), axes=2))
+
+        self.assertEqual(c, cn)
+
+        c = torch.tensordot(a, b).cpu()
+        cn = torch.from_numpy(np.tensordot(a.cpu().numpy(), b.cpu().numpy()))
+        self.assertEqual(c, cn)
+
+        a = torch.tensordot(torch.tensor(0.), torch.tensor(0.), 0)
+        an = torch.from_numpy(np.tensordot(np.zeros((), dtype=np.float32), np.zeros((), dtype=np.float32), 0))
+        self.assertEqual(a, an)
+
+instantiate_device_type_tests(TestBasicGEMM, globals(), only_for="cpu, xpu")
 
 if __name__ == "__main__":
     run_tests()
