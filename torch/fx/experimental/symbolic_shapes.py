754593ca5de45d48e7dba179b40ed862728c1e8a
Edward Z. Yang <ezyang@meta.com>
2024-03-01 05:33:09 -0800

    Update
    [ghstack-poisoned]

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -282,7 +282,7 @@ def _iterate_exprs(val: Union[SymInt, torch.Tensor]) -> Iterable[sympy.Basic]:
     elif isinstance(val, (tuple, list)):
         for s in val:
             yield from _iterate_exprs(s)
-    elif isinstance(val, None):
+    elif val is None:
         pass
     else:
         raise AssertionError(f"cannot extract sympy expressions from {val} {type(val)}")
