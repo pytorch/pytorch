63740b1a6dde12299088a6b3c56b7ddede507134
Valentine233 <xuan.liao@intel.com>
2024-03-01 01:40:49 -0800

    add woq mm pattern matcher
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/joint_graph.py
+++ torch/_inductor/fx_passes/joint_graph.py
@@ -29,9 +29,11 @@ def lazy_init():
     from .fuse_attention import _sfdp_init
     from .misc_patterns import _misc_patterns_init
     from .pad_mm import _pad_mm_init
+    from .woq_mm import _woq_mm_init
 
     _pad_mm_init()
     _sfdp_init()
+    _woq_mm_init()
     _misc_patterns_init()
 
 
