74bc4fd4e080a6d4f806df83959ccac2467cfbe9
Elias Ellison <elias.ellison@gmail.com>
2024-03-01 20:07:03 +0000

    Update base for Update on "Add Cudagraphs disable checking"
    Adds the same cudagraphs disable checking from inductor - cudagraph trees to cudagraphs backend.
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler amjames desertfire
    
    [ghstack-poisoned]

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -41,8 +41,8 @@ from torch._functorch.aot_autograd import aot_export_module, make_boxed_func
 from torch._inductor.codecache import code_hash, CompiledFxGraph, FxGraphCache
 
 from torch._inductor.debug import save_args_for_compile_fx_inner
-from torch._logging import trace_structured
 from torch._inductor.utils import BoxedBool, count_tangents
+from torch._logging import trace_structured
 from torch._ops import OpOverload
 from torch._subclasses.fake_tensor import FakeTensor
 from torch._utils_internal import signpost_event
