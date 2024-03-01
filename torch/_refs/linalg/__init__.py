5e5630eb1f64ed424e183aa993a1e23861656a44
Xu Zhao <i@xuzhao.net>
2024-03-01 08:15:28 -0500

    Use IntLike to check the types, make the linter happy

--- torch/_refs/linalg/__init__.py
+++ torch/_refs/linalg/__init__.py
@@ -13,6 +13,7 @@ from torch import Tensor
 from torch._prims_common import (
     check_fp_or_complex,
     check_is_matrix,
+    IntLike,
     Dim,
     DimsType,
     ELEMENTWISE_TYPE_PROMOTION_KIND,
@@ -148,7 +149,7 @@ def vector_norm(
         x = _maybe_convert_to_dtype(x, computation_dtype)  # type: ignore[assignment]
         reduce_sum = partial(torch.sum, dim=dim, keepdim=keepdim)
 
-        is_ord_even = ord % 2 == 0 if isinstance(ord, int) else ord % 2.0 == 0.0
+        is_ord_even = ord % 2 == 0 if isinstance(ord, IntLike) else ord % 2.0 == 0.0
         if not (is_ord_even and utils.is_float_dtype(x.dtype)):
             x = torch.abs(x)
         return to_result_dtype(torch.pow(reduce_sum(torch.pow(x, ord)), 1.0 / ord))  # type: ignore[return-value]
