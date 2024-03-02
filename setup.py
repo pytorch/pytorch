a3ce6243ca1a60f93df99a103199328904033506
Kurman Karabukaev <kurman@meta.com>
2024-03-02 08:07:52 +0000

    [Torchelastic][Logging] Pluggable logsspecs using python entrypoints and option to specify one by name. (#120942)
    Summary:
    Expose an option to users to specify name of the LogsSpec implementation to use.
    - Has to be defined in entrypoints under `torchrun.logs_specs` group.
    - Must implement LogsSpec defined in prior PR/diff.
    
    Test Plan: unit test+local tests
    
    Reviewed By: ezyang
    
    Differential Revision: D54180838
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120942
    Approved by: https://github.com/ezyang

--- setup.py
+++ setup.py
@@ -1055,7 +1055,10 @@ def configure_extension_build():
             "convert-caffe2-to-onnx = caffe2.python.onnx.bin.conversion:caffe2_to_onnx",
             "convert-onnx-to-caffe2 = caffe2.python.onnx.bin.conversion:onnx_to_caffe2",
             "torchrun = torch.distributed.run:main",
-        ]
+        ],
+        "torchrun.logs_specs": [
+            "default = torch.distributed.elastic.multiprocessing:DefaultLogsSpecs",
+        ],
     }
 
     return extensions, cmdclass, packages, entry_points, extra_install_requires
