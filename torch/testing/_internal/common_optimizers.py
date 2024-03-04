89d5db30bb6d6e397df22ad79b60e1ad41164232
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2024-03-04 08:44:03 -0800

    Update on "Add exhaustive module and optimizer tests for torch.load(state_dict, weights_only=True)"
    [ghstack-poisoned]

--- torch/testing/_internal/common_optimizers.py
+++ torch/testing/_internal/common_optimizers.py
@@ -1366,6 +1366,13 @@ optim_db: List[OptimizerInfo] = [
             ),
             DecorateInfo(
                 skipIfTorchDynamo(
+                    "Should be fixed by https://github.com/pytorch/pytorch/issues/117836"
+                ),
+                "TestOptimRenewed",
+                "test_save_load_state_dict",
+            ),
+            DecorateInfo(
+                skipIfTorchDynamo(
                     "Should be fixed by https://github.com/pytorch/pytorch/issues/118230"
                 ),
                 "TestOptimRenewed",
@@ -1791,6 +1798,13 @@ optim_db: List[OptimizerInfo] = [
                 ),
                 "TestOptimRenewed",
                 "test_step_all_hooks",
+            ),
+            DecorateInfo(
+                skipIfTorchDynamo(
+                    "Should be fixed by https://github.com/pytorch/pytorch/issues/118230"
+                ),
+                "TestOptimRenewed",
+                "test_save_load_state_dict",
             ),
         ),
     ),
