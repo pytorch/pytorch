9b72424fc468396f01e724da34d2d96c0d29a949
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-03-06 10:19:34 -0800

    Build sccache from source for aarch64

--- .ci/docker/common/install_cache.sh
+++ .ci/docker/common/install_cache.sh
@@ -32,7 +32,9 @@ sed -e 's|PATH="\(.*\)"|PATH="/opt/cache/bin:\1"|g' -i /etc/environment
 export PATH="/opt/cache/bin:$PATH"
 
 # Setup compiler cache
-if [ -n "$ROCM_VERSION" ]; then
+if [ $(uname -m) == aarch64]; then
+  install_ubuntu
+elif [ -n "$ROCM_VERSION" ]; then
   curl --retry 3 http://repo.radeon.com/misc/.sccache_amd/sccache -o /opt/cache/bin/sccache
 else
   ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
