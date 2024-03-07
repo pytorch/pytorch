ee448801efcd92eb2509a585e77437173517130c
Nikita Shulga <nshulga@meta.com>
2024-03-07 10:42:53 -0800

    Fix lint

--- .github/scripts/get_aws_session_tokens.py
+++ .github/scripts/get_aws_session_tokens.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import boto3
+import boto3  # type: ignore[import]
 
-def main():
-    creds_dict=boto3.Session().get_credentials().get_frozen_credentials()._asdict()
+
+def main() -> None:
+    creds_dict = boto3.Session().get_credentials().get_frozen_credentials()._asdict()
     print(f"export AWS_ACCESS_KEY_ID={creds_dict['access_key']}")
     print(f"export AWS_SECRET_ACCESS_KEY={creds_dict['secret_key']}")
     print(f"export AWS_SESSION_TOKEN={creds_dict['token']}")
