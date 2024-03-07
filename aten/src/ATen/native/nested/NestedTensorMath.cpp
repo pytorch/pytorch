da8b243a0971f7de4ccb2732449693be1e530c7d
cyy <cyyever@outlook.com>
2024-03-07 09:52:21 +0000

    Change ATEN generator argument type to const std::optional<Generator>& (#120076)
    This PR proposes to use std::optional<Generator>& for underlying functions to avoid unnecessary copy and move operations. The torchgen code was changed to generate the new type.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120076
    Approved by: https://github.com/malfet

--- aten/src/ATen/native/nested/NestedTensorMath.cpp
+++ aten/src/ATen/native/nested/NestedTensorMath.cpp
@@ -931,7 +931,7 @@ Tensor reshape_as_nested(const Tensor& self, const Tensor& other) {
   return self.reshape(sizes);
 }
 
-Tensor& normal_nested_(Tensor& self, double mean, double std, c10::optional<Generator> gen) {
+Tensor& normal_nested_(Tensor& self, double mean, double std, const std::optional<Generator>& gen) {
   const auto& self_buf = get_nested_tensor_impl(self)->get_buffer();
   self_buf.normal_(mean, std, gen);
   return self;
