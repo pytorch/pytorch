90fe67165d151664b879909edb72a47c914a7138
soulitzer <soulitzer@gmail.com>
2024-03-06 20:34:29 -0500

    Prevent __torch_function__ running a second time in AOTAutograd
    [ghstack-poisoned]

--- torch/_functorch/aot_autograd.py
+++ torch/_functorch/aot_autograd.py
@@ -443,9 +443,12 @@ def create_aot_dispatcher_function(
         enable_python_dispatcher() if shape_env is not None else nullcontext()
     )
 
-    with torch.autograd.set_multithreading_enabled(
-        False
-    ), preserve_rng_state(), fake_mode, python_dispatcher_mode, PhiloxStateTracker():
+    with torch.autograd.set_multithreading_enabled(False), \
+            preserve_rng_state(), \
+            fake_mode, \
+            python_dispatcher_mode, \
+            PhiloxStateTracker(), \
+            torch._C.DisableTorchFunctionNonInfraSubclass():
 
         def process_inputs(flat_args):
             def convert(idx, x):
