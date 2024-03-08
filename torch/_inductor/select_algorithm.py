199ecd038f02b2c649750840cfd58a142d319713
Kai Londenberg <klondenberg@meta.com>
2024-03-08 16:55:24 +0100

    Update base for Update on "[Inductor Cutlass backend] DO NOT REVIEW - to be splitted up"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 muchulee8 aakhundov ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -797,6 +797,10 @@ class ErrorFromChoice(RuntimeError):
         self.choice = choice
 
 
+class NoValidChoicesError(RuntimeError):
+    pass
+
+
 class AlgorithmSelectorCache(PersistentCache):
     def __call__(
         self,
