6e0ed565ca091324001149d07403bc3639c3509c
Sam Larsen <slarsen@meta.com>
2024-03-08 12:49:25 -0800

    Update on "Refactor some testing helpers for FX graph cache testing"
    Summary: I plan to enable the FX graph cache for more inductor unit tests. This PR does some refactoring to prepare by moving the `TestCase` base class to `torch._inductor.test_case` (which mirrors the existing `torch._dynamo.test_case`). In a subsequent diff, I'll modify tests importing `torch._dynamo.test_case.TestCase` to use `torch._inductor.test_case.TestCase` instead.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- test/inductor/test_codecache.py
+++ test/inductor/test_codecache.py
@@ -1,9 +1,12 @@
 # Owner(s): ["module: inductor"]
 import functools
 import pickle
+import tempfile
 import unittest
+from unittest.mock import patch
 
 import torch
+from torch._inductor.test_case import run_tests, TestCase
 from torch._dynamo.utils import counters
 from torch._inductor import config, metrics
 from torch._inductor.codecache import (
@@ -13,7 +16,6 @@ from torch._inductor.codecache import (
     TensorMetadata,
     TensorMetadataAndValues,
 )
-from torch._inductor.test_case import run_tests, TestCase
 from torch.testing._internal.common_cuda import SM80OrLater
 from torch.testing._internal.common_device_type import largeTensorTest
 from torch.testing._internal.common_utils import (
