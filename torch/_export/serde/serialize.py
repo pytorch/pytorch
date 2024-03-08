87a7fe1a50601318d426876d3cc6de6d6e4cfa7d
Pian Pawakapan <pianpwk@meta.com>
2024-03-07 16:18:26 -0800

    check on None

--- torch/_export/serde/serialize.py
+++ torch/_export/serde/serialize.py
@@ -1502,11 +1502,12 @@ class GraphModuleDeserializer:
             # deserialization does analysis with checks on 0/1, so we create fake range constraints and
             # restore the original range constraints afterwards
             self.symbol_name_to_range = {}
-            for k, vr in symbol_name_to_range.items():
-                lower = int(vr.lower)
-                if vr.upper >= 2:  # no specialization on 0/1
-                    lower = max(2, lower)
-                self.symbol_name_to_range[k] = symbolic_shapes.ValueRanges(_int_to_sympy_int(lower), vr.upper)
+            if symbol_name_to_range:
+                for k, vr in symbol_name_to_range.items():
+                    lower = int(vr.lower)
+                    if vr.upper >= 2:  # no specialization on 0/1
+                        lower = max(2, lower)
+                    self.symbol_name_to_range[k] = symbolic_shapes.ValueRanges(_int_to_sympy_int(lower), vr.upper)
 
             self.deserialize_graph(serialized_graph_module.graph)
 
