b95dda127738e35c31dbdb5b0123e85c05ddb8ce
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-03-09 07:20:53 +0000

    Revert "Add CUTLASS kernel as choice for _int_mm() Inductor autotuning (#119685)"
    This reverts commit 752d164b2f0d401042de4a75f36f7e84bae91daa.
    
    Reverted https://github.com/pytorch/pytorch/pull/119685 on behalf of https://github.com/huydhn due to Sorry for reverting your change, but it is crashing on ROCm https://hud.pytorch.org/pytorch/pytorch/commit/752d164b2f0d401042de4a75f36f7e84bae91daa ([comment](https://github.com/pytorch/pytorch/pull/119685#issuecomment-1986773384))

--- torch/_inductor/codegen/cuda/gemm_template.py
+++ torch/_inductor/codegen/cuda/gemm_template.py
@@ -294,8 +294,7 @@ class CUTLASSGemmTemplate(CUTLASSTemplate):
     @staticmethod
     def set_alignment(torch_layout, op_element) -> bool:
         alignment = cutlass_utils.get_max_alignment(torch_layout)
-        cuda_arch = cutlass_utils.get_cuda_arch()
-        if cuda_arch and int(cuda_arch) >= 90 and alignment < op_element.alignment:
+        if alignment < op_element.alignment:
             return False
         else:
             op_element.alignment = alignment
