511289fc022e857a04b5a7b9165b6cfb3547d3ac
bhack <bhack@users.noreply.github.com>
2024-03-12 00:07:13 +0100

    Try with specific upsample

--- aten/src/ATen/autocast_mode.cpp
+++ aten/src/ATen/autocast_mode.cpp
@@ -330,7 +330,7 @@ TORCH_LIBRARY_IMPL(aten, Autocast, m) {
   KERNEL_CUDA(cdist, fp32)
   KERNEL_CUDA(renorm, fp32)
   KERNEL_CUDA(logsumexp, fp32)
-  KERNEL_CUDA(upsample, fp32)
+  KERNEL_CUDA(upsample_nearest2d, fp32)
   // fp32_set_opt_dtype
   KERNEL_CUDA(prod, fp32_set_opt_dtype)
   KERNEL_CUDA2(prod, dim_int, fp32_set_opt_dtype)
