e23986d2e4ff1eb1f3cfa97efd2fd315cd716f31
GdoongMathew <gdoongmathew@gmail.com>
2024-03-12 02:01:44 +0800

    fix: fix linter error, change import order.

--- torch/optim/lr_scheduler.pyi
+++ torch/optim/lr_scheduler.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, Iterable, List, Optional, Union, Sequence
+from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Union
 
 from .optimizer import Optimizer
 
