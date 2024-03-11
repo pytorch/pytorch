4bfb911da9402fd629cbd10d08399eb83ab4a7b1
DanilBaibak <baibak@meta.com>
2024-03-11 10:16:26 +0100

    Run the linux-jammy-py3.8-gcc11 job on the ARC runner
    Fixed group name
    
    Fixed setup linux step
    
    Fixed protobuf installation
    
    Configurated aws credetials
    
    Added S3 bucket
    
    Fixed bucket name
    
    Triggered the workflow
    
    Set DOCKER_HOST
    
    Set DOCKER_HOST
    
    Set DOCKER_HOST
    
    Checked docker
    
    Debug container issue
    
    Debug container issue
    
    Fixed worklfow
    
    Debug the test step
    
    Debug the test step
    
    Debug the test step
    
    Debug the test step
    
    Fixed libstdc++ issue
    
    Fix permissions
    
    Fixed doc test
    
    Chnaged the owner of the workspace
    
    Fixed docs-test
    
    Debug zip command
    
    Switched runners
    
    Fixed docs_test
    
    Debug the access issue
    
    Fixed worklfow format
    
    Changed the owner for the pytorch folder
    
    Changed the owner for the pytorch folder
    
    Test all jobs

--- .ci/pytorch/docs-test.sh
+++ .ci/pytorch/docs-test.sh
@@ -5,5 +5,14 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 echo "Testing pytorch docs"
 
+sudo chown -R jenkins ../workspace
+
 cd docs
+whoami
+pwd
+ls -l
+ls -l source/
+ls -l source/scripts
+#sudo mkdir source/scripts/activation_images
+
 make doctest
