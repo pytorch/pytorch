e56e650aaeaea9090c7ee83bfcaa15b3485c4ab8
Michael Lazos <mlazos@fb.com>
2024-03-12 00:32:24 +0000

    Allow custom attributes for torch function sublcasses

--- test/dynamo/test_subclasses.py
+++ test/dynamo/test_subclasses.py
@@ -32,6 +32,7 @@ from torch.testing._internal.common_utils import (
 from torch.testing._internal.inductor_utils import HAS_CUDA
 from torch.testing._internal.two_tensor import TwoTensor
 
+test_subclass = lambda x: torch._dynamo.config.patch("traceable_tensor_subclasses", {x})
 
 requires_cuda = unittest.skipUnless(HAS_CUDA, "requires cuda")
 
@@ -51,6 +52,18 @@ class MockSubclass(torch.Tensor):
     def __torch_function__(cls, func, types, args=(), kwargs=None):
         if kwargs is None:
             kwargs = {}
+        return func(*args, **kwargs)
+
+
+class AttrSubclass(torch.Tensor):
+    x: int = 10
+    size: int = 10
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+
         return func(*args, **kwargs)
 
 
@@ -517,6 +530,29 @@ class SubclassTests(torch._dynamo.test_case.TestCase):
         res_exp = fn(wrapped)
         res_act = fn_opt(wrapped)
         self.assertEqual(res_exp, res_act)
+
+    def test_tensor_subclass_custom_attr(self):
+        class AttrSubclass(torch.Tensor):
+            x: int = 10
+
+            @classmethod
+            def __torch_function__(cls, func, types, args=(), kwargs=None):
+                if kwargs is None:
+                    kwargs = {}
+
+                return super().__torch_function__(func, types, args, kwargs)
+
+        @torch.compile(backend="eager", fullgraph=True)
+        def fn(x):
+            return x.x + torch.ones(2, 2)
+
+        with test_subclass(AttrSubclass):
+            input = torch.ones(2, 2).as_subclass(AttrSubclass)
+            fn_opt = compile_full_eager(fn)
+
+            res_exp = fn(input)
+            res_act = fn_opt(input)
+            self.assertEqual(res_exp, res_act)
 
     def test_compile_with_fake_tensor_dynamic_dim(self):
         x = torch.randn([3, 4])
