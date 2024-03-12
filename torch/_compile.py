bd3e58314ad63365977227fa0f4106d0a01dbc18
BartonAshley <vitalrema2001@gmail.com>
2024-03-12 02:12:04 +0000

    Made some grammatical corrections to _compile.py

--- torch/_compile.py
+++ torch/_compile.py
@@ -7,12 +7,12 @@ import functools
 
 def _disable_dynamo(fn=None, recursive=True):
     """
-    This API should be only used inside torch, external users should still use
+    This API should only be used inside torch, whereas external users should still use
     torch._dynamo.disable. The main goal of this API is to avoid circular
-    imports issues that is common while using _dynamo.disable inside torch
+    imports issues, that are common while using _dynamo.disable, inside torch
     itself.
 
-    This API avoids it by lazily importing torch._dynamo from the import time to
+    This API avoids the issues by lazily importing torch._dynamo from the import time to
     the invocation of the decorated function.
     """
     if fn is not None:
