01aa038dd528ce577cff2880d5ea0d97348e0ded
Oguz Ulgen <oulgen@meta.com>
2024-03-11 17:49:35 -0700

    Update on "[RFC] Support `triton.language.dtype` with `torch.compile`"
    Putting this PR as an RFC since I have resorted to some horrible hacks in order to make this work.
    ```
    (Pdb) p triton.language.float32
    triton.language.fp32
    (Pdb) p str(triton.language.float32)
    'fp32'
    (Pdb) p repr(triton.language.float32)
    'triton.language.fp32'
    ```
    This means that we need to "rewrite" them for fx graph and inductor execution.
    
    This PR helps towards Mamba2 execution with `torch.compile`.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -100,9 +100,6 @@ from torch.nn.modules.lazy import LazyModuleMixin
 from torch.utils._pytree import tree_map_only
 from torch.utils._triton import has_triton
 
-if has_triton():
-    import triton
-
 counters: DefaultDict[str, Counter[str]] = collections.defaultdict(collections.Counter)
 optimus_scuba_log: Dict[str, Any] = {}
 troubleshooting_url = "https://pytorch.org/docs/master/compile/troubleshooting.html"
@@ -981,6 +978,8 @@ common_constant_types = {
     torch.layout,
 }
 if has_triton():
+    import triton
+
     common_constant_types.add(triton.language.dtype)
 
 
