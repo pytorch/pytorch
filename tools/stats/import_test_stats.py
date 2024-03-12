9e2a72f1a17e1f6d130a9f74c75604aa4e67a074
atalman <atalman@fb.com>
2024-03-12 12:36:14 -0700

    Revert "Release fix pinning slow-tests.json"
    This reverts commit cc21233e27c449d53cd49473ae110c600ec15383.

--- tools/stats/import_test_stats.py
+++ tools/stats/import_test_stats.py
@@ -76,7 +76,7 @@ def fetch_and_cache(
 def get_slow_tests(
     dirpath: str, filename: str = SLOW_TESTS_FILE
 ) -> Optional[Dict[str, float]]:
-    url = "https://ossci-metrics.s3.amazonaws.com/slow-tests.json?versionId=test"
+    url = "https://ossci-metrics.s3.amazonaws.com/slow-tests.json"
     try:
         return fetch_and_cache(dirpath, filename, url, lambda x: x)
     except Exception:
