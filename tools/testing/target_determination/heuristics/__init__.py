5aea7bcac324cb54230fcc32e3bd2dcae78bcb79
Catherine Lee <csl@fb.com>
2024-03-13 11:15:37 -0700

    udpate

--- tools/testing/target_determination/heuristics/__init__.py
+++ tools/testing/target_determination/heuristics/__init__.py
@@ -17,6 +17,7 @@ from tools.testing.target_determination.heuristics.interface import (
     TestPrioritizations as TestPrioritizations,
 )
 from tools.testing.target_determination.heuristics.mentioned_in_pr import MentionedInPR
+from tools.testing.target_determination.heuristics.llm import LLM
 
 from tools.testing.target_determination.heuristics.previously_failed_in_pr import (
     PreviouslyFailedInPR,
@@ -33,4 +34,5 @@ HEURISTICS: List[HeuristicInterface] = [
     CorrelatedWithHistoricalFailures(),
     HistorialEditedFiles(),
     Profiling(),
+    LLM(),
 ]
