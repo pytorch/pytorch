b99556acc89c2db0f4db466285d2f1b8c4bad8a9
Kai Londenberg <klondenberg@meta.com>
2024-03-13 14:33:02 +0100

    [Inductor] Fix for WrapperCodeGen.statically_known_int_or_none
    There's obviously a small typo in WrapperCodeGen.statically_known_int_or_none,
    where the return value of a call to V.graph._shape_env._maybe_evaluate_static
    is being discarded.
    
    This fix changes that to work how it was likely intended to.
    
    Test Plan:
    CI
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -1527,7 +1527,7 @@ class WrapperCodeGen(CodeGen):
     def statically_known_int_or_none(x):
         try:
             val = V.graph._shape_env._maybe_evaluate_static(x)
-            return int(x)
+            return int(val)
         except Exception:
             return None
 
