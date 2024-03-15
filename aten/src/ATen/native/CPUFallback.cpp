54a487fd5f7e097cb9a7db5d0bcf26cceedc73ec
chentianyi16 <545429469@qq.com>
2024-03-15 11:50:42 +0800

    fix the problem that cpu_fallback for aten::triu_indices on custom device crashed

--- aten/src/ATen/native/CPUFallback.cpp
+++ aten/src/ATen/native/CPUFallback.cpp
@@ -188,7 +188,7 @@ void cpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack, bool 
   auto returns = torch::jit::last(stack, num_returns);
   const auto returns_begin = stack->size() - num_returns;
 
-  if (computed_tgt_device == c10::nullopt) {
+  if (tgt_device == c10::nullopt) {
     tgt_device = compute_target_device(tensor_args, tensorlist_args);
   }
 
