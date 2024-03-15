29ddb59136d6b05c200e99cfe4b2aa1336889214
Peter Y Yeh <peter.yeh@gmail.com>
2024-03-15 04:18:09 +0000

    driver api change

--- c10/cuda/driver_api.cpp
+++ c10/cuda/driver_api.cpp
@@ -1,5 +1,11 @@
-#if !defined(USE_ROCM) && defined(PYTORCH_C10_DRIVER_API_SUPPORTED)
-#include <c10/cuda/driver_api.h>
+#if defined(PYTORCH_C10_DRIVER_API_SUPPORTED)
+
+#ifdef USE_ROCM
+  #include <c10/hip/driver_api.h>
+#else
+  #include <c10/cuda/driver_api.h>
+#endif
+
 #include <c10/util/CallOnce.h>
 #include <c10/util/Exception.h>
 #include <dlfcn.h>
@@ -9,9 +15,28 @@ namespace c10::cuda {
 namespace {
 
 DriverAPI create_driver_api() {
-  void* handle_0 = dlopen("libcuda.so.1", RTLD_LAZY | RTLD_NOLOAD);
-  TORCH_CHECK(handle_0, "Can't open libcuda.so.1: ", dlerror());
-  void* handle_1 = DriverAPI::get_nvml_handle();
+  #ifdef USE_ROCM
+    void* handle_0 = dlopen("libamdhip64.so", RTLD_LAZY | RTLD_NOLOAD);
+    TORCH_CHECK(handle_0, "Can't open libamdhip64.so: ", dlerror());
+  #else
+    void* handle_0 = dlopen("libcuda.so.1", RTLD_LAZY | RTLD_NOLOAD);
+    TORCH_CHECK(handle_0, "Can't open libcuda.so.1: ", dlerror());
+    void* handle_1 = DriverAPI::get_nvml_handle();
+
+    #define LOOKUP_LIBCUDA_ENTRY(name)                       \
+      r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); \
+      TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
+      C10_LIBCUDA_DRIVER_API(LOOKUP_LIBCUDA_ENTRY)
+    #undef LOOKUP_LIBCUDA_ENTRY
+
+      if (handle_1) {
+    #define LOOKUP_NVML_ENTRY(name)                          \
+      r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
+      TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
+        C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
+    #undef LOOKUP_NVML_ENTRY
+  #endif
+
   DriverAPI r{};
 
 #define LOOKUP_LIBCUDA_ENTRY(name)                       \
@@ -37,10 +62,12 @@ DriverAPI create_driver_api() {
 }
 } // namespace
 
+#ifndef USE_ROCM
 void* DriverAPI::get_nvml_handle() {
   static void* nvml_hanle = dlopen("libnvidia-ml.so.1", RTLD_LAZY);
   return nvml_hanle;
 }
+#endif
 
 C10_EXPORT DriverAPI* DriverAPI::get() {
   static DriverAPI singleton = create_driver_api();
