c9677a143206f3906c329ea0e032869813b6853e
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2024-03-15 09:21:07 -0700

    add linker script optimization

---
+++ tools/setup_helpers/generate_linker_script.py
@@ -0,0 +1,25 @@
+import os
+
+def gen_linker_script(filein="cmake/prioritized_text.txt", fout="cmake/linker_script.ld"):
+    with open (filein,"r") as f:
+        prioritized_text = f.readlines()
+        prioritized_text = [line.replace("\n", "") for line in prioritized_text if line != "\n"]
+    f.close()
+    
+    linker_script_lines = os.popen("ld -verbose").read().split("\n")
+    indices = [i for i, x in enumerate(linker_script_lines) if x == "=================================================="]
+    linker_script_lines = linker_script_lines[indices[0]+1:indices[1]]
+
+    text_line_start = [i for i, line in enumerate(linker_script_lines) if ".text           :" in line]
+    assert len(text_line_start) == 1, "The linker script has multiple text sections!"
+    text_line_start = text_line_start[0]
+
+    with open (fout, "w") as f:
+        for lineid, line in enumerate(linker_script_lines):
+            if lineid == text_line_start + 2:
+                f.write("    *(\n")
+                for plines in prioritized_text:
+                    f.write(f"      .text.{plines}\n")
+                f.write("    )\n")
+            f.write(f"{line}\n")
+    f.close()
