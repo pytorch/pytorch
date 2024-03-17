6ce042e3e2cc69d20783e8fffadebe4295fb79ed
Michal Gallus <Michal.Gallus@amd.com>
2024-03-17 23:15:29 +0100

    Remove reference to a complex sparse hip blas function.
    Also remove sparse reference that is currently unavailable.
    
    (cherry picked from commit d745bfba8f9d91f9dd4fb92aa40e6c3671a3567c)

--- aten/src/ATen/cuda/CUDABlas.cpp
+++ aten/src/ATen/cuda/CUDABlas.cpp
@@ -2124,7 +2124,7 @@ void vdot<c10::complex<double>>(CUDABLAS_DOT_ARGTYPES(c10::complex<double>)) {
                                    incx, reinterpret_cast<const cuDoubleComplex*>(y), incy,
                                    reinterpret_cast<cuDoubleComplex*>(result)));
 }
-
+#if !defined(USE_ROCM) && !defined(_MSC_VER)
 template <>
 void getrsBatched<float>(CUDABLAS_GETRS_ARGTYPES(float)) {
   TORCH_CUDABLAS_CHECK(cublasSgetrsBatched(
@@ -2323,5 +2323,6 @@ void gelsBatched<c10::complex<float>>(CUDABLAS_GELS_BATCHED_ARGTYPES(c10::comple
       devInfoArray,
       batchSize));
 }
+#endif
 
 } // namespace at::cuda::blas
