633e7451220ac49fd8d9b0cb4900865126e51186
Raghav Saboo <saboo.raghav@gmail.com>
2024-03-18 17:00:48 -0400

    simple refactor to setup quantization parameters for rnn modules

--- torch/ao/nn/quantized/reference/modules/rnn.py
+++ torch/ao/nn/quantized/reference/modules/rnn.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch import Tensor
 from .utils import _quantize_and_dequantize_weight
 from .utils import _quantize_weight
+from .utils import _setup_quantization_parameters
 from typing import Optional, Dict, Any, Tuple
 from torch import _VF
 from torch.nn.utils.rnn import PackedSequence
@@ -63,35 +64,7 @@ class RNNCellBase(nn.RNNCellBase):
         for key, weight_qparams in weight_qparams_dict.items():
             if key == "is_decomposed":
                 continue
-            # TODO: refactor the duplicated code to utils.py
-            weight_qscheme = weight_qparams["qscheme"]
-            weight_dtype = weight_qparams["dtype"]
-            setattr(self, key + "_qscheme", weight_qscheme)
-            setattr(self, key + "_dtype", weight_dtype)
-            assert weight_qscheme in [None, torch.per_tensor_affine, torch.per_channel_affine], \
-                Exception(f"qscheme: {weight_qscheme} is not support in {self._get_name()}")
-            if weight_qscheme is not None:
-                scale = weight_qparams["scale"]
-                scale_tensor = scale.clone().detach() \
-                    if isinstance(scale, torch.Tensor) else \
-                    torch.tensor(scale, dtype=torch.float, device=device)
-                self.register_buffer(key + "_scale", scale_tensor)
-                zp = weight_qparams["zero_point"]
-                zp_tensor = zp.clone().detach() \
-                    if isinstance(zp, torch.Tensor) else \
-                    torch.tensor(zp, dtype=torch.int, device=device)
-                self.register_buffer(key + "_zero_point", zp_tensor)
-                if weight_qscheme == torch.per_channel_affine:
-                    axis = weight_qparams["axis"]
-                    axis_tensor = axis.clone().detach() \
-                        if isinstance(axis, torch.Tensor) else \
-                        torch.tensor(axis, dtype=torch.int, device=device)
-                    self.register_buffer(key + "_axis", axis_tensor)
-                else:
-                    # added for TorchScriptability, not used
-                    self.register_buffer(
-                        key + "_axis", torch.tensor(0, dtype=torch.int, device=device))
-                setattr(self, key + "_axis_int", getattr(self, key + "_axis").item())
+            _setup_quantization_parameters(self, key, weight_qparams, device)
 
     def _get_name(self):
         return "QuantizedRNNCellBase(Reference)"
@@ -308,28 +281,7 @@ class RNNBase(nn.RNNBase):
         for key, weight_qparams in weight_qparams_dict.items():
             if key == "is_decomposed":
                 continue
-            weight_qscheme = weight_qparams["qscheme"]
-            weight_dtype = weight_qparams["dtype"]
-            setattr(self, key + "_qscheme", weight_qscheme)
-            setattr(self, key + "_dtype", weight_dtype)
-            assert weight_qscheme in [None, torch.per_tensor_affine, torch.per_channel_affine], \
-                Exception(f"qscheme: {weight_qscheme} is not support in {self._get_name()}")
-            if weight_qscheme is not None:
-                self.register_buffer(
-                    key + "_scale",
-                    torch.tensor(weight_qparams["scale"], dtype=torch.float, device=device))
-                self.register_buffer(
-                    key + "_zero_point",
-                    torch.tensor(weight_qparams["zero_point"], dtype=torch.int, device=device))
-                if weight_qscheme == torch.per_channel_affine:
-                    self.register_buffer(
-                        key + "_axis",
-                        torch.tensor(weight_qparams["axis"], dtype=torch.int, device=device))
-                else:
-                    # added for TorchScriptability, not used
-                    self.register_buffer(
-                        key + "_axis", torch.tensor(0, dtype=torch.int, device=device))
-                setattr(self, key + "_axis_int", getattr(self, key + "_axis").item())
+            _setup_quantization_parameters(self, key, weight_qparams, device)
 
 class LSTM(RNNBase):
     """ Reference Quantized LSTM Module
