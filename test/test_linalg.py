f50b7d2bd280e97144d6cce52fced3a2c0b54f25
Peter Y Yeh <peter.yeh@gmail.com>
2024-03-18 22:48:43 +0000

    seperate rocm test case

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -5816,7 +5816,8 @@ scipy_lobpcg  | {eq_err_scipy:10.2e}  | {eq_err_general_scipy:10.2e}  | {iters2:
         SM75 = torch.cuda.is_available() and torch.cuda.get_device_capability() == (7, 5)
         if TEST_WITH_ROCM:
             _test(17, k, n, use_transpose_a, use_transpose_b, False)
-        elif version >= (11, 7):
+        
+        if version >= (11, 7):
             if not use_transpose_a and use_transpose_b:
                 if SM80OrLater or (version >= (12, 3) and (SM70 or SM75)):
                     _test(17, k, n, use_transpose_a, use_transpose_b, version > (11, 7))
