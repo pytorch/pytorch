a86899e1bd341912e478a7a8ac76499ca3cdb657
DanilBaibak <baibak@meta.com>
2024-03-18 08:44:28 +0100

    Cleanup

--- .ci/pytorch/docs-test.sh
+++ .ci/pytorch/docs-test.sh
@@ -7,12 +7,4 @@ echo "Testing pytorch docs"
 
 sudo chown -R jenkins ../workspace
 
-cd docs
-whoami
-pwd
-ls -l
-ls -l source/
-ls -l source/scripts
-#sudo mkdir source/scripts/activation_images
-
 make doctest
