b3791a74113a3d05aab3af134e49b3739e24088f
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-03-18 07:36:10 -0700

    Update torch/_inductor/codecache.py

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -917,8 +917,7 @@ def install_gcc_via_conda() -> str:
 
 
 def is_gcc() -> bool:
-    # Mac OS not support real gcc, it is acturally a clang.
-    if sys.platform == "darwin":
+    if sys.platform == "darwin" and is_apple_clang():
         return False
     return bool(re.search(r"(gcc|g\+\+)", cpp_compiler()))
 
