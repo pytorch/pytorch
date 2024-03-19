12e3b32f06ee01f908f8f71c07bbd360a25efe0d
Yifu Wang <yifu@fb.com>
2024-03-18 18:03:09 -0700

    ProcessGroupCudaP2P
    [ghstack-poisoned]

---
+++ torch/csrc/distributed/c10d/ProcessGroupCudaP2P.cpp
@@ -0,0 +1,169 @@
+#include <torch/csrc/distributed/c10d/ProcessGroupCudaP2P.hpp>
+
+#include <torch/csrc/distributed/c10d/PrefixStore.hpp>
+
+namespace c10d {
+
+using namespace c10d::intra_node_comm;
+
+ProcessGroupCudaP2P::ProcessGroupCudaP2P(
+    const c10::intrusive_ptr<Store>& store,
+    int rank,
+    int size,
+    c10::intrusive_ptr<Options> options)
+    : Backend(rank, size) {
+  ncclBackend_ = c10::make_intrusive<ProcessGroupNCCL>(
+      c10::make_intrusive<PrefixStore>("nccl", store),
+      rank,
+      size,
+      options->ncclOptions);
+  ncclBackend_->setSequenceNumberForGroup();
+
+  p2pBackend_ = c10::make_intrusive<IntraNodeComm>(
+      c10::make_intrusive<PrefixStore>("p2p", store), rank, size);
+  if (!p2pBackend_->rendezvous()) {
+    p2pBackend_ = nullptr;
+  }
+}
+
+bool ProcessGroupCudaP2P::isP2PAvailable() {
+  return p2pBackend_ != nullptr;
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::broadcast(
+    std::vector<at::Tensor>& tensors,
+    const BroadcastOptions& opts) {
+  return ncclBackend_->broadcast(tensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allreduce(
+    std::vector<at::Tensor>& tensors,
+    const AllreduceOptions& opts) {
+  return ncclBackend_->allreduce(tensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allreduce_sparse(
+    std::vector<at::Tensor>& tensors,
+    const AllreduceOptions& opts) {
+  return ncclBackend_->allreduce_sparse(tensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allreduce_coalesced(
+    std::vector<at::Tensor>& tensors,
+    const AllreduceCoalescedOptions& opts) {
+  return ncclBackend_->allreduce_coalesced(tensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::reduce(
+    std::vector<at::Tensor>& tensors,
+    const ReduceOptions& opts) {
+  return ncclBackend_->reduce(tensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allgather(
+    std::vector<std::vector<at::Tensor>>& outputTensors,
+    std::vector<at::Tensor>& inputTensors,
+    const AllgatherOptions& opts) {
+  return ncclBackend_->allgather(outputTensors, inputTensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::_allgather_base(
+    at::Tensor& outputBuffer,
+    at::Tensor& inputBuffer,
+    const AllgatherOptions& opts) {
+  return ncclBackend_->_allgather_base(outputBuffer, inputBuffer, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allgather_coalesced(
+    std::vector<std::vector<at::Tensor>>& outputTensorLists,
+    std::vector<at::Tensor>& inputTensors,
+    const AllgatherOptions& opts) {
+  return ncclBackend_->allgather_coalesced(
+      outputTensorLists, inputTensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::allgather_into_tensor_coalesced(
+    std::vector<at::Tensor>& outputs,
+    std::vector<at::Tensor>& inputs,
+    const AllgatherOptions& opts) {
+  return ncclBackend_->allgather_into_tensor_coalesced(outputs, inputs, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::gather(
+    std::vector<std::vector<at::Tensor>>& outputTensors,
+    std::vector<at::Tensor>& inputTensors,
+    const GatherOptions& opts) {
+  return ncclBackend_->gather(outputTensors, inputTensors);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::scatter(
+    std::vector<at::Tensor>& outputTensors,
+    std::vector<std::vector<at::Tensor>>& inputTensors,
+    const ScatterOptions& opts) {
+  return ncclBackend_->scatter(outputTensors, inputTensors);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::reduce_scatter(
+    std::vector<at::Tensor>& outputTensors,
+    std::vector<std::vector<at::Tensor>>& inputTensors,
+    const ReduceScatterOptions& opts) {
+  return ncclBackend_->reduce_scatter(outputTensors, inputTensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::_reduce_scatter_base(
+    at::Tensor& outputBuffer,
+    at::Tensor& inputBuffer,
+    const ReduceScatterOptions& opts) {
+  return ncclBackend_->_reduce_scatter_base(outputBuffer, inputBuffer, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::reduce_scatter_tensor_coalesced(
+    std::vector<at::Tensor>& outputs,
+    std::vector<at::Tensor>& inputs,
+    const ReduceScatterOptions& opts) {
+  return ncclBackend_->reduce_scatter_tensor_coalesced(outputs, inputs, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::alltoall_base(
+    at::Tensor& outputBuffer,
+    at::Tensor& inputBuffer,
+    std::vector<int64_t>& outputSplitSizes,
+    std::vector<int64_t>& inputSplitSizes,
+    const AllToAllOptions& opts) {
+  return ncclBackend_->alltoall_base(
+      outputBuffer, inputBuffer, outputSplitSizes, inputSplitSizes);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::alltoall(
+    std::vector<at::Tensor>& outputTensors,
+    std::vector<at::Tensor>& inputTensors,
+    const AllToAllOptions& opts) {
+  return ncclBackend_->alltoall(outputTensors, inputTensors, opts);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::send(
+    std::vector<at::Tensor>& tensors,
+    int dstRank,
+    int tag) {
+  return ncclBackend_->send(tensors, dstRank, tag);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::recv(
+    std::vector<at::Tensor>& tensors,
+    int srcRank,
+    int tag) {
+  return ncclBackend_->recv(tensors, srcRank, tag);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::recvAnysource(
+    std::vector<at::Tensor>& tensors,
+    int tag) {
+  return ncclBackend_->recvAnysource(tensors, tag);
+}
+
+c10::intrusive_ptr<Work> ProcessGroupCudaP2P::barrier(
+    const BarrierOptions& opts) {
+  return ncclBackend_->barrier(opts);
+}
+
+} // namespace c10d
