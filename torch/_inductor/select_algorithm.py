d5006a4d4768c4c4c2a08bcd4fc6d5dd50b1745e
eellison <elias.ellison@gmail.com>
2024-03-18 18:57:57 -0700

    Precompile triton templates (#121998)
    Before this PR we were not precompiling triton templates in parallel. Compilation would occur during benchmarking.
    
    Triton benchmarking templates were emitted as :
    
    ```
    @triton.jit
    def triton_mm(arg_A, arg_B, out_ptr0):
    ```
    
    In order to precompile we need to give the full kernel specification, as we do when we emit the template in the final output code generation.
    
    ```
    @triton_heuristics.template(
        num_stages=3,
        num_warps=8,
        triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: '*fp32'}, 'device': 0, 'device_type': 'cuda', 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1, 2), equal_to_1=(), ids_of_folded_args=(), divisible_by_8=())]},
        inductor_meta={'kernel_name': 'Placeholder.DESCRIPTIVE_NAME', 'backend_hash': 'cdeecfeccd31ad7810f96b5752194b1c2406d0a81e39a6ca09c8ee150baae183'},
    )
    @triton.jit
    def triton_mm(arg_A, arg_B, out_ptr0):
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121998
    Approved by: https://github.com/jansel
    ghstack dependencies: #121996, #120275, #121997

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -94,7 +94,7 @@ class TritonTemplateKernel(TritonKernel):
         grid_fn,
         meta,
         call_sizes,
-        use_jit=True,
+        use_jit=False,
         prefix_args=0,
         suffix_args=0,
         epilogue_fn=identity,
@@ -150,8 +150,8 @@ class TritonTemplateKernel(TritonKernel):
         argdefs, _, signature = self.args.python_argdefs()
         triton_meta = {
             "signature": signature_to_meta(signature, size_dtype=self.index_dtype),
-            "device": V.graph.scheduler.current_device.index,
-            "device_type": V.graph.scheduler.current_device.type,
+            "device": self.output_node.get_device().index,
+            "device_type": self.output_node.get_device().type,
             "constants": {},
         }
         triton_meta["configs"] = [config_of(signature)]
@@ -498,7 +498,7 @@ class TritonTemplate(KernelTemplate):
         ), TritonTemplateKernel(
             kernel_name=kernel_name,
             output_node=fake_out,
-            use_jit=True,
+            use_jit=False,
             **kernel_options,
         ) as kernel:
             try:
@@ -684,6 +684,10 @@ class TritonTemplateCaller(ir.TritonTemplateCallerBase):
         assert self.bmreq is not None
         return self.bmreq.benchmark(*args, output_tensor=out)
 
+    def precompile(self):
+        assert self.bmreq is not None
+        self.bmreq.precompile()
+
     def __str__(self):
         return f"TritonTemplateCaller({self.bmreq.module_path}, {self.debug_extra})"
 
@@ -821,6 +825,7 @@ class AlgorithmSelectorCache(PersistentCache):
 
         # TODO(nmacchioni): remove once CI tests are fixed
         choices = [choice for choice in choices if choice is not None]
+
         if len(choices) == 0:
             raise RuntimeError(
                 "No choices to select, please consider adding ATEN into max_autotune_gemm_backends "
