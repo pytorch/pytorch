8157ced14c085aa479ceca9608471cc605a67508
Edward Z. Yang <ezyang@meta.com>
2024-03-19 02:48:51 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -353,11 +353,6 @@ _save_config_ignore = {
     "skipfiles_inline_module_allowlist",
 }
 
-# for backend="cudagraphs", mutations on input be sent to the cudagraph backend
-# or replayed in aot_autograd epilogue. default is False because mutation on inputs
-# can prevent cudagraphing.
-cudagraph_backend_keep_input_mutation = False
-
 # When True, only ops that have the torch.Tag.pt2_compliant tag
 # will be allowed into the graph; all other ops will be disallowed
 # and will fall back to eager-mode PyTorch. Useful to ensure
--- torch/fx/proxy.py
+++ torch/fx/proxy.py
@@ -7,13 +7,13 @@ import sys
 import torch
 import inspect
 import operator
-import traceback
 import collections
 
 from dataclasses import is_dataclass, fields
 
 
 from .graph import magic_methods, reflectable_magic_methods, Graph
+from torch.utils._traceback import CapturedTraceback
 from typing import Tuple, Dict, OrderedDict, Optional, Any, Iterator, Callable
 from .node import Target, Node, Argument, base_types, map_aggregate
 from ._compatibility import compatibility
@@ -197,12 +197,8 @@ class TracerBase:
             proxy = proxy_factory_fn(node)
 
         if self.record_stack_traces and not proxy.node.stack_trace:
-            user_frame = self._find_user_frame()
-            if user_frame:
-                summary = traceback.extract_stack(user_frame)
-                tb_lines = summary.format()
-                # stack_trace would have innermost frame at the bottom
-                proxy.node.stack_trace = ''.join(tb_lines)
+            proxy.node.stack_trace = ''.join(CapturedTraceback.extract().format())
+
 
         return proxy
 
@@ -245,9 +241,9 @@ class TracerBase:
 
         Can be override to support more trace-specific types.
         """
-        from torch.utils._triton import has_triton
+        from torch.utils._triton import has_triton_package
 
-        if has_triton():
+        if has_triton_package():
             import triton
 
         if not isinstance(a, Proxy) and hasattr(a, '__fx_create_arg__'):
@@ -285,7 +281,7 @@ class TracerBase:
 
         elif isinstance(a, torch._ops.OpOverload):
             return a
-        elif has_triton() and isinstance(a, triton.language.dtype):
+        elif has_triton_package() and isinstance(a, triton.language.dtype):
             return a
 
         if isinstance(a, Proxy):
