75bc023ed9e43ee702648b85746b01f77e9e10aa
Jiong Gong <jiong.gong@intel.com>
2024-03-20 23:08:29 +0800

    Update base for Update on "[inductor][cpp] move vectorized type conversion to aten/cpu/vec"
    cc mingfeima XiaobingSuper sanchitintel ashokei jingxu10 voznesenskym penguinwu EikanWang Guobing-Chen zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler
    
    [ghstack-poisoned]

--- test/inductor/test_cpu_repro.py
+++ test/inductor/test_cpu_repro.py
@@ -3417,8 +3417,7 @@ class CPUReproTests(TestCase):
         x = torch.randint(0, 100, (819,), dtype=torch.int64)
         metrics.reset()
         self.common(fn, (x,))
-        # TODO: support vectorized int64 masked load
-        assert metrics.generated_cpp_vec_kernel_count == 0
+        assert metrics.generated_cpp_vec_kernel_count == 1
 
 
 if __name__ == "__main__":
