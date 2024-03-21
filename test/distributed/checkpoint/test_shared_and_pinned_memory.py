8388fd602801bddb7f1c1746c5f580556346968a
Lucas Pasqualin <lpasqualin@meta.com>
2024-03-21 10:20:47 -0700

    updated for correctness

--- test/distributed/checkpoint/test_shared_and_pinned_memory.py
+++ test/distributed/checkpoint/test_shared_and_pinned_memory.py
@@ -12,39 +12,48 @@ def test_shared_pinned_ipc(
     use_shared=True,
     use_pinned=True,
 ):
+
+    print(f"{os.getpid()=}")
+
     state_dict = {"a": rand_tensor()}
+    rand_event = torch.cuda.Event()
+    rand_event.record()
 
     cache = _create_cpu_state_dict(
         state_dict,
         share_memory=use_shared,
         pin_memory=use_pinned
     )
+    torch.cuda.synchronize()
+
     spawn_context = mp.get_context("spawn")
     send_queue = spawn_context.Queue()
     recv_queue = spawn_context.Queue()
-    print(os.getpid())
     mp.spawn(fn=sub_process, args=(send_queue, recv_queue), nprocs=1, join=False)
+
     t0 = time.monotonic()
-
+    stream = torch.cuda.Stream()
+    offload_event = torch.cuda.Event(interprocess=True, blocking=True)
     for idx in range(100):
-        print(idx)
 
-        event = torch.cuda.Event(blocking=True, interprocess=True)
-        with torch.cuda.stream(torch.cuda.Stream()):
+        with torch.cuda.stream(stream):
+            rand_event.wait()
             _offload_state_dict_to_cpu(
                 state_dict,
                 cpu_offload_state_dict=cache,
                 cpu_offload_sync=False
             )
-            event.record()
+            offload_event.record()
 
-        send_queue.put((cache, event)) # we will synchronize before here, bc of child process
+        send_queue.put((cache, offload_event))
 
+        # we will synchronize before here, bc of child process
         t_recv = recv_queue.get()
         t_recv["a"] -= 1
         torch.testing.assert_close(state_dict["a"].cpu(), t_recv["a"])
 
         state_dict = {"a": rand_tensor()}
+        rand_event.record()
 
     send_queue.put((None, None))
     print(f"{time.monotonic() - t0}")
@@ -53,7 +62,7 @@ def sub_process(_, recv_queue, send_queue):
     try:
         t, event = recv_queue.get()
         while t is not None:
-            event.synchronize()
+            event.wait()
             t["a"] += 1
             send_queue.put(t)
             t, event = recv_queue.get()
