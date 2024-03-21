2a72879eb069c39862fdc51c5afd9436012bd4a4
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-03-21 23:05:59 +0000

    Revert "Precompile triton templates (#121998)"
    This reverts commit b8df2f0ca530ebe01fa079c891c170a1f4b22823.
    
    Reverted https://github.com/pytorch/pytorch/pull/121998 on behalf of https://github.com/huydhn due to Sorry for reverting your change but it is causing all ROCm trunk job to fail https://hud.pytorch.org/pytorch/pytorch/commit/b8df2f0ca530ebe01fa079c891c170a1f4b22823 ([comment](https://github.com/pytorch/pytorch/pull/121998#issuecomment-2014003037))

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -2449,9 +2449,16 @@ class Scheduler:
 
         self.flush()
 
-    def get_buffer_layout(self, buf_name: str) -> ir.Layout:
+    def is_unaligned_buffer(self, buf_name):
+        if buf_name in V.graph.graph_inputs or buf_name in V.graph.constants:
+            # all graph inputs or constants are assumed to be aligned
+            return False
         node = self.name_to_node[buf_name]
-        return node.node.get_layout()
+        layout = node.node.get_layout()
+        if isinstance(layout, ir.AliasedLayout):
+            return not layout.maybe_guard_aligned()
+        else:
+            return False
 
 
 class BaseScheduling:
