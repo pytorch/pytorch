d49ca948a2060ea8c610b1ca6dcd9579f07e45d1
Yifu Wang <yifu@fb.com>
2024-03-23 15:15:42 -0700

    Don't create world pg variable out of thin air when rewriting c10d collectives (#122561)
    Fixes https://github.com/pytorch/pytorch/issues/122404
    
    Previously, when rewriting c10d collectives, if the group argument is
    unspecified or None, we create a world pg variable out of thin air and
    pass it to the rewrite target. The approach was problematic, as it
    assumes the symbol `torch` is available in the scope (see #122404).
    
    After #120560, dynamo can now trace dist.group.WORLD. If the group
    argument is unspecified, we can just set it with dist.group.WORLD in the
    rewrite target.
    
    Testing
    
    pytest test/distributed/test_inductor_collectives.py -k test_dynamo_rewrite_dist_allreduce
    
    Also verified with the repro provided in #122404
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122561
    Approved by: https://github.com/wconstab
    ghstack dependencies: #120560

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -18,7 +18,6 @@ from ..source import AttrSource, ConstantSource, DefaultsSource, GetItemSource
 from ..utils import check_constant_args, get_first_attr, identity, istype, make_cell
 from .base import MutableLocal, typestr, VariableTracker
 from .constant import ConstantVariable
-from .distributed import ProcessGroupVariable
 
 if TYPE_CHECKING:
     from torch._guards import Source
@@ -718,9 +717,6 @@ class CollectiveFunctionRewriteVariable(UserFunctionVariable):
             unimplemented(
                 f"CollectiveFunctionRewriteVariable can't support async_op=True for {self.fn}"
             )
-
-        if kwargs.get("group") is None or kwargs["group"].value is None:
-            kwargs["group"] = ProcessGroupVariable.get_global_pg_variable()
 
         if self.fn == dist.all_reduce:
             reduce_op_var = kwargs.get("op")
