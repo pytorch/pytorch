cc8c5d87d543e2b57e2a50f7b43cb8774fa8e3ea
Michael Lazos <mlazos@fb.com>
2024-03-24 04:42:00 +0000

    Fix int

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -278,7 +278,7 @@ class GraphLowering(torch.fx.Interpreter):
         self.creation_time = time.time()
         self.name = name
         self.cpp_wrapper = cpp_wrapper
-        self.foreach_tbs: Set[str] = set()
+        self.foreach_tbs: Set[int] = set()
 
         # record multi_kernel choice for cpp_wrapper so the second pass knows
         # which sub-kernel is picked. Copy cpp_wrapper to another variable
