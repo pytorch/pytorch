5ed958d65254c1d8192ce11126a621152ca08954
Mu-Chu Lee <mlee8@meta.com>
2024-03-24 00:33:54 -0700

    [TEST] [AOTInductor] Support use_runtime_constant_folding for CPU.
    Summary:
    We allow CPU to use the config use_runtime_constant_folding.
    Changes include
    
    1. Rearrange USE_CUDA flags. Add CPU sections that consumes memory directly.
    2. Codegen changes to accomodate cpp fusions for CPU only. Specifically, we shouldn't generate 2 headers that would cause re-declaration.
    
    Test Plan:
    Activate tests that were deactivated for CPU before.
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -712,7 +712,10 @@ class WrapperCodeGen(CodeGen):
         if config.profile_bandwidth:
             self.write_triton_header_once()
         result = IndentedBuffer()
-        result.splice(self.header)
+        # We do not want the cpp header for intermediate const graph. Headers would be
+        # rendered by the main module instead.
+        if V.graph.aot_mode and V.graph.cpp_wrapper and V.graph.is_const_graph:
+            result.splice(self.header)
 
         with contextlib.ExitStack() as stack:
             stack.enter_context(self.wrapper_call.indent())
