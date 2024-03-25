52928e8bff1ed6eb827420c10c9b427bb951aea1
eellison <elias.ellison@gmail.com>
2024-03-25 10:30:09 -0700

    Precompile triton templates (#121998)
    Before this PR we were not precompiling triton templates in parallel. Compilation would occur during benchmarking.
    
    Triton benchmarking templates were emitted as :
    
    ```
    @triton.jit
    def triton_mm(arg_A, arg_B, out_ptr0):
    ```
    
    In order to precompile we need to give the full kernel specification, as we do when we emit the template in the final output code generation.
    
    ```
    @triton_heuristics.template(
        num_stages=3,
        num_warps=8,
        triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: '*fp32'}, 'device': 0, 'device_type': 'cuda', 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1, 2), equal_to_1=(), ids_of_folded_args=(), divisible_by_8=())]},
        inductor_meta={'kernel_name': 'Placeholder.DESCRIPTIVE_NAME', 'backend_hash': 'cdeecfeccd31ad7810f96b5752194b1c2406d0a81e39a6ca09c8ee150baae183'},
    )
    @triton.jit
    def triton_mm(arg_A, arg_B, out_ptr0):
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121998
    Approved by: https://github.com/jansel

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -2451,18 +2451,9 @@ class Scheduler:
 
         self.flush()
 
-    def is_unaligned_buffer(self, buf_name):
-        if buf_name in V.graph.graph_inputs:
-            return not config.assume_aligned_inputs
-        if buf_name in V.graph.constants:
-            # all constants are assumed to be aligned
-            return False
+    def get_buffer_layout(self, buf_name: str) -> ir.Layout:
         node = self.name_to_node[buf_name]
-        layout = node.node.get_layout()
-        if isinstance(layout, ir.NonOwningLayout):
-            return not layout.maybe_guard_aligned()
-        else:
-            return False
+        return node.node.get_layout()
 
 
 class BaseScheduling:
