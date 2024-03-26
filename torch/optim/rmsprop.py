a2c516403aeac75bb47c1af06cb915248310cf0f
Michael Lazos <mlazos@meta.com>
2024-03-25 18:15:45 -0700

    remove duplicate check

--- torch/optim/rmsprop.py
+++ torch/optim/rmsprop.py
@@ -261,11 +261,6 @@ def rmsprop(
     else:
         func = _single_tensor_rmsprop
 
-    # this check is slow during compilation, so we skip it
-    # if it's strictly needed we can add this check back in dynamo
-    if not torch._utils.is_compiling() and not all(isinstance(t, torch.Tensor) for t in state_steps):
-        raise RuntimeError("API has changed, `state_steps` argument must contain a list of singleton tensors")
-
     func(
         params,
         grads,
