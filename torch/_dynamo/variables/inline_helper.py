de48bf1c25b800a0357cbf384285d91e3c3d9770
JackCaoG <jackcao@google.com>
2024-03-27 21:05:38 +0000

    eliminate_dead_code on fx, fix test_quantization.py TestXNNPACKQuantizer.test_linear_gru/test_gru

--- torch/_dynamo/variables/inline_helper.py
+++ torch/_dynamo/variables/inline_helper.py
@@ -117,9 +117,9 @@ def decompose_and_inline_function_with_makefx(tx, fn, args, kwargs, function_key
                 fake_value_args, fake_value_kwargs
             )
 
-    # # Perform a dead code elimination
-    # fx_g.graph.eliminate_dead_code()
-    # fx_g.compile()
+    # Perform a dead code elimination
+    fx_g.graph.eliminate_dead_code()
+    fx_g.recompile()
 
     # print("\nfx code")
     # this is a hack, we want to access `.code` here to trigger the `real_recompile`
