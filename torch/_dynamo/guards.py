60efc2dab39e3bf7b6b7a1dac41a69edc1dc621a
Animesh Jain <anijain@umich.edu>
2024-03-27 09:08:33 -0700

    [dynamo][improve-guard-overhead] Skip TENSOR_MATCH guards on parameters for optimizers (#122647)
    **1.32x  guard overhead reduction** (1.092 vs vs 0.827 ms) for MegatronBertForCausalLM with 394 params.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122647
    Approved by: https://github.com/jansel, https://github.com/mlazos
    ghstack dependencies: #122646

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -370,6 +370,18 @@ def getitem_on_dict_manager(
     )
 
 
+def is_grad_source(source):
+    if isinstance(source, AttrSource):
+        return source.member == "grad"
+    return False
+
+
+def match_on_id_for_tensor(guard):
+    return guard.originating_source.is_dict_key() and not is_grad_source(
+        guard.originating_source
+    )
+
+
 # The ready to eval generated code (possibly multiple parts) for a guard, plus
 # the original guard object that created it for provenance
 @dataclasses.dataclass
@@ -1294,7 +1306,7 @@ class GuardBuilder(GuardBuilderBase):
                 self._produce_guard_code(guard, [shape_guard], shape_env=True)
 
     def TENSOR_MATCH(self, guard: Guard, value=None):
-        if guard.is_nn_module() or guard.originating_source.is_dict_key():
+        if guard.is_nn_module() or match_on_id_for_tensor(guard):
             self.ID_MATCH(guard)
         else:
             if isinstance(value, TensorWeakRef):
