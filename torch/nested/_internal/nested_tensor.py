83debb657ec006dce9c083308ef5807095185b4d
Joel Schlosser <jbschlosser@meta.com>
2024-03-28 13:03:13 -0400

    Use zeros for NJT dummy to avoid messing with randomness (#122902)
    Use of randomness was breaking vmap.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122902
    Approved by: https://github.com/vmoens, https://github.com/zou3519

--- torch/nested/_internal/nested_tensor.py
+++ torch/nested/_internal/nested_tensor.py
@@ -420,8 +420,8 @@ def _nt_view_dummy() -> torch.Tensor:
     global _dummy_instance
     if _dummy_instance is None:
         _dummy_instance = NestedTensor(
-            values=torch.randn(3, 3, device="meta"),
-            offsets=torch.randint(3, (2,), device="meta", dtype=torch.int64),
+            values=torch.zeros(3, 3, device="meta"),
+            offsets=torch.zeros(3, device="meta", dtype=torch.int64),
         ).detach()
     return _dummy_instance
 
