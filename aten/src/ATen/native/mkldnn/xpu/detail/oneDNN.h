70d5499d1ebc4e0c1ee61dc834ca752e77c01eb2
ZhiweiYan-96 <zhiwei.yan@intel.com>
2024-03-28 16:27:10 +0000

    Update base for Update on "[WIP]Intel GPU oneDNN upstreaming: Conv primitive integration"
    # Motivation
    
    This PR is a part of RFC #114848,. This PR would depend on oneDNN compilation in #117098 and basic integration support in #117112.  Some runtime support is needed in #116019.
    
    This PR provides the oneDNN integration code for Convolution and Deconvolution related operators. All aten convolution operators(conv, deconv, and conv-pointwise fusion) will goes into this layer before executing oneDNN primitive.  The integration code is responsible for providing correct memory description for primitive and accompanied with primitive attribute description.
    
    Wit this PR land, we will add Conv related operators accompanied with their registration.
    
    
    
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- aten/src/ATen/native/mkldnn/xpu/detail/oneDNN.h
+++ aten/src/ATen/native/mkldnn/xpu/detail/oneDNN.h
@@ -5,4 +5,16 @@
 #include <ATen/native/mkldnn/xpu/detail/Attr.h>
 #include <ATen/native/mkldnn/xpu/detail/Utils.h>
 
-#include <ATen/native/mkldnn/xpu/detail/Matmul.h>
+
+namespace at::native::onednn{
+
+sycl::event matmul(
+    at::Tensor& result,
+    const at::Tensor& mat1,
+    const at::Tensor& mat2,
+    const at::Tensor& b_raw,
+    bool m2_trans,
+    Attr attr,
+    const std::vector<sycl::event>& deps = {});
+
+} // namespace at::native::onednn
