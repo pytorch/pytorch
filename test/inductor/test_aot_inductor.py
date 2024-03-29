ea08bc2d81642c1b71ac5ee2dca032cfbf89a9e3
Colin Peppler <colinpeppler@meta.com>
2024-03-28 17:25:55 -0700

    Update on "[AOTInductor] Support many outputs aliasing the same tensor"
    fixes https://github.com/pytorch/pytorch/issues/122826
    
    # Problem
    When the model returns multiple outputs which alias the same tensor, we get a SEGFAULT. Because we try to release the same buffer twice.
    ```
    def forward(x):
      x_out = x + 1
      contig = x.contiguous()   # alias of same tensor as x_out
      return x_out, contig
    
    run_impl() {
      output_handles[0] = buf0.release();
      output_handles[1] = buf0.release();   # SEGFAULT
    }
    
    # if we try to workaround this by assign aliases without creating a new tensor, 
    # then, we'll get a double free error during handle clean-up.
    output_handles[1] = output_handles[0];    # assign without creating a new tensor
    ...
    alloc_tensors_by_stealing_from_handles(){
      aoti_torch_delete_tensor_object(handles[0]);
      aoti_torch_delete_tensor_object(handles[1]);   # Double free
    }
    ```
    
    # Solution
    ~~Instead, we use the first `output_handle` that shares the same tensor and alias it.~~
    ```
    output_handles[0] = buf0.release();
    aoti_torch_alias_tensor(output_handles[0], &output_handles[1]);  # No SEGFAULT & No double free!
    ```
    
    A simpler approach is to figure out which handles are duplicate. Then we simply copy all duplicate except the last one. The last one will use `std::move` and free the tensor owned by the model instance.
    ```
    output_handles[0] = buf0.release();
    output_handles[1] = output_handles[0];
    ```
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov amjames desertfire chauhang
    
    Differential Revision: [D55455344](https://our.internmc.facebook.com/intern/diff/D55455344)
    
    [ghstack-poisoned]

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -2363,6 +2363,8 @@ CPU_TEST_FAILURES = {
     # FIXME: failed with compilation error
     "test_runtime_checks": fail_minimal_arrayref_interface(is_skip=True),
     "test_normal_functional": fail_with_and_without_stack_allocation(),
+    # undefined symbol: _Z16aoti_torch_dtypeIN3c104HalfEEiv
+    "test_non_contiguous_output_alias": fail_with_and_without_stack_allocation(is_skip=True),
     # the test segfaults
     "test_repeat_output": fail_stack_allocation(is_skip=True),
     "test_multiple_output_alias": fail_with_and_without_stack_allocation(is_skip=True),
