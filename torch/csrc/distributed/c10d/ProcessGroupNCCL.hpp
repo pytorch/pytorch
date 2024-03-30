3e7683ae18823e21e7c0376e2736fd457c68c2cc
Shuqiang Zhang <sqzhang@meta.com>
2024-03-29 22:20:13 -0700

    [c10d] dump on any exception (timeout + nccl error) (#123023)
    Summary:
    Existing flight recorder dumping logic is: dump only on timeout, but not
    on NCCL error. This resulted in the faulty ranks missing dumps when NCCL
    error happens.
    
    So in this PR, we revise the logic of dump such that records are dumped
    when any exception is detected. Exception could be 1. NCCL async errors.
    2. watchdog timeout
    
    Also the existing code tends to mix the logic of flight recorder dump
    and desync debug, which is no desirable. We only dump the desync debug
    report only when timeout is detected.
    Test Plan:
    Added a new unit test to trigger nccl error and dump, and make sure the
    dump is triggered by the error.
    
    Also existing dump on timeout tests should still pass.
    
    sqzhang_1) [sqzhang@devgpu009.cln1 ~/pytorch (84bf9d4c)]$ python
    test/distributed/test_c10d_nccl.py NcclErrorDumpTest
    NCCL version 2.19.3+cuda12.0
    [E329 19:15:11.775879730 ProcessGroupNCCL.cpp:565] [Rank 0] Watchdog
    caught collective operation timeout: WorkNCCL(SeqNum=2,
    OpType=ALLREDUCE, NumelIn=10, NumelOut=10, Timeout(ms)=10000) ran for
    10028 milliseconds before timing out.
    [E329 19:15:11.777459894 ProcessGroupNCCL.cpp:1561] [PG 0 Rank 0]
    Exception hit in NCCL work: 2
    [E329 19:15:12.660717323 ProcessGroupNCCL.cpp:1332] [PG 0 Rank 0]
    Received a timeout signal from this local rank and will start to dump
    the debug info. Last enqueued NCCL work: 2, last completed NCCL work: 1.
    [E329 19:15:12.660932242 ProcessGroupNCCL.cpp:1167] [PG 0 Rank 0]
    ProcessGroupNCCL preparing to dump debug info.
    [E329 19:15:12.661192990 ProcessGroupNCCL.cpp:1174] [PG 0 Rank 0]
    ProcessGroupNCCL dumping nccl trace to /tmp/tmp06psqil3/trace_0
    [F329 19:15:12.661485601 ProcessGroupNCCL.cpp:1185] [PG 0 Rank 0] [PG 0
    Rank 0] ProcessGroupNCCL's watchdog detected a collective timeout from
    the local rank. This is most likely caused by incorrect usages of
    collectives, e.g., wrong sizes used across ranks, the order of
    collectives is not same for all ranks or the scheduled collective, for
    some reason, didn't run. Additionally, this can be caused by GIL
    deadlock or other reasons such as network errors or bugs in the
    communications library (e.g. NCCL), etc. We tried our best to dump the
    debug info into the storage to help you debug the issue.
    
    Tags:
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123023
    Approved by: https://github.com/wconstab

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
@@ -102,7 +102,7 @@ static std::vector<std::string> TORCH_NCCL_COORD_CHECK_MILSEC = {
 
 constexpr const char* NCCL_BACKEND_NAME = "nccl";
 
-constexpr const char* TIMEOUT_DUMP = "timeout_dump";
+constexpr const char* EXCEPTION_DUMP = "exception_dump";
 
 constexpr auto kProcessGroupNCCLDefaultTimeout =
     std::chrono::milliseconds(10 * 60 * 1000);
@@ -1037,8 +1037,9 @@ class TORCH_API ProcessGroupNCCL : public Backend {
   // Whether or not to enable timeout root cause analysis.
   bool desyncDebug_;
 
-  // Whether or not to dump debug info on timeout
-  bool dumpOnTimeout_;
+  // Whether or not to dump debug info on exception including both watchdog
+  // timeout and nccl errors.
+  bool dumpOnException_;
 
   // Whether or not to create start CUDAEvent and enable timing for start
   // and end events. Note that enableTiming_ is always true if desyncDebug_
