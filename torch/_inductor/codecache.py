e100bb0d1dd4c55c65679dbc945e78b5a06fffed
Jason Ansel <jansel@meta.com>
2024-03-29 17:13:51 -0700

    [inductor] Don't compile MultiKernelCall in a subprocess (#123010)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123010
    Approved by: https://github.com/shunting314
    ghstack dependencies: #123009

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -2734,12 +2734,6 @@ class AsyncCompile:
             return task()
         return cls.pool().submit(task)
 
-    @classmethod
-    def map(cls, fn: Callable[..., Any], seq: List[Any]) -> List[Any]:
-        if config.compile_threads <= 1 or len(seq) <= 1:
-            return list(map(fn, seq))
-        return [t.result() for t in [cls.pool().submit(fn, x) for x in seq]]
-
     def triton(
         self, kernel_name: str, source_code: str, device_str: str = "cuda"
     ) -> Union[TritonFuture, ModuleType]:
@@ -2756,17 +2750,11 @@ class AsyncCompile:
         else:
             return _load_kernel(kernel_name, source_code)
 
-    def multi_kernel(self, *args, **kwargs) -> ModuleType:
-        """
-        Async compile the python shim for multi-kernel.
-        """
+    def multi_kernel(self, *args, **kwargs) -> Any:
+        from torch._inductor.codegen.multi_kernel import MultiKernelCall
 
-        def task():
-            from torch._inductor.codegen.multi_kernel import MultiKernelCall
-
-            return MultiKernelCall(*args, **kwargs)
-
-        return self.submit(task)
+        # no need to call this in parallel since the sub-kernels are already parallel tasks
+        return MultiKernelCall(*args, **kwargs)
 
     def cpp(self, source_code: str) -> ModuleType:
         def task():
