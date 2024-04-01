c3d676aadf4fa2173f2c7b9276eebd29a9549670
Azad Free <azad@meta.com>
2024-04-01 07:10:11 -0700

    [BASE_MODULE] removing base_module for glow/fb/test:test_utils
    Summary: This is an attempt to reduce the number of buck targets with unnecessary base_module.
    
    Test Plan: CI
    
    Differential Revision: D55589390

--- caffe2/contrib/fakelowp/test/test_fc_nnpi_fp16.py
+++ caffe2/contrib/fakelowp/test/test_fc_nnpi_fp16.py
@@ -1,14 +1,14 @@
 import numpy as np
 import unittest
 
-import caffe2.python.fakelowp.init_shared_libs  # noqa
+import glow.fb.test.init_shared_libs  # noqa
 from hypothesis import given, settings
 from hypothesis import strategies as st
 from caffe2.proto import caffe2_pb2
 from caffe2.python import core
 from caffe2.python import workspace
 from caffe2.python.onnx.onnxifi import onnxifi_caffe2_net
-from caffe2.python.fakelowp.test_utils import print_test_debug_info
+from glow.fb.test.test_utils import print_test_debug_info
 import datetime
 import caffe2.python.serialized_test.serialized_test_util as serial
 
