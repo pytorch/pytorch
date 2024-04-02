fe2a9e6ce594f13cf2e6c8225f156930d347558a
haozhe.zhu <haozhe.zhu@intel.com>
2024-04-02 21:36:11 +0800

    Update on "[WIP][optim] add fused_adam_kernel support for CPU device"
    On par with `CUDA` implementation.
    
    For `autocast` logic, same with `CUDA` + `Fused Adam`:
     - check inf in `gradscalar.step`
     - In fused kernel, if there is `inf`, do nothing. If not, unscale the grad ( also write back) and update the param.
    
    **TestPlan**:
    ```
    # extend CUDA only test for CPU fused adagrad
    python test_optim.py -k test_fused_matches_forloop
    python test_optim.py -k test_fused_large_tensor
    python test_torch.py -k test_grad_scaling_autocast_fused
    
    # extend fused test
    python test_torch.py -k test_params_invalidated_with_grads_invalidated_between_unscale_and_step
    python test_optim.py -k test_can_load_older_state_dict
    
    # newly added test (follow https://github.com/pytorch/pytorch/blob/6b1f13ea2f3b1bcd575620eecd7d84a4d2e3eb76/test/test_cuda.py#L1108)
    python test_optim.py -k test_grad_scaling_autocast_fused_optimizers
    ```
    
    **Benchmark**:
    **5.1x** on 56 core SPR
    **Parameter-size=1M**
    **Nparams=10**
    [test script](https://gist.github.com/zhuhaozhe/ef9a290ad3f8f4067b3373a3bdaa33e7)
    
    ```
    numactl -C 0-55 -m 0 python bench_adam.py
    non-fused 6.0174267292022705 s
    fused 1.1787631511688232 s
    ```
    
    **Note: Fused kernel accuracy**
    The accuracy failure in CI shows a little higher than default tolerance
    ```
    2024-04-02T06:09:16.2213887Z Mismatched elements: 21 / 64 (32.8%)
    2024-04-02T06:09:16.2214339Z Greatest absolute difference: 1.5735626220703125e-05 at index (6, 6) (up to 1e-05 allowed)
    2024-04-02T06:09:16.2214813Z Greatest relative difference: 1.0073336852656212e-05 at index (4, 1) (up to 1.3e-06 allowed)
    ```
    I have debug it step by step and unfortunately we may not able to make the `fused kernel` exactly same with `non fused` one due to compiler optimizations.
    For example, in non-fused impl
    ```
    exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
    ```
    and in fused impl
    ```
      exp_avg_sq_ptr[d] = scalar_t(beta2) * exp_avg_sq_ptr[d];
      //  std::cout << "exp_avg_sq " <<   exp_avg_sq_ptr[d] << std::endl;
      exp_avg_sq_ptr[d] = exp_avg_sq_ptr[d] +
          scalar_t(exp_avg_sq_grad_coefficient) * grad_val * grad_val;
    ```
    If I keep `std::cout`, I can get exactly same results in UT
    ```
    ===============param
    0.6796758770942688
    0.6796758770942688
    ```
    But when I comment out it, there will be a difference
    ```
    ===============param
    0.6796758770942688
    0.6796759366989136
    ```
    So I will make the tolerance a little higher than default one.
    
    
    
    
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cpu/FusedAdamKernel.cpp
+++ aten/src/ATen/native/cpu/FusedAdamKernel.cpp
@@ -1,13 +1,230 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/core/Tensor.h>
 #include <ATen/Parallel.h>
+#include <ATen/OpMathType.h>
 #include <ATen/native/DispatchStub.h>
 #include <ATen/native/FusedAdam.h>
 #include <ATen/Dispatch.h>
 #include <ATen/cpu/vec/vec.h>
+#include <ATen/cpu/vec/functional.h>
 namespace at::native {
 
 namespace{
+
+template <typename scalar_t, typename opmath_t>
+typename std::enable_if<
+    std::is_same<scalar_t, Half>::value || std::is_same<scalar_t, BFloat16>::value,
+    void>::
+    type inline adam_math(
+  scalar_t* param_ptr,
+  scalar_t* exp_avg_ptr,
+  scalar_t* exp_avg_sq_ptr,
+  scalar_t* grad_ptr,
+  scalar_t* max_exp_avg_sq_ptr,
+  opmath_t step_size,
+  opmath_t bias_correction2,
+  opmath_t exp_avg_grad_coefficient,
+  opmath_t exp_avg_sq_grad_coefficient,
+  opmath_t bias_correction2_sqrt,
+  opmath_t eps,
+  opmath_t weight_decay,
+  opmath_t beta2,
+  bool amsgrad,
+  bool maximize,
+  const float* grad_scale_ptr,
+  int64_t size
+){
+  using lpVec = at::vec::Vectorized<scalar_t>;
+  using fVec = at::vec::Vectorized<opmath_t>;
+  lpVec grad_vec_to_store;
+  int64_t d = 0;
+  fVec param_vec1, param_vec2;
+  fVec grad_vec1, grad_vec2;
+  fVec exp_avg_vec1, exp_avg_vec2;
+  fVec exp_avg_sq_vec1, exp_avg_sq_vec2;
+  fVec max_exp_avg_sq_vec1, max_exp_avg_sq_vec2;
+  for (; d < size - (size % lpVec::size()); d += lpVec::size()) {
+    lpVec param_lpvec = lpVec::loadu(param_ptr + d);
+    std::tie(param_vec1, param_vec2) = vec::convert_to_float<scalar_t>(param_lpvec);
+    lpVec grad_lpvec = lpVec::loadu(grad_ptr + d);
+    std::tie(grad_vec1, grad_vec2) = vec::convert_to_float<scalar_t>(grad_lpvec);
+    if (grad_scale_ptr) {
+      grad_vec1 = grad_vec1 / fVec(float(*grad_scale_ptr));
+      grad_vec2 = grad_vec2 / fVec(float(*grad_scale_ptr));
+      grad_vec_to_store = vec::convert_from_float<scalar_t>(grad_vec1, grad_vec2);
+      grad_vec_to_store.store(grad_ptr + d);
+    }
+    if (maximize){
+      grad_vec1 = grad_vec1 * fVec(opmath_t(-1.0));
+      grad_vec2 = grad_vec2 * fVec(opmath_t(-1.0));
+    }
+    if (weight_decay != 0.f){
+      grad_vec1 += param_vec1 * fVec(opmath_t(weight_decay));
+      grad_vec2 += param_vec2 * fVec(opmath_t(weight_decay));
+    }
+
+    lpVec exp_avg_lpvec = lpVec::loadu(exp_avg_ptr + d);
+    std::tie(exp_avg_vec1, exp_avg_vec2) = vec::convert_to_float<scalar_t>(exp_avg_lpvec);
+    exp_avg_vec1 = exp_avg_vec1 + fVec(opmath_t(exp_avg_grad_coefficient)) * (grad_vec1 - exp_avg_vec1);
+    exp_avg_vec2 = exp_avg_vec2 + fVec(opmath_t(exp_avg_grad_coefficient)) * (grad_vec1 - exp_avg_vec2);
+
+    lpVec exp_avg_sq_lpvec = lpVec::loadu(exp_avg_sq_ptr + d);
+    std::tie(exp_avg_sq_vec1, exp_avg_sq_vec2) = vec::convert_to_float<scalar_t>(exp_avg_sq_lpvec);
+    exp_avg_sq_vec1 = exp_avg_sq_vec1 * fVec(opmath_t(beta2)) +
+        fVec(opmath_t(exp_avg_sq_grad_coefficient)) * grad_vec1 * grad_vec1;
+    exp_avg_sq_vec2 = exp_avg_sq_vec2 * fVec(opmath_t(beta2)) +
+        fVec(opmath_t(exp_avg_sq_grad_coefficient)) * grad_vec2 * grad_vec2;
+
+    vec::convert_from_float<scalar_t>(exp_avg_vec1, exp_avg_vec2).store(exp_avg_ptr + d);
+    vec::convert_from_float<scalar_t>(exp_avg_sq_vec1, exp_avg_sq_vec2).store(exp_avg_sq_ptr + d);
+
+    fVec denom_vec1, denom_vec2;
+    if (amsgrad) {
+      lpVec max_exp_avg_sq_lpvec = lpVec::loadu(max_exp_avg_sq_ptr + d);
+      std::tie(max_exp_avg_sq_vec1, max_exp_avg_sq_vec2) = vec::convert_to_float<scalar_t>(max_exp_avg_sq_lpvec);
+      max_exp_avg_sq_vec1 = maximum(max_exp_avg_sq_vec1, exp_avg_sq_vec1);
+      max_exp_avg_sq_vec2 = maximum(max_exp_avg_sq_vec2, exp_avg_sq_vec2);
+      vec::convert_from_float<scalar_t>(max_exp_avg_sq_vec1, max_exp_avg_sq_vec2).store(max_exp_avg_sq_ptr + d);
+      denom_vec1 =
+          (max_exp_avg_sq_vec1.sqrt() / fVec(opmath_t(bias_correction2_sqrt))) + fVec(opmath_t(eps));
+      denom_vec2 =
+          (max_exp_avg_sq_vec2.sqrt() / fVec(opmath_t(bias_correction2_sqrt))) + fVec(opmath_t(eps));
+    } else {
+      denom_vec1 =
+          (exp_avg_sq_vec1.sqrt() / fVec(opmath_t(bias_correction2_sqrt))) + fVec(opmath_t(eps));
+      denom_vec2 =
+          (exp_avg_sq_vec2.sqrt() / fVec(opmath_t(bias_correction2_sqrt))) + fVec(opmath_t(eps));
+    }
+    param_vec1 = param_vec1 - fVec(opmath_t(step_size)) * exp_avg_vec1 / denom_vec1;
+    param_vec2 = param_vec2 - fVec(opmath_t(step_size)) * exp_avg_vec2 / denom_vec2;
+    vec::convert_from_float<scalar_t>(param_vec1, param_vec2).store(param_ptr + d);
+  }
+  scalar_t grad_val_to_store;
+  for (; d < size; d++) {
+    opmath_t grad_val = grad_ptr[d];
+    if (grad_scale_ptr) {
+      grad_val = grad_ptr[d] / float(*grad_scale_ptr);
+      grad_val_to_store = scalar_t(grad_val);
+      grad_ptr[d] = grad_val_to_store;
+    }
+    if (maximize) grad_val = -grad_val;
+    if (weight_decay != 0.f){
+      grad_val += opmath_t(param_ptr[d]) * opmath_t(weight_decay);
+    }
+    opmath_t exp_avg_var = exp_avg_ptr[d];
+    exp_avg_var = exp_avg_var + exp_avg_grad_coefficient * (grad_val - exp_avg_var);
+    exp_avg_ptr[d] = scalar_t(exp_avg_var);
+    opmath_t exp_avg_sq_var = exp_avg_sq_ptr[d];
+    exp_avg_sq_var = exp_avg_sq_var * beta2;
+    exp_avg_sq_var = exp_avg_sq_var +
+        exp_avg_sq_grad_coefficient * grad_val * grad_val;
+    exp_avg_sq_ptr[d] = scalar_t(exp_avg_sq_var);
+    opmath_t demon_val;
+    if (amsgrad) {
+      opmath_t max_exp_avg_sq_var = max_exp_avg_sq_ptr[d];
+      max_exp_avg_sq_var = std::max(max_exp_avg_sq_var, exp_avg_sq_var);
+      max_exp_avg_sq_ptr[d] =
+          scalar_t(max_exp_avg_sq_var);
+      demon_val =
+          std::sqrt(max_exp_avg_sq_var) / bias_correction2_sqrt + opmath_t(eps);
+    } else {
+      demon_val = std::sqrt(exp_avg_sq_var) / bias_correction2_sqrt + opmath_t(eps);
+    }
+    param_ptr[d] = opmath_t(param_ptr[d]) - step_size * exp_avg_var / demon_val;
+  }
+}
+
+
+template <typename scalar_t, typename opmath_t>
+typename std::enable_if<
+    std::is_same<scalar_t, float>::value || std::is_same<scalar_t, double>::value,
+    void>::
+    type inline adam_math(
+  scalar_t* param_ptr,
+  scalar_t* exp_avg_ptr,
+  scalar_t* exp_avg_sq_ptr,
+  scalar_t* grad_ptr,
+  scalar_t* max_exp_avg_sq_ptr,
+  opmath_t step_size,
+  opmath_t bias_correction2,
+  opmath_t exp_avg_grad_coefficient,
+  opmath_t exp_avg_sq_grad_coefficient,
+  opmath_t bias_correction2_sqrt,
+  opmath_t eps,
+  opmath_t weight_decay,
+  opmath_t beta2,
+  bool amsgrad,
+  bool maximize,
+  const float* grad_scale_ptr,
+  int64_t size
+){
+  using Vec = at::vec::Vectorized<scalar_t>;
+  Vec grad_vec_to_store;
+  int64_t d = 0;
+  for (; d < size - (size % Vec::size()); d += Vec::size()) {
+    Vec param_vec = Vec::loadu(param_ptr + d);
+    Vec grad_vec = Vec::loadu(grad_ptr + d);
+    if (grad_scale_ptr) {
+      grad_vec = grad_vec / Vec(scalar_t(*grad_scale_ptr));
+      grad_vec_to_store = grad_vec;
+      grad_vec_to_store.store(grad_ptr + d);
+    }
+    if (maximize) grad_vec = grad_vec * Vec(scalar_t(-1.0));
+    if (weight_decay != 0.f){
+      grad_vec += param_vec * Vec(scalar_t(weight_decay));
+    }
+    Vec exp_avg_vec = Vec::loadu(exp_avg_ptr + d);
+    exp_avg_vec = exp_avg_vec + Vec(scalar_t(exp_avg_grad_coefficient)) * (grad_vec - exp_avg_vec);
+
+    Vec exp_avg_sq_vec = Vec::loadu(exp_avg_sq_ptr + d) * Vec(scalar_t(beta2)) +
+        Vec(scalar_t(exp_avg_sq_grad_coefficient)) * grad_vec * grad_vec;
+    exp_avg_vec.store(exp_avg_ptr + d);
+    exp_avg_sq_vec.store(exp_avg_sq_ptr + d);
+
+    Vec denom_vec;
+    if (amsgrad) {
+      Vec max_exp_avg_sq_vec =
+          maximum(Vec::loadu(max_exp_avg_sq_ptr + d), exp_avg_sq_vec);
+      max_exp_avg_sq_vec.store(max_exp_avg_sq_ptr + d);
+      denom_vec =
+          (max_exp_avg_sq_vec.sqrt() / Vec(scalar_t(bias_correction2_sqrt))) + Vec(scalar_t(eps));
+    } else {
+      denom_vec =
+          (exp_avg_sq_vec.sqrt() / Vec(scalar_t(bias_correction2_sqrt))) + Vec(scalar_t(eps));
+    }
+
+    param_vec = param_vec - Vec(scalar_t(step_size)) * exp_avg_vec / denom_vec;
+    param_vec.store(param_ptr + d);
+  }
+  scalar_t grad_val_to_store;
+  for (; d < size; d++) {
+    scalar_t grad_val = grad_ptr[d];
+    if (grad_scale_ptr) {
+      grad_val = grad_ptr[d] / scalar_t(*grad_scale_ptr);
+      grad_val_to_store = grad_val;
+      grad_ptr[d] = grad_val_to_store;
+    }
+    if (maximize) grad_val = -grad_val;
+    if (weight_decay != 0.f){
+      grad_val += param_ptr[d] * scalar_t(weight_decay);
+    }
+    exp_avg_ptr[d] = exp_avg_ptr[d] + exp_avg_grad_coefficient * (grad_val - exp_avg_ptr[d]);
+    exp_avg_sq_ptr[d] = exp_avg_sq_ptr[d] * beta2;
+    exp_avg_sq_ptr[d] = exp_avg_sq_ptr[d] +
+        exp_avg_sq_grad_coefficient * grad_val * grad_val;
+    scalar_t demon_val;
+    if (amsgrad) {
+      max_exp_avg_sq_ptr[d] =
+          std::max(max_exp_avg_sq_ptr[d], exp_avg_sq_ptr[d]);
+      demon_val =
+          std::sqrt(max_exp_avg_sq_ptr[d]) / bias_correction2_sqrt + scalar_t(eps);
+    } else {
+      demon_val = std::sqrt(exp_avg_sq_ptr[d]) / bias_correction2_sqrt + scalar_t(eps);
+    }
+    param_ptr[d] = param_ptr[d] - step_size * exp_avg_ptr[d] / demon_val;
+  }
+}
+
 
 template <typename scalar_t>
 void adam_fused_step_impl(
@@ -25,6 +242,7 @@ void adam_fused_step_impl(
     const bool amsgrad,
     const bool maximize,
     const float* grad_scale_ptr) {
+  using opmath_t = at::opmath_type<scalar_t>;
   float step = state_step.item<float>();
   scalar_t* param_data = param.data_ptr<scalar_t>();
   scalar_t* exp_avg_data = exp_avg.data_ptr<scalar_t>();
@@ -34,13 +252,11 @@ void adam_fused_step_impl(
 
   // need to use double here to align with non-fused adam
   double bias_correction1 = 1 - std::pow(beta1, step);
-  scalar_t step_size =  lr / bias_correction1;
-  scalar_t bias_correction2 = 1 - std::pow(beta2, step);
-  scalar_t exp_avg_grad_coefficient = 1 - beta1;
-  scalar_t exp_avg_sq_grad_coefficient = 1 - beta2;
-  scalar_t bias_correction2_sqrt = std::sqrt(bias_correction2);
-
-  using Vec = at::vec::Vectorized<scalar_t>;
+  opmath_t step_size =  lr / bias_correction1;
+  opmath_t bias_correction2 = 1 - std::pow(beta2, step);
+  opmath_t exp_avg_grad_coefficient = 1 - beta1;
+  opmath_t exp_avg_sq_grad_coefficient = 1 - beta2;
+  opmath_t bias_correction2_sqrt = std::sqrt(bias_correction2);
 
   // update momentum vt and mt
   // also accumulate sum of param_norm and rtw_norm
@@ -54,71 +270,25 @@ void adam_fused_step_impl(
         scalar_t* max_exp_avg_sq_ptr = amsgrad ? max_exp_avg_sq_data + begin : nullptr;
 
         const int64_t size = end - begin;
-        Vec grad_vec_to_store;
-
-        int64_t d = 0;
-        for (; d < size - (size % Vec::size()); d += Vec::size()) {
-          Vec param_vec = Vec::loadu(param_ptr + d);
-          Vec grad_vec = Vec::loadu(grad_ptr + d);
-          if (grad_scale_ptr) {
-            grad_vec = grad_vec / Vec(scalar_t(*grad_scale_ptr));
-            grad_vec_to_store = grad_vec;
-            grad_vec_to_store.store(grad_ptr + d);
-          }
-          if (maximize) grad_vec = grad_vec * Vec(scalar_t(-1.0));
-          if (weight_decay != 0.f){
-            grad_vec += param_vec * Vec(scalar_t(weight_decay));
-          }
-          Vec exp_avg_vec = Vec::loadu(exp_avg_ptr + d);
-          exp_avg_vec = exp_avg_vec + Vec(scalar_t(exp_avg_grad_coefficient)) * (grad_vec - exp_avg_vec);
-
-          Vec exp_avg_sq_vec = Vec::loadu(exp_avg_sq_ptr + d) * Vec(scalar_t(beta2)) +
-              Vec(scalar_t(exp_avg_sq_grad_coefficient)) * grad_vec * grad_vec;
-          exp_avg_vec.store(exp_avg_ptr + d);
-          exp_avg_sq_vec.store(exp_avg_sq_ptr + d);
-
-          Vec denom_vec;
-          if (amsgrad) {
-            Vec max_exp_avg_sq_vec =
-                maximum(Vec::loadu(max_exp_avg_sq_ptr + d), exp_avg_sq_vec);
-            max_exp_avg_sq_vec.store(max_exp_avg_sq_ptr + d);
-            denom_vec =
-                (max_exp_avg_sq_vec.sqrt() / Vec(scalar_t(bias_correction2_sqrt))) + Vec(scalar_t(eps));
-          } else {
-            denom_vec =
-                (exp_avg_sq_vec.sqrt() / Vec(scalar_t(bias_correction2_sqrt))) + Vec(scalar_t(eps));
-          }
-
-          param_vec = param_vec - Vec(scalar_t(step_size)) * exp_avg_vec / denom_vec;
-          param_vec.store(param_ptr + d);
-        }
-        scalar_t grad_val_to_store;
-        for (; d < size; d++) {
-          scalar_t grad_val = grad_ptr[d];
-          if (grad_scale_ptr) {
-            grad_val = grad_ptr[d] / scalar_t(*grad_scale_ptr);
-            grad_val_to_store = grad_val;
-            grad_ptr[d] = grad_val_to_store;
-          }
-          if (maximize) grad_val = -grad_val;
-          if (weight_decay != 0.f){
-            grad_val += param_ptr[d] * scalar_t(weight_decay);
-          }
-          exp_avg_ptr[d] = exp_avg_ptr[d] + exp_avg_grad_coefficient * (grad_val - exp_avg_ptr[d]);
-          exp_avg_sq_ptr[d] = exp_avg_sq_ptr[d] * beta2;
-          exp_avg_sq_ptr[d] = exp_avg_sq_ptr[d] +
-              exp_avg_sq_grad_coefficient * grad_val * grad_val;
-          scalar_t demon_val;
-          if (amsgrad) {
-            max_exp_avg_sq_ptr[d] =
-                std::max(max_exp_avg_sq_ptr[d], exp_avg_sq_ptr[d]);
-            demon_val =
-                std::sqrt(max_exp_avg_sq_ptr[d]) / bias_correction2_sqrt + scalar_t(eps);
-          } else {
-            demon_val = std::sqrt(exp_avg_sq_ptr[d]) / bias_correction2_sqrt + scalar_t(eps);
-          }
-          param_ptr[d] = param_ptr[d] - step_size * exp_avg_ptr[d] / demon_val;
-        }
+        adam_math<scalar_t, opmath_t>(
+          param_ptr,
+          exp_avg_ptr,
+          exp_avg_sq_ptr,
+          grad_ptr,
+          max_exp_avg_sq_ptr,
+          step_size,
+          bias_correction2,
+          exp_avg_grad_coefficient,
+          exp_avg_sq_grad_coefficient,
+          bias_correction2_sqrt,
+          eps,
+          weight_decay,
+          beta2,
+          amsgrad,
+          maximize,
+          grad_scale_ptr,
+          size
+        );
       });
 }
 
--- test/test_torch.py
+++ test/test_torch.py
@@ -47,7 +47,7 @@ from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
     onlyCUDA, onlyCPU,
     dtypes, dtypesIfCUDA, dtypesIfCPU, deviceCountAtLeast,
-    skipMeta,
+    skipMeta, precisionOverride,
     PYTORCH_CUDA_MEMCHECK, largeTensorTest, onlyNativeDeviceTypes,
     get_all_device_types, skipXLA)
 from typing import Tuple
@@ -5998,6 +5998,49 @@ else:
         scaler.step(optimizer)
         scaler.update()
         assert scaler._scale != float("inf") and scaler._scale != float("nan")
+
+    @onlyNativeDeviceTypes
+    @dtypes(*floating_types_and(torch.bfloat16, torch.half))
+    @precisionOverride({torch.half : 1e-4, torch.bfloat16 : 1e-4})
+    def test_fused_adam(self, device, dtype):
+        r"""
+        This testcase will compare the results between _single_tensor_adam and _fused_adam.
+        """
+        from torch.optim.adam import _single_tensor_adam, _fused_adam
+
+        def _test_fused_adam_base(kwargs):
+            non_fused_kwargs = copy.deepcopy(kwargs)
+            fused_kwargs = copy.deepcopy(kwargs)
+            _single_tensor_adam(**non_fused_kwargs)
+            _fused_adam(**fused_kwargs)
+            self.assertEqual(non_fused_kwargs, fused_kwargs)
+
+        # generate input args
+        TENSOR_SIZE = (33, )  # make sure to cover cpu vec and scalar
+        NPARAM = 3
+        kwargs = {}
+        kwargs['params'] = [torch.randn(TENSOR_SIZE, device=device, dtype=dtype) for _ in range(NPARAM)]
+        kwargs['grads'] = [torch.randn(TENSOR_SIZE, device=device, dtype=dtype) for _ in range(NPARAM)]
+        kwargs['exp_avgs'] = [torch.randn(TENSOR_SIZE, device=device, dtype=dtype) for _ in range(NPARAM)]
+        kwargs['exp_avg_sqs'] = [torch.randn(TENSOR_SIZE, device=device, dtype=dtype) for _ in range(NPARAM)]
+        kwargs['state_steps'] = [torch.tensor([10.0]) for _ in range(NPARAM)]
+        kwargs['grad_scale'] = None
+        kwargs['found_inf'] = None
+        kwargs['beta1'] = 0.9
+        kwargs['beta2'] = 0.999
+        kwargs['lr'] = 0.1
+        kwargs['eps'] = 1e-8
+        kwargs['has_complex'] = False
+        kwargs['capturable'] = False
+        kwargs['differentiable'] = False
+        for amsgrad, maximize, weight_decay in product([True, False], [True, False], [0.0, 0.1]):
+            kwargs['amsgrad'] = amsgrad
+            kwargs['maximize'] = maximize
+            kwargs['weight_decay'] = weight_decay
+            kwargs['max_exp_avg_sqs'] = []
+            if amsgrad:
+                kwargs['max_exp_avg_sqs'] = [torch.randn(TENSOR_SIZE, device=device, dtype=dtype) for _ in range(NPARAM)]
+            _test_fused_adam_base(kwargs)
 
     @onlyNativeDeviceTypes
     def test_grad_scaling_clipping(self, device):
