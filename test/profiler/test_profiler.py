fca33523531794c1d6a8b80d608e7e7f9cfff942
Shivam Raikundalia <sraikund@meta.com>
2024-04-03 21:58:09 +0000

    make RecordFunctionFast take inputs (#123208)
    Summary: RECORD_FUNCTION in C++ and torch.profiler.record_function already support recording inputs. Let's do the same for RecordFunctionFast.
    
    Test Plan: Add tests in test_profiler.py that take args and also do not take args so we can support it being an optional parameter
    
    Differential Revision: D55648870
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123208
    Approved by: https://github.com/davidberard98

--- test/profiler/test_profiler.py
+++ test/profiler/test_profiler.py
@@ -1826,24 +1826,31 @@ assert KinetoStepTracker.current_step() == initial_step + 2 * niters
 
     def test_record_function_fast(self):
         x, y = (torch.rand((4, 4)) for _ in range(2))
-        with profile() as p:
+        with profile(record_shapes=True) as p:
             for _ in range(4):
+                # Test first with no optional args
                 with torch._C._profiler._RecordFunctionFast("add_test_fast_rf1"):
                     x.add(y)
 
         self.assertGreaterEqual(len([e for e in p.events() if e.name == "add_test_fast_rf1"]), 4)
-
-        with profile() as p:
-            cm = torch._C._profiler._RecordFunctionFast("add_test_fast_rf2")
+        for e in p.events():
+            if e.name == "add_test_fast_rf1":
+                self.assertTrue(e.input_shapes == [])
+        with profile(record_shapes=True) as p:
+            # add optional args
+            cm = torch._C._profiler._RecordFunctionFast("add_test_fast_rf2", [x, y])
             for _ in range(4):
                 with cm:
                     x.add(y)
 
         self.assertGreaterEqual(len([e for e in p.events() if e.name == "add_test_fast_rf2"]), 4)
 
+        for e in p.events():
+            if e.name == "add_test_fast_rf2":
+                self.assertTrue(e.input_shapes == [[4, 4], [4, 4]])
 
-        with profile() as p:
-            cm = torch._C._profiler._RecordFunctionFast("add_test_fast_rf3")
+        with profile(record_shapes=True) as p:
+            cm = torch._C._profiler._RecordFunctionFast("add_test_fast_rf3", ["hi"])
             for _ in range(4):
                 try:
                     with cm:
@@ -1856,14 +1863,25 @@ assert KinetoStepTracker.current_step() == initial_step + 2 * niters
         self.assertGreaterEqual(len([e for e in p.events() if e.name == "add_test_fast_rf3"]), 4)
         self.assertFalse(any((e.name and "relu" in e.name) for e in p.events()))
 
+        for e in p.events():
+            if e.name == "add_test_fast_rf3":
+                self.assertTrue(e.input_shapes == [[]])
+
+
         with profile() as p:
             for _ in range(4):
-                with torch._C._profiler._RecordFunctionFast("add_test_fast_rf4"):
+                with torch._C._profiler._RecordFunctionFast("add_test_fast_rf4", [x, y]):
                     x.add(y)
                     with torch._C._profiler._RecordFunctionFast("add_test_fast_rf5"):
                         x.relu()
 
         self.assertGreaterEqual(len([e for e in p.events() if e.name == "add_test_fast_rf4"]), 4)
+
+        for e in p.events():
+            if e.name == "add_test_fast_rf4":
+                self.assertTrue(e.input_shapes == [])
+
+
         self.assertGreaterEqual(len([e for e in p.events() if e.name == "add_test_fast_rf5"]), 4)
 
     def test_is_profiler_enabled(self):
