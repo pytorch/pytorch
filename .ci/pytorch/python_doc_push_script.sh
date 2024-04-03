2aa48bfdc31626d145f819a955d3feb2226c2c1b
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-04-03 10:48:02 -0700

    Update .ci/pytorch/python_doc_push_script.sh

--- .ci/pytorch/python_doc_push_script.sh
+++ .ci/pytorch/python_doc_push_script.sh
@@ -27,7 +27,7 @@ fi
 
 # Argument 1: Where to copy the built documentation to
 # (docs/$install_path)
-install_path="${1:-${DOCS_INSTALL_PATH:-docs/${DOCS_VERSION}}}"
+install_path="${1:-${DOCS_INSTALL_PATH:-${DOCS_VERSION}}}"
 if [ -z "$install_path" ]; then
 echo "error: python_doc_push_script.sh: install_path (arg1) not specified"
   exit 1
