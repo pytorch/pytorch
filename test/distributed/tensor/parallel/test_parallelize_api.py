7af4f8fe8f29d5855d0f1005c5cbf9bf9160821f
Ke Wen <kw2501@meta.com>
2024-04-03 03:08:04 +0000

    [TP] Improve MLPStacked test (#123199)
    Improve tests per @wanchaol 's suggestions in #122968
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123199
    Approved by: https://github.com/wanchaol

--- test/distributed/tensor/parallel/test_parallelize_api.py
+++ test/distributed/tensor/parallel/test_parallelize_api.py
@@ -279,7 +279,7 @@ class TensorParallelAPITests(DTensorTestBase):
     @with_comms
     def test_parallelize_module_multi_wildcard(self):
         inp_size = [12, 10]
-        model = MLPStacked(self.device_type)
+        model = MLPStacked(self.device_type, n_layers=2)
         device_mesh = DeviceMesh(self.device_type, torch.arange(self.world_size))
 
         model_tp = deepcopy(model)
@@ -287,7 +287,8 @@ class TensorParallelAPITests(DTensorTestBase):
             model_tp,
             device_mesh,
             {
-                "layers.*.net?": ColwiseParallel(output_layouts=Replicate()),
+                "layers.*.net[1]": ColwiseParallel(),
+                "layers.*.net[2]": RowwiseParallel(),
             },
         )
         self._compare_module(model, model_tp, inp_size, rank0_only=False)
