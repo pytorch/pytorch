cb72c02e31595cd2a35bc8cfc5d20b7f644efb6c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-04-04 02:57:09 +0000

    Revert "Revert usage of NJT views in SDPA (#123215)"
    This reverts commit 0fcddb56252c9b4401e8b888eddd4bc4bce3e624.
    
    Reverted https://github.com/pytorch/pytorch/pull/123215 on behalf of https://github.com/huydhn due to Sorry for reverting your PR but I think it needs to be skipped on ROCm https://hud.pytorch.org/pytorch/pytorch/commit/0fcddb56252c9b4401e8b888eddd4bc4bce3e624 ([comment](https://github.com/pytorch/pytorch/pull/123215#issuecomment-2036080570))

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -46,11 +46,9 @@ from torch.testing._internal.common_utils import (
 )
 
 from torch.nested._internal.nested_tensor import (
-    buffer_from_jagged,
     jagged_from_list,
     NestedTensor,
     nested_view_from_values_offsets,
-    ViewNestedFromBuffer,
 )
 
 # Tests are ported from pytorch/nestedtensor.
@@ -183,33 +181,6 @@ def random_nt_from_similar(other, dims=None):
 def layout_name(layout):
     # e.g. "torch.jagged" -> "jagged"
     return layout.__repr__().split(".")[-1]
-
-
-# Internally-defined conversion functions are lifted to here for maximum test realism.
-# TODO: Remove these when ViewNestedFromBuffer, etc. are deprecated.
-@torch.fx.wrap
-def convert_dense_to_nested_tensor(values):
-    offsets = torch.arange(
-        0, values.shape[0] * values.shape[1] + 1, values.shape[1], device=values.device
-    )
-    metadata_cache = {"max_seqlen": values.shape[1], "min_seqlen": 1}
-    nt = ViewNestedFromBuffer.apply(
-        values.view(-1, values.shape[-1]), offsets, metadata_cache
-    )
-    return nt
-
-@torch.fx.wrap
-def convert_jagged_to_nested_tensor(
-    values: torch.Tensor, offsets: torch.Tensor, max_length: int
-) -> torch.Tensor:
-    metadata_cache = {"max_seqlen": max_length, "min_seqlen": 1}
-    nt = ViewNestedFromBuffer.apply(values, offsets, metadata_cache)
-    return nt
-
-
-@torch.fx.wrap
-def convert_nt_to_jagged(nt):
-    return buffer_from_jagged(nt)
 
 
 @markDynamoStrictTest
@@ -3940,8 +3911,6 @@ class TestNestedTensorSubclass(TestCase):
         nt1_t, nt2_t, nt3_t, nt4_t = (x.transpose(1, 2) for x in (nt1, nt2, nt3, nt4))
         check_size(nt1_t, nt2_t, nt3_t, nt4_t)
 
-    # Doesn't work until we have real views
-    @xfailIfTorchDynamo
     # Note 1: Math fallback doesn't work with bfloat16 on CUDA
     # Note 2: ROCm doesn't support flash attention or mem_efficient attention for NT
     @unittest.skipIf(
@@ -4135,8 +4104,6 @@ class TestNestedTensorSubclass(TestCase):
         output_dense = F.scaled_dot_product_attention(query._values, key._values, value._values)
         self.assertEqual(output._values, output_dense)
 
-    # Doesn't work until we have real views
-    @xfailIfTorchDynamo
     @onlyCUDA
     @unittest.skipIf(
         not PLATFORM_SUPPORTS_FUSED_ATTENTION,
@@ -4181,30 +4148,6 @@ class TestNestedTensorSubclass(TestCase):
             output_ref_atol, output_ref_rtol = get_tolerances(out, out_lp_ref)
 
             self.assertEqual(out, out_component, atol=output_ref_atol, rtol=output_ref_rtol)
-
-    @skipIfTorchDynamo("SDPA test compiles internally")
-    @unittest.skipIf(IS_WINDOWS, reason="Windows not yet supported for torch.compile")
-    @unittest.skipIf(sys.version_info >= (3, 12), "torch.compile is not supported on python 3.12+")
-    @skipCUDAIf(not SM70OrLater, "GPU capability is < SM70")
-    @onlyCUDA
-    @dtypes(*([torch.float16, torch.bfloat16, torch.float32] if SM80OrLater
-            else [torch.float16, torch.float32]))
-    def test_sdpa_backwards(self, device, dtype):
-        values = torch.randn(9, 3, 256, requires_grad=True, device=device, dtype=dtype)
-        offsets = torch.tensor([0, 1, 3, 5, 9], device=device, dtype=torch.int64)
-
-        @torch.compile
-        def f(values, offsets):
-            nt = convert_jagged_to_nested_tensor(values, offsets, max_length=4)
-            nt = nt.transpose(-2, -3)
-            # purposefully graph break to trigger view replay for subclass view input
-            torch.tensor(1).item()
-            output = F.scaled_dot_product_attention(nt, nt, nt).transpose(-2, -3)
-            return convert_nt_to_jagged(output)
-
-        output = f(values, offsets)
-        output.sum().backward()
-        self.assertEqual(values.grad, torch.ones_like(values))
 
 
 instantiate_parametrized_tests(TestNestedTensor)
