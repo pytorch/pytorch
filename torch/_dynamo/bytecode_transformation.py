284b07ba6360e7a9f78e2c90c02915be918ee3b9
William Wen <williamwen@meta.com>
2024-04-05 14:14:24 -0700

    [dynamo, 3.12] fix block stack related issues (#123392)
    `JUMP_BACKWARD` in 3.12+ may not be in the exception table even though it should be considered a part of the block. Also fix a issue where we didn't propagate the exception table entry to new instructions when expanding the `POP_JUMP_IF_[NOT_]NONE` instruction.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123392
    Approved by: https://github.com/jansel

--- torch/_dynamo/bytecode_transformation.py
+++ torch/_dynamo/bytecode_transformation.py
@@ -813,6 +813,12 @@ def remove_jump_if_none(instructions: List[Instruction]) -> None:
                 )
             else:
                 jump_op = create_instruction("POP_JUMP_IF_TRUE", target=inst.target)
+            # update inst.exn_tab_entry.end if necessary
+            if inst.exn_tab_entry and inst.exn_tab_entry.end is inst:
+                inst.exn_tab_entry.end = jump_op
+            # preserve exception table entries
+            is_op.exn_tab_entry = copy.copy(inst.exn_tab_entry)
+            jump_op.exn_tab_entry = copy.copy(inst.exn_tab_entry)
             # modify inst in-place to preserve jump target
             inst.opcode = dis.opmap["LOAD_CONST"]
             inst.opname = "LOAD_CONST"
