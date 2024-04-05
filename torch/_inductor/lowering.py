9bb31a41684c194009668c76705373daceef3b47
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-04-05 02:09:32 +0000

    Revert "[inductor] Add explicit ops.fma and use it in softmax_backward (#122518)"
    This reverts commit 05984e642b16b289f0871d3db9d14426a57b76f0.
    
    Reverted https://github.com/pytorch/pytorch/pull/122518 on behalf of https://github.com/huydhn due to Sorry for reverting your change but it starts failing in trunk https://hud.pytorch.org/pytorch/pytorch/commit/05984e642b16b289f0871d3db9d14426a57b76f0 ([comment](https://github.com/pytorch/pytorch/pull/122518#issuecomment-2038631010))

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -5637,7 +5637,6 @@ register_pointwise_numeric(aten.hypot)
 register_pointwise_numeric(aten.log10)
 register_pointwise_numeric(aten.log2)
 register_pointwise_numeric(aten.nextafter)
-register_pointwise_numeric(inductor_prims.fma, name="fma")
 
 from .codegen.common import pointwise_overrides_data
 
