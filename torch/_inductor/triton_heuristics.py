85279f3466356578b8e2ae0af0c8c5b4d6f3af17
sraikund16 <57577085+sraikund16@users.noreply.github.com>
2024-04-06 02:44:06 +0000

    Enhance RecordFunctionFast input args and use input args in triton_heuristics.py (#123459)
    Summary: Now that we can input shapes as input args for RecordFunctionFast, let's add that to the triton heuristics. Also, lets add the ability to pass in a tuple into the RecordFunctionFast constructor.
    
    Test Plan:
    Ran both the _inductor/test_profile.py and profiler/test_profiler.py unit tests. Also added tuple based unit test to profiler/test_profiler.py
    
    Ran record_function_fast.py from the following branch
    https://github.com/pytorch/pytorch/compare/sraikund/record_funct_test?expand=1
    
    No shape or args: tests function fast with no args and profile without record_shapes
    With shape tests: tests function fast with args and profile with record_shapes true
    Args no shape: tests function fast with args inputted but record_shapes set to false
    Args shape tuple: tests function fast with args inputted in form of tuple and record_shapes true
    
    Stdout:
    
    No shape or args:: 1.8491458892822266 us
    With shape:: 2.211381196975708 us
    Args no shape:: 1.9212646484375 us
    With shape tuple:: 2.245788335800171 us
    
    Differential Revision: D55809967
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123459
    Approved by: https://github.com/davidberard98

--- torch/_inductor/triton_heuristics.py
+++ torch/_inductor/triton_heuristics.py
@@ -190,11 +190,6 @@ class CachingAutotuner(KernelInterface):
             is_mm=False, name=self.fn.__name__, size_hints=size_hints
         )
 
-        # pre-create the profiler context manager to reduce latency
-        self.record_function_ctx = torch._C._profiler._RecordFunctionFast(
-            self.inductor_meta.get("kernel_name", "triton kernel")
-        )
-
     def precompile(self, warm_cache_only_with_cc=None):
         with self.lock:
             if self.launchers:
@@ -732,13 +727,15 @@ class CachingAutotuner(KernelInterface):
                 {**dict(zip(self.arg_names, args)), **launcher.config.kwargs, **kwargs}
             )
 
-        # guard the record_function_ctx and only call it if profiling is currently
+        # guard the record function and only call it if profiling is currently
         # in progress, to reduce latency when profiler is not turned on. Note that
         # the "if" statement (instead of, say, a contextlib.nullcontext) is intentional;
         # it is faster than entering and exiting a context manager, even if the context
         # manager is a nullcontext.
         if autograd_profiler._is_profiler_enabled:
-            with self.record_function_ctx:
+            with torch._C._profiler._RecordFunctionFast(
+                self.inductor_meta.get("kernel_name", "triton kernel"), args
+            ):
                 return launcher(
                     *args,
                     **kwargs,
