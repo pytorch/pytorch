fad40e4412b93f312581394fdb3b44ef62a7a43e
Animesh Jain <anijain@umich.edu>
2024-04-07 10:53:51 -0700

    [dynamo][cpp-guards] Fix bug for slices (#123516)
    Automatic testing as soon as we turn on cpp guards by default.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123516
    Approved by: https://github.com/jansel
    ghstack dependencies: #123515

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -579,13 +579,13 @@ class GuardBuilder(GuardBuilderBase):
                     source=source_name,
                     example_value=example_value,
                 )
-            elif isinstance(base_example_value, list):
+            elif isinstance(base_example_value, list) and not source.index_is_slice:
                 return base_guard_manager.list_getitem_manager(
                     key=source.index,
                     source=source_name,
                     example_value=example_value,
                 )
-            elif isinstance(base_example_value, tuple):
+            elif isinstance(base_example_value, tuple) and not source.index_is_slice:
                 return base_guard_manager.tuple_getitem_manager(
                     key=source.index,
                     source=source_name,
