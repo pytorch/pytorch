c9163d01681bcbc875b51bdcdea5283ce466aa88
Isuru Fernando <ifernando@quansight.com>
2024-04-09 18:52:17 +0000

    Improve decomposition for constand_pad_nd
    Replaces the previous decomposition that used a fill and a copy
    using _unsafe_masked_index and removes the lowering
    
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -3773,56 +3773,6 @@ def rev(x, dims):
     )
 
 
-@register_lowering(aten.constant_pad_nd, type_promotion_kind=None)
-def constant_pad_nd(x, padding, fill_value=0):
-    assert (len(padding) % 2) == 0
-    if all(p == 0 for p in padding):
-        return clone(x)
-
-    sizes = x.get_size()
-
-    bounds = list(reversed(list(zip(padding[::2], padding[1::2]))))
-    n = len(sizes) - len(bounds)
-
-    # if padding is a complicated expression, hoist it
-    bounds_precomp: List[Tuple[sympy.Symbol, Any]] = []
-    for l, h in bounds:
-        bounds_precomp.append((V.graph.sizevars.lookup_precomputed_size(l), h))  # type: ignore[arg-type]
-
-    output_size = list(sizes[:n])
-    mask_sizes = []
-    for (low, high), size in zip(bounds, sizes[n:]):
-        mask_sizes.append(size)
-        output_size.append(sympy.expand(size + low + high))
-    assert len(output_size) == len(sizes)
-    fill_value = dtype_to_type(x.get_dtype())(fill_value)
-
-    def mask(index):
-        mask = []
-        for idx, (low, high), length in zip(index[n:], bounds, mask_sizes):
-            if low != 0:
-                mask.append(range_mask_low(idx, 0))
-            if high != 0:
-                mask.append(range_mask_high(idx, length))
-        mask = functools.reduce(ops.and_, mask)
-        return ops.masked(mask, lambda: x_loader(index), fill_value)
-
-    def offset_fn(index):
-        new_index = list(index[:n])
-        for idx, (low, high) in zip(index[n:], bounds_precomp):
-            new_index.append(idx - low)
-        assert len(new_index) == len(index)
-        return mask(new_index)
-
-    x_loader = x.make_loader()
-    return Pointwise.create(
-        device=x.get_device(),
-        dtype=x.get_dtype(),
-        inner_fn=offset_fn,
-        ranges=output_size,
-    )
-
-
 def range_mask_low(i: sympy.Expr, low: Union[sympy.Expr, int]):
     return ops.ge(
         ops.index_expr(i, torch.int64),
--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2816,82 +2816,49 @@ def conj(input: TensorLikeType) -> TensorLikeType:
     return prims.conj(input)
 
 
-# This replicates at::constant_pad_nd, defined in ATen/native/PadNd.cpp
 @register_decomposition(aten.constant_pad_nd)
+@pw_cast_for_opmath
 @out_wrapper()
 def constant_pad_nd(
-    input: TensorLikeType, pad: List[int], value: NumberType = 0
-) -> TensorLikeType:
+    a: Tensor,
+    padding: Tuple[int, ...],
+    fill_value: NumberType = 0,
+) -> Tensor:
     torch._check(
-        len(pad) % 2 == 0,
-        lambda: f"Length of pad must be even but instead it equals {len(pad)}",
+        len(padding) % 2 == 0,
+        lambda: f"constant_pad_nd requires an even number of padding",
     )
+    dim = len(padding) // 2
+    inp_shape = a.shape[-dim:]
+    nc_dim = a.dim() - dim
 
-    input_sizes = input.shape
-    l_inp = len(input_sizes)
+    padding_left = [padding[2 * (dim - 1 - i)] for i in range(dim)]
+    padding_right = [padding[2 * (dim - 1 - i) + 1] for i in range(dim)]
 
-    l_pad = len(pad) // 2
-    l_diff = l_inp - l_pad
+    out_indices = [torch.arange(-padding_left[i], inp_shape[i] + padding_right[i], device=a.device) for i in range(dim)]
 
-    torch._check(
-        l_inp >= l_pad,
-        lambda: "Length of pad should be no more than twice the number of "
-        f"dimensions of the input. Pad length is {len(pad)} while the input has "
-        f"{l_inp} dimensions.",
-    )
+    idx: List[Any] = [None] * a.dim()
+    for i in range(dim):
+        idx[i + nc_dim] = out_indices[i]
 
-    c_input = input
-    for i in range(l_diff, l_inp):
-        pad_idx = 2 * (l_inp - i - 1)
-        if pad[pad_idx] < 0:
-            c_input = c_input.narrow(i, -pad[pad_idx], c_input.shape[i] + pad[pad_idx])
-
-        if pad[pad_idx + 1] < 0:
-            c_input = c_input.narrow(i, 0, c_input.shape[i] + pad[pad_idx + 1])
-
-    # if none of the pads are positive we can just return the result
-    if builtins.all(p <= 0 for p in pad):
-        return c_input.clone()
-
-    new_shape = list(input_sizes[:l_diff])
-
-    for i in range(l_pad):
-        pad_idx = len(pad) - ((i + 1) * 2)
-        new_dim = input_sizes[l_diff + i] + pad[pad_idx] + pad[pad_idx + 1]
-        torch._check(
-            new_dim > 0,
-            lambda: f"The input size {input_sizes[l_diff + i]}, plus negative padding "
-            f"{pad[pad_idx]} and {pad[pad_idx + 1]} resulted in a negative output size, "
-            f"which is invalid. Check dimension {l_diff + i} of your input.",
-        )
-        new_shape.append(new_dim)
-
-    memory_format = utils.suggest_memory_format(input)
-    output = torch.empty(
-        new_shape,
-        dtype=input.dtype,
-        device=input.device,
-        requires_grad=input.requires_grad,
-        memory_format=memory_format,
-    )
-
-    if value == 0 and input.dtype == torch.bool:
-        value = False
-    # torch.fill isn't typed to allow complex values
-    output = torch.fill(output, value)  # type: ignore[arg-type]
-
-    c_output = output
-    for i in range(l_diff, l_inp):
-        pad_idx = 2 * (l_inp - i - 1)
-        if pad[pad_idx] > 0:
-            c_output = c_output.narrow(
-                i, pad[pad_idx], c_output.shape[i] - pad[pad_idx]
+    if all(pad <=0 for pad in padding):
+        result = aten._unsafe_index(a, idx)
+    else:
+        conds = []
+        for i in range(dim):
+            view_shape = [1] * a.dim()
+            view_shape[nc_dim + i] = out_indices[i].shape[0]
+            idx = out_indices[i].view(view_shape)
+            conds.append(
+                torch.logical_and(idx >= 0, idx < a.shape[nc_dim + i])
             )
-        if pad[pad_idx + 1] > 0:
-            c_output = c_output.narrow(i, 0, c_output.shape[i] - pad[pad_idx + 1])
+        mask = functools.reduce(torch.logical_and, conds)
+        result = aten._unsafe_masked_index(a, mask, idx, fill_value)
 
-    prims.copy_to(c_output, c_input)
-    return output
+    # convert output to correct memory format, if necessary
+    memory_format = utils.suggest_memory_format(result)
+    result = result.contiguous(memory_format=memory_format)
+    return result
 
 
 def contiguous(
