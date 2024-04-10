33e42541de124b09cc0e727adce7a1d7cac1bce1
xinan.lin <xinan.lin@intel.com>
2024-04-09 18:20:15 -0700

    Update on "[WIP][ATen] Add CPU fp16 support for nll_loss_out_frame"
    Fix issue #123328.
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -4014,13 +4014,6 @@ module_db: List[ModuleInfo] = [
                    # No channels_last support for loss functions.
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_memory_format'),
                    # Expect failures for tests that rely on torch.half implementation on CPU
-                   DecorateInfo(unittest.expectedFailure, "TestModule", "test_forward", dtypes=[torch.float16], device_type='cpu'),
-                   DecorateInfo(unittest.expectedFailure, "TestModule", "test_if_train_and_eval_modes_differ",
-                                dtypes=[torch.float16], device_type='cpu'),
-                   DecorateInfo(unittest.expectedFailure, "TestModule", "test_save_load", dtypes=[torch.float16],
-                                device_type='cpu'),
-                   DecorateInfo(unittest.expectedFailure, "TestModule", "test_non_contiguous_tensors", dtypes=[torch.float16],
-                                device_type='cpu'),
                    DecorateInfo(unittest.expectedFailure, "TestModule", "test_multiple_device_transfer", dtypes=[torch.float16],
                                 device_type='cuda'),
                    DecorateInfo(unittest.expectedFailure, "TestModule", "test_cpu_gpu_parity", dtypes=[torch.float16],
