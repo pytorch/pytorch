1a2d442c55b464bae06f1c33187ee70d2abe8ea1
willfengg <weif@meta.com>
2024-04-10 10:02:30 -0700

    cd docs instead pytorch/docs
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- .ci/pytorch/python_doc_push_script.sh
+++ .ci/pytorch/python_doc_push_script.sh
@@ -105,7 +105,7 @@ if [ "$is_main_doc" = true ]; then
     echo undocumented objects found:
     cat build/coverage/python.txt
     echo "Make sure you've updated relevant .rsts in docs/source!"
-    echo "You can reproduce locally by running 'cd pytorch/docs && make coverage && cat build/coverage/python.txt'"
+    echo "You can reproduce locally by running 'cd docs && make coverage && cat build/coverage/python.txt'"
     exit 1
   fi
 else
