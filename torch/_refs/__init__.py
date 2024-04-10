de0c707bd800f632de8c45dc9e9581e4af88c2eb
Isuru Fernando <ifernando@quansight.com>
2024-04-10 18:37:31 +0000

    Update base for Update on "Use _unsafe_masked_index in masked_scatter decomposition"
    and remove masked_scatter_with_index inductor prims
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 aakhundov ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2816,51 +2816,82 @@ def conj(input: TensorLikeType) -> TensorLikeType:
     return prims.conj(input)
 
 
+# This replicates at::constant_pad_nd, defined in ATen/native/PadNd.cpp
 @register_decomposition(aten.constant_pad_nd)
 @out_wrapper()
 def constant_pad_nd(
-    input: Tensor,
-    pad: Tuple[int, ...],
-    value: NumberType = 0,
-) -> Tensor:
+    input: TensorLikeType, pad: List[int], value: NumberType = 0
+) -> TensorLikeType:
     torch._check(
         len(pad) % 2 == 0,
-        lambda: "constant_pad_nd requires an even number of padding",
+        lambda: f"Length of pad must be even but instead it equals {len(pad)}",
     )
-    dim = len(pad) // 2
-    inp_shape = input.shape[-dim:]
-    nc_dim = input.dim() - dim
 
-    pad_left = [pad[2 * (dim - 1 - i)] for i in range(dim)]
-    pad_right = [pad[2 * (dim - 1 - i) + 1] for i in range(dim)]
+    input_sizes = input.shape
+    l_inp = len(input_sizes)
 
-    out_indices = [
-        torch.arange(
-            -pad_left[i], inp_shape[i] + pad_right[i], device=input.device
-        ).reshape(-1, *[1] * (dim - 1 - i))
-        for i in range(dim)
-    ]
+    l_pad = len(pad) // 2
+    l_diff = l_inp - l_pad
 
-    indices: List[Any] = [None] * input.dim()
-    for i in range(dim):
-        indices[i + nc_dim] = out_indices[i]
+    torch._check(
+        l_inp >= l_pad,
+        lambda: "Length of pad should be no more than twice the number of "
+        f"dimensions of the input. Pad length is {len(pad)} while the input has "
+        f"{l_inp} dimensions.",
+    )
 
-    if builtins.all(pad <= 0 for pad in pad):
-        result = aten._unsafe_index(input, indices)
-    else:
-        conds = []
-        for i in range(dim):
-            view_shape = [1] * input.dim()
-            view_shape[nc_dim + i] = out_indices[i].shape[0]
-            idx = out_indices[i].view(view_shape)
-            conds.append(torch.logical_and(idx >= 0, idx < input.shape[nc_dim + i]))
-        mask = reduce(torch.logical_and, conds)
-        result = aten._unsafe_masked_index(input, mask, indices, value)
+    c_input = input
+    for i in range(l_diff, l_inp):
+        pad_idx = 2 * (l_inp - i - 1)
+        if pad[pad_idx] < 0:
+            c_input = c_input.narrow(i, -pad[pad_idx], c_input.shape[i] + pad[pad_idx])
 
-    # convert output to correct memory format, if necessary
+        if pad[pad_idx + 1] < 0:
+            c_input = c_input.narrow(i, 0, c_input.shape[i] + pad[pad_idx + 1])
+
+    # if none of the pads are positive we can just return the result
+    if builtins.all(p <= 0 for p in pad):
+        return c_input.clone()
+
+    new_shape = list(input_sizes[:l_diff])
+
+    for i in range(l_pad):
+        pad_idx = len(pad) - ((i + 1) * 2)
+        new_dim = input_sizes[l_diff + i] + pad[pad_idx] + pad[pad_idx + 1]
+        torch._check(
+            new_dim > 0,
+            lambda: f"The input size {input_sizes[l_diff + i]}, plus negative padding "
+            f"{pad[pad_idx]} and {pad[pad_idx + 1]} resulted in a negative output size, "
+            f"which is invalid. Check dimension {l_diff + i} of your input.",
+        )
+        new_shape.append(new_dim)
+
     memory_format = utils.suggest_memory_format(input)
-    result = result.contiguous(memory_format=memory_format)
-    return result
+    output = torch.empty(
+        new_shape,
+        dtype=input.dtype,
+        device=input.device,
+        requires_grad=input.requires_grad,
+        memory_format=memory_format,
+    )
+
+    if value == 0 and input.dtype == torch.bool:
+        value = False
+    # torch.fill isn't typed to allow complex values
+    output = torch.fill(output, value)  # type: ignore[arg-type]
+
+    c_output = output
+    for i in range(l_diff, l_inp):
+        pad_idx = 2 * (l_inp - i - 1)
+        if pad[pad_idx] > 0:
+            c_output = c_output.narrow(
+                i, pad[pad_idx], c_output.shape[i] - pad[pad_idx]
+            )
+        if pad[pad_idx + 1] > 0:
+            c_output = c_output.narrow(i, 0, c_output.shape[i] - pad[pad_idx + 1])
+
+    prims.copy_to(c_output, c_input)
+    return output
 
 
 def contiguous(
