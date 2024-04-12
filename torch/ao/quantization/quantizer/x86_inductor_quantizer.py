2b4b6e354d5dd07aa3f4d21b960ec983c76c4cce
Xia, Weiwen <weiwen.xia@intel.com>
2024-04-12 20:40:20 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/ao/quantization/quantizer/x86_inductor_quantizer.py
+++ torch/ao/quantization/quantizer/x86_inductor_quantizer.py
@@ -750,7 +750,6 @@ class X86InductorQuantizer(Quantizer):
             self._annotate_linear_binary_unary(model, config)
             self._annotate_linear_unary(model, config)
             self._annotate_linear(model, config)
-            self._annotate_linear(model, config)
 
     def _annotate_conv2d_binary_unary(
         self, gm: torch.fx.GraphModule, quantization_config: QuantizationConfig
@@ -1050,13 +1049,6 @@ class X86InductorQuantizer(Quantizer):
                 input_node = node.all_input_nodes[0]
                 self._annotate_output_share_observer_as_input(input_node, node)
         return
-
-    def _annotate_linear_fusion_pattern(
-        self, model: torch.fx.GraphModule, config: QuantizationConfig
-    ):
-        self._annotate_linear_binary_unary(model, config)
-        self._annotate_linear_unary(model, config)
-        self._annotate_linear(model, config)
 
     def _annotate_linear(
         self, gm: torch.fx.GraphModule, quantization_config: QuantizationConfig
