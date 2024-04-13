pytorch/pytorch

commit 1d6c5972c12e4742b2f2b2776f0b3421c0e1c678

[BE]: Optimize min/max/sum comprehensions C419 (#123960)
Automatic fixes that replaces certain list comprehensions with generator ones where appropriate so that they are immediately consumed. This is preview functionality in ruff for rule C419 and it was automatically applied.

Co-authored-by: Nikita Shulga <2453524+malfet@users.noreply.github.com>
Pull Request resolved: https://github.com/pytorch/pytorch/pull/123960
Approved by: https://github.com/malfet

torch/amp/grad_scaler.py
@@ -426,10 +426,8 @@ class GradScaler:
                 found_inf = cast(
                     torch.Tensor,
                     sum(
-                        [
-                            t.to(scaler.device, non_blocking=True)
-                            for t in optimizer_state["found_inf_per_device"].values()
-                        ]
+                        t.to(scaler.device, non_blocking=True)
+                        for t in optimizer_state["found_inf_per_device"].values()
                     ),
                 )
                 optimizer.grad_scale = (  # type: ignore[attr-defined]

benchmarks/gpt_fast/benchmark.py
@@ -194,10 +194,8 @@ def run_experiment(
 
     torch.manual_seed(1234)
     model_size = sum(
-        [
-            p.numel() * p.dtype.itemsize
-            for p in itertools.chain(model.parameters(), model.buffers())
-        ]
+        p.numel() * p.dtype.itemsize
+        for p in itertools.chain(model.parameters(), model.buffers())
     )
 
     aggregate_metrics = {"tokens_per_sec": []}

benchmarks/tensorexpr/rnn_eltwise.py
@@ -57,7 +57,7 @@ class RNNEltwise(benchmark.Benchmark):
         def memsize(t):
             return t.numel() * t.element_size()
 
-        input_size = sum([memsize(t) for t in self.inputs])
+        input_size = sum(memsize(t) for t in self.inputs)
         output_size = 2 * memsize(self.cx)
         io_size = input_size + output_size
         return {"sol": io_size, "algorithmic": io_size}

scripts/compile_tests/failures_histogram.py
@@ -97,7 +97,7 @@ def failures_histogram(eager_dir, dynamo_dir, verbose=False, format_issues=False
         else "(num_failed_tests, error_msg, sample_test)"
     )
     print(header)
-    sum_counts = sum([r[0] for r in result])
+    sum_counts = sum(r[0] for r in result)
     for row in result:
         if format_issues:
             print(as_issue(*row))

test/distributed/optim/test_zero_redundancy_optimizer.py
@@ -530,7 +530,7 @@ class TestZeroRedundancyOptimizerDistributed(TestZeroRedundancyOptimizer):
             params.append(torch.rand(size, 1))
         o = ZeroRedundancyOptimizer(params, optimizer_class=SGD, lr=LR)
         self.assertEqual(
-            sum([x.numel() for x in o.optim.param_groups[0]["params"]]),
+            sum(x.numel() for x in o.optim.param_groups[0]["params"]),
             sum(sizes),
         )
 
@@ -567,7 +567,7 @@ class TestZeroRedundancyOptimizerDistributed(TestZeroRedundancyOptimizer):
             # all partitions have the same elements
             self.assertEqual(len(o.param_groups), 2)
             self.assertEqual(
-                sum([x.numel() for g in o.optim.param_groups for x in g["params"]]),
+                sum(x.numel() for g in o.optim.param_groups for x in g["params"]),
                 sum(sizes),
             )
             self.assertEqual(len(o.optim.param_groups), 2)

test/distributed/pipeline/sync/test_transparency.py
@@ -15,7 +15,7 @@ from torch.testing._internal.common_utils import run_tests
 
 def test_simple_linears(setup_rpc):
     def sum_grad(parameters):
-        return sum([p.grad.sum() for p in parameters if p.grad is not None])
+        return sum(p.grad.sum() for p in parameters if p.grad is not None)
 
     def zero_grad(parameters):
         for p in parameters:

test/functorch/discover_coverage.py
@@ -254,11 +254,11 @@ def get_ops_percentage(torch_threshold, nn_fn_threshold):
 
     # get all operators that are not in the denylist
     all_ops = get_top_ops(999999, 999999)
-    total_op_usages = sum([get_num_usages(op) for op in all_ops])
+    total_op_usages = sum(get_num_usages(op) for op in all_ops)
 
     # get subset of all operators
     subset_ops = get_top_ops(torch_threshold, nn_fn_threshold)
-    subset_op_usages = sum([get_num_usages(op) for op in subset_ops])
+    subset_op_usages = sum(get_num_usages(op) for op in subset_ops)
     return subset_op_usages / total_op_usages
 
 

test/functorch/test_ops.py
@@ -467,7 +467,7 @@ class TestOperators(TestCase):
                 # Reduce into single value for grad
                 if isinstance(result, torch.Tensor):
                     return abs_if_complex(result.sum())
-                result = sum([abs_if_complex(res.sum()) for res in result])
+                result = sum(abs_if_complex(res.sum()) for res in result)
                 return result
 
             result = grad(wrapped_fn, diff_argnums)(*args, **kwargs)

commit a88494bfaea0e44232737b227a4aa49d7c103c8a

Fix minor perf regression from #123960

torch/amp/grad_scaler.py
@@ -426,8 +426,10 @@ class GradScaler:
                 found_inf = cast(
                     torch.Tensor,
                     sum(
-                        t.to(scaler.device, non_blocking=True)
-                        for t in optimizer_state["found_inf_per_device"].values()
+                        [  # noqa: C419
+                            t.to(scaler.device, non_blocking=True)
+                            for t in optimizer_state["found_inf_per_device"].values()
+                        ]
                     ),
                 )
                 optimizer.grad_scale = (  # type: ignore[attr-defined]