fc9e890073e54e4291a8fc6f266ff9f1ad8d3b19
Catherine Lee <csl@fb.com>
2024-04-15 16:00:51 -0700

    update

--- tools/testing/target_determination/gen_artifact.py
+++ tools/testing/target_determination/gen_artifact.py
@@ -2,7 +2,7 @@ import json
 import pathlib
 from typing import Any, List
 
-REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
+REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent.parent
 
 
 def gen_ci_artifact(included: List[Any], excluded: List[Any]) -> None:
