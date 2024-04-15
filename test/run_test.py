8e612d0f7d54d79cfc895b5d162f38e48f9a2b9c
Catherine Lee <csl@fb.com>
2024-04-15 14:24:11 -0700

    update

--- test/run_test.py
+++ test/run_test.py
@@ -1285,6 +1285,7 @@ def must_serial(file: Union[str, ShardedTest]) -> bool:
         or DISTRIBUTED_TEST_PREFIX in file
         or file in CUSTOM_HANDLERS
         or file in RUN_PARALLEL_BLOCKLIST
+        or file in CI_SERIAL_LIST
         or file in JIT_EXECUTOR_TESTS
         or file in ONNX_SERIAL_LIST
         or NUM_PROCS == 1
