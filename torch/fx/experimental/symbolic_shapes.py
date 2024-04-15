769ac5df40a028fd8543ca04f3ca4357a759012e
Edward Z. Yang <ezyang@meta.com>
2024-04-15 14:59:53 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -4108,7 +4108,7 @@ class ShapeEnv:
                     "version": 2,
                 },
             )
-            log.warning("Ignored guard %s == %s, this could result in accuracy problems", expr, concrete_val)
+            log.warning("Ignored guard %s == %s, this could result in accuracy problems", expr, concrete_val, stack_info=True)
 
 
     def _get_stack_summary(self, is_debug: bool = False):
