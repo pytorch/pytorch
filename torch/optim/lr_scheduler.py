84ce8386bf67318a4ff629c2af1aae7ad22daf32
FFFrog <ljw1101.vip@gmail.com>
2024-04-15 17:32:39 +0800

    add preparatory work for torch/optim/lr_scheduler.py (#124048)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124048
    Approved by: https://github.com/albanD

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -1781,8 +1781,9 @@ class OneCycleLR(LRScheduler):
         step_num = self.last_epoch
 
         if step_num > self.total_steps:
-            raise ValueError("Tried to step {} times. The specified number of total steps is {}"
-                             .format(step_num, self.total_steps))
+            raise ValueError(
+                f"Tried to step {step_num} times. The specified number of total steps is {self.total_steps}"  # noqa: UP032
+            )
 
         for group in self.optimizer.param_groups:
             start_step = 0
