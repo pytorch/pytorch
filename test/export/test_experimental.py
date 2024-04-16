d34fe764296eb9ab88ba1697ae613c41dbb91b05
ydwu4 <ydwu2014@gmail.com>
2024-04-16 12:26:52 -0700

    Update on "Fix example_value of map"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang
    
    [ghstack-poisoned]

--- test/export/test_experimental.py
+++ test/export/test_experimental.py
@@ -45,11 +45,11 @@ class TestExperiment(TestCase):
             """\
 def forward(self, b_submodule_buffer1, x):
     sin = torch.ops.aten.sin.default(x)
-    strict_graph_1 = self.strict_graph_1
-    strict_mode_1 = torch.ops.higher_order.strict_mode(strict_graph_1, (sin, b_submodule_buffer1));  strict_graph_1 = sin = b_submodule_buffer1 = None
-    getitem_1 = strict_mode_1[0];  strict_mode_1 = None
+    strict_graph_0 = self.strict_graph_0
+    strict_mode = torch.ops.higher_order.strict_mode(strict_graph_0, (sin, b_submodule_buffer1));  strict_graph_0 = sin = b_submodule_buffer1 = None
+    getitem = strict_mode[0];  strict_mode = None
     add = torch.ops.aten.add.Tensor(x, 3);  x = None
-    return (getitem_1, add)""",
+    return (getitem, add)""",
         )
 
         self.assertExpectedInline(
