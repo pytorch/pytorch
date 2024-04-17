6337b99cde694c17f56acc06a21b810e4f415354
Pearu Peterson <pearu.peterson@gmail.com>
2024-04-17 20:20:18 +0300

    Add index_reduce decomposition (#122579)
    As in the title.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122579
    Approved by: https://github.com/peterbell10
    ghstack dependencies: #123375

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -27,6 +27,7 @@ from torch._prims_common import (
 )
 
 from . import config, inductor_prims
+from .utils import needs_fallback_due_to_atomic_add_limitations, use_scatter_fallback
 
 log = logging.getLogger(__name__)
 aten = torch.ops.aten
@@ -705,3 +706,50 @@ def _softmax_backward_data(grad_output, output, dim, input_dtype):
     if grad_output.dtype != input_dtype:
         grad_input = grad_input.to(input_dtype)
     return grad_input.contiguous()
+
+
+@register_decomposition(aten.index_reduce)
+def index_reduce(
+    self, dim: int, index, src, reduction_type: str, *, include_self: bool = True
+):
+    if reduction_type == "mean" and not needs_fallback_due_to_atomic_add_limitations(
+        self.dtype
+    ):
+        true_division = self.dtype.is_floating_point or self.dtype.is_complex
+        ones = torch.ones_like(src)
+        if include_self:
+            out = self
+            counts = torch.ones_like(self).index_add(dim, index, ones)
+        else:
+            out = self.index_fill(dim, index, 0)
+            counts = torch.zeros_like(self).index_add(dim, index, ones)
+            counts = counts.masked_fill(counts < 1, 1)
+        out = out.index_add(dim, index, src)
+        return out / counts if true_division else out // counts
+
+    if use_scatter_fallback(
+        "aten.scatter_reduce_",
+        reduction_type,
+        self.dtype,
+        src.dtype,
+        src.device.type,
+        True,
+    ):
+        return NotImplemented
+
+    repeats = self.shape[dim + 1 :].numel() * self.shape[:dim].numel()
+    index_shape = (index.numel(), *self.shape[dim + 1 :], *self.shape[:dim])
+    perm = (*range(self.ndim - dim, self.ndim), 0, *range(1, self.ndim - dim))
+    scatter_index = (
+        index.to(torch.int64)
+        .repeat_interleave(repeats)
+        .reshape(index_shape)
+        .permute(perm)
+    )
+    return self.scatter_reduce(
+        dim,
+        scatter_index,
+        src,
+        reduction_type,
+        include_self=include_self,
+    )
