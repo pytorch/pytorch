a56957da65acb49ea8152797257bb4f8a7b80d3a
Jiong Gong <jiong.gong@intel.com>
2024-04-17 06:51:52 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_gemm_template.py
+++ torch/_inductor/codegen/cpp_gemm_template.py
@@ -16,7 +16,6 @@ GEMM_TEMPLATE = r"""
 extern "C"
 {{kernel.def_kernel(inputs=[X, W, inp], outputs=[Y], names_str="X, W, inp, Y")}}
 {
-    // TODO: support dynamic shapes
     int64_t M = {{kernel.size(Y, 0)}};
     int64_t N = {{kernel.size(Y, 1)}};
     int64_t K = {{kernel.size(X, 1)}};
