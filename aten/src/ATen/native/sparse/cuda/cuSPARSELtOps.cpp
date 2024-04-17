8cd0bdf461cef703f65cf89242b095b65774ffca
Peter Y. Yeh <pyeh@amd.com>
2024-04-16 20:38:12 -0700

    add USE_ROCM && ROCM_VERSION

--- aten/src/ATen/native/sparse/cuda/cuSPARSELtOps.cpp
+++ aten/src/ATen/native/sparse/cuda/cuSPARSELtOps.cpp
@@ -148,7 +148,7 @@ std::tuple<int64_t, at::Tensor> _cslt_sparse_mm_impl(
         break;
 
 // cuSPARSELt v0.5.2 onwards changes CUSPARSE_COMPUTE_TF32, CUSPARSE_COMPUT_16F to CUSPARSE_COMPUTE_32F
-#if defined(CUSPARSELT_VERSION) && CUSPARSELT_VERSION >= 502
+#if ((defined(CUSPARSELT_VERSION) && CUSPARSELT_VERSION >= 502)) || (defined(USE_ROCM) && (ROCM_VERSION >= 61000))
     case at::ScalarType::Half:
         input_type = CUDA_R_16F;
         output_type = CUDA_R_16F;
@@ -159,7 +159,7 @@ std::tuple<int64_t, at::Tensor> _cslt_sparse_mm_impl(
         output_type = CUDA_R_16BF;
         compute_type = CUSPARSE_COMPUTE_32F;
         break;
-    case at::ScalarType::Float:
+    case at::ScalarType::Float: //TODO ROCM does not support R_32F
         input_type = CUDA_R_32F;
         output_type = CUDA_R_32F;
         compute_type = CUSPARSE_COMPUTE_32F;
