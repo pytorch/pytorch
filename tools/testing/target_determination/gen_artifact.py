0bc0248275c53b9f3ad859204db3ed787bbf5307
Catherine Lee <csl@fb.com>
2024-04-17 09:42:52 -0700

    update

--- tools/testing/target_determination/gen_artifact.py
+++ tools/testing/target_determination/gen_artifact.py
@@ -1,11 +1,12 @@
 import json
+import os
 import pathlib
 from typing import Any, List
-import os
 
 REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent.parent
 
 
 def gen_ci_artifact(included: List[Any], excluded: List[Any]) -> None:
-    with open(REPO_ROOT / f"test/test-reports/td_exclusions-{os.urandom(10).hex()}.json", "w") as f:
+    file_name = f"td_exclusions-{os.urandom(10).hex()}.json"
+    with open(REPO_ROOT / f"test/test-reports/{file_name}.json", "w") as f:
         json.dump({"included": included, "excluded": excluded}, f)
