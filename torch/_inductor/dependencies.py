2a82063d54ba0f081631c067de373f6118968221
Shunting Zhang <shunting@fb.com>
2024-04-18 11:21:50 -0700

    [inductor] consider pointwise nodes when deciding reduction hint (#124131)
    In certain **rare** scenarios, inductor can generate a reduction kernel with really bad perf. E.g., if
    - the reduction kernel contains a reduction node followed by a pointwise node
    - And the pointwise node use a transposed layout.
    - the reduction node is an inner reduction
    - and rnumel <= 1024 ,
    
    then inductor will generate a persistent reduction kernel and it causes really bad perf when doing tl.store for the pointwise node since we use a very skinny tile `(XBLOCK=1, RBLOCK=next_power_of_2(rnumel))` .
    
    I've tried a few version of fix.
    - The first version is, if I found any pointwise node in a reduction kernel uses a non-contiguous dependency, we use ReductionHint.DEFAULT. This cause 8s compilation time increase for huggingface with no perf wins... The reason is ReductionHint.DEFAULT does more autotunings.
    - Then I changed the code to be more specific. We change the hint from INNER to DEFAULT if we are sure that the pointwise kernel can use a >1 stride for the lowest dimension. Kernels meet this condition should mostly have really bad perf anyways.
    
    The situation mentioned above is rare. But it's reported by internal users. I'll also run one more perf test.
    
    Testing script: https://gist.github.com/shunting314/9d3389891fa43633b49b8b7564ad6d8b . Something equivalent is also added as a unit test.
    
    For this specific test from user reports, we improve the mentioned reduction kernels perf by **4.14x** (451us -> 109us)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124131
    Approved by: https://github.com/jansel

--- torch/_inductor/dependencies.py
+++ torch/_inductor/dependencies.py
@@ -71,6 +71,35 @@ class MemoryDep(typing.NamedTuple):
     def is_contiguous(self) -> bool:
         return isinstance(self.index, sympy.Symbol) and self.index in self.var_names
 
+    def stride1_for_last_dim(self, result_for_complex_expression=True) -> bool:
+        """
+        Whether the stride for the last dimension is 1.
+        """
+        # python test/inductor/test_torchinductor_opinfo.py -k test_comprehensive_masked_scatter_cuda_float16
+        # will exercise thru this corner case.
+        if len(self.var_names) == 0:
+            return True
+
+        terms = self.index.args if isinstance(self.index, sympy.Add) else [self.index]
+
+        last_sym = self.var_names[-1]
+        for term in terms:
+            if term is last_sym:
+                return True
+
+            # Having a >1 stride for the last dimension is bad for perf
+            # return False.
+            if (
+                isinstance(term, sympy.Mul)
+                and len(term.args) == 2
+                and term.args[1] is last_sym
+                and isinstance(term.args[0], (int, sympy.Integer))
+                and term.args[0] > 1
+            ):
+                return False
+
+        return result_for_complex_expression
+
     def is_scalar(self) -> bool:
         if isinstance(self.index, sympy.Symbol):
             return self.index not in self.var_names and not self.is_indirect()
