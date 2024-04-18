d7f54f3ae7b1c30e380c985753f047c84972fe3c
Isuru Fernando <ifernando@quansight.com>
2024-04-18 15:58:42 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/dependencies.py
+++ torch/_inductor/dependencies.py
@@ -1,3 +1,4 @@
+import abc
 import collections
 import dataclasses
 import itertools
@@ -25,11 +26,35 @@ from .virtualized import OpsHandler, ReductionType, V
 
 log = logging.getLogger(__name__)
 is_indirect = re.compile(r"indirect|tmp").search
-Dep = Union["MemoryDep", "StarDep", "WeakDep"]
 
 
-class MemoryDep(typing.NamedTuple):
+@dataclasses.dataclass(frozen=True)
+class Dep(abc.ABC):
     name: str
+
+    @abc.abstractmethod
+    def rename(self, renames: Dict[str, str]) -> "Dep":
+        pass
+
+    @abc.abstractmethod
+    def get_numel(self) -> sympy.Expr:
+        pass
+
+    @abc.abstractmethod
+    def numbytes_hint(self):
+        pass
+
+    @abc.abstractmethod
+    def has_unbacked_symbols(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def is_contiguous(self) -> bool:
+        pass
+
+
+@dataclasses.dataclass(frozen=True)
+class MemoryDep(Dep):
     index: sympy.Expr  # type: ignore[assignment]
     var_names: Tuple[sympy.Symbol, ...]
     size: Tuple[sympy.Expr, ...]
@@ -80,10 +105,8 @@ class MemoryDep(typing.NamedTuple):
         return any(is_indirect(v.name) for v in self.index.free_symbols)  # type: ignore[attr-defined]
 
 
-class StarDep(typing.NamedTuple):
+class StarDep(Dep):
     # depends on the entire buffer
-    name: str
-
     @property
     def index(self):
         raise NotImplementedError("StarDep does not have an index")
@@ -120,9 +143,7 @@ class StarDep(typing.NamedTuple):
 #
 # It is weak because if it turns out A's read is never used, we can still
 # eliminate it
-class WeakDep(typing.NamedTuple):
-    name: str
-
+class WeakDep(Dep):
     @property
     def index(self):
         raise NotImplementedError("WeakDep does not have an index")
@@ -145,7 +166,8 @@ class WeakDep(typing.NamedTuple):
         return False
 
 
-class IndexExprDep(typing.NamedTuple):
+@dataclasses.dataclass(frozen=True)
+class IndexExprDep:
     index: sympy.Expr  # type: ignore[assignment]
     var_names: Tuple[sympy.Symbol, ...]
     size: Tuple[sympy.Expr, ...]
