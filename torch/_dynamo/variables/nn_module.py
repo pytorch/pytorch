2a31e4ebe76dbb4d8949811a4386b577550bed53
Animesh Jain <anijain@umich.edu>
2024-04-18 16:31:15 -0700

    [dynamo][easy] Check that obj has __code__ attr before accessing it
    [ghstack-poisoned]

--- torch/_dynamo/variables/nn_module.py
+++ torch/_dynamo/variables/nn_module.py
@@ -790,7 +790,10 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
                     kwargs,
                 )
 
-            if id(method.__code__) in self._nn_module_method_ids():
+            if (
+                hasattr(method, "__code__")
+                and id(method.__code__) in self._nn_module_method_ids()
+            ):
                 unimplemented(f"UnspecializedNNModuleVariable missing {name}")
 
             # "_parameters" in self.value.__dict__ checks that module is initialized
