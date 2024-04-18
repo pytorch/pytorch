a5c32fbdb96d7797ec6dc8f6b6aa612c83143e11
Yuanhao Ji <jiyuanhao@apache.org>
2024-04-18 20:21:54 +0800

    fix: strip device_type in DeviceMesh

--- torch/distributed/device_mesh.py
+++ torch/distributed/device_mesh.py
@@ -523,6 +523,7 @@ else:
 
         Args:
             device_type (str): The device type of the mesh. Currently supports: "cpu", "cuda/cuda-like".
+                "cuda:0" will be converted to 'cuda'.
             mesh_shape (Tuple[int]): A tuple defining the dimensions of the multi-dimensional array
                 describing the layout of devices.
             mesh_dim_names (Tuple[str], optional): A tuple of mesh dimension names to assign to each dimension
@@ -553,6 +554,9 @@ else:
                     f"Found len(mesh_dim_names): {len(mesh_dim_names)} and len(mesh_shape):{len(mesh_shape)}.",
                 )
 
+        # e.g., strip the ':0' from 'cuda:0'
+        if device_type:
+            device_type = device_type.split(":")[0]
         mesh = torch.arange(math.prod(mesh_shape), dtype=torch.int).view(mesh_shape)
         device_mesh = DeviceMesh(
             device_type=device_type,
