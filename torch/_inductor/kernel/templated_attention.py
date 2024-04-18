aa4534b0e29fdc2baf8ac3871dccbdd58e452723
chilli <chilli@meta.com>
2024-04-18 11:40:41 -0700

    Optimized templated attention to use exp2 (#124356)
    0.705 (vs. FA2) to 0.860 after this change.
    
    <img width="1270" alt="image" src="https://github.com/pytorch/pytorch/assets/6355099/d58f57ba-e50e-44ea-8a8a-4f13b8650adf">
    
    to
    
    <img width="1277" alt="image" src="https://github.com/pytorch/pytorch/assets/6355099/f1945b67-0cfc-463c-a2f6-5812b90677fe">
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124356
    Approved by: https://github.com/drisspg

--- torch/_inductor/kernel/templated_attention.py
+++ torch/_inductor/kernel/templated_attention.py
@@ -1,8 +1,10 @@
 """ Triton Implementation of the Templated SDPA Kernel"""
 import logging
+from typing import Any, List
 
 import torch
-from ..select_algorithm import TritonTemplate
+from ..lowering import lowerings, register_lowering
+from ..select_algorithm import autotune_select_algorithm, TritonTemplate
 
 log = logging.getLogger(__name__)
 aten = torch.ops.aten
@@ -28,6 +30,13 @@ sdpa_template = TritonTemplate(
     # Q: Query, K: Key, V: Value
     # M: Number of queries, N: Number of keys/values, D: Model dimension
     # z: Batch size, h: Number of heads, m: Number of queries per head, k: Number of keys per head
+    # (Modifiable) Config options:
+    # BLOCK_M
+    # BLOCK_N
+    # SCORE_MOD_IS_LINEAR: Is the score modifier linear? If so, we can lift the
+    # change of base out of the loop
+    # ROWS_GUARANTEED_SAFE: Is it guaranteed that at least one value in each row
+    # is not masked out? If so, we can skip an extra safety check
 
     # Define Q Strides
     stride_qz = {{stride("Q", 0)}}
@@ -49,10 +58,8 @@ sdpa_template = TritonTemplate(
     H = {{size("Q", 1)}}
     N_CTX = {{size("Q", 2)}}
 
-    # TODO I think we should do some performance work
-    # to find the optimal calls for perf/accuracy to tl.dot
     qk_scale = 1.0
-    MATMUL_PRECISION = tl.float16
+    MATMUL_PRECISION = Q.dtype.element_ty
 
     start_m = tl.program_id(0)
     off_hz = tl.program_id(1)
@@ -89,12 +96,10 @@ sdpa_template = TritonTemplate(
     m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
     l_i = tl.zeros([BLOCK_M], dtype=tl.float32)
     acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-    # scale sm_scale by log_2(e) and use
-    # 2^x instead of exp in the loop because CSE and LICM
-    # don't work as expected with `exp` in the loop
-    # TODO fix me
-    # qk_scale = sm_scale * 1.44269504
+
     q = tl.load(Q_block_ptr)
+    if SCORE_MOD_IS_LINEAR:
+        qk_scale *= 1.44269504
     q = (q * qk_scale).to(MATMUL_PRECISION)
     # loop over k, v and update accumulator
     lo = 0
@@ -106,9 +111,8 @@ sdpa_template = TritonTemplate(
         v = tl.load(V_block_ptr)
         # -- compute qk ---
         qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-        qk += tl.dot(q, k.to(MATMUL_PRECISION))
+        qk = tl.dot(q, k.to(MATMUL_PRECISION), acc=qk)
         # ~~~~~~~~~~~~~~~~~~~ Apply score modification  ~~~~~~~~~~~~~~~~~~~
-
         {{ modification(
             score="qk",
             b="off_hz // H",
@@ -117,6 +121,9 @@ sdpa_template = TritonTemplate(
             n="start_n + offs_n[None, :]",
             out="qk"
         ) | indent_except_first(2) }}
+        # TODO: In the case that score_mod is linear, this can be LICMed
+        if not SCORE_MOD_IS_LINEAR:
+            qk *= 1.44269504
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         # -- compute scaling constant ---
@@ -124,18 +131,16 @@ sdpa_template = TritonTemplate(
         m_i_new = tl.maximum(m_i, row_max)
         masked_out_rows = (m_i_new == float("-inf"))
 
-        # TODO FIX ME and use 2^x instead of exp
-        # alpha = tl.math.exp2(m_i - m_i_new)
-        # p = tl.math.exp2(qk - m_i_new[:, None])
-        alpha = tl.math.exp(m_i - m_i_new)
-        alpha = tl.where(masked_out_rows, 0, alpha)
-        p = tl.math.exp(qk - m_i_new[:, None])
-        p = tl.where(masked_out_rows[:, None], 0, p)
+        alpha = tl.math.exp2(m_i - m_i_new)
+        p = tl.math.exp2(qk - m_i_new[:, None])
+        if not ROWS_GUARANTEED_SAFE:
+            alpha = tl.where(masked_out_rows, 0, alpha)
+            p = tl.where(masked_out_rows[:, None], 0, p)
 
         # -- scale and update acc --
         acc_scale = l_i * 0 + alpha  # workaround some compiler bug
         acc *= acc_scale[:, None]
-        acc += tl.dot(p.to(MATMUL_PRECISION), v.to(MATMUL_PRECISION))
+        acc = tl.dot(p.to(MATMUL_PRECISION), v.to(MATMUL_PRECISION), acc)
 
         # -- update m_i and l_i --
         l_i = l_i * alpha + tl.sum(p, 1)
@@ -159,3 +164,125 @@ sdpa_template = TritonTemplate(
     {{store_output(("idx_z", "idx_h", "idx_m", "idx_d"), "acc")}}
  """,
 )
+
+
+@register_lowering(torch.ops.higher_order.templated_attention)
+def templated_attention(*args, **kwargs):
+    from torch._prims_common import make_contiguous_strides_for
+    from ..ir import (
+        ComputedBuffer,
+        FixedLayout,
+        FlexibleLayout,
+        InputBuffer,
+        StorageBox,
+        TensorBox,
+    )
+
+    query, key, value, subgraph = args
+
+    def create_placeholder(name: str, dtype: torch.dtype) -> InputBuffer:
+        return TensorBox.create(
+            InputBuffer(
+                name,
+                FixedLayout(
+                    query.get_device(),
+                    dtype,
+                    [
+                        1,
+                    ],
+                    [
+                        1,
+                    ],
+                ),
+            )
+        )
+
+    scalar_inps = ["score", "b", "h", "m", "n"]
+    env = {}
+    cnt = 0
+    placeholder_inps = [
+        create_placeholder(name, dtype)
+        for name, dtype in [
+            ("score", query.get_dtype()),
+            ("b", torch.int64),
+            ("h", torch.int64),
+            ("m", torch.int64),
+            ("n", torch.int64),
+        ]
+    ]
+    for node in subgraph.graph_module.graph.nodes:
+        # There are two classes of placeholder inpts that we need
+        # to handle differently. For the first n_scalar_inps inputs
+        # we expect that these placeholders were generated by the make_fx call
+        # in the templated Attention HOP. So we need to create a new placeholder
+        # TensorBox for each of these inputs. For the rest of the inputs we
+        # expect that these are lifted inputs that fill up the '*other_buffers'
+        # tuple and already have corresponding TensorBoxes passed in as args.
+        if node.op == "placeholder":
+            is_lifted_input = cnt >= len(scalar_inps)
+            env[node] = args[cnt - 1] if is_lifted_input else placeholder_inps[cnt]
+            cnt += 1
+        elif node.op == "call_function":
+            # For call_function we use the defulat lowerings and pass in the
+            # already created TensorBoxes as args
+            from torch.utils._pytree import tree_map
+
+            env[node] = lowerings[node.target](
+                *tree_map(lambda x: env[x] if x in env else x, node.args)
+            )
+        elif node.op == "output":
+            # For the output node we need to create a ComputedBuffer
+            # which represents the actual score modification
+
+            output_buffer = env[node.args[0]]
+            assert isinstance(output_buffer.data, StorageBox), (
+                "The output node for the templated attention subgraph must be a StorageBox, but got: ",
+                type(output_buffer),
+            )
+            # Create the ComputedBuffere directly that will be inlined into the modfication block
+            subgraph_buffer = ComputedBuffer(
+                name=None,
+                layout=FlexibleLayout(
+                    device=output_buffer.data.get_device(),
+                    dtype=output_buffer.data.get_dtype(),
+                    size=output_buffer.data.get_size(),
+                ),
+                data=output_buffer.data.data,  # type: ignore[arg-type]
+            )
+
+            layout = FixedLayout(
+                output_buffer.get_device(),
+                query.get_dtype(),
+                query.get_size(),
+                make_contiguous_strides_for(query.get_size()),
+            )
+            choices: List[Any] = []
+            configs: List[Any] = []
+            if query.get_dtype() == torch.float32:
+                configs.append((64, 64, 4, 3))
+            configs += [
+                (128, 64, 4, 3),
+                (128, 128, 4, 3),
+                (128, 128, 8, 2),
+                (64, 128, 4, 3),
+            ]
+
+            for BLOCK_M, BLOCK_N, num_warps, num_stages in configs:
+                sdpa_template.maybe_append_choice(
+                    choices=choices,
+                    input_nodes=(query, key, value),
+                    layout=layout,
+                    subgraphs=subgraph_buffer,
+                    num_stages=num_stages,
+                    num_warps=num_warps,
+                    BLOCK_M=BLOCK_M,
+                    BLOCK_N=BLOCK_N,
+                    BLOCK_DMODEL=query.get_size()[-1],
+                    # For now, we always assume the "sound" option
+                    SCORE_MOD_IS_LINEAR=False,
+                    ROWS_GUARANTEED_SAFE=False,
+                )
+            return autotune_select_algorithm(
+                "sdpa", choices, [query, key, value], layout
+            )
+    raise ValueError("TemplatedAttention was passed a subgraph with no output node!")
