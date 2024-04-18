6df2f8e09b23514d34d25ecac9a6cff67fddfbb6
Boyuan Feng <boyuan@meta.com>
2024-04-18 16:13:27 +0000

    [Export] Add runtime assert to non-strict export (#123681)
    This PR moves insert_deferred_runtime_asserts from dynamo to torch.fx.passes and uses it to add runtime assertion for non-strict export.
    
    Differential Revision: D55944267
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123681
    Approved by: https://github.com/tugsbayasgalan, https://github.com/angelayi

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -36,6 +36,7 @@ from torch._guards import detect_fake_mode
 from torch._subclasses.fake_tensor import FakeTensor, FakeTensorMode
 from torch._utils_internal import log_export_usage
 from torch.export.exported_program import OutputKind
+from torch.fx._utils import first_call_function_nn_module_stack
 from torch.fx.experimental.symbolic_shapes import (
     ConstraintViolationError,
     free_unbacked_symbols,
@@ -43,6 +44,7 @@ from torch.fx.experimental.symbolic_shapes import (
     ShapeEnv,
 )
 from torch.fx.graph import _PyTreeCodeGen, _PyTreeInfo
+from torch.fx.passes.runtime_assert import insert_deferred_runtime_asserts
 from torch.utils._pytree import TreeSpec
 from torch.utils._sympy.value_ranges import ValueRangeError
 
@@ -1061,6 +1063,12 @@ def _export(
             ),
             example_inputs=(args, kwargs),
             constants=ep_non_strict.constants,
+        )
+        insert_deferred_runtime_asserts(
+            exported_program.graph_module,
+            fake_mode.shape_env,
+            f"non strict exported program: {first_call_function_nn_module_stack(exported_program.graph)}",
+            export=True,
         )
         return exported_program
 
