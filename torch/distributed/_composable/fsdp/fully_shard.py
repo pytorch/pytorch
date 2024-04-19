1900f79b728ea94ff34a7743d92f400253983682
Andrew Gu <andgu@fb.com>
2024-04-19 08:06:40 -0700

    [FSDP2] Added `set_reshard_after_backward` (#124319)
    This PR adds a `set_reshard_after_backward` method to allow disabling resharding after backward. `reshard_after_backward=False` can be used with `reshard_after_forward=False` to implement "ZeRO-1", where there is only all-gather on the first microbatch forward and reduce-scatter on the last microbatch backward.
    
    ```
    for microbatch_idx, microbatch in dataloader:
        is_last_microbatch = microbatch_idx == num_microbatches - 1
        model.set_requires_gradient_sync(is_last_microbatch)
        model.set_reshard_after_backward(is_last_microbatch)
        model.set_is_last_backward(is_last_microbatch)
        microbatch_fwd_bwd(model, microbatch, microbatch_idx)
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124319
    Approved by: https://github.com/weifengpy

--- torch/distributed/_composable/fsdp/fully_shard.py
+++ torch/distributed/_composable/fsdp/fully_shard.py
@@ -220,7 +220,9 @@ class FSDP:
                     fsdp_param_group.reduce_grads = requires_gradient_sync
                     fsdp_param_group.all_reduce_grads = requires_gradient_sync
 
-    def set_requires_all_reduce(self, requires_all_reduce: bool, recurse: bool = True):
+    def set_requires_all_reduce(
+        self, requires_all_reduce: bool, recurse: bool = True
+    ) -> None:
         """
         Sets if the module should all-reduce gradients. This can be used to
         implement gradient accumulation with only reduce-scatter but not
@@ -236,6 +238,28 @@ class FSDP:
                 state = module._get_fsdp_state()
                 if fsdp_param_group := state._fsdp_param_group:
                     fsdp_param_group.all_reduce_grads = requires_all_reduce
+
+    def set_reshard_after_backward(
+        self, reshard_after_backward: bool, recurse: bool = True
+    ) -> None:
+        """
+        Sets if the module should reshard parameters after backward. This can
+        be used during gradient accumulation to trade off higher memory for
+        reduced communication.
+
+        Args:
+            reshard_after_backward (bool): Whether to reshard parameters after
+                backward.
+            recurse (bool): Whether to set for all submodules or just the
+                passed-in module.
+        """
+        self_module = cast(nn.Module, self)
+        modules = list(self_module.modules()) if recurse else [self_module]
+        for module in modules:
+            if isinstance(module, FSDP):
+                state = module._get_fsdp_state()
+                if fsdp_param_group := state._fsdp_param_group:
+                    fsdp_param_group.reshard_after_backward = reshard_after_backward
 
     def _get_fsdp_state(self) -> FSDPState:
         if (state := _get_module_fsdp_state(cast(nn.Module, self))) is None:
