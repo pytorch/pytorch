1ffb6c301a29bef75b4dbb06b35126b65481cfe6
Jason Ansel <jansel@meta.com>
2024-04-20 13:09:32 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/runtime/__init__.py
+++ torch/_inductor/runtime/__init__.py
@@ -1,13 +1,6 @@
-from . import triton_heuristics
+from . import triton_helpers, triton_heuristics
+from .triton_helpers import libdevice, math as tl_math
 from .triton_heuristics import AutotuneHint
-
-try:
-    from . import triton_helpers
-    from .triton_helpers import libdevice, math as tl_math
-except ImportError:
-    triton_helpers = None  # type: ignore[assignment]
-    libdevice = None
-    tl_math = None
 
 
 __all__ = [
