3678af8cd6eb7b943a067c40a122a9296ecf5b44
Kai Londenberg <klondenberg@meta.com>
2024-04-21 14:20:25 +0200

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/device_interface.py
+++ torch/_dynamo/device_interface.py
@@ -39,7 +39,7 @@ class DeviceInterface(metaclass=DeviceInterfaceMeta):
 
     class device:
         def __new__(cls, device: _device_t):
-            raise NotImplementedError()
+            raise NotImplementedError
 
     class Worker:
         """
@@ -51,71 +51,71 @@ class DeviceInterface(metaclass=DeviceInterfaceMeta):
 
         @staticmethod
         def set_device(device: int):
-            raise NotImplementedError()
+            raise NotImplementedError
 
         @staticmethod
         def current_device() -> int:
-            raise NotImplementedError()
+            raise NotImplementedError
 
         @staticmethod
         def get_device_properties(device: _device_t = None):
-            raise NotImplementedError()
+            raise NotImplementedError
 
     @staticmethod
     def current_device():
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def set_device(device: _device_t):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def maybe_exchange_device(device: int) -> int:
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def exchange_device(device: int) -> int:
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def device_count():
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def is_available() -> bool:
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def stream(stream: torch.Stream):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def current_stream():
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def set_stream(stream: torch.Stream):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def _set_stream_by_id(stream_id: int, device_index: int, device_type: int):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def get_raw_stream():
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def synchronize(device: _device_t = None):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def get_device_properties(device: _device_t = None):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @staticmethod
     def get_compute_capability(device: _device_t = None):
-        raise NotImplementedError()
+        raise NotImplementedError
 
 
 class DeviceGuard:
