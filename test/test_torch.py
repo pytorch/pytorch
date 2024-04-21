5a1216bb2e85119118aa0f88f60da7de7d446f65
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-04-21 14:06:20 +0000

    [BE]: Update ruff to 0.4.1 (#124549)
    Update ruff to 0.4.1 .
    This version fixes a lot false negatives/false positives, is 20-40% faster, and has various other bug fixes.
    
    Below is a before and after table showing the execution time of ruff lint and ruff format in milliseconds courtesy of https://astral.sh/blog/ruff-v0.4.0
    
    | Repository                                         | Linter (v0.3) | Linter (v0.4) | Formatter (v0.3) | Formatter (v0.4) |
    |----------------------------------------------------|---------------|---------------|------------------|------------------|
    | [pytorch/pytorch](https://github.com/pytorch/pytorch) | 328.7         | 251.8         | 351.1            | 274.9            |
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124549
    Approved by: https://github.com/ezyang

--- test/test_torch.py
+++ test/test_torch.py
@@ -9126,8 +9126,8 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
         for seed, expected_initial_seed in test_cases:
             torch.manual_seed(seed)
             actual_initial_seed = torch.initial_seed()
-            msg = "expected initial_seed() = {:x} after calling manual_seed({:x}), but got {:x} instead".format(
-                expected_initial_seed, seed, actual_initial_seed)
+            msg = (f"expected initial_seed() = {expected_initial_seed:x} "
+                   f"after calling manual_seed({seed:x}), but got {actual_initial_seed:x} instead")
             self.assertEqual(expected_initial_seed, actual_initial_seed, msg=msg)
         for invalid_seed in [min_int64 - 1, max_uint64 + 1]:
             with self.assertRaisesRegex(RuntimeError, r'Overflow when unpacking long'):
