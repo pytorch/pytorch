a2b24a0e59f522e30fc33f171c0333486bc41bb9
dminor <dminor@nvidia.com>
2024-04-22 12:13:02 -0500

    Workaround for JIT tracing error in layer_norm issue 120822
    This was a workaround for the error message I reported in the following issue when differentiating through a layer norm during a trace:
    
    https://github.com/pytorch/pytorch/issues/120822
    
    Obviously completely dumb that I just commented it out but I was just brute forcing this branch to get it to work and I could get a bit further by doing this.
    
    This change ceased to matter after I overrode the torch.layer_norm() function in python but I've left it in here anyway for the sake of documentation.

--- torch/csrc/jit/frontend/tracer.h
+++ torch/csrc/jit/frontend/tracer.h
@@ -364,8 +364,8 @@ void addInputs(
 
 template <size_t N>
 void addInputs(Node* n, const char* name, std::array<bool, N> value) {
-  throw std::runtime_error(
-      "Found an unsupported argument type in the JIT tracer. File a bug report.");
+  //throw std::runtime_error(
+  //    "Found an unsupported argument type in the JIT tracer. File a bug report.");
 }
 
 TORCH_API void addInputs(
