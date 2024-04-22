186e58e4cf307ff11a440298a958b43075b0d4f4
Peter Bell <peterbell10@live.co.uk>
2024-04-22 19:03:49 +0100

    Update
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/joint_graph.py
+++ torch/_inductor/fx_passes/joint_graph.py
@@ -480,6 +480,7 @@ def div_softmax_pattern(match: Match, *, inp, other, dim, keepdim, dtype=None):
         if dtype is not None:
             inp = inp.to(dtype)
 
+        sign: Union[int, float, torch.Tensor]
         if isinstance(other, (int, float)):
             sign = 1 if other >= 0 else -1
         else:
