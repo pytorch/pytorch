a076a4a6f935c712dbd7a8fbd26c10355e63ad99
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-04-22 23:29:04 +0000

    unhardcode layout check

--- torch/_inductor/codegen/cuda/gemm_template.py
+++ torch/_inductor/codegen/cuda/gemm_template.py
@@ -890,21 +890,35 @@ class CKGemmTemplate(CKTemplate):
 
     def filter_op(self, op: CKGemmOperation) -> Optional[CKGemmOperation]:
         # TBD return None if alignment or layout or dtype is invalid
-        if op.a_layout != "Row" or op.b_layout != "Row" or op.c_layout != "Row":
-            return None
+        def torch_layout_to_ck_layout(torch_layout):
+            if torch_layout.stride[-1] == 1:
+                return "Row"
+            elif torch_layout.stride[-2] == 1:
+                return "Col"
+            else:
+                return None
 
-        X_meta, W_meta = map(lambda T: T.get_layout(), self.input_nodes)
-        Y_meta = self.output_node.get_layout()
-        M = X_meta.size[-2]
-        K = X_meta.size[-1]
-        N = W_meta.size[-1]
-
+        X_meta, W_meta, Y_meta = map(lambda T: T.get_layout(), [*self.input_nodes, self.output_node])
+        # disable the instance if dtypes don't match
         if op.a_element_dtype != self._TORCH_DTYPE_TO_CK[X_meta.dtype]:
             return None
         if op.b_element_dtype != self._TORCH_DTYPE_TO_CK[W_meta.dtype]:
             return None
         if op.c_element_dtype != self._TORCH_DTYPE_TO_CK[Y_meta.dtype]:
             return None
+        # disable the instance if layouts don't match
+        if op.a_layout != torch_layout_to_ck_layout(X_meta):
+            return None
+        if op.b_layout != torch_layout_to_ck_layout(W_meta):
+            return None
+        if op.c_layout != torch_layout_to_ck_layout(Y_meta):
+            return None
+        # try to avoid launching the gemm instance with invalid problem size
+
+        M = X_meta.size[-2]
+        K = X_meta.size[-1]
+        N = W_meta.size[-1]
+
         if M % op.m_per_block != 0:
             return None
         if N % op.n_per_block != 0:
