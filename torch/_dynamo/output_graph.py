8ad19e8007948f01cfe05a9c81eef8aa3a695f1e
Animesh Jain <anijain@umich.edu>
2024-04-23 22:49:28 -0700

    [dynamo][inline inbuilt nn modules] Do not inline for export
    [ghstack-poisoned]

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -732,7 +732,7 @@ class OutputGraph:
         *names,
         **options,
     ):
-        if is_dynamic_nn_module(target):
+        if is_dynamic_nn_module(target, self.root_tx.export):
             return variables.UnspecializedNNModuleVariable(target, **options)
 
         options = dict(options)
