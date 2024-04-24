efe62b3bf46fea8b2cdbd9525e6f6489bf3259a3
Feng Yuan <feng1.yuan@intel.com>
2024-04-25 02:28:54 +0800

    Update base for Update on "Update torch-xpu-ops pin (ATen XPU implementation)"
    Signed-off-by: Feng Yuan <feng1.yuanintel.com>
    
    cc frank-wei jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    [ghstack-poisoned]

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -283,11 +283,6 @@ test_python() {
   assert_git_not_dirty
 }
 
-test_python_xpu() {
-  # shellcheck disable=SC2086
-  PYTORCH_ENABLE_XPU_FALLBACK=1 time python test/run_test.py --exclude-jit-executor --exclude-distributed-tests $INCLUDE_CLAUSE --verbose $PYTHON_TEST_EXTRA_OPTION
-  assert_git_not_dirty
-}
 
 test_dynamo_shard() {
   if [[ -z "$NUM_TEST_SHARDS" ]]; then
@@ -1274,7 +1269,8 @@ elif [[ "${BUILD_ENVIRONMENT}" == *rocm* && -n "$TESTS_TO_INCLUDE" ]]; then
   test_aten
 elif [[ "${BUILD_ENVIRONMENT}" == *xpu* ]]; then
   install_torchvision
-  test_python_xpu
+  export PYTORCH_ENABLE_XPU_FALLBACK=1
+  test_python
   test_aten
   test_xpu_bin
 else
