f7ff7dc9dfa8c5a83eabe95234d121d882a41b02
chilli <chilli@meta.com>
2024-04-24 22:48:37 -0700

    Make configs not error upon failure
    [ghstack-poisoned]

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -199,12 +199,6 @@ class CachingAutotuner(KernelInterface):
                         c, warm_cache_only
                     )
                 except OutOfResources as e:
-                    if len(self.configs) == 1:
-                        raise RuntimeError(
-                            f"Failed to compile triton config: {c}. "
-                            f"Report a fatal compilation error. "
-                            f"{e}"
-                        )
                     # Skip the config if we run out of resource
                     continue
                 self.launchers.append(launcher)
