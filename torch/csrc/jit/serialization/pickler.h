8fc399439b75b6798fc75c0cc24fc2284e4bfc1a
FFFrog <ljw1101.vip@gmail.com>
2024-04-26 13:07:39 +0800

    Update on "Fix & optimze open device registration test."
    1. Fix the wrong tests about lazy init for PrivateUse1 named foo
    2. Refactor the tests and make it more flexible
    3. Disable the two tests temporarily and will fix it later
         - test_open_device_faketensor
         - test_open_device_scalar_type_fallback
         - test_compile_autograd_function_aliasing
    
    [ghstack-poisoned]

--- torch/csrc/jit/serialization/pickler.h
+++ torch/csrc/jit/serialization/pickler.h
@@ -302,27 +302,18 @@ bool checkHasValidSetGetState(const std::shared_ptr<c10::ClassType>& cls);
 using BackendMetaPtr = std::function<
     void(const at::Tensor&, std::unordered_map<std::string, bool>&)>;
 
+// Dynamically obtain serialization function pairs
+// that require the corresponding backend.
+TORCH_API std::array<
+    c10::optional<std::pair<BackendMetaPtr, BackendMetaPtr>>,
+    at::COMPILE_TIME_MAX_DEVICE_TYPES>&
+GetBackendMetaSerialization();
+
 // A allowlist of device type, currently available is PrivateUse1
 inline std::unordered_set<c10::DeviceType>& GetBackendMetaAllowlist() {
   static std::unordered_set<c10::DeviceType> DeviceTypeAllowlist{
       c10::DeviceType::PrivateUse1};
   return DeviceTypeAllowlist;
-}
-
-// Dynamically obtain serialization function pairs
-// that require the corresponding backend.
-inline std::array<
-    c10::optional<std::pair<BackendMetaPtr, BackendMetaPtr>>,
-    at::COMPILE_TIME_MAX_DEVICE_TYPES>&
-GetBackendMetaSerialization() {
-  // The array to save function pointer for BackendMeta serialization.
-  // key is the DeviceType, value is std::pair obj.
-  // value.first represent get function and value.seconde represent set function
-  static std::array<
-      c10::optional<std::pair<BackendMetaPtr, BackendMetaPtr>>,
-      at::COMPILE_TIME_MAX_DEVICE_TYPES>
-      BackendMetaSerialization;
-  return BackendMetaSerialization;
 }
 
 // Register function pointer of Tensor BackendMetadata for serialization.
