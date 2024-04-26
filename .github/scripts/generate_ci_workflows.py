f18024ae63823fcdcb5f5d1500e04a7b72110e69
PaliC <sahancpal@gmail.com>
2024-04-26 12:55:03 -0700

    [BE] Make macos build libtorch with wheel

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -276,22 +276,6 @@ WINDOWS_BINARY_SMOKE_WORKFLOWS = [
 MACOS_BINARY_BUILD_WORKFLOWS = [
     BinaryBuildWorkflow(
         os=OperatingSystem.MACOS_ARM64,
-        package_type="libtorch",
-        abi_version=generate_binary_build_matrix.CXX11_ABI,
-        build_configs=generate_binary_build_matrix.generate_libtorch_matrix(
-            OperatingSystem.MACOS,
-            generate_binary_build_matrix.CXX11_ABI,
-            libtorch_variants=["shared-with-deps"],
-        ),
-        cross_compile_arm64=False,
-        macos_runner="macos-13-xlarge",
-        ciflow_config=CIFlowConfig(
-            labels={LABEL_CIFLOW_BINARIES, LABEL_CIFLOW_BINARIES_LIBTORCH},
-            isolated_workflow=True,
-        ),
-    ),
-    BinaryBuildWorkflow(
-        os=OperatingSystem.MACOS_ARM64,
         package_type="wheel",
         build_configs=generate_binary_build_matrix.generate_wheels_matrix(
             OperatingSystem.MACOS_ARM64
