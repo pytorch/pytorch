e96306fc79111562de1da1c1b1db4ff48663d5a2
Isuru Fernando <ifernando@quansight.com>
2024-04-28 15:40:19 +0000

    Update
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -10216,7 +10216,10 @@ if HAS_GPU and RUN_GPU and not TEST_WITH_ASAN:
             fn_opt = torch.compile(fn)
             code = run_and_get_triton_code(fn_opt, x, 8)
             # load should be masked
-            self.assertTrue("tl.load(in_ptr0 + (tmp0), xmask" in code)
+            self.assertTrue(
+                "tl.load(in_ptr0 + (tmp0), xmask" in code
+                or "tl.load(in_ptr0 + (tmp0), (xmask).to(tl.int1)" in code
+            )
             self.assertEqual(fn(x, 8), fn_opt(x, 8))
 
         def test_kernel_names_descriptive(self):
