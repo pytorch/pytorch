96cf2ec3fc000e3f8d037b91065a8f75357b1487
Nikita Shulga <nikita.shulga@gmail.com>
2024-04-28 13:14:32 -0700

    [MPS] And naive int8 and int4 Linear

---
+++ aten/src/ATen/native/mps/operations/Quantized.mm
@@ -0,0 +1,102 @@
+#define TORCH_ASSERT_ONLY_METHOD_OPERATORS
+
+#ifndef AT_PER_OPERATOR_HEADERS
+#include <ATen/Functions.h>
+#include <ATen/NativeFunctions.h>
+#else
+#include <ATen/ops/_weight_int4pack_mm_native.h>
+#include <ATen/ops/_weight_int8pack_mm_native.h>
+#include <ATen/ops/empty.h>
+#endif
+#include <ATen/native/mps/OperationUtils.h>
+
+namespace at::native {
+
+using namespace mps;
+
+Tensor _weight_int4pack_mm_mps(const Tensor& A, const Tensor& B, int64_t qGroupSize, const Tensor& qScaleAndZeros) {
+  constexpr int64_t kNTileSize = 8;
+
+  auto M = A.size(0);
+  auto N = B.size(0) * kNTileSize;
+  auto K = A.size(1);
+
+  TORCH_CHECK(A.dtype() == kBFloat16 || A.dtype() == kHalf || A.dtype() == kFloat,
+              __func__,
+              " : expect A to be either 32-bit or 16-bit float tensor.");
+  TORCH_CHECK(A.is_contiguous(), __func__, " : expect A to be contiguous.");
+  TORCH_CHECK(A.dim() == 2, __func__, " : expect A to be 2D tensor.");
+
+  TORCH_CHECK(B.dtype() == kInt, __func__, " : expect B to be int32 tensor.");
+  TORCH_CHECK(B.is_contiguous(), __func__, " : expect B to be contiguous.");
+  TORCH_CHECK(B.dim() == 4, __func__, " : expect B to 4d tensor.");
+
+  TORCH_CHECK(qGroupSize == 32 || qGroupSize == 64 || qGroupSize == 128 || qGroupSize == 256,
+              __func__,
+              ": expect qGroupSize to be 32, 64, 128 or 256, got ",
+              qGroupSize);
+
+  TORCH_CHECK(qScaleAndZeros.dim() == 3 && qScaleAndZeros.size(1) == N && qScaleAndZeros.size(2) == 2,
+              __func__,
+              ": expect qScaleAndZeros to be 3d tensor with sizes [:, ",
+              N,
+              ", 2]");
+
+  auto C = at::empty({M, N}, A.options());
+
+  return C;
+}
+
+Tensor _weight_int8pack_mm_mps(const Tensor& A, const Tensor& B, const Tensor& scales) {
+  auto M = A.size(0);
+  auto N = B.size(0);
+  auto K = A.size(1);
+
+  TORCH_CHECK(A.dtype() == kBFloat16 || A.dtype() == kHalf || A.dtype() == kFloat,
+              __func__,
+              " : expect A to be either 32-bit or 16-bit float tensor.");
+  TORCH_CHECK(A.is_contiguous(), __func__, " : expect A to be contiguous.");
+  TORCH_CHECK(A.dim() == 2, __func__, " : expect A to be 2D tensor.");
+
+  TORCH_CHECK(B.dtype() == kChar, __func__, " : expect B to be int8 tensor.");
+  TORCH_CHECK(B.is_contiguous(), __func__, " : expect B to be contiguous.");
+  TORCH_CHECK(B.size(1) == K, __func__, " : expect B.size(1) == ", K);
+
+  TORCH_CHECK(scales.dim() == 1 && scales.size(0) == N, __func__, " : expect scales to be 1d tensor with size ", N);
+
+  auto C = at::empty({M, N}, A.options());
+
+  struct CachedGraph : public MPSCachedGraph {
+    CachedGraph(MPSGraph* graph) : MPSCachedGraph(graph) {}
+    MPSGraphTensor *ATensor = nil, *BTensor = nil, *scalesTensor = nil;
+    MPSGraphTensor* outputTensor = nil;
+  };
+  @autoreleasepool {
+    std::string key = __func__ + getTensorsStringKey({A, B, scales});
+    auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
+      newCachedGraph->ATensor = mpsGraphRankedPlaceHolder(mpsGraph, A);
+      newCachedGraph->BTensor = mpsGraphRankedPlaceHolder(mpsGraph, B);
+      newCachedGraph->scalesTensor = mpsGraphRankedPlaceHolder(mpsGraph, scales);
+      auto castB = castMPSTensor(mpsGraph, newCachedGraph->BTensor, getMPSScalarType(A));
+      auto transposedB = [mpsGraph transposeTensor:castB dimension:-1 withDimension:-2 name:nil];
+      auto mmTensor = [mpsGraph matrixMultiplicationWithPrimaryTensor:newCachedGraph->ATensor
+                                                      secondaryTensor:transposedB
+                                                                 name:nil];
+      newCachedGraph->outputTensor = [mpsGraph multiplicationWithPrimaryTensor:mmTensor
+                                                               secondaryTensor:newCachedGraph->scalesTensor
+                                                                          name:nil];
+    });
+    auto APlaceholder = Placeholder(cachedGraph->ATensor, A);
+    auto BPlaceholder = Placeholder(cachedGraph->BTensor, B);
+    auto scalesPlaceholder = Placeholder(cachedGraph->scalesTensor, scales);
+    auto outputPlaceholder = Placeholder(cachedGraph->outputTensor, C);
+    runMPSGraph(getCurrentMPSStream(),
+                cachedGraph->graph(),
+                dictionaryFromPlaceholders(APlaceholder, BPlaceholder, scalesPlaceholder),
+                outputPlaceholder);
+  }
+
+  return C;
+}
+
+} // namespace at::native
