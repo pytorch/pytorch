37ed3415175a34c9027f458e423e521a65a099b9
Jeff Daily <jeff.daily@amd.com>
2024-04-29 22:51:09 +0000

    use 8-4-4-4-16 uuid for __repr__

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -275,7 +275,9 @@ class TestCuda(TestCase):
         self.assertTrue((tensor == 1).all())
 
     def test_uuid(self):
-        assert len(torch.cuda.get_device_properties(0).uuid.bytes) == 16
+        uuid = torch.cuda.get_device_properties(0).uuid
+        assert len(str(uuid)) == 36  # xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        assert len(uuid.bytes) == 16
 
     def test_copy_non_blocking(self):
         def _test_copy_non_blocking(a, b):
