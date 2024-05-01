81bad9f65c4280c67d7d93fd073d2110c44282a3
lsakka <lsakka@devvm4561.ash0.facebook.com>
2024-05-01 16:20:20 -0700

    test new faluires
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -345,7 +345,7 @@ enable_cpp_guard_manager = os.environ.get("TORCHDYNAMO_CPP_GUARD_MANAGER", "1") 
 
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = (
-    os.environ.get("TORCHDYNAMO_INLINE_INBUILT_NN_MODULES", "0") == "1"
+    os.environ.get("TORCHDYNAMO_INLINE_INBUILT_NN_MODULES", "1") == "1"
 )
 
 
@@ -440,8 +440,7 @@ _experimental_support_context_fn_in_torch_utils_checkpoint = False
 if TYPE_CHECKING:
     from torch.utils._config_typing import *  # noqa: F401, F403
 
-    def _make_closure_patcher(**changes):
-        ...
+    def _make_closure_patcher(**changes): ...
 
 
 from torch.utils._config_module import install_config_module
