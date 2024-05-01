7663166ff247e11d0ed439d4071ddb40932199f3
Chien-Chin Huang <chienchin@fb.com>
2024-05-01 14:11:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -3541,3 +3541,11 @@ def lookup_inner(
         return SkipFunctionVariable
     else:
         return UserFunctionVariable
+
+
+def clear_lru_cache():
+    torch._dynamo.trace_rules.get_torch_obj_rule_map.cache_clear()
+    torch._dynamo.trace_rules.get_tensor_method.cache_clear()
+    torch._dynamo.trace_rules.get_legacy_mod_inlinelist.cache_clear()
+    torch._dynamo.trace_rules.get_mod_inlinelist.cache_clear()
+    torch._dynamo.trace_rules.dynamo_dir.cache_clear()
