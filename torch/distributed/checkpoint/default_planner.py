f1d9763a7386aa46efd668ad14a0c87047a37874
Chien-Chin Huang <chienchin@fb.com>
2024-05-01 14:11:22 -0700

    Update
    [ghstack-poisoned]

--- torch/distributed/checkpoint/default_planner.py
+++ torch/distributed/checkpoint/default_planner.py
@@ -338,7 +338,10 @@ def create_default_local_save_plan(
         if isinstance(obj, DTensor):
             if obj.device_mesh.get_coordinate() is not None:
                 requests += _create_write_items(fqn, obj)
-        elif isinstance(obj, (torch.Tensor)) or is_coordinator:
+        else:
+            # For the plain tensor and non-tensor values, add the request for all
+            # the ranks. Coordinator will decides whether to deduplicate the
+            # values based on the keys.
             requests += _create_write_items(fqn, obj)
 
     return SavePlan(requests)
