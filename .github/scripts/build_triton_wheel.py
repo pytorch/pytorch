e2678b544a00ba05f4731a5a7c60ab28d57d24e0
Pruthvi Madugundu <pruthvigithub@gmail.com>
2024-04-30 21:12:35 -0700

    Parameterize the triton build dir
    - Removes hard coding and helps in internal builds

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -156,11 +156,8 @@ def build_triton(
         shutil.copy(whl_path, Path.cwd())
 
         if build_rocm:
-            check_call(
-                [f"{SCRIPT_DIR}/amd/patch_triton_wheel.sh"],
-                cwd=triton_basedir,
-                shell=True,
-            )
+            check_call([f"{SCRIPT_DIR}/amd/patch_triton_wheel.sh", Path.cwd()], cwd=triton_basedir)
+
         return Path.cwd() / whl_path.name
 
 
