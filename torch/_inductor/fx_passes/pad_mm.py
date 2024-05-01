ce334fe472ee0b9a2c05d8fd4e4913695a2c05b2
eellison <elias.ellison@gmail.com>
2024-04-30 17:12:23 -0700

    [Testing] pad bandwidth bound mms
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/pad_mm.py
+++ torch/_inductor/fx_passes/pad_mm.py
@@ -178,30 +178,6 @@ def pad_addmm(
         ]
 
 
-def is_mm_compute_bound(M: int, K: int, N: int, dtype: torch.dtype) -> bool:
-    denominator = M * K + N * K + M * N
-    if denominator == 0:
-        return False
-    arithmetic_intensity = (M * N * K) / denominator
-
-    # Fails with AMD
-    try:
-        machine_balance = (
-            1000 * utils.get_device_tflops(dtype)
-        ) / utils.get_gpu_dram_gbps()
-    except Exception:
-        return True
-
-    # dram_gbps might be underestimating bandwidth because of cache.
-    # if we estimate machine balance too low we might miss some speedups,
-    # if we extimate too high there will be unnecessary compilation time increase.
-    # TODO - finetune coefficient here. As a reference point, Triton mm model assumes
-    # 80% of reads are in cache and cache is 4x faster than dram_gbps
-    machine_balance = machine_balance * 0.5
-
-    return arithmetic_intensity > machine_balance
-
-
 @functools.lru_cache(None)
 def get_pad_cache():
     return torch._inductor.codecache.LocalCache()
@@ -267,9 +243,6 @@ def should_pad_bench(
             return False
 
         if m_padded_length == k_padded_length == n_padded_length == 0:
-            return False
-
-        if not is_mm_compute_bound(m, k, n, mat1.dtype):
             return False
 
         # We don't want to look up the cache for cases that are trivially false
