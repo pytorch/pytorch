150b528965c1b1e06533803086b3992ecaca10ec
soulitzer <soulitzer@gmail.com>
2024-05-02 01:12:26 -0400

    Support custom layout call under torch dispatch mode

--- torch/csrc/PyInterpreter.cpp
+++ torch/csrc/PyInterpreter.cpp
@@ -831,12 +831,16 @@ c10::Layout ConcretePyInterpreterVTable::layout(
       "torch.ops.prim");
 
   TORCH_CHECK(
-      THPLayout_Check(out.ptr()),
+      THPLayout_Check(out.ptr()) || PyLong_Check(out.ptr()),
       "layout returned invalid type ",
       py::detail::get_fully_qualified_tp_name(Py_TYPE(out.ptr())),
       ", expected Layout");
 
-  return toLayout(out.ptr());
+  if (THPLayout_Check(out.ptr())) {
+    return toLayout(out.ptr());
+  } else {
+    return c10::Layout(py::cast<int64_t>(out));
+  }
 }
 
 int64_t ConcretePyInterpreterVTable::numel(const c10::TensorImpl* self) const {
