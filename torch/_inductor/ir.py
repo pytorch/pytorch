pytorch/pytorch

commit 218ea32601e02b693a7bd53b8d189cf2c8725fc9

[AOTI] Generate mul_Scalar instead of mul_Tensor (#125397)
Summary: Fix https://github.com/pytorch/pytorch/issues/117365. When the second argument to aten.mul.Tensor is a scalar (e.g. scale factor), the cpp wrapper expects to generate a call to mul_Scalar when fallback happens (e.g. Complex dtype).

Pull Request resolved: https://github.com/pytorch/pytorch/pull/125397
Approved by: https://github.com/chenyang78
ghstack dependencies: #125329

torch/_inductor/ir.py
@@ -5522,6 +5522,15 @@ class FallbackKernel(ExternKernelAlloc):
         if kernel.namespace == "aten":  # type: ignore[union-attr]
             # Aten Fallback Ops
             assert isinstance(kernel, torch._ops.OpOverload)
+
+            if (
+                kernel == aten.mul.Tensor
+                and len(self.inputs) == 1
+                and len(self.constant_args) == 1
+            ):
+                # When aten.mul.Tensor's second arg is constant, cpp wrapper expects to call mul_Scalar
+                kernel = aten.mul.Scalar
+
             if V.graph.cpp_wrapper:
                 if (
                     config.is_fbcode()

test/inductor/test_cpu_cpp_wrapper.py
@@ -71,6 +71,7 @@ test_failures_cpp_wrapper = {
 
 if config.abi_compatible:
     xfail_list = [
+        "test_add_complex_cpu",
         "test_bernoulli1_cpu",  # cpp fallback op naming issue
         "test_conv2d_binary_inplace_fusion_failed_cpu",
         "test_conv2d_binary_inplace_fusion_pass_cpu",
@@ -173,6 +174,7 @@ if RUN_CPU:
         code_string_count: dict = {}
 
     for item in [
+        BaseTest("test_add_complex"),
         BaseTest("test_add_complex4"),
         BaseTest("test_as_strided"),  # buffer reuse
         BaseTest("test_bernoulli1"),

test/inductor/test_cuda_cpp_wrapper.py
@@ -97,6 +97,7 @@ if TEST_WITH_ROCM:
 
 if config.abi_compatible:
     xfail_list = [
+        "test_add_complex_cuda",
         "test_bernoulli1_cuda",  # cpp fallback op naming issue
         "test_conv_backward_cuda",
         "test_profiler_mark_wrapper_call_cuda",
@@ -183,6 +184,7 @@ if RUN_CUDA:
 
     # Maintain two separate test lists for cuda and cpp for now
     for item in [
+        BaseTest("test_add_complex"),
         BaseTest("test_add_complex4"),
         BaseTest("test_as_strided"),  # buffer reuse
         BaseTest("test_batch_norm_2d_2"),

commit 6135e4afa6b7de2d3a2cf94cbfdd0044371e5655

[AOTI] Add a missing mypy ignore
Summary: Caused by https://github.com/pytorch/pytorch/pull/125397, but somehow was not caught by CI.

[ghstack-poisoned]

torch/_inductor/ir.py
@@ -5547,7 +5547,7 @@ class FallbackKernel(ExternKernelAlloc):
                     self.set_cpp_kernel(kernel)
                 else:
                     self.cpp_kernel_name = get_aten_cpp_kernel_name(kernel)
-                    schema = kernel._schema
+                    schema = kernel._schema  # type: ignore[union-attr]
                     self.init_args_default_value(schema)
             else:
                 self.python_kernel_name = str(kernel)