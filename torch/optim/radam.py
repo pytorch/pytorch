7ebdfdf826b15297962a874b42d5baae5f557456
David Chiu <david20571015@gmail.com>
2024-05-03 09:34:37 +0800

    typing: radam

--- torch/optim/radam.py
+++ torch/optim/radam.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import cast, List, Optional, Tuple, Union
 
 import torch
 from torch import Tensor
@@ -15,6 +15,7 @@ from .optimizer import (
     _use_grad_for_differentiable,
     _view_as_real,
     Optimizer,
+    ParamsT,
 )
 
 __all__ = ["RAdam", "radam"]
@@ -23,9 +24,9 @@ __all__ = ["RAdam", "radam"]
 class RAdam(Optimizer):
     def __init__(
         self,
-        params,
+        params: ParamsT,
         lr=1e-3,
-        betas=(0.9, 0.999),
+        betas: Tuple[float, float] = (0.9, 0.999),
         eps=1e-8,
         weight_decay=0,
         decoupled_weight_decay: bool = False,
@@ -127,12 +128,12 @@ class RAdam(Optimizer):
                 loss = closure()
 
         for group in self.param_groups:
-            params_with_grad = []
-            grads = []
-            exp_avgs = []
-            exp_avg_sqs = []
-            state_steps = []
-            beta1, beta2 = group["betas"]
+            params_with_grad: List[Tensor] = []
+            grads: List[Tensor] = []
+            exp_avgs: List[Tensor] = []
+            exp_avg_sqs: List[Tensor] = []
+            state_steps: List[Tensor] = []
+            beta1, beta2 = cast(Tuple[float, float], group["betas"])
 
             has_complex = self._init_group(
                 group, params_with_grad, grads, exp_avgs, exp_avg_sqs, state_steps
@@ -247,8 +248,8 @@ def _single_tensor_radam(
     lr: float,
     weight_decay: float,
     eps: float,
-    differentiable: bool,
     decoupled_weight_decay: bool,
+    differentiable: bool,
     capturable: bool,
     has_complex: bool,
 ):
@@ -261,7 +262,7 @@ def _single_tensor_radam(
         # If compiling, the compiler will handle cudagraph checks, see note [torch.compile x capturable]
         if not torch._utils.is_compiling() and capturable:
             assert (param.is_cuda and step_t.is_cuda) or (
-                param.is_xla and step_t.is_xla
+                param.is_xla and step_t.is_xla  # type: ignore[attr-defined]
             ), "If capturable=True, params and state_steps must be CUDA or XLA tensors."
 
         if torch.is_complex(param):
@@ -388,6 +389,9 @@ def _multi_tensor_radam(
         # maximum length of the approximated SMA
         rho_inf = 2 / (1 - beta2) - 1
         # compute the length of the approximated SMA
+        bias_correction1: Union[Tuple[Tensor, ...], List[Tensor]]
+        bias_correction2: Union[Tuple[Tensor, ...], List[Tensor]]
+        rho_t_list: Union[Tuple[Tensor, ...], List[Tensor]]
         if capturable:
             bias_correction1 = torch._foreach_pow(beta2, grouped_state_steps)
             torch._foreach_neg_(bias_correction1)
@@ -413,7 +417,7 @@ def _multi_tensor_radam(
             if decoupled_weight_decay:
                 torch._foreach_mul_(grouped_params, 1 - lr * weight_decay)
             else:
-                grouped_grads = torch._foreach_add(
+                grouped_grads = torch._foreach_add(  # type: ignore[assignment]
                     grouped_grads, grouped_params, alpha=weight_decay
                 )
 
