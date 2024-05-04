802d61f8fba3500699603048d94e62fad231c0e9
Mu-Chu Lee <mlee8@meta.com>
2024-05-04 16:11:21 -0700

    Remove assertion for cat target_func
    Summary:
    We remove the assertion for target_func being cat.
    The reason is that we have multiple flavors of concat, such as
    cat/cat.default/cat_slice/cat_slice_cat/...
    Assertion here is causing multiple times of false positives.
    
    Test Plan: Removing assertion code only.
    
    Differential Revision: D56971387

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3822,12 +3822,6 @@ class ConcatKernel(NopKernel):
                     break
         any_input_is_storage_and_layout = any(is_storage_and_layout(x) for x in inputs)
         fx_node_args = V.graph.current_node.args[0]
-        target_func = (
-            V.graph.current_node.target.func
-            if isinstance(V.graph.current_node.target, functools.partial)
-            else V.graph.current_node.target
-        )
-        assert target_func in [aten.cat, aten.cat.default]
         assert isinstance(fx_node_args, list)
         # If any of the inputs has meta tensor and the meta tensor is in CL format, use CL format for the output
         if any_input_is_storage_and_layout is False and any(
