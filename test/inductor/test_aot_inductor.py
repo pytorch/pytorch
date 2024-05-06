9c63b76140ac0240a8a10ef79bd1ef94ed213ef6
David Berard <dberard@fb.com>
2024-05-06 11:19:49 -0700

    Add an input, use check_model_with_multiple_inputs

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -2702,34 +2702,30 @@ class AOTInductorTestsTemplate:
             torch.tensor([0, 1, 20, 25, 31], device=self.device),
         )
 
-        # different "NT batch size"
+        # same actual amount of data, different "NT batch size"
         example_inputs_3 = (
+            torch.randn((15, 128), device=self.device),
+            torch.tensor([0, 3, 10, 15], device=self.device),
+        )
+
+        # different "NT batch size"
+        example_inputs_4 = (
             torch.randn((37, 128), device=self.device),
             torch.tensor([0, 5, 16, 25, 29, 37], device=self.device),
         )
 
-        with torch.no_grad(), config.patch(
-            {
-                "abi_compatible": self.abi_compatible,
-                "aot_inductor.debug_compile": True,
-            }
-        ):
-            dim0_values = Dim("dim0_values", min=1, max=128)
-            dim0_offsets = Dim("dim0_offsets", min=1, max=9)
-            dynamic_shapes = {"values": {0: dim0_values}, "offsets": {0: dim0_offsets}}
-            so_path: str = AOTIRunnerUtil.compile(
-                model, example_inputs_1, dynamic_shapes=dynamic_shapes
-            )
-        aot_inductor_module = AOTIRunnerUtil.load(self.device, so_path)
+        dim0_values = Dim("dim0_values", min=1, max=128)
+        dim0_offsets = Dim("dim0_offsets", min=1, max=9)
+        dynamic_shapes = {"values": {0: dim0_values}, "offsets": {0: dim0_offsets}}
+        example_inputs_list = [
+            example_inputs_1,
+            example_inputs_2,
+            example_inputs_3,
+            example_inputs_4,
+        ]
 
-        self.assertEqual(
-            aot_inductor_module(*example_inputs_1), model(*example_inputs_1)
-        )
-        self.assertEqual(
-            aot_inductor_module(*example_inputs_2), model(*example_inputs_2)
-        )
-        self.assertEqual(
-            aot_inductor_module(*example_inputs_3), model(*example_inputs_3)
+        self.check_model_with_multiple_inputs(
+            model, example_inputs_list, dynamic_shapes=dynamic_shapes
         )
 
     def test_misc_1(self):
