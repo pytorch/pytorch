e2e90125e1c9311d67bcf09d6eedcfc348a3966c
lezcano <lezcano-93@hotmail.com>
2024-05-06 17:23:29 +0000

    Update on "Implement svd_lowrank and pca_lowrank for complex numbers"
    We fix a number of bugs previously present in the complex
    implementation.
    
    We also heavily simplify the implementation, using, among
    other things, that we now have conjugate views.
    
    I saw there is a comment regarding how slow some checks on this
    function are. As such, I removed quite a few of the combinations of inputs
    to make the OpInfo lighter. I still left a couple relevant examples to not regress
    coverage though.
    
    Fixes https://github.com/pytorch/pytorch/issues/122188
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang
    
    [ghstack-poisoned]

--- torch/_lowrank.py
+++ torch/_lowrank.py
@@ -134,7 +134,7 @@ def _svd_lowrank(
     matmul = _utils.matmul
     if M is not None:
         # add(sparse, dense) is not implemented...
-        if M.is_sparse() and not A.is_sparse():
+        if not A.is_sparse and M.is_sparse :
             A = -M + A
         else:
             A = A - M
