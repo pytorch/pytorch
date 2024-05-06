97a4ff64a622b4d96cab6823bfc49bfc2df36107
Guilherme Leobas <guilhermeleobas@gmail.com>
2024-05-06 17:33:33 -0300

    Update
    [ghstack-poisoned]

--- test/functorch/test_eager_transforms.py
+++ test/functorch/test_eager_transforms.py
@@ -4001,17 +4001,17 @@ class TestExamplesCorrectness(TestCase):
             def __init__(self):
                 super().__init__()
                 self.fc1 = nn.Linear(1, 40)
-                # self.relu1 = nn.ReLU()
-                # self.fc2 = nn.Linear(40, 40)
-                # self.relu2 = nn.ReLU()
-                # self.fc3 = nn.Linear(40, 1)
+                self.relu1 = nn.ReLU()
+                self.fc2 = nn.Linear(40, 40)
+                self.relu2 = nn.ReLU()
+                self.fc3 = nn.Linear(40, 1)
 
             def forward(self, x):
                 x = self.fc1(x)
-                # x = self.relu1(x)
-                # x = self.fc2(x)
-                # x = self.relu2(x)
-                # x = self.fc3(x)
+                x = self.relu1(x)
+                x = self.fc2(x)
+                x = self.relu2(x)
+                x = self.fc3(x)
                 return x
 
         # TODO: should replace with F.mse_loss
