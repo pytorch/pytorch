2f79a1832421c9310ad686d91fdc11830e574008
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-05-07 21:04:49 +0000

    Revert "[inductor] add cpp builder code. (#124045)"
    This reverts commit 7864d287a1e56685aa754285cc2d3c31ff055f62.
    
    Reverted https://github.com/pytorch/pytorch/pull/124045 on behalf of https://github.com/huydhn due to Sorry for reverting your change, but it is failing trunk jobs https://hud.pytorch.org/pytorch/pytorch/commit/7864d287a1e56685aa754285cc2d3c31ff055f62 including lint ([comment](https://github.com/pytorch/pytorch/pull/124045#issuecomment-2099306071))

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -6245,11 +6245,6 @@ class CommonTemplate:
 
         self.common(fn, [torch.randn(64, 64)])
 
-    def test_new_cpp_build_logical(self):
-        from torch._inductor.codecache import validate_new_cpp_commands
-
-        validate_new_cpp_commands()
-
     def test_as_strided(self):
         def fn(x):
             return (
