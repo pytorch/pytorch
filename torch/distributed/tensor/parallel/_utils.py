84366458ff3592c0b62acce4ec92a52225962982
Will Constable <whc@meta.com>
2024-05-08 10:01:15 -0700

    Update
    [ghstack-poisoned]

--- torch/distributed/tensor/parallel/_utils.py
+++ torch/distributed/tensor/parallel/_utils.py
@@ -43,3 +43,12 @@ def _validate_tp_mesh_dim(
     if device_mesh.ndim > 1:
         raise ValueError(f"Tensor Parallel only accepts a 1D DeviceMesh, but found {device_mesh.ndim}D!"
                          "If you have a 2-D or N-D device_mesh, consider passing in device_mesh[\"tp\"]")
+
+    parent_mesh = _mesh_resources.get_parent_mesh(device_mesh)
+    if parent_mesh:
+        tp_mesh_dim_in_parent = _mesh_resources.get_parent_mesh_dim(device_mesh)
+        if tp_mesh_dim_in_parent != parent_mesh.ndim - 1:
+            raise RuntimeError(
+                f"Found TP device_mesh on the {tp_mesh_dim_in_parent} dimension of its parent mesh.",
+                "Currently we only support intranode TP and TP needs to be the innermost dimension on its parent mesh.",
+            )
