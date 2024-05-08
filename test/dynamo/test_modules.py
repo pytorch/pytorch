b091dd72e401c6d2fa82bc884e9d4c0aef5b8b35
--global <manhtai.lmt@gmail.com>
2024-05-09 03:51:02 +0700

    Fix Tensor-likes leak of test_module_order

--- test/dynamo/test_modules.py
+++ test/dynamo/test_modules.py
@@ -1790,6 +1790,9 @@ class OptimizedModuleTest(torch._dynamo.test_case.TestCase):
         self.assertEqual(ref, res)
         self.assertEqual(cnts.frame_count, 1)
 
+        # Ensure that modification in user module causes recompile
+        mod.eval()
+
         mod._modules["linear1"] = mod._modules.pop("linear1")
         ref = fn(torch.ones(1))
         res = opt_fn(torch.ones(1))
