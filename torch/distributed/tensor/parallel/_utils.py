f6c1fc82d341a1949b8eeefddf97dd9d5d926218
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-05-08 13:57:14 -0700

    Update on "[fsdp2] Accomodate FSDP2 to accept parent mesh > 2"
    as titled, to support higher dimension parallelism
    
    cc mrshenli pritamdamania87 zhaojuanmao satgera gqchen aazzolini osalpekar jiayisuse H-Huang kwen2501 awgu penguinwu fegin XilunWu fduwjj wz337 tianyu-l wconstab yf225 chauhang d4l3k
    
    [ghstack-poisoned]

--- torch/distributed/tensor/parallel/_utils.py
+++ torch/distributed/tensor/parallel/_utils.py
@@ -46,14 +46,8 @@ def _validate_tp_mesh_dim(
 
     parent_mesh = _mesh_resources.get_parent_mesh(device_mesh)
     if parent_mesh:
-        if parent_mesh.ndim != 2:
-            raise RuntimeError(
-                f"Found TP device_mesh has a parent mesh with dims {parent_mesh.ndim}",
-                "Currently we only support 2D TP composition with DP.",
-            )
-
         tp_mesh_dim = _mesh_resources.get_parent_mesh_dim(device_mesh)
-        if tp_mesh_dim != 1:
+        if tp_mesh_dim != parent_mesh.ndim - 1:
             raise RuntimeError(
                 f"Found TP device_mesh on the {tp_mesh_dim} dimension of its parent mesh.",
                 "Currently we only support intranode TP and TP needs to be the innermost dimension on its parent mesh.",
