a261e47830ce2c48dd729bc6f047a53979681f0b
Edward Z. Yang <ezyang@meta.com>
2024-05-08 22:01:15 -0700

    Update
    [ghstack-poisoned]

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -166,7 +166,7 @@ class TestDynamismExpression(TestCase):
         res = gm(*inp)
         self.assertTrue(torchdynamo.utils.same(ref, res))
 
-    def test_export_constraints_error(self):
+    def test_export_constraints_error_not_in_range(self):
         class InvalidInputConflictWithInputConstraints(torch.nn.Module):
             def forward(self, x):
                 return x + 1
@@ -180,6 +180,7 @@ class TestDynamismExpression(TestCase):
                 dynamic_shapes={"x": {0: dim_x}},
             )
 
+    def test_export_constraints_error(self):
         class ConflictingConstraints(torch.nn.Module):
             def forward(self, x):
                 b = x.item()
@@ -2382,8 +2383,6 @@ def forward(self, x):
 
         if is_non_strict_test(self._testMethodName):
             error_msg = r"Trying to create tensor with negative dimension -1"
-        elif is_retracebility_test(self._testMethodName):
-            error_msg = r"Runtime assertion failed for _local_scalar_dense_default >= 0"
         else:
             error_msg = "is outside of inline constraint"
         with self.assertRaisesRegex(RuntimeError, error_msg):
