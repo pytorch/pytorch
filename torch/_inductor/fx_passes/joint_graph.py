188b8bc9057686c417ebb5b5747a3c7f62952b57
James Wu <jjwu@meta.com>
2024-05-09 10:09:09 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/joint_graph.py
+++ torch/_inductor/fx_passes/joint_graph.py
@@ -360,5 +360,5 @@ def pointless_view(match: Match, arg, size):
     node = match.output_node()
     arg_size = list(node.args[0].meta["val"].shape)  # type: ignore[union-attr]
     if size == arg_size:
-        node.replace_all_uses_with(node.args[0])  # type: ignore[arg-type]
+        node.replace_all_uses_with(node.args[0])
         match.erase_nodes(graph)
