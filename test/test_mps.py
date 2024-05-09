4e29e80bf09f084b3dd67a5bde231f927b7a012b
Nikita Shulga <nshulga@meta.com>
2024-05-09 13:43:12 +0000

    Run MPS tests on MacOS Sonoma (#125801)
    Those ones are running 14.4.1, so I wonder if they actually pass CI
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125801
    Approved by: https://github.com/kit1980, https://github.com/huydhn

--- test/test_mps.py
+++ test/test_mps.py
@@ -61,6 +61,10 @@ _ref_test_ops = tuple(
     )
 )
 
+def xfailIfMacOS14_4Plus(func):
+    return unittest.expectedFailure(func) if product_version > 14.3 else func  # noqa: F821
+
+
 def mps_ops_grad_modifier(ops):
     XFAILLIST_GRAD = {
 
@@ -11414,6 +11418,8 @@ class TestRNNMPS(TestCaseMPS):
             for test_options in self.LSTM_TEST_CASES:
                 self._lstm_helper(num_layers=num_layers, dtype=dtype, device=device, **test_options)
 
+    # Broke on MacOS-14.4 (but works on 14.2), see https://github.com/pytorch/pytorch/issues/125803
+    @xfailIfMacOS14_4Plus
     def test_lstm_backward(self, device="mps", dtype=torch.float32):
         for num_layers in [1, 2, 5]:
             for test_options in self.LSTM_TEST_CASES:
