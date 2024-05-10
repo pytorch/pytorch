53aa6b33cb5ca98c0bbe4e1f8ab5491d3f6ce359
Catherine Lee <csl@fb.com>
2024-05-10 21:11:37 +0000

    [CI] Relax per proc memory by a little bit, mark a test as serial (#125960)
    test failure is here https://github.com/pytorch/pytorch/actions/runs/9036789873/job/24836020415
    
    * OOMs etc rel to https://github.com/pytorch/pytorch/pull/125598
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125960
    Approved by: https://github.com/huydhn

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1297,7 +1297,7 @@ if TEST_CUDA and 'NUM_PARALLEL_PROCS' in os.environ:
     num_procs = int(os.getenv("NUM_PARALLEL_PROCS", "2"))
     gb_available = torch.cuda.mem_get_info()[1] / 2 ** 30
     # other libraries take up about a little under 1 GB of space per process
-    torch.cuda.set_per_process_memory_fraction(round((gb_available - num_procs * .9) / gb_available / num_procs, 2))
+    torch.cuda.set_per_process_memory_fraction(round((gb_available - num_procs * .85) / gb_available / num_procs, 2))
 
 requires_cuda = unittest.skipUnless(torch.cuda.is_available(), "Requires CUDA")
 
