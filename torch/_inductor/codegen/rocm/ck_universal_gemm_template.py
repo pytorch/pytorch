8d586dd3a247d629e3d2fc4193aeb30083eb406d
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-05-10 23:40:12 +0000

    add class name to version comment

--- torch/_inductor/codegen/rocm/ck_universal_gemm_template.py
+++ torch/_inductor/codegen/rocm/ck_universal_gemm_template.py
@@ -250,9 +250,12 @@ class CKGemmTemplate(CKTemplate):
         Y = self.output_node
         Bias = None  # TBD support gemm_bias
 
-        version_comment = rf"""/** Generated code for CK inductor backend
-{torch.__version__=}
-{torch.version.git_version=}
+        version_comment = rf"""/** 
+* Generated code for CK inductor backend
+* See {type(self).__module__}.{type(self).__qualname__}
+*
+* {torch.__version__=}
+* {torch.version.git_version=}
 */
 """
 
