e78e48829924a25c70486db9dee318e99d2e4c6f
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-05-11 00:26:29 +0000

    cleanup cuda benchmark request

--- torch/_inductor/autotune_process.py
+++ torch/_inductor/autotune_process.py
@@ -743,9 +743,6 @@ class CUDABenchmarkRequest(GPUDeviceBenchmarkRequest):
         )
 
     def update_workspace_size(self) -> None:
-        if torch.version.hip:
-            # TODO: fix memory access errors on ROCm
-            return
         if self._workspace_size_updated:
             return
         self.ensure_dll_loaded()
