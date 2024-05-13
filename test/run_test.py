2f79b3aaaa02b294724c6a208463f9a9a52021cf
Catherine Lee <csl@fb.com>
2024-05-13 12:54:00 -0700

    update

--- test/run_test.py
+++ test/run_test.py
@@ -1197,7 +1197,8 @@ def parse_args():
         and not IS_SLOW
         and not TEST_WITH_ROCM
         and not IS_MACOS
-        and not "onnx" in BUILD_ENVIRONMENT,
+        and not "onnx" in BUILD_ENVIRONMENT
+        and not "debug" in BUILD_ENVIRONMENT,
     )
     parser.add_argument(
         "additional_unittest_args",
