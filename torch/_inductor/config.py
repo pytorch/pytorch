e5484a147e7faffb8a1025d53a8e71d97c87c576
eellison <elias.ellison@gmail.com>
2024-05-13 21:15:40 +0000

    Enable epilogue fusion benchmarking internally (#125455)
    Differential Revision: [D56920738](https://our.internmc.facebook.com/intern/diff/D56920738)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125455
    Approved by: https://github.com/Chillee

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -318,15 +318,13 @@ debug_fusion = os.environ.get("TORCHINDUCTOR_DEBUG_FUSION") == "1"
 benchmark_fusion = os.environ.get("TORCHINDUCTOR_BENCHMARK_FUSION") == "1"
 enabled_metric_tables = os.environ.get("TORCHINDUCTOR_ENABLED_METRIC_TABLES", "")
 
-benchmark_multi_templates = (
-    os.environ.get(
-        "TORCHINDUCTOR_BENCHMARK_MULTI_TEMPLATES", "0" if is_fbcode() else "1"
-    )
-    == "1"
+# For Triton Templates, select fastest of best template + epilogue vs best template + separate epilogue kernel
+benchmark_epilogue_fusion = (
+    os.environ.get("TORCHINDUCTOR_BENCHMARK_EPILOGUE_FUSION", "1") == "1"
 )
 
 # Take how many of the top triton kernels to benchmark epilogue
-max_epilogue_benchmarked_choices = 3
+max_epilogue_benchmarked_choices = 1
 
 # how many nodes to allow into a single fusion
 max_fusion_size = 64
