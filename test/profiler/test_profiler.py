cad6ed53deca25f120f7f80d1078125d36b1565b
Luoshang Pan <pls331@meta.com>
2024-05-13 10:17:28 -0700

    allow user to set customized  filename for trace file (#122910)
    Summary:
    
    as title
    
    Reviewed By: SherlockNoMad
    
    Differential Revision: D55349229

--- test/profiler/test_profiler.py
+++ test/profiler/test_profiler.py
@@ -1086,6 +1086,7 @@ class TestProfiler(TestCase):
             self.assertTrue(os.path.exists(dname))
             file_num = 0
             for file_name in os.listdir(dname):
+                print(file_name)
                 parts = file_name.split(".")
                 self.assertTrue(len(parts) > 4)
                 self.assertTrue(
