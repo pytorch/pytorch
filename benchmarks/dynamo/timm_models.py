ea5996221c55e9c5ee86a80d24e79de35738ae00
Sam Larsen <slarsen@meta.com>
2024-05-14 13:19:56 -0700

    Enable FX graph cache for huggingface and timm benchmarks
    [ghstack-poisoned]

--- benchmarks/dynamo/timm_models.py
+++ benchmarks/dynamo/timm_models.py
@@ -13,6 +13,9 @@ from common import BenchmarkRunner, download_retry_decorator, main
 from torch._dynamo.testing import collect_results, reduce_to_scalar_loss
 from torch._dynamo.utils import clone_inputs
 
+# Enable FX graph caching
+torch._inductor.config.fx_graph_cache = True
+
 
 def pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
