befff049d4f098e36c733db27cd168acd606e2f9
PaliC <sahancpal@gmail.com>
2024-05-15 13:31:32 -0700

    Update (base update)
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Welcome to the PyTorch setup.py.
-#
+# modification
 # Environment variables you are probably interested in:
 #
 #   DEBUG
@@ -87,6 +87,12 @@
 #   USE_SYSTEM_NCCL=0
 #     disables use of system-wide nccl (we will use our submoduled
 #     copy in third_party/nccl)
+#
+#   BUILD_CAFFE2_OPS=0
+#     disable Caffe2 operators build
+#
+#   BUILD_CAFFE2=0
+#     disable Caffe2 build
 #
 #   USE_IBVERBS
 #     toggle features related to distributed support
@@ -220,7 +226,15 @@
 #
 #   USE_PRIORITIZED_TEXT_FOR_LD
 #      Uses prioritized text form cmake/prioritized_text.txt for LD
+#
+#   BUILD_LIBTORCH_WHL
+#      Builds libtorch.so and its dependencies as a wheel
+#
+#   BUILD_PYTHON_ONLY
+#      Builds pytorch as a wheel using libtorch.so from a seperate wheel
 
+import os
+import pkgutil
 import sys
 
 if sys.platform == "win32" and sys.maxsize.bit_length() == 31:
@@ -229,7 +243,35 @@ if sys.platform == "win32" and sys.maxsize.bit_length() == 31:
     )
     sys.exit(-1)
 
+import os
 import platform
+
+
+def _get_package_path(package_name):
+    loader = pkgutil.find_loader(package_name)
+    if loader:
+        # The package might be a namespace package, so get_data may fail
+        try:
+            file_path = loader.get_filename()
+            return os.path.dirname(file_path)
+        except AttributeError:
+            pass
+    return None
+
+
+BUILD_LIBTORCH_WHL = os.getenv("BUILD_LIBTORCH_WHL", "0") == "1"
+BUILD_PYTORCH_USING_LIBTORCH_WHL = os.getenv("BUILD_PYTHON_ONLY", "0") == "1"
+
+# set up appropriate env variables
+if BUILD_LIBTORCH_WHL:
+    # Set up environment variables for ONLY building libtorch.so and not libtorch_python.so
+    # functorch is not supported without python
+    os.environ["BUILD_FUNCTORCH"] = "OFF"
+
+
+if BUILD_PYTORCH_USING_LIBTORCH_WHL:
+    os.environ["BUILD_LIBTORCHLESS"] = "ON"
+    os.environ["LIBTORCH_LIB_PATH"] = f"{_get_package_path('libtorch')}/lib"
 
 python_min_version = (3, 8, 0)
 python_min_version_str = ".".join(map(str, python_min_version))
@@ -238,6 +280,19 @@ if sys.version_info < python_min_version:
         f"You are using Python {platform.python_version()}. Python >={python_min_version_str} is required."
     )
     sys.exit(-1)
+
+BUILD_LIBTORCH_WHL = os.getenv("BUILD_LIBTORCH_WHL", "0") == "1"
+BUILD_PYTORCH_USING_LIBTORCH_WHL = False
+
+# set up appropriate env variables
+if BUILD_LIBTORCH_WHL:
+    # Set up environment variables for ONLY building libtorch.so and not libtorch_python.so
+
+    # functorch is not supported without python
+    os.environ["BUILD_FUNCTORCH"] = "OFF"
+    os.environ["BUILD_PYTHONLESS"] = "ON"
+else:
+    os.environ["BUILD_PYTHONLESS"] = "OFF"
 
 import filecmp
 import glob
@@ -335,12 +390,22 @@ cmake_python_include_dir = sysconfig.get_path("include")
 ################################################################################
 # Version, create_version_file, and package_name
 ################################################################################
-package_name = os.getenv("TORCH_PACKAGE_NAME", "torch")
+
+
+DEFAULT_PACKAGE_NAME = "libtorch" if BUILD_LIBTORCH_WHL else "torch"
+
+package_name = os.getenv("TORCH_PACKAGE_NAME", DEFAULT_PACKAGE_NAME)
 package_type = os.getenv("PACKAGE_TYPE", "wheel")
 version = get_torch_version()
 report(f"Building wheel {package_name}-{version}")
 
 cmake = CMake()
+
+<<<<<<< HEAD
+=======
+DEFAULT_PACKAGE_NAME = "libtorch" if BUILD_LIBTORCH_WHL else "torch"
+
+package_name = os.getenv("TORCH_PACKAGE_NAME", DEFAULT_PACKAGE_NAME)
 
 
 def get_submodule_folders():
@@ -458,11 +523,12 @@ def build_deps():
 
     check_submodules()
     check_pydep("yaml", "pyyaml")
+    build_python = not BUILD_LIBTORCH_WHL
 
     build_caffe2(
         version=version,
         cmake_python_library=cmake_python_library,
-        build_python=True,
+        build_python=build_python,
         rerun_cmake=RERUN_CMAKE,
         cmake_only=CMAKE_ONLY,
         cmake=cmake,
@@ -719,6 +785,8 @@ class build_ext(setuptools.command.build_ext.build_ext):
             "caffe2.python.caffe2_pybind11_state_gpu",
             "caffe2.python.caffe2_pybind11_state_hip",
         ]
+        if BUILD_LIBTORCH_WHL:
+            caffe2_pybind_exts = []
         i = 0
         while i < len(self.extensions):
             ext = self.extensions[i]
@@ -950,8 +1018,13 @@ def configure_extension_build():
 
     main_compile_args = []
     main_libraries = ["torch_python"]
+
     main_link_args = []
     main_sources = ["torch/csrc/stub.c"]
+
+    if BUILD_LIBTORCH_WHL:
+        main_libraries = ["torch"]
+        main_sources = []
 
     if cmake_cache_vars["USE_CUDA"]:
         library_dirs.append(os.path.dirname(cmake_cache_vars["CUDA_CUDA_LIB"]))
@@ -1003,6 +1076,29 @@ def configure_extension_build():
                 macos_sysroot_path,
             ]
             extra_link_args += ["-arch", macos_target_arch]
+
+    def rename_torch_packages(package_list):
+        """
+        Create a dictionary from a list of package names, renaming packages where
+        the top-level package is 'torch' to 'libtorch'.
+        Args:
+            package_list (list of str): The list of package names.
+        Returns:
+            dict: A dictionary where keys are the package names with 'torch' replaced by 'libtorch',
+                and values are the original package names, only including those where the
+                top-level name is 'torch'.
+        """
+        result = {}
+        for package in package_list:
+            # Split the package name by dots to handle subpackages or modules
+            parts = package.split(".")
+            # Check if the top-level package is 'torch'
+            if parts[0] == "torch":
+                # Replace 'torch' with 'libtorch' in the top-level package name
+                new_key = "libtorch" + package[len("torch") :]
+                result[new_key] = package
+
+        return result
 
     def make_relative_rpath_args(path):
         if IS_DARWIN:
@@ -1074,7 +1170,6 @@ def configure_extension_build():
             "default = torch.distributed.elastic.multiprocessing:DefaultLogsSpecs",
         ],
     }
-
     return extensions, cmdclass, packages, entry_points, extra_install_requires
 
 
@@ -1100,6 +1195,32 @@ def print_box(msg):
     print("-" * (size + 2))
 
 
+def rename_torch_packages(package_list):
+    """
+    Create a dictionary from a list of package names, renaming packages where
+    the top-level package is 'torch' to 'libtorch'.
+
+    Args:
+        package_list (list of str): The list of package names.
+
+    Returns:
+        dict: A dictionary where keys are the package names with 'torch' replaced by 'libtorch',
+              and values are the original package names, only including those where the
+              top-level name is 'torch'.
+    """
+    result = {}
+    for package in package_list:
+        # Split the package name by dots to handle subpackages or modules
+        parts = package.split(".")
+        # Check if the top-level package is 'torch'
+        if parts[0] == "torch":
+            # Replace 'torch' with 'libtorch' in the top-level package name
+            new_key = "libtorch" + package[len("torch") :]
+            result[new_key] = package
+
+    return result
+
+
 def main():
     # the list of runtime dependencies required by this built package
     install_requires = [
@@ -1111,6 +1232,9 @@ def main():
         "fsspec",
         'mkl>=2021.1.1,<=2021.4.0; platform_system == "Windows"',
     ]
+
+    if BUILD_PYTORCH_USING_LIBTORCH_WHL:
+        install_requires.append("libtorch")
 
     use_prioritized_text = str(os.getenv("USE_PRIORITIZED_TEXT_FOR_LD", ""))
     if (
@@ -1187,10 +1311,6 @@ def main():
         "nn/parallel/*.pyi",
         "utils/data/*.pyi",
         "utils/data/datapipes/*.pyi",
-        "lib/*.so*",
-        "lib/*.dylib*",
-        "lib/*.dll",
-        "lib/*.lib",
         "lib/*.pdb",
         "lib/torch_shm_manager",
         "lib/*.h",
@@ -1356,6 +1476,22 @@ def main():
         "utils/model_dump/*.mjs",
     ]
 
+    if BUILD_PYTORCH_USING_LIBTORCH_WHL:
+        torch_package_data.extend(
+            [
+                "lib/libtorch_python*",
+                "lib/*shm*",
+            ]
+        )
+    else:
+        torch_package_data.extend(
+            [
+                "lib/*.so*",
+                "lib/*.dylib*",
+                "lib/*.dll",
+                "lib/*.lib",
+            ]
+        )
     if get_cmake_cache_vars()["BUILD_CAFFE2"]:
         torch_package_data.extend(
             [
@@ -1398,6 +1534,29 @@ def main():
         "packaged/autograd/*",
         "packaged/autograd/templates/*",
     ]
+
+    if BUILD_LIBTORCH_WHL:
+        modified_packages = []
+        for package in packages:
+            parts = package.split(".")
+            if parts[0] == "torch":
+                modified_packages.append("libtorch" + package[len("torch") :])
+        packages = modified_packages
+        package_dir = {"libtorch": "torch"}
+        torch_package_dir_name = "libtorch"
+        package_data = {"libtorch": torch_package_data}
+        extensions = []
+    else:
+        torch_package_dir_name = "torch"
+        package_dir = {}
+        package_data = {
+            "torch": torch_package_data,
+            "torchgen": torchgen_package_data,
+            "caffe2": [
+                "python/serialized_test/data/operator_test/*.zip",
+            ],
+        }
+
     setup(
         name=package_name,
         version=version,
@@ -1413,13 +1572,8 @@ def main():
         entry_points=entry_points,
         install_requires=install_requires,
         extras_require=extras_require,
-        package_data={
-            "torch": torch_package_data,
-            "torchgen": torchgen_package_data,
-            "caffe2": [
-                "python/serialized_test/data/operator_test/*.zip",
-            ],
-        },
+        package_data=package_data,
+        package_dir=package_dir,
         url="https://pytorch.org/",
         download_url="https://github.com/pytorch/pytorch/tags",
         author="PyTorch Team",
