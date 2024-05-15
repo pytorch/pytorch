082b3f275211fd25b4a4edba15864ded146530b8
Animesh Jain <anijain@umich.edu>
2024-05-15 13:03:33 -0700

    Update base for Update on "[DONT MERGE][dynamo] Turn on inlining of inbuilt nn modules"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang
    
    [ghstack-poisoned]

--- test/dynamo/test_higher_order_ops.py
+++ test/dynamo/test_higher_order_ops.py
@@ -1921,7 +1921,8 @@ class GraphModule(torch.nn.Module):
         self.assertTrue(len(wrap_node.args), 3)
 
         # Check that the linear bias and weight are getattr in the outer graph
-        self.assertTrue(len(dict(backend.graphs[0].named_parameters())) == 2)
+        if not torch._dynamo.config.inline_inbuilt_nn_modules:
+            self.assertTrue(len(dict(backend.graphs[0].named_parameters())) == 2)
 
         # Check that the inner function has one op and its a linear op
         body_function = getattr(backend.graphs[0], wrap_node.args[0].name)
@@ -2052,7 +2053,8 @@ class GraphModule(torch.nn.Module):
         self.assertTrue(len(wrap_node.args), 3)
 
         # Check that the linear bias and weight are getattr in the outer graph
-        self.assertTrue(len(dict(backend.graphs[0].named_parameters())) == 2)
+        if not torch._dynamo.config.inline_inbuilt_nn_modules:
+            self.assertTrue(len(dict(backend.graphs[0].named_parameters())) == 2)
 
         # Check that the inner function has one op and its a linear op
         body_function = getattr(backend.graphs[0], wrap_node.args[0].name)
