7a730c948c4d59c0a5aebc07d1b3f14ad3cf0a6f
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-05-16 05:52:13 +0000

    Revert "Add Lowering for FlexAttention Backwards (#125515)"
    This reverts commit 95b9e981c3ab68fc17f78b8a6bbfd9569745ae4c.
    
    Reverted https://github.com/pytorch/pytorch/pull/125515 on behalf of https://github.com/huydhn due to Sorry for reverting your change but the newly added test runs out of memory https://hud.pytorch.org/pytorch/pytorch/commit/95b9e981c3ab68fc17f78b8a6bbfd9569745ae4c ([comment](https://github.com/pytorch/pytorch/pull/125515#issuecomment-2114084869))

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3595,10 +3595,7 @@ class TritonTemplateBuffer(TemplateBuffer):
         self.mutated_inputs = mutated_inputs
         if mutated_inputs is not None:
             # Ensure that the mutated inputs are only allowed for certain nodes
-            allowed_set = {
-                torch.ops.higher_order.flex_attention,
-                torch.ops.higher_order.flex_attention_backward,
-            }
+            allowed_set = {torch.ops.higher_order.flex_attention}
             current_node = V.graph.current_node.target
             assert (
                 current_node in allowed_set
