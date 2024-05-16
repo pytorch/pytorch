dd2f8d1f72a6ea322582834c22e18727e7558c91
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-05-16 05:52:13 +0000

    Revert "Add Lowering for FlexAttention Backwards (#125515)"
    This reverts commit 95b9e981c3ab68fc17f78b8a6bbfd9569745ae4c.
    
    Reverted https://github.com/pytorch/pytorch/pull/125515 on behalf of https://github.com/huydhn due to Sorry for reverting your change but the newly added test runs out of memory https://hud.pytorch.org/pytorch/pytorch/commit/95b9e981c3ab68fc17f78b8a6bbfd9569745ae4c ([comment](https://github.com/pytorch/pytorch/pull/125515#issuecomment-2114084869))

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -103,7 +103,7 @@ class TritonTemplateKernel(TritonKernel):
         prefix_args=0,
         suffix_args=0,
         epilogue_fn=identity,
-        subgraphs: Optional[List[ir.ComputedBuffer]] = None,
+        subgraphs=None,
         *,
         index_dtype,
     ):
@@ -114,7 +114,7 @@ class TritonTemplateKernel(TritonKernel):
         )
         self.input_nodes = input_nodes
         self.output_node = output_node
-        self.named_input_nodes = {}  # type: ignore[var-annotated]
+        self.named_input_nodes = {}
         self.defines = defines
         self.kernel_name = kernel_name
         self.template_mask = None
@@ -128,10 +128,10 @@ class TritonTemplateKernel(TritonKernel):
         self.prefix_args = prefix_args
         self.suffix_args = suffix_args
         self.epilogue_fn = epilogue_fn
-        self.render_hooks = dict()  # type: ignore[var-annotated]
+        self.render_hooks = dict()
         self.triton_meta: Optional[Dict[str, object]] = None
-        # For Templated Attention this can be a list of ir.Subgraph
-        self.subgraphs: Optional[List[ir.ComputedBuffer]] = subgraphs
+        # For Templated Attention
+        self.subgraphs = subgraphs
 
     def need_numel_args(self):
         return False
@@ -271,28 +271,19 @@ class TritonTemplateKernel(TritonKernel):
             val = self.named_input_nodes[name].get_stride()[index]
         return texpr(self.rename_indexing(val))
 
-    def modification(self, subgraph_number: int, **fixed_inputs) -> str:
-        """This creates a modification function for a subgraph.
-        To use this inside a template, the first argument should specify which subgraph to codegen for
+    def modification(self, **fixed_inputs) -> str:
+        """This function generates the code body to populate
+        a 'modification' placeholder within a template
 
-        Args:
-            subgraph_number (int): The index of the subgraph in self.subgraphs
+        TODO come up with standardized way to modify templates, with
+        potential multiple modifications
         """
-        assert isinstance(subgraph_number, int)
-        assert isinstance(self.subgraphs, list)
-        assert subgraph_number < len(
-            self.subgraphs
-        ), f"Invalid subgraph number provided to create_modification, {subgraph_number} must be < {len(self.subgraphs)}"
-
-        subgraph = self.subgraphs[subgraph_number]
 
         def add_input(name):
             return self.args.input(name)
 
-        name = f"PlaceholderSubstitution_{subgraph_number}"
-
         class PlaceholderSubstitution(V.WrapperHandler):  # type: ignore[name-defined]
-            self.name = name
+            self.name = "PlaceholderSubstitution"
 
             def load(self, name: str, index: sympy.Expr):
                 if name not in fixed_inputs:
@@ -306,14 +297,15 @@ class TritonTemplateKernel(TritonKernel):
             def indirect_indexing(self, index_var, size, check):
                 return sympy_index_symbol(str(index_var))
 
+        # if self.modification_cache is None:
         with V.set_ops_handler(PlaceholderSubstitution(V.ops)):
             assert isinstance(
-                subgraph, ir.ComputedBuffer
-            ), f"Expected the subgraph to be a ComputedBuffer, got {type(subgraph)}"
-            if isinstance(subgraph.data, ir.InputBuffer):
-                out = subgraph.data.make_loader()((1,))
+                self.subgraphs, ir.ComputedBuffer
+            ), "Expected the subgraph to be a ComputedBuffer"
+            if isinstance(self.subgraphs.data, ir.InputBuffer):
+                out = self.subgraphs.data.make_loader()((1,))
             else:
-                out = subgraph.data.inner_fn((1,))
+                out = self.subgraphs.data.inner_fn((1,))
 
         self.codegen_body()
         self.body.writeline(f"{fixed_inputs['out']} = {out.value}")
@@ -328,18 +320,11 @@ class TritonTemplateKernel(TritonKernel):
         indices: Union[List[Any], Tuple[Any]],
         val: str,
         mask: Optional[str] = None,
-        indent_width: int = 4,
     ):
-        """Stores the final output and appends any epilogue fusions if the buffer hasn't been optimized away.
-
-        Args:
-            indices (Union[List, Tuple]): The index for each dimension of the output. The dot product of
-                these indices and output strides must match `val`.
-            val (str): The value to store.
-            mask (Optional[str]): An optional mask to use for the store operation. If provided, this mask
-                will be applied to the store.
-            indent_width (int): The number of spaces to use for indentation. This is used when the call to
-                store_output is indented in the kernel definition.
+        """
+        Hook called from template code to store the final output
+        (if the buffer hasn't been optimized away), then append any
+        epilogue fusions.
         """
         assert isinstance(indices, (list, tuple))
         assert isinstance(val, str)
@@ -363,7 +348,7 @@ class TritonTemplateKernel(TritonKernel):
         self.range_trees[0].lookup(sympy.Integer(1), sympy_product(lengths)).set_name(
             "xindex"
         )
-        self.template_mask = mask  # type: ignore[assignment]
+        self.template_mask = mask
         self.template_indices = indices
         output_index = self.output_node.get_layout().make_indexer()(index_symbols)
         output_index = self.rename_indexing(output_index)
@@ -388,7 +373,7 @@ class TritonTemplateKernel(TritonKernel):
         def hook():
             # more stuff might have been added since the codegen_body above
             self.codegen_body()
-            return textwrap.indent(self.body.getvalue(), " " * indent_width).strip()
+            return textwrap.indent(self.body.getvalue(), "    ").strip()
 
         assert "<STORE_OUTPUT>" not in self.render_hooks
         self.render_hooks["<STORE_OUTPUT>"] = hook
@@ -1436,7 +1421,7 @@ class AlgorithmSelectorCache(PersistentCache):
             result = timings[choice]
             if result:
                 sys.stderr.write(
-                    f"  {choice.name} {result:.4f} ms {best_time / result:.1%}\n"
+                    f"  {choice.name} {result:.4f} ms {best_time/result:.1%}\n"
                 )
             else:
                 sys.stderr.write(
