25ff5fe193d6aa5786252ddb1d9c154b938736c4
hippocookie <zesheng.zong@outlook.com>
2024-05-16 09:11:53 +0800

    Fix supress comment mismatch line after UFMT format

--- test/typing/fail/creation_ops.py
+++ test/typing/fail/creation_ops.py
@@ -2,11 +2,11 @@
 import torch
 
 torch.tensor(
-    [3], dtype="int32"
-)  # E: Argument "dtype" to "tensor" has incompatible type "str"; expected "dtype | None"  [arg-type]
-torch.ones(
+    [3], dtype="int32"  # E: Argument "dtype" to "tensor" has incompatible type "str"; expected "dtype | None"  [arg-type]
+)
+torch.ones(  # E: No overload variant of "ones" matches argument types "int", "str"
     3, dtype="int32"
-)  # E: No overload variant of "ones" matches argument types "int", "str"
-torch.zeros(
+)
+torch.zeros(  # E: No overload variant of "zeros" matches argument types "int", "str"
     3, dtype="int32"
-)  # E: No overload variant of "zeros" matches argument types "int", "str"
+)
