0df33c1c081bc88b8845ed6fd7f8288e9b7777b7
Tom Ritchford <tom@swirly.com>
2024-05-16 19:41:46 +0000

    Update on "Add Opinfo entry for add_alias"
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -20998,10 +20998,12 @@ op_db: List[OpInfo] = [
         sample_inputs_func=sample_inputs_alias_copy,
         supports_fwgrad_bwgrad=True,
         skips=(
-             # RuntimeError: aten::alias_copy hit the vmap fallback which is currently disabled
-             DecorateInfo(unittest.expectedFailure, 'TestVmapOperatorsOpInfoCPU', 'test_op_has_batch_rule_alias_copy_cpu_float32'),
-             DecorateInfo(unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'),
-             DecorateInfo(unittest.expectedFailure, 'TestVmapOperatorsOpInfoCUDA', 'test_op_has_batch_rule_alias_copy_cuda_float32'),
+            # RuntimeError: aten::alias_copy hit the vmap fallback which is currently disabled
+            DecorateInfo(unittest.expectedFailure, 'TestVmapOperatorsOpInfoCPU', 'test_op_has_batch_rule_alias_copy_cpu_float32'),
+            DecorateInfo(unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestVmapOperatorsOpInfoCUDA', 'test_op_has_batch_rule_alias_copy_cuda_float32',
+            ),
         ),
     ),
 ]
