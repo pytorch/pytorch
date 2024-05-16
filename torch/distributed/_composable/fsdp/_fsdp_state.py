1078ad318ded31017416d18500449ff4a88f2a51
Will Feng <yf225@cornell.edu>
2024-05-15 19:51:25 -0700

    lint

--- torch/distributed/_composable/fsdp/_fsdp_state.py
+++ torch/distributed/_composable/fsdp/_fsdp_state.py
@@ -5,7 +5,6 @@ from typing import Any, Dict, List, Optional, Tuple, TYPE_CHECKING
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from torch.autograd.graph import register_multi_grad_hook
 from torch.distributed._composable_state import (
     _get_module_state,
     _insert_module_state,
