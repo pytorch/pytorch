3e66ffc4dfce15761f87e266607abd211c3595d7
Isuru Fernando <ifernando@quansight.com>
2024-05-16 16:04:03 +0000

    Update
    [ghstack-poisoned]

--- test/test_mps.py
+++ test/test_mps.py
@@ -139,8 +139,8 @@ def mps_ops_grad_modifier(ops):
         'round': [torch.float16],
 
         # atomic operation in backward pass
-        '_unsafe_masked_index': [torch.int8, torch.float16],
-        '_unsafe_masked_index_put_accumulate': [torch.int8, torch.float16],
+        '_unsafe_masked_index': [torch.int8, torch.float16, torch.int64],
+        '_unsafe_masked_index_put_accumulate': [torch.int8, torch.float16, torch.int64],
     }
 
     MACOS_12_3_XFAILLIST_GRAD = {
@@ -885,7 +885,7 @@ def mps_ops_modifier(ops):
         'round': [torch.float16],
 
         # atomic operations not supported
-        '_unsafe_masked_index_put_accumulate': [torch.int8, torch.float16],
+        '_unsafe_masked_index_put_accumulate': [torch.int8, torch.float16, torch.int64],
     }
 
     if product_version < 14.0:
