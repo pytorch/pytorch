8de6e54afb33b83db7055e269e6da57db6926c9b
Nathaniel Burke <nathanielrburke3@gmail.com>
2024-05-17 18:57:13 +0100

    Add quantized::layer_norm to ONNX opset>=17

--- torch/onnx/symbolic_opset17.py
+++ torch/onnx/symbolic_opset17.py
@@ -66,6 +66,22 @@ def layer_norm(
         axis_i=axis,
     )
 
+@_onnx_symbolic("quantized::layer_norm")
+def quantized_layer_norm(
+    g: jit_utils.GraphContext,
+    x,
+    normalized_shape,
+    weight,
+    bias,
+    eps,
+    op_scale,
+    op_zero_point,
+):
+    x, _, _, _ = symbolic_helper.dequantize_helper(g, x)
+
+    output = layer_norm(g, x, normalized_shape, weight, bias, eps, False)
+
+    return symbolic_helper.quantize_helper(g, output, op_scale, op_zero_point)
 
 def _compute_edge_sizes(n_fft, window_size):
     """Helper function to compute the sizes of the edges (left and right)
