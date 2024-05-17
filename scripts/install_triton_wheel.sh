9462ff173b0b592d0484efbf8873efaa4c2d165a
Stonepia <tong.su@intel.com>
2024-05-17 06:23:30 +0000

    refine code

--- scripts/install_triton_wheel.sh
+++ scripts/install_triton_wheel.sh
@@ -2,11 +2,11 @@
 # Updates Triton to the pinned version for this copy of PyTorch
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-TRITON_VERSION="pytorch-triton==$(cat .ci/docker/triton_version.txt)"
-DOWNLOAD_PYTORCH_ORG="https://download.pytorch.org/whl"
-
 if [[ -z "${USE_XPU}" ]]; then
     # Default install from PyTorch source
+
+    TRITON_VERSION="pytorch-triton==$(cat .ci/docker/triton_version.txt)"
+    DOWNLOAD_PYTORCH_ORG="https://download.pytorch.org/whl"
     if [[ "$BRANCH" =~ .*release.* ]]; then
         pip install --index-url ${DOWNLOAD_PYTORCH_ORG}/test/ $TRITON_VERSION
     else
@@ -14,12 +14,13 @@ if [[ -z "${USE_XPU}" ]]; then
     fi
 else
     # Install Triton for XPU
+
     if [[ "$BRANCH" =~ .*release.* ]]; then
-        # Option 1: Stable version. Use the pre-built wheel from pypi.
+        # Stable version. Use the pre-built wheel from pypi.
         TRITON_XPU_VERSION="triton-xpu==$(cat .ci/docker/triton_xpu_version.txt)"
         pip install ${TRITON_XPU_VERSION}
     else
-        # Option 2: Nightly version. Always build triton-xpu from source.
+        # Nightly version. Always build triton-xpu from the source.
         TRITON_XPU_REPO="https://github.com/intel/intel-xpu-backend-for-triton"
         TRITON_XPU_COMMIT_ID="$(cat .ci/docker/ci_commit_pins/triton-xpu.txt)"
 
