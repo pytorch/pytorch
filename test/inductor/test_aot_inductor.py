a2d48703bddd35bdf62cb71e1d11ce5665a71915
Yueming Hao <yhao@meta.com>
2024-05-17 10:33:48 -0700

    skip two tests with known segmentfault errors

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -2853,6 +2853,8 @@ CPU_TEST_FAILURES = {
     "test_duplicate_constant_folding": fail_with_and_without_stack_allocation(
         is_skip=True
     ),
+    "test_dup_unbacked_sym_decl": fail_minimal_arrayref_interface(is_skip=True),
+    "test_dup_unbacked_sym_decl_with_refinement": fail_minimal_arrayref_interface(is_skip=True),
     "test_dynamic_cat": fail_minimal_arrayref_interface(),
     # https://github.com/pytorch/pytorch/issues/122978
     "test_dynamic_scalar": fail_stack_allocation(is_skip=True),
