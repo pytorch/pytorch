44a6e1f4e42b5267226903d1f3c5805d323ea7ba
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-19 19:22:28 +0100

    feat: implement job scheduling logic

--- c10/util/FunctionScheduler.cpp
+++ c10/util/FunctionScheduler.cpp
@@ -11,15 +11,31 @@ namespace c10 {
 Job::Job(std::function<void()> function, std::chrono::microseconds interval)
     : _function(function), _interval(interval) {}
 
+std::chrono::time_point<std::chrono::steady_clock> Job::next_run() const {
+  return _next_run;
+}
+
 std::chrono::microseconds Job::interval() const {
   return _interval;
+}
+
+int Job::id() const {
+  return _id;
+}
+
+void Job::set_next_run(std::chrono::time_point<std::chrono::steady_clock> next_run) {
+  _next_run = next_run;
+}
+
+void Job::set_id(int id) {
+  _id = id;
 }
 
 void Job::run() {
   _function();
 }
 
-FunctionScheduler::FunctionScheduler() : _running(false) {}
+FunctionScheduler::FunctionScheduler() {}
 
 FunctionScheduler::~FunctionScheduler() {
   stop();
@@ -27,13 +43,44 @@ FunctionScheduler::~FunctionScheduler() {
 
 void FunctionScheduler::runNextJob() {}
 
+std::chrono::microseconds getNextInterval() {}
+
+int FunctionScheduler::id() {
+  return _current_id++;
+}
+
+int FunctionScheduler::scheduleJob(Job job) {
+  int job_id = id();
+  job.set_id(job_id);
+
+  if (!_running) {
+    _jobs.push_back(job);
+  } else {
+    job.set_next_run(std::chrono::steady_clock::now() + job.interval());
+    _queue.insert(job);
+  }
+
+  return job_id;
+}
+
 int FunctionScheduler::scheduleJob(
     std::function<void()> function,
-    std::chrono::microseconds interval) {}
+    std::chrono::microseconds interval) {
+
+  return scheduleJob(Job(function, interval));
+}
 
 int FunctionScheduler::removeJob(int id) {}
 
-void FunctionScheduler::start() {}
+void FunctionScheduler::start() {
+  while (!_jobs.empty()) {
+    Job job = _jobs.back();
+    _jobs.pop_back();
+    job.set_next_run(std::chrono::steady_clock::now() + job.interval());
+    _queue.insert(job);
+  }
+  _running = true;
+}
 
 void FunctionScheduler::stop() {}
 
--- c10/util/FunctionScheduler.h
+++ c10/util/FunctionScheduler.h
@@ -3,29 +3,43 @@
 #include <functional>
 #include <chrono>
 #include <thread>
-#include <vector>
 #include <atomic>
+#include <vector>
+#include <set>
 
 namespace c10 {
 
 class Job {
+    int _id;
     std::function<void()> _function;
     std::chrono::microseconds _interval;
+    std::chrono::time_point<std::chrono::steady_clock> _next_run;
 
 public:
+    static inline bool lt(Job a, Job b) { return a.next_run() < b.next_run(); }
+
     Job(std::function<void()> function, std::chrono::microseconds interval);
 
+    std::chrono::time_point<std::chrono::steady_clock> next_run() const;
     std::chrono::microseconds interval() const;
+    int id() const;
+
+    void set_next_run(std::chrono::time_point<std::chrono::steady_clock> next_run);
+    void set_id(int id);
+
     void run();
 };
 
 class FunctionScheduler {
-    std::atomic_bool _running;
+    int _current_id = 0;
+    std::atomic_bool _running = false;
+    std::multiset<Job, decltype(&Job::lt)> _queue;
     std::vector<Job> _jobs;
     std::thread _thread;
 
     void runNextJob();
     std::chrono::microseconds getNextInterval() const;
+    int id();
 
 public:
     FunctionScheduler();
