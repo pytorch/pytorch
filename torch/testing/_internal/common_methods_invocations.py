2137a29006771aa10d974c13c6fcea1f02e4ca22
Tom Ritchford <tom@swirly.com>
2024-05-19 20:42:45 +0000

    Update on "Add Opinfo entry for add_alias"
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -20996,13 +20996,27 @@ op_db: List[OpInfo] = [
         dtypes=all_types_and(torch.bool, torch.float16, torch.bfloat16),
         supports_out=True,
         sample_inputs_func=sample_inputs_alias_copy,
+        supports_forward_ad=True,
         supports_fwgrad_bwgrad=True,
         skips=(
             # RuntimeError: aten::alias_copy hit the vmap fallback which is currently disabled
-            DecorateInfo(unittest.expectedFailure, 'TestVmapOperatorsOpInfoCPU', 'test_op_has_batch_rule_alias_copy_cpu_float32'),
-            DecorateInfo(unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'),
             DecorateInfo(
-                unittest.expectedFailure, 'TestVmapOperatorsOpInfoCUDA', 'test_op_has_batch_rule_alias_copy_cuda_float32',
+                unittest.expectedFailure, 'TestFwdGradientsCUDA', 'test_forward_mode_AD_alias_copy_cuda_float64'
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestOperatorsCUDA', 'test_vmapvjp_has_batch_rule_alias_copy_cuda_float32'
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestVmapOperatorsOpInfoCPU', 'test_op_has_batch_rule_alias_copy_cpu_float32'
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, 'TestVmapOperatorsOpInfoCUDA', 'test_op_has_batch_rule_alias_copy_cuda_float32'
             ),
         ),
     ),
