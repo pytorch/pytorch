6a91d6342aa38639657c870d22a7e86737d78e0b
cyy <cyyever@outlook.com>
2024-05-20 08:37:21 +0800

    Cleanup

--- torch/csrc/profiler/stubs/cuda.cpp
+++ torch/csrc/profiler/stubs/cuda.cpp
@@ -1,6 +1,10 @@
 #include <sstream>
 
+#if __has_include(<nvtx3/nvToolsExt.h>)
 #include <nvtx3/nvToolsExt.h>
+#else
+#include <nvToolsExt.h>
+#endif
 
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/util/ApproximateClock.h>
