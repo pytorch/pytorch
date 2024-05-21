16bbeff51b86d23059c659c81ed8b61bf2e497af
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2024-05-21 03:25:47 +0100

    feat: implement FunctionScheduler methods
    runNextJob, getNextInterval, removeJob, stop

--- c10/util/FunctionScheduler.cpp
+++ c10/util/FunctionScheduler.cpp
@@ -1,5 +1,7 @@
+#include "./FunctionScheduler.h"
 #include <c10/util/FunctionScheduler.h>
 
+#include <algorithm>
 #include <atomic>
 #include <chrono>
 #include <functional>
@@ -8,7 +10,11 @@
 
 namespace c10 {
 
-/* static */ bool Job::lt(Job a, Job b) { return a.next_run() < b.next_run(); }
+/* Job */
+
+bool Job::lt(Job a, Job b) {
+  return a.next_run() < b.next_run();
+}
 
 Job::Job(std::function<void()> function, std::chrono::microseconds interval)
     : _function(function), _interval(interval) {}
@@ -25,7 +31,8 @@ int Job::id() const {
   return _id;
 }
 
-void Job::set_next_run(std::chrono::time_point<std::chrono::steady_clock> next_run) {
+void Job::set_next_run(
+    std::chrono::time_point<std::chrono::steady_clock> next_run) {
   _next_run = next_run;
 }
 
@@ -33,19 +40,33 @@ void Job::set_id(int id) {
   _id = id;
 }
 
-void Job::run() {
+void Job::run() const {
   _function();
 }
 
-FunctionScheduler::FunctionScheduler() {}
+/* FunctionScheduler */
+
+FunctionScheduler::FunctionScheduler() = default;
 
 FunctionScheduler::~FunctionScheduler() {
   stop();
 }
 
-void FunctionScheduler::runNextJob() {}
+void FunctionScheduler::runNextJob() {
+  if (_queue.empty())
+    return;
 
-std::chrono::microseconds getNextInterval() {}
+  if (getNextInterval().count() < 0)
+    _queue.begin()->run();
+}
+
+std::chrono::microseconds FunctionScheduler::getNextInterval() const {
+  if (_queue.empty())
+    return std::chrono::microseconds(999);
+
+  auto interval = _queue.begin()->next_run() - std::chrono::steady_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(interval);
+}
 
 int FunctionScheduler::id() {
   return _current_id++;
@@ -68,11 +89,30 @@ int FunctionScheduler::scheduleJob(Job job) {
 int FunctionScheduler::scheduleJob(
     std::function<void()> function,
     std::chrono::microseconds interval) {
-
   return scheduleJob(Job(function, interval));
 }
 
-int FunctionScheduler::removeJob(int id) {}
+int FunctionScheduler::removeJob(int id) {
+  // TODO make sure job doesn't start while removing it?
+  _running = false;
+
+  // remove from _queue
+  for (auto it = _queue.begin(); it != _queue.end();) {
+    if (it->id() == id)
+      it = _queue.erase(it);
+    else
+      it++;
+  }
+  _running = true;
+
+  // remove from _jobs
+  _jobs.erase(
+      std::remove_if(
+          _jobs.begin(),
+          _jobs.end(),
+          [id](const Job& job) { return job.id() == id; }),
+      _jobs.end());
+}
 
 void FunctionScheduler::start() {
   while (!_jobs.empty()) {
@@ -84,6 +124,10 @@ void FunctionScheduler::start() {
   _running = true;
 }
 
-void FunctionScheduler::stop() {}
+void FunctionScheduler::stop() {
+  _running = false;
+  _jobs.insert(_jobs.end(), _queue.begin(), _queue.end());
+  _queue.clear();
+}
 
 } // namespace c10
