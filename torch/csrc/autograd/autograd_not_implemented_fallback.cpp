c067164907a995960b6ca95d7335e7dcfa8a0a25
Joel Schlosser <jbschlosser@meta.com>
2024-05-21 15:02:27 -0400

    Update base for Update on "Short-term fix to preserve NJT metadata cache in torch.compile"
    Idea: close over min / max sequence length in the main NJT view func (`_nested_view_from_jagged`) so that view replay during fake-ification propagates these correctly in torch.compile.
    
    For dynamic shapes support for min / max sequence length, this PR uses a hack that stores the values in `(val, 0)` shaped tensors.
    
    **NB: This PR changes SDPA to operate on real views instead of using `buffer_from_jagged()` / `ViewNestedFromBuffer`, which may impact the internal FIRST model. That is, it undoes the partial revert from #123215 alongside a fix to the problem that required the partial revert. We need to verify that there are no regressions there before landing.**
    
    Differential Revision: [D55448636](https://our.internmc.facebook.com/intern/diff/D55448636)
    
    [ghstack-poisoned]

--- torch/csrc/autograd/autograd_not_implemented_fallback.cpp
+++ torch/csrc/autograd/autograd_not_implemented_fallback.cpp
@@ -413,15 +413,24 @@ static void autogradNotImplementedFallbackImpl(
     if (aliased_input.has_storage()) {
       if (aliased_output_iv.isTensor()) {
         const at::Tensor& aliased_output = aliased_input_iv.toTensor();
-        TORCH_INTERNAL_ASSERT(
-            aliased_input.storage().is_alias_of(aliased_output.storage()),
-            op_name);
-      } else {
-        const auto aliased_output_vec = aliased_output_iv.toTensorVector();
-        for (const auto& aliased_output : aliased_output_vec) {
+        // only check aliasing for dense -> dense and subclass -> subclass views
+        if (at::impl::tensor_has_dispatch(aliased_input) ==
+            at::impl::tensor_has_dispatch(aliased_output)) {
           TORCH_INTERNAL_ASSERT(
               aliased_input.storage().is_alias_of(aliased_output.storage()),
               op_name);
+        }
+      } else {
+        const auto aliased_output_vec = aliased_output_iv.toTensorVector();
+        for (const auto& aliased_output : aliased_output_vec) {
+          // only check aliasing for dense -> dense and subclass -> subclass
+          // views
+          if (at::impl::tensor_has_dispatch(aliased_input) ==
+              at::impl::tensor_has_dispatch(aliased_output)) {
+            TORCH_INTERNAL_ASSERT(
+                aliased_input.storage().is_alias_of(aliased_output.storage()),
+                op_name);
+          }
         }
       }
     }
