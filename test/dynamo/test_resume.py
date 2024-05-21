b7ad5f30e81691524b32598ef519613ca599d41d
youkaichao <youkaichao@gmail.com>
2024-05-21 15:48:21 -0700

    fix lint

--- test/dynamo/test_resume.py
+++ test/dynamo/test_resume.py
@@ -3,16 +3,21 @@
 import torch
 import torch._dynamo.test_case
 
+
 def fn_creator():
     var1 = 1
+
     def fn(x):
         x = x + 1
         var2 = 1
         torch._dynamo.graph_break()
         x = x + var1
+
         def inner_fn():
             return var2
+
         return x
+
     return fn
 
 
@@ -24,7 +29,7 @@ class ResumeFunctionTests(torch._dynamo.test_case.TestCase):
         codes = [v for k, v in list(globals().items()) if k.startswith("__resume_at")]
         self.assertEqual(len(codes), 1)
         # co_freevars of resume functions, are sorted concatenation of the original function's co_freevars and co_cellvars
-        self.assertEqual(codes[0].co_freevars, ('var1', 'var2'))
+        self.assertEqual(codes[0].co_freevars, ("var1", "var2"))
 
 
 if __name__ == "__main__":
