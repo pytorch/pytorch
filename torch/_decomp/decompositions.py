1a674eb1120ad41d5c5d0569a253c79fb3ecf507
Isuru Fernando <ifernando@quansight.com>
2024-05-21 22:18:47 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -3564,7 +3564,6 @@ def upsample_linear1d(
 @register_decomposition(
     [aten.upsample_bilinear2d.default, aten.upsample_bilinear2d.out]
 )
-@aten.upsample_bilinear2d.default.py_impl(DispatchKey.Autograd)
 @out_wrapper()
 def upsample_bilinear2d(
     input: Tensor,
@@ -3695,6 +3694,129 @@ def _upsample_linear(
 
     if not input.is_floating_point():
         result = result.round()
+
+    return result
+
+
+@register_decomposition(aten.upsample_linear1d_backward)
+@out_wrapper("grad_input")
+def upsample_linear1d_backward(
+    grad_output: Tensor,
+    output_size: List[int],
+    input_size: List[int],
+    align_corners: bool,
+    scales_w: Optional[float] = None,
+) -> Tensor:
+    return _upsample_linear_backward(
+        grad_output, output_size, input_size, align_corners, [scales_w]
+    )
+
+
+@register_decomposition(aten.upsample_bilinear2d_backward)
+@out_wrapper("grad_input")
+def upsample_bilinear2d_backward(
+    grad_output: Tensor,
+    output_size: List[int],
+    input_size: List[int],
+    align_corners: bool,
+    scales_h: Optional[float] = None,
+    scales_w: Optional[float] = None,
+) -> Tensor:
+    return _upsample_linear_backward(
+        grad_output, output_size, input_size, align_corners, [scales_h, scales_w]
+    )
+
+
+@register_decomposition(aten.upsample_trilinear3d_backward)
+@out_wrapper("grad_input")
+def upsample_trilinear3d_backward(
+    grad_output: Tensor,
+    output_size: List[int],
+    input_size: List[int],
+    align_corners: bool,
+    scales_d: Optional[float] = None,
+    scales_h: Optional[float] = None,
+    scales_w: Optional[float] = None,
+) -> Tensor:
+    return _upsample_linear_backward(
+        grad_output,
+        output_size,
+        input_size,
+        align_corners,
+        [scales_d, scales_h, scales_w],
+    )
+
+
+@pw_cast_for_opmath
+def _upsample_linear_backward(
+    grad_output: Tensor,
+    output_size: List[int],
+    input_size: List[int],
+    align_corners: bool,
+    scales: List[Optional[float]],
+) -> Tensor:
+    # get dimensions of original image
+    n_batch, n_channels = input_size[:2]
+    inp_sizes = input_size[2:]
+    n_dims = len(inp_sizes)
+
+    _, dtype = utils.elementwise_dtypes(
+        grad_output,
+        type_promotion_kind=utils.ELEMENTWISE_TYPE_PROMOTION_KIND.INT_TO_FLOAT,
+    )
+
+    def get_values(inp_size, out_size, scales, nsqueeze):
+        # First Calculate scaling factor
+        scale_factor = _compute_scale(inp_size, out_size, align_corners, scales)
+        # We have to create arange with int64 dtype and use .to in order to avoid
+        # additional kernels creation in inductor and get a perf slowdown
+        i = torch.arange(out_size, device=grad_output.device).to(dtype=dtype)
+
+        x_f32 = _compute_source_index(scale_factor, i, align_corners).clamp(min=0.0)
+        x_f32 = x_f32.reshape(x_f32.shape[0], *[1] * (nsqueeze))
+        x = x_f32.to(torch.int64)
+        xp1 = (x + 1).clamp(max=inp_size - 1)
+        return x_f32, x, xp1
+
+    values = [
+        get_values(inp_size, out_size, scales, n_dims - 1 - i)
+        for i, (inp_size, out_size, scales) in enumerate(
+            zip(inp_sizes, output_size, scales)
+        )
+    ]
+    xs_f32, xs, xp1s = list(zip(*values))
+
+    # Using lists here to keep the coefficients and multiply at the
+    # end to help inductor do fusions easier.
+    # This results in (a * (b * (c * x)) instead of (a * (b * c)) * x
+    # in the generated code and therefore inductor does not try to
+    # create temporaries for (b * c).
+    coeff_lists: List[List[Any]] = [[]]
+    for i in range(n_dims):
+        xscale = (xs_f32[i] - xs[i]).clamp(0.0, 1.0).to(dtype)
+        new_coeffs: List[Any] = [None] * (2 * len(coeff_lists))
+        new_coeffs[::2] = [coeff_list + [1 - xscale] for coeff_list in coeff_lists]
+        new_coeffs[1::2] = [coeff_list + [xscale] for coeff_list in coeff_lists]
+        coeff_lists = new_coeffs
+
+    result = grad_output.new_zeros(input_size)
+    for coeff_list, a in zip(coeff_lists, product(*[[0, 1]] * n_dims)):
+        idx = [None, None] + [xs[k] if a[k] == 0 else xp1s[k] for k in range(n_dims)]
+        coeff = grad_output
+        for c in reversed(coeff_list):
+            coeff = torch.mul(coeff, c)
+        result = aten._unsafe_index_put(result, idx, coeff, accumulate=True)
+
+    # convert output to correct memory format, if necessary
+    memory_format = utils.suggest_memory_format(grad_output)
+
+    # following "heuristic: only use channels_last path when it's faster than the contiguous path"
+    if grad_output.device.type == "cuda" and n_channels < 16:
+        memory_format = torch.contiguous_format
+
+    assert isinstance(result, torch.Tensor)
+
+    result = result.contiguous(memory_format=memory_format)
 
     return result
 
