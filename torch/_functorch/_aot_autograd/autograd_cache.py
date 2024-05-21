f490da64f7cc331dc9707a6bc74e4977b7b0934e
James Wu <jjwu@meta.com>
2024-05-21 09:58:27 -0700

    Rebase
    [ghstack-poisoned]

--- torch/_functorch/_aot_autograd/autograd_cache.py
+++ torch/_functorch/_aot_autograd/autograd_cache.py
@@ -4,11 +4,22 @@ Utils for caching the outputs of AOTAutograd
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import Callable, List, Optional, TYPE_CHECKING
 
 from torch._inductor.codecache import _ident, FxGraphCachePickler
 
-from .schemas import AOTConfig  # noqa: F401
+from .runtime_wrappers import (
+    AOTDispatchAutograd,
+    AOTDispatchSubclassWrapper,
+    CompilerWrapper,
+    FunctionalizedRngRuntimeWrapper,
+    post_compile,
+    RuntimeWrapper,
+    SubclassMeta,
+)
+
+from .schemas import AOTConfig, ViewAndMutationMeta  # noqa: F401
 
 if TYPE_CHECKING:
     import torch
@@ -68,3 +79,109 @@ def autograd_cache_hash(
     key = "a" + AOTAutogradCachePickler.get_hash(details)
     log.debug("FX graph cache hash details for key %s:\n%s", key, details.debug_str())
     return key
+
+
+@dataclass
+class CompiledForward:
+    """
+    Cacheable entry for a forward function
+    """
+
+    fw_key: str  # FXGraphCache hash key
+
+    def load(self):
+        # TODO: do we save the entire CompiledFXGraph or just the key to FXGraphCache?
+        pass
+
+
+@dataclass
+class CompiledBackward:
+    """
+    Cacheable entry for a forward function
+    """
+
+    bw_key: str  # FXGraphCache hash key
+    backward_state_indices: List[int]
+    num_symints_saved_for_bw_: int
+
+    def load(self):
+        # TODO: do we save the entire CompiledFXGraph or just the key to FXGraphCache?
+        pass
+
+
+@dataclass
+class AOTAutogradCacheEntry:
+    """A single entry into the cache"""
+
+    compiled_fw: CompiledForward
+    compiled_bw: Optional[CompiledBackward]
+    runtime_metadata: ViewAndMutationMeta
+
+    # Wrappers that run after each aot_dispatch_* function
+    dispatch_wrappers: List[CompilerWrapper]
+
+    # Used by AOTSubclassWrapper
+    maybe_subclass_meta: Optional[SubclassMeta]
+    num_fw_outs_saved_for_bw: int
+
+    # Used by RuntimeWrapepr
+    indices_of_inps_to_detach: List[int]
+
+    # Turn cache entry into the original callable
+    def wrap_post_compile(self, aot_config: AOTConfig) -> Callable:
+        compiled_fw_func = self.compiled_fw.load()
+        compiled_bw_func = None
+        if self.compiled_bw is not None:
+            compiled_bw_func = self.compiled_bw.load()
+            needs_autograd = True
+        else:
+            needs_autograd = False
+
+        # Wrap the forward function in post compile wrappers
+        compiled_fw_func = AOTDispatchSubclassWrapper(
+            trace_joint=needs_autograd,
+            fw_only=None,
+            maybe_subclass_meta=self.maybe_subclass_meta,
+            num_fw_outs_saved_for_bw=self.num_fw_outs_saved_for_bw,
+        ).post_compile(
+            compiled_fw_func, aot_config, runtime_metadata=self.runtime_metadata
+        )
+
+        # In autograd case, functionalizedRngWrapper should not modify outs
+        return_new_outs = not needs_autograd
+        compiled_fw_func = FunctionalizedRngRuntimeWrapper(
+            return_new_outs=return_new_outs
+        ).post_compile(
+            compiled_fw_func, aot_config, runtime_metadata=self.runtime_metadata
+        )
+        disable_amp = torch._C._is_any_autocast_enabled()
+
+        if needs_autograd:
+            assert self.compiled_bw is not None
+            compiled_function = AOTDispatchAutograd.post_compile(
+                compiled_fw_func,
+                compiled_bw_func,
+                self.maybe_subclass_meta,
+                self.compiled_bw.num_symints_saved_for_bw_,
+                self.compiled_bw.backward_state_indices,
+                disable_amp,
+                self.indices_of_inps_to_detach,
+                None,  # lazy_backward_info
+                aot_config,
+                fw_metadata=self.runtime_metadata,
+            )
+        else:
+            compiled_function = RuntimeWrapper(
+                indices_of_inps_to_detach=self.indices_of_inps_to_detach,
+                trace_joint=False,
+                disable_amp=disable_amp,
+            )
+
+        compiled_function = post_compile(
+            self.dispatch_wrappers,
+            compiled_function,
+            aot_config,
+            runtime_metadata=self.runtime_metadata,
+        )
+
+        return compiled_function
