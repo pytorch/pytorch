ded61e1299167ae78d04c174c76e3cfbb34c425d
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-21 17:04:03 +0100

    refactor: improve scheduler logic

--- c10/util/FunctionScheduler.cpp
+++ c10/util/FunctionScheduler.cpp
@@ -11,36 +11,31 @@ namespace c10 {
 
 /* Job */
 
-bool Job::lt(const Job& a, const Job& b) {
-  return a.next_run() < b.next_run();
-}
-
 Job::Job(std::function<void()> function, std::chrono::microseconds interval)
     : _function(std::move(function)), _interval(interval) {}
-
-std::chrono::time_point<std::chrono::steady_clock> Job::next_run() const {
-  return _next_run;
-}
 
 std::chrono::microseconds Job::interval() const {
   return _interval;
 }
 
-int Job::id() const {
-  return _id;
-}
-
-void Job::set_next_run(
-    std::chrono::time_point<std::chrono::steady_clock> next_run) {
-  _next_run = next_run;
-}
-
-void Job::set_id(int id) {
-  _id = id;
-}
-
 void Job::run() const {
   _function();
+}
+
+/* Run */
+
+/* static */ bool Run::lt(Run a, Run b) {
+  return a.time() < b.time();
+}
+
+Run::Run(int job_id, std::chrono::time_point<std::chrono::steady_clock> time) : _job_id(job_id), _time(time) {}
+
+int Run::job_id() const {
+  return _job_id;
+}
+
+std::chrono::time_point<std::chrono::steady_clock> Run::time() const {
+  return _time;
 }
 
 /* FunctionScheduler */
@@ -51,35 +46,43 @@ FunctionScheduler::~FunctionScheduler() {
   stop();
 }
 
+std::chrono::microseconds FunctionScheduler::getNextWaitTime() {
+  _next_run = _queue.top(); // TODO may be removed in the meantime?
+  _queue.pop();
+
+  while (_jobs.find(_next_run.job_id()) == _jobs.end()) {
+      _next_run = _queue.top();
+      _queue.pop();
+  }
+
+  auto now = std::chrono::steady_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(_next_run.time() - now);
+}
+
 void FunctionScheduler::run() {
   while (_running) {
     if (_queue.empty()) {
       // TODO wait with mutex or check periodically?
       continue;
     }
+
+    std::chrono::microseconds wait_time = getNextWaitTime();
+    if (wait_time.count() > 0) {
+        /* TODO
+        * 1. other jobs may be scheduled with lower interval while this is waiting
+        * 2. stop() may occur while waiting
+        */
+        std::this_thread::sleep_for(wait_time);
+    }
+
     runNextJob();
   }
 }
 
 void FunctionScheduler::runNextJob() {
-  Job next_job = *_queue.begin(); // TODO may be removed in the meantime?
-  std::chrono::microseconds next_interval = getJobInterval(next_job);
-  if (next_interval.count() > 0) {
-    /* TODO
-     * 1. other jobs may be scheduled with lower interval while this is waiting
-     * 2. stop() may occur while waiting
-     */
-    std::this_thread::sleep_for(next_interval);
-  }
-
   // check if the job was removed in the meantime
-  if (_queue.begin()->id() == next_job.id())
-    next_job.run();
-}
-
-std::chrono::microseconds FunctionScheduler::getJobInterval(Job& job) const {
-  auto interval = job.next_run() - std::chrono::steady_clock::now();
-  return std::chrono::duration_cast<std::chrono::milliseconds>(interval);
+  if (_jobs.find(_next_run.job_id()) != _jobs.end())
+    _jobs.find(_next_run.job_id())->second.run();
 }
 
 int FunctionScheduler::id() {
@@ -88,13 +91,11 @@ int FunctionScheduler::id() {
 
 int FunctionScheduler::scheduleJob(Job job) {
   int job_id = id();
-  job.set_id(job_id);
+  _jobs.insert(std::make_pair(job_id, job));
 
-  if (!_running) {
-    _jobs.push_back(job);
-  } else {
-    job.set_next_run(std::chrono::steady_clock::now() + job.interval());
-    _queue.insert(job);
+  if (_running) {
+    Run run = Run(job_id, std::chrono::steady_clock::now() + job.interval());
+    _queue.push(run);
   }
 
   return job_id;
@@ -107,34 +108,14 @@ int FunctionScheduler::scheduleJob(
 }
 
 int FunctionScheduler::removeJob(int id) {
-  if (_running) {
-    for (auto it = _queue.begin(); it != _queue.end();) {
-      if (it->id() == id) {
-        _queue.erase(it);
-        return id;
-      }
-      it++;
-    }
-    return -1;
-  } else {
-    const auto end = _jobs.end();
-    const auto updated_end = _jobs.erase(
-        std::remove_if(
-            _jobs.begin(),
-            _jobs.end(),
-            [id](const Job& job) { return job.id() == id; }),
-        end);
-    return updated_end == end ? -1 : id;
-  }
+  return _jobs.erase(id) ? id : -1;
 }
 
 void FunctionScheduler::start() {
   auto now = std::chrono::steady_clock::now();
-  while (!_jobs.empty()) {
-    Job job = _jobs.back();
-    _jobs.pop_back();
-    job.set_next_run(now + job.interval());
-    _queue.insert(job);
+  for (const auto &entry : _jobs) {
+    Run run = Run(entry.first, now + entry.second.interval());
+    _queue.push(run);
   }
 
   _running = true;
@@ -145,10 +126,6 @@ void FunctionScheduler::stop() {
   _running = false;
   if (_thread.joinable())
     _thread.join();
-
-  // save unfinished jobs
-  _jobs.insert(_jobs.end(), _queue.begin(), _queue.end());
-  _queue.clear();
 }
 
 } // namespace c10
--- c10/util/FunctionScheduler.h
+++ c10/util/FunctionScheduler.h
@@ -3,44 +3,54 @@
 #include <atomic>
 #include <chrono>
 #include <functional>
-#include <set>
+#include <queue>
 #include <thread>
 #include <vector>
+#include <unordered_map>
 
 namespace c10 {
 
 class Job {
-  int _id = -1;
   std::function<void()> _function;
   std::chrono::microseconds _interval;
-  std::chrono::time_point<std::chrono::steady_clock> _next_run;
 
  public:
-  static bool lt(const Job& a, const Job& b);
-
   Job(std::function<void()> function, std::chrono::microseconds interval);
 
-  int id() const;
   std::chrono::time_point<std::chrono::steady_clock> next_run() const;
   std::chrono::microseconds interval() const;
 
-  void set_id(int id);
   void set_next_run(
       std::chrono::time_point<std::chrono::steady_clock> next_run);
 
   void run() const;
 };
 
+class Run {
+  int _job_id;
+  std::chrono::time_point<std::chrono::steady_clock>  _time;
+
+ public:
+  static bool lt(Run a, Run b);
+
+  Run(int job_id, std::chrono::time_point<std::chrono::steady_clock> time);
+
+  int job_id() const;
+  std::chrono::time_point<std::chrono::steady_clock> time() const;
+
+};
+
 class FunctionScheduler {
   int _current_id = 0;
   std::atomic_bool _running = false;
-  std::multiset<Job, decltype(&Job::lt)> _queue;
-  std::vector<Job> _jobs;
+  std::priority_queue<Run, std::vector<Run>, decltype(&Run::lt)> _queue;
+  std::unordered_map<int, Job> _jobs;
+  Run _next_run = Run(-1, std::chrono::steady_clock::now()); // TODO remove this after changing to unique_ptr
   std::thread _thread;
 
   void run();
   void runNextJob();
-  std::chrono::microseconds getJobInterval(Job& job) const;
+  std::chrono::microseconds getNextWaitTime();
   int id();
 
  public:
