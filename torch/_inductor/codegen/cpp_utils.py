c8ab9d33d8f576c2db42b4cbf784e8168ccaacad
leslie-fang-intel <leslie.fang@intel.com>
2024-05-21 20:10:42 -0700

    Update on "Add Min/Max with VecMask"
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10 voznesenskym penguinwu EikanWang Guobing-Chen zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_utils.py
+++ torch/_inductor/codegen/cpp_utils.py
@@ -247,7 +247,7 @@ def unify_mask_base_type(
     Given list of cse variables,
     Cast each to new mask base dtype and return casted cse variable.
     """
-    new_vars = tuple(
+    new_vars = (
         V.kernel.cse.generate(
             buffer,
             f"{V.kernel._get_mask_cast(var, dtype)}",
