fe58519c9b8000bf5383e826a053a65600438356
Jon Janzen <jonjanzen@meta.com>
2024-05-23 09:10:51 -0700

    Adjust paths per feedback from PyTorch devs

--- .github/scripts/sync_distributed_folder_prototype.sh
+++ .github/scripts/sync_distributed_folder_prototype.sh
@@ -2,7 +2,7 @@
 
 ORIG_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout fbcode/pytorch-stable-prototype
-for SHA in $(git log 4333e122d4b74cdf84351ed2907045c6a767b4cd..main --pretty="%h" --reverse -- torch/distributed benchmark/distributed test/distributed)
+for SHA in $(git log 4333e122d4b74cdf84351ed2907045c6a767b4cd..main --pretty="%h" --reverse -- torch/distributed torch/csrc/distributed test/distributed test/cpp/c10d benchmarks/distributed)
 do
     if git merge-base --is-ancestor $SHA HEAD || [[ $(git log --grep="(cherry picked from commit $SHA") ]]
     then
