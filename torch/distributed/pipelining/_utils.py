ed838793dfb530273fec30ea64954333672794e3
Ke Wen <kw2501@meta.com>
2024-05-23 15:50:05 -0700

    [pipelining] Remove qualname mapping (#127018)
    `QualnameMapMixin` was intended to provide a mapping from new FQN of the piped model to the FQN of the original model. It was there because previous tracers and flattening during tracing would modify the FQNs.
    
    Now that we use unflattener, the FQN of the stage modules are the same as the original FQNs. We don't need `QualnameMapMixin` any more.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127018
    Approved by: https://github.com/H-Huang

--- torch/distributed/pipelining/_utils.py
+++ torch/distributed/pipelining/_utils.py
@@ -1,6 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 import logging
-from typing import Dict, List, Optional, Tuple, Union
+from typing import List, Tuple, Union
 
 import torch
 from torch import fx
@@ -86,52 +86,6 @@ def modify_graph_op_device(
 
     if modified:
         gm.recompile()
-
-
-class QualnameMapMixin:
-    """
-    A mixin class that helps a `Pipe` object to remap its qualnames back to
-    original qualnames.
-    """
-
-    def __init__(
-        self,
-        splitter_qualname_map: Optional[Dict[str, str]] = None,
-        tracer_qualname_map: Optional[Dict[str, str]] = None,
-    ):
-        self.new_to_old_qualname_mapping: Dict[str, str] = splitter_qualname_map or {}
-        self.tracer_qualname_map = tracer_qualname_map
-
-    def remap_qualname(self, qualname: str):
-        # TODO: annoying
-        if qualname.startswith("split_gm."):
-            qualname = qualname[len("split_gm.") :]
-
-        name_before_split = None
-        if qualname in self.new_to_old_qualname_mapping:
-            name_before_split = self.new_to_old_qualname_mapping[qualname]
-        else:
-            # The qualname map does not store recursive items, thus,
-            # when passed a qualname with leaves, we need to perform longest prefix match
-            # Split from the right, one each time
-            split_names = qualname.rsplit(".", 1)
-            leaf = split_names[-1]
-            while len(split_names) > 1:
-                prefix = split_names[0]
-                if prefix in self.new_to_old_qualname_mapping:
-                    old_prefix = self.new_to_old_qualname_mapping[prefix]
-                    name_before_split = ".".join([old_prefix, leaf])
-                    break
-                split_names = prefix.rsplit(".", 1)
-                leaf = ".".join([split_names[-1], leaf])
-
-        if name_before_split is None:
-            raise RuntimeError(f"Could not find mapping for {qualname}")
-
-        if self.tracer_qualname_map is not None:
-            return self.tracer_qualname_map[name_before_split]
-        else:
-            return name_before_split
 
 
 class PipeliningShapeError(RuntimeError):
