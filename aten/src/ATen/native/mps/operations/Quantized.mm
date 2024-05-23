fe19b32ec2ec2763d4a4fd0d98b5463d4a9bee19
Manuel Candales <mcandales@meta.com>
2024-05-23 10:48:23 -0700

    Improve perf of int8pack_mm metal shader (#126987)
    Summary:
    
    Improved perf by 2.5x
    
    Using https://github.com/malfet/llm_experiments for benchmarking on Apple M1 Pro
    
    Test Plan: CI
    
    Reviewed By: malfet
    
    Differential Revision: D57732273

--- aten/src/ATen/native/mps/operations/Quantized.mm
+++ aten/src/ATen/native/mps/operations/Quantized.mm
@@ -110,33 +110,43 @@ kernel void
 int8pack_mm(constant T *A [[buffer(0)]], constant char *B [[buffer(1)]],
             constant T *scales [[buffer(2)]],
             device T *outputData [[buffer(3)]],
-            constant int3 &sizes [[buffer(4)]],
+            constant int3 &sizes [[buffer(4)]], // M, K, N
             uint2 group_index [[threadgroup_position_in_grid]],
             uint2 threadgroup_index [[thread_position_in_threadgroup]]) {
   using vecT = typename Vec4Type<T>::type;
-  const uint lda = sizes.y;
-  const uint ldc = sizes.z;
-  int out_idx = (group_index.x * blockSize + threadgroup_index.x) * 4;
-  int n = out_idx % sizes.z;
-  int m = out_idx / sizes.z;
+  const uint K = sizes.y;
+  const uint N = sizes.z;
+  int out_idx = (group_index.x * blockSize + threadgroup_index.x);
+  int n = 4 * (out_idx % (N/4));
+  int m = 4 * (out_idx / (N/4));
   // Offset pointers
-  A += m * lda;
-  B += n * lda;
-  outputData += m *ldc;
+  constant vecT *A_ptr = reinterpret_cast<constant vecT *>(A + m * K);
+  constant char4 *B_ptr = reinterpret_cast<constant char4 *>(B + n * K);
 
-  float4 rc = 0;
-  for (unsigned k = threadgroup_index.y * 4; k < sizes.y; k += 4 * blockSize) {
+  outputData += m * N;
+
+  float4x4 rc;
+  for(int j = 0; j < 4; ++j) {
+    rc[j] = float4(0.0);
+  }
+  for (unsigned k = threadgroup_index.y * 4; k < K; k += 4 * blockSize) {
     threadgroup_barrier(mem_flags::mem_none);
-    auto a_val = float4(*reinterpret_cast<constant vecT *>(A  + k));
-    float4x4 b_val;
-    for (int i = 0; i < 4; ++i) {
-      b_val[i] = float4(*reinterpret_cast<constant char4 *>(B + i * lda + k));
+
+    float4x4 b_mat;
+    for(int j = 0; j < 4; ++j) {
+      b_mat[j] = float4(B_ptr[k / 4 + j * K / 4]);
     }
-    rc += transpose(b_val) * a_val;
+
+    float4x4 a_mat;
+    for(int j = 0; j < 4; ++j) {
+      a_mat[j] = float4(A_ptr[k / 4 + j * K / 4]);
+    }
+
+    rc += transpose(b_mat) * a_mat;
   }
 
   // Accumulate results acorss SIMD group? (8 threads using vec4)
-  threadgroup float4 tgp_memory[blockSize][blockSize];
+  threadgroup float4x4 tgp_memory[blockSize][blockSize];
   tgp_memory[threadgroup_index.x][threadgroup_index.y] = rc;
   threadgroup_barrier(mem_flags::mem_threadgroup);
   if (threadgroup_index.y == 0) {
@@ -144,7 +154,13 @@ int8pack_mm(constant T *A [[buffer(0)]], constant char *B [[buffer(1)]],
       rc += tgp_memory[threadgroup_index.x][i];
     }
     *reinterpret_cast<device vecT *>(outputData + n) =
-        vecT(rc * float4(*reinterpret_cast<constant vecT *>(scales + n)));
+        vecT(rc[0] * float4(*reinterpret_cast<constant vecT *>(scales + n)));
+    *reinterpret_cast<device vecT *>(outputData + n + N) =
+        vecT(rc[1] * float4(*reinterpret_cast<constant vecT *>(scales + n)));
+    *reinterpret_cast<device vecT *>(outputData + n + 2 * N) =
+        vecT(rc[2] * float4(*reinterpret_cast<constant vecT *>(scales + n)));
+    *reinterpret_cast<device vecT *>(outputData + n + 3 * N) =
+        vecT(rc[3] * float4(*reinterpret_cast<constant vecT *>(scales + n)));
   }
 }
 
@@ -263,7 +279,7 @@ Tensor _weight_int8pack_mm_mps(const Tensor& A, const Tensor& B, const Tensor& s
       mtl_setBuffer(computeEncoder, scales, 2);
       mtl_setBuffer(computeEncoder, C, 3);
       [computeEncoder setBytes:sizes.data() length:sizeof(uint32_t) * sizes.size() atIndex:4];
-      [computeEncoder dispatchThreads:MTLSizeMake(M * N / 4, 8, 1) threadsPerThreadgroup:MTLSizeMake(8, 8, 1)];
+      [computeEncoder dispatchThreads:MTLSizeMake(M * N / 16, 8, 1) threadsPerThreadgroup:MTLSizeMake(8, 8, 1)];
 #if _CAPTURE_KERNEL
       if (getMPSProfiler().isCapturing()) {
         getMPSProfiler().stopCapture(mpsStream);
