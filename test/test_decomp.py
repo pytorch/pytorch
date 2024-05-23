5b69e05ca897d91052d059648c0b12ec1f75539d
Peter Bell <peterbell10@live.co.uk>
2024-05-23 15:58:37 +0100

    Add more sample inputs to rrelu
    [ghstack-poisoned]

--- test/test_decomp.py
+++ test/test_decomp.py
@@ -1284,14 +1284,6 @@ class HasDecompTest(TestCase):
         core_aten_ops = useful_decomps - core_decomps
         self.assertExpected("".join(sorted(op.name() + "\n" for op in core_aten_ops)))
 
-    @unittest.skipIf(IS_WINDOWS, "torch.compile not supported on windows")
-    def test_compile_rrelu(self):
-        def f(x):
-            return torch.rrelu(x)
-
-        inp = torch.rand(1, 2, 3)
-        self.assertEqual(f(inp), torch.compile(f)(inp))
-
 
 if __name__ == "__main__":
     run_tests()
