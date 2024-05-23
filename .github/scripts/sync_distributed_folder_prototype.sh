eaf5dc4a2f7c5136c99892fa8927cc61b8aafd45
Jon Janzen <jonjanzen@meta.com>
2024-05-23 13:24:47 -0700

    Ensure main branch is present

--- .github/scripts/sync_distributed_folder_prototype.sh
+++ .github/scripts/sync_distributed_folder_prototype.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -eu
+set -eoux pipefail
 
 SYNC_BRANCH=fbcode/pytorch-stable-prototype
 
+git fetch origin main
 git fetch origin "$SYNC_BRANCH"
 git checkout "$SYNC_BRANCH"
 
-for SHA in $(git log 4333e122d4b74cdf84351ed2907045c6a767b4cd..main --pretty="%h" --reverse -- torch/distributed torch/csrc/distributed test/distributed test/cpp/c10d benchmarks/distributed)
+for SHA in $(git log 4333e122d4b74cdf84351ed2907045c6a767b4cd..origin/main --pretty="%h" --reverse -- torch/distributed torch/csrc/distributed test/distributed test/cpp/c10d benchmarks/distributed)
 do
     # `git merge-base --is-ancestor` exits with code 0 if the given SHA is an ancestor, and non-0 otherwise
     if git merge-base --is-ancestor $SHA HEAD || [[ $(git log --grep="(cherry picked from commit $SHA") ]]
