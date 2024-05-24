b504ce1806f4c2145950b9d8417217c3a36f029f
Chien-Chin Huang <chienchin@fb.com>
2024-05-24 00:15:07 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/distributed/checkpoint/test_state_dict.py
+++ test/distributed/checkpoint/test_state_dict.py
@@ -424,52 +424,6 @@ class TestStateDict(DTensorTestBase, VerifyStateDictMixin):
         with self.assertRaisesRegex(RuntimeError, "Missing key"):
             set_model_state_dict(model, model_state_dict=model_state_dict)
 
-    @with_comms
-    @skip_if_lt_x_gpu(1)
-    def test_partial(self) -> None:
-        model = CompositeParamModel(device=torch.device("cuda"))
-
-        model_state_dict1 = get_model_state_dict(model)
-        model_state_dict1 = copy.deepcopy(model_state_dict1)
-        model_state_dict2 = get_model_state_dict(model, submodules={model.l})
-        model_state_dict2 = copy.deepcopy(model_state_dict2)
-        model_state_dict3 = get_model_state_dict(
-            model,
-            submodules={model.l},
-            options=StateDictOptions(keep_submodule_prefixes=False),
-        )
-        model_state_dict3 = copy.deepcopy(model_state_dict3)
-        self.assertEqual(len(model_state_dict2), 2)
-        self.assertEqual(len(model_state_dict3), 2)
-        for key in model_state_dict3.keys():
-            full_fqn = f"l.{key}"
-            value1 = model_state_dict1[full_fqn]
-            value2 = model_state_dict2[full_fqn]
-            value3 = model_state_dict3[key]
-            self.assertEqual(value1, value2)
-            self.assertEqual(value2, value3)
-
-        zeros_state_dict = {
-            k: torch.zeros_like(v) for k, v in model_state_dict1.items()
-        }
-        model.load_state_dict(zeros_state_dict)
-        set_model_state_dict(
-            model,
-            model_state_dict=model_state_dict2,
-            options=StateDictOptions(strict=False),
-        )
-        self.assertEqual(model.l.weight, model_state_dict1["l.weight"])
-        self.assertEqual(model.l.bias, model_state_dict1["l.bias"])
-
-        model.load_state_dict(zeros_state_dict)
-        set_model_state_dict(
-            model,
-            model_state_dict={model.l: model_state_dict3},
-            options=StateDictOptions(strict=False),
-        )
-        self.assertEqual(model.l.weight, model_state_dict1["l.weight"])
-        self.assertEqual(model.l.bias, model_state_dict1["l.bias"])
-
     def _test_cpu_offload_full_state_dict(
         self, optimizer_class: Type[Optimizer]
     ) -> None:
@@ -682,6 +636,19 @@ class TestStateDict(DTensorTestBase, VerifyStateDictMixin):
             },
             inner_test,
         )
+
+    @with_comms
+    @skip_if_lt_x_gpu(2)
+    def test_fsdp_root_not_initialized(self) -> None:
+        # This test verifies that FSDP root is not initialized but we should
+        # still be able to  get the state_dict without errors because
+        # fsdp_model.state_dict() will trigger the FSDP initialization.
+        device_mesh = init_device_mesh("cuda", (self.world_size,))
+        model = CompositeParamModel(device=torch.device("cuda"))
+        fsdp_model = FSDP(copy.deepcopy(model), device_mesh=device_mesh)
+        fsdp_optim = torch.optim.Adam(fsdp_model.parameters())
+        get_model_state_dict(fsdp_model)
+        get_optimizer_state_dict(fsdp_model, fsdp_optim)
 
 
 if __name__ == "__main__":
