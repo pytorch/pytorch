665637714f46f6dcc7f6d2ad860184e6335fe35a
Jane Xu <janeyx@meta.com>
2024-05-24 06:14:48 -0700

    Remove SparseAdam weird allowance of raw Tensor input (#127081)
    This continues the full deprecation after https://github.com/pytorch/pytorch/pull/114425. It's been 6 months! And I'm fairly certain no one is going to yell at me as this patch is not really used.
    
    ------
    
    # BC Breaking note
    
    As of this PR, SparseAdam will become consistent with the rest of our optimizers in that it will only accept containers of Tensors/Parameters/param groups and fully complete deprecation of this path. Hitherto, the SparseAdam constructor had allowed raw tensors as the params argument to the constructor. Now, if you write the following code, there will be an error similar to every other optim: "params argument given to the optimizer should be an iterable of Tensors or dicts"
    
    ```
    import torch
    param = torch.rand(16, 32)
    optimizer = torch.optim.SparseAdam(param)
    ```
    
    Instead you should replace the last line with
    ```
    optimizer = torch.optim.SparseAdam([param])
    ```
    to no longer error.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127081
    Approved by: https://github.com/soulitzer

--- torch/optim/optimizer.py
+++ torch/optim/optimizer.py
@@ -349,21 +349,10 @@ class Optimizer:
         self._patch_step_function()
 
         if isinstance(params, torch.Tensor):
-            if self.__class__.__name__ == "SparseAdam":
-                warnings.warn(
-                    (
-                        "Passing in a raw Tensor as ``params`` to SparseAdam "
-                        "is deprecated. In the future, this will raise an error. "
-                        "Please wrap your Tensor in an iterable instead."
-                    ),
-                    FutureWarning,
-                )
-                params = [params]
-            else:
-                raise TypeError(
-                    "params argument given to the optimizer should be "
-                    "an iterable of Tensors or dicts, but got " + torch.typename(params)
-                )
+            raise TypeError(
+                "params argument given to the optimizer should be "
+                "an iterable of Tensors or dicts, but got " + torch.typename(params)
+            )
 
         self.state: DefaultDict[torch.Tensor, Any] = defaultdict(dict)
         self.param_groups: List[Dict[str, Any]] = []
