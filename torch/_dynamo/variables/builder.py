90a8416ad0c4d1aea1ec4deb0b85838849fb0ea4
Will Feng <yf225@cornell.edu>
2024-05-27 18:59:19 -0700

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -2385,6 +2385,8 @@ class SourcelessBuilder:
             return PlacementVariable(value)
         elif DeviceMeshVariable.is_device_mesh(value):
             return DeviceMeshVariable(value)
+        elif isinstance(value, torch._ops.HigherOrderOperator):
+            return TorchHigherOrderOperatorVariable.make(value)
         elif isinstance(value, re.Pattern):
             return RegexPatternVariable(value)
         unimplemented(
