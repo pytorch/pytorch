84ab06f7444e73259f44ce3922cedd9725d786b7
Will Feng <yf225@cornell.edu>
2024-05-27 18:51:56 -0700

    Update
    [ghstack-poisoned]

--- torch/_dynamo/compiled_autograd.py
+++ torch/_dynamo/compiled_autograd.py
@@ -156,7 +156,6 @@ class AutogradCompilerInstance:
     def tensor_pre_hook(self, inputs, hook_id, i: int):
         assert self.hooks_proxy is not None
         hook = self.hooks_proxy[hook_id]  # type: ignore[index]
-        print(f"tensor_pre_hook: {hook}")
         proxy = self.proxy_call_hook(
             hook,
             inputs[i],
@@ -169,7 +168,6 @@ class AutogradCompilerInstance:
     def pre_hook(self, inputs, hook_id):
         assert self.hooks_proxy is not None
         hook = self.hooks_proxy[hook_id]  # type: ignore[index]
-        print(f"pre_hook: {hook}")
         proxies = self.proxy_call_hook(
             hook,
             inputs,
@@ -182,7 +180,6 @@ class AutogradCompilerInstance:
     def post_hook(self, outputs, inputs, hook_id):
         assert self.hooks_proxy is not None
         hook = self.hooks_proxy[hook_id]  # type: ignore[index]
-        print(f"post_hook: {hook}")
         proxies = self.proxy_call_hook(
             hook,
             outputs,
@@ -197,7 +194,6 @@ class AutogradCompilerInstance:
         assert isinstance(input, torch.Tensor)
         assert self.hooks_proxy is not None
         hook = self.hooks_proxy[hook_id]  # type: ignore[index]
-        print(f"post_acc_grad_hook: {hook}")
         proxies = self.proxy_call_hook(
             hook,
             input,
