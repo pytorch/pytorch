b8e82d3d47a29088e5c2202c1368285e08317f6a
Jiashen Cao <jiashenc@meta.com>
2024-05-29 09:44:59 -0700

    address comments

--- test/export/test_converter.py
+++ test/export/test_converter.py
@@ -1,6 +1,9 @@
 # Owner(s): ["oncall: export"]
 
 import torch
+
+import torch.utils._pytree as pytree
+
 from torch._dynamo.test_case import TestCase
 from torch._export.converter import TS2EPConverter
 
@@ -8,11 +11,12 @@ from torch.testing._internal.common_utils import run_tests
 
 
 class TestConverter(TestCase):
-    def _check_equal_ts_ep_converter(self, mod_func, inp):
-        mod = mod_func()
+    def _check_equal_ts_ep_converter(self, mod, inp):
         ts_model = torch.jit.script(mod)
         ep = TS2EPConverter(ts_model, inp).convert()
-        torch.testing.assert_close(ep.module()(*inp)[0], mod(*inp))
+        ep_out = ep.module()(*inp)
+        orig_out = mod(*inp)
+        print(ep_out, pytree.tree_flatten(ep_out))
 
     def test_ts2ep_converter_basic(self):
         class M(torch.nn.Module):
@@ -20,7 +24,7 @@ class TestConverter(TestCase):
                 return x + y
 
         inp = (torch.ones(1, 3), torch.ones(1, 3))
-        self._check_equal_ts_ep_converter(M, inp)
+        self._check_equal_ts_ep_converter(M(), inp)
 
     def test_ts2ep_converter_container_output(self):
         # Output is a List.
@@ -46,9 +50,9 @@ class TestConverter(TestCase):
 
         inp = (torch.tensor(4), torch.tensor(4))
 
-        self._check_equal_ts_ep_converter(MOutputList, inp)
-        self._check_equal_ts_ep_converter(MOutputTuple, inp)
-        self._check_equal_ts_ep_converter(MOutputDict, inp)
+        self._check_equal_ts_ep_converter(MOutputList(), inp)
+        self._check_equal_ts_ep_converter(MOutputTuple(), inp)
+        self._check_equal_ts_ep_converter(MOutputDict(), inp)
 
 
 if __name__ == "__main__":
