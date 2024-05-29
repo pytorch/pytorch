b38bd51567d2ee3414b5e0e1ecc3b8e909b15585
Richard Barnes <rbarnes@meta.com>
2024-05-29 15:51:25 -0700

    Add new convenience checks for PyTorch operators/kernels (#127469)
    Summary:
    
    These checks generate enhanced error messages and are self-documenting.
    
    Test Plan: Sandcastle
    
    Differential Revision: D57938595

--- c10/util/Exception.h
+++ c10/util/Exception.h
@@ -1,6 +1,6 @@
-#ifndef C10_UTIL_EXCEPTION_H_
-#define C10_UTIL_EXCEPTION_H_
+#pragma once
 
+#include <ATen/ATen.h>
 #include <c10/macros/Export.h>
 #include <c10/macros/Macros.h>
 #include <c10/util/Backtrace.h>
@@ -10,6 +10,7 @@
 #include <cstdint>
 #include <exception>
 #include <memory>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -711,4 +712,281 @@ inline void deprecated_AT_ASSERTM() {}
     C10_EXPAND_MSVC_WORKAROUND(TORCH_CHECK(false, ::c10::str(__VA_ARGS__))); \
   } while (false)
 
-#endif // C10_UTIL_EXCEPTION_H_
+
+namespace c10::detail {
+
+inline bool torch_tensor_on_cpu_check(const at::Tensor& ten) {
+  return ten.is_cpu();
+}
+
+inline bool torch_tensor_on_cpu_check(const std::optional<at::Tensor>& ten) {
+  return !ten.has_value() || torch_tensor_on_cpu_check(ten.value());
+}
+
+inline std::optional<int64_t> get_device_index_from_tensor(
+    const at::Tensor& ten) {
+  return {ten.device().index()};
+}
+
+inline std::optional<int64_t> get_device_index_from_tensor(
+    const std::optional<at::Tensor>& ten) {
+  if (ten) {
+    return {ten->device().index()};
+  } else {
+    return {};
+  }
+}
+
+inline std::string torch_tensor_device_name(const at::Tensor& ten) {
+  return c10::DeviceTypeName(ten.device().type());
+}
+
+inline std::string torch_tensor_device_name(
+    const std::optional<at::Tensor>& ten) {
+  if (ten.has_value()) {
+    return torch_tensor_device_name(ten.value());
+  } else {
+    return "N/A";
+  }
+}
+
+inline bool torch_tensor_on_same_device_check(
+    const at::Tensor& ten1,
+    const at::Tensor& ten2) {
+  return ten1.get_device() == ten2.get_device();
+}
+
+inline bool torch_tensor_on_same_device_check(
+    const at::Tensor& ten1,
+    const std::optional<at::Tensor>& ten2) {
+  return !ten2.has_value() || ten1.get_device() == ten2->get_device();
+}
+
+inline bool torch_tensor_undefined(const at::Tensor& ten) {
+  return ten.defined();
+}
+
+inline bool torch_tensor_undefined(const std::optional<at::Tensor>& ten) {
+  return !ten.has_value() || torch_tensor_undefined(ten.value());
+}
+
+inline bool torch_tensor_on_cuda_gpu_check(const at::Tensor& ten) {
+  return ten.is_cuda();
+}
+
+inline bool torch_tensor_on_cuda_gpu_check(
+    const std::optional<at::Tensor>& ten) {
+  return !ten.has_value() || torch_tensor_on_cuda_gpu_check(ten.value());
+}
+
+inline bool torch_tensor_empty_or_on_cuda_gpu_check(const at::Tensor& ten) {
+  return (ten.numel() == 0) || ten.is_cuda();
+}
+
+inline bool torch_tensor_empty_or_on_cuda_gpu_check(
+    const std::optional<at::Tensor>& ten) {
+  return !ten.has_value() ||
+      torch_tensor_empty_or_on_cuda_gpu_check(ten.value());
+}
+
+inline bool torch_tensor_empty_or_on_cpu_check(const at::Tensor& ten) {
+  return (ten.numel() == 0) || ten.is_cpu();
+}
+
+inline bool torch_tensor_empty_or_on_cpu_check(
+    const std::optional<at::Tensor>& ten) {
+  return !ten.has_value() || torch_tensor_empty_or_on_cpu_check(ten.value());
+}
+
+template <typename... Tensors>
+std::string tensor_on_same_gpu_if_not_optional_check(
+    const std::string& var_names_str,
+    const Tensors&... tensors) {
+  std::optional<int64_t> gpu_index;
+  bool on_same_gpu = true;
+
+  // Collect the GPU index of the first non-empty optional tensor and make sure
+  // that all tensors are on this same index.
+  (
+      [&](const auto& tensor) {
+        if (!torch_tensor_undefined(tensor)) {
+          return;
+        }
+        if (!torch_tensor_on_cuda_gpu_check(tensor)) {
+          on_same_gpu = false;
+          return;
+        }
+        const auto my_gpu_index = get_device_index_from_tensor(tensor);
+        if (my_gpu_index) {
+          if (!gpu_index) {
+            gpu_index = my_gpu_index;
+          } else if (*gpu_index != my_gpu_index) {
+            on_same_gpu = false;
+          }
+        }
+      }(tensors),
+      ...);
+
+  if (on_same_gpu) {
+    return "";
+  }
+
+  std::vector<std::string> var_names;
+  {
+    std::string temp = "";
+    for (const auto& x : var_names_str) {
+      if (x == ',') {
+        var_names.push_back(temp);
+        temp = "";
+      } else {
+        temp.push_back(x);
+      }
+    }
+    var_names.push_back(temp);
+  }
+
+  // Not all the tensors on a GPU or on the same GPU, generate a message.
+  std::string msg = "Not all tensors were on the same GPU: ";
+  size_t current_idx = 0;
+  (
+      [&](const auto& tensor) {
+        if (current_idx > 0) {
+          msg.append(", ");
+        }
+        msg.append(
+            var_names.at(current_idx++) + "(" +
+            torch_tensor_device_name(tensor));
+        const auto gpu_device_index = get_device_index_from_tensor(tensor);
+        if (gpu_device_index) {
+          msg.append(":" + std::to_string(*gpu_device_index));
+        }
+        msg.append(")");
+      }(tensors),
+      ...);
+
+  return msg;
+}
+
+} // namespace c10::detail
+
+
+#define TORCH_TENSOR_ON_CPU(x)                                      \
+  TORCH_CHECK(                                                \
+      c10::detail::torch_tensor_on_cpu_check(x),                           \
+      #x " must be a CPU tensor; it is currently on device ", \
+      c10::detail::torch_tensor_device_name(x))
+
+#define TORCH_TENSOR_EMPTY_OR_ON_CPU(x)                                      \
+  TORCH_CHECK(                                                         \
+      c10::detail::torch_tensor_empty_or_on_cpu_check(x),                           \
+      #x " must be empty or a CPU tensor; it is currently on device ", \
+      c10::detail::torch_tensor_device_name(x))
+
+#define TORCH_TENSOR_ON_CUDA_GPU(x)                                  \
+  TORCH_CHECK(                                                 \
+      c10::detail::torch_tensor_on_cuda_gpu_check(x),                       \
+      #x " must be a CUDA tensor; it is currently on device ", \
+      c10::detail::torch_tensor_device_name(x))
+
+#define TORCH_TENSOR_EMPTY_OR_ON_CUDA_GPU(x)                                  \
+  TORCH_CHECK(                                                          \
+      c10::detail::torch_tensor_empty_or_on_cuda_gpu_check(x),                       \
+      #x " must be empty or a CUDA tensor; it is currently on device ", \
+      c10::detail::torch_tensor_device_name(x))
+
+#define TORCH_TENSORS_EMPTY_OR_ON_SAME_DEVICE(x, y)                           \
+  TORCH_CHECK(                                                          \
+      c10::detail::torch_tensor_on_same_device_check(x, y) || (x.numel() == 0),      \
+      #x " must be empty or a CUDA tensor; it is currently on device ", \
+      c10::detail::torch_tensor_device_name(x))
+
+#define TORCH_TENSORS_ON_SAME_DEVICE(x, y)                                       \
+  TORCH_CHECK(                                                             \
+      c10::detail::torch_tensor_on_same_device_check(x, y),                             \
+      #x " must be on the same device as " #y "! " #x " is currently on ", \
+      c10::detail::torch_tensor_device_name(x),                                         \
+      #y " is currently on ",                                              \
+      torch_tensor_device_name(y))
+
+#define TORCH_TENSORS_HAVE_SAME_TYPE(x, y)                       \
+  TORCH_CHECK(                                             \
+      (x).dtype() == (y).dtype(),                          \
+      #x " must have the same type as " #y " types were ", \
+      (x).dtype().name(),                                  \
+      " and ",                                             \
+      (y).dtype().name())
+
+#define TORCH_TENSOR_NDIM_EQUALS(ten, dims)      \
+  TORCH_CHECK(                             \
+      (ten).ndimension() == (dims),        \
+      "Tensor '" #ten "' must have " #dims \
+      " dimension(s). "                    \
+      "Found ",                            \
+      (ten).ndimension())
+
+#define TORCH_TENSOR_CONTIGUOUS(x) \
+  TORCH_CHECK((x).is_contiguous(), #x " must be contiguous")
+
+#define TORCH_TENSOR_CONTIGUOUS_AND_ON_CPU(x) \
+  TENSOR_ON_CPU(x);                     \
+  TENSOR_CONTIGUOUS(x)
+
+#define TORCH_TENSOR_CONTIGUOUS_AND_ON_CUDA_GPU(x) \
+  TENSOR_ON_CUDA_GPU(x);                     \
+  TENSOR_CONTIGUOUS(x)
+
+#define TORCH_TENSOR_NDIM_IS_GE(ten, dims)         \
+  TORCH_CHECK(                               \
+      (ten).dim() >= (dims),                 \
+      "Tensor '" #ten "' must have >=" #dims \
+      " dimension(s). "                      \
+      "Found ",                              \
+      (ten).ndimension())
+
+#define TORCH_TENSOR_TYPE_MUST_BE(ten, typ)                                      \
+  TORCH_CHECK(                                                             \
+      (ten).scalar_type() == typ,                                          \
+      "Tensor '" #ten "' must have scalar type " #typ " but it had type ", \
+      (ten).dtype().name())
+
+#define TORCH_TENSOR_NDIM_EXCEEDS(ten, dims)               \
+  TORCH_CHECK(                                       \
+      (ten).dim() > (dims),                          \
+      "Tensor '" #ten "' must have more than " #dims \
+      " dimension(s). "                              \
+      "Found ",                                      \
+      (ten).ndimension())
+
+#define TORCH_TENSORS_HAVE_SAME_NUMEL(x, y)                                  \
+  TORCH_CHECK(                                                         \
+      (x).numel() == (y).numel(),                                      \
+      #x " must have the same number of elements as " #y " They had ", \
+      (x).numel(),                                                     \
+      " and ",                                                         \
+      (y).numel())
+
+#define TORCH_TENSOR_NUMEL_IS_GT(ten, num)                \
+  TORCH_CHECK(                                      \
+      (ten).numel() > (num),                        \
+      "Tensor '" #ten "' must have more than " #num \
+      " element(s). "                               \
+      "Found ",                                     \
+      (ten).numel())
+
+#define TORCH_TENSORS_HAVE_SAME_SYM_NUMEL(x, y)                              \
+  TORCH_SYM_CHECK(                                                     \
+      x.sym_numel().sym_eq(y.sym_numel()),                             \
+      #x " must have the same number of elements as " #y " They had ", \
+      (x).sym_numel(),                                                 \
+      " and ",                                                         \
+      (y).sym_numel())
+
+// Generate constexpr array of variable names to improve diagnostic output and
+// raise a message if any non-empty tensor is not on a GPU or not on the same
+// GPU as all the other non-empty tensors.
+#define TORCH_TENSORS_ON_SAME_CUDA_GPU_IF_NOT_OPTIONAL(...)                        \
+  do {                                                                       \
+    const auto tensors_on_same_gpu =                                         \
+        c10::detail::tensor_on_same_gpu_if_not_optional_check(#__VA_ARGS__, __VA_ARGS__); \
+    TORCH_CHECK(tensors_on_same_gpu.empty(), tensors_on_same_gpu);           \
+  } while (false)
