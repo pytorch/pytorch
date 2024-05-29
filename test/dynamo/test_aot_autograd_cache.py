1b7f2f594425e51c996792f7cf46a4efce427643
James Wu <jjwu@meta.com>
2024-05-29 11:13:17 -0700

    Update
    [ghstack-poisoned]

--- test/dynamo/test_aot_autograd_cache.py
+++ test/dynamo/test_aot_autograd_cache.py
@@ -16,8 +16,18 @@ from torch._functorch._aot_autograd.autograd_cache import (
 )
 from torch._functorch._aot_autograd.schemas import AOTConfig
 from torch._inductor import config as inductor_config
+from torch.testing._internal.inductor_utils import (
+    GPU_TYPE,
+    HAS_GPU,
+)
+from torch.testing._internal.common_utils import (
+    instantiate_parametrized_tests,
+    parametrize,
+)
+from torch.testing._internal.common_cuda import SM80OrLater
+from torch.testing._internal.common_device_type import largeTensorTest
 
-
+@instantiate_parametrized_tests
 class AOTAutogradCacheTests(torch._dynamo.test_case.TestCase):
     def setUp(self):
         """
@@ -179,6 +189,69 @@ class AOTAutogradCacheTests(torch._dynamo.test_case.TestCase):
         self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], 1)
         self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], 1)
         self.assertEqual(counters["aot_autograd"]["autograd_cache_saved"], 1)
+
+
+    @largeTensorTest("64GB", device=GPU_TYPE)
+    @parametrize("device", (GPU_TYPE,))
+    @parametrize("dtype", (torch.float16, torch.bfloat16))
+    @inductor_config.patch("fx_graph_cache", True)
+    @functorch_config.patch({"enable_autograd_cache": True})
+    def test_autograd_inductor_guards(self, device, dtype):
+        """
+        Tests that functions that would add inductor guards are cached properly
+        """
+        if device == GPU_TYPE and not HAS_GPU:
+            raise unittest.SkipTest(f"requires {GPU_TYPE}")
+        if device == "cuda" and dtype == torch.bfloat16 and not SM80OrLater:
+            raise unittest.SkipTest("requires CUDA SM80 or later")
+
+        def fn(x, y):
+            return (x + x, y + y)
+
+        compiled_fn = torch.compile(fn, dynamic=True)
+
+        # Iterate over different shapes, varying whether the total
+        # size is below or above int32. For each combination, we expect
+        # different guards around whether the symbolic sizes do or do
+        # not exceed int32.
+        shapes = (
+            ((5, 6), (7, 8)),
+            ((5, 6), (47000, 47001)),
+            ((47000, 47001), (5, 6)),
+        )
+        expected_hits = expected_misses = expected_saves = 0
+        for a_shape, b_shape in shapes:
+            a = torch.rand(a_shape, device=device, dtype=dtype)
+            b = torch.rand(b_shape, device=device, dtype=dtype)
+
+            # AVOID a dynamo reset here. We expect guards to have been
+            # added that will be violated with the new shape. We should
+            # see a recompilation (along with a cache miss).
+            res1 = compiled_fn(a, b)
+            # A first call should miss in the cache.
+            # NOTE: Currently, this cache miss is *not* due to guards,
+            # but instead because the AOTAutogradCache key calculation specializes on input shapes.
+            # Once we allow tensors with symints as part of the cache key calculation, it will
+            # instead cache miss because of guard failure.
+            expected_misses += 1
+            expected_saves += 1
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], expected_misses)
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], expected_hits)
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_saved"], expected_saves)
+
+            # A second call should hit. (First reset so in-memory guards
+            # don't prevent compilation).
+
+            # Now clear dynamo and we should see a cache hit
+            # This should populate guards to dynamo's cache, so that a subsequent run with a different
+            # shape will still trigger a second call to autograd_cache.
+            self._clear_dynamo_and_codecache()
+            res2 = compiled_fn(a, b)
+            expected_hits += 1
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], expected_misses)
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], expected_hits)
+            self.assertEqual(counters["aot_autograd"]["autograd_cache_saved"], expected_saves)
+
 
 
 @inductor_config.patch("fx_graph_cache", True)
