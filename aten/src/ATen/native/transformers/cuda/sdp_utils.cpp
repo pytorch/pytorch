291fdff635161bb12ac69a57f40671ab9feed239
Eddie Yan <eddiey@nvidia.com>
2024-05-30 19:58:28 +0000

    lint

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -526,9 +526,9 @@ bool can_use_cudnn_attention(const sdp_params& params, bool debug) {
           check_all_tensors_on_device,
           check_tensor_shapes,
           check_cudnn_tensor_shapes,
-	  check_cudnn_deterministic,
-	  check_nonzero_sequence_lengths_dense,
-	  check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim>*/>,
+          check_cudnn_deterministic,
+          check_nonzero_sequence_lengths_dense,
+          check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim>*/>,
           // check_cudnn_layout,
           // check_is_causal,
           check_dtypes_low_precision);
