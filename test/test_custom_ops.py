38e2e4004ec05aafc8baf3650e5fe6e194edd5b7
James Wu <jjwu@meta.com>
2024-05-30 11:07:44 -0700

    Update
    [ghstack-poisoned]

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -1,4 +1,4 @@
-# Owner(s): ["module: custom-operators"]
+# Owner(s): ["module: custom operators"]
 
 from torch.testing._internal.common_utils import *  # noqa: F403
 from torch.testing._internal.common_device_type import *  # noqa: F403
