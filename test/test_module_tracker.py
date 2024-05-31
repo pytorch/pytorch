e7a72d45039039f8398ab653819a115d4bbc6e43
Sanket Jayant Purandare <sanketpurandare@meta.com>
2024-05-31 12:14:50 -0700

    Added tests for user hooks

--- test/test_module_tracker.py
+++ test/test_module_tracker.py
@@ -77,7 +77,62 @@ class TestModuleTracker(TestCase):
             self.assertEqual(tracker.parents, {"Global"})
 
     def test_user_hooks(self):
-        
+        class Bar(torch.nn.Module):
+            def __init__(self):
+                super(__class__, self).__init__()
+                self.foo = torch.nn.Linear(10, 10)
+
+            def forward(self, x):
+                return self.foo(x).relu_()
+
+        mt = ModuleTracker()
+        test_op = []
+
+        def hook(mod, mt, hook_name):
+            mfqn = mt.get_known_fqn(mod) if mod is not None else None
+            test_op.append((hook_name, mfqn, mfqn in mt.parents, mt.is_bw))
+
+        mod = Bar()
+
+        mt.register_user_hooks(
+            lambda m, inp: hook(m, mt, "pre_fw"),
+            lambda m, inp, op: hook(m, mt, "post_fw"),
+            lambda m, gop: hook(m, mt, "pre_bw"),
+            lambda m, ginp: hook(m, mt, "post_bw"),
+        )
+        with mt:
+            mod(torch.rand(10, 10, requires_grad=True)).sum().backward()
+        expected_op = [
+            ("pre_fw", "Bar", True, False),
+            ("pre_fw", "Bar.foo", True, False),
+            ("post_fw", "Bar.foo", True, False),
+            ("post_fw", "Bar", True, False),
+            ("pre_bw", "Bar", True, True),
+            ("pre_bw", "Bar.foo", True, True),
+            ("post_bw", "Bar", True, True),
+            ("post_bw", "Bar.foo", True, True),
+        ]
+        self.assertEqual(test_op, expected_op)
+
+        with self.assertRaises(AssertionError):
+            mt.register_user_hooks(lambda x, y: x, None, None, None)
+
+        test_op.clear()
+        with mt:
+            loss = mod(torch.rand(10, 10, requires_grad=True)).sum()
+            del mod
+            loss.backward()
+        expected_op = [
+            ("pre_fw", "Bar", True, False),
+            ("pre_fw", "Bar.foo", True, False),
+            ("post_fw", "Bar.foo", True, False),
+            ("post_fw", "Bar", True, False),
+            ("pre_bw", None, False, True),
+            ("pre_bw", None, False, True),
+            ("post_bw", None, False, True),
+            ("post_bw", None, False, True),
+        ]
+        self.assertEqual(test_op, expected_op)
 
 
 if __name__ == "__main__":
