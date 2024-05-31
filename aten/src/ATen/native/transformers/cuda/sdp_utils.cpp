dea0914f95ab3310cc8023131d19d33ae7f64c6d
Eddie Yan <eddiey@nvidia.com>
2024-05-31 22:15:07 +0000

    move test order for older cudnn

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -522,14 +522,14 @@ bool can_use_cudnn_attention(const sdp_params& params, bool debug) {
   constexpr auto general_constraints =
       array_of<bool (*)(sdp_params const&, bool)>(
           check_for_nested_inputs,
-          check_runtime_disabled_cudnn,
-          check_cudnn_hardware_support,
+          check_nonzero_sequence_lengths_dense,
+          check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim>*/>,
           check_all_tensors_on_device,
           check_tensor_shapes,
           check_cudnn_tensor_shapes,
+          check_runtime_disabled_cudnn,
+          check_cudnn_hardware_support,
           check_cudnn_deterministic,
-          check_nonzero_sequence_lengths_dense,
-          check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim>*/>,
           // check_cudnn_layout,
           // check_is_causal,
           check_dtypes_low_precision);
