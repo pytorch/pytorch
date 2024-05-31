033e7330211e9c9f85cd7745c221aaccd02c5c76
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-05-31 19:47:24 +0000

    Revert "[BE] wrap deprecated function/class with `typing_extensions.deprecated` (#126898)"
    This reverts commit 749a132fb0a8325cbad4734a563aa459ca611991.
    
    Reverted https://github.com/pytorch/pytorch/pull/126898 on behalf of https://github.com/fbgheith due to switching typing-extensions=4.3.0 to 4.9.0 causes internal failure ([comment](https://github.com/pytorch/pytorch/pull/126898#issuecomment-2142884456))

--- torch/distributed/_sharding_spec/__init__.py
+++ torch/distributed/_sharding_spec/__init__.py
@@ -5,15 +5,10 @@ import torch
 import warnings
 
 from torch.distributed._shard.sharding_spec import *  # noqa: F403
-
-with warnings.catch_warnings():
-    warnings.simplefilter("always")
-    warnings.warn(
-        "`torch.distributed._sharding_spec` will be deprecated, "
-        "use `torch.distributed._shard.sharding_spec` instead",
-        DeprecationWarning,
-        stacklevel=2,
-    )
+warnings.warn(
+    "torch.distributed._sharding_spec will be deprecated, use torch.distributed._shard.sharding_spec instead",
+    DeprecationWarning
+)
 
 import torch.distributed._shard.sharding_spec as _sharding_spec
 sys.modules['torch.distributed._sharding_spec'] = _sharding_spec
