039469917769bdce6da5f661f579f1bbc93b9e36
Fuzzkatt <zonghan2000@gmail.com>
2024-05-31 17:36:07 -0700

    adjust thresholds for gluon_inception_v3, beit_base_patch16_224, phlippe_resnet

--- benchmarks/dynamo/timm_models.py
+++ benchmarks/dynamo/timm_models.py
@@ -69,7 +69,9 @@ BATCH_SIZE_DIVISORS = {
 }
 
 REQUIRE_HIGHER_TOLERANCE = {
+    "beit_base_patch16_224",
     "fbnetv3_b",
+    "gluon_inception_v3",
     "gmixer_24_224",
     "hrnet_w18",
     "inception_v3",
