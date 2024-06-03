83bcae0dac158f44c1acef5f85f94b4d7a72fdf2
Tom Ritchford <tom@swirly.com>
2024-06-03 09:15:12 +0000

    Add decompositions for copy variants of view ops
    [ghstack-poisoned]

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -15,6 +15,7 @@ import torch
 
 import torch._prims as prims
 import torch._prims_common as utils
+import torch.utils._pytree as pytree
 from torch import sym_float, sym_int
 from torch._prims_common import (
     BoolLike,
@@ -232,10 +233,12 @@ __all__ = [
     # View & Shape Ops
     #
     "alias",
+    "alias_copy",
     "atleast_1d",
     "atleast_2d",
     "atleast_3d",
     "as_strided",
+    "as_strided_copy",
     "as_strided_scatter",
     "block_diag",
     "broadcast_shapes",
@@ -256,6 +259,7 @@ __all__ = [
     "dstack",
     "expand",
     "expand_as",
+    "expand_copy",
     "flatten",
     "flip",
     "fliplr",
@@ -269,6 +273,7 @@ __all__ = [
     "native_group_norm",
     "native_layer_norm",
     "permute",
+    "permute_copy",
     "ravel",
     "repeat",
     "reshape",
@@ -279,21 +284,27 @@ __all__ = [
     "stack",
     "swap_axes",  # alias for transpose
     "squeeze",
+    "squeeze_copy",
     "t",
+    "t_copy",
     "T",
     "take_along_dim",
     "tensor_split",
     "transpose",
+    "transpose_copy",
     "unfold",
     "unfold_copy",
     "unsqueeze",
+    "unsqueeze_copy",
     "view",
     "view_as",
+    "view_copy",
     "vsplit",
     "vstack",
     "view_as_complex",
     "unflatten",
     "unbind",
+    "unbind_copy",
     "triu",
     "tril",
     "triu_indices",
@@ -2189,17 +2200,21 @@ def _make_copy_from_view(fn):
     Given a view function (e.g. torch.diagonal) generates its copy variant (e.g. torch.diagonal_copy)
     """
     name = fn.__name__
-    fn = out_wrapper()(fn)
-
-    def _fn(*args, out=None, **kwargs):
-        result = fn(*args, out=out, **kwargs)
-        if out is None:
-            return result.clone(memory_format=torch.contiguous_format)
-        return result
-
     copy_name = f"{name}_copy"
+    op_copy = getattr(aten, copy_name)
+
+    @wraps(fn)
+    def _fn(*args, **kwargs):
+        return pytree.tree_map(
+            lambda x: x.clone(memory_format=torch.contiguous_format),
+            fn(*args, **kwargs),
+        )
+
+    if "out" in op_copy.overloads():
+        _fn = out_wrapper()(_fn)
+
     _fn.__name__ = copy_name
-    _fn = register_decomposition(getattr(aten, copy_name))(_fn)
+    _fn = register_decomposition(op_copy)(_fn)
     return _fn
 
 
@@ -4293,9 +4308,6 @@ def diagonal(
     return result
 
 
-diagonal_copy = _make_copy_from_view(diagonal)
-
-
 @register_decomposition(aten.diag_embed)
 @out_wrapper()
 def diag_embed(
@@ -4476,14 +4488,6 @@ def unfold(
         self.shape, self.stride(), dimension, size, step
     )
     return self.as_strided(shape, strides)
-
-
-@register_decomposition(aten.unfold_copy)
-@out_wrapper()
-def unfold_copy(self: TensorLikeType, dimension: int, size: int, step: int):
-    return self.unfold(dimension, size, step).clone(
-        memory_format=torch.contiguous_format
-    )
 
 
 def _cumsumprod_common(
@@ -6273,6 +6277,20 @@ exponential_ = _make_inplace(exponential)
 geometric_ = _make_inplace(geometric)
 log_normal_ = _make_inplace(log_normal)
 zero_ = _make_inplace(zero)
+
+# make copy variants of ops that returns views
+alias_copy = _make_copy_from_view(alias)
+as_strided_copy = _make_copy_from_view(as_strided)
+diagonal_copy = _make_copy_from_view(diagonal)
+expand_copy = _make_copy_from_view(expand)
+permute_copy = _make_copy_from_view(permute)
+squeeze_copy = _make_copy_from_view(squeeze)
+t_copy = _make_copy_from_view(t)
+transpose_copy = _make_copy_from_view(transpose)
+unbind_copy = _make_copy_from_view(unbind)
+unfold_copy = _make_copy_from_view(unfold)
+unsqueeze_copy = _make_copy_from_view(unsqueeze)
+view_copy = _make_copy_from_view(view)
 
 
 # xref: isStorage in torch/csrc/DynamicTypes.cpp
--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -12246,6 +12246,12 @@ op_db: List[OpInfo] = [
            sample_inputs_func=sample_inputs_addcmul_addcdiv,
            reference_inputs_func=partial(
                reference_inputs_elementwise_ternary, sample_inputs_func=reference_inputs_addcmul_addcdiv)),
+    UnaryUfuncInfo('alias_copy',
+           dtypes=all_types_and_complex_and(torch.bool, torch.half, torch.bfloat16),
+           dtypesIfCUDA=all_types_and_complex_and(torch.chalf, torch.bool, torch.half, torch.bfloat16),
+           supports_forward_ad=True,
+           supports_fwgrad_bwgrad=True,
+           supports_out=True,),
     UnaryUfuncInfo('asin',
                    aliases=('arcsin', ),
                    ref=np.arcsin,
@@ -14408,6 +14414,27 @@ op_db: List[OpInfo] = [
                DecorateInfo(unittest.expectedFailure, 'TestMeta', 'test_dispatch_meta_inplace'),
                DecorateInfo(unittest.expectedFailure, 'TestMeta', 'test_dispatch_symbolic_meta_inplace'),
                DecorateInfo(unittest.expectedFailure, 'TestMeta', 'test_dispatch_symbolic_meta_inplace_all_strides'),
+           )),
+    OpInfo('as_strided_copy',
+           dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
+           supports_out=True,
+           supports_forward_ad=True,
+           supports_fwgrad_bwgrad=True,
+           sample_inputs_func=sample_inputs_as_strided,
+           skips=(
+               # Note: This xfail is fine -- it's inherent to how as_strided works
+               DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_noncontiguous_samples'),
+               # AssertionError: False is not true : Scalars failed to compare as equal!
+               DecorateInfo(unittest.skip("Errors when storage_offset is included"),
+                            'TestCommon', 'test_variant_consistency_eager'),
+               # Not close
+               DecorateInfo(unittest.skip("Errors when storage_offset is included"),
+                            'TestCommon', 'test_complex_half_reference_testing'),
+               # Not close
+               DecorateInfo(unittest.skip("Errors when storage_offset is included"), 'TestMathBits', 'test_conj_view'),
+               DecorateInfo(unittest.skip("Errors when storage_offset is included"), 'TestMathBits', 'test_neg_view'),
+               DecorateInfo(unittest.skip("Numerous errors"), 'TestFwdGradients'),
+               DecorateInfo(unittest.skip("Numerous errors"), 'TestBwdGradients'),
            )),
     OpInfo('as_strided_scatter',
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16, torch.chalf),
@@ -19064,6 +19091,16 @@ op_db: List[OpInfo] = [
            supports_gradgrad=True,
            supports_out=False,
            ),
+    OpInfo('unbind_copy',
+           dtypes=all_types_and_complex_and(torch.complex32, torch.bool, torch.float16, torch.bfloat16),
+           ref=reference_unbind,
+           sample_inputs_func=sample_inputs_unbind,
+           error_inputs_func=error_inputs_unbind,
+           supports_forward_ad=True,
+           supports_fwgrad_bwgrad=True,
+           supports_gradgrad=True,
+           supports_out=True,
+           ),
     OpInfo('vstack',
            aliases=('row_stack',),
            dtypes=all_types_and_complex_and(torch.complex32, torch.bool, torch.float16, torch.bfloat16),
@@ -21173,6 +21210,14 @@ python_ref_db = [
         torch_opinfo_name="lerp",
     ),
     PythonRefInfo(
+        "_refs.alias_copy",
+        torch_opinfo_name="alias_copy",
+    ),
+    PythonRefInfo(
+        "_refs.view_copy",
+        torch_opinfo_name="view_copy",
+    ),
+    PythonRefInfo(
         "_refs.ones",
         torch_opinfo_name="ones",
         skips=(
@@ -23221,6 +23266,18 @@ python_ref_db = [
         ),
     ),
     PythonRefInfo(
+        "_refs.as_strided_copy",
+        torch_opinfo_name="as_strided_copy",
+        # FIXME: doesn't support chalf
+        dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16),
+        skips=(
+            # cloned_mutable_input.is_same(returned_output) INTERNAL ASSERT FAILED
+            DecorateInfo(unittest.skip("Errors when storage_offset is included"), 'TestMathBits', 'test_neg_view'),
+            DecorateInfo(unittest.skip("Errors when storage_offset is included"), 'TestMathBits', 'test_conj_view'),
+            DecorateInfo(unittest.skip("Errors when storage_offset is included"), 'TestMathBits', 'test_neg_conj_view'),
+        ),
+    ),
+    PythonRefInfo(
         "_refs.as_strided_scatter",
         torch_opinfo_name="as_strided_scatter",
         # returns a view of an intermediate tensor (as_strided)
@@ -23517,6 +23574,10 @@ python_ref_db = [
     PythonRefInfo(
         "_refs.unbind",
         torch_opinfo_name="unbind",
+    ),
+    PythonRefInfo(
+        "_refs.unbind_copy",
+        torch_opinfo_name="unbind_copy",
     ),
     #
     # Reduction Reference OpInfos
