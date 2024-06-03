ec7a0a6f7c7b4f1c220b4ff4fd99615bfddf9689
atalman <atalman@fb.com>
2024-06-03 10:20:28 -0700

    Revert "[Inductor] FlexAttention backward kernel optimization (#127208)"
    This reverts commit f7171313abf14d9501a330457140b2f8a01c9985.

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -310,10 +310,7 @@ class TritonTemplateKernel(TritonKernel):
         Args:
             subgraph_number (int): The index of the subgraph in self.subgraphs
         """
-        num = 0
-        while f"mod_{subgraph_number}_{num}" in self.subgraph_bodies:
-            num += 1
-        with self.create_subgraph_body(f"mod_{subgraph_number}_{num}"):
+        with self.create_subgraph_body(f"modification_{subgraph_number}"):
             assert isinstance(subgraph_number, int)
             assert isinstance(self.subgraphs, list)
             assert (
