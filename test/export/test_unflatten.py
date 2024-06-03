d77d298b146bbc274243e0cc908277c9f92cebc6
Pian Pawakapan <pianpwk@meta.com>
2024-06-03 15:39:16 -0700

    uncomment
    [ghstack-poisoned]

--- test/export/test_unflatten.py
+++ test/export/test_unflatten.py
@@ -43,7 +43,7 @@ from torch.testing._internal.common_utils import (
     TestCase,
 )
 
-# from torch.testing._internal.torchbind_impls import init_torchbind_implementations
+from torch.testing._internal.torchbind_impls import init_torchbind_implementations
 from torch.utils._pytree import (
     LeafSpec,
     tree_flatten,
