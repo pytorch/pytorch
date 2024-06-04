47d86948e9614fe30a792228dec37646be61565e
joydddd <joydong@meta.com>
2024-06-04 13:56:10 -0700

    Remove Eager flex decoding Implementation

---
+++ joy-dev/triton_flexdec.py
@@ -0,0 +1,268 @@
+
+# AOT ID: ['0_inference']
+from ctypes import c_void_p, c_long
+import torch
+import math
+import random
+import os
+import tempfile
+from math import inf, nan
+from torch._inductor.hooks import run_intermediate_hooks
+from torch._inductor.utils import maybe_profile
+from torch._inductor.codegen.memory_planning import _align as align
+
+from torch import device, empty_strided
+from torch._inductor.codecache import AsyncCompile
+from torch._inductor.select_algorithm import extern_kernels
+from torch._inductor.codegen.multi_kernel import MultiKernelCall
+
+aten = torch.ops.aten
+inductor_ops = torch.ops.inductor
+_quantized = torch.ops._quantized
+assert_size_stride = torch._C._dynamo.guards.assert_size_stride
+empty_strided_cpu = torch._C._dynamo.guards._empty_strided_cpu
+empty_strided_cuda = torch._C._dynamo.guards._empty_strided_cuda
+alloc_from_pool = torch.ops.inductor._alloc_from_pool
+reinterpret_tensor = torch.ops.inductor._reinterpret_tensor
+async_compile = AsyncCompile()
+
+
+# kernel path: /tmp/torchinductor_joydong/gf/cgfqm2jdqangsynao4zxyecf3z3w3ur4z2srrlkdfwmhxaspbodh.py
+# Source Nodes: [flex_attention], Original ATen: []
+# flex_attention => flex_attention
+triton_tem_fused_0 = async_compile.triton('triton_', '''
+import triton
+import triton.language as tl
+from triton.compiler.compiler import AttrsDescriptor
+
+from torch._inductor.runtime import triton_helpers, triton_heuristics
+from torch._inductor.runtime.triton_helpers import libdevice, math as tl_math
+from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, instance_descriptor, DeviceProperties
+
+@triton_heuristics.template(
+    num_stages=3,
+    num_warps=4,
+    triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: '*fp32', 3: '*fp32', 4: '*fp32'}, 'device': DeviceProperties(type='cuda', index=0, cc=90, major=9, regs_per_multiprocessor=65536, max_threads_per_multi_processor=2048, multi_processor_count=132), 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1, 2, 3, 4), equal_to_1=())]},
+    inductor_meta={'kernel_name': 'triton_tem_fused_0', 'backend_hash': '3d930ae76fa5e25713342ca2a1b35b7ee99023f455bffb679bcc13dbe5fe0c6e', 'are_deterministic_algorithms_enabled': False, 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': True, 'autotune_remote_cache': False, 'force_disable_caches': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False},
+)
+@triton.jit
+def triton_(arg_Q, arg_K, arg_V, arg_LSE, out_ptr0):
+    BLOCK_M : tl.constexpr = 128
+    BLOCK_N : tl.constexpr = 32
+    BLOCK_DMODEL : tl.constexpr = 64
+    SCORE_MOD_IS_LINEAR : tl.constexpr = False
+    ROWS_GUARANTEED_SAFE : tl.constexpr = False
+    OUTPUT_LOGSUMEXP : tl.constexpr = True
+    Q = arg_Q
+    K = arg_K
+    V = arg_V
+    LSE = arg_LSE
+
+    # Sub notation for this kernel:
+    # Q: Query, K: Key, V: Value
+    # M: Number of queries, N: Number of keys/values, D: Model dimension
+    # z: Batch size, h: Number of heads, m: Number of queries per head, k: Number of keys per head
+    # (Modifiable) Config options:
+    # BLOCK_M
+    # BLOCK_N
+    # SCORE_MOD_IS_LINEAR: Is the score modifier linear? If so, we can lift the
+    # change of base out of the loop
+    # ROWS_GUARANTEED_SAFE: Is it guaranteed that at least one value in each row
+    # is not masked out? If so, we can skip an extra safety check
+    # OUTPUT_LOGSUMEXP: We only need to store the logsumexp if we require grad
+
+    # Define Q Strides
+    stride_qz = 1048576
+    stride_qh = 131072
+    stride_qm = 64
+    stride_qk = 1
+    # Define K Strides
+    stride_kz = 1048576
+    stride_kh = 131072
+    stride_kn = 64
+    stride_kk = 1
+    # Define V Strides
+    stride_vz = 1048576
+    stride_vh = 131072
+    stride_vk = 64
+    stride_vn = 1
+
+    Z = 8
+    H = 8
+    N_CTX = 2048
+
+    qk_scale = 1.0
+    MATMUL_PRECISION = Q.dtype.element_ty
+
+    start_m = tl.program_id(0)
+    off_hz = tl.program_id(1)
+
+    qkv_offset = off_hz * stride_qh
+    Q_block_ptr = tl.make_block_ptr(
+        base=Q + qkv_offset,
+        shape=(N_CTX, BLOCK_DMODEL),
+        strides=(stride_qm, stride_qk),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, BLOCK_DMODEL),
+        order=(1, 0)
+    )
+    K_block_ptr = tl.make_block_ptr(
+        base=K + qkv_offset,
+        shape=(BLOCK_DMODEL, N_CTX),
+        strides=(stride_kk, stride_kn),
+        offsets=(0, 0),
+        block_shape=(BLOCK_DMODEL, BLOCK_N),
+        order=(0, 1)
+    )
+    V_block_ptr = tl.make_block_ptr(
+        base=V + qkv_offset,
+        shape=(N_CTX, BLOCK_DMODEL),
+        strides=(stride_vk, stride_vn),
+        offsets=(0, 0),
+        block_shape=(BLOCK_N, BLOCK_DMODEL),
+        order=(1, 0)
+    )
+    # initialize offsets
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    # initialize pointer to m and l
+    m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
+    l_i = tl.zeros([BLOCK_M], dtype=tl.float32)
+    acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+
+    q = tl.load(Q_block_ptr)
+    if SCORE_MOD_IS_LINEAR:
+        qk_scale *= 1.44269504
+    q = (q * qk_scale).to(MATMUL_PRECISION)
+    # loop over k, v and update accumulator
+    lo = 0
+    hi = N_CTX
+    for start_n in range(lo, hi, BLOCK_N):
+        start_n = tl.multiple_of(start_n, BLOCK_N)
+        # -- load k, v --
+        k = tl.load(K_block_ptr)
+        v = tl.load(V_block_ptr)
+        # -- compute qk ---
+        qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+        qk = tl.dot(q, k.to(MATMUL_PRECISION), acc=qk)
+        # ~~~~~~~~~~~~~~~~~~~ Apply score modification  ~~~~~~~~~~~~~~~~~~~
+        m = offs_m[:, None]
+        n = start_n + offs_n[None, :]
+        tmp0 = (n) - (m)
+        tmp1 = tl_math.abs(tmp0)
+        tmp2 = tl.full([1], 2, tl.int32)
+        tmp3 = tmp1 == tmp2
+        tmp4 = tl.full([1], 1, tl.int32)
+        tmp5 = tmp1 == tmp4
+        tmp6 = 0.5
+        tmp7 = (qk) * tmp6
+        tmp8 = tl.where(tmp5, tmp7, (qk))
+        tmp9 = 2.0
+        tmp10 = tmp8 * tmp9
+        tmp11 = tl.where(tmp3, tmp10, tmp8)
+        qk = tmp11
+
+        # TODO: In the case that score_mod is linear, this can be LICMed
+        if not SCORE_MOD_IS_LINEAR:
+            qk *= 1.44269504
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # -- compute scaling constant ---
+        row_max = tl.max(qk, 1)
+        m_i_new = tl.maximum(m_i, row_max)
+
+        alpha = tl.math.exp2(m_i - m_i_new)
+        p = tl.math.exp2(qk - m_i_new[:, None])
+        if not ROWS_GUARANTEED_SAFE:
+            masked_out_rows = (m_i_new == float("-inf"))
+            alpha = tl.where(masked_out_rows, 0, alpha)
+            p = tl.where(masked_out_rows[:, None], 0, p)
+
+        # -- scale and update acc --
+        acc_scale = l_i * 0 + alpha  # workaround some compiler bug
+        acc *= acc_scale[:, None]
+        acc = tl.dot(p.to(MATMUL_PRECISION), v.to(MATMUL_PRECISION), acc)
+
+        # -- update m_i and l_i --
+        l_i = l_i * alpha + tl.sum(p, 1)
+        m_i = m_i_new
+        # update pointers
+        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
+        V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
+
+    # Store output and logsumexp
+    acc = acc / l_i[:, None]
+    idx_z = tl.program_id(1) // H
+    idx_h = tl.program_id(1) % H
+    idx_m = offs_m[:, None]
+    idx_d = tl.arange(0, BLOCK_DMODEL)[None, :]
+
+    # TODO generalize and add proper mask support
+    mask = (idx_m != -1) & (idx_d != -1)
+    xindex = idx_d + (64*idx_m) + (131072*idx_h) + (1048576*idx_z)
+    tl.store(out_ptr0 + (xindex), acc, None)
+
+    # TODO dont want to write this if we dont require grad
+    if OUTPUT_LOGSUMEXP:
+        l_ptrs = LSE + off_hz * N_CTX + offs_m
+        lse = m_i + tl.math.log2(l_i)
+        tl.store(l_ptrs, lse)
+''', device_str='cuda')
+
+import triton
+import triton.language as tl
+from torch._inductor.runtime.triton_heuristics import grid, split_scan_grid, start_graph, end_graph
+from torch._C import _cuda_getCurrentRawStream as get_raw_stream
+import torch._inductor.kernel.flex_attention
+meta0 = {'BLOCK_M': 128, 'BLOCK_N': 32, 'BLOCK_DMODEL': 64, 'SCORE_MOD_IS_LINEAR': False, 'ROWS_GUARANTEED_SAFE': False, 'OUTPUT_LOGSUMEXP': True}
+
+
+async_compile.wait(globals())
+del async_compile
+
+def call(args):
+    arg0_1, arg1_1, arg2_1 = args
+    args.clear()
+    assert_size_stride(arg0_1, (8, 8, 2048, 64), (1048576, 131072, 64, 1))
+    assert_size_stride(arg1_1, (8, 8, 2048, 64), (1048576, 131072, 64, 1))
+    assert_size_stride(arg2_1, (8, 8, 2048, 64), (1048576, 131072, 64, 1))
+    buf0 = empty_strided_cuda((8, 8, 2048), (16384, 2048, 1), torch.float32)
+    with torch.cuda._DeviceGuard(0):
+        torch.cuda.set_device(0)
+        buf1 = empty_strided_cuda((8, 8, 2048, 64), (1048576, 131072, 64, 1), torch.float32)
+        # Source Nodes: [flex_attention], Original ATen: []
+        stream0 = get_raw_stream(0)
+        triton_tem_fused_0.run(arg0_1, arg1_1, arg2_1, buf0, buf1, grid=torch._inductor.kernel.flex_attention.sdpa_grid(8, 8, 2048, 64, meta0), stream=stream0)
+        del arg0_1
+        del arg1_1
+        del arg2_1
+    return (buf1, )
+
+
+from torch.nn.attention._flex_attention import _flex_attention as flex_attention
+
+
+def checkerboard(score, batch, head, token_q, token_kv):
+    score = torch.where(torch.abs(token_kv - token_q) == 1, score * 0.5, score)
+    score = torch.where(torch.abs(token_kv - token_q) == 2, score * 2.0, score)
+    return score
+
+def benchmark_compiled_module(times=10, repeat=10):
+    from torch._dynamo.testing import rand_strided
+    from torch._inductor.utils import print_performance
+    arg0_1 = rand_strided((8, 8, 2048, 64), (1048576, 131072, 64, 1), device='cuda:0', dtype=torch.float32)
+    arg1_1 = rand_strided((8, 8, 2048, 64), (1048576, 131072, 64, 1), device='cuda:0', dtype=torch.float32)
+    arg2_1 = rand_strided((8, 8, 2048, 64), (1048576, 131072, 64, 1), device='cuda:0', dtype=torch.float32)
+    fn = lambda: call([arg0_1, arg1_1, arg2_1])
+
+    (triton_results,) = fn()
+    gold_results = flex_attention(arg0_1, arg1_1, arg2_1, checkerboard)
+    torch.testing.assert_close(triton_results, gold_results, atol=2e-2, rtol=2e-2)
+
+
+    return print_performance(fn, times=times, repeat=repeat)
+
+
+if __name__ == "__main__":
+    from torch._inductor.wrapper_benchmark import compiled_module_main
+    compiled_module_main('None', benchmark_compiled_module)
---
+++ joy-dev/triton_reduction.py
@@ -0,0 +1,152 @@
+
+# AOT ID: ['0_inference']
+from ctypes import c_void_p, c_long
+import torch
+import math
+import random
+import os
+import tempfile
+from math import inf, nan
+from torch._inductor.hooks import run_intermediate_hooks
+from torch._inductor.utils import maybe_profile
+from torch._inductor.codegen.memory_planning import _align as align
+
+from torch import device, empty_strided
+from torch._inductor.codecache import AsyncCompile
+from torch._inductor.select_algorithm import extern_kernels
+from torch._inductor.codegen.multi_kernel import MultiKernelCall
+
+aten = torch.ops.aten
+inductor_ops = torch.ops.inductor
+_quantized = torch.ops._quantized
+assert_size_stride = torch._C._dynamo.guards.assert_size_stride
+empty_strided_cpu = torch._C._dynamo.guards._empty_strided_cpu
+empty_strided_cuda = torch._C._dynamo.guards._empty_strided_cuda
+alloc_from_pool = torch.ops.inductor._alloc_from_pool
+reinterpret_tensor = torch.ops.inductor._reinterpret_tensor
+async_compile = AsyncCompile()
+
+
+# kernel path: /tmp/torchinductor_joydong/w7/cw7zmzgzdp5ckq6pv7upp7xkbjsyeun4nqk7emwpox45smaplksk.py
+# Source Nodes: [sum_1], Original ATen: [aten.sum]
+# sum_1 => sum_1
+triton_red_fused_sum_0 = async_compile.triton('triton_', '''
+import triton
+import triton.language as tl
+from triton.compiler.compiler import AttrsDescriptor
+
+from torch._inductor.runtime import triton_helpers, triton_heuristics
+from torch._inductor.runtime.triton_helpers import libdevice, math as tl_math
+from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, instance_descriptor, DeviceProperties
+
+@triton_heuristics.reduction(
+    size_hints=[16, 8192],
+    reduction_hint=ReductionHint.INNER,
+    filename=__file__,
+    triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: 'i32', 3: 'i32'}, 'device': DeviceProperties(type='cuda', index=0, cc=90, major=9, regs_per_multiprocessor=65536, max_threads_per_multi_processor=2048, multi_processor_count=132), 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1), equal_to_1=())]},
+    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_red_fused_sum_0', 'mutated_arg_names': [], 'no_x_dim': False, 'backend_hash': '3d930ae76fa5e25713342ca2a1b35b7ee99023f455bffb679bcc13dbe5fe0c6e', 'are_deterministic_algorithms_enabled': False, 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': True, 'autotune_remote_cache': False, 'force_disable_caches': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False}
+)
+@triton.jit
+def triton_(in_ptr0, out_ptr0, xnumel, rnumel, XBLOCK : tl.constexpr, RBLOCK : tl.constexpr):
+    xnumel = 13
+    rnumel = 7693
+    xoffset = tl.program_id(0) * XBLOCK
+    xindex = xoffset + tl.arange(0, XBLOCK)[:, None]
+    xmask = xindex < xnumel
+    rbase = tl.arange(0, RBLOCK)[None, :]
+    x0 = xindex
+    _tmp7 = tl.full([XBLOCK, RBLOCK], 0, tl.float32)
+    for roffset in range(0, rnumel, RBLOCK):
+        rindex = roffset + rbase
+        rmask = rindex < rnumel
+        r1 = rindex
+        tmp0 = r1 + (7693*x0)
+        tmp1 = tl.full([1, 1], 100000, tl.int32)
+        tmp2 = tmp0 < tmp1
+        tmp3 = tl.load(in_ptr0 + (r1 + (7693*x0)), rmask & tmp2 & xmask, eviction_policy='evict_first', other=0.0)
+        tmp4 = tl.full(tmp3.shape, 0, tmp3.dtype)
+        tmp5 = tl.where(tmp2, tmp3, tmp4)
+        tmp6 = tl.broadcast_to(tmp5, [XBLOCK, RBLOCK])
+        tmp8 = _tmp7 + tmp6
+        _tmp7 = tl.where(rmask & xmask, tmp8, _tmp7)
+    tmp7 = tl.sum(_tmp7, 1)[:, None]
+    tl.store(out_ptr0 + (x0), tmp7, xmask)
+''', device_str='cuda')
+
+import triton
+import triton.language as tl
+from torch._inductor.runtime.triton_heuristics import grid, split_scan_grid, start_graph, end_graph
+from torch._C import _cuda_getCurrentRawStream as get_raw_stream
+
+
+# kernel path: /tmp/torchinductor_joydong/7y/c7y6q67ibwn4bpv2i6w4dmgvsw3pppoysvhldcj6d6xz332jp4sf.py
+# Source Nodes: [sum_1], Original ATen: [aten.sum]
+# sum_1 => sum_1
+triton_per_fused_sum_1 = async_compile.triton('triton_', '''
+import triton
+import triton.language as tl
+from triton.compiler.compiler import AttrsDescriptor
+
+from torch._inductor.runtime import triton_helpers, triton_heuristics
+from torch._inductor.runtime.triton_helpers import libdevice, math as tl_math
+from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, instance_descriptor, DeviceProperties
+
+@triton_heuristics.persistent_reduction(
+    size_hints=[1, 16],
+    reduction_hint=ReductionHint.INNER,
+    filename=__file__,
+    triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: 'i32', 3: 'i32'}, 'device': DeviceProperties(type='cuda', index=0, cc=90, major=9, regs_per_multiprocessor=65536, max_threads_per_multi_processor=2048, multi_processor_count=132), 'constants': {2: 1}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1), equal_to_1=(2,))]},
+    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_per_fused_sum_1', 'mutated_arg_names': [], 'no_x_dim': False, 'backend_hash': '3d930ae76fa5e25713342ca2a1b35b7ee99023f455bffb679bcc13dbe5fe0c6e', 'are_deterministic_algorithms_enabled': False, 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': True, 'autotune_remote_cache': False, 'force_disable_caches': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False}
+)
+@triton.jit
+def triton_(in_ptr0, out_ptr0, xnumel, rnumel, XBLOCK : tl.constexpr):
+    xnumel = 1
+    rnumel = 13
+    RBLOCK: tl.constexpr = 16
+    xoffset = tl.program_id(0) * XBLOCK
+    xindex = xoffset + tl.arange(0, XBLOCK)[:, None]
+    xmask = xindex < xnumel
+    rindex = tl.arange(0, RBLOCK)[None, :]
+    roffset = 0
+    rmask = rindex < rnumel
+    r0 = rindex
+    tmp0 = tl.load(in_ptr0 + (r0), rmask, other=0.0)
+    tmp1 = tl.broadcast_to(tmp0, [XBLOCK, RBLOCK])
+    tmp3 = tl.where(rmask, tmp1, 0)
+    tmp4 = tl.sum(tmp3, 1)[:, None]
+    tl.store(out_ptr0 + (tl.full([XBLOCK, 1], 0, tl.int32)), tmp4, None)
+''', device_str='cuda')
+
+
+async_compile.wait(globals())
+del async_compile
+
+def call(args):
+    arg0_1, = args
+    args.clear()
+    assert_size_stride(arg0_1, (100000, ), (1, ))
+    with torch.cuda._DeviceGuard(0):
+        torch.cuda.set_device(0)
+        buf0 = empty_strided_cuda((13, ), (1, ), torch.float32)
+        # Source Nodes: [sum_1], Original ATen: [aten.sum]
+        stream0 = get_raw_stream(0)
+        triton_red_fused_sum_0.run(arg0_1, buf0, 13, 7693, grid=grid(13), stream=stream0)
+        del arg0_1
+        buf1 = empty_strided_cuda((), (), torch.float32)
+        # Source Nodes: [sum_1], Original ATen: [aten.sum]
+        triton_per_fused_sum_1.run(buf0, buf1, 1, 13, grid=grid(1), stream=stream0)
+        del buf0
+    return (buf1, )
+
+
+def benchmark_compiled_module(times=10, repeat=10):
+    from torch._dynamo.testing import rand_strided
+    from torch._inductor.utils import print_performance
+    arg0_1 = rand_strided((100000, ), (1, ), device='cuda:0', dtype=torch.float32)
+    fn = lambda: call([arg0_1])
+    return print_performance(fn, times=times, repeat=repeat)
+
+
+if __name__ == "__main__":
+    from torch._inductor.wrapper_benchmark import compiled_module_main
+    compiled_module_main('None', benchmark_compiled_module)
