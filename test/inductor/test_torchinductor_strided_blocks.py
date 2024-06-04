32309c7b034adb0c4532c50c49972590102dc268
Blaine Burton Rister <145300525+blaine-rister@users.noreply.github.com>
2024-06-03 17:27:48 -0700

    More linting

--- test/inductor/test_torchinductor_strided_blocks.py
+++ test/inductor/test_torchinductor_strided_blocks.py
@@ -1,25 +1,22 @@
+# Owner(s): ["module: inductor"]
 import contextlib
 import functools
 import importlib
+import unittest
+from typing import Any, Callable, Iterable, Optional, Tuple
+
 import torch
 import torch.utils._pytree as pytree
 from torch._inductor import config
 from torch._inductor.runtime.hints import TRITON_MAX_BLOCK
 from torch._inductor.runtime.runtime_utils import is_power_of_2
 from torch._inductor.test_case import TestCase as InductorTestCase
-from torch.testing._internal.inductor_utils import (
-    HAS_GPU,
-    GPU_TYPE,
-    skip_windows_ci,
-)
+from torch._inductor.utils import run_and_get_code
 from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
     parametrize,
-    subtest,
 )
-from torch._inductor.utils import run_and_get_code
-from typing import Any, Callable, Iterable, Optional, Tuple, Type
-import unittest
+from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_GPU, skip_windows_ci
 
 
 requires_gpu = functools.partial(unittest.skipIf, not HAS_GPU, "requires gpu")
@@ -33,16 +30,17 @@ importlib.import_module("filelock")
 @config.patch("triton.use_block_ptr", True)
 @instantiate_parametrized_tests
 class TritonBlockPointerTest(InductorTestCase):
-
     def count_block_pointers(self, code: Iterable[str]) -> int:
         return sum(prog.count("tl.make_block_ptr") for prog in code)
 
-    def run_and_compare(self,
-                        func: Callable[..., Any],
-                        *args,
-                        compile_kwargs: Optional[dict] = None,
-                        expected_num_block_pointers: Optional[int] = None,
-                        expected_num_programs: int = 1):
+    def run_and_compare(
+        self,
+        func: Callable[..., Any],
+        *args,
+        compile_kwargs: Optional[dict] = None,
+        expected_num_block_pointers: Optional[int] = None,
+        expected_num_programs: int = 1,
+    ):
         """
         Runs the module through Inductor, comparing to eager reference.
         """
@@ -74,14 +72,17 @@ class TritonBlockPointerTest(InductorTestCase):
     @parametrize(
         "expected_num_block_pointers,raises",
         [
-            (3, False), # This should pass
-            (9, True), # This should fail
-        ]
+            (3, False),  # This should pass
+            (9, True),  # This should fail
+        ],
     )
-    def test_expected_num_block_pointers(self, expected_num_block_pointers: int, raises: bool):
+    def test_expected_num_block_pointers(
+        self, expected_num_block_pointers: int, raises: bool
+    ):
         """
         Checks that the test harness verifies the number of block pointers correctly.
         """
+
         def foo(x, y):
             return x + y
 
@@ -89,33 +90,32 @@ class TritonBlockPointerTest(InductorTestCase):
         inputs = [torch.randn(8).to(device) for arg_idx in range(2)]
 
         # Allow failure for bad inputs
-        with (self.assertRaises(AssertionError) if raises
-              else contextlib.nullcontext()):
+        with self.assertRaises(AssertionError) if raises else contextlib.nullcontext():
             # Expect 3 block pointers: 2 inputs 1 output
-            self.run_and_compare(foo, *inputs,
-                expected_num_block_pointers = expected_num_block_pointers
+            self.run_and_compare(
+                foo, *inputs, expected_num_block_pointers=expected_num_block_pointers
             )
 
     @parametrize(
         "full_size,view_size,stride,offset,require_block_ptr",
         [
-            args for args in (
-                ((64, 32, 32), (32, 16, 8), None, None, True),
-                ((16, 8, 8, 8), (8, 8, 4, 2), None, None, True),
-                ((8, 8), (4, 4), None, 10, True), # Storage offset
-                ((8, 8), (4, 4), (16, 2), None, True), # Non-default strides
-                ((15, 9), (8, 8), None, None, True), # Non-power-of-2 full dims
-                ((15, 9), (15, 3), None, None, False), # Non-power-of-2 view dims
-                ((1, 1, 1), (1, 1, 1), None, None, False), # Scalar
-            )
+            ((64, 32, 32), (32, 16, 8), None, None, True),
+            ((16, 8, 8, 8), (8, 8, 4, 2), None, None, True),
+            ((8, 8), (4, 4), None, 10, True),  # Storage offset
+            ((8, 8), (4, 4), (16, 2), None, True),  # Non-default strides
+            ((15, 9), (8, 8), None, None, True),  # Non-power-of-2 full dims
+            ((15, 9), (15, 3), None, None, False),  # Non-power-of-2 view dims
+            ((1, 1, 1), (1, 1, 1), None, None, False),  # Scalar
         ],
     )
-    def test_strided_block_ptr(self,
-                               full_size: Tuple[int],
-                               view_size: Tuple[int],
-                               stride: Optional[Tuple[int]],
-                               offset: Optional[int],
-                               require_block_ptr: bool):
+    def test_strided_block_ptr(
+        self,
+        full_size: Tuple[int],
+        view_size: Tuple[int],
+        stride: Optional[Tuple[int]],
+        offset: Optional[int],
+        require_block_ptr: bool,
+    ):
         """
         Test generating strided ND block pointers.
 
@@ -124,6 +124,7 @@ class TritonBlockPointerTest(InductorTestCase):
         we also test some odd shapes with `require_block_ptr=False`, to ensure that
         block pointer analysis does not break these cases.
         """
+
         def view(full: torch.Tensor):
             # Use the original tensor's stride by default
             view_stride = full.stride() if stride is None else stride
@@ -137,8 +138,8 @@ class TritonBlockPointerTest(InductorTestCase):
         args = [torch.randn(full_size).to(device) for arg_idx in range(2)]
 
         # Expect 3 block pointers: 2 inputs 1 output
-        self.run_and_compare(foo, *args,
-            expected_num_block_pointers = 3 if require_block_ptr else None
+        self.run_and_compare(
+            foo, *args, expected_num_block_pointers=3 if require_block_ptr else None
         )
 
     def test_different_sized_blocks(self):
@@ -146,6 +147,7 @@ class TritonBlockPointerTest(InductorTestCase):
         Test that we can generate strided block pointers when inputs have different
         shapes.
         """
+
         def foo(x, y):
             return x + 1, y * 2
 
@@ -154,7 +156,9 @@ class TritonBlockPointerTest(InductorTestCase):
         full = torch.randn(full_size).to(device)
         x_size = (8, 8)
         y_size = (4, 4)
-        x, y = tuple(torch.as_strided(full, size, full.stride()) for size in (x_size, y_size))
+        x, y = tuple(
+            torch.as_strided(full, size, full.stride()) for size in (x_size, y_size)
+        )
 
         # Check that input sizes are not the same
         self.assertNotEqual(x_size, y_size)
@@ -166,11 +170,12 @@ class TritonBlockPointerTest(InductorTestCase):
         """
         Test mixing block pointers with non-structured pointers.
         """
+
         def foo(*args):
             return sum(torch.sum(arg) for arg in args)
 
         device = torch.device(GPU_TYPE)
-        full_size = (15, 15) # Not a power of 2
+        full_size = (15, 15)  # Not a power of 2
         view_size = (8, 8)
         full = torch.randn(full_size).to(device)
         view = torch.as_strided(full, view_size, full.stride())
@@ -183,6 +188,7 @@ class TritonBlockPointerTest(InductorTestCase):
         Check that we support dims of size n * MAX_BLOCK, where n is any positive integer, not
         necessarily a power of 2.
         """
+
         def foo(x):
             return x - 1
 
@@ -209,16 +215,17 @@ class TritonBlockPointerTest(InductorTestCase):
         Test a generic strided block with dynamic shapes. Block pointers are not
         expected. This only checks that the analysis doesn't break this case.
         """
+
         def foo(x, y):
             return x / y
 
         device = torch.device(GPU_TYPE)
-        full_size = (8,8)
-        view_size = (4,4)
+        full_size = (8, 8)
+        view_size = (4, 4)
         full = torch.randn(full_size).to(device)
         view = torch.as_strided(full, view_size, full.stride())
 
-        self.run_and_compare(foo, view, view, compile_kwargs={'dynamic': True})
+        self.run_and_compare(foo, view, view, compile_kwargs={"dynamic": True})
 
     @unittest.skip(reason="Dynamo tracing error")
     def test_dynamic_shapes_multiple_max_block(self):
@@ -242,8 +249,9 @@ class TritonBlockPointerTest(InductorTestCase):
         x = torch.randn(x_size).to(device)
 
         # Expect 3 block pointers: 2 inputs and output
-        self.run_and_compare(x, compile_kwargs={'dynamic': True},
-                                               expected_num_block_pointers=3)
+        self.run_and_compare(
+            x, compile_kwargs={"dynamic": True}, expected_num_block_pointers=3
+        )
 
 
 if __name__ == "__main__":
