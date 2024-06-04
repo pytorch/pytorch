ab375858456feea719f4cac91d23b093664fa36f
Wang, Eikan <eikan.wang@intel.com>
2024-06-04 14:43:16 +0000

    Update base for Update on "[1/N] Dynamic Shape: To enable export.aot_compile to switch between static shape and dynamic shape"
    cc voznesenskym penguinwu jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx peterbell10 ipiszy yf225 chenyang78 kadeng muchulee8 ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_inductor/aoti_eager.py
+++ torch/_inductor/aoti_eager.py
@@ -184,7 +184,9 @@ def aoti_compile_with_persistent_cache(
     Compile the given function with persistent cache for AOTI eager mode.
     """
     assert not dynamic, "Only support static shape for now"
-    flattened_inputs = pytree.arg_tree_leaves(*args, **kwargs)
+    type_to_torch_dtype = {int: torch.int32, float: torch.float, bool: torch.bool}
+    supported_scalar_types = tuple(type_to_torch_dtype.keys())
+    flattened_inputs = list(args) + list(kwargs.values())
     if not all(
         isinstance(
             input,
