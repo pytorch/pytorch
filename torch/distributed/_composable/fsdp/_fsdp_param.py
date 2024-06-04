be445c70b08161cef82665691e31eb98c6246027
Will Feng <yf225@cornell.edu>
2024-06-03 21:46:39 -0700

    Update
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_param.py
+++ torch/distributed/_composable/fsdp/_fsdp_param.py
@@ -330,7 +330,10 @@ class FSDPParam:
             self._extensions_data.clear()
             return
         inner_tensor = self._sharded_local_tensor
-        if hasattr(inner_tensor, "fsdp_post_all_gather"):
+        if (
+            not torch._dynamo.compiled_autograd.compiled_autograd_enabled
+            and hasattr(inner_tensor, "fsdp_post_all_gather")
+        ):
             all_gather_outputs = self._unflatten_all_gather_outputs()
             (
                 unsharded_tensor,
@@ -500,7 +503,10 @@ class FSDPParam:
     def all_gather_inputs(self) -> List[torch.Tensor]:  # 1D
         self._assert_in_states(ShardedState.SHARDED, ShardedState.SHARDED_POST_FORWARD)
         if self.sharded_state == ShardedState.SHARDED:
-            if hasattr(self._sharded_local_tensor, "fsdp_pre_all_gather"):
+            if (
+                not torch._dynamo.compiled_autograd.compiled_autograd_enabled
+                and hasattr(self._sharded_local_tensor, "fsdp_pre_all_gather")
+            ):
                 sharded_local_tensor = self._sharded_local_tensor
                 if self.offload_to_cpu:
                     sharded_local_tensor = sharded_local_tensor.to(
@@ -521,7 +527,10 @@ class FSDPParam:
                 )
             return [_to_dtype_if_needed(sharded_param_data, self.param_dtype)]
         elif self.sharded_state == ShardedState.SHARDED_POST_FORWARD:
-            if hasattr(self._sharded_local_tensor, "fsdp_pre_all_gather"):
+            if (
+                not torch._dynamo.compiled_autograd.compiled_autograd_enabled
+                and hasattr(self._sharded_local_tensor, "fsdp_pre_all_gather")
+            ):
                 raise NotImplementedError
             all_gather_input = _to_dtype_if_needed(
                 cast(torch.Tensor, self._sharded_post_forward_param_data),
