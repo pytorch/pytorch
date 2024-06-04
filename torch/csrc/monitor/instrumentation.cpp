64d6e98aaa538dd4f9d5c93e1fd5bc8d6d1199c6
James Perng <jamesperng@meta.com>
2024-06-04 11:28:33 -0700

    [pytorch][logging] add empty wait counter implementation
    [ghstack-poisoned]

---
+++ torch/csrc/monitor/instrumentation.cpp
@@ -0,0 +1,27 @@
+#include <torch/csrc/monitor/instrumentation.h>
+
+#include <chrono>
+#include <string_view>
+
+
+namespace torch {
+namespace monitor {
+
+WaitCounterUs::WaitCounterUs(std::string_view key) : key_(key) {
+    // implement
+}
+
+WaitCounterUs::~WaitCounterUs() {
+    // implement
+}
+
+void WaitCounterUs::start(std::chrono::steady_clock::time_point now) {
+    // implement
+}
+
+void WaitCounterUs::stop(std::chrono::steady_clock::time_point now) {
+    // implement
+}
+
+} // namespace monitor
+} // namespace torch
