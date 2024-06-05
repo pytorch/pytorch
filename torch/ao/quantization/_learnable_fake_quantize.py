b484b8dc9c9195cc0cf5b1a4bb0368aadd950f03
rk7697 <91646263+rk7697@users.noreply.github.com>
2024-06-04 19:49:28 -0700

    Update _learnable_fake_quantize.py
    Remove sentence "For literature references, please see the class _LearnableFakeQuantizePerTensorOp."

--- torch/ao/quantization/_learnable_fake_quantize.py
+++ torch/ao/quantization/_learnable_fake_quantize.py
@@ -9,8 +9,7 @@ class _LearnableFakeQuantize(torch.ao.quantization.FakeQuantizeBase):
 
     This is an extension of the FakeQuantize module in fake_quantize.py, which
     supports more generalized lower-bit quantization and support learning of the scale
-    and zero point parameters through backpropagation. For literature references,
-    please see the class _LearnableFakeQuantizePerTensorOp.
+    and zero point parameters through backpropagation.
 
     In addition to the attributes in the original FakeQuantize module, the _LearnableFakeQuantize
     module also includes the following attributes to support quantization parameter learning.
