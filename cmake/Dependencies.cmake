970d4441457a3a1884c0d2dd9f37339ac99bf142
Daniel Richard G <skunk@iSKUNK.ORG>
2024-06-05 01:15:06 +0000

    Support use of system/external googletest and (google)benchmark

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -611,85 +611,79 @@ endif()
 
 # ---[ Googletest and benchmark
 if(BUILD_TEST OR BUILD_MOBILE_BENCHMARK OR BUILD_MOBILE_TEST)
-  # Preserve build options.
-  set(TEMP_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+  if(NOT USE_SYSTEM_GOOGLETEST)
+    # Preserve build options.
+    set(TEMP_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
 
-  # We will build gtest as static libs and embed it directly into the binary.
-  set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libs" FORCE)
+    # We will build gtest as static libs and embed it directly into the binary.
+    set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libs" FORCE)
 
-  # For gtest, we will simply embed it into our test binaries, so we won't
-  # need to install it.
-  set(INSTALL_GTEST OFF CACHE BOOL "Install gtest." FORCE)
-  set(BUILD_GMOCK ON CACHE BOOL "Build gmock." FORCE)
-  # For Windows, we will check the runtime used is correctly passed in.
-  if(NOT CAFFE2_USE_MSVC_STATIC_RUNTIME)
-      set(gtest_force_shared_crt ON CACHE BOOL "force shared crt on gtest" FORCE)
-  endif()
-  # We need to replace googletest cmake scripts too.
-  # Otherwise, it will sometimes break the build.
-  # To make the git clean after the build, we make a backup first.
-  if((MSVC AND MSVC_Z7_OVERRIDE) OR USE_CUDA)
-    execute_process(
-      COMMAND ${CMAKE_COMMAND}
-              "-DFILENAME=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake"
-              "-DBACKUP=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake.bak"
-              "-DREVERT=0"
-              "-P"
-              "${CMAKE_CURRENT_LIST_DIR}/GoogleTestPatch.cmake"
-      RESULT_VARIABLE _exitcode)
-    if(NOT _exitcode EQUAL 0)
-      message(WARNING "Patching failed for Google Test. The build may fail.")
+    # For gtest, we will simply embed it into our test binaries, so we won't
+    # need to install it.
+    set(INSTALL_GTEST OFF CACHE BOOL "Install gtest." FORCE)
+    set(BUILD_GMOCK ON CACHE BOOL "Build gmock." FORCE)
+    # For Windows, we will check the runtime used is correctly passed in.
+    if(NOT CAFFE2_USE_MSVC_STATIC_RUNTIME)
+        set(gtest_force_shared_crt ON CACHE BOOL "force shared crt on gtest" FORCE)
     endif()
+    # We need to replace googletest cmake scripts too.
+    # Otherwise, it will sometimes break the build.
+    # To make the git clean after the build, we make a backup first.
+    if((MSVC AND MSVC_Z7_OVERRIDE) OR USE_CUDA)
+      execute_process(
+        COMMAND ${CMAKE_COMMAND}
+                "-DFILENAME=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake"
+                "-DBACKUP=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake.bak"
+                "-DREVERT=0"
+                "-P"
+                "${CMAKE_CURRENT_LIST_DIR}/GoogleTestPatch.cmake"
+        RESULT_VARIABLE _exitcode)
+      if(NOT _exitcode EQUAL 0)
+        message(WARNING "Patching failed for Google Test. The build may fail.")
+      endif()
+    endif()
+
+    # Add googletest subdirectory but make sure our INCLUDE_DIRECTORIES
+    # don't bleed into it. This is because libraries installed into the root conda
+    # env (e.g. MKL) add a global /opt/conda/include directory, and if there's
+    # gtest installed in conda, the third_party/googletest/**.cc source files
+    # would try to include headers from /opt/conda/include/gtest/**.h instead of
+    # its own. Once we have proper target-based include directories,
+    # this shouldn't be necessary anymore.
+    get_property(INC_DIR_temp DIRECTORY PROPERTY INCLUDE_DIRECTORIES)
+    set_property(DIRECTORY PROPERTY INCLUDE_DIRECTORIES "")
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest)
+    set_property(DIRECTORY PROPERTY INCLUDE_DIRECTORIES ${INC_DIR_temp})
+
+    # To make the git clean after the build, we revert the changes here.
+    if(MSVC AND MSVC_Z7_OVERRIDE)
+      execute_process(
+        COMMAND ${CMAKE_COMMAND}
+                "-DFILENAME=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake"
+                "-DBACKUP=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake.bak"
+                "-DREVERT=1"
+                "-P"
+                "${CMAKE_CURRENT_LIST_DIR}/GoogleTestPatch.cmake"
+        RESULT_VARIABLE _exitcode)
+      if(NOT _exitcode EQUAL 0)
+        message(WARNING "Reverting changes failed for Google Test. The build may fail.")
+      endif()
+    endif()
+
+    # Recover build options.
+    set(BUILD_SHARED_LIBS ${TEMP_BUILD_SHARED_LIBS} CACHE BOOL "Build shared libs" FORCE)
+  else()
+    find_package(GTest REQUIRED)
   endif()
 
-  # Add googletest subdirectory but make sure our INCLUDE_DIRECTORIES
-  # don't bleed into it. This is because libraries installed into the root conda
-  # env (e.g. MKL) add a global /opt/conda/include directory, and if there's
-  # gtest installed in conda, the third_party/googletest/**.cc source files
-  # would try to include headers from /opt/conda/include/gtest/**.h instead of
-  # its own. Once we have proper target-based include directories,
-  # this shouldn't be necessary anymore.
-  get_property(INC_DIR_temp DIRECTORY PROPERTY INCLUDE_DIRECTORIES)
-  set_property(DIRECTORY PROPERTY INCLUDE_DIRECTORIES "")
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest)
-  set_property(DIRECTORY PROPERTY INCLUDE_DIRECTORIES ${INC_DIR_temp})
-
-  include_directories(BEFORE SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/include)
-  include_directories(BEFORE SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googlemock/include)
-
-  # We will not need to test benchmark lib itself.
-  set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark testing as we don't need it.")
-  # We will not need to install benchmark since we link it statically.
-  set(BENCHMARK_ENABLE_INSTALL OFF CACHE BOOL "Disable benchmark install to avoid overwriting vendor install.")
   if(NOT USE_SYSTEM_BENCHMARK)
+    # We will not need to test benchmark lib itself.
+    set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark testing as we don't need it.")
+    # We will not need to install benchmark since we link it statically.
+    set(BENCHMARK_ENABLE_INSTALL OFF CACHE BOOL "Disable benchmark install to avoid overwriting vendor install.")
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/benchmark)
   else()
-    add_library(benchmark SHARED IMPORTED)
-    find_library(BENCHMARK_LIBRARY benchmark)
-    if(NOT BENCHMARK_LIBRARY)
-      message(FATAL_ERROR "Cannot find google benchmark library")
-    endif()
-    message("-- Found benchmark: ${BENCHMARK_LIBRARY}")
-    set_property(TARGET benchmark PROPERTY IMPORTED_LOCATION ${BENCHMARK_LIBRARY})
-  endif()
-  include_directories(${CMAKE_CURRENT_LIST_DIR}/../third_party/benchmark/include)
-
-  # Recover build options.
-  set(BUILD_SHARED_LIBS ${TEMP_BUILD_SHARED_LIBS} CACHE BOOL "Build shared libs" FORCE)
-
-  # To make the git clean after the build, we revert the changes here.
-  if(MSVC AND MSVC_Z7_OVERRIDE)
-    execute_process(
-      COMMAND ${CMAKE_COMMAND}
-              "-DFILENAME=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake"
-              "-DBACKUP=${CMAKE_CURRENT_LIST_DIR}/../third_party/googletest/googletest/cmake/internal_utils.cmake.bak"
-              "-DREVERT=1"
-              "-P"
-              "${CMAKE_CURRENT_LIST_DIR}/GoogleTestPatch.cmake"
-      RESULT_VARIABLE _exitcode)
-    if(NOT _exitcode EQUAL 0)
-      message(WARNING "Reverting changes failed for Google Test. The build may fail.")
-    endif()
+    find_package(benchmark REQUIRED)
   endif()
 
   # Cacheing variables to enable incremental build.
