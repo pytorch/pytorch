f8e6c2cc2620a92d84304869e7597a7ecfba1abc
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-06 18:25:02 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/__init__.py
+++ torch/__init__.py
@@ -128,91 +128,97 @@ assert __all__ == sorted(__all__)
 # Load the extension module
 ################################################################################
 
-if sys.platform == 'win32':
-    import sysconfig
-    pfiles_path = os.getenv('ProgramFiles', 'C:\\Program Files')
-    py_dll_path = os.path.join(sys.exec_prefix, 'Library', 'bin')
-    th_dll_path = os.path.join(os.path.dirname(__file__), 'lib')
-    usebase_path = os.path.join(sysconfig.get_config_var("userbase"), 'Library', 'bin')
+if platform.system() == 'Windows':
 
-    # When users create a virtualenv that inherits the base environment,
-    # we will need to add the corresponding library directory into
-    # DLL search directories. Otherwise, it will rely on `PATH` which
-    # is dependent on user settings.
-    if sys.exec_prefix != sys.base_exec_prefix:
-        base_py_dll_path = os.path.join(sys.base_exec_prefix, 'Library', 'bin')
-    else:
-        base_py_dll_path = ''
+    def _load_dll_libraries():
+        import sysconfig
 
-    dll_paths = list(filter(os.path.exists, [th_dll_path, py_dll_path, base_py_dll_path, usebase_path]))
+        from torch.version import cuda as cuda_version
 
-    if all(not os.path.exists(os.path.join(p, 'nvToolsExt64_1.dll')) for p in dll_paths):
-        nvtoolsext_dll_path = os.path.join(
-            os.getenv('NVTOOLSEXT_PATH', os.path.join(pfiles_path, 'NVIDIA Corporation', 'NvToolsExt')), 'bin', 'x64')
-    else:
-        nvtoolsext_dll_path = ''
+        pfiles_path = os.getenv('ProgramFiles', r'C:\Program Files')
+        py_dll_path = os.path.join(sys.exec_prefix, 'Library', 'bin')
+        th_dll_path = os.path.join(os.path.dirname(__file__), 'lib')
+        usebase_path = os.path.join(sysconfig.get_config_var("userbase"), 'Library', 'bin')
 
-    from torch.version import cuda as cuda_version
+        # When users create a virtualenv that inherits the base environment,
+        # we will need to add the corresponding library directory into
+        # DLL search directories. Otherwise, it will rely on `PATH` which
+        # is dependent on user settings.
+        if sys.exec_prefix != sys.base_exec_prefix:
+            base_py_dll_path = os.path.join(sys.base_exec_prefix, 'Library', 'bin')
+        else:
+            base_py_dll_path = ''
 
-    if cuda_version and all(not glob.glob(os.path.join(p, 'cudart64*.dll')) for p in dll_paths):
-        cuda_version_1 = cuda_version.replace('.', '_')
-        cuda_path_var = 'CUDA_PATH_V' + cuda_version_1
-        default_path = os.path.join(pfiles_path, 'NVIDIA GPU Computing Toolkit', 'CUDA', 'v' + cuda_version)
-        cuda_path = os.path.join(os.getenv(cuda_path_var, default_path), 'bin')
-    else:
-        cuda_path = ''
+        dll_paths = [p for p in (th_dll_path, py_dll_path, base_py_dll_path, usebase_path) if os.path.exists(p)]
 
-    dll_paths.extend(filter(os.path.exists, [nvtoolsext_dll_path, cuda_path]))
+        if not any(os.path.exists(os.path.join(p, 'nvToolsExt64_1.dll')) for p in dll_paths):
+            nvtoolsext_dll_path = os.path.join(
+                os.getenv('NVTOOLSEXT_PATH', os.path.join(pfiles_path, 'NVIDIA Corporation', 'NvToolsExt')), 'bin', 'x64')
+        else:
+            nvtoolsext_dll_path = ''
 
-    kernel32 = ctypes.WinDLL('kernel32.dll', use_last_error=True)
-    with_load_library_flags = hasattr(kernel32, 'AddDllDirectory')
-    prev_error_mode = kernel32.SetErrorMode(0x0001)
+        if cuda_version and all(not glob.glob(os.path.join(p, 'cudart64*.dll')) for p in dll_paths):
+            cuda_version_1 = cuda_version.replace('.', '_')
+            cuda_path_var = 'CUDA_PATH_V' + cuda_version_1
+            default_path = os.path.join(pfiles_path, 'NVIDIA GPU Computing Toolkit', 'CUDA', 'v' + cuda_version)
+            cuda_path = os.path.join(os.getenv(cuda_path_var, default_path), 'bin')
+        else:
+            cuda_path = ''
 
-    kernel32.LoadLibraryW.restype = ctypes.c_void_p
-    if with_load_library_flags:
-        kernel32.LoadLibraryExW.restype = ctypes.c_void_p
+        dll_paths.extend(p for p in (nvtoolsext_dll_path, cuda_path) if os.path.exists(p))
 
-    for dll_path in dll_paths:
-        os.add_dll_directory(dll_path)
+        kernel32 = ctypes.WinDLL('kernel32.dll', use_last_error=True)
+        with_load_library_flags = hasattr(kernel32, 'AddDllDirectory')
+        prev_error_mode = kernel32.SetErrorMode(0x0001)
 
-    try:
-        ctypes.CDLL('vcruntime140.dll')
-        ctypes.CDLL('msvcp140.dll')
-        ctypes.CDLL('vcruntime140_1.dll')
-    except OSError:
-        print(
-            textwrap.dedent(
-                """
-                Microsoft Visual C++ Redistributable is not installed, this may lead to the DLL load failure.
-                It can be downloaded at https://aka.ms/vs/16/release/vc_redist.x64.exe
-                """
-            ).strip()
-        )
-
-    dlls = glob.glob(os.path.join(th_dll_path, '*.dll'))
-    path_patched = False
-    for dll in dlls:
-        is_loaded = False
+        kernel32.LoadLibraryW.restype = ctypes.c_void_p
         if with_load_library_flags:
-            res = kernel32.LoadLibraryExW(dll, None, 0x00001100)
-            last_error = ctypes.get_last_error()
-            if res is None and last_error != 126:
-                err = ctypes.WinError(last_error)
-                err.strerror += f' Error loading "{dll}" or one of its dependencies.'
-                raise err
-            elif res is not None:
-                is_loaded = True
-        if not is_loaded:
-            if not path_patched:
-                os.environ['PATH'] = ';'.join(dll_paths + [os.environ['PATH']])
-                path_patched = True
-            res = kernel32.LoadLibraryW(dll)
-            if res is None:
-                err = ctypes.WinError(ctypes.get_last_error())
-                err.strerror += f' Error loading "{dll}" or one of its dependencies.'
-                raise err
+            kernel32.LoadLibraryExW.restype = ctypes.c_void_p
 
-    kernel32.SetErrorMode(prev_error_mode)
+        for dll_path in dll_paths:
+            os.add_dll_directory(dll_path)
+
+        try:
+            ctypes.CDLL('vcruntime140.dll')
+            ctypes.CDLL('msvcp140.dll')
+            ctypes.CDLL('vcruntime140_1.dll')
+        except OSError:
+            print(
+                textwrap.dedent(
+                    """
+                    Microsoft Visual C++ Redistributable is not installed, this may lead to the DLL load failure.
+                    It can be downloaded at https://aka.ms/vs/16/release/vc_redist.x64.exe
+                    """
+                ).strip()
+            )
+
+        dlls = glob.glob(os.path.join(th_dll_path, '*.dll'))
+        path_patched = False
+        for dll in dlls:
+            is_loaded = False
+            if with_load_library_flags:
+                res = kernel32.LoadLibraryExW(dll, None, 0x00001100)
+                last_error = ctypes.get_last_error()
+                if res is None and last_error != 126:
+                    err = ctypes.WinError(last_error)
+                    err.strerror += f' Error loading "{dll}" or one of its dependencies.'
+                    raise err
+                elif res is not None:
+                    is_loaded = True
+            if not is_loaded:
+                if not path_patched:
+                    os.environ['PATH'] = ';'.join(dll_paths + [os.environ['PATH']])
+                    path_patched = True
+                res = kernel32.LoadLibraryW(dll)
+                if res is None:
+                    err = ctypes.WinError(ctypes.get_last_error())
+                    err.strerror += f' Error loading "{dll}" or one of its dependencies.'
+                    raise err
+
+        kernel32.SetErrorMode(prev_error_mode)
+
+    _load_dll_libraries()
+    del _load_dll_libraries
 
 
 def _preload_cuda_deps(lib_folder, lib_name):
