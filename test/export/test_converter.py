b811ca598c32ac7e05cabb2b50936034dda61742
Jiashen Cao <jiashenc@meta.com>
2024-06-06 16:52:27 -0700

    add function test

--- test/export/test_converter.py
+++ test/export/test_converter.py
@@ -764,9 +764,5 @@ class TestConverter(TestCase):
         self._check_equal_ts_ep_converter(Module(), inp, ["script"])
 
 
-    def test_convert_func_without_param(self):
-        pass
-
-
 if __name__ == "__main__":
     run_tests()
