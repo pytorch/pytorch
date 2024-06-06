a1adb3c99ff69ccd118d318ba822211e1ef7e5dc
Animesh Jain <anijain@umich.edu>
2024-06-06 10:00:13 -0700

    [dynamo][nn-modules] Trace through nn.Module dunder methods for UnspecializedNNModule (#126578)
    Tracing through `__init__`  is important because it initializes (calls STORE_ATTR) on members. By doing that, we kick in the mutation tracking for these objects. So, things like mutating `_modules` etc is tracked automatically.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/126578
    Approved by: https://github.com/jansel
    ghstack dependencies: #128001

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -2018,12 +2018,12 @@ def object_has_getattribute(value: Any):
     return False
 
 
-def get_custom_getattr(value: Any):
+def get_custom_getattr(value: Any, ignore_nn_module_getattr: bool = False):
     try:
         getattr_fn = inspect.getattr_static(type(value), "__getattr__")
     except AttributeError:
         getattr_fn = None
-    if getattr_fn is torch.nn.Module.__getattr__:
+    if ignore_nn_module_getattr and getattr_fn is torch.nn.Module.__getattr__:
         # ignore this case of getattr
         getattr_fn = None
     return getattr_fn
