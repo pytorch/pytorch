1a84b1d343749f74a996927d428d05ecfc15bfb3
Yuanhao Ji <jiyuanhao@apache.org>
2024-06-07 22:36:24 +0800

    test: add test_autoload_enable and test_autoload_disable (part 3)

--- test/test_autoload.py
+++ test/test_autoload.py
@@ -9,14 +9,15 @@ from torch.testing._internal.common_utils import run_tests, TestCase
 
 class TestDeviceBackendAutoload(TestCase):
     def test_autoload(self):
-        switch = os.getenv("TORCH_DEVICE_BACKEND_AUTOLOAD", "True")
+        switch = os.getenv("TORCH_DEVICE_BACKEND_AUTOLOAD", None)
 
         # After importing the extension, the value of this environment variable should be true
         # See: test/cpp_extensions/torch_test_cpp_extension/__init__.py
         is_imported = os.getenv("IS_CUSTOM_DEVICE_BACKEND_IMPORTED", "False")
 
         # Both values should be equal
-        self.assertEqual(is_imported, switch)
+        if switch:
+            self.assertEqual(is_imported, switch)
 
 
 if __name__ == "__main__":
