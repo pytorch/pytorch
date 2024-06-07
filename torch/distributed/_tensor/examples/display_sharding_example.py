9afaa4bf6982204a1f0763e3ddab88ec0e89801f
Anshul Sinha <50644008+sinhaanshul@users.noreply.github.com>
2024-06-07 14:31:14 -0700

    [dtensor][experiment] experimenting with displaying distributed model parameters and printing sharding info (#127987)
    **Summary**
    Example code to display distributed model parameters and verify them against ground truth. Also prints sharding information.
    
    **Test Plan**
    torchrun --standalone --nnodes=1 --nproc-per-node=4 torch/distributed/_tensor/examples/display_sharding_example.py
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127987
    Approved by: https://github.com/XilunWu
    ghstack dependencies: #127358, #127360, #127630

--- torch/distributed/_tensor/examples/display_sharding_example.py
+++ torch/distributed/_tensor/examples/display_sharding_example.py
@@ -1,15 +1,37 @@
+import os
 from typing import Any, Dict
 
 import torch
 
+from torch.distributed._tensor import DeviceMesh, Shard
 from torch.distributed._tensor.debug import CommDebugMode
-
 from torch.distributed._tensor.debug.comm_mode import ModuleParamaterShardingTracker
+
+from torch.distributed.tensor.parallel import (
+    ColwiseParallel,
+    parallelize_module,
+    RowwiseParallel,
+)
 
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     MLPModule,
     MLPStacked,
+    NUM_DEVICES,
 )
+
+
+def get_device_type():
+    return (
+        "cuda"
+        if torch.cuda.is_available() and torch.cuda.device_count() >= 4
+        else "cpu"
+    )
+
+
+c10d_functional = torch.ops.c10d_functional
+
+aten = torch.ops.aten
+supported_ops = [aten.view.default, aten._to_copy.default]
 
 
 class DisplayShardingExample:
@@ -17,6 +39,11 @@ class DisplayShardingExample:
     Checks if the set of keys in ground truth dictionary and the set
     produced in advanced_module_tracker are in the same order
     """
+
+    def __init__(self, world_size, rank):
+        self.world_size = world_size
+        self.rank = rank
+        self.device_type = get_device_type()
 
     def same_set_of_keys(self, dict1, dict2):
         dict1_keys = []
@@ -54,9 +81,7 @@ class DisplayShardingExample:
         return module_parameters_dict
 
     def test_display_parameters_MLP(self):
-        """
-        Example of using obtaining all module's FQN and parameters for a given model
-        """
+        """Example of obtaining all module's FQN and parameters for a given model"""
 
         inp_size = [8, 10]
 
@@ -67,7 +92,6 @@ class DisplayShardingExample:
 
         LR = 0.25
 
-        optim = torch.optim.SGD(model.parameters(), lr=LR)
         comm_mode = CommDebugMode()
         module_tracker = ModuleParamaterShardingTracker()
 
@@ -91,7 +115,61 @@ class DisplayShardingExample:
             )
         )
 
+    def test_display_parameters_MLP_distributed(
+        self, is_seq_parallel=False, recompute_activation=False
+    ):
+        "Example of obtaining all module's FQN and parameters for a given distributed model and printing the sharding info"
+        device_mesh = DeviceMesh(
+            self.device_type,
+            torch.arange(0, NUM_DEVICES),
+        )
+        inp_size = [8, 10]
+        rng_seed = self.rank if is_seq_parallel else 0
+        torch.manual_seed(rng_seed)
+        inp = torch.rand(*inp_size, device=self.device_type)
+        model = MLPModule(self.device_type)
+
+        LR = 0.25
+
+        parallelize_plan = {
+            "net1": ColwiseParallel(input_layouts=Shard(0))
+            if is_seq_parallel
+            else ColwiseParallel(),
+            "net2": RowwiseParallel(output_layouts=Shard(0))
+            if is_seq_parallel
+            else RowwiseParallel(),
+        }
+
+        model = parallelize_module(model, device_mesh, parallelize_plan)
+
+        comm_mode = CommDebugMode()
+
+        with comm_mode:
+            output_tp = model(inp)
+            output_tp.sum().backward()
+
+        print(
+            self.same_set_of_keys(
+                self.ground_truth(model), comm_mode.get_parameter_info()
+            )
+        )
+
+        comm_mode.print_sharding_info()
+
+
+def run_example(world_size, rank):
+    # set manual seed
+    torch.manual_seed(0)
+
+    # run the example
+    instantiated_test = DisplayShardingExample(world_size, rank)
+    instantiated_test.test_display_parameters_MLP_distributed()
+
 
 if __name__ == "__main__":
-    instantiated_test = DisplayShardingExample()
-    instantiated_test.test_display_parameters_MLP()
+    # this script is launched via torchrun which automatically manages ProcessGroup
+    rank = int(os.environ["RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+    assert world_size == 4  # our example uses 4 worker ranks
+
+    run_example(world_size, rank)
