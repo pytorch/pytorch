a9a6e8e1c32adb49bf001aa8522a0d08eb988b5e
Anshul Sinha <50644008+sinhaanshul@users.noreply.github.com>
2024-06-07 11:13:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/distributed/_tensor/debug/comm_mode.py
+++ torch/distributed/_tensor/debug/comm_mode.py
@@ -2,14 +2,17 @@ from collections import defaultdict
 from typing import Any, Dict
 
 import torch
+from torch.autograd.graph import register_multi_grad_hook
 from torch.distributed._tensor.api import DTensor
 
-from torch.distributed._tensor.examples.advanced_module_tracker import (
-    ModuleParamaterShardingTracker,
+from torch.nn.modules.module import (
+    register_module_forward_hook,
+    register_module_forward_pre_hook,
 )
-
 from torch.utils._python_dispatch import TorchDispatchMode
 
+from torch.utils._pytree import tree_flatten
+from torch.utils.module_tracker import ModuleTracker
 
 funcol_native = torch.ops._c10d_functional
 funcol_py = torch.ops.c10d_functional
@@ -46,6 +49,40 @@ c10d_collective_ops = {
     c10d_ops.reduce_scatter_,
     c10d_ops.reduce_scatter_tensor_coalesced_,
 }
+
+
+class ModuleParamaterShardingTracker(ModuleTracker):
+    """
+    Inherits ModuleTracker and expands on its functionality to track the
+    parameters and sharding information of a model at a module-level
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.module_parameters_dict = {}
+
+    def _fw_pre_hook(self, mod, input):
+        name = super()._get_mod_name(mod)
+        super()._get_append_fn(name, False)()
+
+        args, _ = tree_flatten(input)
+        tensors = [a for a in args if isinstance(a, torch.Tensor) and a.requires_grad]
+        if tensors:
+            register_multi_grad_hook(tensors, super()._get_pop_fn(name, True))
+
+        for param_name, param in mod.named_parameters(recurse=False):
+            if name not in self.module_parameters_dict:
+                self.module_parameters_dict[name] = {}
+
+            self.module_parameters_dict[name][param_name] = param.data
+
+    def __enter__(self):
+        self.module_parameters_dict.clear()
+        self._fw_pre_handle = register_module_forward_pre_hook(self._fw_pre_hook)
+        self._fw_post_handle = register_module_forward_hook(super()._fw_post_hook)
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
 
 
 class CommDebugMode(TorchDispatchMode):
