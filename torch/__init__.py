d3522f23e3014a5cd9ce38999ad065e871a9a51a
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-07 14:42:15 +0000

    Update
    [ghstack-poisoned]

--- torch/__init__.py
+++ torch/__init__.py
@@ -76,7 +76,7 @@ __all__ = [
 # Load the extension module
 ################################################################################
 
-if platform.system() == 'Windows':
+if sys.platform == 'win32':
 
     def _load_dll_libraries():
         import sysconfig
