66e2ebfcdb7ad64e4db3a7af44768486fcb55526
Animesh Jain <anijain@umich.edu>
2024-06-08 14:14:36 -0700

    [dynamo][inlining inbuilt modules] Ensure BC for nn_module_stack
    [ghstack-poisoned]

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -853,9 +853,22 @@ class UserDefinedObjectVariable(UserDefinedVariable):
                 new_source = None
                 if self.source:
                     new_source = AttrSource(self.source, "__getattr__")
-                return variables.UserMethodVariable(
+                out = variables.UserMethodVariable(
                     getattr_fn, self, source=new_source
                 ).call_function(tx, [ConstantVariable.create(name)], {})
+                if getattr_fn is torch.nn.Module.__getattr__ and isinstance(
+                    out,
+                    (
+                        variables.UnspecializedNNModuleVariable,
+                        variables.NNModuleVariable,
+                    ),
+                ):
+                    # nn_module_stack source is BC surface area. Ensure that
+                    # mod._modules["linear"] is reflected as mod.linear for
+                    # nn_module_stack.
+                    out.set_nn_module_stack_source(AttrSource(self.source, name))
+                return out
+
             elif getattr_fn is not None:
                 unimplemented("UserDefined with non-function __getattr__")
 
