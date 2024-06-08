b3e094da81b435dac8743762bbd56e72aae75190
Aaron Orenstein <aorenste@fb.com>
2024-06-08 11:41:14 -0700

    Flip default value for mypy disallow_untyped_defs [7/11] (#127844)
    See #127836 for details.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127844
    Approved by: https://github.com/oulgen
    ghstack dependencies: #127842, #127843

--- torch/fx/experimental/shape_inference/infer_shape.py
+++ torch/fx/experimental/shape_inference/infer_shape.py
@@ -1,3 +1,4 @@
+# mypy: allow-untyped-defs
 import copy
 from collections import defaultdict
 
--- torch/nn/attention/_flex_attention.py
+++ torch/nn/attention/_flex_attention.py
@@ -1,3 +1,4 @@
+# mypy: allow-untyped-defs
 """This module implements the user facing API for flex_attention in PyTorch."""
 import functools
 from typing import Callable
