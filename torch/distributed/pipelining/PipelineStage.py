3dffa5bb9600d1c162fa33a663388c0b2c2e3cc3
Ke Wen <kw2501@meta.com>
2024-06-07 18:23:03 -0700

    [pipelining] Add pipe.build_stage() (#128240)
    Given `PipelineStage` name to manual side.
    Thus adding a method under `Pipe` to create PipelineStage.
    Moved `PipeInfo` to utils.py to avoid circular dependency between `_IR` and `PipelineStage`.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128240
    Approved by: https://github.com/wconstab, https://github.com/H-Huang

--- torch/distributed/pipelining/PipelineStage.py
+++ torch/distributed/pipelining/PipelineStage.py
@@ -15,13 +15,16 @@ from torch.nn.parallel import DistributedDataParallel
 
 from ._backward import stage_backward
 from ._debug import map_debug_info
-from ._IR import Pipe
-from ._utils import flatten_args, modify_graph_op_device, validate_tensors_metadata
+from ._utils import (
+    flatten_args,
+    modify_graph_op_device,
+    PipeInfo,
+    validate_tensors_metadata,
+)
 
 
 __all__ = [
     "PipelineStage",
-    "TracerPipelineStage",
 ]
 
 logger = logging.getLogger(__name__)
@@ -80,7 +83,8 @@ def _make_tensor_from_meta(
 class _PipelineStageBase(ABC):
     """
     Base class for pipeline stages.
-    Implements common methods used by both the `TracerPipelineStage` used by the tracing frontend and `PipelineStage`.
+    Defines or implements common methods used by the `_PipelineStage` used by
+    the tracing frontend and `PipelineStage` used by manual frontend.
     """
 
     def __init__(
@@ -97,7 +101,6 @@ class _PipelineStageBase(ABC):
             stage_index (int): The index of this stage.
             num_stages (int): The total number of stages in this pipeline.
             device (torch.device): The device to run this stage on.
-            num_microbatches (int): The number of microbatches to be run with this stage.
             group (Optional[dist.ProcessGroup]): The process group to use for communication.
                 If `None`, the default process group will be used.
                 Default: `None`.
@@ -641,9 +644,8 @@ class _PipelineStage(_PipelineStageBase):
         self,
         stage_module: torch.nn.Module,
         stage_index: int,
-        pipe_info: Pipe.PipeInfo,
+        pipe_info: PipeInfo,
         device: torch.device,
-        num_chunks: int,
         group: Optional[dist.ProcessGroup] = None,
     ):
         """
@@ -902,28 +904,6 @@ class _PipelineStage(_PipelineStageBase):
             f"{self.log_prefix} Grad recv info: {grad_recv_info_tuple}"  # noqa: G004
         )
         return grad_recv_info_tuple
-
-
-# TODO: Update this to be returned by helper method under Pipe (kwen)
-class TracerPipelineStage(_PipelineStage):
-    def __init__(
-        self,
-        pipe: Pipe,
-        stage_index: int,
-        device: torch.device,
-        num_chunks: int,  # To be cleaned
-        group: Optional[dist.ProcessGroup] = None,
-    ):
-        """
-        Create a pipeline stage given a `Pipe` (representing the whole pipeline) and a stage index.
-        """
-        # Find my stage module
-        stage_module = pipe.get_stage_module(stage_index)
-        # Get my pipe info
-        pipe_info = pipe.info()
-        super().__init__(
-            stage_module, stage_index, pipe_info, device, num_chunks, group
-        )
 
 
 # Manual PipelineStage functions and definition
