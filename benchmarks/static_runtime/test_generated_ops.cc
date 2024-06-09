4bdbcb934b93abc66f25691dadb337a72eecd5ac
David Berard <dberard@fb.com>
2024-06-08 18:46:08 -0700

    Update base for Update on "[WIP] add optional boolean args "prepend" to cumsum, cumprod"
    It's frequently convenient to get a `cumsum` where the result is padded with a 0 at the beginning (or `cumprod` with a 1 padded at the beginning). This introduces an optional arg to specify `prepend=True` which will add the 0 at the beginning.
    
    This PR only adds the signatures; it does not add the implementations. For the most part, this PR should add asserts if a user tries to call `cumsum` or `cumprod` with `prepend=True`. A follow-up PR will add the actual implementations.
    
    TODO
    * [ ] figure out how to do the AOTI stuff
    * [x] ONNX - what should I update / not update
      - done I think
    * [ ] TorchXLA - same ^^
    * [ ] address any other failures
    * [x] choose a better name than "full" ?
      - chose "prepend" (Ed's suggestion)
    * [ ] .dimname versions
    
    other TODO but for later PRs:
    * refs / decompositions / lowerings
    * common_methods_invocations
    
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10
    
    Differential Revision: [D58329265](https://our.internmc.facebook.com/intern/diff/D58329265)
    
    [ghstack-poisoned]

--- benchmarks/static_runtime/test_generated_ops.cc
+++ benchmarks/static_runtime/test_generated_ops.cc
@@ -7967,7 +7967,6 @@ TEST(StaticRuntime, autogen_diagonal) {
   auto offset0 = 0;
   auto dim10 = 2;
   auto dim20 = 1;
-  auto dim00 = 1;
   std::vector<IValue> args{self0, offset0, dim10, dim20};
   testStaticRuntime(script, args);
 }
@@ -7985,7 +7984,6 @@ TEST(StaticRuntime, autogen_linalg_diagonal) {
   auto offset0 = 0;
   auto dim10 = 2;
   auto dim20 = 1;
-  auto dim00 = 1;
   std::vector<IValue> args{A0, offset0, dim10, dim20};
   testStaticRuntime(script, args);
 }
