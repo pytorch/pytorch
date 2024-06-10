0bd2961edf75ce17f995f718af7045be34010a86
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-10 19:16:57 +0000

    Update
    [ghstack-poisoned]

--- torch/__init__.py
+++ torch/__init__.py
@@ -1987,8 +1987,6 @@ from torch.autograd import (  # usort: skip
     set_grad_enabled as set_grad_enabled,
 )
 
-import torch.utils.backcompat
-import torch.utils.data
 from torch import (
     __config__ as __config__,
     __future__ as __future__,
@@ -2015,6 +2013,7 @@ from torch import (
     special as special,
     testing as testing,
     types as types,
+    utils as utils,
     xpu as xpu,
 )
 from torch.signal import windows as windows
