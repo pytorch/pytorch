ba05ddeb005642156b33e9eade6c2a628e0b19ff
Jiashen Cao <jiashenc@meta.com>
2024-06-10 11:08:19 -0700

    remove extra print

--- test/export/test_converter.py
+++ test/export/test_converter.py
@@ -372,7 +372,6 @@ class TestConverter(TestCase):
         class MUnpackList(torch.nn.Module):
             def forward(self, x):
                 x, y = torch.split(x, 2)
-                # print(x, y)
                 return x + y
 
         class MUnpackTuple(torch.nn.Module):
