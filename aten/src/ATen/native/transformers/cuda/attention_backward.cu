cb864b43cbe0ad09c99118616c7493b6470a67a9
Eddie Yan <eddiey@nvidia.com>
2024-06-10 21:23:04 +0000

    check in

--- aten/src/ATen/native/transformers/cuda/attention_backward.cu
+++ aten/src/ATen/native/transformers/cuda/attention_backward.cu
@@ -194,12 +194,11 @@ std::tuple<Tensor, Tensor, Tensor> _scaled_dot_product_cudnn_attention_backward_
 
     const int64_t batch_size = query.size(0);
     const int64_t num_heads = query.size(1);
-    const int64_t head_dim = query.size(3);
+    const int64_t head_dim_k = query.size(3);
+    const int64_t head_dim_v = value.size(3);
     const int64_t max_seqlen_batch_q = query.size(1);
     const int64_t max_seqlen_batch_k = key.size(1);
-
     const auto softmax_scale = sdp::calculate_scale(query, scale).as_float_unchecked();
-
     auto dq = at::empty_like(query);
     auto dk = at::empty_like(key);
     auto dv = at::empty_like(value);
@@ -207,7 +206,8 @@ std::tuple<Tensor, Tensor, Tensor> _scaled_dot_product_cudnn_attention_backward_
                         num_heads /*int64_t h*/,
                         max_seqlen_batch_q /*int64_t s_q*/,
                         max_seqlen_batch_k /*int64_t s_kv*/,
-                        head_dim /*int64_t d*/,
+                        head_dim_k /*int64_t d_k*/,
+                        head_dim_v /*int64_t d_v*/,
                         softmax_scale /*float scaling_factor*/,
                         is_causal /*bool is_causal*/,
                         dropout_p /*float dropout_probability*/,
