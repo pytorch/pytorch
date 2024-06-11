7d458c9cc3f3059148d023bacfbd75ce2debe6a3
Xu Han <xu.han@outlook.com>
2024-06-11 03:06:12 +0000

    delete tmp code and add function guide.

--- torch/_inductor/cpp_builder.py
+++ torch/_inductor/cpp_builder.py
@@ -198,7 +198,6 @@ def _remove_dir(path_dir):
 
 def run_command_line(cmd_line, cwd=None):
     cmd = shlex.split(cmd_line)
-    print("!!!! cmd: ", cmd)
     try:
         status = subprocess.check_output(args=cmd, cwd=cwd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
@@ -959,6 +958,20 @@ class CppTorchCudaOptions(CppTorchOptions):
 def get_name_and_dir_from_output_file_path(
     file_path: str,
 ):
+    """
+    This function help prepare parameters to new cpp_builder.
+    Example:
+        input_code: /tmp/tmpof1n5g7t/5c/c5crkkcdvhdxpktrmjxbqkqyq5hmxpqsfza4pxcf3mwk42lphygc.cpp
+        name, dir = get_name_and_dir_from_output_file_path(input_code)
+    Run result:
+        name = c5crkkcdvhdxpktrmjxbqkqyq5hmxpqsfza4pxcf3mwk42lphygc
+        dir = /tmp/tmpof1n5g7t/5c/
+
+    put 'name' and 'dir' to CppBuilder's 'name' and 'output_dir'.
+    CppBuilder --> get_target_file_path will format output path accoding OS:
+    Linux: /tmp/tmppu87g3mm/zh/czhwiz4z7ca7ep3qkxenxerfjxy42kehw6h5cjk6ven4qu4hql4i.so
+    Windows: [Windows temp path]/tmppu87g3mm/zh/czhwiz4z7ca7ep3qkxenxerfjxy42kehw6h5cjk6ven4qu4hql4i.dll
+    """
     name_and_ext = os.path.basename(file_path)
     name, ext = os.path.splitext(name_and_ext)
     dir = os.path.dirname(file_path)
