Fix rowwiseNHWC logic and apply scaling properly.

test/test_nn.py
@@ -7802,6 +7802,8 @@ class TestNNDeviceType(NNTestCase):
             gn = nn.GroupNorm(g, c, eps=0).to(device, dtype)
             gn.weight.data.fill_(1)
             gn.bias.data.fill_(0)
+            sentinel = x.is_contiguous(memory_format=torch.channels_last);
+            print(f"${sentinel} sentinel")
             output = gn(x)
             out_reshaped = output.view(b, g, -1)
             mean = out_reshaped.mean(-1)
@@ -8386,7 +8388,8 @@ class TestNNDeviceType(NNTestCase):
             with torch.backends.cudnn.flags(enabled=False):
                 _test_module_empty_input(self, mod, inp)
 
-    @onlyCPU
+    #@onlyCPU
+    @dtypesIfCUDA(torch.float, torch.double)
     @dtypes(torch.float, torch.double, torch.bfloat16, torch.half)
     def test_groupnorm_nhwc(self, device, dtype):
         def helper(self, size, groups, memory_format, is_mixed):
@@ -8414,9 +8417,13 @@ class TestNNDeviceType(NNTestCase):
             out.backward(grad)
             ref_out = ref_gn(ref_input)
             ref_out.backward(ref_grad)
-
+           
             self.assertTrue(out.is_contiguous(memory_format=memory_format))
             self.assertTrue(ref_out.is_contiguous(memory_format=torch.contiguous_format))
+            
+            # for i, (a, b) in enumerate(zip(out.flatten(), ref_out.flatten())):
+            #     if not torch.isclose(a, b, rtol=8e-3, atol=5e-4):
+            #      #   print(f"Index {i}: out = {a.item()}, ref_out = {b.item()}")
             self.assertEqual(out, ref_out)
             # parameters in bfloat16/Half is not recommended
             atol = 5e-4
@@ -8433,9 +8440,11 @@ class TestNNDeviceType(NNTestCase):
             helper(self, (4, 40, 40, 40), 2, torch.channels_last, is_mixed)
             helper(self, (2, 30, 50, 50), 3, torch.channels_last, is_mixed)
             helper(self, (2, 60, 50, 50), 3, torch.channels_last, is_mixed)
-            helper(self, (2, 9, 7, 11, 15), 3, torch.channels_last_3d, is_mixed)
-            helper(self, (2, 9, 7, 200, 15), 3, torch.channels_last_3d, is_mixed)
-            helper(self, (2, 60, 7, 200, 15), 3, torch.channels_last_3d, is_mixed)
+
+            if device == 'cpu':
+                helper(self, (2, 9, 7, 11, 15), 3, torch.channels_last_3d, is_mixed)
+                helper(self, (2, 9, 7, 200, 15), 3, torch.channels_last_3d, is_mixed)
+                helper(self, (2, 60, 7, 200, 15), 3, torch.channels_last_3d, is_mixed)
 
     @onlyNativeDeviceTypes
     def test_GroupNorm_memory_format(self, device):