ec6cf8b7b7e5c19b2515421c7f2889ad12a6c0c4
Xu Han <xu.han@outlook.com>
2024-06-12 10:05:31 +0800

    Update torch/cpu/__init__.py
    Co-authored-by: Jiong Gong <jiong.gong@intel.com>

--- torch/cpu/__init__.py
+++ torch/cpu/__init__.py
@@ -41,7 +41,7 @@ def _is_cpu_support_avx512() -> bool:
 
 def _is_cpu_support_vnni() -> bool:
     r"""Returns a bool indicating if CPU supports VNNI."""
-    # Note: Currently, it only check avx512_vnni, Maybe need to support avx2_vnni later.
+    # Note: Currently, it only checks avx512_vnni, will add the support of avx2_vnni later.
     return torch._C._cpu._is_cpu_support_avx512_vnni()
 
 
