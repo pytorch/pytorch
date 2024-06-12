14845dac6f5683c6ca0694d0e8089c7fcbe8a25f
Jiashen Cao <jiashenc@meta.com>
2024-06-12 13:53:47 -0700

    remove print

--- torch/_export/passes/lift_constants_pass.py
+++ torch/_export/passes/lift_constants_pass.py
@@ -90,7 +90,6 @@ def get_constant_fqn(node: torch.fx.Node, constant_name: str) -> str:
     # The FQN of the constant tensor in the state dict should
     # correspond to the module where the constant tensor was
     # originally used.
-    print(f"Get Constant FQN: {node}:{node.op}:{node.meta}")
     parent_fqn = list(node.meta["nn_module_stack"].values())[-1][0]
     if len(parent_fqn) > 0:
         return f"{parent_fqn}.{constant_name}"
