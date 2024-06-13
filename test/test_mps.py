6df26a0ec2c569c0fb0a3edb51c8ddcaf5c83602
Joona Havukainen <jhavukainen@apple.com>
2024-06-12 18:08:29 -0700

    Fixes: #128429
    Fixes triu op when k > 0 and the lower triangle of the input tensor contains inf leading to NaNs in the computation.

--- test/test_mps.py
+++ test/test_mps.py
@@ -1617,6 +1617,14 @@ class TestMPS(TestCaseMPS):
             a = a.to(dtype).to("mps")
             self.compare_with_numpy(torch.exp, np.exp, a)
 
+    def test_triu_inf(self, device="mps", dtype=torch.float):
+        for diag in [-1, 0, 1]:
+            mask = torch.full((3, 6, 6), float("-inf"))
+            mask_mps = mask.clone().detach().to('mps')
+            cpu_ref = torch.triu(mask, diagonal=diag)
+            mps_out = torch.triu(mask_mps, diagonal=diag)
+            self.assertEqual(cpu_ref, mps_out)
+
     def test_exp1(self, device="mps", dtype=torch.float):
         input = torch.tensor([-0.1, 3.0, -0.9]).to('mps')
         output = torch.exp(input).to('cpu')
