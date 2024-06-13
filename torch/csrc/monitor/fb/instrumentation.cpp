f98b09dfbe56b6e0bca8b58775f5e9d05230db36
James Perng <jamesperng@meta.com>
2024-06-13 03:27:25 -0700

    [pytorch][logging] add fb internal ODS implementation of wait counter
    * created fb internal implementation in `caffe2/torch/csrc/monitor/fb/instrumentation.cpp`
        * uses `facebook::data_preproc::WaitCounterUs` under the hood by having `WaitCounterImpl` trivially subclass it.
        * this makes `WaitCounterHandle` a glorified pointer to `facebook::data_preproc::WaitCounterUs` which is statically defined in the `STATIC_WAIT_COUNTER` macro making these pointers Meyer's singletons.
            * `facebook::data_preproc::WaitCounterUs` uses 3 singletons:
                 1. `std::unique_ptr<DynamicCounter::State>` map — leaky singleton
                 2. `std::weak_ptr<WaitCounterUs::State>` map — leaky singleton
                 3. publisherSingleton — normal singleton since it manages resources (threads)
            * `facebook::data_preproc::WaitCounterUs` actually owns shared pointers to the state and its destructor will remove it from the `std::weak_ptr<WaitCounterUs::State>` map when the reference count for the state hits 0.
    * linked `caffe2/torch/csrc/monitor/fb/instrumentation.cpp` and added `//data_preproc/common:counters` (dpp dependency) to `caffe2/fb/fbcode/target_definitions.bzl`
    * wrapped OSS null implementation in `#ifndef FBCODE_CAFFE2` so that internally we use the fb internal implementation.
    
    as a follow-up I might move the counter implementation out of the data_preproc/counters library to a more common ai infra library?
    
    Differential Revision: [D58458751](https://our.internmc.facebook.com/intern/diff/D58458751/)
    
    [ghstack-poisoned]

--- torch/csrc/monitor/fb/instrumentation.cpp
+++ torch/csrc/monitor/fb/instrumentation.cpp
@@ -1,5 +1,5 @@
-#ifndef FBCODE_CAFFE2
 #include <torch/csrc/monitor/instrumentation.h>
+#include "data_preproc/common/Counters.h"
 
 #include <chrono>
 #include <string_view>
@@ -7,19 +7,24 @@
 namespace torch {
 namespace monitor {
 
+namespace detail {
+class WaitCounterImpl : public facebook::data_preproc::WaitCounterUs {
+ public:
+  explicit WaitCounterImpl(std::string_view key)
+      : facebook::data_preproc::WaitCounterUs(key) {}
+};
+} // namespace detail
+
 WaitCounterHandle::WaitCounterHandle(std::string_view key)
-    : impl_(nullptr) {
-  // implement
-}
+    : impl_(std::make_shared<detail::WaitCounterImpl>(key)) {}
 
 void WaitCounterHandle::start(std::chrono::steady_clock::time_point now) {
-  // implement
+  impl_->start(now);
 }
 
 void WaitCounterHandle::stop(std::chrono::steady_clock::time_point now) {
-  // implement
+  impl_->stop(now);
 }
 
 } // namespace monitor
 } // namespace torch
-#endif
