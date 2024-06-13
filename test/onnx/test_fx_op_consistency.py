9839eecd7d1f46a77015dd30d8123c59552e2b4c
titaiwangms <titaiwang@microsoft.com>
2024-06-13 18:30:59 +0000

    revert nema changed and delete sarif log

--- test/onnx/test_fx_op_consistency.py
+++ test/onnx/test_fx_op_consistency.py
@@ -1898,9 +1898,6 @@ def _run_test_output_match(
                         )
                     else:
                         raise e
-                onnx_program.save_diagnostics(
-                    f"{test_suite.id()}_sample_{i}_op_{op.name}.sarif"
-                )
                 _compare_onnx_and_torch_exported_program(
                     model,
                     onnx_program,
@@ -2038,9 +2035,7 @@ class TestOnnxModelOutputConsistency(onnx_test_common._TestONNXRuntime):
 for opset in onnx_test_common.FX_TESTED_OPSETS:
     for model_type in pytorch_test_common.TorchModelType:
         # The name needs to match the parameterized_class name.
-        test_class_name = (
-            f"TestOnnxModelOutputConsistency_opset_{opset}_model_type_{model_type.name}"
-        )
+        test_class_name = f"TestOnnxModelOutputConsistency_opset_version_{opset}_model_type_TorchModelType.{model_type.name}"
         onnx_test_common.add_decorate_info(
             OPS_DB,
             test_class_name,
