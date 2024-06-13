63397e33c1e188724c08af9eefcfa301a5984535
Yanbo Liang <ybliang8@gmail.com>
2024-06-13 11:46:55 -0700

    Update

--- benchmarks/gpt_fast/benchmark.py
+++ benchmarks/gpt_fast/benchmark.py
@@ -8,9 +8,13 @@ from triton.testing import do_bench
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.utils.flop_counter import FlopCounterMode
 
 WARMUP_ITER = 5
+
+A100_80G_BF16_TFLOPS = 312
+
+torch._inductor.config.coordinate_descent_tuning = True
 
 
 @dataclasses.dataclass
@@ -24,28 +28,30 @@ class Experiment:
 class SimpleMLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, dtype):
         super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim, dtype=dtype)
-        self.ln1 = nn.LayerNorm(hidden_dim, dtype=dtype)
-        self.fc2 = nn.Linear(hidden_dim, output_dim, dtype=dtype)
-        self.ln2 = nn.LayerNorm(output_dim, dtype=dtype)
+        self.layers = nn.ModuleList(
+            [
+                nn.Linear(input_dim, hidden_dim, dtype=dtype),
+                nn.LayerNorm(hidden_dim, dtype=dtype),
+                nn.Linear(hidden_dim, output_dim, dtype=dtype),
+                nn.LayerNorm(output_dim, dtype=dtype),
+            ]
+        )
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = F.gelu(self.ln1(x))
-        x = self.fc2(x)
-        x = F.gelu(self.ln2(x))
+        for layer in self.layers:
+            x = layer(x)
         return x
 
 
 def run_mlp_layer_norm_gelu():
-    dtype_memory_bandwidth_map = {
-        torch.bfloat16: "1805",
+    dtype_flops_utilization_map = {
+        torch.bfloat16: "0.72",
     }
     input_shapes = [1024, 4096, 8192, 16384]
     intermediate_size = 14336
     results = []
-    for dtype, expected_memory_bandwidth in dtype_memory_bandwidth_map.items():
-        memory_bandwidth = 0
+    for dtype, expected_flops_utilization in dtype_flops_utilization_map.items():
+        flops_utilization = 0
         for D in input_shapes:
             mod = SimpleMLP(
                 input_dim=D, hidden_dim=intermediate_size, output_dim=D, dtype=dtype
@@ -53,24 +59,27 @@ def run_mlp_layer_norm_gelu():
 
             x = torch.randn(D, device="cuda", dtype=torch.bfloat16)
 
-            compiled_mod = torch.compile(mod)
+            with FlopCounterMode(display=False) as mode:
+                mod(x)
+
+            flops = mode.get_total_flops()
+
+            compiled_mod = torch.compile(mod, dynamic=False)
 
             for _ in range(WARMUP_ITER):
                 compiled_mod(x)
 
             us_per_iter = do_bench(lambda: compiled_mod(x)) * 1000
-            memory_bandwidth += (
-                (1e6 / us_per_iter) * 4 * D * intermediate_size * dtype.itemsize / 1e9
-            )
+            flops_utilization += us_per_iter * flops / 1e9 / A100_80G_BF16_TFLOPS
 
-        memory_bandwidth = memory_bandwidth / len(input_shapes)
+        flops_utilization = flops_utilization / len(input_shapes)
         dtype_str = str(dtype).replace("torch.", "")
         results.append(
             Experiment(
                 f"mlp_layer_norm_gelu_{dtype_str}",
-                "memory_bandwidth(GB/s)",
-                expected_memory_bandwidth,
-                f"{memory_bandwidth:.02f}",
+                "flops_utilization",
+                expected_flops_utilization,
+                f"{flops_utilization:.02f}",
             )
         )
     return results
@@ -90,7 +99,7 @@ def run_layer_norm():
 
             x = torch.randn(BS, D, device="cuda", dtype=dtype)
 
-            compiled_mod = torch.compile(mod)
+            compiled_mod = torch.compile(mod, dynamic=False)
 
             for _ in range(WARMUP_ITER):
                 compiled_mod(x)
@@ -112,6 +121,7 @@ def run_layer_norm():
 
 
 def run_gather_gemv():
+    torch._inductor.config.coordinate_descent_tuning = True
     E = 8
     dtype_memory_bandwidth_map = {
         torch.int8: "1195",
@@ -130,13 +140,13 @@ def run_gather_gemv():
             x = torch.randn(D, device="cuda", dtype=torch.bfloat16)
             score_idxs = torch.tensor([3, 5], device="cuda")
 
-            compiled_fn = torch.compile(gather_gemv)
+            compiled_fn = torch.compile(gather_gemv, dynamic=False)
 
             for _ in range(WARMUP_ITER):
                 compiled_fn(W, score_idxs, x)
 
             us_per_iter = do_bench(lambda: compiled_fn(W, score_idxs, x)) * 1000
-            memory_bandwidth += (1e6 / us_per_iter) * 4 * D * D * dtype.itemsize / 1e9
+            memory_bandwidth += (1e6 / us_per_iter) * 2 * D * D * dtype.itemsize / 1e9
 
         memory_bandwidth = memory_bandwidth / len(input_shapes)
         dtype_str = str(dtype).replace("torch.", "")
@@ -152,6 +162,7 @@ def run_gather_gemv():
 
 
 def run_gemv():
+    torch._inductor.config.coordinate_descent_tuning = True
     dtype_memory_bandwidth_map = {
         torch.int8: "1080",
         torch.bfloat16: "1750",
@@ -168,7 +179,7 @@ def run_gemv():
             W = torch.randn(D, D, device="cuda").to(dtype=dtype)
             x = torch.randn(D, device="cuda", dtype=torch.bfloat16)
 
-            compiled_fn = torch.compile(gemv)
+            compiled_fn = torch.compile(gemv, dynamic=False)
 
             for _ in range(WARMUP_ITER):
                 compiled_fn(W, x)
