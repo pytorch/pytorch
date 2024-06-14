5cfc979d92ef9562045d0801924d91a5efc9cabb
Isuru Fernando <ifernando@quansight.com>
2024-06-14 21:47:07 +0000

    Update
    [ghstack-poisoned]

--- test/onnx/test_fx_op_consistency.py
+++ test/onnx/test_fx_op_consistency.py
@@ -177,6 +177,11 @@ EXPECTED_SKIPS_OR_FAILS_WITH_DTYPES: Tuple[onnx_test_common.DecorateMeta, ...] =
     ),
     xfail(
         "_unsafe_masked_index_put_accumulate",
+        reason="fixme: Status Message: updates tensor should have shape equal to "
+               "indices.shape[:-1] + data.shape[indices.shape[-1]:]",
+    ),
+    xfail(
+        "_unsafe_masked_index_put_accumulate",
         dtypes=onnx_test_common.BOOL_TYPES,
         reason=onnx_test_common.reason_onnx_runtime_does_not_support("Where", "bool"),
     ),
