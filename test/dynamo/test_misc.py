77edebf6ef369b105746848613cd9633e3464503
Yueming Hao <yhao@meta.com>
2024-06-14 12:52:05 -0700

    add unit test

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -574,6 +574,25 @@ class MiscTests(torch._inductor.test_case.TestCase):
         cleanup_op("mylib::foo")
         del lib
 
+    def test_auto_functionalize_can_with_none_return(self):
+        lib = torch.library.Library("mylib", "FRAGMENT")
+        lib.define("foo(Tensor x, Tensor(a!) out) -> None")
+
+        def foo_impl(x, out):
+            out.copy_(x)
+
+        lib.impl("foo", foo_impl, "CompositeExplicitAutograd")
+        x = torch.randn(3)
+        out = torch.zeros(3)
+
+        @torch.compile
+        def f(x, out):
+            torch.ops.mylib.foo(x, out)
+
+        f(x, out)
+        cleanup_op("mylib::foo")
+        del lib
+
     def test_user_defined_setattr1(self):
         @torch.compile(backend="eager", fullgraph=True)
         def fn(obj):
