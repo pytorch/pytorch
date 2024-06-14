f4494f513ab2fa401717ed14288c8ce2de3f50b6
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-06-14 01:44:24 +0000

    Revert "[ONNX] Add upsample trilinear to skip decomp (#128259)"
    This reverts commit b72989a2b5ac4637612e31e325d7c8233fcbd7a1.
    
    Reverted https://github.com/pytorch/pytorch/pull/128259 on behalf of https://github.com/huydhn due to Sorry for reverting your change but its ONNX job is failing in trunk https://hud.pytorch.org/pytorch/pytorch/commit/b72989a2b5ac4637612e31e325d7c8233fcbd7a1 ([comment](https://github.com/pytorch/pytorch/pull/128259#issuecomment-2167058937))

--- test/onnx/test_fx_op_consistency.py
+++ test/onnx/test_fx_op_consistency.py
@@ -527,7 +527,8 @@ EXPECTED_SKIPS_OR_FAILS_WITH_DTYPES: Tuple[onnx_test_common.DecorateMeta, ...] =
     ),
     xfail(
         "gather",
-        reason="GatherElements op: Rank of input 'data' needs to be equal to rank of input 'indices'"
+        reason="HandleNegativeAxis(int64_t, int64_t) IsAxisInRange(axis, tensor_rank) was \
+            false. axis 0 is not in valid range [-0,-1]"
     ),
     xfail(
         "geometric",
@@ -1516,6 +1517,7 @@ SKIP_XFAIL_SUBTESTS_WITH_MATCHER_AND_MODEL_TYPE: tuple[
         "nn.functional.batch_norm",
         matcher=lambda sample: sample.kwargs.get("training") is True
         and any(arg is not None for arg in sample.args[2:4]),
+        model_type=pytorch_test_common.TorchModelType.TORCH_EXPORT_EXPORTEDPROGRAM,
         reason="Flaky failure: https://github.com/pytorch/pytorch/issues/115106",
     ),
     xfail(
