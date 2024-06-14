1bbed3d69a328474a30b510f382ddcd1c29614e3
Xu Han <xu.han@outlook.com>
2024-06-14 15:41:36 +0000

    test mac os UTs.

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1302,13 +1302,7 @@ class VecISA:
 #include <ATen/cpu/vec/vec.h>
 #endif
 
-#if defined(_WIN32)
-#define __at_align__ __declspec(align(64))
-#else
-#define __at_align__ __attribute__((aligned(64)))
-#endif
-
-__at_align__ float in_out_ptr0[16] = {0.0};
+__attribute__((aligned(64))) float in_out_ptr0[16] = {0.0};
 
 extern "C" void __avx_chk_kernel() {
     auto tmp0 = at::vec::Vectorized<float>(1);
