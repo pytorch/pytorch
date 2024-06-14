0d437d21bd0a85b1f3b40e61e524fedbdf324b0e
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2024-06-14 16:50:18 -0700

    test xfail

--- test/test_mps.py
+++ test/test_mps.py
@@ -599,11 +599,6 @@ def mps_ops_modifier(ops):
         # - MPS output: tensor([102.6681, inf])
         # In the latter case, inf is probably correct (this is what scipy does).
         'polygamma': [torch.float32, torch.uint8],
-        #'polygammapolygamma_n_0': [torch.int16, torch.int8],
-        'polygammapolygamma_n_2': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_1': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_3': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_4': [torch.float32, torch.int16, torch.int8],
         'special.polygamma': [torch.float32, torch.int16, torch.int32, torch.int8],
         'special.polygammaspecial_polygamma_n_0': [torch.float32, torch.int16, torch.int8],
 
@@ -662,11 +657,6 @@ def mps_ops_modifier(ops):
         # - MPS output: tensor([102.6681, inf])
         # In the latter case, inf is probably correct (this is what scipy does).
         'polygamma': [torch.float32, torch.uint8],
-        #'polygammapolygamma_n_0': [torch.int16, torch.int8],
-        'polygammapolygamma_n_2': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_1': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_3': [torch.float32, torch.int16, torch.int8],
-        'polygammapolygamma_n_4': [torch.float32, torch.int16, torch.int8],
         'special.polygamma': [torch.float32, torch.int16, torch.int32, torch.int8],
         'special.polygammaspecial_polygamma_n_0': [torch.float32, torch.int16, torch.int8],
     }
