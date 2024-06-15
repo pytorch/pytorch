060fec43361814551d9db8c016bd0561b8def462
Blaine Burton Rister <145300525+blaine-rister@users.noreply.github.com>
2024-06-14 18:04:36 -0700

    always use block ptr

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -734,7 +734,7 @@ class triton:
     spill_threshold: int = 16
 
     # Generate code containing the newer tl.make_block_ptr() API for loads/store
-    use_block_ptr = False
+    use_block_ptr = True
 
     # Inject a bug into our relu implementation; useful for testing our repro
     # extraction and minification functionality.
