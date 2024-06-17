32542be764ffea2797bc4daa963667ba4f3456d2
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-18 02:52:22 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/nn/qat/modules/__init__.py
+++ torch/nn/qat/modules/__init__.py
@@ -4,15 +4,12 @@ r"""QAT Modules.
 This package is in the process of being deprecated.
 Please, use `torch.ao.nn.qat.modules` instead.
 """
+from torch.ao.nn.qat.modules.conv import Conv1d, Conv2d, Conv3d
+from torch.ao.nn.qat.modules.embedding_ops import Embedding, EmbeddingBag
 from torch.ao.nn.qat.modules.linear import Linear
-from torch.ao.nn.qat.modules.conv import Conv1d
-from torch.ao.nn.qat.modules.conv import Conv2d
-from torch.ao.nn.qat.modules.conv import Conv3d
-from torch.ao.nn.qat.modules.embedding_ops import EmbeddingBag, Embedding
 
-from . import conv
-from . import embedding_ops
-from . import linear
+from . import conv, embedding_ops, linear
+
 
 __all__ = [
     "Linear",
