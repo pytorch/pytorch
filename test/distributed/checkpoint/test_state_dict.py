1a527915a64b8e5f60951715b09fa294b1a8844f
Chien-Chin Huang <chienchin@fb.com>
2024-06-17 09:54:11 -0700

    [DSD] Correctly handle shared parameters for optimizer state_dict (#128685)
    *
    Fixes https://github.com/pytorch/pytorch/issues/128011
    
    See the discussion in https://github.com/pytorch/pytorch/pull/128076
    
    Current implementation of `set_optimizer_state_dict()` assumes that all the fqns returned by `_get_fqns()` must exist in the optimizer state_dict. This is not true if the model has shared parameters. In such a case, only one fqn of the shared parameters will appear in the optimizer state_dict. This PR addresses the issue.
    
    Differential Revision: [D58573487](https://our.internmc.facebook.com/intern/diff/D58573487/)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128685
    Approved by: https://github.com/LucasLLC

--- test/distributed/checkpoint/test_state_dict.py
+++ test/distributed/checkpoint/test_state_dict.py
@@ -851,6 +851,33 @@ class TestStateDict(DTensorTestBase, VerifyStateDictMixin):
             ):
                 get_model_state_dict(model)
 
+    @with_comms
+    @skip_if_lt_x_gpu(2)
+    def test_shared_weight(self):
+        class TiedEmbeddingModel(nn.Module):
+            def __init__(self, vocab_size, embedding_dim):
+                super().__init__()
+                self.embedding = nn.Embedding(vocab_size, embedding_dim)
+                self.decoder = nn.Linear(embedding_dim, vocab_size)
+                self.decoder.weight = self.embedding.weight  # Tying weights
+
+            def forward(self, input):
+                input = (input * 10).to(torch.int)
+                embedded = self.embedding(input)
+                output = self.decoder(embedded)
+                return output
+
+        def init_model_optim():
+            device_mesh = init_device_mesh("cuda", (self.world_size,))
+            orig_model = TiedEmbeddingModel(10000, 300).to(torch.device("cuda"))
+            orig_optim = torch.optim.AdamW(orig_model.parameters(), lr=1e-3)
+            copy_optim = torch.optim.AdamW(orig_model.parameters(), lr=1e-3)
+            dist_model = FSDP(copy.deepcopy(orig_model), device_mesh=device_mesh)
+            dist_optim = torch.optim.AdamW(dist_model.parameters(), lr=1e-3)
+            return orig_model, orig_optim, copy_optim, dist_model, dist_optim
+
+        self._test_save_load(init_model_optim)
+
 
 class TestNoComm(MultiProcessTestCase):
     def setUp(self) -> None:
