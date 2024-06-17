c82df1262b1263b73369262efe1f8e91818b6568
Will Feng <yf225@cornell.edu>
2024-06-17 13:39:25 -0700

    Update
    [ghstack-poisoned]

--- torch/_functorch/_aot_autograd/dispatch_and_compile_graph.py
+++ torch/_functorch/_aot_autograd/dispatch_and_compile_graph.py
@@ -282,7 +282,7 @@ def aot_dispatch_autograd_graph(
     # a fake tensor. Unlikely.
     # See Note: [Fake Modules and AOTAutograd]
     torch._dynamo.utils.assert_no_fake_params_or_buffers(fx_g)
-    fx_g.graph.eliminate_dead_code()
+    # fx_g.graph.eliminate_dead_code()
 
     fx_g.recompile()
     # TODO: in AOTAutograd, we create metadata like _indices_of_inps_to_detach to detect
