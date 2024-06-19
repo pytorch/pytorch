069ab35936ec810cd7552471668f6b8b69b5113b
Xu Han <xu.han@outlook.com>
2024-06-19 13:11:40 +0000

    optimze func name.

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -3964,8 +3964,8 @@ class KernelGroup:
         kernel_name = str(Placeholder.DESCRIPTIVE_NAME) if name is None else name
         arg_defs, _, _ = self.args.cpp_argdefs()
         arg_defs = ",\n".ljust(25).join(arg_defs)
-        func_export = self.get_export_declaration()
-        code.writeline(f'extern "C" {func_export} void {kernel_decl_name}({arg_defs})')
+        func_export_decl = self.get_export_declaration()
+        code.writeline(f'extern "C" {func_export_decl} void {kernel_decl_name}({arg_defs})')
 
         # 3. Function body
         with code.indent():
