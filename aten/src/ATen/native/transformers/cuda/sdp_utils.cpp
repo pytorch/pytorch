2695f5954ab425160a305494262f0666bf264610
Apurva <appy@meta.com>
2024-06-19 11:35:12 -0700

    Lint fixes

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -588,7 +588,7 @@ bool can_use_flash_attention(sdp_params const& params, bool debug) {
   }
   if (has_only_dense_inputs(params)) {
     constexpr auto dense_constraints = array_of<bool (*)(sdp_params const&, bool)>(
-        check_batch_size_and_num_heads_dense<true>, // supports_grouped_query_attention = false,
+        check_batch_size_and_num_heads_dense<true>, // supports_grouped_query_attention = true,
         check_nonzero_sequence_lengths_dense,
         check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim=*/>);
     for (auto& constraint : dense_constraints) {
