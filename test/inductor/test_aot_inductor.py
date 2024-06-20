7beb583284eaebf5fe5ea26b526dd7924335cfa5
Ying Zhao <yingzhaoseattle@meta.com>
2024-06-20 14:28:17 -0700

    fix cpp compilation error

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -3028,7 +3028,7 @@ CPU_TEST_FAILURES = {
     ),
     "test_dynamic_cat": fail_minimal_arrayref_interface(),
     # https://github.com/pytorch/pytorch/issues/122978
-    "test_dynamic_scalar": fail_stack_allocation(is_skip=True),
+    # "test_dynamic_scalar": fail_stack_allocation(is_skip=True),
     # https://github.com/pytorch/pytorch/issues/122980
     "test_fft_c2c": fail_stack_allocation(is_skip=True),
     # TODO: test_freezing_abi_compatible_cpu fails,
