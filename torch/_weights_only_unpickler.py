f9723fac2c9119b7bdeaf1f868f061e7bdd5ff05
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2024-06-21 14:15:49 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_weights_only_unpickler.py
+++ torch/_weights_only_unpickler.py
@@ -98,8 +98,6 @@ def _get_user_allowed_globals():
     rc: Dict[str, Any] = {}
     for f in _marked_safe_globals_list:
         module, name = f.__module__, f.__name__
-        if module == "builtins":
-            module = "__builtin__"
         rc[f"{module}.{name}"] = f
     return rc
 
@@ -192,6 +190,10 @@ class Unpickler:
             # Risky operators
             if key[0] == GLOBAL[0]:
                 module = readline()[:-1].decode("utf-8")
+                # Patch since torch.save default protocol is 2
+                # but __builtin__ was renamed builtins in python>=3.0
+                if module == "__builtin__":
+                    module = "builtins"
                 name = readline()[:-1].decode("utf-8")
                 full_path = f"{module}.{name}"
                 if full_path in _get_allowed_globals():
@@ -199,8 +201,6 @@ class Unpickler:
                 elif full_path in _get_user_allowed_globals():
                     self.append(_get_user_allowed_globals()[full_path])
                 else:
-                    if module == "__builtin__":
-                        full_path = f"builtins.{name}"
                     raise RuntimeError(
                         f"Unsupported global: GLOBAL {full_path} was not an allowed global by default. "
                         "Please use `torch.serialization.add_safe_globals` to allowlist this global "
