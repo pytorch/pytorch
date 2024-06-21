450ae28f267b0290ee4520bd784b7ebe27eac7cd
PaliC <sahancpal@gmail.com>
2024-06-21 16:31:27 -0700

    [Split Build][BE] consolidate pip install commands (#129253)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129253
    Approved by: https://github.com/atalman
    ghstack dependencies: #129011

--- .circleci/scripts/binary_linux_test.sh
+++ .circleci/scripts/binary_linux_test.sh
@@ -98,13 +98,10 @@ if [[ "$PACKAGE_TYPE" == conda ]]; then
 elif [[ "$PACKAGE_TYPE" != libtorch ]]; then
   if [[ "\$BUILD_ENVIRONMENT" != *s390x* ]]; then
     if [[ "$USE_SPLIT_BUILD" == "true" ]]; then
-      pkg="$(ls -1 /final_pkgs/torch_no_python* | sort |tail -1)"
+      pkg_no_python="$(ls -1 /final_pkgs/torch_no_python* | sort |tail -1)"
+      pkg_torch="$(ls -1 /final_pkgs/torch-* | sort |tail -1)"
       # todo: after folder is populated use the pypi_pkg channel instead
-      pip install "\$pkg" --index-url "https://download.pytorch.org/whl/\${CHANNEL}/${DESIRED_CUDA}_pypi_pkg"
-      retry pip install -q numpy protobuf typing-extensions
-      pkg="$(ls -1 /final_pkgs/torch-* | sort |tail -1)"
-      # todo: after folder is populated use the pypi_pkg channel instead
-      pip install "\$pkg" --index-url "https://download.pytorch.org/whl/\${CHANNEL}/${DESIRED_CUDA}_pypi_pkg"
+      pip install "\$pkg_no_python" "\$pkg_torch" --index-url "https://download.pytorch.org/whl/\${CHANNEL}/${DESIRED_CUDA}_pypi_pkg"
       retry pip install -q numpy protobuf typing-extensions
     else
       pip install "\$pkg" --index-url "https://download.pytorch.org/whl/\${CHANNEL}/${DESIRED_CUDA}"
