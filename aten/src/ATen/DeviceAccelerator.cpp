41c8dce559b7c99d67e655e14e080af5b35b68e0
Yu, Guangye <guangye.yu@intel.com>
2024-06-21 11:31:21 +0000

    Update on "Refine the logic of device construction when only device index is given"
    # Motivation
    Before this PR, device construction was `cuda` type when only a device index was given. It also returns the `PrivateUser1` type if a `PrivateUser1` type is registered.
    ```bash
    >>> import torch
    >>> device = torch.device(0)
    >>> device.type
    'cuda'
    >>> a = torch.tensor([1, 2])
    >>> b = a.to(0)
    >>> b
    tensor([1, 2], device='cuda:0')
    ```
    It works well on CUDA GPU. But it will raise unexpected information and error running on XPU.
    ```bash
    >>> import torch
    >>> device = torch.device(0)
    >>> device.type
    'cuda'
    >>> a = torch.tensor([1, 2])
    >>> b = a.to(0)
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/home/xxx/pytorch/torch/cuda/__init__.py", line 302, in _lazy_init
        raise AssertionError("Torch not compiled with CUDA enabled")
    AssertionError: Torch not compiled with CUDA enabled
    ```
    With this PR, refine the logic for XPU. It will return 'xpu' device type if PyTorch is built with XPU. And raise an error if PyTorch is built without any device but only accepts a device index. Now, it works well on XPU.
    ```bash
    >>> import torch
    >>> device = torch.device(0)
    >>> device.type
    'xpu'
    >>> a = torch.tensor([1, 2])
    >>> b = a.to(0)
    >>> b
    tensor([1, 2], device='xpu:0')
    ```
    
    
    
    [ghstack-poisoned]

--- aten/src/ATen/DeviceAccelerator.cpp
+++ aten/src/ATen/DeviceAccelerator.cpp
@@ -1,13 +1,19 @@
 #include <ATen/Context.h>
 #include <ATen/DeviceAccelerator.h>
-
+#include <iostream>
 namespace at {
 
 C10_API std::optional<DeviceType> getAccelerator(bool checked) {
-#define CHECK_ACCELERATOR_MUTEX(name)                                          \
-  TORCH_CHECK(                                                                 \
-      !is_mutex_device_detected, "Cannot have " #name " with other devices."); \
-  is_mutex_device_detected = true;
+#define ASSIGN_ACCELERATOR_AND_CHECK_MUTEX(device_name) \
+  if (at::has##device_name()) {                         \
+    device_type = k##device_name;                       \
+    TORCH_CHECK(                                        \
+        !is_mutex_device_detected,                      \
+        "Cannot have ",                                 \
+        device_type.value(),                            \
+        " with other accelerators.");                   \
+    is_mutex_device_detected = true;                    \
+  }
 
   if (is_privateuse1_backend_registered()) {
     // We explicitly allow PrivateUse1 and another device at the same time as we
@@ -17,23 +23,16 @@ C10_API std::optional<DeviceType> getAccelerator(bool checked) {
   }
   std::optional<DeviceType> device_type = std::nullopt;
   bool is_mutex_device_detected = false;
-  if (at::hasCUDA()) {
-    CHECK_ACCELERATOR_MUTEX(CUDA)
-    device_type = kCUDA;
-  } else if (at::hasMTIA()) {
-    CHECK_ACCELERATOR_MUTEX(MTIA)
-    device_type = kMTIA;
-  } else if (at::hasXPU()) {
-    CHECK_ACCELERATOR_MUTEX(XPU)
-    device_type = kXPU;
-  }
+  ASSIGN_ACCELERATOR_AND_CHECK_MUTEX(CUDA)
+  ASSIGN_ACCELERATOR_AND_CHECK_MUTEX(MTIA)
+  ASSIGN_ACCELERATOR_AND_CHECK_MUTEX(XPU)
   if (checked) {
     TORCH_CHECK(
         device_type, "Cannot access accelerator device when none is available.")
   }
   return device_type;
 
-#undef CHECK_ACCELERATOR_MUTEX
+#undef ASSIGN_ACCELERATOR_AND_CHECK_MUTEX
 }
 
 } // namespace at
