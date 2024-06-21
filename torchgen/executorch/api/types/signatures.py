e7cd3097b9f1780ddd374d6325c2475b5fd6a239
James Wu <jjwu@meta.com>
2024-06-21 08:12:44 -0700

    Update (base update)
    [ghstack-poisoned]

--- torchgen/executorch/api/types/signatures.py
+++ torchgen/executorch/api/types/signatures.py
@@ -2,11 +2,9 @@ from dataclasses import dataclass
 from typing import List, Optional, Set
 
 import torchgen.api.cpp as aten_cpp
-
 from torchgen.api.types import Binding, CType
+from torchgen.executorch.api.types.types import contextArg
 from torchgen.model import FunctionSchema, NativeFunction
-
-from .types import contextArg
 
 
 @dataclass(frozen=True)
