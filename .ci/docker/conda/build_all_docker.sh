491c8cca62680fa088922d95b3ba4f4a26cb1c49
atalman <atalman@fb.com>
2024-06-21 14:18:38 -0700

    Cleanup docker move

--- .ci/docker/conda/build_all_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eou pipefail
-
-TOPDIR=$(git rev-parse --show-toplevel)
-
-for CUDA_VERSION in 12.4 12.1 11.8 cpu; do
-  CUDA_VERSION="${CUDA_VERSION}" conda/build_docker.sh
-done
