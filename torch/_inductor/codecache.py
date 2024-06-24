acc5bf1c15ced06450cf567fd77601addaa392b5
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-06-24 16:50:14 +0000

    Revert "[halide-backend] Initial implementation of HalideKernel and HalideScheduling (#126417)"
    This reverts commit 4f9399bd0d2bc0cbd14348b80e32b263de5c6bc0.
    
    Reverted https://github.com/pytorch/pytorch/pull/126417 on behalf of https://github.com/fbgheith due to breaking internal builds ([comment](https://github.com/pytorch/pytorch/pull/126417#issuecomment-2186999121))

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -2928,10 +2928,10 @@ class HalideCodeCache(CppPythonBindingsCodeCache):
         buffers = []
         buffer_names = []
         for i, arg in enumerate(argtypes):
-            if arg.is_buffer():
+            if arg.numel:
                 buffer_names.append(f"hl_buf_{i}")
                 buffers.append(
-                    f"    Halide::Runtime::Buffer {buffer_names[-1]}({arg.halide_type()}, {arg.name}, {', '.join(arg.shape)});"
+                    f"    Halide::Runtime::Buffer {buffer_names[-1]}({arg.halide_type()}, {arg.name}, {arg.numel});"
                 )
             else:
                 assert "*" not in arg.ctype
@@ -2952,10 +2952,26 @@ class HalideCodeCache(CppPythonBindingsCodeCache):
             "\n".join(
                 [
                     cls.glue_template,
+                    f"{cls.cpu_cache_size()}",
                     cpp_compile_command("I", "O"),
                 ]
             ).encode("utf-8")
         )
+
+    @staticmethod
+    @functools.lru_cache(None)
+    def cpu_cache_size():
+        try:
+            cpuinfo = open("/proc/cpuinfo").read()
+        except OSError:
+            return 16777216
+        m = re.search(r"cache size\s*: (\d+) KB", cpuinfo)
+        if m:
+            return int(m.group(1)) * 1024
+        m = re.search(r"cache size\s*: (\d+) MB", cpuinfo)
+        if m:
+            return int(m.group(1)) * 1024 * 1024
+        raise RuntimeError("failed to find 'cache size: ... KB' in /proc/cpuinfo")
 
     @staticmethod
     def _search_for_file(suffix, errmsg):
@@ -3031,6 +3047,7 @@ class HalideCodeCache(CppPythonBindingsCodeCache):
         lockfile = str(dirpath / "lock")
         need_compile = not os.path.exists(donefile)
         jobs = []
+
         if need_compile:
             write_atomic(genfile, source_code)
             jobs.append(
@@ -3046,7 +3063,7 @@ class HalideCodeCache(CppPythonBindingsCodeCache):
                         "-f",
                         "halide_kernel",
                         "-e",
-                        "static_library,h,schedule",
+                        "static_library,h,schedule,pytorch_wrapper",
                         "-p",
                         cls.find_libautoschedule(meta.scheduler),
                         *meta.args(),
