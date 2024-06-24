cc34025125056fd98ae3f6efd07233782d4bfb95
Peter Bell <peterbell10@live.co.uk>
2024-06-24 17:38:10 +0100

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3655,12 +3655,10 @@ class TritonTemplateBuffer(TemplateBuffer):
                 current_node in allowed_set
             ), f"Mutated inputs are only allowed for {allowed_set} but got {current_node}"
             device = self.inputs[0].get_device()
-            self.outputs.extend(
-                [
-                    MutationOutput(NoneLayout(device), buf, self)
-                    for buf in mutated_inputs
-                ]
-            )
+            self.outputs += [
+                MutationOutput(NoneLayout(device), buf, self)
+                for buf in mutated_inputs
+            ]
 
     def get_outputs(self) -> List[Buffer]:
         return self.outputs
@@ -3796,7 +3794,7 @@ class InputsKernel(OperationBuffer):
         return dependencies.StarDep(x.get_name())
 
     def get_read_writes(self):
-        reads = set()
+        reads: Set[Dep] = set()
         StarDep = dependencies.StarDep
         for input in self.inputs:
             if isinstance(input, list):
@@ -3804,7 +3802,7 @@ class InputsKernel(OperationBuffer):
             else:
                 reads.add(StarDep(input.get_name()))
 
-        writes = {StarDep(buf.get_name()) for buf in self.get_outputs()}
+        writes: Set[Dep] = {StarDep(buf.get_name()) for buf in self.get_outputs()}
 
         return dependencies.ReadWrites(
             reads=reads,
@@ -4861,7 +4859,7 @@ class MutationOperation(InputsKernel):
     def __init__(self, layout, mutated_node, node_doing_mutating):
         super().__init__(None, layout, inputs=[node_doing_mutating])
         self.device = node_doing_mutating.get_device()
-        self.outputs = [MutationOutput(layout, mutated_node, self)]
+        self.outputs: List[Buffer] = [MutationOutput(layout, mutated_node, self)]
         V.graph.register_operation(self)
 
     def get_device(self):
