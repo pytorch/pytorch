66cae65fef34c15bb1718c8a62dedd77fd566aef
joydddd <joydong@meta.com>
2024-06-25 13:38:29 -0700

    Manual merge

--- torch/_inductor/kernel/flex_attention.py
+++ torch/_inductor/kernel/flex_attention.py
@@ -5,7 +5,10 @@ import logging
 from enum import auto, Enum
 from typing import Any, List, Tuple
 
+import sympy
+
 import torch
+from torch._inductor.virtualized import V
 from .. import config
 from ..ir import (
     ComputedBuffer,
@@ -17,7 +20,9 @@ from ..ir import (
     Subgraph,
     TensorBox,
 )
+
 from ..lowering import empty_strided, lowerings, register_lowering
+from ..runtime.runtime_utils import next_power_of_2
 from ..select_algorithm import autotune_select_algorithm, TritonTemplate
 
 log = logging.getLogger(__name__)
@@ -309,6 +314,50 @@ flex_attention_template = TritonTemplate(
  """,
 )
 
+def _use_flex_decoding(query):
+    # Decide which kernel to use, return true if use flex decoding kernel.
+    return V.graph.sizevars.evaluate_expr(sympy.Lt(query.get_size()[-2], 32 + 1))
+
+
+def filtered_configs(
+    b: int,
+    h: int,
+    m: int,
+    d: int,
+    n: int,
+    configs: List[Tuple[int, int, int, int]],
+):
+    """Filter out configs that are too large for input size"""
+    min_block_size = 32
+    m = max(
+        next_power_of_2(
+            V.graph.sizevars.size_hint(
+                m, fallback=torch._inductor.config.unbacked_symint_fallback  # type: ignore[arg-type]
+            )
+        ),
+        min_block_size,
+    )
+
+    n = max(
+        next_power_of_2(
+            V.graph.sizevars.size_hint(
+                n, fallback=torch._inductor.config.unbacked_symint_fallback  # type: ignore[arg-type]
+            )
+        ),
+        min_block_size,
+    )
+
+    filtered_configs = []
+
+    for BLOCK_M, BLOCK_N, num_warps, num_stages in configs:
+        # shrink configs for small inputs
+        BLOCK_M = max(min(BLOCK_M, m), min_block_size)
+        BLOCK_N = max(min(BLOCK_N, n), min_block_size)
+        if (BLOCK_M, BLOCK_N, num_warps, num_stages) not in filtered_configs:
+            filtered_configs.append((BLOCK_M, BLOCK_N, num_warps, num_stages))
+
+    return filtered_configs
+
 
 _h100_default_config = {
     (torch.float32, 64): (128, 32, 4, 3),
@@ -380,6 +429,9 @@ def _get_default_config_bwd(query) -> Tuple[int, int, int, int]:
         return (16, 16, 4, 1)
 
 
+from torch._inductor.kernel.flex_decoding import create_flex_decoding_kernel
+
+
 # TODO: We probably also need a layout constraint?
 @register_lowering(torch.ops.higher_order.flex_attention, type_promotion_kind=None)
 def flex_attention(*args, **kwargs):
@@ -405,6 +457,11 @@ def flex_attention(*args, **kwargs):
         query.get_size(),
         query.get_stride(),
     )
+
+    if _use_flex_decoding(query):
+        return create_flex_decoding_kernel(
+            subgraph_buffer, layout, query, key, value, subgraph, *other_buffers
+        )
     # see NOTE:[TritonTemplates with multiple outputs]
     logsumexp_shape = query.get_size()[:-1]  # [B, H, M]
     logsumexp = empty_strided(
@@ -428,7 +485,14 @@ def flex_attention(*args, **kwargs):
     # Note, we don't need to pass in the captured buffers explicitly
     # because they're implicitly added by the score_mod function
     # We do need to explicitly pass it in for autotuning though.
-    for BLOCK_M, BLOCK_N, num_warps, num_stages in configs:
+    for BLOCK_M, BLOCK_N, num_warps, num_stages in filtered_configs(
+        b=query.get_size()[0],
+        h=query.get_size()[1],
+        m=query.get_size()[-2],
+        d=query.get_size()[-1],
+        n=key.get_size()[-2],
+        configs=configs,
+    ):
         flex_attention_template.maybe_append_choice(
             choices=choices,
             input_nodes=[query, key, value, logsumexp],
