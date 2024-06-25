4c75b6239f2a1b6e95cbd04915990681d9e4bdef
Will Feng <yf225@cornell.edu>
2024-06-25 14:42:29 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -292,6 +292,10 @@ def _register_lowering(
             unpacked = True
             args = args[0]
 
+        # explicitly assert for "out=" ops for better error messages
+        assert not any(
+            x == "out" for x in kwargs.keys()
+        ), "out= ops aren't yet supported"
         # kwargs tensors not supported yet unless it's a fallback op
         assert not any(isinstance(x, TensorBox) for x in kwargs.values()) or all(
             fn in fallbacks for fn in aten_fn
