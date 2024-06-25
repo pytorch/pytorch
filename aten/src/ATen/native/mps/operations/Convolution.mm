fbe5e15080539638410f672ca61d612e3fad2760
Joona Havukainen <jhavukainen@apple.com>
2024-06-25 14:39:02 -0700

    Lintrunner

--- aten/src/ATen/native/mps/operations/Convolution.mm
+++ aten/src/ATen/native/mps/operations/Convolution.mm
@@ -161,13 +161,14 @@ static Tensor _mps_convolution_impl(const Tensor& input_t,
   }
   TensorArg output{output_t, "result", 0};
 
-  //TODO: MPS convolution kernel currently does not support output channels > 2^16
+  // TODO: MPS convolution kernel currently does not support output channels > 2^16
   for (auto elem : output_t.sizes()) {
-    TORCH_CHECK_NOT_IMPLEMENTED(elem <= (1 << 16), "Output channels > 65536 not supported at the MPS device. ",
-      "As a temporary fix, you can set the environment variable `PYTORCH_ENABLE_MPS_FALLBACK=1` ",
-      "to use the CPU as a fallback for this op. WARNING: this will be slower than running natively ",
-      "on MPS."
-    );
+    TORCH_CHECK_NOT_IMPLEMENTED(
+        elem <= (1 << 16),
+        "Output channels > 65536 not supported at the MPS device. ",
+        "As a temporary fix, you can set the environment variable `PYTORCH_ENABLE_MPS_FALLBACK=1` ",
+        "to use the CPU as a fallback for this op. WARNING: this will be slower than running natively ",
+        "on MPS.");
   }
 
   convolution_shape_check(c, input, weight, output, padding, stride, dilation, groups);
@@ -355,13 +356,14 @@ static Tensor mps_convolution_backward_input(IntArrayRef input_size,
   using namespace mps;
   bool is3DConv = grad_output_t.dim() == 5;
 
-  //TODO: MPS convolution kernel currently does not support output channels > 2^16
+  // TODO: MPS convolution kernel currently does not support output channels > 2^16
   for (auto elem : grad_output_t.sizes()) {
-    TORCH_CHECK_NOT_IMPLEMENTED(elem <= (1 << 16), "Output channels > 65536 not supported at the MPS device. ",
-      "As a temporary fix, you can set the environment variable `PYTORCH_ENABLE_MPS_FALLBACK=1` ",
-      "to use the CPU as a fallback for this op. WARNING: this will be slower than running natively ",
-      "on MPS."
-    );
+    TORCH_CHECK_NOT_IMPLEMENTED(
+        elem <= (1 << 16),
+        "Output channels > 65536 not supported at the MPS device. ",
+        "As a temporary fix, you can set the environment variable `PYTORCH_ENABLE_MPS_FALLBACK=1` ",
+        "to use the CPU as a fallback for this op. WARNING: this will be slower than running natively ",
+        "on MPS.");
   }
 
   TORCH_CHECK(isFloatingType(grad_output_t.scalar_type()), "Convolution is supported only for Floating types");
