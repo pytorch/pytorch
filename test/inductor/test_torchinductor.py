6d887b95f15372fc88ced40031fa11eb0e5fec89
Will Feng <yf225@cornell.edu>
2024-06-25 13:33:09 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -7459,7 +7459,7 @@ class CommonTemplate:
 
             with self.assertRaisesRegex(
                 torch._inductor.exc.LoweringException,
-                "NYI: Can't generate FallbackKernel",
+                "Operators that accept mutable List[Tensor] are not yet"
             ):
                 compiled_f = compile_fx_inner(mod, cloned_args)
 
