2bf37985b173e2c39e7db897d81dbb4e63140838
Chien-Chin Huang <chienchin@fb.com>
2024-06-26 07:31:15 -0700

    Support HSDP + Monolith Checkpointing (#128446) (#129254)
    Fixes #128444. Rank 0 check should be in the same group as the broadcast
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128446
    Approved by: https://github.com/fegin
    
    (cherry picked from commit 153362fbc9e8642fb851a4de3b99e3871a2cc714)
    
    Co-authored-by: Mihir Patel <mihir.v.patel7@gmail.com>

--- torch/distributed/fsdp/_optim_utils.py
+++ torch/distributed/fsdp/_optim_utils.py
@@ -341,14 +341,14 @@ def _broadcast_processed_state(
     group: Optional[dist.ProcessGroup],
 ) -> Dict[str, Any]:
     objects: List[Any] = [None]
-    if fsdp_state.rank == 0:
+    if dist.get_rank(group) == 0:
         objects[0] = tree_map_only(
             torch.Tensor,
             lambda v: v.cpu() if v.dim() == 0 else _PosDimTensorInfo(v.shape, v.dtype),  # type: ignore[union-attr]
             optim_state,
         )
     dist.broadcast_object_list(objects, src=0, group=group)
-    if fsdp_state.rank == 0:
+    if dist.get_rank(group) == 0:
         return optim_state
     else:
         return objects[0]
@@ -357,7 +357,7 @@ def _broadcast_processed_state(
 def _broadcast_state(
     fsdp_state: _FSDPState, state: Any, group: Optional[dist.ProcessGroup]
 ) -> Any:
-    if fsdp_state.rank == 0:
+    if dist.get_rank(group) == 0:
         if not isinstance(state, torch.Tensor) or state.dim() == 0:
             return state
         tensor = state.to(fsdp_state.compute_device)
