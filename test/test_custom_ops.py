d07be5a55176704ecc48d005126adb184978df3d
Shangdi Yu <shangdiy@meta.com>
2024-06-26 10:21:49 -0700

    add for example to test case

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -866,7 +866,7 @@ class TestCustomOp(CustomOpTestCaseBase):
 
             del foo
 
-        with self.assertRaisesRegex(ValueError, r"List\[int\]"):
+        with self.assertRaisesRegex(ValueError, r"For example, typing.List\[int\]"):
             # test that we propose a correct and supported type.
             @custom_ops.custom_op(f"{TestCustomOp.test_ns}::foo")
             def foo(x: Tensor, y: Tuple[int, int]) -> Tensor:
