ffd28de8b2f9f0f6e4ff753909ad8390449230fd
Peter Bell <peterbell10@live.co.uk>
2024-06-26 01:22:24 +0100

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
@@ -260,6 +260,10 @@ inline Vectorized<scalar_t> div_floor_floating_vec(
   const auto basic_div = a / b;
   floordiv = vec_t::blendv(floordiv, zero.copysign(basic_div), div == zero);
   floordiv = vec_t::blendv(floordiv, basic_div, b == zero);
+  vec_t inf(std::numeric_limits<scalar_t>::infinity());
+  // Sleef_fmod doesn't handle inf/nan well, so use basic_div for extremal values
+  auto nonfinite = basic_div.isnan() | (basic_div.abs() == inf);
+  floordiv = vec_t::blendv(floordiv, basic_div, nonfinite);
   return floordiv;
 };
 
