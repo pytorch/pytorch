93e0b3fd1840d26486d38928c1d88d30a8b03795
Yueming Hao <yhao@meta.com>
2024-06-26 15:17:41 -0700

    fix wrong source dtype

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -2546,7 +2546,7 @@ class DtypeView(BaseView):
     def make_loader(self):
         inner = self.data.make_loader()
         def loader(idx):
-            return ops.to_dtype_bitcast(inner(idx), self.target_dtype, self.dtype)
+            return ops.to_dtype_bitcast(inner(idx), self.target_dtype, self.data.dtype)
         return loader
 
     def codegen_reference(self, writer=None):
