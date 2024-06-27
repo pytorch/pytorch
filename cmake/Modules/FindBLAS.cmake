pytorch/pytorch

commit 29ae0888a1bbe4e0e0706e052c9a15807e6899c3

Fix logic to find sbgemm in BLAS library (#125227)
Current logic to set the HAS_SBGEMM flag is ignored in case the BLAS libraries are found already, ie, if set from environment variable BLAS=OpenBLAS . If BLAS_LIBRARIES are already set the code to find if BLAS_LIBRARY has sbgemm is never executed. The following commit brings out this logic outside unconditionally.

Fixes #ISSUE_NUMBER

Pull Request resolved: https://github.com/pytorch/pytorch/pull/125227
Approved by: https://github.com/malfet

cmake/Modules/FindBLAS.cmake
@@ -385,16 +385,6 @@ IF (BLAS_LIBRARIES)
   cmake_pop_check_state()
 ENDIF(BLAS_LIBRARIES)
 
-# Blas has bfloat16 support?
-IF(BLAS_LIBRARIES)
-  SET(CMAKE_REQUIRED_LIBRARIES ${BLAS_LIBRARIES})
-  check_function_exists("sbgemm_" BLAS_HAS_SBGEMM)
-  set(CMAKE_REQUIRED_LIBRARIES)
-  IF(BLAS_HAS_SBGEMM)
-    add_compile_options(-DBLAS_HAS_SBGEMM)
-  ENDIF(BLAS_HAS_SBGEMM)
-ENDIF(BLAS_LIBRARIES)
-
 # epilogue
 
 if(BLAS_LIBRARIES)
@@ -416,3 +406,13 @@ ENDIF(NOT BLAS_FIND_QUIETLY)
 
 # Do nothing is BLAS was found before
 ENDIF(NOT BLAS_FOUND)
+
+# Blas has bfloat16 support?
+IF(BLAS_LIBRARIES)
+  SET(CMAKE_REQUIRED_LIBRARIES ${BLAS_LIBRARIES})
+  check_function_exists("sbgemm_" BLAS_HAS_SBGEMM)
+  set(CMAKE_REQUIRED_LIBRARIES)
+  IF(BLAS_HAS_SBGEMM)
+    add_compile_options(-DBLAS_HAS_SBGEMM)
+  ENDIF(BLAS_HAS_SBGEMM)
+ENDIF(BLAS_LIBRARIES)

commit 92f6c3f247135049d748d3c25cedadd8d39d640a

Fix  FindBLAS.cmake
Fixes regression introduced by https://github.com/pytorch/pytorch/pull/125227

cmake/Modules/FindBLAS.cmake
@@ -404,9 +404,6 @@ IF(NOT BLAS_FIND_QUIETLY)
   ENDIF(BLAS_FOUND)
 ENDIF(NOT BLAS_FIND_QUIETLY)
 
-# Do nothing is BLAS was found before
-ENDIF(NOT BLAS_FOUND)
-
 # Blas has bfloat16 support?
 IF(BLAS_LIBRARIES)
   SET(CMAKE_REQUIRED_LIBRARIES ${BLAS_LIBRARIES})
@@ -416,3 +413,6 @@ IF(BLAS_LIBRARIES)
     add_compile_options(-DBLAS_HAS_SBGEMM)
   ENDIF(BLAS_HAS_SBGEMM)
 ENDIF(BLAS_LIBRARIES)
+
+# Do nothing is BLAS was found before
+ENDIF(NOT BLAS_FOUND)