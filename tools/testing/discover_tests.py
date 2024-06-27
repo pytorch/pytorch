9e1f3ecaa710785a1ab03c6ad5093a5566d6c5e5
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-28 05:37:39 +0800

    [BE][Easy] use `pathlib.Path` instead of `dirname` / `".."` / `pardir` (#129374)
    Changes by apply order:
    
    1. Replace all `".."` and `os.pardir` usage with `os.path.dirname(...)`.
    2. Replace nested `os.path.dirname(os.path.dirname(...))` call with `str(Path(...).parent.parent)`.
    3. Reorder `.absolute()` ~/ `.resolve()`~ and `.parent`: always resolve the path first.
    
        `.parent{...}.absolute()` -> `.absolute().parent{...}`
    
    4. Replace chained `.parent x N` with `.parents[${N - 1}]`: the code is easier to read (see 5.)
    
        `.parent.parent.parent.parent` -> `.parents[3]`
    
    5. ~Replace `.parents[${N - 1}]` with `.parents[${N} - 1]`: the code is easier to read and does not introduce any runtime overhead.~
    
        ~`.parents[3]` -> `.parents[4 - 1]`~
    
    6. ~Replace `.parents[2 - 1]` with `.parent.parent`: because the code is shorter and easier to read.~
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129374
    Approved by: https://github.com/justinchuby, https://github.com/malfet

--- tools/testing/discover_tests.py
+++ tools/testing/discover_tests.py
@@ -4,10 +4,11 @@ import sys
 from pathlib import Path
 from typing import List, Optional, Union
 
+
 CPP_TEST_PREFIX = "cpp"
 CPP_TEST_PATH = "build/bin"
 CPP_TESTS_DIR = os.path.abspath(os.getenv("CPP_TESTS_DIR", default=CPP_TEST_PATH))
-REPO_ROOT = Path(__file__).resolve().parent.parent.parent
+REPO_ROOT = Path(__file__).resolve().parents[2]
 
 
 def parse_test_module(test: str) -> str:
