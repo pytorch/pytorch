9a09ceab1a21a2c67b4590e604303c957fd90b30
mori360 <yifanmao@meta.com>
2024-06-26 23:09:31 -0700

    Update test_state_dict.py

--- test/distributed/checkpoint/test_state_dict.py
+++ test/distributed/checkpoint/test_state_dict.py
@@ -895,6 +895,31 @@ class TestStateDict(DTensorTestBase, VerifyStateDictMixin):
 
         self._test_save_load(init_model_optim)
 
+    @with_comms
+    @skip_if_lt_x_gpu(2)
+    def test_full_state_dict_with_fsdp(self) -> None:
+        device = "cuda"
+        torch.manual_seed(0)
+        with torch.device("meta"):
+            meta_model = nn.Sequential(*[nn.Linear(4, 4, bias=False) for _ in range(2)])
+            for layer in meta_model:
+                fully_shard(layer)
+            fully_shard(meta_model)
+        with torch.device("cpu"):
+            cpu_model = nn.Sequential(*[nn.Linear(4, 4, bias=False) for _ in range(2)])
+            full_sd = cpu_model.state_dict()
+        ptd_state_dict.set_model_state_dict(
+            meta_model,
+            model_state_dict=full_sd,
+            # 'aten.copy_.default: got mixed torch.Tensor and DTensor, need to convert all torch.Tensor to DTensor before calling distributed operators!',).
+            options=ptd_state_dict.StateDictOptions(
+                broadcast_from_rank0=True, full_state_dict=True, strict=False
+            )
+            # NotImplementedError: c10d::broadcast_: attempted to run this operator with Meta tensors
+            # options=ptd_state_dict.StateDictOptions(
+            #     broadcast_from_rank0=True, full_state_dict=True, strict=False
+            # )
+        )
 
 class TestNoComm(MultiProcessTestCase):
     def setUp(self) -> None:
