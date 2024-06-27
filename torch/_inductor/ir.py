00ad6d3024a63b71279e95f14c3d85fd3ad69652
Yueming Hao <yhao@meta.com>
2024-06-27 11:16:53 -0700

    fix layout error; remove comments

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1986,7 +1986,7 @@ def as_storage_and_layout(
         return buffer, x.layout
     if isinstance(x, DtypeView):
         buffer, _ = as_storage_and_layout(x.data, freeze=freeze)
-        return buffer, x.data.layout
+        return buffer, x.layout
     raise NotImplementedError
 
 
--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -587,15 +587,6 @@ def to_dtype_bitcast(x: TensorBox, dtype: torch.dtype, *, copy=False):
         return fallback_handler(aten.view.dtype)(x, dtype)
     else:
         return TensorBox(ir.DtypeView(x, dtype))
-    # def _to_dtype_bitcast(x):
-    #     # Because we may promote tensor type from float16 or bfloat16
-    #     # to float, we will need to pass the original src dtype (i.e. x_dtype),
-    #     # which is used for correctly constructing type conversion before bitcast,
-    #     # which requires the bitwidth of the input tensor type is the same as the
-    #     # target type.
-    #     return ops.to_dtype_bitcast(x, dtype, x_dtype)
-
-    # return make_pointwise(_to_dtype_bitcast, override_return_dtype=dtype)(x)
 
 
 @register_lowering(aten.view.dtype, type_promotion_kind=None)
@@ -605,7 +596,6 @@ def _view_dtype(x: TensorBox, dtype: torch.dtype):
             ir.ComplexView.create(torch.ops.aten.view.dtype, x, dtype)
         )
     return to_dtype_bitcast(x, dtype)
-    # return to_dtype_bitcast(x, dtype, copy=True)
 
 
 def to_device(x: TensorBox, device: torch.device, *, copy=False):
