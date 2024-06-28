063c364f7897d270aa08ede57491bf7cbe9ae08b
Xu Han <xu.han@outlook.com>
2024-06-28 11:42:21 +0000

    [Windows] remove mkl shared library dependency. (#129493)
    # Background
    I have fixed pytorch Windows missing mkl shared library dependency issue: https://github.com/pytorch/pytorch/issues/124009
    The solution is change torch_cpu module static link mkl library:
    1. pytorch static link mkl PR: https://github.com/pytorch/pytorch/pull/124925
    2. builder install mkl static library: https://github.com/pytorch/builder/pull/1790
    
    Double confirmed current build is using mkl static link: https://github.com/pytorch/pytorch/issues/124009#issuecomment-2160941802
    
    # Goal
    Remove setup.py `install_requires` will install mkl shared lib on pytorch Windows. It is not required now, due to we have static linked it.
    It will reduce the pytorch install network traffic and avoid install useless mkl shared library package.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129493
    Approved by: https://github.com/malfet

--- setup.py
+++ setup.py
@@ -1125,7 +1125,6 @@ def main():
         "networkx",
         "jinja2",
         "fsspec",
-        'mkl>=2021.1.1,<=2021.4.0; platform_system == "Windows"',
     ]
 
     if sys.version_info >= (3, 12, 0):
