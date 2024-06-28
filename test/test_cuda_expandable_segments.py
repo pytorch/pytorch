c7b8ec25c1a588b9c04b3062f2c9e7d99e83b5c8
Bilal Khan <bilal2vec@gmail.com>
2024-06-28 17:56:28 +0000

    edit

--- test/test_cuda_expandable_segments.py
+++ test/test_cuda_expandable_segments.py
@@ -10,22 +10,19 @@ import torch
 from torch.testing._internal.common_cuda import IS_JETSON, IS_WINDOWS
 from torch.testing._internal.common_utils import run_tests
 
-REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
-
-sys.path.insert(0, str(REPO_ROOT))
 from test_cuda import (  # noqa: F401
     TestBlockStateAbsorption,
     TestCuda,
     TestCudaMallocAsync,
 )
-from tools.stats.import_test_stats import get_disabled_tests
-
-# Make the helper files in test/ importable
-pytorch_test_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(pytorch_test_dir)
 
 from dynamo.test_cudagraphs import TestAotCudagraphs  # noqa: F401
 from inductor.test_cudagraph_trees import CudaGraphTreeTests  # noqa: F401
+
+REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+sys.path.insert(0, str(REPO_ROOT))
+from tools.stats.import_test_stats import get_disabled_tests
 
 if __name__ == "__main__":
     if torch.cuda.is_available() and not IS_JETSON and not IS_WINDOWS:
