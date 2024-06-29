760788eecc90d5604260e4a2c2530cb8f1e929a9
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-06-29 20:26:01 +0800

    Update (base update)
    [ghstack-poisoned]

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -14,7 +14,7 @@ import torch.testing._internal.optests as optests
 import torch.utils.cpp_extension
 from functorch import make_fx
 from torch import Tensor
-from torch._custom_op.impl import custom_op, CustomOp, infer_schema
+from torch._custom_op.impl import CustomOp, infer_schema
 from torch._library.infer_schema import tuple_to_list
 from torch._utils_internal import get_file_path_2
 from torch.testing._internal import custom_op_db
@@ -22,6 +22,10 @@ from torch.testing._internal.common_cuda import TEST_CUDA
 from torch.testing._internal.common_device_type import *  # noqa: F403
 from torch.testing._internal.common_utils import *  # noqa: F403
 from torch.testing._internal.custom_op_db import numpy_nonzero
+
+
+# Shadowed by `torch.testing._internal.common_utils.custom_op`
+from torch._custom_op.impl import custom_op  # usort: skip
 
 
 def requires_compile(fun):
