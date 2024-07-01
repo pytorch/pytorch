fa8eef140118ecb01fe002ef2bf6d0366c8bbcb7
Peter Bell <peterbell10@live.co.uk>
2024-07-01 18:12:29 +0100

    Rebase (base update)
    [ghstack-poisoned]

--- aten/src/ATen/Context.h
+++ aten/src/ATen/Context.h
@@ -216,6 +216,9 @@ class TORCH_API Context {
   void setSDPUseCuDNN(bool);
   bool userEnabledCuDNNSDP() const;
 
+  void setSDPUseOverrideable(bool);
+  bool userEnabledOverrideableSDP() const;
+
   at::LinalgBackend linalgPreferredBackend() const;
   void setLinalgPreferredBackend(at::LinalgBackend);
 
@@ -367,7 +370,8 @@ class TORCH_API Context {
   bool enabled_flashSDP = true;
   bool enabled_mem_efficientSDP = true;
   bool enabled_mathSDP = true;
-  bool enabled_cudnnSDP = true;
+  bool enabled_cudnnSDP = false;
+  bool enabled_overrideable = true;
 #ifdef USE_ROCM
   bool benchmark_cudnn = true;
 #else
