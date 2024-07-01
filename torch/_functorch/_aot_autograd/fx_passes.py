567c07fde740427e6715a4eb7e0d3e8d808e57ee
Will Feng <yf225@cornell.edu>
2024-06-30 21:31:26 -0700

    Update
    [ghstack-poisoned]

--- torch/_functorch/_aot_autograd/fx_passes.py
+++ torch/_functorch/_aot_autograd/fx_passes.py
@@ -108,12 +108,12 @@ def refunctionalize_set(graph: fx.Graph) -> None:
                 )
     # Replace set_ nodes in graph output with the corresponding primal input.
     new_return_node_args = []
-    for arg in list(graph.nodes)[-1].args:
+    for arg in return_node.args:
         if arg in set_nodes_to_be_deleted:
             arg = arg.args[0]
             assert arg in primal_inputs
         new_return_node_args.append(arg)
-    list(graph.nodes)[-1].args = tuple(new_return_node_args)
+    return_node.args = tuple(new_return_node_args)
     # Finally, delete the old set_ nodes.
     for set_node in set_nodes_to_be_deleted:
         graph.erase_node(set_node)
