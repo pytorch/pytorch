577d451d278ffc375d9e7d5699328bb9b05c0d0c
Bilal Khan <bilal2vec@gmail.com>
2024-07-01 23:49:45 +0000

    update import paths for dynamo/inductor expandable segment tests

--- test/inductor/test_cudagraph_trees_expandable_segments.py
+++ test/inductor/test_cudagraph_trees_expandable_segments.py
@@ -12,7 +12,7 @@ from torch.testing._internal.common_utils import run_tests
 
 from test_cudagraph_trees import CudaGraphTreeTests  # noqa: F401
 
-REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
+REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
 
 sys.path.insert(0, str(REPO_ROOT))
 from tools.stats.import_test_stats import get_disabled_tests
