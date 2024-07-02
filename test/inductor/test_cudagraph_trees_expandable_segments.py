e9b161227b48c199baf8d3f7b9abd69b2aef5978
Bilal Khan <bilal2vec@gmail.com>
2024-07-02 16:13:01 +0000

    edit

--- test/inductor/test_cudagraph_trees_expandable_segments.py
+++ test/inductor/test_cudagraph_trees_expandable_segments.py
@@ -8,17 +8,25 @@ import sys
 import torch
 
 from torch.testing._internal.common_cuda import IS_JETSON, IS_WINDOWS
-from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.common_utils import run_tests, TEST_WITH_ASAN
+from torch.testing._internal.inductor_utils import HAS_CUDA
 
-from inductor.test_cudagraph_trees import CudaGraphTreeTests  # noqa: F401
+pytorch_test_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(pytorch_test_dir)
+
+if HAS_CUDA and not TEST_WITH_ASAN:
+    from inductor.test_cudagraph_trees import CudaGraphTreeTests  # noqa: F401
 
 REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
 
 sys.path.insert(0, str(REPO_ROOT))
 from tools.stats.import_test_stats import get_disabled_tests
 
+# Make sure to remove REPO_ROOT after import is done
+sys.path.remove(str(REPO_ROOT))
+
 if __name__ == "__main__":
-    if torch.cuda.is_available() and not IS_JETSON and not IS_WINDOWS:
+    if torch.cuda.is_available() and not IS_JETSON and not IS_WINDOWS and HAS_CUDA and not TEST_WITH_ASAN:
         get_disabled_tests(".")
 
         torch.cuda.memory._set_allocator_settings("expandable_segments:True")
--- test/test_cuda_expandable_segments.py
+++ test/test_cuda_expandable_segments.py
@@ -21,6 +21,9 @@ REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(REPO_ROOT))
 from tools.stats.import_test_stats import get_disabled_tests
 
+# Make sure to remove REPO_ROOT after import is done
+sys.path.remove(str(REPO_ROOT))
+
 if __name__ == "__main__":
     if torch.cuda.is_available() and not IS_JETSON and not IS_WINDOWS:
         get_disabled_tests(".")
