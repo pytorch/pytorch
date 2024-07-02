cb2cb15a786169b24355d0147a4322e64c6182da
Khushi Agrawal <khushiagrawal411@gmail.com>
2024-07-02 13:23:21 +0530

    [better-engineering] use AT_DISPATCH_V2 macro

--- aten/src/ATen/native/cuda/UnaryGeometricSinKernel.cu
+++ aten/src/ATen/native/cuda/UnaryGeometricSinKernel.cu
@@ -22,33 +22,43 @@ void sin_kernel_cuda(TensorIteratorBase& iter) {
 #if AT_USE_JITERATOR()
     static const auto sin_string = jiterator_stringify(
         template <typename T> T sin_impl(T a) { return std::sin(a); });
-    AT_DISPATCH_COMPLEX_TYPES_AND(
-        kComplexHalf, common_dtype, "sin_name", [&]() {
+    AT_DISPATCH_V2(
+        common_dtype,
+        "sin_name",
+        AT_WRAP([&] {
           jitted_gpu_kernel<
               /*name=*/sin_name,
               /*return_dtype=*/scalar_t,
               /*common_dtype=*/scalar_t,
               /*arity=*/1>(iter, sin_string);
-        });
+        }),
+        kComplexHalf,
+    )
 #else
-    AT_DISPATCH_COMPLEX_TYPES_AND(
-        kComplexHalf, common_dtype, "sin_name", [&]() {
+    AT_DISPATCH_V2(
+        common_dtype,
+        "sin_name",
+        AT_WRAP([&] {
           gpu_kernel(iter, [] GPU_LAMBDA(scalar_t a) -> scalar_t {
             using opmath_t = at::opmath_type<scalar_t>;
             return ::sin(static_cast<opmath_t>(a));
           });
-        });
+        }),
+        AT_EXPAND(AT_COMPLEX_TYPES),
+   )
 #endif
   } else {
-    AT_DISPATCH_FLOATING_TYPES_AND2(
-        ScalarType::Half,
-        ScalarType::BFloat16,
+    AT_DISPATCH_V2(
         common_dtype,
         "sin_cuda",
-        [&]() {
+        AT_WRAP([&] {
           gpu_kernel(
-              iter, [] GPU_LAMBDA(scalar_t a) -> scalar_t { return ::sin(a); });
-        });
+            iter, [] GPU_LAMBDA(scalar_t a) -> scalar_t { return ::sin(a); });
+        }),
+        AT_EXPAND(AT_FLOATING_TYPES),
+        ScalarType::Half,
+        ScalarType::BFloat16,
+    ),
   }
 }
 
