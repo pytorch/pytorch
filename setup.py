d0665a6ddc7b9d931e0604f6c98481a7993dd5d8
Catherine Lee <csl@fb.com>
2024-07-05 11:48:20 -0700

    tc

--- setup.py
+++ setup.py
@@ -1183,7 +1183,7 @@ def main():
     install_requires += extra_install_requires
 
     extras_require = {
-        "optree": ["optree>=0.11.0"],
+        "optree": ["optree==0.11.0"],
         "opt-einsum": ["opt-einsum>=3.3"],
     }
 
