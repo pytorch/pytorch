c0735a3dd36760f04c2dd05599fc4c63c85f1cb7
Shunting Zhang <shunting@fb.com>
2024-07-05 00:18:07 -0700

    [pt2-bench] fix accuracy failure for a few models (#129941)
    This PR batch the fix for a few accuracy failures issues during training by raising tolerance. I do that only for models that I think it fails not due to real issue.
    
    ## sebotnet33ts_256
    
    The accuracy test for this model start to fail around June 05 [link](https://hud.pytorch.org/benchmark/timm_models/inductor_with_cudagraphs?dashboard=torchinductor&startTime=Sun%2C%2002%20Jun%202024%2007%3A19%3A38%20GMT&stopTime=Tue%2C%2002%20Jul%202024%2007%3A19%3A38%20GMT&granularity=day&mode=training&dtype=amp&lBranch=main&lCommit=04a0d856207d83c2031e4b9cb6825ba3e0092850&rBranch=main&rCommit=e62925930f6a62f6aeeb1fe1a661a9bd3352b53d&model=sebotnet33ts_256).
    
    I can not repro locally, but from the log from the dashboard:
    ```
    RMSE (res-fp64): 0.09441, (ref-fp64): 0.02971 and shape=torch.Size([1536]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.040000
    ```
    raising the tolerance should fix it.
    
    ## DebertaForQuestionAnswering
    
    This model fails accuracy test on the dashboard only in max-autotune mode. I can not repro locally by command:
    ```
    TORCHINDUCTOR_MAX_AUTOTUNE=1 time python benchmarks/dynamo/huggingface.py --accuracy --no-translation-validation --training --amp --backend inductor --device cuda --only DebertaForQuestionAnswering
    ```
    
    From error message on the dashboard:
    ```
    RMSE (res-fp64): 0.01803, (ref-fp64): 0.00537 and shape=torch.Size([2]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.010000
    ```
    
    0.02 tolerance should suppress this error.
    
    ## gluon_inception_v3
    
    This model fail on the dashboard in max-autotune mode. I can not repro locally by command
    ```
    TORCHINDUCTOR_MAX_AUTOTUNE=1 time python benchmarks/dynamo/timm_models.py --accuracy --training --amp --backend inductor --disable-cudagraphs --device cuda --only gluon_inception_v3
    ```
    
    From error message on the dashboard
    ```
    RMSE (res-fp64): 0.02798, (ref-fp64): 0.00730 and shape=torch.Size([384]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.010000
    Accuracy failed for key name Mixed_7c.branch3x3dbl_3a.bn.running_var
    ```
    raising tolerance should suppress this error.
    
    # mobilenetv3_large_100
    Fail in MA model. I can not repro locally by command
    ```
    TORCHINDUCTOR_MAX_AUTOTUNE=1 time python benchmarks/dynamo/timm_models.py --accuracy --training --amp --backend inductor --disable-cudagraphs --device cuda --only
    ```
    The error message on the dashboard is
    ```
    RMSE (res-fp64): 0.29754, (ref-fp64): 0.05205 and shape=torch.Size([]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.040000
    ```
    
    The tensor is so small that the noise can be high. I use larger multiplier for smaller tensor in torch._dynamo.utils.same.
    
    # yolov3
    
    Fail on dashboard with error
    ```
    Error on the dashboard: RMSE (res-fp64): 0.01278, (ref-fp64): 0.00246 and shape=torch.Size([256]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.001000
    ```
    
    Fix it by using a larger multiplier for smaller tensors and raising the tolereance.
    
    # timm_efficientdet
    
    Fail on the dashboard with error
    ```
    E0623 18:37:43.638000 139924418725056 torch/_dynamo/utils.py:1468] RMSE (res-fp64): 0.00096, (ref-fp64): 0.00009 and shape=torch.Size([2]). res.dtype: torch.float32, multiplier: 3.000000, tol: 0.001000
    ```
    But I can not repro locally with command
    ```
    time python benchmarks/dynamo/torchbench.py --backend inductor --amp --performance --only timm_efficientdet  --training
    ```
    
    Raise the tolerance should fix.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129941
    Approved by: https://github.com/jansel
    ghstack dependencies: #129996

--- benchmarks/dynamo/huggingface.py
+++ benchmarks/dynamo/huggingface.py
@@ -185,6 +185,12 @@ REQUIRE_HIGHER_TOLERANCE_TRAINING = {
     # harmful.
     "AlbertForQuestionAnswering",
 }
+
+REQUIRE_HIGHER_TOLERANCE_MAX_AUTOTUNE_TRAINING = {
+    # DebertaForQuestionAnswering needs higher tolerance in Max-Autotune mode
+    "DebertaForQuestionAnswering",
+}
+
 REQUIRE_HIGHER_TOLERANCE_INFERENCE = {
     "GPT2ForSequenceClassification",
     "RobertaForQuestionAnswering",
@@ -562,7 +568,12 @@ class HuggingfaceRunner(BenchmarkRunner):
     def get_tolerance_and_cosine_flag(self, is_training, current_device, name):
         cosine = self.args.cosine
         if is_training:
-            if name in REQUIRE_HIGHER_TOLERANCE_TRAINING:
+            from torch._inductor import config as inductor_config
+
+            if (name in REQUIRE_HIGHER_TOLERANCE_TRAINING) or (
+                inductor_config.max_autotune
+                and name in REQUIRE_HIGHER_TOLERANCE_MAX_AUTOTUNE_TRAINING
+            ):
                 return 2e-2, cosine
             else:
                 return 1e-2, cosine
