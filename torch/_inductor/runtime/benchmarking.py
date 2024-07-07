fix sleep cycle calculation

torch/_inductor/runtime/benchmarking.py
@@ -40,7 +40,7 @@ class Benchmarker:
 
         return benchmark
     
-    def benchmark_gpu(self, _callable: Callable[[], Any], estimation_iters: int = 5, memory_warmup_iters: int = 1000, benchmark_iters: int = 100, max_benchmark_duration: int = 25) -> float:        
+    def benchmark_gpu(self, _callable: Callable[[], Any], estimation_iters: int = 5, memory_warmup_iters: int = 100, benchmark_iters: int = 25, max_benchmark_duration: int = 25) -> float:        
         def benchmark(buffer, _callable, iters, measure_launch_overhead=False):
             event_pairs = self.get_event_pairs(iters)
             if measure_launch_overhead:
@@ -55,16 +55,18 @@ class Benchmarker:
                 end_time = time.perf_counter()
             torch.cuda.synchronize()
             if measure_launch_overhead:
-                return self.get_min_timing(event_pairs), end_time - start_time
+                return self.get_min_timing(event_pairs), (end_time - start_time) / iters
             return self.get_min_timing(event_pairs)
         
         def get_required_sleep_cycles(launch_overhead, memory_warmup_iters, benchmark_iters) -> int:
             total_overhead = (launch_overhead * benchmark_iters) + (self.get_launch_overhead_per_buffer_clear() * memory_warmup_iters)
             required_sleep_cycles = (total_overhead / self.get_time_per_million_sleep_cycles()) * 1000000
-            required_sleep_cycles *= 1.5
-            return required_sleep_cycles
+            return int(required_sleep_cycles)
+        
+        _callable()
 
         buffer = torch.empty(int(self.get_cache_size() // 4), dtype=torch.int, device="cuda")
+        buffer.zero_()
 
         estimated_timing, launch_overhead = benchmark(buffer, _callable, estimation_iters, measure_launch_overhead=True)
         benchmark_iters = min(benchmark_iters, max(int(max_benchmark_duration / estimated_timing), 1))
@@ -78,7 +80,7 @@ class Benchmarker:
         
         del buffer
 
-        return time
+        return timing
 
     @functools.lru_cache(None)
     def get_cache_size(self) -> int: