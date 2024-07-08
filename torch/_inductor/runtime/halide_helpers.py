6d3c4d9ac7f2c350714d3e87e8b4f4c4607212fc
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2024-07-08 00:42:34 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/runtime/halide_helpers.py
+++ torch/_inductor/runtime/halide_helpers.py
@@ -1,7 +1,15 @@
 # mypy: allow-untyped-defs
-import halide as hl  # type: ignore[import-untyped]
+import halide as hl  # type: ignore[import-untyped, import-not-found]
 
-N_ROUNDS_DEFAULT = 10  # Default number of rounds for philox
+PHILOX_N_ROUNDS_DEFAULT = 10  # Default number of rounds for philox
+PHILOX_KEY_A_U32 = hl.u32(0x9E3779B9)
+PHILOX_KEY_B_U32 = hl.u32(0xBB67AE85)
+PHILOX_ROUND_A_U32 = hl.u32(0xD2511F53)
+PHILOX_ROUND_B_U32 = hl.u32(0xCD9E8D57)
+PHILOX_KEY_A_U64 = hl.u64(0x9E3779B97F4A7C15)
+PHILOX_KEY_B_U64 = hl.u64(0xBB67AE8584CAA73B)
+PHILOX_ROUND_A_U64 = hl.u64(0xD2E7470EE14C6C93)
+PHILOX_ROUND_B_U64 = hl.u64(0xCA5A826395121157)
 
 
 def _pair_uniform_to_normal(u1, u2):
@@ -40,15 +48,19 @@ def philox_impl(c0, c1, c2, c3, k0, k1, n_rounds):
         return hl.cast(hl.UInt(32), ((a * b) >> 32) & hl.u64(0xFFFFFFFF))
 
     if c0.type() == hl.UInt(32):
-        PHILOX_KEY_A = hl.u32(0x9E3779B9)
-        PHILOX_KEY_B = hl.u32(0xBB67AE85)
-        PHILOX_ROUND_A = hl.u32(0xD2511F53)
-        PHILOX_ROUND_B = hl.u32(0xCD9E8D57)
+        PHILOX_KEY_A, PHILOX_KEY_B, PHILOX_ROUND_A, PHILOX_ROUND_B = (
+            PHILOX_KEY_A_U32,
+            PHILOX_KEY_B_U32,
+            PHILOX_ROUND_A_U32,
+            PHILOX_ROUND_B_U32,
+        )
     else:
-        PHILOX_KEY_A = hl.u64(0x9E3779B97F4A7C15)
-        PHILOX_KEY_B = hl.u64(0xBB67AE8584CAA73B)
-        PHILOX_ROUND_A = hl.u64(0xD2E7470EE14C6C93)
-        PHILOX_ROUND_B = hl.u64(0xCA5A826395121157)
+        PHILOX_KEY_A, PHILOX_KEY_B, PHILOX_ROUND_A, PHILOX_ROUND_B = (
+            PHILOX_KEY_A_U64,
+            PHILOX_KEY_B_U64,
+            PHILOX_ROUND_A_U64,
+            PHILOX_ROUND_B_U64,
+        )
 
     for _ in range(n_rounds):
         _c0, _c2 = c0, c2
@@ -79,13 +91,13 @@ def halide_philox(seed, c0, c1, c2, c3, n_rounds):
     return philox_impl(c0, c1, c2, c3, seed_lo, seed_hi, n_rounds)
 
 
-def randint4x(seed: hl.Expr, offsets: hl.Expr, n_rounds=10):
+def randint4x(seed: hl.Expr, offsets: hl.Expr, n_rounds):
     offsets = hl.cast(hl.UInt(32), offsets)
     _0 = offsets * hl.u32(0)
     return halide_philox(seed, offsets, _0, _0, _0, n_rounds)
 
 
-def rand4x(seed, offsets, n_rounds=N_ROUNDS_DEFAULT):
+def rand4x(seed, offsets, n_rounds=PHILOX_N_ROUNDS_DEFAULT):
     i1, i2, i3, i4 = randint4x(seed, offsets, n_rounds)
     u1 = _uint_to_uniform_float(i1)
     u2 = _uint_to_uniform_float(i2)
@@ -94,18 +106,18 @@ def rand4x(seed, offsets, n_rounds=N_ROUNDS_DEFAULT):
     return u1, u2, u3, u4
 
 
-def randint(seed, offset, n_rounds=N_ROUNDS_DEFAULT):
+def randint(seed, offset, n_rounds=PHILOX_N_ROUNDS_DEFAULT):
     ret, _, _, _ = randint4x(seed, offset, n_rounds)
     return ret
 
 
-def rand(seed, offset, n_rounds=N_ROUNDS_DEFAULT):
+def rand(seed, offset, n_rounds=PHILOX_N_ROUNDS_DEFAULT):
     source = randint(seed, offset, n_rounds)
     return _uint_to_uniform_float(source)
 
 
 def randn(seed, offset):
-    i1, i2, _, _ = randint4x(seed, offset, N_ROUNDS_DEFAULT)
+    i1, i2, _, _ = randint4x(seed, offset, PHILOX_N_ROUNDS_DEFAULT)
     u1 = _uint_to_uniform_float(i1)
     u2 = _uint_to_uniform_float(i2)
     n1, _ = _pair_uniform_to_normal(u1, u2)
@@ -113,7 +125,7 @@ def randn(seed, offset):
 
 
 def randint64(seed: hl.Expr, offsets: hl.Expr, low, high):
-    r0, r1, r2, r3 = randint4x(seed, offsets)
+    r0, r1, r2, r3 = randint4x(seed, offsets, PHILOX_N_ROUNDS_DEFAULT)
     r0 = hl.cast(hl.UInt(64), r0)
     r1 = hl.cast(hl.UInt(64), r1)
 
@@ -138,8 +150,8 @@ def main(seed: int, offsets: list[int]):
 
     randints = hl.Func("randints")
 
-    y = randint4x(seed_input, offset_input, N_ROUNDS_DEFAULT)
-    # y = rand4x(seed_input, offset_input, N_ROUNDS_DEFAULT) ok
+    y = randint4x(seed_input, offset_input, PHILOX_N_ROUNDS_DEFAULT)
+    # y = rand4x(seed_input, offset_input, PHILOX_N_ROUNDS_DEFAULT) ok
     # y = randint64(seed_input, offset_input, 0, 1000) ok
     # y = randint(seed_input, offset_input, n_rounds)
     # y = rand(seed_input, offset_input, n_rounds)
