1a5037d55507a866b8cee5f0fdf88d570340ce3b
mwlon <m.w.loncaric@gmail.com>
2024-07-09 13:25:10 -0400

    replace error with warning, added new error

--- torch/csrc/distributed/c10d/socket.cpp
+++ torch/csrc/distributed/c10d/socket.cpp
@@ -588,6 +588,11 @@ bool SocketListenOp::tryListen(int family) {
     }
   }
 
+  recordError(
+      "The server could not be initialized on any address for port={}, family={}",
+      port_,
+      family);
+
   return false;
 }
 
@@ -595,7 +600,7 @@ bool SocketListenOp::tryListen(const ::addrinfo& addr) {
   SocketImpl::Handle hnd =
       ::socket(addr.ai_family, addr.ai_socktype, addr.ai_protocol);
   if (hnd == SocketImpl::invalid_socket) {
-    recordError(
+    C10D_DEBUG(
         "The server socket cannot be initialized on {} {}.",
         addr,
         getSocketError());
