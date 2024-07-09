bed62b3cbfb92ba31659c0de3b8f3693747e1bf5
Peter Bell <peterbell10@live.co.uk>
2024-07-09 15:51:14 +0100

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/ProcessGroupGloo.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupGloo.cpp
@@ -2425,7 +2425,7 @@ class AsyncScatterWork : public ProcessGroupGloo::AsyncWork {
             seq,
             "gloo:scatter",
             !inputs.empty() ? std::optional<std::vector<at::Tensor>>(inputs[0])
-                            : c10::nullopt),
+                            : std::nullopt),
         context(context),
         outputs(outputs),
         inputs(inputs),
@@ -2888,7 +2888,7 @@ class AsyncBarrierWork : public ProcessGroupGloo::AsyncWork {
             OpType::BARRIER,
             seq,
             "gloo:barrier",
-            c10::nullopt),
+            std::nullopt),
         context(context),
         priorWork(std::move(priorWork)),
         tag(tag) {}
