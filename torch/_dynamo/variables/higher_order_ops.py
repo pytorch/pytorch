230bbc01ee058a7cddb99b7c3e4db4f03cebd6ab
chilli <chilli@meta.com>
2024-07-08 19:45:20 -0700

    Add scale kwarg to FlexAttention (and some changes that get FlexAttention numerics to be as accurate as FA2) (#130250)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130250
    Approved by: https://github.com/drisspg
    ghstack dependencies: #130160, #130106, #130224, #130227

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -1599,6 +1599,7 @@ class FlexAttentionHigherOrderVariable(TorchHigherOrderOperatorVariable):
             value,
             score_mod,
             block_mask,
+            scale,
         ) = self.normalize_to_args(args, kwargs)
 
         p_args = self.create_wrapped_node(tx, query, score_mod)
@@ -1607,6 +1608,7 @@ class FlexAttentionHigherOrderVariable(TorchHigherOrderOperatorVariable):
             key,
             value,
             block_mask,
+            scale,
         ]
 
         # Store the invocation as a call
@@ -1624,7 +1626,7 @@ class FlexAttentionHigherOrderVariable(TorchHigherOrderOperatorVariable):
         example_value = (out_meta, lse_meta)
 
         # Compose the ordered HOO args from two parts:
-        # - inp_args: [query, key, value, block_mask]
+        # - inp_args: [query, key, value, block_mask, scale]
         # - p_args: [score_mod, *other_buffers]
         return wrap_fx_proxy(
             tx=tx,
