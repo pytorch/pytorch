afc84208109d2338fc60615c391d45f4de96189a
chuanqiw <chuanqi.wang@intel.com>
2024-07-10 14:13:01 -0700

    disable python 3.13 for xpu nightly build

--- .github/scripts/generate_binary_build_matrix.py
+++ .github/scripts/generate_binary_build_matrix.py
@@ -364,8 +364,10 @@ def generate_wheels_matrix(
                 else arch_version
             )
 
-            # TODO: Enable python 3.13 on rocm, aarch64, windows
-            if (gpu_arch_type == "rocm" or os != "linux") and python_version == "3.13":
+            # TODO: Enable python 3.13 on rocm, xpu, aarch64, windows
+            if (
+                gpu_arch_type in ["rocm", "xpu"] or os != "linux"
+            ) and python_version == "3.13":
                 continue
 
             # 12.1 linux wheels require PYTORCH_EXTRA_INSTALL_REQUIREMENTS to install
