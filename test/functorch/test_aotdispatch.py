e57189de8f135d72ac2c14d6b792eff8322ea737
Colin Peppler <colinpeppler@meta.com>
2024-07-11 16:25:22 -0700

    Update on "[dynamo] add meta fn for aten.kthvalue.default"
    I saw
    ```
    torch._dynamo.exc.Unsupported: unsupported operator: aten.kthvalue.default
    ```
    
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -5917,7 +5917,6 @@ symbolic_aot_autograd_failures = {
     xfail(
         "index_fill", ""
     ),  # Cannot call sizes() on tensor with symbolic sizes/strides
-    xfail("kthvalue", ""),  # Cannot call sizes() on tensor with symbolic sizes/strides
     xfail(
         "linalg.lstsq", ""
     ),  # aten.linalg_lstsq.default - couldn't find symbolic meta function/decomposition
