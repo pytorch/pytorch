26657cb40b806bc8fcc80533aa9b3ba2ef780ed8
eqy <eddiey@nvidia.com>
2024-07-10 18:04:49 -0700

    lint

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -2274,21 +2274,21 @@ torch.cuda.synchronize()
     )
     def test_graph_debugdump(self):
         torch.cuda.empty_cache()
-        x=torch.randn(10240000, device="cuda")
-        y=torch.rand_like(x)
-        g=torch.cuda.CUDAGraph()
+        x = torch.randn(10240000, device="cuda")
+        y = torch.rand_like(x)
+        g = torch.cuda.CUDAGraph()
         g.enable_debug_mode()
         s0 = torch.cuda.Stream()
         s1 = torch.cuda.Stream()
         s0.wait_stream(torch.cuda.current_stream())
         with torch.cuda.stream(s0):
-          g.capture_begin()
-          z = x+y
-          with torch.cuda.stream(s1):
-            s1.wait_stream(s0)
-            w = z+y
-          s0.wait_stream(s1)
-          g.capture_end()
+            g.capture_begin()
+            z = x + y
+            with torch.cuda.stream(s1):
+                s1.wait_stream(s0)
+                w = z + y
+            s0.wait_stream(s1)
+            g.capture_end()
         s0.synchronize()
         torch.cuda.synchronize()
         with tempfile.TemporaryDirectory() as tempdir:
