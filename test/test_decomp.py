93081bab452c6c9254bb8a299f90fa405c88b5c4
Isuru Fernando <ifernando@quansight.com>
2024-07-12 17:28:32 +0000

    Update (base update)
    [ghstack-poisoned]

--- test/test_decomp.py
+++ test/test_decomp.py
@@ -214,6 +214,12 @@ def op_assert_ref(test_case, op, test_dtype, i, orig, decomp, ref, args, kwargs)
         (torch.bfloat16, torch.ops.aten.multi_margin_loss.default): 3e-2,
         (torch.float16, torch.ops.aten.multilabel_margin_loss_forward.default): 3e-2,
         (torch.bfloat16, torch.ops.aten.multilabel_margin_loss_forward.default): 3e-2,
+        (torch.float16, torch.ops.aten.reflection_pad1d_backward.default): 5e-3,
+        (torch.bfloat16, torch.ops.aten.reflection_pad1d_backward.default): 5e-3,
+        (torch.float16, torch.ops.aten.reflection_pad2d_backward.default): 5e-3,
+        (torch.bfloat16, torch.ops.aten.reflection_pad2d_backward.default): 5e-3,
+        (torch.float16, torch.ops.aten.reflection_pad3d_backward.default): 5e-3,
+        (torch.bfloat16, torch.ops.aten.reflection_pad3d_backward.default): 5e-3,
         # see https://github.com/pytorch/pytorch/pull/96264
         (torch.float16, torch.ops.aten.mv.default): 1e-5,
     }
--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -1759,7 +1759,9 @@ class CppKernel(Kernel):
 
         size_str = V.kernel.sexpr(self.rename_indexing(size)) if upper else None
 
-        line = self.indirect_assert(csevar, "0" if lower else None, size_str)
+        line = self.indirect_assert(
+            csevar, "0" if lower else None, size_str, self._load_mask
+        )
         self.cse.generate(buffer, line, assignment=False)
 
     def load(self, name: str, index: sympy.Expr):
@@ -2585,32 +2587,38 @@ class CppVecKernel(CppKernel):
             raise NotImplementedError
 
     def indirect_assert(self, var, lower, upper, mask=None):
-        assert not mask, "do not support mask in indirect_indexing assertion"
         assert isinstance(var, CppCSEVariable)
         assert var.dtype is not None
-        if not var.is_vec:
+        if not (var.is_vec or (mask and mask.is_vec)):
             return super().indirect_assert(var, lower, upper, mask)
+
+        var_dtype = var.dtype
         lower_scalar = lower
         upper_scalar = upper
+        var_scalar = var
+        if not var.is_vec:
+            var = f"{self._get_vec_type(var_dtype)}({var})"
+        if mask and not mask.is_vec:
+            mask = f"{self._get_vec_type(var_dtype)}({mask})"
         if lower:
-            lower = f"{self._get_vec_type(var.dtype)}({lower})"
+            lower = f"{self._get_vec_type(var_dtype)}({lower})"
         if upper:
-            upper = f"{self._get_vec_type(var.dtype)}({upper})"
+            upper = f"{self._get_vec_type(var_dtype)}({upper})"
         if lower and upper:
             cond = f"({lower} <= {var}) & ({var} < {upper})"
-            cond_print = f"{lower_scalar} <= {var} < {upper_scalar}"
+            cond_print = f"{lower_scalar} <= {var_scalar} < {upper_scalar}"
         elif lower:
             cond = f"{lower} <= {var}"
-            cond_print = f"{lower_scalar} <= {var}"
+            cond_print = f"{lower_scalar} <= {var_scalar}"
         else:
             assert upper
             cond = f"{var} < {upper}"
-            cond_print = f"{var} < {upper_scalar}"
-        cond = f"({self._get_mask_type(var.dtype)}({cond}))"
-        if self._load_mask is not None:
-            # We need not check when load_mask is False
-            cond = f"({cond} | ~({self._load_mask}))"
-        cond = f"{cond}.all_masked()"
+            cond_print = f"{var_scalar} < {upper_scalar}"
+        cond = f"{self._get_mask_type(var_dtype)}({cond})"
+        if mask is not None:
+            # We need not check when the mask is False
+            cond = f"({cond}) | ~({mask})"
+        cond = f"({cond}).all_masked()"
         return f'{self.assert_function}({cond}, "index out of bounds: {cond_print}")'
 
 
