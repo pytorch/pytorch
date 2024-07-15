a8d16bbfe62777a960c0cc29401dd9db750518de
IvanKobzarev <ivan.kobzarev@gmail.com>
2024-07-15 10:04:10 -0700

    [WIP] tensor subclasses + effects
    [ghstack-poisoned]

--- test/higher_order_ops/test_with_effects.py
+++ test/higher_order_ops/test_with_effects.py
@@ -369,6 +369,79 @@ def forward(self, arg0_1, arg1_1, arg2_1):
             self.assertTrue("MockModule.linear:mean" in recorded_dict)
             self.assertTrue("MockModule:mean" in recorded_dict)
 
+    def test_effectful_custom_op_with_subclass(self):
+        with torch.library._scoped_library("_mylib", "FRAGMENT") as lib:
+            lib.define("foo(Tensor x) -> Tensor")
+
+            def foo_impl(a):
+                return a.clone()
+
+            lib.impl("foo", foo_impl, "CPU")
+            lib.impl("foo", foo_impl, "CUDA")
+            lib.impl("foo", foo_impl, "Meta")
+
+            from torch._higher_order_ops.effects import (
+                _EffectType,
+                _register_effectful_op,
+            )
+
+            _register_effectful_op(torch.ops._mylib.foo.default, _EffectType.ORDERED)
+
+            class DoubleTensor(torch.Tensor):
+                @staticmethod
+                def __new__(cls, inner):
+                    outer_shape = (inner.shape[0] * 2,) + inner.shape[1:]
+                    return torch.Tensor._make_wrapper_subclass(
+                        cls,
+                        outer_shape,
+                        inner.stride(),
+                        None,
+                        None,
+                        inner.dtype,
+                        inner.layout,
+                        inner.device,
+                        False,
+                        inner.requires_grad,
+                    )
+
+                def __init__(self, inner):
+                    self.inner_elem = inner
+
+                def __repr__(self):
+                    inner_repr = repr(self.inner_elem)
+                    return f"DoubleTensor({inner_repr})"
+
+                def __tensor_flatten__(self):
+                    return ["inner_elem"], None
+
+                @staticmethod
+                def __tensor_unflatten__(inner_tensors, _, outer_size, outer_stride):
+                    return DoubleTensor(inner_tensors["inner_elem"])
+
+                @classmethod
+                def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
+                    if kwargs is None:
+                        kwargs = {}
+
+                    args_inner = torch.utils._pytree.tree_map_only(
+                        DoubleTensor, lambda x: x.inner_elem, args
+                    )
+                    kwargs_inner = torch.utils._pytree.tree_map_only(
+                        DoubleTensor, lambda x: x.inner_elem, kwargs
+                    )
+
+                    out_inner = func(*args_inner, **kwargs_inner)
+
+                    return DoubleTensor(out_inner)
+
+            def fn(x):
+                return torch.ops._mylib.foo(x)
+
+            compiled_fn = torch.compile(fn, backend="aot_eager")
+            subclass_x = DoubleTensor(torch.tensor([1.0, 2.0, 3.0]))
+
+            out = compiled_fn(subclass_x)
+
 
 if __name__ == "__main__":
     run_tests()
