42a480df51ec82f01041cb12390080272a8dafc6
Jiashen Cao <jiashenc@meta.com>
2024-07-16 09:32:58 -0700

    remove print

--- torch/fx/graph_module.py
+++ torch/fx/graph_module.py
@@ -83,8 +83,6 @@ _loader = _EvalCacheLoader()
 
 def _exec_with_source(src: str, globals: Dict[str, Any], co_fields=None):
     key = _loader.cache(src, globals, co_fields)
-    print("Src", src)
-    print("Key", key)
     exec(compile(src, key, "exec"), globals)
 
 
