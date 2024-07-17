e98135d1ad2f999fec649ecd21b35f3d5676be43
IvanKobzarev <ivan.kobzarev@gmail.com>
2024-07-17 04:12:30 -0700

    [aota] Needs autograd if an input requires_grad, agnostic to enable_grad (#128890)
    Reland of:  https://github.com/pytorch/pytorch/pull/128016
    
    Summary from previous PR:
    We assume only two possible mutually exclusive scenarios:
    
    Running compiled region for training (Any of inputs has requires_grad)
    
    Produced differentiable outputs should have requires_grad.
    Running compiled region for inference (None of inputs has requires_grad)
    
    All outputs do not have requires_grad.
    Even if user runs the region under no_grad(), but has an input Tensor with requires_grad - we go Training scenario (1).
    
    With current state that means:
    1/ needs_autograd should not check torch.is_grad_enabled(), only that any of inputs requires_grad
    2/ if needs_autograd => trace_joint (We are in training scenario 1.) => always run compiled region under with.enable_grad()
    
    Changes in partitioner?
    
    Inference and Training graphs had difference in return container, list/tuple.
    The changes in partitioner are done to unify and return always tuple.
    As a result - some changes in test_aotdispatch.py for graph contents list -> tuple.
    
    Why was revert?
    
    There was a regression of hf_Reformer model on inference.
    ```
    TORCHINDUCTOR_FX_GRAPH_CACHE=0 python benchmarks/dynamo/torchbench.py --performance --inference --bfloat16 --backend inductor --device cuda --only hf_Reformer --cold-start-latency --use-eval-mode
    ```
    
    Because one of the compiled graphs contained outputs, which are aliases to the inputs that are nn.Parameter(requires_grad=True).
    
    Even if inference bencharmsk torchbench runs inside with` torch.no_grad()` - alias (specifically for hf_Reformer - expand) ops preserve requires_grad.
    
    As a result we started compiling training graph instead of inference.
    
    Fix for view ops:
    
    If we have outputs, that are aliases to inputs that requires_grad, those outputs requires grad is not a reason to generate training graph.
    
    This is handled in aot_autograd.py, where output_and_mutation_safe are calculated.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128890
    Approved by: https://github.com/bdhirsh

--- test/distributed/_tensor/test_dtensor_compile.py
+++ test/distributed/_tensor/test_dtensor_compile.py
@@ -530,7 +530,7 @@ def forward(self, primals_1):
     wait_tensor = torch.ops._c10d_functional.wait_tensor.default(primals_1)
     sin = torch.ops.aten.sin.default(wait_tensor)
     sin_1 = torch.ops.aten.sin.default(sin);  sin = None
-    return [sin_1, primals_1, wait_tensor]""",
+    return (sin_1, primals_1, wait_tensor)""",
         )
 
     @unittest.skipIf(not has_triton(), "Inductor+gpu needs triton and recent GPU arch")
