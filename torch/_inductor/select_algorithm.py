e55a9c04202e6523f79de5d2b2f236736090046d
Nicolas Macchioni <nmacchioni@fb.com>
2024-07-17 12:03:29 -0700

    temporarily bypass kernel verify

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -1436,10 +1436,10 @@ class AlgorithmSelectorCache(PersistentCache):
             else:
                 # triton templates want the base pointer for sliced tensors
                 result = choice.benchmark(*example_inputs, out=out)
-            if VERIFY and expected is not None:
-                torch.testing.assert_close(out_extern, expected, **VERIFY)
-            if torch.cuda.is_available():
-                torch.cuda.synchronize()  # shake out any CUDA errors
+            # if VERIFY and expected is not None:
+            #     torch.testing.assert_close(out_extern, expected, **VERIFY)
+            # if torch.cuda.is_available():
+            #     torch.cuda.synchronize()  # shake out any CUDA errors
             return result
 
         def benchmark_in_current_process(choices):
