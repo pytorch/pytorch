83dc222a5710003edd6f57d8dc4d0810426b0d31
Catherine Lee <csl@fb.com>
2024-07-18 13:27:47 -0700

    tc

--- tools/testing/target_determination/heuristics/public_bindings.py
+++ tools/testing/target_determination/heuristics/public_bindings.py
@@ -1,25 +1,35 @@
 from __future__ import annotations
 
+from logging import warn
 from typing import Any
 
 from tools.testing.target_determination.heuristics.interface import (
     HeuristicInterface,
     TestPrioritizations,
 )
+from tools.testing.target_determination.heuristics.utils import (
+    query_changed_files,
+)
 
 from tools.testing.test_run import TestRun
 
 
-class HiPri(HeuristicInterface):
+class PublicBindings(HeuristicInterface):
     # Literally just a heuristic for test_public_bindings.  Pretty much anything
     # that changes the public API can affect this test
-    hi_pri = [
-        "test_public_bindings",
-    ]
+    test_public_bindings = "test_public_bindings"
 
     def __init__(self, **kwargs: dict[str, Any]) -> None:
         super().__init__(**kwargs)
 
     def get_prediction_confidence(self, tests: list[str]) -> TestPrioritizations:
-        test_ratings = {TestRun(k): 1.0 for k in self.hi_pri if k in tests}
+        test_ratings = {}
+        try:
+            changed_files = query_changed_files()
+        except Exception as e:
+            warn(f"Can't query changed test files due to {e}")
+            changed_files = []
+
+        if any(file.startswith("torch/") for file in changed_files):
+            test_ratings[TestRun(self.test_public_bindings)] = 1.0
         return TestPrioritizations(tests, test_ratings)
