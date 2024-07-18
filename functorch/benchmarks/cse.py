dc181825f0879020a257aeb15e33ac0a472e438e
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-18 23:18:26 +0800

    Update (base update)
    [ghstack-poisoned]

--- functorch/benchmarks/cse.py
+++ functorch/benchmarks/cse.py
@@ -1,6 +1,8 @@
 import torch
 import torch.fx as fx
+
 from functorch import make_fx
+
 from torch._functorch.compile_utils import fx_graph_cse
 from torch.profiler import profile, ProfilerActivity
 
