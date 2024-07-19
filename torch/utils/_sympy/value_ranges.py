e156ac04bbed9f10a569f0c8b32e029c824d84a4
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-19 20:55:15 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/utils/_sympy/value_ranges.py
+++ torch/utils/_sympy/value_ranges.py
@@ -161,13 +161,10 @@ class ValueRanges(Generic[_T]):
                 raise ValueRangeError(f"Invalid ranges [{lower}:{upper}]")
         except TypeError as e:
             raise TypeError(f"Could not compare {lower} <= {upper}") from e
-        # Because this is a frozen class
-        object.__setattr__(self, "lower", lower)
-        object.__setattr__(self, "upper", upper)
-        # Unlike bool/int in Python, we don't report bools are ints
-        object.__setattr__(self, "is_bool", isinstance(lower, SympyBoolean))
-        if self.is_bool:
-            assert isinstance(upper, SympyBoolean), (lower, upper)
+
+        is_bool_lower = isinstance(lower, SympyBoolean)
+        is_bool_upper = isinstance(upper, SympyBoolean)
+        assert is_bool_lower == is_bool_upper, (lower, upper)
 
         # Warning: is_int/is_float is best effort.  We do pretty well in
         # Dynamo, but in Inductor these attributes are often wrong because we
@@ -175,15 +172,26 @@ class ValueRanges(Generic[_T]):
         # the flexible analysis for is_int: sometimes a sympy.oo pops in for
         # an integer bound. I would /like/ for us not to do this, but it's
         # too hard to push the invariant through right now.
+        if isinstance(lower, sympy.Integer) and upper == sympy.oo:
+            upper = int_oo
+        if isinstance(upper, sympy.Integer) and lower == -sympy.oo:
+            lower = -int_oo
+        # NB: [-int_oo, -int_oo] and [int_oo, int_oo] are allowed
+        integer_types = (sympy.Integer, NegativeIntInfinity, IntInfinity)
+        is_int_lower = isinstance(lower, integer_types)
+        is_int_upper = isinstance(upper, integer_types)
 
+        # Because this is a frozen class
+        object.__setattr__(self, "lower", lower)
+        object.__setattr__(self, "upper", upper)
+        # Unlike bool/int in Python, we don't report bools are ints
+        #
+        # NB: is_bool_lower == is_bool_upper, so we only need to check one
+        object.__setattr__(self, "is_bool", is_bool_lower)
         object.__setattr__(
             self,
             "is_int",
-            not self.is_bool
-            and (
-                isinstance(lower, (sympy.Integer, NegativeIntInfinity))
-                and isinstance(upper, (sympy.Integer, IntInfinity))
-            ),
+            not self.is_bool and is_int_lower and is_int_upper,
         )
         """
         # This assert is just impossible right now, too many sympy bugs
