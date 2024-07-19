33c79f16d6d74da5700300d9ae33f081790e3831
drisspg <drisspguessous@gmail.com>
2024-07-18 21:11:54 -0700

    Update on "use _masked_softmax in math path"
    [ghstack-poisoned]

--- test/test_transformers.py
+++ test/test_transformers.py
@@ -1944,7 +1944,7 @@ class TestSDPA(NNTestCase):
     @parametrize("n_head", [3])
     @parametrize("head_dim", [8])
     @parametrize("mask_dim", [2, 4])
-    @parametrize("bool_mask", [0, 1])
+    @parametrize("bool_mask", [True, False])
     @parametrize("train", [True, False])
     def test_scaled_dot_product_fused_attention_mask_vs_math_cpu(
         self,
@@ -2011,6 +2011,25 @@ class TestSDPA(NNTestCase):
 
             if dtype in [torch.bfloat16, torch.float16]:
                 math_ref = math_ref.to(dtype)
+
+            # TODO WE NEED TO UNIFY THESE SEMANTICS
+            # This test the fully masked out rows case
+            self.assertFalse(torch.isnan(math_ref).any())
+            if torch.isnan(actual).any():
+                row_sums = attn_mask.sum(dim=-1)
+                masked_out_rows = (row_sums == 0)
+                if attn_mask.dim() == 2:
+                    masked_out_rows = masked_out_rows[None, None, :].expand(math_ref.shape[:-1])
+
+                # Slice out the fully masked rows from expected and actual
+                math_ref_masked_out = math_ref[masked_out_rows]
+                actual_masked_out = actual[masked_out_rows]
+
+                math_ref_all_zero = (math_ref_masked_out.abs().sum() == 0)
+                actual_all_nan = torch.isnan(actual_masked_out).all()
+                self.assertTrue(math_ref_all_zero)
+                self.assertTrue(actual_all_nan)
+                return
 
             self.assertEqual(actual, math_ref, atol=tol.atol, rtol=tol.rtol)
 
