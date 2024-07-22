7015344a1b0cc430046813c928976c1f3bde86d6
drisspg <drisspguessous@gmail.com>
2024-07-22 13:51:05 -0700

    Update on "Trying out different things for ima fix for flash-attention"
    # Summary
    My line of thinking for this: 
    
    We create the temporary split accum + LSE stats tensors to store the data for each split. We then launch a follow up kernel to do the reduction.
    
    Under ordinary non roofline memory usage the cuda memory caching allocator will keep these allocations alive even though the tensors were created within a temporary scope and no longer have any live references.
    
    On CI we often run near max memory usage. We change/add tests and suddenly we get close to oom threshold. The memory allocator will reap these segments and we get write after free errors.
    
    After that  fix I did get further past the splitkv_flash kernel and then got the following error:
    
    ``` Shell
    ❯ TORCH_DISABLE_ADDR2LINE=1 PYTORCH_NO_CUDA_MEMORY_CACHING=1  compute-sanitizer --show-backtrace=device --tool memcheck --log-file ima.txt python ima.py
    
    softmax_lseaccum_ptr =0x7f5ebb208a00
    oaccum_ptr =0x7f5ebb208c00
    softmax_lse_ptr = 0x7f5ebb208800
    ❯ 
    ❯ head ima.txt -n 10
    ========= COMPUTE-SANITIZER
    ========= Invalid __global__ write of size 4 bytes
    =========     at void pytorch_flash::flash_fwd_splitkv_combine_kernel<pytorch_flash::Flash_fwd_kernel_traits<(int)32, (int)64, (int)256, (int)4, (bool)0, (bool)0, cutlass::bfloat16_t, pytorch_flash::Flash_kernel_traits<(int)32, (int)64, (int)256, (int)4, cutlass::bfloat16_t>>, (int)16, (int)1, (bool)1>(pytorch_flash::Flash_fwd_params)+0x630
    =========     by thread (2,0,0) in block (0,0,0)
    =========     Address 0x7f5ebb208804 is out of bounds
    =========     and is 1 bytes after the nearest allocation at 0x7f5ebb208800 of size 4 bytes
    ```
    
    Okay I looked at the address and it looks like we are writing consective bytes past the softmax_lse_ptr in from the combine func: I tried padding out the softmax_lse to q_padded and no more illegal memory errors on my repro:
    ```
    ========= COMPUTE-SANITIZER
    ========= ERROR SUMMARY: 0 errors
    ```
    
    Fixes https://github.com/pytorch/pytorch/issues/131240
    Fixes https://github.com/pytorch/pytorch/issues/131227
    Fixes https://github.com/pytorch/pytorch/issues/131221
    
    
    
    
    
    [ghstack-poisoned]

--- aten/src/ATen/native/transformers/cuda/flash_attn/flash_api.cpp
+++ aten/src/ATen/native/transformers/cuda/flash_attn/flash_api.cpp
@@ -38,7 +38,6 @@
 
 #include <c10/util/Exception.h>
 
-
 namespace pytorch_flash {
 
 #define CHECK_DEVICE(x) TORCH_CHECK(x.is_cuda(), #x " must be on CUDA")
@@ -317,8 +316,8 @@ std::tuple<at::Tensor, at::Tensor> set_params_splitkv(Flash_fwd_params &params, 
         if (params.num_splits > 1) {
             softmax_lse_accum = at::empty({params.num_splits, batch_size, num_heads, max_seqlen_q}, opts.dtype(at::kFloat));
             out_accum = at::empty({params.num_splits, batch_size, num_heads, max_seqlen_q, head_size_rounded}, opts.dtype(at::kFloat));
-            // params.softmax_lseaccum_ptr = softmax_lse_accum.data_ptr();
-            // params.oaccum_ptr = out_accum.data_ptr();
+            params.softmax_lseaccum_ptr = softmax_lse_accum.data_ptr();
+            params.oaccum_ptr = out_accum.data_ptr();
         }
         TORCH_CHECK(params.num_splits <= 128, "num_splits > 128 not supported");
     }
@@ -479,23 +478,6 @@ mha_fwd(const at::Tensor &q,         // batch_size x seqlen_q x num_heads x head
     std::tie(softmax_lse_accum, out_accum) = set_params_splitkv(params, batch_size, num_heads,
                         head_size, seqlen_k, seqlen_q,
                         head_size_rounded, p_dropout, /*num_splits*/0, dprops, opts);
-    if (softmax_lse_accum.defined()){
-       params.softmax_lseaccum_ptr = softmax_lse_accum.data_ptr();
-    }
-    if (out_accum.defined()){
-       params.oaccum_ptr = out_accum.data_ptr();
-    }
-
-    // std::cout<<"softmax_lseaccum_ptr =" << softmax_lse_accum.data_ptr() << std::endl;
-    // std::cout<<"oaccum_ptr =" << out_accum.data_ptr() << std::endl;
-    // std::cout <<"softmax_lse_ptr = "<< params.softmax_lse_ptr << std::endl;
-
-    // Keep references to these tensors to extend their lifetime
-    // params.softmax_lse_accum = softmax_lse_accum;
-    // params.out_accum = out_accum;
-    // set_params_splitkv(params, batch_size, num_heads,
-    //                    head_size, seqlen_k, seqlen_q,
-    //                    head_size_rounded, p_dropout, /*num_splits*/0, dprops, opts);
 
     // We want to checkpoint and save the RNG state for backward if dropout
     // We get the default generator and return the seed and offset which will
@@ -548,7 +530,6 @@ mha_fwd(const at::Tensor &q,         // batch_size x seqlen_q x num_heads x head
         q_padded = q_padded.transpose(1, 2).reshape({batch_size, 1, num_heads_k * seqlen_q, head_size_og});
         softmax_lse = softmax_lse.reshape({batch_size, num_heads_k * seqlen_q, 1});
     }
-
     softmax_lse = softmax_lse.slice(2, 0, seqlen_q);
     return {out, q_padded, k_padded, v_padded, softmax_lse, seed_t, offset_t, p};
 }
