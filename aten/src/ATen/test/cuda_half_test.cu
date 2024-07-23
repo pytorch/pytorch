c848de362b2f38e51ca06d5c8c88b328c344684e
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-07-22 18:25:42 -0700

    Update aten/src/ATen/test/cuda_half_test.cu

--- aten/src/ATen/test/cuda_half_test.cu
+++ aten/src/ATen/test/cuda_half_test.cu
@@ -54,7 +54,7 @@ __device__ void test(){
   assert(::abs(::asinh(Half(1.0)) - ::asinh(1.0f)) <= threshold);
   // See note below about VC++ and isinf
 #ifndef  _MSC_VER
-  assert(::isinf(::atanh(Half(1.0));
+  assert(::isinf(::atanh(Half(1.0))));
 #endif
   assert(::abs(::atanh(Half(.5)) - ::atanh(.5f)) <= threshold);
   assert(::abs(::asin(Half(1.0)) - ::asin(1.0f)) <= threshold);
