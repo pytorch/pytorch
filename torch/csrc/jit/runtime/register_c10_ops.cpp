98f850168a61c648b36250ee21b277637b3867e3
Zain Rizvi <ZainR@meta.com>
2024-07-23 11:29:51 -0500

    add fake cpp change

--- torch/csrc/jit/runtime/register_c10_ops.cpp
+++ torch/csrc/jit/runtime/register_c10_ops.cpp
@@ -25,6 +25,10 @@ class RegistrationListener final : public c10::OpRegistrationListener {
       // TODO Find a better way to handle this.
       return;
     }
+    if (op.schema().name() == "fake")
+    {
+      return
+    }
     torch::jit::registerOperator(createOperatorFromC10(op));
   }
 
