78b934df4e759e85d587397cceeb35bf942d4720
Nicolas Macchioni <nmacchioni@fb.com>
2024-07-22 17:12:00 -0700

    Update test_benchmarking.py

--- test/inductor/test_benchmarking.py
+++ test/inductor/test_benchmarking.py
@@ -8,7 +8,7 @@ import torch
 from torch._dynamo.utils import counters
 from torch._inductor.runtime.benchmarking import Benchmarker, LazyBenchmark
 from torch._inductor.test_case import run_tests, TestCase
-from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_CPU, HAS_CUDA
+from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_CPU, HAS_CUDA, requires_gpu
 
 
 log = logging.getLogger(__name__)
@@ -138,7 +138,7 @@ class TestLazyBenchmark(TestCase):
     def test_rsub(self):
         # test LazyBenchmark.__rsub__ implementation
         lazy_benchmark = LazyBenchmark(lambda: 0.0)
-        self.assertEqual(float(1 - lazy_benchmark), -1.0)
+        self.assertEqual(float(1 - lazy_benchmark), 1.0)
     
     @patches
     def test_rsub_lazyness(self):
@@ -195,33 +195,22 @@ class TestLazyBenchmark(TestCase):
         self.is_not_finalized()
 
 
-
 class TestBenchmarking(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
-    def get_cpu_fn_args_kwargs(self):
-        return torch.sum, [torch.randn(1000)], {}
+    def get_fn_args_kwargs(self, device):
+        return (torch.sum, [torch.randn(1000, device=device)], {})
 
-    def get_gpu_fn_args_kwargs(self):
-        return torch.sum, [torch.randn(1000, device=GPU_TYPE)], {}
-
-    def get_cpu_callable(self):
-        fn, args, kwargs = self.get_cpu_fn_args_kwargs()
-        return lambda: fn(*args, **kwargs)  # noqa: E731
-
-    def get_gpu_callable(self):
-        fn, args, kwargs = self.get_gpu_fn_args_kwargs()
-        return lambda: fn(*args, **kwargs)  # noqa: E731
-
-    def sanity_check_cpu_benchmark(self, _callable):
+    def sanity_check_cpu_benchmark(self, _callable, timing_ms):
         start_time = time.perf_counter()
         for _ in range(10):
             _callable()
-        return ((time.perf_counter() - start_time) * 1000) / 10
+        roofline_timing_ms = ((time.perf_counter() - start_time) * 1000) / 10
+        self.assertEqual(timing_ms <= roofline_timing_ms, True)
 
-    def sanity_check_gpu_benchmark(self, _callable):
+    def sanity_check_gpu_benchmark(self, _callable, timing_ms):
         start_event = torch.cuda.Event(enable_timing=True)
         end_event = torch.cuda.Event(enable_timing=True)
         start_event.record()
@@ -229,307 +218,73 @@ class TestBenchmarking(TestCase):
             _callable()
         end_event.record()
         torch.cuda.synchronize()
-        return start_event.elapsed_time(end_event) / 10
-
-    @patches
-    def test_benchmarker_basics_cpu(self):
-        benchmarker = Benchmarker()
-
-        # test benchmarker.benchmark
-        fn, args, kwargs = self.get_cpu_fn_args_kwargs()
-        _callable = self.get_cpu_callable()
-        timing = benchmarker.benchmark(fn, args, kwargs)
-        sanity_check_timing = self.sanity_check_cpu_benchmark(_callable)
-        self.assertEqual(timing <= sanity_check_timing, True)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_cpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_cpu
-        timing = benchmarker.benchmark_cpu(_callable)
-        self.assertEqual(timing <= sanity_check_timing, True)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_cpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_many_cpu with single callable
-        timings = benchmarker.benchmark_many_cpu([_callable for _ in range(1)])
-        self.assertEqual(
-            all([timing <= sanity_check_timing for timing in timings], True)
-        )
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_many_cpu"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_cpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_many_cpu with many callables
-        timings = benchmarker.benchmark_many_cpu([_callable for _ in range(10)])
-        self.assertEqual(
-            all([timing <= sanity_check_timing for timing in timings], True)
-        )
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_many_cpu"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_cpu"], 10)
-        counters.clear()
-
-    @patches
-    def test_benchmarker_basics_gpu(self):
-        benchmarker = Benchmarker()
-
-        # test benchmarker.benchmark
-        fn, args, kwargs = self.get_gpu_fn_args_kwargs()
-        timing = benchmarker.benchmark(fn, args, kwargs)
-        sanity_check_timing = self.sanity_check_gpu_benchmark(_callable)
-        self.assertEqual(timing <= sanity_check_timing, True)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_gpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_gpu
-        _callable = self.get_gpu_callable()
-        timing = benchmarker.benchmark_gpu(_callable)
-        self.assertEqual(timing <= sanity_check_timing, True)
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_gpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_many_gpu with single callable
-        timings = benchmarker.benchmark_many_gpu([_callable for _ in range(1)])
-        self.assertEqual(
-            all([timing <= sanity_check_timing for timing in timings], True)
-        )
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_many_gpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_many_gpu with many callables
-        timings = benchmarker.benchmark_many_gpu([_callable for _ in range(10)])
-        self.assertEqual(
-            all([timing <= sanity_check_timing for timing in timings], True)
-        )
-        self.assertEqual(counters["inductor"]["benchmarking_benchmark_many_gpu"], 1)
-        counters.clear()
-
-        # test benchmarker.benchmark_gpu with failing callable
-        bad_callable = lambda: 1 / 0  # noqa: E731
-        timing = benchmarker.benchmark_gpu(bad_callable)
-        self.assertEqual(timing, float("inf"))
-        self.assertEqual(
-            counters["inductor"]["benchmarking_callable_initialization_failed"], 1
-        )
-        counters.clear()
-
-        # test benchmarker.benchmark_many_gpu with all failing callables
-        bad_callable = lambda: 1 / 0  # noqa: E731
-        timings = benchmarker.benchmark_many_gpu([bad_callable for _ in range(10)])
-        self.assertEqual(all([timing == float("inf") for timing in timings]), True)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_callable_initialization_failed"], 10
-        )
-        counters.clear()
-
-        # test benchmarker.benchmark_many_gpu with some failing callables
-        good_callable = self.get_gpu_callable()
-        bad_callable = lambda: 1 / 0  # noqa: E731
-        good_callable_timing, bad_callable_timing = benchmarker.benchmark_many_gpu(
-            [_callable, bad_callable]
-        )
-        self.assertEqual(good_callable_timing <= sanity_check_timing, True)
-        self.assertEqual(bad_callable_timing, float("inf"))
-        self.assertEqual(
-            counters["inductor"]["benchmarking_callable_initialization_failed"], 1
-        )
-        counters.clear()
-
-    @patches
-    def test_benchmarker_early_ranking(self):
-        benchmarker = Benchmarker()
-        _callable = self.get_gpu_callable()
-        benchmarker.benchmark_gpu([_callable for _ in range(10)], ranking_key="testing")
-        self.assertEqual(counters["inductor"]["benchmarking_early_ranking"], 1)
-        counters.clear()
-
-    @patches
-    def test_benchmarker_early_pruning(self):
-        benchmarker = Benchmarker()
-        _callable = self.get_gpu_callable()
-        benchmarker.benchmark_gpu([_callable for _ in range(10)], pruning_key="testing")
-        self.assertEqual(counters["inductor"]["benchmarking_early_pruning"], 1)
-        counters.clear()
-
-    @patches
-    def test_benchmarker_properties_cpu(self):
-        benchmarker = Benchmarker()
-        benchmarker.benchmark_cpu(self.get_cpu_callable())
+        roofline_timing_ms = start_event.elapsed_time(end_event) / 10
+        self.assertEqual(timing_ms <= roofline_timing_ms, True)
+    
+    def gpu_properties_are_not_initialized(self):
         self.assertEqual(counters["inductor"]["benchmarking_L2_cache_size"], 0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_gpu_time_per_gpu_clock_cycle"], 0
-        )
-        self.assertEqual(
-            counters["inductor"][
-                "benchmarking_get_cpu_launch_overhead_and_gpu_time_per_gpu_cache_clear"
-            ],
-            1,
-        )
-        self.assertEqual(
-            counters["inductor"][
-                "benchmarking_cpu_launch_overhead_per_gpu_cache_clear"
-            ],
-            0,
-        )
-        self.assertEqual(
-            counters["inductor"]["benchmarking_gpu_time_per_gpu_cache_clear"], 0
-        )
-        counters.clear()
-
-    @patches
-    def test_benchmarker_properties_gpu(self):
-        benchmarker = Benchmarker()
-        benchmarker.benchmark_gpu(self.get_gpu_callable())
-        benchmarker.benchmark_gpu(self.get_gpu_callable())
+        self.assertEqual(counters["inductor"]["benchmarking_gpu_queue_limit"], 0)
+        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_per_event_record"], 0)
+        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 0)
+        self.assertEqual(counters["inductor"]["benchmarking_gpu_time_ms_per_gpu_clock_cycle"], 0)
+    
+    def gpu_properties_are_singly_initialized(self):
         self.assertEqual(counters["inductor"]["benchmarking_L2_cache_size"], 1)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_gpu_time_per_gpu_clock_cycle"], 1
-        )
-        self.assertEqual(
-            counters["inductor"][
-                "benchmarking_get_cpu_launch_overhead_and_gpu_time_per_gpu_cache_clear"
-            ],
-            1,
-        )
-        self.assertEqual(
-            counters["inductor"][
-                "benchmarking_cpu_launch_overhead_per_gpu_cache_clear"
-            ],
-            1,
-        )
-        self.assertEqual(
-            counters["inductor"]["benchmarking_gpu_time_per_gpu_cache_clear"], 1
-        )
-        counters.clear()
-
+        self.assertEqual(counters["inductor"]["benchmarking_gpu_queue_limit"], 1)
+        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_per_event_record"], 1)
+        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 1)
+        self.assertEqual(counters["inductor"]["benchmarking_gpu_time_ms_per_gpu_clock_cycle"], 1)
+    
+    @requires_gpu()
     @patches
-    def test_lazy_benchmark_magic_methods(self):
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(float(lazy_benchmark), 0.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
+    def test_benchmark_validity(self):
+        benchmarker = Benchmarker()
 
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(format(lazy_benchmark, f"{0}"), format(0.0, f"{0}"))
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
+        # test CPU path
+        fn, args, kwargs = self.get_fn_args_kwargs(device="cpu")
+        _callable = lambda: fn(*args, **kwargs)
+        timing_ms = benchmarker.benchmark(fn, args, kwargs)
+        self.sanity_check_cpu_benchmark(_callable, timing_ms)
 
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(str(lazy_benchmark), str(0.0))
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(0.0 > -1.0, True)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(0.0 >= -1.0, True)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(0.0 < 1.0, True)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(0.0 <= 1.0, True)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 1
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0) + 1.0  # noqa: E731
-        self.assertEqual(lazy_benchmark.value, 1.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = 1.0 + LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 1.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0) - 1.0  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, -1.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = 1.0 - LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 1.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0) * 1.0  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 0.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = 1.0 * LazyBenchmark(lambda: 0.0)  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 0.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = LazyBenchmark(lambda: 0.0) / 1.0  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 0.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
-
-        lazy_benchmark = 0.0 / LazyBenchmark(lambda: 1.0)  # noqa: E731
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 0
-        )
-        self.assertEqual(lazy_benchmark.value, 0.0)
-        self.assertEqual(
-            counters["inductor"]["benchmarking_finalize_lazy_benchmark"], 2
-        )
-        counters.clear()
+        # test GPU path
+        fn, args, kwargs = self.get_fn_args_kwargs(device="cuda")
+        _callable = lambda: fn(*args, **kwargs)
+        timing_ms = benchmarker.benchmark(fn, args, kwargs)
+        self.sanity_check_gpu_benchmark(_callable, timing_ms)
+    
+    @patches
+    def test_benchmark_cpu_validity(self):
+        benchmarker = Benchmarker()
+        fn, args, kwargs = self.get_fn_args_kwargs(device="cpu")
+        _callable = lambda: fn(*args, **kwargs)
+        timing_ms = benchmarker.benchmark_cpu(_callable)
+        self.sanity_check_cpu_benchmark(_callable, timing_ms)
+    
+    @patches
+    def test_benchmark_gpu_validity(self):
+        benchmarker = Benchmarker()
+        fn, args, kwargs = self.get_fn_args_kwargs(device="cuda")
+        _callable = lambda: fn(*args, **kwargs)
+        timing_ms = benchmarker.benchmark_gpu(_callable)
+        self.sanity_check_gpu_benchmark(_callable, timing_ms)
+    
+    @patches
+    def test_benchmark_many_cpu_validity(self):
+        benchmarker = Benchmarker()
+        fn_args_kwargs_list = [self.get_fn_args_kwargs(device="cpu") for _ in range(10)]
+        callables = [lambda: fn(*args, **kwargs) for fn, args, kwargs in fn_args_kwargs_list]
+        timings_ms = benchmarker.benchmark_many_cpu(callables)
+        for _callable, timing_ms in zip(callables, timings_ms):
+            self.sanity_check_cpu_benchmark(_callable, timing_ms)
+    
+    @patches
+    def test_benchmark_many_gpu_validity(self):
+        benchmarker = Benchmarker()
+        fn_args_kwargs_list = [self.get_fn_args_kwargs(device="cuda") for _ in range(10)]
+        callables = [lambda: fn(*args, **kwargs) for fn, args, kwargs in fn_args_kwargs_list]
+        timings_ms = benchmarker.benchmark_many_gpu(callables)
+        for _callable, timing_ms in zip(callables, timings_ms):
+            self.sanity_check_gpu_benchmark(_callable, timing_ms)
 
 
 if __name__ == "__main__":
