7499771dc3c81dc9996d83ce4ff9afa86fb04d92
Aaron Orenstein <aorenste@meta.com>
2024-07-23 23:37:04 +0000

    Fix fake tensor SymInt caching when there's a SymInt storage_offset (#131500)
    Test Plan: Internal unit tests failed before and succeeded after.
    
    Differential Revision: D60131273
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131500
    Approved by: https://github.com/clee2000

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -1580,10 +1580,19 @@ class FakeTensorMode(TorchDispatchMode):
             storage = view_arg.untyped_storage()
             with in_kernel_invocation_manager(self), maybe_suppress():
                 empty.set_(storage, storage_offset, shape, stride)
-        elif storage_offset != 0:
-            storage = empty.untyped_storage()
-            with in_kernel_invocation_manager(self), maybe_suppress():
-                empty.set_(storage, storage_offset, shape, stride)
+        else:
+            if isinstance(storage_offset, SymInt):
+                # Do it this way so we don't import symbolic_shapes (which imports
+                # expensive sympy) unless we have to.
+                from torch.fx.experimental.symbolic_shapes import guard_size_oblivious
+
+                zero_offset = guard_size_oblivious(storage_offset == 0)
+            else:
+                zero_offset = storage_offset == 0
+            if not zero_offset:
+                storage = empty.untyped_storage()
+                with in_kernel_invocation_manager(self), maybe_suppress():
+                    empty.set_(storage, storage_offset, shape, stride)
 
         if isinstance(storage_bytes, SymInt):
             # Do it this way so we don't import symbolic_shapes (which imports
