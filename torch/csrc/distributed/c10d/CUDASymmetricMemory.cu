9e286e34baf21ec728089cf098bf2b9cabe07d96
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-07-23 17:15:06 +0000

    Revert "Replace manual parsing of "TMPDIR", "TMP", "TEMP" and "TEMPDIR" with std::filesystem::temp_directory_path() (#130842)"
    This reverts commit d33804f8b6e2ea38f8446826a16be13ce4f9b71e.
    
    Reverted https://github.com/pytorch/pytorch/pull/130842 on behalf of https://github.com/clee2000 due to breaking some builds internally D60085710, Im not sure what the logs mean but I think its something about build size ([comment](https://github.com/pytorch/pytorch/pull/130842#issuecomment-2245799309))

--- torch/csrc/distributed/c10d/CUDASymmetricMemory.cu
+++ torch/csrc/distributed/c10d/CUDASymmetricMemory.cu
@@ -9,8 +9,6 @@
 #include <c10/cuda/driver_api.h>
 #endif
 
-#include <filesystem>
-
 #include <sys/socket.h>
 #include <sys/syscall.h>
 #include <sys/un.h>
@@ -114,9 +112,15 @@ class IpcChannel {
 
  private:
   static std::string get_socket_name(int pid) {
+    const char* tmp_dir = "/tmp";
+    for (const char* env_var : {"TMPDIR", "TMP", "TEMP", "TEMPDIR"}) {
+      if (const char* path = getenv(env_var)) {
+        tmp_dir = path;
+        break;
+      }
+    }
     std::ostringstream oss;
-    oss << std::filesystem::temp_directory_path().string() << "/symm_mem-"
-        << pid;
+    oss << tmp_dir << "/symm_mem-" << pid;
     return oss.str();
   }
 
