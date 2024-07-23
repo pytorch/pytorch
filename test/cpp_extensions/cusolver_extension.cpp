54c55acec341e30a70fb25a9c5e83ab9f5eb318d
Zitong Zhan <ztzhan1108@gmail.com>
2024-07-23 14:38:32 -0400

    revert cusolver_extension.cpp

--- test/cpp_extensions/cusolver_extension.cpp
+++ test/cpp_extensions/cusolver_extension.cpp
@@ -2,16 +2,12 @@
 #include <ATen/cuda/CUDAContext.h>
 
 #include <cusolverDn.h>
-#include <cuDSS.h>
 
 
 torch::Tensor noop_cusolver_function(torch::Tensor x) {
   cusolverDnHandle_t handle;
   TORCH_CUSOLVER_CHECK(cusolverDnCreate(&handle));
   TORCH_CUSOLVER_CHECK(cusolverDnDestroy(handle));
-  cudssHandle_t sphandle;
-  TORCH_CUDSS_CHECK(cudssCreate(&sphandle));
-  TORCH_CUDSS_CHECK(cudssDestroy(sphandle));
   return x;
 }
 
