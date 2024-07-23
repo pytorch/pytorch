568dcbc81eb2200427263bfee1b9d3299c35e128
Nicolas Macchioni <nmacchioni@fb.com>
2024-07-22 21:03:45 -0700

    Update test_benchmarking.py

--- test/inductor/test_benchmarking.py
+++ test/inductor/test_benchmarking.py
@@ -200,8 +200,9 @@ class TestBenchmarking(TestCase):
     def setUpClass(cls):
         super().setUpClass()
 
-    def get_fn_args_kwargs(self, device):
-        return (torch.sum, [torch.randn(1000, device=device)], {})
+    def get_fn_args_kwargs_callable(self, device):
+        fn, args, kwargs = torch.sum, [torch.randn(1000, device=device)], {}
+        return fn, args, kwargs, lambda: fn(*args, **kwargs)
 
     def sanity_check_cpu_benchmark(self, _callable, timing_ms):
         start_time_s = time.perf_counter()
@@ -224,15 +225,15 @@ class TestBenchmarking(TestCase):
     def gpu_properties_are_not_initialized(self):
         self.assertEqual(counters["inductor"]["benchmarking_L2_cache_size"], 0)
         self.assertEqual(counters["inductor"]["benchmarking_gpu_queue_limit"], 0)
-        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_per_event_record"], 0)
-        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 0)
+        self.assertEqual(counters["inductor"]["benchmarking_cpu_launch_overhead_ms_per_event_record"], 0)
+        self.assertEqual(counters["inductor"]["benchmarking_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 0)
         self.assertEqual(counters["inductor"]["benchmarking_gpu_time_ms_per_gpu_clock_cycle"], 0)
     
     def gpu_properties_are_singly_initialized(self):
         self.assertEqual(counters["inductor"]["benchmarking_L2_cache_size"], 1)
         self.assertEqual(counters["inductor"]["benchmarking_gpu_queue_limit"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_per_event_record"], 1)
-        self.assertEqual(counters["inductor"]["benchmarking_get_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 1)
+        self.assertEqual(counters["inductor"]["benchmarking_cpu_launch_overhead_ms_per_event_record"], 1)
+        self.assertEqual(counters["inductor"]["benchmarking_cpu_launch_overhead_ms_and_gpu_time_ms_per_gpu_cache_clear"], 1)
         self.assertEqual(counters["inductor"]["benchmarking_gpu_time_ms_per_gpu_clock_cycle"], 1)
     
     @requires_gpu()
@@ -247,13 +248,30 @@ class TestBenchmarking(TestCase):
         benchmarker = Benchmarker()
         gpu_queue_limit = benchmarker.gpu_queue_limit
         torch.cuda.synchronize()
-        torch.cuda._sleep(10000000)
-        for _ in range(gpu_queue_limit):
+        torch.cuda._sleep(100000000)
+        for _ in range(gpu_queue_limit - 1):
+            start_time_s = time.perf_counter()
             torch.cuda.Event(enable_timing=True).record()
-        start_time = time.perf_counter()
+            elapsed_time_ms = (time.perf_counter() - start_time_s) * 100
+            self.assertEqual(elapsed_time_ms < 1)
+        start_time_s = time.perf_counter()
         torch.cuda.Event(enable_timing=True).record()
-        elapsed_time_ms = (time.perf_counter() - start_time) * 1000
+        elapsed_time_ms = (time.perf_counter() - start_time_s) * 1000
+        torch.cuda.synchronize()
         self.assertEqual(elapsed_time_ms > 1)
+    
+    @requires_gpu
+    @patches
+    def test_cpu_launch_overhead_ms_per_event_record(self):
+        benchmarker = Benchmarker()
+        cpu_launch_overhead_ms_per_event_record = benchmarker.cpu_launch_overhead_ms_per_event_record
+        torch.cuda.synchronize()
+        start_time_s = time.perf_counter()
+        for _ in range(1000):
+            torch.cuda.Event(enable_timing=True).record()
+        elapsed_time_ms = (time.perf_counter() - start_time_s) * 1000
+        torch.cuda.synchronize()
+        self.assertEqual((elapsed_time_ms / 1000) <= (cpu_launch_overhead_ms_per_event_record * 1.5), True)
     
     @requires_gpu()
     @patches
@@ -261,22 +279,19 @@ class TestBenchmarking(TestCase):
         benchmarker = Benchmarker()
 
         # test CPU path
-        fn, args, kwargs = self.get_fn_args_kwargs(device="cpu")
-        _callable = lambda: fn(*args, **kwargs)
+        fn, args, kwargs, _callable = self.get_fn_args_kwargs_callable(device="cpu")
         timing_ms = benchmarker.benchmark(fn, args, kwargs)
         self.sanity_check_cpu_benchmark(_callable, timing_ms)
 
         # test GPU path
-        fn, args, kwargs = self.get_fn_args_kwargs(device="cuda")
-        _callable = lambda: fn(*args, **kwargs)
+        fn, args, kwargs, _callable = self.get_fn_args_kwargs_callable(device="cuda")
         timing_ms = benchmarker.benchmark(fn, args, kwargs)
         self.sanity_check_gpu_benchmark(_callable, timing_ms)
     
     @patches
     def test_benchmark_cpu_validity(self):
         benchmarker = Benchmarker()
-        fn, args, kwargs = self.get_fn_args_kwargs(device="cpu")
-        _callable = lambda: fn(*args, **kwargs)
+        _, _, _, _callable = self.get_fn_args_kwargs_callable(device="cpu")
         timing_ms = benchmarker.benchmark_cpu(_callable)
         self.sanity_check_cpu_benchmark(_callable, timing_ms)
     
@@ -284,16 +299,14 @@ class TestBenchmarking(TestCase):
     @patches
     def test_benchmark_gpu_validity(self):
         benchmarker = Benchmarker()
-        fn, args, kwargs = self.get_fn_args_kwargs(device="cuda")
-        _callable = lambda: fn(*args, **kwargs)
+        _, _, _, _callable = self.get_fn_args_kwargs_callable(device="cuda")
         timing_ms = benchmarker.benchmark_gpu(_callable)
         self.sanity_check_gpu_benchmark(_callable, timing_ms)
     
     @patches
     def test_benchmark_many_cpu_validity(self):
         benchmarker = Benchmarker()
-        fn_args_kwargs_list = [self.get_fn_args_kwargs(device="cpu") for _ in range(10)]
-        callables = [lambda: fn(*args, **kwargs) for fn, args, kwargs in fn_args_kwargs_list]
+        callables = [self.get_fn_args_kwargs_callable(device="cpu")[3] for _ in range(10)]
         timings_ms = benchmarker.benchmark_many_cpu(callables)
         for _callable, timing_ms in zip(callables, timings_ms):
             self.sanity_check_cpu_benchmark(_callable, timing_ms)
@@ -302,8 +315,7 @@ class TestBenchmarking(TestCase):
     @patches
     def test_benchmark_many_gpu_validity(self):
         benchmarker = Benchmarker()
-        fn_args_kwargs_list = [self.get_fn_args_kwargs(device="cuda") for _ in range(10)]
-        callables = [lambda: fn(*args, **kwargs) for fn, args, kwargs in fn_args_kwargs_list]
+        callables = [self.get_fn_args_kwargs_callable(device="cuda")[3] for _ in range(10)]
         timings_ms = benchmarker.benchmark_many_gpu(callables)
         for _callable, timing_ms in zip(callables, timings_ms):
             self.sanity_check_gpu_benchmark(_callable, timing_ms)
