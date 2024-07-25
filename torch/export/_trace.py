28914dc8029e12a408dd3f3faa8ec81b6620a2f9
Zhengxu Chen <zhxchen17@meta.com>
2024-07-25 09:58:13 -0700

    [export] Remove deprecated fields from ExportedProgram ctor. (#131697)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131697
    
    as title.
    
    Test Plan: CI
    
    Reviewed By: SherlockNoMad, ydwu4
    
    Differential Revision: D60078426

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -1912,7 +1912,7 @@ def _export_for_training(
         module_call_graph=module_call_graph,
         example_inputs=(args, kwargs),
         constants=export_artifact.aten.constants,
-        verifier=TrainingIRVerifier,
+        verifiers=[TrainingIRVerifier],
     )
 
     return exported_program
@@ -2036,6 +2036,8 @@ def _export(
     if not _is_torch_jit_trace:
         _verify_placeholder_names(gm, export_graph_signature)
 
+    from torch._export.verifier import Verifier
+
     exported_program = ExportedProgram(
         root=gm,
         graph=gm.graph,
@@ -2045,6 +2047,7 @@ def _export(
         module_call_graph=module_call_graph,
         example_inputs=(args, kwargs),
         constants=export_artifact.aten.constants,
+        verifiers=[Verifier],
     )
 
     return exported_program
