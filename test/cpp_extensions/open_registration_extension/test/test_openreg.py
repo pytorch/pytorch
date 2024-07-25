228481c1efe75616cbbe9cb1de9117900e05a974
albanD <desmaison.alban@gmail.com>
2024-07-25 17:07:54 -0400

    Update (base update)
    [ghstack-poisoned]

---
+++ test/cpp_extensions/open_registration_extension/test/test_openreg.py
@@ -0,0 +1,34 @@
+import torch
+
+from torch.testing._internal.common_utils import TestCase, run_tests
+import psutil
+import os
+
+import pytorch_openreg
+
+class TestOpenReg(TestCase):
+    def test_initializes(self):
+        self.assertEqual(torch._C._get_privateuse1_backend_name(), "openreg")
+
+    def test_autograd_init(self):
+        # Make sure autograd is initialized
+        torch.rand(2, requires_grad=True, device="openreg").sum().backward()
+
+        pid = os.getpid()
+        task_path = f'/proc/{pid}/task'
+        all_threads = psutil.Process(pid).threads()
+
+        all_thread_names = set()
+
+        for t in all_threads:
+            with open(f'{task_path}/{t.id}/comm', 'r') as file:
+                thread_name = file.read().strip()
+            all_thread_names.add(thread_name)
+
+        print(all_thread_names)
+        for i in range(pytorch_openreg.NUM_DEVICES):
+            self.assertIn(f"pt_autograd_{i}", all_thread_names)
+
+
+if __name__ == "__main__":
+    run_tests()
