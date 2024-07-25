7886d3d53713fe1fa847bb54684288d6851a5b20
Boyuan Feng <boyuan@meta.com>
2024-07-25 15:27:39 -0700

    nit

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -735,7 +735,7 @@ class triton:
 
     # Warn loudly when the number of cudagraphs due to dynamic shape
     # exceeds this limit
-    cudagraph_dynamic_shape_warn_limit = 50
+    cudagraph_dynamic_shape_warn_limit: Optional[int] = 50
 
     # synchronize after cudagraph invocation
     force_cudagraph_sync = False
