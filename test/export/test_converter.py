941e390f1c54cf9db674c43bada1a15b1163f356
angelayi <yiangela7@gmail.com>
2024-07-25 01:11:03 +0000

    [ts_convert] Update conversion for aten.tensor (#131549)
    Fixes aten::tensor issues in edgeml models
    P1492137675
    | suite   |   #models |   #has_ts_model |   #has_sample_inputs |   #ts_can_run |   #can_convert |   #ep_result_correct |   #can_package |   #sigmoid_can_run |   #sigmoid_result_correct |
    |---------|-----------|-----------------|----------------------|---------------|----------------|----------------------|----------------|--------------------|---------------------------|
    | EDGEML  |        34 |              25 |                   23 |            21 |              2 |                    2 |              2 |                  2 |                         2 |
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131549
    Approved by: https://github.com/jiashenC, https://github.com/SherlockNoMad

--- test/export/test_converter.py
+++ test/export/test_converter.py
@@ -26,7 +26,8 @@ class TestConverter(TestCase):
     ) -> ExportedProgram:
         # By default, it tests both jit.trace and jit.script.
         if option is None:
-            option = ["trace", "script"]
+            # option = ["trace", "script"]
+            option = ["script"]
 
         if check_persistent:
             num_iterations = 10
@@ -57,6 +58,7 @@ class TestConverter(TestCase):
             else:
                 raise RuntimeError(f"Unrecognized mode for torch.jit: {opt}")
 
+            print(opt, ts_model.graph)
             ep = TS2EPConverter(ts_model, inp).convert()
             ep_list.append(ep)
 
@@ -867,6 +869,67 @@ class TestConverter(TestCase):
         #             torch.randn([1, 1, 1]).to(torch.float32),
         #             torch.randn([1, 1, 1]).to(torch.float64),
         #         )[0], 1
+        #     )
+
+    def test_aten_tensor_dtype_int(self):
+        class M(torch.nn.Module):
+            def forward(self, x):
+                y = torch.tensor(1, dtype=torch.int32)
+                return y + x
+
+        ep_list = self._check_equal_ts_ep_converter(M(), (torch.tensor(1),))
+        for ep in ep_list:
+            self.assertEqual(len(ep.constants), 1)
+
+    def test_aten_tensor_prim_dtype(self):
+        class M(torch.nn.Module):
+            def forward(self, x):
+                y = torch.tensor(1, dtype=x.dtype)
+                return y + x
+
+        ep_list = self._check_equal_ts_ep_converter(M(), (torch.tensor(1),))
+        for ep in ep_list:
+            self.assertEqual(len(ep.constants), 1)
+
+    def test_aten_tensor_dynamic(self):
+        class M(torch.nn.Module):
+            def forward(self, x):
+                s = x.shape[0]
+                y = torch.tensor(s)
+                return y
+
+        ep = torch.export.export(
+            M(), (torch.ones(3),), dynamic_shapes=({0: torch.export.Dim("m")},)
+        )
+        ep_list = self._check_equal_ts_ep_converter(M(), (torch.ones(3),))
+        for ep in ep_list:
+            self.assertEqual(len(ep.constants), 0)
+
+        # TODO: Additional check once dynamic shape is supported.
+        # for ep in ep_list:
+        #     torch.testing.assert_close(
+        #         ep.module()(torch.ones(4)),
+        #         M()(torch.ones(4)),
+        #     )
+
+        class M(torch.nn.Module):
+            def forward(self, x):
+                s = x.shape[0]
+                y = torch.tensor([s, s * 2, 1])
+                return y
+
+        ep = torch.export.export(
+            M(), (torch.ones(3),), dynamic_shapes=({0: torch.export.Dim("m")},)
+        )
+        ep_list = self._check_equal_ts_ep_converter(M(), (torch.ones(3),))
+        for ep in ep_list:
+            self.assertEqual(len(ep.constants), 0)
+
+        # TODO: Additional check once dynamic shape is supported.
+        # for ep in ep_list:
+        #     torch.testing.assert_close(
+        #         ep.module()(torch.ones(4)),
+        #         M()(torch.ones(4)),
         #     )
 
     def test_prim_tolist(self):
