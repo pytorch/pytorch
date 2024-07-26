3c15e389fd048f5f444e2046751f0fc91e3201fc
Joel Schlosser <jbschlosser@meta.com>
2024-07-26 15:55:45 -0400

    Fix to support unary pointwise ops when an NJT is not the first arg
    [ghstack-poisoned]

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -6477,10 +6477,6 @@ FORWARD_FAILURES = {
     "masked.sum",
     "masked.var",
     # === BEGIN BUG SECTION ===
-    # NJT arg is second for these signatures; need to support this
-    *(f"polygamma.polygamma_n_{n}" for n in range(5)),
-    "special.polygamma.special_polygamma_n_0",
-    "ldexp",
     # Returns a tuple of Tensors so it doesn't work with NJT's unary pointwise logic
     "frexp",
     # Need to adjust sample input func to pass the right thing
@@ -6504,26 +6500,10 @@ FORWARD_FAILURES = {
 
 BACKWARD_FAILURES = {
     *FORWARD_FAILURES,
-    # TODO: sort these
-    "__rpow__",
-    "atanh",
+    # TODO: categorize these
     "cdouble",
     "cfloat",
     "chalf",
-    "clamp_max",
-    "clamp_min",
-    "copysign",
-    "digamma",
-    "float_power",
-    "max.binary",
-    "maximum",
-    "min.binary",
-    "minimum",
-    "pow",
-    "sgn",
-    "sinc",
-    "special.i1",
-    "special.i1e",
 }
 
 COMPILE_BACKWARD_FAILURES = {
