fdf8297a2c4635d60171e495c713fd1eeede306c
Luca Wehrstedt <lcw@meta.com>
2024-07-26 09:04:57 +0000

    Update
    [ghstack-poisoned]

--- torch/testing/_internal/common_cuda.py
+++ torch/testing/_internal/common_cuda.py
@@ -5,7 +5,7 @@ r"""This file is allowed to initialize CUDA context when imported."""
 import functools
 import torch
 import torch.cuda
-from torch.testing._internal.common_utils import LazyVal, TEST_NUMBA, TEST_WITH_ROCM, TEST_CUDA, IS_WINDOWS
+from torch.testing._internal.common_utils import LazyVal, TEST_NUMBA, TEST_WITH_ROCM, TEST_CUDA
 import inspect
 import contextlib
 import os
