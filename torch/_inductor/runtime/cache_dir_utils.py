24beefe432a78f66c5451435b614c512dfce8fe1
eellison <elias.ellison@gmail.com>
2024-07-26 13:42:16 -0700

    Update on "[WIP] Add compiler bisector"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy yf225 chenyang78 kadeng muchulee8 ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

---
+++ torch/_inductor/runtime/cache_dir_utils.py
@@ -0,0 +1,21 @@
+import os
+import getpass
+import re
+import tempfile
+
+# Factoring out to file without torch dependencies
+
+def cache_dir() -> str:
+    cache_dir = os.environ.get("TORCHINDUCTOR_CACHE_DIR")
+    if cache_dir is None:
+        os.environ["TORCHINDUCTOR_CACHE_DIR"] = cache_dir = default_cache_dir()
+    os.makedirs(cache_dir, exist_ok=True)
+    return cache_dir
+
+
+def default_cache_dir():
+    sanitized_username = re.sub(r'[\\/:*?"<>|]', "_", getpass.getuser())
+    return os.path.join(
+        tempfile.gettempdir(),
+        "torchinductor_" + sanitized_username,
+    )
