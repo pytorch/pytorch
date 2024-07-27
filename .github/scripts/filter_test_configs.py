50f20a24b933f43c2726f209d4cbae97fb7427eb
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-28 00:19:33 +0800

    Update
    [ghstack-poisoned]

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -505,6 +505,7 @@ def perform_misc_tasks(
         "ci-verbose-test-logs",
         check_for_setting(labels, pr_body, "ci-verbose-test-logs"),
     )
+    set_output("ci-showlocals", check_for_setting(labels, pr_body, "ci-showlocals"))
     set_output(
         "ci-no-test-timeout", check_for_setting(labels, pr_body, "ci-no-test-timeout")
     )
