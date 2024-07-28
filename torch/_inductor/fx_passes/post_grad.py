2cd7a7f88e100b7e140832f651468950b511bc9a
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-07-28 03:29:32 +0000

    Revert "[BE] typing for decorators - _inductor/fx_passes/post_grad (#131575)"
    This reverts commit 42dc5a47a157f9a441ceba53cf569cc42a640732.
    
    Reverted https://github.com/pytorch/pytorch/pull/131575 on behalf of https://github.com/clee2000 due to breaking lint internally D60265575 ([comment](https://github.com/pytorch/pytorch/pull/131572#issuecomment-2254328359))

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -5,18 +5,7 @@ import itertools
 import logging
 import operator
 from collections import Counter, defaultdict
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    List,
-    Optional,
-    Set,
-    TYPE_CHECKING,
-    TypeVar,
-    Union,
-)
-from typing_extensions import ParamSpec
+from typing import Any, Dict, List, Optional, Set, TYPE_CHECKING, Union
 
 import torch
 import torch._inductor as inductor
@@ -67,8 +56,6 @@ from .split_cat import POST_GRAD_PATTERNS
 if TYPE_CHECKING:
     from sympy import Expr
 
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
 
 log = logging.getLogger(__name__)
 aten = torch.ops.aten
@@ -202,9 +189,7 @@ def reorder_for_locality(graph: torch.fx.Graph):
         torch.fx.map_arg((node.args, node.kwargs), visit)
 
 
-def register_lowering_pattern(
-    pattern, extra_check=_return_true, pass_number=1
-) -> Callable[[Callable[_P, _T]], Callable[_P, _T]]:
+def register_lowering_pattern(pattern, extra_check=_return_true, pass_number=1):
     """
     Register an aten to inductor IR replacement pattern
     """
