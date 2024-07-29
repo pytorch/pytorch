9a133755e21f39ca32b35d68ffd6f60209aa5503
atalman <atalman@fb.com>
2024-07-29 16:06:25 -0700

    lint

--- .github/scripts/generate_binary_build_matrix.py
+++ .github/scripts/generate_binary_build_matrix.py
@@ -387,7 +387,11 @@ def generate_wheels_matrix(
                     }
                 )
                 # Special build building to use on Colab. PyThon 3.10 for 12.1 CUDA
-                if arch_version != "cuda-aarch64" and python_version == "3.10" and arch_version == "12.1":
+                if (
+                    arch_version != "cuda-aarch64"
+                    and python_version == "3.10"
+                    and arch_version == "12.1"
+                ):
                     ret.append(
                         {
                             "python_version": python_version,
