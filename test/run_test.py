4dd1ad02459a368359e33780cd72af7de237d7ce
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-29 22:37:10 +0800

    Update (base update)
    [ghstack-poisoned]

--- test/run_test.py
+++ test/run_test.py
@@ -1129,14 +1129,14 @@ def parse_args():
         parser.add_argument(
             "--showlocals",
             action=argparse.BooleanOptionalAction,
-            default=(not IS_CI),
+            default=False,
             help="Show local variables in tracebacks (default: True)",
         )
     else:
         parser.add_argument(
             "--showlocals",
             action="store_true",
-            default=(not IS_CI),
+            default=False,
             help="Show local variables in tracebacks (default: True)",
         )
         parser.add_argument("--no-showlocals", dest="showlocals", action="store_false")
