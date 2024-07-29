921f617fc34081fa64a1b1820ca29baec699f8bb
laithsakka <lsakka@meta.com>
2024-07-29 16:16:32 -0700

    Update base for Update on "build head~1 and run pr-time benchmarks on it"
    This PR will checkout the commit HEAD~1 and build it and then run the benchmarks on it and store the results 
    in a file. Next PR will add the comparison of numbers and the regression reporting. 
    check results by checking logs o the test (https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121)
    ```
    benchmark results on current PR: 
    [481](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:482)
    + cat /var/lib/jenkins/workspace/test/test-reports/pr_time_benchmarks_after.txt
    [482](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:483)
    update_hint_regression,instruction_count,28833781796
    [483](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:484)
    
    
    [32061](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:32062)
    + echo 'benchmark results on main: '
    [32062](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:32063)
    benchmark results on main: 
    [32063](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:32064)
    + cat /var/lib/jenkins/workspace/test/test-reports/pr_time_benchmarks_before.txt
    [32064](https://github.com/pytorch/pytorch/actions/runs/10149355968/job/28065140121#step:20:32065)
    update_hint_regression,instruction_count,28514131958
    
    ```
    
    
    
    [ghstack-poisoned]

--- benchmarks/dynamo/pr_time_benchmarks/benchmarks/update_hint_benchmark.py
+++ benchmarks/dynamo/pr_time_benchmarks/benchmarks/update_hint_benchmark.py
@@ -1,8 +1,9 @@
 import random
+import sys
+
+from benchmarks.dynamo.pr_time_benchmarks.benchmark_base import Benchmark
 
 import torch
-
-from ..benchmark_base import Benchmark
 
 
 class P0(Benchmark):
@@ -14,16 +15,15 @@ class P0(Benchmark):
     def description(self):
         return "information at https://github.com/pytorch/pytorch/pull/129893"
 
-    def reset(self):
-        pass
-
-    def prepare(self):
+    def prepare_once(self):
         torch._dynamo.config.capture_scalar_outputs = True
-        torch._dynamo.reset()
         random.seed(42)
         self.splits = torch.randint(10, (self.N,))
         sz = self.splits.sum().item()
         self.input = torch.randn(sz)
+
+    def prepare(self):
+        torch._dynamo.reset()
 
     def work(self):
         @torch.compile(fullgraph=True)
@@ -38,7 +38,8 @@ class P0(Benchmark):
 
 
 def main():
-    print(P0().enable_instruction_count().collect_all())
+    result_path = sys.argv[1]
+    P0().enable_instruction_count().collect_all().append_results(result_path)
 
 
 if __name__ == "__main__":
