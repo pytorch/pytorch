0341f5351c91f9477bc6529478dc33c52fc8acbf
James Wu <jjwu@meta.com>
2024-07-29 08:36:14 -0700

    Enable fx graph cache properly
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -27,7 +27,7 @@ disable_progress = True
 verbose_progress = False
 
 # use fx aot graph codegen cache
-fx_graph_cache = False
+fx_graph_cache = os.environ.get("TORCHINDUCTOR_FX_GRAPH_CACHE") == "1"
 
 # use remote fx aot graph codegen cache
 # False: Disables the cache
