5dca77d80c0a0492cf58447fba839948abbca424
CaoE <e.cao@intel.com>
2024-07-29 23:40:33 -0700

    add Half support for LSTM on CPU

--- aten/src/ATen/native/RNN.cpp
+++ aten/src/ATen/native/RNN.cpp
@@ -97,7 +97,7 @@ bool use_mkldnn(const Tensor& input, TensorList params, TensorList hx) {
   };
   return input.options().backend() == at::Backend::CPU &&
       is_cpu_backend(params) && is_cpu_backend(hx) &&
-      (input.scalar_type() == kFloat || input.scalar_type() == kBFloat16) &&
+      (input.scalar_type() == kFloat || input.scalar_type() == kBFloat16 || (input.scalar_type() == kHalf && !at::GradMode::is_enabled())) &&
       input.numel() != 0;
 #endif
   return false;
