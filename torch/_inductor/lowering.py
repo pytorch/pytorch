5ffe7eb0b7ad57aa9067c7a413ee41fd866a2a34
Siyu Yang <yangsiyu@meta.com>
2024-07-30 23:48:48 +0000

    Add lowering for updated _scaled_mm (fixing submodules) (#130422)
    Add the Inductor lowering for `torch._scaled_mm`, whose API was last updated in https://github.com/pytorch/pytorch/pull/128683.
    
    The lowering does:
    - for tensor-wise scaling, auto-tune between the default ATen kernel (cuBLAS) and Triton kernel configurations.
    - for row-wise scaling, auto-tune between the default ATen kernel (CUTLASS kernel added in https://github.com/pytorch/pytorch/pull/125204) and Triton kernel configurations.
    
    The Triton kernel template is based on https://github.com/htyu/FBGEMM/commit/3ad9031d0281550cd5e7a1e953e109f020f9bd8a (D56337896) by @choutim, without using SPLIT_K, and that of mm `torch/_inductor/kernel/mm.py`
    
    ## Testing:
    - Logging shows max-autotune tuning (`AUTOTUNE scaled_mm`) for both tensor-wise and row-wise scaling when called with the two scaling types.
    - Row-wise scaling allows operator fusion between preceding pointwise/reduction op and amax/cast:
        - output code Evaluating m=256, n=256, k=256, fusion_case='pointwise', scaling_mode='row'
            - P1477224245 - 2 kernels
        - output code Evaluating m=2048, n=256, k=2048, fusion_case='reduction', scaling_mode='row'
            - P1477227340 - 2 kernels
    
    - UT `python test/inductor/test_fp8.py -- TestFP8Lowering`
    
    ## Benchmarking
    
    Eager/compiled tensor-wise/row-wise scaling for various shapes:
    https://docs.google.com/spreadsheets/d/1VfWEVuyrwoWysfbS0_u2VHJ-PsdWkF1qIsiD60AzTes/edit?gid=2113587669#gid=2113587669
    - Some of the “compiled” cases are slightly slower than “eager”. It’s because max-autotune selected the ATen kernel in the compiled case, and I think the discrepancy is variance.
    
    Eager/compiled tensor-wise/row-wise scaling with pointwise/reduction preceding op for various shapes:
    https://docs.google.com/spreadsheets/d/1Nv07NrdffQIoDeMjo9E0V-E-EYrEN0WysO_bn1bc6ns/edit?gid=1715488446#gid=1715488446
    
    ## Questions for reviewers:
    - Should the type of the accumulator `ACC_TYPE` always be in float32? If not, where is this type set (output layout?)?
    
    ## Todo:
    - Make the Triton template use the improved persistent kernel version (https://github.com/pytorch/FBGEMM/pull/2735 by @htyu)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130422
    Approved by: https://github.com/ipiszy

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -2329,7 +2329,6 @@ make_fallback(aten._flash_attention_forward.default, sdpa_constraint)
 make_fallback(aten._flash_attention_backward.default, sdpa_constraint)
 make_fallback(aten._efficient_attention_forward.default, sdpa_constraint)
 make_fallback(aten._efficient_attention_backward.default, sdpa_constraint)
-make_fallback(aten._scaled_mm.default, constrain_to_fx_strides)
 
 # index_reduce requires fallback when use_scatter_fallback(...) returns True
 make_fallback(aten.index_reduce)
