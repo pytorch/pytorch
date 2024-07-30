5eb7962b70e6b1d3ca8a6bc3bd1754f4eb44798f
Taeksang Kim <voidbag@gmail.com>
2024-07-30 11:24:37 +0000

    special.betainc: support backward operation
    It has been implemented based on tensorflow_probability impl
    
    - Reference: https://github.com/tensorflow/probability/blob/
      b14ae1d79de4a52d834a3ba2dc88f7e5d849e6c7/
      tensorflow_probability/python/math/special.py#L432-L491
    
    - It doesn't support (x :Scalar, alpha: Tensor, beta: Scalar)
                         (x :Scalar, alpha, Scalar, beta: Tensor)
    
    Signed-off-by: Taeksang Kim <voidbag@gmail.com>

--- torch/csrc/autograd/FunctionsManual.h
+++ torch/csrc/autograd/FunctionsManual.h
@@ -1154,4 +1154,34 @@ mkldnn_rnn_layer_differentiable_backward(
 
 Tensor values_backward(const Tensor& grad, const Tensor& self);
 
+std::tuple<Tensor, Tensor, Tensor> special_betainc_backward_full(
+    const Tensor& grad,
+    const Tensor& a,
+    const Tensor& b,
+    const Tensor& x);
+
+std::tuple<Tensor, Tensor> special_betainc_backward_ab(
+    const Tensor& grad,
+    const Tensor& a,
+    const Tensor& b,
+    const Tensor& x);
+
+std::tuple<Tensor, Tensor> special_betainc_backward_ax(
+    const Tensor& grad,
+    const Tensor& a,
+    const Tensor& b,
+    const Tensor& x);
+
+std::tuple<Tensor, Tensor> special_betainc_backward_bx(
+    const Tensor& grad,
+    const Tensor& a,
+    const Tensor& b,
+    const Tensor& x);
+
+Tensor special_betainc_backward_x(
+    const Tensor& grad,
+    const Tensor& a,
+    const Tensor& b,
+    const Tensor& x);
+
 } // namespace torch::autograd::generated::details
