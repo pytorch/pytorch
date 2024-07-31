8b0baeacdccf465d7bf8bb6a42e08bb59dae569e
Yichen Yan <wenji.yyc@alibaba-inc.com>
2024-07-31 17:16:56 +0800

    [nccl] Wrap nccl code update with version check

--- torch/csrc/distributed/c10d/NCCLUtils.hpp
+++ torch/csrc/distributed/c10d/NCCLUtils.hpp
@@ -320,7 +320,6 @@ class NCCLComm {
     comm->initialized_ = isInitialized;
     return comm;
   }
-#endif
 
   static std::shared_ptr<NCCLComm> split(
       NCCLComm* source,
@@ -328,6 +327,7 @@ class NCCLComm {
       int rank,
       ncclConfig_t& config,
       std::vector<uint64_t>& ranks_ull);
+#endif
 
 #if defined(IS_NCCLX) && defined(NCCL_COMM_DUMP)
   std::unordered_map<std::string, std::string> ncclCommDump() {
