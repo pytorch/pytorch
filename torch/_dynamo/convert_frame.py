aed64e487d6bdd8c856e13b92dccc5ac8179dbb2
laithsakka <lsakka@meta.com>
2024-07-30 23:43:08 -0700

    introduce regression to be detected
    [ghstack-poisoned]

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -644,7 +644,7 @@ def _compile(
         nonlocal dynamo_time_before_restart
         nonlocal restart_reasons
         last_attempt_start_time = start_time = time.time()
-
+        time.sleep(10)
         def log_bytecode(
             prefix: str, name: str, filename: str, line_no: int, code: CodeType
         ) -> None:
