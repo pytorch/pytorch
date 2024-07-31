0b0d884701488c84c24772140ff76fdeb700d57f
Justin Chu <justinchuby@users.noreply.github.com>
2024-07-31 06:18:32 -0700

    Update converter.py

--- torch/_export/converter.py
+++ torch/_export/converter.py
@@ -68,7 +68,7 @@ def _trace_and_get_graph_from_model(model, args):
 
 def _create_jit_graph(
     model: Union[torch.nn.Module, torch.jit.ScriptFunction], args: Sequence[Any]
-) -> tuple[torch.Graph, list[_C.IValue], Any | None, torch.ScriptModule | None]:
+) -> tuple[torch.Graph, list[torch.jit.IValue], Any | None, torch.ScriptModule | None]:
     if isinstance(model, (torch.jit.ScriptFunction, torch.jit.ScriptModule)):
         flattened_args = tuple(torch.jit._flatten(tuple(args))[0])
         torch_out = None
