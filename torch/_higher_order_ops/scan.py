bdb0aeaeb4b97a98937745eb078945f6a9f6f588
Yidi Wu <yidi@meta.com>
2024-08-01 12:39:20 -0700

    Update on "[hop] makes compiled hops not share code objects"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_higher_order_ops/scan.py
+++
@@ -1,9 +0,0 @@
-import torch.utils._pytree as pytree
-
-
-def scan(
-    combine_fn: Callable[[pytree.PyTree, pytree.PyTree], pytree.PyTree],
-    init: pytree.PyTree,
-    xs: pytree.PyTree,
-):
-    pass
