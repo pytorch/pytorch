8607f509c3022a0bb8b27f5618f351f706ce16c7
Justin Chu <justinchu@microsoft.com>
2024-08-02 18:29:23 +0000

    Update
    [ghstack-poisoned]

--- torch/onnx/_internal/exporter/__init__.py
+++ torch/onnx/_internal/exporter/__init__.py
@@ -4,14 +4,11 @@ __all__ = [
     "analyze",
     "export",
     "exported_program_to_ir",
-    "patch_torch",
-    "unpatch_torch",
     "verify_onnx_program",
 ]
 
 from ._analysis import analyze
 from ._core import export, exported_program_to_ir
 from ._onnx_program import ONNXProgram
-from ._patch import patch_torch, unpatch_torch
 from ._registration import ONNXRegistry
 from ._verification import verify_onnx_program
