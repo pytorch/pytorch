9673fd2783b8f80ca44e3b01a211e483f2217bd8
Animesh Jain <anijain@umich.edu>
2024-08-02 12:20:12 -0700

    [dynamo] Reland 132308, 132314, 132318, 132334
    ghstack-source-id: 8a129f0a04f6557d4346ae3887c42e4c2f44a306
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/132538

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -81,7 +81,11 @@ from .misc import (
     TypingVariable,
     UnknownVariable,
 )
-from .nn_module import NNModuleVariable, UnspecializedNNModuleVariable
+from .nn_module import (
+    NNModuleVariable,
+    UnspecializedBuiltinNNModuleVariable,
+    UnspecializedNNModuleVariable,
+)
 from .optimizer import OptimizerVariable
 from .sdpa import SDPAParamsVariable
 from .tensor import (
