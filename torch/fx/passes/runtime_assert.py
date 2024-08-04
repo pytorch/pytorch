1100132fbe305a43a7890bf521afa0fd7cb830b0
Aart Bik <ajcbik@google.com>
2024-08-04 12:29:56 -0700

    lint

--- torch/fx/passes/runtime_assert.py
+++ torch/fx/passes/runtime_assert.py
@@ -23,6 +23,7 @@ from torch.fx.experimental.proxy_tensor import py_sym_types
 from torch.fx.experimental.sym_node import SymNode
 from torch.fx.graph_module import GraphModule
 
+
 __all__ = ["insert_deferred_runtime_asserts"]
 
 log = logging.getLogger(__name__)
