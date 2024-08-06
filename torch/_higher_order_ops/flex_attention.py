703caac98da89e65c90f9aae85630e6e6257e43e
Edward Z. Yang <ezyang@meta.com>
2024-08-06 07:55:22 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_higher_order_ops/flex_attention.py
+++ torch/_higher_order_ops/flex_attention.py
@@ -834,39 +834,22 @@ def flex_attention_backward_proxy_torch_dispatch_mode(
     mask_mod_other_buffers: Tuple = (),
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     assert mode is not None, "Mode should always be enabled for python fallback key"
-    if mode.enable_tracing:
-        return trace_flex_attention_backward(
-            mode,
-            query,
-            key,
-            value,
-            out,
-            logsumexp,
-            grad_out,
-            fw_graph,
-            joint_graph,
-            block_mask,
-            scale,
-            kernel_options,
-            score_mod_other_buffers,
-            mask_mod_other_buffers,
-        )
-    else:
-        return flex_attention_backward(
-            query,
-            key,
-            value,
-            out,
-            logsumexp,
-            grad_out,
-            fw_graph,
-            joint_graph,
-            block_mask,
-            scale,
-            kernel_options,
-            score_mod_other_buffers,
-            mask_mod_other_buffers,
-        )
+    return trace_flex_attention_backward(
+        mode,
+        query,
+        key,
+        value,
+        out,
+        logsumexp,
+        grad_out,
+        fw_graph,
+        joint_graph,
+        block_mask,
+        scale,
+        kernel_options,
+        score_mod_other_buffers,
+        mask_mod_other_buffers,
+    )
 
 
 @flex_attention_backward.py_functionalize_impl
