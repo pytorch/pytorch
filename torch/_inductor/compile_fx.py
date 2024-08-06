5058254f4a10ecdcf83fc8d486ecdab75f9e8fae
Michael Lazos <mlazos@meta.com>
2024-08-05 20:20:12 -0700

    Add cudagraph static inputs logging
    [ghstack-poisoned]

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -94,6 +94,9 @@ else:
 log = logging.getLogger(__name__)
 perf_hint_log = torch._logging.getArtifactLogger(__name__, "perf_hints")
 post_grad_graphs_log = torch._logging.getArtifactLogger(__name__, "post_grad_graphs")
+static_inputs_log = torch._logging.getArtifactLogger(
+    __name__, "cudagraph_static_inputs"
+)
 
 
 # copy_ fails when trying to write to tensors with memory overlap,
@@ -488,6 +491,8 @@ def compile_fx_inner(
 
     if static_input_idxs is None:
         static_input_idxs = []
+
+    static_inputs_log.debug("static input idxs compile_fx_inner: %s", static_input_idxs)
 
     assert isinstance(
         next(iter(reversed(gm.graph.nodes))).args[0], (tuple, list)
