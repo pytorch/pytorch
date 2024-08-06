6e6e0b9186a4dc8019b8bac0d78fb705d640989d
soulitzer <soulitzer@gmail.com>
2024-08-06 09:37:33 -0400

    Update base for Update on "Ensure FakeTensor post-creation has associated nested int cached"
    [ghstack-poisoned]

--- torch/nested/_internal/nested_tensor.py
+++ torch/nested/_internal/nested_tensor.py
@@ -5,7 +5,6 @@ from typing import Tuple
 import torch
 from torch._C import DispatchKey, DispatchKeySet
 from torch._prims_common import is_expandable_to
-from torch.fx.experimental.symbolic_shapes import has_free_symbols
 from torch.utils.weak import WeakTensorKeyDictionary
 
 
