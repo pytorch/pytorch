3fb55ddbc52dedbbe7fa91d242dd3645c2ea8810
Stefan-Alin Pahontu <56953855+alinpahontu2912@users.noreply.github.com>
2024-08-07 13:15:19 +0000

    attempt to fix msvc issue

--- aten/src/ATen/cpu/vec/vec_base.h
+++ aten/src/ATen/cpu/vec/vec_base.h
@@ -293,8 +293,10 @@ public:
 #if defined(_WIN32) && defined(__aarch64__)
   Vectorized<T> map(T (*const f)(T)) const {
     Vectorized<T> ret;
-    for (int64_t i = 0; i != size(); i++) {
+    for (int64_t i = 0; i < size(); i++) {
       ret[i] = f(values[i]);
+      if (++i < size())
+        ret[i] = f(values[i]);
     }
     return ret;
   }
