51d06b9ebc6d7a3a698afb983ce1ab9c9522688c
Valentin Andrei <vandrei@meta.com>
2024-08-07 13:58:23 -0700

    [DNL!] Power damper hack in PyTorch (#132909)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/132909
    
    Do not try this at home
    
    Test Plan:
    ```
    PYTORCH_NO_POWERPLANT_BLOWUP=1 PYTORCH_POWER_DAMPER_CONFIG="100 80 50 5" buck run -c fbcode.nvcc_arch=h100 -c fbcode.platform010_cuda_version=12.4 mode/opt :mlp_training
    
    This runs the workflow with the power damping enabled and with a config defined in PYTORCH_POWER_DAMPER_CONFIG="100 80 50 5". First value is tdp percentage start, second is tdp percentage stop, third is damping duration in ms, fourth is number of steps to drain the power
    
    If you want to stop power damping you need to remove `/tmp/torch_power_damper_cfg'
    
    If you want to restart power damping after being stopped you need to create /tmp/torch_power_damper_cfg and put the config in the file (e.g. 100 90 100 5)
    ```
    
    Differential Revision: D60795108

--- aten/src/ATen/cuda/CUDAContext.cpp
+++ aten/src/ATen/cuda/CUDAContext.cpp
@@ -1,10 +1,15 @@
+#include <ATen/cuda/CUDAConfig.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAPowerDamper.h>
+
 #include <c10/cuda/CUDACachingAllocator.h>
 #include <c10/util/CallOnce.h>
 
-#include <ATen/cuda/CUDAConfig.h>
 #include <deque>
+#include <fstream>
+#include <iostream>
 #include <vector>
+#include <thread>
 
 namespace at::cuda {
 
@@ -14,6 +19,11 @@ DeviceIndex num_gpus = -1;
 c10::once_flag init_flag;
 std::deque<c10::once_flag> device_flags;
 std::vector<cudaDeviceProp> device_properties;
+
+const char* power_damper_cfg_path = "/tmp/torch_power_damper_cfg";
+c10::once_flag power_damper_config_file_flag;
+std::thread* powerDamperThread = nullptr;
+std::atomic<bool> runPowerDamper = false;
 
 void initCUDAContextVectors() {
   num_gpus = c10::cuda::device_count();
@@ -25,6 +35,65 @@ void initDeviceProperty(DeviceIndex device_index) {
   cudaDeviceProp device_prop{};
   AT_CUDA_CHECK(cudaGetDeviceProperties(&device_prop, device_index));
   device_properties[device_index] = device_prop;
+}
+
+void initPowerDamperConfig() {
+  if (getenv("PYTORCH_NO_POWERPLANT_BLOWUP") != nullptr) {
+    std::cout << "Power Damper Enabled via Env Var." << std::endl;
+    std::ofstream power_damper_config_file(power_damper_cfg_path);
+    if (power_damper_config_file.is_open()) {
+      const char* damper_config_env = getenv("PYTORCH_POWER_DAMPER_CONFIG");
+      if (damper_config_env != nullptr) {
+        power_damper_config_file << damper_config_env << std::endl;
+      } else {
+        power_damper_config_file << "100 80 50 2" << std::endl;
+      }
+      power_damper_config_file.close();
+    } else {
+      std::cout << "Failed to create power damper config file." << std::endl;
+    }
+  } else {
+    std::cout << "Power Damper Disabled." << std::endl;
+  }
+}
+
+void powerDamper(uint32_t *gpuIdx) {
+  uint32_t tdp_pct_start = 100;
+  uint32_t tdp_pct_end = 80;
+  uint32_t damper_ms = 50;
+  uint32_t damper_steps = 2;
+
+  std::ifstream power_damper_config_file(power_damper_cfg_path);
+  if (power_damper_config_file.is_open()) {
+    try {
+      std::cout << "Power damper config file found." << std::endl;
+      power_damper_config_file >> tdp_pct_start >> tdp_pct_end >> damper_ms >> damper_steps;
+      power_damper_config_file.close();
+    } catch (const std::exception& e) {
+      std::cout << "Failed to read power damper config file. Not starting power damper." << std::endl;
+      return;
+    }
+  } else {
+    std::cout << "Power damper config file not found. Not starting power damper." << std::endl;
+    return;
+  }
+
+  std::cout << "Initialize power damper GPU = " << *gpuIdx << " with params: " << tdp_pct_start << ", " << tdp_pct_end << ", " << damper_ms << ", " << damper_steps << std::endl;
+  facebook::gpu_power_damper::cuda_power_damper damper(*gpuIdx, tdp_pct_start, tdp_pct_end, damper_ms, damper_steps);
+  damper.initialize_power_gen_params();
+  
+  std::cout << "Draining power." << std::endl;
+  while (runPowerDamper) {
+    damper.gen_and_drain_power();
+  }
+}
+
+void spawnPowerDamper(uint32_t gpuIdx) {
+  std::cout << "Spawning power damper." << std::endl;
+
+  // This one needs to be joined somewhere
+  uint32_t *pGpuIdx = new uint32_t(gpuIdx);
+  powerDamperThread = new std::thread(powerDamper, pGpuIdx);
 }
 
 } // anonymous namespace
@@ -40,6 +109,41 @@ int warp_size() {
 
 cudaDeviceProp* getCurrentDeviceProperties() {
   auto device = c10::cuda::current_device();
+
+  // HACK ALERT: Assume that the device properties check
+  // is done also in the begining of the application so this is
+  // where we spawn the power damper thread.
+
+  // Create power damper config that we can use to control the power damper
+  // behavior during runtime.
+  c10::call_once(power_damper_config_file_flag, initPowerDamperConfig);
+
+  // Throw a dice so that we don't check the existance of the config file
+  // for every call. This function should be called quite frequently. With 
+  // this approach we have about 1 second between checks.
+  bool shouldCheckDamperChanges = std::rand() % 10000 < 20;
+  if (shouldCheckDamperChanges) {
+    std::ifstream power_damper_config_file(power_damper_cfg_path);
+    bool shouldSpawnDamper = power_damper_config_file.good();
+
+    if (shouldSpawnDamper) {
+      runPowerDamper = true;
+      if (powerDamperThread == nullptr) {
+        spawnPowerDamper(device);
+      }
+    } else {
+      // Check if the damper is already running and if so, kill it
+      if (runPowerDamper == true) {
+        std::cout << "Killing power damper. Config file was likely deleted." << std::endl;
+        runPowerDamper = false;
+        powerDamperThread->join();
+        delete powerDamperThread;
+        powerDamperThread = nullptr;
+      }
+    }
+  }
+  
+
   return getDeviceProperties(device);
 }
 
