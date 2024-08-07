e0a2c4ad18b3c084ced105ca7a795a9671a18cd5
HDCharles <charlesdavidhernandez@gmail.com>
2024-08-06 18:05:05 -0700

    Run performance test non-alternately
    Summary:
    By default, performance tests (speedup experiments) will run the baseline and test backend alternately.
    
    However, this does not work for the torchao backend, which will change the model in-place, therefore the baseline run will also run with torchao backend since the model has already been quantized.
    
    Add a new experiment "latency_experiment" to run performance tests non-alternately (first run baseline for a few iterations, then run the test backend).
    
    other changes:
    
    need to add torch.compiler.cudagraph_mark_step_begin() to avoid the
    slowdown from             # Unable to hit fast path of CUDAGraphs because of pending, uninvoked backwards
    
    also updated the torchao APIs to the current versions
    
    X-link: https://github.com/pytorch/benchmark/pull/2394
    
    Test Plan:
    python run_benchmark.py torchao --only AlbertForMaskedLM --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune python run_benchmark.py torchao --only BartForCausalLM --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune python run_benchmark.py torchao --only timm_efficientnet --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune
    
    (should all be ~1.0
    0.997x
    1.006x
    0.994x
    
    Reviewed By: xuzhao9
    
    Differential Revision: D60252821
    
    Pulled By: HDCharles

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -4430,7 +4430,15 @@ def run(runner, args, original_dir=None):
                 fullgraph=args.nopython,
                 mode=args.inductor_compile_mode,
             )
-            runner.model_iter_fn = baseline_ctx(runner.model_iter_fn)
+            model_iter_fn = baseline_ctx(runner.model_iter_fn)
+
+            # needed to avoid error that causes inconsistent timing due to:
+            # Unable to hit fast path of CUDAGraphs because of pending, uninvoked backwards
+            def model_iter_fn_and_mark_step(*args, **kwargs):
+                torch.compiler.cudagraph_mark_step_begin()
+                model_iter_fn(*args, **kwargs)
+
+            runner.model_iter_fn = model_iter_fn_and_mark_step
             optimize_ctx = torchao_optimize_ctx(args.quantization)
         else:
             optimize_ctx = torch._dynamo.optimize(args.backend, nopython=args.nopython)
