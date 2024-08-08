0f19d4150bc59d4467160dfd9ed346e4614975a5
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-08-08 11:59:38 +0000

    Revert "[inductor]a less ambitious way to slove the scalar tensor (#132702)"
    This reverts commit b483ca05a91f2876b0f1f5a435fa264f5467762d.
    
    Reverted https://github.com/pytorch/pytorch/pull/132702 on behalf of https://github.com/ezyang due to breaks trunk jobs ([comment](https://github.com/pytorch/pytorch/pull/132702#issuecomment-2275642109))

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -10850,28 +10850,6 @@ class CommonTemplate:
         _, code = run_and_get_code(fn, x, x2)
         FileCheck().check("aten.view.dtype(reinterpret_tensor").run(code[0])
 
-    @requires_gpu()
-    def test_scalar_cpu_tensor_arg(self):
-        def fn(x, y):
-            return x + y.sum()
-
-        test_dtypes = [
-            torch.float32,
-            torch.float64,
-            torch.float16,
-            torch.bfloat16,
-        ]
-        for cpu_dtype in test_dtypes:
-            x = torch.rand([20], device="cuda")
-            y = torch.rand([4], device="cpu", dtype=cpu_dtype)
-            self.common(
-                fn,
-                (x, y),
-                check_lowp=False,
-                copy_to_gpu=False,
-                reference_in_float=False,
-            )
-
     def test_float16_to_int16(self):
         def fn(x):
             x_view = x.view(dtype=torch.int16)
