5574a4cd8c18bd432c6f8f53e79e90856f2153d0
Nicolas Macchioni <nmacchioni@fb.com>
2024-08-08 02:07:07 -0700

    Update
    [ghstack-poisoned]

--- test/inductor/test_benchmarking.py
+++ test/inductor/test_benchmarking.py
@@ -11,55 +11,56 @@ from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_CPU, HAS_GPU
 
 
 class TestBenchmarker(TestCase):
-    def setUpGeneric(self):
+    def setUp(self):
+        super().setUp()
         torch.manual_seed(12345)
         counters.clear()
 
-    def setUp(self):
-        self.setUpGeneric()
-        self.ThisBenchmarker = Benchmarker
+    @property
+    def benchmarker(self):
+        return Benchmarker()
+
+    def counter_value(self, fn_name):
+        return counters["inductor"][
+            f"benchmarking.{type(self.benchmarker).__name__}.{fn_name}"
+        ]
 
     def make_sum(self, device, size=100):
         fn, args, kwargs = torch.sum, (torch.randn(size, device=device),), {}
         _callable = lambda: fn(*args, **kwargs)  # noqa: E731
         return (fn, args, kwargs), _callable
 
-    def get_counter_value(self, name):
-        return counters["inductor"][
-            f"benchmarking.{self.ThisBenchmarker.__name__}.{name}"
-        ]
-
     @unittest.skipIf(not HAS_CPU or not HAS_GPU, "requires CPU and GPU")
     @parametrize("device", (GPU_TYPE, "cpu"))
     def test_benchmark(self, device):
-        benchmarker = self.ThisBenchmarker()
+        benchmarker = self.benchmarker
         fn, args, kwargs, _ = self.make_sum(device)
         if device == "cpu":
             _ = benchmarker.benchmark(fn, *args, **kwargs)
-            self.assertEqual(self.get_counter_value("benchmark"), 1)
-            self.assertEqual(self.get_counter_value("benchmark_cpu"), 1)
+            self.assertEqual(self.counter_value("benchmark"), 1)
+            self.assertEqual(self.counter_value("benchmark_cpu"), 1)
         else:
             self.assertExpectedRaises(
                 NotImplementedError, lambda: benchmarker.benchmark(fn, *args, **kwargs)
             )
-            self.assertEqual(self.get_counter_value("benchmark"), 1)
-            self.assertEqual(self.get_counter_value("benchmark_gpu"), 1)
+            self.assertEqual(self.counter_value("benchmark"), 1)
+            self.assertEqual(self.counter_value("benchmark_gpu"), 1)
 
     @unittest.skipIf(not HAS_CPU, "requires CPU")
     def test_benchmark_cpu(self):
-        benchmarker = self.ThisBenchmarker()
+        benchmarker = self.benchmarker
         _, _callable = self.make_sum("cpu")
         _ = benchmarker.benchmark_cpu(_callable)
-        self.assertEqual(self.get_counter_value("benchmark_cpu"), 1)
+        self.assertEqual(self.counter_value("benchmark_cpu"), 1)
 
     @unittest.skipIf(not HAS_GPU, "requires GPU")
     def test_benchmark_gpu(self):
-        benchmarker = self.ThisBenchmarker()
+        benchmarker = self.benchmarker
         _, _callable = self.make_sum(GPU_TYPE)
         self.assertExpectedRaises(
             NotImplementedError, lambda: benchmarker.benchmark_gpu(_callable)
         )
-        self.assertEqual(self.get_counter_value("benchmark_gpu"), 1)
+        self.assertEqual(self.counter_value("benchmark_gpu"), 1)
 
 
 if __name__ == "__main__":
