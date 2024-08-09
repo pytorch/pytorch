f616ddb022920f138b45a0606a6a628caec1b3ba
wz337 <wz337@cornell.edu>
2024-08-09 16:54:57 -0700

    fix

--- torch/distributed/_tensor/placement_types.py
+++ torch/distributed/_tensor/placement_types.py
@@ -399,7 +399,7 @@ class Partial(Placement):
     Args:
         reduce_op (str, optional): The reduction op to be used for the partial DTensor
         to produce Replicated/Sharded DTensor. Only element-wise reduction operations
-        are supportd, including: "sum", "avg", "prod", "max", "min", default: "sum".
+        are supportd, including: "sum", "avg", "product", "max", "min", default: "sum".
 
     ::note:: The ``Partial`` placement can be generated as a result of the DTensor operators,
         and can only be used by the ``DTensor.from_local`` API.
