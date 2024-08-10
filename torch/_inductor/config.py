33354a25ec35d77bc8c7f4ed894840eab34f6b5b
Nicolas Macchioni <nmacchioni@fb.com>
2024-08-09 17:50:19 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -1151,7 +1151,7 @@ class benchmarking:
         env_val: Optional[str] = os.environ.get(
             "TORCHINDUCTOR_BENCHMARKING_INDUCTOR_GROUPED_BENCHMARKER"
         )
-        oss_default: bool = False
+        oss_default: bool = True
         local_version: Optional[int] = get_feature_local_version(
             "inductor_grouped_benchmarker"
         )
