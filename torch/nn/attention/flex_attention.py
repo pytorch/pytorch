a495161243d7d941e7774632742928bd954ec15b
Patrick von Platen <patrick.v.platen@gmail.com>
2024-08-10 19:08:30 +0200

    Improve error messages in `flex_attention` for sequence length

--- torch/nn/attention/flex_attention.py
+++ torch/nn/attention/flex_attention.py
@@ -861,12 +861,21 @@ def flex_attention(
     # Some basic input validation
     _validate_sdpa_input(query, key, value)
     if query.dim() != 4 or key.dim() != 4 or value.dim() != 4:
-        raise NotImplementedError("NYI: query, key, and value must be 4D tensors")
+        raise NotImplementedError(
+            "query, key, and value must be 4D tensors, "
+            f"but got {query.shape}, {key.shape}, {value.shape}"
+        )
     if query.size(-2) >= 32:  # use Attention Kernel
         if query.size(-2) >= 128 and query.size(-2) % 128 != 0:
-            raise NotImplementedError("NYI: S must be <128 or a multiple of 128")
+            raise NotImplementedError(
+                "Source sequence length (S) of query must be <128 or a multiple of 128, "
+                f"but got S={query.size(-2)}"
+            )
     if key.size(-2) % 128 != 0:
-        raise NotImplementedError("NYI: L must be a multiple of 128")
+        raise NotImplementedError(
+            "Target sequence length (L) of key must be a multiple of 128, "
+            fbut got L={key.size(-2)}"
+        )
     if (not enable_gqa) and query.size(-3) != key.size(-3):
         raise ValueError(
             f"Expect query and key/value to have the same number of heads "
