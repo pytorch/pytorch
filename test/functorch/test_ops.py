f8d9997b4cd685a7cd16d3709b831ab1e3e76760
Drew Fustin <drewfustin@gmail.com>
2024-08-12 08:36:48 -0500

    added explanatory comment for xfail, removed old xfail, removed not implemented note in docs

--- test/functorch/test_ops.py
+++ test/functorch/test_ops.py
@@ -1676,8 +1676,6 @@ class TestOperators(TestCase):
                 # TODO: implement batching rule
                 xfail("_batch_norm_with_update"),
                 xfail("as_strided", "partial_views"),
-                # xfail("igamma"),  # Unexpected success?
-                # xfail("igammac"),  # Unexpected success?
             }
         ),
     )
@@ -2068,6 +2066,8 @@ class TestOperators(TestCase):
                 # TODO: implement batching rule
                 xfail("_batch_norm_with_update"),
                 xfail("native_dropout_backward"),
+                # NotImplementedError: Trying to use forward AD with
+                # aten::igamma_self_backward that does not support it.
                 xfail("igamma"),
                 xfail("igammac"),
             }
