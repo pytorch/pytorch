69c4697a2737b8ea90b5b894da39ac36bc1c49bd
Xu Han <xu.han@outlook.com>
2024-08-12 22:01:46 +0800

    fix lint issue.

--- torch/_dynamo/test_case.py
+++ torch/_dynamo/test_case.py
@@ -6,7 +6,6 @@ import logging
 import torch
 import torch.testing
 from torch.testing._internal.common_utils import (  # type: ignore[attr-defined]
-    IS_WINDOWS,
     TEST_WITH_CROSSREF,
     TEST_WITH_TORCHDYNAMO,
     TestCase as TorchTestCase,
