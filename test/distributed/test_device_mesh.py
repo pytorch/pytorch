11e1c3f1df52f07d3942c76dc6160df47aa8df3d
wz337 <wz337@cornell.edu>
2024-08-11 17:43:20 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/distributed/test_device_mesh.py
+++ test/distributed/test_device_mesh.py
@@ -543,7 +543,7 @@ class TestDeviceMeshGetItem(DTensorTestBase):
         self.assertEqual(_world.group_count, ref_pg_count)
 
     @with_comms
-    def test_get_item_3d_noncontinuous_slicing(self):
+    def test_get_item_3d_noncontiguous_slicing(self):
         mesh_shape = (2, 2, 2)
         mesh_dim_names = ("dp", "pp", "cp")
         mesh_3d = init_device_mesh(
@@ -564,18 +564,11 @@ class TestDeviceMeshGetItem(DTensorTestBase):
         # Check on the current dp_local_rank, whether the cp mesh tensor is the same.
         self.assertEqual(dp_cp_mesh.mesh[dp_local_rank], cp_mesh.mesh)
 
-        # For dp_cp_mesh, dp mesh is the innermost dimension.
-        cp_dp_mesh = mesh_3d["cp", "dp"]
-        expected_mesh_tensor = (
-            torch.tensor([[0, 4], [1, 5]], dtype=torch.int)
-            if self.rank in (0, 1, 4, 5)
-            else torch.tensor([[2, 6], [3, 7]], dtype=torch.int)
-        )
-        cp_local_rank = cp_dp_mesh.get_local_rank("cp")
-        self.assertEqual(cp_dp_mesh.mesh, expected_mesh_tensor)
-        dp_mesh = mesh_3d["dp"]
-        # Check on the current cp_local_rank, whether the dp mesh tensor is the same.
-        self.assertEqual(cp_dp_mesh.mesh[cp_local_rank], dp_mesh.mesh)
+        with self.assertRaisesRegex(
+            KeyError,
+            "Valid mesh_dim_names should be a contiguous or non-contiguous subsequence",
+        ):
+            cp_dp_mesh = mesh_3d["cp", "dp"]
 
     @with_comms
     def test_flatten_mesh(self):
