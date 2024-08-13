e1397c33d5586781725c2c812eaaecc983cac8fd
Xavier Dupré <xadupre@microsoft.com>
2024-08-13 12:47:00 +0000

    lint
    Signed-off-by: Xavier Dupré <xadupre@microsoft.com>

--- torch/onnx/symbolic_opset14.py
+++ torch/onnx/symbolic_opset14.py
@@ -148,7 +148,9 @@ def scaled_dot_product_attention(
     assert (not is_causal) or (
         is_causal and symbolic_helper._is_none(attn_mask)
     ), "is_causal and attn_mask cannot be set at the same time"
-    assert not enable_gqa, "conversion of scaled_dot_product_attention not implemented if enable_gqa is True"
+    assert (
+        not enable_gqa
+    ), "conversion of scaled_dot_product_attention not implemented if enable_gqa is True"
 
     scale = symbolic_helper._maybe_get_const(scale, "f")
     if symbolic_helper._is_none(scale):
