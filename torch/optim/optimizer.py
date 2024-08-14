bd6acb96a49a0e25c210c61fa66cbf8f321e064e
Masaki Kozuki <mkozuki@nvidia.com>
2024-08-14 12:06:07 +0900

    adagrad
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- torch/optim/optimizer.py
+++ torch/optim/optimizer.py
@@ -192,12 +192,16 @@ def _default_to_fused_or_foreach(
     return fused, foreach
 
 
-def _device_dtype_check_for_fused(p: torch.Tensor) -> None:
+def _device_dtype_check_for_fused(
+    p: torch.Tensor, cuda_unsupported: bool = False
+) -> None:
     fused_supported_devices = _get_fused_kernels_supported_devices()
+    if cuda_unsupported:
+        fused_supported_devices.remove("cuda")
     if not (p.device.type in fused_supported_devices and torch.is_floating_point(p)):
         raise RuntimeError(
             "`fused=True` requires all the params to be floating point Tensors of "
-            f"supported devices: {fused_supported_devices}."
+            f"supported devices: {fused_supported_devices} but {p.dtype} and {p.device.type}"
         )
 
 
