61c33f8f071e4796ee16b6416302c377e8e1eef9
Xu Han <xu.han@outlook.com>
2024-08-14 10:40:55 +0800

    update code

--- test/dynamo/test_backward_higher_order_ops.py
+++ test/dynamo/test_backward_higher_order_ops.py
@@ -13,7 +13,7 @@ from torch._dynamo._trace_wrapped_higher_order_op import trace_wrapped
 from torch._dynamo.testing import normalize_gm
 from torch._dynamo.utils import counters
 from torch.fx.experimental.proxy_tensor import make_fx
-from torch.testing._internal.inductor_utils import HAS_CUDA, skipIfWindowsCuda
+from torch.testing._internal.common_utils import HAS_CUDA, skipIfWindowsCuda
 
 
 def _multiply(x):
