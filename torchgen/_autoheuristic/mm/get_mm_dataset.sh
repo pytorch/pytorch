ef26434784bd1b62854fd5e1f5ed5ba7e4be24f8
Alnis Murtovi <murtovi@meta.com>
2024-08-15 13:02:53 -0700

    AutoHeuristic: mm ranking heuristic h100 (#133608)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133608
    Approved by: https://github.com/eellison
    ghstack dependencies: #131705, #131710, #131714

--- torchgen/_autoheuristic/mm/get_mm_dataset.sh
+++ torchgen/_autoheuristic/mm/get_mm_dataset.sh
@@ -2,7 +2,8 @@
 
 base_url='https://github.com/AlnisM/autoheuristic-datasets/raw/main/'
 a100_data='a100_mm.zip'
-datasets=("${a100_data}")
+h100_data='h100_mm.zip'
+datasets=("${a100_data}" "${h100_data}")
 for dataset in "${datasets[@]}"; do
     url="${base_url}${dataset}"
     wget ${url}
