[inductor] Fix needs_fixed_stride_order silent incorrectness
Fixes #128084

The approach is two-fold (based on what Elias suggested in the comment
thread):
1. We realize Tensors and ensure they are the right stride.
2. If we can't ensure they have the right stride (if the tensors are
   already in an incorrect stride with FixedLayout, or if there's a
   view), then we require the correct stride on use via a clone and then
   copy_ back the result of the mutation.

We can improve this based on use cases (if the tensor is a view, then we
can require the base of the view to have the right stride order). The
view case isn't very common so I'm ignoring it for now.

Test Plan:
- tests

[ghstack-poisoned]

test/inductor/test_torchinductor.py
@@ -38,7 +38,7 @@ from torch._dynamo.testing import (
     same,
     skipIfPy312,
 )
-from torch._dynamo.utils import ifdynstaticdefault
+from torch._dynamo.utils import counters, ifdynstaticdefault
 from torch._inductor.aoti_eager import (
     aoti_compile_with_persistent_cache,
     aoti_eager_cache_dir,
@@ -10573,6 +10573,7 @@ class CommonTemplate:
 
     @config.patch(implicit_fallbacks=True)
     def test_mutable_custom_op_fixed_layout(self):
+        counters.clear()
         with torch.library._scoped_library("mylib", "DEF") as lib:
             lib.define(
                 "copy_(Tensor(a!) ret, Tensor tensors, int dim) -> ()",
@@ -10601,6 +10602,7 @@ class CommonTemplate:
             compiled_inductor_f = torch.compile(f, backend="inductor", fullgraph=True)
             compiled_inductor_out = compiled_inductor_f(x)
             self.assertEqual(compiled_inductor_out, eager_out)
+            self.assertEqual(counters["inductor"]["require_stride_order_clones"], 0)
 
     @requires_gpu()
     @config.patch(implicit_fallbacks=True)

torch/_inductor/lowering.py
@@ -78,6 +78,7 @@ from .virtualized import ops, V
 log = logging.getLogger(__name__)
 lowerings: Dict[torch._ops.OpOverload, Callable[..., Any]] = {}
 layout_constraints: Dict[torch._ops.OpOverload, Callable[..., Any]] = {}
+requires_fx_strides: Set[torch._ops.OpOverload] = set()
 fallbacks: Set[torch._ops.OpOverload] = set()
 aten = torch.ops.aten
 tr_c10d = torch.ops.tr_c10d
@@ -2073,37 +2074,6 @@ def constrain_to_fx_strides(fx_node, *args, ignore_mutated_args_FIXME=False, **k
             stride_order = ir.get_stride_order(fx_arg.meta["val"].stride())
             return ir.ExternKernel.require_stride_order(arg, stride_order)
         return arg
-
-    # There's a silent incorrectness bug where we if we constrain a mutated arg,
-    # we may end up cloning it, writing in-place to the clone, and then using
-    # the original value (instead of the cloned value). Our short-term fix for this
-    # is to never constrain mutated args; longer term we do want to fix this.
-    # https://github.com/pytorch/pytorch/issues/128084
-    if ignore_mutated_args_FIXME:
-        assert isinstance(fx_node.target, torch._ops.OpOverload)
-        schema = fx_node.target._schema
-
-        def maybe_apply_constraint(schema_arg, arg, fx_arg):
-            if schema_arg.alias_info is not None and schema_arg.alias_info.is_write:
-                return arg
-            return apply_constraint(arg, fx_arg)
-
-        new_args = []
-        new_kwargs = {}
-
-        for idx, (arg, fx_arg) in enumerate(zip(args, fx_node.args)):
-            schema_arg = schema.arguments[idx]
-            new_args.append(maybe_apply_constraint(schema_arg, arg, fx_arg))
-
-        schema_kwargs = {arg.name: arg for arg in schema.arguments}
-
-        for key in kwargs.keys():
-            arg = kwargs[key]
-            fx_arg = fx_node.kwargs[key]
-            schema_arg = schema_kwargs[key]
-            new_kwargs[key] = maybe_apply_constraint(schema_arg, arg, fx_arg)
-
-        return tuple(new_args), new_kwargs
 
     args = tuple(
         apply_constraint(arg, fx_arg) for arg, fx_arg in zip(args, fx_node.args)