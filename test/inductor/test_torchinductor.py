[inductor] Fix needs_fixed_stride_order silent incorrectness
Fixes #128084

The approach is two-fold (based on what Elias suggested in the comment
thread):
1. We realize Tensors and ensure they are the right stride.
2. If we can't ensure they have the right stride (if the tensors are
   already in an incorrect stride with FixedLayout, or if there's a
   view), then we require the correct stride on use via a clone and then
   copy_ back the result of the mutation.

We can improve this based on use cases (if the tensor is a view, then we
can require the base of the view to have the right stride order). The
view case isn't very common so I'm ignoring it for now.

Test Plan:
- tests

[ghstack-poisoned]

test/inductor/test_torchinductor.py
@@ -38,7 +38,7 @@ from torch._dynamo.testing import (
     same,
     skipIfPy312,
 )
-from torch._dynamo.utils import ifdynstaticdefault, counters
+from torch._dynamo.utils import counters, ifdynstaticdefault
 from torch._inductor.aoti_eager import (
     aoti_compile_with_persistent_cache,
     aoti_eager_cache_dir,
@@ -10603,24 +10603,6 @@ class CommonTemplate:
             compiled_inductor_out = compiled_inductor_f(x)
             self.assertEqual(compiled_inductor_out, eager_out)
             self.assertEqual(counters["inductor"]["require_stride_order_clones"], 0)
-
-            # def f_with_views(x):
-            #     full_default_3 = torch.full([10], 7.0, device="cpu")
-            #     y = full_default_3[::2]
-            #     y = y[:3]
-            #     chunk_cat_default_1 = torch.ops.mylib.copy_.default(
-            #         y, x, 0
-            #     )
-            #     mul_out = torch.mul(y, y)
-            #     return mul_out
-
-            # x = torch.arange(3, dtype=torch.float, device="cpu")
-            # eager_out = f_with_views(x)
-
-            # compiled_inductor_f = torch.compile(f_with_views, backend="inductor", fullgraph=True)
-            # compiled_inductor_out = compiled_inductor_f(x)
-            # self.assertEqual(compiled_inductor_out, eager_out)
-            # self.assertEqual(counters["inductor"]["require_stride_order_clones"], 1)
 
     @requires_gpu()
     @config.patch(implicit_fallbacks=True)

torch/_inductor/graph.py
@@ -19,6 +19,7 @@ from typing import (
     NoReturn,
     Optional,
     Sequence,
+    Set,
     Tuple,
     TYPE_CHECKING,
     Union,
@@ -85,7 +86,6 @@ from .lowering import (
     fallback_handler,
     fallback_node_due_to_unsupported_type,
     layout_constraints,
-    requires_fx_strides,
     lowerings,
     make_fallback,
     needs_realized_inputs,
@@ -403,7 +403,7 @@ class GraphLowering(torch.fx.Interpreter):
         self.creation_time = time.time()
         self.name = name  # type: ignore[assignment]
         self.cpp_wrapper = cpp_wrapper
-        self.seen_custom_ops = set()
+        self.seen_custom_ops: Set[torch._ops.OpOverload] = set()
 
         # record multi_kernel choice for cpp_wrapper so the second pass knows
         # which sub-kernel is picked. Copy cpp_wrapper to another variable
@@ -1215,12 +1215,21 @@ class GraphLowering(torch.fx.Interpreter):
         )
         return ir.TensorBox(torch._inductor.ir.ReinterpretView(storage, new_layout))
 
-    def propagate_mutation(self, fx_node, old_args, old_kwargs, new_args, new_kwargs):
+    def propagate_mutation(
+        self,
+        fx_node: torch.fx.Node,
+        old_args: Tuple[Any],
+        old_kwargs: Dict[str, Any],
+        new_args: Tuple[Any],
+        new_kwargs: Dict[str, Any],
+    ) -> None:
         assert isinstance(fx_node.target, torch._ops.OpOverload)
         assert len(old_args) == len(new_args)
         assert len(old_kwargs) == len(new_kwargs)
 
-        def maybe_propagate(schema_arg, old_arg, new_arg):
+        def maybe_propagate(
+            schema_arg: torch._C.Argument, old_arg: ir.IRNode, new_arg: ir.IRNode
+        ) -> None:
             if old_arg is new_arg:
                 return
             if schema_arg.alias_info is not None and schema_arg.alias_info.is_write:
@@ -1239,10 +1248,12 @@ class GraphLowering(torch.fx.Interpreter):
             schema_arg = schema_kwargs[key]
             maybe_propagate(schema_arg, old_arg, new_arg)
 
-    def maybe_add_custom_op_layout_constraints(self, op):
+    def maybe_add_custom_op_layout_constraints(self, op: Callable[..., Any]) -> None:
+        if not isinstance(op, torch._ops.OpOverload):
+            return
         if op in self.seen_custom_ops:
             return
-        if isinstance(op, torch._ops.OpOverload) and torch._C.Tag.needs_fixed_stride_order in op.tags:
+        if torch._C.Tag.needs_fixed_stride_order in op.tags:
             torch._inductor.lowering.add_layout_constraint(op, constrain_to_fx_strides)
         self.seen_custom_ops.add(op)
 
@@ -1254,7 +1265,8 @@ class GraphLowering(torch.fx.Interpreter):
         operation_watermark = len(self.operations)
 
         origins = {n}
-        if n.op == "call_function":
+        is_call_function = n.op == "call_function"
+        if is_call_function:
             args, kwargs = self.fetch_args_kwargs_from_env(n)
             origins |= gather_origins(args, kwargs)
         with ir.IRNode.current_origins(origins), self.set_current_node(  # type: ignore[arg-type]
@@ -1262,11 +1274,11 @@ class GraphLowering(torch.fx.Interpreter):
         ), V.set_current_node(
             n
         ):
-            if n.op == "call_function":
-                self.maybe_add_custom_op_layout_constraints(n.target)
+            if is_call_function:
+                self.maybe_add_custom_op_layout_constraints(n.target)  # type: ignore[arg-type]
 
             if (
-                n.op == "call_function"
+                is_call_function
                 and n.target is not operator.getitem
                 and fallback_node_due_to_unsupported_type(n)
             ):
@@ -1274,16 +1286,16 @@ class GraphLowering(torch.fx.Interpreter):
                 result = fallback_handler(n.target, add_to_fallback_set=False)(
                     *args, **kwargs  # type: ignore[possibly-undefined]
                 )
-            elif n.op == "call_function" and n.target in layout_constraints:
+            elif is_call_function and n.target in layout_constraints:
                 debug("layout_constraints")
-                old_args = args
-                old_kwargs = kwargs
+                old_args = args  # type: ignore[possibly-undefined]
+                old_kwargs = kwargs  # type: ignore[possibly-undefined]
                 args, kwargs = layout_constraints[n.target](n, *args, **kwargs)  # type: ignore[index]
                 result = self.call_function(n.target, args, kwargs)  # type: ignore[arg-type]
                 # layout_constraints are allowed to make new copies of the inputs.
                 # if they do, and if the target is mutable, then we need to
                 # write the new values back into the original inputs.
-                self.propagate_mutation(n, old_args, old_kwargs, args, kwargs)
+                self.propagate_mutation(n, old_args, old_kwargs, args, kwargs)  # type: ignore[possibly-undefined]
             elif is_magic_method(n.target):
                 # TODO: this is sus, it probably should be handled in the
                 # lowerings themselves similarly to sym_size/sym-stride

torch/_inductor/lowering.py
@@ -78,7 +78,7 @@ from .virtualized import ops, V
 log = logging.getLogger(__name__)
 lowerings: Dict[torch._ops.OpOverload, Callable[..., Any]] = {}
 layout_constraints: Dict[torch._ops.OpOverload, Callable[..., Any]] = {}
-requires_fx_strides: Set[torch._ops.OpOverload] = set() 
+requires_fx_strides: Set[torch._ops.OpOverload] = set()
 fallbacks: Set[torch._ops.OpOverload] = set()
 aten = torch.ops.aten
 tr_c10d = torch.ops.tr_c10d