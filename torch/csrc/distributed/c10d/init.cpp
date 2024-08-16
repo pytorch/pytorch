03b6b556a6a097a85c3ab9c3e5cb3e9047a38b64
Yifu Wang <yifu@fb.com>
2024-08-16 02:23:22 -0700

    [SymmetricMemory] make buffer_ptrs_dev, signal_pad_ptrs_dev, buffer_size, and signal_pad_size accessible in python
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -1046,9 +1046,19 @@ This class does not support ``__members__`` property.)");
           &::c10d::symmetric_memory::get_symmetric_memory)
       .def_property_readonly("rank", &SymmetricMemory::get_rank)
       .def_property_readonly("world_size", &SymmetricMemory::get_world_size)
-      .def("get_buffer_ptrs_dev", [](c10::intrusive_ptr<SymmetricMemory> symm_mem) {
+      .def_property_readonly(
+          "buffer_ptrs_dev",
+          [](c10::intrusive_ptr<SymmetricMemory> symm_mem) {
             return reinterpret_cast<uintptr_t>(symm_mem->get_buffer_ptrs_dev());
           })
+      .def_property_readonly(
+          "signal_pad_ptrs_dev",
+          [](c10::intrusive_ptr<SymmetricMemory> symm_mem) {
+            return reinterpret_cast<uintptr_t>(symm_mem->get_buffer_ptrs_dev());
+          })
+      .def_property_readonly("buffer_size", &SymmetricMemory::get_buffer_size)
+      .def_property_readonly(
+          "signal_pad_size", &SymmetricMemory::get_signal_pad_size)
       .def(
           "get_buffer",
           &SymmetricMemory::get_buffer,
