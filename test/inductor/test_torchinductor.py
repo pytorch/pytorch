adbbd0870901374556f0b48b8364a3701796b718
rzou <zou3519@gmail.com>
2024-08-15 17:16:36 -0700

    Update on "[Inductor] short-term fix for needs_fixed_stride_order silent incorrectness"
    This is a low-risk short-term fix for
    https://github.com/pytorch/pytorch/issues/128084, for the purposes of
    2.4.1. The actual fix for that issue is more risky and we'll target 2.5.
    
    needs_fixed_stride_order is silently incorrect with args that are
    mutable because it creates clones of those args, writes into them, and
    doesn't update the original args.
    
    This PR makes it so that needs_fixed_stride_order doesn't apply to
    inputs that are being mutated.
    
    This PR doesn't completely fix the problem, but it makes it less
    incorrect: most of the time the input already has the correct strides
    but inductor fails to recognize it, and in those cases writing directly
    to the input is fine.
    
    Test Plan:
    - new test
    
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -10575,23 +10575,21 @@ class CommonTemplate:
     def test_mutable_custom_op_fixed_layout(self):
         with torch.library._scoped_library("mylib", "DEF") as lib:
             lib.define(
-                "copy_(Tensor(a!) ret, Tensor tensors, int dim) -> ()",
+                "copy_(Tensor(a!) dst, Tensor src) -> ()",
                 tags=torch.Tag.needs_fixed_stride_order,
             )
 
             @torch.library.impl(lib, "copy_", "Meta")
-            def _(ret, tensors, dim):
+            def _(dst, src):
                 return None
 
-            @torch.library.impl(lib, "copy_", "CPU")
-            def _(ret, tensors, dim):
-                ret.copy_(tensors)
+            @torch.library.impl(lib, "copy_", "CompositeExplicitAutograd")
+            def _(dst, src):
+                dst.copy_(src)
 
             def f(x):
                 full_default_3 = torch.full([3], 7.0, device="cpu")
-                chunk_cat_default_1 = torch.ops.mylib.copy_.default(
-                    full_default_3, x, 0
-                )
+                chunk_cat_default_1 = torch.ops.mylib.copy_.default(full_default_3, x)
                 mul_out = torch.mul(full_default_3, full_default_3)
                 return mul_out
 
