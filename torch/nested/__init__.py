0d79d524b4164fb280bbee195d19bc83c53e0440
krzysztofjordan <krzysztofjordan@users.noreply.github.com>
2024-08-17 13:23:01 -0700

    Update
    [ghstack-poisoned]

--- torch/nested/__init__.py
+++ torch/nested/__init__.py
@@ -430,7 +430,7 @@ def masked_select(tensor: Tensor, mask: Tensor) -> Tensor:
     >>> nt.shape
     torch.Size([3, j4])
     >>> # Length of each item in the batch:
-    >>> offsets.diff()
+    >>> nt.offsets().diff()
     tensor([1, 2, 1])
 
     >>> tensor = torch.randn(6, 5)
@@ -438,7 +438,7 @@ def masked_select(tensor: Tensor, mask: Tensor) -> Tensor:
     >>> nt = torch.nested_select(tensor, mask)
     torch.Size([3, j5])
     >>> # Length of each item in the batch:
-    >>> offsets.diff()
+    >>> nt.offsets().diff()
     tensor([0, 0, 0, 0, 0, 0])
     """
     if tensor.layout != torch.strided:
