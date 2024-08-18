911ffd8ab898e11f597f6d908218b0a9bd36b8d5
Will Feng <yf225@cornell.edu>
2024-08-18 00:49:17 -0700

    Update base for Update on "[Traceable FSDP2] Support fsdp_pre_all_gather / fsdp_post_all_gather"
    cc XilunWu H-Huang awgu kwen2501 wanchaol fegin fduwjj wz337 wconstab d4l3k c-p-i-o voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- test/distributed/_composable/fsdp/test_fully_shard_compile.py
+++ test/distributed/_composable/fsdp/test_fully_shard_compile.py
@@ -707,7 +707,6 @@ class TestFullyShardCompile(FSDPTest):
                     "Expected two separate lowerings to Triton code, one from FWD graph and one from Compiled Autograd BWD graph",
                 )
                 fwd_code = triton_codes[0]
-                print(f"fwd_code: {fwd_code}")
                 file_check = FileCheck().check("def call(args):")
                 for fwd_ag_block_info in [
                     dict(overlapped_compute_op_str="triton_", num_copy=4),
