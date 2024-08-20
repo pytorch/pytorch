c140edad379f6e92ff1f92e49003dc70badbf7e3
Wuxun Zhang <wzhang@habana.ai>
2024-08-20 18:26:41 +0300

    import hints from _higher_order_ops/__init__.py

--- torch/_higher_order_ops/__init__.py
+++ torch/_higher_order_ops/__init__.py
@@ -4,6 +4,7 @@ from torch._higher_order_ops.flex_attention import (
     flex_attention_backward,
 )
 from torch._higher_order_ops.while_loop import while_loop
+from torch._higher_order_ops.hints_wrap import hints_wrapper
 
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "while_loop",
     "flex_attention",
     "flex_attention_backward",
+    "hints_wrapper",
 ]
