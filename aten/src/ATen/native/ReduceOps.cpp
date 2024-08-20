eea26f31ce5e9327287873edea741973c8bc9b8c
Mengwei Liu <larryliu@meta.com>
2024-08-20 13:12:59 -0700

    Check input and result scalar type
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- aten/src/ATen/native/ReduceOps.cpp
+++ aten/src/ATen/native/ReduceOps.cpp
@@ -1412,12 +1412,11 @@ Tensor& mean_out(const Tensor& self, DimnameList dim,
 }
 
 Tensor& mean_dtype_out(const Tensor &self, std::optional<ScalarType> dtype, Tensor& result) {
-  if (dtype.has_value()) {
-    TORCH_CHECK(
-      canCast(dtype.value(), result.scalar_type()),
-        "mean.dtype_out(): input types can't be cast to the desired output type ",
-        dtype.value());
-  }
+  TORCH_CHECK(
+    canCast(self.scalar_type(), result.scalar_type()),
+      "mean.dtype_out(): input types can't be cast to the desired output type ",
+      result.scalar_type());
+  // at::mean_out should make sure dtype and result.scalar_type() are the same
   return at::mean_out(result, self, IntArrayRef{}, false, dtype);
 }
 
