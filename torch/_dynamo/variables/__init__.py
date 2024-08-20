71dd52f51a05d110c06e83f74cef165f64627842
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-21 00:23:11 +0800

    [RFC][dynamo] add decorator to register polyfill for unsupported C++ function to avoid graph break (#133712)
    Add decorator `torch.compiler.substitute_in_graph` to register polyfill for unsupported C++ function to avoid graph break. This API provides an official way to add support for dynamo for third-party C extensions. Also, it can be used to simplify our implementation for `torch._dynamo.polyfill`.
    
    https://github.com/pytorch/pytorch/blob/5ee070266fb39916559ae773272c57521397eb1a/torch/_dynamo/variables/builtin.py#L97-L107
    
    Example:
    
    ```python
    >>> import operator
    >>> operator.indexOf([1, 2, 3, 4, 5], 3)
    2
    
    >>> torch.compile(operator.indexOf, fullgraph=True)([1, 2, 3, 4, 5], 3)
    Unsupported: ...
    
    >>> @torch.compiler.substitute_in_graph(operator.indexOf)
    ... def indexOf(sequence, x):
    ...     for i, item in enumerate(sequence):
    ...         if item is x or item == x:
    ...             return i
    ...     raise ValueError("sequence.index(x): x not in sequence")
    
    >>> torch.compile(operator.indexOf, fullgraph=True)([1, 2, 3, 4, 5], 3)
    2
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133712
    Approved by: https://github.com/jansel

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -30,6 +30,7 @@ from .distributed import BackwardHookVariable, DistributedVariable, PlacementVar
 from .functions import (
     FunctoolsPartialVariable,
     NestedUserFunctionVariable,
+    PolyfilledFunctionVariable,
     SkipFunctionVariable,
     UserFunctionVariable,
     UserMethodVariable,
@@ -144,6 +145,7 @@ __all__ = [
     "NumpyVariable",
     "OptimizerVariable",
     "PlacementVariable",
+    "PolyfilledFunctionVariable",
     "PythonModuleVariable",
     "RangeVariable",
     "RegexPatternVariable",
