751bdc8905263e544c86d8105b3cc67534dcf4e2
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2024-08-19 17:23:29 -0700

    fix set_autocast_dtype

--- test/test_autocast.py
+++ test/test_autocast.py
@@ -343,6 +343,22 @@ class TestAutocastGPU(TestCase):
         finally:
             torch._C._set_cached_tensors_enabled(False)
 
+    def test_autocast_prioritize(self):
+        device = "cuda"
+        dtype = torch.bfloat16
+
+        with torch.autocast(device_type=device, enabled=True, dtype=dtype):
+            t = torch.randn([3, 4, 5], dtype=dtype, device=device, requires_grad=True)
+            index = torch.randint(
+                low=0, high=3, size=[3, 4, 5], dtype=torch.int64, device=device
+            )
+            val = torch.randn(1, dtype=dtype, device=device)
+
+            res = torch.index_put(t, [index], val)
+
+            loss = res.mean()
+            loss.backward()
+
 
 class TestTorchAutocast(TestCase):
     def test_autocast_fast_dtype(self):
