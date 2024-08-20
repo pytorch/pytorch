4db2bb9b64bddfa7463abfb69220d03f5fff0543
Yidi Wu <yidi@meta.com>
2024-08-20 15:52:46 -0700

    [hop] ban creating hop by directly instantiating HigherOrderOperator. (#133645)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133645
    Approved by: https://github.com/zou3519
    ghstack dependencies: #133521

--- torch/_higher_order_ops/run_const_graph.py
+++ torch/_higher_order_ops/run_const_graph.py
@@ -8,7 +8,12 @@ from torch.fx.experimental.proxy_tensor import ProxyTorchDispatchMode, track_ten
 from torch.utils import _pytree as pytree
 
 
-run_const_graph = HigherOrderOperator("run_const_graph")
+class RunConstGraph(HigherOrderOperator):
+    def __init__(self):
+        super().__init__("run_const_graph")
+
+
+run_const_graph = RunConstGraph()
 
 
 @run_const_graph.py_impl(ProxyTorchDispatchMode)
