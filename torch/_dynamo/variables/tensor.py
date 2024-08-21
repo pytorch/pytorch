cb3e5d3afc185ca0ad99191634c9669f49539e97
Spencer Gibson <spencergibson26@gmail.com>
2024-08-21 19:50:19 -0400

    Correctly handles call_hasattr in tests

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -218,7 +218,11 @@ class TensorVariable(VariableTracker):
         return props
 
     def call_hasattr(self, tx, name):
-        return variables.ConstantVariable.create(hasattr(self.proxy, name))
+        try:
+            self.var_getattr(tx, name)
+            return variables.ConstantVariable.create(True)
+        except:
+            return variables.ConstantVariable.create(False)
 
     def dynamic_getattr(self, tx: "InstructionTranslator", name):
         fake_val = self.proxy.node.meta["example_value"]
