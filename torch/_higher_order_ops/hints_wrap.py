6db2ffe8717a3553c748ab78dbd33ae78d62ba9b
Wuxun Zhang <wzhang@habana.ai>
2024-08-21 04:00:50 +0300

    fix lint

--- torch/_higher_order_ops/hints_wrap.py
+++ torch/_higher_order_ops/hints_wrap.py
@@ -3,11 +3,11 @@ import torch
 import torch.utils._pytree as pytree
 from torch._C import DispatchKey
 from torch._higher_order_ops.utils import (
+    _has_potential_branch_input_alias,
+    _has_potential_branch_input_mutation,
     autograd_not_implemented,
     reenter_make_fx,
     unique_graph_id,
-    _has_potential_branch_input_alias,
-    _has_potential_branch_input_mutation,
     UnsupportedAliasMutationException,
 )
 from torch._ops import HigherOrderOperator
@@ -98,13 +98,13 @@ def hints_wrapper_functionalize(ctx, body_fn, args, kwargs, hints):
         functional_body_fn = ctx.functionalize(body_fn)
         pre_dispatch = hasattr(ctx, "mode") and ctx.mode.pre_dispatch
         if _has_potential_branch_input_mutation(
-                functional_body_fn, unwrapped_args, pre_dispatch=pre_dispatch
+            functional_body_fn, unwrapped_args, pre_dispatch=pre_dispatch
         ):
             raise UnsupportedAliasMutationException(
                 "body_fn of hints_wrapper might be modifying the input!"
             )
         if _has_potential_branch_input_alias(
-                functional_body_fn, unwrapped_args, pre_dispatch=pre_dispatch
+            functional_body_fn, unwrapped_args, pre_dispatch=pre_dispatch
         ):
             raise UnsupportedAliasMutationException(
                 "body_fn of hints_wrapper might be aliasing the input!"
