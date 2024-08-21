f9764f2dc72935fa26f16c6a017eea7f13990eb8
Bin Bao <binbao@meta.com>
2024-08-21 13:39:17 -0700

    [AOTI] Introduce DeferredCudaGridLine for cuda cpp wrapper (#129268)
    Summary: Similar to https://github.com/pytorch/pytorch/pull/129135, use DeferredCudaGridLine to create a deferred grid computation line when generating cpp wrapper.
    
    Differential Revision: [D61800622](https://our.internmc.facebook.com/intern/diff/D61800622)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129268
    Approved by: https://github.com/angelayi

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -788,7 +788,13 @@ class WrapperCodeGen(CodeGen):
         self.writeline(f"{kernel}({', '.join(args)})")
 
     def generate_user_defined_triton_kernel(
-        self, kernel_name, raw_args, grid, configs, triton_meta, constexprs
+        self,
+        kernel_name: str,
+        raw_args: List[Any],
+        grid: List[Any],
+        configs,
+        triton_meta,
+        constexprs,
     ):
         grid_fn, code = user_defined_kernel_grid_fn_code(
             kernel_name, configs, grid, wrapper=self
@@ -1541,7 +1547,7 @@ class WrapperCodeGen(CodeGen):
 
     def generate_default_grid(
         self,
-        name: str,
+        kernel_name: str,
         grid: List[Any],
         cuda: bool = True,
         grid_callable: Optional[Callable[..., Any]] = None,
@@ -1632,6 +1638,7 @@ class WrapperCodeGen(CodeGen):
         raw_args=None,
         grid_fn: str = "grid",
         triton_meta=None,
+        autotune_configs=None,
         grid_extra_kwargs="",
     ):
         """
