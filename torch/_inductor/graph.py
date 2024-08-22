8bc6dcafcd7f30b9ad2de1cbac8ebf5aca840d77
atalman <atalman@fb.com>
2024-08-22 09:18:30 -0700

    Revert "[Inductor] short-term fix for needs_fixed_stride_order silent incorrectness (#133452) (#133888)"
    This reverts commit 346e0f605fdea5618c91b30894b94a97ddf1354b.

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -1,5 +1,4 @@
 # mypy: allow-untyped-defs
-import functools
 import itertools
 import logging
 import operator
@@ -935,13 +934,12 @@ class GraphLowering(torch.fx.Interpreter):
                 # We have to set the current args because call_function will immediately
                 # evaluate this lowering after creating the fallback, without evaluating
                 # the layout constraint
-                constrain_fn = functools.partial(
-                    constrain_to_fx_strides, ignore_mutated_args_FIXME=True
+                args, kwargs = constrain_to_fx_strides(
+                    self.current_node, *args, **kwargs
                 )
-                args, kwargs = constrain_fn(self.current_node, *args, **kwargs)
                 # Also register the layout constraint so when the fallback
                 # is used again, we can constrain the args to the same layout
-                layout_constraint = constrain_fn
+                layout_constraint = constrain_to_fx_strides
             return layout_constraint, args, kwargs
 
         if target not in lowerings:
