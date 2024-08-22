72af63f4c434fb9a6c5c5377a4cecd5645d203ee
Justin Chu <justinchuby@users.noreply.github.com>
2024-08-22 19:49:06 +0000

    lint

--- torch/onnx/_internal/io_adapter.py
+++ torch/onnx/_internal/io_adapter.py
@@ -572,7 +572,7 @@ class PrependParamsBuffersConstantAotAutogradInputStep(InputAdaptStep):
         """
         ordered_params = tuple(
             model.state_dict[name]  # type: ignore[union-attr,index]
-            for name in model.graph_signature.parameters
+            for name in model.graph_signature.parameters  # type: ignore[union-attr]
         )
         non_persistent_buffers = set(model.graph_signature.non_persistent_buffers)  # type: ignore[union-attr]
         ordered_buffers = []
@@ -583,7 +583,7 @@ class PrependParamsBuffersConstantAotAutogradInputStep(InputAdaptStep):
                 ordered_buffers.append(model.state_dict[name])  # type: ignore[union-attr,index]
         ordered_constant_tensors = tuple(
             model.constants[fqn]  # type: ignore[union-attr,index]
-            for fqn in model.graph_signature.lifted_tensor_constants
+            for fqn in model.graph_signature.lifted_tensor_constants  # type: ignore[union-attr]
         )
 
         # NOTE: calling convention is first params, then buffers, then args as user supplied them.
