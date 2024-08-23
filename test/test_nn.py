b316cb8632165b0ac3f23ddbecb32748a57783ae
Jagadish Krishnamoorthy <jagadish.krishnamoorthy@amd.com>
2024-08-22 23:11:42 -0700

    Fix for SWDEV-466009
    issue: torch.cuda.OutOfMemoryError
    test: test_cross_entropy_large_tensor_reduction_mean_cuda
    Patch ported from PT main branch
    hash: 2f0fc04fa39017761a72f46ae50571e2fd35d9da
    
    Signed-off-by: Jagadish Krishnamoorthy <jagadish.krishnamoorthy@amd.com>

--- test/test_nn.py
+++ test/test_nn.py
@@ -11880,10 +11880,10 @@ class TestNNDeviceType(NNTestCase):
                 # i.e. we don't count the ignored_idx at all.
                 check_equal(loss, (inp1, targ_positive_ignore_index), (inp2[1:], targ_positive_ignore_index[1:]))
 
-    # Ref: https://github.com/pytorch/pytorch/issue/85005
+    # Ref: https://github.com/pytorch/pytorch/issues/85005
     @onlyCUDA
     @largeTensorTest("45GB", "cpu")
-    @largeTensorTest("45GB", "cuda")
+    @largeTensorTest("70GB", "cuda")
     @parametrize_test("reduction", ("none", "mean", "sum"))
     def test_cross_entropy_large_tensor(self, device, reduction):
         logits = torch.randn(int(2 ** 16), int(2 ** 16) + 1, dtype=torch.float32, device='cuda', requires_grad=True)
