3e20a50ccb7d394ba263410df5fa417b481589d0
mori360 <yifanmao@meta.com>
2024-08-23 14:11:48 -0700

    remove strategy

--- torch/distributed/_tensor/ops/_pointwise_ops.py
+++ torch/distributed/_tensor/ops/_pointwise_ops.py
@@ -512,37 +512,6 @@ def common_pointwise_strategy(
     return pointwise_strategy
 
 
-def distribute_tensor_isinf(mesh: DeviceMesh, op_schema: OpSchema) -> OpStrategy:
-    input_strategy = op_schema.args_schema[0]
-    input_placements: List[Placement] = []
-    input_placements.append(Partial("max"))
-    output_placements: List[Placement] = []
-    output_placements.append(Replicate())
-    assert isinstance(input_strategy, OpStrategy)
-    assert isinstance(input_strategy.strategies[0].output_specs, DTensorSpec)
-    input_strategy.strategies[0].output_specs.placements = tuple(input_placements)
-    reduction_strategy = OpStrategy([])
-    for strtg in input_strategy.strategies:
-        input_spec = DTensorSpec(
-            mesh=mesh,
-            placements=tuple(output_placements),
-            tensor_meta=strtg.output_spec.tensor_meta,
-        )
-        redistribute_cost = [generate_redistribute_costs(input_strategy, input_spec)]
-        reduction_strategy.strategies.append(
-            PlacementStrategy(
-                output_specs=DTensorSpec(
-                    mesh=mesh,
-                    placements=tuple(output_placements),
-                ),
-                input_specs=(input_spec,),
-                redistribute_cost=redistribute_cost,
-            )
-        )
-
-    return reduction_strategy
-
-
 def linear_pointwise_strategy(mesh: DeviceMesh, op_schema: OpSchema) -> StrategyType:
     """
     Linear pointwise operators can propagate pending reductions.
