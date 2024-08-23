adc50d405b3789ed075f3ff5cd6dd5ddb31b7736
Animesh Jain <anijain@umich.edu>
2024-08-23 10:17:30 -0700

    Update on "[wip/rfc] Guard filter hook"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- torch/_dynamo/hooks.py
+++ torch/_dynamo/hooks.py
@@ -1,5 +1,6 @@
 import dataclasses
-from typing import Callable, List, Optional
+import functools
+from typing import Any, Callable, List, Optional, Set
 
 from torch._guards import GuardsSet
 
@@ -12,3 +13,61 @@ class Hooks:
     guard_fail_fn: Optional[Callable[[GuardFail], None]] = None
     guard_filter_fn: Optional[Callable[[GuardsSet], GuardsSet]] = None
     symbolic_guard_filter_fn: Optional[Callable[[List[str]], List[str]]] = None
+
+
+@functools.lru_cache(None)
+def dont_skip_guards() -> Set[Callable[..., Any]]:
+    # Curated set of guards that are better to be not skipped.
+    from torch._dynamo.guards import GuardBuilder
+
+    return set(
+        {
+            GuardBuilder.TORCH_FUNCTION_STATE,
+            GuardBuilder.DEFAULT_DEVICE,
+            GuardBuilder.DETERMINISTIC_ALGORITHMS,
+            GuardBuilder.GRAD_MODE,
+            # Removing SHAPE_ENV will remove all the symbolic shape guards
+            GuardBuilder.SHAPE_ENV,
+        }
+    )
+
+
+# Some examples of how to skip guards
+def keep_tensor_guards(guards: GuardsSet) -> GuardsSet:
+    from torch._dynamo.guards import GuardBuilder, guards_log
+
+    new_guards = GuardsSet()
+
+    for g in guards:
+        if g.inner_create_fn() in dont_skip_guards():
+            new_guards.add(g)
+        elif g.inner_create_fn() is GuardBuilder.TENSOR_MATCH:
+            new_guards.add(g)
+        else:
+            guards_log.debug("%s", f"Skipping guard: {g}")
+    return new_guards
+
+
+def skip_guards_on_nn_module_attributes(guards: GuardsSet) -> GuardsSet:
+    # Skip guards on nn module attributes. For deep models, a lot of time can be spent on nn module attribute checks
+    from torch._dynamo.guards import guards_log
+
+    new_guards = GuardsSet()
+
+    for g in guards:
+        if (
+            g.guard_source.is_unspecialized_nn_module()
+            or g.guards_source.is_specialized_nn_module()
+        ):
+            guards_log.debug("%s", f"Skipping guard: {g}")
+        else:
+            new_guards.add(g)
+    return new_guards
+
+
+def skip_all_symbolic_guards(guards: List[str]) -> List[str]:
+    from torch._dynamo.guards import guards_log
+
+    for g in guards:
+        guards_log.debug("%s", f"Skipping guard: {g}")
+    return []
