d62335a4adbd522199f7b480dd9e61a08297cf03
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-23 12:50:21 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -2979,6 +2979,7 @@ def _disallowed_callable_ids() -> Dict[int, str]:
 
 @FunctionIdSet
 def _builtin_function_ids() -> Dict[int, str]:
+    # See also torch/_dynamo/polyfills/loader.py, which removes items in _builtin_function_ids
     rv = {
         id(v): f"builtins.{k}"
         for k, v in builtins.__dict__.items()
@@ -3067,6 +3068,7 @@ def is_forbidden(obj) -> bool:
 
 
 def is_builtin_callable(obj) -> bool:
+    # See also torch/_dynamo/polyfills/loader.py, which removes items in _builtin_function_ids
     return id(obj) in _builtin_function_ids
 
 
