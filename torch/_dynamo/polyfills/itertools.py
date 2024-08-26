72918fc7834a9fa38f7932affc1f9fecef032ec9
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-27 03:56:10 +0800

    [dynamo][itertools] support `itertools.tee` (#133771)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133771
    Approved by: https://github.com/jansel

---
+++ torch/_dynamo/polyfills/itertools.py
@@ -0,0 +1,34 @@
+"""
+Python polyfills for itertools
+"""
+
+import itertools
+from typing import Iterable, Iterator, Tuple, TypeVar
+
+from ..decorators import substitute_in_graph
+
+
+__all__ = ["tee"]
+
+
+_T = TypeVar("_T")
+
+
+# Reference: https://docs.python.org/3/library/itertools.html#itertools.tee
+@substitute_in_graph(itertools.tee)
+def tee(iterable: Iterable[_T], n: int = 2, /) -> Tuple[Iterator[_T], ...]:
+    iterator = iter(iterable)
+    shared_link = [None, None]
+
+    def _tee(link) -> Iterator[_T]:  # type: ignore[no-untyped-def]
+        try:
+            while True:
+                if link[1] is None:
+                    link[0] = next(iterator)
+                    link[1] = [None, None]
+                value, link = link
+                yield value
+        except StopIteration:
+            return
+
+    return tuple(_tee(shared_link) for _ in range(n))
