pytorch/pytorch

commit 3541e450af86cbfb3e18b590c3ca1c6411f6ab70

Support larger page sizes with `use_mmap_weights` (#131000)
Fixes e.g., `test_large_mmaped_weights_non_abi_compatible_cuda` on machines with 64K page size

CC @malfet @tinglvv @nWEIdia

Pull Request resolved: https://github.com/pytorch/pytorch/pull/131000
Approved by: https://github.com/malfet

torch/_inductor/codecache.py
@@ -1904,10 +1904,14 @@ class AotCodeCompiler:
                     run_command_and_check(link_cmd)
 
                 if use_mmap_weights:
+                    import resource
+                    page_size_ = resource.getpagesize()
+                    page_size = max(16384, page_size_)
+
                     with open(output_so, "a+b") as f_so:
                         so_size = f_so.tell()
                         # Page align the weights
-                        f_so.write(b" " * (16384 - so_size % 16384))
+                        f_so.write(b" " * (page_size - so_size % page_size))
                         f_so.write(serialized_weights)
                         f_so.write(struct.pack("q", magic_number))
 

commit 3beb50153adfdbdf9e4d9b31d24ec27ab0fb1690

Fix lint failures
Introduced by https://github.com/pytorch/pytorch/pull/131000

torch/_inductor/codecache.py
@@ -1905,6 +1905,7 @@ class AotCodeCompiler:
 
                 if use_mmap_weights:
                     import resource
+
                     page_size_ = resource.getpagesize()
                     page_size = max(16384, page_size_)
 