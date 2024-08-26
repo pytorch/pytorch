5276130eaa54c8d5e5fe7abbdf2c2f3d06718c03
Yiming Zhou <yimingzhou@meta.com>
2024-08-26 14:06:24 -0700

    [export] Skip unsupported models in export benchmark

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -2679,6 +2679,12 @@ class BenchmarkRunner:
             output_csv(output_filename, headers, fields)
             return accuracy_status
 
+        if self.args.export and (
+            name in self.skip_models_due_to_control_flow
+            or name in self.skip_models_due_to_export_unsupported
+        ):
+            return record_status("pass_due_to_skip", dynamo_start_stats=start_stats)
+
         if name in self.skip_accuracy_checks_large_models_dashboard:
             return record_status("pass_due_to_skip", dynamo_start_stats=start_stats)
 
