pytorch/pytorch

commit 6618f6ee11feff02ea0a85704244866d10d42798

Fix global flake8 issues (#124771)
Prior to this `lintrunner --all-files --take FLAKE8` failed.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/124771
Approved by: https://github.com/Skylion007
ghstack dependencies: #124428

torch/signal/windows/windows.py
@@ -748,7 +748,7 @@ Computes the minimum 4-term Blackman-Harris window according to Nuttall.
 .. math::
     w_n = 1 - 0.36358 \cos{(z_n)} + 0.48917 \cos{(2z_n)} - 0.13659 \cos{(3z_n)} + 0.01064 \cos{(4z_n)}
 
-where ``z_n = 2 π n/ M``.
+where ``z_n = 2 \u03c0 n/ M``.
     """,
     """
 
@@ -766,12 +766,12 @@ Keyword args:
 
 References::
 
-    - A. Nuttall, “Some windows with very good sidelobe behavior,”
+    - A. Nuttall, "Some windows with very good sidelobe behavior,"
       IEEE Transactions on Acoustics, Speech, and Signal Processing, vol. 29, no. 1, pp. 84-91,
       Feb 1981. https://doi.org/10.1109/TASSP.1981.1163506
 
-    - Heinzel G. et al., “Spectrum and spectral density estimation by the Discrete Fourier transform (DFT),
-      including a comprehensive list of window functions and some new flat-top windows”,
+    - Heinzel G. et al., "Spectrum and spectral density estimation by the Discrete Fourier transform (DFT),
+      including a comprehensive list of window functions and some new flat-top windows",
       February 15, 2002 https://holometer.fnal.gov/GH_FFT.pdf
 
 Examples::

.github/scripts/cherry_pick.py
@@ -29,7 +29,7 @@ def parse_args() -> Any:
         "--onto-branch", type=str, required=True, help="the target release branch"
     )
     parser.add_argument(
-        "--github-actor", type=str, required=True, help="all the world’s a stage"
+        "--github-actor", type=str, required=True, help="all the world's a stage"
     )
     parser.add_argument(
         "--classification",

benchmarks/transformer/better_transformer_vs_mha_functional.py
@@ -152,8 +152,8 @@ def run(
     result_entry["sequence_length"] = sequence_length
     result_entry["n_heads"] = num_heads
     result_entry["embed_dim"] = embed_dim
-    result_entry["time_native_mha_slow(μs)"] = f"{time_native_mha_slow:.3f}"
-    result_entry["time_native_mha_fast (μs)"] = f"{time_native_mha_fast:.3f}"
+    result_entry["time_native_mha_slow(\u00B5s)"] = f"{time_native_mha_slow:.3f}"
+    result_entry["time_native_mha_fast (\u00B5s)"] = f"{time_native_mha_fast:.3f}"
     result_entry["speedup flash_mha v native_mha"] = f"{speedup_fast_internal:.3f}"
     result_entry["padding"] = f"{padding:.3f}"
     return result_entry

benchmarks/transformer/sdp.py
@@ -81,10 +81,10 @@ class ExperimentResults:
     @classmethod
     def get_entry_names(cls) -> List[str]:
         return [
-            "nn_mha_time (μs)",
-            "compiled_nn_mha_time (μs)",
-            "composite_mha_time (μs)",
-            "compiled_composite_mha_time (μs)",
+            "nn_mha_time (\u00B5s)",
+            "compiled_nn_mha_time (\u00B5s)",
+            "composite_mha_time (\u00B5s)",
+            "compiled_composite_mha_time (\u00B5s)",
         ]
 
 

functorch/einops/_parsing.py
@@ -28,7 +28,7 @@ import keyword
 import warnings
 from typing import Collection, List, Mapping, Optional, Set, Tuple, Union
 
-_ellipsis: str = "…"  # NB, this is a single unicode symbol. String is used as it is not a list, but can be iterated
+_ellipsis: str = "\u2026"  # NB, this is a single unicode symbol. String is used as it is not a list, but can be iterated
 
 
 class AnonymousAxis:

test/distributions/test_distributions.py
@@ -3752,11 +3752,11 @@ class TestDistributions(DistributionsTestCase):
 
     @unittest.skipIf(not TEST_NUMPY, "NumPy not found")
     def test_dirichlet_log_prob_zero(self):
-        # Specifically test the special case where x=0 and α=1.  The PDF is
-        # proportional to x**(α-1), which in this case works out to 0**0=1.
+        # Specifically test the special case where x=0 and alpha=1.  The PDF is
+        # proportional to x**(alpha-1), which in this case works out to 0**0=1.
         # The log PDF of this term should therefore be 0.  However, it's easy
         # to accidentally introduce NaNs by calculating log(x) without regard
-        # for the value of α-1.
+        # for the value of alpha-1.
         alpha = torch.tensor([1, 2])
         dist = Dirichlet(alpha)
         x = torch.tensor([0, 1])

test/functorch/test_parsing.py
@@ -107,7 +107,7 @@ class TestParsedExpression(TestCase):
             ParsedExpression("(a) ((b c) (d ...))")
 
         # invalid identifiers
-        ParsedExpression("camelCase under_scored cApiTaLs ß ...")
+        ParsedExpression("camelCase under_scored cApiTaLs \u00DF ...")
         with self.assertRaises(ValueError):
             ParsedExpression("1a")
         with self.assertRaises(ValueError):

test/inductor/test_templated_attention.py
@@ -190,8 +190,8 @@ class TestTemplatedSDPA(InductorTestCase):
         # this means that the base for the LSE computed by ref is e while for the compiled
         # version it is 2. To compare we use the change of base formula
         # log_2(x_compiled) = log_e(x_ref) * log_2(e) where
-        # x_ref      = ∑_i e^(scores[i])
-        # x_compiled = ∑_i 2^(log2(e) * scores[i])
+        # x_ref      = sum(_i e^(scores[i]))
+        # x_compiled = sum(_i 2^(log2(e) * scores[i]))
 
         self.assertTrue(ref_lse.dtype == torch.float32)
         self.assertTrue(compiled_lse.dtype == torch.float32)

commit 1f8f6432ee558503b23da6cb3cff8000fe657fe2

Fix docstring for torch.signal.windows.nuttall
This partially fixes regression introduced by https://github.com/pytorch/pytorch/pull/124771 but also just improves `z_n` rendering, by using MathML

torch/signal/windows/windows.py
@@ -750,7 +750,7 @@ Computes the minimum 4-term Blackman-Harris window according to Nuttall.
 .. math::
     w_n = 1 - 0.36358 \cos{(z_n)} + 0.48917 \cos{(2z_n)} - 0.13659 \cos{(3z_n)} + 0.01064 \cos{(4z_n)}
 
-where ``z_n = 2 \u03c0 n/ M``.
+where :math:`z_n = 2 \pi \frac{n}{M}`.
     """,
     """
 