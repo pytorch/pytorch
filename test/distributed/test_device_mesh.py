d183e15750d0e2f3df8d846c9b7db92966a4220d
wz337 <wz337@cornell.edu>
2024-08-27 11:26:48 -0700

    Update
    [ghstack-poisoned]

--- test/distributed/test_device_mesh.py
+++ test/distributed/test_device_mesh.py
@@ -136,6 +136,10 @@ class DeviceMeshTest(DTensorTestBase):
         self.assertEqual(dp_mesh.get_local_rank(), mesh_2d.get_local_rank("dp"))
         self.assertEqual(tp_mesh.get_local_rank(), mesh_2d.get_local_rank("tp"))
 
+        # Verify flattened mesh local rank correctness.
+        flattened_mesh = mesh_2d["dp", "tp"]._flatten()
+        self.assertEqual(flattened_mesh.get_local_rank(), self.rank)
+        
     @with_comms
     def test_device_mesh_2d(self):
         mesh_tensor = torch.arange(4).reshape(2, 2)
