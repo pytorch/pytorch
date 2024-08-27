103097da27e8c2b38d2763007274f784b71d6a5f
Simon Fan <xmfan@meta.com>
2024-08-26 17:08:37 -0700

    [compiled autograd] error instead of hang on reentrant autograd
    [ghstack-poisoned]

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -2460,6 +2460,20 @@ TORCH_LIBRARY(test_cudagraphs_cpu_scalar_used_in_cpp_custom_op, m) {
 
         self.assertEqual(sum(1 for e in unexpected_logs if e in logs.getvalue()), 0)
 
+    def test_reentrant_checkpointing(self):
+        def fn(x):
+            y = x.sin()
+            z = y.cos()
+            return (y * z).sum()
+
+        inp = torch.rand(10, 10, requires_grad=True)
+        out = torch.utils.checkpoint.checkpoint(fn, inp, use_reentrant=True)
+        with self.assertRaisesRegex(
+            RuntimeError,
+            "(e.g. reentrant checkpointing), this is not supported yet.",
+        ), torch._dynamo.compiled_autograd.enable(torch.compile):
+            out.backward()
+
 
 def load_test_module(name):
     testdir = Path(__file__).absolute().parent.parent
