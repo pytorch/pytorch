f9872888e0fbf685cddb54bd612f10834c5fc885
mori360 <yifanmao@meta.com>
2024-08-28 16:09:35 -0700

    rebase

--- test/distributed/_composable/fsdp/test_fully_shard_grad_scaler.py
+++ test/distributed/_composable/fsdp/test_fully_shard_grad_scaler.py
@@ -5,8 +5,14 @@ import torch
 import torch.nn as nn
 from torch.amp.grad_scaler import GradScaler, OptState
 from torch.distributed._composable.fsdp import fully_shard
+from torch.distributed._tensor import init_device_mesh
+from torch.distributed.tensor.parallel import (
+    ColwiseParallel,
+    parallelize_module,
+    RowwiseParallel,
+)
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
-from torch.testing._internal.common_fsdp import FSDPTest
+from torch.testing._internal.common_fsdp import FSDPTest, MLP
 from torch.testing._internal.common_utils import run_tests
 
 
@@ -14,11 +20,11 @@ class TestFullyShardGradientScaler(FSDPTest):
     @skip_if_lt_x_gpu(2)
     def test_gradient_scaler(self):
         self.run_subtests(
-            {"has_inf": [True, False]},
+            {"has_inf": [True, False], "test_2d": [True, False]},
             self._test_gradient_scaler,
         )
 
-    def _test_gradient_scaler(self, has_inf: bool):
+    def _test_gradient_scaler(self, has_inf: bool, test_2d: bool):
         torch.manual_seed(0)
         model = nn.Sequential(
             *[nn.Linear(4, 4, device="cuda", bias=False) for _ in range(2)]
@@ -26,15 +32,40 @@ class TestFullyShardGradientScaler(FSDPTest):
         for layer in model:
             fully_shard(layer)
         fully_shard(model)
-        scaler = GradScaler(init_scale=2.0, enabled=True)
         input = torch.randn([4, 4], device="cuda")
+
+        if test_2d:
+            mesh_2d = init_device_mesh(
+                "cuda", (2, self.world_size // 2), mesh_dim_names=("dp", "tp")
+            )
+            dp_mesh, tp_mesh = mesh_2d["dp"], mesh_2d["tp"]
+            model = nn.Sequential(MLP(2), MLP(2), MLP(2))
+            tp_parallelize_plan = {
+                "0.in_proj": ColwiseParallel(),
+                "0.out_proj": RowwiseParallel(),
+                "1.in_proj": ColwiseParallel(),
+                "1.out_proj": RowwiseParallel(),
+                "2.in_proj": ColwiseParallel(),
+                "2.out_proj": RowwiseParallel(),
+            }
+            model = parallelize_module(
+                model,
+                device_mesh=tp_mesh,
+                parallelize_plan=tp_parallelize_plan,
+            )
+            for module in model:
+                fully_shard(module, mesh=dp_mesh)
+            fully_shard(model, mesh=dp_mesh)
+            input = torch.randn((2,), device="cuda")
+
         loss = model(input).sum()
+        scaler = GradScaler(init_scale=2.0, enabled=True)
         opt = torch.optim.Adam(model.parameters(), lr=1e-2)
         scaler.scale(loss).backward()
         inv_scale = scaler._scale.double().reciprocal().float()
         if (
             has_inf is True
-            and opt.param_groups[0]["params"][0].grad._local_tensor.device.index == 0
+            and opt.param_groups[0]["params"][0].grad._local_tensor.device.index == 1
         ):
             opt.param_groups[0]["params"][0].grad._local_tensor[0, 0].fill_(
                 float("inf")
--- torch/distributed/_tensor/_dispatch.py
+++ torch/distributed/_tensor/_dispatch.py
@@ -24,7 +24,13 @@ from torch.distributed._tensor._tp_conv import (
     convolution_handler,
 )
 from torch.distributed._tensor._utils import try_find_mesh_from_args
-from torch.distributed._tensor.placement_types import DTensorSpec, Partial, Replicate, TensorMeta
+from torch.distributed._tensor.placement_types import (
+    DTensorSpec,
+    Partial,
+    Placement,
+    Replicate,
+    TensorMeta,
+)
 from torch.distributed._tensor.random import is_rng_supported_mesh
 
 
@@ -66,6 +72,46 @@ def is_same_size_handler(
     return lhs.shape == rhs.shape
 
 
+def found_inf_reduce_handler(
+    op_call: torch._ops.OpOverload,
+    args: Tuple[object, ...],
+    kwargs: Dict[str, object],
+) -> None:
+    op_info = dtensor.DTensor._op_dispatcher.unwrap_to_op_info(op_call, args, kwargs)
+    local_tensor_args = pytree.tree_unflatten(
+        cast(List[object], op_info.local_args), op_info.args_tree_spec
+    )
+    local_tensor_args = cast(Tuple[object, ...], local_tensor_args)
+    local_results = op_call(*local_tensor_args, **op_info.local_kwargs)
+
+    grad_dtensor = cast(list[dtensor.DTensor], args[0])[0]
+    grad_placements = grad_dtensor.placements
+    mesh = grad_dtensor.device_mesh
+
+    found_inf_placements: list[Placement] = []
+    for placement in grad_placements:
+        if isinstance(placement, Replicate):
+            found_inf_placements.append(placement)
+        else:
+            found_inf_placements.append(Partial("max"))
+
+    target_tensor = cast(torch.Tensor, args[1])
+    spec = DTensorSpec(
+        mesh=mesh,
+        placements=tuple(found_inf_placements),
+        tensor_meta=TensorMeta(
+            shape=target_tensor.size(),
+            stride=target_tensor.stride(),
+            dtype=target_tensor.dtype,
+        ),
+    )
+    found_inf_dtensor = dtensor.DTensor(
+        local_tensor=target_tensor, spec=spec, requires_grad=False
+    )
+    found_inf = found_inf_dtensor.full_tensor()
+    target_tensor.copy_(found_inf)
+
+
 class OpDispatcher:
     """
     Op dispatching class instance to handle args/kwargs pre-processing (un-wrapping), sharding
@@ -99,6 +145,7 @@ class OpDispatcher:
             aten.is_same_size.default: is_same_size_handler,
             aten.convolution.default: convolution_handler,
             aten.convolution_backward.default: convolution_backward_handler,
+            aten._amp_foreach_non_finite_check_and_unscale_.default: found_inf_reduce_handler,
         }
 
         # This flag is used internally to control whether we treat the torch.Tensor(non-DTensor)
@@ -231,12 +278,6 @@ class OpDispatcher:
             if output_sharding.output_spec is not None:
                 return args[0]
             else:
-                if op_call == aten._amp_foreach_non_finite_check_and_unscale_.default:
-                    found_inf_dtensor = dtensor.DTensor.from_local(args[1], mesh, [Partial("max")])
-                    found_inf = found_inf_dtensor.max().full_tensor()
-                    arg_list = list(args)
-                    arg_list[1].copy_(found_inf)
-                    args = tuple(arg_list)
                 return None
         elif _is_out_variant_op(op_call):
             # out variant could possibly have multiple out args (i.e. lu_unpack.out)
