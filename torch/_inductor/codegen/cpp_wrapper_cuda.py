650a84a1bbbf000477152b1f4af96425fe9974d5
Rachel Guo <guorachel@meta.com>
2024-08-29 18:38:45 +0000

    [AOTI] Fix cosmetic indentation issue in cuda cpp wrapper codegen for DeferredCudaKernelLine/GridLine (#134705)
    Summary:
    Follow up fix for D61018114, D61800622
    
    Increase indentation for `loadKernel` `launchKernel` and `Grid` lines.
    
    Test Plan:
    ```
    TORCH_LOGS="+graph, inductor, +schedule, output_code" buck2 run -c fbcode.enable_gpu_sections=true -c fbcode.nvcc_arch=h100 @//mode/opt fbcode//caffe2/test/inductor:test_aot_inductor -- -r test_zero_grid_with_unbacked_symbols_abi_compatible_cuda
    ```
    ```
    TORCH_LOGS="+graph, inductor, +schedule, output_code" buck2 run -c fbcode.enable_gpu_sections=true -c fbcode.nvcc_arch=h100 @//mode/opt fbcode//caffe2/test/inductor:test_aot_inductor -- -r test_zero_grid_with_backed_symbols_abi_compatible_cuda
    ```
    
    Differential Revision: D61927248
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134705
    Approved by: https://github.com/ColinPeppler

--- torch/_inductor/codegen/cpp_wrapper_cuda.py
+++ torch/_inductor/codegen/cpp_wrapper_cuda.py
@@ -145,7 +145,7 @@ class DeferredCudaGridLine(DeferredLineBase):
         grid_args_str = ", ".join(
             [cexpr(V.graph.sizevars.simplify(item)) for item in grid]
         )
-        return f"Grid {self.grid_var} = Grid({grid_args_str});"
+        return f"    Grid {self.grid_var} = Grid({grid_args_str});"
 
     def _new_line(self, line):
         return DeferredCudaGridLine(
@@ -251,9 +251,13 @@ class CppWrapperCuda(CppWrapperCpu):
         self.writeline(
             DeferredCudaKernelLine(
                 kernel_name,
-                kernel_var_name + """ = loadKernel("%s", "%s", %s, this->cubin_dir_);"""
+                """    """
+                + kernel_var_name
+                + """ = loadKernel("%s", "%s", %s, this->cubin_dir_);"""
                 if V.graph.aot_mode
-                else kernel_var_name + """ = loadKernel("%s", "%s", %s);""",
+                else """    """
+                + kernel_var_name
+                + """ = loadKernel("%s", "%s", %s);""",
                 keys,
             )
         )
@@ -404,7 +408,7 @@ class CppWrapperCuda(CppWrapperCpu):
         self.writeline(
             DeferredCudaKernelLine(
                 kernel_name,
-                r"launchKernel({}, {}, {}, {}, %s, %s, {}, {});".format(
+                r"    launchKernel({}, {}, {}, {}, %s, %s, {}, {});".format(
                     kernel_var_name,
                     f"{grid_var}.grid_x",
                     f"{grid_var}.grid_y",
