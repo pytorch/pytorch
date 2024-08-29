4f5a5683f587cf4233a96734f5d39c94ab3e5642
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-08-29 02:02:10 +0000

    Revert "[dynamo][exceptions] Use exception subclass whenever possible (#134610)"
    This reverts commit 880e3d18a406777dbea6aeaf14443b0e3a8b441c.
    
    Reverted https://github.com/pytorch/pytorch/pull/134610 on behalf of https://github.com/ZainRizvi due to Sorry, I had to revert this in order to revert another PR ([comment](https://github.com/pytorch/pytorch/pull/134610#issuecomment-2316568553))

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -1379,10 +1379,9 @@ class InstructionTranslatorBase(
 
             # 2) when user raises exception instance
             if isinstance(val, variables.ExceptionVariable):
-                if observed_exception_type := exc.observed_exception_map.get(
-                    val.exc_type
-                ):
-                    raise observed_exception_type(f"raised exception {val}")
+                if val.exc_type is StopIteration:
+                    # StopIteration is used to find the end of iteration while tracing __next__
+                    raise exc.ObservedUserStopIteration(f"raised exception {val}")
                 raise exc.ObservedException(f"raised exception {val}")
             unimplemented(f"raise {exc}")
         else:
