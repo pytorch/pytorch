c41e494016ef6b86de5d581290ebfbda393d2321
David Berard <dberard@fb.com>
2024-08-29 14:10:39 -0700

    [inductor] don't allow triton config pre_hook (#134633)
    The caching autotuner caches triton configs, and it doesn't try to hash or save the pre_hook from the config if it exists. If we had a config that had a pre_hook, then we might autotune -> save the config (without the pre_config) -> later load the saved config and try to run it, but this time without the pre_hook.
    
    So this PR adds an assert and deletes the pre_hook handling. We can be confident that we didn't have functional pre_hooks, because the pre_hook handling tries to use `self.arg_name`, which doesn't exist.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134633
    Approved by: https://github.com/shunting314, https://github.com/jansel

--- test/inductor/test_triton_heuristics.py
+++ test/inductor/test_triton_heuristics.py
@@ -10,14 +10,20 @@ from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_GPU
 
 try:
     import triton  # noqa: F401
+    import triton.language as tl
 except ImportError:
     if __name__ == "__main__":
         sys.exit(0)
     raise unittest.SkipTest("requires triton")  # noqa: B904
 
 from torch._inductor import config
-from torch._inductor.runtime.hints import TRITON_MAX_BLOCK
-from torch._inductor.runtime.triton_heuristics import triton_config
+from torch._inductor.runtime.hints import (
+    DeviceProperties,
+    HeuristicType,
+    TRITON_MAX_BLOCK,
+)
+from torch._inductor.runtime.triton_helpers import math as tl_math
+from torch._inductor.runtime.triton_heuristics import CachingAutotuner, triton_config
 from torch._inductor.test_case import run_tests, TestCase
 
 
@@ -80,6 +86,59 @@ class TestTritonHeuristics(TestCase):
     @config.patch("cpp_wrapper", True)
     def test_artificial_grid_cpp_wrapper(self):
         self._test_artificial_zgrid()
+
+    def _get_cos_kernel_caching_autotuner_args(self):
+        from triton.compiler.compiler import AttrsDescriptor
+
+        @triton.jit
+        def triton_(in_ptr0, out_ptr0, xnumel, XBLOCK: tl.constexpr):
+            xnumel = 16
+            xoffset = tl.program_id(0) * XBLOCK
+            xindex = xoffset + tl.arange(0, XBLOCK)[:]
+            xmask = xindex < xnumel
+            x0 = xindex
+            tmp0 = tl.load(in_ptr0 + (x0), xmask)
+            tmp1 = tl_math.cos(tmp0)
+            tl.store(out_ptr0 + (x0), tmp1, xmask)
+
+        triton_meta = {
+            "signature": {0: "*fp32", 1: "*fp32", 2: "i32"},
+            "device": DeviceProperties.create(torch.device("cuda")),
+            "constants": {},
+            "configs": [AttrsDescriptor(divisible_by_16=(0, 1, 2), equal_to_1=())],
+        }
+
+        configs = [
+            triton_config([16], 64),
+            triton_config([256], 64),
+        ]
+
+        inductor_meta = {}
+
+        return {
+            "fn": triton_,
+            "triton_meta": triton_meta,
+            "configs": configs,
+            "save_cache_hook": False,
+            "mutated_arg_names": [],
+            "heuristic_type": HeuristicType.POINTWISE,
+            "inductor_meta": inductor_meta,
+        }
+
+    @skipIfXpu
+    def test_pre_hook_assert(self):
+        # assert if any of the configs passed to the CachingAutotuner have pre-hooks
+        args = self._get_cos_kernel_caching_autotuner_args()
+
+        def pre_hook(kwargs):
+            if "in_ptr0" in kwargs:
+                kwargs["in_ptr0"].zero_()
+
+        for cfg in args["configs"]:
+            cfg.pre_hook = pre_hook
+
+        with self.assertRaisesRegex(AssertionError, "pre_hook"):
+            autotuner = CachingAutotuner(**args)
 
 
 if __name__ == "__main__":
