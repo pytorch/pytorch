fc4ade313ee522bc8c9c4ea9fe47c1a3fca410ab
fduwjj <fduwjj@gmail.com>
2024-08-29 13:34:09 -0700

    Update on "[FR] Make pg_name unique, show P2P collective status and fix bugs when running the script as command"
    cc XilunWu H-Huang awgu kwen2501 wanchaol fegin wz337 wconstab d4l3k c-p-i-o
    
    [ghstack-poisoned]

--- tools/flight_recorder/components/utils.py
+++ tools/flight_recorder/components/utils.py
@@ -185,7 +185,6 @@ def find_coalesced_group(
 ) -> List[Tuple[int, Dict[str, Any]]]:
     """Given a list of entries, if the collective_seq_id of the first entry matches that of subsequent ones,
     build an return a list of entries terminating in a 'coalesced' op entry all sharing a collective_seq_id
-    TODO: handle p2p_seq_id v/s collective_seq_id separately here.
     """
     found = []
     collective_seq_id = None
@@ -193,9 +192,13 @@ def find_coalesced_group(
         if _pg_guids[(e["process_group"][0], rank)] != pg_name:
             continue
         elif collective_seq_id is None:
-            collective_seq_id = e["collective_seq_id"]
+            collective_seq_id = (
+                e["p2p_seq_id"] if e["is_p2p"] else e["collective_seq_id"]
+            )
             found.append((i, e))
-        elif e["collective_seq_id"] == collective_seq_id:
+        elif not e["is_p2p"] and e["collective_seq_id"] == collective_seq_id:
+            found.append((i, e))
+        elif e["is_p2p"] and e["p2p_seq_id"] == collective_seq_id:
             found.append((i, e))
         else:
             break
