04f92aafa5651bec1dd9a5148423474ca9cca0d4
Xinya Zhang <Xinya.Zhang@amd.com>
2024-08-29 16:31:57 +0000

    Fix lint

--- test/nn/test_multihead_attention.py
+++ test/nn/test_multihead_attention.py
@@ -17,9 +17,9 @@ from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
     parametrize as parametrize_test,
     run_tests,
+    skipIfRocm,
     TEST_NUMPY,
     TEST_WITH_CROSSREF,
-    skipIfRocm,
 )
 
 
@@ -746,7 +746,7 @@ class TestMultiheadAttentionNN(NNTestCase):
 
 
 class TestMultiheadAttentionNNDeviceType(NNTestCase):
-    @skipIfRocm(msg='To investigate: yields NaN')
+    @skipIfRocm(msg="To investigate: yields NaN")
     def test_multihead_self_attn_two_masks_fast_path(self, device):
         """
         Multihead self-attention should give the same result on the fast path (BetterTransformer) as on the slow path
