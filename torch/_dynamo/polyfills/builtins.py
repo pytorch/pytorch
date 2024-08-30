48c6eea8386a1ea9f4f6b78599f63f8246548d9a
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-31 04:39:48 +0800

    [dynamo] refactor `builtins.enumerate` to use polyfill (#133894)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133894
    Approved by: https://github.com/jansel
    ghstack dependencies: #133864

--- torch/_dynamo/polyfills/builtins.py
+++ torch/_dynamo/polyfills/builtins.py
@@ -2,8 +2,10 @@
 Python polyfills for builtins
 """
 
+from __future__ import annotations
+
 import builtins
-from typing import Iterable
+from typing import Iterable, TypeVar
 
 from ..decorators import substitute_in_graph
 
@@ -11,7 +13,11 @@ from ..decorators import substitute_in_graph
 __all__ = [
     "all",
     "any",
+    "enumerate",
 ]
+
+
+_T = TypeVar("_T")
 
 
 @substitute_in_graph(builtins.all, can_constant_fold_through=True)
@@ -28,3 +34,15 @@ def any(iterable: Iterable[object], /) -> bool:
         if elem:
             return True
     return False
+
+
+@substitute_in_graph(builtins.enumerate, is_embedded_type=True)  # type: ignore[arg-type]
+def enumerate(iterable: Iterable[_T], start: int = 0) -> Iterable[tuple[int, _T]]:
+    if not isinstance(start, int):
+        raise TypeError(
+            f"{type(start).__name__!r} object cannot be interpreted as an integer"
+        )
+
+    for x in iterable:
+        yield start, x
+        start += 1
