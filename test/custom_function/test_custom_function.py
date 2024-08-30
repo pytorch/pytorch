aec701f53cfa44ca4a5db5fbb94c6c40377a975e
Jan Wieczorek <jwieczorek@habana.ai>
2024-08-30 11:12:19 +0300

    Update tests affected by matmul folding returning _unsafe_view
    - Removed return types from forward/backward functions in test_custom_function
      to be compatible with python 3.8
    - Updated graph in test_proxy_tensor test_reflect_r_over_x
      os that _unsafe_view is added instead of view_4 after mm

--- test/custom_function/test_custom_function.py
+++ test/custom_function/test_custom_function.py
@@ -1,6 +1,8 @@
-import torch
+# Owner(s): ["module: unknown"]
 
-from torch.testing._internal.common_utils import TestCase, run_tests
+import torch
+from torch.testing._internal.common_utils import run_tests, TestCase
+
 
 class TestCustomFunction(TestCase):
     def test_autograd_function_with_matmul_folding_at_output(self):
@@ -15,21 +17,22 @@ class TestCustomFunction(TestCase):
         """
 
         class CustomFunction(torch.autograd.Function):
-
             @staticmethod
-            def forward(ctx, inp1, inp2) -> torch.Tensor:
+            def forward(ctx, inp1, inp2):
                 ctx.save_for_backward(inp2)
                 ctx.output_shape = inp1.size()
                 return torch.matmul(inp1, inp2)
 
             @staticmethod
-            def backward(ctx, grad_output) -> tuple[torch.Tensor, None]:
+            def backward(ctx, grad_output):
                 output_shape = ctx.output_shape
-                inp2, = ctx.saved_tensors
-                return torch.mm(grad_output.squeeze(), inp2.t()).view(output_shape), None
+                (inp2,) = ctx.saved_tensors
+                return (
+                    torch.mm(grad_output.squeeze(), inp2.t()).view(output_shape),
+                    None,
+                )
 
-
-        def outer_function(inp1, inp2) -> torch.Tensor:
+        def outer_function(inp1, inp2):
             res = CustomFunction.apply(inp1, inp2)
             res.add_(1.0)
             return res.sum()
@@ -38,7 +41,6 @@ class TestCustomFunction(TestCase):
             res = torch.matmul(inp1, inp2)
             res.add_(1.0)
             return res.sum()
-
 
         inp1_custom = torch.randn(4, 1, 2, requires_grad=True)
         inp1_usual = inp1_custom.detach().clone().requires_grad_(True)
