117fcdcdf821c97429b27d017e67cf1d79a59b35
Roy Hvaara <roy@lightyear.no>
2024-08-30 02:25:06 +0200

    Add conv fallback for max size > 2**16

--- aten/src/ATen/mps/MPSDevice.h
+++ aten/src/ATen/mps/MPSDevice.h
@@ -79,6 +79,7 @@ class TORCH_API MPSDevice {
 
 TORCH_API bool is_available();
 TORCH_API bool is_macos_13_or_newer(MacOSVersion version);
+TORCH_API bool is_mps_fallback_enabled();
 TORCH_API at::Allocator* GetMPSAllocator(bool useSharedAllocator = false);
 
 } // namespace at::mps
