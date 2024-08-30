1c6173f8a3375d1997df2836b6bddd0bdb1c2553
Tianyi Wang <1109797240@qq.com>
2024-08-30 14:40:48 +1000

    Fix FC breaking issue

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -5025,7 +5025,10 @@ def grid_sample(
         )
         align_corners = False
 
-    return torch.grid_sampler(input, grid, mode_enum, padding_mode_enum, align_corners, value)
+    if value is None:
+        return torch.grid_sampler(input, grid, mode_enum, padding_mode_enum, align_corners)
+    else:
+        return torch.grid_sampler(input, grid, mode_enum, padding_mode_enum, align_corners, value)
 
 
 def affine_grid(
