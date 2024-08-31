91b718b32d3d6692c1c65e8edaf80cc0fb0afde5
Richeek Das <richeekdas2001@gmail.com>
2024-08-31 00:52:53 -0400

    [dynamo] do not raise an unimplemented error with boolean masking setitem

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -874,15 +874,20 @@ class TensorVariable(VariableTracker):
             else:
                 return False
 
-        if (
-            has_bool_key(key)
-            and isinstance(value, TensorVariable)
-            and value.requires_grad
-            and torch.is_grad_enabled()
-        ):
-            unimplemented(
-                "boolean masking setitem backwards, see https://github.com/pytorch/pytorch/issues/114123"
-            )
+        # cudagraph breaks on boolean masking setitem, but there's no need
+        # to raise an error here. The user can still use the boolean mask.
+        # Further discussion here:
+        # - https://github.com/pytorch/pytorch/issues/114123
+        # - https://github.com/pytorch/pytorch/issues/134241
+        # if (
+        #     has_bool_key(key)
+        #     and isinstance(value, TensorVariable)
+        #     and value.requires_grad
+        #     and torch.is_grad_enabled()
+        # ):
+        #     unimplemented(
+        #         "boolean masking setitem backwards, see https://github.com/pytorch/pytorch/issues/114123"
+        #     )
         from ..symbolic_convert import InstructionTranslator
 
         tx = InstructionTranslator.current_tx()
