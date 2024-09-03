385a9af6c04f2be5352d85c53ae494562ad442f9
Yu, Guangye <guangye.yu@intel.com>
2024-09-03 10:37:16 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -48,6 +48,9 @@ C10_DECLARE_REGISTRY(FreeCudaMemoryCallbacksRegistry, FreeMemoryCallback);
 
 namespace c10::cuda::CUDACachingAllocator {
 
+// Preserved only for BC reasons
+using c10::CachingDeviceAllocator::DeviceStats;
+
 extern const size_t kLargeBuffer;
 
 typedef std::shared_ptr<GatheredContext> (*CreateContextFn)();
