3842b3e03ce301465ffe34bd3e4ca825c8d3acb1
Animesh Jain <anijain@umich.edu>
2024-09-04 11:31:30 -0700

    [dynamo][super] Corner case where the class is not present in the __mro__ (#135129)
    I could not come up with a testcase. This was seen in https://github.com/pytorch/pytorch/issues/93633
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135129
    Approved by: https://github.com/yanboliang
    ghstack dependencies: #135039, #135121

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -105,7 +105,13 @@ class SuperVariable(VariableTracker):
         resolved_class = None
         resolved_attr = None
         search_mro = type_to_use.__mro__
-        start_index = search_mro.index(search_type) + 1
+
+        try:
+            start_index = search_mro.index(search_type) + 1
+        except ValueError:
+            # Corner case where the typevar is not in the mro of the objvar
+            # https://github.com/python/cpython/blob/3.11/Objects/typeobject.c#L8843-L8844
+            return getattr(super(search_type, type_to_use), name), None
         # Implemented based on https://github.com/python/cpython/blob/3.11/Objects/typeobject.c#L8812
         # super has its getattro implementation. The key point is that instead of calling getattr, it checks the
         # attribute in the class __dict__
