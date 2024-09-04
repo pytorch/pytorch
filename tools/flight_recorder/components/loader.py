1c93f2978337ae12ae9a0e73271c5b790992b208
Will Constable <whc@meta.com>
2024-09-04 16:15:44 -0700

    Update
    [ghstack-poisoned]

--- tools/flight_recorder/components/loader.py
+++ tools/flight_recorder/components/loader.py
@@ -7,15 +7,15 @@
 import gc
 import os
 import pickle
+import re
 import time
-from typing import Any, Dict, List, Union
+from collections import defaultdict
+from typing import Any, Dict, List, Optional, Union
 
 
 def read_dump(prefix: str, filename: str) -> Dict[str, Union[str, int, List[Any]]]:
     basename = os.path.basename(filename)
-    assert (
-        basename.find(prefix) == 0
-    ), f"args.prefix ({prefix}) must match the beginning of each filename ({basename})"
+
     rank = int(basename[len(prefix) :])
     host_name = f"host_rank{rank}"
 
@@ -35,12 +35,40 @@ def read_dump(prefix: str, filename: str) -> Dict[str, Union[str, int, List[Any]
     }
 
 
-def read_dir(prefix: str, folder: str) -> Dict[str, Dict[str, Any]]:
+exp = re.compile(r"([\w\-\_]*?)(\d+)$")
+
+
+def determine_prefix(files):
+    """If the user doesn't specify a prefix, but does pass a dir full of similarly-prefixed files, we should be able to
+    infer the common prefix most of the time.  But if we can't confidently infer, just fall back to requring the user
+    to specify it
+    """
+    possible_prefixes = defaultdict(set)
+    for f in files:
+        m = exp.search(f)
+        if m:
+            p, r = m.groups()
+            possible_prefixes[p].add(int(r))
+    if len(possible_prefixes) == 1:
+        prefix = next(iter(possible_prefixes))
+        print(f"Inferred common prefix {prefix}")
+        return prefix
+    else:
+        raise ValueError(
+            "Unable to automatically determine the common prefix for the trace file names.  Please specify --prefix argument manually"
+        )
+
+
+def read_dir(prefix: Optional[str], folder: str) -> Dict[str, Dict[str, Any]]:
     gc.disable()
     details = {}
     t0 = time.time()
     for root, _, files in os.walk(folder):
+        if prefix is None:
+            prefix = determine_prefix(files)
         for f in files:
+            if f.find(prefix) != 0:
+                continue
             details[f] = read_dump(prefix, os.path.join(root, f))
     tb = time.time()
     print(f"loaded {len(files)} files in {tb - t0}s")
