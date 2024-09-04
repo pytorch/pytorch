b9f9086ee3a486e76e70d9f01731d42ed5aabda1
Scott Wolchok <swolchok@fb.com>
2024-09-04 14:16:33 -0700

    [PyTorch] Add isfinite to BFloat16-math.h (#135052)
    Missing function from <cmath>.
    
    Differential Revision: [D62148884](https://our.internmc.facebook.com/intern/diff/D62148884/)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135052
    Approved by: https://github.com/PaliC, https://github.com/albanD
    ghstack dependencies: #135031

--- c10/util/BFloat16-math.h
+++ c10/util/BFloat16-math.h
@@ -71,6 +71,12 @@ inline T expm1(T a) {
 template <
     typename T,
     typename std::enable_if_t<is_reduced_floating_point_v<T>, int> = 0>
+inline bool isfinite(T a) {
+  return std::isfinite(float(a));
+}
+template <
+    typename T,
+    typename std::enable_if_t<is_reduced_floating_point_v<T>, int> = 0>
 inline T log(T a) {
   return std::log(float(a));
 }
