45c83e142ddb8b8e96192b35997303c692aadc3b
Bin Bao <binbao@meta.com>
2024-09-04 14:24:12 -0700

    [AOTI] Fix a unbacked symint retrieve bug (#134670)
    Summary: Fix https://github.com/pytorch/pytorch/issues/134081. When a unbacked symint is computed as the shape of a tensor from a tuple, generated C++ code needs to use std::get<> to extract the tensor.
    
    Differential Revision: [D62142113](https://our.internmc.facebook.com/intern/diff/D62142113)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134670
    Approved by: https://github.com/angelayi, https://github.com/22quinn, https://github.com/chenyang78

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -5864,7 +5864,11 @@ class FallbackKernel(ExternKernelAlloc):
                 elif isinstance(keypath[0], CallMethodKey):
                     return go(f"{expr}.{keypath[0].name}()", keypath[1:])
                 elif isinstance(keypath[0], pytree.SequenceKey):
-                    return go(f"{expr}[{keypath[0].idx}]", keypath[1:])
+                    return (
+                        go(f"std::get<{keypath[0].idx}>({expr})", keypath[1:])
+                        if V.graph.cpp_wrapper
+                        else go(f"{expr}[{keypath[0].idx}]", keypath[1:])
+                    )
                 elif isinstance(keypath[0], DivideByKey):
                     # TODO: need to assert divisibility
                     # TODO: this is invalid C++ codegen
