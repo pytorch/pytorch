5ec04c50cf2f4690aa2bae0cc79f94ab1211f60f
Yidi Wu <yidi@meta.com>
2024-09-04 16:21:39 -0700

    [hop] preserve metadata in re-tracing hop subgraph by running with interpreter (#135159)
    In this way, the interpreter.run can preserve the current metadata of subgraphs correctly when tracing the subgraphs.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135159
    Approved by: https://github.com/tugsbayasgalan

--- torch/_higher_order_ops/map.py
+++ torch/_higher_order_ops/map.py
@@ -7,6 +7,7 @@ from torch._functorch.aot_autograd import AOTConfig, create_joint
 from torch._higher_order_ops.utils import (
     _has_potential_branch_input_alias,
     _has_potential_branch_input_mutation,
+    _maybe_run_with_interpreter,
     reenter_make_fx,
     UnsupportedAliasMutationException,
 )
@@ -243,7 +244,7 @@ def map_fake_tensor_mode(mode, f, xs, args):
 def map_functionalize(ctx, f, xs, pos_args):
     unwrapped_xs = ctx.unwrap_tensors(xs)
     unwrapped_args = ctx.unwrap_tensors(pos_args)
-    wrapped_fn = ctx.functionalize(f)
+    wrapped_fn = ctx.functionalize(_maybe_run_with_interpreter(f))
 
     with ctx.redispatch_to_next():
         with disable_proxy_modes_tracing():
