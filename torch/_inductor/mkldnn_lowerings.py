44c7658eac8ec2b123e3ab6648c47ab1f3524250
Wu, Chunyuan <chunyuan.wu@intel.com>
2024-09-05 23:45:10 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/mkldnn_lowerings.py
+++ torch/_inductor/mkldnn_lowerings.py
@@ -29,6 +29,12 @@ from .utils import use_aten_gemm_kernels, use_cpp_packed_gemm_template, use_max_
 from .virtualized import ops, V
 
 
+def require_contiguous_input(x):
+    req_stride_order = list(reversed(range(len(x.get_size()))))
+    x = ir.ExternKernel.require_stride_order(x, req_stride_order)
+    return x
+
+
 def register_onednn_fusion_ops():
     if torch._C._has_mkldnn:
         from . import mkldnn_ir
@@ -182,6 +188,9 @@ def register_onednn_fusion_ops():
             if use_max_autotune():
                 transposed_w = permute(w, [1, 0])
                 *_, layout, x, transposed_w = mm_args(x, transposed_w, layout=layout)
+
+                x = require_contiguous_input(x)
+
                 if use_cpp_packed_gemm_template(layout, x, transposed_w):
 
                     def epilogue_creator(buf):
@@ -247,6 +256,9 @@ def register_onednn_fusion_ops():
                 *_, layout, x, transposed_w, y = mm_args(
                     x, transposed_w, y, layout=layout
                 )
+
+                x = require_contiguous_input(x)
+
                 if use_cpp_packed_gemm_template(layout, x, transposed_w):
 
                     def epilogue_creator(buf):
@@ -526,6 +538,9 @@ def register_onednn_fusion_ops():
                 *_, layout, x, packed_weight = mm_args(
                     x, packed_weight, layout=layout, out_dtype=output_dtype
                 )
+
+                x = require_contiguous_input(x)
+
                 if (
                     isinstance(
                         ir.InputsKernel.unwrap_storage_for_input(x_zp),
@@ -805,6 +820,9 @@ def register_onednn_fusion_ops():
                 *_, layout, x, packed_weight, x2 = mm_args(
                     x, packed_weight, x2, layout=layout, out_dtype=output_dtype
                 )
+
+                x = require_contiguous_input(x)
+
                 if (
                     isinstance(
                         ir.InputsKernel.unwrap_storage_for_input(x_zp),
@@ -1047,9 +1065,8 @@ def register_onednn_fusion_ops():
                     *_, layout, x, transposed_w = mm_args(
                         x, transposed_w, layout=layout
                     )
-                    # TODO: add the check for all if use_max_autotune()
-                    req_stride_order = list(reversed(range(len(x.get_size()))))
-                    x = ir.ExternKernel.require_stride_order(x, req_stride_order)
+
+                    x = require_contiguous_input(x)
 
                     if use_cpp_packed_gemm_template(layout, x, transposed_w):
                         CppPackedGemmTemplate.add_choices(
