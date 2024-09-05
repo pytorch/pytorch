82c006b4731534081b18d440993290bec4cad53a
wz337 <wz337@cornell.edu>
2024-09-05 12:42:13 -0700

    Update
    [ghstack-poisoned]

--- test/distributed/_tensor/test_view_ops.py
+++ test/distributed/_tensor/test_view_ops.py
@@ -549,18 +549,26 @@ class TestViewOps(DTensorTestBase):
                 view = dtensor.view(dim0_size, 2, 4)
                 self.assertEqual(len(comm_mode.get_comm_counts()), 0)
                 # when no communication happens, the data pointer should be the same.
-                self.assertEqual(view.to_local().data_ptr(), dtensor.to_local().data_ptr())
+                self.assertEqual(
+                    view.to_local().data_ptr(), dtensor.to_local().data_ptr()
+                )
 
                 view = dtensor.view(dim0_size, 4, 2)
-                self.assertEqual(view.to_local().data_ptr(), dtensor.to_local().data_ptr())
+                self.assertEqual(
+                    view.to_local().data_ptr(), dtensor.to_local().data_ptr()
+                )
                 self.assertEqual(len(comm_mode.get_comm_counts()), 0)
 
                 view = dtensor.view(dim0_size, 8)
-                self.assertEqual(view.to_local().data_ptr(), dtensor.to_local().data_ptr())
+                self.assertEqual(
+                    view.to_local().data_ptr(), dtensor.to_local().data_ptr()
+                )
                 self.assertEqual(len(comm_mode.get_comm_counts()), 0)
 
                 view = dtensor.view(dtensor.shape)
-                self.assertEqual(view.to_local().data_ptr(), dtensor.to_local().data_ptr())
+                self.assertEqual(
+                    view.to_local().data_ptr(), dtensor.to_local().data_ptr()
+                )
                 self.assertEqual(len(comm_mode.get_comm_counts()), 0)
 
 
