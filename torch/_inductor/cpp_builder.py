69cd11b4dfa56da80c43d508f7188e83e710cb13
Xu Han <xu.han@outlook.com>
2024-09-05 20:18:12 +0800

    add compiler check.

--- torch/_inductor/cpp_builder.py
+++ torch/_inductor/cpp_builder.py
@@ -24,7 +24,7 @@ from torch._dynamo.utils import dynamo_timed
 from torch._inductor import config, exc
 from torch._inductor.cpu_vec_isa import invalid_vec_isa, VecISA
 from torch._inductor.runtime.runtime_utils import cache_dir
-from torch.torch_version import Version
+from torch.torch_version import TorchVersion
 
 
 if config.is_fbcode():
@@ -201,6 +201,13 @@ def _is_msvc_cl(cpp_compiler: str) -> bool:
 
 @functools.lru_cache(None)
 def _is_intel_compiler(cpp_compiler: str) -> bool:
+    def check_minimal_version(compiler_version: TorchVersion) -> None:
+        min_version = "2024.2.1" if _IS_WINDOWS else "0.0.0"
+        if compiler_version < TorchVersion(min_version):
+            raise RuntimeError(
+                f"Intel Compiler error: less than minimal version {min_version}."
+            )
+
     try:
         output_msg = (
             subprocess.check_output(
@@ -220,7 +227,8 @@ def _is_intel_compiler(cpp_compiler: str) -> bool:
             # version check
             icx_ver_search = re.search(r"(\d+[.]\d+[.]\d+[.]\d+)", output_msg)
             if icx_ver_search is not None:
-                icx_ver = Version(icx_ver_search.group(1))
+                icx_ver = icx_ver_search.group(1)
+                check_minimal_version(TorchVersion(icx_ver))
                 # print(f"{icx_ver.major}.{icx_ver.minor}.{icx_ver.micro}")
         return is_intel_compiler
     except FileNotFoundError as exc:
