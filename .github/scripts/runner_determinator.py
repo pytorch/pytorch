7524119ed08c096dc03223e00a09aef8e6546b2b
Zain Rizvi <ZainR@meta.com>
2024-09-05 10:34:46 -0500

    Update
    [ghstack-poisoned]

--- .github/scripts/runner_determinator.py
+++ .github/scripts/runner_determinator.py
@@ -351,7 +351,7 @@ def get_runner_prefix(
             for requestor in workflow_requestors
             if is_user_opted_in(requestor, user_optins, experiment_name)
         ]
-        log.info(user_optins)
+
         if opted_in_users:
             log.info(
                 f"{', '.join(opted_in_users)} have opted into experiment {experiment_name}."
