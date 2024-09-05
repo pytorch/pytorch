bc00fa125c2a608060a9e6be3f5a67ac23ac78df
rzou <zou3519@gmail.com>
2024-09-05 10:41:29 -0700

    Add Inductor config for default stride behavior (#135238)
    By default, Inductor is allowed to manipulate the layout
    (strides+storage offset) of input tensors to custom operators.
    
    We want to change it so that the default is that Inductor should respect
    the stride order of input tensors to custom operators.
    
    This PR adds a config to toggle the behavior, in the next PR up we'll
    change the default. We also make the following changes:
    - We add a new operator Tag (flexible_layout), which means that
    inductor is allowed to manipulate the layout. When we flip the default,
    users can specify they want the old behavior by using this tag.
    
    This is a reland of https://github.com/pytorch/pytorch/pull/126986,
    which was previously reverted due to silent incorrectness. We've since
    fixed the silent incorrectness
    (https://github.com/pytorch/pytorch/pull/133639)
    
    Test Plan:
    - new test
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135238
    Approved by: https://github.com/albanD

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -101,11 +101,30 @@ def maybe_layout_constraints(fn: Callable[..., Any]) -> Optional[Callable[..., A
         _maybe_layout_constraints[fn] = None
         return None
     # We lazily register tag-based layout constraints.
-    if torch._C.Tag.needs_fixed_stride_order in fn.tags:
-        _maybe_layout_constraints[fn] = constrain_to_fx_strides
-        return _maybe_layout_constraints[fn]
-    _maybe_layout_constraints[fn] = None
-    return None
+
+    def handle_layout_constraint_tag(tag):
+        if tag is torch._C.Tag.needs_fixed_stride_order:
+            _maybe_layout_constraints[fn] = constrain_to_fx_strides
+            return _maybe_layout_constraints[fn]
+        elif tag is torch._C.Tag.flexible_layout:
+            _maybe_layout_constraints[fn] = None
+            return None
+        else:
+            raise AssertionError(f"Unknown layout constraint tag: {tag}")
+
+    tag = get_layout_constraint_tag(fn)
+    return handle_layout_constraint_tag(tag)
+
+
+def get_layout_constraint_tag(fn):
+    tags_by_priority = [
+        torch._C.Tag.needs_fixed_stride_order,
+        torch._C.Tag.flexible_layout,
+    ]
+    for tag in tags_by_priority:
+        if tag in fn.tags:
+            return tag
+    return getattr(torch._C.Tag, config.custom_op_default_layout_constraint)
 
 
 def assert_nyi(cond, msg):
