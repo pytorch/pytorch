e4a5e2c5b81dcaaad4b73585388a64b04c14f431
Laith Sakka <lsakka@meta.com>
2024-09-06 04:52:30 +0000

    [RDP] Fix "No module named 'libfb’" (#135244)
    Summary:
    D62215095 Introduced an import error to arvr pipelines as the is_fbcode() function does not work as intended.
    
    This changes is_fbcode() to be a much stricter check.
    
    Test Plan:
    ```
    buck2 run arvr/mode/platform010/opt-stripped //arvr/libraries/depthlink/clients/mr_replay:pipeline_runner -c bolt.use_eva3_sim=True -- --config_file arvr/libraries/depthlink/clients/mr_replay/configs/runner_config.yaml --features DEPTH
    ```
    
    Differential Revision: D62237502
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135244
    Approved by: https://github.com/aorenste

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -573,17 +573,18 @@ compile_threads = decide_compile_threads()
 
 # gemm autotuning global cache dir
 if is_fbcode():
-    from libfb.py import parutil
-
     try:
+        from libfb.py import parutil
+
         if __package__:
             global_cache_dir = parutil.get_dir_path(
                 os.path.join(__package__.replace(".", os.sep), "fb/cache")
             )
         else:
             global_cache_dir = parutil.get_dir_path("fb/cache")
-    except ValueError:
+    except (ValueError, ModuleNotFoundError):
         global_cache_dir = None
+
 else:
     global_cache_dir = None
 
