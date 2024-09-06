7b98a37907cf4c9fb8252fd5ef16b0263918b259
Tristan Rice <rice@fn.lc>
2024-09-06 19:54:23 +0000

    [TCPStore] use wait counters (#135283)
    This replaces the existing TCPStore counters with the new shared wait counters. There's no users of the tcpstore counters so should be completely safe to remove.
    
    Test plan:
    
    Existing tests + build
    
    There's no OSS backend for wait counters so can't write any tests with them currently.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135283
    Approved by: https://github.com/c-p-i-o

--- torch/csrc/distributed/c10d/TCPStore.hpp
+++ torch/csrc/distributed/c10d/TCPStore.hpp
@@ -18,30 +18,6 @@ struct SocketAddress {
   std::uint16_t port{};
 };
 
-class Counter {
- public:
-  void update(double val);
-  std::unordered_map<std::string, double> observe() const;
-
-  double mean() const noexcept {
-    return mean_;
-  }
-  int64_t count() const noexcept {
-    return count_;
-  }
-  double variance() const noexcept {
-    return m2_ / static_cast<double>(count_);
-  }
-  double sample_variance() const noexcept {
-    return m2_ / static_cast<double>(count_ - 1);
-  }
-
- private:
-  int64_t count_ = 0;
-  double mean_ = 0;
-  double m2_ = 0;
-};
-
 } // namespace detail
 
 struct TCPStoreOptions {
@@ -130,9 +106,6 @@ class TORCH_API TCPStore : public Store {
     return addr_.port;
   }
 
-  std::unordered_map<std::string, std::unordered_map<std::string, double>>
-  collectClientCounters() const noexcept;
-
   bool isLibUvBackend() const noexcept {
     return usingLibUv_;
   }
@@ -162,7 +135,6 @@ class TORCH_API TCPStore : public Store {
   const std::string initKey_ = "init/";
   const std::string keyPrefix_ = "/";
   std::mutex activeOpLock_;
-  std::unordered_map<std::string, detail::Counter> clientCounters_;
   bool usingLibUv_ = true;
 };
 
