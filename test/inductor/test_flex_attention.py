ba02ad1d1c2fe876392d50e69aa607a373541a8a
Yanbo Liang <ybliang8@gmail.com>
2024-09-06 17:44:19 -0700

    Update
    [ghstack-poisoned]

--- test/inductor/test_flex_attention.py
+++ test/inductor/test_flex_attention.py
@@ -6,7 +6,7 @@ import string
 from collections import namedtuple
 from contextlib import nullcontext
 from typing import Callable, Optional
-from unittest import expectedFailure, skip, skipIf, skipUnless
+from unittest import expectedFailure, skip, skipUnless
 from unittest.mock import patch
 
 import torch
@@ -39,6 +39,15 @@ supported_platform = skipUnless(
     and has_triton()
     and torch.cuda.get_device_capability() >= (8, 0),
     "Requires CUDA and Triton",
+)
+
+# Use this decorator only when hitting Triton bugs on H100
+running_on_a100_only = skipUnless(
+    torch.cuda.is_available()
+    and torch.version.hip is None
+    and has_triton()
+    and torch.cuda.get_device_capability() == (8, 0),
+    "Requires A100 and Triton",
 )
 
 Tolerances = namedtuple("Tolerances", ["atol", "rtol"])
@@ -526,8 +535,7 @@ class TestFlexAttention(InductorTestCase):
     def test_builtin_score_mods(self, dtype: torch.dtype, score_mod: Callable):
         self.run_test(score_mod, dtype)
 
-    @supported_platform
-    @skipIf(torch.cuda.get_device_capability() >= (9, 0), "H100 Triton bug")
+    @running_on_a100_only
     @common_utils.parametrize("dtype", test_dtypes_fast)
     @common_utils.parametrize("score_mod", test_score_mods)
     def test_builtin_score_mods_seqlen_lt_default_sparse_block_size(
@@ -541,8 +549,7 @@ class TestFlexAttention(InductorTestCase):
         )
         self.run_test_with_call(attention, dtype, B, H, 64, D, B, H, 64, D)
 
-    @supported_platform
-    @skipIf(torch.cuda.get_device_capability() >= (9, 0), "H100 Triton bug")
+    @running_on_a100_only
     @common_utils.parametrize("dtype", test_dtypes_fast)
     @common_utils.parametrize("score_mod", test_score_mods)
     def test_builtin_score_mods_seqlen_lt_custom_sparse_block_size(
