61a570b55e6e0c7608e48d533f5ae466fbf5ccfd
Yanbo Liang <ybliang8@gmail.com>
2024-09-06 17:44:20 -0700

    [FlexAttention] Skip very small block size unit tests on H100 due to Triton bug (#135393)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135393
    Approved by: https://github.com/BoyuanFeng

--- test/inductor/test_flex_attention.py
+++ test/inductor/test_flex_attention.py
@@ -41,6 +41,15 @@ supported_platform = skipUnless(
     "Requires CUDA and Triton",
 )
 
+# Use this decorator only when hitting Triton bugs on H100
+running_on_a100_only = skipUnless(
+    torch.cuda.is_available()
+    and torch.version.hip is None
+    and has_triton()
+    and torch.cuda.get_device_capability() == (8, 0),
+    "Requires A100 and Triton",
+)
+
 Tolerances = namedtuple("Tolerances", ["atol", "rtol"])
 torch.set_float32_matmul_precision("high")
 
@@ -526,7 +535,7 @@ class TestFlexAttention(InductorTestCase):
     def test_builtin_score_mods(self, dtype: torch.dtype, score_mod: Callable):
         self.run_test(score_mod, dtype)
 
-    @supported_platform
+    @running_on_a100_only
     @common_utils.parametrize("dtype", test_dtypes_fast)
     @common_utils.parametrize("score_mod", test_score_mods)
     def test_builtin_score_mods_seqlen_lt_default_sparse_block_size(
@@ -540,7 +549,7 @@ class TestFlexAttention(InductorTestCase):
         )
         self.run_test_with_call(attention, dtype, B, H, 64, D, B, H, 64, D)
 
-    @supported_platform
+    @running_on_a100_only
     @common_utils.parametrize("dtype", test_dtypes_fast)
     @common_utils.parametrize("score_mod", test_score_mods)
     def test_builtin_score_mods_seqlen_lt_custom_sparse_block_size(
