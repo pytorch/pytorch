178618cfe60c6cfffbf3975e0b40ba00a9769cf5
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-09-08 17:08:40 +0000

    [reland][dtensor] move DTensor to public namespace (#134203)
    reland of https://github.com/pytorch/pytorch/pull/133113
    
    I have to create a new PR because the previous reverted PR could not either be rebased, or imported successfully :(
    
    ----
    
    Moving DTensor to be in the public namespace, to formally add the documentation page that includes all the public APIs. This includes:
    
    * many path renames and path import fixes
    * a dedicated doc page without too much content yet (adding in the next PRs)
    * To preserve the BC for users still using the torch.distributed._tensor, I added a shim script to redirect old path calls to the new module
    
    The BC preserving is evidented by the fact that all DTensor tests are still working without changing the public imports. So it's safe to land the changes
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134203
    Approved by: https://github.com/tianyu-l

--- test/distributed/_tensor/test_attention.py
+++ test/distributed/_tensor/test_attention.py
@@ -7,14 +7,14 @@ import torch.distributed as dist
 import torch.nn.functional as F
 from torch import nn
 from torch.distributed._tensor import DeviceMesh
-from torch.distributed._tensor.debug import CommDebugMode
-from torch.distributed._tensor.experimental.attention import (
+from torch.distributed._tensor.experimental._attention import (
     _AttentionContextParallel,
     _CausalBehavior,
     _context_parallel_buffers,
     _is_causal_behavior,
     context_parallel,
 )
+from torch.distributed.tensor.debug import CommDebugMode
 from torch.distributed.tensor.parallel import parallelize_module
 from torch.nn.attention import sdpa_kernel, SDPBackend
 from torch.testing._internal.common_cuda import (
