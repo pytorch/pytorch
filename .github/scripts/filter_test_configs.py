e13069627086d0c720a96f1e513510e1edc14071
Andrey Talman <atalman@fb.com>
2024-09-09 14:05:58 -0400

    [RELEASE-ONLY CHANGES] Branch Cut for Release 2.5 (#135506)
    * [RELEASE-ONLY CHANGES] Branch Cut for Release 2.5
    
    * fix_lint

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -39,9 +39,9 @@ SUPPORTED_PERIODICAL_MODES: Dict[str, Callable[[Optional[str]], bool]] = {
 }
 
 # The link to the published list of disabled jobs
-DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json"
+DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json?versionId=sxzMTP57qj.Vwz8dN1glkTK560Txq9W3"
 # and unstable jobs
-UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json"
+UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json?versionId=8f1.4S3MupuHXH8t0waxyGnPsGHJYdv9"
 
 # Some constants used to handle disabled and unstable jobs
 JOB_NAME_SEP = "/"
