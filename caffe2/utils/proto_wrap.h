12f6011abb272aa4a5880d04d4abf7bf2d051da0
cyy <cyyever@outlook.com>
2024-09-09 05:03:29 +0000

    Check function declarations in Caffe2 code (#134925)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134925
    Approved by: https://github.com/ezyang

--- caffe2/utils/proto_wrap.h
+++ caffe2/utils/proto_wrap.h
@@ -9,6 +9,29 @@ namespace caffe2 {
 // testing and valgrind cases to avoid protobuf appearing to "leak" memory).
 TORCH_API void ShutdownProtobufLibrary();
 
+// Caffe2 wrapper functions for protobuf's GetEmptyStringAlreadyInited()
+// function used to avoid duplicated global variable in the case when protobuf
+// is built with hidden visibility.
+TORCH_API const ::std::string& GetEmptyStringAlreadyInited();
 } // namespace caffe2
 
+namespace ONNX_NAMESPACE {
+
+// ONNX wrapper functions for protobuf's GetEmptyStringAlreadyInited() function
+// used to avoid duplicated global variable in the case when protobuf
+// is built with hidden visibility.
+TORCH_API const ::std::string& GetEmptyStringAlreadyInited();
+
+} // namespace ONNX_NAMESPACE
+
+namespace torch {
+
+// Caffe2 wrapper functions for protobuf's GetEmptyStringAlreadyInited()
+// function used to avoid duplicated global variable in the case when protobuf
+// is built with hidden visibility.
+TORCH_API const ::std::string& GetEmptyStringAlreadyInited();
+
+void ShutdownProtobufLibrary();
+
+} // namespace torch
 #endif // CAFFE2_UTILS_PROTO_WRAP_H_
