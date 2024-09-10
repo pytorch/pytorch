a833a899bd185996c008553a73c968024bd4ff85
Xinya Zhang <Xinya.Zhang@amd.com>
2024-09-10 02:12:56 +0000

    AOTriton: download binary from github unless AOTRITON_INSTALL_FROM_SOURCE=1 is set

--- cmake/External/aotriton.cmake
+++ cmake/External/aotriton.cmake
@@ -9,7 +9,7 @@ if(NOT __AOTRITON_INCLUDED)
   if(DEFINED ENV{AOTRITON_INSTALLED_PREFIX})
     set(__AOTRITON_INSTALL_DIR "$ENV{AOTRITON_INSTALLED_PREFIX}")
     message(STATUS "Using Preinstalled AOTriton at ${__AOTRITON_INSTALL_DIR}")
-  else()
+  else(DEFINED ENV{AOTRITON_INSTALL_FROM_SOURCE})
     file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/.ci/docker/aotriton_version.txt" __AOTRITON_CI_INFO)
     list(GET __AOTRITON_CI_INFO 3 __AOTRITON_CI_COMMIT)
     ExternalProject_Add(aotriton_external
@@ -34,6 +34,33 @@ if(NOT __AOTRITON_INCLUDED)
       )
     add_dependencies(__caffe2_aotriton aotriton_external)
     message(STATUS "Using AOTriton compiled from source directory ${__AOTRITON_SOURCE_DIR}")
+  else()
+    file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/.ci/docker/aotriton_version.txt" __AOTRITON_CI_INFO)
+    list(GET __AOTRITON_CI_INFO 0 __AOTRITON_VER)
+    list(GET __AOTRITON_CI_INFO 1 __AOTRITON_MANY)
+    list(GET __AOTRITON_CI_INFO 2 __AOTRITON_ROCM)
+    list(GET __AOTRITON_CI_INFO 3 __AOTRITON_COMMIT)
+    list(GET __AOTRITON_CI_INFO 4 __AOTRITON_SHA256)
+    list(GET __AOTRITON_CI_INFO 5 __AOTRITON_Z)
+    set(__AOTRITON_URL "https://github.com/ROCm/aotriton/releases/download/\
+    ${__AOTRITON_VER}/aotriton-\
+    ${__AOTRITON_VER}-${__AOTRITON_MANY}\
+    _${__AOTRITON_ARCH}-${__AOTRITON_ROCM}-shared.tar.${__AOTRITON_Z}")
+    set(__AOTRITON_ARCH "x86_64"
+    ExternalProject_Add(aotriton_external
+      URL "${__AOTRITON_URL}"
+      URL_HASH SHA256=${__AOTRITON_SHA256}
+      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+      "${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball/aotriton"
+      "${__AOTRITON_INSTALL_DIR}"
+      BUILD_BYPRODUCTS "${__AOTRITON_INSTALL_DIR}/lib/libaotriton_v2.so"
+    )
+    add_dependencies(__caffe2_aotriton aotriton_external)
+    message(STATUS "Using AOTriton from pre-compiled binary ${__AOTRITON_URL}.\
+    Set env variables AOTRITON_INSTALL_FROM_SOURCE=1 to build from source.")
   endif()
   target_link_libraries(__caffe2_aotriton INTERFACE ${__AOTRITON_INSTALL_DIR}/lib/libaotriton_v2.so)
   target_include_directories(__caffe2_aotriton INTERFACE ${__AOTRITON_INSTALL_DIR}/include)
