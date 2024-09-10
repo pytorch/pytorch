dc5013acaeacc1fc77c2783b42c78cf4ca17910c
Yu, Guangye <guangye.yu@intel.com>
2024-09-10 13:38:52 +0000

    Update
    [ghstack-poisoned]

--- torch/csrc/autograd/utils/wrap_outputs.h
+++ torch/csrc/autograd/utils/wrap_outputs.h
@@ -47,7 +47,7 @@ inline PyObject* wrap(c10::complex<double> value) {
 }
 
 inline PyObject* wrap(void* value) {
-  return THPUtils_packUInt64(reinterpret_cast<uintptr_t>(value));
+  return PyLong_FromVoidPtr(value);
 }
 
 inline PyObject* wrap(THPDtype* dtype) {
