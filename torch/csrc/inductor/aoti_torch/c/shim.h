c4ccc3a6d2eabc411a2860adbf5d17c52928247a
Shangdi Yu <shangdiy@meta.com>
2024-09-10 13:21:27 -0700

    lint, smaller test size

--- torch/csrc/inductor/aoti_torch/c/shim.h
+++ torch/csrc/inductor/aoti_torch/c/shim.h
@@ -526,8 +526,7 @@ aoti_torch_cpu__wrapped_quantized_linear_prepacked(
 AOTI_TORCH_EXPORT AOTITorchError
 aoti_torch_nonzero(AtenTensorHandle self, AtenTensorHandle* out);
 
-AOTI_TORCH_EXPORT AOTITorchError
-aoti_torch_zero_(AtenTensorHandle self);
+AOTI_TORCH_EXPORT AOTITorchError aoti_torch_zero_(AtenTensorHandle self);
 
 AOTI_TORCH_EXPORT AOTITorchError aoti_torch_repeat_interleave_Tensor(
     AtenTensorHandle repeats,
