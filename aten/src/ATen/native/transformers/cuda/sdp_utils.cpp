0acd85640f49827daf605b0009161c533066a2ad
Eddie Yan <eddiey@nvidia.com>
2024-09-11 05:59:25 +0000

    [cuDNN][SDPA] Support `attn_bias` in cuDNN (#130482)
    CC @drisspg
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130482
    Approved by: https://github.com/drisspg, https://github.com/Skylion007, https://github.com/malfet
    
    Co-authored-by: Nikita Shulga <2453524+malfet@users.noreply.github.com>

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -561,7 +561,7 @@ bool can_use_cudnn_attention(const sdp_params& params, bool debug) {
           check_cudnn_deterministic,
           // check_is_causal,
           check_dtypes_low_precision,
-          check_for_attn_mask_cudnn,
+          check_attn_mask_shape,
           check_cudnn_hardware_support
           );
   for (auto& constraint : general_constraints) {
