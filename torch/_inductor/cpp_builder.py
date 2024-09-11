c42ec0f9b597e220e2bd7be139774b8503af228c
pytorchbot <pytorchbot@pytorch.com>
2024-09-11 07:33:46 +0000

    2024-09-11 nightly release (26e5572dd24acd69b08eb94577f8796a480c9fe0)

--- torch/_inductor/cpp_builder.py
+++ torch/_inductor/cpp_builder.py
@@ -1166,8 +1166,8 @@ def _transform_cuda_paths(lpaths: List[str]) -> None:
                     break
 
 
-def get_cpp_torch_cuda_options(
-    cuda: bool,
+def get_cpp_torch_device_options(
+    device_type: str,
     aot_mode: bool = False,
     compile_only: bool = False,
 ) -> Tuple[List[str], List[str], List[str], List[str], List[str], List[str], List[str]]:
@@ -1190,10 +1190,10 @@ def get_cpp_torch_cuda_options(
     _set_gpu_runtime_env()
     from torch.utils import cpp_extension
 
-    include_dirs = cpp_extension.include_paths(cuda)
-    libraries_dirs = cpp_extension.library_paths(cuda)
+    include_dirs = cpp_extension.include_paths(device_type)
+    libraries_dirs = cpp_extension.library_paths(device_type)
 
-    if cuda:
+    if device_type == "cuda":
         definations.append(" USE_ROCM" if torch.version.hip else " USE_CUDA")
 
         if torch.version.hip is not None:
@@ -1208,6 +1208,11 @@ def get_cpp_torch_cuda_options(
             else:
                 libraries += ["c10_cuda", "cuda", "torch_cuda"]
 
+    if device_type == "xpu":
+        definations.append(" USE_XPU")
+        cflags += ["fsycl"]
+        libraries += ["c10_xpu", "sycl", "ze_loader", "torch_xpu"]
+
     if aot_mode:
         if config.is_fbcode():
             from torch._inductor.codecache import cpp_prefix_path
@@ -1215,7 +1220,7 @@ def get_cpp_torch_cuda_options(
             cpp_prefix_include_dir = [f"{os.path.dirname(cpp_prefix_path())}"]
             include_dirs += cpp_prefix_include_dir
 
-        if cuda and torch.version.hip is None:
+        if device_type == "cuda" and torch.version.hip is None:
             _transform_cuda_paths(libraries_dirs)
 
     if config.is_fbcode():
@@ -1224,7 +1229,7 @@ def get_cpp_torch_cuda_options(
         else:
             include_dirs.append(os.path.join(build_paths.cuda(), "include"))
 
-        if aot_mode and cuda:
+        if aot_mode and device_type == "cuda":
             if torch.version.hip is None:
                 if not compile_only:
                     # Only add link args, when compile_only is false.
@@ -1241,18 +1246,18 @@ def get_cpp_torch_cuda_options(
     )
 
 
-class CppTorchCudaOptions(CppTorchOptions):
+class CppTorchDeviceOptions(CppTorchOptions):
     """
     This class is inherited from CppTorchOptions, which automatic contains
     base cxx build options and torch common build options. And then it will
-    maintains cuda device related build args.
+    maintains cuda/xpu device related build args.
     """
 
     def __init__(
         self,
         vec_isa: VecISA = invalid_vec_isa,
         include_pytorch: bool = False,
-        cuda: bool = True,
+        device_type: str = "cuda",
         aot_mode: bool = False,
         compile_only: bool = False,
         use_absolute_path: bool = False,
@@ -1269,33 +1274,37 @@ class CppTorchCudaOptions(CppTorchOptions):
             use_mmap_weights=use_mmap_weights,
             extra_flags=extra_flags,
         )
+        if device_type == "xpu":
+            from torch.utils.cpp_extension import _join_sycl_home
 
-        cuda_definations: List[str] = []
-        cuda_include_dirs: List[str] = []
-        cuda_cflags: List[str] = []
-        cuda_ldflags: List[str] = []
-        cuda_libraries_dirs: List[str] = []
-        cuda_libraries: List[str] = []
-        cuda_passthough_args: List[str] = []
+            self._compiler = _join_sycl_home("bin", "icpx")
+
+        device_definations: List[str] = []
+        device_include_dirs: List[str] = []
+        device_cflags: List[str] = []
+        device_ldflags: List[str] = []
+        device_libraries_dirs: List[str] = []
+        device_libraries: List[str] = []
+        device_passthough_args: List[str] = []
 
         (
-            cuda_definations,
-            cuda_include_dirs,
-            cuda_cflags,
-            cuda_ldflags,
-            cuda_libraries_dirs,
-            cuda_libraries,
-            cuda_passthough_args,
-        ) = get_cpp_torch_cuda_options(
-            cuda=cuda, aot_mode=aot_mode, compile_only=compile_only
+            device_definations,
+            device_include_dirs,
+            device_cflags,
+            device_ldflags,
+            device_libraries_dirs,
+            device_libraries,
+            device_passthough_args,
+        ) = get_cpp_torch_device_options(
+            device_type=device_type, aot_mode=aot_mode, compile_only=compile_only
         )
-        _append_list(self._definations, cuda_definations)
-        _append_list(self._include_dirs, cuda_include_dirs)
-        _append_list(self._cflags, cuda_cflags)
-        _append_list(self._ldflags, cuda_ldflags)
-        _append_list(self._libraries_dirs, cuda_libraries_dirs)
-        _append_list(self._libraries, cuda_libraries)
-        _append_list(self._passthough_args, cuda_passthough_args)
+        _append_list(self._definations, device_definations)
+        _append_list(self._include_dirs, device_include_dirs)
+        _append_list(self._cflags, device_cflags)
+        _append_list(self._ldflags, device_ldflags)
+        _append_list(self._libraries_dirs, device_libraries_dirs)
+        _append_list(self._libraries, device_libraries)
+        _append_list(self._passthough_args, device_passthough_args)
         self._finalize_options()
 
 
--- torch/fx/passes/utils/fuser_utils.py
+++ torch/fx/passes/utils/fuser_utils.py
@@ -92,6 +92,7 @@ def validate_partition(partition: NodeList) -> bool:
 @compatibility(is_backward_compatible=False)
 def fuse_as_graphmodule(gm: GraphModule,
                         nodes: NodeList,
+                        partition: Dict[Node, None],
                         module_name: str) -> Tuple[GraphModule, Tuple[Node, ...], Tuple[Node, ...]]:
 
     """
@@ -135,7 +136,7 @@ def fuse_as_graphmodule(gm: GraphModule,
             # do something here
             pass
 
-        if x in nodes:
+        if x in partition:
             # x is inside subgraph, return the copied node
             # the node should have been copied aleady, as we are copying graph in the topological order
             return node_map[x]
@@ -159,7 +160,7 @@ def fuse_as_graphmodule(gm: GraphModule,
 
     for node in nodes:
         for user_node in node.users:
-            if user_node not in nodes:
+            if user_node not in partition:
                 # external user node, need to expose as an output
                 output_mapping[node] = node_map[node]
 
@@ -219,12 +220,12 @@ def erase_nodes(gm: GraphModule, nodes: NodeList):
 
 
 @compatibility(is_backward_compatible=False)
-def fuse_by_partitions(gm: GraphModule, partitions: List[NodeList], prefix: str = "fused_") -> GraphModule:
-    for partition_id, nodes in enumerate(partitions):
-        sorted_nodes = topo_sort(nodes)
+def fuse_by_partitions(gm: GraphModule, partitions: List[Dict[Node, None]], prefix: str = "fused_") -> GraphModule:
+    for partition_id, partition in enumerate(partitions):
+        sorted_nodes = topo_sort(list(partition))
 
         submodule_name = prefix + str(partition_id)
-        sub_gm, orig_inputs, orig_outputs = fuse_as_graphmodule(gm, sorted_nodes, submodule_name)
+        sub_gm, orig_inputs, orig_outputs = fuse_as_graphmodule(gm, sorted_nodes, partition, submodule_name)
 
         insert_subgm(gm, sub_gm, orig_inputs, orig_outputs)
 
