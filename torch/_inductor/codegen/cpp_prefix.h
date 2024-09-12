e39c174e4da2b0b04437dae10535327e42586092
Bin Bao <binbao@meta.com>
2024-09-12 16:18:10 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_prefix.h
+++ torch/_inductor/codegen/cpp_prefix.h
@@ -25,6 +25,7 @@
 #include <c10/util/generic_math.h>
 #include <c10/util/Half.h>
 #include <c10/util/TypeCast.h>
+#include <torch/csrc/inductor/aoti_torch/c/shim.h>
 
 #if defined(CPU_CAPABILITY_AVX512) || defined(CPU_CAPABILITY_AVX2) || defined(CPU_CAPABILITY_ZVECTOR) || defined(CPU_CAPABILITY_NEON) || defined(CPU_CAPABILITY_VSX)
 #define INDUCTOR_USE_VECTOR_TYPES() 1
