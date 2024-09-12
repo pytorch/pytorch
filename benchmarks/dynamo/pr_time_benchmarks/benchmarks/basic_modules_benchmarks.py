9a862299d820b4b7a640064cb8dc42ee226646ca
Laith Sakka <lsakka@meta.com>
2024-09-12 14:56:55 -0700

    Update on "Only keep ListOfLinears module in basic_modules_benchmarks and add gpu version."
    All of the previous benchmarks are similar, ListOfLinears should be representative enough.
    I copied the previous benchmarks from unit tests without an intention, was just trying to create a large
    number of benchmarks to better observe noise.
    
    This PR keeps only one, we can add more as we see value and regressions in the future. 
    Also this diff adds a GPU version. 
    ```
    collecting compile time instruction count for basic_modules_ListOfLinears_eager
    compile time instruction count for iteration 0 is 6479525851
    compile time instruction count for iteration 1 is 1024432680
    compile time instruction count for iteration 2 is 1019417317
    compile time instruction count for iteration 3 is 1013603566
    compile time instruction count for iteration 4 is 1008853980
    compile time instruction count for iteration 5 is 1009541481
    compile time instruction count for iteration 6 is 1005025533
    compile time instruction count for iteration 7 is 1004116323
    compile time instruction count for iteration 8 is 1000828633
    compile time instruction count for iteration 9 is 999788323
    collecting compile time instruction count for basic_modules_ListOfLinears_inductor
    compile time instruction count for iteration 0 is 40837529730
    compile time instruction count for iteration 1 is 18411921909
    compile time instruction count for iteration 2 is 18383665161
    compile time instruction count for iteration 3 is 18348983522
    compile time instruction count for iteration 4 is 18349276590
    compile time instruction count for iteration 5 is 18353046274
    compile time instruction count for iteration 6 is 18346818581
    compile time instruction count for iteration 7 is 18340057998
    compile time instruction count for iteration 8 is 18331267320
    compile time instruction count for iteration 9 is 18328381338
    collecting compile time instruction count for basic_modules_ListOfLinears_inductor_gpu
    compile time instruction count for iteration 0 is 15408870979
    compile time instruction count for iteration 1 is 10949520859
    compile time instruction count for iteration 2 is 11058786167
    compile time instruction count for iteration 3 is 11003606719
    compile time instruction count for iteration 4 is 10896406770
    compile time instruction count for iteration 5 is 10982875189
    compile time instruction count for iteration 6 is 10931848275
    compile time instruction count for iteration 7 is 10956345008
    compile time instruction count for iteration 8 is 11045384499
    ```
    
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- benchmarks/dynamo/pr_time_benchmarks/benchmarks/basic_modules_benchmarks.py
+++ benchmarks/dynamo/pr_time_benchmarks/benchmarks/basic_modules_benchmarks.py
@@ -1,11 +1,9 @@
-import gc
 import sys
 
 from benchmark_base import BenchmarkBase
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch._inductor.utils import fresh_inductor_cache
 
 
@@ -19,106 +17,6 @@ class ListOfLinears(nn.Module):
         for i, l in enumerate(self.linears):
             x = self.linears[i // 2](x) + l(x)
         return x
-
-
-class BasicModule(torch.nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.linear1 = torch.nn.Linear(10, 10)
-        self.scale = torch.randn(1, 10)
-
-    def forward(self, x):
-        return F.relu(self.linear1(x)) * self.scale
-
-
-class ModuleForwardHasGraphBreak(torch.nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.layer1 = BasicModule()
-        self.layer2 = BasicModule()
-        self.layer3 = torch.nn.Sequential(BasicModule(), BasicModule())
-        self.layer4 = torch.nn.ModuleList(
-            [
-                torch.nn.Linear(10, 10),
-                torch.nn.ReLU(),
-                torch.nn.Linear(10, 10),
-                torch.nn.ReLU(),
-            ]
-        )
-        self.layer5 = torch.nn.ModuleDict(
-            {
-                "0": torch.nn.Linear(10, 10),
-            }
-        )
-        self.scale = torch.randn(1, 10)
-
-    def forward(self, x):
-        """
-        This is used to test if the results of functions like `named_parameters`
-        can be reconstructed correctly after graph break.
-
-        https://github.com/pytorch/torchdynamo/issues/1931
-        """
-        x = self.layer1(x)
-        params1 = dict(self.named_parameters())
-        params2 = list(self.parameters())
-        buffers1 = dict(self.named_buffers())
-        buffers2 = list(self.buffers())
-        modules1 = dict(self.named_modules())
-        modules2 = list(self.modules())
-        torch._dynamo.graph_break()
-        y = modules2
-        y = modules1
-        y = buffers2
-        y = buffers1
-        y = params2
-        y = params1
-        x = (
-            self.layer2(x)
-            + y["layer3.1.linear1.weight"]
-            + y["layer4.2.weight"]
-            + y["layer5.0.weight"]
-        )
-        return x * self.scale
-
-
-class SequentialWithDuplicatedModule(torch.nn.Module):
-    # Sequential module(self.layer) contains three duplicated ReLU module.
-    def __init__(self) -> None:
-        super().__init__()
-        self.relu = torch.nn.ReLU()
-        self.layer = torch.nn.Sequential(
-            torch.nn.Linear(10, 20),
-            self.relu,
-            torch.nn.Linear(20, 20),
-            self.relu,
-            torch.nn.Linear(20, 10),
-            self.relu,
-        )
-
-    def forward(self, x):
-        return self.layer(x)
-
-
-class ModuleComparison(torch.nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.layer0 = torch.nn.Linear(10, 10)
-        self.layer1 = torch.nn.Linear(10, 10)
-        self.layer2 = torch.nn.Linear(10, 10)
-
-    @property
-    def encoder_layers(self):
-        return [self.layer0, self.layer1, self.layer2]
-
-    def forward(self, x):
-        for layer in self.encoder_layers:
-            output = layer(x)
-            if layer is None or layer == self.layer0:
-                output = F.relu6(output)
-            else:
-                output = F.relu(output)
-        return output
 
 
 class Benchmark(BenchmarkBase):
@@ -141,8 +39,6 @@ class Benchmark(BenchmarkBase):
         self.m = self.ModuleClass()
         torch.set_float32_matmul_precision("high")
         self.input = torch.ones(10, device="cuda" if self._is_gpu else "cpu")
-        gc.collect()
-        gc.disable()
 
     def _prepare(self):
         torch._dynamo.reset()
@@ -158,32 +54,9 @@ class Benchmark(BenchmarkBase):
 def main():
     result_path = sys.argv[1]
     benchmarks = [
-        Benchmark(
-            ListOfLinears,
-            "inductor",
-        ),
         Benchmark(ListOfLinears, "eager"),
         Benchmark(ListOfLinears, "inductor"),
-        Benchmark(ListOfLinears, "eager", dynamic=True),
-        Benchmark(ListOfLinears, "inductor", dynamic=True),
         Benchmark(ListOfLinears, "inductor", is_gpu=True),
-        Benchmark(ListOfLinears, "inductor", is_gpu=True, dynamic=True),
-        Benchmark(ModuleForwardHasGraphBreak, "inductor"),
-        Benchmark(ModuleForwardHasGraphBreak, "eager"),
-        Benchmark(SequentialWithDuplicatedModule, "inductor"),
-        Benchmark(SequentialWithDuplicatedModule, "eager"),
-        Benchmark(SequentialWithDuplicatedModule, "inductor", dynamic=True),
-        Benchmark(SequentialWithDuplicatedModule, "eager", dynamic=True),
-        Benchmark(SequentialWithDuplicatedModule, "inductor", is_gpu=True),
-        Benchmark(
-            SequentialWithDuplicatedModule, "inductor", is_gpu=True, dynamic=True
-        ),
-        Benchmark(ModuleComparison, "inductor"),
-        Benchmark(ModuleComparison, "eager"),
-        Benchmark(ModuleComparison, "inductor", dynamic=True),
-        Benchmark(ModuleComparison, "eager", dynamic=True),
-        Benchmark(ModuleComparison, "inductor", is_gpu=True),
-        Benchmark(ModuleComparison, "inductor", is_gpu=True, dynamic=True),
     ]
     for b in benchmarks:
         b.enable_compile_time_instruction_count().collect_all().append_results(
