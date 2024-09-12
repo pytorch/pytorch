2215b426c8ebcbb90b5a958348afb0e2ff534644
Justin Chu <justinchuby@users.noreply.github.com>
2024-09-12 15:33:07 -0700

    Update test_fx_type_promotion.py

--- test/onnx/test_fx_type_promotion.py
+++ test/onnx/test_fx_type_promotion.py
@@ -9,6 +9,8 @@ class TestGeneratedTypePromotionRuleSet(common_utils.TestCase):
         generated_set = type_promotion._GENERATED_ATEN_TYPE_PROMOTION_RULE_SET
         latest_set = type_promotion.ElementwiseTypePromotionRuleSetGenerator.generate_from_torch_refs()
 
+        # Please update the list in torch/onnx/_internal/fx/passes/type_promotion.py following the instruction
+        # if this test fails
         self.assertEqual(generated_set, latest_set)
 
     def test_initialize_type_promotion_table_succeeds(self):
