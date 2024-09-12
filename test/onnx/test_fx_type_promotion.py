035a0fd97753213148c4981b73908d1203d0ee6f
Justin Chu <justinchu@microsoft.com>
2024-09-12 15:31:03 -0700

    [ONNX] Run type promotion test in CI and update the table

--- test/onnx/test_fx_type_promotion.py
+++ test/onnx/test_fx_type_promotion.py
@@ -1,20 +1,14 @@
 # Owner(s): ["module: onnx"]
 
-import pytorch_test_common
-
 from torch.onnx._internal.fx.passes import type_promotion
 from torch.testing._internal import common_utils
 
 
 class TestGeneratedTypePromotionRuleSet(common_utils.TestCase):
-    @pytorch_test_common.skip_in_ci(
-        "Reduce noise in CI. "
-        "The test serves as a tool to validate if the generated rule set is current. "
-    )
     def test_generated_rule_set_is_up_to_date(self):
         generated_set = type_promotion._GENERATED_ATEN_TYPE_PROMOTION_RULE_SET
         latest_set = (
-            type_promotion.TypePromotionRuleSetGenerator.generate_from_torch_refs()
+            type_promotion.ElementwiseTypePromotionRuleSetGenerator.generate_from_torch_refs()
         )
 
         self.assertEqual(generated_set, latest_set)
