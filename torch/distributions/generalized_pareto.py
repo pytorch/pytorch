fix variance

torch/distributions/generalized_pareto.py
@@ -130,9 +130,7 @@ class GeneralizedPareto(Distribution):
         concentration = self.concentration
         valid = concentration < 0.5
         safe_conc = torch.where(valid, concentration, 0.25)
-        result = self.scale**2 / (
-            (1 - safe_conc) ** 2 * (1 - 2 * safe_conc)
-        ) + torch.zeros_like(self.loc)
+        result = self.scale**2 / ((1 - safe_conc) ** 2 * (1 - 2 * safe_conc))
         return torch.where(valid, result, torch.full_like(result, nan))
 
     def entropy(self):