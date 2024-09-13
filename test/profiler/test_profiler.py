2075e244ea6ef74f0af2c391e746bf806213a12f
Chen, Zejun <zejun.chen@intel.com>
2024-09-13 16:53:51 +0800

    [No merge] test case failure
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>

--- test/profiler/test_profiler.py
+++ test/profiler/test_profiler.py
@@ -224,94 +224,7 @@ class TestProfilerITT(TestCase):
 
 
 @instantiate_parametrized_tests
-class TestProfiler(TestCase):
-    @unittest.skipIf(
-        TEST_WITH_CROSSREF, "crossref intercepts calls and changes the callsite."
-    )
-    def test_source(self):
-        """Checks that source code attribution works for eager, TS and autograd mode"""
-        # avoid automatic inlining
-        prev_opt = torch._C._get_graph_executor_optimize()
-        torch._C._set_graph_executor_optimize(False)
-
-        @torch.jit.script
-        def ts_method_2(x, y):
-            return torch.matmul(x, y)
-
-        @torch.jit.script
-        def ts_method_1(x, y, z):
-            a = x + z
-            w = ts_method_2(x, y) + a
-            return w.sum()
-
-        class DummyModule(nn.Module):
-            def __init__(self) -> None:
-                super().__init__()
-                self.conv = torch.nn.Conv2d(
-                    3, 2, kernel_size=1, stride=2, padding=3, bias=False
-                )
-
-            def forward(self, x):
-                return self.conv(x)
-
-        mod = DummyModule()
-
-        def call_module(x):
-            return mod(x)
-
-        with _profile(
-            with_stack=True,
-            use_kineto=kineto_available(),
-            experimental_config=_ExperimentalConfig(verbose=True),
-        ) as p:
-            x = torch.randn(10, 10, requires_grad=True)
-            y = torch.randn(10, 10, requires_grad=True)
-            z = x + y
-            w = ts_method_1(x, y, z)
-            v = 2 * w
-            v.backward()
-            a = torch.randn(2, 3, 2, 2, requires_grad=True)
-            b = call_module(a)
-            c = b.sum()
-            c.backward()
-
-        for e in p.function_events:
-            if "aten::add" in e.name or "AddBackward" in e.name:
-                self.assertTrue(any("test_profiler" in entry for entry in e.stack))
-                self.assertTrue(
-                    any(
-                        (
-                            "test_source" in entry
-                            or "ts_method_1" in entry
-                            or "ts_method_2" in entry
-                        )
-                        for entry in e.stack
-                    )
-                )
-
-        # TODO: https://github.com/pytorch/kineto/issues/617
-        if kineto_available() and not IS_WINDOWS:
-            with TemporaryFileName(mode="w+") as fname:
-                p.export_chrome_trace(fname)
-                with open(fname) as f:
-                    events = json.load(f)["traceEvents"]
-
-                def extract(pattern: str):
-                    matches = [e for e in events if re.search(pattern, e["name"])]
-                    self.assertEqual(
-                        len(matches), 1, repr([e["name"] for e in matches])
-                    )
-                    return matches[0]
-
-                module_event = extract(r"DummyModule_0")
-                wrapper_event = extract(r"call_module")
-                self.assertEqual(
-                    module_event["args"]["Python parent id"],
-                    wrapper_event["args"]["Python id"],
-                )
-
-        torch._C._set_graph_executor_optimize(prev_opt)
-
+class TestProfiler1(TestCase):
     @parametrize(
         "name,thread_spec",
         {
@@ -476,6 +389,95 @@ class TestProfiler(TestCase):
         ]
         self.assertEqual(len(observed_during_run), worker_threads)
         self.assertEqual(len(observed_during_run), len(set(observed_during_run)))
+
+
+class TestProfiler(TestCase):
+    @unittest.skipIf(
+        TEST_WITH_CROSSREF, "crossref intercepts calls and changes the callsite."
+    )
+    def test_source(self):
+        """Checks that source code attribution works for eager, TS and autograd mode"""
+        # avoid automatic inlining
+        prev_opt = torch._C._get_graph_executor_optimize()
+        torch._C._set_graph_executor_optimize(False)
+
+        @torch.jit.script
+        def ts_method_2(x, y):
+            return torch.matmul(x, y)
+
+        @torch.jit.script
+        def ts_method_1(x, y, z):
+            a = x + z
+            w = ts_method_2(x, y) + a
+            return w.sum()
+
+        class DummyModule(nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+                self.conv = torch.nn.Conv2d(
+                    3, 2, kernel_size=1, stride=2, padding=3, bias=False
+                )
+
+            def forward(self, x):
+                return self.conv(x)
+
+        mod = DummyModule()
+
+        def call_module(x):
+            return mod(x)
+
+        with _profile(
+            with_stack=True,
+            use_kineto=kineto_available(),
+            experimental_config=_ExperimentalConfig(verbose=True),
+        ) as p:
+            x = torch.randn(10, 10, requires_grad=True)
+            y = torch.randn(10, 10, requires_grad=True)
+            z = x + y
+            w = ts_method_1(x, y, z)
+            v = 2 * w
+            v.backward()
+            a = torch.randn(2, 3, 2, 2, requires_grad=True)
+            b = call_module(a)
+            c = b.sum()
+            c.backward()
+
+        for e in p.function_events:
+            if "aten::add" in e.name or "AddBackward" in e.name:
+                self.assertTrue(any("test_profiler" in entry for entry in e.stack))
+                self.assertTrue(
+                    any(
+                        (
+                            "test_source" in entry
+                            or "ts_method_1" in entry
+                            or "ts_method_2" in entry
+                        )
+                        for entry in e.stack
+                    )
+                )
+
+        # TODO: https://github.com/pytorch/kineto/issues/617
+        if kineto_available() and not IS_WINDOWS:
+            with TemporaryFileName(mode="w+") as fname:
+                p.export_chrome_trace(fname)
+                with open(fname) as f:
+                    events = json.load(f)["traceEvents"]
+
+                def extract(pattern: str):
+                    matches = [e for e in events if re.search(pattern, e["name"])]
+                    self.assertEqual(
+                        len(matches), 1, repr([e["name"] for e in matches])
+                    )
+                    return matches[0]
+
+                module_event = extract(r"DummyModule_0")
+                wrapper_event = extract(r"call_module")
+                self.assertEqual(
+                    module_event["args"]["Python parent id"],
+                    wrapper_event["args"]["Python id"],
+                )
+
+        torch._C._set_graph_executor_optimize(prev_opt)
 
     def payload(self, use_cuda=False):
         x = torch.randn(10, 10)
