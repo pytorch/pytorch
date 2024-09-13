e5d826a3cd7e065ae53f43819904f804d1f57bd8
atalman <atalman@fb.com>
2024-09-13 12:52:14 -0700

    Use python 3.11 for Colab Wheel

--- .github/scripts/generate_binary_build_matrix.py
+++ .github/scripts/generate_binary_build_matrix.py
@@ -413,7 +413,7 @@ def generate_wheels_matrix(
                     }
                 )
                 # Special build building to use on Colab. PyThon 3.10 for 12.1 CUDA
-                if python_version == "3.10" and arch_version == "12.1":
+                if python_version == "3.11" and arch_version == "12.1":
                     ret.append(
                         {
                             "python_version": python_version,
