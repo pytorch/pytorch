e9763a58958e780cd4c05b84300fd6ce8c446724
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-09-14 08:24:13 -0700

    Save Self for another PR

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -7,7 +7,6 @@ from collections import OrderedDict
 from copy import deepcopy
 from numbers import Number
 from typing import Any, Dict, Optional, Tuple, Union
-from typing_extensions import Self
 
 import torch
 import torch._C as _C
@@ -1354,7 +1353,7 @@ class Tensor(torch._C.TensorBase):
             [name for name in names if not is_ellipsis(name)], ellipsis_idx
         )
 
-    def unflatten(self, dim, sizes) -> Self:  # typing: ignore[override]
+    def unflatten(self, dim, sizes):  # typing: ignore[override]
         r"""
         unflatten(dim, sizes) -> Tensor
 
