bee496cfde78767507334fda7da6a2cc93524f7f
Donald Tolley <tolleybot@gmail.com>
2024-09-14 18:49:17 -0400

    making sure "weights" are canged to "weight"

--- test/test_nn.py
+++ test/test_nn.py
@@ -2580,24 +2580,24 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""")
     def test_weighted_mse_loss(self):
         inputs = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
         targets = torch.tensor([1.5, 2.5, 3.5, 4.5])
-        weights = torch.tensor([1.0, 2.0, 3.0, 4.0])
-        loss = F.mse_loss(inputs, targets, weights=weights, reduction='mean')
+        weight = torch.tensor([1.0, 2.0, 3.0, 4.0])
+        loss = F.mse_loss(inputs, targets, weight=weight, reduction='mean')
         expected_loss = torch.tensor(0.25)
         self.assertTrue(torch.isclose(loss, expected_loss), f"Expected {expected_loss}, but got {loss}")
 
     def test_weighted_l1_loss_with_weights(self):
         inputs = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
         targets = torch.tensor([1.5, 2.5, 3.5, 4.5])
-        weights = torch.tensor([1.0, 2.0, 3.0, 4.0])
-        loss = F.l1_loss(inputs, targets, weights=weights, reduction='mean')
+        weight = torch.tensor([1.0, 2.0, 3.0, 4.0])
+        loss = F.l1_loss(inputs, targets, weight=weight, reduction='mean')
         expected_loss = torch.tensor(0.5)
         self.assertTrue(torch.isclose(loss, expected_loss), f"Expected {expected_loss}, but got {loss}")
 
     def test_weighted_huber_loss(self):
         inputs = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
         targets = torch.tensor([1.5, 2.5, 3.5, 4.5])
-        weights = torch.tensor([1.0, 2.0, 3.0, 4.0])
-        loss = F.huber_loss(input=inputs, target=targets, weights=weights, reduction='mean', delta=1.0)
+        weight = torch.tensor([1.0, 2.0, 3.0, 4.0])
+        loss = F.huber_loss(input=inputs, target=targets, weight=weight, reduction='mean', delta=1.0)
         expected_loss = torch.tensor(0.25)
         print(torch.isclose(loss, expected_loss, atol=1e-6), f"Expected {expected_loss}, but got {loss}")
 
