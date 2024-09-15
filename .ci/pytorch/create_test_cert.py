cd68205e072ca197db41f8853d5ad158726d0d25
Amin Alam <ma.alamalhoda@gmail.com>
2024-09-16 01:12:54 +0200

    datetime.utcnow() is deprecated

--- .ci/pytorch/create_test_cert.py
+++ .ci/pytorch/create_test_cert.py
@@ -42,10 +42,10 @@ def create_cert(path, C, ST, L, O, key):
         .issuer_name(issuer)
         .public_key(key.public_key())
         .serial_number(x509.random_serial_number())
-        .not_valid_before(datetime.utcnow())
+        .not_valid_before(datetime.now(datetime.timezone.utc))
         .not_valid_after(
             # Our certificate will be valid for 10 days
-            datetime.utcnow()
+            datetime.now(datetime.timezone.utc)
             + timedelta(days=10)
         )
         .add_extension(
@@ -88,10 +88,10 @@ def sign_certificate_request(path, csr_cert, ca_cert, private_ca_key):
         .issuer_name(ca_cert.subject)
         .public_key(csr_cert.public_key())
         .serial_number(x509.random_serial_number())
-        .not_valid_before(datetime.utcnow())
+        .not_valid_before(datetime.now(datetime.timezone.utc))
         .not_valid_after(
             # Our certificate will be valid for 10 days
-            datetime.utcnow()
+            datetime.now(datetime.timezone.utc)
             + timedelta(days=10)
             # Sign our certificate with our private key
         )
