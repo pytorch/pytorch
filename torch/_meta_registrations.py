4c313ac6678a9d0c36713ce218fb6a8355b12c6c
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-09-15 11:19:52 -0700

    Fix python meta registraton

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -6343,7 +6343,7 @@ def meta_searchsorted(
 
 def _check_for_unsupported_isin_dtype(dtype):
     torch._check(
-        dtype not in [torch.bool, torch.bfloat16, torch.complex128, torch.complex64],
+        dtype not in [torch.bool, torch.complex128, torch.complex64],
         lambda: f"Unsupported input type encountered for isin(): {dtype}",
     )
 
