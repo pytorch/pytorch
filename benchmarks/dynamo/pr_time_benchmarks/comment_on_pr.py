cc35213577e00131e4052d8aaa891ddb3c997691
Laith Sakka <lsakka@meta.com>
2024-09-16 08:18:45 -0700

    Update on "something"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- benchmarks/dynamo/pr_time_benchmarks/comment_on_pr.py
+++ benchmarks/dynamo/pr_time_benchmarks/comment_on_pr.py
@@ -1,14 +1,14 @@
+import json
 
 import requests
-import json
-import os
+
 
 def comment_on_pr(text, pr_number) -> None:
     headers = {
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization" :"Bearer XXX",
-            "X-GitHub-Api-Version": "2022-11-28"
-            }
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": "Bearer XXX",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
 
     response = requests.post(
         f"https://api.github.com/repos/pytorch/pytorch/issues/{pr_number}/comments",
@@ -19,5 +19,4 @@ def comment_on_pr(text, pr_number) -> None:
 
 
 if __name__ == "__main__":
-
-    comment_on_pr("this comment is auto generated testing , ",131751 )
+    comment_on_pr("this comment is auto generated testing , ", 131751)
