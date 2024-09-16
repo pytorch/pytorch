ebd4755b0d8a815b5ec81e5afa4a594caa4d3b6e
FindHao <yhao@meta.com>
2024-09-16 16:17:58 -0400

    fix lint

--- benchmarks/dynamo/operatorbench/run.py
+++ benchmarks/dynamo/operatorbench/run.py
@@ -1,13 +1,18 @@
+import click
 import operators
 from operators import BaseOperator
-from utils.common import BenchmarkConfig, Device, Phase, dtype_mapping
-from utils.metrics import Metrics, get_execution_time, MetricResult
-import click
+from utils.common import BenchmarkConfig, Device, dtype_mapping, Phase
+from utils.metrics import get_execution_time, MetricResult, Metrics
+
 import torch
+
+
 QUANTILES = [0.2, 0.5, 0.8]
 
 
-def benchmark_operator(OperatorClass: BaseOperator, device, dtype, phase, max_samples, repeat, metrics):
+def benchmark_operator(
+    OperatorClass: BaseOperator, device, dtype, phase, max_samples, repeat, metrics
+):
     benchmark_config = BenchmarkConfig(
         device=device,
         dtype=dtype,
@@ -42,33 +47,57 @@ def benchmark_operator(OperatorClass: BaseOperator, device, dtype, phase, max_sa
 
             def fn():
                 return phase_fn(input, target)
+
             for metric in metrics:
                 if metric == Metrics.EXECUTION_TIME:
-                    execution_time.append(get_execution_time(fn, quantiles=QUANTILES, grad_to_none=grad_to_none, device=device))
+                    execution_time.append(
+                        get_execution_time(
+                            fn,
+                            quantiles=QUANTILES,
+                            grad_to_none=grad_to_none,
+                            device=device,
+                        )
+                    )
         metric_result.execution_time.append(execution_time)
-    
+
     print(metric_result.execution_time)
-                    
 
 
 @click.command()
 @click.option("--op", help="operator overload to benchmark. split by ','.")
-@click.option("--dtype", help="dtype to benchmark. [bfloat16, float16, float32]", default="bfloat16")
-@click.option("--max-samples", help="max samples per op. each operator may have different inputs. this is the number of inputs to sample.", default=1)
-@click.option("--device", help=f"device to benchmark, {[device.value.lower() for device in Device]}. ", default=Device.CUDA.value)
-@click.option("--phase", help=f"phase to benchmark. {[phase.value.lower() for phase in Phase]}. ", default="forward")
-@click.option("--repeat", help="repeat", default=1)
-@click.option("--metrics", help=f"metrics to benchmark. {[metric.value.lower() for metric in Metrics]}. split by ','", default=Metrics.EXECUTION_TIME.value)
-@click.option("--skip-variants", help="variants to be skipped, [liger, baseline, inductor]", default="")
+@click.option(
+    "--dtype",
+    help="dtype to benchmark. [bfloat16, float16, float32]",
+    default="bfloat16",
+)
+@click.option(
+    "--max-samples",
+    help="max samples per op. each operator may have different inputs. this is the number of inputs to sample.",
+    default=1,
+)
+@click.option(
+    "--device",
+    help=f"device to benchmark, {[device.value.lower() for device in Device]}. ",
+    default=Device.CUDA.value,
+)
+@click.option(
+    "--phase",
+    help=f"phase to benchmark. {[phase.value.lower() for phase in Phase]}. ",
+    default="forward",
+)
+@click.option("--repeat", help="repeat", default=15)
+@click.option(
+    "--metrics",
+    help=f"metrics to benchmark. {[metric.value.lower() for metric in Metrics]}. split by ','",
+    default=Metrics.EXECUTION_TIME.value,
+)
+@click.option(
+    "--skip-variants",
+    help="variants to be skipped, [liger, baseline, inductor]",
+    default="",
+)
 def run_benchmarks(
-    op,
-    dtype,
-    max_samples,
-    device,
-    phase,
-    repeat,
-    metrics,
-    skip_variants
+    op, dtype, max_samples, device, phase, repeat, metrics, skip_variants
 ):
     # This is a list of classes, not instances
     operators_list: list[BaseOperator] = operators.list_operators()
@@ -79,20 +108,27 @@ def run_benchmarks(
         desired_op_names = [operator.name for operator in operators_list]
 
     dtype = dtype_mapping.get(dtype, torch.float32)  # Default to float32 if not found
-    metrics = [Metrics[metric.strip().upper()] for metric in metrics.split(",")
-               if metric.strip().upper() in Metrics.__members__]
+    metrics = [
+        Metrics[metric.strip().upper()]
+        for metric in metrics.split(",")
+        if metric.strip().upper() in Metrics.__members__
+    ]
     device = Device[device.upper()]
     if device != Device.CUDA and Metrics.GPU_PEAK_MEM in metrics:
         print(f"{Metrics.GPU_PEAK_MEM.value} is only supported on cuda")
         metrics.remove(Metrics.GPU_PEAK_MEM)
     skip_variants = skip_variants.split(",")
-    skip_variants = [variant.lower().strip() for variant in skip_variants if variant.strip()]
+    skip_variants = [
+        variant.lower().strip() for variant in skip_variants if variant.strip()
+    ]
     phase = Phase[phase.upper()]
     for operator in operators_list:
         if operator.name in desired_op_names:
             if operator.variant.lower().strip() in skip_variants:
                 continue
-            benchmark_operator(operator, device, dtype, phase, max_samples, repeat, metrics)
+            benchmark_operator(
+                operator, device, dtype, phase, max_samples, repeat, metrics
+            )
 
 
 if __name__ == "__main__":
