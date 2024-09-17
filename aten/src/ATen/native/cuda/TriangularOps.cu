c61cd8d819609fcc9ca74d170af629050e8aaaeb
grad0s <84247753+grad0s@users.noreply.github.com>
2024-09-17 21:26:19 +0300

    int overflow bug fix

--- aten/src/ATen/native/cuda/TriangularOps.cu
+++ aten/src/ATen/native/cuda/TriangularOps.cu
@@ -44,7 +44,7 @@ __global__ void triu_tril_kernel(
     const int64_t k,
     const int64_t N_padded,
     const IndexType last_dim_padded) {
-  int64_t linear_idx = (blockIdx.x * blockDim.x + threadIdx.x) * elements_per_thread;
+  int64_t linear_idx = ((uint64_t)blockIdx.x * (uint64_t)blockDim.x + (uint64_t)threadIdx.x) * (uint64_t)elements_per_thread;
   if (linear_idx >= N_padded) {
     return;
   }
