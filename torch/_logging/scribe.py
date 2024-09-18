d51429603d86f4db8ad411acdb99e8700026876d
Aaron Orenstein <aorenste@fb.com>
2024-09-18 12:19:47 -0700

    Update on "Existing mypy issues"
    cc jgong5 mingfeima XiaobingSuper sanchitintel ashokei jingxu10 voznesenskym penguinwu EikanWang Guobing-Chen zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy yf225 chenyang78 kadeng muchulee8 ColinPeppler amjames desertfire chauhang
    
    [ghstack-poisoned]

--- torch/_logging/scribe.py
+++ torch/_logging/scribe.py
@@ -3,8 +3,8 @@ from typing_extensions import TypeAlias
 
 
 try:
-    from fbscribelogger import (
-        make_scribe_logger,  # type: ignore[import-untyped, import-not-found]
+    from fbscribelogger import (  # type: ignore[import-untyped, import-not-found]
+        make_scribe_logger,
     )
 except ImportError:
     TAtom: TypeAlias = Union[int, float, bool, str]
