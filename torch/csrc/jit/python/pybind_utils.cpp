243c3c42cbc8fa95fa95b3f9b272f2afdb4f0fed
albanD <desmaison.alban@gmail.com>
2024-09-19 14:57:28 -0400

    lint

--- torch/csrc/jit/python/pybind_utils.cpp
+++ torch/csrc/jit/python/pybind_utils.cpp
@@ -34,14 +34,15 @@ ToIValueAllowNumbersAsTensors::~ToIValueAllowNumbersAsTensors() {
 // if C++ creates a new object at the memory location of the deleted object.
 void clear_registered_instances(void* ptr) {
 #if IS_PYBIND_2_13_PLUS
-  py::detail::with_instance_map(ptr, [&](py::detail::instance_map& registered_instances) {
-    auto range = registered_instances.equal_range(ptr);
-    for (auto it = range.first; it != range.second; ++it) {
-      auto vh = it->second->get_value_and_holder();
-      vh.set_instance_registered(false);
-    }
-    registered_instances.erase(ptr);
-  });
+  py::detail::with_instance_map(
+    ptr, [&](py::detail::instance_map& registered_instances) {
+      auto range = registered_instances.equal_range(ptr);
+      for (auto it = range.first; it != range.second; ++it) {
+        auto vh = it->second->get_value_and_holder();
+        vh.set_instance_registered(false);
+      }
+      registered_instances.erase(ptr);
+    });
 #else
   auto& registered_instances =
       pybind11::detail::get_internals().registered_instances;
