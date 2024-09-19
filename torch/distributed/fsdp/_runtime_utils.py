4179bd03b48573641dd530621fc5c819a65ca067
yongzx <zheng_xin_yong@brown.edu>
2024-09-19 00:19:37 +0000

    fix docstrings

--- torch/distributed/fsdp/_runtime_utils.py
+++ torch/distributed/fsdp/_runtime_utils.py
@@ -356,8 +356,7 @@ def _pre_forward(
     also converts forward ``args`` and ``kwargs`` to the given precision.
 
     Args:
-        state (_FSDPState): ...
-        handle (Optional[FlatParamHandle]): Handles giving the parameters used in
+        handle (Optional[FlatParamHandle]): Handle giving the parameters used in
             the current forward.
         unshard_fn (Optional[Callable]): A callable to unshard any currently
             sharded parameters or ``None`` to not do any unsharding.
@@ -446,8 +445,7 @@ def _post_forward(
     and registering pre-backward hooks on the forward outputs.
 
     Args:
-        state (_FSDPState): ...
-        handle (Optional[FlatParamHandle]): Handles giving the parameters used in
+        handle (Optional[FlatParamHandle]): Handle giving the parameters used in
             the current forward.
         reshard_fn (Optional[Callable]): A callable to reshard any currently
             unsharded parameters (e.g. from the current forward) or ``None`` to
