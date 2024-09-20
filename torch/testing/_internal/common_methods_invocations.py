92c1e9bfb8675c84132d7c469a468727d8734678
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-09-20 12:09:44 -0700

    [BE]: Add half CUDA support nextafter

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -16408,7 +16408,6 @@ op_db: List[OpInfo] = [
     ),
     BinaryUfuncInfo('nextafter',
                     dtypes=floating_types_and(torch.bfloat16, torch.half),
-                    dtypesIfCUDA=floating_types_and(torch.bfloat16),
                     supports_autograd=False,
                     supports_rhs_python_scalar=False),
     OpInfo(
