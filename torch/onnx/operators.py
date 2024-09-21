af00002d587042f945808ae47e8523602c61cc19
Justin Chu <justinchu@microsoft.com>
2024-09-21 13:18:06 -0700

    update

--- torch/onnx/operators.py
+++ torch/onnx/operators.py
@@ -1,5 +1,5 @@
 # mypy: allow-untyped-defs
-r"""This file provides a location for operators that help exporting models via onnx.
+"""This file provides a location for operators that help exporting models via onnx.
 
 E.g. `shape_as_tensor` and `reshape_from_tensor_shape`
 are to make all dynamic sizes operations traceable.
