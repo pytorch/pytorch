1691c292c7f7851c54db6fca48ba79725a5fb75f
Tom Ritchford <tom@swirly.com>
2024-09-21 14:32:48 +0000

    Update
    [ghstack-poisoned]

--- benchmarks/gpt_fast/quantize.py
+++ benchmarks/gpt_fast/quantize.py
@@ -93,7 +93,6 @@ class WeightOnlyInt8Linear(torch.nn.Module):
         device=None,
         dtype=None,
     ) -> None:
-        factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
