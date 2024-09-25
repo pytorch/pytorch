e0c0948c4c042076a5dc70935744875e9c785075
Xinya Zhang <Xinya.Zhang@amd.com>
2024-09-25 21:02:36 +0000

    Fix the use of PREFIX in ExternalProject_Add

--- cmake/External/aotriton.cmake
+++ cmake/External/aotriton.cmake
@@ -1,9 +1,7 @@
 if(NOT __AOTRITON_INCLUDED)
   set(__AOTRITON_INCLUDED TRUE)
 
-  set(__AOTRITON_STAMP_DIR "${CMAKE_CURRENT_BINARY_DIR}/aotriton/cmake-stamp")
-  set(__AOTRITON_SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/aotriton/src")
-  set(__AOTRITON_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/aotriton/build")
+  set(__AOTRITON_EXTERN_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/aotriton")
   set(__AOTRITON_INSTALL_DIR "${PROJECT_SOURCE_DIR}/torch")
   add_library(__caffe2_aotriton INTERFACE)
   # Note it is INSTALL"ED"
@@ -20,10 +18,8 @@ if(NOT __AOTRITON_INCLUDED)
     ExternalProject_Add(aotriton_external
       GIT_REPOSITORY https://github.com/ROCm/aotriton.git
       GIT_TAG ${__AOTRITON_CI_COMMIT}
-      STAMP_DIR ${__AOTRITON_STAMP_DIR}
-      SOURCE_DIR ${__AOTRITON_SOURCE_DIR}
-      BINARY_DIR ${__AOTRITON_BUILD_DIR}
-      PREFIX ${__AOTRITON_INSTALL_DIR}
+      PREFIX ${__AOTRITON_EXTERN_PREFIX}
+      INSTALL_DIR ${__AOTRITON_INSTALL_DIR}
       CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${__AOTRITON_INSTALL_DIR}
       -DAOTRITON_COMPRESS_KERNEL=ON
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -39,7 +35,7 @@ if(NOT __AOTRITON_INCLUDED)
       # INSTALL_COMMAND ${MAKE_COMMAND} install
       )
     add_dependencies(__caffe2_aotriton aotriton_external)
-    message(STATUS "Using AOTriton compiled from source directory ${__AOTRITON_SOURCE_DIR}")
+    message(STATUS "Using AOTriton compiled from source directory ${__AOTRITON_EXTERN_PREFIX}")
   else()
     file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/.ci/docker/aotriton_version.txt" __AOTRITON_CI_INFO)
     list(GET __AOTRITON_CI_INFO 0 __AOTRITON_VER)
