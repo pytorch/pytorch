4968f207c5cc69f18d1a09d2cfc362053c02e363
IvanKobzarev <ivan.kobzarev@gmail.com>
2024-09-25 06:03:48 -0700

    [aotd] Test rrelu noise mutation in compile
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -6001,6 +6001,20 @@ class TestAOTModuleSimplified(AOTTestCase):
         torch.compile(fn, backend="inductor", fullgraph=True)(x)
         torch.compile(fn_, backend="inductor", fullgraph=True)(x)
 
+    def test_rrelu_noise_mutation(self):
+        def fn(x):
+            noise = torch.ones_like(x)
+            result = torch._C._nn.rrelu_with_noise(x, noise, 0.2, 0.8, True)
+            return result, noise
+
+        x = -torch.abs(torch.randn(4, 4, requires_grad=True))
+
+        ref_y, ref_noise = fn(x)
+        self.assertTrue(torch.all(ref_noise < torch.ones_like(ref_noise)).item())
+
+        comp_y, comp_noise = torch.compile(fn, backend="inductor", fullgraph=True)(x)
+        self.assertTrue(torch.all(comp_noise < torch.ones_like(comp_noise)).item())
+
 
 # entries in here don't work and need to be fixed.
 # Each one of these is a bug (or needs to be investigated)
