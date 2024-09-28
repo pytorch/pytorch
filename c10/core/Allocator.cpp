6a987a80d48658db4a3ed4fbf6d2a8ef8375e530
cyy <cyyever@outlook.com>
2024-09-28 08:09:27 +0800

    Fix clang-tidy warnings

--- c10/core/Allocator.cpp
+++ c10/core/Allocator.cpp
@@ -1,5 +1,5 @@
-#include <array>
 #include <c10/core/Allocator.h>
+#include <array>
 
 #include <c10/util/ThreadLocalDebugInfo.h>
 
@@ -37,8 +37,10 @@ at::DataPtr InefficientStdFunctionContext::makeDataPtr(
       device};
 }
 
-static std::array<at::Allocator*,at::COMPILE_TIME_MAX_DEVICE_TYPES> allocator_array{};
-static std::array<uint8_t, at::COMPILE_TIME_MAX_DEVICE_TYPES> allocator_priority{};
+static std::array<at::Allocator*, at::COMPILE_TIME_MAX_DEVICE_TYPES>
+    allocator_array{};
+static std::array<uint8_t, at::COMPILE_TIME_MAX_DEVICE_TYPES>
+    allocator_priority{};
 
 void SetAllocator(at::DeviceType t, at::Allocator* alloc, uint8_t priority) {
   if (priority >= allocator_priority[static_cast<int>(t)]) {
