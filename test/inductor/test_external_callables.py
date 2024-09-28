5a01ead5aff95d7f93f9b752d8c9f8240500aebd
Jason Ansel <jansel@meta.com>
2024-09-28 14:46:16 -0700

    lints

--- test/inductor/test_external_callables.py
+++ test/inductor/test_external_callables.py
@@ -2,10 +2,8 @@
 import unittest
 
 import torch
-
 from torch._inductor import config
 from torch._inductor.test_case import run_tests, TestCase
-
 from torch.testing._internal.common_cuda import TEST_CUDA
 
 
