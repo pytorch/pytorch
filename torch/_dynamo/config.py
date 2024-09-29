0682359e68961361703577e27456963aa9a0a8e5
Bob Ren <bobren@fb.com>
2024-09-29 10:27:56 -0700

    Update on "remove use_lazy_graph_module flag"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -479,7 +479,8 @@ enable_compiler_collectives = os.environ.get("TORCH_COMPILER_COLLECTIVES", "0") 
 if TYPE_CHECKING:
     from torch.utils._config_typing import *  # noqa: F401, F403
 
-    def _make_closure_patcher(**changes): ...
+    def _make_closure_patcher(**changes):
+        ...
 
 
 from torch.utils._config_module import install_config_module
--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -21,6 +21,7 @@ import weakref
 from pathlib import Path
 from types import CodeType, FrameType, FunctionType, ModuleType
 from typing import Any, Callable, Dict, List, Optional, Set, TypeVar, Union
+from typing_extensions import ParamSpec
 from weakref import ReferenceType
 
 import torch
@@ -48,7 +49,6 @@ from torch.utils._python_dispatch import (
     is_in_torch_dispatch_mode,
 )
 from torch.utils._traceback import CapturedTraceback, format_traceback_short
-from typing_extensions import ParamSpec
 
 from . import config, exc, trace_rules
 from .bytecode_analysis import remove_dead_code, remove_pointless_jumps
@@ -335,9 +335,9 @@ def exception_handler(
 
 
 FRAME_COUNTER = 0
-FRAME_COMPILE_COUNTER: typing.Counter[Union[int, FrameStateSizeEntry]] = (
-    collections.Counter()
-)
+FRAME_COMPILE_COUNTER: typing.Counter[
+    Union[int, FrameStateSizeEntry]
+] = collections.Counter()
 
 
 def maybe_cprofile(func: Callable[_P, _T]) -> Callable[_P, _T]:
@@ -606,9 +606,9 @@ def _compile(
     output: Optional[OutputGraph] = None
     tracer: Optional[InstructionTranslator] = None
 
-    tf_mode_stack: List[torch.overrides.TorchFunctionMode] = (
-        torch.overrides._get_current_function_mode_stack()
-    )
+    tf_mode_stack: List[
+        torch.overrides.TorchFunctionMode
+    ] = torch.overrides._get_current_function_mode_stack()
 
     @preserve_global_state
     def transform(
@@ -1241,7 +1241,8 @@ class ConvertFrameProtocol(typing.Protocol):
         frame_state: Dict[str, Union[int, FrameStateSizeEntry]],
         *,
         skip: int = 0,
-    ) -> Optional[GuardedCode]: ...
+    ) -> Optional[GuardedCode]:
+        ...
 
 
 class CatchErrorsWrapper:
