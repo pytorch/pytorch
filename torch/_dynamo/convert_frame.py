d5a9c35e6e72c7db433437438249df493d87227a
Jovian Anthony Jaison <38627145+jovianjaison@users.noreply.github.com>
2024-09-30 14:41:19 -0700

    Update convert_frame.py

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -1086,7 +1086,7 @@ def _compile(
                 config.suppress_errors,
                 config.inline_inbuilt_nn_modules,
                 config.specialize_float,
-                json.dumps(config),
+                json.dumps(config.shallow_copy_dict()),
             )
             record_compilation_metrics(metrics)
             torch._dynamo.callback_handler.run_end_callbacks()
