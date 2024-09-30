a876b18b509ebec309be0b7719768752a2782248
Jovian Anthony Jaison <38627145+jovianjaison@users.noreply.github.com>
2024-09-29 21:54:55 -0700

    Update utils.py

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -805,6 +805,8 @@ class CompilationMetrics:
     remote_cache_time_saved_s: Optional[float]
     structured_logging_overhead_s: Optional[float]
     config_suppress_errors: Optional[bool]
+    specialize_float: Optional[bool]
+    dynamo_config: Optional[str]
 
 
 @dataclasses.dataclass
