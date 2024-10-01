1b30e77083e2681406fee44ab6afc8623f65ee04
Syed Tousif Ahmed <syeahmed@nvidia.com>
2024-09-30 21:31:17 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/cuda/memory.py
+++ torch/cuda/memory.py
@@ -999,7 +999,7 @@ class MemPool(_MemPool):
         r"""Returns the allocator this MemPool routes allocations to."""
         return super().allocator
 
-    def use_count(self):
+    def use_count(self) -> int:
         r"""Returns the reference count of this pool."""
         return super().use_count()
 
