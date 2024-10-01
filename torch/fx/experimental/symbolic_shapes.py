716b2450bf8a743dac4b845c205a7576d25feebf
rzou <zou3519@gmail.com>
2024-09-30 20:09:24 -0700

    Update base for Update on "[reland] Flip triton kernel default layout constraint to "needs_fixed_stride_order""
    This is to match the default layout constraint for custom operators. By
    default, Inductor should match the stride order of inputs to a triton
    kernel.
    
    IF THIS IS BREAKING YOU, PLEASE REACH OUT, especially if it's been
    more than two weeks since this landed. You can flip the config locally
    as a workaround.
    
    Test Plan:
    - existing tests
    
    [ghstack-poisoned]

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -252,8 +252,6 @@ def guard_size_oblivious(expr: Union[torch.SymBool, bool]) -> bool:
     if isinstance(expr, torch.SymBool):
         return expr.node.guard_size_oblivious("", 0)
     else:
-        if not isinstance(expr, bool):
-            breakpoint()
         assert isinstance(expr, bool), expr
         return expr
 
