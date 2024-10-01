b73a385b1d6ee6f8837504e5f7799b40227a22a5
Menglu Yu <mengluy@meta.com>
2024-09-30 17:09:10 -0700

    [PT2][Inductor] Add runtime numeric check for the post grad pass (#136724)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136724
    
    Similar to D51838043, we further add post grad runtime numeric check since some graph passes are performed at aten-level.
    
    more context
    https://fb.workplace.com/groups/1075192433118967/permalink/1357759604862247/
    
    Test Plan:
    # local reproduce
    
    ```
    CUDA_VISIBLE_DEVICES=3 OC_CAUSE=1 buck2 run mode/opt //scripts/jackiexu0313/pt2:local_model_with_pt2 -- --test_mode split-batch --flow_id 645018639 -n
    ```
    
    P1610951684
    
    Reviewed By: Yuzhen11
    
    Differential Revision: D63438718

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -1,5 +1,6 @@
 # mypy: allow-untyped-decorators
 # mypy: allow-untyped-defs
+import copy
 import functools
 import itertools
 import logging
@@ -49,6 +50,7 @@ from .b2b_gemm import B2B_GEMM_PASS
 from .ddp_fusion import fuse_ddp_communication
 from .group_batch_fusion import group_batch_fusion_passes, POST_GRAD_FUSIONS
 from .micro_pipeline_tp import micro_pipeline_tp_pass
+from .numeric_utils import enable_runtime_numeric_check
 from .pre_grad import is_same_dict, save_inductor_dict
 from .reinplace import reinplace_inplaceable_ops
 from .split_cat import POST_GRAD_PATTERNS
@@ -70,7 +72,9 @@ pass_patterns = [
 ]
 
 
-def post_grad_passes(gm: torch.fx.GraphModule, is_inference: bool):
+def post_grad_passes(
+    gm: torch.fx.GraphModule, is_inference: bool, example_inputs=None, fake_mode=None
+):
     """
     Passes that run on after grad.  This is called once on the forwards
     graph and once on the backwards graph.
@@ -97,6 +101,11 @@ def post_grad_passes(gm: torch.fx.GraphModule, is_inference: bool):
 
     if config.pattern_matcher:
         lazy_init()
+        gm_before_fx_passes = None
+        if hasattr(
+            config, "fx_passes_numeric_check"
+        ) and config.fx_passes_numeric_check.get("post_grad", False):
+            gm_before_fx_passes = copy.deepcopy(gm)
         optimus_scuba_log["before_recompile_post_grad"] = upload_graph(gm.graph)
         group_batch_fusion_passes(gm.graph, pre_grad=False)
         remove_noop_ops(gm.graph)
@@ -115,6 +124,11 @@ def post_grad_passes(gm: torch.fx.GraphModule, is_inference: bool):
                 optimus_scuba_log[
                     f"{pattern_matcher_pass.pass_name}_post_grad"
                 ] = upload_graph(gm.graph)
+        optimus_scuba_log["after_recompile_post_grad"] = upload_graph(gm.graph)
+        fx_passes_numeric_check = config.fx_passes_numeric_check.get("post_grad", False)
+        enable_runtime_numeric_check(
+            example_inputs, fake_mode, gm_before_fx_passes, gm, fx_passes_numeric_check
+        )
         if config.b2b_gemm_pass:
             B2B_GEMM_PASS.apply(gm.graph)  # type: ignore[arg-type]
 
@@ -148,7 +162,6 @@ def post_grad_passes(gm: torch.fx.GraphModule, is_inference: bool):
     comms.reinplace_fsdp_all_gather(gm.graph)
 
     gm.recompile()
-    optimus_scuba_log["after_recompile_post_grad"] = upload_graph(gm.graph)
     gm.graph.lint()
 
 
