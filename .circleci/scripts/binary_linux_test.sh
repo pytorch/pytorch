324d4e67982e0e40c998890f788b66e4955b9ca1
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-10-02 17:44:42 +0000

    Revert "Add py3.13t linux wheel build (#137127)"
    This reverts commit 6b7adc12140d3073c5700cc1c48998556489857e.
    
    Reverted https://github.com/pytorch/pytorch/pull/137127 on behalf of https://github.com/jovianjaison due to Sorry for reverting your changes but 2 jobs are failing ([comment](https://github.com/pytorch/pytorch/pull/137127#issuecomment-2389250791))

--- .circleci/scripts/binary_linux_test.sh
+++ .circleci/scripts/binary_linux_test.sh
@@ -27,13 +27,12 @@ if [[ "$PACKAGE_TYPE" == conda ]]; then
   source activate testenv >/dev/null
 elif [[ "$PACKAGE_TYPE" != libtorch ]]; then
   python_path="/opt/python/cp\$python_nodot-cp\${python_nodot}"
-
-  if [[ $DESIRED_PYTHON =~ ([0-9].[0-9]+)t ]]; then
-    python_digits="\$(echo $DESIRED_PYTHON | tr -cd [:digit:])"
-    python_path="/opt/python/cp\${python_digits}-cp\${python_digits}t"
+  # Prior to Python 3.8 paths were suffixed with an 'm'
+  if [[ -d  "\${python_path}/bin" ]]; then
+    export PATH="\${python_path}/bin:\$PATH"
+  elif [[ -d "\${python_path}m/bin" ]]; then
+    export PATH="\${python_path}m/bin:\$PATH"
   fi
-  export PATH="\${python_path}/bin:\$PATH"
-
 fi
 
 EXTRA_CONDA_FLAGS=""
