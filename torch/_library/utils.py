3bf08fedb00bf9d37ae32e1f82822b96725cd693
Laith Sakka <lsakka@meta.com>
2024-10-03 15:22:07 -0700

    Proper habdle of args in kwargs in zip_schema
    [ghstack-poisoned]

--- torch/_library/utils.py
+++ torch/_library/utils.py
@@ -193,6 +193,8 @@ def zip_schema(
                 yield info, kwargs[info.name]
             continue
         if i >= len(args):
+            if not info.kwarg_only and  info.name in kwargs:
+                yield info, kwargs[info.name]
             # args that are equal to their default values are not populated
             # if they are followed by args that are equal to their defaults.
             # Skip these.
