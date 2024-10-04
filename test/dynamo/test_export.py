6bf3d74a768685b7dd8b833b8b14d2136facefa6
Henry Tsang <henrylhtsang@meta.com>
2024-10-04 10:33:02 -0700

    https://github.com/pytorch/pytorch/pull/134902 (#137353)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/137353
    
    Test Plan:
    ```
    buck2 test 'fbcode//mode/opt' fbcode//caffe2/test/dynamo:test_dynamo -- --exact 'caffe2/test/dynamo:test_dynamo - test_export.py::ExportTests::test_export_masking_with_no_grad'
    ```
    
    Differential Revision: D63862380

--- test/dynamo/test_export.py
+++ test/dynamo/test_export.py
@@ -4565,10 +4565,7 @@ def forward(self, x, b, y):
     return pytree.tree_unflatten([x], self._out_spec)""",  # NOQA: B950
         )
 
-        with self.assertRaisesRegex(
-            torch._dynamo.exc.Unsupported, "boolean masking setitem backwards"
-        ):
-            gm, _ = torch._dynamo.export(fn)(x, b, y)
+        gm, _ = torch._dynamo.export(fn)(x, b, y)
 
     def test_dynamo_list_index(self):
         def fn(x, in_list):
