14e33b88619539148bd6716db06fe99cdacce137
Jeff Daily <jeff.daily@amd.com>
2024-10-04 19:36:52 +0000

    remove redundant code, smaller diff

--- aten/src/ATen/Context.cpp
+++ aten/src/ATen/Context.cpp
@@ -290,11 +290,12 @@ at::BlasBackend Context::blasPreferredBackend() {
 #ifdef USE_ROCM
   if (blas_preferred_backend == at::BlasBackend::Cublaslt) {
     static const bool hipblaslt_unsupported = []() {
+      static const std::vector<std::string> archs = {
+	      "gfx90a", "gfx940", "gfx941", "gfx942",
 #if ROCM_VERSION >= 60200
-      static const std::vector<std::string> archs = {"gfx90a", "gfx940", "gfx941", "gfx942", "gfx1100", "gfx1101"};
-#else
-      static const std::vector<std::string> archs = {"gfx90a", "gfx940", "gfx941", "gfx942"};
+	      "gfx1100", "gfx1101"
 #endif
+      };
       for (auto index: c10::irange(getNumGPUs())) {
         if (!detail::getCUDAHooks().isGPUArch(index, archs)) {
           TORCH_WARN_ONCE(
