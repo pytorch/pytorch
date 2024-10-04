8bd85ce3c1628bb0baaf4ab3bc678750c7df933b
Jeff Daily <jeff.daily@amd.com>
2024-10-04 15:36:29 +0000

    raw_alloc ignores PYTORCH_NO_CUDA_MEMORY_CACHING (#131114)
    raw_alloc is used by cudnn, miopen, thrust, and tunableop.  Without this PR, the env var for disabling the caching allocator will only partially work.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131114
    Approved by: https://github.com/eqy, https://github.com/houseroad, https://github.com/albanD
    
    Co-authored-by: Nichols A. Romero <nick.romero@amd.com>

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -3770,6 +3770,43 @@ class TestCudaMallocAsync(TestCase):
                 "pinned_num_register_threads:1024"
             )
 
+    def test_cachingAllocator_raw_alloc(self):
+        # Test that raw_alloc respects the setting that
+        # activates/deactivates the caching allocator
+
+        # Helper function that calls raw_alloc and returns
+        # relevant field in data structure
+        def requested_bytes_alloc_stats(raw_alloc_size, stream):
+            start = torch.cuda.memory_stats()["requested_bytes.all.allocated"]
+            torch._C._cuda_cudaCachingAllocator_raw_alloc(raw_alloc_size, stream)
+            finish = torch.cuda.memory_stats()["requested_bytes.all.allocated"]
+            return finish - start
+
+        torch.cuda.empty_cache()
+        device = torch._C._cuda_getDevice()
+        stream = torch._C._cuda_getCurrentRawStream(device)
+        torch._C._cuda_resetAccumulatedMemoryStats(device)
+
+        # size of allocation
+        raw_alloc_size = 1024 * 1024  # 1 MB
+
+        try:
+            # Deactivate the caching allocator
+            torch.cuda.caching_allocator_enable(False)
+
+            # For a deactivated caching allocator, result is zero
+            cuda_alloc_size = requested_bytes_alloc_stats(raw_alloc_size, stream)
+            self.assertEqual(cuda_alloc_size, 0)
+
+        finally:
+            # Make sure we get back to the default state that is
+            # an activated caching allocator
+            torch.cuda.caching_allocator_enable(True)
+
+            # For an active caching allocator, result matches raw_alloc_size
+            cuda_alloc_size = requested_bytes_alloc_stats(raw_alloc_size, stream)
+            self.assertEqual(cuda_alloc_size, raw_alloc_size)
+
     @parametrize("max_split_size_mb_setting", [False, True])
     def test_raises_oom(self, max_split_size_mb_setting):
         if max_split_size_mb_setting:
