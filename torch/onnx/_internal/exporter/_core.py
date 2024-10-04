c7bf7468dea18d07e5844bddb90fad8546fcd6c3
Xavier Dupré <xadupre@users.noreply.github.com>
2024-10-04 20:36:41 +0200

    Update torch/onnx/_internal/exporter/_core.py
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

--- torch/onnx/_internal/exporter/_core.py
+++ torch/onnx/_internal/exporter/_core.py
@@ -725,7 +725,7 @@ def _prepare_exported_program_for_export(
     """Decompose and apply pre-export transformations to the exported program."""
     # Before decomposing, we search for the subsequence transpose + view and insert
     # a node flatten in between to bypass the wrong decomposition.
-    # See issue https://github.com/pytorch/pytorch/issues/136543
+    # Remove before 2.6 release and after issue https://github.com/pytorch/pytorch/issues/136543 is fixed.
     exported_program = _fx_passes.insert_flatten_between_transpose_and_view(
         exported_program
     )
