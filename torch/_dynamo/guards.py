000fd50d9aa666b20d041016bce5f2954b4100de
Michael Lazos <mlazos@meta.com>
2024-10-06 15:58:40 -0700

    [Dynamo] Remove ignored modes workaround (#135502) (#137115)
    Approved by: https://github.com/anijain2305
    ghstack dependencies: #134732, #133137, #135443, #135444, #135422
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137115
    Approved by: https://github.com/yanboliang
    ghstack dependencies: #137114

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -2356,15 +2356,13 @@ class CheckFunctionManager:
         )
 
         if config.enable_cpp_guard_manager:
-            from .variables.torch_function import IGNORED_MODES
-
             # Insert the global_state guard
             assert self.guard_manager  # to make mypy happy
             self.guard_manager.root.add_global_state_guard(["___check_global_state()"])
 
             self.guard_manager.root.add_torch_function_mode_stack_guard(
                 self.torch_function_mode_stack,
-                list(IGNORED_MODES),
+                list(),
                 ["___check_torch_function_mode_stack()"],
             )
             # Clear references to torch_function modes held in the list
@@ -2671,18 +2669,14 @@ def is_recompiles_verbose_enabled():
 # this will only be used if cpp guards are disabled
 def make_torch_function_mode_stack_guard(intial_stack):
     types = [type(x) for x in intial_stack]
-    from .variables.torch_function import IGNORED_MODES
 
     def check_torch_function_mode_stack():
         cur_stack = get_torch_function_mode_stack()
 
-        types_ = [ty for ty in types if ty not in IGNORED_MODES]
-        cur_stack_ = [mode for mode in cur_stack if type(mode) not in IGNORED_MODES]
-
-        if len(cur_stack_) != len(types_):
+        if len(cur_stack) != len(types):
             return False
 
-        for ty, mode in zip(types_, cur_stack_):
+        for ty, mode in zip(types, cur_stack):
             if ty != type(mode):
                 return False
 
