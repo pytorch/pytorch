abc71565ad0b104bd06ba496a8da671721a82d39
xadupre <xadupre@microsoft.com>
2024-10-07 19:13:17 +0200

    lint

--- torch/onnx/_internal/exporter/_fx_passes.py
+++ torch/onnx/_internal/exporter/_fx_passes.py
@@ -79,7 +79,9 @@ def insert_contiguous_between_transpose_and_view(
 
         modified = True
         with graph.inserting_after(node):
-            new_node = graph.call_function(torch.ops.aten.contiguous.default, args=(node,))
+            new_node = graph.call_function(
+                torch.ops.aten.contiguous.default, args=(node,)
+            )
             node.replace_all_uses_with(new_node)
             # new_node is replaced as well so we manually revert the replacement
             new_node.update_arg(0, node)
