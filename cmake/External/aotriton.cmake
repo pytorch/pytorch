2c6da22da29703cf4d58caff7c379d2659f44162
Xinya Zhang <Xinya.Zhang@amd.com>
2024-10-07 20:35:29 +0000

    Merge aotriton_version.txt into aotriton.cmake

--- cmake/External/aotriton.cmake
+++ cmake/External/aotriton.cmake
@@ -16,6 +16,28 @@ if(NOT __AOTRITON_INCLUDED)
   set(__AOTRITON_EXTERN_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/aotriton")
   set(__AOTRITON_INSTALL_DIR "${PROJECT_SOURCE_DIR}/torch")
   add_library(__caffe2_aotriton INTERFACE)
+
+  # AOTriton package information from GitHub Release Pages
+  # Replaces .ci/docker/aotriton_version.txt
+  set(__AOTRITON_VER "0.7b")
+  list(JOIN
+       "manylinux_2_17"  # rocm6.1
+       "manylinux_2_17"  # rocm6.2
+       ";"
+       __AOTRITON_MANYLINUX_LIST)
+  list(JOIN
+       "rocm6.1"
+       "rocm6.2"
+       ";"
+       __AOTRITON_ROCM_LIST)
+  set(__AOTRITON_CI_COMMIT "9be04068c3c0857a4cfd17d7e39e71d0423ebac2")
+  list(JOIN
+       "006f4d982c9a9c768f31f0095128705fecb792136827e2456241fe79764de7a4"  # rocm6.1
+       "3e9e1959d23b93d78a08fcc5f868125dc3854dece32fd9458be9ef4467982291"  # rocm6.2
+       ";"
+       __AOTRITON_SHA256_LIST)
+  set(__AOTRITON_Z "gz")
+
   # Note it is INSTALL"ED"
   if(DEFINED ENV{AOTRITON_INSTALLED_PREFIX})
     install(DIRECTORY
@@ -25,10 +47,6 @@ if(NOT __AOTRITON_INCLUDED)
     set(__AOTRITON_INSTALL_DIR "$ENV{AOTRITON_INSTALLED_PREFIX}")
     message(STATUS "Using Preinstalled AOTriton at ${__AOTRITON_INSTALL_DIR}")
   elseif(DEFINED ENV{AOTRITON_INSTALL_FROM_SOURCE})
-    file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/.ci/docker/aotriton_version.txt" __AOTRITON_CI_INFO)
-    list(GET __AOTRITON_CI_INFO 3 __AOTRITON_CI_COMMIT)
-    set(target_gpus "")
-    get_target_gpus_from_pytorch(target_gpus)
     ExternalProject_Add(aotriton_external
       GIT_REPOSITORY https://github.com/ROCm/aotriton.git
       GIT_TAG ${__AOTRITON_CI_COMMIT}
@@ -53,17 +71,27 @@ if(NOT __AOTRITON_INCLUDED)
     add_dependencies(__caffe2_aotriton aotriton_external)
     message(STATUS "Using AOTriton compiled from source directory ${__AOTRITON_EXTERN_PREFIX}")
   else()
-    file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/.ci/docker/aotriton_version.txt" __AOTRITON_CI_INFO)
-    list(GET __AOTRITON_CI_INFO 0 __AOTRITON_VER)
-    list(GET __AOTRITON_CI_INFO 1 __AOTRITON_MANYLINUX)
-    list(GET __AOTRITON_CI_INFO 2 __AOTRITON_ROCM)
-    list(GET __AOTRITON_CI_INFO 3 __AOTRITON_COMMIT)
-    list(GET __AOTRITON_CI_INFO 4 __AOTRITON_SHA256)
+    list(GET __AOTRITON_ROCM_LIST 0 __AOTRITON_ROCM_LOW_STR)
+    list(GET __AOTRITON_ROCM_LIST -1 __AOTRITON_ROCM_HIGH_STR)
+    # len("rocm") == 4
+    string(SUBSTRING ${__AOTRITON_ROCM_LOW_STR} 4 -1 __AOTRITON_ROCM_LOW)
+    string(SUBSTRING ${__AOTRITON_ROCM_HIGH_STR} 4 -1 __AOTRITON_ROCM_HIGH)
+    set(__AOTRITON_SYSTEM_ROCM "${ROCM_VERSION_DEV_MAJOR}.${ROCM_VERSION_DEV_MINOR}")
+    if(__AOTRITON_SYSTEM_ROCM VERSION_LESS __AOTRITON_ROCM_LOW)
+      set(__AOTRITON_ROCM ${__AOTRITON_ROCM_LOW})
+    elseif(__AOTRITON_SYSTEM_ROCM VERSION_GREATER __AOTRITON_ROCM_HIGH)
+      set(__AOTRITON_ROCM ${__AOTRITON_ROCM_HIGH})
+    else()
+      set(__AOTRITON_ROCM ${__AOTRITON_SYSTEM_ROCM})
+    endif()
+    list(FIND __AOTRITON_ROCM_LIST "rocm${__AOTRITON_ROCM}" __AOTRITON_ROCM_INDEX)
+    list(GET __AOTRITON_SHA256_LIST ${__AOTRITON_ROCM_INDEX} __AOTRITON_SHA256)
+    list(GET __AOTRITON_MANYLINUX_LIST ${__AOTRITON_ROCM_INDEX} __AOTRITON_MANYLINUX)
     set(__AOTRITON_ARCH ${CMAKE_HOST_SYSTEM_PROCESSOR})
     string(CONCAT __AOTRITON_FILE "aotriton-"
                                   "${__AOTRITON_VER}-${__AOTRITON_MANYLINUX}"
-                                  "_${__AOTRITON_ARCH}-${__AOTRITON_ROCM}"
-                                  "-shared.tar.gz")
+                                  "_${__AOTRITON_ARCH}-rocm${__AOTRITON_ROCM}"
+                                  "-shared.tar.${__AOTRITON_Z}")
     string(CONCAT __AOTRITON_URL "https://github.com/ROCm/aotriton/releases/download/"
                                  "${__AOTRITON_VER}/${__AOTRITON_FILE}")
     ExternalProject_Add(aotriton_external
