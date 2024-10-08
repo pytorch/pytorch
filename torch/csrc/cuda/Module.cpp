14e6dbfec4b23e5d6ee9e05a6c96ba828be7b192
cyy <cyyever@outlook.com>
2024-10-08 19:05:00 +0000

    [2/N] Fix extra warnings brought by clang-tidy-17 (#137459)
    Follows #137407
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137459
    Approved by: https://github.com/Skylion007

--- torch/csrc/cuda/Module.cpp
+++ torch/csrc/cuda/Module.cpp
@@ -1266,8 +1266,7 @@ static void registerCudaPluggableAllocator(PyObject* module) {
   m.def(
       "_tensors_data_ptrs_at_indices_equal",
       [](py::list& tensors, py::list& data_ptrs, py::list& indices) {
-        for (size_t i = 0, end = indices.size(); i < end; ++i) {
-          auto index = indices[i].cast<int64_t>();
+        for (auto index : indices) {
           auto t = tensors[index].cast<at::Tensor>();
           auto data_ptr = data_ptrs[index].cast<int64_t>();
           if (reinterpret_cast<int64_t>(t.data_ptr()) != data_ptr) {
@@ -1451,7 +1450,6 @@ PyObject* THCPModule_getCurrentBlasHandle_wrap(
     PyObject* self,
     PyObject* noargs) {
   HANDLE_TH_ERRORS
-  // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
   cublasHandle_t handle = at::cuda::getCurrentCUDABlasHandle();
   return PyLong_FromVoidPtr(handle);
   END_HANDLE_TH_ERRORS
