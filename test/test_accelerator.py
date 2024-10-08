2b77d645c81ed41c41668700fede93c477b2303f
Yu, Guangye <guangye.yu@intel.com>
2024-10-08 22:42:03 +0000

    Update
    [ghstack-poisoned]

--- test/test_accelerator.py
+++ test/test_accelerator.py
@@ -1,69 +1,93 @@
 # Owner(s): ["module: tests"]
 
+import sys
 import unittest
 
 import torch
 from torch.testing._internal.common_utils import (
+    NoTest,
     run_tests,
+    TEST_ACCELERATOR,
     TEST_CUDA,
     TEST_XPU,
     TestCase,
 )
 
 
-TEST_ACCELERATOR = TEST_CUDA or TEST_XPU
+if not TEST_ACCELERATOR:
+    assert torch.acc.current_accelerator() == "cpu" "No available accelerator detected."
+    print("No available accelerator detected, skipping tests", file=sys.stderr)
+    TestCase = NoTest  # noqa: F811
+
+HAS_MULTIACC = torch.acc.device_count() > 1
 
 
 class TestAccelerator(TestCase):
     def test_generic_device_behavior(self):
-        if not TEST_ACCELERATOR:
-            self.assertIsNone(torch.current_accelerator())
-            return
-        current_device = torch.current_device()
-        torch.set_device(current_device)
-        self.assertEqual(current_device, torch.current_device())
+        self.assertTrue(torch.acc.is_available())
+        orig_device = torch.acc.current_device()
+        torch.acc.set_device(orig_device)
+        self.assertEqual(orig_device, torch.acc.current_device())
         if torch.cuda.is_available():
-            self.assertTrue(torch.has_accelerator())
-            self.assertEqual(torch.current_accelerator(), "cuda")
+            self.assertEqual(torch.acc.current_accelerator(), "cuda")
+            with self.assertRaisesRegex(
+                ValueError, "doesn't match the current accelerator"
+            ):
+                torch.acc.set_device("xpu")
         if torch.xpu.is_available():
-            self.assertTrue(torch.has_accelerator())
-            self.assertEqual(torch.current_accelerator(), "xpu")
+            self.assertEqual(torch.acc.current_accelerator(), "xpu")
 
-    @unittest.skipIf(not TEST_ACCELERATOR, "no avaliable accelerators detected")
+    @unittest.skipIf(not HAS_MULTIACC, "requires multiple accelerators")
     def test_generic_multi_device_behavior(self):
-        current_device = torch.current_device()
-        target_device = (current_device + 1) % torch.device_count()
+        orig_device = torch.acc.current_device()
+        target_device = (orig_device + 1) % torch.acc.device_count()
 
-        with torch.DeviceGuard(target_device):
-            self.assertEqual(target_device, torch.current_device())
-        self.assertEqual(current_device, torch.current_device())
+        with torch.acc.DeviceGuard(target_device):
+            self.assertEqual(target_device, torch.acc.current_device())
+        self.assertEqual(orig_device, torch.acc.current_device())
 
         s1 = torch.Stream(target_device)
-        torch.set_stream(s1)
-        self.assertEqual(target_device, torch.current_device())
+        torch.acc.set_stream(s1)
+        self.assertEqual(target_device, torch.acc.current_device())
+        torch.acc.synchronize(orig_device)
+        self.assertEqual(target_device, torch.acc.current_device())
 
-    @unittest.skipIf(not TEST_ACCELERATOR, "no avaliable accelerators detected")
     def test_generic_stream_behavior(self):
         s1 = torch.Stream()
         s2 = torch.Stream()
-        torch.set_stream(s1)
-        self.assertEqual(torch.current_stream(), s1)
+        torch.acc.set_stream(s1)
+        self.assertEqual(torch.acc.current_stream(), s1)
         event = torch.Event()
         a = torch.randn(100)
         b = torch.randn(100)
         c = a + b
-        with torch.StreamGuard(s2):
-            self.assertEqual(torch.current_stream(), s2)
-            a_acc = a.to(torch.current_accelerator(), non_blocking=True)
-            b_acc = b.to(torch.current_accelerator(), non_blocking=True)
-        self.assertEqual(torch.current_stream(), s1)
+        with torch.acc.StreamGuard(s2):
+            self.assertEqual(torch.acc.current_stream(), s2)
+            a_acc = a.to(torch.acc.current_accelerator(), non_blocking=True)
+            b_acc = b.to(torch.acc.current_accelerator(), non_blocking=True)
+        self.assertEqual(torch.acc.current_stream(), s1)
         event.record(s2)
         event.synchronize()
         c_acc = a_acc + b_acc
         event.record(s2)
-        torch.synchronize()
+        torch.acc.synchronize()
         self.assertTrue(event.query())
         self.assertEqual(c_acc.cpu(), c)
+
+    @unittest.skip((not TEST_CUDA) and (not TEST_XPU), "requires CUDA or XPU")
+    def test_device_specific_stream_behavior(self):
+        s1 = torch.cuda.Stream() if torch.cuda.is_available() else torch.xpu.Stream()
+        s2 = torch.cuda.Stream() if torch.cuda.is_available() else torch.xpu.Stream()
+        torch.acc.set_stream(s1)
+        self.assertEqual(torch.acc.current_stream(), s1)
+        with torch.acc.StreamGuard(s2):
+            self.assertEqual(torch.acc.current_stream(), s2)
+        self.assertEqual(torch.acc.current_stream(), s1)
+        if torch.cuda.is_available():
+            with self.assertRaisesRegex(
+                RuntimeError, "doesn't match the current accelerator"
+            ):
+                torch.acc.set_stream(torch.xpu.Stream())
 
 
 if __name__ == "__main__":
