fix: overflow in torch.linalg.matrix_norm

test/test_linalg.py
@@ -1392,6 +1392,10 @@ class TestLinalg(TestCase):
         make_arg = partial(make_tensor, dtype=dtype, device=device)
 
         def run_test_case(input, ord, dim, keepdim):
+            if isinstance(ord, complex):
+                error_msg = "Expected a non-complex scalar"
+                with self.assertRaisesRegex(RuntimeError, error_msg):
+                    torch.linalg.matrix_norm(input, ord, dim, keepdim)
             msg = f'input.size()={input.size()}, ord={ord}, dim={dim}, keepdim={keepdim}, dtype={dtype}'
             result = torch.linalg.norm(input, ord, dim, keepdim)
             input_numpy = input.cpu().numpy()
@@ -1403,7 +1407,7 @@ class TestLinalg(TestCase):
                 result = torch.linalg.matrix_norm(input, ord, dim, keepdim)
                 self.assertEqual(result, result_numpy, msg=msg)
 
-        ord_matrix = [1, -1, 2, -2, inf, -inf, 'nuc', 'fro']
+        ord_matrix = [1, -1, 2, -2, inf, -inf, 'nuc', 'fro', 1 + 2j]
         S = 10
         test_cases = [
             # input size, dim