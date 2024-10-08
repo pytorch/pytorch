b577686a2f8d3391319a6ef3988f673be79f27b5
Yu, Guangye <guangye.yu@intel.com>
2024-10-08 09:23:30 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/accelerator/__init__.py
+++ torch/accelerator/__init__.py
@@ -1,33 +1,45 @@
+# mypy: allow-untyped-defs
 from typing import Any
+
 import torch
+
 from ._utils import _device_t, _get_device_index
+
 
 def current_accelerator() -> str:
     return torch._C._accelerator_getAccelerator()
 
+
 def device_count() -> int:
     return torch._C._accelerator_deviceCount()
+
 
 def is_available() -> bool:
     return device_count() > 0
 
+
 def current_device() -> int:
     return torch._C._accelerator_getDevice()
 
+
 def set_device(device: _device_t) -> None:
     device_index = _get_device_index(device)
-    return torch._C._accelerator_setDevice(device_index)
+    torch._C._accelerator_setDevice(device_index)
+
 
 def current_stream(device: _device_t = None) -> torch.Stream:
     device_index = _get_device_index(device, True)
     return torch._C._accelerator_getStream(device_index)
 
+
 def set_stream(stream: torch.Stream) -> None:
-    return torch._C._accelerator_setStream(stream)
+    torch._C._accelerator_setStream(stream)
+
 
 def synchronize(device: _device_t = None) -> None:
     device_index = _get_device_index(device, True)
-    return torch._C._accelerator_synchronizeDevice(device_index)
+    torch._C._accelerator_synchronizeDevice(device_index)
+
 
 class DeviceGuard:
     def __init__(self, device_index: int):
