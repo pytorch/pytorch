05348be994ae1fc1dddfb4d3734f6fb910b3f13c
Edward Z. Yang <ezyang@meta.com>
2024-10-08 14:24:32 +0000

    Introduce torch.sym_sum (#136429)
    Partially addresses https://github.com/pytorch/pytorch/issues/128150
    
    When you have big sums of values, we end up computing long chains of
    binary addition in our FX graph representation.  Not only is this ugly,
    it also is quadratic, as the sympy.Add constructor is O(N) in number
    of arguments.  Instead, ensure that we maintain the summation as a
    single FX node so we can do the entire addition all in one go.
    
    update_hint_regression benchmark, before and after:
    
    ```
    update_hint_regression,compile_time_instruction_count,2648328980
    update_hint_regression,compile_time_instruction_count,2563748678
    ```
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136429
    Approved by: https://github.com/isuruf

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -1319,6 +1319,7 @@ class OutputGraph:
                     fx.GraphModule(root, self.graph),
                     self.shape_env,
                     name,
+                    export=self.export,
                 )
             # NB: deferred runtime asserts can keep graphargs live, so make sure
             # those are inserted before pruning
