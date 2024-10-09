b5690a95b46d132a7050175d2f5465965338ecde
Animesh Jain <anijain@umich.edu>
2024-10-09 11:46:57 -0700

    [hierarchical-compilation][inductor] Lower invoke_subgraph
    [ghstack-poisoned]

--- test/higher_order_ops/test_invoke_subgraph.py
+++ test/higher_order_ops/test_invoke_subgraph.py
@@ -172,7 +172,7 @@ class TestInvokeSubgraphCompile(TestCase):
 
         x_clone = x.clone().detach().requires_grad_(True)
         y_clone = y.clone().detach().requires_grad_(True)
-        res = torch.compile(fn, backend="aot_eager")(x_clone, y_clone)
+        res = torch.compile(fn, backend="inductor")(x_clone, y_clone)
 
         # Run backward
         ref.sum().backward()
@@ -196,7 +196,7 @@ class TestInvokeSubgraphCompile(TestCase):
 
         x_clone = x.clone().detach().requires_grad_(True)
         y_clone = y.clone().detach().requires_grad_(True)
-        res = torch.compile(fn, backend="aot_eager")(x_clone, y_clone)
+        res = torch.compile(fn, backend="inductor")(x_clone, y_clone)
 
         # Run backward
         ref.sum().backward()
@@ -227,7 +227,7 @@ class TestInvokeSubgraphCompile(TestCase):
 
         x_clone = x.clone().detach().requires_grad_(True)
         y_clone = y.clone().detach().requires_grad_(True)
-        res = torch.compile(fn, backend="aot_eager")(x_clone, y_clone)
+        res = torch.compile(fn, backend="inductor")(x_clone, y_clone)
 
         # Run backward
         ref.sum().backward()
