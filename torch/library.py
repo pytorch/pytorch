a410b6250d1f61e7cf935b4907f0f1e8bc877e20
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2024-10-09 15:11:11 -0700

    Update on "Fix custom op bug of clearing dir"
    Previously when we delete a custom op out of context manager, we weren't clearing the dir field of the op namespace. As a result, it was polluting other tests. 
    
    Differential Revision: [D64141465](https://our.internmc.facebook.com/intern/diff/D64141465/)
    
    [ghstack-poisoned]

--- torch/library.py
+++ torch/library.py
@@ -1,4 +1,4 @@
-# mypy: allow-untyped-defs
+caf# mypy: allow-untyped-defs
 import contextlib
 import functools
 import inspect
@@ -424,6 +424,7 @@ class Library:
             if not hasattr(namespace, name):
                 continue
             delattr(namespace, name)
+            namespace._dir.remove(name)
 
 
 def _del_library(
