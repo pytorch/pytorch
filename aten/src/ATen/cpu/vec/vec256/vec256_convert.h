35de4dd531962607366d6186027694a22e845689
Scott Wolchok <swolchok@fb.com>
2024-10-09 15:38:49 -0700

    [PyTorch] Move NEON VecConvert specialization from vec256_convert to vec128_convert
    NEON vectors are 128-bit and don't belong with 256 stuff.
    
    Differential Revision: [D64143615](https://our.internmc.facebook.com/intern/diff/D64143615/)
    
    [ghstack-poisoned]

--- aten/src/ATen/cpu/vec/vec256/vec256_convert.h
+++ aten/src/ATen/cpu/vec/vec256/vec256_convert.h
@@ -297,39 +297,8 @@ struct VecConvert<
     return convert_int8_to_float<src_t>(src[0]);
   }
 };
-#elif defined(CPU_CAPABILITY_NEON)
-template <typename src_t>
-struct VecConvert<
-    float,
-    2,
-    src_t,
-    1,
-    typename std::enable_if_t<is_8bit_integer_v<src_t>,
-        void>> {
-  static inline VectorizedN<float, 2> apply(const VectorizedN<src_t, 1>& src) {
-    const auto [v0, v1] = convert_int8_to_float<src_t>(src[0]);
-    return VectorizedN<float, 2>(v0, v1);
-  }
-};
 #endif
 
-#if defined(CPU_CAPABILITY_NEON)
-template <>
-struct VecConvert<float, 2, BFloat16, 1> {
-  static inline VectorizedN<float, 2> apply(
-      const VectorizedN<BFloat16, 1>& src) {
-    VectorizedN<float, 2> result;
-    uint16x8_t u16_8 = vld1q_u16(reinterpret_cast<const uint16_t*>(&src[0]));
-    auto u16_low1 = vget_low_u16(u16_8);
-    auto u16_high1 = vget_high_u16(u16_8);
-    float32x4_t f32x4_0 = vreinterpretq_f32_u32(vshlq_n_u32(vmovl_u16(u16_low1), 16));
-    float32x4_t f32x4_1 = vreinterpretq_f32_u32(vshlq_n_u32(vmovl_u16(u16_high1), 16));
-    result[0] = f32x4_0;
-    result[1] = f32x4_1;
-    return result;
-  }
-};
-#endif
 
 template <typename src_t>
 struct VecConvert<
