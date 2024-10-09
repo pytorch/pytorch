b5eaaea3206447e5d14848154916ecfb88764e4f
Jez Ng <jezng@fb.com>
2024-10-09 10:58:32 -0700

    Have Triton custom extension test use privateuseone device
    The original PR #122396 used the CPU device since at that point in time
    there was no actual Triton CPU backend. After #133408, this is no longer
    the case, so we now have multiple backends getting registered for the
    CPU. The test still works in OSS but fails internally due to different
    test runners initializing the backends in a different order.
    
    This PR doesn't actually end up fixing the test internally because
    cpp_extension -- needed to implement the privateuseone device -- isn't
    supported there, so we simply skip it instead. However, it still makes the
    OSS test independent of initialization order, which is good.
    
    Differential Revision: [D63838169](https://our.internmc.facebook.com/intern/diff/D63838169/)
    
    [ghstack-poisoned]

--- test/inductor/test_extension_backend.py
+++ test/inductor/test_extension_backend.py
@@ -48,8 +48,7 @@ run_and_get_cpp_code = test_torchinductor.run_and_get_cpp_code
 TestCase = test_torchinductor.TestCase
 
 
-@unittest.skipIf(IS_FBCODE, "cpp_extension doesn't work in fbcode right now")
-class ExtensionBackendTests(TestCase):
+class BaseExtensionBackendTests(TestCase):
     module = None
 
     @classmethod
@@ -95,6 +94,9 @@ class ExtensionBackendTests(TestCase):
         # return the working directory (see setUp)
         os.chdir(self.old_working_dir)
 
+
+@unittest.skipIf(IS_FBCODE, "cpp_extension doesn't work in fbcode right now")
+class ExtensionBackendTests(BaseExtensionBackendTests):
     def test_open_device_registration(self):
         torch.utils.rename_privateuse1_backend("extension_device")
         torch._register_device_module("extension_device", self.module)
