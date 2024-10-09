4b3294db216dc700ef785b3c90a1d4fb5e763c49
Shuqiang Zhang <sqzhang@meta.com>
2024-10-08 23:29:08 -0700

    Update on "[c10d] simplify barrier implementation and further decouple CPU/GPU"
    synchronization
    Summary:
    Barrier is  essentially intended to block CPU thread (instead of GPU
    streams). Before we used 2 stream synchronizations (1. current stream
    blocked by nccl stream end event, 2. CPU thread blocked on current
    stream). This is unnecessary as we already have CPU thread blocking
    logic in wait(). Also, adding barrier specific code block in the general
    GPU synchronize() API is intrusive and confusing.
    
    This PR cleans this.
    
    Test Plan:
    CI
    
    Tags:
    
    cc XilunWu H-Huang awgu kwen2501 wanchaol fegin fduwjj wz337 wconstab d4l3k c-p-i-o
    
    [ghstack-poisoned]

--- test/cpp/c10d/ProcessGroupNCCLErrorsTest.cpp
+++ test/cpp/c10d/ProcessGroupNCCLErrorsTest.cpp
@@ -320,6 +320,10 @@ TEST_F(ProcessGroupNCCLErrorsTest, testNCCLTimedoutErrorsBlocking) {
 }
 
 TEST_F(ProcessGroupNCCLErrorsTest, testNCCLErrorsNonBlocking) {
+  // Avoid watchdog thread to throw the exception first to test the barrier
+  // throw behavior.
+  ASSERT_TRUE(
+      setenv(c10d::TORCH_NCCL_ASYNC_ERROR_HANDLING[0].c_str(), "0", 1) == 0);
   auto options = c10d::ProcessGroupNCCL::Options::create();
   options->timeout = std::chrono::milliseconds(3000);
   ProcessGroupNCCLSimulateErrors pg(store_, 0, 1, options);
@@ -332,12 +336,10 @@ TEST_F(ProcessGroupNCCLErrorsTest, testNCCLErrorsNonBlocking) {
   pg.simulateError();
   work = pg.allreduce(tensors_);
 
-  // Should not throw exceptions.
   work->wait();
-  pg.barrier()->wait();
-
-  EXPECT_TRUE(work->isCompleted());
-  // Communicators might be aborted here, further operations would fail.
+  // an NCCL ERROR happened before should stop the thread from passing the
+  // barrier.
+  EXPECT_THROW(pg.barrier()->wait(), std::runtime_error);
 }
 
 // Function to read what we wrote to the local disk for validation.
