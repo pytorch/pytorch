99110565d20c54716bf0e566d668a93692877149
yanbing-j <yanbing.jiang@intel.com>
2024-10-09 12:57:57 +0800

    Fix lint error

--- c10/core/Device.cpp
+++ c10/core/Device.cpp
@@ -37,8 +37,9 @@ DeviceType parse_type(const std::string& device_string) {
           {"privateuseone", DeviceType::PrivateUse1},
       }};
   if (device_string == "mkldnn") {
-    TORCH_WARN("torch.device('mkldnn') is deprecated and will be removed in the future."
-               "Please use torch.device('cpu') instead.");
+    TORCH_WARN(
+        "torch.device('mkldnn') is deprecated and will be removed in the future."
+        "Please use torch.device('cpu') instead.");
   }
   auto device = std::find_if(
       types.begin(),
