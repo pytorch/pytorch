8d020f2568154543d2de858a17f32d97f19e15f3
Peter Y. Yeh <pyeh@amd.com>
2024-10-14 12:23:06 -0700

    fix merge error

--- c10/cuda/driver_api.cpp
+++ c10/cuda/driver_api.cpp
@@ -18,25 +18,9 @@ DriverAPI create_driver_api() {
 #endif
   void* handle_0 = dlopen(libName.data(), RTLD_LAZY | RTLD_NOLOAD);
   TORCH_CHECK(handle_0, "Can't open libs", dlerror());
+#ifndef USE_ROCM
   void* handle_1 = DriverAPI::get_nvml_handle();
-  DriverAPI r{};
-
-#define LOOKUP_LIBCUDA_ENTRY(name)                       \
-  r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); \
-  TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
-  C10_LIBCUDA_DRIVER_API(LOOKUP_LIBCUDA_ENTRY)
-#undef LOOKUP_LIBCUDA_ENTRY
-
-    #ifndef USE_ROCM
-      if (handle_1) {
-    #define LOOKUP_NVML_ENTRY(name)                          \
-      r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
-      TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
-        C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
-    #undef LOOKUP_NVML_ENTRY
-      }
-  #endif
-
+#endif
   DriverAPI r{};
 
 #define LOOKUP_LIBCUDA_ENTRY(name)                       \
@@ -51,6 +35,7 @@ DriverAPI create_driver_api() {
   C10_LIBCUDA_DRIVER_API_12030(LOOKUP_LIBCUDA_ENTRY)
 #undef LOOKUP_LIBCUDA_ENTRY
 
+#ifndef USE_ROCM
   if (handle_1) {
 #define LOOKUP_NVML_ENTRY(name)                          \
   r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
@@ -58,15 +43,17 @@ DriverAPI create_driver_api() {
     C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
 #undef LOOKUP_NVML_ENTRY
   }
+#endif
   return r;
 }
 } // namespace
 
+#ifndef USE_ROCM
 void* DriverAPI::get_nvml_handle() {
   static void* nvml_hanle = dlopen("libnvidia-ml.so.1", RTLD_LAZY);
   return nvml_hanle;
 }
-
+#endif
 C10_EXPORT DriverAPI* DriverAPI::get() {
   static DriverAPI singleton = create_driver_api();
   return &singleton;
