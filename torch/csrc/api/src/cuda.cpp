injiiiiil/654

Fix clang-tidy warnings

torch/csrc/api/src/cuda.cpp
@@ -41,7 +41,8 @@ void manual_seed(uint64_t seed) {
 void manual_seed_all(uint64_t seed) {
   auto num_gpu = device_count();
   for (const auto i : c10::irange(num_gpu)) {
-    auto gen = at::detail::getCUDAHooks().getDefaultCUDAGenerator(i);
+    auto gen = at::detail::getCUDAHooks().getDefaultCUDAGenerator(
+        static_cast<c10::DeviceIndex>(i));
     {
       // See Note [Acquire lock when using random generators]
       std::lock_guard<std::mutex> lock(gen.mutex());
@@ -52,12 +53,13 @@ void manual_seed_all(uint64_t seed) {
 
 void synchronize(int64_t device_index) {
   TORCH_CHECK(is_available(), "No CUDA GPUs are available");
-  int64_t num_gpus = cuda::device_count();
+  auto num_gpus = cuda::device_count();
   TORCH_CHECK(
-      device_index == -1 || device_index < num_gpus,
+      device_index < 0 || static_cast<size_t>(device_index) < num_gpus,
       "Device index out of range: ",
       device_index);
-  at::detail::getCUDAHooks().deviceSynchronize(device_index);
+  at::detail::getCUDAHooks().deviceSynchronize(
+      static_cast<c10::DeviceIndex>(device_index));
 }
 
 } // namespace torch::cuda

pytorch/pytorch

torch/csrc/api/src/cuda.cpp
@@ -41,7 +41,8 @@ void manual_seed(uint64_t seed) {
 void manual_seed_all(uint64_t seed) {
   auto num_gpu = device_count();
   for (const auto i : c10::irange(num_gpu)) {
-    auto gen = at::detail::getCUDAHooks().getDefaultGenerator(i);
+    auto gen = at::detail::getCUDAHooks().getDefaultGenerator(
+        static_cast<c10::DeviceIndex>(i));
     {
       // See Note [Acquire lock when using random generators]
       std::lock_guard<std::mutex> lock(gen.mutex());
@@ -52,12 +53,13 @@ void manual_seed_all(uint64_t seed) {
 
 void synchronize(int64_t device_index) {
   TORCH_CHECK(is_available(), "No CUDA GPUs are available");
-  int64_t num_gpus = cuda::device_count();
+  auto num_gpus = cuda::device_count();
   TORCH_CHECK(
-      device_index == -1 || device_index < num_gpus,
+      device_index < 0 || static_cast<size_t>(device_index) < num_gpus,
       "Device index out of range: ",
       device_index);
-  at::detail::getCUDAHooks().deviceSynchronize(device_index);
+  at::detail::getCUDAHooks().deviceSynchronize(
+      static_cast<c10::DeviceIndex>(device_index));
 }
 
 } // namespace torch::cuda