674b866831d2304448ad12d7f329cd59fb181d12
William Wen <williamwen@meta.com>
2024-10-15 15:16:47 -0700

    [dynamo] add torch.compiler.set_stance (#137504)
    Attempt # 2 at https://github.com/pytorch/pytorch/pull/132926 to implement https://github.com/pytorch/pytorch/issues/123771.
    
    Implement a new `torch.compiler.set_stance` function that can force `torch.compile` regions to run eagerly.
    
    See added tests for usage examples.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137504
    Approved by: https://github.com/yf225, https://github.com/jansel

--- torch/_dynamo/__init__.py
+++ torch/_dynamo/__init__.py
@@ -17,6 +17,7 @@ from .decorators import (
     mark_static_address,
     maybe_mark_dynamic,
     run,
+    set_stance,
     substitute_in_graph,
 )
 from .eval_frame import (
@@ -57,6 +58,7 @@ __all__ = [
     "run",
     "replay",
     "disable",
+    "set_stance",
     "reset",
     "OptimizedModule",
     "is_compiling",
