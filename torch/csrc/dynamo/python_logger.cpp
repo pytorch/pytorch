e66039e2a3470230bb3356a79f2d6f727c84a3ae
Simon Fan <xmfan@meta.com>
2024-10-15 11:46:09 -0700

    Update base for Update on "[compiled autograd] Compiled autograd configs in TLS"
    Multithreaded doesn't work yet, this adds python side TLS for the configs passing
    
    
    
    
    cc XilunWu H-Huang awgu kwen2501 wanchaol fegin fduwjj wz337 wconstab d4l3k c-p-i-o voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- torch/csrc/dynamo/python_logger.cpp
+++ torch/csrc/dynamo/python_logger.cpp
@@ -5,6 +5,16 @@
 
 namespace torch::dynamo {
 
+namespace {
+// see https://github.com/pytorch/pytorch/pull/34845
+void throw_python_error() {
+  python_error err;
+  err.persist();
+  // NOLINTNEXTLINE(misc-throw-by-value-catch-by-reference)
+  throw err;
+}
+} // namespace
+
 PythonLogger::PythonLogger(PyObject* logger) : logger_(logger) {
   TORCH_INTERNAL_ASSERT(logger_ != nullptr);
 }
@@ -12,13 +22,14 @@ PythonLogger::PythonLogger(PyObject* logger) : logger_(logger) {
 // must be called while GIL is held
 void PythonLogger::log(PythonLogger::Level level, std::string_view msg) const {
   THPObjectPtr pymethod(PyUnicode_FromString(levelNames_[level].data()));
+  TORCH_INTERNAL_ASSERT(pymethod != nullptr);
   THPObjectPtr pyfunc(PyObject_GetAttr(logger_, pymethod.get()));
+  if (pyfunc == nullptr) {
+    throw_python_error();
+  }
   PyObject* result = PyObject_CallFunction(pyfunc.get(), "s", msg.data());
   if (result == nullptr) {
-    python_error err;
-    err.persist();
-    // NOLINTNEXTLINE(misc-throw-by-value-catch-by-reference)
-    throw err;
+    throw_python_error();
   }
 }
 
