60eb3fccfa603188e925a44bd2f2e1259e8b82db
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-10-15 17:40:06 +0000

    Revert "[ONNX] Remove ExportTypes (#137789)"
    This reverts commit 3e0b83ad1f0a998ef8a72c5e82d9250ab800cce5.
    
    Reverted https://github.com/pytorch/pytorch/pull/137789 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/137789#issuecomment-2414632100))

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -24,6 +24,7 @@ __all__ = [
     "symbolic_opset19",
     "symbolic_opset20",
     # Enums
+    "ExportTypes",
     "OperatorExportTypes",
     "TrainingMode",
     "TensorProtoDataType",
@@ -56,6 +57,7 @@ from torch import _C
 from torch._C import _onnx as _C_onnx
 from torch._C._onnx import OperatorExportTypes, TensorProtoDataType, TrainingMode
 
+from ._exporter_states import ExportTypes
 from ._internal.exporter._onnx_program import ONNXProgram
 from ._internal.onnxruntime import (
     is_onnxrt_backend_supported,
@@ -113,6 +115,7 @@ if TYPE_CHECKING:
 # Set namespace for exposed private names
 DiagnosticOptions.__module__ = "torch.onnx"
 ExportOptions.__module__ = "torch.onnx"
+ExportTypes.__module__ = "torch.onnx"
 JitScalarType.__module__ = "torch.onnx"
 ONNXProgram.__module__ = "torch.onnx"
 ONNXRuntimeOptions.__module__ = "torch.onnx"
