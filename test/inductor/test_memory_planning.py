61c8c27cfe21cb9f548f178dee1445cc8b617fc1
Bin Bao <binbao@meta.com>
2024-10-15 18:43:30 -0700

    [AOTI] Remove explicit abi_compatible setting in tests (#138016)
    Differential Revision: [D64439674](https://our.internmc.facebook.com/intern/diff/D64439674)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138016
    Approved by: https://github.com/malfet
    ghstack dependencies: #137982

--- test/inductor/test_memory_planning.py
+++ test/inductor/test_memory_planning.py
@@ -64,7 +64,7 @@ class TestMemoryPlanning(TestCase):
     def test_cpp_wrapper(self):
         f, args = self._generate(device="cuda")
         compiled = torch.compile(f, dynamic=True)
-        with config.patch({"cpp_wrapper": True, "abi_compatible": True}):
+        with config.patch({"cpp_wrapper": True}):
             result, code = run_and_get_cpp_code(compiled, *args)
 
         FileCheck().check(
@@ -77,7 +77,7 @@ class TestMemoryPlanning(TestCase):
         self.assertTrue(same(f(*args), result))
 
     @skipIfRocm(msg="test_aot_inductor doesn't work on ROCm")
-    def test_abi_compatible(self):
+    def test_aoti(self):
         try:
             from .test_aot_inductor import AOTIRunnerUtil
         except ImportError:
@@ -88,12 +88,9 @@ class TestMemoryPlanning(TestCase):
         f, args = self._generate(device="cuda")
         dim0_x = Dim("dim0_x", min=1, max=2048)
         dynamic_shapes = ({0: dim0_x}, None, None)
-        with config.patch("abi_compatible", True):
-            result, code = run_and_get_cpp_code(
-                lambda: AOTIRunnerUtil.run(
-                    "cuda", f, args, dynamic_shapes=dynamic_shapes
-                )
-            )
+        result, code = run_and_get_cpp_code(
+            lambda: AOTIRunnerUtil.run("cuda", f, args, dynamic_shapes=dynamic_shapes)
+        )
 
         FileCheck().check(
             "int64_t int_array_2[] = {24L + (align(12L*s0)), };"
