229fea89bf5f4b98664f0c140809c55b1f46a95e
Joy Dong <joydong@umich.edu>
2024-10-15 23:52:44 -0400

    Fix context error for sdpa

--- benchmarks/transformer/score_mod.py
+++ benchmarks/transformer/score_mod.py
@@ -227,65 +227,67 @@ def run_single_backend_sdpa(
                 config.attn_type, config.shape, config.dtype, block_mask
             )
 
-    if config.attn_type == "document_mask":
-        if backend in ["og-eager"]:
-            print(f"Backend {backend} not supported for document mask")
-        else:  # sdpa
-            q_eager, k_eager, v_eager = generate_jagged_inputs(
-                query, key, value, **mask_kwargs
-            )
-            q_eager = q_eager.transpose(1, 2).requires_grad_(query.requires_grad)
-            k_eager = k_eager.transpose(1, 2).requires_grad_(key.requires_grad)
-            v_eager = v_eager.transpose(1, 2).requires_grad_(value.requires_grad)
-    else:
-        q_eager, k_eager, v_eager = query_key_value_clones(query, key, value)
+        if config.attn_type == "document_mask":
+            if backend in ["og-eager"]:
+                print(f"Backend {backend} not supported for document mask")
+            else:  # sdpa
+                q_eager, k_eager, v_eager = generate_jagged_inputs(
+                    query, key, value, **mask_kwargs
+                )
+                q_eager = q_eager.transpose(1, 2).requires_grad_(query.requires_grad)
+                k_eager = k_eager.transpose(1, 2).requires_grad_(key.requires_grad)
+                v_eager = v_eager.transpose(1, 2).requires_grad_(value.requires_grad)
+        else:
+            q_eager, k_eager, v_eager = query_key_value_clones(query, key, value)
 
-    if eager_sdpa:
-        try:
-            out_eager = eager_sdpa(query=q_eager, key=k_eager, value=v_eager)
-        except RuntimeError as e:
-            print(f"Error: {e}")
-            return ExperimentResults(fwd_time=float("nan"), bwd_time=float("nan"))
-        if config.attn_type in ["document_mask"]:
-            flatten_o_eager = torch.cat(torch.unbind(out_eager.transpose(1, 2)))
-            flatten_o_compile = out_compile.transpose(1, 2).flatten(
-                start_dim=0, end_dim=1
-            )
-            torch.testing.assert_close(
-                flatten_o_eager, flatten_o_compile, atol=1e-2, rtol=1e-2
-            )
-        elif not (
-            config.attn_type in ["rel", "alibi"]
-            and config.dtype in [torch.float16, torch.bfloat16]
-        ):  # rel has accuracy issue with 16bit floats
-            torch.testing.assert_close(out_eager, out_compile, atol=1e-2, rtol=1e-2)
+        if eager_sdpa:
+            try:
+                out_eager = eager_sdpa(query=q_eager, key=k_eager, value=v_eager)
+            except RuntimeError as e:
+                print(
+                    f"Backend: {backend} failed on shape {config.shape} with error: {e} "
+                )
+                return ExperimentResults(fwd_time=float("nan"), bwd_time=float("nan"))
+            if config.attn_type in ["document_mask"]:
+                flatten_o_eager = torch.cat(torch.unbind(out_eager.transpose(1, 2)))
+                flatten_o_compile = out_compile.transpose(1, 2).flatten(
+                    start_dim=0, end_dim=1
+                )
+                torch.testing.assert_close(
+                    flatten_o_eager, flatten_o_compile, atol=1e-2, rtol=1e-2
+                )
+            elif not (
+                config.attn_type in ["rel", "alibi"]
+                and config.dtype in [torch.float16, torch.bfloat16]
+            ):  # rel has accuracy issue with 16bit floats
+                torch.testing.assert_close(out_eager, out_compile, atol=1e-2, rtol=1e-2)
 
-    if eager_sdpa:
-        forward_eager_time = benchmark_torch_function_in_microseconds(
-            eager_sdpa, query=q_eager, key=k_eager, value=v_eager
-        )
-    else:
-        forward_eager_time = float("nan")
-
-    if config.calculate_bwd_time:
-        # TODO: debug backward pass for njt
-        if eager_sdpa and not config.attn_type == "document_mask":
-            dOut = torch.randn_like(out_eager.transpose(1, 2)).transpose(1, 2)
-            backward_eager_time = benchmark_torch_function_in_microseconds(
-                out_eager.backward, dOut, retain_graph=True
+        if eager_sdpa:
+            forward_eager_time = benchmark_torch_function_in_microseconds(
+                eager_sdpa, query=q_eager, key=k_eager, value=v_eager
             )
         else:
-            backward_eager_time = float("nan")
+            forward_eager_time = float("nan")
 
-        return ExperimentResults(
-            fwd_time=forward_eager_time,
-            bwd_time=backward_eager_time,
-        )
-    else:
-        return ExperimentResults(
-            fwd_time=forward_eager_time,
-            bwd_time=None,
-        )
+        if config.calculate_bwd_time:
+            # TODO: debug backward pass for njt
+            if eager_sdpa and not config.attn_type == "document_mask":
+                dOut = torch.randn_like(out_eager.transpose(1, 2)).transpose(1, 2)
+                backward_eager_time = benchmark_torch_function_in_microseconds(
+                    out_eager.backward, dOut, retain_graph=True
+                )
+            else:
+                backward_eager_time = float("nan")
+
+            return ExperimentResults(
+                fwd_time=forward_eager_time,
+                bwd_time=backward_eager_time,
+            )
+        else:
+            return ExperimentResults(
+                fwd_time=forward_eager_time,
+                bwd_time=None,
+            )
 
 
 def run_single_backend_FA(
@@ -323,7 +325,7 @@ def run_single_backend_FA(
         else:
             out_FA_updated = out_FA
         torch.testing.assert_close(
-            out_FA_updated, out_compile.transpose(1, 2), atol=1e-2, rtol=1e-2
+            out_FA_updated, out_compile.transpose(1, 2), atol=1e-1, rtol=1e-1
         )
 
     if FA:
@@ -563,6 +565,8 @@ def print_results(results: List[Experiment], save_path: Optional[str] = None):
     print(tabulate(table_data, headers="keys", tablefmt="github", floatfmt=".3f"))
 
     for backend in results[0].config.backends:
+        if np.isnan(table_data[f"fwd_{backend}_speedup"]).all():
+            continue
         print("\n")
         print(f"FWD Speedups vs. {backend}".center(125, "="))
         print("\n")
@@ -613,7 +617,7 @@ def generate_score_mod(attn_type: str, shape: Tuple[int]) -> Callable | None:
         "alibi": generate_alibi_bias(Hq),
         "sliding_window": None,
         "document_mask": None,
-        "prefix_ml": None,
+        "prefix_lm": None,
         "softcap": generate_tanh_softcap(softcap_value, approx=True),
         "transfusion": generate_tanh_softcap(softcap_value, approx=True),
     }
@@ -923,7 +927,7 @@ def generate_eager_sdpa(
 ) -> Callable | None:
     B, Hq, M, Hkv, N, D = shape
     is_decoding = M == 1
-    if attn_type == "sliding_window" or attn_type == "prefix_ml":
+    if attn_type == "sliding_window" or attn_type == "prefix_lm":
         attn_mask = create_mask(block_mask.mask_mod, 1, 1, M, N, device="cuda")
     elif attn_type == "rel":
         m = torch.arange(M, dtype=int, device="cuda")
@@ -1127,7 +1131,7 @@ if __name__ == "__main__":
         "-mods",
         type=str,
         nargs="+",
-        help="score mods: noop, causal, rel, head_bias, alibi, sliding_window, document_mask, prefix_ml, softcap",
+        help="score mods: noop, causal, rel, head_bias, alibi, sliding_window, document_mask, prefix_lm, softcap",
         default=["noop", "causal", "rel", "head_bias"],
     )
     parser.add_argument(
