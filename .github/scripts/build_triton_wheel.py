8243d786d8415bcf269dab3458c14534959cc7ed
Ting Lu <tingl@nvidia.com>
2024-10-17 15:00:31 -0700

    test custom llvm build

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -108,7 +108,7 @@ def build_triton(
                 ["git", "checkout", f"release/{ver}.{rev}.x"], cwd=triton_basedir
             )
         else:
-            check_call(["git", "checkout", "debug-ninja"], cwd=triton_basedir)
+            check_call(["git", "checkout", "llvm"], cwd=triton_basedir)
 
         # TODO: remove this and patch_setup_py() once we have a proper fix for https://github.com/triton-lang/triton/issues/4527
         patch_setup_py(triton_pythondir / "setup.py")
