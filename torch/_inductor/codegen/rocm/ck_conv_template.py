83e64b3f1ea0bdc805326b9e051fef9e69682f41
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-10-18 17:23:36 +0000

    move out instance parsing

--- torch/_inductor/codegen/rocm/ck_conv_template.py
+++ torch/_inductor/codegen/rocm/ck_conv_template.py
@@ -1,14 +1,20 @@
 # mypy: allow-untyped-defs
 import copy
 import logging
-import os
 import random
-import subprocess
-from dataclasses import asdict, dataclass, replace
-from functools import lru_cache
-from typing import List, Optional, Tuple
+from typing import Tuple
 
-from ck4inductor.util import library_path
+
+try:
+    import ck4inductor
+except ImportError:
+    ck4inductor = None
+
+if ck4inductor is not None:
+    from ck4inductor.grouped_conv_fwd.gen_instances import gen_conv_ops_library
+else:
+    def gen_conv_ops_library():
+        return []
 
 from torch._inductor import config
 from torch._inductor.codegen.rocm.ck_template import CKTemplate
@@ -58,231 +64,6 @@ def torch_layout_to_ck_output_layout(torch_layout):
         return "NHWGK"
     else:
         return None
-
-
-@dataclass
-class CKGroupedConvFwdOp:
-    n_dim_spatial: int
-    a_layout: str
-    b_layout: str
-    ds_layout: Tuple[str]
-    e_layout: str
-    a_element_dtype: str
-    b_element_dtype: str
-    acc_dtype: str
-    c_shuffle_dtype: str
-    ds_element_dtype: Tuple[str]
-    e_element_dtype: str
-    a_elementwise_op: str
-    b_elementwise_op: str
-    cde_elementwise_op: str
-    conv_forward_specialization: str
-    gemm_specialization: str
-
-    block_size: int
-    m_per_block: int
-    n_per_block: int
-    k_per_block: int
-    ak1: int
-    bk1: int
-    m_per_xdl: int
-    n_per_xdl: int
-    m_xdl_per_wave: int
-    n_xdl_per_wave: int
-    a_block_transfer_thread_cluster_lengths_ak0_m_ak1: Tuple[int, int, int]
-    a_block_transfer_thread_cluster_arrange_order: Tuple[int, int, int]
-    a_block_transfer_src_access_order: Tuple[int, int, int]
-    a_block_transfer_src_vector_dim: int
-    a_block_transfer_src_scalar_per_vector: int
-    a_block_transfer_dst_scalar_per_vector_ak1: int
-    a_block_lds_extra_m: bool
-
-    b_block_transfer_thread_cluster_lengths_bk0_n_bk1: Tuple[int, int, int]
-    b_block_transfer_thread_cluster_arrange_order: Tuple[int, int, int]
-    b_block_transfer_src_access_order: Tuple[int, int, int]
-
-    b_block_transfer_src_vector_dim: int
-    b_block_transfer_src_scalar_per_vector: int
-    b_block_transfer_dst_scalar_per_vector_bk1: int
-    b_block_lds_extra_n: bool
-
-    c_shuffle_m_xdl_per_wave_per_shuffle: int
-    c_shuffle_n_xdl_per_wave_per_shuffle: int
-    cde_block_transfer_cluster_lengths_m_block_m_per_block_n_block_n_per_block: Tuple[
-        int, int, int, int
-    ]
-    cde_block_transfer_scalar_per_vector_n_per_block: int
-    block_gemm_pipeline_scheduler: str
-    block_gemm_pipeline_version: str
-
-    a_compute_dtype: Optional[str] = None
-    b_compute_dtype: Optional[str] = None
-
-    def name(self):
-        # cpp alias for template instance
-        return f"ck_device_grouped_convolution_fwd_multiple_abd_xdl_c_shuffle_v3_{self.key_name()}"
-
-    def key_name(self):
-        # TBD; must be unique per instance. Intended to use as dict key
-        return "_".join(
-            [
-                "K"
-                + field_name.replace("_", "").lower()
-                + "V"
-                + (
-                    "x".join(map(str, iter(field_value)))
-                    if isinstance(field_value, tuple)
-                    else str(field_value).replace(":", "")
-                )
-                for field_name, field_value in self.dict_items()
-            ]
-        )
-
-    def dict_items(self):
-        return asdict(self).items()
-
-
-def _ck_conv_instances_path():
-    conv_instances_path = os.path.join(  # noqa: F821
-        library_path(),
-        "include",
-        "ck",
-        "library",
-        "tensor_operation_instance",
-        "gpu",
-        "grouped_conv_fwd",
-    )
-    if not os.path.exists(conv_instances_path):
-        log.error(
-            "CK library conv instances path %s does not exist", conv_instances_path
-        )
-        return None
-    return conv_instances_path
-
-
-def parse_instances(str_instances: List[str]) -> List[CKGroupedConvFwdOp]:
-    """
-    Parse the lines containing Grouped Convolution Forward template instances into `CKGroupedConvFwdOp` instances
-    """
-
-    def maybe_int(s):
-        try:
-            return int(s)
-        except ValueError:
-            return s
-
-    op_instances = []
-    # TODO: maybe use libclang for parsing C++ code in the future to avoid this hacky parsing logic below ? :) - copilot
-    for line in str_instances:
-        s_template_args = line.split("DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3")[
-            -1
-        ].strip("<>, ")
-        template_args = []
-        i_current = 0
-        while i_current < len(s_template_args):
-            if s_template_args[i_current] == " ":
-                # skip whitespace
-                i_current += 1
-                continue
-            elif s_template_args[i_current : i_current + 2] == "S<":
-                # parse template S<Index...>
-                i_next = s_template_args.find(">", i_current)
-                template_args.append(
-                    tuple(map(int, s_template_args[i_current + 2 : i_next].split(",")))
-                )
-                i_current = i_next + 2
-            else:
-                # all string attributes must be either type aliases or global constants in C++
-                i_next = s_template_args.find(",", i_current)
-                template_args.append(
-                    maybe_int(
-                        s_template_args[i_current : i_next if i_next != -1 else None]
-                    )
-                )
-                if i_next != -1:
-                    i_current = i_next + 1
-            if i_next == -1:
-                break
-
-        template_args[0] = -1  # n_dim_spatial
-        template_args[3] = tuple()  # ds_layout
-        template_args[9] = tuple()  # ds_element_dtype
-
-        new_instance = CKGroupedConvFwdOp(
-            *template_args,  # type: ignore[arg-type]
-        )
-
-        op_instances.append(new_instance)
-    return op_instances
-
-
-@lru_cache(None)
-def gen_conv_ops_library() -> List[CKGroupedConvFwdOp]:
-    """
-    Parse the Grouped Convolution Forward instances defined in the composable kernel library folder.
-    """
-    ck_library_dir = _ck_conv_instances_path()
-    if not ck_library_dir:
-        return []
-
-    grep_result = subprocess.run(
-        [
-            "grep",
-            "-inR",
-            "DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3",
-            ck_library_dir,
-        ],
-        capture_output=True,
-        text=True,
-    )
-
-    op_instances = parse_instances(grep_result.stdout.strip().split("\n"))
-
-    log.debug("ck instances from library: %d", len(op_instances))
-
-    schedulers = [
-        "BlockGemmPipelineScheduler::Intrawave",
-        "BlockGemmPipelineScheduler::Interwave",
-    ]
-    conv_specs = [
-        "ConvolutionForwardSpecialization::Default",
-        "ConvolutionForwardSpecialization::Filter1x1Pad0",
-        "ConvolutionForwardSpecialization::Filter1x1Stride1Pad0",
-        "ConvolutionForwardSpecialization::OddC",
-    ]
-
-    # substitute templated args by looping through their domains
-    substitute_instances = []
-    for instance in op_instances:
-        sub_scheduler = instance.block_gemm_pipeline_scheduler == "BlkGemmPipeSched"
-        sub_spec = instance.conv_forward_specialization == "ConvSpec"
-        schedulers_range = (
-            schedulers if sub_scheduler else [instance.block_gemm_pipeline_scheduler]
-        )
-        spec_range = conv_specs if sub_spec else [instance.conv_forward_specialization]
-        for scheduler in schedulers_range:
-            for spec in spec_range:
-                for channels_last in [True, False]:
-                    if channels_last:
-                        a_layout = "NHWGC"
-                        e_layout = "NHWGK"
-                    else:
-                        a_layout = "NGCHW"
-                        e_layout = "NGKHW"
-                    substitute_instances.append(
-                        replace(
-                            instance,
-                            block_gemm_pipeline_scheduler=scheduler,
-                            conv_forward_specialization=spec,
-                            gemm_specialization="GemmSpecialization::MNKPadding",
-                            n_dim_spatial=2,
-                            a_layout=a_layout,
-                            b_layout="GKYXC",
-                            e_layout=e_layout,
-                        )
-                    )
-
-    return substitute_instances
 
 
 class CKGroupedConvFwdTemplate(CKTemplate):
@@ -637,7 +418,7 @@ class CKGroupedConvFwdTemplate(CKTemplate):
         self.groups = groups
         self.n_spatial_dimensions = n_spatial_dimensions
 
-    def filter_op(self, op: CKGroupedConvFwdOp) -> bool:
+    def filter_op(self, op: "CKGroupedConvFwdOp") -> bool:
         metas = [
             T.get_layout()
             for T in [*self.input_nodes, self.output_node]
