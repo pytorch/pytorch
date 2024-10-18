0e07adbafc06f746a381b4e874740fb145b88c75
Colin L. Rice <clr@fb.com>
2024-10-18 16:09:48 -0600

    config: remove internal pytorch references to shallow_copy_dict
    All use cases upon inspection did not rely on the shallow copy behaviour
    (i.e. dicts and other reference values can be modified).
    
    Using the deep copy behaviour makes it possible to remove the shallow
    copy behaviour, which makes changing internal implementations almost
    impossible.

--- torch/_inductor/__init__.py
+++ torch/_inductor/__init__.py
@@ -258,7 +258,7 @@ def list_options() -> List[str]:
 
     from torch._inductor import config
 
-    current_config: Dict[str, Any] = config.shallow_copy_dict()
+    current_config: Dict[str, Any] = config.get_config_copy()
 
     return list(current_config.keys())
 
