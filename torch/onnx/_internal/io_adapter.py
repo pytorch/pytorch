1662ec2bcb95dc61fdfad899c2e064c6911d5b3b
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-10-22 19:28:27 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/onnx/_internal/io_adapter.py
+++ torch/onnx/_internal/io_adapter.py
@@ -151,7 +151,7 @@ def _replace_list_with_tuple(spec: pytree.TreeSpec) -> pytree.TreeSpec:
             )
         return x
 
-    dummy_leaf = _DummyLeaf
+    dummy_leaf = _DummyLeaf()
     dummy_tree = pytree.tree_unflatten([dummy_leaf] * spec.num_leaves, spec)
     dummy_tree = pytree.tree_map(
         replace_list_with_tuple,
