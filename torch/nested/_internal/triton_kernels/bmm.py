246e9951f7d17e39c0ec0a9ca7013fb29a5b0183
Christian Puhrsch <cpuhrsch@gmail.com>
2024-10-23 04:27:51 +0000

    Some masking

--- torch/nested/_internal/triton_kernels/bmm.py
+++ torch/nested/_internal/triton_kernels/bmm.py
@@ -31,7 +31,6 @@ import torch
 import triton
 import triton.language as tl
 
-
 @triton.autotune(
     configs=[
         triton.Config({
@@ -65,7 +64,7 @@ import triton.language as tl
 def grouped_matmul_kernel(
     # device tensor of matrices pointers
     # group_a_ptrs,
-    b_ptr_base,
+    b_ptr,
     # group_c_ptrs,
     # # device tensor of gemm sizes. its shape is [group_size, 3]
     # # dim 0 is group_size, dim 1 is the values of <M, N, K> of each gemm
@@ -92,20 +91,24 @@ def grouped_matmul_kernel(
     BLOCK_SIZE_K: tl.constexpr,
 ):
     tile_idx = tl.program_id(0)
-    num_n_tiles = tl.cdiv(gn, BLOCK_SIZE_N)
     g = tl.program_id(1)
 
     # get the gemm size of the current problem
     a_offset_0 = tl.load(a_offsets_ptr + g)
     a_offset_1 = tl.load(a_offsets_ptr + g + 1)
     gm = a_offset_1 - a_offset_0
+
+    num_n_tiles = tl.cdiv(gn, BLOCK_SIZE_N)
     num_m_tiles = tl.cdiv(gm, BLOCK_SIZE_M)
     num_tiles = num_m_tiles * num_n_tiles
     last_problem_end = g * num_tiles
+
+    offs_k = g + tl.arange(0, BLOCK_SIZE_K)
+
     # iterate through the tiles in the current gemm problem
     while (tile_idx >= last_problem_end and tile_idx < last_problem_end + num_tiles):
         # pick up a tile from the current gemm problem
-        b_ptr = (b_ptr_base + g * gn * k).to(tl.pointer_type(tl.float16))
+
         # figure out tile coordinates
         tile_idx_in_gemm = tile_idx - last_problem_end
         tile_m_idx = tile_idx_in_gemm // num_n_tiles
@@ -114,14 +117,14 @@ def grouped_matmul_kernel(
         # do regular gemm here
         offs_am = a_offset_0 + tile_m_idx * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
         offs_bn = tile_n_idx * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-        offs_k = tl.arange(0, BLOCK_SIZE_K)
+
         a_ptrs = a_ptr + offs_am[:, None] * lda + offs_k[None, :]
         b_ptrs = b_ptr + offs_k[:, None] * ldb + offs_bn[None, :]
         accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
         for kk in range(0, tl.cdiv(k, BLOCK_SIZE_K)):
-            # hint to Triton compiler to do proper loop pipelining
-            tl.multiple_of(a_ptrs, [16, 16])
-            tl.multiple_of(b_ptrs, [16, 16])
+            # # # hint to Triton compiler to do proper loop pipelining
+            # tl.multiple_of(a_ptrs, [16, 16])
+            # tl.multiple_of(b_ptrs, [16, 16])
             # assume full tile for now
             a = tl.load(a_ptrs)
             b = tl.load(b_ptrs)
@@ -134,8 +137,10 @@ def grouped_matmul_kernel(
         offs_cn = tile_n_idx * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
         c_ptrs = c_ptr + ldc * offs_cm[:, None] + offs_cn[None, :]
 
+        c_mask = (offs_cm[:, None] < a_offset_1) & (offs_cn[None, :] < gn)
+
         # assumes full tile for now
-        tl.store(c_ptrs, c)
+        tl.store(c_ptrs, c, mask=c_mask)
 
         # go to the next tile by advancing NUM_SM
         tile_idx += NUM_SM
@@ -149,7 +154,7 @@ def group_gemm_fn(tensor_a, tensor_b):
     assert tensor_b.is_contiguous()
 
     a_values = tensor_a.values()
-    a_offsets = tensor_a.offsets()
+    a_offsets = tensor_a.offsets().to(torch.int32)
 
     assert a_values.dim() == 2
 
@@ -166,7 +171,7 @@ def group_gemm_fn(tensor_a, tensor_b):
         N,
         K,
         tensor_b.stride(1),
-        a_offsets.to(torch.int32),
+        a_offsets,
         a_values,
         a_values.stride(0),
         c_values.stride(0),
