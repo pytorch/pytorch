8fb78b000ae2c5243858290c235124cddefb6561
Sam Larsen <slarsen@meta.com>
2024-10-22 21:49:20 -0700

    [fx graph cache] Refactor FxGraphCachePickler
    Summary: In an upcoming change, we need to modify how FxGraphCachePickler pickles tensor constants. Specifically, we'll pickle differently depending on whether parameters have been frozen. To do that, it will be convenient to change FxGraphCachePickler into a regular object instead of a collection of classmethods.
    
    Test Plan: unit tests
    
    [ghstack-poisoned]

--- torch/_functorch/_aot_autograd/autograd_cache.py
+++ torch/_functorch/_aot_autograd/autograd_cache.py
@@ -223,9 +223,6 @@ class AOTAutogradCacheDetails(FxGraphHashDetails):
             # Sometimes inductor configs are unpickleable and can fail
             raise BypassAOTAutogradCache from e
 
-    def debug_lines(self) -> List[str]:
-        return AOTAutogradCachePickler.debug_lines(self)
-
 
 def _reduce_aot_config(aot_config: AOTConfig):
     """
@@ -250,16 +247,20 @@ def _reduce_tensor(tensor):
     """
     Reduce the tensor to a stable key for caching.
     """
-    return (
-        _ident,
-        extract_tensor_metadata_for_cache_key(tensor),
-    )
+    metadata = extract_tensor_metadata_for_cache_key(tensor)
+    return (_ident, (metadata,))
 
 
 class AOTAutogradCachePickler(FxGraphCachePickler):
-    dispatch_table = FxGraphCachePickler.dispatch_table.copy()
-    dispatch_table[AOTConfig] = _reduce_aot_config
-    dispatch_table[torch.Tensor] = _reduce_tensor
+    def __init__(self):
+        super().__init__()
+        self.dispatch_table: Dict
+        self.dispatch_table.update(
+            {
+                AOTConfig: _reduce_aot_config,
+                torch.Tensor: _reduce_tensor,
+            }
+        )
 
 
 def autograd_cache_key(
@@ -274,9 +275,10 @@ def autograd_cache_key(
     """
     check_cacheable(gm)
     details = AOTAutogradCacheDetails(gm, example_inputs, config, fx_config)
+    pickler = AOTAutogradCachePickler()
     # The prefix distinguishes among the other kinds of objects we cache
-    key = "a" + AOTAutogradCachePickler.get_hash(details)
-    debug_lines = details.debug_lines()
+    key = "a" + pickler.get_hash(details)
+    debug_lines = pickler.debug_lines(details)
     log.debug(
         "Autograd graph cache hash details for key %s:\n%s",
         key,
