9c706af01317b210df7f0b5fa64686151e7a8cdf
Bob Ren <bobren@fb.com>
2024-10-22 19:18:13 -0700

    Update on "Have as_tensor always return a float64 tensor in dynamo"
    As discussed with ezyang, this set of diffs are extracting fixes to problems discovered to flipping `specialize_float=False` in https://github.com/pytorch/pytorch/pull/137782. Since these codepaths are exercised in existing tests, I'm going to bias towards shipping speed and put these up with the primary test plan as the global CI. These code paths are all tested via existing tests when `specialize_float=False` and it feels a bit wonky to add more gated tests that only test behavior when this flag is True, especially since these code paths are already covered. That being said, I'm happy to add individual tests if reviewers insist or have a different POV.  
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -1146,18 +1146,11 @@ class SymNodeVariable(VariableTracker):
     def as_proxy(self):
         return self.proxy
 
-    def as_tensor(self, tx):
+    def as_tensor(self, tx, dtype):
         if self._tensor_var is None:
-            if isinstance(self.sym_num, torch.SymFloat):
-                self._tensor_var = VariableTracker.build(
-                    tx, torch.scalar_tensor
-                ).call_function(
-                    tx, [self], {"dtype": VariableTracker.build(tx, torch.float64)}
-                )
-            else:
-                self._tensor_var = VariableTracker.build(
-                    tx, torch.scalar_tensor
-                ).call_function(tx, [self], {})
+            self._tensor_var = VariableTracker.build(
+                tx, torch.scalar_tensor
+            ).call_function(tx, [self], {"dtype": VariableTracker.build(tx, dtype)})
         return self._tensor_var
 
     def evaluate_expr(self, output_graph=None):
