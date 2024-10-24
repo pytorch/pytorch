3f5c64da9696cc515f2c7c699a152a3c78d66455
Yifu Wang <yifu@fb.com>
2024-10-24 01:50:23 -0700

    debug
    [ghstack-poisoned]

--- test/distributed/test_symmetric_memory.py
+++ test/distributed/test_symmetric_memory.py
@@ -776,6 +776,8 @@ class SymmMemUtilTest(TestCase):
         self.assertTrue(t[32:48].eq(1).all())
         self.assertTrue(t[48:].eq(0).all())
 
+        print(torch.ops.symm_mem.memset32_)
+
         with self.assertRaises(RuntimeError):
             _SymmetricMemory.memset32(t, offset=-1, val=1, count=16)
 
