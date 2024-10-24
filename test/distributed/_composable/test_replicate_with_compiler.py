5e191b0344e322b8444136296d5726ed5a8fa434
Xuan Zhang <xuanzh@meta.com>
2024-10-24 15:13:11 -0700

    fix additional test case failure

--- test/distributed/_composable/test_replicate_with_compiler.py
+++ test/distributed/_composable/test_replicate_with_compiler.py
@@ -313,7 +313,9 @@ class ReplicateTest(MultiProcessInductorTestCase):
     )
     # todo: This pass mucks things up since Inductor thinks its inference
     # and can apply this. Should turn off these passes in compiled autograd
-    @torch._inductor.config.patch(reorder_for_locality=False)
+    @torch._inductor.config.patch(
+        reorder_for_locality=False, reorder_for_peak_memory=False
+    )
     def test_bucketing_coalesced_op(self):
         # Gradient is None
         code = self._test_bucketing()
