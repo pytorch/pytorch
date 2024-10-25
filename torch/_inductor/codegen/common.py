4c771895df0491e43f6d35a4f3f0bdf29db25730
Jason Ansel <jansel@meta.com>
2024-10-24 21:25:02 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -1466,7 +1466,7 @@ class KernelArgs:
         arg = WorkspaceArg(
             count=min_size,
             zero_mode=WorkspaceZeroMode.ZERO_PER_GRAPH,
-            dtype=torch.int32,
+            dtype=torch.uint32,
             inner_name="sem_ptr",
             outer_name=f"semaphores_{current_device.type}_{current_device.index}",
             device=current_device,
