c154992065c85430d4a17e3272d7030bd7fa713e
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-10-26 11:59:20 -0400

    Add one more file update

--- torch/_subclasses/meta_utils.py
+++ torch/_subclasses/meta_utils.py
@@ -19,7 +19,7 @@ from typing import (
     TYPE_CHECKING,
     Union,
 )
-from typing_extensions import TypeAlias, TypeGuard
+from typing_extensions import TypeAlias, TypeIs
 
 import torch
 from torch._C._autograd import CreationMeta
@@ -143,7 +143,7 @@ def is_sparse_compressed(t):
     return isinstance(t, torch.Tensor) and is_sparse_compressed_layout(t.layout)
 
 
-def is_sparse_any(t: object) -> TypeGuard[torch.Tensor]:
+def is_sparse_any(t: object) -> TypeIs[torch.Tensor]:
     return is_sparse_coo(t) or is_sparse_compressed(t)
 
 
