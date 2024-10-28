f5e6a0f76a5c095c7711338ab1573822576af9c1
Scott Wolchok <swolchok@fb.com>
2024-10-28 10:20:53 -0700

    [PyTorch] Extract value_type-generic NEON Vectorized<Half> functions to CRTP base class
    This is in prepraration for adding NEON Vectorized<BFloat16>, which will be simplified by sharing this stuff.
    
    Differential Revision: [D64997744](https://our.internmc.facebook.com/intern/diff/D64997744/)
    
    [ghstack-poisoned]

--- aten/src/ATen/cpu/vec/vec128/vec128_half_neon.h
+++ aten/src/ATen/cpu/vec/vec128/vec128_half_neon.h
@@ -6,6 +6,7 @@
 #include <ATen/cpu/vec/intrinsics.h>
 #include <ATen/cpu/vec/vec128/vec128_convert.h>
 #include <ATen/cpu/vec/vec128/vec128_float_neon.h>
+#include <ATen/cpu/vec/vec128/vec128_reduced_precision_common_neon.h>
 #include <ATen/cpu/vec/vec_base.h>
 #include <c10/util/Half.h>
 #include <c10/util/irange.h>
@@ -60,34 +61,11 @@ struct BlendHalfRegs<index, false> {
 // On ARM, Half type supports float16_t->Half constructor and Half->float16_t
 // conversion
 template <>
-class Vectorized<c10::Half> {
- private:
-  float16x8_t values;
-
- public:
-  // value_type should be c10::Half to fit interface with vec_base.h
-  using value_type = c10::Half;
-  using size_type = int;
-  static constexpr size_type size() {
-    static_assert(sizeof(float16x8_t) == 8 * sizeof(value_type));
-    return 8;
-  }
-
+class Vectorized<c10::Half> : public Vectorized16<float16x8_t, c10::Half, BlendHalfRegs, Vectorized<c10::Half>> {
+  using Base = Vectorized16<float16x8_t, c10::Half, BlendHalfRegs, Vectorized<c10::Half>>;
+  friend Base;
  private:
   // We use these private map functions to implement various methods
-  Vectorized<c10::Half> map2(
-      const Vectorized<c10::Half>& second,
-      c10::Half (*const f)(c10::Half, c10::Half)) const {
-    __at_align__ c10::Half tmp_first[size()];
-    __at_align__ c10::Half tmp_second[size()];
-    store(tmp_first); // store this to tmp_first
-    second.store(tmp_second);
-    for (const auto i : c10::irange(size())) {
-      tmp_first[i] = f(tmp_first[i], tmp_second[i]);
-    }
-    return loadu(tmp_first);
-  }
-
   Vectorized<c10::Half> map_with_vec_float_method(
       Vectorized<float> (Vectorized<float>::*m)() const) const {
     float32x4_t v00 = vcvt_f32_f16(vget_low_f16(values));
@@ -117,70 +95,24 @@ class Vectorized<c10::Half> {
   }
 
  public:
-   // constructor
-  Vectorized() {}
-  Vectorized(float16x8_t v) : values(v) {}
+  using Vectorized16::Vectorized16;
+
+  Vectorized() = default;
 
   // A ctor that accepts c10::Half is needed to fit interface with vec_base.h
   // A second constructor that takes float16_t is also included
   Vectorized(c10::Half val)
-      : values{vdupq_n_f16((float16_t)val)} {
-  }
-  Vectorized(float16_t val) : values{vdupq_n_f16(val)} {}
-  Vectorized(
-      float16_t val0,
-      float16_t val1,
-      float16_t val2,
-      float16_t val3,
-      float16_t val4,
-      float16_t val5,
-      float16_t val6,
-      float16_t val7)
-      : values{
-            val0,
-            val1,
-            val2,
-            val3,
-            val4,
-            val5,
-            val6,
-            val7} {}
-  operator float16x8_t() const {
-    return values;
-  }
-  template <int64_t mask>
-  static Vectorized<c10::Half> blend(
-      const Vectorized<c10::Half>& a,
-      const Vectorized<c10::Half>& b) {
-    Vectorized<c10::Half> vec;
-    // 0.
-    vec.values = BlendHalfRegs<0, (mask & 0x01) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<1, (mask & 0x02) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<2, (mask & 0x04) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<3, (mask & 0x08) != 0>::impl(
-        a.values, b.values, vec.values);
+      : Vectorized((float16_t)val) {}
+  Vectorized(float16_t val)
+      : Vectorized16(vdupq_n_f16(val)) {}
 
-    vec.values = BlendHalfRegs<4, (mask & 0x10) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<5, (mask & 0x20) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<6, (mask & 0x40) != 0>::impl(
-        a.values, b.values, vec.values);
-    vec.values = BlendHalfRegs<7, (mask & 0x80) != 0>::impl(
-        a.values, b.values, vec.values);
-
-    return vec;
-  }
   static Vectorized<c10::Half> blendv(
       const Vectorized<c10::Half>& a,
       const Vectorized<c10::Half>& b,
       const Vectorized<c10::Half>& mask) {
-    // Note: using blendv is very awkward because 0xFFFF is one of many NaN's in
-    // FP16 It's unfortunate that the mask has type Half (required from
-    // vec_base)
+    // Note: using blendv is very awkward because 0xFFFF is one of
+    // many NaN's in FP16 It's unfortunate that the mask has type Half
+    // (required from vec_base)
 
     // TODO
     // NB: This requires that each value, i.e., each uint value,
@@ -198,16 +130,6 @@ class Vectorized<c10::Half> {
             vreinterpretq_u16_f16(b.values),
             vreinterpretq_u16_f16(a.values)));
     return vec;
-  }
-  template <typename step_t>
-  static Vectorized<c10::Half> arange(
-      c10::Half base = 0.0,
-      step_t step = static_cast<step_t>(1)) {
-    const Vectorized<c10::Half> base_vec(base);
-    const Vectorized<c10::Half> step_vec(step);
-    const Vectorized<c10::Half> step_sizes(
-        0, 1, 2, 3, 4, 5, 6, 7);
-    return fmadd(step_sizes, step_vec, base_vec);
   }
   static Vectorized<c10::Half> set(
       const Vectorized<c10::Half>& a,
@@ -254,33 +176,8 @@ class Vectorized<c10::Half> {
       std::memcpy(ptr, tmp_values, count * sizeof(float16_t));
     }
   }
-  // Very slow implementation of indexing.
-  // Only required because vec256_qint refers to this.
-  // Once we specialize that implementation for ARM
-  // this should be removed. TODO (kimishpatel)
-  c10::Half operator[](int idx) const {
-    __at_align__ c10::Half tmp[size()];
-    store(tmp);
-    return tmp[idx];
-  }
-  c10::Half operator[](int idx) {
-    __at_align__ c10::Half tmp[size()];
-    store(tmp);
-    return tmp[idx];
-  }
   // For boolean version where we want to if any 1/all zero
   // etc. can be done faster in a different way.
-  int zero_mask() const {
-    __at_align__ c10::Half tmp[size()];
-    store(tmp);
-    int mask = 0;
-    for (int i = 0; i < size(); ++i) {
-      if (tmp[i] == 0) {
-        mask |= (1 << i);
-      }
-    }
-    return mask;
-  }
   Vectorized<c10::Half> isnan() const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
     return vreinterpretq_f16_u32(vmvnq_u32(vceqq_f16(values, values)));
@@ -310,14 +207,6 @@ class Vectorized<c10::Half> {
     }
     return false;
   }
-  Vectorized<c10::Half> map(c10::Half (*const f)(c10::Half)) const {
-    __at_align__ c10::Half tmp[size()];
-    store(tmp);
-    for (const auto i : c10::irange(size())) {
-      tmp[i] = f(tmp[i]);
-    }
-    return loadu(tmp);
-  }
   Vectorized<c10::Half> abs() const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
     return Vectorized<c10::Half>(vabsq_f16(values));
@@ -325,126 +214,7 @@ class Vectorized<c10::Half> {
     return map_with_vec_float_method(&Vectorized<float>::abs);
 #endif
   }
-  Vectorized<c10::Half> angle() const {
-    auto zero = Vectorized<c10::Half>(0);
-    auto pi = Vectorized<c10::Half>(c10::pi<c10::Half>);
-    auto tmp = blendv(zero, pi, *this < zero);
-    return blendv(tmp, *this, isnan());
-  }
-  Vectorized<c10::Half> real() const {
-    return *this;
-  }
-  Vectorized<c10::Half> imag() const {
-    return Vectorized<c10::Half>(0);
-  }
-  Vectorized<c10::Half> conj() const {
-    return *this;
-  }
-
-  // Sleef does not support FP16, so many math functions are applied by
-  // converting to FP32, applying the math function, and then converting back to
-  // FP16.
-  Vectorized<c10::Half> acos() const {
-    return map_with_vec_float_method(&Vectorized<float>::acos);
-  }
-  Vectorized<c10::Half> acosh() const {
-    return map_with_vec_float_method(&Vectorized<float>::acosh);
-  }
-  Vectorized<c10::Half> asin() const {
-    return map_with_vec_float_method(&Vectorized<float>::asin);
-  }
-  Vectorized<c10::Half> atan() const {
-    return map_with_vec_float_method(&Vectorized<float>::atan);
-  }
-  Vectorized<c10::Half> atanh() const {
-    return map_with_vec_float_method(&Vectorized<float>::atanh);
-  }
-  Vectorized<c10::Half> atan2(const Vectorized<c10::Half>& exp) const {
-    return map2_with_vec_float_method(exp, &Vectorized<float>::atan2);
-  }
-  Vectorized<c10::Half> copysign(const Vectorized<c10::Half>& sign) const {
-    return map2_with_vec_float_method(sign, &Vectorized<float>::copysign);
-  }
-  Vectorized<c10::Half> erf() const {
-    return map_with_vec_float_method(&Vectorized<float>::erf);
-  }
-  Vectorized<c10::Half> erfc() const {
-    return map_with_vec_float_method(&Vectorized<float>::erfc);
-  }
-  Vectorized<c10::Half> erfinv() const {
-    return map_with_vec_float_method(&Vectorized<float>::erfinv);
-  }
-  Vectorized<c10::Half> exp() const {
-    return map_with_vec_float_method(&Vectorized<float>::exp);
-  }
-  Vectorized<c10::Half> exp2() const {
-    return map_with_vec_float_method(&Vectorized<float>::exp2);
-  }
-  Vectorized<c10::Half> expm1() const {
-    return map_with_vec_float_method(&Vectorized<float>::expm1);
-  }
-  Vectorized<c10::Half> exp_u20() const {
-    return map_with_vec_float_method(&Vectorized<float>::exp_u20);
-  }
-  Vectorized<c10::Half> fmod(const Vectorized<c10::Half>& q) const {
-    // This function is questionable with a conversion, so we use map2
-    return map2(q, std::fmod);
-  }
-  Vectorized<c10::Half> hypot(const Vectorized<c10::Half>& b) const {
-    return map2_with_vec_float_method(b, &Vectorized<float>::hypot);
-  }
-  Vectorized<c10::Half> i0() const {
-    return map_with_vec_float_method(&Vectorized<float>::i0);
-  }
-  Vectorized<c10::Half> i0e() const {
-    return map_with_vec_float_method(&Vectorized<float>::i0e);
-  }
-  Vectorized<c10::Half> digamma() const {
-    return map_with_vec_float_method(&Vectorized<float>::digamma);
-  }
-  Vectorized<c10::Half> igamma(const Vectorized<c10::Half>& x) const {
-    return map2_with_vec_float_method(x, &Vectorized<float>::igamma);
-  }
-  Vectorized<c10::Half> igammac(const Vectorized<c10::Half>& x) const {
-    return map2_with_vec_float_method(x, &Vectorized<float>::igammac);
-  }
-  Vectorized<c10::Half> log() const {
-    return map_with_vec_float_method(&Vectorized<float>::log);
-  }
-  Vectorized<c10::Half> log10() const {
-    return map_with_vec_float_method(&Vectorized<float>::log10);
-  }
-  Vectorized<c10::Half> log1p() const {
-    return map_with_vec_float_method(&Vectorized<float>::log1p);
-  }
-  Vectorized<c10::Half> log2() const {
-    return map_with_vec_float_method(&Vectorized<float>::log2);
-  }
-  Vectorized<c10::Half> nextafter(const Vectorized<c10::Half>& b) const {
-    // This function does not make sense with conversion, so we use map2
-    return map2(b, std::nextafter);
-  }
   Vectorized<c10::Half> frac() const;
-  Vectorized<c10::Half> sin() const {
-    return map_with_vec_float_method(&Vectorized<float>::sin);
-  }
-  Vectorized<c10::Half> sinh() const {
-    return map_with_vec_float_method(&Vectorized<float>::sinh);
-  }
-  Vectorized<c10::Half> cos() const {
-    return map_with_vec_float_method(&Vectorized<float>::cos);
-  }
-  Vectorized<c10::Half> cosh() const {
-    return map_with_vec_float_method(&Vectorized<float>::cosh);
-  }
-  Vectorized<c10::Half> ceil() const {
-    // This function is questionable with a conversion, so we use map
-    return map(at::native::ceil_impl);
-  }
-  Vectorized<c10::Half> floor() const {
-    // This function is questionable with a conversion, so we use map
-    return map(at::native::floor_impl);
-  }
   Vectorized<c10::Half> neg() const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
     return Vectorized<c10::Half>(vnegq_f16(values));
@@ -452,25 +222,12 @@ class Vectorized<c10::Half> {
     return map_with_vec_float_method(&Vectorized<float>::neg);
 #endif
   }
-  inline Vectorized<c10::Half> round() const {
-    // This function is questionable with a conversion, so we use map
-    return map(at::native::round_impl);
-  }
-  inline Vectorized<c10::Half> tan() const {
-    return map_with_vec_float_method(&Vectorized<float>::tan);
-  }
-  inline Vectorized<c10::Half> tanh() const {
-    return map_with_vec_float_method(&Vectorized<float>::tanh);
-  }
   Vectorized<c10::Half> trunc() const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
     return Vectorized<c10::Half>(vrndq_f16(values));
 #else
     return map_with_vec_float_method(&Vectorized<float>::trunc);
 #endif
-  }
-  Vectorized<c10::Half> lgamma() const {
-    return map_with_vec_float_method(&Vectorized<float>::lgamma);
   }
   Vectorized<c10::Half> sqrt() const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
@@ -486,12 +243,6 @@ class Vectorized<c10::Half> {
 #else
     return map_with_vec_float_method(&Vectorized<float>::reciprocal);
 #endif
-  }
-  Vectorized<c10::Half> rsqrt() const {
-    return this->sqrt().reciprocal();
-  }
-  Vectorized<c10::Half> pow(const Vectorized<c10::Half>& exp) const {
-    return map2_with_vec_float_method(exp, &Vectorized<float>::pow);
   }
   Vectorized<c10::Half> operator==(const Vectorized<c10::Half>& other) const {
 #ifdef __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
