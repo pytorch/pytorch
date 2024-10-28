3116a148259d6786e4db9c0baf16bb4c49414ac0
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-10-28 15:12:20 +0000

    Revert "Adds snapshot API for MemPools to get pool memory segments (#133601)"
    This reverts commit 00504aa6b8b0ae68761b89f023184202e8c79bc8.
    
    Reverted https://github.com/pytorch/pytorch/pull/133601 on behalf of https://github.com/wdvr due to reverting for now as this breaks lots of internal tests. Details below ([comment](https://github.com/pytorch/pytorch/pull/133601#issuecomment-2441864871))

--- torch/cuda/memory.py
+++ torch/cuda/memory.py
@@ -980,25 +980,6 @@ def _get_current_allocator() -> _CUDAAllocator:
     return _CUDAAllocator(torch._C._cuda_getAllocator())
 
 
-class MemPoolContext(_MemPoolContext):
-    r"""MemPoolContext holds the currently active pool and stashes the previous
-    pool. On deletion it makes the previous pool active.
-
-    Args:
-        pool(torch.cuda.MemPool): a MemPool object to be made active so that
-        allocations route to this pool.
-
-    """
-
-    def __init__(self, pool: _MemPool):
-        super().__init__(pool)
-
-    @staticmethod
-    def active_pool() -> Optional[_MemPool]:
-        r"""Returns the active MemPool"""
-        return _MemPoolContext.active_pool()
-
-
 class MemPool(_MemPool):
     r"""MemPool represents a pool of memory in a caching allocator. Currently,
     it's just the ID of the pool object maintained in the CUDACachingAllocator.
@@ -1029,23 +1010,24 @@ class MemPool(_MemPool):
         r"""Returns the reference count of this pool."""
         return super().use_count()
 
-    def snapshot(self):
-        r"""Return a snapshot of the CUDA memory allocator pool state across all
-        devices.
 
-        Interpreting the output of this function requires familiarity with the
-        memory allocator internals.
+class MemPoolContext(_MemPoolContext):
+    r"""MemPoolContext holds the currently active pool and stashes the previous
+    pool. On deletion it makes the previous pool active.
 
-        .. note::
-            See :ref:`cuda-memory-management` for more details about GPU memory
-            management.
-        """
-        try:
-            ctx = MemPoolContext(self)
-            snapshot = torch.cuda.memory_snapshot()
-        finally:
-            del ctx
-        return snapshot
+    Args:
+        pool(torch.cuda.MemPool): a MemPool object to be made active so that
+        allocations route to this pool.
+
+    """
+
+    def __init__(self, pool: MemPool):
+        super().__init__(pool)
+
+    @staticmethod
+    def active_pool() -> Optional[_MemPool]:
+        r"""Returns the active MemPool"""
+        return _MemPoolContext.active_pool()
 
 
 @contextlib.contextmanager
