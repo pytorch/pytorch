59de1f047f448e66112be5cf130577e2b1406527
Henry Tsang <henrylhtsang@meta.com>
2024-10-28 11:35:48 -0700

    [aoti] Print output name for sympy.Expr as well (#138524)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/138524
    
    Differential Revision: D64724460

--- torch/_inductor/codegen/cpp_wrapper_cpu.py
+++ torch/_inductor/codegen/cpp_wrapper_cpu.py
@@ -1055,9 +1055,9 @@ class CppWrapperCpu(PythonWrapperCodegen):
                 output_name = f"{output_name_base}_{idx}"
                 self.writeline(f"int64_t {output_name} = {output};")
                 output_args.append(f"&{output_name}")
-            elif isinstance(output, sympy.Symbol):
+            elif isinstance(output, sympy.Expr):
                 output_name = f"{output_name_base}_{idx}"
-                self.writeline(f"auto {output_name} = {output};")
+                self.writeline(f"auto {output_name} = {self.expr_printer(output)};")
                 output_args.append(f"&{output_name}")
             elif output is None:
                 output_args.append("nullptr")
