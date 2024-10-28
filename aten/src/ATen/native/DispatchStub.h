9e29df6a15073395732b6766f39d0f2329e93ea0
cyy <cyyever@outlook.com>
2024-10-28 09:33:57 +0800

    Fix clang-tidy warnings

--- aten/src/ATen/native/DispatchStub.h
+++ aten/src/ATen/native/DispatchStub.h
@@ -378,6 +378,9 @@ struct RegisterPRIVATEUSE1Dispatch {
     name##_DECLARE_DISPATCH_type() = default;                                              \
     name##_DECLARE_DISPATCH_type(const name##_DECLARE_DISPATCH_type&) = delete;            \
     name##_DECLARE_DISPATCH_type& operator=(const name##_DECLARE_DISPATCH_type&) = delete; \
+    name##_DECLARE_DISPATCH_type(name##_DECLARE_DISPATCH_type&&) = delete;                 \
+    name##_DECLARE_DISPATCH_type& operator=(name##_DECLARE_DISPATCH_type&&) = delete;      \
+    ~name##_DECLARE_DISPATCH_type() = default;                                             \
   };                                                                                       \
   extern TORCH_API struct name##_DECLARE_DISPATCH_type name;
 
