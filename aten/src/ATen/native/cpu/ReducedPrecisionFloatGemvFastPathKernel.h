958f58f19765fa1d75ddc3d422e5b317fc5861a5
Scott Wolchok <swolchok@fb.com>
2024-10-28 10:20:42 -0700

    [PyTorch] Move bf16_gemv_trans to ReducedPrecisionFloatGemvFastPathKernel
    Following the previous move of fp16_gemv_trans.
    
    Differential Revision: [D64930872](https://our.internmc.facebook.com/intern/diff/D64930872/)
    
    [ghstack-poisoned]

--- aten/src/ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.h
+++ aten/src/ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.h
@@ -2,6 +2,7 @@
 
 #include <ATen/native/DispatchStub.h>
 #include <c10/macros/Macros.h>
+#include <c10/util/BFloat16.h>
 #include <c10/util/Half.h>
 
 namespace at::native {
@@ -10,5 +11,12 @@ using fp16_dot_fn = float(*)(const Half*, const Half*, int64_t);
 using fp16_gemv_fn = void(*)(int, int, float, const Half*, int, const Half*, int, float, Half*, int);
 DECLARE_DISPATCH(fp16_dot_fn, fp16_dot_with_fp32_arith_stub);
 DECLARE_DISPATCH(fp16_gemv_fn, fp16_gemv_trans_stub);
+
+#ifdef __aarch64__
+using bf16_dot_fn = float(*)(const BFloat16*, const BFloat16*, int64_t);
+using bf16_gemv_fn = void(*)(int, int, BFloat16, const BFloat16*, int, const BFloat16*, int, BFloat16, BFloat16*, int);
+DECLARE_DISPATCH(bf16_dot_fn, bf16_dot_with_fp32_arith_stub);
+DECLARE_DISPATCH(bf16_gemv_fn, bf16_gemv_trans_stub);
+#endif // __aarch64__
 #endif // !defined(C10_MOBILE)
 } // namespace at::native
