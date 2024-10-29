0bcf2983bccda3127d8a2fbc3e4b8af30bbc37b0
Wu, Chunyuan <chunyuan.wu@intel.com>
2024-10-29 14:13:32 +0000

    Update
    [ghstack-poisoned]

--- torch/ao/quantization/quantizer/x86_inductor_quantizer.py
+++ torch/ao/quantization/quantizer/x86_inductor_quantizer.py
@@ -349,9 +349,7 @@ def get_default_x86_inductor_quantization_config(
     act_quantization_spec = QuantizationSpec(
         dtype=torch.uint8,
         quant_min=0,
-        quant_max=127
-        if reduce_range
-        else 255,
+        quant_max=127 if reduce_range else 255,
         qscheme=torch.per_tensor_affine,
         is_dynamic=is_dynamic,
         observer_or_fake_quant_ctr=act_observer_or_fake_quant_ctr.with_args(
