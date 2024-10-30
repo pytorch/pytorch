80639630067369c5b71282f0fd49837e1d27af42
Michael Lazos <mlazos@meta.com>
2024-10-29 17:55:21 -0700

    Fix mypy

--- torch/_dynamo/graph_region_tracker.py
+++ torch/_dynamo/graph_region_tracker.py
@@ -1,11 +1,12 @@
 from collections import defaultdict, deque
-from typing import Dict, List, Set
+from typing import Any, Deque, Dict, List, Optional, Set, Tuple
 
 import torch.fx
 
 
-Region = Set[torch.fx.Node]
-IdenticalNodes = Set[torch.fx.Node]
+Node = torch.fx.Node
+Region = Set[Node]
+IdenticalNodes = Set[Node]
 
 
 # This is typical BFS with the caveat
@@ -14,17 +15,17 @@ IdenticalNodes = Set[torch.fx.Node]
 # The flow is yield a node and check if it's valid for all regions
 # if not valid, discard and continue onto the next node
 class BfsRegionIter:
-    def __init__(self, origin):
-        self._cur_node = (None, origin)
-        self._queue = deque()
+    def __init__(self, origin: Node) -> None:
+        self._cur_node: Tuple[Optional[str], Optional[Node]] = (None, origin)
+        self._queue: Deque[Tuple[Optional[str], Optional[Node]]] = deque()
 
     @staticmethod
-    def create(origin):
+    def create(origin: Node) -> "BfsRegionIter":
         it = BfsRegionIter(origin)
         it.add_children(origin)
         return it
 
-    def next(self):
+    def next(self) -> Tuple[Optional[str], Optional[Node]]:
         ret_node = self._cur_node
         if not self._queue:
             self._cur_node = (None, None)
@@ -32,35 +33,38 @@ class BfsRegionIter:
             self._cur_node = self._queue.popleft()
         return ret_node
 
-    def peek(self):
+    def peek(self) -> Tuple[Optional[str], Optional[Node]]:
         return self._cur_node
 
-    def add_children(self, node):
+    def add_children(self, node: Node) -> None:
+        arg: Any
         for arg in node.args:
-            if isinstance(arg, torch.fx.Node):
+            if isinstance(arg, Node):
                 self._queue.append((None, arg))
 
-        for key, kwarg in node.kwargs:
-            if isinstance(kwarg, torch.fx.Node):
+        key: str
+        kwarg: Any
+        for key, kwarg in node.kwargs.items():
+            if isinstance(kwarg, Node):
                 self._queue.append((key, kwarg))
 
 
 class GraphRegionTracker:
-    def __init__(self):
+    def __init__(self) -> None:
         self.loc_to_duplicates: Dict[str, IdenticalNodes] = defaultdict(set)
-        self.node_to_duplicates: Dict[torch.fx.Node, IdenticalNodes] = {}
+        self.node_to_duplicates: Dict[Node, IdenticalNodes] = {}
 
     @staticmethod
-    def _get_loc_str(filename: str, lineno: int):
+    def _get_loc_str(filename: str, lineno: int) -> str:
         return f"{filename}:{lineno}"
 
-    def track_node(self, filename: str, lineno: int, node: torch.fx.Node):
+    def track_node(self, filename: str, lineno: int, node: Node) -> None:
         loc_str = self._get_loc_str(filename, lineno)
         duplicates = self.loc_to_duplicates[loc_str]
         duplicates.add(node)
         self.node_to_duplicates[node] = duplicates
 
-    def has_same_loc(self, n0, n1):
+    def has_same_loc(self, n0: Node, n1: Node) -> bool:
         return (
             n0 in self.node_to_duplicates
             and n1 in self.node_to_duplicates
@@ -79,7 +83,7 @@ class GraphRegionTracker:
 
         return region_groups
 
-    def fully_expand_region_group(self, regions: List[Region]):
+    def fully_expand_region_group(self, regions: List[Region]) -> None:
         # All regions should start with 1 node
         assert all(len(region) == 1 for region in regions)
         region_iters = []
@@ -87,11 +91,12 @@ class GraphRegionTracker:
             (origin,) = region  # Only works for 1 element sets
             region_iters.append(BfsRegionIter.create(origin))
 
-        nodes_to_add = []
-        seen_nodes = set()
+        nodes_to_add: List[Node] = []
+        seen_nodes: Set[Node] = set()
 
         # arg_name is set for kwargs, None for args
         current_arg_name, current_node = region_iters[0].next()
+        assert current_node is not None
         seen_nodes.add(current_node)
         while current_node:
             add_node = True
@@ -107,10 +112,9 @@ class GraphRegionTracker:
                         and self.has_same_loc(node, current_node)
                     )
                     nodes_to_add.append(node)
+                    seen_nodes.add(node)
                 else:
                     add_node = False
-
-                seen_nodes.add(node)
 
             if add_node:
                 for region, region_it, node in zip(regions, region_iters, nodes_to_add):
@@ -119,5 +123,5 @@ class GraphRegionTracker:
 
             current_arg_name, current_node = region_iters[0].next()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"GraphRegionTracker(loc_to_duplicates={self.loc_to_duplicates}, node_to_duplicates={self.node_to_duplicates})"
