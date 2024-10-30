pytorch/pytorch

commit 214d1dd3baada598b93f13534ee81250b0241f95

[PyTorch] Build ReducedPrecisionFloatGemvFastPathKernel & entry points for non-ARM architectures too (#137917)
Remove reasons to gate it on ARM.

Differential Revision: [D64280687](https://our.internmc.facebook.com/intern/diff/D64280687/)

Pull Request resolved: https://github.com/pytorch/pytorch/pull/137917
Approved by: https://github.com/malfet
ghstack dependencies: #137661, #137911, #137912, #137913, #137914, #137915, #137916

aten/src/ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.cpp
@@ -2,6 +2,7 @@
 #include <ATen/Context.h>
 #include <ATen/Dispatch.h>
 #include <ATen/Parallel.h>
+#include <ATen/cpu/vec/functional.h>
 #include <ATen/cpu/vec/vec.h>
 #include <ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.h>
 #include <c10/macros/Macros.h>
@@ -16,7 +17,7 @@
 
 namespace at::native {
 inline namespace CPU_CAPABILITY {
-#if defined(__aarch64__) && !defined(C10_MOBILE)
+#if !defined(C10_MOBILE)
 
 constexpr auto kF32RegisterPairsPerIteration = 4;
 constexpr auto kF32RegistersPerIteration = kF32RegisterPairsPerIteration * 2;
@@ -58,12 +59,12 @@ constexpr int IntegerLog2(T n, int p = 0) {
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#ifdef __ARM_FEATURE_FP16_SCALAR_ARITHMETIC
+#if !defined(__aarch64__) || defined( __ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
 constexpr auto kF16RegistersPerIteration = 16;
 constexpr auto kF16ElementsPerRegister = vec::Vectorized<Half>::size();
 constexpr auto kF16ElementsPerIteration = kF16RegistersPerIteration * kF16ElementsPerRegister;
 
-static inline float reduce(vec::VectorizedN<Half, kF16RegistersPerIteration>& x) {
+float reduce(vec::VectorizedN<Half, kF16RegistersPerIteration>& x) {
   int offset = kF16RegistersPerIteration;
   c10::ForcedUnroll<IntegerLog2(kF16RegistersPerIteration)>{}([&offset, &x](auto idx) {
     offset /= 2;
@@ -72,7 +73,13 @@ static inline float reduce(vec::VectorizedN<Half, kF16RegistersPerIteration>& x)
     }
   });
   const auto [t0, t1] = vec::convert_half_float(x[0]);
+#if defined(__aarch64__)
   return vaddvq_f32(t0 + t1);
+#else
+  return vec::vec_reduce_all<float>(
+      std::plus<vec::Vectorized<float>>(),
+      t0 + t1);
+#endif
 }
 
 float fp16_dot_with_fp16_arith(const Half* x, const Half* a, int len) {
@@ -105,21 +112,31 @@ static void fp16_gemv_trans_fp16_arith_by_dot_products(const int m, const int n,
   });
 }
 
-#endif // __ARM_FEATURE_FP16_SCALAR_ARITHMETIC
+#endif // !defined(__aarch64__) || defined( __ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
+
+float reduce(vec::Vectorized<float> x) {
+#if defined(__aarch64__)
+  return vaddvq_f32(x);
+#else
+  return vec::vec_reduce_all<float>(
+      std::plus<vec::Vectorized<float>>(),
+      x);
+#endif
+}
 
 // The below reduce overload and fp16_dot_with_fp32_arith are adapted
 // from llama.cpp's ggml_vec_dot_f32 and surrounding utility
 // functions. See NOTE [ GGML Copyright Notice ] above for the
 // required notice.
-static inline float reduce(vec::VectorizedN<float, kF32RegistersPerIteration>& x) {
+float reduce(vec::VectorizedN<float, kF32RegistersPerIteration>& x) {
   int offset = kF32RegistersPerIteration;
   c10::ForcedUnroll<IntegerLog2(kF32RegistersPerIteration)>{}([&offset, &x](auto idx) {
     offset /= 2;
     for (int i = 0; i < offset; ++i) {
-      x[i] = vaddq_f32(x[i], x[offset + i]);
+      x[i] = x[i] + x[offset + i];
     }
   });
-  return vaddvq_f32(x[0]);
+  return reduce(x[0]);
 }
 
 namespace {
@@ -154,8 +171,8 @@ C10_ALWAYS_INLINE void dot_with_fp32_arith_main_inner_loop_no_bfdot(
 }
 
 // Return a + b_low * c_low + b_high * c_high
-inline vec::Vectorized<float> f32_dot_f16(vec::Vectorized<float> a, vec::Vectorized<Half> b, vec::Vectorized<Half> c) {
-#ifdef __ARM_FEATURE_FP16_FML
+vec::Vectorized<float> f32_dot_f16(vec::Vectorized<float> a, vec::Vectorized<Half> b, vec::Vectorized<Half> c) {
+#if defined(__aarch64__) && defined(__ARM_FEATURE_FP16_FML)
   // NOTE: this instruction is an optional instruction in ARM v8.2 and
   // v8.3, but mandatory in v8.4 per
   // https://developer.arm.com/documentation/ddi0596/2021-03/SIMD-FP-Instructions/FMLAL--FMLAL2--vector---Floating-point-fused-Multiply-Add-Long-to-accumulator--vector--?lang=en
@@ -237,8 +254,7 @@ static_assert(
   for (int j = len_aligned; j < len_aligned_vec; j += vec::Vectorized<Half>::size()) { \
     dot_with_fp32_arith_vectorized_tail_inner_loop##bfdot_suffix(vec1, vec2, &tail_sum, j); \
   }                                                                     \
-  auto reduced_tail = vpaddq_f32(tail_sum, tail_sum);                   \
-  reduced_sum += vgetq_lane_f32(vpaddq_f32(reduced_tail, reduced_tail), 0); \
+  reduced_sum += reduce(tail_sum);                                      \
                                                                         \
   /* Second-tier tail fixup: handle all workloads. */                   \
   for (int j = len_aligned_vec; j < len; ++j) {                         \
@@ -288,18 +304,18 @@ void fp16_gemv_trans(
     Half* y,
     const int incy) {
   TORCH_INTERNAL_ASSERT_DEBUG_ONLY(incx == 1 && alpha == 1.0 && beta == 0.0);
-#ifdef __ARM_FEATURE_FP16_SCALAR_ARITHMETIC
+#if !defined(__aarch64__) || defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
   if (at::globalContext().allowFP16ReductionCPU()) {
     return fp16_gemv_trans_fp16_arith_by_dot_products(m, n, a, lda, x, y, incy);
   }
 #endif
   return fp16_gemv_trans_fp32_arith_by_dot_products(m, n, a, lda, x, y, incy);
 }
-#endif // defined(__aarch64__) && !defined(C10_MOBILE)
+#endif // !defined(C10_MOBILE)
 
 } // namespace CPU_CAPABILITY
 
-#if defined(__aarch64__) && !defined(C10_MOBILE)
+#if !defined(C10_MOBILE)
 // NOTE: we don't *need* to go through dispatch for the ARM-only
 // implementation right now, but we will need it when we cover x86.
 REGISTER_DISPATCH(fp16_dot_with_fp32_arith_stub, &fp16_dot_with_fp32_arith);

aten/src/ATen/native/BlasKernel.cpp
@@ -84,23 +84,13 @@ extern "C" void sgemv_(char *trans, int *m, int *n, float *alpha, float *a, int 
 #endif // AT_BUILD_WITH_BLAS
 
 namespace at::native {
+#if !defined(C10_MOBILE)
 DEFINE_DISPATCH(fp16_dot_with_fp32_arith_stub);
 DEFINE_DISPATCH(fp16_gemv_trans_stub);
+#endif // !defined(C10_MOBILE)
 
 namespace blas_impl {
-#if defined(__aarch64__) && !defined(C10_MOBILE)
-void fp16_gemv_notrans(
-    const int m,
-    const int n,
-    const float alpha,
-    const Half* a,
-    const int lda,
-    const Half* x,
-    const int incx,
-    const float beta,
-    Half* y,
-    const int incy);
-
+#if !defined(C10_MOBILE)
 void fp16_gemv_trans(
     const int m,
     const int n,
@@ -118,6 +108,41 @@ float fp16_dot_with_fp32_arith(
     const Half* vec2,
     int64_t len);
 
+float fp16_dot_with_fp32_arith(
+  const Half* x,
+  const Half* a,
+  int64_t len) {
+  return fp16_dot_with_fp32_arith_stub(kCPU, x, a, len);
+}
+
+void fp16_gemv_trans(
+    const int m,
+    const int n,
+    const float alpha,
+    const Half* a,
+    const int lda,
+    const Half* x,
+    const int incx,
+    const float beta,
+    Half* y,
+    const int incy) {
+  fp16_gemv_trans_stub(kCPU, m, n, alpha, a, lda, x, incx, beta, y, incy);
+}
+#endif // !defined(C10_MOBILE)
+
+#if defined(__aarch64__) && !defined(C10_MOBILE)
+void fp16_gemv_notrans(
+    const int m,
+    const int n,
+    const float alpha,
+    const Half* a,
+    const int lda,
+    const Half* x,
+    const int incx,
+    const float beta,
+    Half* y,
+    const int incy);
+
 void bf16_gemv_trans(
     const int m,
     const int n,
@@ -134,7 +159,7 @@ float bf16_dot_with_fp32_arith(
     const at::BFloat16* vec1,
     const at::BFloat16* vec2,
     int64_t len);
-#endif
+#endif // defined(__aarch64__) && !defined(C10_MOBILE)
 
 template <typename scalar_t>
 bool scal_use_fast_path(
@@ -570,27 +595,6 @@ static void bf16_gemv_trans_fp32_arith_by_dot_products(const int m, const int n,
       y[i * incy] = bf16_dot_with_fp32_arith(x, a + lda * i, m);
     }
   });
-}
-
-float fp16_dot_with_fp32_arith(
-  const Half* x,
-  const Half* a,
-  int64_t len) {
-  return fp16_dot_with_fp32_arith_stub(kCPU, x, a, len);
-}
-
-void fp16_gemv_trans(
-    const int m,
-    const int n,
-    const float alpha,
-    const Half* a,
-    const int lda,
-    const Half* x,
-    const int incx,
-    const float beta,
-    Half* y,
-    const int incy) {
-  fp16_gemv_trans_stub(kCPU, m, n, alpha, a, lda, x, incx, beta, y, incy);
 }
 
 void bf16_gemv_trans(

aten/src/ATen/native/cpu/BlasKernel.cpp
@@ -6,22 +6,8 @@
 #include <c10/util/irange.h>
 #include <c10/util/Unroll.h>
 
-#if defined(__aarch64__) && !defined(C10_MOBILE)
-#include <arm_neon.h>
-
+#if !defined(C10_MOBILE)
 namespace at::native::blas_impl {
-void fp16_gemv_notrans(
-    const int m,
-    const int n,
-    const float alpha,
-    const Half* a,
-    const int lda,
-    const Half* x,
-    const int incx,
-    const float beta,
-    Half* y,
-    const int incy);
-
 void fp16_gemv_trans(
     const int m,
     const int n,
@@ -38,6 +24,23 @@ float fp16_dot_with_fp32_arith(
   const Half* x,
   const Half* a,
   int64_t len);
+} // namespace at::native::blas_impl
+#endif
+#if defined(__aarch64__) && !defined(C10_MOBILE)
+#include <arm_neon.h>
+
+namespace at::native::blas_impl {
+void fp16_gemv_notrans(
+    const int m,
+    const int n,
+    const float alpha,
+    const Half* a,
+    const int lda,
+    const Half* x,
+    const int incx,
+    const float beta,
+    Half* y,
+    const int incy);
 
 float bf16_dot_with_fp32_arith(
   const at::BFloat16* x,
@@ -360,20 +363,44 @@ void gemm_notrans_(
   }
 }
 
-
-inline float32x4_t load_as_float32x4(const BFloat16* ptr) {
-  int32x4_t shift = vdupq_n_s32(16);
-  uint32x4_t as_int = vmovl_u16(vld1_u16(reinterpret_cast<const uint16_t *>(ptr)));
-  return vreinterpretq_f32_u32(vshlq_u32(as_int, shift));
+static float compute_dot(const at::BFloat16* a, const at::BFloat16* b, int64_t len) {
+  return at::native::blas_impl::bf16_dot_with_fp32_arith(a, b, len);
 }
 
+template <>
+void gemm_transa_(
+    TransposeType transa,
+    int64_t m, int64_t n, int64_t k,
+    float alpha,
+    const at::BFloat16 *a, int64_t lda,
+    const at::BFloat16 *b, int64_t ldb,
+    float beta,
+    at::BFloat16 *c, int64_t ldc) {
+  // c = alpha * (a.T @ b) + beta * c
+  parallel_for(0, m, 1, [&](int64_t begin, int64_t end) {
+    const auto *a_ = a + begin * lda;
+    for (const auto i : c10::irange(begin, end)) {
+      const auto *b_ = b;
+      for (const auto j : c10::irange(n)) {
+        const auto dot = compute_dot(a_, b_, k);
+        b_ += ldb;
+        if (beta == 0) {
+          c[j*ldc+i] = alpha*dot;
+        } else {
+          c[j*ldc+i] = beta*c[j*ldc+i]+alpha*dot;
+        }
+      }
+      a_ += lda;
+    }
+  });
+}
+
+#endif // defined(__aarch64__) && !defined(C10_MOBILE)
+
+#if !defined(C10_MOBILE)
 static float compute_dot(const at::Half* a, const at::Half* b, int64_t len) {
   return at::native::blas_impl::fp16_dot_with_fp32_arith(
       a, b, len);
-}
-
-static float compute_dot(const at::BFloat16* a, const at::BFloat16* b, int64_t len) {
-  return at::native::blas_impl::bf16_dot_with_fp32_arith(a, b, len);
 }
 
 template <>
@@ -407,36 +434,7 @@ void gemm_transa_(
     }
   });
 }
-
-template <>
-void gemm_transa_(
-    TransposeType transa,
-    int64_t m, int64_t n, int64_t k,
-    float alpha,
-    const at::BFloat16 *a, int64_t lda,
-    const at::BFloat16 *b, int64_t ldb,
-    float beta,
-    at::BFloat16 *c, int64_t ldc) {
-  // c = alpha * (a.T @ b) + beta * c
-  parallel_for(0, m, 1, [&](int64_t begin, int64_t end) {
-    const auto *a_ = a + begin * lda;
-    for (const auto i : c10::irange(begin, end)) {
-      const auto *b_ = b;
-      for (const auto j : c10::irange(n)) {
-        const auto dot = compute_dot(a_, b_, k);
-        b_ += ldb;
-        if (beta == 0) {
-          c[j*ldc+i] = alpha*dot;
-        } else {
-          c[j*ldc+i] = beta*c[j*ldc+i]+alpha*dot;
-        }
-      }
-      a_ += lda;
-    }
-  });
-}
-
-#endif
+#endif // !defined(C10_MOBILE)
 
 template <typename scalar_t, typename opmath_t>
 void gemm_core_(

aten/src/ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <ATen/native/DispatchStub.h>
+#include <c10/macros/Macros.h>
 #include <c10/util/Half.h>
 
 namespace at::native {
+#if !defined(C10_MOBILE)
 using fp16_dot_fn = float(*)(const Half*, const Half*, int64_t);
 using fp16_gemv_fn = void(*)(int, int, float, const Half*, int, const Half*, int, float, Half*, int);
 DECLARE_DISPATCH(fp16_dot_fn, fp16_dot_with_fp32_arith_stub);
 DECLARE_DISPATCH(fp16_gemv_fn, fp16_gemv_trans_stub);
+#endif // !defined(C10_MOBILE)
 } // namespace at::native

commit c37e9f26c807e64363ab1e67aba9231a883e65e8

Unbreak fp16 dot issues caused by #137917
See comment for explanation. In short, doing the fixup in float.

aten/src/ATen/native/cpu/ReducedPrecisionFloatGemvFastPathKernel.cpp
@@ -218,22 +218,6 @@ dot_with_fp32_arith_main_loop_no_bfdot(
   return reduce(sum);
 }
 
-template <typename T>
-struct half_to_float16 {
-  using type = T;
-};
-
-
-#ifdef __aarch64__
-template <>
-struct half_to_float16<Half> {
-  using type = float16_t;
-};
-#endif
-
-template <typename T>
-using half_to_float16_t = typename half_to_float16<T>::type;
-
 static_assert(
     (vec::Vectorized<Half>::size() & (vec::Vectorized<Half>::size() - 1)) == 0,
     "Below code expects power-of-2 vector register size!");
@@ -258,13 +242,10 @@ static_assert(
                                                                         \
   /* Second-tier tail fixup: handle all workloads. */                   \
   for (int j = len_aligned_vec; j < len; ++j) {                         \
-    /* We use half_to_float16_t here because changing to Half was */    \
-    /* causing arithmetic to at fp16 precision, but the necessary */    \
-    /* necessary behavior to pass python test/test_mps.py -k */         \
-    /* test_output_grad_match_nn_functional_linear_cpu_float16 is */    \
-    /* fp32. (I'm not sure exactly why this fixes it.) */               \
-    half_to_float16_t<std::decay_t<decltype(vec1[j])>> x1 = vec1[j];    \
-    half_to_float16_t<std::decay_t<decltype(vec2[j])>> x2 = vec2[j];    \
+    /* Attempting to use Half here caused multiple test failures; */    \
+    /* using float to unbreak. (Suspect we need a scalar FMA.) */       \
+    float x1 = vec1[j];                                                 \
+    float x2 = vec2[j];                                                 \
     reduced_sum += x1 * x2;                                             \
   }                                                                     \
   return reduced_sum