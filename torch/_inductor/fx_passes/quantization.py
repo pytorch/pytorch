6ba79dd9c718e3c884ff214bb3f766797a6639be
Sun, Jiayi <jiayi.sun@intel.com>
2024-10-30 02:50:44 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/quantization.py
+++ torch/_inductor/fx_passes/quantization.py
@@ -552,7 +552,8 @@ def _is_valid_quantized_op_binary_optimization_pattern(
 ):
     # Check if it's a valid Binary Pattern for qconv2d and qlinear:
     # * qop_pointwise should only has one users
-    # * If extra_input_from_dequant is True, extra input of binary node should come from dequant pattern
+    # * If extra_input_from_dequant is True and the two inputs of binary node have the same shape,
+    #   extra input of binary node should come from dequant pattern
     # * the two inputs of binary node should have attribute "meta" and should be tensors
     # * All users of the extra input in this pattern should be
     #   ancestor nodes of the compute node, except for the binary node
@@ -565,23 +566,6 @@ def _is_valid_quantized_op_binary_optimization_pattern(
             return False
         binary_node_inputs = next(iter(compute_node.users)).args
         assert len(binary_node_inputs) == 2, "Expects binary node with 2 inputs"
-        if output_dtype in [torch.float32, torch.bfloat16]:
-            extra_input_of_binary_node = None
-            for arg in binary_node_inputs:
-                if arg != compute_node:
-                    extra_input_of_binary_node = arg
-                    break
-            assert extra_input_of_binary_node is not None
-            # Extra input of binary node comes from dequant pattern
-            if extra_input_from_dequant and (
-                (not isinstance(extra_input_of_binary_node, torch.fx.Node))
-                or (
-                    extra_input_of_binary_node.target
-                    != quantized_decomposed.dequantize_per_tensor.default
-                )
-            ):
-                return False
-
         # the two inputs of binary node should have attribute "meta" and should be tensors
         if not (
             hasattr(binary_node_inputs[0], "meta")
@@ -591,6 +575,30 @@ def _is_valid_quantized_op_binary_optimization_pattern(
             and isinstance(binary_node_inputs[1].meta.get("val", None), torch.Tensor)  # type: ignore[union-attr]
         ):
             return False
+
+        if output_dtype in [torch.float32, torch.bfloat16]:
+            extra_input_of_binary_node = None
+            for arg in binary_node_inputs:
+                if arg != compute_node:
+                    extra_input_of_binary_node = arg
+                    break
+            assert extra_input_of_binary_node is not None
+            # Extra input of binary node comes from dequant pattern
+            if (
+                (
+                    binary_node_inputs[0].meta["val"].size()  # type: ignore[union-attr]
+                    == binary_node_inputs[1].meta["val"].size()  # type: ignore[union-attr]
+                )
+                and extra_input_from_dequant
+                and (
+                    (not isinstance(extra_input_of_binary_node, torch.fx.Node))
+                    or (
+                        extra_input_of_binary_node.target
+                        != quantized_decomposed.dequantize_per_tensor.default
+                    )
+                )
+            ):
+                return False
 
         # All users of the extra input in this pattern should be
         # ancestor nodes of the compute node, except for the binary node
