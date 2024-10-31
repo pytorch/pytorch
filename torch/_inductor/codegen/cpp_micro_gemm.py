c8620974ce6adc4321b5c2d25cb270c2c0b38305
sanchitintel <sanchit.jain@intel.com>
2024-10-31 01:21:03 -0700

    Move num_b_rows computation outside inner loop

--- torch/_inductor/codegen/cpp_micro_gemm.py
+++ torch/_inductor/codegen/cpp_micro_gemm.py
@@ -553,9 +553,9 @@ class CppMicroGemmAMX(CppMicroGemm):
         // Load a tile of B & cache it in L1D.
         {{kernel.unroll_pragma(4)}}
         for (int k = 0; k < K; k += {{block_k}}) {
+            int num_b_rows = (k < last_k_offset) ? 16 : tail_k_size;
             {{kernel.unroll_pragma(2)}}
             for (int tile_col = 0; tile_col <= 1; tile_col++) {
-                int num_b_rows = (k < last_k_offset) ? 16 : tail_k_size;
                 load_B_tile(
                     const_cast<{{input2_t}}*>(B) + n + k * ldb + tile_col * {{16 * vnni_size}},
                     (k / {{block_k // 2}} + tile_col) * num_elements_per_b_tile,
