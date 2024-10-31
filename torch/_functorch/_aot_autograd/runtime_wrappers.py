8134456a276e3f2bc80c91d1142034eb2cd48e36
Simon Fan <xmfan@meta.com>
2024-10-30 18:00:29 -0700

    [aot autograd] refactor CompiledFunction.backward: epilogue (2/N) (#139343)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139343
    Approved by: https://github.com/zou3519
    ghstack dependencies: #139331

--- torch/_functorch/_aot_autograd/runtime_wrappers.py
+++ torch/_functorch/_aot_autograd/runtime_wrappers.py
@@ -1946,21 +1946,7 @@ To fix this, your tensor subclass must implement the dunder method __force_to_sa
 
                         @staticmethod
                         def forward(ctx, *unused_args):
-                            outs = CompiledFunction._backward_impl(ctx, all_args)
-                            # TODO: figure out how to refactor the backward properly
-                            # so I can use aot_dispatch_subclass_wrapper() here.
-                            if CompiledFunction.maybe_subclass_metadata is not None:
-                                assert (
-                                    CompiledFunction.maybe_subclass_metadata.grad_input_metas
-                                    is not None
-                                )
-                                outs_wrapped = wrap_tensor_subclasses(
-                                    outs,
-                                    subclass_metas=CompiledFunction.maybe_subclass_metadata.grad_input_metas,
-                                    included_subclass_symints=False,
-                                )
-                                return outs_wrapped
-                            return outs
+                            return CompiledFunction._backward_impl(ctx, all_args)
 
                         @staticmethod
                         def backward(ctx, *args):
@@ -1977,19 +1963,6 @@ To fix this, your tensor subclass must implement the dunder method __force_to_sa
                 else:
                     out = CompiledFunction._backward_impl(ctx, all_args)
 
-                # TODO: figure out how to refactor the backward properly so I can use aot_dispatch_subclass_wrapper() here.
-                if CompiledFunction.maybe_subclass_metadata is not None:
-                    assert (
-                        CompiledFunction.maybe_subclass_metadata.grad_input_metas
-                        is not None
-                    )
-                    outs_wrapped = wrap_tensor_subclasses(
-                        out,
-                        subclass_metas=CompiledFunction.maybe_subclass_metadata.grad_input_metas,
-                        included_subclass_symints=True,
-                        is_runtime=True,
-                    )
-                    return outs_wrapped
                 return out
 
             @staticmethod
@@ -2011,11 +1984,8 @@ To fix this, your tensor subclass must implement the dunder method __force_to_sa
                     context = torch._C._DisableAutocast if disable_amp else nullcontext
                     with context():
                         out = normalize_as_list(bw_module(*all_args))
-                    # TODO: replace with post_compile wrapper
-                    out = FunctionalizedRngRuntimeWrapper()._functionalized_rng_runtime_epilogue(
-                        CompiledFunction.metadata, out, offset_index=len(out) - 1
-                    )
-                    return tuple(out)
+                    return CompiledFunction._backward_epilogue(ctx, out)
+
                 assert (
                     not backward_state_indices
                 ), "BackwardState requires CompiledAutograd"
@@ -2086,7 +2056,10 @@ To fix this, your tensor subclass must implement the dunder method __force_to_sa
                     steal_args=True,
                     disable_amp=disable_amp,
                 )
+                return CompiledFunction._backward_epilogue(ctx, out)
 
+            @staticmethod
+            def _backward_epilogue(ctx, out):
                 # Toss out the backward output tokens
                 num_bw_tokens = CompiledFunction.metadata.num_backward_tokens
                 if num_bw_tokens > 0:
@@ -2096,7 +2069,22 @@ To fix this, your tensor subclass must implement the dunder method __force_to_sa
                 out = FunctionalizedRngRuntimeWrapper()._functionalized_rng_runtime_epilogue(
                     CompiledFunction.metadata, out, offset_index=len(out) - 1
                 )
-                return tuple(out)
+                out = tuple(out)
+
+                # TODO: figure out how to refactor the backward properly so I can use aot_dispatch_subclass_wrapper() here.
+                if CompiledFunction.maybe_subclass_metadata is not None:
+                    assert (
+                        CompiledFunction.maybe_subclass_metadata.grad_input_metas
+                        is not None
+                    )
+                    outs_wrapped = wrap_tensor_subclasses(
+                        out,
+                        subclass_metas=CompiledFunction.maybe_subclass_metadata.grad_input_metas,
+                        included_subclass_symints=True,
+                        is_runtime=True,
+                    )
+                    return outs_wrapped
+                return out
 
         compiled_function = RuntimeWrapper(
             indices_of_inps_to_detach=indices_of_inps_to_detach,
