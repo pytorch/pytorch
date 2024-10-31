8351fb79c4ba69b6b2723c7f4f0eb5c34b2bae57
eqy <eddiey@nvidia.com>
2024-10-30 18:01:32 -0700

    lint

--- aten/src/ATen/native/cudnn/MHA.cpp
+++ aten/src/ATen/native/cudnn/MHA.cpp
@@ -294,7 +294,7 @@ auto fixSizeOneDimStrideSDPA(
 }
 
 void alloc_with_matching_layout(
-    const Tensor& q, 
+    const Tensor& q,
     Tensor& output,
     const std::vector<int64_t>& shape) {
   TORCH_CHECK(
@@ -311,8 +311,8 @@ void alloc_with_matching_layout(
   const auto q_strides = q.strides();
   std::stable_sort(
       fill_order.begin(), fill_order.end(), [&q_strides](int idx1, int idx2) {
-        return q_strides[idx1] < q_strides[idx2]
-      ;});
+        return q_strides[idx1] < q_strides[idx2];
+      });
   std::vector<int64_t> ordered_strides(shape.size());
   int64_t current_stride = 1;
   for (const int dim_idx : fill_order) {
