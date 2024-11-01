e9a7b07ffe649bba0f2d0044cf08f2d39b40f2f4
Will Constable <whc@meta.com>
2024-10-31 17:46:20 -0700

    [Pipelining] Update schedules to use I, B actions. (#138886)
    Also, update tests to use I (BACKWARD_INPUT) vs B (FULL_BACKWARD)
    consistently.
    
    Previously, schedules would issue a 'B' operation and leave it ambiguous
    whether that operation should be BACKWARD_INPUT or FULL_BACKWARD,
    depending on a separate flag (use_full_backward) passed to the schedule
    class, which would determine which behavior was taken at runtime.
    
    Now, use_full_backward is removed and the schedule class is required to
    produce unambiguous IR.  The logic for 'use_full_backward' is removed
    from the runtime.
    
    _validate_pipeline_order is replaced  with _simulate_comms_compute. Both
    offer similar functionality, to validate the corrrectness of a schedule
    IR.  'validate' operates on compute-only IR, while simulate operates on
    compute + comm IR.  To convert from using validate to simulate, you have
    to first insert comm actions via '_add_send_recv'.
    
    'simulate' was inefficiently written before this PR and needed to be
    optimized to run quickly for extra large schedules with >32 ranks and
    microbatches per rank used in some unit tests.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138886
    Approved by: https://github.com/H-Huang

--- test/distributed/pipelining/test_schedule_multiproc.py
+++ test/distributed/pipelining/test_schedule_multiproc.py
@@ -364,7 +364,7 @@ class ScheduleTest(MultiProcContinousTest):
         "ScheduleClass",
         [ScheduleInterleaved1F1B, ScheduleLoopedBFS, ScheduleInterleavedZeroBubble],
     )
-    @parametrize("use_new_runtime", [False])
+    @parametrize("use_new_runtime", [False, True])
     def test_grad_with_manual_interleaved(self, ScheduleClass, use_new_runtime):
         stages_per_rank = 2
         n_stages = stages_per_rank * self.world_size
@@ -422,7 +422,6 @@ class ScheduleTest(MultiProcContinousTest):
                 num_microbatches,
                 loss_fn=loss_fn,
                 stage_index_to_group_rank=old_schedule.stage_index_to_group_rank,
-                use_full_backward=old_schedule.use_full_backward,
             )
             tmp_schedule._load_actions(old_schedule.pipeline_order)
             # test that csv round-trip works for compute_comms schedule
@@ -431,7 +430,6 @@ class ScheduleTest(MultiProcContinousTest):
                 num_microbatches,
                 loss_fn=loss_fn,
                 stage_index_to_group_rank=old_schedule.stage_index_to_group_rank,
-                use_full_backward=old_schedule.use_full_backward,
             )
             with tempfile.NamedTemporaryFile() as f:
                 tmp_schedule._dump_csv(f.name)
@@ -442,7 +440,6 @@ class ScheduleTest(MultiProcContinousTest):
                 num_microbatches,
                 loss_fn=loss_fn,
                 stage_index_to_group_rank=old_schedule.stage_index_to_group_rank,
-                use_full_backward=old_schedule.use_full_backward,
             )
             one_more_schedule._load_actions(
                 schedule.pipeline_order_with_comms, format="compute_comms"
