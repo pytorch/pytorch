pytorch/pytorch

commit 2fa34418df29c16632b40e07db25fd699e487438

[PyTorch] Hook up fp16_gemv_trans to x86 fp16 GEMM (#137918)
This is the first big milestone we've been building towards!
(Following rev also hooks this up to actual gemv.)
Testing: To check perf, I ran python torchchat.py generate stories110M
--dtype fp16 --device cpu on an x86 machine without AVX512FP16. Observed roughly 5x tokens/sec increase.
Differential Revision: [D64280688](https://our.internmc.facebook.com/intern/diff/D64280688/)

**NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D64280688/)!
Pull Request resolved: https://github.com/pytorch/pytorch/pull/137918
Approved by: https://github.com/malfet
ghstack dependencies: #139082, #139083

aten/src/ATen/native/CPUBlas.cpp
@@ -8,6 +8,7 @@
 #include <c10/util/irange.h>
 
 #include <climits>
+#include <cpuinfo.h>
 
 #if AT_BUILD_WITH_BLAS()
 #if C10_IOS
@@ -351,7 +352,18 @@ void gemm(
    at::Half *c, int64_t ldc) {
    internal::normalize_last_dims(transa, transb, m, n, k, &lda, &ldb, &ldc);
 #if AT_MKLDNN_ENABLED()
-   if (mkldnn_fp16_gemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)) {
+   // Per https://github.com/pytorch/pytorch/pull/137918#discussion_r1825460179 ,
+   // we should not bother checking for !cpuinfo_has_x86_avx512fp16() here,
+   // because "onednn (mkldnn) won't use avx512fp16 to compute gemms by default
+   // because the avx512fp16 fma would incur accuracy loss".
+   const bool fp16_gemv_trans_would_be_faster = cpuinfo_initialize() &&
+     cpuinfo_has_x86_f16c();
+   const bool use_fp16_gemv_trans = fp16_gemv_trans_would_be_faster &&
+     transa == TransposeType::Transpose &&
+     transb == TransposeType::NoTranspose && n == 1 && beta == 0.0 &&
+     alpha == 1.0;
+   if (!use_fp16_gemv_trans &&
+       mkldnn_fp16_gemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)) {
      return;
    }
 #endif

commit 0da10905c51ed1b805c111d6c08f414254f493eb

Fix S390 builds
Caused by https://github.com/pytorch/pytorch/pull/137918
By guarding all cpuinfo use with `!defined(__s390x__ ) && !defined(__powerpc__)`

aten/src/ATen/native/CPUBlas.cpp
@@ -8,7 +8,9 @@
 #include <c10/util/irange.h>
 
 #include <climits>
+#if !defined(__s390x__ ) && !defined(__powerpc__)
 #include <cpuinfo.h>
+#endif
 
 #if AT_BUILD_WITH_BLAS()
 #if C10_IOS