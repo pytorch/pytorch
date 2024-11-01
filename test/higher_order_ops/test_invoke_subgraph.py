47cc7ea927f7c4077e85a9498a35008470cbc84f
Animesh Jain <anijain@umich.edu>
2024-11-01 10:54:43 -0700

    Update base for Update on "[invoke_subgraph] Generate fake_inputs correctly"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames rec
    
    [ghstack-poisoned]

--- test/higher_order_ops/test_invoke_subgraph.py
+++ test/higher_order_ops/test_invoke_subgraph.py
@@ -391,53 +391,6 @@ class GraphModule(torch.nn.Module):
         ):
             opt_fn(x, y)
 
-    def test_ac(self):
-        def fn1(x):
-            return torch.cos(x)
-
-        def fn1_checkpoint(x):
-            return (torch.utils.checkpoint.checkpoint(fn1, x, use_reentrant=False),)
-
-        def fn2(x):
-            return torch.sin(x)
-
-        def fn2_checkpoint(x):
-            return (torch.utils.checkpoint.checkpoint(fn2, x, use_reentrant=False),)
-
-        def fn(x):
-            return (
-                invoke_subgraph(fn1_checkpoint, None, (x,))[0]
-                # repeat the same fn1_checkpoint to see that we dedupe
-                + invoke_subgraph(fn1_checkpoint, None, (x,))[0]
-                # Check that a new fn2_checkpoint goes through a different HOP
-                + invoke_subgraph(fn2_checkpoint, None, (x,))[0]
-            )
-
-        x = torch.randn(8, requires_grad=True)
-        ref = fn(x)
-
-        x_clone = x.clone().detach().requires_grad_(True)
-        backend = AotEagerAndRecordGraphs()
-        res = torch.compile(fn, backend=backend, fullgraph=True)(x_clone)
-
-        # Run backward
-        ref.sum().backward()
-        res.sum().backward()
-
-        self.assertEqual(ref, res)
-        self.assertEqual(x.grad, x_clone.grad)
-
-        # Check that the Dynamo and AOT graphs have just one subgraph module
-        self.assertEqual(len(backend.graphs), 1)
-        self.assertEqual(len(backend.fw_graphs), 1)
-        self.assertEqual(len(backend.bw_graphs), 1)
-        self.count_unique_get_attr_nodes(backend.graphs[0], [], 2)
-        self.count_unique_get_attr_nodes(backend.fw_graphs[0], [], 2)
-        self.count_unique_get_attr_nodes(backend.bw_graphs[0], [], 2)
-
-        res = torch.compile(fn, backend="inductor", fullgraph=True)(x_clone)
-        self.assertEqual(ref, res)
-
 
 if __name__ == "__main__":
     run_tests()
