66d5e2405d54bedc7119dde65eec689423ece961
Jason Ansel <jansel@meta.com>
2024-11-03 15:22:29 -0800

    [inductor] Remove Node.last_usage mutation (#139365)
    I can't figure out why this is needed.  Let's see if tests fail.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139365
    Approved by: https://github.com/shunting314
    ghstack dependencies: #139364

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -475,9 +475,6 @@ class BaseSchedulerNode:
                             V.kernel.mutations.add(input_buf.get_name())
                             V.kernel.mutations.add(buf.get_name())
 
-                        # update last usage of reused node
-                        self.last_usage.discard(input_buf.get_name())
-
                         V.kernel.inplace_update_buffers[
                             buf.get_name()
                         ] = input_buf.get_name()
