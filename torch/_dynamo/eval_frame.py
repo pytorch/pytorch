9e701c7eb3acb5ad224ed586a91d6c2fd8eace5f
Bob Ren <bobren@fb.com>
2024-11-03 19:17:21 -0800

    Update base for Update on "Specialize symfloats irrespective of free symbols"
    This fix was a bit more involved:
    1) It fixes a item_memo loss place.
    2) It fixes a bug where we would specialize bools without guarding correctly.
    3) It updates tensorify to specialize more places now that the aforementioned bug is fixed.
    
    Fixes `PYTORCH_OPINFO_SAMPLE_INPUT_INDEX=6 python test/inductor/test_torchinductor_opinfo.py TestInductorOpInfoCPU.test_comprehensive_linalg_norm_cpu_float16` when `specialize_float=False`
    
    while ensuring `python test/dynamo/test_dynamic_shapes.py DynamicShapesMiscTests.test_runtime_assert_replacement_dynamic_shapes` doesn't regress
    
    cc ezyang SherlockNoMad EikanWang jgong5 wenzhe-nrv voznesenskym penguinwu Guobing-Chen XiaobingSuper zhuhaozhe blzheng jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -399,7 +399,6 @@ class _TorchDynamoContext:
         export=False,
         dynamic=None,
         compiler_config=None,
-        enter_exit_hooks=None,
     ) -> None:
         super().__init__()
         assert callable(callback) or callback is False or callback is None
@@ -411,7 +410,7 @@ class _TorchDynamoContext:
         self._dynamic = dynamic
         self.compiler_config = compiler_config
         self.cleanup_fns: List[Callable[[], Any]] = []
-        self.enter_exit_hooks = enter_exit_hooks or []
+        self.enter_exit_hooks = []
         patch_fn()
 
         # Save the backends so that we can reset them during torch._dynamo.reset
@@ -632,7 +631,6 @@ class OptimizeContext(_TorchDynamoContext):
         rebuild_ctx: Optional[
             Callable[[], Union[OptimizeContext, _NullDecorator]]
         ] = None,
-        enter_exit_hooks=None,
     ) -> None:
         def on_enter():
             install_generation_tagging_init()
@@ -646,7 +644,6 @@ class OptimizeContext(_TorchDynamoContext):
             export=export,
             dynamic=dynamic,
             compiler_config=compiler_config,
-            enter_exit_hooks=enter_exit_hooks,
         )
 
         if config.compiled_autograd:
@@ -745,7 +742,6 @@ def _optimize_catch_errors(
     dynamic=None,
     compiler_config=None,
     rebuild_ctx=None,
-    enter_exit_hooks=None,
 ):
     return OptimizeContext(
         convert_frame.catch_errors_wrapper(compile_fn, hooks),
@@ -755,7 +751,6 @@ def _optimize_catch_errors(
         dynamic=dynamic,
         compiler_config=compiler_config,
         rebuild_ctx=rebuild_ctx,
-        enter_exit_hooks=enter_exit_hooks,
     )
 
 
@@ -827,7 +822,6 @@ def _optimize(
     guard_fail_fn=None,
     disable=False,
     dynamic=None,
-    enter_exit_hooks=None,
 ) -> Union[OptimizeContext, _NullDecorator]:
     """
     The main entrypoint of TorchDynamo.  Do graph capture and call
@@ -881,7 +875,6 @@ def _optimize(
             dynamic=dynamic,
             hooks=hooks,
             rebuild_ctx=rebuild_ctx,
-            enter_exit_hooks=enter_exit_hooks,
         )
     # The backend function is stashed in the callable returned by
     # _optimize_catch_errors in the field _torchdynamo_orig_callable. This can
@@ -897,7 +890,6 @@ def _optimize(
             else None
         ),
         rebuild_ctx=rebuild_ctx,
-        enter_exit_hooks=enter_exit_hooks,
     )
 
 
@@ -1723,7 +1715,6 @@ def optimize_assert(
     export_constraints=None,
     dynamic=None,
     rebuild_ctx=None,
-    enter_exit_hooks=None,
 ):
     """
     The same as `torch._dynamo.optimize(backend, nopython=True)`
@@ -1742,7 +1733,6 @@ def optimize_assert(
         export=export,
         dynamic=dynamic,
         rebuild_ctx=rebuild_ctx,
-        enter_exit_hooks=enter_exit_hooks,
     )
 
 
