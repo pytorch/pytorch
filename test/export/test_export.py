69ffddd8e53b2e120cacdd91a7142e6bd0694c88
Henry Tsang <henrylhtsang@meta.com>
2024-11-04 23:31:46 +0000

    [export] Add support for symbool to make it usable for torch.cond (#138765)
    # Why?
    
    I want the following code to work.
    
    minimal repro:
    ```
    class M(torch.nn.Module):
        def forward(self, dilate_flag):
            return dilate_flag.item()
    
    input1 = (torch.tensor([1], dtype=torch.bool, device="cuda"),)
    model = M().cuda()
    
    ep = torch.export.export(model, input1, strict=True)
    path = torch._inductor.aot_compile(ep.module(), input1)
    aot_model = torch._export.aot_load(path, device="cuda")
    actual_output = aot_model(*input1)
    ```
    
    error: AssertionError: Encountered an unsupported object of type <class 'torch.SymBool'> while writing the metadata for exported program
    
    second error will be handled by https://github.com/pytorch/pytorch/pull/138760
    
    # Motivation
    
    I could technically bypass it with a torch.int tensor. However, it doesn't work with torch.cond. I want the following to work. It would also require https://github.com/pytorch/pytorch/pull/138760 for aot compile to work.
    
    ```
    class M(torch.nn.Module):
        def __init__(self) -> None:
            super().__init__()
            self.dilate_flag = 0
    
        def forward(self, dilate_flag):
            self.dilate_flag = dilate_flag.item()
    
            def true_fn(dilate_flag):
                return dilate_flag.clone()
    
            def false_fn(dilate_flag):
                return dilate_flag.clone()
    
            torch.cond(
                self.dilate_flag,
                true_fn,
                false_fn,
                (dilate_flag,),
            )
            return self.dilate_flag
    
    input1 = (torch.tensor([1], dtype=torch.bool, device="cuda"),)
    input2 = (torch.tensor([0], dtype=torch.bool, device="cuda"),)
    inputs = (input1, input2)
    model = M().cuda()
    
    for input in inputs:
        expected_output = model(*input)
    
        ep = torch.export.export(model, input, strict=False)
        path = torch._inductor.aot_compile(ep.module(), input)
        aot_model = torch._export.aot_load(path, device="cuda")
        actual_output = aot_model(*input)
    
        assert (
            expected_output == actual_output
        ), f"henry they are not equal {expected_output} != {actual_output}"
    ```
    
    Differential Revision: D64867504
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138765
    Approved by: https://github.com/ydwu4

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -843,6 +843,28 @@ graph():
                 foo, bad_example_inp, dynamic_shapes=dynamic_shapes, strict=False
             )
 
+    def test_symint_item(self):
+        class M(torch.nn.Module):
+            def forward(self, tensor):
+                return tensor.item()
+
+        input = (torch.tensor([1], dtype=torch.int),)
+
+        orig_res = M()(*input)
+        ep_res = torch.export.export(M(), input).module()(*input)
+        self.assertEqual(orig_res, ep_res)
+
+    def test_symbool_item(self):
+        class M(torch.nn.Module):
+            def forward(self, tensor):
+                return tensor.item()
+
+        input = (torch.tensor([1], dtype=torch.bool),)
+
+        orig_res = M()(*input)
+        ep_res = torch.export.export(M(), input).module()(*input)
+        self.assertEqual(orig_res, ep_res)
+
     def test_unbacked_to_cond(self):
         class M(torch.nn.Module):
             def forward(self, a):
@@ -5319,6 +5341,28 @@ def forward(self, b_a_buffer, x):
 
         inp = (torch.tensor(5),)
         self.assertTrue(torch.allclose(ep.module()(*inp), Foo()(*inp)))
+
+        transform = ep.run_decompositions()
+        self.assertEqual(len(ep.graph_signature.input_specs), 4)
+        self.assertTrue(torch.allclose(ep.module()(*inp), transform.module()(*inp)))
+
+        class Boo(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+                self.a = torch.tensor(True)
+
+            def forward(self, x):
+                list_tensor = [torch.tensor(False), torch.tensor(True)]
+                return x + self.a + list_tensor[0] + list_tensor[1]
+
+        ep = export(Boo(), (torch.tensor(False),))
+
+        self.assertEqual(len(ep.graph_signature.input_specs), 4)
+        self.assertEqual(len(ep.state_dict), 0)
+        self.assertEqual(len(ep.constants), 3)
+
+        inp = (torch.tensor(True),)
+        self.assertTrue(torch.allclose(ep.module()(*inp), Boo()(*inp)))
 
         transform = ep.run_decompositions()
         self.assertEqual(len(ep.graph_signature.input_specs), 4)
