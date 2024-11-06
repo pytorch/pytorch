ae090000b991d01962a9f85784b9ed40bedc08e3
James Wu <jjwu@meta.com>
2024-11-06 16:34:00 +0000

    Allow OpOverloadPackets as safe torch functions, sanitize dynamo gm before running aotdispatch with cache (#139785)
    Summary:
    This diff implements two things to improve cache hit rates after testing AOTAutogradCache with internal cogwheel jobs:
    - We should allow torch functions that are OpOverloadPackets
    - When running with cache, there are some fields that dynamo puts into the input graph module to aotdispatch that are not stable between runs. We use a context manager to null these out so that they can't be used to affect the output of AOTAutograd, and then we put the fields back onto the gm before returning from AOTAutogradCache.load().
    
    Test Plan:
    New unit tests + running nanogpt with AOTAutogradCache.
    
    Meta:
    
    Run on a long running job
    Cache miss:
     {F1953831996}
    
    Cache hit:
     {F1953830872}
    
    Servicelabs here:
    https://www.internalfb.com/servicelab/experiment/4301352991/
    
    Cache hit:
    https://interncache-all.fbcdn.net/manifold/tlparse_reports/tree/logs/f660597709-TrainingApplication/attempt_0/version_0/rank_0/index.html
    
    Cache miss:
    https://interncache-all.fbcdn.net/manifold/tlparse_reports/tree/logs/f660569960-TrainingApplication/attempt_0/version_0/rank_0/index.html
    
    We can see that with these changes, autograd cache hits and saves compile time:
    https://fburl.com/scuba/pt2_compile_events/ycddxstd
    
    Differential Revision: D65436373
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139785
    Approved by: https://github.com/bdhirsh

--- test/dynamo/test_aot_autograd_cache.py
+++ test/dynamo/test_aot_autograd_cache.py
@@ -14,6 +14,7 @@ from torch._functorch._aot_autograd.autograd_cache import (
     AOTAutogradCache,
     autograd_cache_key,
     BypassAOTAutogradCache,
+    sanitize_gm_for_cache,
 )
 from torch._functorch._aot_autograd.schemas import AOTConfig
 from torch._inductor import config as inductor_config
@@ -775,6 +776,31 @@ class AOTAutogradCachePicklerTests(torch._dynamo.test_case.TestCase):
 
         config = self.default_config()
         self.gen_cache_key(fn, config)
+
+    def test_sanitize_gm_for_cache(self):
+        def fn(x):
+            y = torch.sin(x)
+            z = torch.cos(x)
+            w = y + z
+            w.abs()
+            return w
+
+        _, fx_g, example_inputs = self._get_dynamo_output(fn, torch.ones(3))
+        fx_g.meta = {"foo": "bar"}
+        fx_g.compile_subgraph_reason = "Blah"
+        config = self.default_config()
+        with sanitize_gm_for_cache(fx_g):
+            c1 = autograd_cache_key(fx_g, example_inputs, config, {})
+        c3 = autograd_cache_key(fx_g, example_inputs, config, {})
+
+        fx_g.meta = {"foo": "baz"}
+        fx_g.compile_subgraph_reason = None
+        with sanitize_gm_for_cache(fx_g):
+            c2 = autograd_cache_key(fx_g, example_inputs, config, {})
+        c4 = autograd_cache_key(fx_g, example_inputs, config, {})
+
+        self.assertEqual(c1, c2)
+        self.assertNotEqual(c3, c4)
 
 
 if __name__ == "__main__":
