ac8af713f518be16767580cb8733aae678b2ae13
Justin Chu <justinchuby@users.noreply.github.com>
2024-11-05 16:27:22 -0800

    [ONNX] WIP safetensors load

--- torch/onnx/_internal/fx/patcher.py
+++ torch/onnx/_internal/fx/patcher.py
@@ -10,15 +10,13 @@ if TYPE_CHECKING:
     import io
 
 
-# TODO: Remove after https://github.com/huggingface/safetensors/pull/318
 @functools.lru_cache(None)
-def has_safetensors_and_transformers():
+def _has_safetensors():
     try:
         # safetensors is not an exporter requirement, but needed for some huggingface models
-        import safetensors  # type: ignore[import]  # noqa: F401
-        import transformers  # type: ignore[import]  # noqa: F401
-        from safetensors import torch as safetensors_torch  # noqa: F401
+        import safetensors  # type: ignore[import]
 
+        del safetensors
         return True
     except ImportError:
         return False
@@ -69,9 +67,8 @@ class ONNXTorchPatcher:
         # Wrapper or modified version of torch functions.
         self.torch_load_wrapper = torch_load_wrapper
 
-        if has_safetensors_and_transformers():
+        if _has_safetensors():
             import safetensors
-            import transformers
 
             def safetensors_load_file_wrapper(filename, device="cpu"):
                 # Record path for later serialization into ONNX proto
@@ -119,7 +116,7 @@ class ONNXTorchPatcher:
             desired_wrapped_methods.append((torch.Tensor, "__getitem__"))
         torch.fx._symbolic_trace._wrapped_methods_to_patch = desired_wrapped_methods
 
-        if has_safetensors_and_transformers():
+        if _has_safetensors():
             import safetensors
             import transformers
 
@@ -133,7 +130,7 @@ class ONNXTorchPatcher:
         torch.fx._symbolic_trace._wrapped_methods_to_patch = (
             self.torch_fx__symbolic_trace__wrapped_methods_to_patch
         )
-        if has_safetensors_and_transformers():
+        if _has_safetensors():
             import safetensors
             import transformers
 
