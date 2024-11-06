46f144e1b2c603f31b000d330969dbc9bf58856c
Nichols A. Romero <nick.romero@amd.com>
2024-11-06 14:37:45 +0000

    [ROCM] Fix hipBLASLt version check in TunableOp test (#139811)
    Allow 3 or more digits for hipBLASLt version check in TunableOp test. Needed due to upcoming ROCm 6.3 release.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139811
    Approved by: https://github.com/eqy, https://github.com/malfet

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -4544,7 +4544,7 @@ class TestLinalg(TestCase):
             validators[key] = value
         if torch.version.hip:
             assert "HIPBLASLT_VERSION" in validators
-            assert re.match(r'^\d{3}-[a-z0-9]{8}$', validators["HIPBLASLT_VERSION"])
+            assert re.match(r'^\d{3,}-[a-z0-9]{8}$', validators["HIPBLASLT_VERSION"])
         assert len(torch.cuda.tunable.get_results()) > 0
 
         assert torch.cuda.tunable.write_file()  # use default filename
