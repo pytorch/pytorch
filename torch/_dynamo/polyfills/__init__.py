dfb8d275de27c84d54f8c0e47179d00eae5aad9f
Yuanhao Ji <jiyuanhao@apache.org>
2024-11-08 15:39:44 +0800

    update

--- torch/_dynamo/polyfills/__init__.py
+++ torch/_dynamo/polyfills/__init__.py
@@ -186,7 +186,7 @@ def addcmul_inplace(self, tensor1, tensor2, value):
     return self.add_(tensor1 * tensor2 * value)
 
 
-def predicate(obj):
+def predicate(obj: Any) -> bool:
     # This will cause the rest of dynamo to handle the if statement correctly, so we don't have to rewrite it here.
     # We can't just use bool() here since we can't trace into that in general.
     if obj:
