beae7725be141f4539d87a58b455b5467118dcdf
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-11-08 23:44:41 +0000

    Revert "Tighten type hints for tensor arithmetic (#135392)"
    This reverts commit d3788190685685cb828bdf6bed90270c0b60affc.
    
    Reverted https://github.com/pytorch/pytorch/pull/135392 on behalf of https://github.com/ZainRizvi due to Sorry but this is breaking internally. See D65641103 for more details ([comment](https://github.com/pytorch/pytorch/pull/135392#issuecomment-2465906839))

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -2291,8 +2291,7 @@ def native_batch_norm_backward(
     mean = save_mean_cast
     invstd = save_invstd_cast
     if train:
-        assert mean is not None and invstd is not None
-
+        assert save_mean_cast is not None and save_invstd_cast is not None
     else:
         assert running_mean_cast is not None and running_var_cast is not None
         mean = running_mean_cast
