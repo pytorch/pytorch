8c85bf9f4e4d1ed2a719fb485e414608d9376830
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-11-11 17:02:05 +0000

    Revert "[SymmetricMemory] improve the API for stream_write_value32 (#139934)"
    This reverts commit 2f3a5a15ef701ffab9a880cf822ff8e5224a4b33.
    
    Reverted https://github.com/pytorch/pytorch/pull/139934 on behalf of https://github.com/malfet due to Broke distributed tests, see https://github.com/pytorch/pytorch/actions/runs/11770673088/job/32784210441 ([comment](https://github.com/pytorch/pytorch/pull/139934#issuecomment-2468641512))

--- torch/csrc/distributed/c10d/CUDASymmetricMemoryOps.cu
+++ torch/csrc/distributed/c10d/CUDASymmetricMemoryOps.cu
@@ -513,16 +513,8 @@ at::Tensor memset32_(
       "symm_mem::memset32_: input must be a flat, contiguous uint32 tensor.");
 
   TORCH_CHECK(
-      offset >= 0,
-      "symm_mem::memset32_: offset must be greater than or equal to 0 (got ",
-      offset,
-      ")");
-
-  TORCH_CHECK(
-      count > 0,
-      "symm_mem::memset32_: count must be a positive integer (got ",
-      count,
-      ")");
+      offset > 0 && count > 0,
+      "symm_mem::memset32_: offset and count must be positive integers.");
 
   TORCH_CHECK(
       val >= 0 &&
@@ -548,59 +540,6 @@ at::Tensor memset32_(
       val,
       count,
       at::cuda::getCurrentCUDAStream()));
-#else
-  TORCH_CHECK(
-      false, "CUDASymmetricMemory requires PYTORCH_C10_DRIVER_API_SUPPORTED");
-#endif
-  return input;
-}
-
-at::Tensor stream_write_value32_(
-    at::Tensor& input,
-    int64_t offset,
-    int64_t val) {
-#if !defined(USE_ROCM) && defined(PYTORCH_C10_DRIVER_API_SUPPORTED)
-  TORCH_CHECK(
-      input.dim() == 1 && input.is_contiguous() &&
-          input.scalar_type() == c10::ScalarType::UInt32,
-      "symm_mem::stream_write_value32_: input must be a flat, contiguous "
-      "uint32 tensor.");
-
-  TORCH_CHECK(
-      offset >= 0,
-      "symm_mem::stream_write_value32_: offset must be greater than or "
-      "equal to 0 (got ",
-      offset,
-      ")");
-
-  TORCH_CHECK(
-      val >= 0 &&
-          static_cast<size_t>(val) <= std::numeric_limits<uint32_t>::max(),
-      "symm_mem::stream_write_value32_: "
-      "val must be in the range of [0, 4294967295] (uint32_t).")
-
-  auto element_size = c10::elementSize(input.scalar_type());
-  TORCH_CHECK(
-      offset < input.numel(),
-      "symm_mem::stream_write_value32_: offset (",
-      offset,
-      ") exceeded the numel of the input (",
-      input.numel(),
-      ")");
-
-  auto addr = reinterpret_cast<uint32_t*>(input.data_ptr()) + offset;
-
-  c10::cuda::CUDAGuard guard(input.device());
-  auto driver_api = c10::cuda::DriverAPI::get();
-  // According to the documentation of CUstreamWriteValue_flags,
-  // cuStreamWriteValue32 will provide a memory fence before the write, which
-  // has similar semantics to __threadfence_system() but is scoped to the
-  // stream rather than a CUDA thread.
-  C10_CUDA_DRIVER_CHECK(driver_api->cuStreamWriteValue32_(
-      at::cuda::getCurrentCUDAStream(),
-      reinterpret_cast<CUdeviceptr>(addr),
-      val,
-      0));
 #else
   TORCH_CHECK(
       false, "CUDASymmetricMemory requires PYTORCH_C10_DRIVER_API_SUPPORTED");
@@ -672,11 +611,6 @@ TORCH_LIBRARY_FRAGMENT(symm_mem, m) {
       {at::Tag::pt2_compliant_tag});
 
 #endif
-  m.def(
-      "stream_write_value32_(Tensor(a!) input, int offset, int val) -> Tensor(a!)",
-      torch::dispatch(c10::DispatchKey::CUDA, ::stream_write_value32_),
-      {at::Tag::pt2_compliant_tag});
-
   m.def(
       "memset32_(Tensor(a!) input, int offset, int val, int count) -> Tensor(a!)",
       torch::dispatch(c10::DispatchKey::CUDA, ::memset32_),
