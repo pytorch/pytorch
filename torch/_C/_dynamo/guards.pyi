ef7da6efdfe09c51165a3a5cf19370829f807dfd
Animesh Jain <anijain@umich.edu>
2024-11-11 14:06:47 -0800

    Update base for Update on "[dynamo] skip_guard_eval_unsafe stance for power users"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_C/_dynamo/guards.pyi
+++ torch/_C/_dynamo/guards.pyi
@@ -1,5 +1,5 @@
 # mypy: allow-untyped-defs
-from typing import Any, Dict, Callable
+from typing import Any, Callable, Dict
 
 import torch
 
@@ -77,7 +77,9 @@ class RootGuardManager(GuardManager):
         guard: LeafGuard,
         verbose_code_parts: list[str],
     ) -> None: ...
-    def clone(self, clone_filter_fn: Callable[[GuardManager], bool]) -> RootGuardManager: ...
+    def clone_manager(
+        self, clone_filter_fn: Callable[[GuardManager], bool]
+    ) -> RootGuardManager: ...
 
 class DictGuardManager(GuardManager):
     def get_key_manager(
