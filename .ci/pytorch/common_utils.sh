93547bdcafc331673376bdeb9964d3156b64d9f5
Eli Uriegas <eliuriegas@meta.com>
2024-11-12 10:06:43 -0800

    Switch to python3 -m pip
    Signed-off-by: Eli Uriegas <eliuriegas@meta.com>

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -81,7 +81,7 @@ function pip_install_whl() {
 
 function pip_install() {
   # retry 3 times
-  pip_install_pkg="pip install --progress-bar off"
+  pip_install_pkg="python3 -m pip install --progress-bar off"
   ${pip_install_pkg} $@ || \
     ${pip_install_pkg} $@ || \
     ${pip_install_pkg} $@
