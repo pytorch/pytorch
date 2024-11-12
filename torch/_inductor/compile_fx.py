03d059542981b071028737e43707dc96ae0e6614
angelayi <yiangela7@gmail.com>
2024-11-12 21:36:24 +0000

    [aoti] Remove dir after packaging (#140022)
    Update AOTI to return a list of files that it generates when `aot_inductor.package=True`. Then we will only package the files that are in that list.
    
    This should fix the [caching issue](https://fb.workplace.com/groups/1028545332188949/permalink/1081702043539944/) and hopefully https://github.com/pytorch/pytorch/issues/140053.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140022
    Approved by: https://github.com/larryliu0820, https://github.com/desertfire, https://github.com/malfet

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -5,7 +5,6 @@ import functools
 import io
 import itertools
 import logging
-import os
 import sys
 import time
 import warnings
@@ -649,7 +648,7 @@ def _compile_fx_inner(
         """
         with _WaitCounter("pytorch.wait_counter.actual_codegen_and_compile").guard():
             compiled_graph = fx_codegen_and_compile(gm, example_inputs, **fx_kwargs)
-            if isinstance(compiled_graph, str):
+            if isinstance(compiled_graph, str) or fx_kwargs["aot_mode"]:
                 # We only return a string in aot mode, in which case we don't
                 # need to do any post-compilation steps: we just return the string,
                 # which is the filename of the compiled code.
@@ -1222,17 +1221,25 @@ def compile_fx_aot(
     example_inputs_: List[InputType],
     inner_compile: _CompileFxCallableEx = compile_fx_inner,
     config_patches: Optional[Dict[str, str]] = None,
-) -> str:
+) -> Union[List[str], str]:
     config_patches: Dict[str, Any] = (
         {"cpp_wrapper": True}
         if config_patches is None
         else {**config_patches, "cpp_wrapper": True}
     )
 
-    if (
-        "aot_inductor.output_path" not in config_patches
-        and not config.aot_inductor.output_path
-    ):
+    output_path = config_patches.get(
+        "aot_inductor.output_path", config.aot_inductor.output_path
+    )
+
+    if output_path:
+        assert not output_path.endswith(".pt2"), (
+            "The output path for aot_compile should not have an extension with .pt2 "
+            "this is for specifying the output path for the .so in AOTInductor. "
+            "If you would like to package the AOTInductor generated files "
+            "into a pt2, please call `torch._inductor.aoti_compile_and_package`."
+        )
+    else:
         config_patches = {
             **config_patches,
             "aot_inductor.output_path": code_hash(model_.code),
@@ -1244,7 +1251,7 @@ def compile_fx_aot(
     with V.set_aot_compilation(True), torch._guards.compile_context(
         saved_compile_context
     ):
-        compiled_lib_path = compile_fx(
+        compiled_artifacts = compile_fx(
             model_,
             example_inputs_,
             inner_compile=functools.partial(
@@ -1254,11 +1261,12 @@ def compile_fx_aot(
             ),
             config_patches=config_patches,
         )
-        assert isinstance(compiled_lib_path, str)
-        assert os.path.exists(
-            compiled_lib_path
-        ), f"AOTInductor compiled library does not exist at {compiled_lib_path}"
-        return compiled_lib_path
+
+        assert isinstance(compiled_artifacts, str) or (
+            isinstance(compiled_artifacts, list)
+            and isinstance(compiled_artifacts[0], str)
+        )
+        return compiled_artifacts
 
 
 _graph_counter = count(0)
