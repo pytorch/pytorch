f0a182142fec080b23b87d9be31bd46cc42d4403
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-11-12 23:27:29 +0000

    add test

--- test/inductor/test_ck_backend.py
+++ test/inductor/test_ck_backend.py
@@ -280,13 +280,22 @@ class TestCKBackend(TestCase):
     @parametrize("dtype", (torch.bfloat16,))
     @parametrize("use_fast_accum", (True,))
     @parametrize("quantize_type", ("tensorwise", "rowwise"))
+    @parametrize("has_bias", (True, False))
     def test_max_autotune_scaled_mm(
-        self, max_autotune_gemm_backends, dtype, use_fast_accum, quantize_type
+        self, max_autotune_gemm_backends, dtype, use_fast_accum, quantize_type, has_bias
     ):
         tensor_options = {"device": "cuda", "dtype": dtype}
 
-        x = torch.randn(2240, 256, **tensor_options)
-        w = torch.randn(2048, 256, **tensor_options)
+        M = 2240
+        N = 2048
+        K = 256
+
+        x = torch.randn(M, K, **tensor_options)
+        w = torch.randn(K, N, **tensor_options)
+
+        bias = None
+        if has_bias:
+            bias = torch.randn(N, **tensor_options)
 
         dtype_float8 = torch.float8_e4m3fnuz
 
@@ -303,8 +312,6 @@ class TestCKBackend(TestCase):
         x_fp8, x_inverse_scale = f_quantize(x, dtype_float8)
 
         assert "rocm" in dir(config)
-
-        bias = None
 
         def linear(x_fp8, x_inverse_scale, w_t_fp8, w_inverse_scale, bias):
             y = torch._scaled_mm(
