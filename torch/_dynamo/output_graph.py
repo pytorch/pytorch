99ee354952f4d9ea9c843b1b806bfcbba68afa0b
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-11-13 18:47:51 +0000

    Revert "[logging] Overhaul dynamo_timed and CompilationMetrics logging. (#139849)"
    This reverts commit cb15c1515778499ae801dcf67d55c8bdab4724ef.
    
    Reverted https://github.com/pytorch/pytorch/pull/139849 on behalf of https://github.com/kit1980 due to Breaking an internal tests + there is a bug according to the author ([comment](https://github.com/pytorch/pytorch/pull/139849#issuecomment-2474459094))

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -1394,7 +1394,6 @@ class OutputGraph:
             "OutputGraph.call_user_compiler",
             phase_name="backend_compile",
             log_pt2_compile_event=True,
-            dynamo_compile_column_us="aot_autograd_cumulative_compile_time_us",
         ):
             return self._call_user_compiler(gm)
 
