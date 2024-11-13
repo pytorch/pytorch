42d9da899707960a253629657fb0398ed6749bd0
Benjamin Glass <bglass@quansight.com>
2024-11-13 22:27:00 +0000

    Update
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -5254,7 +5254,6 @@ class CommonTemplate:
         if self.device != "cpu":
             assertGeneratedKernelCountEqual(self, 1)
 
-    @skip_if_cpp_wrapper
     def test_complex_fallback(self):
         def fn(x):
             return x * x + 10
