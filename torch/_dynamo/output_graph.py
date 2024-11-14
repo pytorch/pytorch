f6550c9dfc6b8c8bd05db7f3591a74b74f80a3ff
Bob Ren <bobren@fb.com>
2024-11-13 17:48:59 -0800

    Update on "Restart dynamo analysis when we fail to tensorify away all symfloat inputs"
    Fixes a bunch of benchmarks that failed with cudagraph errors including `tlp python benchmarks/dynamo/timm_models.py --device cuda --inductor --accuracy --amp --training --only resmlp_12_224` when `specialize_float=False`
    
    
    [ghstack-poisoned]

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -1309,16 +1309,6 @@ class OutputGraph:
             counters["stats"]["calls_captured"] += ncalls
 
             self.remove_tensorify_specialized_graphargs()
-            # This is a pretty interesting function. Basically we have this problem
-            # where our compiler tends to choke when we have unused inputs. The way
-            # we support dynamic float arguments is by doing a joint fx pass and
-            # tensorifying away as many symfloats as we can. For the remaining symfloats
-            # we have no choice but to specialize... HOWEVER at that point in time
-            # we can no longer remove graph inputs. So our sledgehammer solution is to
-            # save the state of what inputs we should have specialized in dynamo and
-            # restart analysis. This function incorporates this "view from the future"
-            # state and specializes inputs that we know we won't be able to tensorify
-            # away in the joint pass.
 
             # free a bit of memory
             self.real_value_cache.clear()
