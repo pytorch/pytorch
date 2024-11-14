868e6a915996b847b906a3f58a6149557bbfa157
Yifu Wang <yifu@fb.com>
2024-11-14 08:41:03 -0800

    [SymmetricMemory] fix a bug where get_signal_pad() returns a tensor backed by a buffer ptr instead of a signal_pad ptr (#140128)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140128
    Approved by: https://github.com/lw
    ghstack dependencies: #140127

--- test/distributed/test_symmetric_memory.py
+++ test/distributed/test_symmetric_memory.py
@@ -199,6 +199,9 @@ class SymmetricMemoryTest(MultiProcessTestCase):
         symm_mem = _SymmetricMemory.rendezvous(t)
         peer_rank = (self.rank + 1) % self.world_size
 
+        signal_pad = symm_mem.get_signal_pad(self.rank)
+        self.assertEqual(signal_pad.data_ptr(), symm_mem.signal_pad_ptrs[symm_mem.rank])
+
         signal_pad = symm_mem.get_signal_pad(peer_rank)
         self.assertEqual(signal_pad.dtype, torch.uint32)
         self.assertEqual(signal_pad.numel(), symm_mem.signal_pad_size // 4)
@@ -217,6 +220,22 @@ class SymmetricMemoryTest(MultiProcessTestCase):
         signal_pad = symm_mem.get_signal_pad(peer_rank, (8, 8), dtype=torch.uint64)
         self.assertEqual(signal_pad.dtype, torch.uint64)
         self.assertEqual(signal_pad.numel(), 64)
+
+        # Sanity check that writes to buffer doesn't corrupt signal_pad
+        t = _SymmetricMemory.empty_strided_p2p(
+            (0,),
+            (0,),
+            torch.float32,
+            self.device,
+            dist.group.WORLD.group_name,
+        )
+        symm_mem = _SymmetricMemory.rendezvous(t)
+        signal_pad = symm_mem.get_signal_pad(self.rank)
+        signal_pad.fill_(42)
+        t.fill_(0)
+        self.assertTrue(signal_pad.eq(42).all())
+
+        dist.destroy_process_group()
 
     @skipIfRocm
     @skip_if_lt_x_gpu(2)
