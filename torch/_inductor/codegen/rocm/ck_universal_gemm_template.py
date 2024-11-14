fefdbdc2d89a092f2ef1d14cb02e291c0a9eeb87
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-11-14 19:38:09 +0000

    comments and lints

--- torch/_inductor/codegen/rocm/ck_universal_gemm_template.py
+++ torch/_inductor/codegen/rocm/ck_universal_gemm_template.py
@@ -278,7 +278,12 @@ class CKGemmTemplate(CKTemplate):
                     template <typename E, typename C, typename D0, typename D1, typename D2>
                     __host__ __device__ constexpr void
                     operator()(E& e, const C& c, const D0& d0, const D1& d1, const D2& d2) const {
-                        e = ck::type_convert<E>(ck::type_convert<float>(c) * ck::type_convert<float>(d0) * ck::type_convert<float>(d1) + ck::type_convert<float>(d2));
+                        e = ck::type_convert<E>(
+                           ck::type_convert<float>(c)
+                           * ck::type_convert<float>(d0)
+                           * ck::type_convert<float>(d1)
+                           + ck::type_convert<float>(d2)
+                        );
                     }
                 };
             """
@@ -422,10 +427,22 @@ class CKGemmTemplate(CKTemplate):
         template_buffer_node = kwargs.get("template_buffer_node", None)
         if template_buffer_node is not None:
             self.output_node = template_buffer_node
+        # input nodes:
+        # * X, W for matmul
+        # * X, W, Bias for addmm
+        # * X, W, inv_scale_x, inv_scale_w for scaled_mm
+        # * X, W, inv_scale_x, inv_scale_w, Bias for scaled_mm with bias
         X, W = self.input_nodes[0], self.input_nodes[1]
         Y = self.output_node
-        Bias = self.input_nodes[2] if 3 == len(self.input_nodes) else None
-
+        Bias = (
+            self.input_nodes[2]
+            if 3 == len(self.input_nodes)
+            else self.input_nodes[4]
+            if 5 == len(self.input_nodes)
+            else None
+        )
+        has_bias = Bias is not None
+        has_scale = len(self.input_nodes) in (4, 5)
         op = copy.deepcopy(op)
 
         # This parameter is converted into tuple because of change
@@ -435,13 +452,21 @@ class CKGemmTemplate(CKTemplate):
             op.c_shuffle_block_transfer_scalar_per_vector_n_per_block,
         )
 
-        if len(self.input_nodes) in (4, 5):
+        if has_scale:
             scale_x = self.input_nodes[2]
             scale_w = self.input_nodes[3]
             if 1 == scale_x.get_numel() and 1 == scale_w.get_numel():
-                op.c_elementwise_op = "Scale"
+                # tensorwise scale for both X, W
+                if has_bias:
+                    op.c_elementwise_op = "ScaleAdd"
+                else:
+                    op.c_elementwise_op = "Scale"
             else:
-                op.c_elementwise_op = "MultiplyMultiply"
+                # rowwise scale for both X, W
+                if has_bias:
+                    op.c_elementwise_op = "MultiplyMultiplyAdd"
+                else:
+                    op.c_elementwise_op = "MultiplyMultiply"
                 op.c_shuffle_dtype = "F32"
                 op.ds_layouts = (
                     torch_layout_to_ck_layout(scale_x.get_layout()),
@@ -456,19 +481,13 @@ class CKGemmTemplate(CKTemplate):
             scale_x = None
             scale_w = None
 
-        if len(self.input_nodes) == 5:
-            Bias = self.input_nodes[4]
-            if 1 == scale_x.get_numel() and 1 == scale_w.get_numel():
-                op.c_elementwise_op = "ScaleAdd"
-            else:
-                op.c_elementwise_op = "MultiplyMultiplyAdd"
-
+        bias_dtype = ""
         if Bias is not None:
             bias_layout = torch_layout_to_ck_layout(Bias.get_layout())
             bias_dtype = self._TORCH_DTYPE_TO_CK[Bias.get_layout().dtype]
             op.ds_layouts += (bias_layout,)
             op.ds_element_dtypes += (bias_dtype,)
-            if scale_w is None:
+            if not has_scale:
                 op.c_elementwise_op = "Bilinear"
             # c_shuffle_dtype is also used for adding bias to matmul result
             # before converting down to the result dtype
@@ -544,13 +563,13 @@ class CKGemmTemplate(CKTemplate):
             a_element_dtype=op.a_element_dtype,
             b_element_dtype=op.b_element_dtype,
             c_element_dtype=op.c_element_dtype,
-            bias_element_dtype=bias_dtype if Bias is not None else "",
+            bias_element_dtype=bias_dtype,
             alpha=self.alpha,
             beta=self.beta,
             a_elementwise_op="PassThrough {}",
             b_elementwise_op="PassThrough {}",
             epilogue=epilogue,
-            has_bias=Bias is not None,
+            has_bias=has_bias,
             ds_size=1
             if op.c_elementwise_op in ("Bilinear", "ScaleAdd")
             else 2
@@ -588,8 +607,6 @@ class CKGemmTemplate(CKTemplate):
                     f"std::stoi(argv[{k}])" for k, _ in enumerate(self.size_args(), 1)
                 )
             )
-            has_bias = Bias is not None
-            has_scale = scale_x is not None and scale_w is not None
             runner_code = self._template_from_string(
                 self.standalone_runner_template
             ).render(
