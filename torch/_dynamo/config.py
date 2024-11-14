130b2a20b0cd68b9ef6ae4ced5dd697dc57b37fe
Isuru Fernando <ifernando@quansight.com>
2024-11-14 22:00:46 +0000

    Update
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -384,7 +384,7 @@ use_numpy_random_stream = False
 enable_cpp_guard_manager = True
 
 # Use C++ guard manger for symbolic shapes
-enable_cpp_symbolic_shape_guards = False
+enable_cpp_symbolic_shape_guards = True
 
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = not is_fbcode()
