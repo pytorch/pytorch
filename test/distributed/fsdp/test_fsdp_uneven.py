8ad7819b09182dc19a28ea27dec49fcde62988a8
rahulsingh-intel <rahul.singh@intel.com>
2024-11-15 09:38:46 +0530

    Update test_fsdp_uneven.py

--- test/distributed/fsdp/test_fsdp_uneven.py
+++ test/distributed/fsdp/test_fsdp_uneven.py
@@ -8,8 +8,8 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.nn import Linear
 from torch.optim import SGD
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
-from torch.testing._internal.common_fsdp import FSDPTest
-from torch.testing._internal.common_utils import run_tests, TEST_HPU, TEST_WITH_DEV_DBG_ASAN, TEST_CUDA
+from torch.testing._internal.common_fsdp import FSDPTest, get_devtype
+from torch.testing._internal.common_utils import run_tests, TEST_HPU, TEST_WITH_DEV_DBG_ASAN
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
 
 if not dist.is_available():
@@ -23,17 +23,17 @@ if TEST_WITH_DEV_DBG_ASAN:
     )
     sys.exit(0)
 
+device_type = torch.device(get_devtype())
 
 class TestUnevenParamShard(FSDPTest):
     def _get_ref_results(self, device, model, input, my_lr):
         with torch.no_grad():
-            to_device = self.rank if TEST_CUDA else device
             # Compute one iteration local output.
-            weight = model.weight.T.clone().to(to_device)
-            v = torch.Tensor(input[self.rank]).to(to_device)
+            weight = model.weight.T.clone().to(device_type)
+            v = torch.Tensor(input[self.rank]).to(device_type)
             ref_forward_output_my_rank = torch.matmul(v, weight)
             # Compute one iteration global weight update.
-            v = torch.Tensor(input[: self.world_size]).to(to_device)
+            v = torch.Tensor(input[: self.world_size]).to(device_type)
             grad = v.float().sum(0).repeat(weight.shape[0], 1).div(self.world_size)
             ref_weight_out = weight - grad.T * my_lr
 
@@ -41,7 +41,6 @@ class TestUnevenParamShard(FSDPTest):
 
     @skip_if_lt_x_gpu(2)
     def test_one_iteration(self, device):
-        to_device = self.rank if TEST_CUDA else device
         """Test FSDP with uneven divide of parameter shards."""
         model = Linear(3, 3, bias=False)
         input = torch.rand(8, 3)
@@ -51,11 +50,11 @@ class TestUnevenParamShard(FSDPTest):
             device, model, input, my_lr
         )
 
-        model.to(to_device)
+        model.to(device_type)
         model = FSDP(model)
         optim = SGD(model.parameters(), lr=my_lr)
         self.assertTrue(len(input) >= self.world_size)
-        in_data = torch.Tensor(input[self.rank]).to(to_device)
+        in_data = torch.Tensor(input[self.rank]).to(device_type)
         out = model(in_data)
         out.float().sum().backward()
         optim.step()
@@ -66,7 +65,7 @@ class TestUnevenParamShard(FSDPTest):
             self.assertEqual(ref_forward_output_my_rank, out)
             self.assertEqual(ref_weight_out, weight_out)
 
-devices = ("hpu" if TEST_HPU else "cuda")
+devices = ("cuda", "hpu")
 instantiate_device_type_tests(TestUnevenParamShard, globals(), only_for=devices)
 if __name__ == "__main__":
     run_tests()
