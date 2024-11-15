480629aff2a3518acf753fa3b0b130bb659be0dd
rahulsingh-intel <rahul.singh@intel.com>
2024-11-15 10:41:54 +0530

    Update test_fsdp_multiple_wrapping.py

--- test/distributed/fsdp/test_fsdp_multiple_wrapping.py
+++ test/distributed/fsdp/test_fsdp_multiple_wrapping.py
@@ -6,9 +6,12 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.nn import Linear, Module, Sequential
 from torch.optim import SGD
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
-from torch.testing._internal.common_fsdp import FSDPTest
-from torch.testing._internal.common_utils import run_tests, TEST_HPU, TEST_WITH_DEV_DBG_ASAN
+from torch.testing._internal.common_fsdp import FSDPTest, get_devtype
+from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
+
+device_type = torch.device(get_devtype())
+
 if not dist.is_available():
     print("Distributed not available, skipping tests", file=sys.stderr)
     sys.exit(0)
@@ -21,8 +24,7 @@ if TEST_WITH_DEV_DBG_ASAN:
 class InnerModel(Module):
     def __init__(self, device):
         super().__init__()
-        device_id = "hpu:0" if TEST_HPU else torch.cuda.current_device()
-        self.layers = Sequential(FSDP(Linear(5, 5), device_id=device_id))
+        self.layers = Sequential(FSDP(Linear(5, 5), device_id=device_type.type))
     def forward(self, x):
         return self.layers(x)
 class TestMultipleWrapping(FSDPTest):
@@ -34,24 +36,23 @@ class TestMultipleWrapping(FSDPTest):
         contains nested FSDP wrappers within the module.
         """
         inner_model = InnerModel(device)
-        device_id = device if TEST_HPU else self.rank
-        model = FSDP(inner_model).to(device_id)
+        model = FSDP(inner_model).to(device_type.type)
         optim = SGD(model.parameters(), lr=0.1)
         for i in range(3):
-            input = torch.rand((1, 5), dtype=torch.float).to(device_id)
+            input = torch.rand((1, 5), dtype=torch.float).to(device_type.type)
             input.requires_grad = True
             output = model(input)
             output.sum().backward()
             optim.step()
             optim.zero_grad()
-        input = torch.rand((1, 5), dtype=torch.float).to(device_id)
+        input = torch.rand((1, 5), dtype=torch.float).to(device_type.type)
         output = model(input)
         # second time to rewrap the inner model
         #rewrapped_model = FSDP(inner_model, device_id=device)
-        rewrapped_model = FSDP(inner_model).to(device_id)
+        rewrapped_model = FSDP(inner_model).to(device_type.type)
         rewrapped_output = rewrapped_model(input)
         self.assertEqual(output, rewrapped_output)
-devices = ("hpu" if TEST_HPU else "cuda")
+devices = ("cuda", "hpu")
 instantiate_device_type_tests(TestMultipleWrapping, globals(), only_for=devices)
 if __name__ == "__main__":
     run_tests()
