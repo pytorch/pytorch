5ec6e5cb436c1f241ae22c9d5aa10f4f81134196
Nikita Shulga <nikita.shulga@gmail.com>
2024-11-15 03:48:37 +0000

    [BE] Replace `skipIfMPS` with `expectedFailureMPS` (#139940)
    Functionally two decorators are very similar, but one should rely on expectedFailure as much as possible to get signal when something is fixed.
    - Move `product_version` variable from `test_mps` to common_utils, but call it `MACOS_VERSION`
    - Introduce `skipIfMPSOnMacOS13`  to decorate the hard crashes that happens only on MacOS13 (which at this point will not get any fixes and will be deprecated soon)
    - Add `device_type='mps'` to all `skipIfMPS` per https://github.com/pytorch/pytorch/issues/140560
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139940
    Approved by: https://github.com/janeyx99, https://github.com/huydhn

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1402,6 +1402,7 @@ TEST_FAIRSEQ = _check_module_exists('fairseq')
 TEST_SCIPY = _check_module_exists('scipy')
 TEST_MKL = torch.backends.mkl.is_available()
 TEST_MPS = torch.backends.mps.is_available()
+MACOS_VERSION = float('.'.join(platform.mac_ver()[0].split('.')[:2]) or -1)
 TEST_XPU = torch.xpu.is_available()
 TEST_HPU = True if (hasattr(torch, "hpu") and torch.hpu.is_available()) else False
 TEST_CUDA = torch.cuda.is_available()
@@ -1864,6 +1865,17 @@ def skipIfMPS(fn):
         else:
             fn(*args, **kwargs)
     return wrapper
+
+
+def skipIfMPSOnMacOS13(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if TEST_MPS and int(MACOS_VERSION) == 13:
+            raise unittest.SkipTest("Test crashes MPSGraph on MacOS13")
+        else:
+            fn(*args, **kwargs)
+    return wrapper
+
 
 def skipIfHpu(fn):
     @wraps(fn)
