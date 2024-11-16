1671ac65e25ec246099270918a7d4b9f97ad70c1
Yang Wang <elainewy@meta.com>
2024-11-15 18:58:36 -0800

    test

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-
 from __future__ import annotations
-from collections import defaultdict
 
+import argparse
 import datetime
 import json
 import signal
 import time
-import subprocess
 from datetime import timezone
 from typing import Any
 
@@ -19,13 +16,12 @@ def get_processes_running_python_tests() -> list[Any]:
     for process in psutil.process_iter():
         try:
             if "python" in process.name() and process.cmdline():
-                cmd = " ".join(process.cmdline())
-                if not cmd.startswith("/opt/conda/envs"):
-                    python_processes.append(process)
+                python_processes.append(process)
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             # access denied or the process died
             pass
     return python_processes
+
 
 def get_per_process_cpu_info() -> list[dict[str, Any]]:
     processes = get_processes_running_python_tests()
@@ -34,9 +30,6 @@ def get_per_process_cpu_info() -> list[dict[str, Any]]:
         info = {
             "pid": p.pid,
             "cmd": " ".join(p.cmdline()),
-            "cpu_percent": p.cpu_percent(),
-            "rss_memory": p.memory_info().rss,
-            "ppid":p.ppid(),
         }
         # https://psutil.readthedocs.io/en/latest/index.html?highlight=memory_full_info
         # requires higher user privileges and could throw AccessDenied error, i.e. mac
@@ -47,39 +40,19 @@ def get_per_process_cpu_info() -> list[dict[str, Any]]:
             if "pss" in memory_full_info:
                 # only availiable in linux
                 info["pss_memory"] = memory_full_info.pss
+
         except psutil.AccessDenied as e:
             # It's ok to skip this
             pass
+
         per_process_info.append(info)
     return per_process_info
 
-def get_parent_processes(handle) -> dict[int, list[int]]:
-    processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
-    parent_processes = {}
-    for process in processes:
-        pid = process.pid
-        parent_pid = psutil.Process(pid).ppid()
-        if pid not in parent_processes:
-            parent_processes[pid] = []
-        if parent_pid not in parent_processes:
-            parent_processes[parent_pid] = []
-        parent_processes[parent_pid].append(pid)
-    return parent_processes
-
 def get_per_process_gpu_info(handle: Any) -> list[dict[str, Any]]:
     processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
-    total_memory = pynvml.nvmlDeviceGetMemoryInfo(handle).total
     per_process_info = []
-    for process in processes:
-        pid = process.pid
-        mem_usage = process.usedGpuMemory / 1024**2  # Convert to MB
-        memory_utilization_percent = (mem_usage * 100) / (total_memory / 1024**2)
-        info ={
-            "pid": pid,
-            "gpu_memory": mem_usage,
-            "ppid": psutil.Process(process.pid).ppid(),
-            "est": memory_utilization_percent
-        }
+    for p in processes:
+        info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
         per_process_info.append(info)
     return per_process_info
 
@@ -100,25 +73,41 @@ def rocm_get_per_process_gpu_info(handle: Any) -> list[dict[str, Any]]:
         per_process_info.append(info)
     return per_process_info
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=" Test Utilization Monitoring")
+    args = parser.parse_args()
+
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        help="set time interval for logging utilization data, default is 10 seconds",
+    )
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    nvml_handle = None
-    amdsmi_handle = None
+    args = parse_args()
+    interval = args.log_interval
+    has_pynvml = False
+    has_amdsmi = False
+
+
     try:
         import pynvml  # type: ignore[import]
         try:
             pynvml.nvmlInit()
-            nvml_handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+            has_pynvml = True
         except pynvml.NVMLError:
             pass
     except ModuleNotFoundError:
-        # no pynvml avaliable, probably because not cuda
         pass
+
     try:
         import amdsmi  # type: ignore[import]
         try:
             amdsmi.amdsmi_init()
-            amdsmi_handle = amdsmi.amdsmi_get_processor_handles()[0]
+            has_amdsmi = True
         except amdsmi.AmdSmiException:
             pass
     except ModuleNotFoundError:
@@ -126,54 +115,58 @@ if __name__ == "__main__":
         pass
 
     kill_now = False
-
     def exit_gracefully(*args: Any) -> None:
         global kill_now
         kill_now = True
-
     signal.signal(signal.SIGTERM, exit_gracefully)
 
+    num_cpus = psutil.cpu_count()
+    # log info
+    info = {
+        "log_interval": f"{interval} seconds",
+        "gpu": "pynvml" if has_pynvml else "amdsmi",
+    }
+
+    print(json.dumps(info))
     while not kill_now:
-        # each job does not share gpus, they will run in one ec2 instance, but each job will be assigned to one gpu,
         try:
             stats = {
                 "time": datetime.datetime.now(timezone.utc).isoformat("T") + "Z",
                 "total_cpu_percent": psutil.cpu_percent(),
                 "per_process_cpu_info": get_per_process_cpu_info(),
             }
-            if nvml_handle is not None:
+            if has_pynvml:
                 # https://docs.nvidia.com/deploy/nvml-api/structnvmlUtilization__t.html
                 gpu_count = pynvml.nvmlDeviceGetCount()
+                stats["num_of_gpu"]=gpu_count
                 # Iterate over the available GPUs
-                for i in range(gpu_count):
+                for i in (gpu_count):
                     # Get the handle to the current GPU
                     gpu_handle = pynvml.nvmlDeviceGetHandleByIndex(i)
                     gpu_utilization = pynvml.nvmlDeviceGetUtilizationRates(gpu_handle)
-                    # Get the message for the current GPU
-                    stats[f"parent_pid_utilization_{i}"] = get_parent_processes(gpu_handle)
-                    stats[f"per_process_gpu_info_{i}"] = get_per_process_gpu_info(gpu_handle)
+                    # Get the rangemessage for the current GPU
                     stats[f"total_gpu_utilization_{i}"] = gpu_utilization.gpu
                     stats[f"total_gpu_mem_utilization_{i}"] = gpu_utilization.memory
-                # Run the nvidia-smi command and capture its output
-                output = subprocess.check_output(['nvidia-smi', '--query-gpu=utilization.gpu', '--format=csv,noheader,nounits'])# Decode the output from bytes to string
-                output_str = output.decode('utf-8')
-                # Print the output
-                stats["nvidia-smi-test"] = output_str
-            if amdsmi_handle is not None:
-                stats["per_process_gpu_info"] = rocm_get_per_process_gpu_info(
-                    amdsmi_handle
-                )
-                stats["total_gpu_utilization"] = amdsmi.amdsmi_get_gpu_activity(
-                    amdsmi_handle
-                )["gfx_activity"]
-                stats["total_gpu_mem_utilization"] = amdsmi.amdsmi_get_gpu_activity(
-                    amdsmi_handle
-                )["umc_activity"]
+            if has_amdsmi:
+                amd_handles  = amdsmi.amdsmi_get_processor_handles()
+                for handle in amd_handles:
+                    stats["total_gpu_utilization"] = amdsmi.amdsmi_get_gpu_activity(handle)["gfx_activity"]
+                    stats["total_gpu_mem_utilization"] = amdsmi.amdsmi_get_gpu_activity(handle)["umc_activity"]
         except Exception as e:
             stats = {
                 "time": datetime.datetime.now(timezone.utc).isoformat("T") + "Z",
                 "error": str(e),
             }
         finally:
+            if has_amdsmi:
+                try:
+                    amdsmi.amdsmi_shut_down()
+                except amdsmi.AmdSmiException as e:
+                    pass
+            if has_pynvml:
+                try:
+                    pynvml.nvmlShutdown()
+                except pynvml.NVMLError as e:
+                    pass
             print(json.dumps(stats))
             time.sleep(5)
