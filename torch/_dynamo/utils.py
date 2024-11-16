0beac737f3cb742cea135464f95d2ce26724a0f3
Bob Ren <bobren@meta.com>
2024-11-16 13:09:22 -0800

    Update base for Update on "Specialize symfloats during equality checks"
    Fixes `PYTORCH_OPINFO_SAMPLE_INPUT_INDEX=0 python
        test/inductor/test_torchinductor_opinfo.py
        TestInductorOpInfoCPU.test_comprehensive_nn_functional_local_response_norm_cpu_float32`
        when `specialize_float=False`
    
    
    
    [ghstack-poisoned]

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -2202,9 +2202,7 @@ def get_fake_value(node, tx, allow_non_graph_fake=False):
         # no matter it's lazy module or not, we should copy to fake mode.
         nnmodule = deepcopy_to_fake_tensor(nnmodule, tx.fake_mode)
 
-    if node.name in ["interpolate", "is_integer", "wrapped_gradient"] or any(
-        isinstance(a, complex) for a in args
-    ):
+    if node.name in ["interpolate", "is_integer", "wrapped_gradient"]:
         # We need to specialize symfloats for now. Eventually we should do a tensorify pass in dynamo.
         args = tuple(
             float(arg)
