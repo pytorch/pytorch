cc3b3a89b1aac69efeb454d4c732b92d87f914f3
Nikita Shulga <nikita.shulga@gmail.com>
2024-11-17 09:12:18 -0800

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/mps/operations/MultiTensorApply.h
+++ aten/src/ATen/native/mps/operations/MultiTensorApply.h
@@ -16,18 +16,15 @@ struct MetadataArguments { // the size of this struct must be less than 4 bytes
 };
 
 struct FusedAdamEncodingFunctor {
-  void operator()(
-      id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const double lr,
-      const double beta1,
-      const double beta2,
-      const double weight_decay,
-      const double eps,
-      const bool maximize
-    ) const {
-
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const double lr,
+                  const double beta1,
+                  const double beta2,
+                  const double weight_decay,
+                  const double eps,
+                  const bool maximize) const {
     float lr_lv = lr;
     float beta1_lv = beta1;
     float beta2_lv = beta2;
@@ -35,27 +32,41 @@ struct FusedAdamEncodingFunctor {
     float eps_lv = eps;
     uint8_t maximize_lv = maximize;
 
-    mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, lr_lv, beta1_lv, beta2_lv, weight_decay_lv, eps_lv, maximize_lv);
+    mtl_setArgs(computeEncoder,
+                tensorArgumentBuffer,
+                metadata_arguments,
+                lr_lv,
+                beta1_lv,
+                beta2_lv,
+                weight_decay_lv,
+                eps_lv,
+                maximize_lv);
   }
 
-  void operator()(
-      id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const at::Tensor& lr,
-      const double beta1,
-      const double beta2,
-      const double weight_decay,
-      const double eps,
-      const bool maximize
-    ) const {
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const at::Tensor& lr,
+                  const double beta1,
+                  const double beta2,
+                  const double weight_decay,
+                  const double eps,
+                  const bool maximize) const {
     float beta1_lv = beta1;
     float beta2_lv = beta2;
     float weight_decay_lv = weight_decay;
     float eps_lv = eps;
     uint8_t maximize_lv = maximize;
 
-    mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, lr, beta1_lv, beta2_lv, weight_decay_lv, eps_lv, maximize_lv);
+    mtl_setArgs(computeEncoder,
+                tensorArgumentBuffer,
+                metadata_arguments,
+                lr,
+                beta1_lv,
+                beta2_lv,
+                weight_decay_lv,
+                eps_lv,
+                maximize_lv);
   }
 };
 
@@ -64,104 +75,111 @@ struct FusedSgdEncodingFunctor {};
 
 template <>
 struct FusedSgdEncodingFunctor<true> {
-  void operator()(
-    id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const double weight_decay,
-      const double momentum,
-      const double lr,
-      const double dampening,
-      const bool nesterov,
-      const bool maximize,
-      const bool is_first_step
-    ) const {
-      float weight_decay_lv = weight_decay;
-      float momentum_lv = momentum;
-      float lr_lv = lr;
-      float dampening_lv = dampening;
-      uint8_t nesterov_lv = nesterov;
-      uint8_t maximize_lv = maximize;
-      uint8_t is_first_step_lv = is_first_step;
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const double weight_decay,
+                  const double momentum,
+                  const double lr,
+                  const double dampening,
+                  const bool nesterov,
+                  const bool maximize,
+                  const bool is_first_step) const {
+    float weight_decay_lv = weight_decay;
+    float momentum_lv = momentum;
+    float lr_lv = lr;
+    float dampening_lv = dampening;
+    uint8_t nesterov_lv = nesterov;
+    uint8_t maximize_lv = maximize;
+    uint8_t is_first_step_lv = is_first_step;
 
-      mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, momentum_lv, lr_lv, dampening_lv, nesterov_lv, maximize_lv, is_first_step_lv);
+    mtl_setArgs(computeEncoder,
+                tensorArgumentBuffer,
+                metadata_arguments,
+                weight_decay_lv,
+                momentum_lv,
+                lr_lv,
+                dampening_lv,
+                nesterov_lv,
+                maximize_lv,
+                is_first_step_lv);
   }
 
-  void operator()(
-    id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const double weight_decay,
-      const double momentum,
-      const at::Tensor& lr,
-      const double dampening,
-      const bool nesterov,
-      const bool maximize,
-      const bool is_first_step
-    ) const {
-      float weight_decay_lv = weight_decay;
-      float momentum_lv = momentum;
-      float dampening_lv = dampening;
-      uint8_t nesterov_lv = nesterov;
-      uint8_t maximize_lv = maximize;
-      uint8_t is_first_step_lv = is_first_step;
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const double weight_decay,
+                  const double momentum,
+                  const at::Tensor& lr,
+                  const double dampening,
+                  const bool nesterov,
+                  const bool maximize,
+                  const bool is_first_step) const {
+    float weight_decay_lv = weight_decay;
+    float momentum_lv = momentum;
+    float dampening_lv = dampening;
+    uint8_t nesterov_lv = nesterov;
+    uint8_t maximize_lv = maximize;
+    uint8_t is_first_step_lv = is_first_step;
 
-      mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, momentum_lv, lr, dampening_lv, nesterov_lv, maximize_lv, is_first_step_lv);
+    mtl_setArgs(computeEncoder,
+                tensorArgumentBuffer,
+                metadata_arguments,
+                weight_decay_lv,
+                momentum_lv,
+                lr,
+                dampening_lv,
+                nesterov_lv,
+                maximize_lv,
+                is_first_step_lv);
   }
 };
 
 template <>
 struct FusedSgdEncodingFunctor<false> {
-  void operator()(
-    id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const double weight_decay,
-      const double lr,
-      const bool maximize
-    ) const {
-      float weight_decay_lv = weight_decay;
-      float lr_lv = lr;
-      uint8_t maximize_lv = maximize;
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const double weight_decay,
+                  const double lr,
+                  const bool maximize) const {
+    float weight_decay_lv = weight_decay;
+    float lr_lv = lr;
+    uint8_t maximize_lv = maximize;
 
-      mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, lr_lv, maximize_lv);
+    mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, lr_lv, maximize_lv);
   }
 
-  void operator()(
-    id<MTLComputeCommandEncoder>& computeEncoder,
-      id<MTLBuffer>& tensorArgumentBuffer,
-      const MetadataArguments& metadata_arguments,
-      const double weight_decay,
-      const at::Tensor& lr,
-      const bool maximize
-    ) const {
-      float weight_decay_lv = weight_decay;
-      uint8_t maximize_lv = maximize;
+  void operator()(id<MTLComputeCommandEncoder>& computeEncoder,
+                  id<MTLBuffer>& tensorArgumentBuffer,
+                  const MetadataArguments& metadata_arguments,
+                  const double weight_decay,
+                  const at::Tensor& lr,
+                  const bool maximize) const {
+    float weight_decay_lv = weight_decay;
+    uint8_t maximize_lv = maximize;
 
-      mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, lr, maximize_lv);
+    mtl_setArgs(computeEncoder, tensorArgumentBuffer, metadata_arguments, weight_decay_lv, lr, maximize_lv);
   }
 };
 
 template <int depth, uint32_t kThreadGroupSize, typename encoder_func_t, typename... ArgTypes>
-static void multi_tensor_apply_for_fused_optimizer(
-    const std::string& kernel_name,
-    std::vector<std::vector<at::Tensor>>& tensor_lists,
-    at::TensorList state_steps,
-    encoder_func_t encode,
-    ArgTypes... args
-    ) {
+static void multi_tensor_apply_for_fused_optimizer(const std::string& kernel_name,
+                                                   std::vector<std::vector<at::Tensor>>& tensor_lists,
+                                                   at::TensorList state_steps,
+                                                   encoder_func_t encode,
+                                                   ArgTypes... args) {
   const auto num_tensors = tensor_lists[0].size();
 
   if (num_tensors == 0) {
     return;
   }
 
-  TORCH_CHECK(
-      tensor_lists.size() == depth,
-      "Number of tensor lists has to match the depth");
+  TORCH_CHECK(tensor_lists.size() == depth, "Number of tensor lists has to match the depth");
   for (const auto& d : c10::irange(depth)) {
-    TORCH_CHECK(
-      tensor_lists[d][0].scalar_type() == at::ScalarType::Float || tensor_lists[d][0].scalar_type() == at::ScalarType::Half, "Only float and half are supported");
+    TORCH_CHECK(tensor_lists[d][0].scalar_type() == at::ScalarType::Float ||
+                    tensor_lists[d][0].scalar_type() == at::ScalarType::Half,
+                "Only float and half are supported");
   }
 
   id<MTLDevice> device = MPSDevice::getInstance()->device();
@@ -189,7 +207,8 @@ static void multi_tensor_apply_for_fused_optimizer(
 
       // BufferIndex is the index in the kernel function
       auto tensorArgumentEncoder = [[fusedOptimizerFunc newArgumentEncoderWithBufferIndex:0] autorelease];
-      id<MTLBuffer> tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength options:0] autorelease];
+      id<MTLBuffer> tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength
+                                                                options:0] autorelease];
       [tensorArgumentEncoder setArgumentBuffer:tensorArgumentBuffer offset:0];
 
       int64_t tensor_loc = 0;
@@ -203,10 +222,11 @@ static void multi_tensor_apply_for_fused_optimizer(
         }
 
         for (const auto& d : c10::irange(depth)) {
-            mtl_setBuffer(tensorArgumentEncoder, tensor_lists[d][tensor_index], d * kmaxTensors + tensor_loc);
-            [computeEncoder useResource:getMTLBufferStorage(tensor_lists[d][tensor_index]) usage:MTLResourceUsageRead | MTLResourceUsageWrite];
+          mtl_setBuffer(tensorArgumentEncoder, tensor_lists[d][tensor_index], d * kmaxTensors + tensor_loc);
+          [computeEncoder useResource:getMTLBufferStorage(tensor_lists[d][tensor_index])
+                                usage:MTLResourceUsageRead | MTLResourceUsageWrite];
         }
-        if (state_steps.size() > 0){
+        if (state_steps.size() > 0) {
           mtl_setBuffer(tensorArgumentEncoder, state_steps[tensor_index], depth * kmaxTensors + tensor_loc);
           [computeEncoder useResource:getMTLBufferStorage(state_steps[tensor_index]) usage:MTLResourceUsageRead];
         }
@@ -219,47 +239,50 @@ static void multi_tensor_apply_for_fused_optimizer(
         TORCH_CHECK(chunks > -1);
 
         for (const auto& chunk : c10::irange(chunks)) {
-            metadata_arguments.threadgroup_to_tensor[threadgroup_loc] = tensor_loc - 1;
-            metadata_arguments.threadgroup_to_chunk[threadgroup_loc] = chunk;
+          metadata_arguments.threadgroup_to_tensor[threadgroup_loc] = tensor_loc - 1;
+          metadata_arguments.threadgroup_to_chunk[threadgroup_loc] = chunk;
 
-            threadgroup_loc++;
+          threadgroup_loc++;
 
-            const auto tensor_full = tensor_loc == kmaxTensors && chunk == chunks - 1;
-            // Reach the maximum threadgroups per dispatch
-            const auto blocks_full = threadgroup_loc == kmaxThreadGroups;
+          const auto tensor_full = tensor_loc == kmaxTensors && chunk == chunks - 1;
+          // Reach the maximum threadgroups per dispatch
+          const auto blocks_full = threadgroup_loc == kmaxThreadGroups;
 
-            if (tensor_full || blocks_full){
-                encode(computeEncoder, tensorArgumentBuffer, metadata_arguments, args...);
-                MTLSize gridSize = MTLSizeMake(threadgroup_loc, 1, 1);
-                uint32_t maxThreadsPerGroup = [fusedOptimizerPSO maxTotalThreadsPerThreadgroup];
-                MTLSize threadGroupSize = MTLSizeMake(std::min(maxThreadsPerGroup, kThreadGroupSize), 1, 1);
-                [computeEncoder dispatchThreadgroups:gridSize threadsPerThreadgroup:threadGroupSize];
+          if (tensor_full || blocks_full) {
+            encode(computeEncoder, tensorArgumentBuffer, metadata_arguments, args...);
+            MTLSize gridSize = MTLSizeMake(threadgroup_loc, 1, 1);
+            uint32_t maxThreadsPerGroup = [fusedOptimizerPSO maxTotalThreadsPerThreadgroup];
+            MTLSize threadGroupSize = MTLSizeMake(std::min(maxThreadsPerGroup, kThreadGroupSize), 1, 1);
+            [computeEncoder dispatchThreadgroups:gridSize threadsPerThreadgroup:threadGroupSize];
 
-                // Reset
-                threadgroup_loc = 0;
-                if (chunk == chunks - 1) {
-                  // last chunk
-                  tensor_loc = 0;
-                  tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength options:0] autorelease];
-                  [tensorArgumentEncoder setArgumentBuffer:tensorArgumentBuffer offset:0];
-                } else {
-                  // reuse the current tensor since the current one isn't done.
-                  metadata_arguments.numels[0] = metadata_arguments.numels[tensor_loc - 1];
+            // Reset
+            threadgroup_loc = 0;
+            if (chunk == chunks - 1) {
+              // last chunk
+              tensor_loc = 0;
+              tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength
+                                                          options:0] autorelease];
+              [tensorArgumentEncoder setArgumentBuffer:tensorArgumentBuffer offset:0];
+            } else {
+              // reuse the current tensor since the current one isn't done.
+              metadata_arguments.numels[0] = metadata_arguments.numels[tensor_loc - 1];
 
-                  tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength options:0] autorelease];
-                  [tensorArgumentEncoder setArgumentBuffer:tensorArgumentBuffer offset:0];
+              tensorArgumentBuffer = [[device newBufferWithLength:tensorArgumentEncoder.encodedLength
+                                                          options:0] autorelease];
+              [tensorArgumentEncoder setArgumentBuffer:tensorArgumentBuffer offset:0];
 
-                  for (const auto& d : c10::irange(depth)) {
-                      mtl_setBuffer(tensorArgumentEncoder, tensor_lists[d][tensor_index], d * kmaxTensors);
-                      [computeEncoder useResource:getMTLBufferStorage(tensor_lists[d][tensor_index]) usage:MTLResourceUsageWrite | MTLResourceUsageRead];
-                  }
-                  if (state_steps.size() > 0){
-                    mtl_setBuffer(tensorArgumentEncoder, state_steps[tensor_index], depth * kmaxTensors);
-                    [computeEncoder useResource:getMTLBufferStorage(state_steps[tensor_index]) usage:MTLResourceUsageRead];
-                  }
-                  tensor_loc = 1;
-                }
+              for (const auto& d : c10::irange(depth)) {
+                mtl_setBuffer(tensorArgumentEncoder, tensor_lists[d][tensor_index], d * kmaxTensors);
+                [computeEncoder useResource:getMTLBufferStorage(tensor_lists[d][tensor_index])
+                                      usage:MTLResourceUsageWrite | MTLResourceUsageRead];
+              }
+              if (state_steps.size() > 0) {
+                mtl_setBuffer(tensorArgumentEncoder, state_steps[tensor_index], depth * kmaxTensors);
+                [computeEncoder useResource:getMTLBufferStorage(state_steps[tensor_index]) usage:MTLResourceUsageRead];
+              }
+              tensor_loc = 1;
             }
+          }
         }
       }
 
@@ -272,7 +295,6 @@ static void multi_tensor_apply_for_fused_optimizer(
       }
 
       getMPSProfiler().endProfileKernel(fusedOptimizerPSO);
-
     }
   });
 }
