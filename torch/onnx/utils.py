cc9dbf608e3441b15c03397537f83e744e0288db
titaiwangms <titaiwang@microsoft.com>
2024-11-18 18:05:24 +0000

    fix a bug

--- torch/onnx/utils.py
+++ torch/onnx/utils.py
@@ -653,9 +653,12 @@ def _optimize_graph(
     graph = _C._jit_pass_canonicalize(graph)
     _C._jit_pass_lint(graph)
     if GLOBALS.onnx_shape_inference:
-        _C._jit_pass_onnx_graph_shape_type_inference(
-            graph, params_dict, GLOBALS.export_onnx_opset_version
-        )
+        try:
+            _C._jit_pass_onnx_graph_shape_type_inference(
+                graph, params_dict, GLOBALS.export_onnx_opset_version
+            )
+        except RuntimeError as e:
+            pass
 
     return graph
 
