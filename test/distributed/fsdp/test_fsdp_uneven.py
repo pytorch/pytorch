d0d9388a8a171cd9a52ecccf7d4240fd9ad99557
RAHUL SINGH <rahul.singh@intel.com>
2024-11-19 13:55:43 +0530

    Update test_fsdp_uneven.py

--- test/distributed/fsdp/test_fsdp_uneven.py
+++ test/distributed/fsdp/test_fsdp_uneven.py
@@ -7,10 +7,11 @@ from torch import distributed as dist
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.nn import Linear
 from torch.optim import SGD
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_fsdp import FSDPTest, get_devtype
-from torch.testing._internal.common_utils import run_tests, TEST_HPU, TEST_WITH_DEV_DBG_ASAN
-from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
+
 
 if not dist.is_available():
     print("Distributed not available, skipping tests", file=sys.stderr)
@@ -24,6 +25,7 @@ if TEST_WITH_DEV_DBG_ASAN:
     sys.exit(0)
 
 device_type = torch.device(get_devtype())
+
 
 class TestUnevenParamShard(FSDPTest):
     def _get_ref_results(self, device, model, input, my_lr):
@@ -64,6 +66,7 @@ class TestUnevenParamShard(FSDPTest):
             weight_out = model.module.weight.T.clone()
             self.assertEqual(ref_forward_output_my_rank, out)
             self.assertEqual(ref_weight_out, weight_out)
+
 
 devices = ("cuda", "hpu")
 instantiate_device_type_tests(TestUnevenParamShard, globals(), only_for=devices)
