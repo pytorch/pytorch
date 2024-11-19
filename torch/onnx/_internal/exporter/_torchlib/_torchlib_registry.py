9d338b5f9274021e2505ef454cb851792525ee76
Justin Chu <justinchu@microsoft.com>
2024-11-18 17:50:39 -0800

    registry

---
+++ torch/onnx/_internal/exporter/_torchlib/_torchlib_registry.py
@@ -0,0 +1,43 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""Registry for aten functions."""
+
+from __future__ import annotations
+
+import functools
+import re
+from typing import Any, Callable, Generator, Optional, TypeVar
+
+import onnxscript
+
+_T = TypeVar("_T", bound=Callable)
+
+
+class Registry:
+    """Registry for aten functions."""
+
+    def __init__(self):
+        self._registry: dict[Callable, list[Callable]] = {}
+
+    def register(self, target: Callable, impl: Callable) -> None:
+        """Register a function."""
+
+        self._registry.setdefault(target, []).append((impl))
+
+
+# Default registry
+default_registry = Registry()
+
+
+def onnx_impl(
+    target: Callable,
+) -> Callable[[_T], _T]:
+    """Register an ONNX implementation of a torch op."""
+
+    def wrapper(
+        func: _T,
+    ) -> _T:
+        default_registry.register(target, func)
+        return func
+
+    return wrapper
