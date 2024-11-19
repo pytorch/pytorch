3d6c45f3af3486fe3802093977acc1b9214702d1
Sun, Jiayi <jiayi.sun@intel.com>
2024-11-19 07:26:19 +0000

    [inductor] support linear+binary foldinig for freezing path (#138807)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138807
    Approved by: https://github.com/jgong5, https://github.com/jansel
    
    Co-authored-by: Jiong Gong <jiong.gong@intel.com>

--- test/inductor/test_binary_folding.py
+++ test/inductor/test_binary_folding.py
@@ -7,6 +7,7 @@ import sys
 
 import torch
 from torch import nn
+from torch._dynamo.utils import counters
 from torch._inductor import config as inductor_config
 from torch.testing._internal.common_cuda import TEST_CUDNN
 
@@ -49,9 +50,6 @@ class BinaryFoldingTemplate(TestCase):
                     self.conv = module(
                         in_channels, out_channels, bias=use_bias, **kwargs
                     ).to(device)
-                    self.conv2 = module(
-                        in_channels, out_channels, bias=use_bias, **kwargs
-                    ).to(device)
                     self.use_scalar = scalar
                     tensor_size = [1 for _ in range(self.conv.weight.ndim)]
                     tensor_size[1] = self.conv.weight.size(0)
@@ -69,29 +67,10 @@ class BinaryFoldingTemplate(TestCase):
                     else:
                         return self.op(x, self.tensor)
 
-            from torch._inductor.compile_fx import compile_fx, compile_fx_inner
-
-            aten_binary = {
-                torch.add: aten.add.Tensor,
-                torch.sub: aten.sub.Tensor,
-                torch.mul: aten.mul.Tensor,
-                torch.div: aten.div.Tensor,
-            }
-            n_binary_ops = 0
-
-            def my_inner_compile(gm, example_inputs, *args, **kwargs):
-                out = compile_fx_inner(gm, example_inputs, *args, **kwargs)
-                nonlocal n_binary_ops
-                binarry_ops = [n for n in gm.graph.nodes if n.target == aten_binary[op]]
-                n_binary_ops += len(binarry_ops)
-                return out
-
             torch._dynamo.reset()
+            counters.clear()
             mod_eager = ConvOp(3, 32, self.device, kernel_size=3, stride=2).eval()
-            out_optimized = torch.compile(
-                mod_eager,
-                backend=functools.partial(compile_fx, inner_compile=my_inner_compile),
-            )
+            out_optimized = torch.compile(mod_eager)
 
             inps = [4, 3, 4]
             if module == nn.Conv2d:
@@ -106,9 +85,9 @@ class BinaryFoldingTemplate(TestCase):
             out_optimized = out_optimized(inp)
             self.assertEqual(out_optimized, out_eager)
             if expect_success:
-                self.assertTrue(n_binary_ops == 0)
+                self.assertEqual(counters["inductor"]["binary_folding"], 1)
             else:
-                self.assertTrue(n_binary_ops == 1)
+                self.assertEqual(counters["inductor"]["binary_folding"], 0)
 
         conv_bias = [True, False]
         modules = [nn.Conv1d, nn.Conv2d, nn.Conv3d]
@@ -117,15 +96,13 @@ class BinaryFoldingTemplate(TestCase):
         for use_bias, module, pytorch_op, scalar in itertools.product(
             conv_bias, modules, ops, use_scalar
         ):
-            # TODO: support scalar case
-            expect_success = not scalar
             test_conv_fusion(
                 use_bias,
                 module,
                 pytorch_op,
                 scalar,
                 add_tensor=None,
-                expect_success=expect_success,
+                expect_success=True,
             )
 
         for use_bias, pytorch_op in itertools.product(conv_bias, ops):
@@ -230,6 +207,117 @@ class BinaryFoldingTemplate(TestCase):
                 use_bias,
                 module,
                 expect_success=True,
+            )
+
+    def test_linear_binary_folding(self):
+        @torch.no_grad()
+        def test_linear_fusion(
+            use_bias, op, scalar, add_tensor, expect_success, input_3d=False
+        ):
+            class LinearOp(nn.Module):
+                __constants__ = ["use_scalar"]
+
+                def __init__(self, in_channels, out_channels, device, **kwargs):
+                    super().__init__()
+                    self.linear = nn.Linear(
+                        in_channels, out_channels, bias=use_bias, **kwargs
+                    ).to(device)
+                    self.use_scalar = scalar
+                    tensor_size = [
+                        self.linear.weight.size(0),
+                    ]
+                    self.tensor = torch.nn.Parameter(
+                        add_tensor
+                        if add_tensor is not None
+                        else torch.rand(tensor_size).to(device)
+                    )
+                    self.op = op
+
+                def forward(self, x):
+                    x = self.linear(x)
+                    if self.use_scalar:
+                        return self.op(x, 2.0)
+                    else:
+                        return self.op(x, self.tensor)
+
+            torch._dynamo.reset()
+            counters.clear()
+            mod_eager = LinearOp(3, 32, self.device).eval()
+            out_optimized = torch.compile(mod_eager)
+
+            torch.manual_seed(1234)
+            if input_3d:
+                inp = torch.rand([2, 4, 3]).to(self.device)
+            else:
+                inp = torch.rand([4, 3]).to(self.device)
+            out_eager = mod_eager(inp)
+            out_optimized = out_optimized(inp)
+            self.assertEqual(out_optimized, out_eager, atol=5e-05, rtol=5e-06)
+            if expect_success:
+                self.assertEqual(counters["inductor"]["binary_folding"], 1)
+            else:
+                self.assertEqual(counters["inductor"]["binary_folding"], 0)
+
+        linear_bias = [True, False]
+        use_scalar = [True, False]
+        ops = [torch.add, torch.sub, torch.mul, torch.div]
+        add_tensor_size = [
+            [
+                32,
+            ],
+            [1, 32],
+            [
+                1,
+            ],
+            [1, 1],
+        ]
+        for use_bias, pytorch_op, scalar, tensor_size in itertools.product(
+            linear_bias, ops, use_scalar, add_tensor_size
+        ):
+            test_linear_fusion(
+                use_bias,
+                pytorch_op,
+                scalar,
+                add_tensor=torch.rand(tensor_size).to(self.device),
+                expect_success=True,
+            )
+
+        add_tensor_size.extend([[1, 1, 32], [1, 1, 1]])
+        for use_bias, pytorch_op, scalar, tensor_size in itertools.product(
+            linear_bias, ops, use_scalar, add_tensor_size
+        ):
+            test_linear_fusion(
+                use_bias,
+                pytorch_op,
+                scalar,
+                add_tensor=torch.rand(tensor_size).to(self.device),
+                expect_success=True,
+                input_3d=True,
+            )
+
+        # In the following test, the shape of 'add_tensor' does not satisfy
+        # the requirements of binary folding, so it will not be folded.
+        for use_bias, pytorch_op in itertools.product(linear_bias, ops):
+            test_linear_fusion(
+                use_bias,
+                pytorch_op,
+                False,
+                add_tensor=torch.rand(
+                    4,
+                    32,
+                ).to(self.device),
+                expect_success=False,
+            )
+
+            test_linear_fusion(
+                use_bias,
+                pytorch_op,
+                False,
+                add_tensor=torch.rand(
+                    4,
+                    1,
+                ).to(self.device),
+                expect_success=False,
             )
 
 
