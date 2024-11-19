0480669efc3e19b959e3829172c9f6a4d3af2721
Will Constable <whc@meta.com>
2024-11-19 11:39:43 -0800

    [C10D] Support group ranks in P2POp and batch_isend_irecv
    Changes semantic of __repr__ of P2POp: s, d are now group ranks instead
    of global ranks. I think this is OK since I also updated the field names
    to make this obvious.
    
    Also add mypy annotations
    
    Partially addresses RFC 0042 (pytorch/rfcs#71)
    See more details/motivation in #140460
    
    [ghstack-poisoned]

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -3943,6 +3943,40 @@ class LargeCommTest(test_c10d_common.AbstractLargeCommTest, MultiProcessTestCase
     @requires_nccl()
     @skip_if_lt_x_gpu(4)
     @parametrize("group_rank", [True, False])
+    def test_batch_send_recv_subgroup(self, group_rank):
+        world_size = 4
+        if self.rank >= world_size:
+            return
+        subgroup = self._init_two_pg2_subgroups(world_size)
+        device = torch.device("cuda:%d" % self.rank)
+        ops = []
+        if self.rank == 0 or self.rank == 2:
+            x = torch.empty((10,), device=device)
+            if group_rank:
+                ops.append(c10d.P2POp(dist.irecv, x, group=subgroup, group_peer=1))
+            else:
+                ops.append(
+                    c10d.P2POp(dist.irecv, x, peer=self.rank + 1, group=subgroup)
+                )
+
+            for work in dist.batch_isend_irecv(ops):
+                work.wait()
+            expected = torch.ones((10,), device=device) * (self.rank + 1)
+            self.assertEqual(x, expected)
+        else:
+            x = torch.ones((10,), device=device) * self.rank
+            if group_rank:
+                ops.append(c10d.P2POp(dist.isend, x, group=subgroup, group_peer=0))
+            else:
+                ops.append(
+                    c10d.P2POp(dist.isend, x, peer=self.rank - 1, group=subgroup)
+                )
+            for work in dist.batch_isend_irecv(ops):
+                work.wait()
+
+    @requires_nccl()
+    @skip_if_lt_x_gpu(4)
+    @parametrize("group_rank", [True, False])
     def test_broadcast_subgroup(self, group_rank):
         world_size = 4
         if self.rank >= world_size:
