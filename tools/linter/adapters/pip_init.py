pytorch/pytorch

commit 1f25294f0793659400fe84e86888f7f9d819290d

Use uv in lintrunner init when it is available. (#124033)
Before, a no-op lintrunner init takes 12s.  After, it takes 1s;
a full order of magnitude improvement.

Signed-off-by: Edward Z. Yang <ezyang@meta.com>
Pull Request resolved: https://github.com/pytorch/pytorch/pull/124033
Approved by: https://github.com/cyyever, https://github.com/Skylion007

tools/linter/adapters/pip_init.py
@@ -4,6 +4,7 @@ Initializer script that installs stuff to pip.
 import argparse
 import logging
 import os
+import shutil
 import subprocess
 import sys
 import time
@@ -50,7 +51,12 @@ if __name__ == "__main__":
         stream=sys.stderr,
     )
 
-    pip_args = ["pip3", "install"]
+    uv_available = shutil.which("uv") is not None
+
+    if uv_available:
+        pip_args = ["uv", "pip", "install"]
+    else:
+        pip_args = ["pip", "install"]
 
     # If we are in a global install, use `--user` to install so that you do not
     # need root access in order to initialize linters.

commit 392c01cdda1e431e210265816eda951c7bc88fe3

[BE] Use `pip` corresponding to python executable
Sometimes `python` and `pip` are aliased to different command, but linter should install packages into the same python environment it's being executed from

Fixes regression introduced by https://github.com/pytorch/pytorch/pull/124033

tools/linter/adapters/pip_init.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     if uv_available:
         pip_args = ["uv", "pip", "install"]
     else:
-        pip_args = ["pip", "install"]
+        pip_args = [sys.executable, "-mpip", "install"]
 
     # If we are in a global install, use `--user` to install so that you do not
     # need root access in order to initialize linters.