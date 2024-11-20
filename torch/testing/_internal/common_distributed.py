88f46a9833597919a87479ac051e43921e7307f9
Ke Wen <kw2501@meta.com>
2024-11-20 13:51:01 -0800

    [CI] Reduce distributed test timeout to 60s
    [ghstack-poisoned]

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -426,7 +426,7 @@ if TEST_WITH_TSAN:
     # TSAN runs much slower.
     TIMEOUT_DEFAULT = 500
 else:
-    TIMEOUT_DEFAULT = int(os.getenv('DISTRIBUTED_TESTS_DEFAULT_TIMEOUT', '300'))
+    TIMEOUT_DEFAULT = int(os.getenv('DISTRIBUTED_TESTS_DEFAULT_TIMEOUT', '60'))
 TIMEOUT_OVERRIDE = {"test_ddp_uneven_inputs": 400}
 
 
