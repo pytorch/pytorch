8813d2f6f40230e7d9e0f9c2e69990ecfffdb452
Jovian Anthony Jaison <38627145+jovianjaison@users.noreply.github.com>
2024-11-21 12:20:02 -0800

    [dynamo] Added cuda and triton versions to dynamo_compile (#141140)
    [dynamo] Added cuda and triton versions to dynamo_compile (#141140)
    
    Summary:
    
    Add cuda and triton versions to dynamo_compile logging site.
    
    Test Plan:
    $ buck2 run mode/opt //scripts/oulgen:runner
    File changed: fbcode//caffe2/torch/_dynamo/convert_frame.py
    Buck UI: https://www.internalfb.com/buck2/1a8ada1f-d54e-44b2-a368-b2ff2030e113
    Network: Up: 65KiB  Down: 0B  (reSessionID-8f4d1d6d-a680-4ecc-8e73-c29c932d824b)
    Jobs completed: 2166. Time elapsed: 7.0s.
    Cache hits: 0%. Commands: 3 (cached: 0, remote: 0, local: 3)
    BUILD SUCCEEDED
    ...
    Cuda: 12.4.0
    Triton: 3.0.0
    
    Reviewed By: masnesral
    
    Differential Revision: D66181508

--- test/dynamo/test_utils.py
+++ test/dynamo/test_utils.py
@@ -201,6 +201,8 @@ class TestDynamoTimed(TestCase):
             e.co_filename = None
             e.co_firstlineno = None
             e.inductor_config = None
+            e.cuda_version = None
+            e.triton_version = None
 
         # First event is for the forward. Formatting makes reading diffs
         # much easier.
@@ -221,6 +223,7 @@ class TestDynamoTimed(TestCase):
  'config_inline_inbuilt_nn_modules': False,
  'config_suppress_errors': False,
  'cuda_synchronize_time_us': None,
+ 'cuda_version': None,
  'distributed_ephemeral_timeout_us': None,
  'duration_us': 0,
  'dynamo_compile_time_before_restart_us': 0,
@@ -264,7 +267,8 @@ class TestDynamoTimed(TestCase):
  'start_time_us': 100,
  'structured_logging_overhead_s': 0.0,
  'structured_logging_overhead_us': 0,
- 'triton_compile_time_us': None}""",  # noqa: B950
+ 'triton_compile_time_us': None,
+ 'triton_version': None}""",  # noqa: B950
         )
 
         # Second event is for the backward
@@ -285,6 +289,7 @@ class TestDynamoTimed(TestCase):
  'config_inline_inbuilt_nn_modules': None,
  'config_suppress_errors': None,
  'cuda_synchronize_time_us': None,
+ 'cuda_version': None,
  'distributed_ephemeral_timeout_us': None,
  'duration_us': 0,
  'dynamo_compile_time_before_restart_us': None,
@@ -328,7 +333,8 @@ class TestDynamoTimed(TestCase):
  'start_time_us': 100,
  'structured_logging_overhead_s': None,
  'structured_logging_overhead_us': 0,
- 'triton_compile_time_us': None}""",  # noqa: B950
+ 'triton_compile_time_us': None,
+ 'triton_version': None}""",  # noqa: B950
         )
 
 
