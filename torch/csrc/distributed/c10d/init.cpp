cc413d4fd4e05f84eec6c10c21a0b11fef701e73
cyyever <cyyever@outlook.com>
2024-11-21 00:25:20 +0000

    [Environment Variable][7/N] Use thread-safe getenv functions (#140211)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140211
    Approved by: https://github.com/ezyang, https://github.com/eqy

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -2816,9 +2816,10 @@ options :class:`~torch.distributed.ProcessGroupNCCL.Options`).
             auto options = ::c10d::ProcessGroupGloo::Options::create();
 
             // Use interfaces listed in "GLOO_SOCKET_IFNAME", if set.
-            char* ifnameEnv = getenv(GLOO_SOCKET_IFNAME_ENV.c_str());
-            if (ifnameEnv && strlen(ifnameEnv) > 1) {
-              for (const auto& iface : ::c10d::split(',', ifnameEnv)) {
+            auto ifnameEnv =
+                c10::utils::get_env(GLOO_SOCKET_IFNAME_ENV.c_str());
+            if (ifnameEnv && ifnameEnv->size() > 1) {
+              for (const auto& iface : ::c10d::split(',', ifnameEnv->c_str())) {
                 options->devices.push_back(
                     ::c10d::ProcessGroupGloo::createDeviceForInterface(iface));
               }
