056dfaf57096a87397ede630bec1f77a81dd1e2d
ZhiweiYan-96 <zhiwei.yan@intel.com>
2024-11-21 12:40:25 +0000

    Update
    [ghstack-poisoned]

--- torch/ao/quantization/quantizer/xpu_inductor_quantizer.py
+++ torch/ao/quantization/quantizer/xpu_inductor_quantizer.py
@@ -1,18 +1,22 @@
 # mypy: allow-untyped-defs
 import functools
-from typing import Optional
+from typing import Any, Dict, Optional, TYPE_CHECKING
 
 import torch
+from torch.ao.quantization.observer import HistogramObserver, PerChannelMinMaxObserver
+from torch.ao.quantization.quantizer.quantizer import QuantizationSpec
 from torch.ao.quantization.quantizer.x86_inductor_quantizer import (
     _is_any_annotated,
     FilterFn,
-    get_default_x86_inductor_quantization_config,
     int8_in_int8_out_ops,
     X86InductorQuantizer,
 )
 from torch.ao.quantization.quantizer.xnnpack_quantizer_utils import QuantizationConfig
 from torch.fx import Node
 
+
+if TYPE_CHECKING:
+    from torch.ao.quantization.qconfig import _ObserverOrFakeQuantizeConstructor
 
 __all__ = [
     "XPUInductorQuantizer",
@@ -22,7 +26,44 @@ __all__ = [
 
 @functools.lru_cache
 def get_default_xpu_inductor_quantization_config():
-    return get_default_x86_inductor_quantization_config()
+    extra_args: Dict[str, Any] = {"eps": 2**-12}
+    act_observer_or_fake_quant_ctr = HistogramObserver
+    act_quantization_spec = QuantizationSpec(
+        dtype=torch.int8,
+        quant_min=-128,
+        quant_max=127,
+        qscheme=torch.per_tensor_affine,
+        is_dynamic=False,
+        observer_or_fake_quant_ctr=act_observer_or_fake_quant_ctr.with_args(
+            **extra_args
+        ),
+    )
+
+    weight_observer_or_fake_quant_ctr: _ObserverOrFakeQuantizeConstructor = (
+        PerChannelMinMaxObserver
+    )
+
+    weight_quantization_spec = QuantizationSpec(
+        dtype=torch.int8,
+        quant_min=-128,
+        quant_max=127,
+        qscheme=torch.per_channel_symmetric,
+        ch_axis=0,  # 0 corresponding to weight shape = (oc, ic, kh, kw) of conv
+        is_dynamic=False,
+        observer_or_fake_quant_ctr=weight_observer_or_fake_quant_ctr.with_args(
+            **extra_args
+        ),
+    )
+
+    bias_quantization_spec = None  # will use placeholder observer by default
+    quantization_config = QuantizationConfig(
+        act_quantization_spec,
+        act_quantization_spec,
+        weight_quantization_spec,
+        bias_quantization_spec,
+        False,
+    )
+    return quantization_config
 
 
 class XPUInductorQuantizer(X86InductorQuantizer):
