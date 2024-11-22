b9c242bd15b1c3b66c873aa2fba743f84ac98c68
Colin Peppler <colinpeppler@meta.com>
2024-11-22 14:47:50 -0800

    [CUTLASS] Lift shape & stride information as kernel args (#138611)
    Differential Revision: [D64773324](https://our.internmc.facebook.com/intern/diff/D64773324)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138611
    Approved by: https://github.com/chenyang78

--- torch/_inductor/codegen/cuda/cuda_kernel.py
+++ torch/_inductor/codegen/cuda/cuda_kernel.py
@@ -1,7 +1,21 @@
 # mypy: allow-untyped-defs
 import logging
-from typing import Any, Callable, Dict, List, Optional, TYPE_CHECKING, Union
+from dataclasses import dataclass
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Literal,
+    Optional,
+    Tuple,
+    TYPE_CHECKING,
+    Union,
+)
 
+from sympy import Expr
+
+from torch import dtype as torch_dtype
 from torch._inductor.codegen.cpp_wrapper_cpu import CppWrapperCpu
 
 from ...autotune_process import CUDABenchmarkRequest
@@ -38,12 +52,108 @@ def _normalize_idx(index: int, total_length: int) -> int:
     return index if index >= 0 else index + total_length
 
 
+ValidLayoutSymbols = Literal["M", "N", "K", "lda", "ldb", "ldc", "ldd"]
+ValidLayoutAttrs = Literal["size", "stride"]
+
+
+@dataclass(frozen=True)
+class LayoutArg:
+    node: IRNode
+    symbol: ValidLayoutSymbols
+    attr: ValidLayoutAttrs
+    dim: int
+
+    def matches(self, node, attr, dim) -> bool:
+        return self.node == node and self.attr == attr and self.dim == dim
+
+
 class CUDAKernel(Kernel):
     """
     Baseclass for CUDA / Cutlass based Kernels
     """
 
     overrides = OpOverrides  # type: ignore[assignment]
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.layout_args: Dict[str, LayoutArg] = {}
+        # Mapping from arg name to IRNode.
+        self.named_nodes: Dict[str, IRNode] = {}
+
+    def find_symbol(
+        self, node: IRNode, attr: ValidLayoutAttrs, dim: int
+    ) -> Optional[str]:
+        arg = self.find_layout_arg(node, attr, dim)
+        return arg.symbol if arg else None
+
+    def find_layout_arg(
+        self, node: IRNode, attr: ValidLayoutAttrs, dim: int
+    ) -> Optional[LayoutArg]:
+        matches = [
+            arg for arg in self.layout_args.values() if arg.matches(node, attr, dim)
+        ]
+        assert len(matches) <= 1, matches
+        return None if len(matches) == 0 else matches[0]
+
+    def add_layout_arg(
+        self, symbol: ValidLayoutSymbols, node: IRNode, attr: ValidLayoutAttrs, dim: int
+    ):
+        arg = LayoutArg(node, symbol, attr, dim)
+        self.layout_args.setdefault(symbol, arg)
+
+    def init_layout_args(self) -> None:
+        X = self.named_nodes["X"]
+        W = self.named_nodes["W"]
+        Y = self.named_nodes["Y"]
+        Bias = self.named_nodes.get("Bias", None)
+        mdim = _normalize_idx(-2, len(X.get_size()))
+        ndim = _normalize_idx(-1, len(W.get_size()))
+        kdim = _normalize_idx(-1, len(X.get_size()))
+        self.add_layout_arg("M", X, "size", mdim)
+        self.add_layout_arg("N", X, "size", ndim)
+        self.add_layout_arg("K", X, "size", kdim)
+
+        lda_dim = self.find_ld_idx(X)
+        ldb_dim = self.find_ld_idx(W)
+        ldc_dim = self.find_ld_idx(Y)
+        ldd_dim = self.find_ld_idx(Bias) if Bias else None
+        self.add_layout_arg("lda", X, "stride", lda_dim)
+        self.add_layout_arg("ldb", W, "stride", ldb_dim)
+        self.add_layout_arg("ldc", Y, "stride", ldc_dim)
+        if Bias and ldd_dim:
+            self.add_layout_arg("ldd", Bias, "stride", ldd_dim)
+
+    def get_layout_args(self) -> Tuple[Union[Expr, int], ...]:
+        X = self.named_nodes["X"]
+        W = self.named_nodes["W"]
+        Y = self.named_nodes["Y"]
+        Bias = self.named_nodes.get("Bias", None)
+        mdim = _normalize_idx(-2, len(X.get_size()))
+        ndim = _normalize_idx(-1, len(W.get_size()))
+        kdim = _normalize_idx(-1, len(X.get_size()))
+
+        def get_ld(node) -> Union[Expr, int]:
+            dim = self.find_ld_idx(node)
+            return node.get_stride()[dim]
+
+        M = X.get_size()[mdim]
+        N = W.get_size()[ndim]
+        K = X.get_size()[kdim]
+        LDA = get_ld(X)
+        LDB = get_ld(W)
+        LDC = get_ld(Y)
+        LDD = get_ld(Bias) if Bias else 0
+        return (M, N, K, LDA, LDB, LDC, LDD)
+
+    @staticmethod
+    def find_ld_idx(node: IRNode) -> int:
+        strides = node.get_stride()
+        # Handle 1D tensor case
+        if V.graph.sizevars.statically_known_equals(strides[-1], 1):
+            return _normalize_idx(-2, len(strides))
+
+        assert V.graph.sizevars.statically_known_equals(strides[-2], 1), strides[-2]
+        return _normalize_idx(-1, len(strides))
 
 
 class CUDATemplateKernel(CUDAKernel):
@@ -62,8 +172,6 @@ class CUDATemplateKernel(CUDAKernel):
         """
         super().__init__()
         self.kernel_name = kernel_name
-        # Mapping from arg name to IRNode.
-        self.named_nodes: Dict[str, IRNode] = {}
 
     def arg_name(self, node: IRNode) -> Optional[str]:
         """
@@ -79,7 +187,6 @@ class CUDATemplateKernel(CUDAKernel):
         """
         Generates code to check that a node is not null.
         """
-
         if node is None:
             return ""
 
@@ -127,7 +234,6 @@ class CUDATemplateKernel(CUDAKernel):
                            and the actual input passed into this template could be [Bias, X, W].
                            In this case, the `input_reorder` would be [2, 0, 1].
         """
-
         names = [x.strip() for x in names_str.strip().split(",")]
         if len(inputs) + len(outputs) != len(names):
             raise RuntimeError(
@@ -152,9 +258,13 @@ class CUDATemplateKernel(CUDAKernel):
                 self.args.output_buffers[node.get_name()] = name
 
         arg_defs, *_ = self.args.cpp_argdefs()
-        signature = (
-            f"int {self.kernel_name}({', '.join(arg_defs)}, {self._EXTRA_CPP_ARGS})"
-        )
+
+        self.init_layout_args()
+        size_args = [
+            f"const int {s}" for s in ("M", "N", "K", "lda", "ldb", "ldc", "ldd")
+        ]
+
+        signature = f"int {self.kernel_name}({', '.join(arg_defs + size_args)}, {self._EXTRA_CPP_ARGS})"
         self.signature = signature
         return signature
 
@@ -184,11 +294,15 @@ class CUDATemplateKernel(CUDAKernel):
             _, call_args, arg_types = self.args.cpp_argdefs()
         else:
             _, call_args, _, arg_types = self.args.python_argdefs()
+
+        layout_args = self.get_layout_args()
+        call_args.extend(layout_args)
+        arg_types.extend("int" for a in layout_args)
         # dynamo wraps unspec variable as 0d CPU tensor, need convert to scalar
         for i in range(len(call_args)):
             if V.graph.is_unspec_arg(call_args[i]):
                 call_args[i] = call_args[i] + ".item()"
-            else:
+            elif isinstance(arg_types[i], torch_dtype):
                 call_args[i] = (
                     call_args[i]
                     if V.graph.cpp_wrapper
@@ -300,13 +414,15 @@ class CUDATemplateKernel(CUDAKernel):
         if end_index is None:
             end_index = start_index
         end_index = _normalize_idx(end_index, len(node.get_size()))
-
-        sizes = node.get_size()[start_index : end_index + 1]
+        sizes = [
+            self.find_symbol(node, "size", dim=i) or node.get_size()[i]
+            for i in range(start_index, end_index + 1)
+        ]
         if len(sizes) == 0:
             return str(default_value)
 
         val = sympy_product(sizes)
-        return cexpr(self.rename_indexing(val))
+        return val
 
     def stride(self, node: IRNode, index: int, default_value: int = 0) -> str:
         """
@@ -325,7 +441,9 @@ class CUDATemplateKernel(CUDAKernel):
             return str(default_value)
 
         stride = node.get_stride()[index]
-        return cexpr(self.rename_indexing(stride))
+        if V.graph.sizevars.statically_known_leq(stride, 1):
+            return str(stride)
+        return self.find_symbol(node, "stride", dim=index) or str(stride)
 
     def row_or_column_stride(self, node: IRNode, default_value: int = 0) -> str:
         """
