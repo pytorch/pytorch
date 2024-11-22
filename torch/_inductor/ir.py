eddb777c042f28ecc1b48f5eb1de0a2723d4155e
Jason Ansel <jansel@meta.com>
2024-11-22 22:00:18 +0000

    [inductor] Add missing get_reads() method (#141310)
    Summary: This is a possible fix for https://fb.workplace.com/groups/1075192433118967/permalink/794017756161443/
    
    Test Plan:
    ```
    buck2 test 'fbcode//mode/opt' fbcode//ai_infra/distributed_ai/pyper_test_framework/pt2_staging_tests/sw_v2:smallworld_cmf_test -- --exact 'ai_infra/distributed_ai/pyper_test_framework/pt2_staging_tests/sw_v2:smallworld_cmf_test - test_train (ai_infra.distributed_ai.pyper_test_framework.pt2_staging_tests.sw_v2.smallworld_cmf_test.CmfTest)'
    ```
    
    Differential Revision: D66340927
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141310
    Approved by: https://github.com/ezyang

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -4498,6 +4498,9 @@ class InputsKernel(OperationBuffer):
             index_exprs=OrderedSet(),
         )
 
+    def get_reads(self) -> OrderedSet[Dep]:
+        return self.get_read_writes().reads
+
     @classmethod
     def unwrap_storage_for_input(cls, x: IRNode) -> IRNode:
         if isinstance(x, TensorBox):
@@ -4541,6 +4544,9 @@ class InputsKernel(OperationBuffer):
 class NopKernel(InputsKernel):
     def is_no_op(self) -> bool:
         return True
+
+    def get_reads(self) -> OrderedSet[Dep]:
+        return OrderedSet()
 
 
 class ConcatKernel(NopKernel):
