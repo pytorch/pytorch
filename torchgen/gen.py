ac5e7251774b3140e897991150f68313b38bed7e
Shreyans Pathak <pathak.shreyans@gmail.com>
2024-11-22 07:44:47 +0000

    Throw an error if args contain reserved python keywords (#135357)
    This PR adds a check for reserved python keywords in the `torchgen/gen.py/error_check_native_functions`  function.
    
    Fixes #135127
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135357
    Approved by: https://github.com/ezyang
    
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>

--- torchgen/gen.py
+++ torchgen/gen.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import functools
 import json
+import keyword
 import os
 from collections import defaultdict, namedtuple, OrderedDict
 from dataclasses import dataclass, field
@@ -289,6 +290,23 @@ def error_check_native_functions(funcs: Sequence[NativeFunction]) -> None:
                 f"{f.structured_delegate}, but {f.structured_delegate} is not marked as structured. "
                 f"Consider adding 'structured=True' to the delegated operator"
             )
+
+        # Check for reserved Python keywords
+        PYTHON_RESERVED_KEYWORDS = set(keyword.kwlist)
+        # List of pre-existing operators that are known to have reserved keywords
+        # Exclusion list is used to suppress the assertion for these operators
+        EXCLUSION_LIST = {
+            ("_has_compatible_shallow_copy_type", "from"),
+            ("random_.from", "from"),
+            ("uniform_", "from"),
+        }
+
+        for arg in f.func.arguments.flat_all:
+            if arg.name in PYTHON_RESERVED_KEYWORDS:
+                if (str(f.func.name), arg.name) not in EXCLUSION_LIST:
+                    raise AssertionError(
+                        f"Argument name '{arg.name}' in function '{f.func.name}' is a reserved Python keyword."
+                    )
         # See Note [resize_ in Functionalization]
         # resize_() is technically an inplace view op (and therefore needs the tag),
         # but it would be overkill to add a true "view" variant of resize.
