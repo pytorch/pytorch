8b87482bd90a798418e71a00500b23a03347ea9b
ZhiweiYan-96 <zhiwei.yan@intel.com>
2024-11-25 04:58:19 +0000

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/mkldnn/xpu/Linear.h
+++ aten/src/ATen/native/mkldnn/xpu/Linear.h
@@ -8,21 +8,6 @@ struct LinearConverter {
     is_fused_ = false;
   }
 
-  // linear with post-ops
-  template <typename Func>
-  Tensor& call(
-      Tensor& result,
-      const Tensor& input,
-      const Tensor& weight,
-      const c10::optional<Tensor>& bias,
-      Func func) {
-    onednn::Attr attr = func();
-    Tensor _bias = bias.has_value() ? bias.value() : at::Tensor();
-    Tensor _input = input.dim() <= 2 ? input : input.contiguous();
-    return impl::matmul_fusion_variants(
-        result, _input, weight, false, attr, is_fused_, _bias);
-  }
-
   Tensor& call(
       Tensor& result,
       const Tensor& input,
@@ -32,7 +17,7 @@ struct LinearConverter {
     Tensor _bias = bias.has_value() ? bias.value() : at::Tensor();
     Tensor _input = input.dim() <= 2 ? input : input.contiguous();
     return impl::matmul_fusion_variants(
-        result, _input, weight, /*trans*/ true, attr, is_fused_, _bias);
+        result, _input, weight, /*trans*/ false, attr, is_fused_, _bias);
   }
 
   bool is_fused() {
