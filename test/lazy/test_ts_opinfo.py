dd241c79fc206c4b68027452f1c3b6ddd0d3194a
cyy <cyyever@outlook.com>
2024-11-26 09:43:25 +0800

    Enable asan tests

--- test/lazy/test_ts_opinfo.py
+++ test/lazy/test_ts_opinfo.py
@@ -54,12 +54,6 @@ def init_lists():
     SKIP_RUNTIME_ERROR_LIST = {
         "index_select",  # Empty output_sizes is not supported
         "clone",  # is clone decomposed?
-        # General ASAN Failure due to related to generating bool values.
-        # https://github.com/pytorch/pytorch/issues/74519
-        # https://github.com/pytorch/pytorch/issues/63034
-        "nonzero",  # ASAN failure (paste: P501906539)
-        "all",  # ASAN failure
-        "any",  # ASAN failure
         "logdet",  # ASAN failure
     }
     SKIP_INCORRECT_RESULTS_LIST = {
