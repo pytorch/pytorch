cc59ffa56e2dcc947accd640ecb7fbb9dbea3ee7
iremyux <iremyuksel@microsoft.com>
2024-11-26 22:19:56 +0100

    Export NIGHTLIES_PYTORCH_ROOT for both builds

--- .circleci/scripts/binary_windows_arm64_build.sh
+++ .circleci/scripts/binary_windows_arm64_build.sh
@@ -7,10 +7,11 @@ mkdir -p "$PYTORCH_FINAL_PACKAGE_DIR"
 echo "Free space on filesystem before build:"
 df -h
 
+export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
+
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
     $BUILDER_ROOT/windows/arm64/build_libtorch.bat
 elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then
-    export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
     $BUILDER_ROOT/windows/arm64/build_pytorch.bat
 fi
 
