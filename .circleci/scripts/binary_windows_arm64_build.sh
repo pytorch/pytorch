781bd89a2ab1f1b4f0f36c607789dc65f5bd009d
iremyux <iremyuksel@microsoft.com>
2024-11-26 16:42:32 +0100

    Do not change the current directory

--- .circleci/scripts/binary_windows_arm64_build.sh
+++ .circleci/scripts/binary_windows_arm64_build.sh
@@ -7,13 +7,11 @@ mkdir -p "$PYTORCH_FINAL_PACKAGE_DIR"
 echo "Free space on filesystem before build:"
 df -h
 
-export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
-
-pushd "$BUILDER_ROOT"
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
-    ./windows/arm64/build_libtorch.bat
+    $BUILDER_ROOT/windows/arm64/build_libtorch.bat
 elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then
-    ./windows/arm64/build_pytorch.bat
+    export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
+    $BUILDER_ROOT/windows/arm64/build_pytorch.bat
 fi
 
 echo "Free space on filesystem after build:"
