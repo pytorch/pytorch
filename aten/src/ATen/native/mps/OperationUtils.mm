88f3e118709d8111544749c947ca2665b092894e
Nikita Shulga <nikita.shulga@gmail.com>
2024-11-26 17:45:49 -0800

    Update (base update)
    [ghstack-poisoned]

--- aten/src/ATen/native/mps/OperationUtils.mm
+++ aten/src/ATen/native/mps/OperationUtils.mm
@@ -863,8 +863,9 @@ std::vector<std::string> MetalShaderLibrary::getFunctionNames() {
   std::vector<std::string> rc;
   @autoreleasepool {
     NSArray<NSString*>* names = [getLibrary() functionNames];
+    rc.reserve([names count]);
     for (auto idx : c10::irange([names count])) {
-      rc.push_back([[names objectAtIndex:idx] UTF8String]);
+      rc.emplace_back([[names objectAtIndex:idx] UTF8String]);
     }
   }
   return rc;
