2c8a9fd6fac97070848f96b90fb532707d16a93b
Yukio Siraichi <yukio.siraichi@gmail.com>
2024-11-27 15:41:17 -0300

    Update
    [ghstack-poisoned]

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -4600,7 +4600,6 @@ class ReproTests(torch._dynamo.test_case.TestCase):
         else:
             self.assertExpectedInline(cnt.frame_count, """1""")
 
-    @unittest.expectedFailure
     def test_many_overlapping_inputs_does_not_explode_guards(self):
         from torch._dynamo.backends.common import aot_autograd
 
