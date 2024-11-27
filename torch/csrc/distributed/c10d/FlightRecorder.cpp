360000ed4d6da1fab7d266fb88e1248bfeea6c1b
Ke Wen <kw2501@meta.com>
2024-11-27 11:35:15 -0800

    [2/N] Rename NCCLTraceBuffer to TraceBuffer
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/FlightRecorder.cpp
+++ torch/csrc/distributed/c10d/FlightRecorder.cpp
@@ -155,7 +155,7 @@ void DebugInfoWriter::registerWriter(std::unique_ptr<DebugInfoWriter> writer) {
 // Note: `getTraceback` invokes `torch::symbolize`, which may need to acquire
 // the GIL. If you don't want to block the current thread or take the risk of a
 // GIL deadlock, you can use an asynchronous calling mechanism like std::async.
-std::string NCCLTraceBuffer::Entry::getTraceback() {
+std::string TraceBuffer::Entry::getTraceback() {
   torch::CapturedTraceback* traceback = traceback_.get();
   torch::SymbolizedTracebacks s_tbs = torch::symbolize({traceback});
   // We use 0 because we only have one traceback here.
@@ -178,7 +178,7 @@ std::string NCCLTraceBuffer::Entry::getTraceback() {
   return oss.str();
 }
 
-std::optional<size_t> NCCLTraceBuffer::record(
+std::optional<size_t> TraceBuffer::record(
     size_t pg_id,
     const std::tuple<std::string, std::string>& pg_name,
     size_t collective_seq_id,
@@ -252,7 +252,7 @@ std::optional<size_t> NCCLTraceBuffer::record(
   return id_++;
 }
 
-void NCCLTraceBuffer::record_pg_ranks(
+void TraceBuffer::record_pg_ranks(
     const std::tuple<std::string, std::string>& pg_name,
     std::vector<uint64_t> ranks) {
   if (!enabled_) {
@@ -262,7 +262,7 @@ void NCCLTraceBuffer::record_pg_ranks(
   pg_name_to_ranks_[pg_name] = std::move(ranks);
 }
 
-void NCCLTraceBuffer::update_state(Entry& r) {
+void TraceBuffer::update_state(Entry& r) {
   if (r.start_ != nullptr) {
     bool started = r.start_->query();
     if (started && !r.time_discovered_started_) {
@@ -277,7 +277,7 @@ void NCCLTraceBuffer::update_state(Entry& r) {
   }
 }
 
-std::vector<NCCLTraceBuffer::Entry> NCCLTraceBuffer::dump_entries() {
+std::vector<TraceBuffer::Entry> TraceBuffer::dump_entries() {
   std::lock_guard<std::mutex> guard(mutex_);
   std::vector<Entry> result;
   result.reserve(entries_.size());
@@ -299,7 +299,7 @@ std::vector<NCCLTraceBuffer::Entry> NCCLTraceBuffer::dump_entries() {
 
 // Returns the entry with the given id, if it exists. Otherwise, returns
 // std::nullopt.
-std::optional<NCCLTraceBuffer::Entry> NCCLTraceBuffer::getEntry(
+std::optional<TraceBuffer::Entry> TraceBuffer::getEntry(
     std::optional<size_t> id) {
   if (!enabled_ || !id) {
     return std::nullopt;
@@ -314,9 +314,7 @@ std::optional<NCCLTraceBuffer::Entry> NCCLTraceBuffer::getEntry(
   }
 }
 
-void NCCLTraceBuffer::retire_id(
-    std::optional<size_t> id,
-    bool compute_duration) {
+void TraceBuffer::retire_id(std::optional<size_t> id, bool compute_duration) {
   if (!enabled_ || !id) {
     return;
   }
@@ -363,7 +361,7 @@ void NCCLTraceBuffer::retire_id(
   }
 }
 
-const c10::List<c10::IValue> NCCLTraceBuffer::getCollectiveTrace(
+const c10::List<c10::IValue> TraceBuffer::getCollectiveTrace(
     bool includeStacktraces,
     bool onlyActive) {
   auto entries = new_list();
@@ -467,7 +465,7 @@ const c10::List<c10::IValue> NCCLTraceBuffer::getCollectiveTrace(
   return entries;
 }
 
-const c10::Dict<c10::IValue, c10::IValue> NCCLTraceBuffer::getPgConfig() {
+const c10::Dict<c10::IValue, c10::IValue> TraceBuffer::getPgConfig() {
   auto pg_config = new_dict();
   for (const auto& [pg_name, ranks] : pg_name_to_ranks_) {
     auto pg_info = new_dict();
@@ -479,7 +477,7 @@ const c10::Dict<c10::IValue, c10::IValue> NCCLTraceBuffer::getPgConfig() {
   return pg_config;
 }
 
-const std::map<std::string, std::map<std::string, std::string>> NCCLTraceBuffer::
+const std::map<std::string, std::map<std::string, std::string>> TraceBuffer::
     getPgConfigJson() {
   std::map<std::string, std::map<std::string, std::string>> result;
   for (const auto& [pg_name, ranks] : pg_name_to_ranks_) {
@@ -492,7 +490,7 @@ const std::map<std::string, std::map<std::string, std::string>> NCCLTraceBuffer:
   return result;
 }
 
-const c10::Dict<c10::IValue, c10::IValue> NCCLTraceBuffer::getPgStatus() {
+const c10::Dict<c10::IValue, c10::IValue> TraceBuffer::getPgStatus() {
   auto all_pg_status = new_dict();
   for (const auto& [pg_id, status] : all_pg_status_) {
     auto pg_status = new_dict();
@@ -504,7 +502,7 @@ const c10::Dict<c10::IValue, c10::IValue> NCCLTraceBuffer::getPgStatus() {
   return all_pg_status;
 }
 
-const std::map<std::string, std::map<std::string, std::string>> NCCLTraceBuffer::
+const std::map<std::string, std::map<std::string, std::string>> TraceBuffer::
     getPgStatusJson() {
   std::map<std::string, std::map<std::string, std::string>> result;
   for (const auto& [pg_id, status] : all_pg_status_) {
@@ -520,7 +518,7 @@ const std::map<std::string, std::map<std::string, std::string>> NCCLTraceBuffer:
   return result;
 }
 
-std::string NCCLTraceBuffer::dump_json(
+std::string TraceBuffer::dump_json(
     const std::optional<std::unordered_map<
         std::string,
         std::unordered_map<std::string, std::string>>>& ncclDumpMap,
@@ -611,14 +609,14 @@ std::string NCCLTraceBuffer::dump_json(
   return result.dump();
 }
 
-std::string NCCLTraceBuffer::dump(
+std::string TraceBuffer::dump(
     const std::optional<std::unordered_map<
         std::string,
         std::unordered_map<std::string, std::string>>>& ncclDumpMap,
     bool includeCollectives,
     bool includeStackTraces,
     bool onlyActive) {
-  STATIC_SCOPED_WAIT_COUNTER(pytorch.wait_counter.NCCLTraceBuffer__dump);
+  STATIC_SCOPED_WAIT_COUNTER(pytorch.wait_counter.TraceBuffer__dump);
   auto result = new_dict();
   // common values
   result.insert(version_key, version_val);
--- torch/csrc/distributed/c10d/FlightRecorder.hpp
+++ torch/csrc/distributed/c10d/FlightRecorder.hpp
@@ -92,14 +92,14 @@ float getDurationFromEvent(
     at::cuda::CUDAEvent& ncclStartEvent,
     at::cuda::CUDAEvent& ncclEndEvent);
 
-struct NCCLTraceBuffer {
-  static NCCLTraceBuffer* get() {
+struct TraceBuffer {
+  static TraceBuffer* get() {
     // intentionally leak on exit
     // because this will hold python state that may get destructed
-    static NCCLTraceBuffer* instance = new NCCLTraceBuffer();
+    static TraceBuffer* instance = new TraceBuffer();
     return instance;
   }
-  NCCLTraceBuffer() {
+  TraceBuffer() {
     max_entries_ = getCvarInt({"TORCH_NCCL_TRACE_BUFFER_SIZE"}, 0);
     capture_cpp_stack_ = getCvarBool({"TORCH_NCCL_TRACE_CPP_STACK"}, false);
     enabled_ = max_entries_ > 0;
