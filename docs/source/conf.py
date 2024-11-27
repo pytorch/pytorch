784e1771310c5c7fb5466e7530eef4a484ed623f
Mark Saroufim <marksaroufim@fb.com>
2024-11-27 22:52:53 +0000

    [BE] Remove Model Dump utility (#141540)
    So I found this utility by accident, trying to find how many html files we have in the repo so I could convert them to markdown
    
    Turns out we package some html and js files in pytorch to visualize torchscript models. This seems kinda strange, probably shouldn't be in core, I removed the tests I could find. Maybe some internal tests will break but considering torchscript is being superseded might make sense to do this
    
    Last time there was a meaningful update to the test for this file was about 2 years ago by @digantdesai since then it's a bunch of routine upgrades
    
    It seems like this package is unused https://github.com/search?type=code&auto_enroll=true&q=torch.utils.model_dump&p=1 I skimmed through 5 pages of these and the only time this shows up in code search is when someone is either cloning pytorch or checking in their venv into github
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141540
    Approved by: https://github.com/malfet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,7 +156,6 @@ coverage_ignore_functions = [
     "DistributedDataParallelCPU",
     # torch.utils
     "set_module",
-    # torch.utils.model_dump
     "burn_in_info",
     "get_info_and_burn_skeleton",
     "get_inline_skeleton",
