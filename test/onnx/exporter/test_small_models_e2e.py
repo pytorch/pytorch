fe00c14b9037a2ae4adcb6d6cc093f58532698ba
Bludator <bludator@gmail.com>
2024-11-28 01:28:17 +0000

    fix parameter mismatch

--- test/onnx/exporter/test_small_models_e2e.py
+++ test/onnx/exporter/test_small_models_e2e.py
@@ -132,19 +132,16 @@ class DynamoExporterTest(common_utils.TestCase):
             def __init__(self):
                 super().__init__()
 
-            def forward(self, x):
+            def forward(self, *x):
                 out = torchvision.ops.batched_nms(x[0], x[1], x[2], x[3])
                 return out
 
         torch.manual_seed(1)
         args = (
-            (
-                torch.rand(20, 4, dtype=torch.float),
-                torch.rand(20, dtype=torch.float),
-                torch.randint(0, 2, (20,), dtype=torch.float),
-                0,
-            ),
-            3,
+            torch.rand(20, 4, dtype=torch.float),
+            torch.rand(20, dtype=torch.float),
+            torch.randint(0, 2, (20,), dtype=torch.float),
+            0,
         )
         onnx_program = torch.onnx.export(VisionModel(), args, dynamo=True)
         onnx_testing.assert_onnx_program(onnx_program)
