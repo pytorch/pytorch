457a186fbe09686f90d7c2756cd9c0ac2ec62c2c
Yu, Guangye <guangye.yu@intel.com>
2024-11-28 19:18:38 +0000

    Update
    [ghstack-poisoned]

--- torch/distributed/_composable/fsdp/_fsdp_init.py
+++ torch/distributed/_composable/fsdp/_fsdp_init.py
@@ -58,7 +58,7 @@ def _init_default_fully_shard_mesh() -> DeviceMesh:
     if not dist.distributed_c10d.is_initialized():
         dist.distributed_c10d.init_process_group()
     default_pg = dist.distributed_c10d._get_default_group()
-    device = torch._C._get_accelerator()
+    device = torch.accelerator.current_accelerator()
     mesh = init_device_mesh(device.type, mesh_shape=(default_pg.size(),))
     return mesh
 
