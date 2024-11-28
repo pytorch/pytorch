c80b602fddfb47f4b65e2fcaf17684d6db86f7bd
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2024-11-28 03:02:39 -0600

    #129777 Fix libnvrtc.so shorthash by regressing to ${Python_EXECUTABLE}

--- cmake/public/cuda.cmake
+++ cmake/public/cuda.cmake
@@ -132,7 +132,7 @@ set(CUDA_NVRTC_LIB "${CUDA_nvrtc_LIBRARY}" CACHE FILEPATH "")
 if(CUDA_NVRTC_LIB AND NOT CUDA_NVRTC_SHORTHASH)
   find_package(Python COMPONENTS Interpreter)
   execute_process(
-    COMMAND Python::Interpreter -c
+    COMMAND ${Python_EXECUTABLE} -c
     "import hashlib;hash=hashlib.sha256();hash.update(open('${CUDA_NVRTC_LIB}','rb').read());print(hash.hexdigest()[:8])"
     RESULT_VARIABLE _retval
     OUTPUT_VARIABLE CUDA_NVRTC_SHORTHASH)
