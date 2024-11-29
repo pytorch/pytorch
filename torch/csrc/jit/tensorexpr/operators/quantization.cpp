b875d4cebda0358e883d8f7c613a59d2ec6ca383
cyy <cyyever@outlook.com>
2024-11-29 12:23:31 +0800

    Fix NOLINTNEXTLINE

--- torch/csrc/jit/tensorexpr/operators/quantization.cpp
+++ torch/csrc/jit/tensorexpr/operators/quantization.cpp
@@ -543,7 +543,6 @@ Tensor computeQuantizedMulScalar(
     const std::vector<ArgValue>& inputs,
     const std::vector<ExprHandle>& outputShape,
     const std::vector<ExprHandle>& outputStrides,
-    // NOLINTNEXTLINE
     const std::optional<ScalarType>& outputType,
     at::Device device) {
   const BufHandle& qa = std::get<BufHandle>(inputs[0]);
@@ -598,9 +597,7 @@ Tensor computeQuantizedCat(
     const std::vector<ArgValue>& inputs,
     const std::vector<ExprHandle>& outputShape,
     const std::vector<ExprHandle>& outputStrides,
-    // NOLINTNEXTLINE
     const std::optional<ScalarType>& outputType,
-    // NOLINTNEXTLINE
     at::Device device) {
   // NOLINTNEXTLINE(performance-unnecessary-copy-initialization)
   auto inputList = std::get<BufList>(inputs[0]);
