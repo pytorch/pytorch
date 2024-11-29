b5e96ba04b0c326c9d67a1d6ad492b8d8bd4b0eb
cyyever <cyyever@outlook.com>
2024-11-29 23:40:00 +0800

    Update torch/csrc/jit/tensorexpr/external_functions.cpp
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>

--- torch/csrc/jit/tensorexpr/external_functions.cpp
+++ torch/csrc/jit/tensorexpr/external_functions.cpp
@@ -204,7 +204,7 @@ std::vector<at::Tensor> constructTensors2(
                          .requires_grad(false);
       auto tensor = at::from_blob(
           buf_data_vec[i], buf_dims_vec[i], buf_strides_vec[i], options);
-      tensors.emplace_back(tensor);
+      tensors.emplace_back(std::move(tensor));
     }
   } else {
     // handle quantized
