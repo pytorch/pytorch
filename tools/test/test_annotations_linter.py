72e7547114e867dbf17e2d4e1f61d34374c880e2
Tom Ritchford <tom@swirly.com>
2024-11-30 14:26:04 +0000

    Update
    [ghstack-poisoned]

---
+++ tools/test/test_annotations_linter.py
@@ -0,0 +1,79 @@
+# mypy: ignore-errors
+from __future__ import annotations
+
+from pathlib import Path
+
+from tools.linter.adapters._linter import is_name, is_op, PythonFile
+from tools.linter.adapters.annotations_linter import (
+    _close_bracket,
+    _split_on_commas,
+    AnnotationsLinter,
+    FROM_FUTURE,
+)
+from tools.test.test_set_linter import LinterTestCase
+
+
+TEST_FILE = Path("tools/test/annotations_linter_testdata/python_code.py.txt")
+
+
+def run_on(fn, list_of_tests):
+    def run(x):
+        try:
+            return fn(x)
+        except Exception as e:
+            return e
+
+    args, expected = zip(*list_of_tests)
+    actual = (run(a) for a in args)
+    errors = [rea for rea in zip(args, expected, actual) if rea[1] != rea[2]]
+    return [{"args": r, "expected": e, "actual": a} for r, e, a in errors]
+
+
+class TestAnnotationsLinter(LinterTestCase):
+    LinterClass = AnnotationsLinter
+
+    def test_broken_case(self):
+        s = "b: Union[int | None, str]"
+        r = self.replace(s).replace(FROM_FUTURE, "").strip()
+        self.assertEqual(r, "b: int | None | str")
+
+    def test_close_bracket(self):
+        def cb(s):
+            pf = PythonFile("annotation_linter", contents=s)
+            return _close_bracket(pf.tokens, 1)
+
+        errors = run_on(cb, BRACKET_TESTS)
+        self.assertEqual(errors, [])
+
+    def test_commas(self):
+        def cb(s):
+            pf = PythonFile("annotation_linter", contents=s)
+            tokens = list(pf.tokens)
+            self.assertTrue(is_name(tokens[0], "Union"))
+            self.assertTrue(is_op(tokens[1], "["))
+            while not is_op(tokens.pop(), "]"):
+                pass
+            return [i.strip() for i in _split_on_commas(tokens[2:])]
+
+        errors = run_on(cb, COMMA_TESTS)
+        self.assertEqual(errors, [])
+
+    def test_python_json(self):
+        self.lint_fix_test(TEST_FILE, [])
+
+
+BRACKET_TESTS = (
+    ("Set", None),
+    ("List[]", 2),
+    ("tuple[Set[str]]", 6),
+    ("tuple[Optional[str]]", 6),
+    ("Sequence[Dict[str, Map[Any]]]", 11),
+    ("Iterator[Dict[str, Map[Union[set, dict]]]]", 16),
+)
+
+COMMA_TESTS = (
+    ("Union[str, int]", ["str", "int"]),
+    ("Union[str]", ["str"]),
+    ("Union[str, int]", ["str", "int"]),
+    ("Union[str, dict[str, int]]", ["str", "dict[str, int]"]),
+)
