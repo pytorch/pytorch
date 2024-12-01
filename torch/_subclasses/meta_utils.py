3adcae29ad29eb16853888cec3cf4b5d4b56ecc3
soulitzer <soulitzer@gmail.com>
2024-12-01 14:03:10 -0800

    Update on "Allow MetaConverter callback to be parametrized by device"
    [ghstack-poisoned]

--- torch/_subclasses/meta_utils.py
+++ torch/_subclasses/meta_utils.py
@@ -935,7 +935,7 @@ class MetaConverter(Generic[_TensorT]):
                             )
 
                     current_source = AttrSource(source, attr)
-                    inner_callback = functools.partial(callback_, device=t.device)
+                    inner_callback = functools.partial(callback_, device=meta_tensor_desc.device)
                     new_empty_tensor = _empty_create_subclass(
                         meta_tensor_desc,
                         meta_tensor_desc.size,
