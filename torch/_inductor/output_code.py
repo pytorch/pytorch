49fc8aad413b5467d4f19f68e2a664b647f365fb
James Wu <jjwu@meta.com>
2024-12-02 14:34:52 -0800

    Update
    [ghstack-poisoned]

--- torch/_inductor/output_code.py
+++ torch/_inductor/output_code.py
@@ -538,7 +538,12 @@ class MockFXGraphCacheOutput(OutputCode):
         self._time_taken_ns = 0
         self._boxed_call = True
 
-    def post_compile(self, example_inputs, cudagraphs, gm) -> None:
+    def post_compile(
+        self,
+        example_inputs: Sequence[InputType],
+        cudagraphs: BoxedBool,
+        gm: GraphModule,
+    ) -> None:
         pass
 
     def __call__(self, inputs: Sequence[Any]) -> Any:
