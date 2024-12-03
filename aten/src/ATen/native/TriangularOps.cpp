7c1d5db1f3879913d85305b6d9119adfb89fb2ad
cyy <cyyever@outlook.com>
2024-12-03 20:52:23 +0000

    [2/N] Enable UBSAN tests  (#141740)
    Apply c10::load in more places. The function was introduced to cast a byte to valid boolean values, thus fixing the UBSAN errors.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141740
    Approved by: https://github.com/ezyang

--- aten/src/ATen/native/TriangularOps.cpp
+++ aten/src/ATen/native/TriangularOps.cpp
@@ -61,7 +61,7 @@ void apply_triu_tril_single(
         }
         if (!inplace) {  // copy the rest of the self if not inplace
           for (int64_t j = std::max(zero, i + k); j < m; j++) {
-            result[i * res_row_stride + j * res_col_stride] = self[i * self_row_stride + j * self_col_stride];
+            result[i * res_row_stride + j * res_col_stride] = c10::load(&self[i * self_row_stride + j * self_col_stride]);
           }
         }
       }
@@ -74,7 +74,7 @@ void apply_triu_tril_single(
         }
         if (!inplace) {  // copy the rest of the self if not inplace
           for (int64_t j = zero; j < std::min(m, i + k + 1); j++) {
-            result[i * res_row_stride + j * res_col_stride] = self[i * self_row_stride + j * self_col_stride];
+            result[i * res_row_stride + j * res_col_stride] = c10::load(&self[i * self_row_stride + j * self_col_stride]);
           }
         }
       }
