6fb06f0ec210c964a1ea5845a976088c866ce008
James Wu <jjwu@meta.com>
2024-12-03 18:06:09 -0800

    Update
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -6743,7 +6743,9 @@ class MockFXGraphCache:
         gm = self.cache.get(key)
         if gm is not None:
             gm = make_boxed_func(gm)
-            gm = MockFXGraphCacheOutput(gm, key, 0)
+            gm = MockFXGraphCacheOutput(gm)
+            gm._fx_graph_cache_key = key
+            gm._time_taken_ns = 0
         return gm, {}
 
 
