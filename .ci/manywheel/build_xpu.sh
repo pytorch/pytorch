f6f20aab6f6522bfebaeab783242bf554f410ac3
chuanqiw <chuanqi.wang@intel.com>
2024-12-04 09:41:40 +0100

    temp test

--- .ci/manywheel/build_xpu.sh
+++ .ci/manywheel/build_xpu.sh
@@ -21,6 +21,7 @@ source /opt/intel/oneapi/compiler/latest/env/vars.sh
 source /opt/intel/oneapi/pti/latest/env/vars.sh
 source /opt/intel/oneapi/umf/latest/env/vars.sh
 export USE_STATIC_MKL=1
+export TORCH_XPU_ARCH_LIST=xe2-lpg,xe2-hpg
 
 WHEELHOUSE_DIR="wheelhousexpu"
 LIBTORCH_HOUSE_DIR="libtorch_housexpu"
