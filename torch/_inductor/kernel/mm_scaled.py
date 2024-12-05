33715c6f9167953be07c0680a94734c550d2ca7e
drisspg <drisspguessous@gmail.com>
2024-12-05 13:30:57 -0800

    Update
    [ghstack-poisoned]

--- torch/_inductor/kernel/mm_scaled.py
+++ torch/_inductor/kernel/mm_scaled.py
@@ -486,7 +486,8 @@ def get_workspace_arg(num_sms: int, device: torch.device) -> WorkspaceArg:
 
 def use_persistent_tma(k: sympy.core.numbers.Integer) -> bool:
     available = has_triton_tma_device() and triton_config.enable_persistent_matmul
-    # _determine_swizzle_mode_2d requires dim_k to be 32 at least 32 contiguous bytes
+    # _determine_swizzle_mode_2d requires BLOCK_K to be at least 32 contiguous bytes
+    # When K is 16, BLOCK_K = 16 and is not valid
     min_k = k >= 32
     return available and min_k
 
