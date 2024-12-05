b31689b57374cff4874c7489f47ae6afc815d4ee
Jerry Zhang <jerryzh168@gmail.com>
2024-12-04 19:50:18 -0800

    Update on "Reduce num_layers to enable autoquant in CI"
    Summary:
    original autoquant will require A100-80G to run in CI, but we don't have that right now, so reducing num_layer so that we can
    track autoquant in dashboard
    
    Test Plan:
    dashboard
    https://hud.pytorch.org/benchmark/llms?repoName=pytorch%2Fpytorch
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- benchmarks/gpt_fast/benchmark.py
+++ benchmarks/gpt_fast/benchmark.py
@@ -10,7 +10,6 @@ from generate import (
     run_llama2_7b_int8,
     run_llama2_7b_autoquant,
     run_mixtral_8x7b_int8,
-    run_mixtral_8x7b_autoquant,
 )
 
 import torch
@@ -310,7 +309,8 @@ all_experiments = {
     run_llama2_7b_bf16,
     run_llama2_7b_int8,
     run_mixtral_8x7b_int8,
-    run_mixtral_8x7b_autoquant,
+    # with this, benchmark requries more than 40G memory
+    # run_mixtral_8x7b_autoquant,
     # A list of micro-benchmarks.
     run_mlp_layer_norm_gelu,
     run_layer_norm,
@@ -329,7 +329,11 @@ def main(output_file=DEFAULT_OUTPUT_FILE):
             # This happens when torch is compiled with CUDA turning off completely
             device = "cpu"
 
-        torch.compiler.cudagraph_mark_step_begin()
+        if device == "cuda":
+            torch.cuda.empty_cache()
+            torch.cuda.reset_peak_memory_stats()
+            torch.compiler.cudagraph_mark_step_begin()
+
         lst = func(device)
         for x in lst:
             results.append(dataclasses.astuple(x))
