3dd667797a7f46f6c241e492ad1586058acc5174
pytorchbot <pytorchbot@pytorch.com>
2024-12-06 07:34:55 +0000

    2024-12-06 nightly release (80ca6dd892613fd4f1dee9040b8273ddeadb1c50)

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -1454,6 +1454,7 @@ class LoggingLoggerVariable(VariableTracker):
 
     def __init__(self, value, **kwargs) -> None:
         super().__init__(**kwargs)
+        self.value = value
 
     def call_method(
         self,
@@ -1465,7 +1466,15 @@ class LoggingLoggerVariable(VariableTracker):
         if tx.export:
             # For export cases, we can just make debugging functions no-ops
             return
-        unimplemented("Logger not supported for non-export cases")
+        method = getattr(self.value, name, None)
+        function = getattr(method, "__func__", None)
+        if {method, function}.intersection(torch._dynamo.config.ignore_logger_methods):
+            return variables.ConstantVariable.create(None)
+        unimplemented(
+            "Logger not supported for non-export cases. "
+            "To avoid graph breaks caused by logger in compile-mode, it is recommended to"
+            " disable logging by adding logging methods to config.ignore_logger_methods"
+        )
 
 
 class ConstantLikeVariable(VariableTracker):
