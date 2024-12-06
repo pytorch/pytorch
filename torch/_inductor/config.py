4c92e22847626cdd68e6a41a3e7847ff637a45ec
nandesuka <11392812+nandesuka@users.noreply.github.com>
2024-12-05 18:23:31 -0800

    Enable concat support through inductor using pointwise kernels (#141966)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141966
    
    Add ability to always force pointwise kernel for concat codegen through Inductor.
    
    Reviewed By: eellison, blaine-rister
    
    Differential Revision: D66669372

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -470,6 +470,9 @@ max_fusion_size = 64
 # max number of inputs to generate cat as a pointwise op with masked laods
 max_pointwise_cat_inputs = 8
 
+# force concat to be generated as a pointwise op with masked loads
+force_pointwise_cat = False
+
 # replace small reductions with pointwise, disable with `= 1`
 unroll_reductions_threshold = 8
 
