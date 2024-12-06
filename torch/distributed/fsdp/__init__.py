bab15df40a9a04034512c1fa9d75cb3304ba1ae0
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-12-06 18:38:11 +0000

    Revert "[FSDP2] Move to public `torch.distributed.fsdp` (#141868)"
    This reverts commit 45583a5df907a7948693c047e5fe2c8349622069.
    
    Reverted https://github.com/pytorch/pytorch/pull/141868 on behalf of https://github.com/atalman due to failing internally ([comment](https://github.com/pytorch/pytorch/pull/141868#issuecomment-2523925180))

--- torch/distributed/fsdp/__init__.py
+++ torch/distributed/fsdp/__init__.py
@@ -1,13 +1,4 @@
 from ._flat_param import FlatParameter as FlatParameter
-from ._fully_shard import (
-    CPUOffloadPolicy,
-    FSDPModule,
-    fully_shard,
-    MixedPrecisionPolicy,
-    OffloadPolicy,
-    register_fsdp_forward_method,
-    UnshardHandle,
-)
 from .fully_sharded_data_parallel import (
     BackwardPrefetch,
     CPUOffload,
@@ -29,7 +20,6 @@ from .fully_sharded_data_parallel import (
 
 
 __all__ = [
-    # FSDP1
     "BackwardPrefetch",
     "CPUOffload",
     "FullOptimStateDictConfig",
@@ -46,21 +36,4 @@ __all__ = [
     "StateDictConfig",
     "StateDictSettings",
     "StateDictType",
-    # FSDP2
-    "CPUOffloadPolicy",
-    "FSDPModule",
-    "fully_shard",
-    "MixedPrecisionPolicy",
-    "OffloadPolicy",
-    "register_fsdp_forward_method",
-    "UnshardHandle",
 ]
-
-# Set namespace for exposed private names
-CPUOffloadPolicy.__module__ = "torch.distributed.fsdp"
-FSDPModule.__module__ = "torch.distributed.fsdp"
-fully_shard.__module__ = "torch.distributed.fsdp"
-MixedPrecisionPolicy.__module__ = "torch.distributed.fsdp"
-OffloadPolicy.__module__ = "torch.distributed.fsdp"
-register_fsdp_forward_method.__module__ = "torch.distributed.fsdp"
-UnshardHandle.__module__ = "torch.distributed.fsdp"
