8d1640b63180be1334fec65de67ac6f40a2bce81
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-12-07 23:33:37 +0800

    [pytree][4/N] make `torch.utils.pytree` as public API
    Populate APIs from `torch.utils._pytree` (default) or `torch.utils._cxx_pytree` to a new public module `torch.utils.pytree`. There is a environment varaible `PYTORCH_USE_CXX_PYTREE` (disabled by default) to control this.
    
    Since the CXX pytree is now Dynamo traceable, the users can change the underlining pytree implementation by flipping the environment variable while using `torch.utils.pytree`.
    
    ghstack-source-id: 83fb5e30a4dae8e9cfea8172d037c0cfb871241d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137400

--- torch/utils/_pytree.py
+++ torch/utils/_pytree.py
@@ -1623,16 +1623,20 @@ def treespec_loads(serialized: str) -> TreeSpec:
     )
 
 
-class _DummyLeaf:
+class _Asterisk(str):
+    def __new__(cls) -> Self:
+        return super().__new__(cls, "*")
+
     def __repr__(self) -> str:
-        return "*"
+        return "*"  # no quotes
+
+
+_asterisk = _Asterisk()
+del _Asterisk
 
 
 def treespec_pprint(treespec: TreeSpec) -> str:
-    dummy_tree = tree_unflatten(
-        [_DummyLeaf() for _ in range(treespec.num_leaves)],
-        treespec,
-    )
+    dummy_tree = tree_unflatten([_asterisk] * treespec.num_leaves, treespec)
     return repr(dummy_tree)
 
 
