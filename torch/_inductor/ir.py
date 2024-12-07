843018f407c2cdc2a07045ea01998e2233eca897
Jason Ansel <jansel@meta.com>
2024-12-06 21:56:29 -0800

    [inductor] Refactor split factor into V.choices.reduction_split_factor (#142222)
    I want to reuse this for cooperative reduction heuristics (in a later PR).
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/142222
    Approved by: https://github.com/eellison
    ghstack dependencies: #142219, #142033

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1060,85 +1060,22 @@ class Reduction(Loops):
         props = DeviceProperties.create(device)
         num_sm = props.multi_processor_count
         min_elements_per_thread = 32
-        max_elements_per_thread = 512
-        threads_per_sm = 2048
-        min_elements_per_device = min_elements_per_thread * num_sm * threads_per_sm
-        max_elements_per_device = max_elements_per_thread * num_sm * threads_per_sm
-
-        def inner_reduction_splits(reduction_numel_hint: int, numel_hint: int) -> int:
-            if not should_split:
-                return 1
-            # do heuristics that's close to eager mode for split inner reduction
-            # we leak reduction autotune configs here, and will need to refactor to avoid this later
-            num_warps = 8
-            num_threads = 32 * num_warps
-            if numel_hint >= 2 * num_sm:  # don't split if there are enough outputs
-                return 1
-            if reduction_numel_hint <= 8192:
-                return 1
-            if reduction_numel_hint * numel_hint <= min_elements_per_device:
-                split_size = min_elements_per_thread
-            elif reduction_numel_hint * numel_hint < max_elements_per_device:
-                target_blocks = num_sm * threads_per_sm // (2 * num_threads)
-                blocks_per_output = (target_blocks + numel_hint - 1) // numel_hint
-                tmp_split_size = (
-                    reduction_numel_hint + num_threads * blocks_per_output - 1
-                ) // (num_threads * blocks_per_output)
-                divisors = sympy.divisors(reduction_numel_hint)
-                closest = min(divisors, key=lambda x: abs(x - tmp_split_size))
-                if abs(closest - tmp_split_size) < 30:
-                    # prefer even splits, but never smalle than min_elements_per_thread
-                    split_size = max(closest, min_elements_per_thread)
-                else:
-                    split_size = tmp_split_size
-            else:
-                divisors = sympy.divisors(reduction_numel_hint)
-                closest = min(divisors, key=lambda x: abs(x - max_elements_per_thread))
-                if abs(closest - max_elements_per_thread) < 50:
-                    # prefer even splits
-                    split_size = closest
-                else:
-                    split_size = max_elements_per_thread
-            return (reduction_numel_hint + split_size * num_threads - 1) // (
-                split_size * num_threads
+        if should_split:
+            inner_reduction_splits: Callable[[int, int], int] = functools.partial(
+                V.choices.reduction_split_factor, device, inner_reduction=True
             )
-
-        def outer_reduction_splits(reduction_numel_hint: int, numel_hint: int) -> int:
-            if not should_split:
-                return 1
-            # TODO the best heuristic currently has XBLOCK (corresponding to numel_hint) 128
-            # extend to even smaller number of outputs
-            num_warps = 8
-            num_threads = num_warps * 32
-            rvals_per_thread = 4  # comes from heuristics, refactor to not leak here
-            xvals_per_block = 128
-            xblocks = (numel_hint + xvals_per_block - 1) // xvals_per_block
-            if reduction_numel_hint * numel_hint < min_elements_per_device:
-                split_size = min_elements_per_thread
-            elif reduction_numel_hint * numel_hint < max_elements_per_device:
-                target_blocks = num_sm * threads_per_sm // (num_threads)
-                target_blocks = (target_blocks + xblocks - 1) // xblocks
-                tmp_split_size = (
-                    reduction_numel_hint + rvals_per_thread * target_blocks - 1
-                ) // (rvals_per_thread * target_blocks)
-                divisors = sympy.divisors(reduction_numel_hint)
-                closest = min(divisors, key=lambda x: abs(x - tmp_split_size))
-                if abs(tmp_split_size - closest) < 20:
-                    split_size = max(closest, min_elements_per_thread)
-                else:
-                    split_size = tmp_split_size
-            else:
-                divisors = sympy.divisors(reduction_numel_hint)
-                closest = min(divisors, key=lambda x: abs(x - max_elements_per_thread))
-                if abs(closest - max_elements_per_thread) < 50:
-                    # prefer even splits
-                    split_size = closest
-                else:
-                    split_size = max_elements_per_thread
-
-            return (reduction_numel_hint + rvals_per_thread * split_size - 1) // (
-                rvals_per_thread * split_size
+            outer_reduction_splits: Callable[[int, int], int] = functools.partial(
+                V.choices.reduction_split_factor, device, inner_reduction=False
             )
+        else:
+
+            def inner_reduction_splits(
+                reduction_numel_hint: int,
+                numel_hint: int,
+            ) -> int:
+                return 1
+
+            outer_reduction_splits = inner_reduction_splits
 
         # easy cases
         if numel_hint == 1:
