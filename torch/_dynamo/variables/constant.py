16b4e38729f4c8895b82c53c4b969ebcf55bb1f8
Ryan Guo <ryanguo99@meta.com>
2024-12-06 16:11:13 -0800

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/constant.py
+++ torch/_dynamo/variables/constant.py
@@ -7,7 +7,7 @@ import torch
 from torch._dynamo.source import GetItemSource
 
 from .. import variables
-from ..exc import unimplemented, UserError, UserErrorType
+from ..exc import unimplemented
 from ..utils import common_constant_types, istype, np
 from .base import typestr, VariableTracker
 
