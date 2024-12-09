a57688a6d917fa5ee902a8684f2b20a37414fdff
Prachi Gupta <prachi.gupta@amd.com>
2024-12-09 18:55:17 +0000

    [ROCm]scatter_add: use deterministic approach+prefetch duplicated indicies
    On MI300, we are relying on the deterministic
    sort approach to make scatter_add work faster. However, since sorting
    ensures that we are reading from same index or adjacent index, we can
    optimize this approach by prefetching 32x before processing.
    
    With this approach, scatter_add with deterministic path has a
    performance of 0.57ms as compared to 2.70ms before this for this repro:
    
    ```
    dtype = torch.float32
    dim = 1305301
    ind_range = 100
    a = torch.zeros(ind_range, device="cuda", dtype=dtype)
    index = torch.randint(0, ind_range, (dim,), device="cuda")
    src = torch.ones(dim, device="cuda", dtype=dtype)
    
    print("=" * 20)
    print(
        triton.testing.do_bench(
            lambda: a.scatter_add(0, index, src),
            return_mode="median",
        )
    )
    print("=" * 20)
    ```

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -1902,6 +1902,10 @@ TORCH_IMPL_FUNC(scatter_add)
   if (index.numel() == 0) return;
 
   // See Note [Enabling Deterministic Operations]
+#if (defined(USE_ROCM))
+  //For MI300, sort implementation is faster, enforce this path
+    _scatter_via_index_put(self, dim, index, src, mut_out, /*accumulate*/true);
+#else
   // Avoid gpuAtomicAdd for CUDA and XPU if deterministic mode is turned on
   if (globalContext().deterministicAlgorithms() && (self.device().type() == DeviceType::CUDA || self.device().type() == DeviceType::XPU)) {
     _scatter_via_index_put(self, dim, index, src, mut_out, /*accumulate*/true);
@@ -1912,6 +1916,7 @@ TORCH_IMPL_FUNC(scatter_add)
       scatter_add_stub(self.device().type(), mut_out, dim, index, src);
     }
   }
+#endif
 }
 
 TORCH_IMPL_FUNC(scatter_reduce_two)
