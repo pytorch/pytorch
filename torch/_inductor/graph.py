c76c0845fbf35838cc4ae9efc69446daefe43f89
blzheng <beilei.zheng@intel.com>
2024-12-08 19:37:46 -0800

    [inductor] update numbytes_hint for NoneLayout to allow more fusions (#141766)
    We found that [this commit](https://github.com/pytorch/pytorch/commit/6eca0aee76cb9b02c5daeace1f0f62b4dff76e7a) caused a ~6% performance drop in ViT INT8. This was due to changes to the `numbytes_hint` for `NoneLayout`. In this PR, we reverted the changes in `numbytes_hint` to allow more fusions.
    
    ```
    class Model(torch.nn.Module):
        def __init__(self):
            super().__init__()
            self.dense = torch.nn.Linear(768, 768)
            self.layernorm = torch.nn.LayerNorm(768, eps=1e-12)
        def forward(self, context_layer, hidden_states):
            attention_output = self.dense(context_layer)
            hidden_states = attention_output + hidden_states
            layer_output = self.layernorm(hidden_states)
            return layer_output
    ```
    The generated code before (left) and after (right) this PR is as follows:
    ![image](https://github.com/user-attachments/assets/0ec65ae5-103e-4e2c-bf7c-e8bed24fc179)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141766
    Approved by: https://github.com/leslie-fang-intel, https://github.com/jgong5, https://github.com/jansel

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -812,6 +812,16 @@ class GraphLowering(torch.fx.Interpreter):
     def get_dtype(self, buffer_name: str) -> torch.dtype:
         if buffer_name in self.constants:
             return self.constants[buffer_name].dtype
+        # For a mutation op we should return the dtype of the buffer being mutated
+        if (
+            hasattr(self.scheduler, "mutation_real_name")
+            and buffer_name in self.scheduler.mutation_real_name
+        ):
+            mutated_buf = self.scheduler.mutation_real_name[buffer_name]
+            if mutated_buf in self.name_to_buffer:
+                return self.name_to_buffer[mutated_buf].get_dtype()
+            if mutated_buf in self.graph_inputs:
+                return self.graph_inputs[mutated_buf].get_dtype()
         if buffer_name in self.name_to_buffer:
             return self.name_to_buffer[buffer_name].get_dtype()
         if buffer_name in self.graph_inputs:
