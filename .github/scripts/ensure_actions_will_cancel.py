7e8aef42d0d7f7525c1da4b2be12d9111cea2ede
Huy Do <huydhn@gmail.com>
2024-12-10 10:42:08 -0800

    Back to square one

--- .github/scripts/ensure_actions_will_cancel.py
+++ .github/scripts/ensure_actions_will_cancel.py
@@ -20,61 +20,55 @@ def should_check(filename: Path) -> bool:
     with open(filename) as f:
         content = f.read()
 
-    return True
-    #data = yaml.safe_load(content)
-    #on = data.get("on", data.get(True, {}))
-    #return "pull_request" in on
+    data = yaml.safe_load(content)
+    on = data.get("on", data.get(True, {}))
+    return "pull_request" in on
 
 
 if __name__ == "__main__":
     errors_found = False
-    print(WORKFLOWS)
-    # files = [f for f in WORKFLOWS.glob("*.yml") if should_check(f)]
-    # print(files)
-    # names = set()
-#    for filename in files:
-#        with open(filename) as f:
-#            data = yaml.safe_load(f)
-#
-#        name = data.get("name")
-#        if name is not None and name in names:
-#            print("ERROR: duplicate workflow name:", name, file=sys.stderr)
-#            errors_found = True
-#        names.add(name)
-#        print(f"==== {name}")
-#        print(filename.name)
-        #actual = data.get("concurrency", {})
-        #print(actual)
-        #if filename.name == "create_release.yml":
-        #    if not actual.get("group", "").startswith(EXPECTED_GROUP_PREFIX):
-        #        print(
-        #            f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
-        #            file=sys.stderr,
-        #        )
-        #        print(
-        #            f"concurrency group should start with {EXPECTED_GROUP_PREFIX} but found {actual.get('group', None)}",
-        #            file=sys.stderr,
-        #        )
-        #        errors_found = True
-        #elif not actual.get("group", "").startswith(EXPECTED_GROUP):
-        #    print(
-        #        f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
-        #        file=sys.stderr,
-        #    )
-        #    print(
-        #        f"concurrency group should start with {EXPECTED_GROUP} but found {actual.get('group', None)}",
-        #        file=sys.stderr,
-        #    )
-        #    errors_found = True
-        #if not actual.get("cancel-in-progress", False):
-        #    print(
-        #        f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
-        #        file=sys.stderr,
-        #    )
-        #    print(
-        #        f"concurrency cancel-in-progress should be True but found {actual.get('cancel-in-progress', None)}",
-        #        file=sys.stderr,
-        #    )
+    files = [f for f in WORKFLOWS.glob("*.yml") if should_check(f)]
+    names = set()
+    for filename in files:
+        with open(filename) as f:
+            data = yaml.safe_load(f)
+
+        name = data.get("name")
+        if name is not None and name in names:
+            print("ERROR: duplicate workflow name:", name, file=sys.stderr)
+            errors_found = True
+        names.add(name)
+        actual = data.get("concurrency", {})
+        if filename.name == "create_release.yml":
+            if not actual.get("group", "").startswith(EXPECTED_GROUP_PREFIX):
+                print(
+                    f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
+                    file=sys.stderr,
+                )
+                print(
+                    f"concurrency group should start with {EXPECTED_GROUP_PREFIX} but found {actual.get('group', None)}",
+                    file=sys.stderr,
+                )
+                errors_found = True
+        elif not actual.get("group", "").startswith(EXPECTED_GROUP):
+            print(
+                f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
+                file=sys.stderr,
+            )
+            print(
+                f"concurrency group should start with {EXPECTED_GROUP} but found {actual.get('group', None)}",
+                file=sys.stderr,
+            )
+            errors_found = True
+        if not actual.get("cancel-in-progress", False):
+            print(
+                f"'concurrency' incorrect or not found in '{filename.relative_to(REPO_ROOT)}'",
+                file=sys.stderr,
+            )
+            print(
+                f"concurrency cancel-in-progress should be True but found {actual.get('cancel-in-progress', None)}",
+                file=sys.stderr,
+            )
 
     if errors_found:
         sys.exit(1)
