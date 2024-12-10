99851250d2065844c81db5d34b31bf3e4071d00f
Huy Do <huydhn@gmail.com>
2024-12-10 11:29:31 -0800

    Finally the fix

--- .github/scripts/report_git_status.sh
+++ .github/scripts/report_git_status.sh
@@ -3,6 +3,7 @@
 set -eux
 
 CHANGES=$(git status --porcelain "$1")
-#echo "$CHANGES"
-#git diff "$1"
+echo "$CHANGES"
+# NB: Use --no-pager here to avoid git diff asking for a prompt to continue
+git --no-pager diff "$1"
 [ -z "$CHANGES" ]
