6d641219836aaf1ae795b7527c6d46ea50f88c55
Michael Lazos <mlazos@meta.com>
2024-12-09 21:56:38 -0800

    [Dynamo] Implement graph region tracking for deduplication
    Fixes for bfs
    
    Initial region tracking tests
    
    hashing + test fixes
    
    fix for hash
    
    more tests
    
    more fixes
    
    region tracker updates
    
    Update tests
    
    Update tests2
    
    ghstack-source-id: cc782e4cdefbe6ab02e609168c05733fe4516280
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141381
    
    Fixes for region tracking
    
    Reuse input hashing
    
    fixes
    
    fixes2
    
    Fixes
    
    Fixes
    
    Fixes2
    
    f
    
    f
    
    f
    
    f2
    
    f2
    
    PR
    
    Add config
    
    Fixes for tracking
    
    Fix test
    
    fixes

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -382,6 +382,14 @@ enable_cpp_guard_manager = True
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = not is_fbcode()
 
+# Whether to automatically find and replace identical graph
+# regions with a call to invoke_subgraph
+use_graph_deduplication = False
+
+# Whether to track nodes for deduplication (testing only)
+# This flag is ignored if use_graph_deduplication is True
+track_nodes_for_deduplication = False
+
 # Issues a warning in Python 3.13.0 for possibly slower guard evaluation and
 # instructs user to attempt using 3.13.1+, where the CPython bug is fixed.
 # Should be disabled in dynamo-wrapped tests since some tests check that no warnings are issued.
