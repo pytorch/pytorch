1e04de9a481af73d2838ded49473e31aa6c35fb6
Huy Do <huydhn@gmail.com>
2024-12-10 11:19:06 -0800

    git diff then

--- .github/scripts/report_git_status.sh
+++ .github/scripts/report_git_status.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-#CHANGES=$(git status --porcelain "$1")
+
+set -eux
+
+CHANGES=$(git status --porcelain "$1")
 #echo "$CHANGES"
-git diff "$1"
-#[ -z "$CHANGES" ]
+#git diff "$1"
+[ -z "$CHANGES" ]
