9a1f560087f3918e511dc5475ef9ea3ed18e7954
Guilherme Leobas <guilhermeleobas@gmail.com>
2024-12-09 21:57:49 -0300

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/test_functions.py
+++ test/dynamo/test_functions.py
@@ -3466,7 +3466,6 @@ class GraphModule(torch.nn.Module):
         return max(map(lambda x: x.sum(), [a, b]))
 
     # max(map(...)) graph breaks
-    @unittest.expectedFailure
     @make_test
     def test_map_max_const(a):
         return max(map(lambda x: x, [1, 2, 3])), a + 1
