474c69062f87d01667f3951a5e9950e5341b25db
eellison <elias.ellison@gmail.com>
2024-12-09 16:03:44 -0800

    Infer whether prologues can be computed without upcasting to fp32 without changing numerics (#142402)
    For prologues which only do either loads like gathers or dtype conversions, and no actual arithmetic on lower-precision types, we can codegen them without upcasting to fp32 without changing numerics.
    
    Prologues that actually do arithmetic will need to use invoke quant. But I would like to to support upcasts/gathers out of the box.
    
    We could potentially extend this in the future to avoid upcasting max pooling operations as well, if there were perf benefits to be had (less likely).
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/142402
    Approved by: https://github.com/jansel
    ghstack dependencies: #134532, #142350, #142400, #142401

--- torch/_inductor/analyze_preserves_zero_mask.py
+++ torch/_inductor/analyze_preserves_zero_mask.py
@@ -1,9 +1,11 @@
+import dataclasses
 import itertools
 from typing import Any, Callable, Optional, TYPE_CHECKING
 
 import sympy
 
 import torch
+from torch._inductor import config
 from torch._inductor.dtype_propagation import DtypePropagationOpsHandler
 from torch._inductor.index_propagation import SymPyOps, TypedExpr
 
@@ -66,14 +68,88 @@ class PreservesZeros(SymPyOps):
         return inner
 
 
-def prologue_preserves_zero_mask(node: "SchedulerNode") -> bool:
+def prologue_preserves_zero_mask(prologue: "SchedulerNode") -> bool:
     """
     Does this prologue preserve zero masks
     """
     preserves_zeros = PreservesZeros()
     with V.set_ops_handler(preserves_zeros):
-        node._body(*node.get_ranges())
+        prologue._body(*prologue.get_ranges())
 
     store_preserves_zeros = preserves_zeros.store_preserves_zeros
     assert isinstance(store_preserves_zeros, bool)
+
     return store_preserves_zeros
+
+
+@dataclasses.dataclass
+class DTypeContainer:
+    dtype: torch.dtype
+    is_scalar: bool = False
+
+
+class RecordLowPrecisionOps:
+    def __init__(self) -> None:
+        self.low_precision_numeric_op = False
+        self.dtype_prop = DtypePropagationOpsHandler()
+        self.non_numeric_ops = (
+            "to_dtype",
+            "constant",
+        )
+
+    @staticmethod
+    def load(name: str, index: sympy.Expr) -> DTypeContainer:
+        return DTypeContainer(V.get_ops_handler().dtype_prop.load(name, index))
+
+    @staticmethod
+    def store(
+        name: str, index: sympy.Expr, value: TypedExpr, mode: "StoreMode" = None
+    ) -> None:
+        pass
+
+    @staticmethod
+    def indirect_indexing(*args: Any, **kwargs: Any) -> sympy.Expr:
+        return sympy.S.Zero
+
+    def __getattr__(self, name: str) -> Callable[..., Any]:
+        def low_prec_float(dtype: torch.dtype) -> bool:
+            return dtype.is_floating_point and dtype.itemsize < 4
+
+        def inner(*args: Any, **kwargs: Any) -> DTypeContainer:
+            out_dtype = getattr(self.dtype_prop, name)(*args, **kwargs)
+            out = DTypeContainer(out_dtype, is_scalar=(name == "constant"))
+            if name == "constant":
+                out = DTypeContainer(torch.float, is_scalar=True)
+
+            uses_low_prec = any(
+                isinstance(dtype_cont, DTypeContainer)
+                and low_prec_float(dtype_cont.dtype)
+                for dtype_cont in itertools.chain((out,), args, kwargs.values())
+            )
+
+            if uses_low_prec and name not in self.non_numeric_ops:
+                self.low_precision_numeric_op = True
+
+            return out
+
+        return inner
+
+
+def can_codegen_without_upcasts(
+    prologue: "SchedulerNode",
+) -> bool:
+    """
+    Can this prologue be run without `upcast_to_fp32` while preserving numerics.
+
+    This is only true if the node only contains dtype conversions, indexing, and other non-arithmetic operators.
+    """
+
+    low_prec_analysis = RecordLowPrecisionOps()
+
+    # Need to turn off upcasting to do analysis of whether we can turn it off
+    with config.patch("triton.codegen_upcast_to_fp32", False), V.set_ops_handler(
+        low_prec_analysis
+    ):
+        prologue._body(*prologue.get_ranges())
+
+    return not low_prec_analysis.low_precision_numeric_op
