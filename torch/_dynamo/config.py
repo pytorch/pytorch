d739b4809d102d141f8d878e20b8b812c57fb3bb
Guilherme Leobas <guilhermeleobas@gmail.com>
2024-12-10 21:28:02 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -383,7 +383,7 @@ enable_cpp_guard_manager = True
 enable_trace_contextlib = True
 
 # Changes dynamo behavior on generator functions
-enable_yield_on_generator = False
+enable_yield_on_generator = True
 
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = not is_fbcode()
