23732fc5e3d25d3ed44df19dc6f61253d1a38aa9
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-12-11 17:32:26 +0000

    Revert "Prologue Fusion (#134532)"
    This reverts commit 59ab3825e77451b29c3a118fd24304afcbf52c09.
    
    Reverted https://github.com/pytorch/pytorch/pull/134532 on behalf of https://github.com/clee2000 due to A couple of PRs in this stack are breaking internally on different tests ([comment](https://github.com/pytorch/pytorch/pull/134532#issuecomment-2536643675))

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -3474,9 +3474,6 @@ class TritonKernel(SIMDKernel):
             if self._has_constant_mask(tree):
                 mask_vars.discard(f"{tree.prefix}mask")
 
-        # can be added as an override_mask
-        mask_vars.discard("None")
-
     def iteration_ranges_codegen_header(self, entry, code):
         x = entry.prefix
         if entry.is_loop:
@@ -3624,7 +3621,7 @@ class TritonScheduling(SIMDScheduling):
 
         return kernel_name
 
-    def benchmark_fused_nodes(self, nodes, n_spills_threshold=8):
+    def benchmark_fused_nodes(self, nodes):
         with preserve_rng_state(), torch.cuda.device(
             V.graph.get_current_device_or_throw()
         ):
@@ -3677,9 +3674,7 @@ class TritonScheduling(SIMDScheduling):
 
             launchers = wrapped_jit_function.launchers
             assert len(launchers) == 1
-            # n_spills does not necessarily mean it's not profitable to fuse,
-            # and sometimes it can be inaccurate
-            if launchers[0].n_spills > n_spills_threshold:
+            if launchers[0].n_spills > 0:
                 # skip benchmarking the kernel if there are register spills
                 ms = float("inf")
             else:
