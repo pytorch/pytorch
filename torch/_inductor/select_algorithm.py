f8407e20064f3a892630156ae35cb733ef4a72bf
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-12-11 17:26:46 +0000

    Revert "filelock: Make waitcounter variant to use (#139816)"
    This reverts commit 237c4b559c0f928dd89cf1e773458a1bdcea0b9d.
    
    Reverted https://github.com/pytorch/pytorch/pull/139816 on behalf of https://github.com/clee2000 due to Sorry, I need to revert this in order to revert something else.  The only thing you need to do is rebase and remerge ([comment](https://github.com/pytorch/pytorch/pull/139816#issuecomment-2536616808))

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -30,12 +30,12 @@ from typing import (
 from unittest.mock import patch
 
 import sympy
+from filelock import FileLock
 
 import torch
 import torch._inductor.async_compile  # noqa: F401 required to warm up AsyncCompile pools
 from torch._dynamo.testing import rand_strided
 from torch._dynamo.utils import counters, dynamo_timed, identity, preserve_rng_state
-from torch.utils._filelock import FileLock
 from torch.utils._ordered_set import OrderedSet
 
 from . import config, ir
