584c89d995918044b2e8a095d07f04fbf3410041
Nripesh Niketan <nripesh14@gmail.com>
2024-12-12 20:34:05 +0000

    Remove unnecessary classes

--- test/test_mps.py
+++ test/test_mps.py
@@ -1194,41 +1194,6 @@ class MpsMemoryLeakCheck:
                    f"MPS driver allocated memory was {self.driver_before} and is now {driver_mem_allocated}.")
 
             raise RuntimeError(msg)
-        
-class SelfAttentionBlock(nn.Module):
-    def __init__(self, in_channels):
-        super().__init__()
-        self.query_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
-        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
-        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-    def forward(self, x):
-        B, C, H, W = x.size()
-        query = self.query_conv(x).reshape(B, -1, H * W)  # Use reshape here
-        print("Query output:", query)
-        key = self.key_conv(x).reshape(B, -1, H * W)
-        value = self.value_conv(x).reshape(B, -1, H * W)
-
-        attention = torch.bmm(query.permute(0, 2, 1), key)
-        attention = torch.softmax(attention, dim=-1)
-        out = torch.bmm(value, attention.permute(0, 2, 1))
-        out = out.reshape(B, C, H, W)  # Use reshape here
-        return self.gamma * out + x
-
-
-class SimpleDenoiseNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv_in = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.attn = SelfAttentionBlock(32)
-        self.conv_out = nn.Conv2d(32, 3, kernel_size=3, padding=1)
-
-    def forward(self, x):
-        x = self.conv_in(x)
-        x = self.attn(x)
-        x = self.conv_out(x)
-        return x
 
 class TestAutocastMPS(TestCase):
 
