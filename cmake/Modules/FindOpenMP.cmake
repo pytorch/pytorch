d2d0a424a2f078a9c77481c5d50a58745236af07
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-12-11 16:06:59 -0800

    Check if FindOpenMP is actually used

--- cmake/Modules/FindOpenMP.cmake
+++ cmake/Modules/FindOpenMP.cmake
@@ -1,3 +1,5 @@
+message(FATAL You should not be called)
+
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
