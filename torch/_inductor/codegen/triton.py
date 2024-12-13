9b2b06ba528aeabbaf8d8bb0d02e4a2f0f43ed7b
eellison <elias.ellison@gmail.com>
2024-12-13 14:29:36 -0800

    Update
    [ghstack-poisoned]

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -775,17 +775,17 @@ def maybe_upcast_float32(convert_output: bool = True):
             upcast_args = [maybe_upcast_arg(arg) for arg in args]
             upcast_kwargs = {key: maybe_upcast_arg(val) for key, val in kwargs.items()}
 
-            result_dtype = getattr(get_dtype_handler(), func.__name__)(*args, **kwargs)
-
             # Call the decorated function, optionally downcasting the result.
             result = func(*upcast_args, **upcast_kwargs)
-            needs_downcast = (
-                convert_output
-                and any(
-                    needs_upcast(var) for var in itertools.chain(args, kwargs.values())
-                )
-                and result_dtype not in (torch.float32, None)
+            any_needs_upcast = convert_output and any(
+                needs_upcast(var) for var in itertools.chain(args, kwargs.values())
             )
+            result_dtype = (
+                None
+                if not any_needs_upcast
+                else getattr(get_dtype_handler(), func.__name__)(*args, **kwargs)
+            )
+            needs_downcast = result_dtype not in (torch.float32, None)
             downcast_string = (
                 f".to({triton_type(result_dtype)})"
                 if needs_downcast and result_dtype is not None
