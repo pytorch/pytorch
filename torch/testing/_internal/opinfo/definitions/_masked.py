089b804abd6ce31b4529c0832965a1ec5f3aac09
George Wigley <georgewi@graphcore.ai>
2024-12-13 11:40:10 +0000

    Add back all dtypes and bool

--- torch/testing/_internal/opinfo/definitions/_masked.py
+++ torch/testing/_internal/opinfo/definitions/_masked.py
@@ -769,7 +769,7 @@ op_db: List[OpInfo] = [
         supports_forward_ad=True,
         supports_fwgrad_bwgrad=True,
         promotes_int_to_float=True,
-        dtypes=floating_and_complex_types_and(torch.float16, torch.bfloat16),
+        dtypes=all_types_and_complex_and(torch.float16, torch.bfloat16, torch.bool),
         skips=(
             DecorateInfo(
                 unittest.expectedFailure,
