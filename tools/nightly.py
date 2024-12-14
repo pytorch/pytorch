85f8bd5cd04d99edd20f85c46e3bd6defc5c66b9
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-12-15 05:09:21 +0800

    Update
    [ghstack-poisoned]

--- tools/nightly.py
+++ tools/nightly.py
@@ -44,6 +44,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import textwrap
 import time
 import uuid
 from ast import literal_eval
@@ -317,6 +318,42 @@ class Venv:
         self.base_python("-m", "venv", str(self.prefix))
         assert self.is_venv(), "Failed to create virtual environment."
         (self.prefix / ".gitignore").write_text("*\n", encoding="utf-8")
+
+        if LINUX:
+            activate_script = self.prefix / "bin" / "activate"
+            st_mode = activate_script.stat().st_mode
+            activate_script.chmod(st_mode | 0o200)
+            with (self.prefix / "bin" / "activate").open(mode="a") as f:
+                f.write(
+                    "\n"
+                    + textwrap.dedent(
+                        f"""
+                        # Add NVIDIA PyPI packages to LD_LIBRARY_PATH
+                        export LD_LIBRARY_PATH="$(
+                            {self.executable.name} - <<EOS
+                        import glob
+                        import itertools
+                        import os
+                        import site
+
+                        nvidia_libs = [
+                            p.rstrip("/")
+                            for p in itertools.chain.from_iterable(
+                                glob.iglob(f"{{site_dir}}/{{pattern}}/", recursive=True)
+                                for site_dir in site.getsitepackages()
+                                for pattern in ("nvidia/**/lib", "cu*/**/lib")
+                            )
+                        ]
+                        ld_library_path = os.getenv("LD_LIBRARY_PATH", "").split(":")
+                        print(":".join(dict.fromkeys((*nvidia_libs, *ld_library_path))))
+                        EOS
+                        )"
+                        """
+                    ).strip()
+                    + "\n"
+                )
+            activate_script.chmod(st_mode)
+
         return self.ensure()
 
     def ensure(self) -> Path:
