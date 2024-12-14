0c1c533163a65b82a07114048386ac0c3232bcbe
Yang Wang <elainewy@meta.com>
2024-12-13 23:05:38 -0800

    test

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -126,12 +126,13 @@ class UsageLogger:
         self._data_collect_interval = data_collect_interval
         self._has_pynvml = pynvml_enabled
         self._has_amdsmi = amdsmi_enabled
-        self._kill_now = False
         self._gpu_handles: list[Any] = []
         self._gpu_libs_detected: list[str] = []
         self._num_of_cpus = 0
         self._debug_mode = is_debug_mode
         self._initial_gpu_handler()
+
+        self.exit_event = threading.Event()
 
         self.data_list: list[UsageData] = []
         self.lock = threading.Lock()
@@ -142,7 +143,7 @@ class UsageLogger:
         """
         Collects the data every data_collect_interval (in seconds).
         """
-        while not self._kill_now:
+        while not self.exit_event.is_set():
             try:
                 # collect cpu, memory and gpu metrics
                 memory = psutil.virtual_memory().percent
@@ -165,6 +166,89 @@ class UsageLogger:
                 continue
             finally:
                 time.sleep(self._data_collect_interval)
+
+    def _ouput_data(self) -> None:
+        """
+        output the data.
+        """
+        self._summary_info["start_time"] = datetime.datetime.now().timestamp()
+        self.log_json(self._summary_info)
+
+        while not self.exit_event.is_set():
+            collecting_start_time = time.time()
+            stats = {}
+            try:
+                with self.lock:
+                    if self._debug_mode:
+                        print("collected:", len(self.data_list))
+
+                    if not self.data_list or len(self.data_list) < 1:
+                        continue
+
+                    # record timestamp
+                    stats.update(
+                        {
+                            "level": "record",
+                            "time": datetime.datetime.now().timestamp(),
+                        }
+                    )
+
+                    # collect cpu and memory metrics
+                    total_cpu = sum(
+                        usageData.cpu_percent for usageData in self.data_list
+                    )
+                    avg_cpu = total_cpu / len(self.data_list)
+                    max_cpu = max(usageData.cpu_percent for usageData in self.data_list)
+
+                    max_memory = max(
+                        usageData.memory_percent for usageData in self.data_list
+                    )
+                    total_memory = sum(
+                        usageData.memory_percent for usageData in self.data_list
+                    )
+                    avg_memory = total_memory / len(self.data_list)
+
+                    cmds = list(set(process['cmd'] for data in self.data_list for process in data.processes))
+                    
+                    stats.update(
+                        {
+                            "cpu": {
+                                "avg": round(avg_cpu, 2),
+                                "max": round(max_cpu, 2),
+                            },
+                            "memory": {
+                                "avg": round(avg_memory, 2),
+                                "max": round(max_memory, 2),
+                            },
+                            "cmds":cmds
+                        }
+                    )
+
+                    # collect gpu metrics
+                    if self._has_pynvml or self._has_amdsmi:
+                        gpu_list = self._calculate_gpu_utilization(self.data_list)
+                        stats.update(
+                            {
+                                "gpu_list": gpu_list,
+                            }
+                        )
+                    self.data_list.clear()
+            except Exception as e:
+                stats = {
+                    "level": "record",
+                    "time": datetime.datetime.now().timestamp(),
+                    "error": str(e),
+                }
+            finally:
+                collecting_end_time = time.time()
+                time_diff = collecting_end_time - collecting_start_time
+                # verify there is data
+                if stats:
+                    stats["log_duration"] = f"{time_diff * 1000:.2f} ms"
+                    self.log_json(stats)
+                time.sleep(self._log_interval)
+        # shut down gpu connections when exiting
+        self._shutdown_gpu_connections()
 
     def _calculate_gpu_utilization(self, data_list) -> list[dict[str, Any]]:
         """
@@ -212,103 +296,17 @@ class UsageLogger:
             )
         return calculate_gpu
 
-    def _ouput_data(self) -> None:
-        """
-        output the data.
-        """
-        self._summary_info["start_time"] = datetime.datetime.now().timestamp()
-        self.log_json(self._summary_info)
-
-        while not self._kill_now:
-            collecting_start_time = time.time()
-            stats = {}
-            try:
-                with self.lock:
-                    if self._debug_mode:
-                        print("collected:", len(self.data_list))
-
-                    if not self.data_list or len(self.data_list) < 1:
-                        continue
-
-                    # collect cpu and memory metrics
-                    stats.update(
-                        {
-                            "level": "record",
-                            "time": datetime.datetime.now().timestamp(),
-                        }
-                    )
-
-                    total_cpu = sum(
-                        usageData.cpu_percent for usageData in self.data_list
-                    )
-                    avg_cpu = total_cpu / len(self.data_list)
-                    max_cpu = max(usageData.cpu_percent for usageData in self.data_list)
-
-                    max_memory = max(
-                        usageData.memory_percent for usageData in self.data_list
-                    )
-                    total_memory = sum(
-                        usageData.memory_percent for usageData in self.data_list
-                    )
-                    avg_memory = total_memory / len(self.data_list)
-
-                    stats.update(
-                        {
-                            "cpu": {
-                                "avg": round(avg_cpu, 2),
-                                "max": round(max_cpu, 2),
-                            },
-                            "memory": {
-                                "avg": round(avg_memory, 2),
-                                "max": round(max_memory, 2),
-                            },
-                        }
-                    )
-
-                    # collect gpu metrics
-                    if self._has_pynvml or self._has_amdsmi:
-                        gpu_list = self._calculate_gpu_utilization(self.data_list)
-                        stats.update(
-                            {
-                                "gpu_list": gpu_list,
-                            }
-                        )
-                    self.data_list.clear()
-            except Exception as e:
-                stats = {
-                    "level": "record",
-                    "time": datetime.datetime.now().timestamp(),
-                    "error": str(e),
-                }
-            finally:
-                collecting_end_time = time.time()
-                time_diff = collecting_end_time - collecting_start_time
-                stats["log_duration"] = f"{time_diff * 1000:.2f} ms"
-                # output the data to stdout
-                self.log_json(stats)
-                time.sleep(self._log_interval)
-
-        # shut down gpu connections when exiting
-        self._shutdown_gpu_connections()
-
-    def start_collection(self):
+    def start(self):
         self.collect_thread = threading.Thread(target=self._collect_data)
         self.collect_thread.start()
-
-    def start_output(self):
-        self.output_thread = threading.Thread(target=self._ouput_data)
-        self.output_thread.start()
-
-    def start(self):
-        self.start_collection()
-        self.start_output()
+        self._ouput_data()
+        self.collect_thread.join()
 
     def stop(self, *args: Any) -> None:
         """
         Exits the program gracefully. this shuts down the logging loop.
         """
-        # TODO: add interruptable timer, that if the script is killed, it will stop the sleep immediatly.
-        self._kill_now = True
+        self.exit_event.set()
 
     def log_json(self, stats: Any) -> None:
         """
@@ -428,7 +426,7 @@ class UsageLogger:
                     processName = process.name()
                     pid = process.pid
                     if "python" in processName and cmd.startswith("python"):
-                        python_test_processes.append({"pid": process.pid, "cmd": cmd})
+                        python_test_processes.append({"pid": pid, "cmd": cmd})
                 except Exception as e:
                     pass
             return python_test_processes
