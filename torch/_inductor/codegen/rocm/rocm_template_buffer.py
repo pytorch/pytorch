be02b75559be56f47923b210e117d7582a2abefb
bobrenjc93 <bobren@meta.com>
2024-12-16 09:00:14 -0800

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/codegen/rocm/rocm_template_buffer.py
+++ torch/_inductor/codegen/rocm/rocm_template_buffer.py
@@ -1,14 +1,13 @@
-from typing import Callable, List, Optional
-
-from ...ir import IRNode, Layout, TemplateBuffer
+# mypy: allow-untyped-defs
+from ...ir import TemplateBuffer
 
 
 class ROCmTemplateBuffer(TemplateBuffer):
     def __init__(
         self,
-        layout: Layout,
-        inputs: List[IRNode],
-        make_kernel_render: Callable[[Optional[List[IRNode]]], str],
+        layout,
+        inputs,
+        make_kernel_render,
         workspace_size: int,
         template: "ROCmTemplate",  # type: ignore[name-defined]  # noqa: F821
     ) -> None:
@@ -17,5 +16,5 @@ class ROCmTemplateBuffer(TemplateBuffer):
         self.workspace_size = workspace_size
         self.template = template
 
-    def get_workspace_size(self) -> int:
+    def get_workspace_size(self):
         return self.workspace_size if self.workspace_size is not None else 0
