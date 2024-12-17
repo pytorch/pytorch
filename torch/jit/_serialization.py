3c950114716aa1fc608098974ec39da0db751f1f
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2024-12-17 10:25:30 -0800

    Add warning to torch.jit.load
    [ghstack-poisoned]

--- torch/jit/_serialization.py
+++ torch/jit/_serialization.py
@@ -110,6 +110,11 @@ def load(f, map_location=None, _extra_files=None, _restore_shapes=False):
     Returns:
         A :class:`ScriptModule` object.
 
+    .. warning::
+        It is possible to construct malicious pickle data which will execute arbitrary code
+        during unpickling. Never load data that could have come from an untrusted
+        source in an unsafe mode, or that could have been tampered with. **Only load data you trust**.
+
     Example:
     .. testcode::
 
