0f4661037f010d7b514ca0ce6f984b11661ce8ad
qiurc <qiurc90@gmail.com>
2024-12-17 09:08:01 +0000

    update 8

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -556,7 +556,7 @@ _unsafe_skip_fsdp_module_guards = (
 )
 
 # Run GC at the end of compilation
-run_gc_after_compile = Config(  # type: ignore[var-annotated
+run_gc_after_compile = Config(  # type: ignore[var-annotated]
     default=True,
     justknob="pytorch/compiler:enable_run_gc_after_compile",
     env_name_default="TORCH_DYNAMO_RUN_GC_AFTER_COMPILE",
