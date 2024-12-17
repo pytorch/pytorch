2aec70fe24c50ff819131985ce1dddca3af79599
qiurc <qiurc90@gmail.com>
2024-12-17 05:11:47 +0000

    update

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -24,6 +24,7 @@ import weakref
 from pathlib import Path
 from types import CellType, CodeType, FunctionType, ModuleType
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, TypeVar, Union
+from typing_extensions import ParamSpec
 from weakref import ReferenceType
 
 import torch
@@ -52,7 +53,6 @@ from torch.utils._python_dispatch import (
     is_in_torch_dispatch_mode,
 )
 from torch.utils._traceback import CapturedTraceback, format_traceback_short
-from typing_extensions import ParamSpec
 
 from . import config, exc, trace_rules
 from .bytecode_analysis import remove_dead_code, remove_pointless_jumps
@@ -350,9 +350,9 @@ def exception_handler(
 
 
 FRAME_COUNTER = 0
-FRAME_COMPILE_COUNTER: typing.Counter[Union[int, FrameStateSizeEntry]] = (
-    collections.Counter()
-)
+FRAME_COMPILE_COUNTER: typing.Counter[
+    Union[int, FrameStateSizeEntry]
+] = collections.Counter()
 
 
 def maybe_cprofile(func: Callable[_P, _T]) -> Callable[_P, _T]:
@@ -629,9 +629,9 @@ def _compile(
     output: Optional[OutputGraph] = None
     tracer: Optional[InstructionTranslator] = None
 
-    tf_mode_stack: List[torch.overrides.TorchFunctionMode] = (
-        torch.overrides._get_current_function_mode_stack()
-    )
+    tf_mode_stack: List[
+        torch.overrides.TorchFunctionMode
+    ] = torch.overrides._get_current_function_mode_stack()
 
     @preserve_global_state
     def transform(
@@ -1305,7 +1305,8 @@ class ConvertFrameProtocol(typing.Protocol):
         frame_state: Dict[str, Union[int, FrameStateSizeEntry]],
         *,
         skip: int = 0,
-    ) -> Optional[GuardedCode]: ...
+    ) -> Optional[GuardedCode]:
+        ...
 
 
 class CatchErrorsWrapper:
