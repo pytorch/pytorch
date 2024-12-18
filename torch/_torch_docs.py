6fa70ed6dcf4c9ddebbbe1d5ae5b2ace6fe036e5
zeshengzong <zesheng.zong@outlook.com>
2024-12-18 17:16:27 +0800

    Fix torch.histogramdd description

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -5053,7 +5053,7 @@ For each N-dimensional point in input:
 If :attr:`bins` is a sequence of N 1D tensors, it explicitly specifies the N sequences
 of bin edges. Each 1D tensor should contain a strictly increasing sequence with at
 least one element. A sequence of K bin edges defines K-1 bins, explicitly specifying
-the left and right edges of all bins. Every bin is exclusive of its left edge. Only
+the left and right edges of all bins. Every bin is inclusive of its left edge. Only
 the rightmost bin is inclusive of its right edge.
 
 If :attr:`bins` is a sequence of N ints, it specifies the number of equal-width bins
