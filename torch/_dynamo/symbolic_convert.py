da765abc5b7fb1d128cdfa7c428bb5df687f01d4
Jane Xu <janeyx@meta.com>
2024-12-19 11:48:45 -0800

    Improve cond error messaging
    [ghstack-poisoned]

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -641,7 +641,7 @@ def generic_jump(truth_fn: typing.Callable[[object], bool], push: bool):
                 raise exc.UserError(
                     exc.UserErrorType.DYNAMIC_CONTROL_FLOW,
                     "Dynamic control flow is not supported at the moment. Please use "
-                    "functorch.experimental.control_flow.cond to explicitly capture the control flow.",
+                    "torch.cond to explicitly capture the control flow.",
                     case_name="cond_operands",
                 )
 
