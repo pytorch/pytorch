0546d4fa63952d4e874c71669f411511593c8ed5
drisspg <drisspguessous@gmail.com>
2024-12-19 09:11:35 -0800

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/sparse/cuda/ComputeSparseTile.h
+++ aten/src/ATen/native/sparse/cuda/ComputeSparseTile.h
@@ -4,13 +4,14 @@
 #include <ATen/native/sparse/cuda/StaticSort.h>
 #include <cutlass/bfloat16.h>
 #include <cutlass/half.h>
+#include <cutlass/platform/platform.h>
 #include <cutlass/version.h>
 
 // Given 4x4 values, computes the selected indices that will remain after 2:4
 // sparsification, as a bitmask.
 // NOTE: Algorithms might select LESS than 8 values in total in some cases.
 
-namespace platform {
+namespace cutlass::platform {
 template <>
 struct numeric_limits<cutlass::bfloat16_t> {
   CUTLASS_HOST_DEVICE
@@ -18,9 +19,20 @@ struct numeric_limits<cutlass::bfloat16_t> {
     return cutlass::bfloat16_t::bitcast(0x7f80);
   }
 };
-} // namespace platform
 
-namespace at::native{
+#if CUTLASS_VERSION == 341
+template <>
+struct numeric_limits<cutlass::half_t> {
+  CUTLASS_HOST_DEVICE
+  static cutlass::half_t infinity() {
+    return std::numeric_limits<cutlass::half_t>::infinity();
+  }
+};
+#endif
+
+} // namespace cutlass::platform
+
+namespace at::native {
 
 template <typename Element, typename Pointwise>
 struct TileValueOrderedT {
@@ -69,11 +81,7 @@ template <typename Op = IdentityOp>
 struct LargestValuesGreedy {
   template <typename T>
   static CUTLASS_DEVICE T outOfBoundsFillValue() {
-    #if CUTLASS_VERSION == 351
-    return -std::numeric_limits<T>::infinity();
-    #else
-    return -platform::numeric_limits<T>::infinity();
-    #endif
+    return -cutlass::platform::numeric_limits<T>::infinity();
   }
 
   template <typename Tile4x4Accessor>
@@ -133,11 +141,7 @@ template <typename Op = IdentityOp>
 struct Causal1122 {
   template <typename T>
   static CUTLASS_DEVICE T outOfBoundsFillValue() {
-    #if CUTLASS_VERSION == 351
-    return -std::numeric_limits<T>::infinity();
-    #else
-    return -platform::numeric_limits<T>::infinity();
-    #endif
+    return -cutlass::platform::numeric_limits<T>::infinity();
   }
 
   template <typename Tile4x4Accessor>
@@ -190,4 +194,4 @@ void named_algorithms(T callback) {
   callback(LargestValuesGreedy<IdentityOp>(), "");
 }
 
-} // namespace
+} // namespace at::native
