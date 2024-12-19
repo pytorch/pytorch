fc4c4fa76911208bb694ccba0797da4dcf9d72b8
Yanbo Liang <ybliang8@gmail.com>
2024-12-19 13:04:37 -0800

    Update
    [ghstack-poisoned]

---
+++ benchmarks/gpt_fast/common.py
@@ -0,0 +1,25 @@
+import dataclasses
+from typing import Callable, Dict
+
+all_experiments: Dict[str, Callable] = {}
+
+
+@dataclasses.dataclass
+class Experiment:
+    name: str
+    metric: str
+    target: float
+    actual: float
+    dtype: str
+    device: str
+    arch: str  # GPU name for CUDA or CPU arch for CPU
+    is_model: bool = False
+
+
+def register_experiment(name: str = None):
+    def decorator(func):
+        key = name or func.__name__
+        all_experiments[key] = func
+        return func
+
+    return decorator
