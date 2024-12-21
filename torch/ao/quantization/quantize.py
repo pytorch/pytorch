cf063192a55166397bc0e9c4e859670b72f7fdcc
Tom Ritchford <tom@swirly.com>
2024-12-21 16:36:52 +0000

    Update
    [ghstack-poisoned]

--- torch/ao/quantization/quantize.py
+++ torch/ao/quantization/quantize.py
@@ -778,8 +778,8 @@ def swap_module(
 
             # respect device affinity when swapping modules
             devices = _get_unique_devices_(mod)
-            assert (
-                len(devices) <= 1 or (len(devices) == 2 and torch.device("meta") in devices)
+            assert len(devices) <= 1 or (
+                len(devices) == 2 and torch.device("meta") in devices
             ), f"swap_module only works with cpu or single-device CUDA modules, but got devices {devices}"
             device = next(iter(devices)) if len(devices) > 0 else None
             if device:
