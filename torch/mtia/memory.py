a9ddf5acb995ce0c76b691bff0eaeb07eaa24329
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-12-21 04:01:23 +0000

    Revert "(MTIA) Move "empty_cache" API (#143402)"
    This reverts commit c7d9f298072a3f59b39517e367c7d3d2ea30e6d9.
    
    Reverted https://github.com/pytorch/pytorch/pull/143402 on behalf of https://github.com/huydhn due to The internal diff D67148738 has been reverted ([comment](https://github.com/pytorch/pytorch/pull/143402#issuecomment-2557982597))

--- torch/mtia/memory.py
+++ torch/mtia/memory.py
@@ -10,11 +10,6 @@ from . import _device_t, is_initialized
 from ._utils import _get_device_index
 
 
-def empty_cache() -> None:
-    r"""Empty the MTIA device cache."""
-    return torch._C._mtia_emptyCache()
-
-
 def max_memory_allocated(device: Optional[_device_t] = None) -> int:
     r"""Return the maximum memory allocated in bytes for a given device.
 
@@ -42,7 +37,6 @@ def memory_stats(device: Optional[_device_t] = None) -> Dict[str, Any]:
 
 
 __all__ = [
-    "empty_cache",
     "memory_stats",
     "max_memory_allocated",
 ]
