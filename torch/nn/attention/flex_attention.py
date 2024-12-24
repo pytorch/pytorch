b557dde5c10e74f192c776d496d3c79a76f4e494
Zhang, Liangang <liangang.zhang@intel.com>
2024-12-23 16:55:41 -0800

    Add xpu for _validate_device

--- torch/nn/attention/flex_attention.py
+++ torch/nn/attention/flex_attention.py
@@ -1128,7 +1128,8 @@ def _validate_device(query: Tensor, key: Tensor, value: Tensor):
     """TODO: Remove once non cuda/cpu devices support is added
     We only need to check query since we have already that q,k,v are on the same device
     """
-    if query.device.type != "cuda" and query.device.type != "cpu":
+    supported_devices = {"cuda", "cpu", "xpu"}
+    if query.device.type not in supported_devices:
         raise ValueError(
             "FlexAttention is only supported on CUDA or CPU devices. "
             f"Found input tensors on {query.device.type} device."
