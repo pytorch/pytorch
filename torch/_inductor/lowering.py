b5a660a65283875975d06eca3bd82fbfe2fd4407
Jason Ansel <jansel@meta.com>
2024-12-23 21:20:05 -0800

    [inductor] Make adaptive_max_pool2d error on int64 (#143762)
    Fixes #143752
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/143762
    Approved by: https://github.com/yanboliang

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -4630,6 +4630,9 @@ fallback_adaptive_max_pool2d = fallback_handler(
 
 @register_lowering(aten.adaptive_max_pool2d)
 def adaptive_max_pool2d(x, output_size):
+    if x.get_dtype() == torch.int64:
+        # not supported in eager
+        raise RuntimeError("adaptive_max_pool2d not implemented for Long")
     assert isinstance(x, TensorBox)
     assert len(output_size) == 2
     x.realize_hint()
