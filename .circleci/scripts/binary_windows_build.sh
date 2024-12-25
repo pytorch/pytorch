637ec2b8c27cc17df4290946aa09831287f665c7
Xu Han <xu.han@outlook.com>
2024-12-25 21:34:22 +0800

    update code.

--- .circleci/scripts/binary_windows_build.sh
+++ .circleci/scripts/binary_windows_build.sh
@@ -5,7 +5,7 @@ source "${BINARY_ENV_FILE:-/c/w/env}"
 mkdir -p "$PYTORCH_FINAL_PACKAGE_DIR"
 
 export CUDA_VERSION="${DESIRED_CUDA/cu/}"
-export USE_SCCACHE=1
+# export USE_SCCACHE=1
 # export SCCACHE_BUCKET=ossci-compiler-cache
 export SCCACHE_IGNORE_SERVER_IO_ERROR=1
 export VC_YEAR=2022
