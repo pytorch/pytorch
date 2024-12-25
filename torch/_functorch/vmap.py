723558c50406f13b30ef21d2a7040e349d33f550
Yanbo Liang <ybliang8@gmail.com>
2024-12-24 18:21:26 -0800

    Update
    [ghstack-poisoned]

--- torch/_functorch/vmap.py
+++ torch/_functorch/vmap.py
@@ -521,9 +521,9 @@ def wrap_batched(args, bdims, level):
 
 
 def unwrap_batched(args, level):
-    if all(not isinstance(arg, torch.Tensor) for arg in args):
-        return args
     flat_args, spec = tree_flatten(args)
+    if all(not isinstance(arg, torch.Tensor) for arg in flat_args):
+        return args
     if len(flat_args) == 0:
         return args, ()
     result = [
