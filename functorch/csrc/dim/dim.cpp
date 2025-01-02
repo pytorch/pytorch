96f5a4e12aaf856fdd33d7093bad2252ba2924d7
eqy <eddiey@nvidia.com>
2025-01-01 19:41:44 -0800

    try to fix 3.13 include

--- functorch/csrc/dim/dim.cpp
+++ functorch/csrc/dim/dim.cpp
@@ -40,7 +40,13 @@ PyObject* Dim_init() {
 #if IS_PYTHON_3_11_PLUS
 
 #define Py_BUILD_CORE
+#if IS_PYTHON_3_13_PLUS
+#define NEED_OPCODE_METADATA
+#include "internal/pycore_opcode_metadata.h"
+#undef NEED_OPCODE_METADATA
+#else
 #include "internal/pycore_opcode.h"
+#endif
 #undef Py_BUILD_CORE
 #endif
 
