0d9c35f15d0536e44245dca31a52ff5142bdaee0
Shaobin Ma <44595835+bmeoaountiful@users.noreply.github.com>
2025-01-03 20:43:06 +0800

    add builtinfunction into common_constant_types

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1734,6 +1734,7 @@ common_constant_types: Set[type] = {
     type(None),
     Ellipsis.__class__,
     NotImplemented.__class__,
+    types.BuiltinFunctionType,
     types.CodeType,
     # Commonly used immutable types from torch.
     torch.device,
