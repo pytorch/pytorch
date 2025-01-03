1a95fefcd050f20f02df2964591c1c871b4fadae
cyy <cyyever@outlook.com>
2025-01-03 09:33:12 +0800

    Fix C++20 Wambiguous-reversed-operator warnings

--- aten/src/ATen/cpu/vec/vec512/vec512_bfloat16.h
+++ aten/src/ATen/cpu/vec/vec512/vec512_bfloat16.h
@@ -636,8 +636,8 @@ public:
     return cvt_from_fp32<T>(o1, o2);
   }
 private:
-  template<typename Op>
-  Vectorized<T> inline binary_compare(const Vectorized<T>& b, Op op) const {
+  template<typename Op, typename VectorizedType>
+  Vectorized<T> inline binary_compare(const VectorizedType& b, Op op) const {
     __m512 a_lo, a_hi;
     __m512 b_lo, b_hi;
     cvt_to_fp32<T>(values, a_lo, a_hi);
@@ -676,14 +676,14 @@ public:
       return _mm512_castsi512_ps(_mm512_mask_set1_epi32(zero_vec, cmp, 0xFFFFFFFF));
     });
   }
-  Vectorized<T> inline operator==(const Vectorized<T>& other) const {
+  Vectorized<T> inline operator==(const Vectorized16<T>& other) const {
     return binary_compare(other, [](__m512 x, __m512 y) {
       auto zero_vec = _mm512_set1_epi32(0);
       auto cmp = _mm512_cmp_ps_mask(x, y, _CMP_EQ_OQ);
       return _mm512_castsi512_ps(_mm512_mask_set1_epi32(zero_vec, cmp, 0xFFFFFFFF));
     });
   }
-  Vectorized<T> inline operator!=(const Vectorized<T>& other) const {
+  Vectorized<T> inline operator!=(const Vectorized16<T>& other) const {
     return binary_compare(other, [](__m512 x, __m512 y) {
       auto zero_vec = _mm512_set1_epi32(0);
       auto cmp = _mm512_cmp_ps_mask(x, y, _CMP_NEQ_UQ);
