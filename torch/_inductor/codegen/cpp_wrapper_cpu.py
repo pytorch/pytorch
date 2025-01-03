37a8871d45083fe3b6dae957534ff7382a2d3237
Isuru Fernando <ifernando@quansight.com>
2025-01-03 11:20:18 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_wrapper_cpu.py
+++ torch/_inductor/codegen/cpp_wrapper_cpu.py
@@ -1965,7 +1965,9 @@ if (custom_op_wrapper.get() == NULL) {
                 elif isinstance(raw_arg, bool):
                     return f"PyBool_FromLong({1 if raw_arg else 0})"
                 elif isinstance(raw_arg, complex):
-                    return f"PyComplex_FromDoubles({self.generate_float_value(raw_arg.real), self.generate_float_value(raw_arg.imag)})"
+                    real = self.generate_float_value(raw_arg.real)
+                    imag = self.generate_float_value(raw_arg.imag)
+                    return f"PyComplex_FromDoubles({real, imag})"
                 elif isinstance(raw_arg, torch.SymInt):
                     expr = raw_arg.node.expr
                     return f"PyLong_FromLongLong({cexpr(expr)})"
