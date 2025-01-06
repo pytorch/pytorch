7e4f9f067886021d8e3215e8ecfa9113bd11d2e5
bobrenjc93 <bobren@meta.com>
2025-01-06 08:12:54 -0800

    Update base for Update on "remove allow-untyped-defs from torch/_prims/executor.py"
    [ghstack-poisoned]

--- torch/ao/nn/sparse/quantized/utils.py
+++ torch/ao/nn/sparse/quantized/utils.py
@@ -1,13 +1,11 @@
+# mypy: allow-untyped-defs
 import threading
-from typing import Optional, Type
 
 
 __all__ = ["LinearBlockSparsePattern"]
 
 
-def _is_valid_linear_block_sparse_pattern(
-    row_block_size: int, col_block_size: int
-) -> bool:
+def _is_valid_linear_block_sparse_pattern(row_block_size, col_block_size):
     return (row_block_size == 1 and col_block_size == 4) or (
         row_block_size == 8 and col_block_size == 1
     )
@@ -21,12 +19,12 @@ def _is_valid_linear_block_sparse_pattern(
 # Once the flow supports it, this should be removed.
 class LinearBlockSparsePattern:
     rlock = threading.RLock()
-    row_block_size: int = 1
-    col_block_size: int = 4
-    prev_row_block_size: int = 1
-    prev_col_block_size: int = 4
+    row_block_size = 1
+    col_block_size = 4
+    prev_row_block_size = 1
+    prev_col_block_size = 4
 
-    def __init__(self, row_block_size: int = 1, col_block_size: int = 4):
+    def __init__(self, row_block_size=1, col_block_size=4):
         assert _is_valid_linear_block_sparse_pattern(row_block_size, col_block_size)
         LinearBlockSparsePattern.rlock.acquire()
         LinearBlockSparsePattern.prev_row_block_size = (
@@ -38,15 +36,10 @@ class LinearBlockSparsePattern:
         LinearBlockSparsePattern.row_block_size = row_block_size
         LinearBlockSparsePattern.col_block_size = col_block_size
 
-    def __enter__(self) -> None:
+    def __enter__(self):
         pass
 
-    def __exit__(
-        self,
-        exc_type: Optional[Type[BaseException]],
-        exc_value: Optional[BaseException],
-        backtrace: Optional[object],
-    ) -> None:
+    def __exit__(self, exc_type, exc_value, backtrace):
         LinearBlockSparsePattern.row_block_size = (
             LinearBlockSparsePattern.prev_row_block_size
         )
@@ -56,7 +49,7 @@ class LinearBlockSparsePattern:
         LinearBlockSparsePattern.rlock.release()
 
     @staticmethod
-    def block_size() -> tuple[int, int]:
+    def block_size():
         return (
             LinearBlockSparsePattern.row_block_size,
             LinearBlockSparsePattern.col_block_size,
