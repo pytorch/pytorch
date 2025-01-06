b67eb8bb7d45aec7c651b261b210f0b7e262cab1
hongxyan <hongxyan@amd.com>
2025-01-06 20:07:30 +0000

    update the large tensor test to compare with a smaller tensor for correctness

--- test/test_nn.py
+++ test/test_nn.py
@@ -7144,15 +7144,18 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""")
         # test for https://github.com/pytorch/pytorch/issues/136291
         device = torch.device("cuda")
         b, n, dp = 16, 3000, 16
-        pairwise_repr = torch.ones(b, n, n, dp)
+        pairwise_repr = torch.randn(b, n, n, dp)
 
         attn_bias_norm = nn.LayerNorm(dp).to(device=device)
         pairwise_repr = pairwise_repr.to(dtype=torch.float32, device=device)
+        # we want a smaller copy to compare the results
+        pairwise_small = pairwise_repr[-1, -1, -1].detach().clone()
         norm = attn_bias_norm(pairwise_repr)
+        norm_small = attn_bias_norm(pairwise_small)
+
         self.assertEqual(norm.shape, torch.Size([16, 3000, 3000, 16]))
-        # check last value to make sure it is correct.
-        # check all values will take too long
-        self.assertEqual(norm[-1][-1][-1][-1], 0.0)
+        # Check output to make sure it is correct.
+        torch.testing.assert_close(norm_small, norm[-1, -1, -1])
 
     def test_padding_list(self):
         # Padding can be a list, or tuple (regression test for gh-54452)
