32d00dd73db4fd54553566c714b0ece6542e3ed1
Benjamin Glass <bglass@quansight.com>
2025-01-07 00:15:54 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/codegen/cuda/device_op_overrides.py
+++ torch/_inductor/codegen/cuda/device_op_overrides.py
@@ -225,9 +225,6 @@ class CUDADeviceOpOverrides(DeviceOpOverrides):
             #endif
         """
 
-    def abi_compatible_header(self):
-        return "#include <torch/csrc/inductor/aoti_runtime/utils_cuda.h>"
-
     def cpp_stream_type(self):
         return "cudaStream_t"
 
