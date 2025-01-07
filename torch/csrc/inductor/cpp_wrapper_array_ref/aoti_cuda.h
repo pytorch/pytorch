9b9430bc140fe086d1b2b5b328284f3a3e971aa1
Benjamin Glass <bglass@quansight.com>
2025-01-07 22:35:20 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/inductor/cpp_wrapper_array_ref/aoti_cuda.h
+++ torch/csrc/inductor/cpp_wrapper_array_ref/aoti_cuda.h
@@ -1,7 +1,4 @@
-#ifndef TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CUDA
-#define TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CUDA
+#pragma once
 
 #include <torch/csrc/inductor/cpp_wrapper/device_internal/cuda.h>
 #include <torch/csrc/inductor/cpp_wrapper_array_ref/aoti_common.h>
-
-#endif // TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CUDA
