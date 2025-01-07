3f22f58d4293d6a1b81297915787705541100fe7
xinan.lin <xinan.lin@intel.com>
2025-01-06 18:43:09 -0800

    [Windows Inductor] Enable Inductor XPU backend on Windows.

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -5594,7 +5594,7 @@ def remove_cpp_extensions_build_root():
         if IS_WINDOWS:
             # rmtree returns permission error: [WinError 5] Access is denied
             # on Windows, this is a workaround
-            subprocess.run(["rm", "-rf", default_build_root], stdout=subprocess.PIPE)
+            subprocess.run(["cmd", "/c", "rd", "/s", "/q", default_build_root], stdout=subprocess.PIPE)
         else:
             shutil.rmtree(default_build_root, ignore_errors=True)
 
