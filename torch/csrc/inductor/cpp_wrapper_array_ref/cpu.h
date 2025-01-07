d296acb37d9ddf2f7ed8a72b64c93f8075aac3bf
Benjamin Glass <bglass@quansight.com>
2025-01-07 22:38:40 +0000

    Update
    [ghstack-poisoned]

--- torch/csrc/inductor/cpp_wrapper_array_ref/cpu.h
+++ torch/csrc/inductor/cpp_wrapper_array_ref/cpu.h
@@ -1,7 +1,4 @@
-#ifndef TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_CPU
-#define TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_CPU
+#pragma once
 
 #include <torch/csrc/inductor/cpp_wrapper/device_internal/cpu.h>
 #include <torch/csrc/inductor/cpp_wrapper_array_ref/common.h>
-
-#endif // TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_CPU
