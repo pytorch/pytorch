df5d9666fa62edb7e7c25c93f71bd590a49fedf1
fan.mo <fan.mo@mthreads.com>
2025-01-07 21:11:36 +0800

    Support parseDispatchKey with AutogardPrivateUse1 or QuantizedPrivateUse1 when PrivateUse1 is modified to other name like 'XLA'

--- c10/core/DispatchKey.cpp
+++ c10/core/DispatchKey.cpp
@@ -384,6 +384,12 @@ c10::DispatchKey parseDispatchKey(const std::string& k) {
        c10::DispatchKey::FuncTorchBatchedDecomposition},
   };
   auto it = key_map.find(k);
+  if (it == key_map.end() && c10::get_privateuse1_backend() != "PrivateUse1") {
+    std::string pu1_backend_name = c10::get_privateuse1_backend();
+    std::transform(pu1_backend_name.begin(), pu1_backend_name.end(), pu1_backend_name.begin(), ::toupper);
+    std::string processed_k = std::regex_replace(k, std::regex(pu1_backend_name), "PrivateUse1");
+    it = key_map.find(processed_k);
+  }
   TORCH_CHECK(it != key_map.end(), "could not parse dispatch key: ", k);
   return it->second;
 }
