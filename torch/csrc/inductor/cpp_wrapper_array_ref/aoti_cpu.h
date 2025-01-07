ab9704838da913d40a507be997b2a9bd4f0b28b0
Benjamin Glass <bglass@quansight.com>
2025-01-07 00:17:41 +0000

    Update (base update)
    [ghstack-poisoned]

---
+++ torch/csrc/inductor/cpp_wrapper_array_ref/aoti_cpu.h
@@ -0,0 +1,7 @@
+#ifndef TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CPU
+#define TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CPU
+
+#include <torch/csrc/inductor/cpp_wrapper/device_internal/cpu.h>
+#include <torch/csrc/inductor/cpp_wrapper_array_ref/aoti_common.h>
+
+#endif // TORCH_CSRC_INDUCTOR_CPP_WRAPPER_ARRAY_REF_AOTI_CPU
