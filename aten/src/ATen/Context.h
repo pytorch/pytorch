57f9c1cd20e5e07837aeae1654fa60697d0ff18c
Yu, Guangye <guangye.yu@intel.com>
2025-01-08 11:18:13 +0000

    Update
    [ghstack-poisoned]

--- aten/src/ATen/Context.h
+++ aten/src/ATen/Context.h
@@ -505,7 +505,7 @@ inline bool hasMAIA() {
 }
 
 inline bool hasXPU() {
-  return globalContext().hasXPU();
+  return globalContext().hasXPU() && (detail::getXPUHooks().deviceCount() > 0);
 }
 
 inline bool hasHPU() {
@@ -574,16 +574,13 @@ inline void manual_seed(uint64_t seed) {
     }
   }
 
-  const auto xpu_num_gpus = detail::getXPUHooks().deviceCount();
-  if (hasXPU() && xpu_num_gpus) {
-    for (const auto i : c10::irange(xpu_num_gpus)) {
-      auto xpu_gen = globalContext().defaultGenerator(
-          Device(at::kXPU, static_cast<c10::DeviceIndex>(i)));
-      {
-        // See Note [Acquire lock when using random generators]
-        std::lock_guard<std::mutex> lock(xpu_gen.mutex());
-        xpu_gen.set_current_seed(seed);
-      }
+  for (const auto i : c10::irange(detail::getXPUHooks().deviceCount())) {
+    auto xpu_gen = globalContext().defaultGenerator(
+        Device(at::kXPU, static_cast<c10::DeviceIndex>(i)));
+    {
+      // See Note [Acquire lock when using random generators]
+      std::lock_guard<std::mutex> lock(xpu_gen.mutex());
+      xpu_gen.set_current_seed(seed);
     }
   }
 
