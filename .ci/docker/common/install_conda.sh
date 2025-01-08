f63899836003269b4d39297b4eada125267f70bf
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2025-01-08 01:52:32 -0600

    [release/2.5] Changes to support UB 24.04 build (#1816)
    Fixes SWDEV-505665
    
    Changes applied from https://github.com/ROCm/pytorch/pull/1816
    
    Successful PyTorch build:
    http://rocm-ci.amd.com/job/mainline-framework-pytorch-2.5-ub24-py3.12-ci/17/
    
    ---------
    
    Co-authored-by: pramenku <7664080+pramenku@users.noreply.github.com>
    Co-authored-by: Nichols A. Romero <165712832+naromero77amd@users.noreply.github.com>

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -106,6 +106,10 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
     conda_install_through_forge libstdcxx-ng=12
   fi
 
+  if [ "$ANACONDA_PYTHON_VERSION" = "3.12" ] || [ "$UBUNTU_VERSION" == "24.04"* ] ; then
+    conda_install_through_forge libstdcxx-ng=14
+  fi
+
   # Install some other packages, including those needed for Python test reporting
   pip_install -r /opt/conda/requirements-ci.txt
   pip_install numpy=="$NUMPY_VERSION"
