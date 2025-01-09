fd4b7cf484334a9fdb12dcf6db5e4cdec01fa594
Alexander Brauckmann <alexbrauckmann@meta.com>
2025-01-09 10:51:45 -0800

    Make k_cache and v_cache buffers

--- torch/padded/test_padded_tensor.py
+++ torch/padded/test_padded_tensor.py
@@ -9,9 +9,9 @@ from padded_tensor import *
 from utils import *
 
 
-class TestAttention(TestCase):
-    def setUp(self):
-        super().setUp()
+class TransformerModel(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
 
         # Define model parameters
         self.n_local_heads = 8
@@ -43,9 +43,108 @@ class TestAttention(TestCase):
             max_seq_length,
             self.head_dim,
         )
-        k_cache, v_cache = torch.zeros(cache_shape), torch.zeros(cache_shape)
+        self.register_buffer(
+            "k_cache", torch.zeros(cache_shape, dtype=self.dtype, device="cuda")
+        )
+        self.register_buffer(
+            "v_cache", torch.zeros(cache_shape, dtype=self.dtype, device="cuda")
+        )
 
-        return k_cache, v_cache
+    def reset_kv_cache(self):
+        self.k_cache.zero_()
+        self.v_cache.zero_()
+
+    def f_1(self, idx):
+        x = self.tok_embeddings(idx)
+
+        kv_size = self.n_local_heads * self.head_dim
+        yy = self.wqkv(x)
+        q, k, v = yy.split([self.dim, kv_size, kv_size], dim=-1)
+
+        return q, k, v
+
+    def f_2(self, q, k, v, bsz, seqlen):
+        q = q.view(bsz, seqlen, self.n_head, self.head_dim)
+        k = k.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+        v = v.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+
+        return q, k, v
+
+    def apply_rotary_emb(self, x: Tensor, freqs_cis: Tensor) -> Tensor:
+        xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
+        freqs_cis = freqs_cis.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
+        x_out2 = torch.stack(
+            [
+                xshaped[..., 0] * freqs_cis[..., 0]
+                - xshaped[..., 1] * freqs_cis[..., 1],
+                xshaped[..., 1] * freqs_cis[..., 0]
+                + xshaped[..., 0] * freqs_cis[..., 1],
+            ],
+            -1,
+        )
+
+        x_out2 = x_out2.flatten(3)
+        return x_out2.type_as(x)
+
+    def f_3(self, q, k, v, freqs_cis):
+        q = self.apply_rotary_emb(q, freqs_cis)
+        k = self.apply_rotary_emb(k, freqs_cis)
+
+        q, k, v = map(lambda x: x.transpose(1, 2), (q, k, v))
+
+        return q, k, v
+
+    def kv_update(self, input_pos, k_val, v_val, k_cache, v_cache):
+        # input_pos: [S], k_val: [B, H, S, D]
+        assert input_pos.shape[0] == k_val.shape[2]
+
+        k_out = self.k_cache
+        v_out = self.v_cache
+        k_out[:, :, input_pos] = k_val
+        v_out[:, :, input_pos] = v_val
+
+        return k_out, v_out
+
+    def f_4(self, k, v, input_pos, k_cache, v_cache):
+        k, v = self.kv_update(input_pos, k, v, k_cache, v_cache)
+
+        k = k.repeat_interleave(self.n_head // self.n_local_heads, dim=1)
+        v = v.repeat_interleave(self.n_head // self.n_local_heads, dim=1)
+
+        return k, v
+
+    def f_5(self, q, k, v, mask):
+        outs = torch.ops.aten._scaled_dot_product_flash_attention(
+            q, k, v, dropout_p=0.0
+        )
+        y = outs[0]
+        return (y,)
+
+    def f_6(self, y, bsz, seqlen):
+        y = y.transpose(1, 2).contiguous().view(bsz, seqlen, self.dim)
+        return (y,)
+
+    def f_7(self, y):
+        y = self.wo(y)
+        return (y,)
+
+    def f_attention(self, x, freqs_cis, mask, input_pos, k_cache, v_cache):
+        bsz, seqlen = x.shape
+
+        q, k, v = self.f_1(x)
+        q, k, v = self.f_2(q, k, v, bsz, seqlen)
+        q, k, v = self.f_3(q, k, v, freqs_cis)
+        k, v = self.f_4(k, v, input_pos, k_cache, v_cache)
+        (y,) = self.f_5(q, k, v, mask)
+        (y,) = self.f_6(y, bsz, seqlen)
+        (y,) = self.f_7(y)
+
+        return (y,)
+
+
+class TestAttention(TestCase):
+    def setUp(self):
+        super().setUp()
 
     def are_equal(self, outputs, outputs_p):
         for x, x_p in zip(outputs, outputs_p):
@@ -65,7 +164,7 @@ class TestAttention(TestCase):
         # Check the outputs are equal
         self.are_equal(outputs, outputs_p)
 
-    def run_unpadded_padded_bench(self, fn, inputs, inputs_p, outputs_p_shapes):
+    def run_unpadded_padded_bench(self, model, fn, inputs, inputs_p, outputs_p_shapes):
         # Precondition: Are inputs and inputs_p equal?
         self.are_equal(inputs, inputs_p)
 
@@ -77,6 +176,7 @@ class TestAttention(TestCase):
             outputs = None
 
             for _ in range(n_iter):
+                model.reset_kv_cache()
                 inps = pytree.tree_map(lambda x: x.clone(), inputs)
 
                 start = time.time()
@@ -107,63 +207,29 @@ class TestAttention(TestCase):
         print(f"Unpadded time (compiled): {time_compiled}")
         print(f"Padded time (compiled): {time_compiled_padded}")
 
-    def f_1(self, idx):
-        x = self.tok_embeddings(idx)
-
-        kv_size = self.n_local_heads * self.head_dim
-        yy = self.wqkv(x)
-        q, k, v = yy.split([self.dim, kv_size, kv_size], dim=-1)
-
-        return q, k, v
-
     def test_attention_1(self):
+        model = TransformerModel()
+
         inputs = [torch.ones(4, 2, dtype=torch.int32)]
         MULTIPLIERS = {0: 128, 1: 128, 2: 1}
         inputs_p = pytree.tree_map(
             lambda x: PaddedTensor(x, {0: 128, 1: 128, 2: 1}, None), inputs
         )
 
-        self.run_unpadded_padded(self.f_1, inputs, inputs_p)
-
-    def f_2(self, q, k, v, bsz, seqlen):
-        q = q.view(bsz, seqlen, self.n_head, self.head_dim)
-        k = k.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-        v = v.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-
-        return q, k, v
+        self.run_unpadded_padded(model.f_1, inputs, inputs_p)
 
     def test_attention_2(self):
+        model = TransformerModel()
+
         inputs = [torch.randn(4, 2, 10), torch.randn(4, 2, 10), torch.randn(4, 2, 10)]
         MULTIPLIERS = {0: 8, 1: 8, 2: 1}
         inputs_p = pytree.tree_map(lambda x: PaddedTensor(x, MULTIPLIERS, None), inputs)
 
-        self.run_unpadded_padded(self.f_2, inputs + [4, 2], inputs_p + [8, 8])
-
-    def apply_rotary_emb(self, x: Tensor, freqs_cis: Tensor) -> Tensor:
-        xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
-        freqs_cis = freqs_cis.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
-        x_out2 = torch.stack(
-            [
-                xshaped[..., 0] * freqs_cis[..., 0]
-                - xshaped[..., 1] * freqs_cis[..., 1],
-                xshaped[..., 1] * freqs_cis[..., 0]
-                + xshaped[..., 0] * freqs_cis[..., 1],
-            ],
-            -1,
-        )
-
-        x_out2 = x_out2.flatten(3)
-        return x_out2.type_as(x)
-
-    def f_3(self, q, k, v, freqs_cis):
-        q = self.apply_rotary_emb(q, freqs_cis)
-        k = self.apply_rotary_emb(k, freqs_cis)
-
-        q, k, v = map(lambda x: x.transpose(1, 2), (q, k, v))
-
-        return q, k, v
+        self.run_unpadded_padded(model.f_2, inputs + [4, 2], inputs_p + [8, 8])
 
     def test_attention_3(self):
+        model = TransformerModel()
+
         inputs = [
             torch.randn(4, 2, 5, 2),
             torch.randn(4, 2, 5, 2),
@@ -175,28 +241,11 @@ class TestAttention(TestCase):
             PaddedTensor(inputs[-1], {0: 8, 1: 1, 2: 1}, None)
         ]
 
-        self.run_unpadded_padded(self.f_3, inputs, inputs_p)
-
-    def kv_update(self, input_pos, k_val, v_val, k_cache, v_cache):
-        # input_pos: [S], k_val: [B, H, S, D]
-        assert input_pos.shape[0] == k_val.shape[2]
-
-        k_out = k_cache
-        v_out = v_cache
-        k_out[:, :, input_pos] = k_val
-        v_out[:, :, input_pos] = v_val
-
-        return k_out, v_out
-
-    def f_4(self, k, v, input_pos, k_cache, v_cache):
-        k, v = self.kv_update(input_pos, k, v, k_cache, v_cache)
-
-        k = k.repeat_interleave(self.n_head // self.n_local_heads, dim=1)
-        v = v.repeat_interleave(self.n_head // self.n_local_heads, dim=1)
-
-        return k, v
+        self.run_unpadded_padded(model.f_3, inputs, inputs_p)
 
     def test_attention_4(self):
+        model = TransformerModel()
+
         k_cache, v_cache = self.create_kv_cache()
 
         inputs = [
@@ -209,16 +258,11 @@ class TestAttention(TestCase):
         MULTIPLIERS = {0: 1, 1: 1, 2: 1, 3: 1}
         inputs_p = pytree.tree_map(lambda x: PaddedTensor(x, MULTIPLIERS, None), inputs)
 
-        self.run_unpadded_padded(self.f_4, inputs, inputs_p)
-
-    def f_5(self, q, k, v, mask):
-        outs = torch.ops.aten._scaled_dot_product_flash_attention(
-            q, k, v, dropout_p=0.0
-        )
-        y = outs[0]
-        return (y,)
+        self.run_unpadded_padded(model.f_4, inputs, inputs_p)
 
     def test_attention_5(self):
+        model = TransformerModel()
+
         inputs = [
             torch.randn([4, 5, 2, 2]),
             torch.randn([4, 5, 16, 2]),
@@ -228,52 +272,35 @@ class TestAttention(TestCase):
         MULTIPLIERS = {0: 8, 1: 8, 2: 1}
         inputs_p = pytree.tree_map(lambda x: PaddedTensor(x, MULTIPLIERS, None), inputs)
 
-        self.run_unpadded_padded(self.f_5, inputs, inputs_p)
-
-    def f_6(self, y, bsz, seqlen):
-        y = y.transpose(1, 2).contiguous().view(bsz, seqlen, self.dim)
-        return (y,)
+        self.run_unpadded_padded(model.f_5, inputs, inputs_p)
 
     def test_attention_6(self):
+        model = TransformerModel()
+
         inputs = [torch.randn([4, 5, 2, 2])]
         MULTIPLIERS = {0: 8, 1: 8, 2: 1}
         inputs_p = pytree.tree_map(lambda x: PaddedTensor(x, MULTIPLIERS, None), inputs)
-        self.run_unpadded_padded(self.f_6, inputs + [4, 2], inputs_p + [4, 2])
 
-    def f_7(self, y):
-        y = self.wo(y)
-        return (y,)
+        self.run_unpadded_padded(model.f_6, inputs + [4, 2], inputs_p + [4, 2])
 
     def test_attention_7(self):
+        model = TransformerModel()
+
         inputs = [torch.randn([4, 2, 10])]
         MULTIPLIERS = {0: 8, 1: 8, 2: 1}
         inputs_p = pytree.tree_map(lambda x: PaddedTensor(x, MULTIPLIERS, None), inputs)
-        self.run_unpadded_padded(self.f_7, inputs, inputs_p)
 
-    def f_attention(self, x, freqs_cis, mask, input_pos, k_cache, v_cache):
-        bsz, seqlen = x.shape
+        self.run_unpadded_padded(model.f_7, inputs, inputs_p)
 
-        q, k, v = self.f_1(x)
-        q, k, v = self.f_2(q, k, v, bsz, seqlen)
-        q, k, v = self.f_3(q, k, v, freqs_cis)
-        k, v = self.f_4(k, v, input_pos, k_cache, v_cache)
-        (y,) = self.f_5(q, k, v, mask)
-        (y,) = self.f_6(y, bsz, seqlen)
-        (y,) = self.f_7(y)
-
-        return (y,)
-
-    def gen_rand_inputs(self, batchsize, seqlen):
+    def gen_rand_inputs(self, model, batchsize, seqlen):
         x = torch.randint(0, 128256, (batchsize, seqlen)).to(device="cuda")
-        freqs_cis = torch.randn(seqlen, 64, 2).to(device="cuda", dtype=self.dtype)
+        freqs_cis = torch.randn(seqlen, 64, 2).to(device="cuda", dtype=model.dtype)
         mask = torch.ones([batchsize, 1, seqlen, 15], device="cuda")
         input_pos = torch.arange(0, seqlen, dtype=torch.int32, device="cuda")
 
-        k_cache, v_cache = self.create_kv_cache()
-        k_cache = k_cache.to(device="cuda", dtype=self.dtype)
-        v_cache = v_cache.to(device="cuda", dtype=self.dtype)
+        model.create_kv_cache()
 
-        inputs = [x, freqs_cis, mask, input_pos, k_cache, v_cache]
+        inputs = [x, freqs_cis, mask, input_pos, model.k_cache, model.v_cache]
 
         return inputs
 
@@ -292,17 +319,23 @@ class TestAttention(TestCase):
         return inputs_p
 
     def test_attention_all(self):
-        inputs = self.gen_rand_inputs(15, 991)
+        model = TransformerModel()
+
+        inputs = self.gen_rand_inputs(model, 15, 991)
         inputs_p = self.pad_inputs(inputs, 16)
 
-        self.run_unpadded_padded(self.f_attention, inputs, inputs_p)
+        self.run_unpadded_padded(model.f_attention, inputs, inputs_p)
 
     def test_attention_bench(self):
+        model = TransformerModel()
+
         with torch.no_grad():
-            inputs = self.gen_rand_inputs(15, 991)
+            inputs = self.gen_rand_inputs(model, 15, 991)
             inputs_p = self.pad_inputs(inputs, 16)
 
-            self.run_unpadded_padded_bench(self.f_attention, inputs, inputs_p, None)
+            self.run_unpadded_padded_bench(
+                model, model.f_attention, inputs, inputs_p, None
+            )
 
 
 if __name__ == "__main__":
