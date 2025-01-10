9e97640dc85533ed78c5296b0bda6afbde1eafb5
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-01-10 23:39:08 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/optim/swa_utils.py
+++ torch/optim/swa_utils.py
@@ -274,9 +274,9 @@ class AveragedModel(Module):
                 ) in grouped_tensors.items():
                     if self.multi_avg_fn:
                         self.multi_avg_fn(
-                            self_params,
-                            model_params,
-                            self.n_averaged.to(device),  # type: ignore[arg-type]
+                            self_params,  # type: ignore[arg-type]
+                            model_params,  # type: ignore[arg-type]
+                            self.n_averaged.to(device),
                         )
                     elif (
                         device is not None
