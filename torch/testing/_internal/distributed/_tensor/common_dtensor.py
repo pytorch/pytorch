815a522c658cf52f552feac3c3c099ab9d990543
Wanchao Liang <wanchaol@users.noreply.github.com>
2025-01-10 17:23:35 -0800

    Fix DTensorTestBase to barrier with device ids
    Get rid of the below annoying warnings when running the unit tests
    
    ```
    test/distributed/test_device_mesh.py [rank1]:[W106 17:08:03.158159859 ProcessGroupNCCL.cpp:4561] [PG ID 0 PG GUID 0 Rank 1]  using GPU 1 to perform barrier as devices used by this process are currently unknown. This can potentially cause a hang if this rank to GPU mapping is incorrect. Specify device_ids in barrier() to force use of a particular device, or call init_process_group() with a device_id.
    [rank2]:[W106 17:08:03.216576760 ProcessGroupNCCL.cpp:4561] [PG ID 0 PG GUID 0 Rank 2]  using GPU 2 to perform barrier as devices used by this process are currently unknown. This can potentially cause a hang if this rank to GPU mapping is incorrect. Specify device_ids in barrier() to force use of a particular device, or call init_process_group() with a device_id.
    [rank0]:[W106 17:08:04.766730880 ProcessGroupNCCL.cpp:4561] [PG ID 0 PG GUID 0 Rank 0]  using GPU 0 to perform barrier as devices used by this process are currently unknown. This can potentially cause a hang if this rank to GPU mapping is incorrect. Specify device_ids in barrier() to force use of a particular device, or call init_process_group() with a device_id.
    [rank3]:[W106 17:08:04.773544169 ProcessGroupNCCL.cpp:4561] [PG ID 0 PG GUID 0 Rank 3]  using GPU 3 to perform barrier as devices used by this process are currently unknown. This can potentially cause a hang if this rank to GPU mapping is incorrect. Specify device_ids in barrier() to force use of a particular device, or call init_process_group() with a device_id.
    NCCL version 2.21.5+cuda12.1
    ```
    
    [ghstack-poisoned]

--- torch/testing/_internal/distributed/_tensor/common_dtensor.py
+++ torch/testing/_internal/distributed/_tensor/common_dtensor.py
@@ -349,7 +349,8 @@ class DTensorTestBase(MultiProcessTestCase):
         # dist.all_reduce(torch.zeros((1,), device="cuda" if torch.cuda.is_available() else "cpu"))
         # FIXME can't use the above all_reduce as it causes hangs on bionic and focal. It hangs:
         #  test_dtensor.py  -- DTensorMeshTest.test_dtensor_device_mesh_device_conversion
-        dist.barrier()
+        if self.device_type == "cuda":
+            dist.barrier(device_ids=[torch.cuda.current_device()])
         dist.destroy_process_group()
 
     def setUp(self) -> None:
