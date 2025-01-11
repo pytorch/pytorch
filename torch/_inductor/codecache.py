590fb19913238f50c88c3fe7cae9707125cf6baa
Benjamin Glass <bglass@quansight.com>
2025-01-11 00:10:37 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -2010,7 +2010,7 @@ def _get_cpp_wrapper_header(device: str, aot_mode: bool = False) -> str:
     """Given a device type (and optionally whether we're in AOT Inductor mode), returns
     the path to the cpp_wrapper header file to be precompiled."""
     base_device = device.split(":")[0]
-    is_array_ref = config.aot_inductor.allow_stack_allocation
+    is_array_ref = config.aot_inductor.allow_stack_allocation and base_device == "cpu"
     return (
         "torch/csrc/inductor/"
         f"{'aoti_include' if aot_mode else 'cpp_wrapper'}/"
