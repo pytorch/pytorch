a9a3b50b7620186061590a0e92bfb10783bdf8c5
Nikita Shulga <nikita.shulga@gmail.com>
2025-01-11 18:03:43 -0800

    [MPSInductor] Implement `check_bounds`
    Although at the moment it returns rather than rasises assert due to https://github.com/pytorch/pytorch/pull/144632
    
    `pytest test/inductor/test_torchinductor.py -v -k _mps` score is `368
    failed, 391 passed, 32 skipped`

--- test/inductor/test_mps_basic.py
+++ test/inductor/test_mps_basic.py
@@ -59,6 +59,7 @@ class MPSBasicTests(TestCase):
     test_silu = CommonTemplate.test_silu
     test_tanh = CommonTemplate.test_tanh
     test_view_as_complex = CommonTemplate.test_view_as_complex
+    test_views3 = CommonTemplate.test_views3
     test_views6 = CommonTemplate.test_views6
     test_zero_dim_reductions = CommonTemplate.test_zero_dim_reductions
 
