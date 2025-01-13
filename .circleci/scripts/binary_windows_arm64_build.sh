7fe5bde68671d35fdb5b3fb2a4652bcec6d8726b
iremyux <iremyuksel@microsoft.com>
2025-01-13 22:28:02 +0100

    Add sccache env variables

--- .circleci/scripts/binary_windows_arm64_build.sh
+++ .circleci/scripts/binary_windows_arm64_build.sh
@@ -4,15 +4,21 @@ set -eux -o pipefail
 source "${BINARY_ENV_FILE:-/c/w/env}"
 mkdir -p "$PYTORCH_FINAL_PACKAGE_DIR"
 
+export USE_SCCACHE=1
+export SCCACHE_BUCKET=ossci-compiler-cache
+export SCCACHE_IGNORE_SERVER_IO_ERROR=1
+
 echo "Free space on filesystem before build:"
 df -h
 
 export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
 
+pushd "$BUILDER_ROOT/"
+
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
-    $BUILDER_ROOT/windows/arm64/build_libtorch.bat
+    windows/arm64/build_libtorch.bat
 elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then
-    $BUILDER_ROOT/windows/arm64/build_pytorch.bat
+    windows/arm64/build_pytorch.bat
 fi
 
 echo "Free space on filesystem after build:"
