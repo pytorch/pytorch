67d09bbb8cff91ece5f7622653b35cab2bfe76e6
Nikita Shulga <nikita.shulga@gmail.com>
2025-01-13 15:14:01 +0000

    [MPSInductor] Fix maximum/minimum for int types (#144665)
    `metal::isnan` is only defined for floats, so provide a generic wrapper
    that is false for integral types
    
    TODO: Figure out why type propagantion is not working (or should it?)
    
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144665
    Approved by: https://github.com/dcci

--- test/inductor/test_mps_basic.py
+++ test/inductor/test_mps_basic.py
@@ -41,6 +41,7 @@ class MPSBasicTests(TestCase):
     test_add_const_int = CommonTemplate.test_add_const_int
     test_add_inplace_permuted_mps = CommonTemplate.test_add_inplace_permuted
     test_addmm = CommonTemplate.test_addmm
+    test_argmax_min_int32 = CommonTemplate.test_argmax_min_int32
     test_div1 = CommonTemplate.test_div1
     test_div3 = CommonTemplate.test_div3
     test_cat_empty = CommonTemplate.test_cat_empty
