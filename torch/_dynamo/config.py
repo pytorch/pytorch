12c0f96b798db941030dff8587bbb774cb9e08de
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-01-15 18:08:31 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -408,8 +408,9 @@ enable_cpp_guard_manager = True
 # Enable tracing through contextlib.contextmanager
 enable_trace_contextlib = True
 
-# Changes dynamo behavior on generator functions
-enable_faithful_generator_behavior = False
+# Enable tracing generator functions lazily. If False, Dynamo will exhaust
+# generators upon first execution. And if True, the generator will be accessed lazily
+enable_faithful_generator_behavior = True
 
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = not is_fbcode()
