c29250db4d2a80e4b9ee8ff4e9fb36f4e9fae9ac
Colin L. Rice <clr@fb.com>
2025-01-14 17:48:10 -0700

    Update
    [ghstack-poisoned]

--- test/dynamo/test_compile.py
+++ test/dynamo/test_compile.py
@@ -143,6 +143,16 @@ class InPlaceCompilationTests(TestCase):
             printed_output, "Counter = 1\nCounter = 2\nCounter = 3\nCounter = 4"
         )
 
+    def test_compilation_tensor_invalid_method(self):
+        @torch.compile(backend="eager")
+        def fn():
+            y = torch.tensor(x)
+            return y.doesnotexist()
+
+        x = torch.randn(10, 10)
+
+        fn(x)
+
 
 # The private variants of the below functions are extensively tested
 # So as long as the signatures match we're good
