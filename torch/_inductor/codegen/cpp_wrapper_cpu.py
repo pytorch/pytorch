5d47b1f4204a5b6c4dd6537050683aa8a584ca5b
Bin Bao <binbao@meta.com>
2025-01-15 17:10:18 -0800

    [AOTI] Add an option to skip optimizing generated wrapper code (#144866)
    Summary:
    
    In some cases, generated wrapper code faces a long cpp compilation time. As an alleviation, this PR adds an option to skip cpp compiler optimizers for the generated main wrapper function body.
    
    Interstingly, an expriment that turns this on as default shows a reduction in compilation time without any noticable perf regression, https://hud.pytorch.org/benchmark/compilers?dashboard=torchinductor&startTime=Wed%2C%2008%20Jan%202025%2017%3A45%3A53%20GMT&stopTime=Wed%2C%2015%20Jan%202025%2017%3A45%3A53%20GMT&granularity=hour&mode=inference&dtype=bfloat16&deviceName=cpu%20(x86)&lBranch=export-D68174038&lCommit=29a1e91075e49be5656bc0ec69d81b0e8031b0f5&rBranch=main&rCommit=69b883d7ace02cb84a713c3a73e804ace1158eae
    
    Test Plan: CI
    
    Reviewed By: hl475, chenyang78
    
    Differential Revision: D68174038

--- torch/_inductor/codegen/cpp_wrapper_cpu.py
+++ torch/_inductor/codegen/cpp_wrapper_cpu.py
@@ -378,7 +378,17 @@ class CppWrapperCpu(PythonWrapperCodegen):
                         """
                     )
 
-                run_impl_proto = """
+                run_impl_proto = ""
+                if config.aot_inductor.compile_wrapper_with_O0:
+                    run_impl_proto += """
+                    #ifdef __clang__
+                    __attribute__((optnone))
+                    #else
+                    __attribute__((optimize("O0")))
+                    #endif
+                    """
+
+                run_impl_proto += """
                     void AOTInductorModel::run_impl(
                         AtenTensorHandle*
                             input_handles, // array of input AtenTensorHandle; handles
@@ -391,6 +401,7 @@ class CppWrapperCpu(PythonWrapperCodegen):
                         AOTIProxyExecutorHandle proxy_executor
                     ) {
                     """
+
                 if config.aot_inductor.debug_compile:
                     self.generate_input_output_runtime_checks()
                     run_impl_proto += """
