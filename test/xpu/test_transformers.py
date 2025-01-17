612d2df13a89273171532e3a6eb9ffef4c00ce3d
Ding, Yi1 <yi1.ding@intel.com>
2025-01-17 09:33:59 +0000

    Support causal mask

--- test/xpu/test_transformers.py
+++ test/xpu/test_transformers.py
@@ -159,7 +159,7 @@ class TestSDPAXpuOnly(NNTestCase):
         (1, 32, 2016, 2016, 128),
         (4, 32, 2016, 2016, 128),
     ])
-    @parametrize("mask_type", ["float"])
+    @parametrize("mask_type", ["float", "causal"])
     @parametrize("train", [False])
     def test_scaled_dot_product_fused_attention_mask_vs_math(
         self,
@@ -197,30 +197,34 @@ class TestSDPAXpuOnly(NNTestCase):
             k2.requires_grad_(True)
             v2.requires_grad_(True)
 
-        q2, k2, v2 = q2.float(), k2.float(), v2.float()
         # (B, nh, T, hs)
         q = q.view(batch_size, q_size, n_head, head_dim).transpose(1, 2)
         k = k.view(batch_size, kv_size, n_head, head_dim).transpose(1, 2)
         v = v.view(batch_size, kv_size, n_head, head_dim).transpose(1, 2)
         attn_mask = None
+        is_causal = False
         if mask_type == "bool":
             attn_mask = torch.randint(0, 2, size=mask_shape, dtype=torch.bool, device=device)
         elif mask_type == "float":
             attn_mask = torch.randn(mask_shape, dtype=dtype, device=device)
+        elif mask_type == "causal":
+            is_causal = True
 
+        q2, k2, v2 = q2.float(), k2.float(), v2.float()
         q2 = q2.view(batch_size, q_size, n_head, head_dim).transpose(1, 2)
         k2 = k2.view(batch_size, kv_size, n_head, head_dim).transpose(1, 2)
         v2 = v2.view(batch_size, kv_size, n_head, head_dim).transpose(1, 2)
+        attn_mask2 = attn_mask.float() if attn_mask is not None else None
 
         if fused_kernel == SDPBackend.MATH:
             actual = torch.ops.aten._scaled_dot_product_attention_math(
-                q, k, v, attn_mask=attn_mask, dropout_p=0.0, is_causal=False)[0]
+                q, k, v, attn_mask=attn_mask, dropout_p=0.0, is_causal=is_causal)[0]
         elif fused_kernel == SDPBackend.OVERRIDEABLE:
             actual = torch.ops.aten._scaled_dot_product_fused_attention_overrideable(
-                q, k, v, attn_bias=attn_mask, dropout_p=0.0, is_causal=False)[0]
+                q, k, v, attn_bias=attn_mask, dropout_p=0.0, is_causal=is_causal)[0]
 
         math_ref = torch.ops.aten._scaled_dot_product_attention_math(
-            q2, k2, v2, attn_mask=attn_mask.float(), dropout_p=0.0, is_causal=False)[0]
+            q2, k2, v2, attn_mask=attn_mask2, dropout_p=0.0, is_causal=is_causal)[0]
 
         self.assertEqual(actual.float(), math_ref, atol=tol.atol, rtol=tol.rtol)
 
