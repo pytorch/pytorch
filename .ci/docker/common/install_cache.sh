8b1f171412d7ac116d22173503ec015b4ae544df
Wouter Devriendt <wouterdevriendt@meta.com>
2025-01-17 14:40:30 -0800

    remove -E workaround for nvcc

--- .ci/docker/common/install_cache.sh
+++ .ci/docker/common/install_cache.sh
@@ -48,13 +48,6 @@ function write_sccache_stub() {
     cat >"/opt/cache/bin/$1" <<EOF
 #!/bin/sh
 
-# sccache does not support -E flag, so we need to call the original compiler directly in order to avoid calling this wrapper recursively
-for arg in "\$@"; do
-  if [ "\$arg" = "-E" ]; then
-    exec $(which $1) "\$@"
-  fi
-done
-
 if [ \$(env -u LD_PRELOAD ps -p \$PPID -o comm=) != sccache ]; then
   exec sccache $(which $1) "\$@"
 else
