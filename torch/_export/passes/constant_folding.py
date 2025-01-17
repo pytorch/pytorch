44bf750d735fd7226f569bc50e23efa1d1fcb994
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-01-17 14:27:53 +0000

    Revert "patch for block-wise quantization + pt2e (#144492)"
    This reverts commit 1d43b8150852cdfcbe754edcf027d6e25f33ac63.
    
    Reverted https://github.com/pytorch/pytorch/pull/144492 on behalf of https://github.com/albanD due to Broke a few things in trunk ([comment](https://github.com/pytorch/pytorch/pull/144492#issuecomment-2598485291))

--- torch/_export/passes/constant_folding.py
+++ torch/_export/passes/constant_folding.py
@@ -75,7 +75,6 @@ class ConstantFolder(torch.fx.Interpreter):
             torch.ops.quantized_decomposed.dequantize_per_channel.default,
             torch.ops.quantized_decomposed.dequantize_per_tensor.default,
             torch.ops.quantized_decomposed.dequantize_per_tensor.tensor,
-            torch.ops.quant.dequantize_affine,
         ]:
             # For the pattern fp32_weight -> q -> dq
             # We only folding fp32_weight -> q
