f6ce464988f4f96b8667303faa8c56a0637480a6
Aby Mathew C <amathewc@habana.ai>
2025-01-17 09:09:26 +0200

    Update c10d_object_collectives using DistributedTestBase

--- test/distributed/test_c10d_object_collectives.py
+++ test/distributed/test_c10d_object_collectives.py
@@ -1,6 +1,5 @@
 # Owner(s): ["oncall: distributed"]
 
-import os
 import sys
 from functools import partial, wraps
 
@@ -12,8 +11,15 @@ if not dist.is_available():
     print("Distributed not available, skipping tests", file=sys.stderr)
     sys.exit(0)
 
-from torch.testing._internal.common_distributed import MultiProcessTestCase, TEST_SKIPS
-from torch.testing._internal.common_utils import run_tests, TEST_WITH_DEV_DBG_ASAN
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch.testing._internal.common_distributed import DistributedTestBase, TEST_SKIPS
+from torch.testing._internal.common_utils import (
+    run_tests,
+    skipIfHpu,
+    TEST_CUDA,
+    TEST_HPU,
+    TEST_WITH_DEV_DBG_ASAN,
+)
 
 
 if TEST_WITH_DEV_DBG_ASAN:
@@ -23,70 +29,44 @@ if TEST_WITH_DEV_DBG_ASAN:
     )
     sys.exit(0)
 
-BACKEND = dist.Backend.NCCL if torch.cuda.is_available() else dist.Backend.GLOO
+if TEST_HPU:
+    BACKEND = dist.Backend.HCCL
+    device_count = torch.hpu.device_count()
+    DEVICE = "hpu"
+elif TEST_CUDA:
+    BACKEND = dist.Backend.NCCL
+    device_count = torch.cuda.device_count()
+    DEVICE = "cuda"
+else:
+    BACKEND = dist.Backend.GLOO
+    DEVICE = "cpu"
+
+
+WORLD_SIZE = min(4, max(2, device_count))
 
 
 def with_comms(func=None):
     if func is None:
-        return partial(
-            with_comms,
-        )
+        return partial(with_comms)
 
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if BACKEND == dist.Backend.NCCL and torch.cuda.device_count() < self.world_size:
             sys.exit(TEST_SKIPS[f"multi-gpu-{self.world_size}"].exit_code)
-        self.dist_init()
-        func(self)
-        self.destroy_comms()
+
+        kwargs["device"] = DEVICE
+        self.pg = self.create_pg(device=DEVICE)
+        try:
+            return func(self, *args, **kwargs)
+        finally:
+            torch.distributed.destroy_process_group()
 
     return wrapper
 
 
-class TestObjectCollectives(MultiProcessTestCase):
-    def setUp(self):
-        super().setUp()
-        os.environ["WORLD_SIZE"] = str(self.world_size)
-        os.environ["BACKEND"] = BACKEND
-        self._spawn_processes()
-
-    @property
-    def device(self):
-        return (
-            torch.device("cuda", self.rank % torch.cuda.device_count())
-            if BACKEND == dist.Backend.NCCL
-            else torch.device("cpu")
-        )
-
-    @property
-    def world_size(self):
-        if BACKEND == dist.Backend.NCCL:
-            return torch.cuda.device_count()
-        return super().world_size
-
-    @property
-    def process_group(self):
-        return dist.group.WORLD
-
-    def destroy_comms(self):
-        # Wait for all ranks to reach here before starting shutdown.
-        dist.barrier()
-        dist.destroy_process_group()
-
-    def dist_init(self):
-        dist.init_process_group(
-            backend=BACKEND,
-            world_size=self.world_size,
-            rank=self.rank,
-            init_method=f"file://{self.file_name}",
-        )
-
-        # set device for nccl pg for collectives
-        if BACKEND == "nccl":
-            torch.cuda.set_device(self.rank)
-
+class TestObjectCollectives(DistributedTestBase):
     @with_comms()
-    def test_all_gather_object(self):
+    def test_all_gather_object(self, device):
         output = [None] * dist.get_world_size()
         dist.all_gather_object(object_list=output, obj=self.rank)
 
@@ -94,7 +74,7 @@ class TestObjectCollectives(MultiProcessTestCase):
             self.assertEqual(i, v, f"rank: {self.rank}")
 
     @with_comms()
-    def test_gather_object(self):
+    def test_gather_object(self, device):
         output = [None] * dist.get_world_size() if self.rank == 0 else None
         dist.gather_object(obj=self.rank, object_gather_list=output)
 
@@ -103,21 +83,7 @@ class TestObjectCollectives(MultiProcessTestCase):
                 self.assertEqual(i, v, f"rank: {self.rank}")
 
     @with_comms()
-    def test_send_recv_object_list(self):
-        val = 99 if self.rank == 0 else None
-        object_list = [val] * dist.get_world_size()
-        if self.rank == 0:
-            dist.send_object_list(object_list, 1)
-        if self.rank == 1:
-            dist.recv_object_list(object_list, 0)
-
-        if self.rank < 2:
-            self.assertEqual(99, object_list[0])
-        else:
-            self.assertEqual(None, object_list[0])
-
-    @with_comms()
-    def test_broadcast_object_list(self):
+    def test_broadcast_object_list(self, device):
         val = 99 if self.rank == 0 else None
         object_list = [val] * dist.get_world_size()
         # TODO test with broadcast_object_list's device argument
@@ -126,7 +92,7 @@ class TestObjectCollectives(MultiProcessTestCase):
         self.assertEqual(99, object_list[0])
 
     @with_comms()
-    def test_scatter_object_list(self):
+    def test_scatter_object_list(self, device):
         input_list = list(range(dist.get_world_size())) if self.rank == 0 else None
         output_list = [None]
         dist.scatter_object_list(
@@ -144,30 +110,34 @@ class TestObjectCollectives(MultiProcessTestCase):
         my_pg = dist.new_group(ranks, use_local_synchronization=True)
         return rank, ranks, my_pg
 
+    @skipIfHpu
     @with_comms()
-    def test_subpg_scatter_object(self):
+    def test_subpg_scatter_object(self, device):
         rank, ranks, my_pg = self.setup_sub_pg()
         out_list = [None]
         dist.scatter_object_list(out_list, ranks, src=ranks[0], group=my_pg)
         self.assertEqual(rank, out_list[0])
 
+    @skipIfHpu
     @with_comms()
-    def test_subpg_all_gather_object(self):
+    def test_subpg_all_gather_object(self, device):
         rank, ranks, my_pg = self.setup_sub_pg()
         out_list = [None] * len(ranks)
         dist.all_gather_object(out_list, rank, group=my_pg)
         self.assertEqual(ranks, out_list)
 
+    @skipIfHpu
     @with_comms()
-    def test_subpg_gather_object(self):
+    def test_subpg_gather_object(self, device):
         rank, ranks, my_pg = self.setup_sub_pg()
         out_list = [None] * len(ranks) if rank == ranks[0] else None
         dist.gather_object(rank, out_list, dst=ranks[0], group=my_pg)
         if rank == ranks[0]:
             self.assertEqual(ranks, out_list)
 
+    @skipIfHpu
     @with_comms()
-    def test_subpg_broadcast_object(self):
+    def test_subpg_broadcast_object(self, device):
         rank, ranks, my_pg = self.setup_sub_pg()
         out_list = [None]
         if rank == ranks[0]:
@@ -176,5 +146,7 @@ class TestObjectCollectives(MultiProcessTestCase):
         self.assertEqual(ranks[0], out_list[0])
 
 
+devices = ["cpu", "cuda", "hpu"]
+instantiate_device_type_tests(TestObjectCollectives, globals(), only_for=devices)
 if __name__ == "__main__":
     run_tests()
