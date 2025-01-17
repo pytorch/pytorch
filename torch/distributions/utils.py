6378795cdfa66c03a5d847410f26b9e8a403ffe5
Randolf Scholz <randolf.scholz@gmail.com>
2025-01-17 18:17:18 +0100

    fixed docstrings

--- torch/distributions/utils.py
+++ torch/distributions/utils.py
@@ -28,20 +28,20 @@ def broadcast_all(*values):
     Given a list of values (possibly containing numbers), returns a list where each
     value is broadcasted based on the following rules:
       - `torch.*Tensor` instances are broadcasted as per :ref:`_broadcasting-semantics`.
-      - _Number instances (scalars) are upcast to tensors having
+      - Number instances (scalars) are upcast to tensors having
         the same size and type as the first tensor passed to `values`.  If all the
         values are scalars, then they are upcasted to scalar Tensors.
 
     Args:
-        values (list of `_Number`, `torch.*Tensor` or objects implementing __torch_function__)
+        values (list of `Number`, `torch.*Tensor` or objects implementing __torch_function__)
 
     Raises:
-        ValueError: if any of the values is not a `_Number` instance,
+        ValueError: if any of the values is not a `Number` instance,
             a `torch.*Tensor` instance, or an instance implementing __torch_function__
     """
     if not all(is_tensor_like(v) or isinstance(v, _Number) for v in values):
         raise ValueError(
-            "Input arguments must all be instances of _Number, "
+            "Input arguments must all be instances of Number, "
             "torch.Tensor or objects implementing __torch_function__."
         )
     if not all(is_tensor_like(v) for v in values):
