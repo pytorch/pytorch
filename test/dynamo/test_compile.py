28efa10fbbaf3c0873b06050aafce74f91d3d476
clr <clr@fb.com>
2025-01-17 16:52:00 -0700

    Update on "inductor: Don't throw an internal error when a nn.module is missing a attribute"
    If a nn.module getattr call throws, we should make sure that we don't crash with an internal error
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- test/dynamo/test_compile.py
+++ test/dynamo/test_compile.py
@@ -193,13 +193,6 @@ class InPlaceCompilationTests(TestCase):
         class testModel(torch.nn.Module):
             def __init__(self):
                     super().__init__()
-                    self.custom_dict = {"queue": [torch.rand((2, 2)) for _ in range(3)]}
-                    self.other_attr = torch.rand((2, 2))
-            def __getattr__(self, name):
-                    custom_dict = self.custom_dict
-                    if name in custom_dict:
-                            return custom_dict[name]
-                    return super().__getattr__(name)
 
             def forward(self, x):
                     return self.does_not_exist(x)
