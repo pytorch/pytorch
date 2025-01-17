8b010f06b89cc61a923eedf44667bb78b562efdc
Aaron Orenstein <aorenste@meta.com>
2025-01-17 12:04:08 -0800

    PEP585 update - torch/_C torch/_decomp torch/_lazy torch/_library torch/_numpy torch/_prims torch/_refs torch/_strobelight See #145101 for details.
    [ghstack-poisoned]

--- torch/_library/triton.py
+++ torch/_library/triton.py
@@ -1,6 +1,7 @@
 import contextlib
 import threading
-from typing import Any, Callable, Generator, Iterable, Optional, Union
+from collections.abc import Generator, Iterable
+from typing import Any, Callable, Optional, Union
 
 from torch.utils._exposed_in import exposed_in
 
