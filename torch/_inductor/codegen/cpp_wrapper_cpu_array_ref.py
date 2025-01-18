0b151f260f21e002659965887abdf332382c3118
Bin Bao <binbao@meta.com>
2025-01-18 01:44:19 +0000

    [AOTI] Add an option to skip optimizing generated wrapper code (#144866)
    Summary: In some cases, generated wrapper code faces a long cpp compilation time. As an alleviation, this PR adds an option to skip cpp compiler optimizers for the generated main wrapper function body.
    
    D68174038
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144866
    Approved by: https://github.com/chenyang78, https://github.com/hl475

--- torch/_inductor/codegen/cpp_wrapper_cpu_array_ref.py
+++ torch/_inductor/codegen/cpp_wrapper_cpu_array_ref.py
@@ -209,7 +209,17 @@ class CppWrapperCpuArrayRef(CppWrapperCpu):
                         """
                     )
 
-                run_impl_proto = """
+                run_impl_proto = ""
+                if config.aot_inductor.compile_wrapper_with_O0:
+                    run_impl_proto += """
+                    #ifdef __clang__
+                    __attribute__((optnone))
+                    #else
+                    __attribute__((optimize("O0")))
+                    #endif
+                    """
+
+                run_impl_proto += """
                     void AOTInductorModel::run_impl(
                         AtenTensorHandle*
                             input_handles, // array of input AtenTensorHandle; handles
@@ -222,11 +232,13 @@ class CppWrapperCpuArrayRef(CppWrapperCpu):
                         AOTIProxyExecutorHandle proxy_executor
                     ) {
                     """
+
                 if config.aot_inductor.debug_compile:
                     self.generate_input_output_runtime_checks()
                     run_impl_proto += """
                         __check_inputs_outputs(input_handles, output_handles);
                     """
+
                 if config.aot_inductor.use_minimal_arrayref_interface:
                     self.prefix.splice(
                         """
