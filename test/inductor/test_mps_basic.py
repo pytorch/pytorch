acadd4d0d511827729f2efe7c4f9687849ccac3f
Davide Italiano <davidino@meta.com>
2025-01-19 02:29:03 +0000

    [mps/inductor] Introduce a metal approx for erf() and use it. (#145161)
    Probably we can do better, but this is a start.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/145161
    Approved by: https://github.com/malfet

--- test/inductor/test_mps_basic.py
+++ test/inductor/test_mps_basic.py
@@ -131,6 +131,9 @@ class MPSBasicTests(TestCase):
     def test_pointwise_i1(self):
         self.common(torch.special.i1, (torch.rand(128, 128),), check_lowp=False)
 
+    def test_pointwise_erf(self):
+        self.common(torch.special.erf, (torch.rand(128, 128),), check_lowp=False)
+
     def test_broadcast(self):
         self.common(torch.add, (torch.rand(32, 1024), torch.rand(1024)))
 
