afde707639af8b67c7aef9a1da4defd54271be74
Aaron Gokaslan <aaronGokaslan@gmail.com>
2025-01-21 10:53:49 -0500

    Apply reviewer suggestion
    Co-authored-by: Xuehai Pan <XuehaiPan@outlook.com>

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -851,9 +851,12 @@ def _get_non_persistent_buffers(mod: torch.nn.Module) -> set[str]:
     """
     result: Set[str] = set()
     for name, m in mod.named_modules(remove_duplicate=False):
-        result.update(
-            f"{name}.{b}" if name else b for b in m._non_persistent_buffers_set
-        )
+        if name:
+            result.update(
+                f"{name}.{b}" for b in m._non_persistent_buffers_set
+            )
+        else:
+            result.update(m._non_persistent_buffers_set)
     return result
 
 
