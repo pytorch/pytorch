73b04e162b29bd23ec0f8fc97f4762331e75f183
Jane Xu <janeyx@meta.com>
2025-01-21 10:23:38 -0800

    Update on "Use TORCH_CHECK instead of std::runtime_error in stack.h and ivalue.h"
    TORCH_CHECK will preserve the stacktrace for when TORCH_CPP_SHOW_STACKTRACES=1, whereas std::runtime_error will not.
    
    
    
    
    [ghstack-poisoned]

--- aten/src/ATen/core/stack.h
+++ aten/src/ATen/core/stack.h
@@ -103,7 +103,7 @@ inline void drop(Stack* stack, size_t n) {
   drop(*stack, n);
 }
 inline IValue pop(Stack& stack) {
-  TORCH_CHECK(not stack.empty(), "pop() called on empty stack");
+  TORCH_CHECK(!stack.empty(), "pop() called on empty stack");
   auto r = std::move(stack.back());
   stack.pop_back();
   return r;
