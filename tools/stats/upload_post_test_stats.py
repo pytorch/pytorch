29242b8e9d1d9d504a95fa834cdb38ef56247beb
Yang Wang <elainewy@meta.com>
2025-01-21 16:56:19 -0800

    test

--- tools/stats/upload_post_test_stats.py
+++ tools/stats/upload_post_test_stats.py
@@ -5,6 +5,11 @@ import argparse
 import json
 import os
 import sys
+
+
+# adding sys.path makes the monitor script able to import path tools.stats.utilization_stats_lib
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+
 import xml.etree.ElementTree as ET
 from multiprocessing import cpu_count, Pool
 from pathlib import Path
@@ -25,17 +30,8 @@ from tools.stats.upload_stats_lib import (
 
 USAGE_LOG_FILENAME = "usage_log.txt"
 
-def unzip_file(path: Path,file_name: str):
-    try:
-        with zipfile.ZipFile(path) as zip_file:
-            # Read the desired file from the zip archive
-            return zip_file.read(name=file_name).decode()
-    except Exception as e:
-        print(f"::warning trying to download test log {object} failed by: {e}")
-        return ""
 
-
-def get_records(workflow_run_id: int,job_id: int, workflow_run_attempt: int):
+def processTestLog(workflow_run_id: int,job_id: int, workflow_run_attempt: int):
     artifact_paths = download_s3_artifacts("logs-test",workflow_run_id,workflow_run_attempt,job_id)
     if len(artifact_paths) == 0:
         print(f"Failed to download artifacts for workflow {workflow_run_id} and job {job_id}")
@@ -50,6 +46,17 @@ def get_records(workflow_run_id: int,job_id: int, workflow_run_attempt: int):
     if metadata == None:
         return
     print(f"metadata: {metadata}")
+    for record in records:
+        print(record.to_json())
+
+def unzip_file(path: Path,file_name: str):
+    try:
+        with zipfile.ZipFile(path) as zip_file:
+            # Read the desired file from the zip archive
+            return zip_file.read(name=file_name).decode()
+    except Exception as e:
+        print(f"::warning trying to download test log {object} failed by: {e}")
+        return ""
 
 def process_line(line: str):
     try:
@@ -144,12 +151,13 @@ def prepareDbBatchInsertion(metadata: UtilizationMetadata, records: list[Utiliza
     pass
 
 if __name__ == "__main__":
-    args = parse_args()
-    print(f"Workflow id is: {args.workflow_run_id}")
-    workflow_run_id =
-    job_id = 35805057544
-    workflow_name = "inductor"
+    #args = parse_args()
+    #print(f"Workflow id is: {args.workflow_run_id}")
+    workflow_run_id = 12838344646
+    job_id = 35805158112
+    workflow_name = "inductor-unittest"
     workflow_run_attempt = 1
-    get_records(args.workflow_run_id,args.job_id,args.workflow_run_attempt)
+    job_name = "cuda12.4-py3.12-gcc9-sm86 / test (inductor, 2, 2, linux.g5.4xlarge.nvidia.gpu)"
+    processTestLog(workflow_run_id,job_id,workflow_run_attempt)
     # Flush stdout so that any errors in the upload show up last in the logs.
     sys.stdout.flush()
