6e53588789c48682c7da969de9cbace67a1dd9f3
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-01-22 22:14:26 +0000

    Revert "[BE]: Simplify set add with set update (#145152)"
    This reverts commit 0cb9b2284a31fa497d684dbc2f56398c1d1e3114.
    
    Reverted https://github.com/pytorch/pytorch/pull/145152 on behalf of https://github.com/davidberard98 due to land race with https://github.com/pytorch/pytorch/pull/145165 broke lint ([comment](https://github.com/pytorch/pytorch/pull/145152#issuecomment-2608378172))

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -849,12 +849,10 @@ def _get_non_persistent_buffers(mod: torch.nn.Module) -> set[str]:
     """
     Returns set of non-persistent buffers in a module and its submodules.
     """
-    result: Set[str] = set()
+    result = set()
     for name, m in mod.named_modules(remove_duplicate=False):
-        if name:
-            result.update(f"{name}.{b}" for b in m._non_persistent_buffers_set)
-        else:
-            result.update(m._non_persistent_buffers_set)
+        for b in m._non_persistent_buffers_set:
+            result.add(f"{name}.{b}" if name else b)
     return result
 
 
