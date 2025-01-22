4825ca8dbf50fdaa90ad17300b7cddf0e0924ca2
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2025-01-22 10:09:09 +0000

    Fix UT

--- test/test_torch.py
+++ test/test_torch.py
@@ -8997,13 +8997,13 @@ tensor([[[1.+1.j, 1.+1.j, 1.+1.j,  ..., 1.+1.j, 1.+1.j, 1.+1.j],
             )
             
     # For testing in64 support in upsample_nearest3d
-    @onlyCUDA
     def test_int64_upsample3d(self):
-        x = torch.ones((1, 256, 16, 720, 1280), dtype=torch.bfloat16).cuda()
-        try:
-            torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
-        except Exception as e:
-            self.fail(f"Unexpected exception raised: {e}")
+        for device in get_all_device_types():
+            x = torch.ones((1, 256, 16, 720, 1280), dtype=torch.bfloat16, device=device)
+            try:
+                torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
+            except Exception as e:
+                self.fail(f"Unexpected exception raised: {e}")
 
     def test_add_meta_scalar(self):
         # From https://github.com/pytorch/pytorch/issues/53815
