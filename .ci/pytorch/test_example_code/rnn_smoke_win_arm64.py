fb7e4ea66938d613f1e8e64ddb94c482633aff17
Ozan Aydin <148207261+ozanMSFT@users.noreply.github.com>
2025-01-22 15:56:56 +0100

    - lint patches

--- .ci/pytorch/test_example_code/rnn_smoke_win_arm64.py
+++ .ci/pytorch/test_example_code/rnn_smoke_win_arm64.py
@@ -6,6 +6,7 @@ For example, it would throw exception if missing some components are missing
 import torch
 import torch.nn as nn
 
+
 rnn = nn.RNN(10, 20, 2)
 inputs = torch.randn(5, 3, 10)
 h0 = torch.randn(2, 3, 20)
