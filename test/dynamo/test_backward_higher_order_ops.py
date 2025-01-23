8b174a371da8a45722cca0faa6ec4ef1f904c602
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-01-23 23:34:13 +0000

    Revert "[compiled autograd] Proxy opaque nodes for built-in autograd nodes (#143296)"
    This reverts commit 5531fafffefc45cd894040b2b07b0d5227430082.
    
    Reverted https://github.com/pytorch/pytorch/pull/143296 on behalf of https://github.com/izaitsevfb due to breaking internal tests T213390054 ([comment](https://github.com/pytorch/pytorch/pull/143296#issuecomment-2611224926))

--- test/dynamo/test_backward_higher_order_ops.py
+++ test/dynamo/test_backward_higher_order_ops.py
@@ -121,27 +121,23 @@ class _multiply_invoke(torch.nn.Module):
                 out.backward(grad_out)
             actual = normalize_gm(graph.print_readable(False))
             self.assertEqual(x.grad, grad_out * grad_out)
-            if backend in ["aot_eager", "inductor"]:
-                self.assertExpectedInline(
-                    actual,
-                    """\
+            self.assertExpectedInline(
+                actual,
+                """\
 class GraphModule(torch.nn.Module):
     def forward(self, L_inputs_ : list):
         l_inputs_ = L_inputs_
 
-        getitem: "f32[2]" = l_inputs_[0];  l_inputs_ = None
+        getitem: "f32[s0]" = l_inputs_[0];  l_inputs_ = None
 
-        validate_outputs = torch__dynamo_compiled_autograd_ops_validate_outputs([getitem], [((None, None, device(type='cpu'), 6, 0, None), [2], False)]);  getitem = None
-        getitem_3: "f32[2]" = validate_outputs[0];  validate_outputs = None
+        new_grad: "f32[s0]" = torch.clone(getitem)
 
-        new_grad: "f32[2]" = torch.clone(getitem_3)
+        result: "f32[s0]" = getitem * getitem;  getitem = None
 
-        result: "f32[2]" = getitem_3 * getitem_3;  getitem_3 = None
-
-        new_grad_1: "f32[2]" = torch.clone(result);  result = None
+        new_grad_1: "f32[s0]" = torch.clone(result);  result = None
         return (new_grad, new_grad_1)
 """,
-                )
+            )
 
             graph = None
 
@@ -191,30 +187,26 @@ class GraphModule(torch.nn.Module):
             actual = normalize_gm(graph.print_readable(False))
             self.assertEqual(obj.counter, 1)
             self.assertEqual(x.grad, grad_out + grad_out)
-            if backend in ["aot_eager", "inductor"]:
-                self.assertExpectedInline(
-                    actual,
-                    """\
+            self.assertExpectedInline(
+                actual,
+                """\
 class GraphModule(torch.nn.Module):
     def forward(self, L_inputs_ : list, L_hooks_0_keywords_fn_keywords_obj_counter: "Sym(s1)"):
         l_inputs_ = L_inputs_
         l_hooks_0_keywords_fn_keywords_obj_counter = L_hooks_0_keywords_fn_keywords_obj_counter
 
-        getitem: "f32[2]" = l_inputs_[0];  l_inputs_ = None
+        getitem: "f32[s0]" = l_inputs_[0];  l_inputs_ = None
 
-        validate_outputs = torch__dynamo_compiled_autograd_ops_validate_outputs([getitem], [((None, None, device(type='cpu'), 6, 0, None), [2], False)]);  getitem = None
-        getitem_3: "f32[2]" = validate_outputs[0];  validate_outputs = None
-
-        new_grad: "f32[2]" = torch.clone(getitem_3)
+        new_grad: "f32[s0]" = torch.clone(getitem)
 
         add: "Sym(s1 + 1)" = l_hooks_0_keywords_fn_keywords_obj_counter + 1;  l_hooks_0_keywords_fn_keywords_obj_counter = None
 
-        result: "f32[2]" = getitem_3 * getitem_3;  getitem_3 = None
+        result: "f32[s0]" = getitem * getitem;  getitem = None
 
-        new_grad_1: "f32[2]" = torch.clone(result);  result = None
+        new_grad_1: "f32[s0]" = torch.clone(result);  result = None
         return (new_grad, new_grad_1, add)
 """,
-                )
+            )
 
             out = fn(x, y)
             out.backward(grad_out)
