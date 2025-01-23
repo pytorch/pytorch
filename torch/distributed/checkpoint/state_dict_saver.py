8be1f5727ae3070bdc51d45211b001964510bb3c
Daulet Askarov <dauleta@meta.com>
2025-01-23 12:49:26 -0800

    Fix staging for CPU tensors in OSS DCP async_save (#145408)
    Fix staging for CPU tensors in OSS DCP async_save (#145408)
    
    Summary:
    
    As found in
    https://github.com/pytorch/pytorch/issues/144657
    for CPU tensors we accidentally skip copying during staging due to using offload to cpu helper, which does a no-op for CPU tensors. This means that if the trainer changes the original source CPU tensor value after launch async save but before the actual writing/uploading to the destination commences, the writing/uploading logic will accidentally pick up the latest state of the tensor, while it should have dealt with its own dedicated copy saved earlier. Dropping _offload_state_dict_to_cpu in favor of _copy_state_dict fixes this bug.
    
    Test Plan:
    Running the user script from the linked GitHub issue verifies the fix:
    ```
    import os
    
    import torch
    
    import torch.distributed as dist
    import torch.distributed.checkpoint as dcp
    from torch.distributed.checkpoint.state_dict import get_model_state_dict
    import torch.nn as nn
    
    
    class Net(nn.Module):
        def __init__(self):
            super().__init__()
            self.weight = nn.Parameter(torch.ones(1, 1))
    
        def forward(self, x):
            return self.layer(x)
    
    os.environ["MASTER_ADDR"] = "localhost"
    os.environ["MASTER_PORT"] = "12345"
    os.environ["WORLD_SIZE"] = "1"
    os.environ["RANK"] = "0"
    
    dist.init_process_group()
    
    model = Net()
    state_dict = get_model_state_dict(model)
    pg = dist.new_group(backend="gloo")
    
    try:
        steps = [10, 20, 30, 40, 50]
        future = None
        for step in steps:
            # simulate a training step, e.g. optimizer updating values
            with torch.no_grad():
                model.weight.data.fill_(step)
    
            if future is not None:
                future.result()
                future = None
            future = dcp.async_save(
                state_dict,
                checkpoint_id=f"outputs/{step}",
                process_group=pg,
            )
    
        future.result()
    
        for step in steps:
            dcp.load(
                state_dict,
                checkpoint_id=f"outputs/{step}",
                process_group=pg,
            )
            assert state_dict["weight"][0, 0] == step, f"got {state_dict['weight'][0, 0]=} on {step=}"
    finally:
        dist.destroy_process_group(pg)
        dist.destroy_process_group()
    ```
    passes all asserts with this fix. If the script is run in trunk, confirmed that it fails the first assert.
    
    Differential Revision: D68518689

--- torch/distributed/checkpoint/state_dict_saver.py
+++ torch/distributed/checkpoint/state_dict_saver.py
@@ -9,7 +9,7 @@ from typing_extensions import deprecated
 
 import torch
 import torch.distributed as dist
-from torch.distributed._state_dict_utils import _offload_state_dict_to_cpu
+from torch.distributed._state_dict_utils import _copy_state_dict, _create_cpu_state_dict
 from torch.distributed.checkpoint._storage_utils import _storage_setup
 from torch.distributed.checkpoint.default_planner import DefaultSavePlanner
 from torch.distributed.checkpoint.logger import _dcp_method_logger
@@ -228,7 +228,8 @@ def async_save(
     if isinstance(storage_writer, AsyncStager):
         staged_state_dict = storage_writer.stage(state_dict)
     else:  # provides bwc for storage_writers not implementing AsyncStager
-        staged_state_dict = _offload_state_dict_to_cpu(state_dict, type_check=False)
+        staged_state_dict = _create_cpu_state_dict(state_dict)
+        _copy_state_dict(state_dict, staged_state_dict, type_check=False)
 
     executor = ThreadPoolExecutor(max_workers=1)
     f: Future = executor.submit(
