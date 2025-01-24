54e2f4b2013decbf764d0d6b22c561509cadc42c
zeshengzong <zesheng.zong@outlook.com>
2025-01-24 01:18:18 +0000

    Fix lerp weight type promotion (#141117)
    Fixes #140601
    
    Enable `promote_inputs_to_common_dtype` when tensors not same dtype when invoke `lerp` function.
    
    For `lerp_Tensor`
    - Check whether same `dtype` of tensors, enable promote if not
    - Remove type check assert
    
    For `lerp_Scalar`
    - Seems already enable `promote_inputs_to_common_dtype` by default, just remove the type check. Make sure promote behavior consistent with `lerp_Tensor`
    
    `lerp_Scalar` get TensorIteratorConfig from here
    https://github.com/pytorch/pytorch/blob/c37185c76ae4068899869e48a8388e78437508e8/aten/src/ATen/TensorIterator.cpp#L979-L985
    
    **Test Result**
    Test case in issue passed
    
    ```python
    >>> import torch
    >>>
    >>> x = torch.ones(2, 2, dtype=torch.float64)
    >>> w = torch.ones(2, 2, dtype=torch.float64)
    >>> s = torch.tensor(2.2)
    >>> x.lerp_(w, s)
    tensor([[1., 1.],
            [1., 1.]], dtype=torch.float64)
    
    >>> x = torch.ones(2, 2, dtype=torch.float16)
    >>> w = torch.ones(2, 2, dtype=torch.float16)
    >>> s = torch.tensor(2.2)
    >>> x.lerp_(w, s)
    tensor([[1., 1.],
            [1., 1.]], dtype=torch.float16)
    
    ```
    
    ```bash
    $ pytest test/test_binary_ufuncs.py -k 'test_lerp_tensor_type_promotion or test_lerp_scalar_type_promotion'
    ```
    ![image](https://github.com/user-attachments/assets/288a5294-a9ee-47f3-bbf7-d4ff986f3ba8)
    
    ```bash
    $ lintrunner
    ```
    ![image](https://github.com/user-attachments/assets/d469836f-5c49-4d89-a2fd-379cad4db3af)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141117
    Approved by: https://github.com/janeyx99
    
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -6972,10 +6972,11 @@ def lerp(start, end, weight):
     )
     args = [start, end]
     if isinstance(weight, TensorLike):
-        torch._check(
-            start.dtype == weight.dtype,
-            lambda: f"expected dtype {start.dtype} for `weight`, but got dtype {weight.dtype}",
-        )
+        if weight.ndim != 0:
+            torch._check(
+                start.dtype == weight.dtype,
+                lambda: f"expected dtype {start.dtype} for `weight`, but got dtype {weight.dtype}",
+            )
         args.append(weight)
     return elementwise_meta(
         *args, type_promotion=ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT
