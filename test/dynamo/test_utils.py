4b18a47fd625071c64e701e9525cfe254a1e52f3
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-01-24 00:11:49 +0000

    Revert "[dynamo] Log guard latency (#145132)"
    This reverts commit 0a310d738819ae000f49b32298305724117634c2.
    
    Reverted https://github.com/pytorch/pytorch/pull/145132 on behalf of https://github.com/anijain2305 due to CI failures observed after PR was merged ([comment](https://github.com/pytorch/pytorch/pull/145132#issuecomment-2611268421))

--- test/dynamo/test_utils.py
+++ test/dynamo/test_utils.py
@@ -212,8 +212,6 @@ class TestDynamoTimed(TestCase):
         # much easier.
         raw = dataclasses.asdict(compilation_events[0])
         del raw["feature_usage"]
-        # guard_latency_us is not deterministic
-        del raw["guard_latency_us"]
         self.assertExpectedInline(
             pprint.pformat(raw),
             """\
@@ -294,7 +292,6 @@ class TestDynamoTimed(TestCase):
         # Second event is for the backward
         raw = dataclasses.asdict(compilation_events[1])
         del raw["feature_usage"]
-        del raw["guard_latency_us"]
         self.assertExpectedInline(
             pprint.pformat(raw),
             """\
