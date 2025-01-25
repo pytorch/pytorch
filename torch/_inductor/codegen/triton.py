18e2fdeeadb39ba2d20decf3c514fc8ffdbbdd16
Jason Ansel <jansel@meta.com>
2025-01-25 15:37:18 -0800

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1510,6 +1510,9 @@ class FixedTritonConfig:
     def __getitem__(self, item):
         return self.config[item]
 
+    def __contains__(self, item):
+        return item in self.config
+
 
 class TritonCSE(CSE):
     """
@@ -1931,9 +1934,10 @@ class TritonKernel(SIMDKernel):
         if isinstance(index, sympy.Integer):
             expand_str = f"{copy_shape}.shape" if copy_shape else self.dense_size_str()
             index_str = f"tl.full({expand_str}, {index_str}, tl.int32)"
-            mask_vars = OrderedSet()
+            mask_vars = dense_mask_vars
             if self._load_mask:
                 mask_vars.add(self._load_mask)
+            self.filter_masks(mask_vars)
             mask_str = " & ".join(sorted(map(str, mask_vars))) if mask_vars else "None"
             return IndexingOptions(
                 index_str, mask_vars, mask_str, expand_str, has_rindex, index
@@ -2724,7 +2728,7 @@ class TritonKernel(SIMDKernel):
         value independently.
         """
         xnumel = self.numels["x"]
-        mask = "xindex < xnumel" if xnumel != 1 and not self.no_x_dim else None
+        mask = "xindex < xnumel" if not self._has_constant_xmask() else None
         expand = "" if self.no_x_dim else "[None,:]"
 
         nbytes = xnumel * dtype.itemsize * self.max_rsplit()
@@ -3668,8 +3672,13 @@ class TritonKernel(SIMDKernel):
     def _has_constant_mask(self, tree: IterationRangesRoot):
         if not self.optimize_mask:
             return False
-        if V.graph.sizevars.statically_known_equals(tree.numel, 1):  # type: ignore[arg-type]
-            return True
+
+        if self.fixed_config and f"{tree.prefix.upper()}BLOCK" in self.fixed_config:
+            if self.fixed_config[f"{tree.prefix.upper()}BLOCK"] == 1:
+                return True
+        else:
+            if V.graph.sizevars.statically_known_equals(tree.numel, 1):
+                return True
 
         # Masks are superfluous if numel is a multiple of BLOCK
         # (We use the fact that BLOCK is required by triton to be a power of 2)
@@ -3701,6 +3710,11 @@ class TritonKernel(SIMDKernel):
             )
 
         return False
+
+    def _has_constant_xmask(self):
+        xtree = self.range_trees[0]
+        assert xtree.prefix == "x"
+        return self._has_constant_mask(xtree)
 
     def filter_masks(self, mask_vars):
         for tree in self.range_trees:
