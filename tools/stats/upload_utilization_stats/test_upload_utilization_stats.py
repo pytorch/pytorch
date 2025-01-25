668849f785c16b131e5cc58e70023b1e5babf1a5
Yang Wang <elainewy@meta.com>
2025-01-24 16:55:07 -0800

    test

--- tools/stats/upload_utilization_stats/test_upload_utilization_stats.py
+++ tools/stats/upload_utilization_stats/test_upload_utilization_stats.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import Counter
 import os
 import sys
@@ -13,9 +13,25 @@ from tools.stats.upload_utilization_stats.upload_utilization_stats import (
     SegmentGenerator,
 )
 
-# timestamp January 1, 2022 12:00:00
-TEST_TIME_STAMP_1 = datetime(2022, 1, 1, 12, 0, 0).timestamp()
+# datetimes from January 1, 2022 12:00:00
+TEST_DT_BASE = datetime(2022, 1, 1, 12, 0, 0)
+TEST_DT_PLUS_5S = TEST_DT_BASE + timedelta(seconds=5)
+TEST_DT_PLUS_10S = TEST_DT_BASE + timedelta(seconds=10)
+TEST_DT_PLUS_15S = TEST_DT_BASE + timedelta(seconds=15)
+TEST_DT_PLUS_30S = TEST_DT_BASE + timedelta(seconds=30)
+TEST_DT_PLUS_40S = TEST_DT_BASE + timedelta(seconds=40)
+
+# timestamps from January 1, 2022 12:00:00
+TEST_TS_BASE = TEST_DT_BASE.timestamp()
+TEST_TS_PLUS_5S = TEST_DT_PLUS_5S.timestamp()
+TEST_TS_PLUS_10S = TEST_DT_PLUS_10S.timestamp()
+TEST_TS_PLUS_15S = TEST_DT_PLUS_15S.timestamp()
+TEST_TS_PLUS_30S = TEST_DT_PLUS_30S.timestamp()
+TEST_TS_PLUS_40S = TEST_DT_PLUS_40S.timestamp()
+
+# test cmd names
 PYTEST1_NAME = "python test1.py"
+PYTEST2_NAME = "python test2.py"
 PYPIP_INSTALL_NAME = "python pip install install1"
 
 class TestSegmentGenerator(unittest.TestCase):
@@ -31,7 +47,7 @@ class TestSegmentGenerator(unittest.TestCase):
 
     def test_generate_single_record(self):
         record = UtilizationRecord(
-            timestamp=TEST_TIME_STAMP_1, cmd_names=[PYTEST1_NAME], level="PYTHON_CMD"
+            timestamp=TEST_TS_BASE, cmd_names=[PYTEST1_NAME], level="PYTHON_CMD"
         )
         records = [record]
 
@@ -44,7 +60,7 @@ class TestSegmentGenerator(unittest.TestCase):
 
     def test_generate_single_record_with_multiple_cmds(self):
         record = UtilizationRecord(
-            timestamp=TEST_TIME_STAMP_1,
+            timestamp=TEST_TS_BASE,
             cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
             level="PYTHON_CMD",
         )
@@ -57,31 +73,8 @@ class TestSegmentGenerator(unittest.TestCase):
         # assert
         self.assertEqual(len(segments), 2)
 
-    def test_generate_multiple_records_overlaping(self):
-        record1 = UtilizationRecord(
-            timestamp=TEST_TIME_STAMP_1, cmd_names=[PYTEST1_NAME], level="PYTHON_CMD"
-        )
-        record2 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 0, 30).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        record3 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 1, 15).timestamp(),
-            cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
-            level="PYTHON_CMD",
-        )
-        record4 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 1, 31).timestamp(),
-            cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
-            level="PYTHON_CMD",
-        )
-        record5 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 1, 42).timestamp(),
-            cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
-            level="PYTHON_CMD",
-        )
-        records = [record1, record2, record3, record4, record5]
+    def test_generate_multiple_records(self):
+        records = get_base_test_records()
 
         # execute
         generator = SegmentGenerator()
@@ -89,35 +82,28 @@ class TestSegmentGenerator(unittest.TestCase):
 
         # assert
         self.assertEqual(len(segments), 2)
+        self.validate_segment(segments[0], PYTEST1_NAME, TEST_TS_BASE, TEST_TS_PLUS_30S)
+        self.validate_segment(
+            segments[1], PYPIP_INSTALL_NAME, TEST_TS_PLUS_10S, TEST_TS_PLUS_15S
+        )
 
-    def test_generate_multiple_segments_larger_than_default_threshold_setting(self):
-        record1 = UtilizationRecord(
-            timestamp=TEST_TIME_STAMP_1,
-            cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
-            level="PYTHON_CMD",
-        )
-        record2 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 0, 30).timestamp(),
+    def test_generate_cmd_interval_larger_than_default_threshold_setting(self):
+        records = get_base_test_records()
+
+        # record has more than 1 minute gap than last default record 
+        test_gap_dt1 = TEST_DT_PLUS_30S + timedelta(seconds=80)
+        test_gap_dt2 = TEST_DT_PLUS_30S + timedelta(seconds=85)
+        record_gap_1 = UtilizationRecord(
+            timestamp=test_gap_dt1.timestamp(),
             cmd_names=[PYTEST1_NAME],
             level="PYTHON_CMD",
         )
-        record3 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 1, 15).timestamp(),
+        record_gap_2 = UtilizationRecord(
+            timestamp=test_gap_dt2.timestamp(),
             cmd_names=[PYTEST1_NAME],
             level="PYTHON_CMD",
         )
-        # record has more than 1 minute gap
-        record4 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 5, 15).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        record5 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 5, 18).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        records = [record1, record2, record3, record4, record5]
+        records += [record_gap_1, record_gap_2]
 
         # execute
         generator = SegmentGenerator()
@@ -129,37 +115,22 @@ class TestSegmentGenerator(unittest.TestCase):
         self.assertEqual(counter[PYPIP_INSTALL_NAME], 1)
         self.assertEqual(len(segments), 3)
 
+        self.validate_segment(segments[0], PYTEST1_NAME, TEST_TS_BASE, TEST_TS_PLUS_30S)
+        self.validate_segment(
+            segments[1],
+            PYTEST1_NAME,
+            test_gap_dt1.timestamp(),
+            test_gap_dt2.timestamp(),
+        )
+        self.validate_segment(
+            segments[2], PYPIP_INSTALL_NAME, TEST_TS_PLUS_10S, TEST_TS_PLUS_15S
+        )
+
     def test_generate_multiple_segments_with_customized_threshold(self):
         # set threshold to consider as continuous segment to 10 seconds
         test_threshold = 10
 
-        record1 = UtilizationRecord(
-            timestamp=TEST_TIME_STAMP_1,
-            cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
-            level="PYTHON_CMD",
-        )
-        record2 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 0, 20).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        record3 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 1, 15).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        # record has more than 1 minute gap
-        record4 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 5, 15).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        record5 = UtilizationRecord(
-            timestamp=datetime(2022, 1, 1, 12, 5, 18).timestamp(),
-            cmd_names=[PYTEST1_NAME],
-            level="PYTHON_CMD",
-        )
-        records = [record1, record2, record3, record4, record5]
+        records = get_base_test_records()
 
         # execute
         generator = SegmentGenerator()
@@ -167,9 +138,54 @@ class TestSegmentGenerator(unittest.TestCase):
 
         # assert
         counter = Counter(seg.name for seg in segments)
-        self.assertEqual(counter[PYTEST1_NAME], 4)
+        self.assertEqual(counter[PYTEST1_NAME], 2)
         self.assertEqual(counter[PYPIP_INSTALL_NAME], 1)
-        self.assertEqual(len(segments), 5)
+        self.assertEqual(len(segments), 3)
+
+        self.validate_segment(segments[0], PYTEST1_NAME, TEST_TS_BASE, TEST_TS_PLUS_15S)
+        self.validate_segment(
+            segments[1], PYTEST1_NAME, TEST_TS_PLUS_30S, TEST_TS_PLUS_30S
+        )
+        self.validate_segment(
+            segments[2], PYPIP_INSTALL_NAME, TEST_TS_PLUS_10S, TEST_TS_PLUS_15S
+        )
+
+    def validate_segment(self, segment, name, start_at, end_at):
+        self.assertEqual(segment.name, name)
+        self.assertEqual(segment.start_at, start_at)
+        self.assertEqual(segment.end_at, end_at)
+
+
+def get_base_test_records():
+    record1 = UtilizationRecord(
+        timestamp=TEST_TS_BASE, cmd_names=[PYTEST1_NAME], level="PYTHON_CMD"
+    )
+    record2 = UtilizationRecord(
+        timestamp=TEST_TS_PLUS_5S,
+        cmd_names=[PYTEST1_NAME],
+        level="PYTHON_CMD",
+    )
+    record3 = UtilizationRecord(
+        timestamp=TEST_TS_PLUS_10S,
+        cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
+        level="PYTHON_CMD",
+    )
+    record4 = UtilizationRecord(
+        timestamp=TEST_TS_PLUS_15S,
+        cmd_names=[PYTEST1_NAME, PYPIP_INSTALL_NAME],
+        level="PYTHON_CMD",
+    )
+    record5 = UtilizationRecord(
+        timestamp=TEST_TS_PLUS_30S,
+        cmd_names=[PYTEST1_NAME],
+        level="PYTHON_CMD",
+    )
+    record6 = UtilizationRecord(
+        timestamp=TEST_TS_PLUS_40S,
+        cmd_names=[],
+        level="PYTHON_CMD",
+    )
+    return [record1, record2, record3, record4, record5, record6]
 
 
 if __name__ == "__main__":
