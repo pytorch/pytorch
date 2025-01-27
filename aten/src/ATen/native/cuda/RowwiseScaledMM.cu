1e2e184d212571adef0f83d0354e101e68abf22e
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2025-01-27 09:30:59 -0800

    include sm_89

--- aten/src/ATen/native/cuda/RowwiseScaledMM.cu
+++ aten/src/ATen/native/cuda/RowwiseScaledMM.cu
@@ -791,7 +791,7 @@ void check_inputs(
     const std::optional<at::Tensor>& bias,
     const at::Tensor& out) {
   auto dprops = at::cuda::getCurrentDeviceProperties();
-  TORCH_CHECK(dprops->major == 9, "f8f8bf16_rowwise is sm_90 specific.");
+  TORCH_CHECK(dprops->major <= 9, "f8f8bf16_rowwise is sm_90 specific.");
 
   TORCH_CHECK(a.is_cuda());
   TORCH_CHECK(a.device() == b.device());
