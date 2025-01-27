c3d9352a4377e970060bffd41dee5188e1c10077
iremyux <iremyuksel@microsoft.com>
2025-01-27 16:21:25 +0100

    Fix file paths

--- .circleci/scripts/binary_windows_arm64_build.sh
+++ .circleci/scripts/binary_windows_arm64_build.sh
@@ -17,9 +17,9 @@ df -h
 export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
 
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
-    C:/a/pytorch/pytorch/builder/windows/arm64/build_libtorch.bat
-elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then    
-    C:/a/pytorch/pytorch/builder/windows/arm64/build_pytorch.bat
+    pytorch/.ci/pytorch/windows/arm64/build_libtorch.bat
+elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then
+    pytorch/.ci/pytorch/windows/arm64//build_pytorch.bat
 fi
 
 echo "Free space on filesystem after build:"
