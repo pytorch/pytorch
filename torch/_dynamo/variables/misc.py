277baf133f90f4c28b94f6d4b4909530f94a2b05
Aaron Orenstein <aorenste@meta.com>
2025-01-27 20:39:30 -0800

    Fix dynamo use of `list[int]` in graph break (#145554)
    This reintroduces the change backed out by #145393 and fixes the underlying problem.
    
    Although using a BuiltinVariable was better than nothing when we saw a GenericAlias it had problems if there was a graph break and we had to reconstruct the original python code which BuiltinVariable did as a simple `list` instead of a `list[int]`.
    
    This changes it to use a TypingVariable instead and then teaches TypingVariable how to reconstruct.
    
    Original commit changeset: 77b9193acb23
    
    python test/dynamo/test_repros.py ReproTests.test_graph_break_on_jit_isinstance
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/145554
    Approved by: https://github.com/anijain2305
    ghstack dependencies: #145551, #145552, #145553

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -1208,6 +1208,32 @@ class TypingVariable(VariableTracker):
     def as_python_constant(self):
         return self.value
 
+    def reconstruct(self, codegen: "torch._dynamo.codegen.PyCodegen") -> None:
+        # We're just trying to load the type here. Reconstructing the type from
+        # scratch is tricky - for a type like `typing.List[int]` we'd need to
+        # deconstruct the origin and args.  The origin for `List[int]` is `list`
+        # and the args is `(int,)`. When we recombine those we get the parts
+        # back and need to emit code for:
+        #
+        #     `typing.List[int]`
+        #
+        # But it's # worse than that - what if `typing` isn't in the globals (or
+        # was loaded like `import typing as _typing ; _typing.List[int]`?) so we
+        # really need to do something like:
+        #
+        #   `sys.modules["typing"].List[int]`
+        #
+        # Argh - but what if they rewrote the global `int`?  So we have to do:
+        #
+        #   `sys.modules["typing"].List[sys.modules["builtins"].int]`
+        #
+        # But where do we get `sys`? What if they never imported it or have
+        # something ELSE called `sys`?
+        #
+        # Let's skip all that noise and just emit it as a simple const.
+        #
+        codegen.append_output(codegen.create_load_const(self.value))
+
 
 @functools.lru_cache(maxsize=1)
 def get_np_to_tnp_map():
