879d9cee36b32d383c35ca8e23d99785a3087943
Animesh Jain <anijain@umich.edu>
2025-01-27 22:51:58 -0800

    Update base for Update on "[dynamo][builtin-skipfile-cleanup] Remove collections"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -19,7 +19,6 @@ import os
 import posixpath
 import re
 import selectors
-import signal
 import sys
 import tempfile
 import threading
@@ -3167,7 +3166,6 @@ BUILTIN_SKIPLIST = (
     multiprocessing,
     operator,
     posixpath,
-    re,
     selectors,
     tempfile,
     threading,
