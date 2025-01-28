bd9c45bf5db1557458ee35b07a04b47ccd40bc73
Jack Zhang <dvorjackz@gmail.com>
2025-01-28 14:27:31 -0800

    Improve error msg

--- torch/onnx/symbolic_opset17.py
+++ torch/onnx/symbolic_opset17.py
@@ -141,7 +141,7 @@ def stft(
 
     if center or pad_mode != "reflect":
         raise errors.SymbolicValueError(
-            msg="STFT does not currentlyt support center = True",
+            msg='STFT does not currently support center = True or pad_mode != "reflect"',
             value=input,
         )  # TODO(#insert-issue-id): add center functionality and tests.
 
