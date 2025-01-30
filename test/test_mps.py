pytorch/pytorch

commit 2d073fb521f29d72cd468c3b0682e2811cd5de38

[MPS] Add native strided API for MPSNDArray starting with macOS 15 (#128393)
Add support for native strides in MPS starting with macOS Sequoia. This will get rid of the additional gather and scatter operations needed to solve the strides or storage offsets of the tensors.

Summary of changes (starting with macOS 15):
- Add support for **MPS strided API** (strides/storage offsets etc):
   - [initWithBuffer:offset:descriptor:](https://developer.apple.com/documentation/metalperformanceshaders/mpsndarray/4391636-initwithbuffer?language=objc)
   - [arrayViewWithCommandBuffer:descriptor:aliasing:](https://developer.apple.com/documentation/metalperformanceshaders/mpsndarray/3114040-arrayviewwithcommandbuffer?language=objc)
   - [arrayViewWithShape:strides:](https://developer.apple.com/documentation/metalperformanceshaders/mpsndarray/4408694-arrayviewwithshape?language=objc)
   - [reshapeWithCommandBuffer:sourceArray:shape:destinationArray:](https://developer.apple.com/documentation/metalperformanceshaders/mpsndarrayidentity/4438557-reshapewithcommandbuffer?language=objc)
- Add native support for NHWC convolutions (without incurring any extra copy from NCHW -> NHWC -> NCHW).
- Add support for strided output buffers (previously we would create a contiguous buffer

OSes older than macOS 15 will run the old gather/scatter code path to solve strides/storage offsets.

---

Couple performance stats collected from torchbench comparing macOS 15 vs macOS 14:
```
- test_train[functorch_maml_omniglot-mps]: 27% faster
- test_train[timm_vision_transformer-mps]: 12% faster
- test_train[hf_T5-mps]: 9.46% faster
```

Pull Request resolved: https://github.com/pytorch/pytorch/pull/128393
Approved by: https://github.com/albanD

Co-authored-by: Siddharth Kotapati <skotapati@apple.com>

test/test_mps.py
@@ -23,7 +23,7 @@ from torch.nn import Buffer, Parameter
 from torch.testing._internal import opinfo
 from torch.testing._internal.common_utils import \
     (gradcheck, gradgradcheck, parametrize, run_tests, TestCase, download_file, IS_CI,
-     NoTest, skipIfSlowGradcheckEnv, suppress_warnings, serialTest)
+     NoTest, skipIfSlowGradcheckEnv, suppress_warnings, serialTest, instantiate_parametrized_tests)
 from torch.testing import make_tensor
 from torch.testing._internal.common_dtype import get_all_dtypes, integral_types
 import torch.backends.mps
@@ -728,7 +728,6 @@ def mps_ops_modifier(ops):
         'masked.median': None,
         'matrix_exp': None,
         'mode': None,
-        'nanquantile': None,
         'nanmedian': None,
         'native_dropout_backward': None,
         'normnuc': None,
@@ -758,7 +757,6 @@ def mps_ops_modifier(ops):
         'ormqr': None,
         'pca_lowrank': None,
         'qr': None,
-        'quantile': None,
         'rsub': None,
         'scatter_reduceamax': None,
         'scatter_reduceamin': None,
@@ -920,6 +918,12 @@ def mps_ops_modifier(ops):
             # not reproducible in later OS. Added assert to op if used in < 14.0
             'isin': [torch.int64, torch.int32, torch.int16, torch.uint8, torch.int8],
             'nn.functional.max_pool2d': [torch.uint8],
+        })
+
+    if product_version < 15.0:
+        UNIMPLEMENTED_XFAILLIST.update({
+            'quantile': None,
+            'nanquantile': None,
         })
 
     UNDEFINED_XFAILLIST = {
@@ -3540,6 +3544,76 @@ class TestMPS(TestCaseMPS):
         mps_slice4 = mps_x[1, :].to('cpu')
         self.assertEqual(cpu_slice4, mps_slice4)
 
+    @parametrize("torch_type", arg_values=[torch.float16, torch.float32, torch.bfloat16])
+    def test_slice_view_api(self, torch_type: torch.dtype):
+
+        def helper(x_tensor, y_func, z_func, r_func=None):
+            x_mps = x_tensor.detach().clone().to("mps")
+
+            y = y_func(x_tensor)
+            y_mps = y_func(x_mps)
+            self.assertEqual(y, y_mps)
+
+            z = z_func(y)
+            z_mps = z_func(y_mps)
+            self.assertEqual(z, z_mps)
+            self.assertEqual(z.storage_offset(), z_mps.storage_offset())
+
+            if r_func:
+                r = r_func(z)
+                r_mps = r_func(z_mps)
+                self.assertEqual(r, r_mps)
+
+        # Skip bfloat16 before MacOS15
+        if not (product_version < 15.0 and torch_type == torch.bfloat16):
+            # Tests for previously encountered MPS bugs
+            helper(
+                torch.randn(4, 4, dtype=torch_type),
+                lambda x: x[1],
+                lambda y: y.reshape(2, 2),
+                lambda z: z + 1
+            )
+            helper(
+                torch.randn(2, 4, dtype=torch_type),
+                lambda x: x[1],
+                lambda y: y + torch.ones(4, device=y.device)
+            )
+            helper(
+                torch.randn(4, 6, dtype=torch_type),
+                lambda x: x[1],
+                lambda y: y.reshape(3, 2).t(),
+                lambda z: z + 1
+            )
+            helper(
+                torch.arange(4, dtype=torch_type).resize(1, 2, 2),
+                lambda x: x.permute(2, 0, 1),
+                lambda y: y + 1
+            )
+            helper(
+                torch.randn(4, 8, dtype=torch_type),
+                lambda x: x.transpose(0, 1).reshape(-1),
+                lambda y: y[:2],
+                lambda z: z + 1
+            )
+            helper(
+                torch.randn(1, dtype=torch_type),
+                lambda x: x.expand(2, 3),
+                lambda y: y + torch.ones(2, 3, device=y.device)
+            )
+
+    def test_slice_reshape_contiguous(self):
+        x = torch.randn(4, 4)
+        x_mps = x.detach().clone().to("mps")
+
+        y = x[1]
+        y_mps = x_mps[1]
+        self.assertEqual(y, y_mps)
+
+        z = y.reshape(2, 2)
+        z_mps = y_mps.reshape(2, 2)
+        self.assertEqual(z, z_mps)
+        self.assertEqual(z.storage_offset(), z_mps.storage_offset())
+
     def test_scalar_from_slice_unary(self):
         # https://github.com/pytorch/pytorch/issues/82543
         tensor_list = torch.tensor([1.0, 1.2], device="mps")
@@ -3949,6 +4023,11 @@ class TestMPS(TestCaseMPS):
     def test_bool_expand(self):
         x = torch.tensor([[1], [0]], dtype=torch.bool, device='mps')
         y = torch.tensor([0, 1], dtype=torch.bool, device='mps')
+        self.assertFalse(torch.equal(x.expand(2, 2), y.expand(2, 2)))
+
+    def test_int_expand(self):
+        x = torch.tensor([[1], [0]], dtype=torch.int8, device='mps')
+        y = torch.tensor([0, 1], dtype=torch.int8, device='mps')
         self.assertFalse(torch.equal(x.expand(2, 2), y.expand(2, 2)))
 
     # Empty unary op should return tensor of the same size
@@ -10343,10 +10422,10 @@ class TestConvolutionMPS(TestCaseMPS):
                     res_cpu = conv_cpu(x_cpu)
                     res_mps = conv_mps(x_mps)
                     self.assertEqual(res_cpu, res_mps.cpu(), rtol=1e-03, atol=1e-05)
-
                     res_cpu = res_cpu.sum().backward()
                     res_mps = res_mps.sum().backward()
                     self.assertEqual(res_cpu, res_mps, rtol=2.6e-05, atol=2e-04)
+
                     self.assertEqual(conv_cpu.weight.grad, conv_mps.weight.grad, rtol=2.6e-05, atol=2e-04)
                     self.assertEqual(conv_cpu.bias.grad, conv_mps.bias.grad)
                     self.assertEqual(x_cpu.grad, x_mps.grad)
@@ -10776,12 +10855,12 @@ class TestAdvancedIndexing(TestCaseMPS):
 
     def test_nonzero_non_diff(self):
         device = "mps"
-        x = torch.randn(10, requires_grad=True)
+        x = torch.randn(10, requires_grad=True, device=device)
         nz = x.nonzero()
         self.assertFalse(nz.requires_grad)
 
     def test_nonzero_multi_threading(self):
-        # Test that MPS does not crash if nonzero called concurrently
+        # Test that MPS doesn't crash if nonzero called concurrently
         # See https://github.com/pytorch/pytorch/issues/100285
         x = torch.rand(3, 3, device="mps")
         t1 = threading.Thread(target=torch.nonzero, args=(x,))
@@ -12160,6 +12239,7 @@ instantiate_device_type_tests(TestConsistency, globals(), only_for="cpu")
 instantiate_device_type_tests(TestErrorInputs, globals(), allow_mps=True, only_for="mps")
 instantiate_device_type_tests(TestCommon, globals(), allow_mps=True, only_for="mps")
 instantiate_device_type_tests(TestLinalgMPS, globals(), allow_mps=True, only_for="mps")
+instantiate_parametrized_tests(TestMPS)
 
 if __name__ == "__main__":
     run_tests()

aten/src/ATen/mps/MPSDevice.mm
@@ -6,6 +6,7 @@
 #include <ATen/mps/MPSAllocatorInterface.h>
 #include <ATen/mps/MPSDevice.h>
 #include <ATen/mps/MPSStream.h>
+#include <ATen/native/mps/MPSGraphSequoiaOps.h>
 
 namespace at::mps {
 
@@ -29,8 +30,14 @@ id<MTLLibrary> MPSDevice::getMetalIndexingLibrary() {
   NSError* error = nil;
   if (!_mtl_indexing_library) {
     MTLCompileOptions* options = [MTLCompileOptions new];
+
     [options setLanguageVersion:getMetalLanguageVersion(_mtl_device)];
-    [options setFastMathEnabled:YES];
+
+    if (isMacOS13Plus(MacOSVersion::MACOS_VER_15_0_PLUS)) {
+      options.mathMode = MTLMathModeFast;
+    } else {
+      [options setFastMathEnabled:YES];
+    }
     _mtl_indexing_library = [_mtl_device newLibraryWithSource:[NSString stringWithCString:mps::indexing_metal_shaders
                                                                                  encoding:NSASCIIStringEncoding]
                                                       options:options

aten/src/ATen/mps/MPSHooks.mm
@@ -20,27 +20,38 @@ bool MPSHooks::hasMPS() const {
 }
 
 bool MPSHooks::isOnMacOSorNewer(unsigned major, unsigned minor) const {
-  if (major >= 14) {
-    if (major > 14) {
-      TORCH_WARN("Can't check whether running on ", major, ".", minor, "+ returning one for 14.0+");
-    } else if (minor > 0) {
-      TORCH_WARN("Can't check whether running on 14.", minor, "+ returning one for 14.0+");
-    }
-    return is_macos_13_or_newer(MacOSVersion::MACOS_VER_14_0_PLUS);
-  }
-  TORCH_CHECK(major == 13, "Trying to check for unexpected MacOS major ", major);
-  switch (minor) {
-    case 0:
-      return true;
-    case 1:
-      return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_1_PLUS);
-    case 2:
-      return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_2_PLUS);
-    case 3:
-      return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_3_PLUS);
+  switch (major) {
+    case 15:
+      if (minor > 0)
+        TORCH_WARN("Can't check whether running on 15.", minor, "+ returning one for 15.0+");
+      return is_macos_13_or_newer(MacOSVersion::MACOS_VER_15_0_PLUS);
+    case 14:
+      switch (minor) {
+        case 0:
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_14_0_PLUS);
+        case 4:
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_14_4_PLUS);
+        default:
+          TORCH_WARN("Can't check whether running on 14.", minor, "+ returning one for 14.4+");
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_14_4_PLUS);
+      }
+    case 13:
+      switch (minor) {
+        case 0:
+          return true;
+        case 1:
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_1_PLUS);
+        case 2:
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_2_PLUS);
+        case 3:
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_3_PLUS);
+        default:
+          TORCH_WARN("Can't check whether running on 13.", minor, "+ returning one for 13.3+");
+          return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_3_PLUS);
+      }
     default:
-      TORCH_WARN("Can't check whether running on 13.", minor, "+ returning one for 13.3+");
-      return is_macos_13_or_newer(MacOSVersion::MACOS_VER_13_3_PLUS);
+      TORCH_WARN("Checking for unexpected MacOS ", major, ".", minor, " returning false");
+      return false;
   }
 }
 

aten/src/ATen/native/Convolution.cpp
@@ -1664,7 +1664,7 @@ at::Tensor _convolution(
                "Input type (", input.toString(), ") and bias type (", bias.toString(),
                ") should be the same");
 
-      output = at::_mps_convolution(input.contiguous(), weight, bias.defined() ? bias.contiguous() : bias,
+      output = at::_mps_convolution(input, weight, bias.defined() ? bias.contiguous() : bias,
                                      params.padding, params.stride, params.dilation,
                                      params.groups);
 #else

aten/src/ATen/native/mps/MPSGraphSequoiaOps.h
@@ -1,0 +1,38 @@
+#pragma once
+
+#include <MetalPerformanceShadersGraph/MetalPerformanceShadersGraph.h>
+
+#if !defined(__MAC_15_0) && \
+    (!defined(MAC_OS_X_VERSION_15_0) || (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_15_0))
+
+@interface MPSNDArrayIdentity : MPSNDArrayUnaryKernel
+-(MPSNDArray * __nullable) reshapeWithCommandBuffer: (__nullable id <MTLCommandBuffer>) cmdBuf
+                                        sourceArray: (MPSNDArray * __nonnull) sourceArray
+                                              shape: (MPSShape * __nonnull) shape
+                                   destinationArray: (MPSNDArray * __nullable) destinationArray;
+@end
+
+@interface MPSNDArrayDescriptor()
+@property (readwrite, nonatomic) BOOL preferPackedRows;
+@end
+
+@interface MPSNDArray()
+-(nonnull instancetype) initWithBuffer:(id<MTLBuffer> _Nonnull) buffer
+                                offset:(NSUInteger) offset
+                            descriptor:(MPSNDArrayDescriptor * _Nonnull) descriptor;
+-(MPSNDArray * __nullable) arrayViewWithShape:(MPSShape * _Nullable) shape
+                                      strides:(MPSShape * _Nonnull)  strides;
+@end
+
+typedef NS_ENUM(NSInteger, MTLMathMode)
+{
+    MTLMathModeSafe = 0,
+    MTLMathModeRelaxed = 1,
+    MTLMathModeFast = 2,
+};
+
+@interface MTLCompileOptions()
+@property (readwrite, nonatomic) MTLMathMode mathMode;
+@end
+
+#endif

aten/src/ATen/native/mps/OperationUtils.h
@@ -86,7 +86,10 @@ MPSGraphTensorData* getMPSGraphTensorDataForView(const Tensor& src, MPSShape *mp
 MPSGraphTensor* castToIHFTypes(MPSGraph* mpsGraph, MPSGraphTensor* inputTensor, const Tensor& input, bool includesInt64 = false);
 MPSGraphTensor* castFromIHFTypes(MPSGraph* mpsGraph, MPSGraphTensor* inputTensor, const Tensor& input, bool includesInt64 = false);
 
+MPSNDArray* getMPSNDArray(const at::Tensor& t, const IntArrayRef& sizes = {}, const IntArrayRef& strides = {});
+MPSNDArray* getMPSNDArray(const at::Tensor& t, MPSShape* sizes = nil, MPSShape* strides = nil);
 // The MPSShape could vary based on memory format
+Tensor getTensorView(const Tensor& t, MPSShape* shape);
 MPSShape* getMPSShape(const Tensor& t, c10::MemoryFormat memory_format = MemoryFormat::Contiguous);
 MPSShape* getMPSShape(IntArrayRef sizes, c10::MemoryFormat memory_format = MemoryFormat::Contiguous);
 
@@ -98,8 +101,9 @@ class Placeholder {
  public:
   Placeholder() : _placeholder(nullptr), _value(nullptr), _tensor(Tensor()) {}
   Placeholder(MPSGraphTensor* mpsGraphTensor) : _placeholder(mpsGraphTensor), _value(nullptr), _tensor(Tensor()) {}
+  Placeholder(MPSGraphTensor* mpsGraphTensor, MPSNDArray* mpsNDArray);
   Placeholder(MPSGraphTensor* mpsGraphTensor, const Tensor& self, MPSShape *mpsShape = nullptr,
-              bool gatherTensorData = true, MPSDataType dataType = MPSDataTypeInvalid);
+              bool gatherTensorData = true, MPSDataType dataType = MPSDataTypeInvalid, bool useMPSStridedAPI = true);
   MPSGraphTensor* getMPSGraphTensor() {
     return _placeholder;
   }
@@ -452,7 +456,8 @@ inline bool supportedFloatingOrComplexType(const Tensor& t) {
 
 
 inline bool needsGather(const Tensor& t) {
-  return !t.is_contiguous() || t.storage_offset();
+  static const bool is_macOS_15_0_or_newer = is_macos_13_or_newer(MacOSVersion::MACOS_VER_15_0_PLUS);
+  return !is_macOS_15_0_or_newer && (!t.is_contiguous() || t.storage_offset()) ;
 }
 
 } // namespace at::native::mps

aten/src/ATen/native/mps/OperationUtils.mm
@@ -3,6 +3,7 @@
 #include <ATen/TensorIterator.h>
 #include <ATen/mps/MPSAllocatorInterface.h>
 #include <ATen/mps/MPSProfiler.h>
+#include <ATen/native/mps/MPSGraphSequoiaOps.h>
 #include <ATen/native/mps/MPSGraphSonomaOps.h>
 #include <ATen/native/mps/MPSGraphVenturaOps.h>
 #include <ATen/native/mps/OperationUtils.h>
@@ -303,6 +304,16 @@ std::string getTensorsStringKey(const TensorList& tensors, bool short_dtype, boo
   return str;
 }
 
+Tensor getTensorView(const Tensor& t, MPSShape* shape) {
+  std::vector<int64_t> res;
+  res.reserve([shape count]);
+  for (NSNumber* elem in shape) {
+    res.push_back(elem.longLongValue);
+  }
+  IntArrayRef r = IntArrayRef(res);
+  return t.view(res);
+}
+
 MPSShape* getMPSShape(const Tensor& t, c10::MemoryFormat memory_format) {
   return getMPSShape(t.sizes(), memory_format);
 }
@@ -359,26 +370,152 @@ MPSNDArray* ndArrayFromTensor(const Tensor& tensor, MPSShape* shape, MPSDataType
   return [tmpGraphTensorData mpsndarray];
 }
 
+static std::vector<int64_t> getSortedStrides(const IntArrayRef& s) {
+  std::vector<int64_t> idx(s.size());
+  iota(idx.begin(), idx.end(), 0);
+  sort(idx.begin(), idx.end(), [&s](size_t i1, size_t i2) { return s[i1] > s[i2]; });
+
+  return idx;
+}
+
+static std::vector<int64_t> inversePermutation(const std::vector<int64_t>& permuteOrder) {
+  auto size = permuteOrder.size();
+  std::vector<int64_t> inversePerm(permuteOrder.size());
+
+  for (int i = 0; i < size; i++) {
+    inversePerm[permuteOrder[i]] = i;
+  }
+  return inversePerm;
+}
+
+static MPSNDArray* permuteNDArray(MPSNDArray* inArray, const std::vector<int64_t>& permuteOrder_) {
+  auto permuteOrder = inversePermutation(permuteOrder_);
+  NSUInteger srcRank = [inArray numberOfDimensions];
+  if (srcRank != permuteOrder.size()) {
+    TORCH_INTERNAL_ASSERT(false);
+    return nil;
+  }
+  std::vector<NSUInteger> dimensionOrder(srcRank);
+  std::iota(std::begin(dimensionOrder), std::end(dimensionOrder), 0);
+  MPSNDArrayDescriptor* desc = [inArray descriptor];
+
+  for (int64_t i = srcRank - 1; i >= 0; i--) {
+    NSUInteger axis = permuteOrder[i];
+    auto axisIter = std::find(dimensionOrder.begin(), dimensionOrder.end(), axis);
+    NSUInteger axis1 = srcRank - i - 1;
+    NSUInteger axis2 = dimensionOrder.end() - axisIter - 1;
+    iter_swap(dimensionOrder.begin() + i, axisIter);
+    if (axis1 != axis2) {
+      [desc transposeDimension:axis1 withDimension:axis2];
+    }
+  }
+  C10_CLANG_DIAGNOSTIC_PUSH()
+#if C10_CLANG_HAS_WARNING("-Wnonnull")
+  C10_CLANG_DIAGNOSTIC_IGNORE("-Wnonnull")
+#endif
+  MPSNDArray* result = [inArray arrayViewWithCommandBuffer:nil descriptor:desc aliasing:MPSAliasingStrategyShallAlias];
+  C10_CLANG_DIAGNOSTIC_POP()
+
+  TORCH_INTERNAL_ASSERT(result != nil);
+  return result;
+}
+
+MPSNDArray* getMPSNDArray(const at::Tensor& t, MPSShape* sizes, MPSShape* strides) {
+  id<MTLBuffer> srcBuf = getMTLBufferStorage(t);
+
+  MPSDataType mpsDataType = getMPSDataType(t.scalar_type());
+  MPSNDArrayDescriptor* srcTensorDesc = [MPSNDArrayDescriptor descriptorWithDataType:mpsDataType shape:sizes];
+  srcTensorDesc.preferPackedRows = YES;
+  MPSNDArray* srcNDArray = [[[MPSNDArray alloc] initWithBuffer:srcBuf
+                                                        offset:t.storage_offset() * t.element_size()
+                                                    descriptor:srcTensorDesc] autorelease];
+  if (strides != nil) {
+    srcNDArray = [srcNDArray arrayViewWithShape:sizes strides:strides];
+  }
+  return srcNDArray;
+}
+
+MPSNDArray* getMPSNDArray(const at::Tensor& t, const IntArrayRef& sizes, const IntArrayRef& strides) {
+  return getMPSNDArray(t, getMPSShape(sizes.empty() ? t.sizes() : sizes), strides.empty() ? nil : getMPSShape(strides));
+}
+
+static MPSNDArray* getStridedMPSNDArray(const at::Tensor& src, MPSNDArray* srcNDArray) {
+  auto strides = src.strides();
+  auto sizes = src.sizes();
+  auto nStrides = strides.size();
+  auto nonZeroStrides = src.strides();
+  int64_t crtNonZeroStride = 1;
+  bool hasZeroStrides = false;
+  auto sortedStridesIndices = getSortedStrides(nonZeroStrides);
+
+  NSMutableArray<NSNumber*>* sortedStridesShape = [NSMutableArray arrayWithCapacity:nStrides];
+  NSMutableArray<NSNumber*>* sortedMPSShape = [NSMutableArray arrayWithCapacity:nStrides];
+  for (const auto i : c10::irange(nStrides)) {
+    sortedStridesShape[i] = [NSNumber numberWithInteger:nonZeroStrides[sortedStridesIndices[i]]];
+    sortedMPSShape[i] = [NSNumber numberWithInteger:sizes[sortedStridesIndices[i]]];
+  }
+  MPSShape* originalSortedMPSShape = sortedMPSShape;
+  MPSShape* originalSortedStridesShape = sortedStridesShape;
+  bool hasNonZeroStrides = nStrides == 0 ? false : nonZeroStrides[sortedStridesIndices[nStrides - 1]] != 1;
+  if (hasNonZeroStrides) {
+    originalSortedMPSShape = [sortedMPSShape copy];
+    originalSortedStridesShape = [sortedStridesShape copy];
+    [sortedStridesShape addObject:[NSNumber numberWithInteger:1]];
+    [sortedMPSShape addObject:[NSNumber numberWithInteger:1]];
+  }
+  if (nStrides == 0) {
+    originalSortedMPSShape = getMPSShape(src);
+    originalSortedStridesShape = getMPSShape(src.strides());
+  }
+
+  srcNDArray = [srcNDArray arrayViewWithShape:sortedMPSShape strides:sortedStridesShape];
+  if (hasNonZeroStrides) {
+    MPSNDArrayIdentity* identity =
+        [[[MPSNDArrayIdentity alloc] initWithDevice:MPSDevice::getInstance()->device()] autorelease];
+    srcNDArray = [identity reshapeWithCommandBuffer:nil
+                                        sourceArray:srcNDArray
+                                              shape:originalSortedMPSShape
+                                   destinationArray:nil];
+  }
+  TORCH_INTERNAL_ASSERT(srcNDArray);
+
+  srcNDArray = permuteNDArray(srcNDArray, sortedStridesIndices);
+  TORCH_INTERNAL_ASSERT(srcNDArray);
+
+  return srcNDArray;
+}
+
+Placeholder::Placeholder(MPSGraphTensor* mpsGraphTensor, MPSNDArray* mpsNDArray) {
+  _placeholder = mpsGraphTensor;
+  _value = [[[MPSGraphTensorData alloc] initWithMPSNDArray:mpsNDArray] autorelease];
+}
+
 Placeholder::Placeholder(MPSGraphTensor* mpsGraphTensor,
                          const Tensor& src,
-                         MPSShape* mpsShape,
+                         MPSShape* mpsShape_,
                          bool gatherTensorData,
-                         MPSDataType dataType)
+                         MPSDataType dataType,
+                         bool useMPSStridedAPI)
     : _tensor(src) {
   TORCH_CHECK(src.is_mps(), "Placeholder storage has not been allocated on MPS device!");
   // extract the pointer to MTLBuffer from the Tensor's storage
   id<MTLBuffer> srcBuf = getMTLBufferStorage(src);
-  // a view tensor could be contiguous (e.g., slice ops) or non-contiguous (e.g., transpose())
-  if (needsGather(src) && gatherTensorData) {
-    Tensor emptyShell = Tensor();
-    // use "_tensor" from Placeholder to retain view's output during its usage in other ops
-    _tensor = gatherViewTensor(src, emptyShell);
-    if (!_tensor.has_storage()) {
-      // if we cannot gather, we make the tensor contiguous implicitly, and keep
-      // it in placeholder to be able to retrieve it when we return from constructor
-      _tensor = src.clone(MemoryFormat::Contiguous);
+
+  static const bool is_macOS_15_0_or_newer = is_macos_13_or_newer(MacOSVersion::MACOS_VER_15_0_PLUS);
+  // Use gather kernel to solve strides for macOS < 15.0
+  // Starting with macOS 15.0, MPS supports native strides direclty in the kernels
+  if (!is_macOS_15_0_or_newer || !useMPSStridedAPI) {
+    if ((!src.is_contiguous() || src.storage_offset()) && gatherTensorData) {
+      Tensor emptyShell = Tensor();
+      // use "_tensor" from Placeholder to retain view's output during its usage in other ops
+      _tensor = gatherViewTensor(src, emptyShell);
+      if (!_tensor.has_storage()) {
+        // if we cannot gather, we make the tensor contiguous implicitly, and keep
+        // it in placeholder to be able to retrieve it when we return from constructor
+        _tensor = src.clone(MemoryFormat::Contiguous);
+      }
+      srcBuf = getMTLBufferStorage(_tensor);
     }
-    srcBuf = getMTLBufferStorage(_tensor);
   }
 
   // tensor.numel() could be zero, but tensor is valid as long as the buffer size is non-zero.
@@ -389,9 +526,66 @@ Placeholder::Placeholder(MPSGraphTensor* mpsGraphTensor,
     const auto scalar_type = _tensor.scalar_type();
     dataType = _tensor.dim() == 0 ? getMPSScalarType(scalar_type) : getMPSDataType(scalar_type);
   }
-  _value = [[[MPSGraphTensorData alloc] initWithMTLBuffer:srcBuf
-                                                    shape:mpsShape ? mpsShape : getMPSShape(_tensor)
-                                                 dataType:dataType] autorelease];
+
+  // Tensor is contiguous and has no storage offset.
+  // Wrap it directly inside MPSGraphTensorData
+  if ((_tensor.is_contiguous() && !_tensor.storage_offset()) || !useMPSStridedAPI || !is_macOS_15_0_or_newer) {
+    _value = [[[MPSGraphTensorData alloc] initWithMTLBuffer:srcBuf
+                                                      shape:mpsShape_ ? mpsShape_ : getMPSShape(_tensor)
+                                                   dataType:dataType] autorelease];
+  } else {
+    IntArrayRef view_shape;
+    if (mpsShape_) {
+      _tensor = getTensorView(src, mpsShape_);
+    }
+
+    MPSShape* mpsShape = getMPSShape(_tensor);
+    MPSShape* mpsStrides = getMPSShape(_tensor.strides());
+
+    IntArrayRef baseShape;
+    if (src.is_view()) {
+      baseShape = src._base().sizes();
+    } else {
+      baseShape = getIMPSAllocator()->getBufferShape(src.storage().data());
+    }
+    int flattenedShaped = 1;
+    for (const auto i : c10::irange(baseShape.size())) {
+      flattenedShaped *= baseShape[i];
+    }
+    MPSShape* mpsBaseShape = @[ @(flattenedShaped) ];
+    MPSNDArrayDescriptor* srcTensorDesc = [MPSNDArrayDescriptor descriptorWithDataType:dataType shape:mpsBaseShape];
+    srcTensorDesc.preferPackedRows = YES;
+    MPSNDArray* srcNDArray = [[[MPSNDArray alloc] initWithBuffer:srcBuf
+                                                          offset:src.storage_offset() * src.element_size()
+                                                      descriptor:srcTensorDesc] autorelease];
+    TORCH_INTERNAL_ASSERT(srcNDArray);
+    if (src.dim() != 0) {
+      srcNDArray = getStridedMPSNDArray(_tensor, srcNDArray);
+    } else {
+      bool needsReshape = false;
+      NSMutableArray* mpsExpandedShape = nil;
+      NSMutableArray* mpsExpandedStrides = nil;
+
+      if (src.dim() > 0 && src.stride(-1) != 1) {
+        needsReshape = true;
+        mpsExpandedShape = [NSMutableArray arrayWithArray:mpsShape];
+        mpsExpandedStrides = [NSMutableArray arrayWithArray:mpsStrides];
+        [mpsExpandedShape addObject:@1];
+        [mpsExpandedStrides addObject:@1];
+      }
+      srcNDArray = [srcNDArray arrayViewWithShape:needsReshape ? mpsExpandedShape : getMPSShape(src)
+                                          strides:needsReshape ? mpsExpandedStrides : getMPSShape(src.strides())];
+      TORCH_INTERNAL_ASSERT(srcNDArray);
+
+      if (needsReshape) {
+        MPSNDArrayIdentity* identity =
+            [[[MPSNDArrayIdentity alloc] initWithDevice:MPSDevice::getInstance()->device()] autorelease];
+        srcNDArray = [identity reshapeWithCommandBuffer:nil sourceArray:srcNDArray shape:mpsShape destinationArray:nil];
+      }
+      TORCH_INTERNAL_ASSERT(srcNDArray);
+    }
+    _value = [[[MPSGraphTensorData alloc] initWithMPSNDArray:srcNDArray] autorelease];
+  }
 
   TORCH_INTERNAL_ASSERT(_value);
   _placeholder = mpsGraphTensor;

commit ae6c1e59f6f8c6dd4841ed23a1e0e2307ab31914

[MPS] Fix regression in con-contig bitwise ops
Caused by https://github.com/pytorch/pytorch/pull/128393 that change semantic of `needsGather`, which resulted in silent correctness errors on MacOS-15+ if output tensor is non-contiguous

Fixes https://github.com/pytorch/pytorch/issues/145203

aten/src/ATen/native/mps/operations/BitwiseOps.mm
@@ -226,7 +226,7 @@ static void _bitwise_op_out_mps(const Tensor& self,
 
   auto output_size = at::infer_size_dimvector(self.sizes(), other.sizes());
   resize_output(output, output_size);
-  if (needsGather(output)) {
+  if (!output.is_contiguous()) {
     output = output.contiguous();
     needs_output_copy = true;
   }

test/test_mps.py
@@ -4442,6 +4442,16 @@ class TestMPS(TestCaseMPS):
                     getattr(torch.tensor(val1, dtype=dtype1, device='cpu'), binop)
                            (torch.full(full_shape, val2, dtype=dtype2, device='cpu')))
 
+    def test_xor_non_contigous(self):
+        # See https://github.com/pytorch/pytorch/issues/145203
+        x_mps = torch.randint(-16000, 16000, (10, 2), dtype=torch.int16, device="mps")
+        x_cpu = x_mps.detach().cpu()
+
+        x_mps[:, 0] ^= 3
+        x_cpu[:, 0] ^= 3
+
+        self.assertEqual(x_mps.cpu(), x_cpu)
+
     def test_nansum(self):
         def helper(dtype, noncontiguous, dim):
             zero_cpu = torch.zeros((), dtype=dtype)