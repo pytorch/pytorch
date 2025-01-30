dd797f7ed9c804d05aab29061b46bd0ae3305da6
Ke Wen <kw2501@meta.com>
2025-01-29 16:25:30 -0800

    Update on "[PGNCCL] Simplify support macro definition"
    - Promotes usage of `NCCL_VERSION_CODE >= NCCL_VERSION(X, Y, Z)`
    
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k c-p-i-o
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/NCCLUtils.hpp
+++ torch/csrc/distributed/c10d/NCCLUtils.hpp
@@ -34,14 +34,14 @@ constexpr int64_t kCommInitBusyWaitMillis = 2;
 #endif
 
 static_assert(
-    NCCL_VERSION_CODE >= NCCL_VERSION(2, 10, 0),
-    "NCCL version must be 2.10 or later");
-// The following macros represent features supported prior to NCCL 2.10,
+    NCCL_VERSION_CODE >= NCCL_VERSION(2, 7, 0),
+    "NCCL version must be 2.7 or later");
+// The following macros represent features supported prior to NCCL 2.7,
 // therefore we can define them unconditionally, given the static_assert above.
 // TODO: remove these macros from code.
 #define ENABLE_NCCL_ERROR_CHECKING
 #define ENABLE_NCCL_P2P_SUPPORT
-// End of macros for NCCL 2.10 and below.
+// End of macros for NCCL 2.7 and below.
 
 #if NCCL_VERSION_CODE >= NCCL_VERSION(2, 11, 0)
 #define ENABLE_NCCL_PREMUL_SUM_SUPPORT
