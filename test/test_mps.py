05a51d1883c6952e59c3329a04f15a5edf46cd4d
Siddharth Kotapati <skotapati@apple.com>
2025-01-31 15:28:22 -0800

    Add grad tests to mps op db

--- test/test_mps.py
+++ test/test_mps.py
@@ -12750,7 +12750,8 @@ class TestConsistency(TestCaseMPS):
                 atol, rtol = 2e-5, 2e-6
             self.assertEqual(cpu_out, mps_out, atol=atol, rtol=rtol)
 
-    @ops(mps_ops_grad_modifier(copy.deepcopy(test_consistency_op_db)), allowed_dtypes=MPS_GRAD_DTYPES)
+
+    @ops(mps_op_db(copy.deepcopy(test_consistency_op_db)), allowed_dtypes=MPS_GRAD_DTYPES)
     def test_output_grad_match(self, device, dtype, op):
         self.assertEqual(device, "cpu")
 
