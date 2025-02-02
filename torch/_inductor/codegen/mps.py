ca19d25c031e68ee51bbdaa82fa631dac7972dfc
Davide Italiano <davidino@meta.com>
2025-02-02 01:54:23 +0000

    [mps/inductor] Implement support for polygamma(). (#146259)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146259
    Approved by: https://github.com/jansel

--- torch/_inductor/codegen/mps.py
+++ torch/_inductor/codegen/mps.py
@@ -252,6 +252,14 @@ class MetalOverrides(OpOverrides):
         return f"c10::metal::log_gamma({x})"
 
     @staticmethod
+    def polygamma(n: CSEVariable, x: CSEVariable) -> str:
+        # polygamma's API takes order as first argument
+        # and the input tensor as second, while the
+        # metal shader has these inverted.
+        # TODO (dcci): make this more uniform.
+        return f"c10::metal::polygamma({x}, {n})"
+
+    @staticmethod
     def tan(x: CSEVariable) -> str:
         return f"metal::tan({x})"
 
