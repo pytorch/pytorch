fd751932deb9a5a90a9d8e394507c7c8608ba384
Sam Larsen <slarsen@meta.com>
2025-02-03 16:02:20 -0800

    Update on "Make partitioning more deterministic"
    Copied from https://github.com/pytorch/pytorch/pull/145024, but made _all_ sets OrderedSets and fixed mypy complaints. Note that this change does not fix the non-determinism we're seeing in the internal model. But let's at least eliminate this potential source of non-determinism before investigating any changes to the mincut approach?
    
    [ghstack-poisoned]

--- torch/_functorch/partitioners.py
+++ torch/_functorch/partitioners.py
@@ -1077,43 +1077,30 @@ def solve_min_cut(
     # backwards pass instead of only relying on whether it's unfusible in the
     # forwards.
 
-    tiebreak_count = 0
-
-    def get_tiebreak_count() -> int:
-        nonlocal tiebreak_count
-        tiebreak_count += 1
-        return tiebreak_count
-
     def find_first_unfusible(start_nodes: list[fx.Node], max_range: int) -> int:
         """
         Finds the first unfusible node in the chain of nodes starting from
         `start_nodes` and returns its position.
         """
-        sorted_nodes: list[tuple[int, fx.Node, bool, int]] = []
+        sorted_nodes: list[tuple[int, fx.Node, bool]] = []
         for n in start_nodes:
-            heapq.heappush(
-                sorted_nodes, (node_info.get_fw_order(n), n, True, get_tiebreak_count())
-            )
+            heapq.heappush(sorted_nodes, (node_info.get_fw_order(n), n, True))
 
         while len(sorted_nodes) > 0:
-            _, node, node_is_fusible, _tiebreak = heapq.heappop(sorted_nodes)
+            _, node, node_is_fusible = heapq.heappop(sorted_nodes)
             if not node_is_fusible:
                 return node_info.get_fw_order(node)
             for user in node.users:
                 if node_info.is_required_fw(user):
                     if node_info.get_fw_order(user) > max_range:
                         continue
-                    val: tuple[int, fx.Node, bool, int] = (
+                    val: tuple[int, fx.Node, bool] = (
                         node_info.get_fw_order(user),
                         user,
                         is_fusible(node, user),
-                        get_tiebreak_count(),
                     )
                     if val not in sorted_nodes:
-                        heapq.heappush(
-                            sorted_nodes,
-                            val,
-                        )
+                        heapq.heappush(sorted_nodes, val)
         return max_range
 
     if min_cut_options.ban_if_used_far_apart:
@@ -1162,12 +1149,12 @@ def solve_min_cut(
         for start_node in joint_graph.nodes:
             if not node_info.is_required_fw(start_node):
                 continue
-            fusible: list[tuple[int, fx.Node, int]] = [
-                (node_info.get_fw_order(start_node), start_node, get_tiebreak_count())
+            fusible: list[tuple[int, fx.Node]] = [
+                (node_info.get_fw_order(start_node), start_node)
             ]
             start_order = node_info.get_fw_order(start_node)
             while len(fusible) > 0:
-                _, cur, _tiebreak = heapq.heappop(fusible)
+                _, cur = heapq.heappop(fusible)
                 if cur in visited:
                     continue
                 visited.add(cur)
@@ -1192,10 +1179,7 @@ def solve_min_cut(
                         and is_fusible(cur, user)
                         and user not in banned_nodes
                     ):
-                        heapq.heappush(
-                            fusible,
-                            (node_info.get_fw_order(user), user, get_tiebreak_count()),
-                        )
+                        heapq.heappush(fusible, (node_info.get_fw_order(user), user))
 
     try:
         cut_value, partition = nx.minimum_cut(nx_graph, "source", "sink")
