88c961dc021b70f57ddfd09b6511769473314b3b
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-03 22:20:42 -0800

    [MPSInductor] Implement `prod` reduction (#146396)
    Mostly reusing `sum` reduction logic
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146396
    Approved by: https://github.com/dcci
    ghstack dependencies: #146369, #146370, #146380, #146389

--- c10/metal/reduction_utils.h
+++ c10/metal/reduction_utils.h
@@ -19,6 +19,18 @@ opmath_t<T> threadgroup_sum(threadgroup T* data, unsigned size) {
 }
 
 template <typename T>
+opmath_t<T> threadgroup_prod(threadgroup T* data, unsigned size) {
+  opmath_t<T> rc = 1;
+  // TODO: This should be moved to the callee
+  ::metal::threadgroup_barrier(::metal::mem_flags::mem_threadgroup);
+  // TODO: Use `simd_shuffle_down`
+  for (auto idx = 0; idx < size; ++idx) {
+    rc *= data[idx];
+  }
+  return rc;
+}
+
+template <typename T>
 T threadgroup_max(threadgroup T* data, unsigned size) {
   // TODO: This should be moved to the callee
   ::metal::threadgroup_barrier(::metal::mem_flags::mem_threadgroup);
