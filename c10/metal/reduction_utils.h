735b60b64daa90c3d279a5dbc07d3fb675a7f282
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-03 21:30:04 -0800

    Update
    [ghstack-poisoned]

--- c10/metal/reduction_utils.h
+++ c10/metal/reduction_utils.h
@@ -6,13 +6,13 @@
 namespace c10 {
 namespace metal {
 
-template<typename T>
+template <typename T>
 opmath_t<T> threadgroup_sum(threadgroup T* data, unsigned size) {
   opmath_t<T> rc = 0;
   // TODO: This should be moved to the callee
   ::metal::threadgroup_barrier(::metal::mem_flags::mem_threadgroup);
   // TODO: Use `simd_shuffle_down`
-  for(auto idx = 0; idx < size; ++idx) {
+  for (auto idx = 0; idx < size; ++idx) {
     rc += data[idx];
   }
   return rc;
