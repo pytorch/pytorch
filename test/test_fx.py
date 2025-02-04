48dda6585a2d3b18b0abb453046b250d790cb629
Aaron Orenstein <aorenste@meta.com>
2025-02-03 20:19:47 -0800

    PEP585: PEP585: Add noqa to necessary tests
    [ghstack-poisoned]

--- test/test_fx.py
+++ test/test_fx.py
@@ -52,7 +52,7 @@ from fx.test_source_matcher_utils import TestSourceMatcher  # noqa: F401
 
 from fx.test_gradual_type import AnnotationsTest  # noqa: F401
 from fx.test_gradual_type import TypeCheckerTest  # noqa: F401
-from typing import Any, Callable, NamedTuple, Optional, Union, Tuple, List
+from typing import Any, Callable, NamedTuple, Optional, Union
 from torch.testing._internal.common_utils import (
     IS_FBCODE,
     IS_MACOS,
@@ -2149,7 +2149,16 @@ class TestFX(JitTestCase):
         graph : torch.fx.Graph = torch.fx.Graph()
         x : torch.fx.Node = graph.create_node('placeholder', 'x')
         b : torch.fx.Node = graph.create_node('call_function', target=torch.relu, args=(x,),
-                                              type_expr=List[float])
+                                              type_expr=list[float])
+        output : torch.fx.Node = graph.output(b)
+
+        self.assertTrue('list[float]' in str(graph))
+
+    def test_typename_print_pre_pep585(self):
+        graph : torch.fx.Graph = torch.fx.Graph()
+        x : torch.fx.Node = graph.create_node('placeholder', 'x')
+        b : torch.fx.Node = graph.create_node('call_function', target=torch.relu, args=(x,),
+                                              type_expr=typing.List[float])  # noqa: UP006
         output : torch.fx.Node = graph.output(b)
 
         self.assertTrue('typing.List[float]' in str(graph))
@@ -3555,7 +3564,7 @@ class TestFX(JitTestCase):
     @unittest.skipIf(sys.version_info > (3, 11), "Does not work in 3.11")
     def test_annotations_empty_tuple(self):
         class Foo(torch.nn.Module):
-            def forward(self, x: Tuple[()], y: Tuple[str, Tuple[()]]):
+            def forward(self, x: tuple[()], y: tuple[str, tuple[()]]):
                 return "foo"
 
         traced = torch.fx.symbolic_trace(Foo())
@@ -4095,10 +4104,10 @@ class TestFXAPIBackwardCompatibility(JitTestCase):
         tuple,
         type,
         typing.Callable,
-        typing.Dict,
-        typing.List,
-        typing.Tuple,
-        typing.Type,
+        typing.Dict,  # noqa: UP006
+        typing.List,  # noqa: UP006
+        typing.Tuple,  # noqa: UP006
+        typing.Type,  # noqa: UP006
         typing.Union,
     }
 
