f80a55c092be6abf2acb83e1418a4e2f28ffa020
Davide Italiano <davidino@meta.com>
2025-02-04 06:40:34 +0000

    [Metal][BE] Fix the arguments of `polygamma` (#146382)
    In the public API, order comes before input, while here they're
     reversed. Match for consistency (and make this less error prone).
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146382
    Approved by: https://github.com/jansel, https://github.com/malfet

--- torch/_inductor/codegen/mps.py
+++ torch/_inductor/codegen/mps.py
@@ -256,11 +256,7 @@ class MetalOverrides(OpOverrides):
 
     @staticmethod
     def polygamma(n: CSEVariable, x: CSEVariable) -> str:
-        # polygamma's API takes order as first argument
-        # and the input tensor as second, while the
-        # metal shader has these inverted.
-        # TODO (dcci): make this more uniform.
-        return f"c10::metal::polygamma({x}, {n})"
+        return f"c10::metal::polygamma({n}, {x})"
 
     @staticmethod
     def digamma(x: CSEVariable) -> str:
