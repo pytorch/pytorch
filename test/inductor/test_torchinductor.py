1952c4d3aee06d2d7a3571d1b320908c39b9af84
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-04 10:54:08 -0800

    Update
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2818,7 +2818,8 @@ class CommonTemplate:
         def fn(a, b):
             return (torch.exp(a), torch.exp(a + b))
 
-        self.common(fn, (torch.randn(8, 8), torch.randn(8, 8)))
+        # exp(a+b) could be significantly different than exp(a.half()+b.half())
+        self.common(fn, (torch.randn(8, 8), torch.randn(8, 8)), reference_in_float=False)
 
     def test_exp2(self):
         def fn(a, b):
