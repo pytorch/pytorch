bbe95341d9259b464538720a9cd2b99de25f4a47
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-03 22:20:39 -0800

    [MPSInductor] Implement `min` and `max` reductions (#146389)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146389
    Approved by: https://github.com/jansel, https://github.com/dcci
    ghstack dependencies: #146369, #146370, #146380

--- c10/metal/reduction_utils.h
+++ c10/metal/reduction_utils.h
@@ -18,5 +18,27 @@ opmath_t<T> threadgroup_sum(threadgroup T* data, unsigned size) {
   return rc;
 }
 
+template <typename T>
+T threadgroup_max(threadgroup T* data, unsigned size) {
+  // TODO: This should be moved to the callee
+  ::metal::threadgroup_barrier(::metal::mem_flags::mem_threadgroup);
+  T rc = data[0];
+  for (auto idx = 1; idx < size; ++idx) {
+    rc = ::c10::metal::max(rc, data[idx]);
+  }
+  return rc;
+}
+
+template <typename T>
+T threadgroup_min(threadgroup T* data, unsigned size) {
+  // TODO: This should be moved to the callee
+  ::metal::threadgroup_barrier(::metal::mem_flags::mem_threadgroup);
+  T rc = data[0];
+  for (auto idx = 1; idx < size; ++idx) {
+    rc = ::c10::metal::min(rc, data[idx]);
+  }
+  return rc;
+}
+
 } // namespace metal
 } // namespace c10
