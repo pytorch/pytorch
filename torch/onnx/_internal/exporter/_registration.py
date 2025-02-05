61c9814fb743bc1c0be42cc1e1ae50e561c8bce4
Justin Chu <justinchuby@users.noreply.github.com>
2025-02-05 08:33:59 -0800

    raise

--- torch/onnx/_internal/exporter/_registration.py
+++ torch/onnx/_internal/exporter/_registration.py
@@ -63,19 +63,24 @@ class OnnxDecompMeta:
                         self.onnx_function, "__traced", self.onnx_function.__name__
                     )
             except Exception as e:
-                logger.info(
-                    "Failed to infer the signature for function '%s' because '%s'"
-                    "All nodes targeting `%s` will be dispatched to this function",
-                    self.onnx_function,
-                    e,
-                    self.fx_target,
-                )
-                # When the function is targeting an HOP, for example, it will accept
-                # functions as arguments and fail to generate an ONNX signature.
-                # In this case we set signature to None and dispatch to this function always.
+                # Log an warning if the op is custom. Raise exception for builtin ops.
+                if not self.is_custom:
+                    raise
+                else:
+                    # When the function is targeting an HOP, for example, it will accept
+                    # functions as arguments and fail to generate an ONNX signature.
+                    # In this case we set signature to None and dispatch to this function always.
+                    logger.warning(
+                        "Failed to infer the signature for function '%s' because '%s'"
+                        "All nodes targeting `%s` will be dispatched to this function",
+                        self.onnx_function,
+                        e,
+                        self.fx_target,
+                    )
             else:
                 self.signature = signature
                 self.onnx_function._pt_onnx_signature = signature  # type: ignore[attr-defined]
+
 
 class ONNXRegistry:
     """Registry for ONNX functions.
