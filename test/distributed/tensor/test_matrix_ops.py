bdfd4a78acc9b926992e934596d045ba81389ac1
wz337 <wz337@cornell.edu>
2025-02-05 11:33:51 -0800

    create a simple tensordot unit test

--- test/distributed/tensor/test_matrix_ops.py
+++ test/distributed/tensor/test_matrix_ops.py
@@ -476,52 +476,26 @@ class DistMatrixOpsTest(DTensorTestBase):
 
     @with_comms
     @skip_unless_torch_gpu
-    def test_tensordot(self):
+    def test_tensordot_shampoo(self):
+        """
+        Create a simple test for Shampoo's use case.
+        """
         device_mesh = init_device_mesh(self.device_type, (self.world_size,))
 
-        local_a = torch.randn(3, 5, 4, 6)
-        local_b = torch.randn(6, 4, 5, 3)
-        dims = ([2, 1, 3], [1, 2, 0])
-        local_result = torch.tensordot(local_a, local_b, dims=dims)
-
-        local_a = torch.randn(15, 15)
+        local_a = torch.randn(4, 4)
         local_b = torch.randn(4, 15)
         dims = ([0], [0])
-        local_result = torch.tensordot(local_a, local_b, dims=dims)
+        local_result = torch.tensordot(local_a, local_b, dims=(dims))
 
-        # # No sharding. Simply replicate the tensor on each rank.
-        # dist_a = distribute_tensor(local_a, device_mesh, [Replicate()])
-        # dist_b = distribute_tensor(local_b, device_mesh, [Replicate()])
-        # dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
-        # print(f"1. dist_result: {dist_result}")
-        # self.assertEqual(local_result, dist_result.to_local())
+        placements = [Replicate(), Shard(0), Shard(1)]
+        placements_tuples = itertools.product(placements, repeat=2)
 
-        # dist_a = distribute_tensor(local_a, device_mesh, [Shard(0)])
-        # dist_b = distribute_tensor(local_b, device_mesh, [Replicate()])
-        # dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
-        # print(f"2. dist_result: {dist_result}")
-        # self.assertEqual(local_result, dist_result.to_local())
-
-        # dist_a = distribute_tensor(local_a, device_mesh, [Replicate()])
-        # dist_b = distribute_tensor(local_b, device_mesh, [Shard(1)])
-        # dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
-        # dist_result_full = dist_result.full_tensor()
-        # print(f"3. dist_result: {dist_result}")
-        # self.assertEqual(local_result, dist_result_full)
-
-        # dist_a = distribute_tensor(local_a, device_mesh, [Shard(0)])
-        # dist_b = distribute_tensor(local_b, device_mesh, [Shard(1)])
-        # dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
-        # dist_result_full = dist_result.full_tensor()
-        # print(f"4. dist_result: {dist_result}")
-        # self.assertEqual(local_result, dist_result_full)
-
-        dist_a = distribute_tensor(local_a, device_mesh, [Shard(1)])
-        dist_b = distribute_tensor(local_b, device_mesh, [Replicate()])
-        dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
-        dist_result_full = dist_result.full_tensor()
-        print(f"5. dist_result: {dist_result}")
-        self.assertEqual(local_result, dist_result_full)
+        for placement1, placement2 in placements_tuples:
+            dist_a = distribute_tensor(local_a, device_mesh, [placement1])
+            dist_b = distribute_tensor(local_b, device_mesh, [placement2])
+            dist_result = torch.tensordot(dist_a, dist_b, dims=dims)
+            dist_result_full = dist_result.full_tensor()
+            self.assertEqual(local_result, dist_result_full)
 
 
 if __name__ == "__main__":
