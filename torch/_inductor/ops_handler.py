93e1e6e07cb9ecaba3da380b66c3b2f9798cefc9
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-02-05 23:13:08 +0000

    Revert "[inductor] Minor compile time optimizations in DefaultHandler (#146282)"
    This reverts commit b8a529cca18ae4d21b1681c5ea3a40635aba5a83.
    
    Reverted https://github.com/pytorch/pytorch/pull/146282 on behalf of https://github.com/atalman due to Sorry need to revert https://github.com/pytorch/pytorch/pull/146252 ([comment](https://github.com/pytorch/pytorch/pull/146282#issuecomment-2638239575))

--- torch/_inductor/ops_handler.py
+++ torch/_inductor/ops_handler.py
@@ -1,10 +1,8 @@
 # mypy: allow-untyped-defs
 from __future__ import annotations
 
-import inspect
 import itertools
 import re
-from io import StringIO
 from typing import Any, Callable, Generic, Literal, NamedTuple, Optional, TypeVar, Union
 from unittest.mock import patch
 
@@ -734,7 +732,7 @@ class OpsHandler(Generic[T]):
     ) -> T:
         raise NotImplementedError
 
-    def output(self, *args: T) -> None:
+    def output(self, x0: T) -> None:
         """This is a fake op used in analysis but not codegen"""
         raise NotImplementedError
 
@@ -760,7 +758,7 @@ class DefaultHandler(OpsHandler[Any]):
         provide generic op behavior.
 
         Args:
-            name: name of the op, see OpHandler.{name}
+            target: name of the op, see OpHandler.target
             args: positional args passed to the op
             kwargs: keyword args passed to the op
 
@@ -780,41 +778,8 @@ class DefaultHandler(OpsHandler[Any]):
 
     @classmethod
     def _init_cls(cls):
-        """
-        Here we codegen many functions of the form:
-
-            def add(self, a, b):
-                return self._default('add', (a, b), {})
-
-        and install them in cls.  This is the same as _call_default above,
-        but is about 1.2x faster since CPython varargs parsing is slow.
-        """
-        code = StringIO()
         for target in OP_NAMES:
-            sig = inspect.signature(getattr(OpsHandler, target))
-            if all(
-                p.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
-                and p.default is inspect.Parameter.empty
-                for p in sig.parameters.values()
-            ):
-                self_arg, *args = sig.parameters.keys()
-                assert self_arg == "self"
-                code.write(
-                    f"""
-                    def {target}(self, {', '.join(args)}):
-                        return self._default({target!r}, ({', '.join(args)}, ), {{}})
-                    """.strip()
-                )
-                code.write("\n\n")
-            else:
-                # slower fallback for ops with default or variadic arguments
-                setattr(cls, target, cls._call_default(target))
-
-        ctx: dict[str, Any] = {}
-        exec(code.getvalue(), ctx)
-        for target, impl in ctx.items():
-            if target in OP_NAMES:
-                setattr(cls, target, impl)
+            setattr(cls, target, cls._call_default(target))
 
 
 DefaultHandler._init_cls()
