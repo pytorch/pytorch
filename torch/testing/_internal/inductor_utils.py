d6e6bd47df8ef346449e6d1e112eaa9c2ff3e3fa
Shunting Zhang <shunting@fb.com>
2025-02-06 12:13:04 -0800

    [inductor] add size-asserts for fallback ops (#145904)
    Fix https://github.com/pytorch/pytorch/issues/144717
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/145904
    Approved by: https://github.com/jansel

--- torch/testing/_internal/inductor_utils.py
+++ torch/testing/_internal/inductor_utils.py
@@ -5,6 +5,7 @@ import torch
 import re
 import unittest
 import functools
+import contextlib
 import os
 from subprocess import CalledProcessError
 import sys
@@ -140,3 +141,39 @@ IS_H100 = LazyVal(
 )
 
 IS_BIG_GPU = LazyVal(lambda: HAS_CUDA and is_big_gpu())
+
+def maybe_skip_size_asserts(op):
+    """
+    For certain ops, there meta and eager implementation returns differents
+    strides. This cause size/strides assert fail. Skip adding those
+    asserts for now.
+    """
+    if (
+        op.aten_name
+        in (
+            "fft_hfftn",
+            "fft_hfft",
+            "fft_hfft2",
+            "fft_ihfftn",
+            "fft_fft",
+            "fft_fft2",
+            "fft_fftn",
+            "fft_ifft",
+            "fft_ifft2",
+            "fft_ifftn",
+            "fft_irfft",
+            "fft_irfft2",
+            "fft_irfftn",
+            "fft_ihfft",
+            "fft_ihfft2",
+            "fft_rfft",
+            "fft_rfft2",
+            "fft_rfftn",
+            "linalg_eig",
+            "linalg_eigvals",
+        )
+        and "TORCHINDUCTOR_SIZE_ASSERTS" not in os.environ
+    ):
+        return torch._inductor.config.patch(size_asserts=False)
+    else:
+        return contextlib.nullcontext()
