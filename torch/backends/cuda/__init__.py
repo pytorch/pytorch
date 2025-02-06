5da96a78f732d241a83b1e1b5b5325e0b763671b
Jiang, Yanbing <yanbing.jiang@intel.com>
2025-02-06 06:30:24 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/backends/cuda/__init__.py
+++ torch/backends/cuda/__init__.py
@@ -134,7 +134,7 @@ class cuBLASModule:
         elif name == "allow_bf16_reduced_precision_reduction":
             return torch._C._get_cublas_allow_bf16_reduced_precision_reduction()
         elif name == "fp32_precision":
-            return torch._C._get_fp32_precision("cuda", "matmul")
+            return torch._C._get_fp32_precision_getter("cuda", "matmul")
         raise AttributeError("Unknown attribute " + name)
 
     def __setattr__(self, name, value):
@@ -145,7 +145,7 @@ class cuBLASModule:
         elif name == "allow_bf16_reduced_precision_reduction":
             return torch._C._set_cublas_allow_bf16_reduced_precision_reduction(value)
         elif name == "fp32_precision":
-            return torch._C._set_fp32_precision("cuda", "matmul", value)
+            return torch._C._set_fp32_precision_setter("cuda", "matmul", value)
         raise AttributeError("Unknown attribute " + name)
 
 
