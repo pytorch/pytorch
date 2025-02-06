fa3bcf05a05bfc04848556d906f6ae8fb1dc7017
Tom Ritchford <tom@swirly.com>
2025-02-06 15:56:33 +0000

    Update
    [ghstack-poisoned]

--- test/typing/pass/arithmetic_ops.py
+++ test/typing/pass/arithmetic_ops.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Union
 from typing_extensions import assert_type
 
 from torch import randn, Tensor
@@ -150,3 +150,273 @@ assert_type(FLOAT >> TENSOR, Any)
 assert_type(FLOAT & TENSOR, Tensor)  # type: ignore[operator]
 assert_type(FLOAT | TENSOR, Tensor)  # type: ignore[operator]
 assert_type(FLOAT ^ TENSOR, Tensor)  # type: ignore[operator]
+
+
+NUMBER = Union[int, float, bool]
+
+
+class Binary:
+    """This class shows what is possible by overridding every magic method"""
+    def __add__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __and__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __div__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __eq__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __floordiv__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __ge__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __gt__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __invert__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __le__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __lshift__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __lt__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __mod__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __mul__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __ne__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __or__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __pow__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __radd__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rand__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rdiv__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rfloordiv__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rlshift__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rmod__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rmul__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __ror__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rpow__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rrshift__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rshift__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rsub__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rtruediv__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __rxor__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __sub__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __truediv__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+    def __xor__(self, other: NUMBER) -> "Binary":  # type: ignore[override]
+        return self
+
+
+BINARY = Binary()
+
+assert_type(BINARY + INT, Binary)
+assert_type(BINARY & INT, Binary)
+assert_type(BINARY / INT, Binary)
+assert_type(BINARY == INT, Binary)
+assert_type(BINARY // INT, Binary)
+assert_type(BINARY >= INT, Binary)
+assert_type(BINARY > INT, Binary)
+assert_type(BINARY <= INT, Binary)
+assert_type(BINARY << INT, Binary)
+assert_type(BINARY < INT, Binary)
+assert_type(BINARY % INT, Binary)
+assert_type(BINARY * INT, Binary)
+assert_type(BINARY != INT, Binary)
+assert_type(BINARY | INT, Binary)
+assert_type(BINARY**INT, Binary)
+assert_type(BINARY >> INT, Binary)
+assert_type(BINARY - INT, Binary)
+assert_type(BINARY ^ INT, Binary)
+
+assert_type(INT + BINARY, Binary)
+assert_type(INT & BINARY, Binary)
+assert_type(INT / BINARY, Binary)
+assert_type(INT == BINARY, bool)
+assert_type(INT // BINARY, Binary)
+assert_type(INT >= BINARY, Binary)
+assert_type(INT > BINARY, Binary)
+assert_type(INT <= BINARY, Binary)
+assert_type(INT << BINARY, Binary)
+assert_type(INT < BINARY, Binary)
+assert_type(INT % BINARY, Binary)
+assert_type(INT * BINARY, Binary)
+assert_type(INT != BINARY, bool)
+assert_type(INT | BINARY, Binary)
+assert_type(INT**BINARY, Binary)
+assert_type(INT >> BINARY, Binary)
+assert_type(INT - BINARY, Binary)
+assert_type(INT ^ BINARY, Binary)
+
+assert_type(BINARY + FLOAT, Binary)
+assert_type(BINARY & FLOAT, Binary)
+assert_type(BINARY / FLOAT, Binary)
+assert_type(BINARY == FLOAT, Binary)
+assert_type(BINARY // FLOAT, Binary)
+assert_type(BINARY >= FLOAT, Binary)
+assert_type(BINARY > FLOAT, Binary)
+assert_type(BINARY <= FLOAT, Binary)
+assert_type(BINARY << FLOAT, Binary)
+assert_type(BINARY < FLOAT, Binary)
+assert_type(BINARY % FLOAT, Binary)
+assert_type(BINARY * FLOAT, Binary)
+assert_type(BINARY != FLOAT, Binary)
+assert_type(BINARY | FLOAT, Binary)
+assert_type(BINARY**FLOAT, Binary)
+assert_type(BINARY >> FLOAT, Binary)
+assert_type(BINARY - FLOAT, Binary)
+assert_type(BINARY ^ FLOAT, Binary)
+
+assert_type(FLOAT + BINARY, Binary)
+assert_type(FLOAT & BINARY, Binary)
+assert_type(FLOAT / BINARY, Binary)
+assert_type(FLOAT == BINARY, bool)
+assert_type(FLOAT // BINARY, Binary)
+assert_type(FLOAT >= BINARY, Binary)
+assert_type(FLOAT > BINARY, Binary)
+assert_type(FLOAT <= BINARY, Binary)
+assert_type(FLOAT << BINARY, Binary)
+assert_type(FLOAT < BINARY, Binary)
+assert_type(FLOAT % BINARY, Binary)
+assert_type(FLOAT * BINARY, Binary)
+assert_type(FLOAT != BINARY, bool)
+assert_type(FLOAT | BINARY, Binary)
+assert_type(FLOAT**BINARY, Binary)
+assert_type(FLOAT >> BINARY, Binary)
+assert_type(FLOAT - BINARY, Binary)
+assert_type(FLOAT ^ BINARY, Binary)
+
+assert_type(BINARY + BOOL, Binary)
+assert_type(BINARY & BOOL, Binary)
+assert_type(BINARY / BOOL, Binary)
+assert_type(BINARY == BOOL, Binary)
+assert_type(BINARY // BOOL, Binary)
+assert_type(BINARY >= BOOL, Binary)
+assert_type(BINARY > BOOL, Binary)
+assert_type(BINARY <= BOOL, Binary)
+assert_type(BINARY << BOOL, Binary)
+assert_type(BINARY < BOOL, Binary)
+assert_type(BINARY % BOOL, Binary)
+assert_type(BINARY * BOOL, Binary)
+assert_type(BINARY != BOOL, Binary)
+assert_type(BINARY | BOOL, Binary)
+assert_type(BINARY**BOOL, Binary)
+assert_type(BINARY >> BOOL, Binary)
+assert_type(BINARY - BOOL, Binary)
+assert_type(BINARY ^ BOOL, Binary)
+
+assert_type(BOOL + BINARY, Binary)
+assert_type(BOOL & BINARY, Binary)
+assert_type(BOOL / BINARY, Binary)
+assert_type(BOOL == BINARY, bool)
+assert_type(BOOL // BINARY, Binary)
+assert_type(BOOL >= BINARY, Binary)
+assert_type(BOOL > BINARY, Binary)
+assert_type(BOOL <= BINARY, Binary)
+assert_type(BOOL << BINARY, Binary)
+assert_type(BOOL < BINARY, Binary)
+assert_type(BOOL % BINARY, Binary)
+assert_type(BOOL * BINARY, Binary)
+assert_type(BOOL != BINARY, bool)
+assert_type(BOOL | BINARY, Binary)
+assert_type(BOOL**BINARY, Binary)
+assert_type(BOOL >> BINARY, Binary)
+assert_type(BOOL - BINARY, Binary)
+assert_type(BOOL ^ BINARY, Binary)
+
+# Tensor operators whose types could be improved
+# This is the "diff" of the first and second sections.
+
+assert_type(BOOL // TENSOR, Any)
+assert_type(FLOAT // TENSOR, Any)
+assert_type(INT // TENSOR, Any)
+assert_type(TENSOR // BOOL, Any)
+assert_type(TENSOR // FLOAT, Any)
+assert_type(TENSOR // INT, Any)
+assert_type(TENSOR // TENSOR, Any)
+
+assert_type(BOOL**TENSOR, Any)
+assert_type(FLOAT**TENSOR, Any)
+assert_type(INT**TENSOR, Any)
+assert_type(TENSOR**BOOL, Any)
+assert_type(TENSOR**FLOAT, Any)
+assert_type(TENSOR**INT, Any)
+assert_type(TENSOR**TENSOR, Any)
+
+assert_type(BOOL - TENSOR, Any)
+assert_type(FLOAT - TENSOR, Any)
+assert_type(INT - TENSOR, Any)
+
+assert_type(BOOL / TENSOR, Any)
+assert_type(FLOAT / TENSOR, Any)
+assert_type(INT / TENSOR, Any)
+
+assert_type(BOOL % TENSOR, Any)
+assert_type(FLOAT % TENSOR, Any)
+assert_type(INT % TENSOR, Any)
+
+assert_type(BOOL << TENSOR, Any)
+assert_type(FLOAT << TENSOR, Any)
+assert_type(INT << TENSOR, Any)
+
+assert_type(BOOL >> TENSOR, Any)
+assert_type(FLOAT >> TENSOR, Any)
+assert_type(INT >> TENSOR, Any)
+
+assert_type(FLOAT & TENSOR, Tensor)  # type: ignore[operator]
+assert_type(INT & TENSOR, Any)  # type: ignore[operator]
+
+assert_type(FLOAT | TENSOR, Tensor)  # type: ignore[operator]
+assert_type(INT | TENSOR, Any)  # type: ignore[operator]
+
+assert_type(FLOAT ^ TENSOR, Tensor)  # type: ignore[operator]
+assert_type(INT ^ TENSOR, Any)  # type: ignore[operator]
