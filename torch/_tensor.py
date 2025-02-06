882087d5b3093054a99ac3e4d341e1bcf57390a0
gasoonjia <gasoonjia@icloud.com>
2025-02-06 13:48:59 -0800

    [dim order]  solve broken doc
    Differential Revision: [D69265340](https://our.internmc.facebook.com/intern/diff/D69265340/)
    
    [ghstack-poisoned]

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -1517,6 +1517,7 @@ class Tensor(torch._C.TensorBase):
         Args:
             ambiguity_check (bool or List[torch.memory_format]): The check method for ambiguity of dim order.
 
+        Examples::
             >>> torch.empty((2, 3, 5, 7)).dim_order()
             (0, 1, 2, 3)
             >>> torch.empty((2, 3, 5, 7)).transpose(1, 2).dim_order()
