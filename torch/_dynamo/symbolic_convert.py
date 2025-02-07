5683041b196de099050ca085db1970b34f388919
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-02-07 14:55:20 -0300

    Implement `generator.close()` (#144423)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144423
    Approved by: https://github.com/zou3519
    ghstack dependencies: #141055, #144421, #144422

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -3339,8 +3339,6 @@ class InliningInstructionTranslator(InstructionTranslatorBase):
             ):
                 assert isinstance(self, InliningGeneratorInstructionTranslator)
                 # When the generator returns None, we raise StopIteration
-                r = self.symbolic_result
-                assert r.as_python_constant() is None
                 exc.raise_observed_exception(StopIteration, self)
             else:
                 return self.symbolic_result
