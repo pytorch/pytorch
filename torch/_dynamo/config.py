f7be6546ecbdcd7cf0dea9712a86716e7bbc4067
Simon Fan <xmfan@fb.com>
2025-02-07 10:33:32 -0800

    [dynamo] check for incompatible configs (#146513)
    internal: https://fb.workplace.com/groups/1075192433118967/permalink/1599802033991335/
    
    Assuming flags don't change during compilation, we shouldn't allow incompatible configs to be set at torch.compile wrap time.
    
    Not in this PR: For flags that need to change during compilation, we'd have to be strict about where they can be used in the compile lifecycle
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146513
    Approved by: https://github.com/williamwen42
    
    Co-authored-by: Gabriel Ferns <gabeferns@meta.com>

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -52,6 +52,7 @@ skip_code_recursive_on_recompile_limit_hit = True
 # raise a hard error if cache limit is hit.  If you are on a model where you
 # know you've sized the cache correctly, this can help detect problems when
 # you regress guards/specialization.  This works best when recompile_limit = 1.
+# This flag is incompatible with: suppress_errors.
 # [@compile_ignored: runtime_behaviour]
 fail_on_recompile_limit_hit = False
 
@@ -164,6 +165,7 @@ traceable_tensor_subclasses: set[type[Any]] = set()
 # This is a good way to get your model to work one way or another, but you may
 # lose optimization opportunities this way.  Devs, if your benchmark model is failing
 # this way, you should figure out why instead of suppressing it.
+# This flag is incompatible with: fail_on_recompile_limit_hit.
 suppress_errors = bool(os.environ.get("TORCHDYNAMO_SUPPRESS_ERRORS", False))
 
 # Record and write an execution record of the current frame to a file
