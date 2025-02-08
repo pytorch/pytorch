dc5b78c855681b5e28c932cbe5e402a317160a3a
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2025-02-07 16:30:53 -0800

    Testing
    This reverts commit 5cd5b4d2d54c0220b92ee488dd36d789c9b60af3.
    
    [ghstack-poisoned]

--- .ci/pytorch/smoke_test/smoke_test.py
+++ .ci/pytorch/smoke_test/smoke_test.py
@@ -6,7 +6,6 @@ import re
 import subprocess
 import sys
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 
 import torch
 import torch._dynamo
@@ -160,32 +159,6 @@ def test_cuda_runtime_errors_captured() -> None:
             raise e
     if cuda_exception_missed:
         raise RuntimeError("Expected CUDA RuntimeError but have not received!")
-
-
-def test_cuda_gds_errors_captured() -> None:
-    major_version = int(torch.version.cuda.split(".")[0])
-    minor_version = int(torch.version.cuda.split(".")[1])
-
-    if major_version < 12 or (major_version == 12 and minor_version < 6):
-        print("CUDA version is not supported for GDS smoke test")
-        return
-
-    cuda_exception_missed = True
-    try:
-        print("Testing test_cuda_gds_errors_captured")
-        with NamedTemporaryFile() as f:
-            torch.cuda.gds._GdsFile(f.name, os.O_CREAT | os.O_RDWR)
-    except RuntimeError as e:
-        expected_error = "cuFileHandleRegister failed"
-        if re.search(expected_error, f"{e}"):
-            print(f"Caught CUDA exception with success: {e}")
-            cuda_exception_missed = False
-        else:
-            raise e
-    if cuda_exception_missed:
-        raise RuntimeError(
-            "Expected cuFileHandleRegister failed RuntimeError but have not received!"
-        )
 
 
 def smoke_test_cuda(
@@ -408,7 +381,6 @@ def main() -> None:
     test_numpy()
     if is_cuda_system:
         test_linalg("cuda")
-        test_cuda_gds_errors_captured()
 
     if options.package == "all":
         smoke_test_modules()
