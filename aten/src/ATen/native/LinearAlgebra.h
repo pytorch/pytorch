49c0f7e297c30315a624e2c74cf73345c2a70cda
Isalia20 <irakli.salia854@gmail.com>
2025-02-08 14:23:29 +0400

    fix inverse bug for N>1024

--- aten/src/ATen/native/LinearAlgebra.h
+++ aten/src/ATen/native/LinearAlgebra.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ATen/core/Tensor.h>
 #include <ATen/native/DispatchStub.h>
 
 namespace c10 {
@@ -14,4 +15,8 @@ namespace at::native {
 
 using addr_fn = void (*)(TensorIterator &, const Scalar& beta, const Scalar& alpha);
 DECLARE_DISPATCH(addr_fn, addr_stub)
+
+namespace mps {
+    TORCH_API void linalg_solve_out_mps_impl(const Tensor& A, const Tensor& B, bool left, bool check_errors, const Tensor& result, const Tensor& LU, const Tensor& pivots, const Tensor& info);
+}
 } // namespace at::native
