732a58f9a282b2eca1b8dd60a12ef00ad17aa5c7
atalman <atalman@fb.com>
2025-02-09 10:42:55 -0800

    fix

--- .github/scripts/generate_binary_build_matrix.py
+++ .github/scripts/generate_binary_build_matrix.py
@@ -187,7 +187,7 @@ DEFAULT_TAG = os.getenv("RELEASE_VERSION_TAG", "main")
 
 WHEEL_CONTAINER_IMAGES = {
     **{
-        gpu_arch: f"308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/manylinux2_28-builder-cuda{gpu_arch}:d5060598229ee1cf649af7f40d9a7e1435cec172"
+        gpu_arch: f"308535385114.dkr.ecr.us-east-1.amazonaws.com/pytorch/manylinux2_28-builder-cuda{gpu_arch}:fd2ca1a925af628094b127ac4132038efa47371f"
         for gpu_arch in CUDA_ARCHES
     },
     **{
