a076b383fc76cc6d77aa67d172d46ff9d08aaadc
angelayi <yiangela7@gmail.com>
2025-02-11 00:35:45 -0800

    Update base for Update on "[export] Dedup expression_created logs"
    [ghstack-poisoned]

--- torch/export/_draft_export.py
+++ torch/export/_draft_export.py
@@ -211,8 +211,8 @@ class LogRecord:
             return hash((key, data["op"]))
         elif key == "mismatched_fake_kernel":
             return hash((key, data["op"], data["reason"]))
-        elif key == "propagate_real_tensors":
-            return hash((key, json.dumps(data["user_stack"])))
+        elif key == "propagate_real_tensors_provenance":
+            return hash((key, json.dumps(data["stack"])))
         elif key == "create_unbacked_symbol":
             return hash((key, json.dumps(data["user_stack"])))
 
