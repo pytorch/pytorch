5548df9541fe76be615c380a18329bc5c2bc3f56
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-02-11 03:17:59 +0000

    Revert "Exclude upsample_bilinear2d.vec from default core ATen decomposition table (#141791)"
    This reverts commit 3d604b17d91b928c850ded83b2ec25ea066bb3f6.
    
    Reverted https://github.com/pytorch/pytorch/pull/141791 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/141791#issuecomment-2649717140))

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -11881,48 +11881,6 @@ class GraphModule(torch.nn.Module):
             ]
             self.assertEqual(len(shift_op), 1)
 
-    def test_default_decomposition_core_cia_ops(self):
-        """
-        Verify that core ATen ops with Composite Implicit Autograd dispatch are not
-        decomposed by default.
-        """
-
-        # TODO Add avg_pool1d, and adaptive_avg_pool1d when ready.
-        # See issue #116684.
-        core_cia_ops = {
-            "torch.ops.aten.upsample_bilinear2d.vec": (
-                torch.ops.aten.upsample_bilinear2d.vec,
-                {
-                    "align_corners": False,
-                    "scale_factors": [2, 2],
-                    "output_size": None,
-                },
-            ),
-            "torch.ops.aten.upsample_nearest2d.vec": (
-                torch.ops.aten.upsample_nearest2d.vec,
-                {
-                    "scale_factors": [2, 2],
-                    "output_size": None,
-                },
-            ),
-        }
-
-        for op_name, (op, kwargs) in core_cia_ops.items():
-
-            class M(torch.nn.Module):
-                def forward(self, x):
-                    return op(x, **kwargs)
-
-            ep = export(M(), (torch.randn(2, 3, 4, 5),))
-            FileCheck().check_count(op_name, 1, exactly=True).run(ep.graph_module.code)
-
-            decomp_table = default_decompositions()
-
-            ep = ep.run_decompositions(
-                decomp_table=decomp_table,
-            )
-            FileCheck().check_count(op_name, 1, exactly=True).run(ep.graph_module.code)
-
 
 @unittest.skipIf(not torchdynamo.is_dynamo_supported(), "dynamo isn't support")
 class TestOneOffModelExportResult(TestCase):
@@ -12528,30 +12486,30 @@ class TestExportCustomClass(TorchTestCase):
             torch.distributed.destroy_process_group()
 
     def test_preserve_cia_op(self):
-        class StaticResizeTrilinear2dModule(torch.nn.Module):
+        class StaticResizeBilinear2dModule(torch.nn.Module):
             def forward(self, x):
                 a = torch.nn.functional.interpolate(
                     x,
-                    size=(x.shape[2] * 2, x.shape[3] * 3, x.shape[4] * 4),
-                    mode="trilinear",
+                    size=(x.shape[2] * 2, x.shape[3] * 3),
+                    mode="bilinear",
                     align_corners=False,
                     antialias=False,
                 )
                 return a
 
-        ep = export(StaticResizeTrilinear2dModule(), (torch.randn(2, 3, 4, 5, 6),))
+        ep = export(StaticResizeBilinear2dModule(), (torch.randn(2, 3, 4, 5),))
         FileCheck().check_count(
-            "torch.ops.aten.upsample_trilinear3d.vec", 1, exactly=True
+            "torch.ops.aten.upsample_bilinear2d.vec", 1, exactly=True
         ).run(ep.graph_module.code)
 
         decomp_table = default_decompositions()
-        del decomp_table[torch.ops.aten.upsample_trilinear3d.vec]
+        del decomp_table[torch.ops.aten.upsample_bilinear2d.vec]
         ep = ep.run_decompositions(
             decomp_table=decomp_table,
         )
 
         FileCheck().check_count(
-            "torch.ops.aten.upsample_trilinear3d.vec", 1, exactly=True
+            "torch.ops.aten.upsample_bilinear2d.vec", 1, exactly=True
         ).run(ep.graph_module.code)
 
 
