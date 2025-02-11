904196bc70c5987c11e532a3681be90230ad959b
Ryan Guo <ryanguo99@meta.com>
2025-02-11 15:45:48 -0800

    Update
    [ghstack-poisoned]

--- test/dynamo/test_decorators.py
+++ test/dynamo/test_decorators.py
@@ -503,7 +503,7 @@ Found a mark_traceable decorator to a function which is created inside the paren
             self.assertFalse(True)  # must raise error before this
         except torch._dynamo.exc.Unsupported as e:
             msg = """
-Attempting to call a `mark_traceable`-ed function with arguments that contain a value of type <class 'dynamo.test_decorators.DecoratorTests.test_mark_traceable_custom_class_error.<locals>.Point'>, please use one of the following to register the type with pytree:
+Attempting to call a `mark_traceable`-ed function with arguments that contain a value of type <class '__main__.DecoratorTests.test_mark_traceable_custom_class_error.<locals>.Point'>, please use one of the following to register the type with pytree:
   * `torch.utils._pytree.register_pytree_node`
 """  # NOQA: B950
             self.assertIn(msg, str(e))
