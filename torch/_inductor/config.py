076708b78acbfaf40edd65e3d72bae4ccca35234
eellison <elias.ellison@gmail.com>
2025-02-12 15:41:38 -0800

    Update
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -516,7 +516,7 @@ benchmark_epilogue_fusion = (
 )
 
 # Take how many of the top triton kernels to benchmark epilogue
-max_epilogue_benchmarked_choices = 1
+max_epilogue_benchmarked_choices = 3
 
 # how many nodes to allow into a single fusion
 max_fusion_size = 64
