8d05ab4712f162761b53016ff44fe98ff3ca6e96
Yang Wang <elainewy@meta.com>
2025-02-11 16:38:11 -0800

    add fixcfo

--- tools/stats/upload_stats_lib.py
+++ tools/stats/upload_stats_lib.py
@@ -165,7 +165,7 @@ def upload_to_s3(
         ContentEncoding="gzip",
         ContentType="application/json",
     )
-    print("Done! Writing document to S3 {bucket_name}/{key} ")
+    print(f"Done! Writing document to S3 {bucket_name}/{key} ")
 
 
 def read_from_s3(
