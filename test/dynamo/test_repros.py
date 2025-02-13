312c13967f42cbe6e1d60fcc5d3488703cd7dd82
atalman <atalman@fb.com>
2025-02-13 12:56:37 -0800

    fix_ncc_check

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -6369,7 +6369,7 @@ def forward(self, s0 : torch.SymInt, s1 : torch.SymInt, L_x_ : torch.Tensor):
     @unittest.skipIf(not TEST_CUDA, "test requires CUDA")
     @unittest.skipIf(not dist.is_available(), "test requires distributed")
     # Failing with ncc update 2.25.1 : https://github.com/pytorch/pytorch/issues/147141
-    @dist.requires_nccl_version((2, 21), "Failing with nccl 2.25.1")
+    @dist.requires_nccl_version_less_or_equal((2, 21), "Failing with nccl 2.25.1")
     def test_ddp_checkpoint(self):
         # https://github.com/pytorch/pytorch/issues/144035
         DIM = 256
