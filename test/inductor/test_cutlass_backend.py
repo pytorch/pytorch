72050cb08dfe82c71418585f9f952747c186a160
Henry Tsang <henrylhtsang@meta.com>
2025-02-13 11:10:50 -0800

    test 2 ghstack
    [ghstack-poisoned]

--- test/inductor/test_cutlass_backend.py
+++ test/inductor/test_cutlass_backend.py
@@ -71,9 +71,9 @@ class TestCutlassBackend(TestCase):
             os.environ["INDUCTOR_TEST_DISABLE_FRESH_CACHE"] = "1"
             super().setUp()
         finally:
-            os.environ[
-                "INDUCTOR_TEST_DISABLE_FRESH_CACHE"
-            ] = old_disable_fresh_cache_envvar
+            os.environ["INDUCTOR_TEST_DISABLE_FRESH_CACHE"] = (
+                old_disable_fresh_cache_envvar
+            )
         torch.random.manual_seed(1234)
 
     def tearDown(self):
@@ -168,6 +168,8 @@ class TestCutlassBackend(TestCase):
         max_autotune_gemm_backends = "CUTLASS"
 
         test
+
+        test2
 
         class MyModel(torch.nn.Module):
             def forward(self, a, b):
