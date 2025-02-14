10bc8f25b2845d21f90f0ca32b994e451584590e
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-13 20:58:02 -0800

    [MPS][BE] Migrate polar to use functor (#147184)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147184
    Approved by: https://github.com/dcci
    ghstack dependencies: #147182, #147183

--- aten/src/ATen/native/mps/operations/BinaryKernel.mm
+++ aten/src/ATen/native/mps/operations/BinaryKernel.mm
@@ -141,7 +141,7 @@ Tensor& polar_out_mps(const Tensor& abs, const Tensor& angle, Tensor& output) {
   auto output_as_real = at::view_as_real(output).select(output.dim(), 0);
   auto iter = TensorIteratorConfig().add_output(output_as_real).add_input(abs).add_input(angle).build();
 
-  mps::binary_mps_impl(iter, "polar", false);
+  mps::binary_mps_impl(iter, "polar");
   return output;
 }
 
