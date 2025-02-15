e95fbb9a46fbef208a82b35bdb77dc5463223776
henrylhtsang <henrylhtsang@meta.com>
2025-02-14 18:23:35 -0800

    check if config.autotune_fallback_to_aten before using aten as a fallback
    ghstack-source-id: adfd926a15f44dfa619bb887661ad37ee62c4fc5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147148

--- torch/_inductor/kernel/mm_common.py
+++ torch/_inductor/kernel/mm_common.py
@@ -11,10 +11,26 @@ from torch._inductor.virtualized import V
 from .. import config as inductor_config
 from ..codegen.wrapper import PythonWrapperCodegen
 from ..ir import Layout
-from ..utils import ceildiv as cdiv, get_num_sms, TMA_DESCRIPTOR_SIZE
+from ..utils import (
+    ceildiv as cdiv,
+    get_num_sms,
+    TMA_DESCRIPTOR_SIZE,
+    use_aten_gemm_kernels,
+)
 
 
 log = logging.getLogger(__name__)
+
+
+def should_fallback_to_aten(choices) -> bool:
+    fallback_to_aten: bool = (
+        len(choices) == 0
+        and not use_aten_gemm_kernels()
+        and inductor_config.autotune_fallback_to_aten
+    )
+    if fallback_to_aten:
+        log.warning("No choices for GEMM, using ATen backend as fallback")
+    return fallback_to_aten
 
 
 def mm_grid(m, n, meta):
