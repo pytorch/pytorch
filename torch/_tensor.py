fc4e570a983d458666e9087a4e8afbea1d930247
Tom Ritchford <tom@swirly.com>
2025-02-17 14:23:21 +0000

    Update
    [ghstack-poisoned]

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -29,19 +29,18 @@ from torch.overrides import (
 
 
 _P = ParamSpec("_P")
-_T = TypeVar("_T")
+_TensorLike = TypeVar("_TensorLike")
 
 
 def _handle_torch_function_and_wrap_type_error_to_not_implemented(
-    f: Callable[Concatenate[_T, _P], "Tensor"],
-) -> Callable[Concatenate[_T, _P], "Tensor"]:
+    f: Callable[Concatenate[_TensorLike, _P], "Tensor"],
+) -> Callable[Concatenate[_TensorLike, _P], "Tensor"]:
     @functools.wraps(f)
-    def wrapped(self: _T, *args: _P.args, **kwargs: _P.kwargs) -> "Tensor":
+    def wrapped(self: _TensorLike, *args: _P.args, **kwargs: _P.kwargs) -> "Tensor":
         try:
-            if has_torch_function((self,) + args):
-                return handle_torch_function(
-                    wrapped, (self,) + args, self, *args, **kwargs
-                )
+            sargs = self, *args
+            if has_torch_function(sargs):
+                return handle_torch_function(wrapped, sargs, *sargs, **kwargs)
             return f(self, *args, **kwargs)
         except TypeError:
             return NotImplemented
