a23decf7c96f32c55724f7d0003a40391596d34b
Nikita Shulga <nikita.shulga@gmail.com>
2025-02-16 23:44:03 -0800

    [MPS][BE] Turn `exec_unary_kernel` as MetalShaderLibrary method (#147299)
    And delete duplicate implementations from SpecialOps and UnaryKernel.
    Change input and output arguments order for SpecialOps kernels to match those of UnaryOps
    
    Fixes https://github.com/pytorch/pytorch/issues/146770
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147299
    Approved by: https://github.com/dcci
    ghstack dependencies: #147296, #147297

--- aten/src/ATen/native/mps/operations/UnaryKernel.mm
+++ aten/src/ATen/native/mps/operations/UnaryKernel.mm
@@ -14,71 +14,24 @@ static auto& lib = mps::MetalShaderLibrary::getBundledLibrary();
 #include <ATen/native/mps/UnaryKernel_metallib.h>
 #endif
 
-static void exec_unary_kernel(TensorIteratorBase& iter,
-                              const std::string& name,
-                              std::optional<int64_t> extra = std::nullopt) {
-  auto inputTensor = iter.input(0).contiguous();
-  auto outputTensor = iter.output(0);
-  bool needs_output_copy = false;
-  uint32_t length = outputTensor.numel();
-  if (length == 0) {
-    return;
-  }
-  using namespace mps;
-  @autoreleasepool {
-    id<MTLComputePipelineState> cplState = nil;
-    if (c10::isComplexType(inputTensor.scalar_type())) {
-      auto scalarStr = inputTensor.scalar_type() == kComplexFloat ? "float" : "half";
-      cplState = lib.getPipelineStateForFunc(fmt::format("{}_complex_{}_{}", name, scalarStr, scalarStr));
-    } else {
-      cplState = lib.getPipelineStateForFunc(
-          fmt::format("{}_{}_{}", name, scalarToMetalTypeString(outputTensor), scalarToMetalTypeString(inputTensor)));
-    }
-
-    if (!outputTensor.is_contiguous()) {
-      outputTensor = outputTensor.contiguous();
-      needs_output_copy = true;
-    }
-
-    MPSStream* mpsStream = getCurrentMPSStream();
-    dispatch_sync(mpsStream->queue(), ^() {
-      id<MTLComputeCommandEncoder> computeEncoder = mpsStream->commandEncoder();
-
-      getMPSProfiler().beginProfileKernel(cplState, name, {inputTensor});
-
-      [computeEncoder setComputePipelineState:cplState];
-      mtl_setArgs(computeEncoder, outputTensor, inputTensor);
-      if (extra) {
-        mtl_setBytes(computeEncoder, *extra, 2);
-      }
-      mtl_dispatch1DJob(computeEncoder, cplState, length);
-
-      getMPSProfiler().endProfileKernel(cplState);
-    });
-  }
-  if (needs_output_copy) {
-    iter.output(0).copy_(outputTensor);
-  }
-}
-
 static void erfinv_kernel(TensorIteratorBase& iter) {
-  exec_unary_kernel(iter, "erfinv");
+  lib.exec_unary_kernel(iter, "erfinv");
 }
 
 static void exp_kernel(TensorIteratorBase& iter) {
-  exec_unary_kernel(iter, "exp");
+  lib.exec_unary_kernel(iter, "exp");
 }
 
 static void sinc_kernel(TensorIteratorBase& iter) {
-  exec_unary_kernel(iter, "sinc");
+  lib.exec_unary_kernel(iter, "sinc");
 }
 
 static void tanh_kernel(TensorIteratorBase& iter) {
-  exec_unary_kernel(iter, "tanh");
+  lib.exec_unary_kernel(iter, "tanh");
 }
 
 static void round_decimals_kernel(TensorIteratorBase& iter, int64_t decimals) {
-  exec_unary_kernel(iter, "round_decimals", decimals);
+  lib.exec_unary_kernel(iter, "round_decimals", decimals);
 }
 
 REGISTER_DISPATCH(exp_stub, exp_kernel);
