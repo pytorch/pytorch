bcf84ff5eeffe8583ca90ccfc4dbbe27b627a0d9
fulvius31 <mail@alessandrosangiorgi.net>
2025-02-17 13:00:39 -0600

    Better naming for triton cache ref on best_config

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -968,10 +968,9 @@ class CachingAutotuner(KernelInterface):
             launcher.shared,
         )
 
-        triton_binary_hash = launcher.cache_hash
 
         if self.save_cache_hook:
-            self.save_cache_hook(launcher.config, self.autotune_time_taken_ns, triton_binary_hash=triton_binary_hash,)
+            self.save_cache_hook(launcher.config, self.autotune_time_taken_ns, triton_cache_hash=launcher.cache_hash,)
 
     def save_gpu_kernel(self, stream, launcher):
         key = self.inductor_meta.get("kernel_name", None)  # unique kernel name
