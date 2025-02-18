49746bc180445ace64ba7183cb54d9580a39addf
FFFrog <ljw1101.vip@gmail.com>
2025-02-18 14:04:05 +0800

    Unify all sympy versions to avoid conflicts within PyTorch (#147197)
    As the title stated.
    
    There are some tiny diffrences between 1.13.1 and 1.13.3:
    1.13.1:
    https://github.com/sympy/sympy/blob/2e489cf4b1438ae134ba98a44a80cc9add1306b0/sympy/core/numbers.py#L1591
    
    1.13.3:
    https://github.com/sympy/sympy/blob/b4ce69ad5d40e4e545614b6c76ca9b0be0b98f0b/sympy/core/numbers.py#L1591
    
    **Previous PR:**
    https://github.com/pytorch/pytorch/pull/143908
    
    **ISSUE Related:**
    https://github.com/pytorch/pytorch/issues/147144
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147197
    Approved by: https://github.com/malfet

--- setup.py
+++ setup.py
@@ -1120,7 +1120,7 @@ def main():
         "filelock",
         "typing-extensions>=4.10.0",
         'setuptools ; python_version >= "3.12"',
-        'sympy==1.13.1 ; python_version >= "3.9"',
+        "sympy==1.13.3",
         "networkx",
         "jinja2",
         "fsspec",
