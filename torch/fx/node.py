863640184f2cfdf62db0cfdaf826c46c9285c844
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-02-19 04:30:08 +0800

    Update
    [ghstack-poisoned]

--- torch/fx/node.py
+++ torch/fx/node.py
@@ -921,7 +921,7 @@ def map_aggregate(a: ArgumentT, fn: Callable[[Argument], Argument]) -> ArgumentT
     elif isinstance(a, list):
         result = immutable_list([map_aggregate(elem, fn) for elem in a])
     elif isinstance(a, dict):
-        return immutable_dict({k: map_aggregate(v, fn) for k, v in a.items()})
+        return immutable_dict({k: map_aggregate(v, fn) for k, v in a.items()})  # type: ignore[return-value]
     elif isinstance(a, slice):
         result = slice(
             map_aggregate(a.start, fn),
