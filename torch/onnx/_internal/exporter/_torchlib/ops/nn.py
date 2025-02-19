480805e3044453351d5ff615f3a5954660c001a1
Justin Chu <justinchuby@users.noreply.github.com>
2025-02-19 15:51:26 -0800

    Update (base update)
    [ghstack-poisoned]

--- torch/onnx/_internal/exporter/_torchlib/ops/nn.py
+++ torch/onnx/_internal/exporter/_torchlib/ops/nn.py
@@ -1,6 +1,6 @@
 """torch.ops.aten operators under the `nn` module."""
 
-# mypy: disable-error-code="misc,arg-type,type-arg,valid-type,assignment,return-value,type-var,operator,no-untyped-def,index"
+# mypy: disable-error-code="misc,arg-type,type-arg,valid-type,assignment,return-value,type-var,operator,no-untyped-def,index,list-item"
 # ruff: noqa: TCH001,TCH002
 # flake8: noqa
 
