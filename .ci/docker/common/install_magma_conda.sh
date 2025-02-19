85980b6839cd1f05330f188ccf88e30842a6f7d3
atalman <atalman@fb.com>
2025-02-19 12:00:33 -0800

    Remove metion of magma-cuda in readme.md, refactor magma_conda install

--- .ci/docker/common/install_magma_conda.sh
+++ .ci/docker/common/install_magma_conda.sh
@@ -3,24 +3,22 @@
 
 set -eou pipefail
 
-function do_install() {
-    cuda_version_nodot=${1/./}
-    anaconda_python_version=$2
+cuda_version_nodot=${1/./}
+if [[ $# -ge 2 &&  -n "$2" ]]; then
+    anaconda_dir="/opt/conda/envs/py_$2"
+else
+    anaconda_dir="$CONDA_PREFIX"
+fi
 
-    MAGMA_VERSION="2.6.1"
-    magma_archive="magma-cuda${cuda_version_nodot}-${MAGMA_VERSION}-1.tar.bz2"
-
-    anaconda_dir="/opt/conda/envs/py_${anaconda_python_version}"
-    (
-        set -x
-        tmp_dir=$(mktemp -d)
-        pushd ${tmp_dir}
-        curl -OLs https://ossci-linux.s3.us-east-1.amazonaws.com/${magma_archive}
-        tar -xvf "${magma_archive}"
-        mv include/* "${anaconda_dir}/include/"
-        mv lib/* "${anaconda_dir}/lib"
-        popd
-    )
-}
-
-do_install $1 $2
+MAGMA_VERSION="2.6.1"
+magma_archive="magma-cuda${cuda_version_nodot}-${MAGMA_VERSION}-1.tar.bz2"
+(
+    set -x
+    tmp_dir=$(mktemp -d)
+    pushd ${tmp_dir}
+    curl -OLs https://ossci-linux.s3.us-east-1.amazonaws.com/${magma_archive}
+    tar -xvf "${magma_archive}"
+    mv include/* "${anaconda_dir}/include/"
+    mv lib/* "${anaconda_dir}/lib"
+    popd
+)
