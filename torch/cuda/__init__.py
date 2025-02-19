f27cc01ec993b920e3c59fc5ccd746e3c30b1ce8
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2025-02-19 18:30:15 +0000

    [release/2.5] [CP] Respect ROCR_VISIBLE_DEVICES on AMD GPU device discovery (#144026) (#1895)
    Respect ROCR_VISIBLE_DEVICES on AMD GPU device discovery (#142292)
    
    Reland of #140320 after failing test on trunk. Fixes potential
    environment clobbering in test, makes ROCr+HIP devices (if specified
    together) more robust to index errors.
    
    Fixes #140318
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/142292
    Approved by: https://github.com/jataylo, https://github.com/huydhn,
    https://github.com/jeffdaily
    
    Co-authored-by: Jack Taylor <108682042+jataylo@users.noreply.github.com>
    Co-authored-by: Jeff Daily <jeff.daily@amd.com>
    (cherry picked from commit c0d710634fcce172490c3ace0de977829b38bc06)
    
    Co-authored-by: Tal Ben-Nun <tbennun@users.noreply.github.com>
    (cherry picked from commit 23e390c711670095a8a4a64cc01b0c86eb3af04e)
    
    Fixes #ISSUE_NUMBER
    
    Co-authored-by: pytorchbot <soumith+bot@pytorch.org>

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -646,7 +646,25 @@ def _parse_visible_devices() -> Union[List[int], List[str]]:
 
     if torch.version.hip:
         hip_devices = os.getenv("HIP_VISIBLE_DEVICES")
-        if hip_devices is not None:
+        rocr_devices = os.getenv("ROCR_VISIBLE_DEVICES")
+
+        # You must take care if both HIP and ROCR env vars are set as they have
+        # different meanings. Both env vars accept either a list of ints or a
+        # list of UUIDs. The ROCR env var is processed first which then reduces
+        # the number of GPUs that HIP can select from.
+        if rocr_devices is not None:
+            rocr_count = len(rocr_devices.split(","))
+            if hip_devices is not None:
+                # sanity check if both env vars are set
+                if len(hip_devices.split(",")) > rocr_count:
+                    raise RuntimeError(
+                        "HIP_VISIBLE_DEVICES contains more devices than ROCR_VISIBLE_DEVICES"
+                    )
+                # HIP_VISIBLE_DEVICES is preferred over ROCR_VISIBLE_DEVICES
+                var = hip_devices
+            else:
+                return list(range(rocr_count))
+        elif hip_devices is not None:
             var = hip_devices
 
     if var is None:
