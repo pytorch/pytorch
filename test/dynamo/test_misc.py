00d13a2cb8998919d0bd984cc3496c13dc9aaf8f
bobrenjc93 <bobren@meta.com>
2025-02-19 15:57:41 -0800

    Support size oblivious max equation (#147344)
    Addresses https://github.com/pytorch/pytorch/issues/125914 by detecting when we have a sym_max between {0, 1} and a summation of size-like unbacked symints.
    
    The basic idea is max(1, u0 + u1) can be simplified to u0 + u1 if both u0 and u1 are size-like since their value ranges are [2, inf].
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147344
    Approved by: https://github.com/angelayi

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -10773,6 +10773,21 @@ fn
         with self.assertRaisesRegex(RuntimeError, "RelaxedUnspecConstraint"):
             fn(x, y)
 
+    def test_sym_max_unbacked_sizelike_simplification(self):
+        @torch.compile(fullgraph=True, backend="eager")
+        def cf(x):
+            u0, u1 = x.tolist()
+            torch._check_is_size(u0)
+            torch._check_is_size(u1)
+            torch._check(u0 + u1 == 20)
+            if guard_size_oblivious(torch.sym_max(1, u0 + u1) == 20):
+                return torch.tensor(True)
+            else:
+                return torch.tensor(False)
+
+        # Previously would have thrown guard on data dependent
+        cf(torch.tensor([10, 10])).item()
+
     @torch._dynamo.config.patch(capture_scalar_outputs=True)
     def test_guard_size_oblivious(self):
         # This code, in fact, does NOT work in eager
