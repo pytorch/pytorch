0b0da81021e061c021e515bc35d7dc0dbbb05941
Shangdi Yu <shangdiy@meta.com>
2025-02-20 03:33:16 +0000

    Support static method of torchbind attributes in torch.compile with inductor backend (#146927)
    As title.
    
    Many changes adapted from https://github.com/pytorch/pytorch/pull/129537.
    
    Also this diff is only for *static* method of torchbind *attributes*. Some case that's not supported/tested:
    - dynamic torchbind objects
    -  torchbind objects as an input to the module.
    
    Note that in JIT Inductor, the attributes are lifted as inputs. So even if we just have torchbind objects as attributes, they will show up as inputs in the graph.
    
    Example generated python code in torch.compile with inductor backend for the test case in `inductor/test_torchbind.py` (P1730554370):
    
    ```python
    async_compile.wait(globals())
    del async_compile
    
    def call(args):
        arg1_1, arg2_1, arg3_1 = args
        args.clear()
        assert_size_stride(arg1_1, (2, 3), (3, 1))
        assert_size_stride(arg2_1, (2, 3), (3, 1))
        buf2 = empty_strided_cpu((2, 3), (3, 1), torch.float32)
        cpp_fused_add_0(arg1_1, arg2_1, buf2)
        del arg1_1
        del arg2_1
        # Topologically Sorted Source Nodes: [x, takes_foo_tuple_return], Original ATen: [aten.add]
        buf3 = torch.ops._TorchScriptTesting.takes_foo_tuple_return.default(arg3_1, buf2)
        buf4 = buf3[0]
        assert_size_stride(buf4, (2, 3), (3, 1))
        buf5 = buf3[1]
        assert_size_stride(buf5, (2, 3), (3, 1))
        buf6 = buf4; del buf4  # reuse
        cpp_fused_add_1(buf6, buf5)
        del buf5
        # Topologically Sorted Source Nodes: [y, b], Original ATen: [aten.add]
        buf7 = torch.ops._TorchScriptTesting.takes_foo.default(arg3_1, buf6)
        del buf3
        del buf6
        buf8 = buf7
        assert_size_stride(buf8, (2, 3), (3, 1))
        # Topologically Sorted Source Nodes: [c], Original ATen: []
        buf9 = torch.ops.higher_order.call_torchbind(arg3_1, 'add_tensor', buf2)
        del arg3_1
        del buf7
        buf10 = buf9
        assert_size_stride(buf10, (2, 3), (3, 1))
        del buf9
        buf11 = buf2; del buf2  # reuse
        cpp_fused_add_2(buf11, buf8, buf10)
        return (buf11, )
    
    def benchmark_compiled_module(times=10, repeat=10):
        from torch._dynamo.testing import rand_strided
        from torch._inductor.utils import print_performance
        arg1_1 = rand_strided((2, 3), (3, 1), device='cpu', dtype=torch.float32)
        arg2_1 = rand_strided((2, 3), (3, 1), device='cpu', dtype=torch.float32)
        import pickle
        global arg3_1
        arg3_1 = pickle.loads(b'\x80\x04\x95[\x00\x00\x00\x00\x00\x00\x00\x8c\x05torch\x94\x8c\x0cScriptObject\x94\x93\x94)\x81\x94]\x94(K\nK\x14e\x8c0__torch__.torch.classes._TorchScriptTesting._Foo\x94\x86\x94b.')
        fn = lambda: call([arg1_1, arg2_1, arg3_1])
        return print_performance(fn, times=times, repeat=repeat)
    
    if __name__ == "__main__":
        from torch._inductor.wrapper_benchmark import compiled_module_main
        compiled_module_main('None', benchmark_compiled_module)
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146927
    Approved by: https://github.com/angelayi

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -4952,6 +4952,7 @@ class ExternKernel(InputsKernel):
         args_flat, args_spec = pytree.tree_flatten(binded_args)
 
         is_arg_tensor = []
+        # tensor_args can be either tensor or torchbind objects
         tensor_args = []
         non_tensor_args: list[Any] = []
         for arg in args_flat:
@@ -5001,6 +5002,8 @@ class ExternKernel(InputsKernel):
                 and x.get_name() in V.graph.torchbind_constants
             ):
                 example_args.append(V.graph.torchbind_constants[x.get_name()])
+            elif isinstance(x, TorchBindObject):
+                example_args.append(x.get_real_obj())
             else:
                 example_args.append(ir_node_to_tensor(x, guard_shape=True))
 
@@ -6563,12 +6566,14 @@ class FallbackKernel(ExternKernelAlloc):
             self.get_kwargs_value(key, **kwargs)
             for key in self.ordered_kwargs_for_cpp_kernel
         ]
+        target = self.op_overload
+
         if not V.graph.aot_mode:
             # No need to serialize in the cpp wrapper JIT mode
             return [*args, *ordered_kwargs]
 
         serializer = GraphModuleSerializer(None, None)  # type: ignore[arg-type]
-        named_arguments = serializer.serialize_inputs(self.op_overload, args, kwargs)
+        named_arguments = serializer.serialize_inputs(target, args, kwargs)
 
         # serialize_outputs
         def handle_single_output(return_type, output):  # type: ignore[no-untyped-def]
@@ -6594,7 +6599,6 @@ class FallbackKernel(ExternKernelAlloc):
             else:
                 raise RuntimeError(f"Unsupported return type {type(return_type)}")
 
-        target = self.op_overload
         returns = target._schema.returns  # type: ignore[union-attr]
         if len(returns) == 1:
             # NOTE: [special handling of all_reduce_coalesced_'s return value]
@@ -6648,7 +6652,6 @@ class FallbackKernel(ExternKernelAlloc):
             self.use_runtime_dispatch = True
 
         def do_runtime_dispatch() -> None:
-            exported_args = None
             args = None
             exported_args = self.export_extern_kernel_node()
 
@@ -7513,7 +7516,10 @@ class EffectfulKernel(FallbackKernel):
 
         from torch._higher_order_ops.effects import get_effect_key
 
-        effect_type = get_effect_key(kernel, (*nontensor_args, *tensor_args), kwargs)
+        uncovered_args = [
+            a.value if isinstance(a, TorchBindObject) else a for a in tensor_args
+        ]
+        effect_type = get_effect_key(kernel, (*nontensor_args, *uncovered_args), kwargs)
         assert effect_type is not None
         self.effect_type = effect_type
         self.prev_effect_buffer = V.graph.effectful_ops.get(effect_type, None)
@@ -7535,14 +7541,37 @@ class EffectfulKernel(FallbackKernel):
 
 @ir_dataclass
 class TorchBindObject(IRNode):
+    from torch._library.fake_class_registry import FakeScriptObject
+
     name: str
-    value: torch._C.ScriptObject
+    value: Union[FakeScriptObject, torch.ScriptObject]
 
     def get_name(self):  # type: ignore[no-untyped-def]
         return self.name
 
     def codegen_reference(self, writer: Optional[IndentedBuffer] = None) -> str:
         return self.name
+
+    def get_value(self) -> Union[FakeScriptObject, torch.ScriptObject]:
+        return self.value
+
+    def get_real_obj(self) -> torch.ScriptObject:
+        if isinstance(self.value, torch.ScriptObject):
+            return self.value
+        else:
+            return self.value.real_obj
+
+    def get_buf_bytes(self) -> int:
+        # Returns the sum of all tensors in the flattened object
+        real_script_obj = self.get_real_obj()
+        flat_dict = dict(real_script_obj.__obj_flatten__())  # type: ignore[attr-defined]
+        flat_elems = pytree.tree_flatten(flat_dict)[0]
+        flat_sizes = [
+            x.element_size() * x.numel()
+            for x in flat_elems
+            if isinstance(x, torch.Tensor)
+        ]
+        return functools.reduce(lambda x, y: x + y, flat_sizes, 0)
 
 
 class _CollectiveKernel(FallbackKernel):
