6f6000393a744b6b778986e1113983ddab3d1146
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-02-20 14:38:14 -0300

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/test_exceptions.py
+++ test/dynamo/test_exceptions.py
@@ -1,5 +1,6 @@
 # Owner(s): ["module: dynamo"]
 
+import contextlib
 import sys
 import unittest
 
@@ -119,6 +120,33 @@ class ExceptionTests(torch._dynamo.test_case.TestCase):
         res = opt_fn(x)
         self.assertEqual(ref, res)
 
+    @make_dynamo_test
+    def test_foo(self):
+        @contextlib.contextmanager
+        def cm():
+            try:
+                yield
+            except BaseException:
+                raise ValueError  # noqa: B904
+
+        @contextlib.contextmanager
+        def nothing():
+            try:
+                yield
+            finally:
+                pass
+
+        z = 0
+        with nothing():
+            try:
+                with cm():
+                    raise IndexError
+            except ValueError:
+                z = 1
+            except IndexError:
+                z = 2
+            assert z == 1
+
     def test_exception_else(self):
         def gn(x):
             return torch.cos(x)
@@ -140,6 +168,64 @@ class ExceptionTests(torch._dynamo.test_case.TestCase):
         opt_fn = torch.compile(fn, backend="eager", fullgraph=True)
         res = opt_fn(x)
         self.assertEqual(ref, res)
+
+    @unittest.skipIf(sys.version_info < (3, 11), "Python 3.11+")
+    @make_dynamo_test
+    def test_raise_match(self):
+        a = AttributeError
+        b = BytesWarning
+        c = ConnectionError
+        d = DeprecationWarning
+        e = Exception
+
+        def fn(a, b):
+            try:
+                raise a
+            finally:
+                raise b
+
+        def fix_exc_context(frame_exc, new_exc, old_exc):
+            # slightly change from ExitStack.fix_exc_context function
+            while 1:
+                exc_context = new_exc.__context__
+                if exc_context is None or exc_context is old_exc:
+                    return
+                if exc_context is frame_exc:
+                    break
+                new_exc = exc_context
+            new_exc.__context__ = old_exc
+
+        @contextlib.contextmanager
+        def ctx():
+            try:
+                yield
+            finally:
+                frame_exc = prev_exc = sys.exc_info()
+                args = [(d, c), (b, a)]
+                for x, y in args:
+                    try:
+                        fn(x, y)
+                    except BaseException:
+                        new_exc = sys.exc_info()
+                        fix_exc_context(frame_exc[1], new_exc[1], prev_exc[1])
+                        prev_exc = new_exc
+
+                try:
+                    fixed_ctx = prev_exc[1].__context__
+                    raise prev_exc[1]
+                except BaseException:
+                    prev_exc[1].__context__ = fixed_ctx
+                    raise
+
+        try:
+            with ctx():
+                raise e
+        except Exception as exc:
+            assert isinstance(exc, a)
+            assert isinstance(exc.__context__, b)
+            assert isinstance(exc.__context__.__context__, c)
+            assert isinstance(exc.__context__.__context__.__context__, d)
+            assert isinstance(exc.__context__.__context__.__context__.__context__, e)
 
     # TODO(anijain2305) - does not work with fullgraph=True
     def test_exception_with_another_exception2(self):
@@ -450,6 +536,103 @@ class ExceptionTests(torch._dynamo.test_case.TestCase):
         res = opt_fn(x, d, "m")
         self.assertEqual(ref[0], res[0])
         self.assertEqual(ref[1], res[1])
+
+    @make_dynamo_test
+    def test_reraise_first_exc(self):
+        def fn():
+            try:
+                raise ZeroDivisionError
+            except ZeroDivisionError:
+                try:
+                    raise ValueError
+                except ValueError:
+                    pass
+                raise
+
+        try:
+            fn()
+        except ZeroDivisionError:
+            pass
+        assert sys.exc_info()[0] is None
+
+    @make_dynamo_test
+    def test_ensure_exception_is_active_after_try_except_block(self):
+        try:
+            try:
+                raise ZeroDivisionError
+            except ZeroDivisionError:
+                for exc in (KeyError, IndexError):
+                    try:
+                        raise exc
+                    except exc:
+                        pass
+                raise
+        except ZeroDivisionError:
+            pass
+        assert sys.exc_info()[0] is None
+
+    @make_dynamo_test
+    def test_ensure_exception_is_active_inside_try_except_block(self):
+        try:
+            try:
+                raise ZeroDivisionError
+            except ZeroDivisionError:
+                for exc in (KeyError, IndexError):
+                    try:
+                        raise exc
+                    except exc as e:
+                        assert isinstance(e.__context__, ZeroDivisionError)
+                raise
+        except ZeroDivisionError:
+            pass
+        assert sys.exc_info()[0] is None
+
+    @make_dynamo_test
+    def test_handle_all_exceptions(self):
+        def cm():
+            try:
+                yield 1
+            except ValueError:
+                try:
+                    raise TypeError
+                finally:
+                    pass
+
+        try:
+            gen = cm()
+            next(gen)
+            gen.throw(ValueError)
+        except TypeError:
+            pass
+        assert sys.exc_info()[0] is None
+
+    @make_dynamo_test
+    def test_reraise(self):
+        try:
+            try:
+                raise ValueError
+            except ValueError:  # noqa: TRY203
+                raise
+        except ValueError:
+            pass
+        assert sys.exc_info()[0] is None
+
+    @make_dynamo_test
+    def test_raise_finally_simple(self):
+        def fn():
+            try:
+                raise ValueError
+            except ValueError:
+                try:
+                    raise TypeError
+                finally:
+                    pass
+
+        try:
+            fn()
+        except TypeError:
+            pass
+        assert sys.exc_info()[0] is None
 
     @unittest.expectedFailure
     def test_reconstruct___context__(self):
