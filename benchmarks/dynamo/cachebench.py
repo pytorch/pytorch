bea7698988208ab982c402ea91422c63ed6ea392
Oguz Ulgen <oulgen@meta.com>
2025-02-20 11:20:14 -0800

    Add cachebench
    [ghstack-poisoned]

---
+++ benchmarks/dynamo/cachebench.py
@@ -0,0 +1,173 @@
+import argparse
+import dataclasses
+import json
+import logging
+import os
+import subprocess
+import sys
+import tempfile
+
+from torch._inductor.utils import fresh_inductor_cache
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+TIMEOUT: int = 2000
+
+MODELS: list[str] = [
+    "nanogpt",
+    "BERT_pytorch",
+    "resnet50"
+]
+
+
+@dataclasses.dataclass
+class RunResult:
+    model: str
+    mode: str
+    dynamic: bool
+    cold_compile_s: float
+    warm_compile_s: float
+    speedup: float
+
+
+def get_compile_time(file: tempfile._TemporaryFileWrapper) -> float:
+    lines = file.readlines()
+    # Decode from byte string, remove new lines, parse csv
+    lines = [line.decode("utf-8").strip().split(",") for line in lines]
+    compilation_time_idx = lines[0].index("compilation_latency")
+    compilation_time = lines[1][compilation_time_idx]
+    return float(compilation_time)
+
+
+def _run_torchbench_from_args(model: str, args: list[str]) -> tuple[float, float]:
+    with fresh_inductor_cache():
+        env = os.environ.copy()
+        with tempfile.NamedTemporaryFile(suffix=".csv") as file:
+            args.append("--output=" + file.name)
+            logger.info(f"Performing cold-start run for {model}")
+            subprocess.check_call(args, timeout=TIMEOUT, env=env)
+            cold_compile_time = get_compile_time(file)
+
+        args.pop()
+        with tempfile.NamedTemporaryFile(suffix=".csv") as file:
+            args.append("--output=" + file.name)
+            logger.info(f"Performing warm-start run for {model}")
+            subprocess.check_call(args, timeout=TIMEOUT, env=env)
+            warm_compile_time = get_compile_time(file)
+
+        return cold_compile_time, warm_compile_time
+
+
+def _run_torchbench_model(results: list[RunResult], model: str) -> None:
+    cur_file = os.path.abspath(__file__)
+    torchbench_file = os.path.join(os.path.dirname(cur_file), "torchbench.py")
+    assert os.path.exists(
+        torchbench_file
+    ), f"Torchbench does not exist at {torchbench_file}"
+
+    base_args = [
+        sys.executable,
+        torchbench_file,
+        f"--only={model}",
+        "--repeat=5",
+        "--performance",
+        "--backend=inductor",
+    ]
+    for mode in ["inference", "training"]:
+        for dynamic in [False, True]:
+            args = list(base_args)
+            if mode == "inference":
+                args.extend(["--inference", "--bfloat16"])
+            else:
+                assert mode == "training"
+                args.extend(["--training", "--amp"])
+            if dynamic:
+                args.extend(["--dynamic-shapes", "--dynamic-batch-only"])
+
+            logger.info(f"Command: {args}")
+            try:
+                cold_compile_t, warm_compile_t = _run_torchbench_from_args(model, args)
+                results.append(
+                    RunResult(
+                        "model",
+                        mode,
+                        dynamic,
+                        cold_compile_t,
+                        warm_compile_t,
+                        cold_compile_t / warm_compile_t,
+                    )
+                )
+            except Exception as e:
+                print(e)
+                return None
+
+
+def _write_results_to_json(results: list[RunResult], output_filename: str) -> None:
+    records = []
+    for result in results:
+        for metric_name in ["cold_compile_time(s)", "warm_compile_time(s)", "speedup"]:
+            if metric_name.startswith("cold"):
+                value = result.cold_compile_s
+            elif metric_name.startswith("warm"):
+                value = result.warm_compile_s
+            else:
+                assert metric_name == "speedup"
+                value = result.speedup
+            records.append(
+                {
+                    "benchmark": {
+                        "name": "cache_benchmarks",
+                        "mode": result.mode,
+                        "extra_info": {
+                            "is_dynamic": result.dynamic,
+                        },
+                    },
+                    "model": {
+                        "name": result.model,
+                        "backend": "inductor",
+                    },
+                    "metric": {
+                        "name": metric_name,
+                        "type": "OSS model",
+                        "benchmark_values": [value],
+                    },
+                }
+            )
+    with open(output_filename, "w") as f:
+        json.dump(records, f)
+
+
+def parse_cmd_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Run a TorchBench ServiceLab benchmark."
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="Name of the model to run",
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        help="The output filename (json)",
+    )
+    args, _ = parser.parse_known_args()
+    return args
+
+
+def main() -> None:
+    args = parse_cmd_args()
+
+    results: list[RunResult] = []
+
+    if args.model is not None:
+        _run_torchbench_model(results, args.model)
+    else:
+        for model in MODELS:
+            _run_torchbench_model(results, model)
+    _write_results_to_json(results, args.output)
+
+
+if __name__ == "__main__":
+    main()
