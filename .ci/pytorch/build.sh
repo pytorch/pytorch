6f7b445c6945c89b1fc380c00d6a7bd9a219dbba
Catherine Lee <csl@fb.com>
2025-02-19 16:35:27 -0800

    tc

--- .ci/pytorch/build.sh
+++ .ci/pytorch/build.sh
@@ -378,7 +378,8 @@ else
     # is set to be the number of CPU minus 2. So, let's try a more conservative value here. A 4xlarge has
     # 16 CPUs
     if [ -z "$MAX_JOBS_OVERRIDE" ]; then
-      export MAX_JOBS=$(nproc --ignore=2)
+      MAX_JOBS=$(nproc --ignore=2)
+      export MAX_JOBS
     fi
 
     # NB: Install outside of source directory (at the same level as the root
