07c98e2c0177aeeac7919eead9da127f1903abb1
Chien-Lin Chen <zero064@hotmail.com>
2025-02-19 19:38:15 -0800

    modified device check logic and added tests

--- aten/src/ATen/core/op_registration/adaption.h
+++ aten/src/ATen/core/op_registration/adaption.h
@@ -9,8 +9,8 @@
  * [Note: hacky wrapper removal for optional tensor]
  *
  * The kernel implementation takes an optional tensor marked in the schema as
- * Tensor? but the C++ function takes Tensor instead of the std::optional<Tensor>
- * expected by the dispatcher.
+ * Tensor? but the C++ function takes Tensor instead of the
+ * std::optional<Tensor> expected by the dispatcher.
  *
  * To remove the hacky wrapper, the C++ function is changed to take
  * std::optional<Tensor> and unwrap the Tensor value at the beginning of
@@ -42,9 +42,22 @@
 
 namespace c10::impl {
 
-TORCH_API void common_device_check_failure(Device common_device, const at::Tensor& tensor, at::CheckedFrom methodName, at::CheckedFrom argName);
+TORCH_API void common_device_check_failure(
+    Device common_device,
+    const at::Tensor& tensor,
+    at::CheckedFrom methodName,
+    at::CheckedFrom argName);
 
-inline void check_and_update_common_device(std::optional<Device>& common_device, const at::Tensor& tensor, at::CheckedFrom methodName, at::CheckedFrom argName) {
+inline bool cpu_zero_dim(const at::Tensor& tensor) {
+  return tensor.device().is_cpu() && (tensor.dim() == 0);
+}
+
+inline void check_and_update_common_device(
+    std::optional<Device>& common_device,
+    bool& is_cpu_zero_dim,
+    const at::Tensor& tensor,
+    at::CheckedFrom methodName,
+    at::CheckedFrom argName) {
   // TODO: Remove this once the following issue is addressed:
   // https://github.com/pytorch/pytorch/issues/57380
   if (!tensor.defined()) {
@@ -53,29 +66,60 @@ inline void check_and_update_common_device(std::optional<Device>& common_device,
 
   if (!common_device.has_value()) {
     common_device = tensor.device();
+    is_cpu_zero_dim = cpu_zero_dim(tensor);
     return;
   }
-
-  if (C10_UNLIKELY(common_device != tensor.device())) {
-    common_device_check_failure(*common_device, tensor, methodName, argName);
+  bool tensor_is_cpu_zero_dim = cpu_zero_dim(tensor);
+  if (C10_LIKELY(common_device == tensor.device())) {
+    if (is_cpu_zero_dim) {
+      is_cpu_zero_dim = tensor_is_cpu_zero_dim;
+    }
+    return;
   }
+  if (tensor_is_cpu_zero_dim) {
+    return;
+  }
+  if (is_cpu_zero_dim) {
+    common_device = tensor.device();
+    is_cpu_zero_dim = tensor_is_cpu_zero_dim;
+    return;
+  }
+  common_device_check_failure(*common_device, tensor, methodName, argName);
 }
 
-inline void check_and_update_common_device(std::optional<Device>& common_device, const std::optional<at::Tensor>& tensor, at::CheckedFrom methodName, at::CheckedFrom argName) {
+inline void check_and_update_common_device(
+    std::optional<Device>& common_device,
+    bool& is_cpu_zero_dim,
+    const std::optional<at::Tensor>& tensor,
+    at::CheckedFrom methodName,
+    at::CheckedFrom argName) {
   if (tensor.has_value()) {
-    check_and_update_common_device(common_device, tensor.value(), methodName, argName);
+    check_and_update_common_device(
+        common_device, is_cpu_zero_dim, tensor.value(), methodName, argName);
   }
 }
 
-inline void check_and_update_common_device(std::optional<Device>& common_device, at::ITensorListRef tensors, at::CheckedFrom methodName, at::CheckedFrom argName) {
+inline void check_and_update_common_device(
+    std::optional<Device>& common_device,
+    bool& is_cpu_zero_dim,
+    at::ITensorListRef tensors,
+    at::CheckedFrom methodName,
+    at::CheckedFrom argName) {
   for (const auto& tensor : tensors) {
-    check_and_update_common_device(common_device, tensor, methodName, argName);
+    check_and_update_common_device(
+        common_device, is_cpu_zero_dim, tensor, methodName, argName);
   }
 }
 
-inline void check_and_update_common_device(std::optional<Device>& common_device, const List<std::optional<at::Tensor>>& tensors, at::CheckedFrom methodName, at::CheckedFrom argName) {
+inline void check_and_update_common_device(
+    std::optional<Device>& common_device,
+    bool& is_cpu_zero_dim,
+    const List<std::optional<at::Tensor>>& tensors,
+    at::CheckedFrom methodName,
+    at::CheckedFrom argName) {
   for (const auto& tensor : tensors) {
-    check_and_update_common_device(common_device, tensor, methodName, argName);
+    check_and_update_common_device(
+        common_device, is_cpu_zero_dim, tensor, methodName, argName);
   }
 }
 } // namespace c10::impl
