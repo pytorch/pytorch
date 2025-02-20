44f18be69204bc62fb90ae6b72868e36122bf39f
Zhang, Jianyi <jianyi.zhang@intel.com>
2025-02-20 09:58:15 +0000

    update

---
+++ torch/backends/xpu/__init__.py
@@ -0,0 +1,30 @@
+# mypy: allow-untyped-defs
+import contextlib
+from typing import Union
+from typing_extensions import deprecated
+
+import torch
+
+
+__all__ = [
+    "matmul",
+]
+
+class XPUModule:
+    def __getattr__(self, name):
+        if name == "allow_tf32":
+            return torch.get_float32_matmul_precision() == "high"
+        raise AttributeError("Unknown attribute " + name)
+
+    def __setattr__(self, name, value):
+        if name == "allow_tf32":
+            assert isinstance(value, bool), "allow_tf32 should be a boolean, but got {}".format(type(value))
+            if value:
+                torch.set_float32_matmul_precision("high")
+            else:
+                torch.set_float32_matmul_precision("highest")
+            return
+        raise AttributeError("Unknown attribute " + name)
+
+
+matmul = XPUModule()
