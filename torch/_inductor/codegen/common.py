7e8a7331323a9a0fac8c0af9adf04408856e917a
Ding, Yi1 <yi1.ding@intel.com>
2025-02-21 07:52:01 +0000

    Refine cpp check_dtype

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -560,16 +560,17 @@ def check_dtype(
         from .cpp_utils import CppCSEVariable, DTYPE_TO_CPP
 
         assert isinstance(var, CppCSEVariable)
-        if dtype == torch.bool and var.is_vec:
-            is_same_dt = f"IsVecMaskType<decltype({var})>::value"
+        if dtype == torch.bool:
+            if var.is_vec:
+                is_same_dt = f"IsVecMaskType<decltype({var})>::value"
+            else:
+                # operator&(bool, bool) returns int and it can be used as boolean in C++
+                is_same_dt = f"std::is_same_v<decltype({var}), bool> || std::is_same_v<decltype({var}), int>"
         else:
             c_var_type = f"decltype({var})"
             if var.is_vec:
                 c_var_type = f"typename {c_var_type}::value_type"
             is_same_dt = f"std::is_same_v<{c_var_type}, {DTYPE_TO_CPP[dtype]}>"
-            if dtype == torch.bool:
-                # operator&(bool, bool) returns int and int can be used as boolean in C++
-                is_same_dt = f"({is_same_dt}) || (std::is_same_v<{c_var_type}, int>)"
 
         buffer.writeline(f"static_assert({is_same_dt});")
 
