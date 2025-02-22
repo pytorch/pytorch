78d794f1bedbbee049d78576b9dae44db16a5480
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-02-22 15:42:43 +0800

    Ensure only one mismatch dimension and its size is 1

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3263,13 +3263,13 @@ def gaussian_nll_loss(
         if input.size()[:-1] == var.size():
             var = torch.unsqueeze(var, -1)
 
-        # This checks if the var is broadcastable to the input.
+        # This checks if the var is broadcastable to the input and there is only one mismatched dimension.
         # This is also a homoscedastic case.
         # e.g. input.size = (10, 2, 3), var.size = (10, 2, 1)
         # or  input.size = (4, 3, 32, 32), var.size = (4, 1, 32, 32)
         elif (
             input.ndim == var.ndim
-            and all(x == y or y == 1 for x, y in zip(input.size(), var.size()))
+            and sum(y for x, y in zip(input.size(), var.size()) if x != y) == 1
         ):  # Heteroscedastic case
             pass
 
