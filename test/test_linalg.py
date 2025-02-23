aa38184838ce65a2b692693702fd15b7255663ed
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-02-23 12:04:50 +0000

    Revert "[ROCm] OCP FP8 Support for new GPUs (#146632)"
    This reverts commit f95ab46797e1f3e8cc48ce2f45e4f6985132fb19.
    
    Reverted https://github.com/pytorch/pytorch/pull/146632 on behalf of https://github.com/jeanschmidt due to Breaking internal builds, I'll find someone to help merge this PR back to main ([comment](https://github.com/pytorch/pytorch/pull/146632#issuecomment-2676823614))

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -50,13 +50,7 @@ if TEST_SCIPY:
 def blaslt_supported_device():
     if torch.cuda.is_available():
         if torch.version.hip:
-            ROCM_VERSION = tuple(int(v) for v in torch.version.hip.split('.')[:2])
-            archs = ['gfx90a', 'gfx94']
-            if ROCM_VERSION >= (6, 3):
-                archs.extend(['gfx110', 'gfx120'])
-            if ROCM_VERSION >= (6, 5):
-                archs.append('gfx95')
-            for arch in archs:
+            for arch in ['gfx90a', 'gfx94']:
                 if arch in torch.cuda.get_device_properties(0).gcnArchName:
                     return True
         else:
