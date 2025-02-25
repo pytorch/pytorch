f09a816a376971ca0ac018146c0230b950a45ca7
Ryan Guo <ryanguo99@meta.com>
2025-02-25 14:33:43 -0800

    [hop] Support more output types for `flat_apply` (#146714)
    This patch enables `flat_apply` to support certain non-Tensor output
    types like containers and graphable types. This will in turn enable the
    upcoming `mark_traceable` to support more output types.
    
    The patch also exposes a `func_to_graphable` rather than having the
    users calling the lower level `pytree.flatten(ConstantFunction(...))`.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146714
    Approved by: https://github.com/zou3519

--- test/dynamo/test_flat_apply.py
+++ test/dynamo/test_flat_apply.py
@@ -4,6 +4,12 @@ from dataclasses import dataclass
 import torch
 import torch._dynamo.test_case
 import torch.utils._pytree as pytree
+from torch._higher_order_ops.flat_apply import (
+    flat_apply,
+    func_to_graphable,
+    is_graphable,
+    to_graphable,
+)
 
 
 def distance(a, b, norm):
@@ -40,14 +46,8 @@ class FlatApplyTests(torch._dynamo.test_case.TestCase):
 
         args = (a, b)
         kwargs = {"norm": norm}
-        from torch._higher_order_ops.flat_apply import (
-            ConstantFunction,
-            flat_apply,
-            is_graphable,
-            to_graphable,
-        )
 
-        empty_list, func_spec = pytree.tree_flatten(ConstantFunction(distance))
+        empty_list, func_spec = func_to_graphable(distance)
         self.assertEqual(empty_list, [])
 
         flat_args, in_spec = to_graphable((args, kwargs))
@@ -58,6 +58,30 @@ class FlatApplyTests(torch._dynamo.test_case.TestCase):
         # Test flat_apply returns same thing as original function
         result = flat_apply(func_spec, in_spec, *flat_args)
         self.assertEqual(result, distance(*args, **kwargs))
+
+    def test_non_tensor_output(self):
+        tensor = torch.tensor
+
+        a = Point(tensor(0.0), tensor(0.0))
+        b = Point(tensor(3.0), tensor(4.0))
+
+        args = (a, b)
+        kwargs = {}
+
+        def f(a, b):
+            return [a.x + 1, (b.x + 2, [a.y + 3, 4.0], "5"), 6 + b.y]
+
+        empty_list, func_spec = func_to_graphable(f)
+        self.assertEqual(empty_list, [])
+
+        flat_args, in_spec = to_graphable((args, kwargs))
+
+        for arg in flat_args:
+            self.assertTrue(is_graphable(arg))
+
+        # Test flat_apply returns same thing as original function
+        result = flat_apply(func_spec, in_spec, *flat_args)
+        self.assertEqual(result, f(*args, **kwargs))
 
 
 if __name__ == "__main__":
