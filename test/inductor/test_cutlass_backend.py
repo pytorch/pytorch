449ec0076b6f8f6821d272adfcade825c264e37a
henrylhtsang <henrylhtsang@meta.com>
2025-02-26 11:34:42 -0800

    Update base for Update on "[cutlass backend] cache_clear algorithm select cache on fresh inductor cache"
    Differential Revision: [D69959917](https://our.internmc.facebook.com/intern/diff/D69959917/)
    
    AlgorithmSelectorCache is a cache. The expectation is that when we force disable cache + clear inductor caches, it would be clear. However that is not the case.
    
    The reason why this is a problem can be seen by following this repro:
    What we will see is 
    ```
    SingleProcess AUTOTUNE benchmarking takes 6.2202 seconds and 46.0568 seconds precompiling for 36 choices
    SingleProcess AUTOTUNE benchmarking takes 492.3141 seconds and 0.0010 seconds precompiling for 36 choices
    ```
    
    The root cause is, while precompiling is skipped, due to it being cache, autotuning isn't skipped since we force disable it.
    
    repro:
    ```
    import logging
    import os
    
    os.environ["TORCH_LOGS"] = "+output_code,+benchmarking,+inductor"
    
    import torch
    
    import torch._inductor.config
    from torch._inductor.utils import clear_inductor_caches
    
    torch._inductor.config.max_autotune = True
    torch._inductor.config.force_disable_caches = True
    torch._inductor.config.autotune_num_choices_displayed = None
    torch._inductor.config.max_autotune_gemm_backends = "CUTLASS"
    torch._inductor.config.autotune_fallback_to_aten = False
    torch._inductor.config.cuda.cutlass_instantiation_level = "0001"
    
    def main():
        M, N, K = 2048, 2048, 2048
        dtype = torch.bfloat16
        A = torch.randn(M, K, device="cuda", dtype=dtype)
        B = torch.randn(K, N, device="cuda", dtype=dtype)
        for _ in range(2):
            torch._dynamo.reset()
            clear_inductor_caches()
            compiled_model = torch.compile(torch.mm, fullgraph=True)
            _ = compiled_model(A, B)
    
        print("done")
    
    
    if __name__ == "__main__":
        main()
    ```
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy yf225 chenyang78 kadeng muchulee8 amjames chauhang aakhundov
    
    [ghstack-poisoned]

--- test/inductor/test_cutlass_backend.py
+++ test/inductor/test_cutlass_backend.py
@@ -347,7 +347,6 @@ class TestCutlassBackend(TestCase):
                     2,
                     4,
                 ],  # guarantees > 1 choices
-                "force_disable_caches": True,
             }
         ):
             from torch._inductor.utils import run_and_get_code
