0f4ae27a51f89c88339a0f312f263275904ba786
sanchitintel <sanchit.jain@intel.com>
2025-02-26 18:09:25 -0800

    Fix typo

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -4969,7 +4969,7 @@ class CppScheduling(BaseScheduling):
         ]
         # The counter cpp_templated_kernel_counter is used for verifying if a
         # a templated kernel was successfully compiled in a UT
-        counters["inductor"]["cpp_compiled_kernels_counter"] += 1
+        counters["inductor"]["cpp_templated_kernel_counter"] += 1
         counters["inductor"]["cpp_epilogue_fusion_counter"] += len(epilogue_nodes)
         assert self.is_cpp_template(template_node), (
             "Template node passed to CppScheduler.codegen_template must be a SchedulerNode that wraps a CppTemplateBuffer"
