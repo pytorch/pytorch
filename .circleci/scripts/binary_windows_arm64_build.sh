c856aa69d45fe1649a4a7a406e9e3563d0d2c0ce
iremyux <iremyuksel@microsoft.com>
2025-02-28 13:19:31 +0100

    Revert "Move bootstrap_python to see if calling it from .sh file would work with env variables"
    This reverts commit 7d9680ac65adf96d90b76f85f9975eed5b9142da.

--- .circleci/scripts/binary_windows_arm64_build.sh
+++ .circleci/scripts/binary_windows_arm64_build.sh
@@ -12,8 +12,6 @@ df -h
 
 export NIGHTLIES_PYTORCH_ROOT="$PYTORCH_ROOT"
 
-pytorch/.ci/pytorch/windows/arm64/bootstrap_python.bat
-
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
     pytorch/.ci/pytorch/windows/arm64/build_libtorch.bat
 elif [[ "$PACKAGE_TYPE" == 'wheel' ]]; then
