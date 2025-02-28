d3f45624d9ced74e79a3833098b35bd24e5ae5b0
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2025-02-28 10:48:43 -0800

    Update on "Fix decomp for linspace"
    In python decompositions, we shouldn't do any non-functional operations for functional operators. This should go away once we start decomposing before functionalization. 
    
    Differential Revision: [D70265200](https://our.internmc.facebook.com/intern/diff/D70265200)
    
    [ghstack-poisoned]

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -22076,6 +22076,13 @@ python_ref_db = [
             DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_python_ref_executor',
                          dtypes=(torch.int16, torch.int32, torch.int64),
                          device_type="cuda"),
+            # TODO copy doesn't have prim refs
+            DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_python_ref',
+                         dtypes=(torch.float32, torch.float64, torch.float16, torch.complex64, torch.complex128, torch.bfloat16),
+                         device_type="cuda"),
+            DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_python_ref',
+                         dtypes=(torch.float32, torch.float64, torch.float16, torch.complex64, torch.complex128, torch.bfloat16),
+                         device_type="cpu"),
         ),
     ),
     PythonRefInfo(
