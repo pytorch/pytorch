6bd3e6a92e95c5bd3f32bd9b5837618031829be1
Yuanhao Ji <jiyuanhao@apache.org>
2025-02-28 03:11:48 +0000

    fix

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -12749,6 +12749,7 @@ class CommonTemplate:
 
                 assert len(inps) == 0
 
+    @expectedFailureCodegenDynamic
     def test_special_polygamma(self):
         fn = torch.special.polygamma
         x = torch.tensor(2, dtype=torch.float32)
