aad5c560fcae989c4d94f80c76ff86298302bfb8
Matthew Hoffman <matthew@protopia.ai>
2025-03-01 03:49:46 -0600

    Only Py_INCREF the state tensor
    conversation: https://github.com/pytorch/pytorch/pull/147907#discussion_r1976031723

--- torch/csrc/Generator.cpp
+++ torch/csrc/Generator.cpp
@@ -256,10 +256,11 @@ static PyObject* THPGenerator_reduce(PyObject* _self, PyObject* noargs) {
       1,
       device_type != at::kCPU ? THPGenerator_getOffset(_self, nullptr)
                               : Py_None);
-  PyTuple_SET_ITEM(state.get(), 2, THPGenerator_getState(_self, nullptr));
+  PyObject* state_tensor = THPGenerator_getState(_self, nullptr);
+  Py_INCREF(state_tensor);
+  PyTuple_SET_ITEM(state.get(), 2, state_tensor);
   PyTuple_SET_ITEM(ret.get(), 2, state.release());
 
-  Py_INCREF(ret);
   return ret.release();
   END_HANDLE_TH_ERRORS
 }
