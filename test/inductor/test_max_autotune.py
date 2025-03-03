d9ea6bea2e8a474a062070521c1d25ffe2784961
amdfaa <107946068+amdfaa@users.noreply.github.com>
2025-03-03 13:34:30 -0600

    Update trunk.yml
    Update rocm.yml
    
    Update trunk.yml
    
    Update build.sh
    
    Update action.yml
    
    Update trunk.yml
    
    Update rocm.yml
    
    Update action.yml
    
    Update action.yml to output 0 even if it doesn't find the search
    
    skip several tests on navi for upstream
    
    Change gfx1100 check to use equal to 0
    
    reduce set of tests to run on Navi31
    
    Syntax
    
    refactor names
    
    getRocmVersion
    
    fix lint and syntax errors
    
    Lint
    
    Remove torchinductor_opinfo
    
    trigger rebuild
    
    trigger rebuild
    
    trigger rebuild
    
    Update rocm.yml to new naming
    
    Update job name in rocm.yml
    
    Update job name in rocm.yml

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -43,7 +43,9 @@ from torch.testing._internal.common_cuda import PLATFORM_SUPPORTS_FP8
 from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
     IS_WINDOWS,
+    NAVI_ARCH,
     parametrize,
+    skipIfRocmArch,
     TEST_WITH_ROCM,
 )
 from torch.testing._internal.logging_utils import multiple_logs_to_string
@@ -908,6 +910,7 @@ class TestMaxAutotune(TestCase):
 
         self.assertIn("NoValidChoicesError", str(context.exception))
 
+    @skipIfRocmArch(NAVI_ARCH)
     def test_non_contiguous_input_mm(self):
         """
         Make sure the triton template can work with non-contiguous inputs without crash.
@@ -926,6 +929,7 @@ class TestMaxAutotune(TestCase):
         act = f(x, y)
         torch.testing.assert_close(act, ref, atol=2e-2, rtol=1e-2)
 
+    @skipIfRocmArch(NAVI_ARCH)
     def test_non_contiguous_input_addmm(self):
         b = torch.randn((768), dtype=torch.bfloat16, device=GPU_TYPE)
         x = rand_strided(
@@ -941,6 +945,7 @@ class TestMaxAutotune(TestCase):
         act = f(x, y)
         torch.testing.assert_close(act, ref, atol=2e-2, rtol=1e-2)
 
+    @skipIfRocmArch(NAVI_ARCH)
     def test_non_contiguous_input_bmm(self):
         x = rand_strided(
             (1, 50257, 2048), (0, 1, 50304), dtype=torch.bfloat16, device=GPU_TYPE
