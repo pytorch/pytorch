608377d3418653932d6461093a77db44e720290d
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-03-03 00:49:31 +0000

    Revert "[import][inductor] Simplify grid handling (#147583)"
    This reverts commit b59776d8572a56e2d2366174eac11015b1776f1e.
    
    Reverted https://github.com/pytorch/pytorch/pull/147583 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/147583#issuecomment-2693016036))

--- torch/_inductor/fx_passes/b2b_gemm.py
+++ torch/_inductor/fx_passes/b2b_gemm.py
@@ -27,7 +27,6 @@ from ..pattern_matcher import (
 from ..select_algorithm import (
     autotune_select_algorithm,
     ExternKernelChoice,
-    SymbolicGridFn,
     TritonTemplate,
     TritonTemplateCaller,
 )
@@ -39,9 +38,8 @@ B2B_GEMM_PASS = PatternMatcherPass(
 )
 
 
-@SymbolicGridFn
-def b2b_gemm_grid(M, P, meta, *, cdiv):
-    return (cdiv(M, meta["BLOCK_SIZE_M"]) * cdiv(P, meta["BLOCK_SIZE_P"]), 1, 1)
+def b2b_gemm_grid(M, P, meta):
+    return (ceildiv(M, meta["BLOCK_SIZE_M"]) * ceildiv(P, meta["BLOCK_SIZE_P"]), 1, 1)
 
 
 b2b_gemm_left_template = TritonTemplate(
