302c66029857a40e3cc84448674574f0983fa903
Alexander Grund <alexander.grund@tu-dresden.de>
2025-03-03 19:37:24 +0000

    Consistently use load_torchbind_test_lib in tests (#148082)
    The same code is repeated multiple times with slightly different implementations.
    Use the existing function for brevity and consistency.
    
    In the function the code from `test_export` is used which does a single `load_library` with cleaner conditions
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148082
    Approved by: https://github.com/angelayi

--- torch/testing/_internal/torchbind_impls.py
+++ torch/testing/_internal/torchbind_impls.py
@@ -1,5 +1,6 @@
 # mypy: allow-untyped-defs
 import contextlib
+from pathlib import Path
 from typing import Optional
 
 import torch
@@ -113,15 +114,15 @@ def load_torchbind_test_lib():
         IS_WINDOWS,
     )
 
-    if IS_SANDCASTLE or IS_FBCODE:
-        torch.ops.load_library("//caffe2/test/cpp/jit:test_custom_class_registrations")
-    elif IS_MACOS:
+    if IS_MACOS:
         raise unittest.SkipTest("non-portable load_library call used in test")
+    elif IS_SANDCASTLE or IS_FBCODE:
+        lib_file_path = Path("//caffe2/test/cpp/jit:test_custom_class_registrations")
+    elif IS_WINDOWS:
+        lib_file_path = find_library_location("torchbind_test.dll")
     else:
         lib_file_path = find_library_location("libtorchbind_test.so")
-        if IS_WINDOWS:
-            lib_file_path = find_library_location("torchbind_test.dll")
-        torch.ops.load_library(str(lib_file_path))
+    torch.ops.load_library(str(lib_file_path))
 
 
 @contextlib.contextmanager
