d882cd722b974d3dc2b64b4b1727bbcf4dabde8e
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-03-03 00:49:31 +0000

    Revert "[import][inductor] Simplify grid handling (#147583)"
    This reverts commit b59776d8572a56e2d2366174eac11015b1776f1e.
    
    Reverted https://github.com/pytorch/pytorch/pull/147583 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/147583#issuecomment-2693016036))

--- torch/_inductor/codegen/mps.py
+++ torch/_inductor/codegen/mps.py
@@ -569,7 +569,7 @@ class MetalKernel(SIMDKernel):
         wrapper.generate_kernel_call(
             name,
             args,
-            device=torch.device("cpu"),  # TODO: Fix me, MPS does not expose streams now
+            gpu=False,  # TODO: Fix me, MPS does not expose streams now
             triton=False,
         )
 
--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -1143,6 +1143,8 @@ class _InProcessFxCompile(FxCompile):
                                     serialized_extern_kernel_nodes,
                                 )
 
+                            additional_files = graph.wrapper_code.additional_files
+
                             with dynamo_timed(
                                 "AotCodeCompiler.compile", log_pt2_compile_event=True
                             ):
@@ -1152,11 +1154,7 @@ class _InProcessFxCompile(FxCompile):
                                     code,
                                     serialized_extern_kernel_nodes,
                                     device_type=graph.device_type,
-                                    additional_files=[
-                                        *dict.fromkeys(
-                                            graph.wrapper_code.additional_files
-                                        )
-                                    ],
+                                    additional_files=additional_files,
                                 )
                         else:
                             compiled_fn = graph.compile_to_module().call
