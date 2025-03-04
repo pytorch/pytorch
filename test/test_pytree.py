8ff9e5c1e24218962b8918529886fb6b2081326b
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-03-05 00:03:19 +0800

    Update
    [ghstack-poisoned]

--- test/test_pytree.py
+++ test/test_pytree.py
@@ -224,10 +224,13 @@ class TestGenericPytree(TestCase):
         ],
     )
     def test_flatten_unflatten_leaf(self, pytree_impl):
+        leafspec = pytree_impl.tree_structure(object())
+
         def run_test_with_leaf(leaf):
             values, treespec = pytree_impl.tree_flatten(leaf)
             self.assertEqual(values, [leaf])
-            self.assertEqual(treespec, pytree_impl.LeafSpec())
+            self.assertEqual(treespec, leafspec)
+            self.assertTrue(treespec.is_leaf())
 
             unflattened = pytree_impl.tree_unflatten(values, treespec)
             self.assertEqual(unflattened, leaf)
@@ -1384,9 +1387,6 @@ class TestCxxPytree(TestCase):
     def setUp(self):
         if IS_FBCODE:
             raise unittest.SkipTest("C++ pytree tests are not supported in fbcode")
-
-    def test_treespec_equality(self):
-        self.assertEqual(cxx_pytree.LeafSpec(), cxx_pytree.LeafSpec())
 
     def test_treespec_repr(self):
         # Check that it looks sane
