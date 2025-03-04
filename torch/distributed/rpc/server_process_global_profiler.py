0b1bd28b74222a2d57c38f96cfb551f5ab09567e
Yu, Guangye <guangye.yu@intel.com>
2025-03-04 02:37:43 +0000

    Document poison fork note for accelerator APIs
    ghstack-source-id: 055bda8cf136f23eb9b67eaa20787ef816bc0c72
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147507

--- torch/distributed/rpc/server_process_global_profiler.py
+++ torch/distributed/rpc/server_process_global_profiler.py
@@ -47,11 +47,11 @@ class _server_process_global_profile(profile):
 
         profile_memory (bool, optional): Whether to report memory usage, default: ``False``
 
-    .. warning:
+    .. warning::
         Enabling memory profiling incurs additional profiler overhead
 
-    .. warning:
-        Due to some CUDA multiprocessing limitations (multiprocessing-cuda-note_),
+    .. warning::
+        Due to some CUDA multiprocessing limitations (see :ref:`multiprocessing-cuda-note`),
         one cannot use the profiler with ``use_cuda = True`` to benchmark
         DataLoaders with ``num_workers > 0``. If you wish to benchmark data loading,
         please use ``use_cuda = False`` or ``num_workers = 0``.
