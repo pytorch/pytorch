9dc732ffdba40d4d222bc004e67992a22e8594df
Nichols A. Romero <165712832+naromero77amd@users.noreply.github.com>
2025-03-04 11:25:41 -0600

    Update torch/cuda/tunable.py
    Co-authored-by: Svetlana Karslioglu <svekars@meta.com>

--- torch/cuda/tunable.py
+++ torch/cuda/tunable.py
@@ -116,7 +116,7 @@ There are several use cases for offline tuning.
 
 One use case involves a workload with a high-memory utilization, where regular tuning might lead to running out of memory.
 
-Another use case would be a workload that is compute intensive to run and it would be more resource efficient to collect
+Another use case is for compute-intensive workloads. In such cases, it is more resource-efficient to collect
 the GEMMs for the workload once, and then tune repeatedly with different tuning parameters or libraries.
 
 Workflow
