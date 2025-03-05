9044dc3ce08511494cbc0deb257183a05e9bbfee
Ozan Aydin <148207261+ozanMSFT@users.noreply.github.com>
2025-03-05 13:11:34 +0100

    - component update

--- .ci/pytorch/windows/arm64/bootstrap_buildtools.bat
+++ .ci/pytorch/windows/arm64/bootstrap_buildtools.bat
@@ -26,7 +26,7 @@ echo Installing MSVC %MSVC_VERSION%
     --add Microsoft.VisualStudio.Component.VC.CoreBuildTools ^
     --add Microsoft.VisualStudio.Component.VC.CoreIde ^
     --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest ^
-    --add Microsoft.VisualStudio.Component.VC.14.42.17.12.ARM ^
+    --add Microsoft.VisualStudio.Component.VC.14.42.17.12.ARM64 ^
     --add Microsoft.VisualStudio.Component.VC.14.42.17.12.x86.x64
 
 echo exitcode = %errorlevel%
