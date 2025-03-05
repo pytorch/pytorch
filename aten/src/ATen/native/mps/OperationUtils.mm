pytorch/pytorch

commit 0c0a4baddd7db4f20dda6115c4193a0bf934edb9

[MPS] unary kernels - avoid copying tensors if they have same stride (#148350)
I was a bit concerned when I saw in #148272 that metal unary kernel was 0.02x of the performance of what we had with MPS Graphs for sqrt(for non contiguous) tensors. This change makes it so that copying is only done if we don't have same strided tensors(for input/output). So if out tensor is not provided then we don't do copy(don't call contiguous) at all and dispatch the kernel as is. After making this change the script that I listed at the end of the above PR has the same execution time as the non-transposed one.

Times for reference(on transposed tensor where matrix is NxN matrix):

| N     | time_old           | time_new           |
|-------|--------------------|--------------------|
| 100   | 0.0002241021       | 0.0001548659       |
| 1000  | 0.0005934822       | 0.0002150342       |
| 10000 | 0.3242016407       | 0.0045755033       |

Pull Request resolved: https://github.com/pytorch/pytorch/pull/148350
Approved by: https://github.com/janeyx99

aten/src/ATen/native/mps/OperationUtils.mm
@@ -963,9 +963,10 @@ class BundledShaderLibary : public MetalShaderLibrary {
 void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
                                            const std::string& name,
                                            std::optional<int64_t> extra) {
-  auto inputTensor = iter.input(0).contiguous();
+  auto inputTensor = iter.input(0);
   auto outputTensor = iter.output(0);
-  bool needs_output_copy = false;
+  bool same_strides =
+      inputTensor.strides().equals(outputTensor.strides()) && inputTensor.sizes().equals(outputTensor.sizes());
   uint32_t length = outputTensor.numel();
   if (length == 0) {
     return;
@@ -976,9 +977,9 @@ void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
     cplState = getPipelineStateForFunc(fmt::format(
         "{}_dense_{}_{}", name, scalarToMetalTypeString(outputTensor), scalarToMetalTypeString(inputTensor)));
 
-    if (!outputTensor.is_contiguous()) {
+    if (!same_strides) {
+      inputTensor = inputTensor.contiguous();
       outputTensor = outputTensor.contiguous();
-      needs_output_copy = true;
     }
 
     MPSStream* mpsStream = getCurrentMPSStream();
@@ -997,7 +998,7 @@ void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
       getMPSProfiler().endProfileKernel(cplState);
     });
   }
-  if (needs_output_copy) {
+  if (!same_strides) {
     iter.output(0).copy_(outputTensor);
   }
 }

test/test_mps.py
@@ -8514,6 +8514,15 @@ class TestMPS(TestCaseMPS):
                 op(mps_x, out=mps_y)
                 self.assertEqual(mps_y, cpu_y)
 
+                # test for non contiguous input/output with similar strides
+                cpu_x = torch.randn(shape, device='cpu', dtype=dtype).mT
+                mps_x = cpu_x.to('mps')
+                cpu_y = torch.empty_like(cpu_x)
+                mps_y = cpu_y.to('mps')
+                op(cpu_x, out=cpu_y)
+                op(mps_x, out=mps_y)
+                self.assertEqual(mps_y, cpu_y)
+
 
         helper((5, 5), torch.exp, False)
         helper((5, 5), torch.cos, False)

commit 091e496d027989a80717652f1616aa4cb75a7da4

[MPS] Fix unary_kernel_strided logic
Fixes bug introduced by https://github.com/pytorch/pytorch/pull/148350
One can not avoid copies if both input and output tensors have the same strides, one needs to make sure that they are dense-in-storage (transposed tensor would be dense, but say selecting every odd and even column wouldn't)

Add regression test to prevent those from happening again

Though all this logic will sort of become irrelevant after https://github.com/pytorch/pytorch/pull/148468 is landed

aten/src/ATen/native/mps/OperationUtils.mm
@@ -965,8 +965,8 @@ void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
                                            std::optional<int64_t> extra) {
   auto inputTensor = iter.input(0);
   auto outputTensor = iter.output(0);
-  bool same_strides =
-      inputTensor.strides().equals(outputTensor.strides()) && inputTensor.sizes().equals(outputTensor.sizes());
+  bool is_dense_strided = is_dense_in_storage(inputTensor) && inputTensor.strides().equals(outputTensor.strides());
+  bool needs_output_copy = false;
   uint32_t length = outputTensor.numel();
   if (length == 0) {
     return;
@@ -977,9 +977,12 @@ void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
     cplState = getPipelineStateForFunc(fmt::format(
         "{}_dense_{}_{}", name, scalarToMetalTypeString(outputTensor), scalarToMetalTypeString(inputTensor)));
 
-    if (!same_strides) {
+    if (!is_dense_strided) {
       inputTensor = inputTensor.contiguous();
-      outputTensor = outputTensor.contiguous();
+      if (!outputTensor.is_contiguous()) {
+          outputTensor = outputTensor.contiguous();
+          needs_output_copy = true;
+      }
     }
 
     MPSStream* mpsStream = getCurrentMPSStream();
@@ -998,7 +1001,7 @@ void MetalShaderLibrary::exec_unary_kernel(TensorIteratorBase& iter,
       getMPSProfiler().endProfileKernel(cplState);
     });
   }
-  if (!same_strides) {
+  if (needs_output_copy) {
     iter.output(0).copy_(outputTensor);
   }
 }

test/test_mps.py
@@ -8514,7 +8514,7 @@ class TestMPS(TestCaseMPS):
                 op(mps_x, out=mps_y)
                 self.assertEqual(mps_y, cpu_y)
 
-                # test for non contiguous input/output with similar strides
+                # test for non contiguous but dense input/output with similar strides
                 cpu_x = torch.randn(shape, device='cpu', dtype=dtype).mT
                 mps_x = cpu_x.to('mps')
                 cpu_y = torch.empty_like(cpu_x)
@@ -8522,6 +8522,10 @@ class TestMPS(TestCaseMPS):
                 op(cpu_x, out=cpu_y)
                 op(mps_x, out=mps_y)
                 self.assertEqual(mps_y, cpu_y)
+                # test for sliced inputs and outputs with similar strides
+                mps_x, mps_y = torch.randn((2, shape[0]*2, shape[1]*2), device='mps', dtype=dtype).unbind(0)
+                op(mps_x[::2, ::2], out=mps_y[::2, ::2])
+                self.assertEqual(mps_y[::2, ::2], op(mps_x[::2, ::2].contiguous()))
 
 
         helper((5, 5), torch.exp, False)