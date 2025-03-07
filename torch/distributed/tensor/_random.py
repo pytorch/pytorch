906a6434c759d9adeca63c46370ce10196df661e
Goswami, Subrata <subrata.goswami@intel.com>
2025-03-07 10:54:46 -0800

    Adding device_type to fork_rng call.
    And address lintrunner  checks.

--- torch/distributed/tensor/_random.py
+++ torch/distributed/tensor/_random.py
@@ -159,7 +159,7 @@ class OffsetBasedRNGTracker(_RNGStateTracker):
     should be shared and synchronized among all ranks to respect the semantics of DTensor
     random operators.
 
-    note: _RNGStateTracker only supports cuda/cuda-like device. Xpu support is experimental.
+    note: _RNGStateTracker only supports cuda/cuda-like device.
     """
 
     def __init__(
@@ -170,7 +170,7 @@ class OffsetBasedRNGTracker(_RNGStateTracker):
         super().__init__(_resolve_device(device_mesh=device_mesh))
         assert self._device_handle is not None
         # DTensor RNG tracker so far only supports CUDA/CUDA-like devices
-        if self._device.type not in [ "cuda", "xpu"]:
+        if self._device.type not in ["cuda", "xpu"]:
             raise RuntimeError(
                 f"{self.__class__.__name__} instantiation requires the presence of "
                 f"CUDA/CUDA-like/XPU device. Got {self._device.type} instead."
@@ -198,7 +198,7 @@ class OffsetBasedRNGTracker(_RNGStateTracker):
         if self.distribute_region_enabled:
             old_offset = self.get_offset("parallel-rng")
             self._set_pre_op_offset(spec)
-            with torch.random.fork_rng(devices=[self._device]):
+            with torch.random.fork_rng(devices=[self._device], device_type=self._device.type):
                 assert self._device_handle is not None
                 self._device_handle.set_rng_state(self.rng_states["parallel-rng"])
                 try:
