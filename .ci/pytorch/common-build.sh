cb48e4d5415c11e09c51a0a02d4f90cd6461d477
Catherine Lee <csl@fb.com>
2025-03-07 10:07:27 -0800

    tc

--- .ci/pytorch/common-build.sh
+++ .ci/pytorch/common-build.sh
@@ -38,15 +38,18 @@ if [[ "$BUILD_ENVIRONMENT" != *win-* ]]; then
             # explicitly
             echo "Skipping sccache server initialization, setting environment variables"
             export SCCACHE_IDLE_TIMEOUT=0
-            export SCCACHE_ERROR_LOG=/var/lib/jenkins/workspace/sccache_error.log
-            export RUST_LOG=sccache::server=debug
-            export SCCACHE_LOG=debug
+            # export SCCACHE_ERROR_LOG=/var/lib/jenkins/workspace/sccache_error.log
+            # export RUST_LOG=sccache::server=debug
+            # export SCCACHE_LOG=debug
+            export SCCACHE_ERROR_LOG=~/sccache_error.log
+            export RUST_LOG=sccache::server=error
         elif [[ "${BUILD_ENVIRONMENT}" == *rocm* ]]; then
             SCCACHE_ERROR_LOG=~/sccache_error.log SCCACHE_IDLE_TIMEOUT=0 sccache --start-server
         else
             # increasing SCCACHE_IDLE_TIMEOUT so that extension_backend_test.cpp can build after this PR:
             # https://github.com/pytorch/pytorch/pull/16645
-            SCCACHE_ERROR_LOG=/var/lib/jenkins/workspace/sccache_error.log SCCACHE_IDLE_TIMEOUT=0 RUST_LOG=sccache::server=debug SCCACHE_LOG=debug sccache --start-server
+            # SCCACHE_ERROR_LOG=/var/lib/jenkins/workspace/sccache_error.log SCCACHE_IDLE_TIMEOUT=0 RUST_LOG=sccache::server=debug SCCACHE_LOG=debug sccache --start-server
+            SCCACHE_ERROR_LOG=~/sccache_error.log SCCACHE_IDLE_TIMEOUT=0 RUST_LOG=sccache::server=error sccache --start-server
         fi
 
         # Report sccache stats for easier debugging. It's ok if this commands
