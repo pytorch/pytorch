1a4cc8311572b599889d08bfbbaa6f93112e5a77
Xu Han <xu.han@outlook.com>
2025-03-09 23:26:55 +0800

    Update torch/_dynamo/test_case.py
    Co-authored-by: Jason Ansel <jansel@jansel.net>

--- torch/_dynamo/test_case.py
+++ torch/_dynamo/test_case.py
@@ -47,7 +47,7 @@ def run_tests(needs: Union[str, tuple[str, ...]] = ()) -> None:
     if TEST_WITH_TORCHDYNAMO or TEST_WITH_CROSSREF:
         return  # skip testing
 
-    if not torch.xpu.is_available() and check_skip_windows_inductor_UTs_switch():
+    if not torch.xpu.is_available() and IS_WINDOWS and os.environ.get("TORCHINDUCTOR_WINDOWS_TESTS", "0") == "0":
         return
 
     if isinstance(needs, str):
