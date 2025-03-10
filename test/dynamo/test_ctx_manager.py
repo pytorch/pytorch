653263d6138a27a9e0d77d611033be969c4d6d25
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-03-10 14:05:28 -0300

    Add `__context/cause/suppress_context/traceback__` to Exception
    ghstack-source-id: 3537d4fdc668e4a6d7b3112bbefff78644063adb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146499

--- test/dynamo/test_ctx_manager.py
+++ test/dynamo/test_ctx_manager.py
@@ -2899,7 +2899,7 @@ class CPythonContextManagerTestCase(torch._dynamo.test_case.TestCase):
 
         f(torch.randn(2))
 
-    @unittest.expectedFailure
+    @unittest.skipIf(sys.version_info < (3, 11), "Python 3.11+")
     def test_contextmanager_except(self):
         state = []
 
