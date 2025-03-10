0105041be58bb266c155cbba23436a5f22efbdea
ZhiweiYan-96 <zhiwei.yan@intel.com>
2025-03-10 08:01:00 +0000

    Facilitate at::_weight_int4pack_mm_with_scale_and_zeros related registration
    ghstack-source-id: af6be0f613e128ba5c3914edb9d5d5c26ba61d14
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147962

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -3609,7 +3609,7 @@ def meta__convert_weight_to_int4pack_for_cpu(w, inner_k_tiles):
 @register_meta([aten._weight_int4pack_mm])
 def meta__weight_int4pack_mm(x, w, q_group_size, q_scale_and_zeros):
     torch._check(x.dim() == 2, lambda: "x must be a 2D tensor")
-    torch._check(w.dim() == 4, lambda: "w must be a 4D tensor")
+    torch._check(w.dim() == 2 or w.dim() == 4, lambda: "w must be a 4D tensor")
     torch._check(
         x.dtype in [torch.float32, torch.float16, torch.bfloat16],
         lambda: f"expected x to be f32/f16/bf16, got {x.dtype}",
@@ -3618,7 +3618,9 @@ def meta__weight_int4pack_mm(x, w, q_group_size, q_scale_and_zeros):
         w.dtype is torch.int32,
         lambda: f"expected w to be int32, got {w.dtype}",
     )
-    return x.new_empty(x.size(0), w.size(0) * 8, dtype=x.dtype)
+    return x.new_empty(
+        x.size(0), w.size(0) * 8 if w.dim() == 4 else w.size(0), dtype=x.dtype
+    )
 
 
 @register_meta([aten._weight_int4pack_mm_for_cpu])
@@ -3631,6 +3633,21 @@ def meta__weight_int4pack_mm_for_cpu(x, w, q_group_size, q_scale_and_zeros):
     )
     torch._check(
         w.dtype is torch.uint8,
+        lambda: f"expected w to be uint8, got {w.dtype}",
+    )
+    return x.new_empty(x.size(0), w.size(0), dtype=x.dtype)
+
+
+@register_meta([aten._weight_int4pack_mm_with_scales_and_zeros])
+def _weight_int4pack_mm_with_scales_and_zeros(x, w, q_group_size, qScale, qZeros):
+    torch._check(x.dim() == 2, lambda: "x must be a 2D tensor")
+    torch._check(w.dim() == 2, lambda: "w must be a 2D tensor")
+    torch._check(
+        x.dtype in [torch.float32, torch.float16, torch.bfloat16],
+        lambda: f"expected x to be f32/f16/bf16, got {x.dtype}",
+    )
+    torch._check(
+        w.dtype is torch.int32,
         lambda: f"expected w to be uint8, got {w.dtype}",
     )
     return x.new_empty(x.size(0), w.size(0), dtype=x.dtype)
