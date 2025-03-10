67e4d006f06a8978a17c99811dfb2deb40542ec4
Nichols A. Romero <nick.romero@amd.com>
2025-03-10 22:35:40 +0000

    Use ternary operator to write more compact C++ code.

--- aten/src/ATen/cuda/tunable/GemmCommon.h
+++ aten/src/ATen/cuda/tunable/GemmCommon.h
@@ -588,15 +588,9 @@ struct ScaledGemmParams : OpParams {
     // params.bias_dtype = bias ? bias->scalar_type() : isFloat8Type(out_dtype_) ? at::ScalarType::Half : out_dtype_;
     //
     // In TunableOp, we must distinguish in param signature these two cases: with and without a bias vector.
-    std::string bias_dtype_string;
-    if (bias_ptr == nullptr) {
-      bias_dtype_string = "None";
-    }
-    else {
-      bias_dtype_string = at::toString(bias_dtype);
-    }
     return fmt::sprintf("%c%c_%ld_%ld_%ld_ld_%ld_%ld_%ld_rw_%d_bias_%s",
-      transa, transb, m, n, k, lda, ldb, ldc, use_rowwise, bias_dtype_string);
+      transa, transb, m, n, k, lda, ldb, ldc, use_rowwise,
+      bias_ptr == nullptr ? "None" : at::toString(bias_dtype));
   }
 
   size_t GetSizeA() const {
