e57e68143cae8c64422d3cff3194a7222c271c5d
atalman <atalman@fb.com>
2025-03-11 16:41:18 -0700

    remove_old_python

--- .github/scripts/get_ci_variable.py
+++ .github/scripts/get_ci_variable.py
@@ -2,19 +2,7 @@
 """Helper script - Return CI variables such as stable cuda, min python version, etc."""
 
 import argparse
-import re
 import sys
-from typing import Optional
-
-
-def parse_version(version_string: str) -> Optional[tuple[int, ...]]:
-    pattern = r"(\d+)\.(\d+)?"
-    match = re.match(pattern, version_string)
-
-    if match:
-        return tuple(int(group) for group in match.groups())
-    else:
-        return None
 
 
 def main(args: list[str]) -> None:
@@ -31,21 +19,11 @@ def main(args: list[str]) -> None:
         action="store_true",
         help="get min supported python version",
     )
-    parser.add_argument(
-        "--old-python-version",
-        action="store_true",
-        help="get min supported python version - 0.1",
-    )
     options = parser.parse_args(args)
     if options.cuda_stable_version:
         return print(generate_binary_build_matrix.CUDA_STABLE)
     if options.min_python_version:
         return print(generate_binary_build_matrix.FULL_PYTHON_VERSIONS[0])
-    if options.old_python_version:
-        version = parse_version(generate_binary_build_matrix.FULL_PYTHON_VERSIONS[0])
-        if version is not None:
-            major, minor = version
-            return print(f"{major}.{minor - 1}")
 
 
 if __name__ == "__main__":
