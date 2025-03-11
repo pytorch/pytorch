46852109042c0d0fb58f48986713f95a46ce20bc
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-03-11 14:40:58 +0000

    Revert "[WIP] Initial implementation of Grouped Gemm API (#148531)"
    This reverts commit ff29791ed8f815bdbca1a5606de046380baca69d.
    
    Reverted https://github.com/pytorch/pytorch/pull/148531 on behalf of https://github.com/janeyx99 due to Sorry but this broke ROCm jobs on trunk ([comment](https://github.com/pytorch/pytorch/pull/148531#issuecomment-2714577498))

--- aten/src/ATen/native/cuda/RowwiseScaledMM.cu
+++ aten/src/ATen/native/cuda/RowwiseScaledMM.cu
@@ -946,6 +946,7 @@ void dispatch_fp8_rowwise_kernel_on_input_dtypes(
   }
 }
 
+template <typename... Types>
 void dispatch_fp8_rowwise_kernel_on_bias_dtype(
     at::Tensor XQ,
     at::Tensor WQ,
@@ -956,13 +957,12 @@ void dispatch_fp8_rowwise_kernel_on_bias_dtype(
     at::Tensor out) {
   if (bias.has_value() && bias->dtype() == at::kBFloat16) {
     dispatch_fp8_rowwise_kernel_on_input_dtypes<
-        cutlass::bfloat16_t>
-        (XQ, WQ, x_scale, w_scale, bias, use_fast_accum, out);
+        cutlass::bfloat16_t,
+        Types...>(XQ, WQ, x_scale, w_scale, bias, use_fast_accum, out);
   } else {
     dispatch_fp8_rowwise_kernel_on_input_dtypes<
-        float>
-        //Types...>
-        (XQ, WQ, x_scale, w_scale, bias, use_fast_accum, out);
+        float,
+        Types...>(XQ, WQ, x_scale, w_scale, bias, use_fast_accum, out);
   }
 }
 
