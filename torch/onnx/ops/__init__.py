cd6808188ace5a259e4f29773a55b3156b4857ad
Justin Chu <justinchuby@users.noreply.github.com>
2025-03-12 12:23:25 -0700

    interface

--- torch/onnx/ops/__init__.py
+++ torch/onnx/ops/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Sequence
 from typing import Any, Optional, Union, Literal
 import torch
@@ -27,12 +29,29 @@ def symbolic(
     *,
     dtype: torch.dtype,
     shape: Sequence[int | torch.SymInt],
-    version: Optional[int] = None,
-    matadata_props: Optional[dict[str, str]] = None,
-    num_outputs: int = 1,
+    version: int | None = None,
+    matadata_props: dict[str, str] | None = None,
 ) -> torch.Tensor:
-    return _impl._symbolic(...)
-
+    attr_keys, attr_ints, attr_floats, attr_strs, attr_bools, attr_tensors = (
+        _impl.encode_onnx_attrs(attrs)
+    )
+    # TODO: Convert dtype
+    return _impl._symbolic(
+        inputs,
+        domain_op,
+        dtype,
+        attr_tensors,
+        shape=shape,
+        attr_keys=attr_keys,
+        attr_ints=attr_ints,
+        attr_floats=attr_floats,
+        attr_strs=attr_strs,
+        attr_bools=attr_bools,
+        metadata_props_keys=matadata_props.keys() if matadata_props else [],
+        metadata_props_values=matadata_props.values() if matadata_props else [],
+        domain=domain_op,
+        version=version,
+    )
 
 
 def symbolic_multi_out(
@@ -54,10 +73,28 @@ def symbolic_multi_out(
     ]
     | None = None,
     *,
-    dtype: torch.dtype,
-    shape: Sequence[int | torch.SymInt],
-    version: Optional[int] = None,
-    matadata_props: Optional[dict[str, str]] = None,
     num_outputs: int,
-) -> Sequence[torch.Tensor]: ...
-    return _impl._symbolic_multi_out(...)
+    dtypes: torch.dtype,
+    shapes: Sequence[Sequence[Union[int, torch.SymInt]]],
+    version: int | None = None,
+    matadata_props: dict[str, str] | None = None,
+) -> Sequence[torch.Tensor]:
+    attr_keys, attr_ints, attr_floats, attr_strs, attr_bools, attr_tensors = (
+        _impl.encode_onnx_attrs(attrs)
+    )
+    return _impl._symbolic_multi_out(
+        inputs,
+        domain_op,
+        dtypes,
+        attr_tensors,
+        shape=shapes,
+        attr_keys=attr_keys,
+        attr_ints=attr_ints,
+        attr_floats=attr_floats,
+        attr_strs=attr_strs,
+        attr_bools=attr_bools,
+        metadata_props_keys=matadata_props.keys() if matadata_props else [],
+        metadata_props_values=matadata_props.values() if matadata_props else [],
+        domain=domain_op,
+        version=version,
+    )
