161a92587f46d36625520a7f162466f6da657dba
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-03-12 22:45:39 +0000

    Revert "Make dynamism code robust to NotImplementedException (#148823)"
    This reverts commit 60576419a2a5cc09e4a92be870fda8f3fc305ddc.
    
    Reverted https://github.com/pytorch/pytorch/pull/148823 on behalf of https://github.com/ZainRizvi due to Sorry but this is breaking internally, see D71042206 for details. To validate your fixes internally before relanding, you can follow the instructions here: https://fburl.com/fixing-ghfirst-reverts ([comment](https://github.com/pytorch/pytorch/pull/148823#issuecomment-2719287467))

--- torch/fx/experimental/_dynamism.py
+++ torch/fx/experimental/_dynamism.py
@@ -29,21 +29,14 @@ def module_to_nested_dict(module: torch.nn.Module) -> dict[str, Any]:
     self_dict["_modules"] = {}
 
     for attr_name in dir(module):
-        try:
-            if not attr_name.startswith("_") and not callable(
-                getattr(module, attr_name)
+        if not attr_name.startswith("_") and not callable(getattr(module, attr_name)):
+            attr_value = getattr(module, attr_name)
+            if (
+                not isinstance(attr_value, torch.nn.Module)
+                and isinstance(attr_value, (int, float, torch.Tensor))
+                and type(attr_value) is not bool
             ):
-                attr_value = getattr(module, attr_name)
-                if (
-                    not isinstance(attr_value, torch.nn.Module)
-                    and isinstance(attr_value, (int, float, torch.Tensor))
-                    and type(attr_value) is not bool
-                ):
-                    self_dict[attr_name] = attr_value
-        except NotImplementedError:
-            # Skip attributes that raise NotImplementedError since they won't
-            # contain any dynamism anyways.
-            continue
+                self_dict[attr_name] = attr_value
 
     for name, param in module.named_parameters(recurse=False):
         self_dict["_parameters"][name] = param
