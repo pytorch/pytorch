099ef602d7598158c36daadd3ea8eaf80a90148f
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-03-13 22:11:06 +0800

    Update torch/_dynamo/exc.py
    Co-authored-by: Guilherme Leobas <guilhermeleobas@gmail.com>

--- torch/_dynamo/exc.py
+++ torch/_dynamo/exc.py
@@ -364,7 +364,7 @@ def raise_observed_exception(
     # stack and raise the exception.
     exception_vt = BuiltinVariable(exc_type).call_function(tx, args or [], kwargs or {})  # type: ignore[arg-type]
     tx.exn_vt_stack.set_current_exception(exception_vt)
-    raise observed_exception_map[exc_type](*(observed_exception_args or []))
+    raise observed_exception_map[exc_type](args or [])
 
 
 def handle_observed_exception(tx: Any) -> None:
