a12a9e79c369693daaba59a1baeb6b5589894c74
wdziurdz <witold.dziurdz@intel.com>
2025-03-13 08:03:48 +0000

    [Profiler][HPU] Fix incorrect availabilities for HPU (#148663)
    Fixes #148661
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148663
    Approved by: https://github.com/jeromean, https://github.com/albanD
    
    (cherry picked from commit 75c8b7d9725af59d8348379a4165d1252c4ac208)

--- torch/csrc/autograd/init.cpp
+++ torch/csrc/autograd/init.cpp
@@ -396,6 +396,9 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
     if (at::hasXPU()) {
       activities.insert(torch::profiler::impl::ActivityType::XPU);
     }
+    if (at::hasHPU()) {
+      activities.insert(torch::profiler::impl::ActivityType::HPU);
+    }
     if (at::hasMTIA()) {
       activities.insert(torch::profiler::impl::ActivityType::MTIA);
     }
