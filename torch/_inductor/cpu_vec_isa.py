7812e17c4d9a9410e80fcc1d51f555a9d70a4186
Ryo Suzuki <ryo.suzuki@arm.com>
2025-03-13 16:51:49 +0000

    Change compile flag to armv8

--- torch/_inductor/cpu_vec_isa.py
+++ torch/_inductor/cpu_vec_isa.py
@@ -176,7 +176,7 @@ class VecSVE256(VecISA):
         "CPU_CAPABILITY_SVE256",
         "AT_BUILD_ARM_VEC256_WITH_SLEEF",
     ]
-    _arch_flags = "-march=armv8.2-a+sve -msve-vector-bits=256"
+    _arch_flags = "-march=armv8-a+sve -msve-vector-bits=256"
     _dtype_nelements = {torch.float: 8, torch.bfloat16: 16, torch.float16: 16}
 
     def __str__(self) -> str:
@@ -195,7 +195,7 @@ class VecSVE256_BF16(VecSVE256):
         "AT_BUILD_ARM_VEC256_WITH_SLEEF",
         "__ARM_FEATURE_BF16",
     ]
-    _arch_flags = "-march=armv8.2-a+sve+bf16 -msve-vector-bits=256"
+    _arch_flags = "-march=armv8-a+sve+bf16 -msve-vector-bits=256"
 
     __hash__: Callable[[VecISA], Any] = VecISA.__hash__
 
