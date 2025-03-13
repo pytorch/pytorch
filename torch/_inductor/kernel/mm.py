4e19aad543e1c15f48173cdb695c66f6cf6325f9
atalman <atalman@fb.com>
2025-03-13 11:36:05 -0700

    lint

--- torch/_inductor/kernel/mm.py
+++ torch/_inductor/kernel/mm.py
@@ -55,16 +55,6 @@ from .mm_common import (
 )
 
 
-def parse_version(version_string: str) -> Optional[tuple[int, ...]]:
-    pattern = r"(\d+)\.(\d+)?"
-    match = re.match(pattern, version_string)
-
-    if match:
-        return tuple(int(group) for group in match.groups())
-    else:
-        return None
-
-
 try:
     import triton
 
@@ -146,8 +136,7 @@ mm_template = TritonTemplate(
     # inductor generates a suffix
     {{store_output(("idx_m", "idx_n"), "acc", "mask")}}
 """
-        if (torch.version.hip is None)
-        or (has_triton and triton_version >= "3.3.0")
+        if (torch.version.hip is None) or (has_triton and triton_version >= "3.3.0")
         # FIXME: To get around rocm failures like https://github.com/pytorch/pytorch/actions/runs/13123783322/job/36617154943
         # The only difference between the two templates is M >= BLOCK_M and N >= BLOCK_N checking.
         # See more details in https://github.com/pytorch/pytorch/pull/146293
