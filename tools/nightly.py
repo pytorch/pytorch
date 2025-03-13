b9afe1a99b9661ab2f250d4a1ec846ddaa37ebcb
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-03-14 02:37:19 +0800

    Update
    [ghstack-poisoned]

--- tools/nightly.py
+++ tools/nightly.py
@@ -129,15 +129,15 @@ PIP_SOURCES = {
         supported_platforms={"Linux", "Windows"},
         accelerator="cuda",
     ),
-    "cuda-12.4": PipSource(
-        name="cuda-12.4",
-        index_url=f"{PYTORCH_NIGHTLY_PIP_INDEX_URL}/cu124",
-        supported_platforms={"Linux", "Windows"},
-        accelerator="cuda",
-    ),
     "cuda-12.6": PipSource(
         name="cuda-12.6",
         index_url=f"{PYTORCH_NIGHTLY_PIP_INDEX_URL}/cu126",
+        supported_platforms={"Linux", "Windows"},
+        accelerator="cuda",
+    ),
+    "cuda-12.8": PipSource(
+        name="cuda-12.8",
+        index_url=f"{PYTORCH_NIGHTLY_PIP_INDEX_URL}/cu128",
         supported_platforms={"Linux", "Windows"},
         accelerator="cuda",
     ),
