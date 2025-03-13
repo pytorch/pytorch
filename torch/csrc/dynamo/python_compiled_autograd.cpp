ceac2bc9bfc7b5376bef43bbdce1bcf40bac5efe
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-03-13 17:04:45 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/dynamo/python_compiled_autograd.cpp
+++ torch/csrc/dynamo/python_compiled_autograd.cpp
@@ -152,7 +152,7 @@ static variable_list call_function(
       jit::toPyObject(output_metadata));
 
   // Convert the output from PyObject* to vector<Tensor>
-  auto tmp = py::cast<std::vector<std::optional<at::Tensor>>>(std::move(stuff));
+  auto tmp = py::cast<std::vector<std::optional<at::Tensor>>>(stuff);
   return toTensorList(tmp);
 }
 
@@ -163,7 +163,7 @@ struct PyCompilerInterfaceImpl : PyCompilerInterface {
       functional_apply_t fn,
       std::vector<at::TypePtr> packed_args_schema,
       bool is_custom_function = false,
-      bool is_traceable = true) const override {
+      bool is_traceable = true) override {
     return torch::dynamo::autograd::bind_function(
         py_compiler,
         fn_name,
@@ -178,7 +178,7 @@ struct PyCompilerInterfaceImpl : PyCompilerInterface {
       const std::string& fn_name,
       const variable_list& inputs,
       const ivalue_list& packed_args,
-      const c10::IValue& output_metadata) const override {
+      const c10::IValue& output_metadata) override {
     return torch::dynamo::autograd::call_function(
         py_compiler,
         method_name,
@@ -191,35 +191,28 @@ struct PyCompilerInterfaceImpl : PyCompilerInterface {
       PyObject* py_compiler,
       const variable_list& inputs,
       const at::TensorGeometry& base,
-      const at::TensorGeometry& view) const override {
+      const at::TensorGeometry& view) override {
     py::handle handle(py_compiler);
-    py::object stuff = handle.attr("call_copy_slices_prologue")(
-        inputs,
-        base.sym_sizes(),
-        base.sym_strides(),
-        base.sym_storage_offset(),
-        view.sym_sizes(),
-        view.sym_strides(),
-        view.sym_storage_offset());
-    return py::cast<std::vector<at::Tensor>>(std::move(stuff));
+    py::object stuff =
+        handle.attr("call_copy_slices_prologue")(inputs, base, view);
+    return py::cast<std::vector<at::Tensor>>(stuff);
   }
   variable_list call_copy_slices_epilogue(
       PyObject* py_compiler,
       const std::vector<bool>& needs_input_grad,
       const at::Tensor& result,
       const variable_list& res,
-      const at::Tensor& grad_slice) const override {
+      const at::Tensor& grad_slice) override {
     py::handle handle(py_compiler);
     py::object stuff = handle.attr("call_copy_slices_epilogue")(
         needs_input_grad, result, res, grad_slice);
-    auto output =
-        py::cast<std::vector<std::optional<at::Tensor>>>(std::move(stuff));
+    auto output = py::cast<std::vector<std::optional<at::Tensor>>>(stuff);
     return toTensorList(output);
   }
   at::Tensor call_unpack(
       PyObject* py_compiler,
       std::optional<size_t> hook_id,
-      size_t hook_input_id) const override {
+      size_t hook_input_id) override {
     py::handle handle(py_compiler);
     py::object proxy = handle.attr("unpack_hook")(hook_id, hook_input_id);
     auto tmp = py::cast<std::optional<at::Tensor>>(proxy);
@@ -714,7 +707,7 @@ static at::Tensor call_accumulate(
   }
   py::handle handle(py_compiler);
   py::object stuff = handle.attr("accumulate")(old_var, new_var);
-  return py::cast<at::Tensor>(std::move(stuff));
+  return py::cast<at::Tensor>(stuff);
 }
 
 static TraceState call_begin_capture(
