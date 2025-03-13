ce13c73ff7beaee345981d1ee77d682db7502166
Justin Chu <justinchuby@users.noreply.github.com>
2025-03-13 11:07:32 -0700

    Create docs

--- torch/onnx/ops/__init__.py
+++ torch/onnx/ops/__init__.py
@@ -10,6 +10,7 @@ if TYPE_CHECKING:
     from collections.abc import Sequence
 
 
+# https://github.com/onnx/onnx/blob/f542e1f06699ea7e1db5f62af53355b64338c723/onnx/onnx.proto#L597
 _TORCH_DTYPE_TO_ONNX_DTYPE = {
     torch.float32: 1,  # FLOAT
     torch.uint8: 2,  # UINT8
@@ -69,6 +70,45 @@ def symbolic(
     version: int | None = None,
     metadata_props: dict[str, str] | None = None,
 ) -> torch.Tensor:
+    """Create a symbolic ONNX operator for exporting to ONNX.
+
+    This function is used to create a symbolic ONNX operator with a single output.
+    To create an operator with multiple outputs, use :func:`symbolic_multi_out`.
+
+    Example::
+        class CustomOp(torch.nn.Module):
+            def forward(self, x: torch.Tensor):
+                return torch.onnx.ops.symbolic(
+                    "custom_domain::CustomOp",
+                    (x,),
+                    dict(attr_key="attr_value"),
+                    dtype=x.dtype,
+                    shape=x.shape,
+                    version=1,
+                )
+                # This will create a symbolic ONNX operator with the name "CustomOp" in the "custom_domain" domain.
+                # The output tensor will have the specified dtype and shape.
+
+
+        # You may then export this model to ONNX using torch.onnx.export.
+
+    Args:
+        domain_op: The domain and operator name, separated by "::". For example,
+            "custom_domain::CustomOp".
+        inputs: The input tensors to the operator.
+        attrs: The attributes of the operator. The keys are attribute names and
+            the values are attribute values.
+        dtype: The data type of the output tensor.This can be either a torch.dtype
+            or an integer representing the ONNX data type.
+        shape: The shape of the output tensor. This can be a list of integers or
+            SymInt values.
+        version: The version of the opset used for the operator.
+        metadata_props: Metadata properties for the ONNX node.
+            This is a dictionary of str-str pairs.
+
+    Returns:
+        The output tensor of the operator.
+    """
     if not isinstance(dtype, int):
         if dtype not in _TORCH_DTYPE_TO_ONNX_DTYPE:
             raise ValueError(f"Unsupported dtype: {dtype}")
@@ -119,6 +159,43 @@ def symbolic_multi_out(
     version: int | None = None,
     metadata_props: dict[str, str] | None = None,
 ) -> Sequence[torch.Tensor]:
+    """Create a symbolic ONNX operator with multiple outputs.
+
+    Example::
+        class CustomOp(torch.nn.Module):
+            def forward(self, x: torch.Tensor):
+                return torch.onnx.ops.symbolic(
+                    "custom_domain::CustomOp",
+                    (x,),
+                    dict(attr_key="attr_value"),
+                    dtypes=(x.dtype, torch.float32),
+                    shapes=(x.shape, [1, 2, 3]),
+                    version=1,
+                )
+                # This will create a symbolic ONNX operator with the name "CustomOp" in the "custom_domain" domain.
+                # The output tensor will have the specified dtype and shape.
+
+
+        # You may then export this model to ONNX using torch.onnx.export.
+
+    Args:
+        domain_op: The domain and operator name, separated by "::". For example,
+            "custom_domain::CustomOp".
+        inputs: The input tensors to the operator.
+        attrs: The attributes of the operator. The keys are attribute names and
+            the values are attribute values.
+        dtypes: The data types of the output tensors. This can be a list of
+            torch.dtype or integers representing the ONNX data types. The length
+            of this list must be the number of outputs.
+        shapes: The shapes of the output tensors. This can be a list of lists of
+            integers or SymInt values. The length of this list must be the number of outputs.
+        version: The version of the opset used for the operator.
+        metadata_props: Metadata properties for the ONNX node.
+            This is a dictionary of str-str pairs.
+
+    Returns:
+        A list of output tensors of the operator.
+    """
     onnx_dtypes = []
     for dtype in dtypes:
         if not isinstance(dtype, int):
