fcbc067bb01b5bbd6e9a4968e91d97e912c5bb0e
Aaron Gokaslan <aaronGokaslan@gmail.com>
2025-03-15 10:41:26 -0400

    [BE]: Update CU126 to CUDNN 12.8 too

--- .ci/docker/common/install_cuda_aarch64.sh
+++ .ci/docker/common/install_cuda_aarch64.sh
@@ -4,7 +4,7 @@
 set -ex
 
 NCCL_VERSION=v2.21.5-1
-CUDNN_VERSION=9.5.1.17
+CUDNN_VERSION=9.8.0.87
 
 function install_cusparselt_062 {
     # cuSparseLt license: https://docs.nvidia.com/cuda/cusparselt/license.html
@@ -161,7 +161,6 @@ function prune_126 {
 }
 
 function install_128 {
-  CUDNN_VERSION=9.8.0.87
   echo "Installing CUDA 12.8.0 and cuDNN ${CUDNN_VERSION} and NCCL ${NCCL_VERSION} and cuSparseLt-0.6.3"
   rm -rf /usr/local/cuda-12.8 /usr/local/cuda
   # install CUDA 12.8.0 in the same container
