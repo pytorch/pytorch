d5d0c8e51c0e72b0f0becedc4bc5a5dcf48d2609
Aaron Gokaslan <aaronGokaslan@gmail.com>
2025-03-16 23:52:58 +0000

    [BE]: Apply ruff PERF403 to use dict comprehensions more often (#149257)
    Fixes #ISSUE_NUMBER
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/149257
    Approved by: https://github.com/jansel

--- test/test_fx.py
+++ test/test_fx.py
@@ -2324,9 +2324,7 @@ class TestFX(JitTestCase):
 
         copied_graph = copy.deepcopy(g)
 
-        val_map = {}
-        for orig_node, new_node in zip(g.nodes, copied_graph.nodes):
-            val_map[orig_node] = new_node
+        val_map = dict(zip(g.nodes, copied_graph.nodes))
 
         for orig_node, new_node in zip(g.nodes, copied_graph.nodes):
             orig_users = set(orig_node.users.keys())
--- torch/distributed/checkpoint/state_dict.py
+++ torch/distributed/checkpoint/state_dict.py
@@ -596,8 +596,7 @@ def _load_model_state_dict(
             )
         elif info.full_state_dict:
             _distribute_state_dict(state_dict, local_state_dict, device=devices.pop())
-        for fqn, local_state in local_state_dict.items():
-            state_dict[fqn] = local_state
+        state_dict.update(local_state_dict)
 
     with info.fsdp_context():
         return cast(
