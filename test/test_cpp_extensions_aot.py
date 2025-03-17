55c2a5116950d170cddaccc18e1513b2fc0e0b38
Jane Xu <janeyx@meta.com>
2025-03-17 13:06:02 -0700

    Update on "[BE] simplify test_cpp_extensions_aot and .gitignore"
    It is shady to clean up an install mid-test. So don't do that anymore and use .gitignore instead.
    
    
    
    
    [ghstack-poisoned]

--- test/test_cpp_extensions_aot.py
+++ test/test_cpp_extensions_aot.py
@@ -3,7 +3,6 @@
 import os
 import re
 import subprocess
-import sys
 import unittest
 from itertools import repeat
 from pathlib import Path
@@ -16,7 +15,6 @@ import torch.utils.cpp_extension
 from torch.testing._internal.common_cuda import TEST_CUDA
 from torch.testing._internal.common_utils import (
     IS_WINDOWS,
-    shell,
     skipIfTorchDynamo,
     TEST_XPU,
     xfailIfTorchDynamo,
@@ -216,7 +214,6 @@ class TestCppExtensionAOT(common.TestCase):
 
         missing_symbols = subprocess.check_output(["nm", "-u", so_file]).decode("utf-8")
         self.assertFalse("Py" in missing_symbols)
-
 
     @unittest.skipIf(not TEST_CUDA, "some aspects of this test require CUDA")
     def test_libtorch_agnostic(self):
