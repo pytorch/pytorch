f1d49e997dd05b6207850fbd674f7d85225c0425
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-03-18 00:10:50 +0000

    Revert "[dynamo][guards][serialization] Dont use ID_MATCH guard for bool and None (#149228)"
    This reverts commit 5905bbe745b0acb4909243c93014c0e6f3512c2d.
    
    Reverted https://github.com/pytorch/pytorch/pull/149228 on behalf of https://github.com/malfet due to I wonder if this will fix the pr-time-benchmark regressions ([comment](https://github.com/pytorch/pytorch/pull/149228#issuecomment-2731237949))

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -1464,35 +1464,6 @@ class GuardBuilder(GuardBuilderBase):
             not invert, key, get_verbose_code_parts(code, guard)
         )
 
-    def BOOL_MATCH(self, guard: Guard):
-        # checks val == True or val == False
-        ref = self.arg_ref(guard)
-        val = self.get(guard.name)
-        assert istype(val, bool)
-        code = [f"{ref} == {val!r}"]
-        self._set_guard_export_info(guard, code)
-
-        if val:
-            self.get_guard_manager(guard).add_true_match_guard(
-                get_verbose_code_parts(code, guard)
-            )
-        else:
-            self.get_guard_manager(guard).add_false_match_guard(
-                get_verbose_code_parts(code, guard)
-            )
-
-    def NONE_MATCH(self, guard: Guard):
-        # checks `val is None`
-        ref = self.arg_ref(guard)
-        val = self.get(guard.name)
-        assert val is None
-        code = [f"{ref} is None"]
-        self._set_guard_export_info(guard, code)
-
-        self.get_guard_manager(guard).add_none_match_guard(
-            get_verbose_code_parts(code, guard)
-        )
-
     def ID_MATCH(self, guard: Guard):
         # ___check_obj_id is same as `id(x) == y`
         if isinstance(guard.originating_source, TypeSource):
@@ -1711,11 +1682,7 @@ class GuardBuilder(GuardBuilderBase):
 
     def CONSTANT_MATCH(self, guard: Guard):
         val = self.get(guard.name)
-        if istype(val, bool):
-            self.BOOL_MATCH(guard)
-        elif val is None:
-            self.NONE_MATCH(guard)
-        elif istype(val, types.CodeType):
+        if istype(val, (bool, type(None), types.CodeType)):
             self.ID_MATCH(guard)
         else:
             self.EQUALS_MATCH(guard)
