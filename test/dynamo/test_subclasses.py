421ae72ed87e74e5b5ef02bcbb0bf34e08e931b7
Michael Lazos <mlazos@meta.com>
2025-03-19 00:27:12 -0700

    [Dynamo] Support the torch._C.DisableTorchFunction ctx manager
    [ghstack-poisoned]

--- test/dynamo/test_subclasses.py
+++ test/dynamo/test_subclasses.py
@@ -522,6 +522,22 @@ class SubclassTests(torch._dynamo.test_case.TestCase):
         res, _ = fn(input)
         self.assertFalse(res)
 
+    def test_disable_all_torch_function(self):
+        @torch.compile(backend="eager")
+        def fn(x):
+            with torch._C.DisableTorchFunction():
+                torch._dynamo.graph_break()
+                return (
+                    torch._C._is_torch_function_enabled(),
+                    torch._C._is_torch_function_all_disabled(),
+                    torch.add(x, 1.0),
+                )
+
+        input = torch.ones(2, 2)
+        res1, res2, _ = fn(input)
+        self.assertFalse(res1)
+        self.assertTrue(res2)
+
     def test_torch_function_state_nested(self):
         @torch.compile(backend="eager")
         def fn(x):
