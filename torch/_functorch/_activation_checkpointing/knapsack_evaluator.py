2b633362f551ab070d96f33bdf822fbedbfc48d4
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2025-03-20 16:42:47 -0700

    Do not depend on numpy during the import
    Fixes https://github.com/pytorch/pytorch/issues/149681

--- torch/_functorch/_activation_checkpointing/knapsack_evaluator.py
+++ torch/_functorch/_activation_checkpointing/knapsack_evaluator.py
@@ -2,7 +2,6 @@ from collections import deque
 from typing import Callable
 
 import networkx as nx
-import numpy as np
 
 from torch._functorch._activation_checkpointing.graph_info_provider import (
     GraphInfoProvider,
@@ -237,6 +236,7 @@ class KnapsackEvaluator:
         Returns:
             float: Memory budget at the knee point.
         """
+        import numpy as np
         results = self.evaluate_distribution_of_results_for_knapsack_algo(
             knapsack_algo=knapsack_algo,
             memory_budget_values=np.linspace(  # type: ignore[arg-type]
