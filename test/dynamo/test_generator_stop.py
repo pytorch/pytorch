48a0119599f1726d583a18252bd44e41b64588b5
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-03-20 11:04:39 -0300

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/test_generator_stop.py
+++ test/dynamo/test_generator_stop.py
@@ -8,16 +8,9 @@ import torch._dynamo.test_case
 from torch.testing._internal.common_utils import make_dynamo_test
 
 
-class TestPEP479(torch._dynamo.test_case.TestCase):
+class TestPEP479(torch._dynamo.test_case.CPythonTestCase):
     # Tests taken from CPython source code in cpython/Lib/test/test_generator_stop.py
     # https://github.com/python/cpython/blob/v3.13.1/Lib/test/test_generator_stop.py
-    def setUp(self):
-        self._prev = torch._dynamo.config.enable_trace_unittest
-        torch._dynamo.config.enable_trace_unittest = True
-
-    def tearDown(self):
-        torch._dynamo.config.enable_trace_unittest = self._prev
-
     @unittest.skipIf(sys.version_info < (3, 12), "Test does not work in Python < 3.12")
     @make_dynamo_test
     def test_stopiteration_wrapping(self):
