86bf082fe0c90230fa98f6046d40fbd0760c62a8
Andrey Talman <atalman@fb.com>
2025-03-20 07:38:01 -0700

    Update .circleci/scripts/binary_populate_env.sh

--- .circleci/scripts/binary_populate_env.sh
+++ .circleci/scripts/binary_populate_env.sh
@@ -75,7 +75,7 @@ TRITON_VERSION=$(cat $PYTORCH_ROOT/.ci/docker/triton_version.txt)
 # Here PYTORCH_EXTRA_INSTALL_REQUIREMENTS is already set for the all the wheel builds hence append TRITON_CONSTRAINT
 TRITON_CONSTRAINT="platform_system == 'Linux' and platform_machine == 'x86_64'"
 
-# This is Linux aarch64 CUDA build. We do have triton build for it. Hence add triton constrain
+# CUDA 12.8 builds have triton for Linux and Linux aarch64 binaries. 
 if [[ "$DESIRED_CUDA" == cu128 ]]; then
   TRITON_CONSTRAINT="platform_system == 'Linux'"
 fi
