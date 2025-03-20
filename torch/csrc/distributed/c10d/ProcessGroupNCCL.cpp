c277f9b150ffb2cf30888394d64de97e9bec876a
Shunzhi Wen <shunzhi.wen@arm.com>
2025-03-20 16:50:24 +0000

    Enable complex datatype support in ProcessGroupGloo
    - Enable communication of tensors with Complex datatype in ProcessGroupGloo,
    similar to how ProcessGroupNCCL handles it.
    - Move a function, which checks if Complex datatype is supported by a reduce
    operation, from ProcessGroupNCCL.cpp into a new file to be shared with
    ProcessGroupGloo.
    
    Signed-off-by: Shunzhi Wen <shunzhi.wen@arm.com>

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -68,23 +68,6 @@ inline bool isUnsupportedFloat8(at::ScalarType t) {
   );
 }
 
-bool complexViewAsRealAllowed(const ReduceOp& reduceOp) {
-  switch (reduceOp) {
-    // NOLINTNEXTLINE(bugprone-branch-clone)
-    case ReduceOp::SUM:
-      return true;
-    case ReduceOp::AVG:
-      return true;
-    case ReduceOp::PREMUL_SUM:
-      return true;
-    case ReduceOp::UNUSED:
-      return true;
-    default:
-      return false;
-  }
-  return false;
-}
-
 #ifdef ENABLE_NCCL_PREMUL_SUM_SUPPORT
 template <typename T, ncclDataType_t dataType>
 ncclRedOpRAII unpackPreMulSum(
@@ -4403,7 +4386,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::allreduce(
   auto tensor = tensors.back();
   if (tensor.is_complex()) {
     TORCH_CHECK(
-        complexViewAsRealAllowed(opts.reduceOp),
+        c10d::isComplexViewAsRealAllowed(opts.reduceOp),
         "all_reduce does not support",
         opts.reduceOp,
         "on complex tensors");
@@ -4597,7 +4580,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::reduce(
   auto tensor = tensors.back();
   if (tensor.is_complex()) {
     TORCH_CHECK(
-        complexViewAsRealAllowed(opts.reduceOp),
+        c10d::isComplexViewAsRealAllowed(opts.reduceOp),
         "reduce does not support",
         opts.reduceOp,
         "on complex tensors");
