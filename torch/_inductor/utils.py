17d870798a3cb1aa01abbc8597c604c9d9cd7f77
Ding, Yi1 <yi1.ding@intel.com>
2025-03-20 10:08:53 +0000

    Check backend at last

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -2634,10 +2634,11 @@ def get_current_backend() -> str:
 
 def upcast_compute_type(dtype: torch.dtype) -> torch.dtype:
     """Maybe upcast [b]float16 to float32"""
-    triton_upcast = (
-        get_current_backend() == "triton" and config.triton.codegen_upcast_to_fp32
-    )
-    if triton_upcast and dtype in (torch.float16, torch.bfloat16):
+    if (
+        dtype in (torch.float16, torch.bfloat16)
+        and config.triton.codegen_upcast_to_fp32
+        and get_current_backend() == "triton"
+    ):
         return torch.float32
     return dtype
 
