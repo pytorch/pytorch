e6c6b1ec07523c294b5ee95cfe149c920207adbf
shubhambhokare1 <shubhambhokare@gmail.com>
2025-03-24 23:52:17 +0000

    prototype for supporting diff op vers torchlib

--- torch/onnx/_internal/exporter/_compat.py
+++ torch/onnx/_internal/exporter/_compat.py
@@ -105,8 +105,10 @@ def export_compat(
     dynamic_shapes_with_export_dim, need_axis_mapping = (
         _dynamic_shapes.convert_str_to_export_dim(dynamic_shapes)
     )
-
-    registry = _registration.ONNXRegistry.from_torchlib()
+    if opset_version is not None:
+        registry = _registration.ONNXRegistry().from_torchlib(opset_version=opset_version)
+    else:
+        registry = _registration.ONNXRegistry().from_torchlib()
     if custom_translation_table is not None:
         for torch_op, onnx_ops in custom_translation_table.items():
             # TODO(justinchuby): Support complex inputs with annotations
