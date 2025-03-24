2657b8f3afb3e7002e0a3b3c989a82ff34520e83
Anant Gulati <agulati@habana.ai>
2025-03-24 11:14:49 +0200

    adding fsdp files

--- test/distributed/_composable/fsdp/test_fully_shard_grad_scaler.py
+++ test/distributed/_composable/fsdp/test_fully_shard_grad_scaler.py
@@ -13,7 +13,10 @@ from torch.distributed.tensor.parallel import (
 )
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_fsdp import FSDPTest, get_devtype, MLP
-from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.common_utils import run_tests, skipIfRocm
+
+
+device_type = torch.device(get_devtype())
 
 
 device_type = torch.device(get_devtype())
