f93d392e1fc43b3d8b4f03bd44bd4e4559d9e855
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-03-25 05:29:11 +0000

    add 2D tensor for xpu

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -3609,7 +3609,7 @@ def meta__convert_weight_to_int4pack_for_cpu(w, inner_k_tiles):
 @register_meta([aten._weight_int4pack_mm])
 def meta__weight_int4pack_mm(x, w, q_group_size, q_scale_and_zeros):
     torch._check(x.dim() == 2, lambda: "x must be a 2D tensor")
-    torch._check(w.dim() == 4, lambda: "w must be a 4D tensor")
+    torch._check(w.dim() == 2 or w.dim() == 4, lambda: "w must be a 4D or 2D tensor")
     torch._check(
         x.dtype in [torch.float32, torch.float16, torch.bfloat16],
         lambda: f"expected x to be f32/f16/bf16, got {x.dtype}",
