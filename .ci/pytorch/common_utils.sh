299c3f1955dd111d3ce758cd3277b5f742f8a8aa
xinan.lin <xinan.lin@intel.com>
2025-03-27 02:01:28 -0700

    [RLEASE ONLY CHANGES] Apply release only chnages to release 2.7 (#149056)
    * [RLEASE ONLY CHANGES] Apply release only chnages to release 2.7
    
    * fix_lint_workflow
    
    * docker_release
    
    * fix_check_binary
    
    [ghstack-poisoned]

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -202,7 +202,7 @@ function install_torchrec_and_fbgemm() {
 
 function clone_pytorch_xla() {
   if [[ ! -d ./xla ]]; then
-    git clone --recursive --quiet https://github.com/pytorch/xla.git
+    git clone --recursive -b r2.7 https://github.com/pytorch/xla.git
     pushd xla
     # pin the xla hash so that we don't get broken by changes to xla
     git checkout "$(cat ../.github/ci_commit_pins/xla.txt)"
