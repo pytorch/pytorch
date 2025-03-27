b3030963fa5c9f83ee0578160676f24ee7399398
Ethan Wee <Ethan.Wee@amd.com>
2025-03-26 20:20:22 -0700

    [release/2.4] Remove custom kwargs before calling BuildExtension.__init__(...) (#2006)
    Port fix from pytorch#149636 
    Update to torchvision branch
    https://github.com/ROCm/vision/tree/release/0.19
    
    Validation:http://rocm-ci.amd.com/view/Release-6.4/job/pytorch2.4-manylinux-wheels_rel-6.4/33/
    
    ---------
    
    Co-authored-by: Jithun Nair <jithun.nair@amd.com>

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -487,10 +487,11 @@ class BuildExtension(build_ext):
         return cls_with_options
 
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
         self.no_python_abi_suffix = kwargs.get("no_python_abi_suffix", False)
-
         self.use_ninja = kwargs.get('use_ninja', True)
+        filtered_kwargs = {kw: val for kw, val in kwargs.items() if kw not in ["no_python_abi_suffix", "use_ninja"]}
+        super().__init__(*args, **filtered_kwargs)
+
         if self.use_ninja:
             # Test if we can use ninja. Fallback otherwise.
             msg = ('Attempted to use ninja as the BuildExtension backend but '
