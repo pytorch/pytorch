d07bc9c63692376d2f8bd15343134abc008e3001
yuchengliu1 <yucheng.liu@intel.com>
2025-03-27 18:56:28 +0800

    update

--- aten/src/ATen/TensorUtils.cpp
+++ aten/src/ATen/TensorUtils.cpp
@@ -168,6 +168,30 @@ void checkAllSameGPU(CheckedFrom c, ArrayRef<TensorArg> tensors) {
   checkAllSame(c, tensors, checkSameGPU);
 }
 
+void checkSameXPU(CheckedFrom c, const TensorArg& t1, const TensorArg& t2) {
+  if (t1->is_cpu() || t2->is_cpu()) {
+    std::ostringstream oss;
+    if (t1->is_cpu()) {
+      oss << "Tensor for " << t1 << " is on CPU, ";
+    }
+    if (t2->is_cpu()) {
+      oss << "Tensor for " << t2 << " is on CPU, ";
+    }
+    oss << "but expected " << ((!t1->is_cpu() && !t2->is_cpu()) ? "them" : "it")
+        << " to be on XPU (while checking arguments for " << c << ")";
+    TORCH_CHECK(false, oss.str());
+  }
+  TORCH_CHECK(
+    t1->get_device() == t2->get_device(),
+    "Expected tensor for ", t1, " to have the same device as tensor for ", t2,
+    "; but device ", t1->get_device(), " does not equal ", t2->get_device(),
+    " (while checking arguments for ", c, ")");
+}
+
+void checkAllSameXPU(CheckedFrom c, ArrayRef<TensorArg> tensors) {
+  checkAllSame(c, tensors, checkSameXPU);
+}
+
 void checkSameType(CheckedFrom c, const TensorArg& t1, const TensorArg& t2) {
   TORCH_CHECK(
     t1->options().type_equal(t2->options()),
