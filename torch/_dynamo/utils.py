67cd9cc270cd0f41089cd743c07a96d879b890ce
Yuanhao Ji <jiyuanhao@apache.org>
2025-03-28 07:16:29 +0000

    [Dynamo] Fix `is_compile_supported()` when `device_type` contains device index (#147837)
    Fixes #147826
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147837
    Approved by: https://github.com/anijain2305

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -3751,10 +3751,11 @@ def build_checkpoint_variable(**options):
 def is_compile_supported(device_type):
     from .eval_frame import is_dynamo_supported
 
+    type = torch.device(device_type).type
     compile_supported = is_dynamo_supported()
-    if device_type == "cpu":
+    if type == "cpu":
         pass
-    elif device_type in ["cuda", "xpu"] and compile_supported:
+    elif type in ["cuda", "xpu"] and compile_supported:
         compile_supported = has_triton()
     else:
         compile_supported = False
