43d7d039c0555d1d21611dd124e70be5a454c6c4
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-03-31 14:57:43 -0700

    Memory leak base tests for compile
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -72,6 +72,7 @@ from torch.testing._internal.common_utils import (
     outs_and_grads,
     parametrize,
     run_tests,
+    serialTest,
     skipIfRocm,
     TestCase,
     xfail_inherited_tests,
@@ -6560,6 +6561,28 @@ metadata incorrectly.
             y.backward(tg)
 
             self.assertEqual(ref_x.grad, x.grad)
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA is unavailable")
+    @parametrize("backend", ["eager", "aot_eager", "inductor"])
+    @serialTest()
+    def test_memory_leaks_base(self, backend):
+        bsz = 10
+        emb_dim = 5
+        num_classes = 2
+        x = torch.randn(bsz, emb_dim, device="cuda", requires_grad=True)
+        w = torch.randn(num_classes, emb_dim, device="cuda")
+        l = torch.randint(num_classes, (bsz,), device="cuda", dtype=torch.int64)
+
+        def fn(inp, w, l):
+            x = torch.nn.functional.linear(inp, w)
+            loss = torch.nn.functional.cross_entropy(x, l)
+            return loss
+
+        def inp_fn():
+            return x, w, l
+
+        _test_fn = torch.compile(fn, backend=backend)
+        torch.testing._utils.test_cuda_memory_leak(_test_fn, inp_fn, self.assertTrue)
 
     @patch("torch._functorch.config.guess_tangent_strides_as_outputs", True)
     def test_flex_attn_noncontiguous_tangents(self):
