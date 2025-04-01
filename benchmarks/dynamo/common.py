b709c653a2d668c48b4488587447b0cc2116a381
Zhang, Jianyi <jianyi.zhang@intel.com>
2025-04-01 01:00:11 +0000

    [Intel gpu] always set deterministic for xpu accuracy test (#149028)
    On Intel Max 1550, models like Super_SloMo can actually pass accuracy test after set deterministic, because we do not use atomic in upsampling bilinear backward in some cases when running on XPU. Furthermore, I guess the only reason not to set deterministic on these models is just avoiding errors. We should use warn_only = True.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/149028
    Approved by: https://github.com/guangyey, https://github.com/desertfire
    
    Co-authored-by: Yu, Guangye <106960996+guangyey@users.noreply.github.com>

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -3542,6 +3542,8 @@ def run(runner, args, original_dir=None):
         }:
             # some of the models do not support use_deterministic_algorithms
             torch.use_deterministic_algorithms(True)
+        if args.devices == ["xpu"]:
+            torch.use_deterministic_algorithms(True, warn_only=True)
         os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
         # TODO(eqy): revisit when cuBLASLt workspace size is bumped
         # if args.only is not None and args.only in {
