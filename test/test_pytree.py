843dd8c556b9bb7aa4a469c68bb18978608e56fe
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-04-01 23:32:40 +0800

    [BE][pytree] rename argument name in register function to match the type annotations: `*_fn -> *_func`
    ghstack-source-id: b0a6d373eb47d5ee293a05932d9537e91c2026f1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148484

--- test/test_pytree.py
+++ test/test_pytree.py
@@ -1361,9 +1361,9 @@ if "optree" in sys.modules:
 
         py_pytree.register_pytree_node(
             ACustomPytree,
-            flatten_fn=lambda f: ([f.x, f.y], f.z),
-            unflatten_fn=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
-            flatten_with_keys_fn=lambda f: ((("x", f.x), ("y", f.y)), f.z),
+            flatten_func=lambda f: ([f.x, f.y], f.z),
+            unflatten_func=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
+            flatten_with_keys_func=lambda f: ((("x", f.x), ("y", f.y)), f.z),
         )
         from_two_trees = py_pytree.tree_map_with_path(
             lambda kp, a, b: a + b, tree1, tree2
@@ -1393,9 +1393,9 @@ if "optree" in sys.modules:
 
         py_pytree.register_pytree_node(
             ACustomPytree,
-            flatten_fn=lambda f: ([f.x, f.y], f.z),
-            unflatten_fn=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
-            flatten_with_keys_fn=lambda f: ((("x", f.x), ("y", f.y)), f.z),
+            flatten_func=lambda f: ([f.x, f.y], f.z),
+            unflatten_func=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
+            flatten_with_keys_func=lambda f: ((("x", f.x), ("y", f.y)), f.z),
         )
 
         SOME_PYTREES = [
@@ -1423,9 +1423,9 @@ if "optree" in sys.modules:
 
         py_pytree.register_pytree_node(
             ACustomPytree,
-            flatten_fn=lambda f: ([f.x, f.y], f.z),
-            unflatten_fn=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
-            flatten_with_keys_fn=lambda f: ((("x", f.x), ("y", f.y)), f.z),
+            flatten_func=lambda f: ([f.x, f.y], f.z),
+            unflatten_func=lambda xy, z: ACustomPytree(xy[0], xy[1], z),
+            flatten_with_keys_func=lambda f: ((("x", f.x), ("y", f.y)), f.z),
         )
 
         SOME_PYTREES = [
