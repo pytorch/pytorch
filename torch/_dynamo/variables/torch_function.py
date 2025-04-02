ce86f934ccfe0b6e70d1ffd60c9e8770619a2783
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-04-02 20:30:33 +0000

    Revert "[dynamo] Support tensor subclass with overriden tensor methods and properties (#149484)"
    This reverts commit 7e53c58687482d58461e1dd8e09f59a9daf8f7b3.
    
    Reverted https://github.com/pytorch/pytorch/pull/149484 on behalf of https://github.com/malfet due to Broke trunk, see https://hud.pytorch.org/hud/pytorch/pytorch/b03c42109c4e7dd52228f0a2bd65963a1d86523c/1?per_page=50&name_filter=clang10%20%2F%20test&mergeEphemeralLF=true ([comment](https://github.com/pytorch/pytorch/pull/149482#issuecomment-2773650522))

--- torch/_dynamo/variables/torch_function.py
+++ torch/_dynamo/variables/torch_function.py
@@ -597,9 +597,8 @@ class TensorWithTFOverrideVariable(TensorVariable):
 
         # This simulates shallow-copying the tensor object.
         kwargs = dict(tensor_var.__dict__)
-        input_tensor_type = kwargs.pop("class_type")
-        assert input_tensor_type in (torch.Tensor, torch.nn.Parameter), (
-            f"invalid class type {input_tensor_type} in TensorWithTFOverrideVariable.from_tensor_var"
+        assert kwargs.pop("class_type") is torch.Tensor, (
+            "invalid class type in TensorWithTFOverrideVariable.from_tensor_var"
         )
         torch_fn_var = build_torch_function_fn(tx, class_type, cls_source)
         var = cls(torch_function_fn=torch_fn_var, class_type=class_type, **kwargs)
@@ -639,9 +638,13 @@ class TensorWithTFOverrideVariable(TensorVariable):
                 f"Accessing {name} on a tensor subclass with a __torch_function__ override is not supported"
             )
 
-        # Handle non-overriden attributes inherited from `torch.Tensor`.
-        attr_is_overriden = _is_attr_overidden(tx, self, name)
-        if hasattr(torch.Tensor, name) and not attr_is_overriden:
+        if hasattr(torch.Tensor, name):
+            if _is_attr_overidden(tx, self, name):
+                unimplemented(
+                    f"Accessing overridden method/attribute {name} on a tensor"
+                    " subclass with a __torch_function__ override is not supported"
+                )
+
             if tx.output.torch_function_enabled:
                 if self.source:
                     install_guard(
@@ -671,23 +674,11 @@ class TensorWithTFOverrideVariable(TensorVariable):
             else:
                 import types
 
-                cls_source = GlobalSource(self.global_mangled_class_name(tx))
-                attr_source = AttrSource(cls_source, name)
                 if isinstance(attr, types.FunctionType):
-                    install_guard(attr_source.make_guard(GuardBuilder.FUNCTION_MATCH))
+                    cls_source = GlobalSource(self.global_mangled_class_name(tx))
+                    func_source = AttrSource(cls_source, name)
+                    install_guard(func_source.make_guard(GuardBuilder.FUNCTION_MATCH))
                     return UserMethodVariable(attr, self)
-
-                elif isinstance(attr, property):
-                    getter_source = AttrSource(attr_source, "fget")
-                    getter = attr.fget
-                    getter_var = UserMethodVariable(getter, self, source=getter_source)
-                    return getter_var.call_function(tx, [], {})
-
-                elif attr_is_overriden:
-                    unimplemented(
-                        f"Currently only support accessing overridden attributes that are functions or properties, but got {type(attr)}"  # noqa: B950
-                    )
-
         return super().var_getattr(tx, name)
 
     def call_torch_function(self, tx: "InstructionTranslator", fn, types, args, kwargs):
