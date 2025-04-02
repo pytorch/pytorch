1dd4c2b77e8f862838cbaef598a6884ac58a348c
Pian Pawakapan <pianpwk@meta.com>
2025-04-02 14:51:26 -0700

    Update __init__.py

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -3831,7 +3831,7 @@ def _reshape_view_helper(a: TensorLikeType, *shape, allow_copy: bool) -> TensorL
             a_ = flatten(a_, idx, end)
 
         # Splits the (possibly flattened) dimension to create the desired dim length
-        if not guard_or_false(accum == length):
+        if guard_or_true(accum == length):
             a_ = prims.split_dim(a_, idx, length)
 
         idx = idx + 1
