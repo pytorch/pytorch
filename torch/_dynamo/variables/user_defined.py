08ea35a3261eeaa7fc591d19700efac011069f97
Lucas Kabela <lucaskabela@meta.com>
2025-04-02 12:04:59 -0700

    Make primary change

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -240,10 +240,13 @@ class UserDefinedClassVariable(UserDefinedVariable):
 
         if name in cmp_name_to_op_mapping and not isinstance(obj, types.FunctionType):
             return variables.GetAttrVariable(self, name, source=source)
-
+        import logging
+        logging.warning(f"searching {obj} with name {name} source {source}")
         if isinstance(obj, staticmethod):
+            logging.warning(f"In here now for static method")
             return VariableTracker.build(tx, obj.__get__(self.value), source)
         elif isinstance(obj, classmethod):
+            logging.warning(f"class method")
             if isinstance(obj.__func__, property):
                 return variables.UserFunctionVariable(obj.__func__.fget).call_function(
                     tx, [self], {}
@@ -252,6 +255,7 @@ class UserDefinedClassVariable(UserDefinedVariable):
         elif isinstance(obj, types.ClassMethodDescriptorType):
             # e.g.: inspect.getattr_static(dict, "fromkeys")
             #       inspect.getattr_static(itertools.chain, "from_iterable")
+            logging.warning(f"Descriptor actually")
             func = obj.__get__(None, self.value)
             return VariableTracker.build(tx, func, source)
         elif source:
@@ -259,6 +263,7 @@ class UserDefinedClassVariable(UserDefinedVariable):
             if inspect.ismemberdescriptor(obj) or (
                 sys.version_info >= (3, 12) and name == "__mro__"
             ):
+                logging.warning(f"Buiild source here")
                 return VariableTracker.build(tx, obj.__get__(self.value), source)
 
         if ConstantVariable.is_literal(obj):
@@ -269,7 +274,19 @@ class UserDefinedClassVariable(UserDefinedVariable):
             self.value.__module__.startswith("torch.")
             or self.value.__module__ == "torch"
         ):
-            if source:
+            if source is not None and hasattr(torch.ops.aten, name):
+                fn = getattr(torch.ops.aten, name)
+                if (
+                    hasattr(fn, "overloads")
+                    and hasattr(fn, fn.overloads()[0])
+                    and torch.Tag.inplace_view in getattr(fn, fn.overloads()[0]).tags
+                ):
+                    # Delay the graph break to the actual call of unsqueeze_/resize_/resize_as_ etc.
+                    return variables.misc.DelayGraphBreakVariable(
+                        source=AttrSource(self.source, name)
+                    )
+            elif source:
+                # NOTE: Takes this path
                 return VariableTracker.build(tx, obj, source)
 
         if (
