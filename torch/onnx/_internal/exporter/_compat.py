7fec74ad995dab9ae9f4bad9259ec03779e7ad01
shubhambhokare1 <shubhambhokare@gmail.com>
2025-04-04 23:29:21 +0000

    minor fix

--- torch/onnx/_internal/exporter/_compat.py
+++ torch/onnx/_internal/exporter/_compat.py
@@ -105,7 +105,7 @@ def export_compat(
     dynamic_shapes_with_export_dim, need_axis_mapping = (
         _dynamic_shapes.convert_str_to_export_dim(dynamic_shapes)
     )
-    registry = _registration.ONNXRegistry().from_torchlib()
+    registry = _registration.ONNXRegistry().from_torchlib(opset_version=opset_version)
     if custom_translation_table is not None:
         for torch_op, onnx_ops in custom_translation_table.items():
             # TODO(justinchuby): Support complex inputs with annotations
