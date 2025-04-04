5c7473d266933e2ae60f6cd380f599dbca131c87
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2025-04-04 09:15:17 -0700

    Update aten/src/ATen/native/LinearAlgebra.h

--- aten/src/ATen/native/LinearAlgebra.h
+++ aten/src/ATen/native/LinearAlgebra.h
@@ -14,8 +14,4 @@ namespace at::native {
 
 using addr_fn = void (*)(TensorIterator &, const Scalar& beta, const Scalar& alpha);
 DECLARE_DISPATCH(addr_fn, addr_stub)
-
-namespace mps {
-    void linalg_solve_out_mps_impl(const Tensor& A, const Tensor& B, bool left, bool check_errors, const Tensor& result, const Tensor& LU, const Tensor& pivots, const Tensor& info);
-}
 } // namespace at::native
