e3a6bad2ec0b00f54e85cb8d9005e43b63bce071
Aleksei Nikiforov <aleksei.nikiforov@linux.ibm.com>
2025-04-04 14:52:34 +0200

    s390x: enable test_cpp_extensions_open_device_registration.py test

--- test/run_test.py
+++ test/run_test.py
@@ -185,7 +185,6 @@ S390X_BLOCKLIST = [
     "inductor/test_aot_inductor_arrayref",
     "lazy/test_meta_kernel",
     "profiler/test_profiler",
-    "test_cpp_extensions_open_device_registration",
     "test_jit",
     # these tests run long and fail in addition to that
     "dynamo/test_dynamic_shapes",
