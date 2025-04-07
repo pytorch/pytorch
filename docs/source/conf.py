314a62672393ea13b07d9dc6b1e1d0de02e2b9bf
Svetlana Karslioglu <svekars@meta.com>
2025-04-07 16:11:04 -0700

    Add last-updated to pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -176,6 +176,7 @@ html_theme_options = {
 
 theme_variables = pytorch_sphinx_theme2.get_theme_variables()
 
+
 html_context = {
     "theme_variables": theme_variables,
     "display_github": True,
@@ -190,6 +191,11 @@ html_context = {
     "library_links": theme_variables.get("library_links", []),
     "community_links": theme_variables.get("community_links", []),
     "language_bindings_links": theme_variables.get("language_bindings_links", []),
+    "date_info": {
+        "enabled": True,
+        "paths_to_skip": ["generated/"],
+        "source_to_build_mapping": {"": ""},
+    },
 }
 
 napoleon_use_ivar = True
@@ -3718,6 +3724,9 @@ def setup(app):
     app.connect("build-finished", coverage_post_process)
     app.connect("autodoc-process-docstring", process_docstring)
     app.connect("html-page-context", hide_edit_button_for_pages)
+    # add last-updated date to doc
+    from pytorch_sphinx_theme2 import on_build_finished
+    app.connect("build-finished", on_build_finished)
     return {"version": "0.1", "parallel_read_safe": True}
 
 def hide_edit_button_for_pages(app, pagename, templatename, context, doctree):
