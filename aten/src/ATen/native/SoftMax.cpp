a7435e506089141b1327c2e1237ab1f782eb7a11
Weishi.Deng <weishi.deng@intel.com>
2025-04-08 14:11:04 +0800

    Update aten/src/ATen/native/SoftMax.cpp
    Co-authored-by: Yu, Guangye <106960996+guangyey@users.noreply.github.com>

--- aten/src/ATen/native/SoftMax.cpp
+++ aten/src/ATen/native/SoftMax.cpp
@@ -428,7 +428,7 @@ Tensor& softmax_out(
     std::optional<ScalarType> dtype,
     Tensor& output_) {
   Tensor output_temp;
-  if ((input_.is_cuda()||input_.is_xpu()) && input_.scalar_type() == ScalarType::Half &&
+  if ((input_.is_cuda() || input_.is_xpu()) && input_.scalar_type() == ScalarType::Half &&
       dtype == ScalarType::Float) {
     if (!output_.is_contiguous()) {
       auto options =
