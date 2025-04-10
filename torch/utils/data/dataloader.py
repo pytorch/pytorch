c0c9d7cbc48e5b2e65189c5f2502fc3af33a8223
zeshengzong <zesheng.zong@outlook.com>
2025-04-10 09:35:24 +0800

    Update

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -1175,11 +1175,13 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
 
             # Queue is not type-annotated
             self._data_queue = queue.Queue()  # type: ignore[var-annotated]
+            current_device = torch.accelerator.current_device_index()
             pin_memory_thread = threading.Thread(
                 target=_utils.pin_memory._pin_memory_loop,
                 args=(
                     self._worker_result_queue,
                     self._data_queue,
+                    current_device,
                     self._pin_memory_thread_done_event,
                     self._pin_memory_device,
                 ),
