3beb3e4ee45111fc97a58b3a723d97b5f6d32031
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-04-10 21:46:23 +0000

    Revert "[inductor] Change minimum number of SMs to 60 to let Ada use Triton GEMM backend (#150888)"
    This reverts commit 115a165f9b24e3aaaeb2d0994678116758bd636f.
    
    Reverted https://github.com/pytorch/pytorch/pull/150888 on behalf of https://github.com/malfet due to This indeed broke all those inductor tests ([comment](https://github.com/pytorch/pytorch/pull/150888#issuecomment-2795231901))

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -1275,7 +1275,7 @@ def is_big_gpu(index_or_device: Union[int, torch.device] = 0) -> bool:
             return False
         return True
 
-    min_sms = 16 if device.type == "xpu" else 60  # 3080
+    min_sms = 16 if device.type == "xpu" else 68  # 3080
     avail_sms = prop.multi_processor_count
     if avail_sms < min_sms:
         log.warning(
