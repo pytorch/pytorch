7b71ebd9fcc8ec8a340de7135779cd2a44e81978
Hexin Wang <hexinw@nvidia.com>
2025-04-10 17:33:26 +0000

    Fixing NCCL abort hang issue when a ProcessGroupNCCL manages multiple ncclComms (#150690)
    Detail of the issue:
    
    If PyTorch issues send/recv to each 2 rank comm, and these comms are managed by a single ProcessGroupNCCL instance, then comms need to abort either in sequence or in group.
    
    I.e. the following sequential abort will cause hang in NCCL. recv(..., comm0, stream);
    send(..., comm1, stream);
    abort(comm1);
    abort(comm0);
    
    Fixes #119797
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/150690
    Approved by: https://github.com/kwen2501, https://github.com/eqy, https://github.com/atalman

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1371,6 +1371,11 @@ void ProcessGroupNCCL::abortCommsFromMap(
     const std::optional<std::string>& abortReason) {
   // The process may control multiple devices, loop through the communicators on
   // each device
+  // NCCL expects Group abort when there are multiple communicators created in a
+  // device. Group abort requires 2.22.0 release and up.
+  if (getNcclVersionNumber() >= NCCL_VERSION(2, 22, 0)) {
+    groupStart();
+  }
   for (auto& it : ncclCommsMap) {
     auto& devName = it.first;
     auto& ncclComm = it.second;
@@ -1390,6 +1395,9 @@ void ProcessGroupNCCL::abortCommsFromMap(
 
     VLOG(2) << logPrefix() << "ProcessGroupNCCL destroyed "
             << " communicator on CUDA device: " << devName;
+  }
+  if (getNcclVersionNumber() >= NCCL_VERSION(2, 22, 0)) {
+    groupEnd();
   }
 }
 
