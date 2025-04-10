b2aaf9ed7481915eac98ca7a06650f88d008c078
iremyux <iremyuksel@microsoft.com>
2025-04-10 10:21:57 +0200

    Revert "Remove cache related stuff since we do not use it anyways, it fails the build somehow"
    This reverts commit 987b35eb7d4e798f9d482f0e8295739cf9cf948c.

--- .ci/pytorch/windows/arm64/build_libtorch.bat
+++ .ci/pytorch/windows/arm64/build_libtorch.bat
@@ -2,6 +2,8 @@
 
 :: environment variables
 set CMAKE_BUILD_TYPE=%BUILD_TYPE%
+set CMAKE_C_COMPILER_LAUNCHER=sccache
+set CMAKE_CXX_COMPILER_LAUNCHER=sccache
 set libuv_ROOT=%DEPENDENCIES_DIR%\libuv\install
 set MSSdk=1
 if defined PYTORCH_BUILD_VERSION (
@@ -39,6 +41,11 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 :: DISTUTILS_USE_SDK should be set after psutil dependency
 set DISTUTILS_USE_SDK=1
+
+:: start sccache server and reset sccache stats
+sccache --start-server
+sccache --zero-stats
+sccache --show-stats
  
 :: Prepare the environment
 mkdir libtorch
