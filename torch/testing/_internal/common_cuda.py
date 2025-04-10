c09ed9677337ff4dead47fe5b69f75c7173c2290
Nichols A. Romero <nick.romero@amd.com>
2025-04-10 20:13:10 +0000

    Also return zero tuple when ROCm is not supported.

--- torch/testing/_internal/common_cuda.py
+++ torch/testing/_internal/common_cuda.py
@@ -260,7 +260,7 @@ def _get_torch_cuda_version():
     return tuple(int(x) for x in cuda_version.split("."))
 
 def _get_torch_rocm_version():
-    if not TEST_WITH_ROCM:
+    if not TEST_WITH_ROCM or torch.version.hip is None:
         return (0, 0)
     rocm_version = str(torch.version.hip)
     rocm_version = rocm_version.split("-")[0]    # ignore git sha
