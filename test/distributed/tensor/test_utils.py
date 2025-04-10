f0b0961b893a3dc08ab78b8d433739287188f9e3
Will Constable <whc@meta.com>
2025-04-09 17:29:29 -0700

    [DTensor] Fix empty shard global-offset calculation
    `compute_local_shape_and_global_offset` util computes the local shape of
    a particular shard of a DTensor, and the global offset (which describes
    how the shard fits into the global tensor).
    
    When the tensor dim does not evenly divide into the mesh dim, uneven
    sharding occurs.  In some cases, uneven sharding results in an empty
    shard.
    
    e.g.
       tensor dim size: 4096
       mesh dim size: 30
       ranks 0..27 have local size 18
       rank 28 has local size 8
       rank 29 has local size 0 <--- empty shard
    
    The global offset for an empty shard was previously undefined and
    returned values that were computed based on logic that assumes no empty
    shards.  This caused DCP to fail to save a checkpoint, becuase
    deduplication logic could 'throw away' real (non-empty) shards thinking
    they were duplicates of zero-sized shards with the same offset.
    
    Now, we define the global offset of an empty shard to be the dim-size,
    which is out of bounds of the tensor and can't overlap with any
    non-empty shards.
    
    ghstack-source-id: 32a7439ef337ef264fb6864fce130031b55aa8af
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/150862

--- test/distributed/tensor/test_utils.py
+++ test/distributed/tensor/test_utils.py
@@ -7,6 +7,7 @@ from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.tensor import distribute_tensor, DTensor
 from torch.distributed.tensor._dtensor_spec import DTensorSpec, TensorMeta
 from torch.distributed.tensor._utils import (
+    _compute_local_shape_and_global_offset,
     _explicit_order_placements,
     compute_local_shape_and_global_offset,
 )
@@ -101,6 +102,37 @@ class LocalTest(TestCase):
                 _explicit_order_placements(
                     test_case["mesh_shape"], test_case["placements"]
                 )
+
+    def test_compute_local_shape_and_global_offset_uneven(self):
+        # This case is not only 'uneven' bug also has an empty shard
+        # (e.g. most DP ranks have local shape 18,4096, one has 8,4096, one has 0,4096
+        global_shape = (4096, 4096)
+        DP = 30
+        TP = 8
+        mesh_shape = (DP, TP)
+        placements = [_StridedShard(0, split_factor=8), Shard(0)]
+        TP_shard_size = global_shape[0] / TP
+        for my_coordinate in itertools.product(range(DP), range(TP)):
+            local_shape, global_offset = _compute_local_shape_and_global_offset(
+                global_shape, mesh_shape, list(my_coordinate), placements
+            )
+            print(f"{my_coordinate=}: {local_shape=}, {global_offset=}")
+            dp_rank, tp_rank = my_coordinate
+            if dp_rank < 28:
+                self.assertEqual(local_shape, (18, 4096))
+                self.assertEqual(
+                    global_offset, (tp_rank * TP_shard_size + 18 * dp_rank, 0)
+                )
+            elif dp_rank == 28:
+                self.assertEqual(local_shape, (8, 4096))
+                self.assertEqual(
+                    global_offset, (tp_rank * TP_shard_size + 18 * dp_rank, 0)
+                )
+            elif dp_rank == 29:
+                self.assertEqual(local_shape, (0, 4096))
+                # we define the offset value of a zero-sized shard as the dim size
+                # this actually matters, because DCP uses offset to deduplicate shards when saving
+                self.assertEqual(global_offset, (4096, 0))
 
 
 class UtilTest(DTensorTestBase):
