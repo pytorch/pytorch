f36a479f0ae4b77edf693aef97ee75bbf2671712
Thomas Adams <tommyadams5@gmail.com>
2025-04-10 10:07:49 -0700

    Replaced Any return type

--- torch/_prims_common/wrappers.py
+++ torch/_prims_common/wrappers.py
@@ -451,7 +451,7 @@ def backwards_not_supported(prim):
 # TODO: when tracing this will add torch tensors and not TensorMeta objects
 # to the trace -- we should fix this by adding a tracing context and NumberMeta classes
 # TODO: this wrapper is currently untested
-def elementwise_unary_scalar_wrapper(fn: Callable[_P, _T]) -> Callable[_P, Any]:
+def elementwise_unary_scalar_wrapper(fn: Callable[_P, _T]) -> Callable[_P, _T | NumberType]:
     """
     Allows unary operators that accept tensors to work with Python numbers.
     """
