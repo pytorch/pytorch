23ca7c2d762a727c7872e26b8640e12dbe36ddbc
FFFrog <ljw1101.vip@gmail.com>
2025-04-11 13:04:14 +0800

    [Openreg][PrivateUse1] Refactor csrc files of Pytorch_openreg
    I want to format and refactor the csrc file of pytorch_openreg. To make the code review clearer and easier to understand, I divide the code refactoring into two parts:
    
    - Part 1: Code formatting
    - Part 2: Code refactoring and optimization (Next PR)
    
    ghstack-source-id: 4006a34cb23cb85faa265636bcf8b6dfd94db197
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/151004

--- test/cpp_extensions/open_registration_extension/pytorch_openreg/csrc/OpenReg.h
+++ test/cpp_extensions/open_registration_extension/pytorch_openreg/csrc/OpenReg.h
@@ -1,11 +1,10 @@
 #pragma once
-// Shared header for OpenReg module
 
 #include <torch/csrc/utils/pybind.h>
 
 namespace openreg {
 
-    void set_impl_factory(PyObject* factory);
-    py::function get_method(const char* name);
+void set_impl_factory(PyObject* factory);
+py::function get_method(const char* name);
 
-}
+} // namespace openreg
