ef710b531ddd68417fe260e49135ff1b5c0ca6b6
Boyuan Feng <boyuan@meta.com>
2025-04-12 16:45:30 -0700

    improve noop slice

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -813,9 +813,11 @@ def register_noop_decomp(targets, nop_arg=0):
 def slice_noop(self, dim=0, start=None, end=None, step=1):
     if start is None or end is None:
         return False
+
+    slice_dim_size = self.shape[dim]
     if (
         statically_known_true(sym_eq(start, 0))
-        and statically_known_true(end >= 2**63 - 1)
+        and statically_known_true(end >= slice_dim_size)
         and statically_known_true(sym_eq(step, 1))
     ):
         return True
