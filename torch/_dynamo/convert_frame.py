a86eb52fc2a0dcccc7dc0deddc44ed170b089a7e
Animesh Jain <anijain@umich.edu>
2025-04-15 14:49:32 -0700

    [compile][compile time traces] Add more timers for missing gaps
    [ghstack-poisoned]

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -812,7 +812,10 @@ def _compile(
         for attempt in itertools.count():
             CompileContext.get().attempt = attempt
             try:
-                out_code = transform_code_object(code, transform)
+                with dynamo_timed(
+                    f"bytecode_transform_attempt{attempt}", log_pt2_compile_event=True
+                ):
+                    out_code = transform_code_object(code, transform)
                 break
             except exc.RestartAnalysis as e:
                 if not isinstance(e, exc.TensorifyScalarRestartAnalysis):
