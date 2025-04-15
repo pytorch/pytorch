b06f8b5dbdc66878bf2492f08f42d7b1ad42a4f3
Aditya Tewari <aditya.tewari@arm.com>
2025-04-15 13:53:45 +0000

    update openblas

--- .ci/docker/common/install_openblas.sh
+++ .ci/docker/common/install_openblas.sh
@@ -4,7 +4,11 @@
 set -ex
 
 cd /
-git clone https://github.com/OpenMathLib/OpenBLAS.git -b "${OPENBLAS_VERSION:-v0.3.29}" --depth 1 --shallow-submodules
+OPENBLAS_HASH="b30dc9701f8e971720a02e24068acea274fd9cee" #Use SVE kernel for S/DGEMVT for SVE machines
+OPENBLAS_CHECKOUT_DIR="OpenBLAS"
+git clone https://github.com/OpenMathLib/OpenBLAS.git -b develop --shallow-submodules
+git -C $OPENBLAS_CHECKOUT_DIR fetch --depth 1 origin $OPENBLAS_HASH
+git -C $OPENBLAS_CHECKOUT_DIR checkout $OPENBLAS_HASH
 
 OPENBLAS_BUILD_FLAGS="
 NUM_THREADS=128
@@ -13,9 +17,8 @@ NO_SHARED=0
 DYNAMIC_ARCH=1
 TARGET=ARMV8
 CFLAGS=-O3
+BUILD_BFLOAT16=1
 "
-
-OPENBLAS_CHECKOUT_DIR="OpenBLAS"
 
 make -j8 ${OPENBLAS_BUILD_FLAGS} -C ${OPENBLAS_CHECKOUT_DIR}
 make -j8 ${OPENBLAS_BUILD_FLAGS} install -C ${OPENBLAS_CHECKOUT_DIR}
