97b35d45c533e8211a7b891afed38dba23246fff
Svetlana Karslioglu <svekars@meta.com>
2025-04-15 09:20:39 -0700

    Update

--- docs/cpp/source/conf.py
+++ docs/cpp/source/conf.py
@@ -25,6 +25,7 @@ import textwrap
 # sys.path.insert(0, os.path.abspath('.'))
 import pytorch_sphinx_theme2
 
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
