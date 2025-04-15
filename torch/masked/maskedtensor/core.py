9c207c25e615ba88887e524efdfcd7b9690fdc89
zeshengzong <zesheng.zong@outlook.com>
2025-04-15 10:32:09 +0800

    Update

--- torch/masked/maskedtensor/core.py
+++ torch/masked/maskedtensor/core.py
@@ -2,7 +2,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 
 import warnings
-from typing import Any
+from typing import Any, get_args
 from typing_extensions import TypeIs
 
 import torch
@@ -364,7 +364,7 @@ class MaskedTensor(torch.Tensor):
         device_to = current_device
         if len(args) == 1:
             arg = args[0]
-            if isinstance(arg, DeviceLikeType):
+            if isinstance(arg, get_args(DeviceLikeType)):
                 device_to = canonicalize_device(arg)
             elif isinstance(arg, torch.Tensor):
                 device_to = arg.device
