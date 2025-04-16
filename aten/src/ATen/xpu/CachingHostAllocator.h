29c0c96647fcda4721e5a1578d9a182e81ed48f6
Yu, Guangye <guangye.yu@intel.com>
2025-04-16 18:03:41 +0000

    Update
    [ghstack-poisoned]

--- aten/src/ATen/xpu/CachingHostAllocator.h
+++ aten/src/ATen/xpu/CachingHostAllocator.h
@@ -7,10 +7,14 @@
 
 namespace at::xpu {
 
+C10_DEPRECATED_MESSAGE(
+    "at::xpu::getCachingHostAllocator() is deprecated. Please use at::getHostAllocator(at::kXPU) instead.")
 inline TORCH_XPU_API at::HostAllocator* getCachingHostAllocator() {
   return at::getHostAllocator(at::kXPU);
 }
 
+C10_DEPRECATED_MESSAGE(
+  "at::xpu::CachingHostAllocator_recordEvent(...) is deprecated. Please use at::getHostAllocator(at::kXPU)->record_event(...) instead.")
 inline TORCH_XPU_API bool CachingHostAllocator_recordEvent(
     void* ptr,
     void* ctx,
@@ -18,10 +22,14 @@ inline TORCH_XPU_API bool CachingHostAllocator_recordEvent(
   return getCachingHostAllocator()->record_event(ptr, ctx, stream.unwrap());
 }
 
+C10_DEPRECATED_MESSAGE(
+  "at::xpu::CachingHostAllocator_emptyCache() is deprecated. Please use at::getHostAllocator(at::kXPU)->empty_cache() instead.")
 inline TORCH_XPU_API void CachingHostAllocator_emptyCache() {
   getCachingHostAllocator()->empty_cache();
 }
 
+C10_DEPRECATED_MESSAGE(
+  "at::xpu::HostAlloc(...) is deprecated. Please use at::getHostAllocator(at::kXPU)->allocate(...) instead.")
 inline TORCH_XPU_API at::DataPtr HostAlloc(size_t size) {
   return getCachingHostAllocator()->allocate(size);
 }
