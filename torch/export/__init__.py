f861dc1826f7b49de37a5578d6e9ef6300498606
Chen Zhu <chzhu@linkedin.com>
2025-04-16 09:36:58 -0700

    fix lint

--- torch/export/__init__.py
+++ torch/export/__init__.py
@@ -133,7 +133,7 @@ def export_for_training(
          WARNING: This option is experimental and use this at your own risk.
 
         preserve_module_call_signature: A list of submodule paths for which the original
-         calling conventions are preserved as metadata. The metadata will be used when calling 
+         calling conventions are preserved as metadata. The metadata will be used when calling
          torch.export.unflatten to preserve the original calling conventions of modules.
 
     Returns:
@@ -252,7 +252,7 @@ def export(
          is passed here.
 
         preserve_module_call_signature: A list of submodule paths for which the original
-         calling conventions are preserved as metadata. The metadata will be used when calling 
+         calling conventions are preserved as metadata. The metadata will be used when calling
          torch.export.unflatten to preserve the original calling conventions of modules.
 
     Returns:
