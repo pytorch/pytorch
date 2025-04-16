55595e0c85b1f5d33a7a163afa774ab12f8d3eaf
Laith Sakka <lsakka@meta.com>
2025-04-15 18:04:33 -0700

    Fix Issues in deferring runtime assertions. (#151170)
    This PR fix two bugs:
    1)  Update self.bound_unbacked_symbols before emitting runtime asserts :
    set self.bound_unbacked_symbols before emitting runtime asserts to include runtime asserts depending on the current node
    
    2) In the pass that remove unused graph inputs, we should not remove symbols that are used by runtime assertions.
    
    Address some of the issues in https://github.com/pytorch/pytorch/issues/151127
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/151170
    Approved by: https://github.com/bobrenjc93, https://github.com/eellison

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -2992,6 +2992,18 @@ class TestGuardsExpressions(TestCase):
         self.assertEqual(f"{x_clean.stride()}", "(8, 1)")
         self.assertEqual(f"{x_clean.shape}", "torch.Size([5, 8])")
 
+    @torch._dynamo.config.patch("capture_scalar_outputs", True)
+    def test_deferred_neq_assert(self):
+        @torch.compile(fullgraph=True)
+        def func(a):
+            torch._check(a.item() != 5)
+            return a.item() * 10
+
+        func(torch.tensor([100]))
+
+        with self.assertRaises(RuntimeError):
+            func(torch.tensor([5]))
+
 
 if __name__ == "__main__":
     run_tests()
