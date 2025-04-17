ad3def1526025d3b4fa79c5cd34b0aa8b6b91bf6
Siddharth Kotapati <skotapati@apple.com>
2025-04-17 11:12:47 -0700

    Revert "Add mul kernel"
    This reverts commit 280f396ace2cc77b5090f9ee0e7f99f99de6c359.

--- aten/src/ATen/native/mps/operations/BinaryOps.mm
+++ aten/src/ATen/native/mps/operations/BinaryOps.mm
@@ -387,10 +387,16 @@ CREATE_MPS_BINARY_COMPARISON_OP_FUNC(logical_or_out_mps, logicalOR, Tensor);
 CREATE_MPS_BINARY_COMPARISON_OP_FUNC(logical_xor_out_mps, logicalXOR, Tensor);
 
 TORCH_IMPL_FUNC(mul_out_mps)(const Tensor& self, const Tensor& other, const Tensor& output) {
-  if (c10::isComplexType(self.scalar_type()) || c10::isComplexType(other.scalar_type())) {
+  if (!mps::supportsComplex() && (c10::isComplexType(self.scalar_type()) || c10::isComplexType(other.scalar_type()))) {
     return mps::complex_mul_out(self, other, output);
   }
-  mps::binary_op_kernel("mul", self, other, output);
+  mps::binaryOpTensor(
+      self, other, Scalar(1.0), output, "mul", ^BinaryOpFn(cachedGraph, primaryCastTensor, secondaryCastTensor) {
+        MPSGraph* mpsGraph = cachedGraph->graph();
+        return [mpsGraph multiplicationWithPrimaryTensor:primaryCastTensor
+                                         secondaryTensor:secondaryCastTensor
+                                                    name:nil];
+      });
 }
 TORCH_IMPL_FUNC(atan2_out_mps)(const Tensor& self, const Tensor& other, const Tensor& output) {
   mps::binaryOpTensor(
