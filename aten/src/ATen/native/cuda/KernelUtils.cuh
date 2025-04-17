355b942d376ef10b008ed04a2bc072a61f90f0a1
Prachi Gupta <prachi.gupta@amd.com>
2025-04-17 14:32:57 +0000

    fp16 fix

--- aten/src/ATen/native/cuda/KernelUtils.cuh
+++ aten/src/ATen/native/cuda/KernelUtils.cuh
@@ -253,19 +253,21 @@ __device__ __forceinline__ void opportunistic_fastAtomicAdd(
 
         bfi_oneUpVal.s = __builtin_amdgcn_mov_dpp(bfi_.s, 0x130, 0xf, 0xf, 0);
         auto oneUpVal = bfi_oneUpVal.bf;
-        if ((ill_oneUpDst.il - reinterpret_cast<int64_t>(dst) == sizeof(scalar_t)) &&
-                +   (__lane_id()%2==0))
+        if ((ill_oneUpDst.il - reinterpret_cast<int64_t>(dst) == sizeof(scalar_t)) && (__lane_id()%2==0))
         {
-          union bfvs { __hip_bfloat16 bf[2]; vec_short2 vs2; } bfvs_;
+          union bfvs { scalar_t bf[2]; vec_short2 vs2; __half2 df16; };
+          bfvs bfvs_ = {};
           bfvs_.bf[0] = value;
           bfvs_.bf[1] = oneUpVal;
-          __builtin_amdgcn_flat_atomic_fadd_v2bf16((vec_short2*)dst, bfvs_.vs2);
+          if constexpr (std::is_same<scalar_t, c10::BFloat16>::value)
+            __builtin_amdgcn_flat_atomic_fadd_v2bf16((vec_short2*)dst, bfvs_.vs2);
+          else
+            __builtin_amdgcn_flat_atomic_fadd_v2f16((__half2*)dst, bfvs_.df16);
           return;
-          } else if ((reinterpret_cast<int64_t>(dst) - ill_oneDnDst.il == sizeof(scalar_t)) &&
-                  + (__lane_id()%2==1))
-          {
-            return;
-          }
+        } else if ((reinterpret_cast<int64_t>(dst) - ill_oneDnDst.il == sizeof(scalar_t)) && (__lane_id()%2==1))
+        {
+          return;
+        }
     }
 
     // not coalsced, so now let try to capture lane-matches...
