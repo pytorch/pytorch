d8634edbb95daf59b2cf90a20ed772d6020c7bdf
James Wu <jjwu@meta.com>
2025-04-17 11:53:11 -0700

    Update
    [ghstack-poisoned]

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -7003,11 +7003,11 @@ class MockFXGraphCache:
         key, _ = compiled_fx_graph_hash(gm, inputs, {}, [])
         if key not in self.cache:
             self.cache[key] = gm
-        gm, _ = self.load_with_key(key, [], inputs, None, None, None, None)
+        gm, _ = self.load_with_key(key, [], inputs, None, None, None, None, None)
         return gm
 
     def load_with_key(
-        self, key, debug_lines, inputs, local, remote_cache, is_backward, constants
+        self, key, debug_lines, inputs, local, remote_cache, is_backward, constants, evaluate_guards
     ):
         gm = self.cache.get(key)
         if gm is not None:
