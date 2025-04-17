bdb34f55a0c44f82d914dc9b41e785b2eed97675
Animesh Jain <anijain@umich.edu>
2025-04-17 10:58:03 -0700

    [fake tensor cache] Support index with non bool/int8 indices (#151477)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/151477
    Approved by: https://github.com/zou3519, https://github.com/bdhirsh
    ghstack dependencies: #151330, #151256, #151357

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -1539,6 +1539,21 @@ class FakeTensorMode(TorchDispatchMode):
             raise _BypassDispatchCache("data dependent output")
 
         if torch.Tag.dynamic_output_shape in func.tags:
+            if func is aten.index.Tensor:
+                _, new_kwargs = normalize_function(  # type: ignore[misc]
+                    func, args=args, kwargs=kwargs, normalize_to_only_use_kwargs=True  # type: ignore[arg-type]
+                )
+                for index in new_kwargs["indices"]:
+                    # index calls nonzero for bool or int8 tensors, and
+                    # therefore has a dynamic shape output. For other dtypes,
+                    # the output shape depends on the input shape (and not data)
+                    if isinstance(index, torch.Tensor) and index.dtype in (
+                        torch.bool,
+                        torch.int8,
+                    ):
+                        raise _BypassDispatchCache("dynamic output shape")
+                return
+
             raise _BypassDispatchCache("dynamic output shape")
 
         if torch.Tag.inplace_view in func.tags:
