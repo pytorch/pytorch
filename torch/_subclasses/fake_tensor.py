24cbfbd4c05991455b29ab5c7c99520d19f2aee0
Animesh Jain <anijain@umich.edu>
2025-04-17 17:41:17 -0700

    [invoke_subgraph][fake tensor] Add finalizer on subgraph instead of the functionalize ctx wrapper
    [ghstack-poisoned]

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -1632,7 +1632,9 @@ class FakeTensorMode(TorchDispatchMode):
                 # Special case for AOT Dispatcher first pass, where the fake
                 # tensor is called on the functional wrapper of the subgraph.
                 result.append(hash(arg))
-                id_hashed_objects.append(arg)
+                # functional wrapper is destroyed after fake tensor prop. We
+                # need to put the finalizer on the subgraph.
+                id_hashed_objects.append(arg.subgraph)
             else:
                 # It's important to capture the type of the arg since, e.g., 1 and 1.0
                 # hash to the same value, but can produce different dtypes for the
