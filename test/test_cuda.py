783be8f93248ca3af24b968bdf84188f5a3257d1
FFFrog <ljw1101.vip@gmail.com>
2025-04-18 10:09:57 +0800

    [Easy] Add more check for elapsedTime of torch.xxx.Event and torch.Event (#151404)
    As the title stated
    
    **Changes:**
    - Add **record**, **query** and **enable_timing** check
    - Add related tests
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/151404
    Approved by: https://github.com/albanD

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -961,6 +961,22 @@ class TestCuda(TestCase):
         self.assertTrue(event.query())
         self.assertGreater(start_event.elapsed_time(event), 0)
 
+    def test_events_elapsedtime(self):
+        event1 = torch.cuda.Event(enable_timing=False)
+        event2 = torch.cuda.Event(enable_timing=False)
+        with self.assertRaisesRegex(
+            ValueError, "Both events must be recorded before calculating elapsed time"
+        ):
+            event1.elapsed_time(event2)
+
+        event1.record()
+        event2.record()
+        with self.assertRaisesRegex(
+            ValueError,
+            "Both events must be created with argument 'enable_timing=True'",
+        ):
+            event1.elapsed_time(event2)
+
     def test_generic_stream_event(self):
         stream = torch.Stream("cuda")
         self.assertEqual(stream.device_index, torch.cuda.current_device())
