2c242dc0ce17f4ffd0337d5df1a6a7d7bffacfba
Boyuan Feng <boyuan@meta.com>
2025-04-17 17:52:10 -0700

    fix error in vars_and_sizes

--- test/inductor/test_flex_attention.py
+++ test/inductor/test_flex_attention.py
@@ -1230,6 +1230,50 @@ class TestFlexAttention(InductorTestCase):
 
     @supported_platform
     @common_utils.parametrize("dtype", test_dtypes_fast)
+    def test_small_block_mask(self, device, dtype: torch.dtype):
+        compiled_create_block_mask = torch.compile(create_block_mask)
+
+        def create_block_mask_from_seqlens(
+            q_batch: torch.Tensor,
+            kv_batch: torch.Tensor,
+        ) -> BlockMask:
+            B, H = None, None
+            Q_LEN = q_batch.size(0)
+            KV_LEN = kv_batch.size(0)
+
+            def batch_mask_mod(
+                b: torch.Tensor,
+                h: torch.Tensor,
+                q_idx: torch.Tensor,
+                kv_idx: torch.Tensor,
+            ):
+                q_idx_batch = q_batch[q_idx]
+                kv_idx_batch = kv_batch[kv_idx]
+                batch_mask = (
+                    (q_idx_batch == kv_idx_batch)
+                    & (q_idx_batch != -1)
+                    & (kv_idx_batch != -1)
+                )
+
+                return batch_mask
+
+            return compiled_create_block_mask(
+                batch_mask_mod,
+                B=B,
+                H=H,
+                Q_LEN=Q_LEN,
+                KV_LEN=KV_LEN,
+            )
+
+        a = torch.tensor([2, 42, 18, 21, 4, 2, 7, 1, 1]).cuda()
+        b = torch.tensor([57, 21, 16, 8]).cuda()
+
+        for seqlen in [a, b]:
+            create_block_mask_from_seqlens(seqlen, seqlen)
+            torch.cuda.synchronize()
+
+    @supported_platform
+    @common_utils.parametrize("dtype", test_dtypes_fast)
     @common_utils.parametrize("batch_dims", test_Bq_Bkv)
     @common_utils.parametrize("head_dims", test_Hq_Hkv)
     @common_utils.parametrize("score_mod", test_score_mods)
