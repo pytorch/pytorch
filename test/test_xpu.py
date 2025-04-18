891e6cc27807b402501d30ecc81b4b29188b4aaa
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2025-04-18 11:03:54 +0800

    Update test/test_xpu.py

--- test/test_xpu.py
+++ test/test_xpu.py
@@ -159,7 +159,7 @@ if __name__ == "__main__":
         )
         self.assertRegex(stderr, "Cannot re-initialize XPU in forked subprocess.")
 
-    @unittest.skipIf(IS_WINDOWS, "not applicable to Windows (only fails with fork)")
+    @unittest.skipIf(IS_WINDOWS, "Only for lazy initialization on Linux, not applicable on Windows.")
     def test_lazy_init(self):
         """Validate that no XPU calls are made during `import torch` call"""
 
