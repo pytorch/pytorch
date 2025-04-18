9c77e32ce1459245f24a80c60e386fc216845bd8
Nikita Shulga <nshulga@meta.com>
2025-04-17 18:47:38 -0700

    Update
    [ghstack-poisoned]

--- test/test_mps.py
+++ test/test_mps.py
@@ -8900,9 +8900,11 @@ class TestLogical(TestCaseMPS):
         [helper(dtype) for dtype in dtypes]
 
         # Mixed dtypes (see https://github.com/pytorch/pytorch/issues/151443 )
-        x = torch.arange(4.0, device="mps")
-        y = torch.tensor([1, 3], device="mps", dtype=torch.float16)
-        self.assertEqual(torch.isin(x, y), torch.tensor([False, True, False, True], device="mps"))
+        # torch.isin is broken in MacOS-13.2 even for the same dtype
+        if MACOS_VERSION >= 14.0:
+            x = torch.arange(4.0, device="mps")
+            y = torch.tensor([1, 3], device="mps", dtype=torch.float16)
+            self.assertEqual(torch.isin(x, y), torch.tensor([False, True, False, True], device="mps"))
 
     def test_isin_asserts(self):
         C = torch.randn(size=[1, 4], device='mps', dtype=torch.float32)
