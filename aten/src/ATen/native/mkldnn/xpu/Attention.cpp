8283902b12dcba7aa9bbfa0cf72b411f495b88a9
LuFengqing <fengqing.lu@intel.com>
2025-04-21 09:41:44 +0800

    Update aten/src/ATen/native/mkldnn/xpu/Attention.cpp
    Co-authored-by: Yu, Guangye <106960996+guangyey@users.noreply.github.com>

--- aten/src/ATen/native/mkldnn/xpu/Attention.cpp
+++ aten/src/ATen/native/mkldnn/xpu/Attention.cpp
@@ -21,8 +21,7 @@ bool check_head_dim_size_xpu(sdp::sdp_params const& params, bool debug) {
     }
     return false;
   }
-  const auto max_size_last =
-      query_size_last > value_size_last ? query_size_last : value_size_last;
+  const auto max_size_last = query_size_last.max(value_size_last);
   constexpr int MAX_HEAD_DIM = 256;
   if (max_size_last > MAX_HEAD_DIM) {
     if (debug) {
