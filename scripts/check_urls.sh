8d0f32e6bba77f095e3ef7573e44f8e44772baf8
Anthony Shoumikhin <shoumikhin@meta.com>
2025-04-21 15:15:14 -0700

    Update check_urls.sh

--- scripts/check_urls.sh
+++ scripts/check_urls.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 
 set -euo pipefail
 
