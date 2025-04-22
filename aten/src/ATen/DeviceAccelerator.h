01347fa3e86b286194112dbd0d8158d97f694376
FFFrog <ljw1101.vip@gmail.com>
2025-04-22 12:11:40 +0800

    Update
    [ghstack-poisoned]

--- aten/src/ATen/DeviceAccelerator.h
+++ aten/src/ATen/DeviceAccelerator.h
@@ -26,13 +26,6 @@ TORCH_API std::optional<c10::DeviceType> getAccelerator(bool checked = false);
 // Check if the given device type is an accelerator.
 TORCH_API bool isAccelerator(c10::DeviceType device_type);
 
-// Check if the given device type is an accelerator, not an excluded one.
-TORCH_API inline bool isAcceleratorExcluded(
-    c10::DeviceType device_type,
-    c10::DeviceType excluded) {
-  return device_type != excluded && isAccelerator(device_type);
-}
-
 // Check if the given device type is an accelerator, not the excluded ones.
 template <
     typename... T,
@@ -41,8 +34,12 @@ TORCH_API inline bool isAcceleratorExcluded(
     c10::DeviceType device_type,
     c10::DeviceType first_excluded,
     T... rest_excluded) {
-  return device_type != first_excluded &&
-      isAcceleratorExcluded(device_type, rest_excluded...);
+  if constexpr (sizeof...(rest_excluded) > 0) {
+    return device_type != first_excluded &&
+        isAcceleratorExcluded(device_type, rest_excluded...);
+  } else {
+    return device_type != first_excluded && isAccelerator(device_type);
+  }
 }
 
 // Return the number of the device available. Note that this is *REQUIRED* to
