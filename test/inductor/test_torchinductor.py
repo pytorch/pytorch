f68c917cfa3deb329e83260b661ec2f4b692bc27
Shaoyu Yang <shaoyuyoung@gmail.com>
2025-04-25 12:28:24 +0800

    Update test_torchinductor.py

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -6619,10 +6619,12 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
 
     def test_replication_pad_errors_with_bool(self):
         for dim in (1, 2, 3):
+
             def fn(x):
                 x = torch.signbit(x)
                 x = eval(f"nn.ReplicationPad{dim}d(padding=1)")(x)
                 return x
+    
             c_fn = torch.compile(fn)
             x = torch.randn([1] * (dim + 2))
             with self.assertRaisesRegex(
