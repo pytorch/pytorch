fa1b4ef6499cacf82da13b5e4da857e0e2e24f66
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-04-26 00:44:11 +0000

    Revert "Rewrite the guts of torch::jit::Lexer to speed it up (#151850)"
    This reverts commit 47d34261e06e2416e7a1e7d51a3d428e4ea51f9d.
    
    Reverted https://github.com/pytorch/pytorch/pull/151850 on behalf of https://github.com/ZainRizvi due to This codev PR is breaking  on it's internal counterpart diff D73129443.  For codev PRs like this one, please always make sure the internal diff is green and then land the diff internally. The Github PR will be automatically merged ([comment](https://github.com/pytorch/pytorch/pull/151850#issuecomment-2831686141))

--- torch/csrc/jit/frontend/parser_constants.h
+++ torch/csrc/jit/frontend/parser_constants.h
@@ -1,6 +1,5 @@
 #pragma once
 
 namespace torch::jit {
-[[maybe_unused]] static const char* valid_single_char_tokens =
-    "+-*/%@()[]:,={}><.?!&^|~";
+static const char* valid_single_char_tokens = "+-*/%@()[]:,={}><.?!&^|~";
 } // namespace torch::jit
