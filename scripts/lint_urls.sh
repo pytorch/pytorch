8f9ec7aad9ad55ba799c1d6661d3bdd1dd584ec3
Anthony Shoumikhin <shoumikhin@meta.com>
2025-04-25 17:21:09 -0700

    Update lint_urls.sh

--- scripts/lint_urls.sh
+++ scripts/lint_urls.sh
@@ -5,33 +5,41 @@ set -euo pipefail
 status=0
 green='\e[1;32m'; red='\e[1;31m'; cyan='\e[1;36m'; yellow='\e[1;33m'; reset='\e[0m'
 user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-last_filepath=
+max_jobs=20
+
+running_jobs() {
+  jobs -rp | wc -l
+}
 
 while IFS=: read -r filepath url; do
-  if [ "$filepath" != "$last_filepath" ]; then
-    printf '\n%s:\n' "$filepath"
-    last_filepath=$filepath
-  fi
-  code=$(curl -gsLm30 --retry 3 --retry-delay 3 --retry-connrefused -o /dev/null -w "%{http_code}" -I "$url") || code=000
-  if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
-    code=$(curl -gsLm30 --retry 3 --retry-delay 3 --retry-connrefused -o /dev/null -w "%{http_code}" -r 0-0 -A "$user_agent" "$url") || code=000
-  fi
-  if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
-    request_id=$(curl -sS -H 'Accept: application/json' "https://check-host.net/check-http?host=$url&max_nodes=1&node=us3.node.check-host.net" \
-      | jq -r .request_id)
-    for _ in {1..3}; do
-      code=$(curl -sS -H 'Accept: application/json' "https://check-host.net/check-result/$request_id" \
-        | jq -r -e '.[][0][3]') || code=000
-      [[ "$code" =~ ^[0-9]+$ ]] || code=000
-      sleep 3
-    done
-  fi
-  if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
-    printf "${red}%s${reset} ${yellow}%s${reset}\n" "$code" "$url" >&2
-    status=1
-  else
-    printf "${green}%s${reset} ${cyan}%s${reset}\n" "$code" "$url"
-  fi
+  fpath="$filepath"
+  (
+    code=$(curl -gsLm30 --retry 3 --retry-delay 3 --retry-connrefused -o /dev/null -w "%{http_code}" -I "$url") || code=000
+    if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
+      code=$(curl -gsLm30 --retry 3 --retry-delay 3 --retry-connrefused -o /dev/null -w "%{http_code}" -r 0-0 -A "$user_agent" "$url") || code=000
+    fi
+    if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
+      request_id=$(curl -sS -H 'Accept: application/json' \
+        "https://check-host.net/check-http?host=$url&max_nodes=1&node=us3.node.check-host.net" \
+        | jq -r .request_id)
+      for _ in {1..3}; do
+        code=$(curl -sS -H 'Accept: application/json' \
+          "https://check-host.net/check-result/$request_id" \
+          | jq -r -e '.[][0][3]') || code=000
+        [[ "$code" =~ ^[0-9]+$ ]] || code=000
+        sleep 3
+      done
+    fi
+    if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
+      printf "${red}%s${reset} ${yellow}%s${reset} %s\n" "$code" "$url" "$fpath" >&2
+      status=1
+    else
+      printf "${green}%s${reset} ${cyan}%s${reset} %s\n" "$code" "$url" "$fpath"
+    fi
+  ) &
+  while [ "$(running_jobs)" -ge "$max_jobs" ]; do
+    sleep 0.1
+  done
 done < <(
   git --no-pager grep --no-color -I -P -o \
     '(?!.*@lint-ignore)(?<!git\+)(?<!\$\{)https?://(?![^\s<>\")]*[<>\{\}\$])[^[:space:]<>\")\[\]\(\\]+' \
@@ -43,12 +51,12 @@ done < <(
     ':(exclude)**/*.xml' \
     ':(exclude)**/*.gradle*' \
     ':(exclude)**/*gradle*' \
-    ':(exclude)**/third-party/**' \
-    ':(exclude)**/third_party/**' \
+    ':(exclude,glob)**/third-party/**' \
+    ':(exclude,glob)**/third_party/**' \
   | sed -E 's/[^/[:alnum:]]+$//' \
   | grep -Ev '://(0\.0\.0\.0|127\.0\.0\.1|localhost)([:/])' \
   | grep -Ev 'fwdproxy:8080' \
   || true
 )
-
+wait
 exit $status
