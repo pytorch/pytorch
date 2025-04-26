0a427840e1be7aa427779b50c3ff6ab8263ec530
Anthony Shoumikhin <shoumikhin@meta.com>
2025-04-26 00:54:28 -0700

    Update lint_urls.sh

--- scripts/lint_urls.sh
+++ scripts/lint_urls.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 status=0
 green='\e[1;32m'; red='\e[1;31m'; cyan='\e[1;36m'; yellow='\e[1;33m'; reset='\e[0m'
 user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-max_jobs=20
+max_jobs=10
+pids=()
 
 running_jobs() {
   jobs -rp | wc -l
@@ -32,11 +33,13 @@ while IFS=: read -r filepath url; do
     fi
     if [ "$code" -lt 200 ] || [ "$code" -ge 400 ]; then
       printf "${red}%s${reset} ${yellow}%s${reset} %s\n" "$code" "$url" "$fpath" >&2
-      status=1
+      exit 1
     else
       printf "${green}%s${reset} ${cyan}%s${reset} %s\n" "$code" "$url" "$fpath"
+      exit 0
     fi
   ) &
+  pids+=($!)
   while [ "$(running_jobs)" -ge "$max_jobs" ]; do
     sleep 0.1
   done
@@ -45,12 +48,12 @@ done < <(
     '(?!.*@lint-ignore)(?<!git\+)(?<!\$\{)https?://(?![^\s<>\")]*[<>\{\}\$])[^[:space:]<>\")\[\]\(\\]+' \
     -- '*' \
     ':(exclude).*' \
-    ':(exclude)**/.*' \
-    ':(exclude)**/*.lock' \
-    ':(exclude)**/*.svg' \
-    ':(exclude)**/*.xml' \
-    ':(exclude)**/*.gradle*' \
-    ':(exclude)**/*gradle*' \
+    ':(exclude,glob)**/.*' \
+    ':(exclude,glob)**/*.lock' \
+    ':(exclude,glob)**/*.svg' \
+    ':(exclude,glob)**/*.xml' \
+    ':(exclude,glob)**/*.gradle*' \
+    ':(exclude,glob)**/*gradle*' \
     ':(exclude,glob)**/third-party/**' \
     ':(exclude,glob)**/third_party/**' \
   | sed -E 's/[^/[:alnum:]]+$//' \
@@ -58,5 +61,9 @@ done < <(
   | grep -Ev 'fwdproxy:8080' \
   || true
 )
-wait
+for pid in "${pids[@]}"; do
+  if ! wait $pid; then
+    status=1
+  fi
+done
 exit $status
