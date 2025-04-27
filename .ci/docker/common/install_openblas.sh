c02edba86388d1f86a78bce99d16c5405b54086e
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-04-27 18:58:35 +0000

    Revert "Update OpenBLAS commit  (#151547)"
    This reverts commit c4b085475062270946eeec854aa54d0739c7a0c9.
    
    Reverted https://github.com/pytorch/pytorch/pull/151547 on behalf of https://github.com/malfet due to It breaks all aarch64 tests ([comment](https://github.com/pytorch/pytorch/pull/151547#issuecomment-2833593427))

--- .ci/docker/common/install_openblas.sh
+++ .ci/docker/common/install_openblas.sh
@@ -4,11 +4,8 @@
 set -ex
 
 cd /
-OPENBLAS_HASH="b30dc9701f8e971720a02e24068acea274fd9cee" #Use SVE kernel for S/DGEMVT for SVE machines
-OPENBLAS_CHECKOUT_DIR="OpenBLAS"
-git clone https://github.com/OpenMathLib/OpenBLAS.git -b develop --shallow-submodules
-git -C $OPENBLAS_CHECKOUT_DIR fetch --depth 1 origin $OPENBLAS_HASH
-git -C $OPENBLAS_CHECKOUT_DIR checkout $OPENBLAS_HASH
+git clone https://github.com/OpenMathLib/OpenBLAS.git -b v0.3.29 --depth 1 --shallow-submodules
+
 
 OPENBLAS_BUILD_FLAGS="
 NUM_THREADS=128
@@ -17,8 +14,9 @@ NO_SHARED=0
 DYNAMIC_ARCH=1
 TARGET=ARMV8
 CFLAGS=-O3
-BUILD_BFLOAT16=1
 "
+
+OPENBLAS_CHECKOUT_DIR="OpenBLAS"
 
 make -j8 ${OPENBLAS_BUILD_FLAGS} -C ${OPENBLAS_CHECKOUT_DIR}
 make -j8 ${OPENBLAS_BUILD_FLAGS} install -C ${OPENBLAS_CHECKOUT_DIR}
