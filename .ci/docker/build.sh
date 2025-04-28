8f5c956762ea1a09e45df199e96375033fde7448
Aditya Tewari <aditya.tewari@arm.com>
2025-04-28 07:13:49 +0000

    update openblas revert

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -371,7 +371,6 @@ case "$image" in
     ACL=yes
     VISION=yes
     CONDA_CMAKE=yes
-    OPENBLAS=yes
     # snadampal: skipping llvm src build install because the current version
     # from pytorch/llvm:9.0.1 is x86 specific
     SKIP_LLVM_SRC_BUILD_INSTALL=yes
@@ -382,7 +381,6 @@ case "$image" in
     ACL=yes
     VISION=yes
     CONDA_CMAKE=yes
-    OPENBLAS=yes
     # snadampal: skipping llvm src build install because the current version
     # from pytorch/llvm:9.0.1 is x86 specific
     SKIP_LLVM_SRC_BUILD_INSTALL=yes
@@ -484,7 +482,6 @@ docker build \
        --build-arg "HALIDE=${HALIDE}" \
        --build-arg "XPU_VERSION=${XPU_VERSION}" \
        --build-arg "ACL=${ACL:-}" \
-       --build-arg "OPENBLAS=${OPENBLAS:-}" \
        --build-arg "SKIP_SCCACHE_INSTALL=${SKIP_SCCACHE_INSTALL:-}" \
        --build-arg "SKIP_LLVM_SRC_BUILD_INSTALL=${SKIP_LLVM_SRC_BUILD_INSTALL:-}" \
        -f $(dirname ${DOCKERFILE})/Dockerfile \
