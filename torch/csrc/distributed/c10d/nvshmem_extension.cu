6751fecdae12cd18e4a97e26a8f39fd3641359d8
Ke Wen <kw2501@meta.com>
2025-04-28 10:04:29 -0700

    Update on "[WIP] experimental NVSHMEM integration"
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k
    
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/nvshmem_extension.cu
+++ torch/csrc/distributed/c10d/nvshmem_extension.cu
@@ -3,6 +3,7 @@
 #include <c10/cuda/CUDAGuard.h>
 
 #include <torch/csrc/distributed/c10d/CUDASymmetricMemory-inl.h>
+#include <torch/csrc/distributed/c10d/CUDASymmetricMemoryUtils.hpp>
 #include <torch/csrc/distributed/c10d/SymmetricMemory.hpp>
 
 #include <cuda_awbarrier_primitives.h>
@@ -11,46 +12,6 @@
 namespace c10d::nvshmem_extension {
 
 const std::string store_comm_prefix = "nvshmem_extension";
-static size_t store_comm_seq_id = 0;
-
-template <typename T>
-std::vector<T> store_all_gather(
-    const c10::intrusive_ptr<c10d::Store>& store,
-    int rank,
-    int world_size,
-    T val) {
-  static_assert(std::is_trivially_copyable_v<T>);
-
-  std::vector<std::string> peer_keys;
-  for (int r = 0; r < world_size; ++r) {
-    std::ostringstream oss;
-    oss << store_comm_prefix << "/" << store_comm_seq_id << "/" << r;
-    peer_keys.push_back(oss.str());
-  }
-  ++store_comm_seq_id;
-
-  {
-    std::vector<uint8_t> payload(
-        reinterpret_cast<uint8_t*>(&val),
-        reinterpret_cast<uint8_t*>(&val) + sizeof(T));
-    store->set(peer_keys[rank], payload);
-  }
-
-  std::vector<T> peer_vals;
-  for (int r = 0; r < world_size; ++r) {
-    if (r == rank) {
-      peer_vals.push_back(val);
-      continue;
-    }
-    store->wait({peer_keys[r]});
-    auto payload = store->get(peer_keys[r]);
-    TORCH_CHECK(payload.size() == sizeof(T));
-    T peer_val{};
-    std::memcpy(&peer_val, payload.data(), sizeof(T));
-    peer_vals.push_back(peer_val);
-  }
-  return peer_vals;
-}
 
 void maybe_initialize_env_vars() {
   auto nccl_socket_if_name = c10::utils::get_env("NCCL_SOCKET_IFNAME");
@@ -91,7 +52,7 @@ void initialize_nvshmem_with_store(
 
   // Using an existing store_all_gather due to laziness.
   // TODO(yifu): should use broadcast
-  auto unique_ids = store_all_gather(store, rank, world_size, unique_id);
+  auto unique_ids = c10d::symmetric_memory::store_all_gather(store, rank, world_size, unique_id);
 
   nvshmemx_init_attr_t attr;
   nvshmemx_set_attr_uniqueid_args(rank, world_size, &unique_ids[0], &attr);
@@ -419,3 +380,11 @@ at::Tensor nvshmem_reduce_scatter_out(
 }
 
 } // namespace c10d::nvshmem_extension
+
+
+TORCH_LIBRARY_IMPL(symm_mem, CUDA, m) {
+  m.impl("nvshmem_broadcast", c10d::nvshmem_extension::nvshmem_broadcast);
+  m.impl(
+      "nvshmem_reduce_scatter_out",
+      c10d::nvshmem_extension::nvshmem_reduce_scatter_out);
+}
