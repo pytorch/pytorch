8a1acbce405a6a1d4d84161ce84cbb4a42428229
Ke Wen <kw2501@meta.com>
2025-04-28 10:04:30 -0700

    Update base for Update on "[WIP][SymmMem] Add sendrecv op"
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k
    
    [ghstack-poisoned]

---
+++ torch/csrc/distributed/c10d/CUDASymmetricMemoryUtils.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <torch/csrc/distributed/c10d/CUDASymmetricMemoryTypes.hpp>
+#include <torch/csrc/distributed/c10d/Store.hpp>
+#include <torch/csrc/distributed/c10d/SymmetricMemory.hpp>
+
+namespace c10d {
+namespace symmetric_memory {
+
+bool device_has_multicast_support(int device_idx);
+
+bool allow_overlapping_devices();
+
+// Query environment variable to get the backend used for CUDA Symmetric Memory.
+std::string getSymmMemBackendCUDA();
+
+class IpcChannel {
+ public:
+  IpcChannel();
+  ~IpcChannel();
+
+  void send_fd(int dst_pid, int fd);
+  int recv_fd();
+
+  std::vector<int> all_gather_fds(
+      int rank,
+      const std::vector<int>& pids,
+      int fd);
+
+  int broadcast_fds(
+      int rank,
+      int src_rank,
+      const std::vector<int>& pids,
+      int fd);
+
+ private:
+  static std::string get_socket_name(int pid);
+
+  std::string socket_name_;
+  int socket_;
+};
+
+// Defined at respective callsites i.e. SymmetricMemory implementation files.
+extern const std::string store_comm_prefix;
+
+// Put template function in header file so that compiler can easily access it.
+template <typename T>
+std::vector<T> store_all_gather(
+    const c10::intrusive_ptr<c10d::Store>& store,
+    int rank,
+    int world_size,
+    T val) {
+  static_assert(std::is_trivially_copyable_v<T>);
+  static size_t store_comm_seq_id = 0;
+
+  std::vector<std::string> peer_keys;
+  for (int r = 0; r < world_size; ++r) {
+    std::ostringstream oss;
+    oss << store_comm_prefix << "/" << store_comm_seq_id << "/" << r;
+    peer_keys.push_back(oss.str());
+  }
+  ++store_comm_seq_id;
+
+  {
+    std::vector<uint8_t> payload(
+        reinterpret_cast<uint8_t*>(&val),
+        reinterpret_cast<uint8_t*>(&val) + sizeof(T));
+    store->set(peer_keys[rank], payload);
+  }
+
+  std::vector<T> peer_vals;
+  for (int r = 0; r < world_size; ++r) {
+    if (r == rank) {
+      peer_vals.push_back(val);
+      continue;
+    }
+    store->wait({peer_keys[r]});
+    auto payload = store->get(peer_keys[r]);
+    TORCH_CHECK(payload.size() == sizeof(T));
+    T peer_val{};
+    std::memcpy(&peer_val, payload.data(), sizeof(T));
+    peer_vals.push_back(peer_val);
+  }
+  return peer_vals;
+}
+
+void store_barrier(
+    const c10::intrusive_ptr<c10d::Store>& store,
+    int rank,
+    int world_size);
+
+void map_block(
+    void** ptr,
+    c10d::symmetric_memory::HandleType handle,
+    size_t size,
+    int device_idx);
+
+} // namespace symmetric_memory
+} // namespace c10d
