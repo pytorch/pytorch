e407ea1e5e22a41d14ce141295bf391cd46f2677
Scott Wolchok <swolchok@meta.com>
2025-04-28 09:15:36 -0700

    Reapply "Rewrite the guts of torch::jit::Lexer to speed it up (#151850)" (#152250)
    Almost-exact reapply of #151850 (adding minor reviewer nits) . AFAICT it was reverted unnecessarily.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152250
    Approved by: https://github.com/Skylion007, https://github.com/cyyever

--- test/cpp/jit/test_lexer.cpp
+++ test/cpp/jit/test_lexer.cpp
@@ -29,7 +29,7 @@ TEST(LexerTest, AllTokens) {
 TEST(LexerTest, SlightlyOffIsNot) {
   std::vector<std::string> suffixes = {"", " ", "**"};
   for (const auto& suffix : suffixes) {
-    std::vector<std::string> extras = {"n", "no", "no3"};
+    std::vector<std::string> extras = {"n", "no", "no3", "note"};
     for (const auto& extra : extras) {
       std::string s = "is " + extra + suffix;
       Lexer l(std::make_shared<Source>(s));
@@ -45,7 +45,7 @@ TEST(LexerTest, SlightlyOffIsNot) {
 TEST(LexerTest, SlightlyOffNotIn) {
   std::vector<std::string> suffixes = {"", " ", "**"};
   for (const auto& suffix : suffixes) {
-    std::vector<std::string> extras = {"i", "i3"};
+    std::vector<std::string> extras = {"i", "i3", "inn"};
     for (const auto& extra : extras) {
       std::string s = "not " + extra + suffix;
       Lexer l(std::make_shared<Source>(s));
@@ -56,33 +56,5 @@ TEST(LexerTest, SlightlyOffNotIn) {
       EXPECT_EQ(in_tok.range.text().str(), extra) << in_tok.range.text().str();
     }
   }
-}
-
-TEST(LexerTest, IsNoteBug) {
-  // The code string `is note` is lexed as TK_ISNOT followed by a
-  // TK_IDENT that is an e. This is not how it works in Python, but
-  // presumably we need to maintain this behavior.
-  Lexer l(std::make_shared<Source>("is note"));
-  const auto is_not_tok = l.next();
-  EXPECT_EQ(is_not_tok.kind, TK_ISNOT);
-  const auto e_tok = l.next();
-  EXPECT_EQ(e_tok.kind, TK_IDENT);
-  EXPECT_EQ(e_tok.range.text(), "e");
-  const auto eof_tok = l.next();
-  EXPECT_EQ(eof_tok.kind, TK_EOF);
-}
-
-TEST(LexerTest, NotInpBug) {
-  // Another manifestation of the above IsNoteBug; `not inp` is lexed
-  // as TK_NOT_IN followed by a TK_IDENT that is a p. Again, not how
-  // it works in Python.
-  Lexer l(std::make_shared<Source>("not inp"));
-  const auto not_in_tok = l.next();
-  EXPECT_EQ(not_in_tok.kind, TK_NOTIN);
-  const auto p_tok = l.next();
-  EXPECT_EQ(p_tok.kind, TK_IDENT);
-  EXPECT_EQ(p_tok.range.text(), "p");
-  const auto eof_tok = l.next();
-  EXPECT_EQ(eof_tok.kind, TK_EOF);
 }
 } // namespace torch::jit
