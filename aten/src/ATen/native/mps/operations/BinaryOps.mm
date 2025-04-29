663bcb68ba5dec3f9d0385e62336eea9d14ae686
Siddharth Kotapati <skotapati@apple.com>
2025-04-29 14:24:49 +0000

    Implement metal kernel for basic MPS arithmetic ops using TensorIterator (#147644)
    Add metal kernels for add, subtract, & lerp ops using TensorIterator. Should help resolve: https://github.com/pytorch/pytorch/issues/143874
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147644
    Approved by: https://github.com/malfet

--- aten/src/ATen/native/mps/operations/BinaryOps.mm
+++ aten/src/ATen/native/mps/operations/BinaryOps.mm
@@ -265,9 +265,21 @@ static void add_sub_lerp_template(const Tensor& self,
   }
 
   const bool alpha_has_value = alpha.toDouble() != 1.0;
-  if (alpha_has_value) {
-    auto commonDtype = at::result_type(self, other);
-    at::native::alpha_check(commonDtype, alpha);
+  auto self_complex = c10::isComplexType(self.scalar_type());
+  auto other_complex = c10::isComplexType(other.scalar_type());
+  auto commonDtype = at::result_type(self, other);
+  if (self.is_mps() && other.is_mps() && (output.scalar_type() == commonDtype) && (self_complex == other_complex)) {
+    if (alpha_has_value) {
+      at::native::alpha_check(commonDtype, alpha);
+      mps::binary_op_kernel((self_complex || other_complex) ? "complex_" + op_name : op_name,
+                            self,
+                            other,
+                            output,
+                            getMPSScalar(alpha, commonDtype));
+    } else {
+      mps::binary_op_kernel(op_name, self, other, output);
+    }
+    return;
   }
 
   if (!alpha_has_value && op_name == "lerp") {
