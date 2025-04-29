1dca9417edd61234ab10078ef7fe424722339732
Catherine Lee <csl@fb.com>
2025-04-29 09:58:44 -0700

    tc

--- .github/scripts/lintrunner.sh
+++ .github/scripts/lintrunner.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Use uv to speed up lintrunner init
-python3 -m pip install uv==0.1.45 setuptools==80.0.0
+python3 -m pip install uv==0.1.45 setuptools
 
 CACHE_DIRECTORY="/tmp/.lintbin"
 # Try to recover the cached binaries
