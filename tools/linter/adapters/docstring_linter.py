a777cba1f027e8ff3b4333c17151fc7e895acdd0
Tom Ritchford <tom@swirly.com>
2025-04-29 10:51:12 +0000

    Update (base update)
    [ghstack-poisoned]

--- tools/linter/adapters/docstring_linter.py
+++ tools/linter/adapters/docstring_linter.py
@@ -126,7 +126,7 @@ class Block:
         Each decorator both the @ symbol and any arguments to the decorator
         but no extra whitespace.
         """
-        return list(_get_decorators(self.tokens, self.begin))
+        return _get_decorators(self.tokens, self.begin)
 
     @cached_property
     def is_override(self) -> bool:
@@ -195,7 +195,9 @@ def _get_decorators(tokens: Sequence[TokenInfo], block_start: int) -> list[str]:
                     return  # A statement means no more decorators
             end = begin
 
-    return list(decorators())[::-1]
+    out = list(decorators())
+    out.reverse()
+    return out
 
 
 class DocstringFile(_linter.PythonFile):
