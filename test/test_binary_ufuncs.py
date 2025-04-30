afc3b201ff7b5a75ce22f276d0b8feb93fdc6623
Raman kumar <k.raman1998@yahoo.in>
2025-04-30 20:22:17 +0530

    remove dtype

--- test/test_binary_ufuncs.py
+++ test/test_binary_ufuncs.py
@@ -1687,8 +1687,8 @@ class TestBinaryUfuncs(TestCase):
             self._test_pow(base, exp)
 
     @onlyCUDA
-    def test_pow_cuda_complex_extremal_passing(self, device, dtype):
-        t = torch.tensor(complex(-1.0, float("inf")), dtype=dtype, device=device)
+    def test_pow_cuda_complex_extremal_passing(self, device):
+        t = torch.tensor(complex(-1.0, float("inf")), device=device)
         cuda_out = t.pow(2)
         cpu_out = t.cpu().pow(2)
         self.assertEqual(cpu_out, cuda_out)
