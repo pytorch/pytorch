eabc9dd4dc2709f2a4d167fff7da4c92cdaff091
Nikita Shulga <nshulga@meta.com>
2025-04-29 17:51:49 -0700

    Update (base update)
    [ghstack-poisoned]

--- aten/src/ATen/native/mps/operations/BinaryOps.mm
+++ aten/src/ATen/native/mps/operations/BinaryOps.mm
@@ -289,7 +289,6 @@ static void add_sub_lerp_template(const Tensor& self,
     return;
   }
 
-
   BinaryOpBlock add_sub_lerp_op_block = ^BinaryOpFn(cachedGraph, primaryCastTensor, secondaryCastTensor) {
     MPSGraph* mpsGraph = cachedGraph->graph();
     MPSGraphTensor* secondaryTensor = secondaryCastTensor;
