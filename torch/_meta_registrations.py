761d313e17e8e85b8bfbcaa94c4d6cda98338451
Pian Pawakapan <pianpwk@meta.com>
2025-04-29 17:59:15 -0700

    add torch._checks

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -7326,15 +7326,34 @@ def softmax(x: Tensor, dim: int, half_to_float: bool) -> Tensor:
 @register_meta(aten.constant_pad_nd)
 @out_wrapper()
 def _constant_pad_nd_meta(input, pad, value=0):
+    # same checks as decomposition in torch/_refs/__init__.py:constant_pad_nd()
+    torch._check(
+        len(pad) % 2 == 0,
+        lambda: f"Length of pad must be even but instead it equals {len(pad)}",
+    )
+
     input_sizes = input.shape
     l_inp = len(input_sizes)
     l_pad = len(pad) // 2
     l_diff = l_inp - l_pad
 
+    torch._check(
+        l_inp >= l_pad,
+        lambda: "Length of pad should be no more than twice the number of "
+        f"dimensions of the input. Pad length is {len(pad)} while the input has "
+        f"{l_inp} dimensions.",
+    )
+
     new_shape = list(input_sizes[:l_diff])
     for i in range(l_pad):
         pad_idx = len(pad) - ((i + 1) * 2)
         new_dim = input_sizes[l_diff + i] + pad[pad_idx] + pad[pad_idx + 1]
+        torch._check(
+            new_dim > 0,
+            lambda: f"The input size {input_sizes[l_diff + i]}, plus negative padding "
+            f"{pad[pad_idx]} and {pad[pad_idx + 1]} resulted in a negative output size, "
+            f"which is invalid. Check dimension {l_diff + i} of your input.",
+        )
         new_shape.append(new_dim)
 
     return torch.empty(
