01a7fbe4116a10f29fc64dcf06d7b98b84128ec7
Pian Pawakapan <pianpwk@meta.com>
2025-04-29 22:55:44 -0700

    Update test_export.py

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -4274,7 +4274,6 @@ def forward(self, p_linear_weight, p_linear_bias, b_buffer, x):
         ):
             _ = export(M(), (torch.tensor([2, 3, 5]),))
 
-    @testing.expectedFailureTrainingIRToRunDecomp
     @testing.expectedFailureTrainingIRToRunDecompNonStrict
     def test_unbacked_pad(self):
         class Foo(torch.nn.Module):
