0d65b9e95f46aec34ccea559fd771e3031050756
Randolf Scholz <randolf.scholz@gmail.com>
2025-04-30 21:36:22 +0200

    fixed typing test fail

--- test/typing/pass/torch_size.py
+++ test/typing/pass/torch_size.py
@@ -11,16 +11,21 @@ class ZeroIndex:
     def __index__(self) -> int:
         return 0
 
-
+# assignability to tuple
+tup: tuple[int, ...] = s1
 # __getitem__
 assert_type(s1[0], int)
 assert_type(s1[ZeroIndex()], int)
 assert_type(s1[:2], Size)
 # __add__
 assert_type(s1 + s2, Size)
-assert_type(s1 + (1, 2), Size)
+assert_type(s1 + tup, Size)
+assert_type(s1 + (), Size)
+assert_type(s1 + (1,2), Size)
 # __radd__
-assert_type((1, 2) + s1, Size)
+assert_type(tup + s1, Size)
+assert_type(() + s1, Size)  # type: ignore[assert-type]
+assert_type((1, 2) + s1, Size)  # type: ignore[assert-type]
 # __mul__
 assert_type(s1 * 3, Size)
 assert_type(s1 * ZeroIndex(), Size)
