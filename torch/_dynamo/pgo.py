686cfca519399ea2d06388c546aebce53a72d7de
Laith Sakka <lsakka@meta.com>
2025-05-01 15:58:30 -0700

    Update on "Make PGO code state non sensitive to file path by hashing file content when the file is available. "
    In some internal frameworks, on second attempts the actual code is copied to a different path than previous attempts. 
    but its still the same. PGO will not work on those cased due to the following, sate entries before this PR used to be identified by (filepath, function name, line number).
    
    after this PR they are identified by (hash(filepath) , function name, line number). This way PGO will work for those jobs on future attempts and re-compilations of static versions will be avoided.
    
    Sometimes we do not have access to the source code, (file does not exisits)
    This seems to happen mostly when we re-trace a compiled function. 
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_dynamo/pgo.py
+++ torch/_dynamo/pgo.py
@@ -107,6 +107,10 @@ LOCK_TIMEOUT = 10
 
 @functools.cache
 def _hash_containing_file(filepath: str) -> str:
+    # if the file does not exists we consider filepath to be the hash.
+    if not os.path.exists(filepath):
+        return filepath
+
     with open(filepath, "rb") as file:
         content = file.read()
         crc32_value = zlib.crc32(content)
