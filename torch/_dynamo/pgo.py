cb4988dff8b4e91b593d755ee39889f08a45021e
Laith Sakka <lsakka@meta.com>
2025-05-01 13:04:05 -0700

    Update on "Make PGO code state non sensitive to file path by hashing file content. "
    In some internal frameworks, on second attempts the actual code is copied to a different path than previous attempts. 
    but its still the same. PGO will not work on those cased due to the following, sate entries before this PR used to be identified by (filepath, function name, line number).
    
    after this PR they are identified by (hash(filepath) , function name, line number). This way PGO will work for those jobs on future attempts and re-compilations of static versions will be avoided.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_dynamo/pgo.py
+++ torch/_dynamo/pgo.py
@@ -127,7 +127,7 @@ class CodeId:
     # code state it will refer to the first seen file path.
     file_hash: str
 
-    # execlude file name.
+    # Exclude file name.
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, CodeId):
             return False
@@ -137,7 +137,7 @@ class CodeId:
             and self.name == other.name
         )
 
-    # Ensure if two CodeIds are the same, then they have the same hash by execluding filename.
+    # Ensure if two CodeIds are the same, then they have the same hash by excluding filename.
     def __hash__(self) -> int:
         return hash((self.file_hash, self.name, self.firstlineno))
 
