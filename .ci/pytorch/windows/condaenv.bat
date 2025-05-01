7491edad46a2a6054bd1a4413a25d25fdab087c5
atalman <atalman@fb.com>
2025-05-01 12:00:27 -0700

    Revert "test"
    This reverts commit 649de568a80c4effc7b0d5e9a0943fad3e00fd69.

--- .ci/pytorch/windows/condaenv.bat
+++ .ci/pytorch/windows/condaenv.bat
@@ -3,7 +3,6 @@ IF "%DESIRED_PYTHON%"=="" (
     exit /b 1
 )
 
-echo Setting up Python %DESIRED_PYTHON%
 set PYTHON_INSTALLER_URL=
 if "%DESIRED_PYTHON%" == "3.13t" set "PYTHON_INSTALLER_URL=https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe"
 if "%DESIRED_PYTHON%" == "3.13" set "PYTHON_INSTALLER_URL=https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe"
@@ -16,8 +15,6 @@ if "%PYTHON_INSTALLER_URL%" == "" (
 )
 
 del python-amd64.exe
-
-echo Downloading Python %DESIRED_PYTHON%
 curl --retry 3 -kL "%PYTHON_INSTALLER_URL%" --output python-amd64.exe
 if errorlevel 1 exit /b 1
 
@@ -26,19 +23,14 @@ if "%DESIRED_PYTHON%" == "3.13t" (
     set ADDITIONAL_OPTIONS="Include_freethreaded=1"
 )
 
-echo Installing Python %DESIRED_PYTHON%
 start /wait "" python-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 %ADDITIONAL_OPTIONS% TargetDir=%CD%\Python
 if errorlevel 1 exit /b 1
 
 :: Create venv and activate it
-echo Activating Environment py%DESIRED_PYTHON%
 py -%DESIRED_PYTHON% -mvenv py%DESIRED_PYTHON%
 py%DESIRED_PYTHON%\Scripts\activate.bat
 
-python --version
-
 set "PATH=%CD%\Python\Lib\site-packages\cmake\data\bin;%CD%\Python\Scripts;%CD%\Python;%PATH%"
-echo Installing numpy and cmake
 if "%DESIRED_PYTHON%" == "3.13t" python -m pip install numpy==2.2.1 cmake
 if "%DESIRED_PYTHON%" == "3.13" python -m pip install numpy==2.1.2 cmake
 if "%DESIRED_PYTHON%" == "3.12" python -m pip install numpy==2.0.2 cmake
