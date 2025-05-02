c659299f60bfaaab57a047cc272a834b8710f541
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-05-03 00:45:00 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/utils/_cxx_pytree.py
+++ torch/utils/_cxx_pytree.py
@@ -974,8 +974,16 @@ def _broadcast_to_and_flatten(
 
 def treespec_dumps(treespec: TreeSpec, protocol: Optional[int] = None) -> str:
     """Serialize a treespec to a JSON string."""
-    treespec = python_pytree._ensure_python_treespec_instance(treespec)
-    return python_pytree.treespec_dumps(treespec, protocol=protocol)
+    if not _is_pytreespec_instance(treespec):
+        raise TypeError(
+            f"Expected `treespec` to be instance of "
+            f"PyTreeSpec but got item of type {type(treespec)}."
+        )
+
+    dummy_tree = tree_unflatten([0] * treespec.num_leaves, treespec)
+    orig_treespec = python_pytree.tree_structure(dummy_tree)
+    return python_pytree.treespec_dumps(orig_treespec, protocol=protocol)
+
 
 
 @functools.lru_cache
