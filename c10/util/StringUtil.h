3c56547b9f3a3256ffd2e5ebe74d2228a972d4e2
TJ Yin <ytj@meta.com>
2025-05-02 10:39:42 -0700

    [caffe2] Make c10::str works with scoped enum
    Test Plan:
    ```
    buck2 test fbcode//caffe2/c10/test:util_base_tests --fail-fast
    ```
    
    Differential Revision: D73872860

--- c10/util/StringUtil.h
+++ c10/util/StringUtil.h
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <type_traits>
 
 C10_CLANG_DIAGNOSTIC_PUSH()
 #if C10_CLANG_HAS_WARNING("-Wshorten-64-to-32")
@@ -52,9 +53,13 @@ inline std::ostream& _str(std::ostream& ss) {
 
 template <typename T>
 inline std::ostream& _str(std::ostream& ss, const T& t) {
-  // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage)
-  ss << t;
-  return ss;
+  if constexpr (std::is_enum_v<T>) {
+    return _str(ss, static_cast<typename std::underlying_type<T>::type>(t));
+  } else {
+    // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage)
+    ss << t;
+    return ss;
+  }
 }
 
 template <typename T>
