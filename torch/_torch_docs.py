88f765f2681479d63a79fd2d43ad5599438df823
ILCSFNO <138545608+ILCSFNO@users.noreply.github.com>
2025-05-02 19:16:12 +0800

    Fix merge contradiction in torch.quantile()

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -109,16 +109,6 @@ as :attr:`input` except in the dimension :attr:`dim` where it is of size 1.
 Otherwise, :attr:`dim` is squeezed (see :func:`torch.squeeze`), resulting in
 the output tensor having 1 fewer dimension than :attr:`input`."""
     },
-    {
-        "opt_dim": """
-    dim (int, optional): the dimension to reduce.
-"""
-    },
-    {
-        "opt_keepdim": """
-    keepdim (bool, optional): whether the output tensor has :attr:`dim` retained or not. Default: ``False``.
-"""
-    },
 )
 
 factory_common_args = merge_dicts(
