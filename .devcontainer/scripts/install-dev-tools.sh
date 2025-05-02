b02be3e0f4e144435eeb08fcba0720b3589a47c9
Wouter Devriendt <wouterdevriendt@meta.com>
2025-05-02 11:19:15 -0700

    remove conda from devcontainer

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Run this command from the PyTorch directory after cloning the source code using the “Get the PyTorch Source“ section below
+# Run this command from the PyTorch directory after cloning the source code using the "Get the PyTorch Source" section below
 pip install -r requirements.txt
 git submodule sync
 git submodule update --init --recursive
@@ -8,6 +8,7 @@ git submodule update --init --recursive
 make setup-lint
 
 # Add CMAKE_PREFIX_PATH to bashrc
-echo 'export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}' >> ~/.bashrc
+echo 'export CMAKE_PREFIX_PATH=/usr/local' >> ~/.bashrc
+
 # Add linker path so that cuda-related libraries can be found
-echo 'export LDFLAGS="-L${CONDA_PREFIX}/lib/ $LDFLAGS"' >> ~/.bashrc
+echo 'export LDFLAGS="-L/usr/local/cuda/lib64/ $LDFLAGS"' >> ~/.bashrc
