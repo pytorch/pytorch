23994cb8c6238fa4ca19cc86d5312ed093b61b9d
Wei Wang <weiwan@nvidia.com>
2025-05-02 10:44:31 -0700

    Change test_extra_cuda_context non pynvml path heuristics

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -657,7 +657,7 @@ class ProcessGroupNCCLGroupTest(MultiProcessTestCase):
             # fail because one context takes about 1 GB -- much more than the
             # tensor size created in this test.
             self.assertTrue(
-                used_after < used_before * 1.5,
+                used_after < used_before * 1.7,
                 f"{device} used {used_after} bytes after collective, "
                 f"50% more than the status before ({used_before} bytes). "
                 f"Extra CUDA context may have been created.",
