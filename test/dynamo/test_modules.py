9099594fccde76325cd8ad4d2c12c1d0af83e7a7
Ryan Guo <ryanguo99@meta.com>
2025-05-02 17:13:35 -0700

    Update on "[dynamo] Support `delattr` on result of `torch.compile(module)`"
    This is essentially a follow-up on #122098, where we added support of
    `getattr` and `setattr` on result of `torch.compile(module)`, but didn't
    add support for `delattr`.
    
    Fixes #150711.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- test/dynamo/test_modules.py
+++ test/dynamo/test_modules.py
@@ -3057,8 +3057,8 @@ class OptimizedModuleTest(torch._dynamo.test_case.TestCase):
         compiled_model.foo = 42
         del compiled_model.foo
 
-        self.assertFalse(hasattr(model, 'foo'))
-        self.assertFalse(hasattr(compiled_model, 'foo'))
+        self.assertFalse(hasattr(model, "foo"))
+        self.assertFalse(hasattr(compiled_model, "foo"))
 
     def test_globals_change_in_other_file(self):
         @torch.compile(backend="eager", fullgraph=True)
