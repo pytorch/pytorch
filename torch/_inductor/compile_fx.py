5d36485b4aa5823bb9ee5eb070548c4ee355f3b2
Jovian Anthony Jaison <jovian@meta.com>
2025-05-06 16:21:58 +0000

    Log aot and idx waitcounters. (#152444)
    Summary:
    Added for create_aot_dispatcher_function and compile_fx_inner.
    
    Note:
    Log wait counters flag is already set for:
    1. async_compile.precompile
    2. remote_fx_graph_cache_get
    3. remote_fx_graph_cache_put
    
    Test Plan: contbuild
    
    Differential Revision: D73866124
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152444
    Approved by: https://github.com/ppanchalia, https://github.com/masnesral

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -695,6 +695,8 @@ def compile_fx_inner(
                 "compile_fx_inner",
                 phase_name="inductor_compile",
                 log_pt2_compile_event=True,
+                log_waitcounter=True,
+                waitcounter_name_override="compile_inductor",
                 dynamo_compile_column_us="inductor_cumulative_compile_time_us",
             )
         )
