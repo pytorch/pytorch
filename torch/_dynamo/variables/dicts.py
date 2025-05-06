86a006ccb7d474eaa13947ba8d8a255187764638
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-05 22:48:53 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -818,6 +818,15 @@ class SetVariable(ConstDictVariable):
                 return super().call_method(tx, "pop", args, kwargs)
             else:
                 return ConstantVariable.create(value=None)
+        elif name in ("issubset", "issuperset"):
+            op = {
+                "issubset": operator.le,
+                "issuperset": operator.ge,
+            }
+            other = variables.BuiltinVariable(set).call_function(tx, [args[0]], {})
+            return variables.BuiltinVariable(op.get(name)).call_function(
+                tx, [self, other], {}
+            )
         return super().call_method(tx, name, args, kwargs)
 
     def getitem_const(self, tx: "InstructionTranslator", arg: VariableTracker):
