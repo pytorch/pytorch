bf4fbf8b22ee7e963f7e69679330841c4125d6db
Yidi Wu <yidi@meta.com>
2025-05-07 09:49:04 -0700

    [map] always turn on dynamo for map (#150962) (#152041)
    Summary:
    
    
    Reland D72896450
    
    Make map consistent with other control flow ops. After the change, map is able to support accessing closures in the map fn.
    
    Test Plan: See existing tests.
    
    Reviewed By: zou3519, angelayi
    
    Differential Revision: D73138427

--- test/dynamo/test_misc.py
+++ test/dynamo/test_misc.py
@@ -5825,11 +5825,15 @@ utils_device.CURRENT_DEVICE == None""".split(
 
         error_message = ""
         if torch._dynamo.config.inline_inbuilt_nn_modules:
-            error_message = r"HigherOrderOperator: Mutating a variable not in the current scope \(SideEffects\)"
+            error_message = (
+                "map doesn't work unless it is captured completely with torch.compile"
+            )
         else:
             error_message = "Can't inplace modify module params/buffers"
 
-        with self.assertRaisesRegex(Unsupported, error_message):
+        with self.assertRaisesRegex(
+            torch._dynamo.exc.UncapturedHigherOrderOpError, error_message
+        ):
             opt_fn = torch.compile(mod, backend="eager", fullgraph=True)
             opt_fn(torch.randn(3, 2))
 
