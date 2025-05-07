8400400448f384c4a429c325dbbbbb13df796d0c
atalman <atalman@fb.com>
2025-05-07 12:17:14 -0700

    fix

--- .ci/pytorch/check_binary.sh
+++ .ci/pytorch/check_binary.sh
@@ -308,12 +308,12 @@ if [[ "$(uname)" == 'Linux' &&  "$PACKAGE_TYPE" == 'manywheel' ]]; then
   pushd /tmp
   # Per https://gcc.gnu.org/onlinedocs/gcc/C_002b_002b-Dialect-Options.html
   # gcc-11 is ABI16, gcc-13 is ABI18, gcc-14 is ABI19
-  # gcc 11 - CUDA 11.8
+  # gcc 11 - CUDA 11.8 and xpu
   # gcc 13 - CUDA 12.6, 12.8 and cpu
   # Please see issue for reference: https://github.com/pytorch/pytorch/issues/152426
   if [[ "$(uname -m)" == "s390x" ]]; then
     cxx_abi="19"
-  elif [[ "$DESIRED_CUDA" != '11.8' ]]; then
+  elif [[ "$DESIRED_CUDA" != 'cu118' && "$DESIRED_CUDA" != 'xpu' ]]; then
     cxx_abi="18"
   else
     cxx_abi="16"
