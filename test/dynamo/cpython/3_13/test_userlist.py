7fb559445b92c5bedb875772e67289ab41d79f12
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-08 16:30:35 -0300

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/cpython/3_13/test_userlist.py
+++ test/dynamo/cpython/3_13/test_userlist.py
@@ -9,16 +9,10 @@ import torch
 import torch._dynamo.test_case
 import unittest
 from torch._dynamo.test_case import CPythonTestCase
-from torch.testing._internal.common_utils import (
-    TEST_WITH_TORCHDYNAMO,
-    run_tests,
-)
+from torch.testing._internal.common_utils import run_tests
 
 __TestCase = CPythonTestCase
-# if TEST_WITH_TORCHDYNAMO:
-#     __TestCase = CPythonTestCase
-# else:
-#     __TestCase = unittest.TestCase
+
 
 # redirect import statements
 import sys
