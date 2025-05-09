1cad8170f918db7ab7e8d9bdc93b3816368973f6
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-09 11:41:40 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/test_case.py
+++ torch/_dynamo/test_case.py
@@ -15,6 +15,7 @@ import importlib
 import inspect
 import logging
 import os
+import pathlib
 import re
 import sys
 import unittest
@@ -178,8 +179,9 @@ class CPythonTestCase(TestCase):
     def setUpClass(cls) -> None:
         # Skip test if python versions doesn't match
         prefix = os.path.join("dynamo", "cpython") + os.path.sep
-        regex = prefix + r"\d_\d{2}"
-        m = re.search(regex, inspect.getfile(cls))
+        regex = re.escape(prefix) + r"\d_\d{2}"
+        search_path = inspect.getfile(cls)
+        m = re.search(regex, search_path)
         if m:
             test_py_ver = tuple(map(int, m.group().removeprefix(prefix).split("_")))
             py_ver = sys.version_info[:2]
