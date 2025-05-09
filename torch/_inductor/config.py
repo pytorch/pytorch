74d03008045b888c6cb4099d2fe80ba4d2adbb7b
James Wu <jjwu@meta.com>
2025-05-09 15:29:36 +0000

    Change unsafe_marked_cacheable_functions to a dictionary, so that you can specify a static cache key (#152486)
    Fixes https://github.com/pytorch/pytorch/issues/152434
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152486
    Approved by: https://github.com/oulgen

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -145,8 +145,19 @@ force_disable_caches: bool = Config(
 # Unsafe way to skip dynamic shape guards to get faster cache load
 unsafe_skip_cache_dynamic_shape_guards: bool = False
 
-# Unsafe way to mark function as cacheable
-unsafe_marked_cacheable_functions: list[str] = []
+# Unsafe way to mark non torch functions as safe to cache
+# dictionary is from function name -> cache key
+# Any function name in the dictionary will be allowed to be cacheable
+# by AOTAutogradCache and FxGraphCache.
+# changing the cache key value will change the resulting
+# FXGraphCache key.
+# Example usage:
+# torch._inductor.config.unsafe_marked_cacheable_functions = {
+# 'torch.ops.my_function' : torch.__version__
+# }
+# The above example causes the custom op torch.ops.my_function to be cacheable,
+# and for cache keys to be keyed by the current torch version
+unsafe_marked_cacheable_functions: dict[str, str] = {}
 
 # sleep in inductor for testing
 sleep_sec_TESTING_ONLY: Optional[int] = None
