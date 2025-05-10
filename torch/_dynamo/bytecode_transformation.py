0651729890749c0ea4fd181e5994e5908b63dfc7
Animesh Jain <anijain@umich.edu>
2025-05-09 17:03:14 -0700

    Update on "[dynamo] Move propagate_line_nums in cleaned_instructions"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- torch/_dynamo/bytecode_transformation.py
+++ torch/_dynamo/bytecode_transformation.py
@@ -1423,7 +1423,6 @@ def transform_code_object(
     assert len(code_options["co_varnames"]) == code_options["co_nlocals"]
 
     instructions = cleaned_instructions(code, safe)
-    propagate_line_nums(instructions)
 
     new_instructions = transformations(instructions, code_options)
     assert new_instructions is not None
