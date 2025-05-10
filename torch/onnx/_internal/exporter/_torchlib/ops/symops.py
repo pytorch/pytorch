c4ca8c7e5e8337e4c0e1a2795454f3e6288aaada
Aaron Gokaslan <aaronGokaslan@gmail.com>
2025-05-10 10:42:13 -0400

    Add external rules to pyproject.toml

--- torch/onnx/_internal/exporter/_torchlib/ops/symops.py
+++ torch/onnx/_internal/exporter/_torchlib/ops/symops.py
@@ -2,7 +2,6 @@
 
 # mypy: disable-error-code="misc,arg-type,type-arg,valid-type,assignment,return-value,type-var,operator,no-untyped-def,index"
 # ruff: noqa: TCH001,TCH002
-# flake8: noqa
 
 from __future__ import annotations
 
