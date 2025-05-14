a480252e64fc883cc799e50e21da1d558533107a
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2025-05-14 14:04:01 +0400

    revert make_transform_iterator

--- aten/src/ATen/native/cuda/UniqueCub.cu
+++ aten/src/ATen/native/cuda/UniqueCub.cu
@@ -57,7 +57,7 @@ struct LoadBoolOp {
 auto wrap_input_iterator(const bool *data) {
   // See NOTE [Loading boolean values]
   LoadBoolOp op;
-  return thrust::make_transform_iterator(reinterpret_cast<const uint8_t*>(data), op);
+  return thrust::transform_iterator<LoadBoolOp, const uint8_t*, int>(reinterpret_cast<const uint8_t*>(data), op);
 }
 
 // A variation of compute_unique (defined in Unique.cu) that doesn't allow
@@ -261,7 +261,8 @@ struct UniqueCub<bool> {
 
     const bool* self_data = self.const_data_ptr<bool>();
     MapNumberOfTrueValues op;
-    auto data_iter = thrust::make_transform_iterator(reinterpret_cast<const uint8_t*>(self_data), op);
+    thrust::transform_iterator<MapNumberOfTrueValues, const uint8_t*, int>
+        data_iter(reinterpret_cast<const uint8_t*>(self_data), op);
     at::cuda::cub::reduce(data_iter, tmp_num_true.get(), num_inp,
                           ::cuda::std::plus<>{}, 0);
 
