7178019c6c396297d3c702efce5eefc6ff58ec76
cyy <cyyever@outlook.com>
2025-05-14 10:00:54 +0800

    Fix clang-tidy warnings

--- caffe2/serialize/read_adapter_interface.cc
+++ caffe2/serialize/read_adapter_interface.cc
@@ -1,10 +1,1 @@
 #include "caffe2/serialize/read_adapter_interface.h"
-
-namespace caffe2 {
-namespace serialize {
-
-// NOLINTNEXTLINE(modernize-use-equals-default)
-ReadAdapterInterface::~ReadAdapterInterface() {}
-
-} // namespace serialize
-} // namespace caffe2
