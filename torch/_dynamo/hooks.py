baaaacb183ca972831a5a4cc7c5043117a1d0cfb
Joel Schlosser <jbschlosser@meta.com>
2025-05-19 10:32:49 -0400

    [Dynamo] Introduce hook receiving list of traced code objects (#153622)
    This PR:
    * Expands `Hooks` with a new, optional `frame_traced_fn` field. It should be a callable receiving the list of traced code objects
    * Maintains a list of `traced_code` objects in the `TracingContext` of an `OutputGraph`
        *  Whenever an `inline_call()` is encountered, the corresponding code object is added to this set
        * `OutputGraph`'s associated `f_code` is added to the list just before the hook is called
    
    I believe use of this hook should enable the source code hashing that vLLM does in a better way than monkey-patching `inline_call()`.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/153622
    Approved by: https://github.com/jansel

--- torch/_dynamo/hooks.py
+++ torch/_dynamo/hooks.py
@@ -6,11 +6,13 @@ guard-related operations.
 The Hooks class manages two types of hook functions:
 - guard_export_fn: Called when guards need to be exported, taking a GuardsSet as input
 - guard_fail_fn: Called when a guard check fails, taking a GuardFail object as input
-
+- frame_traced_fn: Called when a frame has finished tracing, resulting in a non-empty graph.
+                   This hook will be passed the set of filenames containing traced code
 These hooks enable customization of guard export and failure handling behaviors.
 """
 
 import dataclasses
+from types import CodeType
 from typing import Callable, Optional
 
 from torch._guards import GuardsSet
@@ -23,3 +25,4 @@ class Hooks:
     guard_export_fn: Optional[Callable[[GuardsSet], None]] = None
     guard_fail_fn: Optional[Callable[[GuardFail], None]] = None
     guard_filter_fn: Optional[Callable[[list[GuardFilterEntry]], list[bool]]] = None
+    frame_traced_fn: Optional[Callable[[list[CodeType]], None]] = None
