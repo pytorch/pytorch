507e70e3406d566eb3544e2f5771e67506257622
Nikita Shulga <nshulga@meta.com>
2025-05-19 13:32:13 -0700

    Update
    [ghstack-poisoned]

--- torch/csrc/autograd/profiler_python.cpp
+++ torch/csrc/autograd/profiler_python.cpp
@@ -886,8 +886,8 @@ void PythonTracer::recordPyCall(
 #if PY_MAJOR_VERSION < 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION < 13)
       auto self = THPObjectPtr(PyDict_GetItemString(locals, "self"));
 #else
-      // In Python-3.13+ `PyFrame_GetLocals()` returns instance of PyFrameLocalsProxy_Type
-      // See PEP 667 for more info
+      // In Python-3.13+ `PyFrame_GetLocals()` returns instance of
+      // PyFrameLocalsProxy_Type See PEP 667 for more info
       auto self = THPObjectPtr(PyMapping_GetItemString(locals, "self"));
 #endif
       Py_INCREF(self.get());
