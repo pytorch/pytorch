a76b5d5769aa304e5c073f37575e8a225bbf9d17
Gabriel Ferns <gabeferns@meta.com>
2025-05-20 14:33:44 -0700

    remove breakpoint

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -1400,7 +1400,6 @@ class _InProcessFxCompile(FxCompile):
                                 compiled_module, "runner", None
                             )
 
-                    breakpoint()
                     if inductor_metrics_log.isEnabledFor(logging.INFO):
                         num_bytes, nodes_num_elem, node_runtimes = graph.count_bytes()
                         metrics.num_bytes_accessed += num_bytes
