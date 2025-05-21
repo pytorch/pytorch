cfd83eedc08b373a325728c81f9f5cd6e359fa1c
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-21 18:25:26 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/polyfills/__init__.py
+++ torch/_dynamo/polyfills/__init__.py
@@ -9,7 +9,7 @@ Python polyfills for common builtins.
 # mypy: allow-untyped-defs
 
 import types
-from collections.abc import Iterable, MutableMapping, Sequence
+from collections.abc import Hashable, Iterable, MutableMapping, Sequence
 from itertools import repeat as _repeat
 from typing import Any, Callable, TYPE_CHECKING
 
@@ -129,6 +129,13 @@ def set_intersection(set1, *others):
     if len(others) == 0:
         return set1.copy()
 
+    if not all(isinstance(s, Iterable) for s in others):
+        raise TypeError(f"set.difference expected an iterable, got {type(others)}")
+
+    for s in others:
+        if any(not isinstance(x, Hashable) for x in s):
+            raise TypeError("unhashable type")
+
     intersection_set = set()
     for x in set1:
         for set2 in others:
@@ -169,6 +176,10 @@ def set_difference(set1, *others):
 
     if not all(isinstance(s, Iterable) for s in others):
         raise TypeError(f"set.difference expected an iterable, got {type(others)}")
+
+    for s in others:
+        if any(not isinstance(x, Hashable) for x in s):
+            raise TypeError("unhashable type")
 
     difference_set = set()
     for x in set1:
