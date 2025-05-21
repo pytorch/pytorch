918ae5d36188f419a47f3b1315f9fb373035ed66
angelayi <yiangela7@gmail.com>
2025-05-21 08:34:20 -0700

    [aoti] Add MPS runner and shim (#153964)
    Added AOTIModelContainerRunnerMps and a shim for mps fallback ops.
    I also added a mps-specific shim which contains one operator, which will be used to set arguments being passed to the Metal kernel:
    
    ```
    AOTI_TORCH_EXPORT AOTITorchError aoti_torch_mps_set_arg(
        AOTIMetalKernelFunctionHandle func,
        unsigned idx,
        AtenTensorHandle tensor);
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/153964
    Approved by: https://github.com/malfet, https://github.com/desertfire

---
+++ torch/csrc/inductor/aoti_torch/shim_mps.cpp
@@ -0,0 +1,19 @@
+#include <ATen/native/mps/MetalShaderLibrary.h>
+#include <torch/csrc/inductor/aoti_torch/c/shim_mps.h>
+#include <torch/csrc/inductor/aoti_torch/utils.h>
+
+using namespace torch::aot_inductor;
+
+AOTITorchError aoti_torch_mps_set_arg(
+    AOTIMetalKernelFunctionHandle handle,
+    unsigned idx,
+    AtenTensorHandle tensor) {
+  AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({
+    auto t = tensor_handle_to_tensor_pointer(tensor);
+    if (t == nullptr) {
+      throw std::runtime_error("Tensor is null.");
+    }
+    auto func = reinterpret_cast<at::native::mps::MetalKernelFunction*>(handle);
+    func->setArg(idx, *t);
+  });
+}
