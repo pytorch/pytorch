c1055f41a67ef9e76626fa14eea38073f4a09b62
Laith Sakka <lsakka@meta.com>
2025-05-22 14:47:24 -0700

    Data dependent free reshape. (#153198)
    #### change 1: if compute_strides stride fail for reshape just clone.
    
    Lets consider the most general case, if torch compile is asked to reshape [u0, u1][u3, u4] -> [u5, u6] what shall it do?
    The shape is general enough to represent both contiguous and non contiguous tensors, tensors where a clone free reshape can happen and other where a clone free cant happen.  The current algorithm will fail due to data dependent errors.
    
    The general idea is if its impossible to tell if the reshape can happen in place, (because for some concrete inputs
    it will and other not) then its ok to take the general path and clone, instead of failing or asking the user to give hints.
    **Because the user want a single graph (single compilations)** and this is the only way it can be done.
    Had this been a view? then the user is explicitly asking for a copy-free reshape, we would fail asking for more
    information (hints in torch.checks form).
    
    with this change reshape works as the following:
    1. if we know the input is contiguous we will convert the reshape to view.
    2. if compute_strides succeed we will use view. (compute_strides  was changed to not fail when when unbacked presented instead it will just return nullptr if it cant compute the strides meaning we shall use a clone).
    3. if neither 1, 2 works clone and use a view.
    
    Side note: having a view does not mean that inductor will not clone, for inductor there is a pass that converts all views back to reshapes and inductor has its logic dealing with those.
    
    #### change 2 : skip  _reshape_view_helper and fall back to simpler logic if it fail.
    We trace _reshape_view_helper when doing fake tensor tracing , but not during proxy tracing. hence such tracing wont effect the graph (only compute output shapes of several operations). We should not fail there, because it should always be possible for us to pass it in case of reshape.
    
    i.e. when reshape_symint was called we would have either cloned, or compute_strides succeeded so the view should pass. What I did is the following: we run _reshape_view_helper, if we fail due to unbacked we call _view_simple which will succeed always for reshapes, (might fail for views when its impossible to do the view, in such case we throw the dde that was thrown by the original algorithm).
    
    Ideally I would want to register _view_simple as the meta for view and avoid calling  _reshape_view_helper completely but I am running some issues with the dispatcher with subclasses and I do not have time to debug it. Namely one test
    would end up calling some c++ view function that does not support symints during meta dispatch when i register a
    python meta decompositions
    ```python test/dynamo/test_subclasses.py SubclassTests.test_subclass_views_dynamic_True ```
     https://github.com/pytorch/pytorch/issues/153303.I will follow up with that change in a separate PR.  cc @H-Huang @awgu @wanchaol @fegin @fduwjj @wz337 @wconstab @d4l3k @voznesenskym @penguinwu @EikanWang @jgong5 @Guobing-Chen @XiaobingSuper @zhuhaozhe @blzheng @wenzhe-nrv @jiayisunx @ipiszy @chenyang78 @kadeng @muchulee8 @amjames @chauhang @aakhundov @bdhirsh
    
     Two other alternatives for registering   _view_simple as meta and the try catch approach in this PR is:
     1. call _view_simple if any input is dynamic see  #153521
     2. if we make is_compiling works for framework code tracing (does not work rn) we can call _view_simple
     is if is_compiling.
    
    #### Note:
    Reshape can still fail when is_contiguous is called, Next PR will handle that by calling is_known_contiguous.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/153198
    Approved by: https://github.com/etaf, https://github.com/bobrenjc93

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -1345,9 +1345,7 @@ def sym_eq(x: _T, y: _T) -> BoolLikeType:
     Like ==, but when run on list/tuple, it will recursively test equality
     and use sym_and to join the results together, without guarding.
     """
-    if (isinstance(x, tuple) and isinstance(y, tuple)) or (
-        isinstance(x, list) and isinstance(y, list)
-    ):
+    if isinstance(x, (tuple, list)) and isinstance(y, (list, tuple)):
         if len(x) != len(y):
             return False
         return functools.reduce(operator.and_, map(sym_eq, x, y), True)
