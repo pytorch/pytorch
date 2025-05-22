1a427b6ca48bd894cf9aac594953c8e9ab30ceaf
Xuanteng Huang <xuanteng.huang@outlook.com>
2025-05-22 14:13:42 +0800

    relax orig_dtype and reduce_dtype consistency check on no-grad params

--- torch/distributed/fsdp/_fully_shard/_fsdp_param_group.py
+++ torch/distributed/fsdp/_fully_shard/_fsdp_param_group.py
@@ -3,6 +3,8 @@ import contextlib
 import logging
 from typing import Any, Callable, cast, NamedTuple, Optional
 
+from networkx.convert_matrix import from_scipy_sparse_array
+
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -212,14 +214,15 @@ class FSDPParamGroup:
     def _init_mp_dtypes(self) -> None:
         for fsdp_param in self.fsdp_params:
             fsdp_param.init_dtype_attrs(self.mp_policy)
-        orig_dtypes = {fsdp_param.orig_dtype for fsdp_param in self.fsdp_params}
+        grad_params: list[FSDPParam] = [p for p in self.fsdp_params if p.sharded_param.requires_grad]
+        orig_dtypes = {p.orig_dtype for p in grad_params}
+        reduce_dtypes = {p.reduce_dtype for p in grad_params}
         if len(orig_dtypes) != 1:
             # This can be relaxed if we copy-out for the reduce-scatter
             raise AssertionError(
                 f"FSDP expects uniform original parameter dtype but got {orig_dtypes}"
             )
         self._orig_dtype = next(iter(orig_dtypes))
-        reduce_dtypes = {fsdp_param.reduce_dtype for fsdp_param in self.fsdp_params}
         if len(reduce_dtypes) != 1:
             # This can be relaxed if we issue one reduce-scatter per reduce
             # dtype (but we would need a way for users to specify multiple
