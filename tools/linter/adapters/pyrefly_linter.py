d0ec48d168f56ecfd50eda13279afa2ce1f606ec
Tom Ritchford <tom@swirly.com>
2025-05-23 13:20:14 +0000

    Update
    [ghstack-poisoned]

--- tools/linter/adapters/pyrefly_linter.py
+++ tools/linter/adapters/pyrefly_linter.py
@@ -1,11 +1,13 @@
 import json
 import logging
+import sys
 import subprocess
 import time
 import traceback
 from argparse import ArgumentParser, Namespace
 from collections.abc import Iterator
 from dataclasses import asdict, dataclass
+from functools import partial
 from pathlib import Path
 from typing import Optional
 
@@ -14,6 +16,11 @@ SEVERITY = "error"
 LINTER_CODE = "PYREFLY"
 LOG_FORMAT = "<%(threadName)s:%(levelname)s> %(message)s"
 IGNORE_FILENAMES = True
+REPORT_ERRORS = False
+
+_fp = open("logfile.txt", "w")
+_print = partial(print, file=_fp)
+_print(f"{sys.argv=}")
 
 
 def main() -> None:
@@ -39,6 +46,8 @@ def main() -> None:
     filenames = [] if IGNORE_FILENAMES else list(filedict)
     for msg in lint_files(filenames, args):
         print(json.dumps(asdict(msg)))
+        if not True:
+            break
 
 
 @dataclass
@@ -59,25 +68,37 @@ class LintMessage:
     replacement: Optional[str] = None
 
     @staticmethod
-    def make(source_line: str) -> "LintMessage":
+    def make(source_line: str) -> Optional["LintMessage"]:
         # e.g. tools/linter.py:15:9-20: error: Incompatibl...int")  [assignment]
+        path = ""
         try:
             path, line, columns, rest = source_line.split(":", maxsplit=3)
-            char, _ = columns.split("-")
-            description, _, tail = rest.strip().rpartition("[")
-            name, _, tail = tail.rpartition("]")
-            if path and line and columns and char and description and not tail:
-                return LintMessage(
-                    path=path,
-                    line=int(line),
-                    char=int(char),
-                    name=name,
-                    description=description,
-                )
-            error = f"Couldn't parse: {locals()}"
         except Exception:
-            error = f"{source_line=}\n{traceback.format_exc()}"
-        return LintMessage(name="bad-error-line", description=error)
+            pass
+
+        if not ((path.endswith(".py") or path.endswith(".pyi")) and "/" in path):
+            if REPORT_ERRORS:
+                return LintMessage(name="bad-error-line", description=source_line)
+            return None
+
+        try:
+            line = int(line)
+        except Exception:
+            line = None
+        try:
+            char = int(columns.partition("-")[0])
+        except Exception:
+            char = None
+
+        description, _, tail = rest.strip().rpartition("[")
+        name = tail.rpartition("]")[0] or "bad-error-line"
+        return LintMessage(
+            path=path,
+            line=line,
+            char=char,
+            name=name,
+            description=description or source_line,
+        )
 
 
 def run(*args: str) -> str:
@@ -90,6 +111,7 @@ def run(*args: str) -> str:
 
 
 def lint_files(files: list[str], args: Namespace) -> Iterator[LintMessage]:
+    bad_lines = 0
     try:
         try:
             run("pyrefly", "--help")
@@ -97,25 +119,35 @@ def lint_files(files: list[str], args: Namespace) -> Iterator[LintMessage]:
             yield LintMessage(
                 name="command-failed", description="pyrefly does not exist"
             )
+            return
+
         try:
-            cmd = (
-                "pyrefly",
-                "check",
-                "--config",
-                args.config,
-                *args.other_commands,
-                *files,
-            )
-            run(*cmd)
-        except subprocess.CalledProcessError as e:
-            if e.returncode == 1:
-                yield from (LintMessage.make(line) for line in e.stdout.splitlines())
-            else:
-                description = f"Could not run '{' '.join(cmd)}': {e.stderr} {e.stdout}"
-                yield LintMessage(name="command-failed", description=description)
+            cmd = "pyrefly", "check", "--config"
+            argv = args.config, *args.other_commands, *files
+            run(*cmd, *argv)
+            return
+
+        except subprocess.CalledProcessError as err:
+            e = err
+
+        if e.returncode == 1:
+            for line in e.stdout.splitlines():
+                if m := LintMessage.make(line):
+                    yield m
+                else:
+                    bad_lines += 1
+        else:
+            description = f"Could not run '{' '.join(cmd)}': {e.stderr} {e.stdout}"
+            yield LintMessage(name="command-failed", description=description)
 
     except Exception:
         yield LintMessage(name="traceback", description=traceback.format_exc())
+
+    if bad_lines:
+        description = (
+            f"Number of pyrefly error lines which could not be parsed: {bad_lines}"
+        )
+        yield LintMessage(name="bad_lines", description=description)
 
 
 def parse_args() -> Namespace:
