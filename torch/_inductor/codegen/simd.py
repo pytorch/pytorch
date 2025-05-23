b3018a807528f86153daf238451971a8e9e4f46b
eellison <elias.ellison@gmail.com>
2025-05-23 07:22:03 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/codegen/simd.py
+++ torch/_inductor/codegen/simd.py
@@ -2269,10 +2269,12 @@ class SIMDScheduling(BaseScheduling):
             out = cls.compute_tiling_strategy(
                 node_schedule, numel, reduction_numel, coalesce_analysis
             )
-            out2 = cls.get_tiling_and_scores(
-                node_schedule, numel, reduction_numel, None
-            )
-            assert out[0] == out2[0]
+            # out2 = cls.get_tiling_and_scores(
+            #     node_schedule, numel, reduction_numel, None
+            # )
+            # if not out[0] == out2[0]:
+            #     breakpoint()
+            # assert out[0] == out2[0]
             return out
                 
         if (
