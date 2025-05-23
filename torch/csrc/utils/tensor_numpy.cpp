bddacaf3e91e386594515020c73c5b98ab1d67de
Yuanhao Ji <jiyuanhao@apache.org>
2025-05-23 01:33:54 +0000

    export tensor_to_numpy

--- torch/csrc/utils/tensor_numpy.cpp
+++ torch/csrc/utils/tensor_numpy.cpp
@@ -120,7 +120,7 @@ static std::vector<int64_t> seq_to_aten_shape(PyObject* py_seq) {
   return result;
 }
 
-PyObject* tensor_to_numpy(const at::Tensor& tensor, bool force /*=false*/) {
+TORCH_API PyObject* tensor_to_numpy(const at::Tensor& tensor, bool force /*=false*/) {
   TORCH_CHECK(is_numpy_available(), "Numpy is not available");
 
   TORCH_CHECK(
