113e4059f9e4dba04ca1ca06168608e097f0c672
Yu, Guangye <guangye.yu@intel.com>
2025-05-23 19:17:52 +0000

    Update
    [ghstack-poisoned]

--- torch/csrc/DeviceAccelerator.cpp
+++ torch/csrc/DeviceAccelerator.cpp
@@ -72,6 +72,57 @@ void initModule(PyObject* module) {
     torch::utils::maybe_initialize_device(device_type);
     return at::accelerator::maybeExchangeDevice(device_index);
   });
+
+  m.def("_accelerator_allocaterInitialized", []() {
+    const auto device_type = at::accelerator::getAccelerator(true).value();
+    return at::GetDeviceAllocator(device_type)->initialized();
+  });
+
+  m.def("_accelerator_emptyCache", []() {at::accelerator::emptyCache();});
+
+  m.def("_accelerator_getDeviceStats", [](c10::DeviceIndex device_index) {
+    using c10::CachingAllocator::Stat;
+    using c10::CachingAllocator::StatArray;
+    using c10::CachingAllocator::StatType;
+    using c10::CachingDeviceAllocator::DeviceStats;
+
+    const auto stats = at::accelerator::getDeviceStats(device_index);
+
+    static constexpr std::
+        array<const char*, static_cast<size_t>(StatType::NUM_TYPES)>
+            kStatTypeNames = {"all", "small_pool", "large_pool"};
+
+    const auto stat_to_dict = [](const Stat& stat) -> py::dict {
+      return py::dict(
+          {"current", stat.current},
+          {"peak", stat.peak},
+          {"allocated", stat.allocated},
+          {"freed", stat.freed});
+    };
+
+    const auto stat_array_to_dict = [&](const StatArray& stats) -> py::dict {
+      py::dict dict;
+      for (const auto i : c10::irange(kStatTypeNames.size())) {
+        dict[kStatTypeNames[i]] = stat_to_dict(stats[i]);
+      }
+      return dict;
+    };
+
+    return py::dict{
+        {"allocated_bytes", stat_array_to_dict(stats.allocated_bytes)},
+        {"reserved_bytes", stat_array_to_dict(stats.reserved_bytes)},
+        {"active_bytes", stat_array_to_dict(stats.active_bytes)},
+        {"requested_bytes", stat_array_to_dict(stats.requested_bytes)}};
+  });
+
+  m.def(
+      "_accelerator_resetAccumulatedStats", [](c10::DeviceIndex device_index) {
+        at::accelerator::resetAccumulatedStats(device_index);
+      });
+
+  m.def("_accelerator_resetPeakStats", [](c10::DeviceIndex device_index) {
+    at::accelerator::resetPeakStats(device_index);
+  });
 }
 
 } // namespace torch::accelerator
