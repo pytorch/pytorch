4cb0843134cb2ff81a28a9c736b041daa7d94300
Jing Xu <jing.xu@intel.com>
2025-05-23 21:32:13 +0900

    replace the C10_STRINGIZE with std::string constructor to fix the issue

--- torch/csrc/xpu/Module.cpp
+++ torch/csrc/xpu/Module.cpp
@@ -18,11 +18,8 @@ using namespace torch;
 static PyObject* THXPModule_getArchFlags(PyObject* self, PyObject* noargs) {
   HANDLE_TH_ERRORS
 #ifdef XPU_ARCH_FLAGS
-  static const char* flags = C10_STRINGIZE(XPU_ARCH_FLAGS);
-  // Since C10_STRINGIZE doesn't handle an empty string as its input correctly,
-  // the string "\"\"" will be passed from cmake to indicate an empty string.
-  static const char* EMPTY_FLAGS = "\"\"";
-  return THPUtils_packString(std::strcmp(flags, EMPTY_FLAGS) == 0 ? "" : flags);
+  static const char* flags = std::string(XPU_ARCH_FLAGS).c_str();
+  return THPUtils_packString(flags);
 #else
   Py_RETURN_NONE;
 #endif
