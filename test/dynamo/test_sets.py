90ce815090b878f38abe277ead7b54b4153b8d0e
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-26 16:11:57 -0300

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/test_sets.py
+++ test/dynamo/test_sets.py
@@ -5,8 +5,8 @@
 import unittest
 
 import torch
-import torch._dynamo.config
 import torch._dynamo.test_case
+from torch._dynamo.testing import CompileCounter
 from torch.testing._internal.common_utils import make_dynamo_test
 
 
@@ -54,7 +54,7 @@ class SetGuardsSet(torch._dynamo.test_case.TestCase):
             torch.amp._exit_autocast,
         }
 
-        cnts = torch._dynamo.testing.CompileCounter()
+        cnts = CompileCounter()
 
         def fn(x, s):
             if torch.amp._exit_autocast in s:
@@ -84,7 +84,7 @@ class SetGuardsSet(torch._dynamo.test_case.TestCase):
             torch.amp._exit_autocast,
         }
 
-        cnts = torch._dynamo.testing.CompileCounter()
+        cnts = CompileCounter()
 
         def fn(x, s):
             if torch.amp._exit_autocast in s:
@@ -118,7 +118,7 @@ class SetGuardsSet(torch._dynamo.test_case.TestCase):
             torch.amp._exit_autocast,
         }
 
-        cnts = torch._dynamo.testing.CompileCounter()
+        cnts = CompileCounter()
 
         def fn(x, s):
             for e in s:
