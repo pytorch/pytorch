aea1745085cc84c5ab6ce868dc569420c35606d2
uu4f5 <uu2fu3o@gmail.com>
2025-05-29 00:24:02 +0800

    Update docathon-label-sync.py

--- .github/scripts/docathon-label-sync.py
+++ .github/scripts/docathon-label-sync.py
@@ -4,7 +4,6 @@ import sys
 import subprocess
 from github import Github
 
-def test()
 def get_token():
     my_secret = os.environ.get("GITHUB_TOKEN", None)
     if my_secret is None:
@@ -26,7 +25,6 @@ def get_token():
         print("Error:", e.stderr)
 
 def main() -> None:
-    test()
     get_token()
     token = os.environ.get("GITHUB_TOKEN")
 
