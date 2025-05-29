3aa8e3f0b6a01129449c89c66a773ff26adc0a2d
Laith Sakka <lsakka@meta.com>
2025-05-28 21:12:55 -0700

    Update on "check fallback_value first. "
    This is just a refactor, not a fix for any issue.
    we do check fallback_value first  and early exit instead of checking it not set over and over.
    
    cc ezyang SherlockNoMad EikanWang jgong5 wenzhe-nrv
    
    [ghstack-poisoned]

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -7126,7 +7126,7 @@ class ShapeEnv:
                 if not (new_expr.free_symbols <= self.var_to_val.keys()):
                     ok = False
 
-                    # fallback_value is set when guard_or_true or gaurd_or_false are used.
+                    # fallback_value is set when guard_or_true or guard_or_false are used.
                     if not ok and fallback_value is not None:
                         self._log_suppressed_dde(orig_expr, fallback_value)
                         return fallback_value
