cc661c7de78ecd1052d54243939ae2acb47073b8
Kshitij Khode <kshitij.khode@intel.com>
2025-05-28 23:50:02 -0700

    Allow exporter to export SDPA to Attention onnx operator

--- torch/onnx/_internal/exporter/_torchlib/ops/nn.py
+++ torch/onnx/_internal/exporter/_torchlib/ops/nn.py
@@ -7,7 +7,8 @@ from __future__ import annotations
 
 from typing import Optional
 
-from onnxscript.onnx_opset import opset20 as op20, opset21 as op21
+import onnxscript.function_libs.torch_lib.ops.nn as nn
+from onnxscript.onnx_opset import opset18 as op18, opset20 as op20, opset21 as op21, opset23 as op23
 
 import torch
 from torch.onnx._internal._lazy_import import onnxscript_ir as ir
@@ -45,4 +46,69 @@ def aten_group_norm(
         bias = op21.ConstantOfShape(c, value=ir.tensor(0.0, dtype=input.dtype))
     return op21.GroupNormalization(
         input, weight, bias, epsilon=eps, num_groups=num_groups
+    )
+
+@onnx_impl(aten.scaled_dot_product_attention.default, trace_only=True, opset_introduced=23)
+def scaled_dot_product_attention(
+    query: TFloat,
+    key: TFloat,
+    value: TFloat,
+    attn_mask: Optional[TFloat] = None,
+    dropout_p: float = 0.0,
+    is_causal: bool = False,
+    scale: Optional[float] = None,
+    enable_gqa: bool = False,
+) -> TFloat:
+    """scaled_dot_product_attention(Tensor query, Tensor key, Tensor value, Tensor? attn_mask=None, float dropout_p=0.0, bool is_causal=False, *, float? scale=None, bool enable_gqa=False) -> Tensor
+
+    Reference:
+        1. https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
+        2. https://onnx.ai/onnx/operators/onnx__Attention.html
+
+    Attempts to convert SDPA to Attention onnx op and fallbacks to an onnx graph equivivalent to the following PyTorch code::
+        scale_factor = 1 / math.sqrt(Q.size(-1)) if scale is None else scale
+        attn_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0) if is_causal else attn_mask
+        attn_mask = attn_mask.masked_fill(not attn_mask, -float('inf')) if attn_mask.dtype==torch.bool else attn_mask
+        attn_weight = torch.softmax((Q @ K.transpose(-2, -1) * scale_factor) + attn_mask, dim=-1)
+        attn_weight = torch.dropout(attn_weight, dropout_p)
+        return attn_weight @ V
+
+    where Q, K, V are the query, key, and value tensors, respectively.
+    L is the target sequence length, S is the source sequence length, and E is the embedding size.
+    """
+    # Use trace_only to handle optional inputs
+    assert (not is_causal) or (is_causal and attn_mask is None), (
+        "is_causal and attn_mask cannot be set at the same time"
+    )
+
+    # Attention onnx op can only handle non-training scenarios where dropout is disabled.
+    if dropout_p == 0:
+        if enable_gqa:
+            assert (query.shape[1] > (key.shape[1] == value.shape[1]) and query.shape[1] % key.shape[1] == 0), (
+                "SDPA (GQA or MQA) requires q_num_heads > kv_num_heads & q_num_heads % kv_num_heads == 0"
+            )
+        else:
+            assert (query.shape[1] == key.shape[1] == value.shape[1]), (
+                "SDPA (MHA) requires q_num_heads = kv_num_heads"
+            )
+        Y, _, _, _ = op23.Attention(
+            query, key, value, attn_mask=attn_mask, scale=scale, q_num_heads=query.shape[3], kv_num_heads=key.shape[3],
+            is_causal=(1 if is_causal == True else 0)
+        )
+        return Y
+
+    if scale is None:
+        scale = nn._attention_scale(query)
+    scale = op18.CastLike(scale, query)
+
+    if is_causal:
+        attn_mask = nn._causal_attention_mask(query, key)
+
+    if attn_mask is None:
+        return nn._aten_scaled_dot_product_attention_no_mask_onnx(
+            query, key, value, scale, dropout_p
+        )
+
+    return nn._aten_scaled_dot_product_attention_float_mask_onnx(
+        query, key, value, attn_mask, scale, dropout_p
     )
