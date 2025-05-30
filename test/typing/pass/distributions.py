e9d8d3b545e41afe7523dba2e58a572dea869015
Randolf Scholz <randolf.scholz@gmail.com>
2025-05-30 20:04:42 +0200

    added static test for dependent property

--- test/typing/pass/distributions.py
+++ test/typing/pass/distributions.py
@@ -2,6 +2,8 @@ from typing_extensions import assert_type
 
 import torch
 from torch import distributions, Tensor
+from torch.distributions import Pareto
+from torch.distributions.constraints import Constraint, GreaterThanEq
 
 
 dist = distributions.Normal(0, 1)
@@ -9,3 +11,11 @@ assert_type(dist.mean, Tensor)
 
 dist = distributions.MultivariateNormal(torch.zeros(2), torch.eye(2))
 assert_type(dist.covariance_matrix, Tensor)
+
+
+def _check_transformed_distributions(td: distributions.TransformedDistribution) -> None:
+    assert_type(td.support, Constraint)
+
+
+def _check_pareto_distribution(p: Pareto) -> None:
+    assert_type(p.support, GreaterThanEq)
