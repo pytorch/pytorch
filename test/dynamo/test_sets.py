5e86242e5aaa920e9912b8713b95962a74a4c605
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-29 23:43:33 -0300

    [Dynamo] [Set] Implement some binop operators for dict/set/frozenset/dict_keys
    ghstack-source-id: 5c19ad1a2068d2807be56e223c49fe1be59d2ac3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/154063

--- test/dynamo/test_sets.py
+++ test/dynamo/test_sets.py
@@ -11,6 +11,10 @@ from torch._dynamo.testing import CompileCounter
 from torch.testing._internal.common_utils import make_dynamo_test
 
 
+class SetSubclass(set):
+    pass
+
+
 class _BaseSetTests(torch._dynamo.test_case.TestCase):
     def setUp(self):
         self.old = torch._dynamo.config.enable_trace_unittest
@@ -23,6 +27,9 @@ class _BaseSetTests(torch._dynamo.test_case.TestCase):
 
     def assertEqual(self, a, b):
         return self.assertTrue(a == b, f"{a} != {b}")
+
+    def assertNotEqual(self, a, b):
+        return self.assertTrue(a != b, f"{a} == {b}")
 
 
 class CustomSetTests(_BaseSetTests):
@@ -198,7 +205,7 @@ class _FrozensetBase:
     # + symmetric_difference
     # + union
     # BinOps:
-    # +, -, |, &, ^, <, >, <=, >=
+    # +, -, |, &, ^, <, >, <=, >=, ==, !=
 
     @make_dynamo_test
     def test_binop_sub(self):
@@ -225,6 +232,23 @@ class _FrozensetBase:
         p, q = map(self.thetype, ["abc", "bef"])
         self.assertEqual(p ^ p, self.thetype())
         self.assertEqual(p ^ q, self.thetype("acef"))
+
+    @make_dynamo_test
+    def test_cmp_eq(self):
+        p = self.thetype("abc")
+        self.assertEqual(p, p)
+        for C in set, frozenset, SetSubclass:
+            self.assertEqual(p, C("abc"))
+            self.assertEqual(p, C(p))
+
+    @make_dynamo_test
+    def test_cmp_ne(self):
+        p, q = map(self.thetype, ["abc", "bef"])
+        self.assertNotEqual(p, q)
+        self.assertNotEqual(q, p)
+        for C in set, frozenset, SetSubclass, dict.fromkeys, str, list, tuple:
+            self.assertNotEqual(p, C("abe"))
+        self.assertNotEqual(p, 1)
 
     @make_dynamo_test
     def test_cmp_less_than(self):
@@ -480,8 +504,8 @@ class UserDefinedSetTests(_SetBase, _BaseSetTests):
         super().test_cmp_less_than_or_equal()
 
     @unittest.expectedFailure
-    def test_binop_sub(self):
-        super().test_binop_sub()
+    def test_binop_or(self):
+        super().test_binop_or()
 
     @unittest.expectedFailure
     def test_in_frozenset(self):
