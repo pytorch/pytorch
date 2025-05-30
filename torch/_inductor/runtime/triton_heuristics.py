a97bdce09720aad594e1a6942f880a603665ccc2
Mu-Chu Lee <mlee8@meta.com>
2025-05-29 17:46:08 -0700

    [Inductor] Include math and torch in launcher scope (#154673)
    Summary:
    
    For grid computation, if we have sympy, it is possible we have math and torch used.
    We include the math and torch module in the launcher scope to make sure those grid get computed correctly.
    
    Test Plan:
    ```
    buck2 run mode/opt  mode/inplace -c fbcode.platform010_cuda_version=12 -c fbcode.nvcc_arch=h100 caffe2/torch/fb/model_transform/experimental/benchmark:mts_gpu_benchmark -- --lower-backend=AOT_INDUCTOR   --model-path=manifold://ads_storage_fblearner/tree/user/facebook/fblearner/predictor/739097956/0/gpu_lowering/input.predictor.disagg.gpu.merge --trace-aot-inductor-module=True --disable-acc-tracer=False --batch-size=2048 --node_replacement_dict "{'torch.nn.Linear':{'(4000+,1000+)':'fp8_float_model_dynamic_quantization_rowwise','(0+,2000+)':'fp8_float_model_dynamic_quantization_rowwise'}}" --use_dynamic_dim_hints=True --allow_refine_dynamic_shapes_on_constants=True
    ```
    
    Differential Revision: D75642931

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -1541,6 +1541,10 @@ class TritonCompileResult(CompileResult[CompiledKernel]):
             launch_enter = knobs.runtime.launch_enter_hook
             launch_exit = knobs.runtime.launch_exit_hook
 
+        import math as math_lib
+
+        import torch as torch_lib
+
         scope = {
             "grid_meta": cfg.kwargs,
             "bin": binary,
@@ -1571,6 +1575,8 @@ class TritonCompileResult(CompileResult[CompiledKernel]):
             ),
             "function": get_first_attr(binary, "function", "cu_function"),
             "runner": get_first_attr(binary, "run", "c_wrapper"),
+            "math": math_lib,
+            "torch": torch_lib,
         }
 
         if not hasattr(binary, "launch_metadata"):
