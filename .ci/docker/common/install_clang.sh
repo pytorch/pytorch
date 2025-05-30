b284dbf0e306821553680dd62c290b80f1c22bee
Huy Do <huydhn@gmail.com>
2025-05-29 18:27:22 -0700

    Spurious commit
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .ci/docker/common/install_clang.sh
+++ .ci/docker/common/install_clang.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 
 set -ex
 
-install_ubuntu() {
-  apt-get update
+if [ -n "$CLANG_VERSION" ]; then
 
-  apt-get install -y --no-install-recommends clang-"$CLANG_VERSION"
-  apt-get install -y --no-install-recommends llvm-"$CLANG_VERSION"
-  # Also require LLD linker from llvm and libomp to build PyTorch from source
-  apt-get install -y lld "libomp-${CLANG_VERSION}-dev" "libc++-${CLANG_VERSION}-dev"
+  if [[ $UBUNTU_VERSION == 22.04 ]]; then
+    # work around ubuntu apt-get conflicts
+    sudo apt-get -y -f install
+    wget --no-check-certificate -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    if [[ $CLANG_VERSION == 18 ]]; then
+      apt-add-repository "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-18 main"
+    fi
+  fi
+
+  sudo apt-get update
+  if [[ $CLANG_VERSION -ge 18 ]]; then
+    apt-get install -y libomp-${CLANG_VERSION}-dev libclang-rt-${CLANG_VERSION}-dev clang-"$CLANG_VERSION" llvm-"$CLANG_VERSION"
+  else
+    apt-get install -y --no-install-recommends clang-"$CLANG_VERSION" llvm-"$CLANG_VERSION"
+  fi
+
+  # Install dev version of LLVM.
+  if [ -n "$LLVMDEV" ]; then
+    sudo apt-get install -y --no-install-recommends llvm-"$CLANG_VERSION"-dev
+  fi
 
   # Use update-alternatives to make this version the default
   update-alternatives --install /usr/bin/clang clang /usr/bin/clang-"$CLANG_VERSION" 50
@@ -22,21 +32,14 @@ install_ubuntu() {
   update-alternatives --install /usr/bin/cc cc /usr/bin/clang 50
   update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 50
 
+  # clang's packaging is a little messed up (the runtime libs aren't
+  # added into the linker path), so give it a little help
+  clang_lib=("/usr/lib/llvm-$CLANG_VERSION/lib/clang/"*"/lib/linux")
+  echo "$clang_lib" >/etc/ld.so.conf.d/clang.conf
+  ldconfig
+
   # Cleanup package manager
   apt-get autoclean && apt-get clean
   rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-}
 
-if [ -n "$CLANG_VERSION" ]; then
-  # Install base packages depending on the base OS
-  ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
-  case "$ID" in
-    ubuntu)
-      install_ubuntu
-      ;;
-    *)
-      echo "Unable to determine OS..."
-      exit 1
-      ;;
-  esac
 fi
