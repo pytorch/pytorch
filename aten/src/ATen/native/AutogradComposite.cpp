f7680b3b7b4f430c9435820db0d84787b5263db8
Kurt Mohler <kmohler@quansight.com>
2025-05-30 20:40:45 -0500

    Enable lazy cloning in `Tensor.to` between CPU and MPS
    ghstack-source-id: 28b05a761a548005aa940f49b84b4e38da9a0b9a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/150569

--- aten/src/ATen/native/AutogradComposite.cpp
+++ aten/src/ATen/native/AutogradComposite.cpp
@@ -11,6 +11,7 @@
 #include <ATen/ops/_has_same_storage_numel_native.h>
 #include <ATen/ops/_make_dual_native.h>
 #include <ATen/ops/_new_zeros_with_same_feature_meta_native.h>
+#include <ATen/ops/zeros_like.h>
 #include <ATen/ops/_unpack_dual_native.h>
 #include <ATen/ops/_lazy_clone_native.h>
 #include <ATen/ops/alias.h>
@@ -96,11 +97,15 @@ bool _has_same_storage_numel(const at::Tensor& base, const at::Tensor& other) {
 Tensor _lazy_clone(Tensor const& self, std::optional<c10::Device> device_opt) {
   c10::StorageImpl* self_storage = self.storage().unsafeGetStorageImpl();
   c10::intrusive_ptr<c10::StorageImpl> storage = nullptr;
+  c10::Device src_device = self.device();
+  c10::DeviceType src_device_type = src_device.type();
+
+  if (src_device_type == c10::kMeta) {
+    return zeros_like(self, self.options());
+  }
 
   if (device_opt.has_value()) {
-    c10::Device src_device = self.device();
     c10::Device dst_device = device_opt.value();
-    c10::DeviceType src_device_type = src_device.type();
     c10::DeviceType dst_device_type = dst_device.type();
     TORCH_CHECK(
       (src_device_type == dst_device_type)
