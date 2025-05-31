4c9b38ed8bf0d9b9f5719c47334924a0e9c04d1e
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-05-31 21:59:59 +0800

    [pytree] add another simplified pytree module `torch.pytree`
    Differences between `torch.pytree` and `torch.utils.pytree`:
    
    1. APIs in `torch.utils.pytree` have a `tree_` prefix:
    
        ```python
        leaves, treespec = torch.utils.pytree.tree_flatten(tree)
        new_tree = torch.utils.pytree.tree_map(func, tree)
    
        leaevs, treespec = torch.pytree.flatten(tree)
        new_tree = torch.pytree.map(func, tree)
        ```
    
    2. The argument order of `unflatten` is reversed for better `functools.partial` support:
    
        ```python
        tree = torch.utils.pytree.tree_unflatten(leaves, treespec)
    
        tree = torch.pytree.unflatten(treespec, leaves)
    
        unflatten_fn = functools.partial(torch.pytree.unflatten, treespec)
        tree1 = unflatten_fn(leaves1)
        tree2 = unflatten_fn(leaves2)
        ```
    
        This is also aligned with the JAX pytree API: `jax.tree.unflatten(treedef, leaves)`.
    
    ghstack-source-id: c42a1e5f9a8896cae7d53cd61597cf5402d7da9e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148180

--- torch/__init__.py
+++ torch/__init__.py
@@ -2697,6 +2697,7 @@ if TYPE_CHECKING:
         _inductor as _inductor,
         _subclasses as _subclasses,
         onnx as onnx,
+        pytree as pytree,
     )
 
 else:
@@ -2706,6 +2707,7 @@ else:
         "_export",
         # ONNX must be imported after _dynamo, _ops, _subclasses, fx, func and jit
         "onnx",
+        "pytree",
     }
 
     def __getattr__(name):
