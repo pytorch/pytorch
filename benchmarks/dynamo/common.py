c4aea3418250fa6dd6f2fe283cec0234a81de7f9
bobrenjc93 <bobren@meta.com>
2025-06-01 08:59:48 -0700

    Update
    [ghstack-poisoned]

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import random
 import argparse
 import collections
 import contextlib
@@ -15,6 +14,7 @@ import itertools
 import json
 import logging
 import os
+import random
 import shutil
 import signal
 import subprocess
