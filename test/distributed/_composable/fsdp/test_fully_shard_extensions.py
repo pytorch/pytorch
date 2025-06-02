b299aba43a3f746a84ac1af6a11c08d747c38db5
Wei Feng <weif@meta.com>
2025-06-01 21:03:51 -0700

    Update on "[FSDP2] respect reshard_after_forward=True for root model"
    resolve https://github.com/pytorch/pytorch/issues/154655
    
    
    `fully_shard(root, reshard_after_forward=True)` didn't really reshard parameters after forward, because we assumed root model will be used in backward immeidately. The assumption becomes invalid in 2 cases
    * we have 3 roots for CLIP, T5, FLUX. we should reshard parameters are CLIP and T5 immeidately after their forward
    for recommendation model, we may have mutiple root for dense part
    
    Change default beahvior to always respect `reshard_after_forward=True`
    
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k
    
    Differential Revision: [D75663200](https://our.internmc.facebook.com/intern/diff/D75663200)
    
    [ghstack-poisoned]

--- test/distributed/_composable/fsdp/test_fully_shard_extensions.py
+++ test/distributed/_composable/fsdp/test_fully_shard_extensions.py
@@ -394,7 +394,7 @@ class TestFullyShardAllGatherExtensionsMultiThread(
                 if "weight" in param_name:
                     param = nn.Parameter(BFloat16AllGatherTensor(param))
                     setattr(module, param_name, param)
-        fully_shard(model)
+        fully_shard(model, reshard_after_forward=False)
         optim = torch.optim.AdamW(model.parameters(), lr=1e-2, fused=True)
         torch.manual_seed(42 + self.rank + 1)
         inp = torch.randn((2, 3), device=device_type)
