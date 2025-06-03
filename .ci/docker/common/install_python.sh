3c47a497544da07ca7fe0b92c195211b0edbe1fb
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2025-06-03 15:14:53 -0500

    Typo

--- .ci/docker/common/install_python.sh
+++ .ci/docker/common/install_python.sh
@@ -6,7 +6,7 @@ apt-get update
 sudo add-apt-repository ppa:deadsnakes/ppa
 apt-get install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip python${PYTHON_VERSION}-venv
 
-# Use a venv because uv and some other package managers don't support install
+# Use a venv because uv and some other package managers don't support --user install
 ln -s /usr/bin/python${PYTHON_VERSION} /usr/bin/python
 python -m venv /var/lib/jenkins/ci_env
 source /var/lib/jenkins/ci_env/bin/activate
