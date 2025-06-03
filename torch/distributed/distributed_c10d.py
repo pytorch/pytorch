8b72f5ee1f0cad53cd57b8be491e7d314d355007
Han, Chao1 <chao1.han@intel.com>
2025-06-03 18:07:48 +0800

    Build XCCL as default and make XCCL the default distributed backend for XPU

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -340,7 +340,7 @@ class Backend(str):  # noqa: SLOT000
 
         if devices is not None:
             for device in devices:
-                if device != "cpu" and device != "cuda":
+                if device != "cpu" and device != "cuda" and device != "xpu":
                     Backend.default_device_backend_map[device] = name.lower()
         Backend.backend_type_map[name.lower()] = ProcessGroup.BackendType.CUSTOM
 
