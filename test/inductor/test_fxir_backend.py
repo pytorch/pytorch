921c26f86a045659710a77e509d77c962ccd49d9
Blaine Burton Rister <145300525+blaine-rister@users.noreply.github.com>
2025-06-02 21:01:21 -0700

    bug fixes and tests

--- test/inductor/test_fxir_backend.py
+++ test/inductor/test_fxir_backend.py
@@ -182,6 +182,33 @@ class FxirTestCase(InductorTestCase):
         args = [torch.randn(8, device=self.device) for _ in range(2)]
         self._compile_and_check(foo, args, expected_num_triton_kernels=1)
 
+    def test_cat_views(self):
+        """
+        Test concatenation with multiple kernels writing to the same buffer.
+        """
+
+        def foo(x, y):
+            a = x - 2
+            b = y.sum(0, keepdim=True)
+            c = torch.cat((a, b)).clone()
+            return a, b, c
+
+        args = [torch.randn(8, device=self.device) for _ in range(2)]
+        (gm,) = self._compile_and_check(foo, args, expected_num_triton_kernels=2)
+
+        def get_offset(node: torch.fx.Node) -> int:
+            (input_, shape, stride, offset) = node.args
+            assert isinstance(offset, int)
+            return offset
+
+        # Check for 2 views, one of which is offset.
+        as_strided_nodes = list(
+            gm.graph.find_nodes(op="call_function", target=torch.as_strided)
+        )
+        self.assertEqual(len(as_strided_nodes), 2)
+        num_offset_views = sum(get_offset(node) > 0 for node in as_strided_nodes)
+        self.assertEqual(num_offset_views, 1)
+
     def test_cat_to_alloc(self):
         """
         Test concatenation that's optimized out to an allocation.
@@ -374,6 +401,28 @@ class FxirTestCase(InductorTestCase):
         with open(output_filename) as f:
             output_code = f.read()
         self.assertIn("triton_kernel_wrapper_mutation", output_code)
+
+    def test_export_const_placeholder(self):
+        """
+        Test that we can compile a graph coming from torch.export with a constant input.
+        """
+
+        class TestModule(torch.nn.Module):
+            def forward(self, x, y):
+                return x - y
+
+        args = (torch.randn(8, device=self.device), 1)
+        mod = TestModule()
+        export_gm = torch.export.export(mod, args).module()
+
+        def compile_module(*inps):
+            torch._inductor.compile(export_gm, inps)
+
+        (inductor_gm,) = self._run_and_capture_graphs(compile_module, args)
+        result = inductor_gm(*args)
+        ref = mod(*args)
+
+        self.assertTrue(same(ref, result))
 
     @torch._inductor.config.patch("graph_partition", True)
     def test_subgraph_raises(self):
