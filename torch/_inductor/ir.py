645b4dd77bf4a4909bc9aee61688b833cbb96944
Valentine233 <xuan.liao@intel.com>
2025-06-02 22:48:39 -0700

    update

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -8388,7 +8388,6 @@ class _AllReduce_Kernel(_CollectiveKernel):
             unbacked_bindings=unbacked_bindings,
         )
         self.set_cpp_kernel_name("aoti_torch_cpu__c10d_functional_all_reduce_")
-        # self.cpp_kernel_name = "aoti_torch_cpu__c10d_functional_all_reduce_"
 
     def codegen(self, wrapper) -> None:  # type: ignore[no-untyped-def]
         wrapper.include_extra_header("torch/csrc/inductor/aoti_torch/c/shim_cpu.h")
@@ -8420,7 +8419,6 @@ class _AllReduceKernel(_CollectiveKernel):
             unbacked_bindings=unbacked_bindings,
         )
         self.set_cpp_kernel_name("aoti_torch_cpu__c10d_functional_all_reduce")
-        # self.cpp_kernel_name = "aoti_torch_cpu__c10d_functional_all_reduce"
 
     def codegen(self, wrapper) -> None:  # type: ignore[no-untyped-def]
         wrapper.include_extra_header("torch/csrc/inductor/aoti_torch/c/shim_cpu.h")
