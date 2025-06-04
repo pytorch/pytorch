f5a88ff20ba6db695c8a23bd397fa1dc293322ef
Xia, Weiwen <weiwen.xia@intel.com>
2025-06-04 07:25:08 -0700

    fix CI

--- aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
+++ aten/src/ATen/native/quantized/cpu/kernels/QuantizedOpKernels.cpp
@@ -2718,7 +2718,7 @@ void _fake_quantize_tensor_helper(
           bool* mask_val = (bool*)(data[1] + i * strides[1]);
           scalar_t* input_val = (scalar_t*)(data[2] + i * strides[2]);
 
-          const auto qval = static_cast<int64_t>(z_point + (double)std::nearbyint(*input_val * inv_scale));
+          const auto qval = static_cast<int64_t>((int32_t)z_point + std::nearbyint(*input_val * inv_scale));
           if (fake_quant_on) {
           *output_val = (std::fmin(std::fmax(qval, quant_min), quant_max) - z_point) * sc;
           *mask_val = ((quant_min <= qval) && (qval <= quant_max));
