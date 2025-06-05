3bf41c86d105cdb88914a4dfe1a3a1a11f810585
Bob Ren <bobren@meta.com>
2025-06-05 16:01:20 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_export/serde/union.py
+++ torch/_export/serde/union.py
@@ -1,7 +1,7 @@
-# mypy: allow-untyped-defs
 import functools
 from collections.abc import Hashable
 from dataclasses import fields
+from typing import Any
 
 
 class _UnionTag(str):
@@ -9,13 +9,13 @@ class _UnionTag(str):
     _cls: Hashable
 
     @staticmethod
-    def create(t, cls):
+    def create(t: str, cls: type) -> "_UnionTag":
         tag = _UnionTag(t)
         assert not hasattr(tag, "_cls")
         tag._cls = cls
         return tag
 
-    def __eq__(self, cmp) -> bool:
+    def __eq__(self, cmp: object) -> bool:
         assert isinstance(cmp, str)
         other = str(cmp)
         assert other in _get_field_names(
@@ -23,12 +23,12 @@ class _UnionTag(str):
         ), f"{other} is not a valid tag for {self._cls}. Available tags: {_get_field_names(self._cls)}"
         return str(self) == other
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(str(self))
 
 
 @functools.cache
-def _get_field_names(cls) -> set[str]:
+def _get_field_names(cls: type) -> set[str]:
     return {f.name for f in fields(cls)}
 
 
@@ -36,13 +36,13 @@ class _Union:
     _type: _UnionTag
 
     @classmethod
-    def create(cls, **kwargs):
+    def create(cls, **kwargs: Any) -> "_Union":
         assert len(kwargs) == 1
         obj = cls(**{**{f.name: None for f in fields(cls)}, **kwargs})  # type: ignore[arg-type]
         obj._type = _UnionTag.create(next(iter(kwargs.keys())), cls)
         return obj
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         assert not any(f.name in ("type", "_type", "create", "value") for f in fields(self))  # type: ignore[arg-type, misc]
 
     @property
@@ -55,17 +55,17 @@ class _Union:
             ) from e
 
     @property
-    def value(self):
+    def value(self) -> Any:
         return getattr(self, self.type)
 
-    def __getattribute__(self, name):
+    def __getattribute__(self, name: str) -> Any:
         attr = super().__getattribute__(name)
         if attr is None and name in _get_field_names(type(self)) and name != self.type:  # type: ignore[arg-type]
             raise AttributeError(f"Field {name} is not set.")
         return attr
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__repr__()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{type(self).__name__}({self.type}={getattr(self, self.type)})"
