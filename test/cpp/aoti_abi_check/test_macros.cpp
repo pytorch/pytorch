c7a504096e1f093fa59c8c9e7e86d03dbf23acf4
Jane Xu <janeyx@meta.com>
2025-06-05 12:36:27 -0700

    Add STD_TORCH_CHECK to torch::standalone
    ghstack-source-id: fc227afac1cd4c41d6e86fb308ccd3f141f39c22
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/155253

--- test/cpp/aoti_abi_check/test_macros.cpp
+++ test/cpp/aoti_abi_check/test_macros.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <torch/standalone/macros/Export.h>
+#include <torch/standalone/macros/Macros.h>
 
 namespace torch {
 namespace aot_inductor {
@@ -12,6 +13,12 @@ C10_API bool equal(int a, int b) {
 TEST(TestMacros, TestC10API) {
   EXPECT_TRUE(equal(1, 1));
   EXPECT_FALSE(equal(1, 2));
+}
+
+TEST(TestMacros, TestC10Macros) {
+  EXPECT_TRUE(C10_STRINGIZE("whatever") == "whatever");
+  EXPECT_TRUE(C10_LIKELY(true));
+  EXPECT_TRUE(C10_UNLIKELY(false));
 }
 
 } // namespace aot_inductor
