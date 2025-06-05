4040ec147f34c748cb27614a0458ee3659aa0f04
Yu, Guangye <guangye.yu@intel.com>
2025-06-05 17:40:29 +0000

    Update (base update)
    [ghstack-poisoned]

---
+++ c10/core/CachingDeviceAllocator.cpp
@@ -0,0 +1,16 @@
+#include <c10/core/CachingDeviceAllocator.h>
+
+namespace c10 {
+
+DeviceAllocator::DeviceAllocator() = default;
+DeviceAllocator::~DeviceAllocator() = default;
+
+DeviceAllocator* GetDeviceAllocator(const DeviceType& t) {
+  auto* allocator = c10::GetAllocator(t);
+  auto* device_allocator = dynamic_cast<DeviceAllocator*>(allocator);
+  TORCH_INTERNAL_ASSERT(
+      device_allocator, "Allocator for ", t, " is not a DeviceAllocator.");
+  return device_allocator;
+}
+
+} // namespace c10
