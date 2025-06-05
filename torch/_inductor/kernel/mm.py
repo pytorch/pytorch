978c31ea1fbed664a9d88656c0c13706d324ce0f
David Berard <dberard@fb.com>
2025-06-04 18:11:33 -0700

    [inductor][triton pin] TMA shim for mm, mm_scaled_grouped
    [ghstack-poisoned]

--- torch/_inductor/kernel/mm.py
+++ torch/_inductor/kernel/mm.py
@@ -264,23 +264,25 @@ persistent_tma_mm_template = TritonTemplate(
     a_desc_ptr = workspace_base
     b_desc_ptr = workspace_base + TMA_SIZE
 
-    triton.language.extra.cuda.experimental_device_tensormap_create2d(
+    triton_helpers.make_tensor_descriptor(
+        base_ptr=A,
+        global_shape=[M, K] if A_ROW_MAJOR else [K, M],
+        strides=[K if A_ROW_MAJOR else M, 1],
+        block_shape=[BLOCK_M, BLOCK_K] if A_ROW_MAJOR else [BLOCK_K, BLOCK_M],
         desc_ptr=a_desc_ptr,
-        global_address=A,
-        load_size=[BLOCK_M, BLOCK_K] if A_ROW_MAJOR else [BLOCK_K, BLOCK_M],
-        global_size=[M, K] if A_ROW_MAJOR else [K, M],
         element_ty=A.dtype.element_ty,
     )
-    triton.language.extra.cuda.experimental_device_tensormap_create2d(
+    triton_helpers.make_tensor_descriptor(
+        base_ptr=B,
+        global_shape=[K, N] if B_ROW_MAJOR else [N, K],
+        strides=[N if B_ROW_MAJOR else K, 1],
+        block_shape=[BLOCK_K, BLOCK_N] if B_ROW_MAJOR else [BLOCK_N, BLOCK_K],
         desc_ptr=b_desc_ptr,
-        global_address=B,
-        load_size=[BLOCK_K, BLOCK_N] if B_ROW_MAJOR else [BLOCK_N, BLOCK_K],
-        global_size=[K, N] if B_ROW_MAJOR else [N, K],
         element_ty=B.dtype.element_ty,
     )
 
-    tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(a_desc_ptr)
-    tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(b_desc_ptr)
+    triton_helpers.tensormap_fenceproxy_acquire(a_desc_ptr)
+    triton_helpers.tensormap_fenceproxy_acquire(b_desc_ptr)
 
     pid_m = 0
     pid_n = 0
@@ -302,13 +304,13 @@ persistent_tma_mm_template = TritonTemplate(
 
         rk = ki * BLOCK_K
 
-        a = tl._experimental_descriptor_load(
+        a = triton_helpers.load_tensor_descriptor(
             a_desc_ptr,
             [rm, rk] if A_ROW_MAJOR else [rk, rm],
             [BLOCK_M, BLOCK_K] if A_ROW_MAJOR else [BLOCK_K, BLOCK_M],
             A.dtype.element_ty,
         )
-        b = tl._experimental_descriptor_load(
+        b = triton_helpers.load_tensor_descriptor(
             b_desc_ptr,
             [rk, rn] if B_ROW_MAJOR else [rn, rk],
             [BLOCK_K, BLOCK_N] if B_ROW_MAJOR else [BLOCK_N, BLOCK_K],
@@ -415,23 +417,25 @@ device_tma = r"""
     a_desc_ptr = workspace_base
     b_desc_ptr = workspace_base + TMA_SIZE
 
-    triton.language.extra.cuda.experimental_device_tensormap_create2d(
+    triton_helpers.make_tensor_descriptor(
+        base_ptr=A,
+        global_shape=[M, K],
+        strides=[K, 1],
+        block_shape=[BLOCK_M, BLOCK_K],
         desc_ptr=a_desc_ptr,
-        global_address=A,
-        load_size=[BLOCK_M, BLOCK_K],
-        global_size=[M, K],
         element_ty=A.dtype.element_ty,
     )
-    triton.language.extra.cuda.experimental_device_tensormap_create2d(
+    triton_helpers.make_tensor_descriptor(
+        base_ptr=B,
+        global_shape=[N, K],
+        strides=[K, 1],
+        block_shape=[BLOCK_N, BLOCK_K],
         desc_ptr=b_desc_ptr,
-        global_address=B,
-        load_size=[BLOCK_N, BLOCK_K],
-        global_size=[N, K],
         element_ty=B.dtype.element_ty,
     )
 
-    tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(a_desc_ptr)
-    tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(b_desc_ptr)
+    triton_helpers.tensormap_fenceproxy_acquire(a_desc_ptr)
+    triton_helpers.tensormap_fenceproxy_acquire(b_desc_ptr)
 
     tiles_per_SM = num_tiles // NUM_SMS
     if start_pid < num_tiles % NUM_SMS:
@@ -464,10 +468,10 @@ device_tma = r"""
 
         offs_k = ki * BLOCK_K
 
-        a = tl._experimental_descriptor_load(
+        a = triton_helpers.load_tensor_descriptor(
             a_desc_ptr, [offs_am, offs_k], [BLOCK_M, BLOCK_K],  A.dtype.element_ty
         )
-        b = tl._experimental_descriptor_load(
+        b = triton_helpers.load_tensor_descriptor(
             b_desc_ptr, [offs_bn, offs_k], [BLOCK_N, BLOCK_K],  B.dtype.element_ty
         )
         if USE_FAST_ACCUM:
