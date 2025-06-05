a4647859b26fd9cdc5fcbb633b97d52c9c4e55f1
Animesh Jain <anijain@umich.edu>
2025-06-05 15:07:31 -0700

    [invoke_subgraph] Throw assertion on uncaptured speculate_subgraph
    [ghstack-poisoned]

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -3384,6 +3384,9 @@ class InvokeSubgraphHigherOrderVariable(WrapHigherOrderVariable):
 
         return body_name
 
+    @raise_hard_error_if_graph_break(
+        reason="torch.compile could not capture the `mark_compile_region` decorated function in one graph."
+    )
     def call_function(
         self,
         tx: "InstructionTranslator",
