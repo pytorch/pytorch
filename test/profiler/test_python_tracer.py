e951cb1f9dbb7c565ac83a6428da167088d1409d
Denghui Dong <denghui.ddh@alibaba-inc.com>
2025-06-06 23:57:08 +0800

    [Profiler] Ignore C call events on Python 3.12.0-3.12.4
    Python 3.12 reimplemented PyEval_SetProfile using sys.monitoring.
    There is a problem of C call events loss on 3.12.0-3.12.4. That problem was fixed on 3.12.5.
    This patch ignores the C call events on 3.12.0-3.12.4 and suggests users upgrade to 3.12.5+.

---
+++ test/profiler/test_python_tracer.py
@@ -0,0 +1,54 @@
+from torch.profiler import profile, ProfilerActivity
+from torch.testing._internal.common_utils import run_tests, skipIfPythonVersionNotIn, TestCase, TemporaryFileName
+
+import json
+import sys
+import time
+
+
+class TestPythonTracer(TestCase):
+
+    @skipIfPythonVersionNotIn("3.12", "3.13")
+    def test_method_with_c_function(self):
+        micro = sys.version_info.micro
+
+        class A:
+            method_with_c_function = classmethod(repr)
+
+        def get_key(x):
+            A().method_with_c_function()
+            time.sleep(1)
+            return len(x)
+
+        names = ["Alice", "Bob"]
+
+        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True) as prof:
+            sorted(names, key=get_key)
+
+        with TemporaryFileName(mode="w+") as fname:
+            print(fname)
+            prof.export_chrome_trace(fname)
+            with open(fname) as f:
+                events = json.load(f)["traceEvents"]
+                if micro >= 5:
+                    found = False
+                    for event in events:
+                        if event.get("cat", "") == "python_function" and event.get("name", "") == "<built-in function sorted>":
+                            duration = event.get("dur", 0)
+                            print(duration)
+                            if (duration >= 2000000):
+                                found = True
+                                break
+                    self.assertTure(found)
+                else:
+                    # On Python 3.12.0-3.12.4, c functions are ingores, so found should be False
+                    found = False
+                    for event in events:
+                        if event.get("cat", "") == "python_function" and event.get("name", "") == "<built-in function sorted>":
+                            found = True
+                            break
+                    self.assertFalse(found)
+
+
+if __name__ == "__main__":
+    run_tests()
