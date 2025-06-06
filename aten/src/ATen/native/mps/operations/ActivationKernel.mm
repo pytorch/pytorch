2e1a6bc912a82cba2ab6d25901d51ec405306084
Manuel Candales <mcandales@meta.com>
2025-06-06 01:50:33 -0400

    Update (base update)
    [ghstack-poisoned]

---
+++ aten/src/ATen/native/mps/operations/ActivationKernel.mm
@@ -0,0 +1,24 @@
+#define TORCH_ASSERT_ONLY_METHOD_OPERATORS
+#include <ATen/Scalar.h>
+#include <ATen/TensorIterator.h>
+#include <ATen/mps/MPSProfiler.h>
+// #include <ATen/native/Activation.h>
+#include <ATen/native/Activation.h>
+#include <ATen/native/mps/OperationUtils.h>
+#include <fmt/format.h>
+
+namespace at::native {
+
+#ifndef PYTORCH_JIT_COMPILE_SHADERS
+static auto& lib = mps::MetalShaderLibrary::getBundledLibrary();
+#else
+#include <ATen/native/mps/ActivationKernel_metallib.h>
+#endif
+
+static void hardshrink_kernel(TensorIteratorBase& iter, const Scalar& lambda=0.5) {
+  lib.exec_unary_kernel(iter, "hardshrink", lambda, ScalarType::Float);
+}
+
+REGISTER_DISPATCH(hardshrink_stub, hardshrink_kernel);
+
+} // namespace at::native
