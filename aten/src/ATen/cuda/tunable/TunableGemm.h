9d984d5a17494f2049caa5ceb0c972ac47084f41
Eddie Yan <eddiey@nvidia.com>
2025-06-06 20:41:28 +0000

    lint

--- aten/src/ATen/cuda/tunable/TunableGemm.h
+++ aten/src/ATen/cuda/tunable/TunableGemm.h
@@ -55,13 +55,13 @@ class DefaultGemmAndBiasOp : public Callable<GemmAndBiasParams<T>> {
           _transposeBoolFromChar(params->transb),
           params->m, params->n, params->k,
           params->alpha,
-	  params->beta,
+          params->beta,
           params->a, params->lda,
           params->b, params->ldb,
           params->bias,
           params->c, params->ldc,
           params->activation,
-	  params->bias2d);
+          params->bias2d);
       return OK;
     }
 };
