82fe6488b77f5e7acf94aff00faf244b72d9336e
xinan.lin <xinan.lin@intel.com>
2025-06-05 23:23:52 -0700

    [Break XPU] Fix failed test cases which are introduced by community for XPU.
    [ghstack-poisoned]

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -6056,7 +6056,6 @@ GPU_TEST_FAILURES = {
     "test_scaled_dot_product_efficient_attention": fail_gpu(("xpu",)),
     # No fft implementation for XPU yet.
     "test_fft_c2c": fail_gpu(("xpu",), is_skip=True),
-    "test_stft": fail_gpu(("xpu",)),
 }
 
 
