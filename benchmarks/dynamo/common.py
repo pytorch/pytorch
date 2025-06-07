55e235485c1539127c1eaf5d5945c5158c22c7d4
Simon Fan <xmfan@meta.com>
2025-06-06 17:01:57 -0700

    bench

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -3227,6 +3227,7 @@ def parse_args(args=None):
         "--compiled-autograd",
         action="store_true",
         help="Enables compiled autograd on compiled benchmark",
+        default=True,
     )
 
     parser.add_argument(
