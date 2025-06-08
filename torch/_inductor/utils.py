27df0c56b7c6b75a5f77f5714aeef2ef8f1faa2c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-06-08 16:58:58 +0000

    Revert "[inductor] use int64 for large index (#154575)"
    This reverts commit 2596e3d0617852469241be8777cf46db5c83928c.
    
    Reverted https://github.com/pytorch/pytorch/pull/154575 on behalf of https://github.com/clee2000 due to broke inductor/test_op_dtype_prop.py::TestCaseCUDA::test_op_dtype_propagation_add_cuda_int32 [GH job link](https://github.com/pytorch/pytorch/actions/runs/15510656657/job/43673763835) [HUD commit link](https://hud.pytorch.org/pytorch/pytorch/commit/2596e3d0617852469241be8777cf46db5c83928c), note for self: bad TD ([comment](https://github.com/pytorch/pytorch/pull/154575#issuecomment-2954175761))

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -3129,14 +3129,3 @@ def is_codegen_graph_partition_subgraph(wrapper: PythonWrapperCodegen) -> bool:
         isinstance(wrapper, SubgraphPythonWrapperCodegen)
         and wrapper.partition_signatures is not None
     )
-
-
-def dtype_from_size(size: int) -> torch.dtype:
-    from .virtualized import V
-
-    if V.graph.sizevars.statically_known_lt(
-        size, 2**31
-    ) and V.graph.sizevars.statically_known_geq(size, -(2**31)):
-        return torch.int32
-    else:
-        return torch.int64
