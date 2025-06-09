7a5c4cfed89a204f6848f9af88d97e98b3fa7507
Yu, Guangye <guangye.yu@intel.com>
2025-06-09 14:22:49 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/core/CachingDeviceAllocator.cpp
+++ c10/core/CachingDeviceAllocator.cpp
@@ -4,15 +4,7 @@ namespace c10 {
 
 // Ensures proper DLL export of this pure virtual base class on Windows,
 // since it's mainly used in other DLLs outside c10.dll.
-DeviceAllocator::DeviceAllocator() = default;
-DeviceAllocator::~DeviceAllocator() = default;
-
-DeviceAllocator* GetDeviceAllocator(const DeviceType& t) {
-  auto* allocator = c10::GetAllocator(t);
-  auto* device_allocator = dynamic_cast<DeviceAllocator*>(allocator);
-  TORCH_INTERNAL_ASSERT(
-      device_allocator, "Allocator for ", t, " is not a DeviceAllocator.");
-  return device_allocator;
-}
+// DeviceAllocator::DeviceAllocator() = default;
+// DeviceAllocator::~DeviceAllocator() = default;
 
 } // namespace c10
