2f1bd3ca28f9cc1d88eca3a1edb5d1f058fd483e
Yu, Guangye <guangye.yu@intel.com>
2025-06-09 16:42:35 +0000

    Update
    [ghstack-poisoned]

--- test/test_torch.py
+++ test/test_torch.py
@@ -248,6 +248,17 @@ class TestTorchDeviceType(TestCase):
         s[2:7] = 1
         self.assertEqual(s, storage_type(l))
 
+    @onlyNativeDeviceTypes
+    def test_storage_use_count(self, device):
+        a = torch.randn(10, device=device)
+        prev_cf = torch._C._storage_Use_Count(a.untyped_storage()._cdata)
+        self.assertEqual(prev_cf, 1)
+        b = a.view(2, 5)
+        self.assertEqual(a.untyped_storage()._cdata, b.untyped_storage()._cdata)
+        self.assertEqual(torch._C._storage_Use_Count(a.untyped_storage()._cdata, prev_cf + 1))
+        del b
+        self.assertEqual(torch._C._storage_Use_Count(a.untyped_storage()._cdata, prev_cf))
+
     @xfailIfTorchDynamo
     @onlyNativeDeviceTypes
     @dtypes(*all_types_and_complex_and(torch.half, torch.bool, torch.bfloat16))
