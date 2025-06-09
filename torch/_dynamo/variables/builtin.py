4e87afe19bb31df151245cabccf9b850588c5a86
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-06-09 20:45:16 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -2044,7 +2044,6 @@ class BuiltinVariable(VariableTracker):
                     "assertRaisesRegex",
                     "assertNotWarns",
                     "assertWarnsRegex",
-                    "assertDictEqual",
                     "assertWarns",
                 )
             ):
