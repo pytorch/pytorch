245f427bf0150517fa964a5767bba7630e3d0d97
Dhia-naouali <dhia12naouali@gmail.com>
2025-06-10 00:17:31 +0100

    fix lintint and typos in torch/distributed/tensor/examples/__init__.py

--- torch/distributed/tensor/examples/__init__.py
+++ torch/distributed/tensor/examples/__init__.py
@@ -1,21 +1,9 @@
-from .comm_model_features_example import CommDebugModelExample, run_example
-from .convnext_example import (
-    LayerNorm,
-    Block,
-    DownSampling,
-    init_weights ?,
-    ConvNeXt,
-)
-
-from .flex_attention_cp import (
-    create_block_mask_cached,
-    flex_attn_example,
-)
-
-from .torchrec_sharing_example import (
-    LocalShardWrapper,
+from .comm_mode_features_example import CommDebugModeExample
+from .convnext_example import Block, ConvNeXt, DownSampling, LayerNorm
+from .flex_attention_cp import create_block_mask_cached, flex_attn_example
+from .torchrec_sharding_example import (
+    LocalShardsWrapper,
     run_torchrec_row_wise_even_sharding_example,
     run_torchrec_row_wise_uneven_sharding_example,
     run_torchrec_table_wise_sharding_example,
-    run_example,
 )
