96213c06e729375a39286af937e1afac41747cb1
hlky <hlky@hlky.ac>
2025-06-10 16:40:39 +0100

    fix

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1500,6 +1500,7 @@ class Reduction(Loops):
             and src_dtype in (torch.float, torch.bfloat16, torch.float16)
             and isinstance(reduction_numel, Integer)
             and reduction_numel > 1_000_000
+            and reduction_numel % 2 == 0
         ):
             split = 1024
             log.info(
