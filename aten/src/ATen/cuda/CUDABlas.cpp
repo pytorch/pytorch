5c408aabadc3c8c4b7b3a8495f9d65d1aa31384f
Eddie Yan <eddiey@nvidia.com>
2025-06-10 21:54:28 +0000

    initialize epilogue

--- aten/src/ATen/cuda/CUDABlas.cpp
+++ aten/src/ATen/cuda/CUDABlas.cpp
@@ -1629,7 +1629,7 @@ bool gemm_and_bias(
   }
 #endif
 
-  cublasLtEpilogue_t epilogue;
+  cublasLtEpilogue_t epilogue = GEMMAndBiasActivationEpilogue::None;
   if (activation == GEMMAndBiasActivationEpilogue::RELU) {
     epilogue = CUBLASLT_EPILOGUE_RELU;
   } else if (activation == GEMMAndBiasActivationEpilogue::GELU) {
