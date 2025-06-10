91e3474004260c4d8f8ce51617f1153bcfb70d82
Laith Sakka <lsakka@meta.com>
2025-06-10 08:56:13 +0000

    Apply all replacements on backward graph args during inductor codegen. (#155469)
    Summary: temporary mitigation for https://github.com/pytorch/pytorch/issues/155468
    
    Test Plan:
    NA
    
    Rollback Plan:
    
    Differential Revision: D76096355
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/155469
    Approved by: https://github.com/bobrenjc93

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -1060,7 +1060,12 @@ class GraphLowering(torch.fx.Interpreter):
         example = super().placeholder(target, args, kwargs)  # type: ignore[arg-type]
         target = self.qualify_name(target)
         if isinstance(example, SymTypes):
-            expr = _get_placeholder_expr(example.node)
+            # TODO fix partitioning issue and re-enable for backward
+            # https://github.com/pytorch/pytorch/issues/155468.
+            if not V.graph.is_backward:
+                expr = _get_placeholder_expr(example.node)
+            else:
+                expr = example.node.expr
             self.graph_inputs[target] = expr
             self.graph_input_names.append(target)
             return expr
