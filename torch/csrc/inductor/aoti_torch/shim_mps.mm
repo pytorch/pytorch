27e2f0939c7c355b98f9714e867f315c681608c5
angelayi <yiangela7@gmail.com>
2025-06-10 11:25:38 -0700

    Update
    [ghstack-poisoned]

--- torch/csrc/inductor/aoti_torch/shim_mps.mm
+++ torch/csrc/inductor/aoti_torch/shim_mps.mm
@@ -10,26 +10,23 @@ using namespace torch::aot_inductor;
 AOTITorchError aoti_torch_mps_malloc(
     void** buffer,
     size_t num_bytes) {
+  if (num_bytes == 0) {
+    *buffer = nullptr;
+    return AOTI_TORCH_SUCCESS;
+  }
   AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({
-    if (num_bytes != 0) {
       id<MTLDevice> device = at::mps::MPSDevice::getInstance()->device();
-      if (!device) {
-        throw std::runtime_error("Failed to get MPS device");
-      }
+      TORCH_CHECK(device, "Failed to get MPS device");
       id<MTLBuffer> metal_buffer = [device newBufferWithLength:num_bytes options:MTLResourceCPUCacheModeWriteCombined | MTLResourceStorageModeShared];
-      if (!metal_buffer) {
-        throw std::runtime_error("Failed to allocate memory on MPS device");
-      }
-      auto buffer_pointer = static_cast<uint8_t*>([metal_buffer contents]);
+      TORCH_CHECK(metal_buffer, "Failed to allocate memory on MPS device");
       *buffer = (void*)metal_buffer;
-    }
   });
 }
 
 AOTITorchError aoti_torch_mps_free(
     void* ptr) {
   AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({
-    id<MTLBuffer> metal_buffer = (id<MTLBuffer>)ptr;
+    auto metal_buffer = (id<MTLBuffer>)ptr;
     [metal_buffer release];
   });
 }
@@ -38,7 +35,7 @@ AOTITorchError aoti_torch_mps_free(
 AOTITorchError
 aoti_torch_mps_memcpy(void* buffer, size_t constant_offset, size_t bytes_read, size_t data_size, uint8_t* constants_start) {
   AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({
-    id<MTLBuffer> metal_buffer = (id<MTLBuffer>)buffer;
+    auto metal_buffer = (id<MTLBuffer>)buffer;
     auto buffer_pointer = static_cast<uint8_t*>([metal_buffer contents]);
     memcpy(buffer_pointer + constant_offset, constants_start + bytes_read, data_size);
   });
