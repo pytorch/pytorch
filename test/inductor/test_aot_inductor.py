44df7cf28dd37a4d900eb6b2f78dabf72b209d9b
Bin Bao <binbao@meta.com>
2025-06-10 13:05:16 -0700

    [AOTI] Fix embed_kernel_binary error when max_autotune is ON (#155569)
    Summary: Stop removing cubin files so that it won't be missing when max_autotune is ON.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/155569
    Approved by: https://github.com/angelayi, https://github.com/yushangdi

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -126,7 +126,11 @@ except (unittest.SkipTest, ImportError):
 
 class AOTInductorTestsTemplate:
     @common_utils.parametrize("embed_kernel_binary", [False, True])
-    def test_simple(self, embed_kernel_binary):
+    @common_utils.parametrize("max_autotune", [False, True])
+    def test_simple(self, embed_kernel_binary, max_autotune):
+        if self.device == "cpu" and IS_MACOS and max_autotune:
+            raise unittest.SkipTest("max_autotune not supported on macos")
+
         class Model(torch.nn.Module):
             def __init__(self) -> None:
                 super().__init__()
@@ -140,7 +144,12 @@ class AOTInductorTestsTemplate:
             torch.randn(10, 10, device=self.device),
         )
         model = Model()
-        with config.patch({"aot_inductor.embed_kernel_binary": embed_kernel_binary}):
+        with config.patch(
+            {
+                "aot_inductor.embed_kernel_binary": embed_kernel_binary,
+                "max_autotune": max_autotune,
+            }
+        ):
             self.check_model(model, example_inputs)
 
             _, code = run_and_get_cpp_code(
