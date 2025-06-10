c232c1f04d9286a93ed8448898f476d39e0cc445
atalman <atalman@fb.com>
2025-06-10 21:32:08 +0000

    [BE] Remove CUDA 11 artifacts. Fix Check Binary workflow (#155555)
    Please see: https://github.com/pytorch/pytorch/issues/147383
    
    1. Remove CUDA 11 build and test artifacts. One place CUDA 12.4
    2. Fix Check Binary Workflow to use Stable Cuda version variable rather then hardcoded one
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/155555
    Approved by: https://github.com/malfet, https://github.com/Skylion007

--- .ci/docker/common/install_cuda.sh
+++ .ci/docker/common/install_cuda.sh
@@ -40,20 +40,6 @@ function install_cudnn {
   rm -rf tmp_cudnn
 }
 
-function install_118 {
-    CUDNN_VERSION=9.1.0.70
-    echo "Installing CUDA 11.8 and cuDNN ${CUDNN_VERSION} and NCCL and cuSparseLt-0.4.0"
-    install_cuda 11.8.0 cuda_11.8.0_520.61.05_linux
-
-    install_cudnn 11 $CUDNN_VERSION
-
-    CUDA_VERSION=11.8 bash install_nccl.sh
-
-    CUDA_VERSION=11.8 bash install_cusparselt.sh
-
-    ldconfig
-}
-
 function install_126 {
   CUDNN_VERSION=9.5.1.17
   echo "Installing CUDA 12.6.3 and cuDNN ${CUDNN_VERSION} and NCCL and cuSparseLt-0.7.1"
@@ -82,37 +68,6 @@ function install_129 {
   CUDA_VERSION=12.9 bash install_cusparselt.sh
 
   ldconfig
-}
-
-function prune_118 {
-    echo "Pruning CUDA 11.8 and cuDNN"
-    #####################################################################################
-    # CUDA 11.8 prune static libs
-    #####################################################################################
-    export NVPRUNE="/usr/local/cuda-11.8/bin/nvprune"
-    export CUDA_LIB_DIR="/usr/local/cuda-11.8/lib64"
-
-    export GENCODE="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86 -gencode arch=compute_90,code=sm_90"
-    export GENCODE_CUDNN="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86 -gencode arch=compute_90,code=sm_90"
-
-    if [[ -n "$OVERRIDE_GENCODE" ]]; then
-        export GENCODE=$OVERRIDE_GENCODE
-    fi
-
-    # all CUDA libs except CuDNN and CuBLAS (cudnn and cublas need arch 3.7 included)
-    ls $CUDA_LIB_DIR/ | grep "\.a" | grep -v "culibos" | grep -v "cudart" | grep -v "cudnn" | grep -v "cublas" | grep -v "metis"  \
-      | xargs -I {} bash -c \
-                "echo {} && $NVPRUNE $GENCODE $CUDA_LIB_DIR/{} -o $CUDA_LIB_DIR/{}"
-
-    # prune CuDNN and CuBLAS
-    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublas_static.a -o $CUDA_LIB_DIR/libcublas_static.a
-    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublasLt_static.a -o $CUDA_LIB_DIR/libcublasLt_static.a
-
-    #####################################################################################
-    # CUDA 11.8 prune visual tools
-    #####################################################################################
-    export CUDA_BASE="/usr/local/cuda-11.8/"
-    rm -rf $CUDA_BASE/libnvvp $CUDA_BASE/nsightee_plugins $CUDA_BASE/nsight-compute-2022.3.0 $CUDA_BASE/nsight-systems-2022.4.2/
 }
 
 function prune_126 {
@@ -169,8 +124,6 @@ function install_128 {
 while test $# -gt 0
 do
     case "$1" in
-    11.8) install_118; prune_118
-        ;;
     12.6|12.6.*) install_126; prune_126
         ;;
     12.8|12.8.*) install_128;
