492c8a6a40b4ea4bb696817a5436ee620070acc3
Bob Ren <bobren@meta.com>
2025-06-10 15:32:25 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/utils/_get_clean_triton.py
+++ torch/utils/_get_clean_triton.py
@@ -1,5 +1,4 @@
-from __future__ import annotations
-
+# mypy: allow-untyped-defs
 import argparse
 import os
 import re
@@ -114,7 +113,7 @@ def process_file(input_filename: str, output_filename: str) -> str:
 
 def get_clean_triton(
     input_path: Path, output_path: Path = Path("triton_only_repro.py")
-) -> str:
+):
     """Run experiments and output results to file
 
     Args:
