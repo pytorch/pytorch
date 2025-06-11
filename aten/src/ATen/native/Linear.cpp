b1ad022d0a35d8540054c9528e7d3b3a525fbd84
Pian Pawakapan <pianpwk@meta.com>
2025-06-11 12:29:36 -0700

    Update Linear.cpp

--- aten/src/ATen/native/Linear.cpp
+++ aten/src/ATen/native/Linear.cpp
@@ -93,13 +93,10 @@ Tensor linear(const Tensor& input, const Tensor& weight, const std::optional<Ten
   if (bias->defined() && !input.is_xla()) {
     // Also hit the fused path for contiguous 3D input, if not using xla
     // backend. Reshaping/flattening has some performance implications on xla.
-    if (definitely_contiguous(input.sym_sizes(), input.sym_strides(), input.sym_numel()) && input_dim == 3) {
+    bool is_contiguous = definitely_contiguous(input.sym_sizes(), input.sym_strides(), input.sym_numel());
+    if (is_contiguous && input_dim == 3) {
       return _flatten_nd_linear(input, weight, *bias);
-    } else if (
-      definitely_contiguous(input.sym_sizes(), input.sym_strides(), input.sym_numel()) &&
-      input.layout() == c10::kStrided &&
-      weight.layout() == c10::kStrided && bias->dim() == 1
-    ) {
+    } else if (is_contiguous && input.layout() == c10::kStrided && weight.layout() == c10::kStrided && bias->dim() == 1) {
       return _flatten_nd_linear(input, weight, *bias);
     } else if (parseLinearFlatten3d() && input_dim == 3) {
       // If user forces flattening via env var
