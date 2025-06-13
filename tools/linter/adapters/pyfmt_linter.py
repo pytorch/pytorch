01ee1a1a0f16926aef5d5145bc59716eb61e8cbb
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-06-13 17:02:04 +0800

    Update (base update)
    [ghstack-poisoned]

--- tools/linter/adapters/pyfmt_linter.py
+++ tools/linter/adapters/pyfmt_linter.py
@@ -57,6 +57,7 @@ USE_BLACK_FILELIST = re.compile(
                     # torch/[e-m]*/**
                     # torch/optim/**
                     # torch/[p-z]*/**
+                    "torch/[p-z]*/**",
                 ],
             ),
         )
