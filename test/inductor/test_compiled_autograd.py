a824deb502858120b4e311e8741bdda0462f293f
Simon Fan <xmfan@meta.com>
2025-06-12 23:36:07 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -1912,9 +1912,7 @@ main()
             yield model[0].weight.grad
             yield model[1].weight.grad
 
-        self.check_output_and_recompiles(
-            fn, [1, 0], compiler_fn=make_compiler_fn(backend="eager")
-        )
+        self.check_output_and_recompiles(fn, 1)
 
     def test_trace_run_with_rng_state(self):
         def sdpa(xq, xk):
