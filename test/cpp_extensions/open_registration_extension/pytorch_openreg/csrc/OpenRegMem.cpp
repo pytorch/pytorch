b1ea59e9ef66e40ca4ed1578daae1e8c21411fe4
FFFrog <ljw1101.vip@gmail.com>
2025-06-13 16:41:11 +0800

    [OpenReg][4/N] Migrate cpp_extensions_open_device_registration to OpenReg (#155101)
    As the title stated.
    
    **Involved testcases**:
    - test_open_device_storage_pin_memory
    - test_open_device_serialization
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/155101
    Approved by: https://github.com/albanD
    ghstack dependencies: #153947, #154018, #154019, #154106, #154181

--- test/cpp_extensions/open_registration_extension/pytorch_openreg/csrc/OpenRegMem.cpp
+++ test/cpp_extensions/open_registration_extension/pytorch_openreg/csrc/OpenRegMem.cpp
@@ -14,6 +14,7 @@
 #include <c10/core/Allocator.h>
 
 #include <torch/library.h>
+#include <torch/csrc/jit/serialization/pickler.h>
 
 namespace openreg {
 namespace {
@@ -274,6 +275,50 @@ TORCH_LIBRARY_IMPL(aten, PrivateUse1, m) {
   m.impl("_scaled_dot_product_fused_attention_overrideable", &custom_scaled_dot_product_fused_attention_overrideable);
   m.impl("_scaled_dot_product_fused_attention_overrideable_backward", &custom_scaled_dot_product_fused_attention_overrideable_backward);
 }
+
+struct OpenRegBackendMeta : public c10::BackendMeta {
+  OpenRegBackendMeta(int version_number, int format_number)
+      : version_number_(version_number), format_number_(format_number) {}
+
+  int version_number_{-1};
+  int format_number_{-1};
+};
+
+void for_serialization(
+    const at::Tensor& t,
+    std::unordered_map<std::string, bool>& m) {
+  auto meta_ptr = t.unsafeGetTensorImpl()->get_backend_meta();
+
+  if (meta_ptr != nullptr) {
+    auto o_meta_ptr = dynamic_cast<OpenRegBackendMeta*>(meta_ptr);
+    if (o_meta_ptr->version_number_ == 1) {
+      m["version_number"] = true;
+    }
+    if (o_meta_ptr->format_number_ == 29) {
+      m["format_number"] = true;
+    }
+  }
+}
+
+void for_deserialization(
+    const at::Tensor& t,
+    std::unordered_map<std::string, bool>& m) {
+  int version_number{-1};
+  int format_number{-1};
+
+  if (m.find("version_number") != m.end()) {
+    version_number = 1;
+  }
+  if (m.find("format_number") != m.end()) {
+    format_number = 29;
+  }
+
+  c10::intrusive_ptr<c10::BackendMeta> meta{std::unique_ptr<c10::BackendMeta>(
+      new OpenRegBackendMeta(version_number, format_number))};
+  t.unsafeGetTensorImpl()->set_backend_meta(meta);
+}
+
+REGISTER_PRIVATEUSE1_SERIALIZATION(&for_serialization, &for_deserialization)
 } // namespace openreg
 
 namespace at::native {
