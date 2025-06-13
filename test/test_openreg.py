406b225b68669d5c429b4f4dbca053c76119a3dc
FFFrog <ljw1101.vip@gmail.com>
2025-06-13 16:41:08 +0800

    [Openreg] Split TestOpenReg into two parts (#154018)
    ----
    
    - TestPrivateUse1: testing 3rd accelerator integration mechinasm itself
    - TestOpenReg: testing openreg itself
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/154018
    Approved by: https://github.com/albanD
    ghstack dependencies: #153947

--- test/test_openreg.py
+++ test/test_openreg.py
@@ -1,4 +1,4 @@
-# Owner(s): ["module: cpp"]
+# Owner(s): ["module: PrivateUse1"]
 
 import os
 import unittest
@@ -15,98 +15,21 @@ from torch.testing._internal.common_utils import (
 )
 
 
-class TestOpenReg(TestCase):
-    def test_initializes(self):
+class TestPrivateUse1(TestCase):
+    """Tests of third-parth device integration mechinasm based PrivateUse1"""
+
+    def test_backend_name(self):
         self.assertEqual(torch._C._get_privateuse1_backend_name(), "openreg")
+        # backend can be renamed to the same name multiple times
+        torch.utils.rename_privateuse1_backend("openreg")
+        with self.assertRaisesRegex(RuntimeError, "has already been set"):  # type: ignore[misc]
+            torch.utils.rename_privateuse1_backend("dev")
 
-    @unittest.skipIf(not IS_LINUX, "Only works on linux")
-    def test_autograd_init(self):
-        # Make sure autograd is initialized
-        torch.ones(2, requires_grad=True, device="openreg").sum().backward()
 
-        pid = os.getpid()
-        task_path = f"/proc/{pid}/task"
-        all_threads = psutil.Process(pid).threads()
+class TestOpenReg(TestCase):
+    """Tests of mimick accelerator named OpenReg based on PrivateUse1"""
 
-        all_thread_names = set()
-
-        for t in all_threads:
-            with open(f"{task_path}/{t.id}/comm") as file:
-                thread_name = file.read().strip()
-            all_thread_names.add(thread_name)
-
-        for i in range(torch.accelerator.device_count()):
-            self.assertIn(f"pt_autograd_{i}", all_thread_names)
-
-    def test_factory(self):
-        a = torch.empty(50, device="openreg")
-        self.assertEqual(a.device.type, "openreg")
-
-        a.fill_(3.5)
-
-        self.assertTrue(a.eq(3.5).all())
-
-    def test_printing(self):
-        a = torch.ones(20, device="openreg")
-        # Does not crash!
-        str(a)
-
-    def test_cross_device_copy(self):
-        a = torch.rand(10)
-        b = a.to(device="openreg").add(2).to(device="cpu")
-        self.assertEqual(b, a + 2)
-
-    def test_copy_same_device(self):
-        a = torch.ones(10, device="openreg").clone()
-        self.assertEqual(a, torch.ones(10, device="openreg"))
-
-    def test_cross_diff_devices_copy(self):
-        a = torch.ones(10, device="openreg:0").to(device="openreg:1").to(device="cpu")
-        self.assertEqual(a, torch.ones(10))
-
-    def test_data_dependent_output(self):
-        cpu_a = torch.randn(10)
-        a = cpu_a.to(device="openreg")
-        mask = a.gt(0)
-        out = torch.masked_select(a, mask)
-
-        self.assertEqual(out, cpu_a.masked_select(cpu_a.gt(0)))
-
-    def test_generator(self):
-        generator = torch.Generator(device="openreg:1")
-        self.assertEqual(generator.device.type, "openreg")
-        self.assertEqual(generator.device.index, 1)
-
-    # TODO(FFFrog): Add more check for rng_state
-    def test_rng_state(self):
-        state = torch.openreg.get_rng_state(0)
-        torch.openreg.set_rng_state(state, 0)
-
-    def test_manual_seed(self):
-        torch.openreg.manual_seed_all(2024)
-        self.assertEqual(torch.openreg.initial_seed(), 2024)
-
-    @skipIfTorchDynamo("unsupported aten.is_pinned.default")
-    def test_pin_memory(self):
-        cpu_a = torch.randn(10)
-        self.assertFalse(cpu_a.is_pinned())
-        pinned_a = cpu_a.pin_memory()
-        self.assertTrue(pinned_a.is_pinned())
-        slice_a = pinned_a[2:5]
-        self.assertTrue(slice_a.is_pinned())
-
-    @skipIfTorchDynamo("unsupported aten.is_pinned.default")
-    def test_rewrapped_storage(self):
-        pinned_a = torch.randn(10).pin_memory()
-        rewrapped_a = torch.tensor((), dtype=torch.float32).set_(
-            pinned_a.untyped_storage()[2:],
-            size=(5,),
-            stride=(1,),
-            storage_offset=0,
-        )
-        self.assertTrue(rewrapped_a.is_pinned())
-        self.assertNotEqual(pinned_a.data_ptr(), rewrapped_a.data_ptr())
-
+    # Stream & Event
     def test_stream_synchronize(self):
         stream = torch.Stream(device="openreg:1")
         stream.synchronize()
@@ -154,6 +77,98 @@ class TestOpenReg(TestCase):
         s2 = torch.Stream(device="openreg")
         e1 = s1.record_event()
         e1.wait(s2)
+
+    # Copy
+    def test_cross_device_copy(self):
+        a = torch.rand(10)
+        b = a.to(device="openreg").add(2).to(device="cpu")
+        self.assertEqual(b, a + 2)
+
+    def test_copy_same_device(self):
+        a = torch.ones(10, device="openreg").clone()
+        self.assertEqual(a, torch.ones(10, device="openreg"))
+
+    def test_cross_diff_devices_copy(self):
+        a = torch.ones(10, device="openreg:0").to(device="openreg:1").to(device="cpu")
+        self.assertEqual(a, torch.ones(10))
+
+    # RNG
+    def test_generator(self):
+        generator = torch.Generator(device="openreg:1")
+        self.assertEqual(generator.device.type, "openreg")
+        self.assertEqual(generator.device.index, 1)
+
+    def test_rng_state(self):
+        state = torch.openreg.get_rng_state(0)  # type: ignore[misc]
+        torch.openreg.set_rng_state(state, 0)  # type: ignore[misc]
+
+    def test_manual_seed(self):
+        torch.openreg.manual_seed_all(2024)  # type: ignore[misc]
+        self.assertEqual(torch.openreg.initial_seed(), 2024)  # type: ignore[misc]
+
+    # Autograd
+    @unittest.skipIf(not IS_LINUX, "Only works on linux")
+    def test_autograd_init(self):
+        # Make sure autograd is initialized
+        torch.ones(2, requires_grad=True, device="openreg").sum().backward()
+
+        pid = os.getpid()
+        task_path = f"/proc/{pid}/task"
+        all_threads = psutil.Process(pid).threads()
+
+        all_thread_names = set()
+
+        for t in all_threads:
+            with open(f"{task_path}/{t.id}/comm") as file:
+                thread_name = file.read().strip()
+            all_thread_names.add(thread_name)
+
+        for i in range(torch.accelerator.device_count()):
+            self.assertIn(f"pt_autograd_{i}", all_thread_names)
+
+    # Storage & Pin Memory
+    @skipIfTorchDynamo("unsupported aten.is_pinned.default")
+    def test_pin_memory(self):
+        cpu_a = torch.randn(10)
+        self.assertFalse(cpu_a.is_pinned())
+        pinned_a = cpu_a.pin_memory()
+        self.assertTrue(pinned_a.is_pinned())
+        slice_a = pinned_a[2:5]
+        self.assertTrue(slice_a.is_pinned())
+
+    @skipIfTorchDynamo("unsupported aten.is_pinned.default")
+    def test_rewrapped_storage(self):
+        pinned_a = torch.randn(10).pin_memory()
+        rewrapped_a = torch.tensor((), dtype=torch.float32).set_(
+            pinned_a.untyped_storage()[2:],
+            size=(5,),
+            stride=(1,),
+            storage_offset=0,
+        )
+        self.assertTrue(rewrapped_a.is_pinned())
+        self.assertNotEqual(pinned_a.data_ptr(), rewrapped_a.data_ptr())
+
+    # Opeartors
+    def test_factory(self):
+        a = torch.empty(50, device="openreg")
+        self.assertEqual(a.device.type, "openreg")
+
+        a.fill_(3.5)
+
+        self.assertTrue(a.eq(3.5).all())
+
+    def test_printing(self):
+        a = torch.ones(20, device="openreg")
+        # Does not crash!
+        str(a)
+
+    def test_data_dependent_output(self):
+        cpu_a = torch.randn(10)
+        a = cpu_a.to(device="openreg")
+        mask = a.gt(0)
+        out = torch.masked_select(a, mask)
+
+        self.assertEqual(out, cpu_a.masked_select(cpu_a.gt(0)))
 
     def test_expand(self):
         x = torch.tensor([[1], [2], [3]], device="openreg")
