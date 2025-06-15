bbe70f07ba85dd4a704eab3f58ae0536cb704438
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-06-16 00:22:31 +0800

    Update
    [ghstack-poisoned]

--- torch/__init__.py
+++ torch/__init__.py
@@ -1025,10 +1025,10 @@ except ImportError:
                     of the PyTorch repository rather than the C extensions which
                     are expected in the `torch._C` namespace. This can occur when
                     using the `install` workflow. e.g.
-                        $ python setup.py install && python -c "import torch"
+                        $ python -m pip install . && python -c "import torch"
 
                     This error can generally be solved using the `develop` workflow
-                        $ python setup.py develop && python -c "import torch"  # This should succeed
+                        $ python -m pip install -e . && python -c "import torch"  # This should succeed
                     or by running Python from a different directory.
                 """
             ).strip()
