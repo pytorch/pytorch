6582e53eed9fe4b3113ea6f588e7ee53f6715bcb
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-06-16 10:07:31 +0800

    Update
    [ghstack-poisoned]

--- tools/linter/clang_tidy/generate_build_files.py
+++ tools/linter/clang_tidy/generate_build_files.py
@@ -31,7 +31,7 @@ def gen_compile_commands() -> None:
     os.environ["USE_PRECOMPILED_HEADERS"] = "1"
     os.environ["CC"] = "clang"
     os.environ["CXX"] = "clang++"
-    run_cmd([sys.executable, "setup.py", "build", "--cmake-only"])
+    run_cmd([sys.executable, "setup.py", "--cmake-only", "build"])
 
 
 def run_autogen() -> None:
