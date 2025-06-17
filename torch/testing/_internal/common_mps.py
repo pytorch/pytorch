744a39938347879fafd8793966e8da066c2423dd
Manuel Candales <mcandales@meta.com>
2025-06-17 11:43:41 -0400

    update

--- torch/testing/_internal/common_mps.py
+++ torch/testing/_internal/common_mps.py
@@ -313,11 +313,6 @@ if torch.backends.mps.is_available():
             # but in case of the returned indices this results in undefined behaviour.
             "sort": [torch.int8, torch.uint8, torch.bool, torch.float16],
             # Unsupported dtypes
-            "cumsum": [torch.int64],
-            "cumprod": [torch.int64],
-            "cumulative_trapezoid": [torch.int64],
-            "masked.cumsum": [torch.int64],
-            "masked.cumprod": [torch.int64],
             "linalg.vander": [torch.int64],
             # Fail with `Expected 1.0 but got nan.` for empty tensors
             # Caused by sample input at index 23: SampleInput(
