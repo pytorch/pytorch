pytorch/pytorch

commit 5116293f7eb587ada6076f3c3ea1711a0ec7ab4a

[XPU] Split triton version as 2 files to decouple triton version bump (#155313)
Triton XPU shares its version file with the community one. When the community updates Triton version, it will temporarily break the XPU CI/CD because they use different repositories and commits. To decouple Triton version bumps between the community and XPU, we propose splitting the version into two separate files.

Refer the latest community triton version bump PR #153117

Pull Request resolved: https://github.com/pytorch/pytorch/pull/155313
Approved by: https://github.com/etaf, https://github.com/EikanWang, https://github.com/atalman

.github/scripts/windows/build_triton.bat
@@ -15,4 +15,4 @@ call conda run -n %PYTHON_PREFIX% pip install wheel pybind11 certifi cython cmak
 dir "%VC_INSTALL_PATH%"
 
 call "%VC_INSTALL_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
-call conda run -n %PYTHON_PREFIX% python .github/scripts/build_triton_wheel.py --device=%BUILD_DEVICE% %RELEASE%
+call conda run -n %PYTHON_PREFIX% python .github/scripts/build_triton_wheel.py --device=%BUILD_DEVICE% %TRITON_VERSION% %RELEASE%

.github/workflows/build-triton-wheel.yml
@@ -156,8 +156,9 @@ jobs:
           fi
 
           if [[ "${BUILD_DEVICE}" == xpu ]]; then
+            TRITON_VERSION=$(cat .ci/docker/triton_xpu_version.txt)
             docker exec -t "${container_name}" bash -c "dnf install -y gcc-toolset-13-gcc-c++"
-            docker exec -t "${container_name}" bash -c "source /opt/rh/gcc-toolset-13/enable && ${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE $RELEASE"
+            docker exec -t "${container_name}" bash -c "source /opt/rh/gcc-toolset-13/enable && ${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE --triton-version=$TRITON_VERSION $RELEASE"
           else
             docker exec -t "${container_name}" bash -c "${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE $RELEASE $WITH_CLANG_LDD"
           fi
@@ -253,6 +254,11 @@ jobs:
           export RELEASE=""
           if [[ "${IS_RELEASE_TAG}" == true ]]; then
             export RELEASE="--release"
+          fi
+          export TRITON_VERSION=""
+          if [[ "${{ matrix.device }}" == "xpu" ]]; then
+            triton_version=$(cat .ci/docker/ci_commit_pins/triton-xpu.txt)
+            export TRITON_VERSION="--triton-version=${triton_version}"
           fi
           .github/scripts/windows/build_triton.bat
           mkdir -p "${RUNNER_TEMP}/artifacts/"

.ci/docker/ubuntu-xpu/Dockerfile
@@ -72,7 +72,7 @@ ARG TRITON
 COPY ./common/install_triton.sh install_triton.sh
 COPY ./common/common_utils.sh common_utils.sh
 COPY ci_commit_pins/triton-xpu.txt triton-xpu.txt
-COPY triton_version.txt triton_version.txt
+COPY triton_xpu_version.txt triton_version.txt
 RUN if [ -n "${TRITON}" ]; then bash ./install_triton.sh; fi
 RUN rm install_triton.sh common_utils.sh triton-xpu.txt triton_version.txt
 

.circleci/scripts/binary_populate_env.sh
@@ -105,6 +105,7 @@ fi
 
 # Set triton via PYTORCH_EXTRA_INSTALL_REQUIREMENTS for triton xpu package
 if [[ "$PACKAGE_TYPE" =~ .*wheel.* && -n "$PYTORCH_BUILD_VERSION" && "$PYTORCH_BUILD_VERSION" =~ .*xpu.* ]]; then
+    TRITON_VERSION=$(cat $PYTORCH_ROOT/.ci/docker/triton_xpu_version.txt)
     TRITON_REQUIREMENT="pytorch-triton-xpu==${TRITON_VERSION}"
     if [[ -n "$PYTORCH_BUILD_VERSION" && "$PYTORCH_BUILD_VERSION" =~ .*dev.* ]]; then
         TRITON_SHORTHASH=$(cut -c1-8 $PYTORCH_ROOT/.ci/docker/ci_commit_pins/triton-xpu.txt)

scripts/install_triton_wheel.sh
@@ -20,7 +20,7 @@ else
     # 2. [Not exposed to user] If the user set `TRITON_XPU_BUILD_FROM_SOURCE=1` flag,
     #    it will install Triton from the source.
 
-    TRITON_VERSION="pytorch-triton-xpu==$(cat .ci/docker/triton_version.txt)"
+    TRITON_VERSION="pytorch-triton-xpu==$(cat .ci/docker/triton_xpu_version.txt)"
     TRITON_XPU_COMMIT_ID="$(head -c 8 .ci/docker/ci_commit_pins/triton-xpu.txt)"
     if [[ -z "${TRITON_XPU_BUILD_FROM_SOURCE}" ]]; then
         ${PIP} install --index-url ${DOWNLOAD_PYTORCH_ORG}/nightly/ ${TRITON_VERSION}+git${TRITON_XPU_COMMIT_ID}

commit 11bb1ece50ade57b963afe29b27cab6a0a56ff7d

[CI] Fix triton version split issue (#155670)
Fix a bug caused by #155313, refer https://github.com/pytorch/pytorch/actions/runs/15576592378/job/43862613039?pr=154194#step:7:652
Pull Request resolved: https://github.com/pytorch/pytorch/pull/155670
Approved by: https://github.com/atalman, https://github.com/EikanWang

.github/scripts/build_triton_wheel.py
@@ -21,8 +21,11 @@ def read_triton_pin(device: str = "cuda") -> str:
         return f.read().strip()
 
 
-def read_triton_version() -> str:
-    with open(REPO_DIR / ".ci" / "docker" / "triton_version.txt") as f:
+def read_triton_version(device: str = "cuda") -> str:
+    triton_version_file = "triton_version.txt"
+    if device == "xpu":
+        triton_version_file = "triton_xpu_version.txt"
+    with open(REPO_DIR / ".ci" / "docker" / triton_version_file) as f:
         return f.read().strip()
 
 
@@ -91,7 +94,7 @@ def build_triton(
         patch_init_py(
             triton_pythondir / "triton" / "__init__.py",
             version=f"{version}",
-            expected_version=None,
+            expected_version=read_triton_version(device),
         )
 
         if device == "rocm":
@@ -137,15 +140,19 @@ def main() -> None:
     parser.add_argument("--py-version", type=str)
     parser.add_argument("--commit-hash", type=str)
     parser.add_argument("--with-clang-ldd", action="store_true")
-    parser.add_argument("--triton-version", type=str, default=read_triton_version())
+    parser.add_argument("--triton-version", type=str, default=None)
     args = parser.parse_args()
+
+    triton_version = read_triton_version(args.device)
+    if args.triton_version:
+        triton_version = args.triton_version
 
     build_triton(
         device=args.device,
         commit_hash=(
             args.commit_hash if args.commit_hash else read_triton_pin(args.device)
         ),
-        version=args.triton_version,
+        version=triton_version,
         py_version=args.py_version,
         release=args.release,
         with_clang_ldd=args.with_clang_ldd,

.github/scripts/windows/build_triton.bat
@@ -15,4 +15,4 @@ call conda run -n %PYTHON_PREFIX% pip install wheel pybind11 certifi cython cmak
 dir "%VC_INSTALL_PATH%"
 
 call "%VC_INSTALL_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
-call conda run -n %PYTHON_PREFIX% python .github/scripts/build_triton_wheel.py --device=%BUILD_DEVICE% %TRITON_VERSION% %RELEASE%
+call conda run -n %PYTHON_PREFIX% python .github/scripts/build_triton_wheel.py --device=%BUILD_DEVICE% %RELEASE%

.github/workflows/build-triton-wheel.yml
@@ -156,9 +156,8 @@ jobs:
           fi
 
           if [[ "${BUILD_DEVICE}" == xpu ]]; then
-            TRITON_VERSION=$(cat .ci/docker/triton_xpu_version.txt)
             docker exec -t "${container_name}" bash -c "dnf install -y gcc-toolset-13-gcc-c++"
-            docker exec -t "${container_name}" bash -c "source /opt/rh/gcc-toolset-13/enable && ${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE --triton-version=$TRITON_VERSION $RELEASE"
+            docker exec -t "${container_name}" bash -c "source /opt/rh/gcc-toolset-13/enable && ${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE $RELEASE"
           else
             docker exec -t "${container_name}" bash -c "${PYTHON_EXECUTABLE} /pytorch/.github/scripts/build_triton_wheel.py --device=$BUILD_DEVICE $RELEASE $WITH_CLANG_LDD"
           fi
@@ -254,11 +253,6 @@ jobs:
           export RELEASE=""
           if [[ "${IS_RELEASE_TAG}" == true ]]; then
             export RELEASE="--release"
-          fi
-          export TRITON_VERSION=""
-          if [[ "${{ matrix.device }}" == "xpu" ]]; then
-            triton_version=$(cat .ci/docker/ci_commit_pins/triton-xpu.txt)
-            export TRITON_VERSION="--triton-version=${triton_version}"
           fi
           .github/scripts/windows/build_triton.bat
           mkdir -p "${RUNNER_TEMP}/artifacts/"