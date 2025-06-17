fc49b8bad7d9603f50d8cd98f38d3dd9f6e7bb7c
Yu, Guangye <guangye.yu@intel.com>
2025-06-17 14:47:04 +0000

    Update
    [ghstack-poisoned]

--- c10/cuda/CUDAAllocatorConfig.h
+++ c10/cuda/CUDAAllocatorConfig.h
@@ -8,15 +8,8 @@
 namespace c10::cuda::CUDACachingAllocator {
 
 // Keep this for backwards compatibility
-class C10_CUDA_API CUDAAllocatorConfig {
+class C10_CUDA_API CUDAAllocatorConfig : CachingAllocator::AllocatorConfig {
  public:
-  static size_t max_split_size() {
-    return CachingAllocator::AllocatorConfig::max_split_size();
-  }
-  static double garbage_collection_threshold() {
-    return CachingAllocator::AllocatorConfig::garbage_collection_threshold();
-  }
-
   static bool expandable_segments() {
     return CachingAllocator::AllocatorConfig::use_expandable_segments();
   }
@@ -30,37 +23,6 @@ class C10_CUDA_API CUDAAllocatorConfig {
   static bool pinned_use_cuda_host_register() {
     return CachingAllocator::AllocatorConfig::pinned_use_device_host_register();
   }
-
-  static size_t pinned_num_register_threads() {
-    return CachingAllocator::AllocatorConfig::pinned_num_register_threads();
-  }
-
-  static bool pinned_use_background_threads() {
-    return CachingAllocator::AllocatorConfig::pinned_use_background_threads();
-  }
-
-  static size_t pinned_max_register_threads() {
-    return CachingAllocator::AllocatorConfig::pinned_max_register_threads();
-  }
-
-  static size_t roundup_power2_divisions(size_t size) {
-    return CachingAllocator::AllocatorConfig::roundup_power2_divisions(size);
-  }
-
-  static std::vector<size_t> roundup_power2_divisions() {
-    return CachingAllocator::AllocatorConfig::roundup_power2_divisions();
-  }
-
-  static size_t max_non_split_rounding_size() {
-    return CachingAllocator::AllocatorConfig::max_non_split_rounding_size();
-  }
-
-  static std::string last_allocator_settings() {
-    return CachingAllocator::AllocatorConfig::last_allocator_settings();
-  }
-
- private:
-  CUDAAllocatorConfig() = default;
 };
 
 // Keep this for backwards compatibility
