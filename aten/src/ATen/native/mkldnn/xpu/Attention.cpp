c4f99ce8a2409e01c1512a01fa687ed1d7a90879
fengqing.lu <fengqing.lu@intel.com>
2025-06-17 22:38:52 -0700

    fix lint

--- aten/src/ATen/native/mkldnn/xpu/Attention.cpp
+++ aten/src/ATen/native/mkldnn/xpu/Attention.cpp
@@ -225,9 +225,8 @@ _scaled_dot_product_fused_attention_overrideable_xpu(
   const int64_t seq_len_kv = key.size(2);
 
   at::Tensor output;
-  auto opts = query.options();
   std::vector<int64_t> output_shape = {
-        batch_size, num_head_q, seq_len_q, head_dim_v};
+      batch_size, num_head_q, seq_len_q, head_dim_v};
   alloc_with_matching_layout(query, output, output_shape);
   at::Tensor logsumexp, debug_attn_mask; // not supported
 
