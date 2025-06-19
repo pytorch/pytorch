2d3c7cda8854b35cdf814acd4b6f27c1087c4d4f
Cui, Yifeng <yifeng.cui@intel.com>
2025-06-18 20:07:17 -0700

    Reuse CUDA's logic

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -330,14 +330,15 @@ def meta_fft_r2c(self, dim, normalization, onesided):
     if onesided:
         out_sizes[last_dim] = last_dim_halfsize
 
-    if device_hint(self) == "cuda":
+    if device_hint(self) == "cuda" or device_hint(self) == "xpu":
         # _fft_r2c_cufft in aten/src/ATen/native/cuda/SpectralOps.cpp
+        # _fft_r2c_xpu in torch-xpu-ops/src/ATen/native/xpu/SpectralOps.cpp
         output = self.new_empty(
             out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
         )
 
         working_tensor = self
-        if use_optimized_cufft_path(dim):
+        if device_hint(self) == "cuda" and use_optimized_cufft_path(dim):
             _exec_fft(output, working_tensor, out_sizes, dim, forward=True)
         else:
             # First do the R2C transform on the last dimension
@@ -370,37 +371,6 @@ def meta_fft_r2c(self, dim, normalization, onesided):
 
         return output
 
-    elif device_hint(self) == "xpu":
-        out = self.new_empty(
-            out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
-        )
-
-        working_tensor = self
-        # First do the R2C transform on the last dimension
-        target_sizes = out_sizes if len(dim) == 1 else onesided_sizes
-        _exec_fft(out, working_tensor, target_sizes, [last_dim], forward=True)
-        if len(dim) > 1:
-            working_tensor = self.new_empty(
-                out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
-            )
-
-        # Then any remaining C2C transforms
-        sorted_dims = dim[:-1]
-        while sorted_dims:
-            out, working_tensor = working_tensor, out
-
-            sorted_dims = _sort_dims(self, sorted_dims)
-            max_dims = min(cufft_max_ndim, len(sorted_dims))
-            last_dims = sorted_dims[len(sorted_dims) - max_dims :]
-            _exec_fft(out, working_tensor, onesided_sizes, last_dims, forward=True)
-            sorted_dims = sorted_dims[: len(sorted_dims) - max_dims]
-
-        if not onesided:
-            if out.size(last_dim) != out_sizes[last_dim]:
-                working_tensor.resize_(out_sizes, memory_format=torch.contiguous_format)
-                out = working_tensor
-
-        return out
     else:
         return self.new_empty(
             out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
