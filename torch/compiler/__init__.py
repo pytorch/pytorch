622f1554a09aecd7299ded0ffda5017cf7c4ca0f
Animesh Jain <anijain@umich.edu>
2025-06-20 10:34:35 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/compiler/__init__.py
+++ torch/compiler/__init__.py
@@ -32,7 +32,7 @@ __all__ = [
     "skip_guard_on_all_nn_modules_unsafe",
     "keep_tensor_guards_unsafe",
     "skip_guard_on_globals_unsafe",
-    "mark_compile_region",
+    "nested_compile_region",
 ]
 
 
@@ -569,26 +569,28 @@ def skip_guard_on_globals_unsafe(guard_entries):
     return [not entry.is_global for entry in guard_entries]
 
 
-def mark_compile_region(fn=None):
+def nested_compile_region(fn=None):
     """
-    Hint **``torch.compile``** that the marked set of operations forms a
-    *repeated region* whose code can be compiled once and safely reused.
-    ``mark_compile_region`` can also be used as a decorator.
+    Tells **``torch.compile``** that the marked set of operations forms a nested
+    compile region (which is often repeated in the full model) whose code can be
+    compiled once and safely reused.  ``nested_compile_region`` can also be used
+    as a decorator.
 
     During **``torch.compile``** tracing, the compiler applies *hierarchical
-    compilation*: it emits optimized code for the marked region the first time
-    it is encountered and re-emits (or “stamps out”) the previously compiled
-    code on every subsequent invocation.  This can substantially reduce overall
-    compile time for deeply-stacked, structurally-identical components such as
-    the transformer layers of a large-language-model (LLM).
+    compilation* with ``nested_compile_region``: it emits optimized code for the
+    marked region the first time it is encountered and re-emits (or “stamps
+    out”) the previously compiled code on every subsequent invocation.  This can
+    substantially reduce overall compile time for deeply-stacked,
+    structurally-identical components such as the transformer layers of a
+    large-language-model (LLM).
 
     Outside a ``torch.compile`` context—i.e., in standard eager execution—the
     call is a no-op, so existing workflows remain unaffected.
 
-    Note that ``mark_compile_region`` **does not** promise that a region will be
-    compiled exactly once.  If the compiler detects that new input conditions
+    Note that ``nested_compile_region`` **does not** promise that a region will
+    be compiled exactly once.  If the compiler detects that new input conditions
     (shape, dtype, device, stride, globals etc.) make the cached version invalid
-    to reuse, it will transparently re-compile the region.  Using this hint is
+    to reuse, it will transparently re-compile the region.  Using it is
     therefore *safe*: correctness is always preserved, and you pay the extra
     compilation cost only when required.
     """
