fix metashape and causal

test/test_transformers.py
@@ -4251,7 +4251,7 @@ class TestSDPAXpuOnly(NNTestCase):
         (1, 32, 2016, 2016, 128),
         (4, 32, 2016, 2016, 128),
     ])
-    @parametrize("mask_type", ["float",]) #"causal"])
+    @parametrize("mask_type", ["float", "causal"])
     @parametrize("train", [False, True])
     def test_scaled_dot_product_fused_attention_mask_vs_math(
         self,
@@ -4309,7 +4309,7 @@ class TestSDPAXpuOnly(NNTestCase):
         attn_mask2 = attn_mask.float() if attn_mask is not None else None
 
         if fused_kernel == SDPBackend.OVERRIDEABLE:
-            with sdpa_kernel(backends=[SDPBackend.OVERRIDEABLE]):
+            with sdpa_kernel(backends=[SDPBackend.OVERRIDEABLE, SDPBackend.MATH]):
                 actual = F.scaled_dot_product_attention(
                     q, k, v, attn_mask=attn_mask, dropout_p=0.0, is_causal=is_causal)
         else:

torch/_meta_registrations.py
@@ -5962,7 +5962,11 @@ def meta__scaled_dot_product_fused_attention_overrideable_backward(
     grad_q = torch.empty_like(query)
     grad_k = torch.empty_like(key)
     grad_v = torch.empty_like(value)
-    return grad_q, grad_k, grad_v, None
+
+    grad_attn_bias = None
+    if attn_bias is not None:
+        grad_attn_bias = torch.empty_like(attn_bias)
+    return grad_q, grad_k, grad_v, grad_attn_bias
 
 
 @register_meta(