6446ff1837d376b375f0e2e79148c6381c5082db
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-06-20 14:34:39 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -1612,7 +1612,7 @@ class BuiltinVariable(VariableTracker):
             # (e.g. when __iter__ just returns iter(self.list)) or return a user-defined iterator.
             # If the object implements a __getitem__ method, iter(...) will call obj.__getitem__()
             # with an integer argument starting at 0, until __getitem__ raises IndexError
-            ret = variables.UserFunctionVariable(polyfills.iter_protocol).call_function(
+            ret = variables.UserFunctionVariable(polyfills.builtins.iter).call_function(
                 tx, [obj, *args], {}
             )
 
