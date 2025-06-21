604b30b3adacac2c1970134d8d1bcbbce7b1c059
Ke Wen <kw2501@meta.com>
2025-06-21 01:58:26 -0700

    Update
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/symm_mem/nvshmem_extension.cuh
+++ torch/csrc/distributed/c10d/symm_mem/nvshmem_extension.cuh
@@ -15,7 +15,7 @@ void initialize_nvshmem_with_store(
 // operations.
 TORCH_API void nvshmemx_cumodule_init(uintptr_t module);
 
-TORCH_API void nvshmem_put(at::Tensor& inp, int64_t peer);
+TORCH_API void nvshmem_put(at::Tensor& tensor, int64_t peer);
 
 at::Tensor nvshmem_broadcast(at::Tensor& input, const std::string& group_name);
 
