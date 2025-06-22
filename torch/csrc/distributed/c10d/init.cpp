4b55871e06d6bad54eac45e45a9af615d758a39f
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-06-22 12:27:36 +0000

    Revert "[BE][11/16] fix typos in torch/ (torch/csrc/distributed/) (#156321)"
    This reverts commit c95f7fa874a3116f1067f9092456ee7281003614.
    
    Reverted https://github.com/pytorch/pytorch/pull/156321 on behalf of https://github.com/atalman due to export/test_torchbind.py::TestCompileTorchbind::test_compile_error_on_input_aliasing_contents_backend_aot_eager [GH job link](https://github.com/pytorch/pytorch/actions/runs/15804799771/job/44548489912) [HUD commit link](https://hud.pytorch.org/pytorch/pytorch/commit/c95f7fa874a3116f1067f9092456ee7281003614) ([comment](https://github.com/pytorch/pytorch/pull/156321#issuecomment-2994163667))

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -1006,7 +1006,7 @@ This class does not support ``__members__`` property.)");
   });
 
 #ifdef USE_NVSHMEM
-  // Initializes the device state in CUmodule so that it’s able to perform
+  // Intializes the device state in CUmodule so that it’s able to perform
   // NVSHMEM operations.
   module.def(
       "_nvshmemx_cumodule_init",
@@ -3297,7 +3297,7 @@ Arguments:
             Default is False.
 
 Attributes:
-    config (NCCLConfig): configures NCCL communicators (only available for
+    config (NCCLConfig): configures NCCL communicators (only avaiable for
             builds using NCCL 2.17+). This can be used to improve
             communication-computation overlap for NCCL kernels by tuning
             available parameters in the config. See
