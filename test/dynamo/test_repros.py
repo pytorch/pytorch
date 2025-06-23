5ad2bee2c8a7defd2580bb138145a49c37146fcc
William Wen <williamwen@meta.com>
2025-06-23 15:57:14 -0700

    [dynamo] fix segfault due to dangling CacheEntry backend pointer (#156527)
    Fixes https://github.com/pytorch/pytorch/issues/155057
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/156527
    Approved by: https://github.com/anijain2305, https://github.com/jansel

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -6978,6 +6978,18 @@ def forward(self, s77 : torch.SymInt, s27 : torch.SymInt, L_x_ : torch.Tensor):
         res = torch.compile(f, backend="aot_eager")()
         self.assertEqual(ref, res)
 
+    def test_deleted_compile_wrapper_segfault(self):
+        def fn(x):
+            return x + 1
+
+        opt_fn = torch.compile(fn, backend="eager")
+        # This calls cached_backend.clear() which removes any strong references
+        # to the callback
+        torch._dynamo.reset()
+        opt_fn(torch.randn(3))
+        opt_fn = torch.compile(fn, backend="eager")
+        opt_fn(torch.randn(3))  # possible segfault due to first opt_fn deletion
+
     def test_delete_local_error(self):
         @torch.compile(backend="eager", fullgraph=True)
         def fn(x):
