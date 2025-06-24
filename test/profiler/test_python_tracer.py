3efd908ab84fd08fe78d502fa419a0da58401027
Denghui Dong <denghui.ddh@alibaba-inc.com>
2025-06-24 11:19:05 +0800

    update according to the comments

--- test/profiler/test_python_tracer.py
+++ test/profiler/test_python_tracer.py
@@ -1,28 +1,34 @@
-from torch.profiler import profile, ProfilerActivity
-from torch.testing._internal.common_utils import run_tests, skipIfPythonVersionMismatch, TestCase, TemporaryFileName
+# Owner(s): ["oncall: profiler"]
 
 import json
 import sys
 import time
 
+from torch.profiler import profile, ProfilerActivity
+from torch.testing._internal.common_utils import (
+    run_tests,
+    skipIfPythonVersionMismatch,
+    TemporaryFileName,
+    TestCase,
+)
+
 
 class TestPythonTracer(TestCase):
-
     @skipIfPythonVersionMismatch(lambda major, minor, micro: major == 3 and minor == 12)
     def test_method_with_c_function(self):
-        micro = sys.version_info.micro
-
         class A:
             method_with_c_function = classmethod(repr)
 
         def get_key(x):
             A().method_with_c_function()
-            time.sleep(1)
+            time.sleep(1.2)
             return len(x)
 
         names = ["Alice", "Bob"]
 
-        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True) as prof:
+        with profile(
+            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True
+        )as prof:
             sorted(names, key=get_key)
 
         with TemporaryFileName(mode="w+") as fname:
@@ -31,19 +37,23 @@ class TestPythonTracer(TestCase):
                 events = json.load(f)["traceEvents"]
                 found = False
                 for event in events:
-                    if event.get("cat", "") == "python_function" and event.get("name", "") == "<built-in function sorted>":
+                    if (
+                        event.get("cat", "") == "python_function"
+                        and event.get("name", "") == "<built-in function sorted>"
+                    ):
                         duration = event.get("dur", 0)
-                        if (duration >= 2000000):
+                        if duration >= 2000000:
                             found = True
                             break
                 self.assertTrue(found)
 
     @skipIfPythonVersionMismatch(lambda major, minor, micro: major == 3 and minor == 12)
     def test_monitoring_callback(self):
-        from torch._vendor.packaging import version
         vi = sys.version_info
         from sys import monitoring
-        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True) as prof:
+        with profile(
+            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True
+        ):
             name = monitoring.get_tool(2)
             if vi.micro < 5:
                 self.assertEqual(name, "PyTorch Profiler")
