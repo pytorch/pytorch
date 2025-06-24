0bd1aa32cfd78c4694e023baa8837789fda27206
Ryan Guo <ryanguo99@meta.com>
2025-06-23 17:06:11 -0700

    Update
    [ghstack-poisoned]

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -2238,7 +2238,7 @@ def _reduction(
     return result
 
 
-def _make_copy_from_view(fn, return_none=False):
+def _make_copy_from_view(fn, return_none_on_out_variant=False):
     """
     Given a view function (e.g. torch.diagonal) generates its copy variant (e.g. torch.diagonal_copy)
     """
@@ -2249,7 +2249,7 @@ def _make_copy_from_view(fn, return_none=False):
     @wraps(fn)
     def _fn(*args, out=None, **kwargs):
         result = fn(*args, out=out, **kwargs)
-        if return_none:
+        if return_none_on_out_variant and out is not None:
             return None
         if out is not None:
             return result
@@ -6633,7 +6633,7 @@ squeeze_copy = _make_copy_from_view(aten.squeeze)
 permute_copy = _make_copy_from_view(aten.permute)
 t_copy = _make_copy_from_view(aten.t)
 transpose_copy = _make_copy_from_view(aten.transpose)
-unbind_copy = _make_copy_from_view(aten.unbind, return_none=True)
+unbind_copy = _make_copy_from_view(aten.unbind, return_none_on_out_variant=True)
 unsqueeze_copy = _make_copy_from_view(aten.unsqueeze)
 view_copy = _make_copy_from_view(aten.view)
 
