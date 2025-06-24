102ae27d2c3f0348734b1912c5fff714415dcccf
Manuel Candales <mcandales@meta.com>
2025-06-24 12:01:37 -0400

    [MPS] Revert cumsum/cumprod to MPSGraph implementation (#156708)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/156708
    Approved by: https://github.com/malfet
    
    (cherry picked from commit 2d7e6c6241971106a56073d7a53c7d1336b11a51)

--- torch/testing/_internal/common_mps.py
+++ torch/testing/_internal/common_mps.py
@@ -313,6 +313,13 @@ if torch.backends.mps.is_available():
             # The values of the sorted tensor match the CPU,
             # but in case of the returned indices this results in undefined behaviour.
             "sort": [torch.int8, torch.uint8, torch.bool, torch.float16],
+            # Unsupported dtypes
+            "cumsum": [torch.int64],
+            "cumprod": [torch.int64],
+            "cumulative_trapezoid": [torch.int64],
+            "masked.cumsum": [torch.int64],
+            "masked.cumprod": [torch.int64],
+            "linalg.vander": [torch.int64],
             # Fail with `Expected 1.0 but got nan.` for empty tensors
             # Caused by sample input at index 23: SampleInput(
             #     input=Tensor[size=(), device="mps:0", dtype=torch.float32],
