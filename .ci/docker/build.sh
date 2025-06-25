38d73978ce25fb17860c5393614366d87fd80b62
atalman <atalman@fb.com>
2025-06-25 16:32:37 -0700

    pull_changes

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -219,6 +219,17 @@ case "$tag" in
     UCC_COMMIT=${_UCC_COMMIT}
     TRITON=yes
     ;;
+  pytorch-linux-jammy-cuda12.9-cudnn9-py3-gcc9)
+    CUDA_VERSION=12.9
+    CUDNN_VERSION=9
+    ANACONDA_PYTHON_VERSION=3.10
+    GCC_VERSION=9
+    VISION=yes
+    KATEX=yes
+    UCX_COMMIT=${_UCX_COMMIT}
+    UCC_COMMIT=${_UCC_COMMIT}
+    TRITON=yes
+    ;;
   pytorch-linux-jammy-py3-clang12-onnx)
     ANACONDA_PYTHON_VERSION=3.9
     CLANG_VERSION=12
