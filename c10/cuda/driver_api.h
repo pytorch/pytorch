3b735034729993e00e1ffcfffce77dee9d6ebbe6
Frank Lin <eee4017@gmail.com>
2025-06-26 07:08:37 +0000

    remove compile-time version check

--- c10/cuda/driver_api.h
+++ c10/cuda/driver_api.h
@@ -20,30 +20,24 @@
     }                                                                      \
   } while (0)
 
-#if defined(CUDA_VERSION) && CUDA_VERSION >= 12030
-#define IF_CUDA_VERSION_GE_12030(x) x
-#else
-#define IF_CUDA_VERSION_GE_12030(x)
-#endif
-
-#define C10_LIBCUDA_DRIVER_API(_)                          \
-  _(cuDeviceGetAttribute, 12000)                           \
-  _(cuMemAddressReserve, 12000)                            \
-  _(cuMemRelease, 12000)                                   \
-  _(cuMemMap, 12000)                                       \
-  _(cuMemAddressFree, 12000)                               \
-  _(cuMemSetAccess, 12000)                                 \
-  _(cuMemUnmap, 12000)                                     \
-  _(cuMemCreate, 12000)                                    \
-  _(cuMemGetAllocationGranularity, 12000)                  \
-  _(cuMemExportToShareableHandle, 12000)                   \
-  _(cuMemImportFromShareableHandle, 12000)                 \
-  _(cuMemsetD32Async, 12000)                               \
-  _(cuStreamWriteValue32, 12000)                           \
-  _(cuGetErrorString, 12000)                               \
-  IF_CUDA_VERSION_GE_12030(_(cuMulticastAddDevice, 12030)) \
-  IF_CUDA_VERSION_GE_12030(_(cuMulticastBindMem, 12030))   \
-  IF_CUDA_VERSION_GE_12030(_(cuMulticastCreate, 12030))
+#define C10_LIBCUDA_DRIVER_API(_)          \
+  _(cuDeviceGetAttribute, 12000)           \
+  _(cuMemAddressReserve, 12000)            \
+  _(cuMemRelease, 12000)                   \
+  _(cuMemMap, 12000)                       \
+  _(cuMemAddressFree, 12000)               \
+  _(cuMemSetAccess, 12000)                 \
+  _(cuMemUnmap, 12000)                     \
+  _(cuMemCreate, 12000)                    \
+  _(cuMemGetAllocationGranularity, 12000)  \
+  _(cuMemExportToShareableHandle, 12000)   \
+  _(cuMemImportFromShareableHandle, 12000) \
+  _(cuMemsetD32Async, 12000)               \
+  _(cuStreamWriteValue32, 12000)           \
+  _(cuGetErrorString, 12000)               \
+  _(cuMulticastAddDevice, 12030)           \
+  _(cuMulticastBindMem, 12030)             \
+  _(cuMulticastCreate, 12030)
 
 #define C10_NVML_DRIVER_API(_)            \
   _(nvmlInit_v2)                          \
