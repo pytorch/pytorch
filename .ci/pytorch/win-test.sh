2fc5475f95d91dd373ce067281ed3e774c6c3906
Mark Saroufim <marksaroufim@gmail.com>
2025-06-26 16:13:32 -0700

    Update win-test.sh

--- .ci/pytorch/win-test.sh
+++ .ci/pytorch/win-test.sh
@@ -52,7 +52,7 @@ python -m pip install parameterized==0.8.1
 # Install pulp for testing ilps under torch\distributed\_tools
 python -m pip install pulp==2.9.0
 
-# Install expecttestt to merge https://github.com/pytorch/pytorch/pull/155308
+# Install expecttest to merge https://github.com/pytorch/pytorch/pull/155308
 python -m pip install expecttest==0.3.0
 
 run_tests() {
