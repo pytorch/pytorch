ba0b76a5d82cf7bf13ec00cd4b4f55aa8d61c628
Jane Xu <janeyx@meta.com>
2025-06-27 09:26:47 -0700

    Update base for Update on "chunk_size should always be int64_t for Foreach functors"
    See https://github.com/pytorch/pytorch/issues/156261#issuecomment-3002394773
    
    Testing is a valid q--it is pretty expensive to test such large tensors for all these ops.
    
    
    
    
    [ghstack-poisoned]

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -12,7 +12,7 @@ from numbers import Number
 import torch
 from torch.testing import make_tensor
 from torch.testing._comparison import default_tolerances
-from torch.testing._internal.common_cuda import TEST_MULTIGPU
+from torch.testing._internal.common_cuda import _get_torch_cuda_version, TEST_MULTIGPU
 from torch.testing._internal.common_device_type import (
     dtypes,
     instantiate_device_type_tests,
@@ -79,8 +79,13 @@ class ForeachFuncWrapper:
     def __call__(self, inputs, is_cuda, expect_fastpath, **kwargs):
         actual = None
         zero_size = kwargs.pop("zero_size", False)
+
+        # Skip profiler check for CUDA 12.6, 12.8 as the upgrade makes profiler results flaky
+        # https://github.com/pytorch/pytorch/issues/148681. TODO: ADD IT BACK!!!
+        skip_profiler_check = _get_torch_cuda_version() in [(12, 6), (12, 8)]
         if (
             is_cuda
+            and not skip_profiler_check
             and torch.autograd.kineto_available()
             and torch.profiler.ProfilerActivity.CUDA
             in torch.profiler.supported_activities()
@@ -91,11 +96,10 @@ class ForeachFuncWrapper:
                 torch.cuda.synchronize()
             keys = tuple([e.key for e in p.key_averages()])
             mta_called = any("multi_tensor_apply_kernel" in k for k in keys)
-            # Skip profiler check for CUDA 12.6, 12.8 as the upgrade makes profiler results flaky
-            # https://github.com/pytorch/pytorch/issues/148681
-            # assert mta_called == (expect_fastpath and (not zero_size)), (
-            #     f"{mta_called=}, {expect_fastpath=}, {zero_size=}, {self.func.__name__=}, {keys=}"
-            # )
+
+            assert mta_called == (expect_fastpath and (not zero_size)), (
+                f"{mta_called=}, {expect_fastpath=}, {zero_size=}, {self.func.__name__=}, {keys=}"
+            )
         else:
             actual = self.func(*inputs, **kwargs)
         if self.is_inplace:
