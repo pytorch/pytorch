4961df13687beab80a16d02bc9fe4e8a6a533569
Laith Sakka <lsakka@meta.com>
2025-06-27 21:58:58 -0700

    Update on "simplify max(1,x) to x when x known >=1"
    Creating contiguous strides creates an expression max(1, x). Often we know that x >= 1, in
     which case we should simplify max(1, x) to x.
    
    This appeared in two situations:
    1) An internal user complained about statically_known_true(x == max(1, x)) failing (internal link: https://fb.workplace.com/groups/1028545332188949/permalink/1232958568414290). 
    This https://github.com/pytorch/pytorch/pull/155938 won't be needed with this.
    
    3) Not simplifying the above could result in wrong ConstraintViolationErrors.
    Because we assume non-trival single arg guards shall evaporate see the logic at this line
    with this change we longer throw ConstraintViolationErrors with the program bellow 
    this is blocking landing this [PR](https://github.com/pytorch/pytorch/pull/155590) from landing
    internally. Due to internal export tests throwing ConstraintViolationErrors.
    
    
    ```
    x = torch.rand(10)
    torch._dynamo.mark_dynamic(x, 0, max=20, min=5)
    
    torch.compile(fullgraph=True, dynamic=True)
    def func(x):
        if max(1, (-1 + x.size()[0]//2)) == (-1+x.size()[0]//2):
            return x*400
        else:
            return (x*10)*100
    
    func(x)
    
    ```
    
    
    
    [ghstack-poisoned]

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -1863,8 +1863,7 @@ class TestDimConstraints(TestCase):
 
         @torch.compile(fullgraph=True)
         def func(x, v):
-            # test that statically_known_true(max(1,X))
-
+            # test that statically_known_true 
             if (v == 0 or v == 1) and not statically_known_true(
                 max(v, (-1 + x.size()[0] // 2)) == (-1 + x.size()[0] // 2)
             ):
