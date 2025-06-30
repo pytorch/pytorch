878fc9fb8ee93d23805aaf993096c7710c7a9503
Yu, Guangye <guangye.yu@intel.com>
2025-06-30 09:44:15 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/test/core/AllocatorConfig_test.cpp
+++ c10/test/core/AllocatorConfig_test.cpp
@@ -5,9 +5,9 @@
 using namespace c10::CachingAllocator;
 constexpr size_t kMB = 1024 * 1024ul;
 
-struct DeviceAllocatorConfig {
-  static DeviceAllocatorConfig& instance() {
-    static DeviceAllocatorConfig instance;
+struct ExtendedAllocatorConfig {
+  static ExtendedAllocatorConfig& instance() {
+    static ExtendedAllocatorConfig instance;
     return instance;
   }
 
@@ -40,7 +40,7 @@ struct DeviceAllocatorConfig {
 };
 
 REGISTER_ALLOCATOR_CONFIG_PARSE_HOOK([](const std::string& env) {
-  DeviceAllocatorConfig::instance().parseArgs(env);
+  ExtendedAllocatorConfig::instance().parseArgs(env);
 });
 
 TEST(AllocatorConfigTest, allocator_config_test) {
@@ -54,21 +54,26 @@ TEST(AllocatorConfigTest, allocator_config_test) {
       "device_specific_option_mb:64";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::max_split_size(), 40 * kMB);
-  EXPECT_EQ(AllocatorConfig::max_non_split_rounding_size(), 30 * kMB);
-  EXPECT_EQ(AllocatorConfig::garbage_collection_threshold(), 0.5);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(32 * kMB), 8);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(64 * kMB), 8);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(128 * kMB), 2);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(256 * kMB), 4);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(512 * kMB), 2);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(1024 * kMB), 4);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(2048 * kMB), 1);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(4096 * kMB), 1);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(8192 * kMB), 1);
-  EXPECT_EQ(AllocatorConfig::use_expandable_segments(), true);
-  EXPECT_EQ(AllocatorConfig::pinned_use_background_threads(), true);
-  EXPECT_EQ(DeviceAllocatorConfig::device_specific_option(), 64 * kMB);
+  EXPECT_EQ(AcceleratorAllocatorConfig::max_split_size(), 40 * kMB);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::max_non_split_rounding_size(), 30 * kMB);
+  EXPECT_EQ(AcceleratorAllocatorConfig::garbage_collection_threshold(), 0.5);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(32 * kMB), 8);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(64 * kMB), 8);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(128 * kMB), 2);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(256 * kMB), 4);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(512 * kMB), 2);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(1024 * kMB), 4);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(2048 * kMB), 1);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(4096 * kMB), 1);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(8192 * kMB), 1);
+  EXPECT_EQ(AcceleratorAllocatorConfig::use_expandable_segments(), true);
+  EXPECT_EQ(AcceleratorAllocatorConfig::pinned_use_background_threads(), true);
+  EXPECT_EQ(ExtendedAllocatorConfig::device_specific_option(), 64 * kMB);
 
   env =
       "max_split_size_mb:20,"
@@ -76,37 +81,43 @@ TEST(AllocatorConfigTest, allocator_config_test) {
       "garbage_collection_threshold:0.8";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::max_split_size(), 20 * kMB);
-  EXPECT_EQ(AllocatorConfig::max_non_split_rounding_size(), 40 * kMB);
-  EXPECT_EQ(AllocatorConfig::garbage_collection_threshold(), 0.8);
+  EXPECT_EQ(AcceleratorAllocatorConfig::max_split_size(), 20 * kMB);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::max_non_split_rounding_size(), 40 * kMB);
+  EXPECT_EQ(AcceleratorAllocatorConfig::garbage_collection_threshold(), 0.8);
 
   // roundup_power2_divisions knob array syntax
   env = "roundup_power2_divisions:[128:8,256:16,512:1,2048:8,>:2]";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(64 * kMB), 8);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(128 * kMB), 8);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(256 * kMB), 16);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(512 * kMB), 1);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(1024 * kMB), 0);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(2048 * kMB), 8);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(4096 * kMB), 2);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(64 * kMB), 8);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(128 * kMB), 8);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(256 * kMB), 16);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(512 * kMB), 1);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(1024 * kMB), 0);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(2048 * kMB), 8);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(4096 * kMB), 2);
 
   // roundup_power2_divisions single value syntax for backward compatibility
   env = "roundup_power2_divisions:4";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(64 * kMB), 4);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(256 * kMB), 4);
-  EXPECT_EQ(AllocatorConfig::roundup_power2_divisions(2048 * kMB), 4);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(64 * kMB), 4);
+  EXPECT_EQ(AcceleratorAllocatorConfig::roundup_power2_divisions(256 * kMB), 4);
+  EXPECT_EQ(
+      AcceleratorAllocatorConfig::roundup_power2_divisions(2048 * kMB), 4);
 
   env = "expandable_segments:False,";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::use_expandable_segments(), false);
+  EXPECT_EQ(AcceleratorAllocatorConfig::use_expandable_segments(), false);
 
   env = "pinned_use_background_threads:False";
   c10::CachingAllocator::setAllocatorSettings(env);
   EXPECT_EQ(c10::CachingAllocator::getAllocatorSettings(), env);
-  EXPECT_EQ(AllocatorConfig::pinned_use_background_threads(), false);
+  EXPECT_EQ(AcceleratorAllocatorConfig::pinned_use_background_threads(), false);
 }
