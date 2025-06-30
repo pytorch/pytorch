df51add9aca075b77644f73837b2b0f5bf5b0664
Yu, Guangye <guangye.yu@intel.com>
2025-06-30 14:38:19 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/core/CachingDeviceAllocator.h>
+#include <c10/cuda/CUDAAllocatorConfig.h>
 #include <c10/cuda/CUDAGraphsC10Utils.h>
 #include <c10/cuda/CUDAMacros.h>
 #include <c10/cuda/CUDAStream.h>
@@ -50,8 +51,7 @@ namespace c10::cuda::CUDACachingAllocator {
 // Preserved only for BC reasons
 // NOLINTNEXTLINE(misc-unused-using-decls)
 using c10::CachingDeviceAllocator::DeviceStats;
-
-extern const size_t kLargeBuffer;
+using c10::CachingAllocator::kLargeBuffer;
 
 typedef std::shared_ptr<GatheredContext> (*CreateContextFn)();
 
