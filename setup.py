e8e600f8e54a8b13b902ed662d92592751620e42
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-07-01 00:14:44 +0800

    Update
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -1314,11 +1314,12 @@ def main() -> None:
                 "lib/*.lib",
             ]
         )
-        aotriton_image_path = TORCH_LIB_DIR / "aotriton.images"
-        aks2_files: list[str] = []
-        for file in filter(lambda p: p.is_file(), aotriton_image_path.glob("**")):
-            subpath = file.relative_to(aotriton_image_path)
-            aks2_files.append(os.path.join("lib/aotriton.images", subpath))
+        aotriton_image_path = os.path.join(TORCH_LIB_DIR, "aotriton.images")
+        aks2_files = []
+        for root, dirs, files in os.walk(aotriton_image_path):
+            subpath = os.path.relpath(root, start=aotriton_image_path)
+            for fn in files:
+                aks2_files.append(os.path.join("lib/aotriton.images", subpath, fn))
         torch_package_data += aks2_files
     if get_cmake_cache_vars()["USE_TENSORPIPE"]:
         torch_package_data.extend(
