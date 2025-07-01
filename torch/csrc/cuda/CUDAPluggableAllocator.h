c3f73c375993b536fdb80ede8aa40a89c571a33a
Jeff Daily <jeff.daily@amd.com>
2025-07-01 00:56:19 +0000

    fix build

--- torch/csrc/cuda/CUDAPluggableAllocator.h
+++ torch/csrc/cuda/CUDAPluggableAllocator.h
@@ -116,7 +116,7 @@ struct TORCH_CUDA_CPP_API CUDAPluggableAllocator
   void cacheInfo(c10::DeviceIndex device, size_t* largestBlock) override;
   void* getBaseAllocation(void* ptr, size_t* size) override;
 
-  void recordStream(const c10::DataPtr&, streamType stream) override;
+  void recordStream(const c10::DataPtr&, c10::cuda::CUDAStream stream) override;
 
   c10::CachingDeviceAllocator::DeviceStats getDeviceStats(
       c10::DeviceIndex device) override;
