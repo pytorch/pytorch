d401be4f7aab80c4d66f8074d0ff8d844263aa09
Xinya Zhang <Xinya.Zhang@amd.com>
2025-07-02 13:42:26 -0500

    Enable FA/ME UT on gfx950 for ROCM >= 6.5

--- torch/testing/_internal/common_cuda.py
+++ torch/testing/_internal/common_cuda.py
@@ -48,6 +48,9 @@ def CDNA2OrLater():
 def evaluate_platform_supports_flash_attention():
     if TEST_WITH_ROCM:
         arch_list = ["gfx90a", "gfx942", "gfx1100"]
+        version = _get_torch_rocm_version()
+        if version >= (6, 5):
+            arch_list += ["gfx950"]
         return evaluate_gfx_arch_within(arch_list)
     if TEST_CUDA:
         return not IS_WINDOWS and SM80OrLater
@@ -56,6 +59,9 @@ def evaluate_platform_supports_flash_attention():
 def evaluate_platform_supports_efficient_attention():
     if TEST_WITH_ROCM:
         arch_list = ["gfx90a", "gfx942", "gfx1100"]
+        version = _get_torch_rocm_version()
+        if version >= (6, 5):
+            arch_list += ["gfx950"]
         return evaluate_gfx_arch_within(arch_list)
     if TEST_CUDA:
         return True
