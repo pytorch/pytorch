d2aa7fc135f17f7132f62ff4920912e5478c0794
Catherine Lee <csl@fb.com>
2025-07-02 10:10:15 -0700

    tc

--- .github/scripts/test_delete_old_branches.py
+++ .github/scripts/test_delete_old_branches.py
@@ -1,6 +1,10 @@
+import os
 import unittest
 from datetime import datetime
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
+
+
+os.environ["GITHUB_TOKEN"] = "test_token"
 
 from delete_old_branches import delete_old_tags
 
@@ -8,7 +12,9 @@ from delete_old_branches import delete_old_tags
 @patch("delete_old_branches.delete_branch")
 @patch("gitutils.GitRepo._run_git")
 class TestDeleteTag(unittest.TestCase):
-    def test_delete_tag(self, mock_run_git, mock_delete_tag):
+    def test_delete_tag(
+        self, mock_run_git: "MagicMock", mock_delete_tag: "MagicMock"
+    ) -> None:
         for tag in [
             "ciflow/branch/12345",
             "ciflow/commitsha/1234567890abcdef1234567890abcdef12345678",
@@ -30,7 +36,9 @@ class TestDeleteTag(unittest.TestCase):
             delete_old_tags()
             mock_delete_tag.assert_not_called()
 
-    def test_do_not_delete_tag(self, mock_run_git, mock_delete_tag):
+    def test_do_not_delete_tag(
+        self, mock_run_git: "MagicMock", mock_delete_tag: "MagicMock"
+    ) -> None:
         for tag in [
             "ciflow/doesntseemtomatch",
             "trunk/doesntseemtomatch",
