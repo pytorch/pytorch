88cebeb7f5186a317dde72c5979d502200ca4d56
eqy <eddiey@nvidia.com>
2025-07-03 11:32:26 -0700

    lint

--- test/dynamo/test_activation_checkpointing.py
+++ test/dynamo/test_activation_checkpointing.py
@@ -1369,16 +1369,35 @@ Non-primal fwd outputs from model w/o backward hook: {mod_no_hook_fwd_outputs_no
         op1 = torch.ops.aten._scaled_dot_product_flash_attention.default
         op2 = torch.ops.aten._scaled_dot_product_cudnn_attention.default
         self.assertTrue(
-            count_ops(fwd_graph, [], freq=1, op=op1,) or
-            count_ops(fwd_graph, [], freq=1, op=op2,)
+            count_ops(
+                fwd_graph,
+                [],
+                freq=1,
+                op=op1,
+            )
+            or count_ops(
+                fwd_graph,
+                [],
+                freq=1,
+                op=op2,
+            )
         )
         bwd_graph = aot_graphs[1]
         # Check that sin is not recomputed in the backward graph - checks percolate tags
         self.assertTrue(count_ops(bwd_graph, [], freq=0, op=torch.ops.aten.sin.default))
         # Check that the sdpa op is recomputed in the backward graph
         self.assertTrue(
-            count_ops(bwd_graph, [], freq=1, op=op1,) or
-            count_ops(bwd_graph, [], freq=1, op=op2,)
+            count_ops(
+                bwd_graph,
+                [],
+                freq=1,
+                op=op1,
+            ) or count_ops(
+                bwd_graph,
+                [],
+                freq=1,
+                op=op2,
+            )
         )
 
     @requires_distributed()
