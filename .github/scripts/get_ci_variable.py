e3d80a0746246955e6e5c69cb32bf0e34b788834
Andrey Talman <atalman@meta.com>
2025-07-04 13:11:37 -0400

    simplify

--- .github/scripts/get_ci_variable.py
+++ .github/scripts/get_ci_variable.py
@@ -15,11 +15,6 @@ def main(args: list[str]) -> None:
         help="get cuda stable version",
     )
     parser.add_argument(
-        "--cuda-aarch64-version",
-        action="store_true",
-        help="get cuda aarch64 version",
-    )
-    parser.add_argument(
         "--min-python-version",
         action="store_true",
         help="get min supported python version",
@@ -27,8 +22,6 @@ def main(args: list[str]) -> None:
     options = parser.parse_args(args)
     if options.cuda_stable_version:
         return print(generate_binary_build_matrix.CUDA_STABLE)
-    if options.cuda_aarch64_version:
-        return print(generate_binary_build_matrix.CUDA_AARCH64_ARCHES[0].removesuffix("-aarch64"))
     if options.min_python_version:
         return print(generate_binary_build_matrix.FULL_PYTHON_VERSIONS[0])
 
