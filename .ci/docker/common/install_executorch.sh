52c765b60a0330f420caa197ce63c7cd648fa490
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-03 17:26:46 -0700

    Update install_executorch.sh

--- .ci/docker/common/install_executorch.sh
+++ .ci/docker/common/install_executorch.sh
@@ -36,26 +36,27 @@ install_conda_dependencies() {
 }
 
 install_pip_dependencies() {
-  pushd executorch/.ci/docker
-  pip install -r requirements-ci.txt
+  pushd executorch
+  as_jenkins bash install_executorch.sh
 
-  # # A workaround, ExecuTorch has moved to numpy 2.0 which is not compatible with the current
-  # # numba and scipy version used in PyTorch CI
-  # conda_run pip uninstall -y numba scipy
+  pushd .ci/docker
+  pip install -r requirements-ci.txt
+  popd
+
+  # A workaround, ExecuTorch has moved to numpy 2.0 which is not compatible with the current
+  # numba and scipy version used in PyTorch CI
+  conda_run pip uninstall -y numba scipy
 
   popd
 }
 
 setup_executorch() {
   pushd executorch
-  as_jenkins PYTHON_EXECUTABLE=python \
-  CMAKE_ARGS="-DEXECUTORCH_BUILD_EXECUTOR_RUNNER=ON -DEXECUTORCH_BUILD_TESTS=ON" \
-  .ci/scripts/setup-linux.sh "$@"
 
-  # A workaround, ExecuTorch has moved to numpy 2.0 which is not compatible with the current
-  # numba and scipy version used in PyTorch CI
-  conda_run pip uninstall -y numba scipy
+  export PYTHON_EXECUTABLE=python
+  export CMAKE_ARGS="-DEXECUTORCH_BUILD_PYBIND=ON -DEXECUTORCH_BUILD_XNNPACK=ON -DEXECUTORCH_BUILD_KERNELS_QUANTIZED=ON -DEXECUTORCH_BUILD_EXECUTOR_RUNNER=ON -DEXECUTORCH_BUILD_TESTS=ON"
 
+  as_jenkins .ci/scripts/setup-linux.sh --build-tool cmake || true
   popd
 }
 
