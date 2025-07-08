fbc9a7567f455bc429adba9d01f8f16e657eccd9
nullplay <jaeyeon@mit.edu>
2025-07-08 00:01:46 -0400

    lint and fix

--- test/inductor/test_native_matmul.py
+++ test/inductor/test_native_matmul.py
@@ -2,19 +2,12 @@
 
 import os
 import sys
-import unittest
 
 import torch
-from torch import nn
 from torch._dynamo.utils import same
-from torch._inductor.test_case import run_tests, TestCase, config
+from torch._inductor.test_case import run_tests, TestCase
 from torch._inductor.utils import run_and_get_triton_code
-from torch.testing import FileCheck
-from torch.testing._internal.inductor_utils import (
-    GPU_TYPE,
-    HAS_GPU,
-    requires_cuda_with_enough_memory,
-)
+from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_GPU
 
 
 # Make the helper files in test/ importable
@@ -27,50 +20,50 @@ from torch._inductor import config as inductor_config
 
 aten = torch.ops.aten
 
+
 @inductor_config.patch(
-    {"triton.enable_native_matmul": True,
-     "coordinate_descent_tuning": False}
+    {"triton.enable_native_matmul": True, "coordinate_descent_tuning": False}
 )
 class TestTritonDotReduction(TestCase):
     def test_matmul(self):
-        def f(x,y):
-            z = x @ y 
-            return z 
+        def f(x, y):
+            z = x @ y
+            return z
 
-        M, K, N = 128, 128, 128 
+        M, K, N = 128, 128, 128
         x = rand_strided((M, K), (K, 1), device=GPU_TYPE)
         y = rand_strided((K, N), (N, 1), device=GPU_TYPE)
 
         compiled = torch.compile(f)
-        actual = compiled(x,y)
-        expect = f(x,y)
+        actual = compiled(x, y)
+        expect = f(x, y)
         self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
-        
-        code = run_and_get_triton_code(compiled, x,y)
+
+        code = run_and_get_triton_code(compiled, x, y)
         lines = [line for line in code.split("\n") if "tl.dot" in line]
         assert len(lines) == 1
 
     @inductor_config.patch({"triton.codegen_upcast_to_fp32": False})
     def test_matmul_fp16(self):
-        def f(x,y):
-            z = x @ y 
-            return z 
+        def f(x, y):
+            z = x @ y
+            return z
 
         M, K, N = 128, 128, 128
-        x = rand_strided((M, K), (K, 1), dtype = torch.float16, device=GPU_TYPE)
-        y = rand_strided((K, N), (N, 1), dtype = torch.float16, device=GPU_TYPE)
+        x = rand_strided((M, K), (K, 1), dtype=torch.float16, device=GPU_TYPE)
+        y = rand_strided((K, N), (N, 1), dtype=torch.float16, device=GPU_TYPE)
 
         compiled = torch.compile(f)
-        actual = compiled(x,y)
-        expect = f(x,y)
+        actual = compiled(x, y)
+        expect = f(x, y)
         self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
-        
-        code = run_and_get_triton_code(compiled, x,y)
+
+        code = run_and_get_triton_code(compiled, x, y)
         lines = [line for line in code.split("\n") if "tl.dot" in line]
         assert len(lines) == 1
 
     def test_mm_add(self):
-        def f(x,y,z,w):
+        def f(x, y, z, w):
             return x @ y + z @ w
 
         M, K, N = 128, 128, 128
@@ -80,53 +73,51 @@ class TestTritonDotReduction(TestCase):
         z = rand_strided((K, N), (N, 1), device=GPU_TYPE)
 
         compiled = torch.compile(f)
-        actual = compiled(x,y,z,w)
-        expect = f(x,y,z,w)
+        actual = compiled(x, y, z, w)
+        expect = f(x, y, z, w)
         self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
-        
-        code = run_and_get_triton_code(compiled, x,y,z,w)
+
+        code = run_and_get_triton_code(compiled, x, y, z, w)
         lines = [line for line in code.split("\n") if "tl.dot" in line]
         assert len(lines) == 2
 
     def test_mm_complex(self):
-        def f(x,y,z,w):
-            return x[z] @ y + w + 3 
+        def f(x, y, z, w):
+            return x[z] @ y + w + 3
 
         M, K, N = 128, 128, 128
         x = rand_strided((M, K), (K, 1), device=GPU_TYPE)
         y = rand_strided((K, N), (N, 1), device=GPU_TYPE)
-        
+
         z = torch.randint(M, (M, K), dtype=torch.long, device=GPU_TYPE)
         w = rand_strided((M, N), (N, 1), device=GPU_TYPE)
 
         compiled = torch.compile(f)
-        actual = compiled(x,y,z,w)
-        expect = f(x,y,z,w)
+        actual = compiled(x, y, z, w)
+        expect = f(x, y, z, w)
         self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
-        
-        code = run_and_get_triton_code(compiled, x,y,z,w)
-        lines = [line for line in code.split("\n") if "tl.dot" in line]
-        assert len(lines) == 1 
 
-
-    def test_batchmatmul(self):
-        def f(x,y):
-            z = torch.bmm(x,y) 
-            return z 
-
-        B, M, K, N = 256, 128, 128, 128
-        x = rand_strided((B, M, K), (M*K, K, 1), device=GPU_TYPE)
-        y = rand_strided((B, K, N), (K*N, N, 1), device=GPU_TYPE)
-
-        compiled = torch.compile(f)
-        actual = compiled(x,y)
-        expect = f(x,y)
-        self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
-        
-        code = run_and_get_triton_code(compiled, x,y)
+        code = run_and_get_triton_code(compiled, x, y, z, w)
         lines = [line for line in code.split("\n") if "tl.dot" in line]
         assert len(lines) == 1
 
+    def test_batchmatmul(self):
+        def f(x, y):
+            z = torch.bmm(x, y)
+            return z
+
+        B, M, K, N = 256, 128, 128, 128
+        x = rand_strided((B, M, K), (M * K, K, 1), device=GPU_TYPE)
+        y = rand_strided((B, K, N), (K * N, N, 1), device=GPU_TYPE)
+
+        compiled = torch.compile(f)
+        actual = compiled(x, y)
+        expect = f(x, y)
+        self.assertTrue(same(expect, actual, tol=1e-3), f"{expect=}\n{actual=}\n")
+
+        code = run_and_get_triton_code(compiled, x, y)
+        lines = [line for line in code.split("\n") if "tl.dot" in line]
+        assert len(lines) == 1
 
 
 if HAS_GPU:
