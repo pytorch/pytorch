2d063243864a6665b44ac9bb79eb0fcc7fc0dc36
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-07-08 01:04:18 -0700

    [simple_fsdp] Port fx pass to bucket reduce_scatters
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -388,6 +388,12 @@ bucket_all_gathers_fx: Literal["none", "all", "only_fsdp"] = "none"
 # By default torch._inductor.fx_passes.bucketing.bucket_size_determinator is used
 bucket_all_gathers_fx_bucket_size_determinator: Optional[Callable[[int], int]] = None
 
+bucket_reduce_scatters_fx: Literal["none", "all"] = "none"
+# By default torch._inductor.fx_passes.bucketing.bucket_size_determinator is used
+bucket_reduce_scatters_fx_bucket_size_determinator: Optional[Callable[[int], int]] = (
+    None
+)
+
 # runtime estimation function for ops
 # for built-in estimation function, pass in "default"; for user-defined estimation function, pass in the function handle
 estimate_op_runtime = "default"
