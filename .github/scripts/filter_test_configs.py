19070dad99038f944816b56b99663a32d1eb2a64
xinan.lin <xinan.lin@intel.com>
2025-07-08 11:00:39 -0700

    [RELEASE 2.8] Release only changes (#156728)
    * [RELEASE 2.8] Release only changes
    
    * test
    
    * tests_files
    
    * docker
    
    [ghstack-poisoned]

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -40,9 +40,9 @@ SUPPORTED_PERIODICAL_MODES: dict[str, Callable[[Optional[str]], bool]] = {
 }
 
 # The link to the published list of disabled jobs
-DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json"
+DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json?versionId=HnkH0xQWnnsoeMsSIVf9291NE5c4jWSa"
 # and unstable jobs
-UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json"
+UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json?versionId=iP_F8gBs60PfOMAJ8gnn1paVrzM1WYsK"
 
 # Some constants used to handle disabled and unstable jobs
 JOB_NAME_SEP = "/"
