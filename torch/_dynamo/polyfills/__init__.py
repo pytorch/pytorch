84a08700e6a4fe3e8232460a3837dfc1b5f5369a
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-08 11:57:43 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/polyfills/__init__.py
+++ torch/_dynamo/polyfills/__init__.py
@@ -88,6 +88,18 @@ def accumulate_grad(x, new_grad):
         x.grad.add_(new_grad_strided)
 
 
+def iter_sequence_protocol(obj):
+    if hasattr(obj, "__getitem__"):
+        i = 0
+        while True:
+            try:
+                # Can this execute user code?
+                yield obj.__getitem__(i)
+                i += 1
+            except IndexError:
+                break
+
+
 # This mirrors
 # https://github.com/python/cpython/blob/a1c52d1265c65bcf0d9edf87e143843ad54f9b8f/Objects/listobject.c#L3352-L3413
 def list_cmp(op: Callable[[Any, Any], bool], left: Sequence[Any], right: Sequence[Any]):
@@ -243,6 +255,10 @@ def mapping_get(obj, key, value=None):
         return obj.__getitem__(key)
     except KeyError:
         return value
+
+
+def assert_sequence_equal(self_, seq1, seq2, msg=None, seq_type=None):
+    self_.assertTrue(seq1 == seq2, msg)
 
 
 def instantiate_user_defined_class_object(cls, /, *args, **kwargs):
