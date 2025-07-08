ce4d91b94dd58389ca3e79f7990c458226c1aa58
Michelle Madubuike <michellecmadubuike@gmail.com>
2025-07-07 23:41:47 -0500

    Used wrong Binomial function in test. Fixed this and changed test location

---
+++ test/test_binomial.py
@@ -0,0 +1,32 @@
+# Owner(s): ["module: cpp"]
+import torch
+from torch.testing._internal.common_utils import run_tests, TestCase
+
+
+class BinomialDatatypeErrors(TestCase):
+    def test_binomial_dtype_errors(self):
+        dtypes = [torch.int, torch.long, torch.short]
+
+        for count_dtype in dtypes:
+            total_count = torch.tensor([10, 10], dtype=count_dtype)
+            total_prob = torch.tensor([0.5, 0.5], dtype=torch.float)
+
+            with self.assertRaisesRegex(
+                RuntimeError,
+                "binomial only supports floating-point dtypes for count.*",
+            ):
+                torch.binomial(total_count, total_prob)
+
+        for prob_dtype in dtypes:
+            total_count = torch.tensor([10, 10], dtype=torch.float)
+            total_prob = torch.tensor([0.5, 0.5], dtype=prob_dtype)
+
+            with self.assertRaisesRegex(
+                RuntimeError,
+                "binomial only supports floating-point dtypes for prob.*",
+            ):
+                torch.binomial(total_count, total_prob)
+
+
+if __name__ == "__main__":
+    run_tests()
