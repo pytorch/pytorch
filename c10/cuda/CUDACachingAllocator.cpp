1937a411726285a7914a6cd19cf2c839e19675e5
Yu, Guangye <guangye.yu@intel.com>
2025-07-08 11:25:20 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/cuda/CUDACachingAllocator.cpp
+++ c10/cuda/CUDACachingAllocator.cpp
@@ -3741,7 +3741,7 @@ class NativeCachingAllocator : public CUDAAllocator {
     md.max_split_size = AcceleratorAllocatorConfig::max_split_size();
     md.pinned_num_register_threads =
         CUDAAllocatorConfig::pinned_num_register_threads();
-    md.expandable_segments = CUDAAllocatorConfig::use_expandable_segments();
+    md.expandable_segments = CUDAAllocatorConfig::expandable_segments();
     md.release_lock_on_malloc =
         CUDAAllocatorConfig::release_lock_on_cudamalloc();
     md.pinned_use_host_register =
