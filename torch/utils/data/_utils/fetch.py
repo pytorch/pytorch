8d903d01063dd94d852f1f93ce8cd09949e728a6
Kai Zhang <kz298@cornell.edu>
2025-07-07 17:17:46 -0700

    add asyncio and multi-threading

--- torch/utils/data/_utils/fetch.py
+++ torch/utils/data/_utils/fetch.py
@@ -4,16 +4,56 @@ r"""Contains definitions of the methods used by the _BaseDataLoaderIter to fetch
 This logic is shared in both single- and multi-processing data loading.
 """
 
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 
 class _BaseDatasetFetcher:
-    def __init__(self, dataset, auto_collation, collate_fn, drop_last):
+    def __init__(self, dataset, auto_collation, collate_fn, drop_last, num_threads=1):
         self.dataset = dataset
         self.auto_collation = auto_collation
         self.collate_fn = collate_fn
         self.drop_last = drop_last
+        self.num_threads = num_threads
 
-    def fetch(self, possibly_batched_index):
-        raise NotImplementedError
+        if self.num_threads > 1:
+            self._executor = ThreadPoolExecutor(self.num_threads)
+            self._loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self._loop)
+
+    def _run_async(self, indices, fetch_fn):
+        async def _worker(task_queue, result_queue):
+            while not task_queue.empty():
+                index = await task_queue.get()
+                try:
+                    result = await self._loop.run_in_executor(
+                        self._executor, fetch_fn, index
+                    )
+                    await result_queue.put((index, result))
+                except Exception as e:
+                    print(f"Exception during fetch: {e}")
+                finally:
+                    task_queue.task_done()
+
+        async def _run(indices):
+            task_queue = asyncio.Queue()
+            result_queue = asyncio.Queue()
+            for idx in indices:
+                await task_queue.put(idx)
+
+            workers = [
+                asyncio.create_task(_worker(task_queue, result_queue))
+                for _ in range(self.num_threads)
+            ]
+            await asyncio.gather(*workers)
+
+            results = {}
+            while not result_queue.empty():
+                idx, value = await result_queue.get()
+                results[idx] = value
+            return [results[i] for i in indices if i in results]
+
+        return self._loop.run_until_complete(_run(indices))
 
 
 class _IterableDatasetFetcher(_BaseDatasetFetcher):
@@ -22,18 +62,27 @@ class _IterableDatasetFetcher(_BaseDatasetFetcher):
         self.dataset_iter = iter(dataset)
         self.ended = False
 
+    def _fetch_one(self, _):
+        return next(self.dataset_iter)
+
     def fetch(self, possibly_batched_index):
         if self.ended:
             raise StopIteration
 
         if self.auto_collation:
             data = []
-            for _ in possibly_batched_index:
+            if self.num_threads > 1:
                 try:
-                    data.append(next(self.dataset_iter))
+                    data = self._run_async(possibly_batched_index, self._fetch_one)
                 except StopIteration:
                     self.ended = True
-                    break
+            else:
+                for _ in possibly_batched_index:
+                    try:
+                        data.append(next(self.dataset_iter))
+                    except StopIteration:
+                        self.ended = True
+                        break
             if len(data) == 0 or (
                 self.drop_last and len(data) < len(possibly_batched_index)
             ):
@@ -44,10 +93,15 @@ class _IterableDatasetFetcher(_BaseDatasetFetcher):
 
 
 class _MapDatasetFetcher(_BaseDatasetFetcher):
+    def _fetch_one(self, idx):
+        return self.dataset[idx]
+
     def fetch(self, possibly_batched_index):
         if self.auto_collation:
             if hasattr(self.dataset, "__getitems__") and self.dataset.__getitems__:
                 data = self.dataset.__getitems__(possibly_batched_index)
+            elif self.num_threads > 1:
+                data = self._run_async(possibly_batched_index, self._fetch_one)
             else:
                 data = [self.dataset[idx] for idx in possibly_batched_index]
         else:
