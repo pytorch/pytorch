91335effab5974e72af5164bff5b0f541e553e4c
charlifu <charlifu@amd.com>
2025-07-09 18:00:58 +0000

    fix lint
    Signed-off-by: charlifu <charlifu@amd.com>

--- test/inductor/test_auto_functionalize.py
+++ test/inductor/test_auto_functionalize.py
@@ -445,14 +445,17 @@ def forward(self, arg0_1: "f32[3][1]cpu", arg1_1: "f32[3][1]cpu", arg2_1: "f32[3
             graph = "\n".join(log_stream.getvalue().strip().split("\n")[4:]).strip()
         return [aot_eager_args, result, graph]
 
-    def run_inductor(self, f, orig_args, _dynamic=False,
-                     log_module="torch._inductor.compile_fx",
-                     log_function="post_grad_graphs"):
+    def run_inductor(
+        self,
+        f,
+        orig_args,
+        _dynamic=False,
+        log_module="torch._inductor.compile_fx",
+        log_function="post_grad_graphs",
+    ):
         compiled_args = pytree.tree_map_only(torch.Tensor, torch.clone, orig_args)
 
-        log_stream, ctx = logs_to_string(
-            log_module, log_function
-        )
+        log_stream, ctx = logs_to_string(log_module, log_function)
         result = None
         with ctx():
             result = torch.compile(
@@ -1751,19 +1754,21 @@ def forward(self, arg0_1: "f32[2][1]cpu"):
                 pass
 
             def func(x, w):
-                a = torch.empty_like(x) # buf0
-                b = torch.empty_like(x) # buf1
-                torch.ops.mylib.foo(a, b, x) # buf2, buf3, buf4
-                c = torch.mm(a, w) # buf5
-                torch.ops.mylib.foo(c, b, x) # buf6, buf7, buf8
+                a = torch.empty_like(x)  # buf0
+                b = torch.empty_like(x)  # buf1
+                torch.ops.mylib.foo(a, b, x)  # buf2, buf3, buf4
+                c = torch.mm(a, w)  # buf5
+                torch.ops.mylib.foo(c, b, x)  # buf6, buf7, buf8
                 return c
 
             input = torch.rand(2, 2)
             weight = torch.rand(2, 2)
             [inductor_args, output, graph_inductor] = self.run_inductor(
-                func, [input, weight], False,
+                func,
+                [input, weight],
+                False,
                 "torch._inductor.scheduler",
-                "compute_dependencies"
+                "compute_dependencies",
             )
             name_to_users = eval(graph_inductor)
             self.assertNotEqual(name_to_users["buf1"], name_to_users["buf5"])
