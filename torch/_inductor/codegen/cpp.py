2864afc12c8c549e3ed59d98dac19efaa169ac02
thenumberouscode <dream20151224@163.com>
2025-07-09 17:32:33 +0800

    In cpp.py, the store_reduction for CppVecKernel hardcodes the dtype to torch.int64, but it can actually be torch.int32. This could lead to incorrect conversions in the C++ kernel that's generated.

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -2989,7 +2989,7 @@ class CppVecKernel(CppKernel):
         dtype = (
             (out_dtype if out_dtype == torch.double else torch.float)
             if out_dtype.is_floating_point
-            else torch.int64
+            else out_dtype
         )
         out_num_vectors = V.kernel._get_num_vectors(out_dtype)
         src_num_vectors = V.kernel._get_num_vectors(dtype)
