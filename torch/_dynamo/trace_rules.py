1a461ed04f0e770a70558329bf7f60dfa469a705
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-10 15:01:23 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -3143,6 +3143,7 @@ def _builtin_function_ids() -> dict[int, str]:
         {
             id(cast): "typing.cast",
             id(copy.deepcopy): "copy.deepcopy",
+            id(builtins.__build_class__): "builtins.__build_class__",
         }
     )
     return rv
