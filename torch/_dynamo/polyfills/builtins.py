09b9457248ba14f667580b638c8c11df0dde2ea0
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-10 15:44:25 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/polyfills/builtins.py
+++ torch/_dynamo/polyfills/builtins.py
@@ -81,16 +81,16 @@ class _CallableIterator:
         return r
 
 
-class _INITIAL_MISSING:
+class _SENTINEL_MISSING:
     pass
 
 
 # TODO(guilhermeleobas): use substitute_in_graph for iter()
-def iter_(fn_or_iterable, sentinel=_INITIAL_MISSING):  # type: ignore[no-untyped-def]
+def iter_(fn_or_iterable, sentinel=_SENTINEL_MISSING, /):  # type: ignore[no-untyped-def]
     # Without a second argument, object must be a collection object which supports
     # the iterable (__iter__) or the sequence protocol (__getitem__ with an integer
     # starting at 0)
-    if sentinel is _INITIAL_MISSING:
+    if sentinel is _SENTINEL_MISSING:
         iterable = fn_or_iterable
         if hasattr(iterable, "__iter__"):
             return iterable.__iter__()
