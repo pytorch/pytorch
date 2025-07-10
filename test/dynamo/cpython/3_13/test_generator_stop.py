7fe8226a8bbe8cebf129c0daf6edf1ba0a2877d7
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-10 13:34:54 -0300

    Update
    [ghstack-poisoned]

--- test/dynamo/cpython/3_13/test_generator_stop.py
+++ test/dynamo/cpython/3_13/test_generator_stop.py
@@ -6,6 +6,9 @@ from __future__ import generator_stop
 # ruff: noqa
 # flake8: noqa
 
+# Test copied from
+# https://raw.githubusercontent.com/python/cpython/refs/heads/3.13/Lib/test/test_generator_stop.py
+
 import sys
 import torch
 import torch._dynamo.test_case
--- test/dynamo/cpython/3_13/test_list.py
+++ test/dynamo/cpython/3_13/test_list.py
@@ -4,6 +4,9 @@
 # ruff: noqa
 # flake8: noqa
 
+# Test copied from
+# https://raw.githubusercontent.com/python/cpython/refs/heads/3.13/Lib/test/test_list.py
+
 import sys
 import torch
 import torch._dynamo.test_case
