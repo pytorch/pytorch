1fe079bd8c22203a0c3a7ed145c263cc1347832a
eqy <eddiey@nvidia.com>
2025-07-10 10:32:58 -0700

    Update torch/cuda/__init__.py
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -238,7 +238,7 @@ def _sleep(cycles):
 
 def _extract_arch_version(arch_string: str) -> int:
     """Extracts the architecture string from a CUDA version"""
-    base = arch_string.split("_")[1]
+    base = arch_string.split("_", maxsplit=2)[1]
     base = base.removesuffix("a").removesuffix("f")
     return int(base)
 
