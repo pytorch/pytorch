0687a4d505c7726e01dd8c6c135f086f36676d39
Michaelgathara <mikegtrm@gmail.com>
2025-07-10 10:54:09 -0500

    fix: uncoupled this branch from michaelg/main to match pytorch/main

--- test/test_numpy_interop.py
+++ test/test_numpy_interop.py
@@ -6,7 +6,6 @@ import sys
 from itertools import product
 
 import numpy as np
-from packaging.version import Version
 
 import torch
 from torch.testing import make_tensor
@@ -639,28 +638,6 @@ class TestNumPyInterop(TestCase):
         self.assertEqual(torch.mul(x, y).shape, y.shape)
         # Regression test for https://github.com/pytorch/pytorch/issues/113037
         self.assertEqual(torch.div(x, y, rounding_mode="floor").shape, y.shape)
-
-    def test_ndarray_astype_object_graph_break(self):
-        @torch.compile(backend="eager", fullgraph=True)
-        def f(xs):
-            xs.astype("O")
-
-        xs = np.array([1, 2])
-        with self.assertRaisesRegex(
-            torch._dynamo.exc.Unsupported, "ndarray.astype\\(object\\)"
-        ):
-            f(xs)
-
-    def test_ndarray_astype_object_graph_break_2(self):
-        @torch.compile(backend="eager", fullgraph=True)
-        def f(xs):
-            xs.astype(object)
-
-        xs = np.array([1, 2])
-        with self.assertRaisesRegex(
-            torch._dynamo.exc.Unsupported, "ndarray.astype\\(object\\)"
-        ):
-            f(xs)
 
 
 instantiate_device_type_tests(TestNumPyInterop, globals())
