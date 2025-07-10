671650aaa3b18f39c788dd37b63f96ffb8791bb6
Mrigank Srivastava <Mrigank.Srivastava@amd.com>
2025-07-10 08:25:26 -0600

    [ZENDNN] Single linear post-op fusions.
    - Add relu, silu, gelu, tanh and sigmoid fusions with zendnn_linear.
    
    Co-authored-by: priyansh jain <priyansh.jain2@amd.com>
    Change-Id: I49c735b7e848f1c2f04d0dbe85b9a93e29a01c52

--- torch/_inductor/fx_passes/zendnn_optimize.py
+++ torch/_inductor/fx_passes/zendnn_optimize.py
@@ -3,6 +3,7 @@ from torch._inductor import config
 
 from .zendnn_custom_passes import add_zendnn_weight_prepack_ops
 from .zendnn_op_replacements import replace_with_zendnn_ops
+from .zendnn_single_post_op_fusions import zendnn_single_post_op_fusions
 
 
 def optimize(graph):
@@ -11,4 +12,6 @@ def optimize(graph):
     if config.cpp.weight_prepack:
         # replace zendnn ops with zendnn custom passes
         opt_graph = add_zendnn_weight_prepack_ops(opt_graph)
+    # single post-op fusion passes
+    opt_graph = zendnn_single_post_op_fusions(opt_graph)
     return opt_graph
