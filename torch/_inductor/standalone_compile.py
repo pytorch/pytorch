a9537b626c91ce617139ade60b9107a2805a4248
rzou <zou3519@gmail.com>
2025-07-09 20:03:30 -0700

    [standalone_compile] Fix single Tensor outputs from split_module (#157803)
    We assumed that the output in an FX graph would always just be a
    list[Tensor], even in the single tensor return case.
    It is possible for the output to be a single Tensor. This can happen
    by calling torch.fx.split_module on the module.
    
    Test Plan:
    - new test
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/157803
    Approved by: https://github.com/oulgen

--- torch/_inductor/standalone_compile.py
+++ torch/_inductor/standalone_compile.py
@@ -213,11 +213,26 @@ def standalone_compile(
         last_node = next(iter(reversed(gm.graph.nodes)))
         assert last_node.op == "output"
         assert len(last_node.args) == 1
-        for node in last_node.args[0]:
+
+        def handle_node(node: torch.fx.Node) -> None:
+            nonlocal fake_mode
             if "example_value" in node.meta:
                 maybe_tensor = node.meta["example_value"]
                 if isinstance(maybe_tensor, torch._subclasses.fake_tensor.FakeTensor):
                     fake_mode = maybe_tensor.fake_mode
+
+        # If gm came from Dynamo, then last_node.args[0] is always a list,
+        # even in single-Tensor returns.
+        #
+        # It's possible to get into a situation where last_node.args[0]
+        # is a Node (and not a list!). This happens if you call split_module
+        # on the graph. We allow for this case since it is common.
+        if isinstance(last_node.args[0], torch.fx.Node):
+            handle_node(last_node.args[0])
+        else:
+            for node in last_node.args[0]:
+                handle_node(node)
+
     else:
         raise ValueError(
             f"standalone_compile got unsupported `dynamic_shapes` value: dynamic_shapes={dynamic_shapes}."
