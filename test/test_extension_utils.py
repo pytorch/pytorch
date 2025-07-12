a4f7758fc488779de079565fc2cf93e18247492e
Jiapeng Li <jiapli@microsoft.com>
2025-07-12 01:43:37 +0000

    separate python test

--- test/test_extension_utils.py
+++ test/test_extension_utils.py
@@ -1,15 +1,9 @@
 # Owner(s): ["module: PrivateUse1"]
-import os
-
-from torch.testing._internal.common_utils import run_tests, TestCase
-
-
-class TestExtensionUtils(TestCase):
-    def _get_external_module_register_with_renamed_backend_template(self, backend_name):
-        return f"""\
 import sys
+
 import torch
 from torch.testing._internal.common_utils import run_tests, TestCase
+
 
 class DummyPrivateUse1Module:
     @staticmethod
@@ -36,7 +30,16 @@ class DummyPrivateUse1Module:
     def get_amp_supported_dtype():
         return [torch.float16]
 
-class TestRenamedBackend(TestCase):
+
+class TestExtensionUtils(TestCase):
+    def tearDown(self):
+        # Clean up
+        backend_name = torch._C._get_privateuse1_backend_name()
+        if hasattr(torch, backend_name):
+            delattr(torch, backend_name)
+        if f"torch.{backend_name}" in sys.modules:
+            del sys.modules[f"torch.{backend_name}"]
+
     def test_external_module_register(self):
         # Built-in module
         with self.assertRaisesRegex(RuntimeError, "The runtime module of"):
@@ -58,46 +61,27 @@ class TestRenamedBackend(TestCase):
             torch._register_device_module("privateuseone", DummyPrivateUse1Module)
 
     def test_external_module_register_with_renamed_backend(self):
-        torch.utils.rename_privateuse1_backend("{backend_name}")
+        torch.utils.rename_privateuse1_backend("foo")
         with self.assertRaisesRegex(RuntimeError, "has already been set"):
             torch.utils.rename_privateuse1_backend("dummmy")
 
         custom_backend_name = torch._C._get_privateuse1_backend_name()
-        self.assertEqual(custom_backend_name, "{backend_name}")
+        self.assertEqual(custom_backend_name, "foo")
 
         with self.assertRaises(AttributeError):
-            getattr(torch, "{backend_name}").is_available()  # type: ignore[attr-defined]
+            torch.foo.is_available()  # type: ignore[attr-defined]
 
         with self.assertRaisesRegex(AssertionError, "Tried to use AMP with the"):
             with torch.autocast(device_type=custom_backend_name):
                 pass
-        torch._register_device_module("{backend_name}", DummyPrivateUse1Module)
+        torch._register_device_module("foo", DummyPrivateUse1Module)
 
-        getattr(torch, "{backend_name}").is_available()  # type: ignore[attr-defined]
+        torch.foo.is_available()  # type: ignore[attr-defined]
         with torch.autocast(device_type=custom_backend_name):
             pass
 
-        self.assertEqual(torch._utils._get_device_index("{backend_name}:1"), 1)
-        self.assertEqual(torch._utils._get_device_index(torch.device("{backend_name}:2")), 2)
-
-if __name__ == "__main__":
-    run_tests()
-"""
-
-    def test_external_module_register_with_renamed_backend(self):
-        env = dict(os.environ)
-        # a backend name is not a c10:DeviceType
-        _, stderr = self.run_process_no_exception(
-            self._get_external_module_register_with_renamed_backend_template("foo"),
-            env=env,
-        )
-        self.assertIn("Ran 2 test", stderr.decode("utf-8"))
-        # a backend name is a c10:DeviceType
-        _, stderr = self.run_process_no_exception(
-            self._get_external_module_register_with_renamed_backend_template("maia"),
-            env=env,
-        )
-        self.assertIn("Ran 2 test", stderr.decode("utf-8"))
+        self.assertEqual(torch._utils._get_device_index("foo:1"), 1)
+        self.assertEqual(torch._utils._get_device_index(torch.device("foo:2")), 2)
 
 
 if __name__ == "__main__":
