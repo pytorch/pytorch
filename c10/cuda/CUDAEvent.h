b2aeee430bb62e1566996d81cfb5d093667e2c27
Yu, Guangye <guangye.yu@intel.com>
2025-07-14 15:01:01 +0000

    Update
    [ghstack-poisoned]

--- c10/cuda/CUDAEvent.h
+++ c10/cuda/CUDAEvent.h
@@ -275,4 +275,55 @@ struct TORCH_CUDA_CPP_API CUDAEvent {
   }
 };
 
+// Note: cudaEventCreate when concurrently invoked from multiple threads can be
+// very expensive (at least on certain device/driver combinations). Thus, we a)
+// serialize event creation at a per-device level, and b) pool the events to
+// avoid constantly calling cudaEventCreate/cudaEventDestroy. This results in
+// significant improvements in multithreaded workloads with high allocation
+// rates.
+class EventPool {
+ public:
+  using Event = std::unique_ptr<
+      c10::cuda::CUDAEvent,
+      std::function<void(at::cuda::CUDAEvent*)>>;
+  EventPool() : pools_(at::cuda::device_count()) {}
+
+  Event get(DeviceIndex device) {
+    TORCH_INTERNAL_ASSERT(0 <= device);
+    TORCH_INTERNAL_ASSERT(device < static_cast<DeviceIndex>(pools_.size()));
+    auto& pool = pools_[device];
+    auto destructor = [&pool](at::cuda::CUDAEvent* event) {
+      std::lock_guard<std::mutex> g(pool.mutex_);
+      pool.event_pool_.push_back(std::unique_ptr<at::cuda::CUDAEvent>(event));
+    };
+
+    // Try to acquire an event from the per-device pool.
+    {
+      std::lock_guard<std::mutex> g(pool.mutex_);
+      if (!pool.event_pool_.empty()) {
+        auto* event = pool.event_pool_.back().release();
+        pool.event_pool_.pop_back();
+        return Event(event, destructor);
+      }
+    }
+    // otherwise, allocate a new event that will be returned to the pool on
+    // destruction.
+    return Event(std::make_unique<at::cuda::CUDAEvent>().release(), destructor);
+  }
+
+  void empty_cache() {
+    for (auto& pool : pools_) {
+      std::lock_guard<std::mutex> g(pool.mutex_);
+      pool.event_pool_.clear();
+    }
+  }
+
+ private:
+  struct PerDevicePool {
+    alignas(64) std::mutex mutex_;
+    std::vector<std::unique_ptr<at::cuda::CUDAEvent>> event_pool_;
+  };
+  std::vector<PerDevicePool> pools_;
+};
+
 } // namespace at::cuda
