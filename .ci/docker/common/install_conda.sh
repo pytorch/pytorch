4dc9198880c3dbf93190b117585db830464d5127
Camyll Harajli <camyllh@meta.com>
2025-07-14 16:46:04 -0700

    Update .ci/docker/common/install_conda.sh
    Co-authored-by: Andrey Talman <atalman@fb.com>

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -75,7 +75,6 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   if [[ $(uname -m) != "aarch64" ]]; then
     pip_install mkl==2024.2.0
     pip_install mkl-static==2024.2.0
-    pip_install mkl-include==2024.2.0
   fi
 
   # Install llvm-8 as it is required to compile llvmlite-0.30.0 from source
