0cbd40355cede23ed9e055048bada99ad538f150
Denghui Dong <denghui.ddh@alibaba-inc.com>
2025-07-14 09:36:45 +0800

    Update

--- torch/csrc/profiler/python/init.cpp
+++ torch/csrc/profiler/python/init.cpp
@@ -359,7 +359,7 @@ void initPythonBindings(PyObject* module) {
           "       match the parent python event duration. This feature is new and currently disabled by default.\n"
           "    disable_external_correlation (bool) : whether to disable external correlation\n"
           "    profile_all_threads (bool) : whether to profile all threads\n"
-          "    capture_overload_names (bool) : whether to include ATen overload names in the profile\n",
+          "    capture_overload_names (bool) : whether to include ATen overload names in the profile\n"
           "    custom_profiler_config (string) : Used to pass some configurations to the custom profiler backend.\n",
           py::arg("profiler_metrics") = std::vector<std::string>(),
           py::arg("profiler_measure_per_kernel") = false,
