ec37e57640953fd845da7746645e8adc171b6fa2
Andrey Talman <atalman@fb.com>
2025-07-15 19:31:34 -0400

    Update install_conda.sh

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -4,14 +4,9 @@ set -ex
 
 # Optionally install conda
 if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
-  BASE_URL="https://repo.anaconda.com/miniconda"
-  # pin to version before required TOS
-  CONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
-  if [[ $(uname -m) == "aarch64" ]] || [[ "$BUILD_ENVIRONMENT" == *xpu* ]] || [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
-    BASE_URL="https://github.com/conda-forge/miniforge/releases/latest/download"  # @lint-ignore
-    CONDA_FILE="Miniforge3-Linux-$(uname -m).sh"
-  fi
-
+  BASE_URL="https://github.com/conda-forge/miniforge/releases/latest/download"  # @lint-ignore
+  CONDA_FILE="Miniforge3-Linux-$(uname -m).sh"
+  
   MAJOR_PYTHON_VERSION=$(echo "$ANACONDA_PYTHON_VERSION" | cut -d . -f 1)
   MINOR_PYTHON_VERSION=$(echo "$ANACONDA_PYTHON_VERSION" | cut -d . -f 2)
 
@@ -57,7 +52,6 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   
   # Install correct Python version
   # Also ensure sysroot is using a modern GLIBC to match system compilers
-  as_jenkins conda config --show-sources
   as_jenkins conda create -n py_$ANACONDA_PYTHON_VERSION -y\
              python="$ANACONDA_PYTHON_VERSION" \
              ${SYSROOT_DEP}
