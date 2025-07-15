ec7d86122ccdef8ebaf3c5aa00ed5e6625dfbc1a
Camyll Harajli <camyllh@meta.com>
2025-07-15 09:28:36 -0700

    Update .ci/docker/common/install_conda.sh
    Co-authored-by: Andrey Talman <atalman@fb.com>

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -73,7 +73,7 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
 
   # Install PyTorch conda deps, as per https://github.com/pytorch/pytorch README
   if [[ $(uname -m) != "aarch64" ]]; then
-    pip_install mkl==2024.2.0
+    conda_install "mkl=2021.4.0 mkl-include=2021.4.0"
     pip_install mkl-static==2024.2.0
   fi
 
