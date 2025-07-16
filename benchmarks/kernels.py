2b04d1282214748c5ed773db9013e13ee8636cf9
Boyuan Feng <boyuan@meta.com>
2025-07-16 11:27:53 -0700

    add CrossEntropyBackward

--- benchmarks/kernels.py
+++ benchmarks/kernels.py
@@ -1,20 +1,20 @@
+from dataclasses import dataclass
 from typing import Any, Callable
-
-import torch
-
 import cutlass
 import cutlass.torch as cutlass_torch
+
+import torch
+from torch._inductor.runtime.benchmarking import benchmarker
+import torch.nn.functional as F
 
 torch._dynamo.config.automatic_dynamic_shapes = False
 # Needed since changing args to function causes recompiles
 torch._dynamo.config.recompile_limit = 1000
 
+torch._inductor.config.force_disable_caches = True
+
 # TODO1: visualization
 # TODO2: roof line analysis
-
-
-from torch._inductor.runtime.benchmarking import benchmarker
-
 
 def benchmark_kernel_in_milliseconds(func: Callable, *args, **kwargs) -> float:
     # warmup
@@ -23,18 +23,62 @@ def benchmark_kernel_in_milliseconds(func: Callable, *args, **kwargs) -> float:
     return benchmarker.benchmark_gpu(lambda: func(*args, **kwargs))
 
 
+@dataclass
+class Performance:
+    # Benchmark setting usually the shape of the input tensor
+    setting: str
+
+    # Latency in milliseconds
+    latency: float
+
+    # Number of  memory access in bytes
+    memory_bytes: float
+
+    # Number of flops computation
+    flops: float
+
+    # Memory bandwidth in GB/s
+    memory_bandwidth: float = 0.0
+
+    # Compute intensity in FLOPs/byte
+    compute_intensity: float = 0.0
+
+    # Computationi per second
+    tflops_per_seconds: float = 0.0
+
+    def __post_init__(self):
+        self.memory_bandwidth = self.memory_bytes / (self.latency / 1000) / 1e9
+        self.compute_intensity = self.flops / self.memory_bytes
+        self.flops_per_seconds = self.flops / (self.latency / 1000) / 1e12
+
+    def __str__(self):
+        return f"setting: {self.setting}, latency: {self.latency} ms, memory bandwidth: {self.memory_bandwidth} GB/s"
+
+
 class BenchmarkKernel:
     def __init__(self):
         self.name = self.__class__.__name__
         self.available_backends = []
 
     def get_memory_bytes(self, args, kwargs) -> int:
+        # Get the necessary memory access in bytes for the kernelßß
+        raise NotImplementedError
+
+    def get_flops(self, args, kwargs) -> int:
+        # Get the number of flops for the kernel
+        raise NotImplementedError
+
+    def get_shapes(self) -> tuple[tuple[int, ...], ...]:
+        # Get a list of input shapes to benchmark the kernel
         raise NotImplementedError
 
     def eager(self, args, kwargs) -> Any:
         raise NotImplementedError
 
     def compiled(self, args, kwargs) -> Any:
+        raise NotImplementedError
+
+    def helion(self, args, kwargs) -> Any:
         raise NotImplementedError
 
     def quack(self, args, kwargs) -> Any:
@@ -56,7 +100,7 @@ class BenchmarkKernel:
 
         return args_ref, kwargs_ref
 
-    def check_accuracy(self, args, kwargs) -> bool:
+    def check_accuracy(self, args, kwargs) -> None:
         res = {}
         for backend in self.available_backends:
             args_ref, kwargs_ref = self.clone_inputs(args, kwargs)
@@ -72,37 +116,63 @@ class BenchmarkKernel:
             except:
                 print(f"Accuracy check failed for {backend} backend on {self.name} kernel")
 
-    def benchmark_single_shape(self, args, kwargs=None, should_check_accuracy=True):
+    def benchmark_single_shape(self, args, kwargs=None, should_check_accuracy=True, setting: str = ""):
         for backend in self.available_backends:
             args_ref, kwargs_ref = self.clone_inputs(args, kwargs)
             avg_time = benchmark_kernel_in_milliseconds(getattr(self, backend)(args, kwargs))
             mem_bytes = self.get_memory_bytes(args_ref, kwargs_ref)
-            print(f"{self.name} kernel on {backend} backend, latency: {avg_time} ms, mem throughput: {mem_bytes / (avg_time / 1000) / 1e9} GB/s")
+            flops = self.get_flops(args_ref, kwargs_ref)
+            perf = Performance(setting, avg_time, mem_bytes, flops)
+            print(f"{self.name} kernel on {backend} backend. {perf}")
 
         if should_check_accuracy:
             self.check_accuracy(args, kwargs)
 
+    # def roofline_analysis(self, backend: str, perfs: list[Performance]) -> None:
+    #     for perf in perfs:
+    #         compute_intensity = perf.flops / perf.memory_bytes
 
-class CrossEntropy(BenchmarkKernel):
+
+
+
+    #     for backend, backend_perf in perf.items():
+    #         for shape, shape_perf in backend_perf.items():
+    #             mem_bw = shape_perf["mem_bw"]
+    #             flops = shape_perf["flops"]
+    #             latency = shape_perf["latency"]
+    #             print(f"{self.name} kernel on {backend} backend, shape: {shape}, latency: {latency} ms, mem throughput: {mem_bw} GB/s, flops: {flops} FLOPs")
+
+
+class CrossEntropyForward(BenchmarkKernel):
     def __init__(self):
         super().__init__()
         self.available_backends = ["eager", "compiled", "quack"]
 
+    def get_shapes(self) -> tuple[tuple[int, ...], ...]:
+        return ((32768,256), (32768,512), (32768,1024), (32768,2048), (32768,4096), (32768,8192), (32768,16384), (32768,32768), (32768,65536), (16384,131072), (8192,262144))
+
     def get_memory_bytes(self, args, kwargs) -> int:
+        # Read x (M*N elements) + read target (M elements) + write loss (M elements)
         x, target = args
         M, N = x.shape
         dtype = x.dtype
         return (M * N + M + M) * dtype.itemsize
 
+    def get_flops(self, args, kwargs) -> int:
+        # TODO
+        return 0
+
     def eager(self, args, kwargs=None) -> Any:
         assert kwargs is None
         x, target = args
-        return lambda: torch.nn.functional.cross_entropy(x, target, reduction="none")
+        return lambda: F.cross_entropy(x, target, reduction="none")
 
     def compiled(self, args, kwargs=None) -> Any:
         assert kwargs is None
         x, target = args
-        compiled_cross_entropy = torch.compile(lambda x, target: torch.nn.functional.cross_entropy(x, target, reduction='none'), mode="max-autotune-no-cudagraphs")
+        # Need `lambda` otherwise torch.compile will not trace the function.
+        # More discussion: https://github.com/pytorch/pytorch/issues/158455
+        compiled_cross_entropy = torch.compile(lambda x, target: F.cross_entropy(x, target, reduction='none'), mode="max-autotune-no-cudagraphs")
         fn = lambda: compiled_cross_entropy(x, target)
         return fn
 
@@ -113,13 +183,67 @@ class CrossEntropy(BenchmarkKernel):
         return lambda: _cross_entropy(x, target)
 
     def benchmark(self):
-        candidate_shapes = ((32768,256), (32768,512),(32768,1024),(32768,2048),(32768,4096),(32768,8192),(32768,16384),(32768,32768),(32768,65536),(16384,131072), (8192,262144))
-        for (M, N) in candidate_shapes:
+        for (M, N) in self.get_shapes():
             print(f"Tensor dimensions: [{M}, {N}]")
             torch_dtype = cutlass_torch.dtype(cutlass.BFloat16)
             x = 0.1 * torch.randn(M, N, device="cuda", dtype=torch_dtype)
             target = torch.randint(0, N, (M,), device="cuda", dtype=torch.int64)
-            self.benchmark_single_shape((x, target))
+            self.benchmark_single_shape((x, target), setting=f"shape: [{M}, {N}]")
 
 
-CrossEntropy().benchmark()
+class CrossEntropyBackward(BenchmarkKernel):
+    def __init__(self):
+        super().__init__()
+        self.available_backends = ["eager", "compiled", "quack"]
+
+    def get_shapes(self) -> tuple[tuple[int, ...], ...]:
+        return ((32768,256), (32768,512), (32768,1024), (32768,2048), (32768,4096), (32768,8192), (32768,16384), (32768,32768), (32768,65536), (16384,131072), (8192,262144))
+
+    def get_memory_bytes(self, args, kwargs) -> int:
+        # Read x (M*N elements) + read target (M elements) + read dloss (M elements) + write grad(M*N elements)
+        x, target, dloss = args
+        # Memory ba
+        M, N = x.shape
+        return 2*M*N * x.dtype.itemsize + M * target.dtype.itemsize + M * dloss.dtype.itemsize
+
+    def get_flops(self, args, kwargs) -> int:
+        # TODO
+        return 0
+
+    def eager(self, args, kwargs=None) -> Any:
+        assert kwargs is None
+        x, target, dloss = args
+        loss = F.cross_entropy(x, target, reduction='none')
+        fn = lambda: torch.autograd.grad(loss, x, grad_outputs=dloss, retain_graph=True)
+        return fn
+
+    def compiled(self, args, kwargs=None) -> Any:
+        assert kwargs is None
+        x, target, dloss = args
+
+        compiled_cross_entropy = torch.compile(lambda x, target: F.cross_entropy(x, target, reduction='none'), mode="max-autotune-no-cudagraphs")
+        loss = compiled_cross_entropy(x, target)
+        fn = lambda: torch.autograd.grad(loss, x, grad_outputs=dloss, retain_graph=True)
+        return fn
+
+    def quack(self, args, kwargs=None) -> Any:
+        from quack.cross_entropy import cross_entropy
+
+        assert kwargs is None
+        x, target, dloss = args
+        loss = cross_entropy(x, target)
+        fn = lambda: torch.autograd.grad(loss, x, grad_outputs=dloss, retain_graph=True)
+        return fn
+
+    def benchmark(self):
+        for (M, N) in self.get_shapes():
+            print(f"Tensor dimensions: [{M}, {N}]")
+            torch_dtype = cutlass_torch.dtype(cutlass.BFloat16)
+            x = 0.1 * torch.randn(M, N, device="cuda", dtype=torch_dtype, requires_grad=True)
+            target = torch.randint(0, N, (M,), device="cuda", dtype=torch.int64)
+            dloss = torch.randn(M, device="cuda", dtype=torch.float32)
+            self.benchmark_single_shape((x, target, dloss), setting=f"shape: [{M}, {N}]")
+
+
+CrossEntropyForward().benchmark()
+CrossEntropyBackward().benchmark()
