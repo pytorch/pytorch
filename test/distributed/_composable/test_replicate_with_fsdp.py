effd44322d5d49c53c4c8b800c2121ab12b0830c
Anshul Sinha <50644008+sinhaanshul@users.noreply.github.com>
2025-07-16 11:04:55 -0700

    Update on "[FSDP][Replicate] added replicate function that uses FSDP instead of DDP"
    **Summary**
    Users would like to use Replicate with TP. Currently, the replicate function uses DDP, which has not been maintained resulting in a lack of integration options. Since users can use FSDP with TP, we will make the replicate function use FSDP so that users can use replicate with FSDP. To that end I have created a replicate function that uses FSDP instead of DDP. One blocker that I ran into is that the replicate function has a contract which assigns a module "replicate" attribute in registry. This would mean that fully_shards is_composable requirement would not be satisfied making it impossible to apply fully_shard to a replicate module. The solution to this was to copy the fully_shard function and state and modify it for replicate. In the future, it should be explored making the replicate_state inherit from FSDP_state to get rid of code duplicity. 
    
    **Test Case**
    1.  pytest test/distributed/_composable/test_replicate_with_fsdp.py
    
    
    
    
    
    [ghstack-poisoned]

--- test/distributed/_composable/test_replicate_with_fsdp.py
+++ test/distributed/_composable/test_replicate_with_fsdp.py
@@ -5,7 +5,6 @@ from copy import deepcopy
 
 import torch
 import torch.distributed as dist
-import torch.nn.functional as F
 from torch import nn
 from torch.distributed._composable.contract import _get_registry
 from torch.distributed._composable.replicate_with_fsdp import (
@@ -14,12 +13,13 @@ from torch.distributed._composable.replicate_with_fsdp import (
 )
 from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.fsdp import fully_shard
-from torch.distributed.tensor import distribute_tensor, DTensor, Replicate, Shard
+from torch.distributed.tensor import Replicate, Shard
 from torch.testing._internal.common_distributed import (
     MultiProcessTestCase,
     run_subtests,
     skip_if_lt_x_gpu,
 )
+from torch.testing._internal.common_fsdp import check_sharded_parity
 from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     ModelArgs,
@@ -90,79 +90,6 @@ class ReplicateTest(MultiProcessTestCase):
             store=dist.FileStore(self.file_name, self.world_size),
         )
 
-    def check_sharded_parity(
-        self,
-        replicated_module: nn.Module,
-        sharded_module: nn.Module,
-        prefixes_to_ignore: tuple[str, ...] = (),
-    ):
-        for (replicated_name, replicated_param), (sharded_name, sharded_param) in zip(
-            replicated_module.named_parameters(), sharded_module.named_parameters()
-        ):
-            clean_sharded_name = sharded_name
-            for prefix in prefixes_to_ignore:
-                clean_sharded_name = clean_sharded_name.replace(prefix, "")
-            self.assertEqual(replicated_name, clean_sharded_name)
-            self.assertIsInstance(sharded_param, DTensor)
-            assert isinstance(sharded_param, DTensor)  # mypy
-            mesh, placements = sharded_param.device_mesh, sharded_param.placements
-            if tuple(placements) == (Shard(0), Shard(0)):
-                raise AssertionError(
-                    "FSDP's (Shard(0), Shard(0)) layout differs from distribute_tensor(), "
-                    "so we cannot check for equality using it"
-                )
-            sharded_ref_param = distribute_tensor(replicated_param, mesh, placements)
-            self.assertEqual(sharded_param.to_local(), sharded_ref_param.to_local())
-            if replicated_param.grad is None:
-                self.assertIsNone(sharded_param.grad)
-                continue
-            self.assertIsNotNone(sharded_param.grad)
-            sharded_ref_grad = distribute_tensor(
-                replicated_param.grad, mesh, placements
-            )
-            self.assertIsInstance(sharded_param.grad, DTensor)
-            assert isinstance(sharded_param.grad, DTensor)  # mypy
-            self.assertEqual(sharded_param.grad.to_local(), sharded_ref_grad.to_local())
-
-    def _compare_module(self, mod, replicate_mod):
-        local_batch_size = 1
-        global_batch_size = self.world_size * local_batch_size
-        input = torch.randn(global_batch_size, 2)
-        target = torch.randn(global_batch_size, 2)
-
-        def step_model(model, input, target):
-            model.train()
-            output = model(input)
-            loss = F.mse_loss(output, target.to(output.device))
-            loss.backward()
-            for param in model.parameters():
-                with torch.no_grad():
-                    param -= param.grad
-                param.grad = None
-
-        for iteration in range(2):
-            step_model(mod, input, target)
-            step_model(
-                replicate_mod,
-                input[
-                    self.rank * local_batch_size : (self.rank + 1) * local_batch_size
-                ],
-                target[
-                    self.rank * local_batch_size : (self.rank + 1) * local_batch_size
-                ],
-            )
-
-            self.assertEqual(
-                len(list(mod.parameters())),
-                len(list(replicate_mod.parameters())),
-            )
-            for x, y in zip(mod.parameters(), replicate_mod.parameters()):
-                self.assertEqual(x, y.to_local(), rtol=1.3e-06, atol=5e-5)
-
-            # Shuffle the input so that DDP input is different
-            torch.manual_seed(iteration)
-            input = input[torch.randperm(global_batch_size)]
-
     def run_subtests(self, *args, **kwargs):
         return run_subtests(self, *args, **kwargs)
 
@@ -211,7 +138,9 @@ class ReplicateTest(MultiProcessTestCase):
 
     @skip_if_lt_x_gpu(2)
     def test_replicate_transformer(self):
-        """This tests that replicate works on a transformer model with fully_shard and replicate layers"""
+        """
+        This tests that replicate works on a transformer model with fully_shard and replicate layers
+        """
         self._init_pg()
         self.run_subtests(
             {
@@ -292,6 +221,9 @@ class ReplicateTest(MultiProcessTestCase):
             self.assertEqual(parameter.placements, (Shard(dim=0),))
 
     def test_train_replicate_fsdp(self):
+        """
+        Tests that replicate_model has the same behavior as original model when training
+        """
         self._init_pg()
 
         model = Net()
@@ -331,7 +263,7 @@ class ReplicateTest(MultiProcessTestCase):
             replicate_optim.zero_grad()
 
             self.assertEqual(replicate_loss, loss)
-            self.check_sharded_parity(model, replicate_model)
+            check_sharded_parity(self, model, replicate_model)
 
 
 if __name__ == "__main__":
