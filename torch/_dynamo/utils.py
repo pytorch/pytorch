Fix for parameterized graph launch not updating gradients properly.
Also fixes hf_BigBird and hf_Bart.

Unfortunately, not all training runs are completing correctly, though.

torch/_dynamo/utils.py
@@ -2875,6 +2875,7 @@ def same(
         assert set(ref.keys()) == set(res.keys()), (
             f"keys mismatch {set(ref.keys())} == {set(res.keys())}"
         )
+        is_same = True
         for k in sorted(ref.keys()):
             if not (
                 same(
@@ -2893,8 +2894,8 @@ def same(
                 )
             ):
                 log_error("Accuracy failed for key name %s", k)
-                return False
-        return True
+                is_same = False
+        return is_same
     elif isinstance(ref, set):
         assert isinstance(res, set)
         assert set(ref) == set(res), f"elements mismatch {set(ref)} == {set(res)}"
@@ -3033,7 +3034,7 @@ def same(
                 if not passes_test:
                     log_error(
                         "RMSE (res-fp64): %.5f, (ref-fp64): %.5f and shape=%s. res.dtype: %s, multiplier: %f, tol: %f"
-                        ", use_larger_multiplier_for_smaller_tensor: %d",
+                        ", use_larger_multiplier_for_smaller_tensor: %d, res sum: %f, ref sum: %f",
                         res_error,
                         ref_error,
                         res.size(),
@@ -3041,6 +3042,8 @@ def same(
                         multiplier,
                         tol,
                         use_larger_multiplier_for_smaller_tensor,
+                        torch.sum(res),
+                        torch.sum(ref),
                     )
                     # import ipdb; ipdb.set_trace()
                 return passes_test

torch/_inductor/compile_fx.py
@@ -233,12 +233,15 @@ def get_static_input_idxs(num_fixed: int) -> list[int]:
     # If we are inlining NNModules, we treat all torch.nn.Parameters as static for the purposes
     # of cudagraphs. Rather than copying these into cudagraph-owned memory
     # like we do for normal inputs on each run, we will re-record a cudagraph if these
-    # parameter locations change.
+    # parameter locations change. How do we know whether the parameter locations have changed? Not clear to me...
     context = torch._guards.TracingContext.try_get()
     fixed = list(range(num_fixed))
     if not context or not context.fw_metadata:
+        print("GALVEZ: returning fixed")
         return fixed
 
+    # Why is this happenning only once?
+    print("GALVEZ: returning context.fw_metadata.static_input_indices")
     return context.fw_metadata.static_input_indices
 
 
@@ -1714,6 +1717,7 @@ def cudagraphify(
 
     compiled_fn = None
 
+    # how is new_inputs?
     def run(new_inputs: Sequence[InputType]) -> Any:
         nonlocal compiled_fn
         if compiled_fn is None:
@@ -1881,6 +1885,7 @@ def cudagraphify_impl(
     static_input_idxs: Sequence[int] = (),
 ) -> Callable[[list[InputType]], Any]:
     """
+    I suppose this assumption is made for the backward pass
     Assumes inputs[static_input_idxs[i]] are always the same memory address
     """
 
@@ -2569,10 +2574,16 @@ def compile_fx(
                 # original strides
                 _recursive_record_user_visible_output_idxs(gm)
 
+                if config.triton.cudagraphs_elide_input_output_copies:
+                    static_input_idxs = []
+                else:
+                    static_input_idxs = get_static_input_idxs(fixed)
+
                 return inner_compile(
                     gm,
                     example_inputs,
-                    static_input_idxs=get_static_input_idxs(fixed),
+                    # TODO: Understand get_static_input_idxs
+                    static_input_idxs=static_input_idxs,
                     cudagraphs=cudagraphs,
                     graph_id=graph_id,
                     is_inference=is_inference,
@@ -2648,7 +2659,10 @@ def compile_fx(
                 else:
                     model_outputs_node.meta["user_visible_output_idxs"] = []
 
-                fixed = count_tangents(gm)
+                if config.triton.cudagraphs_elide_input_output_copies:
+                    fixed = 0
+                else:
+                    fixed = count_tangents(gm)
                 with (
                     config.patch(get_cpp_wrapper_config())
                     if config.cpp_wrapper
@@ -2657,6 +2671,7 @@ def compile_fx(
                     return inner_compile(
                         gm,
                         example_inputs,
+                        # What in the world?
                         static_input_idxs=list(range(fixed)),
                         cudagraphs=cudagraphs,
                         is_backward=True,

torch/_inductor/cudagraph_digraphs.py
@@ -85,10 +85,15 @@ log = torch._logging.getArtifactLogger(__name__, "cudagraphs")
 from . import config
 
 def nbytes_underlying_storage(tensor: torch.Tensor):
-    max_index = tensor.storage_offset() + sum((s-1)*st for s,st in zip(tensor.shape, tensor.stride()))
+    # tensor.storage_offset() + 
+    max_index = sum((s-1)*st for s,st in zip(tensor.shape, tensor.stride()))
     covered_bytes = (max_index + 1) * tensor.element_size()
     if tensor.is_contiguous():
-        assert tensor.nbytes == covered_bytes
+        try:
+            assert tensor.nbytes == covered_bytes
+        except AssertionError:
+            # import ipdb; ipdb.set_trace()
+            raise
     return covered_bytes
 
 def cudagraphify_impl(
@@ -99,6 +104,9 @@ def cudagraphify_impl(
     *args: Any,
     **kwargs: Any,
 ) -> ModelType:
+
+
+    print(f"GALVEZ:{torch._dynamo.config.compiled_autograd=}")
     fn_cache: dict[tuple[int, ...], Callable[..., Any]] = {}
 
     # Detect int inputs: we need to index on these
@@ -132,7 +140,10 @@ def cudagraphify_impl(
         # first get indices we need to check to align, then update our static inputs,
         # and finally copy
         check_input_idxs = get_input_idxs_to_check(inputs, static_input_idxs)
+        print(f"GALVEZ: {check_input_idxs=}")
         new_static_input_idxs = remove_unaligned_input_idxs(inputs, static_input_idxs)
+        print(f"GALVEZ: {new_static_input_idxs=}")
+        print(f"GALVEZ: {static_input_idxs=}")
         copy_misaligned_inputs(inputs, check_input_idxs)
 
         # here is the real cudagraphify. If I understand correctly, it
@@ -185,10 +196,26 @@ def cudagraphify(
 
     print("GALVEZ:cudagraph_digraphs.py cudagraphify")
 
+    for i, input in enumerate(inputs):
+        if isinstance(input, torch.Tensor):
+            print("GALVEZ:", i, " input.requires_grad=", input.requires_grad, "input.shape=", input.shape)
+
+    print("GALVEZ:", len(static_input_idxs))
+    print("GALVEZ:", len(mutated_input_idxs))
+
+    print("GALVEZ:static_input_idxs")
+    for idx in static_input_idxs:
+        print(f"GALVEZ: {idx=}")
+    print("GALVEZ:mutated_input_idxs")
+    for idx in mutated_input_idxs:
+        print(f"GALVEZ: {idx=}")
+
     # print("GALVEZ:total_memory_snapshot")
     # total_memory_snapshot = torch.cuda.memory_snapshot()
     # import pprint
     # pprint.pprint(total_memory_snapshot)
+
+    graphs = []
 
     graph = torch.cuda.CUDAGraph(keep_graph=True)
     dynamic_graph_arguments = True
@@ -196,6 +223,8 @@ def cudagraphify(
     input_pool = torch.cuda.MemPool(mem_allocator)
 
     # torch.cuda.memory._record_memory_history(True)
+
+    assert len(static_input_idxs) == 0
 
     with torch.cuda.use_mem_pool(input_pool):
         old_value = torch.utils.deterministic.fill_uninitialized_memory
@@ -205,8 +234,6 @@ def cudagraphify(
                 x
                 if not isinstance(x, torch.Tensor)
                 else static_input(x)
-                if idx not in static_input_idxs
-                else x.detach() # Interesting to detatch these...
             )
             for idx, x in enumerate(inputs)
         ]
@@ -260,15 +287,22 @@ def cudagraphify(
     static_inputs_only_dynamic_tensors = [(input.data_ptr(), nbytes_underlying_storage(input)) for idx, input in enumerate(static_inputs) if idx not in static_input_idxs and isinstance(input, torch.Tensor) and input.is_cuda]
 
     non_tensor_output_idxs = set()
+    empty_tensor_idxs_to_devices = {}
 
-    for static_output in static_outputs:
-        print("GALVEZ: output data_ptr=", static_output.data_ptr(), " nbytes=", nbytes_underlying_storage(static_output))
+    for i, static_output in enumerate(static_outputs):
+        if isinstance(static_output, torch.Tensor):
+            print("GALVEZ: output ", i, " data_ptr=", static_output.data_ptr(), " nbytes=", nbytes_underlying_storage(static_output))
 
     # I need to map each segment index to all output tensors, I think.
     for static_output_idx, static_output in enumerate(static_outputs):
         if not isinstance(static_output, torch.Tensor):
-            assert isinstance(static_output, int)
+            assert static_output is None or isinstance(static_output, int)
             non_tensor_output_idxs.add(static_output_idx)
+            segment_idx_containing_this_output_tensor.append(None)
+            continue
+        if static_output.data_ptr() == 0:
+            assert static_output.nbytes == 0
+            empty_tensor_idxs_to_devices[static_output_idx] = static_output.device
             segment_idx_containing_this_output_tensor.append(None)
             continue
         assert static_output.is_cuda, "I suppose non cuda outputs are allowed, but I would like to catch them explicitly for now"
@@ -322,13 +356,17 @@ def cudagraphify(
         dynamic_tensors = []
 
         for idx in dynamic_input_idxs:
+            print(f"{idx=} {new_inputs[idx].data_ptr()=} {new_inputs[idx].shape=}")
             dynamic_tensors.append(new_inputs[idx])
+
+        for idx in range(len(new_inputs_only_tensors)):
+            print(f"{idx=} {new_inputs_only_tensors[idx].data_ptr()=} {new_inputs_only_tensors[idx].shape=}")
 
         for segment_size, segment_device in zip(segment_sizes, segment_devices):
             storage_tensor = torch.empty(segment_size, dtype=torch.int8, device=segment_device)
-            # print("GALVEZ: storage_tensor=", storage_tensor.data_ptr())
+            print("GALVEZ: storage_tensor=", storage_tensor.data_ptr())
             # debug only
-            # storage_tensor[:] = 0
+            storage_tensor[:] = 0
             dynamic_tensors.append(storage_tensor)
 
         graph.replay_dynamic(dynamic_tensors)
@@ -338,6 +376,9 @@ def cudagraphify(
         for i, static_output in enumerate(static_outputs):
             if i in non_tensor_output_idxs:
                 outputs.append(static_output)
+                continue
+            if i in empty_tensor_idxs_to_devices:
+                outputs.append(torch.empty(0, device=empty_tensor_idxs_to_devices[i]))
                 continue
             containing_segment_idx = segment_idx_containing_this_output_tensor[i]
             if containing_segment_idx == -1:

torch/_inductor/utils.py
@@ -973,8 +973,8 @@ def get_first_incompatible_cudagraph_node(
             (
                 "aten._unsafe_index_put.default",
                 "aten._unsafe_masked_index_put_accumulate.default",
-                "aten.index_put.default",
-                "aten.index_put_.default",
+                # "aten.index_put.default",
+                # "aten.index_put_.default",
                 "aten.scatter.src",
                 "aten.scatter.reduce",
                 "aten.scatter.value_reduce",
@@ -2485,6 +2485,9 @@ def count_tangents(fx_g: torch.fx.GraphModule) -> int:
     Infers which inputs are static for a backwards graph
     """
 
+    # So why aren't parameters static for a backwards graph? I suppose
+    # that backwards graphs don't actually apply their changes. That's
+    # the responsibility of the optimizer.
     def is_saved_tensor(x: Node) -> bool:
         return (
             "tangents" not in x.name
@@ -2497,6 +2500,7 @@ def count_tangents(fx_g: torch.fx.GraphModule) -> int:
     static_arg_idxs = []
     for n in fx_g.graph.nodes:
         if n.op == "placeholder":
+            # It must be a saved tensor...
             if is_saved_tensor(n):
                 static_arg_idxs.append(arg_count)
             arg_count += 1
@@ -2659,6 +2663,9 @@ def should_assume_input_aligned(example_input: torch.Tensor) -> bool:
     # See Note: [Input Alignment handling in Inductor]
 
     # right now, we only care about alignment for cuda tensors.
+    
+    # Doesn't triton also have alignment constraints for cpu code as
+    # well? Wait,d oes triton even generate cpu code?
     if not is_gpu(example_input.device.type):
         return False
     return config.assume_aligned_inputs or tensor_is_aligned(example_input)
@@ -2784,6 +2791,7 @@ def copy_misaligned_inputs(
             f"Expected tensors only, but got: {type(_inp)}"
         )
         if _inp.data_ptr() % ALIGNMENT:
+            assert False, "GALVEZ: Input is not appropriately aligned! We cannot handle input mutations in this case :("
             new_inputs[i] = clone_preserve_strides(_inp)
 
             if ret_pair_defined and i in return_pair_idxs:  # type: ignore[operator]
@@ -2798,7 +2806,7 @@ def remove_unaligned_input_idxs(
     static_input_idxs: Sequence[int],
 ) -> Sequence[int]:
     """
-    We require all inputs to be aligned, so introduce a copy for any
+    We require all inputs to be aligned for the sake of triton, so introduce a copy for any
     that aren't.
     """
     aligned_static_input_idxs = []