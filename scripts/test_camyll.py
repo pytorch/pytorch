d61ae9a2ec9621cb0dc2dcffde417288d7c7a9eb
Camyll Harajli <camyllh@meta.com>
2025-07-16 10:16:01 -0700

    lint

--- scripts/test_camyll.py
+++ scripts/test_camyll.py
@@ -1,4 +1,8 @@
+warning: Selection `PLW1507` has no effect because preview is not enabled.
+warning: Selection `RUF041` has no effect because preview is not enabled.
+warning: Selection `RUF048` has no effect because preview is not enabled.
 #!/usr/bin/env python3
+
 
 def main():
     print("hello world")
