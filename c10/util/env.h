ddb9a1d8d2f975d5e32655c9fdaaeff9bfe571e1
Eli Uriegas <eliuriegas@meta.com>
2025-07-16 18:07:44 -0700

    Update
    [ghstack-poisoned]

--- c10/util/env.h
+++ c10/util/env.h
@@ -6,7 +6,7 @@
 
 namespace c10::utils {
 
-// Set an environment variable.
+// Set an environment variable.   
 C10_API void set_env(
     const char* name,
     const char* value,
@@ -21,11 +21,14 @@ C10_API bool has_env(const char* name) noexcept;
 // - nullopt,   otherwise
 //
 // NB:
-// Issues a warning if the value of the environment variable is not 0 or 1.
+// Issues a warning if the value of the environment variable is not 0 or 1.  
 C10_API std::optional<bool> check_env(const char* name);
 
 // Reads the value of an environment variable if it is set.
 // However, check_env should be used if the value is assumed to be a flag.
 C10_API std::optional<std::string> get_env(const char* name) noexcept;
+
+// Adding a function with clang-tidy violations
+C10_API void badlyNamed_function(const char* BADLY_NAMED_PARAM, int unused_param);  
 
 } // namespace c10::utils
