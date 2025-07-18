2bc4a7d6b62d39d8b0164804d42a455c391da9af
Huy Do <huydhn@gmail.com>
2025-07-17 20:24:28 -0700

    Update
    [ghstack-poisoned]

--- .ci/docker/common/install_inductor_benchmark_deps.sh
+++ .ci/docker/common/install_inductor_benchmark_deps.sh
@@ -15,8 +15,6 @@ function install_timm() {
   commit=$(get_pinned_commit timm)
 
   pip_install "git+https://github.com/huggingface/pytorch-image-models@${commit}"
-  # Clean up
-  conda_run pip uninstall -y torch torchvision triton
 }
 
 function install_torchbench() {
@@ -26,8 +24,6 @@ function install_torchbench() {
   pushd torchbench
   git checkout "$commit"
 
-  # Stable packages are ok here, just to satify TorchBench check
-  pip_install torch torchvision torchaudio
   python install.py --continue_on_fail
 
   # TODO (huydhn): transformers-4.44.2 added by https://github.com/pytorch/benchmark/pull/2488
@@ -41,6 +37,13 @@ function install_torchbench() {
 
 # Pango is needed for weasyprint which is needed for doctr
 conda_install pango
+
+# Stable packages are ok here, just to satisfy TorchBench check
+pip_install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+
+install_torchbench
 install_huggingface
 install_timm
-install_torchbench
+
+# Clean up
+conda_run pip uninstall -y torch torchvision torchaudio triton
