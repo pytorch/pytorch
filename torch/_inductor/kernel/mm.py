81c833c09f67fdb4e0267c38dfa23f8e3a41b6ef
PaulZhang12 <paulzhan@fb.com>
2025-07-18 14:08:15 -0700

    Fix Triton GEMM templates with k=1
    ghstack-source-id: b07e6e766650f7512dfcc60b51b2f463dcc96ac0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/158650

--- torch/_inductor/kernel/mm.py
+++ torch/_inductor/kernel/mm.py
@@ -110,11 +110,11 @@ mm_template = TritonTemplate(
 
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
-    if ((stride_am == 1 and stride_ak == M) or (stride_am == K and stride_ak == 1)) and M >= BLOCK_M:
+    if ((stride_am == 1 and stride_ak == M) or (stride_am == K and stride_ak == 1)) and (M >= BLOCK_M and K > 1):
         offs_a_m = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M)
     else:
         offs_a_m = rm % M
-    if ((stride_bk == 1 and stride_bn == K) or (stride_bk == N and stride_bn == 1)) and N >= BLOCK_N:
+    if ((stride_bk == 1 and stride_bn == K) or (stride_bk == N and stride_bn == 1)) and (N >= BLOCK_N and K > 1):
         offs_b_n = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N)
     else:
         offs_b_n = rn % N
