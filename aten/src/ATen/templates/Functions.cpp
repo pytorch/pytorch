6f2ef4a2dcba9b94f55197c7f995f199f8656c19
Nikita Shulga <nshulga@meta.com>
2025-07-18 16:04:07 -0700

    Update
    [ghstack-poisoned]

--- aten/src/ATen/templates/Functions.cpp
+++ aten/src/ATen/templates/Functions.cpp
@@ -64,7 +64,7 @@ Tensor TensorMaker::make_tensor() {
    if (strides_) {
      auto storage_size = detail::computeStorageNbytes(sizes_, *strides_, itemsize);
      if (storage_offset_) {
-       storage_size += storage_offset_.value();
+       storage_size += storage_offset_.value() * itemsize;
      }
      return storage_size;
    }
