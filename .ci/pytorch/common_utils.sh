046bc530cf63adb1c26828fe45b5974145fac83d
Huy Do <huydhn@gmail.com>
2025-07-18 23:47:32 -0700

    Update
    [ghstack-poisoned]

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -174,7 +174,12 @@ function install_torchvision() {
     echo 'char* dlerror(void) { return "";}'|gcc -fpic -shared -o "${HOME}/dlerror.so" -x c -
     LD_PRELOAD=${orig_preload}:${HOME}/dlerror.so
   fi
+
+  if [[ -n "${TORCH_CUDA_ARCH_LIST}" ]]; then
+    export WITH_CUDA=1
+  fi
   pip_build_and_install "git+https://github.com/pytorch/vision.git@${commit}" dist/vision
+
   if [ -n "${LD_PRELOAD}" ]; then
     LD_PRELOAD=${orig_preload}
   fi
