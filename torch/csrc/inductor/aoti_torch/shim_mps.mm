abf11a8b193f831ff192afb412e0d7439a4ab527
angelayi <yiangela7@gmail.com>
2025-07-18 23:46:52 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/inductor/aoti_torch/shim_mps.mm
+++ torch/csrc/inductor/aoti_torch/shim_mps.mm
@@ -3,6 +3,8 @@
 #include <torch/csrc/inductor/aoti_torch/utils.h>
 #include <ATen/mps/MPSAllocatorInterface.h>
 #include <ATen/mps/MPSDevice.h>
+#include <ATen/mps/MPSStream.h>
+#include <ATen/mps/MPSProfiler.h>
 
 
 using namespace torch::aot_inductor;
@@ -38,5 +40,18 @@ aoti_torch_mps_memcpy(void* buffer, size_t constant_offset, size_t bytes_read, s
     auto metal_buffer = (id<MTLBuffer>)buffer;
     auto buffer_pointer = static_cast<uint8_t*>([metal_buffer contents]);
     memcpy(buffer_pointer + constant_offset, constants_start + bytes_read, data_size);
+  });
+}
+
+AOTITorchError
+aoti_torch_mps_copy_buffer(void* buffer, size_t constant_offset, size_t bytes_read, size_t data_size, void* constant_buffer) {
+  AOTI_TORCH_CONVERT_EXCEPTION_TO_ERROR_CODE({
+
+    auto constant_metal_buffer = (id<MTLBuffer>)constant_buffer;
+    auto metal_buffer = (id<MTLBuffer>)buffer;
+
+    at::mps::MPSStream* stream = at::mps::getCurrentMPSStream();
+    uint64_t profile_id = at::mps::getMPSProfiler().beginProfileCopy(constant_metal_buffer, metal_buffer, at::OptionalTensorRef(), at::OptionalTensorRef(), data_size, true);
+    stream->copy_and_sync(constant_metal_buffer, metal_buffer, data_size, bytes_read, constant_offset, true, profile_id);
   });
 }
