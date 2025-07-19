7d29fbd4cabbf67be043c69a50ecbd81beb99c37
Natalia Gimelshein <ngimel@meta.com>
2025-07-19 12:20:12 -0700

    don't set CUDA_MODULE_LOAD

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -379,8 +379,6 @@ def _lazy_init():
             )
         # This function throws if there's a driver initialization error, no GPUs
         # are found or any other error occurs
-        if "CUDA_MODULE_LOADING" not in os.environ:
-            os.environ["CUDA_MODULE_LOADING"] = "LAZY"
         torch._C._cuda_init()
         # Some of the queued calls may reentrantly call _lazy_init();
         # we need to just return without initializing in that case.
