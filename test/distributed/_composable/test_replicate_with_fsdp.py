634c1e3270d729170bd3cc3f5c3ab6be1468764d
Anshul Sinha <50644008+sinhaanshul@users.noreply.github.com>
2025-07-22 16:31:56 -0700

    Update on "[FSDP][Replicate] added replicate function that uses FSDP instead of DDP"
    **Summary**
    Users would like to use Replicate with TP. Currently, the replicate function uses DDP, which has not been maintained resulting in a lack of integration options. Since users can use FSDP with TP, we will make the replicate function use FSDP so that users can use replicate with FSDP. To that end I have created a replicate function that uses FSDP instead of DDP. One blocker that I ran into is that the replicate function has a contract which assigns a module "replicate" attribute in registry. This would mean that fully_shards is_composable requirement would not be satisfied making it impossible to apply fully_shard to a replicate module. The solution to this was to copy the fully_shard function and state and modify it for replicate. In the future, it should be explored making the replicate_state inherit from FSDP_state to get rid of code duplicity. 
    
    **Test Case**
    1.  pytest test/distributed/_composable/test_replicate_with_fsdp.py
    
    
    
    
    
    [ghstack-poisoned]

--- test/distributed/_composable/test_replicate_with_fsdp.py
+++ test/distributed/_composable/test_replicate_with_fsdp.py
@@ -1,5 +1,7 @@
 # Owner(s): ["oncall: distributed"]
 
+import copy
+import functools
 import os
 from copy import deepcopy
 
@@ -19,7 +21,7 @@ from torch.testing._internal.common_distributed import (
     run_subtests,
     skip_if_lt_x_gpu,
 )
-from torch.testing._internal.common_fsdp import check_sharded_parity
+from torch.testing._internal.common_fsdp import check_sharded_parity, MLPStack
 from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     ModelArgs,
@@ -38,40 +40,12 @@ class Net(nn.Module):
         return self.fc3(self.fc2(self.fc1(x)))
 
 
-class ReplicateStateDictTest(MultiProcessTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        self._spawn_processes()
-
-    def tearDown(self):
-        super().tearDown()
-        try:
-            os.remove(self.file_name)
-        except OSError:
-            pass
-
-    def _init_pg(self):
-        dist.init_process_group(
-            backend="nccl",
-            rank=self.rank,
-            world_size=self.world_size,
-            store=dist.FileStore(self.file_name, self.world_size),
-        )
-
-    def _check_state_dict_parity(self, sd_1, sd_2):
-        for k1, k2 in zip(sd_1.keys(), sd_2.keys()):
-            self.assertEqual(k1, k2)
-
-        for v1, v2 in zip(sd_1.values(), sd_2.values()):
-            self.assertEqual(v1, v2)
-
-
 class ReplicateTest(MultiProcessTestCase):
     @property
     def world_size(self) -> int:
         return 4
 
-    def init_global_mesh(self) -> DeviceMesh:
+    def init_replicate_tp_mesh(self) -> DeviceMesh:
         # Prefer to test with >=4 GPUs, but for 2 GPUs, use 2-way TP
         replicate_size = 2
         return init_device_mesh(
@@ -92,8 +66,10 @@ class ReplicateTest(MultiProcessTestCase):
             pass
 
     def _init_pg(self):
+        # Set the device explicitly before initializing the process group
+        torch.cuda.set_device(self.rank % torch.cuda.device_count())
         dist.init_process_group(
-            backend="gloo",
+            backend="nccl",
             rank=self.rank,
             world_size=self.world_size,
             store=dist.FileStore(self.file_name, self.world_size),
@@ -121,14 +97,14 @@ class ReplicateTest(MultiProcessTestCase):
 
     def _test_replicate_transformer(self, sharding_strategy):
         model_args = ModelArgs()
-        model = Transformer(model_args)
 
+        model = Transformer(model_args)
         replicate_model = deepcopy(model)
 
         for i, layer in enumerate(replicate_model.layers):
-            if i % 3 == 0:
+            if i % 2 == 0:
                 replicate(layer)
-            elif i % 3 == 1:
+            elif i % 2 == 1:
                 fully_shard(layer)
 
         if sharding_strategy == "replicate":
@@ -140,29 +116,76 @@ class ReplicateTest(MultiProcessTestCase):
         self._composable_api_module_check(replicate_model, sharding_strategy)
 
         for i, layer in enumerate(replicate_model.layers):
-            if i % 3 == 0:
+            if i % 2 == 0:
                 self.assertTrue("replicate" in _get_registry(layer))
                 for parameter in layer.parameters():
                     self.assertEqual(parameter.placements, (Replicate(), Shard(dim=0)))
-            elif i % 3 == 1:
+            elif i % 2 == 1:
                 self.assertTrue("fully_shard" in _get_registry(layer))
                 for parameter in layer.parameters():
                     self.assertEqual(parameter.placements, (Shard(dim=0),))
 
-        managed_modules = _get_managed_modules((replicate_model,))
+    def test_replicate_transformer_managed_modules(self):
+        """
+        This tests that replicate managed modules works properly. In this test we use a Transformer Module with 3 layers,
+        which means there are 49 submodules. We apply replicate on the first layer and fully shard on the second layer,
+        each consisting of 14 submodules, leaving 21 remaining submodules. I have shown below how there are this many submodules
 
-        if sharding_strategy == "replicate":
-            self.assertEqual(len(managed_modules), 7)
+        1. Transformer Module
+            2. tok_embeddings
+            3. pos_embeddings
+            4. dropout
+            5. layers
+            6. norm
+            7. output
+
+        In the layers we have Transformer Blocks
+
+        1. Transformer Block
+            2. attention_norm
+            3. Attention
+                4. resid_dropout
+                5. wq
+                6. wk
+                7. wv
+                8. wo
+            9. ffn_norm
+            10. Feed_forward
+                11. w1
+                12. gelu
+                13. w2
+                14. resid_dropout
+
+        """
+        self._init_pg()
+
+        model_args = ModelArgs()
+        model_args.n_layers = 3
+
+        model = Transformer(model_args)
+        replicate_model = deepcopy(model)
+
+        self.assertEqual(len(_get_managed_modules((replicate_model,))), 49)
+
+        for i, layer in enumerate(replicate_model.layers):
+            if i % 3 == 0:
+                replicate(layer)
+            elif i % 3 == 1:
+                fully_shard(layer)
+
+        replicate_model = replicate(replicate_model)
+        self.assertEqual(len(_get_managed_modules((replicate_model,))), 21)
 
     @skip_if_lt_x_gpu(2)
-    def test_replicate_device_mesh(self):
+    def test_replicate_tp_device_mesh(self):
         """
         This tests that a user can pass in a device mesh to replicate a module
         """
 
         self._init_pg()
 
-        model = Net()
+        device = torch.device(f"cuda:{self.rank % torch.cuda.device_count()}")
+        model = Net().to(device)
         replicate_model = deepcopy(model)
 
         layers = [
@@ -171,17 +194,15 @@ class ReplicateTest(MultiProcessTestCase):
             replicate_model.fc3,
         ]
 
-        global_mesh = self.init_global_mesh()
+        global_mesh = self.init_replicate_tp_mesh()
         replicate_mesh = global_mesh["replicate", "shard"]
 
         for layer in layers:
-            replicate_model = replicate(layer, device_mesh=replicate_mesh)
+            replicate(layer, device_mesh=replicate_mesh)
 
             for parameter in layer.parameters():
                 self.assertEqual(parameter.device_mesh.shape, (2, 1))
                 self.assertEqual(parameter.placements, (Replicate(), Shard(dim=0)))
-
-        # replicate_model = replicate(replicate_model, device_mesh=replicate_mesh)
 
     def test_train_replicate_fsdp(self):
         """
@@ -189,7 +210,8 @@ class ReplicateTest(MultiProcessTestCase):
         """
         self._init_pg()
 
-        model = Net()
+        device = torch.device(f"cuda:{self.rank % torch.cuda.device_count()}")
+        model = Net().to(device)
         replicate_model = deepcopy(model)
 
         layers = [
@@ -199,22 +221,22 @@ class ReplicateTest(MultiProcessTestCase):
         ]
 
         for layer in layers:
-            fully_shard(layer)
+            replicate(layer)
 
-        replicate_model = fully_shard(replicate_model)
+        replicate_model = replicate(replicate_model)
 
         optim = torch.optim.Adam(model.parameters(), lr=0.01)
         replicate_optim = torch.optim.Adam(replicate_model.parameters(), lr=0.01)
 
         torch.manual_seed(42 + self.rank + 1)
-        inp = torch.randn(2, 2)
+        inp = torch.randn(2, 2, device=device)
 
         for _ in range(10):
             loss = model(inp).sum()
             loss.backward()
 
             for param in model.parameters():
-                dist.all_reduce(param.grad)
+                dist.all_reduce(param.grad, op=dist.ReduceOp.SUM)
 
             replicate_loss = replicate_model(inp).sum()
             replicate_loss.backward()
@@ -227,6 +249,62 @@ class ReplicateTest(MultiProcessTestCase):
 
             self.assertEqual(replicate_loss, loss)
             check_sharded_parity(self, model, replicate_model)
+
+    @skip_if_lt_x_gpu(2)
+    def test_train_parity_2d_mlp(self):
+        """
+        Verifies that when a device mesh is passed in, the model has the same behavior as the original model when training
+        """
+        self._init_pg()
+        global_mesh = self.init_replicate_tp_mesh()
+        run_subtests(
+            self,
+            {
+                "reshard_after_forward": [False, True],
+                "use_activation_checkpointing": [False, True],
+                "mlp_dim": [3, 16, 17],
+            },
+            functools.partial(self._test_train_parity_2d_mlp, global_mesh),
+        )
+
+    def _test_train_parity_2d_mlp(
+        self,
+        global_mesh: DeviceMesh,
+        reshard_after_forward: bool,
+        use_activation_checkpointing: bool,
+        mlp_dim: int,
+    ):
+        replicate_shard_mesh, tp_mesh = (
+            global_mesh["replicate", "shard"],
+            global_mesh["tp"],
+        )
+        replicate_mesh = global_mesh["replicate"]
+        replicate_pg = replicate_mesh.get_group()  # used for `replicate()`
+
+        torch.manual_seed(42)
+        model = MLPStack(mlp_dim)
+        ref_model = copy.deepcopy(model).cuda()
+        replicate(ref_model, device_mesh=replicate_shard_mesh)
+        ref_optim = torch.optim.Adam(ref_model.parameters(), lr=1e-2, foreach=False)
+        model.parallelize(
+            tp_mesh,
+            replicate_shard_mesh,
+            use_activation_checkpointing,
+            reshard_after_forward=reshard_after_forward,
+        )
+        optim = torch.optim.Adam(model.parameters(), lr=1e-2, foreach=False)
+
+        torch.manual_seed(42 + replicate_pg.rank() + 1)
+        device = torch.device("cuda")
+        for iter_idx in range(10):
+            inp = torch.randn((8, mlp_dim), device=device)
+            losses: list[torch.Tensor] = []
+            for _model, _optim in ((ref_model, ref_optim), (model, optim)):
+                _optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
+                losses.append(_model(inp).sum())
+                losses[-1].backward()
+                _optim.step()
+            self.assertEqual(losses[0], losses[1])
 
 
 if __name__ == "__main__":
--- torch/distributed/_composable/replicate_with_fsdp.py
+++ torch/distributed/_composable/replicate_with_fsdp.py
@@ -1,32 +1,21 @@
 # mypy: allow-untyped-defs
 from __future__ import annotations
 
-import functools
 import logging
-from typing import Any, Callable, Optional, TYPE_CHECKING, Union
+from typing import Callable, Optional, TYPE_CHECKING, Union
 
 import torch
 import torch.distributed as dist
 import torch.nn as nn
-from torch._logging import warning_once
-from torch.autograd import Variable
-from torch.autograd.graph import _MultiHandle
-from torch.distributed._composable_state import (
-    _get_module_state,
-    _insert_module_state,
-    _State,
-)
+from torch.distributed._composable_state import _get_module_state, _insert_module_state
 from torch.distributed.device_mesh import _get_device_handle
 from torch.distributed.fsdp._fully_shard._fsdp_api import (
     MixedPrecisionPolicy,
     OffloadPolicy,
 )
 from torch.distributed.fsdp._fully_shard._fsdp_common import (
-    _cast_fp_tensor,
-    compiled_autograd_enabled,
     detect_compiled_autograd,
     HSDPMeshInfo,
-    TrainingState,
 )
 from torch.distributed.fsdp._fully_shard._fsdp_init import (
     _get_device_from_mesh,
@@ -35,34 +24,30 @@ from torch.distributed.fsdp._fully_shard._fsdp_init import (
     _init_default_fully_shard_mesh,
     _move_states_to_device,
 )
-from torch.distributed.fsdp._fully_shard._fsdp_param_group import (
-    FSDPCommContext,
-    FSDPParamGroup,
+from torch.distributed.fsdp._fully_shard._fsdp_param_group import FSDPParamGroup
+from torch.distributed.fsdp._fully_shard._fsdp_state import (
+    _register_group_forward_hooks,
+    FSDPState,
 )
 from torch.distributed.fsdp._fully_shard._fully_shard import (
     _unimplemented_deepcopy,
     FSDPModule,
 )
 from torch.distributed.tensor import DeviceMesh, init_device_mesh
-from torch.distributed.utils import _apply_to_tensors, _get_root_modules, _to_kwargs
-from torch.utils._pytree import tree_flatten
+from torch.distributed.utils import _get_root_modules
 
 from .contract import _get_registry, contract
 
 
-logger = logging.getLogger("torch.distributed.fsdp.fully_shard")
-
 if TYPE_CHECKING:
-    from collections.abc import Sequence
-
-    from torch.distributed.fsdp._fully_shard._fsdp_param import FSDPParam
-    from torch.distributed.fsdp._fully_shard._fsdp_state import FSDPState
     from torch.distributed.tensor import Shard
 
 
 cls_to_fsdp_cls: dict[type, type] = {}
 
 _ROOT_MODULE_PREFIX = ""
+
+logger = logging.getLogger("torch.distributed.fsdp.fully_shard")
 
 
 class _ReplicateStateContext:
@@ -92,7 +77,7 @@ def _get_module_replicate_state(module: nn.Module) -> Optional[_ReplicateState]:
     return None
 
 
-class _ReplicateState(_State):
+class _ReplicateState(FSDPState):
     """
     Replicate state functionality is adapted from FSDP state.
     In the future, could experiment with inheriting from it instead.
@@ -100,17 +85,7 @@ class _ReplicateState(_State):
 
     def __init__(self) -> None:
         super().__init__()
-        self._fsdp_param_group: Optional[FSDPParamGroup] = None
-        self._is_root: Optional[bool] = None  # root set during lazy init
-        self._state_ctx = _ReplicateStateContext()
-        self._comm_ctx = FSDPCommContext()
-        self._training_state: TrainingState = TrainingState.IDLE
-        self._states_to_forward_prefetch: list[FSDPState] = []
-        self._states_to_backward_prefetch: list[FSDPState] = []
-        self._modules_to_run_forward: set[nn.Module] = set()
-        # ``False`` when user set reshard_after_forward
-        # through ``fully_shard`` or ``set_reshard_after_forward``
-        self._auto_reshard_after_forward: Optional[bool] = True
+        self._state_ctx = _ReplicateStateContext()  # type: ignore[assignment]
 
     # Define a separate init since `__init__` is called in the contract
     def init(
@@ -143,39 +118,6 @@ class _ReplicateState(_State):
             )
             self._pre_forward_hook_handle = hook_handle
             self._post_forward_hook_handle = hook_handle
-
-    def _root_pre_forward(
-        self, module: nn.Module, args: tuple[Any, ...], kwargs: dict[str, Any]
-    ) -> tuple[tuple[Any, ...], dict[str, Any]]:
-        self._lazy_init()
-        if self._state_ctx.iter_forward_root is not None:
-            return args, kwargs
-        if not compiled_autograd_enabled():
-            logger.debug("FSDP::root_pre_forward")
-        self._state_ctx.iter_forward_root = self
-        with torch.profiler.record_function("FSDP::root_pre_forward"):
-            # Wait for optimizer before implicitly prefetched all-gathers
-            if (event := self._state_ctx.post_optim_event) is not None:
-                self._comm_ctx.all_gather_copy_in_stream.wait_event(event)
-                self._comm_ctx.all_gather_stream.wait_event(event)
-                self._state_ctx.post_optim_event = None
-            else:
-                current_stream = self._device_handle.current_stream()
-                self._comm_ctx.all_gather_copy_in_stream.wait_stream(current_stream)
-                self._comm_ctx.all_gather_stream.wait_stream(current_stream)
-            if self._device.type in [
-                "cuda",
-                "hpu",
-                "xpu",
-                "mtia",
-                torch._C._get_privateuse1_backend_name(),
-            ]:
-                with torch.profiler.record_function("FSDP::inputs_to_device"):
-                    args_tuple, kwargs_tuple = _to_kwargs(
-                        args, kwargs, self._device, False
-                    )  # same as DDP
-                args, kwargs = args_tuple[0], kwargs_tuple[0]
-        return args, kwargs
 
     def _lazy_init(self) -> None:
         """
@@ -215,214 +157,16 @@ class _ReplicateState(_State):
         self._init_shared_state()
         # Run parameter group lazy inits after initializing FQNs for improved
         # error messages
-        for state in self._state_ctx.all_states:
-            if state._fsdp_param_group:
-                state._fsdp_param_group.lazy_init()
-
-    def _init_shared_state(self) -> None:
-        self._comm_ctx.lazy_init(self._device)
-        for state in self._state_ctx.all_states:
-            state._state_ctx = self._state_ctx
-            state._comm_ctx = self._comm_ctx
-            if fsdp_param_group := state._fsdp_param_group:
-                fsdp_param_group.comm_ctx = self._comm_ctx
-
-    def _init_fqns(self) -> None:
-        """Sets module and parameter FQN attributes for debugging."""
-        assert self._is_root
-        root_module = self._modules[0]
-        param_to_fsdp_param: dict[nn.Parameter, FSDPParam] = {}
-        module_to_fsdp_param_group: dict[nn.Module, FSDPParamGroup] = {}
-        for state in self._state_ctx.all_states:
-            if fsdp_param_group := state._fsdp_param_group:
-                for fsdp_param in fsdp_param_group.fsdp_params:
-                    param_to_fsdp_param[fsdp_param.sharded_param] = fsdp_param
-                for module in fsdp_param_group.modules:
-                    module_to_fsdp_param_group[module] = fsdp_param_group
-        for param_name, param in root_module.named_parameters():
-            if param in param_to_fsdp_param:
-                param_to_fsdp_param[param]._param_fqn = param_name
-        for module_name, module in root_module.named_modules():
-            if module in module_to_fsdp_param_group:
-                module_fqn = module_to_fsdp_param_group[module]._module_fqn
-                if module_fqn is None:
-                    module_to_fsdp_param_group[module]._module_fqn = module_name
-                else:
-                    assert isinstance(module_fqn, str), f"{module_fqn}"
-                    module_fqn += f", {module_name}"
-                    module_to_fsdp_param_group[module]._module_fqn = module_fqn
-
-    def _pre_forward(
-        self, module: nn.Module, args: tuple[Any, ...], kwargs: dict[str, Any]
-    ) -> tuple[tuple[Any, ...], dict[str, Any]]:
-        # When composing with module-hook-based activation checkpointing, the
-        # the pre-backward hook is responsible for the unshard
-        if self._training_state == TrainingState.PRE_BACKWARD:
-            return args, kwargs
-        self._training_state = TrainingState.FORWARD
-        args, kwargs = self._root_pre_forward(module, args, kwargs)
-        if self._mp_policy.cast_forward_inputs and self._mp_policy.param_dtype:
-            with torch.profiler.record_function("Replicate::cast_forward_inputs"):
-                cast_fn = functools.partial(
-                    _cast_fp_tensor, self._mp_policy.param_dtype
-                )
-                args, kwargs = (
-                    _apply_to_tensors(cast_fn, args),
-                    _apply_to_tensors(cast_fn, kwargs),
-                )
-        if self._fsdp_param_group:
-            args, kwargs = self._fsdp_param_group.pre_forward(module, args, kwargs)
-        for fsdp_state in self._states_to_forward_prefetch:
-            if (target_param_group := fsdp_state._fsdp_param_group) is not None:
-                FSDPParamGroup._prefetch_unshard(target_param_group, "forward")
-        return args, kwargs
-
-    def _post_forward(self, module: nn.Module, input: Any, output: Any) -> Any:
-        # When composing with module-hook-based activation checkpointing, the
-        # post-backward hook is responsible for the reshard
-        if self._training_state == TrainingState.PRE_BACKWARD:
-            return output
-        if self._fsdp_param_group:
-            output = self._fsdp_param_group.post_forward(module, input, output)
-        output = self._register_pre_backward_hook(output)
-        self._training_state = TrainingState.IDLE
-        if self._state_ctx.iter_forward_root is self:
-            if all_gather_state := self._comm_ctx.all_gather_state:
-                # Free the last all-gather result if needed; refer to
-                # [Note: Overlapping all-gather copy-in and all-gather]
-                self._comm_ctx.all_gather_copy_in_stream.wait_event(
-                    all_gather_state.event
-                )
-                self._comm_ctx.all_gather_stream.wait_event(all_gather_state.event)
-                self._comm_ctx.all_gather_state = None  # free the all-gather result
-            self._state_ctx.iter_forward_root = None
-        if self._mp_policy.output_dtype is not None:
-            with torch.profiler.record_function("Replicate::cast_forward_outputs"):
-                output = _apply_to_tensors(
-                    functools.partial(_cast_fp_tensor, self._mp_policy.output_dtype),
-                    output,
-                )
-        return output
-
-    def _pre_backward(self, grad: torch.Tensor) -> torch.Tensor:
-        self._training_state = TrainingState.PRE_BACKWARD
-        self._register_root_post_backward_final_callback()
-        if self._fsdp_param_group:
-            default_prefetch = len(self._states_to_backward_prefetch) == 0
-            self._fsdp_param_group.pre_backward(default_prefetch)
-        for fsdp_state in self._states_to_backward_prefetch:
-            if (target_param_group := fsdp_state._fsdp_param_group) is not None:
-                FSDPParamGroup._prefetch_unshard(target_param_group, "backward")
-        return grad
-
-    def _root_post_backward_final_callback(self) -> None:
-        if not compiled_autograd_enabled():
-            logger.debug("Replicate::root_post_backward")
-        with torch.profiler.record_function("Replicate::root_post_backward_callback"):
-            for state in self._state_ctx.all_states:
-                fsdp_param_group = state._fsdp_param_group
-                if (
-                    fsdp_param_group
-                    and fsdp_param_group._training_state != TrainingState.POST_BACKWARD
-                ):
-                    # Run post-backward in case forward inputs did not require
-                    # gradient so the autograd backward did not run
-                    fsdp_param_group.post_backward()
-                state._training_state = TrainingState.IDLE
-                if fsdp_param_group:
-                    fsdp_param_group._training_state = TrainingState.IDLE
-                if self._state_ctx.is_last_backward:
-                    state._finalize_backward()
-            if self._state_ctx.is_last_backward:
-                self._comm_ctx.post_forward_order.clear()
-                if self._comm_ctx.reduce_scatter_state is not None:
-                    self._device_handle.current_stream().wait_event(
-                        self._comm_ctx.reduce_scatter_state.event
-                    )
-                    self._comm_ctx.reduce_scatter_state = None
-            self._state_ctx.post_backward_final_callback_queued = False
-
-    def _finalize_backward(self) -> None:
-        if self._modules_to_run_forward:
-            msg = (
-                f"{len(self._modules_to_run_forward)} of the {len(self._modules)} "
-                f"modules passed to fully_shard did not run forward before backward, "
-                "which is error-prone since Replicate post-forward/pre-backward logic "
-                "will not run for these modules. We recommend passing only modules "
-                "that run forward together. Modules that did not run forward: "
-                f"{list(self._modules_to_run_forward)}"
-            )
-            warning_once(logger, msg, stacklevel=2)
-            # Clear since we want the next forward to run
-            self._modules_to_run_forward.clear()
-        if self._fsdp_param_group:
-            self._fsdp_param_group.finalize_backward()
-
-    def _register_pre_backward_hook(self, output: Any) -> Any:
-        if not torch.is_grad_enabled():
-            return output
-        flat_outputs, _ = tree_flatten(output)
-        for t in flat_outputs:
-            if torch.is_tensor(t) and t.requires_grad:
-                t.register_hook(self._pre_backward)
-        return output
-
-    def _register_root_post_backward_final_callback(self):
-        if self._state_ctx.post_backward_final_callback_queued:
-            return
-        self._state_ctx.post_backward_final_callback_queued = True
-        Variable._execution_engine.queue_callback(
-            self._root_post_backward_final_callback
-        )
-
-
-def _register_group_forward_hooks(
-    modules: Sequence[nn.Module],
-    pre_hook: Callable,
-    post_hook: Callable,
-    modules_to_run: set[nn.Module],
-):
-    """
-    Registers group forward pre and post-hooks. The pre-hook runs upon the
-    first module pre-forward, and the post-hook runs upon the last. If at least
-    one module does not run forward, then the post-hook does not run.
-    """
-    modules_set = set(modules)
-
-    @functools.wraps(pre_hook)
-    def wrapped_pre_hook(*args: Any, **kwargs: Any):
-        if len(modules_to_run) == 0:  # first to run
-            modules_to_run.update(modules_set)
-            return pre_hook(*args, **kwargs)
-
-    def get_wrapped_post_hook(module: nn.Module):
-        @functools.wraps(post_hook)
-        def wrapped_post_hook(*args: Any, **kwargs: Any):
-            modules_to_run.discard(module)
-            if len(modules_to_run) == 0:
-                return post_hook(*args, **kwargs)
-
-        return wrapped_post_hook
-
-    pre_handles = [
-        module.register_forward_pre_hook(
-            wrapped_pre_hook, prepend=True, with_kwargs=True
-        )
-        for module in modules
-    ]
-    post_handles = [
-        module.register_forward_hook(
-            get_wrapped_post_hook(module), prepend=False, always_call=True
-        )
-        for module in modules
-    ]
-    return _MultiHandle(tuple(pre_handles + post_handles))
+        for state in self._state_ctx.all_states:  # type: ignore[assignment]
+            if state._fsdp_param_group:  # type: ignore[union-attr]
+                state._fsdp_param_group.lazy_init()  # type: ignore[union-attr]
 
 
 def replicate_impl(
     module,
     mesh: DeviceMesh,
     *,
+    device_id: Optional[Union[int, torch.device]] = None,
     reshard_after_forward: Optional[Union[bool, int]] = None,
     shard_placement_fn: Optional[Callable[[nn.Parameter], Optional[Shard]]] = None,
     mp_policy: MixedPrecisionPolicy = MixedPrecisionPolicy(),
