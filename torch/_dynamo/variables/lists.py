4f3e26dcdede5f1afa0c17f6728b91d3722b2b54
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-22 13:41:18 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/lists.py
+++ torch/_dynamo/variables/lists.py
@@ -1258,9 +1258,7 @@ class ListIteratorVariable(IteratorVariable):
         return self.items[old_index]
 
     def call_obj_hasattr(self, tx, name):
-        return variables.ConstantVariable.create(
-            hasattr(iter([]), name)
-        )
+        return variables.ConstantVariable.create(hasattr(iter([]), name))
 
     def python_type(self):
         return type(iter([]))
