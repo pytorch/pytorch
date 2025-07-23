3f916dda175465217fcdc96f5dfdd3a6da778025
Catherine Lee <csl@fb.com>
2025-07-22 21:20:48 -0700

    tc

--- tools/testing/test_selections.py
+++ tools/testing/test_selections.py
@@ -9,6 +9,9 @@ from typing import Callable, TYPE_CHECKING
 from tools.stats.import_test_stats import get_disabled_tests
 from tools.testing.test_run import ShardedTest, TestRun
 
+from torch.testing._internal.common_cuda import SM80OrLater
+from torch.testing._internal.common_utils import TEST_CUDA
+
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -18,18 +21,13 @@ REPO_ROOT = Path(__file__).resolve().parents[2]
 
 IS_MEM_LEAK_CHECK = os.getenv("PYTORCH_TEST_CUDA_MEM_LEAK_CHECK", "0") == "1"
 BUILD_ENVIRONMENT = os.getenv("BUILD_ENVIRONMENT", "")
-USE_3_PROCS = (
-    "sm8" in BUILD_ENVIRONMENT
-    or "sm9" in BUILD_ENVIRONMENT
-    or "cuda" not in BUILD_ENVIRONMENT
-)
 
 # NUM_PROCS_FOR_SHARDING_CALC must remain consistent across all shards of a job
 # to ensure that sharding is consistent, NUM_PROCS is the actual number of procs
 # used to run tests.  If they are not equal, the only consequence should be
 # unequal shards.
 IS_ROCM = os.path.exists("/opt/rocm")
-NUM_PROCS = 1 if IS_MEM_LEAK_CHECK else 3 if USE_3_PROCS else 2
+NUM_PROCS = 1 if IS_MEM_LEAK_CHECK else 3 if not TEST_CUDA or SM80OrLater else 2
 NUM_PROCS_FOR_SHARDING_CALC = NUM_PROCS if not IS_ROCM or IS_MEM_LEAK_CHECK else 2
 THRESHOLD = 60 * 10  # 10 minutes
 
