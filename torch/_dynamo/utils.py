add37ba086b6f81587c2c1a3c8a35367a08988a5
pytorchbot <pytorchbot@pytorch.com>
2025-07-23 07:38:56 +0000

    2025-07-23 nightly release (2dccff7dcf56b0d168ebfd7ca08bdeca37273c56)

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -2439,6 +2439,15 @@ def is_int_specialization_case(value, source):
             source.guard_source().is_specialized_nn_module()
             and not config.allow_unspec_int_on_nn_module
         )
+        # integers coming from FSDP modules are considered static. This is
+        # purely empirical and perhaps we should have a better heuristic.
+        or (
+            source.guard_source().is_fsdp_module()
+            and not (
+                config.allow_unspec_int_on_nn_module
+                or config.allow_unspec_int_on_fsdp_module
+            )
+        )
         or (
             source.guard_source().is_unspecialized_builtin_nn_module()
             and not config.allow_unspec_int_on_nn_module
