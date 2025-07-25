d3594d2fcc28fddfb5dc1a539b65f8afdab6969d
Yang Wang <elainewy@meta.com>
2025-07-24 18:52:26 -0700

    setup pages
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- .github/test/build_cli/controllers/external_vllm_build.py
+++ .github/test/build_cli/controllers/external_vllm_build.py
@@ -1,6 +1,4 @@
-warning: Selection `PLW1507` has no effect because preview is not enabled.
-warning: Selection `RUF041` has no effect because preview is not enabled.
-warning: Selection `RUF048` has no effect because preview is not enabled.
+
 import os
 import subprocess
 
