f8fafdc7a6d260cea6c145643f4cf73631c81460
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-07-25 16:09:39 +0000

    Revert "[BE] remove torch deploy - conditionals (#158288)"
    This reverts commit ab26d4fbeb5bc4b4e6ef1c37fbec9fab6e5a9edd.
    
    Reverted https://github.com/pytorch/pytorch/pull/158288 on behalf of https://github.com/ZainRizvi due to Reverting as per offline discussion to fix internal breaks.  @PaliC will reland this as a codev diff. Instructions here: https://fburl.com/fixing-ghfirst-reverts ([comment](https://github.com/pytorch/pytorch/pull/158288#issuecomment-3119037960))

--- torch/distributed/_functional_collectives.py
+++ torch/distributed/_functional_collectives.py
@@ -19,16 +19,22 @@ except ImportError:
     from torch.utils._pytree import tree_map_only  # type: ignore[no-redef]
 
 
-try:
-    from torch.compiler import is_dynamo_compiling as is_torchdynamo_compiling
-except Exception:
-    warnings.warn(
-        "Unable to import torchdynamo util `is_torchdynamo_compiling`, so won't support torchdynamo correctly"
-    )
+if torch._running_with_deploy():
 
-    def is_torchdynamo_compiling():  # type: ignore[misc]
+    def is_torchdynamo_compiling():
+        """Can't import torchdynamo in torchdeploy builds currently."""
         return False
-        return False
+
+else:
+    try:
+        from torch.compiler import is_dynamo_compiling as is_torchdynamo_compiling
+    except Exception:
+        warnings.warn(
+            "Unable to import torchdynamo util `is_torchdynamo_compiling`, so won't support torchdynamo correctly"
+        )
+
+        def is_torchdynamo_compiling():
+            return False
 
 
 """
@@ -981,58 +987,66 @@ def _reduce_scatter_tensor_coalesced_native_meta(
     ]
 
 
-# Library MUST be defined at module scope or it doesn't work
-lib_impl = torch.library.Library("_c10d_functional", "IMPL")
-lib_impl.impl("all_reduce", _all_reduce_meta, "Meta")
-lib_impl.impl("all_reduce_", _all_reduce__meta, "Meta")
-lib_impl.impl("all_reduce_coalesced", _all_reduce_coalesced_meta, "Meta")
-lib_impl.impl("all_reduce_coalesced_", _all_reduce_coalesced__meta, "Meta")
-lib_impl.impl("wait_tensor", _wait_tensor_meta, "Meta")
-lib_impl.impl(
-    "all_gather_into_tensor_out", _all_gather_into_tensor_out_native_meta, "Meta"
-)
-lib_impl.impl("all_gather_into_tensor", _all_gather_into_tensor_native_meta, "Meta")
-lib_impl.impl(
-    "all_gather_into_tensor_coalesced",
-    _all_gather_into_tensor_coalesced_native_meta,
-    "Meta",
-)
-lib_impl.impl("reduce_scatter_tensor", _reduce_scatter_tensor_native_meta, "Meta")
-lib_impl.impl(
-    "reduce_scatter_tensor_coalesced",
-    _reduce_scatter_tensor_coalesced_native_meta,
-    "Meta",
-)
-lib_impl.impl("all_to_all_single", _all_to_all_single_meta, "Meta")
-lib_impl.impl("broadcast", _broadcast_meta, "Meta")
-lib_impl.impl("broadcast_", _broadcast__meta, "Meta")
+if not torch._running_with_deploy():
+    # Library MUST be defined at module scope or it doesn't work
+    # Creating a "DEF" Library always crashes torch::deploy so we create our
+    # Library instances here guarded against running inside it
+    lib_impl = torch.library.Library("_c10d_functional", "IMPL")
+    lib_impl.impl("all_reduce", _all_reduce_meta, "Meta")
+    lib_impl.impl("all_reduce_", _all_reduce__meta, "Meta")
+    lib_impl.impl("all_reduce_coalesced", _all_reduce_coalesced_meta, "Meta")
+    lib_impl.impl("all_reduce_coalesced_", _all_reduce_coalesced__meta, "Meta")
+    lib_impl.impl("wait_tensor", _wait_tensor_meta, "Meta")
+    lib_impl.impl(
+        "all_gather_into_tensor_out", _all_gather_into_tensor_out_native_meta, "Meta"
+    )
+    lib_impl.impl("all_gather_into_tensor", _all_gather_into_tensor_native_meta, "Meta")
+    lib_impl.impl(
+        "all_gather_into_tensor_coalesced",
+        _all_gather_into_tensor_coalesced_native_meta,
+        "Meta",
+    )
+    lib_impl.impl("reduce_scatter_tensor", _reduce_scatter_tensor_native_meta, "Meta")
+    lib_impl.impl(
+        "reduce_scatter_tensor_coalesced",
+        _reduce_scatter_tensor_coalesced_native_meta,
+        "Meta",
+    )
+    lib_impl.impl("all_to_all_single", _all_to_all_single_meta, "Meta")
+    lib_impl.impl("broadcast", _broadcast_meta, "Meta")
+    lib_impl.impl("broadcast_", _broadcast__meta, "Meta")
 
-# mark these ops has side effect so that they won't be removed by DCE
-torch.fx.node.has_side_effect(torch.ops._c10d_functional.wait_tensor.default)
-torch.fx.node.has_side_effect(torch.ops._c10d_functional.wait_tensor)
+    # mark these ops has side effect so that they won't be removed by DCE
+    torch.fx.node.has_side_effect(torch.ops._c10d_functional.wait_tensor.default)
+    torch.fx.node.has_side_effect(torch.ops._c10d_functional.wait_tensor)
 
-# Register legacy ops for backward compatibility
-# TODO(yifu): remove these in functional collective beta release
-legacy_lib = torch.library.Library("c10d_functional", "DEF")
-legacy_lib_impl = torch.library.Library("c10d_functional", "IMPL")
-ops_defs = [
-    "broadcast(Tensor self, int src, str tag, int[] ranks, int group_size) -> Tensor",
-    "all_reduce(Tensor self, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor",
-    "all_reduce_coalesced(Tensor[] self, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor[]",
-    "wait_tensor(Tensor self) -> Tensor",
-    "all_gather_into_tensor(Tensor shard, str tag, int[] ranks, int group_size) -> Tensor",
-    "all_gather_into_tensor_coalesced(Tensor[] input, str tag, int[] ranks, int group_size) -> Tensor[]",
-    "reduce_scatter_tensor(Tensor input, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor",
-    "reduce_scatter_tensor_coalesced(Tensor[] inputs, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor[]",
-    "all_to_all_single(Tensor input, SymInt[]? output_split_sizes, SymInt[]? input_split_sizes, str tag, int[] ranks, int group_size) -> Tensor",  # noqa: B950
-]
+    # Register legacy ops for backward compatibility
+    # TODO(yifu): remove these in functional collective beta release
+    legacy_lib = torch.library.Library("c10d_functional", "DEF")
+    legacy_lib_impl = torch.library.Library("c10d_functional", "IMPL")
+    ops_defs = [
+        "broadcast(Tensor self, int src, str tag, int[] ranks, int group_size) -> Tensor",
+        "all_reduce(Tensor self, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor",
+        "all_reduce_coalesced(Tensor[] self, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor[]",
+        "wait_tensor(Tensor self) -> Tensor",
+        "all_gather_into_tensor(Tensor shard, str tag, int[] ranks, int group_size) -> Tensor",
+        "all_gather_into_tensor_coalesced(Tensor[] input, str tag, int[] ranks, int group_size) -> Tensor[]",
+        "reduce_scatter_tensor(Tensor input, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor",
+        "reduce_scatter_tensor_coalesced(Tensor[] inputs, str reduceOp, str tag, int[] ranks, int group_size) -> Tensor[]",
+        "all_to_all_single(Tensor input, SymInt[]? output_split_sizes, SymInt[]? input_split_sizes, str tag, int[] ranks, int group_size) -> Tensor",  # noqa: B950
+    ]
 
-my_module = sys.modules[__name__]
-for op_def in ops_defs:
-    op_name = op_def[0 : op_def.index("(")]
-    backend_impl = getattr(fun_col_impl, f"_{op_name}")
-    legacy_lib.define(op_def, tags=torch.Tag.pt2_compliant_tag)
-    legacy_lib_impl.impl(op_name, backend_impl, "CompositeImplicitAutograd")
+    my_module = sys.modules[__name__]
+    for op_def in ops_defs:
+        op_name = op_def[0 : op_def.index("(")]
+        backend_impl = getattr(fun_col_impl, f"_{op_name}")
+        legacy_lib.define(op_def, tags=torch.Tag.pt2_compliant_tag)
+        legacy_lib_impl.impl(op_name, backend_impl, "CompositeImplicitAutograd")
+
+else:
+    warnings.warn(
+        "PyTorch Distributed functional collectives do not work with torch::deploy."
+    )
 
 
 """
