81431879314da0dc72127f1b9b6eb7229aa3bcef
Catherine Lee <csl@fb.com>
2025-07-24 18:51:24 -0700

    tc

--- test/inductor/test_inplace_padding.py
+++ test/inductor/test_inplace_padding.py
@@ -9,11 +9,11 @@ from torch._dynamo.utils import same
 from torch._inductor.test_case import run_tests, TestCase
 from torch._inductor.utils import run_and_get_code
 from torch.testing import FileCheck
+from torch.testing._internal.common_utils import serialTest
 from torch.testing._internal.inductor_utils import (
     GPU_TYPE,
     HAS_GPU,
     requires_cuda_with_enough_memory,
-    serialTest,
 )
 
 
