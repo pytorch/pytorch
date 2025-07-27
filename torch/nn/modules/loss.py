72a65cc9345a15a049cc24530493c1cfa6973c59
redwrasse <mail@redwrasse.io>
2025-07-26 22:26:21 -0700

    fix lint torch/nn/modules/loss.py

--- torch/nn/modules/loss.py
+++ torch/nn/modules/loss.py
@@ -2049,7 +2049,6 @@ class CTCLoss(_Loss):
         input_lengths: Tensor,
         target_lengths: Tensor,
     ) -> Tensor:
-        """Runs the forward pass."""
 
         return F.ctc_loss(
             inputs,
