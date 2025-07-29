Fix issue with launch grid calculation (#159388)
Summary:

per title

Test Plan:
CI

Rollback Plan:

Differential Revision: D79186985

torch/_inductor/runtime/triton_heuristics.py
@@ -2949,6 +2949,8 @@ class GridExpr:
 
     def __post_init__(self) -> None:
         assert self.mode in ("python", "cpp")
+        if self.mode == "python":
+            self.prefix.append("from torch.utils._sympy.functions import CeilDiv")
 
     def generate(self, meta: dict[str, int]) -> None:
         raise NotImplementedError
@@ -2961,8 +2963,7 @@ class GridExpr:
         if isinstance(numel, int) and isinstance(block, int):
             return ceildiv(numel, block)  # constant fold
         if self.mode == "python":
-            return f"-(({numel}) // -({block}))"
-        # trick above doesn't work in C++ due to rounding differences
+            return f"CeilDiv({numel}, {block})"
         return f"(({numel} + ({block} - 1)) / ({block}))"
 
     def maximum(self, seq: list[Union[int, str]]) -> Union[int, str]: