fcf59df2b6bc1fe7d5ef9f47b7f7da35a58efcb4
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-07-29 16:11:37 -0300

    [iter] Add support for sequence protocol in `iter(..)` (#156371)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/156371
    Approved by: https://github.com/zou3519

--- torch/_dynamo/polyfills/builtins.py
+++ torch/_dynamo/polyfills/builtins.py
@@ -58,3 +58,21 @@ def enumerate(iterable: Iterable[_T], start: int = 0) -> Iterable[tuple[int, _T]
 @substitute_in_graph(builtins.sum, can_constant_fold_through=True)  # type: ignore[arg-type]
 def sum(iterable: Iterable[_T], /, start: _T = 0) -> _T:  # type: ignore[assignment]
     return functools.reduce(operator.add, iterable, start)
+
+
+# TODO(guilhermeleobas): use substitute_in_graph for iter()
+def iter(iterable):  # type: ignore[no-untyped-def]
+    if hasattr(iterable, "__iter__"):
+        return iterable.__iter__()
+    if hasattr(iterable, "__getitem__"):
+        # Needs to be a new function to avoid iter_protocol becoming a generator
+        def sequence_protocol(iterable):  # type: ignore[no-untyped-def]
+            i = 0
+            while True:
+                try:
+                    yield iterable.__getitem__(i)
+                    i += 1
+                except IndexError:
+                    break
+
+        return sequence_protocol(iterable)
