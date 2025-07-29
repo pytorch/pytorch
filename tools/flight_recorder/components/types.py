c185a72558c9d2e9162b4fdb35fc7c9805ae8854
frost-intel <frost.mitchell@intel.com>
2025-07-29 20:12:47 +0000

    change nccl to ccl_backend

--- tools/flight_recorder/components/types.py
+++ tools/flight_recorder/components/types.py
@@ -388,9 +388,9 @@ class Op:
         self, event: dict[Any, Any], memberships: dict[str, set[Any]], pg_name: str
     ):
         self.profiling_name = event["profiling_name"]
-        nccl, name = self.profiling_name.split(":")
-        assert nccl in ["nccl", "xccl"], (
-            f"name formatting error? {nccl} != 'nccl' or 'xccl'"
+        ccl_backend, name = self.profiling_name.split(":")
+        assert ccl_backend in ["nccl", "xccl"], (
+            f"name formatting error? {ccl_backend} != 'nccl' or 'xccl'"
         )
         parts = name.split(" ")
         type = parts[0]
