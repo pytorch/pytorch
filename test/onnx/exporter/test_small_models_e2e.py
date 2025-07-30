32e05680b3dd1f9af13c97d38e4f313fb2fe35fb
Justin Chu <justinchu@microsoft.com>
2025-07-29 20:07:03 -0700

    opset_version
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- test/onnx/exporter/test_small_models_e2e.py
+++ test/onnx/exporter/test_small_models_e2e.py
@@ -771,7 +771,7 @@ class DynamoExporterNewOpsetsTest(common_utils.TestCase, _WithExport):
                 return torch.nn.functional.rms_norm(x, [10, 10])
 
         x = torch.randn(20, 5, 10, 10)
-        onnx_program = self.export(RMSNormModel2D(), (x,))
+        onnx_program = self.export(RMSNormModel2D(), (x,), opset_version=23)
         onnx_testing.assert_onnx_program(onnx_program, backend="reference")
 
     def test_rms_norm_with_weight(self):
