ccdb10fd69e3899f2336a4cc97b5a91e7ef9b6e1
omkar kakarparthi <75638701+okakarpa@users.noreply.github.com>
2025-07-31 18:08:48 -0500

    [AUTOGENERATED] [release/2.8] [ROCm] Use opportunistic fastatomics based on heuristics (#2441)

--- aten/src/ATen/native/cuda/KernelUtils.cuh
+++ aten/src/ATen/native/cuda/KernelUtils.cuh
@@ -281,6 +281,13 @@ __device__ __forceinline__ void opportunistic_fastAtomicAdd(
         }
     }
 
+    if (numel > 16 /*<-hueristic threshold*/ * 64 ) {
+      // well shucks, unlikely to capture same-dest atomics in a wave.
+      // fall back to direct fastAtomic...
+      fastAtomicAdd(self_ptr, index, numel, value, true);
+      return;
+    }
+
     // not coalsced, so now let try to capture lane-matches...
     // __activemask() -- finds the set of threads in the warp that are about to perform atomicAdd
     // __match_any_sync() -- returns bit mask of the threads that have same dest addr
