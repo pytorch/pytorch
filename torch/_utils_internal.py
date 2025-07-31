d6374b43ff2ddeebed0722426dfad93a0b62f6a9
Mengtian Xu <mengtian@meta.com>
2025-07-30 17:57:00 -0700

    [AIDIR] Internal util function to insert MLHub debugging insight for dynamic shape (#159391)
    Summary:
    Meta internal-only
    
    Add a util function to put dynamic shape-related suggestion to MLHubDebugInsightService, which will then be surfaced to users in the MLHub.
    
    Controlled by JKs
    
    Test Plan:
    MAST job aps-omnifmv3_dev_baseline_test-a34fdccf21 
    
    https://fburl.com/mlhub/gb5gwvvo 
     {F1980593060}
    
    * If you're not able to see the insight, please add yourself to this gk https://www.internalfb.com/intern/gatekeeper/projects/mlhub_debugging_insights_dev_visibility/
    * The URL link should route to a new Job Inspector page that will provide details and straight forward instructions of how to config the ds. The page is currently still in development so here we use the general PT2 compile JI page. 
    * Test fails because of the export checks. I'll export after addressing all the comments from reviewers.
    
    Rollback Plan:
    
    Reviewed By: jingsh, pianpwk
    
    Differential Revision: D78526522

--- torch/_utils_internal.py
+++ torch/_utils_internal.py
@@ -117,6 +117,10 @@ def signpost_event(category: str, name: str, parameters: dict[str, Any]):
     log.info("%s %s: %r", category, name, parameters)
 
 
+def add_mlhub_insight(category: str, insight: str, insight_description: str):
+    pass
+
+
 def log_compilation_event(metrics):
     log.info("%s", metrics)
 
