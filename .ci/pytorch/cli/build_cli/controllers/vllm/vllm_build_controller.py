40d2f466d2f3c5ebbb07c35930f155b66fa05d4b
Yang Wang <elainewy@meta.com>
2025-07-30 19:42:38 -0700

    setup pages
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- .ci/pytorch/cli/build_cli/controllers/vllm/vllm_build_controller.py
+++ .ci/pytorch/cli/build_cli/controllers/vllm/vllm_build_controller.py
@@ -1,6 +1,3 @@
-warning: Selection `PLW1507` has no effect because preview is not enabled.
-warning: Selection `RUF041` has no effect because preview is not enabled.
-warning: Selection `RUF048` has no effect because preview is not enabled.
 from cement import Controller, ex
 from lib.utils import generate_dataclass_help
 
