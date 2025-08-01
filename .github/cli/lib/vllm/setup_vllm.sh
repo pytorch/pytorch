338911383631e169f63a8d3d39965a8cd1682b2b
Yang Wang <elainewy@meta.com>
2025-08-01 13:13:47 -0700

    setup

--- .github/cli/lib/vllm/setup_vllm.sh
+++ .github/cli/lib/vllm/setup_vllm.sh
@@ -8,7 +8,7 @@ ls
 
 echo "Installing torch whls and vllm dependencies"
 
-pip install $(echo dist/torch-*.whl)[opt_einsum]
+pip install "$(echo dist/torch-*.whl)[opt_einsum]"
 pip install dist/vision/torchvision*.whl
 pip install dist/audio/torchaudio*.whl
 pip install wheels/xformers/xformers*.whl
@@ -19,28 +19,28 @@ git clone https://github.com/vllm-project/vllm.git
 cd vllm
 git checkout 29d1ffc5b4c763ef76aff9e3f617fa60dd292418
 git submodule update --init --recursive
-
 rm -rf vllm
 
 python3 -m pip install uv
-
 uv pip install --system -e tests/vllm_test_utils
-
 uv pip install --system hf_transfer
 export HF_HUB_ENABLE_HF_TRANSFER=1
 
+# Install common dependencies
 python3 use_existing_torch.py
-
 pip install -r requirements/common.txt
 pip install -r requirements/build.txt
 pip freeze | grep -E 'torch|xformers|torchvision|torchaudio|flashinfer'
 
+# clean the test.in file
+echo "Cleaning the test.in file"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "${SCRIPT_DIR}"
+bash "$SCRIPT_DIR/clean_test_in.sh.sh"
 
+echo "Installing test dependencies"
 uv pip compile  requirements/test.in -o  test.txt --index-strategy unsafe-best-match
 uv pip install --system -r test.txt
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-bash "$SCRIPT_DIR/clean_test_in.sh.sh"
 
 #95d8aba8a8c75aedcaa6143713b11e745e7cd0d9
 uv pip install --system --no-build-isolation "git+https://github.com/state-spaces/mamba@v2.2.4"
