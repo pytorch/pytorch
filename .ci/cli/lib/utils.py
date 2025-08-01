4ce19fc8458e10170a9740029597c18c6c5b6061
Yang Wang <elainewy@meta.com>
2025-08-01 01:33:41 -0700

    setup

---
+++ .ci/cli/lib/utils.py
@@ -0,0 +1,112 @@
+import os
+import shlex
+import shutil
+import subprocess
+import time
+from typing import Optional
+from dataclasses import fields
+from textwrap import indent
+from pathlib import Path
+import yaml
+
+def run(
+    cmd: str,
+    cwd: Optional[str] = None,
+    env: Optional[dict] = None,
+    logging: bool = True,
+):
+    if logging:
+        print(f">>> {cmd}",flush=True)
+    subprocess.run(shlex.split(cmd), check=True, cwd=cwd, env=env)
+
+#eliainwy
+def get_post_build_pinned_commit(name: str, prefix=".github/ci_commit_pins") -> str:
+    path = Path(prefix) / f"{name}.txt"
+    if not path.exists():
+        raise FileNotFoundError(f"Pin file not found: {path}")
+    return path.read_text(encoding="utf-8").strip()
+
+
+def get_env(name: str, default: str = "") -> str:
+    return os.environ.get(name, default)
+
+
+def force_create_dir(path: str):
+    """
+    Forcefully create a fresh directory.
+    If the directory exists, it will be removed first.
+    """
+    remove_dir(path)
+    ensure_dir_exists(path)
+
+
+def ensure_dir_exists(path: str):
+    """
+    Ensure the directory exists. Create it if necessary.
+    """
+    if not os.path.exists(path):
+        print(f"[INFO] Creating directory: {path}", flush=True)
+        os.makedirs(path, exist_ok=True)
+    else:
+        print(f"[INFO] Directory already exists: {path}", flush=True)
+
+
+def remove_dir(path: str):
+    """
+    Remove a directory if it exists.
+    """
+    if os.path.exists(path):
+        print(f"[INFO] Removing directory: {path}", flush=True)
+        shutil.rmtree(path)
+    else:
+        print(f"[INFO] Directory not found (skipped): {path}", flush=True)
+
+def get_abs_path(path: str):
+    return os.path.abspath(path)
+
+def generate_dataclass_help(cls) -> str:
+    """Auto-generate help text for dataclass default values."""
+    lines = []
+    for field in fields(cls):
+        default = field.default
+        if default is not None and default != "":
+            lines.append(f"{field.name:<22} = {repr(default)}")
+        else:
+            lines.append(f"{field.name:<22} = ''")
+    return indent("\n".join(lines), "    ")
+
+def get_existing_abs_path(path: str) -> str:
+    path = os.path.abspath(path)
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Path does not exist: {path}")
+    return path
+
+
+def clone_vllm(commit: str):
+    """
+    cloning vllm and checkout pinned commmit
+    """
+    print(f"clonening vllm....", flush=True)
+    cwd = "vllm"
+    # delete the directory if it exists
+    remove_dir(cwd)
+    # Clone the repo & checkout commit
+    run("git clone https://github.com/vllm-project/vllm.git")
+    run(f"git checkout {commit}", cwd)
+    run("git submodule update --init --recursive", cwd)
+
+def read_yaml_file( file_path: str) -> dict:
+    p = get_abs_path(file_path)
+    if not os.path.exists(p):
+        raise FileNotFoundError(f"YAML file not found: {file_path}")
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+
+def pip_install(package: str):
+    cmd = f"python3 -m pip install {package}"
+    subprocess.run(shlex.split(cmd), check=True)
+
+def uv_pip_install(package: str):
+    cmd = f"uv pip install --system {package}"
+    subprocess.run(shlex.split(cmd), check=True)
---
+++ .ci/cli/lib/vllm/setup_vllm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -ex
+# for torch nightly
+# https://download.pytorch.org/whl/nightly/torch/
+# torch-2.9.0.dev20250729+cu128-cp312-cp312-manylinux_2_28_x86_64.whl
+pip install $(echo dist/torch-*.whl)[opt_einsum]
+pip install dist/vision/torchvision*.whl
+pip install dist/audio/torchaudio*.whl
+pip install wheels/xformers/xformers*.whl
+pip install wheels/vllm/vllm*.whl
+pip install wheels/flashinfer-python/flashinfer*.whl
+
+git clone https://github.com/vllm-project/vllm.git
+cd vllm
+git checkout 29d1ffc5b4c763ef76aff9e3f617fa60dd292418
+git submodule update --init --recursive
+
+rm -rf vllm
+
+python3 -m pip install uv
+
+uv pip install --system -e tests/vllm_test_utils
+
+uv pip install --system hf_transfer
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+python3 use_existing_torch.py
+
+pip install -r requirements/common.txt
+pip install -r requirements/build.txt
+pip freeze | grep -E 'torch|xformers|torchvision|torchaudio|flashinfer'
+
+
+uv pip compile  requirements/test.in -o  test.txt --index-strategy unsafe-best-match
+uv pip install --system -r test.txt
+
+
+#95d8aba8a8c75aedcaa6143713b11e745e7cd0d9
+uv pip install --system --no-build-isolation "git+https://github.com/state-spaces/mamba@v2.2.4"
+
+export TORCH_CUDA_ARCH_LIST="8.0"
+python3 -c "from torch.utils.cpp_extension import _get_cuda_arch_flags as f; print(f())"
