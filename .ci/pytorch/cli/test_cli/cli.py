f818578c5f40a10519bc788a10c59f3e4aa55a8e
Yang Wang <elainewy@meta.com>
2025-07-31 19:57:54 -0700

    setup pages
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- .ci/pytorch/cli/test_cli/cli.py
+++ .ci/pytorch/cli/test_cli/cli.py
@@ -1,10 +1,15 @@
-from cement import App, Controller
+from cement import App, Controller, ex
 from test_cli.controllers import ALL_CONTROLLERS
+
 
 class MainController(Controller):
     class Meta:
         label = "base"
         help = "base for build CLI"
+
+    @ex(help="Show hello world")  # <-- 添加至少一个命令
+    def hello(self):
+        print("hello world")
 
 class BuildApp(App):
     class Meta:
@@ -15,3 +20,6 @@ class BuildApp(App):
 def main():
     with BuildApp() as app:
         app.run()
+
+if __name__ == "__main__":  # <-- 加上这句
+    main()
