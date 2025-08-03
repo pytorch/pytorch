e61a979b024185c327f533f21818d79cc7575276
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2025-08-03 13:00:48 +0400

    copy cutlass_mock_imports

--- setup.py
+++ setup.py
@@ -1679,6 +1679,8 @@ def main() -> None:
         "_inductor/codegen/*.h",
         "_inductor/codegen/aoti_runtime/*.h",
         "_inductor/codegen/aoti_runtime/*.cpp",
+        "_inductor/codegen/cuda/cutlass_lib_extensions/*.py",
+        "_inductor/codegen/cuda/cutlass_lib_extensions/**/*.py",
         "_inductor/script.ld",
         "_export/serde/*.yaml",
         "_export/serde/*.thrift",
