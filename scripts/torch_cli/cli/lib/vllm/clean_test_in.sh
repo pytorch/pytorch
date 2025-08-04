3cf15f46f96121bcb6a8f27065fd6b8027b38a99
Yang Wang <elainewy@meta.com>
2025-08-04 12:54:09 -0700

    setup

--- scripts/torch_cli/cli/lib/vllm/clean_test_in.sh
+++ scripts/torch_cli/cli/lib/vllm/clean_test_in.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 TARGET_FILE="requirements/test.in"
 TMP_HEAD=$(mktemp)
 
-PKGS=("torch" "torchvision" "torchaudio" "xformers" "mamba_ssm")
+PKGS=("torch" "torchvision" "torchaudio" "xformers" "mamba_ssm" "schemathesis")
 
 echo "[INFO] Scanning and removing the following local packages from $TARGET_FILE:"
 for pkg in "${PKGS[@]}"; do
@@ -18,14 +18,18 @@ for pkg in "${PKGS[@]}"; do
     # torch == ...
     # torch @ ...
     # torch@ ...
+    # torch>=...
     sed -i "/^${pkg}[[:space:]]*==/Id" "$TARGET_FILE"
     sed -i "/^${pkg}[[:space:]]*@/Id" "$TARGET_FILE"
+    sed -i "/^${pkg}[[:space:]]*>=/Id" "$TARGET_FILE"
   fi
 done
 
 echo "[INFO] Done."
 
 pip freeze | grep -iE '^(torch|torchvision|torchaudio)\s+@ file://' > "$TMP_HEAD"
+echo "schemathesis==3.39.15" >> "$TMP_HEAD"
+
 
 echo "" >> "$TMP_HEAD"
 cat "$TARGET_FILE" >> "$TMP_HEAD"
