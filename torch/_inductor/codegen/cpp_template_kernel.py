5361d722a0dbdf12877b5a21f0b3c372531b510b
Mu-Chu Lee <mlee8@meta.com>
2025-08-05 11:30:15 -0700

    Update on "[AOTInductor] ABI-Compatibility for RecordFunction."
    Summary:
    Previous our implementation for RecordFunction injects Aten into
    codegen, which is breaking the ABI contract for AOTInductor.
    
    C10::IValue is aded to call the full record function. The extension of
    more profiling info will come in later PRs.
    
    Test Plan:
    Included in commit.
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng amjames chauhang aakhundov coconutruben
    
    Differential Revision: [D79622071](https://our.internmc.facebook.com/intern/diff/D79622071)
    
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp_template_kernel.py
+++ torch/_inductor/codegen/cpp_template_kernel.py
@@ -190,8 +190,10 @@ class CppTemplateKernel(CppKernel):
         if config.cpp.enable_kernel_profile:
             graph_id = V.graph.graph_id
             prefix = "graph_" + str(graph_id) + "_" if graph_id is not None else ""
-            handle_str = ('torch::aot_inductor::RAIIAtenRecordFunctionHandle '
-                    f'record_{prefix}{self.kernel_name}_("{prefix}{self.kernel_name}", nullptr);')
+            handle_str = (
+                "torch::aot_inductor::RAIIAtenRecordFunctionHandle "
+                f'record_{prefix}{self.kernel_name}_("{prefix}{self.kernel_name}", nullptr);'
+            )
             return handle_str
         else:
             return ""
