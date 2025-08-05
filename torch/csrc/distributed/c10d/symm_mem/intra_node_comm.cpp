07b5dbe2202933abc43f494b63bd146be3ae27b1
Dev Sashidhar <dsashidh@redhat.com>
2025-08-05 17:29:15 +0000

    Move rocm_smi.h check to top-level CMakeLists.txt and fail early if missing

--- torch/csrc/distributed/c10d/symm_mem/intra_node_comm.cpp
+++ torch/csrc/distributed/c10d/symm_mem/intra_node_comm.cpp
@@ -2,7 +2,7 @@
 #include <torch/csrc/distributed/c10d/symm_mem/DMAConnectivity.hpp>
 #include <torch/csrc/distributed/c10d/symm_mem/intra_node_comm.hpp>
 
-#if defined(USE_ROCM) && defined(HAS_ROCM_SMI)
+#if defined(USE_ROCM)
 #include <rocm_smi/rocm_smi.h>
 #endif
 
