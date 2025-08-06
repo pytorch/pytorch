0a2fdf7c769930c0617948aefbc26687538bfb24
William Wen <williamwen@meta.com>
2025-08-06 11:32:19 -0700

    Update on "[dynamo] update pytorch-labs -> meta-pytorch in graph break URLs"
    Related PR: https://github.com/meta-pytorch/compile-graph-break-site/pull/30
    
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k pragupta voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames Lucaskabela
    
    [ghstack-poisoned]

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -15,12 +15,12 @@ from pathlib import Path
 from typing import *  # noqa: F403
 
 import numpy as np
+import yaml
 
 import torch._custom_ops as custom_ops
 import torch.testing._internal.optests as optests
 import torch.utils._pytree as pytree
 import torch.utils.cpp_extension
-import yaml
 from functorch import make_fx
 from torch import Tensor
 from torch._custom_op.impl import CustomOp, infer_schema
