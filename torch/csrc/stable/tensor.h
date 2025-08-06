7190f26b65160045cb12b672d47d03ca901855ea
Sam Anklesaria <sanklesaria@openteams.com>
2025-08-06 20:48:06 +0000

    Add is_cpu and dtype to stable tensor

--- torch/csrc/stable/tensor.h
+++ torch/csrc/stable/tensor.h
@@ -109,6 +109,20 @@ class Tensor {
     return device_type == aoti_torch_device_type_cuda();
   }
 
+  bool is_cpu() const {
+    int32_t device_type;
+    AOTI_TORCH_ERROR_CODE_CHECK(
+        aoti_torch_get_device_type(ath_.get(), &device_type));
+    return device_type == aoti_torch_device_type_cpu();
+  }
+
+  int32_t dtype() const {
+    int32_t ty;
+    AOTI_TORCH_ERROR_CODE_CHECK(
+        aoti_torch_get_dtype(ath_.get(), &ty));
+    return ty;
+  }
+
   int64_t size(int64_t dim) const {
     int64_t size;
     TORCH_ERROR_CODE_CHECK(aoti_torch_get_size(ath_.get(), dim, &size));
