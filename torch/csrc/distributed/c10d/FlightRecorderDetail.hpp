863dbaf293b1f8c629af176bcdd03c45ebd8fab7
frost-intel <frost.mitchell@intel.com>
2025-08-06 14:44:21 +0000

    Lint/fix

--- torch/csrc/distributed/c10d/FlightRecorderDetail.hpp
+++ torch/csrc/distributed/c10d/FlightRecorderDetail.hpp
@@ -425,8 +425,8 @@ std::string FlightRecorder<EventType>::dump_json(
     bool onlyActive) {
   json result;
   result[version_key_str] = version_val_str;
-  result[nccl_version_key_str_] = ccl_version_; // Keep nccl_version_key_str for BC
-  result[ccl_version_key_str_] = ccl_version_;
+  result[nccl_version_key_str] = ccl_version_; // Keep for BC
+  result[ccl_version_key_str] = ccl_version_;
   result[pg_config_key_str] = getPgConfigJson();
   result[pg_status_key_str] = getPgStatusJson();
 
@@ -523,8 +523,8 @@ std::string FlightRecorder<EventType>::dump(
   // common values
   result.insert(version_key, version_val);
   result.insert(pg_config_key, getPgConfig());
-  result.insert(nccl_version_key_str_, ccl_version_);
-  result.insert(ccl_version_key_str_, ccl_version_);
+  result.insert(nccl_version_key_str, ccl_version_);
+  result.insert(ccl_version_key_str, ccl_version_);
   result.insert(pg_status_key, getPgStatus());
 
   // collective trace
