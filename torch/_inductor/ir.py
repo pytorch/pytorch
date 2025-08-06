3b97edd857537c32df690cc72aaa6fdf6bab8779
angelayi <yiangela7@gmail.com>
2025-08-06 15:22:45 -0700

    [export] Fix custom ops in subgraphs

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -7568,7 +7568,7 @@ class FallbackKernel(ExternKernelAlloc):
             ),
         )
 
-        V.graph.extern_kernel_nodes.append(node)
+        V.extern_kernel_nodes.append(node)
 
         return [*args, *ordered_kwargs]
 
