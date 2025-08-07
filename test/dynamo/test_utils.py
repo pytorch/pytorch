e270263a3dc1ac7f38bfb2838066dba97eec3b3a
Jovian Anthony Jaison <38627145+jovianjaison@users.noreply.github.com>
2025-08-07 14:12:35 -0700

    Update test_utils.py

--- test/dynamo/test_utils.py
+++ test/dynamo/test_utils.py
@@ -257,7 +257,7 @@ class TestDynamoTimed(TestCase):
             stack_trace_list.append(e.stack_trace)
 
         self.assertGreater(len(stack_trace_list), 0)
-        result = '\n'.join(
+        result = "\n".join(
             item
             for sublist in stack_trace_list
             if sublist
