72be9cb2577ee33ecd937dc37efdfadb6ffb265a
Yang Wang <elainewy@meta.com>
2025-08-06 17:00:58 -0700

    update
    ghstack-source-id: 21a261de48a94f6d16c2f567f6483e013f03b554
    Pull-Request: https://github.com/pytorch/pytorch/pull/160027

--- scripts/torch_cli/cli/build_cli/__init__.py
+++
@@ -1,5 +0,0 @@
-from cli.build_cli.external.controller import ExternalBuildController
-from cli.build_cli.main import BuildController
-
-
-BUILD_CONTROLLERS = [BuildController, ExternalBuildController]
---
+++ scripts/torch_cli/cli/lib/logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+
+def setup_logging(level: int = logging.INFO):
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        stream=sys.stdout,
+    )
