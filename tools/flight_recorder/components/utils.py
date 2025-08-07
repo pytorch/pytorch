3dde3ca776e368346b9eccdfb2bc071fb0751d67
fduwjj <fduwjj@gmail.com>
2025-08-07 11:08:17 -0700

    [FR] Add stack_id and an optional print of stack_id to stack_trace mapping
    [ghstack-poisoned]

--- tools/flight_recorder/components/utils.py
+++ tools/flight_recorder/components/utils.py
@@ -616,6 +616,7 @@ def just_print_entries(
     _memberships: dict[str, set[Any]],
     _pg_guids: dict[tuple[str, int], str],
     args: argparse.Namespace,
+    stack_trace_id_map: dict[str, int],
 ) -> None:
     rows = []
     ranks = sorted(all_entries.keys())
@@ -650,6 +651,17 @@ def just_print_entries(
 
     logger.info(tabulate(rows, headers=headers))
 
+    if stack_trace_id_map and args.print_stack_trace:
+        headers = ["stack_trace_id", "frames"]
+        rows = []
+
+        for frame, trace_id in sorted(
+            stack_trace_id_map.items(), key=lambda item: item[1]
+        ):
+            rows.append([str(trace_id), frame])
+
+        logger.info(tabulate(rows, headers=headers))
+
 
 def check_no_missing_dump_files(
     entries: dict[int, Any], memberships: list[Membership]
@@ -675,6 +687,27 @@ def get_version_detail(version: str) -> tuple[int, int]:
     assert len(version) == 2, f"Invalid version {version}"
     major, minor = map(int, version)
     return major, minor
+
+
+def add_trace_id_in_entries(
+    entries: dict[int, list[dict[str, Any]]],
+) -> tuple[dict[int, list[dict[str, Any]]], dict[str, int]]:
+    stack_id = 0
+    stack_trace_id_map = {}
+    for rank in entries:
+        for dump in entries[rank]:
+            if dump.get("frames", []):
+                frames = str(dump["frames"])
+                if frames not in stack_trace_id_map:
+                    stack_trace_id_map[frames] = stack_id
+                    dump["trace_id"] = stack_id
+                    stack_id += 1
+                else:
+                    dump["trace_id"] = stack_trace_id_map[frames]
+            else:
+                dump["trace_id"] = -1
+
+    return entries, stack_trace_id_map
 
 
 def align_trace_from_beginning(
