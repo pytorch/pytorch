543bc62de8ed48326bd324ef9a00b265f1d1954b
Yang Wang <elainewy@meta.com>
2025-08-06 17:14:07 -0700

    [ghstack] setup torch_cli build
    ghstack-source-id: 477b6cac7fd1b9c30a8053c8b239897f82f36863
    Pull-Request: https://github.com/pytorch/pytorch/pull/160043

---
+++ scripts/torch_cli/cli/lib/vllm.py
@@ -0,0 +1,19 @@
+import logging
+
+from cli.lib.utils import read_yaml_file
+
+
+logger = logging.getLogger(__name__)
+
+
+def build_vllm(config_path: str = ""):
+    config_map = {}
+    if config_path:
+        logger.info("use config file user provided")
+        logger.info("Reading config yaml file ...")
+        config_map = read_yaml_file(config_path)
+        logger.info(f"config_map: {config_map}")
+    else:
+        logger.info("please input config file, otherwise use default config")
+    logger.info("running vllm build ....")
+    # TODO(elainewy): implement vllm build
