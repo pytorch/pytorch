04916adcc56927194bad1baa35a7ef06eccee081
FFFrog <ljw1101.vip@gmail.com>
2025-08-07 23:57:35 +0800

    Update (base update)
    [ghstack-poisoned]

--- test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Minimal.h
+++ test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Minimal.h
@@ -1,8 +1,8 @@
 #include "Common.h"
 
-namespace at::native {
+namespace at::native::openreg {
 
-at::Tensor empty_memory_format_openreg(
+at::Tensor empty_memory_format(
     c10::IntArrayRef size,
     std::optional<c10::ScalarType> dtype_opt,
     std::optional<c10::Layout> layout_opt,
@@ -10,7 +10,7 @@ at::Tensor empty_memory_format_openreg(
     std::optional<bool> pin_memory_opt,
     std::optional<c10::MemoryFormat> memory_format_opt);
 
-at::Tensor empty_strided_openreg(
+at::Tensor empty_strided(
     c10::IntArrayRef size,
     c10::IntArrayRef stride,
     std::optional<c10::ScalarType> dtype_opt,
@@ -18,50 +18,44 @@ at::Tensor empty_strided_openreg(
     std::optional<c10::Device> device_opt,
     std::optional<bool> pin_memory_opt);
 
-at::Tensor as_strided_openreg(
+at::Tensor as_strided(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     c10::SymIntArrayRef stride,
     std::optional<c10::SymInt> storage_offset);
 
-const at::Tensor& resize_openreg_(
+const at::Tensor& resize_(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     ::std::optional<at::MemoryFormat> memory_format);
 
-at::Tensor _reshape_alias_openreg(
+at::Tensor _reshape_alias(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     c10::SymIntArrayRef stride);
 
-at::Tensor _copy_from_openreg(
+at::Tensor _copy_from(
     const at::Tensor& self,
     const at::Tensor& dst,
     bool non_blocking);
 
-at::Tensor _copy_from_and_resize_openreg(
-    const at::Tensor& self,
-    const at::Tensor& dst);
+at::Tensor _copy_from_and_resize(const at::Tensor& self, const at::Tensor& dst);
 
-at::Scalar _local_scalar_dense_openreg(const at::Tensor& self);
+at::Scalar _local_scalar_dense(const at::Tensor& self);
 
-at::Tensor& set_source_Tensor_openreg_(
-    at::Tensor& self,
-    const at::Tensor& source);
+at::Tensor& set_source_Tensor_(at::Tensor& self, const at::Tensor& source);
 
-at::Tensor& set_source_Storage_openreg_(at::Tensor& self, at::Storage source);
+at::Tensor& set_source_Storage_(at::Tensor& self, at::Storage source);
 
-at::Tensor& set_source_Storage_storage_offset_openreg_(
+at::Tensor& set_source_Storage_storage_offset_(
     at::Tensor& result,
     at::Storage storage,
     int64_t storage_offset,
     c10::IntArrayRef size,
     c10::IntArrayRef stride);
 
-at::Tensor view_openreg(const at::Tensor& self, c10::SymIntArrayRef size);
+at::Tensor view(const at::Tensor& self, c10::SymIntArrayRef size);
 
-void cpu_fallback_openreg(
-    const c10::OperatorHandle& op,
-    torch::jit::Stack* stack);
+void cpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack);
 
-} // namespace at::native
+} // namespace at::native::openreg
