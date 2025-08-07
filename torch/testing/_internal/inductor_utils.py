23a779b90b0ca185dd5cd28fd53844019e76dc18
gaoyufeng <15834128411@126.com>
2025-08-07 23:23:41 +0800

    Resolve conflicts

--- torch/testing/_internal/inductor_utils.py
+++ torch/testing/_internal/inductor_utils.py
@@ -75,7 +75,7 @@ HAS_XPU_AND_TRITON = torch.xpu.is_available() and HAS_TRITON
 
 HAS_MPS = torch.mps.is_available()
 
-HAS_GPU = HAS_CUDA or HAS_XPU_AND_TRITON
+HAS_GPU = HAS_CUDA_AND_TRITON or HAS_XPU_AND_TRITON
 
 
 GPU_TYPE = get_gpu_type()
