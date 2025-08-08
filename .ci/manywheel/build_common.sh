89a5baca2ea40d66e995499b9f6c29f4e8779c2b
Eli Uriegas <eliuriegas@meta.com>
2025-08-08 11:14:05 -0700

    Update
    [ghstack-poisoned]

--- .ci/manywheel/build_common.sh
+++ .ci/manywheel/build_common.sh
@@ -476,7 +476,7 @@ if [[ -n "$PYTORCH_FINAL_PACKAGE_DIR" ]]; then
     if [[ -n "$BUILD_PYTHONLESS" ]]; then
         cp /$LIBTORCH_HOUSE_DIR/libtorch*.zip "$PYTORCH_FINAL_PACKAGE_DIR"
     else
-        if [[ "${USE_SEQUENTIAL:-0}" = "1" ]] then
+        if [[ "${USE_SEQUENTIAL:-0}" = "1" ]]; then
             cp /$WHEELHOUSE_DIR/*.log "$PYTORCH_FINAL_PACKAGE_DIR"
         fi
         cp /$WHEELHOUSE_DIR/torch*.whl "$PYTORCH_FINAL_PACKAGE_DIR"
