528f7b7ef22ae312f4de111d0e7bd3edb70ce9f3
zhaoguoan <zhaoguoan@ultrarisc.com>
2025-08-08 09:37:05 +0800

    aten: Add CPU feature detection support for RISC-V platform
    Add __riscv macro definition check in CPU feature detection functions
    to ensure that the RISC-V platform does not attempt to use x86-specific
    cpuinfo library and instruction set feature detection.
    
    Modified functions include:
    - is_avx2_supported()
    - is_avx512_supported()
    - is_avx512_vnni_supported()
    - is_avx512_bf16_supported()
    - is_amx_tile_supported()
    - is_amx_fp16_supported()
    - get_cache_size()
    
    These functions will return false or 0 on the RISC-V platform to avoid
    calling unsupported x86-specific functions.
    
    Signed-off-by: Zhao guoan<zhaoguoan@ultrarisc.com>

--- aten/src/ATen/cpu/Utils.cpp
+++ aten/src/ATen/cpu/Utils.cpp
@@ -1,6 +1,6 @@
 #include <cassert>
 #include <ATen/cpu/Utils.h>
-#if !defined(__s390x__ ) && !defined(__powerpc__)
+#if !defined(__s390x__ ) && !defined(__powerpc__) && !defined(__riscv)
 #include <cpuinfo.h>
 #endif
 #if defined(__linux__)
@@ -10,7 +10,7 @@
 
 namespace at::cpu {
 bool is_avx2_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return cpuinfo_initialize() && cpuinfo_has_x86_avx2();
 #else
   return false;
@@ -18,7 +18,7 @@ bool is_avx2_supported() {
 }
 
 bool is_avx512_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return cpuinfo_initialize() && cpuinfo_has_x86_avx512f() && cpuinfo_has_x86_avx512vl() && cpuinfo_has_x86_avx512bw() && cpuinfo_has_x86_avx512dq();
 #else
   return false;
@@ -26,7 +26,7 @@ bool is_avx512_supported() {
 }
 
 bool is_avx512_vnni_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return cpuinfo_initialize() && cpuinfo_has_x86_avx512vnni();
 #else
   return false;
@@ -34,7 +34,7 @@ bool is_avx512_vnni_supported() {
 }
 
 bool is_avx512_bf16_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return cpuinfo_initialize() && cpuinfo_has_x86_avx512bf16();
 #else
   return false;
@@ -42,7 +42,7 @@ bool is_avx512_bf16_supported() {
 }
 
 bool is_amx_tile_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return cpuinfo_initialize() && cpuinfo_has_x86_amx_tile();
 #else
   return false;
@@ -50,7 +50,7 @@ bool is_amx_tile_supported() {
 }
 
 bool is_amx_fp16_supported() {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   return is_amx_tile_supported() && cpuinfo_has_x86_amx_fp16();
 #else
   return false;
@@ -93,7 +93,7 @@ bool init_amx() {
 }
 
 static uint32_t get_cache_size(int level) {
-#if !defined(__s390x__) && !defined(__powerpc__)
+#if !defined(__s390x__) && !defined(__powerpc__) && !defined(__riscv)
   if (!cpuinfo_initialize()) {
     return 0;
   }
