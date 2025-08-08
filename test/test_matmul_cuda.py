280e0431b83a2afff5f20a9155b09a609a6f2de3
Eddie Yan <eddiey@nvidia.com>
2025-08-08 23:46:49 +0000

    address comments

--- test/test_matmul_cuda.py
+++ test/test_matmul_cuda.py
@@ -861,7 +861,7 @@ class TestMatmulCuda(InductorTestCase):
     @unittest.skipIf(not _get_torch_cuda_version() >= (12, 8), "Green Context only tested on 12.8+")
     def test_greencontext_carveout(self):
         a = torch.randn(4096, 4096, device='cuda', dtype=torch.bfloat16)
-        ctx = torch.cuda.green_contexts.GreenContext.create(num_sms=1, device_id=0)
+        ctx = torch.cuda.green_contexts.GreenContext.create(1, 0)
         ctx.make_current()
         torch.matmul(a, a)
         torch.cuda.synchronize()
