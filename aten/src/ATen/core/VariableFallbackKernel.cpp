0ad2a4980cbf5d6e07be9b003f0034bc633eff6f
FFFrog <ljw1101.vip@gmail.com>
2025-08-09 13:08:34 +0800

    Moved Autograd Fallback Interface to Header for Use by Out-of-tree Backends
    As the title stated.
    
    The new backend located in out-of-tree need to add custom operators as well,
    so alse need to implemet autograd fallback.
    
    ghstack-source-id: 8c019f4ba74ffccd6cb06045f0d219a769d269c6
    Pull-Request-resolved: https://github.com/pytorch/pytorch/pull/159639

--- aten/src/ATen/core/VariableFallbackKernel.cpp
+++ aten/src/ATen/core/VariableFallbackKernel.cpp
@@ -1,47 +1,7 @@
-#include <ATen/core/LegacyTypeDispatch.h>
-#include <ATen/core/dispatch/Dispatcher.h>
-#include <ATen/core/VariableHooksInterface.h>
-#include <torch/library.h>
+#include <ATen/core/VariableFallbackKernel.h>
 
-/*
- * This file implements a variable fallback kernel for custom operators.
- * Since tensors always have the Autograd set, but custom operators
- * usually don't have a kernel registered for Autograd, the dispatcher
- * will call into this fallback kernel instead.
- * Note that this is not a correct autograd implementation. It will just
- * fallthrough to the custom operator implementation.
- * If you want a custom operator to work with autograd, you need to use
- * autograd::Function so that the custom operator implementation knows how to
- * do autograd.
- * Note also that ops from native_functions.yaml register their own variable
- * kernels, so this is never called for them.
- */
+#ifndef C10_MOBILE
 
-// TODO This whole file should be deleted and replaced with the mechanism
-//      described in https://github.com/pytorch/pytorch/issues/29548
-
-using c10::Stack;
-
-namespace {
-
-#ifdef C10_MOBILE
-// NOTE [mobile/edge builds and the autograd fallback]
-// To save on binary size, some of the mobile configs don't include the
-// autograd kernels for built-in operators (VariableTypeEverything.cpp).
-// For the mobile build:
-// - we don't care about having a nice autograd fallback that warns if
-// an operator has incorrect autograd support. If you're running
-// a custom operator on mobile then it's already too late for us to warn
-// or error on it.
-// - for perf reasons, we do not want mobile to go through autograd_fallback
-// for all operators (the boxing/unboxing adds overhead).
-// As a result, on mobile we set the fallback to the fallthrough.
-#define AUTOGRAD_FALLBACK torch::CppFunction::makeFallthrough()
-#else
-
-// Register fallthrough for Autograd backends dispatch keys
-// NB: But not the private use ones; maybe the extension wants
-// to override it themselves!
 void autograd_fallback(
     const c10::OperatorHandle& op,
     c10::DispatchKeySet dispatch_keys,
@@ -54,11 +14,13 @@ void autograd_fallback(
     op.redispatchBoxed(dispatch_keys & c10::after_autograd_keyset, stack);
     return;
   }
-  at::impl::GetVariableHooks()->basic_autograd_not_implemented_fallback(op, dispatch_keys, stack);
+  at::impl::GetVariableHooks()->basic_autograd_not_implemented_fallback(
+      op, dispatch_keys, stack);
 }
 
-#define AUTOGRAD_FALLBACK torch::CppFunction::makeFromBoxedFunction<&autograd_fallback>()
 #endif
+
+namespace {
 
 TORCH_LIBRARY_IMPL(_, AutogradOther, m) {
   m.fallback(AUTOGRAD_FALLBACK);
@@ -108,7 +70,5 @@ TORCH_LIBRARY_IMPL(_, ADInplaceOrView, m) {
 TORCH_LIBRARY_IMPL(_, AutogradHPU, m) {
   m.fallback(AUTOGRAD_FALLBACK);
 }
-
-#undef AUTOGRAD_FALLBACK
 
 } // namespace
