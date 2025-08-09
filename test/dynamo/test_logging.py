9dc4bd3eaf1eb0d3874469a78f6989f72c491f9b
Xu Han <xu.han@outlook.com>
2025-08-09 08:01:22 +0800

    update code.

--- test/dynamo/test_logging.py
+++ test/dynamo/test_logging.py
@@ -24,6 +24,7 @@ from torch.testing._internal.common_utils import (
     IS_WINDOWS,
     munge_exc,
     skipIfTorchDynamo,
+    skipIfWindows,
     TEST_XPU,
     xfailIf,
 )
@@ -545,6 +546,7 @@ LoweringException: AssertionError:
         self.assertEqual(lines[-4], "Valid settings:")
 
     @requires_distributed()
+    @skipIfWindows(msg="TODO: (xuhancn), Can't reproduce locally")
     def test_distributed_rank_logging(self):
         env = dict(os.environ)
         env["TORCH_LOGS"] = "dynamo"
