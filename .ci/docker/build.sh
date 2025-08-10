af2d035f7d8686e49d3b11303c81a920f66005bf
cyy <cyyever@outlook.com>
2025-08-10 08:53:29 +0800

    Try debugging ONNXShapeTypeInference
    Signed-off-by: cyy <cyyever@outlook.com>

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -233,6 +233,7 @@ case "$tag" in
   pytorch-linux-jammy-py3-clang18-asan)
     ANACONDA_PYTHON_VERSION=3.10
     CLANG_VERSION=18
+    ONNX=yes
     VISION=yes
     ;;
   pytorch-linux-jammy-py3.10-gcc11)
