b4750fbb6814df699b122424bf4289e93d7aadfc
zeshengzong <zesheng.zong@outlook.com>
2025-08-12 02:08:54 +0000

    Update

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3477,6 +3477,29 @@ def binary_cross_entropy(
     r"""Compute Binary Cross Entropy between the target and input probabilities.
 
     See :class:`~torch.nn.BCELoss` for details.
+
+    Args:
+        input: Tensor of arbitrary shape as probabilities.
+        target: Tensor of the same shape as input with values between 0 and 1.
+        weight (Tensor, optional): a manual rescaling weight
+                if provided it's repeated to match input tensor shape
+        size_average (bool, optional): Deprecated (see :attr:`reduction`).
+        reduce (bool, optional): Deprecated (see :attr:`reduction`).
+        reduction (str, optional): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+            ``'mean'``: the sum of the output will be divided by the number of
+            elements in the output, ``'sum'``: the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: ``'mean'``
+        label_smoothing (float, optional): A float in [0.0, 1.0]. Specifies the amount
+            of smoothing when computing the loss, where 0.0 means no smoothing. The targets
+            become a mixture of the original ground truth and a uniform distribution as described in
+            `Rethinking the Inception Architecture for Computer Vision <https://arxiv.org/abs/1512.00567>`__. Default: :math:`0.0`.
+    Examples::
+        >>> input = torch.randn(3, 2, requires_grad=True)
+        >>> target = torch.rand(3, 2, requires_grad=False)
+        >>> loss = F.binary_cross_entropy(torch.sigmoid(input), target)
+        >>> loss.backward()
     """
     if has_torch_function_variadic(input, target, weight):
         return handle_torch_function(
@@ -3504,9 +3527,9 @@ def binary_cross_entropy(
         new_size = _infer_size(target.size(), weight.size())
         weight = weight.expand(new_size)
 
-    assert (
-        0 <= label_smoothing <= 1
-    ), f"label_smoothing must be between 0.0 and 1.0. Got: {label_smoothing}"
+    assert 0 <= label_smoothing <= 1, (
+        f"label_smoothing must be between 0.0 and 1.0. Got: {label_smoothing}"
+    )
 
     if label_smoothing > 0:
         target = target * (1 - label_smoothing) + (1 - target) * label_smoothing
@@ -3527,6 +3550,37 @@ def binary_cross_entropy_with_logits(
     r"""Compute Binary Cross Entropy between target and input logits.
 
     See :class:`~torch.nn.BCEWithLogitsLoss` for details.
+
+    Args:
+        input: Tensor of arbitrary shape as unnormalized scores (often referred to as logits).
+        target: Tensor of the same shape as input with values between 0 and 1
+        weight (Tensor, optional): a manual rescaling weight
+            if provided it's repeated to match input tensor shape
+        size_average (bool, optional): Deprecated (see :attr:`reduction`).
+        reduce (bool, optional): Deprecated (see :attr:`reduction`).
+        reduction (str, optional): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+            ``'mean'``: the sum of the output will be divided by the number of
+            elements in the output, ``'sum'``: the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: ``'mean'``
+        pos_weight (Tensor, optional): a weight of positive examples to be broadcasted with target.
+            Must be a tensor with equal size along the class dimension to the number of classes.
+            Pay close attention to PyTorch's broadcasting semantics in order to achieve the desired
+            operations. For a target of size [B, C, H, W] (where B is batch size) pos_weight of
+            size [B, C, H, W] will apply different pos_weights to each element of the batch or
+            [C, H, W] the same pos_weights across the batch. To apply the same positive weight
+            along all spatial dimensions for a 2D multi-class target [C, H, W] use: [C, 1, 1].
+            Default: ``None``
+        label_smoothing (float, optional): A float in [0.0, 1.0]. Specifies the amount
+            of smoothing when computing the loss, where 0.0 means no smoothing. The targets
+            become a mixture of the original ground truth and a uniform distribution as described in
+            `Rethinking the Inception Architecture for Computer Vision <https://arxiv.org/abs/1512.00567>`__. Default: :math:`0.0`.
+    Examples::
+         >>> input = torch.randn(3, requires_grad=True)
+         >>> target = torch.empty(3).random_(2)
+         >>> loss = F.binary_cross_entropy_with_logits(input, target)
+         >>> loss.backward()
     """
     if has_torch_function_variadic(input, target, weight, pos_weight):
         return handle_torch_function(
@@ -3551,9 +3605,9 @@ def binary_cross_entropy_with_logits(
             f"Target size ({target.size()}) must be the same as input size ({input.size()})"
         )
 
-    assert (
-        0 <= label_smoothing <= 1
-    ), f"label_smoothing must be between 0.0 and 1.0. Got: {label_smoothing}"
+    assert 0 <= label_smoothing <= 1, (
+        f"label_smoothing must be between 0.0 and 1.0. Got: {label_smoothing}"
+    )
 
     if label_smoothing > 0:
         target = target * (1 - label_smoothing) + (1 - target) * label_smoothing
