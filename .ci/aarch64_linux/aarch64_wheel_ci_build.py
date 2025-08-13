a1cb590399d85f1b15dcbfab34982c115459261c
Nikita Shulga <nshulga@meta.com>
2025-08-12 18:19:15 -0700

    Update
    [ghstack-poisoned]

--- .ci/aarch64_linux/aarch64_wheel_ci_build.py
+++ .ci/aarch64_linux/aarch64_wheel_ci_build.py
@@ -208,7 +208,7 @@ if __name__ == "__main__":
     build_vars = "CMAKE_SHARED_LINKER_FLAGS=-Wl,-z,max-page-size=0x10000 "
     # MAX_JOB=5 is not required for CPU backend (see commit 465d98b)
     if enable_cuda:
-        build_vars+ = "MAX_JOBS=5 "
+        build_vars += "MAX_JOBS=5 "
         #nvshmem is broken for aarch64 see https://github.com/pytorch/pytorch/issues/160425
         build_vars += "USE_NVSHMEM=OFF "
 
