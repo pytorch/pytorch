f15ada5c6fad97a7dcbfa4673f067b6942dda640
nandesuka <11392812+nandesuka@users.noreply.github.com>
2025-08-13 01:28:19 +0000

    Enable output padding when only outermost dim is dynamic (#159404)
    Summary: When the shape of the output tensor has a dynamic outer most dim, the stride can still be padded to conform to configured alignment if required.
    
    Test Plan:
    CI
    
    Rollback Plan:
    
    Differential Revision: D79146886
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/159404
    Approved by: https://github.com/blaine-rister, https://github.com/eellison

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3733,10 +3733,8 @@ class Layout(OutputSpec):
         # do for dynamic shape.
         #
         # Skip padding the strides for dynamic shape for now.
-        if not all(
-            isinstance(s, (int, sympy.Integer))
-            for s in itertools.chain(in_strides, size)
-        ):
+        # If outermost dim is dynamic, stride still can be fully static
+        if not all(isinstance(s, (int, sympy.Integer)) for s in in_strides):
             return in_strides
 
         stride_order = get_stride_order(in_strides)
@@ -3751,11 +3749,11 @@ class Layout(OutputSpec):
         for rank, idx in enumerate(fill_order[1:], start=1):
             prev_idx = fill_order[rank - 1]
             stride = new_strides[prev_idx] * size[prev_idx]
-
-            if stride > config.padding_stride_threshold and stride % align != 0:
-                stride = ceildiv(stride, align) * align
-                padded = True
-            new_strides[idx] = stride
+            if isinstance(stride, (int, sympy.Integer)):
+                if stride > config.padding_stride_threshold and stride % align != 0:
+                    stride = ceildiv(stride, align) * align
+                    padded = True
+                new_strides[idx] = stride
 
         if not padded:
             # Consider a tensor with shape [256, 1, 5, 5]
