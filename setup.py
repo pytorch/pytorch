7c6e0c36b82f780dce4a055fa09c8d4eb6e9bfa8
FFFrog <ljw1101.vip@gmail.com>
2025-08-13 17:53:45 +0800

    Update
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -1194,16 +1194,6 @@ class build_ext(setuptools.command.build_ext.build_ext):
         else:
             report("-- Not using ITT")
 
-        # Do not use clang to compile extensions if `-fstack-clash-protection` is defined
-        # in system CFLAGS
-        c_flags = os.getenv("CFLAGS", "")
-        if (
-            IS_LINUX
-            and "-fstack-clash-protection" in c_flags
-            and "clang" in os.getenv("CC", "")
-        ):
-            os.environ["CC"] = str(os.environ["CC"])
-
         super().run()
 
         if IS_DARWIN:
