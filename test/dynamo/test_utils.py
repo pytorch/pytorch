2781fa9e7dae74b69ee4ec0e4e424ef5f34c0563
Jovian Anthony Jaison <38627145+jovianjaison@users.noreply.github.com>
2025-08-13 15:42:41 -0700

    Update test_utils.py

--- test/dynamo/test_utils.py
+++ test/dynamo/test_utils.py
@@ -282,10 +282,12 @@ class TestDynamoTimed(TestCase):
             self.run_forward_backward()
             compilation_events = [arg[0][0] for arg in log_event.call_args_list]
 
-        self.assertEqual(
-            compilation_events[0].graph_node_shapes,
-            "{'l_self_modules_linear_parameters_weight_': [1, 3], 'l_self_modules_linear_parameters_bias_': [1], 'l_x_': [3], 'linear': [1]}",
-        )
+    self.assertEqual(
+        compilation_events[0].graph_node_shapes,
+        "{'l_self_modules_linear_parameters_weight_': [1, 3], "
+        "'l_self_modules_linear_parameters_bias_': [1], "
+        "'l_x_': [3], 'linear': [1]}"
+    )
 
     @dynamo_config.patch(
         {
