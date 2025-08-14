cae8ab9ca8aa071a2eff41101afd9d5a6fb85b4b
Yang Wang <elainewy@meta.com>
2025-08-13 22:01:42 -0700

    Update
    [ghstack-poisoned]

--- .ci/lumen_cli/cli/lib/core/vllm.py
+++ .ci/lumen_cli/cli/lib/core/vllm.py
@@ -33,7 +33,7 @@ from cli.lib.common.pip_helper import (
     pip_install_packages,
     run_python,
 )
-from cli.lib.common.utils import run_command, temp_environ, working_directory, run_cmd
+from cli.lib.common.utils import run_command, temp_environ, working_directory
 
 
 logger = logging.getLogger(__name__)
@@ -335,7 +335,7 @@ class VllmTestRunner(BaseRunner):
             self._install_wheels(params)
             self._install_dependencies()
         # verify the torches are not overridden by test dependencies
-        # self.check_versions()
+        self.check_versions()
 
     def run(self):
         """
@@ -481,8 +481,7 @@ class VllmTestRunner(BaseRunner):
         logger.info("Running tests: %s", tests["title"])
 
         pkgs = tests.get("package_install", [])
-        if pkgs:
-            pip_install_packages(requirements=pkgs, prefer_uv=True)
+        pip_install_packages(packages=pkgs, prefer_uv=True)
         with (
             temp_environ(tests.get("env_var", {})),
             working_directory(tests.get("working_directory", "tests")),
@@ -578,9 +577,6 @@ def sample_test_plans():
             # required: command
             # available fields: env_var (env_var only set within the scope of the test step), package_install(pip package)
             "steps": [
-                {
-                    "command": "echo $VLLM_WORKER_MULTIPROC_METHOD"
-                },
                 {
                     "command": "pytest -v -s basic_correctness/test_cumem.py",
                 },
