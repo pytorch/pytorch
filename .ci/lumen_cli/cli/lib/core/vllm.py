9e0e5a43bec00dcc2dd33063027ba32ff476b97e
Yang Wang <elainewy@meta.com>
2025-08-14 00:30:42 -0700

    Update
    [ghstack-poisoned]

--- .ci/lumen_cli/cli/lib/core/vllm.py
+++ .ci/lumen_cli/cli/lib/core/vllm.py
@@ -481,7 +481,9 @@ class VllmTestRunner(BaseRunner):
         logger.info("Running tests: %s", tests["title"])
 
         pkgs = tests.get("package_install", [])
-        pip_install_packages(packages=pkgs, prefer_uv=True)
+        if pkgs:
+            logger.info("Installing packages: %s", pkgs)
+            pip_install_packages(packages=pkgs, prefer_uv=True)
         with (
             temp_environ(tests.get("env_var", {})),
             working_directory(tests.get("working_directory", "tests")),
