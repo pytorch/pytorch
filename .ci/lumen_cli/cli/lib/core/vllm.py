4e4e1f81b3665510472d72570668bf15c742a5b1
Yang Wang <elainewy@meta.com>
2025-08-13 22:31:19 -0700

    add test

--- .ci/lumen_cli/cli/lib/core/vllm.py
+++ .ci/lumen_cli/cli/lib/core/vllm.py
@@ -481,7 +481,7 @@ class VllmTestRunner(BaseRunner):
         logger.info("Running tests: %s", tests["title"])
 
         pkgs = tests.get("package_install", [])
-        pip_install_packages(requirements=pkgs, prefer_uv=True)
+        pip_install_packages(packages=pkgs, prefer_uv=True)
         with (
             temp_environ(tests.get("env_var", {})),
             working_directory(tests.get("working_directory", "tests")),
