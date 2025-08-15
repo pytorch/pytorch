a5cc327908b671cde626a6b146c2aed30034b171
Simon Fan <xmfan@meta.com>
2025-08-14 18:13:03 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/external_utils.py
+++ torch/_dynamo/external_utils.py
@@ -251,7 +251,9 @@ def wrap_inline_with_set_fullgraph(
     return wrapper
 
 
-def call_input_buffer_accumulate(old_grad, grad):
+def call_input_buffer_accumulate(
+    old_grad: Optional[torch.Tensor], grad: Optional[torch.Tensor]
+) -> Optional[torch.Tensor]:
     if old_grad is None:
         return grad
     if grad is None:
