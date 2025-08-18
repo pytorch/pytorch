dc4a94f2aa69817fa69df13a69f9a7403cdb35d3
Animesh Jain <anijain@umich.edu>
2025-08-17 23:10:43 -0700

    [dynamo][guards] Remove long lines from TORCH_LOGS=guards
    [ghstack-poisoned]

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -756,6 +756,13 @@ def get_verbose_code_part(code_part: str, guard: Optional[Guard]) -> str:
             for fs in reversed(guard.user_stack):
                 if fs.filename not in uninteresting_files():
                     extra = f"  # {format_frame(fs, line=True)}"
+                    if len(extra) > 1024:
+                        # For fx graphs, the line can be very long in case of
+                        # torch.stack ops, where many inputs are set to None
+                        # after the operation.  This increases the size of the
+                        # guards log file.  In such cases, do not print the line
+                        # contents.
+                        extra = f"  # {format_frame(fs)}"
                     break
         elif guard.stack:
             summary = guard.stack.summary()
