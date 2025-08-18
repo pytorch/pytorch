2ede2cc1147f847172a6422f6fd69f996242be70
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-08-18 06:14:35 -0700

    Update base for Update on "[inductor] reorder, heuristics for reserved memory regression"
    Problem:
    sink_waits pass regressed "reserved" memory, preserving "active".
    
    The reason was that Wait deallocates the input of Collective.
    So sinking too far can preserve peak memory, but regress integral level of allocated memory, as a result it regresses "reserved".
    
    We need guardrails using runtime estimations , but we do not have them now.
    As a workaround - config settings to set limits in number of gemm_like ops that we sink/prefetch.
    
    Another WAR - not sinking beyond next collective (on the same group).
    
    In future we should replace this WARs after having estimate runtime ops.
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov coconutruben
    
    Differential Revision: [D80178878](https://our.internmc.facebook.com/intern/diff/D80178878)
    
    [ghstack-poisoned]

--- torch/_inductor/comms_debug.py
+++ torch/_inductor/comms_debug.py
@@ -18,11 +18,11 @@ def _debug_iterative_memory_recompute(
     candidate: BaseSchedulerNode,
     gns: list[BaseSchedulerNode],
     group_names: str,
-    _snodes: list[BaseSchedulerNode],
+    snodes: list[BaseSchedulerNode],
     name_to_freeable_input_buf: dict[str, FreeableInputBuffer],
     graph_outputs: OrderedSet[str],
     peak_memory: int,
-    _curr_memory: dict[BaseSchedulerNode, tuple[int, int]],
+    iter_curr_memory: dict[BaseSchedulerNode, tuple[int, int]],
     snodes_allocfree: dict[BaseSchedulerNode, SNodeMemory],
     tlparse_name: str,
     gn_to_bufs_last_use: dict[
@@ -31,24 +31,24 @@ def _debug_iterative_memory_recompute(
 ) -> bool:
     iterative_recompute_error = False
     candidate_allocfree = snodes_allocfree[candidate]
-    _peak_memory, _snodes_curr_memory, _snodes_allocfree, _ = (
+    est_peak_memory, snodes_curr_memory, snodes_allocfree, _ = (
         estimate_peak_memory_allocfree(
-            _snodes, name_to_freeable_input_buf, graph_outputs
+            snodes, name_to_freeable_input_buf, graph_outputs
         )
     )
-    __curr_memory = dict(zip(_snodes, _snodes_curr_memory))
-    iter_cm = _curr_memory[candidate]
-    new_cm = __curr_memory[candidate]
+    est_curr_memory = dict(zip(snodes, snodes_curr_memory))
+    iter_cm = iter_curr_memory[candidate]
+    new_cm = est_curr_memory[candidate]
     log = ""
-    if _peak_memory > peak_memory:
+    if est_peak_memory > peak_memory:
         log = "ITERATIVE PEAK DOES NOT MATCH"
         iterative_recompute_error = True
     if iter_cm != new_cm:
         log = "ITERATIVE CURR MEMORY CANDIDATE DOES NOT MATCH"
         iterative_recompute_error = True
     for i, gn in enumerate(gns):
-        iter_gnm = _curr_memory[gn]
-        new_gnm = __curr_memory[gn]
+        iter_gnm = iter_curr_memory[gn]
+        new_gnm = est_curr_memory[gn]
         if iter_gnm != new_gnm:
             log = f"ITERATIVE GN CURR MEMORY DOES NOT MATCH:{gn.get_name()}"
             iterative_recompute_error = True
@@ -57,32 +57,32 @@ def _debug_iterative_memory_recompute(
             f"\nCANDIDATE:{candidate.get_name()}"
             f"\nGROUP:{group_names}"
             f"\nPEAK_MEMORY_BEFORE:{peak_memory}"
-            f"\nPEAK_MEMORY_AFTER_SWAP:{_peak_memory}"
+            f"\nPEAK_MEMORY_AFTER_SWAP:{est_peak_memory}"
             f"\nCANDIDATE:{candidate.debug_str()}"
             f"\nCANDIDATE_ITER_CURR_MEMORY:{iter_cm}"
             f"\nCANDIDATE_NEW__CURR_MEMORY:{new_cm}"
             f"\nCANDIDATE_ITER_ALLOCFREE:{candidate_allocfree}"
-            f"\nCANDIDATE_NEW_ALLOCFREE:{_snodes_allocfree[candidate]}"
+            f"\nCANDIDATE_NEW_ALLOCFREE:{snodes_allocfree[candidate]}"
         )
         peak_log = ""
-        for i, (pre, post) in enumerate(_snodes_curr_memory):
-            if _peak_memory == pre:
-                n = _snodes[i]
+        for i, (pre, post) in enumerate(snodes_curr_memory):
+            if est_peak_memory == pre:
+                n = snodes[i]
                 peak_log = (
-                    f"\nNEW_PEAK:{_peak_memory}(BASE:{peak_memory})"
-                    f" @ SNODE[{i}/{len(_snodes)}]:{n.get_name()} {n.debug_str()}"
+                    f"\nNEW_PEAK:{est_peak_memory}(BASE:{peak_memory})"
+                    f" @ SNODE[{i}/{len(snodes)}]:{n.get_name()} {n.debug_str()}"
                 )
                 break
         group_log = ""
         for i, gn in enumerate(gns):
-            iter_gnm = _curr_memory[gn]
-            new_gnm = __curr_memory[gn]
+            iter_gnm = iter_curr_memory[gn]
+            new_gnm = est_curr_memory[gn]
             group_log += (
                 f"\nGROUP_NODE[{i}]:{gn.debug_str()}"
                 f"\nGROUP_NODE[{i}] ITER_GNM[{gn.get_name()}]:{iter_gnm}"
                 f"\nGROUP_NODE[{i}] ESTM_GNM[{gn.get_name()}]:{new_gnm}"
-                f"\nGROUP_NODE[{i}] ITER_allocfree:{_snodes_allocfree[gn]}"
-                f"\nGROUP_NODE[{i}] ESTM_allocfree:{_snodes_allocfree[gn]}"
+                f"\nGROUP_NODE[{i}] ITER_allocfree:{snodes_allocfree[gn]}"
+                f"\nGROUP_NODE[{i}] ESTM_allocfree:{snodes_allocfree[gn]}"
             )
         log += peak_log
         log += group_log
@@ -91,12 +91,12 @@ def _debug_iterative_memory_recompute(
             [
                 (
                     f"\nSNODE[{i}]\n{n.debug_str()}"
-                    f"\nITER_cur_mem:{_curr_memory[n]}"
-                    f"\nESTM_cur_mem:{__curr_memory[n]}"
+                    f"\nITER_cur_mem:{iter_curr_memory[n]}"
+                    f"\nESTM_cur_mem:{est_curr_memory[n]}"
                     f"\nITER_allocfree:{snodes_allocfree[n]}"
-                    f"\nESTM_allocfree:{_snodes_allocfree[n]}"
+                    f"\nESTM_allocfree:{snodes_allocfree[n]}"
                 )
-                for i, n in enumerate(_snodes)
+                for i, n in enumerate(snodes)
             ]
         )
         tname = f"{tlparse_name}_ITERATIVE_RECOMPUTE_ERROR"
