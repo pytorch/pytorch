4f6e4710b554c0220530f86a1ecadbe79a72b38a
bobrenjc93 <bobren@meta.com>
2025-08-19 14:33:27 -0700

    Update on "add hint_override kwarg to mark_dynamic"
    cc ezyang SherlockNoMad EikanWang jgong5 wenzhe-nrv voznesenskym penguinwu Guobing-Chen XiaobingSuper zhuhaozhe blzheng jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov coconutruben Lucaskabela
    
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -10578,7 +10578,7 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
         def override(x):
             return x.sum(dim=0)
 
-        x_small = torch.randn(4096, 512)
+        x_small = torch.randn(4096, 512, device=GPU_TYPE)
         no_override(x_small)
         torch._dynamo.decorators.mark_dynamic(x_small, 0, hint_override=4096 * 1000)
         override(x_small)
