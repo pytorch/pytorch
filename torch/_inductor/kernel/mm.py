pytorch/pytorch

commit 723ab9a3653968cb5255461f0458ec15f932d026

[Triton] [Inductor] Remove deadcode that references an incorrect descriptor with the old TMA API (#159407)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/159407

Fixes a clear template typo where `a_desc_ptr` was passed instead of `b_desc_ptr` to define `b_desc`.

Test Plan:
Tested via TritonBench with `TORCHINDUCTOR_MAX_AUTOTUNE=1 TORCHINDUCTOR_FORCE_DISABLE_CACHES=1 buck2 run mode/opt -m ovr_config//triton:legacy  //pytorch/tritonbench:run -c fbcode.enable_gpu_sections=true -- --op fp8_gemm --iter 10000 --m 4096 --k 1024 --n 2048 --only pt2_fp8_gemm`. I manually pruned the configs and set the debug flag to print the generated code.

I'll land the TritonBench change in a followup.

Rollback Plan:

Reviewed By: NikhilAPatel, NoamPaz

Differential Revision: D79178538

torch/_inductor/kernel/mm.py
@@ -284,8 +284,6 @@ persistent_tma_mm_template = TritonTemplate(
     tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(a_desc_ptr)
     tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(b_desc_ptr)
 
-    a_desc = a_desc_ptr
-    b_desc = b_desc_ptr
     {%- else %}
     a_desc = triton.language.make_tensor_descriptor(
         base=A,
@@ -464,8 +462,6 @@ device_tma = r"""
     tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(a_desc_ptr)
     tl.extra.cuda.experimental_tensormap_fenceproxy_acquire(b_desc_ptr)
 
-    a_desc = a_desc_ptr
-    b_desc = a_desc_ptr
     {%- else %}
     a_desc = triton.language.make_tensor_descriptor(
         base=A,

commit 6427332c7308c2bce3e5202a4c0dc38a3ad903e3

[BE] Fix old TMA API in persistent matmul template
Summary: Fixes a bug introduced by https://github.com/pytorch/pytorch/pull/159407

Test Plan:
NA

Rollback Plan:

Differential Revision: D80588320

torch/_inductor/kernel/mm.py
@@ -323,13 +323,13 @@ persistent_tma_mm_template = TritonTemplate(
 
         {%- if TMA_EXPERIMENTAL_API %}
         a = tl._experimental_descriptor_load(
-            a_desc,
+            a_desc_ptr,
             [rm, rk] if A_ROW_MAJOR else [rk, rm],
             [BLOCK_M, BLOCK_K] if A_ROW_MAJOR else [BLOCK_K, BLOCK_M],
             A.dtype.element_ty,
         )
         b = tl._experimental_descriptor_load(
-            b_desc,
+            b_desc_ptr,
             [rk, rn] if B_ROW_MAJOR else [rn, rk],
             [BLOCK_K, BLOCK_N] if B_ROW_MAJOR else [BLOCK_N, BLOCK_K],
             B.dtype.element_ty,