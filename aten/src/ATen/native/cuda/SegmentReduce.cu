b45454a58d68a56bac3425395f0094cab06f297d
Ting Lu <tingl@nvidia.com>
2025-08-21 09:03:56 -0700

    fix spacing

--- aten/src/ATen/native/cuda/SegmentReduce.cu
+++ aten/src/ATen/native/cuda/SegmentReduce.cu
@@ -18,9 +18,9 @@
 #include <ATen/ops/cumsum.h>
 #endif
 
-// SegmentReduce compilation with CUDA-12.9 causes  NVCC crash on Windows	
-// See https://github.com/pytorch/pytorch/issues/156181	
-#if !defined(_WIN32) || CUDART_VERSION < 12090	
+// SegmentReduce compilation with CUDA-12.9 causes  NVCC crash on Windows
+// See https://github.com/pytorch/pytorch/issues/156181
+#if !defined(_WIN32) || CUDART_VERSION < 12090
 
 namespace at::native {
 
