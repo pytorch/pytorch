b5b1b106e93fe9d47faa2a6ef07d85ad65dabb82
Robert Hardwick <robert.hardwick@arm.com>
2025-08-21 10:08:02 +0000

    Update (base update)
    [ghstack-poisoned]

--- tools/setup_helpers/generate_linker_script.py
+++ tools/setup_helpers/generate_linker_script.py
@@ -1,11 +1,13 @@
+import argparse
 import os
 import subprocess
+from pathlib import Path
 
 
 def gen_linker_script(
-    filein: str = "cmake/prioritized_text.txt", fout: str = "cmake/linker_script.ld"
+    file_in: str = "cmake/prioritized_text.txt", file_out: str = "linker_script.ld"
 ) -> None:
-    with open(filein) as f:
+    with open(file_in) as f:
         prioritized_text = f.readlines()
         prioritized_text = [
             line.replace("\n", "") for line in prioritized_text if line != "\n"
@@ -28,7 +30,11 @@ def gen_linker_script(
     assert len(text_line_start) == 1, "The linker script has multiple text sections!"
     text_line_start = text_line_start[0]
 
-    with open(fout, "w") as f:
+    # ensure that parent directory exists before writing
+    file_out = Path(file_out)
+    file_out.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(file_out, "w") as f:
         for lineid, line in enumerate(linker_script_lines):
             if lineid == text_line_start + 2:
                 f.write("    *(\n")
@@ -36,3 +42,20 @@ def gen_linker_script(
                     f.write(f"      .text.{plines}\n")
                 f.write("    )\n")
             f.write(f"{line}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate linker file based on prioritized symbols. Used for link-time optimization.",
+    )
+    parser.add_argument(
+        "--file-in",
+        help="Path to prioritized_text.txt input file",
+        default=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--file-out", help="Output path for linker ld file", default=argparse.SUPPRESS
+    )
+    # convert args to a dict to pass to gen_linker_script
+    kwargs = vars(parser.parse_args())
+    gen_linker_script(**kwargs)
