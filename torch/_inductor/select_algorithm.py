8a7cba66520f56399d6c7471a5677e0bd38be9c7
Wenyuan Chi <wychi@meta.com>
2025-08-20 18:31:00 -0700

    [Inductor] Prune configs that require more shared memory than the hardware limit (#161040)
    Summary:
    
    This diff removes configurations that exceed the hardware shared memory limit, which causes the following compilation error:
    ```
    No valid triton configs. OutOfMemoryError: out of resource: triton_mm Required: 327680 Hardware limit:232448 Reducing block sizes or `num_stages` may help.
    ```
    
    Test Plan:
    ```
    buck2 test mode/dev-nosan fbcode//caffe2/test/inductor:max_autotune -- test_max_autotune_prune_choices -v 1,stderr
    ```
    
    Rollback Plan:
    
    Reviewed By: eellison
    
    Differential Revision: D80594562

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -2756,6 +2756,9 @@ class AlgorithmSelectorCache(PersistentCache):
                 timeout=precompilation_timeout_seconds,
             ):
                 if e := future.exception():
+                    counters["inductor"][
+                        "select_algorithm_num_precompilation_exceptions"
+                    ] += 1
                     exceptions.append((futures[future], e))
                     from torch._inductor.codegen.cuda.cuda_kernel import (
                         CUDATemplateCaller,
