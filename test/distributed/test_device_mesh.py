7b6af21705165adbc4a1adcd8ddf9cfe182a7e39
fduwjj <fduwjj@gmail.com>
2025-08-22 07:28:40 -0700

    [WIP][device_mesh] Implement  on top of CuTe layout bookkeeping
    [ghstack-poisoned]

--- test/distributed/test_device_mesh.py
+++ test/distributed/test_device_mesh.py
@@ -889,6 +889,44 @@ class TestDeviceMeshGetItem(DTensorTestBase):
         self.assertEqual(_mesh_resources.get_root_mesh(dp_cp_mesh), mesh_4d)
 
     @with_comms
+    def test_unflatten_mesh_3d(self):
+        mesh_shape = (4, 2)
+        mesh_dim_names = ("dp", "tp")
+        mesh_2d = init_device_mesh(
+            self.device_type, mesh_shape, mesh_dim_names=mesh_dim_names
+        )
+        unflatten_mesh = mesh_2d._unflatten(0, (2, 2), ("dp_shard", "dp_replicate"))
+        self.assertEqual(
+            unflatten_mesh.mesh_dim_names, ["dp_shard", "dp_replicate", "tp"]
+        )
+        self.assertEqual(mesh_2d["tp"].mesh, unflatten_mesh["tp"].mesh)
+        self.assertEqual(mesh_2d["dp_shard"].mesh, unflatten_mesh["dp_shard"].mesh)
+        # Existing unflatten dim name should not create a new pg.
+        self.assertEqual(
+            get_mesh_pg_names(mesh_2d["tp"]), get_mesh_pg_names(unflatten_mesh["tp"])
+        )
+
+        # # Not supporting flatten a unflattened mesh.
+        # with self.assertRaises(NotImplementedError):
+        #     mesh_2d["dp_shard", "dp_replicate"]._flatten()
+
+        # # Not supporting unflatten into a different shape for the same dim name.
+        # with self.assertRaises(RuntimeError):
+        #     mesh_2d._unflatten(0, (2, 2), ("dp", "cp"))
+
+        # Test unflatten from a dummy world mesh, which is the case we need for Expert Parallelism(EP).
+        global_mesh = init_device_mesh(
+            self.device_type,
+            (8,),
+            mesh_dim_names=("world",),
+        )
+        self.assertFalse(hasattr(global_mesh.mesh_dim_names, "_dim_group_names"))
+        global_mesh._unflatten(0, (2, 2, 2), ("dp", "cp", "tp"))
+        global_mesh._unflatten(0, (2, 2, 2), ("dp", "ep", "ep_tp"))
+        self.assertEqual(global_mesh["cp"].mesh, global_mesh["ep"].mesh)
+        self.assertEqual(global_mesh["tp"].mesh, global_mesh["ep_tp"].mesh)
+
+    @with_comms
     def test_reconstruct_mesh_with_flatten_dim(self):
         mesh_3d = init_device_mesh(
             self.device_type, (2, 2, 2), mesh_dim_names=("replicate", "shard", "cp")
