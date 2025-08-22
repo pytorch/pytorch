c4670e40c9b741d50a79b714e3830149833be908
Xu Han <xu.han@outlook.com>
2025-08-22 06:23:41 +0000

    [inductor] remove Windows unsupported build options. (#161197)
    Changes:
    1. Math related build option is not supported by msvc, skip them on Windows.
    2. Move all math related build option to `_get_ffast_math_flags` function.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/161197
    Approved by: https://github.com/jansel

--- torch/_inductor/cpp_builder.py
+++ torch/_inductor/cpp_builder.py
@@ -597,22 +597,30 @@ def _get_os_related_cpp_definitions(cpp_compiler: str) -> list[str]:
 
 
 def _get_ffast_math_flags() -> list[str]:
-    # ffast-math is equivalent to these flags as in
-    # https://github.com/gcc-mirror/gcc/blob/4700ad1c78ccd7767f846802fca148b2ea9a1852/gcc/opts.cc#L3458-L3468
-    # however gcc<13 sets the FTZ/DAZ flags for runtime on x86 even if we have
-    # -ffast-math -fno-unsafe-math-optimizations because the flags for runtime
-    # are added by linking in crtfastmath.o. This is done by the spec file which
-    # only does globbing for -ffast-math.
-    flags = [
-        "fno-trapping-math",
-        "funsafe-math-optimizations",
-        "ffinite-math-only",
-        "fno-signed-zeros",
-        "fno-math-errno",
-    ]
+    if _IS_WINDOWS:
+        flags = []
+    else:
+        # ffast-math is equivalent to these flags as in
+        # https://github.com/gcc-mirror/gcc/blob/4700ad1c78ccd7767f846802fca148b2ea9a1852/gcc/opts.cc#L3458-L3468
+        # however gcc<13 sets the FTZ/DAZ flags for runtime on x86 even if we have
+        # -ffast-math -fno-unsafe-math-optimizations because the flags for runtime
+        # are added by linking in crtfastmath.o. This is done by the spec file which
+        # only does globbing for -ffast-math.
+        flags = [
+            "fno-trapping-math",
+            "funsafe-math-optimizations",
+            "ffinite-math-only",
+            "fno-signed-zeros",
+            "fno-math-errno",
+        ]
 
-    if is_gcc():
-        flags.append("fexcess-precision=fast")
+        flags.append("fno-finite-math-only")
+        if not config.cpp.enable_unsafe_math_opt_flag:
+            flags.append("fno-unsafe-math-optimizations")
+        flags.append(f"ffp-contract={config.cpp.enable_floating_point_contract_flag}")
+
+        if is_gcc():
+            flags.append("fexcess-precision=fast")
 
     return flags
 
@@ -660,10 +668,6 @@ def _get_optimization_cflags(
             cflags = [wrapper_opt_level if min_optimize else "O3", "DNDEBUG"]
 
     cflags += _get_ffast_math_flags()
-    cflags.append("fno-finite-math-only")
-    if not config.cpp.enable_unsafe_math_opt_flag:
-        cflags.append("fno-unsafe-math-optimizations")
-    cflags.append(f"ffp-contract={config.cpp.enable_floating_point_contract_flag}")
 
     if sys.platform != "darwin":
         # on macos, unknown argument: '-fno-tree-loop-vectorize'
