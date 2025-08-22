8fddf117e28a11a8b27509e88a9dbee18bcadc15
Wang, Chuanqi <chuanqi.wang@intel.com>
2025-08-22 10:25:38 +0200

    [CD] Delete duplicate MSVC2022 installation in triton xpu Windows build

--- .github/scripts/windows/build_triton.bat
+++ .github/scripts/windows/build_triton.bat
@@ -6,7 +6,8 @@ call .ci/pytorch/windows/internal/install_python.bat
 :: Fix cmake version for issue https://github.com/pytorch/pytorch/issues/150480
 %PYTHON_EXEC% -m pip install wheel pybind11 certifi cython cmake==3.31.6 setuptools==72.1.0 ninja==1.11.1.4
 
-dir "%VC_INSTALL_PATH%"
+set VC_YEAR=2022
+call .ci/pytorch/windows/internal/vc_install_helper.bat
+call "%VS15INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-call "%VC_INSTALL_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
 %PYTHON_EXEC% .github/scripts/build_triton_wheel.py --device=%BUILD_DEVICE% %RELEASE%
