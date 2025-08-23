be402c049831d2d0af9addfe11173de9a1f75f7d
Ruben Rodriguez Buchillon <coconutruben@fb.com>
2025-08-23 14:41:14 -0700

    Update base for Update on "[inductor][aten] treat like a template in GEMMs"
    \# why
    
    - central point to analyze and override all generated choices
    
    \# what
    
    - add a pseudo heuristic for aten that just yields a single, empty
      kwargs
    - add a pseudo heuristic with the bias_addmm logic for it
    - add an addmm specific heuristic that yields a single choice, but
      also expands it with alpha and beta kwargs
    
    - replace all the aten.bind calls with V.choices.get_mm_configs
      using the now matching API for aten
    
    \# testing
    
    ```
    python3 -bb -m pytest test/inductor/test_max_autotune.py -v
    ```
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov
    
    [ghstack-poisoned]

--- torch/_inductor/kernel/mm.py
+++ torch/_inductor/kernel/mm.py
@@ -581,7 +581,7 @@ def bias_addmm(inp, mat1, mat2, *, out=None, alpha=1, beta=1):
     kernel under the hood.  There are a few shapes where this is slower,
     but they are rare.
     """
-    if inp.stride(0) == 0 or inp.size(0) == 1:
+    if (inp.stride(0) == 0 and inp.size(0) != 0) or inp.size(0) == 1:
         return torch.addmm(inp[0], mat1, mat2, out=out, alpha=alpha, beta=beta)
     return torch.addmm(inp, mat1, mat2, out=out, alpha=alpha, beta=beta)
 
@@ -1011,7 +1011,6 @@ def tuned_addmm(inp, mat1, mat2, *, alpha=1, beta=1, layout=None):
             beta=beta,
             has_bias=True,
         )
-
     return autotune_select_algorithm("addmm", choices, kernel_inputs.nodes(), layout)
 
 
