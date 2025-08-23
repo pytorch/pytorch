171f3f0b65cfa18f68dd5a8b51c147fe48dc69d2
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-22 19:23:27 -0700

    docs
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- torch/onnx/__init__.py
+++ torch/onnx/__init__.py
@@ -166,7 +166,10 @@ def export(
         output_names: names to assign to the output nodes of the graph, in order.
         opset_version: The version of the
             `default (ai.onnx) opset <https://github.com/onnx/onnx/blob/master/docs/Operators.md>`_
-            to target. Must be >= 7.
+            to target. You should set ``opset_version`` according to the supported opset versions
+            of the runtime backend or compiler you want to run the exported model with.
+            Leave as default (``None``) to use the recommended version, or refer to
+            the ONNX operators documentation for more information.
         dynamic_axes:
 
             By default the exported model will have the shapes of all input and output tensors
