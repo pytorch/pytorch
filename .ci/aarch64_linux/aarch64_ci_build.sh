98d32ce5119d167d78161ee030ac86145cd591cc
Johnny <johnnync13@gmail.com>
2025-08-24 22:38:19 +0200

    Update aarch64_ci_build.sh

--- .ci/aarch64_linux/aarch64_ci_build.sh
+++ .ci/aarch64_linux/aarch64_ci_build.sh
@@ -4,6 +4,9 @@ set -eux -o pipefail
 GPU_ARCH_VERSION=${GPU_ARCH_VERSION:-}
 
 if [[ "$GPU_ARCH_VERSION" == *"12.9"* ]]; then
+    export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;12.0"
+fi
+if [[ "$GPU_ARCH_VERSION" == *"13.0"* ]]; then
     export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;11.0;12.0;12.1"
 fi
 
