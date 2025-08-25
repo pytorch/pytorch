0ddf18ff56c88d740d73d5170dc901c5c85f2515
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-25 15:53:34 -0700

    Update
    [ghstack-poisoned]

--- torch/onnx/symbolic_opset15.py
+++ torch/onnx/symbolic_opset15.py
@@ -1,3 +1,3 @@
 """Backward compatibility module for torch.onnx.symbolic_opset15."""
 
-from torch.onnx._internal.torchscript_exporter.symbolic_opset15 import *  # noqa: F403
+from torch.onnx._internal.torchscript_exporter.symbolic_opset15 import *  # noqa: F401,F403
