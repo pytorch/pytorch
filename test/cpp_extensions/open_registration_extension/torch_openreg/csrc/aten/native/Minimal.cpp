59f489ade8ec0cda731003e7a49739b0c1420e4f
Ruben Rodriguez Buchillon <coconutruben@fb.com>
2025-08-25 21:57:09 -0700

    Update base for Update on "[inductor] V.choices.get_mm_configs override point"
    \# why
    
    - enable us to override the default configs, or fall back to them
      through subclassing InductorChoices
    
    \# what
    
    - override (private) function
    - default implementationt takes the kernel template choice (ktc)
      generator for every template and just executes the generator
    - future overrides can decide to replace those generators, or filter
      out choices
    
    - the 2nd expensive step (maybe_append_choices, choice_or_none) is
      handled outside this function, in the main V.choices.get_mm_configs
      this means that any overriding benefits from not generating expensive
      templates that aren't going to be used
    
    \# testing
    
    ```
    python3 -bb -m pytest test/inductor/test_max_autotune.py -v
    ```
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov
    
    [ghstack-poisoned]

--- test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Minimal.cpp
+++ test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Minimal.cpp
@@ -1,11 +1,8 @@
 #include "Minimal.h"
 
-#include <unordered_set>
+namespace at::native {
 
-namespace at::native::openreg {
-
-// LITERALINCLUDE START: EMPTY.MEMORY_FORMAT IMPL
-at::Tensor empty_memory_format(
+at::Tensor empty_memory_format_openreg(
     c10::IntArrayRef size,
     std::optional<c10::ScalarType> dtype_opt,
     std::optional<c10::Layout> layout_opt,
@@ -27,9 +24,8 @@ at::Tensor empty_memory_format(
   return at::detail::empty_generic(
       size, allocator, pu1_dks, dtype, memory_format_opt);
 }
-// LITERALINCLUDE END: EMPTY.MEMORY_FORMAT IMPL
 
-at::Tensor empty_strided(
+at::Tensor empty_strided_openreg(
     c10::IntArrayRef size,
     c10::IntArrayRef stride,
     std::optional<c10::ScalarType> dtype_opt,
@@ -52,7 +48,7 @@ at::Tensor empty_strided(
       size, stride, allocator, pu1_dks, dtype);
 }
 
-at::Tensor as_strided(
+at::Tensor as_strided_openreg(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     c10::SymIntArrayRef stride,
@@ -62,7 +58,7 @@ at::Tensor as_strided(
   return at::cpu::as_strided_symint(self, size, stride, storage_offset);
 }
 
-const at::Tensor& resize_(
+const at::Tensor& resize_openreg_(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     ::std::optional<at::MemoryFormat> memory_format) {
@@ -70,7 +66,7 @@ const at::Tensor& resize_(
       self, C10_AS_INTARRAYREF_SLOW(size), memory_format);
 }
 
-at::Tensor _reshape_alias(
+at::Tensor _reshape_alias_openreg(
     const at::Tensor& self,
     c10::SymIntArrayRef size,
     c10::SymIntArrayRef stride) {
@@ -78,7 +74,7 @@ at::Tensor _reshape_alias(
       self, C10_AS_INTARRAYREF_SLOW(size), C10_AS_INTARRAYREF_SLOW(stride));
 }
 
-at::Tensor _copy_from(
+at::Tensor _copy_from_openreg(
     const at::Tensor& self,
     const at::Tensor& dst,
     bool non_blocking) {
@@ -128,58 +124,50 @@ at::Tensor _copy_from(
   return dst;
 }
 
-at::Tensor _copy_from_and_resize(
+at::Tensor _copy_from_and_resize_openreg(
     const at::Tensor& self,
     const at::Tensor& dst) {
   at::native::resize_(dst, self.sizes(), std::nullopt);
+
+  MemoryGuard guard(self, dst);
+
   return at::native::copy_(const_cast<at::Tensor&>(dst), self, false);
 }
 
-at::Scalar _local_scalar_dense(const at::Tensor& self) {
+at::Scalar _local_scalar_dense_openreg(const at::Tensor& self) {
   MemoryGuard guard(self);
   return at::native::_local_scalar_dense_cpu(self);
 }
 
-at::Tensor& set_source_Tensor_(at::Tensor& self, const at::Tensor& source) {
+at::Tensor& set_source_Tensor_openreg_(
+    at::Tensor& self,
+    const at::Tensor& source) {
   return at::native::set_tensor_(self, source);
 }
 
-at::Tensor& set_source_Storage_(at::Tensor& self, at::Storage source) {
+at::Tensor& set_source_Storage_openreg_(at::Tensor& self, at::Storage source) {
   return at::native::set_(self, source);
 }
 
-at::Tensor& set_source_Storage_storage_offset_(
+at::Tensor& set_source_Storage_storage_offset_openreg_(
     at::Tensor& result,
     at::Storage storage,
     int64_t storage_offset,
     c10::IntArrayRef size,
     c10::IntArrayRef stride) {
+  // call native::
   return at::cpu::set_(result, storage, storage_offset, size, stride);
 }
 
-at::Tensor view(const at::Tensor& self, c10::SymIntArrayRef size) {
+at::Tensor view_openreg(const at::Tensor& self, c10::SymIntArrayRef size) {
   MemoryGuard guard(self);
   return at::native::view(self, C10_AS_INTARRAYREF_SLOW(size));
 }
 
-// LITERALINCLUDE START: FALLBACK IMPL
-void cpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
-  static const std::unordered_set<c10::OperatorName> cpu_fallback_blacklist = {
-      c10::OperatorName("aten::abs", ""),
-      c10::OperatorName("aten::abs", "out"),
-  };
-
-  const auto& op_name = op.schema().operator_name();
-  if (cpu_fallback_blacklist.count(op_name)) {
-    TORCH_CHECK(
-        false,
-        "Operator '",
-        op_name,
-        "' is not implemented for device openreg.");
-  } else {
-    at::native::cpu_fallback(op, stack);
-  }
+void cpu_fallback_openreg(
+    const c10::OperatorHandle& op,
+    torch::jit::Stack* stack) {
+  at::native::cpu_fallback(op, stack);
 }
-// LITERALINCLUDE END: FALLBACK IMPL
 
-} // namespace at::native::openreg
+} // namespace at::native
