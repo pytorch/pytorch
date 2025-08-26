c233e6a6b35e525f6a0d89b02ee0de6570b3c747
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-26 12:36:05 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/onnx/symbolic_opset9.py
+++ torch/onnx/symbolic_opset9.py
@@ -1,6 +1,9 @@
 """Backward compatibility module for torch.onnx.symbolic_opset9."""
 
-__all__ = []
+from __future__ import annotations
+
+
+__all__: list[str] = []
 
 from torch.onnx._internal.torchscript_exporter.symbolic_opset9 import *  # noqa: F401,F403
 from torch.onnx._internal.torchscript_exporter.symbolic_opset9 import (  # noqa: F401
