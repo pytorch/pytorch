aacb9862fdf11be7d0305daa4c4f199c4cf654c8
nullplay <jaeyeon@mit.edu>
2025-08-25 21:12:30 -0400

    Blockshapetype get str

--- torch/_inductor/shape_propagation.py
+++ torch/_inductor/shape_propagation.py
@@ -123,6 +123,8 @@ class ShapePropagationOpsHandler:
 
     @staticmethod
     def dot(a: sympy.Expr, b: sympy.Expr) -> BlockShapeType:
+        from torch._inductor.codegen.triton import TritonKernel
+        assert isinstance(V.kernel, TritonKernel), "dot supports Triton only"
         return tuple(['YBLOCK', 'XBLOCK'])
 
     @staticmethod
