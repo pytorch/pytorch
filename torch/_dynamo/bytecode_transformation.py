6686974ddd7a616652d9f3dea9195ba8d6d02769
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-08-26 20:40:16 +0000

    Revert "[dynamo, nested graph breaks] add nested graph break tests (#144516)"
    This reverts commit 9a756c2d710a0680bac93ab0b42db519ec2dc6cf.
    
    Reverted https://github.com/pytorch/pytorch/pull/144516 on behalf of https://github.com/atalman due to failing internal tests ([comment](https://github.com/pytorch/pytorch/pull/144516#issuecomment-3225659358))

--- torch/_dynamo/bytecode_transformation.py
+++ torch/_dynamo/bytecode_transformation.py
@@ -25,7 +25,6 @@ from collections.abc import Iterable, Iterator, Mapping, Sequence
 from typing import Any, Callable, cast, Optional, TYPE_CHECKING, Union
 
 from ..utils._backport_slots import dataclass_slots
-from . import config
 from .bytecode_analysis import (
     get_indexof,
     propagate_line_nums,
@@ -1201,50 +1200,6 @@ def remove_fused_load_store(instructions: list[Instruction]) -> None:
     instructions[:] = new_insts
 
 
-# adds GRAPH_BREAK_IF_LEAF (not a real instruction) before RETURN_* instructions
-# for testing purposes
-def add_graph_break_if_leaf_instructions(instructions: list[Instruction]) -> None:
-    new_insts = []
-    for inst in instructions:
-        if "RETURN" in inst.opname:
-            replace_insts = [
-                create_instruction("NOP", argval="GRAPH_BREAK_IF_LEAF"),
-                create_instruction(inst.opname, argval=inst.argval),
-            ]
-            # breakpoint()
-            new_insts.extend(overwrite_instruction(inst, replace_insts))
-        else:
-            new_insts.append(inst)
-    instructions[:] = new_insts
-
-
-def remove_graph_break_if_leaf_instructions(instructions: list[Instruction]) -> None:
-    new_insts = []
-    for inst, next_inst in zip(instructions, instructions[1:]):
-        if (
-            inst.opname == "NOP"
-            and inst.argval == "GRAPH_BREAK_IF_LEAF"
-            and next_inst.opname.startswith("RETURN")
-        ):
-            # remove this instruction and update all other instructions' jump targets
-            for i in range(len(instructions)):
-                if instructions[i].target is inst:
-                    instructions[i].target = next_inst
-                if instructions[i].exn_tab_entry:
-                    # linter is mistakenly complaining that None has no attribute "..."
-                    # but this codepath only runs if instructions[i] is not None
-                    if instructions[i].exn_tab_entry.start is inst:  # type: ignore[union-attr]
-                        instructions[i].exn_tab_entry.start = next_inst  # type: ignore[union-attr]
-                    if instructions[i].exn_tab_entry.end is inst:  # type: ignore[union-attr]
-                        instructions[i].exn_tab_entry.end = next_inst  # type: ignore[union-attr]
-                    if instructions[i].exn_tab_entry.target is inst:  # type: ignore[union-attr]
-                        instructions[i].exn_tab_entry.target = next_inst  # type: ignore[union-attr]
-        else:
-            new_insts.append(inst)
-    new_insts.append(instructions[-1])
-    instructions[:] = new_insts
-
-
 def explicit_super(code: types.CodeType, instructions: list[Instruction]) -> None:
     """convert super() with no args into explicit arg form"""
     cell_and_free = (code.co_cellvars or ()) + (code.co_freevars or ())
@@ -1566,7 +1521,6 @@ def transform_code_object(
 def clean_and_assemble_instructions(
     instructions: list[Instruction], keys: list[str], code_options: dict[str, Any]
 ) -> tuple[list[Instruction], types.CodeType]:
-    remove_graph_break_if_leaf_instructions(instructions)
     # also implicitly checks for no duplicate instructions
     check_inst_exn_tab_entries_valid(instructions)
 
@@ -1682,8 +1636,6 @@ def _cached_cleaned_instructions(
                 remove_binary_store_slice(instructions)
             if sys.version_info >= (3, 13):
                 remove_fused_load_store(instructions)
-        if config.debug_force_graph_break_on_leaf_return:
-            add_graph_break_if_leaf_instructions(instructions)
     if sys.version_info >= (3, 11):
         update_offsets(instructions)
         devirtualize_jumps(instructions)
