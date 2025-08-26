42c99cc2bba5ea04fdce7acf90dce456c75fa2c2
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-08-26 03:25:42 -0700

    [DEBUG] Accuracy: revert bucketing dtype, cycles PRs
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/fsdp.py
+++ torch/_inductor/fx_passes/fsdp.py
@@ -38,19 +38,7 @@ def is_graph_output(node: torch.fx.Node) -> bool:
 
 
 def is_fsdp_reduce_scatter_wait(wait: torch.fx.Node) -> bool:
-    if is_graph_output(wait):
-        return True
-
-    if len(wait.users) == 1:
-        user = next(iter(wait.users))
-        assert user is not None
-        return (
-            is_graph_output(user)
-            and user.op == "call_function"
-            and user.target == torch.ops.prims.convert_element_type.default
-        )
-
-    return False
+    return is_graph_output(wait)
 
 
 def bucket_fsdp_all_gather(
