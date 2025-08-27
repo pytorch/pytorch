058b302f6298b531328d2291868c773b8010a754
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2025-08-27 14:15:25 -0700

    Error when TORCH_STABLE_ONLY is defined in TensorBase.h
    [ghstack-poisoned]

---
+++ test/cpp_extensions/torch_stable_test_extension/torch_stable_test/test_torch_stable.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from torch.testing._internal.common_utils import (
+    install_cpp_extension,
+    IS_WINDOWS,
+    run_tests,
+    TestCase,
+)
+
+
+if not IS_WINDOWS:
+
+    class TestLibtorchAgnostic(TestCase):
+        def test_setup_fails(self):
+            with self.assertRaisesRegex(RuntimeError, "build failed for cpp extension"):
+                install_cpp_extension(extension_root=Path(__file__).parent.parent)
+
+if __name__ == "__main__":
+    run_tests()
