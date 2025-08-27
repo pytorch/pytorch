5c9c7d482927a529a2b4b2ff333c0bfcba742f42
Huy Do <huydhn@gmail.com>
2025-08-27 14:59:40 -0700

    Botched rebase
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -203,7 +203,7 @@ case "$tag" in
     NINJA_VERSION=1.9.0
     TRITON=yes
     ;;
-  pytorch-linux-jammy-py3.9-gcc11-inductor-benchmarks)
+  pytorch-linux-jammy-py3-gcc11-inductor-benchmarks)
     # TODO (huydhn): Upgrade this to Python >= 3.10
     ANACONDA_PYTHON_VERSION=3.9
     GCC_VERSION=11
