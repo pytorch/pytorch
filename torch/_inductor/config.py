7dac18429c5f52b0f0f6f06ecff069e3bb1ffee0
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-08-27 03:06:17 -0700

    Update base for Update on "[DEBUG] Accuracy: revert bucketing dtype, cycles PRs"
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k pragupta voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov coconutruben
    
    [ghstack-poisoned]

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -402,6 +402,9 @@ sink_waits_iterative_debug_limit_to_sink: Optional[int] = (
 
 # Comparing estimations vs real benchmarks showed big divergence.
 # Exposing extensive config for easier experimentation.
+reorder_iterative_use_runtime_estimations: bool = False
+sink_iterative_use_runtime_estimations: bool = False
+
 reorder_sink_runtime_estimations_comm_mult: float = 1.0
 reorder_sink_runtime_estimations_non_comm_mult: float = 1.0
 # Ratio of comm_time to cover deviations of comm_time from estimations
@@ -411,8 +414,8 @@ sink_iterative_extra_comm_comp_overlap: float = 0.2
 # Experimental unsafe configuration that allows changing relative collectives order,
 # No guarantees for now that all the rank will do the same order of collectives,
 # which can result in collective hangs.
-reorder_iterative_unsafe_collectives_reorder: bool = False
-sink_waits_iterative_unsafe_collectives_reorder: bool = False
+reorder_iterative_unsafe_collectives_reorder: bool = True
+sink_waits_iterative_unsafe_collectives_reorder: bool = True
 
 bucket_all_gathers_fx: Literal["none", "all", "only_fsdp"] = "none"
 # By default torch._inductor.fx_passes.bucketing.bucket_size_determinator is used
@@ -428,7 +431,7 @@ bucket_reduce_scatters_fx_bucket_size_determinator: Optional[Callable[[int], int
 # for built-in estimation function, pass in "default"; for user-defined estimation function, pass in the function handle
 estimate_op_runtime = "default"
 
-runtime_estimations_use_nccl_lib_estimations: bool = True
+runtime_estimations_use_nccl_lib_estimations: bool = False
 runtime_estimations_mms_benchmark: bool = False
 
 # unit: GB/s, uni-directional P2P bandwidth per card
