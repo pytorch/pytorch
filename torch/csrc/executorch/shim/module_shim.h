fc7b4e41beffb4fa87ea0af66c73dc93ed494bf7
Jacob Szwejbka <jakeszwe@fb.com>
2025-08-27 21:25:09 -0700

    Add unified runtime module shim and add nativert implementation

---
+++ torch/csrc/executorch/shim/module_shim.h
@@ -0,0 +1,39 @@
+#include <cstdint>
+#include "inductor/aoti_torch/c/macros.h"
+#include "inductor/aoti_torch/c/shim.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * The type of a TypedStableIValue.
+ */
+enum class StableIValueTag : uint32_t {
+  None = 0,
+  Tensor = 1,
+  Double = 2,
+  Int = 3,
+  Bool = 4,
+};
+
+/**
+ * A wrapper containing StableIValue which is opaque and the type that it contains.
+ */
+struct TypedStableIValue{
+    StableIValue val;
+    StableIValueTag tag;
+};
+
+struct ModuleOpaque;
+using ModuleHandle = ModuleOpaque*;
+
+AOTITorchError experimental_torch_load_module_from_file(const char* package_path, uint64_t package_path_len, const char* model_name, uint64_t model_name_len, ModuleHandle* ret_value);
+AOTITorchError experimental_torch_delete_module_object(ModuleHandle handle);
+AOTITorchError experimental_torch_module_num_outputs(ModuleHandle handle, uint64_t* ret_value);
+AOTITorchError experimental_torch_module_forward_flattened(ModuleHandle handle, const TypedStableIValue* args, uint64_t num_args, TypedStableIValue* ret_values, uint64_t num_outputs);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
