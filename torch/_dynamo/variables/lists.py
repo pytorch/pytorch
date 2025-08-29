a3c11a4774dfde53e8270bf0c640c0aa740567aa
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-08-29 15:32:06 -0300

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/lists.py
+++ torch/_dynamo/variables/lists.py
@@ -368,13 +368,17 @@ class RangeVariable(BaseListVariable):
 
         return [start, stop, step]
 
-    def apply_index(self, index):
+    def apply_index(self, tx, index):
         length = self.range_length()
         if index < 0:
             index = length + index
 
         if index < 0 or index >= length:
-            raise IndexError(f"index {index} is out of range")
+            raise_observed_exception(
+                IndexError,
+                tx,
+                args=[ConstantVariable("range object index out of range")],
+            )
 
         return variables.ConstantVariable.create(self.start() + (index * self.step()))
 
@@ -409,7 +413,7 @@ class RangeVariable(BaseListVariable):
         if isinstance(index, slice):
             return self.apply_slice(index)
         else:
-            return self.apply_index(index)
+            return self.apply_index(tx, index)
 
     def as_proxy(self):
         return self.python_type()(*self._as_proxy())
@@ -477,6 +481,8 @@ class RangeVariable(BaseListVariable):
             return ConstantVariable.create(self.range_length())
         elif name in ("count", "__contains__"):
             return ConstantVariable(self.range_count(*args))
+        elif name == "__getitem__":
+            return self.getitem_const(tx, *args)
         elif name in cmp_name_to_op_mapping:
             other = args[0]
             pt = other.python_type()
