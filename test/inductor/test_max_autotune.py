d574e9cf1b67d8afc765924a522d0fe7ba0ca2bf
Xinya Zhang <Xinya.Zhang@amd.com>
2025-08-29 05:17:59 +0000

    fix lint

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -50,9 +50,9 @@ from torch.testing._internal.common_utils import (
 )
 from torch.testing._internal.logging_utils import multiple_logs_to_string
 from torch.utils._triton import (
+    get_triton_version,
     has_triton_stable_tma_api,
     has_triton_tma_device,
-    get_triton_version,
 )
 
 
@@ -705,7 +705,10 @@ class TestMaxAutotune(TestCase):
     @parametrize("search_space", ("DEFAULT", "EXHAUSTIVE"))
     def test_baddmm(self, search_space):
         if TEST_WITH_ROCM and get_triton_version() == (3, 4):
-            self.skipTest("To investigate. Error info: RuntimeError: PassManager::run failed")
+            self.skipTest(
+                "To investigate. Error info: RuntimeError: PassManager::run failed"
+            )
+
         class M(torch.nn.Module):
             def __init__(self):
                 super().__init__()
--- torch/utils/_triton.py
+++ torch/utils/_triton.py
@@ -14,11 +14,11 @@ def has_triton_package() -> bool:
 
 
 @functools.cache
-def get_triton_version(fallback=(0,0)) -> tuple[int]:
+def get_triton_version(fallback=(0, 0)) -> tuple[int]:
     try:
         import triton  # noqa: F401
 
-        return tuple(int(v) for v in triton.__version__.split('.')[:2])
+        return tuple(int(v) for v in triton.__version__.split(".")[:2])
     except ImportError:
         return fallback
 
