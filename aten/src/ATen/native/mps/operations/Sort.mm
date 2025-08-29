4f47a13dec0d0c571d5d966608f0f1b4eef01ded
Nikita Shulga <nikita.shulga@gmail.com>
2025-08-29 16:25:46 -0700

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/mps/operations/Sort.mm
+++ aten/src/ATen/native/mps/operations/Sort.mm
@@ -2,29 +2,24 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/MemoryOverlap.h>
 #include <ATen/WrapDimUtils.h>
+#include <ATen/native/SortingUtils.h>
 #include <ATen/native/TensorShape.h>
 #include <ATen/native/TypeProperties.h>
 #include <ATen/native/mps/MPSGraphVenturaOps.h>
 #include <ATen/native/mps/OperationUtils.h>
-#include <ATen/native/SortingUtils.h>
 
 #ifndef AT_PER_OPERATOR_HEADERS
 #include <ATen/Functions.h>
 #include <ATen/NativeFunctions.h>
 #else
+#include <ATen/ops/kthvalue_native.h>
 #include <ATen/ops/sort.h>
 #include <ATen/ops/sort_native.h>
-#include <ATen/ops/kthvalue_native.h>
 #endif
 namespace at::native {
 namespace {
 
-void kthvalue_out_mps_impl(
-    const Tensor& self,
-    int64_t k,
-    int64_t dim,
-    Tensor& values,
-    Tensor& indices) {
+void kthvalue_out_mps_impl(const Tensor& self, int64_t k, int64_t dim, Tensor& values, Tensor& indices) {
   using namespace mps;
   if (self.dim() == 0 && self.numel() == 1) {
     values.copy_(self);
@@ -70,14 +65,18 @@ void kthvalue_out_mps_impl(
                                                          name:nil];
       sortedTensor = [mpsGraph sliceTensor:sortedTensor
                                  dimension:(NSUInteger)dim
-                                     start:((NSUInteger)k-1)
-                                     length:1
+                                     start:((NSUInteger)k - 1)
+                                    length:1
                                       name:nil];
       MPSGraphTensor* argSortedTensor = [mpsGraph argSortWithTensor:castInputTensor
                                                                axis:(NSInteger)dim
                                                          descending:false
                                                                name:@"kthvalue_out"];
-      argSortedTensor = [mpsGraph sliceTensor:argSortedTensor dimension:dim start:((NSUInteger)k-1) length:1 name:nil];
+      argSortedTensor = [mpsGraph sliceTensor:argSortedTensor
+                                    dimension:dim
+                                        start:((NSUInteger)k - 1)
+                                       length:1
+                                         name:nil];
       newCachedGraph->valuesTensor = sortedTensor;
       newCachedGraph->indicesTensor = argSortedTensor;
     });
@@ -159,13 +158,12 @@ TORCH_IMPL_FUNC(sort_stable_out_mps)
   }
 }
 
-std::tuple<Tensor&, Tensor&> kthvalue_out_mps(
-    const Tensor& self,
-    int64_t k,
-    int64_t dim_,
-    bool keepdim,
-    Tensor& values,
-    Tensor& indices) {
+std::tuple<Tensor&, Tensor&> kthvalue_out_mps(const Tensor& self,
+                                              int64_t k,
+                                              int64_t dim_,
+                                              bool keepdim,
+                                              Tensor& values,
+                                              Tensor& indices) {
   // See note [Writing Nondeterministic Operations]
   // If there are duplicate elements of the kth value, the procedure for choosing which
   // of the duplicates to use for the indices output is nondeterministic.
@@ -173,11 +171,9 @@ std::tuple<Tensor&, Tensor&> kthvalue_out_mps(
 
   int64_t dim = maybe_wrap_dim(dim_, self.dim(), /*wrap_scalar=*/true);
   int64_t slicesize = self.dim() == 0 ? 1 : self.size(dim);
-  TORCH_CHECK(k >= 1 && k <= slicesize,
-              "kthvalue(): selected number k out of range for dimension ", dim);
+  TORCH_CHECK(k >= 1 && k <= slicesize, "kthvalue(): selected number k out of range for dimension ", dim);
   at::assert_no_overlap(self, values);
-  _reduction_with_indices_allocate_or_resize_output(
-      values, indices, self, dim, keepdim);
+  _reduction_with_indices_allocate_or_resize_output(values, indices, self, dim, keepdim);
 
   kthvalue_out_mps_impl(self, k, dim, values, indices);
 
