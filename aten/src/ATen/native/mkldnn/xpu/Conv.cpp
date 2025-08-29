986b8c12923cc343f4549e10cdfbd4c223f49cee
Neo Zhang Jianyu <jianyu.zhang@intel.com>
2025-08-29 12:23:13 +0800

    check abnormal stride before execute to()

--- aten/src/ATen/native/mkldnn/xpu/Conv.cpp
+++ aten/src/ATen/native/mkldnn/xpu/Conv.cpp
@@ -536,6 +536,19 @@ Tensor convolution_overrideable(
       Attr());
 }
 
+bool is_stride_decrease_order(Tensor &tensor) {
+  std::vector<int64_t> strides = tensor.strides().vec();
+  if (strides.size() < 2)
+    return true;
+
+  for (size_t i = 0; i < strides.size() - 1; ++i) {
+    if (strides[i] < strides[i + 1])
+      return false;
+  }
+
+  return true;
+}
+
 std::tuple<Tensor, Tensor, Tensor> convolution_backward_overrideable(
     const Tensor& grad_output,
     const Tensor& input,
@@ -601,7 +614,11 @@ std::tuple<Tensor, Tensor, Tensor> convolution_backward_overrideable(
   auto mfmt = is_channels_last_suggested
       ? get_cl_tag_by_ndim(input_.ndimension())
       : at::MemoryFormat::Contiguous;
-  grad_output_ = grad_output_.to(mfmt);
+
+
+  if (!is_stride_decrease_order(grad_output_))
+    grad_output_ = grad_output_.to(mfmt);
+
   if (!grad_output_.is_contiguous())
     grad_output_ = grad_output_.contiguous(mfmt);
 
