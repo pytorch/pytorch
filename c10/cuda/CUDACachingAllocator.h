28f52f891ea3ff5dae6cb33bd570f267dbc60a37
Yu, Guangye <guangye.yu@intel.com>
2025-09-01 09:05:34 +0000

    [Reland] Refactor CUDAAllocatorConfig to reuse AcceleratorAllocatorConfig
    ghstack-source-id: ed9a44f670a0c9281a1d702fcf01dfc577ed1f74
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/161786

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/core/CachingDeviceAllocator.h>
+#include <c10/cuda/CUDAAllocatorConfig.h>
 #include <c10/cuda/CUDAGraphsC10Utils.h>
 #include <c10/cuda/CUDAMacros.h>
 #include <c10/cuda/CUDAStream.h>
@@ -49,9 +50,8 @@ namespace c10::cuda::CUDACachingAllocator {
 
 // Preserved only for BC reasons
 // NOLINTNEXTLINE(misc-unused-using-decls)
+using c10::CachingAllocator::kLargeBuffer;
 using c10::CachingDeviceAllocator::DeviceStats;
-
-extern const size_t kLargeBuffer;
 
 typedef std::shared_ptr<GatheredContext> (*CreateContextFn)();
 
