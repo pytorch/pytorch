13b65196db422bdb394cb482e208c61ed448898c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-09-01 21:34:13 +0000

    Revert "Defer loading hipify until it is needed (#160824)"
    This reverts commit 403a3a393cda7e60f503f3b04b8805a845dcf45d.
    
    Reverted https://github.com/pytorch/pytorch/pull/160824 on behalf of https://github.com/atalman due to Broke slow tests test_utils.py::TestHipifyTrie::test_special_char_export_trie_to_regex [GH job link](https://github.com/pytorch/pytorch/actions/runs/17387051351/job/49355619371) [HUD commit link](https://hud.pytorch.org/pytorch/pytorch/commit/403a3a393cda7e60f503f3b04b8805a845dcf45d) ([comment](https://github.com/pytorch/pytorch/pull/160824#issuecomment-3243281628))

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -172,20 +172,6 @@ class TestCompiledAutograd(TestCase):
         except subprocess.CalledProcessError as e:
             self.fail(f"Subprocess exited with return code: {e.returncode}")
 
-    def test_hipify_not_loaded_with_import_torch(self):
-        script = """
-import torch
-assert globals().get("hipify", False) is False
-"""
-        self.run_as_subprocess(script)
-
-    def test_hipify_not_loaded_with_import_cpp_extension(self):
-        script = """
-import torch.utils.cpp_extension
-assert globals().get("hipify", False) is False
-"""
-        self.run_as_subprocess(script)
-
     def test_dynamo_flaky_segfault(self):
         script = """
 import torch
