388f962f7a15f20aab70060ebbeaeebb57e8ca25
Sun, Jiayi <jiayi.sun@intel.com>
2025-09-02 16:42:09 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/fx_passes/quantization.py
+++ torch/_inductor/fx_passes/quantization.py
@@ -2338,57 +2338,65 @@ def _register_qlinear_weight_prepack():
     #   |            OPT(add)               |
 
     linear_weight_prepack_cases = itertools.product(
-        [torch.float32, torch.bfloat16], [True, False], [True, False]
+        [torch.float32, torch.bfloat16], [True, False], [True, False], [True, False]
     )
 
     # Step 1: register patterns from mm and addmm
-    for dtype, input_dim_exceeds_two, is_tensor_overload in linear_weight_prepack_cases:
-        with_dtype_convert_cases = [False] if dtype == torch.float32 else [True, False]
-        for with_dtype_convert in with_dtype_convert_cases:
-            weight_prepack_patterns = _generate_qlinear_weight_prepack_patterns(
-                dtype,
-                input_dim_exceeds_two,
-                is_tensor_overload=is_tensor_overload,
+    for (
+        dtype,
+        input_dim_exceeds_two,
+        is_tensor_overload,
+        with_dtype_convert,
+    ) in linear_weight_prepack_cases:
+        if dtype == torch.float32 and with_dtype_convert:
+            continue
+        weight_prepack_patterns = _generate_qlinear_weight_prepack_patterns(
+            dtype,
+            input_dim_exceeds_two,
+            is_tensor_overload=is_tensor_overload,
+            with_dtype_convert=with_dtype_convert,
+        )
+        for weight_prepack_pattern in weight_prepack_patterns:
+            # Register to pass_number 1, so we can do dequant promotion in pass_number 0.
+            _register_qlinear_weight_prepack_pass(
+                weight_prepack_pattern,
+                pass_number=1,
+                dtype=dtype,
+                input_dim_exceeds_two=input_dim_exceeds_two,
                 with_dtype_convert=with_dtype_convert,
             )
-            for weight_prepack_pattern in weight_prepack_patterns:
-                # Register to pass_number 1, so we can do dequant promotion in pass_number 0.
-                _register_qlinear_weight_prepack_pass(
-                    weight_prepack_pattern,
-                    pass_number=1,
-                    dtype=dtype,
-                    input_dim_exceeds_two=input_dim_exceeds_two,
-                    with_dtype_convert=with_dtype_convert,
-                )
 
     # Step 2: register patterns from bmm
     # Linear might be decomposed into bmm when input dim exceeds 2 and not contiguous
     # refer to:
     # https://github.com/pytorch/pytorch/blob/80c07df659362a95da7cd4f3ec367abfdace38c4/torch/_decomp/decompositions.py#L3965-L3968
     # in this case, we can convert it back to qlinear
-    for dtype, with_bias, is_tensor_overload in itertools.product(
-        [torch.float32, torch.bfloat16], [True, False], [True, False]
-    ):
-        with_dtype_convert_cases = [False] if dtype == torch.float32 else [True, False]
-        for with_dtype_convert in with_dtype_convert_cases:
-            bmm_pattern = _generate_qlinear_weight_prepack_patterns(
-                dtype=dtype,
-                input_dim_exceeds_two=True,
-                input_contiguous=False,
-                with_bias=with_bias,
-                is_tensor_overload=is_tensor_overload,
-                with_dtype_convert=with_dtype_convert,
-            )
-            _register_qlinear_weight_prepack_pass(
-                bmm_pattern,
-                pass_number=1
-                if with_bias
-                else 2,  # if with_bias, there is an output add, so we should try to match it firstly
-                dtype=dtype,
-                input_dim_exceeds_two=True,
-                input_contiguous=False,
-                with_dtype_convert=with_dtype_convert,
-            )
+    for (
+        dtype,
+        with_bias,
+        is_tensor_overload,
+        with_dtype_convert,
+    ) in linear_weight_prepack_cases:
+        if dtype == torch.float32 and with_dtype_convert:
+            continue
+        bmm_pattern = _generate_qlinear_weight_prepack_patterns(
+            dtype=dtype,
+            input_dim_exceeds_two=True,
+            input_contiguous=False,
+            with_bias=with_bias,
+            is_tensor_overload=is_tensor_overload,
+            with_dtype_convert=with_dtype_convert,
+        )
+        _register_qlinear_weight_prepack_pass(
+            bmm_pattern,
+            pass_number=1
+            if with_bias
+            else 2,  # if with_bias, there is an output add, so we should try to match it firstly
+            dtype=dtype,
+            input_dim_exceeds_two=True,
+            input_contiguous=False,
+            with_dtype_convert=with_dtype_convert,
+        )
 
 
 def _register_linear_dynamic_fp16_weight_prepack_pass(
