bc295082bfc05655b2f8ecc70ef54fe1e6effa72
Chien-Chin Huang <chienchin@fb.com>
2025-09-02 22:52:53 -0700

    Update
    [ghstack-poisoned]

--- test/distributed/test_symmetric_memory.py
+++ test/distributed/test_symmetric_memory.py
@@ -555,7 +555,10 @@ class AsyncTPTest(MultiProcContinuousTest):
                 )
 
         assert outputs[0].stride() == outputs[1].stride()
-        assert torch.allclose(outputs[0], outputs[1]), (outputs[0], outputs[1])
+        assert torch.allclose(outputs[0], outputs[1], rtol=1e-3, atol=1e-3), (
+            outputs[0],
+            outputs[1],
+        )
 
     @runOnRocmArch(MI300_ARCH)
     @parametrize("dim", [0, 1, 2])
