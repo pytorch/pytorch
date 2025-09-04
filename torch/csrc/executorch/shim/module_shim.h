339fe1a29d3e51c443725578edfc71622e1b15f5
Jacob Szwejbka <jakeszwe@fb.com>
2025-09-03 22:03:20 -0700

    lint

--- torch/csrc/executorch/shim/module_shim.h
+++ torch/csrc/executorch/shim/module_shim.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <cstdint>
 #include <torch/csrc/inductor/aoti_torch/c/macros.h>
 #include <torch/csrc/inductor/aoti_torch/c/shim.h>
+#include <cstdint>
 
 // This header defines a prototype stable C API for certain Module
 // functionality. It is inspired by:
@@ -39,37 +39,33 @@ enum class StableIValueTag : uint32_t {
  * now, we have the user manually specify the type. This struct is likely
  * to change as we iterate on the API.
  */
-struct TypedStableIValue{
-    StableIValue val;
-    StableIValueTag tag;
+struct TypedStableIValue {
+  StableIValue val;
+  StableIValueTag tag;
 };
 
 struct ModuleOpaque;
 using ModuleHandle = ModuleOpaque*;
 
-AOTI_TORCH_EXPORT AOTITorchError
-experimental_torch_load_module_from_file(
-  const char* package_path,
-  uint64_t package_path_len,
-  const char* model_name,
-  uint64_t model_name_len,
-  ModuleHandle* ret_value);
+AOTI_TORCH_EXPORT AOTITorchError experimental_torch_load_module_from_file(
+    const char* package_path,
+    uint64_t package_path_len,
+    const char* model_name,
+    uint64_t model_name_len,
+    ModuleHandle* ret_value);
 
 AOTI_TORCH_EXPORT AOTITorchError
 experimental_torch_delete_module_object(ModuleHandle handle);
 
 AOTI_TORCH_EXPORT AOTITorchError
-experimental_torch_module_num_outputs(
-  ModuleHandle handle,
-  uint64_t* ret_value);
+experimental_torch_module_num_outputs(ModuleHandle handle, uint64_t* ret_value);
 
-AOTI_TORCH_EXPORT AOTITorchError
-experimental_torch_module_forward_flattened(
-  ModuleHandle handle,
-  const TypedStableIValue* args,
-  uint64_t num_args,
-  TypedStableIValue* ret_values,
-  uint64_t num_outputs);
+AOTI_TORCH_EXPORT AOTITorchError experimental_torch_module_forward_flattened(
+    ModuleHandle handle,
+    const TypedStableIValue* args,
+    uint64_t num_args,
+    TypedStableIValue* ret_values,
+    uint64_t num_outputs);
 
 #ifdef __cplusplus
 } // extern "C"
