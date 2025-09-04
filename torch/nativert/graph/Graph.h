42c1595db8b4f7f21f5edd75d9ccdb2a502d0d04
Kevin Fu <kevinqfu@meta.com>
2025-09-03 23:35:15 -0700

    [PT2]: Overriding Tensor device by SubmodNameToDevice
    Summary:
    A temporarily solution mainly for weights that are not moved to cuda in fake mode during publishing, but runs on cuda in serving.
    
    This has some overlap with placement, but with 2 differences:
    1. OverrideWeightsDevice only changes weights, not graph.
    2. Placement only handles mapping between non-empty cuda indices, while here we override everything as submodNameToDevice is the ground truth.
    
    Test Plan:
    ICE replayer with custom package:
    https://www.internalfb.com/intern/unidash/dashboard/ads_infra_cost_estimation/model_infra_cost_estimation/?e[select_ESTIMATION_RUN_ID]=ICE_kevinqfu_1756939411c164_replayeripnext_00
    
    Rollback Plan:
    
    Differential Revision: D81284723

--- torch/nativert/graph/Graph.h
+++ torch/nativert/graph/Graph.h
@@ -441,6 +441,9 @@ class Graph {
   void removeNode(Node* node);
 
   void applyDevicePlacement(const Placement& placement);
+  
+  // Override all weights in the graph if matching name is found in the map.
+  void overrideWeightsDevice(const std::unordered_map<std::string, std::optional<c10::Device>>& submodNameToDevice);
 
   std::string getUniqueValueName();
 
