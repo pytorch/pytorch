746a2c13ba70de170a5066495924c242a3c3ecbd
Ruben Rodriguez Buchillon <coconutruben@fb.com>
2025-09-04 10:57:50 -0700

    Update base for Update on "[inductor] add kernel template choice (ktc)"
    # why
    
    - gather everything up to make choices, without running
      potentially expensive generators
    - enables overrides where we toss the entire list of configs
      from inductor, without having to enumrate it (expensive)
    
    # what
    
    - add a holding class that just gets all the components necessary
      to generate a ChoiceCaller
    - use that class to generate ChoiceCallers
    - this does not (yet) add the override function, but just prepares
      the scene
    
    ```
    python3 -bb -m pytest test/inductor/test_max_autotune.py -v
    ```
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov
    
    Differential Revision: [D81520569](https://our.internmc.facebook.com/intern/diff/D81520569)
    
    [ghstack-poisoned]

--- torch/_inductor/template_heuristics/gemm.py
+++ torch/_inductor/template_heuristics/gemm.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from .. import config as inductor_config
 from .base import TemplateConfigHeuristics
 
 
+if TYPE_CHECKING:
+    from ..ir import Layout
+    from ..kernel_inputs import KernelInputs
+
+
 class GemmMaxAutotuneTemplateConfigHeuristics(TemplateConfigHeuristics):
-    @property
-    def should_run(self) -> bool:
+    def should_run(self, inputs: KernelInputs, layout: Layout) -> bool:
         """
         simple base override for GEMM family templates that run only in max-autotune
         """
