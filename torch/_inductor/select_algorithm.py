d5d48298484c72d321ca65319fa64793b98607d3
xinan.lin <xinan.lin@intel.com>
2025-09-04 08:53:35 +0000

    [Inductor XPU GEMM] Step 2/N: Move cutlass files from torch/_inductor/codegen/cuda to torch/_inductor/codegen/cutlass.
    Signed-off-by: xinan.lin <xinan.linintel.com>
    
    ghstack-source-id: 86050cd6046ac5b3f0e6f2284223dda2fd7e9f8a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160685

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -2373,7 +2373,7 @@ class AlgorithmSelectorCache(PersistentCache):
         return_multi_template=False,
         best_config_future=None,
     ):
-        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
+        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
 
         # Run preprocessing functions on choices
         for preprocessing_fn in self.preprocessing_fns:
@@ -2781,7 +2781,7 @@ class AlgorithmSelectorCache(PersistentCache):
                         "select_algorithm_num_precompilation_exceptions"
                     ] += 1
                     exceptions.append((futures[future], e))
-                    from torch._inductor.codegen.cuda.cuda_kernel import (
+                    from torch._inductor.codegen.cutlass.cuda_kernel import (
                         CUDATemplateCaller,
                     )
 
@@ -2914,7 +2914,9 @@ class AlgorithmSelectorCache(PersistentCache):
             try:
                 timing = cls.benchmark_choice(choice, autotune_args)
             except CUDACompileError as e:
-                from torch._inductor.codegen.cuda.cuda_kernel import CUDATemplateCaller
+                from torch._inductor.codegen.cutlass.cuda_kernel import (
+                    CUDATemplateCaller,
+                )
 
                 if not isinstance(choice, CUDATemplateCaller):
                     log.error(
@@ -2926,7 +2928,9 @@ class AlgorithmSelectorCache(PersistentCache):
                 log.warning("Not yet implemented: %s", e)
                 timing = float("inf")
             except RuntimeError as e:
-                from torch._inductor.codegen.cuda.cuda_kernel import CUDATemplateCaller
+                from torch._inductor.codegen.cutlass.cuda_kernel import (
+                    CUDATemplateCaller,
+                )
 
                 msg = str(e)
                 if "invalid argument" in msg:
@@ -3069,7 +3073,7 @@ class AlgorithmSelectorCache(PersistentCache):
             return prescreen_winners
 
         # prescreen cutlass
-        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
+        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
 
         candidates = []
         if (
@@ -3103,7 +3107,7 @@ class AlgorithmSelectorCache(PersistentCache):
         """
         Prune the choices after prescreening.
         """
-        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
+        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
 
         prescreen_key = f"{name}:{inputs_key}"
 
