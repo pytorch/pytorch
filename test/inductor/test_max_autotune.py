29b867a4b66e34b07bdb5c20ee4ad58cb7bac9ee
Eddie Yan <eddiey@nvidia.com>
2025-09-04 17:44:12 +0000

    check in

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -962,7 +962,7 @@ class TestMaxAutotune(TestCase):
 
         ref = x1 @ y1 + x2 @ y2
         act = f(x1, y1, x2, y2)
-        torch.testing.assert_close(act, ref, atol=1e-2, rtol=1e-2)
+        torch.testing.assert_close(act, ref, atol=8e-2, rtol=7e-2)
 
     @config.patch(
         max_autotune=True,
