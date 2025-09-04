e8bfd91ac2d545d56344a0d9d2d703ec5a08dc3a
Jacob Szwejbka <jakeszwe@fb.com>
2025-09-03 20:19:06 -0700

    clean up

--- torch/csrc/executorch/shim/module_shim.h
+++ torch/csrc/executorch/shim/module_shim.h
@@ -10,7 +10,7 @@ extern "C" {
 #endif
 
 /**
- * The type of a TypedStableIValue.
+ * The type of a StableIValue.
  */
 enum class StableIValueTag : uint32_t {
   None = 0,
@@ -21,7 +21,7 @@ enum class StableIValueTag : uint32_t {
 };
 
 /**
- * A wrapper containing StableIValue which is opaque and the type that it contains.
+ * A wrapper containing StableIValue which is opaque and its actual type.
  */
 struct TypedStableIValue{
     StableIValue val;
@@ -31,10 +31,29 @@ struct TypedStableIValue{
 struct ModuleOpaque;
 using ModuleHandle = ModuleOpaque*;
 
-AOTI_TORCH_EXPORT AOTITorchError experimental_torch_load_module_from_file(const char* package_path, uint64_t package_path_len, const char* model_name, uint64_t model_name_len, ModuleHandle* ret_value);
-AOTI_TORCH_EXPORT AOTITorchError experimental_torch_delete_module_object(ModuleHandle handle);
-AOTI_TORCH_EXPORT AOTITorchError experimental_torch_module_num_outputs(ModuleHandle handle, uint64_t* ret_value);
-AOTI_TORCH_EXPORT AOTITorchError experimental_torch_module_forward_flattened(ModuleHandle handle, const TypedStableIValue* args, uint64_t num_args, TypedStableIValue* ret_values, uint64_t num_outputs);
+AOTI_TORCH_EXPORT AOTITorchError
+experimental_torch_load_module_from_file(
+  const char* package_path,
+  uint64_t package_path_len,
+  const char* model_name,
+  uint64_t model_name_len,
+  ModuleHandle* ret_value);
+
+AOTI_TORCH_EXPORT AOTITorchError 
+experimental_torch_delete_module_object(ModuleHandle handle);
+
+AOTI_TORCH_EXPORT AOTITorchError 
+experimental_torch_module_num_outputs(
+  ModuleHandle handle,
+  uint64_t* ret_value);
+
+AOTI_TORCH_EXPORT AOTITorchError 
+experimental_torch_module_forward_flattened(
+  ModuleHandle handle,
+  const TypedStableIValue* args,
+  uint64_t num_args,
+  TypedStableIValue* ret_values,
+  uint64_t num_outputs);
 
 #ifdef __cplusplus
 } // extern "C"
--- torch/nativert/executorch/module_shim_impl.cpp
+++ torch/nativert/executorch/module_shim_impl.cpp
@@ -19,73 +19,106 @@
 using namespace::torch::nativert;
 
 namespace {
-  c10::IValue to_ivalue(const TypedStableIValue& v) {
+  AOTITorchError to_ivalue(const TypedStableIValue& v, c10::IValue* ret_val) {
     switch (v.tag) {
       case StableIValueTag::None:
-        return c10::IValue();
+        *ret_val = c10::IValue();
+        return 0;
       case StableIValueTag::Int:
-        return c10::IValue(to<int64_t>(v.val));
+        *ret_val =  c10::IValue(to<int64_t>(v.val));
+        return 0;
       case StableIValueTag::Bool:
-        return c10::IValue(to<bool>(v.val));
+        *ret_val =  c10::IValue(to<bool>(v.val));
+        return 0;
       case StableIValueTag::Double:
-        return c10::IValue(to<double>(v.val));
+        *ret_val =  c10::IValue(to<double>(v.val));
+        return 0;
       case StableIValueTag::Tensor:
         auto ret_raiiath = torch::aot_inductor::RAIIAtenTensorHandle(
           to<AtenTensorHandle>(v.val));
-        return (c10::IValue(*torch::aot_inductor::tensor_handle_to_tensor_pointer(
+        *ret_val =  (c10::IValue(*torch::aot_inductor::tensor_handle_to_tensor_pointer(
             ret_raiiath.get())));
+        return 0;
     }
   }
 
-  TypedStableIValue from_ivalue(const c10::IValue& v) {
+  AOTITorchError from_ivalue(const c10::IValue& v, TypedStableIValue* ret_val) {
     if (v.isNone()) {
-      return TypedStableIValue{from(std::nullopt), StableIValueTag::None};
+      *ret_val = TypedStableIValue{from(std::nullopt), StableIValueTag::None};
+      return 0;
     } else if (v.isInt()) {
-      return TypedStableIValue{from(v.toInt()), StableIValueTag::Int};
+      *ret_val = TypedStableIValue{from(v.toInt()), StableIValueTag::Int};
+      return 0;
     } else if (v.isBool()) {
-      return TypedStableIValue{from(v.toBool()), StableIValueTag::Int};
+      *ret_val = TypedStableIValue{from(v.toBool()), StableIValueTag::Int};
+      return 0;
     } else if (v.isDouble()) {
-      return TypedStableIValue{from(v.toDouble()), StableIValueTag::Int};
+      *ret_val = TypedStableIValue{from(v.toDouble()), StableIValueTag::Int};
+      return 0;
     } else if (v.isTensor()) {
       AtenTensorHandle ath = torch::aot_inductor::new_tensor_handle(
           std::move(const_cast<at::Tensor&>(v.toTensor())));
-      return TypedStableIValue{from(ath), StableIValueTag::Tensor};
+      *ret_val =  TypedStableIValue{from(ath), StableIValueTag::Tensor};
+      return 0;
     } else {
-      TORCH_CHECK(false, "Unsupported type");
+      return 1;
     }
   }
 } // namespace
 
 
-AOTITorchError experimental_torch_load_module_from_file(const char* package_path, uint64_t package_path_len, const char* model_name, uint64_t model_name_len, ModuleHandle* ret_value) {
+AOTITorchError experimental_torch_load_module_from_file(
+    const char* package_path,
+    uint64_t package_path_len,
+    const char* model_name,
+    uint64_t model_name_len,
+    ModuleHandle* ret_value) {
   std::string package_path_str(package_path, package_path_len);
   std::string model_name_str(model_name, model_name_len);
-  *ret_value = reinterpret_cast<ModuleHandle>(new ModelRunner(package_path_str, model_name_str));
+  *ret_value = reinterpret_cast<ModuleHandle>(
+    new ModelRunner(package_path_str, model_name_str));
   return 0;
 }
 
-AOTITorchError experimental_torch_delete_module_object(ModuleHandle handle) {
+AOTITorchError experimental_torch_delete_module_object(
+  ModuleHandle handle) {
   delete reinterpret_cast<ModelRunner*>(handle);
   return 0;
 }
 
-AOTITorchError experimental_torch_module_num_outputs(ModuleHandle handle, uint64_t* ret_value) {
+AOTITorchError experimental_torch_module_num_outputs(
+  ModuleHandle handle,
+  uint64_t* ret_value) {
   *ret_value = reinterpret_cast<ModelRunner*>(handle)->numOutputs();
   return 0;
 }
 
-AOTITorchError experimental_torch_module_forward_flattened(ModuleHandle handle, const TypedStableIValue* args, uint64_t num_args, TypedStableIValue* ret_values, uint64_t num_outputs) {
+AOTITorchError experimental_torch_module_forward_flattened(
+    ModuleHandle handle,
+    const TypedStableIValue* args,
+    uint64_t num_args,
+    TypedStableIValue* ret_values,
+    uint64_t num_outputs) {
+
   std::vector<c10::IValue> vec;
   vec.reserve(num_args);
   for (uint64_t i = 0; i < num_args; ++i) {
-    vec.push_back(to_ivalue(args[i]));
+    auto err = to_ivalue(args[i], &vec.emplace_back());
+    if (err != 0) {
+      return err;
+    }
   }
 
   std::vector<c10::IValue> out = reinterpret_cast<ModelRunner*>(handle)->runWithFlatInputsAndOutputs(std::move(vec));
-  TORCH_CHECK(out.size() == num_outputs);
+  if (out.size() != num_outputs) {
+    return 1;
+  }
 
   for (uint64_t i = 0; i < num_outputs; ++i) {
-    ret_values[i] = from_ivalue(out[i]);
+    auto err = from_ivalue(out[i], &ret_values[i]);
+    if (err != 0) {
+      return err;
+    }
   }
   return 0;
 }
