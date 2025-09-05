900950bb9f4b69e6784339cbab707f063ac6a0fe
Arsh Zahed <arshzahed@meta.com>
2025-09-05 16:32:56 -0700

    Use self.stack.enter_context

--- torch/_dynamo/compiled_autograd.py
+++ torch/_dynamo/compiled_autograd.py
@@ -347,65 +347,66 @@ class AutogradCompilerInstance:
 
         # Turn on PythonDispatcher during initial trace to make it identifiable
         # that tracing is happening, which is needed to prevent hashing symints
-        with enable_python_dispatcher():
-            # tensor inputs to fake tensors
-            x = inputs[0]  # mypy will complain about unbound x
-            try:
-                for idx, x in enumerate(inputs):
-                    inputs[idx] = self.wrap_fake(x, self.source("inputs", idx))
-            except Exception as e:
-                raise NotImplementedError(
-                    f"Found tensor of type {type(x)}, which is not supported by FakeTensorMode. {TURN_OFF_MSG}"
-                ) from e
-            self.bind_objects_to_proxies(inputs, args_proxy, inputs_origins)
+        self.stack.enter_context(enable_python_dispatcher())
 
-            # size inputs to symints
-            sym_sizes = [
-                self.shape_env.create_unspecified_symint_and_symbol(
+        # tensor inputs to fake tensors
+        x = inputs[0]  # mypy will complain about unbound x
+        try:
+            for idx, x in enumerate(inputs):
+                inputs[idx] = self.wrap_fake(x, self.source("inputs", idx))
+        except Exception as e:
+            raise NotImplementedError(
+                f"Found tensor of type {type(x)}, which is not supported by FakeTensorMode. {TURN_OFF_MSG}"
+            ) from e
+        self.bind_objects_to_proxies(inputs, args_proxy, inputs_origins)
+
+        # size inputs to symints
+        sym_sizes = [
+            self.shape_env.create_unspecified_symint_and_symbol(
+                val,
+                self.source("sizes", idx),
+                DimDynamic.DYNAMIC,
+            )
+            for idx, val in enumerate(sizes)
+        ]
+
+        # We want to mark every size as dynamic, but since there's no way to
+        # mark a primitive `int` as dynamic, we need to wrap it in a tensor.
+        # In the graph, we unwrap it with `unwrap_maybe_dynamic_int` back into a primitive.
+        proxies = [self.sizes_proxy[i] for i in range(len(sym_sizes))]  # type: ignore[index]
+        for i, symint in enumerate(sym_sizes):
+            proxies[i] = self.fx_tracer.create_proxy(
+                "call_function",
+                unwrap_maybe_dynamic_int,
+                (proxies[i],),
+                {},
+            )
+            self.symnode_proxy_lookup[symint.node] = proxies[i]
+        proxies = self.bind_objects_to_proxies(sym_sizes, proxies, sizes_origins)
+
+        for idx, val in enumerate(scalars):
+            source = self.source("scalars", idx)
+            if isinstance(val, int):
+                scalars[idx] = self.shape_env.create_unspecified_symint_and_symbol(
                     val,
-                    self.source("sizes", idx),
+                    source,
                     DimDynamic.DYNAMIC,
                 )
-                for idx, val in enumerate(sizes)
-            ]
-
-            # We want to mark every size as dynamic, but since there's no way to
-            # mark a primitive `int` as dynamic, we need to wrap it in a tensor.
-            # In the graph, we unwrap it with `unwrap_maybe_dynamic_int` back into a primitive.
-            proxies = [self.sizes_proxy[i] for i in range(len(sym_sizes))]  # type: ignore[index]
-            for i, symint in enumerate(sym_sizes):
-                proxies[i] = self.fx_tracer.create_proxy(
-                    "call_function",
-                    unwrap_maybe_dynamic_int,
-                    (proxies[i],),
-                    {},
-                )
-                self.symnode_proxy_lookup[symint.node] = proxies[i]
-            proxies = self.bind_objects_to_proxies(sym_sizes, proxies, sizes_origins)
-
-            for idx, val in enumerate(scalars):
-                source = self.source("scalars", idx)
-                if isinstance(val, int):
-                    scalars[idx] = self.shape_env.create_unspecified_symint_and_symbol(
+            elif isinstance(val, float):
+                scalars[idx] = self.shape_env.create_symfloatnode(
+                    self.shape_env.create_unspecified_symbol(
                         val,
-                        source,
-                        DimDynamic.DYNAMIC,
-                    )
-                elif isinstance(val, float):
-                    scalars[idx] = self.shape_env.create_symfloatnode(
-                        self.shape_env.create_unspecified_symbol(
-                            val,
-                            source=source,
-                            dynamic_dim=DimDynamic.DYNAMIC,
-                        ),
-                        hint=val,
                         source=source,
-                    )
-                else:
-                    raise AssertionError("Unexpected scalar type: ", type(val))
-            self.bind_objects_to_proxies(scalars, self.scalars_proxy, scalars_origins)
-            for i, symval in enumerate(scalars):
-                self.symnode_proxy_lookup[symval.node] = self.scalars_proxy[i]  # type: ignore[union-attr]
+                        dynamic_dim=DimDynamic.DYNAMIC,
+                    ),
+                    hint=val,
+                    source=source,
+                )
+            else:
+                raise AssertionError("Unexpected scalar type: ", type(val))
+        self.bind_objects_to_proxies(scalars, self.scalars_proxy, scalars_origins)
+        for i, symval in enumerate(scalars):
+            self.symnode_proxy_lookup[symval.node] = self.scalars_proxy[i]  # type: ignore[union-attr]
 
         # TODO(jansel): are all these modes needed?
         self.stack.enter_context(decompose({}))
