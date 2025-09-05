4c22d538f4ca2ceffb879924da2f82d8f5e1d90f
rzou <zou3519@gmail.com>
2025-09-05 15:28:59 -0700

    [DTensor] fix F.one_hot
    F.one_hot(dtensor) used to run into a mixed DTensor-Tensor operation due
    to an arange call creating a new Tensor (not DTensor). This PR fixes it
    by allowing implicit replication of Tensors for the arange call and the
    one consumer of the arange call (the at::eq call).
    
    Test Plan:
    - new test. Also, F.one_hot(num_classes=-1) is broken so we skip that.
    
    [ghstack-poisoned]

--- torch/distributed/tensor/_api.py
+++ torch/distributed/tensor/_api.py
@@ -355,11 +355,12 @@ class DTensor(torch.Tensor):
     # pyre-fixme[3]: Return type must be annotated.
     # pyre-fixme[2]: Parameter must be annotated.
     def __torch_dispatch__(cls, func, types, args=(), kwargs=None):  # type: ignore[override]
-        return DTensor._op_dispatcher.dispatch(
+        result = DTensor._op_dispatcher.dispatch(
             func,
             args,
             kwargs or {},
         )
+        return result
 
     @staticmethod
     def from_local(
