8a31c89a91ab01b43b61045f59eb11ee70891856
morrison-turnansky <mturnans@redhat.com>
2025-09-05 15:53:20 +0000

    updated release behaviour of release_available_cached_blocks to prevent crash

--- c10/cuda/CUDACachingAllocator.cpp
+++ c10/cuda/CUDACachingAllocator.cpp
@@ -2977,8 +2977,8 @@ class DeviceCachingAllocator {
           --it;
         }
         if (!(*cur)->expandable_segment_) {
-          release_block(*cur, context);
           totalReleased += (*cur)->size;
+          release_block(*cur, context);
         }
         if (is_first) {
           break;
