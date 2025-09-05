ca7e93b373ded794d40600004be74626276c7105
Simon Fan <xmfan@meta.com>
2025-09-04 17:24:09 -0700

    Update
    [ghstack-poisoned]

--- test/higher_order_ops/test_local_map.py
+++ test/higher_order_ops/test_local_map.py
@@ -1,4 +1,5 @@
 # Owner(s): ["module: higher order operators"]
+# flake8: noqa: B950
 
 
 import torch
@@ -11,7 +12,7 @@ from torch import nn
 from torch._dynamo.variables.higher_order_ops import LocalMapWrappedHigherOrderVariable
 from torch.distributed._tensor.experimental import local_map
 from torch.distributed.tensor.placement_types import Replicate, Shard
-from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.testing._internal.common_utils import run_tests, TEST_WITH_CROSSREF, TestCase
 from torch.testing._internal.triton_utils import requires_cuda_and_triton
 
 
@@ -112,12 +113,84 @@ class TestLocalMap(TestCase):
         nheads = 48
         seq_len = 256
 
+        from torch._dynamo.testing import EagerAndRecordGraphs, normalize_gm
+
+        backend = EagerAndRecordGraphs()
+
         model = create_model(cp_decorated, nheads, dim1, dim2).cuda()
         inputs = (torch.randn(bs, seq_len, dim1, requires_grad=True).cuda(),)
         with LocalMapWrappedHigherOrderVariable.enable():
-            out = torch.compile(model, fullgraph=False)(*inputs)
+            out = torch.compile(model, backend=backend)(*inputs)
         out.sum().backward()
 
+        model = create_model(cp_function, nheads, dim1, dim2).cuda()
+        inputs = (torch.randn(bs, seq_len, dim1, requires_grad=True).cuda(),)
+        with LocalMapWrappedHigherOrderVariable.enable():
+            out = torch.compile(model, backend=backend)(*inputs)
+        out.sum().backward()
+
+        if not TEST_WITH_CROSSREF:
+            self.assertEqual(len(backend.graphs), 2)
+            # should see local_map_hop in both
+            self.assertExpectedInline(
+                normalize_gm(backend.graphs[0].print_readable(print_output=False)),
+                """\
+class GraphModule(torch.nn.Module):
+    def forward(self, L_self_modules_wq_parameters_weight_: "f32[6144, 6144]", L_x_: "f32[256, 256, 6144]", L_self_modules_wk_parameters_weight_: "f32[6144, 6144]", L_self_modules_wv_parameters_weight_: "f32[6144, 6144]", L_self_modules_wo_parameters_weight_: "f32[6144, 6144]", L_self_modules_w1_parameters_weight_: "f32[24576, 6144]", L_self_modules_w2_parameters_weight_: "f32[6144, 24576]"):
+        l_self_modules_wq_parameters_weight_ = L_self_modules_wq_parameters_weight_
+        l_x_ = L_x_
+        l_self_modules_wk_parameters_weight_ = L_self_modules_wk_parameters_weight_
+        l_self_modules_wv_parameters_weight_ = L_self_modules_wv_parameters_weight_
+        l_self_modules_wo_parameters_weight_ = L_self_modules_wo_parameters_weight_
+        l_self_modules_w1_parameters_weight_ = L_self_modules_w1_parameters_weight_
+        l_self_modules_w2_parameters_weight_ = L_self_modules_w2_parameters_weight_
+
+        q: "f32[256, 256, 6144]" = torch._C._nn.linear(l_x_, l_self_modules_wq_parameters_weight_, None);  l_self_modules_wq_parameters_weight_ = None
+
+        k: "f32[256, 256, 6144]" = torch._C._nn.linear(l_x_, l_self_modules_wk_parameters_weight_, None);  l_self_modules_wk_parameters_weight_ = None
+
+        v: "f32[256, 256, 6144]" = torch._C._nn.linear(l_x_, l_self_modules_wv_parameters_weight_, None);  l_self_modules_wv_parameters_weight_ = None
+
+        unflatten: "f32[256, 256, 48, 128]" = q.unflatten(-1, (48, -1));  q = None
+        q_1: "f32[256, 48, 256, 128]" = unflatten.permute(0, 2, 1, 3);  unflatten = None
+
+        unflatten_1: "f32[256, 256, 48, 128]" = k.unflatten(-1, (48, -1));  k = None
+        k_1: "f32[256, 48, 256, 128]" = unflatten_1.permute(0, 2, 1, 3);  unflatten_1 = None
+
+        unflatten_2: "f32[256, 256, 48, 128]" = v.unflatten(-1, (48, -1));  v = None
+        v_1: "f32[256, 48, 256, 128]" = unflatten_2.permute(0, 2, 1, 3);  unflatten_2 = None
+
+        subgraph_0 = self.subgraph_0
+        local_map_hop = torch.ops.higher_order.local_map_hop(subgraph_0, q_1, k_1, v_1);  subgraph_0 = q_1 = k_1 = v_1 = None
+        o: "f32[256, 48, 256, 128]" = local_map_hop[0];  local_map_hop = None
+
+        permute_3: "f32[256, 256, 48, 128]" = o.permute(0, 2, 1, 3);  o = None
+        o_1: "f32[256, 256, 6144]" = permute_3.flatten(-2);  permute_3 = None
+
+        o_2: "f32[256, 256, 6144]" = torch._C._nn.linear(o_1, l_self_modules_wo_parameters_weight_, None);  o_1 = l_self_modules_wo_parameters_weight_ = None
+
+        o0: "f32[256, 256, 6144]" = o_2 + l_x_;  o_2 = l_x_ = None
+
+        o_3: "f32[256, 256, 24576]" = torch._C._nn.linear(o0, l_self_modules_w1_parameters_weight_, None);  l_self_modules_w1_parameters_weight_ = None
+
+        o_4: "f32[256, 256, 24576]" = torch.nn.functional.relu(o_3);  o_3 = None
+
+        o_5: "f32[256, 256, 6144]" = torch._C._nn.linear(o_4, l_self_modules_w2_parameters_weight_, None);  o_4 = l_self_modules_w2_parameters_weight_ = None
+
+        o_6: "f32[256, 256, 6144]" = o0 + o_5;  o0 = o_5 = None
+        return (o_6,)
+
+    class subgraph_0(torch.nn.Module):
+        def forward(self, q_1: "f32[256, 48, 256, 128]", k_1: "f32[256, 48, 256, 128]", v_1: "f32[256, 48, 256, 128]"):
+            out: "f32[256, 48, 256, 128]" = torch._C._nn.scaled_dot_product_attention(query = q_1, key = k_1, value = v_1, is_causal = False);  q_1 = k_1 = v_1 = None
+            return (out,)
+""",
+            )
+
+            self.assertEqual(
+                normalize_gm(backend.graphs[0].print_readable(print_output=False)),
+                normalize_gm(backend.graphs[1].print_readable(print_output=False))
+            )
 
 if __name__ == "__main__":
     run_tests()
