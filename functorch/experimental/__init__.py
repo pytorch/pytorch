a863b0663bc0776936f8f4935528e901181d31d1
gaoyufeng <15834128411@126.com>
2025-09-07 07:22:27 +0800

    Development environment rollback

--- functorch/experimental/__init__.py
+++ functorch/experimental/__init__.py
@@ -1,5 +1,8 @@
 # PyTorch forward-mode is not mature yet
-from functorch import functionalize
+try:
+    from functorch import functionalize  # deprecation warnings
+except ImportError:
+    from torch._functorch.deprecated import functionalize  # Development environment 
 from torch._functorch.apis import chunk_vmap
 from torch._functorch.batch_norm_replacement import replace_all_batch_norm_modules_
 from torch._functorch.eager_transforms import hessian, jacfwd, jvp
