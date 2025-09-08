9c991b63ff59e66aae579b868abc4be06c6054fa
Ting Lu <tingl@nvidia.com>
2025-09-08 20:00:20 +0000

    [CD] [aarch64] Add CUDA 12.6 and 12.8 to build matrix, remove 12.9 build (#162364)
    https://github.com/pytorch/pytorch/issues/159779
    
    Add the full CUDA support matrix to sbsa build (12.6, 12.8)
    Same arch support as x86 build
    Remove 12.9 sbsa build
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162364
    Approved by: https://github.com/atalman

--- .ci/aarch64_linux/aarch64_ci_build.sh
+++ .ci/aarch64_linux/aarch64_ci_build.sh
@@ -3,12 +3,13 @@ set -eux -o pipefail
 
 GPU_ARCH_VERSION=${GPU_ARCH_VERSION:-}
 
-if [[ "$GPU_ARCH_VERSION" == *"12.9"* ]]; then
-    export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;12.0"
-fi
-
-if [[ "$GPU_ARCH_VERSION" == *"13.0"* ]]; then
-    export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;11.0;12.0"
+# Set CUDA architecture lists to match x86 build_cuda.sh
+if [[ "$GPU_ARCH_VERSION" == *"12.6"* ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;8.0;9.0"
+elif [[ "$GPU_ARCH_VERSION" == *"12.8"* ]]; then
+    export TORCH_CUDA_ARCH_LIST="7.0;8.0;9.0;10.0;12.0"
+elif [[ "$GPU_ARCH_VERSION" == *"13.0"* ]]; then
+    export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;11.0;12.0+PTX"
 fi
 
 # Compress the fatbin with -compress-mode=size for CUDA 13
