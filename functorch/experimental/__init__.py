46809b32db076dc1032523105d53bfec9f05b955
gaoyufeng <15834128411@126.com>
2025-09-08 22:12:21 +0800

    format update

--- functorch/experimental/__init__.py
+++ functorch/experimental/__init__.py
@@ -2,7 +2,8 @@
 try:
     from functorch import functionalize  # deprecation warnings
 except ImportError:
-    from torch._functorch.deprecated import functionalize  # Development environment 
+    from torch._functorch.deprecated import functionalize  # Development environment
+
 from torch._functorch.apis import chunk_vmap
 from torch._functorch.batch_norm_replacement import replace_all_batch_norm_modules_
 from torch._functorch.eager_transforms import hessian, jacfwd, jvp
