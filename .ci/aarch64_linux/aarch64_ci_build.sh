056a148658aaf78a55305fef52eaf2a9e8ba43bd
Ting Lu <tingl@nvidia.com>
2025-09-08 00:01:32 -0700

    remove some archs due to size limit

--- .ci/aarch64_linux/aarch64_ci_build.sh
+++ .ci/aarch64_linux/aarch64_ci_build.sh
@@ -5,11 +5,11 @@ GPU_ARCH_VERSION=${GPU_ARCH_VERSION:-}
 
 # Set CUDA architecture lists to match x86 build_cuda.sh
 if [[ "$GPU_ARCH_VERSION" == *"12.6"* ]]; then
-    export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;8.0;9.0"
 elif [[ "$GPU_ARCH_VERSION" == *"12.8"* ]]; then
-    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0;10.0;12.0"
+    export TORCH_CUDA_ARCH_LIST="7.0;8.0;9.0;10.0;12.0"
 elif [[ "$GPU_ARCH_VERSION" == *"13.0"* ]]; then
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0;10.0;12.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0;11.0;12.0+PTX"
 fi
 
 # Compress the fatbin with -compress-mode=size for CUDA 13
