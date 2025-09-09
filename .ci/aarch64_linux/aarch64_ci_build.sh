17fdeecd4a007c15963f5adca94b9d0ddd26b795
Ting Lu <tingl@nvidia.com>
2025-09-09 10:32:33 -0700

    remove the platform_machine for 12.6 and 12.8

--- .ci/aarch64_linux/aarch64_ci_build.sh
+++ .ci/aarch64_linux/aarch64_ci_build.sh
@@ -41,9 +41,6 @@ else
         echo "Bundling CUDA libraries with wheel for aarch64."
     else
         echo "Using nvidia libs from pypi for aarch64."
-        # Fix platform constraints in PYTORCH_EXTRA_INSTALL_REQUIREMENTS for aarch64
-        # Replace 'platform_machine == "x86_64"' with 'platform_machine == "aarch64"'
-        export PYTORCH_EXTRA_INSTALL_REQUIREMENTS="${PYTORCH_EXTRA_INSTALL_REQUIREMENTS//platform_machine == \'x86_64\'/platform_machine == \'aarch64\'}"
         echo "Updated PYTORCH_EXTRA_INSTALL_REQUIREMENTS for aarch64: $PYTORCH_EXTRA_INSTALL_REQUIREMENTS"
         export USE_NVIDIA_PYPI_LIBS=1
     fi
