212f7b2963538aa914ac5bc83558e2b427317419
Jeffro <0xjeffro@gmail.com>
2025-09-10 03:17:11 -0700

    Use `steady_clock` consistently in `WorkInfo` and `runHookLoop`
    Unify `timeStarted` and `timeFinished` to use `std::chrono::steady_clock` across
    `ProcessGroupNCCL::runHookLoop` and `WorkInfo`.

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -2624,10 +2624,10 @@ void ProcessGroupNCCL::runHookLoop() {
         // Hook might grab GIL, unlock first to prevent deadlock
         lock.unlock();
 
-        auto timeFinished = std::chrono::system_clock::now();
+        auto timeFinished = std::chrono::steady_clock::now();
         auto timeStarted =
             timeFinished +
-            std::chrono::duration_cast<std::chrono::system_clock::duration>(
+            std::chrono::duration_cast<std::chrono::steady_clock::duration>(
                 work.workStartTime_ - std::chrono::steady_clock::now());
         onCompletionHook_(std::make_shared<WorkInfo>(
             work.retrieveOpType(), // OpType
