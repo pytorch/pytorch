995229c6de2ecc3e411369d76611198feb11deda
atalman <atalman@fb.com>
2025-09-10 15:53:58 -0700

    fix

--- .ci/pytorch/smoke_test/smoke_test.py
+++ .ci/pytorch/smoke_test/smoke_test.py
@@ -396,7 +396,7 @@ def smoke_test_nvshmem() -> None:
     except ImportError:
         # Not built with NVSHMEM support.
         # torch is not compiled with NVSHMEM prior to 2.9
-        if torch.__version__ < "2.9":
+        if torch.__version__ < "2.9.*":
             return
         else:
             # After 2.9: NVSHMEM is expected to be compiled in current build
