8413e5157b882a0a08afa2da0fc6ff48acfa88c4
Yu, Guangye <guangye.yu@intel.com>
2025-09-10 11:11:56 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/DeviceAccelerator.cpp
+++ torch/csrc/DeviceAccelerator.cpp
@@ -136,6 +136,12 @@ void initModule(PyObject* module) {
   m.def("_accelerator_resetPeakStats", [](c10::DeviceIndex device_index) {
     at::accelerator::resetPeakStats(device_index);
   });
+
+  m.def("_accelerator_getMemoryInfo", [](c10::DeviceIndex device_index) {
+    const auto device_type = at::accelerator::getAccelerator(true).value();
+    torch::utils::maybe_initialize_device(device_type);
+    return at::accelerator::getMemoryInfo(device_index);
+  });
 }
 
 } // namespace torch::accelerator
