29f84b0f617013e091fc7a0575684ddf46c541d4
FFFrog <ljw1101.vip@gmail.com>
2025-09-13 02:06:25 +0800

    [OpenReg] Improve the Event and Stream capabilities of DeviceGuardImplInterface (#160101)
    **Changes:**
    
    - Based on `OpenRegStream` and `OpenRegEvent`, we improve the implementation of Device Guard for `OpenReg`
    - Add some related testcases
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160101
    Approved by: https://github.com/albanD
    ghstack dependencies: #161917, #161918

--- test/cpp_extensions/open_registration_extension/torch_openreg/tests/test_streams.py
+++ test/cpp_extensions/open_registration_extension/torch_openreg/tests/test_streams.py
@@ -5,10 +5,55 @@ from torch.testing._internal.common_utils import run_tests, skipIfTorchDynamo, T
 
 
 class TestStream(TestCase):
+    def test_stream_create(self):
+        stream = torch.Stream(device="openreg")
+        self.assertEqual(stream.device_index, torch.openreg.current_device())
+
+        stream = torch.Stream(device="openreg:1")
+        self.assertEqual(stream.device.type, "openreg")
+        self.assertEqual(stream.device_index, 1)
+
+        stream = torch.Stream(1)
+        self.assertEqual(stream.device.type, "openreg")
+        self.assertEqual(stream.device_index, 1)
+
+        stream1 = torch.Stream(
+            stream_id=stream.stream_id,
+            device_type=stream.device_type,
+            device_index=stream.device_index,
+        )
+        self.assertEqual(stream, stream1)
+
+    def test_stream_context(self):
+        with torch.Stream(device="openreg:1") as stream:
+            self.assertEqual(torch.accelerator.current_stream(), stream)
+
+    @skipIfTorchDynamo()
+    def test_stream_switch(self):
+        stream1 = torch.Stream(device="openreg:0")
+        torch.accelerator.set_stream(stream1)
+        current_stream = torch.accelerator.current_stream()
+        self.assertEqual(current_stream, stream1)
+
+        stream2 = torch.Stream(device="openreg:1")
+        torch.accelerator.set_stream(stream2)
+        current_stream = torch.accelerator.current_stream()
+        self.assertEqual(current_stream, stream2)
+
     def test_stream_synchronize(self):
         stream = torch.Stream(device="openreg:1")
+        self.assertEqual(True, stream.query())
+
+        event = torch.Event()
+        event.record(stream)
         stream.synchronize()
         self.assertEqual(True, stream.query())
+
+    def test_stream_repr(self):
+        stream = torch.Stream(device="openreg:1")
+        self.assertTrue(
+            "torch.Stream device_type=openreg, device_index=1" in repr(stream)
+        )
 
     def test_stream_wait_stream(self):
         stream_1 = torch.Stream(device="openreg:0")
