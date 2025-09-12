b0dbf8a5ebc8572247266260574613502b857320
Yu, Guangye <guangye.yu@intel.com>
2025-09-12 03:15:34 +0000

    Add a new API torch.cuda.can_device_access_peer
    ghstack-source-id: 0a869e74251fb7bc733cbb1d6624bbfdafef0ddd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162705

--- torch/csrc/xpu/Module.cpp
+++ torch/csrc/xpu/Module.cpp
@@ -415,6 +415,12 @@ static void initXpuMethodBindings(PyObject* module) {
         return std::make_tuple(
             stream.id(), stream.device_index(), stream.device_type());
       });
+  m.def(
+      "_xpu_canDeviceAccessPeer",
+      [](c10::DeviceIndex device, c10::DeviceIndex peer) {
+        torch::utils::device_lazy_init(at::kXPU);
+        return at::xpu::canDeviceAccessPeer(device, peer);
+      });
 }
 
 // Callback for python part. Used for additional initialization of python
