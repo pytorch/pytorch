76044b67da336fb6d14842cc81813218b86f47cb
Thien Tran <gau.nernst@yahoo.com.sg>
2025-09-13 14:39:57 +0800

    enable mangled names

--- torch/cuda/_utils.py
+++ torch/cuda/_utils.py
@@ -1,6 +1,6 @@
 import ctypes
 import sys
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, Tuple
 
 import torch
 
@@ -115,7 +115,7 @@ def _nvrtc_compile(
     header_code: str = "",
     cuda_include_dirs: Optional[list] = None,
     nvcc_options: Optional[list] = None,
-) -> bytes:
+) -> Tuple[bytes, str]:
     """
     Compiles a CUDA kernel using NVRTC and returns the PTX code.
 
@@ -129,7 +129,7 @@ def _nvrtc_compile(
         nvcc_options (list, None): Additional options to pass to NVRTC
 
     Returns:
-        str: The compiled PTX code
+        Tuple[bytes, str]: The compiled PTX code and mangled kernel name
     """
     # Ensure CUDA is initialized
     import torch.cuda
@@ -153,8 +153,8 @@ def _nvrtc_compile(
             raise RuntimeError(f"CUDA error: {error_message}")
 
     # Add 'extern "C"' if not already present to ensure C linkage
-    if not kernel_source.strip().startswith('extern "C"'):
-        kernel_source = f'extern "C" {kernel_source}'
+    # if not kernel_source.strip().startswith('extern "C"'):
+    #     kernel_source = f'extern "C" {kernel_source}'
 
     # Combine header code and kernel source
     if header_code:
@@ -217,6 +217,10 @@ def _nvrtc_compile(
         )
     )
 
+    # Add kernel name, which can be a template expression
+    c_kernel_name = kernel_name.encode("utf-8")
+    check_nvrtc(libnvrtc.nvrtcAddNameExpression(prog, c_kernel_name))
+
     # Compile program
     res = libnvrtc.nvrtcCompileProgram(prog, num_options, options_array)
 
@@ -234,12 +238,18 @@ def _nvrtc_compile(
     check_nvrtc(libnvrtc.nvrtcGetPTXSize(prog, ctypes.byref(ptx_size)))
     ptx = ctypes.create_string_buffer(ptx_size.value)
     check_nvrtc(libnvrtc.nvrtcGetPTX(prog, ptx))
+
+    c_mangled_name = ctypes.c_char_p()
+    check_nvrtc(libnvrtc.nvrtcGetLoweredName(prog, c_kernel_name, ctypes.byref(c_mangled_name)))
+    mangled_name = c_mangled_name.value.decode()  # make a copy
+
     libnvrtc.nvrtcDestroyProgram(ctypes.byref(prog))
 
     # For HIP, hipRTC generates raw CO binaries instead of PTX,
     # and for some reason, ".value" causes the string to be truncated,
     # likely due to the presence of '\0' in the string. So we use .raw instead.
-    return ptx.raw if torch.version.hip else ptx.value
+    ptx_bytes = ptx.raw if torch.version.hip else ptx.value
+    return ptx_bytes, mangled_name
 
 
 class _CudaModule:
