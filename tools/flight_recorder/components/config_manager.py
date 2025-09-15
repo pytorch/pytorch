ed9dc772ed47ee699493d93c50b9a0792d1578dc
Phillip Liu <phillipliu01@meta.com>
2025-09-15 15:05:25 -0700

    [Flight Recorder][WP] Added mismatch tail as an arg (#162991)
    Summary:
    
    Mismatch tail is used as a fixed variable and there are cases that there are more than 10 mismatches FR gives up producing results (e.g. https://fburl.com/ai_infra/7gjl5ucb). This diff added the mismatch tail in the parsed args so make this configuarble.
    
    
    Also tho the variable name is `mismatch_tail`(last 10) it is used as `mismatch_head` (the first 10). Updated it to be `num_mismatch_to_print`
    
    Test Plan:
    `buck2 run @//mode/opt //caffe2/fb/flight_recorder:fr_trace -- --mast_job_id aps-ctx_fm_pipeline_change-1c8ea38a94 --mast_job_version 0 --mast_job_attempt 2 --bucket tlcm_log_blob --world_size 128 --dump_file_name_offset 0 --allow-incomplete-ranks ----mismatch_cap 20 1>out 2>err`
    Confirm no error and output 20 mismatches.
    
    Rollback Plan:
    
    Reviewed By: fduwjj
    
    Differential Revision: D82335995

--- tools/flight_recorder/components/config_manager.py
+++ tools/flight_recorder/components/config_manager.py
@@ -68,6 +68,12 @@ class JobConfig:
         self.parser.add_argument("-j", "--just_print_entries", action="store_true")
         self.parser.add_argument("-v", "--verbose", action="store_true")
         self.parser.add_argument("--print_stack_trace", action="store_true")
+        self.parser.add_argument(
+            "--mismatch_cap",
+            type=int,
+            default=10,
+            help="Maximum number of mismatches we print (from earliest).",
+        )
 
     def parse_args(
         self: "JobConfig", args: Optional[Sequence[str]]
