f56ada96545af308885dfafff2ad5c381728a8f2
Markus Hoehnerbach <mhoehnerbach@fb.com>
2025-09-16 06:22:44 -0700

    [inductor] in emulate_precision_casts, disable fma fusion in triton
    [ghstack-poisoned]

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -754,6 +754,7 @@ class CachingAutotuner(KernelInterface):
             "num_stages": compile_meta["num_stages"],
             "debug": compile_meta["debug"],
             "sanitize_overflow": False,  # turn off additional asserts added for overflow checks
+            "enable_fp_fusion": compile_meta["enable_fp_fusion"],
         }
         if HAS_WARP_SPEC:
             options.update(
