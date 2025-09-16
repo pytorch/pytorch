fa9cdc4699e4efde1105f67f838c04a20eb1d8a6
Michael Lazos <mlazos@meta.com>
2025-09-15 17:48:43 -0700

    Update base for Update on "[user-streams] update stream context to use fork/join"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames Lucaskabela
    
    [ghstack-poisoned]

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -2144,6 +2144,8 @@ class GuardBuilder(GuardBuilderBase):
                 range,
                 dict_keys,
                 torch.Size,
+                torch.Stream,
+                torch.cuda.streams.Stream,
                 *np_types,
                 *ok_mutable_types,
             }
