4d160bae369bd3eab8ce4b5d9a43fc651f136ddc
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2025-09-16 14:33:50 -0700

    Update base for Update on "Fix inconsistent test and add new tracer as config"
    It is better to have the new tracer as global config that can be manipulated easily. Also I believe dynamo-like config infra is useful instead of relying on custom way of patching stuff.
    
    Differential Revision: [D82478649](https://our.internmc.facebook.com/intern/diff/D82478649)
    
    [ghstack-poisoned]

--- test/export/test_swap.py
+++ test/export/test_swap.py
@@ -378,12 +378,11 @@ def forward(self, x, y):
                 return x + torch.matmul(inputs.a, inputs.b)
 
         for use_new_tracer in [True, False]:
-            ep = torch.export._trace._export(
+            ep = export(
                 Foo(),
                 (torch.randn(2, 2),),
                 {"inputs": CustomInput(torch.randn(2, 3), torch.randn(3, 2))},
                 strict=self.strict,
-                pre_dispatch=True,
                 _use_new_tracer_experimental=use_new_tracer,
             )
         swapped = _swap_modules(ep, {})
@@ -392,6 +391,30 @@ def forward(self, x, y):
         res1 = torch.fx.Interpreter(swapped).run(*(*inp_args, *inp_kwargs.values()))
         res2 = swapped(*inp_args, **inp_kwargs)
         self.assertTrue(torch.allclose(res1, res2))
+
+    def test_custom_input_kwargs_use_private(self):
+        @dataclass
+        class CustomInput:
+            a: Tensor
+            b: Tensor
+
+        register_dataclass_as_pytree_node(
+            CustomInput,
+            serialized_type_name="test_swap.test_custom_input.CustomInput",
+        )
+
+        class Foo(torch.nn.Module):
+            def forward(self, x, *, inputs):
+                return x + torch.matmul(inputs.a, inputs.b)
+
+        # shouldn't error
+        _ = export(
+            Foo(),
+            (torch.randn(2, 2),),
+            {"inputs": CustomInput(torch.randn(2, 3), torch.randn(3, 2))},
+            strict=self.strict,
+            _use_new_tracer_experimental=True,
+        )
 
     def test_custom_output(self):
         @dataclass
