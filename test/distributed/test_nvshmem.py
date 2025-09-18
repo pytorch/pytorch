7569835700500b4c6904b27e3ba8fe680e17e6db
Ke Wen <kw2501@meta.com>
2025-09-18 14:52:33 -0700

    [SymmMem] Fix memory allocation hold-up (#162680)
    Problem:
    Without MemPool it looks like nvshmem backend never deallocates memory.
    
    Cause:
    Handles in `symm_mems_` (a map) keeps reference to memory allocations.
    
    Solution:
    - Remove reference to allocation from handles -- the reference is never used anyway.
    - Use `unique_ptr` instead of `shared_ptr` to wrap allocation to ensure single ownership.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162680
    Approved by: https://github.com/ezyang
    ghstack dependencies: #163298
    
    (cherry picked from commit 7130b174e07dbc1a708934b18dede3d88e8f779f)

--- test/distributed/test_nvshmem.py
+++ test/distributed/test_nvshmem.py
@@ -240,8 +240,6 @@ class NVSHMEMAll2AllTest(MultiProcContinuousTest):
     def _init_device(self) -> None:
         # TODO: relieve this (seems to hang if without)
         device_module.set_device(self.device)
-        # NOTE: required for nvshmem allocation
-        torch.empty(1, device=self.device)
         # Set NVSHMEM as SymmMem backend
         symm_mem.set_backend("NVSHMEM")
 
@@ -306,6 +304,9 @@ class NVSHMEMAll2AllTest(MultiProcContinuousTest):
         )
         # Row 0 is input splits
         in_out_splits[0].copy_(inp_splits)
+
+        # Sync all ranks to ensure remote tensors are allocated
+        dist.barrier()
 
         torch.ops.symm_mem.all_to_all_vdev(inp, out, in_out_splits, group_name)
 
@@ -374,6 +375,9 @@ class NVSHMEMAll2AllTest(MultiProcContinuousTest):
         out_splits_offsets = symm_mem.empty(
             (2, nsplits), dtype=torch.int64, device=self.device
         ).fill_(-1)
+
+        # Sync all ranks to ensure remote tensors are allocated
+        dist.barrier()
 
         torch.ops.symm_mem.all_to_all_vdev_2d(
             inp, out, in_splits, out_splits_offsets, group_name, major_align=align
@@ -489,6 +493,9 @@ class NVSHMEMAll2AllTest(MultiProcContinuousTest):
         in_splits_offsets[0].copy_(inp_splits)
         # Row 1 is input offsets
         in_splits_offsets[1].copy_(inp_offsets)
+
+        # Sync all ranks to ensure remote tensors are allocated
+        dist.barrier()
 
         torch.ops.symm_mem.all_to_all_vdev_2d_offset(
             inp, out, in_splits_offsets, out_splits_offsets, group_name
