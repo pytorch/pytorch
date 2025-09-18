dd9a00b4a99aedd94e0bedcfa947be3434ead390
FFFrog <ljw1101.vip@gmail.com>
2025-09-19 00:07:15 +0800

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/autograd/utils/error_messages.h
+++ torch/csrc/autograd/utils/error_messages.h
@@ -4,14 +4,12 @@
 
 namespace torch::autograd::utils {
 
-inline std::string requires_grad_leaf_error(bool requires_grad) {
+inline std::string requires_grad_leaf_error() {
   std::ostringstream oss;
-  oss << "you can only change requires_grad flags of leaf variables.";
-  if (requires_grad == false) {
-    oss << " If you want to use a computed variable in a subgraph "
-           "that doesn't require differentiation use "
-           "var_no_grad = var.detach().";
-  }
+  oss << "you can only change requires_grad flags of leaf variables."
+         " If you want to use a computed variable in a subgraph "
+         "that doesn't require differentiation use "
+         "var_no_grad = var.detach().";
   return oss.str();
 }
 
