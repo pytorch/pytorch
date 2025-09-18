3481232947deab0a13d72f5d798a9893c9c9b310
CaoE <e.cao@intel.com>
2025-09-18 16:11:24 +0000

    Add SDPA patterns for T5 variants generate by batch size 1

--- torch/_inductor/fx_passes/fuse_attention.py
+++ torch/_inductor/fx_passes/fuse_attention.py
@@ -581,42 +581,6 @@ def _sfdp_replacement_20(query, key, value, attn_mask, dropout_p):
     )
 
 
-def _sfdp_pattern_24(query, key, value, attention_mask):
-    """
-    this pattern is for MBartForCausalLM/PLBartForCausalLM.
-    attn_mask has a different dtype with QKV.
-    there is no scale in sdpa.
-    """
-    bs = query.size(0)
-    n_head = query.size(1)
-    seq_len = query.size(2)
-    head_size = query.size(3)
-    q = query.view(bs * n_head, -1, head_size)
-    k = key.reshape(bs * n_head, -1, head_size)
-    v = value.reshape(bs * n_head, -1, head_size)
-    attn_weights = torch.bmm(q, k.transpose(1, 2))
-    attn_weights = attn_weights.view(bs, n_head, seq_len, -1) + attention_mask
-    attn_weights = attn_weights.view(bs * n_head, seq_len, -1)
-    attn_weights = torch.nn.functional.softmax(attn_weights, dim=-1)
-    if query.dtype == torch.half:
-        attn_weights = attn_weights.to(torch.half)
-    attn_output = torch.bmm(attn_weights, v)
-    attn_output = attn_output.view(bs, n_head, seq_len, head_size)
-    return attn_output
-
-
-def _sfdp_replacement_24(query, key, value, attention_mask):
-    counters["inductor"]["fuse_attention"] += 1
-    return _scaled_dot_product_attention(
-        query,
-        key,
-        value,
-        attn_mask=attention_mask.to(dtype=query.dtype),
-        is_causal=False,
-        scale=1,
-    )
-
-
 def _sfdp_pattern_21(query, key, value, attn_mask):
     # for T5 with inplace add
     query = query.permute([0, 2, 1, 3])
@@ -720,6 +684,42 @@ def _sfdp_replacement_23(query, key, value):
         ),
         key,
         value,
+    )
+
+
+def _sfdp_pattern_24(query, key, value, attention_mask):
+    """
+    this pattern is for MBartForCausalLM/PLBartForCausalLM.
+    attn_mask has a different dtype with QKV.
+    there is no scale in sdpa.
+    """
+    bs = query.size(0)
+    n_head = query.size(1)
+    seq_len = query.size(2)
+    head_size = query.size(3)
+    q = query.view(bs * n_head, -1, head_size)
+    k = key.reshape(bs * n_head, -1, head_size)
+    v = value.reshape(bs * n_head, -1, head_size)
+    attn_weights = torch.bmm(q, k.transpose(1, 2))
+    attn_weights = attn_weights.view(bs, n_head, seq_len, -1) + attention_mask
+    attn_weights = attn_weights.view(bs * n_head, seq_len, -1)
+    attn_weights = torch.nn.functional.softmax(attn_weights, dim=-1)
+    if query.dtype == torch.half:
+        attn_weights = attn_weights.to(torch.half)
+    attn_output = torch.bmm(attn_weights, v)
+    attn_output = attn_output.view(bs, n_head, seq_len, head_size)
+    return attn_output
+
+
+def _sfdp_replacement_24(query, key, value, attention_mask):
+    counters["inductor"]["fuse_attention"] += 1
+    return _scaled_dot_product_attention(
+        query,
+        key,
+        value,
+        attn_mask=attention_mask.to(dtype=query.dtype),
+        is_causal=False,
+        scale=1,
     )
 
 
@@ -1017,26 +1017,49 @@ def _get_sfdp_patterns():
                 d,
                 _sfdp_extra_check(aten.div.Tensor),
             ),
+            # disable cuda because the last dim stride of attn_mask in the model is not 1
+            # and cuda will fallback to _scaled_dot_product_attention_math
             (
                 _sfdp_pattern_21,
                 _sfdp_replacement_21,
                 [g(), g(), g(), m_float()],
                 {},
-                _sfdp_params_check,
+                _sfdp_extra_check(disable_cuda=True),
+            ),
+            (
+                _sfdp_pattern_21,
+                _sfdp_replacement_21,
+                [g_bs1(), g_bs1(), g_bs1(), m_bs1_float()],
+                {},
+                _sfdp_extra_check(disable_cuda=True),
             ),
             (
                 _sfdp_pattern_22,
                 _sfdp_replacement_22,
                 [g(), g(), g(), m_float()],
                 {},
-                _sfdp_params_check,
+                _sfdp_extra_check(disable_cuda=True),
+            ),
+            (
+                _sfdp_pattern_22,
+                _sfdp_replacement_22,
+                [g_bs1(), g_bs1(), g_bs1(), m_bs1_float()],
+                {},
+                _sfdp_extra_check(disable_cuda=True),
             ),
             (
                 _sfdp_pattern_23,
                 _sfdp_replacement_23,
                 [g(), g(), g()],
                 {},
-                _sfdp_params_check,
+                _sfdp_extra_check(disable_cuda=True),
+            ),
+            (
+                _sfdp_pattern_23,
+                _sfdp_replacement_23,
+                [g_bs1(), g_bs1(), g_bs1()],
+                {},
+                _sfdp_extra_check(disable_cuda=True),
             ),
             (
                 _sfdp_pattern_24,
