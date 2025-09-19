1810b54a794cb06f028d44f3676eba649cf043e8
Jeff Daily <jeff.daily@amd.com>
2025-09-19 03:11:57 +0000

    update logic

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -414,14 +414,17 @@ def requires_multicast_support():
 
 
 def evaluate_platform_supports_symm_mem():
-    if TEST_WITH_ROCM:
-        arch_list = ["gfx942", "gfx950"]
-        for arch in arch_list:
-            if arch in torch.cuda.get_device_properties(0).gcnArchName:
-                return True
-        return False
+    if TEST_CUDA:
+        if TEST_WITH_ROCM:
+            arch_list = ["gfx942", "gfx950"]
+            for arch in arch_list:
+                if arch in torch.cuda.get_device_properties(0).gcnArchName:
+                    return True
+            return False
+        else:
+            return True
     else:
-        return True
+        return False
 
 
 PLATFORM_SUPPORTS_SYMM_MEM: bool = LazyVal(
