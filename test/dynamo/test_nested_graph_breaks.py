4ce31b682d20bd9305b064fc97520d0b0bcf5781
William Wen <williamwen@meta.com>
2025-09-19 04:07:03 -0700

    [dynamo, nested graph breaks] enable strong nested graph break wrapped tests
    ghstack-source-id: c71865aa1c641003d7e77f7087965cfd2a8bfef8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160611

--- test/dynamo/test_nested_graph_breaks.py
+++ test/dynamo/test_nested_graph_breaks.py
@@ -1,4 +1,5 @@
 # Owner(s): ["module: dynamo"]
+import unittest
 
 import torch
 import torch._dynamo.test_case
@@ -7,6 +8,18 @@ import torch._dynamo.testing
 
 # for use in test_side_effects_globals
 global1, global2, global3, global4 = (torch.zeros(3),) * 4
+
+
+class CustomizedCtxManager:
+    def __init__(self, x):
+        self.x = x
+        torch._dynamo.graph_break()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
 
 
 class NestedGraphBreakTests(torch._dynamo.test_case.TestCase):
@@ -548,6 +561,44 @@ class NestedGraphBreakTests(torch._dynamo.test_case.TestCase):
         # all ops except + 4
         self.assertEqual(cnts.op_count, 4)
         self.assertEqual(torch._dynamo.utils.counters["frames"]["total"], 3)
+
+    @unittest.expectedFailure
+    def test_nested_decorated_function(self):
+        # decorator must call ContextWrappingVariable.cleanup_assert to trigger this test
+        def f(x):
+            @torch.autocast("cpu")
+            def inner(y):
+                y = y + 1
+                torch._dynamo.graph_break()
+                return y + 1
+
+            return inner(x)
+
+        cnts = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(backend=cnts)(f)
+        x = torch.zeros(3)
+        res = f(x)
+        ref = opt_fn(x)
+        print(ref, res)
+        self.assertEqual(ref, res)
+        self.assertEqual(cnts.frame_count, 2)
+        self.assertEqual(cnts.op_count, 6)
+
+    @unittest.expectedFailure
+    def test_nested_graph_break_in_custom_ctx_manager_init(self):
+        def f(x):
+            with CustomizedCtxManager(x):
+                return x + 1
+
+        cnts = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(backend=cnts)(f)
+        x = torch.zeros(3)
+        res = f(x)
+        ref = opt_fn(x)
+        print(ref, res)
+        self.assertEqual(ref, res)
+        self.assertEqual(cnts.frame_count, 2)
+        self.assertEqual(cnts.op_count, 2)
 
 
 if __name__ == "__main__":
