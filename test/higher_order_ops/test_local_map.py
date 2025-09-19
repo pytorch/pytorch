274bbd2b851d51c6c403937a66c3fe8f19353c8b
Simon Fan <xmfan@meta.com>
2025-09-19 14:15:16 -0700

    Update on "[hop] support local_map + SAC"
    [ghstack-poisoned]

--- test/higher_order_ops/test_local_map.py
+++ test/higher_order_ops/test_local_map.py
@@ -2,6 +2,7 @@
 # flake8: noqa: B950
 
 
+import contextlib
 import functools
 import unittest
 
@@ -13,6 +14,7 @@ import torch._inductor.decomposition
 import torch.nn.functional as F
 from torch import nn
 from torch._dynamo.variables.higher_order_ops import LocalMapWrappedHigherOrderVariable
+from torch.nn.attention import sdpa_kernel, SDPBackend
 from torch.utils.checkpoint import create_selective_checkpoint_contexts
 
 
@@ -44,14 +46,11 @@ def context_parallel_attention(query, key, value):
     return out
 
 
-def save_attention(ctx, op, *args, **kwargs):
-    if (
-        op == torch.ops.aten._scaled_dot_product_flash_attention.default
-        or op == torch.ops.aten._scaled_dot_product_efficient_attention.default
-    ):
-        # NOTE: we can't save nondeterministic_seeded ops, the run with rng wrapper is not traceable yet
-        return torch.utils.checkpoint.CheckpointPolicy.PREFER_SAVE
-    return torch.utils.checkpoint.CheckpointPolicy.PREFER_RECOMPUTE
+# NOTE: we use this function directly in the node checks
+def save_scalar_muls(ctx, op, *args, **kwargs):
+    if op == torch.ops.aten.mul.Scalar:
+        return torch.utils.checkpoint.CheckpointPolicy.MUST_SAVE
+    return torch.utils.checkpoint.CheckpointPolicy.MUST_RECOMPUTE
 
 
 def create_model(attention_fn, nheads, dim1, dim2, sac_policy=None):
@@ -143,6 +142,13 @@ def get_local_mapped_functions():
 
 
 class TestLocalMap(TestCase):
+    def setUp(self):
+        self.exit_stack = contextlib.ExitStack()
+        self.exit_stack.enter_context(sdpa_kernel(backends=[SDPBackend.MATH]))
+
+    def tearDown(self):
+        self.exit_stack.close()
+
     @requires_cuda_and_triton
     @unittest.skipIf(
         not torch.distributed.is_available(), "Torch distributed not available."
@@ -244,12 +250,12 @@ class GraphModule(torch.nn.Module):
         nheads = 16
         seq_len = 16
 
-        from torch._dynamo.testing import EagerAndRecordGraphs, normalize_gm
+        from torch._dynamo.testing import AotEagerAndRecordGraphs, normalize_gm
 
-        backend = EagerAndRecordGraphs()
+        backend = AotEagerAndRecordGraphs()
 
         model = create_model(
-            cp_decorated, nheads, dim1, dim2, sac_policy=save_attention
+            cp_decorated, nheads, dim1, dim2, sac_policy=save_scalar_muls
         ).cuda()
         inputs = (torch.randn(bs, seq_len, dim1, requires_grad=True).cuda(),)
         with LocalMapWrappedHigherOrderVariable.enable():
@@ -257,7 +263,7 @@ class GraphModule(torch.nn.Module):
         out.sum().backward()
 
         model = create_model(
-            cp_function, nheads, dim1, dim2, sac_policy=save_attention
+            cp_function, nheads, dim1, dim2, sac_policy=save_scalar_muls
         ).cuda()
         inputs = (torch.randn(bs, seq_len, dim1, requires_grad=True).cuda(),)
         with LocalMapWrappedHigherOrderVariable.enable():
@@ -271,6 +277,14 @@ class GraphModule(torch.nn.Module):
                 normalize_gm(backend.graphs[1].print_readable(print_output=False)),
             )
             self.assertEqual(
+                normalize_gm(backend.fw_graphs[0].print_readable(print_output=False)),
+                normalize_gm(backend.fw_graphs[1].print_readable(print_output=False)),
+            )
+            self.assertEqual(
+                normalize_gm(backend.bw_graphs[0].print_readable(print_output=False)),
+                normalize_gm(backend.bw_graphs[1].print_readable(print_output=False)),
+            )
+            self.assertEqual(
                 len(
                     backend.graphs[0].graph.find_nodes(
                         op="call_function",
@@ -279,67 +293,26 @@ class GraphModule(torch.nn.Module):
                 ),
                 1,
             )
-            self.assertExpectedInline(
-                normalize_gm(backend.graphs[0].print_readable(print_output=False)),
-                """\
-class GraphModule(torch.nn.Module):
-    def forward(self, L_x_: "f32[8, 16, 96]", L_self_modules_wq_parameters_weight_: "f32[96, 96]", L_self_modules_wk_parameters_weight_: "f32[96, 96]", L_self_modules_wv_parameters_weight_: "f32[96, 96]", L_self_modules_wo_parameters_weight_: "f32[96, 96]", L_self_modules_w1_parameters_weight_: "f32[384, 96]", L_self_modules_w2_parameters_weight_: "f32[96, 384]"):
-        l_x_ = L_x_
-        l_self_modules_wq_parameters_weight_ = L_self_modules_wq_parameters_weight_
-        l_self_modules_wk_parameters_weight_ = L_self_modules_wk_parameters_weight_
-        l_self_modules_wv_parameters_weight_ = L_self_modules_wv_parameters_weight_
-        l_self_modules_wo_parameters_weight_ = L_self_modules_wo_parameters_weight_
-        l_self_modules_w1_parameters_weight_ = L_self_modules_w1_parameters_weight_
-        l_self_modules_w2_parameters_weight_ = L_self_modules_w2_parameters_weight_
-
-        wrap_body_0 = self.wrap_body_0
-        tag_activation_checkpoint = torch.ops.higher_order.tag_activation_checkpoint(wrap_body_0, l_x_, l_self_modules_wq_parameters_weight_, l_self_modules_wk_parameters_weight_, l_self_modules_wv_parameters_weight_, l_self_modules_wo_parameters_weight_, l_self_modules_w1_parameters_weight_, l_self_modules_w2_parameters_weight_, use_reentrant = False);  wrap_body_0 = l_x_ = l_self_modules_wq_parameters_weight_ = l_self_modules_wk_parameters_weight_ = l_self_modules_wv_parameters_weight_ = l_self_modules_wo_parameters_weight_ = l_self_modules_w1_parameters_weight_ = l_self_modules_w2_parameters_weight_ = None
-        getitem: "f32[8, 16, 96]" = tag_activation_checkpoint[0];  tag_activation_checkpoint = None
-        return (getitem,)
-
-    class wrap_body_0(torch.nn.Module):
-        def forward(self, l_x_: "f32[8, 16, 96]", l_self_modules_wq_parameters_weight_: "f32[96, 96]", l_self_modules_wk_parameters_weight_: "f32[96, 96]", l_self_modules_wv_parameters_weight_: "f32[96, 96]", l_self_modules_wo_parameters_weight_: "f32[96, 96]", l_self_modules_w1_parameters_weight_: "f32[384, 96]", l_self_modules_w2_parameters_weight_: "f32[96, 384]"):
-            q: "f32[8, 16, 96]" = torch._C._nn.linear(l_x_, l_self_modules_wq_parameters_weight_, None);  l_self_modules_wq_parameters_weight_ = None
-
-            k: "f32[8, 16, 96]" = torch._C._nn.linear(l_x_, l_self_modules_wk_parameters_weight_, None);  l_self_modules_wk_parameters_weight_ = None
-
-            v: "f32[8, 16, 96]" = torch._C._nn.linear(l_x_, l_self_modules_wv_parameters_weight_, None);  l_self_modules_wv_parameters_weight_ = None
-
-            unflatten: "f32[8, 16, 16, 6]" = q.unflatten(-1, (16, -1));  q = None
-            q_1: "f32[8, 16, 16, 6]" = unflatten.permute(0, 2, 1, 3);  unflatten = None
-
-            unflatten_1: "f32[8, 16, 16, 6]" = k.unflatten(-1, (16, -1));  k = None
-            k_1: "f32[8, 16, 16, 6]" = unflatten_1.permute(0, 2, 1, 3);  unflatten_1 = None
-
-            unflatten_2: "f32[8, 16, 16, 6]" = v.unflatten(-1, (16, -1));  v = None
-            v_1: "f32[8, 16, 16, 6]" = unflatten_2.permute(0, 2, 1, 3);  unflatten_2 = None
-
-            subgraph_0 = self.subgraph_0
-            local_map_hop = torch.ops.higher_order.local_map_hop(subgraph_0, q_1, k_1, v_1);  subgraph_0 = q_1 = k_1 = v_1 = None
-            o: "f32[8, 16, 16, 6]" = local_map_hop[0];  local_map_hop = None
-
-            permute_3: "f32[8, 16, 16, 6]" = o.permute(0, 2, 1, 3);  o = None
-            o_1: "f32[8, 16, 96]" = permute_3.flatten(-2);  permute_3 = None
-
-            o_2: "f32[8, 16, 96]" = torch._C._nn.linear(o_1, l_self_modules_wo_parameters_weight_, None);  o_1 = l_self_modules_wo_parameters_weight_ = None
-
-            o0: "f32[8, 16, 96]" = o_2 + l_x_;  o_2 = l_x_ = None
-
-            o_3: "f32[8, 16, 384]" = torch._C._nn.linear(o0, l_self_modules_w1_parameters_weight_, None);  l_self_modules_w1_parameters_weight_ = None
-
-            o_4: "f32[8, 16, 384]" = torch.nn.functional.relu(o_3);  o_3 = None
-
-            o_5: "f32[8, 16, 96]" = torch._C._nn.linear(o_4, l_self_modules_w2_parameters_weight_, None);  o_4 = l_self_modules_w2_parameters_weight_ = None
-
-            o_6: "f32[8, 16, 96]" = o0 + o_5;  o0 = o_5 = None
-            return (o_6,)
-
-        class subgraph_0(torch.nn.Module):
-            def forward(self, q_1: "f32[8, 16, 16, 6]", k_1: "f32[8, 16, 16, 6]", v_1: "f32[8, 16, 16, 6]"):
-                out: "f32[8, 16, 16, 6]" = torch._C._nn.scaled_dot_product_attention(query = q_1, key = k_1, value = v_1, is_causal = False);  q_1 = k_1 = v_1 = None
-                return (out,)
-""",
-            )
+            # TODO: add joint to the testing compile backend
+            fw_outs = {
+                n.name
+                for n in backend.fw_graphs[0].graph.find_nodes(op="output")[0].args[0]
+            }
+            bw_ins = {
+                n.name for n in backend.bw_graphs[0].graph.find_nodes(op="placeholder")
+            }
+            for node in backend.fw_graphs[0].graph.nodes:
+                if "recompute" in node.meta:
+                    expected = save_scalar_muls(None, node.target, None, None)
+                    actual = node.meta["recompute"]
+                    self.assertEqual(expected, actual)
+                    if actual == torch.utils.checkpoint.CheckpointPolicy.MUST_SAVE:
+                        self.assertTrue(node.name in fw_outs and node.name in bw_ins)
+                    elif (
+                        actual == torch.utils.checkpoint.CheckpointPolicy.MUST_RECOMPUTE
+                    ):
+                        # can still be in fw_outs for post-graph bytecode
+                        self.assertFalse(node.name in bw_ins)
 
 
 if __name__ == "__main__":
