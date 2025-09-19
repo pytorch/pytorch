1cd855ababe181741381555ca6019402f37f0f2a
William Wen <williamwen@meta.com>
2025-09-19 04:07:03 -0700

    [dynamo, nested graph breaks] enable basic nested graph break wrapped tests
    ghstack-source-id: 1d25d1f2cae9be6f54843eb18a8b749a163bc78c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/163335

--- torch/_dynamo/testing.py
+++ torch/_dynamo/testing.py
@@ -33,7 +33,7 @@ from torch import fx
 from torch._dynamo.backends.debugging import aot_eager
 from torch._dynamo.output_graph import OutputGraph
 
-from . import config, eval_frame, optimize_assert, reset
+from . import config, eval_frame, optimize, reset
 from .bytecode_transformation import (
     create_instruction,
     debug_checks,
@@ -379,7 +379,7 @@ def standard_test(
     correct1 = fn(*args1)
     correct2 = fn(*args2)
     reset()
-    opt_fn = optimize_assert(actual)(fn)
+    opt_fn = optimize(actual, error_on_graph_break=True)(fn)
     val1a = opt_fn(*args1)
     val2a = opt_fn(*args2)
     val1b = opt_fn(*args1)
