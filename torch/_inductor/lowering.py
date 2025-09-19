301d09ca72abe76e00bc8ffd7e9905674a4ff4de
Simon Fan <xmfan@meta.com>
2025-09-19 13:07:56 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -1205,7 +1205,14 @@ def as_strided(x, size, stride, storage_offset=None):
     new_device = None
     new_dtype = None
     if isinstance(x, TensorBox) and isinstance(x.data, ir.BaseView):
-        # as_strided ignores views
+        # Note: Merging views
+        # When we use as_strided, we can rewrite the size/stride/offset
+        # of the incoming buffer x. If x is a view, we would overwrite
+        # its metadata. Except for device type and dtype, which we need
+        # to propagate.
+
+        # Technically device is not needed because it is handled as a copy
+        # instead of a view today.
         new_device = x.get_device()
         new_dtype = x.dtype
         x = x.data.unwrap_view()
