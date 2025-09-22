91b8fad8b9fb905720d83014460ad514869fc16b
Scott Wolchok <swolchok@meta.com>
2025-09-22 15:51:12 -0700

    Update base for Update on "DTensor: C++ compute_global_tensor_info"
    compute_global_tensor_info is on the hot path for DTensor.{from,to}_local. More incremental progress toward C++.
    
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k pragupta ezyang msaroufim dcci
    
    [ghstack-poisoned]

--- test/distributed/tensor/test_placement_types.py
+++ test/distributed/tensor/test_placement_types.py
@@ -4,6 +4,7 @@ import itertools
 import sys
 import unittest
 
+from torch._dynamo.variables.distributed import PlacementClassVariable
 from torch.distributed.tensor.placement_types import (
     _StridedShard,
     Partial,
@@ -70,6 +71,9 @@ class PlacementTypesTestCase(TestCase):
             _StridedShard(3, 4)
         _StridedShard(3, split_factor=4)
 
+    def test_strided_shard_isinstance_shard(self):
+        assert isinstance(_StridedShard(dim=3, split_factor=7), Shard)
+
     def test_deepcopy(self):
         for placement in (
             Shard(3),
@@ -78,6 +82,12 @@ class PlacementTypesTestCase(TestCase):
             Replicate(),
         ):
             copy.deepcopy(placement)
+
+    def test_dynamo_can_identify_placement_classes(self):
+        for cls in (Replicate, Shard, _StridedShard, Partial):
+            self.assertTrue(
+                PlacementClassVariable.is_placement_type(cls), msg=f"failed on {cls}"
+            )
 
 
 if __name__ == "__main__":
