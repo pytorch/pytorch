a4b42967ad5680c64c04004729fa46c70cdc5298
Bob Ren <bobren@meta.com>
2025-09-23 10:08:45 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/ao/quantization/pt2e/duplicate_dq_pass.py
+++ torch/ao/quantization/pt2e/duplicate_dq_pass.py
@@ -1,3 +1,4 @@
+# mypy: allow-untyped-defs
 import logging
 import operator
 
@@ -30,7 +31,7 @@ _DEQUANTIZE_OPS = [
 
 def _maybe_duplicate_dq(
     gm: torch.fx.GraphModule, dq_node: torch.fx.Node, user: torch.fx.Node
-) -> None:
+):
     annotation = user.meta.get("quantization_annotation", None)
     if not _is_valid_annotation(annotation):  # type: ignore[arg-type]
         return
