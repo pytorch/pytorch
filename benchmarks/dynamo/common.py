400d1e97771605c0011f257168c31883e55aeebf
Huy Do <huydhn@gmail.com>
2025-09-23 04:19:32 -0700

    Skip LLM training and increase the number of H100 shard for HF
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -3355,7 +3355,7 @@ def parse_args(args=None):
     parser.add_argument(
         "--timeout",
         type=int,
-        default=2000,
+        default=3600,
         help="timeout (second) for benchmarking.",
     )
 
