79fe4d9047c7c0d40093bc163c45032ab6cce949
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2025-09-23 11:03:36 +0100

    Condition on if config.use_fast_math:

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1683,7 +1683,8 @@ class TritonOverrides(OpOverrides):
     @maybe_upcast_float32()
     def tanh(x):
         if (get_triton_version() > (3, 4) and torch.version.hip):
-            return f"libdevice.fast_tanhf({x})"
+            if config.use_fast_math:
+                return f"libdevice.fast_tanhf({x})"
         else:
             return f"libdevice.tanh({x})"
 
