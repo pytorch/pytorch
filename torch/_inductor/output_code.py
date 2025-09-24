b87582b6ce7f332e362f0c416d48c2e1d0e1e38e
Arsh Zahed <arshzahed@meta.com>
2025-09-23 23:53:51 -0700

    Check if profiling before using record_function

--- torch/_inductor/output_code.py
+++ torch/_inductor/output_code.py
@@ -607,9 +607,12 @@ class CompiledFxGraph(OutputCode):
                 }
             )
         try:
-            with record_function(
-                f"## Call CompiledFxGraph {self._fx_graph_cache_key} ##"
-            ):
+            if torch.autograd.profiler._is_profiler_enabled:
+                with record_function(
+                    f"## Call CompiledFxGraph {self._fx_graph_cache_key} ##"
+                ):
+                    return self.current_callable(inputs)
+            else:
                 return self.current_callable(inputs)
         finally:
             get_runtime_metrics_context().finish()
