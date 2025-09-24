7049d892cc088e9f337fc18576145f33ad4060fe
Bob Ren <bobren@meta.com>
2025-09-24 15:18:43 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -26,7 +26,6 @@
 #include <ATen/native/Normalization.h>
 #include <c10/core/Device.h>
 #include <c10/core/DispatchKeySet.h>
-#include <c10/core/impl/DeviceGuardImplInterface.h>
 #include <c10/util/AbortHandler.h>
 #include <c10/util/Backtrace.h>
 #include <c10/util/Logging.h>
@@ -1551,15 +1550,6 @@ static PyObject* THPModule_are_vmap_fallback_warnings_enabled(
   END_HANDLE_TH_ERRORS
 }
 
-static PyObject* THCPModule_ensureCUDADeviceGuardSet(
-    PyObject* self,
-    PyObject* noargs) {
-  HANDLE_TH_ERRORS
-  c10::impl::ensureCUDADeviceGuardSet();
-  Py_RETURN_NONE;
-  END_HANDLE_TH_ERRORS
-}
-
 static std::initializer_list<PyMethodDef> TorchMethods = {
     {"_initExtension", THPModule_initExtension, METH_O, nullptr},
     {"_autograd_init", THPAutograd_initExtension, METH_NOARGS, nullptr},
@@ -1855,13 +1845,7 @@ static std::initializer_list<PyMethodDef> TorchMethods = {
      (PyCFunction)(void (*)())THPModule_has_torch_function_variadic,
      METH_FASTCALL,
      nullptr},
-    {"_ensureCUDADeviceGuardSet",
-     THCPModule_ensureCUDADeviceGuardSet,
-     METH_NOARGS,
-     nullptr},
-    {nullptr, nullptr, 0, nullptr}
-
-};
+    {nullptr, nullptr, 0, nullptr}};
 
 #ifdef USE_CUDA
 // NOLINTBEGIN(misc-use-internal-linkage)
