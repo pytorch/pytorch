366961df78d7f42a50d619f294c036913713b656
Anshul Sinha <anshulsi@meta.com>
2025-09-25 12:23:43 -0700

    [FSDP][Replicate] tests replicate parity with activation checkpointing (#162830)
    **Summary:**  In order to ensure that replicate acts as intended (a specialized version of hsdp) we need to make sure that it can pass the same tests that fully_shard can for training. This tests that replicate function works correctly when combined with activation checkpointing
    
    **Test Case**
    1. pytest test/distributed/_composable/test_replicate_training.py -k test_train_parity_with_activation_checkpointing
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162830
    Approved by: https://github.com/mori360

--- test/distributed/_composable/test_replicate_training.py
+++ test/distributed/_composable/test_replicate_training.py
@@ -11,7 +11,12 @@ from typing import Union
 import torch
 import torch.distributed as dist
 import torch.nn as nn
+from torch.distributed._composable import checkpoint
 from torch.distributed._composable.replicate_with_fsdp import replicate
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    _CHECKPOINT_PREFIX,
+    apply_activation_checkpointing,
+)
 from torch.distributed.fsdp import CPUOffloadPolicy, FSDPModule, OffloadPolicy
 from torch.distributed.tensor import DTensor, init_device_mesh
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
@@ -650,6 +655,135 @@ class TestReplicate1DTrainingCore(FSDPTest):
             torch.get_device_module(device_type)._sleep(int(25 * get_cycles_per_ms()))
         for ref_loss, loss in zip(ref_losses, losses):
             self.assertEqual(ref_loss, loss)
+
+
+class TestReplicateTrainingCompose(FSDPTest):
+    @property
+    def world_size(self) -> int:
+        # Since these tests run with a larger transformer model, they may see
+        # some numeric drift with >2 GPUs
+        return min(torch.get_device_module(device_type).device_count(), 2)
+
+    @skip_if_lt_x_gpu(2)
+    @compiled_fsdp_test(compile_compute_on_module=Transformer)
+    def test_train_parity_with_activation_checkpointing(self):
+        """
+        Tests train parity against DDP when composing with activation
+        checkpointing.
+        """
+        self.run_subtests(
+            {
+                "reshard_after_forward": [True, False],
+                "checkpoint_impl": ["composable", "utils", "wrapper"],
+                "module_grouping": ["block", "mem_eff", "mem_eff_weight_tied"],
+                "test_device_type": [device_type.type],
+            },
+            self._test_train_parity_with_activation_checkpointing,
+        )
+
+    def _test_train_parity_with_activation_checkpointing(
+        self,
+        reshard_after_forward: Union[bool, int],
+        checkpoint_impl: str,
+        module_grouping: str,
+        test_device_type: str,
+    ):
+        assert checkpoint_impl in ("composable", "utils", "wrapper")
+        testing_compile = replicate != torch.distributed._composable.replicate_with_fsdp
+        if testing_compile and checkpoint_impl == "composable":
+            return
+        torch.manual_seed(42)
+        vocab_size = 1024
+        with torch.device(device_type):
+            model_args = ModelArgs(
+                n_layers=3,
+                n_heads=4,
+                vocab_size=vocab_size,
+                max_seq_len=64,
+                dropout_p=0,
+                checkpoint_activations=(checkpoint_impl == "utils"),
+                # For the mem-efficient module grouping, we separate the
+                # embeddings from the output projection, which does not support
+                # weight tying
+                weight_tying=module_grouping != "mem_eff",
+            )
+            model = Transformer(model_args)
+        ref_model = copy.deepcopy(model).to(device_type)
+        ref_optim = torch.optim.Adam(ref_model.parameters(), lr=1e-2)
+
+        # Apply activation checkpointing
+        prefixes_to_ignore = ()
+        if checkpoint_impl == "wrapper":
+            prefixes_to_ignore = (_CHECKPOINT_PREFIX,)
+            apply_activation_checkpointing(
+                model, check_fn=lambda m: isinstance(m, TransformerBlock)
+            )
+        elif checkpoint_impl == "composable":
+            for module in model.modules():
+                if isinstance(module, TransformerBlock):
+                    checkpoint(module)
+
+        # Apply Replicate
+        device_mesh = init_device_mesh(
+            test_device_type,
+            (self.world_size, 1),
+            mesh_dim_names=("replicate", "shard"),
+        )
+        fsdp_kwargs = {
+            "reshard_after_forward": reshard_after_forward,
+            "device_mesh": device_mesh,
+        }
+        if module_grouping == "mem_eff":
+            assert model_args.n_layers == 3
+            replicate(model.layers[0], **fsdp_kwargs)
+            replicate([model.layers[1], model.layers[2]], **fsdp_kwargs)
+            replicate([model.tok_embeddings, model.pos_embeddings], **fsdp_kwargs)
+            # Embedding weights are not needed for embedding backward
+            model.tok_embeddings.set_unshard_in_backward(False)
+            replicate([model.norm, model.output], **fsdp_kwargs)
+        elif module_grouping == "mem_eff_weight_tied":
+            replicate([model.tok_embeddings, model.output], **fsdp_kwargs)
+            for layer in model.layers:
+                replicate(layer, **fsdp_kwargs)
+        elif module_grouping == "block":
+            for layer in model.layers:
+                replicate(layer, **fsdp_kwargs)
+        else:
+            raise NotImplementedError(f"Unknown module grouping: {module_grouping}")
+        replicate(model, **fsdp_kwargs)
+        optim = torch.optim.Adam(model.parameters(), lr=1e-2)
+
+        torch.manual_seed(42 + self.rank)
+        # Reuse the same input across iterations to avoid loss explosion from
+        # trying to learn from random inputs
+        inp = torch.randint(0, vocab_size, (3, 64), device=device_type.type)
+        check_sharded_parity(
+            self, ref_model, model, prefixes_to_ignore=prefixes_to_ignore
+        )
+        for iter_idx in range(10):
+            losses: list[torch.Tensor] = []
+            for _model in (ref_model, model):
+                torch.manual_seed(iter_idx + 1)  # for dropout determinism
+                losses.append(_model(inp).sum())
+                losses[-1].backward()
+
+            for param in ref_model.parameters():
+                if param.grad is not None:
+                    dist.all_reduce(param.grad)
+                    param.grad.div_(self.world_size)
+
+            if not testing_compile:
+                check_sharded_parity(
+                    self, ref_model, model, prefixes_to_ignore=prefixes_to_ignore
+                )
+            self.assertEqual(losses[0], losses[1])
+            for _optim in (ref_optim, optim):
+                _optim.step()
+                _optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
+            if not testing_compile:
+                check_sharded_parity(
+                    self, ref_model, model, prefixes_to_ignore=prefixes_to_ignore
+                )
 
 
 if __name__ == "__main__":
