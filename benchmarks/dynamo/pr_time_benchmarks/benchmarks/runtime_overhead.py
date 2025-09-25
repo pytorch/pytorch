9ac02be9e71c3a370f74ea4e1a9073daee5e2b40
Arsh Zahed <arshzahed@meta.com>
2025-09-25 09:05:12 -0700

    Add dynamic, some cleanup

--- benchmarks/dynamo/pr_time_benchmarks/benchmarks/runtime_overhead.py
+++ benchmarks/dynamo/pr_time_benchmarks/benchmarks/runtime_overhead.py
@@ -3,12 +3,11 @@ import sys
 from benchmark_base import BenchmarkBase
 
 import torch
-from torch._inductor.utils import fresh_inductor_cache
 from torch.autograd.grad_mode import inference_mode
 
 
 class Benchmark(BenchmarkBase):
-    def __init__(self, requires_grad, inference_mode):
+    def __init__(self, requires_grad, inference_mode, dynamic):
         self._requires_grad = requires_grad
         self._inference_mode = inference_mode
 
@@ -16,7 +15,7 @@ class Benchmark(BenchmarkBase):
             category="runtime_overhead",
             backend="inductor",
             device="cuda",
-            dynamic=False,
+            dynamic=dynamic,
         )
 
     def name(self):
@@ -25,6 +24,8 @@ class Benchmark(BenchmarkBase):
             prefix += "_requires_grad"
         if self._inference_mode:
             prefix += "_inference_mode"
+        if self.is_dynamic():
+            prefix += "_dynamic"
         return prefix
 
     def description(self):
@@ -32,8 +33,8 @@ class Benchmark(BenchmarkBase):
 
     def _prepare_once(self):
         torch._dynamo.reset()
-        self.a = torch.ones(1, device=self.device(), requires_grad=self._requires_grad)
-        
+        self.a = torch.ones(2, device=self.device(), requires_grad=self._requires_grad)
+
         @torch.compile(
             backend=self.backend(),
             fullgraph=True,
@@ -61,9 +62,12 @@ class Benchmark(BenchmarkBase):
 def main():
     result_path = sys.argv[1]
     all = [
-        Benchmark(False, False),
-        Benchmark(False, True),
-        Benchmark(True, False),
+        Benchmark(False, False, False),
+        Benchmark(False, True, False),
+        Benchmark(True, False, False),
+        Benchmark(False, False, True),
+        Benchmark(False, True, True),
+        Benchmark(True, False, True),
     ]
 
     for benchmark in all:
