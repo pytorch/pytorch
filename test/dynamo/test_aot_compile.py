66d37265f4add1fe5341a32ee37e88d53f43a9f1
James Wu <jjwu@meta.com>
2025-09-26 09:39:19 -0700

    WIP
    [ghstack-poisoned]

--- test/dynamo/test_aot_compile.py
+++ test/dynamo/test_aot_compile.py
@@ -364,6 +364,85 @@ from user code:
         assert hasattr(backend_result.compiled_fn, "serialize")
         self.assertIsNotNone(backend_result.compiled_fn.serialize)
 
+    def test_aot_compile_module_aot_eager(self):
+        mod = SimpleLinearModule()
+
+        model = torch.compile(
+            mod,
+            fullgraph=True,
+            backend="aot_eager",
+            options={
+                "guard_filter_fn": torch.compiler.skip_guard_on_globals_unsafe,
+            },
+        )
+
+        @contextmanager
+        def train_mode(model):
+            """
+            Context manager that sets the model to training mode before entering the context.
+            """
+            model.train()
+            yield
+
+        @contextmanager
+        def eval_mode(model):
+            """
+            Context manager that sets the model to evaluation mode before entering the context.
+            """
+            model.eval()
+            yield
+
+        inputs = [
+            ModelInput(
+                args=(torch.randn(3, 3),), kwargs={}, contexts=[train_mode(model)]
+            ),
+            ModelInput(
+                args=(torch.randn(3, 3),),
+                kwargs={},
+                contexts=[torch.no_grad(), eval_mode(model)],
+            ),
+        ]
+        assert isinstance(model, torch._dynamo.eval_frame.OptimizedModule)
+        model._aot_compile(
+            inputs,
+        )
+        with torch.compiler.set_stance("fail_on_recompile"):
+            model.eval()
+            inputs = (torch.randn(3, 3),)
+            expected = mod(*inputs)
+            actual = model(*inputs)
+            self.assertEqual(expected, actual)
+
+            # Shouldn't recompile
+            model.train()
+            expected.sum().backward()
+
+        model._save_aot_compiled_module(self.path())
+        torch._dynamo.reset()
+        model = torch.compile(
+            mod,
+            fullgraph=True,
+            backend="aot_eager",
+            options={
+                "guard_filter_fn": torch.compiler.skip_guard_on_globals_unsafe,
+            },
+        )
+        assert isinstance(model, torch._dynamo.eval_frame.OptimizedModule)
+        with open(self.path(), "rb") as f:
+            data = f.read()
+            model._load_aot_compiled_module(data)
+
+        with torch.compiler.set_stance("fail_on_recompile"):
+            model.eval()
+            inputs = (torch.randn(3, 3),)
+            expected = mod(*inputs)
+            actual = model(*inputs)
+            self.assertEqual(expected, actual)
+
+            # Shouldn't recompile
+            model.train()
+            expected.sum().backward()
+
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
