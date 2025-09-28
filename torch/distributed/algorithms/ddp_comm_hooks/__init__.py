931478019505adc55c362c48c82a5f95e728af9c
Xuanteng Huang <xuanteng.huang@outlook.com>
2025-09-28 10:45:40 +0800

    rename wrapper

--- torch/distributed/algorithms/ddp_comm_hooks/__init__.py
+++ torch/distributed/algorithms/ddp_comm_hooks/__init__.py
@@ -8,11 +8,11 @@ from functools import partial
 if sys.version_info >= (3, 13):
     from enum import member
 
-    def partial_wrapper(x):
+    def _enum_member(x):
         return member(x)
 else:
 
-    def partial_wrapper(x):
+    def _enum_member(x):
         return x
 
 
@@ -65,26 +65,26 @@ class DDPCommHookType(Enum):
     ``DDPCommHookType.ALLREDUCE.value(model=model, state=process_group)``.
     """
 
-    ALLREDUCE = partial_wrapper(
+    ALLREDUCE = _enum_member(
         partial(_ddp_comm_hook_wrapper, comm_hook=default.allreduce_hook)
     )
-    FP16_COMPRESS = partial_wrapper(
+    FP16_COMPRESS = _enum_member(
         partial(_ddp_comm_hook_wrapper, comm_hook=default.fp16_compress_hook)
     )
-    BF16_COMPRESS = partial_wrapper(
+    BF16_COMPRESS = _enum_member(
         partial(_ddp_comm_hook_wrapper, comm_hook=default.bf16_compress_hook)
     )
-    QUANTIZE_PER_TENSOR = partial_wrapper(
+    QUANTIZE_PER_TENSOR = _enum_member(
         partial(
             _ddp_comm_hook_wrapper, comm_hook=quantization.quantization_pertensor_hook
         )
     )
-    QUANTIZE_PER_CHANNEL = partial_wrapper(
+    QUANTIZE_PER_CHANNEL = _enum_member(
         partial(
             _ddp_comm_hook_wrapper, comm_hook=quantization.quantization_perchannel_hook
         )
     )
-    POWER_SGD = partial_wrapper(
+    POWER_SGD = _enum_member(
         partial(
             _powerSGD_comm_hook_wrapper,
             comm_hook=powerSGD.powerSGD_hook,
@@ -93,7 +93,7 @@ class DDPCommHookType(Enum):
     )
     # Rank-2 PowerSGD can give a higher accuracy than the default rank-1 version,
     # but it runs slower and consumes more memory.
-    POWER_SGD_RANK2 = partial_wrapper(
+    POWER_SGD_RANK2 = _enum_member(
         partial(
             _powerSGD_comm_hook_wrapper,
             comm_hook=powerSGD.powerSGD_hook,
@@ -101,21 +101,21 @@ class DDPCommHookType(Enum):
         )
     )
     # Batching can lead to a faster training at the cost of accuracy.
-    BATCHED_POWER_SGD = partial_wrapper(
+    BATCHED_POWER_SGD = _enum_member(
         partial(
             _powerSGD_comm_hook_wrapper,
             comm_hook=powerSGD.batched_powerSGD_hook,
             matrix_approximation_rank=1,
         )
     )
-    BATCHED_POWER_SGD_RANK2 = partial_wrapper(
+    BATCHED_POWER_SGD_RANK2 = _enum_member(
         partial(
             _powerSGD_comm_hook_wrapper,
             comm_hook=powerSGD.batched_powerSGD_hook,
             matrix_approximation_rank=2,
         )
     )
-    NOOP = partial_wrapper(
+    NOOP = _enum_member(
         partial(
             _ddp_comm_hook_wrapper,
             comm_hook=debugging.noop_hook,
