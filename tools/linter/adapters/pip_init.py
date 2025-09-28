3deb54458ba203f107e03a7e15851e1f77b62234
can-gaa-hou <jiahaochen535@gmail.com>
2025-09-28 16:33:47 +0800

    [Fix] Adding missing f prefixes to formatted strings [3/N]

--- tools/linter/adapters/pip_init.py
+++ tools/linter/adapters/pip_init.py
@@ -89,7 +89,7 @@ def main() -> None:
         package_name, _, version = package.partition("=")
         if version == "":
             raise RuntimeError(
-                "Package {package_name} did not have a version specified. "
+                f"Package {package_name} did not have a version specified. "
                 "Please specify a version to produce a consistent linting experience."
             )
 
