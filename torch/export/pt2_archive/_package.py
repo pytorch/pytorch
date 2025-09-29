46f5849491c1e02f6a70a1063469e5a08ff9ec2c
Dev Sashidhar <dsashidh@redhat.com>
2025-09-29 19:48:18 +0000

    torch.export.load: TypeError for wrong types; accept any path, warn on non-.pt2

--- torch/export/pt2_archive/_package.py
+++ torch/export/pt2_archive/_package.py
@@ -989,18 +989,20 @@ def load_pt2(
 
     from torch._inductor.cpp_builder import normalize_path_separator
 
-    if not (
-        (isinstance(f, (io.IOBase, IO)) and f.readable() and f.seekable())
-        or (isinstance(f, (str, os.PathLike)) and os.fspath(f).endswith(".pt2"))
-    ):
-        # TODO: turn this into an error in 2.9
-        logger.warning(
-            "Unable to load package. f must be a buffer or a file ending in "
-            ".pt2. Instead got {%s}",
-            f,
+    is_buffer = isinstance(f, (io.IOBase, IO))
+    is_path = isinstance(f, (str, os.PathLike))
+
+    if not ((is_buffer and f.readable() and f.seekable()) or is_path):
+        raise TypeError(
+            "Unable to load package. `f` must be a buffer (i.e. a readable and "
+            "seekable file-like object) or a string path. "
+            f"Instead got {type(f).__name__}."
         )
 
-    if isinstance(f, (str, os.PathLike)):
+    if is_path and not os.fspath(f).endswith(".pt2"):
+        logger.warning("Path does not end with .pt2; proceeding anyway: {%s}", f)
+
+    if is_path:
         f = os.fspath(f)
 
     weights = {}
