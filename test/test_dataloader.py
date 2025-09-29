0ad7a180ec0dcf537af004988b9d1bad9595d718
Rohit Singh Rathaur <rrathaur@redhat.com>
2025-09-29 11:43:06 +0530

    Fix linting failure

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -2483,6 +2483,7 @@ except RuntimeError as e:
         Regression test for issue #163184 where DataLoader ignores custom
         transformations in Subset subclasses that override __getitem__.
         """
+
         class SimpleDataset(Dataset):
             def __init__(self):
                 self.data = torch.arange(20)
@@ -2529,6 +2530,7 @@ except RuntimeError as e:
 
         class SumSubset(Subset):
             """Subset that returns sum instead of tuple"""
+
             def __getitem__(self, idx):
                 original_idx = self.indices[idx]
                 a, b = self.dataset[original_idx]
