0f619c1f893fa475296470ecbb73ccecb353fb55
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-09-29 21:53:42 +0000

    Revert "[inductor] do comm compute overlap at aten fx level (#163215)"
    This reverts commit c9b5af9a384e7ef5f95613abe1622f5f55133c3a.
    
    Reverted https://github.com/pytorch/pytorch/pull/163215 on behalf of https://github.com/yangw-dev due to seems fails inductor/test_aten_comm_compute_reordering for macos test, see https://hud.pytorch.org/pytorch/pytorch/commit/c9b5af9a384e7ef5f95613abe1622f5f55133c3a#51526707590-box ([comment](https://github.com/pytorch/pytorch/pull/163215#issuecomment-3349177940))

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -9058,7 +9058,6 @@ class _CollectiveKernel(FallbackKernel):
         assert not unbacked_bindings, f"{kernel} {unbacked_bindings}"
         for tensor_arg in tensor_args:
             tensor_arg.realize()
-            V.graph.mark_buffer_mutated(tensor_arg.get_name())
 
         device = tensor_args[0].get_device()
         packed = cls(
