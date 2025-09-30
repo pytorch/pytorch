059dc10eefa927e5c125a6afb7e6a880bcbba0db
Eddie Yan <eddiey@nvidia.com>
2025-09-30 22:08:53 +0000

    lint

--- torch/cuda/green_contexts.py
+++ torch/cuda/green_contexts.py
@@ -1,18 +1,20 @@
 import torch
 
+
 _GreenContext = object
 SUPPORTED = False
 
 if hasattr(torch._C, "GreenContext"):
-    _GreenContext = torch._C.GreenContext # type: ignore[misc]
+    _GreenContext = torch._C.GreenContext  # type: ignore[misc]
     SUPPORTED = True
+
 
 # Python shim helps Sphinx process docstrings more reliably.
 class GreenContext(_GreenContext):
     r"""Wrapper around a CUDA green context.
 
-     .. warning::
-        This API is in beta and may change in future releases.
+    .. warning::
+       This API is in beta and may change in future releases.
     """
 
     @staticmethod
@@ -25,17 +27,16 @@ class GreenContext(_GreenContext):
         """
         if not SUPPORTED:
             raise RuntimeError("PyTorch was not built with Green Context support!")
-        return _GreenContext.create(num_sms, device_id) # type: ignore[attr-defined]
+        return _GreenContext.create(num_sms, device_id)  # type: ignore[attr-defined]
 
     # Note that these functions are bypassed by we define them here
     # for Sphinx documentation purposes
     def make_current(self) -> None:
-        r"""Make the green context the current context.
-        """
-        return super().make_current() # type: ignore[misc]
+        r"""Make the green context the current context."""
+        return super().make_current()  # type: ignore[misc]
 
     def pop_current(self) -> None:
         r"""Assuming the green context is the current context, pop it from the
-            context stack and restore the previous context.
+        context stack and restore the previous context.
         """
-        return super().pop_current() # type: ignore[misc]
+        return super().pop_current()  # type: ignore[misc]
