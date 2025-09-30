4ad4627c01fd62a5e04b0800e177b1c85e59a2bf
Morrison Turnansky <mturnans@redhat.com>
2025-09-30 13:06:22 +0000

    renamed _prims_common.is_non_overlapping_and_dense to _prims_common.is_non_overlapping_and_dense_or_false

--- torch/_prims_common/__init__.py
+++ torch/_prims_common/__init__.py
@@ -518,7 +518,7 @@ def _is_non_overlapping_and_dense_or_false(sizes, strides) -> bool:
     return check_contiguous_sizes_strides(sizes, strides, false_if_dde=True)
 
 
-def is_non_overlapping_and_dense(a: Tensor) -> bool:
+def is_non_overlapping_and_dense_or_false(a: Tensor) -> bool:
     """
     True when a tensor is non-overlapping and dense.
 
@@ -688,7 +688,7 @@ def compute_elementwise_output_strides(*tensors) -> tuple[int, ...]:
         return (1,)
     
     if len(tensors) == 1:
-        if torch._prims_common.is_non_overlapping_and_dense(tensors[0]):
+        if torch._prims_common.is_non_overlapping_and_dense_or_false(tensors[0]):
             return tensors[0].stride()
 
     logical_to_physical_perm, _ = compute_elementwise_output_logical_to_physical_perm(
