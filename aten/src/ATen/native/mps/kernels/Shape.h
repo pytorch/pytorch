84f69e46845342ab2ecb15e939349073c69e77df
Kurt Mohler <kmohler@quansight.com>
2025-10-01 18:03:35 -0500

    [MPS] Support large tensors in `torch.cat`
    Fixes #164415
    
    
    ghstack-source-id: c35e34e32679786d1d4704ff447bdad2581369bc
    Pull-Request: https://github.com/pytorch/pytorch/pull/164416

---
+++ aten/src/ATen/native/mps/kernels/Shape.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <c10/metal/common.h>
+
+template <unsigned N = c10::metal::max_ndim, typename idx_type_t = int64_t>
+struct CatLargeSharedParams {
+  int32_t ndim;
+  int32_t cat_dim;
+  ::c10::metal::array<idx_type_t, N> output_strides;
+  ::c10::metal::array<idx_type_t, N> output_sizes;
+};
+
+template <unsigned N = c10::metal::max_ndim, typename idx_type_t = int64_t>
+struct CatLargeInputParams {
+  idx_type_t cat_dim_offset;
+  idx_type_t elements_per_thread;
+  idx_type_t input_numel;
+  ::c10::metal::array<idx_type_t, N> input_strides;
+  ::c10::metal::array<idx_type_t, N> input_sizes;
+};
