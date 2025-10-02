0104dc622d24405e1569e3c222fc0cf86acc0283
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2025-10-02 14:01:03 -0700

    Update base for Update on "[Testing only] Try fdump-record-layouts test in CI"
    [ghstack-poisoned]

--- torch/csrc/stable/ops.h
+++ torch/csrc/stable/ops.h
@@ -8,7 +8,6 @@
 #include <vector>
 
 #include <torch/csrc/inductor/aoti_torch/generated/c_shim_aten.h>
-#include <torch/csrc/stable/version.h>
 #include <torch/headeronly/core/ScalarType.h>
 
 namespace torch::stable {
@@ -25,8 +24,8 @@ inline torch::stable::Tensor empty_like(const torch::stable::Tensor& self) {
       from(std::nullopt),
       from(std::nullopt),
       from(std::nullopt)};
-  TORCH_ERROR_CODE_CHECK(aoti_torch_call_dispatcher_v2(
-      "aten::empty_like", "", stack.data(), TORCH_ABI_VERSION));
+  TORCH_ERROR_CODE_CHECK(
+      aoti_torch_call_dispatcher("aten::empty_like", "", stack.data()));
   return to<torch::stable::Tensor>(stack[0]);
 }
 
@@ -196,8 +195,8 @@ inline torch::stable::Tensor transpose(
     int64_t dim1) {
   const auto num_args = 3;
   std::array<StableIValue, num_args> stack{from(self), from(dim0), from(dim1)};
-  TORCH_ERROR_CODE_CHECK(aoti_torch_call_dispatcher_v2(
-      "aten::transpose", "int", stack.data(), TORCH_ABI_VERSION));
+  TORCH_ERROR_CODE_CHECK(
+      aoti_torch_call_dispatcher("aten::transpose", "int", stack.data()));
   return to<torch::stable::Tensor>(stack[0]);
 }
 
@@ -207,8 +206,8 @@ inline torch::stable::Tensor transpose(
 inline torch::stable::Tensor zero_(torch::stable::Tensor& self) {
   const auto num_args = 1;
   std::array<StableIValue, num_args> stack{from(self)};
-  TORCH_ERROR_CODE_CHECK(aoti_torch_call_dispatcher_v2(
-      "aten::zero_", "", stack.data(), TORCH_ABI_VERSION));
+  TORCH_ERROR_CODE_CHECK(
+      aoti_torch_call_dispatcher("aten::zero_", "", stack.data()));
   return to<torch::stable::Tensor>(stack[0]);
 }
 
@@ -221,8 +220,8 @@ inline torch::stable::Tensor copy_(
   const auto num_args = 3;
   std::array<StableIValue, num_args> stack{
       from(self), from(src), from(non_blocking.value_or(false))};
-  TORCH_ERROR_CODE_CHECK(aoti_torch_call_dispatcher_v2(
-      "aten::copy_", "", stack.data(), TORCH_ABI_VERSION));
+  TORCH_ERROR_CODE_CHECK(
+      aoti_torch_call_dispatcher("aten::copy_", "", stack.data()));
   return to<torch::stable::Tensor>(stack[0]);
 }
 
@@ -231,15 +230,9 @@ inline torch::stable::Tensor copy_(
 inline torch::stable::Tensor clone(const torch::stable::Tensor& self) {
   const auto num_args = 2;
   std::array<StableIValue, num_args> stack{from(self), from(std::nullopt)};
-  TORCH_ERROR_CODE_CHECK(aoti_torch_call_dispatcher_v2(
-      "aten::clone", "", stack.data(), TORCH_ABI_VERSION));
+  TORCH_ERROR_CODE_CHECK(
+      aoti_torch_call_dispatcher("aten::clone", "", stack.data()));
   return to<torch::stable::Tensor>(stack[0]);
 }
-
-#if TORCH_FEATURE_VERSION >= (((0ULL + 2) << 56) | ((0ULL + 10) << 48))
-
-// New ops should be added here if they do not use aoti_torch_call_dispatcher
-
-#endif
 
 } // namespace torch::stable
