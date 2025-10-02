0e80a08fb39522d6778c1f19d6269366ed4de4f3
drisspg <drisspguessous@gmail.com>
2025-10-02 00:10:51 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/kernel/flex/flex_attention.py
+++ torch/_inductor/kernel/flex/flex_attention.py
@@ -569,6 +569,7 @@ def flex_attention_backward(*args, **kwargs):
         query,
         key,
         value,
+        logsumexp,
         grad_out,
         kv_num_blocks,
         kv_indices,
@@ -583,6 +584,7 @@ def flex_attention_backward(*args, **kwargs):
             query,
             key,
             value,
+            logsumexp,
             grad_out,
             kv_num_blocks,
             kv_indices,
@@ -782,7 +784,7 @@ def flex_attention_backward(*args, **kwargs):
         for attrib in ["kpack", "matrix_instr_nonkdim", "waves_per_eu"]:
             if hasattr(conf, attrib):
                 cur_kernel_options[attrib] = getattr(conf, attrib)
-
+        # import transformer_nuggets; transformer_nuggets.vscode_breakpoint()
         flex_attention_backward_template.maybe_append_choice(
             choices=choices,
             input_nodes=[
