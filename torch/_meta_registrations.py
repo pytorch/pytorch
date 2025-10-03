24e0be003eef4cad93222859f8c1817c318af312
Parshant Sharma <parsshar@redhat.com>
2025-10-03 22:12:54 +0530

    fix cuda issue

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -1021,8 +1021,9 @@ def meta_linalg_eig(input: Tensor):
     )
     values = input.new_empty(input.shape[:-1], dtype=complex_dtype)
     vectors = input.new_empty(input.shape, dtype=complex_dtype)
+    is_cuda = device_hint(input) == "cuda"
     vectors.as_strided_(
-        input.shape, make_contiguous_strides_for(input.shape, row_major=False)
+        input.shape, make_contiguous_strides_for(input.shape, row_major=is_cuda)
     )
     return values, vectors
 
