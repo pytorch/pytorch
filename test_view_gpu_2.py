c1d0bc044960964398ef8f006a04849e8dc71b4c
Wei Feng <weif@meta.com>
2025-10-03 15:29:47 -0700

    simplify 2 gpu repro
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: a5265da4fa412859a1d793e5ee72bc8a95f2e415
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/164558

---
+++ test_view_gpu_2.py
@@ -0,0 +1,38 @@
+# torchrun --nproc_per_node=2 test_view_gpu_2.py
+
+import torch
+import torch.nn as nn
+from torch.distributed.tensor import (
+    distribute_module,
+    distribute_tensor,
+    DTensor,
+    init_device_mesh,
+    Replicate,
+    Shard,
+)
+from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.distributed._tensor.common_dtensor import (
+    DTensorTestBase,
+    with_comms,
+)
+
+import os
+import torch
+import torch.distributed as dist
+
+def main():
+    dist.init_process_group(backend="nccl")
+    gpu_id = int(os.environ["LOCAL_RANK"])
+    device = f"cuda:{gpu_id}"
+    torch.cuda.set_device(device)
+    mesh_shape = (2, )
+    mesh = init_device_mesh("cuda", mesh_shape)
+    input_dim, batch_size, seq_len = 4, 8, 8
+    global_input = torch.randn([batch_size, seq_len, input_dim])
+    input_sharding = (Shard(1), )
+    distributed_input = distribute_tensor(global_input, mesh, input_sharding)
+    distributed_input.view(batch_size * seq_len, input_dim)
+
+
+if __name__ == "__main__":
+    main()
