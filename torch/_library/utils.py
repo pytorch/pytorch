07902264e55ffa180b67cef2e0862063663e3eb9
albanD <desmaison.alban@gmail.com>
2025-10-03 14:33:12 -0400

    Update
    [ghstack-poisoned]

--- torch/_library/utils.py
+++ torch/_library/utils.py
@@ -7,6 +7,7 @@ from typing import Any, Literal, Optional, overload, Union
 
 import torch
 import torch.utils._pytree as pytree
+import torchgen
 from torch import _C, _utils_internal
 from torch._ops import OpOverload
 
@@ -91,13 +92,26 @@ def is_functional_schema(schema: Any, *, view_ok: bool = False) -> bool:
             r.alias_info is not None and not r.alias_info.is_write for r in rets
         )
         num_tensor_inputs = 0
-        for arg in schema.arguments:
-            if isinstance(arg.type, torch.TensorType):
-                num_tensor_inputs += 1
         num_tensor_outputs = 0
-        for ret in schema.returns:
-            if isinstance(ret.type, torch.TensorType):
-                num_tensor_outputs += 1
+
+        if isinstance(schema, torch.FunctionSchema):
+            for arg in schema.arguments:
+                if isinstance(arg.type, torch.TensorType):
+                    num_tensor_inputs += 1
+
+            for ret in schema.returns:
+                if isinstance(ret.type, torch.TensorType):
+                    num_tensor_outputs += 1
+
+        elif isinstance(schema, torchgen.model.FunctionSchema):
+            for arg in schema.arguments.flat_non_out:
+                if arg.type.is_tensor_like():
+                    num_tensor_inputs += 1
+
+            for ret in schema.returns:
+                if ret.type.is_tensor_like():
+                    num_tensor_outputs += 1
+
         if is_non_mutating_view:
             return view_ok and (num_tensor_inputs == 1 and num_tensor_outputs == 1)
         if not schema.returns:
