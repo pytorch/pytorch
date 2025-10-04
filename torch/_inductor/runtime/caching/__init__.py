2795f0881610aa8e98c91aae34e31be0e67b2440
Nicolas Macchioni <nmacchioni@meta.com>
2025-10-04 01:56:59 -0700

    exceptions + unit tests (#164550)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/164550
    
    Test Plan:
    ```
    buck test fbcode//mode/opt caffe2/test/inductor:caching
    ```
    
    Reviewed By: aorenste
    
    Differential Revision: D83714688

--- torch/_inductor/runtime/caching/__init__.py
+++ torch/_inductor/runtime/caching/__init__.py
@@ -1,8 +1,32 @@
 from .context import IsolationSchema, SelectedCompileContext, SelectedRuntimeContext
+from .exceptions import (
+    CacheError,
+    SystemError,
+    UserError,
+    LockTimeoutError,
+    FileLockTimeoutError,
+    KeyEncodingError,
+    KeyPicklingError,
+    ValueEncodingError,
+    ValuePicklingError,
+    ValueDecodingError,
+    ValueUnPicklingError,
+)
 
 
 __all__ = [
     "SelectedCompileContext",
     "SelectedRuntimeContext",
     "IsolationSchema",
+    "CacheError",
+    "SystemError",
+    "UserError",
+    "LockTimeoutError",
+    "FileLockTimeoutError",
+    "KeyEncodingError",
+    "KeyPicklingError",
+    "ValueEncodingError",
+    "ValuePicklingError",
+    "ValueDecodingError",
+    "ValueUnPicklingError",
 ]
