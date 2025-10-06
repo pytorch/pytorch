901929fc6270b6910bfb7680cd1cd38537f73ec4
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2025-10-06 12:22:40 -0700

    Update on "Return fake mode from export graph capture API"
    This PR is to temporarily unblock various experiments to re-use dynamo create fake mode. Note that this is still not what we want as the end state. The end state should look sth like:
    ```
    out = fulllgraph_capture(mod, inputs)
    fake_mode = out.backend_inputs.fake_mode 
    gm  = out.module()
    ```
    This doesn't work today because export requires we need to wrap the original module to setup a flat module to trace for easier handling of pytree. As a result, we would need to carry export specific flag in fullgraph_capture which seems not ideal. 
    Regardless, the end state is that we need to give downstream user a graph module and a fake mode in some form, so I think _dynamo_graph_capture_for_export returning a fake mode within graph module itself via gm.meta 
    
    cc H-Huang awgu wanchaol fegin fduwjj wz337 wconstab d4l3k pragupta msaroufim dcci voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames Lucaskabela
    
    [ghstack-poisoned]

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -834,8 +834,8 @@ def _export_to_torch_ir(
                     # We can't serialize entire fake mode yet, so this is to make sure
                     # things like copy.deepcopy(ep.graph_module) not crash.
                     # see test_export.py::test_custom_tag_metadata_re_export
-                    # Once we delete the old strict export, we can use this fake mode in the 
-                    # subsequent logic when lowering to aten IR. 
+                    # Once we delete the old strict export, we can use this fake mode in the
+                    # subsequent logic when lowering to aten IR.
                     del gm_torch_level.meta["fake_mode"]
 
                 else:
