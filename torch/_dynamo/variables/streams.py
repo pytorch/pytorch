02cbfe34692bdb3f4c898e2be6d3de763f1c0e75
Michael Lazos <mlazos@meta.com>
2025-10-06 22:08:50 -0700

    [user-cuda-streams] Add fork/join custom ops
    Make custom ops inplace
    
    ghstack-source-id: 3e41664853ac60d49b60bdebd8b3859d227925ad
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162900

--- torch/_dynamo/variables/streams.py
+++ torch/_dynamo/variables/streams.py
@@ -14,6 +14,35 @@ if TYPE_CHECKING:
 
     from ..codegen import PyCodegen
 
+from torch._library.custom_ops import custom_op
+
+
+Tensor = torch.Tensor
+
+
+@custom_op("streams::fork", mutates_args={"args"})
+def fork_stream_(
+    index: int, device: torch.device, device_index: int, args: list[Tensor]
+) -> None:
+    pass
+
+
+@fork_stream_.register_fake
+def _(index: int, device: torch.device, device_index: int, args: list[Tensor]) -> None:
+    pass
+
+
+@custom_op("streams::join", mutates_args={"args"})
+def join_stream_(
+    index: int, device: torch.device, device_index: int, args: list[Tensor]
+) -> None:
+    pass
+
+
+@join_stream_.register_fake
+def _(index: int, device: torch.device, device_index: int, args: list[Tensor]) -> None:
+    pass
+
 
 class StreamVariable(VariableTracker):
     def __init__(
