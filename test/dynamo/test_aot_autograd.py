285e77534daeffc8e1aa692a620d4c9e445d229f
Lucas Kabela <lucaskabela@meta.com>
2025-10-07 16:43:03 -0700

    Fix for aot_eager - not inductor yet

--- test/dynamo/test_aot_autograd.py
+++ test/dynamo/test_aot_autograd.py
@@ -1650,6 +1650,17 @@ SeqNr|OrigAten|SrcFn|FwdSrcFn
         # However, at the time this change was introduced, it went down from 15154 to 403.
         self.assertLess(len(shape_env_guards), 1000)
 
+    # See # https://github.com/pytorch/pytorch/issues/164814
+    def test_aot_autograd_stride_reconstruction_on_zero_dim_dynamic_shaped_tensor(self) -> None:
+        def repro(sentinel: torch.Tensor) -> torch.Tensor:
+            x = torch.unique(torch.ones(1))
+            x = torch.reshape(x, [1])
+            x = torch.squeeze(x)  # 0-d tensor
+            return x * sentinel
+
+        # Grad required to trigger the issue (need to replay stride)
+        sentinel = torch.tensor(1.0, requires_grad=True)
+        self.assertEqual(repro(sentinel), torch.compile(repro, backend="aot_eager",fullgraph=True)(sentinel))
 
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
