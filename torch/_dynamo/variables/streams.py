612b321e3c9e8467108cb305848c5221894d96ad
Michael Lazos <mlazos@meta.com>
2025-10-08 16:39:39 -0700

    Update base for Update on "[user-streams] Add stream state manager"
    The stream state manager has been stripped down to only contain the previous stream info when forking a stream, this is to record the stream info for when we join afterward. 
    
    
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames Lucaskabela
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/streams.py
+++ torch/_dynamo/variables/streams.py
@@ -20,27 +20,27 @@ from torch._library.custom_ops import custom_op
 Tensor = torch.Tensor
 
 
-@custom_op("streams::fork", mutates_args={"args"})
-def fork_stream_(
-    index: int, device: torch.device, device_index: int, args: list[Tensor]
+@custom_op("streams::fork", mutates_args=())
+def fork_stream(
+    from_index: int,
+    from_device: torch.device,
+    from_device_index: int,
+    to_index: int,
+    to_device: torch.device,
+    to_device_index: int,
 ) -> None:
     pass
 
 
-@fork_stream_.register_fake
-def _(index: int, device: torch.device, device_index: int, args: list[Tensor]) -> None:
-    pass
-
-
-@custom_op("streams::join", mutates_args={"args"})
-def join_stream_(
-    index: int, device: torch.device, device_index: int, args: list[Tensor]
+@custom_op("streams::join", mutates_args=())
+def join_stream(
+    from_index: int,
+    from_device: torch.device,
+    from_device_index: int,
+    to_index: int,
+    to_device: torch.device,
+    to_device_index: int,
 ) -> None:
-    pass
-
-
-@join_stream_.register_fake
-def _(index: int, device: torch.device, device_index: int, args: list[Tensor]) -> None:
     pass
 
 
