9baa905ed111143c1e9d6b5b6d8426cd67c29f7a
Sam Larsen <slarsen@meta.com>
2025-10-08 17:50:31 -0700

    [inductor] custom_graph_pass.get_hash_for_files: add flag to optionally hash paths
    Summary: We have an internal user where caching broke because the paths that are unzipped are probably different per host. We probably need to give them the option to hash only the file content.
    
    [ghstack-poisoned]

--- torch/_inductor/custom_graph_pass.py
+++ torch/_inductor/custom_graph_pass.py
@@ -92,7 +92,9 @@ CustomGraphPassType: TypeAlias = Optional[
 
 
 @lru_cache(1)
-def get_hash_for_files(paths: tuple[str], extra: str = "") -> bytes:
+def get_hash_for_files(
+    paths: tuple[str], extra: str = "", hash_paths: bool = True
+) -> bytes:
     """
     Helper to compute a unique string by hashing the contents of a list of files.
     """
@@ -100,7 +102,8 @@ def get_hash_for_files(paths: tuple[str], extra: str = "") -> bytes:
     hasher.update(extra.encode("utf-8"))
     for path in paths:
         with open(path, "rb") as f:
-            hasher.update(path.encode("utf-8"))
+            if hash_paths:
+                hasher.update(path.encode("utf-8"))
             hasher.update(f.read())
     return hasher.digest()
 
