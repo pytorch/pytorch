aeb76206c717aeb70ef5b8fd97a5cb860ccbf0ae
Ting Lu <tingl@nvidia.com>
2025-10-08 20:54:41 -0700

    Inductor 13.0 test

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -136,6 +136,17 @@ case "$tag" in
     TRITON=yes
     INDUCTOR_BENCHMARKS=yes
     ;;
+  pytorch-linux-jammy-cuda13.0-cudnn9-py3-gcc9-inductor-benchmarks)
+    CUDA_VERSION=13.0.0
+    ANACONDA_PYTHON_VERSION=3.10
+    GCC_VERSION=9
+    VISION=yes
+    KATEX=yes
+    UCX_COMMIT=${_UCX_COMMIT}
+    UCC_COMMIT=${_UCC_COMMIT}
+    TRITON=yes
+    INDUCTOR_BENCHMARKS=yes
+    ;;
   pytorch-linux-jammy-cuda12.8-cudnn9-py3.12-gcc11-vllm)
     CUDA_VERSION=12.8.1
     ANACONDA_PYTHON_VERSION=3.12
