ee6a1ecb0a1035f068484c8fcfba44b2efc9e837
Jithun Nair <jithun.nair@amd.com>
2025-10-09 18:03:08 +0000

    [ROCm] Enable MI355 CI on PRs, and run full set of UTs on PRs (#160215)
    Useful to have PR testing for PRs such as https://github.com/pytorch/pytorch/pull/151360
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160215
    Approved by: https://github.com/malfet, https://github.com/atalman
    
    Co-authored-by: Jeff Daily <jeff.daily@amd.com>

--- test/inductor/test_cuda_repro.py
+++ test/inductor/test_cuda_repro.py
@@ -39,6 +39,8 @@ from torch.testing._internal.common_utils import (
     DeterministicGuard,
     freeze_rng_state,
     IS_FBCODE,
+    MI350_ARCH,
+    skipIfRocmArch,
     TEST_WITH_ASAN,
     TEST_WITH_ROCM,
     xfailIfPy312Plus,
@@ -218,6 +220,7 @@ class CudaReproTests(TestCase):
         # dont check rng state
         self.assertEqual(out[:2], fn(query, key, value, input_tensor2)[:2])
 
+    @skipIfRocmArch(MI350_ARCH)
     def test_effn_attn_bias_padding_misaligned(self):
         seqlen_start = 1008
 
