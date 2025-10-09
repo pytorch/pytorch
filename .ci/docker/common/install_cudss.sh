3bf9d52569e847a775aa787d12c8dc5595100fc8
Johnny <johnnynuca14@gmail.com>
2025-10-09 12:55:41 +0200

    remove comment

--- .ci/docker/common/install_cudss.sh
+++ .ci/docker/common/install_cudss.sh
@@ -14,7 +14,6 @@ fi
 CUDSS_NAME="libcudss-linux-${arch_path}-0.7.0.20_cuda${CUDA_MAJOR}-archive"
 curl --retry 3 -OLs https://developer.download.nvidia.com/compute/cudss/redist/libcudss/linux-${arch_path}/${CUDSS_NAME}.tar.xz
 
-# only for cuda 12
 tar xf ${CUDSS_NAME}.tar.xz
 cp -a ${CUDSS_NAME}/include/* /usr/local/cuda/include/
 cp -a ${CUDSS_NAME}/lib/* /usr/local/cuda/lib64/
