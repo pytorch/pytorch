6f2773b4cbc9cf059e3eba4968ba17c4307e82ed
Laith Sakka <lsakka@meta.com>
2025-10-09 16:24:23 -0700

    Update on "avoid implicit trivial broadcast specialization on unbacked wip"
    [ghstack-poisoned]

--- torch/_subclasses/fake_impls.py
+++ torch/_subclasses/fake_impls.py
@@ -1243,7 +1243,7 @@ def register_fast_op_impl(func: OpOverload):
 
 # infer_size_impl in ExpandUtils
 def infer_size(a, b):
-    from torch.fx.experimental.symbolic_shapes import sym_or
+    from torch.fx.experimental.symbolic_shapes import guard_or_false
 
     dimsA = len(a)
     dimsB = len(b)
@@ -1268,7 +1268,7 @@ def infer_size(a, b):
         # were not the case, we'd need to write this using torch.sym_or() or
         # something like that).
         torch._check(
-            sym_or (sizeA == 1,sizeB == 1, sizeA == sizeB),
+            guard_or_false (sizeA == 1) or guard_or_false(sizeB == 1) or  sizeA == sizeB,
             lambda: f"The size of tensor a ({sizeA}) "
             f"must match the size of tensor b ({sizeB}) "
             f"at non-singleton dimension {i})",
