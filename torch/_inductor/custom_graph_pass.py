7a3dc7ee0e0171eb5d4a1cc32b72f08116fc338c
Sam Larsen <slarsen@meta.com>
2025-10-09 07:42:50 -0700

    Update on "[inductor] custom_graph_pass.get_hash_for_files: don't hash paths"
    Summary: We have an internal user where caching broke because the paths that are unzipped are probably different per host. We can't think of a use case where a path change matters when the file content has not changed, so removing this part
    
    [ghstack-poisoned]

--- torch/_inductor/custom_graph_pass.py
+++ torch/_inductor/custom_graph_pass.py
@@ -92,9 +92,7 @@ CustomGraphPassType: TypeAlias = Optional[
 
 
 @lru_cache(1)
-def get_hash_for_files(
-    paths: tuple[str], extra: str = "", hash_paths: bool = True
-) -> bytes:
+def get_hash_for_files(paths: tuple[str], extra: str = "") -> bytes:
     """
     Helper to compute a unique string by hashing the contents of a list of files.
     """
@@ -102,8 +100,6 @@ def get_hash_for_files(
     hasher.update(extra.encode("utf-8"))
     for path in paths:
         with open(path, "rb") as f:
-            if hash_paths:
-                hasher.update(path.encode("utf-8"))
             hasher.update(f.read())
     return hasher.digest()
 
