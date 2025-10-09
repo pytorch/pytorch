34ac9b61cbfcf17328ccb8b729509829447fdddd
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-10-09 22:53:50 +0000

    Revert "[export] Turn on install_free_tensors flag (#164691)"
    This reverts commit 0e9b3a772ab96e998ab85591d5b2a9c1d41bacb0.
    
    Reverted https://github.com/pytorch/pytorch/pull/164691 on behalf of https://github.com/izaitsevfb due to breaks tests internally, author asked to revert, see [D84230990](https://www.internalfb.com/diff/D84230990) ([comment](https://github.com/pytorch/pytorch/pull/164691#issuecomment-3387718323))

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -2075,14 +2075,9 @@ class ExecutorchCallDelegateHigherOrderVariable(TorchHigherOrderOperatorVariable
             unimplemented(
                 "executorch_call_delegate: kwargs arguments were not enabled."
             )
-        if isinstance(args[0], variables.NNModuleVariable):
-            lowered_module = args[0].module
-            lowered_module = tx.output.get_submodule(args[0].module_key)
-            lowered_node = make_attr(tx, args[0].module_key)
-        elif isinstance(args[0], variables.UnspecializedNNModuleVariable):
-            lowered_module = args[0].value
-            mod_name = tx.output.install_subgraph("delegate", lowered_module)
-            lowered_node = make_attr(tx, mod_name)
+        lowered_module = tx.output.get_submodule(args[0].module_key)
+
+        lowered_node = make_attr(tx, args[0].module_key)
 
         p_args = tuple(arg.as_proxy() for arg in args[1:])
         real_sub_args = pytree.tree_map_only(
