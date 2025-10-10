77bf23d85c47d6b89a6bd7839b41fa97f6ede7c4
Shangdi Yu <shangdiy@meta.com>
2025-10-10 07:53:54 +0000

    Add an option to put store large mmap weights on disk (#164526)
    As title
    
    In windows, we cannot modify the .dll to append weights at the end, the windows .dll loader will complain it's not a valid .dll file. So we store the weight blob as a separete file.
    
    1. We add the following API which allows passing in a pointer to the weight blob and get the size of the weight blob.
    
    ```cpp
    AOTI_API AOTIRuntimeError AOTInductorModelContainerGetConstantsBlobSize(
        AOTInductorModelContainerHandle container_handle,
        uint64_t* ret_size);
    
    // Load weights from a single blob in weight_blob_ptr
    AOTI_API AOTIRuntimeError AOTInductorModelUpdateConstantsFromBlob(
        AOTInductorModelContainerHandle container_handle,
        const uint8_t* weight_blob_ptr);
    ```
    
    2. We also add a method in ModelContainerRunner to load the weight:
    
    If the runner see that there is a `.blob` file in the package, if will mmap the .blob file and use the content to load the constants.
    
    3. We also add the `USE_MMAP_EXTERNAL` macro. When this macro is defined, the model expects to load the weights from external mmap'd weights.
    
    Test Plan:
    
    ```
    buck run @mode/dev-nosan caffe2/test/inductor:test_aot_inductor -- -r test_large_mmaped_weights_on_disk
    ```
    
    Also tested for windows-cross compilation with https://github.com/meta-pytorch/torchnative/blob/6542566585ab2cb4cd6c8e3873a6a02cfe51664b/demo/main_voxtral.cpp
    
    ```
    Loaded model.dll
    audio_encoder loaded
    C:\Users\shangdiy\source\repos\torchnative\demo\token_embedding\data\aotinductor\model\model.wrapper.so
    Loaded model.dll
    token_embedding loaded
    C:\Users\shangdiy\source\repos\torchnative\demo\text_decoder\data\aotinductor\model\model.wrapper.so
    Loaded model.dll
    Loading weights from C:\Users\shangdiy\source\repos\torchnative\demo\text_decoder\data\aotinductor\model\model.wrapper_weights.blob
    text_decoder loaded
    Load latency (ms):
      audio_encoder: 1011.234
        archive extraction: 0.000
        .so loading: 1011.197
      token_embedding: 525.773
        archive extraction: 0.000
        .so loading: 525.704
      text_decoder: 3324.130
        archive extraction: 0.000
        .so loading: 3323.979
    Run latency (ms):
      audio_encoder: 285.958
        audio_encoder output: dtype=bfloat16, shape=[1, 1125, 3072], numel=3456000
      token_embedding: 6.676
        token_embedding output: dtype=bfloat16, shape=[1, 1138, 3072], numel=3495936
      text_decoder: 576.519
        text_decoder output: dtype=bfloat16, shape=[1, 1138, 131072], numel=149159936
    ```
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/164526
    Approved by: https://github.com/desertfire

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -3624,7 +3624,64 @@ def maybe_aoti_standalone_config(config_patches: dict[str, Any]) -> dict[str, An
         )
         force_patch_config(config_patches, "aot_inductor.dynamic_linkage", False)
 
+    cross_target_platform = config_patches.get(
+        "aot_inductor.cross_target_platform",
+        config.aot_inductor.cross_target_platform,
+    )
+
+    package_constants_in_so = config_patches.get(
+        "aot_inductor.package_constants_in_so",
+        config.aot_inductor.package_constants_in_so,
+    )
+
+    if cross_target_platform == "windows" and package_constants_in_so:
+        raise RuntimeError(
+            "config.aot_inductor.package_constants_in_so is not supported for windows cross-compilation. "
+            "Please use config.aot_inductor.package_constants_on_disk_format = binary_blob."
+        )
+
     return config_patches
+
+
+def determine_aoti_mmap_flags(consts_size: int) -> tuple[bool, bool]:
+    """
+    Decide whether we should mmap weights, and whether to store the weights with .so.
+
+    If force_mmap_weights or package_constants_on_disk_format == "binary_blob" configs are set, respect the config.
+
+    Returns tuple (use_external_weights, use_mmap_weights).
+    """
+
+    if (
+        config.aot_inductor.force_mmap_weights
+        and config.aot_inductor.package_constants_on_disk_format == "binary_blob"
+    ):
+        raise RuntimeError(
+            "config.aot_inductor.package_constants_on_disk_format = binary_blob and "
+            "config.aot_inductor.force_mmap_weights cannot both be True."
+        )
+
+    if config.aot_inductor.force_mmap_weights:
+        if config.aot_inductor.cross_target_platform == "windows":
+            raise RuntimeError(
+                "when cross_target_platform is windows, use_mmap_weights should not be true."
+            )
+        use_mmap_weights = True
+        use_external_weights = False
+        return use_external_weights, use_mmap_weights
+
+    if config.aot_inductor.package_constants_on_disk_format == "binary_blob":
+        use_external_weights = True
+        use_mmap_weights = False
+        return use_external_weights, use_mmap_weights
+
+    if consts_size <= 2_000_000_000:
+        return False, False
+
+    use_external_weights = False
+    use_mmap_weights = not config.is_fbcode()
+
+    return use_external_weights, use_mmap_weights
 
 
 def is_valid_aoti_model_name() -> bool:
