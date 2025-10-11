9df6c8135c6ca0f5fc1aba3041087a35d0d46890
Thomas Bohnstingl <boh@zurich.ibm.com>
2025-10-11 09:38:23 +0200

    Updated LSTM and RNN benchmark

--- benchmarks/dynamo/scan/benchmark_lstm.py
+++ benchmarks/dynamo/scan/benchmark_lstm.py
@@ -6,22 +6,52 @@ from time import perf_counter
 ###############################################################################
 # Auxiliary function for timing
 ###############################################################################
-def time_fn(fn, xs, warm_up=1):
-    for _ in range(warm_up):
-        result = fn(*xs)
+def time_fn(fn, args, warm_up=1):
+    t_initial = -1.
+    for ind in range(warm_up):
+        t_start = perf_counter() 
+        result = fn(*args)
+        t_stop = perf_counter()
+        if ind == 0:
+            t_initial = t_stop - t_start
 
     t_start = perf_counter() 
-    result = fn(*xs)
+    result = fn(*args)
     t_stop = perf_counter()
-    t_ = t_stop - t_start
-    return result, t_
+    t_run = t_stop - t_start
+    return result, t_initial, t_run
 
 ###############################################################################
-# Define the RNN for PyTorch
+# Define the RNN models
 ###############################################################################
-class LSTM(torch.nn.Module):
+warm_up_cycles = 3
+# input_size = 15
+input_size = 50
+# hidden_size = 20
+hidden_size = 200
+time_steps = 20
+
+class LSTM_forloop(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(LSTM_forloop, self).__init__()
+        
+        self.lstm_cell = torch.nn.LSTMCell(input_size, hidden_size)
+        
+    # Implementation adopted from 
+    # https://docs.pytorch.org/docs/stable/generated/torch.nn.LSTMCell.html
+    def forward(self, init: torch.Tensor, xs: torch.Tensor):
+        # The input `xs` has the time as the first dimsion
+        output = []
+        for i in range(xs.size()[0]):
+            hx, cx = self.lstm_cell(xs[i], init)
+            init = (hx, cx)
+            output.append(hx)
+        output = torch.stack(output, dim=0)
+        return output
+
+class LSTM_scan(torch.nn.Module):
     def __init__(self, Wii, bii, Whi, bhi, Wif, bif, Whf, bhf, Wig, big, Whg, bhg, Wio, bio, Who, bho):
-        super(LSTM, self).__init__()
+        super(LSTM_scan, self).__init__()
         self.Wii = Wii.clone()
         self.bii = bii.clone()
         self.Whi = Whi.clone()
@@ -57,15 +87,26 @@ class LSTM(torch.nn.Module):
         
         return carry, outs
 
-# Define the RNN with pure PyTorch
-lstm_pytorch = torch.nn.LSTM(3, 5)
+# Define the for-loop LSTM model
+lstm_forloop = LSTM_forloop(input_size, hidden_size)
+lstm_forloop_comp = torch.compile(lstm_forloop, fullgraph=True)
 
-Wii, Wif, Wig, Wio = torch.chunk(lstm_pytorch.weight_ih_l0, 4)
-Whi, Whf, Whg, Who = torch.chunk(lstm_pytorch.weight_hh_l0, 4)
-bii, bif, big, bio = torch.chunk(lstm_pytorch.bias_ih_l0, 4)
-bhi, bhf, bhg, bho = torch.chunk(lstm_pytorch.bias_hh_l0, 4)
 
-lstm_scan = LSTM(
+# Define the LSTM using CUDA kernels
+lstm_forloop_state_dict = lstm_forloop.state_dict()
+lstm_cuda_state_dict = {}
+for key, value in lstm_forloop_state_dict.items():
+    new_key = key.replace('lstm_cell.', '') + '_l0'
+    lstm_cuda_state_dict[new_key] = value.clone()
+lstm_cuda = torch.nn.LSTM(input_size, hidden_size)
+lstm_cuda.load_state_dict(lstm_cuda_state_dict)
+
+# Define the LSTM model using scan
+Wii, Wif, Wig, Wio = torch.chunk(lstm_cuda.weight_ih_l0, 4)
+Whi, Whf, Whg, Who = torch.chunk(lstm_cuda.weight_hh_l0, 4)
+bii, bif, big, bio = torch.chunk(lstm_cuda.bias_ih_l0, 4)
+bhi, bhf, bhg, bho = torch.chunk(lstm_cuda.bias_hh_l0, 4)
+lstm_scan = LSTM_scan(
                 Wii.T, bii,
                 Whi.T, bhi,
                 
@@ -78,30 +119,47 @@ lstm_scan = LSTM(
                 Wio.T, bio,
                 Who.T, bho,
                 )
+lstm_scan_comp = torch.compile(lstm_scan, fullgraph=True)
 
 ###############################################################################
 # Define the inputs
 ###############################################################################
-warm_up_cycles = 3
-xs = torch.randn(1, 3, requires_grad=True)
-init = (torch.zeros(5, requires_grad=True), torch.zeros(5, requires_grad=True))
+xs = torch.randn(time_steps, input_size, requires_grad=True)
+init = (torch.zeros(hidden_size, requires_grad=True), torch.zeros(hidden_size, requires_grad=True))
 
 ###############################################################################
 # Run models forward (inference-only)
 ###############################################################################
-# Native PyTorch implementation
-result_pytorch, t_pytorch = time_fn(lstm_pytorch, (xs,), warm_up=warm_up_cycles)
-# result_pytorch = result_pytorch[0]
+# For-loop model
+result_forloop, time_initial_forloop, time_run_forloop = time_fn(lstm_forloop, (init, xs), warm_up=warm_up_cycles)
 
-# Implementation with scan
-result_scan, t_scan = time_fn(lstm_scan, (init, xs), warm_up=warm_up_cycles)
-# torch.testing.assert_close(result_pytorch[0], result_scan[1])
+# For-loop model compiled
+result_forloop_comp, time_initial_forloop_comp, time_run_forloop_comp = time_fn(lstm_forloop_comp, (init, xs), warm_up=warm_up_cycles)
 
-# Implementation with scan - compiled
-result_scan, t_scan_comp = time_fn(torch.compile(lstm_scan, fullgraph=True), (init, xs), warm_up=warm_up_cycles)
-# torch.testing.assert_close(result_pytorch[0], result_scan[1])
+# CUDA model
+result_cuda, time_initial_cuda, time_run_cuda = time_fn(lstm_cuda, (xs.clone(), (init[0].clone().unsqueeze(0), init[1].clone().unsqueeze(0))), warm_up=warm_up_cycles)
 
-print(f"PyTorch {t_pytorch} vs. Scan {t_scan} vs. Scan (Comp.) {t_scan_comp}")
+# Scan model
+result_scan, time_initial_scan, time_run_scan = time_fn(lstm_scan, ((init[0].clone().unsqueeze(0), init[1].clone().unsqueeze(0)), xs.clone()), warm_up=warm_up_cycles)
+
+# Scan model compiled
+result_scan_comp, time_initial_scan_comp, time_run_scan_comp = time_fn(lstm_scan_comp, ((init[0].clone().unsqueeze(0), init[1].clone().unsqueeze(0)), xs.clone()), warm_up=warm_up_cycles)
+
+torch.testing.assert_close(result_forloop, result_forloop_comp)
+torch.testing.assert_close(result_forloop, result_cuda[0])
+torch.testing.assert_close(result_forloop, result_scan[1][:, 0, :])
+torch.testing.assert_close(result_forloop, result_scan_comp[1][:, 0, :])
+
+print(f'T={time_steps}:')
+print(f'Compile times:\n\
+For-Loop        : {time_initial_forloop_comp:.5f}\n\
+Scan            : {time_initial_scan_comp:.5f}\n')
+print(f'Run times       :\n\
+For-Loop        : {time_run_forloop:.5f} \n\
+For-Loop compile: {time_run_forloop_comp:.5f} \n\
+CUDA            : {time_run_cuda:.5f} \n\
+Scan            : {time_run_scan:.5f} \n\
+Scan compile    : {time_run_scan_comp:.5f}')
 
 ###############################################################################
 # Run models with backward
--- benchmarks/dynamo/scan/benchmark_rnn.py
+++ benchmarks/dynamo/scan/benchmark_rnn.py
@@ -6,22 +6,51 @@ from time import perf_counter
 ###############################################################################
 # Auxiliary function for timing
 ###############################################################################
-def time_fn(fn, xs, warm_up=1):
-    for _ in range(warm_up):
-        result = fn(*xs)
+def time_fn(fn, args, warm_up=1):
+    t_initial = -1.
+    for ind in range(warm_up):
+        t_start = perf_counter() 
+        result = fn(*args)
+        t_stop = perf_counter()
+        if ind == 0:
+            t_initial = t_stop - t_start
 
     t_start = perf_counter() 
-    result = fn(*xs)
+    result = fn(*args)
     t_stop = perf_counter()
-    t_ = t_stop - t_start
-    return result, t_
+    t_run = t_stop - t_start
+    return result, t_initial, t_run
 
 ###############################################################################
-# Define the RNN for PyTorch
+# Define the RNN models
 ###############################################################################
-class RNN(torch.nn.Module):
+warm_up_cycles = 3
+# input_size = 15
+input_size = 50
+# hidden_size = 20
+hidden_size = 200
+time_steps = 20
+
+class RNN_forloop(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(RNN_forloop, self).__init__()
+        
+        self.rnn_cell = torch.nn.RNNCell(input_size, hidden_size)
+        
+    # Implementation adopted from 
+    # https://docs.pytorch.org/docs/stable/generated/torch.nn.LSTMCell.html
+    def forward(self, init: torch.Tensor, xs: torch.Tensor):
+        # The input `xs` has the time as the first dimsion
+        output = []
+        for i in range(xs.size()[0]):
+            init = self.rnn_cell(xs[i], init)
+            output.append(init)
+        output = torch.stack(output, dim=0)
+        return output
+
+class RNN_scan(torch.nn.Module):
     def __init__(self, Wih, bih, Whh, bhh):
-        super(RNN, self).__init__()
+        super(RNN_scan, self).__init__()
         self.Wih = Wih.clone()
         self.bih = bih.clone()
         self.Whh = Whh.clone()
@@ -31,50 +60,77 @@ class RNN(torch.nn.Module):
         def rnn_combine(carry, x):
             h = torch.tanh(x @ self.Wih + self.bih + carry @ self.Whh + self.bhh)
             # Needed to not trigger aliasing errors in inductor
-            return h, h.clone()
+            return h + 0., h.clone()
         
-        outs = scan(rnn_combine, init, xs, dim=0)[1]
-        return outs
+        carry, outs = scan(rnn_combine, init, xs, dim=0)
+        return carry, outs
 
-# Define the RNN with pure PyTorch
-rnn_pytorch = torch.nn.RNN(
-            input_size=5,
-            hidden_size=7,
-        )
+# Define the for-loop LSTM model
+rnn_forloop = RNN_forloop(input_size, hidden_size)
+rnn_forloop_comp = torch.compile(rnn_forloop, fullgraph=True)
 
-W_ih = rnn_pytorch.weight_ih_l0.T.clone()
-b_ih = rnn_pytorch.bias_ih_l0.clone()
-W_hh = rnn_pytorch.weight_hh_l0.T.clone()
-b_hh = rnn_pytorch.bias_hh_l0.clone()
 
-rnn_scan = RNN(W_ih,
-               b_ih, 
-               W_hh,
-               b_hh)
+# Define the LSTM using CUDA kernels
+rnn_forloop_state_dict = rnn_forloop.state_dict()
+rnn_cuda_state_dict = {}
+for key, value in rnn_forloop_state_dict.items():
+    new_key = key.replace('rnn_cell.', '') + '_l0'
+    rnn_cuda_state_dict[new_key] = value.clone()
+rnn_cuda = torch.nn.RNN(input_size, hidden_size)
+rnn_cuda.load_state_dict(rnn_cuda_state_dict)
+
+# Define the LSTM model using scan
+W_ih = rnn_cuda.weight_ih_l0.T.clone()
+b_ih = rnn_cuda.bias_ih_l0.clone()
+W_hh = rnn_cuda.weight_hh_l0.T.clone()
+b_hh = rnn_cuda.bias_hh_l0.clone()
+rnn_scan = RNN_scan(
+                    W_ih,
+                    b_ih, 
+                    W_hh,
+                    b_hh
+                )
+rnn_scan_comp = torch.compile(rnn_scan, fullgraph=True)
 
 ###############################################################################
 # Define the inputs
 ###############################################################################
-warm_up_cycles = 3
-xs = torch.randn(10, 5, requires_grad=True)
-init = torch.zeros(7, requires_grad=True)
+xs = torch.randn(time_steps, input_size, requires_grad=True)
+init = torch.zeros(hidden_size, requires_grad=True)
 
 ###############################################################################
 # Run models forward (inference-only)
 ###############################################################################
+# For-loop model
+result_forloop, time_initial_forloop, time_run_forloop = time_fn(rnn_forloop, (init, xs), warm_up=warm_up_cycles)
 
-# Native PyTorch implementation
-result_pytorch, t_pytorch = time_fn(rnn_pytorch, (xs,), warm_up=warm_up_cycles)
+# For-loop model compiled
+result_forloop_comp, time_initial_forloop_comp, time_run_forloop_comp = time_fn(rnn_forloop_comp, (init, xs), warm_up=warm_up_cycles)
 
-# Implementation with scan
-result_scan, t_scan = time_fn(rnn_scan, (init, xs), warm_up=warm_up_cycles)
-torch.testing.assert_close(result_pytorch[0], result_scan)
+# CUDA model
+result_cuda, time_initial_cuda, time_run_cuda = time_fn(rnn_cuda, (xs.clone(), init.clone().unsqueeze(0)), warm_up=warm_up_cycles)
 
-# Implementation with scan - compiled
-result_scan, t_scan_comp = time_fn(torch.compile(rnn_scan, fullgraph=True), (init, xs), warm_up=warm_up_cycles)
-torch.testing.assert_close(result_pytorch[0], result_scan)
+# Scan model
+result_scan, time_initial_scan, time_run_scan = time_fn(rnn_scan, (init.clone().unsqueeze(0), xs.clone()), warm_up=warm_up_cycles)
 
-print(f"PyTorch {t_pytorch} vs. Scan {t_scan} vs. Scan (Comp.) {t_scan_comp}")
+# Scan model compiled
+result_scan_comp, time_initial_scan_comp, time_run_scan_comp = time_fn(rnn_scan_comp, (init.clone().unsqueeze(0), xs.clone()), warm_up=warm_up_cycles)
+
+torch.testing.assert_close(result_forloop, result_forloop_comp)
+torch.testing.assert_close(result_forloop, result_cuda[0])
+torch.testing.assert_close(result_forloop, result_scan[1][:, 0, :])
+torch.testing.assert_close(result_forloop, result_scan_comp[1][:, 0, :])
+
+print(f'T={time_steps}:')
+print(f'Compile times:\n\
+For-Loop        : {time_initial_forloop_comp:.5f}\n\
+Scan            : {time_initial_scan_comp:.5f}\n')
+print(f'Run times       :\n\
+For-Loop        : {time_run_forloop:.5f} \n\
+For-Loop compile: {time_run_forloop_comp:.5f} \n\
+CUDA            : {time_run_cuda:.5f} \n\
+Scan            : {time_run_scan:.5f} \n\
+Scan compile    : {time_run_scan_comp:.5f}')
 
 ###############################################################################
 # Run models with backward
