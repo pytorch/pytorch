1803d40c995e72a5993ee0940ec38bca760978b5
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-10-14 03:52:48 +0000

    Reapply "[export] Turn on install_free_tensors flag (#164691)" (#165353)
    This reverts commit 9166f6120f63e2d5d76e6ccdbfccb8d6e41cbb43.
    
    Reverted https://github.com/pytorch/pytorch/pull/165353 on behalf of https://github.com/seemethere due to This is causing merge conflicts since a dependent PR wasn't reverted ([comment](https://github.com/pytorch/pytorch/pull/165353#issuecomment-3400006587))

--- test/dynamo/test_export.py
+++ test/dynamo/test_export.py
@@ -3147,7 +3147,6 @@ def forward(self, x):
             gm, _ = torch._dynamo.export(f, aten_graph=True)(*example_inputs)
             self.assertEqual(gm(*example_inputs), f(*example_inputs))
 
-    @unittest.expectedFailure  # TODO: Not sure why dynamo creates a new inputs for self.a
     def test_sum_param(self):
         # Setting a new attribute inside forward()
         class Foo(torch.nn.Module):
@@ -3538,24 +3537,16 @@ class GraphModule(torch.nn.Module):
             [[], [], [], []],
         )
 
-    def test_invalid_input_global(self) -> None:
+    def test_input_global(self) -> None:
         global bulbous_bouffant
         bulbous_bouffant = torch.randn(3)
 
         def f(y):
             return bulbous_bouffant + y
 
-        self.assertExpectedInlineMunged(
-            UserError,
-            lambda: torch._dynamo.export(f)(torch.randn(3)),
-            """\
-G['bulbous_bouffant'], accessed at:
-  File "test_export.py", line N, in f
-    return bulbous_bouffant + y
-""",
-        )
+        torch._dynamo.export(f)(torch.randn(3))
 
-    def test_invalid_input_global_multiple_access(self) -> None:
+    def test_input_global_multiple_access(self) -> None:
         global macademia
         macademia = torch.randn(3)
 
@@ -3569,33 +3560,17 @@ G['bulbous_bouffant'], accessed at:
             y = g(y)
             return macademia + y
 
-        # NB: This doesn't actually work (it only reports the first usage),
-        # but I'm leaving the test here in case we fix it later
-        self.assertExpectedInlineMunged(
-            UserError,
-            lambda: torch._dynamo.export(f)(torch.randn(3)),
-            """\
-G['macademia'], accessed at:
-  File "test_export.py", line N, in f
-    y = g(y)
-  File "test_export.py", line N, in g
-    y = macademia + y
-""",
-        )
+        torch._dynamo.export(f)(torch.randn(3))
 
-    def test_invalid_input_nonlocal(self) -> None:
+    def test_input_nonlocal(self) -> None:
         arglebargle = torch.randn(3)
 
         def f(y):
             return arglebargle + y
 
-        self.assertExpectedInlineMunged(
-            UserError,
-            lambda: torch._dynamo.export(f)(torch.randn(3)),
-            """L['arglebargle'], a closed over free variable""",
-        )
+        torch._dynamo.export(f)(torch.randn(3))
 
-    def test_invalid_input_unused_nonlocal_ok(self) -> None:
+    def test_input_unused_nonlocal_ok(self) -> None:
         arglebargle = torch.randn(3)
 
         def f(y):
