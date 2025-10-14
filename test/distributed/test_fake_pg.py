e15802a5970e843c1d7b03f0563d8b1b39015029
Catherine Lee <csl@fb.com>
2025-10-14 15:26:08 -0700

    tc

--- test/distributed/test_fake_pg.py
+++ test/distributed/test_fake_pg.py
@@ -275,9 +275,7 @@ class TestFakePG(TestCase):
 
         with self.assertRaisesRegex(
             RuntimeError,
-            r"FakeProcessGroup cannot be constructed directly\. "
-            r"Use torch\.distributed\.init_process_group\(backend='fake'\) instead to ensure "
-            r"proper dispatch system integration\.",
+            r"No constructor defined"
         ):
             fake_pg = FakeProcessGroup(rank=0, world_size=3)
 
