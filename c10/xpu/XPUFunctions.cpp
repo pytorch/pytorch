9c44444c942fccb42352041e13ce583cb770e1f0
Yu, Guangye <guangye.yu@intel.com>
2025-10-16 16:55:52 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/xpu/XPUFunctions.cpp
+++ c10/xpu/XPUFunctions.cpp
@@ -131,8 +131,8 @@ inline void initGlobalDevicePoolState() {
           "The detected GPU (",
           device->get_info<sycl::info::device::name>(),
           ") is not officially supported by PyTorch XPU. Running workloads on this device may result in ",
-          "unexpected behavior, reduced performance, or runtime errors.\n",
-          "For stable and fully supported execution, please use GPUs based on Intel Arc Alchemist series or newer.\n",
+          "unexpected behavior, reduced performance, or even runtime errors.\n",
+          "For stable and fully supported execution, please use GPUs based on Intel Arc (Alchemist) series or newer.\n",
           "Refer to the hardware prerequisites for more information: ",
           "https://github.com/pytorch/pytorch/blob/main/docs/source/notes/get_start_xpu.rst#hardware-prerequisite");
     }
