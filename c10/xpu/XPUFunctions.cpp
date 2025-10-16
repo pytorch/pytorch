f90ecb4ba70fbcdd5dd1e89ae7e647c28345d04c
Yu, Guangye <guangye.yu@intel.com>
2025-10-16 16:27:26 +0000

    Update
    [ghstack-poisoned]

--- c10/xpu/XPUFunctions.cpp
+++ c10/xpu/XPUFunctions.cpp
@@ -135,21 +135,14 @@ inline void initGlobalDevicePoolState() {
     }
   }
 
-#if defined(_WIN32) && SYCL_COMPILER_VERSION < 20250000
-  // The default context feature is disabled by default on Windows for SYCL
-  // compiler versions earlier than 2025.0.0.
-  std::vector<sycl::device> deviceList;
-  for (auto it = gDevicePool.devices.begin(); it != gDevicePool.devices.end();
-       ++it) {
-    deviceList.push_back(*(*it));
-  }
-  gDevicePool.context = std::make_unique<sycl::context>(deviceList);
-#else
   // The default context is utilized for each Intel GPU device, allowing the
   // retrieval of the context from any GPU device.
   const auto& platform = gDevicePool.devices[0]->get_platform();
   gDevicePool.context = std::make_unique<sycl::context>(
+#if SYCL_COMPILER_VERSION >= 20250200
       platform.khr_get_default_context());
+#else
+      platform.ext_oneapi_get_default_context());
 #endif
 }
 
