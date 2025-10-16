8cf0fc9575bcdee8a0c13d94d6d3dbc7de0406c9
William Wen <williamwen@meta.com>
2025-10-15 17:02:47 -0700

    [dynamo, nested graph breaks] add dynamo/test_misc.py nested graph break tests
    [ghstack-poisoned]

--- test/dynamo/test_nested_graph_breaks_wrapped.py
+++ test/dynamo/test_nested_graph_breaks_wrapped.py
@@ -8,10 +8,11 @@ from torch._dynamo.testing import make_test_cls_with_patches
 
 
 try:
-    from . import test_activation_checkpointing, test_ctx_manager
+    from . import test_activation_checkpointing, test_ctx_manager, test_misc
 except ImportError:
     import test_activation_checkpointing
     import test_ctx_manager
+    import test_misc
 
 test_classes = {}
 
@@ -40,6 +41,7 @@ tests = [
         test_activation_checkpointing, "ActivationCheckpointingViaTagsTestsCUDA", None
     ),
     test_ctx_manager.CtxManagerTests,
+    test_misc.MiscTests,
 ]
 
 test = None
@@ -50,7 +52,39 @@ for test in tests:
 
 del test, tests
 
-xfails = []
+xfails = [
+    # variable naming issues
+    NestedGraphBreaksMiscTests.test_flat_name_to_original_fqn_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_compare_shapes_with_constant_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_guard_failure_fn2_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_guard_failure_fn_shape_control_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_guard_failure_fn_tensor_iter_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_guard_filter_fn_by_name_and_value_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_guard_sym_node_fstring_when_used_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_symint_as_device_kwarg_multi_gpu_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_sys_modules_nested_graph_breaks,  # noqa: F821
+    # counters["graph_breaks"] issues
+    NestedGraphBreaksMiscTests.test_data_ptr_graph_break_aten_nested_graph_breaks,  # noqa: F821
+    # nested graph break removes duplicate graph break
+    NestedGraphBreaksMiscTests.test_duplicate_graph_break_log_nested_graph_breaks,  # noqa: F821
+    # doesn't work due to debug_force_nested_calls wrapping the top frame
+    NestedGraphBreaksMiscTests.test_dynamo_cache_invalidate_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_dynamo_cache_move_to_front_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_dynamo_reset_clears_cache_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_fail_on_recompile_error_message_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_get_cache_entry_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_getattrvariable_as_python_constant_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_precompile_entries_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_precompile_entry_hit_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_precompile_fail_on_recompile_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_torch_guards_stack_frame_register_inlining_deep_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_torch_guards_stack_frame_register_inlining_nested_graph_breaks,  # noqa: F821
+    # differing op_count
+    NestedGraphBreaksMiscTests.test_nested_closure_nested_graph_breaks,  # noqa: F821
+    NestedGraphBreaksMiscTests.test_return_nested_function_nested_graph_breaks,  # noqa: F821
+    # unknown
+    NestedGraphBreaksMiscTests.test_inspect_signature_bind_non_user_function_nested_graph_breaks,  # noqa: F821
+]
 
 case = None
 
