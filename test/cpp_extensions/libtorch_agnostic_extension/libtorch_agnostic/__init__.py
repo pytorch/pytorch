a1c5d1198c5594bb2444119945488ebfa39aaf44
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2025-10-16 21:18:03 -0700

    Update on "[Testing] Make TestLibtorchAgnostic run on windows"
    From https://github.com/pytorch/vision/blob/faffd5cf673615583da6517275e361cb3dbc77e6/torchvision/csrc/vision.cpp#L12-L18
    
    
    
    
    [ghstack-poisoned]

--- test/cpp_extensions/libtorch_agnostic_extension/libtorch_agnostic/__init__.py
+++ test/cpp_extensions/libtorch_agnostic_extension/libtorch_agnostic/__init__.py
@@ -1,16 +1,25 @@
 import ctypes
+import sys
 from pathlib import Path
 
 import torch
 
 
-so_files = list(Path(__file__).parent.glob("_C*.so"))
-assert len(so_files) == 1, f"Expected one _C*.so file, found {len(so_files)}"
+if sys.platform == "win32":
+    lib_files = list(Path(__file__).parent.glob("_C*.dll"))
+    lib_extension = "dll"
+else:
+    lib_files = list(Path(__file__).parent.glob("_C*.so"))
+    lib_extension = "so"
+
+assert len(lib_files) == 1, (
+    f"Expected one _C*.{lib_extension} file, found {len(lib_files)}"
+)
 
 # use ctypes.CDLL instead of load_library to be able to test the unload logic
 # below code is reduced from the load_library code
 with torch._ops.dl_open_guard():
-    loaded_lib = ctypes.CDLL(so_files[0])
+    loaded_lib = ctypes.CDLL(lib_files[0])
 
 from . import ops
 
