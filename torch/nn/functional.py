0da0839e5f42e078693d55edaac25882e6a6d88d
Xuanteng Huang <xuanteng.huang@outlook.com>
2025-10-19 13:59:43 +0800

    add `dtype` for `F.one_hot`

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -5484,7 +5484,7 @@ Example::
 one_hot = _add_docstr(
     torch._C._nn.one_hot,
     r"""
-one_hot(tensor, num_classes=-1) -> LongTensor
+one_hot(tensor, num_classes=-1, dtype=torch.long) -> LongTensor
 
 Takes LongTensor with index values of shape ``(*)`` and returns a tensor
 of shape ``(*, num_classes)`` that have zeros everywhere except where the
@@ -5501,6 +5501,8 @@ Arguments:
     num_classes (int, optional):  Total number of classes. If set to -1, the number
         of classes will be inferred as one greater than the largest class
         value in the input tensor. Default: -1
+    dtype (:class:`torch.dtype`): the desired output dtype.
+        Default: ``torch.int64``
 
 Returns:
     LongTensor that has one more dimension with 1 values at the
