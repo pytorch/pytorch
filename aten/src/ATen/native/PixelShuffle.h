29ec432f3ecc6aadf9f060af37f81d89ec6f6523
arkadip-maitra <amaitra@redhat.com>
2025-10-20 15:13:58 +0530

    robust overflow check

--- aten/src/ATen/native/PixelShuffle.h
+++ aten/src/ATen/native/PixelShuffle.h
@@ -12,10 +12,10 @@ inline void check_pixel_shuffle_shapes(const Tensor& self, int64_t upscale_facto
               "pixel_shuffle expects a positive upscale_factor, but got ",
               upscale_factor);
   int64_t c = self.size(-3);
-  uint64_t upscale_factor_squared;
-  TORCH_CHECK_VALUE(!(c10::mul_overflows(upscale_factor, upscale_factor, &upscale_factor_squared)),
+  TORCH_CHECK_VALUE(upscale_factor <= std::numeric_limits<decltype(upscale_factor)>::max() / upscale_factor,
         "upscale factor is too large, (upscale_factor)^2 overflowed: upscale_factor=", upscale_factor);
-  TORCH_CHECK(c % static_cast<int64_t>(upscale_factor_squared) == 0,
+  int64_t upscale_factor_squared = upscale_factor * upscale_factor;
+  TORCH_CHECK(c % upscale_factor_squared == 0
               "pixel_shuffle expects its input's 'channel' dimension to be divisible by the square of "
               "upscale_factor, but input.size(-3)=", c, " is not divisible by ", upscale_factor_squared);
 }
