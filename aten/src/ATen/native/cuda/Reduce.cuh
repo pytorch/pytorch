602ace5eb4f08ebb9e04ccf13f137160b7d6e8aa
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-10-20 03:06:52 +0000

    Revert "[ATen] Fix CUDA reduction warp shuffle order (#164790)"
    This reverts commit 36371b8ec7a1baed255c18451b2c716386a54c95.
    
    Reverted https://github.com/pytorch/pytorch/pull/164790 on behalf of https://github.com/clee2000 due to was reverted due to failing internal tests after merge D84992607 ([comment](https://github.com/pytorch/pytorch/pull/164790#issuecomment-3420373755))

--- aten/src/ATen/native/cuda/Reduce.cuh
+++ aten/src/ATen/native/cuda/Reduce.cuh
@@ -653,14 +653,8 @@ struct ReduceOp {
     }
 
     __syncthreads();
-    // Intra-warp reduction, fix CUDA to have offset decreasing for better numerics
-    // matching Triton, etc.
-    // todo for AMD
-    #ifdef USE_ROCM
+
     for (int offset = 1; offset < dim_x; offset <<= 1) {
-    #else
-    for (int offset = dim_x >> 1; offset > 0; offset >>= 1) {
-    #endif
       #pragma unroll
       for (int i = 0; i < output_vec_size; i++) {
         arg_t other = ops.warp_shfl_down(value[i], offset);
