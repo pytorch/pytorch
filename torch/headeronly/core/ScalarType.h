4fae6968b133129b0a62ee7e3f491a5a51390d4e
Pearu Peterson <pearu.peterson@gmail.com>
2025-10-21 22:12:24 +0300

    Move toString(ScalarType) and ScalarType ostream operator to headeronly (#164405) (#166018)
    This PR is created to replace the reverted PR https://github.com/pytorch/pytorch/pull/164405
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166018
    Approved by: https://github.com/janeyx99

--- torch/headeronly/core/ScalarType.h
+++ torch/headeronly/core/ScalarType.h
@@ -299,6 +299,25 @@ using ScalarTypeToCPPTypeT = typename ScalarTypeToCPPType<N>::type;
 
 } // namespace impl
 
+inline const char* toString(ScalarType t) {
+#define DEFINE_CASE(_, name) \
+  case ScalarType::name:     \
+    return #name;
+
+  switch (t) {
+    AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_CASE)
+    default:
+      return "UNKNOWN_SCALAR";
+  }
+#undef DEFINE_CASE
+}
+
+inline std::ostream& operator<<(
+    std::ostream& stream,
+    c10::ScalarType scalar_type) {
+  return stream << toString(scalar_type);
+}
+
 } // namespace c10
 
 namespace torch::headeronly {
@@ -309,4 +328,6 @@ using c10::ScalarType;
 namespace impl {
 using c10::impl::ScalarTypeToCPPTypeT;
 } // namespace impl
+using c10::toString;
+using c10::operator<<;
 } // namespace torch::headeronly
