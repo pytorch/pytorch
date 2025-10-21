43e89471fe999dbf16339eb6c27e92cb8ea53383
arkadip-maitra <amaitra@redhat.com>
2025-10-21 12:21:15 +0530

    missed a ,

--- aten/src/ATen/native/PixelShuffle.h
+++ aten/src/ATen/native/PixelShuffle.h
@@ -15,7 +15,7 @@ inline void check_pixel_shuffle_shapes(const Tensor& self, int64_t upscale_facto
   TORCH_CHECK_VALUE(upscale_factor <= std::numeric_limits<decltype(upscale_factor)>::max() / upscale_factor,
         "upscale factor is too large, (upscale_factor)^2 overflowed: upscale_factor=", upscale_factor);
   int64_t upscale_factor_squared = upscale_factor * upscale_factor;
-  TORCH_CHECK(c % upscale_factor_squared == 0
+  TORCH_CHECK(c % upscale_factor_squared == 0,
               "pixel_shuffle expects its input's 'channel' dimension to be divisible by the square of "
               "upscale_factor, but input.size(-3)=", c, " is not divisible by ", upscale_factor_squared);
 }
