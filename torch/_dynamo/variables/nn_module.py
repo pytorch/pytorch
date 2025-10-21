771170807b1ed2c9278adfd0bbd1381c47d9e1e4
Animesh Jain <anijain@umich.edu>
2025-10-20 18:01:55 -0700

    [dynamo][nn_module] Replace UserFunctionVariable with VariableTracker build (#165708)
    Audit: To prevent future issues with functools.partial or callable objects.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/165708
    Approved by: https://github.com/Lucaskabela

--- torch/_dynamo/variables/nn_module.py
+++ torch/_dynamo/variables/nn_module.py
@@ -952,7 +952,7 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
             # will not reflect the mutations. So, trace through the `__iter__`
             # function to reflect any tracked mutations.
             return tx.inline_user_function_return(
-                variables.UserFunctionVariable(fn),
+                VariableTracker.build(tx, fn),
                 [
                     self,
                 ],
@@ -1021,9 +1021,17 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
             else nullcontext()
         )
         with ctx:
-            return variables.UserFunctionVariable(fn, source=source).call_function(
-                tx, [self] + list(args), kwargs
-            )
+            if not isinstance(fn, (types.FunctionType, torch.jit.ScriptFunction)):
+                fn_vt = VariableTracker.build(tx, fn, source=source)
+                return fn_vt.call_function(tx, [self] + list(args), kwargs)
+            else:
+                # Ideally we would have just used VariableTracker.build(tx, fn,
+                # source=source) but that introduces guard on the
+                # `forward.__code__` object. Given that we already guard on the
+                # forward not present in generic dict, we dont need this guard.
+                return variables.UserFunctionVariable(fn, source=source).call_function(
+                    tx, [self] + list(args), kwargs
+                )
 
     def call_method(
         self,
@@ -1039,9 +1047,8 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
             else:
                 source = None
 
-            return variables.UserFunctionVariable(fn, source=source).call_function(
-                tx, [self] + list(args), kwargs
-            )
+            fn_vt = VariableTracker.build(tx, fn, source=source)
+            return fn_vt.call_function(tx, [self] + list(args), kwargs)
 
         if name not in getattr(self.value, "__dict__", {}):
             try:
@@ -1051,11 +1058,8 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
 
             if isinstance(method, staticmethod):
                 source = AttrSource(self.get_source_by_walking_mro(name), "__func__")
-                return tx.inline_user_function_return(
-                    variables.UserFunctionVariable(method.__func__, source=source),
-                    args,
-                    kwargs,
-                )
+                fn_vt = VariableTracker.build(tx, method.__func__, source=source)
+                return fn_vt.call_function(tx, args, kwargs)
 
             if (
                 hasattr(method, "__code__")
@@ -1115,11 +1119,8 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
             ) or method is torch.nn.Module.__delattr__:
                 # Trace through __delattr__ to track mutations on the module
                 # members like `_modules``.
-                return tx.inline_user_function_return(
-                    variables.UserFunctionVariable(torch.nn.Module.__delattr__),
-                    [self, args[0]],
-                    kwargs,
-                )
+                fn_vt = VariableTracker.build(tx, torch.nn.Module.__delattr__)
+                return fn_vt.call_function(tx, [self, args[0]], kwargs)
 
         return super().call_method(tx, name, args, kwargs)
 
