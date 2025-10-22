f352bc5e954b669a9c77ae1ca5fe2e11f43b673e
Colin L Reliability Rice <clr@meta.com>
2025-10-22 08:49:59 -0700

    inductor_provenance: Correctly handle null provenance (#166019)
    Summary:
    
    If the provenance is null, we're getting crashes of the form
    ```
    [trainers0]:E1021 10:51:31.990525  2752 PythonApi.h:87] Exception caught in
    GeneratedDynamoCompileLoggerConfig: <class
    'dsi.logger.py3.GeneratedDynamoCompile.LogEntry.thrift_types.GeneratedDynamoCompileLogEntryThriftBase'>:
    error initializing Thrift struct field 'inductor_provenance_thrift_safe':
    Cannot create internal string data representation. Expected type <class 'str'>,
    got: <class 'NoneType'>.
    ```
    
    Also fixed a type signature that wasn't being enforced. (It's still not
    enforced, but it's accurate).
    
    Test Plan: Added a new test which reproduces the logging issue
    
    Reviewed By: ppanchalia
    
    Differential Revision: D85173596

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -1544,9 +1544,10 @@ class _InProcessFxCompile(FxCompile):
                             },
                             payload_fn=lambda: inductor_kernel_stack_trace_str,
                         )
-                        get_metrics_context().add_to_set(
-                            "inductor_provenance", inductor_kernel_stack_trace_str
-                        )
+                        if inductor_kernel_stack_trace_str:
+                            get_metrics_context().add_to_set(
+                                "inductor_provenance", inductor_kernel_stack_trace_str
+                            )
 
                     node_runtimes = None
                     if inductor_metrics_log.isEnabledFor(logging.INFO):
