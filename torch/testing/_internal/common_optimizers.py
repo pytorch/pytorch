035f30c46f0ac8ea1896eb54b7881d7503ad4be4
xinan.lin <xinan.lin@intel.com>
2025-10-21 17:59:40 -0700

    [Inductor UT] Enable more UTs for Intel GPU.

--- torch/testing/_internal/common_optimizers.py
+++ torch/testing/_internal/common_optimizers.py
@@ -46,6 +46,7 @@ from torch.testing._internal.common_utils import (
     skipIfXpu,
     TEST_WITH_TORCHDYNAMO,
 )
+from torch.testing._internal.inductor_utils import GPU_TYPE
 from torch.utils._foreach_utils import _get_foreach_kernels_supported_devices
 
 
@@ -369,7 +370,7 @@ def optim_inputs_func_adadelta(device, dtype=None):
         OptimizerInput(
             params=None, kwargs={"rho": 0.95, "weight_decay": 0.9}, desc="rho"
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_adadelta(device, dtype):
@@ -569,7 +570,7 @@ def optim_inputs_func_adam(device, dtype=None):
                 desc="amsgrad",
             ),
         ]
-        + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+        + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
         + (mps_supported_configs if _get_device_type(device) == "mps" else [])
     )
     if dtype == torch.float16:
@@ -650,7 +651,7 @@ def optim_error_inputs_func_adam(device, dtype):
                 error_regex=r"betas\[0\] as a Tensor is not supported for capturable=False and foreach=True",
             ),
         ]
-    if _get_device_type(device) == "cuda":
+    if _get_device_type(device) == GPU_TYPE:
         sample_tensor = torch.empty((), device=device, dtype=dtype)
         error_inputs += [
             ErrorOptimizerInput(
@@ -721,7 +722,7 @@ def optim_inputs_func_adamax(device, dtype=None):
             kwargs={"weight_decay": 0.1, "maximize": True},
             desc="maximize, weight_decay",
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_adamax(device, dtype):
@@ -792,7 +793,7 @@ def optim_inputs_func_asgd(device, dtype=None):
             kwargs={"weight_decay": 0.1, "maximize": True},
             desc="maximize, nonzero weight_decay",
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_asgd(device, dtype):
@@ -974,7 +975,7 @@ def optim_inputs_func_nadam(device, dtype=None):
             kwargs={"weight_decay": 0.1, "maximize": True},
             desc="maximize",
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_nadam(device, dtype):
@@ -1052,7 +1053,7 @@ def optim_inputs_func_radam(device=None, dtype=None):
             kwargs={"weight_decay": 0.1, "maximize": True},
             desc="maximize",
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_radam(device, dtype):
@@ -1137,7 +1138,7 @@ def optim_inputs_func_rmsprop(device, dtype=None):
             },
             desc="maximize, centered, weight_decay, w/ momentum",
         ),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_rmsprop(device, dtype):
@@ -1179,7 +1180,7 @@ def optim_inputs_func_rprop(device, dtype=None):
             desc="non-default step_sizes",
         ),
         OptimizerInput(params=None, kwargs={"maximize": True}, desc="maximize"),
-    ] + (cuda_supported_configs if _get_device_type(device) == "cuda" else [])
+    ] + (cuda_supported_configs if _get_device_type(device) == GPU_TYPE else [])
 
 
 def optim_error_inputs_func_rprop(device, dtype):
@@ -1671,7 +1672,7 @@ optim_db: list[OptimizerInfo] = [
             "maximize",
             "capturable",
         ),
-        supports_fused_on=("cpu", "cuda", "mps"),
+        supports_fused_on=("cpu", "cuda", "xpu", "mps"),
         decorators=(
             # Expected floating point error between fused and compiled forloop
             DecorateInfo(
@@ -2161,6 +2162,7 @@ optim_db: list[OptimizerInfo] = [
         supports_fused_on=(
             "cpu",
             "cuda",
+            "xpu",
             "mps",
         ),
         skips=(
