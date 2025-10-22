815da6e6e2a55671a36817195c36cab4635c51e5
Isuru Fernando <ifernando@openteams.com>
2025-10-22 18:00:35 +0000

    [inductor] print 0.0 as 0 for triton (#164291)
    Fixes https://github.com/pytorch/pytorch/issues/164157
    Fixes https://github.com/pytorch/pytorch/issues/164086
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/164291
    Approved by: https://github.com/bobrenjc93, https://github.com/mlazos

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -8423,6 +8423,22 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
             self.assertEqual(fn(x[0:]), x[16:][:16])
             self.assertEqual(fn(x[128:]), x[128 + 16 :][:16])
 
+    def test_index_float_zero(self):
+        def fn(arg0, arg1, arg2):
+            t1 = torch.tanh(arg0)
+            t2 = t1.clone()
+            t2.fill_(arg1.item())
+            t3 = torch.clamp(t2, 0, arg2.size(0) - 1).to(torch.long)
+            return torch.nn.functional.embedding(t3, arg2)
+
+        arg0 = torch.randint(0, 1000, [47], dtype=torch.int64, device=self.device)
+        arg1 = torch.randint(0, 1000, [], dtype=torch.int64, device=self.device)
+        arg2 = torch.rand([256, 88], dtype=torch.float16, device=self.device)
+
+        cfn = torch.compile(fullgraph=True, dynamic=True)(fn)
+
+        self.assertEqual(fn(arg0, arg1, arg2), cfn(arg0, arg1, arg2))
+
     # from GPT2ForSequenceClassification
     @skip_if_gpu_halide
     def test_index_tensor(self):
