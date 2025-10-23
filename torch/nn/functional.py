52fdc245d2972af707b450f4bfb7bb33ae6b2054
Daniel Ohayon <danielohayon444@gmail.com>
2025-10-23 10:56:59 +0300

    Fix device mismatch in sdpa docstring example

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -5936,7 +5936,7 @@ scaled_dot_product_attention = _add_docstr(
             attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
             if is_causal:
                 assert attn_mask is None
-                temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+                temp_mask = torch.ones(L, S, device=query.device, dtype=torch.bool).tril(diagonal=0)
                 attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
 
             if attn_mask is not None:
