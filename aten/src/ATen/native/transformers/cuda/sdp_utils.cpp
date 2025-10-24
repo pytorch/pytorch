2f9009df848608e747d323c0f2be608c619dc150
Eddie Yan <eddiey@nvidia.com>
2025-10-24 21:36:12 +0000

    next

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -76,7 +76,7 @@ bool priority_order_init_ = false;
 // TODO(eqy): more benchmarking to determine whether this should include sm86/89
 // Needs to be kept in-sync with test_fused_chocie in test_transformers.py
 bool check_prefer_cudnn_attention() {
-  static const bool prefer_cudnn = c10::utils::check_env("TORCH_CUDNN_SDPA_PREFERRED") != false;
+  static const bool prefer_cudnn = c10::utils::check_env("TORCH_CUDNN_SDPA_DEPRIORITIZED") != true;
   if (!prefer_cudnn) {
     return false;
   }
