d3baca1686d465c3a7601e26e770b520c5075ae7
jmaczan <jedrzejpawel@maczan.pl>
2025-10-24 21:03:40 +0200

    Add test

--- torch/utils/_sympy/value_ranges.py
+++ torch/utils/_sympy/value_ranges.py
@@ -514,6 +514,10 @@ class SymPyValueRangeAnalysis:
         return ValueRanges.coordinatewise_increasing_map(a, b, sympy.And)
 
     @staticmethod
+    def xor_(a, b):
+        return ValueRanges.coordinatewise_increasing_map(a, b, sympy.Xor)
+
+    @staticmethod
     def _bool_to_int(x):
         if x.is_singleton():
             return ValueRanges.wrap(sympy.Integer(1 if x.lower else 0))
@@ -547,6 +551,30 @@ class SymPyValueRangeAnalysis:
         a, b = ValueRanges.wrap(a), ValueRanges.wrap(b)
         if a.is_bool and b.is_bool:
             return cls.or_(a, b)
+        if a.is_bool:
+            a = cls._bool_to_int(a)
+        if b.is_bool:
+            b = cls._bool_to_int(b)
+        upper = max(a.upper, b.upper)
+        if upper == 0:
+            upper = 0
+        elif upper > 0 and upper != sympy.oo and upper != int_oo:
+            # If both upper bounds are positive, then the largest
+            # possible value is 01...1, so we need to find
+            # next largest power of 2 (exclusive), minus 1
+            try:
+                upper = (1 << int(upper).bit_length()) - 1
+            except Exception:
+                upper = int_oo
+        elif upper < 0:
+            upper = -1
+        return ValueRanges(min(a.lower, b.lower), upper)
+
+    @classmethod
+    def bitwise_xor(cls, a, b):
+        a, b = ValueRanges.wrap(a), ValueRanges.wrap(b)
+        if a.is_bool and b.is_bool:
+            return cls.xor_(a, b)
         if a.is_bool:
             a = cls._bool_to_int(a)
         if b.is_bool:
