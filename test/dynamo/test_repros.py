32fe4f681e2bfb2fdecf20027e29e1aeb6ad5297
William Wen <williamwen@meta.com>
2025-10-27 13:02:50 -0700

    [dynamo] fix keyerror in resume_execution (again) (#166040)
    Fixes https://github.com/pytorch/pytorch/issues/166176
    
    The error I attempted to fix in https://github.com/pytorch/pytorch/pull/162318 was still appearing internally.
    
    Surprised that this wasn't caught anywhere 😰
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166040
    Approved by: https://github.com/Lucaskabela
    ghstack dependencies: #166036

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -7260,46 +7260,6 @@ def forward(self, s77 : torch.SymInt, s27 : torch.SymInt, L_x_ : torch.Tensor):
             fn(torch.ones(3)), torch.compile(fn, backend="eager")(torch.ones(3))
         )
 
-    def test_311_resume_block_keyerror(self):
-        # https://github.com/pytorch/pytorch/issues/162313
-        flag = True
-
-        def fn(x):
-            x = x + 1
-            torch._dynamo.graph_break()
-            x = x + 2
-            if flag:
-                with torch.no_grad():
-                    torch._dynamo.graph_break()
-                x = x + 4
-            else:
-                with torch.no_grad():
-                    torch._dynamo.graph_break()
-                x = x + 8
-            return x + 16
-
-        inp = torch.ones(3)
-        opt_fn = torch.compile(fn, backend="eager")
-        self.assertEqual(fn(inp), opt_fn(inp))
-        flag = False
-        self.assertEqual(fn(inp), opt_fn(inp))
-
-    def test_store_attr_graph_break_key_error(self):
-        # STORE_ATTR on dummy should result in graph break
-        def dummy():
-            pass
-
-        def fn(x):
-            x = x + 2
-            with torch.no_grad():
-                dummy.attr1 = x
-            return x + 4
-
-        inp = torch.ones(3)
-        opt_fn = torch.compile(fn, backend="eager")
-        self.assertEqual(fn(inp), opt_fn(inp))
-        self.assertGreater(len(torch._dynamo.utils.counters["graph_break"]), 0)
-
     def test_cells_unsupported_step_exception(self):
         # This error happened because:
         #  - we were generating cells into a list on the stack
