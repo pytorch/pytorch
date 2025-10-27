8e1386f8c4e7c17904ec428b2572056475ce4048
can-gaa-hou <jiahaochen535@gmail.com>
2025-10-27 15:50:20 +0800

    Fix issues in comment

--- c10/core/DeviceCapability.h
+++ c10/core/DeviceCapability.h
@@ -7,11 +7,37 @@
 
 namespace c10 {
 
-  constexpr size_t NUMBER_OF_DEVICE_CAPABILITIES = NumScalarTypes;
+constexpr size_t NUMBER_OF_DEVICE_CAPABILITIES = NumScalarTypes;
 
 // Generate bitfields for each scalar type
 #define DEFINE_SCALAR_TYPE(_1, n) unsigned int has_##n : 1;
 
+/**
+ * @brief DeviceCapability represents the the common capabilities that all devices should support.
+ *
+ * This struct provides a compact way to represent the common capabilities that all devices should support.
+ * Includes the following capabilities:
+ * - Supported data types
+ *
+ * Purpose
+ * - Enable runtime checking of device capability support
+ * - Provide efficient storage for capability information
+ * - Support dynamic capability queries for device selection
+ * - Enable device-specific optimizations based on supported capabilities
+ *
+ * Contract
+ * - Each bitfield represents support for one device capability
+ * - Bit value 1 means the capability is supported, 0 means not supported
+ * - The struct is initialized with all capabilities enabled by default
+ *
+ * @note Adding New Capabilities
+ *
+ * 1. Define the new capability in the `DeviceCapability` struct
+ * 2. Update the support of the new capability in each accelerator implementation
+ * 3. Create a new method to check if the capability is supported
+ * 4. Update to length of NUMBER_OF_DEVICE_CAPABILITIES
+ * 5. Add the new capability to the returned PyObject Dictionary
+ */
 struct C10_API DeviceCapability {
   union {
     struct {
