65868156c613521e9635eb3f8eb3f404826ac1f9
Animesh Jain <anijain@umich.edu>
2025-10-27 15:37:15 -0700

    [dynamo] Guard selectively on the torch APIs (#166329)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166329
    Approved by: https://github.com/Lucaskabela

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -65,6 +65,7 @@ from ..utils import (
     guard_if_dyn,
     has_torch_function,
     hashable,
+    is_wrapper_or_member_descriptor,
     product,
     proxy_args_kwargs,
     unwrap_if_wrapper,
@@ -249,6 +250,17 @@ class BaseTorchVariable(VariableTracker):
             install_guard(source.make_guard(GuardBuilder.CLASS_MATCH))
         elif inspect.ismodule(value):
             install_guard(source.make_guard(GuardBuilder.MODULE_MATCH))
+        elif inspect.isfunction(value):
+            install_guard(source.make_guard(GuardBuilder.CLOSURE_MATCH))
+        elif inspect.isbuiltin(value) or isinstance(
+            value, (torch._ops.OpOverload, torch._ops.OpOverloadPacket)
+        ):
+            install_guard(source.make_guard(GuardBuilder.BUILTIN_MATCH))
+        elif is_wrapper_or_member_descriptor(value) or isinstance(
+            value, torch._dynamo.compiled_autograd.Op
+        ):
+            # Dont need to guard on wrappers
+            pass
         else:
             install_guard(source.make_guard(GuardBuilder.FUNCTION_MATCH))
         return cls(value, source=source)
