82aa638f60efd65f5110a960c8e119da77e53903
Yu, Guangye <guangye.yu@intel.com>
2025-10-29 15:49:27 +0000

    Update
    [ghstack-poisoned]

--- test/test_xpu_expandable_segments.py
+++ test/test_xpu_expandable_segments.py
@@ -5,7 +5,7 @@ import pathlib
 import sys
 
 import torch
-from torch.testing._internal.common_utils import run_tests, IS_WINDOWS
+from torch.testing._internal.common_utils import IS_WINDOWS, run_tests
 
 
 REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
@@ -18,10 +18,7 @@ from tools.stats.import_test_stats import get_disabled_tests
 sys.path.remove(str(REPO_ROOT))
 
 if __name__ == "__main__":
-    if (
-        torch.xpu.is_available()
-        and not IS_WINDOWS
-    ):
+    if torch.xpu.is_available() and not IS_WINDOWS:
         get_disabled_tests(".")
 
         torch._C._accelerator_setAllocatorSettings("expandable_segments:True")
