a6e52c5358c1f4cbf129de0d9544a36c57619011
Samuel Park <sampark@redhat.com>
2025-10-29 19:36:30 +0000

    Move the check of ZeroTensor to see if has storage initialized. If it doesn't it is any meta tensors.

--- torch/csrc/jit/python/pybind_utils.cpp
+++ torch/csrc/jit/python/pybind_utils.cpp
@@ -586,13 +586,8 @@ py::object toPyObject(IValue ivalue) {
     return py::none();
   } else if (ivalue.isTensor()) {
     auto tensor = std::move(ivalue).toTensor();
-    if (tensor.unsafeGetTensorImpl()->is_wrapped_number()) {
-      TORCH_INTERNAL_ASSERT(
-          tensor.device().is_cpu() || (tensor.is_meta() && tensor.dim() == 0));
-      if (tensor.is_meta() && tensor.dim() == 0) {
-        // deal with zero tensor sym with wrapped number logic
-        return py::cast(std::move(tensor));
-      }
+    if (tensor.unsafeGetTensorImpl()->is_wrapped_number() && tensor.unsafeGetTensorImpl()->storage_initialized()) {
+      TORCH_INTERNAL_ASSERT(tensor.device().is_cpu());
       auto py_tensor = py::cast(tensor);
       if (PyObject_HasAttrString(py_tensor.ptr(), "_wrapped_number")) {
         return py_tensor.attr("_wrapped_number");
