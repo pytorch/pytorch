56db97fc7076f15d158b5cf78831eb5bf6e4ab7f
Colin L. Rice <clr@fb.com>
2025-10-29 13:25:37 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/compile_worker/timer.py
+++ torch/_inductor/compile_worker/timer.py
@@ -1,6 +1,6 @@
 from threading import Lock, Thread
 from time import sleep, time
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 
 class Timer:
@@ -10,12 +10,12 @@ class Timer:
 
     def __init__(
         self,
-        duration: int,  # Duration in seconds
+        duration: Union[int, float],  # Duration in seconds
         call: Any,  # Function to call when we expire
     ) -> None:
         # We don't start the background thread until we actually get an event.
         self.background_thread: Optional[Thread] = None
-        self.last_called: Optional[Time] = None
+        self.last_called: Optional[float] = None
         self.duration = duration
         self.sleep_time = 60
         self.call = call
@@ -24,18 +24,18 @@ class Timer:
         # Technically GIL should ensure only one call, but let's be explicit here
         self.lock = Lock()
 
-    def record_call(self):
+    def record_call(self) -> None:
         with self.lock:
             if self.background_thread is None:
                 self.background_thread = Thread(target=self.check)
                 self.background_thread.start()
             self.last_called = time()
 
-    def quit(self):
+    def quit(self) -> None:
         with self.lock:
             self.exit = True
 
-    def check(self):
+    def check(self) -> None:
         while True:
             # We have to be sensitive on checking here, to avoid too much impact on cpu
             sleep(self.sleep_time)
