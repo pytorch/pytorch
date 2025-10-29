1cd3d805f3b48b2629ce9316328c1c56198860c4
Yuanyuan Chen <cyyever@outlook.com>
2025-10-29 09:44:27 +0800

    Rework some changes of BLASConstants.cu
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- aten/src/ATen/cuda/detail/BLASConstants.cu
+++ aten/src/ATen/cuda/detail/BLASConstants.cu
@@ -6,18 +6,30 @@ namespace at {
 namespace cuda {
 namespace detail {
 
-__device__ __constant__ float cublas_one_device{1.f};
-__device__ __constant__ float cublas_zero_device{0.f};
+__device__ __constant__ float cublas_one_device;
+__device__ __constant__ float cublas_zero_device;
 
 float *get_cublas_device_one() {
-  float *ptr;
-  AT_CUDA_CHECK(cudaGetSymbolAddress(reinterpret_cast<void**>(&ptr), cublas_one_device));
+  static float *ptr = nullptr;
+  static auto init_flag = [&]() {
+    const float one = 1.f;
+    AT_CUDA_CHECK(cudaMemcpyToSymbol(cublas_one_device, &one, sizeof(float)));
+    AT_CUDA_CHECK(cudaGetSymbolAddress(reinterpret_cast<void**>(&ptr), cublas_one_device));
+    return true;
+  }();
+
   return ptr;
 }
 
 float *get_cublas_device_zero() {
-  float *ptr;
-  AT_CUDA_CHECK(cudaGetSymbolAddress(reinterpret_cast<void**>(&ptr), cublas_zero_device));
+  static float *ptr = nullptr;
+  static auto init_flag = [&]() {
+    const float zero = 0.f;
+    AT_CUDA_CHECK(cudaMemcpyToSymbol(cublas_zero_device, &zero, sizeof(float)));
+    AT_CUDA_CHECK(cudaGetSymbolAddress(reinterpret_cast<void**>(&ptr), cublas_zero_device));
+    return true;
+  }();
+
   return ptr;
 }
 
