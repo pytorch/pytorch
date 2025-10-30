df3e945f344f79848e9fe998a9f8c02d9a03ee91
linhaifeng <1371675203@qq.com>
2025-10-30 20:07:10 +0800

    [feat] write a typos linter

---
+++ tools/linter/adapters/typos_linter.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+
+import argparse
+import json
+import logging
+import os
+import re
+import subprocess
+import sys
+import time
+from enum import Enum
+from pathlib import Path
+from typing import NamedTuple
+
+
+class LintMessage(NamedTuple):
+    message: str | None
+    filename: str | None
+    line: int | None
+    column: int | None
+    code: str | None
+
+RESULTS_RE: re.Pattern[str] = re.compile(
+    r"""(?mx)
+    ^error:\s+(?P<message>`.*?`\s+should\s+be\s+`.*?`)\s*$
+    (?:.*\n)*?                                                 
+    [^:\n]*?(?P<filename>\.{0,2}/[^\s:]+):(?P<line>\d+):(?P<column>\d+)
+    """,
+)
+
+def run_command(
+    args: list[str]
+) -> subprocess.CompletedProcess[bytes]:
+    logging.debug("$ %s", " ".join(args))
+    start_time = time.monotonic()
+    try:
+        return subprocess.run(
+            args,
+            capture_output=True,
+        )
+    finally:
+        end_time = time.monotonic()
+        logging.debug("took %dms", (end_time - start_time) * 1000)
+
+
+def check_run_typos(filenames: list[str], config: str, code: str) -> list[LintMessage]:
+    filenames = [os.path.relpath(f) for f in filenames]
+    try:
+        print("debug")
+        proc =  run_command(
+        ["typos", "."] + filenames,
+    )
+    except OSError as err:  
+        return [
+            LintMessage(
+                message=None,
+                filename=None,
+                line=None,
+                column=None,
+                code=code,
+            )
+        ]
+    stdout = str(proc.stdout, "utf-8").strip()
+    stderr = str(proc.stderr, "utf-8").strip()
+    if proc.returncode not in (0, 1):
+        return [
+            LintMessage(
+                message=stderr,
+                filename=None,
+                line=None,
+                column=None,
+                code=code,
+            )
+        ]
+
+    rc = [
+        LintMessage(
+            message=match["message"],
+            filename=match["filename"],
+            line=int(match["line"]),
+            column=int(match["column"]),
+            code=code,
+        )
+        for match in RESULTS_RE.finditer(stdout)
+    ]
+    return rc
+
+def check_typos_installed(code: str) -> list[LintMessage]:
+    cmd = ["typos", "-V"]
+    try:
+        subprocess.run(cmd, check=True, capture_output=True)
+        return []
+    except subprocess.CalledProcessError as e:
+        msg = e.stderr.decode(errors="replace")
+        return [
+            LintMessage(
+                message=f"Could not run '{' '.join(cmd)}': {msg}",
+                filename=None,
+                line=None,
+                column=None,
+                code=code,
+            )
+        ]
+    
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="typos wrapper linter.",
+        fromfile_prefix_chars="@",
+    )
+    parser.add_argument(
+        "--config",
+        required=True,
+        help="path to an typos _typos.toml file",
+    )
+    parser.add_argument(
+        "--code",
+        default="TYPOS",
+        help="the code this lint should report as",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="verbose logging",
+    )
+    parser.add_argument(
+        "filenames",
+        nargs="+",
+        help="paths to lint",
+    )
+    args = parser.parse_args()
+    print(args.filenames)
+    logging.basicConfig(
+        format="<%(threadName)s:%(levelname)s> %(message)s",
+        level=logging.NOTSET
+        if args.verbose
+        else logging.INFO,
+        stream=sys.stderr,
+    )
+    lint_messages = check_typos_installed(args.code) + check_run_typos(
+         args.filenames, args.config, args.code
+    )
+    for lint_message in lint_messages:
+        print(json.dumps(lint_message._asdict()), flush=True)
+
+   
+
+
+
+if __name__ == "__main__":
+    main()
