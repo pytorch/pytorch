85ba89cd8fc0b90ecb4becf580ad596572cdb156
Apurva Jain <apurvajain.kota@gmail.com>
2025-10-30 10:04:21 -0700

    Refactor gradient tests for addmm and addbmm
    Updated gradient tests to only use long configurations for addmm and addbmm benchmarks.

--- benchmarks/operator_benchmark/pt/addmm_test.py
+++ benchmarks/operator_benchmark/pt/addmm_test.py
@@ -54,7 +54,7 @@ class AddmmBenchmark(op_bench.TorchBenchmarkBase):
 
 
 op_bench.generate_pt_test(addmm_short_configs + addmm_long_configs, AddmmBenchmark)
-op_bench.generate_pt_gradient_test(addmm_short_configs + addmm_long_configs, AddmmBenchmark)
+op_bench.generate_pt_gradient_test(addmm_long_configs, AddmmBenchmark)
 
 """Mircobenchmark for addbmm operator."""
 
@@ -105,9 +105,7 @@ addbmm_short_configs = op_bench.cross_product_configs(
 )
 
 op_bench.generate_pt_test(addbmm_long_configs + addbmm_short_configs, AddbmmBenchmark)
-op_bench.generate_pt_gradient_test(
-    addbmm_long_configs + addbmm_short_configs, AddbmmBenchmark
-)
+op_bench.generate_pt_gradient_test(addbmm_long_configs, AddbmmBenchmark)
 
 if __name__ == "__main__":
     op_bench.benchmark_runner.main()
