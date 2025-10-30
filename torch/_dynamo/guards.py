51f5001d6aad7b2f1bdac2a8e668b6b37a5c9481
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-10-30 13:22:24 +0000

    Revert "[user-streams] Handle returning the current stream with/without device index (#165356)"
    This reverts commit f1af679270392c83e03808c8af5e2cbe3cdf16ce.
    
    Reverted https://github.com/pytorch/pytorch/pull/165356 on behalf of https://github.com/atalman due to test/test_rename_privateuse1_to_existing_device.py::TestRenamePrivateuseoneToExistingBackend::test_external_module_register_with_existing_backend [GH job link](https://github.com/pytorch/pytorch/actions/runs/18930365446/job/54046768884) [HUD commit link](https://hud.pytorch.org/pytorch/pytorch/commit/a5335263d32b5be2b2647661334d81225c3cc3fc) ([comment](https://github.com/pytorch/pytorch/pull/165356#issuecomment-3467967061))

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -132,7 +132,6 @@ from .source import (
     CodeSource,
     ConstantSource,
     ConstDictKeySource,
-    CurrentStreamSource,
     DataclassFieldsSource,
     DefaultsSource,
     DictGetItemSource,
@@ -1447,13 +1446,6 @@ class GuardBuilder(GuardBuilderBase):
                 python_lambda=lambda _: get_torch_function_mode_stack_at(
                     source._get_index()
                 ),
-                source=source_name,
-                example_value=example_value,
-                guard_manager_enum=guard_manager_enum,
-            )
-        elif istype(source, CurrentStreamSource):
-            out = root_guard_manager.lambda_manager(
-                python_lambda=lambda _: get_current_stream(source.device),
                 source=source_name,
                 example_value=example_value,
                 guard_manager_enum=guard_manager_enum,
