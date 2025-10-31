07b3babffc964562706b4d30e883445d33e62bf3
Justin Chu <justinchuby@users.noreply.github.com>
2025-10-30 21:33:09 -0700

    test
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- test/onnx/exporter/test_small_models_e2e.py
+++ test/onnx/exporter/test_small_models_e2e.py
@@ -17,7 +17,8 @@ from torch.utils import _pytree as torch_pytree
 
 class _WithExport:
     def export(self, model, args=(), kwargs=None, **options) -> torch.onnx.ONNXProgram:
-        model = model.eval()
+        if isinstance(model, torch.nn.Module):
+            model = model.eval()
         onnx_program = torch.onnx.export(
             model,
             args,
