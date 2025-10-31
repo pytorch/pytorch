6fd606436d1573c8431a070393c6c329f515d72a
Xiao Fu <xiaofu@meta.com>
2025-10-31 16:19:02 -0700

    Add hop print functions call enable and proper test for format print
    [ghstack-poisoned]

--- test/higher_order_ops/test_print.py
+++ test/higher_order_ops/test_print.py
@@ -1,27 +1,16 @@
-import unittest
-import unittest.mock as mock
-from unittest.mock import patch
-
+# Owner(s): ["module: higher order operators"]
 import io
+from unittest.mock import patch
 
 import torch
 from torch._dynamo.utils import counters
-
-from torch.testing._internal.common_utils import skipIfTorchDynamo, TestCase
+from torch.testing._internal.common_utils import run_tests, TestCase
 
 
 class TestHopPrint(TestCase):
     def test_base_print(self):
-        # class M(torch.nn.Module):
-        #     def forward(self, x):
-        #         torch._higher_order_ops.print("moo")
-        #         res = x + x
-        #         torch._higher_order_ops.print(("moo")
-        #         return (res,)
         def f(x):
             x = x + x
-            # change it to HOP it should still work
-            # print("moo")
             torch._higher_order_ops.print("moo")
             x = x * x
             return x
@@ -32,8 +21,25 @@ class TestHopPrint(TestCase):
         with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
             opt_out = opt_f(x)
             printed_output = mock_stdout.getvalue().strip()
-            orig_out = f(x)
 
-        self.assertTrue(same(orig_out, opt_out))
         self.assertEqual(printed_output, "moo")
-        self.assertEqual(len(counters["graph_break"]), 1)
+
+    def test_para_print(self):
+        def f(x):
+            x = x + x
+            torch._higher_order_ops.print("moo {x} {y}", x=1, y=2)
+            x = x * x
+            return x
+
+        counters.clear()
+        x = torch.randn(3, 3)
+        opt_f = torch.compile(backend="eager")(f)
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
+            opt_out = opt_f(x)
+            printed_output = mock_stdout.getvalue().strip()
+
+        self.assertEqual(printed_output, "moo 1 2")
+
+
+if __name__ == "__main__":
+    run_tests()
