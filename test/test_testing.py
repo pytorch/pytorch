a663eb9c80e92190832b3dd616f138f938f51525
drisspg <drisspguessous@gmail.com>
2025-11-01 17:41:40 +0000

    [FlexFlash] CuteDSL flat indexer needs to be colexigraphic in coordinate space (#166657)
    Benchmarks on Hopper:
    Note the triton impl is not using max-autotune because I didnt feel like waiting for 90x plots
    <img width="12517" height="5995" alt="combined_comparison" src="https://github.com/user-attachments/assets/d94debd9-920d-4413-b51f-b8e906e4fb01" />
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166657
    Approved by: https://github.com/v0i0, https://github.com/mlazos, https://github.com/eellison
    ghstack dependencies: #166359

--- test/test_testing.py
+++ test/test_testing.py
@@ -2367,6 +2367,7 @@ class TestImports(TestCase):
                            "torch.onnx._internal",  # depends on onnx-script
                            "torch._inductor.runtime.triton_helpers",  # depends on triton
                            "torch._inductor.codegen.cuda",  # depends on cutlass
+                           "torch._inductor.codegen.cutedsl",  # depends on cutlass
                            "torch.distributed.benchmarks",  # depends on RPC and DDP Optim
                            "torch.distributed.examples",  # requires CUDA and torchvision
                            "torch.distributed.tensor.examples",  # example scripts
