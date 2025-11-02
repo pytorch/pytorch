f9d0bf9664eed209e5c9aa60d6938088cda9d160
Yuanyuan Chen <cyyever@outlook.com>
2025-11-02 09:19:29 +0800

    Apply fixes

--- test/distributed/_composable/fsdp/test_fully_shard_state_dict.py
+++ test/distributed/_composable/fsdp/test_fully_shard_state_dict.py
@@ -438,7 +438,9 @@ class TestFullyShardStateDictMultiThread(FSDPTestMultiThread):
         if self.rank == 0:
             self.assertEqual(len(full_sd), len(ref_full_sd))
             self.assertEqual(list(full_sd.keys()), list(ref_full_sd.keys()))
-            for param, ref_param in zip(full_sd.values(), ref_full_sd.values()):
+            for param, ref_param in zip(
+                full_sd.values(), ref_full_sd.values(), strict=True
+            ):
                 self.assertEqual(param.device, torch.device("cpu"))
                 self.assertEqual(param.device, ref_param.device)
                 self.assertEqual(param, ref_param)
