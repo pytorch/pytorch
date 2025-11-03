30fc5481896bc439697a21a2817a897d2f04618c
Yuanyuan Chen <cyyever@outlook.com>
2025-11-03 05:36:52 +0000

    [2/N] Correctly use test parameters  (#166783)
    This PR fixes unused test parameters.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166783
    Approved by: https://github.com/mlazos

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -1527,7 +1527,7 @@ class TestLinalg(TestCase):
                 if error is None:
                     torch.linalg.vector_norm(input, dim=dim)
                 else:
-                    with self.assertRaises(error):
+                    with self.assertRaises(error, msg=error_msg):
                         torch.linalg.vector_norm(input, dim=dim)
 
     # This test compares torch.linalg.norm and numpy.linalg.norm to ensure that
