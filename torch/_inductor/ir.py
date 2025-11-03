a8b864ca5eceb32ceced80e56bdb0b2e1b3676d1
Edward Z. Yang <ezyang@meta.com>
2025-11-03 12:07:17 -0800

    Add some Triton related suppressions that don't show on CI (#166868)
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166868
    Approved by: https://github.com/maggiemoss, https://github.com/zou3519

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -6985,6 +6985,7 @@ class UserDefinedTritonKernel(ExternKernel):
 
             configs = kernel.configs
             kernel = kernel.fn
+        # pyrefly: ignore  # bad-return
         return kernel, configs, restore_value_args, reset_to_zero_args
 
     @override
@@ -7140,7 +7141,10 @@ class UserDefinedTritonKernel(ExternKernel):
         self.mutable_args = [
             kernel_args[key]
             for key in identify_mutated_tensors(
-                kernel, {**kernel_args, **autotuned_kwargs}, tma_descriptor_metadata
+                # pyrefly: ignore  # bad-argument-type
+                kernel,
+                {**kernel_args, **autotuned_kwargs},
+                tma_descriptor_metadata,
             )
         ]
 
