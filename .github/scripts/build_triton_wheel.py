7002824759d56b077de1906d2d91679989ddfb9a
Andrey Talman <atalman@fb.com>
2025-11-04 13:20:16 -0500

    Update build_triton_wheel.py

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -45,7 +45,7 @@ def patch_init_py(
         orig = f.read()
     # Replace version
     orig = check_and_replace(
-        orig, f'__version__ = "{expected_version}"', f'__version__ = "{version}"'
+        orig, f"__version__ = '{expected_version}'", f'__version__ = "{version}"'
     )
     with open(path, "w") as f:
         f.write(orig)
