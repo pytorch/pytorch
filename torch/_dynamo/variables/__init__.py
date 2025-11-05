db5ea701fbbfcbfd4efa7d1040f08761e9472da1
morrison-turnansky <mturnans@redhat.com>
2025-11-05 20:42:39 +0000

    linting

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -101,7 +101,6 @@ from .lists import (
     TupleIteratorVariable,
     TupleVariable,
 )
-from .user_defined import NewNamedTupleVariable
 from .misc import (
     AutogradFunctionContextVariable,
     AutogradFunctionVariable,
@@ -146,6 +145,7 @@ from .torch import TorchCtxManagerClassVariable, TorchInGraphFunctionVariable
 from .user_defined import (
     FrozenDataClassVariable,
     MutableMappingVariable,
+    NewNamedTupleVariable,
     RemovableHandleVariable,
     UserDefinedClassVariable,
     UserDefinedDictVariable,
