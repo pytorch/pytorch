910471526d252257e990fa9c931d7b0e4e62f426
Lucas Kabela <lucaskabela@meta.com>
2025-11-04 16:33:37 -0800

    Type functions

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -472,7 +472,12 @@ class TorchCtxManagerClassVariable(BaseTorchVariable):
             )
         elif self.value is torch.nn.attention.sdpa_kernel.__wrapped__:  # type: ignore[attr-defined]
             name_to_arg_map = bind_args_cached(
-                self.value, tx, self.source, args, kwargs
+                # pyrefly: ignore[bad-argument-type]
+                self.value,
+                tx,
+                self.source,
+                args,
+                kwargs,
             )
             backends = name_to_arg_map["backends"].as_python_constant()
             set_priority = name_to_arg_map["set_priority"].as_python_constant()
