d4dd842b29a1c9eaa7709213f45b45be60a204a1
Erxin Shang <erxin.shang@intel.com>
2025-11-06 13:49:14 +0800

    Change the default value for the device type in the checkpoint to avoid init the class property device_type during the module init time.

--- torch/utils/checkpoint.py
+++ torch/utils/checkpoint.py
@@ -106,7 +106,7 @@ class DefaultDeviceType:
     to save and restore for recomputation.
     """
 
-    _default_device_type = acc.type if (acc := torch.accelerator.current_accelerator(True)) else "cpu"
+    _default_device_type = None
 
     @staticmethod
     def set_device_type(device: str = "cuda") -> None:
@@ -126,6 +126,9 @@ class DefaultDeviceType:
         Returns:
             str: The current default device type.
         """
+        if not DefaultDeviceType._default_device_type:
+            DefaultDeviceType._default_device_type = acc.type if (acc := torch.accelerator.current_accelerator(True)) else "cpu"
+
         return DefaultDeviceType._default_device_type
 
 
