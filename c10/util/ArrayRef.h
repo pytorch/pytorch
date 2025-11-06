97a0c6366e74ed7c0cb0e84c26c9d33dd551bb4a
Pearu Peterson <pearu.peterson@gmail.com>
2025-11-06 07:55:46 +0200

    Update
    [ghstack-poisoned]

--- c10/util/ArrayRef.h
+++ c10/util/ArrayRef.h
@@ -50,7 +50,7 @@ namespace c10 {
 /// However, you should prefer to use ArrayRef when possible, because its use
 /// of TORCH_CHECK will lead to better user-facing error messages.
 template <typename T>
-class ArrayRef final : public HeaderOnlyArrayRef<T> {
+class ArrayRef /*final*/ : public HeaderOnlyArrayRef<T> {
  public:
   /// @name Constructors, all inherited from HeaderOnlyArrayRef except for
   /// SmallVector. As inherited constructors won't work with class template
