9b4ac45d2fd7e4bde216efa18d1e8e4bce33e4ae
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-11-06 22:34:46 +0000

    Revert "[Inductor] addmm with bias -> unfuse bias if there is a pointwise/reduction consumer (#166165)"
    This reverts commit eefa16342c9f322b56c7c0cd6d309c3ed8f0b882.
    
    Reverted https://github.com/pytorch/pytorch/pull/166165 on behalf of https://github.com/jeanschmidt due to Breaking internal tests D86216934 ([comment](https://github.com/pytorch/pytorch/pull/166165#issuecomment-3499645688))

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -15310,7 +15310,7 @@ if RUN_GPU:
                 ),
                 (
                     fn3,
-                    "triton_poi_fused_addmm_native_layer_norm",
+                    "triton_poi_fused_native_layer_norm_relu",
                     (torch.randn(4, 4, device=GPU_TYPE),),
                 ),
             ]
@@ -15323,7 +15323,7 @@ if RUN_GPU:
                 ),
                 (
                     fn3,
-                    "triton_poi_fused_LayerNorm_Linear_ReLU",
+                    "triton_poi_fused_LayerNorm_ReLU",
                     (torch.randn(4, 4, device=GPU_TYPE),),
                 ),
             ]
