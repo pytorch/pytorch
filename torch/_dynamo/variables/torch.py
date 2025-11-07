9a86ef763201e27f031469f0866c893707e9cf38
Lucas Kabela <lucaskabela@meta.com>
2025-11-07 00:40:45 +0000

    [BE][Typing][Dynamo] Type torch/_dynamo/variables/functions.py (#167103)
    Provides type coverage to torch/_dynamo/variables/dicts.py
    
    Coverage report:
    `mypy torch/_dynamo/variables/functions.py --linecount-report /tmp/coverage_log`
    
    Compare before to after - we go from 0 lines and 0 funcs covered to 2698 lines and 166 funcs covered
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167103
    Approved by: https://github.com/mlazos, https://github.com/fxdawnn

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -472,7 +472,12 @@ class TorchCtxManagerClassVariable(BaseTorchVariable):
             )
         elif self.value is torch.nn.attention.sdpa_kernel.__wrapped__:  # type: ignore[attr-defined]
             name_to_arg_map = bind_args_cached(
-                self.value, tx, self.source, args, kwargs
+                # pyrefly: ignore[bad-argument-type]
+                self.value,
+                tx,
+                self.source,
+                args,
+                kwargs,
             )
             backends = name_to_arg_map["backends"].as_python_constant()
             set_priority = name_to_arg_map["set_priority"].as_python_constant()
@@ -1429,7 +1434,7 @@ class TorchInGraphFunctionVariable(BaseTorchVariable):
             packed_input_vt = TupleVariable.build(
                 tx, (TupleVariable.build(tx, args), ConstDictVariable.build(tx, kwargs))
             )
-            out_vt = variables.UserFunctionVariable(tree_flatten).call_function(
+            out_vt = variables.UserFunctionVariable(tree_flatten).call_function(  # type: ignore[arg-type]
                 tx, [packed_input_vt], {}
             )
             assert isinstance(out_vt, TupleVariable) and len(out_vt.items) == 2
