c0b481dda066d306edf3554d1ea6ffa444278c7e
Rob Timpe <rtimpe@openteams.com>
2025-11-07 23:12:56 +0000

    Update
    [ghstack-poisoned]

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -63,12 +63,11 @@ else
   then
       PYTHON_DEP="python-freethreading=${ANACONDA_PYTHON_VERSION%?}"
   else
-      PYTHON_DEP="python=$ANACONDA_PYTHON_VERSION"
+      PYTHON_DEP="python=${ANACONDA_PYTHON_VERSION}"
   fi
 
   as_jenkins conda create -n py_$ANACONDA_PYTHON_VERSION -y\
-             # python="$ANACONDA_PYTHON_VERSION" \
-             ${PYTHON_DEP} \
+             ${PYTHON_DEP}\
              ${SYSROOT_DEP}
 fi
   # libstdcxx from conda default channels are too old, we need GLIBCXX_3.4.30
