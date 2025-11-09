8e6177ee00c1374a9820d70abd4e07e383f1058b
Yuanyuan Chen <cyyever@outlook.com>
2025-11-09 14:26:57 +0800

    Reapply "Remove python workaround for ContextDecorator (#167049)"
    This reverts commit bbf852d87ff527a5cdd9b9ca999356062eadf575.

--- torch/autograd/profiler.py
+++ torch/autograd/profiler.py
@@ -57,26 +57,11 @@ __all__ = [
     "MemRecordsAcc",
 ]
 
-try:
-    # Available in Python >= 3.2
-    from contextlib import ContextDecorator as _ContextDecorator
-except ImportError:
-    import functools
+from contextlib import ContextDecorator
 
-    class _ContextDecorator:  # type: ignore[no-redef]
-        def __enter__(self):
-            raise NotImplementedError
 
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            raise NotImplementedError
-
-        def __call__(self, func):
-            @functools.wraps(func)
-            def wrapped(*args, **kwargs):
-                with self:
-                    return func(*args, **kwargs)
-
-            return wrapped
+# Backward-compat alias: private name used to refer to a local fallback class.
+_ContextDecorator = ContextDecorator
 
 
 # global python state - whether profiler is currently enabled
@@ -836,8 +821,7 @@ class profile:
         return all_function_events
 
 
-# pyrefly: ignore [invalid-inheritance]
-class record_function(_ContextDecorator):
+class record_function(ContextDecorator):
     """Context manager/function decorator that adds a label to a code block/function when running autograd profiler.
     Label will only appear if CPU activity tracing is enabled.
 
