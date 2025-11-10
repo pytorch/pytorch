8cee155a79b406fdaf8562d5051d791f86ec7129
Mwiza Kunda <mwizak@graphcore.ai>
2025-11-10 10:05:08 +0000

    Add overridable triton_meta_common method

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -3306,7 +3306,8 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
     GDC_WAIT = "tl.extra.cuda.gdc_wait()"
     GDC_LAUNCH = "tl.extra.cuda.gdc_launch_dependents()"
 
-    def _enable_pdl_codegen(self):
+    @staticmethod
+    def _enable_pdl_codegen():
         if not torch._inductor.config.triton.enable_pdl:
             return False
         if isinstance(V.kernel, torch._inductor.select_algorithm.TritonTemplateKernel):
@@ -5197,6 +5198,10 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
         return imports.getvalue()
 
     @classmethod
+    def triton_meta_common(cls):
+        return {"enable_fp_fusion": not config.emulate_precision_casts, "disable_ftz": config.eager_numerics.disable_ftz, "launch_pdl": cls._enable_pdl_codegen()}
+
+    @classmethod
     def inductor_meta_common(cls):
         inductor_meta = {
             "backend_hash": torch.utils._triton.triton_hash_with_backend(),
@@ -5379,6 +5384,7 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
                 torch._inductor.config.triton.native_matmul
                 and ("tl.dot" in str(self.body) or "tl.dot" in str(self.compute))
             ),
+            **self.triton_meta_common(),
         }
 
         # Skip memory optimization for forward of the training loop where we expect
@@ -5480,8 +5486,6 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
 
         triton_meta["configs"] = [config_of(signature)]
 
-        triton_meta["launch_pdl"] = self._enable_pdl_codegen()
-
         # Triton compiler includes equal_to_1 args into constants even
         # when they are not constexpr. otherwise there may be a segfault
         # during launching the Inductor-compiled Triton kernel.
@@ -5489,8 +5493,6 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
         # https://github.com/triton-lang/triton/blob/231efe9ed2d200be0f69a07c298e4342b08efe3d/python/triton/runtime/jit.py#L384
         for arg_num in equal_1_arg_indices(signature):  # type: ignore[index]
             triton_meta["constants"][signature[arg_num].name] = 1  # type: ignore[index,union-attr]
-        triton_meta["enable_fp_fusion"] = not config.emulate_precision_casts
-        triton_meta["disable_ftz"] = config.eager_numerics.disable_ftz
 
         self.triton_meta = triton_meta
 
