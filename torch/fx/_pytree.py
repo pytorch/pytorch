b2fc1ebe2c1b4db1a1e10f0411c7a750f92933aa
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-11-10 14:28:19 +0800

    [pytree] support PyStructSequence types for Python pytree
    ghstack-source-id: 8e9af6cb8fbf70a124a4fa59ce8a7e591d73d141
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113258

--- torch/fx/_pytree.py
+++ torch/fx/_pytree.py
@@ -3,8 +3,7 @@ from collections.abc import Callable
 from typing import Any, Optional, TypeVar
 from typing_extensions import NamedTuple
 
-import torch.return_types
-from torch.utils._pytree import PyTree, tree_flatten, TreeSpec
+from torch.utils._pytree import PyTree, structseq, tree_flatten, TreeSpec
 
 
 FlattenFuncSpec = Callable[[PyTree, TreeSpec], list]
@@ -94,21 +93,28 @@ def _namedtuple_flatten_spec_exact_match(d: NamedTuple, spec: TreeSpec) -> bool:
     return len(d) == spec.num_children
 
 
-register_pytree_flatten_spec(dict, _dict_flatten_spec, _dict_flatten_spec_exact_match)
-register_pytree_flatten_spec(list, _list_flatten_spec, _list_flatten_spec_exact_match)
 register_pytree_flatten_spec(
     tuple,
     _tuple_flatten_spec,
     _tuple_flatten_spec_exact_match,
 )
-for return_type in torch.return_types.all_return_types:
-    register_pytree_flatten_spec(
-        return_type,
-        _tuple_flatten_spec,
-        _tuple_flatten_spec_exact_match,
-    )
+register_pytree_flatten_spec(
+    list,
+    _list_flatten_spec,
+    _list_flatten_spec_exact_match,
+)
+register_pytree_flatten_spec(
+    dict,
+    _dict_flatten_spec,
+    _dict_flatten_spec_exact_match,
+)
 register_pytree_flatten_spec(
     namedtuple,  # type: ignore[arg-type]
     _namedtuple_flatten_spec,
     _namedtuple_flatten_spec_exact_match,
+)
+register_pytree_flatten_spec(
+    structseq,
+    _tuple_flatten_spec,
+    _tuple_flatten_spec_exact_match,
 )
