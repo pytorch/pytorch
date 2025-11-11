2e771b7b1c16ed0e4110bb579c313dba4f86245b
Rob Timpe <rtimpe@openteams.com>
2025-11-11 21:45:01 +0000

    Update (base update)
    [ghstack-poisoned]

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -49,18 +49,25 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
     export SYSROOT_DEP="sysroot_linux-64=2.17"
   fi
 
+  if [[ $PYTHON_FREETHREADED == "1" ]]
+  then
+    PYTHON_DEP="python-freethreading=${ANACONDA_PYTHON_VERSION}"
+  else
+    PYTHON_DEP="python=${ANACONDA_PYTHON_VERSION}"
+  fi
+
 # Install correct Python version
 # Also ensure sysroot is using a modern GLIBC to match system compilers
 if [ "$ANACONDA_PYTHON_VERSION" = "3.14" ]; then
   as_jenkins conda create -n py_$ANACONDA_PYTHON_VERSION -y\
-             python="3.14.0" \
+             ${PYTHON_DEP} \
              ${SYSROOT_DEP} \
              -c conda-forge
 else
   # Install correct Python version
   # Also ensure sysroot is using a modern GLIBC to match system compilers
   as_jenkins conda create -n py_$ANACONDA_PYTHON_VERSION -y\
-             python="$ANACONDA_PYTHON_VERSION" \
+             ${PYTHON_DEP} \
              ${SYSROOT_DEP}
 fi
   # libstdcxx from conda default channels are too old, we need GLIBCXX_3.4.30
