a90a8d51043f182e33b2f4e8b58628bf4e3e5845
Garrett Goon <goon@ibm.com>
2025-11-11 16:04:45 +0000

    ReduceOp.AVG for custom reduce_scatter when poss

--- torch/distributed/fsdp/_fully_shard/_fsdp_collectives.py
+++ torch/distributed/fsdp/_fully_shard/_fsdp_collectives.py
@@ -732,9 +732,11 @@ def _get_gradient_divide_factors(
             if data_parallel_size == 1:
                 return None, None, ReduceOp.SUM, ReduceOp.SUM
             return None, None, ReduceOp.AVG, ReduceOp.AVG
+        if factor == reduce_scatter_group.size():
+            reduce_scatter_op = ReduceOp.AVG
         else:
             reduce_scatter_op = torch.distributed._make_nccl_premul_sum(1 / factor)
-            return None, None, reduce_scatter_op, ReduceOp.SUM
+        return None, None, reduce_scatter_op, ReduceOp.SUM
 
     pre_factor: Optional[float]
     if overflow_risk:
