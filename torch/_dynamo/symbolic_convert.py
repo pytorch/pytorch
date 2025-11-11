3ce7b125daa2cb9dfc27c2bf553f2e5e5794132b
Guilherme Leobas <gleobas@quansight.com>
2025-11-11 20:37:02 +0000

    Update
    [ghstack-poisoned]

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -4002,12 +4002,11 @@ class InstructionTranslatorBase(
 
         if flags & 0x10:
             assert sys.version_info >= (3, 14)
-            from annotationlib import Format
 
             # maybe use Format.VALUE_WITH_FAKE_GLOBALS instead?
             # https://docs.python.org/3/library/annotationlib.html#annotationlib.Format.VALUE_WITH_FAKE_GLOBALS
             attr = attr.call_function(
-                self, [ConstantVariable.create(Format.VALUE.value)], {}
+                self, [ConstantVariable.create(1)], {}
             )
             fn.annotations = attr
         elif flags & 0x08:
