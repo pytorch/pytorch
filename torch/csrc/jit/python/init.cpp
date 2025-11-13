4c58282575ae51f4ad404e7894988cf95918e248
angelayi <yiangela7@gmail.com>
2025-11-13 09:27:46 -0800

    [opaque_obj] Remove inital opaque obj
    [ghstack-poisoned]

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -1863,35 +1863,6 @@ void initJITBindings(PyObject* module) {
       py::arg("schema"),
       py::arg("allow_typevars") = true);
   m.def(
-      "_make_opaque_object",
-      [](py::object payload) {
-        auto obj = c10::make_intrusive<OpaqueObject>(payload);
-        auto typePtr =
-            torch::getCustomClass("__torch__.torch.classes.aten.OpaqueObject");
-        return torch::jit::toPyObject(c10::IValue(std::move(obj)));
-      },
-      R"doc(Creates an opaque object which stores the given Python object.)doc");
-  m.def(
-      "_get_opaque_object_payload",
-      [](py::object obj) {
-        auto typePtr =
-            torch::getCustomClass("__torch__.torch.classes.aten.OpaqueObject");
-        auto ivalue = torch::jit::toIValue(std::move(obj), typePtr);
-        auto customObj = ivalue.toCustomClass<OpaqueObject>();
-        return customObj->getPayload();
-      },
-      R"doc(Returns the Python object stored on the given opaque object.)doc");
-  m.def(
-      "_set_opaque_object_payload",
-      [](py::object obj, py::object payload) {
-        auto typePtr =
-            torch::getCustomClass("__torch__.torch.classes.aten.OpaqueObject");
-        auto ivalue = torch::jit::toIValue(std::move(obj), typePtr);
-        auto customObj = ivalue.toCustomClass<OpaqueObject>();
-        customObj->setPayload(std::move(payload));
-      },
-      R"doc(Sets the payload of the given opaque object with the given Python object.)doc");
-  m.def(
       "_register_opaque_type",
       [](const std::string& type_name) {
         torch::jit::registerOpaqueType(type_name);
