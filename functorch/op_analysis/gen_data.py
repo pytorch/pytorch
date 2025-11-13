83fce4d21c646d31fb474c95410164a2c3765a22
linhaifeng <1371675203@qq.com>
2025-11-13 09:01:32 +0800

    [lint] 00

--- functorch/op_analysis/gen_data.py
+++ functorch/op_analysis/gen_data.py
@@ -25,13 +25,10 @@ def gen_data(special_op_lists, analysis_name):
     composite_ops = get_ops_for_key("CompositeImplicitAutograd")
     noncomposite_ops = all_ops - composite_ops
     with open("../../aten/src/ATen/native/native_functions.yaml") as f:
-        ops = yaml.load(f.read(),Loader=yaml.CLoader)
+        ops = yaml.load(f.read(), Loader=yaml.CLoader)
 
     with open("annotated_ops") as f:
-        annotated_ops = {
-        a.strip(): b.strip()
-        for a,b in csv.reader(f)
-    }
+        annotated_ops = {a.strip(): b.strip() for a, b in csv.reader(f)}
 
     uniq_ops = []
     uniq_names = set()
