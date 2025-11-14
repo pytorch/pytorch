3b5388c8d52c0e8f7693b505f4d49b1b409a5cf4
eellison <elias.ellison@gmail.com>
2025-11-14 08:41:33 -0800

    Update
    [ghstack-poisoned]

--- torch/_inductor/tiling_utils.py
+++ torch/_inductor/tiling_utils.py
@@ -348,26 +348,29 @@ class NodeSplitGetter:
         max_pw_split = max(self.pw_split_options.keys())
         max_red_split = max(self.red_split_options.keys())
 
-        def add_combined_split_options(split_options: dict[int, OrderedSet[Split]], curr_length: int) -> None:
+        def add_combined_split_options(
+            split_options: dict[int, OrderedSet[Split]], curr_length: int
+        ) -> None:
             for split in split_options[curr_length]:
                 for i in range(len(split) - 1):
                     new_split = tuple(
-                        split[0:i]
-                        + (sympy_product(split[i : i + 2]),)
-                        + split[i + 2 :]
+                        split[0:i] + (sympy_product(split[i : i + 2]),) + split[i + 2 :]
                     )
                     split_options[len(new_split)].add(new_split)
-
 
         for total_splits in range(max_pw_split + max_red_split, 0, -1):
             for pw_split_len in range(total_splits, 0, -1):
                 for pw_split in self.pw_split_options[pw_split_len]:
-                    for red_split in self.red_split_options[total_splits - pw_split_len]:
+                    for red_split in self.red_split_options[
+                        total_splits - pw_split_len
+                    ]:
                         if out := self.try_split(pw_split, red_split):
                             return out
 
             add_combined_split_options(self.pw_split_options, pw_split_len)
-            add_combined_split_options(self.red_split_options, total_splits - pw_split_len)
+            add_combined_split_options(
+                self.red_split_options, total_splits - pw_split_len
+            )
 
         # if for whatever reason we couldn't split above, return default split
         return ((self.pointwise_numel,), (self.red_numel,))
