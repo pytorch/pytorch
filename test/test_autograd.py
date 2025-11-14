2ee209ebd71045838d84c1bcfa0f07cba05c627a
Sam Gross <colesbury@gmail.com>
2025-11-13 20:32:01 -0800

    Rework PyObject preservation (v2) (#167564)
    Summary:
    Make the PyObject preservation scheme thread-safe with free threaded (nogil) Python. The general idea is:
    
    * Python Tensor and Storage objects always hold a strong reference to their underlying c10 object
    * c10 objects hold a strong reference to their Python objects if there's at least one other reference to the c10 object
    
    This is implemented in `intrusive_ptr`:
    
    * The top most bit (`kHasPyObject`) from the weakref count is now used to indicate if the `intrusive_ptr_target` has an associated PyObject. So `kHasPyObject` is one bit, the weakref count is now 31 bits and the strong refcount remains 32 bits.
    * When the reference count increases from one to two and `kHasPyObject` is set, we incref the associated Python object to ensure that it's kept alive.
    * When the reference count decreases from two to one (i.e., there are no C++ reference to the `intrusive_ptr_target` other than from the Python object), we decre the associated Python object to break the cycle.
    
    Other benefits:
    
    * We can delete a lot of the copypasta from Python internal `subtype_dealloc`
    * This fixes the weakref and GC bugs we had in the previous scheme. Python weakrefs on Tensors and Storages should just work as expected now.
    
    Risks:
    
    * Extra branch for reference count operations on `intrusive_ptr<TensorImpl>`, `intrusive_ptr<StorageImpl>`, and the generic `intrusive_ptr<intrusive_ptr_target>` even when we're not using Python.
    * It's a big change
    
    (Second attempt at https://github.com/pytorch/pytorch/pull/166342)
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy chenyang78 kadeng muchulee8 amjames chauhang aakhundov coconutruben albanD
    
    
    Differential Revision: D86936370
    
    Pulled By: colesbury

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -10895,6 +10895,34 @@ get_out().sum().backward()
 
             self.assertTrue(gradcheck(func, x, fast_mode=True))
 
+    def test_grad_thread_safety(self):
+        import threading
+        from concurrent.futures import ThreadPoolExecutor
+
+        NUM_ITERS = 10
+        NUM_THREADS = 4
+
+        # Concurrent calls to tensor.untyped_storage()
+        def access_grad(tensor, barrier):
+            barrier.wait()
+            return weakref.ref(tensor.grad)
+
+        for i in range(NUM_ITERS):
+            tensor = torch.tensor([1.0, 2.0, 3.0], requires_grad=True)
+            (tensor**2).sum().backward()
+
+            barrier = threading.Barrier(NUM_THREADS)
+            with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+                futures = [
+                    executor.submit(access_grad, tensor, barrier)
+                    for _ in range(NUM_THREADS)
+                ]
+
+                # Check that all the grad tensors returned were the same
+                for future in futures:
+                    self.assertEqual(future.result()(), tensor.grad)
+                self.assertIsNotNone(tensor.grad)
+
 
 def index_perm_variable(shape, max_indices):
     if not isinstance(shape, tuple):
