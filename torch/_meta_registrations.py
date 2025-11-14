92908c63f739a3e5ba1abdb69dad843df9953f32
Turner Richmond <trichmon@redhat.com>
2025-11-14 19:24:39 +0000

    Ops convolution_backward optional flag bug (#165008)
    Fixes #89629
    
    When using torch.ops.aten.convolution_backward, the optional argument bias_sizes was being used in the python function registration without checking whether it was defined.
    
    ## For the fix
    there are two modes to consider with different results.
    
    First @dynamo.optimize("inductor") is the most demanding.
    We cannot be wrong about the size passed into the function. But we should not ignore what the user wants/thinks they are doing. For this case, we want to throw an error when the user is wrong. If the user passes in None, we calculate the expected size directly.
    
    Second @dynamo.optimize("eager") is very lenient.
    We really can provide any value we want here. If the user is wrong about bias shape in eager mode, the op will just reshape the bias to the proper size so no error is thrown here.
    
    ## For testing
    An OpInfo was added for torch.ops.aten.convolution_backward.default.
    For the CUDA test_noncontiguous_samples test, a slightly updated error tolerance was necessary for the compounded add multiply (for 2x2 kernel).
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/165008
    Approved by: https://github.com/bdhirsh

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -3514,8 +3514,13 @@ def meta_convolution_backward(
     if output_mask[1]:
         backend_grad_weight = grad_output_.new_empty(weight_.size())
     if output_mask[2]:
-        backend_grad_bias = grad_output_.new_empty(bias_sizes_opt)
-
+        if bias_sizes_opt:
+            backend_grad_bias = grad_output_.new_empty(bias_sizes_opt)
+        else:
+            if transposed:
+                backend_grad_bias = grad_output_.new_empty(weight_.size(1) * groups)
+            else:
+                backend_grad_bias = grad_output_.new_empty(weight_.size(0))
     return (backend_grad_input, backend_grad_weight, backend_grad_bias)
 
 
