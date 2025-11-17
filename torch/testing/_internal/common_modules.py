2906527f9db1220ce0e44c5064e2b2135c63ef8b
Liao, Wei <wei.liao@intel.com>
2025-11-17 16:58:25 +0800

    skip case in modele db

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -3709,6 +3709,8 @@ module_db: list[ModuleInfo] = [
                    # Not implemented for chalf on CPU
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_cpu_gpu_parity',
                                 dtypes=(torch.chalf,), device_type='cuda'),
+                   DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_cpu_gpu_parity',
+                                dtypes=(torch.chalf,), device_type='xpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
