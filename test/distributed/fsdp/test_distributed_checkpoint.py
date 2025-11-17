4b312fb0d68c1d3c1040edcc613f9e2c9f030c12
eqy <eddiey@nvidia.com>
2025-11-17 10:56:28 -0800

    Add note on iterable order for distributed tests
    Added a note about the importance of order in the iterable for distributed tests.

--- test/distributed/fsdp/test_distributed_checkpoint.py
+++ test/distributed/fsdp/test_distributed_checkpoint.py
@@ -30,7 +30,9 @@ if TEST_WITH_DEV_DBG_ASAN:
     )
     sys.exit(0)
 
-
+# NB: this iterable needs to be orderd as otherwise different ranks may run with
+# conflicting settings when e.g., @parametrize(_DISTRIBUTED_STATE_DICT_IMPLS) is
+# used to decorate tests
 _DISTRIBUTED_STATE_DICT_IMPLS = (
     StateDictType.LOCAL_STATE_DICT,
     StateDictType.SHARDED_STATE_DICT,
