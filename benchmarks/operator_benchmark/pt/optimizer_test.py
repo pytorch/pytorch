074dffa1cc8bd9ce1a3348444ee6b4fcdb180e99
jainapurva <apurvajain.kota@gmail.com>
2025-11-18 12:02:33 -0800

    Add optimizer tests in operator microbenchmarks

---
+++ benchmarks/operator_benchmark/pt/optimizer_test.py
@@ -0,0 +1,64 @@
+import operator_benchmark as op_bench
+
+import torch
+import torch.optim as optim
+
+
+"""Microbenchmarks for optimizer operators."""
+
+
+optimizer_list = op_bench.op_list(
+    attr_names=["op_name", "op_func"],
+    attrs=[
+        ["adamw", optim.AdamW],
+        ["adam", optim.Adam],
+        ["sgd", optim.SGD],
+        ["rmsprop", optim.RMSprop],
+        ["adagrad", optim.Adagrad],
+    ],
+)
+
+optimizer_configs_long = op_bench.cross_product_configs(
+    num_params=[1, 10, 100],
+    param_size=[100000, 1000000, 10000000],
+    device=["cuda"],
+    tags=["long"],
+)
+
+
+class OptimizerBenchmark(op_bench.TorchBenchmarkBase):
+    def init(self, op_func, device, shape=None, num_params=None, param_size=None):
+        if shape is not None:
+            num_params = num_params if num_params is not None else 1
+            self.params = [
+                torch.randn(shape, device=device, requires_grad=True)
+                for _ in range(num_params)
+            ]
+            for param in self.params:
+                param.grad = torch.randn(shape, device=device)
+        else:
+            self.params = [
+                torch.randn(param_size, device=device, requires_grad=True)
+                for _ in range(num_params)
+            ]
+            for param in self.params:
+                param.grad = torch.randn_like(param)
+
+        kwargs = {"momentum": 0.9} if op_func == optim.SGD else {}
+        self.optimizer = op_func(self.params, lr=0.001, **kwargs)
+        self.inputs = {"dummy": self.params[0]} # Added to run memory benchmarking
+
+    def forward(self, dummy):
+        self.optimizer.step()
+        for param in self.params:
+            param.grad = torch.randn_like(param)
+        return self.params[0]
+
+
+op_bench.generate_pt_tests_from_op_list(
+    optimizer_list, optimizer_configs_long, OptimizerBenchmark
+)
+
+
+if __name__ == "__main__":
+    op_bench.benchmark_runner.main()
