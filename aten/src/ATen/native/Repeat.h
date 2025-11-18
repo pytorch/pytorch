b991561ff501ae620fffe7aa912d4897559eadee
arkadip-maitra <amaitra@redhat.com>
2025-11-18 20:04:17 +0530

    fixing overflow handling with proper check

--- aten/src/ATen/native/Repeat.h
+++ aten/src/ATen/native/Repeat.h
@@ -8,6 +8,7 @@
 #else
 #include <ATen/ops/empty.h>
 #include <ATen/ops/empty_like.h>
+#include <c10/util/safe_numerics.h>
 #endif
 
 namespace at::native {
@@ -27,21 +28,26 @@ static inline Tensor repeat_interleave_common(
     return at::empty_like(repeats, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
   }
   Tensor repeats_ = repeats.contiguous();
-  Tensor cumsum = repeats.cumsum(0);
-  int64_t total = 0;
-  if (output_size.has_value()) {
-    total = output_size.value();
-  } else {
-    total = cumsum[-1].item<int64_t>();
+  // custom cumsum with overflow detection
+  Tensor cumsum = at::empty({repeats.size(0)}, repeats.options().dtype(at::kLong));
+  const index_t* repeat_ptr = repeats_.const_data_ptr<index_t>();
+  int64_t* cumsum_ptr = cumsum.data_ptr<int64_t>();
+
+  uint64_t cumsum_val = 0;
+  for (int64_t i = 0; i < repeats.size(0); i++) {
+    TORCH_CHECK(repeat_ptr[i] >= 0, "repeats can not be negative");
+    uint64_t result;
     TORCH_CHECK(
-        (repeats >= 0).all().item<uint8_t>(), "repeats can not be negative");
-    TORCH_CHECK(
-        (cumsum >= 0).all().item<uint8_t>(), "cumulative sum values overflowed. check the repeats tensor values");
+        !c10::add_overflows(cumsum_val, static_cast<uint64_t>(repeat_ptr[i]), &result) &&
+        result <= static_cast<uint64_t>(std::numeric_limits<int64_t>::max()),
+        "cumulative sum values overflowed. check the repeats tensor values");
+    cumsum_val = result;
+    cumsum_ptr[i] = static_cast<int64_t>(cumsum_val);
   }
 
+  int64_t total = output_size.has_value() ? output_size.value() : static_cast<int64_t>(cumsum_val);
+
   Tensor result = at::empty({total}, repeats.options());
-  const index_t* repeat_ptr = repeats_.const_data_ptr<index_t>();
-  const int64_t* cumsum_ptr = cumsum.const_data_ptr<int64_t>();
   index_t* result_ptr = result.data_ptr<index_t>();
   compute(repeat_ptr, cumsum_ptr, result_ptr, repeats.size(0), total);
   return result;
