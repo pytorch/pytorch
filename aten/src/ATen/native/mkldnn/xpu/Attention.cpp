b6a28277a4d694a2717be8bf87178895b7ff45cc
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2025-11-19 09:57:44 +0800

    Correct format
    Co-authored-by: Yu, Guangye <106960996+guangyey@users.noreply.github.com>

--- aten/src/ATen/native/mkldnn/xpu/Attention.cpp
+++ aten/src/ATen/native/mkldnn/xpu/Attention.cpp
@@ -86,7 +86,9 @@ bool can_use_cudnn_attention(sdp::sdp_params const& params, bool debug) {
   return false;
 }
 
-bool can_use_mem_efficient_attention(sdp::sdp_params const& params, bool debug) {
+bool can_use_mem_efficient_attention(
+    sdp::sdp_params const& params,
+    bool debug) {
   return true;
 }
 
