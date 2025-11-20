3e074e6c9f841f7d16e6cb9e252a4aa7e891974a
Will Constable <whc@meta.com>
2025-11-19 21:20:49 -0800

    Update base for Update on "[DTensor] add register_single_dim_strategy"
    **This diff stack is a prototype/experiment.**
    
    ### Motivation
    We find it is too difficult to implement sharding strategies for DTensor, and this slows progress towards full operator coverage, and increases likelihood of bugs in sharding rules.  We also expect to add new Placement types (e.g. more complete support for StridedShard in the near term, but possibly others as well), and the current formulation of sharding strategies is not scalable to adding new placement types.
    
    ### Specific Goals
    1. [**not prototyped**]Op Strategies should automatically support StridedShard and any other 'purely sharding' placement type without additional work.  Use a 'sharding placeholder' to declare sharding rules (e.g. S(0) , S(0) -> S(0) applies equally well to Shard(0) or StridedShard(0, sf=2) - we can make this substitution outside of the per-op rule.
    a. constraints such as 'must evenly shard' can be moved onto the variable, and filtered against real tensor/mesh dims during expansion time
    3. [**prototyped**]Focus Op Strategies on a single mesh-dim, and rely on standard infra to expand to the full mesh
    4. [**prototyped**]Create the possible input-output sharding specs without looking at input args and doing premature filtering, which trades code complexity for runtime optimization.  Try to regain the runtime savings in another common infra layer during 'expansion' to the mesh
    
    ### Experiment Approach
    This stack implements some of the above goals, and then implements some operators (so far, mm and pointwise).  I'm working on figuring out what the right tests and metrics to collect are before going further.  If you have concerns about whether this approach would work for a particular op, i could try that.
    * One major difference in this proposal is the 'premature optimization' in the strategy is removed.  How much does this matter?  Can we recover some of the time by doing filtering during mesh expansion, or by changing to expand only to some 'min cost' set of states around the input placements?
    * How can we test if the new strategy is correct? Can we tell if we're missing a strategy or if we're issuing a wrong strategy?
    
    [ghstack-poisoned]

--- torch/distributed/tensor/_ops/utils.py
+++ torch/distributed/tensor/_ops/utils.py
@@ -4,21 +4,17 @@ import functools
 import itertools
 import operator
 from collections.abc import Callable, Iterable, Sequence
-from typing import cast, Optional, TypeVar, Union
-from typing_extensions import ParamSpec
+from typing import cast, Optional, Union
 
 import torch
 from torch._prims_common import DimsSequenceType, DimsType
-from torch.distributed.tensor._api import DTensor
 from torch.distributed.tensor._collective_utils import redistribute_cost
 from torch.distributed.tensor._dtensor_spec import DTensorSpec
 from torch.distributed.tensor._op_schema import (
     OpSchema,
     OpSpec,
     OpStrategy,
-    OutputSharding,
     PlacementList,
-    RuntimeSchemaInfo,
     StrategyType,
 )
 from torch.distributed.tensor.device_mesh import DeviceMesh
@@ -28,72 +24,6 @@ from torch.distributed.tensor.placement_types import (
     Replicate,
     Shard,
 )
-
-
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
-
-
-# convenient wrapper to register sharding propagation rules
-def register_prop_rule(
-    op: Union[torch._ops.OpOverload, list[torch._ops.OpOverload]],
-    schema_info: Optional[RuntimeSchemaInfo] = None,
-) -> Callable[
-    [Callable[[OpSchema], OutputSharding]], Callable[[OpSchema], OutputSharding]
-]:
-    def wrapper(
-        impl: Callable[[OpSchema], OutputSharding],
-    ) -> Callable[[OpSchema], OutputSharding]:
-        overloads = op if isinstance(op, list) else [op]
-        for overload in overloads:
-            DTensor._op_dispatcher.sharding_propagator.register_sharding_prop_rule(
-                overload, impl, schema_info
-            )
-        return impl
-
-    return wrapper
-
-
-def register_op_strategy(
-    op, schema_info=None
-) -> Callable[[Callable[_P, _T]], Callable[_P, _T]]:
-    # pyre-fixme[2]: Parameter must be annotated.
-
-    # For every ATen op that accepts any args in this list,
-    # the arg itself can impact the strides (and potentially the sharding strategy)
-    # of the output tensor.
-    # thus, we will detect ATen schemas with any of these args and ensure
-    # that they get specialized here.
-    arg_names_that_require_specializing_cache_strategy = [
-        "memory_format",
-    ]
-
-    def wrapper(impl):
-        if isinstance(op, list):
-            overloads = op
-        else:
-            overloads = [op]
-
-        for overload in overloads:
-            curr_schema_info = None
-            if schema_info is None:
-                specialized_args = [
-                    a.name
-                    for a in overload._schema.arguments
-                    if a.name in arg_names_that_require_specializing_cache_strategy
-                ]
-                if any(specialized_args):
-                    curr_schema_info = RuntimeSchemaInfo(
-                        static_kwargkey=specialized_args
-                    )
-            else:
-                curr_schema_info = schema_info
-            DTensor._op_dispatcher.sharding_propagator.register_op_strategy(
-                overload, impl, curr_schema_info
-            )
-        return impl
-
-    return wrapper
 
 
 def replicate_op_strategy(op_schema: OpSchema) -> StrategyType:
