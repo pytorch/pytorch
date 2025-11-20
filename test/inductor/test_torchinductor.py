0552c288be7e36542ed10a0933e7eb39ada7aa14
Aleksei Nikiforov <aleksei.nikiforov@linux.ibm.com>
2025-11-20 13:10:43 +0000

    s390x: fix periodic tests build (#168001)
    It looks like building python_call.cpp with -O3 triggers a bug in gcc-14.
    
    As a workaround, ignore offending warning on s390x in the code.
    
    Build failure link: https://github.com/pytorch/pytorch/actions/runs/19423391774/job/55584553077
    
    GCC bug reference: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=115016
    
    In addition to that, fix docker image names for s390x test workflows similar to build workflows and remove fail marks from couple of tests.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168001
    Approved by: https://github.com/seemethere

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2172,7 +2172,6 @@ class CommonTemplate:
 
     @skipCPUIf(IS_MACOS, "fails on macos")
     @skip_if_halide  # accuracy 4.7% off
-    @xfailIfS390X  # accuracy failure
     def test_multilayer_var_lowp(self):
         def fn(a):
             return torch.var(a)
