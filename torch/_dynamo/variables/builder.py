35c9c0865bca87a7f17b574aee5aa13fa61ec70f
Arsh Zahed <arshzahed@meta.com>
2025-11-20 10:45:53 -0800

    Clear tx lookup_tensor in VariableBuilder call

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -495,6 +495,8 @@ class VariableBuilder:
             vt = self.tx.output.side_effects.track_object_existing(value, vt)
 
         self.tx.output.variable_tracker_cache.add(value, self.source, vt)
+
+        self.tx.fake_mode.fake_tensor_converter.meta_converter.describer.lookup_tensor.clear()
         return vt
 
     def _can_lift_attrs_to_inputs(self, vt):
