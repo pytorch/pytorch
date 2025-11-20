1790285234e48c62c46da217d101748ea142a6a1
Will Constable <whc@meta.com>
2025-11-20 10:05:36 -0800

    [NOT FOR LAND] Benchmark single-dim strategies
    ```
    test_name='mm', mesh_name='mesh_6d', time for expanded strategy:240.83ms (slowdown=1.01x) len(orig_s.strategies)=4096, len(single_s.strategies)=4096
    test_name='mm', mesh_name='mesh_5d', time for expanded strategy:53.08ms (slowdown=1.03x) len(orig_s.strategies)=1024, len(single_s.strategies)=1024
    test_name='mm', mesh_name='mesh_4d', time for expanded strategy:11.53ms (slowdown=1.03x) len(orig_s.strategies)=256, len(single_s.strategies)=256
    test_name='mm', mesh_name='mesh_3d', time for expanded strategy:2.45ms (slowdown=1.03x) len(orig_s.strategies)=64, len(single_s.strategies)=64
    test_name='mm', mesh_name='mesh_2d', time for expanded strategy:0.51ms (slowdown=1.02x) len(orig_s.strategies)=16, len(single_s.strategies)=16
    test_name='mm', mesh_name='mesh_1d', time for expanded strategy:0.11ms (slowdown=0.98x) len(orig_s.strategies)=4, len(single_s.strategies)=4
    test_name='add', mesh_name='mesh_6d', time for expanded strategy:48.93ms (slowdown=828.61x) len(orig_s.strategies)=1, len(single_s.strategies)=729
    test_name='add', mesh_name='mesh_5d', time for expanded strategy:14.37ms (slowdown=252.09x) len(orig_s.strategies)=1, len(single_s.strategies)=243
    test_name='add', mesh_name='mesh_4d', time for expanded strategy:4.18ms (slowdown=86.88x) len(orig_s.strategies)=1, len(single_s.strategies)=81
    test_name='add', mesh_name='mesh_3d', time for expanded strategy:1.20ms (slowdown=26.57x) len(orig_s.strategies)=1, len(single_s.strategies)=27
    test_name='add', mesh_name='mesh_2d', time for expanded strategy:0.34ms (slowdown=7.91x) len(orig_s.strategies)=1, len(single_s.strategies)=9
    test_name='add', mesh_name='mesh_1d', time for expanded strategy:0.10ms (slowdown=2.68x) len(orig_s.strategies)=1, len(single_s.strategies)=3
    ```
    ghstack-source-id: 6634635a2ca35693d0e920aeb6bfcbb28a90be90
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168116

---
+++ bench_singledim.py
@@ -0,0 +1,143 @@
+import math
+import timeit
+
+import torch
+from torch.distributed.tensor import init_device_mesh
+from torch.distributed.tensor._dtensor_spec import DTensorSpec, TensorMeta
+from torch.distributed.tensor._op_schema import OpSchema, OpSpec, OpStrategy
+from torch.distributed.tensor._ops._matrix_ops import (
+    mm_single_dim_strategy,
+    mm_strategy,
+)
+from torch.distributed.tensor._ops._pointwise_ops import (
+    pointwise_strategy,
+    single_mesh_dim_pointwise_strategy,
+)
+from torch.distributed.tensor._ops.utils import _expand_single_dim_strategy_to_mesh
+
+
+def _gen_tensor_meta(shape):
+    empty_tensor = torch.empty(shape)
+    return TensorMeta(
+        empty_tensor.shape,
+        empty_tensor.stride(),
+        empty_tensor.dtype,
+    )
+
+
+def _op_schema(mesh, op_overload, inp_shapes, inp_dim_maps):
+    """
+    Not sure how dim_maps work, but they are some short-hand for specifying shardings on tensors
+    """
+    args_schema = tuple(
+        [
+            OpStrategy(
+                [
+                    OpSpec(
+                        DTensorSpec.from_dim_map(
+                            mesh, dim_map, sums=[], tensor_meta=_gen_tensor_meta(shape)
+                        )
+                    )
+                ]
+            )
+            for (shape, dim_map) in zip(inp_shapes, inp_dim_maps)
+        ]
+    )
+    return OpSchema(op_overload, args_schema, {})
+
+
+pow = 6
+world_size = 2**pow
+torch.distributed.init_process_group(
+    "fake",
+    rank=0,
+    world_size=world_size,
+)
+initial_dims = [
+    2,
+] * pow
+dims_list = []
+for i in range(1, pow + 1):
+    dims_list.append(
+        [
+            math.prod(initial_dims[:i]),
+        ]
+        + initial_dims[i:]
+    )
+meshes = {f"mesh_{len(dims)}d": init_device_mesh("cpu", dims) for dims in dims_list}
+
+tests = {
+    "mm": {
+        "strategy": mm_strategy,
+        "single_dim_strategy": _expand_single_dim_strategy_to_mesh(
+            mm_single_dim_strategy
+        ),
+        "inputs": (
+            (256, 256),
+            (256, 256),
+        ),
+        "dim_maps": (
+            # hopefully -1 means replicate in dim_maps?
+            [-1, -1],
+            [-1, -1],
+        ),
+        "op_overload": torch.ops.aten.mm.default,
+    },
+    "add": {
+        "strategy": pointwise_strategy,
+        "single_dim_strategy": _expand_single_dim_strategy_to_mesh(
+            single_mesh_dim_pointwise_strategy
+        ),
+        "inputs": (
+            (256, 256),
+            (256, 256),
+        ),
+        "dim_maps": (
+            # hopefully -1 means replicate in dim_maps?
+            [-1, -1],
+            [-1, -1],
+        ),
+        "op_overload": torch.ops.aten.div.Tensor,
+    },
+}
+
+repeats = 10
+
+
+def time_fn(mesh, test, key):
+    fn = test[key]
+    schema = _op_schema(mesh, test["op_overload"], test["inputs"], test["dim_maps"])
+
+    strategies = None
+
+    def f():
+        nonlocal strategies
+        strategies = fn(schema)
+
+    t = timeit.timeit(f, number=repeats) / repeats
+    return t, strategies
+
+
+times = {}
+strategies = {}
+for test_name, test in tests.items():
+    times[test_name] = {}
+    strategies[test_name] = {}
+    for mesh_name, mesh in meshes.items():
+        orig_t, orig_s = time_fn(mesh, test, "strategy")
+        single_t, single_s = time_fn(mesh, test, "single_dim_strategy")
+        times[test_name][mesh_name] = {
+            "strategy": orig_t,
+            "single_dim_strategy": single_t,
+        }
+        strategies[test_name][mesh_name] = {
+            "strategy": orig_s,
+            "single_dim_strategy": single_s,
+        }
+        slowdown = single_t / orig_t
+        print(
+            f"{test_name=}, {mesh_name=}, time for expanded strategy:{single_t * 1000:.2f}ms ({slowdown=:.2f}x)"
+            f" {len(orig_s.strategies)=}, {len(single_s.strategies)=}"
+        )
+
+torch.distributed.destroy_process_group()
