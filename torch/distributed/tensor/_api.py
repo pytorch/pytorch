8ad78bbc2b9c3606f05b882d3e7563c3dd6c8b88
Edward Yang <ezyang@meta.com>
2025-11-20 17:48:51 -0500

    Revert C++ fastpath dispatch path for DTensor (#168264)
    ```
    git revert --no-commit 567dcdba757 200156e3850 3d801a4c01f 2034ca99ae7 480b4ff8828 f570e589da1
    ```
    
        And Revert "[DTensor] Document fast-path dispatch (#168192)"
        And Revert "[DTensor] Fix deadlock after fast cache clear (#168069)"
    
    Reverts:
    * #167860
    * #167588
    * #167475
    * #166808
    * #166372
    * #168192
    * #168069
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168264
    Approved by: https://github.com/seemethere, https://github.com/malfet

--- torch/distributed/tensor/_api.py
+++ torch/distributed/tensor/_api.py
@@ -1,7 +1,6 @@
 # mypy: allow-untyped-decorators
 # mypy: allow-untyped-defs
 # Copyright (c) Meta Platforms, Inc. and affiliates
-import copy
 import inspect
 import warnings
 from collections.abc import Callable, Sequence
@@ -97,23 +96,16 @@ class _ToTorchTensor(torch.autograd.Function):
         )
         tensor_stride = tuple(tensor_stride)
         grad_placements = grad_placements or dtensor_spec.placements
-        if (
-            tensor_stride == dtensor_meta.stride
-            and grad_placements == dtensor_spec.placements
-        ):
-            # Avoid actual sharing of specs in case they're modified during (e.g.)
-            # sharding propagation.
-            grad_spec = copy.copy(dtensor_spec)
-        else:
-            grad_spec = DTensorSpec(
-                mesh,
-                grad_placements,
-                tensor_meta=TensorMeta(
-                    shape=dtensor_meta.shape,
-                    stride=tensor_stride,
-                    dtype=dtensor_meta.dtype,
-                ),
-            )
+        grad_spec = DTensorSpec(
+            mesh,
+            grad_placements,
+            tensor_meta=TensorMeta(
+                shape=dtensor_meta.shape,
+                stride=tensor_stride,
+                dtype=dtensor_meta.dtype,
+            ),
+        )
+
         return (
             # pyrefly: ignore [bad-argument-type]
             DTensor(
@@ -346,14 +338,14 @@ class DTensor(torch.Tensor):
         )
 
     @classmethod
+    @torch._disable_dynamo
+    # pyre-fixme[3]: Return type must be annotated.
+    # pyre-fixme[2]: Parameter must be annotated.
     def __torch_dispatch__(cls, func, types, args=(), kwargs=None):  # type: ignore[override]
-        # We just need to have an implementation here; the __torch_dispatch__ machinery
-        # calls into a specific C++ fast path that doesn't call here.
-        # See #167051 for details
-        # python_arg_parser.cpp: dispatch_on_subclass()
-        # -> python_variable.cpp: dispatchDTensorOp()
-        raise NotImplementedError(
-            "DTensor.__torch_dispatch__ should not actually get called"
+        return DTensor._op_dispatcher.dispatch(
+            func,
+            args,
+            kwargs or {},
         )
 
     @staticmethod
