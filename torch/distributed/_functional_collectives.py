f39bebf998e42500008ce429137fbbbe1f8bf484
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-11-21 03:25:32 -0800

    [dist] add reduce_scatter_out
    ghstack-source-id: 74df917ef7d386028c4622bcb247f022369eb09d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168260

--- torch/distributed/_functional_collectives.py
+++ torch/distributed/_functional_collectives.py
@@ -1000,6 +1000,14 @@ def _reduce_scatter_tensor_native_meta(inp, reduce_op, group_size, group_name):
     return inp.new_empty(shape)
 
 
+def _reduce_scatter_tensor_out_native_meta(
+    inp, reduce_op, group_size, group_name, *, out
+):
+    shape = list(inp.size())
+    shape[0] //= group_size
+    return inp.new_empty(shape)
+
+
 def _reduce_scatter_tensor_coalesced_native_meta(
     inputs, reduce_op, group_size, group_name
 ):
@@ -1026,6 +1034,9 @@ lib_impl.impl(
     "Meta",
 )
 lib_impl.impl("reduce_scatter_tensor", _reduce_scatter_tensor_native_meta, "Meta")
+lib_impl.impl(
+    "reduce_scatter_tensor_out", _reduce_scatter_tensor_out_native_meta, "Meta"
+)
 lib_impl.impl(
     "reduce_scatter_tensor_coalesced",
     _reduce_scatter_tensor_coalesced_native_meta,
