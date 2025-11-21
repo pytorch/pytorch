0d5015da4584ce111893df90aaa870bbab645d6b
fengqing.lu <fengqing.lu@intel.com>
2025-11-20 18:15:15 -0800

    set compupte_logsumexp = true as default

--- test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Extra.cpp
+++ test/cpp_extensions/open_registration_extension/torch_openreg/csrc/aten/native/Extra.cpp
@@ -47,8 +47,7 @@ _scaled_dot_product_fused_attention_overrideable(
     double dropout_p,
     bool is_causal,
     bool return_debug_mask,
-    std::optional<double> scale,
-    bool compute_log_sumexp) {
+    std::optional<double> scale) {
   const int64_t batch_size = query.size(0);
   const int64_t num_heads = query.size(1);
   const int64_t head_dim_v = value.size(3);
