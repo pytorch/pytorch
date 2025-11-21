15ec5ae9916aa568d50a6ab52bbc58790f2a35bd
Jeff Daily <jeff.daily@amd.com>
2025-11-21 21:42:40 +0000

    [ROCm] support expandable segments

--- c10/cuda/CUDAAllocatorConfig.h
+++ c10/cuda/CUDAAllocatorConfig.h
@@ -34,7 +34,7 @@ class C10_CUDA_API CUDAAllocatorConfig {
   static bool expandable_segments() {
     bool enabled = c10::CachingAllocator::AcceleratorAllocatorConfig::
         use_expandable_segments();
-#ifndef PYTORCH_C10_DRIVER_API_SUPPORTED
+#if !defined(PYTORCH_C10_DRIVER_API_SUPPORTED) && !defined(USE_ROCM)
     if (enabled) {
       TORCH_WARN_ONCE("expandable_segments not supported on this platform")
     }
