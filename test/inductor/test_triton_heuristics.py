40e7cdf641a9267cb9ad0e93fc8d3ea8e52ed424
Amin Sedaghat <amin32846@gmail.com>
2025-11-22 14:59:58 -0500

    inductor: add persistent mm/bmm config cap tests for native matmul

--- test/inductor/test_triton_heuristics.py
+++ test/inductor/test_triton_heuristics.py
@@ -124,6 +124,38 @@ class TestTritonHeuristics(TestCase):
             r = cfg.kwargs.get("R0_BLOCK", 1)
             self.assertLessEqual(x * y * r, TRITON_MAX_TENSOR_NUMEL)
 
+    def test_native_matmul_persistent_mask_tensor_product_cap(self):
+        """
+        Ensure persistent native matmul configs (mm/bmm) also respect the cap.
+        """
+        from torch._inductor.runtime import triton_heuristics as TH
+
+        mm_persistent = TH._persistent_reduction_configs(
+            size_hints={"x": 1, "r0_": 1, "y": 1},
+            reduction_hint=False,
+            inductor_meta={},
+            triton_meta={"native_matmul": True},
+        )
+        # It's acceptable that persistent configs may be empty in some builds,
+        # but if present, they must obey the cap.
+        for cfg in mm_persistent:
+            x = cfg.kwargs.get("XBLOCK", 1)
+            y = cfg.kwargs.get("YBLOCK", 1)
+            r = cfg.kwargs.get("R0_BLOCK", 1)
+            self.assertLessEqual(x * y * r, TRITON_MAX_TENSOR_NUMEL)
+
+        bmm_persistent = TH._persistent_reduction_configs(
+            size_hints={"x": 1, "y": 1, "z": 1, "r0_": 1},
+            reduction_hint=False,
+            inductor_meta={},
+            triton_meta={"native_matmul": True},
+        )
+        for cfg in bmm_persistent:
+            x = cfg.kwargs.get("XBLOCK", 1)
+            y = cfg.kwargs.get("YBLOCK", 1)
+            r = cfg.kwargs.get("R0_BLOCK", 1)
+            self.assertLessEqual(x * y * r, TRITON_MAX_TENSOR_NUMEL)
+
     def _test_artificial_zgrid(self):
         def forward(primals_1, primals_2, primals_5):
             view = torch.ops.aten.reshape.default(primals_5, [-1, 2, 4])
