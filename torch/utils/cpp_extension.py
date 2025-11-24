3e889b41fe2867c6ba5174f8d7349bd3752227ba
Felix Marty <Felix.Marty@amd.com>
2025-11-24 19:50:05 +0100

    compile rocm kernels only for user arch

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -2475,13 +2475,24 @@ def _get_rocm_arch_flags(cflags: Optional[list[str]] = None) -> list[str]:
     # Allow env var to override, just like during initial cmake build.
     _archs = os.environ.get('PYTORCH_ROCM_ARCH', None)
     if not _archs:
-        archFlags = torch._C._cuda_getArchFlags()
-        if archFlags:
-            archs = archFlags.split()
-        else:
-            archs = []
+        arch_set = set()
+        # the assumption is that the extension should run on any of the currently visible cards,
+        # which could be of different types - therefore all archs for visible cards should be included
+        for i in range(torch.cuda.device_count()):
+            device_properties = torch.cuda.get_device_properties(i)
+            if hasattr(device_properties, "gcnArchName"):
+                device_arch = (device_properties.gcnArchName).split(":", 1)[0]
+                arch_set.add(device_arch)
+        
+        archs = ";".join(arch_set)
+
+        logger.warning(
+            f"The environment variable `PYTORCH_ROCM_ARCH` is not set, all archs for visible cards are included for compilation ({archs}).\n"
+            "If this is not desired, please set the environment variable `PYTORCH_ROCM_ARCH` to specific architectures.")
     else:
-        archs = _archs.replace(' ', ';').split(';')
+        archs = _archs.replace(' ', ';')
+    
+    archs = archs.split(';')
     flags = [f'--offload-arch={arch}' for arch in archs]
     flags += [] if has_gpu_rdc_flag else ['-fno-gpu-rdc']
     return flags
