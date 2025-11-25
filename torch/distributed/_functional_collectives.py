287220465a87782d539f60b024d190c9323c0b35
Vishal Nandavanam <vishal9@meta.com>
2025-11-24 19:35:56 -0800

    Update on "Differentiability Support for Functional Collectives"
    Fixes https://github.com/pytorch/pytorch/issues/152131 
    
    
    [ghstack-poisoned]

--- torch/distributed/_functional_collectives.py
+++ torch/distributed/_functional_collectives.py
@@ -10,6 +10,7 @@ import torch.distributed as dist
 import torch.distributed.distributed_c10d as c10d
 from torch.distributed.device_mesh import DeviceMesh
 from torch.fx.experimental.proxy_tensor import get_proxy_mode
+
 from . import _functional_collectives_impl as fun_col_impl
 
 
