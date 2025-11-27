94e02fe70690e5dbe4b756b6eaa4052dfcebccf4
mengfei25 <mengfei.li@intel.com>
2025-11-27 13:59:14 +0800

    update to 789711cc8dc1b1e163ce74f289849150464395e1

--- cmake/Modules/FindMKLDNN.cmake
+++ cmake/Modules/FindMKLDNN.cmake
@@ -47,7 +47,7 @@ IF(NOT MKLDNN_FOUND)
     endif()
     ExternalProject_Add(xpu_mkldnn_proj
       GIT_REPOSITORY https://github.com/uxlfoundation/oneDNN
-      GIT_TAG a1602472f5ee3d62dc0b3394604b98b4290ceeb7 # v3.10.1
+      GIT_TAG 789711cc8dc1b1e163ce74f289849150464395e1 # v3.10.1
       PREFIX ${XPU_MKLDNN_DIR_PREFIX}
       BUILD_IN_SOURCE 0
       CMAKE_ARGS  -DCMAKE_C_COMPILER=icx
