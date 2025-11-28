612ab58eddbead350a9cca724edef671bcddd70e
fengqing.lu <fengqing.lu@intel.com>
2025-11-27 19:07:52 -0800

    refine bias.py

--- torch/nn/attention/bias.py
+++ torch/nn/attention/bias.py
@@ -232,28 +232,23 @@ class CausalBias(torch.Tensor):
                 query, key, value, None, dropout_p, is_causal, enable_gqa
             )
             if can_use_flash_attention(sdpa_params):
-                if query.device.type == "cuda":
-                    needs_padding = query.size(-1) % 8 != 0
-                    og_head_size = query.size(-1)
-                    og_scale = _calculate_scale(og_head_size, scale)
-                    if needs_padding:
-                        query = torch.nn.functional.pad(query, (0, 8 - query.size(-1) % 8))
-                        key = torch.nn.functional.pad(key, (0, 8 - key.size(-1) % 8))
-                        value = torch.nn.functional.pad(value, (0, 8 - value.size(-1) % 8))
-                elif query.device.type == "xpu":
-                    # XPU FlashAttention needs to pad head dim to multiple of 64
-                    needs_padding = query.size(-1) % 64 != 0
-                    og_head_size = query.size(-1)
-                    og_scale = _calculate_scale(og_head_size, scale)
-                    if needs_padding:
-                        pad_len = 64 - query.size(-1) % 64
-                        query = torch.nn.functional.pad(query, (0, pad_len))
-                        key = torch.nn.functional.pad(key, (0, pad_len))
-                        value = torch.nn.functional.pad(value, (0, pad_len))
+                device_type = query.device.type
+                if device_type == "cuda":
+                    alignment = 8
+                elif device_type == "xpu":
+                    alignment = 64
                 else:
                     raise RuntimeError(
                         "Flash Attention is only supported on CUDA and XPU devices."
                     )
+                og_head_size = query.size(-1)
+                og_scale = _calculate_scale(og_head_size, scale)
+                needs_padding = og_head_size % alignment != 0
+                if needs_padding:
+                    pad_len = alignment - (og_head_size % alignment)
+                    query = torch.nn.functional.pad(query, (0, pad_len))
+                    key = torch.nn.functional.pad(key, (0, pad_len))
+                    value = torch.nn.functional.pad(value, (0, pad_len))
                 out = torch.ops.aten._scaled_dot_product_flash_attention(
                     query,
                     key,
