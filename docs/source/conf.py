b5c7f90fe407c7ac09c103a4cd76d2ee2b212766
Xinya Zhang <Xinya.Zhang@amd.com>
2025-12-02 16:31:04 +0000

    Follow the practice of cuBLASModule and ignore MathSDPModule in docs generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1204,6 +1204,7 @@ coverage_ignore_classes = [
     "PropModule",
     # torch.backends.cuda
     "cuBLASModule",
+    "MathSDPModule",
     "cuFFTPlanCache",
     "cuFFTPlanCacheAttrContextProp",
     "cuFFTPlanCacheManager",
