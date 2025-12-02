6c186d07af49ba75ec5e9ebb12601afcb54c47b6
Simon Fan <xmfan@meta.com>
2025-12-01 17:17:57 -0800

    Update
    [ghstack-poisoned]

--- test/dynamo/test_backends.py
+++ test/dynamo/test_backends.py
@@ -7,6 +7,7 @@ import torch._dynamo
 import torch._dynamo.backends
 import torch._dynamo.test_case
 from torch._dynamo.backends.debugging import ExplainWithBackend
+from torch._dynamo.backends.registry import lookup_backend
 from torch._dynamo.backends.tvm import has_tvm
 from torch._dynamo.testing import same
 from torch.fx._lazy_graph_module import _force_skip_lazy_graph_module
@@ -102,10 +103,23 @@ class TestOptimizations(torch._dynamo.test_case.TestCase):
     def _check_backend_works(self, backend, device, options=None):
         model = Seq().eval()
         model.to(device)
-        input = torch.randn(2, 10, device=device)
-        r1 = model(input)
-        r2 = torch.compile(model, backend=backend, options=options)(input)
+
+        def boxed_assert(gm, *example_args):
+            fn = lookup_backend(backend)(gm, *example_args)
+            assert fn._boxed_call
+            return fn
+
+        compiled_model = torch.compile(model, backend=boxed_assert, options=options)
+
+        input1 = torch.randn(2, 10, device=device, requires_grad=True)
+        input2 = input1.detach().clone().requires_grad_(True)
+        r1 = model(input1)
+        r2 = compiled_model(input2)
         self.assertTrue(same(r1, r2.float(), tol=0.01))
+
+        r1.sum().backward()
+        r2.sum().backward()
+        self.assertTrue(same(input1.grad, input2.grad.float(), tol=0.01))
 
     def test_eager(self, device):
         self._check_backend_works("eager", device)
