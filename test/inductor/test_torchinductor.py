c9b3e94e90b37d2b819f783dbdcd12be41dee20a
Rahul Jain <rahjain@amd.com>
2025-12-03 12:39:29 -0600

    remove skipIfRocm

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -4293,7 +4293,6 @@ class CommonTemplate:
                 (v,),
             )
 
-    @skipIfRocm
     @xfail_if_mps  # Expected to find .run(
     def test_conv_inference_heuristics(self):
         if self.device != GPU_TYPE:
