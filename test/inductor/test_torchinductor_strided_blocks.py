6103cfe4e9df70b4f9e47714b2b7c67fb536a874
AmdSampsa <sampsa.riikonen@amd.com>
2025-12-03 12:57:59 +0000

    etc

--- test/inductor/test_torchinductor_strided_blocks.py
+++ test/inductor/test_torchinductor_strided_blocks.py
@@ -1401,8 +1401,8 @@ test_torchinductor.copy_tests(CommonTemplate, TritonBlockPointerTestGPU, GPU_TYP
     not (
         HAS_CUDA_AND_TRITON
         and torch.cuda.get_device_capability()[0] >= 9
-        and torch.version.hip is not None
-    ),
+    )
+    and torch.version.hip is None,
     "Requires Triton CUDA backend and CUDA compute capability >= 9.0",
 )
 @config.patch({"triton.use_tensor_descriptor": True, "assume_aligned_inputs": True})
