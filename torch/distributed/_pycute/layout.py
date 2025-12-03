2dea6d6638d4ec81a1e213923db2ec7864b01bac
cyy <cyyever@outlook.com>
2025-12-03 08:17:58 +0800

    More fixes
    Signed-off-by: cyy <cyyever@outlook.com>

--- torch/distributed/_pycute/layout.py
+++ torch/distributed/_pycute/layout.py
@@ -36,7 +36,7 @@ of lexicographic instead of co-lexicographic as implemented in the original layo
 """
 
 from itertools import chain
-from typing import Optional, TypeAlias, Union
+from typing import TypeAlias, Union
 from typing_extensions import TypeIs
 
 from .int_tuple import (
@@ -53,12 +53,9 @@ from .int_tuple import (
 
 
 # Type aliases
-LayoutOrIntTuple: TypeAlias = Union["Layout", IntTuple]
-LayoutProfile: TypeAlias = Optional[Union[tuple[object, ...], "Layout"]]
-LayoutInput: TypeAlias = Optional[Union["Layout", IntTuple, tuple[object, ...]]]
-CoordinateType: TypeAlias = Optional[
-    Union[int, IntTuple, tuple[object, ...]]
-]  # Input for slice_ and crd2idx functions
+CoordinateType: TypeAlias = (
+    int | IntTuple | tuple[object, ...] | None
+)  # Input for slice_ and crd2idx functions
 
 
 class LayoutBase:
@@ -70,7 +67,7 @@ def is_layout(x: object) -> TypeIs["Layout"]:
 
 
 class Layout(LayoutBase):
-    def __init__(self, _shape: IntTuple, _stride: Optional[IntTuple] = None) -> None:
+    def __init__(self, _shape: IntTuple, _stride: IntTuple | None = None) -> None:
         self.shape = _shape
         if _stride is None:
             self.stride = suffix_product(self.shape)
@@ -135,8 +132,14 @@ class Layout(LayoutBase):
         return f"Layout({self.shape},{self.stride})"
 
 
+# Type aliases
+LayoutOrIntTuple: TypeAlias = Layout | IntTuple
+LayoutProfile: TypeAlias = tuple[object, ...] | Layout | None
+LayoutInput: TypeAlias = Layout | IntTuple | tuple[object, ...] | None
+
+
 # Make Layout from a list of layouts (each layout it's own mode in the result)
-def make_layout(*layouts: Union[Layout, tuple[Layout, ...]]) -> Layout:
+def make_layout(*layouts: Layout | tuple[Layout, ...]) -> Layout:
     if len(layouts) == 1 and not is_layout(layouts[0]):
         layouts = layouts[0]
 
@@ -321,7 +324,7 @@ def complement(layout: LayoutOrIntTuple, max_idx: int = 1) -> Layout:
 
 
 # Layout right inverse
-def right_inverse(layout: Optional[LayoutOrIntTuple]) -> Optional[Layout]:
+def right_inverse(layout: LayoutOrIntTuple | None) -> Layout | None:
     if layout is None:
         return None
     elif is_int(layout):
@@ -350,7 +353,7 @@ def right_inverse(layout: Optional[LayoutOrIntTuple]) -> Optional[Layout]:
 
 
 # Layout left inverse
-def left_inverse(layout: Optional[LayoutOrIntTuple]) -> Optional[Layout]:
+def left_inverse(layout: LayoutOrIntTuple | None) -> Layout | None:
     if layout is None:
         return None
     elif is_int(layout):
