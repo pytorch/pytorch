c7ac58ebafceee8e2c45e7aaf0278eae2d27f814
Aaron Orenstein <aorenste@meta.com>
2025-12-04 11:22:07 -0800

    Add GroupName NewType (#167552)
    Use a NewType for `GroupName` instead of a raw string so that the typechecker has a chance to notice misuse - the only place we should be turning a raw string into a `GroupName` is where we create the ProcessGroup and assign the name in the first place. Everywhere else it should just be used as an opaque value.
    
    While in there I added a few trivial types to some parameters in distributed_c10d.py as well.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167552
    Approved by: https://github.com/xmfan

--- torch/distributed/_functional_collectives.py
+++ torch/distributed/_functional_collectives.py
@@ -94,7 +94,7 @@ RANK_TYPES = Union[
     dist.ProcessGroup,
     DeviceMesh,
     tuple["dist.tensor.DeviceMesh", int],
-    str,
+    c10d.GroupName,
 ]
 
 
@@ -778,7 +778,7 @@ def _expand_group(group: RANK_TYPES, tag: str = "") -> tuple[str, list[int], int
     return (tag, rankset, group_size)
 
 
-def _resolve_group_name(group: RANK_TYPES, tag: str = "") -> str:
+def _resolve_group_name(group: RANK_TYPES, tag: str = "") -> c10d.GroupName:
     """
     Given group in RANK_TYPES, return the group name.
     """
@@ -787,7 +787,7 @@ def _resolve_group_name(group: RANK_TYPES, tag: str = "") -> str:
     if isinstance(group, dist.ProcessGroup):
         return group.group_name
     elif isinstance(group, str):
-        return group
+        return c10d.GroupName(group)
     elif isinstance(group, DeviceMesh):
         if group.ndim != 1:
             raise AssertionError(
