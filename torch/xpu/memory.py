f281cafc615bf22afc6f43e97fa47ad61ba75edb
Yu, Guangye <guangye.yu@intel.com>
2025-12-04 17:57:04 +0000

    Update
    [ghstack-poisoned]

--- torch/xpu/memory.py
+++ torch/xpu/memory.py
@@ -1,6 +1,7 @@
 import collections
 import ctypes
-from typing import Any, Union
+import sys
+from typing import Any, Literal, Optional, Union
 
 import torch
 from torch._utils import _dummy_type
@@ -264,6 +265,84 @@ def memory_snapshot(
     if not is_initialized():
         return []
     return torch._C._xpu_memorySnapshot(mempool_id)["segments"]
+
+
+def _record_memory_history(
+    enabled: Optional[Literal["state", "all"]] = "all",
+    context: Optional[Literal["state", "alloc", "all"]] = "all",
+    stacks: Literal["python", "all"] = "all",
+    max_entries: int = sys.maxsize,
+    clear_history: bool = False,
+) -> None:
+    """Enable recording of stack traces associated with memory
+    allocations, so you can tell what allocated any piece of memory in
+    :func:`torch.xpu.memory._snapshot()`.
+
+    In addition to keeping stack traces with each current allocation and free,
+    this will also enable recording of a history of all alloc/free events.
+
+    Use :func:`torch.xpu.memory._snapshot()` to retrieve this information,
+    and the tools in `_memory_viz.py` to visualize snapshots.
+
+    Buffer behavior
+    ---------------
+
+    This will store up to `max_entries` instances of `TraceEntry` when enabled.
+    Python trace collection defaults to `sys.maxsize`, meaning long-running
+    or indefinitely running jobs should set a reasonable limit to avoid excessive
+    memory use. Expect each entry to be several KB.
+
+    Longer running workflows or those with smaller `max_entries` values will only
+    store the last accumulated `max_entries` entries, meaning new entries overwrite
+    older entries.
+
+    C++ implementation for reference to ring buffer implementation:
+
+    .. code-block:: cpp
+
+        if (record_history) {
+          if (alloc_trace->size() < alloc_trace_max_entries_) {
+            alloc_trace->emplace_back(te);
+          } else {
+            (*alloc_trace)[alloc_trace_next++] = te;
+            if (alloc_trace_next == alloc_trace_max_entries_) {
+              alloc_trace_next = 0;
+            }
+          }
+        }
+
+    Latency impact
+    --------------
+
+    The Python trace collection is fast (2us per trace), so you may consider
+    enabling this on production jobs if you anticipate ever having to debug
+    memory issues.
+
+    C++ trace collection is also fast (~50ns/frame), which for many typical programs
+    works out to ~2us per trace, but can vary depending on stack depth.
+
+    Args:
+        enabled (Literal[None, "state", "all"], optional):
+            `None`, disable recording memory history.
+            `"state"`, keep information for currently allocated memory.
+            `"all"`, additionally keep a history of all alloc/free calls.
+            Defaults to "all".
+        context (Literal[None, "state", "alloc", "all"], optional):
+            `None`, Do not record any tracebacks.
+            `"state"`, Record tracebacks for currently allocated memory.
+            `"alloc"`, additionally keep tracebacks for alloc calls.
+            `"all"`, additionally keep tracebacks for free calls.
+            Defaults to "all".
+        stacks (Literal["python", "all"], optional):
+            `"python"`, include Python, TorchScript, and inductor frames in tracebacks
+            `"all"`, additionally include C++ frames
+            Defaults to "all".
+        max_entries (int, optional): Keep a maximum of `max_entries`
+            alloc/free events in the recorded history recorded.
+    """
+    torch._C._xpu_recordMemoryHistory(
+        enabled, context, stacks, max_entries, clear_history
+    )
 
 
 class _XPUAllocator:
