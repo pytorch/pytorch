dc3975d6367cadd7ae0a301d277a3c042445b6e4
FFFrog <ljw1101.vip@gmail.com>
2025-12-04 16:22:14 +0800

    [OpenReg] Add poison fork check for OpenReg
    **Background**:
    
    ```bash
    pytest -s tests/test_autocast.py -vv
    
    tests/test_autocast.py::TestAutocastoOpenReg::test_openreg_autocast_float16_supported /home/coder/code/pytorch/torch/_dynamo/eval_frame.py:1037: UserWarning: Set seed for `openreg` device does not take effect, please add API's `_is_in_bad_fork` and `manual_seed_all` to `openreg` device module.
    ```
    
    Therefore, we need to add `_is_in_bad_fork` to OpenReg.
    
    In addition, the OpenReg backend uses the CPU to simulate multi-threaded implementations of Stream and Event, so fork prevention is indeed necessary after initialization.
    
    ghstack-source-id: 2e2b12453c5ba6cf637189187611c2127cb2f4e4
    Pull-Request: https://github.com/pytorch/pytorch/pull/165728

--- test/cpp_extensions/open_registration_extension/torch_openreg/tests/test_device.py
+++ test/cpp_extensions/open_registration_extension/torch_openreg/tests/test_device.py
@@ -1,8 +1,10 @@
 # Owner(s): ["module: PrivateUse1"]
 
+import multiprocessing
+
 import torch
 from torch.testing._internal.common_dtype import get_all_dtypes
-from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.testing._internal.common_utils import run_tests, skipIfWindows, TestCase
 
 
 class TestDevice(TestCase):
@@ -37,6 +39,36 @@ class TestDevice(TestCase):
         expected_dtypes = get_all_dtypes(include_complex32=True, include_qint=True)
 
         self.assertTrue(all(dtype in supported_dtypes for dtype in expected_dtypes))
+
+    @skipIfWindows(msg="Fork not available on Windows")
+    def test_device_poison_fork(self):
+        # First, initialize in the parent process
+        torch.openreg.init()
+
+        def child(q):
+            try:
+                # Second, try to initialize in the child process
+                torch.openreg.init()
+            except Exception as e:
+                q.put(e)
+
+        ctx = multiprocessing.get_context("fork")
+        q = ctx.Queue()
+        p = ctx.Process(target=child, args=(q,))
+        p.start()
+        p.join()
+
+        self.assertTrue(not q.empty())
+
+        exc = q.get()
+        with self.assertRaisesRegex(
+            RuntimeError,
+            (
+                "Cannot re-initialize OpenReg in forked subprocess. "
+                "To use OpenReg with multiprocessing, you must use the 'spawn' start method"
+            ),
+        ):
+            raise exc
 
 
 if __name__ == "__main__":
