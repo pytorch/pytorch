2c6976b1e5c0342fab6e582afb0639a243c8794f
William Wen <williamwen@meta.com>
2025-12-03 18:25:08 -0800

    Update base for Update on "[dynamo] prevent recompilation limit exceeded on external_utils.wrap_inline"
    This will prevent recompilations (i.e. logs to TORCH_LOGS="recompiles") due to wrap_inline's guard on `fn`. If we call wrap_inline on enough different functions, we would hit the recompile limit.
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang amjames Lucaskabela
    
    [ghstack-poisoned]

--- test/dynamo/test_exc.py
+++ test/dynamo/test_exc.py
@@ -177,7 +177,9 @@ from user code:
             munge_exc(record.getMessage()),
             """\
 Graph break in user code at test_exc.py:N
-Graph Break Reason: Call to `torch._dynamo.graph_break()`
+Graph Break Reason: Encountered graph break when attempting to trace CALL: a function call, e.g. f(x, y):
+
+Call to `torch._dynamo.graph_break()`
   Explanation: User-inserted graph break. Message: None
   Hint: Remove the `torch._dynamo.graph_break()` call.
 
