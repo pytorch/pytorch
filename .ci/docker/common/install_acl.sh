395138e11c05597764d7ba3ef91005f630bb833e
Puneet Matharu <puneet.matharu@arm.com>
2025-12-04 12:22:21 +0000

    Add ccache support to ACL/OpenBLAS and manywheel build script.

--- .ci/docker/common/install_acl.sh
+++ .ci/docker/common/install_acl.sh
@@ -6,6 +6,18 @@ set -eux
 ACL_VERSION=${ACL_VERSION:-"v52.6.0"}
 ACL_INSTALL_DIR="/acl"
 
+# Optional ccache support
+USE_CCACHE=${USE_CCACHE:-0}
+SCONS_CACHE_ARG=""
+if [ "${USE_CCACHE}" != "0" ]; then
+  if command -v ccache >/dev/null 2>&1; then
+    echo "Using ccache for ACL build"
+    SCONS_CACHE_ARG="compiler_cache=ccache"
+  else
+    echo "USE_CCACHE is set but 'ccache' is not available on PATH; proceeding without ccache"
+  fi
+fi
+
 # Clone ACL
 git clone https://github.com/ARM-software/ComputeLibrary.git -b "${ACL_VERSION}" --depth 1 --shallow-submodules
 
@@ -14,7 +26,8 @@ ACL_CHECKOUT_DIR="ComputeLibrary"
 pushd $ACL_CHECKOUT_DIR
 scons -j8  Werror=0 debug=0 neon=1 opencl=0 embed_kernels=0 \
   os=linux arch=armv8a build=native multi_isa=1 \
-  fixed_format_kernels=1 openmp=1 cppthreads=0
+  fixed_format_kernels=1 openmp=1 cppthreads=0 \
+  ${SCONS_CACHE_ARG}
 popd
 
 # Install ACL
