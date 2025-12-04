685ba6bc01170c0cb793f872e56164df89b63573
Sherlock Huang <baihan.huang@gmail.com>
2025-12-03 22:07:11 -0800

    add back legalize_graph for BC reason (#169541)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/169541
    Approved by: https://github.com/huydhn

--- torch/fx/passes/utils/fuser_utils.py
+++ torch/fx/passes/utils/fuser_utils.py
@@ -7,7 +7,12 @@ from torch.fx._compatibility import compatibility
 from torch.fx.graph import Graph
 from torch.fx.graph_module import GraphModule
 from torch.fx.node import Node
-from torch.fx.passes.tools_common import NodeList, NodeSet, stable_topological_sort
+from torch.fx.passes.tools_common import (  # noqa: F401
+    legalize_graph,
+    NodeList,
+    NodeSet,
+    stable_topological_sort,
+)
 from torch.fx.passes.utils import lift_subgraph_as_module  # type: ignore[attr-defined]
 
 
