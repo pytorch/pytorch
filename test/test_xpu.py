e595b921992abf6b79adca68d2fffd16f7ab9b21
Ma, Jing1 <jing1.ma@intel.com>
2025-12-04 11:25:13 +0000

    Add test cases
    Signed-off-by: Ma, Jing1 <jing1.ma@intel.com>

--- test/test_xpu.py
+++ test/test_xpu.py
@@ -1373,6 +1373,24 @@ if __name__ == "__main__":
         g.reset()
         del g
 
+    @unittest.skipIf(not TEST_XPU, "XPU not available, skipping tests")
+    def test_graph_timing(self):
+        torch.xpu.empty_cache()
+        x = torch.randn(10240000, device="xpu")
+        y = torch.rand_like(x)
+        g = torch.xpu.XPUGraph()
+        start_event = torch.xpu.Event(enable_timing=True, external=True)
+        end_event = torch.xpu.Event(enable_timing=True, external=True)
+        with torch.xpu.graph(g):
+            start_event.record()
+            z = x + y
+            end_event.record()
+        torch.xpu.synchronize()
+        g.replay()
+        torch.xpu.synchronize()
+        self.assertTrue(start_event.elapsed_time(end_event) > 0)
+
+    @unittest.skipIf(not TEST_XPU, "XPU not available, skipping tests")
     def test_graph_warn_if_has_zero_nodes(self):
         with warnings.catch_warnings(record=True) as caught:
             g = torch.xpu.XPUGraph()
@@ -1383,6 +1401,123 @@ if __name__ == "__main__":
         self.assertTrue(
             any("The XPU Graph is empty" in str(w.message) for w in caught)
         )
+
+    @unittest.skipIf(not TEST_XPU, "XPU not available, skipping tests")
+    def test_graph_capture_oom(self):
+        oom_regex = ("out of memory")
+        with self.assertRaisesRegex(RuntimeError, oom_regex):
+            with torch.xpu.graph(torch.xpu.XPUGraph()):
+                torch.zeros(2**40, device="xpu")
+
+    @unittest.skipIf(not TEST_XPU, "XPU not available, skipping tests")
+    def test_repeat_graph_capture_oneDNN_memory(self):
+        (x, y, z) = 1024, 512, 64
+        a = torch.rand((x, y), device="xpu")
+        b = torch.rand((y, z), device="xpu")
+
+        # warmup
+        torch.mm(a, b)
+
+        free_bytes_before, total_bytes = torch.xpu.mem_get_info()
+        used_gb_before = (total_bytes - free_bytes_before) / 1e9
+
+        for _ in range(100):
+            torch_graph = torch.xpu.XPUGraph()
+            with torch.xpu.graph(torch_graph):
+                torch.mm(a, b)
+            torch_graph.replay()
+
+        free_bytes_after, _ = torch.xpu.mem_get_info()
+        used_gb_after = (total_bytes - free_bytes_after) / 1e9
+
+        self.assertFalse(used_gb_before + 0.1 < used_gb_after)
+
+    def test_graph_rng_functional(self):
+        ops_with_kwargs = (
+            (torch.nn.functional.dropout, {"p": 0.1}),
+            (torch.nn.functional.rrelu, {"training": True}),
+        )
+        size = 10000
+
+        def run(op, kwargs):
+            a = torch.randn((size,), device="xpu", dtype=torch.float)
+
+            # Control
+            torch.xpu.manual_seed(5)
+            eager_out = a
+            for _ in range(6):
+                eager_out = op(eager_out, **kwargs)
+
+            graph_in = a.clone()
+            stream = torch.xpu.Stream()
+            stream.wait_stream(torch.xpu.current_stream())
+            with torch.xpu.stream(stream):
+                torch.xpu.manual_seed(5)
+
+                g = torch.xpu.XPUGraph()
+                torch.xpu.empty_cache()
+                g.capture_begin()
+                graph_out = graph_in
+                for _ in range(2):
+                    graph_out = op(graph_out, **kwargs)
+                g.capture_end()
+            torch.xpu.current_stream().wait_stream(stream)
+
+            # Runs a graphed->eager->graphed sequence of RNG ops.
+            # replay() plays 2 invocations of the op, so the sequence has 6
+            # invocations total, matching Control.
+            # replay() reads from graph_in and writes to graph_out.
+            g.replay()
+            out = op(graph_out, **kwargs)
+            out = op(out, **kwargs)
+            graph_in.copy_(out)
+            g.replay()
+
+            # If replay() updated RNG state correctly, graph_out
+            # should now hold data equal to eager_out.
+            try:
+                self.assertEqual(eager_out, graph_out)
+            except Exception as e:
+                raise RuntimeError("Failed on ", op) from e
+
+            # Do the same operations varying seeds
+            seeds = [6, 128, 9999]
+
+            for seed in seeds:
+                torch.xpu.manual_seed(seed)
+                graph_in.copy_(a)
+                for _ in range(3):
+                    g.replay()
+
+                # If the random seed was not updated then the graph would
+                # generate the same output as in previous check.
+                try:
+                    self.assertNotEqual(eager_out, graph_out)
+                except Exception as e:
+                    raise RuntimeError("Failed on ", op) from e
+
+                # Now repeat the same operations in non-graphed mode.
+                torch.xpu.manual_seed(seed)
+                for _ in range(3):
+                    eager_out.copy_(a)
+                    eager_out = op(eager_out, **kwargs)
+                    eager_out = op(eager_out, **kwargs)
+
+                # In the end, graph_out and eager_out must be equal
+                # as they went under the same set of operations.
+                try:
+                    self.assertEqual(eager_out, graph_out)
+                except Exception as e:
+                    raise RuntimeError("Failed on ", op) from e
+
+            # We hold references to all tensors used across streams up til this sync,
+            # so no need to call record_stream on those tensors.
+            torch.xpu.synchronize()
+
+        for op, kwargs in ops_with_kwargs:
+            run(op, kwargs)
+
+
 
 @unittest.skipIf(not TEST_XPU, "XPU not available, skipping tests")
 class TestXpuOps(TestCase):
