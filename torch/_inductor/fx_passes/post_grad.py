14ebc38c202f45778b7abd2dc40b8947c27b3b1a
Xingyuan Li <xingyuan.li@intel.com>
2025-12-04 03:15:34 +0000

    reformat

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -357,10 +357,11 @@ def prepare_softmax_extra_check(match):
     """
     We only have triton online softmax kernels currently.
     """
+    device_type = match.kwargs["x"].meta["val"].device.type
     return (
         config.online_softmax
-        and match.kwargs["x"].meta["val"].device.type in ["cuda", "xpu"]
-        and getattr(config, f'{match.kwargs["x"].meta["val"].device.type}_backend') == 'triton'
+        and device_type in ["cuda", "xpu"]
+        and getattr(config, f"{device_type}_backend") == "triton"
     )
 
 
