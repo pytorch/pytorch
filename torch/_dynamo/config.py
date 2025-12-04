b8147a4e455fd39af68045d09642eca9e7c27b43
Hinrik S. Gudmundsson <hgudmund@redhat.com>
2025-12-04 22:24:38 +0000

    refactor: avoid future test fail

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -55,7 +55,7 @@ debug_backend_override: str = os.environ.get("TORCH_COMPILE_OVERRIDE_BACKENDS", 
 minimum_call_count = 1
 
 # turn on/off DCE pass (deprecated: always true)
-dead_code_elimination = True
+dead_code_elimination = None
 
 # Enable or disable side effect replay after graph execution.
 # When False, mutations to Python objects (lists, dicts, attributes) won't be
