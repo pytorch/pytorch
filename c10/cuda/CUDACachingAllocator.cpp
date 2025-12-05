0d6e024776b4261ec9ebc0bf7cb31fe79a35cf6c
yifeng.jyf <yifeng.jyf@alibaba-inc.com>
2025-12-05 14:02:12 +0800

    [Memory Snapshot] Release alloc_buffer memory after stopping recording history

--- c10/cuda/CUDACachingAllocator.cpp
+++ c10/cuda/CUDACachingAllocator.cpp
@@ -1082,6 +1082,7 @@ class RingBuffer {
     std::lock_guard<std::mutex> lk(alloc_trace_lock);
     alloc_trace_next = 0;
     alloc_trace->clear();
+    alloc_trace->shrink_to_fit();
   }
 
  private:
