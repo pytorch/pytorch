a61c7883dc8fc3652af86a583d9e9444105e87bc
xinan.lin <xinan.lin@intel.com>
2025-12-06 15:16:54 +0000

    [xpu][feature] Switch Intel triton cmpiled kernel format from `spv` to `zebin`.
    ghstack-source-id: 574d09c5560fa1a0d12ad180e76623d49247100c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167972

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -141,7 +141,11 @@ _T = TypeVar("_T")
 VarRanges = dict[sympy.Expr, sympy.Expr]
 InputType = Optional[Union[torch.Tensor, int, torch.SymInt]]
 
-GPU_KERNEL_BIN_EXTS = {"cuda": ".cubin", "xpu": ".spv"}
+XPU_KERNEL_FORMAT = (
+    "spv" if _IS_WINDOWS else os.getenv("TORCHINDUCTOR_XPU_KERNEL_FORMAT", "zebin")
+)
+
+GPU_KERNEL_BIN_EXTS = {"cuda": ".cubin", "xpu": f".{XPU_KERNEL_FORMAT}"}
 
 GPU_ALIGN_BYTES = 16
 ALIGNMENT = 16
