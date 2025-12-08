27819db728fd5665a899de7d0cf8336f75ef5821
Nikita Shulga <nshulga@meta.com>
2025-12-08 14:04:28 -0800

    [BE][3/N] Make MKLDNN Gelu iDeep free
    Claude coded
    
    ghstack-source-id: 795198094d70785448715feff560a300467372d6
    Pull-Request: https://github.com/pytorch/pytorch/pull/169886

--- aten/src/ATen/native/mkldnn/Gelu.cpp
+++ aten/src/ATen/native/mkldnn/Gelu.cpp
@@ -28,6 +28,7 @@ Tensor mkldnn_gelu_backward(const Tensor& grad_output, const Tensor& input, std:
 
 #include <ATen/native/mkldnn/MKLDNNCommon.h>
 #include <ATen/native/mkldnn/Utils.h>
+#include <oneapi/dnnl/dnnl.hpp>
 
 namespace at::native {
 
@@ -38,10 +39,39 @@ Tensor mkldnn_gelu(const Tensor& input, std::string_view approximate) {
   }
   TORCH_CHECK(get_gelutype_enum(approximate) == GeluType::None,
                   "mkldnn_gelu: fast, approximate gelu is not supported");
-  const ideep::tensor& x = itensor_from_tensor(input);
+
+  auto src_desc = get_mkldnn_memory_desc(input);
+  auto& engine = get_mkldnn_cpu_engine();
+
+  auto src_mem = mkldnn_memory_from_tensor(input, src_desc, engine);
+
+  auto op_attr = dnnl::primitive_attr();
+  op_attr.set_scratchpad_mode(dnnl::scratchpad_mode::user);
+
+  auto pd = dnnl::eltwise_forward::primitive_desc(
+      engine,
+      dnnl::prop_kind::forward_training,
+      dnnl::algorithm::eltwise_gelu_erf,
+      src_desc,
+      src_desc,
+      /*alpha*/ 0.0f,
+      /*beta*/ 0.0f,
+      op_attr);
+
+  // Create output memory
+  dnnl::memory dst_mem(pd.dst_desc(), engine);
+  dnnl::memory scratchpad(pd.scratchpad_desc(), engine);
+
+  dnnl::eltwise_forward(pd).execute(
+      get_mkldnn_default_stream(),
+      {{DNNL_ARG_SRC, src_mem},
+       {DNNL_ARG_DST, dst_mem},
+       {DNNL_ARG_SCRATCHPAD, scratchpad}});
+
+  // Convert dnnl::memory to ideep::tensor for output wrapper
   ideep::tensor y;
-  ideep::eltwise_forward::compute(
-      x, y, ideep::algorithm::eltwise_gelu_erf, ideep::prop_kind::forward_training, /*alpha*/ 0.0);
+  y.init(pd.dst_desc(), dst_mem.get_data_handle());
+
   return new_with_itensor_mkldnn(std::move(y), optTypeMetaToScalarType(input.options().dtype_opt()),
                                  input.options().device_opt());
 }
@@ -49,11 +79,70 @@ Tensor mkldnn_gelu(const Tensor& input, std::string_view approximate) {
 Tensor mkldnn_gelu_backward(const Tensor& grad_output, const Tensor& input, std::string_view approximate) {
   TORCH_CHECK(get_gelutype_enum(approximate) == GeluType::None,
                   "mkldnn_gelu_backward: fast, approximate gelu is not supported");
-  const ideep::tensor& x = itensor_from_tensor(input);
-  ideep::tensor grady = itensor_from_tensor(grad_output);
+
+  auto src_desc = get_mkldnn_memory_desc(input);
+  auto& engine = get_mkldnn_cpu_engine();
+
+  auto src_mem = mkldnn_memory_from_tensor(input, src_desc, engine);
+
+  auto forward_hints = dnnl::eltwise_forward::primitive_desc(
+      engine,
+      dnnl::prop_kind::forward,
+      dnnl::algorithm::eltwise_gelu_erf,
+      src_desc,
+      src_desc,
+      /*alpha*/ 0.0f,
+      /*beta*/ 0.0f);
+
+  auto op_attr = dnnl::primitive_attr();
+  op_attr.set_scratchpad_mode(dnnl::scratchpad_mode::user);
+
+  auto pd = dnnl::eltwise_backward::primitive_desc(
+      engine,
+      dnnl::algorithm::eltwise_gelu_erf,
+      forward_hints.src_desc(),
+      forward_hints.dst_desc(),
+      src_desc,
+      /*alpha*/ 0.0f,
+      /*beta*/ 0.0f,
+      forward_hints,
+      op_attr);
+
+  auto diff_dst_desc = pd.diff_dst_desc();
+  auto grady_mem = mkldnn_memory_from_tensor(grad_output, get_mkldnn_memory_desc(grad_output), engine);
+
+  // Reorder if needed
+  dnnl::memory diff_dst_mem = grady_mem;
+  if (diff_dst_desc != grady_mem.get_desc()) {
+    diff_dst_mem = dnnl::memory(diff_dst_desc, engine);
+    dnnl::reorder(grady_mem, diff_dst_mem)
+        .execute(get_mkldnn_default_stream(), grady_mem, diff_dst_mem);
+  }
+
+  // Create output gradient memory
+  dnnl::memory diff_src_mem(pd.diff_src_desc(), engine);
+
+  // Reorder src if needed
+  dnnl::memory expected_src_mem = src_mem;
+  if (pd.src_desc() != src_mem.get_desc()) {
+    expected_src_mem = dnnl::memory(pd.src_desc(), engine);
+    dnnl::reorder(src_mem, expected_src_mem)
+        .execute(get_mkldnn_default_stream(), src_mem, expected_src_mem);
+  }
+
+  dnnl::memory scratchpad(pd.scratchpad_desc(), engine);
+
+  dnnl::eltwise_backward(pd).execute(
+      get_mkldnn_default_stream(),
+      {{DNNL_ARG_DIFF_DST, diff_dst_mem},
+       {DNNL_ARG_SRC, expected_src_mem},
+       {DNNL_ARG_DIFF_SRC, diff_src_mem},
+       {DNNL_ARG_SCRATCHPAD, scratchpad}});
+
+  // Convert dnnl::memory to ideep::tensor for output wrapper
   ideep::tensor gradx;
-  ideep::eltwise_backward::compute(x, grady, gradx,
-      ideep::algorithm::eltwise_gelu_erf, /*alpha*/ 0.0);
+  gradx.init(pd.diff_src_desc(), diff_src_mem.get_data_handle());
+
   return new_with_itensor_mkldnn(std::move(gradx),
                                  optTypeMetaToScalarType(grad_output.options().dtype_opt()),
                                  grad_output.options().device_opt());
