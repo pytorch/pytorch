a208ed2b54edec0509d4706433d5d2f1f68dcbef
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-12-09 02:11:29 +0000

    Revert "[Inductor XPU GEMM] Step 2/N: Move out cutlass files from torch/_inductor/codegen/cuda (#160685)"
    This reverts commit 25c72018d18d1c3869456fa4624baad4b9c65fb5.
    
    Reverted https://github.com/pytorch/pytorch/pull/160685 on behalf of https://github.com/izaitsevfb due to BC-breaking, meta codebase needs to be patched to land this ([comment](https://github.com/pytorch/pytorch/pull/160685#issuecomment-3629881394))

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -2777,7 +2777,7 @@ class AlgorithmSelectorCache(PersistentCache):
         return_choice=False,  # TODO: return_choice is temporary and will be refactored soon
         is_collective=False,
     ):
-        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
+        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
 
         # Run preprocessing functions on choices
         for preprocessing_fn in self.preprocessing_fns:
@@ -3689,9 +3689,7 @@ class AlgorithmSelectorCache(PersistentCache):
                 else:
                     timing = cls.benchmark_choice(choice, autotune_args)
             except CUDACompileError:
-                from torch._inductor.codegen.cutlass.cuda_kernel import (
-                    CUDATemplateCaller,
-                )
+                from torch._inductor.codegen.cuda.cuda_kernel import CUDATemplateCaller
 
                 if not isinstance(choice, CUDATemplateCaller):
                     log.exception(
@@ -3702,9 +3700,7 @@ class AlgorithmSelectorCache(PersistentCache):
                 log.warning("Not yet implemented", exc_info=True)
                 timing = float("inf")
             except RuntimeError as e:
-                from torch._inductor.codegen.cutlass.cuda_kernel import (
-                    CUDATemplateCaller,
-                )
+                from torch._inductor.codegen.cuda.cuda_kernel import CUDATemplateCaller
 
                 msg = str(e)
                 if "invalid argument" in msg:
@@ -3867,7 +3863,7 @@ class AlgorithmSelectorCache(PersistentCache):
             return prescreen_winners
 
         # prescreen cutlass
-        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
+        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
 
         candidates = []
         if (
@@ -3901,7 +3897,7 @@ class AlgorithmSelectorCache(PersistentCache):
         """
         Prune the choices after prescreening.
         """
-        from .codegen.cutlass.cuda_kernel import CUDATemplateCaller
+        from .codegen.cuda.cuda_kernel import CUDATemplateCaller
 
         prescreen_key = f"{name}:{inputs_key}"
 
