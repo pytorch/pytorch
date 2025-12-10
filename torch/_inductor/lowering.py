8ebd4284a05265fa87126d08702dd2a841957009
Isuru Fernando <ifernando@openteams.com>
2025-12-10 14:35:23 +0000

    Update
    [ghstack-poisoned]

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -7271,7 +7271,9 @@ def triton_kernel_wrap_(
 
 
 @register_lowering(torch.ops.higher_order.cond, type_promotion_kind=None)
-def cond(pred, true_fn, false_fn, operands) -> list[Union[ir.Tensorbox, ir.ShapeAsConstantBuffer]]:
+def cond(
+    pred, true_fn, false_fn, operands
+) -> list[Union[ir.TensorBox, ir.ShapeAsConstantBuffer]]:
     if any(isinstance(x, IRNode) and is_triton(x) for x in [pred, *operands]):
         msg = "control flow operator: torch.cond."
         if stack_trace := V.graph.current_node.meta.get("stack_trace", None):
