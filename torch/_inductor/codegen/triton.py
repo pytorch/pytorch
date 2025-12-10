5a2431ce7bb16555ad7e0a98d2a971034b709790
Cheng Cheng <chengcheng@meta.com>
2025-12-10 01:52:53 +0000

    Revert PR#161280 "[ROCm][inductor] heuristic improvements for reduction kernels" (#169792)
    Summary:
    We observed model quality regression when trained on AMD MI300X training regression on top of [PR#161280](https://github.com/pytorch/pytorch/pull/161280). After backing out, the quality regression is gone.
    
    Meta internal issue tracked in S599433.
    
    Reviewed By: haoyuz, yinbinm
    
    Differential Revision: D88596102
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/169792
    Approved by: https://github.com/izaitsevfb
    
    Co-authored-by: Cheng Cheng <chengcheng@meta.com>

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1284,17 +1284,11 @@ class TritonOverrides(OpOverrides):
 
     @staticmethod
     def minimum(a, b):
-        if torch.version.hip:
-            return f"tl.minimum({a}, {b}, tl.PropagateNan.ALL)"
-        else:
-            return f"triton_helpers.minimum({a}, {b})"
+        return f"triton_helpers.minimum({a}, {b})"
 
     @staticmethod
     def maximum(a, b):
-        if torch.version.hip:
-            return f"tl.maximum({a}, {b}, tl.PropagateNan.ALL)"
-        else:
-            return f"triton_helpers.maximum({a}, {b})"
+        return f"triton_helpers.maximum({a}, {b})"
 
     @staticmethod
     def where(a, b, c):
@@ -1667,10 +1661,7 @@ class TritonOverrides(OpOverrides):
     @staticmethod
     @maybe_upcast_float32()
     def rsqrt(x):
-        if torch.version.hip:
-            return f"tl.rsqrt({x})"
-        else:
-            return f"libdevice.rsqrt({x})"
+        return f"libdevice.rsqrt({x})"
 
     @staticmethod
     @maybe_upcast_float32()
@@ -4856,9 +4847,8 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
                     loop_end = (
                         "rsplit_end" if self.cooperative_reduction else f"{prefix}numel"
                     )
-                    num_stages = ", num_stages = 2" if torch.version.hip else ""
                     self.body.writeline(
-                        f"for {prefix}offset in tl.range({loop_start}, {loop_end}, {prefix.upper()}BLOCK{num_stages}):"
+                        f"for {prefix}offset in range({loop_start}, {loop_end}, {prefix.upper()}BLOCK):"
                     )
                 with self.body.indent(offset=level + 1):
                     self.iteration_ranges_codegen_header(tree, self.body)
