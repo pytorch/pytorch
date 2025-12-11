132ce263559351dffab65ed91779cea05fccaad5
Ethan Wee <Ethan.Wee@amd.com>
2025-12-11 08:53:50 -0800

    [release/2.7] remove amdgpu from install_rocm.sh (#2868)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166575
    Approved by: https://github.com/jeffdaily
    
    Fixes #ISSUE_NUMBER
    
    Co-authored-by: Jeff Daily <jeff.daily@amd.com>

--- .ci/docker/common/install_rocm.sh
+++ .ci/docker/common/install_rocm.sh
@@ -21,9 +21,7 @@ install_ubuntu() {
     apt-get install -y libc++1
     apt-get install -y libc++abi1
 
-    # Add amdgpu repository
     UBUNTU_VERSION_NAME=`cat /etc/os-release | grep UBUNTU_CODENAME | awk -F= '{print $2}'`
-    echo "deb [arch=amd64] https://repo.radeon.com/amdgpu/${ROCM_VERSION}/ubuntu ${UBUNTU_VERSION_NAME} main" > /etc/apt/sources.list.d/amdgpu.list
 
     # Add rocm repository
     wget -qO - http://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
