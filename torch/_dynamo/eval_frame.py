00a2cde3eb7faa0d2fe17e74d8e141180c235d61
Pian Pawakapan <pianpwk@meta.com>
2025-12-12 14:56:39 -0800

    Update base for Update on "[DebugMode] add DebugInterpreter for fx.Node metadata in  eager/aot_eager compiled regions"
    One difficulty with DebugMode has been contextualizing logs. For regions compiled with `backend="eager/aot_eager"`, this PR switches the runtime to use `DebugInterpreter(gm).run(...)` instead of the compiled code, so `run_node()` has access to node metadata, and can contextualize logs.
    
    The is enabled with `DebugMode(run_compile_with_interpreter=True)`, which defaults to False in case the interpreter introduces divergence, but ideally it'd be on.
    
    This PR uses nn_module_stack to add `[nn.Module]` logs for compiled regions. Example for forward + backward with aot_eager:
    ```
      [aot_eager region (compile)] enter
        [nn.Mod (compile)] L['self'].foo
          [nn.Mod (compile)] L['self'].foo.l1
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::addmm(t: f32[4], t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
          aten::relu(t: f32[4, 4])  ->  t: f32[4, 4]
          [nn.Mod (compile)] L['self'].foo.l2
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::addmm(t: f32[4], t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
        [nn.Mod (compile)] L['self'].bar
          aten::add.Tensor(t: f32[4, 4], 2.0)  ->  t: f32[4, 4]
          [nn.Mod (compile)] L['self'].bar.l3
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::addmm(t: f32[4], t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
      [aot_eager region (compile)] exit
        aten::sum(t: f32[4, 4])  ->  t: f32[]
        aten::ones_like(t: f32[], pin_memory=False, memory_format=torch.preserve_format)  ->  t: f32[]
        aten::expand(t: f32[], [4, 4])  ->  t: f32[4, 4]
        aten::clone(t: f32[4, 4], memory_format=torch.contiguous_format)  ->  t: f32[4, 4]
      [aot_eager region (compile)] enter
        [nn.Mod (compile)] L['self'].bar
          [nn.Mod (compile)] L['self'].bar.l3
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::mm(t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::mm(t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::sum.dim_IntList(t: f32[4, 4], [0], True)  ->  t: f32[1, 4]
            aten::view(t: f32[1, 4], [4])  ->  t: f32[4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
        [nn.Mod (compile)] L['self'].foo
          [nn.Mod (compile)] L['self'].foo.l2
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::mm(t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::mm(t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::sum.dim_IntList(t: f32[4, 4], [0], True)  ->  t: f32[1, 4]
            aten::view(t: f32[1, 4], [4])  ->  t: f32[4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
          aten::detach(t: f32[4, 4])  ->  t: f32[4, 4]
          aten::detach(t: f32[4, 4])  ->  t: f32[4, 4]
          aten::threshold_backward(t: f32[4, 4], t: f32[4, 4], 0)  ->  t: f32[4, 4]
          [nn.Mod (compile bwd)] L['self'].foo.l1
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::mm(t: f32[4, 4], t: f32[4, 4])  ->  t: f32[4, 4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
            aten::sum.dim_IntList(t: f32[4, 4], [0], True)  ->  t: f32[1, 4]
            aten::view(t: f32[1, 4], [4])  ->  t: f32[4]
            aten::t(t: f32[4, 4])  ->  t: f32[4, 4]
      [aot_eager region (compile)] exit
    ```
    
    The nn.Module tracking should also work nicely with regional compilation; DebugMode now maintains its own nn_module_stack tracking.
    
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx kadeng chauhang amjames Lucaskabela jataylo
    
    [ghstack-poisoned]

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -145,7 +145,7 @@ cached_backends: dict[int, CompilerFn] = {}
 
 unset = Unset.token
 
-_in_compiled_region = False
+_in_optimized_module = False
 
 
 if DISABLE_JUSTKNOBS:
@@ -206,21 +206,24 @@ def get_example_inputs(key: str) -> list[Any]:
 
 
 @contextlib.contextmanager
-def _set_in_compiled_region():
-    global _in_compiled_region
-    _old_in_compiled_region = (
-        _in_compiled_region  # do we need this? can we just set it to False after
+def _set_in_optimized_module():
+    # Set in dynamo's OptimizedModule forward, to have better coverage than is_compiling().
+    # Prevents graph-breaking forward hooks from being registered & traced.
+    # TODO(pianpwk): subsume this flag with better is_compiling() coverage
+    global _in_optimized_module
+    _old_in_optimized_module = (
+        _in_optimized_module  # do we need this? can we just set it to False after
     )
-    _in_compiled_region = True
+    _in_optimized_module = True
     try:
         yield
     finally:
-        _in_compiled_region = _old_in_compiled_region
+        _in_optimized_module = _old_in_optimized_module
 
 
-def _is_in_compiled_region() -> bool:
-    global _in_compiled_region
-    return _in_compiled_region
+def _is_in_optimized_module() -> bool:
+    global _in_optimized_module
+    return _in_optimized_module
 
 
 def _callback_from_stance(callback: DynamoCallback) -> DynamoCallback:
@@ -458,8 +461,7 @@ class OptimizedModule(torch.nn.Module):
                 ", or use the per-module hooks instead",
                 stacklevel=2,
             )
-        with _set_in_compiled_region():
-            # TODO(pianpwk): subsume this flag with better is_compiling() coverage
+        with _set_in_optimized_module():
             return super().__call__(*args, **kwargs)
 
     def _aot_compile(self, inputs: list[torch._dynamo.aot_compile.ModelInput]) -> None:
