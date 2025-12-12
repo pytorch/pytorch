d14983a817c9e5840c3d34a0c8ded723c5cd5200
xinan.lin <xinan.lin@intel.com>
2025-12-12 02:10:35 -0800

    Update
    [ghstack-poisoned]

--- test/dynamo/test_fx_graph_runnable.py
+++ test/dynamo/test_fx_graph_runnable.py
@@ -135,8 +135,15 @@ class FxGraphRunnableTest(TestCase):
         with WritableTempFile("w", suffix=".py") as tmp:
             tmp.write(payload)
             tmp.flush()
+            timeout = 30
+            if GPU_TYPE == "xpu":
+                # Low CPU frequency of XPU CI machine.
+                timeout = 60
             res = subprocess.run(
-                [sys.executable, tmp.name], capture_output=True, text=True, timeout=30
+                [sys.executable, tmp.name],
+                capture_output=True,
+                text=True,
+                timeout=timeout,
             )
 
             self.assertEqual(
