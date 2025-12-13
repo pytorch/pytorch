bfc3bcc9dda8a00a7db3d9c8e96ed608804491e8
Hector Yuen <hyz@meta.com>
2025-12-13 11:03:03 -0800

    suppress error message of unused variable in debug mode (#170385)
    Summary:
    
    we only look at the exception value when we run in debug mode, ignored in optimized
    add a hint to the compiler that this is the case
    
    
    pytorchbot label "topic: not user facing
    
    Test Plan: run the target buck build --flagfile fbcode//mode/opt fbcode//fblearner/predictor/py/applications/concord:ip_python_predictor_concord_publisher
    
    Reviewed By: vinayakawanti-fb
    
    Differential Revision: D89121639

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -85,7 +85,7 @@ bool check_prefer_cudnn_attention() {
     auto dprops = at::cuda::getCurrentDeviceProperties();
     auto major = dprops->major;
     return (major == 9 || major == 10) && !dprops->minor;
-  } catch (c10::Error const& e) {
+  } catch ([[maybe_unused]] c10::Error const& e) {
 #ifdef DEBUG
     TORCH_WARN("check_prefer_cudnn_attention() caught exception ", e.what());
 #endif
