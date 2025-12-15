ef6b76e10361515ed8641c4647fdbc93ab86ce4e
pytorchbot <pytorchbot@pytorch.com>
2025-12-15 07:38:52 +0000

    2025-12-15 nightly release (a773df9943653229f843d2b63a25db06fa4445b0)

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -2287,6 +2287,7 @@ class OutputGraph(OutputGraphCommon):
                         unused_root_guard_manager,
                         specialization.check_fn,
                         [check_fn_source],
+                        None,  # user_stack
                     )
 
                     log.debug(
