82558b2b0d9710744c6b1a5ce2b27ccd63a076df
Sherlock Huang <baihan.huang@gmail.com>
2025-12-15 18:10:03 -0800

    [DTensor] Fix split_strategy to handle symint split_size
    [ghstack-poisoned]

--- torch/distributed/tensor/_ops/_tensor_ops.py
+++ torch/distributed/tensor/_ops/_tensor_ops.py
@@ -1183,7 +1183,7 @@ def split_strategy(op_schema: OpSchema) -> OpStrategy:
 
     output_size_list = (
         size_split(input_strategy.shape[dim], split_size_or_sections)
-        if isinstance(split_size_or_sections, int)
+        if isinstance(split_size_or_sections, IntLike)
         else split_size_or_sections
     )
     if not isinstance(output_size_list, Sized):
