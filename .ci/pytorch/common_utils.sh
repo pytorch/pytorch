2c4a98995e2f682c013ffc0e440c2bc98851580f
Richard Zou <zou3519@gmail.com>
2025-12-16 07:15:29 -0800

    [RELEASE 2.10] Release only changes (#170112)
    * [RELEASE 2.10] Release only changes
    
    * fix
    
    * Apply suggestion from @atalman
    
    [ghstack-poisoned]

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -294,7 +294,7 @@ EOF
 
 function clone_pytorch_xla() {
   if [[ ! -d ./xla ]]; then
-    git clone --recursive --quiet https://github.com/pytorch/xla.git
+    git clone --recursive -b r2.10 https://github.com/pytorch/xla.git
     pushd xla
     # pin the xla hash so that we don't get broken by changes to xla
     git checkout "$(cat ../.github/ci_commit_pins/xla.txt)"
