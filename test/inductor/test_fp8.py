51c45b4051d90e79d30eeee753455bc85d690cde
Anshul Sinha <anshulsi@meta.com>
2025-12-16 13:14:19 -0800

    Update base for Update on "[dtensor][partial] fixes unnecessary redistributions when subtracting two partial tensors"
    **Summary:** Currently, whenever we subtract two partial dtensors, we redistribute since linearity is -1 for aten.sub.tensor. However, this is an unnecessary redistribution that can be avoided in similar ways to its add counterpart. I moved the op to linear_ops and ensured subtracting a scalar from a partial dtensor continues to redistribute.
    
    **Test Cases:**
    1. pytest test/distributed/tensor/test_pointwise_ops.py -k test_add_sub_scalar_norm_partial
    2. pytest test/distributed/tensor/test_pointwise_ops.py -k test_add_sub_scalar_partial
    
    
    
    
    cc wanchaol tianyu-l wz337 XilunWu d4l3k pragupta SherlockNoMad ppwwyyxx
    
    [ghstack-poisoned]

--- test/inductor/test_fp8.py
+++ test/inductor/test_fp8.py
@@ -824,7 +824,7 @@ class TestFP8Lowering(TestCase):
     @xfailIf(
         torch.cuda.is_available() and torch.cuda.get_device_capability() != (9, 0)
     )  # cuBLAS 128-element blockwise scaling is only supported for CC 9.0
-    @parametrize("shape", ((16, 256, 256), (1024, 512, 1024), (32768, 4096, 4096)))
+    @parametrize("shape", ((16, 256, 256), (1024, 512, 1024)))
     @parametrize("use_fast_accum", (False, True))
     @parametrize(
         "scaling_block_sizes", ((1, 128, 128, 128), (1, 128, 1, 128))
