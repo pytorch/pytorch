bd5dc1a1435cc8c1397a2824d61f9a7436631db3
Markus Hoehnerbach <mhoehnerbach@fb.com>
2025-12-15 16:28:00 -0800

    fix guard ordering on nn modules
    fixes #170429
    
    [ghstack-poisoned]

--- torch/_dynamo/variables/nn_module.py
+++ torch/_dynamo/variables/nn_module.py
@@ -1254,7 +1254,8 @@ class UnspecializedNNModuleVariable(UserDefinedObjectVariable):
                 return key, value
 
             result = dict(
-                build_key_value(i, k, v) for i, (k, v) in enumerate(hooks_dict.items())
+                build_key_value(i, k, v)
+                for i, (k, v) in enumerate(dict.items(hooks_dict))
             )
 
             return variables.NNModuleHooksDictVariable(
