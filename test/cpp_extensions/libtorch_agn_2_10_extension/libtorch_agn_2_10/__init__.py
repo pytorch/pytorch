04ac2d5f04d99086263aa793d2bfb6c03c1fa612
Pearu Peterson <pearu.peterson@gmail.com>
2025-12-17 21:41:38 +0200

    Update
    [ghstack-poisoned]

--- test/cpp_extensions/libtorch_agn_2_10_extension/libtorch_agn_2_10/__init__.py
+++ test/cpp_extensions/libtorch_agn_2_10_extension/libtorch_agn_2_10/__init__.py
@@ -13,7 +13,7 @@ assert len(so_files) == 1, f"Expected one _C*.{{so,pyd}} file, found {len(so_fil
 # use ctypes.CDLL instead of load_library to be able to test the unload logic
 # below code is reduced from the load_library code
 with torch._ops.dl_open_guard():
-    loaded_lib = ctypes.CDLL(so_files[0])
+    loaded_lib = ctypes.CDLL(str(so_files[0]))
 
 from . import ops
 
