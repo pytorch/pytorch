3ffb3c18b88d2cc1cc3788d2b5e7d159a46b352a
Will Constable <whc@meta.com>
2025-12-16 16:18:36 -0800

    Update on "Revert "[DTensor] Refactor strategy/rule registration into dedicated module (#168221)""
    This reverts commit cb3754f39fce0ce5162fc25f3c7cd0ab451ecd79.
    
    Reverting this change as it affects the import path of a publicly
    used API.
    
    [ghstack-poisoned]

--- test/distributed/tensor/test_op_strategy.py
+++ test/distributed/tensor/test_op_strategy.py
@@ -34,7 +34,7 @@ from torch.distributed.tensor._ops.utils import (
     register_op_strategy,
     replicate_op_strategy,
 )
-from torch.distributed.tensor.debug import CommDebugMode
+from torch.distributed.tensor.debug import _clear_sharding_prop_cache, CommDebugMode
 from torch.testing._internal.common_utils import run_tests, TestCase
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     create_local_tensor_test_class,
