c65f67be1c97aa2c2000e3240cc74785f5765b4d
Will Constable <whc@meta.com>
2025-12-17 09:47:07 -0800

    Revert "[DTensor] Refactor strategy/rule registration into dedicated module (#168221)" (#170615)
    This reverts commit cb3754f39fce0ce5162fc25f3c7cd0ab451ecd79.
    
    Reverting this change as it affects the import path of a publicly
    used API.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/170615
    Approved by: https://github.com/wdvr, https://github.com/malfet

--- torch/distributed/tensor/_ops/_tensor_ops.py
+++ torch/distributed/tensor/_ops/_tensor_ops.py
@@ -17,11 +17,6 @@ from torch.distributed.tensor._op_schema import (
     TupleStrategy,
 )
 from torch.distributed.tensor._ops._common_rules import pointwise_rule
-from torch.distributed.tensor._ops._embedding_ops import _MaskPartial
-from torch.distributed.tensor._ops.registration import (
-    register_op_strategy,
-    register_prop_rule,
-)
 from torch.distributed.tensor._ops.utils import (
     expand_to_full_mesh_op_strategy,
     generate_redistribute_costs,
@@ -29,10 +24,13 @@ from torch.distributed.tensor._ops.utils import (
     is_tensor_evenly_shardable,
     is_tensor_partial,
     normalize_dim,
+    register_op_strategy,
+    register_prop_rule,
     shift_shard_dims_after_insert,
     shift_shard_dims_after_remove,
 )
 from torch.distributed.tensor.placement_types import (
+    _MaskPartial,
     Partial,
     Placement,
     Replicate,
