b1661d02f873595defb96cda129dc47c35a3c116
karthickai <karthickps@meta.com>
2025-12-16 23:03:26 -0800

    [Inductor] test SequentialDispatch as default strategy for combo kernels
    ghstack-source-id: c4e1633175e82a8c083a2a9781122ff1bc822775
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/170347

--- torch/_inductor/codegen/triton_combo_kernel.py
+++ torch/_inductor/codegen/triton_combo_kernel.py
@@ -581,22 +581,23 @@ class ComboKernel(Kernel):
     def select_dispatch_strategy(self) -> None:
         if self.dispatch_class is not None:
             return
-        # mixed_sizes is used for optimize_mask, so it only allows sequential dispatch
-        # Not mixed sizes on y dim technically is ok to use round robin as wells.
-        if not self.mixed_sizes or any(isinstance(e, str) for e in self.x_numels_list):
-            # str in x_numels_list means a dynamic shape
-            self.dispatch_class = ComboKernel.SequentialDispatch
-            return
-        # A negative x_blocks_list element means the kernel is not tunable,
-        # i.e., no_x_dim = True
-        x_numels_list = [abs(cast(int, e)) for e in self.x_numels_list]
-        total = max(x_numels_list) * len(x_numels_list)
-        needed = sum(x_numels_list)
-        if needed / total > BLOCK_UTILIZATION:
-            # Introduced overhead (masked blocks) is less than 20%
-            self.dispatch_class = ComboKernel.RoundRobinDispatch
-        else:
-            self.dispatch_class = ComboKernel.SequentialDispatch
+        self.dispatch_class = ComboKernel.SequentialDispatch
+        # # mixed_sizes is used for optimize_mask, so it only allows sequential dispatch
+        # # Not mixed sizes on y dim technically is ok to use round robin as wells.
+        # if not self.mixed_sizes or any(isinstance(e, str) for e in self.x_numels_list):
+        #     # str in x_numels_list means a dynamic shape
+        #     self.dispatch_class = ComboKernel.SequentialDispatch
+        #     return
+        # # A negative x_blocks_list element means the kernel is not tunable,
+        # # i.e., no_x_dim = True
+        # x_numels_list = [abs(cast(int, e)) for e in self.x_numels_list]
+        # total = max(x_numels_list) * len(x_numels_list)
+        # needed = sum(x_numels_list)
+        # if needed / total > BLOCK_UTILIZATION:
+        #     # Introduced overhead (masked blocks) is less than 20%
+        #     self.dispatch_class = ComboKernel.RoundRobinDispatch
+        # else:
+        #     self.dispatch_class = ComboKernel.SequentialDispatch
 
     def jit_line(
         self,
