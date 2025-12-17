0d3560af0ac715029751fd2e43d3a5aa17dc4c8c
Dayananda V <dayananda.vasantha.kumar@intel.com>
2025-12-17 11:31:54 +0530

    Update common_utils.py
    _get_device_name return default "cpu" as device type when accelerator fail to get the current_accelerator device.

--- torch/testing/_internal/distributed/rpc/common_utils.py
+++ torch/testing/_internal/distributed/rpc/common_utils.py
@@ -2,6 +2,8 @@ import torch
 
 
 def _get_device_name(idx):
+    if None == torch.accelerator.current_accelerator():
+        return "cpu"
     if idx < 0:
         return f"{torch.accelerator.current_accelerator().type}"
     return f"{torch.accelerator.current_accelerator().type}:{idx}"
