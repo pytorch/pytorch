eade05683ce3bacaed1e11c71ac600c641c63e00
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-12-18 18:55:15 +0000

    Revert "[DTensor] Add single-dim registration infra (#170359)"
    This reverts commit 32d07821dcc3a1cd3c44688cb5d890a60703b484.
    
    Reverted https://github.com/pytorch/pytorch/pull/170359 on behalf of https://github.com/jeanschmidt due to Required to revert https://github.com/pytorch/pytorch/pull/167677 that is required to revert https://github.com/pytorch/pytorch/pull/170615 that is required to revert https://github.com/pytorch/pytorch/pull/170030 ([comment](https://github.com/pytorch/pytorch/pull/170359#issuecomment-3671746823))

--- torch/distributed/tensor/_ops/single_dim_strategy.py
+++ torch/distributed/tensor/_ops/single_dim_strategy.py
@@ -1,9 +1,8 @@
 #  Copyright (c) Meta Platforms, Inc. and affiliates
 import logging
 from collections.abc import Callable
-from typing import Any, cast, Optional, TypeAlias, TypeVar, Union
+from typing import Any, cast, TypeAlias, TypeVar
 
-import torch
 from torch._ops import OpOverload
 from torch.distributed.tensor._dtensor_spec import DTensorSpec
 from torch.distributed.tensor._op_schema import (
@@ -12,10 +11,10 @@ from torch.distributed.tensor._op_schema import (
     OpSchema,
     OpStrategy,
     PlacementList,
-    RuntimeSchemaInfo,
     StrategyType,
     TupleStrategy,
 )
+from torch.distributed.tensor._ops.utils import expand_to_full_mesh_op_strategy
 from torch.distributed.tensor.device_mesh import DeviceMesh
 from torch.distributed.tensor.placement_types import (
     _StridedShard,
@@ -172,9 +171,6 @@ def _expand_single_dim_strategy_to_mesh(
     The expanded_strategy function accesses both the args_schema/kwargs_schema, which contains TensorMeta in place of
     tensor arguments, but also the op_schema which contains OpStrategy in place of Tensor args.
     """
-    # Note: circular import, failed to untangle with #168221, reverted
-    from torch.distributed.tensor._ops.utils import expand_to_full_mesh_op_strategy
-
     unique_input_placements = _get_unique_placements(op_schema)
     num_inputs = _get_num_tensor_inputs(op_schema)
 
@@ -218,47 +214,3 @@ def _expand_single_dim_strategy_to_mesh(
         )
 
     return expanded_strategy
-
-
-def register_single_dim_strategy(
-    op: Union[torch._ops.OpOverload, list[torch._ops.OpOverload]],
-    schema_info: Optional[RuntimeSchemaInfo] = None,
-) -> Callable[[_SingleDimStrategyFunc], _SingleDimStrategyFunc]:
-    """
-    Registers a single_dim_strategy function for the given op.
-
-    A single_dim_strategy enumerates all the non-trivial sharding specifications for the operator over a single mesh
-    dim. Since it generates the full set of valid shardings regardless of the given input placements, tensor inputs
-    are represented via TensorMetas instead of OpStrategies like in op_strategy functions. TensorMeta inputs, along
-    with all other op inputs (int, float, bool, etc) inform which sharding placements are valid. Single-dim strategies
-    are fed into infra that expands them over multiple mesh dims and fills sharding placeholders with concrete sharding
-    types.
-
-    Single-dim-strategies should not list the trivial "all Replicate" rule, which is assumed for all ops.
-
-    Sharding placeholders should be used to represent generic sharding rules in single_dim_strategies.  For example,
-    most operators that support sharded inputs and outputs do not care how the shards are laid out globally, as long as
-    they are consistent.  It is just as valid to run a matmul on (Shard(0), Replicate -> Shard(0)) as on
-    (StridedShard(0), Replicate -> StridedShard(0)).  For this reason, we use a 'ShardingPlaceholder' to represent all
-    generic sharding types.  Placeholders will be filled by concrete sharding types seen in runtime inputs, not all
-    types known to DTensor.
-
-    """
-    # Note: circular import, failed to untangle with #168221, reverted
-    from torch.distributed.tensor._api import DTensor
-    from torch.distributed.tensor._ops.utils import _get_registration_wrapper
-
-    # For every ATen op that accepts any args in this list,
-    # the arg itself can impact the strides (and potentially the sharding strategy)
-    # of the output tensor.
-    # thus, we will detect ATen schemas with any of these args and ensure
-    # that they get specialized here.
-    arg_names_that_require_specializing_cache_strategy = [
-        "memory_format",
-    ]
-    return _get_registration_wrapper(
-        DTensor._op_dispatcher.sharding_propagator.register_single_dim_op_strategy,
-        op,
-        schema_info,
-        arg_names_that_require_specializing_cache_strategy,
-    )
