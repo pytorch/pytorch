e0907262c459c37cea7291e07a91362e545ada56
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2025-12-18 19:18:31 +0000

    Revert "[dtensor][partial] fixes unnecessary redistributions when subtracting two partial tensors (#170040)"
    This reverts commit 27aaab88b99e8c4836912bb282bf2dff1fc990a4.
    
    Reverted https://github.com/pytorch/pytorch/pull/170040 on behalf of https://github.com/jeanschmidt due to regression in sixlib ([comment](https://github.com/pytorch/pytorch/pull/170030#issuecomment-3671848237))

--- test/distributed/tensor/test_pointwise_ops.py
+++ test/distributed/tensor/test_pointwise_ops.py
@@ -511,7 +511,7 @@ class DistElementwiseOpsTest(DTensorOpTestBase):
         self.assertTrue(res._spec.placements[0].is_replicate())
 
     @with_comms
-    def test_add_sub_scalar_partial(self):
+    def test_add_scalar_partial(self):
         mesh = self.build_device_mesh()
 
         rank = self.rank
@@ -527,21 +527,6 @@ class DistElementwiseOpsTest(DTensorOpTestBase):
         self.assertEqual(res, 7)
         self.assertTrue(res._spec.placements[0].is_replicate())
 
-        # regular partial - scalar -> replicate
-        local_tensor = map_local_for_rank(rank, lambda rank: torch.tensor([rank]))
-
-        dt = DTensor.from_local(
-            local_tensor, device_mesh=mesh, placements=[Partial("sum")]
-        )
-
-        res = dt - 1
-        self.assertEqual(res, 5)
-        self.assertTrue(res._spec.placements[0].is_replicate())
-
-        res = 7 - dt
-        self.assertEqual(res, 1)
-        self.assertTrue(res._spec.placements[0].is_replicate())
-
         # regular partial + regular partial -> partial
         res = dt + dt
         self.assertEqual(res.to_local(), rank + rank)
@@ -549,15 +534,8 @@ class DistElementwiseOpsTest(DTensorOpTestBase):
         res = res.redistribute(dt.device_mesh, placements=[Replicate()])
         self.assertEqual(res, 12)
 
-        # regular partial - regular partial -> partial
-        res = dt - dt
-        self.assertEqual(res.to_local(), rank - rank)
-        self.assertTrue(res._spec.placements[0].is_partial())
-        res = res.redistribute(dt.device_mesh, placements=[Replicate()])
-        self.assertEqual(res, 0)
-
     @with_comms
-    def test_add_sub_scalar_norm_partial(self):
+    def test_add_scalar_norm_partial(self):
         mesh = self.build_device_mesh()
 
         # norm partial + scalar
@@ -569,15 +547,6 @@ class DistElementwiseOpsTest(DTensorOpTestBase):
         norm = norm + 1
 
         self.assertEqual(norm, 11)
-        self.assertTrue(norm._spec.placements[0].is_replicate())
-
-        dt = distribute_tensor(local_tensor, mesh, [Shard(0)])
-
-        norm = dt.norm()
-        self.assertTrue(isinstance(norm._spec.placements[0], _NormPartial))
-        norm = norm - 1
-
-        self.assertEqual(norm, 9)
         self.assertTrue(norm._spec.placements[0].is_replicate())
 
     @with_comms
