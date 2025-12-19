2b3b640543675eb170496a3c1a9565315f63914a
Rahul Jain <rahjain@amd.com>
2025-12-19 15:45:19 +0000

    remove skipIfRocm decorator

--- test/inductor/test_triton_kernels.py
+++ test/inductor/test_triton_kernels.py
@@ -2557,7 +2557,6 @@ def forward(self, arg0_1, arg1_1):
 
     @requires_gpu
     @skipIfXpu(msg="`tl.inline_asm_elementwise` is not yet supported on Intel GPUs")
-    @skipIfRocm
     @inductor_config.patch({"triton.autotune_at_compile_time": True})
     @parametrize("quotes", ["single", "double"])
     def test_kernel_inline_asm(self, quotes):
