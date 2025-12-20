c49dd84df5485cf56ee2de0e0325a69b74a21006
jsandham <jsandham@amd.com>
2025-12-20 01:57:13 +0000

    Fix compile error caused by #endif left in

--- aten/src/ATen/native/sparse/cuda/SparseBlasImpl.cpp
+++ aten/src/ATen/native/sparse/cuda/SparseBlasImpl.cpp
@@ -1189,9 +1189,7 @@ void triangular_solve_out_sparse_csr(
       block_sparse_triangular_solve_mat(A, B, X, upper, transpose, unitriangular); return;
     }
   }
-//#ifdef USE_ROCM
-//  TORCH_CHECK(false, "ROCm is not supported");
-//#else
+
   c10::MaybeOwned<Tensor> X_ = prepare_dense_matrix_for_cusparse(X);
   // It should be possible to use mixed memory format
   // but there is a bug in CUDA 11.3.1 version:
@@ -1314,7 +1312,6 @@ void triangular_solve_out_sparse_csr(
   if (!X.is_same(*X_)) {
     X.copy_(*X_);
   }
-#endif
 }
 
 void sampled_addmm_out_sparse_csr(
