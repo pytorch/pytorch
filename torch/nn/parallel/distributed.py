ad263cff06dc374a8190bf9bfff6403d85a62d47
florian <f@f-kleinicke.de>
2025-12-21 20:23:21 +0100

    fix ProcessGroup return value for join_process_group calls

--- torch/nn/parallel/distributed.py
+++ torch/nn/parallel/distributed.py
@@ -658,7 +658,7 @@ class DistributedDataParallel(Module, Joinable):
         dim=0,
         broadcast_buffers=True,
         init_sync=True,
-        process_group=None,
+        process_group: dist.ProcessGroup | None = None,
         bucket_cap_mb=None,
         find_unused_parameters=False,
         check_reduction=False,
@@ -1899,11 +1899,11 @@ class DistributedDataParallel(Module, Joinable):
         )
 
     @property
-    def join_device(self):
+    def join_device(self) -> torch.device:
         return self.device
 
     @property
-    def join_process_group(self):
+    def join_process_group(self) -> dist.ProcessGroup:
         return self.process_group
 
     def _register_buffer_comm_hook(
