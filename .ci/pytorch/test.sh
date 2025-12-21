00f2bb85a48106e249161eca1b4bfff820538294
jainapurva <apurvajain.kota@gmail.com>
2025-12-21 23:37:15 +0000

    [TESTING] torch2.10 benchmarks

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -1804,8 +1804,8 @@ test_operator_microbenchmark() {
   TEST_DIR=$(pwd)
 
   test_inductor_set_cpu_affinity
-  pip_uninstall torch torchvision torchaudio
-  pip_install torch torchvision torchaudio ninja --force-reinstall
+  pip install torch==2.10.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu128 --force-reinstall
+  pip install ninja --force-reinstall
   cd benchmarks/operator_benchmark/pt_extension
   python -m pip install . -v --no-build-isolation
 
