f5675a5b7f4da92de0cbbf160b57b4fac14b812c
Cui, Yifeng <yifeng.cui@intel.com>
2025-12-23 19:02:37 -0800

    Support half precision FFT on XPU backend

--- aten/src/ATen/native/SpectralOps.cpp
+++ aten/src/ATen/native/SpectralOps.cpp
@@ -82,7 +82,7 @@ ScalarType promote_type_fft(ScalarType type, bool require_complex, Device device
   const bool maybe_support_half = (
     // Only CUDA supports half precision, but since meta tensors don't have a
     // device we err on the side of accepting it
-    device.is_cuda() || device.is_meta()
+    device.is_cuda() || device.is_meta() || device.is_xpu()
   );
   if (maybe_support_half) {
     TORCH_CHECK(type == kHalf || type == kFloat || type == kDouble, "Unsupported dtype ", type);
