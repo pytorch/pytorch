f618440252f63e30dc57bfee8a5ac1c7c8007796
Neo Zhang Jianyu <jianyu.zhang@intel.com>
2025-12-24 17:15:10 +0800

    rm duplicated ch check as review comment

--- aten/src/ATen/native/mkldnn/xpu/Conv.cpp
+++ aten/src/ATen/native/mkldnn/xpu/Conv.cpp
@@ -615,10 +615,10 @@ std::tuple<Tensor, Tensor, Tensor> convolution_backward_overrideable(
       ? get_cl_tag_by_ndim(input_.ndimension())
       : at::MemoryFormat::Contiguous;
 
+  grad_output_ = grad_output_.contiguous(mfmt);
+
   if (!is_stride_decrease_order(grad_output_))
     grad_output_ = grad_output_.to(mfmt);
-  if (!grad_output_.is_contiguous())
-    grad_output_ = grad_output_.contiguous(mfmt);
 
   weight_ = weight_.contiguous(mfmt);
   input_ = input_.contiguous(mfmt);
