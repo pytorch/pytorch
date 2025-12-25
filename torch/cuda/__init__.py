71fb9e242798c747f3fcedd6de3cdd3e225e1b4f
Qi Jia <kuafou@gmail.com>
2025-12-26 05:37:22 +0800

    [Doc] Fix missing space in set_stream docstring

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -791,7 +791,7 @@ def _set_stream_by_id(stream_id, device_index, device_type):
 
 
 def set_stream(stream: Stream):
-    r"""Set the current stream.This is a wrapper API to set the stream.
+    r"""Set the current stream. This is a wrapper API to set the stream.
         Usage of this function is discouraged in favor of the ``stream``
         context manager.
 
