686e9558b1a0f289945849f2b3a534899d286886
Guilherme Leobas <gleobas@quansight.com>
2025-12-29 15:11:05 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -113,7 +113,6 @@ from .misc import (
     MethodWrapperVariable,
     NewGlobalVariable,
     NumpyVariable,
-    ObjectVariable,
     PythonModuleVariable,
     RandomClassVariable,
     RandomVariable,
