d12682fa8be6cf70480525bfe1c62511c1c722bb
AmdSampsa <sampsa.riikonen@amd.com>
2025-12-30 12:23:56 +0000

    linter

--- test/inductor/test_torchinductor_strided_blocks.py
+++ test/inductor/test_torchinductor_strided_blocks.py
@@ -1394,10 +1394,7 @@ test_torchinductor.copy_tests(CommonTemplate, TritonBlockPointerTestGPU, GPU_TYP
 
 
 @unittest.skipIf(
-    not (
-        HAS_CUDA_AND_TRITON
-        and torch.cuda.get_device_capability()[0] >= 9
-    )
+    not (HAS_CUDA_AND_TRITON and torch.cuda.get_device_capability()[0] >= 9)
     or torch.version.hip,
     "Requires Triton CUDA backend and CUDA compute capability >= 9.0. Not supported on ROCm",
     # ROCm triton doesn't support/generate "tl.make_tensor_descriptor" which is exactly what this unit test is about
