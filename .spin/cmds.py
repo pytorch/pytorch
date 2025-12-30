7be719a8768c8e7627f8135c519575b4b15fc638
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-12-30 15:54:00 +0100

    Update
    [ghstack-poisoned]

--- .spin/cmds.py
+++ .spin/cmds.py
@@ -328,25 +328,34 @@ def _run_lintrunner(
         linters &= take
     if skip is not None:
         linters -= skip
-    full_cmd = (
-        cmd
-        + tee_cmd
-        + [
-            "--take",
-            ",".join(linters),
-        ]
-        + (["--apply-patches"] if apply_patches else [])
-        + (["--all-files"] if all_files else [])
-        + (list(lintrunner_args) if lintrunner_args else [])
-    )
-    p = spin.util.run(full_cmd, sys_exit=False)
-    lint_found = bool(p.returncode)
-    if tee_file:
-        tee_path = Path(tee_file)
-        json_output = tee_path.read_text()
-        tee_path.unlink()
+    if not linters:
+        click.echo("No linters to run after applying --take/--skip filters.")
+        click.echo("Skipping lintrunner execution.")
+        lint_found = False
+        if return_json_output:
+            json_output = ""
+        else:
+            json_output = None
     else:
-        json_output = None
+        full_cmd = (
+            cmd
+            + tee_cmd
+            + [
+                "--take",
+                ",".join(linters),
+            ]
+            + (["--apply-patches"] if apply_patches else [])
+            + (["--all-files"] if all_files else [])
+            + (list(lintrunner_args) if lintrunner_args else [])
+        )
+        p = spin.util.run(full_cmd, sys_exit=False)
+        lint_found = bool(p.returncode)
+        if tee_file:
+            tee_path = Path(tee_file)
+            json_output = tee_path.read_text()
+            tee_path.unlink()
+        else:
+            json_output = None
     return lint_found, json_output
 
 
