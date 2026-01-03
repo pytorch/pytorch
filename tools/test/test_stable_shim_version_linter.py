83a59dee018644e36d75bedd63e75d1447960cd8
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2026-01-02 18:24:52 -0800

    Update base for Update on "Add version linter for files in torch/csrc/stable that use torch/csrc/stable/c/shim.h"
    This PR ensures that files in `torch/csrc/stable` that include from `torch/csrc/stable/shim.h` properly gate calls to the stable shim APIs with the appropriate `TORCH_FEATURE_VERSION` macros
    
    ### Examples
    
    1. Commenting out the version guard in
    
    https://github.com/pytorch/pytorch/blob/a64c7d740428010d700b4bcd395af8a7b2d5c21f/torch/csrc/stable/ops.h#L265-L271
    
    ```
    Error (STABLE_SHIM_USAGE) unversioned-shim-call
        Call to versioned shim function 'torch_call_dispatcher' is not wrapped in
        a TORCH_FEATURE_VERSION block. This function requires:
        #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
          // ... your code calling torch_call_dispatcher ...
        #endif // TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
    
            263  |      torch::stable::detail::from(self),
            264  |      torch::stable::detail::from(std::nullopt)};
            265  |  // #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
        >>> 266  |  TORCH_ERROR_CODE_CHECK(torch_call_dispatcher(
            267  |      "aten::clone", "", stack.data(), TORCH_ABI_VERSION));
            268  |  // #else
            269  |  //   TORCH_ERROR_CODE_CHECK(
     ```
     
    2. Changing the first line to say  #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_11_0
     
     Doesn't error because this is still valid!
     
     3. Change that line to #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_9_0
     
     ```
     Error (STABLE_SHIM_USAGE) insufficient-version-for-shim-call
        Call to 'torch_call_dispatcher' is wrapped in TORCH_VERSION_2_9_0, but
        this function requires at least TORCH_VERSION_2_10_0. The version guard
        must be at least the required version:
        #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
          // ... your code calling torch_call_dispatcher ...
        #endif // TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
    
            263  |      torch::stable::detail::from(self),
            264  |      torch::stable::detail::from(std::nullopt)};
            265  |#if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_9_0
        >>> 266  |  TORCH_ERROR_CODE_CHECK(torch_call_dispatcher(
            267  |      "aten::clone", "", stack.data(), TORCH_ABI_VERSION));
            268  |#else
            269  |  TORCH_ERROR_CODE_CHECK(
            
    ```
    
    4. Nested ifdefs 
    
    ```
    #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
    
    // We expect this to be the stable version of the clone op. We will
    // add optional memory_format kwarg support in the future.
    inline torch::stable::Tensor clone(const torch::stable::Tensor& self) {
      const auto num_args = 2;
      std::array<StableIValue, num_args> stack{
          torch::stable::detail::from(self),
          torch::stable::detail::from(std::nullopt)};
    #if TORCH_FEATURE_VERSION >= TORCH_VERSION_2_10_0
    #endif
    #ifdef FOO
      TORCH_ERROR_CODE_CHECK(torch_call_dispatcher(
          "aten::clone", "", stack.data(), TORCH_ABI_VERSION));
    #else
      TORCH_ERROR_CODE_CHECK(
          aoti_torch_call_dispatcher("aten::clone", "", stack.data()));
    #endif
      return torch::stable::detail::to<torch::stable::Tensor>(stack[0]);
    }
    
    #endif
    ```
    
    No error!
     
     
    
    
    
    
    [ghstack-poisoned]

--- tools/test/test_stable_shim_version_linter.py
+++ tools/test/test_stable_shim_version_linter.py
@@ -11,7 +11,7 @@ sys.path.append(str(REPO_ROOT))
 from tools.linter.adapters.stable_shim_version_linter import (
     check_file,
     get_added_lines,
-    get_version_of_block,
+    get_current_version,
 )
 
 
@@ -99,9 +99,9 @@ index 365c954dbe7..c5fcf7a09cf 100644
                 result, {18, 19, 20, 21, 22, 23, 24, 49, 50, 51, 52, 53, 54}
             )
 
-    def test_get_version_of_block(self):
+    def test_get_current_version(self):
         """Test that we can get the current version from version.txt."""
-        version = get_version_of_block()
+        version = get_current_version()
         self.assertIsInstance(version, tuple)
         self.assertEqual(len(version), 2)
         # We can't check torch.__version__ here so this is the best we can do :(
@@ -151,7 +151,7 @@ AOTI_TORCH_EXPORT int function_without_version();
 
                 # Mock version to be 2.10
                 with patch(
-                    "tools.linter.adapters.stable_shim_version_linter.get_version_of_block",
+                    "tools.linter.adapters.stable_shim_version_linter.get_current_version",
                     return_value=(2, 10),
                 ):
                     lint_messages = check_file(temp_file)
