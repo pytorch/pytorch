154533dfdb0ce76d28e1b7a702d3cea4b9f4caa0
benniofficialdev <coden.mit.benni@gmail.com>
2026-01-05 22:35:50 +0100

    Update launch_kernel_pt.hpp

--- aten/src/ATen/native/transformers/hip/flash_attn/ck/launch_kernel_pt.hpp
+++ aten/src/ATen/native/transformers/hip/flash_attn/ck/launch_kernel_pt.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2026, Advanced Micro Devices, Inc. All rights reserved.
 
 #pragma once
 
