32771bce187ef6e1a86d99555657a84892a0d24f
PaulZhang12 <paulzhan@fb.com>
2026-01-05 08:01:41 -0800

    Update base for Update on "Enable persistent tma matmul for oss"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- torch/fx/node.py
+++ torch/fx/node.py
@@ -359,7 +359,7 @@ class Node(_NodeBase):
             "name": self.name,
             "op": self.op,
             "target": self.target,
-            "type": self.type,
+            "type": self.target,
             "_sort_key": self._sort_key,
             "_args": self._args,
             "_kwargs": self._kwargs,
