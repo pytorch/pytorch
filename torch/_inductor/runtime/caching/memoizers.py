08eb1bf505d1680fb8521e74ce46991f131208d7
Dark Knight <generatedunixname89002005232357@meta.com>
2026-01-07 11:24:30 -0800

    Revert D90269212
    Summary:
    This diff reverts D90269212
    (The context such as a Sandcastle job, Task, SEV, etc. was not provided.)
    
    Depends on D90269212
    
    Test Plan: NA
    
    Differential Revision: D90271215

--- torch/_inductor/runtime/caching/memoizers.py
+++ torch/_inductor/runtime/caching/memoizers.py
@@ -5,3 +5,6 @@ from .interfaces import PersistentMemoizer
 
 # Memoizer for _should_pad in pad_mm
 should_pad_memoizer = PersistentMemoizer(sub_dir=Path("should_pad"))
+
+# Memoizer for tuned_mm in kernel/mm
+tuned_mm_memoizer = PersistentMemoizer(sub_dir=Path("tuned_mm"))
