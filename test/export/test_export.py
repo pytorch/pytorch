0c2b2c6977f84b95b3523c6ff20385504387674b
Yu, Guangye <guangye.yu@intel.com>
2026-01-08 21:54:04 +0000

    Update
    [ghstack-poisoned]

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -8127,6 +8127,7 @@ def forward(self, p_linear_weight, p_linear_bias, b_buffer, x):
 
     @requires_gpu
     @skipIfRocm
+    @skipIfXpu
     @testing.expectedFailureSerDer
     @testing.expectedFailureSerDerNonStrict
     def test_export_lstm_gpu(self):
@@ -8153,6 +8154,7 @@ def forward(self, p_linear_weight, p_linear_bias, b_buffer, x):
 
     @requires_gpu
     @skipIfRocm
+    @skipIfXpu
     @testing.expectedFailureSerDer
     @testing.expectedFailureSerDerNonStrict
     def test_export_gru_gpu(self):
@@ -8179,6 +8181,7 @@ def forward(self, p_linear_weight, p_linear_bias, b_buffer, x):
 
     @requires_gpu
     @skipIfRocm
+    @skipIfXpu
     @testing.expectedFailureSerDer
     @testing.expectedFailureSerDerNonStrict
     def test_export_rnn_flatten_parameters_gpu(self):
