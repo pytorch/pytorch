6ca60926c121b97cc65eafffb9a0deac545e4cba
can-gaa-hou <jiahaochen535@gmail.com>
2026-01-08 19:54:47 +0800

    Refactor triton device compatibility checks to simplify logic and support privateuse1 backend

--- torch/utils/_triton.py
+++ torch/utils/_triton.py
@@ -158,37 +158,23 @@ def has_triton() -> bool:
     from torch._dynamo.device_interface import get_interface_for_device
     from torch.accelerator import current_accelerator
 
-    def cuda_extra_check(device_interface: Any) -> bool:
-        return device_interface.Worker.get_device_properties().major >= 7
-
-    def cpu_extra_check(device_interface: Any) -> bool:
-        import triton.backends
-
-        return "cpu" in triton.backends.backends
-
-    def pu1_extra_check(device_interface: Any) -> bool:
-        device = current_accelerator()
-        return device_interface.is_triton_capable(device)
-
-    def _return_true(device_interface: Any) -> bool:
-        return True
-
-    triton_supported_devices = {
-        "cuda": cuda_extra_check,
-        "xpu": _return_true,
-        "cpu": cpu_extra_check,
-        "mtia": _return_true,
-    }
+    triton_supported_devices = ["cuda", "xpu", "cpu", "mtia"]
 
     privateuse1_backend = _get_privateuse1_backend_name()
     if current_accelerator() and current_accelerator().type == privateuse1_backend:
-        triton_supported_devices[privateuse1_backend] = pu1_extra_check
+        triton_supported_devices.append(privateuse1_backend)
 
     def is_device_compatible_with_triton() -> bool:
-        for device, extra_check in triton_supported_devices.items():
-            device_interface = get_interface_for_device(device)
-            if device_interface.is_available() and extra_check(device_interface):
-                return True
+        for device in triton_supported_devices:
+            try:
+                device_interface = get_interface_for_device(device)
+                if (
+                    device_interface.is_available()
+                    and device_interface.is_triton_capable(current_accelerator())
+                ):
+                    return True
+            except NotImplementedError:
+                continue
         return False
 
     return is_device_compatible_with_triton()
