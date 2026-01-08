ad7a78dc4a9ec5b9ae2a17cfccc32145decb5d41
Laith Sakka <lsakka@meta.com>
2026-01-08 09:31:58 -0800

    Update on "[DRAFT] make size_hint fallback arg mandatory else for using size_hint_or_throw"
    cc ezyang EikanWang jgong5 wenzhe-nrv voznesenskym penguinwu Guobing-Chen XiaobingSuper zhuhaozhe blzheng jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -595,7 +595,7 @@ class SizeVarAllocator:
     def size_hint(
         self,
         expr: Union[Expr, int],
-        fallback: int,
+        fallback : int,
         *,
         hint_override: Optional[int] = None,
     ) -> int:
@@ -640,8 +640,8 @@ class SizeVarAllocator:
     def size_hints(
         self,
         exprs: Iterable[Union[Expr, int]],
+        fallback: int,
         *,
-        fallback: Optional[int] = None,
         hint_override: Optional[int] = None,
     ) -> tuple[int, ...]:
         return tuple(
