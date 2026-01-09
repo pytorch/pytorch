30a9b81f0b540cc31dcaaa45d9428033351e2413
Zhang, Jianyi <jianyi.zhang@intel.com>
2026-01-08 21:38:45 -0800

    lint

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -2765,7 +2765,9 @@ def pointwise(
                     ]
                 )
     if len(size_hints) == 3:
-        if not (inductor_meta.get("max_autotune_pointwise") or torch.xpu.is_available()):
+        if not (
+            inductor_meta.get("max_autotune_pointwise") or torch.xpu.is_available()
+        ):
             configs = [triton_config_with_settings(size_hints, 16, 16, 16)]
         else:
             configs = [
