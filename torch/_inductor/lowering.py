352b7f4ead096392f7a0b7cbe4ac06f4467b0089
eellison <elias.ellison@gmail.com>
2026-01-09 13:53:41 -0800

    tmp
    ghstack-source-id: 4206f1f9be71fb178aa6cd055ea7bcc26d60c978
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/172130

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -2483,7 +2483,6 @@ def inductor_random(size: list[int], seed: TensorBox, mode: str, *, offset: int 
         inner_fn=inner_fn,
         ranges=[*size],
     )
-    result.realize()
     return result
 
 
