65b1eb5e895a128dacc7934de4a09b353a33fd90
Aaron Orenstein <aorenste@meta.com>
2026-01-11 09:39:14 -0800

    Fix for test/distributed/test_device_mesh.py::TestDeviceMeshGetItem::test_flatten_mesh_4d
    [ghstack-poisoned]

--- torch/_dynamo/variables/torch.py
+++ torch/_dynamo/variables/torch.py
@@ -1281,7 +1281,7 @@ class TorchInGraphFunctionVariable(BaseTorchVariable):
             else:
                 return None
 
-        @register(torch.fx.experimental.symbolic_shapes.guard_size_oblivious)
+        @register(torch.fx.experimental.symbolic_shapes.guard_size_oblivious)  # type: ignore[deprecated]
         def handle_guard_size_oblivious(
             self, tx: "InstructionTranslator", expr: VariableTracker
         ) -> VariableTracker | None:
@@ -1289,7 +1289,7 @@ class TorchInGraphFunctionVariable(BaseTorchVariable):
                 # TODO: this probably should be folded somewhere else but I'm not sure where
                 # TODO: some of the other symbolic_shapes special tools can also get this treatment too
                 return variables.ConstantVariable.create(
-                    torch.fx.experimental.symbolic_shapes.guard_size_oblivious(
+                    torch.fx.experimental.symbolic_shapes.guard_size_oblivious(  # type: ignore[deprecated]
                         expr.sym_num
                     )
                 )
