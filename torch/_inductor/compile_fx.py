fc4f6ea9cf5589c16970daf806d650fad32896a3
bobrenjc93 <bobren@meta.com>
2026-01-11 21:08:19 -0800

    Update base for Update on "Skip device mesh device setup when using fake backend"
    To truly get cross compile, we need to be able to initialize a cuda device mesh without an actual device. This PR allows us to do that.
    
    
    [ghstack-poisoned]

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -850,11 +850,7 @@ def _compile_fx_inner(
     # may not be valid for use after they are run/autotuned
     torch._inductor.async_compile.CompiledTritonKernels.cache_clear()
 
-    if (
-        dynamo_utils.count_calls(gm.graph) == 0
-        and not aot_mode
-        and not torch._functorch.config.bundled_autograd_cache
-    ):
+    if dynamo_utils.count_calls(gm.graph) == 0 and not aot_mode:
         # trigger the real recompilation for _LazyGraphModule before returning
         # the forward method.
         from torch._dynamo.utils import CompileEventLogLevel
