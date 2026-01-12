5beabef5842ed4a9d9b84a4246e402dd71fd30b2
Pian Pawakapan <pianpwk@meta.com>
2026-01-12 14:08:36 -0800

    copy_strat

--- test/distributed/tensor/test_dtensor_ops.py
+++ test/distributed/tensor/test_dtensor_ops.py
@@ -445,7 +445,6 @@ dtensor_fails = {
     xfail("unsafe_chunk"),
     xfail("_unsafe_masked_index"),
     xfail("_unsafe_masked_index_put_accumulate"),
-    xfail("var_mean", "unbiased"),
     xfail("vdot"),
     xfail("view"),
     xfail("view_as"),
