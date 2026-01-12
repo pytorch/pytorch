48fc46c117874887a518a02830b002b8cf06821a
Wei Feng <weif@meta.com>
2026-01-12 13:38:37 -0800

    Update on "add assert to fix"
    [ghstack-poisoned]

--- torch/distributed/tensor/_ops/utils.py
+++ torch/distributed/tensor/_ops/utils.py
@@ -452,12 +452,19 @@ def expand_to_full_mesh_op_strategy(
                 raise RuntimeError("output spec is None")
 
         # check all inputs are shardable
-        assert False, "fix is_tensor_shardable for _StridedShard"
-        # if not all(
-        #     is_tensor_shardable(inp.shape, s)
-        #     for inp, s in zip(input_args_strategy, input_specs)
-        # ):
-        #     continue
+        # assert False, "fix is_tensor_shardable for _StridedShard"
+        # for inp, s in zip(input_args_strategy, input_specs):
+        #     result = is_tensor_shardable(inp.shape, s)
+        #     if result is False:
+        #         import fbvscode
+        #         fbvscode.set_trace()
+        #         result = is_tensor_shardable(inp.shape, s)
+
+        if not all(
+            is_tensor_shardable(inp.shape, s)
+            for inp, s in zip(input_args_strategy, input_specs)
+        ):
+            continue
 
         # perform additional op-specific filtering
         if is_valid_strategy_cb is not None:
