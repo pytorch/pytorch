7784f9a9c8a23833e221ddd5704c1267419cc5be
Jean Schmidt <contato@jschmidt.me>
2026-01-13 13:37:55 -0800

    perform multiple runs for accuracy tests for some model types
    Signed-off-by: Jean Schmidt <contato@jschmidt.me>

--- benchmarks/dynamo/torchbench.py
+++ benchmarks/dynamo/torchbench.py
@@ -428,6 +428,10 @@ class TorchBenchmarkRunner(BenchmarkRunner):
         iou_thresholds = self._tolerance.get("iou_thresholds", {})
         return iou_thresholds.get(name, 0.99)
 
+    def get_accuracy_check_runs(self, name):
+        accuracy_check_runs = self._tolerance.get("accuracy_check_runs", {})
+        return accuracy_check_runs.get(name, 1)
+
     def get_tolerance_and_cosine_flag(self, is_training, current_device, name):
         tolerance = 1e-4
         cosine = self.args.cosine
