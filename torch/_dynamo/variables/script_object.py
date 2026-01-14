f8540f463d1f04540a4089f0ee734b734c9cc6af
angelayi <yiangela7@gmail.com>
2026-01-14 14:04:24 -0800

    [opaque obj] Fix call_method on tensor subclasses (#172265)
    Changes:
    * Fixes call_methods on tensor subclasses that return an opaque object (both reference/value)
    * Fixes getattr on tensor subclasses that return a value-type opaque object
    * Catches TypeError which occurs when we try to pickle ProcessGroup
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/172265
    Approved by: https://github.com/Lucaskabela

--- torch/_dynamo/variables/script_object.py
+++ torch/_dynamo/variables/script_object.py
@@ -93,7 +93,7 @@ class OpaqueObjectClassVariable(UserDefinedVariable):
         return False
 
     def is_python_hashable(self) -> bool:
-        return is_opaque_value_type(type(self.value))
+        return is_opaque_value_type(self.value)
 
     def as_proxy(self) -> Any:
         return self.value
@@ -370,3 +370,8 @@ class TorchScriptObjectVariable(UserDefinedObjectVariable):
         ):
             return self.value.real_obj  # pyrefly: ignore[missing-attribute]
         return super().as_python_constant()
+
+    def is_python_hashable(self) -> bool:
+        return is_opaque_value_type(
+            type(self.value.real_obj)  # pyrefly: ignore[missing-attribute]
+        )
