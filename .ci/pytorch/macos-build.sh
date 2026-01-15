59ee6567837ad5b29d7662b620b612d0b69defeb
Nikita Shulga <nshulga@meta.com>
2026-01-15 10:55:49 -0800

    Update
    [ghstack-poisoned]

--- .ci/pytorch/macos-build.sh
+++ .ci/pytorch/macos-build.sh
@@ -35,8 +35,8 @@ fi
 
 
 # NS: Ugly hack to be remove by Feb 15 2026
-# Mac runners reuse previos checkouts, and actions/checkout does not clean untracked submodules
-# Which results in build failures, when we validate all the included lincenses, which reference some
+# Mac runners reuse previous checkouts, and actions/checkout does not clean untracked submodules
+# Which results in build failures, when we validate all the included licenses, which reference some
 # no longer used opentelementry submodules
 if [[ -d third_party/opentelementry-cpp ]]; then
   rm -rf third_party/opentelementry-cpp
