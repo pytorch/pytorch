076af0b2e0ca5faf38cbfeacc62ee322730efa52
Yufeng Shi <yufeng.shi@arm.com>
2026-01-16 18:20:19 +0000

    Fix lintrunner failure
    Signed-off-by: Yufeng Shi <yufeng.shi@arm.com>

--- torch/_subclasses/fake_impls.py
+++ torch/_subclasses/fake_impls.py
@@ -726,6 +726,7 @@ def _view_unbacked_meta(
     # as "materialize": clone the input to break aliasing, then reshape.
     if allow_copy:
         strides = make_contiguous_strides_for(shape)
+        # pyrefly: ignore[bad-return]
         return a.clone(memory_format=torch.contiguous_format).as_strided(shape, strides)
 
     msg = f"Cannot view a tensor with shape {a.shape} and strides {a.stride()} as a tensor with shape {shape}!"
