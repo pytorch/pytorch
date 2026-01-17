bac69d2b6c99e225869effaeccc625dd0f55d4c4
Michael Lazos <mlazos@meta.com>
2026-01-16 17:53:35 -0800

    Update base for Update on "[Inductor] Ensure combo kernels respect FMA settings"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -15215,6 +15215,7 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
         self.assertEqual(eager_result2, compiled_result2, atol=atol, rtol=rtol)
 
     @xfail_if_triton_cpu
+    @skipIfRocm
     @requires_cuda_and_triton
     @config.patch({"emulate_precision_casts": True})
     def test_addcmul_fma_uses_fma_instruction(self):
