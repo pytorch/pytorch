2a7d4052eb68934f8b2dd3e1818b5317cd1f7f8a
Ma, Jing1 <jing1.ma@intel.com>
2026-01-19 06:37:48 +0000

    Add input string check in debug_print
    Signed-off-by: Ma, Jing1 <jing1.ma@intel.com>

--- aten/src/ATen/xpu/XPUGraph.cpp
+++ aten/src/ATen/xpu/XPUGraph.cpp
@@ -185,6 +185,9 @@ void XPUGraph::enable_debug_mode() {
 }
 
 void XPUGraph::debug_dump(const std::string& debug_path) {
+  TORCH_CHECK(debug_path.size() >= 4 && debug_path.substr(debug_path.size() - 4) == ".dot",
+              "debug_path must end with .dot extension, got: ", debug_path);
+
   if (_xpu_graphs_debug || keep_graph_) {
     TORCH_WARN("DEBUG: calling debug_dump()");
     if (has_graph_) {
