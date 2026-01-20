02a20536dc2c9c61a700185fd37221d637bb00a7
lakhinderwalia <lakhinder.walia@amd.com>
2026-01-20 15:38:45 -0800

    fix CUDA build with a preprocessor flag

--- aten/src/ATen/native/cuda/KernelUtils.cuh
+++ aten/src/ATen/native/cuda/KernelUtils.cuh
@@ -257,7 +257,6 @@ __device__ inline void cmtdStore(void* address, T value) {
       asm volatile("s_waitcnt vmcnt(0)" ::: "memory");
       __atomic_signal_fence(__ATOMIC_SEQ_CST);
 }
-#endif
 
 // This function implements warp-level opportunistic fastatomics
 // To reduce contention on an atomicAdd, this replaces per-thread atomicAdd with a per-warp atomicAdd.
@@ -270,10 +269,8 @@ __device__ __forceinline__ void opportunistic_fastAtomicAdd(
     index_t index,
     const index_t numel,
     scalar_t value) {
-#ifdef USE_ROCM
   if(!(__builtin_amdgcn_processor_is("gfx942") || __builtin_amdgcn_processor_is("gfx950")))
     return;
-#endif
     scalar_t* dst = self_ptr + index;
 
     //pack coalesced bf16 and fp16
@@ -405,7 +402,7 @@ __device__ __forceinline__ void opportunistic_fastAtomicAdd(
       fastAtomicAdd(self_ptr, index, numel, crnt_val, true);
     }
 }
-
+#endif
 #undef ATOMICADD
 #undef NATIVE_ZERO_BF16
 
