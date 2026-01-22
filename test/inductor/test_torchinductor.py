408707fe9df213724d2abbbc659ea33760896553
Umesh Chand <umesh.chand@amd.com>
2026-01-22 20:50:54 +0000

    [ROCm] Fixes test_3layer_split_reduction by replacing hardcoded configs with device properties (#172499)
    Fixes #166836.
    
    PR #172431 already addressed it and was approved for merge. ROCm infra issues prevented pytorchbot merge there.
    
    Replace hardcoded configs in unit test, test_3layer_split_reduction with device properties.
    This PR also fixes tests which were dependent on split_size in choices.py which was based on hardcoded values for Nvidia devices.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/172499
    Approved by: https://github.com/jeffdaily

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -16644,7 +16644,7 @@ if RUN_GPU:
         def test_donated_buffer_inplace(self):
             batch_size = 32
             seq_length = 50
-            hidden_size = 256
+            hidden_size = 512 if torch.version.hip is not None else 256
 
             inp = torch.randn(
                 batch_size,
