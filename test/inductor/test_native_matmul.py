bcbdc7945e950ab3f0df3537c7149cc8d92ce157
nullplay <jaeyeon@mit.edu>
2026-01-22 01:34:43 -0500

    add bmm fusion tests

--- test/inductor/test_native_matmul.py
+++ test/inductor/test_native_matmul.py
@@ -38,12 +38,8 @@ class TestTritonDotReduction(TestCase):
         code = run_and_get_triton_code(f, *example_inputs)
         FileCheck().check_regex(r"triton.*mm.*\.run\(").run(code)
 
-        FileCheck().check_count(
-            "@triton.jit",
-            kernel_count,
-        ).check_count(
-            "tl.dot",
-            dot_count,
+        FileCheck().check_count("@triton.jit", kernel_count, exactly=True).check_count(
+            "tl.dot", dot_count, exactly=True
         ).run(code)
 
     def test_matmul(self):
@@ -152,6 +148,85 @@ class TestTritonDotReduction(TestCase):
 
         self._check_equal(f, (x, y))
         self._check_code(f, (x, y), 1, 1)
+
+    def test_bmm_vertical_fusion(self):
+        def f(x, y):
+            z = torch.bmm(x, y)
+            w = torch.nn.functional.relu(z)
+            v = w + z * z
+            return v
+
+        B, M, K, N = 128, 16, 128, 16
+        x = rand_strided((B, M, K), (M * K, K, 1), device=GPU_TYPE)
+        y = rand_strided((B, K, N), (K * N, N, 1), device=GPU_TYPE)
+
+        self._check_equal(f, (x, y))
+        self._check_code(f, (x, y), 1, 1)
+
+    def test_bmm_horizontal_fusion(self):
+        def f(x, y, z, w):
+            bmm1 = torch.bmm(x, y)
+            bmm2 = torch.bmm(z, w)
+            return bmm1 - bmm2 + bmm1 * bmm2
+
+        B, M, K, N = 128, 16, 128, 16
+        x = rand_strided((B, M, K), (M * K, K, 1), device=GPU_TYPE)
+        y = rand_strided((B, K, N), (K * N, N, 1), device=GPU_TYPE)
+        z = rand_strided((B, M, K), (M * K, K, 1), device=GPU_TYPE)
+        w = rand_strided((B, K, N), (K * N, N, 1), device=GPU_TYPE)
+
+        self._check_equal(f, (x, y, z, w))
+        self._check_code(f, (x, y, z, w), 1, 2)
+
+    def test_bmm_fusion_complex1(self):
+        # Out[O[b,i],j] += Input[I[b,i],r] * Weight[W[b],r,j] * Val[b,i]
+        def f(inp, weight, val, I_idx, W_idx, O_idx, out):
+            x = inp[I_idx]  # [B,I,R]
+            y = weight[W_idx]  # [B,R,J]
+            t = torch.einsum("bir,brj,bi->bij", x, y, val)
+            out.index_add_(0, O_idx.reshape(-1), t.reshape(-1, J))
+            return out
+
+        B, I, R, J = 128, 16, 128, 16
+        N_in = 1024
+        N_w = 64
+        N_out = 1024
+
+        inp = rand_strided((N_in, R), (R, 1), device=GPU_TYPE)
+        weight = rand_strided((N_w, R, J), (R * J, J, 1), device=GPU_TYPE)
+        val = rand_strided((B, I), (I, 1), device=GPU_TYPE)
+
+        I_idx = torch.randint(0, N_in, (B, I), device=GPU_TYPE, dtype=torch.int64)
+        W_idx = torch.randint(0, N_w, (B,), device=GPU_TYPE, dtype=torch.int64)
+        O_idx = torch.randint(0, N_out, (B, I), device=GPU_TYPE, dtype=torch.int64)
+
+        out = torch.zeros((N_out, J), device=GPU_TYPE)
+
+        self._check_equal(f, (inp, weight, val, I_idx, W_idx, O_idx, out))
+        self._check_code(f, (inp, weight, val, I_idx, W_idx, O_idx, out), 1, 1)
+
+    def test_bmm_fusion_complex2(self):
+        # out[Ai[g],m,n] += Av[g,m,k,p] * B[Ak[g,p],k,n]
+        def f(Av, B, Ai, Ak, out):
+            Bg = B[Ak]  # [G,P,K,N]
+            Cg = torch.einsum("gmkp,gpkn->gmn", Av, Bg)  # [G,M,N]
+            out.index_add_(0, Ai, Cg)
+            return out
+
+        G, M, K, P, N = 128, 16, 64, 8, 16
+        NB = 1024
+        NA = 512
+
+        Av = rand_strided((G, M, K, P), (M * K * P, K * P, P, 1), device=GPU_TYPE)
+        B = rand_strided((NB, K, N), (K * N, N, 1), device=GPU_TYPE)
+
+        Ai = torch.randint(0, NA, (G,), device=GPU_TYPE, dtype=torch.int64)
+        Ak = torch.randint(0, NB, (G, P), device=GPU_TYPE, dtype=torch.int64)
+
+        out = torch.zeros((NA, M, N), device=GPU_TYPE)
+
+        self._check_equal(f, (Av, B, Ai, Ak, out))
+        self._check_code(f, (Av, B, Ai, Ak, out), 1, 1)
 
     def test_bmm_large_batch_reversed_pid(self):
         def f(x, y):
