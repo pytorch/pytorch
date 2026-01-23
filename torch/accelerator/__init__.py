73b3f91f5bdd24d2866ce5061b5d2a92d1060f7a
Yu, Guangye <guangye.yu@intel.com>
2026-01-23 11:00:22 +0000

    [2/2] Introduce torch.accelerator.Graph as a unified frontend Graph interface
    ghstack-source-id: f50ead8c43f162a29660eaf9e23e50d9773773b3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/171285

--- torch/accelerator/__init__.py
+++ torch/accelerator/__init__.py
@@ -9,6 +9,7 @@ from typing_extensions import deprecated
 import torch
 
 from ._utils import _device_t, _get_device_index
+from .graphs import Graph
 from .memory import (
     empty_cache,
     get_memory_info,
@@ -23,6 +24,7 @@ from .memory import (
 
 
 __all__ = [
+    "Graph",
     "current_accelerator",
     "current_device_idx",  # deprecated
     "current_device_index",
