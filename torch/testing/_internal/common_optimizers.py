2ac23e6a382b640efd72207c584c239363d0699d
Jane Xu <janeyx@meta.com>
2026-01-23 19:26:35 +0000

    Update
    [ghstack-poisoned]

--- torch/testing/_internal/common_optimizers.py
+++ torch/testing/_internal/common_optimizers.py
@@ -1702,17 +1702,6 @@ optim_db: list[OptimizerInfo] = [
                 "TestOptimRenewed",
                 "test_fused_matches_forloop",
             ),
-            DecorateInfo(
-                # Note on tolerances:
-                # Tracking through #127000
-                toleranceOverride(
-                    {
-                        torch.float32: tol(atol=3e-5, rtol=1.3e-06),
-                    }
-                ),
-                "TestCudaOptims",
-                "test_grad_scaling_autocast_fused_optimizers",
-            ),
         ),
         skips=(
             DecorateInfo(
