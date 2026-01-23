a4acc5da9d940f77290c8306ed622be4169940aa
Vinitha Vijayan <vinithav@linux.vnet.ibm.com>
2026-01-23 21:09:02 +0000

    Report user stack also when a LoweringException occurs (#171846)
    Reports user stack also  when a LoweringException occurs
    
    Fixes pytorch#117663
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/171846
    Approved by: https://github.com/Lucaskabela

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -1373,9 +1373,17 @@ class GraphLowering(torch.fx.Interpreter):
 
             return out
         except Exception as e:
-            raise LoweringException(e, target, args, kwargs).with_traceback(
-                e.__traceback__
-            ) from None
+            stack_trace = None
+            if (
+                hasattr(self, "current_node")
+                and self.current_node is not None
+                and hasattr(self.current_node, "meta")
+                and self.current_node.meta is not None
+            ):
+                stack_trace = self.current_node.meta.get("stack_trace", None)
+            raise LoweringException(
+                e, target, args, kwargs, stack_trace=stack_trace
+            ).with_traceback(e.__traceback__) from None
 
     @staticmethod
     def can_inline_constant(t: torch.Tensor) -> bool:
