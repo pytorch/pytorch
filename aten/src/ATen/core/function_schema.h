d3c067513a13a017ee6ed5a2f899d2822ced531d
Conrad Jones <jonesconrad1@gmail.com>
2026-01-25 19:10:55 +0000

    Cast returned value to int in argumentIndexWithName
    Currently this triggers a warning C4267 on MSVC with all warnings enabled as it return a size_t to a function with a return signature of int. Adding static_cast<int> to silence warning,

--- aten/src/ATen/core/function_schema.h
+++ aten/src/ATen/core/function_schema.h
@@ -435,7 +435,7 @@ struct TORCH_API FunctionSchema {
   std::optional<int> argumentIndexWithName(std::string_view name) const {
     for (const auto i : c10::irange(arguments().size())) {
       if(name == arguments()[i].name())
-        return i;
+        return static_cast<int>(i);
     }
     return std::nullopt;
   }
