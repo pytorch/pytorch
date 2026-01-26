eb2553bdee99ab6cdc757a8755758878da3be77a
Benedikt Johannes <benedikt.johannes.hofer@gmail.com>
2026-01-26 23:23:04 +0100

    [NATIVE] [CUDA] Switch order of blocked reduce in reduction_template.cuh

--- aten/src/ATen/native/cuda/reduction_template.cuh
+++ aten/src/ATen/native/cuda/reduction_template.cuh
@@ -222,11 +222,13 @@ struct ReduceJitOp {
       value = thread_reduce<${output_vec_size}>(input_slice);
     }
 
-    if (config.should_block_y_reduce()) {
-      value = block_y_reduce<${output_vec_size}>(value, shared_memory);
-    }
+    
     if (config.should_block_x_reduce()) {
       value = block_x_reduce<${output_vec_size}>(value, shared_memory);
+    }
+
+    if (config.should_block_y_reduce()) {
+      value = block_y_reduce<${output_vec_size}>(value, shared_memory);
     }
 
     using out_ptr_vec_t = Array<out_scalar_t*, ${output_vec_size}>;
