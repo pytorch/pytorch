21e6ebb622e133218961530e498292f293cd829f
Svetlana Karslioglu <svekars@meta.com>
2026-01-27 13:03:10 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2385,8 +2385,8 @@ coverage_ignore_classes = [
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = ['.rst', '.md']
+#source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
