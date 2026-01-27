a9bfc171eaedb4cc2f6577bdf2426f95d0ae12dd
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-01-27 05:40:03 +0000

    Revert "Fix hpu backend mapping issue (#160063)"
    This reverts commit db614491b3cd8f936b4a773045b1b67d8fde76f8.
    
    Reverted https://github.com/pytorch/pytorch/pull/160063 on behalf of https://github.com/huydhn due to Sorry for reverting your change but it breaks many distributed tests in trunk ([comment](https://github.com/pytorch/pytorch/pull/160063#issuecomment-3803218266))

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -346,7 +346,7 @@ class Backend(str):  # noqa: SLOT000
 
         if devices is not None:
             for device in devices:
-                if device not in Backend.default_device_backend_map or extended_api:
+                if device not in Backend.default_device_backend_map:
                     Backend.default_device_backend_map[device] = name.lower()
         Backend.backend_type_map[name.lower()] = ProcessGroup.BackendType.CUSTOM
 
