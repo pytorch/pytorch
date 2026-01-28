c4986f88a764d8994379ab676d57fc8a4f87e58b
Will Constable <whc@meta.com>
2026-01-27 16:08:20 -0800

    Update base for Update on "[DTensor] make debugmode print optimized transforminfos"
    [ghstack-poisoned]

--- benchmarks/dynamo/pr_time_benchmarks/benchmarks/dtensor.py
+++ benchmarks/dynamo/pr_time_benchmarks/benchmarks/dtensor.py
@@ -5,7 +5,7 @@ from benchmark_base import BenchmarkBase
 import torch
 from torch.distributed._tensor import DTensor, Partial, Replicate, Shard
 from torch.testing._internal.distributed.fake_pg import FakeStore
-
+from torch.distributed.tensor._utils import ExplicitRedistributionContext
 
 class BenchmarkDTensorDispatch(BenchmarkBase):
     def __init__(self, operator, world_size) -> None:
@@ -84,14 +84,14 @@ class BenchmarkAddBackward(BenchmarkDTensorDispatch):
     def _prepare_once(self) -> None:
         super()._prepare_once()
         self.a = DTensor.from_local(
-            torch.ones(2, 512, device=self.device(), requires_grad=True),
+            torch.ones(512, 512, device=self.device(), requires_grad=True),
             self.mesh,
-            [Shard(0)],
+            [Replicate()],
         )
         self.b = DTensor.from_local(
-            torch.ones(512, 2, device=self.device(), requires_grad=True),
+            torch.ones(512, 512, device=self.device(), requires_grad=True),
             self.mesh,
-            [Shard(1)],
+            [Replicate()],
         )
 
     def _work(self) -> None:
