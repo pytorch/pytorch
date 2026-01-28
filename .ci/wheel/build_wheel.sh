fd107ff5cdf8e37607d841b5924ca82a58ff304e
Isuru Fernando <ifernando@openteams.com>
2026-01-28 18:56:59 +0000

    Update
    [ghstack-poisoned]

--- .ci/wheel/build_wheel.sh
+++ .ci/wheel/build_wheel.sh
@@ -176,21 +176,11 @@ retry pip install "${PINNED_PACKAGES[@]}" -r "${pytorch_rootdir}/requirements.tx
 # Use openmp from conda which supports 11.0. Otherwise we'll end up with
 # whatever version comes with homebrew which only supports the build machine's
 # OS version or higher
-export OMP_PREFIX=/opt/llvm-openmp
-mkdir -p ${OMP_PREFIX}
-# need zstd to extract
-retry brew install zstd
-pushd ${OMP_PREFIX}
-  llvm_openmp_version="21.1.8-h4a912ad_0"
-  retry curl -OLs https://conda.anaconda.org/conda-forge/osx-arm64/llvm-openmp-${llvm_openmp_version}.conda
-  tar -xvf llvm-openmp-${llvm_openmp_version}.conda
-  rm llvm-openmp-${llvm_openmp_version}.conda
-  tar -xvf pkg-llvm-openmp-${llvm_openmp_version}.tar.zst
-  rm pkg-llvm-openmp-${llvm_openmp_version}.tar.zst
-  rm info-llvm-openmp-${llvm_openmp_version}.tar.zst
-  rm lib/libiomp5.dylib
-  install_name_tool -id ${OMP_PREFIX}/lib/libomp.dylib lib/libomp.dylib
-popd
+if [[ -d "/opt/llvm-openmp" ]]; then
+  export OMP_PREFIX=/opt/llvm-openmp
+else
+  retry brew install libomp
+fi
 
 # For USE_DISTRIBUTED=1 on macOS, need libuv, which is build as part of tensorpipe submodule
 export USE_DISTRIBUTED=1
