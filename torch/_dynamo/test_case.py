8414c52b34f3ce4dd59e9ecc4ffeb2cb897da3de
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-01-30 15:38:24 -0500

    Update torch/_dynamo/test_case.py
    Co-authored-by: Nikita Shulga <2453524+malfet@users.noreply.github.com>

--- torch/_dynamo/test_case.py
+++ torch/_dynamo/test_case.py
@@ -200,7 +200,7 @@ class CPythonTestCase(TestCase):
         suffix = super()._dynamo_test_key()
         test_cls = self.__class__
         test_file = (
-            inspect.getfile(test_cls).rsplit(os.sep, maxsplit=1)[-1].split(".")[0]
+            inspect.getfile(test_cls).rsplit(os.sep, maxsplit=1)[-1].split(".", maxsplit=1)[0]
         )
         py_ver = re.search(r"/([\d_]+)/", inspect.getfile(test_cls))
         if py_ver:
