c575e7bea4168583e0be025df63e1781dc06bb07
albanD <desmaison.alban@gmail.com>
2026-01-30 11:47:00 -0500

    finish removing asserts in tools/
    ghstack-source-id: 026ca9d9d2b0facd2f11a9aa596b5df7a8c6ba43
    Pull-Request: https://github.com/pytorch/pytorch/pull/173827

--- tools/test/test_cmake.py
+++ tools/test/test_cmake.py
@@ -76,7 +76,10 @@ class TestCMake(unittest.TestCase):
         # window matches.
         for i in range(len(sequence) - len(subsequence) + 1):
             candidate = sequence[i : i + len(subsequence)]
-            assert len(candidate) == len(subsequence)  # sanity check
+            if len(candidate) != len(subsequence):  # sanity check
+                raise AssertionError(
+                    f"candidate length mismatch: {len(candidate)} != {len(subsequence)}"
+                )
             if candidate == subsequence:
                 return  # found it
         raise AssertionError(f"{subsequence} not found in {sequence}")
