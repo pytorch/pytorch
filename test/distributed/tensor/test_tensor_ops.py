cf87455fdc69c2b9458862ecded0b0140187ba12
Uros Marinkovic <uros.marinkovic@htecgroup.com>
2026-01-30 16:34:07 +0000

    Fixed linting issue

--- test/distributed/tensor/test_tensor_ops.py
+++ test/distributed/tensor/test_tensor_ops.py
@@ -17,7 +17,12 @@ from torch.distributed.tensor import (
 from torch.distributed.tensor._sharding_prop import ShardingPropagator
 from torch.distributed.tensor.debug import CommDebugMode
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
-from torch.testing._internal.common_utils import MI200_ARCH, run_tests, skipIfRocmArch, serialTest
+from torch.testing._internal.common_utils import (
+    MI200_ARCH,
+    run_tests,
+    serialTest,
+    skipIfRocmArch,
+)
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     create_local_tensor_test_class,
     DTensorConverter,
