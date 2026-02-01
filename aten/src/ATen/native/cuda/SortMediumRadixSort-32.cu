793b7f629346570c84d3d0ef826924c23733a058
Taeksang Kim <voidbag@gmail.com>
2026-02-01 20:52:00 +0000

    Split files to reduce kernel bin for speedup
    Signed-off-by: Taeksang Kim <ts.kim@hyperaccel.ai>

---
+++ aten/src/ATen/native/cuda/SortMediumRadixSort-32.cu
@@ -0,0 +1,6 @@
+#include <ATen/native/cuda/SortImpl.cuh>
+
+namespace at::native {
+  template void sortCommon<uint32_t>(MediumRadixSort sorter, const TensorBase &key, const TensorBase &value,
+                int dim, bool descending);
+}
---
+++ aten/src/ATen/native/cuda/SortWarpMergeSort-16.cu
@@ -0,0 +1,8 @@
+#include <ATen/native/cuda/SortImpl.cuh>
+
+namespace at::native {
+#if HAS_WARP_MERGE_SORT()
+  template void sortCommon<uint16_t>(WarpMergeSort<128> sorter, const TensorBase &key, const TensorBase &value,
+                int dim, bool descending);
+#endif
+}
