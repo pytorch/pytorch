bd072702294c35fe92ef32ecbd44c19390587a74
Subin George <sugeorge@redhat.com>
2026-02-04 09:42:52 +0530

    Fix D3 v6+ event handling for zoom and interactions
    The d3.event global was removed in D3 v6. This commit updates all event
    handlers to receive the event as a parameter instead:
    
    - keydown handler: d3.event.key -> event.key
    - zoom handlers: d3.event.transform -> event.transform
    - mouseover/mousedown/mouseleave: d3.event.target -> event.target
    - brush handler: d3.event.selection -> event.selection
    
    This fixes the zoom functionality that was broken after upgrading to D3 v7.

--- torch/utils/viz/MemoryViz.js
+++ torch/utils/viz/MemoryViz.js
@@ -80,13 +80,13 @@ function EventSelector(outer, events, stack_info, memory_view) {
       selected_event_idx = idx;
     },
   };
-  d3.select('body').on('keydown', _e => {
-    const key = d3.event.key;
+  d3.select('body').on('keydown', event => {
+    const key = event.key;
     const actions = {ArrowDown: 1, ArrowUp: -1};
     if (selected_event_idx !== null && key in actions) {
       const new_idx = selected_event_idx + actions[key];
       es.select(Math.max(0, Math.min(new_idx, events.length - 1)));
-      d3.event.preventDefault();
+      event.preventDefault();
     }
   });
 
@@ -208,8 +208,8 @@ function MemoryView(outer, stack_info, snapshot, device) {
     .attr('preserveAspectRatio', 'xMinYMin meet');
   const g = svg.append('g');
   const seg_zoom = zoom();
-  seg_zoom.on('zoom', () => {
-    g.attr('transform', d3.event.transform);
+  seg_zoom.on('zoom', (event) => {
+    g.attr('transform', event.transform);
   });
   svg.call(seg_zoom);
 
@@ -608,20 +608,20 @@ function StackInfo(outer) {
   return {
     register(dom, enter, leave = _e => {}, select = _e => {}) {
       dom
-        .on('mouseover', _e => {
+        .on('mouseover', event => {
           selected.leave();
-          stack_trace.text(enter(d3.select(d3.event.target)));
+          stack_trace.text(enter(d3.select(event.target)));
         })
-        .on('mousedown', _e => {
-          const obj = d3.select(d3.event.target);
+        .on('mousedown', event => {
+          const obj = d3.select(event.target);
           selected = {
             enter: () => stack_trace.text(enter(obj)),
             leave: () => leave(obj),
           };
           select(obj);
         })
-        .on('mouseleave', _e => {
-          leave(d3.select(d3.event.target));
+        .on('mouseleave', event => {
+          leave(d3.select(event.target));
           selected.enter();
         });
     },
@@ -1162,10 +1162,10 @@ function MemoryPlot(
 
   const axis = plot_coordinate_space.append('g').call(yaxis);
 
-  function handleZoom() {
-    const t = d3.event.transform;
+  function handleZoom(event) {
+    const t = event.transform;
     zoom_group.attr('transform', t);
-    axis.call(yaxis.scale(d3.event.transform.rescaleY(yscale)));
+    axis.call(yaxis.scale(event.transform.rescaleY(yscale)));
   }
 
   const thezoom = zoom().on('zoom', handleZoom);
@@ -1191,13 +1191,13 @@ function MemoryPlot(
     },
     set_delegate: delegate => {
       plot
-        .on('mouseover', function (_e, _d) {
+        .on('mouseover', function (event, _d) {
           delegate.set_selected(d3.select(this));
         })
-        .on('mousedown', function (_e, _d) {
+        .on('mousedown', function (event, _d) {
           delegate.default_selected = d3.select(this);
         })
-        .on('mouseleave', function (_e, _d) {
+        .on('mouseleave', function (event, _d) {
           delegate.set_selected(delegate.default_selected);
         });
     },
@@ -1273,8 +1273,8 @@ function MiniMap(mini_svg, plot, data, left_pad, width, height = 70) {
     [left_pad, 0],
     [width, height],
   ]);
-  brush.on('brush', function () {
-    const [begin, end] = d3.event.selection.map(x => x - left_pad);
+  brush.on('brush', function (event) {
+    const [begin, end] = event.selection.map(x => x - left_pad);
 
     const stepbegin = Math.floor(xscale.invert(begin));
     const stepend = Math.floor(xscale.invert(end));
