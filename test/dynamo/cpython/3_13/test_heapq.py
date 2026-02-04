9de5b5e65cc50c4e264eaceb02d0c9017a62804b
xgz2 <xiaominggeorge.zhuang@gmail.com>
2026-02-04 08:22:02 -0800

    remove formatting on test file

--- test/dynamo/cpython/3_13/test_heapq.py
+++ test/dynamo/cpython/3_13/test_heapq.py
@@ -28,32 +28,23 @@ from test.support import import_helper
 from unittest import TestCase, skipUnless
 from operator import itemgetter
 
-py_heapq = import_helper.import_fresh_module("heapq", blocked=["_heapq"])
-c_heapq = import_helper.import_fresh_module("heapq", fresh=["_heapq"])
+py_heapq = import_helper.import_fresh_module('heapq', blocked=['_heapq'])
+c_heapq = import_helper.import_fresh_module('heapq', fresh=['_heapq'])
 
 # _heapq.nlargest/nsmallest are saved in heapq._nlargest/_smallest when
 # _heapq is imported, so check them there
-func_names = [
-    "heapify",
-    "heappop",
-    "heappush",
-    "heappushpop",
-    "heapreplace",
-    "_heappop_max",
-    "_heapreplace_max",
-    "_heapify_max",
-]
-
+func_names = ['heapify', 'heappop', 'heappush', 'heappushpop', 'heapreplace',
+              '_heappop_max', '_heapreplace_max', '_heapify_max']
 
 class TestModules(__TestCase):
     def test_py_functions(self):
         for fname in func_names:
-            self.assertEqual(getattr(py_heapq, fname).__module__, "heapq")
+            self.assertEqual(getattr(py_heapq, fname).__module__, 'heapq')
 
-    @skipUnless(c_heapq, "requires _heapq")
+    @skipUnless(c_heapq, 'requires _heapq')
     def test_c_functions(self):
         for fname in func_names:
-            self.assertEqual(getattr(c_heapq, fname).__module__, "_heapq")
+            self.assertEqual(getattr(c_heapq, fname).__module__, '_heapq')
 
 
 @torch._dynamo.disable
@@ -62,6 +53,7 @@ def randrange(*args):
 
 
 class _TestHeap:
+
     def test_push_pop(self):
         # 1) Push 256 random numbers and pop them off, verifying all's OK.
         heap = []
@@ -94,8 +86,8 @@ class _TestHeap:
     def check_invariant(self, heap):
         # Check the heap invariant.
         for pos, item in enumerate(heap):
-            if pos:  # pos 0 has no parent
-                parentpos = (pos - 1) >> 1
+            if pos: # pos 0 has no parent
+                parentpos = (pos-1) >> 1
                 self.assertTrue(heap[parentpos] <= item)
 
     def test_heapify(self):
@@ -153,7 +145,7 @@ class _TestHeap:
             for item in data[10:]:
                 self.module.heappushpop(heap, item)
             self.assertEqual(list(self.heapiter(heap)), sorted(data)[-10:])
-            self.assertEqual(self.module.heappushpop([], "x"), "x")
+            self.assertEqual(self.module.heappushpop([], 'x'), 'x')
 
     def test_heappushpop(self):
         h = []
@@ -187,10 +179,10 @@ class _TestHeap:
             with torch._dynamo.error_on_graph_break(False):
                 size = randrange(50)
                 data = [randrange(25) for i in range(size)]
-            if trial & 1:  # Half of the time, use heapify
+            if trial & 1:     # Half of the time, use heapify
                 heap = data[:]
                 self.module.heapify(heap)
-            else:  # The rest of the time, use heappush
+            else:             # The rest of the time, use heappush
                 heap = []
                 for item in data:
                     self.module.heappush(heap, item)
@@ -203,7 +195,7 @@ class _TestHeap:
             for i in range(randrange(25)):
                 row = []
                 for j in range(randrange(100)):
-                    tup = random.choice("ABC"), randrange(-500, 500)
+                    tup = random.choice('ABC'), randrange(-500, 500)
                     row.append(tup)
                 inputs.append(row)
 
@@ -212,10 +204,8 @@ class _TestHeap:
                 seqs = []
                 for seq in inputs:
                     seqs.append(sorted(seq, key=key, reverse=reverse))
-                self.assertEqual(
-                    sorted(chain(*inputs), key=key, reverse=reverse),
-                    list(self.module.merge(*seqs, key=key, reverse=reverse)),
-                )
+                self.assertEqual(sorted(chain(*inputs), key=key, reverse=reverse),
+                                 list(self.module.merge(*seqs, key=key, reverse=reverse)))
                 self.assertEqual(list(self.module.merge()), [])
 
     def test_empty_merges(self):
@@ -229,17 +219,14 @@ class _TestHeap:
         def iterable():
             s = list(range(10))
             for i in range(20):
-                yield s[i]  # IndexError when i > 10
-
+                yield s[i]       # IndexError when i > 10
         with self.assertRaises(IndexError):
             list(self.module.merge(iterable(), iterable()))
 
     def test_merge_stability(self):
         with torch._dynamo.error_on_graph_break(False):
-
             class Int(int):
                 pass
-
         inputs = [[], [], [], []]
         for i in range(20000):
             with torch._dynamo.error_on_graph_break(False):
@@ -256,29 +243,22 @@ class _TestHeap:
     def test_nsmallest(self):
         with torch._dynamo.error_on_graph_break(False):
             data = [(randrange(2000), i) for i in range(1000)]
-            for f in (None, lambda x: x[0] * 547 % 2000):
+            for f in (None, lambda x:  x[0] * 547 % 2000):
                 for n in (0, 1, 2, 10, 100, 400, 999, 1000, 1100):
-                    self.assertEqual(
-                        list(self.module.nsmallest(n, data)), sorted(data)[:n]
-                    )
-                    self.assertEqual(
-                        list(self.module.nsmallest(n, data, key=f)),
-                        sorted(data, key=f)[:n],
-                    )
+                    self.assertEqual(list(self.module.nsmallest(n, data)),
+                                    sorted(data)[:n])
+                    self.assertEqual(list(self.module.nsmallest(n, data, key=f)),
+                                    sorted(data, key=f)[:n])
 
     def test_nlargest(self):
         with torch._dynamo.error_on_graph_break(False):
             data = [(randrange(2000), i) for i in range(1000)]
-            for f in (None, lambda x: x[0] * 547 % 2000):
+            for f in (None, lambda x:  x[0] * 547 % 2000):
                 for n in (0, 1, 2, 10, 100, 400, 999, 1000, 1100):
-                    self.assertEqual(
-                        list(self.module.nlargest(n, data)),
-                        sorted(data, reverse=True)[:n],
-                    )
-                    self.assertEqual(
-                        list(self.module.nlargest(n, data, key=f)),
-                        sorted(data, key=f, reverse=True)[:n],
-                    )
+                    self.assertEqual(list(self.module.nlargest(n, data)),
+                                    sorted(data, reverse=True)[:n])
+                    self.assertEqual(list(self.module.nlargest(n, data, key=f)),
+                                    sorted(data, key=f, reverse=True)[:n])
 
     def test_comparison_operator(self):
         # Issue 3051: Make sure heapq works with both __lt__
@@ -287,23 +267,17 @@ class _TestHeap:
             data = [comp(x) for x in data]
             self.module.heapify(data)
             return [self.module.heappop(data).x for i in range(len(data))]
-
         with torch._dynamo.error_on_graph_break(False):
-
             class LT:
                 def __init__(self, x):
                     self.x = x
-
                 def __lt__(self, other):
                     return self.x > other.x
-
             class LE:
                 def __init__(self, x):
                     self.x = x
-
                 def __le__(self, other):
                     return self.x >= other.x
-
             data = [random.random() for i in range(100)]
             target = sorted(data, reverse=True)
             self.assertEqual(hsort(data, LT), target)
@@ -314,135 +288,100 @@ class TestHeapPython(_TestHeap, __TestCase):
     module = py_heapq
 
 
-@skipUnless(c_heapq, "requires _heapq")
+@skipUnless(c_heapq, 'requires _heapq')
 class TestHeapC(_TestHeap, __TestCase):
     module = c_heapq
 
 
-# ==============================================================================
-
+#==============================================================================
 
 class LenOnly:
     "Dummy sequence class defining __len__ but not __getitem__."
-
     def __len__(self):
         return 10
 
-
 class CmpErr:
     "Dummy element that always raises an error during comparison"
-
     def __eq__(self, other):
         raise ZeroDivisionError
-
     __ne__ = __lt__ = __le__ = __gt__ = __ge__ = __eq__
 
-
 def R(seqn):
-    "Regular generator"
+    'Regular generator'
     for i in seqn:
         yield i
 
-
 class G:
-    "Sequence using __getitem__"
-
+    'Sequence using __getitem__'
     def __init__(self, seqn):
         self.seqn = seqn
-
     def __getitem__(self, i):
         return self.seqn[i]
 
-
 class I:
-    "Sequence using iterator protocol"
-
+    'Sequence using iterator protocol'
     def __init__(self, seqn):
         self.seqn = seqn
         self.i = 0
-
     def __iter__(self):
         return self
-
     def __next__(self):
-        if self.i >= len(self.seqn):
-            raise StopIteration
+        if self.i >= len(self.seqn): raise StopIteration
         v = self.seqn[self.i]
         self.i += 1
         return v
 
-
 class Ig:
-    "Sequence using iterator protocol defined with a generator"
-
+    'Sequence using iterator protocol defined with a generator'
     def __init__(self, seqn):
         self.seqn = seqn
         self.i = 0
-
     def __iter__(self):
         for val in self.seqn:
             yield val
 
-
 class X:
-    "Missing __getitem__ and __iter__"
-
+    'Missing __getitem__ and __iter__'
     def __init__(self, seqn):
         self.seqn = seqn
         self.i = 0
-
     def __next__(self):
-        if self.i >= len(self.seqn):
-            raise StopIteration
+        if self.i >= len(self.seqn): raise StopIteration
         v = self.seqn[self.i]
         self.i += 1
         return v
 
-
 class N:
-    "Iterator missing __next__()"
-
+    'Iterator missing __next__()'
     def __init__(self, seqn):
         self.seqn = seqn
         self.i = 0
-
     def __iter__(self):
         return self
-
 
 class E:
-    "Test propagation of exceptions"
-
+    'Test propagation of exceptions'
     def __init__(self, seqn):
         self.seqn = seqn
         self.i = 0
-
     def __iter__(self):
         return self
-
     def __next__(self):
         3 // 0
 
-
 class S:
-    "Test immediate stop"
-
+    'Test immediate stop'
     def __init__(self, seqn):
         pass
-
     def __iter__(self):
         return self
-
     def __next__(self):
         raise StopIteration
 
-
 from itertools import chain
-
-
 def L(seqn):
-    "Test multiple tiers of iterators"
-    return chain(map(lambda x: x, R(Ig(G(seqn)))))
+    'Test multiple tiers of iterators'
+    return chain(map(lambda x:x, R(Ig(G(seqn)))))
 
 
 class SideEffectLT:
@@ -456,15 +395,12 @@ class SideEffectLT:
 
 
 class _TestErrorHandling:
+
     def test_non_sequence(self):
         for f in (self.module.heapify, self.module.heappop):
             self.assertRaises((TypeError, AttributeError), f, 10)
-        for f in (
-            self.module.heappush,
-            self.module.heapreplace,
-            self.module.nlargest,
-            self.module.nsmallest,
-        ):
+        for f in (self.module.heappush, self.module.heapreplace,
+                  self.module.nlargest, self.module.nsmallest):
             self.assertRaises((TypeError, AttributeError), f, 10, 10)
 
     def test_len_only(self):
@@ -486,21 +422,16 @@ class _TestErrorHandling:
 
     def test_arg_parsing(self):
         with torch._dynamo.error_on_graph_break(False):
-            for f in (
-                self.module.heapify,
-                self.module.heappop,
-                self.module.heappush,
-                self.module.heapreplace,
-                self.module.nlargest,
-                self.module.nsmallest,
-            ):
+            for f in (self.module.heapify, self.module.heappop,
+                    self.module.heappush, self.module.heapreplace,
+                    self.module.nlargest, self.module.nsmallest):
                 self.assertRaises((TypeError, AttributeError), f, 10)
 
     def test_iterable_args(self):
         for f in (self.module.nlargest, self.module.nsmallest):
-            for s in ("123", "", range(1000), (1, 1.2), range(2000, 2200, 5)):
+            for s in ("123", "", range(1000), (1, 1.2), range(2000,2200,5)):
                 for g in (G, I, Ig, L, R):
-                    self.assertEqual(list(f(2, g(s))), list(f(2, s)))
+                    self.assertEqual(list(f(2, g(s))), list(f(2,s)))
                 self.assertEqual(list(f(2, S(s))), [])
                 self.assertRaises(TypeError, f, 2, X(s))
                 self.assertRaises(TypeError, f, 2, N(s))
@@ -526,7 +457,6 @@ class _TestErrorHandling:
         # See bpo-39421: Strong references need to be taken
         # when comparing objects as they can alter the heap
         with torch._dynamo.error_on_graph_break(False):
-
             class EvilClass(int):
                 def __lt__(self, o):
                     heap.clear()
@@ -537,8 +467,8 @@ class _TestErrorHandling:
         self.assertRaises(IndexError, self.module.heappushpop, heap, 1)
 
     def test_comparison_operator_modifiying_heap_two_heaps(self):
-        with torch._dynamo.error_on_graph_break(False):
 
+        with torch._dynamo.error_on_graph_break(False):
             class h(int):
                 def __lt__(self, o):
                     list2.clear()
@@ -557,12 +487,10 @@ class _TestErrorHandling:
         self.assertRaises((IndexError, RuntimeError), self.module.heappush, list1, g(1))
         self.assertRaises((IndexError, RuntimeError), self.module.heappush, list2, h(1))
 
-
 class TestErrorHandlingPython(_TestErrorHandling, __TestCase):
     module = py_heapq
 
-
-@skipUnless(c_heapq, "requires _heapq")
+@skipUnless(c_heapq, 'requires _heapq')
 class TestErrorHandlingC(_TestErrorHandling, __TestCase):
     module = c_heapq
 
