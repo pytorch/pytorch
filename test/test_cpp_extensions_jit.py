06da4e9adca7b77f95376ea55fab19f29903cf97
albanD <desmaison.alban@gmail.com>
2026-02-03 22:45:37 -0500

    Update (base update)
    [ghstack-poisoned]

--- test/test_cpp_extensions_jit.py
+++ test/test_cpp_extensions_jit.py
@@ -865,7 +865,8 @@ class TestCppExtensionJIT(common.TestCase):
         # Try calling zero_grad()
         net.zero_grad()
         for p in net.parameters():
-            assert p.grad is None, "zero_grad defaults to setting grads to None"
+            if p.grad is not None:
+                raise AssertionError("zero_grad defaults to setting grads to None")
 
         # Test train(), eval(), training (a property)
         self.assertTrue(net.training)
