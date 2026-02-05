41edf9ccfb7833490b80299eef64f80ed9ec9b03
yuchengliu1 <yucheng.liu@intel.com>
2026-02-05 16:06:29 +0800

    fix lint

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -1504,7 +1504,7 @@ class cpp:
     )
 
     # threshold between two step reduction algorithm and welford reduction algorithm
-    use_two_step_variance_threshold = 1024  
+    use_two_step_variance_threshold = 1024
 
 class triton:
     """
