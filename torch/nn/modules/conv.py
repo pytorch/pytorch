1d0354073aed0d15f992caf402b6f8107610ce5e
Dev Sashidhar <dsashidh@redhat.com>
2026-02-05 20:49:29 +0000

    moving fix to dynamo layer, adding context manager

--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -1468,7 +1468,10 @@ class _LazyConvXdMixin(LazyModuleMixin):
                 f"to {self.__class__.__name__}, but "
                 f"got input of size: {input.shape}"
             )
-        return input.shape[1] if input.dim() == num_dims_batch else input.shape[0]
+        in_channels = (
+            input.shape[1] if input.dim() == num_dims_batch else input.shape[0]
+        )
+        return in_channels
 
     # Function to return the number of spatial dims expected for inputs to the module.
     # This is expected to be implemented by subclasses.
