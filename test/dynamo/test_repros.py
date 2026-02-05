0110bac843f27b6f83db4d42faa5f409b742a512
Erxin Shang <erxin.shang@intel.com>
2026-02-05 17:45:37 +0800

    Merge conflict TEST_XPU,xfailIfS390X,

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -68,7 +68,6 @@ from torch.testing._internal.common_cuda import (
     PLATFORM_SUPPORTS_FP8,
     SM70OrLater,
     TEST_CUDA,
-    TEST_XPU,
 )
 from torch.testing._internal.common_device_type import (
     E4M3_MAX_POS,
@@ -84,7 +83,8 @@ from torch.testing._internal.common_utils import (
     skipIfRocm,
     skipIfWindows,
     TEST_WITH_ROCM,
-    xfailIfS390X,
+	xfailIfS390X,
+	TEST_XPU,
 )
 from torch.testing._internal.logging_utils import LoggingTestCase, make_logging_test
 from torch.testing._internal.two_tensor import TwoTensor
