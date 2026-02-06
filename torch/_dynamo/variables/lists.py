bd0d978773d9b486a023133fe1a1fd2ce5454868
xgz2 <xiaominggeorge.zhuang@gmail.com>
2026-02-06 01:08:36 -0800

    add handling for 0 dim fake tensors in SizeVariable.as_proxy

--- torch/_dynamo/variables/lists.py
+++ torch/_dynamo/variables/lists.py
@@ -1271,6 +1271,8 @@ class SizeVariable(TupleVariable):
         return torch.Size
 
     def as_proxy(self) -> Any:
+        from torch._subclasses.fake_tensor import FakeTensor
+
         if self.proxy is not None:
             return self.proxy
 
@@ -1306,15 +1308,20 @@ class SizeVariable(TupleVariable):
             return torch.Size(proxies)
 
         proxy = tracer.create_proxy("call_function", torch.Size, (proxies,), {})
-        set_example_value(
-            proxy.node,
-            torch.Size(
-                [
-                    p.node.meta["example_value"] if not isinstance(p, int) else p
-                    for p in proxies
-                ]
-            ),
-        )
+
+        proxy_values = []
+        for p in proxies:
+            if isinstance(p, int):
+                proxy_values.append(p)
+            else:
+                example_value = p.node.meta["example_value"]
+                if isinstance(
+                    example_value, FakeTensor
+                ) and example_value.size() == torch.Size([]):
+                    continue
+                proxy_values.append(example_value)
+
+        set_example_value(proxy.node, torch.Size(proxy_values))
         return proxy
 
     def reconstruct(self, codegen: "PyCodegen") -> None:
