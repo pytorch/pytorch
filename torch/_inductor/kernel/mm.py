a9aa0044edafc7fc537d48586cedc902788c256a
Laith Sakka <lsakka@meta.com>
2026-02-05 18:13:17 -0800

    more optimization_hint usage in autotuning context
    ghstack-source-id: 61a4a3bfad8778a88291b791459c9eaf39e3da02
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/174452

--- torch/_inductor/kernel/mm.py
+++ torch/_inductor/kernel/mm.py
@@ -1106,16 +1106,10 @@ def mm_autoheuristic(
 
 def get_size_hints(mat1, mat2, m, n, k):
     if not isinstance(m, int) or not isinstance(k, int):
-        (m, k) = V.graph.sizevars.size_hints(
-            mat1.get_size(),
-            fallback=torch._inductor.config.unbacked_symint_fallback,
-        )
+        (m, k) = V.graph.sizevars.optimization_hints(mat1.get_size())
 
     if not isinstance(n, int) or not isinstance(k, int):
-        (k, n) = V.graph.sizevars.size_hints(
-            mat2.get_size(),
-            fallback=torch._inductor.config.unbacked_symint_fallback,
-        )
+        (k, n) = V.graph.sizevars.optimization_hints(mat2.get_size())
     return m, n, k
 
 
@@ -1126,9 +1120,6 @@ def get_size_hints_strides(mat1, mat2):
     strides_hints = []
     for stride in strides:
         if not isinstance(stride, int):
-            stride = V.graph.sizevars.size_hints(
-                stride,
-                fallback=torch._inductor.config.unbacked_symint_fallback,
-            )
+            stride = V.graph.sizevars.optimization_hints(stride)
         strides_hints.append(stride)
     return strides_hints[0], strides_hints[1]
