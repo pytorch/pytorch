caff652613acbe80c9b82e827e11e1fd3b551c5d
Laith Sakka <lsakka@meta.com>
2026-02-09 13:12:36 -0800

    Update base for Update on "optimize common pattern 1 in symbbolic reasoning"
    [ghstack-poisoned]

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -2379,7 +2379,9 @@ class CppKernel(Kernel):
 
     def size_hint(self):
         assert self.call_ranges is not None
-        return V.graph.sizevars.optimization_hint(sympy_product(self.call_ranges))
+        return V.graph.sizevars.size_hint(
+            sympy_product(self.call_ranges), fallback=8192
+        )
 
     def codegen_loops_impl(self, loop_nest, code, worksharing):
         assert isinstance(self, CppKernelProxy)
@@ -3935,8 +3937,8 @@ class TilingSelect:
                     if tiling_indice < 0 or tiling_indice >= len(call_ranges):
                         continue
                     if has_free_symbols(call_ranges):
-                        call_range = V.graph.sizevars.optimization_hint(
-                            call_ranges[tiling_indice]
+                        call_range = V.graph.sizevars.size_hint(
+                            call_ranges[tiling_indice], fallback=0
                         )
                         if call_range < factor_lowp:
                             V.graph.sizevars.check_lt(call_range, factor_lowp)  # type: ignore[arg-type]
