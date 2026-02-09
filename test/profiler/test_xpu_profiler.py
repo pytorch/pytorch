badc936100f14f43ab407b58af2730ca55026c73
Miroslaw Oksiucik <miroslaw.oksiucik@intel.com>
2026-02-09 13:58:22 +0200

    Fix merge commit

--- test/profiler/test_xpu_profiler.py
+++ test/profiler/test_xpu_profiler.py
@@ -1,5 +1,6 @@
 # Owner(s): ["oncall: profiler"]
 
+
 import json
 import os
 import tempfile
@@ -7,6 +8,7 @@ import unittest
 from collections import defaultdict
 
 import torch
+from torch.profiler import DeviceType
 from torch.testing._internal.common_utils import run_tests, TEST_XPU, TestCase
 
 
@@ -14,6 +16,28 @@ Verbose = False
 
 
 class XpuProfilerTest(TestCase):
+    @unittest.skipIf(not TEST_XPU, "test requires XPU")
+    def test_profiler(self):
+        t = torch.empty(1000, dtype=torch.int, device="xpu")
+        with torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.XPU,
+            ]
+        ) as p:
+            for _ in range(10):
+                t.zero_()
+
+        events = defaultdict(int)
+        for event in p.events():
+            events[event.device_type] += 1
+
+        if Verbose:
+            print(f"{events = }")
+
+        self.assertEqual(len(events), 2)
+        self.assertTrue(DeviceType.CPU in events)
+        self.assertTrue(DeviceType.XPU in events)
+
     def gen_and_check_json(self, p, json_file):
         p.export_chrome_trace(json_file)
 
@@ -70,44 +94,6 @@ class XpuProfilerTest(TestCase):
 
         if Verbose:
             print(p.key_averages().table())
-
-
-if __name__ == "__main__":
-    run_tests()
-
-import unittest
-from collections import defaultdict
-
-import torch
-from torch.profiler import DeviceType
-from torch.testing._internal.common_utils import run_tests, TEST_XPU, TestCase
-
-
-Verbose = False
-
-
-class XpuProfilerTest(TestCase):
-    @unittest.skipIf(not TEST_XPU, "test requires XPU")
-    def test_profiler(self):
-        t = torch.empty(1000, dtype=torch.int, device="xpu")
-        with torch.profiler.profile(
-            activities=[
-                torch.profiler.ProfilerActivity.XPU,
-            ]
-        ) as p:
-            for _ in range(10):
-                t.zero_()
-
-        events = defaultdict(int)
-        for event in p.events():
-            events[event.device_type] += 1
-
-        if Verbose:
-            print(f"{events = }")
-
-        self.assertEqual(len(events), 2)
-        self.assertTrue(DeviceType.CPU in events)
-        self.assertTrue(DeviceType.XPU in events)
 
 
 if __name__ == "__main__":
