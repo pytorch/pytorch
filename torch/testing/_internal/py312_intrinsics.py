0b73b73c1e78cac43f5a95ca1d27560573286c4d
Guilherme Leobas <gleobas@quansight.com>
2026-02-11 13:59:23 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/testing/_internal/py312_intrinsics.py
+++ torch/testing/_internal/py312_intrinsics.py
@@ -5,7 +5,7 @@ import torch
 # CALL_INTRINSIC_2 7. The problem is those intrinsics only exists on Python
 # 3.12+ and the Python code to produce them is not backwards compatible with
 # previous versions.
-class Py312Intrinsics:
+class Foo:
     @classmethod
     def _default_update(cls):
         def f[T](a: "This is a new annotation"):  # noqa: F722
@@ -22,7 +22,7 @@ class Py312Intrinsics:
     def test_default_update(self):
         @torch.compile(backend="eager", fullgraph=True)
         def fn(x):
-            wrapper, f = Py312Intrinsics._default_update()
+            wrapper, f = Foo._default_update()
             return x + 1
 
         x = torch.randn(2)
