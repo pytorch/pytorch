f9500d61e4dd8fd9a399674d0c627fedfa4e7c76
Pian Pawakapan <pianpwk@meta.com>
2026-02-11 10:45:27 -0800

    [dynamic shapes] conv unbacked
    [ghstack-poisoned]

--- torch/_subclasses/fake_impls.py
+++ torch/_subclasses/fake_impls.py
@@ -1320,7 +1320,11 @@ def conv(
         # Avoid importing sympy at a module level
         from torch.fx.experimental.symbolic_shapes import has_hint
 
-        if not has_hint(batch):
+        all_hinted = all(has_hint(s) for s in new_kwargs["input"].shape) and all(
+            has_hint(s) for s in new_kwargs["weight"].shape
+        )
+
+        if not all_hinted:
             # TODO: We can make this a little more faithful with best effort
             # channels last detection (but only if it's statically obvious!)
             mem_fmt = None
