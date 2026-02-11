da238ea6c26685b1064c3dea853e9af3659856f9
Nikita Vedeneev <nvedzeneyeu@nvidia.com>
2026-02-11 12:45:47 +0000

    [Inductor] addmm with bias -> unfuse bias if there is a pointwise/reduction user
    ghstack-source-id: bf4c5811d13a0f621e089da4491eb125b562da62
    Pull-Request: https://github.com/pytorch/pytorch/pull/166165

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -16519,7 +16519,7 @@ if RUN_GPU:
                 ),
                 (
                     fn3,
-                    "triton_poi_fused_native_layer_norm_relu",
+                    "triton_poi_fused_addmm_native_layer_norm",
                     (torch.randn(4, 4, device=GPU_TYPE),),
                 ),
             ]
@@ -16532,7 +16532,7 @@ if RUN_GPU:
                 ),
                 (
                     fn3,
-                    "triton_poi_fused_LayerNorm_ReLU",
+                    "triton_poi_fused_LayerNorm_Linear_ReLU",
                     (torch.randn(4, 4, device=GPU_TYPE),),
                 ),
             ]
