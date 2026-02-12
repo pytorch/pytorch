291cef573e00354533d049c6286f48c98a2b5b2c
Zhang, Liangang <liangang.zhang@intel.com>
2026-02-12 10:59:44 +0800

    Recover the USE_TMA default value according to the device information

--- torch/_inductor/kernel/flex/flex_attention.py
+++ torch/_inductor/kernel/flex/flex_attention.py
@@ -910,7 +910,7 @@ def flex_attention_backward(*args, **kwargs):
             )
 
         # Intel GPU enables TMA by default
-        cur_kernel_options.setdefault("USE_TMA", True)
+        cur_kernel_options.setdefault("USE_TMA", bool(torch.xpu.is_available()))
 
         if (
             cur_kernel_options["USE_TMA"]
