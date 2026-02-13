2c608330741498e7ee60eaea53d79abe76b67771
Laith Sakka <lsakka@meta.com>
2026-02-12 21:29:29 -0800

    Update base for Update on "More size-hinting cleanups"
    Most changes belongs to:
    
    1. all size_hint calls with fallback --> use optimization_hint since we already handle unbacked. 
    2. remove fallback from size_hint call (note size_hint will deleted eventually soon).
    3. calls to symbolic_hint() -> replace_backed_with_hints()
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -693,8 +693,10 @@ class SizeVarAllocator:
             - fallback for NaN
             - None if no special handling needed
         """
-        if isinstance(expr, (int, sympy.Integer, float, sympy.Float, sympy.Rational)):
+        try:
             return int(expr)
+        except (TypeError, ValueError):
+            pass
 
         if isinstance(expr, Expr):
             if expr.has(sympy.I):
@@ -748,7 +750,6 @@ class SizeVarAllocator:
         # unbacked with optimizations hints if exists.
         expr = sympy_subs(expr, self.backed_var_to_val)
         expr = sympy_subs(expr, self.var_to_hint_override)
-        expr = sympy.expand(expr)
         result = self._maybe_realize_expr(expr, fallback)
         if result is not None:
             return result
