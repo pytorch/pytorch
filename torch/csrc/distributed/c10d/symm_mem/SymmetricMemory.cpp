04cc950b48212d4db5073ca077b62902ddabe2ec
Ke Wen <kwen@nvidia.com>
2026-02-14 00:31:07 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/symm_mem/SymmetricMemory.cpp
+++ torch/csrc/distributed/c10d/symm_mem/SymmetricMemory.cpp
@@ -533,7 +533,7 @@ TORCH_LIBRARY_FRAGMENT(symm_mem, m) {
       "nvshmem_broadcast(Tensor(a!) input, int root, str group_name) -> Tensor(a!)");
   m.def("nvshmem_wait_for_signal(Tensor sigpad, int signal, int peer) -> ()");
   m.def(
-      "nvshmem_put_with_signal(Tensor(a) tensor, Tensor(a) sigpad, int signal, int peer) -> ()");
+      "nvshmem_put_with_signal(Tensor inp, Tensor(a) out, Tensor(a) sigpad, int signal, int peer) -> ()");
   m.def("nccl_put(Tensor(a!) tensor, int peer) -> ()");
   m.def("nccl_get(Tensor(a!) tensor, int peer) -> ()");
   m.def("nccl_wait_for_signal(Tensor sigpad, int signal) -> ()");
