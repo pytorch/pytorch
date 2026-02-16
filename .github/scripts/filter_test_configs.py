a3c988d1e88bc7bf2c100825955cf3df9b50abe0
atalman <atalman@fb.com>
2026-02-16 09:29:06 -0800

    [RELEASE 2.11] Release only changes

--- .github/scripts/filter_test_configs.py
+++ .github/scripts/filter_test_configs.py
@@ -42,9 +42,9 @@ SUPPORTED_PERIODICAL_MODES: dict[str, Callable[[Optional[str]], bool]] = {
 }
 
 # The link to the published list of disabled jobs
-DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json"
+DISABLED_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/disabled-jobs.json?versionId=EdtXb8H1wC3KKKfSV9z7QtgG3FngDv3B"
 # and unstable jobs
-UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json"
+UNSTABLE_JOBS_URL = "https://ossci-metrics.s3.amazonaws.com/unstable-jobs.json?versionId=iafjJg17T2MK7wQiJ0qx32zIPMqqwZqv"
 
 # Some constants used to handle disabled and unstable jobs
 JOB_NAME_SEP = "/"
