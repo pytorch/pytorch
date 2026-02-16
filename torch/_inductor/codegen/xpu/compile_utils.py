9879ea695a149029ad29970bf43a18f32ca77151
xinan.lin <xinan.lin@intel.com>
2026-02-16 16:29:18 +0800

    [Inductor XPU GEMM] Step 10/N: Switch XPU triton scheduling to combined scheduling.
    ghstack-source-id: c471593859b12ecaabe8a8aeb90fa365091f9cf9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/161940

--- torch/_inductor/codegen/xpu/compile_utils.py
+++ torch/_inductor/codegen/xpu/compile_utils.py
@@ -1,19 +1,44 @@
 # mypy: allow-untyped-defs
 import logging
+import os
+import shutil
 from typing import Optional
 
 from torch._inductor import config
+from torch._inductor.codegen.xpu.xpu_env import get_xpu_arch
 from torch._inductor.utils import is_linux
 
 from ..cuda.compile_utils import _cutlass_include_paths
-from .xpu_env import get_xpu_arch
 
 
 log = logging.getLogger(__name__)
 
 
 def _sycl_compiler() -> Optional[str]:
-    return "icpx"
+    # Search order:
+    # 0) which icpx
+    # 1) config.xpu.oneapi_root
+    # 2) ONEAPI_ROOT environment variable
+    # 3) default system search PATH.
+    if shutil.which("icpx"):
+        return "icpx"
+
+    if os.path.exists(config.xpu.oneapi_root or ""):
+        oneapi_root = config.xpu.oneapi_root
+    elif os.path.exists(os.getenv("ONEAPI_ROOT") or ""):
+        oneapi_root = os.getenv("ONEAPI_ROOT")
+    else:
+        oneapi_root = None
+
+    if oneapi_root:
+        oneapi_inclue = os.path.join(oneapi_root, "include")
+        if "CPLUS_INCLUDE_PATH" in os.environ:
+            os.environ["CPLUS_INCLUDE_PATH"] += ":" + oneapi_inclue
+        else:
+            os.environ["CPLUS_INCLUDE_PATH"] = oneapi_inclue
+        return os.path.realpath(os.path.join(oneapi_root, "bin/icpx"))
+    else:
+        raise RuntimeError("Can not find Intel compiler.")
 
 
 def _sycl_lib_options() -> list[str]:
