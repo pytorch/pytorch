fb0f0f68407bc8bfb2c11dc42d2ce28b5f5920f0
Taeksang Kim <voidbag@gmail.com>
2026-02-18 13:10:43 +0000

    Merge Files for each dtypes
    Signed-off-by: Taeksang Kim <ts.kim@hyperaccel.ai>

--- aten/src/ATen/native/cuda/SortSmallBitonicSort-16.cu
+++
@@ -1,6 +0,0 @@
-#include <ATen/native/cuda/SortImpl.cuh>
-
-namespace at::native {
-  template void sortCommon<uint16_t>(SmallBitonicSort sorter, const TensorBase &key, const TensorBase &value,
-                int dim, bool descending);
-}
