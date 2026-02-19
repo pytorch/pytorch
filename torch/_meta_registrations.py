48b6522eba13e6ea460ef89269481b759023a8da
ahpoddar <ahpoddar@redhat.com>
2026-02-19 09:12:12 +0000

    fix: initial commit for fft stride

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -255,14 +255,9 @@ def _exec_fft(out, self, out_sizes, dim, *, forward):
     for d in dim:
         is_transformed_dim[d] = True
 
-    # std::partition
-    left, right = [], []
-    for d in dim_permute:
-        if not is_transformed_dim[d]:
-            left.append(d)
-        else:
-            right.append(d)
-    dim_permute = left + right
+    # std::partition + std::copy(dim.begin(), dim.end(), batch_end)
+    left = [d for d in dim_permute if not is_transformed_dim[d]]
+    dim_permute = left + list(dim)
     batch_end = len(left)
 
     self_strides = self.stride()
@@ -300,8 +295,9 @@ def _exec_fft(out, self, out_sizes, dim, *, forward):
 def _sort_dims(self: Tensor, dim: list[int], exclude_last: bool = False):
     sorted_dims = list(dim)
     self_strides = self.stride()
-    sorted_dims[: len(sorted_dims) - int(exclude_last)].sort(
-        key=lambda i: self_strides[i]
+    end = len(sorted_dims) - int(exclude_last)
+    sorted_dims[:end] = sorted(
+        sorted_dims[:end], key=lambda i: self_strides[i], reverse=True
     )
     return sorted_dims
 
@@ -314,6 +310,10 @@ def meta_fft_c2c(self, dim, normalization, forward):
     torch._check(self.dtype.is_complex)
     if not dim:
         return self.clone()
+
+    # PocketFFT writes into a contiguous empty tensor directly
+    if device_hint(self) == "cpu" and not torch._C.has_mkl:
+        return self.new_empty(self.size())
 
     sorted_dims = _sort_dims(self, dim)
     out = self.new_empty(self.size())
@@ -385,7 +385,16 @@ def meta_fft_r2c(self, dim, normalization, onesided):
 
         return output
 
+    elif torch._C.has_mkl:
+        # _fft_r2c_mkl in aten/src/ATen/native/mkl/SpectralOps.cpp
+        sorted_dims = _sort_dims(self, dim, exclude_last=True)
+        output = self.new_empty(
+            out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
+        )
+        return _exec_fft(output, self, out_sizes, sorted_dims, forward=True)
+
     else:
+        # PocketFFT writes into a contiguous empty tensor
         return self.new_empty(
             out_sizes, dtype=utils.corresponding_complex_dtype(self.dtype)
         )
@@ -488,7 +497,8 @@ def meta_fft_c2r(self: Tensor, dim: list[int], normalization: int, lastdim: int)
                 temp = self.clone(memory_format=torch.contiguous_format)
             return _exec_fft(output, temp, out_sizes, [dim[-1]], forward=False)
 
-    else:
+    elif torch._C.has_mkl:
+        # _fft_c2r_mkl in aten/src/ATen/native/mkl/SpectralOps.cpp
         input = self
         if len(dim) > 1:
             c2c_dims = dim[:-1]
@@ -499,6 +509,12 @@ def meta_fft_c2r(self: Tensor, dim: list[int], normalization: int, lastdim: int)
         out_sizes[dim[-1]] = lastdim
         out = self.new_empty(out_sizes, dtype=toRealValueType(self.dtype))
         return _exec_fft(out, input, out_sizes, dim, forward=False)
+
+    else:
+        # PocketFFT writes into a contiguous empty tensor
+        out_sizes = list(self.size())
+        out_sizes[dim[-1]] = lastdim
+        return self.new_empty(out_sizes, dtype=toRealValueType(self.dtype))
 
 
 @register_meta(aten.copy_.default)
