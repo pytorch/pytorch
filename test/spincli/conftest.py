cf3861bc04a54d3faed8f1ce3c746b477bf59fb1
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2026-02-19 14:01:17 +0000

    Integrate custom dir_switcher

--- test/spincli/conftest.py
+++ test/spincli/conftest.py
@@ -1,13 +1,21 @@
+import os
 import pathlib
 
 import pytest
 
-from spin.tests.conftest import dir_switcher
+def dir_switcher(path):
+    # Pre-test code
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    try:
+        yield
+    finally:
+        # Post test code
+        os.chdir(cwd)
 
 
 PYTORCH_ROOT = pathlib.Path(__file__).parent.parent.parent
-
-breakpoint()
 
 
 @pytest.fixture()
