4a6d5d89149667e7297ab22cb399180ce85105ce
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2026-02-23 18:40:05 +0100

    Add basic pyrefly infer command
    ghstack-source-id: 8be6ca834f855c7c375a3fd675c61d9e51baf9b6
    Pull-Request: https://github.com/pytorch/pytorch/pull/173647

---
+++ test/spincli/autotype_test_typed.py
@@ -0,0 +1,16 @@
+NUMERIC_CONSTANT = 42
+STRING_CONSTANT = "Alice"
+LIST_CONSTANT = [1, 2, 3, 4, 5]
+DICT_CONSTANT = {"key1": "value1", "key2": "value2"}
+TUPLE_CONSTANT = (1, 2, 3)
+SET_CONSTANT = {1, 2, 3, 4, 5}
+BOOLEAN_CONSTANT = True
+NONE_CONSTANT = None
+
+
+def greeter(name: int | str | None = None) -> str:
+    return f"Hello, {name}!"
+
+
+greeter(STRING_CONSTANT)
+greeter(NUMERIC_CONSTANT)
