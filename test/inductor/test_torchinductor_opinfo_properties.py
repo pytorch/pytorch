b772c33ddb4ef3f9f9f9254366ac625d2cc3776e
Markus Hoehnerbach <mhoehnerbach@meta.com>
2026-02-23 17:19:41 +0000

    disable on rocm

--- test/inductor/test_torchinductor_opinfo_properties.py
+++ test/inductor/test_torchinductor_opinfo_properties.py
@@ -57,6 +57,7 @@ from torch.testing._internal.common_utils import (
     parametrize,
     skipIfTorchDynamo,
     TEST_WITH_ASAN,
+    TEST_WITH_ROCM,
 )
 from torch.testing._internal.inductor_utils import HAS_GPU
 
@@ -481,6 +482,7 @@ def compile_fn(fn, backend):
 
 @unittest.skipIf(IS_WINDOWS, "Skipped on Windows")
 @unittest.skipIf(TEST_WITH_ASAN, "Skipped under ASAN")
+@unittest.skipIf(TEST_WITH_ROCM, "Skipped on ROCm")
 @unittest.skipIf(not HAS_GPU, "Requires GPU")
 class TestOpInfoProperties(TestCase):
     """Test op properties under various inductor modes using OpInfo on CUDA."""
