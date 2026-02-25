d1ee815e35b602790029edd1cdd710021000557e
iupaikov-amd <Iurii.Paikov@amd.com>
2026-02-25 20:16:11 +0100

    Skipped one more failing test for ROCm

--- test/dynamo/test_structured_trace.py
+++ test/dynamo/test_structured_trace.py
@@ -799,6 +799,7 @@ class StructuredTraceTest(TestCase):
 
         self.assertParses()
 
+    @skipIfRocm(msg="Fails with Triton 3.7")
     @requires_tlparse
     def test_graph_breaks(self):
         @torch.compile(backend="inductor")
