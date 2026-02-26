69dd7da261e8e1e73eeec873af1d4e895539923c
Laith Sakka <lsakka@meta.com>
2026-02-25 23:38:55 -0800

    Handle u0*12>u0 in _dynamic_reshape_indexer (#175232)
    partially address https://github.com/pytorch/pytorch/issues/175167 20% win.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/175232
    Approved by: https://github.com/jansel
    ghstack dependencies: #175220, #175221

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -37,7 +37,6 @@ import torch._export.serde.schema as export_schema
 import torch._library.utils as library_utils
 import torch._logging
 import torch.fx
-import torch.fx.experimental._config as _fx_config
 import torch.utils._pytree as pytree
 from torch._dynamo.utils import identity
 from torch._export.serde.serialize import GraphModuleSerializer
@@ -3363,7 +3362,7 @@ class View(GenericView):
     ) -> Callable[[Sequence[_T]], Sequence[_V]]:
         try:
             reindex = cls._dynamic_reshape_indexer(old_size, new_size, dense_dim)
-        except (AssertionError, IndexError):
+        except (AssertionError, GuardOnDataDependentSymNode, IndexError):
             # optimistic algorithm failed, lets do a fallback
             flat = [sympy_product(old_size)]
             reindex1 = cls._dynamic_reshape_indexer(old_size, flat)
@@ -3381,6 +3380,41 @@ class View(GenericView):
         Perform a reshape entirely by modifying indexing math
         """
         guard_or_false = V.graph.sizevars.guard_or_false
+
+        def compare_sizes(a: Expr, b: Expr) -> int:
+            """
+            Compare two symbolic sizes, returning -1 if a < b, 0 if a == b, 1 if a > b.
+
+            For unbacked symbols, guard_or_false returns False, so we fall back
+            to divisibility checks.
+            """
+            if guard_or_false(sympy.Eq(a, b)):
+                return 0
+            if guard_or_false(sympy.Lt(a, b)):
+                return -1
+            if guard_or_false(sympy.Gt(a, b)):
+                return 1
+
+            # Divisibility fallback for unbacked symbols:
+            # e.g. comparing u0 vs u0*u1, statically_known_multiple_of(u0*u1, u0)
+            # returns True so we take the merge path (return -1).
+            # The merge reindex for old=[u0, u1] -> new=[u0*u1] is:
+            #   for k in range(u0 * u1):
+            #     i = k // u1
+            #     j = k % u1
+            #     z[k] = x[i, j] + 1
+            # Two cases where this could seem wrong but is still safe:
+            #   u1=1: the merge reindex is correct since the extra dim is
+            #         size 1 (a no-op). e.g. k // 1 = k, k % 1 = 0.
+            #   u0=0: loop is range(0), no kernel runs, result doesn't matter.
+
+            if V.graph.sizevars.statically_known_multiple_of(b, a):
+                return -1
+            if V.graph.sizevars.statically_known_multiple_of(a, b):
+                return 1
+
+            raise GuardOnDataDependentSymNode(sympy.Eq(a, b))
+
         # TODO: These symbols may not escape, if they don't assert so and
         # treat them as temporary
         vars = [
@@ -3410,37 +3444,27 @@ class View(GenericView):
                 stack_new.append((var, size_new))  # re-add
             elif size_new == 1:
                 stack_old.append(size_old)  # re-add
-            elif guard_or_false(sympy.Eq(size_new, size_old)):
+            elif compare_sizes(size_new, size_old) == 0:
                 view_expr.append(var)
-            elif guard_or_false(sympy.Lt(size_new, size_old)):
-                while guard_or_false(sympy.Lt(size_new, size_old)):
+            elif compare_sizes(size_new, size_old) < 0:
+                while compare_sizes(size_new, size_old) < 0:
                     var2, size_new2 = stack_new.pop()
                     var = var2 * size_new + var
                     size_new = size_new * size_new2
                 view_expr.append(var)
                 V.graph.sizevars.check_equals(size_new, size_old)
-            elif guard_or_false(sympy.Gt(size_new, size_old)):
+            elif compare_sizes(size_new, size_old) > 0:
                 divisor = sympy.S.One
                 modulus = size_old
                 view_expr.append(ModularIndexing(var, divisor, modulus))
                 divisor = divisor * modulus
-                while guard_or_false(sympy.Gt(size_new, size_old)):
+                while compare_sizes(size_new, size_old) > 0:
                     modulus = stack_old.pop()
                     view_expr.append(ModularIndexing(var, divisor, modulus))
                     divisor = divisor * modulus
                     size_old = size_old * modulus
                 V.graph.sizevars.check_equals(size_new, size_old)
             else:
-                # With backed symbols, one of Eq/Lt/Gt must be true,
-                # so reaching here is a bug. With unbacked symbols
-                # (or backed_size_oblivious mode), guard_or_false
-                # can't resolve all comparisons.
-                if (
-                    free_unbacked_symbols(size_old)
-                    or free_unbacked_symbols(size_new)
-                    or _fx_config.backed_size_oblivious
-                ):
-                    raise GuardOnDataDependentSymNode(sympy.Eq(size_new, size_old))
                 raise AssertionError
 
         while stack_old:
