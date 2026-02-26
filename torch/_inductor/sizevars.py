ad358adb1ef415b7edb1bf16ad4c305d6c9b9844
Laith Sakka <lsakka@meta.com>
2026-02-25 23:43:34 -0800

    Update base for Update on "Enable reduction splitting for explicitly hinted unbacked symbols"
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- torch/_inductor/sizevars.py
+++ torch/_inductor/sizevars.py
@@ -635,7 +635,7 @@ class SizeVarAllocator:
         expr = self.remove_precomputed_replacements(expr)
 
         try:
-            hint = self.guarding_hint_or_throw(expr)
+            hint = self.size_hint(expr)
         except Exception:
             hint = None
         node = SymNode(expr, self.shape_env, int, hint)
@@ -646,6 +646,20 @@ class SizeVarAllocator:
     ) -> list[Union[SymInt, int]]:
         """Convert a sequence of sympy expressions to SymInts, or return ints as is."""
         return [self.to_symint_or_int(e) for e in exprs]
+
+    # TODO this will be deprecated.
+    def size_hint(self, expr: Union[Expr, int]) -> int:
+        if isinstance(expr, SymInt):
+            raise TypeError(
+                "wrong API usage!, use size_hint from torch.fx.experimental.symbolic_shapes or pass sympy expressions instead"
+            )
+
+        out = self.replace_backed_symbols_with_hints(expr)
+        try:
+            return int(out)
+        except Exception:
+            log.debug("failed on: %s", out)
+            raise
 
     def guarding_hint_or_throw(self, expr: Union[Expr, int]) -> int:
         """
@@ -773,6 +787,12 @@ class SizeVarAllocator:
         return tuple(
             self.optimization_hint_with_override(e, hint_override) for e in exprs
         )
+
+    def size_hints(
+        self,
+        exprs: Iterable[Union[Expr, int]],
+    ) -> tuple[int, ...]:
+        return tuple(self.size_hint(x) for x in exprs)
 
     def guarding_hints_or_throw(
         self,
