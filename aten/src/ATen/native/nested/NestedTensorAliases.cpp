28c047f2d50393d381b499ad5b4675b34d9b22e5
Sumit Binnani <sumit.binnani@gmail.com>
2026-02-27 15:43:00 -0800

    WIP

--- aten/src/ATen/native/nested/NestedTensorAliases.cpp
+++ aten/src/ATen/native/nested/NestedTensorAliases.cpp
@@ -6,7 +6,7 @@ namespace at::native {
 // alias for to_padded_tensor in nested namespace
 Tensor nested_to_padded_tensor(
     const Tensor& t,
-    double padding,
+    const Scalar& padding,
     OptionalIntArrayRef output_size) {
     return t.to_padded_tensor(padding, output_size);
 }
