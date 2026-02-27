ec46712c3874ab6a6d78ead0fe03d73ae1a89817
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-02-27 07:23:00 +0000

    Revert "Fixes tracing generator iteration crash (#175769)"
    This reverts commit c5ca4f7a2de49351696cb2a7bcfc125ba2e3a379.
    
    Reverted https://github.com/pytorch/pytorch/pull/175769 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/175769#issuecomment-3971270723))

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -4711,7 +4711,6 @@ def is_tensor_base_attr_getter(value: Any) -> bool:
     return (
         isinstance(value, types.MethodWrapperType)
         and value.__name__ == "__get__"
-        and hasattr(value.__self__, "__objclass__")
         and value.__self__.__objclass__ is torch._C._TensorBase  # type: ignore[attr-defined]
     )
 
