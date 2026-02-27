3faf2e2ac6631bcb238549854864fc8a911f080b
Paul Zhang <paulzhan@meta.com>
2026-02-27 03:30:30 +0000

    [Inductor] Stricter heuristics for epilogue fusion to prevent unprofitable fusions (#175773)
    Differential Revision: D94115251
    
    Previously, the heuristics for whether or not to do epilogue fusion mainly had to do with whether or not register spillage occurred. This still led to a lot of fusions that generally were not profitable. The new heuristics consider occupancy differences between the old and new kernel, along with whether or not the epilogue time dominates the template, which captures more optimal decisions.
    
    Testing was done on 2 internal models, where the decision was essentially equivalent to benchmarking epilogues for all template fusions.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/175773
    Approved by: https://github.com/eellison

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -2389,6 +2389,10 @@ class ExternKernelChoice:
 
 
 class TritonTemplateCaller(ir.TritonTemplateCallerBase):
+    """
+    Represents a ChoiceCaller for a TritonTemplate
+    """
+
     def __init__(
         self,
         name,
@@ -2425,6 +2429,8 @@ class TritonTemplateCaller(ir.TritonTemplateCallerBase):
         )
         self.hint_override = hint_override
 
+        self.n_regs = None
+
     def benchmark(self, *args, out):
         assert self.bmreq is not None
         if (
@@ -2439,6 +2445,8 @@ class TritonTemplateCaller(ir.TritonTemplateCallerBase):
     def precompile(self):
         assert self.bmreq is not None
         self.bmreq.precompile()
+
+        self.n_regs = self.bmreq.n_regs
 
     def __str__(self) -> str:
         return f"TritonTemplateCaller({self.bmreq.module_path}, {self.description})"
