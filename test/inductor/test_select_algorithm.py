b483b0ac0f4d2f982106883ed281a6ccb7d8f319
Yu, Guangye <guangye.yu@intel.com>
2026-02-28 13:38:55 +0000

    Update
    [ghstack-poisoned]

--- test/inductor/test_select_algorithm.py
+++ test/inductor/test_select_algorithm.py
@@ -293,7 +293,7 @@ class TestSelectAlgorithm(TestCase):
             self.assertEqual(counters["inductor"]["select_algorithm_autotune"], 1)
 
     @pytest.mark.xfail(
-        condition=not torch.version.hip,
+        condition=torch.version.cuda,
         reason="C++ wrapper dynamic shapes fails on CUDA, fixed on ROCm",
     )
     @patches
