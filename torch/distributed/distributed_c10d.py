d6c4fb5ac180a82d4857096a774fb6224efced24
HDCharles <charlesdavidhernandez@gmail.com>
2026-02-27 19:49:10 -0500

    add check for sm90
    Summary
    
    Signed-off-by: HDCharles <charlesdavidhernandez@gmail.com>

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -2992,10 +2992,12 @@ def broadcast(
     opts.rootRank = group_src
     opts.rootTensor = 0
     opts.asyncOp = async_op
-    no_native_fp8_support = torch.cuda.get_device_capability(tensor.device)[0] < 9
+    sm90_or_more = not (
+        tensor.is_cuda and torch.cuda.get_device_capability(tensor.device)[0] >= 9
+    )
     if tensor.is_complex():
         tensor = torch.view_as_real(tensor)
-    elif _is_fp8(tensor) and no_native_fp8_support:
+    elif _is_fp8(tensor) and not sm90_or_more:
         # FP8 is supported by NCCL on sm90+, use workaround for older GPUs
         tensor = tensor.view(torch.uint8)
     work = group.broadcast([tensor], opts)
