0a06050b14a6f8c68715f91a995bbb2190f7be7e
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-01 19:20:43 +0000

    Revert "[fx] Move _FindNodesLookupTable and _node_list to C++ (#170973)"
    This reverts commit 6a78d3dff70663c6fdcb519a2d943a3f3311cb14.
    
    Reverted https://github.com/pytorch/pytorch/pull/170973 on behalf of https://github.com/facebook-github-bot due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/170973#issuecomment-3980822092))

--- torch/_functorch/compile_utils.py
+++ torch/_functorch/compile_utils.py
@@ -219,7 +219,7 @@ def strip_overloads(gm: fx.GraphModule) -> None:
     gm.recompile()
 
 
-def get_placeholders(graph: fx.Graph) -> list[fx.Node]:
+def get_placeholders(graph: fx.Graph) -> fx.graph._node_list:
     return graph.find_nodes(op="placeholder")
 
 
