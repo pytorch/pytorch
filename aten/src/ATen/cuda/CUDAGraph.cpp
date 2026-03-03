bde10e9ac0d86eb196e882cf6d22545804b11912
Chinmay Kuchinad <ChinmayDattanand.Kuchinad@amd.com>
2026-03-03 02:45:53 +0000

    Avoid watchdog hipEventQuery during active graph capture

--- aten/src/ATen/cuda/CUDAGraph.cpp
+++ aten/src/ATen/cuda/CUDAGraph.cpp
@@ -12,6 +12,13 @@ namespace at::cuda {
 
 static bool _cuda_graphs_debug = false;
 
+#if defined(USE_ROCM)
+bool is_graph_capture_active() {
+  std::unique_lock<std::mutex> lock(_currently_capturing_graphs_mutex);
+  return !_currently_capturing_graphs.empty();
+}
+#endif
+
 MempoolId_t graph_pool_handle() {
   // Sets just the second value, to distinguish it from MempoolId_ts created from
   // cudaStreamGetCaptureInfo id_s in capture_begin.
@@ -129,7 +136,9 @@ void CUDAGraph::capture_end() {
   TORCH_CHECK(stream.stream() == capture_stream_.stream(),
               "Capture must end on the same stream it began on.");
 
-  AT_CUDA_CHECK(cudaStreamEndCapture(capture_stream_, &graph_));
+  cudaError_t endCaptureErr = cudaStreamEndCapture(capture_stream_, &graph_);
+
+  AT_CUDA_CHECK(endCaptureErr);
 
   c10::cuda::CUDACachingAllocator::endAllocateToPool(capture_dev_, mempool_id_);
   at::getHostAllocator(at::kCUDA)->end_allocate_to_pool(mempool_id_);
