9730fc360ffcec93a4d029daf2d61b5dc8256336
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-03-08 11:56:47 -0700

    [BE][Ez][codemod]: Add a few more moves to attention move and func manual

--- aten/src/ATen/native/transformers/cuda/attention.cu
+++ aten/src/ATen/native/transformers/cuda/attention.cu
@@ -568,7 +568,7 @@ __host__ std::tuple<Tensor, Tensor, Tensor> transform_bias_rescale_qkv_cuda(
 #undef CALL_KERNEL
   auto q_k_v_s =
       at::native::split(q_k_v.view({3 * B, num_head, T, dim_per_head}), B, 0);
-  return std::make_tuple(q_k_v_s[0], q_k_v_s[1], q_k_v_s[2]);
+  return std::make_tuple(std::move(q_k_v_s[0]), std::move(q_k_v_s[1]), std::move(q_k_v_s[2]));
 }
 
 std::tuple<Tensor, Tensor> native_multi_head_attention_cuda(
