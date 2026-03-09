6823d02a31042baa3bdb7e386a2c98e9716b28ce
Eddie Yan <eddiey@nvidia.com>
2026-03-09 11:32:06 -0700

    lint

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1077,9 +1077,7 @@ print(t.is_pinned())
 
     def test_cudnn_allow_reduced_precision_reduction_get_set(self):
         orig = torch._C._get_cudnn_allow_reduced_precision_reduction()
-        self.assertEqual(
-            torch.backends.cudnn.allow_reduced_precision_reduction, orig
-        )
+        self.assertEqual(torch.backends.cudnn.allow_reduced_precision_reduction, orig)
         with torch.backends.cudnn.flags(
             enabled=None,
             benchmark=None,
@@ -1087,9 +1085,7 @@ print(t.is_pinned())
             allow_tf32=None,
             allow_reduced_precision_reduction=False,
         ):
-            self.assertFalse(
-                torch.backends.cudnn.allow_reduced_precision_reduction
-            )
+            self.assertFalse(torch.backends.cudnn.allow_reduced_precision_reduction)
         with torch.backends.cudnn.flags(
             enabled=None,
             benchmark=None,
@@ -1097,9 +1093,7 @@ print(t.is_pinned())
             allow_tf32=None,
             allow_reduced_precision_reduction=True,
         ):
-            self.assertTrue(
-                torch.backends.cudnn.allow_reduced_precision_reduction
-            )
+            self.assertTrue(torch.backends.cudnn.allow_reduced_precision_reduction)
 
     @recover_orig_fp32_precision
     def test_fp32_precision_with_tf32(self):
