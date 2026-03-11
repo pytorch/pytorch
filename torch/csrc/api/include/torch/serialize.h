6766ce485319d1b4c1886494b7fda44cf6d84bab
chani0343 <c9992946@gmail.com>
2026-03-11 13:00:13 +0200

    Add save_state_dict and load_state_dict functions for C++ API
    - Add save_state_dict() template function to save module parameters and buffers
    - Add load_state_dict() template function to load parameters and buffers into module
    - Fixes #150857: Enable cross-compatibility between Python and C++ for model serialization

--- torch/csrc/api/include/torch/serialize.h
+++ torch/csrc/api/include/torch/serialize.h
@@ -10,33 +10,6 @@
 namespace torch {
 
 /// Serializes the given `value`.
-/// There must be an overload of `operator<<` between `serialize::OutputArchive`
-/// and `Value` for this method to be well-formed. Currently, such an overload
-/// is provided for (subclasses of):
-///
-/// - `torch::nn::Module`,
-/// - `torch::optim::Optimizer`
-/// - `torch::Tensor`
-///
-/// To perform the serialization, a `serialize::OutputArchive` is constructed,
-/// and all arguments after the `value` are forwarded to its `save_to` method.
-/// For example, you can pass a filename, or an `ostream`.
-///
-/// \rst
-/// .. code-block:: cpp
-///
-///   torch::nn::Linear model(3, 4);
-///   torch::save(model, "model.pt");
-///
-///   torch::optim::SGD sgd(model->parameters(), 0.9); // 0.9 is learning rate
-///   std::ostringstream stream;
-///   // Note that the same stream cannot be used in multiple torch::save(...)
-///   // invocations, otherwise the header will be corrupted.
-///   torch::save(sgd, stream);
-///
-///   auto tensor = torch::ones({3, 4});
-///   torch::save(tensor, "my_tensor.pt");
-/// \endrst
 template <typename Value, typename... SaveToArgs>
 void save(const Value& value, SaveToArgs&&... args) {
   serialize::OutputArchive archive(std::make_shared<jit::CompilationUnit>());
@@ -45,23 +18,6 @@ void save(const Value& value, SaveToArgs&&... args) {
 }
 
 /// Serializes the given `tensor_vec` of type `std::vector<torch::Tensor>`.
-///
-/// To perform the serialization, a `serialize::OutputArchive` is constructed,
-/// and all arguments after the `tensor_vec` are forwarded to its `save_to`
-/// method. For example, you can pass a filename, or an `ostream`.
-///
-/// \rst
-/// .. code-block:: cpp
-///
-///   std::vector<torch::Tensor> tensor_vec = { torch::randn({1, 2}),
-///   torch::randn({3, 4}) }; torch::save(tensor_vec, "my_tensor_vec.pt");
-///
-///   std::vector<torch::Tensor> tensor_vec = { torch::randn({5, 6}),
-///   torch::randn({7, 8}) }; std::ostringstream stream;
-///   // Note that the same stream cannot be used in multiple torch::save(...)
-///   // invocations, otherwise the header will be corrupted.
-///   torch::save(tensor_vec, stream);
-/// \endrst
 template <typename... SaveToArgs>
 void save(const std::vector<torch::Tensor>& tensor_vec, SaveToArgs&&... args) {
   serialize::OutputArchive archive(std::make_shared<jit::CompilationUnit>());
@@ -72,35 +28,28 @@ void save(const std::vector<torch::Tensor>& tensor_vec, SaveToArgs&&... args) {
   archive.save_to(std::forward<SaveToArgs>(args)...);
 }
 
+/// Saves the state dictionary (parameters and buffers) of a module.
+template <typename Module, typename... SaveToArgs>
+void save_state_dict(const Module& module, SaveToArgs&&... args) {
+  serialize::OutputArchive archive(std::make_shared<jit::CompilationUnit>());
+  
+  // Save parameters
+  for (const auto& param : module->named_parameters()) {
+    archive.write(param.key(), param.value());
+  }
+  
+  // Save buffers
+  for (const auto& buffer : module->named_buffers()) {
+    archive.write(buffer.key(), buffer.value());
+  }
+  
+  archive.save_to(std::forward<SaveToArgs>(args)...);
+}
+
 TORCH_API std::vector<char> pickle_save(const torch::IValue& ivalue);
 TORCH_API torch::IValue pickle_load(const std::vector<char>& data);
 
 /// Deserializes the given `value`.
-/// There must be an overload of `operator>>` between `serialize::InputArchive`
-/// and `Value` for this method to be well-formed. Currently, such an overload
-/// is provided for (subclasses of):
-///
-/// - `torch::nn::Module`,
-/// - `torch::optim::Optimizer`
-/// - `torch::Tensor`
-///
-/// To perform the serialization, a `serialize::InputArchive` is constructed,
-/// and all arguments after the `value` are forwarded to its `load_from` method.
-/// For example, you can pass a filename, or an `istream`.
-///
-/// \rst
-/// .. code-block:: cpp
-///
-///   torch::nn::Linear model(3, 4);
-///   torch::load(model, "model.pt");
-///
-///   torch::optim::SGD sgd(model->parameters(), 0.9); // 0.9 is learning rate
-///   std::istringstream stream("...");
-///   torch::load(sgd, stream);
-///
-///   auto tensor = torch::ones({3, 4});
-///   torch::load(tensor, "my_tensor.pt");
-/// \endrst
 template <typename Value, typename... LoadFromArgs>
 void load(Value& value, LoadFromArgs&&... args) {
   serialize::InputArchive archive;
@@ -109,30 +58,11 @@ void load(Value& value, LoadFromArgs&&... args) {
 }
 
 /// Deserializes the given `tensor_vec` of type `std::vector<torch::Tensor>`.
-///
-/// To perform the serialization, a `serialize::InputArchive` is constructed,
-/// and all arguments after the `value` are forwarded to its `load_from` method.
-/// For example, you can pass a filename, or an `istream`.
-///
-/// \rst
-/// .. code-block:: cpp
-///
-///   std::vector<torch::Tensor> tensor_vec;
-///   torch::load(tensor_vec, "my_tensor_vec.pt");
-///
-///   std::vector<torch::Tensor> tensor_vec;
-///   std::istringstream stream("...");
-///   torch::load(tensor_vec, stream);
-/// \endrst
 template <typename... LoadFromArgs>
 void load(std::vector<torch::Tensor>& tensor_vec, LoadFromArgs&&... args) {
   serialize::InputArchive archive;
   archive.load_from(std::forward<LoadFromArgs>(args)...);
 
-  // NOTE: The number of elements in the serialized `std::vector<torch::Tensor>`
-  // is not known ahead of time, so we need a while-loop to increment the index,
-  // and use `archive.try_read(...)` to check whether we have reached the end of
-  // the serialized `std::vector<torch::Tensor>`.
   size_t index = 0;
   torch::Tensor value;
   while (archive.try_read(std::to_string(index), value)) {
@@ -141,4 +71,28 @@ void load(std::vector<torch::Tensor>& tensor_vec, LoadFromArgs&&... args) {
     index++;
   }
 }
+
+/// Loads the state dictionary (parameters and buffers) into a module.
+template <typename Module, typename... LoadFromArgs>
+void load_state_dict(Module& module, LoadFromArgs&&... args) {
+  serialize::InputArchive archive;
+  archive.load_from(std::forward<LoadFromArgs>(args)...);
+  
+  // Load parameters
+  for (auto& param : module->named_parameters()) {
+    torch::Tensor loaded_param;
+    if (archive.try_read(param.key(), loaded_param)) {
+      param.value().copy_(loaded_param);
+    }
+  }
+  
+  // Load buffers
+  for (auto& buffer : module->named_buffers()) {
+    torch::Tensor loaded_buffer;
+    if (archive.try_read(buffer.key(), loaded_buffer)) {
+      buffer.value().copy_(loaded_buffer);
+    }
+  }
+}
+
 } // namespace torch
