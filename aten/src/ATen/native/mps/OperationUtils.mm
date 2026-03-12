4786164c4175d96951c3470b2656a66fa1fc3030
imperatormk <darko.simonovski@hotmail.com>
2026-03-12 18:16:06 +0100

    Fix clang-format lint

--- aten/src/ATen/native/mps/OperationUtils.mm
+++ aten/src/ATen/native/mps/OperationUtils.mm
@@ -1344,14 +1344,12 @@ PrecompiledMetalShaderLibrary::PrecompiledMetalShaderLibrary(const std::vector<u
     : MetalShaderLibrary("") {
   auto device = MPSDevice::getInstance()->device();
   NSError* error = nil;
-  dispatch_data_t dd = dispatch_data_create(
-      data.data(), data.size(),
-      dispatch_get_main_queue(),
-      DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+  dispatch_data_t dd =
+      dispatch_data_create(data.data(), data.size(), dispatch_get_main_queue(), DISPATCH_DATA_DESTRUCTOR_DEFAULT);
   library = [device newLibraryWithData:dd error:&error];
   dispatch_release(dd);
-  TORCH_CHECK(library, "Failed to load metallib: ",
-              error ? [[error description] UTF8String] : "unknown error");
+  TORCH_CHECK(
+      library, "Failed to load metallib: ", error ? [[error description] UTF8String] : "unknown error");
 }
 
 PrecompiledMetalShaderLibrary::~PrecompiledMetalShaderLibrary() {
