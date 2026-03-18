119e10864a37269f5f726b75715380e368e475a4
Chris Leonard <chleonar@redhat.com>
2026-03-18 17:52:07 +0000

    Very minor cleanup.

--- aten/src/ATen/native/sparse/cuda/cuSPARSELtOps.cpp
+++ aten/src/ATen/native/sparse/cuda/cuSPARSELtOps.cpp
@@ -668,6 +668,7 @@ std::tuple<at::Tensor, int64_t, int64_t, int64_t, int64_t> _cslt_sparse_mm_impl(
   int64_t n = dense_B.size(1);
   int64_t m = compressed_A.size(0);
 
+  // set up matmul descriptors and cache them or retrieve from cache.
   MatDescriptorPtrs* desc_ptrs = getMatDescriptorCache().getOrCreate(
       handle,
       m,
@@ -699,9 +700,6 @@ std::tuple<at::Tensor, int64_t, int64_t, int64_t, int64_t> _cslt_sparse_mm_impl(
         sizeof(dBias)));
   }
 
-  cusparseLtSplitKMode_t splitKMode;
-  int max_alg_id;
-
   // set tensor_alpha_mode and alpha pointer for matmul
   const auto alpha_tensor = alpha_opt.has_value() ? *alpha_opt : Tensor{};
   auto alpha_ptr = &alpha;
@@ -724,7 +722,6 @@ std::tuple<at::Tensor, int64_t, int64_t, int64_t, int64_t> _cslt_sparse_mm_impl(
   TORCH_CUDASPARSE_CHECK(
       cusparseLtMatmulGetWorkspace(&handle, desc_ptrs->plan, &workspace_size));
 
-
   auto& allocator = *::c10::cuda::CUDACachingAllocator::get();
   auto workspacePtr = allocator.allocate(workspace_size);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
@@ -734,6 +731,10 @@ std::tuple<at::Tensor, int64_t, int64_t, int64_t, int64_t> _cslt_sparse_mm_impl(
   c10::TensorOptions().dtype(out_dtype).device(dense_B.device());
   at::Tensor res = (transpose_result) ? at::empty({n, m}, res_tensor_options)
                                     : at::empty({m, n}, res_tensor_options);
+
+  // these are not used unless search_alg_id == true.
+  cusparseLtSplitKMode_t splitKMode = (cusparseLtSplitKMode_t)0;
+  int max_alg_id = 0;
 
   if (search_alg_id) {
     // run matmul search
@@ -800,9 +801,12 @@ std::tuple<at::Tensor, int64_t, int64_t, int64_t, int64_t> _cslt_sparse_mm_impl(
         1));
   }
 
-  // plan and descriptors are cached; no destroy here
-
-  return {res, alg_id, split_k, static_cast<int64_t>(splitKMode), max_alg_id};
+  return {
+      res,
+      alg_id,
+      split_k,
+      static_cast<int64_t>(splitKMode),
+      max_alg_id};
 }
 
 at::Tensor _cslt_sparse_mm(
