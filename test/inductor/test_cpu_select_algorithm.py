a0b3a8d638f328c9789b17c8dffc949ab2a8fc33
Alexander Grund <alexander.grund@tu-dresden.de>
2026-03-18 08:45:10 +0100

    Skip test_int8_woq_mm without MKLDNN
    The tests test_int8_woq_mm(_concat) require MKLDNN or the
    cpp_templated_kernel_counter won't be incremented.

--- test/inductor/test_cpu_select_algorithm.py
+++ test/inductor/test_cpu_select_algorithm.py
@@ -32,6 +32,7 @@ from torch.testing._internal.common_utils import (
     IS_WINDOWS,
     parametrize,
     requires_mkl,
+    requires_onednn,
     TEST_MKL,
     xfailIf,
 )
@@ -1620,6 +1621,7 @@ class TestSelectAlgorithm(BaseTestSelectAlgorithm):
     @inductor_config.patch({"freezing": True})
     @patches
     @torch.no_grad
+    @requires_onednn
     @dtypes(torch.bfloat16)
     @parametrize(
         "batch_size",
@@ -1686,6 +1688,7 @@ class TestSelectAlgorithm(BaseTestSelectAlgorithm):
     @inductor_config.patch({"freezing": True, "cpp.enable_concat_linear": True})
     @patches
     @torch.no_grad
+    @requires_onednn
     @dtypes(torch.bfloat16)
     @parametrize(
         "batch_size",
