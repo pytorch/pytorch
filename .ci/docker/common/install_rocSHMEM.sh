685e56834fbab2e224033bb777d7eff30f7ed0fb
Prachi Gupta <prachi.gupta@amd.com>
2026-03-20 01:13:17 +0000

    Fix docker build failures in CI

--- .ci/docker/common/install_rocSHMEM.sh
+++ .ci/docker/common/install_rocSHMEM.sh
@@ -4,8 +4,14 @@
 set -eou pipefail
 
 function do_install() {
-    ROCSHMEM_VERSION=07517b17db9581296ee6b161612b894ae56b8341
-    rocm_dir="${ROCM_HOME:-/opt/rocm}"
+    ROCSHMEM_VERSION=7a14c59a186016cf8cdebf5afc706f5ede18c922
+    rocm_dir="${ROCM_HOME:-}"
+    if [[ -z "${rocm_dir}" && -f /etc/rocm_env.sh ]]; then
+        source /etc/rocm_env.sh
+        rocm_dir="${ROCM_HOME:-}"
+    fi
+    rocm_dir="${rocm_dir:-/opt/rocm}"
+    echo "install_rocSHMEM.sh: using ROCM install prefix ${rocm_dir}"
     (
         set -x
         curr_dir=$(pwd)
@@ -21,7 +27,7 @@ function do_install() {
         cd ${tmp_dir}/rocm-systems/projects/rocshmem
         mkdir build
         cd build
-        INSTALL_PREFIX=${rocm_dir} ../scripts/build_configs/all_backends
+        INSTALL_PREFIX="${rocm_dir}" ../scripts/build_configs/all_backends
 
         cd ${curr_dir}
 
