706be94e2bab177c4c93eb33a209de645082041a
Corbin Robeck <robeck@meta.com>
2026-03-22 10:08:53 -0400

    lint

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -253,9 +253,8 @@ class TestMaxAutotune(TestCase):
 
         torch.testing.assert_close(c_actual, c_expected, atol=1e-2, rtol=1e-2)
 
-    @unittest.skipIf(not torch.version.hip, 
-        "Non-TMA persistent MM only tested on AMD"
-    )
+    @unittest.skipIf(not torch.version.hip,
+        "Non-TMA persistent MM only tested on AMD")
     @parametrize("a_transposed", (False, True))
     @parametrize("b_transposed", (False, True))
     @parametrize("dynamic", (False, True))
