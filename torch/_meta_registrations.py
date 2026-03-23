8b436cc5c1a28ea22e6ab2c859849991ea22ca45
chani0343 <c9992946@gmail.com>
2026-03-23 12:00:37 +0200

    Remove duplicate meta_topk_values function and add out_wrapper to topk_meta

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -7455,6 +7455,7 @@ def scalar_tensor(s, dtype=None, layout=None, device=None, pin_memory=None):
 
 
 @register_meta([aten.topk.default, aten.topk.values])
+@out_wrapper("values", "indices")
 def topk_meta(self, k, dim=-1, largest=True, sorted=True):
     # From aten/src/ATen/native/Sorting.cpp
     dim = maybe_wrap_dim(dim, self.dim(), wrap_scalar=True)
@@ -7466,21 +7467,6 @@ def topk_meta(self, k, dim=-1, largest=True, sorted=True):
     if len(topKSize) > 0:
         topKSize[dim] = k
     return self.new_empty(topKSize), self.new_empty(topKSize, dtype=torch.int64)
-
-
-@register_meta([aten.topk.values])
-@out_wrapper("values", "indices")
-def meta_topk_values(self, k, dim=-1, largest=True, sorted=True):
-    dim = maybe_wrap_dim(dim, self.dim(), wrap_scalar=True)
-    sliceSize = 1 if self.dim() == 0 else self.size(dim)
-    torch._check(k >= 0)
-    torch._check(k <= sliceSize, lambda: "k not in range for dimension")
-
-    topKSize = list(self.shape)
-    if len(topKSize) > 0:
-        topKSize[dim] = k
-    return self.new_empty(topKSize), self.new_empty(topKSize, dtype=torch.int64)
-
 
 @register_meta(aten._segment_reduce_backward)
 @out_wrapper()
