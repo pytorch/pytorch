c66bb7a8f41f58e65ad9cc91e5f4ad502a5d09bf
cyy <cyyever@outlook.com>
2026-03-24 08:54:14 +0800

    Simplify AOTIModelPackageLoader::load_constants
    Signed-off-by: cyy <cyyever@outlook.com>

--- torch/csrc/inductor/aoti_package/model_package_loader.cpp
+++ torch/csrc/inductor/aoti_package/model_package_loader.cpp
@@ -1001,14 +1001,15 @@ void AOTIModelPackageLoader::load_constants(
   std::unordered_map<std::string, std::string> fqn_to_constant_name;
   fqn_to_constant_name.reserve(constant_name_to_fqn.size());
   for (const auto& [name, fqn] : constant_name_to_fqn) {
-    fqn_to_constant_name.emplace(fqn, name);
+    fqn_to_constant_name.try_emplace(fqn, name);
   }
 
   std::unordered_map<std::string, at::Tensor> updated_constants_map;
   updated_constants_map.reserve(constants_map.size());
   for (const auto& [fqn, tensor] : constants_map) {
-    if (fqn_to_constant_name.find(fqn) != fqn_to_constant_name.end()) {
-      updated_constants_map.emplace(fqn_to_constant_name[fqn], tensor);
+    auto it = fqn_to_constant_name.find(fqn);
+    if (it != fqn_to_constant_name.end()) {
+      updated_constants_map.emplace(it->second, tensor);
     } else {
       TORCH_CHECK(false, "Constant not found: ", fqn);
     }
