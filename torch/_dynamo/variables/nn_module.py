5ab1f7329723a1b5767eda49ce412cd62b527a6c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-25 17:10:58 +0000

    Revert "[Dynamo] Represent `__dict__` as `DunderDictVt` in `NNModuleVariable` (#177808)"
    This reverts commit cc9714f821d5bbca394aaf52c0de96e900c09914.
    
    Reverted https://github.com/pytorch/pytorch/pull/177808 on behalf of https://github.com/georgehong due to Revert this stack due to observing first-time test breakages: FAILED CONSISTENTLY: test/test_multiprocessing_spawn.py::ParallelForkServerShouldWorkTest::test_terminate_signal -- torch._dynamo.exc.InternalTorchDynamoError: AttributeError: 'method' object has no attribute '__dict__' ([comment](https://github.com/pytorch/pytorch/pull/176773#issuecomment-4128280508))

--- torch/_dynamo/variables/nn_module.py
+++ torch/_dynamo/variables/nn_module.py
@@ -80,7 +80,6 @@ if TYPE_CHECKING:
     from torch._dynamo.symbolic_convert import InstructionTranslator
 
     from .constant import ConstantVariable
-    from .dicts import DunderDictVariable
 
 
 def initialize_lazy_module(
@@ -205,11 +204,6 @@ class NNModuleVariable(VariableTracker):
         self.source: Source = self.source
         self.nn_module_stack_source = self.source
 
-    def get_dict_vt(self, tx: "InstructionTranslator") -> "DunderDictVariable":
-        if not hasattr(self, "dict_vt"):
-            self.dict_vt = variables.DunderDictVariable.create(tx, self)
-        return self.dict_vt
-
     def get_nn_module_stack_source(self) -> Source:
         res = self.nn_module_stack_source or self.source
         assert res
@@ -287,6 +281,18 @@ class NNModuleVariable(VariableTracker):
         if tx.f_code.co_name != "__init__":
             GenerationTracker.mark_class_dynamic(type(mod))
         raise UnspecializeRestartAnalysis
+
+    def has_key_in_generic_dict(self, tx: "InstructionTranslator", key: str) -> bool:
+        base = tx.output.get_submodule(self.module_key)
+
+        if tx.output.side_effects.has_pending_mutation_of_attr(self, key):
+            mutated_attr = tx.output.side_effects.load_attr(self, key, deleted_ok=True)
+            return not isinstance(mutated_attr, variables.DeletedVariable)
+
+        # Use object.__getattribute__ to access __dict__ directly,
+        # bypassing any custom __getattribute__ on the module.
+        base_dict = object.__getattribute__(base, "__dict__")
+        return key in base_dict
 
     def _custom_getattr_fallback(
         self,
@@ -375,7 +381,7 @@ class NNModuleVariable(VariableTracker):
             )
 
         if name == "__dict__":
-            return self.get_dict_vt(tx)
+            return variables.GetAttrVariable(self, name, source=source)
 
         subobj = None
         if name in base_dict:
