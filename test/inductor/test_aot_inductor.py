26ec2ede242e654fe23f92d3a7a59bdcbb33441a
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-25 19:01:32 +0000

    Revert "[Inductor][X86] Remove deprecated fusion patterns (#173911)"
    This reverts commit 4e56ea301613b781a30238d6f9793e8e298c0146.
    
    Reverted https://github.com/pytorch/pytorch/pull/173911 on behalf of https://github.com/georgehong due to this is breaking internal tests: Explanation: Dynamo failed to run FX node with fake tensors: call_function onednn.qlinear_prepack...got RuntimeError('Operator \'onednn::qlinear_prepack\' was defined in C++ and has a Python fake impl. In this situation, we require there to also be a companion C++ `m.set_python_module(torch.testing._internal.common_quantization)` call, but we could not find one ([comment](https://github.com/pytorch/pytorch/pull/173911#issuecomment-4129016292))

--- test/inductor/test_aot_inductor.py
+++ test/inductor/test_aot_inductor.py
@@ -2965,11 +2965,11 @@ class AOTInductorTestsTemplate:
         Original PR: https://github.com/pytorch/pytorch/pull/139054
         """
         from torch.testing._internal.common_quantization import (
-            _static_quantized_linear_module,
+            _static_reference_quantized_linear_module,
         )
 
         example_inputs = (torch.randn(32, 16),)
-        model = _static_quantized_linear_module(
+        model = _static_reference_quantized_linear_module(
             N=15, K=16, bias=True, example_input=example_inputs[0]
         )
         model = torch.export.export(model, example_inputs, strict=True).module()
