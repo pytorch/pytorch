8c6923515cfa04984a1381b3e084110aa5c68c36
Ting Lu <tingl@nvidia.com>
2026-03-25 15:32:35 -0700

    Use relaxed range for cublas package

--- .github/scripts/generate_binary_build_matrix.py
+++ .github/scripts/generate_binary_build_matrix.py
@@ -62,7 +62,7 @@ PYTORCH_EXTRA_INSTALL_REQUIREMENTS = {
     ),
     "13.0": (
         "cuda-toolkit[nvrtc,cudart,cupti,cufft,curand,cusolver,cusparse,cufile,nvjitlink,nvtx]==13.0.2; platform_system == 'Linux' | "  # noqa: B950
-        "nvidia-cublas==13.1.1.3; platform_system == 'Linux' | "
+        "nvidia-cublas>=13.1.0.3,<=13.1.1.3; platform_system == 'Linux' | "
         "cuda-bindings>=13.0.3,<14; platform_system == 'Linux' | "
         "nvidia-cudnn-cu13==9.20.0.48; platform_system == 'Linux' | "
         "nvidia-cusparselt-cu13==0.8.1; platform_system == 'Linux' | "
