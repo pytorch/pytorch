3424b5fe08417595958f1f842dddeea24a87ea0a
Esttelle <18220760769@163.com>
2026-03-26 01:17:01 +0800

    fix issue#176211: Add out-of-bounds handling

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -4865,17 +4865,26 @@ def recompilation_reason_for_no_tensor_aliasing_guard(
     assert guard_manager.global_scope is not None
     global_scope = dict(guard_manager.global_scope)
     ids_to_source = collections.defaultdict(list)
+    outbound_tensors = []
     for tensor_source in guard_manager.no_tensor_aliasing_sources:
         global_scope["__compile_source__"] = tensor_source
-        tensor_id = id(eval(tensor_source, global_scope, scope))
+        try:
+            tensor = eval(tensor_source, global_scope, scope)
+        except:
+            outbound_tensors.append(tensor_source)
+        tensor_id = id(tensor)
         ids_to_source[tensor_id].append(tensor_source)
 
     duplicate_tensors = [
         f"{ids_to_source[key]}" for key in ids_to_source if len(ids_to_source[key]) > 1
     ]
 
-    reason = ", ".join(duplicate_tensors)
-    return [f"Duplicate tensors found: {reason}"]
+    duplicate_reason = ", ".join(duplicate_tensors)
+    reason = [f"Duplicate tensors found: {duplicate_reason}"]
+    if len(outbound_tensors) > 0:
+        outbound_reason = ",".join(outbound_tensors)
+        reason.append(f"Outbound tensors found: {outbound_reason}")
+    return reason
 
 
 def strip_local_scope(s: str) -> str:
