f46734ffbe268d82fdb1f78b53135d0509fd7dd4
vvvdwbvvv <vvvdwbvvv@gmail.com>
2026-03-26 18:42:06 +0000

    Dynamo/fix ignore logging functions (#178506)
    ## PR Summary
    
    Fixes #178455  ignore_logger_methods was renamed to ignore_logging_functions in torch 2.11 but wasn't added to blocklist in _get_dynamo_config_for_logging()
    
    ## Repro
    ```
      import torch
      import torch._dynamo.config
      import torch._dynamo.utils
    
      torch._dynamo.config.ignore_logging_functions.add(print)
      torch._dynamo.utils._get_dynamo_config_for_logging()
    ```
    ## Changes
    
    * Include `ignore_logging_functions` from `_get_dynamo_config_for_logging()` (consistent with existing `ignore_logger_methods`)
    * Add a regression test to ensure no crash when logging config includes builtin functions
    *
    
    Added a test that:
    
    * Inserts `print` into `ignore_logging_functions`
    * Verifies `_get_dynamo_config_for_logging()` returns valid JSON without errors
    
    related issue: #178455
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/178506
    Approved by: https://github.com/Lucaskabela
    
    Co-authored-by: PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>

--- test/dynamo/test_utils.py
+++ test/dynamo/test_utils.py
@@ -1,5 +1,6 @@
 # Owner(s): ["module: dynamo"]
 import dataclasses
+import json
 import os
 import pprint
 import sys
@@ -296,6 +297,14 @@ class TestUtils(TestCase):
             ReinplaceCounters._values,
             "Should not use enum value (integer) in key, should use trigger.name instead",
         )
+
+    def test_get_dynamo_config_for_logging_ignores_logging_functions(self):
+        with dynamo_config.patch(ignore_logging_functions={print}):
+            result = utils._get_dynamo_config_for_logging()
+            parsed = json.loads(result)
+
+        self.assertIsInstance(parsed, dict)
+        self.assertNotIn("ignore_logging_functions", parsed)
 
 
 class TestModel(torch.nn.Module):
