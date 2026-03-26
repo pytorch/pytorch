fb253f4d7478a09ed2acb7ca4aecbf045182340b
Anshul Sinha <anshulsi@meta.com>
2026-03-25 23:42:43 -0700

    [dtensor][random_ops] migrating random_ops to single dim strategies and increasing op coverage (#178457)
    **Summary:** migrated random_ops to single dim strategies and added some new ops
    
    **Test Case**
    1. pytest /data/users/anshulsi/pytorch/test/distributed/tensor/test_random_ops.py -k test_init_ops_dtypes
    2. pytest /data/users/anshulsi/pytorch/test/distributed/tensor/test_random_ops.py -k test_multinomial_sharded
    3. pytest test/distributed/tensor/test_pointwise_ops.py -k test_dropout_partial_redistributes
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/178457
    Approved by: https://github.com/wconstab

--- torch/distributed/tensor/_ops/_random_ops.py
+++ torch/distributed/tensor/_ops/_random_ops.py
@@ -1,43 +1,79 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
 import torch
-from torch.distributed.tensor._op_schema import (
-    OpSchema,
-    OpSpec,
-    OpStrategy,
-    StrategyType,
+from torch._ops import OpOverload
+from torch.distributed.tensor._dtensor_spec import TensorMeta
+from torch.distributed.tensor._op_schema import ArgsType, KwargsType
+from torch.distributed.tensor._ops.single_dim_strategy import (
+    _ShardingPlaceholder,
+    register_single_dim_strategy,
 )
-from torch.distributed.tensor._ops.utils import is_tensor_partial, register_op_strategy
+from torch.distributed.tensor.placement_types import Placement
 
 
 aten = torch.ops.aten
 
 
-@register_op_strategy(
-    [
-        aten.normal_.default,
-        aten.uniform_.default,
-        aten.native_dropout.default,
-        aten.bernoulli_.float,
-        aten.bernoulli.default,
-    ]
-)
-def random_op_strategy(op_schema: OpSchema) -> StrategyType:
-    self_strategy = op_schema.args_schema[0]
-    if not isinstance(self_strategy, OpStrategy):
+def _random_inplace_single_dim_strategy(
+    op: OpOverload,
+    args_schema: ArgsType,
+    kwargs_schema: KwargsType,
+) -> list[list[Placement | _ShardingPlaceholder]]:
+    """Single-dim strategy for in-place random ops (single tensor input, output follows input).
+
+    No Partial inputs: random sampling on partial tensors is undefined.
+    """
+    self_meta = args_schema[0]
+    if not isinstance(self_meta, TensorMeta):
         raise AssertionError
-
-    random_strategy = OpStrategy([])
-    for arg_strategy in self_strategy.strategies:
-        arg_spec = arg_strategy.output_spec
-        if is_tensor_partial(arg_spec):
-            # TODO: figure out how inplace random op should behave when it's partial
-            raise RuntimeError(f"{op_schema.op} with Partial is not supported yet!")
-        random_strategy.strategies.append(
-            OpSpec(
-                output_specs=arg_spec,
-                input_specs=(arg_spec,),
-                redistribute_cost=[[0.0] * len(self_strategy.strategies)],
-            )
+    num_outputs = sum(1 for r in op._schema.returns if "Tensor" in str(r.type))
+    placements: list[list[Placement | _ShardingPlaceholder]] = []
+    for i in range(len(self_meta.shape)):
+        rule: list[Placement | _ShardingPlaceholder] = [_ShardingPlaceholder(i)] * (
+            num_outputs + 1
         )
+        placements.append(rule)
+    return placements
 
-    return random_strategy
+
+# In-place random sampling ops: output follows input sharding exactly.
+_inplace_random_ops = [
+    aten.normal_.default,
+    aten.uniform_.default,
+    aten.native_dropout.default,
+    aten.bernoulli_.float,
+    aten.bernoulli.default,
+    aten.log_normal_.default,
+    aten.exponential_.default,
+    aten.geometric_.default,
+]
+
+for _op in _inplace_random_ops:
+    register_single_dim_strategy(_op)(_random_inplace_single_dim_strategy)
+
+
+@register_single_dim_strategy(aten.multinomial.default)
+def multinomial_single_dim_strategy(
+    op: OpOverload,
+    args_schema: ArgsType,
+    kwargs_schema: KwargsType,
+) -> list[list[Placement | _ShardingPlaceholder]]:
+    """Single-dim strategy for multinomial.
+
+    multinomial(self, num_samples, ...) -> Tensor
+    Input: [*, n_categories], Output: [*, num_samples] (dtype=long)
+
+    Only batch dims (all except the last) can be sharded — the last dim
+    (categories) is consumed by the sampling and maps to a different
+    semantic dim (num_samples) in the output.
+    """
+    self_meta = args_schema[0]
+    if not isinstance(self_meta, TensorMeta):
+        raise AssertionError
+    placements: list[list[Placement | _ShardingPlaceholder]] = []
+    for i in range(len(self_meta.shape) - 1):
+        rule: list[Placement | _ShardingPlaceholder] = [
+            _ShardingPlaceholder(i),
+            _ShardingPlaceholder(i),
+        ]
+        placements.append(rule)
+    return placements
