49dc2ea837e5486bb018463239d6b70c03ef2f14
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-30 17:02:27 +0000

    Revert "Enable interoperative set operations across all Dynamo set-like types (#178613)"
    This reverts commit 4fa1689361acaa512868d90cb65fd9184948c7f2.
    
    Reverted https://github.com/pytorch/pytorch/pull/178613 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/178613#issuecomment-4156614249))

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -1492,15 +1492,7 @@ class SetVariable(ConstDictVariable):
                 "__xor__": "symmetric_difference",
                 "__sub__": "difference",
             }.get(name)
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
+            if not isinstance(args[0], (SetVariable, variables.UserDefinedSetVariable)):
                 msg = VariableTracker.build(
                     tx,
                     f"unsupported operand type(s) for {name}: '{self.python_type_name()}' and '{args[0].python_type_name()}'",
@@ -1508,39 +1500,8 @@ class SetVariable(ConstDictVariable):
                 raise_observed_exception(TypeError, tx, args=[msg])
             assert m is not None
             return self.call_method(tx, m, args, kwargs)
-        elif name in ("__rand__", "__ror__", "__rxor__", "__rsub__"):
-            m = {
-                "__rand__": "__and__",
-                "__ror__": "__or__",
-                "__rxor__": "__xor__",
-                "__rsub__": "__sub__",
-            }.get(name)
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
-                msg = VariableTracker.build(
-                    tx,
-                    f"unsupported operand type(s) for {name}: '{args[0].python_type_name()}' and '{self.python_type_name()}'",
-                )
-                raise_observed_exception(TypeError, tx, args=[msg])
-            assert m is not None
-            return args[0].call_method(tx, m, [self], kwargs)
         elif name in ("__iand__", "__ior__", "__ixor__", "__isub__"):
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
+            if not isinstance(args[0], (SetVariable, variables.UserDefinedSetVariable)):
                 msg = VariableTracker.build(
                     tx,
                     f"unsupported operand type(s) for {name}: '{self.python_type_name()}' and '{args[0].python_type_name()}'",
@@ -1556,28 +1517,12 @@ class SetVariable(ConstDictVariable):
             self.call_method(tx, m, args, kwargs)
             return self
         elif name == "__eq__":
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
+            if not isinstance(args[0], (SetVariable, variables.UserDefinedSetVariable)):
                 return CONSTANT_VARIABLE_FALSE
             r = self.call_method(tx, "symmetric_difference", args, kwargs)
             return VariableTracker.build(tx, len(r.set_items) == 0)  # type: ignore[attr-defined]
         elif name in cmp_name_to_op_mapping:
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
+            if not isinstance(args[0], (SetVariable, variables.UserDefinedSetVariable)):
                 return VariableTracker.build(tx, NotImplemented)
             return VariableTracker.build(
                 tx,
@@ -1943,16 +1888,8 @@ class DictKeysVariable(DictViewVariable):
             m = getattr(self.set_items, name)
             r = m(args[0].set_items)  # type: ignore[attr-defined]
             return SetVariable(r)
-        elif name in cmp_name_to_op_mapping:
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
+        if name in cmp_name_to_op_mapping:
+            if not isinstance(args[0], (SetVariable, DictKeysVariable)):
                 return VariableTracker.build(tx, NotImplemented)
             return VariableTracker.build(
                 tx,
@@ -1985,13 +1922,6 @@ class DictValuesVariable(DictViewVariable):
 
 class DictItemsVariable(DictViewVariable):
     kv = "items"
-
-    @property
-    def set_items(self) -> set["ConstDictVariable._HashableTracker"]:
-        return {
-            ConstDictVariable._HashableTracker(variables.TupleVariable([k.vt, v]))
-            for k, v in self.view_items
-        }
 
     @property
     def view_items_vt(self) -> list[VariableTracker]:
@@ -2028,54 +1958,12 @@ class DictItemsVariable(DictViewVariable):
                 raise_args_mismatch(tx, name, "1 args", f"{len(args)} args")
             if isinstance(args[0], DictItemsVariable):
                 return self.dv_dict.call_method(tx, "__eq__", [args[0].dv_dict], {})
-            elif isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
-                return VariableTracker.build(
-                    tx,
-                    len(self.set_items ^ args[0].set_items) == 0,
-                )
             return CONSTANT_VARIABLE_FALSE
         elif name == "__iter__":
             from .lists import ListIteratorVariable
 
             return ListIteratorVariable(
                 self.view_items_vt, mutation_type=ValueMutationNew()
-            )
-        elif name in (
-            "__and__",
-            "__iand__",
-            "__or__",
-            "__ior__",
-            "__sub__",
-            "__isub__",
-            "__xor__",
-            "__ixor__",
-        ):
-            # These methods always returns a set
-            fn_hdl = getattr(self.set_items, name)
-            ret_val = fn_hdl(args[0].set_items)  # type: ignore[attr-defined]
-            return SetVariable(ret_val)
-        elif name in cmp_name_to_op_mapping:
-            if not isinstance(
-                args[0],
-                (
-                    SetVariable,
-                    variables.UserDefinedSetVariable,
-                    DictItemsVariable,
-                    DictKeysVariable,
-                ),
-            ):
-                return VariableTracker.build(tx, NotImplemented)
-            return VariableTracker.build(
-                tx,
-                cmp_name_to_op_mapping[name](self.set_items, args[0].set_items),  # type: ignore[attr-defined]
             )
         return super().call_method(tx, name, args, kwargs)
 
