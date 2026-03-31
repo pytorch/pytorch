8f5eb7cbc99128cb2f014cade01b25f5882f4149
Animesh Jain <anijain@umich.edu>
2026-03-31 15:17:43 -0700

    [dynamo] implement tp_as_number->nb_index slot (#178921)
    Used for operator.index and also for list and tuple getitems
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/178921
    Approved by: https://github.com/guilhermeleobas

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -78,6 +78,7 @@ from ..utils import (
     proxy_args_kwargs,
     raise_args_mismatch,
     set_methods,
+    specialize_symnode,
     str_methods,
     tensortype_to_dtype,
 )
@@ -1868,16 +1869,18 @@ class BuiltinVariable(VariableTracker):
         self, tx: "InstructionTranslator", arg: VariableTracker
     ) -> VariableTracker:
         if arg.is_tensor():
+            # TODO - Support __index__ on tensors
+            # https://github.com/pytorch/pytorch/blob/7cfd054075b/tools/autograd/templates/python_variable_methods.cpp#L372-L385
             unimplemented(
                 gb_type="unsupported index(Tensor)",
                 context="",
                 explanation="Dynamo does not support tracing builtin index() on a Tensor",
                 hints=[],
             )
-
-        arg = guard_if_dyn(arg)
-        constant_value = operator.index(arg)
-        return VariableTracker.build(tx, constant_value)
+        # Specialize SymNodeVariable to a constant first, matching CPython's
+        # PyNumber_Index which forces a concrete int.
+        arg = specialize_symnode(arg)
+        return arg.nb_index_impl(tx)
 
     def call_round(
         self,
