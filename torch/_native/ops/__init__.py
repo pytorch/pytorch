2344f18350e181a3854354b90b43795861862ddd
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-03 14:58:50 +0000

    Revert "Adding stamp out using native api for bmm outer product (#179082)"
    This reverts commit fcf5f21a71a30dc354f39f73b4b51348225e8cce.
    
    Reverted https://github.com/pytorch/pytorch/pull/179082 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/179082#issuecomment-4183818016))

--- torch/_native/ops/__init__.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", ".*Warning only once for all operators.*")
-    from . import bmm_outer_product  # noqa: F401
