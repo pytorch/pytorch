eba69211d27b89f65fc01cae8813cf4be898db9e
Lakshay Garg <lakshayg@nvidia.com>
2026-04-04 16:39:07 +0000

    Replace erase idiom for map/set with erase_if (#179373)
    C++20 provides `std::erase_if(container, pred)` which is equivalent to the following much longer code snippet for associative containers:
    
    ```cpp
    auto it = container.begin();
    while (it != container.end()) {
      if (pred(*it)) {
        it = container.erase(it);
      } else {
        ++it;
      }
    }
    ```
    
    PyTorch now supports C++20: #176662
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179373
    Approved by: https://github.com/cyyever, https://github.com/Skylion007

--- aten/src/ATen/cuda/CublasHandlePool.cpp
+++ aten/src/ATen/cuda/CublasHandlePool.cpp
@@ -142,24 +142,16 @@ void clearCublasWorkspacesForStream(cudaStream_t stream) {
   {
     auto& workspace = cublas_handle_stream_to_workspace();
     std::unique_lock<std::shared_mutex> lock(workspace.mutex);
-    for (auto it = workspace.map.begin(); it != workspace.map.end(); ) {
-      if (std::get<1>(it->first) == stream_ptr) {
-        it = workspace.map.erase(it);
-      } else {
-        ++it;
-      }
-    }
+    std::erase_if(workspace.map, [stream_ptr](const auto& entry) {
+      return std::get<1>(entry.first) == stream_ptr;
+    });
   }
   {
     auto& workspace = cublaslt_handle_stream_to_workspace();
     std::unique_lock<std::shared_mutex> lock(workspace.mutex);
-    for (auto it = workspace.map.begin(); it != workspace.map.end(); ) {
-      if (std::get<1>(it->first) == stream_ptr) {
-        it = workspace.map.erase(it);
-      } else {
-        ++it;
-      }
-    }
+    std::erase_if(workspace.map, [stream_ptr](const auto& entry) {
+      return std::get<1>(entry.first) == stream_ptr;
+    });
   }
 }
 
