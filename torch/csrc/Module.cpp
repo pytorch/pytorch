48cec3ce9f2314c22cd2ff56ec1e696dfad6608b
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-09 22:18:36 +0000

    Revert "Make Generator an Opaque Object (#179661)"
    This reverts commit 4510a721a2287ac57f47b8a0bd5c5e2fbec49575.
    
    Reverted https://github.com/pytorch/pytorch/pull/179661 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/179661#issuecomment-4217993074))

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -241,7 +241,7 @@ static PyObject* THPModule_initExtension(
 
   auto module = THPObjectPtr(PyImport_ImportModule("torch"));
   if (!module)
-    throw python_error(); // @allow-raw-throw
+    throw python_error();
 
   THPStorage_postInit(module);
   THPAutograd_initFunctions();
@@ -473,7 +473,7 @@ static PyObject* THPModule_addDocStr(PyObject* _unused, PyObject* args) {
           m->d_getset->name);
     }
     m->d_getset->doc = doc_str;
-  } else if (PyType_Check(obj)) {
+  } else if (Py_TYPE(obj) == &PyType_Type) {
     PyTypeObject* t = reinterpret_cast<PyTypeObject*>(obj);
     if (t->tp_doc) {
       return PyErr_Format(
@@ -682,7 +682,7 @@ static PyObject* THPModule_torchDeviceToDLDevice(
 
   auto tuple = PyTuple_New(2);
   if (!tuple) {
-    throw python_error(); // @allow-raw-throw
+    throw python_error();
   }
 
   PyTuple_SET_ITEM(tuple, 0, THPUtils_packInt64(dl_device.device_type));
