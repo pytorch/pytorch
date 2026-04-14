09475bc265cdc3321762a76f130211b7dc3a0035
Eddie Yan <eddiey@nvidia.com>
2026-04-14 11:37:54 -0700

    check in

--- test/test_jit_simple.py
+++ test/test_jit_simple.py
@@ -2,6 +2,8 @@
 
 import sys
 sys.argv.append("--jit-executor=simple")
+from torch.testing._internal.common_utils import parse_cmd_line_args
+parse_cmd_line_args()
 from test_jit import *  # noqa: F403
 
 if __name__ == '__main__':
