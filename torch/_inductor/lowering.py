9affa050130594572026b9f8bf6207263f4c7106
Tirth Patel <tirthp@nvidia.com>
2026-04-14 23:53:18 +0000

    Address review comment

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -4537,7 +4537,6 @@ def full(size, fill_value, **kwargs):
 
 @register_lowering(aten._efficientzerotensor)
 def _efficientzerotensor(size, **kwargs):
-    kwargs.setdefault("dtype", torch.get_default_dtype())
     return tensor_constructor(0)(size, **kwargs)
 
 
