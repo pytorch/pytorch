77fbb1f55b486d41397b453bb87fe201da242e0b
Prachi Gupta <prachi.gupta@amd.com>
2026-04-14 22:07:57 +0000

    [ROCm] - Disable test_dtensor_pp_integrating (#180375)
    Trying get CI back to green
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/180375
    Approved by: https://github.com/jeffdaily

--- test/run_test.py
+++ test/run_test.py
@@ -199,6 +199,7 @@ ROCM_BLOCKLIST = [
     "test_cuda_nvml_based_avail",
     "test_jit_cuda_fuser",
     "higher_order_ops/test_inline_asm_elementwise",
+    "distributed/pipelining/test_dtensor_pp_integration",
 ]
 
 # Add architecture-specific blocklist entries
