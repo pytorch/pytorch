8934f93196b415c56bc2fd85890ccd676cb72130
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2026-04-17 13:21:13 +0400

    Pass groupName as an arg

--- torch/csrc/distributed/c10d/symm_mem/intra_node_comm.cu
+++ torch/csrc/distributed/c10d/symm_mem/intra_node_comm.cu
@@ -46,7 +46,7 @@ at::Tensor IntraNodeComm::oneShotAllReduce(
       at::TensorOptions().dtype(input.dtype()).device(input.device()));
 
   symmMemTensor.copy_(input);
-  op.call(symmMemTensor, "sum", "", input);
+  op.call(symmMemTensor, "sum", groupName_, input);
   return input;
 }
 
@@ -65,7 +65,7 @@ at::Tensor IntraNodeComm::twoShotAllReduce(
       at::TensorOptions().dtype(input.dtype()).device(input.device()));
 
   symmMemTensor.copy_(input);
-  op.call(symmMemTensor, "sum", "");
+  op.call(symmMemTensor, "sum", groupName_);
   input.copy_(symmMemTensor);
   return input;
 }
