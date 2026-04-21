a26b10e0a9aaa913c2fc93ab06e1852c01e83cef
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-21 14:52:08 +0000

    Revert "[pyrefly] Add type annotations to torch/fx graph and graph_module (#180879)"
    This reverts commit f71088361f3c3325cc31736765b29e9260d8fd61.
    
    Reverted https://github.com/pytorch/pytorch/pull/180879 on behalf of https://github.com/Lucaskabela due to Breaks internal tests - dont_inherit was not included in the changes :( ([comment](https://github.com/pytorch/pytorch/pull/180879#issuecomment-4289490865))

--- torch/_functorch/compile_utils.py
+++ torch/_functorch/compile_utils.py
@@ -219,7 +219,7 @@ def strip_overloads(gm: fx.GraphModule) -> None:
     gm.recompile()
 
 
-def get_placeholders(graph: fx.Graph) -> list[Any]:
+def get_placeholders(graph: fx.Graph) -> fx.graph._node_list:
     return graph.find_nodes(op="placeholder")
 
 
