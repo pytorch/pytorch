fa55d49288b7c1cc44f9b3ba1fb817cb88abf8dc
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-21 18:12:43 +0000

    Revert "[inductor] Fix cpp_wrapper lazy compile for combo kernels with per-subkernel blocks (#180462)"
    This reverts commit 7231f9e7a302e0368eee7adf8dcbcd6fd79fe2be.
    
    Reverted https://github.com/pytorch/pytorch/pull/180462 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/180462#issuecomment-4290781700))

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -684,8 +684,7 @@ test_inductor_cpp_wrapper_shard() {
     --shard "$1" "$NUM_TEST_SHARDS" \
     --verbose
   python test/run_test.py \
-    --include inductor/test_torchinductor inductor/test_max_autotune inductor/test_cpu_repro \
-              inductor/test_triton_kernels inductor/test_combo_kernels \
+    --include inductor/test_torchinductor inductor/test_max_autotune inductor/test_cpu_repro inductor/test_triton_kernels \
     --shard "$1" "$NUM_TEST_SHARDS" \
     --verbose
   python test/run_test.py --inductor \
@@ -693,14 +692,12 @@ test_inductor_cpp_wrapper_shard() {
     -k 'take' \
     --shard "$1" "$NUM_TEST_SHARDS" \
     --verbose
-
   # Keep testing TORCHINDUCTOR_AUTOTUNE_AT_COMPILE_TIME=1 for the near future.
   # Will drop this after AOTInductor also switches to lazy Triton compilation.
   TORCHINDUCTOR_AUTOTUNE_AT_COMPILE_TIME=1 python test/run_test.py \
     --include inductor/test_torchinductor inductor/test_triton_kernels inductor/test_max_autotune \
     --shard "$1" "$NUM_TEST_SHARDS" \
     --verbose
-
   if [[ "${BUILD_ENVIRONMENT}" == *xpu* ]]; then
     python test/run_test.py \
       --include inductor/test_mkldnn_pattern_matcher \
