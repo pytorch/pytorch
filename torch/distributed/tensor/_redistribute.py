ea3167c8d3ebf88aa149551e54be3e08676886ac
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-22 15:59:24 +0000

    Revert "[DeviceMesh] Enforce 2-level Layouts (#172089)"
    This reverts commit 0e045b525211d91835813671ee2831bdd77a0259.
    
    Reverted https://github.com/pytorch/pytorch/pull/172089 on behalf of https://github.com/ZainRizvi due to Sorry but this is breaking internally. See diff D101859905 for details. To validate your fixes internally, you can follow the instructions here: https://fburl.com/fixing-ghfirst-reverts ([comment](https://github.com/pytorch/pytorch/pull/172089#issuecomment-4297794131))

--- torch/distributed/tensor/_redistribute.py
+++ torch/distributed/tensor/_redistribute.py
@@ -15,7 +15,6 @@ import torch
 import torch.distributed._functional_collectives as funcol
 import torch.distributed.tensor._api as dtensor
 from torch.distributed._functional_collectives import _are_we_tracing
-from torch.distributed._mesh_layout import _MeshLayout
 from torch.distributed.tensor._collective_utils import one_step_redistribute_cost
 from torch.distributed.tensor._dtensor_spec import (
     _StridedShardNotDecodableError,
@@ -276,7 +275,9 @@ def _get_flattened_mesh_by_layout_impl(
     # Compute expected layout WITHOUT creating a submesh (avoids tracing issues)
     # _get_slice_mesh_layout does pure layout math, no tensor operations
     sliced_layout = mesh._get_slice_mesh_layout(dim_names)
-    expected_layout = _MeshLayout([sliced_layout.collapse()])
+    expected_layout = sliced_layout.coalesce()
+    if len(expected_layout) > 1:
+        expected_layout = expected_layout.nest()
 
     # Search existing flattened meshes by comparing layouts
     for flattened_mesh in root_mesh._flatten_mapping.values():
