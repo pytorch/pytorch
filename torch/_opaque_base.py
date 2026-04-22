3c88b2d57ce3b388db42b0cde801b6b4ea851854
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-22 22:10:14 +0000

    Revert "Make Generator an Opaque Object (#179661)"
    This reverts commit 4aef62c058b81814edcde02db43679285db6e83e.
    
    Reverted https://github.com/pytorch/pytorch/pull/179661 on behalf of https://github.com/yushangdi due to breaks internal buck dependency ([comment](https://github.com/pytorch/pytorch/pull/179661#issuecomment-4300265707))

--- torch/_opaque_base.py
+++ torch/_opaque_base.py
@@ -1,8 +1,3 @@
-# Cached lazily on first __instancecheck__ miss to avoid an import cycle at
-# module load (FakeScriptObject's module imports torch, which imports us).
-_FakeScriptObject_cls: type | None = None
-
-
 class OpaqueBaseMeta(type):
     def __instancecheck__(cls, instance):
         # When checking against OpaqueBase itself (not a concrete subclass),
@@ -17,19 +12,11 @@ class OpaqueBaseMeta(type):
         if super().__instancecheck__(instance):
             return True
 
-        # Check FakeScriptObject before hasattr to avoid triggering custom
-        # __getattr__ on arbitrary user objects (e.g. dict-like objects that
-        # raise KeyError on unknown attributes).
-        # e.g. test/dynamo/test_dynamic_shapes.py -k test_user_getattr1_dynamic_shapes
-        global _FakeScriptObject_cls
-        if _FakeScriptObject_cls is None:
+        if hasattr(instance, "real_obj"):
             from torch._library.fake_class_registry import FakeScriptObject
 
-            _FakeScriptObject_cls = FakeScriptObject
-        if isinstance(instance, _FakeScriptObject_cls) and hasattr(
-            instance, "real_obj"
-        ):
-            return super().__instancecheck__(instance.real_obj)
+            if isinstance(instance, FakeScriptObject):
+                return super().__instancecheck__(instance.real_obj)
 
         return False
 
