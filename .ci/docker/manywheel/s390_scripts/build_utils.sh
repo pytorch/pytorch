421b9fe540e56d5cbda6576e6d312e31594d7fdb
Aleksei Nikiforov <aleksei.nikiforov@linux.ibm.com>
2026-04-23 12:27:11 +0200

    Update OpenSSL old downloads location

--- .ci/docker/manywheel/s390_scripts/build_utils.sh
+++ .ci/docker/manywheel/s390_scripts/build_utils.sh
@@ -2,7 +2,7 @@
 # Helper utilities for build
 # Script used only in CD pipeline
 
-OPENSSL_DOWNLOAD_URL=https://www.openssl.org/source/old/1.1.1/  # @lint-ignore
+OPENSSL_DOWNLOAD_URL=https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1l/  # @lint-ignore
 CURL_DOWNLOAD_URL=https://curl.se/download
 
 AUTOCONF_DOWNLOAD_URL=https://ftp.gnu.org/gnu/autoconf
