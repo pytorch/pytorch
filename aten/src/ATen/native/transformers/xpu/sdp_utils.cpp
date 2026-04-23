b07686f3f8d45cb9f3d3bc031bf436c67b3a5d0e
fengqing.lu <fengqing.lu@intel.com>
2026-04-23 08:18:21 +0000

    [XPU] Add address 64byte  alignment check for OneDNN SDPA (#179800)
    OneDNN require base address to be 64byte alignment. This PR add condition check and clone tensor if needs.
    
    Fix  test_singelton_head_dim_stride_ne_1_xpu and  TestTransformersXPU,test_transformerencoder_fastpath for upstream UT
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179800
    Approved by: https://github.com/EikanWang, https://github.com/guangyey

--- aten/src/ATen/native/transformers/xpu/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/xpu/sdp_utils.cpp
@@ -173,7 +173,7 @@ bool can_use_flash_attention(sdp_params const& params, bool debug) {
           check_tensor_shapes,
           check_batch_size_and_num_heads_dense<true /*supports GQA*/>,
           check_nonzero_sequence_lengths_dense,
-          check_last_dim_stride_equals_1_dense<false /*ignore_singleton_dim*/>,
+          check_last_dim_stride_equals_1_dense<true /*ignore_singleton_dim*/>,
           check_flash_causal_non_square_seqlens,
           check_flash_attention_datatype,
           check_flash_attention_head_dim_size,
