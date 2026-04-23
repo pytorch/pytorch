0bb033cf40f2dbf3c6032f7ec2493205a361c521
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-23 20:44:36 +0000

    Revert "[dynamo] Add generic_getiter (#178462)"
    This reverts commit dd2cf663205319b3c4e418e3840da16ca32e0b73.
    
    Reverted https://github.com/pytorch/pytorch/pull/178462 on behalf of https://github.com/malfet due to Not sure, what's going on but looks like it's failing again, see https://hud.pytorch.org/hud/pytorch/pytorch/main/1?per_page=50&name_filter=dynamo_wrapped ([comment](https://github.com/pytorch/pytorch/pull/178462#issuecomment-4307893267))

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -1640,13 +1640,10 @@ class TensorVariable(VariableTracker):
         """Sequence length for tensors (size along first dimension)."""
         return self.call_method(tx, "size", [VariableTracker.build(tx, 0)], {})
 
-    def tp_iter_impl(self, tx: "InstructionTranslator") -> VariableTracker:
+    def method___iter__(self, tx: "InstructionTranslator") -> ListIteratorVariable:
         return ListIteratorVariable(
             self.unpack_var_sequence(tx), mutation_type=ValueMutationNew()
         )
-
-    def method___iter__(self, tx: "InstructionTranslator") -> VariableTracker:
-        return self.tp_iter_impl(tx)
 
     def method_addcmul_(
         self,
