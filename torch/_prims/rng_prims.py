1c5c8cb33ca16d9c3e6247acfad294f9aee35358
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-04-24 15:18:51 +0000

    Revert "Make Generator an Opaque Object (#179661)"
    This reverts commit 7e0ab7a126f68a35b0ec4647bd5a3ec87ec612cf.
    
    Reverted https://github.com/pytorch/pytorch/pull/179661 on behalf of https://github.com/atalman due to sorry need to revert again, not sure why windows builds where not executed ([comment](https://github.com/pytorch/pytorch/pull/179661#issuecomment-4314245101))

--- torch/_prims/rng_prims.py
+++ torch/_prims/rng_prims.py
@@ -394,26 +394,6 @@ def register_graphsafe_run_with_rng_state_op():
 graphsafe_run_with_rng_state = register_graphsafe_run_with_rng_state_op()
 
 
-# Late-bind OpaqueBaseMeta as Generator's metaclass. This is done here
-# rather than in THPGenerator_init (C++) to avoid making torch._C depend
-# on torch._opaque_base at init time.
-from torch._opaque_base import OpaqueBaseMeta
-
-
-torch._C._set_generator_metaclass(OpaqueBaseMeta)
-
-torch._library.opaque_object.register_opaque_type(
-    torch._C.Generator,
-    typ="reference",
-    guard_fn=lambda gen: [gen.device],
-    members={
-        "device": torch._library.opaque_object.MemberType.USE_REAL,
-        "__eq__": torch._library.opaque_object.MemberType.USE_REAL,
-        "__ne__": torch._library.opaque_object.MemberType.USE_REAL,
-    },
-)
-
-
 def register_run_dtensor_rng_op():
     """
     Register a higher-order operator for DTensor distributed random operations.
