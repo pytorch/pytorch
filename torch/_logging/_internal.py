c3e6711a601e696b904a20f9e72ed55c3d04aca7
root <shangdiy@meta.com>
2026-04-30 16:12:38 -0700

    [tlparse] Add subgraph entry to tlparse (#181958)
    Propose to add a "subgraph_name" entry to tlparse. After modifying tlparse like in https://github.com/meta-pytorch/tlparse/pull/181, we will be able to see which subgraph the tlparse artifact is corresponding to.
    
    See https://github.com/meta-pytorch/tlparse/pull/181 for tlparse result screenshots.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/181958
    Approved by: https://github.com/anijain2305
    ghstack dependencies: #181808, #181809, #181834

--- torch/_logging/_internal.py
+++ torch/_logging/_internal.py
@@ -1447,6 +1447,7 @@ def trace_structured(
         "timestamp",
         "pathname",
         "thread",
+        "subgraph_name",
     ]
     if name in reserved_names:
         raise AssertionError(f"name {name!r} is reserved and cannot be used")
@@ -1489,6 +1490,12 @@ def trace_structured(
                         record["frame_compile_id"] = cid.frame_compile_id
                 if trace_id:
                     record["attempt"] = trace_id.attempt
+
+            from torch.fx.traceback import get_regional_inductor_subgraph_name
+
+            subgraph_name = get_regional_inductor_subgraph_name()
+            if subgraph_name is not None:
+                record["subgraph_name"] = subgraph_name
 
         payload = payload_fn()
         if payload is not None:
@@ -1551,3 +1558,4 @@ def dtrace_structured(
 import torch._guards
 import torch._utils_internal
 import torch.distributed as dist
+import torch.fx.traceback
