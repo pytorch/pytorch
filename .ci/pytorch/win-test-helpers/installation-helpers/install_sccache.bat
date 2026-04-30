19daebc8485bb4316b6086ba85720ddc421d96ca
Gopi Kanugula <gkanugula@nvidia.com>
2026-04-30 21:06:48 +0530

    Download magma & sccache install scripts over https instead of aws s3

--- .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -5,9 +5,5 @@ if "%REBUILD%"=="" (
     taskkill /im sccache.exe /f /t || ver > nul
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
   )
-  if "%BUILD_ENVIRONMENT%"=="" (
-    curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-v0.7.4.exe --output %TMP_DIR_WIN%\bin\sccache.exe
-  ) else (
-    aws s3 cp s3://ossci-windows/sccache-v0.7.4.exe %TMP_DIR_WIN%\bin\sccache.exe
-  )
+  curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-v0.7.4.exe --output %TMP_DIR_WIN%\bin\sccache.exe
 )
