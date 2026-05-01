1cf0c11d6df65786520f6a0026d61e42d5c346d0
ethanwee1 <ethanwee@amd.com>
2026-05-01 21:38:31 +0000

    [ROCm] Unskip matmul accuracy tests for hipblas/hipblaslt (#175868)
    Unskipping some 64 UT's that were skipped for ROCm 7.0 due to accuracy regressions. These tests now pass with ROCm 7.2.1
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/175868
    Approved by: https://github.com/jeffdaily

--- test/test_matmul_cuda.py
+++ test/test_matmul_cuda.py
@@ -18,6 +18,7 @@ from torch.quantization._quantized_conversions import (
 
 from torch.testing import make_tensor
 from torch.testing._internal.common_cuda import (
+    _get_torch_rocm_version,
     blas_library_context,
     PLATFORM_SUPPORTS_BF16,
     SM80OrLater,
@@ -771,7 +772,7 @@ class TestMatmulCuda(InductorTestCase):
     @parametrize("batch_size", [None, 1, 16])
     @parametrize("backend", ["cublas", "cublaslt"])
     def test_mm_bmm_dtype_overload(self, input_dtype, M, N, K, batch_size, backend):
-        if torch.version.hip:
+        if torch.version.hip and _get_torch_rocm_version() < (7, 2, 1):
             msg = "accuracy regression in hipblas and hipblaslt in ROCm 7.0 for certain shapes"
             if input_dtype == torch.bfloat16 and N == 1 and K == 32 and batch_size:
                 raise unittest.SkipTest(msg)
@@ -838,7 +839,7 @@ class TestMatmulCuda(InductorTestCase):
     @parametrize("high_precision_self", [False, True])
     @parametrize("backend", ["cublas", "cublaslt"])
     def test_addmm_baddmm_dtype_overload(self, input_dtype, M, N, K, batch_size, broadcast_self, high_precision_self, backend):
-        if torch.version.hip:
+        if torch.version.hip and _get_torch_rocm_version() < (7, 2, 1):
             msg = "accuracy regression in hipblas and hipblaslt in ROCm 7.0 for certain shapes"
             if input_dtype == torch.bfloat16 and N == 1 and K == 32 and batch_size:
                 raise unittest.SkipTest(msg)
