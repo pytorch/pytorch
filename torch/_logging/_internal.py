9a1f9ce7533e403b0b271b4b85395af340983d46
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-01 18:03:40 +0000

    Revert "[tlparse] Add subgraph entry to tlparse (#181958)"
    This reverts commit 0d973fae05afe610311ab1d8342a6aee240c159e.
    
    Reverted https://github.com/pytorch/pytorch/pull/181958 on behalf of https://github.com/huydhn due to The doc build failure is legit ([comment](https://github.com/pytorch/pytorch/pull/181958#issuecomment-4360807405))

--- torch/_logging/_internal.py
+++ torch/_logging/_internal.py
@@ -1447,7 +1447,6 @@ def trace_structured(
         "timestamp",
         "pathname",
         "thread",
-        "subgraph_name",
     ]
     if name in reserved_names:
         raise AssertionError(f"name {name!r} is reserved and cannot be used")
@@ -1490,12 +1489,6 @@ def trace_structured(
                         record["frame_compile_id"] = cid.frame_compile_id
                 if trace_id:
                     record["attempt"] = trace_id.attempt
-
-            from torch.fx.traceback import get_regional_inductor_subgraph_name
-
-            subgraph_name = get_regional_inductor_subgraph_name()
-            if subgraph_name is not None:
-                record["subgraph_name"] = subgraph_name
 
         payload = payload_fn()
         if payload is not None:
@@ -1558,4 +1551,3 @@ def dtrace_structured(
 import torch._guards
 import torch._utils_internal
 import torch.distributed as dist
-import torch.fx.traceback
