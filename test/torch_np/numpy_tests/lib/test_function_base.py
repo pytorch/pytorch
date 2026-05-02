6653b6de23d7c7a4cd7560b815250802a8714ad5
Animesh Jain <anijain@umich.edu>
2026-05-02 15:54:09 -0700

    [dynamo] Remove dynamo_expected_failures entries that pass on main (#182235)
    These tests are reported as "Unexpected success" in CI, so the corresponding
    entries in test/dynamo_expected_failures/ are stale and need to be removed.
    
    Followup from https://github.com/pytorch/pytorch/pull/180271
    
    Authored-by: Claude
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/182235
    Approved by: https://github.com/malfet, https://github.com/houseroad

--- test/torch_np/numpy_tests/lib/test_function_base.py
+++ test/torch_np/numpy_tests/lib/test_function_base.py
@@ -2884,12 +2884,11 @@ class TestPercentile(TestCase):
         o = np.ones((1,))
         np.percentile(d, 5, None, o, False, "linear")
 
-    @xfail  # (reason="TODO: implement")
-    def test_complex(self):
-        arr_c = np.array([0.5 + 3.0j, 2.1 + 0.5j, 1.6 + 2.3j], dtype="D")
-        assert_raises(TypeError, np.percentile, arr_c, 0.5)
-        arr_c = np.array([0.5 + 3.0j, 2.1 + 0.5j, 1.6 + 2.3j], dtype="F")
-        assert_raises(TypeError, np.percentile, arr_c, 0.5)
+    # def test_complex(self):
+    #     arr_c = np.array([0.5 + 3.0j, 2.1 + 0.5j, 1.6 + 2.3j], dtype="D")
+    #     assert_raises(TypeError, np.percentile, arr_c, 0.5)
+    #     arr_c = np.array([0.5 + 3.0j, 2.1 + 0.5j, 1.6 + 2.3j], dtype="F")
+    #     assert_raises(TypeError, np.percentile, arr_c, 0.5)
 
     def test_2D(self):
         x = np.array([[1, 1, 1], [1, 1, 1], [4, 4, 3], [1, 1, 1], [1, 1, 1]])
