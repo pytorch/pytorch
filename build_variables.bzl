pytorch/pytorch

commit 25cf50a47043eeaeb8995d0b8ef0c559ae42c8bb

[autograd] Increase VariableType and TraceType num_shards from 5 to 10 (#178894)
These generated files have grown large enough to cause slow compilation.
Doubling the shard count reduces per-file size and improves parallel build
times.

Fixes #178666.

🤖 Generated with [Claude Code](https://claude.com/claude-code)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/178894
Approved by: https://github.com/atalman

build_variables.bzl
@@ -24,12 +24,22 @@ def libtorch_generated_sources(gencode_pattern):
         "torch/csrc/autograd/generated/VariableType_2.cpp",
         "torch/csrc/autograd/generated/VariableType_3.cpp",
         "torch/csrc/autograd/generated/VariableType_4.cpp",
+        "torch/csrc/autograd/generated/VariableType_5.cpp",
+        "torch/csrc/autograd/generated/VariableType_6.cpp",
+        "torch/csrc/autograd/generated/VariableType_7.cpp",
+        "torch/csrc/autograd/generated/VariableType_8.cpp",
+        "torch/csrc/autograd/generated/VariableType_9.cpp",
         "torch/csrc/autograd/generated/ViewFuncs.cpp",
         "torch/csrc/autograd/generated/TraceType_0.cpp",
         "torch/csrc/autograd/generated/TraceType_1.cpp",
         "torch/csrc/autograd/generated/TraceType_2.cpp",
         "torch/csrc/autograd/generated/TraceType_3.cpp",
         "torch/csrc/autograd/generated/TraceType_4.cpp",
+        "torch/csrc/autograd/generated/TraceType_5.cpp",
+        "torch/csrc/autograd/generated/TraceType_6.cpp",
+        "torch/csrc/autograd/generated/TraceType_7.cpp",
+        "torch/csrc/autograd/generated/TraceType_8.cpp",
+        "torch/csrc/autograd/generated/TraceType_9.cpp",
         "torch/csrc/autograd/generated/ADInplaceOrViewType_0.cpp",
         "torch/csrc/autograd/generated/ADInplaceOrViewType_1.cpp",
     ]]

buckbuild.bzl
@@ -681,6 +681,11 @@ def pt_operator_query_codegen(
         ":{}[autograd/generated/VariableType_2.cpp]".format(unboxing_and_autograd_genrule),
         ":{}[autograd/generated/VariableType_3.cpp]".format(unboxing_and_autograd_genrule),
         ":{}[autograd/generated/VariableType_4.cpp]".format(unboxing_and_autograd_genrule),
+        ":{}[autograd/generated/VariableType_5.cpp]".format(unboxing_and_autograd_genrule),
+        ":{}[autograd/generated/VariableType_6.cpp]".format(unboxing_and_autograd_genrule),
+        ":{}[autograd/generated/VariableType_7.cpp]".format(unboxing_and_autograd_genrule),
+        ":{}[autograd/generated/VariableType_8.cpp]".format(unboxing_and_autograd_genrule),
+        ":{}[autograd/generated/VariableType_9.cpp]".format(unboxing_and_autograd_genrule),
         ":{}[autograd/generated/ADInplaceOrViewType_0.cpp]".format(unboxing_and_autograd_genrule),
         ":{}[autograd/generated/ADInplaceOrViewType_1.cpp]".format(unboxing_and_autograd_genrule),
     ] if train else []) + ([

build.bzl
@@ -311,12 +311,22 @@ GENERATED_AUTOGRAD_CPP = [
     "torch/csrc/autograd/generated/VariableType_2.cpp",
     "torch/csrc/autograd/generated/VariableType_3.cpp",
     "torch/csrc/autograd/generated/VariableType_4.cpp",
+    "torch/csrc/autograd/generated/VariableType_5.cpp",
+    "torch/csrc/autograd/generated/VariableType_6.cpp",
+    "torch/csrc/autograd/generated/VariableType_7.cpp",
+    "torch/csrc/autograd/generated/VariableType_8.cpp",
+    "torch/csrc/autograd/generated/VariableType_9.cpp",
     "torch/csrc/autograd/generated/ViewFuncs.cpp",
     "torch/csrc/autograd/generated/TraceType_0.cpp",
     "torch/csrc/autograd/generated/TraceType_1.cpp",
     "torch/csrc/autograd/generated/TraceType_2.cpp",
     "torch/csrc/autograd/generated/TraceType_3.cpp",
     "torch/csrc/autograd/generated/TraceType_4.cpp",
+    "torch/csrc/autograd/generated/TraceType_5.cpp",
+    "torch/csrc/autograd/generated/TraceType_6.cpp",
+    "torch/csrc/autograd/generated/TraceType_7.cpp",
+    "torch/csrc/autograd/generated/TraceType_8.cpp",
+    "torch/csrc/autograd/generated/TraceType_9.cpp",
     "torch/csrc/autograd/generated/ADInplaceOrViewType_0.cpp",
     "torch/csrc/autograd/generated/ADInplaceOrViewType_1.cpp",
     "torch/csrc/lazy/generated/LazyNativeFunctions.cpp",

caffe2/CMakeLists.txt
@@ -262,11 +262,21 @@ if(NOT INTERN_DISABLE_AUTOGRAD AND NOT BUILD_LITE_INTERPRETER)
     "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_2.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_3.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_4.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_5.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_6.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_7.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_8.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/VariableType_9.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_0.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_1.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_2.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_3.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_4.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_5.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_6.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_7.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_8.cpp"
+    "${TORCH_SRC_DIR}/csrc/autograd/generated/TraceType_9.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/ADInplaceOrViewType_0.cpp"
     "${TORCH_SRC_DIR}/csrc/autograd/generated/ADInplaceOrViewType_1.cpp"
     "${TORCH_SRC_DIR}/csrc/inductor/aoti_torch/generated/c_shim_cpu.cpp"

pt_template_srcs.bzl
@@ -123,6 +123,11 @@ def get_generate_code_bin_outs():
         "autograd/generated/TraceType_2.cpp": ["autograd/generated/TraceType_2.cpp"],
         "autograd/generated/TraceType_3.cpp": ["autograd/generated/TraceType_3.cpp"],
         "autograd/generated/TraceType_4.cpp": ["autograd/generated/TraceType_4.cpp"],
+        "autograd/generated/TraceType_5.cpp": ["autograd/generated/TraceType_5.cpp"],
+        "autograd/generated/TraceType_6.cpp": ["autograd/generated/TraceType_6.cpp"],
+        "autograd/generated/TraceType_7.cpp": ["autograd/generated/TraceType_7.cpp"],
+        "autograd/generated/TraceType_8.cpp": ["autograd/generated/TraceType_8.cpp"],
+        "autograd/generated/TraceType_9.cpp": ["autograd/generated/TraceType_9.cpp"],
         "autograd/generated/VariableType.h": ["autograd/generated/VariableType.h"],
         "autograd/generated/VariableTypeEverything.cpp": ["autograd/generated/VariableTypeEverything.cpp"],
         "autograd/generated/VariableType_0.cpp": ["autograd/generated/VariableType_0.cpp"],
@@ -130,6 +135,11 @@ def get_generate_code_bin_outs():
         "autograd/generated/VariableType_2.cpp": ["autograd/generated/VariableType_2.cpp"],
         "autograd/generated/VariableType_3.cpp": ["autograd/generated/VariableType_3.cpp"],
         "autograd/generated/VariableType_4.cpp": ["autograd/generated/VariableType_4.cpp"],
+        "autograd/generated/VariableType_5.cpp": ["autograd/generated/VariableType_5.cpp"],
+        "autograd/generated/VariableType_6.cpp": ["autograd/generated/VariableType_6.cpp"],
+        "autograd/generated/VariableType_7.cpp": ["autograd/generated/VariableType_7.cpp"],
+        "autograd/generated/VariableType_8.cpp": ["autograd/generated/VariableType_8.cpp"],
+        "autograd/generated/VariableType_9.cpp": ["autograd/generated/VariableType_9.cpp"],
         "autograd/generated/variable_factories.h": ["autograd/generated/variable_factories.h"],
         "autograd/generated/ViewFuncs.cpp": ["autograd/generated/ViewFuncs.cpp"],
         "autograd/generated/ViewFuncs.h": ["autograd/generated/ViewFuncs.h"],

tools/autograd/gen_trace_type.py
@@ -533,7 +533,7 @@ def gen_trace_type(
             + f"generated from {fm.template_dir_for_comments()}/TraceType.cpp",
         },
         env_callable=gen_trace_type_func,
-        num_shards=5,
+        num_shards=10,
         sharded_keys={
             "ops_headers",
             "trace_method_definitions",

tools/autograd/gen_variable_type.py
@@ -937,7 +937,7 @@ def gen_variable_type(
             + f"generated from {fm.template_dir_for_comments()}/VariableType.cpp",
         },
         env_callable=gen_variable_type_func,
-        num_shards=5,
+        num_shards=10,
         sharded_keys=sharded_keys,
     )
 

commit 46e99631c49fd38e39866f6ab1b18a46fe49aed6

[BE] Make ovrsource_defs.bzl use libtorch_generated_sources from build_variables.bzl
Summary: To prevent divergence between two build systems, for example as one caused by https://github.com/pytorch/pytorch/pull/178894

Test Plan: CI

Differential Revision: D103493720

build_variables.bzl
@@ -16,22 +16,22 @@ GENERATED_LAZY_TS_CPP = [
     "lazy/generated/RegisterLazy.cpp",
 ]
 
-def libtorch_generated_sources(gencode_pattern):
-    return [gencode_pattern.format(name) for name in [
-        "torch/csrc/autograd/generated/Functions.cpp",
-        "torch/csrc/autograd/generated/VariableType_0.cpp",
-        "torch/csrc/autograd/generated/VariableType_1.cpp",
-        "torch/csrc/autograd/generated/VariableType_2.cpp",
-        "torch/csrc/autograd/generated/VariableType_3.cpp",
-        "torch/csrc/autograd/generated/VariableType_4.cpp",
-        "torch/csrc/autograd/generated/ViewFuncs.cpp",
-        "torch/csrc/autograd/generated/TraceType_0.cpp",
-        "torch/csrc/autograd/generated/TraceType_1.cpp",
-        "torch/csrc/autograd/generated/TraceType_2.cpp",
-        "torch/csrc/autograd/generated/TraceType_3.cpp",
-        "torch/csrc/autograd/generated/TraceType_4.cpp",
-        "torch/csrc/autograd/generated/ADInplaceOrViewType_0.cpp",
-        "torch/csrc/autograd/generated/ADInplaceOrViewType_1.cpp",
+def libtorch_generated_sources(gencode_pattern, path_prefix="torch/csrc/"):
+    return [gencode_pattern.format(path_prefix + name) for name in [
+        "autograd/generated/Functions.cpp",
+        "autograd/generated/VariableType_0.cpp",
+        "autograd/generated/VariableType_1.cpp",
+        "autograd/generated/VariableType_2.cpp",
+        "autograd/generated/VariableType_3.cpp",
+        "autograd/generated/VariableType_4.cpp",
+        "autograd/generated/ViewFuncs.cpp",
+        "autograd/generated/TraceType_0.cpp",
+        "autograd/generated/TraceType_1.cpp",
+        "autograd/generated/TraceType_2.cpp",
+        "autograd/generated/TraceType_3.cpp",
+        "autograd/generated/TraceType_4.cpp",
+        "autograd/generated/ADInplaceOrViewType_0.cpp",
+        "autograd/generated/ADInplaceOrViewType_1.cpp",
     ]]
 
 # copied from https://github.com/pytorch/pytorch/blob/f99a693cd9ff7a9b5fdc71357dac66b8192786d3/aten/src/ATen/core/CMakeLists.txt