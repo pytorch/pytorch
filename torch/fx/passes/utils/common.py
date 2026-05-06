01da28fe9bbe29982d0dfd2f4cd2a3c482f2e58f
PyDevC <anuskcse@gmail.com>
2026-05-06 17:49:36 +0000

    fix docstrings

--- torch/fx/passes/utils/common.py
+++ torch/fx/passes/utils/common.py
@@ -29,16 +29,18 @@ def lift_subgraph_as_module(
     class_name: str = "GraphModule",
 ) -> tuple[GraphModule, dict[str, str]]:
     """
-    Create a GraphModule for subgraph, which copies the necessary attributes from the original parent graph_module.
+    Create a GraphModule for subgraph, which copies the necessary attributes 
+    from the original parent graph_module.
 
     Args:
-        gm (GraphModule): parent graph module
+        ``gm`` (GraphModule): parent graph module
 
-        subgraph (Graph): a valid subgraph that contains copied nodes from the parent graph
+        ``subgraph`` (:class:`torch.fx.Graph`): a valid subgraph that contains copied nodes from the 
+        parent graph
 
-        comp_name (str): name for the new component
+        ``comp_name`` (str): name for the new component
 
-        class_name (str): name for the submodule
+        ``class_name`` (str): name for the submodule
 
     """
 
