2e2b1855e317b99b8d5665db4a86cf1180122d7a
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-06 13:23:16 +0000

    Revert "[torch.library] Change default allow_override -> True (#182293)"
    This reverts commit ce306e0327779e6bfce86b8edba74cbf94fc1aa0.
    
    Reverted https://github.com/pytorch/pytorch/pull/182293 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/182293#issuecomment-4388445726))

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -1679,9 +1679,7 @@ class TestCustomOp(CustomOpTestCaseBase):
 
         with self.assertRaisesRegex(RuntimeError, r"test_custom_ops.py:\d+"):
 
-            @torch.library.register_fake(
-                f"{TestCustomOp.test_ns}::foo", lib=self.lib(), allow_override=False
-            )
+            @torch.library.register_fake(f"{TestCustomOp.test_ns}::foo", lib=self.lib())
             def foo_meta2(x, dim):
                 output_shape = list(x.shape)
                 del output_shape[dim]
@@ -1886,9 +1884,7 @@ Dynamic shape operator
         op = self.get_op(qualname)
 
         with self.assertRaisesRegex(RuntimeError, r"already has .*Meta implementation"):
-            torch.library.register_fake(
-                qualname, foo_impl, lib=self.lib(), allow_override=False
-            )
+            torch.library.register_fake(qualname, foo_impl, lib=self.lib())
 
     def test_abstract_impl_on_existing_op_with_CompositeImplicitAutograd(self):
         lib = self.lib()
@@ -2252,10 +2248,10 @@ Dynamic shape operator
             return torch.cat([x, x])
 
         with self.assertRaisesRegex(RuntimeError, "already a kernel registered"):
-            lib.impl("foo", foo_impl2, "CPU", allow_override=False)
+            lib.impl("foo", foo_impl2, "CPU")
 
         # Override cpu impl to foo_impl2
-        lib.impl(op_name, foo_impl2, "CPU")
+        lib.impl(op_name, foo_impl2, "CPU", allow_override=True)
         self.assertEqual(op(torch.ones(3)), torch.ones(6))
 
     def test_override_fake(self):
@@ -2278,19 +2274,19 @@ Dynamic shape operator
             return torch.cat([x, x])
 
         with self.assertRaisesRegex(RuntimeError, "already has an fake impl"):
-            torch.library.register_fake(
-                op_name, foo_impl2, lib=lib, allow_override=False
-            )
+            torch.library.register_fake(op_name, foo_impl2, lib=lib)
 
         # Override fake kernel to foo_impl2
-        torch.library.register_fake(op_name, foo_impl2, lib=lib)
+        torch.library.register_fake(op_name, foo_impl2, lib=lib, allow_override=True)
         with torch._subclasses.FakeTensorMode():
             self.assertEqual(op(torch.ones(3)).shape, [6])
         self.assertEqual(op(torch.ones(3, device="meta")).shape, [6])
 
         # Use scoped_library to temporarily register Fake kernel to foo_impl1
         with torch.library._scoped_library(self.test_ns, "FRAGMENT") as lib2:
-            torch.library.register_fake(op_name, foo_impl1, lib=lib2)
+            torch.library.register_fake(
+                op_name, foo_impl1, lib=lib2, allow_override=True
+            )
             with torch._subclasses.FakeTensorMode():
                 self.assertEqual(op(torch.ones(3)).shape, [3])
             self.assertEqual(op(torch.ones(3, device="meta")).shape, [3])
@@ -2317,22 +2313,22 @@ Dynamic shape operator
             return torch.cat([x, x])
 
         with self.assertRaisesRegex(RuntimeError, "already a kernel registered"):
-            lib.impl("foo", foo_impl2, "Meta", allow_override=False)
+            lib.impl("foo", foo_impl2, "Meta")
 
         # Override Meta kernel to foo_impl2
-        lib.impl("foo", foo_impl2, "Meta")
+        lib.impl("foo", foo_impl2, "Meta", allow_override=True)
         self.assertEqual(op(torch.ones(3, device="meta")).shape, [6])
 
         # Use scoped_library to temporarily register Meta kernel to foo_impl1
         with torch.library._scoped_library(self.test_ns, "FRAGMENT") as lib2:
-            lib2.impl("foo", foo_impl1, "Meta")
+            lib2.impl("foo", foo_impl1, "Meta", allow_override=True)
             self.assertEqual(op(torch.ones(3, device="meta")).shape, [3])
 
         # Meta kernel should go back to foo_impl2
         self.assertEqual(op(torch.ones(3, device="meta")).shape, [6])
 
         # Use register_fake to override Meta kernel to foo_impl1
-        torch.library.register_fake(op_name, foo_impl1, lib=lib)
+        torch.library.register_fake(op_name, foo_impl1, lib=lib, allow_override=True)
         self.assertEqual(op(torch.ones(3, device="meta")).shape, [3])
 
     def test_override_cea(self):
@@ -2354,7 +2350,7 @@ Dynamic shape operator
             return torch.cat([x, x])
 
         # Override Meta/fake kernel with foo_impl2
-        torch.library.register_fake(op_name, foo_impl2, lib=lib)
+        torch.library.register_fake(op_name, foo_impl2, lib=lib, allow_override=True)
         with torch._subclasses.FakeTensorMode():
             self.assertEqual(op(torch.ones(3)).shape, [6])
         self.assertEqual(op(torch.ones(3, device="meta")).shape, [6])
@@ -4580,6 +4576,7 @@ Please use `add.register_fake` to add an fake impl.""",
                     "arange.start",
                     partial(conditional_arange_cpu2, new_original_kernel),
                     "CPU",
+                    allow_override=True,
                     with_keyset=True,
                 )
 
