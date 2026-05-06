bf37da8da4ff8d5754f081cd34702eb0020d6a10
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-06 22:58:12 +0000

    Revert "[functorch] Fix grad + compile(dynamic=True) with stride slicing (#181572)"
    This reverts commit 52df0eb670d70e8b2b7951bd9b5b38e4492ca579.
    
    Reverted https://github.com/pytorch/pytorch/pull/181572 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/181572#issuecomment-4392803931))

--- test/functorch/test_eager_transforms.py
+++ test/functorch/test_eager_transforms.py
@@ -5403,25 +5403,6 @@ class TestCompileTransforms(TestCase):
         actual = opt_fn(params_and_buffers, x)
         self.assertEqual(actual, expected)
 
-    @parametrize("backend", ["eager", "aot_eager"])
-    def test_compile_dynamic_grad_stride_slice_mha(self, device, backend):
-        # Regression test for https://github.com/pytorch/pytorch/issues/181305
-        linear = nn.Linear(3, 3).to(device).eval()
-
-        def model(x):
-            y = x[:, ::2, :, :]
-            y = y.mean(dim=0)
-            y = linear(y)
-            return y.mean()
-
-        x = torch.randn(4, 8, 2, 3, device=device)
-        expected = grad(model)(x)
-
-        torch._dynamo.reset()
-        compiled = torch.compile(grad(model), dynamic=True, backend=backend)
-        result = compiled(x)
-        self.assertEqual(result, expected)
-
     # torch.compile is not supported on Windows
     @torch._dynamo.config.patch(suppress_errors=False)
     def test_grad_deprecated_api(self, device):
