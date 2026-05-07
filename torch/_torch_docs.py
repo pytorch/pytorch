4af124fe7e1dbbfe8d56f1ef6fc4a0081bc48991
Raj Kumar Satya <rajkumarrapeti@gmail.com>
2026-05-07 03:57:58 -0500

    Fix torch.trapz documentation signature

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -13175,7 +13175,7 @@ Examples::
 add_docstr(
     torch.trapz,
     r"""
-trapz(y, x=None, *, dim=-1) -> Tensor
+trapz(y, x=None, *, dx=None, dim=-1) -> Tensor
 
 Alias for :func:`torch.trapezoid`.
 """,
