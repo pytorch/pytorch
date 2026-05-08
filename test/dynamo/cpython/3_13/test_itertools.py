c7b3e3fade8b3c7d7f57b5b19f88d42c8fb084aa
Guilherme Leobas <guilhermeleobas@gmail.com>
2026-05-08 15:27:14 +0000

    [Dynamo] Remove `error_on_graph_break` from CPython tests (part 3) (#182353)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/182353
    Approved by: https://github.com/anijain2305
    ghstack dependencies: #181244, #182351, #182352

--- test/dynamo/cpython/3_13/test_itertools.py
+++ test/dynamo/cpython/3_13/test_itertools.py
@@ -1052,15 +1052,13 @@ class TestBasicOps(__TestCase):
         self.assertEqual(list(filterfalse(None, [0,1,0,2,0])), [0,0,0])
         self.assertEqual(list(filterfalse(bool, [0,1,0,2,0])), [0,0,0])
         self.assertEqual(take(4, filterfalse(isEven, count())), [1,3,5,7])
-        with torch._dynamo.error_on_graph_break(False):
-            self.assertRaises(TypeError, filterfalse)
-            self.assertRaises(TypeError, filterfalse, lambda x:x)
-            self.assertRaises(TypeError, filterfalse, lambda x:x, range(6), 7)
+        self.assertRaises(TypeError, filterfalse)
+        self.assertRaises(TypeError, filterfalse, lambda x:x)
+        self.assertRaises(TypeError, filterfalse, lambda x:x, range(6), 7)
         self.assertRaises(TypeError, filterfalse, isEven, 3)
-        with torch._dynamo.error_on_graph_break(False):
-            self.assertRaises(TypeError, next, filterfalse(range(6), range(6)))
-            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-                self.pickletest(proto, filterfalse(isEven, range(6)))
+        self.assertRaises(TypeError, next, filterfalse(range(6), range(6)))
+        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+            self.pickletest(proto, filterfalse(isEven, range(6)))
 
     def test_zip(self):
         # XXX This is rather silly now that builtin zip() calls zip()...
@@ -1359,8 +1357,7 @@ class TestBasicOps(__TestCase):
         argtypes = ['', 'abc', '', range(0), range(4), dict(a=1, b=2, c=3),
                     set('abcdefg'), range(11), tuple(range(13))]
         for i in range(100):
-            with torch._dynamo.error_on_graph_break(False):
-                args = [choice(argtypes) for j in range(randrange(5))]
+            args = [choice(argtypes) for j in range(randrange(5))]
             expected_len = prod(map(len, args))
             self.assertEqual(len(list(product(*args))), expected_len)
             self.assertEqual(list(product(*args)), list(product1(*args)))
@@ -1597,12 +1594,11 @@ class TestBasicOps(__TestCase):
         data = [1, 3, 5, 20, 2, 4, 6, 8]
         self.assertEqual(list(takewhile(underten, data)), [1, 3, 5])
         self.assertEqual(list(takewhile(underten, [])), [])
-        with torch._dynamo.error_on_graph_break(False):
-            self.assertRaises(TypeError, takewhile)
-            self.assertRaises(TypeError, takewhile, operator.pow)
-            self.assertRaises(TypeError, takewhile, operator.pow, [(4,5)], 'extra')
-            self.assertRaises(TypeError, next, takewhile(10, [(4,5)]))
-            # self.assertRaises(ValueError, next, takewhile(errfunc, [(4,5)]))
+        self.assertRaises(TypeError, takewhile)
+        self.assertRaises(TypeError, takewhile, operator.pow)
+        self.assertRaises(TypeError, takewhile, operator.pow, [(4,5)], 'extra')
+        self.assertRaises(TypeError, next, takewhile(10, [(4,5)]))
+        # self.assertRaises(ValueError, next, takewhile(errfunc, [(4,5)]))
         t = takewhile(bool, [1, 1, 1, 0, 0, 0])
         self.assertEqual(list(t), [1, 1, 1])
         self.assertRaises(StopIteration, next, t)
@@ -1619,11 +1615,10 @@ class TestBasicOps(__TestCase):
         data = [1, 3, 5, 20, 2, 4, 6, 8]
         self.assertEqual(list(dropwhile(underten, data)), [20, 2, 4, 6, 8])
         self.assertEqual(list(dropwhile(underten, [])), [])
-        with torch._dynamo.error_on_graph_break(False):
-            self.assertRaises(TypeError, dropwhile)
-            self.assertRaises(TypeError, dropwhile, operator.pow)
-            self.assertRaises(TypeError, dropwhile, operator.pow, [(4,5)], 'extra')
-            self.assertRaises(TypeError, next, dropwhile(10, [(4,5)]))
+        self.assertRaises(TypeError, dropwhile)
+        self.assertRaises(TypeError, dropwhile, operator.pow)
+        self.assertRaises(TypeError, dropwhile, operator.pow, [(4,5)], 'extra')
+        self.assertRaises(TypeError, next, dropwhile(10, [(4,5)]))
         #self.assertRaises(ValueError, next, dropwhile(errfunc, [(4,5)]))
         #
         ## check copy, deepcopy, pickle
