a9e40607279e67f0978324f87b8559ccf9ea1187
Tijana Vukovic <tvukovic@amd.com>
2026-05-11 14:10:59 -0700

    Fix Windows DLL export macros for ATen native headers

--- aten/src/ATen/native/mkldnn/Linear.h
+++ aten/src/ATen/native/mkldnn/Linear.h
@@ -6,7 +6,7 @@
 #if AT_MKLDNN_ENABLED()
 
 namespace at::native {
-C10_API Tensor mkldnn_linear_pointwise(
+TORCH_API Tensor mkldnn_linear_pointwise(
     const Tensor& input_t,
     const Tensor& weight_t,
     const std::optional<Tensor>& bias_opt,
@@ -14,7 +14,7 @@ C10_API Tensor mkldnn_linear_pointwise(
     c10::List<std::optional<at::Scalar>> scalars,
     std::optional<std::string_view> algorithm);
 
-C10_API Tensor mkldnn_linear_pointwise_binary(
+TORCH_API Tensor mkldnn_linear_pointwise_binary(
     const Tensor& input_t,
     const Tensor& other_t,
     const Tensor& weight_t,
