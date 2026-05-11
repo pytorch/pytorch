89782b13b9829e5500fd6848c6f0d431cf20c684
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-05-11 09:15:34 +0000

    [BE][Ez]: Add missing typing vars for decorators. Prevent type erasure (#183116)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/183116
    Approved by: https://github.com/weifengpy

--- torch/_jit_internal.py
+++ torch/_jit_internal.py
@@ -1044,7 +1044,7 @@ def _check_overload_body(func):
         raise RuntimeError(msg)
 
 
-def _overload(func):
+def _overload(func: Callable[_P, _R]) -> Callable[_P, _R]:
     _check_overload_body(func)
     qual_name = _qualified_name(func)
     global _overloaded_fns
@@ -1097,7 +1097,7 @@ _overloaded_methods: dict[str, dict[str, list[Callable]]] = {}  # noqa: T484
 _overloaded_method_class_fileno: dict[tuple[str, str], int] = {}
 
 
-def _overload_method(func):
+def _overload_method(func: Callable[_P, _R]) -> Callable[_P, _R]:
     try:
         _check_overload_body(func)
     except IndentationError:
