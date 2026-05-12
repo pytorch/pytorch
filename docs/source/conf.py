e86f80c187d401065b17dc8f945ea00a4f9921d4
Nazim <nazimfadli25@gmail.com>
2026-05-12 03:10:26 +0200

    Delete commented-out entries from coverage_ignore_functions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -529,18 +529,11 @@ coverage_ignore_functions = [
     # torch.distributed.rendezvous
     "rendezvous",
     # torch.distributed.rpc.api
-    # "get_worker_info",
     "method_factory",
     "new_method",
-    # "remote",
-    # "rpc_async",
-    # "rpc_sync",
-    # "shutdown",
     # torch.distributed.rpc.backend_registry
-    # "backend_registered",
     "construct_rpc_backend_options",
     "init_backend",
-    # "register_backend",
     # torch.distributed.rpc.internal
     "deserialize",
     "serialize",
