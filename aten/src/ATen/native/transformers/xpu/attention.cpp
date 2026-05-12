2a69a3e7b10541cf30c7687ed4e9b6739771e5e5
fengqing.lu <fengqing.lu@intel.com>
2026-05-12 02:30:46 -0700

    enable _flash_attention_forward/_flash_attention_backward for xpu

--- aten/src/ATen/native/transformers/xpu/attention.cpp
+++ aten/src/ATen/native/transformers/xpu/attention.cpp
@@ -5,6 +5,45 @@
 namespace at {
 namespace native {
 
+std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor> _flash_attention_forward_xpu(
+    const Tensor& query,
+    const Tensor& key,
+    const Tensor& value,
+    const std::optional<Tensor>& cumulative_sequence_length_q,
+    const std::optional<Tensor>& cumulative_sequence_length_k,
+    int64_t max_seqlen_batch_q,
+    int64_t max_seqlen_batch_k,
+    double dropout_p,
+    bool is_causal,
+    bool return_debug_mask,
+    std::optional<double> scale,
+    std::optional<int64_t> window_size_left,
+    std::optional<int64_t> window_size_right,
+    const std::optional<Tensor>& _seqused_k,
+    const std::optional<Tensor>& _alibi_slopes,
+    const std::optional<Tensor>& _block_table,
+    std::optional<int64_t> num_splits) {
+  return sycltla::_flash_attention_forward(
+      query,
+      key,
+      value,
+      cumulative_sequence_length_q,
+      cumulative_sequence_length_k,
+      max_seqlen_batch_q,
+      max_seqlen_batch_k,
+      dropout_p,
+      is_causal,
+      return_debug_mask,
+      scale,
+      window_size_left,
+      window_size_right,
+      _seqused_k,
+      _alibi_slopes,
+      _block_table,
+      /*out=*/std::nullopt,
+      num_splits);
+}
+
 std::tuple<
     Tensor,
     Tensor,
@@ -23,34 +62,102 @@ _scaled_dot_product_flash_attention_xpu(
     bool is_causal,
     bool return_debug_mask,
     std::optional<double> scale) {
-  auto
-      [attention,
-       logsumexp,
-       cumulative_sequence_length_q,
-       cumulative_sequence_length_k,
-       max_seqlen_batch_q,
-       max_seqlen_batch_k,
-       philox_seed,
-       philox_offset] =
-          sycltla::flash_attention_forward(
-              query,
-              key,
-              value,
-              dropout_p,
-              is_causal,
-              scale.has_value() ? scale.value()
-                                : (1.0 / std::sqrt(query.size(3))));
+  // Used for tracking usage statistics
+  C10_LOG_API_USAGE_ONCE("torch.sdpa.flash_attention");
+  // Query (Batch x Num_heads x Q_seq_len  x Dim_per_head)
+  // Key   (Batch x Num_heads x KV_seq_len x Dim_per_head)
+  // Value (Batch x Num_heads x KV_seq_len x Dim_per_head)
+
+  const int64_t max_seqlen_batch_q = query.size(2);
+  const int64_t max_seqlen_batch_k = key.size(2);
+  const int64_t max_seqlen_batch_v = value.size(2);
+  TORCH_CHECK(
+      max_seqlen_batch_k == max_seqlen_batch_v,
+      "Key and Value must have the same sequence length");
+
+  // Query -> Query(Batch x Q_seq_len  x Num_heads x Dim_per_head)
+  // Key   -> Key  (Batch x KV_seq_len x Num_heads x Dim_per_head)
+  // Value -> Value(Batch x KV_seq_len x Num_heads x Dim_per_head)
+  Tensor q_t = query.transpose(1, 2);
+  Tensor k_t = key.transpose(1, 2);
+  Tensor v_t = value.transpose(1, 2);
+
+  auto [output, logsumexp, philox_seed, philox_offset, debug_attn_mask] =
+      at::_flash_attention_forward(
+          q_t,
+          k_t,
+          v_t,
+          std::nullopt,
+          std::nullopt,
+          max_seqlen_batch_q,
+          max_seqlen_batch_k,
+          dropout_p,
+          is_causal,
+          return_debug_mask,
+          scale,
+          std::nullopt,
+          std::nullopt);
+  // Reshape output to convert nnz to batch_size and seq_len
+  Tensor attention = output.transpose(1, 2);
+
   return std::make_tuple(
-      attention,
-      logsumexp,
-      cumulative_sequence_length_q,
-      cumulative_sequence_length_k,
+      std::move(attention),
+      std::move(logsumexp),
+      Tensor(),
+      Tensor(),
       max_seqlen_batch_q,
       max_seqlen_batch_k,
-      philox_seed,
-      philox_offset,
-      /* debug_attn_mask */ at::empty({0}, query.options()));
+      std::move(philox_seed),
+      std::move(philox_offset),
+      std::move(debug_attn_mask));
 }
+
+// std::tuple<
+//     Tensor,
+//     Tensor,
+//     Tensor,
+//     Tensor,
+//     c10::SymInt,
+//     c10::SymInt,
+//     Tensor,
+//     Tensor,
+//     Tensor>
+// _scaled_dot_product_flash_attention_xpu(
+//     const Tensor& query,
+//     const Tensor& key,
+//     const Tensor& value,
+//     double dropout_p,
+//     bool is_causal,
+//     bool return_debug_mask,
+//     std::optional<double> scale) {
+//   auto
+//       [attention,
+//        logsumexp,
+//        cumulative_sequence_length_q,
+//        cumulative_sequence_length_k,
+//        max_seqlen_batch_q,
+//        max_seqlen_batch_k,
+//        philox_seed,
+//        philox_offset] =
+//           sycltla::flash_attention_forward(
+//               query,
+//               key,
+//               value,
+//               dropout_p,
+//               is_causal,
+//               scale.has_value() ? scale.value()
+//                                 : (1.0 / std::sqrt(query.size(3))));
+//   return std::make_tuple(
+//       attention,
+//       logsumexp,
+//       cumulative_sequence_length_q,
+//       cumulative_sequence_length_k,
+//       max_seqlen_batch_q,
+//       max_seqlen_batch_k,
+//       philox_seed,
+//       philox_offset,
+//       /* debug_attn_mask */ at::Tensor());
+// }
 
 } // namespace native
 } // namespace at
--- aten/src/ATen/native/transformers/xpu/attention_backward.cpp
+++ aten/src/ATen/native/transformers/xpu/attention_backward.cpp
@@ -5,28 +5,25 @@
 namespace at {
 namespace native {
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor>
-_scaled_dot_product_flash_attention_backward_xpu(
-    const at::Tensor& grad_out,
-    const at::Tensor& query,
-    const at::Tensor& key,
-    const at::Tensor& value,
-    const at::Tensor& out,
-    const at::Tensor& logsumexp,
-    const at::Tensor& cumulative_sequence_length_q,
-    const at::Tensor& cumulative_sequence_length_k,
-    const int64_t max_seqlen_batch_q,
-    const int64_t max_seqlen_batch_k,
+std::tuple<Tensor, Tensor, Tensor> _flash_attention_backward_xpu(
+    const Tensor& grad_out,
+    const Tensor& query,
+    const Tensor& key,
+    const Tensor& value,
+    const Tensor& out,
+    const Tensor& logsumexp,
+    const Tensor& cumulative_sequence_length_q,
+    const Tensor& cumulative_sequence_length_k,
+    int64_t max_seqlen_batch_q,
+    int64_t max_seqlen_batch_k,
     double dropout_p,
     bool is_causal,
-    const at::Tensor& philox_seed,
-    const at::Tensor& philox_offset,
-    std::optional<double> scale) {
-  if (!grad_out.defined()) {
-    return std::make_tuple(Tensor{}, Tensor{}, Tensor{});
-  }
-
-  auto [grad_q, grad_k, grad_v] = sycltla::flash_attention_backward(
+    const Tensor& philox_seed,
+    const Tensor& philox_offset,
+    std::optional<double> scale,
+    std::optional<int64_t> window_size_left,
+    std::optional<int64_t> window_size_right) {
+  return sycltla::_flash_attention_backward(
       grad_out,
       query,
       key,
@@ -41,11 +38,105 @@ _scaled_dot_product_flash_attention_backward_xpu(
       is_causal,
       philox_seed,
       philox_offset,
-      scale.has_value() ? scale.value() : (1.0 / std::sqrt(query.size(3))));
+      scale,
+      window_size_left,
+      window_size_right);
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor>
+_scaled_dot_product_flash_attention_backward_xpu(
+    const at::Tensor& grad_out_,
+    const at::Tensor& query,
+    const at::Tensor& key,
+    const at::Tensor& value,
+    const at::Tensor& out,
+    const at::Tensor& logsumexp,
+    const at::Tensor& cumulative_sequence_length_q,
+    const at::Tensor& cumulative_sequence_length_k,
+    const int64_t max_seqlen_batch_q,
+    const int64_t max_seqlen_batch_k,
+    double dropout_p,
+    bool is_causal,
+    const at::Tensor& philox_seed,
+    const at::Tensor& philox_offset,
+    std::optional<double> scale) {
+  if (!grad_out_.defined()) {
+    return std::make_tuple(Tensor{}, Tensor{}, Tensor{});
+  }
+
+  Tensor q_t = query.transpose(1, 2);
+  Tensor k_t = key.transpose(1, 2);
+  Tensor v_t = value.transpose(1, 2);
+
+  Tensor grad_out_t = grad_out_.transpose(1, 2);
+  Tensor out_t = out.transpose(1, 2);
+
+  auto [grad_q, grad_k, grad_v] = at::_flash_attention_backward(
+      grad_out_t,
+      q_t,
+      k_t,
+      v_t,
+      out_t,
+      logsumexp,
+      cumulative_sequence_length_q,
+      cumulative_sequence_length_k,
+      max_seqlen_batch_q,
+      max_seqlen_batch_k,
+      dropout_p,
+      is_causal,
+      philox_seed,
+      philox_offset,
+      scale);
+
+  grad_q = grad_q.transpose(1, 2);
+  grad_k = grad_k.transpose(1, 2);
+  grad_v = grad_v.transpose(1, 2);
 
   return std::make_tuple(
       std::move(grad_q), std::move(grad_k), std::move(grad_v));
 }
+
+// std::tuple<at::Tensor, at::Tensor, at::Tensor>
+// _scaled_dot_product_flash_attention_backward_xpu(
+//     const at::Tensor& grad_out,
+//     const at::Tensor& query,
+//     const at::Tensor& key,
+//     const at::Tensor& value,
+//     const at::Tensor& out,
+//     const at::Tensor& logsumexp,
+//     const at::Tensor& cumulative_sequence_length_q,
+//     const at::Tensor& cumulative_sequence_length_k,
+//     const int64_t max_seqlen_batch_q,
+//     const int64_t max_seqlen_batch_k,
+//     double dropout_p,
+//     bool is_causal,
+//     const at::Tensor& philox_seed,
+//     const at::Tensor& philox_offset,
+//     std::optional<double> scale) {
+//   if (!grad_out.defined()) {
+//     return std::make_tuple(Tensor{}, Tensor{}, Tensor{});
+//   }
+
+//   auto [grad_q, grad_k, grad_v] = sycltla::flash_attention_backward(
+//       grad_out,
+//       query,
+//       key,
+//       value,
+//       out,
+//       logsumexp,
+//       cumulative_sequence_length_q,
+//       cumulative_sequence_length_k,
+//       max_seqlen_batch_q,
+//       max_seqlen_batch_k,
+//       dropout_p,
+//       is_causal,
+//       philox_seed,
+//       philox_offset,
+//       scale.has_value() ? scale.value() : (1.0 / std::sqrt(query.size(3))));
+
+//   return std::make_tuple(
+//       std::move(grad_q), std::move(grad_k), std::move(grad_v));
+// }
 
 } // namespace native
 } // namespace at
