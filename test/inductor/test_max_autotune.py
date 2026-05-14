420028eac9f96acdc566f512d03abbbf8a14a782
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-14 16:01:31 +0000

    Revert "[Inductor][Refactor] 3/N Move pointwise heuristics from triton_heuristics.py into triton_codegen/ (#183277)"
    This reverts commit ab7cd65b39cf8bc58c50e7c87e14b1f610900716.
    
    Reverted https://github.com/pytorch/pytorch/pull/183277 on behalf of https://github.com/georgehong due to reverting due to needing to revert PR https://github.com/pytorch/pytorch/pull/183275 due to breaking internal sync jobs, and this has a merge conflict. All this PR should need to do is rebase and remerge ([comment](https://github.com/pytorch/pytorch/pull/183277#issuecomment-4452340547))

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -187,8 +187,7 @@ class TestMaxAutotune(TestCase):
         Verify that `max_autotune` includes all pointwise configs from
         `max_autotune_pointwise` for 1D, 2D, and 3D pointwise kernels.
         """
-
-        triton_meta = {"device": torch.device(GPU_TYPE)}
+        triton_meta = {"device": object()}
         inductor_meta_common = {"autotune_pointwise": False}
 
         for size_hints in (
