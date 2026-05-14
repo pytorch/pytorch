pytorch/pytorch

commit a3d446b47e513258036e0be66bcb550787b634e6

[BE][Ez]: Make more dataclass slots=True (#183449)
Reopens an old dataclass slots PR that failed due to unrelated flakes. Reopen of #172173 due to failed rebase
Pull Request resolved: https://github.com/pytorch/pytorch/pull/183449
Approved by: https://github.com/malfet

torch/_subclasses/meta_utils.py
@@ -519,7 +519,7 @@ class MetaTensorDescriber:
         return r
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class MetaStorageDesc:
     id: MetaStorageId
     size: int
@@ -535,7 +535,7 @@ class MetaStorageDesc:
         }
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class ViewFunc(Generic[_TensorT]):
     @abstractmethod
     def apply(
@@ -554,7 +554,7 @@ class ViewFunc(Generic[_TensorT]):
             return _CustomViewFunc(t._view_func_unsafe)
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class _FakeTensorViewFunc(ViewFunc["FakeTensor"]):
     @override
     def apply(
@@ -573,7 +573,7 @@ class _FakeTensorViewFunc(ViewFunc["FakeTensor"]):
         )
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class _CustomViewFunc(ViewFunc[_TensorT], Generic[_TensorT]):
     func: Callable[
         [
@@ -624,7 +624,7 @@ class _MetaTensorCallbackOptDevice(Protocol, Generic[_TensorT_cov]):
     ) -> _TensorT_cov: ...
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class MetaTensorDesc(Generic[_TensorT]):
     id: MetaTensorId
     ndim: int

commit 99861a2cb7ab3828b7b400b2c863a51c8330d28b

fix internal tests for [BE][Ez]: Make more dataclass slots=True (#183449)
Summary:
As title, addresses internal build/test issues introduced by https://github.com/pytorch/pytorch/pull/183449.

Maintains backward compatibility for serialization.

Test Plan: CI passes.

Differential Revision: D105233897

torch/_subclasses/meta_utils.py
@@ -527,6 +527,21 @@ class MetaStorageDesc:
     # serializable in JSON, you want to do something special here anyway
     data: torch.UntypedStorage | None
 
+    def __setstate__(self, state: object) -> None:
+        if isinstance(state, tuple):
+            _, slots_state = state
+            state_dict = slots_state or {}
+        else:
+            state_dict = state
+        for key, value in state_dict.items():
+            object.__setattr__(self, key, value)
+        for f in dataclasses.fields(self):
+            if f.name not in state_dict:
+                if f.default is not dataclasses.MISSING:
+                    object.__setattr__(self, f.name, f.default)
+                elif f.default_factory is not dataclasses.MISSING:
+                    object.__setattr__(self, f.name, f.default_factory())
+
     def as_json(self, describer_id: _DescriberId) -> dict[str, object]:
         return {
             "id": self.id,
@@ -736,6 +751,21 @@ class MetaTensorDesc(Generic[_TensorT]):
     # However, this use of functorch is very "non-lexical" so it's not
     # entirely clear how to make it all lexical again, so we haven't done
     # it for now.
+
+    def __setstate__(self, state: object) -> None:
+        if isinstance(state, tuple):
+            _, slots_state = state
+            state_dict = slots_state or {}
+        else:
+            state_dict = state
+        for key, value in state_dict.items():
+            object.__setattr__(self, key, value)
+        for f in dataclasses.fields(self):
+            if f.name not in state_dict:
+                if f.default is not dataclasses.MISSING:
+                    object.__setattr__(self, f.name, f.default)
+                elif f.default_factory is not dataclasses.MISSING:
+                    object.__setattr__(self, f.name, f.default_factory())
 
     # NB: This will reference numeric IDs, and it is assumed that you've
     # already serialized everything this recursively references