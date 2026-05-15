f510f23228d7078299d9b85fa734b1c5fd9f2236
Yu, Guangye <guangye.yu@intel.com>
2026-05-15 11:27:29 +0800

    Apply suggestions from code review
    Co-authored-by: Yu, Guangye <guangye.yu@intel.com>

--- test/xpu/test_gemm.py
+++ test/xpu/test_gemm.py
@@ -330,18 +330,25 @@ class TestBasicGEMM(TestCase):
         else:
             alpha = 6
             beta = 3
-        self.assertEqual(torch.full((2,), beta * value, dtype=dtype, device=device),
-                         torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta))
-        self.assertEqual(torch.full((2,), beta * value, dtype=dtype, device=device),
-                         torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta, out=out))
+        self.assertEqual(
+            torch.full((2,), beta * value, dtype=dtype, device=device),
+            torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta)
+        )
+        self.assertEqual(
+            torch.full((2,), beta * value, dtype=dtype, device=device),
+            torch.addmv(input=input, mat=mat, vec=vec, alpha=alpha, beta=beta, out=out)
+        )
 
         input = torch.full((2, 3), value, dtype=dtype, device=device)
         mat2 = torch.ones((0, 3), dtype=dtype, device=device)
         out = torch.empty((2, 3), dtype=dtype, device=device)
-        self.assertEqual(torch.full((2, 3), beta * value, dtype=dtype, device=device),
-                         torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta))
-        self.assertEqual(torch.full((2, 3), beta * value, dtype=dtype, device=device),
-                         torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta, out=out))
+        self.assertEqual(
+            torch.full((2, 3), beta * value, dtype=dtype, device=device),
+            torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta))
+        self.assertEqual(
+            torch.full((2, 3), beta * value, dtype=dtype, device=device),
+            torch.addmm(input=input, mat1=mat, mat2=mat2, alpha=alpha, beta=beta, out=out)
+        )
 
     @dtypes(
         torch.half,
