e98e8f8ef8ddc04f86684aa70b6ee9185091af1e
Ivor Wanders <ivor@iwanders.net>
2026-05-16 09:12:49 -0400

    Use suppression file from aten/tools.

--- test/cpp/shim/run_test.sh
+++ test/cpp/shim/run_test.sh
@@ -4,6 +4,11 @@
 VALGRIND=${VALGRIND:=ON}
 export CPP_TESTS_DIR=$1
 if [ "$VALGRIND" == "ON" ]; then
+
+    # Use the aten/tools/valgrind.sup valgrind suppression file.
+    SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    SUPPRESSION_FILE="${SCRIPT_DIR}/../../../aten/tools/valgrind.sup"
+
     # --leak-check=full is necessary to return 1 in case of definite and possible leaks.
-    valgrind   --error-exitcode=1  --leak-check=full ${CPP_TESTS_DIR}/test_shim
+    valgrind --suppressions="${SUPPRESSION_FILE}" --error-exitcode=1  --leak-check=full ${CPP_TESTS_DIR}/test_shim
 fi
