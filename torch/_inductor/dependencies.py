af67c6651ea7d466ac1aea67680ea6ffd990d857
Allan Lin <allin@redhat.com>
2026-05-16 04:43:41 +0000

    Remove unused variable in _inductor/dependencies.py (#183817)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/183817
    Approved by: https://github.com/cyyever, https://github.com/jansel

--- torch/_inductor/dependencies.py
+++ torch/_inductor/dependencies.py
@@ -648,10 +648,8 @@ def index_vars_squeeze(
 
     var_ranges, add_var = var_builder(prefix)
     args: list[Sequence[sympy.Expr]] = []
-    new_sizes: list[Sequence[sympy.Expr]] = []
     for size in argsizes:
         new_size, reindex = SqueezeView.squeezer(size)
-        new_sizes.append(new_size)
         args.append(reindex(list(map(add_var, new_size))))
     return args, var_ranges
 
