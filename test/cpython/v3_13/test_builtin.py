a3b29307d9549f3503d6e45a107c4281a455807a
Animesh Jain <anijain@umich.edu>
2026-05-16 21:56:17 -0700

    Update
    [ghstack-poisoned]

--- test/cpython/v3_13/test_builtin.py
+++ test/cpython/v3_13/test_builtin.py
@@ -2496,35 +2496,29 @@ class PtyTests(CPythonTestCase):
         else:
             yield
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty(self):
         # Test input() functionality when wired to a tty
         self.check_input_tty("prompt", b"quux")
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty_non_ascii(self):
         # Check stdin/stdout encoding is used when invoking PyOS_Readline()
         self.check_input_tty("prompté", b"quux\xc3\xa9", "utf-8")
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty_non_ascii_unicode_errors(self):
         # Check stdin/stdout error handler is used when invoking PyOS_Readline()
         self.check_input_tty("prompté", b"quux\xe9", "ascii")
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty_null_in_prompt(self):
         self.check_input_tty("prompt\0", b"",
                 expected='ValueError: input: prompt string cannot contain '
                          'null characters')
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty_nonencodable_prompt(self):
         self.check_input_tty("prompté", b"quux", "ascii", stdout_errors='strict',
                 expected="UnicodeEncodeError: 'ascii' codec can't encode "
                          "character '\\xe9' in position 6: ordinal not in "
                          "range(128)")
 
-    @torch._dynamo.error_on_graph_break(False)
     def test_input_tty_nondecodable_input(self):
         self.check_input_tty("prompt", b"quux\xe9", "ascii", stdin_errors='strict',
                 expected="UnicodeDecodeError: 'ascii' codec can't decode "
