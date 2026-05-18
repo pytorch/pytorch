bd1f26c2fea61724dbb390a40256decaad011256
Jason Ansel <jansel@meta.com>
2026-05-17 19:21:23 -0700

    Update
    [ghstack-poisoned]

--- test/inductor/test_embedding.py
+++ test/inductor/test_embedding.py
@@ -2,6 +2,7 @@
 
 import torch
 from torch._inductor import config
+from torch._inductor.shape_propagation import get_broadcasted_shape
 from torch._inductor.test_case import run_tests, TestCase
 
 
@@ -35,6 +36,12 @@ class TestEmbedding(TestCase):
 
         self.assertEqual(expected, actual)
         self.assertEqual(actual, torch.cat((torch.ones(4, 1), torch.zeros(4, 1))))
+
+    def test_embedding_block_shape_assert_accepts_list_shape(self):
+        self.assertEqual(
+            get_broadcasted_shape(["XBLOCK", "1"], ("1", "RBLOCK")),
+            ("XBLOCK", "RBLOCK"),
+        )
 
 
 if __name__ == "__main__":
