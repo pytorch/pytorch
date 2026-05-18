14c541baa32b24de8214ea8b2444e8cd33a7015a
Joshua James Venter <venter.joshua@gmail.com>
2026-05-18 20:23:05 +0200

    raise
    Signed-off-by: Joshua James Venter <venter.joshua@gmail.com>

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -7234,7 +7234,10 @@ class FusedUserDefinedTritonKernel(TritonKernel):
                 shape=value.shape,
             )
         else:
-            super().store(name, index, value, mode)
+            raise AssertionError(
+                f"Epilogue attempted to store from '{name}'. "
+                "Inductor indexing variables are not defined in user kernel scope. "
+            )
 
     # returns a str which is the src code of a modified version of the user kernel that includes the epilogues
     def codegen(self) -> str:
