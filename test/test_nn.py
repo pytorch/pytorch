6fe15f27135cc89c74f7b465db8c8a85a43db7ab
Glen Cao <glen.cao@amd.com>
2026-05-18 14:54:35 -0700

    Fixed `NameError: name IS_CPU_CAPABILITY_SVE256 is not defined`

--- test/test_nn.py
+++ test/test_nn.py
@@ -6848,7 +6848,7 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""")
         expected_out_t = torch.tensor([[[[2.5]]]])
         self.assertEqual(expected_out_t, out_t)
 
-    @xfailIf(IS_ARM64 and IS_CPU_EXT_SVE_SUPPORTED and not IS_CPU_CAPABILITY_SVE256)
+    @xfailIf(IS_ARM64 and IS_CPU_EXT_SVE_SUPPORTED and not IS_CPU_CAPABILITY_SVE)
     # see https://github.com/pytorch/pytorch/issues/177250
     def test_upsampling_bfloat16(self, dtype=torch.bfloat16):
         def helper(size, scale_factor, mode, device, memory_format=torch.contiguous_format):
