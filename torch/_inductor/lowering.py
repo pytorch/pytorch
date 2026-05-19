5d60f4f20a9b509ac64d8402cec945550c77d42a
Ali Raza <araza@redhat.com>
2026-05-19 19:18:48 +0000

    Remove make_fallback for adaptive_max_pool3d since decomposition exists
    Signed-off-by: Ali Raza <araza@redhat.com>

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -3461,7 +3461,6 @@ def sdpa_constraint(fx_node, *args, **kwargs):
 
 # WIP
 make_fallback(aten._adaptive_avg_pool3d)  # @isuruf
-make_fallback(aten.adaptive_max_pool3d)  # @isuruf
 make_fallback(aten._scaled_dot_product_attention_math_for_mps)  # @malfet
 
 
