7b4f9d58e35f94648e7c29a8c9739be527cba23c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-20 02:50:04 +0000

    Revert "[S658886] Revert D96769355 cublasLt workspace size change (#183923)"
    This reverts commit cd89b0d3bdd356f8a74cd80c8b2451f783e7ef37.
    
    Reverted https://github.com/pytorch/pytorch/pull/183923 on behalf of https://github.com/facebook-github-tools due to Diff reverted internally ([comment](https://github.com/pytorch/pytorch/pull/183923#issuecomment-4494052698))

--- aten/src/ATen/cuda/CublasHandlePool.cpp
+++ aten/src/ATen/cuda/CublasHandlePool.cpp
@@ -228,7 +228,9 @@ size_t parseCUDABlasLtWorkspaceSize() {
   }
   size_t workspace_size = 76*1024; /* Use 76 MB for hipBLASLt */
 #else
-  size_t workspace_size = 1024; /* default size in KiB according to #73328 */
+  /* use CUDABlas default workspace size if unified */
+  /* otherwise, use default size in KiB according to #73328 */
+  size_t workspace_size = unified_cublas_and_lt_workspaces() ? parseChosenWorkspaceSize() / 1024 : 1024;
 #endif
 
   if (val.has_value()) {
