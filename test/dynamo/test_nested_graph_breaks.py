f3c667d084cef823fe486ce59dea3f9191152c15
William Wen <williamwen@meta.com>
2026-05-19 17:32:50 -0700

    [dynamo, nested graph breaks] trace through disable_nested_graph_breaks, disable nested graph breaks on inner torch.compile (#177931)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/177931
    Approved by: https://github.com/Lucaskabela, https://github.com/mlazos

--- test/dynamo/test_nested_graph_breaks.py
+++ test/dynamo/test_nested_graph_breaks.py
@@ -966,6 +966,52 @@ class NestedGraphBreakTests(torch._dynamo.test_case.TestCase):
         self.assertEqual(cnts.frame_count, 8)
         self.assertEqual(cnts.op_count, 10)
 
+    def test_disable_nested_graph_breaks_context_manager(self):
+        """disable_nested_graph_breaks as a context manager."""
+        global f1, f2, f3
+
+        def f1(x):
+            x = x + 1
+            torch._dynamo.graph_break()
+            return x + 2
+
+        def f2(x):
+            return f1(x + 4) + 8
+
+        def f3(x):
+            with torch._dynamo.disable_nested_graph_breaks():
+                x = f2(x + 16) + 32
+            return x + 64
+
+        cnts = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(backend=cnts)(f3)
+        x = torch.zeros(3)
+        res = f3(x)
+        ref = opt_fn(x)
+        self.assertEqual(ref, res)
+        self.assertEqual(cnts.frame_count, 6)
+
+    def test_recursive_compiled_fn_with_graph_break(self):
+        """Recursive compiled functions should not disable NGB on self-calls.
+
+        When a compiled function recursively calls itself, the inner call
+        should NOT be treated as a nested torch.compile (which would disable
+        NGB). Only calls to a *different* torch.compile wrapper should.
+        """
+
+        def fn(x, n):
+            if n <= 0:
+                return x
+            torch._dynamo.graph_break()
+            return fn(x + 1, n - 1)
+
+        cnts = torch._dynamo.testing.CompileCounter()
+        opt_fn = torch._dynamo.optimize(backend=cnts)(fn)
+        x = torch.zeros(3)
+        ref = fn(x, 3)
+        res = opt_fn(x, 3)
+        self.assertEqual(ref, res)
+
     def test_nested_store_attr_graph_break(self):
         class Foo:
             def __setattr__(self, name, value):
