bb4e099a90140583420f09bd129b1f66f31fede3
Laith Sakka <lsakka@meta.com>
2026-05-19 17:57:10 -0700

    Update
    [ghstack-poisoned]

--- torch/_inductor/freezing.py
+++ torch/_inductor/freezing.py
@@ -258,10 +258,12 @@ def enforce_as_strided_input_layout(gm: torch.fx.GraphModule):
             # Use materialize_symints intentionally; see its docstring for why.
             ft = n.args[0].meta["val"]
             stride_args = tuple(gm.graph.materialize_symints(ft.stride()))
+
+        with gm.graph.inserting_before(n):
             new_node = gm.graph.call_function(
                 prims.inductor_force_stride_order.default, (n.args[0], stride_args)
             )
-            n.replace_input_with(n.args[0], new_node)
+        n.replace_input_with(n.args[0], new_node)
 
     gm.graph.lint()
     gm.recompile()
