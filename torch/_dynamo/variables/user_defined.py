7a5f70aa0afdb3fb97af5a9f541d019d5c98c621
Animesh Jain <anijain@umich.edu>
2026-05-21 10:32:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -976,13 +976,34 @@ class UserDefinedClassVariable(UserDefinedVariable):
                 source = AttrSource(self.source, "__subclasses__")
                 source = CallFunctionNoArgsSource(source)
             return VariableTracker.build(tx, self.value.__subclasses__(), source)
-        elif (
+        elif name == "fromkeys" and (
             self.value in {collections.OrderedDict, collections.defaultdict}
-            and name == "fromkeys"
+            or issubclass(self.value, dict)
         ):
             return variables.DictBuiltinVariable.call_custom_dict_fromkeys(
-                tx, self.value, *args, **kwargs
+                tx, self, *args, **kwargs
             )
+        elif self.value is collections.defaultdict and name == "__copy__":
+            if not args:
+                raise_type_error(
+                    tx,
+                    "unbound method defaultdict.__copy__() needs an argument",
+                )
+            try:
+                receiver_type = args[0].python_type()
+            except NotImplementedError:
+                raise_type_error(
+                    tx,
+                    "descriptor '__copy__' for 'collections.defaultdict' "
+                    "objects doesn't apply to this object",
+                )
+            if not issubclass(receiver_type, collections.defaultdict):
+                raise_type_error(
+                    tx,
+                    "descriptor '__copy__' for 'collections.defaultdict' "
+                    f"objects doesn't apply to a '{receiver_type.__name__}' object",
+                )
+            return args[0].call_method(tx, name, args[1:], kwargs)
         elif self.value is collections.OrderedDict and name == "move_to_end":
             return args[0].call_method(tx, name, [*args[1:]], kwargs)
         elif name == "__len__" and len(args) == 1 and not kwargs:
@@ -1066,8 +1087,6 @@ class UserDefinedClassVariable(UserDefinedVariable):
             get_device_context_manager,
         )
 
-        constant_args = check_constant_args(args, kwargs)
-
         if torch.distributed.is_available() and self.value is torch.distributed.P2POp:
             if not config.enable_p2p_compilation:
                 unimplemented(
@@ -1086,7 +1105,7 @@ class UserDefinedClassVariable(UserDefinedVariable):
             var.call_method(tx, "__init__", list(args), kwargs)  # type: ignore[arg-type]
             return var
 
-        if self.can_constant_fold_through() and constant_args:
+        if self.can_constant_fold_through() and check_constant_args(args, kwargs):
             # constant fold
             return VariableTracker.build(
                 tx,
@@ -4154,6 +4173,15 @@ class UserDefinedDictVariable(UserDefinedObjectVariable):
         args: list[VariableTracker],
         kwargs: dict[str, VariableTracker],
     ) -> VariableTracker:
+        if (
+            name == "fromkeys"
+            and self._maybe_get_baseclass_method(name) in self._base_methods
+        ):
+            cls_vt = VariableTracker.build(tx, self.python_type(), self.cls_source)
+            return variables.DictBuiltinVariable.call_custom_dict_fromkeys(
+                tx, cls_vt, *args, **kwargs
+            )
+
         # Dict subclasses can override __missing__ to provide fallback
         # behavior instead of raising a KeyError. This is used, for example,
         # by collections.Counter.
@@ -4469,6 +4497,16 @@ class DefaultDictVariable(UserDefinedDictVariable):
         new.call_method(tx, "update", [right], {})
         return new
 
+    def nb_inplace_or_impl(
+        self,
+        tx: "InstructionTranslator",
+        other: VariableTracker,
+    ) -> VariableTracker:
+        if self._base_vt is None:
+            raise AssertionError("_base_vt must not be None in nb_inplace_or_impl")
+        self._base_vt.call_method(tx, "update", [other], {})
+        return self
+
     def call_method(
         self,
         tx: "InstructionTranslator",
@@ -4509,11 +4547,27 @@ class DefaultDictVariable(UserDefinedDictVariable):
             if len(args) != 1:
                 raise_args_mismatch(tx, name, "1 args", f"{len(args)} args")
             return self._missing_impl(tx, args[0])
-        elif name == "copy":
+        elif name == "__ior__":
+            if kwargs or len(args) != 1:
+                raise_args_mismatch(
+                    tx,
+                    name,
+                    "1 args and 0 kwargs",
+                    f"{len(args)} args and {len(kwargs)} kwargs",
+                )
+            return self.nb_inplace_or_impl(tx, args[0])
+        elif name in ("copy", "__copy__"):
             # defaultdict.copy() creates a new defaultdict with same factory
             # https://github.com/python/cpython/blob/v3.13.3/Modules/_collectionsmodule.c#L2282
             from .builder import SourcelessBuilder
 
+            if args or kwargs:
+                raise_args_mismatch(
+                    tx,
+                    name,
+                    "0 args and 0 kwargs",
+                    f"{len(args)} args and {len(kwargs)} kwargs",
+                )
             if self._base_vt is None:
                 raise AssertionError("_base_vt must not be None in copy")
             new_dd = tx.output.side_effects.track_new_user_defined_object(
