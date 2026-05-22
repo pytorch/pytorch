b6bd1f0ffc06d223255decdfab57aba0f066ff2f
Guilherme Leobas <guilhermeleobas@gmail.com>
2026-05-22 15:29:39 +0000

    [Dynamo] Add __delitem__ support to sq_ass_item / mp_ass_subscript (#182996)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/182996
    Approved by: https://github.com/rtimpe
    ghstack dependencies: #182862

--- test/dynamo/test_mapping_ops.py
+++ test/dynamo/test_mapping_ops.py
@@ -51,34 +51,6 @@ class _DictSubclassParam(dict):
     pass
 
 
-class _DictSubclassSetitem(dict):
-    def __setitem__(self, key, value):
-        super().__setitem__(key, value + 1000)
-
-
-class _DictSubclassNoOverride(dict):
-    pass
-
-
-class _DictWithMissing(dict):
-    def __missing__(self, key):
-        return -1
-
-
-class _ODSub(collections.OrderedDict):
-    pass
-
-
-class _DictSubclassTrack(dict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.writes = []
-
-    def __setitem__(self, key, value):
-        self.writes.append((key, value))
-        super().__setitem__(key, value)
-
-
 def _make_defaultdict(data):
     return collections.defaultdict(int, data)
 
@@ -105,11 +77,14 @@ class TestMpAssSubscript(torch._dynamo.test_case.TestCase):
     def setUp(self):
         super().setUp()
         self._u_prev = torch._dynamo.config.enable_trace_unittest
+        self._b_prev = torch._dynamo.config.enable_trace_load_build_class
         torch._dynamo.config.enable_trace_unittest = True
+        torch._dynamo.config.enable_trace_load_build_class = True
 
     def tearDown(self):
         super().tearDown()
         torch._dynamo.config.enable_trace_unittest = self._u_prev
+        torch._dynamo.config.enable_trace_load_build_class = self._b_prev
 
     # -- parameterized over mapping container type --
 
@@ -147,7 +122,10 @@ class TestMpAssSubscript(torch._dynamo.test_case.TestCase):
 
     @make_dynamo_test
     def test_subclass_dict_inherited_setitem(self):
-        d = _DictSubclassNoOverride()
+        class D(dict):
+            pass
+
+        d = D()
         d["a"] = 1
         d["b"] = 2
         self.assertEqual(d["a"], 1)
@@ -155,36 +133,55 @@ class TestMpAssSubscript(torch._dynamo.test_case.TestCase):
 
     @make_dynamo_test
     def test_subclass_dict_with_missing(self):
-        d = _DictWithMissing()
+        class D(dict):
+            def __missing__(self, key):
+                return -1
+
+        d = D()
         d["a"] = 5
         self.assertEqual(d["a"], 5)
         self.assertEqual(d["unknown"], -1)
 
     @make_dynamo_test
     def test_subclass_dict_track_writes(self):
-        d = _DictSubclassTrack()
+        class D(dict):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.writes = []
+
+            def __setitem__(self, key, value):
+                self.writes.append((key, value))
+                super().__setitem__(key, value)
+
+        d = D()
         d["a"] = 1
         d["b"] = 2
         self.assertEqual(d.writes, [("a", 1), ("b", 2)])
         self.assertEqual(d["a"], 1)
         self.assertEqual(d["b"], 2)
 
-    @make_dynamo_test
-    def test_subclass_dict_replace_existing(self):
-        d = _DictSubclassTrack({"a": 1})
-        d["a"] = 99
-        self.assertEqual(d["a"], 99)
-        self.assertEqual(d.writes, [("a", 99)])
+        # also exercise replace-existing
+        d2 = D({"a": 1})
+        d2["a"] = 99
+        self.assertEqual(d2["a"], 99)
+        self.assertEqual(d2.writes, [("a", 99)])
 
     @make_dynamo_test
     def test_subclass_dict_overriding(self):
-        d = _DictSubclassSetitem()
+        class D(dict):
+            def __setitem__(self, key, value):
+                super().__setitem__(key, value + 1000)
+
+        d = D()
         d["a"] = 5
         self.assertEqual(d["a"], 1005)
 
     @make_dynamo_test
     def test_subclass_ordereddict(self):
-        d = _ODSub()
+        class D(collections.OrderedDict):
+            pass
+
+        d = D()
         d["a"] = 1
         d["b"] = 2
         self.assertEqual(list(d.keys()), ["a", "b"])
@@ -248,6 +245,101 @@ class TestMpAssSubscript(torch._dynamo.test_case.TestCase):
         d = {}
         with self.assertRaises(TypeError):
             d[[1, 2]] = 1  # type: ignore[index]
+
+    # -- __delitem__ (NULL value via mp_ass_subscript) --
+    # WithSetitemViaMapping has no __delitem__; restrict to dict-like types.
+
+    @parametrize(
+        "label,factory",
+        [
+            ("dict", dict),
+            ("OrderedDict", collections.OrderedDict),
+            ("Counter", collections.Counter),
+            ("defaultdict", _make_defaultdict),
+            ("DictSubclass", _DictSubclassParam),
+        ],
+    )
+    @make_dynamo_test
+    def test_delitem_basic(self, label, factory):
+        c = factory({"a": 1, "b": 2, "c": 3})
+        del c["b"]
+        self.assertNotIn("b", c)
+        self.assertEqual(c["a"], 1)
+        self.assertEqual(c["c"], 3)
+
+    @parametrize(
+        "label,factory",
+        [
+            ("dict", dict),
+            ("OrderedDict", collections.OrderedDict),
+            ("Counter", collections.Counter),
+            ("defaultdict", _make_defaultdict),
+            ("DictSubclass", _DictSubclassParam),
+        ],
+    )
+    @make_dynamo_test
+    def test_delitem_then_set(self, label, factory):
+        c = factory({"a": 1, "b": 2})
+        del c["a"]
+        c["a"] = 99
+        self.assertEqual(c["a"], 99)
+
+    @make_dynamo_test
+    def test_delitem_dict_missing_key(self):
+        d = {"a": 1}
+        with self.assertRaises(KeyError):
+            del d["missing"]
+
+    @make_dynamo_test
+    def test_delitem_dict_all_keys(self):
+        d = {"a": 1, "b": 2, "c": 3}
+        del d["a"]
+        del d["b"]
+        del d["c"]
+        self.assertEqual(len(d), 0)
+
+    @make_dynamo_test
+    def test_delitem_dict_unhashable_key(self):
+        d = {"a": 1}
+        with self.assertRaises(TypeError):
+            del d[[1, 2]]  # type: ignore[index]
+
+    @make_dynamo_test
+    def test_delitem_subclass_dict_inherited(self):
+        class D(dict):
+            pass
+
+        d = D({"a": 1, "b": 2})
+        del d["a"]
+        self.assertEqual(dict(d), {"b": 2})
+
+    @make_dynamo_test
+    def test_delitem_subclass_dict_override(self):
+        class D(dict):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.deleted = []
+
+            def __delitem__(self, key):
+                self.deleted.append(key)
+                super().__delitem__(key)
+
+        d = D({"a": 1, "b": 2, "c": 3})
+        del d["a"]
+        del d["c"]
+        self.assertEqual(dict(d), {"b": 2})
+        self.assertEqual(d.deleted, ["a", "c"])
+
+    def test_delitem_outer_dict_persists(self):
+        outer = {"a": 1, "b": 2}
+
+        @torch.compile(backend="eager", fullgraph=True)
+        def f(x):
+            del outer["a"]
+            return x + 1
+
+        f(torch.zeros(1))
+        self.assertEqual(outer, {"b": 2})
 
 
 instantiate_parametrized_tests(TestMpAssSubscript)
