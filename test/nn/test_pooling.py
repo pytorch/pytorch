82eb8d3d4dcbaeedb32065af56630a8bf7e9d50e
cyy <cyyever@outlook.com>
2026-05-22 12:46:30 +0000

    [MPS] Validate stride > 0 in pool ops to match CPU behavior (#184875)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/184875
    Approved by: https://github.com/malfet

--- test/nn/test_pooling.py
+++ test/nn/test_pooling.py
@@ -2112,7 +2112,6 @@ torch.cuda.synchronize()
             res2.backward(torch.randn_like(res2))
             self.assertTrue(math.isinf(res2.item()))
 
-    @expectedFailureMPS  # TODO: Fix me
     @onlyNativeDeviceTypes  # TODO: RuntimeError message different on XLA
     def test_pooling_zero_stride(self, device):
         for op in ("max", "avg"):
