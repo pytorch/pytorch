441bbde2b966dd6422af15986468726a6b3adebd
Jason Ansel <jansel@meta.com>
2026-05-23 12:31:34 -0700

    Update
    [ghstack-poisoned]

--- test/onnx/test_fx_type_promotion.py
+++ test/onnx/test_fx_type_promotion.py
@@ -1,5 +1,6 @@
 # Owner(s): ["module: onnx"]
 
+import torch
 from torch.onnx._internal.fx.passes import type_promotion
 from torch.testing._internal import common_utils
 
@@ -23,6 +24,37 @@ class TestGeneratedTypePromotionRuleSet(common_utils.TestCase):
 
     def test_initialize_type_promotion_table_succeeds(self):
         type_promotion.TypePromotionTable()
+
+
+class TestFindCompatibleOpOverload(common_utils.TestCase):
+    def test_selects_schema_compatible_overload(self):
+        condition = torch.tensor([True, False])
+        args = (condition, torch.tensor(0.0), -1000.0)
+
+        overload = type_promotion.find_compatible_op_overload(
+            torch.ops.aten.where, args, {}
+        )
+
+        self.assertEqual(overload, torch.ops.aten.where.ScalarOther)
+
+
+class TestTypePromotionONNXExport(common_utils.TestCase):
+    def test_where_with_bool_tensor_and_mixed_scalars_exports(self):
+        class WhereModel(torch.nn.Module):
+            def forward(self, condition):
+                return torch.where(condition, 0, -1000.0)
+
+        model = WhereModel().eval()
+        condition = torch.randn(1, 1, 4, 4) > 0
+
+        onnx_program = torch.onnx.export(
+            model,
+            (condition,),
+            dynamo=True,
+            verbose=False,
+        )
+
+        self.assertIsNotNone(onnx_program)
 
 
 if __name__ == "__main__":
