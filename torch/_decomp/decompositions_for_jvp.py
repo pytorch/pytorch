060b4b3b25fbcdffecb31253d447900e80c93d93
cyy <cyyever@outlook.com>
2026-05-23 11:17:05 +0800

    Use torch.rsqrt() instead of 1 / torch.sqrt()

--- torch/_decomp/decompositions_for_jvp.py
+++ torch/_decomp/decompositions_for_jvp.py
@@ -124,7 +124,7 @@ def recompute_mean_var(
     var = torch.var(input, dim=inner_dim_indices, unbiased=False, keepdim=keepdim)
     eps = torch.pow(1 / rstd, 2) - var  # this makes me so sad inside
     eps = eps.detach()
-    rstd = 1 / torch.sqrt(var + eps)
+    rstd = torch.rsqrt(var + eps)
     return mean, rstd
 
 
