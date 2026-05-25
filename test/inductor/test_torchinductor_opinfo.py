f9d0b9087962ab46c0567fa3893e38b936e27b7c
Benjamin Glass <bglass@openteams.com>
2026-05-25 18:35:37 +0000

    [test] Add OpInfo for aten.native_group_norm
    This is a prerequisite to the refactoring of the decomp and CompositeExplicitAutograd for this op.
    
    ghstack-source-id: 7c736614edf5bf9e4deb4020512dddcff7b3835f
    Pull-Request: https://github.com/pytorch/pytorch/pull/183000

--- test/inductor/test_torchinductor_opinfo.py
+++ test/inductor/test_torchinductor_opinfo.py
@@ -530,6 +530,9 @@ inductor_override_kwargs["cuda"] = {
         "atol": 1e-4,
         "rtol": 7e-1,
     },
+    # The eager gradient for native_group_norm appears to be numerically unstable at low
+    # precisions; more investigation is needed.
+    ("native_group_norm", f16): {"check_gradient": False},
 }
 
 inductor_override_kwargs["xpu"] = {
@@ -696,6 +699,9 @@ inductor_override_kwargs["xpu"] = {
     ("nn.functional.interpolate.trilinear", f64): {
         "check_gradient": False,
     },
+    # The eager gradient for native_group_norm appears to be numerically unstable at low
+    # precisions; more investigation is needed.
+    ("native_group_norm", f16): {"check_gradient": False},
 }
 if TEST_WITH_ROCM:
     inductor_override_kwargs["cuda"].update(
