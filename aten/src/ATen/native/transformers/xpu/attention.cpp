70b7bef4641d6e54b22f5605897f62d5318fa358
fengqing.lu <fengqing.lu@intel.com>
2026-05-27 06:15:22 -0700

    remove function

--- aten/src/ATen/native/transformers/xpu/attention.cpp
+++ aten/src/ATen/native/transformers/xpu/attention.cpp
@@ -112,52 +112,5 @@ _scaled_dot_product_flash_attention_xpu(
       std::move(debug_attn_mask));
 }
 
-// std::tuple<
-//     Tensor,
-//     Tensor,
-//     Tensor,
-//     Tensor,
-//     c10::SymInt,
-//     c10::SymInt,
-//     Tensor,
-//     Tensor,
-//     Tensor>
-// _scaled_dot_product_flash_attention_xpu(
-//     const Tensor& query,
-//     const Tensor& key,
-//     const Tensor& value,
-//     double dropout_p,
-//     bool is_causal,
-//     bool return_debug_mask,
-//     std::optional<double> scale) {
-//   auto
-//       [attention,
-//        logsumexp,
-//        cumulative_sequence_length_q,
-//        cumulative_sequence_length_k,
-//        max_seqlen_batch_q,
-//        max_seqlen_batch_k,
-//        philox_seed,
-//        philox_offset] =
-//           sycltla::flash_attention_forward(
-//               query,
-//               key,
-//               value,
-//               dropout_p,
-//               is_causal,
-//               scale.has_value() ? scale.value()
-//                                 : (1.0 / std::sqrt(query.size(3))));
-//   return std::make_tuple(
-//       attention,
-//       logsumexp,
-//       cumulative_sequence_length_q,
-//       cumulative_sequence_length_k,
-//       max_seqlen_batch_q,
-//       max_seqlen_batch_k,
-//       philox_seed,
-//       philox_offset,
-//       /* debug_attn_mask */ at::Tensor());
-// }
-
 } // namespace native
 } // namespace at
