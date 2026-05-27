481663e29c26131692f30cece7b1bc9b8605ac09
daisyden <daisy.deng@intel.com>
2026-05-27 05:48:18 +0000

    Update
    [ghstack-poisoned]

--- test/test_transformers.py
+++ test/test_transformers.py
@@ -4014,6 +4014,7 @@ class TestSDPAGpuOnly(NNTestCase):
                                                                  dropout_p: float, dtype: torch.dtype,
                                                                  scale: str):
         device_mod = getattr(torch, torch.device(device).type)
+
         def _get_mem_eff_drop_mask(batch_size, n_heads, q_len, kv_len, p, seed, offset, device=device):
             mask = torch.empty((batch_size, n_heads, q_len, kv_len), device=device, dtype=torch.float32)
             rand_uniform = torch._fill_mem_eff_dropout_mask_(mask, p, seed, offset)
