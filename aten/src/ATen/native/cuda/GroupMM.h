e42e7f757ce95c1c9b9bd28d378238ab38aa5bc9
IvanKobzarev <ivan.kobzarev@gmail.com>
2026-05-27 05:54:51 -0700

    Update
    [ghstack-poisoned]

--- aten/src/ATen/native/cuda/GroupMM.h
+++ aten/src/ATen/native/cuda/GroupMM.h
@@ -15,4 +15,10 @@ TORCH_API void bf16bf16_foreach_mm(
     at::TensorList self_list,
     at::TensorList mat2_list,
     std::vector<at::Tensor>& outputs);
+
+TORCH_API std::vector<at::Tensor> grouped_mm_from_ptrs(
+    const at::Tensor& a_ptrs,
+    const at::Tensor& b_ptrs,
+    int64_t M, int64_t N, int64_t K, int64_t G,
+    int64_t lda, int64_t ldb);
 } // namespace at::cuda::detail
