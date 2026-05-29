1da94a369240e8fd785aee17810e096f8d13668a
Allan Lin <allin@redhat.com>
2026-05-29 01:00:56 +0000

    fix sdpa activation dtype mismatched between meta/actual impl

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -6343,7 +6343,7 @@ def meta__scaled_dot_product_flash_attention_for_cpu(
             max_seqlen_batch_q,
             num_heads,
         ),
-        dtype=torch.float,
+        dtype=utils.get_acc_type(query.dtype, torch.device("cpu")),
         device=query.device,
     ).transpose(1, 2)
     return (
