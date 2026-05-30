aecf2c1963160bf66c95c952f1adb69b567b60f5
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-05-30 06:17:54 +0000

    Revert "[cuda] Add built-in UVM allocator context manager for numerics testing (#185411)"
    This reverts commit b6f9631d32f98f750c4905ab53f10963315cae31.
    
    Reverted https://github.com/pytorch/pytorch/pull/185411 on behalf of https://github.com/huydhn due to Sorry for reverting your change but trunk tests were not run in the PR due to an infra sev yesterday and the test is failing in trunk ([comment](https://github.com/pytorch/pytorch/pull/185411#issuecomment-4581931994))

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -1664,7 +1664,6 @@ def _get_rng_state_offset(device: int | str | torch.device = "cuda") -> int:
 
 # pyrefly: ignore [deprecated]
 from .memory import *  # noqa: F403
-from .memory import _use_uvm
 from .random import *  # noqa: F403
 
 
