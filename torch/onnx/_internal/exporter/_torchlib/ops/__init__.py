0a1dee3147b08654899df55afe72b94919917c9b
Jason Ansel <jansel@meta.com>
2026-05-31 22:44:35 -0700

    Update
    [ghstack-poisoned]

--- torch/onnx/_internal/exporter/_torchlib/ops/__init__.py
+++ torch/onnx/_internal/exporter/_torchlib/ops/__init__.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
 
-__all__ = ["core", "hop", "nn", "symbolic", "symops"]
+__all__ = ["core", "hop", "nn", "prims", "symbolic", "symops"]
 
-from torch.onnx._internal.exporter._torchlib.ops import core, hop, nn, symbolic, symops
+from torch.onnx._internal.exporter._torchlib.ops import (
+    core,
+    hop,
+    nn,
+    prims,
+    symbolic,
+    symops,
+)
