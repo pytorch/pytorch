ce470cac127928f87bf05b451b6c3b71051cff0d
cyy <cyyever@outlook.com>
2026-06-02 03:36:22 +0000

    Switch lookup-only std::map/std::set to unordered variants (#184846)
    Convert lookup-only std::map/std::set to unordered variants.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/184846
    Approved by: https://github.com/ezyang

--- torch/csrc/profiler/standalone/execution_trace_observer.cpp
+++ torch/csrc/profiler/standalone/execution_trace_observer.cpp
@@ -19,6 +19,7 @@
 #include <mutex>
 #include <sstream>
 #include <stack>
+#include <unordered_map>
 #include <vector>
 
 #include <ATen/core/TensorBody.h>
@@ -109,11 +110,11 @@ struct TORCH_API ExecutionTraceObserver { // NOLINT
   using ID = size_t;
 
   // Mapping of each thread to its own operator stack
-  std::map<size_t, std::stack<ID>> opStack;
+  std::unordered_map<size_t, std::stack<ID>> opStack;
   // Uses the underlying TensorImpl object pointer as the key and map to its
   // unique id.
 
-  std::map<const void*, ID> objectId;
+  std::unordered_map<const void*, ID> objectId;
   // Observer run state.
   enum class RunState { uninitialized, disabled, enabled };
 
@@ -381,13 +382,10 @@ static ExecutionTraceObserver::ID getObjectID(
     const void* t) {
   const std::lock_guard<std::recursive_mutex> lock(ob.gMutex);
 
-  auto iter = ob.objectId.find(t);
-  if (iter == ob.objectId.end()) {
-    ExecutionTraceObserver::ID objectId = ob.getNewID();
-    ob.objectId[t] = objectId;
-    return objectId;
+  auto [iter, inserted] = ob.objectId.try_emplace(t);
+  if (inserted) {
+    iter->second = ob.getNewID();
   }
-
   return iter->second;
 }
 
