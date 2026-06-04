7b74912a1a3efaef6b3fd1569db0eda69ec5e678
Pian Pawakapan <pianpwk@meta.com>
2026-06-04 11:51:32 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -5720,6 +5720,7 @@ class LocalMapWrappedHigherOrderVariable(WrapHigherOrderVariable):
             in_grad_placements,
             device_mesh,
             redistribute_inputs,
+            enable_spmd_types,
             *user_args,
         ) = args
 
