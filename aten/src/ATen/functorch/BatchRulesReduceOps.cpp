4b0e3f2ea256f25ae86c341c8b45e975a77c15da
Benjamin Glass <bglass@openteams.com>
2026-06-05 20:30:49 +0000

    Update (base update)
    [ghstack-poisoned]

--- aten/src/ATen/functorch/BatchRulesReduceOps.cpp
+++ aten/src/ATen/functorch/BatchRulesReduceOps.cpp
@@ -122,7 +122,7 @@ static void boxed_reduction_batch_rule(const c10::OperatorHandle& op, torch::jit
   int64_t cur_level = maybe_layer->layerId();
 
   auto orig_arguments = torch::jit::last(*stack, num_arguments);
-  if (std::none_of(orig_arguments.begin(), orig_arguments.end(), ivalueParticipatesInCurrentLevel)) {
+  if (std::ranges::none_of(orig_arguments, ivalueParticipatesInCurrentLevel)) {
     c10::impl::ExcludeDispatchKeyGuard guard_2(DispatchKey::FuncTorchBatched);
     op.callBoxed(stack);
     return;
@@ -348,7 +348,7 @@ static std::tuple<Tensor, std::optional<int64_t>> searchsorted_batch_rule(
   if (sorter.has_value() && sorter->defined()) {
     auto sorter__ = moveBatchDimToFront(*sorter, sorter_bdim);
     if (sorted_sequence_bdim.has_value() != sorter_bdim.has_value()) {
-      auto bdim_size = get_bdim_size2(
+      auto bdim_size = get_bdim_size(
           sorted_sequence, sorted_sequence_bdim,
           sorter.value(), sorter_bdim);
       sorter__ = ensure_has_bdim(sorter__, sorter_bdim.has_value(), bdim_size);
