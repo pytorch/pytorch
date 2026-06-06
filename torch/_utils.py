e8caa9501e3fa942fbad05492c6baed99f45f61f
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-06 12:54:41 +0000

    Revert "Support non-blocking D2H pinned copy for XPU/MTIA (#186224)"
    This reverts commit 018f6ff66752d00ccf7130c4db1f386a1777a12d.
    
    Reverted https://github.com/pytorch/pytorch/pull/186224 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/186224#issuecomment-4638642066))

--- torch/_utils.py
+++ torch/_utils.py
@@ -77,8 +77,6 @@ def _to(self, device, non_blocking=False):
     if device.type == "cpu":
         pin_memory = non_blocking and self.device.type in (
             "cuda",
-            "xpu",
-            "mtia",
             torch._C._get_privateuse1_backend_name(),
         )
         untyped_storage = torch.empty(
