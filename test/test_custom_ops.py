114c34d6dccb5b782a19702086766980344c4d05
Mark Harfouche <mark.harfouche@gmail.com>
2026-06-07 13:38:44 -0400

    Fix indentation in test_custom_ops.py

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -331,7 +331,7 @@ class TestCustomOpTesting(CustomOpTestCaseBase):
             @staticmethod
             def forward(ctx, x):
                 # Emulate AutoDispatchBelowADInplaceOrView, which is not bound into python
-                with torch._C._AutoDispatchBelowAutograd(),  torch._C._ExcludeDispatchKeyGuard(
+                with torch._C._AutoDispatchBelowAutograd(), torch._C._ExcludeDispatchKeyGuard(
                     torch._C.DispatchKeySet(torch._C.DispatchKey.ADInplaceOrView)
                 ):
                     return op(x)
