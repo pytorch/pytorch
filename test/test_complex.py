e8ae95797b18ff898eedcc34854dd5219054783b
William Wen <williamwen@fb.com>
2026-06-08 07:21:23 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/test_complex.py
+++ test/test_complex.py
@@ -2,6 +2,7 @@
 # Owner(s): ["module: complex"]
 
 import torch
+import torch._dynamo.config
 from torch.testing._internal.common_device_type import (
     dtypes,
     instantiate_device_type_tests,
@@ -241,6 +242,7 @@ class TestComplexTensor(TestCase):
                 actual, expected, msg=f"\neq(out)\nactual {actual}\nexpected {expected}"
             )
 
+    @torch._dynamo.config.patch(nested_graph_breaks=False)
     @onlyCPU
     @dtypes(*complex_types())
     def test_ne(self, device, dtype):
