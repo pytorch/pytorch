9278fbaac78dbf3be606603f28cd9c1ced7ab37f
Tanmayyyy <ktanmay@redhat.com>
2026-06-08 19:08:22 +0530

    Update test/test_reductions.py
    Co-authored-by: Jiawei Li <ljw1101.vip@gmail.com>

--- test/test_reductions.py
+++ test/test_reductions.py
@@ -1604,7 +1604,7 @@ class TestReductions(TestCase):
 
     @onlyCPU
     def test_min_mixed_devices(self, device):
-        a = torch.randn(10, device=device)
+        a = torch.randn(10, device="cpu")
         if torch.accelerator.is_available():
             accelerator_type = torch.accelerator.current_accelerator().type
             values = torch.randn(10).to(accelerator_type)
