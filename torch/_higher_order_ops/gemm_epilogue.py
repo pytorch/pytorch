b403ddd24fdbd13b6b0d04a549b80e3616409155
drisspg <drisspguessous@gmail.com>
2026-06-09 00:34:54 +0000

    Update
    [ghstack-poisoned]

--- torch/_higher_order_ops/gemm_epilogue.py
+++ torch/_higher_order_ops/gemm_epilogue.py
@@ -423,16 +423,17 @@ def _match_quack_local_m_reduce(
     if shape[0] != -1 or not isinstance(shape[1], int) or shape[1] <= 0:
         return None
     mm_val = mm_node.meta.get("val")
-    if mm_val is not None:
-        mm_shape = tuple(mm_val.shape)
-        reduce_val = node.meta.get("val")
-        if len(mm_shape) != 2 or shape[2] != mm_shape[1]:
-            return None
-        if reduce_val is not None and tuple(reduce_val.shape) != (
-            mm_shape[0] // shape[1],
-            mm_shape[1],
-        ):
-            return None
+    reduce_val = node.meta.get("val")
+    if mm_val is None or reduce_val is None:
+        return None
+    mm_shape = tuple(mm_val.shape)
+    if len(mm_shape) != 2 or shape[2] != mm_shape[1]:
+        return None
+    if tuple(reduce_val.shape) != (
+        mm_shape[0] // shape[1],
+        mm_shape[1],
+    ):
+        return None
     return QuackLocalReduceInfo(
         view_node=view_node,
         reduce_node=node,
