3732fbab01612968b9b701edfff7ac56177b300f
drisspg <drisspguessous@gmail.com>
2026-06-09 00:33:18 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_higher_order_ops/__init__.py
+++ torch/_higher_order_ops/__init__.py
@@ -19,6 +19,7 @@ from torch._higher_order_ops.flex_attention import (
     flex_attention_backward,
 )
 from torch._higher_order_ops.foreach_map import _foreach_map, foreach_map
+from torch._higher_order_ops.gemm_epilogue import gemm_epilogue_fusion
 from torch._higher_order_ops.hints_wrap import hints_wrapper
 from torch._higher_order_ops.inline_asm_elementwise import inline_asm_elementwise
 from torch._higher_order_ops.invoke_leaf_function import invoke_leaf_function
@@ -58,6 +59,7 @@ __all__ = [
     "flat_apply",
     "foreach_map",
     "_foreach_map",
+    "gemm_epilogue_fusion",
     "with_effects",
     "tag_activation_checkpoint",
     "auto_functionalized",
