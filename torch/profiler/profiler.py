e1d38ae49a355407f7434669993a7a1acd1242b9
dolpm <34420038+dolpm@users.noreply.github.com>
2026-06-09 17:27:02 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/profiler/profiler.py
+++ torch/profiler/profiler.py
@@ -322,16 +322,16 @@ class _KinetoProfile:
                 else None,
             )
         if self._use_cupti_monitor:
-            from torch.profiler import _cupti_monitor as _mon
+            from torch.profiler._cupti import monitor as _mon
 
             if _mon.get_monitor() is None:
                 self._monitor_tempdir = tempfile.TemporaryDirectory(
                     prefix="torch_cupti_profiler_"
                 )
-                _mon.start_collection(self._monitor_tempdir.name)
+                _mon.start_collection(self._monitor_tempdir.name)  # pyrefly: ignore
                 self._monitor_started_here = True
             self._monitor_trace_window = None
-            _mon.prepare_trace_window()
+            _mon.prepare_trace_window()  # pyrefly: ignore
         self.profiler._prepare_trace()
 
     def start_trace(self) -> None:
@@ -341,9 +341,9 @@ class _KinetoProfile:
             raise AssertionError("Profiler must be initialized before starting trace")
         self.profiler._start_trace()
         if self._use_cupti_monitor:
-            from torch.profiler import _cupti_monitor as _mon
+            from torch.profiler._cupti import monitor as _mon
 
-            _mon.start_trace_window()
+            _mon.start_trace_window()  # pyrefly: ignore
 
         if self.profile_memory:
             self.add_metadata_json("profile_memory", "1")
@@ -391,16 +391,16 @@ class _KinetoProfile:
         if self.profiler is None:
             raise AssertionError("Profiler must be initialized before stopping trace")
         if self._use_cupti_monitor:
-            from torch.profiler import _cupti_monitor as _mon
+            from torch.profiler._cupti import monitor as _mon
 
             if self.use_device:
                 torch.accelerator.synchronize()
-            self._monitor_trace_window = _mon.end_trace_window()
+            self._monitor_trace_window = _mon.end_trace_window()  # pyrefly: ignore
         self.profiler.__exit__(None, None, None)
         if self._use_cupti_monitor and self._monitor_started_here:
-            from torch.profiler import _cupti_monitor as _mon
+            from torch.profiler._cupti import monitor as _mon
 
-            _mon.stop_collection()
+            _mon.stop_collection()  # pyrefly: ignore
             self._monitor_started_here = False
             if self._monitor_tempdir is not None:
                 self._monitor_tempdir.cleanup()
@@ -420,7 +420,7 @@ class _KinetoProfile:
                 raise AssertionError(
                     "CUPTI monitor trace window must exist before exporting chrome trace"
                 )
-            from torch.profiler._cupti_monitor_trace import (
+            from torch.profiler._cupti.monitor_trace import (
                 merge_trace_window_into_chrome_trace,
             )
 
