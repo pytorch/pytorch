320d8de82c2d7de7379c655a36ad3383079a652d
Yu, Guangye <guangye.yu@intel.com>
2026-06-10 14:45:20 +0000

    Update
    [ghstack-poisoned]

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -349,10 +349,10 @@ def preserve_global_state(fn: Callable[_P, _T]) -> Callable[_P, _T]:
             )
             prior_dtype = torch.get_default_dtype()
             torch_rng_state = torch.random.get_rng_state()
-            cuda_rng_state = None
-            if torch.cuda.is_available():
+            acc_rng_state = None
+            if torch.accelerator.is_available():
                 with torch._C.DisableTorchFunction():
-                    cuda_rng_state = torch.cuda.get_rng_state()
+                    acc_rng_state = torch.accelerator.get_rng_state()
             cuda_matmul_fp32_prec = torch._C._get_fp32_precision_getter(
                 "cuda", "matmul"
             )
@@ -381,9 +381,9 @@ def preserve_global_state(fn: Callable[_P, _T]) -> Callable[_P, _T]:
                 )
                 if prior_mobile_allocator_state != curr_mobile_allocator_state:
                     torch._C._unset_default_mobile_cpu_allocator()
-                if cuda_rng_state is not None:
+                if acc_rng_state is not None:
                     with torch._C.DisableTorchFunction():
-                        torch.cuda.set_rng_state(cuda_rng_state)
+                        torch.accelerator.set_rng_state(acc_rng_state)
                 torch._C._set_fp32_precision_setter(
                     "cuda", "matmul", cuda_matmul_fp32_prec
                 )
