682c4535833fdfadf8de83785c925a88374a78b4
shangerxin <shangerxin@hotmail.com>
2026-06-11 17:10:04 +0800

    Merge conflict

--- test/dynamo/test_compiler_bisector.py
+++ test/dynamo/test_compiler_bisector.py
@@ -13,6 +13,7 @@ from torch._inductor.compiler_bisector import CompilerBisector
 from torch._inductor.custom_graph_pass import CustomGraphPass
 from torch._inductor.test_case import TestCase
 from torch.library import _scoped_library, Library
+from torch.testing._internal.inductor_utils import HAS_XPU_AND_TRITON
 from torch.testing._internal.common_utils import requires_cuda
 from torch.testing._internal.triton_utils import requires_cuda_and_triton, requires_gpu_and_triton
 from torch.testing._internal.inductor_utils import (
@@ -20,7 +21,6 @@ from torch.testing._internal.inductor_utils import (
 	HAS_GPU,
     HAS_XPU_AND_TRITON,
 )
-
 
 
 device_type = acc.type if (acc := torch.accelerator.current_accelerator()) else "cpu"
@@ -183,7 +183,9 @@ class TestCompilerBisector(TestCase):
         self.assertEqual(out.bisect_number, 4)
         self.assertTrue("joint_custom_post_pass" in out.debug_info)
 
-    @skipIf(HAS_XPU_AND_TRITON, "String comparison failed: 'eager' != 'inductor' on XPU")
+    @skipIf(
+        HAS_XPU_AND_TRITON, "String comparison failed: 'eager' != 'inductor' on XPU"
+    )
     def test_rng(self):
         def foo():
             return torch.rand([10], device=device_type) + 1
