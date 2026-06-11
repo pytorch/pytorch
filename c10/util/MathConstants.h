c6d45abc1c04e49e78ef773ede918628cf1fde7e
cyy <cyyever@outlook.com>
2026-06-11 01:05:33 +0000

    Use C++20 std::numbers in c10 MathConstants (#186877)
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/186877
    Approved by: https://github.com/Skylion007

--- c10/util/MathConstants.h
+++ c10/util/MathConstants.h
@@ -4,32 +4,30 @@
 #include <c10/util/BFloat16.h>
 #include <c10/util/Half.h>
 
-C10_CLANG_DIAGNOSTIC_PUSH()
-#if C10_CLANG_HAS_WARNING("-Wimplicit-float-conversion")
-C10_CLANG_DIAGNOSTIC_IGNORE("-Wimplicit-float-conversion")
-#endif
+#include <numbers>
 
 namespace c10 {
-// TODO: Replace me with inline constexpr variable when C++17 becomes available
+// Kept as C10_HOST_DEVICE functions, not plain constexpr variables: NVCC
+// cannot access a namespace-scope constexpr variable from device code.
 namespace detail {
 template <typename T>
 C10_HOST_DEVICE inline constexpr T e() {
-  return static_cast<T>(2.718281828459045235360287471352662);
+  return static_cast<T>(std::numbers::e_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T euler() {
-  return static_cast<T>(0.577215664901532860606512090082402);
+  return static_cast<T>(std::numbers::egamma_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T frac_1_pi() {
-  return static_cast<T>(0.318309886183790671537767526745028);
+  return static_cast<T>(std::numbers::inv_pi_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T frac_1_sqrt_pi() {
-  return static_cast<T>(0.564189583547756286948079451560772);
+  return static_cast<T>(std::numbers::inv_sqrtpi_v<double>);
 }
 
 template <typename T>
@@ -39,47 +37,47 @@ C10_HOST_DEVICE inline constexpr T frac_sqrt_2() {
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T frac_sqrt_3() {
-  return static_cast<T>(0.577350269189625764509148780501957);
+  return static_cast<T>(std::numbers::inv_sqrt3_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T golden_ratio() {
-  return static_cast<T>(1.618033988749894848204586834365638);
+  return static_cast<T>(std::numbers::phi_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T ln_10() {
-  return static_cast<T>(2.302585092994045684017991454684364);
+  return static_cast<T>(std::numbers::ln10_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T ln_2() {
-  return static_cast<T>(0.693147180559945309417232121458176);
+  return static_cast<T>(std::numbers::ln2_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T log_10_e() {
-  return static_cast<T>(0.434294481903251827651128918916605);
+  return static_cast<T>(std::numbers::log10e_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T log_2_e() {
-  return static_cast<T>(1.442695040888963407359924681001892);
+  return static_cast<T>(std::numbers::log2e_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T pi() {
-  return static_cast<T>(3.141592653589793238462643383279502);
+  return static_cast<T>(std::numbers::pi_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T sqrt_2() {
-  return static_cast<T>(1.414213562373095048801688724209698);
+  return static_cast<T>(std::numbers::sqrt2_v<double>);
 }
 
 template <typename T>
 C10_HOST_DEVICE inline constexpr T sqrt_3() {
-  return static_cast<T>(1.732050807568877293527446341505872);
+  return static_cast<T>(std::numbers::sqrt3_v<double>);
 }
 
 template <>
@@ -138,5 +136,3 @@ constexpr T sqrt_2 = c10::detail::sqrt_2<T>();
 template <typename T>
 constexpr T sqrt_3 = c10::detail::sqrt_3<T>();
 } // namespace c10
-
-C10_CLANG_DIAGNOSTIC_POP()
