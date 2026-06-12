d42dadfe5b1d5ca31904cc8a5a73e7de18939d13
Isalia20 <irakli.salia854@gmail.com>
2026-06-12 12:21:27 +0400

    unexpected successes make my day

--- torch/testing/_internal/opinfo/definitions/linalg.py
+++ torch/testing/_internal/opinfo/definitions/linalg.py
@@ -2052,10 +2052,6 @@ op_db: list[OpInfo] = [
                 "test_compare_cpu",
                 active_if=(not TEST_XPU),
             ),
-            # Exception: Resizing an out= argument with no elements threw a resize warning!
-            DecorateInfo(
-                unittest.expectedFailure, "TestCommon", "test_out", device_type="mps"
-            ),
             # RuntimeError: linalg.lu_factor(): MPS doesn't support complex types.
             DecorateInfo(
                 unittest.expectedFailure, "TestCommon", "test_dtypes", device_type="mps"
@@ -2086,10 +2082,6 @@ op_db: list[OpInfo] = [
                 "TestCommon",
                 "test_compare_cpu",
                 active_if=(not TEST_XPU),
-            ),
-            # Exception: Resizing an out= argument with no elements threw a resize warning!
-            DecorateInfo(
-                unittest.expectedFailure, "TestCommon", "test_out", device_type="mps"
             ),
             # RuntimeError: linalg.lu_factor(): MPS doesn't support complex types.
             DecorateInfo(
