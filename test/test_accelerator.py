913518fd8ab4c279b8b3d5882f88787e4a2c643d
Yu, Guangye <guangye.yu@intel.com>
2026-06-12 15:07:49 +0000

    Introduce isLazyInitialized&set_rng_state in torch.accelerator
    ghstack-source-id: ed1c2b6375b56e6f43969e9c256b6baba3084360
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/186599

--- test/test_accelerator.py
+++ test/test_accelerator.py
@@ -304,6 +304,18 @@ class TestAccelerator(TestCase):
                     t = t.to(reference_dtype)
                     t = t.to(dtype)
 
+    def test_random_generator_state(self):
+        for device_idx in range(torch.accelerator.device_count()):
+            generator = torch._C._accelerator_getDefaultGenerator(device_idx)
+            old_state = torch.accelerator.get_rng_state(device_idx)
+            self.assertIsInstance(old_state, torch.Tensor)
+            generator.manual_seed(12345)
+            new_state = torch.accelerator.get_rng_state(device_idx)
+            self.assertNotEqual(old_state, new_state)
+            self.assertEqual(12345, torch.accelerator.initial_seed(device_idx))
+            torch.accelerator.set_rng_state(old_state, device_idx)
+            self.assertEqual(old_state, torch.accelerator.get_rng_state(device_idx))
+
 
 instantiate_device_type_tests(
     TestAccelerator,
