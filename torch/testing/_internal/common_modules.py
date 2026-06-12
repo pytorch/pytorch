f0758d4692e78d3990a277dbf99fe19d7aba346e
Konrad Drozd <konrad.drozd@intel.com>
2026-06-12 12:48:15 +0300

    Skip test_cpu_gpu_parity for chalf xpu
    Align ConvTranspose1D case with the rest of the ConvTransposeNd family as it shares the same problems. This test depending on luck it is either an accuracy error or unexpected pass (convTranspose is too flaky as same problems can be observed for other Nd variants).
    
    CUDA just has expectedFailure citing no CPU implementation depiste there being one however the accuracy mismatch between the two is too flaky.

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -4321,6 +4321,8 @@ module_db: list[ModuleInfo] = [
                    # Not implemented for chalf on CPU
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_cpu_gpu_parity',
                                 dtypes=(torch.chalf,), device_type='cuda'),
+                   DecorateInfo(skipIfXpu, 'TestModule', 'test_cpu_gpu_parity',
+                                dtypes=(torch.chalf,), device_type='xpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
