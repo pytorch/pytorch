dc829ae5371128418c64ee3231bd8e5d61ac2cc0
Jaeyoon Jung <jaeyoon.jung@lge.com>
2026-06-12 16:00:35 +0900

    Use TORCH_INSTALL_LIB_DIR instead of hardcoded path 'lib'
    If TORCH_INSTALL_LIB_DIR is not set, respect CMAKE_INSTALL_LIBDIR
    instead as fallback.
    
    Fixes https://github.com/pytorch/pytorch/issues/89489
    
    Signed-off-by: Jaeyoon Jung <jaeyoon.jung@lge.com>

--- cmake/External/EigenBLAS.cmake
+++ cmake/External/EigenBLAS.cmake
@@ -7,6 +7,11 @@ if(NOT INTERN_BUILD_MOBILE OR NOT INTERN_USE_EIGEN_BLAS)
   return()
 endif()
 
+include(GNUInstallDirs)
+if(NOT TORCH_INSTALL_LIB_DIR)
+  set(TORCH_INSTALL_LIB_DIR ${CMAKE_INSTALL_LIBDIR})
+endif()
+
 ##############################################################################
 # Eigen BLAS is built together with Libtorch mobile.
 # By default, it builds code from third-party/eigen/blas submodule.
@@ -49,5 +54,5 @@ add_library(eigen_blas STATIC ${EigenBlas_SRCS})
 set_property(TARGET eigen_blas PROPERTY POSITION_INDEPENDENT_CODE ON)
 
 install(TARGETS eigen_blas
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib)
+        LIBRARY DESTINATION ${TORCH_INSTALL_LIB_DIR}
+        ARCHIVE DESTINATION ${TORCH_INSTALL_LIB_DIR})
