33e34f67791d525df0a85e4bff2803896c5404b3
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-06-12 13:53:19 -0700

    Fix ClangTidy concerns

--- c10/core/TensorOptions.h
+++ c10/core/TensorOptions.h
@@ -144,9 +144,8 @@ struct C10_API TensorOptions {
 
   /// Constructs a `TensorOptions` object with the given device.
   /// See NOTE [ TensorOptions Constructors ] on why this is templatized.
-  template <
-      typename T,
-      typename = std::enable_if_t<std::is_same_v<std::decay_t<T>, Device>>>
+  template <typename T>
+  requires std::is_same_v<std::decay_t<T>, Device>
   /* implicit */ TensorOptions(T&& device) : TensorOptions() {
     this->set_device(std::forward<T>(device));
   }
@@ -159,9 +158,8 @@ struct C10_API TensorOptions {
   /// NB: Ideally we only allow implicit constructors here. But there is no easy
   ///     way to detect them. So we have this one that allows explicit
   ///     constructors too.
-  template <
-      typename... Args,
-      typename = std::enable_if_t<std::is_constructible_v<Device, Args&&...>>>
+  template <typename... Args>
+  requires std::is_constructible_v<Device, Args&&...>
   /* implicit */ TensorOptions(Args&&... args)
       : TensorOptions(Device(std::forward<Args>(args)...)) {}
 
