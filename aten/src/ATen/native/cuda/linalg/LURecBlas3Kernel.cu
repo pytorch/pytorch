861331b476a3eb1a23f8d1e0721b1d5295c7acb2
Nikita Vedeneev <nvedzeneyeu@nvidia.com>
2026-06-12 09:02:10 +0000

    init

---
+++ aten/src/ATen/native/cuda/linalg/LURecBlas3Kernel.cu
@@ -0,0 +1,15 @@
+#include <ATen/core/Tensor.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/LinearAlgebraUtils.h>
+#include <c10/cuda/CUDAStream.h>
+
+
+namespace at::native {
+
+namespace {
+
+
+} // anonymous namespace
+
+} // at::native
