38182c61cde93bf342956588fe0a1654a1277447
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-06-13 17:43:46 +0000

    [BE][Ez]: Use CPP20 rvalue overload for ostringstream (#187208)
    Followup to #186552 . Uses the rvalue overload of ostringstream newly introduced in CPP20 to reduce the amount of string copy allowing us to steal the internal string buffer.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/187208
    Approved by: https://github.com/d4l3k, https://github.com/lakshayg, https://github.com/malfet

--- c10/cuda/PeerToPeerAccess.cpp
+++ c10/cuda/PeerToPeerAccess.cpp
@@ -10,6 +10,7 @@
 #include <c10/util/Logging.h>
 #include <c10/util/irange.h>
 
+#include <array>
 #include <iomanip>
 #include <sstream>
 #include <vector>
@@ -88,12 +89,10 @@ nvmlDevice_t get_nvml_device(c10::DeviceIndex dev) {
   cudaDeviceProp prop{};
   C10_CUDA_CHECK(cudaGetDeviceProperties(&prop, dev));
 
-  char
-      pci_id // NOLINT(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
-          [NVML_DEVICE_PCI_BUS_ID_BUFFER_SIZE];
+  std::array<char, NVML_DEVICE_PCI_BUS_ID_BUFFER_SIZE> pci_id{};
   snprintf(
-      pci_id,
-      sizeof(pci_id),
+      pci_id.data(),
+      pci_id.size(),
       NVML_DEVICE_PCI_BUS_ID_FMT,
       prop.pciDomainID,
       prop.pciBusID,
@@ -103,7 +102,7 @@ nvmlDevice_t get_nvml_device(c10::DeviceIndex dev) {
   TORCH_INTERNAL_ASSERT(
       NVML_SUCCESS ==
       DriverAPI::get()->nvmlDeviceGetHandleByPciBusId_v2_(
-          pci_id, &nvml_device));
+          pci_id.data(), &nvml_device));
   return nvml_device;
 }
 
@@ -256,12 +255,13 @@ std::string get_nvml_fabric_info([[maybe_unused]] c10::DeviceIndex dev) {
     }
   }
 
-  char uuid_hex[33];
-  for (int i = 0; i < 16; ++i) {
-    snprintf(uuid_hex + i * 2, 3, "%02x", info.clusterUuid[i]);
+  std::array<char, 33> uuid_hex{};
+  for (size_t i = 0; i < 16; ++i) {
+    snprintf(uuid_hex.data() + i * 2, 3, "%02x", info.clusterUuid[i]);
   }
 
   const char* state_str = "unknown";
+  // NOLINTNEXTLINE(bugprone-switch-missing-default-case)
   switch (info.state) {
     case NVML_GPU_FABRIC_STATE_NOT_SUPPORTED:
       state_str = "not_supported";
@@ -278,7 +278,7 @@ std::string get_nvml_fabric_info([[maybe_unused]] c10::DeviceIndex dev) {
   }
 
   std::ostringstream oss;
-  oss << "clique_id=" << info.cliqueId << ", cluster_uuid=" << uuid_hex
+  oss << "clique_id=" << info.cliqueId << ", cluster_uuid=" << uuid_hex.data()
       << ", state=" << state_str << ", status=" << info.status
       << ", health_mask=0x" << std::hex << std::setfill('0') << std::setw(8)
       << info.healthMask;
@@ -288,7 +288,7 @@ std::string get_nvml_fabric_info([[maybe_unused]] c10::DeviceIndex dev) {
         << static_cast<int>(info.healthSummary);
   }
 #endif
-  return oss.str();
+  return std::move(oss).str();
 #else
   return "fabric info unsupported (requires CUDA >= 12.4)";
 #endif
--- torch/csrc/autograd/python_variable.cpp
+++ torch/csrc/autograd/python_variable.cpp
@@ -1165,7 +1165,7 @@ class NativeOpSchema {
       }
     }
     ss << ')';
-    return ss.str();
+    return std::move(ss).str();
   }
 
  private:
@@ -1229,7 +1229,7 @@ void log_sharding_prop_cache_hit(
   if (!output_spec.is_none()) {
     ss << " -> " << py::str(output_spec).cast<std::string>();
   }
-  dtensor_dispatch_logger.attr("debug")(ss.str());
+  dtensor_dispatch_logger.attr("debug")(std::move(ss).str());
 }
 } // namespace
 
