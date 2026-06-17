7693f207b2cb4c85e7073dc8cd2db3ffb5c33b83
Jagadish Krishnamoorthy <jagadish.krishnamoorthy@amd.com>
2026-06-17 18:55:02 +0000

    [ROCm]: Enable test_blockwise_nvfp4_compile (#186191)
    ROCm meta validation used the MXFP8 element count for BlockWise1x32; apply K_multiplier = 2 for float4_e2m1fn_x2 to match ScaledBlas.cpp, fixing test_blockwise_nvfp4_compile_cuda under compile.
    
    Test command:
    PYTORCH_TEST_WITH_ROCM=1 python test/test_scaled_matmul_cuda.py -v -k test_blockwise_nvfp4_compile_cuda
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/186191
    Approved by: https://github.com/drisspg, https://github.com/jeffdaily

--- test/test_scaled_matmul_cuda.py
+++ test/test_scaled_matmul_cuda.py
@@ -2577,7 +2577,6 @@ class TestFP8Matmul(TestCase):
         torch.testing.assert_close(C, C_ref, atol=0, rtol=0)
 
 
-    @skipIfRocm
     @unittest.skipIf(not PLATFORM_SUPPORTS_MX_GEMM, mx_skip_msg)
     def test_blockwise_nvfp4_compile(self, device) -> None:
 
