5de88ad5bfb6564e8698e0621fa38bef745af11d
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-17 21:34:57 +0000

    Revert "[lazy] Fix native_group_norm lazy dispatch (#187397)"
    This reverts commit 686d4e4b26a14bce2c967dc2d7fbcc5e6079c684.
    
    Reverted https://github.com/pytorch/pytorch/pull/187397 on behalf of https://github.com/huydhn due to Sorry for the churn, but the base PR https://github.com/pytorch/pytorch/pull/186411 has been reverted ([comment](https://github.com/pytorch/pytorch/pull/187397#issuecomment-4735728947))

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -16265,6 +16265,8 @@ op_db: list[OpInfo] = [
             DecorateInfo(unittest.expectedFailure, "TestCompositeCompliance", "test_backward"),
             # likely due to dispatching through infinitely_differentiable_native_group_norm_backward
             DecorateInfo(unittest.expectedFailure, "TestConsistency", "test_output_grad_match", device_type="mps", dtypes=(torch.float32,)),
+            # lazy dispatch failure
+            DecorateInfo(unittest.expectedFailure, "TestLazyOpInfo", "test_dispatched_to_lazy"),
             # native_group_norm expects contiguous inputs on CUDA
             DecorateInfo(unittest.expectedFailure, "TestMeta", "test_dispatch_symbolic_meta_outplace_all_strides", device_type="cuda"),
         ),
