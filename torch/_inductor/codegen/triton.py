9b737301fced23ed5f9dcfd2f1bb963d72466184
morrison-turnansky <mturnans@redhat.com>
2026-06-18 22:59:43 +0000

    turned on optimizatino for just triton

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -8023,6 +8023,7 @@ class TritonScheduling(SIMDScheduling):
     """Scheduling backend for Triton kernel code generation."""
 
     kernel_type: type[Any] = TritonKernel
+    supports_polyhedral: bool = True
     backend_features = OrderedSet(
         [
             BackendFeature.FOREACH,
