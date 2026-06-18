3b7ecc2b826f8d5812702cb73f147af908315e1c
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-18 17:23:56 +0000

    Revert "[BE][Ez]: Use CPP20 rvalue overload for ostringstream (#187208)"
    This reverts commit 6f2953ae46a3b5f25bfc7bd3acfe6e2f663d1ed3.
    
    Reverted https://github.com/pytorch/pytorch/pull/187208 on behalf of https://github.com/huydhn due to Sorry for the churn, we are attempting to migrate some internal builds to C++ 20 to unblock this change ([comment](https://github.com/pytorch/pytorch/pull/187208#issuecomment-4744424607))

--- c10/core/TensorOptions.h
+++ c10/core/TensorOptions.h
@@ -146,8 +146,7 @@ struct C10_API TensorOptions {
   /// See NOTE [ TensorOptions Constructors ] on why this is templatized.
   template <
       typename T,
-      typename = std::enable_if_t< // NOLINT(modernize-use-constraints)
-          std::is_same_v<std::decay_t<T>, Device>>>
+      typename = std::enable_if_t<std::is_same_v<std::decay_t<T>, Device>>>
   /* implicit */ TensorOptions(T&& device) : TensorOptions() {
     this->set_device(std::forward<T>(device));
   }
@@ -162,8 +161,7 @@ struct C10_API TensorOptions {
   ///     constructors too.
   template <
       typename... Args,
-      typename = std::enable_if_t< // NOLINT(modernize-use-constraints)
-          std::is_constructible_v<Device, Args&&...>>>
+      typename = std::enable_if_t<std::is_constructible_v<Device, Args&&...>>>
   /* implicit */ TensorOptions(Args&&... args)
       : TensorOptions(Device(std::forward<Args>(args)...)) {}
 
@@ -618,7 +616,7 @@ inline TensorOptions dtype() {
 inline std::string toString(const TensorOptions& options) {
   std::ostringstream stream;
   stream << options;
-  return std::move(stream).str();
+  return stream.str();
 }
 
 // This is intended to be a centralized location by which we can determine
