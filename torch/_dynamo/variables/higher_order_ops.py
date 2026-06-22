3842c7dead0a0b4bd3c59e0bec90c6c3a85f67e5
Pian Pawakapan <pianpwk@meta.com>
2026-06-22 00:18:15 -0700

    [spmd_types] local_map (#181398)
    Adds a `local_map(spmd_types: bool = False)` kwarg, that when True, runs the local_map region with spmd_types type checking.
    
    For Replicate -> R/I, infers from grad_in_placements, and if not available, defaults to R.
    
    Adds checking of output spmd_types, grad_out spmd_types with a grad hook.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/181398
    Approved by: https://github.com/ezyang

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -5890,6 +5890,7 @@ class LocalMapWrappedHigherOrderVariable(WrapHigherOrderVariable):
             in_grad_placements,
             device_mesh,
             redistribute_inputs,
+            enable_spmd_types,
             *user_args,
         ) = args
 
