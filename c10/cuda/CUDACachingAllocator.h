732a6950929efdb138eb637af7e310ef0688b958
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-23 15:07:20 +0000

    Revert "Fix CLANGTIDY errors in c10/cuda (#186624)"
    This reverts commit b9f0608965dbcbede0efd540b7fb159ba7c962a2.
    
    Reverted https://github.com/pytorch/pytorch/pull/186624 on behalf of https://github.com/eqy due to meta internal builds use clang15, see #186624 ([comment](https://github.com/pytorch/pytorch/pull/186624#issuecomment-4780568521))

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -172,7 +172,7 @@ class CUDAAllocator : public DeviceAllocator {
   virtual void createOrIncrefPool(
       c10::DeviceIndex /*device*/,
       MempoolId_t /*mempool_id*/,
-      const std::shared_ptr<CUDAAllocator>& allocator = nullptr) {
+      std::shared_ptr<CUDAAllocator> allocator = nullptr) {
     TORCH_CHECK(
         false,
         name(),
@@ -476,8 +476,8 @@ inline void releasePool(c10::DeviceIndex device, MempoolId_t mempool_id) {
 inline void createOrIncrefPool(
     c10::DeviceIndex device,
     MempoolId_t mempool_id,
-    const std::shared_ptr<CUDAAllocator>& allocator_ptr = nullptr) {
-  get()->createOrIncrefPool(device, mempool_id, allocator_ptr);
+    std::shared_ptr<CUDAAllocator> allocator_ptr = nullptr) {
+  get()->createOrIncrefPool(device, mempool_id, std::move(allocator_ptr));
 }
 inline void setUseOnOOM(
     c10::DeviceIndex device,
