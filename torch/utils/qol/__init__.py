66c1d78065fe0a025a6aab7f5e9957a54463dfee
Akshay R <157674473+Akshay-Ramesh-VITC@users.noreply.github.com>
2026-06-24 12:13:26 +0530

    Update __init__.py

--- torch/utils/qol/__init__.py
+++ torch/utils/qol/__init__.py
@@ -1,4 +1,20 @@
 from .smartsummary import SmartSummary
 from .trainingmonitor import TrainingMonitor
+from .utils import (
+    lazy_flatten,
+    get_flatten_size,
+    loss_ncc,
+    ncc_score,
+    LRFinder,
+    find_lr
+)
 
-__all__ = ["SmartSummary", "TrainingMonitor"]
+__all__ = ["SmartSummary", 
+    "TrainingMonitor,
+    "lazy_flatten",
+    "get_flatten_size",
+    "loss_ncc",
+    "ncc_score",
+    "LRFinder",
+    "find_lr"
+]
