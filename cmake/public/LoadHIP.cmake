1fd1fab35d4ee13e7196dedf8a1692ae6e9443c4
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-25 17:32:47 +0000

    Revert "[ROCm] derive the hipcub compatible cccl version from 'HIPCUB_CCCL_VERSION' (#188072)"
    This reverts commit 0f927a68b20060ec83758269ffe15dcc5680a577.
    
    Reverted https://github.com/pytorch/pytorch/pull/188072 on behalf of https://github.com/georgehong due to Hi @Naraenda, sorry we need to revert this change due to breaking builds on older ROCm. ([comment](https://github.com/pytorch/pytorch/pull/188072#issuecomment-4802408894))

--- cmake/public/LoadHIP.cmake
+++ cmake/public/LoadHIP.cmake
@@ -333,9 +333,6 @@ if(PYTORCH_FOUND_HIP)
 
   # Optional components.
   find_package_and_print_version(hipsparselt)  # Will be required when ready.
-  # ROCm 8.0 and later requires libhipcxx! This should be marked as
-  # 'REQUIRED' once minimal ROCm version is bumped to 8.0 or later.
-  find_package_and_print_version(libhipcxx)
 
   list(REMOVE_DUPLICATES ROCM_INCLUDE_DIRS)
 
