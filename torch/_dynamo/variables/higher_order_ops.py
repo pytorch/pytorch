2a3743be9ea1c27b2052cd786218c8ef327364c9
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-06-25 16:52:20 +0000

    Revert "[spmd_types] local_map (#181398)"
    This reverts commit 37c67b79e2c83f1d7e22548f8dc196ea034a1b11.
    
    Reverted https://github.com/pytorch/pytorch/pull/181398 on behalf of https://github.com/georgehong due to Breaking internal tritonbench signals: torch._dynamo.exc.InternalTorchDynamoError: ModuleNotFoundError: No module named 'spmd_types.checker' ([comment](https://github.com/pytorch/pytorch/pull/181398#issuecomment-4802079523))

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -6091,7 +6091,6 @@ class LocalMapWrappedHigherOrderVariable(WrapHigherOrderVariable):
             in_grad_placements,
             device_mesh,
             redistribute_inputs,
-            enable_spmd_types,
             *user_args,
         ) = args
 
