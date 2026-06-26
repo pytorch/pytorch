fbbdabe4d51437081df30f84530a16123408fcce
orrangetabby17 <shuxin.yang@intel.com>
2026-06-26 09:23:32 +0800

    Update test/inductor/test_torchinductor.py
    Co-authored-by: Yu, Guangye <guangye.yu@intel.com>

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -12548,7 +12548,6 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
             self.assertGreater(torch._inductor.metrics.generated_kernel_count, 0)
 
     def test_max_pool2d_with_indices_backward_xpu_fallback(self):
-        # XPU uses fallback for max_pool2d_with_indices_backward (generated kernel count == 0)
         def fn(a, b, c):
             return aten.max_pool2d_with_indices_backward(
                 a, b, [2, 2], [2, 2], [0, 0], [1, 1], False, c
