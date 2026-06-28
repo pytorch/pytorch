51a0dadd1394a49f562f3785b7d43f12b91df1a8
Isalia20 <irakli.salia854@gmail.com>
2026-06-28 21:15:20 +0400

    Fix channels last batch norm

--- test/test_nn.py
+++ test/test_nn.py
@@ -4946,55 +4946,6 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""")
         x_grad_ref = torch.where(mask, grad, z)
         self.assertEqual(x.grad, x_grad_ref)
 
-    def test_batchnorm_nhwc_cpu(self):
-        def helper(self, mod, size, dtype, mixed_dtype=False, format=torch.channels_last, precision=None):
-            channels = size[1]
-            input = torch.randn(size, dtype=dtype, device='cpu', requires_grad=True)
-            input = input.contiguous(memory_format=format).to(dtype)
-            input.retain_grad()
-            grad = torch.randn(size, dtype=dtype, device='cpu')
-            grad = grad.contiguous(memory_format=format)
-            bn = mod(channels).cpu().to(dtype)
-            bn.weight.data.uniform_()
-            bn.bias.data.uniform_()
-
-            ref_input = input.detach().clone().contiguous().requires_grad_(True)
-            ref_grad = grad.detach().clone().contiguous()
-            ref_bn = mod(channels).cpu().to(dtype)
-            ref_bn.load_state_dict(bn.state_dict())
-
-            if mixed_dtype:
-                bn.float()
-                ref_bn.float()
-
-            out = bn(input)
-            out.backward(grad)
-            ref_out = ref_bn(ref_input)
-            ref_out.backward(ref_grad)
-
-            self.assertTrue(out.is_contiguous(memory_format=format))
-            self.assertTrue(ref_out.is_contiguous())
-            self.assertEqual(out, ref_out)
-            self.assertEqual(bn.weight.grad, ref_bn.weight.grad, atol=precision, rtol=precision)
-            self.assertEqual(bn.bias.grad, ref_bn.bias.grad)
-            self.assertEqual(input.grad, ref_input.grad)
-
-        # test NC11 and N1HW; test mixed dtype
-        for shape in [(4, 8, 10, 10), (4, 1, 9, 9), (4, 9, 1, 1)]:
-            for dtype in [torch.float, torch.bfloat16, torch.float16]:
-                for mixed_dtype in [False, True]:
-                    if dtype == torch.float:
-                        mixed_dtype = False
-                    helper(self, nn.BatchNorm2d, shape, dtype, mixed_dtype, torch.channels_last)
-
-        precisons = {torch.float: 1e-4, torch.bfloat16: 1e-4, torch.float16: None}
-        for shape in [(4, 8, 2, 10, 10), (4, 1, 2, 9, 9), (4, 9, 1, 1, 1)]:
-            for dtype in [torch.float, torch.bfloat16, torch.float16]:
-                for mixed_dtype in [False, True]:
-                    if dtype == torch.float:
-                        mixed_dtype = False
-                    helper(self, nn.BatchNorm3d, shape, dtype, mixed_dtype, torch.channels_last_3d, precisons[dtype])
-
     def test_batchnorm_half_overflow(self):
         def helper(self, mod, size, param_dtype, fwd_format, bwd_format):
             channels = size[1]
@@ -9227,6 +9178,55 @@ class TestNNDeviceType(NNTestCase):
         bn = nn.BatchNorm2d(1).to(device, dtype)
         data = torch.rand(880801, 1, 1, 1, device=device, dtype=dtype)
         out = bn(data).sum().backward()
+
+    def test_batchnorm_nhwc(self, device):
+        def helper(self, mod, size, dtype, mixed_dtype=False, format=torch.channels_last, precision=None):
+            channels = size[1]
+            input = torch.randn(size, dtype=dtype, device=device, requires_grad=True)
+            input = input.contiguous(memory_format=format).to(dtype)
+            input.retain_grad()
+            grad = torch.randn(size, dtype=dtype, device=device)
+            grad = grad.contiguous(memory_format=format)
+            bn = mod(channels).to(device).to(dtype)
+            bn.weight.data.uniform_()
+            bn.bias.data.uniform_()
+
+            ref_input = input.detach().clone().contiguous().requires_grad_(True)
+            ref_grad = grad.detach().clone().contiguous()
+            ref_bn = mod(channels).to(device).to(dtype)
+            ref_bn.load_state_dict(bn.state_dict())
+
+            if mixed_dtype:
+                bn.float()
+                ref_bn.float()
+
+            out = bn(input)
+            out.backward(grad)
+            ref_out = ref_bn(ref_input)
+            ref_out.backward(ref_grad)
+
+            self.assertTrue(out.is_contiguous(memory_format=format))
+            self.assertTrue(ref_out.is_contiguous())
+            self.assertEqual(out, ref_out)
+            self.assertEqual(bn.weight.grad, ref_bn.weight.grad, atol=precision, rtol=precision)
+            self.assertEqual(bn.bias.grad, ref_bn.bias.grad)
+            self.assertEqual(input.grad, ref_input.grad)
+
+        # test NC11 and N1HW; test mixed dtype
+        for shape in [(4, 8, 10, 10), (4, 1, 9, 9), (4, 9, 1, 1)]:
+            for dtype in [torch.float, torch.bfloat16, torch.float16]:
+                for mixed_dtype in [False, True]:
+                    if dtype == torch.float:
+                        mixed_dtype = False
+                    helper(self, nn.BatchNorm2d, shape, dtype, mixed_dtype, torch.channels_last)
+
+        precisons = {torch.float: 1e-4, torch.bfloat16: 1e-4, torch.float16: None}
+        for shape in [(4, 8, 2, 10, 10), (4, 1, 2, 9, 9), (4, 9, 1, 1, 1)]:
+            for dtype in [torch.float, torch.bfloat16, torch.float16]:
+                for mixed_dtype in [False, True]:
+                    if dtype == torch.float:
+                        mixed_dtype = False
+                    helper(self, nn.BatchNorm3d, shape, dtype, mixed_dtype, torch.channels_last_3d, precisons[dtype])
 
     @dtypesIfCUDA(torch.float, torch.double, torch.half, torch.complex128)
     @dtypesIfMPS(torch.float, torch.half, torch.complex64)
