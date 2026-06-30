55232dcc33f42becfbf27d9c77fbaac859af34ab
lakshayg <lakshayg@nvidia.com>
2026-06-30 10:01:26 -0700

    testing if __atomic_fetch_add works on old apple clang

--- aten/src/ATen/native/cpu/AtomicAddFloat.h
+++ aten/src/ATen/native/cpu/AtomicAddFloat.h
@@ -5,7 +5,11 @@
 
 static inline void cpu_atomic_add_float(float* dst, float fvalue)
 {
+#ifdef __APPLE__
+  __atomic_fetch_add(dst, fvalue, __ATOMIC_SEQ_CST);
+#else
   std::atomic_ref<float>(*dst).fetch_add(fvalue);
+#endif
 }
 
 #endif // ATOMIC_ADD_FLOAT
