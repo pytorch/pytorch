2cfadd1dd42b1a29b76dc38b9e972a42980aa939
Guilherme Leobas <guilhermeleobas@gmail.com>
2026-07-06 10:44:04 -0300

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -3510,6 +3510,7 @@ class UserDefinedObjectVariable(UserDefinedVariable):
             AttributeError,
             tx,
             args=[f"'{type(self.value).__name__}' object has no attribute '{name}'"],
+            kwargs={"name": variables.ConstantVariable.create(name), "obj": self},
         )
 
     def getattro_impl(
