9659c1580a3821941bfb956111d40743438faa54
lakshayg <lakshayg@nvidia.com>
2026-07-07 16:16:51 -0700

    Use __cpp_lib_atomic_ref instead of __APPLE__

--- aten/src/ATen/native/cpu/AtomicAddFloat.h
+++ aten/src/ATen/native/cpu/AtomicAddFloat.h
@@ -5,10 +5,10 @@
 
 static inline void cpu_atomic_add_float(float* dst, float fvalue)
 {
-#ifdef __APPLE__
-  __atomic_fetch_add(dst, fvalue, __ATOMIC_SEQ_CST);
-#else
+#ifdef __cpp_lib_atomic_ref
   std::atomic_ref<float>(*dst).fetch_add(fvalue);
+#else
+  __atomic_fetch_add(dst, fvalue, __ATOMIC_SEQ_CST);
 #endif
 }
 
