e450c78fbf61a63eb8ff4cc6eb0889ba7c0e6be0
cyy <cyyever@outlook.com>
2026-07-07 23:17:59 +0800

    Use SafePyObject in PyAnomalyMetadata and PySavedVariableHooks
    Cleanup now routes through the shared PyInterpreter::decref path. Also
    switch the "call_pack_hook was not called" precondition to TORCH_CHECK
    since it is reachable from user code.

--- torch/csrc/autograd/init.cpp
+++ torch/csrc/autograd/init.cpp
@@ -412,7 +412,7 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
       .def("save", &ProfilerResult::save)
       .def(
           "trace_activities",
-          [](py::object self) {
+          [](const py::object& self) {
             auto& r = self.cast<ProfilerResult&>();
             auto* activities = r.traceActivities();
             if (!activities) {
@@ -633,7 +633,7 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
 
   _C_m.def(
       "_register_py_class_for_device",
-      [](const std::string& device, py::object python_type_class) {
+      [](const std::string& device, const py::object& python_type_class) {
         auto cls = python_type_class.ptr();
         registerPythonTensorClass(device, cls);
       });
@@ -707,7 +707,7 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
             // of the hook functions for us
             s.register_hooks(
                 std::make_unique<torch::autograd::PySavedVariableHooks>(
-                    pack_hook, unpack_hook));
+                    std::move(pack_hook), std::move(unpack_hook)));
           })
       .def_property_readonly(
           "data",
--- torch/csrc/autograd/python_saved_variable_hooks.h
+++ torch/csrc/autograd/python_saved_variable_hooks.h
@@ -14,17 +14,24 @@ namespace py = pybind11;
 namespace torch::autograd {
 
 struct PySavedVariableHooks : public SavedVariableHooks {
-  PySavedVariableHooks(py::function& pack_hook, py::function& unpack_hook);
+  PySavedVariableHooks(py::function&& pack_hook, py::function&& unpack_hook);
   void call_pack_hook(const at::Tensor& tensor) override;
   at::Tensor call_unpack_hook() override;
-  ~PySavedVariableHooks() override;
+  ~PySavedVariableHooks() override = default;
   std::optional<std::pair<c10::SafePyObject, c10::SafePyObject>>
   retrieve_unpack_hook_data() const override;
 
  private:
-  PyObject* pack_hook_;
-  PyObject* unpack_hook_;
-  PyObject* data_ = nullptr;
+  const c10::SafePyObject& data() const {
+    TORCH_CHECK(data_.has_value(), "call_pack_hook was not called");
+    return *data_;
+  }
+
+  // SafePyObject destructs through PyInterpreter::decref, so no manual
+  // destructor is needed here.
+  c10::SafePyObject pack_hook_;
+  c10::SafePyObject unpack_hook_;
+  std::optional<c10::SafePyObject> data_;
 };
 
 struct PyDefaultSavedVariableHooks {
