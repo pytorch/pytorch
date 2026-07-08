9fef7f7409a31d3c49e36488d495b992378198b2
gplutop7 <grzegorzx.pluto-prondzinski@intel.com>
2026-07-08 21:14:06 +0000

    freeze_rng_state in distribution rsample gradient tests

--- test/distributions/test_distributions.py
+++ test/distributions/test_distributions.py
@@ -119,6 +119,7 @@ from torch.testing._internal.common_device_type import (
     skipMPS,
 )
 from torch.testing._internal.common_utils import (
+    freeze_rng_state,
     gradcheck,
     load_tests,
     run_tests,
@@ -2299,9 +2300,8 @@ class TestDistributions(DistributionsTestCase):
         self._gradcheck_log_prob(Uniform, (low, 1.0))
         self._gradcheck_log_prob(Uniform, (0.0, high))
 
-        state = torch.get_rng_state()
-        rand = low.new(low.size()).uniform_()
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            rand = low.new(low.size()).uniform_()
         u = Uniform(low, high).rsample()
         u.backward(torch.ones_like(u))
         self.assertEqual(low.grad, 1 - rand)
@@ -2352,9 +2352,8 @@ class TestDistributions(DistributionsTestCase):
         self._gradcheck_log_prob(Cauchy, (loc, 1.0))
         self._gradcheck_log_prob(Cauchy, (0.0, scale))
 
-        state = torch.get_rng_state()
-        eps = loc.new(loc.size()).cauchy_()
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            eps = loc.new(loc.size()).cauchy_()
         c = Cauchy(loc, scale).rsample()
         c.backward(torch.ones_like(c))
         self.assertEqual(loc.grad, torch.ones_like(scale))
@@ -2381,9 +2380,8 @@ class TestDistributions(DistributionsTestCase):
         self._gradcheck_log_prob(HalfCauchy, (scale,))
         self._gradcheck_log_prob(HalfCauchy, (1.0,))
 
-        state = torch.get_rng_state()
-        eps = scale.new(scale.size()).cauchy_().abs_()
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            eps = scale.new(scale.size()).cauchy_().abs_()
         c = HalfCauchy(scale).rsample()
         c.backward(torch.ones_like(c))
         self.assertEqual(scale.grad, eps)
@@ -2767,9 +2765,8 @@ class TestDistributions(DistributionsTestCase):
         self._gradcheck_log_prob(Normal, (loc, 1.0))
         self._gradcheck_log_prob(Normal, (0.0, scale))
 
-        state = torch.get_rng_state()
-        eps = torch.normal(torch.zeros_like(loc), torch.ones_like(scale))
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            eps = torch.normal(torch.zeros_like(loc), torch.ones_like(scale))
         z = Normal(loc, scale).rsample()
         z.backward(torch.ones_like(z))
         self.assertEqual(loc.grad, torch.ones_like(loc))
@@ -3470,9 +3467,8 @@ class TestDistributions(DistributionsTestCase):
         self.assertEqual(Exponential(50.0).sample((1,)).size(), (1,))
 
         self._gradcheck_log_prob(Exponential, (rate,))
-        state = torch.get_rng_state()
-        eps = rate.new(rate.size()).exponential_()
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            eps = rate.new(rate.size()).exponential_()
         z = Exponential(rate).rsample()
         z.backward(torch.ones_like(z))
         self.assertEqual(rate.grad, -eps / rate**2)
@@ -3538,9 +3534,8 @@ class TestDistributions(DistributionsTestCase):
         self._gradcheck_log_prob(Laplace, (loc, 1.0))
         self._gradcheck_log_prob(Laplace, (0.0, scale))
 
-        state = torch.get_rng_state()
-        eps = torch.ones_like(loc).uniform_(-0.5, 0.5)
-        torch.set_rng_state(state)
+        with freeze_rng_state():
+            eps = torch.ones_like(loc).uniform_(-0.5, 0.5)
         z = Laplace(loc, scale).rsample()
         z.backward(torch.ones_like(z))
         self.assertEqual(loc.grad, torch.ones_like(loc))
