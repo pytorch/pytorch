efc58f6af7d0a0653d6cfa39d15a3d066f3b669d
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-07-08 20:14:13 +0000

    Revert "Fix NumPy 2 DeprecationWarning in Tensor.__array_wrap__ (#187190) (#187190)"
    This reverts commit 135435f8981cbfb35368f1a2abc8fa9c2f2c4314.
    
    Reverted https://github.com/pytorch/pytorch/pull/187190 on behalf of https://github.com/tanpuresiddhant due to Reverting due to code freeze ([comment](https://github.com/pytorch/pytorch/pull/187190#issuecomment-4918803522))

--- torch/_tensor.py
+++ torch/_tensor.py
@@ -1253,10 +1253,7 @@ class Tensor(torch._C.TensorBase):
 
     # Wrap Numpy array again in a suitable tensor when done, to support e.g.
     # `numpy.sin(tensor) -> tensor` or `numpy.greater(tensor, 0) -> ByteTensor`
-    # NumPy 2 passes `context` and `return_scalar`; torch has no scalars and
-    # does not need the context, so both are accepted and ignored. Declaring
-    # them avoids a NumPy 2 DeprecationWarning about the old call signature.
-    def __array_wrap__(self, array, context=None, return_scalar=False):
+    def __array_wrap__(self, array):
         if has_torch_function_unary(self):
             return handle_torch_function(
                 Tensor.__array_wrap__, (self,), self, array=array
