73a693d06f89070407b59bf91b82dc991336ac49
morrison-turnansky <mturnans@redhat.com>
2026-07-09 19:57:53 +0000

    ci
    ghstack-source-id: 875fde457cc99969278cc3ce27a3909e54bcfdab
    Pull-Request: https://github.com/pytorch/pytorch/pull/189468

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -4188,10 +4188,28 @@ def _get_fake_value_impl(
             )
 
         msg = get_concrete_sizes_from_symints(str(e), fake_mode)
-        tx.output.graph.erase_node(node)
-        from .exc import raise_observed_exception
 
-        raise_observed_exception(RuntimeError, tx, args=[msg])
+        has_exn_handler = (
+            sys.version_info >= (3, 11)
+            and tx.current_instruction.exn_tab_entry is not None
+        ) or (sys.version_info < (3, 11) and len(tx.block_stack) > 0)
+
+        if has_exn_handler:
+            tx.output.graph.erase_node(node)
+            from .exc import raise_observed_exception
+
+            raise_observed_exception(RuntimeError, tx, args=[msg])
+        else:
+            _wrap_graph_break_with_torch_runtime_err(
+                lambda: unimplemented(
+                    gb_type="RuntimeError when making fake tensor call",
+                    context="",
+                    explanation=msg,
+                    hints=[*graph_break_hints.USER_ERROR],
+                    from_exc=cause,
+                )
+            )
+            raise AssertionError("should not be reachable") from None
 
     if not allow_non_graph_fake:
         _ = pytree.tree_map_only(
