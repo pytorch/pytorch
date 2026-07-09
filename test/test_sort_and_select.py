ce1a97cc205854a218d254ad5d21282b7cb48ac0
daisyden <daisy.deng@intel.com>
2026-07-09 03:41:20 +0000

    Revert "Extend test_sort_and_select CUDA early-return guard to also cover XPU"
    This reverts commit a6ce0ce00db5541b6d8e8f6cc896c105cb078b16.

--- test/test_sort_and_select.py
+++ test/test_sort_and_select.py
@@ -287,7 +287,7 @@ class TestSortAndSelectDevice(TestCase):
                     )
 
                     # assert stride is preserved
-            if self.device_type in ("cuda", "xpu"):
+                    if self.device_type == "cuda":
                         # FIXME: this behavior should be true for all cases, not
                         # just the one specified in if condition
                         self.assertEqual(r1.values.stride(), t.stride())
@@ -365,7 +365,7 @@ class TestSortAndSelectDevice(TestCase):
                 # binary strings
                 yield (torch.tensor([0, 1] * size, dtype=dtype, device=device), 0)
 
-            if self.device_type in ("cuda", "xpu"):
+            if self.device_type == "cuda":
                 return
 
             yield (torch.tensor([0, 1] * 100, dtype=dtype, device=device), 0)
