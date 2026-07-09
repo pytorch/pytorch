04ecf448edefbe70e77c3d3a4616a309c8f9c1b7
daisyden <daisy.deng@intel.com>
2026-07-09 02:24:08 +0000

    [XPU] Add @skipIfXpu for test_flip (QuantizedXPU gap, intel/torch-xpu-ops#2722)

--- test/test_shape_ops.py
+++ test/test_shape_ops.py
@@ -32,6 +32,7 @@ from torch.testing._internal.common_utils import (
     IS_JETSON,
     run_tests,
     skipIfTorchDynamo,
+    skipIfXpu,
     TestCase,
     torch_to_numpy_dtype_dict,
 )
@@ -432,6 +433,7 @@ class TestShapeOps(TestCase):
         with self.assertRaisesRegex(RuntimeError, error_msg):
             torch.clamp(X)
 
+    @skipIfXpu(msg="See https://github.com/intel/torch-xpu-ops/issues/2722")
     @dtypes(*all_passthru_types())
     @dtypesIfCUDA(*all_passthru_types_and(torch.chalf))
     @dtypesIfXPU(*all_passthru_types_and(torch.chalf))
