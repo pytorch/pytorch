b3fa96b374c247271f073554d72110af510cfcef
Guilherme Leobas <guilhermeleobas@gmail.com>
2026-07-09 13:46:24 +0000

    [generator] Close all open generators in compile_subgraph (#157149)
    **Motivation and Example**
    
    Explictly close all open generators in compile_subgraph to ensure that
    all remaining finally blocks are executed. In CPython this is done by
    invoking the `tp_finalize` function of the generator object, which triggers
    `genclose`:
    https://github.com/python/cpython/blob/58a42dea97f4fa0df38ef4a95a2ede65e0549f71/Objects/genobject.c#L128-L134
    
    ```python
    import gc
    
    def whoo(t):
        nonlocal z
        z = 0
        try:
            z += 1
            yield t.sin()
        except ValueError:
            z += 10
            yield t.cos()
        except RuntimeError:
            z += 100
            yield t.tan()
        finally:
            z += 1000
        z += 10_000
    
    gen = whoo(t)
    a = next(gen)
    b = gen.throw(RuntimeError)
    gc.collect()
    print(z)  # 1101
    ```
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/157149
    Approved by: https://github.com/zou3519
    ghstack dependencies: #187690, #187744, #188004, #188638, #188639

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -1149,6 +1149,7 @@ class LocalGeneratorObjectVariable(VariableTracker):
         self.f_globals = f_globals
         self.inline_tracer = inline_tracer
         self.remaining_items: list[VariableTracker] = []
+        inline_tracer.output.track_generator(self)
 
     def get_code(self) -> types.CodeType:
         return self.code
@@ -1401,7 +1402,12 @@ class LocalGeneratorObjectVariable(VariableTracker):
 
         try:
             self.gen_send_ex(tx, ConstantVariable.create(None), True)
-        except ObservedGeneratorExit:
+        except ObservedGeneratorExit as e:
+            # Drop the traceback to break the exception -> traceback -> frame ->
+            # (raised_exception, self) reference cycle. Otherwise the generator's
+            # inline_tracer (and the OutputGraph it points at) survives until
+            # cyclic GC instead of being freed by refcount at frame exit.
+            e.__traceback__ = None
             return ConstantVariable.create(None)
         except ObservedUserStopIteration:
             # generator returned a value while closing. gen_send_ex() raises
