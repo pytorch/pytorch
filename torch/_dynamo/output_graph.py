37ca9b11bbfe0b0892713c78f201bf517763bc5a
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-07-11 06:51:25 +0000

    Revert "[dynamo] Fix segfault in list comprehension graph break (#187631)"
    This reverts commit ba5278147b95f73977791c3a597a806ead4c3149.
    
    Reverted https://github.com/pytorch/pytorch/pull/187631 on behalf of https://github.com/williamwen42 due to broke a bunch of dynamo-unittest CI tests ([comment](https://github.com/pytorch/pytorch/pull/187631#issuecomment-4943207134))

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -2509,10 +2509,7 @@ class OutputGraph(OutputGraphCommon):
                             raise AssertionError(
                                 "cur_tx.post_prune_cell_and_freevars must be set for nested frames"
                             )
-                        var = cur_tx.post_prune_cell_and_freevars[cell]
-                        if isinstance(var, NullVariable):
-                            raise AssertionError("Can't codegen null cell")
-                        cg(var)
+                        cg(cur_tx.post_prune_cell_and_freevars[cell])
                 cg.append_output(create_build_tuple(len(freevars)))
                 cur_tx = cur_tx.parent
                 tx_cnt += 1
