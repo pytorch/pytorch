ed893bdfb34a42978a2d80c6153c888e02895b90
Bob Ren <bobren@meta.com>
2026-07-14 14:45:17 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_subclasses/fake_utils.py
+++ torch/_subclasses/fake_utils.py
@@ -9,8 +9,6 @@ import torch.utils._pytree as pytree
 from torch._subclasses.fake_tensor import (
     FakeTensor,
     FakeTensorMode,
-    is_fake_tensor,
-    maybe_get_fake_mode,
     MetadataMismatchError,
     tree_flatten_only,
     UnsupportedFakeTensorException,
@@ -126,13 +124,13 @@ def try_convert_fake_to_real(
     Note: this is not currently optimized (makes copies of the meta converter internal dictionaries)
     """
 
-    fake_tensor = next((item for item in ten_list if is_fake_tensor(item)), None)
+    fake_tensor = next(
+        (item for item in ten_list if isinstance(item, FakeTensor)), None
+    )
     if fake_tensor is None:
         return ten_list
 
-    fake_mode = maybe_get_fake_mode(fake_tensor)
-    if fake_mode is None:
-        return ten_list
+    fake_mode = fake_tensor.fake_mode
     meta_converter = fake_mode.fake_tensor_converter.meta_converter
     desc = meta_converter.describer
 
@@ -140,7 +138,7 @@ def try_convert_fake_to_real(
     key_to_real_storage = {v: k for k, v in desc.lookup_storage.items()}
     out = []
     for t in ten_list:
-        if not is_fake_tensor(t) or t.layout != torch.strided:
+        if not isinstance(t, FakeTensor) or t.layout != torch.strided:
             out.append(t)
             continue
 
