0dbc8de1df1e77b870155236eaf778e5205109ed
Allan Lin <allin@redhat.com>
2026-07-16 22:54:57 +0000

    Update
    [ghstack-poisoned]

--- torch/distributed/_functional_collectives.py
+++ torch/distributed/_functional_collectives.py
@@ -658,7 +658,7 @@ def all_reduce_backward(ctx, grad_output: torch.Tensor):
     group_name = ctx.group_name
     reduce_op = ctx.reduce_op
 
-    if reduce_op != "sum":
+    if reduce_op not in ["sum", "avg"]:
         raise RuntimeError(
             f"all_reduce backward only supports 'sum' reduction, got '{reduce_op}'"
         )
