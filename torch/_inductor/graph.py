38cab8d1ad1d2c83abd19b1bc940e174d4788a38
Yang, Shuxin <shuxin.yang@intel.com>
2026-07-16 18:25:41 -0700

    only format

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -805,10 +805,7 @@ class GraphLowering(torch.fx.Interpreter):
             if n.target is torch.ops.aten.convolution.default
             or (
                 n.target is torch.ops.aten.convolution_backward.default
-                and any(
-                    n.args[idx].meta["val"].device.type == "xpu"
-                    for idx in [0, 1]
-                )
+                and any(n.args[idx].meta["val"].device.type == "xpu" for idx in [0, 1])
             )
         ]
 
