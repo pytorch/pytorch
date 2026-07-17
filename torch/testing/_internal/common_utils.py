5b92c0ae3db51204b96121af47907220d83a361a
daisyden <daisy.deng@intel.com>
2026-07-17 09:16:25 +0000

    dynamo_skips for some xpu test functions due to torch-xpu-ops/issues/4381， torch-xpu-ops/issues/4382， torch-xpu-ops/issues/4383 avoid duplicated cases generated in add_test.

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -6676,5 +6676,4 @@ def run_concurrently(worker_func, num_threads=None, args=(), kwargs=None):
     # If a worker thread raises an exception, re-raise it.
     if exc_value is not None:
         raise exc_value
-
     return results
