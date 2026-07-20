6594138d130ff40aae71b5e0762d8fd65da33dac
ethanwee1 <ethanwee@amd.com>
2026-07-20 16:18:07 +0000

    [ROCm][CI] Bump preview wheel pin to July 12
    Update the temporary TheRock ROCm preview wheel pin from the June 12 build to the July 12 build.

--- .ci/docker/common/install_rocm.sh
+++ .ci/docker/common/install_rocm.sh
@@ -30,9 +30,9 @@ install_ubuntu() {
       if [[ -z "${THEROCK_NIGHTLY_INDEX_URL:-}" ]]; then
         THEROCK_NIGHTLY_INDEX_URL="https://rocm.nightlies.amd.com/whl-multi-arch/"
       fi
-      # FIXME: temporarily pin TheRock nightly ROCm to the June 12 build while
+      # FIXME: temporarily pin TheRock nightly ROCm to the July 12 build while
       # the rolling nightly index is unstable.
-      THEROCK_NIGHTLY_VERSION="7.14.0a20260612"
+      THEROCK_NIGHTLY_VERSION="7.14.0a20260712"
 
       echo "=============================================="
       echo "ROCm Multi-Arch Wheel Installation (TheRock preview)"
