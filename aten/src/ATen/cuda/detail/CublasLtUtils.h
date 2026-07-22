794512ae0b07cf6fb1d95790d5860758173fe174
Grayson Derossi <graysonderossi@gmail.com>
2026-07-22 15:33:54 +0000

    Bump min supported CUDA to 13.3

--- aten/src/ATen/cuda/detail/CublasLtUtils.h
+++ aten/src/ATen/cuda/detail/CublasLtUtils.h
@@ -242,10 +242,10 @@ inline int cublasLtMatmulScaleMode(
 #endif
     case at::blas::ScalingType::GroupWise:
       TORCH_CHECK(scale_dtype == kFloat);
-#if !defined(USE_ROCM) && CUDA_VERSION >= 13020
+#if !defined(USE_ROCM) && CUDA_VERSION >= 13030
       return CUBLASLT_MATMUL_MATRIX_SCALE_PER_BATCH_SCALAR_32F;
 #else
-      TORCH_CHECK(false, "per-batch scalar scaling requires CUDA >= 13.2");
+      TORCH_CHECK(false, "per-batch scalar scaling requires CUDA >= 13.3");
 #endif
     default:
       TORCH_CHECK(false);
