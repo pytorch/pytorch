887e1d40d6c9a8d4a65beb13d8763aa37bf53be7
shichuanliujie9527 <shichuanliujie@gmail.com>
2026-07-25 17:34:02 +0800

    [FSDP][Test] Fix list_device hardcoded to cuda fallback for out-of-tree accelerators

--- test/distributed/fsdp/test_utils.py
+++ test/distributed/fsdp/test_utils.py
@@ -41,7 +41,8 @@ if TEST_HPU:
 elif TEST_XPU:
     list_device = "xpu"
 else:
-    list_device = "cuda"
+    _acc = torch.accelerator.current_accelerator()
+    list_device = _acc.type if _acc is not None else "cuda"
 
 
 class TestUtils(TestCase):
