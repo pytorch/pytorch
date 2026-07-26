e4760236ea0a77a0a3d68fdb6b5a5f4fd1c88239
JasonTian <tianjiasen@outlook.com>
2026-07-26 08:56:03 +0800

    clean code for lint check

--- test/onnx/pytorch_test_common.py
+++ test/onnx/pytorch_test_common.py
@@ -408,6 +408,8 @@ class ExportTestCase(common_utils.TestCase):
         super().setUp()
         # TODO(#88264): Flaky test failures after changing seed.
         set_rng_seed(0)
-        device_type = getattr(torch.accelerator.current_accelerator(check_available=True), "type", None)
+        device_type = getattr(
+            torch.accelerator.current_accelerator(check_available=True), "type", None
+        )
         if device_type:
             torch.get_device_module(device_type).manual_seed_all(0)
