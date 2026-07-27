137a244db59cca8fe8ae9c912431066357d2601e
djbravo06 <divyanshuj2006@gmail.com>
2026-07-27 22:44:12 +0530

    Update unit tests for non-contiguous inputs

--- test/distributed/test_c10d_gloo.py
+++ test/distributed/test_c10d_gloo.py
@@ -1342,7 +1342,9 @@ class ProcessGroupGlooTest(MultiProcessTestCase):
     @requires_gloo()
     def test_gather_noncontiguous_input(self):
         # Take a column of 2D tensor, such that memory is not dense
-        self._test_gather_basics(lambda t: t.expand(2, 2).tril().contiguous()[:, 0])
+        with self.assertRaisesRegex(RuntimeError, "tensor is not contiguous"):
+            self._test_gather_basics(lambda t: t.expand(2, 2).tril().contiguous()[:, 0])
+
 
     def _test_gather_stress(self, inputs, fn):
         store = c10d.FileStore(self.file_name, self.world_size)
@@ -1478,7 +1480,8 @@ class ProcessGroupGlooTest(MultiProcessTestCase):
     @requires_gloo()
     def test_allgather_noncontiguous_input(self):
         # Take a column of 2D tensor, such that memory is not dense
-        self._test_allgather_basics(lambda t: t.expand(2, 2).tril().contiguous()[:, 0])
+        with self.assertRaisesRegex(RuntimeError, "tensor is not contiguous"):
+            self._test_allgather_basics(lambda t: t.expand(2, 2).tril().contiguous()[:, 0])
 
     @requires_gloo()
     def test_allgather_inference_mode(self):
