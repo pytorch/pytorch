7aef2efa4d57266ae499231886deee177afa854b
Benedykt Bela <benedykt.bela@intel.com>
2026-07-28 12:27:03 +0000

    Just change functions order.
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- test/quantization/fx/test_numeric_suite_fx.py
+++ test/quantization/fx/test_numeric_suite_fx.py
@@ -1964,50 +1964,6 @@ class TestFXNumericSuiteCoreAPIs(FXNumericSuiteQuantizationTestCase):
         ref_shadow = mc_shadows_mp(*example_inputs)
         self.assertEqual(ref_fp32, ref_shadow)
 
-    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
-    def test_extract_weights_cuda(self):
-        # Note: this is not using quantization because quantized kernels do not
-        # work on cuda yet.
-        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        results = extract_weights('a', m1, 'b', m2)
-        extend_logger_results_with_comparison(
-            results, 'a', 'b', compute_sqnr, 'sqnr')
-        self.assert_ns_compare_dict_valid(results)
-
-    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
-    def test_add_loggers_cuda(self):
-        # Note: this is not using quantization because quantized kernels do not
-        # work on cuda yet.
-        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        m1_ns, m2_ns = add_loggers('a', m1, 'b', m2, OutputLogger)
-        datum = torch.randn(1, 1, 1, 1)
-        datum = datum.cuda()
-
-        m1_ns(datum)
-        m2_ns(datum)
-
-        act_compare_dict = extract_logger_info(m1_ns, m2_ns, OutputLogger, 'b')
-        extend_logger_results_with_comparison(
-            act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
-
-    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
-    def test_add_shadow_loggers_cuda(self):
-        # Note: this is not using quantization because quantized kernels do not
-        # work on cuda yet.
-        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
-        m1_shadows_m2 = add_shadow_loggers('a', m1, 'b', m2, OutputLogger)
-        datum = torch.randn(1, 1, 1, 1)
-        datum = datum.cuda()
-
-        m1_shadows_m2(datum)
-
-        act_compare_dict = extract_shadow_logger_info(m1_shadows_m2, OutputLogger, 'b')
-        extend_logger_results_with_comparison(
-            act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
-
     def test_fp16_shadows_fp32(self):
         m = LinearReluFunctional().eval()
         example_inputs = (torch.randn(1, 4),)
@@ -2088,6 +2044,51 @@ class TestFXNumericSuiteCoreAPIs(FXNumericSuiteQuantizationTestCase):
         act_compare_dict = extract_shadow_logger_info(
             mt_shadows_mt_copy, OutputLogger, 'b')
         self.assertTrue(len(act_compare_dict) == 1)
+
+    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
+    def test_extract_weights_cuda(self):
+        # Note: this is not using quantization because quantized kernels do not
+        # work on cuda yet.
+        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        results = extract_weights('a', m1, 'b', m2)
+        extend_logger_results_with_comparison(
+            results, 'a', 'b', compute_sqnr, 'sqnr')
+        self.assert_ns_compare_dict_valid(results)
+
+    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
+    def test_add_loggers_cuda(self):
+        # Note: this is not using quantization because quantized kernels do not
+        # work on cuda yet.
+        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        m1_ns, m2_ns = add_loggers('a', m1, 'b', m2, OutputLogger)
+        datum = torch.randn(1, 1, 1, 1)
+        datum = datum.cuda()
+
+        m1_ns(datum)
+        m2_ns(datum)
+
+        act_compare_dict = extract_logger_info(m1_ns, m2_ns, OutputLogger, 'b')
+        extend_logger_results_with_comparison(
+            act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
+
+    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
+    def test_add_shadow_loggers_cuda(self):
+        # Note: this is not using quantization because quantized kernels do not
+        # work on cuda yet.
+        m1 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        m2 = nn.Sequential(nn.Conv2d(1, 1, 1)).cuda()
+        m1_shadows_m2 = add_shadow_loggers('a', m1, 'b', m2, OutputLogger)
+        datum = torch.randn(1, 1, 1, 1)
+        datum = datum.cuda()
+
+        m1_shadows_m2(datum)
+
+        act_compare_dict = extract_shadow_logger_info(m1_shadows_m2, OutputLogger, 'b')
+        extend_logger_results_with_comparison(
+            act_compare_dict, 'a', 'b', compute_sqnr, 'sqnr')
+
 
 @skipIfNoQNNPACK
 class TestFXNumericSuiteNShadows(FXNumericSuiteQuantizationTestCase):
