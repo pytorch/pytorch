ae46c45d261328ea931a9d0689f9bdaf6fb788b3
JasonTian <tianjiasen@outlook.com>
2026-07-28 09:54:46 +0800

    Use torch.accelerator.current_accelerator(check_available=True)  to get runtime device type

--- test/fx/test_common_passes.py
+++ test/fx/test_common_passes.py
@@ -76,7 +76,9 @@ Test_Cases = [
 ]
 Factory_Test_Cases = [FactoryFunctionCall, MutationFactory]
 Devices = ["cpu"]
-device_type = getattr(torch.accelerator.current_accelerator(), "type", None)
+device_type = getattr(
+    torch.accelerator.current_accelerator(check_available=True), "type", None
+)
 if device_type and device_type not in Devices:
     Devices.append(device_type)
 
