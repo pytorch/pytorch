5cd0d062abfba95ec3e4702f716e887e57d21575
Rob Timpe <rtimpe@openteams.com>
2026-07-29 23:26:13 +0000

    [dynamo, 3.15] Fix pygen_yf
    Fix pygen_yf to account for virtual iterators in python 3.15
    
    
    ghstack-source-id: 6db266c0b2abd30514444e93ecd0338aecebe7a2
    Pull-Request: https://github.com/pytorch/pytorch/pull/190312

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -1199,7 +1199,11 @@ class LocalGeneratorObjectVariable(VariableTracker):
 
     def pygen_yf(self) -> VariableTracker | None:
         if self.inline_tracer.frame_state == FrameState.FRAME_SUSPENDED_YIELD_FROM:
-            return self.inline_tracer.stack[-1]
+            if sys.version_info >= (3, 15):
+                ind = -2
+            else:
+                ind = -1
+            return self.inline_tracer.stack[ind]
         return None
 
     def gen_send_ex2(
