1000523d912929cba617d8c201683c05c3ffa330
William Wen <williamwen@fb.com>
2026-07-30 11:30:13 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -1663,7 +1663,6 @@ torch_c_binding_in_graph_functions = dict.fromkeys(
         "torch._fused_moving_avg_obs_fq_helper",
         "torch._fused_sdp_choice",
         "torch._fw_primal_copy",
-        "torch._grid_sampler_2d_cpu_fallback",
         "torch._grouped_mm",
         "torch._histogramdd_bin_edges",
         "torch._histogramdd_from_bin_cts",
