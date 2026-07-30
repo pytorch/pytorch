02da308e15d1f282d477e7468e93325dbeb94269
drisspg <drisspguessous@gmail.com>
2026-07-30 01:28:45 +0000

    Update (base update)
    [ghstack-poisoned]

--- test/dynamo/test_logging.py
+++ test/dynamo/test_logging.py
@@ -1072,34 +1072,6 @@ Mutating object of type dict (source name: L['mod']._buffers)
         self.assertEqual(concise.artifact_names, set())
         self.assertEqual(verbose.artifact_names, set())
 
-    def test_custom_log_format_respects_trace_id_filter(self):
-        from torch._logging._internal import log_registry, TorchLogsFormatter
-
-        log_name = "torch._inductor.kernel.flex_gemm.debug"
-        record = logging.LogRecord(
-            log_name,
-            logging.INFO,
-            __file__,
-            0,
-            "message",
-            (),
-            None,
-        )
-        with (
-            unittest.mock.patch.dict(
-                log_registry.log_formatters,
-                {log_name: logging.Formatter("%(message)s")},
-            ),
-            unittest.mock.patch.object(
-                torch._guards.CompileContext,
-                "current_trace_id",
-                return_value=None,
-            ),
-        ):
-            self.assertEqual(
-                TorchLogsFormatter(trace_id_filter={"missing"}).format(record), ""
-            )
-
     def test_invalid_artifact_flag(self):
         with self.assertRaises(ValueError):
             torch._logging.set_logs(aot_graphs=5)
