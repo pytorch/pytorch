6fcaad5eb7e7607fa6d16dd82f29c46ecdcc6a12
ccyyy1023 <2637034749@qq.com>
2026-08-01 00:58:10 +0800

    Remove UTF-8 BOM from cudagraph trees

--- torch/_inductor/cudagraph_trees.py
+++ torch/_inductor/cudagraph_trees.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
 CUDA graph trees are a safety abstraction over CUDAGraphs, similar to make_graph_callables,
 which share the same memory pool.  Sharing a memory pool is an extremely
 important optimization when chaining multiple CUDA graphs together, as it
