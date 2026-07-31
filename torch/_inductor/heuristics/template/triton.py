c7a040e6852be5b3e6b8f61c94e1f6468829b848
dolpm <34420038+dolpm@users.noreply.github.com>
2026-07-31 14:24:52 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_inductor/heuristics/template/triton.py
+++ torch/_inductor/heuristics/template/triton.py
@@ -18,7 +18,6 @@ from torch.utils._sympy.functions import Min, Mod
 from torch.utils._triton import has_triton_stable_tma_api
 
 from ... import config
-from ...autows_utils import meta_ws_enabled
 from ...kernel.bmm import bmm_template
 from ...kernel.mm import (
     blackwell_ws_persistent_device_tma_mm_template,
@@ -63,7 +62,7 @@ def _origami_enabled() -> bool:
     return config.rocm.origami
 
 
-USE_META_WS = meta_ws_enabled()
+USE_META_WS = os.environ.get("TRITON_USE_META_WS", "0") == "0"
 
 # Check if running on ROCm
 IS_ROCM = torch.version.hip is not None
@@ -2682,7 +2681,7 @@ class BlackwellTMATemplateConfigMixin(TMATemplateConfigMixin):
             flatten = (
                 template_kwargs.get("FLATTEN", True)
                 and not constraints_violated
-                and not USE_META_WS
+                and USE_META_WS
             )
             yield {
                 **template_kwargs,
