73cfcb76c8da9693836fb832f024bdb5ce6927ff
Guilherme Leobas <gleobas@quansight.com>
2026-08-05 16:50:45 +0000

    Update
    [ghstack-poisoned]

--- test/cpython/v3_13/string_tests.py
+++ test/cpython/v3_13/string_tests.py
@@ -7,52 +7,7 @@
 # Test copied from
 # https://raw.githubusercontent.com/python/cpython/refs/tags/v3.13.5/Lib/test/string_tests.py
 
-import sys
-import torch
-import torch._dynamo.test_case
-import unittest
-from torch._dynamo.test_case import CPythonTestCase
-from torch.testing._internal.common_utils import (
-    run_tests,
-    skipIfTorchDynamo,
-    TEST_WITH_TORCHDYNAMO,
-)
-
-__TestCase = CPythonTestCase
-
-
-# redirect import statements
-import sys
-import importlib.abc
-
-redirect_imports = (
-    "test.mapping_tests",
-    "test.typinganndata",
-    "test.test_grammar",
-    "test.test_math",
-    "test.test_iter",
-    "test.typinganndata.ann_module",
-)
-
-class RedirectImportFinder(importlib.abc.MetaPathFinder):
-    def find_spec(self, fullname, path, target=None):
-        # Check if the import is the problematic one
-        if fullname in redirect_imports:
-            try:
-                # Attempt to import the standalone module
-                name = fullname.removeprefix("test.")
-                r = importlib.import_module(name)
-                # Redirect the module in sys.modules
-                sys.modules[fullname] = r
-                # Return a module spec from the found module
-                return importlib.util.find_spec(name)
-            except ImportError:
-                return None
-        return None
-
-# Add the custom finder to sys.meta_path
-sys.meta_path.insert(0, RedirectImportFinder())
-
+from torch.testing._internal.common_utils import skipIfTorchDynamo
 
 # ======= END DYNAMO PATCH =======
 
