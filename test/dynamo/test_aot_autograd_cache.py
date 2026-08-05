4483abd89091cd8f6719737cf214448d633c8d3c
Rob Timpe <rtimpe@openteams.com>
2026-08-05 00:25:50 +0000

    [DO NOT MERGE] Dynamo 3.15 support
    ghstack-source-id: e3ea687872b71df36f5b70a4143e5d5bf80964dc
    Pull-Request: https://github.com/pytorch/pytorch/pull/178393

--- test/dynamo/test_aot_autograd_cache.py
+++ test/dynamo/test_aot_autograd_cache.py
@@ -9,6 +9,7 @@ import operator
 import os
 import pickle
 import shutil
+import sys
 import unittest
 from collections.abc import Sequence
 from typing import Literal
@@ -66,6 +67,7 @@ from torch.testing._internal.common_utils import (
     TEST_WITH_ASAN,
     TEST_WITH_SLOW,
     TEST_XPU,
+    xfailIf,
 )
 from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_GPU, requires_triton
 from torch.testing._internal.triton_utils import requires_gpu_and_triton
@@ -3913,6 +3915,8 @@ class AOTAutogradCachePicklerTests(torch._dynamo.test_case.TestCase):
             self.assertEqual(c1, c3)
             self.assertEqual(c1, c4)
 
+    # dill doesn't support 3.15 yet
+    @xfailIf(sys.version_info >= (3, 15))
     def test_dill_serialization_with_inner_functions(self):
         """
         Test that with dill, we can now serialize graphs that contain inner functions
