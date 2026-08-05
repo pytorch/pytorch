22272e767ac6c5253b70d36c354262f5688935b2
ethanwee1 <ethanwee@amd.com>
2026-08-05 12:37:07 +0000

    [ROCm][CI] Derive wheel spec from ROCm version

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -205,7 +205,6 @@ case "$tag" in
     GCC_VERSION=13
     ROCM_VERSION=7.14
     THEROCK_INDEX_URL="https://repo.amd.com/rocm/whl-multi-arch/"
-    ROCM_PIP_SPEC="rocm[libraries,devel,device-all]==7.14.*"
     TRITON=yes
     KATEX=yes
     PYTORCH_ROCM_ARCH="gfx90a;gfx942;gfx950;gfx1100"
@@ -218,7 +217,6 @@ case "$tag" in
     GCC_VERSION=13
     ROCM_VERSION=7.15.0a20260712
     THEROCK_INDEX_URL="https://rocm.nightlies.amd.com/whl-multi-arch/"
-    ROCM_PIP_SPEC="rocm[libraries,devel,device-all]==7.15.0a20260712"
     USE_MSLK=0
     TRITON=yes
     KATEX=yes
@@ -410,7 +408,6 @@ build_image() {
        --build-arg "CUDA_VERSION=${CUDA_VERSION}" \
        --build-arg "KATEX=${KATEX:-}" \
        --build-arg "ROCM_VERSION=${ROCM_VERSION:-}" \
-       --build-arg "ROCM_PIP_SPEC=${ROCM_PIP_SPEC:-}" \
        --build-arg "THEROCK_INDEX_URL=${THEROCK_INDEX_URL:-}" \
        --build-arg "USE_MSLK=${USE_MSLK:-}" \
        --build-arg "PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH}" \
