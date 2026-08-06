a1779dd0a5d0d161cd03c879d2ba1769b02016e2
KarhouTam <karhou.tam@outlook.com>
2026-08-06 09:33:43 +0000

    Update (base update)
    [ghstack-poisoned]

--- test/test_bmm_outer_product.py
+++ test/test_bmm_outer_product.py
@@ -11,10 +11,16 @@ from torch.testing._internal.common_device_type import (
     onlyAccelerator,
     skipXPUIf,
 )
-from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.testing._internal.common_utils import (
+    HardwareClassification,
+    run_tests,
+    TestCase,
+)
 
 
 class TestBmmOuterProductDevice(TestCase):
+    hw_classification = HardwareClassification.ACCELERATOR
+
     def _check_bmm(self, a, b, **kwargs):
         self.assertEqual(torch.bmm(a, b), a @ b, **kwargs)
 
