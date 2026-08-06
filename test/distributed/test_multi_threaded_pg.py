be4a694707a8f5fcbab13abe98f5c3f8fbaae7e0
NayanNagabhushana-28 <nkangana@redhat.com>
2026-08-06 15:32:37 +0000

    Modified the enum and updated instantiate_device_type_tests to exclude mps and xpu support

--- test/distributed/test_multi_threaded_pg.py
+++ test/distributed/test_multi_threaded_pg.py
@@ -319,7 +319,7 @@ class TestCollectivesWithBaseClassGeneric(MultiThreadedTestCase):
 
 
 class TestCollectivesWithBaseClassMultiAccelerator(MultiThreadedTestCase):
-    hw_classification = HardwareClassification.MULTI_ACCELERATOR
+    hw_classification = HardwareClassification.ACCELERATOR
 
     @property
     def world_size(self):
@@ -451,8 +451,6 @@ instantiate_device_type_tests(
     TestCollectivesWithBaseClassMultiAccelerator,
     globals(),
     except_for="cpu",
-    allow_mps=True,
-    allow_xpu=True,
 )
 
 
