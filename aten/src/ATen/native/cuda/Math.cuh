d0acb407b1b33b3d24051900d8d0296c426bdc4e
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-08-06 10:01:28 -0700

    Guard this for now

--- aten/src/ATen/native/cuda/Math.cuh
+++ aten/src/ATen/native/cuda/Math.cuh
@@ -299,7 +299,10 @@ const auto digamma_string = jiterator_stringify(
 const auto zeta_string = jiterator_stringify(
   template <typename T>
   T zeta(T x, T q) {
-    constexpr T MACHEP = sizeof(T) <= sizeof(float)
+    static_assert(
+        sizeof(T) == sizeof(float) || sizeof(T) == sizeof(double),
+        "zeta supports only float or double computation");
+    constexpr T MACHEP = sizeof(T) == sizeof(float)
         ? T{5.9604644775390625E-8}
         : T{1.11022302462515654042E-16};
     constexpr T zero{0};
@@ -1436,7 +1439,10 @@ const auto airy_ai_string = jiterator_stringify(
         T t = 1.0;
         T z = x * x * x;
 
-        constexpr T epsilon = sizeof(T) <= sizeof(float)
+        static_assert(
+            sizeof(T) == sizeof(float) || sizeof(T) == sizeof(double),
+            "airy_ai supports only float or double computation");
+        constexpr T epsilon = sizeof(T) == sizeof(float)
             ? T{1.1920928955078125E-7}
             : T{2.22044604925031308085E-16};
         while (t > epsilon) {
