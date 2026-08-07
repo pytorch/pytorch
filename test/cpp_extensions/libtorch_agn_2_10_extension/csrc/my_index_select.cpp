dcf02e51371deb3050899643462349c1941e8979
Chris Leonard <chleonar@redhat.com>
2026-08-07 20:14:16 +0000

    Add stable Tensor overloads for index_select, floor_divide, and is_pinned (2.14+)  (#192097)
    ## Summary
    - Add `torch::stable::{index_select,floor_divide,is_pinned}` Tensor–Tensor wrappers (gated at 2.14) via `torch_call_dispatcher`
    - Document the new APIs in the stable operators docs
    - Cover them in the 2.14 libtorch-agnostic extension tests
    
    'stacked' on top of:
    https://github.com/pytorch/pytorch/pull/192083
    https://github.com/pytorch/pytorch/pull/191973
    Will need to rebase once those are merged.
    
    see commits https://github.com/pytorch/pytorch/pull/192097/changes/c4e0c5c1ef85bb93cd0d6e3cc46cff6446222db7..cb760bb831420502c80f49fa7881c6050a029807 for new edits in this PR
    
    ## Test plan
    - [x] `python -m pytest test/cpp_extensions/test_libtorch_agnostic.py -k 'test_my_index_select or test_my_floor_divide or test_my_is_pinned' -v`
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/192097
    Approved by: https://github.com/janeyx99

--- test/cpp_extensions/libtorch_agn_2_10_extension/csrc/my_index_select.cpp
+++ test/cpp_extensions/libtorch_agn_2_10_extension/csrc/my_index_select.cpp
@@ -4,16 +4,14 @@
 
 using torch::stable::Tensor;
 
-Tensor my_permute(
-    const Tensor& self,
-    torch::headeronly::IntHeaderOnlyArrayRef dims) {
-  return torch::stable::permute(self, dims);
+Tensor my_index_select(const Tensor& self, int64_t dim, const Tensor& index) {
+  return torch::stable::index_select(self, dim, index);
 }
 
 STABLE_TORCH_LIBRARY_FRAGMENT(STABLE_LIB_NAME, m) {
-  m.def("my_permute(Tensor self, int[] dims) -> Tensor");
+  m.def("my_index_select(Tensor self, int dim, Tensor index) -> Tensor");
 }
 
 STABLE_TORCH_LIBRARY_IMPL(STABLE_LIB_NAME, CompositeExplicitAutograd, m) {
-  m.impl("my_permute", TORCH_BOX(&my_permute));
+  m.impl("my_index_select", TORCH_BOX(&my_index_select));
 }
