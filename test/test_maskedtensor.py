c833ea69a38c5f944408cce0221f76dce0ee3302
gplutop7 <grzegorzx.pluto-prondzinski@intel.com>
2026-08-07 13:30:08 +0000

    Enable XPU for maskedtensor device tests

--- test/test_maskedtensor.py
+++ test/test_maskedtensor.py
@@ -1011,9 +1011,10 @@ class TestOperators(TestCase):
         self._test_reduction_equality(device, dtype, op, layout)
 
 
-instantiate_device_type_tests(TestOperators, globals())
+only_for = ("cpu", "cuda", "xpu")
+instantiate_device_type_tests(TestOperators, globals(), only_for=only_for, allow_xpu=True)
 
-instantiate_device_type_tests(TestBasics, globals())
+instantiate_device_type_tests(TestBasics, globals(), only_for=only_for, allow_xpu=True)
 instantiate_parametrized_tests(TestUnary)
 instantiate_parametrized_tests(TestBinary)
 instantiate_parametrized_tests(TestReductions)
