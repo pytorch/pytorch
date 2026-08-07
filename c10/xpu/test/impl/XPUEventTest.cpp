9d9b954a6a092c8b4cf5c731096d575240b4e64b
Yu, Guangye <guangye.yu@intel.com>
2026-08-07 16:49:51 +0000

    [INITIAL] Update
    [ghstack-poisoned]

--- c10/xpu/test/impl/XPUEventTest.cpp
+++ c10/xpu/test/impl/XPUEventTest.cpp
@@ -13,12 +13,12 @@ TEST(XPUEventTest, IPCSupport) {
   }
 
 #if SYCL_COMPILER_VERSION >= 20260200
-  if (!c10::xpu::get_raw_device(c10::xpu::current_device())
-           .has(sycl::aspect::ext_oneapi_ipc_event)) {
-    c10::xpu::XPUEvent event0(false, true);
-    EXPECT_THROW(event0.record(), c10::Error);
-    return;
-  }
+  // if (!c10::xpu::get_raw_device(c10::xpu::current_device())
+  //          .has(sycl::aspect::ext_oneapi_ipc_event)) {
+  //   c10::xpu::XPUEvent event0(false, true);
+  //   EXPECT_THROW(event0.record(), c10::Error);
+  //   return;
+  // }
   c10::xpu::XPUEvent event0(true, true);
   EXPECT_THROW(event0.record(), c10::Error);
 
