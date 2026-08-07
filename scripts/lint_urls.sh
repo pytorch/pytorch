aefd7ce4786ec4ad14f083e9d48586bb7e52bfc2
PrabinDevkota <prabindev789@gmail.com>
2026-08-07 20:14:28 +0530

    [dynamo] Fix cpython diff sync CI (spin, url lint, PR size)

--- scripts/lint_urls.sh
+++ scripts/lint_urls.sh
@@ -80,6 +80,8 @@ done < <(
     ':(exclude,glob)**/third-party/**'
     ':(exclude,glob)**/third_party/**'
     ':(exclude,glob)torch/_vendor/quack/**'
+    # Upstream CPython tests carry historical/dead links; headers point at
+    # raw.githubusercontent.com tags we already pin in each file.
     ':(exclude,glob)test/cpython/**'
   )
   if [ $# -eq 2 ]; then
