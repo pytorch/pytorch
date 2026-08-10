2c96606a7815a2ce726affb478381f481fd9995a
Jane Xu <janeyx@meta.com>
2026-08-10 12:14:51 -0700

    Update (base update)
    [ghstack-poisoned]

--- c10/util/complex.h
+++ c10/util/complex.h
@@ -65,6 +65,4 @@ constexpr c10::complex<T> conj(const c10::complex<T>& z) {
 #define C10_INTERNAL_INCLUDE_COMPLEX_REMAINING_H
 // math functions are included in a separate file
 #include <c10/util/complex_math.h> // IWYU pragma: keep
-// utilities for complex types
-#include <c10/util/complex_utils.h> // IWYU pragma: keep
 #undef C10_INTERNAL_INCLUDE_COMPLEX_REMAINING_H
