14840baf182615d41d8992c649e9367cf6a8f081
Kurt Mohler <kmohler@openteams.com>
2026-08-12 18:06:25 -0500

    Update
    [ghstack-poisoned]

--- aten/src/ATen/mps/MPSHooks.mm
+++ aten/src/ATen/mps/MPSHooks.mm
@@ -75,18 +75,18 @@ void MPSHooks::deviceSynchronize() const {
 }
 
 void MPSHooks::commitStream() const {
-  at::mps::getDefaultMPSStream()->synchronize(SyncType::COMMIT);
+  at::mps::getCurrentMPSStream()->synchronize(SyncType::COMMIT);
 }
 
 void* MPSHooks::getCommandBuffer() const {
-  auto stream = at::mps::getDefaultMPSStream();
+  auto stream = at::mps::getCurrentMPSStream();
   // Release pending computeCommandEncoder, as extensions is likely to allocate new one
   stream->endKernelCoalescing();
   return stream->commandBuffer();
 }
 
 void* MPSHooks::getDispatchQueue() const {
-  return at::mps::getDefaultMPSStream()->queue();
+  return at::mps::getCurrentMPSStream()->queue();
 }
 
 void MPSHooks::emptyCache() const {
