190a54c382ed68f2032181bcf78ede03bb84dd2c
Nikita Shulga <nikita.shulga@gmail.com>
2026-08-13 23:13:50 -0700

    Update
    [ghstack-poisoned]

--- test/torch_np/test_nep50_examples.py
+++ test/torch_np/test_nep50_examples.py
@@ -207,7 +207,7 @@ class TestCompareToNumpy(TestCase):
 
             try:
                 result = ufunc(scalar, array)
-            except RuntimeError, TypeError:
+            except (RuntimeError, TypeError):
                 # RuntimeError: "bitwise_xor_cpu" not implemented for 'ComplexDouble' etc
                 result = None
 
