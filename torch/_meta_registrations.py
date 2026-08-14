330e4d9e429cea143182df89cc2ee88ef213f3e5
Khushi Agrawal <khushiagrawal411@gmail.com>
2026-08-14 14:19:55 +0530

    [meta] fix multiple ops `out=` dtype promotion

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -8184,7 +8184,7 @@ def _amp_foreach_non_finite_check_and_unscale_(self, found_inf, inv_scale):
 
 # From aten/src/ATen/native/UnaryOps.cpp
 @register_meta([aten.nan_to_num.default, aten.nan_to_num.out])
-@out_wrapper()
+@out_wrapper(exact_dtype=True)
 def nan_to_num(self, nan=None, posinf=None, neginf=None):
     return torch.empty_like(self)
 
