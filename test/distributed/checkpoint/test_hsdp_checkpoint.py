7f3bd493c7400365f15ac5f2dab820672e1bdd89
lixing-he <lixing6484@gmail.com>
2026-08-15 11:01:15 +0000

    [Test] Add label to test_hsdp_checkpoint.py

--- test/distributed/checkpoint/test_hsdp_checkpoint.py
+++ test/distributed/checkpoint/test_hsdp_checkpoint.py
@@ -17,9 +17,10 @@ from torch.distributed.fsdp.fully_sharded_data_parallel import (
     StateDictType,
 )
 from torch.distributed.tensor import Replicate
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_utils import (
-    instantiate_parametrized_tests,
+    HardwareClassification,
     parametrize,
     run_tests,
 )
@@ -72,6 +73,8 @@ class SimpleModelUneven(torch.nn.Module):
 
 
 class TestHSDPCheckpoint(DTensorTestBase):
+    hw_classification = HardwareClassification.ACCELERATOR
+
     @property
     def backend(self):
         curr_backend = dist.get_default_backend_for_device(self.device_type)
@@ -209,6 +212,13 @@ class TestHSDPCheckpoint(DTensorTestBase):
             self.assertEqual(v1_all_gather.to_local(), v2_all_gather.to_local())
 
 
-instantiate_parametrized_tests(TestHSDPCheckpoint)
+instantiate_device_type_tests(
+    TestHSDPCheckpoint,
+    globals(),
+    except_for=["cpu"],
+    allow_xpu=True,
+)
+
+
 if __name__ == "__main__":
     run_tests()
