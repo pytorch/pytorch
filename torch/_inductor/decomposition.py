f8674cf0fed2ba5f773b682fcf9c5fa5c2d4c6f7
GdoongMathew <gdoongmathew@gmail.com>
2026-08-16 15:53:55 +0800

    register divmod_tensor inductor decomposition

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -1127,6 +1127,17 @@ def _foreach_lerp_scalarlist(
     )
 
 
+@register_decomposition(aten.divmod.Tensor)
+def divmod_tensor(
+    self: torch.Tensor,
+    other: torch.Tensor,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    return (
+        aten.floor_divide(self, other),
+        aten.remainder(self, other),
+    )
+
+
 @aten.miopen_batch_norm.default.py_impl(torch._C.DispatchKey.Autograd)
 @register_decomposition(aten.miopen_batch_norm)
 def miopen_batch_norm(
