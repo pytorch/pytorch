45da9cab0550230ec69a6219dffeda7b1b58cc3a
Yu, Guangye <guangye.yu@intel.com>
2026-08-18 13:56:51 +0000

    Introduce get_rng_offset&set_rng_offset into torch.accelerator.random
    ghstack-source-id: 026a857e14f2533b7a4673c341b6f7c75c6555bb
    Pull-Request: https://github.com/pytorch/pytorch/pull/190004

--- test/test_accelerator.py
+++ test/test_accelerator.py
@@ -327,6 +327,7 @@ class TestAccelerator(TestCase):
             initial_seed = torch.accelerator.random.initial_seed(device_idx)
             self.assertIsInstance(initial_seed, int)
             old_state = torch.accelerator.random.get_rng_state(device_idx)
+            old_offset = torch.accelerator.random.get_rng_offset(device_idx)
             self.assertIsInstance(old_state, torch.Tensor)
             torch.accelerator.random.manual_seed(12345, device_idx)
             new_state = torch.accelerator.random.get_rng_state(device_idx)
@@ -338,6 +339,9 @@ class TestAccelerator(TestCase):
             )
             self.assertEqual(
                 initial_seed, torch.accelerator.random.initial_seed(device_idx)
+            )
+            self.assertEqual(
+                old_offset, torch.accelerator.random.get_rng_offset(device_idx)
             )
             torch.accelerator.random.seed(device_idx)
             self.assertNotEqual(
