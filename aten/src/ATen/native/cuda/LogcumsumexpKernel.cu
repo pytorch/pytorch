5a54183192278f3f9bbd8afdddb8f877c1af4112
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-08-18 01:16:50 +0000

    Revert "[BE][Ez]: Update c10::complex kernels to use sincos (#193729)"
    This reverts commit e734072b6f48acc6b0295811c60c06550777ac16.
    
    Reverted https://github.com/pytorch/pytorch/pull/193729 on behalf of https://github.com/jeanschmidt due to it have problems with CCCL 2.4.0 and older ([comment](https://github.com/pytorch/pytorch/pull/193729#issuecomment-5322208751))

--- aten/src/ATen/native/cuda/LogcumsumexpKernel.cu
+++ aten/src/ATen/native/cuda/LogcumsumexpKernel.cu
@@ -5,7 +5,6 @@
 
 #include <ATen/native/cuda/ScanKernels.h>
 #include <ATen/native/cuda/ScanUtils.cuh>
-#include <c10/cuda/CUDAMathCompat.h>
 
 #include <cmath>
 #include <limits>
@@ -14,7 +13,7 @@ namespace at::native {
 
 // custom min and max to be used in logcumsumexp for complex arguments
 template <typename scalar_t, bool min>
-__device__ c10::complex<scalar_t> _logcumsumexp_minmax(const c10::complex<scalar_t>& x, const c10::complex<scalar_t>& y) {
+__host__ __device__ c10::complex<scalar_t> _logcumsumexp_minmax(const c10::complex<scalar_t>& x, const c10::complex<scalar_t>& y) {
   scalar_t xr = std::real(x);
   scalar_t yr = std::real(y);
   if (::isnan(yr) || (::isnan(std::imag(y)))) {
@@ -29,7 +28,7 @@ __device__ c10::complex<scalar_t> _logcumsumexp_minmax(const c10::complex<scalar
 }
 
 template <typename scalar_t>
-__device__ scalar_t _log_add_exp_helper(const scalar_t& x, const scalar_t& y) {
+__host__ __device__ scalar_t _log_add_exp_helper(const scalar_t& x, const scalar_t& y) {
   // Reference : https://www.tensorflow.org/api_docs/python/tf/math/cumulative_logsumexp
   // Using the original expression: `at::_isnan(y) ? y : std::min(x, y)` causes an error in ROCM
   auto isnan_x = at::_isnan(x);
@@ -46,29 +45,25 @@ __device__ scalar_t _log_add_exp_helper(const scalar_t& x, const scalar_t& y) {
 }
 
 template <typename scalar_t>
-__device__ c10::complex<scalar_t> _fast_build_exp(const c10::complex<scalar_t>& x) {
+__host__ __device__ c10::complex<scalar_t> _fast_build_exp(const c10::complex<scalar_t>& x) {
   // complex exponential function, but implemented manually to get fast compilation time
   // this function only handles the case where the x is finite (not inf nor nan)
   auto xreal = std::real(x);
   auto ximag = std::imag(x);
   auto exp_x_abs = std::exp(xreal);
-  scalar_t sin_ximag;
-  scalar_t cos_ximag;
-  c10::cuda::compat::sincos(ximag, &sin_ximag, &cos_ximag);
-  auto exp_x_real = exp_x_abs * cos_ximag;
-  auto exp_x_imag = exp_x_abs * sin_ximag;
+  auto exp_x_real = exp_x_abs * std::cos(ximag);
+  auto exp_x_imag = exp_x_abs * std::sin(ximag);
   return {exp_x_real, exp_x_imag};
 }
 
 template <typename scalar_t>
-__device__ c10::complex<scalar_t> _fast_build_exp_inf(const c10::complex<scalar_t>& x) {
+__host__ __device__ c10::complex<scalar_t> _fast_build_exp_inf(const c10::complex<scalar_t>& x) {
   // complex exponential function, but implemented manually to get fast compilation time
   // this function only handles the case where the real part of x is infinite
   auto ximag = std::imag(x);
   auto exp_x_abs = std::numeric_limits<scalar_t>::infinity();
-  scalar_t sin;
-  scalar_t cos;
-  c10::cuda::compat::sincos(ximag, &sin, &cos);
+  auto sin = std::sin(ximag);
+  auto cos = std::cos(ximag);
   // special case if the angle is exactly the multiple of pi/2
   auto exp_x_real = (cos == 0) ? (scalar_t)0.0 : exp_x_abs * cos;
   auto exp_x_imag = (sin == 0) ? (scalar_t)0.0 : exp_x_abs * sin;
@@ -76,7 +71,7 @@ __device__ c10::complex<scalar_t> _fast_build_exp_inf(const c10::complex<scalar_
 }
 
 template <typename scalar_t>
-__device__ c10::complex<scalar_t> _log_add_exp_helper(const c10::complex<scalar_t>& x, const c10::complex<scalar_t>& y) {
+__host__ __device__ c10::complex<scalar_t> _log_add_exp_helper(const c10::complex<scalar_t>& x, const c10::complex<scalar_t>& y) {
   c10::complex<scalar_t> min = _logcumsumexp_minmax<scalar_t, /*min=*/true>(x, y);
   c10::complex<scalar_t> max = _logcumsumexp_minmax<scalar_t, /*min=*/false>(x, y);
   scalar_t min_real = std::real(min);
@@ -112,7 +107,7 @@ void launch_logcumsumexp_cuda_kernel(const TensorBase& result, const TensorBase&
       [&]() {
         using opmath_t = at::opmath_type<scalar_t>;
         scalar_t init = -std::numeric_limits<scalar_t>::infinity();
-        auto log_add_exp = [] C10_DEVICE (const scalar_t x_, const scalar_t y_) -> scalar_t {
+        auto log_add_exp = [] C10_HOST_DEVICE (const scalar_t x_, const scalar_t y_) -> scalar_t {
           const opmath_t x{x_}, y{y_};
           return _log_add_exp_helper(x, y);
         };
