31cc5d24a5a1a7ff13abfc24d80b6151d592fc07
Z Liu <zhuojin.liu@amd.com>
2026-08-18 09:38:52 -0400

    bump to rc3

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -204,7 +204,7 @@ case "$tag" in
       ANACONDA_PYTHON_VERSION=3.12
     fi
     GCC_VERSION=13
-    ROCM_VERSION=10.0.0rc2
+    ROCM_VERSION=10.0.0rc3
     THEROCK_INDEX_URL="https://rocm.prereleases.amd.com/whl-multi-arch/"
     TRITON=yes
     KATEX=yes
