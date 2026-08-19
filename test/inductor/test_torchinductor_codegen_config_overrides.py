3f1c7582d7bca889f1e02a3f789b3b347101327c
chenm-123 <18729740621@163.com>
2026-08-19 10:39:45 +0800

    Add HardwareClassification label to test_torchinductor_codegen_config_overrides.py
    - Add hw_classification = HardwareClassification.GENERIC to CodegenInductorTest
    
    Metadata-only change. No functional impact.

--- test/inductor/test_torchinductor_codegen_config_overrides.py
+++ test/inductor/test_torchinductor_codegen_config_overrides.py
@@ -11,6 +11,7 @@ from torch._inductor import config
 from torch._inductor.test_case import TestCase as InductorTestCase
 from torch._inductor.utils import run_and_get_code
 from torch.testing._internal.common_utils import (
+    HardwareClassification,
     instantiate_parametrized_tests,
     parametrize,
 )
@@ -28,6 +29,8 @@ importlib.import_module("filelock")
 
 @instantiate_parametrized_tests
 class CodegenInductorTest(InductorTestCase):
+    hw_classification = HardwareClassification.GENERIC
+
     def run_and_compare(
         self,
         func: Callable[..., Any],
