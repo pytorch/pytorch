88be5baa9a2a6366fbf671e8faf040b2fc3ced52
zouliuchangsong-debug <zouliuchangsong@gmail.com>
2026-08-19 16:19:09 +0800

    remove redundant import triton

--- test/export/test_experimental.py
+++ test/export/test_experimental.py
@@ -33,7 +33,6 @@ from torch.testing._internal.common_device_type import (
 )
 from torch.testing._internal.common_utils import HardwareClassification
 from torch.utils import _pytree as pytree
-from torch.utils._triton import has_triton
 
 
 
