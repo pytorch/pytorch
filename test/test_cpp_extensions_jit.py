7afde87d2788b32daa283537c1541c4f49653c87
ethanwee1 <ethanwee@amd.com>
2026-08-19 17:09:50 +0000

    [ROCm] Remove added version coverage
    Keep the SDK and HIP handling changes while dropping the additional unit
    tests added during review hardening.

--- test/test_cpp_extensions_jit.py
+++ test/test_cpp_extensions_jit.py
@@ -12,7 +12,6 @@ import sys
 import tempfile
 import unittest
 import warnings
-from unittest import mock
 
 import torch
 import torch.backends.cudnn
@@ -82,55 +81,6 @@ with tempfile.TemporaryDirectory() as tmpdir:
             text=True,
         )
         self.assertEqual(result.returncode, 0, result.stdout + result.stderr)
-
-    def test_rocm_build_directory_includes_sdk_and_hip_versions(self):
-        with (
-            tempfile.TemporaryDirectory() as tmpdir,
-            mock.patch.dict(os.environ),
-            mock.patch.object(
-                torch.utils.cpp_extension,
-                "get_default_build_root",
-                return_value=tmpdir,
-            ),
-            mock.patch.object(torch.version, "rocm", "10.1.0"),
-            mock.patch.object(torch.version, "hip", "7.15.26306"),
-        ):
-            os.environ.pop("TORCH_EXTENSIONS_DIR", None)
-            build_directory = torch.utils.cpp_extension._get_build_directory(
-                "test_extension", verbose=False
-            )
-
-        expected_root = (
-            f"py{sys.version_info.major}{sys.version_info.minor}"
-            f"{getattr(sys, 'abiflags', '')}_rocm10.1.0_hip7.15.26306"
-        )
-        self.assertEqual(
-            build_directory,
-            os.path.join(tmpdir, expected_root, "test_extension"),
-        )
-
-    def test_rocm_build_directory_version_key_does_not_collide(self):
-        def get_build_directory(tmpdir, sdk_version):
-            with (
-                mock.patch.dict(os.environ),
-                mock.patch.object(
-                    torch.utils.cpp_extension,
-                    "get_default_build_root",
-                    return_value=tmpdir,
-                ),
-                mock.patch.object(torch.version, "rocm", sdk_version),
-                mock.patch.object(torch.version, "hip", "7.15.26306"),
-            ):
-                os.environ.pop("TORCH_EXTENSIONS_DIR", None)
-                return torch.utils.cpp_extension._get_build_directory(
-                    "test_extension", verbose=False
-                )
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            first = get_build_directory(tmpdir, "10.1.10")
-            second = get_build_directory(tmpdir, "10.11.0")
-
-        self.assertNotEqual(first, second)
 
 
 # There's only one test that runs gradcheck, run slow mode manually
