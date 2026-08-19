73d56dbee73b7fbfb0cacbb9256b89a7d6639333
Z Liu <zhuojin.liu@amd.com>
2026-08-19 17:36:21 -0400

    Test parallelism impact on test run time

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -217,6 +217,10 @@ if [[ -z "${OMP_NUM_THREADS:-}" ]] && [[ -n "${USE_ARC:-}" ]]; then
   export OMP_NUM_THREADS
 fi
 
+if [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
+  export OMP_NUM_THREADS=8
+fi
+
 export LANG=C.UTF-8
 
 PR_NUMBER=${PR_NUMBER:-${CIRCLE_PR_NUMBER:-}}
