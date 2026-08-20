a9d210f8c5b25390fe0dd20a7cfe88c820ecb0ba
Benedykt Bela <benedykt.bela@intel.com>
2026-08-20 10:04:19 +0000

    Add missing HardwareCLassification for AssociativeScanTests
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- test/functorch/test_control_flow.py
+++ test/functorch/test_control_flow.py
@@ -5037,6 +5037,8 @@ class AssociativeScanModels:
 @unittest.skipIf(IS_WINDOWS, "Windows not supported for this test")
 @skipIfNoDynamoSupport
 class AssociativeScanTests(TestCase):
+    hw_classification = HardwareClassification.GENERIC
+
     def setUp(self):
         torch._dynamo.reset()
         super().setUp()
