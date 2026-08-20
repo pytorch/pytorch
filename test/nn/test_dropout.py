eeedd9a9770615013b05df222931d87508d1467b
zengxian <xiangdong.zeng@intel.com>
2026-08-20 14:38:22 +0800

    test/nn/test_dropout.py

--- test/nn/test_dropout.py
+++ test/nn/test_dropout.py
@@ -321,7 +321,7 @@ class TestDropoutNNDeviceType(NNTestCase):
                         )
 
 
-instantiate_device_type_tests(TestDropoutNNDeviceType, globals(), allow_mps=True)
+instantiate_device_type_tests(TestDropoutNNDeviceType, globals(), allow_mps=True, allow_xpu=True)
 instantiate_parametrized_tests(TestDropoutNN)
 
 if __name__ == "__main__":
